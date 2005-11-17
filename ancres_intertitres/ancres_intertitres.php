7cc5964ecb3a971ce6e17065f49c047f3636acf2
james@rezo.net <james@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2005-11-17 17:02:41 +0000

    plugin: table des matieres
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@1302 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ ancres_intertitres/ancres_intertitres.php
@@ -0,0 +1,73 @@
+<?php
+
+//Quelques codes de langues
+//$GLOBALS['i18n_spip_fr']['table_matiere'] = 'Table des mati&egrave;res';
+//fausse des codes si place ici.
+
+//
+// http://www.spip-contrib.net/spikini/VarianteContribAjouter-des-ID-aux-intertitres
+//
+function ancres_intertitres($texte) {
+	$regexp = "/{{{(.*?)}}}/";
+	$texte = preg_replace_callback($regexp, 'remplace_intertitre', $texte);
+	return $texte;
+}
+
+function remplace_intertitre($matches) {
+	static $cId = 0;
+	$cId++;
+	$url = translitteration(corriger_caracteres(
+		supprimer_tags(supprimer_numero(extraire_multi($matches[1])))
+	));
+	$url = @preg_replace(',[[:punct:][:space:]]+,u', ' ', $url);
+	// S'il reste des caracteres non latins, utiliser l'id a la place
+	if (preg_match(",[^a-zA-Z0-9 ],", $url)) {
+		$url = "ancre$cId";
+	}
+	else {
+		$mots = explode(' ', $url);
+		$url = '';
+		foreach ($mots as $mot) {
+			if (!$mot) continue;
+			$url2 = $url.'-'.$mot;
+			if (strlen($url2) > 35) {
+				break;
+			}
+		$url = $url2;
+		}
+		$url = substr($url, 1);
+		if (strlen($url) < 2) $url = "ancre$cId";
+	}
+	table_matiere('', $url, $matches[1]);
+	return '{{{ ['.$url.'<-] '.$matches[1].' }}}';
+}
+
+//
+//balise #TABLE_MATIERE
+//
+function balise_TABLE_MATIERE_dist($p) {
+	$p->statut = 'php';
+	$p->code = "compose_table_matiere(
+	'\t<li><a href=\"#@url@\">@titre@</a></li>\n',
+	'\n<ul>\n@texte@</ul>\n'," .
+	"table_matiere('retour')"
+	. ")";
+	return $p;
+}
+
+function table_matiere($mode = '', $url = '', $titre ='') {
+	static $tableau = array();
+	if($mode == 'retour') return $tableau;
+	$tableau[$url] = $titre;
+	return '';
+}
+
+function compose_table_matiere($cadre_lien,	$cadre_global, $table_matiere) {
+	$texte = '';
+	if(!empty($table_matiere))
+		foreach($table_matiere as $url => $titre)
+			$texte .= preg_replace(array(',@url@,', ',@titre@,'), array($url, $titre), $cadre_lien);
+	return $texte ? preg_replace(',@texte@,', $texte, $cadre_global) : '';	
+}
+
+?>
---
+++ ancres_intertitres/version.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * ancres_intertitres
+ *
+ * introduit une ancre translittérée pour les intertitres
+ *
+ * Auteur : collectif
+ * © 2005 - Distribue sous licence GNU/GPL
+ *
+ */
+
+$nom = 'ancres_intertitres';
+$version = 0.1;
+
+// s'inserer dans le pipeline 'avant_propre' @ ecrire/inc_texte.php3
+$GLOBALS['spip_pipeline']['pre_propre'][] = 'ancres_intertitres';
+
+$GLOBALS['spip_matrice']['ancres_intertitres'] = dirname(__FILE__).'/ancres_intertitres.php';
+
+?>
