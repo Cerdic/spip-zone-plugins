966419ce5cb4307238ad84e8c030092c5792b422
fil@rezo.net <fil@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-01-07 22:50:02 +0000

    un plugin de démo pour le tuto à venir sur spip-contrib http://www.spip-contrib.net/ecrire/articles.php3?id_article=1211
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@1657 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ toutmulti/version.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * toutmulti
+ *
+ * introduit le raccourci <:texte:> pour introduire librement des
+ * blocs multi dans un flux de texte (via typo ou propre)
+ *
+ * Auteur : collectif
+ * © 2006 - Distribue sous licence BSD
+ *
+ */
+
+$nom = 'toutmulti';
+$version = 0.1;
+
+// s'inserer dans le pipeline 'avant_propre' @ ecrire/inc_texte.php3
+$GLOBALS['spip_pipeline']['pre_typo'][] = 'toutmulti';
+
+// la fonction est tres legere on la definit directement ici
+function toutmulti($texte) {
+	$regexp = "|<:([^>]*):>|";
+	if (preg_match_all($regexp, $texte, $matches, PREG_SET_ORDER))
+	foreach ($matches as $regs)
+		$texte = str_replace($regs[0],
+		_T('spip/ecrire/public:'.$regs[1]), $texte);
+	return $texte;
+}
+
+#$GLOBALS['spip_matrice']['ancres'] = dirname(__FILE__).'/ancres.php';
+
+?>
