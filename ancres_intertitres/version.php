2514ec0aa011757a653af04bdb82666f3f7a3c8a
james@rezo.net <james@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-01-14 18:20:30 +0000

    nouveau format de plugin
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@1703 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- ancres_intertitres/version.php
+++ ancres_intertitres/version.php
@@ -14,7 +14,7 @@ $nom = 'ancres_intertitres';
 $version = 0.1;
 
 // s'inserer dans le pipeline 'avant_propre' @ ecrire/inc_texte.php3
-$GLOBALS['spip_pipeline']['pre_propre'][] = 'ancres_intertitres';
+$GLOBALS['spip_pipeline']['pre_propre'] .= '|ancres_intertitres';
 
 $GLOBALS['spip_matrice']['ancres_intertitres'] = dirname(__FILE__).'/ancres_intertitres.php';
 
--- href_lang/version.php
+++ href_lang/version.php
@@ -15,7 +15,7 @@ $nom = 'href_lang';
 $version = 0.1;
 
 // s'inserer dans le pipeline 'avant_propre' @ ecrire/inc_texte.php3
-$GLOBALS['spip_pipeline']['post_propre'][] = 'href_lang';
+$GLOBALS['spip_pipeline']['post_propre'] .= '|href_lang';
 
 $GLOBALS['spip_matrice']['href_lang'] = dirname(__FILE__).'/href_lang.php';
 
