792f749f5eab7cbc55b76938b3f9f6ebd2f71162
fil@rezo.net <fil@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-01-14 18:16:15 +0000

    nouveau format de plugin
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@1701 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- rien_poo/version.php
+++ rien_poo/version.php
@@ -14,7 +14,7 @@ $nom = 'rien_poo';
 $version = 0.1;
 
 // s'inserer dans le pipeline 'avant_propre' @ ecrire/inc_texte.php3
-$GLOBALS['spip_pipeline']['post_propre'][] = 'Rien::leFiltre';
+$GLOBALS['spip_pipeline']['post_propre'] .= '|Rien::leFiltre';
 
 $GLOBALS['spip_matrice']['Rien::leFiltre'] =
 	dirname(__FILE__).'/Rien.php';
