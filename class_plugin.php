cd8444c22b925b52d26cb045eff6a67f0d84ffbc
christian_lefebvre@laposte.net <christian_lefebvre@laposte.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-01-16 22:55:58 +0000

    une classe par defaut dont le plugin herite => methode de surcharge de la barre de boutons
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@1714 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ class_plugin.php
@@ -0,0 +1,14 @@
+<?php
+
+// classe par defaut pour la definition des plugins
+
+class Plugin {
+	/* public static */
+	function ajouterBoutons(&$boutons_admin, $rubrique, $sousrubrique) { }
+
+	/* public static */
+	function ajouterOnglets(&$onglets, $rubrique, $sousrubrique) { }
+
+}
+
+?>
--- rien_poo/Rien.php
+++ rien_poo/Rien.php
@@ -3,7 +3,7 @@
 /**
  * definition du plugin "rien" version "classe statique"
  */
-class Rien {
+class Rien extends Plugin {
 	/* static public */ function leFiltre($quelquechose) {
 		// ne rien faire = retourner ce qu'on nous a envoye
 		return $quelquechose.'<!-- rien_poo -->';
