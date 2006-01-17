34fe02e38f558223db34ac1be4187b89d8c4d8f3
christian_lefebvre@laposte.net <christian_lefebvre@laposte.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-01-17 22:51:12 +0000

    modif par le plugin des barres d'admin : - ajout d'un bouton si redacteur - ajout dans les menus naviguer et config si admin - ajout d'un onglet dans la page 'config langues' necessite de patcher inc_presentation
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@1722 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- rien_poo/Rien.php
+++ rien_poo/Rien.php
@@ -4,9 +4,42 @@
  * definition du plugin "rien" version "classe statique"
  */
 class Rien extends Plugin {
-	/* static public */ function leFiltre($quelquechose) {
+	/* static public */
+	function leFiltre($quelquechose) {
 		// ne rien faire = retourner ce qu'on nous a envoye
 		return $quelquechose.'<!-- rien_poo -->';
+	}
+
+	/* public static */
+	function ajouterBoutons($boutons_admin) {
+	  // en vrai, il faudrait prevoir des icones differents ...
+		$entree= array(
+			'libelle' => 'rien !',
+			'icone' => '../'._DIR_PLUGINS.'rien/ecrire/img_pack/rien-24.png');
+
+		// si on est admin
+		if ($GLOBALS['connect_statut'] == "0minirezo") {
+		  // on voit le bouton dans la barre "naviguer"
+		  Rien::insertBefore($boutons_admin['naviguer']['sousmenu'],
+							 'breves', 'rien_poo', $entree);
+
+		  // et on accede a la config
+		  $boutons_admin['configuration']['sousmenu']['riens']= $entree;
+		} else {
+		  // sinon, on voit un icone de plus dans la barre du haut
+		  Rien::insertBefore($boutons_admin,
+					 'naviguer', 'rien_poo', $entree);
+		}
+
+		return $boutons_admin;
+	}
+
+	/* public static */
+	function ajouterOnglets($onglets, $rubrique) {
+		$onglets['riens']=array(
+			'libelle' => 'rien ...',
+			'icone' => '../'._DIR_PLUGINS.'rien/ecrire/img_pack/rien-24.png');
+		return $onglets;
 	}
 }
 ?>
--- rien_poo/version.php
+++ rien_poo/version.php
@@ -15,8 +15,12 @@ $version = 0.1;
 
 // s'inserer dans le pipeline 'avant_propre' @ ecrire/inc_texte.php3
 $GLOBALS['spip_pipeline']['post_propre'] .= '|Rien::leFiltre';
+$GLOBALS['spip_pipeline']['ajouter_boutons'] .= '|Rien::ajouterBoutons';
+$GLOBALS['spip_pipeline']['ajouter_onglets'] .= '|Rien::ajouterOnglets';
 
 $GLOBALS['spip_matrice']['Rien::leFiltre'] =
+$GLOBALS['spip_matrice']['Rien::ajouterBoutons'] =
+$GLOBALS['spip_matrice']['Rien::ajouterOnglets'] =
 	dirname(__FILE__).'/Rien.php';
 
 ?>
