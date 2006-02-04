b94eeecf6dffef8e9e24d07deb0f6ee66ffc4235
pierre.andrews@gmail.com <pierre.andrews@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-02-04 16:52:44 +0000

    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@1823 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ mots_partout/mots_partout.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * mots partout
+ *
+ * interface de gestion des mots clefs
+ *
+ * Auteur : Pierre Andrews (Mortimer)
+ * © 2006 - Distribue sous licence GPL
+ *
+ */
+
+class MotsPartout {
+	/* static public */
+
+	/* public static */
+	function ajouterBoutons($boutons_admin) {
+
+	  spip_log('mots_partout');
+
+	  // on voit les bouton dans la barre "accueil"
+	  $boutons_admin['naviguer']->sousmenu["mots_partout"]= new Bouton(
+																		 "../"._DIR_PLUGIN_MOTS_PARTOUT."/tag.png",  // icone
+																		 'NOTS PARTOUT') //titre
+																		 );
+	  return $boutons_admin;
+	}
+
+}
+
+?>
