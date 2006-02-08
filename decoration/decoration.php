262f338f086031296bf49d3c73da8387fe91b756
izo@aucuneid.net <izo@aucuneid.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-02-08 16:27:21 +0000

    Le filtre decoration permet aux redacteurs d'un site spip de d'appliquer les styles souligné, barré, au dessus, blink et fluo à  une phrase , un mot, un parapraphe.
    [souligne]Lorem ipsum dolor sit amet[/souligne]
    [barre]Lorem ipsum dolor sit amet[/barre]
    [blink]Lorem ipsum dolor sit amet[/blink]
    [fluo]Lorem ipsum dolor sit amet[/fluo]
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@1888 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ decoration/decoration.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+  * Le filtre decoration permet aux redacteurs d'un site spip de d'appliquer les styles souligné, barré, au dessus à  une phrase , un  mot, parapraphe. 
+ */
+
+class decoration{
+
+	function decoration($texte) {
+		$texte = preg_replace("/(\[souligne\])(.*?)(\[\/souligne\])/", "<span style=\"text-decoration: underline;\">\\2</span>", $texte);
+		$texte = preg_replace("/(\[barre\])(.*?)(\[\/barre\])/", "<span style=\"text-decoration: line-through;\">\\2</span>", $texte);
+		$texte = preg_replace("/(\[over\])(.*?)(\[\/over\])/", "<span style=\"text-decoration: overline;\">\\2</span>", $texte);
+		$texte = preg_replace("/(\[blink\])(.*?)(\[\/blink\])/", "<span style=\"text-decoration: blink;\">\\2</span>", $texte);
+		$texte = preg_replace("/(\[fluo\])(.*?)(\[\/fluo\])/", "<span style=\"background-color: #ffff00;\">\\2</span>", $texte);
+
+	return $texte;  
+}
+}
+
+?>
+
