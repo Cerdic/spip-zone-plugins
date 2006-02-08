dcba0baae71281141cc8cf65dfad07e4dc87168d
izo@aucuneid.net <izo@aucuneid.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-02-08 22:47:08 +0000

    correction de [] en <> ajout de smallcaps pour voir ...
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@1894 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- decoration/decoration.php
+++ decoration/decoration.php
@@ -1,20 +1,31 @@
 <?php
 
 /*
-  * Le filtre decoration permet aux redacteurs d'un site spip de d'appliquer les styles souligné, barré, au dessus à  une phrase , un  mot, parapraphe. 
+  * Le plugin decoration permet aux redacteurs d'un site spip de d'appliquer les styles souligné, barré, au dessus à  une phrase , un  mot, parapraphe. 
  */
 
 class decoration{
 
 	function pre_typo($texte) {
-		$texte = preg_replace("/(\[souligne\])(.*?)(\[\/souligne\])/", "<span style=\"text-decoration: underline;\">\\2</span>", $texte);
-		$texte = preg_replace("/(\[barre\])(.*?)(\[\/barre\])/", "<span style=\"text-decoration: line-through;\">\\2</span>", $texte);
-		$texte = preg_replace("/(\[over\])(.*?)(\[\/over\])/", "<span style=\"text-decoration: overline;\">\\2</span>", $texte);
-		$texte = preg_replace("/(\[blink\])(.*?)(\[\/blink\])/", "<span style=\"text-decoration: blink;\">\\2</span>", $texte);
-		$texte = preg_replace("/(\[fluo\])(.*?)(\[\/fluo\])/", "<span style=\"background-color: #ffff00;\">\\2</span>", $texte);
+		$texte = preg_replace("/(\<souligne\>)(.*?)(\<\/souligne\>)/", "<span style=\"text-decoration: underline;\">\\2</span>", $texte);
+		$texte = preg_replace("/(\<barre\>)(.*?)(\<\/barre\>)/", "<span style=\"text-decoration: line-through;\">\\2</span>", $texte);
+		$texte = preg_replace("/(\<over\>)(.*?)(\<\/over\>)/", "<span style=\"text-decoration: overline;\">\\2</span>", $texte);
+		$texte = preg_replace("/(\<clignote\>)(.*?)(\<\/clignote\>)/", "<span style=\"text-decoration: blink;\">\\2</span>", $texte);
+		$texte = preg_replace("/(\<fluo\>)(.*?)(\<\/fluo\>)/", "<span style=\"background-color: #ffff00;\">\\2</span>", $texte);
+
+
+// Raccourci typographique <sc></sc>
+
+$texte = str_replace("<sc>",
+			"<span style=\"font-variant: small-caps\">", $texte);
+		$texte = str_replace("</sc>", "</span>", $texte);
 
 	return $texte;  
 }
+
+
+
+
 }
 
 ?>
