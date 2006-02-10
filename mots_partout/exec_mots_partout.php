b84c251180533791e275be8032a5b0dd9d801eda
pierre.andrews@gmail.com <pierre.andrews@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-02-10 10:39:09 +0000

    un define qui manque
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@1918 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- mots_partout/exec_mots_partout.php
+++ mots_partout/exec_mots_partout.php
@@ -20,6 +20,8 @@
 //    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 
+define('_DIR_PLUGIN_MOTS_PARTOUT',(_DIR_PLUGINS . basename(dirname(__FILE__))));
+
 /***********************************************************************
  * function
  ***********************************************************************/
