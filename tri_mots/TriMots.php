647c7e1eba368cb881cc05b93b38619ec8d56c23
pierre.andrews@gmail.com <pierre.andrews@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-02-12 12:18:44 +0000

    avec un patch pour un essaie de pipeline dans exec_article
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@1949 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- tri_mots/TriMots.php
+++ tri_mots/TriMots.php
@@ -6,14 +6,20 @@
 class TriMots {
   
   function ajouter_boite_gauche($arguments) {
-	if($arguments['args'] == 'article') {
-	  return $arguments['data'] .= boite_tri_mots();
+	if($arguments['args'] == 'articles') {
+	  return $arguments['data'] .= TriMots::boite_tri_mots();
 	}
     return $arguments['data'];
   }
   
   function boite_tri_mots() {
-	return "Boite Gauche";
+	$to_ret = '<div>&nbsp;</div>';
+	$to_ret .= '<div class="bandeau_rubriques" style="z-index: 1;">';
+	$to_ret .= bandeau_titre_boite2('Tri Mots',"article-24.gif","white","black", false);
+	$to_ret .= '<div class="plan-articles">';
+	$to_ret .= 'TRI';
+	$to_ret .= '</div></div>';
+	return $to_ret;
   }
 }
 ?>
