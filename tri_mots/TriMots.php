a46054e0d84bdd41636d005976995fe01d84e782
pierre.andrews@gmail.com <pierre.andrews@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-02-12 16:06:49 +0000

    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@1953 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- tri_mots/TriMots.php
+++ tri_mots/TriMots.php
@@ -6,18 +6,18 @@
 class TriMots {
   
   function ajouter_boite_gauche($arguments) {
-	if($arguments['args'] == 'articles') {
-	  return $arguments['data'] .= TriMots::boite_tri_mots();
+	if($arguments['args']['exec'] == 'articles') {
+	  return $arguments['data'] .= TriMots::boite_tri_mots($arguments['args']['id_article']);
 	}
     return $arguments['data'];
   }
   
-  function boite_tri_mots() {
+  function boite_tri_mots($id_article) {
 	$to_ret = '<div>&nbsp;</div>';
 	$to_ret .= '<div class="bandeau_rubriques" style="z-index: 1;">';
 	$to_ret .= bandeau_titre_boite2('Tri Mots',"article-24.gif","white","black", false);
 	$to_ret .= '<div class="plan-articles">';
-	$to_ret .= 'TRI';
+	$to_ret .= 'TRI'.$id_article;
 	$to_ret .= '</div></div>';
 	return $to_ret;
   }
