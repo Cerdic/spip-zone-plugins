e603adbbb6cc083ab1395091d5d4de5a72795395
pierre.andrews@gmail.com <pierre.andrews@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-02-22 18:50:09 +0000

    une interface pour des regles de selection par mot clef. Bizard, je me tappe un fond interdite avec :(
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@2051 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- chercher_squelettes_mots/exec_config_chercher_squelettes_mots.php
+++ chercher_squelettes_mots/exec_config_chercher_squelettes_mots.php
@@ -14,17 +14,104 @@ function config_chercher_squelettes_mots() {
   }
 
   if ($connect_statut == '0minirezo' AND $connect_toutes_rubriques ) {
-	
+
 	/*Affichage*/
-
+	debut_gauche();
+	
 	debut_droite();
-	echo 'Salut!';
-  } 
+	
+	echo '<form action="'.generer_url_ecrire('config_chercher_squelettes_mots').'" method="post">';
 
-  $fonds = array('article' => array(6,'articles','id_article'));
+	$groupes_mots = '';
+	$select = array('id_groupe','titre');
+	$from = array('spip_groupes_mots');
+
+	$rez = spip_abstract_select($select,$from);
+	while($row = spip_abstract_fetch($rez)) {
+	  $groupes_mots[$row['id_groupe']] = $row['titre'];
+	}
+	spip_abstract_free($rez);
+
+	$id_tables = array('articles' => 'id_article',
+					   'rubriques' => 'id_rubrique',
+					   'breves' => 'id_breve',
+					   'sites' => 'id_site');
+	
+
+	$fonds = unserialize(lire_meta('SquelettesMots:fond_pour_groupe'));
+
+	$field_fonds = $_REQUEST['fond'];
+	$id_groupes = $_REQUEST['id_groupe'];
+	$types = $_REQUEST['type'];
+	$actif = $_REQUEST['actif'];
+	
+	/*On transforme les _POST en jolie tableau*/
+	if($field_fonds) {
+	  foreach($field_fonds as $index => $fond) {		
+		$index = intval($index);
+		if($actif[$index]) {
+		  $fond = addslashes($fond);
+		  $id_groupe = intval($id_groupes[$index]);
+		  $type = addslashes($types[$index]);
+		  $fonds[$fond] = array($id_groupe,$type,$id_tables[$type]);
+		}
+	  }
+	}
+	
+	$index = 0;
+	
+	foreach($fonds as $fond => $a) {
+	  list($id_groupe,$type,$id_table) = $a;
+	  $index++;
+	  echo '<fieldset>';
+	  echo "<legend>regle $index</legend>";
+	  echo "<input type=\"checkbox\" name=\"actif[$index]\" checked=\"true\"/>";
+	  echo "<label for=\"fond_$index\">Fond:</label>";
+	  echo "<input type=\"text\" name=\"fond[$index]\" value=\"$fond\" id=\"fond_$index\"/>";
+	  echo "<label for=\"id_groupe_$index\">Groupe:</label>";
+	  echo "<select name=\"id_groupe[$index]\" id=\"id_groupe_$index\">";
+	  foreach($groupes_mots as $id => $titre) {
+		echo "<option value=\"$id\"".(($id_groupe == $id)?' selected="true"':'').">$titre</option>";
+	  }
+	  echo '</select>';
+	  echo "<label for=\"type_$index\">Type:</label>";
+	  echo "<select name=\"type[$index]\" id=\"type_$index\">";
+	  foreach($id_tables as $t => $x) {
+		echo "<option value=\"$t\"".(($type == $t)?' selected="true"':'').">$t</option>";
+	  }
+	  echo '</select>';
+	  echo '</fieldset>';
+	}
+	
+	$index++;
+	
+	echo '<hr/>';
+	echo '<fieldset>';
+	echo "<legend>Nouvelle regle</legend>";
+	echo "<input type=\"checkbox\" name=\"actif[$index]\"/>";
+	echo "<label for=\"fond_$index\">Fond:</label>";
+	echo "<input type=\"text\" name=\"fond[$index]\" value=\"article\"/>";
+	echo "<label for=\"id_groupe_$index\">Groupe:</label>";
+	echo "<select name=\"id_groupe[$index]\" id=\"id_groupe_$index\">";
+	foreach($groupes_mots as $id => $titre) {
+	  echo "<option value=\"$id\">$titre</option>";
+	}
+	echo '</select>';
+	echo "<label for=\"type_$index\">Type:</label>";
+	echo "<select name=\"type[$index]\" id=\"type_$index\">";
+	foreach($id_tables as $t => $x) {
+	  echo "<option value=\"$t\">$t</option>";
+	}
+	echo '</select>';
+	echo '</fieldset>';
+	
+	echo '<input type="submit" value="'._T('valider').'"/>';
+	echo '</form>';
+  } 
+  
   ecrire_meta('SquelettesMots:fond_pour_groupe',serialize($fonds));
   ecrire_metas();
-
+  
   fin_page();
   
 }
