1839f5e9b1e93976fac9b25bd64ac02947078296
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-02-24 22:34:58 +0000

    Suivre la SVN (http://trac.rezo.net/trac/spip/changeset/5757)
    Merci Fil de permettre cette surcharge ;-)
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@2089 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- barre_typo_enrichie/mes_options.php
+++ barre_typo_enrichie/mes_options.php
@@ -1,6 +1,9 @@
 <?php
 
-$barre_typo = 'enrichie';
+// Chemin de la barre typo enrichie
+$surcharge['inc_barre'] = _DIR_PLUGINS.'/barre_typo_enrichie/inc_barre_enrichie.php';
+
+// Definition des différents intertitres possibles
 $GLOBALS['debut_intertitre'] = "<h2>";
 $GLOBALS['fin_intertitre'] = "</h2>";
 $GLOBALS['debut_intertitre_2'] = '<h3>';
