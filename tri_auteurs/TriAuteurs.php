7aa526b869a6ce660bbd9da2e6d59db2734da5b3
pierre.andrews@gmail.com <pierre.andrews@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-02-26 19:56:38 +0000

    italiano, grazie Silvia
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@2139 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- tri_auteurs/TriAuteurs.php
+++ tri_auteurs/TriAuteurs.php
@@ -1,7 +1,23 @@
 <?php
 
-define('_DIR_PLUGIN_TRI_AUTEURS',(_DIR_PLUGINS . 'tri_auteurs'));
+//    Fichier créé pour SPIP avec un bout de code emprunté à celui ci.
+//    Distribué sans garantie sous licence GPL./
+//    Copyright (C) 2006  Pierre ANDREWS
+//
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program; if not, write to the Free Software
+//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
+define('_DIR_PLUGIN_TRI_AUTEURS',(_DIR_PLUGINS . 'tri_auteurs'));
 
 function TriAuteurs_ajouter_boite_gauche($arguments) {  
   if($arguments['args']['exec'] == 'articles') {
--- tri_auteurs/tri_auteurs_utils.php
+++ tri_auteurs/tri_auteurs_utils.php
@@ -1,4 +1,22 @@
 <?php
+//    Fichier créé pour SPIP avec un bout de code emprunté à celui ci.
+//    Distribué sans garantie sous licence GPL./
+//    Copyright (C) 2006  Pierre ANDREWS
+//
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program; if not, write to the Free Software
+//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
 function TriAuteurs_verifier_admin() {
   global $connect_statut, $connect_toutes_rubriques;
   return (($connect_statut == '0minirezo') AND $connect_toutes_rubriques);
