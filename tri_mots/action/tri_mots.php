b6b746f4003cfcbb64e0845c7ee19b5e46c28c4f
pierre.andrews@gmail.com <pierre.andrews@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-02-26 19:56:19 +0000

    italiano, grazie Silvia
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@2137 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- tri_mots/action/tri_mots.php
+++ tri_mots/action/tri_mots.php
@@ -1,5 +1,23 @@
 <?php
 
+//    Fichier créé pour SPIP avec un bout de code emprunté à celui ci.
+//    Distribué sans garantie sous licence GPL./
+//    Copyright (C) 2006  Pierre ANDREWS
+//
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program; if not, write to the Free Software
+//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
 function tri_mots() {
   global $redirect;
   global $hash, $id_auteur;
@@ -11,8 +29,6 @@ function tri_mots() {
   $id_mot = intval($id_mot);
   $id_table = addslashes($id_table);
   $table = addslashes($table);
-
-  var_dump("tri_mots $table $id_table $id_mot");
 
   include_ecrire("inc_session");
   if (!verifier_action_auteur("tri_mots $table $id_table $id_mot", $hash, $id_auteur)) {
