ea581377a6a598ec349efe0bddc2a38f7c626066
pierre.andrews@gmail.com <pierre.andrews@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-02-26 19:56:28 +0000

    italiano, grazie Silvia
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@2138 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- chercher_squelettes_mots/chercher_squelette.php
+++ chercher_squelettes_mots/chercher_squelette.php
@@ -1,5 +1,22 @@
 <?php
 
+//    Fichier créé pour SPIP avec un bout de code emprunté à celui ci.
+//    Distribué sans garantie sous licence GPL.
+//
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program; if not, write to the Free Software
+//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
 function chercher_squelette($fond, $id_rubrique, $lang) {
   global $contexte;
   $ext = $GLOBALS['extension_squelette'];
