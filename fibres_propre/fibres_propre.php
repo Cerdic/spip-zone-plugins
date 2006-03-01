93fa62147ca6a2aeec704e8c74861a9faca577c9
pierre.andrews@gmail.com <pierre.andrews@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-03-01 17:39:26 +0000

    comme d'hab la licence est plus longue que le code en soit.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@2185 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- fibres_propre/fibres_propre.php
+++ fibres_propre/fibres_propre.php
@@ -1,12 +1,31 @@
 <?php
 
+
+//    Fichier créé pour SPIP avec un bout de code emprunté à celui ci.
+//    Distribué sans garantie sous licence GPL./
+//    Copyright (C) 2006  Pierre ANDREWS
+//
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program; if not, write to the Free Software
+//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
 function fibres_propre_pre_propre($texte) {
   include_spip('inc/classTextile');
   if($texte) {
-   $textile = new Textile();
-   $texte = echappe_retour($texte); 
-   $texte = echappe_html('<html>'.$textile->TextileThis($texte).'</html>');
-   return $texte;
+	$textile = new Textile();
+	$texte = echappe_retour($texte); 
+	$texte = echappe_html('<html>'.$textile->TextileThis($texte).'</html>');
+	return $texte;
   }
 }
 
