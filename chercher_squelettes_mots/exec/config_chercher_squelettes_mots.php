32805ef7544c30aa0a9df1d62c98db67980e671c
pierre.andrews@gmail.com <pierre.andrews@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-03-02 21:24:17 +0000

    le path du exec qui collait pas
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@2203 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- chercher_squelettes_mots/exec/config_chercher_squelettes_mots.php
+++ chercher_squelettes_mots/exec/config_chercher_squelettes_mots.php
@@ -18,7 +18,7 @@
 //    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 
-define('_DIR_PLUGIN_CHERCHER_SQUELETTES',(_DIR_PLUGINS.end(explode(basename(_DIR_PLUGINS)."/",str_replace('\\','/',realpath(dirname(__FILE__)))))));
+define('_DIR_PLUGIN_CHERCHER_SQUELETTES',(_DIR_PLUGINS.end(explode(basename(_DIR_PLUGINS)."/",str_replace('\\','/',realpath(dirname(__FILE__).'/..'))))));
 
 function exec_config_chercher_squelettes_mots() {
   global $connect_statut, $connect_toutes_rubriques;
