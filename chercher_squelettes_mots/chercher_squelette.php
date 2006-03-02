bec4bc3ecd4d770b496a40f80416a6d73e40324d
pierre.andrews@gmail.com <pierre.andrews@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-03-02 22:44:03 +0000

    bon, je sais pas ce que j'ai commite avant, mais c'est celui la qui corrige le bug...
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@2206 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- chercher_squelettes_mots/chercher_squelette.php
+++ chercher_squelettes_mots/chercher_squelette.php
@@ -32,18 +32,21 @@ function chercher_squelette($fond, $id_rubrique, $lang) {
   
   // supprimer le ".html" pour pouvoir affiner par id_rubrique ou par langue
   $squelette = substr($base, 0, - strlen(".$ext"));
-  
+  $trouve = false;
+
   // On selectionne, dans l'ordre :
   // fond=10
   $f = "$fond=$id_rubrique";
-  if (($id_rubrique > 0) AND ($squel=find_in_path("$f.$ext")))
+  if (($id_rubrique > 0) AND ($squel=find_in_path("$f.$ext"))) {
 	$squelette = substr($squel, 0, - strlen(".$ext"));
-  else {
+	$trouve = true;
+  } else {
 	// fond-10 fond-<rubriques parentes>
 	while ($id_rubrique > 0) {
 	  $f = "$fond-$id_rubrique";
 	  if ($squel=find_in_path("$f.$ext")) {
 		$squelette = substr($squel, 0, - strlen(".$ext"));
+		$trouve = true;
 		break;
 	  }
 	  else {
@@ -52,18 +55,20 @@ function chercher_squelette($fond, $id_rubrique, $lang) {
 	}
   }
 
-  $fonds = unserialize(lire_meta('SquelettesMots:fond_pour_groupe'));
-  if (is_array($fonds) && (list($id_groupe,$table,$id_table) = $fonds[$fond])) {
-	$trouve = false;
-	if (($id = $contexte[$id_table]) && ($n = sql_mot_squelette($id,$id_groupe,$table,$id_table))) {
-	  if ($squel = find_in_path("$fond-$n.$ext")) {
-		$squelette = substr($squel, 0, - strlen(".$ext"));
-		$trouve = true;
-	  }
-	} 
-	if((!$trouve) && ($n = sql_mot_squelette($id_rubrique,$id_groupe,'rubriques','id_rubrique',true))) {	
-	  if ($squel = find_in_path("$fond-$n.$ext")) {
-		$squelette = substr($squel, 0, - strlen(".$ext"));
+  if(!$trouve) {
+	$fonds = unserialize(lire_meta('SquelettesMots:fond_pour_groupe'));
+	if (is_array($fonds) && (list($id_groupe,$table,$id_table) = $fonds[$fond])) {
+	  $trouve = false;
+	  if (($id = $contexte[$id_table]) && ($n = sql_mot_squelette($id,$id_groupe,$table,$id_table))) {
+		if ($squel = find_in_path("$fond-$n.$ext")) {
+		  $squelette = substr($squel, 0, - strlen(".$ext"));
+		  $trouve = true;
+		}
+	  } 
+	  if((!$trouve) && ($n = sql_mot_squelette($id_rubrique,$id_groupe,'rubriques','id_rubrique',true))) {	
+		if ($squel = find_in_path("$fond-$n.$ext")) {
+		  $squelette = substr($squel, 0, - strlen(".$ext"));
+		}
 	  }
 	}
   }
