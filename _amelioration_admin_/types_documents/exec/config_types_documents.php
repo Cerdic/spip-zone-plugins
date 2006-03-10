03da1a9035b9304472cb084c90af8a1aaa4c38f3
pierre.andrews@gmail.com <pierre.andrews@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-03-10 16:02:11 +0000

    debut de paneau de configuration pour la gestion de document, il faut encore ajouter des actions
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@2298 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _amelioration_admin_/types_documents/exec/config_types_documents.php
@@ -0,0 +1,114 @@
+<?php
+
+//    Fichier créé pour SPIP avec un bout de code emprunté à celui ci.
+//    Distribué sans garantie sous licence GPL./
+//    Copyright (C) 2006  Pierre ANDREWS
+//
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program; if not, write to the Free Software
+//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+define('_DIR_PLUGIN_TYPES_DOCUMENTS',(_DIR_PLUGINS.end(explode(basename(_DIR_PLUGINS)."/",str_replace('\\','/',realpath(dirname(__FILE__).'/..'))))));
+
+function exec_config_types_documents() {
+  global $connect_statut, $connect_toutes_rubriques;
+
+  include_spip("inc/presentation");
+  include_spip ("base/abstract_sql");
+
+  debut_page('&laquo; '._T('typesdocuments:titre_page').' &raquo;', 'configurations', 'types_documents');
+
+  if ($connect_statut != '0minirezo' OR !$connect_toutes_rubriques) {
+	echo _T('avis_non_acces_page');
+	exit;
+  }
+
+  if ($connect_statut == '0minirezo' AND $connect_toutes_rubriques ) {
+
+	echo '<br><br><br>';
+
+	gros_titre(_T('typesdocuments:gros_titre'));
+
+	barre_onglets("configuration", "config_types_documents");
+
+	/*Affichage*/
+	debut_gauche();	
+	
+	debut_boite_info();
+	echo propre(_T('typesdocuments:help'));
+	fin_boite_info();
+
+	debut_droite();
+	
+	include_spip('inc/config');
+	avertissement_config();
+
+	echo '<form action="'.generer_url_action('types_documents').'" method="post">';
+
+	echo "<div class='liste'>";
+	bandeau_titre_boite2('Types', "article-24.gif");
+	
+	echo afficher_liste_debut_tableau();
+
+	$select = array('*');
+	$from = array('spip_types_documents');
+
+	$s_count = array('count(*)');
+	$f_count = array('spip_documents');
+	
+	$table = array();
+	$table[] = array('','<strong>Type</strong>','<strong>Extension</strong>','<strong>Description</strong>','<strong>Permis?</strong>','<strong>nbr doc</strong>');
+	$rez = spip_abstract_select($select,$from);
+	while($row = spip_abstract_fetch($rez)) {
+	  $vals = '';	
+          $id_type = intval($row['id_type']);
+	  $titre = $row['titre'];
+   	  $ext = $row['extension'];
+          $mime = $row['mime_type'];
+          $inclus = $row['inclus'];
+          $upload = $row['upload'];
+          $desc = $row['description'];
+include_spip('inc/documents');
+$vignette = vignette_par_defaut($ext,false);
+if($vignette) {
+  $vals[] = '<img src="'.$vignette.'" alt="'.$titre.'"/>';
+} else
+$vals[] = '';
+          $vals[] = $titre;    
+	  $vals[] = $ext;
+	  $vals[] = $desc;
+	  $vals[] = '<input type="checkbox" name="upload['.$id_type.']"'.(($upload=='oui')?' checked="true"':'').'/>';
+
+	  list($count) = spip_abstract_fetsel($s_count,$f_count,array("id_type=$id_type"));
+	  $vals[] = $count;
+
+	  $table[] = $vals;
+	}
+	spip_abstract_free($rez);
+
+	$largeurs = array(24,11,11,80,11,11);
+        $styles = array('','arial1','arial11','arial11','arial11','arial11');	
+
+	afficher_liste($largeurs, $table, $styles);
+	
+	echo afficher_liste_fin_tableau();
+
+//	echo '<input type="submit" value="'._T('valider').'"/>';
+	echo '</form>';
+  } 
+  
+  fin_page();
+  
+}
+
+?>
