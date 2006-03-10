4a1b033ad18fce5ccea4ab0a2ba3e77da5aa6059
francois@schreuer.org <francois@schreuer.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-03-10 21:54:38 +0000

    Deplacement du bouton dans le menu des statistiques et plus dans le menu de configuration
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@2308 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _amelioration_admin_/statistiques_publication/pipelines.php
+++ _amelioration_admin_/statistiques_publication/pipelines.php
@@ -2,13 +2,12 @@
 
 define('_DIR_PLUGIN_STATS_PUB',(_DIR_PLUGINS.end(explode(basename(_DIR_PLUGINS)."/",str_replace('\\','/',realpath(dirname(__FILE__)))))));
 
-/* public static */
 function stats_pub_ajouterBoutons($boutons_admin) {
 	// si on est admin
 	if ($GLOBALS['connect_statut'] == "0minirezo" && $GLOBALS["connect_toutes_rubriques"]
 	AND $GLOBALS["options"]=="avancees") {
 	  // on voit le bouton dans la barre "naviguer"
-		$boutons_admin['configuration']->sousmenu['stats_pub']= new Bouton(
+		$boutons_admin['statistiques_visites']->sousmenu['stats_pub']= new Bouton(
 		"../"._DIR_PLUGIN_STATS_PUB."/statistiques-24.gif",  // icone
 		_T('statspub:icone_menu_config')	// titre
 		);
@@ -16,10 +15,5 @@ function stats_pub_ajouterBoutons($boutons_admin) {
 	return $boutons_admin;
 }
 
-/* public static */
-function stats_pub_ajouterOnglets($flux) {
-	$rubrique = $flux['args'];
-	return $flux;
-}
 
 ?>
