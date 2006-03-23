072001131e7ebcd14b08153cb8bbeb412c9e4f89
cedric.morin@yterium.com <cedric.morin@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-03-23 18:20:48 +0000

    Un coup de chiffon et un peu de rangement font pas de mal ...
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@2470 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- Agenda/agenda.php
+++ Agenda/agenda.php
@@ -17,11 +17,11 @@ function Agenda_header_prive($flux) {
 	$exec = _request('exec');
 	// les CSS
 	if ($exec == 'calendrier'){
-		$flux .= '<link rel="stylesheet" href="' ._DIR_PLUGIN_AGENDA_EVENEMENTS . '/calendrier.css" type="text/css" />'. "\n";
-		$flux .= '<link rel="stylesheet" href="' ._DIR_PLUGIN_AGENDA_EVENEMENTS . '/agenda.css" type="text/css" />'. "\n";
+		$flux .= '<link rel="stylesheet" href="' ._DIR_PLUGIN_AGENDA_EVENEMENTS . '/img_pack/calendrier.css" type="text/css" />'. "\n";
+		$flux .= '<link rel="stylesheet" href="' ._DIR_PLUGIN_AGENDA_EVENEMENTS . '/img_pack/agenda.css" type="text/css" />'. "\n";
 	}
 	if ($exec == 'articles'){
-		$flux .= '<link rel="stylesheet" href="' ._DIR_PLUGIN_AGENDA_EVENEMENTS . '/agenda_articles.css" type="text/css" />'. "\n";
+		$flux .= '<link rel="stylesheet" href="' ._DIR_PLUGIN_AGENDA_EVENEMENTS . '/img_pack/agenda_articles.css" type="text/css" />'. "\n";
 	}
 	// le JS
 	$flux .= "<script src = '"._DIR_PLUGIN_AGENDA_EVENEMENTS."/img_pack/YAHOO.js' ></script>\n";
@@ -223,38 +223,11 @@ function Agenda_rendu_evenement($flux) {
 
 	
 	$url = $evenement['URL']; 
-	/*$desc = propre($evenement['DESCRIPTION']);
-	$lieu = $evenement['LOCATION'];
-	$sum = typo($evenement['SUMMARY']);
-
-	$texte = "<span class='calendrier-verdana10'><span  style='font-weight: bold;'>";
-	$texte .= wordwrap($sum,15)."</span>";
-	$texte .= "<span class='survol'>";
-	$texte .= "<strong>$sum</strong><br />";
-	$texte .= $lieu ? "$lieu<br />":"";
-	$texte .= $desc;
-	$texte .= "</span>";
-	$texte .= "</span>";*/
 	$texte = Agenda_rendu_boite($evenement['SUMMARY'],$evenement['DESCRIPTION'],$evenement['LOCATION'],$flux['args']['type']);
 	$texte = http_href(quote_amp($url), $texte, '', '', '', '');
 	
 	$flux['data'] = $texte;
 	return $flux;
 }
-
-/* public static */
-/*function Agenda_ajouterBoutons($boutons_admin) {
-	// si on est admin
-	//if ($GLOBALS['connect_statut'] == "0minirezo" && $GLOBALS["connect_toutes_rubriques"]) {
-
-	  // on voit le bouton dans la barre "naviguer"
-		$boutons_admin['accueil']->sousmenu["agenda"]= new Bouton(
-		"../"._DIR_PLUGIN_PUBLIMAP."/habillage/publimap-24.png",  // icone
-		_L("Publimap") //titre
-		);
-	}
-	return $boutons_admin;
-}*/
-
 
 ?>
