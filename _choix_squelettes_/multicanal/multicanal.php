e2558c32dd1ac017daecbe65ac4163e31a455fce
christian_lefebvre@laposte.net <christian_lefebvre@laposte.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-03-29 09:12:12 +0000

    - interdire / dans le nom des squelettes - modif sur l'url pour pas polluer generer_url*
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@2553 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _choix_squelettes_/multicanal/multicanal.php
+++ _choix_squelettes_/multicanal/multicanal.php
@@ -9,26 +9,37 @@ $dossier_squelettes= 'squelettes/html';
 if(!empty($HTTP_GET_VARS["LOOK"])) {
 	// on fixe de force si la valeur est dans l'url
 	$look= $HTTP_GET_VARS["LOOK"];
-	setcookie("LOOK", $HTTP_GET_VARS["LOOK"]);
+	// slashs interdits => dans ce cas, on ne le met pas dans le cookie
+	if (strstr($look, '/')) {
+		$look='html';
+	} else {
+		setcookie("LOOK", $HTTP_GET_VARS["LOOK"]);
+	}
+	// on enleve cet argument de l'url, pour pas polluer les liens qui
+	// seront genere par la suite
+	str_replace("&LOOK=$look", '', $GLOBALS['REQUEST_URI']);
 } else {
 	if(!empty($HTTP_COOKIE_VARS["LOOK"])) {
 		// sinon, on la cherche dans un cookie
 		 $look= $HTTP_COOKIE_VARS["LOOK"];
+		 // slashs interdits, meme en tripotant les cookies
+		 if (strstr($look, '/')) {
+			 $look='html';
+		 }
 	} else {
 		 // sinon, selon le user-agent
 		// A REVOIR : gerer a part une liste de ug/look ?
 		if( stristr($HTTP_USER_AGENT, "nokia")
-		|| stristr($HTTP_USER_AGENT, "OPWV-SDK")) {
+			|| stristr($HTTP_USER_AGENT, "OPWV-SDK")) {
 			$look= "wap";
 		} elseif( stristr($HTTP_USER_AGENT, "portalmmm")) {
 			$look= "imode";
+		} elseif( stristr($HTTP_USER_AGENT, "V3XML")) {
+			$look= "vxml";
 		} else {
 			$look= "html";
 		}
 	}
-	// on truande la request_uri pour que nom_fichier_cache
-	// genere une version par canal => pas de conflit dans le cache
-	$GLOBALS['REQUEST_URI'].="&LOOK=$look";
 }
 
 $dossier_squelettes= "squelettes/$look";
