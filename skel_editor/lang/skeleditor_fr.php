582fc9a872392f1b66393f6d1d818161b0a5b1bd
karim@glums.com <karim@glums.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-04-07 18:32:08 +0000

    repertoire lang skel_editor/
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@2673 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ skel_editor/lang/skeleditor_fr.php
@@ -0,0 +1,19 @@
+<?php
+
+// Fichier de langue [fr] plugins "Skeleditor"
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+'repertoire' => 'r&eacute;pertoire',
+'fichier'=>"fichier",
+'fichier_image'=>"image",
+'inconnu'=>"inconnu",
+'erreur_ouverture_fichier'=>"erreur: impossible d\'ouvrir le fichier",
+'erreur_ecriture_fichier'=>"erreur: impossible d\'&ecute;crire dans le fichier",
+'fichier_sauvegarde_date'=>"fichier sauvegard&ecute; @ ",
+'erreur_edition_ecriture'=>"erreur: fichier non &ecute;ditable en &ecute;criture",
+'skeleditor_description'=>"Permet d'&eacute;diter les fichiers du squelette en cours",
+'skeleditor_dossier'=>"dossier squelette: "
+);
+
+
+?>
