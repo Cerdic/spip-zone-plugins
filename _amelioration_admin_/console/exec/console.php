e98feb5f0a089cf04d10288c5571e93d6d244404
cedric.morin@yterium.com <cedric.morin@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-04-20 16:49:08 +0000

    si j'ose ... j'ai prepare la gestion d'un onglet pour pouvoir avoir spip.log ou pysql.log
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@2757 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _amelioration_admin_/console/exec/console.php
+++ _amelioration_admin_/console/exec/console.php
@@ -1,56 +1,68 @@
 <?php
 
 function exec_console(){
-  global $connect_statut;
-  global $connect_toutes_rubriques;
+	global $connect_statut;
+	global $connect_toutes_rubriques;
   
-  include_ecrire("inc_presentation");
+	include_ecrire("inc_presentation");
 
-
-
-debut_page("Suivi des logs", "", "");
-
-echo "<br><br><br>";
-gros_titre("Suivi des logs");
-
-debut_gauche();
-
-debut_droite();
-
-echo "<FONT SIZE=2 FACE='Georgia,Garamond,Times,serif'>";
-
-if ($connect_statut != "0minirezo" OR !$connect_toutes_rubriques) {
-	echo "<B>Vous n'avez pas acc&egrave;s &agrave; cette page.</B>";
-	exit;
-}
-
-
-//
-// Lire et afficher les fichiers logs
-//
-
-$file  = Array();
-$file1 = Array();
-if (file_exists("data/spip.log.1"))
-	$file1 = file("data/spip.log.1");
-if (file_exists("data/spip.log"))
-	$file = file("data/spip.log");
-
-$s = sizeof($file);
-$s1 = sizeof($file1); 
-$n = min (40, $s+$s1);
-
-for ($i = $n; $i--; $i > 0) {
-	if ($i < $n-$s)
-		echo "<tt>".$file1[$s1 + $i - ($n-$s)]."</tt><br />\n";
-	else
-		echo "<tt>".$file[$i + $s - $n]."</tt><br />\n";
-}
-
-echo "</FONT>";
-
-fin_page();
-
+	debut_page("Suivi des logs", "", "");
+	
+	echo "<br><br><br>";
+	gros_titre("Suivi des logs");
+	//barre_onglets("console", "spip");
+	
+	debut_gauche();
+	
+	debut_droite();
+	
+	if ($connect_statut != "0minirezo" OR !$connect_toutes_rubriques) {
+		echo "<B>Vous n'avez pas acc&egrave;s &agrave; cette page.</B>";
+		exit;
 	}
+	
+	echo "<span style='font-size:medium;font:Georgia,Garamond,Times,serif;'>";
+	//
+	// Lire et afficher les fichiers logs
+	//
+	
+	$files = preg_files(_DIR_SESSIONS,"spip\.log(\.[0-9])?");
+	krsort($files);
+
+	$log = "";
+	foreach($files as $nom){
+		if (lire_fichier($nom,$contenu))
+			$log.=$contenu;
+	}
+	$contenu = explode("<br />",nl2br($contenu));
+	
+	$maxlines = 40;
+	while ($contenu && $maxlines--){
+		echo "<tt>".array_pop($contenu)."</tt><br />\n";
+	}
+
+	/*$file  = Array();
+	$file1 = Array();
+	if (file_exists("data/spip.log.1"))
+		$file1 = file("data/spip.log.1");
+	if (file_exists("data/spip.log"))
+		$file = file("data/spip.log");
+	
+	$s = sizeof($file);
+	$s1 = sizeof($file1); 
+	$n = min (40, $s+$s1);
+	
+	for ($i = $n; $i--; $i > 0) {
+		if ($i < $n-$s)
+			echo "<tt>".$file1[$s1 + $i - ($n-$s)]."</tt><br />\n";
+		else
+			echo "<tt>".$file[$i + $s - $n]."</tt><br />\n";
+	}*/
+	
+	echo "</span>";
+	
+	fin_page();
+
+}
 
 ?>
