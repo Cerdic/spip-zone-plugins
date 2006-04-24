0cafa28591016adb9aa0a1f27c2937a2fa635160
cedric.morin@yterium.com <cedric.morin@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-04-24 14:20:26 +0000

    rangement
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@2861 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _amelioration_admin_/revision_nbsp/revision_nbsp.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * revision_nbsp
+ *
+ * Dans l'espace prive, souligne en grise les espaces insecables
+ *
+ * Auteur : fil@rezo.net
+ * © 2005 - Distribue sous licence GNU/GPL
+ *
+ */
+
+
+	function RevisionNbsp_revision_nbsp($letexte) {
+		if (!_DIR_RESTREINT)
+			return str_replace('&nbsp;',
+				'<span class="spip-nbsp">&nbsp;</span>', $letexte);
+		else
+			return $letexte;
+	}
+
+
+?>
