8855df958c34ddfb2a59fd707d22136e3121130c
erational@erational.org <erational@erational.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-04-27 10:11:07 +0000

    trouve le chemin du plugin
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@2923 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _amelioration_admin_/sauver_config/inc_sauverconfig.php
+++ _amelioration_admin_/sauver_config/inc_sauverconfig.php
@@ -1,11 +1,13 @@
 <?php
 
+define('_DIR_PLUGIN_SAUVERCONFIG',(_DIR_PLUGINS.end(explode(basename(_DIR_PLUGINS)."/",str_replace('\\','/',realpath(dirname(__FILE__)))))));
+
 // ajout bouton ds interface admin
 function Sauverconfig_ajouterBoutons($boutons_admin) {
 		// si on est admin
 		if ($GLOBALS['connect_statut'] == "0minirezo") {		  
 		  $boutons_admin['configuration']->sousmenu['sauverconfig']= new Bouton(
-			'../'._DIR_PLUGINS.'sauver_config/img_pack/icon.png', // icone
+			'../'._DIR_PLUGIN_SAUVERCONFIG.'/img_pack/icon.png', // icone
        _L("Sauver la configuration") // titre
        );
 		} 
