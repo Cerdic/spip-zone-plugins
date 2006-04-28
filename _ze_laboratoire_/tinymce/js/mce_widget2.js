76daa516712aa16e9b1c77658df1deadf16ac0d2
christian_lefebvre@laposte.net <christian_lefebvre@laposte.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-04-28 21:22:14 +0000

    c'est mieux avec tous les morceaux
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@2977 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _ze_laboratoire_/tinymce/js/mce_widget2.js
@@ -0,0 +1,67 @@
+tinyMCE.init({
+  	mode  : 'none',
+	elements : "titre,surtitre,soustitre,descriptif,chapo,text_area,ps",
+
+	theme : "advanced",
+	plugins : "table,save,advimage,advlink,iespell,searchreplace,contextmenu",
+
+    width : "50%",
+
+	theme_advanced_buttons2_add_before: "save,cut,copy,paste,separator,search,replace,separator",
+	theme_advanced_buttons3_add_before : "bold,italic,separator",
+	theme_advanced_buttons3_add : "iespell,separator,tablecontrols",
+
+	theme_advanced_toolbar_location : "top",
+	theme_advanced_toolbar_align : "left",
+	theme_advanced_path_location : "bottom",
+
+	theme_advanced_disable : "bold,italic,underline,strikethrough,separator,justifyleft,justifycenter,justifyright,justifyfull,separator,formatselect,fontselect,fontsizeselect,styleselect,sub,sup,forecolor,backcolor,newdocument",
+
+	save_callback: "beforeSaveCallBack"
+});
+
+function beforeSaveCallBack(element_id, html, body) {
+	return "<!-- TINY_MCE -->"+html;
+}
+
+var currentTinyMCE = false;
+
+function toggleEdit(id) {
+	if(currentTinyMCE==false) {
+		setTinyMCE(id);
+		currentTinyMCE= id;
+	} else {
+		if(currentTinyMCE!=id) {
+			unsetTinyMCE(currentTinyMCE);
+			setTinyMCE(id);
+			currentTinyMCE= id;
+		} else {
+			unsetTinyMCE(id);			
+			currentTinyMCE= false;
+		}
+	}
+}
+
+function setTinyMCE(sEditorID) {
+	var oEditor = document.getElementById(sEditorID);
+	if(oEditor) {
+		tinyMCE.execCommand('mceAddControl', true, sEditorID);
+		currentTinyMCE = sEditorID;
+	}
+	return;
+}
+
+function unsetTinyMCE() {
+	var oEditor = document.getElementById(currentTinyMCE);
+	if(oEditor) {
+		tinyMCE.triggerSave();
+		tinyMCE.execCommand('mceRemoveControl', true, currentTinyMCE);
+		currentTinyMCE= false;
+	}
+	return;
+}
+
+function doAjaxSave() {
+	alert('save');
+	return false;
+}
