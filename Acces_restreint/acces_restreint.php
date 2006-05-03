87f2361139ab67603f9ec3a6aea374fa82a5ed9b
gilles.vincent@gmail.com <gilles.vincent@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-05-03 14:49:05 +0000

    L'admin doit pouvoir vérifier le bon fonctionnement des restrictions qu'il attribue. Cela est particulièrement vrai pour un admin débutant, un peu frileux, voire un poil parano :)
    Pour lui simplifier le travail, éviter de se déconnecter/reconnecter (ce qui est très lourd), on lui laisse le choix de s'affecter les droits d'accès 
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@3103 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- Acces_restreint/acces_restreint.php
+++ Acces_restreint/acces_restreint.php
@@ -5,7 +5,7 @@ include_spip('inc/acces_restreint');
 // <BOUCLE(ARTICLES)>
 //
 function boucle_ARTICLES($id_boucle, &$boucles) {
-	if ($connect_statut == '0minirezo') return boucle_ARTICLES_dist($id_boucle, $boucles);
+	//if ($connect_statut == '0minirezo') return boucle_ARTICLES_dist($id_boucle, $boucles);
 
 	$boucle = &$boucles[$id_boucle];
 	$id_table = $boucle->id_table;
@@ -29,7 +29,7 @@ function boucle_ARTICLES($id_boucle, &$boucles) {
 // <BOUCLE(BREVES)>
 //
 function boucle_BREVES($id_boucle, &$boucles) {
-	if ($connect_statut == '0minirezo') return boucle_BREVES_dist($id_boucle, $boucles);
+	//if ($connect_statut == '0minirezo') return boucle_BREVES_dist($id_boucle, $boucles);
 
 	$boucle = &$boucles[$id_boucle];
 	$id_table = $boucle->id_table;
@@ -53,7 +53,7 @@ function boucle_BREVES($id_boucle, &$boucles) {
 // <BOUCLE(FORUMS)>
 //
 function boucle_FORUMS($id_boucle, &$boucles) {
-	if ($connect_statut == '0minirezo') return boucle_FORUMS_dist($id_boucle, $boucles);
+	//if ($connect_statut == '0minirezo') return boucle_FORUMS_dist($id_boucle, $boucles);
 
 	$boucle = &$boucles[$id_boucle];
 	$id_table = $boucle->id_table;
@@ -76,7 +76,7 @@ function boucle_FORUMS($id_boucle, &$boucles) {
 // <BOUCLE(SIGNATURES)>
 //
 function boucle_SIGNATURES($id_boucle, &$boucles) {
-	if ($connect_statut == '0minirezo') return boucle_SIGNATURES_dist($id_boucle, $boucles);
+	//if ($connect_statut == '0minirezo') return boucle_SIGNATURES_dist($id_boucle, $boucles);
 
 	$boucle = &$boucles[$id_boucle];
 	$id_table = $boucle->id_table;
@@ -99,7 +99,7 @@ function boucle_SIGNATURES($id_boucle, &$boucles) {
 // <BOUCLE(DOCUMENTS)>
 //
 function boucle_DOCUMENTS($id_boucle, &$boucles) {
-	if ($connect_statut == '0minirezo') return boucle_DOCUMENTS_dist($id_boucle, $boucles);
+	//if ($connect_statut == '0minirezo') return boucle_DOCUMENTS_dist($id_boucle, $boucles);
 
 	$boucle = &$boucles[$id_boucle];
 	$id_table = $boucle->id_table;
@@ -122,7 +122,7 @@ function boucle_DOCUMENTS($id_boucle, &$boucles) {
 // <BOUCLE(RUBRIQUES)>
 //
 function boucle_RUBRIQUES($id_boucle, &$boucles) {
-	if ($connect_statut == '0minirezo') return boucle_RUBRIQUES_dist($id_boucle, $boucles);
+	//if ($connect_statut == '0minirezo') return boucle_RUBRIQUES_dist($id_boucle, $boucles);
 
 	$boucle = &$boucles[$id_boucle];
 	$id_table = $boucle->id_table;
@@ -145,7 +145,7 @@ function boucle_RUBRIQUES($id_boucle, &$boucles) {
 // <BOUCLE(HIERARCHIE)>
 //
 function boucle_HIERARCHIE($id_boucle, &$boucles) {
-	if ($connect_statut == '0minirezo') return boucle_HIERARCHIE_dist($id_boucle, $boucles);
+	//if ($connect_statut == '0minirezo') return boucle_HIERARCHIE_dist($id_boucle, $boucles);
 
 	$boucle = &$boucles[$id_boucle];
 	$id_table = $boucle->id_table;
@@ -169,7 +169,7 @@ function boucle_HIERARCHIE($id_boucle, &$boucles) {
 // <BOUCLE(SYNDICATION)>
 //
 function boucle_SYNDICATION($id_boucle, &$boucles) {
-	if ($connect_statut == '0minirezo') return boucle_SYNDICATION_dist($id_boucle, $boucles);
+	//if ($connect_statut == '0minirezo') return boucle_SYNDICATION_dist($id_boucle, $boucles);
 
 	$boucle = &$boucles[$id_boucle];
 	$id_table = $boucle->id_table;
@@ -192,7 +192,7 @@ function boucle_SYNDICATION($id_boucle, &$boucles) {
 // <BOUCLE(SYNDIC_ARTICLES)>
 //
 function boucle_SYNDIC_ARTICLES($id_boucle, &$boucles) {
-	if ($connect_statut == '0minirezo') return boucle_SYNDIC_ARTICLES_dist($id_boucle, $boucles);
+	//if ($connect_statut == '0minirezo') return boucle_SYNDIC_ARTICLES_dist($id_boucle, $boucles);
 
 	$boucle = &$boucles[$id_boucle];
 	$id_table = $boucle->id_table;
