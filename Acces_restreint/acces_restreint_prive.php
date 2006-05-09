01f46d0dda05f80ef00bef7c294d1ec331abcc7f
cedric.morin@yterium.com <cedric.morin@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-05-09 16:18:50 +0000

    debut de mise en place du filtrage cote privé
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@3226 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ Acces_restreint/acces_restreint_prive.php
@@ -0,0 +1,36 @@
+<?php
+
+function AccesRestreint_body_prive($flux) {
+	$exec = _request('exec');
+
+	// arreter tout si on a pas acces
+	if (in_array($exec,array('naviguer','rubriques_edit','articles','articles_edit','breves_voir','breves_edit')	)){
+		include_spip('inc/acces_restreint');
+		$liste_acces_interdit = AccesRestreint_liste_rubriques_exclues();
+		global $id_rubrique;
+		if ($exec == 'articles' || $exec=='articles_edit' ){
+			global $id_article;
+ 			$row = spip_fetch_array(spip_query("SELECT statut, titre, id_rubrique FROM spip_articles WHERE id_article=$id_article"));
+ 			if ($row) {
+				$id_rubrique = $row['id_rubrique'];
+			}
+		}
+		if ($exec == 'breves_voir' || $exec=='breves_edit' ){
+			global $id_breve;
+			$result = spip_query("SELECT * FROM spip_breves WHERE id_breve='$id_breve'");
+			if ($row = spip_fetch_array($result)) {
+				$id_rubrique=$row['id_rubrique'];
+			}
+		}
+		
+		if (!in_array($id_rubrique,$liste_acces_interdit))
+			return $flux; // c'est bon on contine
+			
+		// arreter tout si on a pas acces
+
+		redirige_par_entete(generer_url_ecrire(''));
+		exit();
+	}
+	return $flux; // c'est bon on contine
+}
+?>
