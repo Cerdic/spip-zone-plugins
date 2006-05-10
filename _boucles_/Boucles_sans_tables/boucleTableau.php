e70d621843901942e835a392a107fe072e67360f
christian_lefebvre@laposte.net <christian_lefebvre@laposte.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-05-10 13:42:08 +0000

    ajout d'une balise #TABLEAU pour afficher une donnée unique sans necessité de boucle + ajout d'une boucle et d'une balise AFFECTER pour créer des tableaux
    L'ensemble permet donc de faire des affectations dans un ordre quelconque
    puis d'iterer dessus après coup. On peut donc faire plusieurs boucles
    permettant de récupérer une série d'id par exemple, et faire plus loin une
    boucle pour les afficher d'un bloc.
    
    Ce mécanisme est très proche des <c:set> <c:out> et <jstl:foreach> de jstl
    
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@3238 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _boucles_/Boucles_sans_tables/boucleTableau.php
+++ _boucles_/Boucles_sans_tables/boucleTableau.php
@@ -18,9 +18,21 @@ $tableau = array(
 $tableau_key = array(
 	"PRIMARY KEY"	=> "cle"
 );
+
 $GLOBALS['tables_principales']['spip_tableau'] =
 	array('field' => &$tableau, 'key' => &$tableau_key);
 $GLOBALS['table_des_tables']['tableau'] = 'tableau';
+
+$GLOBALS['tables_principales']['spip_affecter'] =
+	array('field' => &$tableau, 'key' => array());
+$GLOBALS['table_des_tables']['affecter'] = 'affecter';
+
+// A REMPLACER PAR CA ?
+// $GLOBALS['tables_des_serveurs_sql']['']['tableau'] =
+// 	array('field' => &$tableau, 'key' => &$tableau_key);
+// $GLOBALS['tables_des_serveurs_sql']['']['affecter'] =
+// 	array('field' => $tableau, 'key' => $tableau_key);
+// MAIS POURQUOI CA MARCHE PAS ?
 
 function boucle_TABLEAU($id_boucle, &$boucles) {
 	$boucle = &$boucles[$id_boucle];
@@ -60,8 +72,9 @@ function boucle_TABLEAU($id_boucle, &$boucles) {
 	if(empty(\$__t)) { return ''; }
 	\$code=array();
 	\$Pile[\$SP]['var']=&\$__t;
+	\$i= 1;
 	foreach(\$__t as \$k => \$v) {
-		\$Numrows['$id_boucle']['compteur_boucle']=\$i;
+		\$Numrows['$id_boucle']['compteur_boucle']=\$i++;
 		\$Pile[\$SP]['cle']=\$k;
 		\$Pile[\$SP]['valeur']=\$v;
 		\$code[]=$boucle->return;
@@ -71,6 +84,95 @@ function boucle_TABLEAU($id_boucle, &$boucles) {
 CODE;
 
 	return $code;
+}
+
+function boucle_AFFECTER($id_boucle, &$boucles) {
+	$boucle = &$boucles[$id_boucle];
+
+	if (count($boucle->separateur))
+	  $code_sep= ("'" . ereg_replace("'","\'",join('',$boucle->separateur))
+				  . "'");
+	else
+	  $code_sep="''";
+
+	$var=null; $cle='';
+
+	foreach($boucle->criteres as $critere) {
+	  if($critere->op=='=' && $critere->param[0][0]->texte=='var') {
+		$var= '$GLOBALS['.calculer_liste($critere->param[1],
+			array(), $boucles, $boucle->id_parent).']';
+	  } elseif($critere->op=='=' && $critere->param[0][0]->texte=='cle') {
+		$cle.= '['.calculer_liste($critere->param[1],
+			array(), $boucles, $boucle->id_parent).']';
+	  }
+	}
+
+	if($var===null) {
+	  erreur_squelette("pas de variable sélectionnée",
+					   $boucle->id_boucle);
+	  return;
+	}
+
+	$code=<<<CODE
+	\$__t= &${var}$cle;
+	\$SP++;
+	\$__t[]=$boucle->return;
+	return '';
+CODE;
+
+	return $code;
+}
+
+function balise_TABLEAU($p) {
+	$var=null; $cle='';
+
+	if ($p->param && !$p->param[0][0]) {
+		$var=  calculer_liste($p->param[0][1],
+			 $p->descr, $p->boucles, $p->id_boucle);
+
+		// les cles
+		array_shift($p->param[0]);
+		array_shift($p->param[0]);
+		foreach($p->param[0] as $pp) {
+			$cle.= '['.calculer_liste($pp,
+				$p->descr, $p->boucles, $p->id_boucle).']';
+		}
+		array_shift($p->param);
+	} else {
+	  erreur_squelette("pas de variable sélectionnée dans balise TABLEAU",
+					   $boucle->id_boucle);
+	  return;
+	}
+	$p->code = "(\$GLOBALS[$var]$cle)";
+	$p->interdire_scripts = false;
+	return $p;
+}
+
+function balise_AFFECTER($p) {
+	$var=null; $cle='';
+
+	if ($p->param && !$p->param[0][0]) {
+		$var=  calculer_liste($p->param[0][1],
+			 $p->descr, $p->boucles, $p->id_boucle);
+
+		// les cles
+		array_shift($p->param[0]);
+		array_shift($p->param[0]);
+		foreach($p->param[0] as $pp) {
+			$cle.= '['.calculer_liste($pp,
+				$p->descr, $p->boucles, $p->id_boucle).']';
+		}
+		array_shift($p->param);
+	} else {
+	  erreur_squelette("pas de variable sélectionnée dans balise AFFECTER",
+					   $boucle->id_boucle);
+	  return;
+	}
+	$p->code = "((\$GLOBALS[$var]$cle="
+		.calculer_liste($p->avant, $p->descr, $p->boucles, $p->id_boucle).'.'
+		.calculer_liste($p->apres, $p->descr, $p->boucles, $p->id_boucle).")?'':'')";
+	$p->interdire_scripts = false;
+	return $p;
 }
 
 function champ($tableau, $champ) {
