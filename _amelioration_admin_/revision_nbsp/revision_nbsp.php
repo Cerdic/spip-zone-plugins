53653fd82b6b766c088d7d56cc973514f21c6701
fil@rezo.net <fil@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-05-10 13:13:12 +0000

    ce plugin a(vait) tendance à casser une image du genre <docXX> qui donne <img alt="une chaine avec &nbsp;!"> mais bizarrement je n'arrive pas échapper tous les tags sans causer d'autres soucis Z
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@3236 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _amelioration_admin_/revision_nbsp/revision_nbsp.php
+++ _amelioration_admin_/revision_nbsp/revision_nbsp.php
@@ -6,16 +6,17 @@
  * Dans l'espace prive, souligne en grise les espaces insecables
  *
  * Auteur : fil@rezo.net
- * © 2005 - Distribue sous licence GNU/GPL
+ * © 2005-2006 - Distribue sous licence GNU/GPL
  *
  */
 
 
 	function RevisionNbsp_revision_nbsp($letexte) {
-		if (!_DIR_RESTREINT)
+		if (!_DIR_RESTREINT) {
+			$letexte = echappe_html($letexte, '', true, ',(<img[^<]*>),Ums');
 			return str_replace('&nbsp;',
 				'<span class="spip-nbsp">&nbsp;</span>', $letexte);
-		else
+		} else
 			return $letexte;
 	}
 
