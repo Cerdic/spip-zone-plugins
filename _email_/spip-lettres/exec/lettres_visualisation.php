46420c9795104f35e97171fc9f10b13acc350468
pierre.basson@gmail.com <pierre.basson@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-05-17 08:47:16 +0000

    correction bug lors d'un test d'envoi alors que le statut de la lettre est brouillon
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@3325 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _email_/spip-lettres/exec/lettres_visualisation.php
+++ _email_/spip-lettres/exec/lettres_visualisation.php
@@ -209,7 +209,7 @@
 		debut_raccourcis();
 		lettres_afficher_raccourci_liste_lettres(_T('lettres:raccourci_retour_liste_lettres'));
 		lettres_afficher_raccourci_ajouter_abonne($id_lettre);
-		if (@spip_num_rows(spip_query('SELECT * FROM spip_auteurs_lettres WHERE id_lettre="'.$id_lettre.'"')) > 0)
+		if (@spip_num_rows(spip_query('SELECT * FROM spip_auteurs_lettres WHERE id_lettre="'.$id_lettre.'"')) > 0 AND $statut != 'brouillon')
 			lettres_afficher_raccourci_tester_envoi($id_lettre);
 		lettres_afficher_raccourci_import_csv($id_lettre);
 		lettres_afficher_raccourci_export_csv($id_lettre);
