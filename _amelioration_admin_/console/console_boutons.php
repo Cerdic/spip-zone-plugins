66fb3e17945d53b6d7a2002d0e7e6fe465821aed
cedric.morin@yterium.com <cedric.morin@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-05-23 15:48:15 +0000

    find_in_path pour trouver console.swf, c'est plus robuste
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@3368 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _amelioration_admin_/console/console_boutons.php
+++ _amelioration_admin_/console/console_boutons.php
@@ -36,11 +36,12 @@ function Console_body_prive($flux){
 		
 		$urlspiplog = urlencode(generer_url_ecrire('spiplog','logfile=spip',true));
 		$urlsqllog = urlencode(generer_url_ecrire('spiplog','logfile=mysql',true));
+		$flash = find_in_path('console.swf');
 		$flux.="
 		<object type='application/x-shockwave-flash' 
 		id='console'
-		data='"._DIR_PLUGIN_CONSOLE."/console.swf?spiplog=$urlspiplog&sqllog=$urlsqllog' width='300' height='600' style='position:absolute;left:0;bottom:0;'>
-			<param name='movie' value='"._DIR_PLUGIN_CONSOLE."/console.swf?spiplog=$urlspiplog&sqllog=$urlsqllog' />
+		data='$flash?spiplog=$urlspiplog&sqllog=$urlsqllog' width='300' height='600' style='position:absolute;left:0;bottom:0;'>
+			<param name='movie' value='$flash?spiplog=$urlspiplog&sqllog=$urlsqllog' />
 			<param name='wmode' value='transparent' />
 		</object>	";
 	}
