4568f24b660c1291b91c04522a59a0f83ca04351
arno@rezo.net <arno@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-06-02 19:50:13 +0000

    Ajout d'un lien "Telecharger" apres le code.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@3460 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _typo_/coloration_code/coloration_code.php
+++ _typo_/coloration_code/coloration_code.php
@@ -45,6 +45,7 @@ function coloration_code_color($code, $language='php') {
 }
 
 function coloration_code_echappe($texte) {
+	global $spip_lang_right;
   $rempl ='';
 
   if (preg_match_all(
@@ -52,7 +53,23 @@ function coloration_code_echappe($texte) {
 		 $texte, $matches, PREG_SET_ORDER))
 	foreach ($matches as $regs) {
 	  $code = echappe_retour($regs[2]);
+	  
+	  // Gerer le fichier contenant le code au format texte
+		$nom_fichier = md5($code);
+		$dossier = sous_repertoire(_DIR_IMG, 'cache-code');
+		$fichier = "$dossier$nom_fichier.txt";
+
+		if (!file_exists($fichier)) {
+			$handle = fopen($fichier, 'w');
+			fwrite($handle, $code);
+			fclose($handle);
+		}
+	  
+	  
 	  $rempl = coloration_code_color($code,$regs[1]);
+	  $rempl .= "<div class='".$regs[1]."_download' style='text-align: $spip_lang_right;'><a href='$fichier' style='font-family: verdana, arial, sans; font-weight: bold; font-style: normal;'>".
+	  		propre("<multi>Download[fr]T&eacute;l&eacute;charger</multi>").
+	  		"</a></div>";
 	  $texte = str_replace($regs[0],echappe_html("<html>$rempl</html>"),$texte);
 	}
   return $texte;
