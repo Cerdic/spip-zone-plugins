efa98b62dc49171751cc0ab14ac0da0c9a6df654
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-07-02 09:54:39 +0000

    formulaire recherche_etendue, et fonctionnement avec le critere {pagination} et la balise #PAGINATION.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@3779 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _agora_/mnogosearch/inc/mnogo_distant.php
+++ _agora_/mnogosearch/inc/mnogo_distant.php
@@ -1,16 +1,22 @@
 <?php
 
 function mnogo_querystring(){
-	$default_qs=array('q'=>'','m'=>'all','wm'=>'wrd','sp'=>1,'sy'=>1,'wf'=>'2221','type'=>'','ul'=>'','fmt'=>'xml','np'=>0,'ps'=>10,'GroupBySite'=>'no');
-	$key_translate = array('recherche'=>'q','site'=>'ul','debut'=>'np');
+	$default_qs=array('q'=>'','m'=>'bool','wm'=>'wrd','sp'=>1,'sy'=>1,'wf'=>'2221','type'=>'','ul'=>'','fmt'=>'xml','np'=>0,'ps'=>10,'GroupBySite'=>'no');
+	$key_translate = array('recherche'=>'q','site'=>'ul');
 
 	foreach($_REQUEST as $key=>$value){
+		if ($key=='debut_t'){
+			$default_qs['np'] = (int)round($value/$default_qs['ps']);
+		}
 		if (isset($key_translate[$key]))
 			$key = $key_translate[$key];
 		if (isset($default_qs[$key]))
 			$default_qs[$key] = $value;
 	}
 	$default_qs['fmt'] = 'xml'; // obligatoire
+	// remplacer les operateurs ET,AND,OR,OU par leur forme & |
+	$default_qs['q'] = preg_replace(',\s(ET|AND)\s,',urlencode(' & '),$default_qs['q']);
+	$default_qs['q'] = preg_replace(',\s(OU|OR)\s,',urlencode(' | '),$default_qs['q']);
 	$req = "";
 	foreach($default_qs as $key=>$value)
 		$default_qs[$key]=$key."=".$value;
@@ -24,6 +30,8 @@ function mnogo_getresults(){
 	$qs = mnogo_querystring();
 	$url .= (strpos($url,"?")!==FALSE)?"&$qs":"?$qs";
 
+	$offset = 0;
+
 	$arbre = array();
 	include_spip('inc/distant');
 	$contenu = recuperer_page($url);
@@ -35,13 +43,15 @@ function mnogo_getresults(){
 		foreach ($arbre['recherche'][0] as $balise=>$value){
 			if ($balise!='resultats')
 				$mnogo_resultats_synthese[preg_replace(',^balise_,i','',$balise)] = applatit_arbre($value);
-			/*if ($balise=='balise_MNOGO_TOTAL')
-				$mnogo_resultats = array_fill (0, $mnogo_resultats_synthese['MNOGO_TOTAL'], NULL );*/
+			if ($balise=='balise_MNOGO_TOTAL')
+				$mnogo_resultats = array_fill (0, $mnogo_resultats_synthese['MNOGO_TOTAL'], NULL );
+			if ($balise=='balise_MNOGO_PREMIER')
+				$offset = (int)trim(applatit_arbre($value))-1;
 		}
 		if (isset($arbre['recherche'][0]['resultats'][0]['resultat'])){
 			foreach ($arbre['recherche'][0]['resultats'][0]['resultat'] as $key=>$liste){
 				foreach ($liste as $balise=>$value) {
-					$mnogo_resultats[$key][preg_replace(',^balise_,i','',$balise)] = applatit_arbre($value);
+					$mnogo_resultats[$key+$offset][preg_replace(',^balise_,i','',$balise)] = applatit_arbre($value);
 				}
 			}
 		}
