b4f914a2c481d63b01f62f53d2f4032485a31f6d
booz@rezo.net <booz@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-07-03 16:11:11 +0000

    menage
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@3801 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _email_/spip_listes/inc/options_spip_listes.php
+++ _email_/spip_listes/inc/options_spip_listes.php
@@ -206,42 +206,6 @@ $out = ereg_replace ('<li[^>]>', "\n".'-', $out);
 }
 
 
-//Balises Spip-listes
-
-function calcul_MELEUSE_CRON() {
-  global $include_ok;
-   if(!$include_ok) {
-include_spip("inc/meleuse-cron");
-$include_ok = true;
-}
-   return '';
-}
-
-function balise_MELEUSE_CRON($p) {
-   $p->code = "calcul_MELEUSE_CRON()";
-   $p->statut = 'php';
-   return $p;
-}
-
-
-function calcul_DATE_MODIF_SITE() {
-   $date_art=spip_query("SELECT date,titre FROM spip_articles WHERE statut='publie' ORDER BY date DESC LIMIT 0,1");
-   $date_art=spip_fetch_array($date_art);
-   $date_art= $date_art['date'];
-   
-   $date_bre=spip_query("SELECT date_heure,titre FROM spip_breves WHERE statut='publie' ORDER BY date_heure DESC LIMIT 0,1");
-   $date_bre=spip_fetch_array($date_bre);
-   $date_bre= $date_bre['date_heure'];
-   
-   $date_modif= ($date_bre>$date_art)? $date_bre : $date_art ;   
-   return  $date_modif;
-}
-
-function balise_DATE_MODIF_SITE($p) {
-   $p->code = "calcul_DATE_MODIF_SITE()";
-   $p->statut = 'php';
-   return $p;
-}
 
 
 /******************************************************************************************/
--- _email_/spip_listes/spiplistes_mes_options.php
+++ _email_/spip_listes/spiplistes_mes_options.php
@@ -35,7 +35,44 @@ $champs_extra = true;
 				
 );
 
+//Balises Spip-listes
 
-include('inc/options_spip_listes.php3');
+function calcul_MELEUSE_CRON() {
+  global $include_ok;
+   if(!$include_ok) {
+include_spip("inc/meleuse-cron");
+$include_ok = true;
+}
+   return '';
+}
+
+function balise_MELEUSE_CRON($p) {
+   $p->code = "calcul_MELEUSE_CRON()";
+   $p->statut = 'php';
+   return $p;
+}
+
+
+function calcul_DATE_MODIF_SITE() {
+   $date_art=spip_query("SELECT date,titre FROM spip_articles WHERE statut='publie' ORDER BY date DESC LIMIT 0,1");
+   $date_art=spip_fetch_array($date_art);
+   $date_art= $date_art['date'];
+   
+   $date_bre=spip_query("SELECT date_heure,titre FROM spip_breves WHERE statut='publie' ORDER BY date_heure DESC LIMIT 0,1");
+   $date_bre=spip_fetch_array($date_bre);
+   $date_bre= $date_bre['date_heure'];
+   
+   $date_modif= ($date_bre>$date_art)? $date_bre : $date_art ;   
+   return  $date_modif;
+}
+
+function balise_DATE_MODIF_SITE($p) {
+   $p->code = "calcul_DATE_MODIF_SITE()";
+   $p->statut = 'php';
+   return $p;
+}
+
+
+include('inc/options_spip_listes.php');
 
 ?>
