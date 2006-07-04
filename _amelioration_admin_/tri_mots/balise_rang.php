4ab7f397356ed0dd31772340a750ff7d39118961
pierre.andrews@gmail.com <pierre.andrews@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-07-04 10:20:24 +0000

    balise RANG pour les auteurs aussi avec petit emberttement de double declaration. plus fallback pour les articles s'il y a pas de rang (hum).
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@3813 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _amelioration_admin_/tri_mots/balise_rang.php
+++ _amelioration_admin_/tri_mots/balise_rang.php
@@ -4,16 +4,23 @@ global $tables_auxiliaires;
 include_spip('base/auxiliaires');
 $tables_auxiliaires['spip_mots_articles']['field']['rang']='INT NOT NULL';
 
+if(!function_exists('balise_RANG')) {
 function balise_RANG($p) {
-	//get the calling boucle
-	$boucle = &$p->boucles[$p->id_boucle];
-	//consider any automatic join as an explicit join to permit selecting joint table fields
-	$boucle->jointures_explicites = $boucle->jointures;
-	//generate field code
-	$_rang = champ_sql('rang', $p);
-	$p->code = "$_rang";
-	$p->interdire_scripts = false;
-	return $p;
-}
+        //get the calling boucle
+        $boucle = &$p->boucles[$p->id_boucle];
+        //consider any automatic join as an explicit join to permit selecting joint table fields
+        $boucle->jointures_explicites = $boucle->jointures;
+        //generate field code
 
+        $_rang = champ_sql('rang', $p);
+        if($boucle->type_requete == 'article') {
+                $_titre = champ_sql('titre',$p);
+                $p->code = "(isset($_rang)?($_rang):recuperer_numero($_titre))";
+        } else {
+                $p->code = "$_rang";
+        }
+        $p->interdire_scripts = false;
+        return $p;
+}
+}
 ?>
