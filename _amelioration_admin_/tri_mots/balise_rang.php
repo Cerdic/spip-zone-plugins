f3e7cca5ded8132a5470ff86d7853eb04e10e8cc
renatoformato@virgilio.it <renatoformato@virgilio.it@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-07-04 09:05:52 +0000

    #RANG tag now working
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@3811 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _amelioration_admin_/tri_mots/balise_rang.php
+++ _amelioration_admin_/tri_mots/balise_rang.php
@@ -5,6 +5,11 @@ include_spip('base/auxiliaires');
 $tables_auxiliaires['spip_mots_articles']['field']['rang']='INT NOT NULL';
 
 function balise_RANG($p) {
+	//get the calling boucle
+	$boucle = &$p->boucles[$p->id_boucle];
+	//consider any automatic join as an explicit join to permit selecting joint table fields
+	$boucle->jointures_explicites = $boucle->jointures;
+	//generate field code
 	$_rang = champ_sql('rang', $p);
 	$p->code = "$_rang";
 	$p->interdire_scripts = false;
