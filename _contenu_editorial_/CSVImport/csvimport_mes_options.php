07ec03d5a865babf03e5cfc1053bb9d6dbd6ce80
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-07-05 11:08:07 +0000

    encore trop de globales dans le core : ici on affectait $table, manque de chance ca cassait mots_edit...
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@3817 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _contenu_editorial_/CSVImport/csvimport_mes_options.php
+++ _contenu_editorial_/CSVImport/csvimport_mes_options.php
@@ -3,10 +3,10 @@
 // pour qu'elles soient prises en compte dans le dump
 if (isset($GLOBALS['meta']['csvimport_tables_auth'])){
 	global $tables_auxiliaires;
-	$liste = array_keys(unserialize($GLOBALS['meta']['csvimport_tables_auth']));
-	foreach($liste as $table=>$infos){
-		if ($infos['dyn_declare_aux'])
-			$tables_auxiliaires[$table]=false; // on l'init pour savoir qu'elle existe, mais false pour que le compilo fasse un show_table si besoin
+	$csvimport_liste = array_keys(unserialize($GLOBALS['meta']['csvimport_tables_auth']));
+	foreach($csvimport_liste as $csvimport_table=>$csvimport_infos){
+		if ($csvimport_infos['dyn_declare_aux'])
+			$tables_auxiliaires[$csvimport_table]=false; // on l'init pour savoir qu'elle existe, mais false pour que le compilo fasse un show_table si besoin
 	}
 }
 ?>
