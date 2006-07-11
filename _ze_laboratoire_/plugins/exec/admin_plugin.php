edce83966f77011fce1b3ed44811921c17aaa8ed
baptiste.goupil@gmail.com <baptiste.goupil@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-07-11 22:27:18 +0000

    Un peu de style
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@3937 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _ze_laboratoire_/plugins/exec/admin_plugin.php
+++ _ze_laboratoire_/plugins/exec/admin_plugin.php
@@ -35,6 +35,52 @@ function exec_admin_plugin() {
 		$surligne = $_GET['surligne'];
 
 	debut_page(_T('icone_admin_plugin'), "administration", "plugin");
+	echo "<style type='text/css'>\n";
+	echo <<<EOF
+	div.cadre-padding ul li {
+	list-style:none ;
+	}
+	div.cadre-padding ul {
+	padding-left:1em;
+	margin:1em 0 0 0;
+	}
+	div.cadre-padding ul ul {
+	border-left:5px solid #DFDFDF;
+	}
+	div.cadre-padding ul li li {
+	border:1px solid #AFAFAF;
+	margin:0 0 .5em 0;
+	}
+	div.cadre-padding ul li li div.nomplugin {
+	padding:.3em;
+	}
+	div.cadre-padding div.droite label {
+	padding:.3em;
+	background:#EFEFEF;
+	border:1px dotted #95989F !important;
+	border:1px solid #95989F;
+	cursor:pointer;
+	margin:.2em;
+	display:block;
+	width:10.1em;
+	}
+	div.cadre-padding input {
+	cursor:pointer;
+	}
+	div.detailplugin {
+	border-top:1px solid #B5BECF;
+	padding:.6em;
+	margin-top:.5em;
+	background:#F5F5F5;
+	}
+	div.detailplugin hr {
+	border-top:1px solid #67707F;
+	border-bottom:0;
+	border-left:0;
+	border-right:0;
+	}
+	EOF;
+	echo "</style>";
 	echo "<br/><br/><br/>";
 	
 	gros_titre(_T('icone_admin_plugin'));
@@ -162,7 +208,7 @@ function ligne_plug($plug_file){
 	$info = plugin_get_infos($plug_file);
 	$plugok=@in_array($plug_file,$GLOBALS['plug_actifs']);
 
-	$s = "<div id='$plug_file'>";
+	$s = "<div id='$plug_file' class='nomplugin'>";
 	if (isset($info['erreur'])){
 		$s .=  "<div style='background:".$GLOBALS['couleur_claire']."'>";
 		$erreur = true;
