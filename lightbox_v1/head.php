09e50d70220a7f494fdb033cd1b53553e429ae06
fil@rezo.net <fil@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-07-15 15:48:49 +0000

    tout en squelettes c bien plus chouette
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@4018 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- lightbox_v1/head.php
+++ lightbox_v1/head.php
@@ -2,13 +2,9 @@
 
 	function Lightbox_v1_insert_head($flux){
 		$flux .= 	'<script type="text/javascript" src="'
-			.find_in_path('lightbox.js').'"></script>';
+			.generer_url_public('lightbox_js').'"></script>';
 		$flux .= 	"<link rel='stylesheet' href='"
-			.find_in_path('lightbox.css')."' type='text/css' media='all' />\n";
-		$flux .='<script type="text/javascript"><!--
-			var loadingImage = "'.url_absolue(find_in_path('loading.gif')).'";
-			var closeButton = "'.url_absolue(find_in_path('close.gif')).'"; // -->
-			</script>';
+			.generer_url_public('lightbox_css')."' type='text/css' media='all' />\n";
 		return $flux;
 	}
 
