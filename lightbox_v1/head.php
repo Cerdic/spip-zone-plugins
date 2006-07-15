ae1b542a57335c0af9053413f2ad5dea3b4acfc0
fil@rezo.net <fil@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-07-15 15:22:06 +0000

    problemes de chemin resolus
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@4013 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- lightbox_v1/head.php
+++ lightbox_v1/head.php
@@ -5,6 +5,10 @@
 			.find_in_path('lightbox.js').'"></script>';
 		$flux .= 	"<link rel='stylesheet' href='"
 			.find_in_path('lightbox.css')."' type='text/css' media='all' />\n";
+		$flux .='<script type="text/javascript"><!--
+			var loadingImage = "'.url_absolue(find_in_path('loading.gif')).'";
+			var closeButton = "'.url_absolue(find_in_path('close.gif')).'"; // -->
+			</script>';
 		return $flux;
 	}
 
