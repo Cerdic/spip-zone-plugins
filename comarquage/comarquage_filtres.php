917572df133f8e9d28a5b286fc52c771d58bd1ab
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-07-16 14:35:05 +0000

    Un plugin de comarquage avec la documentation francaise pour SPIP Reprend le principe d'utiliser la libxslt en ligne de commande (xsltproc) comme dans le developpement realise par BYS Promotion (http://sourceforge.net/projects/comarquage disponible sous licence PQL).
    Tout a été réécrit pour une intégration complète avec SPIP et la suppression de toute utilisation de javascript dans le rendu des page.
    Le plugin exploite 3 niveaux de cache :
    - un cache pour les fichiers xml issus du serveur de la documentation francaise. Les fichiers sont rappatriés a l'occasion de leur premiere utilisation, puis remis a jour en tache CRON, ce qui les rend en principe toujours immediatement disponible pour le calcul des pages
    - un cache pour les fichiers xml apres parsage et application de la feuille de style xsl associee. Ce cache permet de ne pas reparser le xml en cas de recalcul de la page par SPIP si le contenu est suffisament recent
    - le cache SPIP qui integre directement le resulat du parsing au format html. Les pages en cache ne necessitent donc aucun calcul.
    
    Toute la mise en forme du contenu est faite en PHP, sans intervention de javascript, et la sortie est directement au format HTML (les feuilles de style sont encodees en UTF-8)
    Le resultat permet d'obtenir une navigation rapide sans délai de connexion au serveur de la documentation francaise.
    
    Integration de libxslt en PHP des que j'ai trouve une machine de dev qui l'a :)
    Licence GPL pour le tout
    Cedric
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@4029 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ comarquage/comarquage_filtres.php
@@ -0,0 +1,86 @@
+<?php
+
+/*
+ * Copyright (C) 2006 Cedric Morin
+ * Licence GPL
+ *
+ * Plugin SPIP 1.9 (c) 2006 par Notre-ville.net
+ * Web : http://www.notre-ville.net
+ * Cedric MORIN (cedric.morin@notre-ville.net)
+ *
+ */
+
+include_spip('inc/comarquage');
+  
+function comarquage_run($parametres_defaut){
+	$url_base = preg_replace(',\?.*$,','',self()); 
+	$parametres =& comarquage_parametres($parametres_defaut,$url_base);
+	$page =& comarquage_compile_page_xml($parametres,$url_base);
+	
+	if (!is_string($page)) {
+		if ($page == -20)
+			$page = _T('comarquage:avis_serveur_indisponible');
+		else
+			$page = _T('comarquage:avis_erreur');
+	}
+	
+	return $page;
+}
+
+// recuperer les parametres specifiques au comarquage et les autres
+function & comarquage_parametres($defaut,&$urlbase) {
+	$parametres = array();
+	$parametres_attendus = array('xml' => ',^[a-z0-9_\-]*[.]xml$,i' , 'xsl' => ',^[a-z0-9_\-]*[.]xsl$,i', 'lettre' =>',^[a-z]$,i', 'motcle'=>'');
+	
+	foreach ($parametres_attendus as $k=>$reg) {
+		$p=_request($k);
+		if (($p==NULL)&&isset($defaut[$k]))
+			$p = $defaut[$k];
+		if (strlen($reg) AND !preg_match($reg,$p))
+			$p=NULL;
+		if ($p==NULL)
+			$p = isset($GLOBALS['meta']['comarquage_default_'.$k.'_file'])?$GLOBALS['meta']['comarquage_default_'.$k.'_file']:NULL;
+		if ($p!==NULL){
+			$parametres[$k] = $p;
+			$urlbase = parametre_url($urlbase,$k,'');
+		}
+	}
+	if (strpos($urlbase,'?')===FALSE)
+		$urlbase.='?';
+	else 
+		$urlbase.='&';
+
+	if (isset($parametres['xml'])){
+		$parametres['xml'] = basename($parametres['xml'],'.xml').'.xml';
+		$parametres['xml_full_path'] = sous_repertoire(_DIR_CACHE, _DIR_CACHE_COMARQUAGE_XML).$parametres['xml'];
+	}
+	if (isset($parametres['xsl']))
+		$parametres['xsl'] = basename($parametres['xsl'],'.xsl').'.xsl';
+		$parametres['xsl_full_path'] = _DIR_PLUGIN_COMARQUAGE.'xsl/'.$parametres['xsl'];
+	return $parametres;
+}
+
+
+function comarquage_post_propre($texte){
+	
+	$pattern="<[\s]*comarquage[\s]*([^>]*)>";
+	if ( 	(preg_match_all("{" . $pattern . "}is", $texte, $matches,PREG_SET_ORDER))
+			&& comarquage_processeur_disponible()) {
+		foreach($matches as $occurence){
+			$args = trim($occurence[1]);
+			$args = trim($args);
+			$args = explode(" ",$args);
+			$targs = array();
+			foreach($args as $arg){
+				if (preg_match(",([^=\s]*)\s*=\s*[\"']([^\"']*)[\"'],Uis",$arg,$extract))
+					$targs[$extract[1]] = $extract[2];
+			}
+		  $out = comarquage_run($targs);
+			$texte = str_replace($occurence[0],$out,$texte);
+		}
+	}
+	
+  return $texte;
+}
+
+?>
