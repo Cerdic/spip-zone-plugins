6438b6b199c31831d7d9ae8ac769da4c6414f0f6
iago@iago.ca <iago@iago.ca@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-07-18 19:09:21 +0000

    Quelques modifications pour ouvrir aussi les liens glossaires et directory dans une nouvelles fenêtre.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@4077 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _typo_/target/target.php
+++ _typo_/target/target.php
@@ -3,14 +3,17 @@
 /*
  * Target
  *
- * Ouvre tous les liens de class spip_out dans une nouvelle fenêtre
- *
+ *	Ouvre tous les liens de class spip_out dans une nouvelle fenêtre.
+ *	Ajoute la class spip_out aux liens de type directory des sites syndiqués et les ouvrent dans une nouvelle fenêtre.
+ *	Ouvre tous les liens de class spip_glossaire (vers wikipedia)  aussi dans une nouvelle fenêtre.
  * Auteur : francois.vachon@iago.ca
  * © 2006 - Distribue sous licence GNU/GPL
- *
+ * Dernière mise à jour: 18 juillet 2006
  */
 	function target_affichage_final($texte) {
 		$texte = str_replace('spip_out', 'spip_out" target="_blank"', $texte);
+		$texte = str_replace('rel="directory"', 'class="spip_out" target="_blank"', $texte);
+		$texte = str_replace('spip_glossaire', 'spip_glossaire" target="_blank"', $texte);
 		return $texte;
 	}
 	
