e53b510b7706724922b22cf5606be277053d2473
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-07-19 15:44:12 +0000

    le criter doit etre calcule dans le squelette, pas dans le filtre
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@4091 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _contenu_editorial_/PIM_agenda/public/criteres_agenda.php
+++ _contenu_editorial_/PIM_agenda/public/criteres_agenda.php
@@ -10,8 +10,8 @@ function critere_pim_agenda_actif_dist($idb, &$boucles, $crit){
 			$auteurs_agenda_actif = unserialize($GLOBALS['meta']['pim_agenda_auteurs_actifs']);
 		else
 			$auteurs_agenda_actif = array();
-		$c = calcul_mysql_in($type .'.id_auteur', implode(',',$auteurs_agenda_actif),$not);
-		$boucle->where[]= "'$c'";
+		$c = "calcul_mysql_in('$type.id_auteur', implode(',',isset(\$GLOBALS['meta']['pim_agenda_auteurs_actifs'])?unserialize(\$GLOBALS['meta']['pim_agenda_auteurs_actifs']):array()),'$not')";
+		$boucle->where[]= "$c";
   }
 }
 
