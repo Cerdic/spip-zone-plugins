02d4f734dd8c5cb57ae82192588f60649de17226
booz@rezo.net <booz@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-07-22 16:11:48 +0000

    on la refait moins cripse
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@4167 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- splickrbox/head.php
+++ splickrbox/head.php
@@ -1,9 +1,7 @@
 <?php
 
 	function Splickr_insert_head($flux){
-	//$flux .= 	'<script src="'.find_in_path('jquery.js').'" type="text/javascript"></script>';	
-
-		$flux .= 	'<script type="text/javascript" src="'
+	$flux .= 	'<script type="text/javascript" src="'
 			.generer_url_public('prototype_js').'"></script>';
 		$flux .= 	'<script type="text/javascript" src="'
 			.generer_url_public('rico_js').'"></script>';	
