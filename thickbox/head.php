0b25bebe377757c72123cc6f5a7a3316141836d4
fil@rezo.net <fil@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-07-22 22:59:00 +0000

    thickbox compatible avec sedna
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@4177 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- thickbox/head.php
+++ thickbox/head.php
@@ -4,12 +4,12 @@ function ThickBox_insert_head($flux){
 
 	$flux .='
 <style type="text/css" media="all">
-@import "'.find_in_path('thickbox.css').'";
+@import "'.url_absolue(find_in_path('thickbox.css')).'";
 </style>
 
 <script src="'.find_in_path('jquery.js').'" type="text/javascript"></script>
 <script type="text/javascript"><!--
-TB_chemin_animation = "'.find_in_path('circle_animation.gif').'";
+TB_chemin_animation = "'.url_absolue(find_in_path('circle_animation.gif')).'";
 // --></script>
 <script src="'.find_in_path('thickbox.js').'" type="text/javascript"></script>
 ';
