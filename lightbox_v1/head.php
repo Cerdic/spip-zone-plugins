0df3055823f3aa827cab59f4cd149776430efc21
fil@rezo.net <fil@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-07-24 09:31:18 +0000

    lightbox_v1 utilise jquery
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@4202 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- lightbox_v1/head.php
+++ lightbox_v1/head.php
@@ -1,6 +1,7 @@
 <?php
 
 	function Lightbox_v1_insert_head($flux){
+		$flux .= '<script src="'.find_in_path('jquery.js').'" type="text/javascript"></script>';
 		$flux .= 	'<script type="text/javascript" src="'
 			.generer_url_public('lightbox_js').'"></script>';
 		$flux .= 	"<link rel='stylesheet' href='"
