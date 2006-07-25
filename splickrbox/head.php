a4c6ad49cef65507a2fe9891fea3acb3ea1bfb00
booz@rezo.net <booz@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-07-25 15:50:31 +0000

    le badge spip passe en jquery, c plus in :)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@4215 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- splickrbox/head.php
+++ splickrbox/head.php
@@ -2,10 +2,7 @@
 
 	function Splickr_insert_head($flux){
 	$flux .= 	'<script type="text/javascript" src="'
-			.generer_url_public('prototype_js').'"></script>';
-		$flux .= 	'<script type="text/javascript" src="'
-			.generer_url_public('rico_js').'"></script>';	
-		return $flux;
+			.find_in_path('splickrbox.js').'"></script>';
 	}
 
 ?>
