5b226398ca185040b5053556ead35d1e7365f298
booz@rezo.net <booz@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-07-25 16:13:07 +0000

    reoups
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@4217 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- splickrbox/head.php
+++ splickrbox/head.php
@@ -1,8 +1,10 @@
 <?php
 
 	function Splickr_insert_head($flux){
-	$flux .= 	'<script type="text/javascript" src="'
-			.find_in_path('splickrbox.js').'"></script>';
+	$flux .= 	'<script type="text/javascript" src="'.find_in_path('splickrbox.js').'"></script>';
+
+	return $flux;
+
 	}
 
 ?>
