7fe09ae0e5a538e2535fe8430e9fa47ddd1262a6
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-07-26 19:14:30 +0000

    permettre de debugger le javascript en passant &jqdb=1 dans l'url
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@4226 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- @jQuery/head.php
+++ @jQuery/head.php
@@ -1,8 +1,10 @@
 <?php
 
 function JQuery_insert_head($flux){
-
-		$flux .= '<script src="'.find_in_path('jquery.js').'" type="text/javascript"></script>';
+		if (_request('jqdb')!==NULL)
+			$flux .= '<script src="'.find_in_path('jquery_uncompressed.js').'" type="text/javascript"></script>';
+		else
+			$flux .= '<script src="'.find_in_path('jquery.js').'" type="text/javascript"></script>';
 		
 		return $flux;
 	}
