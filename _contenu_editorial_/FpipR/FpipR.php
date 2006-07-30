40e6b9a28b43e71f35e1035468b02eeec633f20e
pierre.andrews@gmail.com <pierre.andrews@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-07-30 00:08:01 +0000

    un tout petit peu de style, il manque un logo
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@4271 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _contenu_editorial_/FpipR/FpipR.php
+++ _contenu_editorial_/FpipR/FpipR.php
@@ -14,20 +14,28 @@ function FpipR_affiche_milieu($flux) {
 	  ecrire_metas();
 	}
 	if($connect_id_auteur == $flux['args']['id_auteur']) {
+
+	  include('inc/presentation');
+
+	  $html = debut_cadre_relief('',true);
+	  $html .= '<h3>'._T('fpipr:autorisation_titre').'</h3>';
+
 	  $from = array('spip_auteurs');
 	  $select = array('flickr_token','flickr_nsid');
 	  $where = array('id_auteur='.$flux['args']['id_auteur']);
 	  $rez = spip_abstract_select($select,$from,$where);
 	  $row = spip_abstract_fetch($rez);
 	  if($row['flickr_nsid'] != '' && $row['flickr_token'] != '') {
-		$html = "Vous êtes identifié, bravo: ".$row['flickr_nsid'].':'.$row['flickr_token'];
+		$html .= 'Vous êtes identifié avec l\'utilisateur: <a href="http://www.flickr.com/photos/'.$row['flickr_nsid'].'">'.$row['flickr_nsid'].'</a>';
 	  } else {
 		include_spip('inc/flickr_api');
 		$infos = flickr_authenticate_get_frob();
-		$html = 'Veillez d\'abord <a target="blank" href="'.$infos['url'].'">autoriser</a> ce plugin sur flickr,
-<br/> une fois termin&eacute; vous pouvez revenir sur cette fenetre pour '.generer_action_auteur('flickr_authenticate_end',$infos['frob'], generer_url_ecrire('auteurs_edit','id_auteur='.$connect_id_auteur),'<button type="submit">'._T('terminer').'</button> l\'authentification');
+		$html .= '<ol><li>Veillez d\'abord autoriser ce plugin sur flickr en cliquant <strong><a target="blank" href="'.$infos['url'].'">ici</a></strong><br/>
+ Une nouvelle fen&ecirc;tre sera ouverte, suivez les instructions qui y sont fournis.</li>
+<li>Une fois termin&eacute; vous devez revenir sur cette fen&ecirc;tre pour '.generer_action_auteur('flickr_authenticate_end',$infos['frob'], generer_url_ecrire('auteurs_edit','id_auteur='.$connect_id_auteur),'<button type="submit">'._T('fpipr:terminer').'</button> l\'authentification.</li></ol>');
 	  }
 	  spip_abstract_free($rez);
+	  $html .= fin_cadre_relief(true);
 	  $flux['data'].=$html;
 	}
 	return $flux;
@@ -55,9 +63,25 @@ function FpipR_affiche_gauche($flux) {
 	  $row = spip_abstract_fetsel(array('statut','id_rubrique'),array('spip_breves'), array('id_breve='.$flux['args']['id_breve']));
 	  $acces = true; //si on est arrivé là c'est qu'on a le droit de faire les modifs
 	  }*/
-	if($acces)
-	  $flux['data'] .= '<a class="thickbox" href="'.generer_url_ecrire('flickr_choix_photos',"type=$type&id=$id").'">ajouter une photo Flickr</a>';
-	  $flux['data'] .= '<a class="thickbox" href="'.generer_url_ecrire('flickr_choix_sets',"type=$type&id=$id").'">ajouter un set de photos Flickr</a>';
+	if($acces) {
+	  $to_ret = '<div>&nbsp;</div>';
+	  $to_ret .= '<div class="bandeau_rubriques" style="z-index: 1;">';
+	  $to_ret .= "<div style='position: relative;'>";
+	  $to_ret .= "<div style='position: absolute; top: -12px; $spip_lang_left: 3px;'>
+	<img src=''/></div>";
+	  $to_ret .= "<div style='background-color: white; color: black; padding: 3px; padding-$spip_lang_left: 30px; border-bottom: 1px solid #444444;' class='verdana2'><b>"._T('Flickr')."</b></div>";
+	  $to_ret .= "</div>";
+
+	  $to_ret .= '<div class="plan-articles">';
+
+	  $to_ret .= '<a class="thickbox" href="'.generer_url_ecrire('flickr_choix_photos',"type=$type&id=$id").'">ajouter une photo Flickr</a>';
+	  $to_ret .= '<a class="thickbox" href="'.generer_url_ecrire('flickr_choix_sets',"type=$type&id=$id").'">ajouter un set de photos Flickr</a>';
+	  $to_ret .= '</div>';
+	  $to_ret .= '</div></div>';
+
+  $flux['data'] .= $to_ret;
+
+	}
   }
   return $flux;
 }
