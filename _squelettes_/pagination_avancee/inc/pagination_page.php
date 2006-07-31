441df6379c16d04a215b4f8187abfefdffda69f3
maieulrouquette2@tele2.fr <maieulrouquette2@tele2.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-07-31 14:54:26 +0000

    module de pagination avancé.
    etat experimental
    
    doc inclus
    
    une autre doc : http://zone.spip.org/trac/spip-zone/wiki/Pagination_avancee
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@4319 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _squelettes_/pagination_avancee/inc/pagination_page.php
@@ -0,0 +1,90 @@
+<?
+function pagination_page($total, $nom, $pas, $liste){
+static $ancres = array();
+	
+	$separateur="&nbsp;| ";
+	
+	
+
+	
+
+	
+
+	$debut = 'debut'.$nom;
+
+	$pagination = array(
+		'lien_base' => self(),
+		'total' => $total,
+		'position' => intval(_request($debut)),
+		'pas' => $pas,
+		'nombre_pages' => floor(($total-1)/$pas)+1,
+		'page_courante' => floor(intval(_request($debut))/$pas)+1,
+		'lien_pagination' => '<a href="@url@">@item@</a>',
+		'lien_item_courant' => '<span class="on">@item@</span>'
+	);
+
+	
+
+	// n'afficher l'ancre qu'une fois
+	if (!isset($ancres[$ancre]))
+		$bloc_ancre = $ancres[$ancre] = "<a name='$ancre' id='$ancre'></a>";
+
+	// Pas de pagination
+	if ($pagination['nombre_pages']<=1)
+		return '';
+
+	// liste = false : on ne veut que l'ancre
+	if (!$liste)
+		return $bloc_ancre;
+
+	// liste  = true : on retourne tout (ancre + bloc de navigation)
+
+	list ($premiere, $derniere) = calcul_bornes_pagination(
+		PAGINATION_MAX,
+		$pagination['nombre_pages'],
+		$pagination['page_courante']);
+
+	$texte = '';
+
+	if ($premiere > 2)
+		$texte .= pagination_item('',
+			'...',
+			$pagination[
+				($i != $pagination['page_courante']) ?
+				'lien_pagination' : 'lien_item_courant'
+			],
+			$pagination['lien_base'], $debut, $ancre)
+			. $separateur;
+
+	if ($premiere == 2) $premiere = 1; # '...' inutile quand on peut mettre 0
+
+	for ($i = $premiere; $i<=$derniere; $i++) {
+		$num = strval(($i-1)*$pas);
+		$texte .= pagination_item($num,
+			$i,
+			$pagination[
+				($i != $pagination['page_courante']) ?
+				'lien_pagination' : 'lien_item_courant'
+			],
+			$pagination['lien_base'], $debut, $ancre);
+		if ($i<$derniere) $texte .= $separateur;
+	}
+
+	if ($derniere < $pagination['nombre_pages'])
+		$texte .= $separateur.
+		pagination_item(strval(($pagination['nombre_pages']-1)*$pas),
+			'...',
+			$pagination[
+				($i != $pagination['page_courante']) ?
+				'lien_pagination' : 'lien_item_courant'
+			],
+			$pagination['lien_base'], $debut, $ancre);
+
+	return $bloc_ancre.$texte;
+
+}
+
+
+
+
+?>
