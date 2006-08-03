c2d28c041fd703734ead0d6a87661bf146ead298
fil@rezo.net <fil@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-08-03 20:58:33 +0000

    thickbox gere maintenant une galerie entiere et fonctionne dans l'espace prive (a condition d'avoir la derniere modif de spip)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@4388 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- thickbox/head.php
+++ thickbox/head.php
@@ -10,19 +10,10 @@ function ThickBox_insert_head($flux){
 <script type="text/javascript"><!--
 TB_chemin_animation = "'.url_absolue(find_in_path('circle_animation.gif')).'";
 // --></script>
-<script src="'.find_in_path('thickbox.js').'" type="text/javascript"></script>
+<script src="'.url_absolue(find_in_path('thickbox.js')).'" type="text/javascript"></script>
 ';
 
-//jquery.js passe en plugin separe
-//$flux .= '<script src="'.find_in_path('jquery.js').'" type="text/javascript"></script>';
-
-/*
-$flux .= 	'<script type="text/javascript" src="'
-			.generer_url_public('lightbox_js').'"></script>';
-		$flux .= 	"<link rel='stylesheet' href='"
-			.generer_url_public('lightbox_css')."' type='text/css' media='all' />\n";
-*/
-		return $flux;
-	}
+	return $flux;
+}
 
 ?>
--- thickbox/thickbox.js
+++ thickbox/thickbox.js
@@ -3,56 +3,74 @@
  * By Cody Lindley (http://www.codylindley.com)
  * Under an Attribution, Share Alike License
  * Thickbox is built on top of the very light weight jquery library.
+ *
+ * Modified for SPIP <www.spip.net> by Fil <fil@rezo.net>
+ *
  */
 
-//on page load call TB_init
-$(document).ready(TB_init);	
-
-
-//add thickbox to href elements that have a class of .thickbox
-TB_Image = function() {
+function TB_Image() {
 	var t = this.title || this.name || '<small>'+this.href+'</small>';
-	TB_show(t,this.href);
-	this.blur();
+	TB_on();
+	TB_show(t,this.href,'image');
 	return false;
-};
+}
 
 function TB_init(){
+	// add the thickbox to all links of class=thickbox
+	$("a.thickbox").click(TB_Image);
+
+	// and to all links of type image/jpeg (or gif|png)
+	// here we also deal with the prev/next thing
+	$("a[@type]")
+	.each(
+		function(i) {
+			if (
+				this.type.match(/^image[\/](jpeg|gif|png)$/i)
+			) {
+				this.onclick = TB_Image;
+
+				// on stocke le lien dans un tableau
+				imageArray.push ([
+					this.href,
+					this.title || this.name
+				]);
+
+			}
+		}
+	);
+
+	// and to all links to images (maybe too slow?)
+/*
 	$("a[@href]").each(
 		function(i) {
 			if (
-			this.type
-				? this.type.match(/image[\/](jpeg|gif|png)/i)
-				: (this.href
-					? this.href.match(/^[^?]+\.(jpeg|jpg|gif|png)$/i)
-					: false
-				) || this.className == 'thickbox'
+				this.href.match(/^[^?]+\.(jpeg|jpg|gif|png)$/i)
 			) {
 				this.onclick = TB_Image;
 			}
 		}
 	);
+*/
 }
 
+function TB_on() {
 
-
-function TB_show(caption, url) {//function called when the user clicks on a thickbox link
-	try {
-
-		$("body").append("<iframe id='TB_HideSelect'></iframe><div id='TB_overlay'></div><div id='TB_window'></div>");
-		$("#TB_overlay").click(TB_remove);
+		$("body").append("<div id='TB_overlay'></div><div id='TB_window'></div>");
+		$("#TB_overlay").click(TB_next);
 		$(window).scroll(TB_position);
- 		
-		$("#TB_HideSelect").css({display:"block"});
 		TB_overlaySize();
 		
-		$("body").append("<div id='TB_load'><img src='"+TB_chemin_animation+"' /></div>");
+		$("body").append("<div id='TB_load' style='display:none;'><img src='"+TB_chemin_animation+"' /></div>");
 		TB_load_position();
-		
+}
+
+function TB_show(caption, url, type) {//function called when the user clicks on a thickbox link
+	try {
+
 		var urlString = /\.jpg|\.jpeg|\.png|\.gif|\.html|\.htm|\.php|\.cfm|\.asp|\.aspx|\.jsp|\.jst|\.rb|\.txt/g;
 		var urlType = url.toLowerCase().match(urlString);
 		
-		if(urlType == '.jpg' || urlType == '.jpeg' || urlType == '.png' || urlType == '.gif'){//code to show images
+		if (type=='image' || urlType == '.jpg' || urlType == '.jpeg' || urlType == '.png' || urlType == '.gif'){//code to show images
 
 			imgPreloader = new Image();
 			imgPreloader.onload = function(){
@@ -89,7 +107,7 @@ function TB_show(caption, url) {//function called when the user clicks on a thic
 			$("#TB_closeWindowButton").click(TB_remove);		
 			TB_position();
 			$("#TB_load").remove();
-			$("#TB_ImageOff").click(TB_remove);
+			$("#TB_ImageOff").click(TB_next);
 			$("#TB_window").slideDown();
 			}
 	  
@@ -108,7 +126,7 @@ function TB_show(caption, url) {//function called when the user clicks on a thic
 			$("#TB_window").append("<div id='TB_closeAjaxWindow'><a href='#' id='TB_closeWindowButton'>close</a></div><div id='TB_ajaxContent' style='width:"+ajaxContentW+"px;height:"+ajaxContentH+"px;'></div>");
 			$("#TB_closeWindowButton").click(TB_remove);
 			
-				if(url.indexOf('TB_inline') != -1){	
+				if(url.indexOf('TB_inline') != -1){
 					$("#TB_ajaxContent").html($('#' + params['inlineId']).html());
 					TB_position();
 					$("#TB_load").remove();
@@ -133,8 +151,28 @@ function TB_show(caption, url) {//function called when the user clicks on a thic
 //helper functions below
 
 function TB_remove() {
-	$("#TB_window").fadeOut("fast",function(){$('#TB_window,#TB_overlay,#TB_HideSelect').remove();});
+	$("#TB_window").fadeOut("fast",function(){$('#TB_window,#TB_overlay').remove();});
 	$("#TB_load").remove();
+	return false;
+}
+
+function TB_next() {
+	var current = $("#TB_Image").get(0).src;
+	for (var i=0; i<imageArray.length; i++) {
+		if (imageArray[i][0] == current) {
+			var next = i+1;
+		}
+	}
+
+	if (next<imageArray.length) {
+		$("#TB_window").html('');
+		TB_show(imageArray[next][1] || imageArray[next][0],
+			imageArray[next][0], 'image');
+	}
+	else {
+		TB_remove();
+	}
+
 	return false;
 }
 
@@ -169,18 +207,18 @@ function TB_load_position() {
 }
 
 function parseQuery ( query ) {
-   var Params = new Object ();
-   if ( ! query ) return Params; // return empty object
-   var Pairs = query.split(/[;&]/);
-   for ( var i = 0; i < Pairs.length; i++ ) {
-      var KeyVal = Pairs[i].split('=');
-      if ( ! KeyVal || KeyVal.length != 2 ) continue;
-      var key = unescape( KeyVal[0] );
-      var val = unescape( KeyVal[1] );
-      val = val.replace(/\+/g, ' ');
-      Params[key] = val;
-   }
-   return Params;
+	var Params = new Object ();
+	if ( ! query ) return Params; // return empty object
+	var Pairs = query.split(/[;&]/);
+	for ( var i = 0; i < Pairs.length; i++ ) {
+		var KeyVal = Pairs[i].split('=');
+		if ( ! KeyVal || KeyVal.length != 2 ) continue;
+		var key = unescape( KeyVal[0] );
+		var val = unescape( KeyVal[1] );
+		val = val.replace(/\+/g, ' ');
+		Params[key] = val;
+	}
+	return Params;
 }
 
 
@@ -205,3 +243,11 @@ function getPageSize(){
 	arrayPageSize = new Array(w,h) 
 	return arrayPageSize;
 }
+
+
+/* init */
+// $(document).load(TB_init) si on veut que ca marche dans l'espace prive
+// (a cause des document_write() qu'il y a en pagaille...)
+// $(document).ready(TB_init);
+var imageArray = [];
+$(document).load(TB_init);
