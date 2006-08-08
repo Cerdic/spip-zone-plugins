926e48a05f764f5c05988aae8250fd00becd8bb8
renatoformato@virgilio.it <renatoformato@virgilio.it@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-08-08 07:41:33 +0000

    more jquery style coding. added support to URL fragment
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@4426 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _squelettes_/pagination_article2/pagination_article2.js
+++ _squelettes_/pagination_article2/pagination_article2.js
@@ -11,46 +11,55 @@ function() {
 var BLOC = 'texte';
 var HEADING = 'h3';
 
-if ($('.'+BLOC+' '+HEADING).size() > 1) {
-$('.'+BLOC).each(
-	function (j) {
-		var table='';
-		var block = -1;
-		var children = this.childNodes;
+if ($('.'+BLOC+'>'+HEADING).size() > 1) {
+	$('.'+BLOC).each(
+		function (j) {
+			
+			//prepend table to the texte block 
+			$(this).prepend(
+				'<div class="tdm" style="float: right; width: 120px;">'
+				+ '<ul></ul>'
+				+ '</div>'
+				);
+			//get the <ul> for later use		
+			var table=$('div.tdm>ul',this);
 
-		for (k=0; k<children.length; k++) {
-			if (block==-1 || children[k].tagName == HEADING.toUpperCase()) {
-				block ++;
-				classe = 'inter-'+j;
-				nom = classe+'-'+block;
-
-				children[k].onclick='$(\'.'+classe+'\').slideUp();'
-				+ '$(\'#'+nom+'\').filter(\':hidden\').slideDown();';
-
-				table = table
-					+ '<li><a onclick="$(\'.'+classe+'\').slideUp();'
-					+ '$(\'#'+nom+'\').filter(\':hidden\').slideDown();">'
-					+ children[k].innerHTML
-					+ '</a></li>';
-
-				$(this).before('<div class="' + BLOC + '">'
-					+'<div id="'+nom+'" class="'+classe+'">'
-					+'</div>'
-					+'</div>');
-				$("#"+nom).hide().before(children[k]);
-
-			} else {
-				$("#"+nom).append(children[k]);
+			var h = [];
+			//search for indexes of headers
+			childrens = $('>',this).each(
+				function(k) {
+					if(this.tagName == HEADING.toUpperCase()) h[h.length] = k; 
+				}
+			)
+			h[h.length] = childrens.size();
+			classe = 'inter-'+j;
+			//reverse iteration not to change live collection indexes of the elements before the current
+			for (k=h.length-1; k>0; k--) {
+				nom = classe+'-'+k;
+				//create onclick function
+				eval('var f = function() {'+
+					'$(\'.'+classe+':visible\').slideUp();$(\'#'+nom+'\').filter(\':hidden\').slideDown();' +
+				'}'); 
+				//Get current heading
+				var my_heading = $(':nth-child('+h[k-1]+')',this)
+				//bind click function evaluating on each iteration "classe" and "nom"
+				.click(f)
+				//add the div container and get the heading for later use
+				.after('<div id="'+nom+'" class="'+classe+'"></div>').get(0);
+				//append all elements between the current heading and the next one to the div and hide it
+				//that are all children of "this" with index lower then h[k]+1 
+				//(we've just added a div after the heading) and higher than h[k-1]+1
+				//NB: the filter selector must not have ">" since we have already all children in the
+				//current set of elements
+				$('#'+nom).append($('>*:lt('+(h[k]+1)+')',this).filter('*:gt('+(h[k-1]+1)+')').get()).hide();
+				//build table
+				table = table.prepend('<li><a></a></li>').find('li:first-child a').click(f).html(my_heading.innerHTML).end();
+				//manage link inside the page to the blocks
+				//$('a[@href$="#'+nom+'"]').click(f);
 			}
 		}
-		$(this).empty();
-		$('#inter-'+j+'-0').parent().prepend(
-			'<div class="tdm" style="float: right; width: 120px;">'
-			+ '<ul>'+table+'</ul>'
-			+ '</div>'
-			);
-		}
-);
+	);
 }
+if(window.location.hash) $(window.location.hash).show();
 }
 );
