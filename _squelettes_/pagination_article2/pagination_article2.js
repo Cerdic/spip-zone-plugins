f1090f26731730bbaaf16eb932444bb7a94ec495
fil@rezo.net <fil@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-08-08 13:27:11 +0000

    whitespace, comments, alternatives, variable names
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@4431 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _squelettes_/pagination_article2/pagination_article2.js
+++ _squelettes_/pagination_article2/pagination_article2.js
@@ -14,24 +14,23 @@ var HEADING = 'h3';
 if ($('.'+BLOC+'>'+HEADING).size() > 1) {
 	$('.'+BLOC).each(
 		function (j) {
-			
-			//prepend table to the texte block 
+			//prepend table to the texte block
 			$(this).prepend(
 				'<div class="tdm" style="float: right; width: 120px;">'
 				+ '<ul></ul>'
 				+ '</div>'
 				);
-			//get the <ul> for later use		
+			//get the <ul> for later use
 			var table=$('div.tdm>ul',this);
 
 			var h = [];
 			//search for indexes of headers
-			childrens = $('>',this).each(
+			children = $('>',this).each(
 				function(k) {
-					if(this.tagName == HEADING.toUpperCase()) h[h.length] = k; 
+					if(this.tagName == HEADING.toUpperCase()) h[h.length] = k;
 				}
 			)
-			h[h.length] = childrens.size();
+			h[h.length] = children.size();
 			classe = 'inter-'+j;
 			//reverse iteration not to change live collection indexes of the elements before the current
 			for (k=h.length-1; k>0; k--) {
@@ -47,14 +46,16 @@ if ($('.'+BLOC+'>'+HEADING).size() > 1) {
 				//add the div container and get the heading for later use
 				.after('<div id="'+nom+'" class="'+classe+'"></div>').get(0);
 				//append all elements between the current heading and the next one to the div and hide it
-				//that are all children of "this" with index lower then h[k]+1 
+				//that are all children of "this" with index lower than h[k]+1
 				//(we've just added a div after the heading) and higher than h[k-1]+1
 				//NB: the filter selector must not have ">" since we have already all children in the
 				//current set of elements
 				$('#'+nom).append($('>*:lt('+(h[k]+1)+')',this).filter('*:gt('+(h[k-1]+1)+')').get()).hide();
+//				$('#'+nom).prepend(my_heading);
 				//build table
 				table = table.prepend('<li><a></a></li>').find('li:first-child a').click(f).set('href','#'+nom).html(my_heading.innerHTML).end();
-				//manage links inside the page to the blocks. 
+//				table.prepend('<li><a></a></li>').find('li:first-child a').set('href','#'+nom).html(my_heading.innerHTML).click(f);
+				//manage links inside the page to the blocks.
 				//$('a[@href$="#'+nom+'"]').click(f);
 			}
 		}
