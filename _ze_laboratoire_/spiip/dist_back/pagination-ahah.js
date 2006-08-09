7552bac30fed7a5f1c8700a03e9fd7f325df26b7
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-08-09 23:02:45 +0000

    se placer sur l'ancre apres la mise a jour de la boucle
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@4468 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _ze_laboratoire_/spiip/dist_back/pagination-ahah.js
+++ _ze_laboratoire_/spiip/dist_back/pagination-ahah.js
@@ -1,16 +1,18 @@
 $.blocpagination = function(containerId) {
 	$('#'+containerId).each(function(){
 		var id = this.id;
-		var group = this;
-		$('a.lien_pagination',group).each(function(){
+		$('a.lien_pagination',this).each(function(){
+			var reg = new RegExp('^(.*)#([a-z0-9_]*)$','i');
 			var url = this.href;
-			url = url.replace(new RegExp('#[a-z0-9_]*$','i'),'');
-			if (url.indexOf("?")>0) url += '&';
-			else url += '?';
-			url += 'fragment='+id;
+			var ancre = url.replace(reg,'$2');
+			url = url.replace(reg,'$1');
+			if (url.indexOf("?")>0) url = url+'&';
+			else url = url+'?';
+			url=url + 'fragment='+id;
 			$(this).click(function(){
 				$(this.parentNode).before('<div class="ahah_searching_right">&nbsp;</div>');
 				$('div#'+id).load(url,null,function(){
+					window.location.hash = ancre;
 					$.blocpagination(id);
 				});
 				return false;
