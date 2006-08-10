cb6576d4b90547ad33e26d4f749496d1597f59c8
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-08-10 10:09:34 +0000

    ajouter un surlignage qui disparait progressivement sur les elements rafraichis
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@4483 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _ze_laboratoire_/spiip/dist_back/pagination-ahah.js
+++ _ze_laboratoire_/spiip/dist_back/pagination-ahah.js
@@ -1,3 +1,10 @@
+$.addFade = function() {
+	$("*",this).addClass('ahah_surligne_1');
+	setTimeout('$(".ahah_surligne_1").removeClass("ahah_surligne_1").addClass("ahah_surligne_2");',500);
+	setTimeout('$(".ahah_surligne_2").removeClass("ahah_surligne_2").addClass("ahah_surligne_3");',1000);
+	setTimeout('$(".ahah_surligne_3").removeClass("ahah_surligne_3");',1500);
+}
+
 $.blocpagination = function() {
 		var blocfrag = this;
 		$('a.lien_pagination',this).each(function(){
@@ -6,6 +13,7 @@ $.blocpagination = function() {
 			$(this).click(function(){
 				$(this.parentNode).before('<div class="ahah_searching_right">&nbsp;</div>');
 				$(blocfrag).load(url[0],null,function(){
+					$.apply(blocfrag,$.addFade);
 					window.location.hash = url[1];
 					$.apply(blocfrag,$.blocpagination);
 				});
