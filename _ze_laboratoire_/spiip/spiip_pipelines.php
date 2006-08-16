7a65ef8fb33aea8be40e713446d5fbf8812352c2
fil@rezo.net <fil@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-08-16 17:33:39 +0000

    integration du code de modeles dans le plugin spiip
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@4581 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _ze_laboratoire_/spiip/spiip_pipelines.php
+++ _ze_laboratoire_/spiip/spiip_pipelines.php
@@ -24,4 +24,52 @@ function spiip_insert_head($flux){
 	$flux .= "<script type='text/javascript' src='".find_in_path('dist_back/pagination-ahah.js')."'></script>\n";
 	return $flux;
 }
+
+
+/*
+ * Code repris du plugin modeles/
+ */
+
+	// Calcule le modele et retourne la mini-page ainsi calculee
+	function spiip_inclure_modele($squelette, $type, $id, $default) {
+	static $compteur;
+
+		if (++$compteur>4) return ''; # ne pas boucler indefiniment
+
+		$fond = 'modeles/'.$type;
+		if ($squelette)
+			$fond .= "_$squelette";
+
+		// en cas d'echec on passe la main au suivant
+		if (!find_in_path($fond.'.html'))
+			return $default;
+
+		include_spip('public/assembler');
+
+		if (in_array($type, array('img', 'doc', 'emb')))
+			$type = 'document';
+
+		$contexte = array('id_'.$type => $id);
+
+		$page = recuperer_fond($fond, $contexte);
+
+		$compteur--;
+
+		return $page;
+	}
+
+	/* static public */ 
+	function spiip_traiter_modeles($texte) {
+
+		$regexp = ',<([a-z_-]+)([0-9]+)([|]([a-z_0-9]+))?'.'>,';
+		if (preg_match_all($regexp, $texte, $matches, PREG_SET_ORDER))
+			foreach ($matches as $regs) {
+				$modele = spiip_inclure_modele($regs[4], $regs[1], $regs[2], $regs[0]);
+				$texte = str_replace($regs[0], code_echappement($modele), $texte);
+			}
+
+		return $texte;
+	}
+
+
 ?>
