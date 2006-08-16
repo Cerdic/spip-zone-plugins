312e18cb767ab64396adca3b252df8c1988f9be2
bill@adequates.com <bill@adequates.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-08-16 13:44:40 +0000

    ajout statut et idx => update non pris en charge pour le moment
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@4567 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _contenu_editorial_/spipcarto/exec/config_spipcarto.php
+++ _contenu_editorial_/spipcarto/exec/config_spipcarto.php
@@ -24,23 +24,24 @@ function exec_config_spipcarto() {
 	/************************************************************************/
 
 if (($_REQUEST['installation']=='oui')&&(($connect_statut == '0minirezo') AND $connect_toutes_rubriques)){
-	spip_query("CREATE TABLE `".$table_pref."_carto_cartes` (
+	spip_query("CREATE TABLE IF NOT EXISTS `".$table_pref."_carto_cartes` (
   `id_carto_carte` smallint(5) unsigned NOT NULL auto_increment,
   `url_carte` varchar(255) NOT NULL default '',
   `titre` varchar(255) NOT NULL default '',
   `texte` text NOT NULL,
   `callage` text NOT NULL,
   `id_srs` smallint(5) unsigned default NULL,
+  `statut` VARCHAR(8) NOT NULL default 'publie',
   `idx` enum('','1','non','oui','idx') NOT NULL default '',
   PRIMARY KEY  (`id_carto_carte`),
   KEY `titre` (`titre`)
 ) TYPE=MyISAM;");
-	spip_query("CREATE TABLE `".$table_pref."_carto_cartes_articles` (
+	spip_query("CREATE TABLE IF NOT EXISTS `".$table_pref."_carto_cartes_articles` (
   `id_carto_carte` smallint(5) unsigned NOT NULL default '0',
   `id_article` smallint(5) unsigned NOT NULL default '0',
   PRIMARY KEY  (`id_carto_carte`,`id_article`)
 ) TYPE=MyISAM;");
-	spip_query("CREATE TABLE `".$table_pref."_carto_objets` (
+	spip_query("CREATE TABLE IF NOT EXISTS `".$table_pref."_carto_objets` (
   `id_carto_objet` smallint(5) unsigned NOT NULL auto_increment,
   `id_carto_carte` smallint(5) unsigned NOT NULL default '0',
   `titre` varchar(255) NOT NULL default '',
@@ -48,51 +49,74 @@ if (($_REQUEST['installation']=='oui')&&(($connect_statut == '0minirezo') AND $c
   `url_objet` text,
   `url_logo` text,
   `geometrie` text,
+  `statut` VARCHAR(8) NOT NULL default 'publie',
   `idx` enum('','1','non','oui','idx') NOT NULL default '',
   PRIMARY KEY  (`id_carto_objet`),
   KEY `id_carte` (`id_carto_carte`),
-  KEY `titre` (`titre`)
+  KEY `titre` (`titre`),
+  KEY `statut` (`statut`)
 ) TYPE=MyISAM;");
-	spip_query("CREATE TABLE `".$table_pref."_carto_srs` (
+	spip_query("CREATE TABLE IF NOT EXISTS `".$table_pref."_carto_srs` (
   `id_carto_srs` smallint(5) unsigned NOT NULL auto_increment,
   `label` varchar(50) NOT NULL default '',
   `code` varchar(20) NOT NULL default '-1',
   PRIMARY KEY  (`id_carto_srs`)
 ) TYPE=MyISAM;");
-	spip_query("CREATE TABLE `".$table_pref."_documents_carto_cartes` (
+	spip_query("CREATE TABLE IF NOT EXISTS `".$table_pref."_documents_carto_cartes` (
   `id_carto_carte` smallint(5) unsigned NOT NULL default '0',
   `id_document` smallint(5) unsigned NOT NULL default '0',
   `callage` text,
   PRIMARY KEY  (`id_carto_carte`,`id_document`)
 ) TYPE=MyISAM;");
-	spip_query("CREATE TABLE `".$table_pref."_mots_carto_objets` (
+	spip_query("CREATE TABLE IF NOT EXISTS `".$table_pref."_mots_carto_objets` (
   `id_carto_objet` smallint(5) unsigned NOT NULL default '0',
   `id_mot` smallint(5) unsigned NOT NULL default '0',
   `ordre` tinyint(3) unsigned NOT NULL default '0',
   PRIMARY KEY  (`id_carto_objet`,`id_mot`),
   KEY `ordre` (`ordre`)
 ) TYPE=MyISAM;");
-	spip_query("ALTER TABLE `".$table_pref."_groupes_mots` ADD `carto_objets` CHAR( 3 ) NOT NULL AFTER `syndic` ;");
-	spip_query("ALTER TABLE `".$table_pref."_groupes_mots` ADD INDEX ( `carto_objets` ) ;");
-	spip_query("INSERT INTO `".$table_pref."_carto_srs` (id_carto_srs, label, code) VALUES ('-1', 'Par defaut', '-1');");
-	spip_query("INSERT INTO `".$table_pref."_carto_srs` (label, code) VALUES ('NTF (Paris) / Lambert zone II étendu', 'EPSG:27582');");
-	spip_query("INSERT INTO `".$table_pref."_carto_srs` (label, code) VALUES ('NTF (Paris) / Lambert zone II', 'EPSG:27572');");
-	spip_query("INSERT INTO `".$table_pref."_carto_srs` (label, code) VALUES ('NTF (Paris) / Lambert zone III', 'EPSG:27573');");
-	spip_query("INSERT INTO `".$table_pref."_carto_srs` (label, code) VALUES ('NTF (Paris) / Lambert zone I', 'EPSG:27571');");
-	spip_query("INSERT INTO `".$table_pref."_carto_srs` (label, code) VALUES ('NTF (Paris) / Lambert zone IV', 'EPSG:27574');");
-	spip_query("INSERT INTO `".$table_pref."_carto_srs` (label, code) VALUES ('WGS 84', 'epsg:4326');");
-	spip_query("INSERT INTO `".$table_pref."_carto_srs` (label, code) VALUES ('ED50', 'epsg:4230');");
-	spip_query("INSERT INTO `".$table_pref."_meta`(nom, valeur, maj) VALUES ('activer_carto','oui',now());");
-	spip_query("INSERT INTO `".$table_pref."_meta`(nom, valeur, maj) VALUES ('carto_mots','oui',now());");
-
-//	spip_query("INSERT INTO `".$table_pref."_meta`(nom, valeur, maj) VALUES ('config_precise_groupes','oui',now());");
-	spip_query("UPDATE `".$table_pref."_meta` SET valeur='oui', maj=now() WHERE nom='config_precise_groupes';");
-	ecrire_metas();
+	spip_query("ALTER TABLE ".$table_pref."_groupes_mots ADD carto_objets CHAR( 3 ) NOT NULL AFTER syndic ;");
+	spip_query("ALTER TABLE ".$table_pref."_groupes_mots ADD INDEX ( carto_objets ) ;");
+	
+	$r=spip_query("SELECT code FROM ".$table_pref."_carto_srs WHERE code='-1';");
+	if ($row=spip_fetch_array($r)){
+		spip_query("UPDATE ".$table_pref."_meta SET valeur='oui', maj=now() WHERE nom='activer_carto';");
+		spip_query("UPDATE ".$table_pref."_meta SET valeur='oui', maj=now() WHERE nom='carto_mots';");
+	} else {
+		spip_query("INSERT INTO ".$table_pref."_carto_srs (id_carto_srs, label, code) VALUES ('-1', 'Par defaut', '-1');");
+		spip_query("INSERT INTO ".$table_pref."_carto_srs (label, code) VALUES ('NTF (Paris) / Lambert zone II étendu', 'EPSG:27582');");
+		spip_query("INSERT INTO ".$table_pref."_carto_srs (label, code) VALUES ('NTF (Paris) / Lambert zone II', 'EPSG:27572');");
+		spip_query("INSERT INTO ".$table_pref."_carto_srs (label, code) VALUES ('NTF (Paris) / Lambert zone III', 'EPSG:27573');");
+		spip_query("INSERT INTO ".$table_pref."_carto_srs (label, code) VALUES ('NTF (Paris) / Lambert zone I', 'EPSG:27571');");
+		spip_query("INSERT INTO ".$table_pref."_carto_srs (label, code) VALUES ('NTF (Paris) / Lambert zone IV', 'EPSG:27574');");
+		spip_query("INSERT INTO ".$table_pref."_carto_srs (label, code) VALUES ('WGS 84', 'epsg:4326');");
+		spip_query("INSERT INTO ".$table_pref."_carto_srs (label, code) VALUES ('ED50', 'epsg:4230');");
+		spip_query("INSERT INTO ".$table_pref."_meta(nom, valeur, maj) VALUES ('activer_carto','oui',now());");
+		spip_query("INSERT INTO ".$table_pref."_meta(nom, valeur, maj) VALUES ('carto_mots','oui',now());");
+	}
+	$r=spip_query("SELECT valeur FROM ".$table_pref."_meta WHERE nom='MotsPartout:tables_installees';");
+	if ($row=spip_fetch_array($r)){
+		$tables=unserialize($row[0]);
+		$tables['carto_objets']=true;
+		spip_query("UPDATE ".$table_pref."_meta SET valeur='".serialize($tables)."', maj=now() WHERE nom='MotsPartout:tables_installees';");
+	}
+	else {
+		$tables=array('articles'=>true,'rubriques'=>true,'breves'=>true,'syndic'=>true,'documents'=>true,'carto_objets'=>true);
+		spip_query("INSERT INTO ".$table_pref."_meta(nom, valeur, maj) VALUES ('MotsPartout:tables_installees','".serialize($tables)."',now());");
+	}
+//	spip_query("INSERT INTO ".$table_pref."_meta(nom, valeur, maj) VALUES ('config_precise_groupes','oui',now());");
+	spip_query("UPDATE ".$table_pref."_meta SET valeur='oui', maj=now() WHERE nom='config_precise_groupes';");
 }
 elseif (($_REQUEST['installation']=='non')&&(($connect_statut == '0minirezo') AND $connect_toutes_rubriques)){
-	spip_query("UPDATE `".$table_pref."_meta` SET valeur='non', maj=now() WHERE nom='activer_carto';");
-	ecrire_metas();
+	spip_query("UPDATE ".$table_pref."_meta SET valeur='non', maj=now() WHERE nom='activer_carto';");
+	$r=spip_query("SELECT valeur FROM ".$table_pref."_meta WHERE nom='MotsPartout:tables_installees';");
+	if ($row=spip_fetch_array($r)){
+		$tables=unserialize($row[0]);
+		$tables['carto_objets']=false;
+		spip_query("UPDATE ".$table_pref."_meta SET valeur='".serialize($tables)."', maj=now() WHERE nom='MotsPartout:tables_installees';");
+	}
 }	
+	ecrire_metas();
 	/*Affichage*/
 
 	echo '<br><br><br>';
