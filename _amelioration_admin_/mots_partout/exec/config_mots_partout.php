20dfad35b6786200fceed6b63dcb6f1c43940d2d
bill@adequates.com <bill@adequates.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-08-16 13:38:02 +0000

    installation/désintallation des choses possibles
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@4560 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _amelioration_admin_/mots_partout/exec/config_mots_partout.php
+++ _amelioration_admin_/mots_partout/exec/config_mots_partout.php
@@ -8,6 +8,8 @@ function exec_config_mots_partout() {
   include_spip ("inc/presentation");
   include_spip ("base/abstract_sql");
 
+  global $choses_possibles;
+  include(_DIR_PLUGIN_MOTS_PARTOUT."/mots_partout_choses.php");
   debut_page('&laquo; '._T('motspartout:titre_page').' &raquo;', 'configurations', 'mots_partout');
 
   if ($connect_statut != '0minirezo' OR !$connect_toutes_rubriques) {
@@ -25,20 +27,24 @@ function exec_config_mots_partout() {
 	/************************************************************************/
 
 	$type = addslashes($_POST['nom_chose']);
-	
-	include(_DIR_PLUGIN_MOTS_PARTOUT."/mots_partout_choses.php");
 	$id_chose = $choses_possibles[$type]['id_chose'];
 	$table_principale = $choses_possibles[$type]['table_principale'];
 
 	$tables_installees = unserialize(lire_meta('MotsPartout:tables_installees'));
 
-	if(($type != '') && (!$tables_installees[$type])) {
-	  spip_query("ALTER TABLE `".$table_pref."_groupes_mots` ADD `".$type."` CHAR( 3 ) NOT NULL DEFAULT 'non';");
-	  spip_query("CREATE TABLE IF NOT EXISTS `".str_replace('spip_',$table_pref.'_mots_',$table_principale)."` (`id_mot` bigint(20) NOT NULL default '0',`$id_chose` bigint(20) NOT NULL default '0', KEY `$id_chose` (`id_mot`),KEY `id_mot` (`id_mot`)) TYPE=MyISAM;;");
-	  $tables_installees[$type] = true;
-	  ecrire_meta('MotsPartout:tables_installees',serialize($tables_installees));
-	  ecrire_metas();
-	}
+	if ($type != ''){
+		if (!$tables_installees[$type]) {
+		  spip_query("ALTER TABLE `".$table_pref."_groupes_mots` ADD `".$type."` CHAR( 3 ) NOT NULL DEFAULT 'non';");
+		  spip_query("CREATE TABLE IF NOT EXISTS `".str_replace('spip_',$table_pref.'_mots_',$table_principale)."` (`id_mot` bigint(20) NOT NULL default '0',`$id_chose` bigint(20) NOT NULL default '0', KEY `$id_chose` (`id_mot`),KEY `id_mot` (`id_mot`)) TYPE=MyISAM;;");
+		  $tables_installees[$type] = true;
+//		} elseif ($_POST['del']=='oui'){
+		} else {
+		  unset($tables_installees[$type]);
+		}
+		ecrire_meta('MotsPartout:tables_installees',serialize($tables_installees));
+	  	ecrire_metas();
+	  }
+
 	
 	/*Affichage*/
 
@@ -78,6 +84,17 @@ function exec_config_mots_partout() {
 
 	fin_cadre_enfonce();
 
+	debut_cadre_enfonce();
+	echo "<form action=\"".generer_url_ecrire('config_mots_partout')."\" method=\"post\">";
+	echo "<label for=\"nom_chose\">"._T("motspartout:desinstaller").":</label><br><br><select name=\"nom_chose\">";
+	foreach($tables_installees as $chose => $data) {
+	  echo "<option value=\"$chose\">$chose</option>";
+	}
+	echo "</select>";
+//	echo "<input type=\"hidden\" name=\"del\" value=\"oui\"/>";
+	echo "<input type=\"submit\" value=\""._T('valider')."\"/>";
+	echo '</form>';
+	fin_cadre_enfonce();
   } 
 
   fin_page();
