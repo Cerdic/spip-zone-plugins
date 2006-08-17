c390c28915fa858d0b4eed3b84bdd7ffa6d26230
renatoformato@virgilio.it <renatoformato@virgilio.it@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-08-17 22:48:16 +0000

    jquery svn 213
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@4652 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- -jQuery/head.php
+++ -jQuery/head.php
@@ -2,9 +2,9 @@
 
 function JQuery_insert_head($flux){
 		if (_request('jqdb')!==NULL)
-			$flux .= '<script src="'.find_in_path('jquery_uncompressed.js').'" type="text/javascript"></script>';
+			$flux .= '<script src="'.find_in_path('jquery.lite.213.js').'" type="text/javascript"></script>';
 		else
-			$flux .= '<script src="'.find_in_path('jquery.js').'" type="text/javascript"></script>';
+			$flux .= '<script src="'.find_in_path('jquery.pack.213.js').'" type="text/javascript"></script>';
 		
 		return $flux;
 	}
