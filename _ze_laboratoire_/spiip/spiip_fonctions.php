386ca86fa5191c33b929cd8a6a99372f5c5a33f8
maieulrouquette2@tele2.fr <maieulrouquette2@tele2.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-08-17 13:48:26 +0000

    un dexuieme filtre, pour transformer les parms en attribut html
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@4624 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _ze_laboratoire_/spiip/spiip_fonctions.php
+++ _ze_laboratoire_/spiip/spiip_fonctions.php
@@ -1,6 +1,6 @@
 <?
 function params_spip_to_params_html($texte){
-	//transforme les valeurs de parm ecrie de valeur=xx|valeur2=xxx en <param name='valeur1' value='xxx'>...
+	//transforme les valeurs de param écries de maniere "valeur=xx|valeur2=xxx" en <param name='valeur1' value='xxx'>...
 
 	$texte = str_replace("=","' value='",$texte);
 	$texte = str_replace("|","' />\n<param name='",$texte);
@@ -9,5 +9,14 @@ function params_spip_to_params_html($texte){
 	
 	return $texte;
 	}
+
+function params_spip_to_attributs_html($texte){
 	
+	$texte = str_replace("=","='",$texte);
+	$texte = str_replace("|","' ",$texte);
+	
+	
+	
+	return $texte."'";}
+
 ?>
