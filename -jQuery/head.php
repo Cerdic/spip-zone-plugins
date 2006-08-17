2f331e56be249ea996f97fe407f2fd57bec80322
renatoformato@virgilio.it <renatoformato@virgilio.it@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-08-17 22:58:40 +0000

    let jquery be the first script
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@4655 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- -jQuery/head.php
+++ -jQuery/head.php
@@ -2,11 +2,11 @@
 
 function JQuery_insert_head($flux){
 		if (_request('jqdb')!==NULL)
-			$flux .= '<script src="'.find_in_path('jquery.lite.213.js').'" type="text/javascript"></script>';
+			$js = '<script src="'.find_in_path('jquery.lite.213.js').'" type="text/javascript"></script>';
 		else
-			$flux .= '<script src="'.find_in_path('jquery.pack.213.js').'" type="text/javascript"></script>';
+			$js = '<script src="'.find_in_path('jquery.pack.213.js').'" type="text/javascript"></script>';
 		
-		return $flux;
+		return $js.$flux;
 	}
 
 ?>
