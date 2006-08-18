e47e862838edb9dec47bec9fd17e60d62ed51960
james@rezo.net <james@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-08-18 14:19:38 +0000

    un critère {archives} pour faire joli
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@4670 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- calendrier_mini/calendrier_mini_fonctions.php
+++ calendrier_mini/calendrier_mini_fonctions.php
@@ -7,4 +7,16 @@ function balise_DATE_ARCHIVES($p, $var_date = 'archives') {
 	return $p;
 }
 
+function critere_archives($idb, &$boucles, $crit, $var_date = 'archives') {
+	$boucle = &$boucles[$idb];
+ $champ_date = "'" . $boucle->id_table ."." .
+  $GLOBALS['table_date'][$boucle->type_requete] . "'";
+ $boucle->where[] = array(
+  'REGEXP',
+  $champ_date, 
+  "spip_abstract_quote(('^' . interdire_scripts(entites_html(\$Pile[0]['".$var_date."']))))"
+ );
+ 
+}
+
 ?>
