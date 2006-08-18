8cd1a29843db0ebedce7ec35ae617c392cd873b2
fil@rezo.net <fil@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-08-18 13:35:46 +0000

    les modeles de doc passent dans le core
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@4669 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _ze_laboratoire_/spiip/spiip_fonctions.php
+++ _ze_laboratoire_/spiip/spiip_fonctions.php
@@ -1,55 +1,5 @@
 <?php
-function params_spip_to_params_html($texte){
-	//a partir d'un tableau serialisé, retourne des <param name="cle" value="valeur" 
-	$ne_pas_garder=array( 	// tableau de ce qu'il n'est pas nécéssaire de garder
-	'id_document',
-	'date',
-	'date_redac',
-	'align',
-	//'largeur','hauteur',	//on la mettra plus tard, pour regler les problemes de largeur et hauteur trop petite avec un controleur video
-	'fond',''
-	) ;	
 
-	
-	
-	
-	$tableau = unserialize($texte);
-	
-	$texte = "";
-	foreach ($tableau as $i => $j){
-		if (!in_array($i,$ne_pas_garder)) 
-			{
-			$texte .= "<param name='".$i."' value='".$j."' />";
-			}
-		}
-	
-	return $texte;
-	}
-
-function params_spip_to_attributs_html($texte){
-	//a partir d'un tableau serialisé, retourne des attributs html
-	
-	$ne_pas_garder=array( 	// tableau de ce qu'il n'est pas nécéssaire de garder
-	'id_document',
-	'date',
-	'date_redac','align',
-	//'largeur','hauteur',	//on la mettra plus tard, pour regler les problemes de largeur et hauteur trop petite avec un controleur video
-	'fond',''
-	) ;	
-	
-	$tableau = unserialize($texte);
-	
-	$texte = "";
-	foreach ($tableau as $i => $j){
-		
-		if (!in_array($i,$ne_pas_garder)) 
-			{
-			$texte .= $i."='".$j."' "; 
-			}
-		}
-	
-	return $texte;
-	}
 
 	
 	
