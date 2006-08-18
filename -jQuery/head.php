ad3bd95cbdb6875e5eff2b364f66d1fe5ccb1bf7
renatoformato@virgilio.it <renatoformato@virgilio.it@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-08-18 15:30:05 +0000

    compressed version causes an error with safari..disabled for now
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@4673 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- -jQuery/head.php
+++ -jQuery/head.php
@@ -1,10 +1,10 @@
 <?php
 
 function JQuery_insert_head($flux){
-		if (_request('jqdb')!==NULL)
+		//if (_request('jqdb')!==NULL)
 			$js = '<script src="'.find_in_path('jquery.lite.213.js').'" type="text/javascript"></script>';
-		else
-			$js = '<script src="'.find_in_path('jquery.pack.213.js').'" type="text/javascript"></script>';
+		//else
+		//	$js = '<script src="'.find_in_path('jquery.pack.213.js').'" type="text/javascript"></script>';
 		
 		return $js.$flux;
 	}
