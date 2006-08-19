69e5a8dd1f9d8436332072c120d60826b64c75f9
renatoformato@virgilio.it <renatoformato@virgilio.it@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-08-19 00:05:40 +0000

    needs jquery plugin
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@4699 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _squelettes_/fading_rollover/fadingrollover_pipelines.php
+++ _squelettes_/fading_rollover/fadingrollover_pipelines.php
@@ -1,7 +1,6 @@
 <?php
 
 function fadingrollover_insert_head($flux){
-	$flux .= '<script src="'.find_in_path('jquery-1.0a.js').'" type="text/javascript"></script>';
 	$flux .= "<script type='text/javascript' src='".find_in_path('fadingrollover.js')."'></script>\n";
 	return $flux;
 }
