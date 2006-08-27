a81be994c25f224b4bfc9c6f7ec6a0d26407037e
christian_lefebvre@laposte.net <christian_lefebvre@laposte.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-08-27 13:45:26 +0000

    le plugin ne marchait plus en 1.9.1, j'en profite donc pour le modifier de façon plus évolutive
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@4889 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _typo_/fckeditor/fckeditor2_pipelines.php
@@ -0,0 +1,49 @@
+<?php
+
+/* A REVOIR ... */
+define('_DIR_PLUGIN_FCKEDITOR','/spip/plugins/_typo_/fckeditor');
+
+function fckeditor2_header_prive($flux) {
+	global $exec;
+	if($exec=='articles') {
+		$code='
+  <script src="http://www.jquery.info/scripts/jquery-1.0.js" type="text/javascript"></script>
+<script type="text/javascript"><!--
+$(document).ready(function () {
+	$("img[@src=\'img_pack/edit.gif\']").parent().each(function(i){
+		$(this).before(this.cloneNode(true));
+		$(this).attr("href", $(this).attr("href")+"&wysiwyg=oui");
+		$(this).find("span").html(\''.addslashes(_T('fckeditor:icone_modifier_article_fck')).'\');
+	});
+});
+// --></script>
+';
+		return $flux.$code;
+	}
+
+	if($exec=='articles_edit' && $GLOBALS['wysiwyg']=='oui') {
+		$code='
+<script type="text/javascript" src="'._DIR_PLUGIN_FCKEDITOR.'/FCKeditor/fckeditor.js"></script>
+<script type="text/javascript"><!--
+$(document).ready(function () {
+
+	var oFCKeditor = new FCKeditor( \'text_area\' ) ;
+	oFCKeditor.BasePath = "'._DIR_PLUGIN_FCKEDITOR.'/FCKeditor/" ;
+	oFCKeditor.Height = "400";
+	oFCKeditor.ReplaceTextarea();
+
+	$(".spip_barre").remove();
+});
+//-->
+
+</script>
+';
+		return $flux.$code;
+	}
+
+	return $flux;
+}
+
+//	$texte = html_entity_decode(entites_html($row['texte']));
+
+?>
