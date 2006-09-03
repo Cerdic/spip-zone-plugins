13c3cde5ae7436d62a4becdb2794926272513bb9
james@rezo.net <james@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-09-03 20:21:23 +0000

    le calendrier_mini sous forme de modèle
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@5090 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- calendrier_mini/balise/calendrier_mini.php
+++ calendrier_mini/balise/calendrier_mini.php
@@ -1,9 +1,10 @@
 <?php
 
-/* Balise #CALENDRIER_MINI
-   Auteur James (c) 2006
-   Plugin pour spip 1.9
-   Licence GNU/GPL
+/*
+	Balise #CALENDRIER_MINI
+	Auteur James (c) 2006
+	Plugin pour spip 1.9.1
+	Licence GNU/GPL
 */
 
 if (!defined("_ECRIRE_INC_VERSION")) return;	#securite
@@ -28,85 +29,6 @@ function balise_CALENDRIER_MINI_dyn($date, $id_rubrique = 0, $id_article = 0, $i
 			'id_article' => $id_article,
 			'id_mot' => $id_mot
 		));
-}
-
-function agenda_mini($i) {
-  $args = func_get_args();
-  $une_date = array_shift($args); // une date comme balise
-  $sinon = array_shift($args);
-  if (!$une_date) return $sinon;
-  $type = 'mini';
-  $agenda = agenda_memo(0);
-  $evt = array();
-  foreach (($args ? $args : array_keys($agenda)) as $k) {  
-      if (is_array($agenda[$k]))
-		foreach($agenda[$k] as $d => $v) { 
-		  $evt[$d] = $evt[$d] ? (array_merge($evt[$d], $v)) : $v;
-		}
-    }
-	$la_date = mktime(0, 0, 0, mois($une_date), 1, annee($une_date));
-    include_spip('inc/agenda');
-    return http_calendrier_init($la_date, $type, '', '', '', array('', $evt));
-}
-
-function http_calendrier_mini($annee, $mois, $jour, $echelle, $partie_cal, $script, $ancre, $evt) {
-	list($sansduree, $evenements, $premier_jour, $dernier_jour) = $evt;
-
-	if ($sansduree)
-		foreach($sansduree as $d => $r) {
-			$evenements[$d] = !$evenements[$d] ? $r : 
-				 array_merge($evenements[$d], $r);
-			 }
-
-	if (!$premier_jour) $premier_jour = '01';
-	if (!$dernier_jour) {
-		$dernier_jour = 31;
-		while (!(checkdate($mois,$dernier_jour,$annee))) $dernier_jour--;
-	}
-
-	// affichage du debut de semaine hors periode
-	$lang = _request('lang')?_request('lang'):$GLOBALS['spip_lang'];
-	$ligne = '';
-	$debut = date("w",mktime(1,1,1,$mois,$premier_jour,$annee));
-	$jour_semaine_lang=1; 
-	if($lang=='en') {
-		$debut=$debut+1;
-		if($debut==7) $debut=0;
-		$jour_semaine_lang=0;
-	} 
-	for ($i=$debut ? $debut : 7;$i>1;$i--) {
-		$ligne .= "\n\t<td>&nbsp;</td>";
-	}
-
-	$total = '';
-	for ($j=$premier_jour; $j<=$dernier_jour; $j++) {
-		$nom = mktime(1,1,1,$mois,$j,$annee);
-		$jour = date("d",$nom);
-		$jour_semaine = date("w",$nom);
-		$mois_en_cours = date("m",$nom);
-		$annee_en_cours = date("Y",$nom);
-		$amj = date("Y",$nom) . $mois_en_cours . $jour;
-
-		if ($jour_semaine==$jour_semaine_lang AND $ligne != '') { 
-			$total .= "\n<tr>$ligne\n</tr>";
-			$ligne = '';
-		}
-
-		$evts = $evenements[$amj];
-		if ($evts) {
-			$evts = "<a href=\"".$evts[0]['URL']."\">".$evts[0]['SUMMARY']."</a>";
-		}
-		else {
-			$evts = intval($jour);
-		}
-		$ligne .= "\n\t<td".($amj == date("Ymd")?' class="today"':'').">" . $evts . "\n\t</td>";
-	}
-	// affichage de la fin de semaine hors periode
-	for($j=$jour_semaine ? $jour_semaine+(1-$jour_semaine_lang) : 7; $j<7; $j++) {
-		$ligne .= "\n\t<td>&nbsp;</td>";
-	}
-
-	return $total . ($ligne ? "\n<tr>$ligne\n</tr>" : '');
 }
 
 ?>
