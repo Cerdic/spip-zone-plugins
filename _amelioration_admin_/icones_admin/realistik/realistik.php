e8b05f162e328d06abb7bee34cc8be19438ac656
franck.ducas@free.fr <franck.ducas@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-09-03 16:42:25 +0000

    Mis un fond plus joli aux images du menu.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@5081 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _amelioration_admin_/icones_admin/realistik/realistik.php
@@ -0,0 +1,12 @@
+<?php
+
+function realistik_header_prive($flux){
+$p=explode(basename(_DIR_PLUGINS)."/",str_replace('\\','/',realpath(dirname(__FILE__))));
+define('_DIR_PLUGIN_ICONES_ADMIN',(_DIR_PLUGINS.end($p)));
+global $exec;
+	$flux .= '<link rel="stylesheet" href="'._DIR_PLUGIN_ICONES_ADMIN.'/../img_pack/style.css" />'."\n";
+	return $flux;
+}
+
+
+?>
