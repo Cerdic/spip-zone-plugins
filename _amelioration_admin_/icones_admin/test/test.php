637382979dc02461c7f6f86c17aca3187594c31d
franck.ducas@free.fr <franck.ducas@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-09-05 15:25:41 +0000

    Ca ne marche toujours pas, mais je teste. Je me demande si ce marchera un jour en raison de l'initiatlisation des constantes qui se fait avant les plugins...
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@5121 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _amelioration_admin_/icones_admin/test/test.php
+++ _amelioration_admin_/icones_admin/test/test.php
@@ -1,11 +1,11 @@
 <?php
-	
+
 function test_mes_options($flux) {
-	$p=explode(basename(_DIR_PLUGINS)."/",str_replace('\\','/',realpath(dirname(__FILE__))));
-	define('_DIR_PLUGIN_TEST',(_DIR_PLUGINS.end($p)));
-	$directory = _DIR_PLUGIN_TEST;
-	$flux = "define('_DIR_IMG_PACK', ('$directory/img_pack/'));";
-	return $flux;
-}
+    $p=explode(basename(_DIR_PLUGINS)."/",str_replace('\\','/',realpath(dirname(__FILE__))));
+    define('_DIR_PLUGIN_TEST',(_DIR_PLUGINS.end($p)));
+    $directory = _DIR_PLUGIN_TEST;
+    $flux = define('_DIR_IMG_PACK', ('$directory/img_pack/'));
+    return $flux;
+} 
 
 ?>
