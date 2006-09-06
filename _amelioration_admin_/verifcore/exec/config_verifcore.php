52ce7cca39405443655a932665ecab6e58b3cf06
rudjob@gmail.com <rudjob@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-09-06 14:42:08 +0000

    recuperer_page plutot que fopen (James)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@5149 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _amelioration_admin_/verifcore/exec/config_verifcore.php
+++ _amelioration_admin_/verifcore/exec/config_verifcore.php
@@ -3,7 +3,8 @@
 define('_DIR_PLUGIN_VERIFCORE',(_DIR_PLUGINS.end(explode(basename(_DIR_PLUGINS)."/",str_replace('\\','/',realpath(dirname(__FILE__)))))));
 function exec_config_verifcore() {
   include_spip ("inc/presentation");
-  
+  include_spip ("inc/distant"); 
+ 
   debut_page('configurations');
   echo '<br><br><br>';
   gros_titre("yes");
@@ -22,12 +23,12 @@ function exec_config_verifcore() {
   $branche = "";
   $dateversion = "";
 
-  chdir (_DIR_RACINE) ;
-  
-  $fich =fopen("http://zone.spip.org/trac/spip-zone/browser/_plugins_/_amelioration_admin_/verifcore/refcorespip191.txt?format=txt", "r");
+  chdir (_DIR_RACINE) ; 
+  $fich =recuperer_page("http://zone.spip.org/trac/spip-zone/browser/_plugins_/_amelioration_admin_/verifcore/refcorespip191.txt?format=txt");
   if($fich){
-    while (!feof($fich)) {
-      $contenu = fgets($fich, 4096);
+    $tab_fic = split( "\n" ,"$fich") ;
+
+    foreach ( $tab_fic as $contenu ){
       trim($contenu) ;
       if(preg_match('/svn:\/\/trac.rezo.net\/spip\/([0-9a-zA-Z\/\.\_\-]*)/', $contenu,$res))
 	{
@@ -44,35 +45,31 @@ function exec_config_verifcore() {
 	  }
 	}
     }
-    fclose($fich) ;
-  }else{
-    echo "Attention le fichier http://zone.spip.org/trac/spip-zone/browser/_plugins_/_amelioration_admin_/verifcore/refcorespip191.txt?format=txt n\'existe plus\n<br />" ;
-    echo "Contactez auteur du plugin rudjob chez gmail.com" ;
-  }
-  $fich2 =fopen("http://zone.spip.org/trac/spip-zone/browser/_plugins_/_amelioration_admin_/verifcore/fichier_repertoire_supprimer_depuis_svn6797.txt?format=txt", "r"); 
-  if($fich2){
-    $com = 0 ;
-    while (!feof($fich2)) {
-      $contenu = fgets($fich2, 4096);
-      trim($contenu) ;
-      if(preg_match('/Fichiers supprimés/', $contenu )){
-	$com = 1 ;  
-      }elseif(preg_match('/Dossiers supprimés/', $contenu )){
-	$com = 2 ;  
-      }elseif(preg_match('/([0-9a-zA-Z\/\.\_\-]*)/', $contenu,$res)&&$com==1){
-	if(is_file("$res[1]")){
-	  $fichier_supprime[] = "$res[1]" ;	
-	}
-      }elseif(preg_match('/([0-9a-zA-Z\/\.\_\-]*)/', $contenu,$res)&&$com==2){
-	if(is_dir("$res[1]")){
-	  $dossier_supprime[] = "$res[1]" ;
+
+    $fich =recuperer_page("http://zone.spip.org/trac/spip-zone/browser/_plugins_/_amelioration_admin_/verifcore/fichier_repertoire_supprimer_depuis_svn6797.txt?format=txt"); 
+    if($fich){
+      $tab_fic = split( "\n" ,"$fich") ;
+      $com = 0 ;
+      foreach ( $tab_fic as $contenu ){
+	trim($contenu) ;
+	if(preg_match('/Fichiers supprimés/', $contenu )){
+	  $com = 1 ;  
+	}elseif(preg_match('/Dossiers supprimés/', $contenu )){
+	  $com = 2 ;  
+	}elseif(preg_match('/([0-9a-zA-Z\/\.\_\-]*)/', $contenu,$res)&&$com==1){
+	  if(is_file("$res[1]")){
+	    $fichier_supprime[] = "$res[1]" ;	
+	  }
+	}elseif(preg_match('/([0-9a-zA-Z\/\.\_\-]*)/', $contenu,$res)&&$com==2){
+	  if(is_dir("$res[1]")){
+	    $dossier_supprime[] = "$res[1]" ;
+	  }
 	}
       }
     }
-    fclose($fich2) ;
-  }
+
   
-  echo "Ce programme effectue une comparaison entre vos fichiers et ceux de la version $branche dat&eacute; du $dateversion<br />" ;
+    echo "Ce programme effectue une comparaison entre vos fichiers et ceux de la version $branche dat&eacute; du $dateversion<br />" ;
     if( count($fichier_inexistant) == 0 && count($fichier_modifie) == 0 && count($fichier_supprime) == 0 && count($dossier_supprime) == 0 ){
       echo "Tout va tr&egrav;s bien vos fichiers correspondent bien &agrave; la version $branche" ;
     }else{
@@ -107,7 +104,12 @@ function exec_config_verifcore() {
 	}
 	echo "</ul>\n" ;
       }
-    }
+    } 
+
+  }else{
+    echo "Attention le fichier http://zone.spip.org/trac/spip-zone/browser/_plugins_/_amelioration_admin_/verifcore/refcorespip191.txt?format=txt n\'existe plus\n<br />" ;
+    echo "Contactez auteur du plugin rudjob chez gmail.com" ;
+  }
   
   fin_cadre_enfonce();
 
