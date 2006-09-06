2c43bb456540e6a7b3cc979aca039c5cde1a4e9a
maieulrouquette2@tele2.fr <maieulrouquette2@tele2.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-09-06 14:06:34 +0000

    oubli !
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@5148 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _contenu_editorial_/mot-croises/mot_croises_avant_propre.php
+++ _contenu_editorial_/mot-croises/mot_croises_avant_propre.php
@@ -2,13 +2,29 @@
 
 function mot_croises_pre_propre($texte){
 	include_spip('inc/calculer_grille');
-	$tableau = $texte;					//sera uniquement le tableau spip, mais on attend pour le moment
 	
-	$tableau_php = calcul_tableau_grille2($tableau); 
+	$tableau = preg_split("/<grille>|<\/grille>/",$texte);					//sera uniquement le tableau spip, mais on attend pour le moment
+	$j =0;
+	foreach ($tableau as $i){
+			if ($j!=0 and $j!=count($tableau)-1)	//pas lex extremités du tableau
+				{
+				include_spip('inc/affichage_grille');
+				$tableau[$j] = affichage_grille(calcul_tableau_grille2($tableau[$j]));
+				
+				}
+			
+			
+			$j++;
+			}
+		
+		
+	
+	
+	
 	include_spip('inc/affichage_grille');
 	
 	
-	return affichage_grille($tableau_php);
+	return implode($tableau);
 }
 
 
