5ddf074b36b62d15b8d9ea2aee186772a2b98004
james@rezo.net <james@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-09-07 15:48:30 +0000

    corrigé le header_prive
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@5177 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _contenu_editorial_/mot-croises/mot_croises_header_prive.php
+++ _contenu_editorial_/mot-croises/mot_croises_header_prive.php
@@ -1,6 +1,7 @@
 <?php 
 
-function mot_croises_header_prive(){
-
-	return '<link rel="stylesheet" href="'.find_in_path('mots-croises-prive.css').'" />';}
+function mot_croises_header_prive($flux){
+	$flux .= '<link rel="stylesheet" href="'.find_in_path('mots-croises-prive.css').'" />';
+	return $flux;
+}
 ?>
