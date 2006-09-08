f84cb80735f43e68d2c5d7202516b5bd00fcdb00
renato@rezo.net <renato@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-09-08 13:23:24 +0000

    French description, works for public site too (James)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@5190 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _ze_laboratoire_/tableless_barre/tableless_barre_pipelines.php
+++ _ze_laboratoire_/tableless_barre/tableless_barre_pipelines.php
@@ -4,4 +4,9 @@ function tableless_barre_header_prive($flux){
 	$flux .= '<link rel="stylesheet" href="'.find_in_path('tableless_barre.css').'" type="text/css" media="projection, screen" />';
 	return $flux;
 }
+
+function tableless_barre_insert_head($flux){
+	return tableless_barre_header_prive($flux);
+}
+
 ?>
