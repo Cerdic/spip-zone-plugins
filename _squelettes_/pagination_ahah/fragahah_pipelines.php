10cba62e4db550d99e75d50b7163bb4962d2ebc8
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-09-09 22:54:30 +0000

    css+icone searching
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@5224 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _squelettes_/pagination_ahah/fragahah_pipelines.php
+++ _squelettes_/pagination_ahah/fragahah_pipelines.php
@@ -1,6 +1,7 @@
 <?php
 
 function fragahah_insert_head($flux){
+	$flux .= '<link rel="stylesheet" href="'.find_in_path('pagination-ahah.css').'" type="text/css" media="projection, screen" />';
 	$flux .= "<script type='text/javascript' src='".find_in_path('pagination-ahah.js')."'></script>\n";
 	return $flux;
 }
