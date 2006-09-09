a2ebfa5b340fad5181445adf29a56187ef1194ef
maieulrouquette2@tele2.fr <maieulrouquette2@tele2.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-09-09 19:18:25 +0000

    plus grille_xxx mais motcroises_xx (snif, aps de tirer dans les fichiers de langues)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@5219 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _contenu_editorial_/mot-croises/inc/affichage_grille.php
+++ _contenu_editorial_/mot-croises/inc/affichage_grille.php
@@ -4,19 +4,18 @@ function lettre($texte) //return la lettre correpondant au chiffres
  return $alphabet[$texte-1];}
 
 function affichage_grille($tableau_grille,$solution=false){
-	//affiche la grille de mot croisés, avec le forumulaire au cas où
+	//affiche la grille de mot croisés, avec la solution au cas où
 	
 	// les variables de la grille
-	
 	(! $solution) ? $page=self() : pass ; 
 	$hauteur =sizeof($tableau_grille);
     $largeur= sizeof($tableau_grille[0]);
     $grille='';
     //fin variable de la grille
     
-    (! $solution) ? $grille.="<form action=\"".$page."\" method=\"post\">\n" : $grille.="<div class=\"solution\"><h2 class=\"spip\">"._T('grille:Solution')." : </h2>" ;	// debut formulaire
+    (! $solution) ? $grille.="<form action=\"".$page."\" method=\"post\">\n" : $grille.="<div class=\"solution\"><h2 class=\"spip\">"._T('motscroises:Solution')." : </h2>" ;	// debut formulaire
     
-    $grille.="<table class=\"grille\" summary=\""._T('grille:table_summary',Array('hauteur'=>$hauteur,'largeur'=>$largeur))."\">\n
+    $grille.="<table class=\"grille\" summary=\""._T('motscroises:table_summary',Array('hauteur'=>$hauteur,'largeur'=>$largeur))."\">\n
     \t<tr>\n\t\t<td class=\"coin\"></td>\n";	// debut tableau + 1ere celule
 	
 	$increment_largeur=1;   //un iccrément pour les cellules d'entete
@@ -51,7 +50,7 @@ function affichage_grille($tableau_grille,$solution=false){
             		
             		
             		
-            		$grille.='<label for="col'.$colonne.'lig'.$ligne.'">'._T('grille:ligne',Array('n'=>lettre($ligne))).';'._T('grille:colonne',Array('n'=>$colonne)).'</label>';
+            		$grille.='<label for="col'.$colonne.'lig'.$ligne.'">'._T('motscroises:ligne',Array('n'=>lettre($ligne))).';'._T('motscroises:colonne',Array('n'=>$colonne)).'</label>';
                     
                     if (isset($GLOBALS['col'.$colonne.'lig'.$ligne])) //: test l'existence de la variable global correpsonte à cette cellule
                         {
@@ -75,8 +74,8 @@ function affichage_grille($tableau_grille,$solution=false){
 	
 	$grille.="</table>\n";
 	
-	(!$solution) ? $grille.="<br /><input id=\"solution\" name=\"solution[]\" type=\"checkbox\"value=\"1\" /><label for=\"solution\" >"._T('grille:afficher_solution')."</label><br />\n
-<input type=\"submit\" value=\""._T('grille:verifier')."\" name=\"bouton_envoi\" /></form>\n" : $grille.="</div>";
+	(!$solution) ? $grille.="<br /><input id=\"solution\" name=\"solution[]\" type=\"checkbox\"value=\"1\" /><label for=\"solution\" >"._T('motscroises:afficher_solution')."</label><br />\n
+<input type=\"submit\" value=\""._T('motscroises:verifier')."\" name=\"bouton_envoi\" /></form>\n" : $grille.="</div>";
 
 return $grille;}
 ?>
