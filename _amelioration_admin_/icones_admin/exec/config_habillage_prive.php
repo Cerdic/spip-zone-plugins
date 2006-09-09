9a13f24d83bc113cc5c441164accd39a61788eae
franck.ducas@free.fr <franck.ducas@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-09-09 07:04:30 +0000

    Il y avait un probleme si l'on visitait la page sans faire un seul choix et qu'on la quittait : ceci creait un mauvais code dans mes_options. C'est regle dans cette version.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@5206 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _amelioration_admin_/icones_admin/exec/config_habillage_prive.php
+++ _amelioration_admin_/icones_admin/exec/config_habillage_prive.php
@@ -59,104 +59,104 @@ function exec_config_habillage_prive() {
  	$theme = $_REQUEST['theme'];
  	$plugin_directory = _DIR_PLUGIN_HABILLAGE_PRIVE;
 
- 	## ATTENTION : LE CAS DE FIGURE DANS LEQUEL L'UTILISATEUR OUVRE CETTE PAGE SANS
- 	## CHOISIR UNE CASE A COCHER N'A PAS ETE PROGRAMME, CE QUI CAUSE UNE ERREUR DANS
- 	## LE FICHIER MES_OPTIONS.PHP SI L'ON VISITE JUSTE CETTE PAGE SANS RIEN FAIRE.
- 	
- 	// Si le fichier inc/mes_options.php existe deja
- 	if (file_exists($options_file)) {
-		$backup_file = "$options_file.backup";
-	 	rename($options_file, $backup_file);
-	 	$open_backup_file = fopen($backup_file, 'r');
-		$backup_file_size = filesize ($backup_file);
- 		$read_backup_file = fread ($open_backup_file, $backup_file_size);
- 		$search_comment = eregi("//start_define_img_pack(.*)//end_define_img_pack", $read_backup_file, $comment);
- 		$search_original_content = eregi("define\(\'_DIR_IMG_PACK\', \(\'(.*)\'\)\)\;", $read_backup_file, $original_content);
- 		$search_content = eregi("define\(\'_DIR_IMG_PACK\', \(\'(.*)\'\)\)\;(.*)//end_define_img_pack", $read_backup_file, $content);
- 		$search_all_content = eregi("<\?(.*)define\(\'_DIR_IMG_PACK\', \(\'(.*)\'\)\)\;(.*)\?>", $read_backup_file, $all_content);
-	 	
-	 	# Si l'utilisateur ou l'utilisatrice ne demande pas a revenir a la situation 
-	 	# initiale (= a son chemin vers img_pack d'origine).
-	 	if ($theme != "initial") {
-
-		 	# Si le fichier ecrire/mes_options.php contient le commentaire ajoute par 
-	 		# le plugin, cela signifie que le plugin a deja ete active pour un habillage.
-	 		# Il faut donc modifier la ligne existante personnalisee du chemin vers 
-	 		# img_pack. :
-	 		if ($search_comment) {
-		 		$open_options_file = fopen($options_file, 'w+');
-		 		$new_content = $plugin_directory."/themes/".$theme."/img_pack/";
-		 		$insert_new_content = ereg_replace($content[1], $new_content, $read_backup_file);
-		 		$write = fwrite($open_options_file, $insert_new_content);
-		 		fclose($open_options_file);
-	 		}
-	 		
-	 		# Si le fichier mes_options sauvegarde redefinissait le chemin d'img_pack
-	 		# par la ligne define('_DIR_IMG_PACK', [...]) avant le choix d'un autre 
-	 		# habillage :
-	 		else if ($search_original_content) {
-		 		$search_comment_backup = eregi("//backup(.*)", $read_backup_file);
+ 	# N'agir que si le theme n'est pas vide. Ceci permet, en cas de non-choix, de ne pas
+ 	# ecrire un fichier mes_options.php errone. 	
+ 	if ($theme != "") {
+	 	// Si le fichier inc/mes_options.php existe deja
+	 	if (file_exists($options_file)) {
+			$backup_file = "$options_file.backup";
+		 	rename($options_file, $backup_file);
+		 	$open_backup_file = fopen($backup_file, 'r');
+			$backup_file_size = filesize ($backup_file);
+	 		$read_backup_file = fread ($open_backup_file, $backup_file_size);
+	 		$search_comment = eregi("//start_define_img_pack(.*)//end_define_img_pack", $read_backup_file, $comment);
+	 		$search_original_content = eregi("define\(\'_DIR_IMG_PACK\', \(\'(.*)\'\)\)\;", $read_backup_file, $original_content);
+	 		$search_content = eregi("define\(\'_DIR_IMG_PACK\', \(\'(.*)\'\)\)\;(.*)//end_define_img_pack", $read_backup_file, $content);
+	 		$search_all_content = eregi("<\?(.*)define\(\'_DIR_IMG_PACK\', \(\'(.*)\'\)\)\;(.*)\?>", $read_backup_file, $all_content);
+		 	
+		 	# Si l'utilisateur ou l'utilisatrice ne demande pas a revenir a la situation 
+		 	# initiale (= a son chemin vers img_pack d'origine).
+		 	if ($theme != "initial") {
+	
+			 	# Si le fichier ecrire/mes_options.php contient le commentaire ajoute par 
+		 		# le plugin, cela signifie que le plugin a deja ete active pour un habillage.
+		 		# Il faut donc modifier la ligne existante personnalisee du chemin vers 
+		 		# img_pack. :
+		 		if ($search_comment) {
+			 		$open_options_file = fopen($options_file, 'w+');
+			 		$new_content = $plugin_directory."/themes/".$theme."/img_pack/";
+			 		$insert_new_content = ereg_replace($content[1], $new_content, $read_backup_file);
+			 		$write = fwrite($open_options_file, $insert_new_content);
+			 		fclose($open_options_file);
+		 		}
 		 		
-		 		if ($search_comment_backup) {
+		 		# Si le fichier mes_options sauvegarde redefinissait le chemin d'img_pack
+		 		# par la ligne define('_DIR_IMG_PACK', [...]) avant le choix d'un autre 
+		 		# habillage :
+		 		else if ($search_original_content) {
+			 		$search_comment_backup = eregi("//backup(.*)", $read_backup_file);
+			 		
+			 		if ($search_comment_backup) {
+				 		$open_options_file = fopen($options_file, 'w+');
+				 		$new_content = "//start_define_img_pack\ndefine('_DIR_IMG_PACK', ('".$plugin_directory."/themes/".$theme."/img_pack/'));\n//end_define_img_pack\n?>";
+				 		$insert_new_content = ereg_replace( '\?>', $new_content, $read_backup_file);
+				 		$write = fwrite($open_options_file, $insert_new_content);
+				 		fclose($open_options_file);
+			 		}
+			 		else {
+				 		$open_options_file = fopen($options_file, 'w+');
+				 		$replaced_content = "define\(\'_DIR_IMG_PACK\', \(\'";
+				 		$new_content = "//start_define_img_pack\ndefine('_DIR_IMG_PACK', ('".$plugin_directory."/themes/".$theme."/img_pack/'));\n//end_define_img_pack\n//backup_define('_DIR_IMG_PACK', ('";
+				 		$insert_new_content = ereg_replace( $replaced_content, $new_content, $read_backup_file);
+				 		$write = fwrite($open_options_file, $insert_new_content);
+				 		fclose($open_options_file);
+		 			}
+		 		}
+		 		
+		 		# Si le fichier ecrire/mes_options.php existe deja mais qu'il ne redefinie 
+		 		# pas le chemin vers img_pack.
+		 		else {
 			 		$open_options_file = fopen($options_file, 'w+');
 			 		$new_content = "//start_define_img_pack\ndefine('_DIR_IMG_PACK', ('".$plugin_directory."/themes/".$theme."/img_pack/'));\n//end_define_img_pack\n?>";
 			 		$insert_new_content = ereg_replace( '\?>', $new_content, $read_backup_file);
 			 		$write = fwrite($open_options_file, $insert_new_content);
 			 		fclose($open_options_file);
 		 		}
-		 		else {
+		 		
+	 		}
+	 		
+	 		# Si l'utilisateur ou l'utilisatrice veut revenir a la situation initiale.
+	 		else if ($theme == "initial") {
+		 		$search_comment_backup = eregi("//backup_define\(\'_DIR_IMG_PACK\', \(\'(.*)\'\)\)\;", $read_backup_file);
+		 		
+		 		if ($search_comment) {
 			 		$open_options_file = fopen($options_file, 'w+');
-			 		$replaced_content = "define\(\'_DIR_IMG_PACK\', \(\'";
-			 		$new_content = "//start_define_img_pack\ndefine('_DIR_IMG_PACK', ('".$plugin_directory."/themes/".$theme."/img_pack/'));\n//end_define_img_pack\n//backup_define('_DIR_IMG_PACK', ('";
-			 		$insert_new_content = ereg_replace( $replaced_content, $new_content, $read_backup_file);
+			 		$erased_content = "//start_define_img_pack(.*)//end_define_img_pack";
+			 		$insert_new_content = ereg_replace($erased_content, '', $read_backup_file);
 			 		$write = fwrite($open_options_file, $insert_new_content);
 			 		fclose($open_options_file);
-	 			}
+		 		}
+		 		
+		 		else if ($search_comment_backup){
+			 		$open_options_file = fopen($options_file, 'w+');
+			 		$insert_new_content = ereg_replace( '//backup_define', 'define', $read_backup_file);
+			 		$write = fwrite($open_options_file, $insert_new_content);
+			 		fclose($open_options_file);
+			 	}
+			 	
+		 		else {
+			 		rename($backup_file, $options_file);
+		 		}
 	 		}
-	 		
-	 		# Si le fichier ecrire/mes_options.php existe deja mais qu'il ne redefinie 
-	 		# pas le chemin vers img_pack.
-	 		else {
-		 		$open_options_file = fopen($options_file, 'w+');
-		 		$new_content = "//start_define_img_pack\ndefine('_DIR_IMG_PACK', ('".$plugin_directory."/themes/".$theme."/img_pack/'));\n//end_define_img_pack\n?>";
-		 		$insert_new_content = ereg_replace( '\?>', $new_content, $read_backup_file);
-		 		$write = fwrite($open_options_file, $insert_new_content);
-		 		fclose($open_options_file);
-	 		}
-	 		
- 		}
- 		
- 		# Si l'utilisateur ou l'utilisatrice veut revenir a la situation initiale.
- 		else if ($theme == "initial") {
-	 		$search_comment_backup = eregi("//backup_define\(\'_DIR_IMG_PACK\', \(\'(.*)\'\)\)\;", $read_backup_file);
-	 		
-	 		if ($search_comment) {
-		 		$open_options_file = fopen($options_file, 'w+');
-		 		$erased_content = "//start_define_img_pack(.*)//end_define_img_pack";
-		 		$insert_new_content = ereg_replace($erased_content, '', $read_backup_file);
-		 		$write = fwrite($open_options_file, $insert_new_content);
-		 		fclose($open_options_file);
-	 		}
-	 		
-	 		else if ($search_comment_backup){
-		 		$open_options_file = fopen($options_file, 'w+');
-		 		$insert_new_content = ereg_replace( '//backup_define', 'define', $read_backup_file);
-		 		$write = fwrite($open_options_file, $insert_new_content);
-		 		fclose($open_options_file);
-		 	}
-		 	
-	 		else {
-		 		rename($backup_file, $options_file);
-	 		}
- 		}
- 	}
- 	
- 	else {
-	 	$open_options_file = fopen($options_file, 'w+');
-	 	$new_content = "<?\n//start_define_img_pack\ndefine('_DIR_IMG_PACK', ('".$plugin_directory."/themes/".$theme."/img_pack/'));\n//end_define_img_pack\n?>";
-		$write = fwrite($open_options_file, $new_content);
-		fclose($open_options_file);
- 	}
+	 	}
+	 	
+	 	else {
+		 	$open_options_file = fopen($options_file, 'w+');
+		 	$new_content = "<?\n//start_define_img_pack\ndefine('_DIR_IMG_PACK', ('".$plugin_directory."/themes/".$theme."/img_pack/'));\n//end_define_img_pack\n?>";
+			$write = fwrite($open_options_file, $new_content);
+			fclose($open_options_file);
+	 	}
+	}
  	
  	echo '<INPUT type=radio name="theme" value="initial"';
  		if ($_REQUEST['theme'] == "initial") {
