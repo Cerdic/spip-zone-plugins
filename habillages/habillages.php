57d0c01acc7d58e2fb68de2c5a1fa5af2e5f02a8
franck.ducas@free.fr <franck.ducas@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-09-11 06:45:34 +0000

    L'icone du plugine ne s'affichait pas quand ce dernier était active : c'est regle. Attention enependant : tous les pleugines qui definçissent leur icone d'onglet par un chemin ".../".DIR_MON_PLEUGUINE."/img_pack" et non "/img_pack" ne verront pas leurs icones affichées en cas d'activation du présent pleuguine. Je vais tester avec plusieurs plugins et vous donne le fruit de mes tests...
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@5252 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- habillages/habillages.php
+++ habillages/habillages.php
@@ -6,7 +6,7 @@ define('_DIR_PLUGIN_HABILLAGES',(_DIR_PLUGINS.end($p)));
 function habillages_ajouter_onglets($flux) {
   if($flux['args']=='configuration')
 	$flux['data']['config_habillages']= new Bouton(
-	'../'._DIR_PLUGIN_HABILLAGES.'/img_pack/habillage_prive-22.png', 'Habillages', generer_url_ecrire("config_habillages"));
+	'habillage_prive-22.png', 'Habillages', generer_url_ecrire("config_habillages"));
   return $flux;
 }
 
