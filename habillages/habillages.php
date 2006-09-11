c208b0a91bca0b6b9462096ca4504b1f897c8f5f
franck.ducas@free.fr <franck.ducas@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-09-11 12:38:14 +0000

    Ajout d'une feuille de style pour les themes d'icones. Mettre une feuille de style nommee "style.css" dans le repertoire img_pack du theme. Pour l'instant, j'ai juste mis une feuille de style dans habillages/prive/themes/realistik/img_pack.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@5256 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- habillages/habillages.php
+++ habillages/habillages.php
@@ -10,4 +10,18 @@ function habillages_ajouter_onglets($flux) {
   return $flux;
 }
 
+function habillages_header_prive($flux){
+
+	global $exec;
+	
+	$options_file = "mes_options.php";
+	$plugin_directory = _DIR_PLUGIN_HABILLAGES;
+	$open_options_file = fopen($options_file, 'r');
+	$options_file_size = filesize ($options_file);
+	$read_options_file = fread ($open_options_file, $options_file_size);
+	$search_template_name = eregi("$plugin_directory/prive/themes/(.*)/img_pack/", $read_options_file, $template_name);
+	$flux .= '<link rel="stylesheet" href="'.$plugin_directory.'/prive/themes/'.$template_name[1].'/img_pack/style.css" />'."\n";
+	
+	return $flux;
+}
 ?>
