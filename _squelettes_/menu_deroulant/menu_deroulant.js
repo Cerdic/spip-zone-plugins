177570ed8a81747d75c16c56a06ce66cd928011f
fil@rezo.net <fil@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-09-14 14:36:03 +0000

    msie seulement pour la partie js
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@5354 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _squelettes_/menu_deroulant/menu_deroulant.js
+++ _squelettes_/menu_deroulant/menu_deroulant.js
@@ -2,12 +2,14 @@
  * Javascript pour le menu deroulant sur MSIE
  *
  * adapte de http://www.htmldog.com/articles/suckerfish/dropdowns/example/
- * et passe en jquery
+ * et passe en jquery pour sa partie javascript (necessaire sous MSIE)
  *
  */
+if (jQuery.browser.msie) {
 $(document).ready(function(){
 	$('#nav li').hover(
 		function(){$(this).addClass('sfhover')},
 		function(){$(this).removeClass('sfhover')}
 	);
 });
+}
