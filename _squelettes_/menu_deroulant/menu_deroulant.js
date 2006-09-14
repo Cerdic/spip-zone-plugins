0bf5f11595dc9d43fcee967902395bc9f7287845
fil@rezo.net <fil@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-09-14 13:40:17 +0000

    plus de jquery
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@5353 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _squelettes_/menu_deroulant/menu_deroulant.js
+++ _squelettes_/menu_deroulant/menu_deroulant.js
@@ -2,24 +2,12 @@
  * Javascript pour le menu deroulant sur MSIE
  *
  * adapte de http://www.htmldog.com/articles/suckerfish/dropdowns/example/
+ * et passe en jquery
  *
  */
-/*sfHover = function() {
-	var sfEls = document.getElementById("nav").getElementsByTagName("li");
-	for (var i=0; i<sfEls.length; i++) {
-		sfEls[i].onmouseover=function() {
-			this.className+=" sfhover";
-		}
-		sfEls[i].onmouseout=function() {
-			this.className=this.className.replace(new RegExp(" sfhover\\b"), "");
-		}
-	}
-}*/
-//if (window.attachEvent) window.attachEvent("onload", sfHover);
-
 $(document).ready(function(){
-	$('li',document.getElementById("nav")).hover(
+	$('#nav li').hover(
 		function(){$(this).addClass('sfhover')},
 		function(){$(this).removeClass('sfhover')}
-		);
+	);
 });
