65c85b5634227f86ee2590e4c889fc184d084d77
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-09-15 08:48:30 +0000

    mise a jour des title & lang sur les liens dans postpropre fait apparaitre que la structure de la table n'est pas optimale car on ne connait de toute facon pas l'id_objet et l'id_table a ce moment Menu pour le panneau d'admin
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@5382 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- checklink/checklink_mes_fonctions.php
+++ checklink/checklink_mes_fonctions.php
@@ -1,5 +1,32 @@
 <?php
 
+function url_visu_objet($id_table,$id_objet){
+	include_spip('inc/indexation');
+	$liste = liste_index_tables();
+	if (!isset($liste[$id_table])) return "";
+	$script = "";
+	$table = $liste[$id_table];
+	$f = str_replace('spip_','',$table);
+	if (find_in_path("exec/$f.php"))
+		$script = $f;
+	if (!$script){
+		$f2 = str_replace(array('rubriques','syndic'),array('naviguer','sites'),$f);
+		if (find_in_path("exec/$f2.php"))
+			$script = $f2;
+	}
+	if (!$script){
+		if (find_in_path("exec/{$f2}_tous.php"))
+			$script = $f2."_tous";
+	}
+	if (!$script){
+		if (find_in_path("exec/{$f}_tous.php"))
+			$script = $f."_tous";
+	}
+	if ($script){
+		return "index.php?exec=$script&".primary_index_table($table)."=".$id_objet;
+	}
+	return "";
+}
 
 
 ?>
--- checklink/checklink_pipelines.php
+++ checklink/checklink_pipelines.php
@@ -5,6 +5,19 @@ include_spip('base/checklink');
 include_spip('inc/checklink');
 include_spip('inc/texte');
 
+function checklink_ajouter_boutons($boutons_admin) {
+	// si on est admin
+	if ($GLOBALS['connect_statut'] == "0minirezo" && $GLOBALS["connect_toutes_rubriques"]
+	AND $GLOBALS["options"]=="avancees") {
+	  // on voit le bouton dans la barre "forum_admin"
+		$boutons_admin['forum']->sousmenu["liens_tous"]= new Bouton(
+		"../"._DIR_PLUGIN_CHECKLINK."/img_pack/checklink-24.png",  // icone
+		_T("sites_web") //titre
+		);
+	}
+	return $boutons_admin;
+}
+
 function checklink_pre_enregistre_contenu($flux){
 	static $objet_traite=array();
 	if (!isset($flux['args']['id_objet']) || !isset($flux['args']['table']))
@@ -24,6 +37,57 @@ function checklink_pre_enregistre_contenu($flux){
 	// passer le contenu dans propre pour transformer les liens internes et les modeles eventuels
 	$letexte = propre(join(' ',$flux['data']));
 	checklink_extrait_liens($id_table,$id_objet,$letexte);
+	return $flux;
+}
+
+function checklink_post_propre($flux){
+	// recuperer les liens des balises a et img
+	if (preg_match_all(
+	',<(a|img) [^>]*>,UimsS',
+	$flux, $regs, PREG_SET_ORDER)) {
+		$a_tag=array();$a_tag_repl=array();	
+		foreach ($regs as $reg) {
+			if (strtolower($reg[1])=='a')
+				$url = extraire_attribut($reg[0], 'href');
+			if (strtolower($reg[1])=='img'){
+				$url = extraire_attribut($reg[0], 'src');
+				if (!preg_match(',^http://,',$url)) $url = null;
+			}
+			// filtrer les ancres et les mailto:
+			if ($url){
+				$url = trim($url);
+				if (preg_match(',^(#|mailto:),',$url)) $url = null;
+			}
+			if ($url){
+				$tag_repl = $tag = $reg[0];
+				// prevoir les liens dont les attributs ont pu etre renseignes a la main
+				$titre = extraire_attribut($reg[0], 'title');
+				$lang = extraire_attribut($reg[0], 'lang');
+				$titre_auto = strlen($titre)?'non':'oui';
+				$lang_auto = strlen($lang)?'non':'oui';
+				
+				if ($titre_auto=='oui' OR $lang_auto=='oui')
+					if ($row = spip_fetch_array(spip_query("SELECT * FROM spip_liens WHERE url=".spip_abstract_quote($url)." AND titre_auto='$titre_auto' AND lang_auto='$lang_auto'"))){
+						if ($row['statut']=='sus' OR $row['statut']=='off'){
+							$class = extraire_attribut($tag_repl,'class');
+							$tag_repl = inserer_attribut($tag_repl,'class',"$class spip_url_obsolete");
+							$tag_repl = inserer_attribut($tag_repl,'title',_T('syndic_lien_obsolete'));
+						}
+						else{
+							if ($titre_auto=='oui' AND strlen($row['titre']))
+								$tag_repl = inserer_attribut($tag_repl,'title',$row['titre']);
+							if ($lang_auto=='oui' AND strlen($row['lang']))
+								$tag_repl = inserer_attribut($tag_repl,'lang',$row['lang']);
+						}
+						if ($tag_repl!=$tag){
+							$a_tag[] = $tag;
+							$a_tag_repl[] = $tag_repl;
+						}
+					}
+			}
+		}
+		$flux = str_replace($a_tag,$a_tag_repl,$flux);
+	}
 	return $flux;
 }
 
