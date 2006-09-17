1dddb29d429bbf565ad352786f9dd70fca7cf41b
renato@rezo.net <renato@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-09-17 19:39:00 +0000

    update to lastest interface version, i18n of the plugin, set to test state
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@5433 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _squelettes_/memobox/lang/memobox_en.php
@@ -0,0 +1,12 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+'drag_drop' => "Drag &amp; drop titles here to store them",
+'anchor_delete_title' => "delete this item",
+'anchor_delete_text' => "delete"
+);
+
+
+?>
---
+++ _squelettes_/memobox/lang/memobox_it.php
@@ -0,0 +1,12 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+'drag_drop' => "Clicca e trascina qui i titoli per memorizzarli",
+'anchor_delete_title' => "elimina questo titolo",
+'anchor_delete_text' => "elimina"
+);
+
+
+?>
