d9275363a492cdc0532d055cfda58f7e46f39327
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-09-18 22:33:32 +0000

    Rangement
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@5492 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _stable_/widget_calendar/1_9_0/inc/calendar.php
@@ -0,0 +1,65 @@
+<?php
+
+$p=explode(basename(_DIR_PLUGINS)."/",str_replace('\\','/',realpath(dirname(dirname(__FILE__)))));
+define('_DIR_PLUGIN_WIDGET_CALENDAR',(_DIR_PLUGINS.end($p)));
+define('_WIDGET_CALENDAR_BACK_IN_TIME',4);
+
+global $WCalendar_independants,$WCalendar_lies,$WCalendar_statiques;
+$WCalendar_independants=array();
+$WCalendar_lies=array();
+$WCalendar_statiques=array();
+
+function WCalendar_ajoute($titre,$suffixe){
+	global $WCalendar_independants;	
+	$WCalendar_independants[] = array('titre'=>$titre,'suffixe'=>$suffixe);
+}
+function WCalendar_ajoute_lies($titre_debut,$suffixe_debut,$titre_fin,$suffixe_fin){
+	global $WCalendar_lies;	
+	$WCalendar_lies[] = array('titre1'=>$titre_debut,'suffixe1'=>$suffixe_debut,
+														'titre2'=>$titre_fin,'suffixe2'=>$suffixe_fin);
+}
+function WCalendar_ajoute_statique($titre,$suffixe){
+	global $WCalendar_statiques;	
+	$WCalendar_statiques[] = array('titre'=>$titre,'suffixe'=>$suffixe);
+}
+function WCalendar_statique_point_entree($suffixe, $dates = ""){
+	return "<div><div id='container$suffixe' style='z-index:5000;'></div>
+	<div style='display:none;'><textarea id='selected_date$suffixe' name='selected_date$suffixe'>$dates</textarea></div>
+	<a href='javascript:cal$suffixe.reset()'>Reset</a>"
+	//. "<a href='javascript:alert(cal$suffixe.getSelectedDates())'>what's selected?</a>"
+	. "</div>
+	";
+}
+
+
+function WCalendar_header($flux,$onload=""){
+	global $init_functions;
+	$init_functions = $onload;
+	include_spip('inc/calendar_init');
+	return WCalendar_header_prive($flux);
+}
+function WCalendar_body($flux){
+	include_spip('inc/calendar_init');
+	return WCalendar_body_prive($flux);
+}
+
+function WCalendar_controller($date,$suffixe){
+	if (strcmp($date,format_mysql_date())==0)
+		$date=date("Y-m-d H:i:s");
+	$s = "<a href='javascript:void(null)' onclick='showCalendar$suffixe()'>
+	<img id='dateLink$suffixe' src='"._DIR_IMG_PACK."/cal-jour.gif' border='0' style='vertical-align:middle;margin:5px'/></a>
+	";
+	$s .=
+	  afficher_jour(jour($date), "id='jour$suffixe' name='jour$suffixe' size='1' class='fondl verdana1' onchange='changeDate$suffixe()'") .
+    afficher_mois(mois($date), "id='mois$suffixe' name='mois$suffixe' size='1' class='fondl verdana1' onchange='changeDate$suffixe()'") .
+    afficher_annee(annee($date), "id='annee$suffixe' name='annee$suffixe' size='1' class='fondl verdana1' onchange='changeDate$suffixe()'", date('Y')-_WIDGET_CALENDAR_BACK_IN_TIME);
+  return $s;
+}
+
+function WCalendar_statique_controller($dates,$suffixe){
+	if (is_array($dates))
+		$dates = implode(',',$dates);
+  return WCalendar_statique_point_entree($suffixe, $dates);
+}
+
+?>
