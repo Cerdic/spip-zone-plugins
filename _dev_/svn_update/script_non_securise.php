c8de6db4c8505bb496fc55fd0926894dd6488782
fil@rezo.net <fil@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-09-19 13:37:26 +0000

    un script qui sait faire svn checkout, update et switch... a mettre en interface utilisable
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@5552 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _dev_/svn_update/script_non_securise.php
@@ -0,0 +1,75 @@
+<?php
+
+
+	// la fonction qui fait le travail
+	function update_svn($l) {
+		$l = trim($l);
+
+		if (!$l OR substr($l,0,1) == '#') return NULL; // commentaires
+
+		@list($src, $dest, $rev, $user) = explode(' ',$l);
+
+		if (!preg_match(',^(https?|svn)://,', $src))
+			return $src; // erreur
+
+		if (!is_dir($dest))
+			mkdir($dest, 0777, 'recursive');
+
+		if (!is_dir($dest)
+		OR !is_writable($dest))
+			return $dest; // erreur
+
+		// Checkout ?
+		if (!file_exists($entries = "$dest/.svn/entries")) {
+			$command = "checkout $src/ $dest/";
+		}
+
+		else {
+			$entries = join("\n", file($entries));
+			if (!preg_match(',\surl="([^"]+)",', $entries, $r))
+				return "fichier .svn/entries non conforme ou illisible";
+			$old_src = $r[1];
+
+			// Switch ?
+			if ($old_src != $src) {
+				$command = "switch --relocate $old_src/ $src/ $dest/";
+			}
+			
+			// Update
+			else {
+				if ($rev)
+					$command = "update --revision $rev $dest/";
+				else
+					$command = "update $dest/";
+			}
+		}
+
+		if ($command) {
+			$command = _SVN_COMMAND." $user ".$command;
+			$out = array();
+			exec($command,$out);
+			array_unshift($out, $command);
+			return $out;
+		}
+
+	}
+
+	function traiter_config_svn($file = 'svn_update.txt') {
+		$config = file($file);
+		foreach($config as $l) {
+			echo "<hr /><b>", htmlspecialchars($l), "</b>";
+			$res = update_svn($l);
+			if (is_string($res))
+				echo "<br /><b>Erreur: ",
+					htmlspecialchars($res),
+					"</b>";
+			if (is_array($res))
+				echo "<br />".nl2br(htmlspecialchars(join("\n", $res)));
+		}
+	}
+
+	define('_SVN_COMMAND', '/sw/bin/svn');
+	define('_SVN_COMMAND', 'svn');
+	traiter_config_svn();
+
+?>
