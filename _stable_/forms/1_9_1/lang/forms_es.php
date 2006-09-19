47a0c41624030afc036265f08f763c94a33c2e20
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-09-19 13:42:29 +0000

    version espagnole, et une majuscule innoportune en francais
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@5554 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/forms/1_9_1/lang/forms_es.php
+++ _stable_/forms/1_9_1/lang/forms_es.php
@@ -1,10 +1,123 @@
 <?php
 
-// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP -- Esto es un fichero de idioma de SPIP
+// Traducido al español, JSJ septiembre 2006
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-'info_obligatoire_02' => '[Obligatorio]'
+'adresse_invalide'=>"Esta direcci&oacute;n de correo no es v&eacute;lida.",
+'ajouter_champ' => "A&ntilde;adir un campo",
+'ajouter_champ_type' => "Crear un campo de tipo&nbsp;:",
+'ajouter_choix' => "A&ntilde;adir una opci&oacute;n",
+'apparence_formulaire' => "Aspecto del formulario",
+'article_inserer_un_formulaire'=> "Insertar un formulario",
+'article_inserer_un_formulaire_detail' => "Podr&aacute; insertar formularios en sus art&iacute;cules para permitir que los visitantes introduzcan datos. Escoja un formulario de la lista adjunta y copie el bloque en el texto del art&iacute;culo.",
+'article_recopier_raccourci' => "Copie el atajo para insertar este formulario en el texto del art&iacute;cule",
+'articles_utilisant'=> "Art&iacute;culos que utilizan este formulario",
+'attention' => "Atenci&oacute;n:",
+'avis_message_confirmation'=>"Se ha sido enviado un mensaje de confirmaci&oacute;n a @mail@",
 
+'boite_info' => "Haga clic sobre un formulario para modificarlo o verlo antes de eliminarlo.",
+
+'champ_type_ligne'=>"l&iacute;nea de texto",
+'champ_type_texte'=>"texto",
+'champ_type_url'=>"direcci&eacute; del sitio Web",
+'champ_type_email'=>"direcci&eacute; e-mail",
+'champ_type_select'=>"opci&oacute;n &uacute;nica",
+'champ_type_multiple'=>"opci&oacute;n m&uacute;ltiple",
+'champ_type_fichier'=>"archivo a subir (upload)",
+'champ_type_mot'=>"palabras clave",
+'champ_type_separateur'=>"Nuevo bloque de preguntas",
+'champ_type_textestatique'=>"Mensaje de explicati&oacute;n",
+'champ_email_details'=>"Por favor, escriba una direcci&oacute;n de correo v&aacute;lida (como minombre@proveedor.com).",
+'champ_url_details'=>"Por favor, escriba una direcci&oacute;n Web v&aacute;lida (como http://www.misitio.com/...).",
+'champ_monter' => "subir",
+'champ_descendre' => "bajar",
+'champ_necessaire'=>"Debe completar este campo.",
+'champ_nom' => "Nombre del campo",
+'champ_nom_bloc' => "Nombre del bloque",
+'champ_nom_groupe' => "Grupo",
+'champ_nom_texte' => "Texto",
+'champs_formuaire'=>"Campos del formulario ",
+'changer_choix_multiple' => "Cambiar a elecci&oacute;n m&uacute;ltiple",
+'changer_choix_unique' => "Cambiar a elecci&oacute;n &uacute;nica",
+'choisir_email' => "Elegir el email en funci&oacute;n de",
+'confirmer_reponse' => "Confirmar la respuesta por mail con:",
+'csv_classique' => "CSV cl&aacute;sico (,)",
+'csv_excel' => "CSV para Excel (;)",
+'csv_tab' => "CSV con tabulaciones",
+
+'date' => "Fecha",
+'dupliquer' => "Duplicar",
+
+'echec_upload'=>"Ha fallado la transferencia del archivo.",
+'edit_champ_obligatoire' => 'es obligatorio rellenar este campo',
+'email_independant' => 'Email independiente de la respuesta',
+'exporter_article' => 'Exportar a un art&iacute;culo',
+
+'fichier_trop_gros'=>"Este archivo es demasiado grande.",
+'fichier_type_interdit'=>"Este tipo de archivo est&aacute; prohibido.",
+'form_erreur' => "Error&nbsp;:",
+'format_fichier' => "Formato del fichero&nbsp;:",
+'formulaire_aller' => "Ir al formulario ",
+'formulaires_sondages' => "Formularios y encuestas",
+'formulaires_copie' => "Copia de ", 
+
+'icone_creer_formulaire' => "Crear un nuevo formulario",
+'info_apparence' => "He aqu&iacute; una previsualizaci&oacute;n del formulario tal como aparecer&aacute; a los visitantes del sitio p&uacute;blico.",
+'info_champs_formulaire' => "Aqu&iacute; se crean y modifican los campos que luego podr&aacute;n rellenar los visitantes.",
+'info_obligatoire_02' => '[Obligatorio]',
+'info_sondage' => "Si su formulario  es una encuesta, les resultados de los campos de tipo &laquo; selecci&oacute;n &raquo; ser&aacute;n totalizados y mostrados.",
+'info_supprimer_formulaire' => "¿Realmente desea eliminar este formulario?",
+'info_supprimer_formulaire_reponses' => "Hay respuestas a este formulario. ¿Desea realmente eliminarlo?",
+
+'liste_choix' => "Lista de las opciones propuestas",
+
+'nombre_reponses'=>"@nombre@ respuestas",
+'nouveau_champ' => "Nuevo campo",
+'nouveau_choix' => "Nueva opci&oacute;n",
+'nouveau_formulaire' => "Nuevo formulario ",
+
+'page'=>"P&aacute;gina",
+'pas_mail_confirmation'=>"No hay mail de confirmaci&oacute;n",
+'probleme_technique'=>"Problema t&eacute;cnico. Su respuesta no ha podido ser tomada en cuenta.",
+'probleme_technique_upload'=>"Problema t&eacute;cnico. Ha fallado la transferencia del archivo.",
+
+'remplir_un_champ'=>"Por favor, complete al menos un campo.",
+'reponse_enregistree'=>"Su respuesta ha sido registrada.",
+'resultats' => 'Resultados: ',
+'reponse' => 'Respuesta @id_reponse@',
+'reponse_depuis' => 'Desde la p&aacute;gina ', 
+'reponse_envoyee' => 'Respuesta enviada el ',
+'reponse_envoyee_a' => 'a', 
+'reponses' => 'respuestas',
+
+'site_introuvable'=>"No se ha encontrado este sitio.",
+'sondage'=>"Encuesta",
+'sondage_non'=>" Este formulario  no es una encuesta",
+'sondage_pub'=>"Este formulario es una encuesta p&uacute;blica. Los resultados ser&aacute;n accessibles para los visitantes del sitio.",
+'sondage_prot'=>"Este formulario es un encuesta protegida. Los resultados no ser&aacute;n accessibles mas que en la parte privada del sitio.",
+'suivi_formulaire' => "Seguimiento del formulario ",
+'suivi_formulaires' => "Seguimiento de los formularios",
+'suivi_reponses' => "Seguimiento de las respuestas",
+'supprimer_champ' => "Suprimir este campo",
+'supprimer_choix' => "suprimir esta opci&oacute;n",
+'supprimer_formulaire' => "Suprimir este formulario ",
+'supprimer_reponse' => "Suprimir esta respuesta",
+
+'taille_max' => "Tama&ntilde;o m&aacute;ximo (en kb)",
+'telecharger' => "Descargar",
+'telecharger_reponses' => " Descargar las respuestas",
+'titre_formulaire' => "T&iacute;tulo del formulario ",
+'total_votes' => "Total de votos",
+'tous_formulaires' => "Todos los formularios",
+'tous_sondages_proteges' => "Todas las encuestas protegidas",
+'tous_sondages_public' => "Todas las encuestas p&uacute;blicas",
+
+'valider'=>"Aceptar",
+
+'voir_resultats'=>"Ver los resultados",
+'verif_web' => "comprobar la existencia del sitio Web",
+'voir_article'=>"Ver el art&iacute;culo",
 
 );
 
