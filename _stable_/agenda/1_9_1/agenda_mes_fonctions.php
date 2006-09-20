08295370386c7753e3c90737c3f347620f1e94b3
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-09-20 16:31:39 +0000

    bugs sur filtres manquants et chaines erronees
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@5583 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/agenda/1_9_1/agenda_mes_fonctions.php
+++ _stable_/agenda/1_9_1/agenda_mes_fonctions.php
@@ -4,4 +4,12 @@
 include_spip('public/criteres_agenda');
 include_spip('inc/agenda_filtres');
 
+function Agenda_heure_selector($date,$suffixe){
+	$d = strtotime($date);
+	$heure = date('H',$d);
+	$minute = date('i',$d);
+	return
+		afficher_heure($heure, "name='heure_evenement$suffixe' size='1' class='fondl'") .
+  	afficher_minute($minute, "name='minute_evenement$suffixe' size='1' class='fondl'");
+}
 ?>
