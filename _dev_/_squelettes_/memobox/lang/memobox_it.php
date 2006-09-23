69cc948308a35555f192b12171a97d700db7ac84
renato@rezo.net <renato@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-09-23 00:39:10 +0000

    added a configuration variable MEMOBOX_MODE (link or drag) to choose if the links are stored by dragging them to the memobox or clicking on a link
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@5625 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/_squelettes_/memobox/lang/memobox_it.php
+++ _dev_/_squelettes_/memobox/lang/memobox_it.php
@@ -4,8 +4,11 @@
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 'drag_drop' => "Clicca e trascina qui i titoli per memorizzarli",
+'link' => "Clicca sul link di fianco agli articoli per memorizzarli",
 'anchor_delete_title' => "elimina questo titolo",
-'anchor_delete_text' => "elimina"
+'anchor_delete_text' => "elimina",
+'anchor_add_title' => "memorizza questo link per ritrovarlo rapidamente in futuro",
+'anchor_add_text' => "memorizza il link"
 );
 
 
