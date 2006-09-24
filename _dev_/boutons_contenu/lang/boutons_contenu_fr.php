e85e4c4114aaf658cb544ba181e7474609930081
bertrand@toggg.com <bertrand@toggg.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-09-24 13:03:23 +0000

    Un début de traduction pour augmenter/diminuer police
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@5643 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _dev_/boutons_contenu/lang/boutons_contenu_fr.php
@@ -0,0 +1,9 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+'augmenter_police' => 'Augmenter police',
+'diminuer_police' => 'Diminuer police'
+);
+?>
