f6fcb74dac22297360ec93fbe24096907f00cad4
bertrand@toggg.com <bertrand@toggg.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-09-25 00:38:52 +0000

    On bouge , il en manque ...
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@5661 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _test_/boutonstexte/head.php
@@ -0,0 +1,28 @@
+<?php
+// insert le css et le js externes pour boutonstexte dans le <head> du document (#INSERT_HEAD)
+function boutonstexte_insert_head($flux)
+{
+	$jsFile = find_in_path('boutonstexte.js');
+	$cssFile = find_in_path('boutonstexte.css');
+	$imgPath = dirname(find_in_path('img/fontsizeup.png'));
+
+	$txtOnly = _T('boutonstexte:texte_seulement');
+	$txtSizeUp = _T('boutonstexte:augmenter_police');
+	$txtSizeDown = _T('boutonstexte:diminuer_police');
+
+	$incHead = <<<EOH
+<link rel="stylesheet" href="$cssFile" type="text/css" media="all" />
+<script src="{$jsFile}" type="text/javascript"></script>
+<script type="text/javascript">
+	boutonstexte = new boutonsTexte({
+		'imgPath':'{$imgPath}',
+		'txtOnly':'{$txtOnly}',
+		'txtSizeUp':'{$txtSizeUp}',
+		'txtSizeDown':'{$txtSizeDown}'
+	});
+</script >
+EOH;
+
+	return preg_replace('#(</head>)?$#i', $incHead . "\$1\n", $flux, 1);
+}
+?>
