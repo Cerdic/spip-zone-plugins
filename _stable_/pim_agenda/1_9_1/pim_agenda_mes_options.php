1f31f06604b37b813e343c32c1a1d2dc84ea75e7
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-09-25 11:15:01 +0000

    version pour SPIP 1.9.1
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@5669 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _stable_/pim_agenda/1_9_1/pim_agenda_mes_options.php
@@ -0,0 +1,7 @@
+<?php
+$p=explode(basename(_DIR_PLUGINS)."/",str_replace('\\','/',realpath(dirname(__FILE__))));
+define('_DIR_PLUGIN_PIM_AGENDA',(_DIR_PLUGINS.end($p)));
+include_spip('base/pim_agenda');
+
+
+?>
