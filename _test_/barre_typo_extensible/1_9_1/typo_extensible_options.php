d308991e68d00a00a178859cf3fcb645d866d5e0
bill@adequates.com <bill@adequates.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-09-28 09:51:42 +0000

    bug retour chariot
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@5749 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/barre_typo_extensible/1_9_1/typo_extensible_options.php
+++ _test_/barre_typo_extensible/1_9_1/typo_extensible_options.php
@@ -9,18 +9,18 @@ $GLOBALS['spip_pipeline']['BarreTypoEnrichie_tous'] = '';
 $GLOBALS['spip_pipeline']['BarreTypoEnrichie_avancees'] = ''; 
 $GLOBALS['spip_pipeline']['BarreTypoEnrichie_forum'] = ''; 
 include_spip('inc/texte');
-tester_variable('debut_intertitre_1', '\n<h1 class="spip">');
-tester_variable('fin_intertitre_1', '</h1>\n');
-tester_variable('debut_intertitre_2', '\n<h2 class="spip">');
-tester_variable('fin_intertitre_2', '</h2>\n');
-tester_variable('debut_intertitre_3', '\n<h3 class="spip">');
-tester_variable('fin_intertitre_3', '</h3>\n');
-tester_variable('debut_intertitre_4', '\n<h4 class="spip">');
-tester_variable('fin_intertitre_4', '</h4>\n');
-tester_variable('debut_intertitre_5', '\n<h5 class="spip">');
-tester_variable('fin_intertitre_5', '</h5>\n');
-tester_variable('debut_intertitre_6', '\n<h6 class="spip">');
-tester_variable('fin_intertitre_6', '</h6>\n');
+tester_variable('debut_intertitre_1', "\n<h1 class=\"spip\">");
+tester_variable('fin_intertitre_1', "</h1>");
+tester_variable('debut_intertitre_2', "\n<h2 class=\"spip\">");
+tester_variable('fin_intertitre_2', "</h2>");
+tester_variable('debut_intertitre_3', "\n<h3 class=\"spip\">");
+tester_variable('fin_intertitre_3', "</h3>");
+tester_variable('debut_intertitre_4', "\n<h4 class=\"spip\">");
+tester_variable('fin_intertitre_4', "</h4>");
+tester_variable('debut_intertitre_5', "\n<h5 class=\"spip\">");
+tester_variable('fin_intertitre_5', "</h5>");
+tester_variable('debut_intertitre_6', "\n<h6 class=\"spip\">");
+tester_variable('fin_intertitre_6', "</h6>");
 
 
 ?>
