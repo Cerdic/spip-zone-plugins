81e6914d1da881b8d6a5dda678c350d6bd9115e4
bill@adequates.com <bill@adequates.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-09-28 09:51:30 +0000

    bug retour chariot
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@5748 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/pagination_article3/1_9/paginart3_options.php
+++ _test_/pagination_article3/1_9/paginart3_options.php
@@ -2,6 +2,6 @@
 global $table_des_traitements;
 include_spip('inc/texte');
 $table_des_traitements['TEXTE'][]= 'paginart3_paginer(propre(%s))';
-tester_variable('debut_intertitre_p', '<h3 class="paginart3">');
-tester_variable('fin_intertitre_p', '</h3>');
+tester_variable('debut_intertitre_p', "\n<h3 class=\"paginart3\">");
+tester_variable('fin_intertitre_p', "</h3>\n");
 ?>
