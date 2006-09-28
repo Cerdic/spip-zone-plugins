fbf5f41d48aa0e81e5a133c26fd65091dcb5ebd2
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-09-28 17:53:27 +0000

    Rajout dans la prévisualisation de l'affichage des notes de bas de page.
    a nécessité de rajouter :
    include_spip("public/composer"); // Pour pouvoir generer les notes de bas de page (pourquoi c'est pas dans inc/texte ?)
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@5764 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/barre_typo_enrichie/svn/exec/article_preview.php
+++ _stable_/barre_typo_enrichie/svn/exec/article_preview.php
@@ -1,8 +1,9 @@
 <?php
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
-include_spip("inc/meta");
-include_spip("inc/texte");
+include_spip("inc/meta"); // Pour avoir le charset du site
+include_spip("inc/texte"); // Pour pouvoir utiliser propre
+include_spip("public/composer"); // Pour pouvoir generer les notes de bas de page (pourquoi c'est pas dans inc/texte ?)
 
 function exec_article_preview_dist()
 {
@@ -10,5 +11,7 @@ function exec_article_preview_dist()
 	header('Content-Type: text/html;charset='.lire_meta('charset'));
 	echo "<meta http-equiv='Content-Type' content='text/html; charset=".lire_meta('charset')." />";
 	echo propre($_POST['texte']);
+	echo '<hr />';
+	echo calculer_notes();
 }
 ?>
