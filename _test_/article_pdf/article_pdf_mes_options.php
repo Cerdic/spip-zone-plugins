ecd85266e3d130e60f08d72b43b67ec9b56b6c87
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-10-01 17:00:21 +0000

    la balise #ARTICLE_PDF est generee par le modele article_pdf personalisable On revient a juste une icone, sans texte par defaut. La chaine de langue du alt et title est internationalisée
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@5817 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/article_pdf/article_pdf_mes_options.php
+++ _test_/article_pdf/article_pdf_mes_options.php
@@ -1,46 +1,32 @@
 <?php
-
-	function article_pdf_insertion_racourci($arg){
-		$icone = find_in_path('img_pack/article_pdf.png');
-		$url = generer_url_public('article_pdf',$arg);
-		$code = "<a href='$url' title='Enregistrer au format PDF'><img src='$icone' width='24' height='24' alt='Creer un PDF' />Enregistrer au format PDF</a>";
-		return $code;
-	}
 	function balise_ARTICLE_PDF_dist($p) {
-		if ($p->param && !$p->param[0][0]){
-			$quoi =  $p->param[0][1];
-			$nom = ($quoi[0]->type=='texte') ? $quoi[0]->texte : "";
-		}
-	
-		if (!$nom) {
-			// pas de parametre en argument
-			// on cherche dans le contexte d'abord id_article, puis sinon id_rubrique
-			$_id_article = champ_sql('id_article', $p);
-			$arg = "'id_article='.".$_id_article;
-			if (!$_id_article){
-				$_id_rubrique = champ_sql('id_rubrique', $p);
-				$arg = "'$nom='.".$_id_rubrique;
+		if (!is_array($p->param))
+			$p->param=array();
+		
+		// Produire le premier argument {article_pdf}
+		$texte = new Texte;
+		$texte->type='texte';
+		$texte->texte='article_pdf';
+		$param = array(0=>NULL, 1=>array(0=>$texte));
+		array_unshift($p->param, $param);
+		
+		// Transformer les filtres en arguments
+		for ($i=1; $i<count($p->param); $i++) {
+			if ($p->param[$i][0]) {
+				if (!strstr($p->param[$i][0], '='))
+					break;# on a rencontre un vrai filtre, c'est fini
+				$texte = new Texte;
+				$texte->type='texte';
+				$texte->texte=$p->param[$i][0];
+				$param = array(0=>$texte);
+				$p->param[$i][1] = $param;
+				$p->param[$i][0] = NULL;
 			}
 		}
-		else {		
-			if ($nom=='id_article'){
-				$_id_article = champ_sql('id_article', $p);
-				$arg = "'$nom='.".$_id_article;
-			}
-			else if ($nom=='id_rubrique'){
-				$_id_rubrique = champ_sql('id_rubrique', $p);
-				$arg = "'$nom='.".$_id_rubrique;
-			}
-			else 
-				$arg="'$nom'";
-		}
-		$icone = find_in_path('img_pack/article_pdf.png');
-		$url = generer_url_public('article_pdf',$arg);
-		if ($arg)
-		$p->code = "article_pdf_insertion_racourci($arg)";
-	
-		#$p->interdire_scripts = true;
-	
-		return $p;
-	}	
+		
+		// Appeler la balise #MODELE{article_pdf}{arguments}
+		if (!function_exists($f = 'balise_modele'))
+			$f = 'balise_modele_dist';
+		return $f($p);
+	}
 ?>
