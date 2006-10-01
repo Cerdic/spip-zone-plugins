6bffc5960561c45ca01a485e50ad0da17df0bbd5
fil@rezo.net <fil@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-10-01 16:02:20 +0000

    nettoyages et deplacement du projet widgets
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@5811 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _dev_/widgets/widgets.php
@@ -0,0 +1,99 @@
+<?php
+
+
+// Recuperation des donnees
+#var_dump($_POST);
+
+#$meta['alea_ephemere'] = $meta['alea_ephemere_ancien'] = 'x';
+
+
+if (isset($_POST['widgets']) AND is_array($_POST['widgets']))
+foreach ($_POST['widgets'] as $widget) {
+
+	$name = $_POST['name_'.$widget];
+	$content = $_POST['content_'.$widget];
+
+	// Si les donnees POSTees ne correspondent pas a leur md5,
+	// il faut les traiter
+	if (md5($_POST['content_'.$widget]) <> $_POST['md5_'.$widget]) {
+
+		if (!isset($_POST['secu_'.$widget]))
+			echo "<br/>widget $name non securise =&gt; $content\n";
+
+		elseif (verif_secu($name, $_POST['secu_'.$widget]))
+			echo "<br/>widget $name securise =&gt; $content\n";
+		else
+			echo "<br/>widget $name erreur securite =&gt; $content\n";
+	} else
+		echo "<br/>widget $name inchange =&gt; $content\n";
+
+}
+
+
+function verif_secu($w, $secu) {
+	return (
+		$secu == md5($GLOBALS['meta']['alea_ephemere'].'='.$w)
+	OR
+		$secu == md5($GLOBALS['meta']['alea_ephemere_ancien'].'='.$w)
+	);
+}
+
+
+// Definition des widgets
+class Widget {
+	var $name;
+	var $text;
+	var $key;
+	var $md5;
+	var $clean;
+
+	function Widget($name, $text='') {
+		$this->name = $name;
+		$this->text = $text;
+		$this->key = strtr(uniqid('wid', true), '.', '_');
+	}
+
+	function md5() {
+		return md5($this->text);
+	}
+
+	function code() {
+		return
+		'<input type="hidden" name="widgets[]" value="'.$this->key.'" />'."\n"
+		. '<input type="hidden" name="name_'.$this->key.'" value="'.$this->name.'" />'."\n"
+		. '<input type="hidden" name="md5_'.$this->key
+		.'" value="'.$this->md5().'" />'."\n";
+	}
+
+	function input($type = 'ligne') {
+		switch ($type) {
+			case 'ligne':
+			default:
+				return '<input type="text" width="80"'
+				. ' name="content_'.$this->key.'" value="'
+				. htmlspecialchars($this->text)   # entites_html
+				. '" />'."\n";
+		}
+	}
+}
+
+//
+// Un Widget avec une verification de code de securite
+//
+class SecureWidget extends Widget {
+
+	function SecureWidget($name, $text='') {
+		parent::Widget($name, $text);
+	}
+
+	function code() {
+		$code = parent::code();
+		$secu = md5($GLOBALS['meta']['alea_ephemere']. '=' . $this->name);
+
+		return
+			$code
+			.'<input type="hidden" name="secu_'.$this->key.'" value="'.$secu.'" />'."\n";
+	}
+}
+
+?>
