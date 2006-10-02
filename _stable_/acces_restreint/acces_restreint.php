fa08255f484277439131763baf82f667f58f0722
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-10-02 18:43:29 +0000

    le critere {tout_voir} permet de passer outre les restrictions d'acces codé pour 1.9.2r7520, mais semble ok pour 1.9 et 1.9.1 a confirmer... Testeurs, c'est a vous !
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@5841 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/acces_restreint/acces_restreint.php
+++ _stable_/acces_restreint/acces_restreint.php
@@ -1,6 +1,12 @@
 <?php
 include_spip('inc/acces_restreint');
 
+// {tout_voir}
+function critere_tout_voir_dist($idb, &$boucles, $crit) {
+	$boucle = &$boucles[$idb];
+	$boucle->modificateur['tout_voir'] = true;
+}
+
 //
 // <BOUCLE(ARTICLES)>
 //
@@ -8,17 +14,19 @@ function boucle_ARTICLES($id_boucle, &$boucles) {
 	$boucle = &$boucles[$id_boucle];
 	$id_table = $boucle->id_table;
 
-	$t = $boucle->id_table . '.' . $boucle->primary;
-	if (!in_array($t, $boucles[$id_boucle]->select))
-	  $boucle->select[]= $t; # pour postgres, neuneu ici
-
-	$boucle->hash = '
-	// ACCES RESTREINT
-	$acces_where = AccesRestreint_articles_accessibles_where("'.$t.'");
-	' . $boucle->hash ;
-
-	// et le filtrage d'acces filtre !
-	$boucle->where[] = '$acces_where';
+	if (!isset($boucle->modificateur['tout_voir'])){
+		$t = $boucle->id_table . '.' . $boucle->primary;
+		if (!in_array($t, $boucles[$id_boucle]->select))
+		  $boucle->select[]= $t; # pour postgres, neuneu ici
+	
+		$boucle->hash = '
+		// ACCES RESTREINT
+		$acces_where = AccesRestreint_articles_accessibles_where("'.$t.'");
+		' . $boucle->hash ;
+	
+		// et le filtrage d'acces filtre !
+		$boucle->where[] = '$acces_where';
+	}
 
 	return boucle_ARTICLES_dist($id_boucle, $boucles);
 }
@@ -30,18 +38,20 @@ function boucle_BREVES($id_boucle, &$boucles) {
 	$boucle = &$boucles[$id_boucle];
 	$id_table = $boucle->id_table;
 
-	$t = $boucle->id_table . '.' . $boucle->primary;
-	if (!in_array($t, $boucles[$id_boucle]->select))
-	  $boucle->select[]= $t; # pour postgres, neuneu ici
-
-	$boucle->hash = '
-	// ACCES RESTREINT
-	$acces_where = AccesRestreint_breves_accessibles_where("'.$t.'");
-	' . $boucle->hash ;
-
-	// et le filtrage d'acces filtre !
-	$boucle->where[] = '$acces_where';
-
+	if (!isset($boucle->modificateur['tout_voir'])){
+		$t = $boucle->id_table . '.' . $boucle->primary;
+		if (!in_array($t, $boucles[$id_boucle]->select))
+		  $boucle->select[]= $t; # pour postgres, neuneu ici
+	
+		$boucle->hash = '
+		// ACCES RESTREINT
+		$acces_where = AccesRestreint_breves_accessibles_where("'.$t.'");
+		' . $boucle->hash ;
+	
+		// et le filtrage d'acces filtre !
+		$boucle->where[] = '$acces_where';
+	}
+	
 	return boucle_BREVES_dist($id_boucle, $boucles);
 }
 
@@ -51,18 +61,19 @@ function boucle_BREVES($id_boucle, &$boucles) {
 function boucle_FORUMS($id_boucle, &$boucles) {
 	$boucle = &$boucles[$id_boucle];
 	$id_table = $boucle->id_table;
-	$t = $boucle->id_table . '.' . $boucle->primary;
-	if (!in_array($t, $boucles[$id_boucle]->select))
-	  $boucle->select[]= $t; # pour postgres, neuneu ici
-
-	$boucle->hash = '
-	// ACCES RESTREINT
-	$acces_where = AccesRestreint_forum_accessibles_where("'.$t.'");
-	' . $boucle->hash ;
-
-	// et le filtrage d'acces filtre !
-	$boucle->where[] = '$acces_where';
-
+	if (!isset($boucle->modificateur['tout_voir'])){
+		$t = $boucle->id_table . '.' . $boucle->primary;
+		if (!in_array($t, $boucles[$id_boucle]->select))
+		  $boucle->select[]= $t; # pour postgres, neuneu ici
+	
+		$boucle->hash = '
+		// ACCES RESTREINT
+		$acces_where = AccesRestreint_forum_accessibles_where("'.$t.'");
+		' . $boucle->hash ;
+	
+		// et le filtrage d'acces filtre !
+		$boucle->where[] = '$acces_where';
+	}
 	return boucle_FORUMS_dist($id_boucle, $boucles);
 }
 
@@ -72,18 +83,19 @@ function boucle_FORUMS($id_boucle, &$boucles) {
 function boucle_SIGNATURES($id_boucle, &$boucles) {
 	$boucle = &$boucles[$id_boucle];
 	$id_table = $boucle->id_table;
-	$t = $boucle->id_table . '.' . $boucle->primary;
-	if (!in_array($t, $boucles[$id_boucle]->select))
-	  $boucle->select[]= $t; # pour postgres, neuneu ici
-
-	$boucle->hash = '
-	// ACCES RESTREINT
-	$acces_where = AccesRestreint_signatures_accessibles_where("'.$t.'");
-	' . $boucle->hash ;
-
-	// et le filtrage d'acces filtre !
-	$boucle->where[] = '$acces_where';
-
+	if (!isset($boucle->modificateur['tout_voir'])){
+		$t = $boucle->id_table . '.' . $boucle->primary;
+		if (!in_array($t, $boucles[$id_boucle]->select))
+		  $boucle->select[]= $t; # pour postgres, neuneu ici
+	
+		$boucle->hash = '
+		// ACCES RESTREINT
+		$acces_where = AccesRestreint_signatures_accessibles_where("'.$t.'");
+		' . $boucle->hash ;
+	
+		// et le filtrage d'acces filtre !
+		$boucle->where[] = '$acces_where';
+	}
 	return boucle_SIGNATURES_dist($id_boucle, $boucles);
 }
 
@@ -93,18 +105,19 @@ function boucle_SIGNATURES($id_boucle, &$boucles) {
 function boucle_DOCUMENTS($id_boucle, &$boucles) {
 	$boucle = &$boucles[$id_boucle];
 	$id_table = $boucle->id_table;
-	$t = $boucle->id_table . '.' . $boucle->primary;
-	if (!in_array($t, $boucles[$id_boucle]->select))
-	  $boucle->select[]= $t; # pour postgres, neuneu ici
-
-	$boucle->hash = '
-	// ACCES RESTREINT
-	$acces_where = AccesRestreint_documents_accessibles_where("'.$t.'");
-	' . $boucle->hash ;
-
-	// et le filtrage d'acces filtre !
-	$boucle->where[] = '$acces_where';
-
+	if (!isset($boucle->modificateur['tout_voir'])){
+		$t = $boucle->id_table . '.' . $boucle->primary;
+		if (!in_array($t, $boucles[$id_boucle]->select))
+		  $boucle->select[]= $t; # pour postgres, neuneu ici
+	
+		$boucle->hash = '
+		// ACCES RESTREINT
+		$acces_where = AccesRestreint_documents_accessibles_where("'.$t.'");
+		' . $boucle->hash ;
+	
+		// et le filtrage d'acces filtre !
+		$boucle->where[] = '$acces_where';
+	}
 	return boucle_DOCUMENTS_dist($id_boucle, $boucles);
 }
 
@@ -114,18 +127,18 @@ function boucle_DOCUMENTS($id_boucle, &$boucles) {
 function boucle_RUBRIQUES($id_boucle, &$boucles) {
 	$boucle = &$boucles[$id_boucle];
 	$id_table = $boucle->id_table;
-
-	$t = $boucle->id_table . '.' . $boucle->primary;
-	if (!in_array($t, $boucles[$id_boucle]->select))
-	  $boucle->select[]= $t; # pour postgres, neuneu ici
-	$boucle->hash = '
-	// ACCES RESTREINT
-	$acces_where = AccesRestreint_rubriques_accessibles_where("'.$t.'");
-	' . $boucle->hash ;
-
-	// et le filtrage d'acces filtre !
-	$boucle->where[] = '$acces_where';
-
+	if (!isset($boucle->modificateur['tout_voir'])){
+		$t = $boucle->id_table . '.' . $boucle->primary;
+		if (!in_array($t, $boucles[$id_boucle]->select))
+		  $boucle->select[]= $t; # pour postgres, neuneu ici
+		$boucle->hash = '
+		// ACCES RESTREINT
+		$acces_where = AccesRestreint_rubriques_accessibles_where("'.$t.'");
+		' . $boucle->hash ;
+	
+		// et le filtrage d'acces filtre !
+		$boucle->where[] = '$acces_where';
+	}
 	return boucle_RUBRIQUES_dist($id_boucle, $boucles);
 }
 
@@ -135,19 +148,19 @@ function boucle_RUBRIQUES($id_boucle, &$boucles) {
 function boucle_HIERARCHIE($id_boucle, &$boucles) {
 	$boucle = &$boucles[$id_boucle];
 	$id_table = $boucle->id_table;
-
-	$t = $boucle->id_table . '.' . $boucle->primary;
-	if (!in_array($t, $boucles[$id_boucle]->select))
-	  $boucle->select[]= $t; # pour postgres, neuneu ici
-
-	$boucle->hash = '
-	// ACCES RESTREINT
-	$acces_where = AccesRestreint_rubriques_accessibles_where("'.$t.'");
-	' . $boucle->hash ;
-
-	// et le filtrage d'acces filtre !
-	$boucle->where[] = '$acces_where';
-
+	if (!isset($boucle->modificateur['tout_voir'])){
+		$t = $boucle->id_table . '.' . $boucle->primary;
+		if (!in_array($t, $boucles[$id_boucle]->select))
+		  $boucle->select[]= $t; # pour postgres, neuneu ici
+	
+		$boucle->hash = '
+		// ACCES RESTREINT
+		$acces_where = AccesRestreint_rubriques_accessibles_where("'.$t.'");
+		' . $boucle->hash ;
+	
+		// et le filtrage d'acces filtre !
+		$boucle->where[] = '$acces_where';
+	}
 	return boucle_HIERARCHIE_dist($id_boucle, $boucles);
 }
 
@@ -157,18 +170,19 @@ function boucle_HIERARCHIE($id_boucle, &$boucles) {
 function boucle_SYNDICATION($id_boucle, &$boucles) {
 	$boucle = &$boucles[$id_boucle];
 	$id_table = $boucle->id_table;
-	$t = $boucle->id_table . '.' . $boucle->primary;
-	if (!in_array($t, $boucles[$id_boucle]->select))
-	  $boucle->select[]= $t; # pour postgres, neuneu ici
-
-	$boucle->hash = '
-	// ACCES RESTREINT
-	$acces_where = AccesRestreint_syndic_accessibles_where("'.$t.'");
-	' . $boucle->hash ;
-
-	// et le filtrage d'acces filtre !
-	$boucle->where[] = '$acces_where';
-
+	if (!isset($boucle->modificateur['tout_voir'])){
+		$t = $boucle->id_table . '.' . $boucle->primary;
+		if (!in_array($t, $boucles[$id_boucle]->select))
+		  $boucle->select[]= $t; # pour postgres, neuneu ici
+	
+		$boucle->hash = '
+		// ACCES RESTREINT
+		$acces_where = AccesRestreint_syndic_accessibles_where("'.$t.'");
+		' . $boucle->hash ;
+	
+		// et le filtrage d'acces filtre !
+		$boucle->where[] = '$acces_where';
+	}
 	return boucle_SYNDICATION_dist($id_boucle, $boucles);
 }
 
@@ -178,18 +192,19 @@ function boucle_SYNDICATION($id_boucle, &$boucles) {
 function boucle_SYNDIC_ARTICLES($id_boucle, &$boucles) {
 	$boucle = &$boucles[$id_boucle];
 	$id_table = $boucle->id_table;
-	$t = $boucle->id_table . '.' . $boucle->primary;
-	if (!in_array($t, $boucles[$id_boucle]->select))
-	  $boucle->select[]= $t; # pour postgres, neuneu ici
-
-	$boucle->hash = '
-	// ACCES RESTREINT
-	$acces_where = AccesRestreint_syndic_articles_accessibles_where("'.$t.'");
-	' . $boucle->hash ;
-
-	// et le filtrage d'acces filtre !
-	$boucle->where[] = '$acces_where';
-
+	if (!isset($boucle->modificateur['tout_voir'])){
+		$t = $boucle->id_table . '.' . $boucle->primary;
+		if (!in_array($t, $boucles[$id_boucle]->select))
+		  $boucle->select[]= $t; # pour postgres, neuneu ici
+	
+		$boucle->hash = '
+		// ACCES RESTREINT
+		$acces_where = AccesRestreint_syndic_articles_accessibles_where("'.$t.'");
+		' . $boucle->hash ;
+	
+		// et le filtrage d'acces filtre !
+		$boucle->where[] = '$acces_where';
+	}
 	return boucle_SYNDIC_ARTICLES_dist($id_boucle, $boucles);
 }
 
