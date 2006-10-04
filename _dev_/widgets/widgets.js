a2fa84b5290ecdaa8d2ddbd11696ced5ec5cb10b
fil@rezo.net <fil@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-10-04 21:45:59 +0000

    supprimer la 'trap' du widget (??)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@5888 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/widgets/widgets.js
+++ _dev_/widgets/widgets.js
@@ -3,7 +3,7 @@ url_widgets_html = 'spip.php?action=widgets_html&class=';
 
 $.setupwidget = function(e){
     var me = this;
-    var me_orig = me.innerHTML;
+
     // reglages de taille mini/maxi; pas tres beau
     var w,h;
     w = $(me).width()-50; // 50 = largeur du bouton "ok"
@@ -15,15 +15,14 @@ $.setupwidget = function(e){
     // charger le formulaire
     $.get(url_widgets_html+encodeURIComponent(this.className),
        function (c) {
+         var me_orig = $(me).html();
          $(me)
          .unclick()
-         .click(function(e){e.stopPropagation();}) //avoid cancelling on click
          .html(c)
          .find('form')
            .ajaxForm(function(c){
              $(me)
              .html(c.responseText)
-             .unclick() //remove the trap to avoid cancel onclick
              .click($.setupwidget); // recursif
            })
            .find(".widget-active")
@@ -35,29 +34,24 @@ $.setupwidget = function(e){
                'fontFamily': $(me).css('fontFamily')
              })
              .each(function(){this.focus();})
+             .keypress(function(e){
+               if (e.keyCode == 27) {
+                 $(me).html(me_orig).click($.setupwidget);
+               }
+             })
            .end()
            .find(".cancel_widget")
-          	 .click(cancel_widget)	
+             .click(function(){
+               $(me)
+               .html(me_orig) //restore original html
+               .click($.setupwidget);
+               return false;
+             })
            .end()
          .end()
          ;
-         $("html").keypress(exit_on_esc).click(cancel_widget);
        }
      );
-     
-    function cancel_widget() {
-      $(me).html(me_orig) //restore original html
-      .unclick() //remove the trap to avoid cancel onclick
-      .click($.setupwidget);
-      return false;
-    }
-    function exit_on_esc(e) {
-      if(e.keyCode==27) {
-        $("html").unkeypress(exit_on_esc);
-        return cancel_widget();
-      }
-    }
-    e.stopPropagation(); //do not cancel widgets when creating another one
   }
 
 $(function() {
