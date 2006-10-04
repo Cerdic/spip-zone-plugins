097101c2d55a218f3364e081f1e05316b538d1fd
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-10-04 22:23:53 +0000

    Permettre l'optimisation du temps de calcul en inserant #PIPELINE{insert_body} dans le body du squelette. L'affichage du marqueur via le pipeline affichage_final est desactivé lorsque l'on rencontre le pipeline insert_body pour la premiere fois. A defaut, continue fonctionner comme avant
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@5890 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/phpmyvisites/phpmv_fonctions.php
+++ _stable_/phpmyvisites/phpmv_fonctions.php
@@ -1,68 +1,47 @@
 <?php
 
-/*function phpmv_header_prive($flux) {
-	$exec = _request('exec');
-	// les CSS
-	if ($exec == 'phpmv'){
-		$flux .=  '<link href="'.DIR_PLUGIN_PHPMV.'/themes/default/css/{if $styleCommon}{$styleCommon}{else}interfaceCommon{/if}.css" rel="stylesheet" type="text/css" />	
-	<link href="{$DIR_PLUGIN_PHPMV}/themes/default/css/styles.php?dir={'text_dir'|translate}" rel="stylesheet" type="text/css" />	
-	<script type="text/javascript" src="{$DIR_PLUGIN_PHPMV}/themes/default/include/menu.js"></script>
-	<script type="text/javascript" src="{$DIR_PLUGIN_PHPMV}/themes/default/include/misc.js"></script>
-	<link rel="alternate" type="application/rss+xml" title="RSS" href="./?exec=phpmv&mod=view_rss&amp;rss_hash={$rss_hash}" />
-		$flux .= '<link rel="stylesheet" href="' ._DIR_PLUGIN_AGENDA_EVENEMENTS . '/img_pack/calendrier.css" type="text/css" />'. "\n";
-		$flux .= '<link rel="stylesheet" href="' ._DIR_PLUGIN_AGENDA_EVENEMENTS . '/img_pack/agenda.css" type="text/css" />'. "\n";
-	}
-	return $flux;
-}*/
-
-
-function phpmv_affichage_final($texte){
-	//$html= preg_match(',^\s*text/html,',$page['entetes']['Content-Type']);
-	global $html;
-	if ($html){
-		if (!isset($GLOBALS['meta']['_PHPMV_DIR_CONFIG']) || !strlen($GLOBALS['meta']['_PHPMV_DIR_CONFIG'])){
-			include_spip("inc/meta");
-			ecrire_meta('_PHPMV_DIR_CONFIG',realpath(_DIR_SESSIONS . "phpmvconfig"));
-			ecrire_meta('_PHPMV_DIR_DATA',realpath(_DIR_SESSIONS . "phpmvdatas"));
-			ecrire_metas();
-		}
-		$p=explode(basename(_DIR_PLUGINS)."/",str_replace('\\','/',realpath(dirname(__FILE__))));
-		$p = _DIR_PLUGINS.end($p);
-		if (!isset($GLOBALS['meta']['_DIR_PLUGIN_PHPMV']) OR $GLOBALS['meta']['_DIR_PLUGIN_PHPMV']!=$p){
-			include_spip("inc/meta");
-			ecrire_meta('_DIR_PLUGIN_PHPMV',$p);
-			ecrire_metas();
-		}
-
-		//define(PHPMV_URL,'@@');
-		$i_site = 1;
-		
-		$code = '<!-- phpmyvisites -->
-				<div style="display:none;">
-				<script type="text/javascript">
-				<!--
-				var a_vars = Array();
-				var pagename=\'\';
-				
-				var phpmyvisitesSite = '.$i_site.';
-				var phpmyvisitesURL = "'.($url = generer_url_public('phpmyvisites','var_nophpmv=1',true)).'";
-				//-->
-				</script>
-				<script src="'.url_de_base().find_in_path('spip_phpmyvisites.js').'" type="text/javascript"></script>
-				<noscript>
-				<img src="'.generer_url_public('phpmyvisites','var_nophpmv=1',false).'" alt="phpMyVisites" class="phpmyvisitestag" />
-				</noscript>
-				</div>
-				<!-- /phpmyvisites -->';
-		
-		if (!isset($_GET['var_nophpmv'])){
-			if (strpos($texte,"</body>")!==FALSE)
-				$texte=str_replace("</body>","$code\n</body>",$texte);
-			else
-				$texte=str_replace("</BODY>","$code\n</body>",$texte);
-		}
-	}
-	return $texte;
-	
+if (!isset($GLOBALS['meta']['_PHPMV_DIR_CONFIG']) || !strlen($GLOBALS['meta']['_PHPMV_DIR_CONFIG'])){
+	include_spip("inc/meta");
+	ecrire_meta('_PHPMV_DIR_CONFIG',realpath(_DIR_SESSIONS . "phpmvconfig"));
+	ecrire_meta('_PHPMV_DIR_DATA',realpath(_DIR_SESSIONS . "phpmvdatas"));
+	ecrire_metas();
 }
+$p=explode(basename(_DIR_PLUGINS)."/",str_replace('\\','/',realpath(dirname(__FILE__))));
+$p = _DIR_PLUGINS.end($p);
+if (!isset($GLOBALS['meta']['_DIR_PLUGIN_PHPMV']) OR $GLOBALS['meta']['_DIR_PLUGIN_PHPMV']!=$p){
+	include_spip("inc/meta");
+	ecrire_meta('_DIR_PLUGIN_PHPMV',$p);
+	ecrire_metas();
+}
+
+function phpmv_get_code(){
+	$i_site = 1;
+	return '<!-- phpmyvisites -->
+			<div style="display:none;">
+			<script type="text/javascript">
+			<!--
+			var a_vars = Array();
+			var pagename=\'\';
+			
+			var phpmyvisitesSite = '.$i_site.';
+			var phpmyvisitesURL = "'.($url = generer_url_public('phpmyvisites','var_nophpmv=1',true)).'";
+			//-->
+			</script>
+			<script src="'.url_de_base().find_in_path('spip_phpmyvisites.js').'" type="text/javascript"></script>
+			<noscript>
+			<img src="'.generer_url_public('phpmyvisites','var_nophpmv=1',false).'" alt="phpMyVisites" class="phpmyvisitestag" />
+			</noscript>
+			</div>
+			<!-- /phpmyvisites -->';	
+}
+
+function phpmv_insert_body($texte){
+	if (!isset($GLOBALS['meta']['phpmv_flag_insert_body'])){
+		include_spip("inc/meta");
+		ecrire_meta('phpmv_flag_insert_body','oui');
+		ecrire_metas();
+	}
+	return $texte.phpmv_get_code();
+}
+
 ?>
---
+++ _stable_/phpmyvisites/phpmv_options.php
@@ -0,0 +1,16 @@
+<?php
+
+$GLOBALS['spip_pipelines']['insert_body'] = '';
+function phpmv_affichage_final($texte){
+	global $html;
+	if (!isset($GLOBALS['meta']['phpmv_flag_insert_body'])
+		AND $html 
+		AND (!isset($_GET['var_nophpmv'])) 
+		AND (strpos($texte,'<!-- phpmyvisites -->')===FALSE)){
+		include_spip('phpmv_fonctions');
+		$code = phpmv_get_code();
+		$texte=preg_replace(",(</body>),i","$code\n</body>",$texte);
+	}
+	return $texte;
+}
+?>
