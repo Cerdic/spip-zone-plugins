5fb8e5d7c18465e9dc29c2589764460b74ab1a38
aurelien.levy@free.fr <aurelien.levy@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-10-06 21:45:23 +0000

    plugin V1 fonctionnel besoin d'aide pour debugger
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@5958 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/formulaire_personnalisation/personnalisation.php
+++ _dev_/formulaire_personnalisation/personnalisation.php
@@ -5,12 +5,6 @@ function personnalisation_insert_head($flux) {
 	if(isset($_COOKIE['spip_personnalisation_use']) && $_COOKIE['spip_personnalisation_use'] !='') { // si un cookie est posé
 		$flux .= '<link rel="stylesheet" type="text/css" media="screen" href="' ._DIR_PLUGIN_FORMULAIRE_PERSONNALISATION . '/css/zoomlayout.php" />'. "\n";
 	}
-	if(isset($_COOKIE['spip_personnalisation_navigationmode']) && $_COOKIE['spip_personnalisation_navigationmode'] =='autolink') {
-		$flux .= '<script src="' ._DIR_PLUGIN_FORMULAIRE_PERSONNALISATION . '/js/autolink.js" type="text/javascript"></script>'. "\n";
-	}else if(isset($_COOKIE['spip_personnalisation_navigationmode']) && $_COOKIE['spip_personnalisation_navigationmode'] =='autofocus') {
-		$flux .= '<script src="' ._DIR_PLUGIN_FORMULAIRE_PERSONNALISATION . '/js/autofocus.js" type="text/javascript"></script>'. "\n";
-	}
-
 	return $flux;
 }
 
