734d24366baecb7d63c92fe256753937ce5e06f8
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-10-08 18:09:23 +0000

    _DIR_.. pas defini en spip 1.9.1
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@6072 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/w3c_go_home/w3cgh_pipeline.php
+++ _stable_/w3c_go_home/w3cgh_pipeline.php
@@ -4,9 +4,13 @@
 		// si on est admin
 		if ($GLOBALS['connect_statut'] == "0minirezo" && $GLOBALS["connect_toutes_rubriques"]
 		AND $GLOBALS["options"]=="avancees") {
+			if (!defined(_DIR_PLUGIN_W3CGH))
+				$icone = find_in_path('/images/w3cgh-icone.gif');
+			else
+				$icone = _DIR_PLUGIN_W3CGH."/images/w3cgh-icone.gif";
 		  // on voit le bouton dans la barre "naviguer"
 			$boutons_admin['configuration']->sousmenu['w3c_go_home']= new Bouton(
-			_DIR_PLUGIN_W3CGH."/images/w3cgh-icone.gif",  // icone
+			$icone,  // icone
 			_L('Conformit&eacute;')	// titre
 			);
 		}
