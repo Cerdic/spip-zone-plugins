2ce04c5d9921588d84e5b0f3b32495f2ddce3a33
fil@rezo.net <fil@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-10-08 17:40:41 +0000

    tout est maintenant ecrit en plugin jQuery + retour du bleu en hover, et des crayons plus discrets
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@6070 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/widgets/widgets.js
+++ _dev_/widgets/widgets.js
@@ -1,97 +1,75 @@
-// TODO: closure
-url_widgets_html = 'spip.php?action=widgets_html';
-url_widgets_droits = 'spip.php?action=widgets_droits';
-SEARCHING = '<img class="widget-searching" src="dist/images/searching.gif" style="float:right;" />';
-ICONCLICK = "<img onclick='$.initwidget(this.parentNode, event);' style='float:right;border:0' src='dist/images/edit.gif' title='&Eacute;diter' />";
+/*
+ *  widgets.js (c) Fil 2006 -- licence GPL
+ */
 
-$.hidewidgets = function() {
-  $(".widget").each(function(){
-    $(this)
-    .hidewidget();
+// ouvre un widget
+$.fn.openwidget = function() {
+  return this
+  .each(function(){
+    // verifier que je suis un widget
+    if (!$(this).is('.widget'))
+      return;
+
+    // voir si je dispose deja du widget comme voisin
+    if ($(this).is('.has-widget')) {
+      $(this)
+      .hide()
+      .next()
+        .show();
+    }
+    // sinon charger le formulaire
+    else {
+      $(this)
+      .append(SEARCHING); // icone d'attente
+      var me=this;
+      $.get(url_widgets_html
+        + '&w=' + $(this).width()
+        + '&h=' + $(this).height()
+        + '&em=' + $(this).css('fontSize')
+        + '&class=' + encodeURIComponent(me.className)
+       ,
+        function (c) {
+          $(me)
+          .find("img.widget-searching")
+            .remove()
+          .end()
+          .hide()
+          .addClass('has-widget')
+          .next()
+            .html(c)
+            .show() // animate
+            .activatewidget();
+        }
+      );
+    }
   });
-}
-
-$.initallwidgets = function(e) {
-  $('.widget').each(function(){
-    $.initwidget(this);}
-  );
-  e.stopPropagation();
-}
-
-$.initwidget = function(me, e) {
-  // verifier que je suis un widget
-  if (!$(me).is('.widget'))
-    return;
-
-  if (e.stopPropagation)
-    e.stopPropagation();
-
-  // voir si je dispose deja du widget comme voisin
-  if ($(me).is('.has-widget')) {
-    $(me)
-    .hide()
-    .next()
-      .show();
-  }
-  // sinon charger le formulaire
-  else {
-    $(me)
-    .append(SEARCHING); // icone d'attente
-    $.get(url_widgets_html
-      + '&w=' + $(me).width()
-      + '&h=' + $(me).height()
-      + '&em=' + $(me).css('fontSize')
-      + '&class=' + encodeURIComponent(me.className)
-     ,
-      function (c) {
-        $(me)
-        .find("img.widget-searching")
-          .remove()
-        .end()
-        .hide()
-        .addClass('has-widget')
-        .next()
-          .html(c)
-          .show() // animate
-          .activatewidget();
-      }
-    );
-  }
-}
-
-$.clickwidget = function(e){
-  e.stopPropagation(); // avoid sending a global click to the body onclick
-  $.initwidget(this);
 }
 
 // annule le widget ouvert
 $.fn.cancelwidget = function() {
-  this.each(function(){
-    $(this)
-    .filter('.has-widget')
-    .show()
-    .removeClass('has-widget')
-    .next()
-      .html('')
-      .hide();
-  });
-  return this;
+  return this
+  .filter('.has-widget')
+  .show()
+  .removeClass('has-widget')
+  .next()
+    .html('')
+    .hide();
 }
+
 // masque le widget ouvert
 $.fn.hidewidget = function() {
-  this.each(function(){
-    $(this)
-    .filter('.has-widget')
-    .show()
-    .next()
-      .hide();
-  });
-  return this;
+  return this
+  .filter('.has-widget')
+  .show()
+  .next()
+    .hide()
+    .removeClass('widget-hover');
 }
 
-
+// active un widget qui vient d'etre charge
 $.fn.activatewidget = function() {
-  this.each(function(){
+  return this
+  .each(function(){
     var me = this;
     var w,h;
     $(me)
@@ -129,29 +107,70 @@ $.fn.activatewidget = function() {
         })
       .end()
       .find(".cancel_widget")
-        .click(function(){
+        .click(function(e){
+          e.stopPropagation();
           $(me)
           .prev()
-            .cancelwidget();
-          return false;
+          .cancelwidget();
         })
       .end()
       .find(".hide_widget")
-        .click(function(){
+        .click(function(e){
+          e.stopPropagation();
           $(me)
           .prev()
-            .hidewidget();
-          return false;
+          .hidewidget();
         })
       .end()
     .end();
   });
+}
+
+// initialise les widgets (cree le clone actif)
+$.fn.initwidget = function(){
+  this
+  .each(function(){
+    $(this)
+    .after(
+      this.cloneNode(true)
+    );
+  })
+  .next()
+    .hide()
+    .html('')
+    .removeAttr('id') // necessaire ??
+    .removeClass('widget')
+    .click(function(e){
+      e.stopPropagation();
+    })
+  .prev()
+  .prepend(ICONCLICK)
+  .click(function(e){
+    e.stopPropagation();
+  })
+  .dblclick(function(e){
+    e.stopPropagation();
+    $(this).openwidget();
+  });
+
+  // :hover pour MSIE
+  if (jQuery.browser.msie) {
+    this.hover(
+      function(){
+        $(this).addClass('widget-hover');
+      },function(){
+        $(this).removeClass('widget-hover');
+      }
+    );
+  }
+
   return this;
 }
 
-$(function() {
+// demarrage
+$(document).ready(function() {
 
-  // Aller chercher les droits a partir de la liste des classes
+  // Aller chercher les droits a partir de la liste des class
   var vus = '';
   $(".widget")
   .each(function(){
@@ -159,29 +178,30 @@ $(function() {
   });
 
   // Quand on recupere la liste des droits, on active les widgets autorises
+  url_widgets_droits = 'spip.php?action=widgets_droits';
   if (vus)
   $.post(url_widgets_droits, {'vus': vus},
     function(c) {
       c = c.split('|');
+      if (!c.length) return;
+
+      url_widgets_html = 'spip.php?action=widgets_html';
+      SEARCHING = '<img class="widget-searching" src="dist/images/searching.gif" style="float:right;" />';
+      ICONCLICK = "<img class='widget-edit' onclick='event.stopPropagation();$(this).parent().openwidget();' style='float:right;border:0' src='dist/images/edit.gif' title='&Eacute;diter' />";
+
       for (var i=0; i<c.length; i++) {
         $(".widget."+c[i])
-        .each(function(){
-          $(this)
-          .after(this.cloneNode(true))
-          .next()
-            .hide()
-            .html('')
-            .removeAttr('id') // necessaire ??
-            .removeClass('widget')
-            .removeClass(c[i])
-            .click($.clickwidget); // eviter qu'un clic n'annule le widget
-        })
-        .prepend(ICONCLICK)
-        .dblclick($.clickwidget);
-        $("html")
-        .click($.hidewidgets);
+        .initwidget();
       }
+
+      // fermer tous les widgets ouverts
+      $("html")
+      .click(function() {
+        $(".widget.has-widget:hidden")
+        .hidewidget();
+      });
+      $('head')
+      .prepend('<style>img.widget-edit {visibility: hidden;} .widget:hover img.widget-edit, .widget-hover img.widget-edit {visibility: visible;} .widget:hover, .widget-hover {background-color: #e3eeee;}</style>');
     }
   );
 });
-
