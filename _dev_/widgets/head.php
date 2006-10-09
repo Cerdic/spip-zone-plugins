7a8acaee0ee86a3d780426dd8de145703ff33de1
bertrand@toggg.com <bertrand@toggg.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-10-09 21:06:01 +0000

    Plus utlisé , devenu tetewidgets.php
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@6111 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/widgets/head.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function Widgets_insert_head($flux){
-	$js = '<script src="'.find_in_path('widgets.js').'" type="text/javascript"></script>';
-	$js .= '<style>img.widget-edit {visibility: hidden;} .widget:hover img.widget-edit, .widget-hover img.widget-edit {visibility: visible;} .widget:hover, .widget-hover {background-color: #e3eeee;}</style>';
-	return $flux.$js;
-}
-
-?>
