1ff683546deda4ecc1fb5a574b607dff633dd8fa
bertrand@toggg.com <bertrand@toggg.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-10-09 19:43:56 +0000

    Industrialisation (non encore en service)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@6108 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _dev_/widgets/lang/widgets_fr.php
@@ -0,0 +1,9 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+'editer' => '&Eacute;diter',
+'erreur_ou_interdit' => 'Erreur ou interdit !'
+);
+?>
---
+++ _dev_/widgets/tetewidgets.php
@@ -0,0 +1,33 @@
+<?php
+/* insert le css et le js externes pour Widgets dans le <head> (#INSERT_HEAD)
+ *
+ *  Widgets plugin for spip (c) Fil 2006 -- licence GPL
+ */
+
+function Widgets_insert_head($flux)
+{
+	$jsFile = find_in_path('widgets.js');
+	$cssFile = find_in_path('widgets.css');
+	$imgPath = dirname(find_in_path('images/edit.gif'));
+
+	$txtEditer = addslashes(unicode_to_javascript(html2unicode(_T(
+		'widgets:editer'))));
+	$txtErrInterdit = addslashes(unicode_to_javascript(html2unicode(_T(
+		'widgets:erreur_ou_interdit'))));
+
+	$incHead = <<<EOH
+
+<link rel="stylesheet" href="$cssFile" type="text/css" media="all" />
+<script src="{$jsFile}" type="text/javascript"></script>
+<script type="text/javascript">
+	widgets = new widgets({
+		'imgPath':'{$imgPath}',
+		'txtEditer':'{$txtEditer}',
+		'txtErrInterdit':'{$txtErrInterdit}'
+	});
+</script >
+EOH;
+
+	return $flux . $incHead;
+}
+?>
