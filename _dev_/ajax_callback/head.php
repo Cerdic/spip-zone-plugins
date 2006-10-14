5b705094d3c2fbafa8c34c6e3da3bf218487a273
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-10-14 14:03:35 +0000

    "rendre compatible ajax_callback avec la 1,9,1"
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@6269 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/ajax_callback/head.php
+++ _dev_/ajax_callback/head.php
@@ -1,9 +1,13 @@
 <?php
-
 function ajaxcallback_insert_head($flux){
-			return $flux.
-			'<script src="'._DIR_JAVASCRIPT.'layer.js" type="text/javascript"></script>'.
-			'<script src="'.find_in_path('ajaxCallback.js').'" type="text/javascript"></script>';
+	if (!defined(_DIR_JAVASCRIPT)){
+		$p=explode(basename(_DIR_PLUGINS)."/",str_replace('\\','/',realpath(dirname(__FILE__))));
+		define('_DIR_PLUGIN_AJAXCALLBACK',(_DIR_PLUGINS.end($p)));
+		$flux .= '<script src="'._DIR_PLUGIN_AJAXCALLBACK.'/layer.js" type="text/javascript"></script>';
 	}
+	else	$flux .= '<script src="'._DIR_JAVASCRIPT.'layer.js" type="text/javascript"></script>';
+
+	return $flux.'<script src="'.find_in_path('ajaxCallback.js').'" type="text/javascript"></script>';
+}
 
 ?>
