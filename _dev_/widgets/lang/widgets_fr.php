202600eaf8363045e5c8389dfac28cdd6befca69
bertrand@toggg.com <bertrand@toggg.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-10-15 11:24:57 +0000

    Nouvelle chaines (détail des erreurs) ... je sais pas quoi faire des accents pour l'instant :(
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@6312 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/widgets/lang/widgets_fr.php
+++ _dev_/widgets/lang/widgets_fr.php
@@ -4,6 +4,12 @@
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 'editer' => 'Editer',
-'erreur_ou_interdit' => 'Erreur ou interdit !'
+'erreur_ou_interdit' => 'Erreur ou interdit !',
+'donnees_mal_formatees' => 'Donnees mal formatees', // 1
+'non_autorise' => 'Non autorise', // 2
+'modifie_par_ailleurs' => 'Modifie par ailleurs', // 3
+'pas_de_modification' => 'Pas de modification', // 4
+'non_implemente' => 'Non implemente', // 5
+'pas_de_valeur' => 'Pas de valeur' // 6
 );
 ?>
