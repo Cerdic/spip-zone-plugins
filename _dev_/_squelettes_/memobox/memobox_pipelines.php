a2127993d9a8f7d11eb7c28d1270ec46d61a6a90
renato@rezo.net <renato@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-10-18 11:46:42 +0000

    delete copied files; don't know why I couldn't delete all in one commit
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@6410 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/_squelettes_/memobox/memobox_pipelines.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function memobox_insert_head($flux){
-	$flux .= '<link rel="stylesheet" href="'.find_in_path('memobox.css').'" type="text/css" media="projection, screen" />';
-	$flux .= "<script type='text/javascript' src='".generer_url_public('memobox','lang='.$GLOBALS["spip_lang"])."'></script>\n";
-	$flux .= "<script type='text/javascript' src='".find_in_path('memobox_interface.js')."'></script>\n";
-	return $flux;
-}
-?>
