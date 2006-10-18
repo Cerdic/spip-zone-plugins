178b156c48d07ffae5ce82d08761b7890557785f
pierre.andrews@gmail.com <pierre.andrews@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-10-18 15:37:51 +0000

    rangement tout moche, 1er etape
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@6428 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _stable_/coloration_code_1.9.1/coloration_code.php
@@ -0,0 +1,81 @@
+<?php
+
+//    Fichier créé pour SPIP avec un bout de code emprunté à celui ci.
+//    Distribué sans garantie sous licence GPL./
+//    Copyright (C) 2006  Pierre ANDREWS
+//
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program; if not, write to the Free Software
+//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+$p=explode(basename(_DIR_PLUGINS)."/",str_replace('\\','/',realpath(dirname(__FILE__))));
+define('_DIR_PLUGIN_COLORATION_CODE',(_DIR_PLUGINS.end($p)));
+
+function coloration_code_color($code, $language='php', $cadre="cadre") {
+  
+  include_once(_DIR_PLUGIN_COLORATION_CODE.'/geshi/geshi.php');
+  //
+  // Create a GeSHi object
+  //
+  $geshi =& new GeSHi($code, $language);
+
+  if($cadre=="cadre") {
+	  $geshi->set_header_type(GESHI_HEADER_DIV);
+	  $geshi->enable_line_numbers(GESHI_NORMAL_LINE_NUMBERS);
+  } else {
+	  $geshi->set_header_type(GESHI_HEADER_NONE);
+	  $geshi->enable_line_numbers(GESHI_NO_LINE_NUMBERS);
+  }
+
+  //
+  // And echo the result!
+  //
+  return $geshi->parse_code();
+
+}
+
+function coloration_code_echappe($texte) {
+	global $spip_lang_right;
+  $rempl ='';
+
+  if (preg_match_all(
+		 ',<(cadre|code)[[:space:]]+class=("|\')(.*)\2([^>]*)>(.*)</\1>,Uims',
+		 $texte, $matches, PREG_SET_ORDER))
+	foreach ($matches as $regs) {
+	  $code = echappe_retour($regs[5]);
+	  
+	  if(strpos($code, "\n")!==false) {
+	  // Gerer le fichier contenant le code au format texte
+		$nom_fichier = md5($code);
+		$dossier = sous_repertoire(_DIR_IMG, 'cache-code');
+		$fichier = "$dossier$nom_fichier.txt";
+
+		if (!file_exists($fichier)) {
+			$handle = fopen($fichier, 'w');
+			fwrite($handle, $code);
+			fclose($handle);
+		}
+	  }
+	 
+	  $rempl = coloration_code_color(trim($code),$regs[3], $regs[1]);
+	 if(strpos($code, "\n")!==false) {
+	 	$rempl .= "<div class='".$regs[1]."_download' style='text-align: $spip_lang_right;'><a href='$fichier' style='font-family: verdana, arial, sans; font-weight: bold; font-style: normal;'>".
+	  		propre("<multi>Download[fr]T&eacute;l&eacute;charger</multi>").
+	  		"</a></div>";
+	  }
+	  $texte = str_replace($regs[0],echappe_html("<html>$rempl</html>"),$texte);
+	}
+  return $texte;
+}
+
+?>
