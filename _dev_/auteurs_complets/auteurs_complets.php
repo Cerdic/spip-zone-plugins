0b1a3dc45986389fdbe3212e762ae359809ab2d9
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-10-19 21:24:21 +0000

    une petite erreur sur le pipeline
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@6481 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/auteurs_complets/auteurs_complets.php
+++ _dev_/auteurs_complets/auteurs_complets.php
@@ -1,7 +1,7 @@
 <?php
 function auteurs_complets_affiche_milieu($flux) {
 	$exec =  $flux['args']['exec'];
-	if ($exec=='auteurs_infos'){
+	if ($exec=='auteur_infos'){
 		include_spip('inc/auteurs_complets_gestion');
 		$id_auteur = $flux['args']['id_auteur'];
 		$flux['data'] .= auteurs_complets_ajouts($id_auteur);
