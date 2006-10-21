14767447f22a8627523e530b9f68d590fe547cf6
renato@rezo.net <renato@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-10-21 13:54:57 +0000

    js code packed as a jquery plugin. The user has to code only the function to update the page when the upload is successful
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@6506 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/_ze_laboratoire_/async_upload/exec/articles.php
+++ _dev_/_ze_laboratoire_/async_upload/exec/articles.php
@@ -195,76 +195,22 @@ function articles_documents($flag_editable, $type, $id)
 			$align = " align='right'";
 		}
 		$res = "\n<div$align>$res</div>";
-		$res .= <<<EOF
-  <script type="text/javascript">
-      $(".form_upload")
-      .submit(function(){
-        return do_async_upload(this);
+    $res .= "<script src='".find_in_path("async_upload.js")."' type='text/javascript'></script>\n";
+    $res .= <<<EOF
+    <script type='text/javascript'>
+    $(".form_upload").async_upload(function(res){
+      res.find(">div").each(function(){
+        var cont = $("#"+this.id);
+        var self = $(this);
+        if(!cont.size()) {
+          cont = $(this.id.search(/--/)!=-1 ? "#portfolio":"#documents")
+          .append(self.clone().get());
+        }
+        verifForm(cont.html(self.html()));
       });
-
-    var iframe;
-    function do_async_upload(form) {
-      var jForm = $(form);
-      var par = $(jForm).parent();
-      $("div.upload_message",par)
-      .remove();
-      if(!form.async_init) {
-        form.async_init = true
-        jForm
-        .attr("target","upload_frame")
-        .append("<input type='hidden' name='iframe' value='iframe'>")
-        .find("input[@name='redirect']")
-          .val("")
-        .end();
-      }
-
-      if (!iframe) {
-        iframe = $("<iframe id='upload_frame' name='upload_frame' frameborder='0' marginwidth='0' marginheight='0' scrolling='yes' style='position:absolute' onload='this.iframeload()'></iframe>")
-        .appendTo("body");
-      }
-      
-      //IE apparently do not write anything in an iframe onload event handler 
-      iframe[0].iframeload = function() {
-          //remove the previous message
-          \$("div.upload_message",par).remove();
-          var res = $(".upload_answer",this.contentDocument || document.frames("upload_frame").document.body);
-          //possible classes 
-          //upload_document_added
-          if(res.is(".upload_document_added")) {
-            res.find(">div").each(function(){
-              var cont = $("#"+this.id);
-              if(!cont.size()) {
-                cont = $(this.id.search(/--/)!=-1 ? "portfolio":"documents").parent();
-              }
-              verifForm(cont.html($(this).html()));
-            });
-            return true;
-          }
-          //upload_error
-          if(res.is(".upload_error")) {
-            var msg = $("<div class='upload_message'>")
-            .append(res.html())
-            jForm.after(msg[0]);
-            return true;
-          } 
-          //upload_zip_list
-          if(res.is(".upload_zip_list")) {
-            var zip_form = $("<div class='upload_message'>").append(res.html());
-            zip_form
-            .find("form")
-              .attr("target","upload_frame")
-              .append("<input type='hidden' name='iframe' value='iframe'>")
-            .end();
-            jForm.after(zip_form[0]);
-            return true;  
-          }
-      };
-      
-      jForm.before($("<div class='upload_message'>").append(ajax_image_searching)[0]);
-      return true;
-    }
-    
-  </script>
+      return true;             
+    })
+    </script>
 EOF;
 		
 	} else $res = '';
--- _dev_/_ze_laboratoire_/async_upload/inc/documents.php
+++ _dev_/_ze_laboratoire_/async_upload/inc/documents.php
@@ -174,97 +174,41 @@ function afficher_documents_colonne($id, $type="article", $flag_modif = true) {
 		$ret .= "</div>";
 	}
 
-	$ret .= <<<EOF
-  <script type="text/javascript">
-      $(".form_upload")
-      .submit(function(){
-        return do_async_upload(this);
-      });
-
-    var iframe;
-    function do_async_upload(form) {
-      var jForm = $(form);
-      var par = $(jForm).parent();
-      $("div.upload_message",par)
-      .remove();
-      if(!form.async_init) {
-        form.async_init = true
-        jForm
-        .attr("target","upload_frame")
-        .append("<input type='hidden' name='iframe' value='iframe'>")
-        .find("input[@name='redirect']")
-          .val("")
-        .end();
-      }
-
-      if (!iframe) {
-        iframe = $("<iframe id='upload_frame' name='upload_frame' frameborder='0' marginwidth='0' marginheight='0' scrolling='yes' style='position:absolute' onload='this.iframeload()'></iframe>")
-        .appendTo("body");
-      }
-      
-      //IE apparently do not write anything in an iframe onload event handler 
-      iframe[0].iframeload = function() {
-          //remove the previous message
-          \$("div.upload_message",par).remove();
-          var res = $(".upload_answer",this.contentDocument || document.frames("upload_frame").document.body);
-          //possible classes 
-          //upload_document_added
-          if(res.is(".upload_document_added")) {
-            var cont;
-            //add a class to new documents
-            res.
-            find(">div[@class]")
-              .addClass("documents_added")
-              .css("display","none")
-            .end();
-            if (jForm.find("input[@name='arg']").val().search("vignette")!=-1)
-              cont = $("#liste_images");
-            else
-              cont = $("#liste_documents");
-            cont
-            .prepend(res.html());
-            //find added documents, remove label and show them nicely
-            //set overflow to visible to completely show the overlay icon
-            cont.
-            find("div.documents_added")
-              .removeClass("documents_added")
-              .show("slow",function(){
-                  var anim = $(this).css({"height":"","overflow":""});
-                  //bug explorer-opera-safari
-                  if(!jQuery.browser.mozilla) anim.width(this.orig.width-2);
-                  $(anim).find("img[@onclick]").get(0).onclick();
-              })
-              .overflow("");
-            verifForm(cont);
-            return true;
-          }
-          //upload_error
-          if(res.is(".upload_error")) {
-            var msg = $("<div class='upload_message'>")
-            .append(res.html())
-            jForm.after(msg[0]);
-            return true;
-          } 
-          //upload_zip_list
-          if(res.is(".upload_zip_list")) {
-            var zip_form = $("<div class='upload_message'>").append(res.html());
-            zip_form
-            .find("form")
-              .attr("target","upload_frame")
-              .append("<input type='hidden' name='iframe' value='iframe'>")
-            .end();
-            jForm.after(zip_form[0]);
-            return true;  
-          }
-      };
-      
-      jForm.before($("<div class='upload_message'>").append(ajax_image_searching)[0]);
+  $res .= "<script src='".find_in_path("async_upload.js")."' type='text/javascript'></script>\n";
+  $res .= <<<EOF
+    <script type='text/javascript'>
+    $(".form_upload").async_upload(function(res,jForm){
+      var cont;
+      //add a class to new documents
+      res.
+      find(">div[@class]")
+        .addClass("documents_added")
+        .css("display","none")
+      .end();
+      if (jForm.find("input[@name='arg']").val().search("vignette")!=-1)
+        cont = $("#liste_images");
+      else
+        cont = $("#liste_documents");
+      cont
+      .prepend(res.html());
+      //find added documents, remove label and show them nicely
+      //set overflow to visible to completely show the overlay icon
+      cont.
+      find("div.documents_added")
+        .removeClass("documents_added")
+        .show("slow",function(){
+            var anim = $(this).css({"height":"","overflow":""});
+            //bug explorer-opera-safari
+            if(!jQuery.browser.mozilla) anim.width(this.orig.width-2);
+            $(anim).find("img[@onclick]").get(0).onclick();
+        })
+        .overflow("");
+      verifForm(cont);
       return true;
-    }
-    
-  </script>
+    })
+    </script>
 EOF;
-
+    
 	return $ret;
 }
 
