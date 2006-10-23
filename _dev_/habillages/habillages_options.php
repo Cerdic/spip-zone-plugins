985c2e5613dd3cd8b0b19118d63a891f606fa4ad
franck.ducas@free.fr <franck.ducas@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-10-23 18:19:25 +0000

    On force aussi l'utilisation du squelette choisi dans le plugin "Habillages" au detriment du squelette d'origine installe sur le site. Du coup toute la logique du squelette active via plugin dans l'interface de gestion des squelettes fait doublon (?). A voir de plus pres donc.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@6560 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/habillages/habillages_options.php
+++ _dev_/habillages/habillages_options.php
@@ -5,4 +5,7 @@ $lire_habillages_squelettes = $GLOBALS['meta']['habillages_squelettes'];
 if ($lire_habillages_squelettes == "dist") {
 $GLOBALS['dossier_squelettes'] = 'dist';
 }
+else {
+	$GLOBALS['dossier_squelettes'] = "plugins/".$lire_habillages_squelettes;
+}
 ?>
