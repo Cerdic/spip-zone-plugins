3e16cc0df16972d2f03c24c629d40d0d12b076a9
bertrand@toggg.com <bertrand@toggg.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-10-26 17:29:42 +0000

    Stable
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@6644 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _stable_/boutonstexte/lang/boutonstexte_fr.php
@@ -0,0 +1,11 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+'texte_seulement' => 'Texte seulement',
+'retour_a_spip' => 'Retour &agrave; Spip',
+'augmenter_police' => 'Augmenter police',
+'diminuer_police' => 'Diminuer police'
+);
+?>
---
+++ _stable_/boutonstexte/lang/boutonstexte_it.php
@@ -0,0 +1,11 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+'texte_seulement' => 'Solo il testo',
+'retour_a_spip' => 'Ritorna a Spip',
+'augmenter_police' => 'Aumenta la dimensione del testo',
+'diminuer_police' => 'Diminuisci la dimensione del testo'
+);
+?>
