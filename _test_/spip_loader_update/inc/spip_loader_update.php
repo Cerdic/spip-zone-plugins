8f52e47c195981ec42cf7254cbcb1f878cb778bf
james@rezo.net <james@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-10-26 11:48:10 +0000

    priorité à la liste définie dans config/
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@6634 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/spip_loader_update/inc/spip_loader_update.php
+++ _test_/spip_loader_update/inc/spip_loader_update.php
@@ -5,16 +5,18 @@
 define('_SPIP_LOADER_UPDATE_AUTEURS', '1');
 
 # fichier source
-define('_SPIP_LOADER_UPDATE_FILE', find_in_path('spip_loader_update_list.txt'));
+define('_SPIP_LOADER_UPDATE_FILE',
+	(@is_readable($f = _DIR_ETC.'spip_loader_update_list.txt') ? $f :
+	(@is_readable($f = find_in_path('spip_loader_update_list.txt')) ? $f :
+	false))
+);
 
-function spip_loader_liste($fichier = '') {
+function spip_loader_liste() {
 	$spip_loader_liste = array();
-	if(!$fichier) $fichier = _SPIP_LOADER_UPDATE_FILE;
-
-	if (!file_exists($fichier)) {
-		die ("Fichier de configuration ".$fichier." absent");
+	if (!_SPIP_LOADER_UPDATE_FILE) {
+		die ("Fichier de configuration absent");
 	}
-	$config = file($fichier);
+	$config = file(_SPIP_LOADER_UPDATE_FILE);
 	foreach ($config as $l) {
 		$l = trim($l);
 		if ($l AND substr($l,0,1) != "#") {
