103f619f6f4eba08d83c7b820a0217456cb797bf
erational@erational.org <erational@erational.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-10-27 09:44:45 +0000

    installation groupe mots pas bonne, merci izo
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@6660 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/spipicious/exec/spipicious.php
+++ _dev_/spipicious/exec/spipicious.php
@@ -24,13 +24,11 @@ function exec_spipicious() {
   	$table_pref = 'spip';
   	if ($GLOBALS['table_prefix']) $table_pref = $GLOBALS['table_prefix'];
   	
-  	// Creation
-    spip_query("CREATE TABLE IF NOT EXISTS `{$table_pref}_spipicious` (`id_mot` bigint(21) NOT NULL default '0', `id_auteur` bigint(21) NOT NULL default '0',`id_article` bigint(21) NOT NULL default '0', KEY `id_mot` (`id_mot`));");
-    
+  	// Creation   
     $result = spip_query("SELECT id_groupe FROM `{$table_pref}_groupes_mots` WHERE titre = '- tags -'"); // creation du groupe de mots cles uniquement si n'existe pas
 		if (spip_num_rows($result) == 0) {
-      spip_query("INSERT INTO `{$table_pref}_groupes_mots` ( `id_groupe` , `titre` , `descriptif` , `texte` , `unseul` , `obligatoire` , `articles` , `breves` , `rubriques` , `syndic` , `minirezo` , `comite` , `forum` , `maj` , `auteurs` )
-                                           VALUES ('', '- tags -', '', '', 'non', 'non', 'oui', 'non', 'non', 'non', 'non', 'non', 'non', NOW( ) , 'non');");
+      spip_query("INSERT INTO {$table_pref}_groupes_mots (id_groupe, titre, descriptif, texte, unseul, obligatoire, articles, breves, rubriques, syndic, minirezo, comite, forum , maj )
+                                           VALUES ('', '- tags -', '', '', 'non', 'non', 'oui', 'non', 'non', 'non', 'non', 'non', 'non', NOW( ));");
     }   
     
     // Affichage 
