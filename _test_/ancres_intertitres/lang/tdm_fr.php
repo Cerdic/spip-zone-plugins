1532a6fd7b2096281c9b3e1d387a1391f6761350
franck.ducas@free.fr <franck.ducas@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-10-29 09:16:05 +0000

    Coquille.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@6724 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/ancres_intertitres/lang/tdm_fr.php
+++ _test_/ancres_intertitres/lang/tdm_fr.php
@@ -2,7 +2,7 @@
 
 	$GLOBALS['i18n_tdm_fr'] = array(
 		'table_matiere' => 'Table des mati&egrave;res',
-		'retour_table_matiere' => 'Retout &agrave; la table des mati&egrave;res'
+		'retour_table_matiere' => 'Retour &agrave; la table des mati&egrave;res'
 	);
 
 ?>
