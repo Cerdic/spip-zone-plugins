2ca6e397e6a1439cd662c0ac3a61851e5d5c2918
bertrand@toggg.com <bertrand@toggg.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-10-30 14:37:22 +0000

    Merci Marie Gugger , en entités comme widgets_fr.php ... pas interprétés dans les alert() pour l'heure.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@6759 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _dev_/widgets/lang/widgets_de.php
@@ -0,0 +1,14 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	'editer' => 'Inhalt &auml;ndern',
+	'donnees_mal_formatees' => 'Anforderung fehlerhaft formartiert',
+	'non_autorise' => 'Fehlende Rechte',
+	'modifie_par_ailleurs' => 'Wurde von jemand anderem bereits ge&auml;ndert',
+	'pas_de_modification' => 'Keine &Auml;nderung',
+	'non_implemente' => 'Nicht implementiert',
+	'pas_de_valeur' => 'Inhalt existiert nicht'
+	);
+?>
