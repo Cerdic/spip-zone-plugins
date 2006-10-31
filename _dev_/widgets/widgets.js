f8a39991c8b7aed95dafa1f74a973ec8b71dba28
cy_altern@yahoo.fr <cy_altern@yahoo.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-10-31 15:44:20 +0000

    positionnement de l'icone stylo dans les articles widgetisés
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@6818 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/widgets/widgets.js
+++ _dev_/widgets/widgets.js
@@ -209,7 +209,7 @@ $(document).ready(function() {
   if (!jQuery.getJSON) return; // jquery >= 1.0.2
   url_widgets_html = 'spip.php?action=widgets_html';
   SEARCHING = "<img class='widget-searching' src='" + configWidgets.imgPath + "/searching.gif' style='float:right;' />";
-  ICONCLICK = "<span style='float:right;z-index:100;'><img class='widget-edit' onclick='event.stopPropagation();$(this).parent().parent().openwidget();' src='" + configWidgets.imgPath + "/crayon.png' title='" + configWidgets.txtEditer + "' /><img class='widget-img-changed' src='" + configWidgets.imgPath + "/changed.png' title='" + configWidgets.txtChanged + "' /></span>";
+  ICONCLICK = "<span style='z-index:100; position: absolute; margin-left: 510px;'><img class='widget-edit' onclick='event.stopPropagation();$(this).parent().parent().openwidget();' src='" + configWidgets.imgPath + "/crayon.png' title='" + configWidgets.txtEditer + "' /><img class='widget-img-changed' src='" + configWidgets.imgPath + "/changed.png' title='" + configWidgets.txtChanged + "' /></span>";
 
   // sortie, demander pour sauvegarde si oubli
   $(window).unload(function(e) {
