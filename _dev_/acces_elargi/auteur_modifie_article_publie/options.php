8cb5ef7ce1717ae837b4fc1aebc0580632f33f32
fil@rezo.net <fil@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-11-01 09:43:51 +0000

    plugin acces elargi 1 utilisant l'API
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@6833 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _dev_/acces_elargi/auteur_modifie_article_publie/options.php
@@ -0,0 +1,26 @@
+<?php
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+// Autoriser a modifier l'article $id
+// = publier_dans rubrique parente
+// = ou statut 'prop,prepa' et $qui est auteur
+function autoriser_modifier_article($faire, $type, $id, $qui, $opt) {
+	$s = spip_query(
+	"SELECT id_rubrique,statut FROM spip_articles WHERE id_article="._q($id));
+	$r = spip_fetch_array($s);
+	return
+		autoriser('publier_dans', 'rubrique', $r['id_rubrique'], $qui, $opt)
+		OR (
+			in_array($qui['statut'], array('0minirezo', '1comite'))
+/*
+			# on commente cette ligne : tous les articles sont modifiables
+			AND in_array($r['statut'], array('prop','prepa', 'poubelle'))
+*/
+			AND spip_num_rows(auteurs_article($id, "id_auteur=".$qui['id_auteur']))
+		);
+}
+
+
+
+?>
