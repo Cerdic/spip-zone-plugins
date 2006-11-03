83d36fe23d0e7f25a2925839019584028ab74207
pierre.andrews@gmail.com <pierre.andrews@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-11-03 00:57:10 +0000

    il manquait la creation des tables
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@6911 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/FpipR/trunk/FpipR_create/flickr_photos_search.php
+++ _test_/FpipR/trunk/FpipR_create/flickr_photos_search.php
@@ -1,5 +1,8 @@
 <?php
 function FpipR_create_flickr_photos_search_dist() {
   FpipR_make_table('spip_fpipr_photos');
+  FpipR_make_table('spip_fpipr_tags');
+  FpipR_make_table('spip_fpipr_notes');
+  FpipR_make_table('spip_fpipr_tags');
 }
 ?>
