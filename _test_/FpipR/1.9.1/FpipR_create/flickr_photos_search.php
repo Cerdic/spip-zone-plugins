f1995d36f9a273c161c25f1b9e149ed1033d62a3
pierre.andrews@gmail.com <pierre.andrews@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-11-03 01:16:39 +0000

    merge r6913
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@6914 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/FpipR/1.9.1/FpipR_create/flickr_photos_search.php
+++ _test_/FpipR/1.9.1/FpipR_create/flickr_photos_search.php
@@ -1,8 +1,5 @@
 <?php
 function FpipR_create_flickr_photos_search_dist() {
   FpipR_make_table('spip_fpipr_photos');
-  FpipR_make_table('spip_fpipr_tags');
-  FpipR_make_table('spip_fpipr_notes');
-  FpipR_make_table('spip_fpipr_tags');
 }
 ?>
