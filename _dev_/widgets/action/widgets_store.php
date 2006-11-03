755c9b1aceded418c454197cb6d871f11f2920aa
bertrand@toggg.com <bertrand@toggg.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-11-03 11:11:31 +0000

    2 actions separees , un seul inc , j'avais tout melange l'autre fois ...2 actions separees , un seul inc , j'avais tout melange l'autre fois ...
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@6937 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/widgets/action/widgets_store.php
+++ _dev_/widgets/action/widgets_store.php
@@ -47,128 +47,135 @@ function post_widgets() {
 }
 
 
-function traiter_les_widgets() {
-$modifs = post_widgets();
-$anamod = $anaupd = array();  # TODO: expliciter les noms de variables
-if (!is_array($modifs)) {
-    $return['$erreur'] = _U('widgets:donnees_mal_formatees');
-} else {
-    include_spip('inc/autoriser');
+function action_widgets_store_dist() {
 
-    foreach($modifs as $m) {
-        if ($m[2] && preg_match(_PREG_WIDGET, 'widget '.$m[0], $regs)) {
-            list(,$widget,$type,$champ,$id) = $regs;
-            $wid = $m[3];
-            if (!autoriser('modifier', $type, $id, NULL, array('champ'=>$champ))) {
-                $return['$erreur'] =
-                    "$type $id: " . _U('widgets:non_autorise');
-                break;
-            }
+    include_spip('inc/widgets');
+    header("Content-Type: text/html; charset=".$GLOBALS['meta']['charset']);
 
-            // alias temporaire pour titreurl, en attendant un modele
-            if ($champ == 'titreurl') $champtable = 'titre';
-            else $champtable = $champ;
+    $return = array('$erreur'=>'');
 
-            $md5 = md5(valeur_colonne_table($type, $champtable, $id));
+	$modifs = post_widgets();
+	$anamod = $anaupd = array();  # TODO: expliciter les noms de variables
+	if (!is_array($modifs)) {
+	    $return['$erreur'] = _U('widgets:donnees_mal_formatees');
+	} else {
+	    include_spip('inc/autoriser');
 
-            // est-ce que le champ a ete modifie dans la base ?
-            if ($md5 != $m[2]) {
-                // si oui, la modif demandee correspond peut-etre
-                // a la nouvelle valeur ? dans ce cas on procede
-                // comme si "pas de modification", sinon erreur
-                if ($md5 != md5($m[1])) {
-                    $return['$erreur'] = "$type $id $champtable: " .
-                        _U('widgets:modifie_par_ailleurs');
-                    }
-                break;
-            }
-            $anamod[] = array($wid,$type,$champ,$id,$m[1]);
-            if (!isset($anaupd[$type])) {
-                // MODELE
-                switch($type) {
-                    case 'article':
-                        include_spip('action/editer_article');
-                        $fun = 'revisions_articles';
-                        break;
-                    case 'rubrique':
-                        include_spip('action/editer_rubrique');
-                        $fun = 'revisions_rubriques';
-                        break;
-                    case 'breve':
-                        include_spip('action/editer_breve');
-                        $fun = 'revisions_breves';
-                        break;
-                    default :
-                $return['$erreur'] =
-                    "$type: " . _U('widgets:non_implemente');
-                break 2;
-                }
-                $anaupd[$type] = array('fun'=>$fun, 'ids'=>array());
-            }
-            if (!isset($anaupd[$type]['ids'][$id])) {
-                $anaupd[$type]['ids'][$id] = array('wdg'=>array(), 'chval'=>array());
-            }
-            // pour reaffecter le retour d'erreur sql au cas ou
-            $anaupd[$type]['ids'][$id]['wdg'][] = $wid;
-            $anaupd[$type]['ids'][$id]['chval'][$champtable] = $m[1];
-        }
-    }
-}
-if (!$anamod AND !$return['$erreur']) {
-    $return['$erreur'] = _U('widgets:pas_de_modification');
-    $return['$annuler'] = true;
-}
+	    foreach($modifs as $m) {
+	        if ($m[2] && preg_match(_PREG_WIDGET, 'widget '.$m[0], $regs)) {
+	            list(,$widget,$type,$champ,$id) = $regs;
+	            $wid = $m[3];
+	            if (!autoriser('modifier', $type, $id, NULL, array('champ'=>$champ))) {
+	                $return['$erreur'] =
+	                    "$type $id: " . _U('widgets:non_autorise');
+	                break;
+	            }
 
-// une quelconque erreur ... ou rien ==> on ne fait rien !
-if ($return['$erreur']) {
+	            // alias temporaire pour titreurl, en attendant un modele
+	            if ($champ == 'titreurl') $champtable = 'titre';
+	            else $champtable = $champ;
+
+	            $md5 = md5(valeur_colonne_table($type, $champtable, $id));
+
+	            // est-ce que le champ a ete modifie dans la base ?
+	            if ($md5 != $m[2]) {
+	                // si oui, la modif demandee correspond peut-etre
+	                // a la nouvelle valeur ? dans ce cas on procede
+	                // comme si "pas de modification", sinon erreur
+	                if ($md5 != md5($m[1])) {
+	                    $return['$erreur'] = "$type $id $champtable: " .
+	                        _U('widgets:modifie_par_ailleurs');
+	                    }
+	                break;
+	            }
+	            $anamod[] = array($wid,$type,$champ,$id,$m[1]);
+	            if (!isset($anaupd[$type])) {
+	                // MODELE
+	                switch($type) {
+	                    case 'article':
+	                        include_spip('action/editer_article');
+	                        $fun = 'revisions_articles';
+	                        break;
+	                    case 'rubrique':
+	                        include_spip('action/editer_rubrique');
+	                        $fun = 'revisions_rubriques';
+	                        break;
+	                    case 'breve':
+	                        include_spip('action/editer_breve');
+	                        $fun = 'revisions_breves';
+	                        break;
+	                    default :
+	                $return['$erreur'] =
+	                    "$type: " . _U('widgets:non_implemente');
+	                break 2;
+	                }
+	                $anaupd[$type] = array('fun'=>$fun, 'ids'=>array());
+	            }
+	            if (!isset($anaupd[$type]['ids'][$id])) {
+	                $anaupd[$type]['ids'][$id] = array('wdg'=>array(), 'chval'=>array());
+	            }
+	            // pour reaffecter le retour d'erreur sql au cas ou
+	            $anaupd[$type]['ids'][$id]['wdg'][] = $wid;
+	            $anaupd[$type]['ids'][$id]['chval'][$champtable] = $m[1];
+	        }
+	    }
+	}
+	if (!$anamod AND !$return['$erreur']) {
+	    $return['$erreur'] = _U('widgets:pas_de_modification');
+	    $return['$annuler'] = true;
+	}
+
+	// une quelconque erreur ... ou rien ==> on ne fait rien !
+	if ($return['$erreur']) {
+	    echo var2js($return);
+	    exit;
+	}
+
+	// sinon on bosse
+	foreach($anaupd as $type => $idschamps) {
+	    foreach($idschamps['ids'] as $id => $champsvaleurs) {
+
+	        // Enregistrer dans la base
+	        // $updok = ... quand on aura un retour
+	        // -- revisions_articles($id_article, $c) --
+	        $idschamps['fun']($id, $champsvaleurs['chval']);
+	    }
+	}
+	foreach($anamod as $m) {
+	    list($wid,$type,$champ,$id,$valeur) = $m;
+
+	    // VUE
+	    // chercher vues/article_toto.html
+	    // sinon vues/toto.html
+	    if (find_in_path( ($fond = 'vues/' . $type . '_' . $champ) . '.html')
+	        || find_in_path( ($fond = 'vues/' . $champ) .'.html') ) {
+	        $contexte = array(
+	            'id_' . $type => $id,
+	            'lang' => $GLOBALS['spip_lang']
+	        );
+	        include_spip('public/assembler');
+	        $return[$wid] = recuperer_fond($fond, $contexte);
+	    }
+	    // vues par defaut
+	    else {
+	        // Par precaution on va rechercher la valeur
+	        // dans la base de donnees (meme si a priori la valeur est
+	        // ce qu'on vient d'envoyer, il y a nettoyage des caracteres et
+	        // eventuellement d'autres filtres de saisie...)
+	        $valeur = valeur_colonne_table($type, $champ, $id);
+
+	        // seul spip core sait rendre les donnees
+	        include_spip('inc/texte');
+	        if (in_array($champ,
+	        array('chapo', 'texte', 'descriptif', 'ps'))) {
+	            $return[$wid] = propre($valeur);
+	        } else {
+	            $return[$wid] = typo($valeur);
+	        }
+	    }
+	}
+
     echo var2js($return);
     exit;
-}
-
-// sinon on bosse
-foreach($anaupd as $type => $idschamps) {
-    foreach($idschamps['ids'] as $id => $champsvaleurs) {
-
-        // Enregistrer dans la base
-        // $updok = ... quand on aura un retour
-        // -- revisions_articles($id_article, $c) --
-        $idschamps['fun']($id, $champsvaleurs['chval']);
-    }
-}
-foreach($anamod as $m) {
-    list($wid,$type,$champ,$id,$valeur) = $m;
-
-    // VUE
-    // chercher vues/article_toto.html
-    // sinon vues/toto.html
-    if (find_in_path( ($fond = 'vues/' . $type . '_' . $champ) . '.html')
-        || find_in_path( ($fond = 'vues/' . $champ) .'.html') ) {
-        $contexte = array(
-            'id_' . $type => $id,
-            'lang' => $GLOBALS['spip_lang']
-        );
-        include_spip('public/assembler');
-        $return[$wid] = recuperer_fond($fond, $contexte);
-    }
-    // vues par defaut
-    else {
-        // Par precaution on va rechercher la valeur
-        // dans la base de donnees (meme si a priori la valeur est
-        // ce qu'on vient d'envoyer, il y a nettoyage des caracteres et
-        // eventuellement d'autres filtres de saisie...)
-        $valeur = valeur_colonne_table($type, $champ, $id);
-
-        // seul spip core sait rendre les donnees
-        include_spip('inc/texte');
-        if (in_array($champ,
-        array('chapo', 'texte', 'descriptif', 'ps'))) {
-            $return[$wid] = propre($valeur);
-        } else {
-            $return[$wid] = typo($valeur);
-        }
-    }
-}
-
-return $return;
 }
 ?>
--- _dev_/widgets/inc/widgets.php
+++ _dev_/widgets/inc/widgets.php
@@ -1,171 +1,69 @@
 <?php
 
-function affiche_controleur($class) {
-  if (preg_match(_PREG_WIDGET, $class, $regs)) {
-    list(,$widget,$type,$champ,$id) = $regs;
-    include_spip('inc/autoriser');
-    if (!autoriser('modifier',$type, $id, NULL, array('champ'=>$champ))) {
-        $return['$erreur'] = "$type $id: " . _U('widgets:non_autorise');
-    } else {
-        $f = charger_fonction($type.'_'.$champ, 'controleurs', true)
-        OR $f = charger_fonction($champ, 'controleurs', true)
-        OR $f = 'controleur_dist';
-        list($html,$status) = $f($regs);
-        if ($status) {
-            $return['$erreur'] = $html;
-        } else {
-            $return['$html'] = $html;
-        }
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+define('_PREG_WIDGET', ',widget\b[^<>\'"]+\b((\w+)-(\w+)-(\d+))\b,');
+
+function valeur_colonne_table($table, $col, $id) {
+    $s = spip_query(
+        'SELECT ' . $col .
+          ' AS val FROM spip_' . $table .'s    WHERE id_' . $table . '=' . $id);
+    if ($t = spip_fetch_array($s)) {
+        return $t['val'];
     }
-  } else {
-    $return['$erreur'] = _U('widgets:donnees_mal_formatees');
-  }
-  return $return;
+    return false;
 }
 
-function controleur_dist($regs) {
-    list(,$widget,$type,$champ,$id) = $regs;
-
-    // type du widget
-    if (in_array($champ,
-    array('chapo', 'texte', 'descriptif', 'ps')))
-        $mode = 'texte';
-    else
-        $mode = 'ligne';
-
-    // taille du widget
-    $w = intval($_GET['w']);
-    $h = intval($_GET['h']);
-    $wh = intval($_GET['wh']); // window height
-    if ($w<100) $w=100;
-    if ($w>700) $w=700;
-    if ($mode == 'texte') {
-        if ($h<36) $h=36; #ici on pourrait mettre minimum 3*$_GET['em']
+/**
+    * Transform a variable into its javascript equivalent (recursive)
+    * @access private
+    * @param mixed the variable
+    * @return string js script | boolean false if error
+    */
+function var2js($var) {
+    $asso = false;
+    switch (true) {
+        case is_null($var) :
+            return 'null';
+        case is_string($var) :
+            return '"' . addcslashes($var, "\"\\\n\r") . '"';
+        case is_bool($var) :
+            return $var ? 'true' : 'false';
+        case is_scalar($var) :
+            return $var;
+        case is_object( $var) :
+            $var = get_object_vars($var);
+            $asso = true;
+        case is_array($var) :
+            $keys = array_keys($var);
+            $ikey = count($keys);
+            while (!$asso && $ikey--) {
+                $asso = $ikey !== $keys[$ikey];
+            }
+            $sep = '';
+            if ($asso) {
+                $ret = '{';
+                foreach ($var as $key => $elt) {
+                    $ret .= $sep . '"' . $key . '":' . var2js($elt);
+                    $sep = ',';
+                }
+                return $ret ."}\n";
+            } else {
+                $ret = '[';
+                foreach ($var as $elt) {
+                    $ret .= $sep . var2js($elt);
+                    $sep = ',';
+                }
+                return $ret ."]\n";
+            }
     }
-    else // ligne, hauteur naturelle
-        $h='';#$hx = htmlspecialchars($_GET['em']);
-
-    // hauteur maxi d'un textarea -- pas assez ? trop ?
-    $maxheight = min(max($wh-50,400), 700);
-    if ($h>$maxheight) $h=$maxheight;
-
-    $inputAttrs = array(
-        'style' => "width:${w}px;" . ($h ? " height:${h}px;" : ''));
-
-    $valeur = valeur_colonne_table($type, $champ, $id);
-    if ($valeur !== false) {
-        $n = new Widget($widget, $valeur);
-        $widgetsAction = self();
-        $widgetsCode = $n->code();
-        $widgetsInput = $n->input($mode, $inputAttrs);
-        $widgetsImgPath = dirname(find_in_path('images/cancel.png'));
-
-        // title des boutons
-        $OK = texte_backend(_T('bouton_enregistrer'));
-        $Cancel = texte_backend(_L('Annuler'));
-        $Editer = texte_backend(_L("&Eacute;diter $type $id"));
-        $url_edit = "ecrire/?exec={$type}s_edit&amp;id_{$type}=$id";
-
-        $html =
-        <<<FIN_FORM
-
-<form method="post" action="{$widgetsAction}">
-  {$widgetsCode}
-  {$widgetsInput}
-  <div class="widget-boutons">
-  <div>
-    <a class="widget-submit" title="{$OK}">
-      <img src="{$widgetsImgPath}/ok.png" width="20" height="20" />
-    </a>
-    <a class="widget-cancel" title="{$Cancel}">
-      <img src="{$widgetsImgPath}/cancel.png" width="20" height="20" />
-    </a>
-    <a href="{$url_edit}" title="{$Editer}" class="widget-full">
-      <img src="{$widgetsImgPath}/edit.png" width="20" height="20" />
-    </a>
-  </div>
-</div>
-</form>
-
-FIN_FORM;
-        $status = NULL;
-
-    }
-    else {
-        $html = "$type $id $champ: " . _U('widgets:pas_de_valeur');
-        $status = 6;
-    }
-
-    return array($html,$status);
+    return false;
 }
 
-// Definition des widgets
-class Widget {
-    var $name;
-    var $text;
-    var $key;
-    var $md5;
-    var $clean;
-
-    function Widget($name, $text='') {
-        $this->name = $name;
-        $this->text = $text;
-        $this->key = strtr(uniqid('wid', true), '.', '_');
-    }
-
-    function md5() {
-        return md5($this->text);
-    }
-
-    function code() {
-        return
-        '<input type="hidden" class="widget-id" name="widgets[]" value="'.$this->key.'" />'."\n"
-        . '<input type="hidden" name="name_'.$this->key.'" value="'.$this->name.'" />'."\n"
-        . '<input type="hidden" name="md5_'.$this->key
-        .'" value="'.$this->md5().'" />'."\n";
-    }
-
-    function input($type = 'ligne', $attrs = array()) {
-        include_spip('inc/filtres');
-        switch ($type) {
-            case 'texte':
-                $return = '<textarea class="widget-active"'
-                . ' name="content_'.$this->key.'">'
-                . entites_html($this->text)
-                . '</textarea>'."\n";
-                break;
-            case 'ligne':
-            default:
-                $return = '<input class="widget-active" type="text"'
-                . ' name="content_'.$this->key.'"'
-                . ' value="'
-                . entites_html($this->text)
-                . '" />'."\n";
-        }
-        foreach ($attrs as $attr=>$val) {
-            $return = inserer_attribut($return, $attr, $val);
-        }
-        return $return;
-    }
-}
-
-//
-// Un Widget avec une verification de code de securite
-//
-class SecureWidget extends Widget {
-
-    function SecureWidget($name, $text='') {
-        parent::Widget($name, $text);
-    }
-
-    function code() {
-        $code = parent::code();
-        $secu = md5($GLOBALS['meta']['alea_ephemere']. '=' . $this->name);
-
-        return
-            $code
-            .'<input type="hidden" name="secu_'.$this->key.'" value="'.$secu.'" />'."\n";
-    }
+function _U($texte)
+{
+    include_spip('inc/charsets');
+    return html2unicode(_T($texte));
 }
 
 ?>
