aace5f3c880e40b035e6c08c95e7c5c8728e38f4
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-11-04 20:32:57 +0000

    des noeuds plus jolis
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@6978 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/arbre_noel/arbodyn_edite.php
+++ _dev_/arbre_noel/arbodyn_edite.php
@@ -2,8 +2,8 @@
 function arbodyn_header_prive($flux){
 
 	$flux.='<script type="text/javascript"><!--
-var img_deplierhaut = "'._DIR_IMG_PACK.'deplierhaut.gif";
-var img_deplierbas = "'._DIR_IMG_PACK.'deplierbas.gif";
+var img_deplierhaut = "'._DIR_PLUGIN_ARBODYN.'noeud_plus.png";
+var img_deplierbas = "'._DIR_PLUGIN_ARBODYN.'noeud_moins.png";
 //--></script>';
 	$flux .= '<script src="'.find_in_path('dragdrop_interface.js').'" type="text/javascript"></script>';
 	$flux .= '<script src="'.find_in_path('tree_edite.js').'" type="text/javascript"></script>';
