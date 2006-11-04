c14ba71eab45bd147260f0ccfb392edb91a33b65
pierre.andrews@gmail.com <pierre.andrews@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-11-04 11:27:53 +0000

    merge de r6959, merci Cedric
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@6961 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/FpipR/1.9.1/FpipR.php
+++ _test_/FpipR/1.9.1/FpipR.php
@@ -2,67 +2,67 @@
 
 function FpipR_affiche_milieu($flux) {
   if($flux['args']['exec'] == 'auteurs_edit') {
-	global $table_prefix, $connect_id_auteur;
-
-	include_spip('base/abstract_sql');
-
-	//on crée la colonne pour stoquer les frobs
-	$installe = unserialize(lire_meta('FpipR:installe'));
-	if(!$installe) {
-	  spip_query("ALTER TABLE `".$table_prefix."_auteurs` ADD (`flickr_token` TINYTEXT NULL, `flickr_nsid` TINYTEXT NULL);");
-	  ecrire_meta('FpipR:installe',serialize(true)); //histoire de pas faire une recherche dans la base à chaque coup
-	  ecrire_metas();
-	}
-	if($connect_id_auteur == intval($flux['args']['id_auteur'])) {
-
-	  include_spip('inc/presentation');
-
-	  $html = '<div>&nbsp;</div>';
-	  $html .= '<div style="margin-top: 14px;" class="cadre-r">
-<div style="position: relative;">
-<div style="position: absolute; top: -16px; left: 10px;">
-<img src="'.find_in_path('fpipr.gif').'"/>
-</div>
-</div>
-<div style="overflow: hidden;" class="cadre-padding">';
-	  $html .= '<h3>'._T('fpipr:autorisation_titre').'</h3>';
-	  include_spip('inc/flickr_api');
-
-
-	  $from = array('spip_auteurs');
-	  $select = array('flickr_token','flickr_nsid');
-	  $where = array('id_auteur='.intval($flux['args']['id_auteur']));
-	  $rez = spip_abstract_select($select,$from,$where);
-	  $row = spip_abstract_fetch($rez);
-	  $wrong = false;
-	  if($row['flickr_nsid'] != '' && $row['flickr_token'] != '') {
-		$check = flickr_auth_checkToken($row['flickr_token']);
-		if($check) {
-		  $html .= _T('fpipr:identifie_ok',array('user_id'=>'<a href="http://www.flickr.com/photos/'.$row['flickr_nsid'].'">'.$row['flickr_nsid'].'</a>'));
-
-		  $html .= flickr_bookmarklet_info();
-		} else {
-		  include_spip('base/abstract_sql');
-		  global $table_prefix;
-		  spip_query("UPDATE ".$table_prefix."_auteurs SET flickr_nsid = '', flickr_token = '' WHERE id_auteur=$connect_id_auteur");
-		  $wrong = true;
+		global $table_prefix, $connect_id_auteur;
+	
+		include_spip('base/abstract_sql');
+	
+		//on crée la colonne pour stoquer les frobs
+		$installe = unserialize(lire_meta('FpipR:installe'));
+		if(!$installe) {
+		  spip_query("ALTER TABLE `".$table_prefix."_auteurs` ADD (`flickr_token` TINYTEXT NULL, `flickr_nsid` TINYTEXT NULL);");
+		  ecrire_meta('FpipR:installe',serialize(true)); //histoire de pas faire une recherche dans la base à chaque coup
+		  ecrire_metas();
 		}
-	  } else $wrong = true;
-	  if($wrong){
-		$infos = flickr_authenticate_get_frob();
-		$html .= '<ol><li>'.
-		  _T('fpipr:identifie_etape1',array('url'=>$infos['url'])).
-		  '</li>
-<li>'.
-		  _T('fpipr:identifie_etape2',array('form'=>generer_action_auteur('flickr_authenticate_end',$infos['frob'], generer_url_ecrire('auteurs_edit','id_auteur='.$connect_id_auteur,true),'<button type="submit">'._T('fpipr:terminer').'</button>'))).
-		  '</li></ol>';
-	  }
-	  spip_abstract_free($rez);
-	  $html .= fin_cadre_relief(true);
-	  $flux['data'].=$html;
-	}
-	return $flux;
+		if($connect_id_auteur == $flux['args']['id_auteur']) {
+	
+		  include_spip('inc/presentation');
+	
+		  $html = '<div>&nbsp;</div>';
+		  $html .= '<div style="margin-top: 14px;" class="cadre-r">
+	<div style="position: relative;">
+	<div style="position: absolute; top: -16px; left: 10px;">
+	<img src="'.find_in_path('fpipr.gif').'"/>
+	</div>
+	</div>
+	<div style="overflow: hidden;" class="cadre-padding">';
+		  $html .= '<h3>'._T('fpipr:autorisation_titre').'</h3>';
+		  include_spip('inc/flickr_api');
+	
+	
+		  $from = array('spip_auteurs');
+		  $select = array('flickr_token','flickr_nsid');
+		  $where = array('id_auteur='.$flux['args']['id_auteur']);
+		  $rez = spip_abstract_select($select,$from,$where);
+		  $row = spip_abstract_fetch($rez);
+		  $wrong = false;
+		  if($row['flickr_nsid'] != '' && $row['flickr_token'] != '') {
+			$check = flickr_auth_checkToken($row['flickr_token']);
+			if($check) {
+			  $html .= _T('fpipr:identifie_ok',array('user_id'=>'<a href="http://www.flickr.com/photos/'.$row['flickr_nsid'].'">'.$row['flickr_nsid'].'</a>'));
+	
+			  $html .= flickr_bookmarklet_info();
+			} else {
+			  include_spip('base/abstract_sql');
+			  global $table_prefix;
+			  spip_query("UPDATE ".$table_prefix."_auteurs SET flickr_nsid = '', flickr_token = '' WHERE id_auteur=$connect_id_auteur");
+			  $wrong = true;
+			}
+		  } else $wrong = true;
+		  if($wrong){
+			$infos = flickr_authenticate_get_frob();
+			$html .= '<ol><li>'.
+			  _T('fpipr:identifie_etape1',array('url'=>$infos['url'])).
+			  '</li>
+	<li>'.
+			  _T('fpipr:identifie_etape2',array('form'=>generer_action_auteur('flickr_authenticate_end',$infos['frob'], generer_url_ecrire('auteurs_edit','id_auteur='.$connect_id_auteur,true),'<button type="submit">'._T('fpipr:terminer').'</button>'))).
+			  '</li></ol>';
+		  }
+		  spip_abstract_free($rez);
+		  $html .= fin_cadre_relief(true);
+		  $flux['data'].=$html;
+		}
   }
+	return $flux;
 }
 
 function FpipR_affiche_gauche($flux) {
