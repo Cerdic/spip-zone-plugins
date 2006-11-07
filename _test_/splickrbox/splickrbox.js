59ab9a83ab613ce1c92e1950ab5509d669acd8a8
pierre.andrews@gmail.com <pierre.andrews@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-11-07 00:01:23 +0000

    un peu de commentaires du code js+on cree le div changeMe automatiquement
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@7056 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/splickrbox/splickrbox.js
+++ _test_/splickrbox/splickrbox.js
@@ -5,34 +5,38 @@
  * Fonctionne avec jQuery.
  **/
 
+//fonction gadget pour avoir le this du contexte au bon objet 
+//quand on fait des appels depuis des callbacks (setTimeout et autres events)
 function getObjectMethodClosure(object, method) {
 	return function(arg) {
 		return object[method](arg); 
 	}
 }	
 
-
-
+//Quand le document est pret, on lance le plugin sur chaque
+//div de class splickrbox
 $(document).ready(function(){
 		$(".splickrbox").splicker();
 	});
 
-
-
+//le plugin splicker, pour chaque image du div
+//on lui cree un objet SplickerBox
 jQuery.fn.splicker = function() {
 	return this.each(function() {
 			var img_cnt = $(this).find('img').size();
 			if(img_cnt > 0) {
 				var size = $(this).find('img').css('width').replace('px',"");
-				var box = new jQuery.SplickerBox(this,img_cnt,$(this).find('.changeMe'),size);
+				var box = new jQuery.SplickerBox(this,img_cnt,size);
 			}
 		});
 }
 
-jQuery.SplickerBox = function(e,m,changeMe,s) {
+//Constructeur de l'objet
+jQuery.SplickerBox = function(e,m,s) {
 	this.elt = e;
 	this.max = m;
-	this.c = changeMe;
+	$(this.elt).append('<div class="changeMe" style="position:absolute;left:0px;top:0px;"></div>');
+	this.c = $(this).find('.changeMe');
 	this.cptj = 0;
 	this.left = this.top=0;
 	if(s == 0 || s == 'auto')
@@ -42,30 +46,36 @@ jQuery.SplickerBox = function(e,m,changeMe,s) {
 	this.init()
 }
 
-
+//les methodes
 jQuery.SplickerBox.prototype = {
+	//on choisit la prochaine image
 	itere: function() {
 		this.cptj = Math.round(Math.random()*this.max) % this.max;
 		$("#statusMsg").html("it"+this.cptj+"=?"+this.max);
 	},
+	//fixer la taille des images à la moitiée de la taille réelle
+	//l'image doit être carree
 	init: function() {
 		$(this.elt).find('img').css({width:(this.cote/2) + "px",height:(this.cote/2) + "px", height: (this.cote/2) + "px",border:0});
 		$(this.c).css({width: this.cote + "px",height: this.cote + "px"});
 		this.start();
 	},
 	
+	//on lance la premiere vignette apres un temps random entre 0 et 2s
 	start: function() {
 		setTimeout(getObjectMethodClosure(this,'doyourstuff'),(Math.random()*2)*1000);
 	},
 
+	//iteration apres le timeout
 	postpone: function() {
 		this.itere();		
 		$(this.c).empty();
 		this.start();
 	},
 
+	//toute les bidouilles sur la taille etc
+	//pour l'animation
 	doyourstuff: function() {
-
 		var or = $(this.elt).find('img').get(this.cptj);
 		var image = or.cloneNode(true);
 		image.style.width="100%";
@@ -117,13 +127,11 @@ jQuery.SplickerBox.prototype = {
 		}
 		
 		$(this.c).fadeIn(2000);		
-
-
-		setTimeout(getObjectMethodClosure(this,'resize'),4000);
-
+		setTimeout(getObjectMethodClosure(this,'resize'),4000); 
 		setTimeout(getObjectMethodClosure(this,'postpone'),7000);
 
 	},
+	//on anime la grosse vignette
 	resize: function() {
 		var t = new Number(this.top);
 		var l = new Number(this.left);
