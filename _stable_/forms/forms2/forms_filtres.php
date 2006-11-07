bc9e4897537cf6543246eac50db816e6acf452b4
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-11-07 15:12:07 +0000

    styles du date picker
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@7072 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/forms/forms2/forms_filtres.php
+++ _stable_/forms/forms2/forms_filtres.php
@@ -61,6 +61,7 @@ include_spip("inc/forms");
 	}
 	function Forms_insert_head($flux){
 		$flux .= 	"<link rel='stylesheet' href='".find_in_path('spip_forms.css')."' type='text/css' media='all' />\n";
+		$flux .= 	"<link rel='stylesheet' href='"._DIR_PLUGIN_FORMS."img_pack/date_picker.css' type='text/css' media='all' />\n";
 		return $flux;
 	}
 	function Forms_header_prive($flux){
