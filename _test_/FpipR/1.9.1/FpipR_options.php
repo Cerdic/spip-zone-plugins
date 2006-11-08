3b6b94d06ada44811cf909bfad5b631eeecb7c2d
pierre.andrews@gmail.com <pierre.andrews@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-11-08 12:15:12 +0000

    merge des balises de tailles, d'existence, des titles dans les modeles et du calculs de taille dans FpipR_options
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@7104 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/FpipR/1.9.1/FpipR_options.php
+++ _test_/FpipR/1.9.1/FpipR_options.php
@@ -93,8 +93,16 @@ function FpipR_logo_owner($user_id,$server = '') {
 
 
 function FpipR_logo_photo($id_photo,$server,$secret,$taille='',$originalformat='jpg') {
-  if($id_photo && $server)
-	return '<img src="http://static.flickr.com/'.$server."/".$id_photo."_".$secret.($taille?"_$taille":'').'.'.(($taille=='o')?$originalformat:'jpg').'" />';
+  if($id_photo) {
+	$w = ($taille=='s')?75:FpipR_taille_photo($id_photo,$taille,'width');
+	$h = ($taille=='s')?75:FpipR_taille_photo($id_photo,$taille,'height');
+	if($server) {
+	  return '<img src="http://static.flickr.com/'.$server."/".$id_photo."_".$secret.($taille?"_$taille":'').'.'.(($taille=='o')?$originalformat:'jpg').'" width="'.$w.'" height="'.$h.'" style="width:'.$w.';height:'.$h.'"/>';
+	} else {
+	  $src = FpipR_taille_photo($id_photo,$taille,'source');
+	  return '<img src="'.$src.'" width="'.$w.'" height="'.$h.'" style="width:'.$w.';height:'.$h.'"/>';
+	}
+  }
   return NULL;
 }
 
@@ -273,5 +281,10 @@ function FpipR_getAuthToken() {
 	}
   }
   return NULL;
+}
+
+/*En attendant la svn*/
+function _q($q) {
+  return spip_abstract_quote($q);
 }
 ?>
