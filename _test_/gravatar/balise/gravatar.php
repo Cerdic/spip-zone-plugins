0b213fec52b47eaa30561c44c508b5cf8696d74c
tbeaumanoir@gmail.com <tbeaumanoir@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-11-09 22:33:06 +0000

    Mise à jour du plugin suite à différentes remarques : utilisation du cache dans var, utilisation de recuperer_page plutot que file_get_contents...
    Syntaxe un peu simplifiée :
    #GRAVATAR{email,taille,url_du_gravatar_par_defaut}
    
    Exemple :
    #GRAVATAR{#EMAIL,80,#URL_SITE_SPIP/images/gravatar.gif}
    
    Je n'ai finalement pas utilisé le |sinon pour l'image par défaut car je ne rentrais jamais dans cette condition (peut être à cause du délais de récuperer_page, ou peut être car je m'y suis mal pris ;)
    
    TODO : différencier quand gravatar.com est dans les choux, d'un gravatar qui n'existe pas
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@7143 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/gravatar/balise/gravatar.php
+++ _test_/gravatar/balise/gravatar.php
@@ -4,7 +4,7 @@
 	 * Gravatar : Globally Recognized AVATAR
 	 * Author : Thomas Beaumanoir
 	 * Clever Age <http://www.clever-age.com>
-	 * Copyright (c) 2006
+	 * Copyright (c) 2006 - Distribue sous licence GNU/GPL
 	 *
 	 **/
 
@@ -13,27 +13,25 @@ function balise_GRAVATAR($p) {
 }
 
 function balise_GRAVATAR_stat($args, $filtres) {
-	return array($args[0], $args[1], $args[2], $args[3], $args[4]);
+	return array($args[0], $args[1], $args[2]);
 }
 
-function balise_GRAVATAR_dyn($email, $gravatar_default, $size, $alt, $class) {
+function balise_GRAVATAR_dyn($email, $size, $gravatar_default) {
+	include_spip('inc/distant');
 	$md5_email = md5($email);
-	$gravatar_cache = _DIR_PLUGIN_GRAVATAR.'cache/'.$md5_email;
+	$gravatar_cache = sous_repertoire(_DIR_VAR, 'cache-gravatar').$md5_email;
 
 	if(!file_exists($gravatar_cache)) {
-		$str = @file_get_contents('http://www.gravatar.com/avatar.php?gravatar_id='.$md5_email.'&amp;size='.$size);
-		if(!$str) {
-			$gravatar_url = $gravatar_default;
-		} else {
+		$gravatar = recuperer_page('http://www.gravatar.com/avatar.php?gravatar_id='.$md5_email.'&amp;size='.$size);
+		if($gravatar) {
 			$file = fopen($gravatar_cache, "w+");
-			fputs($file, $str);
+			fputs($file, $gravatar);
 			fclose($file);
-			$gravatar_url = $gravatar_cache;
+		} else {
+			copy($gravatar_default, $gravatar_cache);
 		}
-	} else {
-		$gravatar_url = $gravatar_cache;
 	}
 
-	return '<img src="'.$GLOBALS['meta']['adresse_site'].'/'.$gravatar_url.'" width="'.$size.'" height="'.$size.'" alt="'.$alt.'" class="'.$class.'" />';
+	return '<img src="'.$GLOBALS['meta']['adresse_site'].'/'.$gravatar_cache.'" width="'.$size.'" height="'.$size.'" alt="" />';
 }
 ?>
