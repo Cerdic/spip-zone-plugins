9675ebcabedb0d8a8d0f49f60a5ceb50d5f2616c
pierre.andrews@gmail.com <pierre.andrews@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-11-09 22:34:07 +0000

    un argument inverse
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@7144 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/FpipR/trunk/boucle/flickr_photosets_getphotos.php
+++ _test_/FpipR/trunk/boucle/flickr_photosets_getphotos.php
@@ -9,9 +9,9 @@ function boucle_FLICKR_PHOTOSETS_GETPHOTOS_dist($id_boucle, &$boucles) {
 
   $possible_extras = array('license', 'owner_name', 'icon_server', 'original_format', 'last_update');
 
-  $arguments = array_merge(FpipR_utils_search_args_extras($boucle,$id_table,$possible_extras,array('id_photoset')),
+  $arguments = array_merge(FpipR_utils_search_args_extras($boucle,$id_table,array('id_photoset'),$possible_extras),
 						   FpipR_utils_search_criteres($boucle,$possible_criteres,$boucles,$id_boucle));
-  $boucle->hash = FpipR_utils_calculer_hash('flickr.photosets.getPhotos',$arguments,$boucle);
+  $boucle->hash = FpipR_utils_calculer_hash('flickr.photosets.getphotos',$arguments,$boucle);
   return calculer_boucle($id_boucle, $boucles); 
   }
 ?>
