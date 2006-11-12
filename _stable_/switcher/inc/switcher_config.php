82a583e7dc012bafca000a7436b328c71fe9874e
andre@alternatives.ca <andre@alternatives.ca@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-11-12 21:34:33 +0000

    C'est mieux sans accent...
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@7205 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/switcher/inc/switcher_config.php
+++ _stable_/switcher/inc/switcher_config.php
@@ -3,12 +3,12 @@ global $repertoire_squelettes_alternatifs;
 global $styleListeSwitcher;
 global $afficherSeulementPourLesAdmins;
 
-// Répertoire contenant les répertoires squelettes à tester
+// Repertoire contenant les repertoires squelettes a tester
 $repertoire_squelettes_alternatifs ='squelettes-test';
 
-// Style css associée à la liste déroulante
+// Style css associe a la liste deroulante
 $styleListeSwitcher="font-size: 10px;background-color: #FFF;color: #0C479D;border-top: 1px solid #CECECE; border-bottom: 2px solid #4A4A4A; border-left: 1px solid #CECECE; border-right: 1px solid #CECECE;margin:2px .5em;";
 
-// Booléen pour déterminer si seuls les admins ont le droit de jouer ou non
+// Booleen pour determiner si seuls les admins ont le droit de jouer ou non
 $afficherSeulementPourLesAdmins=TRUE;
 ?>
