7a5c22b7e371f880c2b0f91fe647773912c95b8c
renato@rezo.net <renato@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-11-12 20:37:13 +0000

    plugin allowing to index and deindex added tables; works best together with recherche_etendue; to be merged with
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@7202 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/indicizzazione_tabelle/exec/indicizza_tabella.php
+++ _dev_/indicizzazione_tabelle/exec/indicizza_tabella.php
@@ -9,117 +9,193 @@
  *
  */
 
-
-include_spip('inc/presentation');
-
 function exec_indicizza_tabella_dist() {
 	
-global $tables_principales,$INDEX_tables_interdites,$INDEX_elements_objet;
-include_spip("base/serial");
-
-$tabelle_standard = $tables_principales;
-//carica definizioni da mes_fonctions
-include_spip('mes_fonctions');
-
-//carica definizioni tabelle da plugin
-if (@is_readable(_DIR_SESSIONS."charger_plugins_fonctions.php")){ 
- 		        include_once(_DIR_SESSIONS."charger_plugins_fonctions.php"); 
-} 
+	global $tables_principales,$tables_auxiliaires,$INDEX_tables_interdites,$INDEX_elements_objet;
+	include_spip("base/serial");
+	include_spip("base/auxiliaires");
 	
-debut_page(_L('Indicizzazione tabelle esterne'), "administration", "");
+	//carica definizioni da mes_fonctions
+	include_spip('mes_fonctions');
+	
+	//carica definizioni tabelle da plugin
+	if (@is_readable(_DIR_SESSIONS."charger_plugins_fonctions.php")){ 
+	 		        include_once(_DIR_SESSIONS."charger_plugins_fonctions.php"); 
+	} 
 
-debut_gauche();
+	$tabelle_standard = array_merge($tables_principales,$tables_auxiliaires);
 
-debut_boite_info();
-echo propre(_L('Questa pagina permette di indicizzare una tabella.'));
+		
+	include_spip("inc/texte");
+	$ok = true;
+	$tabella = interdire_scripts(_request("tabella"));
+	//Controlli di sicurezza
+	if(!$tabella) {
+		indicizza_tabelle_debut_page();
+		echo "Errore: tabella non specificata. ";
+		indicizza_tabelle_fin_page();
+		die();
+	}
+	$azione = interdire_scripts(_request("action"));
+	//Controlli di sicurezza
+	if($azione!="indicizza" && $azione!="non_indicizza") {
+		indicizza_tabelle_debut_page();
+		echo "Errore: azione non specificata. ";
+		indicizza_tabelle_fin_page();
+		die();
+	}	
+	
+	//Verifiche esistenza definizione tabella e tabelle vietate
+	include_spip("inc/indexation");
+	if((!array_key_exists($tabella,$tabelle_standard) || in_array($tabella,$INDEX_tables_interdites)) ) {
+		indicizza_tabelle_debut_page();
+		echo "Impossibile eseguire azione $azione sulla tabella $tabella";
+		indicizza_tabelle_fin_page();
+		die();
+	}
+	
+	
+	if($ok===true) {
+		//Verifiche di congruenza azioni
+		if($azione=="indicizza" && in_array($tabella,liste_index_tables())) {
+			indicizza_tabelle_debut_page();
+			echo "La tabella $tabella &egrave; gi&agrave; indicizzata";
+			indicizza_tabelle_fin_page();	
+			die();
+		}
+		if($azione=="non_indicizza") {
+			if(!in_array($tabella,liste_index_tables())) {
+				indicizza_tabelle_debut_page();
+				echo "La tabella $tabella non &egrave; indicizzata";
+				indicizza_tabelle_fin_page();	
+				die();
+			} else {
+				indicizza_tabelle_remove_idx_field($tabella);
+				include_spip('inc/headers'); 
+				redirige_par_entete(generer_url_ecrire("tabelle_aggiuntive"));									
+			}
+		}
+		
+		
+		
+		include_spip("base/abstract_sql");
+		$descr = spip_abstract_showtable($tabella);
+		//Elimina chiave primaria
+		$chiavi = explode(",",$descr["key"]["PRIMARY KEY"]);
+		$campi = array_keys($descr["field"]);
+		$descr = array_diff($campi,$chiavi);
+	
+		if(_request("invia") && $azione=="indicizza") {
+			switch($azione) {
+				case "indicizza":
+					if(!indicizza_tabelle_verify_fields($tabella,$campi,$descr)) {
+						indicizza_tabelle_debut_page();
+						echo $campi;
+						indicizza_tabelle_fin_page();
+					} else {
+						indicizza_tabelle_add_idx_field($tabella);
+						indicizza_tabelle_set_points_fields($tabella,$campi);
+						include_spip('inc/headers'); 
+						redirige_par_entete(generer_url_ecrire("tabelle_aggiuntive"));					
+					}
+					break;
+			}
+			
+		} else {
+			indicizza_tabelle_debut_page();
+			$tabelle = array();
+			$tabelle[] = array("<strong>Nome Campo</strong>","<strong>Importanza</strong>");
+			echo "<form method='POST' action=''><div>";	
+			foreach($descr as $campo) {
+				$tabelle[] = array($campo,"<input type='text' name='campo[$campo]' value='5' />");
+			}
+			
+			if($tabelle)
+				echo afficher_liste_debut_tableau().afficher_liste(array('60%','40%'),$tabelle).afficher_liste_fin_tableau();
+		
+			echo "<input type='submit' value='Invia' name='invia' />";
+			echo "</div></form>";
+			indicizza_tabelle_fin_page();
+		}
+			
+	}
 
-fin_boite_info();
+}
 
-debut_droite();
+function indicizza_tabelle_debut_page() {
+	include_spip('inc/presentation');
 
-gros_titre("Indicizzazione tabelle esterne");
+	debut_page(_L('Indicizzazione tabelle esterne'), "administration", "");
+	
+	debut_gauche();
+	
+	debut_boite_info();
+	echo propre(_L('Questa pagina permette di indicizzare una tabella.'));
+	
+	fin_boite_info();
+	
+	debut_droite();
+	
+	gros_titre("Indicizzazione tabelle esterne");
+	
+	
+	debut_cadre_trait_couleur('','','',"Indicizza tabella");
 
+}
 
-debut_cadre_trait_couleur('','','',"Indicizza tabella");
+function indicizza_tabelle_fin_page() {
 
-include_spip("inc_texte");
-$ok = true;
-$tabella = interdire_scripts(_request("tabella"));
-//Controlli di sicurezza
-if(!$tabella) {echo "Errore";$ok=false;}
-else include_spip("inc/indexation");
-if($ok===true && (!array_key_exists($tabella,$tables_principales) || in_array($tabella,$INDEX_tables_interdites) ||
-		in_array($tabella,liste_index_tables()) ) ) {
-	echo "Impossibile indicizzare tabella $tabella";$ok=false;
+	fin_cadre_trait_couleur();
+	
+	fin_page();
+
+}
+
+function indicizza_tabelle_add_idx_field($tabella) {
+		spip_query("ALTER TABLE $tabella ADD COLUMN idx enum('', '1', 'non', 'oui', 'idx') NOT NULL default '';");
+		update_index_tables();		
+}
+
+function indicizza_tabelle_remove_idx_field($tabella) {
+		spip_query("ALTER TABLE $tabella DROP COLUMN idx;");
+		update_index_tables();		
 }
 
 
-if($ok===true) {
-	
-	include_spip("base/abstract_sql");
-	$descr = spip_abstract_showtable($tabella);
-	//Elimina chiave primaria
-	$chiavi = explode(",",$descr["key"]["PRIMARY KEY"]);
-	$campi = array_keys($descr["field"]);
-	$descr = array_diff($campi,$chiavi);
+function indicizza_tabelle_set_points_fields($tabella,$campi) {
+		global $INDEX_elements_objet;
 
-	if(_request("invia")) {
+		include_spip('inc/meta');
+		$INDEX_elements_objet["$tabella"] = $campi;
+		ecrire_meta('INDEX_elements_objet',serialize($INDEX_elements_objet));
+		ecrire_metas();			
+		
+		//Aggiorna tabella spip_index e meta index_table per indicizzazione
+		update_index_tables();
+}
+
+function indicizza_tabelle_verify_fields($tabella,$campi,$descr) {
 		//Verifica sicurezza
 		$ok = true;
 		$campi = _request("campo");
 		if(!is_array($campi)) {
 				$ok=false;
-				echo "Errore";			
+				$campi = "Errore";			
 		} else
 		foreach($campi as $nome => $val) {
 			if(!in_array($nome,$descr)) {
 				$ok=false;
-				echo "Campo ".interdire_scripts($nome)." inesistente";
+				$campi =  "Campo ".interdire_scripts($nome)." inesistente";
 				break;
 			} else {
 				$campi[$nome] = intval($val);
 				if(!$campi[$nome]) {
 					$ok=false;
-					echo "Errore";
+					$campi = "Errore";
 					break;				
 				}
 			}
 		}
-		
-		if($ok===true) {
-			spip_query("ALTER TABLE $tabella ADD COLUMN idx enum('', '1', 'non', 'oui', 'idx') NOT NULL default '';");
-			
-			include_spip('inc/meta');
-			$INDEX_elements_objet["$tabella"] = $campi;
-			ecrire_meta('INDEX_elements_objet',serialize($INDEX_elements_objet));
-			ecrire_metas();			
-			
-			//Aggiorna tabella spip_index e meta index_table per indicizzazione
-			update_index_tables();
-			redirige_par_entete(generer_url_ecrire("tabelle_aggiuntive"));
-		}
-
-	} else {
-		$tabelle = array();
-		$tabelle[] = array("<strong>Nome Campo</strong>","<strong>Importanza</strong>");
-		echo "<form method='POST' action=''><div>";	
-		foreach($descr as $campo) {
-			$tabelle[] = array($campo,"<input type='text' name='campo[$campo]' value='5' />");
-		}
-		
-		if($tabelle)
-			echo afficher_liste_debut_tableau().afficher_liste(array('60%','40%'),$tabelle).afficher_liste_fin_tableau();
-	
-		echo "<input type='submit' value='Invia' name='invia' />";
-		echo "</div></form>";
-	}
-		
+		return $ok; 		
 }
-
-fin_cadre_trait_couleur();
-
-fin_page();
-
-}
-
 ?>
--- _dev_/indicizzazione_tabelle/exec/tabelle_aggiuntive.php
+++ _dev_/indicizzazione_tabelle/exec/tabelle_aggiuntive.php
@@ -16,10 +16,12 @@ include_spip('inc/presentation');
 
 function exec_tabelle_aggiuntive_dist() {
 	
-global $tables_principales,$INDEX_tables_interdites,$INDEX_elements_objet;
+global $tables_principales,$tables_auxiliaires,$INDEX_tables_interdites,$INDEX_elements_objet;
 include_spip("base/serial");
+include_spip("base/auxiliaires");
 
-$tabelle_standard = $tables_principales;
+$tabelle_standard = $tables_principales;//array_merge($tables_principales,$tables_auxiliaires);
+
 //carica definizioni da mes_fonctions
 include_spip('mes_fonctions');
 
@@ -27,7 +29,9 @@ include_spip('mes_fonctions');
 if (@is_readable(_DIR_SESSIONS."charger_plugins_fonctions.php")){ 
  		        include_once(_DIR_SESSIONS."charger_plugins_fonctions.php"); 
 } 
-	
+
+$tabelle_definite = $tables_principales;//array_merge($tables_principales,$tables_auxiliaires);
+
 debut_page(_L('Indicizzazione tabelle esterne'), "administration", "");
 
 debut_gauche();
@@ -35,17 +39,18 @@ debut_gauche();
 debut_boite_info();
 echo propre(_L('Questa pagina permette di selezionare le tabelle da indicizzare.'));
 echo "<h3>Legenda Stato Tabella</h3>";
-echo "<ul style='margin:0;padding:10px;'><li><strong>Tabella non definita in SPIP</strong>: la tabella non &egrave; utilizzabile in un ciclo e quindi &egrave; inutile indicizzarla</li>";
+echo "<ul style='margin:0;padding:10px;'>";
+echo "<li><strong>Tabella non definita in SPIP</strong>: la tabella non &egrave; utilizzabile in un ciclo e quindi &egrave; inutile indicizzarla</li>";
 echo "<li><strong>Tabella esclusa dall'indicizzazione</strong>: la tabella non &egrave; indicizzabile</li>";
 echo "<li><strong>Tabella non indicizzata</strong>: la tabella non &egrave; indicizzata ma &egrave; possibile indicizzarla</li>";
 echo "<li><strong>Nessun campo indicizzato</strong>: la tabella &egrave; indicizzata ma non &egrave; configurato alcun campo per l'indicizzazione</li>";
 echo "<li><strong>Tabella indicizzata</strong>: la tabella &egrave; indicizzata regolarmente</li></ul>";
 
 echo "<h3>Legenda Azioni</h3>";
-//echo "<ul style='margin:0;padding:10px;'><li><strong>Definisci</strong>: rende la tabella utilizzabile in un ciclo</li>";
+echo "<ul style='margin:0;padding:10px;'><li><strong>Definisci</strong>: rende la tabella utilizzabile in un ciclo</li>";
 echo "<ul><li><strong>Indicizza</strong>: Indicizza la tabella</li>";
 echo "<li><strong>Aggiungi campi</strong>: definisce i campi da incidizzare per la tabella</li>";
-echo "<li><strong>Non indicizza</strong>: Non indicizza la tabella e ne elimina i dati di indicizzazione</li></ul>";
+echo "<li><strong>Elimina indice</strong>: Non indicizza la tabella e ne elimina i dati di indicizzazione</li></ul>";
 
 
 fin_boite_info();
@@ -68,29 +73,31 @@ $tabelle=array();
 
 $tabelle[]= array("<strong>Nome tabella</strong>","<strong>Stato tabella</strong>","<strong>Azioni</strong>");
 if($tabelle_db) {
-	while($tab=spip_fetch_array($tabelle_db)) {
+	while($tab=spip_fetch_array($tabelle_db,SPIP_BOTH)) {
 			$stato = "";
 			$idx = "";
 			$azioni ="";
-			if(array_key_exists($tab[0],$tabelle_standard)) continue;
-			if(!array_key_exists($tab[0],$tables_principales)) {
+			//Non enumerare le tabelle ausiliarie
+			if(array_key_exists($tab[0],$tables_auxiliaires)) continue;
+			if(!array_key_exists($tab[0],$tabelle_definite)) {
 				$stato = "nodef";
-			} else {
-				if(in_array($tab[0],$INDEX_tables_interdites)) {
-					$stato = "vietata";
-				} else
-				if(in_array($tab[0],$lista_tabelle_indicizzate)) {
-					if(array_key_exists($tab[0],$INDEX_elements_objet)) {
-						$stato ="ind";
-					} else {
-						$stato = "nocampiind";
-					}
+				//$tabelle_standard[$tab[0]] = spip_abstract_showtable($tab[0],'',"true");
+			} else
+			if(in_array($tab[0],$INDEX_tables_interdites)) {
+				$stato = "vietata";
+			} else
+			if(in_array($tab[0],$lista_tabelle_indicizzate)) {
+				if(array_key_exists($tab[0],$INDEX_elements_objet)) {
+					$stato ="ind";
 				} else {
-					$stato = "noind";
+					$stato = "nocampiind";
 				}
+			} else {
+				$stato = "noind";
 			}
 
 		switch($stato) {
+			
 			case "nodef": 
 				$idx = "<span style='color:red'>tabella non definita in spip</span>";
 				//$azioni ="<a href='#'>definisci</a>";
@@ -102,7 +109,8 @@ if($tabelle_db) {
 				break;
 			case "noind":
 				$idx = "<span style='color:red'>tabella non indicizzata</span>";
-				if (!array_key_exists($tab[0],$tabelle_standard)) $azioni ="<a href='".generer_url_ecrire("indicizza_tabella","tabella=".rawurlencode($tab[0]))."'>indicizza</a>";
+				//if (!array_key_exists($tab[0],$tabelle_standard)) $azioni ="<a href='".generer_url_ecrire("indicizza_tabella","tabella=".rawurlencode($tab[0]))."'>indicizza</a>";
+				$azioni ="<a href='".generer_url_ecrire("indicizza_tabella","tabella=".rawurlencode($tab[0]))."&amp;action=indicizza'>indicizza</a>";
 				break;
 			case "nocampiind":
 				$idx = "nessun campo indicizzato";
@@ -110,7 +118,8 @@ if($tabelle_db) {
 				break;
 			case "ind":
 				$idx = "tabella indicizzata";
-				//if (!array_key_exists($tab[0],$tabelle_standard)) $azioni ="<a href='#'>non indicizza</a>";
+				//if (!array_key_exists($tab[0],$tabelle_standard)) $azioni ="<a href='#'>elimina indice</a>";
+				$azioni ="<a href='".generer_url_ecrire("indicizza_tabella","tabella=".rawurlencode($tab[0]))."&amp;action=non_indicizza'>elimina indice</a>";
 				break;
 		}
 		
