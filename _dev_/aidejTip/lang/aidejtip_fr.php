01dee60c0f17f9cecc6f032be43f773cda2acf71
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-11-13 16:48:52 +0000

    Correction sur 2 accents mal encodés
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@7236 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/aidejTip/lang/aidejtip_fr.php
+++ _dev_/aidejTip/lang/aidejtip_fr.php
@@ -26,12 +26,12 @@ Peut &ecirc;tre utiliser pour pr&eacute;ciser le titre en lui fournissant un com
 Il sera utilis&eacute; pour tous les liens vers l\'article dans le site (plan du site, dans la m&ecirc;me rubrique, quoi de neuf...) comme bulle d\'aide.
 _ Cela permet de pr&eacute;ciser le sens du titre.
 
-Si le Chapo n\'est pas rempli, alors, c\'est le descriptif rapide qui sera affich&eacute; &grave; sa place.
+Si le Chapo n\'est pas rempli, alors, c\'est le descriptif rapide qui sera affich&eacute; &agrave; sa place.
 
 De plus, ce texte est index&eacute; par les moteurs de recherche ce qui ne peut qu\'am&eacute;liorer le r&eacute;f&eacute;rencement du site.',
 'aide_texte' => 'La partie principale de la page.
 
 N\'oubliez pas que la mise en forme doit &ecirc;tre au service du {{sens du texte}} et non de votre go&ucirc;t artistique personnel !',
-'aide_ps' => '&Agrave; utiliser pour donner un compl&eacute;ment d\'information sur l\'article dans un cadre plac&eacute; &grave; la fin de celui-ci.',
+'aide_ps' => '&Agrave; utiliser pour donner un compl&eacute;ment d\'information sur l\'article dans un cadre plac&eacute; &agrave; la fin de celui-ci.',
 );
 ?>
