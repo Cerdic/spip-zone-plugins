0113a420e3faff3d25ea77c95f30995eedc05747
bertrand@toggg.com <bertrand@toggg.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-11-14 19:38:44 +0000

    Just a try, hope some true talkers will correct it
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@7301 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _dev_/widgets/lang/widgets_en.php
@@ -0,0 +1,21 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	'annuler' => 'Cancel',
+	'editer_@type@_@id@' => 'Edit @type@ @id@ as whole',
+	'deja_modifie' => 'Already changed the same',
+	'donnees_mal_formatees' => 'Request format is not correct',
+	'editer' => 'Edit',
+	'editer_tout' => 'Edit as a whole',
+	'modifie_par_ailleurs' => 'Was changed elsewhere in mean time',
+	'non_autorise' => 'Not allowed',
+	'non_implemente' => 'Not implemented',
+	'pas_de_modification' => 'No change',
+	'pas_de_valeur' => 'No value for this id',
+	'sauvegarder' => 'Save changes anyway ?',
+	'svp_copier_coller' => 'Sorry, copy/paste your changes and retry',
+	'veuillez_patienter'=>'Searching ...'
+	);
+?>
