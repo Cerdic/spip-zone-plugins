c304879fb6fa9a76c147c729e7bdd0da5bcdf208
maieulrouquette2@tele2.fr <maieulrouquette2@tele2.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-11-14 11:27:56 +0000

    mot croisés s'ouvre à l'international (c'est la lutte final !)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@7274 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _stable_/mot-croises/lang/motscroises_it.php
@@ -0,0 +1,21 @@
+<?php
+
+// This is a SPIP language file  --  Questo è un file di lingua di  SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+'afficher_solution' 	=>'Visualizza la soluzione (in fondo alla pagina)',
+'verifier' 		=> 'Controlla',
+'horizontalement' 	=> 'Orizzontali',
+'verticalement'		=> 'Verticali',
+'solution'		=>'Soluzione',
+'nombre_erreurs'	=>'@nbr@ errori',
+'aucune_erreur'		=>'Nessun errore',
+'1erreur'		=>'Un errore',
+'table_summary'		=>'Schema di parole crociate di @largeur@ colonne e @hauteur@ righe',
+'ligne'			=>'riga @n@',
+'colonne'		=>'colonna @n@'
+);
+
+
+?>
