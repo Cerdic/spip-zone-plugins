247b1fa9c2e3d247ee5202c7cf39f8c45c8e96c5
renato@rezo.net <renato@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-11-14 15:29:04 +0000

    update of the strings
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@7287 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/widgets/lang/widgets_it.php
+++ _dev_/widgets/lang/widgets_it.php
@@ -3,16 +3,19 @@
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-    'editer' => 'Modifica',
+    'annuler' => 'Annulla',
+    'editer_@type@_@id@' => 'Modifica @type@ @id@ dalla redazione',
     'deja_modifie' => 'Gi&agrave; modificato',
-    'svp_copier_coller' =>
-        'Spiacente, copia ed incolla le tue modifiche e ricomincia',
     'donnees_mal_formatees' => 'Dati mal formattati',
-    'non_autorise' => 'Non autorizzato',
+		'editer' => 'Modifica',
+		'editer_tout' => 'Modifica tutto',
     'modifie_par_ailleurs' => 'Modificato da altri',
-    'pas_de_modification' => 'Nessun cambiamento',
+    'non_autorise' => 'Non autorizzato',
     'non_implemente' => 'Non implementato',
+    'pas_de_modification' => 'Nessun cambiamento',
     'pas_de_valeur' => 'Nessun valore',
     'sauvegarder' => 'Salvare le modifiche?'
+    'svp_copier_coller' => 'Spiacente, copia ed incolla le tue modifiche e ricomincia',
+    'veuillez_patienter'=>'Attendere prego...'
     );
 ?>
