48df186b6c4e7662cd69c71e0f9aaf7fc50bab56
renato@rezo.net <renato@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-11-14 14:43:08 +0000

    italian strings
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@7280 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _dev_/widgets/lang/widgets_it.php
@@ -0,0 +1,18 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+    'editer' => 'Modifica',
+    'deja_modifie' => 'Gi&agrave; modificato',
+    'svp_copier_coller' =>
+        'Spiacente, copia ed incolla le tue modifiche e ricomincia',
+    'donnees_mal_formatees' => 'Dati mal formattati',
+    'non_autorise' => 'Non autorizzato',
+    'modifie_par_ailleurs' => 'Modificato da altri',
+    'pas_de_modification' => 'Nessun cambiamento',
+    'non_implemente' => 'Non implementato',
+    'pas_de_valeur' => 'Nessun valore',
+    'sauvegarder' => 'Salvare le modifiche?'
+    );
+?>
