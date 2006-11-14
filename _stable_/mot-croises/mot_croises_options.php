5e7953761799c5d124e0aec1b4eea55935d2b508
patfr@ifrance.com <patfr@ifrance.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-11-14 14:57:05 +0000

    introduction() en question...
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@7281 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/mot-croises/mot_croises_options.php
+++ _stable_/mot-croises/mot_croises_options.php
@@ -1,6 +1,9 @@
 <?php
-function introduction ($type, $texte, $chapo='', $descriptif='') {
 
+// on ne sait jamais ! mais cette surcharge doit être étudiée...
+if (!function_exists("introduction")) {	
+
+  function introduction ($type, $texte, $chapo='', $descriptif='') {
 
 	switch ($type) {
 		case 'articles':
@@ -9,9 +12,7 @@ function introduction ($type, $texte, $chapo='', $descriptif='') {
 			else if (substr($chapo, 0, 1) == '=')	// article virtuel
 				return '';
 			else
-				
 				return PtoBR(propre(supprimer_tags(couper_intro(pas_de_grille_introduction($chapo."\n\n\n".$texte), 500))));
-				
 			break;
 		case 'breves':
 			return PtoBR(propre(supprimer_tags(couper_intro(pas_de_grille_introduction($texte), 300))));
@@ -26,11 +27,14 @@ function introduction ($type, $texte, $chapo='', $descriptif='') {
 				return PtoBR(propre(supprimer_tags(couper_intro(pas_de_grille_introduction($texte), 600))));
 			break;
 	}
-}
+	
+  }  // function introduction
+}	// if exists
 
-function pas_de_grille_introduction($texte)
-	{$texte = preg_replace(',<(grille)>(.*)<\/\1>,UimsS','',$texte);
+function pas_de_grille_introduction($texte) {
+	$texte = preg_replace(',<(grille)>(.*)<\/\1>,UimsS','',$texte);
 	$texte = preg_replace(',<(horizontal)>(.*)<\/\1>,UimsS','',$texte);
 	$texte = preg_replace(',<(vertical)>(.*)<\/\1>,UimsS','',$texte);
-	return $texte;}
+	return $texte;
+}
 ?>
