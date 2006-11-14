b4ce99ebc753105a657ab5df44a8da79c95e4ae3
bertrand@toggg.com <bertrand@toggg.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-11-14 18:20:14 +0000

    widgets deutsch komplet, la virgule en trop a la fin est correcte, php s'en fout et ca evite des bugs (j'ai honte)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@7299 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/widgets/lang/widgets_de.php
+++ _dev_/widgets/lang/widgets_de.php
@@ -3,16 +3,19 @@
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-    'editer' => 'Inhalt &auml;ndern',
-    'deja_modifie' => 'Schon ge&auml;ndert',
-    'svp_copier_coller' =>
-        'Bitte, &auml;nderungen sicher stellen und wiederholen',
-    'donnees_mal_formatees' => 'Anforderung fehlerhaft formartiert',
-    'non_autorise' => 'Fehlende Rechte',
-    'modifie_par_ailleurs' => 'Wurde von jemand anderem bereits ge&auml;ndert',
-    'pas_de_modification' => 'Keine &Auml;nderung',
-    'non_implemente' => 'Nicht implementiert',
-    'pas_de_valeur' => 'Inhalt existiert nicht',
-    'sauvegarder' => '&Auml;nderungen erfassen ?'
-    );
+	'annuler' => 'Widerrufen',
+	'editer_@type@_@id@' => '@type@ @id@ voll &auml;ndern',
+	'deja_modifie' => 'Schon ge&auml;ndert',
+	'donnees_mal_formatees' => 'Anforderung fehlerhaft formartiert',
+	'editer' => 'Inhalt &auml;ndern',
+	'editer_tout' => 'Voll &auml;ndern',
+	'modifie_par_ailleurs' => 'Wurde von jemand anderem bereits ge&auml;ndert',
+	'non_autorise' => 'Fehlende Rechte',
+	'non_implemente' => 'Nicht implementiert',
+	'pas_de_modification' => 'Keine &Auml;nderung',
+	'pas_de_valeur' => 'Inhalt existiert nicht',
+	'sauvegarder' => '&Auml;nderungen erfassen ?',
+	'svp_copier_coller' => 'Bitte, &auml;nderungen sicher stellen und wiederholen',
+	'veuillez_patienter'=>'Bitte warten ...',
+	);
 ?>
