8c573eb9973869c82ab21a4c67fb26ef300b3fa5
bertrand@toggg.com <bertrand@toggg.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-11-14 14:22:32 +0000

    Langue francaise complete
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@7279 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/widgets/lang/widgets_fr.php
+++ _dev_/widgets/lang/widgets_fr.php
@@ -3,16 +3,19 @@
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-    'editer' => '&Eacute;diter',
+	'annuler' => 'Annuler',
+	'editer_@type@_@id@' => '&Eacute;diter @type@ @id@ en entier',
     'deja_modifie' => 'D&eacute;j&agrave; modifi&eacute;',
-    'svp_copier_coller' =>
-        'D&eacute;sol&eacute;, copiez/collez vos changements et recommencez',
     'donnees_mal_formatees' => 'Donn&eacute;es mal format&eacute;es',
-    'non_autorise' => 'Non autoris&eacute;',
+    'editer' => '&Eacute;diter',
+    'editer_tout' => '&Eacute;diter tout',
     'modifie_par_ailleurs' => 'Modifi&eacute; par ailleurs',
-    'pas_de_modification' => 'Pas de modification',
+    'non_autorise' => 'Non autoris&eacute;',
     'non_implemente' => 'Non impl&eacute;ment&eacute;',
+    'pas_de_modification' => 'Pas de modification',
     'pas_de_valeur' => 'Pas de valeur',
-    'sauvegarder' => 'Sauvegarder les modifications ?'
+    'sauvegarder' => 'Sauvegarder les modifications ?',
+    'svp_copier_coller' => 'D&eacute;sol&eacute;, copiez/collez vos changements et recommencez',
+    'veuillez_patienter'=>'Veuillez patienter...'
     );
 ?>
