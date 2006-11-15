63c42112e6a1f6ea70348cd930dff2b1666a3c30
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-11-15 15:08:22 +0000

    Le script bash fonctionne maintenant reste à trouver comment utiliser ce maudi pipeline affiche_droite
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@7340 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/spipmotion/script_bash/spipmotion.sh
+++ _dev_/spipmotion/script_bash/spipmotion.sh
@@ -8,65 +8,21 @@
 version="0.1"
 
 ################ LOCALISATION #####################
-
-################### English ########################
-messageaide="
-spipmotion $version
-
-Usage: ./spipmotion args
-where args must include source video (--e) and output video (--i) and possibly
-* the conversion mode (--m) : m will use mplayer and f will use ffmpeg only
-	m is slow, but light. Accepted sizes are the sizes supported by your version of mplayer
-	f is quick but heavy. Accepted sizes are the sizes which ffmpeg was compiled
-	-> default is m
-* the path to the binary ffmpeg (--p). default is /usr/local/bin/ffmpeg.
-
-Example :
-./spipmotion --e input-video.avi --s output-video.flv --mode m --p /usr/local/bin/ffmpeg
-
-####################################################
-##       This program need a ffmpeg version       ##
-##         compiled with mp3lame support !        ##
-## See http://kent1.sklunk.net/spip.php?article71 ##
-####################################################
-"
-		formatsortie="spipmotion: outfile name must end in .flv"
-		mauvaisarg="spipmotion: unrecognised argument ${1}
-For a proper use try : \"./spipmotion --help\""
-		pasvideoentree="spipmotion: no source video specified.
-For a proper use try : \"./spipmotion --help\""
-		pasvideosortie="spipmotion: no output video specified.
-For a proper use try : \"./spipmotion --help\""
-		extractvid="Extracting Video"
-		extractson="Extracting Sound"
-		assemblage="Converting to flv"
-		titredejala="Output file already exists"
-		textedejala="The output file you specified already exists. 
-Do you want to overwrite it ? 
-If no, it will be renamed."
-		oui="yes"
-		non="no"
-		succes="Success! Video has been converted in flv format!"
-		nonamr="It seems that your version of ffmpeg was not compiled with AMR support. This program needs AMR support to work properly.
-For a proper use try : \"./spipmotion --help\""
-
-	case $LANG in
-################### Français ######################
-		fr* )
 messageaide="
 spipmotion $version
 
 Utilisation : ./spipmotion arguments
 ou arguments doit inclure la vidéo source et la vidéo de sortie au format flv et éventuellement :
-* le mode de conversion : m se servira de mplayer et f n'utilisera que ffmpeg
-	m est lent mais donne des fichiers légers. Les formats acceptés sont ceux supportés par votre version de mplayer
-	f est plus rapide mais donne des fichiers plus lourd. Les formats acceptés sont ceux avec lesquels vous avez compilé ffmpeg
-	-> par défaut, m est selectionné
+* la taille de la video ex : --size 320x240
+* le bitrate de la video ex : --bitrate 448
+* le nombre d'image par seconde ex : --fps 15
+* le bitrate audio ex : --audiobitrate 64
+* la fréquence d'echantillonnage sonnore ex : --bitrate 22050
 * le chemin vers l'executable ffmpeg (--p). /usr/local/bin/ffmpeg est la valeur par défaut.
 
 
 Exemple :
-./spipmotion --e video-entree.avi --s video-sortie.flv --mode m --p /opt/ffmep/bin/ffmpeg
+./spipmotion --e video-entree.avi --s video-sortie.flv --size 320x240 --bitrate 448 --fps 15 --audiobitrate 64 --audiofreq 22050 --p /usr/local/bin/ffmpeg
 
 #####################################################
 ##  Ce programme recquiert une version de ffmpeg   ##
@@ -93,7 +49,6 @@ Si non, le fichier déjà présent sera renommé."
 		succes="Succès ! La vidéo a bien été convertie en flv !"
 		nonamr="Il apparaît que votre version de ffmpeg n'a pas été compilée avec le support d'AMR. Ce programme en a besoin pour fonctionner correctement
 Pour visualiser le manuel de spipmotion, faîtes : \"./spipmotion --help\""
-	esac
 
 #################################################
 
@@ -112,7 +67,15 @@ while test -n "${1}"; do
 			exit 1;;
 			esac
 		shift;;
-		--mode) mode="${2}"
+		--size) size="${2}"
+		shift;;
+		--bitrate) bitrate="${2}"
+		shift;;
+		--audiobitrate) audiobitrate="${2}"
+		shift;;
+		--audiofreq) audiofreq="${2}"
+		shift;;
+		--fps) fps="${2}"
 		shift;;
 		--p) chemin="${2}"
 		shift;;
@@ -131,29 +94,13 @@ case "$sortie" in
   "") "$sortie" = "$entree.flv"
 esac
 
-case "$mode" in
-  "") "$mode" = "m"
-esac
-
 case "$chemin" in
   "") chemin="/usr/local/bin/ffmpeg"
 esac
 
-
-################# AMR OU PAS ? ##################
-amroupas=`$chemin -version 2>&1`
-
-if echo $amroupas | grep -v amr
-then
-echo "###################################################"
-echo $nonamr;
-echo "###################################################"
-exit;
-fi
-
 ########### SI LA SORTIE EXISTE DÉJÀ #############
 
-	if [ -f $sortie ];
+if [ -f $sortie ];
 	then
 	PS3='> '
 	echo "###############################################################"
@@ -171,45 +118,11 @@ fi
 	        ;;
 	    esac
 	done
-	fi
-
-
-############# SI ON UTILISE FFMPEG ################
-if [ "$mode" = "f" ]
-then
-echo "
-#######################
-## "$assemblage" ##
-#######################
-"
-echo "ah$chemin$mode"
-"$chemin" -i $entree -acodec amr_nb -s 176x144 -ar 8000 -b 80 -vcodec h263 -ac 1 -y $sortie
-else
-
-################# SINON, MPLAYER ##################
-
-
-echo "
-#######################
-## "$extracvid"  ##
-#######################
-"
-mencoder $entree -nosound -ovc lavc -lavcopts vcodec=mpeg4 -vop expand=176:144,scale=176:-2 -o sortie.avi -ofps 12
-echo "
-#######################
-## "$extractson"  ##
-#######################
-"
-mplayer -vo null -ao pcm -af resample=8000,volume=+4db:sc $entree
-echo "
-#######################
-## "$assemblage" ##
-#######################
-"
-"$chemin" -i sortie.avi -i audiodump.wav -b 48 -ac 1 -ab 12 -map 0.0 -map 1.0 -y $sortie
-rm sortie.avi
-rm audiodump.wav
 fi
 
+############# ON UTILISE FFMPEG ################
+
+echo "ah$chemin"
+"$chemin" -i $entree -acodec mp3 -f flv -s $size -b $bitrate.kb -ab $audiobitrate -ar $audiofreq -r $fps -y $sortie
+
 echo "$succes"
-echo "http://www.aozeo.com/"
