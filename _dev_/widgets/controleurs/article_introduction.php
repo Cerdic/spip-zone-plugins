35f1fa85d0364c8a77411d2557c79d4b94de8263
bertrand@toggg.com <bertrand@toggg.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-11-16 17:26:57 +0000

    Rangement et il manquait le largeur, en commentaire anyway
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@7384 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/widgets/controleurs/article_introduction.php
+++ _dev_/widgets/controleurs/article_introduction.php
@@ -10,23 +10,25 @@ function controleurs_article_introduction_dist($regs) {
 	$n = new Widget('article-introduction-' . $id, $valeur,
 			array('hauteurMini' => 234));
     
-// en utilisant les inputs défauts
+/* en utilisant les inputs défauts
     $inputAttrs = array(
     	'descriptif' => array('type' => 'texte', 'attrs' => array(
-	        'style' => "width:${w}px; height:" . (int)ceil($n->hauteur*2/13) . "px;")),
+	        'style' => 'height:' . (int)ceil($n->hauteur*2/13) . 'px;')),
 		'chapo' =>  array('type' => 'texte', 'attrs' => array(
-	        'style' => "width:${w}px; height:" . (int)ceil($n->hauteur*4/13) . "px;")),
+	        'style' => 'height:' . (int)ceil($n->hauteur*4/13) . 'px;')),
 		'texte' =>  array('type' => 'texte', 'attrs' => array(
-	        'style' => "width:${w}px; height:" . (int)ceil($n->hauteur*4/13) . "px;")));
-//	$n = new Widget('article-introduction-' . $id, $valeur);
+	        'style' => 'height:' . (int)ceil($n->hauteur*4/13) . 'px;')));
+    $html = $n->formulaire($inputAttrs,
+    					array('style' => 'width:' . $n->largeur . 'px;'));
+*/
 
 // pour la methode par modeles
     $contexte = array(
     	'h_descriptif' => (int)ceil($n->hauteur*2/13),
 		'h_chapo' => (int)ceil($n->hauteur*4/13),
 		'h_texte' => (int)ceil($n->hauteur*4/13));
-        $html = $n->formulaire($contexte);
-        $status = NULL;
+    $html = $n->formulaire($contexte);
+    $status = NULL;
 
 	return array($html, $status);
 }
