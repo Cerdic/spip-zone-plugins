ba9797f5c8362b2631f504e2858de6e925f9425c
joseluis@digital77.com <joseluis@digital77.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-11-17 23:20:27 +0000

    actualizado para 1.9.1
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@7422 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/enviar_email/baliza_enviar_email.php
+++ _test_/enviar_email/baliza_enviar_email.php
@@ -3,20 +3,36 @@
 function preparar_enlace_enviar($arg){
 
 // funcion para ventana pop-up centrada en la pantalla
-$javascript_centrar_enviar = "<script language=\"JavaScript1.2\">	/*	Open Centered Popup Window Script-	© DHTMLShock (www.dhtmlshock.com)	To add more shock to your site, visit www.DHTMLShock.com	*/	function centrarVentana(theURL,winName,features, myWidth, myHeight, isCenter) { //v3.0		if(window.screen)if(isCenter)if(isCenter==\"true\"){		var myLeft = (screen.width-myWidth)/2;		var myTop = (screen.height-myHeight)/2;		features+=(features!='')?',':'';		features+=',left='+myLeft+',top='+myTop;	}	window.open(theURL,winName,features+((features!='')?',':'')+'width='+myWidth+',height='+myHeight);	}</script>";
+$javascript_centrar_enviar = "<script language=\"JavaScript1.2\">
+	/*
+	Open Centered Popup Window Script-
+	© DHTMLShock (www.dhtmlshock.com)
+	To add more shock to your site, visit www.DHTMLShock.com
+	*/
+	function centrarVentana(theURL,winName,features, myWidth, myHeight, isCenter) { //v3.0
+		if(window.screen)if(isCenter)if(isCenter==\"true\"){
+		var myLeft = (screen.width-myWidth)/2;
+		var myTop = (screen.height-myHeight)/2;
+		features+=(features!='')?',':'';
+		features+=',left='+myLeft+',top='+myTop;
+	}
+	window.open(theURL,winName,features+((features!='')?',':'')+'width='+myWidth+',height='+myHeight);
+	}
+</script>";
 
 // prepara logo
 		$logo = find_in_path('sobre.gif');
 		$url = generer_url_public('',$arg);
 		
 // prepara el enlace completo
-		$enlace = $javascript_centrar_enviar."<a href=\"javascript:;\" onClick=\"centrarVentana('$url','Enviar_documento','scrollbars=yes,resizable=yes','440','440','true')\" title=\"Enviar la referencia de este documento por email\" border=\"0\"><img src=\"$logo\" border=\"0\" /> Enviar por email</a>";
+		$enlace = $javascript_centrar_enviar."<a href=\"javascript:;\" onClick=\"centrarVentana('$url','Enviar_documento','scrollbars=yes,resizable=yes','440','460','true')\" title=\"Enviar la referencia de este documento por email\" border=\"0\"><img src=\"$logo\" border=\"0\" /> Enviar por email</a>";
 		return $enlace;
 
 }
 
 // la baliza a llamar como #ENVIAR_EMAIL** (importante los dos asteriscos para que funcione el javascript anterior de centrar la ventan por-up)
-function balise_ENVIAR_EMAIL($p) {
+
+function balise_ENVIAR_EMAIL($p) {
 
 // numero y enlace al artículo o breve
   	$_id_article = champ_sql('id_article', $p);
@@ -26,6 +42,10 @@ $javascript_centrar_enviar = "<script language=\"JavaScript1.2\">	/*	Open Cent
 		$_id_breve = champ_sql('id_breve', $p);
 		$arg = "'page=enviar_email_breve&amp;id_breve='.".$_id_breve;
 		$url = generer_url_public('',$arg);
-	}   $p->code ="preparar_enlace_enviar($arg)";   $p->statut = 'html';   return $p;}
+	}
+   $p->code ="preparar_enlace_enviar($arg)";
+   $p->statut = 'html';
+   return $p;
+}
 
 ?>
