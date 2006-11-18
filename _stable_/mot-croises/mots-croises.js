1a15dc17a833ac29f5fb581d973e0172eedee2f9
patfr@ifrance.com <patfr@ifrance.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-11-18 20:57:01 +0000

    curseur horizontal d'office... et précaution de trouver une table.grille !
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@7428 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/mot-croises/mots-croises.js
+++ _stable_/mot-croises/mots-croises.js
@@ -1,24 +1,31 @@
-// dans quelle direction on ecrit (h ou v)
-var sens='h';
-if ($('html').attr('dir') == 'rtl') 
-	var sens_curseur='w-resize';
-else
-	var sens_curseur='e-resize';
+// déplacement dans la grille
+var sens_grille='';
+// sens de l'écriture : 'rtl' ou 'ltl'
+var sens_curseur = ($('html').attr('dir') == 'rtl')?'w-resize':'e-resize';
+
+// dès que le DOM est prêt...
 $(document).ready(function(){
-	$('table.grille tr td input').bind('keypress', mykey).attr('style','cursor:'. sens_curseur);
+ // vérification de la présence d'une grille						    
+ if ($('table.grille').length) {
+	// sens d'écriture basculé à 'h' (horizontal)
+	changeDir();
+	// gestion du clavier par function mykey()
+	$('table.grille tr td input').bind('keypress', mykey);
+	// définition du bouton droit de la souris pour changer le sens de déplacement dans la grille
 	if ((jQuery.browser.safari) | (jQuery.browser.Konqueror))
 		$('form.grille').bind('contextmenu', changeDir);
 	else
 		$('table.grille tr td input').bind('contextmenu', changeDir);	
+ }
 });
 
 function changeDir(e) {
 	
-	if(sens=='h') {
-		sens='v';
+	if(sens_grille=='h') {
+		sens_grille='v';
 		style='s-resize';
 	} else {
-		sens='h';
+		sens_grille='h';
 		style=sens_curseur;
 	}
 	$('table.grille tr td input').css('cursor', style);
@@ -38,7 +45,7 @@ function mykey(e) {
 		case 39: if (sens_curseur=='e-resize') x++; else x--; break;
 		case 8 : break;
 		default:
-			if(sens=='h') x++; else y++;
+			if(sens_grille=='h') x++; else y++;
 	}
 	x=""+x; y=""+y;
 
