1778919cb47daed165c93a305f021e60166b872f
patfr@ifrance.com <patfr@ifrance.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-11-18 23:29:53 +0000

    bug IE + qq corrections + majuscules
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@7431 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/mot-croises/mots-croises.js
+++ _stable_/mot-croises/mots-croises.js
@@ -33,25 +33,31 @@ function changeDir(e) {
 }
 
 function mykey(e) {
+	//var key = e.keyCode ? e.keyCode : (e.which ? e.which: 0);
 	var m = this.name.match(/col(\d+)lig(\d+)/);
-	
 	var x=m[1];
 	var y=m[2];
+	var retour = true;
 	
 	switch(e.keyCode) {
 		case 40: y++; break; 
 		case 38: y--; break;
+		case 36: y=1; x=1; break;
 		case 37: if (sens_curseur=='e-resize') x--; else x++; break;
 		case 39: if (sens_curseur=='e-resize') x++; else x--; break;
-		case 8 : break;
+		case 8:; case 46: this.value = ''; break;
+		case 0 : if (e.which) { this.value = String.fromCharCode(e.which).toUpperCase(); retour = false }
 		default:
 			if(sens_grille=='h') x++; else y++;
+			// Pour IE ...
+			if ((e.which==null) && (e.keyCode>=46)) {
+				this.value = String.fromCharCode(e.keyCode).toUpperCase();
+				retour = false;
+			}
 	}
-	x=""+x; y=""+y;
 
-	$('#col'+x+'lig'+y).each(function(){
-		this.focus();
-	});
-
-	return true;
+	var newcell = '#col'+String(x)+'lig'+String(y);
+	$(newcell).each(function(){ this.focus(); });
+	
+	return retour;
 }
