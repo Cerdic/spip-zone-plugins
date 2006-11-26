8b7fa57c9f6e94fe643feb710eef0aef55666b47
james@rezo.net <james@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-11-26 17:10:00 +0000

    un petit coup de ménage par là.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@7577 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/balise_session/public/session_balises.php
+++ _stable_/balise_session/public/session_balises.php
@@ -9,18 +9,11 @@
  *  Ce programme est un logiciel libre distribue sous licence GNU/GPL.     *
 \***************************************************************************/
 
-include_spip('public/session_balises');
-
-//un filtre un peu fun pour la doc en ligne
-function liste_ul_li($tableau) {
-	$texte = '';
-	if(!empty($tableau))
-		foreach($tableau as $titre => $valeur) {
-			$texte .= "\t<li>".$titre."</li>\n";
-		}
-	return $texte ?
-		"<ul>\n".$texte."</ul>\n"
-		: '';
+function balise_SESSION_dist($p) {
+	if(function_exists('balise_ENV'))
+		return balise_ENV($p, '$GLOBALS["auteur_session"]');
+	else
+		return balise_ENV_dist($p, '$GLOBALS["auteur_session"]');
 }
 
 ?>
