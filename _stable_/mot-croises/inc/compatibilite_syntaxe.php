8e33ffded42d8fc693d224a4fdfe288db050168f
patfr@ifrance.com <patfr@ifrance.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-11-27 06:06:29 +0000

    Regles de commit et un peu de ménage...
    Maieul, arrange _REGLES_DE_COMMIT.txt comme bon te semble !
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@7594 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _stable_/mot-croises/inc/compatibilite_syntaxe.php
@@ -0,0 +1,74 @@
+<?php
+// on garde tout ca pour compatibilite avec l'ancienne syntaxe
+
+//transforme les listes verticales/horizontale spip en html
+function mots_croises_listes_vieille_syntaxe($texte) {
+	$texte = preg_replace('/ *-#/','<li>',$texte);
+	$texte = implode("</li>\n", preg_split("/\n*\r*\n+\r*\n*/",$texte));
+	return "<ol>$texte</li></ol>"; 
+}
+
+//fonction principale
+function mots_croises_vieille_syntaxe($texte) {	
+	if (! preg_match("/<grille>|<\/grille>/",$texte)) return $texte;
+	include_spip('inc/gestion_grille');
+	$tableau = preg_split("/<grille>|<\/grille>/",$texte);	//sera uniquement le tableau spip, mais on attend pour le moment
+	$j =0;
+	
+	foreach ($tableau as $i){
+			if ($j!=0 and $j!=count($tableau)-1){	//pas les extremités du tableau
+				$tableau_php = calcul_tableau_grille_vieille_syntaxe($tableau[$j]);
+			
+				//calcul erreur
+				if ($GLOBALS["bouton_envoi"] == '') $erreur='';
+				else {
+					list($nbr_erreurs, $nbr_vides) = comparaison_grille($tableau_php); 
+					$erreur = '<strong class="erreur">';
+					$erreur .= ($nbr_erreurs==0)?_T('motscroises:aucune_erreur'):(
+					 ($nbr_erreurs==1)?_T('motscroises:une_erreur'):_T("motscroises:nombre_erreurs", Array('err'=>$nbr_erreurs))
+					);
+					$erreur .= ($nbr_vides==0)?(($nbr_erreurs==0)?'. '._T('motscroises:bravo'):''):(
+					 ($nbr_vides==1)?' - '._T('motscroises:une_vide'):' - '._T("motscroises:nombre_vides", Array('vid'=>$nbr_vides))
+					);
+					$erreur.='</strong><br />';
+				}
+				//fin calcul erreur
+				
+				$tableau[$j] = code_echappement(_GRILLE_.$erreur.affichage_grille($tableau_php)._GRILLE_);	
+				}
+			
+			$j++;
+			}
+	
+	$texte = implode($tableau);
+	
+	// definitions horizontales
+	$tableau = preg_split("/<horizontal>|<\/horizontal>/",$texte);
+	$j = 0;
+	foreach ($tableau as $i){
+		if ($j!=0 and $j!=count($tableau)-1)	//pas les extremités du tableau
+				$tableau[$j] = code_echappement(_GRILLE_.'<div class="spip horizontal"><h4 class="spip grille">'
+					._T('motscroises:horizontalement')."</h4>\n".mots_croises_listes_vieille_syntaxe(trim($i))."</div>"._GRILLE_);
+		$j++;
+	}
+	$texte = implode($tableau);
+	
+	// definitions verticales
+	$tableau = preg_split("/<vertical>|<\/vertical>/",$texte);
+	$j = 0;
+	foreach ($tableau as $i){
+		if ($j!=0 and $j!=count($tableau)-1)	//pas les extremités du tableau
+				$tableau[$j] = code_echappement(_GRILLE_.'<div class="spip vertical"><h4 class="spip grille">'
+					._T('motscroises:verticalement')."</h4>\n"
+					.mots_croises_listes_vieille_syntaxe(trim($i))."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</div>"._GRILLE_); // Bug IE ?
+		$j++;
+	}
+	$texte = implode($tableau);
+	
+	// solution
+	if ($GLOBALS["solution"][0] == 1) $texte .= code_echappement(_GRILLE_.affichage_grille($tableau_php, true)._GRILLE_);
+
+	return $texte;
+}
+
+?>
--- _stable_/mot-croises/mots_croises_pipelines.php
+++ _stable_/mot-croises/mots_croises_pipelines.php
@@ -23,14 +23,6 @@
 define(_MC_DEBUT, '<mots-croises>');
 define(_MC_FIN, '</mots-croises>');
 
-//transforme les listes verticales/horizontale spip en html
-//on garde pour compatibilite
-function mots_croises_listes_vieille_syntaxe($texte) {
-	$texte = preg_replace('/ *-#/','<li>',$texte);
-	$texte = implode("</li>\n", preg_split("/\n*\r*\n+\r*\n*/",$texte));
-	return "<ol>$texte</li></ol>"; 
-}
-
 //transforme les listes verticales/horizontale listes html 
 function mots_croises_listes($texte) {
 	$tableau = explode("\r", trim($texte));	
@@ -52,77 +44,18 @@ function mots_croises_recupere_le_titre(&$chaine, $ouvrant, $fermant) {
 }
 
 //fonction principale
-//on garde pour compatibilite
-function mots_croises_vieille_syntaxe($texte) {	
-	if (! preg_match("/<grille>|<\/grille>/",$texte)) return $texte;
-	include_spip('inc/gestion_grille');
-	$tableau = preg_split("/<grille>|<\/grille>/",$texte);	//sera uniquement le tableau spip, mais on attend pour le moment
-	$j =0;
-	
-	foreach ($tableau as $i){
-			if ($j!=0 and $j!=count($tableau)-1){	//pas les extremités du tableau
-				$tableau_php = calcul_tableau_grille_vieille_syntaxe($tableau[$j]);
-			
-				//calcul erreur
-				if ($GLOBALS["bouton_envoi"] == '') $erreur='';
-				else {
-					list($nbr_erreurs, $nbr_vides) = comparaison_grille($tableau_php); 
-					$erreur = '<strong class="erreur">';
-					$erreur .= ($nbr_erreurs==0)?_T('motscroises:aucune_erreur'):(
-					 ($nbr_erreurs==1)?_T('motscroises:une_erreur'):_T("motscroises:nombre_erreurs", Array('err'=>$nbr_erreurs))
-					);
-					$erreur .= ($nbr_vides==0)?(($nbr_erreurs==0)?'. '._T('motscroises:bravo'):''):(
-					 ($nbr_vides==1)?' - '._T('motscroises:une_vide'):' - '._T("motscroises:nombre_vides", Array('vid'=>$nbr_vides))
-					);
-					$erreur.='</strong><br />';
-				}
-				//fin calcul erreur
-				
-				$tableau[$j] = code_echappement(_GRILLE_.$erreur.affichage_grille($tableau_php)._GRILLE_);	
-				}
-			
-			$j++;
-			}
-	
-	$texte = implode($tableau);
-	
-	// definitions horizontales
-	$tableau = preg_split("/<horizontal>|<\/horizontal>/",$texte);
-	$j = 0;
-	foreach ($tableau as $i){
-		if ($j!=0 and $j!=count($tableau)-1)	//pas les extremités du tableau
-				$tableau[$j] = code_echappement(_GRILLE_.'<div class="spip horizontal"><h4 class="spip grille">'
-					._T('motscroises:horizontalement')."</h4>\n".mots_croises_listes_vieille_syntaxe(trim($i))."</div>"._GRILLE_);
-		$j++;
-	}
-	$texte = implode($tableau);
-	
-	// definitions verticales
-	$tableau = preg_split("/<vertical>|<\/vertical>/",$texte);
-	$j = 0;
-	foreach ($tableau as $i){
-		if ($j!=0 and $j!=count($tableau)-1)	//pas les extremités du tableau
-				$tableau[$j] = code_echappement(_GRILLE_.'<div class="spip vertical"><h4 class="spip grille">'
-					._T('motscroises:verticalement')."</h4>\n"
-					.mots_croises_listes_vieille_syntaxe(trim($i))."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</div>"._GRILLE_); // Bug IE ?
-		$j++;
-	}
-	$texte = implode($tableau);
-	
-	// solution
-	if ($GLOBALS["solution"][0] == 1) $texte .= code_echappement(_GRILLE_.affichage_grille($tableau_php, true)._GRILLE_);
-
-	return $texte;
-}
-	
-//fonction principale
 function mots_croises($texte){ 
-	if (strpos($texte, '<horizontal>')!==false && strpos($texte, '</horizontal>')!==false) return mots_croises_vieille_syntaxe($texte);
+	if (strpos($texte, '<horizontal>')!==false || strpos($texte, '<vertical>')!==false) {
+		include_spip('inc/compatibilite_syntaxe.php');
+		return mots_croises_vieille_syntaxe($texte);
+	}	
 	if (strpos($texte, _MC_DEBUT)===false || strpos($texte, _MC_FIN)===false) return $texte;
 
+	// isoler les mots-croises...
 	list($texteAvant, $suite) = explode(_MC_DEBUT, $texte, 2); 
 	list($texte, $texteApres) = explode(_MC_FIN, $suite, 2); 
 	
+	// ...et decoder le texte obtenu !
 	include_spip('inc/gestion_grille');
 	$tableau = preg_split("/(#TITRE|#HORIZONTAL|#VERTICAL|#SOLUTION)/", trim($texte), -1, PREG_SPLIT_DELIM_CAPTURE);
 	$titre = $horizontal = $vertical = $solution = false;
@@ -150,6 +83,7 @@ function mots_croises($texte){
 	return code_echappement($texteAvant.'<!-- PLUGIN-DEBUT -->').$texte.code_echappement('<!-- PLUGIN-FIN -->').$texteApres;
 }
 
+// a la place de mots_croises, pour le deboguage...
 function mots_croises2($chaine){
  if (strpos($chaine, _MC_DEBUT)!==false || strpos($chaine, '<horizontal>')!==false) {
 	ob_start();
@@ -162,7 +96,7 @@ function mots_croises2($chaine){
 }
 
 function mots_croises_pre_propre($texte) { 
-	return mots_croises2($texte);
+	return mots_croises($texte);
 }
 
 function mots_croises_header_prive($flux){
