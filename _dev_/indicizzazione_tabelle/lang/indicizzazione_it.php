d0313600ed0d29b15811abf98a4d2439eb97a1a1
renato@rezo.net <renato@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-12-08 21:14:55 +0000

    eliminated string that belonged to another plugin
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@7827 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/indicizzazione_tabelle/lang/indicizzazione_it.php
+++ _dev_/indicizzazione_tabelle/lang/indicizzazione_it.php
@@ -3,8 +3,6 @@
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-
-
 'azioni' => "Azioni",
 'campo_inesistente' => 'Campo @campo@ inesistente',
 'configura_campi' => 'Configura i campi da indicizzare della tabella @tabella@',
@@ -44,26 +42,6 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 'tabella_non_definita' => 'tabella non definita in spip',
 'tabella_non_indicizzata' => 'tabella non indicizzata',
 'tabella_indicizzata' => 'tabella indicizzata',
-
-'convertir_desc' => 'Permet de convertir des formats divers (wiki, phpBB) en format SPIP<br /><br />Pour le format Word Office, vous pouvez utiliser <a href=\'http://www.spip-contrib.net/Macro-word\'>la macro word</a>',
-'convertir' => 'Convertir',
-'from' => 'De ',
-'convertir_en' => 'R&eacute;sultat de la conversion: ',
-
-// les formats
-'MoinWiki_SPIP' => 'Wiki (MoinWiki) vers SPIP', 
-'BBcode_SPIP' => 'BBcode (PhpBB) vers SPIP', 
-'SPIP_txt' => 'SPIP vers texte brut',
-'SPIP_html' => 'SPIP vers HTML',  
-'SPIPrich_SPIP' => 'SPIP enrichie vers SPIP standard', 
-'XPressTags' => 'XPressTags vers SPIP', 
-'Word' => 'Word vers texte', 
-'RTF' => 'RTF vers texte', 
-'PDF' => 'PDF vers texte', 
-
-// les erreurs
-'unknown_format' => 'Erreur: format inconnu',
-'erreur_extracteur' => 'Erreur : l\'extracteur n\'a pas fonctionn&eacute;'
 );
 
 ?>
