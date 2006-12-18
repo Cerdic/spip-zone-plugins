03daa7e7948dcf2eb136d2a80a0d8ddd8df3cd2e
olivier.gendrin@free.fr <olivier.gendrin@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-12-18 13:23:09 +0000

    ajout d'une option d'accessibilité (dans nuage_fonctions.php), et du pipleine insert_head qui va avec. Plugin trop rapidement testé, fonctionne avec deux mots-clés...
    Je suis très insatisfait de l'utilisation que je fais de nuage_options.php (include find_in_path('nuage_options.php');)...
    
    Le comportement par défaut du plugin n'est pas changé.
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@8106 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/nuage/modeles/nuage_fonctions.php
+++ _stable_/nuage/modeles/nuage_fonctions.php
@@ -6,12 +6,14 @@ function balise_NUAGE($p){
 }
 
 function nuage($id_mot, $titre = '', $url = '', $poids = 0){
+	
+	include find_in_path('nuage_options.php');
+	
 	static $nuage;
 	$texte = '';
 	if($titre and $url){
 		$nuage['titre'][$id_mot] = $titre;
 		$nuage['url'][$id_mot] = $url;
-		 
 	}
 	elseif($poids){
 		$nuage['poids'][$id_mot] += $poids;
@@ -21,13 +23,15 @@ function nuage($id_mot, $titre = '', $url = '', $poids = 0){
 		$url = $nuage['url'];
 		$poids = $nuage['poids'];
 		$maxpop = empty($poids)?0:max($poids);
+		$totalpop = array_sum($poids);
 		if($maxpop>0){
 			foreach ($titre as $id => $t) {
 				$score = $poids[$id]/$maxpop; # entre 0 et 1
 				if($score > 0.05){
 					$s = ceil(30*$score);
 					$s = 10 + $s;
-					$l = "<span style='white-space:nowrap; font-size:".$s."px;'>$t</span>";
+					if ($nuage_mode == 'non-accessible') $l = "<span style='white-space:nowrap; font-size:".$s."px;'>$t</span>";
+					else $l = "<span style='white-space:nowrap; font-size:".$s."px;'>$t<span class=\"nuage_frequence\"> (".$poids[$id]."/".$totalpop.")</span></span>";
 					$l = "<a href='".$url[$id]."'>$l</a>";
 					$texte .= "$l &nbsp; \n";
 				}
---
+++ _stable_/nuage/nuage_options.php
@@ -0,0 +1,7 @@
+<?php
+
+// On choisi entre l'affichage 'accessible' et l'affichage 'non-accessible';
+// $nuage_mode = 'accessible'; ou $nuage_mode = 'non-accessible';
+$nuage_mode = 'non-accessible';
+
+?>
