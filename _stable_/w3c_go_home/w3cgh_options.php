57e5ea7ad23634e487dc324ebce2e6d16208085e
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-12-25 23:29:44 +0000

    "activer le validateur xml spip interne sur les pages publiques"
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@8249 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _stable_/w3c_go_home/w3cgh_options.php
@@ -0,0 +1,8 @@
+<?php
+
+$GLOBALS['xhtml'] = 'sax';
+
+//$GLOBALS['xml_indent'] = 'sax'; // pour seulement le validateur XML
+//$GLOBALS['xml_validation'] = true; // pour le validateur selon la DTD
+
+?>
