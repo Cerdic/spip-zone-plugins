76810482ef6bf3c9abb4eccbe6a281f5607b2c82
patfr@ifrance.com <patfr@ifrance.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-12-28 03:19:31 +0000

    - sérieuse refonte et amélioration des filets - protection : les filets ne sont pas transformés sur les balises : cadre|code
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@8296 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/tweak_spip/inc/filets_sep.php
+++ _stable_/tweak_spip/inc/filets_sep.php
@@ -2,26 +2,25 @@
 /*
 *	+----------------------------------+
 *	Nom du Tweak : Filets de Separation
-*	Version : 1.3
-*	Date : 27/12/2006
-*	Auteur : FredoMkb
-*	Adaptation et ameliorations pour Tweak SPIP : Patrice Vanneufville
+*	Idee originale : FredoMkb
+*	Serieuse refonte et ameliorations pour Tweak SPIP : Patrice Vanneufville
+*	Date : decembre 2006
 *	+-------------------------------------+
 *	Fonctions de ce tweak :
 *		Ce tweak permet d'introduire des filets de separation dans le corps du texte.
 *		Le but est d'offrir un moyen simple pour structurer et visualiser les niveaux 
 *		d'imbrication des differents textes a l'interieur d'un article ou breve.
-*		Ce filtre est un complement plus riche et plus souple que le filet de separation
+*		Ce tweak est un complement plus riche et plus souple que le filet de separation
 *		par defaut produit par Spip lors de l'insertion des 4 tirets normaux "----".
 *	+-------------------------------------+ 
 *	Utilisation de ce tweak dans les textes de votre site SPIP :
 *	1) Version predefinie
-*		Les balises s'inscrivent toujours en utilisant 4 tirets bas (4 tirets en soulignes),
+*		Les balises s'inscrivent toujours en utilisant 4 tirets bas (4 tirets en souligne),
 *		separes, deux a deux, par un chiffre correspondant au type de filet a inserer dans le texte.
 *		Cette version du filtre est distribuee avec 7 styles pre-formates, qu'on peut obtenir 
 *		en inserant des balises "__0__" jusqu'a "__6__" dans les corps des articles.
 *	2) Version image
-*		Les balises s'inscrivent toujours en utilisant 4 tirets bas (4 tirets en soulignes),
+*		Les balises s'inscrivent toujours en utilisant 4 tirets bas (4 tirets en souligne),
 *		separes, deux a deux, par un nom de fichier que l'on peut trouver dans le dossier :
 *		img/filets/
 *		Cette version du tweak permet donc d'adapter n'importe quelle image en inserant des 
@@ -35,13 +34,13 @@
 *		Entre <code> et </code> ou <cadre> et </cadre>, il n'y a pas de recherche de balises de filets.
 *	+-------------------------------------+ 
 *	Parametrage de l'aspect des filets  predefinis:
-*		Ce filtre remplace donc les differentes balises inserees par des paragraphes Html vides, 
+*		Ce tweak remplace donc les differentes balises inserees par des paragraphes Html vides, 
 *		mais ayant chacun un style specifique, par exemple "<p class="filet_sep_1"></p>".
 *		Ces styles sont fournis avec ce tweak dans le fichier "inc/filets_sep.css".
 *		Il y a donc 7 styles pre-formates  qui sont separes en deux groupes : 
 *		"Filets" qui produisent des filets simples et "Blocs" qui produisent 
 *		des blocs avec des fonds et filets differents. 
-*		Tous ces filtres sont evidemment personnalisables suivant vos gouts et besoins. 
+*		Tous ces filets sont evidemment personnalisables suivant vos gouts et besoins. 
 *		Vous pouvez aussi ajouter autant de styles supplementaires que necessaire, 
 *		mais en respectant la convention de nommage suivante pour les nouveaux styles :
 *		"filet_sep_N", ou "N" est le numero que vous aurez decide d'attribuer au style.
@@ -52,8 +51,9 @@
 *	+-------------------------------------+ 
 */
 
-function filets_sep($texte) {
 // Fonction pour generer des filets de separation selon les balises presentes dans le texte fourni.
+// Cette fonction n'est pas appelee dans les balises html : cadre|code
+function filets_sep_rempl($texte) {
 	
 	// On memorise les modeles d'expression rationnelle a utiliser pour chercher les balises.
 	$base_nombre = '\d+';
@@ -67,29 +67,11 @@ function filets_sep($texte) {
 	$test= preg_match($modele_total, $texte);
 
 	if ($test) {
-		// On isole les textes presents dans les balises "cadre" et "code".
-		preg_match_all('#<cadre>(.*?)</cadre>#is', $texte, $listeCadre);
-		preg_match_all('#<code>(.*?)</code>#is', $texte, $listeCode);
-		$listeCadreTexte = $listeCadre[0];
-		$listeCodeTexte = $listeCode[0];
-		
-		// On modifie le format des balises filets dans les balises "cadre" pour ne pas les traiter.
-		foreach ($listeCadreTexte as $texteCadreOrig) {
-			$texteCadreNew = preg_replace("#__({$base_total})__#iU",'__-$1-__',$texteCadreOrig);
-			$texte = str_replace($texteCadreOrig,$texteCadreNew,$texte);
-		};
-	
-		// On modifie le format des balises filets dans les balises "code" pour ne pas les traiter.
-		foreach ($listeCodeTexte as $texteCodeOrig) {
-			$texteCodeNew = preg_replace("#__({$base_total})__#iU",'__-$1-__',$texteCodeOrig);
-			$texte = str_replace($texteCodeOrig,$texteCodeNew,$texte);
-		};
-
 		// On remplace les balises filets numeriques dans le texte par le code Html correspondant.
 		$texte = preg_replace($modele_nombre,'<html><p class="filet_sep_$1"><!-- --></p></html>',$texte); 
 
 		// On remplace les balises filets numeriques dans le texte par le code Html correspondant.
-		$t=preg_split($modele_fichier, $texte, -1, PREG_SPLIT_DELIM_CAPTURE); //print_r($t);
+		$t=preg_split($modele_fichier, $texte, -1, PREG_SPLIT_DELIM_CAPTURE);
 		$texte = $t[0];
 		for ($i=1; $i<count($t); $i+=3) {
 			$f=find_in_path('img/filets/'.$t[$i]);
@@ -97,16 +79,12 @@ function filets_sep($texte) {
 			if ($haut) $haut='height:'.$haut.'px; ';
 			$texte .= '<html><p class="filet_sep_image" style="'.$haut.'background-image: url('.$f.');"><!-- --></p></html>'.$t[$i+2];
 		}
-		
-		// On remet les balises filets presents dans les balises "cadre" et "code" a leur format initial.
-		$texte = preg_replace("#__-({$base_total})-__#iU",'__$1__',$texte); 
 	};
 
-	// Traitement des anciens filets, pour assurer une compatibilite descendente.
-//	$texte = str_replace('__l__','<html><p class="filet_sep_long"></p></html>',$texte);
-//	$texte = str_replace('__m__','<html><p class="filet_sep_moyen"></p></html>',$texte);
-//	$texte = str_replace('__c__','<html><p class="filet_sep_court"></p></html>',$texte);
-
 	return $texte;
+}
+
+function filets_sep($texte) {
+	return tweak_exclure_balises('cadre|code', 'filets_sep_rempl', $texte);
 }
 ?>
