f8875e0ed00a4b2b324d0a3bf3972dcf126fb851
patfr@ifrance.com <patfr@ifrance.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-12-28 03:20:28 +0000

    protection : les belles puces ne sont pas transformées sur les balises : cadre|code
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@8297 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/tweak_spip/inc/bellespuces.php
+++ _stable_/tweak_spip/inc/bellespuces.php
@@ -1,9 +1,13 @@
 <?php
 
-	function bellespuces_pre_typo($texte) {
-        $texte = preg_replace('/^-\s+/m','-* ',$texte);
-        return $texte;
-    }
+// cette fonction n'est pas appelee dans les balises html : cadre|code
+function bellespuces_rempl($texte) {
+	return preg_replace('/^-\s+/m','-* ',$texte);
+}
+
+function bellespuces_pre_typo($texte) {
+	return tweak_exclure_balises('cadre|code', 'bellespuces_rempl', $texte);
+}
 
 
 ?>
