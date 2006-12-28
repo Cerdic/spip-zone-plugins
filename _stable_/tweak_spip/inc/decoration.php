862c9bc6859fe2d2f4dcdca76d89a3c41c1e42e2
patfr@ifrance.com <patfr@ifrance.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2006-12-28 03:14:50 +0000

    - protection : les decorations ne sont pas introduites sur les balises : cadre|code
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@8295 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/tweak_spip/inc/decoration.php
+++ _stable_/tweak_spip/inc/decoration.php
@@ -1,30 +1,21 @@
 <?php
 
 /*
-  * Le plugin decoration permet aux redacteurs d'un site spip de d'appliquer les styles souligné, barré, au dessus à  une phrase , un  mot, parapraphe. 
- */
+   Ce tweak decoration permet aux redacteurs d'un site spip de d'appliquer les styles souligné, barré, au dessus aux textes SPIP
+*/
 
-
-
-	function decoration_pre_typo($texte) {
-		$texte = preg_replace("/(\<souligne\>)(.*?)(\<\/souligne\>)/", "<span style=\"text-decoration: underline;\">\\2</span>", $texte);
-		$texte = preg_replace("/(\<barre\>)(.*?)(\<\/barre\>)/", "<span style=\"text-decoration: line-through;\">\\2</span>", $texte);
-		$texte = preg_replace("/(\<dessus\>)(.*?)(\<\/dessus\>)/", "<span style=\"text-decoration: overline;\">\\2</span>", $texte);
-		$texte = preg_replace("/(\<clignote\>)(.*?)(\<\/clignote\>)/", "<span style=\"text-decoration: blink;\">\\2</span>", $texte);
-		$texte = preg_replace("/(\<fluo\>)(.*?)(\<\/fluo\>)/", "<span style=\"background-color: #ffff00;\">\\2</span>", $texte);
-
-		$texte = preg_replace("/(\<sc\>)(.*?)(\<\/sc\>)/", "<span style=\"font-variant: small-caps\">\\2</span>", $texte);
-
-
-		// Raccourci typographique <sc></sc>
-//		$texte = str_replace("<sc>","<span style=\"font-variant: small-caps\">", $texte);
-//		$texte = str_replace("</sc>", "</span>", $texte);
-
+function decoration_rempl($texte) {
+	$texte = preg_replace("/(\<souligne\>)(.*?)(\<\/souligne\>)/", "<span style=\"text-decoration: underline;\">\\2</span>", $texte);
+	$texte = preg_replace("/(\<barre\>)(.*?)(\<\/barre\>)/", "<span style=\"text-decoration: line-through;\">\\2</span>", $texte);
+	$texte = preg_replace("/(\<dessus\>)(.*?)(\<\/dessus\>)/", "<span style=\"text-decoration: overline;\">\\2</span>", $texte);
+	$texte = preg_replace("/(\<clignote\>)(.*?)(\<\/clignote\>)/", "<span style=\"text-decoration: blink;\">\\2</span>", $texte);
+	$texte = preg_replace("/(\<fluo\>)(.*?)(\<\/fluo\>)/", "<span style=\"background-color: #ffff00;\">\\2</span>", $texte);
+	$texte = preg_replace("/(\<sc\>)(.*?)(\<\/sc\>)/", "<span style=\"font-variant: small-caps\">\\2</span>", $texte);
 	return $texte;  
 }
 
-
-
-
+function decoration_pre_typo($texte) {
+	return tweak_exclure_balises('cadre|code', 'decoration_rempl', $texte);
+}
 
 ?>
