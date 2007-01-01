fd3bdc6df003fe526b7920a06b7115fe87e84c3b
edd@no-log.org <edd@no-log.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-01-01 16:51:28 +0000

    pour indépendance du plugin vis à vis du squelette - ajout des balises #CONTRIBUTIONS_LOCALES #CONTRIBUTIONS_NON_LOCALES et #CONTRIBUTIONS_ANALYSES dans le fichier op_fonctions.php - ajout des fonctions getAuteurOpName, getAuteurOpPhone, getAuteurOpGroup, getAuteurOpMail dans le fichier op_fonctions.php
    Modification de plugin.xml pour prise en compte de op_fonctions.php
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@8389 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _stable_/openPublishing/op_fonctions.php
@@ -0,0 +1,66 @@
+<?php
+
+$GLOBALS['contrib_local'] = 13;
+$GLOBALS['contrib_non_local'] = 14;
+$GLOBALS['contrib_analyse'] = 24;
+
+function balise_CONTRIBUTIONS_LOCALES($p) {
+	$p->code = 'return_contrib_local()';
+	$p->statut = 'php';
+	return $p;
+}
+
+function balise_CONTRIBUTIONS_NON_LOCALES($p) {
+	$p->code = 'return_contrib_non_local()';
+	$p->statut = 'php';
+	return $p;
+}
+
+function balise_CONTRIBUTIONS_ANALYSES($p) {
+	$p->code = 'return_analyse()';
+	$p->statut = 'php';
+	return $p;
+}
+
+function return_contrib_local() {
+	$reponse = $GLOBALS['contrib_local'];
+	return $reponse;
+}
+function return_contrib_non_local() {
+	$reponse = $GLOBALS['contrib_non_local'];
+	return $reponse;
+}
+function return_analyse() {
+	$reponse = $GLOBALS['contrib_analyse'];
+	return $reponse;
+}
+
+function getAuteurOpName($id_article) {
+	$query = "SELECT nom FROM spip_op_auteurs WHERE id_article = '$id_article'";
+	$result = spip_query($query);
+	$row = mysql_fetch_array($result);
+	return $row[0];
+}
+
+function getAuteurOpMail($id_article) {
+	$query = "SELECT email FROM spip_op_auteurs WHERE id_article = '$id_article'";
+	$result = spip_query($query);
+	$row = mysql_fetch_array($result);
+	return $row[0];
+}
+
+function getAuteurOpPhone($id_article) {
+	$query = "SELECT phone FROM spip_op_auteurs WHERE id_article = '$id_article'";
+	$result = spip_query($query);
+	$row = mysql_fetch_array($result);
+	return $row[0];
+	}
+
+function getAuteurOpGroup($id_article) {
+	$query = "SELECT group_name FROM spip_op_auteurs WHERE id_article = '$id_article'";
+	$result = spip_query($query);
+	$row = mysql_fetch_array($result);
+	return $row[0];
+}
+
+?> 
--- _stable_/openPublishing/op_options.php
+++ _stable_/openPublishing/op_options.php
@@ -1,15 +1,3 @@
 <?php
 
-/* * variables Open-publishing
-   * modifiez ces variables en fonction du fonctionnement de votre site
-   * ----------------------------------------
-   * statut_article : statut des articles publi�par les visiteurs (prop ou valid)
-   * rubrique_article : rubriques autoris� pour l'open-publishing */
-
-//$statut_article = 'prop';
-//$rubrique_article = array(2,3,4);
-//autoriser l'upload
-//$GLOBALS['flag_upload'] = 0;
-
-
 ?>
