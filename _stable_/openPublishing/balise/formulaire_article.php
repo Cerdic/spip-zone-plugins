0a5b4fbd451541da865f8be4d055b9d5f8e104e0
edd@no-log.org <edd@no-log.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-01-02 18:24:34 +0000

    faire en sotre que les choix dans l'agenda ne disparaissent pas au recalc de la page suppression de l'article temporaire si l'utilisateur publié une brève et non un article
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@8430 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/openPublishing/balise/formulaire_article.php
+++ _stable_/openPublishing/balise/formulaire_article.php
@@ -227,7 +227,7 @@ if($valider) {
                return $retour_forum;
            }
        }*/
-
+	
 	// l'auteur demande une insertion dans l'agenda
 	if ($choix_agenda == "OK") {
 
@@ -250,6 +250,9 @@ if($valider) {
 		" . spip_abstract_quote($statut) . ",
 		" . spip_abstract_quote($rubrique_breve) . "
 		)");
+
+		// supression de l'article temporaire
+		spip_query("DELETE FROM spip_articles WHERE id_article = '$article' LIMIT 1");
 	}
 	else { // soit il s'agit d'un article, soit d'une breve. Les deux à la fois ne sont pas possible
 	/*
@@ -481,11 +484,144 @@ function barre_article($texte, $rows, $cols, $lang='')
 	ondbclick='storeCaret(this);'>$texte</textarea></center>";
 }
 
+// pour garder la valeur lors d'un rechargement de page
 function selected_option($id_rubrique, $rubrique_boucle,$titre_rubrique)
 {
 	$selected = '';
-	if ($id_rubrique == $rubrique_boucle) $selected = "selected";
+	if ($id_rubrique == $rubrique_boucle) $selected = "SELECTED";
 	return "[<option value='$rubrique_boucle' $selected >&nbsp;$titre_rubrique</option>]";
+}
+
+// pour garder la valeur lors d'un rechargement de page
+function checkbox_agenda($choix_agenda) {
+
+	$checked = '';
+	if ($choix_agenda == "OK") $checked = "CHECKED";
+	return "<input type='checkbox' name='choix_agenda' value='OK' '$checked' />&nbsp;Faire apparaitre l'article dans l'agenda<br />";
+}
+
+// pour garder la valeur lors d'un rechargement de page
+function select_annee($annee) {
+	$selected = "";
+	if ($annee !== '') $selected = "SELECTED";
+	$return = "<select name='annee'>"; // pour le moment ne prend en compte que 5 ans
+	for ($i = 0;$i<5;$i++) {
+		$a = 2007 + $i;
+		if ($a == $annee) {
+			$return = $return . "<option value='$a' '$selected' >$a</option>";
+		}
+		else $return = $return . "<option value='$a'>$a</option>";
+	}
+	return $return ."</select>";
+}
+
+// pour garder la valeur lors d'un rechargement de page
+function select_mois($mois) {
+	$selected = "";
+	if ($mois !== '') $selected = "SELECTED";
+	$return = "<select name='mois'>"; 
+
+	if ($mois == "01") $return = $return . "<option value='01' '$selected' >janvier</option>";
+	else $return = $return . "<option value='01'>janvier</option>";
+	if ($mois == "02") $return = $return . "<option value='02' '$selected' >fevrier</option>";
+	else $return = $return . "<option value='02'>fevrier</option>";
+	if ($mois == "03") $return = $return . "<option value='03' '$selected' >mars</option>";
+	else $return = $return . "<option value='03'>mars</option>";
+	if ($mois == "04") $return = $return . "<option value='04' '$selected' >avril</option>";
+	else $return = $return . "<option value='04'>avril</option>";
+	if ($mois == "05") $return = $return . "<option value='05' '$selected' >mai</option>";
+	else $return = $return . "<option value='05'>mai</option>";
+	if ($mois == "06") $return = $return . "<option value='06' '$selected' >juin</option>";
+	else $return = $return . "<option value='06'>juin</option>";
+	if ($mois == "07") $return = $return . "<option value='07' '$selected' >juillet</option>";
+	else $return = $return . "<option value='07'>juillet</option>";
+	if ($mois == "08") $return = $return . "<option value='08' '$selected' >aout</option>";
+	else $return = $return . "<option value='08'>aout</option>";
+	if ($mois == "09") $return = $return . "<option value='09' '$selected' >septembre</option>";
+	else $return = $return . "<option value='09'>septembre</option>";
+	if ($mois == "10") $return = $return . "<option value='10' '$selected' >otobre</option>";
+	else $return = $return . "<option value='10'>octobre</option>";
+	if ($mois == "11") $return = $return . "<option value='11' '$selected' >novembre</option>";
+	else $return = $return . "<option value='11'>novembre</option>";
+	if ($mois == "12") $return = $return . "<option value='12' '$selected' >decembre</option>";
+	else $return = $return . "<option value='12'>decembre</option>";
+
+	return $return ."</select>";
+}
+
+// pour garder la valeur lors d'un rechargement de page
+function select_jour($jour) {
+
+	$selected = "";
+	if ($jour !== '') $selected = "SELECTED";
+	$return = "<select name='jour'>"; // pour le moment tous les mois ont 31 jours
+	for ($i = 0;$i<31;$i++) {
+		$j = 1 + $i;
+		if ($j == $jour) {
+			$return = $return . "<option value='$j' '$selected' >$j</option>";
+		}
+		else $return = $return . "<option value='$j'>$j</option>";
+	}
+	return $return . '</select>';
+}
+
+// pour garder la valeur lors d'un rechargement de page
+function select_heure($heure) {
+
+	$selected = "";
+	if ($heure !== '') $selected = "SELECTED";
+	$return = "<select name='heure'>"; // pour le moment tous les mois ont 31 jours
+
+	if ($heure == "06:00") $return = $return . "<option value='06:00' '$selected' >06:00</option>";
+	else $return = $return . "<option value='06:00' >06:00</option>";
+	if ($heure == "07:00") $return = $return . "<option value='07:00' '$selected' >07:00</option>";
+	else $return = $return . "<option value='07:00' >07:00</option>";
+	if ($heure == "08:00") $return = $return . "<option value='08:00' '$selected' >08:00</option>";
+	else $return = $return . "<option value='08:00' >08:00</option>";
+	if ($heure == "09:00") $return = $return . "<option value='09:00' '$selected' >09:00</option>";
+	else $return = $return . "<option value='09:00' >09:00</option>";
+	if ($heure == "10:00") $return = $return . "<option value='10:00' '$selected' >10:00</option>";
+	else $return = $return . "<option value='10:00' >10:00</option>";
+	if ($heure == "11:00") $return = $return . "<option value='11:00' '$selected' >11:00</option>";
+	else $return = $return . "<option value='11:00' >11:00</option>";
+	if ($heure == "12:00") $return = $return . "<option value='12:00' '$selected' >12:00</option>";
+	else $return = $return . "<option value='12:00' >12:00</option>";
+	if ($heure == "13:00") $return = $return . "<option value='13:00' '$selected' >13:00</option>";
+	else $return = $return . "<option value='13:00' >13:00</option>";
+	if ($heure == "14:00") $return = $return . "<option value='14:00' '$selected' >14:00</option>";
+	else $return = $return . "<option value='14:00' >14:00</option>";
+	if ($heure == "15:00") $return = $return . "<option value='15:00' '$selected' >15:00</option>";
+	else $return = $return . "<option value='15:00' >15:00</option>";
+	if ($heure == "16:00") $return = $return . "<option value='16:00' '$selected' >16:00</option>";
+	else $return = $return . "<option value='16:00' >16:00</option>";
+	if ($heure == "17:00") $return = $return . "<option value='17:00' '$selected' >17:00</option>";
+	else $return = $return . "<option value='17:00' >17:00</option>";
+	if ($heure == "18:00") $return = $return . "<option value='18:00' '$selected' >18:00</option>";
+	else $return = $return . "<option value='18:00' >18:00</option>";
+	if ($heure == "19:00") $return = $return . "<option value='19:00' '$selected' >19:00</option>";
+	else $return = $return . "<option value='19:00' >19:00</option>";
+	if ($heure == "20:00") $return = $return . "<option value='20:00' '$selected' >20:00</option>";
+	else $return = $return . "<option value='20:00' >20:00</option>";
+	if ($heure == "21:00") $return = $return . "<option value='21:00' '$selected' >21:00</option>";
+	else $return = $return . "<option value='21:00' >21:00</option>";
+	if ($heure == "22:00") $return = $return . "<option value='22:00' '$selected' >22:00</option>";
+	else $return = $return . "<option value='22:00' >22:00</option>";
+	if ($heure == "23:00") $return = $return . "<option value='23:00' '$selected' >23:00</option>";
+	else $return = $return . "<option value='23:00' >23:00</option>";
+	if ($heure == "00:00") $return = $return . "<option value='00:00' '$selected' >00:00</option>";
+	else $return = $return . "<option value='00:00' >00:00</option>";
+	if ($heure == "01:00") $return = $return . "<option value='01:00' '$selected' >01:00</option>";
+	else $return = $return . "<option value='01:00' >01:00</option>";
+	if ($heure == "02:00") $return = $return . "<option value='02:00' '$selected' >02:00</option>";
+	else $return = $return . "<option value='02:00' >02:00</option>";
+	if ($heure == "03:00") $return = $return . "<option value='03:00' '$selected' >03:00</option>";
+	else $return = $return . "<option value='03:00' >03:00</option>";
+	if ($heure == "04:00") $return = $return . "<option value='04:00' '$selected' >04:00</option>";
+	else $return = $return . "<option value='04:00' >04:00</option>";
+	if ($heure == "05:00") $return = $return . "<option value='05:00' '$selected' >05:00</option>";
+	else $return = $return . "<option value='05:00' >05:00</option>";
+		
+	return $return . '</select>';
 }
 
 // fonction qui cherche l'i de l'auteur anonymous
