795747a37317ea6977f3d3c02560ded913920f61
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-01-06 19:33:15 +0000

    Rajout de 2 chaînes de langues.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@8589 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/boutonstexte/lang/boutonstexte_fr.php
+++ _stable_/boutonstexte/lang/boutonstexte_fr.php
@@ -3,9 +3,11 @@
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+'textes_icones' => 'Textes des bulles d\'icones',
 'texte_seulement' => 'Texte seulement',
 'retour_a_spip' => 'Retour &agrave; Spip',
 'augmenter_police' => 'Augmenter police',
-'diminuer_police' => 'Diminuer police'
+'diminuer_police' => 'Diminuer police',
+'donnees_techniques' => 'Donn&eacute;es techniques &agrave; ne pas changer',
 );
 ?>
