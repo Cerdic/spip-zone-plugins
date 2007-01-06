6817c67c79ef6e7d4cb63027e09c99a0a3dae5dc
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-01-06 19:52:01 +0000

    Intitulé plus clair pour le retour à la normale.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@8591 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/boutonstexte/lang/boutonstexte_fr.php
+++ _stable_/boutonstexte/lang/boutonstexte_fr.php
@@ -5,7 +5,7 @@
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 'textes_icones' => 'Textes des bulles d\'icones',
 'texte_seulement' => 'Texte seulement',
-'retour_a_spip' => 'Retour &agrave; Spip',
+'retour_a_spip' => 'Retour &agrave; la page compl&egrave;te',
 'augmenter_police' => 'Augmenter police',
 'diminuer_police' => 'Diminuer police',
 'donnees_techniques' => 'Donn&eacute;es techniques &agrave; ne pas changer',
