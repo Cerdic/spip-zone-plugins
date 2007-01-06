05ca508b36cfb1d7117acd0cde565e2380fbce42
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-01-06 20:22:31 +0000

    "langue en (Paolo)"
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@8594 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _stable_/gestion_documents/lang/gestdoc_en.php
@@ -0,0 +1,30 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	'affichage' => 'Display :',
+	'attr_alt' => 'alt tag lacking ? ',
+	'conteneur' => 'Container :',
+	'fichier_introuvable' => 'No file found',
+	'filtrer' => 'Filter :',
+	'info_breve' => 'News item ',
+	'info_breves' => 'News items',
+	'info_doc' => 'This page presents the list of all your attached documents. To change the information concerning a document, follow the link to the page of its section.',
+	'info_reparer' => 'This page reconstitutes the links between documents and the articles in which they are inserted by means of an <code><imgxx>, <docxx> or <embxx></code> tag.',
+	'info_rubrique' => 'Section ',
+	'lien_ajoute' => 'link added',
+	'mis_jour_liens' => 'Links updated',
+	'mis_jour_tailles' => 'Correct the filesizes',
+	'par' => 'By @numero@',
+	'portfolio' => 'Portfolio',
+	'reparer_liens' => 'Repare links',
+	'info_syndication' => 'Syndication',
+	'sans_titre_descriptif' => 'Without title or description',
+	'taille_erronee' => 'With wrong filesize',
+	'type' => 'Type :',
+	'tous' => 'All',
+	'tous_doc'  => 'All your documents',
+	'tous_docs'  => 'All documents'
+);
+?>
