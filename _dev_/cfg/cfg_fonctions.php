abed711e582689cee244afd1211f804fbe45014e
bertrand@toggg.com <bertrand@toggg.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-01-06 05:23:45 +0000

    2eme argument de #CONFIG (defaut) , optimisation de l'explosion de l'argument 1
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@8549 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/cfg/cfg_fonctions.php
+++ _dev_/cfg/cfg_fonctions.php
@@ -8,7 +8,8 @@
 // #CONFIG etendue interpretant les /
 //
 // Par exemple #CONFIG{xxx/yyy/zzz} fait comme #CONFIG{xxx}['yyy']['zzz']
-// xxx etant une table serialisee dans spip_meta
+// xxx etant un tableau serialise dans spip_meta
+// Le 2eme argument de la balise est la valeur defaut comme pour la dist
 //
 // La balise appelle celle de la dist si pas de /
 //
@@ -17,24 +18,20 @@ function balise_CONFIG($p) {
 	if (!$arg || strpos($arg, '/') === false) {
 		return balise_CONFIG_dist($p);
 	}
+	$sinon = interprete_argument_balise(2,$p);
 	$arg = explode('/', $arg);
-	$fin = $deb = '';
-	for ($i = 0; $i < count($arg); ++$i) {
-		if ($i == count($arg) - 1) {
-			$arg[$i] = substr($arg[$i], 0, -1);
-		}
-		if (!$i) {
-			$arg[$i] = substr($arg[$i], 1);
-		}
+	$arg[count($arg) - 1] = substr($arg[count($arg) - 1], 0, -1);
+	$p->code = "((\$cfgarr=unserialize(\$GLOBALS['meta'][{$arg[0]}']))? \$cfgarr";
+	for ($i = 1; $i < count($arg); ++$i) {
 		if (!is_numeric($arg[$i])) {
 			$arg[$i] = "'" . $arg[$i] . "'";
 		}
-	}
-	$p->code = "((\$cfgarr=unserialize(\$GLOBALS['meta'][{$arg[0]}]))? \$cfgarr";
-	for ($i = 1; $i < count($arg); ++$i) {
 		$p->code .= '[' . $arg[$i] . ']';
 	}
 	$p->code .= ": '')";
+	if ($sinon) {
+		$p->code = 'sinon(' . $p->code . ",$sinon)";
+	}
 	return $p;
 }
 
