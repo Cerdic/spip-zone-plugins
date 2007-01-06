da300a1e8a1d86e1c2285adfc0af04678f2336ce
bertrand@toggg.com <bertrand@toggg.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-01-06 15:22:03 +0000

    Une premiere victime de cfg, si plugin cfg installe, essayer ecrire/?exec=cfg&cfg=boutonstexte :)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@8567 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/boutonstexte/teteboutonstexte.php
+++ _stable_/boutonstexte/teteboutonstexte.php
@@ -2,18 +2,25 @@
 // insert le css et le js externes pour boutonstexte dans le <head> du document (#INSERT_HEAD)
 function boutonstexte_insert_head($flux)
 {
-	$jsFile = find_in_path('boutonstexte.js');
-	$cssFile = find_in_path('boutonstexte.css');
-	$imgPath = dirname(find_in_path('images/fontsizeup.png'));
+	$metacfg = array(
+		'jsFile' => 'boutonstexte.js',
+		'cssFile' => 'boutonstexte.css',
+		'imgPath' => 'images/fontsizeup.png',
+		'txtOnly' => 'boutonstexte:texte_seulement',
+		'txtBackSpip' => 'boutonstexte:retour_a_spip',
+		'txtSizeUp' => 'boutonstexte:augmenter_police',
+		'txtSizeDown' => 'boutonstexte:diminuer_police'
+	);
+	meta_boutonstexte($metacfg);
+	
+	$jsFile = find_in_path($metacfg['jsFile']);
+	$cssFile = find_in_path($metacfg['cssFile']);
+	$imgPath = dirname(find_in_path($metacfg['imgPath']));
 
-	$txtOnly = addslashes(unicode_to_javascript(html2unicode(_T(
-		'boutonstexte:texte_seulement'))));
-	$txtBackSpip = addslashes(unicode_to_javascript(html2unicode(_T(
-		'boutonstexte:retour_a_spip'))));
-	$txtSizeUp = addslashes(unicode_to_javascript(html2unicode(_T(
-		'boutonstexte:augmenter_police'))));
-	$txtSizeDown = addslashes(unicode_to_javascript(html2unicode(_T(
-		'boutonstexte:diminuer_police'))));
+	$txtOnly = txt_boutonstexte($metacfg['txtOnly']);
+	$txtBackSpip = txt_boutonstexte($metacfg['txtBackSpip']);
+	$txtSizeUp = txt_boutonstexte($metacfg['txtSizeUp']);
+	$txtSizeDown = txt_boutonstexte($metacfg['txtSizeDown']);
 
 	$incHead = <<<EOH
 <link rel="stylesheet" href="$cssFile" type="text/css" media="all" />
@@ -30,5 +37,32 @@ function boutonstexte_insert_head($flux)
 EOH;
 
 	return preg_replace('#(</head>)?$#i', $incHead . "\$1\n", $flux, 1);
+}
+	
+function txt_boutonstexte($txt)
+{
+	if (!$txt || $txt == '_') {
+		return '';
+	}
+	return addslashes(unicode_to_javascript(html2unicode(_T($txt))));
+}
+	
+function meta_boutonstexte(&$metacfg)
+{
+	include_spip('inc/meta');
+	lire_metas();
+    global $meta;
+    if (empty($meta['boutonstexte'])) {
+    	return 0;
+    }
+   	$return = 0;
+    $metabtxt = unserialize($meta['boutonstexte']);
+    foreach ($metabtxt as $o=>$v) {
+    	if (isset($metacfg[$o])) {
+    		$metacfg[$o] = $v;
+		   	++$return;
+    	}
+    }
+    return $return;
 }
 ?>
