e23645b274b242bab6ccb25f31ce541dcb6a32bc
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-01-08 18:19:36 +0000

    "un fichier de lang oublie"
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@8682 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _stable_/w3c_go_home/lang/w3cgh_fr.php
@@ -0,0 +1,22 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+'erreur' => 'Erreur',
+'une_erreur' => 'Une erreur',
+'erreurs' => '@erreurs@ erreurs',
+'page_valide' => 'Page Valide',
+'reset_all' => 'Tout R&eacute;initialiser',
+'reset' => 'R&eacute;initialiser',
+'tester_page' => 'Tester cette page',
+'titre_formulaire_choix_validateur' => "Verifier la conformit&eacute; avec les validateurs&nbsp;:",
+'titre_page' => 'Validation Site Xhtml&amp;Accessibilit&eacute;',
+'titre_tableau_conformite' => 'Conformit&eacute; du site',
+'titre_conformite_page' => 'Conformit&eacute; de la page :',
+'resultat_pages_conformes' => "@nb@ sur @tot@ pages conformes selon le validateur @nom@",
+'resultat_pages_completement_conformes' => "@nb@ sur @tot@ pages compl&egrave;tement conformes",
+);
+
+
+?>
