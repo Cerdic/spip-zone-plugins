298d0da7d524126ba6fbd47286d1e17a42bfd1d1
fil@rezo.net <fil@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-01-08 13:50:34 +0000

    suite au commentaire du depot [4140], passage de thickbox dans le pipeline header_prive, maintenant qu'il n'y a plus de onload ; helas il semble que onajaxload() ne fonctionne pas ici (??)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@8662 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/thickbox/head.php
+++ _test_/thickbox/head.php
@@ -5,7 +5,7 @@ function ThickBox1_insert_head($flux){
 // on ajoute la class thickbox aux liens de type="image/xxx"
 
 // TODO: ne charger thickbox.js et thickbox.css que si 
-// $("a.thickbox,a[@type='image/jpeg'],...").size() > 0)
+// jQuery("a.thickbox,a[@type='image/jpeg'],...").size() > 0)
 
 $flux .=
 
@@ -17,7 +17,7 @@ $flux .=
 // Inside the function "this" will be "document" when called by ready() 
 // and "the ajaxed element" when called because of onAjaxLoad 
 var init_f = function() {
-	if ($("a.thickbox,a[@type=\'image/jpeg\'],a[@type=\'image/png\'],a[@type=\'image/gif\']",this).addClass("thickbox").size()) {
+	if (jQuery("a.thickbox,a[@type=\'image/jpeg\'],a[@type=\'image/png\'],a[@type=\'image/gif\']",this).addClass("thickbox").size()) {
 		TB_chemin_animation = "'.url_absolue(find_in_path('circle_animation.gif')).'";
 		TB_chemin_close = "'.url_absolue(find_in_path('close.gif')).'";
 		TB_chemin_css = "'.url_absolue(find_in_path('thickbox.css')).'";
@@ -26,7 +26,7 @@ var init_f = function() {
 }
 //onAjaxLoad is defined in private area only
 if(typeof onAjaxLoad == "function") onAjaxLoad(init_f);
-$(document).ready(init_f);
+jQuery(init_f);
 // --></script>';
 
 	return $flux;
