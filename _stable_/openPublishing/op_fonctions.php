edccabf72072e49d0a6f67a5f440bfb5d470b268
edd@no-log.org <edd@no-log.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-01-10 00:36:22 +0000

    ajout boucle op_auteurs retournant le nom, email, phone, group d'un auteur anonyme
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@8755 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/openPublishing/op_fonctions.php
+++ _stable_/openPublishing/op_fonctions.php
@@ -5,13 +5,24 @@ $GLOBALS['contrib_non_local'] = 14;
 $GLOBALS['contrib_analyse'] = 24;
 
 /*
- * <BOUCLE(OP_RUBRIQUES)>
+ * <BOUCLE(op_rubriques)>
  */
 
 function boucle_OP_RUBRIQUES_dist($id_boucle, &$boucles) {
         $boucle = &$boucles[$id_boucle];
         $id_table = $boucle->id_table;
         $boucle->from[$id_table] =  "spip_op_rubriques";
+        return calculer_boucle($id_boucle, $boucles);
+}
+
+/*
+ * <BOUCLE(op_auteurs)>
+ */
+
+function boucle_OP_AUTEURS_dist($id_boucle, &$boucles) {
+        $boucle = &$boucles[$id_boucle];
+        $id_table = $boucle->id_table;
+        $boucle->from[$id_table] =  "spip_op_auteurs";
         return calculer_boucle($id_boucle, $boucles);
 }
 
