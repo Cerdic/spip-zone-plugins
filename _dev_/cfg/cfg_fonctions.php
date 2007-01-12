fc537ed49f44879c58b5764c4984ad369e8ff353
bertrand@toggg.com <bertrand@toggg.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-01-12 09:58:40 +0000

    balise inline , le retour , en fait il fallait juste passer *toujours* par le code de cfg , quel idiot :)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@8839 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/cfg/cfg_fonctions.php
+++ _dev_/cfg/cfg_fonctions.php
@@ -1,5 +1,4 @@
 <?php
-include_spip('balise/config');
 /* etend la balise #CONFIG 
  *
  *  cfg plugin for spip (c) toggg 2007 -- licence LGPL
@@ -14,6 +13,33 @@ include_spip('balise/config');
 //
 // La balise appelle celle de la dist si pas de /
 //
+
+function cfg_meta($cfg = '', $def = NULL)
+{
+	include_spip('inc/meta');
+	lire_metas();
+	global $meta;
+	$ret = '';
+	if (!$cfg) {
+		$ret = serialize($meta);
+	} elseif (strpos($cfg, '/') === false) {
+		$ret = $meta[$cfg];
+	} else {
+		$cfg = explode('/', $cfg);
+		$ret = $cfg[0] ? unserialize($meta[$cfg[0]]) : $meta;
+		for ($i = 1; $ret && $i < count($cfg) && $cfg[$i] !== ''; ++$i) {
+			$ret = is_array($ret) ? (is_numeric($cfg[$i]) ? $ret[0 + $cfg[$i]] : $ret[$cfg[$i]]) : '';
+		}
+	}
+	return !$ret && $def ? $def : $ret;
+}
+function balise_CONFIG($p)
+{
+	$arg = interprete_argument_balise(1,$p);
+	$sinon = interprete_argument_balise(2,$p);
+	$p->code = 'cfg_meta(' . $arg . ($sinon ? ",$sinon)" : ')');
+	return $p;
+}
 
 // lire_cfg() permet de recuperer une config depuis le php
 // $cfg: la config, lire_cfg('montruc') est un tableau
