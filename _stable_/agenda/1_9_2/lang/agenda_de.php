76d6ffa119641e646d5e114f6679914c64194dba
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-01-20 01:19:49 +0000

    allez un agenda en allemand... Certainement des erreurs... Si des germanistes utilisent ... ils nous le diront certainement
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@9016 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _stable_/agenda/1_9_2/lang/agenda_de.php
@@ -0,0 +1,40 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+'bouton_annuler' => 'Storno',
+'activite_editoriale' => 'Activité Éditoriale',  'editoriale Aktivität',
+'ajouter_un_evenement' => 'ajouter un événement à cet article', 'diesem Artikel ein event hinzufügen',
+'texte_agenda' => 'AGENDA',
+'titre_cadre_ajouter_evenement' => 'Event hinzufügen',
+'titre_cadre_modifier_evenement' => 'Event umändern',
+'item_mots_cles_association_evenements' => 'zu den events',
+'info_un_evenement' => 'ein event',
+'info_nombre_evenements' => '@nb_evenements@ event',
+'info_evenements' => 'Event',
+'evenements' => 'Event',
+'evenement_titre' => 'Titel',
+'evenement_descriptif' => 'Beschreibung',
+'evenement_lieu' => 'Ort',
+'evenement_date' => 'Datum',
+'evenement_date_de' => 'Von',
+'evenement_date_du' => 'Vom',
+'evenement_date_a' => 'im',
+'evenement_date_au' => 'bei',
+'evenement_date_a_immediat' => 'zu',
+'evenement_date_debut' => 'Anfangsdatum',
+'evenement_date_fin' => 'Enddatum',
+'lien_retirer_evenement' => 'Löschen',
+'evenement_horaire' => 'Zeitplan',
+'titre_sur_l_agenda' => 'Im Agenda',
+'icone_creer_evenement' => 'Neue Veranstaltung anlegen',
+'evenement_repetitions' => 'Wiederholungen',
+ 'repetition_de' => 'Wiederholung von',
+'sans_titre' => 'ohne Titel',
+'evenement_autres_occurences' => 'Autres occurences :',
+'bouton_supprimer' => 'Löschen',
+);
+
+
+?>
