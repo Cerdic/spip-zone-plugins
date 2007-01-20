0fd4f9befc1f40f71f1bf4db7ed3771e0740c523
bertrand@toggg.com <bertrand@toggg.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-01-20 18:27:54 +0000

    Preparation des modeles de test
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@9033 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/cfg/tests/lire_config.php
+++ _dev_/cfg/tests/lire_config.php
@@ -1,7 +1,7 @@
 <?php
 
-	$test = 'cfg:lire_config';
-	require '../../../tests/test.inc';
+$test = 'cfg:lire_config';
+require '../../../tests/test.inc';
 
 include_spip('cfg_options');
 
@@ -13,6 +13,7 @@ $GLOBALS['meta'] = array(
 	'serie' => serialize($assoc)
 );
 $sermeta = serialize($GLOBALS['meta']);
+
 $essais = array(
 // presents
 	array(array(), $GLOBALS['meta'], $GLOBALS['meta'], $sermeta),
@@ -35,8 +36,11 @@ $essais = array(
 );
 $ok = true;
 $r = array(null, array(), array(), array());
+$s = array(1, '', '');
 $err = array();
 $fun = 'lire_config';
+$bal = '#CONFIG{';
+
 foreach ($essais as $i => $spec) {
 	if (!is_array($spec[0])) {
 		$spec[0] = array($spec[0]);
@@ -46,11 +50,15 @@ foreach ($essais as $i => $spec) {
 			$r[1][$i] = $fun();
 			$r[2][$i] = $fun(null, 'defaut');
 			$r[3][$i] = $fun(null, null, true);
+			$s[1] .= '(((' . $i . ')))' . $bal . "}\n";
+			$s[2] .= '(((' . $i . ')))' . $bal . "'',defaut" . "}\n";
 		break;
 		case 1:
 			$r[1][$i] = $fun($spec[0][0]);
 			$r[2][$i] = $fun($spec[0][0], 'defaut');
 			$r[3][$i] = $fun($spec[0][0], null, true);
+			$s[1] .= '(((' . $i . ')))' . $bal . $spec[0][0] . "}\n";
+			$s[2] .= '(((' . $i . ')))' . $bal . $spec[0][0] . ',defaut' . "}\n";
 		break;
 /*		case 2:
 			$r[1][$i] = $fun($spec[0][0], $spec[0][1]);
@@ -60,9 +68,9 @@ foreach ($essais as $i => $spec) {
 */
 	}
 	for ($nbarg = 1; $nbarg < 4; ++$nbarg) {
-		if ($r[$nbarg][$i] !== ($s = isset($spec[$nbarg]) ? $spec[$nbarg] : $spec[1])) {
-			$err[] = $i . " $nbarg (" . print_r($r[$nbarg][$i], true) .
-				') attendu (' . print_r($s, true) . ')';
+		if ($r[$nbarg][$i] !== ($res = isset($spec[$nbarg]) ? $spec[$nbarg] : $spec[1])) {
+			$err[] = $i . "({$essais[$i][0]}) $nbarg (" . print_r($r[$nbarg][$i], true) .
+				') attendu (' . print_r($res, true) . ')';
 		}
 	}
 }
@@ -79,4 +87,6 @@ if ($_GET['dump']) {
 	echo "<div>\n" . print_r($r[1], true) . "</div>\n";
 	echo "<div>\n" . print_r($r[2], true) . "</div>\n";
 	echo "<div>\n" . print_r($r[3], true) . "</div>\n";
+	echo "<div>\n" . print_r($s[1], true) . "</div>\n";
+	echo "<div>\n" . print_r($s[2], true) . "</div>\n";
 }
