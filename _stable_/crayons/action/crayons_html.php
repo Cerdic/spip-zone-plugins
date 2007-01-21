c6f42314dfd14825cf29fccca7be508e56565603
bertrand@toggg.com <bertrand@toggg.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-01-21 19:04:27 +0000

    Tiens, tetue :) On protege contre les mauvaises insertions de inserer_attribut() de chez core ... tsk tsk
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@9070 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/crayons/action/crayons_html.php
+++ _stable_/crayons/action/crayons_html.php
@@ -212,7 +212,7 @@ class Crayon {
                 case 'texte':
                     $input = '<textarea class="crayon-active"'
                     . ' name="content_'.$this->key.'_'.$champ.'">'
-                    . entites_html($val)
+                    . chr(255)
                     . '</textarea>'."\n";
                     break;
                 case 'ligne':
@@ -220,18 +220,19 @@ class Crayon {
                     $input = '<input class="crayon-active" type="text"'
                     . ' name="content_'.$this->key.'_'.$champ.'"'
                     . ' value="'
-                    . entites_html($val)
+                    . chr(255)
                     . '" />'."\n";
             }
             if (is_array($spec) && isset($spec[$champ]['attrs'])) {
-	            foreach ($spec[$champ]['attrs'] as $attr=>$val) {
-	                $input = inserer_attribut($input, $attr, $val);
+	            foreach ($spec[$champ]['attrs'] as $attr=>$vala) {
+	                $input = inserer_attribut($input, $attr, $vala);
 	            }
             }
 
-            foreach ($attrs as $attr=>$val) {
-                $input = inserer_attribut($input, $attr, $val);
+            foreach ($attrs as $attr=>$vala) {
+                $input = inserer_attribut($input, $attr, $vala);
             }
+            $input = str_replace(chr(255), entites_html($val), $input);
             $return .= $input;
         }
         return $return;
