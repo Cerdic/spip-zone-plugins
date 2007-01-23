8394891bba1e7a49c6e023905e27847c12aaa1d5
patfr@ifrance.com <patfr@ifrance.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-01-23 23:30:20 +0000

    traductions dans la langue d'outre manche (merci Zzz)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@9160 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _stable_/en_travaux/lang/entravaux_en.php
@@ -0,0 +1,13 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+'activer_message' => 'Activate maintenance mode:',
+'en_travaux' => 'Under construction',
+'parametrage_page_travaux' => 'Settings for your maintenance page',
+'info_message' => 'This page lets you set up a temporary message which will be displayed on all the pages of your site during a maintenance period.',
+'message_temporaire' => 'Your temporary message:',
+
+);
+?>
