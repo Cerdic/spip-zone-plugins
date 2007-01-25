9b5b2f017a94012cec1e0414da4ab7f3aedd7699
booz@rezo.net <booz@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-01-25 22:41:46 +0000

    un zoom dans le slideshow qui pete pas
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@9235 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/thickbox2/thickbox.js
+++ _dev_/thickbox2/thickbox.js
@@ -377,7 +377,13 @@ function TB_remove() {
 function TB_position() {
   var pagesize = TB_getPageSize();  
   var arrayPageScroll = TB_getPageScrollTop();  
- if(!FULL_S){
+ if(FULL_S && DIAPO){
+ 	FULL_S = false ;
+	$("#TB_window").animate({top: (arrayPageScroll[1] + (pagesize[1]-TB_HEIGHT)/2),left:(arrayPageScroll[0] + (pagesize[0] - TB_WIDTH)/2), width:TB_WIDTH,height:TB_HEIGHT},1500);         
+	$("#TB_Image").animate({top:20,left:20,width:(TB_WIDTH - 20),height:(TB_HEIGHT - 20)},1500,TB_recadre);         
+		       
+ }
+ if(!FULL_S || DIAPO){
   $("#TB_window").css({width:TB_WIDTH+"px",left: (arrayPageScroll[0] + (pagesize[0] - TB_WIDTH)/2)+"px", top: (arrayPageScroll[1] + (pagesize[1]-TB_HEIGHT)/2)+"px" });
   }    
 
