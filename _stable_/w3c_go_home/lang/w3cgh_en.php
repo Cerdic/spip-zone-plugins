7dc639067edfc761650ad595ccf6e522dab3e262
pierre.andrews@gmail.com <pierre.andrews@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-01-30 14:07:53 +0000

    quelques traductions en anglais.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@9341 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _stable_/w3c_go_home/lang/w3cgh_en.php
@@ -0,0 +1,25 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+'bouton_arreter' => 'Stop the tests',
+'tester_10' => '10 Tests',
+'tester_tout' => 'Test Everything!',
+'erreur' => 'Error',
+'une_erreur' => 'One error',
+'erreurs' => '@erreurs@ errors',
+'page_valide' => 'Page Valid',
+'reset_all' => 'Reinitialise everything',
+'reset' => 'Reinitialise',
+'tester_page' => 'Test this page',
+'titre_formulaire_choix_validateur' => "Check with the validator&nbsp;:",
+'titre_page' => 'Xhtml&amp;Accessibility Site Validation;',
+'titre_tableau_conformite' => 'Validity of the site',
+'titre_conformite_page' => 'Validity of the page :',
+'resultat_pages_conformes' => "@nb@ of @tot@ pages are valid according to the validator @nom@",
+'resultat_pages_completement_conformes' => "@nb@ on @tot@ pages are fully valid",
+);
+
+
+?>
