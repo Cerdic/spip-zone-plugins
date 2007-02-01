1cedff0ba2dbaa6a30390f6876099919f2af26bd
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-02-01 00:23:54 +0000

    "un debut de dev pour de l'export au format odt pas documente, pas essayable, en travaux"
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@9370 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _dev_/export_odt/content_fonctions.php
@@ -0,0 +1,24 @@
+<?php
+function avant_propre($texte){
+	
+//<text:span text:style-name="T1">gras</text:span>
+	return $texte;
+}
+
+function apres_propre($texte){
+	
+	$subst = array(
+	",<strong [^>]*>,"=>'<text:span text:style-name="spip_gras">',
+	",</strong>,"=>'</text:span>',
+	",<i [^>]*>,"=>'<text:span text:style-name="spip_italic">',
+	",</i>,"=>'</text:span>',
+	);
+	$texte = preg_replace(array_keys($subst),array_values($subst),$texte);
+	
+	// convertir les paragraphes
+	$texte = preg_replace(",^\s*<p [^>]*>(.*)</p>\s*$,ms","\\1",$texte);
+	$texte = unicode2charset($texte);
+
+	return $texte;
+}
+?>
