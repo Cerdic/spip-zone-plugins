60da31f634f4494111c031aa2f6dc3deec294ab3
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-02-04 10:53:46 +0000

    ne pas appeler les fonctions d'install en 1.9.2, c'est spip qui s'en occupe !
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@9442 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/agenda_alternatives/minicalalter_pipelines.php
+++ _test_/agenda_alternatives/minicalalter_pipelines.php
@@ -9,18 +9,20 @@
 
 function CalAlter_header_prive($flux){
 	// s'assurer en prive que les tables sont crees
-	include_spip('inc/agenda_gestion'); // spip 1.9
-	if (!function_exists('Agenda_install')) include_spip('base/agenda_upgrade'); // spip 1.9.2
-	if (function_exists('Agenda_install')) Agenda_install();
+	if ($GLOBALS['spip_version_code']<1.92) {
+		include_spip('inc/agenda_gestion'); // spip 1.9
+	 	if function_exists('Agenda_install')) Agenda_install();
+	}
 	return $flux;
 }
 
 function CalAlter_insert_head($flux){
-	include_spip('inc/agenda_gestion'); // spip 1.9
-	if (!function_exists('Agenda_install')) include_spip('base/agenda_upgrade'); // spip 1.9.2
-	if (!function_exists('Agenda_install')) {
-		include_spip('inc/minipres');
-		minipres(_T('minicalalter_activer_agenda'));
+	if ($GLOBALS['spip_version_code']<1.92){
+		include_spip('inc/agenda_gestion'); // spip 1.9
+		if (!function_exists('Agenda_install')) {
+			include_spip('inc/minipres');
+			minipres(_T('minicalalter_activer_agenda'));
+		}
 	}
 	$flux .= "
 	<style type=\"text/css\">
