9e2052848e8817f33126df3ca0aeb8d16b36c8e5
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-02-05 10:51:14 +0000

    inclure manquant et code mort
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@9470 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/charts/exec/charts_tous.php
+++ _stable_/charts/exec/charts_tous.php
@@ -13,13 +13,10 @@ function charts_duplique_chart(){
 	$duplique = intval(_request('duplique_chart'));
 	if ($duplique && charts_chart_administrable($duplique)){
 		// creation
-		$structure = array();
-		spip_query("INSERT INTO spip_charts (structure) VALUES ('".
-			addslashes(serialize($structure))."')");
-		$id_chart = spip_insert_id();
 		$query = "SELECT * FROM spip_charts WHERE id_chart=$duplique";
 		$result = spip_query($query);
 		if ($row = spip_fetch_array($result)) {
+			include_spip("base/abstract_sql");
 			spip_abstract_insert("spip_charts","(titre,descriptif,largeur,hauteur,code,background)",
 			"(".
 				spip_abstract_quote(_T('charts:copie_de',array('titre'=>$row['titre']))).", ".
@@ -29,7 +26,7 @@ function charts_duplique_chart(){
 				spip_abstract_quote($row['code']).", ".
 				spip_abstract_quote($row['background']) . ")");
 		}
-	}	
+	}
 }
 
 function exec_charts_tous(){
