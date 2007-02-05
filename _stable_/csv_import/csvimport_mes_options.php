9550a496051b3c81537ac68456cd3675b79643ac
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-02-05 11:11:56 +0000

    bug
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@9472 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/csv_import/csvimport_mes_options.php
+++ _stable_/csv_import/csvimport_mes_options.php
@@ -3,7 +3,7 @@
 // pour qu'elles soient prises en compte dans le dump
 if (isset($GLOBALS['meta']['csvimport_tables_auth'])){
 	global $tables_auxiliaires;
-	$csvimport_liste = array_keys(unserialize($GLOBALS['meta']['csvimport_tables_auth']));
+	$csvimport_liste = unserialize($GLOBALS['meta']['csvimport_tables_auth']);
 	foreach($csvimport_liste as $csvimport_table=>$csvimport_infos){
 		if ($csvimport_infos['dyn_declare_aux'])
 			$tables_auxiliaires[$csvimport_table]=false; // on l'init pour savoir qu'elle existe, mais false pour que le compilo fasse un show_table si besoin
