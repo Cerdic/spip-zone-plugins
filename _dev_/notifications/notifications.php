4da013c16b11f2d8d093adf2b6bf95d5e870994b
fil@rezo.net <fil@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-02-07 01:24:52 +0000

    plugin 'notifications', premier morceau fonctionnel = les signatures de petition (pour spip.net)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@9510 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _dev_/notifications/notifications.php
@@ -0,0 +1,76 @@
+<?php
+
+
+function Notifications_pre_edition($x) {
+	spip_log($x);
+
+	if (isset($x['args']['table'])) {
+		$notif = 'Notifications_'.$x['args']['table'];
+		if (function_exists($notif)) {
+			$x = $notif($x);
+		}
+	}
+
+	return $x;
+}
+
+
+// insertion d'une nouvelle signature => mail aux moderateurs
+// definis par la constante _SPIP_MODERATEURS_PETITION
+function Notifications_spip_signatures($x) {
+	if (!defined('_SPIP_MODERATEURS_PETITION')
+	OR !_SPIP_MODERATEURS_PETITION)
+		return $x;
+
+	$id_signature = $x['args']['id_objet'];
+
+	$s = spip_query($q = "SELECT * FROM spip_signatures WHERE id_signature="._q($id_signature));
+
+	if ($t = spip_fetch_array($s)) {
+
+		$a = spip_fetch_array(spip_query("SELECT titre,lang FROM spip_articles WHERE id_article="._q($t['id_article'])));
+		lang_select($a['lang']);
+
+		$url = generer_url_article($t['id_article']);
+
+
+		// creer la cle de suppression de la signature
+		include_spip('inc/securiser_action');
+		$cle = _action_auteur("supprimer signature $id_signature", '', '', 'alea_ephemere');
+		$url_suppr =
+			parametre_url(
+			parametre_url($url,
+				'var_confirm', $t['id_signature'], '&'),
+				'refus', $cle, '&').'#sp'.$t['id_article'];
+
+		$sujet = _L("Nouvelle signature pour la petition ").typo(supprimer_numero($a['titre']));
+
+		$texte = $sujet
+			. "\n\n***"
+			. url_absolue($url)
+			. "\n"
+			. "\nNom: ". $a['nom_email']
+			. "\nemail: ". $a['ad_email']
+			. "\nmessage: ". $a['message']
+			. "\n\nnom_site: ". $a['nom_site']
+			. "\nurl_site: ". $a['url_site']
+			. "\n\n"
+			. "Cette signature n'a pas encore ete validee ;"
+			. "\nsi vous souhaitez la supprimer directement :"
+			. "\n"
+			. url_absolue($url_suppr)
+			;
+
+		include_spip('inc/mail');
+		envoyer_mail(_SPIP_MODERATEURS_PETITION,
+			$sujet, $texte);
+
+		lang_dselect();
+
+	}
+
+	return $x;
+}
+
+
+?>
