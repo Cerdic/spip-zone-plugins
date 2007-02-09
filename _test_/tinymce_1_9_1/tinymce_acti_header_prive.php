f608dcf74e41a12c7be5adae297e1ef8490bd921
cdespres69@free.fr <cdespres69@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-02-09 15:24:51 +0000

    utilisation uniquement des fonctions interne à Spip. Aucune librairie externe necessaire.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@9553 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/tinymce_1_9_1/tinymce_acti_header_prive.php
+++ _test_/tinymce_1_9_1/tinymce_acti_header_prive.php
@@ -7,32 +7,34 @@ function tinymce_acti_header_prive($flux) {
 	
 	//vérifie que les fichiers de TinyMCE sont présents, sinon les installe
 	if (!is_file(_DIR_TINYMCE_FILES.'/tiny_mce.js')) {
-		require_once 'includes/zip/dUnzip2.inc.php';
+		include_spip("inc/distant");
+		include_spip("inc/flock");
+		include_spip("inc/pclzip");
 		
 		//récupère l'archive de TinyMCE sur le web et la met dans le répertoire du plugin
-		$zip_content = file_get_contents(_PLUGIN_TINYMCE_ARCHIVE_URL);
-		if (!file_put_contents(_DIR_PLUGIN_TINYMCE.'/files/tinymce_archive.zip', $zip_content))
+		$zip_content = spip_file_get_contents(_PLUGIN_TINYMCE_ARCHIVE_URL);
+		if (!ecrire_fichier(_DIR_RACINE.'/IMG/tinymce_archive.zip', $zip_content, true, false))
 			return;
 		//dézippe l'archive récupérée		
-		$zip = new dUnzip2(_DIR_PLUGIN_TINYMCE.'/files/tinymce_archive.zip');
-		$zip->unzipAll(_DIR_PLUGIN_TINYMCE.'', 'tinymce/jscripts/');
-		@unlink(_DIR_PLUGIN_TINYMCE.'/files/tinymce_archive.zip');
+		$zip = new PclZip(_DIR_RACINE.'/IMG/tinymce_archive.zip');
+		$zip->extract(_DIR_PLUGIN_TINYMCE);
+		supprimer_fichier(_DIR_RACINE.'/IMG/tinymce_archive.zip');
 		
 		//dézippe les plugins filemanager et ibrowser
-		$zip = new dUnzip2(_DIR_PLUGIN_TINYMCE.'/files/filemanager.zip');
-		$zip->unzipAll(_DIR_TINYMCE_FILES.'/plugins/');
-		$zip = new dUnzip2(_DIR_PLUGIN_TINYMCE.'/files/ibrowser.zip');
-		$zip->unzipAll(_DIR_TINYMCE_FILES.'/plugins/');
+		$zip = new PclZip(_DIR_RACINE.'/IMG/filemanager.zip');
+		$zip->extract(_DIR_TINYMCE_FILES.'/plugins/');
+		$zip = new PclZip(_DIR_RACINE.'/IMG/ibrowser.zip');
+		$zip->extract(_DIR_TINYMCE_FILES.'/plugins/');
 		
 		//télécharge les packages de langues TinyMCE nécessaires sur le web (si on veut autre chose que l'Anglais)
 		if (!empty(${_PLUGIN_TINYMCE_LANGUAGES_PACK_VARNAME})) {
-			$zip_content = file_get_contents(_PLUGIN_TINYMCE_LANGUAGES_URL.'?dlang[]='.implode('&dlang[]=',${_PLUGIN_TINYMCE_LANGUAGES_PACK_VARNAME}).'&format=zip&submit=Download');
-			if (!file_put_contents(_DIR_PLUGIN_TINYMCE.'/files/tinymce_languages_archive.zip', $zip_content))
+			$zip_content = spip_file_get_contents(_PLUGIN_TINYMCE_LANGUAGES_URL.'?dlang[]='.implode('&dlang[]=',${_PLUGIN_TINYMCE_LANGUAGES_PACK_VARNAME}).'&format=zip&submit=Download', 'force');
+			if (!ecrire_fichier(_DIR_RACINE.'/IMG/tinymce_languages_archive.zip', $zip_content))
 				return;
 			//dézippe l'archive de langues récupérée		
-			$zip = new dUnzip2(_DIR_PLUGIN_TINYMCE.'/files/tinymce_languages_archive.zip');
-			$zip->unzipAll(_DIR_PLUGIN_TINYMCE.'', 'tinymce/jscripts/');
-			@unlink(_DIR_PLUGIN_TINYMCE.'/files/tinymce_languages_archive.zip');
+			$zip = new PclZip(_DIR_RACINE.'/IMG/tinymce_languages_archive.zip');
+			$zip->extract(_DIR_PLUGIN_TINYMCE.'');
+			supprimer_fichier(_DIR_RACINE.'/IMG/tinymce_languages_archive.zip');
 		}
 	}
 	//fin de vérification que TinyMCE est bien installé
