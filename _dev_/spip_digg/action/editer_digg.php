5565a9d705a4d6f57b3a8e09d0c8e62d101a19f7
crovvfoot@gmail.com <crovvfoot@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-02-11 21:52:59 +0000

    Ajout de la gestion de la MAJ
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@9592 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/spip_digg/action/editer_digg.php
+++ _dev_/spip_digg/action/editer_digg.php
@@ -9,11 +9,11 @@ include_spip("inc/presentation");
 function action_editer_digg_dist(){	
 	$insert = false;
 	$id_digg = _request('id_digg');
+	$id_rubrique = _request('id_rubrique');
+	$id_secteur = '';
 	if ($id_digg == "new") {
 		$id_auteur = $GLOBALS['auteur_session']['id_auteur'];
 		if (lire_meta('spipdigg_type_moderation')  != 0)  { $date_validation = "0000-00-00 00:00:00"; }else{ $date_validation = "CURRENT_TIMESTAMP"; }
-		$id_rubrique = _request('id_rubrique');
-		$id_secteur = '';
 		$sql_insert_digg = "INSERT INTO spip_diggs SET id_digg='', titre='".addslashes(_request('titre'))."', descriptif='".addslashes(_request('texte'))."', url_digg='".addslashes(_request('url_digg'))."', id_rubrique='".$id_rubrique."', id_secteur='".$id_secteur."', date=CURRENT_TIMESTAMP, date_modif=CURRENT_TIMESTAMP, hits='0', points='0', date_validation='".$date_validation."', statut='prepa';";
 		$res_insert_digg = spip_query($sql_insert_digg);
 		$id_digg = mysql_insert_id();
@@ -22,7 +22,7 @@ function action_editer_digg_dist(){
 		if ($res_insert_digg && $res_lier_digg_auteur) $insert = true;
 		//echo $sql_insert_digg;
 	}else{
-		$sql_maj_digg = "UPDATE spip_diggs SET titre='".addslashes(_request('titre'))."', url_digg='".addslashes(_request('url_digg'))."', descriptif='".addslashes(_request('texte'))."' WHERE id_digg='".$id_digg."';";
+		$sql_maj_digg = "UPDATE spip_diggs SET titre='".addslashes(_request('titre'))."', url_digg='".addslashes(_request('url_digg'))."', descriptif='".addslashes(_request('texte'))."', id_rubrique='"._request('id_rubrique')."' WHERE id_digg='".$id_digg."';";
 		//echo $sql_maj_digg;
 		$res_maj_digg = spip_query($sql_maj_digg);
 	}
