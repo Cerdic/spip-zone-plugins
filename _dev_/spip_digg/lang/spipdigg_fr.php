b94f5e1be8f243a38cb2f700d2153f7f8cd6dd0b
crovvfoot@gmail.com <crovvfoot@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-02-11 02:25:57 +0000

    Moteur de Digg pour spip. Commencement du projet ...
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@9579 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _dev_/spip_digg/lang/spipdigg_fr.php
@@ -0,0 +1,30 @@
+<?php
+//
+// lang/spipdigg_fr.php
+//
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+
+// A
+'ajouter_un_digg'=>'Ajouter un Digg\'s',
+// C
+'configurer_les_digg'=>'Configurer Spip-Digg',
+'descriptif_digg'=>'Descriptif :',
+'enregistrer'=>'Enregistrer',
+'editer_digg'=>'Editer le Digg',
+// F
+'forum_des_diggs'=>'Forum des Digg\'s',
+// G
+'gerer_les_diggs'=>'Gerer les Digg\'s',
+// S
+'mes_diggs'=>'Mes Digg\'s',
+'stats_des_diggs'=> 'Statistiques des Digg\'s',
+// T
+'titre_digg'=>'Titre du Digg :',
+// U
+'url_digg'=>'Url du Digg :'
+);
+?>
