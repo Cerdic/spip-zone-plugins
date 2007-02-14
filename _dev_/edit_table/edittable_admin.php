178a77831337af4473f4c75a272fd1fca19c39fe
crovvfoot@gmail.com <crovvfoot@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-02-14 00:48:59 +0000

    Problème de trad.. réglé
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@9653 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/edit_table/edittable_admin.php
+++ _dev_/edit_table/edittable_admin.php
@@ -7,7 +7,7 @@ $p=explode(basename(_DIR_PLUGINS)."/",str_replace('\\','/',realpath(dirname(__FI
 define('_DIR_PLUGIN_EDITTABLE',(_DIR_PLUGINS.end($p)));
 
 function edittable_ajouter_boutons($flux){
-	$flux['configuration']->sousmenu['edittable']= new Bouton("../"._DIR_PLUGIN_edittable."/img_pack/edittable.png",_T('edittable:editer_les_tables'));
+	$flux['configuration']->sousmenu['listetable']= new Bouton("../"._DIR_PLUGIN_edittable."/img_pack/edittable.png",_T('edittable:editer_les_tables'));
 	return $flux;
 }
 
