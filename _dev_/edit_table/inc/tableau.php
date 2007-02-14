fb48b48c18464f7078657915d63b157de013d375
crovvfoot@gmail.com <crovvfoot@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-02-14 00:49:24 +0000

    ajout de editer_tableau_div();
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@9654 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/edit_table/inc/tableau.php
+++ _dev_/edit_table/inc/tableau.php
@@ -12,6 +12,22 @@ function afficher_tableau($tableau){
 	echo '</table>';
 }
 
+function afficher_tableau_div($tableau){
+	echo '<div>';
+	foreach($tableau as $cle=>$valeur)
+	{
+		echo '<span><b>'.$cle.'</b><br /><em>'.$valeur.'</em></span><br />';
+	} 
+	echo '</div>';
+}
+
+
+function afficher_tableau_test($tableau){
+	//echo '<table>';
+	nl2br(var_dump($tableau));
+	//echo '</table>';
+}
+
 function editer_tableau($tableau){
 	echo '<table>';
 	foreach($tableau as $cle=>$valeur)
@@ -27,6 +43,8 @@ function editer_tableau_div($tableau){
 		echo '<div><b>'.$cle.'</b><br /><input type="text" class="formo" value="'.$valeur.'" name="'.$cle.'" /></div><br />';
 	} 
 	echo '</table>';
+	
+
 
 }
 ?>
