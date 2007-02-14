d81bbc8de412caf13aee1bb2e204d69a12aa4bfe
crovvfoot@gmail.com <crovvfoot@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-02-14 00:47:50 +0000

    On peut maintenant voir les table mais aussi la liste des enrtegistrement, chacun d'eux et les éditer sans les sauver. Y a encore des problemes dans la sélection de la cle primaire ... Correction d'autres bug.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@9651 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/edit_table/exec/edittable.php
+++ _dev_/edit_table/exec/edittable.php
@@ -9,28 +9,52 @@ define('_DIR_PLUGIN_EDITTABLE',(_DIR_PLUGINS.end($p)));
 
 include_spip("inc/presentation");
 include_spip("inc/barre");
+include_spip("inc/tableau");
 
 function exec_edittable(){
 	echo debut_page(_T('edittable:spip_edittable'));
 		echo debut_gauche();
 			echo debut_boite_info();
-				echo "Hello World !";
+				echo _request('table');
 			echo fin_boite_info();
 			echo debut_raccourcis();
-				//echo '<a href="?exec=edittable_edit&id=new">'._T('edittable:ajouter_un_edittable').'</a><br />';
-				echo '<a href="?exec=edittable">'._T('edittable:les_table').'</a>';
+				echo '<a href="?exec=listetable">'._T('edittable:les_table').'</a>';
 			echo fin_raccourcis();
 		echo debut_droite();
-			$sql_list_edittable = "SELECT * FROM spip_articles;";
-			$res_list_edittable = spip_query($sql_list_edittable);
+			//var_dump($row_structure);
+			
+			$olonne_cle = "000";
+			
+			$res_structure_table = spip_query(" DESC "._request('table').";");
+			//debut_cadre_formulaire();
+			while ($row_structure = spip_fetch_array($res_structure_table)){
+				//afficher_tableau($row_structure);
+				if ($row_structure['Key'] == "PRI" OR $row_structure['Extra'] == "auto_increment"){
+					$olonne_cle = $row_structure['Field'];
+				}
+				$colone_temp == $row_structure['Field'];
+				$structure = $row_structure;
+			}
+			if ( $olonne_cle == '000' ) { $olonne_cle = $colone_temp; }
+			
+			$res_list_edittable = spip_query("SELECT * FROM "._request('table').";");
 			debut_cadre_trait_couleur("../"._DIR_PLUGIN_edittable."/img_pack/digg.png", false, '', _T('edittable:mes_edittable_en'));
 			echo '<table>';
 			while ($row_edittable = spip_fetch_array($res_list_edittable)){
-				echo '<tr><td>'.$row_edittable['id_article'].'</td><td><a href="?exec=edittable_voir&amp;id_article='.$row_edittable['id_article'].'">'.$row_edittable['titre'].'</a></td></tr>';
+				echo '<tr><td>'.$row_edittable[$structure[1]].'</td><td>'.$row_edittable[$structure[2]].'</td><td>'.$row_edittable[$structure[3]].'</td><td><a href="?exec=edittable_voir&amp;valeur_cle='.$row_edittable[$olonne_cle].'&amp;table='._request('table').'&amp;colonne_cle='.$olonne_cle.'">'._T('edittable:editer_enregistrement').'</a></td></tr>';
 			}
 			echo '</table>';
 			fin_cadre_trait_couleur(false);
 			
+			echo '<br />';
+			
+			$res_structure_table = spip_query(" SHOW INDEX FROM "._request('table').";");
+			debut_cadre_formulaire();
+			while ($row_structure = spip_fetch_array($res_structure_table)){
+				afficher_tableau($row_structure);
+				//if $row_structure
+			}
+			fin_cadre_formulaire();
 			
 	if ($GLOBALS['spip_version_code']>=1.92) { echo fin_gauche(); }
 	echo fin_page();
--- _dev_/edit_table/exec/edittable_edit.php
+++ _dev_/edit_table/exec/edittable_edit.php
@@ -10,23 +10,19 @@ function exec_edittable_edit(){
 	echo debut_page(_T('edittable:spip_edittable'));
 		echo debut_gauche();
 			echo debut_boite_info();
-				echo '<div style="font-weight: bold; text-align: center;" class="verdana1 spip_xx-small">'._T('edittable:edittable_numero');
-				echo '<br /><span class="spip_xx-large">'._request('uid').'</span></div>';
+				echo '<center><font face="Verdana,Arial,Sans,sans-serif" size="1"><b>'._request('table').'</b></font><br><font face="Verdana,Arial,Sans,sans-serif" size="2"><b>'._request('colonne_cle').'&nbsp;=&nbsp;'._request('valeur_cle').'</b></font></center>';
+
 			echo fin_boite_info();
 			echo debut_raccourcis();
-				echo '<a href="?exec=edittable_edit&uid=new">'._T('edittable:ajouter_un_edittable').'</a><br />';
-				echo '<a href="?exec=edittable">'._T('edittable:mes_edittable').'</a>';
+				echo '<a href="?exec=listetable">'._T('edittable:les_table').'</a>';
 			echo fin_raccourcis();
 		echo debut_droite();
-			$sql_edittable = "SELECT * FROM spip_articles WHERE id_article = '"._request('id_article')."';";
+			
+			$sql_edittable = "SELECT * FROM "._request('table')." WHERE ".utf8_decode(_request('colonne_cle'))." = '"._request('valeur_cle')."';";
 			$res_edittable = spip_query($sql_edittable);
 			$contenu_edittable = spip_fetch_array($res_edittable);
 			
 			echo debut_cadre_formulaire();
-			echo '<form action="?exec=editer_edittable" method="post">';
-			echo "<input name='action' type='hidden' value='edittable_edit' />
-				<input name='redirect' type='hidden' value='".lire_meta('adresse_site')."'/ecrire/?exec=editer_edittable' />
-				<input type='hidden' name='editer_edittable' value='oui' />";
 			if (_request('new') == "oui") {
 				echo '<input type="hidden" name="id_article" value="new" />';
 			}else{
