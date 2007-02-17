c3d2d8d569b2afbf4e2bff0083fa1368b6686b79
booz@rezo.net <booz@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-02-17 17:31:36 +0000

    generaliser
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@9728 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/podcast_client/podcast_client.php
+++ _test_/podcast_client/podcast_client.php
@@ -21,7 +21,60 @@
 		PodcastClient_verifier_table_documents_syndic();
 	
 		list($le_lien, $id_syndic, $data) = func_get_arg(0);
-		PodcastClient_traiter_les_enclosures_rss($data['enclosures'],$id_syndic,$le_lien);
+		//print_r($data);
+		
+		
+		//trouver un flash dailymotion
+		# <media:content url="http://www.dailymotion.com/swf/3ndb67rMbTuLh8E2i" type="application/x-shockwave-flash" duration="520" width="320" height="240"/>
+		//echo $item ;
+		if (preg_match(',(<media:content[^>]*type="application/x-shockwave-flash".*>),i',
+		$data['item'], $match)) {
+			$go=str_replace(':','',$match[1]);
+			
+			$data['enc_flash']['url'] = extraire_attribut($go, 'url');
+			$data['enc_flash']['duration'] = extraire_attribut($go, 'duration');
+			$data['enc_flash']['width'] = extraire_attribut($go, 'width');
+			$data['enc_flash']['height'] = extraire_attribut($go, 'height');
+			$data['enc_flash']['type'] = trim(extraire_attribut($go,'type'));
+			if(!$data['enc_flash']['type']) $data['enc_flash']['type'] = substr($data['enc_flash']['url'], -3);
+			//print_r($data['flash']);
+			
+			//$data['url_flash'] = str_replace('&amp;', '&',
+			//	trim(extraire_attribut($match[1], 'url')));
+		}
+		
+		//mettre les documents dans un tableau
+		$enclosure = $data['enclosures'] ;
+		// href et type sont obligatoires
+			if (($enc_regs_url = extraire_attribut($enclosure,'href')
+			AND $enc_regs_type = extraire_attribut($enclosure,'type'))) {
+				$url  = substr(urldecode($enc_regs_url), 0,255);
+				$data['enc_enclosure']['url'] = addslashes(abs_url($url, $le_lien));
+				$data['enc_enclosure']['type'] = trim($enc_regs_type) ;
+				$data['enc_enclosure']['length'] = extraire_attribut($enclosure,'length') ;
+				if(!$data['enc_enclosure']['type']) $data['enc_enclosure']['type'] = substr($url, -3);
+			}
+		
+
+		$enclosure = $data['descriptif'] ;
+		// trouver des images dans le descriptif
+			if (($enc_regs_url = extraire_attribut($enclosure,'src'))) {
+				$url  = substr(urldecode($enc_regs_url), 0,255);
+				$data['enc_image']['url'] = addslashes(abs_url($url, $le_lien));
+				$data['enc_image']['type'] = substr($url, -3);
+			}
+		
+		$data['enclosures_all'][] = $data['enc_enclosure'] ;
+		$data['enclosures_all'][] = $data['enc_flash'] ;
+		$data['enclosures_all'][] = $data['enc_image'] ;
+		
+		/**/
+		
+		//print_r($data['enclosures_all']); die("coucou");
+		PodcastClient_traiter_les_enclosures_rss($data['enclosures_all'],$id_syndic,$le_lien);
+		// mieux vaut preparer les enclosures qu'on veut et passer en une fois, la c n'imp
+		//PodcastClient_traiter_les_enclosures_rss($data['descriptif'],$id_syndic,$le_lien);
+		
 		return func_get_arg(0); # remettre les infos dans le pipeline
 	}
 	
@@ -49,65 +102,79 @@
 	// presentes sous la forme microformat <a rel="enclosure">
 	//
 	function PodcastClient_traiter_les_enclosures_rss($enclosures,$id_syndic,$le_lien) {
-		if (!preg_match_all(
-		',<a([[:space:]][^>]*)?[[:space:]]rel=[\'"]enclosure[^>]*>,',
-		$enclosures, $regs, PREG_PATTERN_ORDER))
-			return false;
-		$enclosures = $regs[0];
+	spip_log('podcast_client'.$le_lien.'\n\n');
+		//if (sizeof($enclosures) == 0) return false ;
 		include_spip('inc/filtres'); # pour extraire_attribut
-	
+	//print_r($enclosures);die("hop");
 		list($id_syndic_article) = spip_fetch_array(spip_query(
 		"SELECT id_syndic_article FROM spip_syndic_articles
-		WHERE id_syndic=$id_syndic AND url='".addslashes($le_lien)."'"),SPIP_NUM);
+		WHERE id_syndic=$id_syndic AND url='".addslashes($le_lien)."'"), SPIP_NUM);
 	
-		// Attention si cet article est deja vu, ne pas doubler les references
+		// Attention si cet article est deja vu, ne pas doubler les references // du coup ca efface les mp3 si y'a une image je pense
 		spip_query("DELETE FROM spip_documents_syndic
-		WHERE id_syndic_article='$id_syndic_article'");
-	
+		WHERE id_syndic_article=$id_syndic_article");
+		spip_log("efface");
 		// Integrer les enclosures
 		foreach ($enclosures as $enclosure) {
-	
+//echo $enclosure;
+			$url = $enclosure['url'] ;
+			$type = $enclosure['type'] ;
 			// href et type sont obligatoires
-			if ($enc_regs_url = extraire_attribut($enclosure,'href')
-			AND $enc_regs_type = extraire_attribut($enclosure,'type')) {
-	
-				$url = substr(urldecode($enc_regs_url), 0,255);
-				$url = addslashes(abs_url($url, $le_lien));
-				$type = $enc_regs_type;
-	
-				// Verifier que le content-type nous convient
+			if ($enclosure['url'] AND $enclosure['type']) {
+			spip_log("type : ".$enclosure['type']) ;
+			// Verifier que le content-type nous convient
+				$row = spip_fetch_array(spip_query("SELECT id_type
+				FROM spip_types_documents WHERE mime_type='$type'"));
+				$id_type = $row['id_type'] ;
+				spip_log("id_type1 : ".$id_type) ;
+				if (!$id_type) {
 				list($id_type) = spip_fetch_array(spip_query("SELECT id_type
-				FROM spip_types_documents WHERE mime_type='$type'"),SPIP_NUM);
-
+					FROM spip_types_documents WHERE extension='$type'"), SPIP_NUM);
+				spip_log("id_type2 : ".$id_type) ;
+				}
+				
+				//echo $id_type; die("coucouman");
+				
 				if (!$id_type) {
 					spip_log("podcast_client: enclosure inconnue ($type) $url");
 					list($id_type) = spip_fetch_array(spip_query("SELECT id_type
-					FROM spip_types_documents WHERE extension='bin'"),SPIP_NUM);
+					FROM spip_types_documents WHERE extension='bin'"), SPIP_NUM);
+					spip_log("id_type3 : ".$id_type) ;
 					// si les .bin ne sont pas autorises, on ignore ce document
 					if (!$id_type) continue;
 				}
-	
+				spip_log("id_type : ".$id_type) ;
 				// length : optionnel (non bloquant)
-				$taille = intval(extraire_attribut($enclosure, 'length'));
-	
+				
+				$taille = intval($enclosure['length']);
+				$largeur = intval($enclosure['width']);
+				$hauteur = intval($enclosure['height']);
+				$duree = intval($enclosure['duration']);
+				spip_log("taille : ".$taille." largeur : ".$largeur." hauteur : ".$hauteur." duree ".$duree);
 				// Inserer l'enclosure dans la table spip_documents
 				if ($t = spip_fetch_array(spip_query("SELECT id_document FROM
-				spip_documents WHERE fichier='$url' AND distant='oui'")))
+				spip_documents WHERE fichier='$url' AND distant='oui'"))){
 					$id_document = $t['id_document'];
-				else {
+				spip_log("deja vu doc->".$id_document) ;
+				} else {
+
 					spip_query("INSERT INTO spip_documents
-					(id_type, titre, fichier, date, distant, taille, mode)
-					VALUES ($id_type,'','$url',NOW(),'oui',$taille, 'document')");
+					(id_type, titre, fichier, date, distant, taille, mode, largeur, hauteur)
+					VALUES ($id_type,'','$url',NOW(),'oui',$taille, 'document', $largeur, $hauteur)");
 					$id_document = spip_insert_id();
 					spip_log("podcast_client: '$url' => id_document=$id_document");
+					//echo "podcast_client: '$url' => id_document=$id_document";
 				}
 	
 				// lier avec l'article syndique
 				spip_query("INSERT INTO spip_documents_syndic
 				(id_document, id_syndic, id_syndic_article)
 				VALUES ($id_document, $id_syndic, $id_syndic_article)");
+				spip_log("lier doc->".$id_document."Sarticle".$id_syndic_article) ;
 				$n++;
 			}
+		
+		
 		}
 	
 		return $n; #nombre d'enclosures integrees
