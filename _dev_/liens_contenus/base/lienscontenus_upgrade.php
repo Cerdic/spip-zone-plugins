1672709f9c8e2ab1957717be2f7d7b3df347104b
nicolas@hoizey.com <nicolas@hoizey.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-02-19 18:55:24 +0000

    -* Meilleur support de l'installation / mise a jour / desinstallation
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@9771 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/liens_contenus/base/lienscontenus_upgrade.php
+++ _dev_/liens_contenus/base/lienscontenus_upgrade.php
@@ -11,37 +11,59 @@
 
 include_spip('inc/meta');
 
-function lienscontenus_upgrade($nom_meta_base_version, $version_cible)
-{
-    $current_version = 0.0;
-    if ((!isset($GLOBALS['meta'][$nom_meta_base_version]))
-            || (($current_version = $GLOBALS['meta'][$nom_meta_base_version]) != $version_cible)) {
-        include_spip('base/lienscontenus');
-        if ($current_version == 0.0) {
-            include_spip('base/create');
-            include_spip('base/abstract_sql');
-            spip_log('Creation de la base', 'liens_contenus');
-            creer_base();
-            include_spip('inc/lienscontenus');
-            lienscontenus_initialiser();
-            $current_version = $version_cible;
-        }
-        if ($current_version < 0.2) {
-            // Mise a jour de la base
-            spip_log('Mise a jour de la base', 'liens_contenus');
-            include_spip('base/abstract_sql');
-            spip_query("UPDATE spip_liens_contenus SET type_objet_contenant='syndic' WHERE type_objet_contenant='site'");
-            spip_query("UPDATE spip_liens_contenus SET type_objet_contenu='syndic' WHERE type_objet_contenu='site'");
-            $current_version = 0.2;
-        }
-        ecrire_meta($nom_meta_base_version, $current_version);
-        ecrire_metas();
-    }
-}
+$GLOBALS['lienscontenus_base_version'] = 0.2;
 
-function lienscontenus_vider_tables($nom_meta_base_version) {
-    spip_query('DROP TABLE spip_liens_contenus');
-    effacer_meta($nom_meta_base_version);
+function lienscontenus_upgrade()
+{
+    $version_base = $GLOBALS['lienscontenus_base_version'];
+    $current_version = 0.0;
+    if ((isset($GLOBALS['meta']['lienscontenus_base_version']))
+            && (($current_version = $GLOBALS['meta']['lienscontenus_base_version']) == $version_base)) {
+        return;
+    }
+
+    include_spip('base/lienscontenus');
+    if ($current_version == 0.0) {
+        include_spip('base/create');
+        include_spip('base/abstract_sql');
+        spip_log('Creation de la base', 'liens_contenus');
+        creer_base();
+        include_spip('inc/lienscontenus');
+        lienscontenus_initialiser();
+        $current_version = $version_base;
+    }
+    if ($current_version < 0.2) {
+        // Mise a jour de la base
+        spip_log('Mise a jour de la base', 'liens_contenus');
+        include_spip('base/abstract_sql');
+        spip_query("UPDATE spip_liens_contenus SET type_objet_contenant='syndic' WHERE type_objet_contenant='site'");
+        spip_query("UPDATE spip_liens_contenus SET type_objet_contenu='syndic' WHERE type_objet_contenu='site'");
+        $current_version = 0.2;
+    }
+    ecrire_meta('lienscontenus_base_version', $current_version, 'non');
     ecrire_metas();
 }
+
+function lienscontenus_vider_tables()
+{
+    spip_query('DROP TABLE spip_liens_contenus');
+    effacer_meta('lienscontenus_base_version');
+    ecrire_metas();
+}
+
+function lienscontenus_install($action)
+{
+    switch ($action) {
+        case 'test':
+            return (isset($GLOBALS['meta']['lienscontenus_base_version'])
+                && ($GLOBALS['meta']['lienscontenus_base_version'] >= $GLOBALS['lienscontenus_base_version']));
+            break;
+        case 'install':
+            lienscontenus_upgrade();
+            break;
+        case 'uninstall':
+            lienscontenus_vider_tables();
+            break;
+    }
+}   
 ?>
