6c00ece7d7607c80a22169be89f4c13a3a6fa8eb
patfr@ifrance.com <patfr@ifrance.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-02-19 02:55:19 +0000

    on normalise les langues...
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@9753 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _test_/agenda_alternatives/lang/minicalalter_fr.php
@@ -0,0 +1,20 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	'info_evenements' => 'Voici ci-dessous les &eacute;v&egrave;nements enregistr&eacute;s sur ce site. Chaque &eacute;v&egrave;nement est li&eacute; &agrave; un article accessible en cliquant sur son titre.',
+	'tout_voir_1' => 'Tout voir :',
+	'tout_voir_2' => 'Tout voir : @month_year@',
+	'evenements' => '@evts@ &eacute;v&eacute;nements',
+	'evenements_prochains' => 'Les &eacute;v&egrave;nements prochains (ou en cours)',
+	'evenements_passes' => 'Les &eacute;v&egrave;nements pass&eacute;s',
+	'evenements_prochains_article' => 'Les &eacute;v&egrave;nements prochains (ou en cours) associ&eacute;s &agrave; cet article',
+	'evenements_passes_article' => 'Les &eacute;v&egrave;nements pass&eacute;s associ&eacute;s &agrave; cet article',
+	'evenements_mois' => 'Les &eacute;v&egrave;nements du mois de @month_year@',
+	'evenements_jour' => 'Les &eacute;v&egrave;nements du @day_month_year@',
+	'activer_agenda' => "ERREUR !<br /><br />Merci d'activer le Plugin Agenda.",
+		
+);
+
+?>
