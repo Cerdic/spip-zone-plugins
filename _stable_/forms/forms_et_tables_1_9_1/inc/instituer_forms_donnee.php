d8908362c2d6c1942d485eb879690365c5427623
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-02-25 11:55:08 +0000

    rendre les status parametrables par le fichier de langue, et adapter le menu survol aux statuts actifs
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@9881 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/forms/forms_et_tables_1_9_1/inc/instituer_forms_donnee.php
+++ _stable_/forms/forms_et_tables_1_9_1/inc/instituer_forms_donnee.php
@@ -69,83 +69,73 @@ function inc_instituer_forms_donnee_dist($id_form, $id_donnee, $statut, $rang=NU
 function puce_statut_donnee($id, $statut, $id_form, $ajax = false) {
 	global $spip_lang_left, $dir_lang, $connect_statut, $options;
 	static $script=NULL;
+	static $pi18n = array();
 	
-	switch ($statut) {
-	case 'publie':
-		$clip = 1;
-		$puce = 'verte';
-		$title = _T('info_article_publie');
-		break;
-	case 'prop':
-		$clip = 0;
-		$puce = 'orange';
-		$title = _T('info_article_propose');
-		break;
-	case 'refuse':
-		$clip = 2;
-		$puce = 'rouge';
-		$title = _T('info_article_refuse');
-		break;
-	case 'poubelle':
-		$clip = 3;
-		$puce = 'poubelle';
-		$title = _T('info_article_supprime');
-		break;
-	}
-	$puce = "puce-$puce.gif";
+	if (!$id) {
+	  $id = $id_form;
+	  $ajax_node ='';
+	} else	$ajax_node = " id='imgstatutforms_donnee$id'";
 
-	if (!include_spip('inc/autoriser'))
-		include_spip('inc/autoriser_compat');
-	if (autoriser('publierdans', 'form', $id_form)) {
-	  // les versions de MSIE ne font pas toutes pareil sur alt/title
-	  // la combinaison suivante semble ok pour tout le monde.
-	  $titles = array(
-			  "orange" => _T('texte_statut_propose_evaluation'),
-			  "verte" => _T('texte_statut_publie'),
-			  "rouge" => _T('texte_statut_refuse'),
-			  "poubelle" => _T('texte_statut_poubelle'));
-		if ($ajax){
-		  $action = "onmouseover=\"montrer('statutdecalforms_donnee$id');\"";
-		  $inser_puce = 
-		  	// "\n<div class='puce_forms_donnee' id='statut$id'$dir_lang>" .
-				"\n<div class='puce_forms_donnee_fixe' $action>" .
-			  http_img_pack($puce, $title, "id='imgstatutforms_donnee$id' style='margin: 1px;'") ."</div>"
-				. "\n<div class='puce_forms_donnee_popup' id='statutdecalforms_donnee$id' onmouseout=\"cacher('statutdecalforms_donnee$id');\" style=' margin-left: -".((11*$clip)+1)."px;'>\n"
-				. afficher_script_statut($id, 'forms_donnee', -1, 'puce-orange.gif', 'prop', $titles['orange'], $action)
-				. afficher_script_statut($id, 'forms_donnee', -12, 'puce-verte.gif', 'publie', $titles['verte'], $action)
-				. afficher_script_statut($id, 'forms_donnee', -23, 'puce-rouge.gif', 'refuse', $titles['rouge'], $action)
-				. afficher_script_statut($id, 'forms_donnee', -34, 'puce-poubelle.gif', 'poubelle', $titles['poubelle'], $action)
-			. "</div>"
-			//. "</div>"
-			;
-		}
-		else{
-		  $inser_puce = "\n<div class='puce_forms_donnee' id='statut$id'$dir_lang>".
-			  http_img_pack($puce, $title, "id='imgstatutforms_donnee$id' style='margin: 1px;'") ."</div>";
-			if ($script==NULL){
-				$action = "'".generer_url_ecrire('puce_statut_forms_donnee',"id='+id",true);
-				$script = "<script type='text/javascript'><!--\n";
-				$script .= "$(document).ready(function(){
-					$('div.puce_forms_donnee').mouseover( function() {
-						if (this.puce_loaded) return;
-						this.puce_loaded = true;
-						id = $(this).id();
-						id = id.substr(6,id.length-1);
-						$('#statut'+id).load($action,function(){ 
-								$('#statutdecalforms_donnee'+id).show(); 
-								/*$('#statut'+id).mouseover(function(){ $(this).children('.puce_forms_donnee_popup').show(); });*/
-							});
-						});
-					
-				})";
-				$script .= "//--></script>";
-				$inser_puce = $script . $inser_puce;
-			}
-		}
-	} else {
-		$inser_puce = http_img_pack($puce, $title, "id='imgstatutforms_donnee$id' style='margin: 1px;'");
+	if (!isset($pi18n[$id_form])){
+		$pi18n[$id_form] = "form";
+		$res = spip_query("SELECT type_form FROM spip_forms WHERE id_form="._q($id_form));
+		if ($row = spip_fetch_array($res)
+			AND $row['type_form']!=''
+			AND $rox['type_form']!='sondage')
+				$pi18n[$id_form] = forms_prefixi18n($row['type_form']);
 	}
-	return $inser_puce;
+
+	$p = $pi18n[$id_form];
+	$puce = array(
+	  'prepa'=>'puce-blanche.gif',
+	  'prop'=>'puce-orange.gif',
+	  'publie'=>'puce-verte.gif',
+	  'refuse'=>'puce-rouge.gif',
+	  'poubelle'=>'puce-poubelle.gif');
+	$lib = array();
+	$clip = array();
+	$c = 0;
+	$statuts = array("prepa","prop","publie","poubelle","refuse");
+	foreach($statuts as $s){
+		$lib[$s] = _T("$p:texte_statut_$s");
+		if (strlen(trim($lib[$s]))){
+			$clip[$s] = $c++;
+		}
+		else $clip[$s] = 0;
+	}
+	$width = 11*$c+1;
+	$inser_puce = http_img_pack($puce[$statut], $lib[$statut], " style='margin: 1px;'$ajax_node");
+
+	if (!autoriser('publierdans', 'form', $id_form))
+		return $inser_puce;
+
+	if ($ajax){
+		$action = "\nonmouseover=\"montrer('statutdecalforms_donnee$id');\"";
+		$res = "<span class='puce_forms_donnee_fixe'\n$action>"
+		. $inser_puce
+		. "</span>"
+		. "<span class='puce_article_popup' id='statutdecalforms_donnee$id'\nonmouseout=\"cacher('statutdecalforms_donnee$id');\" style='margin-left: -".((11*$clip[$statut])+1)."px;width:{$width}px'>";
+		foreach($statuts as $s)
+			if (strlen(trim($lib[$s])))
+		  	$res .= afficher_script_statut($id, 'forms_donnee', -((11*$clip[$s])+1), $puce[$s], $s, $lib[$s], $action);
+		$res .= "</span>";
+		return $res;
+	}
+
+	$nom = "puce_statut_";
+
+	if ((! _SPIP_AJAX))
+	  $over ='';
+	else {
+		$action = generer_url_ecrire('puce_statut_forms_donnee',"",true);
+		$action = "if (!this.puce_loaded) { this.puce_loaded = true; prepare_selec_statut('$nom', 'forms_donnee', $id, '$action'); }";
+		$over = "\nonmouseover=\"$action\"";
+	}
+
+	return 	"<span class='puce_article' id='{$nom}forms_donnee$id'$dir_lang$over>"
+	. $inser_puce
+	. '</span>';
+
 }
 
 ?>
