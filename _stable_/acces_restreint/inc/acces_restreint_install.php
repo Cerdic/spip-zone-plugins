f163c14e5736c3965fe3cd156f45cd4bf275d204
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-02-26 22:04:58 +0000

    "reparer l'upgrade de la base (publique/prive)"
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@9924 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/acces_restreint/inc/acces_restreint_install.php
+++ _stable_/acces_restreint/inc/acces_restreint_install.php
@@ -1,50 +1,46 @@
 <?php
 
-$GLOBALS['accesrestreint_version_base'] = 0.11;
-function AccesRestreint_upgrade(){
-	global $accesrestreint_version_base;
-	$meta_base = 'accesrestreint_base_version';
-	$version_base = $accesrestreint_version_base;
-	$current_version = 0.0;
-	if (   (isset($GLOBALS['meta'][$meta_base]) )
-			&& (($current_version = $GLOBALS['meta'][$meta_base])>=$version_base))
-		return;
-
-	include_spip('base/acces_restreint');
-	if ($current_version==0.0){
-		include_spip('base/create');
-		include_spip('base/abstract_sql');
-		creer_base();
-		ecrire_meta($meta_base,$current_version=$version_base,'non');
+	include_spip('inc/meta');
+	function AccesRestreint_upgrade($nom_meta_base_version,$version_cible){
+		$current_version = 0.0;
+		if (   (!isset($GLOBALS['meta'][$nom_meta_base_version]) )
+				|| (($current_version = $GLOBALS['meta'][$nom_meta_base_version])!=$version_cible)){
+			include_spip('base/acces_restreint');
+			if ($current_version==0.0){
+				include_spip('base/create');
+				include_spip('base/abstract_sql');
+				creer_base();
+				// ajout des champs publique/privee si pas existants
+				$desc = spip_abstract_showtable("spip_zones", '', true);
+				if (!isset($desc['field']['publique']))
+					spip_query("ALTER TABLE spip_zones ADD publique ENUM('non', 'oui') DEFAULT 'oui' NOT NULL AFTER descriptif");
+				if (!isset($desc['field']['privee']))
+					spip_query("ALTER TABLE spip_zones ADD privee ENUM('non', 'oui') DEFAULT 'non' NOT NULL AFTER publique");
+				echo "AccesRestreint Install<br/>";
+				ecrire_meta($nom_meta_base_version,$current_version=$version_cible,'non');
+			}
+			if ($current_version<0.2){
+				include_spip('base/create');
+				include_spip('base/abstract_sql');
+				// ajout des champs publique/privee si pas existants
+				$desc = spip_abstract_showtable("spip_zones", '', true);
+				if (!isset($desc['field']['publique']))
+					spip_query("ALTER TABLE spip_zones ADD publique ENUM('non', 'oui') DEFAULT 'oui' NOT NULL AFTER descriptif");
+				if (!isset($desc['field']['privee']))
+					spip_query("ALTER TABLE spip_zones ADD privee ENUM('non', 'oui') DEFAULT 'non' NOT NULL AFTER publique");
+				echo "AccesRestreint@0.2<br />";
+				ecrire_meta($nom_meta_base_version,$current_version=0.2,'non');
+			}
+			ecrire_metas();
+		}
 	}
-	if ($current_version<0.11){
-		spip_query("ALTER TABLE spip_zones ADD publique ENUM('non', 'oui') DEFAULT 'oui' NOT NULL AFTER descriptif");
-		spip_query("ALTER TABLE spip_zones ADD privee ENUM('non', 'oui') DEFAULT 'non' NOT NULL AFTER publique");
-		ecrire_meta($meta_base,$current_version=0.11,'non');
+	
+	function AccesRestreint_vider_tables($nom_meta_base_version) {
+		spip_query("DROP TABLE spip_zones");
+		spip_query("DROP TABLE spip_zones_auteurs");
+		spip_query("DROP TABLE spip_zones_rubriques");
+		effacer_meta($nom_meta_base_version);
+		ecrire_metas();
 	}
-	ecrire_metas();
-}
 
-function AccesRestreint_vider_tables() {
-	spip_query("DROP TABLE spip_zones");
-	spip_query("DROP TABLE spip_zones_auteurs");
-	spip_query("DROP TABLE spip_zones_rubriques");
-	effacer_meta('accesrestreint_base_version');
-	ecrire_metas();
-}
-
-function AccesRestreint_install($action){
-	global $accesrestreint_version_base;
-	switch ($action){
-		case 'test':
-			return (isset($GLOBALS['meta']['accesrestreint_base_version']) AND ($GLOBALS['meta']['accesrestreint_base_version']>=$accesrestreint_version_base));
-			break;
-		case 'install':
-			AccesRestreint_upgrade();
-			break;
-		case 'uninstall':
-			AccesRestreint_vider_tables();
-			break;
-	}
-}
 ?>
