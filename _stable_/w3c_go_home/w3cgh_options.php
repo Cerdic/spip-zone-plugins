fbbc5fb29219d83e068a2268413e42272abd96a5
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-02-26 09:24:26 +0000

    "compatibilite sans le plugin acces restreint"
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@9910 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/w3c_go_home/w3cgh_options.php
+++ _stable_/w3c_go_home/w3cgh_options.php
@@ -5,4 +5,11 @@ $GLOBALS['xhtml'] = 'sax';
 //$GLOBALS['xml_indent'] = 'sax'; // pour seulement le validateur XML
 //$GLOBALS['xml_validation'] = true; // pour le validateur selon la DTD
 
+if (!defined('_DIR_PLUGIN_ACCESRESTREINT') AND !function_exists('critere_tout_voir_dist')){
+	// {tout_voir}
+	function critere_tout_voir_dist($idb, &$boucles, $crit) {
+		$boucle = &$boucles[$idb];
+		$boucle->modificateur['tout_voir'] = true;
+	}
+}
 ?>
