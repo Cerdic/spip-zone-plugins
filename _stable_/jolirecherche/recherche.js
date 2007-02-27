2303d974fc0412aa7f9691eee8e2e9082e831267
gael@merzok.com <gael@merzok.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-02-27 21:48:14 +0000

    jolirecherche remplace le formulaire recherche par de jolie boite de saisie.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@9953 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _stable_/jolirecherche/recherche.js
@@ -0,0 +1,26 @@
+﻿/*
+ * www.merzok.com
+ * jolie saisie de recherche
+ * Copyleft - all rights reversed.
+ *
+ */
+	if (document.images) {
+		var rerienim =new Image();  rerienim.src ="plugins/jolirecherche/formulaires/rerien.gif"; 
+		var reok =new Image();  reok.src ="plugins/jolirecherche/formulaires/reok.gif"; 
+	}
+	
+	function tac_fo() {
+		document.images['rerien'].src = rerienim.src;	
+		document.re.recherche.value = "";
+		document.re.recherche.focus();
+	}
+	
+	function tac_ok() {	
+		if (document.re.recherche.value > "") {
+			if (document.re.recherche.value == "recherche") { return; }
+			document.images['rerien'].src = reok.src;
+		}
+		else {
+			document.images['rerien'].src = rerienim.src;
+		}
+	}
---
+++ _stable_/jolirecherche/recherche_public.php
@@ -0,0 +1,5 @@
+<?php
+	function recherche_insert_head($flux) {
+		return $flux."<link rel=\"stylesheet\" type=\"text/css\" href=\"".direction_css(find_in_path("recherche.css"))."\" />\n<script src=\"".find_in_path("recherche.js")."\" type=\"text/javascript\" />";
+	}
+?>
