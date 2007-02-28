f31301f0d39002439566b04b60a636c0aa5d03e0
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-02-28 14:41:42 +0000

    Un peu plus de localisation
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@9968 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/imprimir_documento/lang/imprimirdocumento_es.php
+++ _test_/imprimir_documento/lang/imprimirdocumento_es.php
@@ -5,6 +5,8 @@
 		'documentos_descargar' => 'Si visitas nuestra web, en este art&iacute;culo encontrar&aacute;s los siguientes documentos para descargar:',
 		'version_doc_imprimir' => 'Versi&oacute;n para imprimir de este documento',
 		'version_imprimir' => 'Versi&oacute;n imprimir',
+		'otras_imagenes' => 'OTRAS IM&Aacute;GENES',
+		'commentaires' => 'Comentarios:',
 		'Z' => 'ZZzZZzzz'
 	);
 
--- _test_/imprimir_documento/lang/imprimirdocumento_fr.php
+++ _test_/imprimir_documento/lang/imprimirdocumento_fr.php
@@ -5,6 +5,8 @@
 		'documentos_descargar' => 'En visitant notre site Internet, vous pourrez télécharger ces documents :',
 		'version_doc_imprimir' => 'Version imprimable de cet article',
 		'version_imprimir' => 'Version imprimable',
+		'otras_imagenes' => 'AUTRES IMAGES',
+		'commentaires' => 'Commentaires :',
 		'Z' => 'ZZzZZzzz'
 	);
 
