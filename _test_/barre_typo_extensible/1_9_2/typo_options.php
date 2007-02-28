c0559de4c521405c5703df6369a6e2b4a0718ca6
spipcarto@gmail.com <spipcarto@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-02-28 22:03:40 +0000

    declarer les pipelines sinon ca bug pour ceux inutilises, ce qui peut rediriger sur admin_plugin (merci Cedric)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@9997 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _test_/barre_typo_extensible/1_9_2/typo_options.php
@@ -0,0 +1,7 @@
+<?php
+$GLOBALS['spip_pipeline']['BarreTypoEnrichie_boutonsavances']='';
+$GLOBALS['spip_pipeline']['BarreTypoEnrichie_toolbox']='';
+$GLOBALS['spip_pipeline']['BarreTypoEnrichie_tous']='';
+$GLOBALS['spip_pipeline']['BarreTypoEnrichie_avancees']='';
+$GLOBALS['spip_pipeline']['BarreTypoEnrichie_ecrire']='';
+?>
