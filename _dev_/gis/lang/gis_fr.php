58415b3860d92ad213f377bd235fda37102dcfda
berio@alg-a.org <berio@alg-a.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-03-12 20:23:38 +0000

    gis: french traduction auteur
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@10318 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/gis/lang/gis_fr.php
+++ _dev_/gis/lang/gis_fr.php
@@ -2,6 +2,7 @@
 
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP nomme admin_lang genere le NOW()
 // langue / language = fr
+// Traduction -- Pierre FICHES <pierre.fiches@free.fr>
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 //elements de formulaire pour editer les coordonnees
