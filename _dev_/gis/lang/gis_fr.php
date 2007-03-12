a1de0778cb49617b45788142d598ab7940518db1
berio@alg-a.org <berio@alg-a.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-03-12 19:41:53 +0000

    gis: french version changes and files
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@10316 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _dev_/gis/lang/gis_fr.php
@@ -0,0 +1,17 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP nomme admin_lang genere le NOW()
+// langue / language = fr
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+//elements de formulaire pour editer les coordonnees
+'clave_engadida' => 'Clef ajout&eacute;e: ',
+'clic_mapa' => 'Cliquer sur la carte pour changer les coordonn&eacute;es',
+'clic_desplegar' => 'd&eacute;plier',
+'configurar_gis' => 'Configurer Plugin Gis',
+'conseguir' => '(obtenir)',
+'cambiar' => 'changer coordonn&eacute;es',
+'boton_actualizar' => 'actualiser'
+);
+
+?>
