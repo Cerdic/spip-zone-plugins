051a67cc629e2a0425ff8b5292802b125e5f5c6d
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-03-12 22:49:43 +0000

    "desactiver les icones quand elles ne sont pas utilisables"
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@10323 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/spip_outliner/javascript/outline_tous.js
+++ _dev_/spip_outliner/javascript/outline_tous.js
@@ -4,12 +4,19 @@ $(document).ready(function(){
 		$(this).addClass('sel');
 		evt.cancelBubble = true;
 		if (evt.stopPropagation) evt.stopPropagation();
+		update_toolbar_icones();
 	});
 	$('dl').hover(function(){	$(this).addClass('hover');},function(){	$(this).removeClass('hover');});
 	$('dl').dblclick( function() {window.location.replace($(this).attr('name')); } );
-	$('body.outline_tous').click(function(){$('dl.sel').removeClass('sel');});
+	$('body.outline_tous').click(function(){$('dl.sel').removeClass('sel');		update_toolbar_icones();});
 });
 
+function update_toolbar_icones(){
+	if ($('dl.sel').size()>0)
+		$('#toolbar a.inactif').removeClass('inactif');
+	else
+		$('#toolbar a.EditOutline,#toolbar a.SupprimeOutline').addClass('inactif');
+}
 function del_outline(lien){
 	sel = $('dl.sel');
 	if (sel.size()){
