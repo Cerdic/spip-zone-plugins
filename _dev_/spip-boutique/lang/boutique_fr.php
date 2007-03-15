c62c4712e627cd91f2fc7e66f542e65b102e5b12
crovvfoot@gmail.com <crovvfoot@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-03-15 18:03:10 +0000

    Préparation des différent doc de travaille
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@10404 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _dev_/spip-boutique/lang/boutique_fr.php
@@ -0,0 +1,18 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// c
+'configurer_boutique'=>'Configurer Spip-Boutique',
+
+// l
+'les_cadis'=>'Les cadis',
+'les_categories'=>'Les catégories de produits',
+'les_clients'=>'Les clients',
+'les_forum_de_boutique'=>'Discution sur les produits',
+'les_produits'=>'Les produits'
+
+
+);
+?>
