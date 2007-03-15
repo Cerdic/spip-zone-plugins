43bb1bdf2b1cf885d14c61b095d9ede66feeba5a
spip@kailaasa.net <spip@kailaasa.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-03-15 08:47:33 +0000

    Ajout du plugin stable typo_guillemets (remplacement des guillemets droits par des guillemets typographiques), déjà publié dans Contrib et testé en production à http://www.college-petite-camargue.net/.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@10378 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ typo_guillemets_fonctions.php
@@ -0,0 +1,162 @@
+<?php
+/*
+Fichier de formatage typographique des guillemets, par Vincent Ramos
+<spip AD kailaasa PVNCTVM net>, sous licence GNU/GPL.
+
+Ne sont touchees que les paires de guillemets.
+
+Le formatage des guillemets est tire de
+<http://en.wikipedia.org/wiki/Quotation_mark%2C_non-English_usage>
+Certains des usages indiques ne correspondent pas a ceux que la
+barre d'insertion de caracteres speciaux de SPIP propose.
+
+Les variables suivies du commentaire LRTEUIN sont confirmees par le
+_Lexique des regles typographiques en usage a l'Imprimerie nationale_.
+
+Les variables entierement commentees sont celles pour lesquelles
+aucune information n'a ete trouvee. Par defaut, les guillements sont alors
+de la forme &ldquo;mot&rdquo;, sauf si la barre d'insertion de SPIP proposait
+deja une autre forme.
+*/
+
+function typo_guillemets_remplacements($texte){
+	switch ($GLOBALS['spip_lang']){
+		case 'fr':
+			$guilles="&laquo;&nbsp;$1&nbsp;&raquo;"; //LRTEUIN
+		break;
+//		case 'ar':
+//			$guilles="";
+//		break;
+		case 'bg':
+			$guilles="&bdquo;$1&ldquo;";
+		break;
+//		case 'br':
+//			$guilles="";
+//		break;
+//		case 'bs':
+//			$guilles="";
+//		break;
+		case 'ca':
+			$guilles="&laquo;$1&raquo;";
+		break;
+		case 'cpf':
+			$guilles="&laquo;&nbsp;$1&nbsp;&raquo;";
+		break;
+//		case 'cpf_hat':
+//			$guilles="";
+//		break;
+		case 'cs':
+			$guilles="&bdquo;$1&ldquo;";
+		break;
+		case 'da':
+			$guilles="&raquo;$1&laquo;";
+		break;
+		case 'de':
+			$guilles="&bdquo;$1&ldquo;"; //ou "&raquo;$1&laquo;" // LRTEUIN
+		break;
+		case 'en':
+			$guilles="&ldquo;$1&rdquo;"; //LRTEUIN
+		break;
+		case 'eo':
+			$guilles="&laquo;$1&raquo;";
+		break;
+		case 'es':
+			$guilles="&laquo;$1&raquo;";
+		break;
+//		case 'eu':
+//			$guilles="";
+//		break;
+//		case 'fa':
+//			$guilles="";
+//		break;
+//		case 'fon':
+//			$guilles="";
+//		break;
+//		case 'gl':
+//			$guilles="";
+//		break;
+		case 'hu':
+			$guilles="&bdquo;$1&rdquo;";
+		break;
+		case 'it':
+			$guilles="&laquo;$1&raquo;";
+		break;
+		case 'it_fem':
+			$guilles="&laquo;$1&raquo;";
+		break;
+		case 'ja':
+			$guilles="&#12300;$1&#12301;";
+		break;
+//		case 'lb':
+//			$guilles="";
+//		break;
+		case 'nl':
+			$guilles="&bdquo;$1&rdquo;";
+		break;
+//		case 'oc_auv':
+//			$guilles="";
+//		break;
+//		case 'oc_gsc':
+//			$guilles="";
+//		break;
+//		case 'oc_lms':
+//			$guilles="";
+//		break;
+//		case 'oc_lnc':
+//			$guilles="";
+//		break;
+//		case 'oc_ni':
+//			$guilles="";
+//		break;
+//		case 'oc_ni_la':
+//			$guilles="";
+//		break;
+//		case 'oc_prv':
+//			$guilles="";
+//		break;
+//		case 'oc_va':
+//			$guilles="";
+//		break;
+		case 'pl':
+			$guilles="&bdquo;$1&rdquo;";
+		break;
+		case 'pt':
+			$guilles="&laquo;$1&raquo;";
+		break;
+		case 'pt_br':
+			$guilles="&laquo;$1&raquo;";
+		break;
+		case 'ro':
+			$guilles="&bdquo;$1&rdquo;";
+		break;
+		case 'ru':
+			$guilles="&laquo;$1&raquo;";
+		break;
+		case 'tr':
+			$guilles="&laquo;$1&raquo;";
+		break;
+//		case 'vi':
+//			$guilles="";
+//		break;
+		case 'zh':
+			$guilles="&#12300;$1&#12301;"; // ou "&ldquo;$1&rdquo;" en chinois simplifie
+		break;
+		default:
+			$guilles="&ldquo;$1&rdquo;";
+	}
+
+$trouve = array(
+	'/="(.*?)"/', // 1. Echappement des guillemets a l'interieur de balises
+	'/"\s?(.*?)\s?"/', // 2. Remplacement des autres paires de guillemets (et suppression des espaces apres/avant)
+	'/@@GUILLEMETS_ECHAPPES@@/' // 3. Restitution des guillemets a l'interieur de balises
+
+			);
+$remplace = array
+			(
+	'=@@GUILLEMETS_ECHAPPES@@$1@@GUILLEMETS_ECHAPPES@@', // 1
+	$guilles, // 2
+	'"' // 3
+			);
+return preg_replace($trouve, $remplace, $texte);
+}
+?>
