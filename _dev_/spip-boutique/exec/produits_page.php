5067dc894cf60335a9bbefe575d0cc38d40c1614
crovvfoot@gmail.com <crovvfoot@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-03-16 16:31:31 +0000

    Préparation des différent doc de travaille
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@10428 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/spip-boutique/exec/produits_page.php
+++ _dev_/spip-boutique/exec/produits_page.php
@@ -4,6 +4,17 @@
 //
 
 function exec_produits_page(){
-	
+	echo debut_page(_T('boutique:gerer_les_produits'));
+		echo debut_gauche();
+			echo debut_boite_info();
+				echo _T('boutique:gerer_les_produits');
+			echo fin_boite_info();
+			echo debut_raccourcis();
+				echo '<a href="?exec=produits_edit&id_produit=new">'._T('boutique:ajouter_un_produit').'</a>';
+			echo fin_raccourcis();
+		echo debut_droite();
+		
+		if ($GLOBALS['spip_version_code']>=1.92) { echo fin_gauche(); }
+		echo fin_page();
 }
 ?>
