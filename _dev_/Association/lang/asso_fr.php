f51ba198c9db761a70db827b7b6aa8f6ad84d8f9
fdm@ufe.be <fdm@ufe.be@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-03-17 18:42:11 +0000

    Ajouté un répertoire à distance
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@10451 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _dev_/Association/lang/asso_fr.php
@@ -0,0 +1,32 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+		'configuration' => 'Configuration',
+		'effacer_les_tables' => 'Effacer les tables',
+		'gestion_association' =>'Gestion d\'une Association',
+		'gestion_de_lassoc' => 'Gestion de l\'association',
+		'ID' => 'ID',
+		'info_doc' => '<p>Ce plugin vous permet de g&eacute;rer une petite association en ligne.</p> <p>Vous pouvez ainsi  visualiser, ajouter et modifier des membres actifs, lancer des mails de masse pour les relances de cotisations, g&eacute; des dons et des ventes associatives, et tenir une livre de comptes </p>',
+		'message_relance' => '
+Bonjour,
+		
+Votre adh&eacute;sion est arriv&eacute;e &agrave; &eacute;ch&eacute;ance.
+Si vous souhaitez continuer l\'aventure en notre compagnie, n\'oubliez pas de reconduire celle-ci.
+Vous pouvez nous faire parvenir votre r&egrave;glement &agrave; votre convenance (ch&egrave;que, mandat  ou virement ).
+
+Le bureau de l\'association.
+
+Merci de ne pas r&eacute;pondre directement &agrave; ce message automatique 
+		',
+		'ref_int' => 'R&eacute;f. int.',
+		'reference_interne' => 'R&eacute;f&eacute;rence interne',
+		'secteur' => 'Secteur',
+		'titre_relance' => 'Renouvellement de votre cotisation',
+		'utilisateur1' => 'Utilisateur 1',
+		'utilisateur2' => 'Utilisateur 2',
+		'utilisateur3' => 'Utilisateur 3',
+		'utilisateur4' => 'Utilisateur 4'
+);
+?>
