e5684e24f8deac97b5f3ac6e4def9c3fefa8073f
fdm@ufe.be <fdm@ufe.be@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-03-17 18:35:56 +0000

    Ajouté un répertoire à distance
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@10443 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _dev_/Association/association_mes_options.php
@@ -0,0 +1,44 @@
+﻿<?php
+//Conversion de date
+function datefr($date) { 
+	$split = split('-',$date); 
+	$annee = $split[0]; 
+	$mois = $split[1]; 
+	$jour = $split[2]; 
+return $jour.'-'.$mois.'-'.$annee; 
+} 
+
+//Validation d'adresse email
+function validation_email($email) {
+	$atom   = '[-a-z0-9!#$%&\'*+\\/=?^_`{|}~]';   	// caractères autorisés avant l'arobase
+	$domain = '([a-z0-9]([-a-z0-9]*[a-z0-9]+)?)'; 	// caractères autorisés après l'arobase (nom de domaine)
+	$regex = '/^' . $atom . '+' .   					// Une ou plusieurs fois les caractères autorisés avant l'arobase
+	'(\.' . $atom . '+)*' .        							 // Suivis par zéro point ou plus
+														// séparés par des caractères autorisés avant l'arobase
+	'@' .                           							// Suivis d'un arobase
+	'(' . $domain . '{1,63}\.)+' .  						// Suivis par 1 à 63 caractères autorisés pour le nom de domaine
+														// séparés par des points
+	$domain . '{2,63}$/i';          						// Suivi de 2 à 63 caractères autorisés pour le nom de domaine
+
+// test de l'adresse e-mail
+if (preg_match($regex, $email)) 
+{return true;}
+else {return false;}
+}
+
+//Creation d'un login
+function cree_login($email) {
+     $login = substr($email, 0, strpos($email, "@"));
+     $login = strtolower($login);
+     $login = ereg_replace("[^a-zA-Z0-9]", "", $login);     
+		
+     for ($i = 0; ; $i++) {
+     	if ($i) $login = $login.$i;
+     	else $login = $login;
+     	$query = "SELECT id_auteur FROM spip_auteurs WHERE login='$login'";
+     	$result = spip_query($query);
+     	if (!spip_num_rows($result)) break;
+	     }		
+	return $login;
+  }
+?>
---
+++ _dev_/Association/inc_association.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+* Plugin Association
+*
+* Copyright (c) 2007
+* Bernard Blazin & François de Montlivault
+* http://www.plugandspip.com 
+* Ce programme est un logiciel libre distribue sous licence GNU/GPL.
+* Pour plus de details voir le fichier COPYING.txt.
+*  
+**/
+
+$p=explode(basename(_DIR_PLUGINS)."/",str_replace('\\','/',realpath(dirname(__FILE__))));
+define('_DIR_PLUGIN_ASSOCIATION',(_DIR_PLUGINS.end($p)));
+
+/* public static */
+function association_ajouterBoutons($boutons_admin) {
+	// si on est admin
+	if ($GLOBALS['connect_statut'] == "0minirezo" && $GLOBALS["connect_toutes_rubriques"]) {
+	  // on voit le bouton dans la barre "naviguer"
+	  $boutons_admin['naviguer']->sousmenu['association']= new Bouton(
+		"../"._DIR_PLUGIN_ASSOCIATION."/img_pack/annonce.gif",  // icone
+		'Gestion Association'	// titre
+		);
+	}
+	return $boutons_admin;
+}
+
+/* public static */
+function association_ajouterOnglets($flux) {
+	$rubrique = $flux['args'];
+	return $flux;
+}
+
+?>
