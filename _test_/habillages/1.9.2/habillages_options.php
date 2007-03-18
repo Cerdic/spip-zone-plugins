6d64dc8e19e761ad22f7ff2c1de250d50ba62413
franck.ducas@free.fr <franck.ducas@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-03-18 22:15:05 +0000

    Ajout de commentaires (poursuite de la mise a plat des futures modifications).
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@10492 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/habillages/1.9.2/habillages_options.php
+++ _test_/habillages/1.9.2/habillages_options.php
@@ -1,6 +1,10 @@
 <?php
 include_spip('inc/meta');
 
+# TODO : ajout d'un repertoire de personnalisation des squelettes (les 
+# repertoires classiques) pour permettre le bidouillage des squelettes.
+# Ceci doit venir en plus d'une interface de personnalisation des dossiers
+# de squelettes et de themes.
 lire_metas();
 $lire_habillages_squelettes = $GLOBALS['meta']['habillages_squelettes'];
 $lire_habillages_themes = $GLOBALS['meta']['habillages_themes'];
