0beb296084832a00443a170a4739cfad17e34ff2
renato@rezo.net <renato@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-03-18 22:44:25 +0000

    unbind just the functions previously binded; detect mousemove on the entire window (Fil)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@10494 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/crayons/js/resizehandle.js
+++ _stable_/crayons/js/resizehandle.js
@@ -11,18 +11,18 @@ jQuery.fn.resizehandle = function() {
       .bind('mousedown', function(e) {
         var h = me.height();
         var y = e.clientY;
-        jQuery('body')
-        .bind('mousemove', function(e) {
+        var moveHandler = function(e) {
           me
           .height(Math.max(20, e.clientY + h - y));
-        })
-        .bind('mouseup', function(e) {
-          me
-          .height(Math.max(20, e.clientY + h - y));
-          jQuery('body')
-          .unbind('mousemove')
-          .unbind('mouseup');
-        })
+        };
+        var upHandler = function(e) {
+          jQuery('html')
+          .unbind('mousemove',moveHandler)
+          .unbind('mouseup',upHandler);
+        };
+        jQuery('html')
+        .bind('mousemove', moveHandler)
+        .bind('mouseup', upHandler);
       })
     );
   });
