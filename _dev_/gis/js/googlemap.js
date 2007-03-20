aa63c65f1ea884120944a046de800d8263bbf030
horaciogd@vhplab.net <horaciogd@vhplab.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-03-20 19:43:45 +0000

    gis: we are starting to put the hole javascript functions related with googlemaps in a single document
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@10548 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/gis/js/googlemap.js
+++ _dev_/gis/js/googlemap.js
@@ -5,6 +5,10 @@ var contidosHTML = [];
 //array para gardar as URL dos sonidos que se reproduciran en cada xanela
 var URLsons = [];
 
+function getNodeText(node){
+	return node.text || node.firstChild ? node.firstChild.nodeValue : "";
+}
+
 function creaMarcador(point, html, icon, son) {
 	//creamos un obxecto GMarker e o gradamos nunha variable
 	var marcador = new GMarker(point, icon);
@@ -13,80 +17,56 @@ function creaMarcador(point, html, icon, son) {
 		marcador.openInfoWindowHtml(html);
 		//cando se abre a ventana do marcador executamos as seguintes intsruccions
 		GEvent.addListener(map,"infowindowopen", function() {
-			//esta parte del codigo enbebe un obxecto flah na ventana creado con flashobject.js
-			var fo = new FlashObject("http://www.escoitar.org/loudblog/custom/templates/berio/musicplayer.swf?autoplay=true&song_url="+son, "player_x", "17", "17", "6", "#FFFFFF");
-			fo.write("player");
-			//inicializamos lightbox para permitir o efecto de apertura da imaxe
-			//initLightbox();
+			if(son){
+				//esta parte del codigo enbebe un obxecto flah na ventana creado con flashobject.js
+				var fo = new FlashObject(URLbase + "plugins/gis/img_pack/musicplayer.swf?autoplay=true&song_url="+son, "player_x", "17", "17", "6", "#FFFFFF");
+				fo.write("player");
+			}
 		});
 	});		
 	return marcador;
 }
 
-function agregarMarcador (xmlMarker, minZoom,  maxZoom) {
-	//almacenamos en variables a informacion contida nos atributos de cada marcador no xml
-	var id = parseFloat(xmlMarker.getAttribute("id"));
-	var lat = parseFloat(xmlMarker.getAttribute("lat"));
-	var lng = parseFloat(xmlMarker.getAttribute("lng"));
-	var cat = parseFloat(xmlMarker.getAttribute("categoria"));
-	var son = xmlMarker.getAttribute("arquivo");
-	var texto = xmlMarker.getAttribute("texto")
-	var autor = xmlMarker.getAttribute("autor")
-	var img = xmlMarker.getAttribute("img")
-	var urlweb = xmlMarker.getAttribute("urlweb")
-	var web = xmlMarker.getAttribute("web")
-	var hora = xmlMarker.getAttribute("hora")
-	var data = xmlMarker.getAttribute("fecha")
-	//construimos o html que contera a xanala do marcador
-    var html = escribeHtml(texto, autor, img, urlweb, web, hora, data);
-    //asignamos o icono que lle corresponde a sua categoria
-    var icono_categoria = conxunto_iconos[cat];
-    //creamos un GPoint para as coordenadas correspondentes 
-    var point = new GPoint(lat,lng);
-	//creamos o marcador
-    var marcador = creaMarcador(point, html, icono_categoria, son);
-    //na posicion adecuada dos arrays recollemos a informacion que sexa necesaria
+function agregarMarcador (xmlItem, minZoom,  maxZoom) {
+	//almacenamos en distintas variables la informacion contenida nen los chilNodes de cada item-marcador do xml
+	var id = getNodeText(xmlItem.childNodes[2]);
+	var lat = parseFloat(getNodeText(xmlItem.childNodes[6]));
+	var lng = parseFloat(getNodeText(xmlItem.childNodes[7]));
+	var son = xmlItem.childNodes[8].getAttribute("url");
+	var html = getNodeText(xmlItem.childNodes[4]);
+   	
+	//creamos un Gpoint para situar nel o marcador
+	var point = new GPoint(lat,lng);
+		
+	//creamos un icono para o marcador
+	var icono_categoria = new GIcon();
+	icono_categoria.image = URLbase + "/plugins/gis/img_pack/correxir.png";
+	icono_categoria.shadow = URLbase + "/plugins/gis/img_pack/shadow.png";
+	icono_categoria.iconSize = new GSize(20, 34);
+	icono_categoria.shadowSize = new GSize(22, 20);	
+	icono_categoria.iconAnchor = new GPoint(10, 34);
+	icono_categoria.infoWindowAnchor = new GPoint(5, 1);
+			
+	// creamos el marcador con los datos almacenados en las variables
+	var marcador = creaMarcador(point, html, icono_categoria, son);
+		
+	// recollemos a informacion que sexa necesaria en distintos arrays, usando como identificador a id do artigo
 	marcadores[id] = marcador;
 	contidosHTML[id] = html;
 	URLsons[id] = son;
+    		
 	//engadimos o marcador ao markerManager antes "map.addOverlay(marker);"
 	if (maxZoom) {
 		markerManager.addMarker(marcador, minZoom,  maxZoom);
 	} else {
 		markerManager.addMarker(marcador, minZoom);
 	}
-    
-}
-
-function escribeHtml(texto, autor, img, urlweb, web, hora, fecha) {
-
-	var html = "<div style=\"width:200px\"><div id=\"player\" style=\"margin-bottom: 5px;\"></div><span class=\"windowText\">" + texto + "</span>";
-	
-	if (img) {
-		html += "<br><br><a href=\"http://www.escoitar.org/upload/imaxes/";
-		html += img;
-		html += "\" rel=\"lightbox[titulo]\"><img src=\"http://www.escoitar.org/upload/imaxes/mini_";
-		html += img;
-		html += "\" border=\"0\" height=\"60\"></a>";
-	}
-	
-	html += "<br><b class=\"windowBlack\">" + autor + "</b>&nbsp;&nbsp;&nbsp;";
-	
-	if (urlweb.length>4) {
-		html += "<a class=\"window\" target=\"_blank\" href=\"" + urlweb + "\">" + web + "</a><br>";
-	}
-	
-    html += "<span class=\"windowRight\"><b>as " + hora + " do " + fecha + "</b></span><br><br></div>";
-    
-	return html;
 }
 
 function abrirVentana(identificador) {
 	map.closeInfoWindow();
 	marcadores[identificador].openInfoWindowHtml(contidosHTML[identificador]);
 	//enbebemos o flash player do son
-	var fo = new FlashObject("http://www.escoitar.org/loudblog/custom/templates/berio/musicplayer.swf?autoplay=true&song_url=" + URLsons[identificador], "player_x", "17", "17", "6", "#FFFFFF");
+	var fo = new FlashObject( URLbase + "/plugins/gis/img_pack/musicplayer.swf?autoplay=true&song_url=" + URLsons[identificador], "player_x", "17", "17", "6", "#FFFFFF");
 	fo.write("player");
-	//inicializamos lightbox para permitir o efecto de apertura da imaxe
-	//initLightbox();
 }
