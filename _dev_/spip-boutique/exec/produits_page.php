cf97ec5cc620a0076abf0ff4c59f250e4ffd0700
crovvfoot@gmail.com <crovvfoot@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-03-20 18:07:23 +0000

    petit petit ajout ...
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@10543 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/spip-boutique/exec/produits_page.php
+++ _dev_/spip-boutique/exec/produits_page.php
@@ -4,15 +4,34 @@
 //
 
 function exec_produits_page(){
+	global $connect_statut, $connect_id_auteur;
 	echo debut_page(_T('boutique:gerer_les_produits'));
 		echo debut_gauche();
+			echo pipeline('affiche_gauche',array('args'=>array('exec'=>'produits_page'),'data'=>''));
 			echo debut_boite_info();
 				echo _T('boutique:gerer_les_produits');
 			echo fin_boite_info();
 			echo debut_raccourcis();
-				echo '<a href="?exec=produits_edit&id_produit=new">'._T('boutique:ajouter_un_produit').'</a>';
+				$result = spip_query("SELECT id_categorie FROM spip_boutique_categories LIMIT 1");
+				if (spip_num_rows($result) > 0) {
+				echo bloc_des_raccourcis(icone_horizontale(_T('boutique:icone_creer_produit'), generer_url_ecrire("produits_edit","new=oui"), "produit-24.gif", "creer.gif", false));
+				} else {
+					if ($connect_statut == '0minirezo') {
+						echo _T('boutique:creer_categorie');
+						echo	bloc_des_raccourcis(icone_horizontale (_T('boutique:creer_categorie'), generer_url_ecrire("categories_edit","new=oui&retour=nav"), "categorie-24.gif", "creer.gif",false));
+				}
 			echo fin_raccourcis();
 		echo debut_droite();
+		
+		if (spip_num_rows($result) > 0 ){
+			$res_liste_produits_publie = spip_query("SELECT id_produit, titre FROM spip_boutique_categories WHERE id_auteur = '".$connect_id_auteur."' AND etat='publie';");
+			if (spip_num_rows($res_liste_produits_publie) > 0 ){
+				
+			}
+			$res_liste_produits_prepa = spip_query("SELECT id_produit, titre FROM spip_boutique_categories WHERE id_auteur = '".$connect_id_auteur."' AND etat='prepa';");
+			$res_liste_produits_refuse = spip_query("SELECT id_produit, titre FROM spip_boutique_categories WHERE id_auteur = '".$connect_id_auteur."' AND etat='refuse';");
+			
+		}
 		
 		if ($GLOBALS['spip_version_code']>=1.92) { echo fin_gauche(); }
 		echo fin_page();
