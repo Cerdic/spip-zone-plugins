70188f77450338f545fc73d96bb796d9d8536a23
spip@kailaasa.net <spip@kailaasa.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-03-22 12:57:35 +0000

    Mise à jour : ajout de la fonction pour afficher la liste des articles les plus populaires
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@10620 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/eva/javascript/fonction_alterner_visibilite.js
+++ _dev_/eva/javascript/fonction_alterner_visibilite.js
@@ -1,5 +1,5 @@
 var liste_cache = new Array(0);
-
+var liste_cache_secteurs = new Array (0);
 
 function alterner_visibilite(liste, onload){
 	var el  = document.getElementById(liste).getElementsByTagName("LI");
@@ -33,4 +33,16 @@ function tout_ouvrir(){
 	document.getElementById("Contenu").style.width="80%";
 	document.getElementById("Contenu").style.left="10%";
 	
+}
+
+var valeur_pop = "";
+function afficher_populaires(){
+	if(valeur_pop==""){
+		document.getElementById('populaires_menu').style.display='block';
+		valeur_pop="masquer";
+	}
+	else{
+		document.getElementById('populaires_menu').style.display='none';
+		valeur_pop="";
+	}
 }
