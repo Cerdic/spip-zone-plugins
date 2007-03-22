99b57460faadaca0af5b9dd68a5d2258bfc4a7b3
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-03-22 09:30:07 +0000

    corrections entites
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@10612 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/comarquage/1_9_0/lang/comarquage_fr.php
+++ _stable_/comarquage/1_9_0/lang/comarquage_fr.php
@@ -3,9 +3,9 @@
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-'avis_serveur_indisponible' => "La page Service Public ne peut être affich&eacute;e pour le moment car le contenu XML n'est pas disponible.<br/>
-Le serveur de la documentation francaise pourrait &egrave;tre momentan&eacute;ment hors ligne.<br/>
-Veuillez réessayer ultérieurement.",
+'avis_serveur_indisponible' => "La page Service Public ne peut &ecirc;tre affich&eacute;e pour le moment car le contenu XML n'est pas disponible.<br/>
+Le serveur de la documentation fran&ccedil;aise pourrait &egrave;tre momentan&eacute;ment hors ligne.<br/>
+Veuillez r&eacute;essayer ult&eacute;rieurement.",
 'avis_erreur' => "Une erreur interne s'est produite. Nous faisons notre possible pour la corriger dans les meilleurs d&eacute;lais.<br/>
 Nous vous remercions de votre compr&eacute;hension",
 
