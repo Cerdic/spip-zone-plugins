ccff0bf9ab205773b52718dc7e0a3a9689ab9d81
edouard@lafargue.name <edouard@lafargue.name@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-03-25 13:36:29 +0000

    Version initiale du plugin OpenID
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@10706 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _dev_/authentification/lang/openid_fr.php
@@ -0,0 +1,8 @@
+<?php
+   /*******
+    * Fichier de langue pour le plugin OpenID
+    ******/
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+  'login_login2' => 'Login (identifiant de connexion au site) ou bien OpenID&nbsp;:'
+);
