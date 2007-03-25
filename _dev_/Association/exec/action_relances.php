315474e1f40d582befe7658b0c5005fc437d4de4
fdm@ufe.be <fdm@ufe.be@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-03-25 13:03:27 +0000

    fonction envoyer_email implémentée
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@10704 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/Association/exec/action_relances.php
+++ _dev_/Association/exec/action_relances.php
@@ -36,23 +36,24 @@ $email_tab=(isset($_POST["email"])) ? $_POST["email"]:array();
 $statut_tab=(isset($_POST["statut"])) ? $_POST["statut"]:array();
 $relance_tab=(isset($_POST["relance"])) ? $_POST["relance"]:array();
 $count=count ($email_tab);
+
 $query = spip_query ("SELECT * FROM spip_asso_profil WHERE id_profil=1");
 
 while ($data = mysql_fetch_assoc($query)){
 //On prépare le mail et on envoi! On peut modifier le $headers à sa guise
-$headers .= "From: ".$data['nom']." <".$data['mail'].">\n";       //expéditeur Association
-$headers .= "Reply-To: ".$data['mail']."\n";      // réponse automatique à Association
-$headers .= "X-Mailer: PHP/" . phpversion();         // mailer
-$headers .= "X-Sender: < ".$data['mail'].">\n";   } 
-$headers .= "X-Priority: 1\n";                // Message urgent ! 
-$headers .= "X-MSMail-Priority: High\n";         // définition de la priorité
-//$headers .= "Return-Path: < webmaster@ >\n"; // En cas d' erreurs 
-//$headers .= "Errors-To: < webmaster@ >\n";    // En cas d' erreurs 
-//$headers .= "cc:  \n"; // envoi en copie à …
-//$headers .= "bcc: \n";          // envoi en copie cachée à …
-//$headers .= "Content-Type: text/html; charset=iso-8859-1\n"; // Type Mime pour un message au format HTML
+$expediteur = $data['nom']." <".$data['mail'].">";       //expéditeur Association
+$entetes .= "Reply-To: ".$data['mail']."\n";      // réponse automatique à Association
+$entetes .= "X-Mailer: PHP/" . phpversion();         // mailer
+$entetes .= "X-Sender: < ".$data['mail'].">\n";   } 
+$entetes .= "X-Priority: 1\n";                // Message urgent ! 
+$entetes .= "X-MSMail-Priority: High\n";         // définition de la priorité
+//$entetes .= "Return-Path: < webmaster@ >\n"; // En cas d' erreurs 
+//$entetes .= "Errors-To: < webmaster@ >\n";    // En cas d' erreurs 
+//$entetes .= "cc:  \n"; // envoi en copie à …
+//$entetes .= "bcc: \n";          // envoi en copie cachée à …
+//$entetes .= "Content-Type: text/html; charset=iso-8859-1\n"; // Type Mime pour un message au format HTML
 
-//On envoit le mail aux destinatairex sélectionnés, on affiche le membre relancé et on change le statut de relance
+//On envoit le mail aux destinataires sélectionnés, on affiche le membre relancé et on change le statut de relance
 
 for ( $i=0 ; $i < $count ; $i++ )
 {
@@ -62,7 +63,7 @@ $relance = $relance_tab[$i];
 
 	if ( isset ( $relance ) )
 	{
-	mail($email, $sujet, $message, $headers);
+	envoyer_mail ( $email, $sujet, $message, $from = $expediteur, $headers = $entetes );
 
 	if ($statut=="echu"){
 	spip_query("UPDATE spip_asso_adherents SET statut='relance' WHERE id_adherent = '$relance' ");
@@ -70,12 +71,12 @@ $relance = $relance_tab[$i];
 	}
 }
 
-echo '<div><br><strong>'.$count;
+echo '<p><strong>'.$count;
 if ($count=1)
 { echo ' relance effectu&eacute;e';}
 else
 { echo ' relances effectu&eacute;es';}
-echo '</strong></div>';
+echo '</strong></p>';
 
 	
 	//remettre le champ 0 à 1 et réactualiser la date
