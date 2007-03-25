9bd2ea98439a01f23e0b320e0a96ad1b5b695853
franck.ducas@free.fr <franck.ducas@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-03-25 11:49:06 +0000

    Encore un qui traine apres la sonnerie...
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@10701 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/habillages/1.9.2/habillages_options.php
+++ _test_/habillages/1.9.2/habillages_options.php
@@ -7,8 +7,6 @@ $lire_habillages_prefixe_squel = $GLOBALS['meta']['habillages_prefixe_squel'];
 $squelette_reperso = $GLOBALS['meta']['habillages_'.$lire_habillages_prefixe_squel.'_reperso'];
 $lire_habillages_themes = $GLOBALS['meta']['habillages_themes'];
 
-echo $squelette_reperso;
-
 if ($lire_habillages_squelettes == "dist" && $lire_habillages_themes == "defaut") {
 	$habillages_dossiers_squelettes = "dist";
 	$GLOBALS['dossier_squelettes'] = $habillages_dossiers_squelettes;
