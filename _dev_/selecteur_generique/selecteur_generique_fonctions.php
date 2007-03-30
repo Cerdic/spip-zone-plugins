304e63fee55bfe890e9e16b77ed41de169df9102
fil@rezo.net <fil@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-03-30 13:07:35 +0000

    un critere plus specifique, et beaucoup plus rapide
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@10810 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/selecteur_generique/selecteur_generique_fonctions.php
+++ _dev_/selecteur_generique/selecteur_generique_fonctions.php
@@ -8,4 +8,43 @@ include_spip('inc/autoriser');
 if (! autoriser('modifier', 'article', _request('id_article')) )
 	die ('rien a faire la mon ami');
 
+
+// critere {contenu_auteur_select} , cf. sedna
+function critere_contenu_auteur_select($idb, &$boucles, $crit) {
+	$boucle = &$boucles[$idb];
+
+	if (isset($crit->param[0][0])
+	AND $crit->param[0][0]->texte == 'strict')
+		$debut = '';
+	else
+		$debut = '%';
+
+	// un peu trop rapide, ca... le compilateur exige mieux (??)
+	$boucle->hash = '
+	// RECHERCHE
+	if ($r = _request("value")) {
+		$r = _q("'.$debut.'$r%");
+		$s = "(
+			auteurs.nom LIKE $r
+			OR auteurs.email LIKE $r'
+
+	// on ne cherche pas dans la bio etc
+	// si on peut trouver direct dans le nom ou l'email
+	. (!$debut
+		? ''
+		: '
+			OR auteurs.bio LIKE $r
+			OR auteurs.nom_site LIKE $r
+			OR auteurs.url_site LIKE $r
+		')
+	.'
+		)";
+	} else {
+		$s = 1;
+	}
+	';
+	$boucle->where[] = '$s';
+}
+
+
 ?>
