261aaa7fb870695be04cd8a95a496249b9061e79
franck.ducas@free.fr <franck.ducas@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-03-30 09:38:35 +0000

    Quatre plus au lieu de trois, pour coller à tweak.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@10806 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/decoupenpage/decouper_fonctions.php
+++ _test_/decoupenpage/decouper_fonctions.php
@@ -21,7 +21,7 @@ function decouper_en_page($texte) {
         
         if (empty($artsuite)) $artsuite = 0;
         
-        $page = split('\+\+\+', $texte);
+        $page = split('\+\+\+\+', $texte);
         
         $num_pages = count($page);
 
