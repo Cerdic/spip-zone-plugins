4b718c011b278a601830572cf4499c8d1af53003
fil@rezo.net <fil@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-03-30 20:37:31 +0000

    une version qui fonctionne avec FF2 (initial + ajax), Safari (pas initial, mais ajax), et peut-etre FF1.5 sauf si je l'ai casse... c'est un peu delirant le traitement xml, en fait
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@10827 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/selecteur_generique/inserer_javascript.php
+++ _dev_/selecteur_generique/inserer_javascript.php
@@ -20,8 +20,15 @@ if (_request('exec') == 'articles') {
 	$flux .= '<script type="text/javascript"><!--'
 		. <<<EOS
 
-var appliquer_selecteur_cherche_auteur = function(doc) {
-	var inp = jQuery('input[@name=cherche_auteur]', doc);
+var appliquer_selecteur_cherche_auteur = function() {
+
+	// chercher l'input de saisie
+	var inp = jQuery('input[@name=cherche_auteur]', this);
+
+	// ne pas reappliquer si on vient seulement de charger les suggestions
+	if (inp[0].autoCFG) return;
+
+	// attacher l'autocompleter
 	inp.Autocomplete({
 		'source': '$ac',
 		'delay': 300,
@@ -30,25 +37,28 @@ var appliquer_selecteur_cherche_auteur = function(doc) {
 		'selectClass': 'selectAutocompleter',
 		'minchars': 2,
 		'onSelect': function(li) {
-			var hid = jQuery(
-			  "<input type='hidden' name='nouv_auteur' value='"+li.id_auteur+"' />");
-			inp.attr("name", "xxx")
-			.parents("form")
-			.append(hid)
-			.ajaxSubmit();
+			if (li.id_auteur > 0) {
+				inp.attr("name", "xxx")
+				.parents("form")
+				.append(
+					jQuery("<input type='hidden' "
+						+ "name='nouv_auteur' value='"+li.id_auteur+"' />"
+					)
+				)
+				.ajaxSubmit()
+				.trigger('submit'); // FF 1.5 demande ajaxSubmit() ??
+			}
 		}
 	});
 }
-var appliquer_selecteur_cherche_auteur_ajax = function(doc) {
-	console.log(doc);
-	if (typeof doc == 'undefined')
-		return;
-	else
-		return appliquer_selecteur_cherche_auteur(doc);
-}
 
+// Premier chargement
 jQuery(document).ready(appliquer_selecteur_cherche_auteur);
-onAjaxLoad(appliquer_selecteur_cherche_auteur_ajax);
+
+// Chargements ajax suivants
+// setTimeout obligatoire sinon on s'applique au vieux DOM 
+// pre-update quand on clique sur une suppression d'auteur !!
+onAjaxLoad(function(){setTimeout(appliquer_selecteur_cherche_auteur, 200);});
 
 
 EOS
