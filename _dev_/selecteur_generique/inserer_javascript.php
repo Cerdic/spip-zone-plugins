cb22bc8f4917c74c43e327eef4a3964afe99866d
fil@rezo.net <fil@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-03-31 11:19:36 +0000

    avec le click() ca marche pas chez moi, pour les auteurs, mais ca marche pour les mots
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@10834 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/selecteur_generique/inserer_javascript.php
+++ _dev_/selecteur_generique/inserer_javascript.php
@@ -9,8 +9,8 @@ if (_request('exec') == 'articles') {
 	$ac = parametre_url(generer_url_public('selecteur_generique_auteur'),
 		'id_article', _request('id_article'), '\\x26');
 
-	$js .= '<script type="text/javascript"><!--'
-		. <<<EOS
+	$js .= <<<EOS
+<script type="text/javascript"><!--
 
 var appliquer_selecteur_cherche_auteur = function() {
 
@@ -36,8 +36,9 @@ var appliquer_selecteur_cherche_auteur = function() {
 					jQuery("<input type='hidden' name='nouv_auteur' value='"+li.id+"' />"
 					)
 				)
-				.ajaxSubmit() // FF 1.5 exige ajaxSubmit(), mais FF 2 et Safari font un POST sans retour
-				.trigger('submit'); // celui-ci fait un POST avec retour sous FF2 et Safari
+				.find("input[@type=submit]")
+					.click()
+				.end();
 			}
 		}
 	});
@@ -51,10 +52,9 @@ jQuery(document).ready(appliquer_selecteur_cherche_auteur);
 // pre-update quand on clique sur une suppression d'auteur !!
 onAjaxLoad(function(){setTimeout(appliquer_selecteur_cherche_auteur, 200);});
 
+// --></script>
 
-EOS
-		. '// --></script>'
-		. "\n";
+EOS;
 
 }
 
@@ -65,8 +65,8 @@ if (_request('exec') == 'articles' /* ou breves etc */) {
 		'id_article', _request('id_article'), '\\x26');
 
 
-	$js .= '<script type="text/javascript"><!--'
-		. <<<EOS
+	$js .= <<<EOS
+<script type="text/javascript"><!--
 
 var appliquer_selecteur_cherche_mot = function() {
 
@@ -98,8 +98,9 @@ var appliquer_selecteur_cherche_mot = function() {
 						jQuery("<input type='hidden' name='nouv_mot' value='"+li.id+"' />"
 						)
 					)
-					.ajaxSubmit() // FF 1.5 exige ajaxSubmit(), mais FF 2 et Safari font un POST sans retour
-					.trigger('submit'); // celui-ci fait un POST avec retour sous FF2 et Safari
+					.find("input[@type=submit]")
+						.click()
+					.end();
 				}
 			}
 		});
@@ -112,14 +113,62 @@ jQuery(document).ready(appliquer_selecteur_cherche_mot);
 // Chargements ajax suivants
 onAjaxLoad(function(){setTimeout(appliquer_selecteur_cherche_mot, 200);});
 
+// --></script>
 
-EOS
-		. '// --></script>'
-		. "\n";
-
+EOS;
 
 
 
+
+}
+
+if (_request('exec') == 'articles_edit') {
+
+	$ac = parametre_url(generer_url_public('selecteur_generique_rubrique'),
+		'id_article', _request('id_article'), '\\x26');
+
+	$js .= <<<EOS
+<script type="text/javascript"><!--
+
+var appliquer_selecteur_cherche_rubrique = function() {
+
+	// chercher l'input de saisie
+	var inp = jQuery('input#titreparent', this);
+
+	// ne pas reappliquer si on vient seulement de charger les suggestions
+	if (!inp[0] || inp[0].autoCFG) return;
+
+	// attacher l'autocompleter
+	inp
+	.attr('disabled', false)
+	.Autocomplete({
+		'source': '$ac',
+		'delay': 300,
+		'autofill': false,
+		'helperClass': 'autocompleter',
+		'selectClass': 'selectAutocompleter',
+		'minchars': 2,
+		'onSelect': function(li) {
+			if (li.id > 0) {
+				inp
+				.parents('form')
+				.find('input[@name=id_parent]')
+					. attr('value', li.id)
+				.end();
+			}
+		}
+	});
+}
+
+// Premier chargement
+jQuery(document).ready(appliquer_selecteur_cherche_rubrique);
+
+// Chargements ajax suivants (pas pertinent pour le selecteur de rubriques dans articles_edit)
+//onAjaxLoad(function(){setTimeout(appliquer_selecteur_cherche_rubrique, 200);});
+
+// --></script>
+
+EOS;
 
 }
 
