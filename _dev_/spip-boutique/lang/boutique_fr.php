135bef8629a2e70e173801924514ed94383e3c6b
crovvfoot@gmail.com <crovvfoot@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-04-03 17:32:14 +0000

    Finalisation de la table produit et début des developpements proprement dis -- fiche de lang
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@10945 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/spip-boutique/lang/boutique_fr.php
+++ _dev_/spip-boutique/lang/boutique_fr.php
@@ -10,17 +10,19 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 // c
 'configurer_boutique'=>'Configurer Spip-Boutique',
 'creer_categorie'=>'Cr&eacute;er une cat&eacute;gorie',
+'creer_un_produit'=>'Cr&eacute;er un produit',
 
 // g
+'gerer_les_categories'=>'Gestion des cat&eacute;gories',
 'gerer_les_produits'=>'Gestion des produits',
 
 // i
 'icone_creer_produit'=>'Cr&eacute;er un produit',
-
+'icone_creer_categorie'=>'Cr&eacute;er une cat&eacute;gorie',
 
 // l
 'les_cadis'=>'Les caddies',
-'les_categories'=>'Les catégories de produit',
+'les_categories'=>'Les cat&eacute;gories de produit',
 'les_clients'=>'Les clients',
 'les_forum_de_boutique'=>'Discussion sur les produits',
 'les_produits'=>'Les produits'
