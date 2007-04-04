7d8cd99c8e054f875436a538bc4e10bda890cd88
crovvfoot@gmail.com <crovvfoot@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-04-04 21:37:39 +0000

    creation de categorie : debut du developpement
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@11002 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/spip-boutique/lang/boutique_fr.php
+++ _dev_/spip-boutique/lang/boutique_fr.php
@@ -12,6 +12,9 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 'creer_categorie'=>'Cr&eacute;er une cat&eacute;gorie',
 'creer_un_produit'=>'Cr&eacute;er un produit',
 
+// e
+'editer_une_categorie'=>'Edition d\'une cat&eacute;gorie',
+
 // g
 'gerer_les_categories'=>'Gestion des cat&eacute;gories',
 'gerer_les_produits'=>'Gestion des produits',
@@ -25,8 +28,10 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 'les_categories'=>'Les cat&eacute;gories de produit',
 'les_clients'=>'Les clients',
 'les_forum_de_boutique'=>'Discussion sur les produits',
-'les_produits'=>'Les produits'
+'les_produits'=>'Les produits',
 
+// t
+'titre_de_la_categorie'=>'Titre de la cat&eacute;gorie :'
 
 );
 ?>
