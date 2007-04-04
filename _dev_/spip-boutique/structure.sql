6ace1787eb253f698593c01f7ddf4d3fe4cc682a
crovvfoot@gmail.com <crovvfoot@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-04-04 20:35:02 +0000

    Oubli de l'auto increment :p
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@10996 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/spip-boutique/structure.sql
+++ _dev_/spip-boutique/structure.sql
@@ -1,23 +1,5 @@
--- phpMyAdmin SQL Dump
--- version 2.9.1.1-Debian-3
--- http://www.phpmyadmin.net
--- 
--- Serveur: localhost
--- Généré le : Mercredi 04 Avril 2007 à 18:06
--- Version du serveur: 5.0.32
--- Version de PHP: 5.2.0-8+etch1
--- 
--- Base de données: `spip_192`
--- 
-
--- --------------------------------------------------------
-
--- 
--- Structure de la table `spip_produits`
--- 
-
-CREATE TABLE `spip_produits` (
-  `id_produit` bigint(20) NOT NULL,
+CREATE TABLE `spip_boutique_produits` (
+  `id_produit` bigint(20) NOT NULL auto_increment,
   `token` text NOT NULL,
   `titre` text NOT NULL,
   `soustitre` text NOT NULL,
@@ -39,5 +21,6 @@ CREATE TABLE `spip_produits` (
   `statut` varchar(10) NOT NULL,
   `date` datetime NOT NULL,
   `date_redac` datetime NOT NULL,
-  `date_modif` datetime NOT NULL
-) TYPE=MyISAM;
+  `date_modif` datetime NOT NULL,
+  PRIMARY KEY  (`id_produit`)
+) TYPE=MyISAM ;
