56e885d97e895af8c8da6fbd4a1a917e282c4356
crovvfoot@gmail.com <crovvfoot@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-04-06 10:11:16 +0000

    dev de l'ajout de catégorie
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@11060 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/spip-boutique/boutique_pipelines_admin.php
+++ _dev_/spip-boutique/boutique_pipelines_admin.php
@@ -6,9 +6,9 @@ $p=explode(basename(_DIR_PLUGINS)."/",str_replace('\\','/',realpath(dirname(__FI
 define('_DIR_PLUGIN_SPIPDIGG',(_DIR_PLUGINS.end($p)));
 
 function boutique_ajouter_boutons($flux){
-	$flux['naviguer']->sousmenu['produits_page']= new Bouton("../"._DIR_PLUGIN_BOUTIQUE."/img_pack/package-x-generic.png",_T('boutique:les_produits'));
-	$flux['naviguer']->sousmenu['cadis_page']= new Bouton("../"._DIR_PLUGIN_BOUTIQUE."/img_pack/cadis.png",_T('boutique:les_cadis'));
-	$flux['naviguer']->sousmenu['categories_page']= new Bouton("../"._DIR_PLUGIN_BOUTIQUE."/img_pack/folder.png",_T('boutique:les_categories'));
+	$flux['naviguer']->sousmenu['produits_page']= new Bouton("../"._DIR_PLUGIN_BOUTIQUE."/img_pack/petite_caisse.png",_T('boutique:les_produits'));
+	$flux['naviguer']->sousmenu['cadis_page']= new Bouton("../"._DIR_PLUGIN_BOUTIQUE."/img_pack/panier_caddie.png",_T('boutique:les_cadis'));
+	$flux['naviguer']->sousmenu['categories_page']= new Bouton("../"._DIR_PLUGIN_BOUTIQUE."/img_pack/caissons_categorie.png",_T('boutique:les_categories'));
 	$flux['forum']->sousmenu['forums_boutique']= new Bouton("../"._DIR_PLUGIN_BOUTIQUE."/img_pack/internet-group-chat.png",_T('boutique:les_forum_de_boutique'));
 	$flux['auteurs']->sousmenu['clients_boutique']= new Bouton("../"._DIR_PLUGIN_BOUTIQUE."/img_pack/system-users.png",_T('boutique:les_clients'));
 	if ($GLOBALS['connect_statut'] == "0minirezo" && $GLOBALS["connect_toutes_rubriques"]){
