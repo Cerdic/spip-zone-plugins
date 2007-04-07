8e3ad711619fdac74705b6d0926459733ca1a3d3
renato@rezo.net <renato@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-04-07 21:52:21 +0000

    added the comment block options
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@11132 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/surligne_js/surlignejs_pipeline.php
+++ _dev_/surligne_js/surlignejs_pipeline.php
@@ -3,8 +3,14 @@
 function Surlignejs_insert_head($flux) {
     $flux .= "<script type='text/javascript' src='".find_in_path("javascript/SEhighlight.js")."'></script>\n";
     $flux .= "<script type='text/javascript'>
-    jQuery(function(){jQuery(document).SEhighlight({style_name:'spip_surligne',exact:false,style_name_suffix:false,engines:[/^".
-    str_replace(array("/","."),array("\/","\."),$GLOBALS['meta']['adresse_site'])."/i,/recherche=([^&]+)/i]})});
+    jQuery(function(){jQuery(document).SEhighlight({
+    style_name:'spip_surligne',
+    exact:false,
+    style_name_suffix:false,
+    engines:[/^".str_replace(array("/","."),array("\/","\."),$GLOBALS['meta']['adresse_site'])."/i,/recherche=([^&]+)/i],
+    startHighlightComment:'debut_surligneconditionnel',
+    stopHighlightComment:'finde_surligneconditionnel'
+    })});
     </script>
     ";
   return $flux;
