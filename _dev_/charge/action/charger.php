d3420ca5b9515ab5369f8ecb019d0209dd4626b8
bertrand@toggg.com <bertrand@toggg.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-04-09 19:25:18 +0000

    Changer les noms en charge(r)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@11173 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/charge/action/charger.php
+++ _dev_/charge/action/charger.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Plugin kitspip : action charger decompresser
+ * Plugin charge : action charger decompresser
  *
  * Auteur : bertrand@toggg.com
  * © 2007 - Distribue sous licence LGPL
@@ -8,43 +8,43 @@
  * Charger un zip, le decompresser et si plugin, l'activer.
  * Appels apres authentification: [url spip public]/...
  *	* charger un zip et le décompresser:
- *   ?action=charger_decompresser&charger=oui&paquet=[nom paquet sans .zip]
+ *   ?action=charger&zip=[nom paquet sans .zip]
  *
  *	* charger un plugin, le décompresser et l'activer
- *   ?action=charger_decompresser&charger=oui&plugin=[prefixe plugin]
- *  Si le nom du paquet differe du plugin, rajouter: &paquet=[nom paquet sans .zip]
+ *   ?action=charger&plugin=[prefixe plugin]
+ *  Si le nom du paquet differe du plugin, rajouter: &zip=[nom paquet sans .zip]
  *
  * Options supplementaires:
  *  &url_retour=[url retour], défaut ecrire/
  *  &depot=[url du depot des zip] defaut http://files.spip.org/spip-zone/
  *
  * On peut aussi simplement activer un plugin deja present par:
- *   ?action=charger_decompresser&activer_plugin=[prefixe plugin]
+ *   ?action=charger&activer=[prefixe plugin]
  *
  */
 
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
-function action_charger_decompresser()
+function action_charger()
 {
 	include_spip('inc/loader');
-	$url_retour = kit_request('url_retour', 'ecrire/');
-	if ($actplug = kit_request('activer_plugin')) {
+	$url_retour = charge_request('url_retour', 'ecrire/');
+	if ($actplug = charge_request('activer_plugin')) {
 		kit_activer_plugin($actplug);
 		redirige_par_entete($url_retour);
 	}
-	$plugin = kit_request('plugin');
-	$paquet = kit_request('paquet', $plugin);
-	$depot =  kit_request('depot', 'http://files.spip.org/spip-zone/');
-	$remove = kit_request('remove_path', 'spip');
-	$dest = kit_request('dest_path', _DIR_RACINE . ($plugin ? 'plugins/' : ''));
+	$plugin = charge_request('plugin');
+	$paquet = charge_request('paquet', $plugin);
+	$depot =  charge_request('depot', 'http://files.spip.org/spip-zone/');
+	$remove = charge_request('remove_path', 'spip');
+	$dest = charge_request('dest_path', _DIR_RACINE . ($plugin ? 'plugins/' : ''));
 
 	$status = kit_charger_zip($depot, $paquet, $remove, $dest, $plugin);
 
 	redirige_par_entete($url_retour);
 }
 
-function kit_request($var, $def = null)
+function charge_request($var, $def = null)
 {
 	is_null($ret = _request($var)) && ($ret = $def);
 	return $ret;
