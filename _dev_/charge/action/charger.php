f6b39a08a2e163e968095e0d0cb81145280f6c88
bertrand@toggg.com <bertrand@toggg.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-04-09 21:15:58 +0000

    Renommages
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@11176 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/charge/action/charger.php
+++ _dev_/charge/action/charger.php
@@ -8,11 +8,11 @@
  * Charger un zip, le decompresser et si plugin, l'activer.
  * Appels apres authentification: [url spip public]/...
  *	* charger un zip et le décompresser:
- *   ?action=charger&zip=[nom paquet sans .zip]
+ *   ?action=charger&zip=[nom zip sans .zip]
  *
  *	* charger un plugin, le décompresser et l'activer
  *   ?action=charger&plugin=[prefixe plugin]
- *  Si le nom du paquet differe du plugin, rajouter: &zip=[nom paquet sans .zip]
+ *  Si le nom du zip differe du plugin, rajouter: &zip=[nom zip sans .zip]
  *
  * Options supplementaires:
  *  &url_retour=[url retour], défaut ecrire/
@@ -27,26 +27,26 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 
 function action_charger()
 {
-	include_spip('inc/loader');
+	include_spip('inc/chargeur');
 	$url_retour = charge_request('url_retour', 'ecrire/');
-	if ($actplug = charge_request('activer_plugin')) {
+	if ($actplug = charge_request('activer')) {
 		kit_activer_plugin($actplug);
 		redirige_par_entete($url_retour);
 	}
 	$plugin = charge_request('plugin');
-	$paquet = charge_request('paquet', $plugin);
+	$zip = charge_request('zip', $plugin);
 	$depot =  charge_request('depot', 'http://files.spip.org/spip-zone/');
-	$remove = charge_request('remove_path', 'spip');
+	$remove = charge_request('remove_path', 'spip' . ($plugin ? '/plugins' : ''));
 	$dest = charge_request('dest_path', _DIR_RACINE . ($plugin ? 'plugins/' : ''));
 
-	$status = kit_charger_zip($depot, $paquet, $remove, $dest, $plugin);
+	$status = kit_charger_zip($depot, $zip, $remove, $dest, $plugin);
 
 	redirige_par_entete($url_retour);
 }
 
 function charge_request($var, $def = null)
 {
-	is_null($ret = _request($var)) && ($ret = $def);
+	($ret = _request($var)) || ($ret = $def);
 	return $ret;
 }
 
