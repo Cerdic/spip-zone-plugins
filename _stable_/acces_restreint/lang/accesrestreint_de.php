8db30952e4b327d7a0b25769e31eedb5228813cd
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-04-10 14:04:30 +0000

    "traduction allemande (joz)"
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@11188 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _stable_/acces_restreint/lang/accesrestreint_de.php
@@ -0,0 +1,35 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+'icone_menu_config' => 'eingeschr&#228;nkter Zugang',
+'page_zones_acces' => 'eingeschr&#228;nkter Zugang',
+'info_page' => "Hier k&#246;nnen Sie die zugangsbeschr&#228;nkten Bereiche Ihrer Webseite verwalten.",
+'titre_zones_acces' => "zugangsbeschr&#228;nkte Bereiche",
+'info_retirer_zone' => 'Bereich l&#246;schen',
+'info_retirer_zones' => 'alle Bereiche l&#246;schen',
+'selectionner_une_zone' => 'Bereich ausw&#228;hlen',
+'titre_ajouter_zone' => 'zum Bereich zuf&#034;gen',
+'icone_creer_zone' => 'neuen Bereich erstellen',
+'colonne_id' => 'Num',
+'titre' => 'Titel',
+'descriptif' => 'Beschreibung',
+'titre_table' => "alle zugangsbeschr&#228;nkten Bereiche",
+'bouton_creer_la_zone' => 'erstellen',
+'rubriques_zones_acces' => 'Rubriken des Bereiches',
+'rubriques' => 'Rubriken',
+'auteurs' => 'Autoren',
+'icone_supprimer_zone' => 'Bereich l&#246;schen',
+'ajouter_droits_auteur' => "mir Berechtigung f&#252;r diesen Bereich erteilen",
+
+'publique' => '&#246;ffentlich',
+'privee' => 'privat',
+'zone_restreinte_publique' => "Zugang auf der &#246;ffentlichen Webseite beschr&#228;nken",
+'zone_restreinte_espace_prive' => "Zugang im Redaktionsbereich beschr&#228;nken",
+
+);
+
+
+?>
