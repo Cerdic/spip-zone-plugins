82e2e375edf94cf09f4a20d20a4b84cbec117d28
gael@merzok.com <gael@merzok.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-04-11 15:37:20 +0000

    jolirecherche est maintenant valide XHTML 1.0 Transitional
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@11208 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/jolirecherche/recherche_public.php
+++ _stable_/jolirecherche/recherche_public.php
@@ -1,5 +1,5 @@
 <?php
 	function recherche_insert_head($flux) {
-		return $flux."<link rel=\"stylesheet\" type=\"text/css\" href=\"".direction_css(find_in_path("recherche.css"))."\" />\n<script src=\"".find_in_path("recherche.js")."\" type=\"text/javascript\" /></script>";
+		return $flux."<link rel=\"stylesheet\" type=\"text/css\" href=\"".direction_css(find_in_path("recherche.css"))."\" />\n<meta http-equiv=\"Content-Script-Type\" content=\"text/javascript\" />\n<script src=\"".find_in_path("recherche.js")."\" type=\"text/javascript\"></script>";
 	}
 ?>
