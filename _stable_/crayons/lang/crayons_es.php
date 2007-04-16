beb8ea504cdd7dba9ba8ab322e2da4d135d5567d
cmtmt2003@gmail.com <cmtmt2003@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-04-16 16:01:17 +0000

    Traduccion al español
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@11326 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _stable_/crayons/lang/crayons_es.php
@@ -0,0 +1,21 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	'annuler' => 'Cancelar',
+	'editer_@type@_@id@' => 'Editar @type@ @id@ completamente',
+    'deja_modifie' => 'Ya modificado',
+    'donnees_mal_formatees' => 'Datos fuera de formato',
+    'editer' => 'Modificar',
+    'editer_tout' => 'Modificar todo',
+    'modifie_par_ailleurs' => 'Modificado por otra persona',
+    'non_autorise' => 'No autorizado',
+    'non_implemente' => 'No implementado',
+    'pas_de_modification' => 'Sin modificaciones',
+    'pas_de_valeur' => 'Sin valor',
+    'sauvegarder' => '&iquest;Guardar las modificaciones?',
+    'svp_copier_coller' => 'Lo lamentamos copie/pegue los cambios y vuelva a comenzar',
+    'veuillez_patienter'=>'Por favor, espere...'
+    );
+?>
