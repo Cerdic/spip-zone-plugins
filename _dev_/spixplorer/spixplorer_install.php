8a786d805bb05ae2f05918a85271e487742fb754
bertrand@toggg.com <bertrand@toggg.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-04-18 08:51:03 +0000

    Tout redescendu d'un cran, tout renommer en spx_machin, langues en _T() et qq constantes ... R.I.P. ce script, je vais poser tout.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@11393 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/spixplorer/spixplorer_install.php
+++ _dev_/spixplorer/spixplorer_install.php
@@ -11,11 +11,11 @@
 // Ca va finir par une serie et notamment le mes_fichiers.zip du plugin mes_fichiers
 function spixplorer_install($action)
 {
-	$cible = dirname(__FILE__) . '/quixplorer';
+	$cible = dirname(__FILE__);
 //	spip_log('spixplorer: ' . $action . ' (' . $cible . ')');
 
 	if ($action == 'test') {
-		return is_dir($cible);
+		return is_readable($cible . '/quixplorer_index.php');
 	}
 
 	if ($action != 'install') {
@@ -49,36 +49,57 @@ function spixplorer_install($action)
 		'dest' => $cible,
 		'rename' => array(
 			'index.php' => 'quixplorer_index.php',
-			'_lib/lib_zip.php' => 'include/lib_zip.php',
-			'_lib' => 'action',
-//			'action/init.php' => 'inc/init.php'
-			'include/fun_admin.php' => 'action/spx_admin.php',
-			'include/fun_archive.php' => 'action/spx_archive.php',
-			'include/fun_chmod.php' => 'action/spx_chmod.php',
-			'include/fun_copy_move.php' => 'action/spx_copy_move.php',
-			'include/fun_del.php' => 'action/spx_del.php',
-			'include/fun_down.php' => 'action/spx_down.php',
-			'include/fun_edit.php' => 'action/spx_edit.php',
-			'include/fun_extra.php' => 'include/extra.php',
-			'include/fun_list.php' => 'action/spx_list.php',
-			'include/fun_mkitem.php' => 'action/spx_mkitem.php',
-			'include/fun_search.php' => 'action/spx_search.php',
-			'include/fun_up.php' => 'action/spx_up.php',
-			'include/fun_users.php' => 'action/spx_users.php',
 			'include' => 'inc',
-			'_lang' => 'lang'
+			'_lib/lib_zip.php' => 'inc/spx_lib_zip.php',
+			'inc/fun_extra.php' => 'inc/spx_extra.php',
+			'inc/error.php' => 'inc/spx_error.php',
+			'inc/footer.php' => 'inc/spx_footer.php',
+			'inc/header.php' => 'inc/spx_header.php',
+			'inc/init.php' => 'inc/spx_init.php',
+			'inc/javascript.php' => 'inc/spx_javascript.php',
+			'inc/js_admin2.php' => 'inc/spx_js_admin2.php',
+			'inc/js_admin3.php' => 'inc/spx_js_admin3.php',
+			'inc/js_admin.php' => 'inc/spx_js_admin.php',
+			'inc/login.php' => 'inc/spx_login.php',
+
+			'_lib' => 'action',
+			'inc/fun_admin.php' => 'action/spx_admin.php',
+			'inc/fun_archive.php' => 'action/spx_archive.php',
+			'inc/fun_chmod.php' => 'action/spx_chmod.php',
+			'inc/fun_copy_move.php' => 'action/spx_copy_move.php',
+			'inc/fun_del.php' => 'action/spx_del.php',
+			'inc/fun_down.php' => 'action/spx_down.php',
+			'inc/fun_edit.php' => 'action/spx_edit.php',
+			'inc/fun_list.php' => 'action/spx_list.php',
+			'inc/fun_mkitem.php' => 'action/spx_mkitem.php',
+			'inc/fun_search.php' => 'action/spx_search.php',
+			'inc/fun_up.php' => 'action/spx_up.php',
+			'inc/fun_users.php' => 'action/spx_users.php',
+			'_lang' => 'spx_lang',
+
+			'config/conf.php' => 'config/spx_conf.php',
+//			'config/.htaccess' => 'config/spx_htaccess',
+			'config/.htusers.php' => 'config/spx_htusers.php',
+			'config/mimes.php' => 'config/spx_mimes.php'
 		),
 		'edit' => array(
-			'#(?:require|include)[ (]+"\./\.include/fun_extra\.php"\)?#' => 'include_spip("quixplorer/inc/extra")',
+			'#(?:require|include)[ (]+"\./\.include/fun_extra\.php"\)?#' => 'include_spip("inc/spx_extra")',
 			'#(?:require|include)[ (]+"\./\.include/fun_(.*)\.php"\)?#' => 'include_spip("action/spx_$1")',
-			'#(?:require|include)[ (]+"\./\.include/(.*)\.php"\)?#' => 'include_spip("quixplorer/inc/$1")',
-			'#(?:require|include)[ (]+"\./_lib/(.*)\.php"\)?#' => 'include_spip("quixplorer/inc/$1")',
-			'#(?:require|include)[ (]+"\./_lang/(.*)\.php"\)?#' => 'include_spip("quixplorer/lang/$1")',
-			'#(?:require|include)[ (]+"\./\.config/(.*)\.php"\)?#' => 'include_spip("quixplorer/config/$1")',
-			"#isset\(\\\$GLOBALS\['__POST'\]\[([^\]]+)\]\)(.+?)\\\$GLOBALS\['__POST'\]\[\\1\]#"
-				=> '_request($1)',
+			'#(?:require|include)[ (]+"\./\.include/(.*)\.php"\)?#' => 'include_spip("inc/spx_$1")',
+			'#(?:require|include)[ (]+"\./_lib/(.*)\.php"\)?#' => 'include_spip("inc/spx_$1")',
+			'#(?:require|include)[ (]+"\./_lang/(.*)\.php"\)?#' => 'include_spip("spx_lang/$1")',
+			'#(?:require|include)[ (]+"\./\.config/(.*)\.php"\)?#' => 'include_spip("config/spx_$1")',
+			"#isset\(\\\$GLOBALS\['__POST'\]\[([^\]]+)\]\)((.+?=\s*)|[^\$]+?)\\\$GLOBALS\['__POST'\]\[\\1\]#"
+				=> '$3_request($1)',
+			'#\$GLOBALS\["(messages|error_msg)"\]\[([^\]]+)\]#' => '_T(\'spixplorer:$2\')',
+			'#\$GLOBALS\["date_fmt"\]#' =>  '_T(\'spixplorer:date_fmt\')',
+			'#\$GLOBALS\["text_dir"\]#' => '$GLOBALS[\'spip_lang_dir\']',
+
 			'#\$GLOBALS#' => "\$GLOBALS['spx']",
-			'#_img/#' => "plugins/spixplorer/quixplorer/_img/",
+			'#_img/#' => "plugins/spixplorer/_img/",
+			'#fopen\("\./\.config/\.htusers\.php"#'
+				=> 'fopen("plugins/spixplorer/config/spx_htusers.php"',
+			'#_style/#' => 'plugins/spixplorer/_style/',
 			'#.*have fun.*#i' => '$0
 
 	Adaptation spip, plugin spixplorer : bertrand@toggg.com © 2007
@@ -89,7 +110,7 @@ function spixplorer_install($action)
 	));
 	echo $statut . '<br />';
 
-	if (!is_dir($cible)) {
+	if (!is_readable($cible . '/quixplorer_index.php')) {
 		spip_log('spixplorer install: impossible de charger quixplorer');
 		return 0;
 	}
