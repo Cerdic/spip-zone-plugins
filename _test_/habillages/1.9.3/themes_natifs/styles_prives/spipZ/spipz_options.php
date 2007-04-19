5d93ef174abc1b5b0824bff7c87a73a8f359111c
franck.ducas@free.fr <franck.ducas@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-04-19 14:09:48 +0000

    On continue de poser des bases. Toujours pas fonctionnel.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@11477 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _test_/habillages/1.9.3/themes_natifs/styles_prives/spipZ/spipz_options.php
@@ -0,0 +1,6 @@
+<?php
+// Mettre une personnalisation du chemin via une meta dans le plugin
+// habillages. Si la meta n'existe pas, prendre le chemin de ce fichier 
+// comme valeur : ca permet de faire fonctionner ce theme en autonome.
+$GLOBALS['dossier_squelettes'] = 'plugins/habillages/themes_natifs/styles_prives/spipZ';
+?>
