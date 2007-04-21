74fcebe2093d9af4961edc10c28c29d2e251267f
horaciogd@vhplab.net <horaciogd@vhplab.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-04-21 23:22:27 +0000

    Some changes to add geolocalization to mots: - New table "spip_gis_mots" - New kind of map "mini_map"
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@11568 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/gis/inc/parte_privada.php
+++ _dev_/gis/inc/parte_privada.php
@@ -71,52 +71,58 @@ function gis_cambiar_coord($id_article) {
 	$s .= "\n<p>";
 	return $s;
 }
- 
+/*
 function gis_mot_groupe($id_groupe){
 	global $spip_lang_left, $spip_lang_right;
 	global $id_groupe;
-	$s .= "hola";
+	$s .= "hola gis mot groupe";
 	return $s;
-} 
-function gis_grupo_mots($id_groupe) {
+}*/
+function gis_mots($id_mot) {
 	global $spip_lang_left, $spip_lang_right;
-	global $id_groupe;
+	global $id_mot;
 	
+	$glat = NULL;
+	$glonx = NULL;
+	$result = spip_query("SELECT * FROM spip_gis WHERE id_article = " . intval($id_mot) . " AND type = 'mot'");
+	if ($row = spip_fetch_array($result)){
+		$glat = $row['lat'];
+		$glonx = $row['lonx'];
+	}
+	if(_request('actualizar')){
+		$glat = _request('lat');
+		$glonx = _request('lonx');
+		if (!$row)
+			spip_abstract_insert("spip_gis", "(id_article, type, lat, lonx)", "(" . _q($id_mot) .", 'mots', "._q($glat)." ,"._q($glonx).")");
+		else
+			spip_query("UPDATE spip_gis SET lat="._q($glat).", lonx="._q($glonx)."  WHERE id_article = " . _q($id_mot). " AND type = 'mot'");
+	}
 	$s .= "\n<p>";
 	$s .= debut_cadre('r', _DIR_PLUGIN_GIS."img_pack/correxir.png");
 	$s .= bouton_block_invisible("ajouter_form");
+	$s .= "&nbsp;&nbsp;&nbsp;<strong class='verdana3' style='text-transform: uppercase;'>"
+		._T("gis:cambiar")." ".$id_mot." <a onclick=\"$('#cadroFormulario').slideToggle('slow')\">("._T('gis:clic_desplegar').")</a></strong>";
+	$s .= "\n";
+			$s .= debut_block_visible("ajouter_form");
+	$s .= "<div class='verdana2'>";
+	$s .= _T("gis:clic_mapa");
+	$s .= "</div>";
+	
 	$s .= debut_block_visible("ajouter_form");
-	$s .= "hola ".$id_groupe;
 	
-	if(isset($_POST['actualizar'])){
-		if ((isset($_POST['grupo'])) && ($_POST['grupo']!=$_POST['exgrupo'])) {
-			if ($_POST['exgrupo']!=0) {
-				spip_query("DELETE FROM spip_gis_mots_groupes WHERE id_groupe = " . intval($id_groupe));
-			}
-			if ($_POST['grupo']!=0) {
-				spip_abstract_insert("spip_gis_mots_groupes", "(id_gis_groupe, id_groupe)", "( ".$_POST['grupo'].",".$id_groupe.")");
-			}			
-		}
-	}
-	$s .= '<form id="formulaire_gruposgis" name="formulaire_gruposgis" action="'.generer_url_ecrire(mots_type."&id_groupe=".$id_groupe).'" method="post">';
+	$gis_append_mini_map = charger_fonction('geomap_append_mini_map','inc');
 	
-	$grupo = 0;
-	$result = spip_query("SELECT * FROM spip_gis_mots_groupes WHERE id_groupe = " . intval($id_groupe));
-	if ($row = spip_fetch_array($result)){
-	 	$grupo = $row['id_gis_groupe'];
-	 	$s .= '<input type="radio" name="grupo" value="0" /> Sacar do grupo<br>';
-	}
+	$s .= "<div id='cadroFormulario' style='border:1px solid #000'>
+	<div id='formMap' name='formMap' style='width: 180px; height: 180px'></div>"
+	. $gis_append_mini_map('formMap','form_lat','form_long',$glat,$glonx,NULL,NULL,$row?true:false);
 	
-	$result= spip_query("SELECT * FROM spip_gis_groupes");
-	while($row = spip_fetch_array($result)) {
-		$s .= '<input type="radio" name="grupo" value="'.$row['id_groupe'];
-		if ($row['id_groupe']==$grupo) {
-			$s .= ' checked="checked"';
-		}
-		$s .= '" /> '.$row['titre']."<br>";
-	}
-	$s .= '<input type="hidden" name="exgrupo" value="'.$grupo.'" /><input type="submit" name="actualizar" value="actualizar" />
-		</form>';		
+	// Formulario para actualizar as coordenadas do mapa______________________.
+	$s .= '<form id="formulaire_coordenadas" name="formulaire_coordenadas" action="'.generer_url_ecrire(mots_edit."&id_mot=".$id_mot).'" method="post">
+		<input type="text" name="lat" id="form_lat" value="'.$glat.'" />
+		<input type="text" name="lonx" id="form_long" value="'.$glonx.'" />
+		<input type="submit" name="actualizar" value="'._T("gis:boton_actualizar").'" />
+		</form>
+		</div>';
 	$s .= debut_block_visible("ajouter_form");
 	$s .= fin_block();
 	$s .= fin_block();
