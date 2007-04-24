ab5b0c185e10d4be21208660022c00b6a71ae2f9
patfr@ifrance.com <patfr@ifrance.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-04-24 16:31:17 +0000

    amélioration jQuery de 'auteur_forum' et compatibilité Spip v1.9.1
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@11615 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/tweak_spip/tweaks/auteur_forum.php
+++ _stable_/tweak_spip/tweaks/auteur_forum.php
@@ -2,6 +2,7 @@
 
 function Auteur_forum_affichage_final($flux){
 	if(_request('page')=='forum') {
+		$form = $GLOBALS['spip_version_code']<1.92?"$('.previsu').parent()":"$('.previsu').parent().parent()";
 		#	include_spip('inc/charsets');
 		// filtrer et remettre le tout dans le charset cible
 		$nom = unicode2charset(html2unicode(_T('tweak:nom_forum')));
@@ -9,14 +10,18 @@ function Auteur_forum_affichage_final($flux){
 		$code =<<<jscode
 <script type="text/javascript"><!--
 $(document).ready(function(){
- if($('#formulaire_forum @confirmer_forum').length)
+ form = $form;
+ auteur = $('#formulaire_forum #auteur');
+// label = auteur.prev();
+ if(form.length && auteur.length)
  	// eviter les forums anonymes
-	$('#formulaire_forum').bind('submit', function(event){
-		 t = $('#formulaire_forum #auteur');
-		if(t.val().length==0) {
-			t.attr('style','border-color:#E86519;');
-			t.focus();
+	form.bind('submit', function(event){
+		if(auteur.val().length==0) {
 			alert($nom);
+			auteur.focus();
+			auteur.attr('style','border-color:#E86519;');
+//			label.attr('style','color:#E86519;');
+//			label.color("#E86519");
 			return false;
 		}
 	});
