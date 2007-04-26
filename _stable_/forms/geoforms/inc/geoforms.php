306ac9a279e840f9773f4cf1e25defba886fb1fc
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-04-26 13:48:53 +0000

    "fonction manquante"
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@11663 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/forms/geoforms/inc/geoforms.php
+++ _stable_/forms/geoforms/inc/geoforms.php
@@ -9,12 +9,40 @@
  *
  */
 
+
 function geoforms_latitude_longitude($x,$y,$systeme){
 	if (strlen($systeme)){
 		include_spip('inc/geoforms_projections');
 		list($x,$y) = geoforms_systeme_vers_lat_lont($x,$y,$systeme);
 	}
 	return array($x,$y);
+}
+
+function geoforms_donnee_icone($id_form,$id_donnee){
+	return url_absolue(find_in_path('img_pack/correxir.png'));
+}
+
+function geoforms_donnee_latitude($id_form,$id_donnee){
+	$t = geoforms_donnee_latitude_longitude($id_form,$id_donnee);
+	return reset($t);
+}
+function geoforms_donnee_longitude($id_form,$id_donnee){
+	$t = geoforms_donnee_latitude_longitude($id_form,$id_donnee);
+	return end($t);
+}
+function geoforms_donnee_latitude_longitude($id_form,$id_donnee){
+	static $buf = array();
+	if (!isset($buf[$id_donnee])){
+		$res = spip_query("SELECT c.*,dc.*
+		FROM spip_forms_champs as c, spip_forms_donnees_champs as dc 
+		WHERE c.type IN ('geox','geoy') AND c.id_form="._q($id_form)." AND dc.champ=c.champ AND dc.id_donnee="._q($id_donnee)." ORDER BY c.rang");
+		while ($row = spip_fetch_array($res) AND (!isset($buf[$id_donnee]['geox']) OR !isset($buf[$id_donnee]['geoy']))){
+			$buf[$id_donnee][$row['type']] = array('value'=>$row['valeur'],'syst'=>$row['extra_info']);
+		}
+		list($buf[$id_donnee]['geox'],$buf[$id_donnee]['geoy']) = geoforms_latitude_longitude($buf[$id_donnee]['geox']['value'],$buf[$id_donnee]['geoy']['value'],$buf[$id_donnee]['geox']['syst']);
+	}
+	if (!isset($buf[$id_donnee])) return array(0,0);
+	return array($buf[$id_donnee]['geox'],$buf[$id_donnee]['geoy']);
 }
 
 ?>
--- _stable_/forms/geoforms/public/geoforms_balises.php
+++ _stable_/forms/geoforms/public/geoforms_balises.php
@@ -35,31 +35,4 @@ function balise_GEOICONE_dist($p){
 	return $p;
 }
 
-function geoforms_donnee_icone($id_form,$id_donnee){
-	return url_absolue(find_in_path('img_pack/correxir.png'));
-}
-
-function geoforms_donnee_latitude($id_form,$id_donnee){
-	$t = geoforms_donnee_latitude_longitude($id_form,$id_donnee);
-	return reset($t);
-}
-function geoforms_donnee_longitude($id_form,$id_donnee){
-	$t = geoforms_donnee_latitude_longitude($id_form,$id_donnee);
-	return end($t);
-}
-function geoforms_donnee_latitude_longitude($id_form,$id_donnee){
-	static $buf = array();
-	if (!isset($buf[$id_donnee])){
-		$res = spip_query("SELECT c.*,dc.*
-		FROM spip_forms_champs as c, spip_forms_donnees_champs as dc 
-		WHERE c.type IN ('geox','geoy') AND c.id_form="._q($id_form)." AND dc.champ=c.champ AND dc.id_donnee="._q($id_donnee)." ORDER BY c.rang");
-		while ($row = spip_fetch_array($res) AND (!isset($buf[$id_donnee]['geox']) OR !isset($buf[$id_donnee]['geoy']))){
-			$buf[$id_donnee][$row['type']] = array('value'=>$row['valeur'],'syst'=>$row['extra_info']);
-		}
-		list($buf[$id_donnee]['geox'],$buf[$id_donnee]['geoy']) = geoforms_latitude_longitude($buf[$id_donnee]['geox']['value'],$buf[$id_donnee]['geoy']['value'],$buf[$id_donnee]['geox']['syst']);
-	}
-	if (!isset($buf[$id_donnee])) return array(0,0);
-	return array($buf[$id_donnee]['geox'],$buf[$id_donnee]['geoy']);
-}
-
 ?>
