9df030850569b83ab4e505d984ab3253c81feec1
joseph@larmarange.net <joseph@larmarange.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-04-26 23:38:56 +0000

    Mise en place des fichiers du compilateur. la balise inclure_noisette fonctionne pour le moment comme inclure.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@11686 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/noisetier/noisetier_mes_options.php
+++ _dev_/noisetier/noisetier_mes_options.php
@@ -1,4 +1,31 @@
 <?php
 include_spip('base/noisetier');
 
+function balise_INCLURE_NOISETTE($p) {
+	$champ = phraser_arguments_inclure($p, true);
+	$_contexte = argumenter_inclure($champ, $p->descr, $p->boucles, $p->id_boucle, false);
+
+	if (isset($_contexte['fond'])) {
+		// Critere d'inclusion {env} (et {self} pour compatibilite ascendante)
+		if (isset($_contexte['env'])
+		|| isset($_contexte['self'])
+		) {
+			$flag_env = true;
+			unset($_contexte['env']);
+		}
+		$l = 'array(' . join(",\n\t", $_contexte) .')';
+		if ($flag_env) {
+			$l = "array_merge(\$Pile[0],$l)";
+		}
+		$p->code = "recuperer_fond('',".$l.",true, false)";
+	} else {
+		$n = interprete_argument_balise(1,$p);
+		$p->code = '(($c = find_in_path(' . $n . ')) ? spip_file_get_contents($c) : "")';
+	}
+
+
+	$p->interdire_scripts = false; // la securite est assuree par recuperer_fond
+	return $p;
+}
+
 ?>
