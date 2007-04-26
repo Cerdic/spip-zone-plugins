3c6a6aa06f057bcb6d90b133899b0777f641f021
joseph@larmarange.net <joseph@larmarange.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-04-26 21:22:13 +0000

    Installation des tables SQL
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@11680 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _dev_/noisetier/inc/noisetier_install.php
@@ -0,0 +1,20 @@
+<?php
+
+	include_spip('inc/meta');
+	function noisetier_upgrade($nom_meta_base_version,$version_cible){
+		$current_version = 0.0;
+		if (   (!isset($GLOBALS['meta'][$nom_meta_base_version]) )
+				|| (($current_version = $GLOBALS['meta'][$nom_meta_base_version])!=$version_cible)){
+			include_spip('base/noisetier');
+			if ($current_version<0.1){
+				include_spip('base/create');
+				include_spip('base/abstract_sql');
+				creer_base();
+				echo "Noisetier install&eacute;<br/>";
+				ecrire_meta($nom_meta_base_version,$current_version=$version_cible,'non');
+			}
+			ecrire_metas();
+		}
+	}
+
+?>
---
+++ _dev_/noisetier/noisetier_mes_options.php
@@ -0,0 +1,4 @@
+<?php
+include_spip('base/noisetier');
+
+?>
