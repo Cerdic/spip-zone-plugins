a441a429c3ad05729ded9ae9fff2287df76a05c1
fil@rezo.net <fil@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-04-30 21:53:53 +0000

    et hop, on definit d'un clic un secteur qui sera wiki, ainsi que ceux qui pourront le modifier
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@11760 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/autorite/inc/autoriser.php
+++ _dev_/autorite/inc/autoriser.php
@@ -25,13 +25,38 @@ AND !function_exists('autoriser')) {
 // Charger les versions *_dist des fonctions
 include _DIR_RESTREINT.'inc/autoriser.php';
 
-// Dire aux crayons si les visiteurs ont des droits
+
+// Dire aux crayons si les visiteurs anonymes ont des droits
 if (!function_exists('analyse_droits_rapide')) {
 function analyse_droits_rapide() {
+	if ($GLOBALS['autorite']['espace_wiki']
+	AND $GLOBALS['autorite']['espace_wiki_anonyme'])
+		return true;
+
 	return false;
 }
 }
 
+
+// une fonction qui gere les droits wiki
+if ($GLOBALS['autorite']['espace_wiki']
+AND !function_exists('autorisation_wiki_visiteur')) {
+	function autorisation_wiki_visiteur($qui) {
+		switch($qui['statut']) {
+			case '0minirezo':
+			case '1comite':
+				if ($GLOBALS['autorite']['espace_wiki_redacteurs'])
+					return true;
+			case '6forum':
+				if ($GLOBALS['autorite']['espace_wiki_visiteurs'])
+					return true;
+			default:
+				if ($GLOBALS['autorite']['espace_wiki_anonyme'])
+					return true;
+		}
+		return false;
+	}
+}
 
 
 //
@@ -42,22 +67,56 @@ function analyse_droits_rapide() {
 ## autoriser_article_modifier
 ##
 if ($GLOBALS['autorite']['auteur_mod_article']
+OR $GLOBALS['autorite']['espace_wiki']
 OR false // autre possibilite de surcharge ?
 ) {
 function autoriser_article_modifier_super($faire, $type, $id, $qui, $opt) {
 	$s = spip_query(
-	"SELECT id_rubrique,statut FROM spip_articles WHERE id_article="._q($id));
+	"SELECT id_rubrique,id_secteur,statut FROM spip_articles WHERE id_article="._q($id));
 	$r = spip_fetch_array($s);
 	include_spip('inc/auth');
 	return
 		autoriser('publierdans', 'rubrique', $r['id_rubrique'], $qui, $opt)
 		OR (
+			// Cas du wiki, on appelle la fonction qui verifie les droits wiki
+			$r['id_secteur'] == $GLOBALS['autorite']['espace_wiki']
+			AND autorisation_wiki_visiteur($qui)
+		)
+		OR (
 			in_array($qui['statut'], array('0minirezo', '1comite'))
-/*
-			# on commente cette ligne : tous les articles sont modifiables
-			AND in_array($r['statut'], array('prop','prepa', 'poubelle'))
-*/			AND spip_num_rows(auteurs_article($id, "id_auteur=".$qui['id_auteur']))
+
+			# si on commente cette ligne : tous les articles sont modifiables
+			AND (
+				$GLOBALS['autorite']['auteur_mod_article']
+				OR in_array($r['statut'], array('prop','prepa', 'poubelle'))
+			)
+			AND spip_num_rows(auteurs_article($id, "id_auteur=".$qui['id_auteur']))
 		);
+}
+}
+
+##
+## autoriser_rubrique_publierdans
+##
+if ($GLOBALS['autorite']['espace_wiki']
+OR false // autre possibilite de surcharge ?
+) {
+function autoriser_rubrique_publierdans_super($faire, $type, $id, $qui, $opt) {
+	// Si on est deja autorise en standard, dire 'OK'
+	if (autoriser_rubrique_publierdans_dist($faire, $type, $id, $qui, $opt))
+		return true;
+
+	// Sinon, verifier si la rubrique est wiki
+	// et si on est bien redacteur
+	$s = spip_query(
+	"SELECT id_secteur FROM spip_rubriques WHERE id_rubrique="._q($id));
+	$r = spip_fetch_array($s);
+	if ($r['id_secteur'] == $GLOBALS['autorite']['espace_wiki']
+	AND autorisation_wiki_visiteur($qui))
+		return true;
+
+	// par defaut, NIET
+	return false;
 }
 }
 
