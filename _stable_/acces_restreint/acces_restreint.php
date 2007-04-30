05cc62fd7be5fd54ce437637e0669d0e703bd1c8
fil@rezo.net <fil@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-04-30 13:15:26 +0000

    des requetes plus ciblees sur id_rubrique permettent d'eviter de faire trop de calculs tres lents + simplification du code
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@11747 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/acces_restreint/acces_restreint.php
+++ _stable_/acces_restreint/acces_restreint.php
@@ -12,42 +12,16 @@ function critere_tout_voir_dist($idb, &$boucles, $crit) {
 	$boucle->modificateur['tout_voir'] = true;
 }
 
-/*function critere_archive_dist($idb, &$boucles, $crit) {
-	$boucle = &$boucles[$idb];
-	$boucle->modificateur['archive'] = true;
-}*/
-
 //
 // <BOUCLE(ARTICLES)>
 //
 function boucle_ARTICLES($id_boucle, &$boucles) {
 	$boucle = &$boucles[$id_boucle];
-	$id_table = $boucle->id_table;
-
 	if (!isset($boucle->modificateur['tout_voir'])){
-		$t = $boucle->id_table . '.' . $boucle->primary;
-		if (!in_array($t, $boucles[$id_boucle]->select))
-		  $boucle->select[]= $t; # pour postgres, neuneu ici
-	
-		$boucle->hash = '
-		// ACCES RESTREINT
-		$acces_where = AccesRestreint_articles_accessibles_where("'.$t.'");
-		' . $boucle->hash ;
-	
-		// et le filtrage d'acces filtre !
-		$boucle->where[] = '$acces_where';
+		$t = $boucle->id_table . '.id_rubrique';
+		$boucle->select = array_merge($boucle->select, array($t)); // pour postgres
+		$boucle->where[] = 'AccesRestreint_rubriques_accessibles_where("'.$t.'")';
 	}
-	/*
-	$marchive = $id_table .'.archive';
-	if (!$boucle->modificateur['criteres']['archive']) {
-		if (!$GLOBALS['var_preview']) {
-			$boucle->where[]= array("'='", "'$marchive'", "'\"non\"'");
-			//$boucle->where[]= array("'>'", "'$id_table" . ".date_archive'", "'NOW()'");
-			$boucle->where[]= array("'($id_table.date_archive > NOW() OR $id_table.date_archive=0)'");
-		}
-	}
-	*/
-
 	return boucle_ARTICLES_dist($id_boucle, $boucles);
 }
 
@@ -56,22 +30,11 @@ function boucle_ARTICLES($id_boucle, &$boucles) {
 //
 function boucle_BREVES($id_boucle, &$boucles) {
 	$boucle = &$boucles[$id_boucle];
-	$id_table = $boucle->id_table;
-
 	if (!isset($boucle->modificateur['tout_voir'])){
-		$t = $boucle->id_table . '.' . $boucle->primary;
-		if (!in_array($t, $boucles[$id_boucle]->select))
-		  $boucle->select[]= $t; # pour postgres, neuneu ici
-	
-		$boucle->hash = '
-		// ACCES RESTREINT
-		$acces_where = AccesRestreint_breves_accessibles_where("'.$t.'");
-		' . $boucle->hash ;
-	
-		// et le filtrage d'acces filtre !
-		$boucle->where[] = '$acces_where';
+		$t = $boucle->id_table . '.id_rubrique';
+		$boucle->select = array_merge($boucle->select, array($t)); // pour postgres
+		$boucle->where[] = 'AccesRestreint_rubriques_accessibles_where("'.$t.'")';
 	}
-	
 	return boucle_BREVES_dist($id_boucle, $boucles);
 }
 
@@ -80,19 +43,18 @@ function boucle_BREVES($id_boucle, &$boucles) {
 //
 function boucle_FORUMS($id_boucle, &$boucles) {
 	$boucle = &$boucles[$id_boucle];
-	$id_table = $boucle->id_table;
 	if (!isset($boucle->modificateur['tout_voir'])){
-		$t = $boucle->id_table . '.' . $boucle->primary;
-		if (!in_array($t, $boucles[$id_boucle]->select))
-		  $boucle->select[]= $t; # pour postgres, neuneu ici
-	
-		$boucle->hash = '
-		// ACCES RESTREINT
-		$acces_where = AccesRestreint_forum_accessibles_where("'.$t.'");
-		' . $boucle->hash ;
-	
-		// et le filtrage d'acces filtre !
-		$boucle->where[] = '$acces_where';
+		$t = $boucle->id_table . '.id_rubrique';
+		$boucle->select = array_merge($boucle->select, array($t)); // pour postgres
+		$boucle->where[] = 'AccesRestreint_rubriques_accessibles_where("'.$t.'")';
+
+		$t = $boucle->id_table . '.id_article';
+		$boucle->select = array_merge($boucle->select, array($t));
+		$boucle->where[] = 'AccesRestreint_articles_accessibles_where("'.$t.'")';
+
+		$t = $boucle->id_table . '.id_breve';
+		$boucle->select = array_merge($boucle->select, array($t));
+		$boucle->where[] = 'AccesRestreint_breves_accessibles_where("'.$t.'")';
 	}
 	return boucle_FORUMS_dist($id_boucle, $boucles);
 }
@@ -102,41 +64,23 @@ function boucle_FORUMS($id_boucle, &$boucles) {
 //
 function boucle_SIGNATURES($id_boucle, &$boucles) {
 	$boucle = &$boucles[$id_boucle];
-	$id_table = $boucle->id_table;
-	if (!isset($boucle->modificateur['tout_voir'])){
-		$t = $boucle->id_table . '.' . $boucle->primary;
-		if (!in_array($t, $boucles[$id_boucle]->select))
-		  $boucle->select[]= $t; # pour postgres, neuneu ici
-	
-		$boucle->hash = '
-		// ACCES RESTREINT
-		$acces_where = AccesRestreint_signatures_accessibles_where("'.$t.'");
-		' . $boucle->hash ;
-	
-		// et le filtrage d'acces filtre !
-		$boucle->where[] = '$acces_where';
+	if (!isset($boucle->modificateur['tout_voir'])) {
+		$t = $boucle->id_table . '.id_article';
+		$boucle->select = array_merge($boucle->select, array($t));
+		$boucle->where[] = 'AccesRestreint_articles_accessibles_where("'.$t.'")';
 	}
 	return boucle_SIGNATURES_dist($id_boucle, $boucles);
 }
 
 //
-// <BOUCLE(DOCUMENTS)>
+// <BOUCLE(DOCUMENTS)> ; ici la notion d'acces est tres discutable
 //
 function boucle_DOCUMENTS($id_boucle, &$boucles) {
 	$boucle = &$boucles[$id_boucle];
-	$id_table = $boucle->id_table;
 	if (!isset($boucle->modificateur['tout_voir'])){
 		$t = $boucle->id_table . '.' . $boucle->primary;
-		if (!in_array($t, $boucles[$id_boucle]->select))
-		  $boucle->select[]= $t; # pour postgres, neuneu ici
-	
-		$boucle->hash = '
-		// ACCES RESTREINT
-		$acces_where = AccesRestreint_documents_accessibles_where("'.$t.'");
-		' . $boucle->hash ;
-	
-		// et le filtrage d'acces filtre !
-		$boucle->where[] = '$acces_where';
+		$boucle->select = array_merge($boucle->select, array($t));
+		$boucle->where[] = 'AccesRestreint_documents_accessibles_where("'.$t.'")';
 	}
 	return boucle_DOCUMENTS_dist($id_boucle, $boucles);
 }
@@ -146,18 +90,10 @@ function boucle_DOCUMENTS($id_boucle, &$boucles) {
 //
 function boucle_RUBRIQUES($id_boucle, &$boucles) {
 	$boucle = &$boucles[$id_boucle];
-	$id_table = $boucle->id_table;
 	if (!isset($boucle->modificateur['tout_voir'])){
 		$t = $boucle->id_table . '.' . $boucle->primary;
-		if (!in_array($t, $boucles[$id_boucle]->select))
-		  $boucle->select[]= $t; # pour postgres, neuneu ici
-		$boucle->hash = '
-		// ACCES RESTREINT
-		$acces_where = AccesRestreint_rubriques_accessibles_where("'.$t.'");
-		' . $boucle->hash ;
-	
-		// et le filtrage d'acces filtre !
-		$boucle->where[] = '$acces_where';
+		$boucle->select = array_merge($boucle->select, array($t));
+		$boucle->where[] = 'AccesRestreint_rubriques_accessibles_where("'.$t.'")';
 	}
 	return boucle_RUBRIQUES_dist($id_boucle, $boucles);
 }
@@ -167,19 +103,10 @@ function boucle_RUBRIQUES($id_boucle, &$boucles) {
 //
 function boucle_HIERARCHIE($id_boucle, &$boucles) {
 	$boucle = &$boucles[$id_boucle];
-	$id_table = $boucle->id_table;
 	if (!isset($boucle->modificateur['tout_voir'])){
 		$t = $boucle->id_table . '.' . $boucle->primary;
-		if (!in_array($t, $boucles[$id_boucle]->select))
-		  $boucle->select[]= $t; # pour postgres, neuneu ici
-	
-		$boucle->hash = '
-		// ACCES RESTREINT
-		$acces_where = AccesRestreint_rubriques_accessibles_where("'.$t.'");
-		' . $boucle->hash ;
-	
-		// et le filtrage d'acces filtre !
-		$boucle->where[] = '$acces_where';
+		$boucle->select = array_merge($boucle->select, array($t));
+		$boucle->where[] = 'AccesRestreint_rubriques_accessibles_where("'.$t.'")';
 	}
 	return boucle_HIERARCHIE_dist($id_boucle, $boucles);
 }
@@ -189,19 +116,10 @@ function boucle_HIERARCHIE($id_boucle, &$boucles) {
 //
 function boucle_SYNDICATION($id_boucle, &$boucles) {
 	$boucle = &$boucles[$id_boucle];
-	$id_table = $boucle->id_table;
 	if (!isset($boucle->modificateur['tout_voir'])){
-		$t = $boucle->id_table . '.' . $boucle->primary;
-		if (!in_array($t, $boucles[$id_boucle]->select))
-		  $boucle->select[]= $t; # pour postgres, neuneu ici
-	
-		$boucle->hash = '
-		// ACCES RESTREINT
-		$acces_where = AccesRestreint_syndic_accessibles_where("'.$t.'");
-		' . $boucle->hash ;
-	
-		// et le filtrage d'acces filtre !
-		$boucle->where[] = '$acces_where';
+		$t = $boucle->id_table . '.id_rubrique';
+		$boucle->select = array_merge($boucle->select, array($t));
+		$boucle->where[] = 'AccesRestreint_rubriques_accessibles_where("'.$t.'")';
 	}
 	return boucle_SYNDICATION_dist($id_boucle, $boucles);
 }
@@ -211,19 +129,10 @@ function boucle_SYNDICATION($id_boucle, &$boucles) {
 //
 function boucle_SYNDIC_ARTICLES($id_boucle, &$boucles) {
 	$boucle = &$boucles[$id_boucle];
-	$id_table = $boucle->id_table;
 	if (!isset($boucle->modificateur['tout_voir'])){
 		$t = $boucle->id_table . '.' . $boucle->primary;
-		if (!in_array($t, $boucles[$id_boucle]->select))
-		  $boucle->select[]= $t; # pour postgres, neuneu ici
-	
-		$boucle->hash = '
-		// ACCES RESTREINT
-		$acces_where = AccesRestreint_syndic_articles_accessibles_where("'.$t.'");
-		' . $boucle->hash ;
-	
-		// et le filtrage d'acces filtre !
-		$boucle->where[] = '$acces_where';
+		$boucle->select = array_merge($boucle->select, array($t));
+		$boucle->where[] = 'AccesRestreint_syndic_articles_accessibles_where("'.$t.'")';
 	}
 	return boucle_SYNDIC_ARTICLES_dist($id_boucle, $boucles);
 }
@@ -233,22 +142,11 @@ function boucle_SYNDIC_ARTICLES($id_boucle, &$boucles) {
 //
 function boucle_EVENEMENTS($id_boucle, &$boucles) {
 	$boucle = &$boucles[$id_boucle];
-	$id_table = $boucle->id_table;
-
 	if (!isset($boucle->modificateur['tout_voir'])){
 		$t = $boucle->id_table . '.' . $boucle->primary;
-		if (!in_array($t, $boucles[$id_boucle]->select))
-		  $boucle->select[]= $t; # pour postgres, neuneu ici
-	
-		$boucle->hash = '
-		// ACCES RESTREINT
-		$acces_where = AccesRestreint_evenements_accessibles_where("'.$t.'");
-		' . $boucle->hash ;
-	
-		// et le filtrage d'acces filtre !
-		$boucle->where[] = '$acces_where';
+		$boucle->select = array_merge($boucle->select, array($t));
+		$boucle->where[] = 'AccesRestreint_evenements_accessibles_where("'.$t.'")';
 	}
-	
 	return boucle_EVENEMENTS_dist($id_boucle, $boucles);
 }
 
--- _stable_/acces_restreint/acces_restreint_options.php
+++ _stable_/acces_restreint/acces_restreint_options.php
@@ -8,11 +8,10 @@ include_spip('base/acces_restreint');
 include_spip('inc/acces_restreint');
 
 
-// Pipeline : calculer les zones autorisees, sous la forme '1-2-3'
+// Pipeline : calculer les zones autorisees, sous la forme '1,2,3'
 // TODO : avec un petit cache pour eviter de solliciter la base de donnees
 $GLOBALS['AccesRestreint_zones_autorisees'] =
 	pipeline('AccesRestreint_liste_zones_autorisees', '');
-
 
 // Ajouter un marqueur de cache pour le differencier selon les autorisations
 if (!isset($GLOBALS['marqueur'])) $GLOBALS['marqueur'] = '';
@@ -20,57 +19,59 @@ $GLOBALS['marqueur'] .= ":AccesRestreint_zones_autorisees="
 	.$GLOBALS['AccesRestreint_zones_autorisees'];
 
 
-// Etablir la liste des rubriques interdites a ce visiteur
-// TODO : avec un petit cache pour eviter de solliciter la base de donnees
-$GLOBALS['AccesRestreint_rubriques_exclues'] =
-	AccesRestreint_liste_rubriques_exclues(_DIR_RESTREINT!="");
-
-
 //
 // Autorisations
 //
 
-// Voir une rubrique
+if(!function_exists('autoriser_rubrique_voir')) {
 function autoriser_rubrique_voir($faire, $type, $id, $qui, $opt) {
-	static $rub_exclues=NULL;
-	if ($rub_exclues===NULL){
-		$rub_exclues = array_flip($GLOBALS['AccesRestreint_rubriques_exclues']);
+	static $rub_exclues;
+	if (!isset($rub_exclues)) {
+		$rub_exclues = AccesRestreint_liste_rubriques_exclues(_DIR_RESTREINT!="");
+		$rub_exclues = array_flip($rub_exclues);
 	}
-	
 	return !isset($rub_exclues[$id]);
 }
+}
+if(!function_exists('autoriser_article_voir')) {
 function autoriser_article_voir($faire, $type, $id, $qui, $opt) {
-	static $art_exclus=NULL;
-	if ($art_exclus===NULL){
+	static $art_exclus;
+	if (!isset($art_exclus)) {
 		$art_exclus = AccesRestreint_liste_articles_exclus(_DIR_RESTREINT!="");
 		$art_exclus = array_flip($art_exclus);
 	}
 	return !isset($art_exclus[$id]);
 }
+}
+if(!function_exists('autoriser_breve_voir')) {
 function autoriser_breve_voir($faire, $type, $id, $qui, $opt) {
-	static $breves_exclus=NULL;
-	if ($breves_exclus===NULL){
+	static $breves_exclus;
+	if (!isset($breves_exclus)) {
 		$breves_exclus = AccesRestreint_liste_breves_exclues(_DIR_RESTREINT!="");
 		$breves_exclus = array_flip($breves_exclus);
 	}
 	return !isset($breves_exclus[$id]);
 }
+}
+if(!function_exists('autoriser_site_voir')) {
 function autoriser_site_voir($faire, $type, $id, $qui, $opt) {
-	static $sites_exclus=NULL;
-	if ($sites_exclus===NULL){
+	static $sites_exclus;
+	if (!isset($sites_exclus)) {
 		$sites_exclus = AccesRestreint_liste_syndic_exclus(_DIR_RESTREINT!="");
 		$sites_exclus = array_flip($sites_exclus);
 	}
 	return !isset($sites_exclus[$id]);
 }
-
+}
+if(!function_exists('autoriser_evenement_voir')) {
 function autoriser_evenement_voir($faire, $type, $id, $qui, $opt) {
-	static $evenements_exclus=NULL;
-	if ($evenements_exclus===NULL){
+	static $evenements_exclus;
+	if (!isset($evenements_exclus)) {
 		$evenements_exclus = AccesRestreint_liste_evenements_exclus(_DIR_RESTREINT!="");
 		$evenements_exclus = array_flip($evenements_exclus);
 	}
 	return !isset($evenements_exclus[$id]);
+}
 }
 
 ?>
