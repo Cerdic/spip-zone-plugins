9de7bcf700cbd2b9816bceacb8fabd5edf525662
fil@rezo.net <fil@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-05-01 08:08:34 +0000

    grosse simplification (on ne forke plus), et message d'erreur bien propre dans le cas ou les fonctions d'autorite sont deja definies par ailleurs (dans mes_options par exemple)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@11768 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/autorite/inc/autoriser.php
+++ _dev_/autorite/inc/autoriser.php
@@ -11,39 +11,33 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 define ('_DEBUG_AUTORISER', false);
 
 $GLOBALS['autorite'] = @unserialize($GLOBALS['meta']['autorite']);
+$autorite_erreurs = array();
 
-// Si des surcharges d'autorisation ont ete selectionnees,
-// on utilise la surcharge de autoriser() pour appeler autoriser_super()
-// http://doc.spip.org/@autoriser
-if (is_array($GLOBALS['autorite'])
-AND count($GLOBALS['autorite'])
-AND !function_exists('autoriser')) {
-// http://doc.spip.org/@autoriser
-	function autoriser() {
-		$args = func_get_args(); 
-		return call_user_func_array('autoriser_super', $args);
-	}
-}
 
 // Charger les versions *_dist des fonctions
 include _DIR_RESTREINT.'inc/autoriser.php';
 
 
-// Dire aux crayons si les visiteurs anonymes ont des droits
-if (!function_exists('analyse_droits_rapide')) {
-function analyse_droits_rapide() {
-	if ($GLOBALS['autorite']['espace_wiki']
-	AND $GLOBALS['autorite']['espace_wiki_anonyme'])
-		return true;
-
-	return false;
-}
-}
-
-
-// une fonction qui gere les droits wiki
+##
+## Dire aux crayons si les visiteurs anonymes ont des droits
+##
 if ($GLOBALS['autorite']['espace_wiki']
-AND !function_exists('autorisation_wiki_visiteur')) {
+AND $GLOBALS['autorite']['espace_wiki_anonyme']) {
+	if (!function_exists('analyse_droits_rapide')) {
+	function analyse_droits_rapide() {
+		return true;
+	}
+	}
+	else
+		$autorite_erreurs[] = 'analyse_droits_rapide';
+}
+
+
+##
+## une fonction qui gere les droits wiki
+##
+if ($GLOBALS['autorite']['espace_wiki']) {
+	if (!function_exists('autorisation_wiki_visiteur')) {
 	function autorisation_wiki_visiteur($qui) {
 		switch($qui['statut']) {
 			case '0minirezo':
@@ -59,12 +53,10 @@ AND !function_exists('autorisation_wiki_visiteur')) {
 		}
 		return false;
 	}
+	} else
+		$autorite_erreurs[] = 'autorisation_wiki_visiteur';
 }
 
-
-//
-// Surcharger les autorisations qui doivent l'etre
-//
 
 ##
 ## autoriser_article_modifier
@@ -73,7 +65,8 @@ if ($GLOBALS['autorite']['auteur_mod_article']
 OR $GLOBALS['autorite']['espace_wiki']
 OR false // autre possibilite de surcharge ?
 ) {
-function autoriser_article_modifier_super($faire, $type, $id, $qui, $opt) {
+if (!function_exists('autoriser_article_modifier')) {
+function autoriser_article_modifier($faire, $type, $id, $qui, $opt) {
 	$s = spip_query(
 	"SELECT id_rubrique,id_secteur,statut FROM spip_articles WHERE id_article="._q($id));
 	$r = spip_fetch_array($s);
@@ -96,7 +89,10 @@ function autoriser_article_modifier_super($faire, $type, $id, $qui, $opt) {
 			AND spip_num_rows(auteurs_article($id, "id_auteur=".$qui['id_auteur']))
 		);
 }
+} else
+	$autorite_erreurs[] = 'autoriser_article_modifier';
 }
+
 
 ##
 ## autoriser_rubrique_publierdans
@@ -104,7 +100,8 @@ function autoriser_article_modifier_super($faire, $type, $id, $qui, $opt) {
 if ($GLOBALS['autorite']['espace_wiki']
 OR false // autre possibilite de surcharge ?
 ) {
-function autoriser_rubrique_publierdans_super($faire, $type, $id, $qui, $opt) {
+if (!function_exists('autoriser_rubrique_publierdans')) {
+function autoriser_rubrique_publierdans($faire, $type, $id, $qui, $opt) {
 	// Si on est deja autorise en standard, dire 'OK'
 	if (autoriser_rubrique_publierdans_dist($faire, $type, $id, $qui, $opt))
 		return true;
@@ -121,6 +118,8 @@ function autoriser_rubrique_publierdans_super($faire, $type, $id, $qui, $opt) {
 	// par defaut, NIET
 	return false;
 }
+} else
+	$autorite_erreurs[] = 'autoriser_rubrique_publierdans';
 }
 
 
@@ -130,12 +129,15 @@ function autoriser_rubrique_publierdans_super($faire, $type, $id, $qui, $opt) {
 if ($GLOBALS['autorite']['auteur_mod_email']
 OR false // autre possibilite de surcharge ?
 ) {
-function autoriser_auteur_modifier_super($faire, $type, $id, $qui, $opt) {
+if (!function_exists('autoriser_auteur_modifier')) {
+function autoriser_auteur_modifier($faire, $type, $id, $qui, $opt) {
 	if ($GLOBALS['autorite']['auteur_mod_email']) {
 		unset($opt['email']);
 	}
 	return autoriser_auteur_modifier_dist($faire, $type, $id, $qui, $opt);
 }
+} else
+	$autorite_erreurs[] = 'autoriser_auteur_modifier';
 }
 
 
@@ -145,7 +147,8 @@ function autoriser_auteur_modifier_super($faire, $type, $id, $qui, $opt) {
 if ($GLOBALS['autorite']['auteur_modere_forum']
 OR false // autre possibilite de surcharge ?
 ) {
-function autoriser_modererforum_super($faire, $type, $id, $qui, $opt) {
+if (!function_exists('autoriser_modererforum')) {
+function autoriser_modererforum($faire, $type, $id, $qui, $opt) {
 	return
 		autoriser('modifier', $type, $id, $qui, $opt)
 		OR (
@@ -155,75 +158,20 @@ function autoriser_modererforum_super($faire, $type, $id, $qui, $opt) {
 			AND spip_num_rows(auteurs_article($id, "id_auteur=".$qui['id_auteur']))
 		);
 }
+} else
+	$autorite_erreurs[] = 'autoriser_modererforum';
 }
 
 
 
-//
-// Forker la fonction autoriser de maniere a ce qu'elle
-// appelle autoriser_x_y_super() de preference a autoriser_x_y_dist()
-//
-function autoriser_super($faire, $type='', $id=0, $qui = NULL, $opt = NULL) {
-	static $restreint = array();
-
-	// Qui ? auteur_session ?
-	if ($qui === NULL)
-		$qui = $GLOBALS['auteur_session']; // "" si pas connecte
-	elseif (is_int($qui)) {
-		$qui = spip_fetch_array(spip_query(
-		"SELECT * FROM spip_auteurs WHERE id_auteur=$qui"));
-	}
-
-	// Admins restreints, les verifier ici (pas generique mais...)
-	// Par convention $restreint est un array des rubriques autorisees
-	// (y compris leurs sous-rubriques), ou 0 si admin complet
-	if (is_array($qui)
-	AND $qui['statut'] == '0minirezo'
-	AND !isset($qui['restreint'])) {
-		if (!isset($restreint[$qui['id_auteur']])) {
-			include_spip('inc/auth'); # pour auth_rubrique
-			$restreint[$qui['id_auteur']] = auth_rubrique($qui['id_auteur'], $qui['statut']);
-		}
-		$qui['restreint'] = $restreint[$qui['id_auteur']];
-	}
-	if (_DEBUG_AUTORISER) spip_log("autoriser $faire $type $id ($qui[nom]) ?");
-
-	// Chercher une fonction d'autorisation explicite
-	if (
-	// 1. Sous la forme "autoriser_type_faire"
-		(
-		$type
-		AND $f = 'autoriser_'.$type.'_'.$faire
-		AND (function_exists($f) OR function_exists($f=($f2=$f).'_super') OR function_exists($f=$f2.'_dist'))
-		)
-
-	// 2. Sous la forme "autoriser_type"
-	// ne pas tester si $type est vide
-	OR (
-		$type
-		AND $f = 'autoriser_'.$type
-		AND (function_exists($f) OR function_exists($f=($f2=$f).'_super') OR function_exists($f=$f2.'_dist'))
-	)
-
-	// 3. Sous la forme "autoriser_faire"
-	OR (
-		$f = 'autoriser_'.$faire
-		AND (function_exists($f) OR function_exists($f=($f2=$f).'_super') OR function_exists($f=$f2.'_dist'))
-	)
-
-	// 4. Sinon autorisation generique
-	OR (
-		$f = 'autoriser_defaut'
-		AND (function_exists($f) OR function_exists($f=($f2=$f).'_super') OR function_exists($f=$f2.'_dist'))
-	)
-
-	)
-		$a = $f($faire,$type,intval($id),$qui,$opt);
-
-	if (_DEBUG_AUTORISER) spip_log("$f($faire,$type,$id,$qui[nom]): ".($a?'OK':'niet'));
-
-	return $a;
+// Noter les erreurs pour les afficher dans le panneau de config
+if (serialize($autorite_erreurs) != $GLOBALS['meta']['autorite_erreurs']) {
+	include_spip('inc/meta');
+	ecrire_meta('autorite_erreurs', serialize($autorite_erreurs));
+	ecrire_metas();
+	spip_log('Erreur autorite : '.join(', ', $autorite_erreurs));
 }
+unset($autorite_erreurs);
 
 
 
