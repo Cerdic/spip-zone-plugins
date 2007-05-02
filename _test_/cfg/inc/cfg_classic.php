5934be1e6bcfec13e073c61fe84d9a1cc14a87a0
bertrand@toggg.com <bertrand@toggg.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-05-02 11:39:06 +0000

    Le storage "classic" exactement comme spip_meta courant, un test avec email webmaster, nom et descriptif du site
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@11804 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _test_/cfg/inc/cfg_classic.php
@@ -0,0 +1,44 @@
+<?php
+/*
+ * Plugin cfg : classe cfg_classic: storage a plat (classique) dans spip_meta
+ *
+ * Auteur : bertrand@toggg.com
+ * © 2007 - Distribue sous licence LGPL
+ *
+ */
+
+// cfg_classic retrouve et met a jour les donnees a plat dans spip_meta
+class cfg_classic
+{
+	function cfg_classic(&$cfg, $opt = array())
+	{
+		$this->cfg = &$cfg;
+		foreach ($opt as $o=>$v) {
+			$this->$o = $v;
+		}
+	}
+	
+// recuperer les valeurs, utilise la fonction commune lire_config() de cfg_options.php
+	function lire()
+	{
+    	$val = array();
+		foreach ($this->cfg->champs as $name => $def) {
+			$val[$name] = lire_config($name);
+	    }
+	    return $val;
+	}
+
+	// modifier chaque enregistrement de meta pour chaque champ
+	function modifier($supprimer = false)
+	{
+		foreach ($this->cfg->champs as $name => $def) {
+			if ($supprimer || !$this->cfg->val[$name]) {
+			    effacer_meta($name);
+			} else {
+			    ecrire_meta($name, $this->cfg->val[$name]);
+			}
+	    }
+	    ecrire_metas();
+	}
+}
+?>
