0533b5170e6fc3e2cbff2399c55700c10d89869e
fil@rezo.net <fil@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-05-02 23:08:53 +0000

    je hacke a fond pour assurer la securite et un affichage sympa... il vaudrait mieux que je m'abstienne, toggg
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@11812 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/autorite/inc/autoriser.php
+++ _dev_/autorite/inc/autoriser.php
@@ -326,5 +326,36 @@ if (serialize($autorite_erreurs) != $GLOBALS['meta']['autorite_erreurs']) {
 unset($autorite_erreurs);
 
 
+// Le plugin gere lui-meme la securite de sa propre configuration, d'ou cette fonction tortueuse
+if (_request('exec') == 'cfg'
+AND _request('cfg') == 'autorite') {
+	// Qui sont les webmestres ?
+	$webmestres = array();
+	include_spip('inc/texte');
+	$s = spip_query("SELECT * FROM spip_auteurs WHERE id_auteur IN (". join (',', array_filter(explode(':', _ID_WEBMESTRES), is_numeric)).")");
+	while ($qui = spip_fetch_array($s)) {
+		if (autoriser('webmestre','','',$qui))
+			$webmestres[$qui['id_auteur']] = typo($qui['nom']);
+	}
+
+	$message = 'Cette page de configuration est r&#233;serv&#233;e au(x) webmestre(s) du site&nbsp;: &nbsp; <b>';
+	$message .= join(', ', $webmestres);
+	$message .= "</b>\n"
+		."<p>Si vous souhaitez modifier cette liste, veuillez éditer le fichier <tt>config/mes_options.php</tt> (le cr&#233;er le cas &#233;ch&#233;ant) et y indiquer la liste des identifiants des auteurs webmestres, sous la forme suivante&nbsp;:</p>
+<pre style='text-align:left;'>&lt;?php
+define ('_ID_WEBMESTRES',
+  '1:5:8');
+?&gt;</pre>";
+
+	// Ne pas permettre a tous les admins de configurer
+	// TODO: comment se comporte cfg ?
+	if (!autoriser('webmestre')) {
+		include_spip('inc/minipres');
+		echo minipres(_T('info_acces_interdit'), $message);
+		exit;
+	}
+	
+	define('MESSAGE_CONFIG_AUTORISER', $message); // pour le squelette cfg_autorite
+}
 
 ?>
