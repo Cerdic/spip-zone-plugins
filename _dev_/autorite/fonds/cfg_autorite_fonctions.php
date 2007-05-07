9c3672701a696db6c2ed3098b6924906c38e29e3
bertrand@toggg.com <bertrand@toggg.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-05-07 10:57:53 +0000

    Separer le html de la valeur ajoutee en utilisant un pseudo filtre
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@11914 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/autorite/fonds/cfg_autorite_fonctions.php
+++ _dev_/autorite/fonds/cfg_autorite_fonctions.php
@@ -27,30 +27,18 @@ else if (serialize($autorite_erreurs) != $GLOBALS['meta']['autorite_erreurs']) {
 	spip_log('Erreur autorite : '.join(', ', $autorite_erreurs));
 }
 
-
 // Qui sont les webmestres ?
-$webmestres = array();
-include_spip('inc/texte');
-$s = spip_query("SELECT * FROM spip_auteurs WHERE id_auteur IN (". join (',', array_filter(explode(':', _ID_WEBMESTRES), is_numeric)).")");
-while ($qui = spip_fetch_array($s)) {
-	if (autoriser('webmestre','','',$qui))
-		$webmestres[$qui['id_auteur']] = typo($qui['nom']);
+// pour le squelette cfg_autorite
+function liste_webmestres($void)
+{
+	$webmestres = array();
+	include_spip('inc/texte');
+	$s = spip_query("SELECT * FROM spip_auteurs WHERE id_auteur IN (". join (',', array_filter(explode(':', _ID_WEBMESTRES), is_numeric)).")");
+	while ($qui = spip_fetch_array($s)) {
+		if (autoriser('webmestre','','',$qui))
+			$webmestres[$qui['id_auteur']] = typo($qui['nom']);
+	}
+	return  join(', ', $webmestres);
 }
-
-$message = 'Cette page de configuration est r&#233;serv&#233;e au webmestre du site&nbsp;: &nbsp; <b>';
-$message .= join(', ', $webmestres);
-$message .= "</b>\n"
-	."<hr />\n"
-	."<p><small>Si vous souhaitez modifier cette liste, veuillez éditer le fichier <tt>config/mes_options.php</tt> (le cr&#233;er le cas &#233;ch&#233;ant) et y indiquer la liste des identifiants des auteurs webmestres, sous la forme suivante&nbsp;:</small></p>
-<pre style='text-align:left;'>&lt;?php
-define ('_ID_WEBMESTRES',
-  '1:5:8');
-?&gt;</pre>";
-
-$message .= "<p><small>A noter&nbsp;: les webmestres d&#233;finis de cette mani&#232;re n'ont plus besoin de proc&#233;der &#224; l'authentification par FTP pour les op&#233;rations d&#233;licates (mise &#224; niveau de la base de donn&#233;es, par exemple).</small></p>\n";
-
-
-define('MESSAGE_CONFIG_AUTORISER', $message); // pour le squelette cfg_autorite
-
 
 ?>
