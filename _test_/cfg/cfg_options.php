2a6631952dcdf7962a4aa2fc8c59b79973f27755
bertrand@toggg.com <bertrand@toggg.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-05-08 23:12:13 +0000

    Introduction de #CONFIG(~), #CONFIG{~duchmol} et #CONFIG{table:id...}
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@11967 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/cfg/cfg_options.php
+++ _test_/cfg/cfg_options.php
@@ -23,13 +23,56 @@ function balise_CONFIG($p) {
 	return $p;
 }
 
-// lire_cfg() permet de recuperer une config depuis le php
-// $cfg: la config, lire_cfg('montruc') est un tableau
-// lire_cfg('montruc/sub') est l'element "sub" de cette config
+// lire_config() permet de recuperer une config depuis le php
+// $cfg: la config, lire_config('montruc') est un tableau
+// lire_config('montruc/sub') est l'element "sub" de cette config
 // $def: un defaut optionnel
 function lire_config($cfg='', $def=null, $serialize=false) {
-	$config = $GLOBALS['meta'];
-	$cfg = explode('/', $cfg);
+	$table = false;
+	if (is_string($cfg)) {
+		$cfg = explode('/', $cfg);
+		// si ca commence par ~duchmol/ , on veut un auteur par son login ou id
+		if ($cfg[0][0] == '~') {
+			$table = 'spip_auteurs';
+			// si c'est ~duchmol/ , on veut un auteur par son login ou id
+			if (strlen($cfg[0]) > 1) {
+				$id = array(substr(array_shift($cfg), 1));
+				$colid = array(is_numeric($id[0]) ? 'id_auteur' : 'login');
+			// dans l'extra de l'auteur connecte, ne marche que si cache nul
+			} else {;
+				array_shift($cfg);
+				$id = $GLOBALS['auteur_session'] ? $GLOBALS['auteur_session']['id_auteur'] : '';
+				$colid = array('id_auteur');
+			}
+		// si ca commence par table:id:id.../ , on veut une table
+		} elseif (strpos($cfg[0], ':')) {
+			$id = explode(':', array_shift($cfg));
+			list($table, $colid) = get_table_id(array_shift($id));
+		}
+	} else {
+		// on peut aussi comme cfg_extrapack donner directement table et le reste
+		list($table, $colid, $id, $cfg) = $cfg;
+		if (!$colid) {
+			list($table, $colid) = get_table_id($table);
+		}
+	}
+	// dans une table
+	if ($table) {
+		$extra = 'SELECT extra FROM ' . $table;
+		$and = ' WHERE ';
+		foreach ($colid as $i => $name) {
+			$extra .= $and . $name . '=' . 
+				(is_numeric($id[$i]) ? intval($id[$i]) : _q($id[$i]));
+			$and = ' AND ';
+	    }
+		$extra = spip_query($extra);
+		$extra = spip_fetch_array($extra);
+		$config = isset($extra['extra']) && $extra['extra'] ?
+					$extra['extra'] :  array();
+	// sinon classiquement de meta
+	} else {
+		$config = $GLOBALS['meta'];
+	}
 
 	while ($x = array_shift($cfg)) {
 		if (is_string($config) && is_array($c = @unserialize($config))) {
@@ -55,4 +98,24 @@ function lire_config($cfg='', $def=null, $serialize=false) {
 	return is_null($ret) && $def ? $def : $ret;
 }
 
+function get_table_id($table) {
+	static $catab = array(
+		'tables_principales' => 'base/serial',
+		'tables_auxiliaires' => 'base/auxiliaires',
+	);
+	if ($try = table_objet($table)) {
+		return array('spip_' . $try, array(id_table_objet($table)));
+	}
+	$try = array($table, 'spip_' . $table);
+	foreach ($catab as $categ => $catinc) {
+		include_spip($catinc);
+		foreach ($try as $nom) {
+			if (isset($GLOBALS[$categ][$nom])) {
+				return array($nom,
+					preg_split('/\s*,\s*/', $GLOBALS[$categ][$nom]['key']['PRIMARY KEY']));
+			}
+		}
+	}
+	return array(false, false);
+}
 ?>
