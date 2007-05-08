9cb8c40c4a00f3c10e8a003ed93e3b36339cc396
joseph@larmarange.net <joseph@larmarange.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-05-08 23:11:18 +0000

    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@11966 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/noisetier/noisetier_pipelines.php
+++ _dev_/noisetier/noisetier_pipelines.php
@@ -2,7 +2,7 @@
 
 	function noisetier_ajouterBoutons($boutons_admin) {
 		// si on est admin
-		if ($GLOBALS['connect_statut'] == "0minirezo" && $GLOBALS["connect_toutes_rubriques"]) {
+		if (autoriser('modifier','noisette')) {
 		  // on voit le bouton dans la barre "naviguer"
 			$boutons_admin['configuration']->sousmenu['noisetier']= new Bouton(
 			"../"._DIR_PLUGIN_NOISETIER."/img_pack/noisetier-24.png",  // icone
