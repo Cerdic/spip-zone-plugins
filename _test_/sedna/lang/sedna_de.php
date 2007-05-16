9fbea1923b69edc6f44cb5adbb17c2524190773f
fil@rezo.net <fil@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-05-16 21:59:58 +0000

    sedna en allemand (klaus)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@12244 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _test_/sedna/lang/sedna_de.php
@@ -0,0 +1,36 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// pour la contrib sedna
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+'aff_resume' => 'Zusammenfassungen anzeigen',
+'afficher_sources' => 'Diese Quelle anzeigen',
+'annee' => 'Jahr',
+'articles_recents_court' => 'Neue Artikel',
+'connexion' => 'Verbindung',
+'deconnexion' => 'Verbindung trennen',
+'derniere_syndication' => 'Letzte Syndication erfolgte',
+'derniere_syndication' => 'Letzte Syndikation&nbsp;:&nbsp;',
+'deuxjours' => 'Zwei Tage',
+'liens' => 'Artikel',
+'liens_pluriel' => 'Artikel',
+'masquer_resume' => 'Zusammenfassungen verbergen',
+'masquer_sources' => 'Quelle verbergen',
+'mois' => 'Monat',
+'pas_articles' => 'Keine Artikel in diesem Zeitraum!',
+'pas_synchro' => 'Nicht synchronisieren',
+'preferences' => 'Einstellungen',
+'probleme_de_syndication' => 'Syndikationsproblem',
+'semaine' => 'Woche',
+'sources' => 'Quellen',
+'synchro' => 'Synchronisieren',
+'synchro_titre' => 'Liste der gelesenen Artikel auf der Website speichern',
+'syndication_ajour' => 'Jetzt aktualisieren',
+'syndication_fait' => 'Syndication abgeschlossen',
+'toutes' => 'Alle'
+);
+
+
+?>
