db1d9f12c446041a23d726d069276ed8afc0138c
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-05-16 16:03:59 +0000

    "sans le debug"
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@12231 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/forms/forms_et_tables_1_9_1/inc/forms_lier_donnees.php
+++ _stable_/forms/forms_et_tables_1_9_1/inc/forms_lier_donnees.php
@@ -176,14 +176,12 @@ function Forms_liste_recherche_donnees($recherche,$les_donnees,$type,$type_table
 			JOIN spip_forms AS f ON d.id_form = f.id_form
 			WHERE d.statut!='poubelle' AND f.type_form="._q($type_table)
 			." AND $in AND valeur LIKE "._q("$recherche%")." GROUP BY c.id_donnee $limit");
-				var_dump($s);
 			if (spip_num_rows($res)<10){
 				$res = spip_query($s = "SELECT c.id_donnee FROM spip_forms_donnees_champs AS c
 				JOIN spip_forms_donnees AS d ON d.id_donnee = c.id_donnee
 				JOIN spip_forms AS f ON d.id_form = f.id_form
 				WHERE d.statut!='poubelle' AND f.type_form="
 				._q($type_table)." AND $in AND valeur LIKE "._q("%$recherche%")." GROUP BY c.id_donnee $limit");
-				var_dump($s);
 			}
 		}
 		while ($row = spip_fetch_array($res)){
