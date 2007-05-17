ec20e4ad95c9c251f0d9a45f45456ba20c10eca7
patfr@ifrance.com <patfr@ifrance.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-05-17 01:26:14 +0000

    arf, le '$toujours_paragrapher=true' cause encore des pbs...
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@12247 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/couteau_suisse/outils/introduction_fonctions.php
+++ _stable_/couteau_suisse/outils/introduction_fonctions.php
@@ -31,12 +31,18 @@ function cs_introduction($type, $texte, $chapo, $descriptif, $id) {
 	}
 	// si les points de suite ont ete ajoutes
 	if (strpos($result, '@@CS_SUITE@@') !== false) {
+		// precaution sur le tout paragrapher !
+		$mem = $GLOBALS['toujours_paragrapher'];  
+		$GLOBALS['toujours_paragrapher'] = false;  
+		// des points de suite bien propres
 		$intro_suite = propre(_INTRODUCTION_SUITE);
+		// si les points de suite sont cliquables
 		if ($id && _INTRODUCTION_LIEN == 1) {
 			if (substr($intro_suite, 0, 6) == '<br />') 
 				$intro_suite = propre("<br />[".substr($intro_suite, 6)."->art$id]");
 				else $intro_suite = propre("&nbsp;[{$intro_suite}->art$id]");
 		}
+		$GLOBALS['toujours_paragrapher'] = $mem; 
 		$result = str_replace('@@CS_SUITE@@', $intro_suite, $result);
 	}
 	return $result;
