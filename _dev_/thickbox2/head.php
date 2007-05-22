9962770e46f0e6062f0f9a8deb8c53cbb4beb0dc
renato@rezo.net <renato@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-05-22 09:32:05 +0000

    update thickbox to use the pipelines insert_js and verifie_js_necessaire
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@12372 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/thickbox2/head.php
+++ _dev_/thickbox2/head.php
@@ -6,13 +6,50 @@ function ThickBox1_insert_head($flux){
 
 // TODO: ne charger thickbox.js et thickbox.css que si 
 // $("a.thickbox,a[@type='image/jpeg'],...").size() > 0)
+if(!isset($GLOBALS["spip_pipeline"]["insert_js"]))
 
 $flux .=
 
 '
-<script src=\''.url_absolue(find_in_path('thickbox.js')).'\' type=\'text/javascript\'></script>
+<script src=\''.url_absolue(find_in_path('javascript/thickbox.js')).'\' type=\'text/javascript\'></script>
 <link rel="stylesheet" href="'.url_absolue(find_in_path('thickbox.css')).'" type="text/css" media="projection, screen" />
+<script type="text/javascript"><!--
+// Inside the function "this" will be "document" when called by ready() 
+// and "the ajaxed element" when called because of onAjaxLoad 
+var init_f = function() {
+	if ($("a.thickbox,a[@type=\'image/jpeg\'],a[@type=\'image/png\'],a[@type=\'image/gif\']",this).addClass("thickbox").size()) {
+		TB_chemin_animation = "'.url_absolue(find_in_path('circle_animation.gif')).'";
+		TB_chemin_close = "'.url_absolue(find_in_path('close.gif')).'";
+		TB_chemin_css = "'.url_absolue(find_in_path('thickbox.css')).'";
+		TB_init(this);
+	};
+}
+//onAjaxLoad is defined in private area only
+if(typeof onAjaxLoad == "function") onAjaxLoad(init_f);
+$(document).ready(init_f);
+// --></script>';
 
+else
+
+$flux .=
+'
+<link rel="stylesheet" href="'.url_absolue(find_in_path('thickbox.css')).'" type="text/css" media="projection, screen" />';
+
+return $flux;
+}
+
+function ThickBox1_insert_js($flux){
+// on ajoute la class thickbox aux liens de type="image/xxx"
+
+// TODO: ne charger thickbox.js et thickbox.css que si 
+// $("a.thickbox,a[@type='image/jpeg'],...").size() > 0)
+
+if($flux['type']=='fichier') 
+  $flux["data"]["ThickBox1"] = "thickbox";
+
+if($flux['type']=='inline')
+  $flux["data"]["ThickBox1"] =
+'
 <script type="text/javascript"><!--
 // Inside the function "this" will be "document" when called by ready() 
 // and "the ajaxed element" when called because of onAjaxLoad 
@@ -30,6 +67,18 @@ $(document).ready(init_f);
 // --></script>';
 
 	return $flux;
+}
+
+function ThickBox1_verifie_js_necessaire($flux) {
+
+//var_dump($flux["page"]);
+$page = $flux["page"]["texte"];
+$necessaire = preg_match(",<a.*type\s*=\s*['\"]image/(?:jpeg|png|gif)['\"],iUs",$page);
+
+$flux["data"]["ThickBox1"] = $necessaire;
+
+return $flux;
+  
 }
 
 ?>
