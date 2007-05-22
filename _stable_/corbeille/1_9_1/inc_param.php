40d427ccdd303c5f0ceb3684298cc3177b51e29b
cam.lafit@azerttyu.net <cam.lafit@azerttyu.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-05-22 18:46:08 +0000

    Ajout d'un paramétre libelle_court, à utiliser dans le menu de la page d'administration. (?exec=corbeille)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@12382 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/corbeille/1_9_1/inc_param.php
+++ _stable_/corbeille/1_9_1/inc_param.php
@@ -1,6 +1,5 @@
 <?php
 /* Déclaration des paramétres de configuration */
-
 global $corbeille_param;
 $corbeille_param = array (
 	"signatures"=> 		array(	"statut" => "poubelle", 
@@ -9,7 +8,8 @@ $corbeille_param = array (
 								"id" => "id_signature",
 								"temps" => "date_time",
 								"page_voir" => array($page4,'id_document'),
-								"libelle" => _L("Toutes les p&eacute;titions dans la corbeille :")
+								"libelle" => _L("Toutes les p&eacute;titions dans la corbeille :"),
+								"libelle_court" => _T('lien_petitions'),
 								),
 	"breves"=>	 		array(	"statut" => "refuse", 
 								"titre" => "titre",
@@ -17,7 +17,8 @@ $corbeille_param = array (
 								"id" => "id_breve",
 								"temps" => "date_heure",
 								"page_voir" => array("breves_voir",'id_breve'),
-								"libelle" => _L("Toutes les br&egrave;ves dans la corbeille :")
+								"libelle" => _L("Toutes les br&egrave;ves dans la corbeille :"),
+								"libelle_court" => _T('icone_breves')
 								),
 	"articles"=>	 	array(	"statut" => "poubelle",
 								"titre" => "titre",
@@ -25,21 +26,24 @@ $corbeille_param = array (
 								"id" => "id_article",
 								"temps" => "date",
 								"page_voir" => array("articles",'id_article'),
-								"libelle" => _L("Tous les articles dans la corbeille :")
+								"libelle" => _L("Tous les articles dans la corbeille :"),
+								"libelle_court" => _T('icone_articles')
 								),
 	"forums_publics"=>	array(	"statut" => "off",
 								"titre" => "titre",
 								"table" => "spip_forum",
 								"id" => "id_forum",
 								"temps" => "date_heure",
-								"libelle" => _L("Tous les messages du forum dans la corbeille :")
+								"libelle" => _L("Tous les messages du forum dans la corbeille :"),
+								"libelle_court" => _T('titre_forum')
 								),
 	"forums_prives"=>	array(	"statut" => "privoff",
 								"titre" => "titre",
 								"table" => "spip_forum",
 								"id" => "id_forum",
 								"temps" => "date_heure",
-								"libelle" => _L("Tous les messages du forum dans la corbeille :")
+								"libelle" => _L("Tous les messages du forum dans la corbeille :"),
+								"libelle_court" => _T('icone_forum_administrateur')
 								),
 	"auteurs" =>		array(	"statut" => "5poubelle",
 								"titre" => "nom",
@@ -47,10 +51,11 @@ $corbeille_param = array (
 								"id"=>"id_auteur",
 								"temps" => "maj",
 								"page_voir" => array("auteurs_edit",'id_auteur'),
-								"libelle" => _L("Tous les auteurs dans la corbeille :")
+								"libelle" => _L("Tous les auteurs dans la corbeille :"),
+								"libelle_court" => _T('icone_auteurs')
 								)					
 	);
 
-//print_r($corbeille_param["articles"]["statut"]);
+//print_r($corbeille_param);
 
 ?>
