5df960759eba54f7ea36203098ee54aff9597598
renato@rezo.net <renato@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-05-23 13:24:59 +0000

    introduce the experimental tags #DEBUT_TEXTE_HEAD and #FIN_TEXTE_HEAD; The code between these tags will be added to the head of the page directly from the squelette; The tags can be used even inside a boucle. The same text, if added multiple times, is added only once to the head; The plugin provide a demo article page with thickbox added if the portfolio is not empty
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@12405 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _dev_/_ze_laboratoire_/ajoute_scripts/ajoute_script_fonctions.php
@@ -0,0 +1,13 @@
+<?php
+
+function balise_DEBUT_TEXTE_HEAD($p) {
+	$p->code = "'<!-- spip_debut_texte_head -->'";
+	return $p;
+}
+
+function balise_FIN_TEXTE_HEAD($p) {
+	$p->code = "'<!-- spip_fin_texte_head -->'";
+	return $p;
+}
+
+?>
