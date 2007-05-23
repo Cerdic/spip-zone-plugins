4c802286a6a969e996581147b09119c208340d18
vincevg@free.fr <vincevg@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-05-23 17:10:31 +0000

    Petites modif pour geshi
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@12416 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/fckeditor/1.9.1/spip_fck/fckconfig.php
+++ _dev_/fckeditor/1.9.1/spip_fck/fckconfig.php
@@ -14,7 +14,7 @@
     FCKConfig.Plugins.Add( "tablecommands", null, "' . $_GET['path'] . '/FCKeditor/editor/plugins/" ) ;		
     FCKConfig.Plugins.Add( "notebasdepage", "fr","' . $_GET['path'] . '/spip_fck/plugins/" ) ;
     FCKConfig.Plugins.Add( "geshighlighter", "en,nl,fr","' . $_GET['path'] . '/spip_fck/plugins/" ) ;
-    
+    FCKConfig.Plugins.Add( "coloration", "fr","' . $_GET['path'] . '/spip_fck/plugins/" ) ;
 
     
 		FCKConfig.ToolbarSets["BarreArticle"] = [
@@ -27,7 +27,7 @@
       	["OrderedList","UnorderedList","-","Outdent","Indent"],
       	["JustifyLeft","JustifyCenter","JustifyRight","JustifyFull"],
       	["Link","Unlink","Anchor"],
-      	["Image","Flash","Rule","Smiley","SpecialChar","PageBreak","notebasdepage","RtHighlight"],
+      	["Image","Flash","Rule","Smiley","SpecialChar","PageBreak","notebasdepage","RtHighlight","coloration"],
       	["Table","-","TableInsertRow","TableDeleteRows","TableInsertColumn","TableDeleteColumns","TableInsertCell","TableDeleteCells","TableMergeCells","TableSplitCell"],
       	"/",
       	["Style","FontFormat","FontName","FontSize"],
