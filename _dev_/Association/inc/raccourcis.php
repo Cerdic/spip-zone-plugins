cc4659e94ba4367043a57eb9c27b72a2934fcdb4
berio@alg-a.org <berio@alg-a.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-05-24 10:01:43 +0000

    changes for galician language. Association plugin
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@12435 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/Association/inc/raccourcis.php
+++ _dev_/Association/inc/raccourcis.php
@@ -3,10 +3,10 @@
 debut_raccourcis();
 //icone_horizontale(_T('livre:cr&eacute;er_les_tables'), generer_url_ecrire("table"), '../'._DIR_PLUGIN_ASSOCIATION.'/img_pack/sql.png', 'creer.gif');
 	icone_horizontale(_T('asso:effacer_les_tables'), generer_url_ecrire("efface"), '../'._DIR_PLUGIN_ASSOCIATION.'/img_pack/sql.png', 'supprimer.gif');
-	icone_horizontale(_T('asso:Profil de l\'association'), generer_url_ecrire("profil"), '../'._DIR_PLUGIN_ASSOCIATION.'/img_pack/ecole.gif', 'rien.gif');
-	icone_horizontale(_T('asso:Cat&eacute;gories de cotisations'), generer_url_ecrire("categories"), '../'._DIR_PLUGIN_ASSOCIATION.'/img_pack/calculatrice.gif', '');
-	icone_horizontale(_T('asso:Gestion des banques'), generer_url_ecrire("banques"), '../'._DIR_PLUGIN_ASSOCIATION.'/img_pack/EuroOff.gif', '');	
-	icone_horizontale(_T('asso:Gestion de l\'association'), generer_url_ecrire("adherents"), '../'._DIR_PLUGIN_ASSOCIATION.'/img_pack/annonce.gif', '');
+	icone_horizontale(_T('asso:profil_de_lassociation'), generer_url_ecrire("profil"), '../'._DIR_PLUGIN_ASSOCIATION.'/img_pack/ecole.gif', 'rien.gif');
+	icone_horizontale(_T('asso:categories_de_cotisations'), generer_url_ecrire("categories"), '../'._DIR_PLUGIN_ASSOCIATION.'/img_pack/calculatrice.gif', '');
+	icone_horizontale(_T('asso:gestion_des_banques'), generer_url_ecrire("banques"), '../'._DIR_PLUGIN_ASSOCIATION.'/img_pack/EuroOff.gif', '');	
+	icone_horizontale(_T('asso:gestion_de_lassoc'), generer_url_ecrire("adherents"), '../'._DIR_PLUGIN_ASSOCIATION.'/img_pack/annonce.gif', '');
 fin_raccourcis();
 
 ?>
