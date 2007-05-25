5eaeaed52da3be7046b76e97797791414f131573
cmtmt2003@gmail.com <cmtmt2003@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-05-25 15:21:25 +0000

    traduction a l'espagnol
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@12461 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _dev_/Inscription2/lang/inscription2_es.php
@@ -0,0 +1,88 @@
+<?php
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+//page de affichage de la configuration du plugin
+'conf_plugin' => 'Configuraci&oacute;n Inscripci&oacute;on2',
+'conf_plugin_page' => 'Configuraci&oacute;n del plugin Inscripci&oacute;n2',
+'description_page' => 'En esta p&aacute;gina usted puede verificar la configuraci&oacute;n del plugin tal como est&aacute; guardada en la base de datos', 
+'configs' => 'Configuraciones',
+'formulaire_inscription' => 'Formulario de Inscripci&oacute;n',
+'info_defaut' => 'Informaciones por defecto',
+'info_defaut_desc' => 'Informaciones obligatorias por defecto',
+'info_perso' => 'Informaciones Personales',
+'info_perso_desc' => 'Informaciones personales que seran solicitadas a los nuevos adherentes del sitio',
+'info_gener' => 'Informaciones Generales',
+'info_gener_desc' => 'Opciones que seran solicitadas a los nuevos adherentes del sitio',
+'info_internes' => 'Informaciones Internas',
+'info_internes_desc' => 'Opciones que seran almacenadas en la base de datos, pero que no seran solicitadas a los adherentes del sitio en el formulario de inscription',
+
+'inserez_infos' => 'Ingrese los datos solicitados',
+
+'icone_menu_config' => 'Inscripci&oacute;n2',
+'nom' => 'Apellido',
+'prenom' => 'Nombre',
+'naissance' => 'Fecha de nacimiento',
+'adresse' => 'Direcci&oacute;n',
+'code_postal' => 'C&oacute;digo Postal',
+'sexe' => 'Sexo',
+'ville' => 'Ciudad',
+'email' => 'E-Mail',
+'telephone' => 'Tel&eacute;fono',
+'fax' => 'Fax',
+'mobile' => 'Celular',
+'profession' => 'Profesion',
+'societe' => 'Sociedad',
+'secteur' => 'Sector',
+'newsletter' => 'Listas de difusi&oacute;n',
+'fonction' => 'Funci&oacute;n',
+'masculin' => 'masculino',
+'feminin' => 'femenino',
+'user' => 'Usuario (login)',
+'divers' => 'Varios',
+'exp_divers' => 'Campo que sera propuesto con el nombre de comentario',
+'categorie' => 'Categoria de cotisaci&oacute;n',
+'exp_categorie' => 'Lista de categor&iacute;as de la tabla \'spip_asso_categories\' para que el usuario elija una',
+'publication' => 'Publicaci&oacute;n',
+'exp_publication' => 'Autorisaci&oacute;n de publicaci&oacute;n de los datos personales en un eventual anuario',
+
+'creation' => 'Fecha de craci&oacute;n de la fiche',
+'statut_rel' => 'Statut interno',
+'exp_statut_rel' => 'Campo diferente del \'statut\' de SPIP_auteurs, este sirve para el control interno de una instituci&oacute;n',
+
+'activation_compte' => 'Active su cuenta',
+'probleme_email' => 'Problema de e-mail&nbsp;: el correo de activaci&oacute;n no pudo enviarse.',
+'email_bonjour' => 'Buenos D&iacute;as @nom@,',
+'message_auto' => '(Mensaje Autom&aacute;tico)',
+'texte_email_inscription' =>   
+'Usted acaba de solicitar vuestra inscripci&oacute;n en nuestro sitio @nom_site@. 
+Actualmente esta a punto de terminar el ciclo de inscripc&oacute;n. Lo &uacute;nico que le queda por hacer es hacer click en el link siguiente para activar su cuenta e ingresar su contrase&ntilde;a. Una vez finalizado usted podr&aacute; conectarse a nuestra pagina con su nombre de usuario y contrase&ntilde;a.
+@link_activation@
+
+Su nombre de usuario es: *@login@*
+
+Si usted no ha solicitado ser parte de nuestro sitio haga click en el link siguiente para eliminar la solicitud.
+@link_suppresion@
+Muchas Gracias.
+
+El equipo de @nom_site@',
+'lisez_mail' => 'Un email acaba de ser enviado a la direcci&oacute;n email ingresada. Para poder utilizar su cuenta solo le queda un paso, siga las instrucciones que estan en el email y active su cuenta.',
+'mail_registre' => 'Esta direccion email ya existe en nuestra base de datos. Para activar su cuenta sigas las instrucciones que estan en el email que acaba de ser enviado.',
+
+'janvier' => 'Enero',
+'fevrier' => 'Febrero',
+'mars' => 'Marzo',
+'avril' => 'Abril',
+'mai' => 'Mayo',
+'juin' => 'Junio',
+'juillet' => 'Julio',
+'aout' => 'Agosto',
+'septembre' => 'Septiembre',
+'octobre' => 'Octubre',
+'novembre' => 'Noviembre',
+'decembre' => 'Diciembre'
+
+);
+
+
+?>
