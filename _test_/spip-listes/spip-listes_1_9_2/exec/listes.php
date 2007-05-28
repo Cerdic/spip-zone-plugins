bae0e6e775acc089c898bae89027d1a56cf0cf2f
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-05-28 12:10:09 +0000

    Icone necessite un return ... marchat pas en 1.9.3... doit etre compatible avec 1.9.2
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@12542 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/spip-listes/spip-listes_1_9_2/exec/listes.php
+++ _test_/spip-listes/spip-listes_1_9_2/exec/listes.php
@@ -258,7 +258,7 @@ function exec_listes_dist(){
 	if ($flag_editable) {
 		echo "<td><img src='"._DIR_IMG_PACK."rien.gif' width='5'></td>\n";
 		echo "<td align='center'>";
-		icone(_T('spiplistes:modifier_liste'), generer_url_ecrire("liste_edit","id_liste=$id_liste"),_DIR_PLUGIN_SPIPLISTES."img_pack/reply-to-all-24.gif", "edit.gif");
+		echo listes_edit_presentation($id_liste);
 		echo "</td>";
 	}
 	echo "</tr></table>\n";
@@ -470,24 +470,28 @@ function exec_listes_dist(){
 	echo fin_gauche(), fin_page();
 }
 
+function listes_edit_presentation($id_liste){
+	return icone(_T('spiplistes:modifier_liste'), generer_url_ecrire("liste_edit","id_liste=$id_liste"),_DIR_PLUGIN_SPIPLISTES."img_pack/reply-to-all-24.gif", "edit.gif");
+}
+
 /******************************************************************************************/
-/* SPIP-listes est un syst�e de gestion de listes d'abonn� et d'envoi d'information     */
+/* SPIP-listes est un système de gestion de listes d'abonnés et d'envoi d'information     */
 /* par email  pour SPIP.                                                                  */
 /* Copyright (C) 2004 Vincent CARON  v.caron<at>laposte.net , http://bloog.net            */
 /*                                                                                        */
 /* Ce programme est libre, vous pouvez le redistribuer et/ou le modifier selon les termes */
-/* de la Licence Publique G��ale GNU publi� par la Free Software Foundation            */
+/* de la Licence Publique Générale GNU publiée par la Free Software Foundation            */
 /* (version 2).                                                                           */
 /*                                                                                        */
-/* Ce programme est distribu�car potentiellement utile, mais SANS AUCUNE GARANTIE,       */
+/* Ce programme est distribué car potentiellement utile, mais SANS AUCUNE GARANTIE,       */
 /* ni explicite ni implicite, y compris les garanties de commercialisation ou             */
-/* d'adaptation dans un but sp�ifique. Reportez-vous �la Licence Publique G��ale GNU  */
-/* pour plus de d�ails.                                                                  */
+/* d'adaptation dans un but spécifique. Reportez-vous à la Licence Publique Généale GNU  */
+/* pour plus de détails.                                                                  */
 /*                                                                                        */
-/* Vous devez avoir re� une copie de la Licence Publique G��ale GNU                    */
-/* en m�e temps que ce programme ; si ce n'est pas le cas, �rivez �la                  */
+/* Vous devez avoir reçu une copie de la Licence Publique Générale GNU                    */
+/* en même temps que ce programme ; si ce n'est pas le cas, écrivez à la                  */
 /* Free Software Foundation,                                                              */
-/* Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, �ats-Unis.                   */
+/* Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, Etats-Unis.                   */
 /******************************************************************************************/
 
 ?>
