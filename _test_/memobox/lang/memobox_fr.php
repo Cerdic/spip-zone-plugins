14b2c03bc86c7ea238c1894b90f5b6fa33d64b9e
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-05-29 14:19:58 +0000

    "version francaise"
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@12580 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _test_/memobox/lang/memobox_fr.php
@@ -0,0 +1,15 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+'drag_drop' => "Faites glisser les titres pour les m&eacute;moriser",
+'link' => "Cliquer sur les liens sous les titres pour les m&eacute;moriser",
+'anchor_delete_title' => "Enlever cette page",
+'anchor_delete_text' => "enlever",
+'anchor_add_title' => "M&eacute;moriser ce lien pour le retrouver rapidement",
+'anchor_add_text' => "m&eacute;moriser ce lien"
+);
+
+
+?>
