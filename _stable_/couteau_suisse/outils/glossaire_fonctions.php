522222d58e26c567ddf5492db4eeea6b781fd3aa
patfr@ifrance.com <patfr@ifrance.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-05-30 00:27:10 +0000

    Le glossaire laisse tranquille le mot du contexte (par ex : spip.php?mot5)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@12593 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/couteau_suisse/outils/glossaire_fonctions.php
+++ _stable_/couteau_suisse/outils/glossaire_fonctions.php
@@ -10,14 +10,17 @@ charger_generer_url();  # pour generer_url_mot()
 function cs_rempl_glossaire($texte) {
 	$limit = defined('_GLOSSAIRE_LIMITE')?_GLOSSAIRE_LIMITE:-1;
 	$r = spip_query("SELECT id_mot, titre, texte FROM spip_mots WHERE type='Glossaire'");
-	while($mot = spip_fetch_array($r)) {
+	// parcours de tous les mots, sauf celui qui peut faire partie du contexte (par ex : /spip.php?mot5)
+	while($mot = spip_fetch_array($r)) if ($mot['id_mot']<>$GLOBALS['id_mot']) {
 //		$table[$mot[id_mot]] = "<abbr title=\"$mot[texte]\">$mot[titre]</abbr>";
-		$lien = generer_url_mot($mot[id_mot]);
-		$table1[$mot[id_mot]] = "<a name=\"mot$mot[id_mot]\" href=\"$lien\" class=\"cs_glossaire\"><span class=\"mot\">";
-		$table2[$mot[id_mot]] = "</span><span class=\"dl\"><span class=\"dt\">$mot[titre]</span><span class=\"dd\">"
+		$lien = generer_url_mot($mot['id_mot']);
+		$table1[$mot['id_mot']] = "<a name=\"mot$mot[id_mot]\" href=\"$lien\" class=\"cs_glossaire\"><span class=\"mot\">";
+		$table2[$mot['id_mot']] = "</span><span class=\"dl\"><span class=\"dt\">$mot[titre]</span><span class=\"dd\">"
 			. nl2br(trim($mot['texte'])) . "</span></span></a>";
+		// a chaque mot reconnu, on pose une balise temporaire	
 		$texte = preg_replace(",\b$mot[titre]\b,i", "@@GLOSS\\0#$mot[id_mot]@@", $texte, $limit);
 	}
+	// remplacement final des balises posees ci-dessus
 	return preg_replace(",@@GLOSS([^#]+)#([0-9]+)@@,e", '"$table1[\\2]\\1$table2[\\2]"', $texte);
 }
 
