8846b92f522563c25b6051e3f2bea4a2bdc2d437
joseph@larmarange.net <joseph@larmarange.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-06-02 00:21:03 +0000

    - Par défaut, les boucles MOTS et GROUPES_MOTS filtreront les mots-clés techniques. Cependant, le critère {tout} permettra de renvoyer tous les mots clés, et le critère {technique=truc} permettra de renvoyer uniquement les mots des groupes tel que technique vaut truc.
    ATTENTION : il semble que la déclaration des tables de SPIP écrase celle réalisée par le plugin. Il faut donc commenter les lignes 387 et 388 de base/serial.php pour pouvoir tester les boucles.
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@12653 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/mots_techniques/mots_techniques_mes_fonctions.php
+++ _dev_/mots_techniques/mots_techniques_mes_fonctions.php
@@ -1,4 +1,37 @@
 <?php
+include_spip('base/mots_techniques');
 
+function boucle_MOTS_dist($id_boucle, &$boucles) {
+	$boucle = &$boucles[$id_boucle];
+	$id_table = $boucle->id_table;
+	$boucle->from[$id_table] =  "spip_mots";
+
+	// Restreindre aux mots cles non techniques
+	if (!isset($boucle->modificateur['tout'])&&!isset($boucle->modificateur['criteres']['technique'])) {
+		$boucle->from["groupes"] =  "spip_groupes_mots";
+		$boucle->where[]= array("'='", "'groupes.id_groupe'", "'$id_table.id_groupe'");
+		$boucle->where[]= array("'='", "'groupes.technique'", "'\"\"'");
+	}
+
+	return calculer_boucle($id_boucle, $boucles); 
+}
+
+//
+// <BOUCLE(GROUPES_MOTS)>
+//
+// http://doc.spip.org/@boucle_GROUPES_MOTS_dist
+function boucle_GROUPES_MOTS_dist($id_boucle, &$boucles) {
+	$boucle = &$boucles[$id_boucle];
+	$id_table = $boucle->id_table;
+	$boucle->from[$id_table] =  "spip_groupes_mots";
+	$mtechnique = $id_table .'.technique';
+
+	// Restreindre aux mots cles non techniques
+	if (!isset($boucle->modificateur['criteres']['technique'])&&!isset($boucle->modificateur['tout'])) {
+		$boucle->where[]= array("'='", "'$mtechnique'", "'\"\"'");
+	}
+
+	return calculer_boucle($id_boucle, $boucles); 
+}
 
 ?>
