6e561cfd7a6fc4ab505753cc261a5f13430ac75c
vincevg@free.fr <vincevg@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-06-03 15:01:13 +0000

    Version 2.4.2 en attendant l'autochargement.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@12705 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _dev_/fckeditor/1.9.2/fckeditor/editor/plugins/placeholder/lang/fr.js
@@ -0,0 +1,27 @@
+/*
+ * FCKeditor - The text editor for Internet - http://www.fckeditor.net
+ * Copyright (C) 2003-2007 Frederico Caldeira Knabben
+ *
+ * == BEGIN LICENSE ==
+ *
+ * Licensed under the terms of any of the following licenses at your
+ * choice:
+ *
+ *  - GNU General Public License Version 2 or later (the "GPL")
+ *    http://www.gnu.org/licenses/gpl.html
+ *
+ *  - GNU Lesser General Public License Version 2.1 or later (the "LGPL")
+ *    http://www.gnu.org/licenses/lgpl.html
+ *
+ *  - Mozilla Public License Version 1.1 or later (the "MPL")
+ *    http://www.mozilla.org/MPL/MPL-1.1.html
+ *
+ * == END LICENSE ==
+ *
+ * Placholder French language file.
+ */
+FCKLang.PlaceholderBtn			= 'Insérer/Modifier une note de bas de page' ;
+FCKLang.PlaceholderDlgTitle		= 'Note de bas de page' ;
+FCKLang.PlaceholderDlgName		= 'Texte de la note de bas de page :' ;
+FCKLang.PlaceholderErrNoName	= 'Veuillez saisir le texte de la note de bas de page' ;
+FCKLang.PlaceholderErrNameInUse	= 'Ce texte est déjà utilisé' ;
