acee8568b9e94f839bf5bdbd2e6fb0c38827e47f
patfr@ifrance.com <patfr@ifrance.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-06-04 02:26:16 +0000

    Nouveau filtre qui retire le code source des jeux du texte original : pas_de_balise_jeux
    Le plugin "Jeux" aide le Couteau Suisse a construire la balise #INTRODUCTION :
    $GLOBALS['cs_introduire'][] = 'pas_de_balise_jeux';
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@12710 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/jeux/jeux_fonctions.php
+++ _stable_/jeux/jeux_fonctions.php
@@ -9,11 +9,18 @@
 // filtre qui evite d'afficher le resultat obtenu par certains plugins
 // grace aux espions : <!-- PLUGIN-DEBUT --> et <!-- PLUGIN-FIN -->
 // ou : <!-- PLUGIN-DEBUT-#xxxx --> et <!-- PLUGIN-FIN-#xxxx --> ou xxxx est le numero d'identification du plugin.
-
 if (!function_exists("pas_de_plugin")) {	
  function pas_de_plugin($texte){
 		return preg_replace(",<!--\s*PLUGIN-DEBUT(-#[0-9]*)?.*<!--\s*PLUGIN-FIN\\1?\s*-->,UimsS", '', $texte);
  }
 }
+
+// filtre qui retire le code source des jeux du texte original
+function pas_de_balise_jeux($texte){
+	return preg_replace(",<jeux>.*?</jeux>,UimsS", '', $texte);
+}
+
+// aide le Couteau Suisse a calculer la balise #INTRODUCTION
+$GLOBALS['cs_introduire'][] = 'pas_de_balise_jeux';
 
 ?>
