c88c21fa4b9e09c2ea20945140de78d0d08b7797
dov.gabbai@gidouille.net <dov.gabbai@gidouille.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-06-06 12:44:36 +0000

    Mis à jour du fichier de langue française:
    - correction des fautes de syntaxe et d'orthographe
    - correction des accents
    
    Ubu
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@12765 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/Inscription2/lang/inscription2_fr.php
+++ _dev_/Inscription2/lang/inscription2_fr.php
@@ -5,81 +5,81 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 //page de affichage de la configuration du plugin
 'conf_plugin' => 'Configuration Inscription2',
 'conf_plugin_page' => 'Configuration stock&eacute;e du plugin Inscription2',
-'description_page' => 'Ici vous pouvez verifier vos choix de configuration telles qu\'elles sont stock&eacute;es', 
+'description_page' => 'Ici vous pouvez verifier vos choix de configuration tels qu\'ils sont stock&eacute;s', 
 'configs' => 'Configurations',
-'formulaire_inscription' => 'Formulaire d\'Inscription',
+'formulaire_inscription' => 'Formulaire d\'inscription',
 'info_defaut' => 'Informations par d&eacute;faut',
 'info_defaut_desc' => 'Informations obligatoires par d&eacute;faut',
 'info_perso' => 'Informations personnelles',
-'info_perso_desc' => 'Informations personnelles qui seront demandees aux nouveaux adherents du site',
-'info_gener' => 'Informations Generales',
-'info_gener_desc' => 'Options qui seront demandees aux nouveaux adherents du site',
+'info_perso_desc' => 'Informations personnelles qui seront demand&eacute;es aux nouveaux adh&eacute;rents du site',
+'info_gener' => 'Informations G&eacute;n&eacute;rales',
+'info_gener_desc' => 'Options qui seront demand&eacute;es aux nouveaux adh&eacute;rents du site',
 'info_internes' => 'Informations Internes',
-'info_internes_desc' => 'Options qui seront stock&eacute;es dans la base de donn&eacute;es mais ne seront pas affich&eacute;es dans le formulaire des nouveaux adherents',
+'info_internes_desc' => 'Options qui seront stock&eacute;es dans la base de donn&eacute;es mais ne seront pas affich&eacute;es dans le formulaire des nouveaux adh&eacute;rents',
 
-'inserez_infos' => 'Veuillez inserez les informations demand&eacute;s',
+'inserez_infos' => 'Veuillez ins&eacute;rez les informations demand&eacute;s',
 
 'form' => 'Formulaire',
 'fiche' => 'Fiche',
 'fiche_mod' => 'Modifiable',
 'table' => 'Table',
 'form_expl' => ' : Le champ sera affich&eacute; sur le formulaire INSCRIPTION2',
-'fiche_expl'=> ' : Le champ sera visible sur la fiche d\'adherent',
-'fiche_mod_expl' => ' : Le champ sera modifiable depuis l\'interface publique par le propre adherent &agrave; condition d\'utiliser CRAYONS',
-'table_expl' => ' : Le champ sera affich&eacute; sur la liste des adherents (espace priv&eacute;)',
+'fiche_expl'=> ' : Le champ sera visible sur la fiche d\'adh&eacute;rent',
+'fiche_mod_expl' => ' : Le champ sera modifiable depuis l\'interface publique par l\'adh&eacute;rent &agrave; condition d\'utiliser le plugin CRAYONS',
+'table_expl' => ' : Le champ sera affich&eacute; sur la liste des adh&eacute;rents (espace priv&eacute;)',
 
 'icone_menu_config' => 'Inscription2',
 'nom' => 'Nom',
-'prenom' => 'Prenom',
+'prenom' => 'Pr&eacute;nom',
 'naissance' => 'Date de naissance',
 'adresse' => 'Adresse',
 'code_postal' => 'Code Postal',
 'sexe' => 'Sexe',
 'ville' => 'Ville',
 'email' => 'E-Mail',
-'telephone' => 'Telephone',
+'telephone' => 'T&eacute;l&eacute;phone',
 'fax' => 'Fax',
 'mobile' => 'Mobile',
 'profession' => 'Profession',
-'societe' => 'Societe',
+'societe' => 'Soci&eacute;t&eacute;',
 'secteur' => 'Secteur',
 'newsletter' => 'Listes de diffusion',
 'fonction' => 'Fonction',
 'masculin' => 'masculin',
-'feminin' => 'feminin',
+'feminin' => 'f&eacute;minin',
 'username' => 'Nom d\'utilisateur (login)',
 'divers' => 'Divers',
 'exp_divers' => 'Champ qui sera propos&eacute; aux visiteurs avec le nom de commentaire',
 'categorie' => 'Cat&eacute;gorie de cotisation',
-'exp_categorie' => 'Liste deroulante des categories de la table \'spip_asso_categories\' pour que l\'utilisateur choissise une',
+'exp_categorie' => 'Liste d&eacute;roulante des cat&eacute;gories de la table \'spip_asso_categories\' pour que l\'utilisateur choisisse une',
 'publication' => 'Publication',
 'exp_publication' => 'Autorisation de publication des donn&eacute;es personnelles',
 
-'creation' => 'Date de creation de la fiche',
+'creation' => 'Date de cr&eacute;ation de la fiche',
 'statut_rel' => 'Statut interne',
-'exp_statut_rel' => 'Champ different du statut de SPIP, celui-ci sert pour le control interne d\'une institution',
+'exp_statut_rel' => 'Champ diff&eacute;rent du statut de SPIP, celui-ci sert pour le controle interne d\'une institution',
 
 'activation_compte' => 'Activez votre compte',
 'probleme_email' => 'Probl&egrave;me de mail&nbsp;: l\'email d\'activation ne peut pas &ecirc;tre envoy&eacute;.',
 'email_bonjour' => 'Bonjour @nom@,',
 'message_auto' => '(ceci est un message automatique)',
-'texte_email_inscription' =>   'Vous avez demand&eacute; de participer dans notre site @nom_site@. 
+'texte_email_inscription' =>   'Vous avez demand&eacute; de participer &agrave; notre site @nom_site@. 
 Vous &ecirc;tes actuellement sur le point de finir votre inscription. Il vous suffit de cliquer le lien ci dessous pour activer votre compte et choisir votre mot de passe.
 @link_activation@
 Votre login est&nbsp;: @login@
 
-Si vous n\'avez pas demand&eacute; cette inscription ou vous ne voulez plus faire partie de notre site, cliquez le lien ci dessous.\n
+Si vous n\'avez pas demand&eacute; cette inscription ou ne voulez plus faire partie de notre site, cliquez le lien ci dessous.\n
 @link_suppresion@
 Merci de votre confiance.
 
-L\'equipe de @nom_site@',
-'lisez_mail' => 'Un email vient d\'&ecirc;tre envoy&eacute; &agrave; l\'adresse fourni. Pour activer votre compte veuillez suivre les instructions.',
-'mail_renvoye' => 'Cette adresse email a d&eacute;j&agrave; &eacute;t&eacute; enregistr&eacute, veuillez activer votre compte en suivant les instructiones dans votre mail.',
-'redemande_password' => 'Reinserez votre mot de passe&nbsp;:',
-'texte_email_confirmation' => 'Votre compte vient d\'&ecirc;tre activ&eacute;, vous pouvez d&egrave;s maintenant vous connecter dans notre site en utilisant vos identifiants personnalles.\n
+L\'6eacute;quipe de @nom_site@',
+'lisez_mail' => 'Un email vient d\'&ecirc;tre envoy&eacute; &agrave; l\'adresse fournie. Pour activer votre compte veuillez suivre les instructions.',
+'mail_renvoye' => 'Cette adresse email a d&eacute;j&agrave; &eacute;t&eacute; enregistr&eacute;e, veuillez activer votre compte en suivant les instructiones contenues dans le mail.',
+'redemande_password' => 'R&eacute;inserez votre mot de passe&nbsp;:',
+'texte_email_confirmation' => 'Votre compte vient d\'&ecirc;tre activ&eacute;, vous pouvez d&egrave;s maintenant vous connecter &agrave; notre site en utilisant vos identifiants personnels.\n
 
 Votre login est&nbsp;: @login@
-et votre mot de passe vous venez de la saisir.
+et votre mot de passe vous venez de le saisir.
 Merci de Votre Confiance
 
 L\'equipe de @nom_site@',
