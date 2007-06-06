2de614606b23d70f4e35cbeca688bcda4900e5d8
dov.gabbai@gidouille.net <dov.gabbai@gidouille.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-06-06 13:41:55 +0000

    Premier jet de la traduction anglaise.
    N'étant pas anglophone il y a sans doute des corrections à y apporter
    
    Ubu
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@12769 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _dev_/Inscription2/lang/inscription2_en.php
@@ -0,0 +1,104 @@
+<?php
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+//page d'affichage de la configuration du plugin
+'conf_plugin' => 'Inscription2 settings',
+'conf_plugin_page' => 'Registered settings of plugin Inscription2',
+'description_page' => 'Here you can check your settings choices how they are registered', 
+'configs' => 'Configurations',
+'formulaire_inscription' => 'Form of inscription',
+'info_defaut' => 'Informations by defect',
+'info_defaut_desc' => 'Obligatory by defect informations',
+'info_perso' => 'Personal informations',
+'info_perso_desc' => 'Personal informations requested from the new members of the site',
+'info_gener' => 'General Informations',
+'info_gener_desc' => 'Options which will be asked to the new adherents of the site',
+'info_internes' => 'Internal information',
+'info_internes_desc' => 'Options which will be stored in the data base but will not be displayed in the form for the new members',
+
+'inserez_infos' => 'Please fill the form with informations requested',
+
+'form' => 'Form',
+'fiche' => 'card-index',
+'fiche_mod' => 'Modifiable',
+'table' => 'Table',
+'form_expl' => ' : The field will be displayed on the form INSCRIPTION2',
+'fiche_expl'=> ' : The field will be visible on the card of member',
+'fiche_mod_expl' => ' : The field could be modified from the public interface by the member if the plugin CRAYONS is used (PENCILS)',
+'table_expl' => ' : The field will be displayed on the list of the adherents (private area)',
+
+'icone_menu_config' => 'Inscription2',
+'nom' => 'Name',
+'prenom' => 'First name',
+'naissance' => 'Date of birth',
+'adresse' => 'Address',
+'code_postal' => 'Postal Code',
+'sexe' => 'Gender',
+'ville' => 'City',
+'email' => 'E-Mail',
+'telephone' => 'Telephone',
+'fax' => 'Fax',
+'mobile' => 'Mobile',
+'profession' => 'Profession',
+'societe' => 'Company',
+'secteur' => 'Sector of activity',
+'newsletter' => 'Newsletter',
+'fonction' => 'Function',
+'masculin' => 'male',
+'feminin' => 'female',
+'username' => 'Username (login)',
+'divers' => 'any other business',
+'exp_divers' => 'Field which will be proposed to the visitors under the name of comment',
+'categorie' => 'Category of contribution',
+'exp_categorie' => 'Drop-down list of the categories of the table \'spip_asso_categories\' so that the user chooses one',
+'publication' => 'Publication',
+'exp_publication' => 'Publication authorization of the personal data',
+
+'creation' => 'Creation date of the card',
+'statut_rel' => 'Internal status',
+'exp_statut_rel' => 'This field is different from the SPIP status, this one is useful for the internal control of an institution',
+
+'activation_compte' => 'Activate your account',
+'probleme_email' => 'eMail trouble &nbsp;: the activation email cannot be sent.',
+'email_bonjour' => 'Hello @nom@,',
+'message_auto' => '(this is an automatic message)',
+'texte_email_inscription' =>   'You asked to take part in our site @nom_site@. 
+You are currently about to finish your inscription. You just have to click the link below to activate your account and to choose your password.
+@link_activation@
+Your login is&nbsp;: @login@
+
+If you did not ask for this inscription or you do not want to form part of our site any more, click link below.\n
+@link_suppresion@
+Thank you for your confidence.
+
+The @nom_site@\'s Team',
+'lisez_mail' => 'An email has been just sent to the provided address. To activate your account please follow the instructions.',
+'mail_renvoye' => 'This email address was already recorded. Please activate your account following the instructions contained in the mail.',
+'redemande_password' => 'Please fill your password again:',
+'texte_email_confirmation' => 'Your account has been activated. Since now you can access to the site by using your personal identifiers.\n
+
+Your login is&nbsp;: @login@
+and your password you just typed it.
+Thank you for your confidence
+
+The @nom_site@\'s Team',
+'compte_active' => 'Your account was activated',
+'suppression_faite' => "erasure completed",
+'janvier' => 'January',
+'fevrier' => 'February',
+'mars' => 'March',
+'avril' => 'April',
+'mai' => 'May',
+'juin' => 'June',
+'juillet' => 'July',
+'aout' => 'August',
+'septembre' => 'September',
+'octobre' => 'October',
+'novembre' => 'November',
+'decembre' => 'December'
+
+);
+
+
+?>
