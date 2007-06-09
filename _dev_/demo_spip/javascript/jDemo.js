78d16464319a407aa584e2d85b7d7a07e680d34e
renato@rezo.net <renato@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-06-09 13:06:06 +0000

    added support for mouse actions and highlight of an element blocking all the UI other than the element itself
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@12809 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/demo_spip/javascript/jDemo.js
+++ _dev_/demo_spip/javascript/jDemo.js
@@ -9,29 +9,32 @@
 
 (function($) {
   
-  $.startDemo = function(url,name,step) {
-    $.jDemo.loadDemo(url,name,step);
+  $.startDemo = function(url,name,step,path_resources) {
+    jDemo.path_resources = path_resources;
+    jDemo.loadDemo(url,name,step);
   };
   
-  $.jDemo = {
+  var jDemo = {
     xmlData: null,
     actionsToRevert: [],
+    path_resources: "",
+    queues: {},
     init: function(url,name,step) {
       $.get(url,function(data){
-        $.jDemo.xmlData = {data:data,steps:{}};
-        $.jDemo.loadDemo(url,name,step);
+        jDemo.xmlData = {data:data,steps:{}};
+        jDemo.loadDemo(url,name,step);
       });
     },
     loadDemo: function(url,name,step) {
-      if(!$.jDemo.xmlData) 
-        return $.jDemo.init(url,name);
-      if(!$.jDemo.xmlData.steps[name]) {
-        var $demo = $("demo[@name="+name+"]",$.jDemo.xmlData.data);
+      if(!jDemo.xmlData) 
+        return jDemo.init(url,name);
+      if(!jDemo.xmlData.steps[name]) {
+        var $demo = $("demo[@name="+name+"]",jDemo.xmlData.data);
         if(!$demo.size()) 
           return alert("Demo non trovata");
-        $.jDemo.xmlData.steps[name] = $('step',$demo);
+        jDemo.xmlData.steps[name] = $('step',$demo);
       }    
-      $steps = $.jDemo.xmlData.steps[name]; 
+      $steps = jDemo.xmlData.steps[name]; 
       
       if(!step) step = 1;
       step--;
@@ -40,7 +43,7 @@
         return alert("Step non trovato");
       var title = $('title',$step).text();
       var content = $('content',$step).text();
-      var res = '<div id="demo_spip" class="jqmWindow">'+
+      var res = '<div id="jDemo_window" class="jqmWindow">'+
                 (title?'<h3>'+title+'</h3>':'')+
                 (content?'<div class="jDemo_content">'+content+'</div>':'')+
                 '<div class="jDemo_footer">';
@@ -53,10 +56,10 @@
   
       res += ' <a href="#" class="jqmClose">Chiudi</a></div></div>';
       $('body').append(res);
-      $('#jDemo_prev').click($.jDemo.navigate);
-      $('#jDemo_next').click($.jDemo.navigate);
-      $('#demo_spip').jqm({overlay:0,onHide:function(hash){hash.w.hide();$.jDemo.revertActions();}}).jqmShow();
-      $.jDemo.doActions($step);
+      $('#jDemo_prev').click(jDemo.navigate);
+      $('#jDemo_next').click(jDemo.navigate);
+      $('#jDemo_window').jqm({overlay:0,onHide:function(hash){hash.w.hide();jDemo.revertActions();}}).jqmShow();
+      jDemo.doActions($step);
     },
     doActions: function($step) {
       var $actions = $('actions>',$step);
@@ -66,25 +69,85 @@
           case 'highlight':
             var $target = $($el.attr('selector'));
             var curr_color = $target.css('background-color');
-            var f = function() {$target.css('background-color',curr_color)}
-            $.jDemo.actionsToRevert.push(f);
+            jDemo.actionsToRevert.push(function() {$target.css('background-color',curr_color)});
             $target.css('background-color',$el.attr('color') || 'yellow');
+            break;
+          case 'reversehighlight':
+            if(!$.fn.offset) {
+              alert("E' necessario il plugin dimensions");
+              break;
+            }
+            var $target = $($el.attr('selector'));
+            var offset = $target.offset({scroll:false});
+            offset.height = $target.height();
+            offset.bottom = offset.top+offset.height;
+            offset.right = offset.left+$target.width();
+            var $overlay_base = $('<div class="jqmOverlay"></div>').css({position:'absolute','z-index':3000-1,opacity:0.5});
+            var $overlay = $([]).add($overlay_base.clone().css({top:'0px',left:'0px',width:$(document).width(),height:offset.top})).
+            add($overlay_base.clone().css({top:offset.bottom,left:'0px',width:$(document).width(),height:$(document).height()-offset.bottom})).
+            add($overlay_base.clone().css({top:offset.top,left:'0px',width:offset.left,height:offset.height})).
+            add($overlay_base.clone().css({top:offset.top,left:offset.right,width:$(document).width()-offset.right,height:offset.height}));
+            jDemo.actionsToRevert.push(function() {$overlay.remove();});
+            $("body").append($overlay);
+            break;
+          case 'mouse':
+            if(!$.fn.offset) {
+              alert("E' necessario il plugin dimensions");
+              break;
+            }
+            var queue = [];
+            $(">",this).each(function(){
+              $cmd = $(this);
+              switch(this.tagName) {
+                case "set":
+                  var $target = $($cmd.attr('selector'));
+                  var offset = $target.offset({scroll:false});
+                  $mouse = $('<img id="jDemo_pointer" src="'+jDemo.path_resources+'Cursor.png">').css({position:'absolute',left:offset.left,top:offset.top,zIndex:3000+2});
+                  queue.push(function(){$("body").append($mouse);jDemo.executeQueue("mouse");});
+                  jDemo.actionsToRevert.push(function() {$mouse.remove();});
+                  break;
+                case "move":
+                  var $target = $($cmd.attr('selector'));
+                  var speed = 1000/($cmd.attr('speed') || 100);
+                  var offset = $target.offset({scroll:false});
+                  queue.push(function(){
+                    var offsetPointer = $('#jDemo_pointer').offset({scroll:false});
+                    var distance = Math.sqrt(Math.pow(offset.top-offsetPointer.top,2)+Math.pow(offset.left-offsetPointer.left,2));
+                    $('#jDemo_pointer').animate({top:offset.top,left:offset.left},parseInt(distance*speed),function(){jDemo.executeQueue("mouse");});
+                    });
+                  break;
+                 case "click":
+                    queue.push(function(){
+                      var offset = $('#jDemo_pointer').offset({scroll:false});
+                      var $click = $('<img id="jDemo_click" src="'+jDemo.path_resources+'CursorClick.png">').css({position:'absolute',left:offset.left,top:offset.top,zIndex:3000+1});
+                      $("body").append($click);
+                      jDemo.actionsToRevert.push(function() {$click.remove();});
+                      jDemo.executeQueue("mouse");
+                    });
+              }
+            });
+            jDemo.queues["mouse"] = queue;
+            jDemo.executeQueue("mouse");
             break;
         }
       });
     },
     revertActions : function() {
       var action;
-      while(action = $.jDemo.actionsToRevert.pop()) {
+      while(action = jDemo.actionsToRevert.pop()) {
         action();
       }      
     },
     navigate: function() {
-      $('#demo_spip').jqmHide().remove();
-      $.jDemo.revertActions();
+      $('#jDemo_window').jqmHide().remove();
+      jDemo.revertActions();
       var data = this.rel.split(/&/);
-      $.jDemo.loadDemo('',data[0],data[1]);
+      jDemo.loadDemo('',data[0],data[1]);
       return false;    
+    },
+    executeQueue: function(queue) {
+      var f = jDemo.queues[queue].shift();
+      if(f) f(); 
     }
   }
   
--- _dev_/demo_spip/spip_demo_pipelines.php
+++ _dev_/demo_spip/spip_demo_pipelines.php
@@ -10,11 +10,13 @@ function spip_demo_header_prive($flux) {
       break;
   }
   if($file_demo && $name_demo)  
+  $path = _DIR_PLUGIN_SPIP_DEMO."javascript/";
   $flux .= "<link type='text/css' rel='stylesheet' href='"._DIR_PLUGIN_SPIP_DEMO."javascript/jqModal.css' />
-  <script src='"._DIR_PLUGIN_SPIP_DEMO."javascript/jqModal.js'></script>
-  <script src='"._DIR_PLUGIN_SPIP_DEMO."javascript/jDemo.js'></script>
+  <script src='{$path}jquery.dimensions.js'></script>
+  <script src='{$path}jqModal.js'></script>
+  <script src='{$path}jDemo.js'></script>
   <script type='text/javascript'>
-    jQuery(function(){jQuery.startDemo('"._DIR_PLUGIN_SPIP_DEMO."$file_demo','$name_demo');});
+    jQuery(function(){jQuery.startDemo('"._DIR_PLUGIN_SPIP_DEMO."$file_demo','$name_demo',1,'$path');});
   </script>
   "; 
   return $flux;
