e86875cf6c958a462355c8dfc6eb75db9f0df652
maieulrouquette@tele2.fr <maieulrouquette@tele2.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-06-09 09:40:59 +0000

    langue
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@12806 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _test_/enviar_email/lang/enviarmail_es.php
@@ -0,0 +1,23 @@
+<?
+
+
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+'envoyer_article' => 'Enviar artículo',
+'envoyer' => 'Enviar',
+'reference_de' => 'la referencia de',
+'par_courriel' => 'por correo electrónico',
+'a_destination' => 'Enviar a la dirección:',
+'donnees_optionnelles' => 'Datos opcionales',
+'separe_virgule' 	=> 'separa con comas si escribes más de una',
+'sujet' 			=> 'Asunto',
+'titre_vouloir' 	=> 'título que quieres darle al mensaje',
+'nom' 				=> 'Tu nombre',
+'adresse' 			=> 'Tu dirección de correo electrónico',
+'texte' 			=> 'Puedes añadir un texto en el mensaje',
+'envoi'				=> 'Enviar',
+'annuler' 			=> 'Cancelar'
+);
+
+
+?>
