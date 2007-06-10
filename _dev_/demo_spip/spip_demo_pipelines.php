2ccf7f922d6a772cefd477c5df97a1bab9b0bcc1
renato@rezo.net <renato@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-06-10 17:45:47 +0000

    curly braces missing
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@12832 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/demo_spip/spip_demo_pipelines.php
+++ _dev_/demo_spip/spip_demo_pipelines.php
@@ -9,16 +9,17 @@ function spip_demo_header_prive($flux) {
       $name_demo = 'test';
       break;
   }
-  if($file_demo && $name_demo)  
-  $path = _DIR_PLUGIN_SPIP_DEMO."javascript/";
-  $flux .= "<link type='text/css' rel='stylesheet' href='"._DIR_PLUGIN_SPIP_DEMO."javascript/jqModal.css' />
-  <script src='{$path}jquery.dimensions.js'></script>
-  <script src='{$path}jqModal.js'></script>
-  <script src='{$path}jDemo.js'></script>
-  <script type='text/javascript'>
-    jQuery(function(){jQuery.startDemo('"._DIR_PLUGIN_SPIP_DEMO."$file_demo','$name_demo',1,'$path');});
-  </script>
-  "; 
+  if($file_demo && $name_demo) {  
+    $path = _DIR_PLUGIN_SPIP_DEMO."javascript/";
+    $flux .= "<link type='text/css' rel='stylesheet' href='"._DIR_PLUGIN_SPIP_DEMO."javascript/jqModal.css' />
+    <script src='{$path}jquery.dimensions.js'></script>
+    <script src='{$path}jqModal.js'></script>
+    <script src='{$path}jDemo.js'></script>
+    <script type='text/javascript'>
+      jQuery(function(){jQuery.startDemo('"._DIR_PLUGIN_SPIP_DEMO."$file_demo','$name_demo',1,'$path');});
+    </script>
+    ";
+  } 
   return $flux;
 }
 
