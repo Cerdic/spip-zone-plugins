5af5f2fc1fa7e21b5583c934424d679273c7fa5c
fdm@ufe.be <fdm@ufe.be@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-06-12 20:02:20 +0000

    Pour faire propre
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@12872 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/Association/Association_1.9.2/inc_association.php
+++ _test_/Association/Association_1.9.2/inc_association.php
@@ -13,22 +13,13 @@
 $p=explode(basename(_DIR_PLUGINS)."/",str_replace('\\','/',realpath(dirname(__FILE__))));
 define('_DIR_PLUGIN_ASSOCIATION',(_DIR_PLUGINS.end($p)));
 
-/* public static */
 function association_ajouterBoutons($boutons_admin) {
+
 	// si on est admin
 	if ($GLOBALS['connect_statut'] == "0minirezo" && $GLOBALS["connect_toutes_rubriques"]) {
-	  // on voit le bouton dans la barre "naviguer"
-	  
-	  /* Changer la page de preentation pour une version alternative axee sur les abonnements */
-	  /* 
-	  $boutons_admin['naviguer']->sousmenu['abonnes_payants']= new Bouton(
-		"../"._DIR_PLUGIN_ASSOCIATION."/img_pack/annonce.gif",  // icone
-		"abonn&eacute;s" //titre
-		);
-	  */		
 		
-	 /* Accueil original  */
-
+	  // on voit le bouton dans la barre "naviguer"		
+		
 	  $boutons_admin['naviguer']->sousmenu['association']= new Bouton(
 		"../"._DIR_PLUGIN_ASSOCIATION."/img_pack/annonce.gif",  // icone
 		_T('asso:titre_menu_gestion_association') //titre
@@ -38,9 +29,9 @@ function association_ajouterBoutons($boutons_admin) {
 	return $boutons_admin;
 }
 
-/* public static */
 function association_ajouterOnglets($flux) {
 	$rubrique = $flux['args'];
 	return $flux;
 }
+
 ?>
