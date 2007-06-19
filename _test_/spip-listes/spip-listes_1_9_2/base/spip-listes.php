4a3f9dbeb7bde5e690638b10db3adb02f850f772
booz@rezo.net <booz@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-06-19 19:50:13 +0000

    bug install auteurs_elargis
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@12967 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/spip-listes/spip-listes_1_9_2/base/spip-listes.php
+++ _test_/spip-listes/spip-listes_1_9_2/base/spip-listes.php
@@ -16,7 +16,7 @@
 
 	//creer la table auteurs_elargis si besoin
 	$spip_auteurs_elargis['id_auteur'] = "bigint(21) NOT NULL";
-	$spip_auteurs_elargis['spip_listes_format'] = "VARCHAR( 8 ) DEFAULT 'non' NOT NULL";
+	$spip_auteurs_elargis['`spip_listes_format`'] = "VARCHAR( 8 ) DEFAULT 'non' NOT NULL";
 	$spip_auteurs_elargis_key = array("PRIMARY KEY"	=> "id_auteur");
 	$tables_principales['spip_auteurs_elargis']  =	array('field' => &$spip_auteurs_elargis, 'key' => &$spip_auteurs_elargis_key);
 	
