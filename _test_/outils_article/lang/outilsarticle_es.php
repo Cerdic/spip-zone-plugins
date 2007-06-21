50477c52f2d17aa7ec30d32e9aa13e4785437693
cvallois@yahoo.fr <cvallois@yahoo.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-06-21 23:38:02 +0000

    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@13029 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _test_/outils_article/lang/outilsarticle_es.php
@@ -0,0 +1,19 @@
+<?php
+
+	$GLOBALS[$GLOBALS['idx_lang']] = array(
+		'titre'				=>	'Cojee a herramientas',
+		'taille'			=>	'Tamaño ',
+		'pdf'				=>	'Formato pdf ',
+		'impression'		=>	'Impresión ',
+		'impression_title'	=>	'versión imprimible del artículo ',
+		'auteur'			=>	'Del mismo autor ',
+		'reaction'			=>	'Reacciones ',
+		'reagir'			=>	'Reaccionar al artículo ',
+		'config_titre'		=>	'Cojee a herramientas para artículo ',
+		'config_descr'		=>	'puede configurar los elementos que deben indicarse en la cojea a herramientas ',
+		'elements'			=>	'Elementos que deben indicarse ',
+		'mail'				=>	'enviar por email',
+		'compteur'			=>	'muchas reacciones ',
+	);
+
+?>
---
+++ _test_/outils_article/lang/outilsarticle_fr.php
@@ -0,0 +1,20 @@
+<?php
+
+	$GLOBALS[$GLOBALS['idx_lang']] = array(
+		'titre'				=>	'Boite à outils',
+		'taille'			=>	'Taille',
+		'pdf'				=>	'Format pdf',
+		'impression'		=>	'Impression',
+		'impression_title'	=>	'version imprimable de l\'article',
+		'auteur'			=>	'Du même auteur',
+		'reaction'			=>	'Réactions',
+		'reagir'			=>	'Réagir à l\'article',
+		'config_titre'		=>	'Boite à outils pour article',
+		'config_descr'		=>	'vous pouvez configurer les éléments à afficher dans la boite à outils',
+		'elements'			=>	'Eléments à afficher',
+		'mail'				=>	'envoyer par mail',
+		'compteur'			=>	'nombre de réactions',
+		
+	);
+
+?>
