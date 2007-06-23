36a2f1b0db189f1b546101af4f81a699f427c1f1
james@rezo.net <james@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-06-23 08:55:16 +0000

    un petit gadget pour faire des boucles sympas avec spip1.9.3
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@13070 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/agenda/1_9_2/agenda_mes_options.php
+++ _stable_/agenda/1_9_2/agenda_mes_options.php
@@ -3,6 +3,9 @@ $p=explode(basename(_DIR_PLUGINS)."/",str_replace('\\','/',realpath(dirname(__FI
 define('_DIR_PLUGIN_AGENDA',(_DIR_PLUGINS.end($p)));
 include_spip('base/agenda_evenements');
 
+//Pour 1.9.3: permet d'utiliser les criteres racine, meme_parent, id_parent
+$exceptions_des_tables['evenements']['id_parent']='id_evenement_source';
+
 function cron_agenda_nettoyer_base($t){
 	# les evenements lies a un article inexistant
 	$res = spip_query("SELECT evenements.id_evenement,evenements.id_article
