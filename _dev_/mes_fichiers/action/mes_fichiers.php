a4f96393a4fe6e5b993273017b536197633fd903
james@rezo.net <james@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-06-25 21:04:46 +0000

    mmmpf... et puis, faudra passer la fonction autoriser() aussi...
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@13113 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/mes_fichiers/action/mes_fichiers.php
+++ _dev_/mes_fichiers/action/mes_fichiers.php
@@ -2,26 +2,11 @@
 
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
-include_spip('inc/actions');
-
-function action_mes_fichiers()
-{
-	global $connect_statut, $connect_toutes_rubriques;
-	$test = determine_upload();
-	$auth = charger_fonction('auth', 'inc');
-	$auth();
-	if (!($connect_statut == '0minirezo' and $connect_toutes_rubriques)) {
-		include_spip('inc/headers');
-		include_spip('inc/minipres');
-		http_status('403');
-		install_debut_html(_L('mes_fichiers.zip'));
-		echo _T('ecrire:avis_non_acces_page');
-		install_fin_html();
-		exit;
-	}
+function action_mes_fichiers() {
 	include_spip('inc/pclzip');
 	$mes_options = defined('_FILE_OPTIONS') ? _FILE_OPTIONS : 'ecrire/mes_options.php';
 	$IMG = defined('_DIR_IMG') ? _DIR_IMG: 'IMG/';
+	$tmp_dump = defined('_DIR_DUMP') ? _DIR_DUMP: 'tmp/dump/';
 	$liste = array();
 	if (@is_dir($IMG)) {
 		$liste[] = $IMG;
@@ -32,7 +17,7 @@ function action_mes_fichiers()
 	if (@is_readable($mes_options)) {
 		$liste[] = $mes_options;
 	}
-	$dump = preg_files(_DIR_DUMP);
+	$dump = preg_files($tmp_dump);
 	$fichier_dump = '';
 	$mtime = 0;
 	foreach ($dump as $_fichier_dump) {
@@ -51,7 +36,7 @@ function action_mes_fichiers()
 	if ($erreur == 0) {
 		die("Erreur : ".$mes_fichiers->errorInfo(true));
 	}
-	@header('Location: ecrire/?exec=admin_tech&mes_fichier=sauve_ok');
+	redirige_par_entete(generer_url_ecrire('admin_tech', 'mes_fichier=sauve_ok', true));
 }
 
 ?>
