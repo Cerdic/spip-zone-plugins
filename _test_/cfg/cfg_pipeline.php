a101cce8650f898dce8c53fa9b5a1011e6dddc33
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-06-29 14:58:57 +0000

    On a déjà le chemin du plugin via un define à partir de SPIP 1.9.2
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@13181 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/cfg/cfg_pipeline.php
+++ _test_/cfg/cfg_pipeline.php
@@ -1,7 +1,6 @@
 <?php
 function cfg_header_prive($texte) {
-	$p=explode(basename(_DIR_PLUGINS)."/",str_replace('\\','/',realpath(dirname(__FILE__))));
-	$texte.= '<link rel="stylesheet" type="text/css" href="' . (_DIR_PLUGINS.end($p)) . '/css/cfg.css" />' . "\n";
+	$texte.= '<link rel="stylesheet" type="text/css" href="' . _DIR_PLUGIN_CFG . 'css/cfg.css" />' . "\n";
 	return $texte;
 }
 ?>
