500a17b77c122242ed4217230aa6744a211fad50
fil@rezo.net <fil@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-07-01 15:53:57 +0000

    desinstallation correcte
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@13217 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/ortho/inc/ortho_install.php
+++ _stable_/ortho/inc/ortho_install.php
@@ -3,15 +3,21 @@
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 function Ortho_upgrade($nom_meta_base_version,$version_cible) {
+	spip_log("ortho_upgrade $nom_meta_base_version => $version_cible");
 	include_spip('base/ortho');
 	include_spip('base/create');
 	include_spip('base/abstract_sql');
 	creer_base();
+	ecrire_meta($nom_meta_base_version, $version_cible, 'non');
+	ecrire_metas();
 }
 
 function Ortho_vider_tables($nom_meta_base_version) {
+	spip_log("ortho_vider_tables");
 	spip_query("DROP TABLE spip_ortho_cache");
 	spip_query("DROP TABLE spip_ortho_dico");
+	effacer_meta($nom_meta_base_version);
+	ecrire_metas();
 }
 
 ?>
