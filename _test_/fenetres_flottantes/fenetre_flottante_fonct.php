330dff24360910c6207dede19837f095f9444c90
apachot@openstudio.fr <apachot@openstudio.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-07-03 12:28:01 +0000

    nouveau paramètre de configuration pour masquer la croix (fermeture) en haut à droite
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@13254 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/fenetres_flottantes/fenetre_flottante_fonct.php
+++ _test_/fenetres_flottantes/fenetre_flottante_fonct.php
@@ -33,19 +33,19 @@ function FenFlo_Affichage_fenetre($texte)
 {
 	$resultat;
 
-	if(preg_match('/(<div (id|class)="'.lire_config('FenFlo/reglage2_attribut_FenFlo','contenu').'">)/i',$texte))
+	if(preg_match('/(<div (id|class)="'.lire_config('FenFlo/attribut_FenFlo','contenu').'">)/i',$texte))
 	{
 	
-		ereg('<div (id|class)=("|\')'.lire_config('FenFlo/reglage2_attribut_FenFlo','contenu').'("|\')>(.*)',$texte,$resultat);
+		ereg('<div (id|class)=("|\')'.lire_config('FenFlo/attribut_FenFlo','contenu').'("|\')>(.*)',$texte,$resultat);
 		
 		$res = compter_div(substr($resultat[0], 4), 1);
 
-		$texte = preg_replace('/(<div (id|class)="'.lire_config('FenFlo/reglage2_attribut_FenFlo','contenu').'">)/i',"<div ".$resultat[1]."=\"".lire_config('FenFlo/reglage2_attribut_FenFlo','contenu')."\"> <div id=\"window\"> 
+		$texte = preg_replace('/(<div (id|class)="'.lire_config('FenFlo/attribut_FenFlo','contenu').'">)/i',"<div ".$resultat[1]."=\"".lire_config('FenFlo/attribut_FenFlo','contenu')."\"> <div id=\"window\"> 
 	<div id=\"windowTop\">
 	<div id=\"windowTopContent\"></div>
-		<img src=\""._DIR_PLUGINS."fenetres_flottantes/images/".lire_config('FenFlo/reglage1_couleur_FenFlo','vert')."/window_min.jpg\" class=\"format_png\" id=\"windowMin\" alt=\"minimiser la fenetre\" />
-		<img src=\""._DIR_PLUGINS."fenetres_flottantes/images/".lire_config('FenFlo/reglage1_couleur_FenFlo','vert')."/window_max.jpg\" class=\"format_png\" id=\"windowMax\" alt=\"agrandir la fenetre\"/>
-		<img src=\""._DIR_PLUGINS."fenetres_flottantes/images/".lire_config('FenFlo/reglage1_couleur_FenFlo','vert')."/window_close.jpg\" class=\"format_png\" id=\"windowClose\" alt=\"fermer la fenetre\"/>
+		<img src=\""._DIR_PLUGINS."fenetres_flottantes/images/".lire_config('FenFlo/couleur_FenFlo','vert')."/window_min.jpg\" class=\"format_png\" id=\"windowMin\" alt=\"minimiser la fenetre\" />
+		<img src=\""._DIR_PLUGINS."fenetres_flottantes/images/".lire_config('FenFlo/couleur_FenFlo','vert')."/window_max.jpg\" class=\"format_png\" id=\"windowMax\" alt=\"agrandir la fenetre\"/>
+		<img src=\""._DIR_PLUGINS."fenetres_flottantes/images/".lire_config('FenFlo/couleur_FenFlo','vert')."/window_close.jpg\" class=\"format_png\" id=\"windowClose\" alt=\"fermer la fenetre\"/>
 	</div>
 	<div id=\"windowBottom\"><div id=\"windowBottomContent\">&nbsp;</div></div>
 
@@ -53,7 +53,7 @@ function FenFlo_Affichage_fenetre($texte)
 	}
 
 		$fina="</div>
-		<img src=\""._DIR_PLUGINS."fenetres_flottantes/images/".lire_config('FenFlo/reglage1_couleur_FenFlo','vert')."/window_resize.gif\" class=\"format_png\"  id=\"windowResize\" alt=\"resize\" />
+		<img src=\""._DIR_PLUGINS."fenetres_flottantes/images/".lire_config('FenFlo/couleur_FenFlo','vert')."/window_resize.gif\" class=\"format_png\"  id=\"windowResize\" alt=\"resize\" />
 		</div> <a href=\"#\"  id=\"windowOpen\">&nbsp;</a>";
 
 		$texte=str_replace($res, $fina.$res, $texte);
@@ -69,46 +69,61 @@ return $texte;
 function modif_fenetre($arg)
 {
 	
-	$marge_haut= lire_config('FenFlo/reglage1_hauteur_FenFlo','300')-30;
-	$marge2_haut= lire_config('FenFlo/reglage1_hauteur_FenFlo','300')-45;
-	$marge_larg= lire_config('FenFlo/reglage1_largeur_FenFlo','400')-25;
-
+	$marge_haut= lire_config('FenFlo/hauteur_FenFlo','300')-30;
+	$marge2_haut= lire_config('FenFlo/hauteur_FenFlo','300')-45;
+	$marge_larg= lire_config('FenFlo/largeur_FenFlo','400')-25;
+	
+	$afficher_close = "none";
+	$pos_bouton_close = "10";
+	if (lire_config('FenFlo/close_FenFlo') == "on")
+	{
+		$afficher_close = "block";
+		$pos_bouton_close = "25";
+	}
 	if(preg_match('/(<\/head>)/i',$arg))
 	{
 	  $arg = preg_replace('/(<\/head>)/i',"<style type=\"text/css\" media=\"screen\">
 	  #window
 	  {
-	   left:".lire_config('FenFlo/reglage1_posx_FenFlo','100')."px;
-           top:".lire_config('FenFlo/reglage1_posy_FenFlo','100')."px;
-           width:".lire_config('FenFlo/reglage1_largeur_FenFlo','400')."px;
-	   height:".lire_config('FenFlo/reglage1_hauteur_FenFlo','300')."px;
+	   left:".lire_config('FenFlo/posx_FenFlo','100')."px;
+           top:".lire_config('FenFlo/posy_FenFlo','100')."px;
+           width:".lire_config('FenFlo/largeur_FenFlo','400')."px;
+	   height:".lire_config('FenFlo/hauteur_FenFlo','300')."px;
 	   }
           #windowBottom
  	  {
            	height:".$marge_haut."px;
-		background-image: url("._DIR_PLUGINS."fenetres_flottantes/images/".lire_config('FenFlo/reglage1_couleur_FenFlo','vert')."/window_bottom_end.png);  
+		background-image: url("._DIR_PLUGINS."fenetres_flottantes/images/".lire_config('FenFlo/couleur_FenFlo','vert')."/window_bottom_end.png);  
 	  }
 	  #windowBottomContent
 	  {
 	   	height:".$marge_haut."px;
-		background-image: url("._DIR_PLUGINS."fenetres_flottantes/images/".lire_config('FenFlo/reglage1_couleur_FenFlo','vert')."/window_bottom_start.png);
+		background-image: url("._DIR_PLUGINS."fenetres_flottantes/images/".lire_config('FenFlo/couleur_FenFlo','vert')."/window_bottom_start.png);
 	  }
           #windowContent
 	  {
 	   height:".$marge2_haut."px;
            width: ".$marge_larg."px;
-		border: 1px solid ".lire_config('FenFlo/reglage1_couleurbordure_FenFlo','#6caf00').";
+		border: 1px solid ".lire_config('FenFlo/couleurbordure_FenFlo','#6caf00').";
 	  }
 	  #windowTop
 	  {
-		background-image: url("._DIR_PLUGINS."fenetres_flottantes/images/".lire_config('FenFlo/reglage1_couleur_FenFlo','vert')."/window_top_end.png);
+		background-image: url("._DIR_PLUGINS."fenetres_flottantes/images/".lire_config('FenFlo/couleur_FenFlo','vert')."/window_top_end.png);
 
 	  }
 	  #windowTopContent
 	 {
-		background-image:url("._DIR_PLUGINS."fenetres_flottantes/images/".lire_config('FenFlo/reglage1_couleur_FenFlo','vert')."/window_top_start.png);
+		background-image:url("._DIR_PLUGINS."fenetres_flottantes/images/".lire_config('FenFlo/couleur_FenFlo','vert')."/window_top_start.png);
 
 	 }
+	#windowMax, #windowMin{
+		right: ".$pos_bouton_close."px;
+	}
+	#windowClose
+	{
+		display: ".$afficher_close.";
+		
+	}
           </style>
 	  </head>",$arg);
 	}
