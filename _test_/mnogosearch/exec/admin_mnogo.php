55776e02714d15f46f1db113a1f9e6320ed685b0
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-07-03 14:13:19 +0000

    "un bouton pour vider le cache"
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@13260 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/mnogosearch/exec/admin_mnogo.php
+++ _test_/mnogosearch/exec/admin_mnogo.php
@@ -38,6 +38,10 @@ function exec_admin_mnogo(){
 		ecrire_meta('mnogo_url_search',$mnogo_url_search);
 		ecrire_metas();
 	}
+	if (_request('vider_cache')!=NULL){
+		spip_query("DELETE FROM spip_mnogosearch");
+		spip_query("DELETE FROM spip_mnogosearch_summary");
+	}
 
 	$mnogo_url_search = isset($GLOBALS['meta']['mnogo_url_search'])?$GLOBALS['meta']['mnogo_url_search']:"";
 	echo "<div style='font:arial,helvetica,sans-serif;font-size:small;'>";
@@ -49,6 +53,18 @@ function exec_admin_mnogo(){
 	echo "<input type='submit' name='submit' value='"._T('Modifier')."' class='fondo' />";
 	echo "</p></div></form>";
 
+	echo generer_url_post_ecrire('admin_mnogo');
+	echo "<div style='font:arial,helvetica,sans-serif;font-size:small;'>";
+	$res = spip_query("SELECT COUNT(hash) AS n FROM spip_mnogosearch");
+	$nb_results = spip_fetch_array($res);
+	$nb_results = reset($nb_results);
+	$res = spip_query("SELECT COUNT(hash) AS n FROM spip_mnogosearch_summary");
+	$nb_recherches = spip_fetch_array($res);
+	$nb_recherches = reset($nb_recherches);
+	echo "La base contient en cache $nb_results r&eacute;sultats correspondants &agrave $nb_recherches recherches diff&eacute;rentes";
+	echo "<p style='text-align:right;'>";
+	echo "<input type='submit' name='vider_cache' value='"._T('bouton_vider_cache')."' class='fondo' />";
+	echo "</p></div></form>";
 
 	fin_page();
 }
