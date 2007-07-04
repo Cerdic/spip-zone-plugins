0c2f422135331fb97af43fe14ac805750a0fdeba
vincevg@free.fr <vincevg@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-07-04 14:53:11 +0000

    Poursuite du ménage. Plus besoin du 1.9.1 en dev.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@13294 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/fckeditor/1.9.1/spip_fck/plugins/geshighlighter/lang/nl.js
+++
@@ -1,5 +0,0 @@
-FCKLang['GeshighlighterHighlightTitle'] = 'Gekleure code invoeren';
-FCKLang['GeshighlighterSourceCode']     = 'Broncode';
-FCKLang['GeshighlighterLanguage']       = 'Programmeertaal';
-FCKLang['GeshighlighterShowLines']      = 'Laat regelnummers zien';
-FCKLang['GeshighlighterWaitMsg']        = 'Wachten...';
