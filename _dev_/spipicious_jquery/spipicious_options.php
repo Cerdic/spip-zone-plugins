ac0940e9b0bf6a8bc4687265d05ff83063cf31c6
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-07-08 23:02:21 +0000

    un debut de remise au propre (2)... cela devient dans l immediat une simple tag-machine... en jquery...
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@13365 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/spipicious_jquery/spipicious_options.php
+++ _dev_/spipicious_jquery/spipicious_options.php
@@ -1,8 +1,3 @@
 <?php
-if (!defined('_DIR_PLUGIN_SPIPICIOUS')){
-	$p=explode(basename(_DIR_PLUGINS)."/",str_replace('\\','/',realpath(dirname(__FILE__))));
-	define('_DIR_PLUGIN_SPIPICIOUS',(_DIR_PLUGINS.end($p)));
-}
 
-include_spip('base/spipicious');
 ?>
