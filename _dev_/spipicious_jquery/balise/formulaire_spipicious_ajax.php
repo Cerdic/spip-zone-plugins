ec12963390cf7dc3f31999bdc019114707808c56
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-07-09 11:54:03 +0000

    on a fait le menage... on peut ajouter des fonctionnalites maintenant CFG oblige
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@13374 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/spipicious_jquery/balise/formulaire_spipicious_ajax.php
+++ _dev_/spipicious_jquery/balise/formulaire_spipicious_ajax.php
@@ -6,13 +6,6 @@ function balise_FORMULAIRE_SPIPICIOUS_AJAX ($p) {
 	return calculer_balise_dynamique($p,'FORMULAIRE_SPIPICIOUS_AJAX', array('id_article'));
 }
 
-/*
-* La fonction statique retourne fait les verifications sur les variables r�up��s par collecte
-* ici, $args[0] contient donc id_article.
-*
-* ensuite en renvoi les arguments en questions pour la balise dynamique.
-* 
-*/
 function balise_FORMULAIRE_SPIPICIOUS_AJAX_stat($args, $filtres) {  
 	// Pas d'id_article ? Erreur de squelette 
 	if (!$args[0])
@@ -22,28 +15,30 @@ function balise_FORMULAIRE_SPIPICIOUS_AJAX_stat($args, $filtres) {
 					'motif' => 'ARTICLES')), '');
 	return $args;
 }
-/* la fonction dynamique fait les calculs et renvois le squelette qu'il faut afficher.
-_request() cherche dans les valeurs post.
-*/
 
-function balise_FORMULAIRE_SPIPICIOUS_AJAX_dyn($id_article) {
-
-	global $auteur_session;
-	$auteur_nom = $auteur_session['nom'];
-	$auteur_id = $auteur_session['id_auteur'];
-
+function balise_FORMULAIRE_SPIPICIOUS_AJAX_dyn($id_article ) {
+	$autorise = lire_config('spipicious/people');
+	
+	if (!$GLOBALS["auteur_session"] OR !in_array($GLOBALS['auteur_session']['statut'],$autorise)) {
+		return '';
+	} else {
+		$auteur_nom = $GLOBALS['auteur_session']['nom'];
+		$auteur_id = $GLOBALS['auteur_session']['email'];
+		$auteur_statut = $GLOBALS['auteur_session']['statut'];
+	}
+	
 	include_spip('spipicious_fonctions');
-	$msg = "** testing **";
 
 	//recuperation des variables utiles
-	$tags = _request('tags');  // FIXME traitement encodage ? 
-	//$id_article = _request('id_article');
-
+	$tags = _request('tags');
 	$id_groupe_tags = _request('select_groupe');
 	$type_groupe_tags = _request('type_groupe');
-
-	// action ?
-	if (_request('add_tags')&& $auteur_id)
+	$add_tags = _request('add_tags');
+	
+	$autorise = lire_config('spipicious/people');
+	
+	if ($add_tags && $auteur_id){
+	
 	spip_query("DELETE FROM spip_spipicious WHERE id_auteur='$auteur_id' AND id_article='$id_article' "); // on efface les anciens triplets de cet auteur sur cet article 
 
 	if ($tags && $auteur_id) {
@@ -54,45 +49,49 @@ function balise_FORMULAIRE_SPIPICIOUS_AJAX_dyn($id_article) {
 		foreach ($tableau_tags as $k=>$tag) {
 			$tag = trim($tag);
 
-			if ($tag!="" && !in_array($tag,$tag_analysed)) {   //  EXTRA verifier si ce tag n'est pas blackliste  
+			if ($tag!="" && !in_array($tag,$tag_analysed)) {
 
 			// doit on creer un nouveau tag ?
 			$result = spip_query("SELECT * FROM spip_mots AS mots WHERE titre='$tag' AND id_groupe=$id_groupe_tags");
 
 			if (spip_num_rows($result) == 0) { // creation tag
-				spip_log('mots inexistant');
-				$sql = "INSERT INTO spip_mots (titre,id_groupe,type,idx) VALUES(".spip_abstract_quote(corriger_caracteres($tag)).",$id_groupe_tags,'$type_groupe_tags', 'oui')"; // FIXME encodage caract�e ?
+				$exist = 'mot inexistant';
+				$sql = "INSERT INTO spip_mots (titre,id_groupe,type,idx) VALUES(".spip_abstract_quote(corriger_caracteres($tag)).",$id_groupe_tags,'$type_groupe_tags', 'oui')"; // FIXME encodage caractere ?
 				$result = spip_query($sql);
 				$id_tag = spip_insert_id();
 			} else {  // on recupere l'id du tag
-				spip_log('mots existant');
+				$exist = 'mot existant';
+				$log = spip_log($exist,'spipicious');
 				while($row=spip_fetch_array($result)){
 					$id_tag = $row['id_mot'];
+					spip_log($id_tag,'spipicious');
 				}
 			}
-
 			// on lie le mot au couple article (uniquement si pas deja fait)
 			$result = spip_query("SELECT id_mot FROM spip_mots_articles WHERE id_mot=$id_tag AND id_article=$id_article");
 			if (spip_num_rows($result) == 0) {
 				spip_query("INSERT INTO spip_mots_articles(id_mot,id_article) VALUES('$id_tag','$id_article')");
 			}
-
-			// auteur identifie:  on enregistre le couple (mot / article / auteur) ds la table spipicious
-			// FIXME verifier si table installe
+			
 			spip_query("INSERT INTO spip_spipicious(id_mot,id_auteur,id_article,position) VALUES('$id_tag','$auteur_id','$id_article','$position')");
 			$position++;
 			}
 		$tag_analysed[] = $tag;
 		}
 		}
+		}
+		include_spip ("inc/invalideur");
+		suivre_invalideur("1");
+		return header("Location: ".$_SERVER['HTTP_REFERER']);
 	}
-
-	spipicious_maintenance_nuage_article($id_article);
-
-	// retour sur le formulaire
-	include_spip ("inc/invalideur");
-	suivre_invalideur("1");
-	return header("Location: ".$_SERVER['HTTP_REFERER']);
+	else{
+		return array('formulaires/formulaire_spipicious_ajax', 0,
+			array('id' => $id_article,
+				'auteur_nom' => $auteur_nom,
+				'auteur_id' => $auteur_id,
+		));
+	}
+	
 }
 
 ?>
