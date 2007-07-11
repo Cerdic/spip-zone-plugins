00a68f253488b76275965a2c5baa065dd6b3da4f
apachot@openstudio.fr <apachot@openstudio.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-07-11 13:25:19 +0000

    - ajout de la barre typo sur la description du site (page configuration)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@13415 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/barre_typo_partout/typo_partout_define_prive.php
+++ _dev_/barre_typo_partout/typo_partout_define_prive.php
@@ -8,15 +8,19 @@ function typo_partout_insertion_in_body_prive ($texte)
 	$ajout_texte = "<script type=\"text/javascript\">
 		$(document).ready(function(){
 			
-			$('body.rubriques').find('.cadre-formulaire').find(\"form\").attr('name', 'formulaire');
-			$('body.mots').find('#page').find(\"form\").attr('name', 'formulaire');
-			$('#barre_typo_texte').insertBefore(\"textarea[@name=texte]\");
-			$('#barre_typo_descriptif').insertBefore(\"textarea[@name=descriptif]\");
+			$('form[textarea]').attr('name', 'formulaire');
+
+			$('body.rubriques').find('#barre_typo_texte').insertBefore(\"textarea[@name=texte]\");
+			$('body.rubriques').find('#barre_typo_descriptif').insertBefore(\"textarea[@name=descriptif]\");
+			$('body.mots').find('#barre_typo_texte').insertBefore(\"textarea[@name=texte]\");
+			$('body.mots').find('#barre_typo_descriptif').insertBefore(\"textarea[@name=descriptif]\");
+			$('body.configuration').find('#barre_typo_descriptif_site').insertBefore(\"textarea[@name=descriptif_site]\");
 			
 			$('body.rubriques').find('.cadre-formulaire').find('#barre_typo_texte').css(\"display\",\"block\");
 			$('body.rubriques').find('.cadre-formulaire').find('#barre_typo_descriptif').css(\"display\",\"block\");
 			$('body.mots').find('.cadre-formulaire').find('#barre_typo_texte').css(\"display\",\"block\");
 			$('body.mots').find('.cadre-formulaire').find('#barre_typo_descriptif').css(\"display\",\"block\");
+			$('body.configuration').find('.cadre-couleur').find('#barre_typo_descriptif_site').css(\"display\",\"block\");
 		});
 	</script><script type=\"text/javascript\" src=\"../dist/javascript/spip_barre.js\"></script>
 ";
@@ -24,6 +28,7 @@ function typo_partout_insertion_in_body_prive ($texte)
 	$barre_temporaire = "
 		<div id=\"barre_typo_texte\" style=\"display: none;\">".afficher_barre('document.formulaire.texte')."</div>
 		<div id=\"barre_typo_descriptif\" style=\"display: none;\">".afficher_barre('document.formulaire.descriptif')."</div>		
+		<div id=\"barre_typo_descriptif_site\" style=\"display: none;\">".afficher_barre('document.formulaire.descriptif_site')."</div>		
 	";
 
 	return $ajout_texte.$texte.$barre_temporaire;
