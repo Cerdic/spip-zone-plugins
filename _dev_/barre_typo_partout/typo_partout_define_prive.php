165896b224bdd804b33f9b54efe7da51ecedd225
apachot@openstudio.fr <apachot@openstudio.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-07-11 12:28:28 +0000

    un projet pour ajouter la barre typographique sur tous les textarea de l'interface d'administration - ajout de descriptif rubrique - ajout de texte rubrique
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@13413 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _dev_/barre_typo_partout/typo_partout_define_prive.php
@@ -0,0 +1,34 @@
+<?php
+
+function typo_partout_insertion_in_body_prive ($texte)
+{
+	include_spip('inc/barre');
+	include_spip('inc/presentation');
+	include_spip('inc/documents');
+	$ajout_texte = "<script type=\"text/javascript\">
+		$(document).ready(function(){
+			
+			$('body.rubriques').find('.cadre-formulaire').find(\"form\").attr('name', 'formulaire');
+			$('#barre_typo_texte').insertBefore(\"textarea[@name=texte]\");
+			$('#barre_typo_descriptif').insertBefore(\"textarea[@name=descriptif]\");
+			
+			$('body.rubriques').find('.cadre-formulaire').find('#barre_typo_texte').css(\"display\",\"block\");
+			$('body.rubriques').find('.cadre-formulaire').find('#barre_typo_descriptif').css(\"display\",\"block\");
+		});
+	</script><script type=\"text/javascript\" src=\"../dist/javascript/spip_barre.js\"></script>
+";
+
+	$barre_temporaire = "
+		<div id=\"barre_typo_texte\" style=\"display: none;\">".afficher_barre('document.formulaire.texte')."</div>
+		<div id=\"barre_typo_descriptif\" style=\"display: none;\">".afficher_barre('document.formulaire.descriptif')."</div>		
+	";
+
+	return $ajout_texte.$texte.$barre_temporaire;
+	
+}
+function typo_partout_insertion_in_head_prive ($texte)
+{
+	return $texte."<script type=\"text/javascript\" src=\"../dist/javascript/spip_barre.js\"></script>";
+}
+
+?>
