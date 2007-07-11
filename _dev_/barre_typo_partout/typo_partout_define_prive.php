c73b7ca6d7b379eb0e1ed088dbe6290a841cbe9d
apachot@openstudio.fr <apachot@openstudio.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-07-11 16:51:19 +0000

    - ajout de la barre typo sur le chapeau des articles - remise à plat des attributs "name" des formulaires, différents selon le type
    - en utilisation conjointe avec la BTV2, les statistiques et la prévisualisation ne fonctionnent pas. a voir. 
    les autres boutons semblent fonctionner.
    
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@13427 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/barre_typo_partout/typo_partout_define_prive.php
+++ _dev_/barre_typo_partout/typo_partout_define_prive.php
@@ -8,45 +8,60 @@ function typo_partout_insertion_in_body_prive ($texte)
 	$activer_barre_rubriques = "";
 	if (lire_config('typo_partout/rubriques_typo_partout') == "on")
 	{
-		$activer_barre_rubriques = "$('body.rubriques').find('#barre_typo_texte').insertBefore(\"textarea[@name=texte]\");
-		$('body.rubriques').find('#barre_typo_descriptif').insertBefore(\"textarea[@name=descriptif]\");
-		$('body.rubriques').find('.cadre-formulaire').find('#barre_typo_texte').css(\"display\",\"block\");
-		$('body.rubriques').find('.cadre-formulaire').find('#barre_typo_descriptif').css(\"display\",\"block\");";	
+		$activer_barre_rubriques = "$('body.rubriques').find('#barre_typo_rubrique_texte').insertBefore(\"textarea[@name=texte]\");
+		$('body.rubriques').find('#barre_typo_rubrique_descriptif').insertBefore(\"textarea[@name=descriptif]\");
+		$('body.rubriques').find('.cadre-formulaire').find('#barre_typo_rubrique_texte').css(\"display\",\"block\");
+		$('body.rubriques').find('.cadre-formulaire').find('#barre_typo_rubrique_descriptif').css(\"display\",\"block\");	
+		$('body.rubriques').find('form[textarea]').attr('name', 'formulaire_rubrique');";
 	}
 	$activer_barre_mots = "";
 	if (lire_config('typo_partout/mots_typo_partout') == "on")
 	{
-		$activer_barre_mots="$('body.mots').find('#barre_typo_texte').insertBefore(\"textarea[@name=texte]\");
-		$('body.mots').find('#barre_typo_descriptif').insertBefore(\"textarea[@name=descriptif]\");
-		$('body.mots').find('.cadre-formulaire').find('#barre_typo_texte').css(\"display\",\"block\");
-		$('body.mots').find('.cadre-formulaire').find('#barre_typo_descriptif').css(\"display\",\"block\");";
+		$activer_barre_mots="$('body.mots').find('#barre_typo_mot_texte').insertBefore(\"textarea[@name=texte]\");
+		$('body.mots').find('#barre_typo_mot_descriptif').insertBefore(\"textarea[@name=descriptif]\");
+		$('body.mots').find('.cadre-formulaire').find('#barre_typo_mot_texte').css(\"display\",\"block\");
+		$('body.mots').find('.cadre-formulaire').find('#barre_typo_mot_descriptif').css(\"display\",\"block\");
+		$('body.mots').find('form[textarea]').attr('name', 'formulaire_mot');";
 	}
 	$activer_barre_configuration = "";
 	if (lire_config('typo_partout/configuration_typo_partout') == "on")
 	{
-		$activer_barre_configuration="$('body.configuration').find('#barre_typo_descriptif_site').insertBefore(\"textarea[@name=descriptif_site]\");
-		$('body.configuration').find('.cadre-couleur').find('#barre_typo_descriptif_site').css(\"display\",\"block\");";
+		$activer_barre_configuration="$('body.configuration').find('#barre_typo_configuration_descriptif_site').insertBefore(\"textarea[@name=descriptif_site]\");
+		$('body.configuration').find('.cadre-couleur').find('#barre_typo_configuration_descriptif_site').css(\"display\",\"block\");
+		$('body.configuration').find('form[textarea]').attr('name', 'formulaire_configuration');";
 	}
 	$activer_barre_sites = "";
 	if (lire_config('typo_partout/sites_typo_partout') == "on")
 	{
-		$activer_barre_sites="$('body.sites').find('#barre_typo_descriptif').insertBefore(\"textarea[@name=descriptif]\");
-		$('body.sites').find('.cadre-formulaire').find('#barre_typo_descriptif').css(\"display\",\"block\");";
+		$activer_barre_sites="$('body.sites').find('#barre_typo_site_descriptif').insertBefore(\"textarea[@name=descriptif]\");
+		$('body.sites').find('.cadre-formulaire').find('#barre_typo_site_descriptif').css(\"display\",\"block\");
+		$('body.sites').find('form[textarea]').attr('name', 'formulaire_site');";
+	}
+	$activer_barre_articles = "";
+	if (lire_config('typo_partout/articles_typo_partout') == "on")
+	{
+		$activer_barre_articles="$('body.articles').find('#barre_typo_article_chapo').insertBefore(\"textarea[@name=chapo]\");
+		$('body.articles').find('.cadre-formulaire').find('#barre_typo_article_chapo').css(\"display\",\"block\");";
 	}
 	$ajout_texte = "<script type=\"text/javascript\">
 		$(document).ready(function(){
 			
-			$('form[textarea]').attr('name', 'formulaire');
-			".$activer_barre_rubriques.$activer_barre_mots.$activer_barre_configuration.$activer_barre_sites."
+			
+			".$activer_barre_rubriques.$activer_barre_mots.$activer_barre_configuration.$activer_barre_sites.$activer_barre_articles."
 			
 		});
 	</script><script type=\"text/javascript\" src=\"../dist/javascript/spip_barre.js\"></script>
 ";
 
 	$barre_temporaire = "
-		<div id=\"barre_typo_texte\" style=\"display: none;\">".afficher_barre('document.formulaire.texte')."</div>
-		<div id=\"barre_typo_descriptif\" style=\"display: none;\">".afficher_barre('document.formulaire.descriptif')."</div>		
-		<div id=\"barre_typo_descriptif_site\" style=\"display: none;\">".afficher_barre('document.formulaire.descriptif_site')."</div>		
+		<div id=\"barre_typo_rubrique_texte\" style=\"display: none;\">".afficher_barre('document.formulaire_rubrique.texte')."</div>
+		<div id=\"barre_typo_rubrique_descriptif\" style=\"display: none;\">".afficher_barre('document.formulaire_rubrique.descriptif')."</div>		
+		<div id=\"barre_typo_mot_texte\" style=\"display: none;\">".afficher_barre('document.formulaire_mot.texte')."</div>
+		<div id=\"barre_typo_mot_descriptif\" style=\"display: none;\">".afficher_barre('document.formulaire_mot.descriptif')."</div>		
+		<div id=\"barre_typo_site_descriptif\" style=\"display: none;\">".afficher_barre('document.formulaire_site.descriptif')."</div>		
+		<div id=\"barre_typo_configuration_descriptif_site\" style=\"display: none;\">".afficher_barre('document.formulaire_configuration.descriptif_site')."</div>		
+		<div id=\"barre_typo_article_chapo\" style=\"display: none;\">".afficher_barre('document.formulaire.chapo')."</div>
+		
 	";
 
 	return $ajout_texte.$texte.$barre_temporaire;
