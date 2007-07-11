0c533ce58001f77e6a425eac51e1bf5ec0ca9fd7
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-07-11 23:31:55 +0000

    resolving problems using it in public area with geoforms ... please say me (Horacio, Berio or other users) if it costs a lot in perfs. (I am not using the Gis plugin)...
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@13443 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/gis/googlemap_api/inc/geomap_script_init.php
+++ _dev_/gis/googlemap_api/inc/geomap_script_init.php
@@ -12,7 +12,11 @@
 function inc_geomap_script_init_dist(){
 	static $deja_insere = false;
 	if ($deja_insere) return "";
-	return '<script type="text/javascript" src="'.generer_url_public('geomap.js').'"></script>';
+	$config = lire_meta('geomap_googlemapkey');
+	$geomap = compacte_js(find_in_path('js/geomap.js'));
+	$gmap_script = compacte_js(recuperer_page('http://maps.google.com/maps?file=api&v=2&key='.$config));
+	return '<script type="text/javascript" src="'.$geomap.'"></script>'
+	.'<script type="text/javascript">'.$gmap_script.'</script>';
 }
 
 ?>
