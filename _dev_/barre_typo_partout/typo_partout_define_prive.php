140ade3974591c172e711bf873a1f89656e12872
apachot@openstudio.fr <apachot@openstudio.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-07-11 16:05:53 +0000

    configuration avec cfg
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@13426 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/barre_typo_partout/typo_partout_define_prive.php
+++ _dev_/barre_typo_partout/typo_partout_define_prive.php
@@ -5,24 +5,39 @@ function typo_partout_insertion_in_body_prive ($texte)
 	include_spip('inc/barre');
 	include_spip('inc/presentation');
 	include_spip('inc/documents');
+	$activer_barre_rubriques = "";
+	if (lire_config('typo_partout/rubriques_typo_partout') == "on")
+	{
+		$activer_barre_rubriques = "$('body.rubriques').find('#barre_typo_texte').insertBefore(\"textarea[@name=texte]\");
+		$('body.rubriques').find('#barre_typo_descriptif').insertBefore(\"textarea[@name=descriptif]\");
+		$('body.rubriques').find('.cadre-formulaire').find('#barre_typo_texte').css(\"display\",\"block\");
+		$('body.rubriques').find('.cadre-formulaire').find('#barre_typo_descriptif').css(\"display\",\"block\");";	
+	}
+	$activer_barre_mots = "";
+	if (lire_config('typo_partout/mots_typo_partout') == "on")
+	{
+		$activer_barre_mots="$('body.mots').find('#barre_typo_texte').insertBefore(\"textarea[@name=texte]\");
+		$('body.mots').find('#barre_typo_descriptif').insertBefore(\"textarea[@name=descriptif]\");
+		$('body.mots').find('.cadre-formulaire').find('#barre_typo_texte').css(\"display\",\"block\");
+		$('body.mots').find('.cadre-formulaire').find('#barre_typo_descriptif').css(\"display\",\"block\");";
+	}
+	$activer_barre_configuration = "";
+	if (lire_config('typo_partout/configuration_typo_partout') == "on")
+	{
+		$activer_barre_configuration="$('body.configuration').find('#barre_typo_descriptif_site').insertBefore(\"textarea[@name=descriptif_site]\");
+		$('body.configuration').find('.cadre-couleur').find('#barre_typo_descriptif_site').css(\"display\",\"block\");";
+	}
+	$activer_barre_sites = "";
+	if (lire_config('typo_partout/sites_typo_partout') == "on")
+	{
+		$activer_barre_sites="$('body.sites').find('#barre_typo_descriptif').insertBefore(\"textarea[@name=descriptif]\");
+		$('body.sites').find('.cadre-formulaire').find('#barre_typo_descriptif').css(\"display\",\"block\");";
+	}
 	$ajout_texte = "<script type=\"text/javascript\">
 		$(document).ready(function(){
 			
 			$('form[textarea]').attr('name', 'formulaire');
-
-			$('body.rubriques').find('#barre_typo_texte').insertBefore(\"textarea[@name=texte]\");
-			$('body.rubriques').find('#barre_typo_descriptif').insertBefore(\"textarea[@name=descriptif]\");
-			$('body.mots').find('#barre_typo_texte').insertBefore(\"textarea[@name=texte]\");
-			$('body.mots').find('#barre_typo_descriptif').insertBefore(\"textarea[@name=descriptif]\");
-			$('body.configuration').find('#barre_typo_descriptif_site').insertBefore(\"textarea[@name=descriptif_site]\");
-			$('body.sites').find('#barre_typo_descriptif').insertBefore(\"textarea[@name=descriptif]\");
-
-			$('body.rubriques').find('.cadre-formulaire').find('#barre_typo_texte').css(\"display\",\"block\");
-			$('body.rubriques').find('.cadre-formulaire').find('#barre_typo_descriptif').css(\"display\",\"block\");
-			$('body.mots').find('.cadre-formulaire').find('#barre_typo_texte').css(\"display\",\"block\");
-			$('body.mots').find('.cadre-formulaire').find('#barre_typo_descriptif').css(\"display\",\"block\");
-			$('body.configuration').find('.cadre-couleur').find('#barre_typo_descriptif_site').css(\"display\",\"block\");
-			$('body.sites').find('.cadre-formulaire').find('#barre_typo_descriptif').css(\"display\",\"block\");
+			".$activer_barre_rubriques.$activer_barre_mots.$activer_barre_configuration.$activer_barre_sites."
 			
 		});
 	</script><script type=\"text/javascript\" src=\"../dist/javascript/spip_barre.js\"></script>
