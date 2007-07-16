74ba0ab30b960c1737263ecf4f26769a22bfe5d7
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-07-16 13:29:40 +0000

    Mise au carré des fichiers de langue pour compatibilité avec la BTv2
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@13522 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/enluminures_typographiques_v2/barre_typo_enrichie_pipelines.php
+++ _stable_/enluminures_typographiques_v2/barre_typo_enrichie_pipelines.php
@@ -4,12 +4,12 @@
 function afficher_boutonsavances($champ, $champhelp) {
 
 $reta = '';
-$reta .= bouton_barre_racc ("barre_raccourci('\n\n{2{','}2}\n\n',$champ)", _DIR_PLUGIN_TYPOENLUMINEE.'/img_pack/icones_barre/intertitre2.png', _T('bartypenr:barre_intertitre2'), $champhelp);
-$reta .= bouton_barre_racc ("barre_raccourci('\n\n{3{','}3}\n\n',$champ)", _DIR_PLUGIN_TYPOENLUMINEE.'/img_pack/icones_barre/intertitre3.png', _T('bartypenr:barre_intertitre3'), $champhelp);
-$reta .= bouton_barre_racc ("barre_raccourci('[|','|]',$champ)", _DIR_PLUGIN_TYPOENLUMINEE.'/img_pack/icones_barre/center.png', _T('bartypenr:barre_centrer'), $champhelp);
-$reta .= bouton_barre_racc ("barre_raccourci('[/','/]',$champ)", _DIR_PLUGIN_TYPOENLUMINEE.'/img_pack/icones_barre/right.png', _T('bartypenr:barre_alignerdroite'), $champhelp);
-$reta .= bouton_barre_racc ("barre_raccourci('[(',')]',$champ)", _DIR_PLUGIN_TYPOENLUMINEE.'/img_pack/icones_barre/cadretexte.png', _T('bartypenr:barre_encadrer'), $champhelp);
-$reta .= bouton_barre_racc ("barre_raccourci('<poesie>','</poesie>',$champ)", _DIR_PLUGIN_TYPOENLUMINEE."/img_pack/icones_barre/poesie.png", _T('bartypenr:barre_poesie'), $champhelp);
+$reta .= bouton_barre_racc ("barre_raccourci('\n\n{2{','}2}\n\n',$champ)", _DIR_PLUGIN_TYPOENLUMINEE.'/img_pack/icones_barre/intertitre2.png', _T('enlumtypo:barre_intertitre2'), $champhelp);
+$reta .= bouton_barre_racc ("barre_raccourci('\n\n{3{','}3}\n\n',$champ)", _DIR_PLUGIN_TYPOENLUMINEE.'/img_pack/icones_barre/intertitre3.png', _T('enlumtypo:barre_intertitre3'), $champhelp);
+$reta .= bouton_barre_racc ("barre_raccourci('[|','|]',$champ)", _DIR_PLUGIN_TYPOENLUMINEE.'/img_pack/icones_barre/center.png', _T('enlumtypo:barre_centrer'), $champhelp);
+$reta .= bouton_barre_racc ("barre_raccourci('[/','/]',$champ)", _DIR_PLUGIN_TYPOENLUMINEE.'/img_pack/icones_barre/right.png', _T('enlumtypo:barre_alignerdroite'), $champhelp);
+$reta .= bouton_barre_racc ("barre_raccourci('[(',')]',$champ)", _DIR_PLUGIN_TYPOENLUMINEE.'/img_pack/icones_barre/cadretexte.png', _T('enlumtypo:barre_encadrer'), $champhelp);
+$reta .= bouton_barre_racc ("barre_raccourci('<poesie>','</poesie>',$champ)", _DIR_PLUGIN_TYPOENLUMINEE."/img_pack/icones_barre/poesie.png", _T('enlumtypo:barre_poesie'), $champhelp);
 	global $spip_lang;
 	$params = array($champ,$champhelp,$spip_lang);
 	$add = pipeline("BarreTypoEnrichie_boutonsavances",array($champ,$champhelp,$spip_lang));
@@ -21,7 +21,7 @@ $reta .= '&nbsp;';
 $tableau_formulaire = '
 <table class="spip_barre" style="width: auto; padding: 1px!important; border-top: 0px;" summary="">
   <tr class="spip_barre">
-    <td>'._T('bartypenr:barre_avances').'</td>
+    <td>'._T('enlumtypo:barre_avances').'</td>
     <td>'.$reta.'
     </td>
   </tr> 
@@ -36,10 +36,10 @@ function TypoEnluminee_BarreTypoEnrichie_toolbox($paramArray) {
 }
 
 function TypoEnluminee_BarreTypoEnrichie_avancees($paramArray) {
-	$ret = bouton_barre_racc ("barre_raccourci('[*','*]',".$paramArray[0].")", _DIR_PLUGIN_TYPOENLUMINEE.'/img_pack/icones_barre/miseenevidence.png', _T('bartypenr:barre_miseenevidence'), $paramArray[1]);
-	$ret .= bouton_barre_racc ("barre_raccourci('&lt;sup&gt;','&lt;/sup&gt;',".$paramArray[0].")", _DIR_PLUGIN_TYPOENLUMINEE.'/img_pack/icones_barre/exposant.png', _T('bartypenr:barre_exposant'), $paramArray[1]);
-	$ret .= bouton_barre_racc ("barre_raccourci('&lt;sc&gt;','&lt;/sc&gt;',".$paramArray[0].")", _DIR_PLUGIN_TYPOENLUMINEE.'/img_pack/icones_barre/petitescapitales.png', _T('bartypenr:barre_petitescapitales'), $paramArray[1]);
-	$ret .= bouton_barre_racc("swap_couche('".$GLOBALS['numero_block']['tableau_boutonsavances']."','');", _DIR_PLUGIN_TYPOENLUMINEE."/img_pack/icones_barre/avances.png", _T('bartypenr:barre_boutonsavances'), $paramArray[1]);
+	$ret = bouton_barre_racc ("barre_raccourci('[*','*]',".$paramArray[0].")", _DIR_PLUGIN_TYPOENLUMINEE.'/img_pack/icones_barre/miseenevidence.png', _T('enlumtypo:barre_miseenevidence'), $paramArray[1]);
+	$ret .= bouton_barre_racc ("barre_raccourci('&lt;sup&gt;','&lt;/sup&gt;',".$paramArray[0].")", _DIR_PLUGIN_TYPOENLUMINEE.'/img_pack/icones_barre/exposant.png', _T('enlumtypo:barre_exposant'), $paramArray[1]);
+	$ret .= bouton_barre_racc ("barre_raccourci('&lt;sc&gt;','&lt;/sc&gt;',".$paramArray[0].")", _DIR_PLUGIN_TYPOENLUMINEE.'/img_pack/icones_barre/petitescapitales.png', _T('enlumtypo:barre_petitescapitales'), $paramArray[1]);
+	$ret .= bouton_barre_racc("swap_couche('".$GLOBALS['numero_block']['tableau_boutonsavances']."','');", _DIR_PLUGIN_TYPOENLUMINEE."/img_pack/icones_barre/avances.png", _T('enlumtypo:barre_boutonsavances'), $paramArray[1]);
 
     return $ret;   
 }
---
+++ _stable_/enluminures_typographiques_v2/lang/enlumtypo_fr.php
@@ -0,0 +1,26 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// Aide des boutons supplémentaires de la barre typo
+'barre_intertitre2' => 'Transformer en {2{intertitre niveau deux}2}',
+'barre_intertitre3' => 'Transformer en {3{intertitre niveau trois}3}',
+'barre_miseenevidence' => 'Mettre le texte en [*&eacute;vidence*]',
+'barre_exposant' => 'Mettre le texte en &lt;sup&gt;exposant&lt;/sup&gt;',
+'barre_petitescapitales' => 'Mettre le texte en &lt;sc&gt;petites capitales&lt;/sc&gt;',
+'barre_centrer' => '[|Centrer|] le paragraphe',
+'barre_alignerdroite' => '[/Aligne &agrave; droite/] le paragraphe',
+'barre_encadrer' => '[(Encadrer)] le paragraphe',
+
+'barre_poesie' => 'Mettre en forme comme une &lt;poesie&gt;po&eacute;sie&lt;/poesie&gt;',
+
+'barre_avances' => 'Du sens, du sens&nbsp;!',
+'barre_boutonsavances' => 'Mises en sens suppl&eacute;mentaires, &agrave; utiliser avec mod&eacute;ration et discernement&nbsp;!',
+
+'cfg_puces' => 'Traitement des puces',
+'cfg_titraille' => 'Titraille',
+'cfg_insertcss' => 'Insertion CSS'
+
+);
+?>
