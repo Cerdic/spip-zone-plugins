4d07bf53c17f952830f0b111099a587ddd4a311c
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-07-18 14:55:59 +0000

    on devient un peut configurable
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@13620 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/formulaire_upload/lang/formupload_en.php
+++ _dev_/formulaire_upload/lang/formupload_en.php
@@ -4,6 +4,7 @@
 	'yourfiles' => 'Your files',
 	'choosefiles' => 'Choose files to upload&nbsp;:',
 	'nodocs' => 'No document at that time in the upload zone',
+	'insert_code' => 'Code to insert in the textarea&nbsp;:',
 	'Z' => 'ZZzZZzzz'
 
 );
--- _dev_/formulaire_upload/lang/formupload_fr.php
+++ _dev_/formulaire_upload/lang/formupload_fr.php
@@ -4,6 +4,7 @@
 	'yourfiles' => 'Vos fichiers',
 	'choosefiles' => 'Choisir les fichiers à ajouter&nbsp;:',
 	'nodocs' => 'Pas encore de document sur la zone de t&eacute;l&eacute:chargement',
+	'insert_code' => 'Code &agrave; ins&eacute;rer dans la zone de texte&nbsp;:',
 	'Z' => 'ZZzZZzzz'
 
 );
