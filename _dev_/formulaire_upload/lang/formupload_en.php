fbaab48654d94235ca15a2a69d211726d8466f2b
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-07-18 16:24:51 +0000

    un peu d'aide?
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@13624 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/formulaire_upload/lang/formupload_en.php
+++ _dev_/formulaire_upload/lang/formupload_en.php
@@ -5,6 +5,13 @@
 	'choosefiles' => 'Choose files to upload&nbsp;:',
 	'nodocs' => 'No document at that time in the upload zone',
 	'insert_code' => 'Code to insert in the textarea&nbsp;:',
+	'doc_delete' => 'Delete this document',
+	'doc_left' => 'Document floating left',
+	'doc_center' => 'Document centered',
+	'doc_right' => 'Document floating right',
+	'img_left' => 'Image floating left',
+	'img_center' => 'Image centered',
+	'img_right' => 'Image floating right',
 	'Z' => 'ZZzZZzzz'
 
 );
--- _dev_/formulaire_upload/lang/formupload_fr.php
+++ _dev_/formulaire_upload/lang/formupload_fr.php
@@ -5,6 +5,13 @@
 	'choosefiles' => 'Choisir les fichiers à ajouter&nbsp;:',
 	'nodocs' => 'Pas encore de document sur la zone de t&eacute;l&eacute:chargement',
 	'insert_code' => 'Code &agrave; ins&eacute;rer dans la zone de texte&nbsp;:',
+	'doc_delete' => 'Supprimer ce document',
+	'doc_left' => 'Document flottant &agrave; gauche',
+	'doc_center' => 'Document centr&eacute;',
+	'doc_right' => 'Document flottant &agrave; droite',
+	'img_left' => 'Image flottante &agrave; gauche',
+	'img_center' => 'Image centr&eacute;e',
+	'img_right' => 'Image flottante &agrave; droite',
 	'Z' => 'ZZzZZzzz'
 
 );
