a3587540c6d87efe5bb3fdc65059a93a1fb8d5ea
james@rezo.net <james@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-07-19 08:39:50 +0000

    La surchage de la balise #CACHE fonctionne bien.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@13651 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/balise_session/public/session_balises.php
+++ _stable_/balise_session/public/session_balises.php
@@ -10,10 +10,41 @@
 \***************************************************************************/
 
 function balise_SESSION_dist($p) {
+	$p->entetes['X-Session'] = spip_session();
+	
 	if(function_exists('balise_ENV'))
 		return balise_ENV($p, '$GLOBALS["auteur_session"]');
 	else
 		return balise_ENV_dist($p, '$GLOBALS["auteur_session"]');
+}
+
+//
+// #CACHE
+// avec le systeme de gestion d'entetes de ce plugin
+function balise_CACHE($p) {
+	$duree = valeur_numerique($p->param[0][1][0]->texte);
+
+	// noter la duree du cache dans un entete proprietaire
+	$p->entetes['X-Spip-Cache'] = $duree;
+
+	// Remplir le header Cache-Control
+	// cas #CACHE{0}
+	if ($duree == 0) {
+		$p->entetes['Cache-Control'] = 'no-store, no-cache, must-revalidate';
+		$p->entetes['Pragma'] = 'no-cache';
+	}
+
+	// cas #CACHE{360, cache-client}
+	if (isset($p->param[0][2])) {
+		$second = ($p->param[0][2][0]->texte);
+		if ($second == 'cache-client'
+		AND $duree > 0)
+			$p->entetes['Cache-Control'] = 'max-age='.$duree;
+	}
+
+	$p->code = "''";
+	$p->interdire_scripts = false;
+	return $p;
 }
 
 ?>
