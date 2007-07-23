c4b0643af5e74e7243d6a9657a6d888b26883c3c
fdm@ufe.be <fdm@ufe.be@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-07-23 22:10:32 +0000

    categories newlook
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@13777 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/Association/Association_1.9.2/exec/categories.php
+++ _test_/Association/Association_1.9.2/exec/categories.php
@@ -1,69 +1,71 @@
 <?php
-/**
-* Plugin Association
-*
-* Copyright (c) 2007
-* Bernard Blazin & François de Montlivault
-* http://www.plugandspip.com 
-* Ce programme est un logiciel libre distribue sous licence GNU/GPL.
-* Pour plus de details voir le fichier COPYING.txt.
-*  
-**/
-include_spip('inc/presentation');
-function exec_categories(){
-global $connect_statut, $connect_toutes_rubriques;
-
-debut_page(_T('Cat&eacute;gories de cotisation'), "", "");
-$url_categories = generer_url_ecrire('categories');
-$url_ajout_categorie=generer_url_ecrire('ajout_categorie');
-$url_edit_categorie=generer_url_ecrire('edit_categorie');
-$url_action_categorie=generer_url_ecrire('action_categorie');
-
-include_spip ('inc/navigation');
-
-debut_cadre_relief(  "", false, "", $titre = _T('Toutes les cat&eacute;gories de cotisation'));
-	debut_boite_info();
-
-print association_date_du_jour();
-
-echo '<table width="70%" border="0">';
-echo '<tr bgcolor="#D9D7AA">';
-echo '<td><strong>ID</strong></td>';
-echo '<td><strong>Cat&eacute;gorie</strong></td>';
-echo '<td><strong>Libell&eacute; complet</strong></td>';
-echo '<td><strong>Dur&eacute;e (mois)</strong></td>';
-echo '<td><strong>Montant</strong></td>';
-echo '<td><strong>Commentaires</strong></td>';
-echo '<td colspan=2 style="text-align:center;"><strong>Action</strong></td>';
-echo'  </tr>';
-$query = spip_query ( "SELECT * FROM spip_asso_categories ORDER by id_categorie" ) ;
-$class="pair";
-while ($data = spip_fetch_array($query))
-
-{
-echo '<tr> ';
-echo '<td class ='.$class.'>'.$data['id_categorie'].'</td>';
-echo '<td class ='.$class.'>'.$data['valeur'].'</td>';
-echo '<td class ='.$class.'>'.$data['libelle'].'</td>';
-echo '<td class ='.$class.'>'.$data['duree'].'</td>';
-echo '<td class ='.$class.'>'.$data['cotisation'].'</td>';
-echo '<td class ='.$class.'>'.$data['commentaires'].'</td>';
-echo '<td class ='.$class.' style="text-align:center;"><a href="'.$url_action_categorie.'&action=supprime&id='.$data['id_categorie'].'"><img src="'._DIR_PLUGIN_ASSOCIATION.'/img_pack/poubelle-12.gif" title="Supprimer"></a></td>';
-echo '<td class ='.$class.' style="text-align:center;"><a href="'.$url_edit_categorie.'&id='.$data['id_categorie'].'"><img src="'._DIR_PLUGIN_ASSOCIATION.'/img_pack/edit-12.gif" title="Modifier"></a></td>';
-echo'  </tr>';
- }     
-echo'</table>';
-
-echo '<div align="center"><br>';
-echo '<form action="'.$url_ajout_categorie.'" method="POST">';
-echo '<input type="submit" name="Submit" value="Ajouter" class="fondo">';
-echo '<br>';
-echo '</div>';
-
-fin_boite_info();
-
-fin_cadre_relief();  
-
-fin_page();
-}
+	/**
+	* Plugin Association
+	*
+	* Copyright (c) 2007
+	* Bernard Blazin & François de Montlivault
+	* http://www.plugandspip.com 
+	* Ce programme est un logiciel libre distribue sous licence GNU/GPL.
+	* Pour plus de details voir le fichier COPYING.txt.
+	*  
+	**/
+	
+	include_spip('inc/presentation');
+	
+	function exec_categories(){
+		global $connect_statut, $connect_toutes_rubriques;
+		
+		debut_page(_T('Cat&eacute;gories de cotisation'), "", "");
+		
+		$url_categories = generer_url_ecrire('categories');
+		$url_ajout_categorie=generer_url_ecrire('ajout_categorie');
+		$url_edit_categorie=generer_url_ecrire('edit_categorie');
+		$url_action_categorie=generer_url_ecrire('action_categorie');
+		
+		debut_gauche();
+		
+		debut_boite_info();
+		echo '<p></p>';
+		fin_boite_info();
+		
+		debut_raccourcis();
+		icone_horizontale("Ajout", generer_url_ecrire("edit_categorie","action=ajoute"), _DIR_PLUGIN_ASSOCIATION."/img_pack/calculatrice.gif","cree.gif");	
+		fin_raccourcis();
+		
+		if ($connect_statut == '0minirezo') {
+		include_spip ('inc/navigation');
+		}
+		
+		debut_droite();
+		
+		debut_cadre_relief(  "../"._DIR_PLUGIN_ASSOCIATION."/img_pack/calculatrice.gif", false, "", $titre = _T('Cat&eacute;gories de cotisation'));
+		
+		echo "<table border=0 cellpadding=2 cellspacing=0 width='100%' class='arial2' style='border: 1px solid #aaaaaa;'>\n";
+		echo "<tr bgcolor='#DBE1C5'>";
+		echo '<td><strong>ID</strong></td>';
+		echo '<td><strong>Cat&eacute;gorie</strong></td>';
+		echo '<td><strong>Libell&eacute; complet</strong></td>';
+		echo '<td><strong>Dur&eacute;e (mois)</strong></td>';
+		echo '<td><strong>Montant</strong></td>';
+		echo '<td><strong>Commentaires</strong></td>';
+		echo '<td colspan=2 style="text-align:center;"><strong>Action</strong></td>';
+		echo'  </tr>';
+		$query = spip_query ( "SELECT * FROM spip_asso_categories ORDER by id_categorie" ) ;
+		while ($data = spip_fetch_array($query)) {
+			echo '<tr style="background-color: #EEEEEE;">';
+			echo '<td  class="arial11" style="border-top: 1px solid #CCCCCC;text-align:right">'.$data['id_categorie'].'</td>';
+			echo '<td  class="arial11" style="border-top: 1px solid #CCCCCC;">'.$data['valeur'].'</td>';
+			echo '<td  class="arial11" style="border-top: 1px solid #CCCCCC;">'.$data['libelle'].'</td>';
+			echo '<td  class="arial11" style="border-top: 1px solid #CCCCCC;text-align:right">'.$data['duree'].'</td>';
+			echo '<td  class="arial11" style="border-top: 1px solid #CCCCCC;text-align:right">'.$data['cotisation'].'</td>';
+			echo '<td  class="arial11" style="border-top: 1px solid #CCCCCC;">'.$data['commentaires'].'</td>';
+			echo '<td  class="arial11" style="border-top: 1px solid #CCCCCC;text-align:center;"><a href="'.$url_action_categorie.'&action=supprime&id='.$data['id_categorie'].'"><img src="'._DIR_PLUGIN_ASSOCIATION.'/img_pack/poubelle-12.gif" title="Supprimer"></a></td>';
+			echo '<td  class="arial11" style="border-top: 1px solid #CCCCCC;text-align:center;"><a href="'.$url_edit_categorie.'&action=modifie&id='.$data['id_categorie'].'"><img src="'._DIR_PLUGIN_ASSOCIATION.'/img_pack/edit-12.gif" title="Modifier"></a></td>';
+			echo'  </tr>';
+		}     
+		echo'</table>';
+		
+		fin_cadre_relief();  		
+		fin_page();
+	}
 ?>
--- _test_/Association/Association_1.9.2/exec/edit_categorie.php
+++ _test_/Association/Association_1.9.2/exec/edit_categorie.php
@@ -1,77 +1,80 @@
 <?php
-/**
-* Plugin Association
-*
-* Copyright (c) 2007
-* Bernard Blazin & François de Montlivault
-* http://www.plugandspip.com 
-* Ce programme est un logiciel libre distribue sous licence GNU/GPL.
-* Pour plus de details voir le fichier COPYING.txt.
-*  
-**/
-include_spip('inc/presentation');
-function exec_edit_categorie(){
-global $connect_statut, $connect_toutes_rubriques;
-
-debut_page(_T('Cat&eacute;gories de cotisation'), "", "");
-$url_asso = generer_url_ecrire('association');
-$url_ajouter = generer_url_ecrire('ajouter');
-$url_relance = generer_url_ecrire('essai');
-$url_bienfaiteur = generer_url_ecrire('bienfaiteur');
-$url_vente = generer_url_ecrire('ventes');
-$url_banque = generer_url_ecrire('banque');
-$url_delete = generer_url_ecrire('delete_membre');
-$url_action_categorie=generer_url_ecrire('action_categorie');
-
-include_spip ('inc/navigation');
-
-debut_cadre_relief(  "", false, "", $titre = _T('Toutes les cat&eacute;gories de cotisation'));
-	debut_boite_info();
-
-print association_date_du_jour();
-
-$action=$_GET['action'];
-$id=$_GET['id'];
-$query = spip_query( "SELECT * FROM spip_asso_categories WHERE id_categorie='$id' ");
+	/**
+	* Plugin Association
+	*
+	* Copyright (c) 2007
+	* Bernard Blazin & François de Montlivault
+	* http://www.plugandspip.com 
+	* Ce programme est un logiciel libre distribue sous licence GNU/GPL.
+	* Pour plus de details voir le fichier COPYING.txt.
+	*  
+	**/
 	
-echo '<fieldset><legend>Modifier une cat&eacute;gorie de cotisation</legend>';
-echo '<table width="70%">';	
-echo '<form action="'.$url_action_categorie.'" method="post">';	
-
-	while($data = spip_fetch_array($query)) 
-{
-echo '<tr> ';
-echo '<td>Num&eacute;ro :</td>';
-echo '<td><input name="id" type="text" size="3" readonly="true" value="'.$data['id_categorie'].'"></td></tr>';
-echo '<tr> ';
-echo '<td>Cat&eacute;gorie :</td>';
-echo '<td><input name="valeur" type="text" value="'.$data['valeur'].'"></td></tr>';
-echo '<tr> ';
-echo '<td>Libell&eacute; complet :</td>';
-echo '<td><input name="libelle" type="text" size="50" value="'.$data['libelle'].'"></td>';
-echo '<tr> ';
-echo '<td>Dur&eacute;e (en mois) :</td>';
-echo '<td><input name="duree" type="text" value="'.$data['duree'].'"></td>';
-echo '<tr> ';
-echo '<td>Montant (en euros) :</td>';
-echo '<td><input name="montant" type="text" value="'.$data['cotisation'].'"></td></tr>';
-echo '<tr> ';      
-echo '<td>Commentaires :</td>';
-echo '<td colspan="3"><textarea name="commentaires" cols="38" rows="3">'.$data["commentaires"].'</textarea>';
-echo '<input type="hidden" name="action" value="modifie"></td></tr>';
-}
-echo '<tr>';
-echo '<td></td>';
-echo '<td><input name="submit" type="submit" value="Modifier" class="fondo"></td></tr>';
-echo '</form>';
-echo '</table>';
-echo '</fieldset>';
-
-
-fin_boite_info();
-
-fin_cadre_relief();  
-
-fin_page();
-}
+	include_spip('inc/presentation');
+	
+	function exec_edit_categorie(){
+		global $connect_statut, $connect_toutes_rubriques;
+		
+		debut_page(_T('Cat&eacute;gories de cotisation'), "", "");
+		$url_asso = generer_url_ecrire('association');
+		$url_ajouter = generer_url_ecrire('ajouter');
+		$url_relance = generer_url_ecrire('essai');
+		$url_bienfaiteur = generer_url_ecrire('bienfaiteur');
+		$url_vente = generer_url_ecrire('ventes');
+		$url_banque = generer_url_ecrire('banque');
+		$url_delete = generer_url_ecrire('delete_membre');
+		$url_action_categorie=generer_url_ecrire('action_categorie');
+		$url_retour = $_SERVER['HTTP_REFERER'];
+			
+		debut_gauche();
+		
+		debut_boite_info();
+		echo '<p></p>';
+		fin_boite_info();
+		
+		debut_raccourcis();
+		icone_horizontale("Retour", $url_retour, _DIR_PLUGIN_ASSOCIATION."/img_pack/calculatrice.gif","cree.gif");	
+		fin_raccourcis();
+		
+		if ($connect_statut == '0minirezo') {
+		include_spip ('inc/navigation');
+		}
+		
+		debut_droite();
+		
+		debut_cadre_relief(  "", false, "", $titre = _T('Cat&eacute;gories de cotisation'));
+		
+		$action=$_REQUEST['action'];
+		$id=$_REQUEST['id'];
+			
+		$query = spip_query( "SELECT * FROM spip_asso_categories WHERE id_categorie='$id' ");
+		while($data = spip_fetch_array($query)) {
+			$id_categorie=$data['id_categorie'];
+			$valeur=$data['valeur'];
+			$libelle=$data['libelle'];
+			$duree=$data['duree'];
+			$cotisation=$data['cotisation'];
+			$commentaires=$data["commentaires"];
+		}		
+		echo '<form action="'.$url_action_categorie.'" method="post">';	
+		echo '<input name="id" type="hidden" value="'.$id_categorie.'" />';
+		echo '<label for="valeur"><strong>Cat&eacute;gorie :</strong></label>';
+		echo '<input name="valeur" type="text" value="'.$valeur.'" id="valeur" class="formo" />';
+		echo '<label for="libelle"><strong>Libell&eacute; complet :</strong></label>';
+		echo '<input name="libelle" type="text" value="'.$libelle.'" id="libelle" class="formo" />';
+		echo '<label for="duree"><strong>Dur&eacute;e (en mois) :</strong></label>';
+		echo '<input name="duree" type="text" value="'.$duree.'" id="duree" class="formo" />';
+		echo '<label for="montant"><strong>Montant (en euros) :</strong></label>';
+		echo '<input name="montant" type="text" value="'.$cotisation.'" id="montant" class="formo" />';
+		echo '<label for="commentaires"><strong>Commentaires :</strong></label>';
+		echo '<textarea name="commentaires" id="commentaires" class="formo" />'.$commentaires.'</textarea>';
+		echo '<input type="hidden" name="action" value="modifie">';
+		
+		echo '<div style="float:right;"><input name="submit" type="submit" value="'._T('asso:bouton_envoyer').'" class="fondo" />
+		<input name="action" type="hidden" value="'.$action.'"></div>';
+		echo '</form>';
+		
+		fin_cadre_relief();  
+		fin_page();
+	}
 ?>
