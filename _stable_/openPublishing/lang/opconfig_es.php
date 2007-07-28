649d7d22da2d9c7919f9e5e0d2ca018904788e55
gaitan@gmail.com <gaitan@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-07-28 01:51:20 +0000

    jout fichier de langue espagnole Martin Gaitan - gaitan@gmail.com
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@13955 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _stable_/openPublishing/lang/opconfig_es.php
@@ -0,0 +1,184 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// A
+'ajouter_agenda' => 'Adjuntar a la agenda',
+'abandonner' => 'Abandonar',
+'anti_spam' => 'Activar el anti-spam?',
+'antispam_oui' => 'Las @ de las direcciones de mail ser&aacute;n transformadas',
+'antispam_non' => 'Las @ de las direcciones de mail no ser&aacute;n transformadas',
+'agenda_explique' => 'Si se activa, el eventual usuario podr&acute; marcar la casilla "Agenda", y en el art&iacute;culo ser&aacute; como una breve en el secci&oacute;n indicada en el cuadro de arriba.',
+'agenda_explique2' => 'Tenga en cuenta que si publica su art&iacute;culo como evento de "Agenda", la secci&oacute;n seleccionada ser&aacute; ignorada.',
+'agenda_rubrique' =>  'secci&oacute;n de la agenda: ',
+'agenda_active' => 'activar la agenda ?&nbsp;',
+'agenda_oui' => 'la agenda se activar&aacute;',
+'agenda_non' => 'la agenda no se activar&aacute;',
+'ajout_correct' => 'se ha adjuntado correctamente',
+'ajout_incorrect' => ' no se ha adjuntado: error',
+'aide_formulaire' => 'Ayuda: Presiona las flechas para expandir el men&uacute;.',
+'aide_inclusion' => 'Para incluir directamente una imagen o un documento en tu art&iacute;culo, copia en el texto el c&oacute;digo que figura bajo la miniatura. Tambi&eacute;n puedes ignorarlo; en este caso, tus documentos apareceran como documentos adjuntos a tu art&iacute;culo.',
+
+// C
+
+'configurer_op' => 'Configurar openPublishing',
+
+// D
+'document_joint' => 'Adjuntar un documento o una im&aacute;gen.',
+'document_inclusion' => 'Documento (el archivo aparecer&aacute; en forma de link bajo tu art&iacute;culo)',
+'document_explique' => 'Esta opci&oacute;n permite subir documentos desde tu computadora y asociarlos a tu art&iacute;culo.',
+'deja_base' => ' ya est&aacute; en la base.',
+'deinstaller' => 'Desinstalar openPublishing',
+
+// E
+'explication_agenda' => 'Para añadir tu art&iacute;culo en la agenda (en el caso de un evento), marca la casilla a continuaci&oacute;n y define la fecha y la hora que corresponda. Tu art&iacute;culo se publicar&aacute; en la Agenda (y no en una de las secciones de "contribuciones").',
+'extensions_acceptees' => 'Las extensiones de archivos aceptadas son :',
+'erreur_anonymous' => 'error, no hay autor \'an&oacute;nimo\' en la base. Publicaci&oacute;n imposible.',
+'erreur_die' => 'verifica la instalaci&oacute;n del plugin openPublishing',
+'erreur_insertion' => 'error en la inserci&oacute;n de tu art&iacute;culo en la base de datos. Contacta a los/as responsables del sitio',
+'erreur_upload' => 'error de upload.  El archivo temporal es imposible de encontrar. Este puede ser consecuencia de que est&aacute;s intentando subir un archivo demasiado grande. El tamaño m&aacute;ximo permitido es de 5 Mb.',
+'erreur_extension' => 'error de upload. El tipo de archivo que intentas subir no est&aacute; permitido.',
+
+//G
+'gestion_agenda' => 'Gesti&oacute;n de la agenda',
+'gestion_autre' => 'Otros elementos de configuraci&oacute;n',
+
+// I
+'identification' => 'Identificaci&oacute;n',
+'identif_explique' => 'Pueden indentificarse, pero no es obligatorio. Estas opciones añaden simplemente el nombre y otra informaci&oacute;n como el correo electr&oacute;nico, el n&uacute;mero de tel&eacute;fono, etc..., tras su art&iacute;culo.',
+'image_inclusion' => 'Im&aacute;gen (para la inclusi&oacute;n directa dentro del art&iacute;culo)',
+'installer' => 'Instalar las tablas de openPublishing',
+'info_version' => 'Versi&oacute;n instalada : ',
+'info_auteur' => 'Id del autor an&oacute;nimo: ',
+'info_agenda' => 'Gesti&oacute;n de agenda : ',
+'info_document' => 'Gesti&oacute;n de documentos : ',
+'info_statut' => 'Estado de los art&iacute;culos : ',
+'info_motclefstag' => 'Gesti&oacute;n de palabras clave',
+'info_tagmachine' => 'Plugin Tag Machine : ',
+'info_motclefs' => 'Palabras clave : ',
+'info_traitement' => 'Post-tratamiento',
+'info_titre' => 'T&iacute;tulo en min&uacute;scula : ',
+'info_antispam' => 'Anti-spam : ',
+
+// L
+'la_rubrique' => 'la secci&oacute;n ',
+
+// M
+'motclefs' => 'Elegir palabras clave',
+'motclefs_active' => 'Autorizar la gesti&oacute;n de palabras clave ?&nbsp;',
+'motclefs_oui' => 'Los usuarios podr&aacute;n elegir sus palabras clave',
+'motclefs_non' => 'Los usuarios no podr&aacute;n elegir sus palabras clave',
+'motclefs_explique' => 'Las palabras clave son &uacute;tiles para hacer referencias dentro del sitio, y para encontrarlo en una b&uacute;squeda por temas.',
+'motclefs_explique2' => 'Para elegir varias palabras clave, mantener la tecla SHIFT o CTRL (Command sobre Macs) al seleccionar',
+'motclefs_liste' => 'Lista de palabras clave asociadas a tu art&iacute;culo :',
+'motclefs_ajouter' => '&nbsp;&nbsp;Asociar una palabra clave',
+'motclefs_explique3' => 'Esta opci&oacute;n permite crear nuevas palabras clave y asociarlas a tu art&iacute;culo.',
+'motclefs_explique4' => 'Para añadir palabras clave a tu art&iacute;culo, ingresalas abajo separadas por un espacio. Puedes utilizar las palabras clave que se te sugerir&aacute;n durante la introducci&oacute;n, o crear nuevas.',
+'motclefs_explique5' => 'Para palabras compuestas, detallar la expresi&oacute;n entre comillas. Por ejemplo ',
+'motclefs_explique6' => 'pol&iacute;tica "econom&iacute;a solidaria"',
+'motclefs2' => 'palabras clave',
+
+// O
+'op_config' => 'Configuraci&oacute;n del pluginopenPublishing',
+'op_info' => 'informaci&oacute;n sobre la configuraci&oacute;n del plugin openPublishing',
+'configure_op' => 'Configurar el plugin openPublishing',
+'op_voir_info' => '<b>Configuraci&oacute;n actual de openPublishing :</b>',
+'op_modifier_info' => '<b>Esta p&aacute;gina solamente es accesible a las/os responsables del sitio.</b><p />Permite modificar la configuraci&oacute;n de openPublishing. <p /> Las modificaciones efectuadas en estas p&aacute;ginas influyen notablemente en el funcionamiento del sitio. Les recomendamos no cambiar nada mientras no esten familiarizados con el funcionamiento del plugin openPublishing. <p /> <b>M&aacute;s generalmente, se aconseja mucho dejar la configuraci&oacute;n de estas p&aacute;ginas a la webmistress o al webmaster del sitio.</b>',
+'po_fonctions_info' => '<b>Esta p&aacute;gina solamente es accesible a los/as responsables del sitio.</b><p /> Permite activar o modificar las funciones avanzadas de openPublishing. <p />Las modificaciones efectuadas en estas p&aacute;ginas influyen notablemente en el funcionamiento del sitio. Les recomendamos no cambiar nada mientras no esten familiarizados con el funcionamiento del plugin openPublishing. <p /> <b>M&aacute;s generalmente, se aconseja mucho dejar la configuraci&oacute;n de estas p&aacute;ginas a la webmistress o al webmaster del sitio.</b>',
+'op_effacer_info' => '<b>Esta p&aacute;gina solamente es accesible a los/as responsables del sitio.</b><p /> Permite eliminar completamente el plugin openPublishing. <p />Las modificaciones efectuadas en estas p&aacute;ginas influyen notablemente en el funcionamiento del sitio. Les recomendamos no cambiar nada mientras no esten familiarizados con el funcionamiento del plugin. <p /> <b>M&aacute;s generalmente, se aconseja mucho dejar la configuraci&oacute;n de estas p&aacute;ginas a la webmistress o al webmaster del sitio.</b>',
+'op_modifier_info' => '<b>Esta p&aacute;gina solamente es accesible a los/as responsables del sitio. </b><p /> Permite modificar las diferentes opciones de openPublishing.</b>',
+'op_raccourcis_documentation' => '<a href=\'http://www.spip-contrib.net/OpenPublishing-un-plugin-para-la\'>Documentaci&oacute;n del plugin openPublishing</a>',
+'op_configuration_voir_general' => 'Configuraci&oacute;n del plugin openPublishing',
+'op_configuration_effacer' => 'Desinstalar el plugin openPublishing',
+'op_configuration_modifier' => 'Configuraci&oacute;n del plugin openPublishing',
+'op_configuration_surtitre' => 'M&oacute;dulos incluidos en el antet&iacute;tulo.',
+'op_configuration_titre_principal' => 'M&oacute;dulos incluidos en el t&iacute;tulo principal.',
+'op_configuration_sous_titre' => 'M&oacute;dulos incluidos bajo el t&iacute;tulo.',
+'op_configuration_titre_lateral' => 'M&oacute;dulos incluidos en el t&iacute;tulo lateral.',
+'op_configuration_menu_principal' => 'Elementos del men&uacute; principal de navigaci&oacute;n.',
+'op_configuration_barre_laterale' => 'M&oacute;dulos incluidos en la barra lateral.',
+'op_configuration_mentions_techniques' => 'M&oacute;dulos incluidos en el pi&eacute; de p&aacute;gina.',	
+'op_info_base_ok' => 'Se instal&oacute; correctamente el plugin openPublishing',
+'op_info_base_ko' => 'Las tablas de dato del plugin openPublishing fueron instaladas.',
+'op_info_deja_ko' => 'Las tablas de dato del plugin openPublishing fueron  desinstaladas.',
+'op_info_base_up' => 'Atenci&oacute;n, la base de datos del plugin openPublishing debe ser actualizada. Eso es un cambio de versi&oacute;n del plugin openPublishing. Cliquea en el bot&oacute;n para continuar',
+'op_info_base_ko_bis' => 'El plugin openPublishing requiere la instalaci&oacute;n de una tabla suplementaria para funcionar. Si no se instala esta tabla, entonces el plugin no funcionar&aacute;.',
+'op_info_desinstal' => '<b>Este comandoborra la base de datos creada en la instalaci&oacute;n del plugin openPublishing</b>. No se borra la base de datos de spip. Si deseas suprimir el plugin openPublishing, entonces tambi&eacute;n deber&aacute;s suprimir por ftp el contenido del directorio del plugin openPublishing',
+'op_position_info' => '1. Qu&eacute; quieres hacer ?',
+'op_restriction_info' => '2. Localizaci&oacute;n sobre el sitio :',
+'op_titre_info' => '3. T&iacute;tulo del elemento',
+'op_descriptif_info' => '4. Descripci&oacute;n del elemento',
+'op_texte_info' => '5. Texto que debe indicarse o archivo que debe incluirse',
+'op_style_info' => '6. Estilo adjuntado',
+'op_modifier_creer' => 'Añadir un elemento de configuraci&oacute;n',
+'op_modifier_editer' => 'Modificar un elemento de configuraci&oacute;n',
+'op_vide_virgule' => 'asesino de bug debido a la coma',
+'op_voir_configuration' => 'Ver la configuraci&oacute;n',
+'op_modifier_configuration' => 'Modificar la configuraci&oacute;n',
+'op_supprimer_op' => 'Eliminar openPublishing',
+'obligatoire' => 'obligatorio',
+'optionel' => 'opcional',
+
+// P
+'publie_texte' => 'Texto :',
+'publie_titre' => 'T&iacute;tulo :',
+'publie_rubrique' => 'Secci&oacute;n',
+'previsualisation' => 'Previsualizaci&oacute;n',
+'previsualiser' => 'Previsualizar',
+'post_traitement' => 'Post-tratamiento de los textos',
+'publier_agenda' => 'Publicar como breve en la Agenda',
+
+// R
+'redigez_article' => 'Redactar su art&iacute;culo',
+'renvoi_explique' => 'Los textos de retorno son las pequeñas frases que el plugin muestra cuando una publicaci&oacute;n fue satisfactoria o  bien terminada por un abandono (las balizas HTML est&aacute;n permitidas).',
+'renvoi_explique2' => 'Las redirecciones permiten redirigir al usuario a una p&aacute;gina espec&iacute;fica (del tipo "/spip.php?page=sommaire").',
+'renvoi_gestion' => 'Gesti&oacute;n de los retornos',
+'renvoi_normal' => 'Texto de retorno normal',
+'renvoi_abandon' => 'Texto de retorno por abandono',
+'renvoi_modif' => 'modificar este texto',
+'redirection_normal' => 'redirecci&oacute;n normal :',
+'redirection_abandon' => 'redirecci&oacute;n por abandono :',
+'redirection_modif' => 'modificar estas direcciones',
+'rubrique_gestion' => 'Gesti&oacute;n de secciones openPublishing',
+'rubrique_ajouter' => 'adjuntar esta secci&oacute;n',
+'rubrique_pasencore' => 'A&uacute;n no existen secciones openPublishing ...',
+'rubrique_liste' => 'lista de secciones openPublishing :',
+'rubrique_explique' => 'Indica aqu&iacute; las secciones sobre las cuales permiten el openPublishing. ¡Atenci&oacute;n, las secciones deben existir! Presione la cruz para suprimir tu selecci&oacute;n.',
+'resultat' => 'resultados ...',
+
+// S
+'statut_select' => 'Cu&aacute;l ser&aacute; el estado para los art&iacute;culos recibidos ?&nbsp;',
+'statut_publie' => 'Los art&iacute;culos recibidos ser&aacute;n guardados como "publicados"',
+'statut_prop' => 'Los art&iacute;culos recibidos ser&aacute;n guardados como "propuestos"',
+'statut_prepa' => 'Los art&iacute;culos recibidos ser&aacute;n guardados como "en curso de redacci&oacute;n"',
+'suprimmer_op' => 'Eliminar openPublishing',
+
+// T
+'telecharger_document' => 'Subir documento',
+'titre_minuscule' => 'Post-tratamiento de t&iacute;tulo',
+'titre_impo_minuscule' => 'Los t&iacute;tulos se imponen en min&uacute;scula',
+'titre_non_minuscule' => 'Los t&iacute;tulos pueden utilizar may&uacute;sculas',
+'traitement_explique' => 'Estos tratamientos se aplicar&aacute;n cuando el usuario valide su texto.',
+'tagmachine_active' => 'Autorizar la gesti&oacute;n de palabras clave mediante el plugin Tag Machine ?&nbsp;',
+'tagmachine_oui' => 'Los usuarios podr&aacute;n usar el plugin Tag Machine',
+'tagmachine_non' => 'Los usuarios no podr&aacute;n usar el plugin Tag Machine',
+
+//U
+'upload_active' => 'Autorizar la subida de documentos ?&nbsp;',
+'upload_oui' => 'Los usuarios podr&aacute;n subir documentos',
+'upload_non' => 'Los usuarios no podr&aacute;n subir documentos',
+'upgrader' => 'Actualizar las tablas de openPublishing',
+
+
+// V
+'voir_configuration' => 'Ver la configuraci&oacute;n',
+'votre_nom' => 'Tu nombre o pseud&oacute;nimo',
+'votre_mail' => 'Tu email',
+'votre_groupe' => 'Tu posible grupo',
+'votre_phone' => 'Tu n&uacute;mero de tel&eacute;fono'
+);
+
+?>
+ 
