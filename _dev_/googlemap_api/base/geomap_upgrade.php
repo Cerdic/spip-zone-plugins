bf3c910270cd0ec6865a0badf1d67b39488d5715
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-07-28 11:31:02 +0000

    on passe a la version 0.2 pour eviter les pb et changements dans le plugin.xml
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@13963 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/googlemap_api/base/geomap_upgrade.php
+++ _dev_/googlemap_api/base/geomap_upgrade.php
@@ -8,43 +8,38 @@
  * (c) 2007 - Distribu’do baixo licencia GNU/GPL
  *
  */
-
-	include_spip('inc/meta');
-	
-	$GLOBALS['geomap_version'] = 0.1;
+	$GLOBALS['geomap_version'] = 0.2;
 	
 	function geomap_verifier_base(){
 		$version_base = $GLOBALS['geomap_version'];
 		$current_version = 0.0;
-		if (   (!isset($GLOBALS['meta']['geomap_version']) )
+		if ((!isset($GLOBALS['meta']['geomap_version']) )
 				|| (($current_version = $GLOBALS['meta']['geomap_version'])!=$version_base)){
-			effacer_meta("geomap_base_version");
-			ecrire_meta('geomap_version',$current_version=$version_base,'non');
+			if ($current_version==0.0){
+				ecrire_meta('geomap_version',$current_version=$version_base,'non');
+			}
+			if ($current_version<0.2){
+				ecrire_meta('geomap_version',$current_version=0.2);
+			}
 		}
-		if ($current_version<0.1){
-			effacer_meta("geomap_base_version");
-			ecrire_meta('geomap_version',$current_version=0.1,'non');
-			echo _T('geomap:miseajour') $current_version;
-		}	
 		ecrire_metas();
-		}
 	}
 	
-	function geomap_vider_tables() {
+	function geomap_vider_tables(){
 		effacer_meta("geomap_googlemapkey");
 		effacer_meta("geomap_default_lat");
 		effacer_meta("geomap_default_lonx");
 		effacer_meta("geomap_default_zoom");
-		effacer_meta($nom_meta_base_version);
+		effacer_meta("geomap_version");
 		ecrire_metas();
 	}
 
 	function geomap_install($action){
-	$version_base = $GLOBALS['geomap_base_version'];
+		$version_base = $GLOBALS['geomap_version'];
 		switch ($action){
 			case 'test':
 				return (isset($GLOBALS['meta']['geomap_version']) 
-				  AND ($GLOBALS['meta']['geomap_version']>=$version_base));
+					AND ($GLOBALS['meta']['geomap_version']>=$version_base));
 				break;
 			case 'install':
 				geomap_verifier_base();
@@ -52,5 +47,6 @@
 			case 'uninstall':
 				geomap_vider_tables();
 				break;
+		}
 	}
 ?>
