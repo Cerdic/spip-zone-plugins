91112d2d54e0eb7a4aaf7d4fbe95ed5bd53d46d3
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-07-29 20:49:54 +0000

    Ajout du fichier index.php à mettre dans le dossier sites/ si l'on veut lister automatiquement les sites installés.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@13986 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _test_/mutualisation/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="fr" xml:lang="fr" dir="ltr">
+<head>
+<title>SPIP mutualis&eacute;s</title>
+</head>
+<body>
+<?php
+// Création du tableau qui va contenir les fichiers et dossiers
+$files = array();
+
+// Ouverture du répetoire courant
+// Pour le changer utiliser chdir() avant opendir()
+$handle = opendir(".");
+
+// Parcours des fichiers et dossiers du répertoire courant
+while($file = readdir($handle)) {
+    if($file != "." && $file != ".." && is_dir($file)) {
+        $files[] = $file;
+    }
+}
+
+// Fermeture du répertoire courant
+closedir($handle);
+
+// Tri du tableaunat
+sort($files);
+
+// Affichage des fichiers et dossiers triés
+echo "<h1>Liste des sites install&eacute;s (".count($files).")</h1>\n<ul>";
+foreach($files as $v) {
+    echo "<li><a href='http://$v'>$v</a></li>\n";
+}
+echo "</ul>";
+?>
+</body>
+</html>
