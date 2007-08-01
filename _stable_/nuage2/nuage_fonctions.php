ab567694ea42ccac2124b14462c437b531fb1cb7
spipcarto@gmail.com <spipcarto@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-08-01 17:00:57 +0000

    variante de nuage utilisant les jointures, count et sum. bien meilleures perfs quand on a beaucoup de mots...
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@14097 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _stable_/nuage2/nuage_fonctions.php
@@ -0,0 +1,127 @@
+<?php
+function critere_frequence($idb, &$boucles, $crit){
+	$op='';
+	$boucle = &$boucles[$idb];
+	$params = $crit->param;
+	$type = array_shift($params);
+	$type = $type[0]->texte;
+	if(preg_match(',^(\w+)([<>=])([0-9]+)$,',$type,$r)){
+		$type=$r[1];
+		$op=$r[2];
+		$op_val=$r[3];
+	}
+	$type_id = 'freq.id_'.$type;
+	$type_requete = $boucle->type_requete;
+	$id_table = $boucle->id_table . '.' . $boucle->primary;
+	$boucle->select[]= 'COUNT('.$type_id.') AS frequence';
+	$boucle->from['freq']="spip_'.$type_requete.'_".$type."s";
+	$boucle->where[]= array("'='", "'".$id_table."'", "'".$type_id."'");
+	$boucle->group[]=$id_table;
+	if ($op)
+		$boucle->having[]= array("'".$op."'", "'frequence'",$op_val);
+	
+}
+function critere_popularite($idb, &$boucles, $crit){
+	$op='';
+	$boucle = &$boucles[$idb];
+	$params = $crit->param;
+	$type = array_shift($params);
+	$type = $type[0]->texte;
+	if(preg_match(',^(\w+)([<>=])([0-9]+)$,',$type,$r)){
+		$type=$r[1];
+		$op=$r[2];
+		$op_val=$r[3];
+	}
+	$type_id = 'pop.id_'.$type;
+	$type_requete = $boucle->type_requete;
+	$id_table = $boucle->id_table . '.' . $boucle->primary;
+	$boucle->select[]= 'SUM('.$type_id.') AS popularite_relative';
+	$boucle->from['pop']="spip_'.$type_requete.'_".$type."s";
+	$boucle->where[]= array("'='", "'".$id_table."'", "'".$type_id."'");
+	$boucle->group[]=$id_table;
+	if ($op)
+		$boucle->having[]= array("'".$op."'", "'popularite_relative'",$op_val);
+	
+}
+function critere_frequence_branche($idb, &$boucles, $crit){
+	$op='';
+	$boucle = &$boucles[$idb];
+	$params = $crit->param;
+	$type = array_shift($params);
+	$type = $type[0]->texte;
+	if(preg_match(',^(\w+)([<>=])([0-9]+)$,',$type,$r)){
+		$type=$r[1];
+		$op=$r[2];
+		$op_val=$r[3];
+	}
+	$type_id = 'freq.id_'.$type;
+	$type_requete = $boucle->type_requete;
+	$id_table = $boucle->id_table . '.' . $boucle->primary;
+	$boucle->select[]= 'COUNT('.$type_id.') AS frequence';
+	$boucle->from['freq']="spip_'.$type_requete.'_".$type."s";
+	$boucle->where[]= array("'='", "'".$id_table."'", "'".$type_id."'");
+	$boucle->from['lf']="spip_".$type."s";
+	$boucle->where[]= array("'='", "'".$type_id."'", "'lf.id_".$type."'");
+	$boucle->group[]=$id_table;
+	if ($op)
+		$boucle->having[]= array("'".$op."'", "'frequence'",$op_val);
+	
+	$not = $crit->not;
+	$boucle = &$boucles[$idb];
+	$arg = calculer_argument_precedent($idb, 'id_rubrique', $boucles);
+	$c = "calcul_mysql_in('lf.id_rubrique', calcul_branche($arg), '')";
+	$c = "($arg ? $c : 1)";			
+	if ($not)
+		$boucle->where[]= array("'NOT'", $c);
+	else
+		$boucle->where[]= $c;
+}
+function critere_popularite_branche($idb, &$boucles, $crit){
+	$op='';
+	$boucle = &$boucles[$idb];
+	$params = $crit->param;
+	$type = array_shift($params);
+	$type = $type[0]->texte;
+	if(preg_match(',^(\w+)([<>=])([0-9]+)$,',$type,$r)){
+		$type=$r[1];
+		$op=$r[2];
+		$op_val=$r[3];
+	}
+	$type_id = 'pop.id_'.$type;
+	$type_requete = $boucle->type_requete;
+	$id_table = $boucle->id_table . '.' . $boucle->primary;
+	$boucle->select[]= 'SUM('.$type_id.') AS popularite_relative';
+	$boucle->from['pop']="spip_'.$type_requete.'_".$type."s";
+	$boucle->where[]= array("'='", "'".$id_table."'", "'".$type_id."'");
+	$boucle->group[]=$id_table;
+	if ($op)
+		$boucle->having[]= array("'".$op."'", "'popularite_relative'",$op_val);
+	
+	$not = $crit->not;
+	$boucle = &$boucles[$idb];
+	$arg = calculer_argument_precedent($idb, 'id_rubrique', $boucles);
+	$c = "calcul_mysql_in('lf.id_rubrique', calcul_branche($arg), '')";
+	$c = "($arg ? $c : 1)";
+	if ($not)
+		$boucle->where[]= array("'NOT'", $c);
+	else
+		$boucle->where[]= $c;
+}
+
+function nuage_note($score,$scoremax=1,$max=10) {
+	$score = pow(($score/$scoremax),1.5); # lissage
+	return ceil($max*$score);
+}
+
+function balise_FREQUENCE_dist($p) {
+	$p->code = '$Pile[$SP][\'frequence\']';
+	$p->interdire_scripts = false;
+	return $p;
+}
+function balise_POPULARITE_RELATIVE_dist($p) {
+	$p->code = '$Pile[$SP][\'popularite_relative\']';
+	$p->interdire_scripts = false;
+	return $p;
+}
+
+?>
