7bffc6b9d76ca75c919f09521b1f7a8806109b79
maieulrouquette@tele2.fr <maieulrouquette@tele2.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-08-02 15:09:54 +0000

    on peut jouer dansq l'espace privé
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@14135 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _stable_/jeux/exec/jeux_voir.php
@@ -0,0 +1,53 @@
+<?
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+include_spip('inc/presentation');
+
+function exec_jeux_voir(){
+	$id_jeu 	= _request('id_jeu');
+	
+	$requete	= spip_fetch_array(spip_query("SELECT contenu,id_jeu,date FROM spip_jeux WHERE id_jeu =".$id_jeu));
+	$contenu 	= $requete['contenu'];
+	$id_jeu		= $requete['id_jeu'];
+	$date		= $requete['date'];
+	
+	
+	if(!$id_jeu){
+		debut_page(_T("jeux:pas_de_jeu"));
+		gros_titre(_T("jeux:pas_de_jeu"));
+		fin_page();
+		return;
+		}
+	
+	debut_page(_T("jeux:jeu_numero",array('id'=>$id_jeu)));
+			
+	debut_gauche();
+	
+	//edition du jeu
+	debut_boite_info();
+	echo _T("jeux:jeu_numero",array('id'=>$id_jeu));
+	echo "<br /><strong>"._t("jeux:derniere_modif")."</strong><br />".affdate($date).heures($date)." : ".minutes($date);
+	fin_boite_info();
+	
+	debut_cadre_relief();
+	echo _T('jeux:explication_jeu');
+	fin_cadre_relief();
+	
+	//lien vers les résultats
+	debut_cadre_relief();
+	echo "<a href=".generer_url_ecrire('jeux_resultat','id_jeu='.$id_jeu).">"._T("jeux:voir_resultats")."</a>";
+	fin_cadre_relief();
+	
+	
+	creer_colonne_droite();
+	debut_droite();
+	debut_cadre_relief();
+	gros_titre(_T("jeux:jeu_numero",array('id'=>$id_jeu)));
+	echo propre($contenu);
+	fin_cadre_relief();
+	fin_gauche();
+	fin_page();
+	}
+
+
+?>
