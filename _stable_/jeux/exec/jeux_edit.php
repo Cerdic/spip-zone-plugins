5ead6e7aa1d2f8a039ad49e040e8a0048a46ea10
maieulrouquette@tele2.fr <maieulrouquette@tele2.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-08-02 09:57:35 +0000

    on peut désormais modifier un jeu déja existant
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@14123 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/jeux/exec/jeux_edit.php
+++ _stable_/jeux/exec/jeux_edit.php
@@ -4,24 +4,28 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 
 include_spip('inc/presentation');
 
-
-
-
 function exec_jeux_edit(){
-	$valider = _request('valider');
+	include_spip('inc/utils');
 	
+	$valider = _request('valider');
 	$contenu = _request('contenu');
+	$nouveau = _request('nouveau');
+	$id_jeu	 = _request('id_jeu');
+	
+
 	
 	if ($valider)
 		{
-		jeux_ajouter_jeu($contenu);
-		};
+		$id_jeu = jeux_ajouter_jeu($contenu,$id_jeu);
+		 include_spip('inc/headers');	 redirige_par_entete(generer_url_ecrire('jeux_voir', 'id_jeu='.$id_jeu));
+		
+		}
 		
 	
 	// Admin SPIP-Listes
-	echo debut_page(_T('jeux:nouveau_jeu'));
-	gros_titre(_T('jeux:nouveau_jeu'));
-	
+	$nouveau ? debut_page(_T('jeux:nouveau_jeu')) : debut_page(_T('jeux:modifier_jeu'));
+	$nouveau ? gros_titre(_T('jeux:nouveau_jeu')) : gros_titre(_T('jeux:modifier_jeu'));
+	$contenu = spip_fetch_array(spip_query("SELECT contenu FROM spip_jeux WHERE id_jeu =".$id_jeu));
 	
 	debut_gauche();
 	echo debut_cadre_relief();
@@ -31,20 +35,30 @@ function exec_jeux_edit(){
 	debut_droite();
 	debut_cadre_formulaire();
 	echo "<form method='post'  name='jeux_edit'>
-	<textarea  name='contenu'  class='formo' rows='20' cols='40'> </textarea>
-	";
+	<textarea  name='contenu'  class='formo' rows='20' cols='40'>";
 	
+	echo strip_tags($contenu['contenu']);
+	
+	echo " </textarea>";
 	echo "<p align='right'><input type='submit' name='valider' value='"._T('bouton_valider')."' class='fondo' />";
 	echo "</form>";
 		
 	echo fin_cadre_formulaire(),fin_gauche(), fin_page();
 }
 
-function jeux_ajouter_jeu($contenu){
+function jeux_ajouter_jeu($contenu,$id_jeu){
 	include_spip('inc/utils');
 	
-	spip_query("INSERT into spip_jeux (contenu) VALUES('<jeux>".$contenu."</jeux>')");
-	return;
+	if (!$id_jeu) {
+		spip_query("INSERT into spip_jeux (contenu) VALUES('<jeux>".$contenu."</jeux>')");	
+		$id_jeu = mysql_insert_id();		
+				}
+	
+	else 		{
+		spip_query('REPLACE into spip_jeux (id_jeu,contenu) VALUES ('.$id_jeu.',"<jeux>'.$contenu.'")');
+		
+		}
+	return $id_jeu;
 };
 	
 ?>
