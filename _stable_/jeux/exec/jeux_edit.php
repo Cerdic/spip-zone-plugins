a79a3c6d255160c395499b938ab971a3d69aa8da
maieulrouquette@tele2.fr <maieulrouquette@tele2.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-08-03 17:56:51 +0000

    on préselectionne la liste
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@14179 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/jeux/exec/jeux_edit.php
+++ _stable_/jeux/exec/jeux_edit.php
@@ -16,17 +16,19 @@ function exec_jeux_edit(){
 	
 	if ($valider)
 		{
-		echo $id_jeu;
 		$id_jeu = jeux_ajouter_jeu($contenu,$enregistrer_resultat,$id_jeu);
 		 include_spip('inc/headers');	 redirige_par_entete(generer_url_ecrire('jeux_voir', 'id_jeu='.$id_jeu,true));
 		
 		}
 		
 	
-	// Admin SPIP-Listes
+	
 	$nouveau ? debut_page(_T('jeux:nouveau_jeu')) : debut_page(_T('jeux:modifier_jeu',array('id'=>$id_jeu)));
 	
-	$contenu = spip_fetch_array(spip_query("SELECT contenu FROM spip_jeux WHERE id_jeu =".$id_jeu));
+	$requete = spip_fetch_array(spip_query("SELECT enregistrer_resultat,contenu FROM spip_jeux WHERE id_jeu =".$id_jeu));
+	$contenu = $requete['contenu'];
+	$enregistrer_resultat  = $requete['enregistrer_resultat'];
+	
 	
 	debut_gauche();
 	echo debut_cadre_relief();
@@ -48,9 +50,16 @@ function exec_jeux_edit(){
 	
 	debut_cadre_relief();
 	echo "<label><span class='titrem'>"._T('jeux:enregistrer_resultat');
-	echo '<br /></span><select class="formo" name="enregistrer_resultat"><option value="oui">'._T('oui').'</option><option value="non">'._T('non').'</option></select>';
+	echo '<br /></span><select class="formo" name="enregistrer_resultat"><option value="oui">'._T('item_oui').'</option>';
+	echo '<option value="non"';
+	if ($enregistrer_resultat=='item_non') { echo 'selected="selected"' ;} ;
+	
+	
+	echo '>'._T('non').'</option></select>';
 	echo '</label>';
 	fin_cadre_relief();
+	
+	
 	echo "<p align='right'><input type='submit' name='valider' value='"._T('bouton_valider')."' class='fondo' /></p>";
 	
 	
