501242a8acf43d11c95a876c74d8af651202588b
james@rezo.net <james@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-08-04 09:40:00 +0000

    on essaie la technique de Fil
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@14216 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/balise_session/public/assembler.php
+++ _stable_/balise_session/public/assembler.php
@@ -132,15 +132,7 @@ function assembler_page ($fond) {
 		}
 	}
 
-	// Si la page annonce un entete 'X-Session: zz' on ignore le cache si
-	// l'on n'a pas la session 'zz'
-	$utiliser_session = isset($page['entetes'])
-		AND isset($page['entetes']['X-Session']);
-	if ($utiliser_session
-	AND $page['entetes']['X-Session'] != spip_session()) {
-		spip_log('X-Session: '.$page['entetes']['X-Session']);
-		$use_cache = 1;
-	}
+	// Si la page annonce un entete 'X-Session: zz' on ignore le cache si	// l'on n'a pas la session 'zz'	if (isset($page['entetes'])	AND isset($page['entetes']['X-Session'])	AND $page['entetes']['X-Session'] != spip_session()) {		spip_log('X-Session:'.$page['entetes']['X-Session']);		$use_cache = 1;	}
 
 	// Si requete HEAD ou Last-modified compatible, ignorer le texte
 	// et pas de content-type (pour contrer le bouton admin de inc-public)
@@ -159,12 +151,9 @@ function assembler_page ($fond) {
 			$parametrer = charger_fonction('parametrer', 'public');
 			$page = $parametrer($fond, '', $chemin_cache);
 
-			// ajouter les scripts a mettre en cache
+			//ajouter les scripts poue le mettre en cache
 			$page['insert_js_fichier'] = pipeline("insert_js",array("type" => "fichier","data" => array()));
 			$page['insert_js_inline'] = pipeline("insert_js",array("type" => "inline","data" => array()));
-
-			if ($utiliser_session)
-				$page['entetes']['X-Session'] = spip_session();
 
 			if ($chemin_cache)
 				$cacher(NULL, $use_cache, $chemin_cache, $page, $lastmodified);
@@ -282,27 +271,6 @@ function inclure_page($fond, $contexte_inclus) {
 	return $page;
 }
 
-// Renvoie une chaine qui decrit la session courante pour savoir si on peut
-// utiliser un cache enregistre pour cette session.
-// Par convention cette chaine ne doit pas contenir de caracteres [^0-9A-Za-z]
-// Attention on ne peut *pas* inferer id_auteur a partir de la session, qui
-// pourrait etre une chaine arbitraire -- ce n'est pas le cas pour l'instant
-// http://doc.spip.org/@spip_session
-function spip_session() {
-	static $session;
-
-	if (!isset($session)) {
-		$session = $GLOBALS['auteur_session']
-			? 'session'
-				.$GLOBALS['auteur_session']['id_auteur']
-				.'_'
-				.$_COOKIE['spip_session']
-			: '0';
-		spip_log('session: '.$session);
-	}
-
-	return $session;
-}
 
 # Attention, un appel explicite a cette fonction suppose certains include
 # (voir l'exemple de spip_inscription et spip_pass)
@@ -688,5 +656,7 @@ function inclure_modele($type, $id, $params, $lien) {
 	$compteur--;
 	return $retour;
 }
+
+// Renvoie une chaine qui decrit la session courante pour savoir si on peut// utiliser un cache enregistre pour cette session.// Par convention cette chaine ne doit pas contenir de caracteres [^0-9A-Za-z]// Attention on ne peut *pas* inferer id_auteur a partir de la session, qui// pourrait etre une chaine arbitraire -- ce n'est pas le cas pour l'instant// http://doc.spip.org/@spip_sessionfunction spip_session() {	static $session;	if (!isset($session))		$session = isset($GLOBALS['auteur_session'])			? 'session'				.$GLOBALS['auteur_session']['id_auteur']				.'_'				.$_COOKIE['spip_session']			: '';	spip_log('session: '.$session);	return $session;}
 
 ?>
--- _stable_/balise_session/public/compiler.php
+++ _stable_/balise_session/public/compiler.php
@@ -134,7 +134,6 @@ function calculer_boucle($id_boucle, &$boucles) {
   return $req . $corps 
 	. ($notrace ? "" : "
 		boucle_debug_resultat('$id_boucle', 'resultat', \$t0);")
-//	.	"\n	\$entetes = array_merge(\$entetes, array(".$_entetes."));"
 	.  "\n	return \$t0;";
 }
 
@@ -151,7 +150,7 @@ function calculer_boucle_rec($id_boucle, &$boucles) {
 }
 
 // compil d'une boucle non recursive. 
-// c'est un "while (fetch_sql)" dans le cas gÃ©nÃ©ral,
+// c'est un "while (fetch_sql)" dans le cas général,
 // qu'on essaye d'optimiser un max.
 
 // http://doc.spip.org/@calculer_boucle_nonrec
@@ -457,22 +456,17 @@ function calculer_parties($boucles, $id_boucle) {
 // (qui sera argument d'un Return ou la partie droite d'une affectation).
 
 // http://doc.spip.org/@calculer_liste
-function calculer_liste($tableau, $descr, &$boucles, $id_boucle='', $ramener_entetes = false) {
+function calculer_liste($tableau, $descr, &$boucles, $id_boucle='') {
 	if (!$tableau) return "''";
 	if (!isset($descr['niv'])) $descr['niv'] = 0;
-	list($entetes, $codes) = compile_cas($tableau, $descr, $boucles, $id_boucle);
-	$_entetes = array();
-	foreach($entetes as $nom => $valeur)
-		$_entetes[]= '"'.$nom.'" => "'.$valeur.'"';
-	$entetes = 'array(' . join(', ', $_entetes) . ')';
+	$codes = compile_cas($tableau, $descr, $boucles, $id_boucle);
 	$n = count($codes);
 	if (!$n) return "''";
 	$tab = str_repeat("\t", $descr['niv']);
-	if (_request('var_mode_affiche') != 'validation') {
-		$_codes = (($n==1) ? $codes[0] : 
+	if (_request('var_mode_affiche') != 'validation')
+	  return
+		(($n==1) ? $codes[0] : 
 			 "(" . join (" .\n$tab", $codes) . ")");
-		return $ramener_entetes ? array($entetes, $_codes) : $_codes;
-	}
 	else return "@debug_sequence('$id_boucle', '" .
 	  ($descr['nom']) .
 	  "', " .
@@ -483,8 +477,7 @@ function calculer_liste($tableau, $descr, &$boucles, $id_boucle='', $ramener_ent
 
 // http://doc.spip.org/@compile_cas
 function compile_cas($tableau, $descr, &$boucles, $id_boucle) {
-	$entetes = array();
-	$codes = array();
+        $codes = array();
 	// cas de la boucle recursive
 	if (is_array($id_boucle)) 
 	  $id_boucle = $id_boucle[0];
@@ -542,7 +535,7 @@ function compile_cas($tableau, $descr, &$boucles, $id_boucle) {
 			$newdescr['niv']++;
 			$code = 'BOUCLE' .
 			  str_replace("-","_", $nom) . $descr['nom'] .
-			  '($Cache, $Pile, $doublons, $Numrows, $entetes, $SP)';
+			  '($Cache, $Pile, $doublons, $Numrows, $SP)';
 			$commentaire= "?$nom";
 			$avant = calculer_liste($p->avant,
 				$newdescr, $boucles, $id_boucle);
@@ -582,11 +575,6 @@ function compile_cas($tableau, $descr, &$boucles, $id_boucle) {
 			$apres = calculer_liste($p->apres,
 				$descr, $boucles, $id_boucle);
 			$altern = "''";
-
-			// recuperation des entetes
-			if(!empty($p->entetes))
-				$entetes = array_merge($entetes, $p->entetes);
-
 			break;
 
 		default: 
@@ -624,7 +612,7 @@ function compile_cas($tableau, $descr, &$boucles, $id_boucle) {
 				"\n// $commentaire\n$code" :
 				$code));
 	} // foreach
-	return array($entetes, $codes);
+	return $codes;
 }
 
 // affichage du code produit
@@ -772,7 +760,7 @@ function public_compiler_dist($squelette, $nom, $gram, $sourcefile) {
 
 	// idem pour la racine
 	$descr['id_mere'] = '';
-	list($entetes, $corps) = calculer_liste($racine, $descr, $boucles, '', true);
+	$corps = calculer_liste($racine, $descr, $boucles);
 
 	// Calcul du corps de toutes les fonctions PHP,
 	// en particulier les requetes SQL et TOTAL_BOUCLE
@@ -788,7 +776,7 @@ function public_compiler_dist($squelette, $nom, $gram, $sourcefile) {
 		if (!function_exists($f)) $f = 'boucle_DEFAUT_dist';
 		$boucles[$id]->return = 
 			"function BOUCLE" . strtr($id,"-","_") . $nom .
-			'(&$Cache, &$Pile, &$doublons, &$Numrows, &$entetes, $SP) {' .
+			'(&$Cache, &$Pile, &$doublons, &$Numrows, $SP) {' .
 			$f($id, $boucles) .
 			"\n}\n\n";
 		if ($GLOBALS['var_mode'] == 'debug')
@@ -810,6 +798,10 @@ function public_compiler_dist($squelette, $nom, $gram, $sourcefile) {
 		.preg_replace(',\.html$,', '', $sourcefile)
 		."] $nom.php");
 
+	$session = $GLOBALS['squelettes_a_sessions'][$descr['nom']]
+		? "'<'.'?php header(\\'X-Spip-Session: ' . spip_session() . '\\'); ?'.'>'."
+		: '';
+
 	$code = "<"."?php
 /*
  * Squelette : $sourcefile
@@ -824,14 +816,14 @@ function public_compiler_dist($squelette, $nom, $gram, $sourcefile) {
 // Fonction principale du squelette ' . $sourcefile ."
 //
 function " . $nom . '($Cache, $Pile, $doublons=array(), $Numrows=array(), $SP=0) {
-	$entetes = ' . $entetes . ';
 	$page = ' .
+	$session .
 	// ATTENTION, le calcul du l'expression $corps affectera $Cache
 	// c'est pourquoi on l'affecte a cette variable auxiliaire
 	// avant de referencer $Cache
 	$corps . ";
 
-	return analyse_resultat_skel('$nom', \$Cache, \$page, \$entetes);
+	return analyse_resultat_skel('$nom', \$Cache, \$page);
 }
 
 ?".">";
