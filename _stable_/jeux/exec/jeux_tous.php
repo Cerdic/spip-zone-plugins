7c4aeeeb2445f11c7d85554a50a935b230808a97
maieulrouquette@tele2.fr <maieulrouquette@tele2.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-08-05 08:15:59 +0000

    un petit x qui trainait
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@14227 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/jeux/exec/jeux_tous.php
+++ _stable_/jeux/exec/jeux_tous.php
@@ -11,7 +11,7 @@ function exec_jeux_tous(){
 	
 	
 	debut_boite_info();
-	echo icone_horizontale(_T('jeux:gerer_resultats'),generer_url_ecrire('jeux_gerer_resultats','tous=oui'),find_in_path('img/jeux-laurier.png'));
+	echo icone_horizontale(_T('jeux:gerer_resultats'),generer_url_ecrire('jeux_gerer_resultats','tous=oui'),find_in_path('img/jeu-laurier.png'));
 	echo icone_horizontale(_T('jeux:nouveau_jeu'),generer_url_ecrire('jeux_edit','nouveau=oui'),find_in_path('img/jeu-nouveau.png'));
 	fin_boite_info();
 	
