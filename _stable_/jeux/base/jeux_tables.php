17fa1eb0da76baa4b2ad7a3ad67f195507c99afc
patfr@ifrance.com <patfr@ifrance.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-08-06 22:46:13 +0000

    + gestion d'un numéro de version de base en metas + version de base : 0.1 + test/install/uninstall + nouveau champ 'nom' dans la table 'jeux' + tous les jeux existants sont nommés 'Sans nom'. Une simple modif du jeu suffit pour initialiser automatiquement ce champ.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@14275 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/jeux/base/jeux_tables.php
+++ _stable_/jeux/base/jeux_tables.php
@@ -9,30 +9,34 @@ global $tables_jointures;
 $table_des_tables['jeux'] = 'jeux';
 $table_des_tables['jeux_resultats'] = 'jeux_resultats';
 $jeux = array(
-	'id_jeu'	=>'bigint(21) NOT NULL',
-	'date'		=>"timestamp",
-	'contenu'	=>'text NOT NULL',
-	'statut'	=>"varchar(10) DEFAULT '0' NOT NULL",
-	'enregistrer_resultat'=>'enum ("oui","non")'
-	
-	);
+	'id_jeu'	=> 'bigint(21) NOT NULL',
+	'date'		=> 'timestamp');
+if (isset($GLOBALS['meta']['jeux_base_version']))
+	$jeux = array_merge($jeux, array('nom'	=> 'text NOT NULL'));
+$jeux = array_merge($jeux, array(
+	'contenu'	=> 'text NOT NULL',
+	'statut'	=> "varchar(10) DEFAULT '0' NOT NULL",
+	'enregistrer_resultat'=>'enum ("oui", "non")',
+	)
+);
 
 $jeux_key = array(
 	'PRIMARY KEY'	=>'id_jeu');
 $jeux_resultats = array(
-	'id_resultat'	=>'bigint(21) NOT NULL',
+	'id_resultat'	=> 'bigint(21) NOT NULL',
 	'id_jeu'		=> 'bigint(21) NOT NULL',
 	'id_auteur'		=> 'bigint(21) NOT NULL',
-	'date'			=>	"timestamp",
+	'date'			=>	'timestamp',
 	'resultat_court'	=>	'int(12)',
 	'resultat_long'	=>	'text NOT NULL');
 $jeux_resultats_key=array('PRIMARY KEY' =>'id_resultat',
-'KEY id_jeu'	=>'id_jeu',
-	'KEY id_auteur'	=>'id_auteur');
+	'KEY id_jeu'	=>'id_jeu',
+	'KEY id_auteur'	=>'id_auteur'
+);
 
-$tables_principales['spip_jeux']=
+$tables_principales['spip_jeux'] =
 	array('field' => &$jeux, 'key' => &$jeux_key);
-$tables_principales['spip_jeux_resultats']=
+$tables_principales['spip_jeux_resultats'] =
 	array('field' => &$jeux_resultats, 'key' => &$jeux_resultats_key);
 
 
--- _stable_/jeux/base/jeux_upgrade.php
+++ _stable_/jeux/base/jeux_upgrade.php
@@ -1,17 +1,51 @@
 <?	
+$GLOBALS['jeux_base_version'] = 0.1;
+
 function jeux_install($install){
-	switch($install){
-		case 'install' :
+	$version_base = $GLOBALS['jeux_base_version'];
+	switch($install) {
+		case 'test':
+			return isset($GLOBALS['meta']['jeux_base_version']) AND ($GLOBALS['meta']['jeux_base_version']>=$version_base);
+		case 'install':
 			jeux_verifier_base();
-			
 			break;
-				};	
+		case 'uninstall':
+			jeux_vider_tables();
+			break;
 	}
-function jeux_verifier_base(){
-	include_spip('base/jeux_tables');
-	include_spip('base/create');
+}
+
+function jeux_vider_tables() {
 	include_spip('base/abstract_sql');
-	creer_base();	
+	spip_query("DROP TABLE jeux");
+	spip_query("DROP TABLE jeux_resultats");
+	effacer_meta('jeux_base_version');
+	ecrire_metas();
+}
+
+function jeux_verifier_base(){
+	$version_base = $GLOBALS['jeux_base_version'];
+	$current_version = 0.0;
+	if (   (!isset($GLOBALS['meta']['jeux_base_version']) )
+			|| (($current_version = $GLOBALS['meta']['jeux_base_version']) != $version_base)){
+		include_spip('base/jeux_tables');
+		if ($current_version==0.0){
+			include_spip('base/create');
+			include_spip('base/abstract_sql');
+			creer_base();
+			// ajout du champ 'nom' a la table spip_jeux, si pas deja existant
+			$desc = spip_abstract_showtable("spip_jeux", '', true);
+			if (!isset($desc['field']['nom'])){
+				spip_query("ALTER TABLE spip_jeux ADD `nom` text NOT NULL AFTER `date`");
+				// ajout d'un nom par defaut aux jeux existants
+				$res = spip_query ("SELECT id_jeu FROM spip_jeux");
+				while ($row = spip_fetch_array($res))
+					spip_query("UPDATE spip_jeux SET nom='Sans nom' WHERE id_jeu=".$row['id_jeu']);
+			}
+			ecrire_meta('jeux_base_version', $current_version=$version_base, 'non');
+			ecrire_metas();
+		}
 	}
+}
 	
 ?>
