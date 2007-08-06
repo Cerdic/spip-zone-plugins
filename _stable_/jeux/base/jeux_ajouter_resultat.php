fb5990dc270135175eb45f152b210dff4989b8df
patfr@ifrance.com <patfr@ifrance.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-08-06 22:39:42 +0000

    test si lire_config() existe... ok pour écraser le résultat par défaut ?
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@14274 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/jeux/base/jeux_ajouter_resultat.php
+++ _stable_/jeux/base/jeux_ajouter_resultat.php
@@ -1,33 +1,29 @@
 <?
-function jeux_ajouter_resulat($id_jeu,$resultat,$resultat_detaille=''){
-	$id_auteur =  $GLOBALS["auteur_session"]['id_auteur'];
-	if (!$id_auteur){return;}
-	
-	
-	$requete	= spip_fetch_array(spip_query("SELECT id_resultat,enregistrer_resultat FROM spip_jeux WHERE id_jeu =".$id_jeu));
-	$enregistrer_resultat 	= $requete['enregistrer_resultat'];
-	
-	
-	$id_resultat			= $requete['id_resultat']; 		
-	if ($enregistrer_resultat=='non'){return;}
-	 
-	if (lire_config('jeux/ecraser_resultat')!='non'){
-		
-		spip_query('INSERT into  spip_jeux_resultats (id_jeu,id_auteur,resultat_court,resultat_long) VALUES ('.$id_jeu.','.$id_auteur.',"'.$resultat.'","'.$resultat_detaille.'")'); 
-		}
+function jeux_ajouter_resulat($id_jeu, $resultat, $resultat_detaille=''){
+	$id_auteur = $GLOBALS["auteur_session"]['id_auteur'];
+	if (!$id_auteur) return;
+
+	$requete = spip_fetch_array(spip_query("SELECT id_resultat,enregistrer_resultat FROM spip_jeux WHERE id_jeu =".$id_jeu));
+	$enregistrer_resultat = $requete['enregistrer_resultat'];
+
+	$id_resultat = $requete['id_resultat']; 		
+	if ($enregistrer_resultat=='non') return;
+
+	if (function_exists('lire_config')) 
+		$ecraser_resultat = lire_config('jeux/ecraser_resultat');
 	else
-		{
+		$ecraser_resultat = 'oui';
+
+	if ($ecraser_resultat!='non') {
+		spip_query('INSERT into spip_jeux_resultats (id_jeu,id_auteur,resultat_court,resultat_long) VALUES ('.$id_jeu.','.$id_auteur.',"'.$resultat.'","'.$resultat_detaille.'")'); 
+	} else {
 		$requete = spip_fetch_array(spip_query("SELECT id_resultat FROM spip_jeux_resultats WHERE id_jeu =".$id_jeu." and id_auteur=".$id_auteur));
 		$id_resultat = $requete['id_resultat'];
 		
-		if (!$id_resultat){
-			spip_query('INSERT into  spip_jeux_resultats (id_jeu,id_auteur,resultat_court,resultat_long) VALUES ('.$id_jeu.','.$id_auteur.',"'.$resultat.'","'.$resultat_detaille.'")'); 
-			}
-		
-		else{
+		if (!$id_resultat)
+			spip_query('INSERT into spip_jeux_resultats (id_jeu,id_auteur,resultat_court,resultat_long) VALUES ('.$id_jeu.','.$id_auteur.',"'.$resultat.'","'.$resultat_detaille.'")'); 
+		else
 			spip_query('UPDATE spip_jeux_resultats SET resultat_court="'.$resultat.'",resultat_long="'.$resultat_detaille.'" WHERE id_resultat='.$id_resultat);
-			}
-		
-		}
 	}
+}
 ?>
