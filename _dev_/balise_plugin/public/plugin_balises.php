68d381b1d1a2bf896ee3c6e82625f88ca2b09e77
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-08-06 16:16:02 +0000

    En arriere toute avec quelques modifications pour plus de cohérence (ICON devient ICONE, EST_ACTIF renvoie oui ou non)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@14264 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/balise_plugin/public/plugin_balises.php
+++ _dev_/balise_plugin/public/plugin_balises.php
@@ -7,83 +7,83 @@
 // =======================================================================================================================================
 //
 include_spip('inc/plugin');
-
 function balise_PLUGIN($p) {
-	$dir_tous_plugins = array();
-	$inf_tous_plugins = array();
-	$plugins_valides = array();
-	$plugins_actifs = array();
-	$plugin_infos = array();
 
 	$plugin = interprete_argument_balise(1,$p);
 	if (!isset($plugin)) {
 		$p->code = "''";
 	}
 	else {
-		// plugin : doit etre un prefixe valide de plugin installe
-		$dir_tous_plugins = liste_plugin_files();
-		$plugins_valides = liste_plugin_valides($dir_tous_plugins, $inf_tous_plugins);
-		if (!array_key_exists(strtoupper($plugin), $plugins_valides)) 
-			$p->code = "''";
-		else {
-			// info : si vide prend la valeur par defaut 'est_actif'
-			$type_info = interprete_argument_balise(2,$p);
-			if (!isset($type_info)) $type_info = 'est_actif';
+		$type_info = interprete_argument_balise(2,$p);
+		if (!isset($type_info)) $type_info = 'est_actif';
+		$p->code = 'calcul_info_plugin('.$plugin.', '.$type_info.')';
+	}
+	$p->interdire_scripts = false;
+	return $p;
+}
 
-			// Determination du plugin dans la liste des plugins valides. Si plusieurs plugins de meme prefixe on choisit dans l'ordre
-			// - 1. Le plugin actif (il y en a forcement qu'un seul actif)
-			// - 2. Le plugin inactif recense dans la liste des plugins valides
-			$plugins_actifs = liste_plugin_actifs();
-			if (isset($plugins_actifs[strtoupper($plugin)])) {
-				$plugin_infos = $inf_tous_plugins[$plugins_actifs[strtoupper($plugin)]['dir']];
-				$plugin_infos['est_actif'] = TRUE;
-			}
-			else {
-				$plugin_infos = $inf_tous_plugins[$plugins_valides[strtoupper($plugin)]['dir']];
-				$plugin_infos['est_actif'] = FALSE;
-			}
+function calcul_info_plugin($plugin, $type_info) {
+	$dir_tous_plugins = array();
+	$inf_tous_plugins = array();
+	$plugins_valides = array();
+	$plugins_actifs = array();
+	$plugin_infos = array();
+	
+	// Validite du prefixe
+	$dir_tous_plugins = liste_plugin_files();
+	$plugins_valides = liste_plugin_valides($dir_tous_plugins, $inf_tous_plugins);
+	if (!array_key_exists(strtoupper($plugin), $plugins_valides)) 
+		return $valeur_info;
 
-			// Calcul  de l'information demandee
-			$valeur_info = "''";
-			switch (strtoupper($type_info)) {
-			case 'NOM':
-			    $valeur_info = propre($plugin_infos['nom']);
-			    break;
-			case 'VERSION':
-			    $valeur_info = $plugin_infos['version'];
-			    break;
-			case 'REVISION':
-			    $valeur_info = $plugin_infos['revision'];
-			    break;
-			case 'AUTEUR':
-			    $valeur_info = propre($plugin_infos['auteur']);
-			    break;
-			case 'ICON':
-			    $valeur_info = $plugin_infos['icon'];
-			    break;
-			case 'DESCRIPTION':
-			    $valeur_info = propre($plugin_infos['description']);
-			    break;
-			case 'LIEN':
-				$valeur_info = formate_lien_plugin($plugin_infos['lien']);
-			    break;
-			case 'ETAT':
-			    $valeur_info = formate_etat_plugin($plugin_infos['etat']);
-			    break;
-			case 'EST_ACTIF':
-			    $valeur_info = $plugin_infos['est_actif'];
-			    break;
-			case 'TOUT':
-			    $valeur_info = serialize($plugin_infos);
-			    break;
-			}
-		}
+	// Determination du plugin dans la liste des plugins valides. Si plusieurs plugins de meme prefixe on choisit dans l'ordre
+	// - 1. Le plugin actif (il y en a forcement qu'un seul actif)
+	// - 2. Le plugin inactif recense dans la liste des plugins valides
+	$plugins_actifs = liste_plugin_actifs();
+	if (isset($plugins_actifs[strtoupper($plugin)])) {
+		$plugin_infos = $inf_tous_plugins[$plugins_actifs[strtoupper($plugin)]['dir']];
+		$plugin_infos['est_actif'] = 'oui';
+	}
+	else {
+		$plugin_infos = $inf_tous_plugins[$plugins_valides[strtoupper($plugin)]['dir']];
+		$plugin_infos['est_actif'] = 'non';
 	}
 
-	$p->statut = 'php';
-	$p->interdire_scripts = false;
-	
-	return $p;
+	// Renvoi de l'information demandee
+	switch (strtoupper($type_info)) {
+		case 'NOM':
+		    $valeur_info = propre($plugin_infos['nom']);
+		    break;
+		case 'VERSION':
+		    $valeur_info = $plugin_infos['version'];
+		    break;
+		case 'REVISION':
+		    $valeur_info = $plugin_infos['revision'];
+		    break;
+		case 'AUTEUR':
+		    $valeur_info = propre($plugin_infos['auteur']);
+		    break;
+		case 'ICONE':
+		    $valeur_info = $plugin_infos['icon'];
+		    break;
+		case 'DESCRIPTION':
+		    $valeur_info = propre($plugin_infos['description']);
+		    break;
+		case 'LIEN':
+			$valeur_info = formate_lien_plugin($plugin_infos['lien']);
+		    break;
+		case 'ETAT':
+		    $valeur_info = formate_etat_plugin($plugin_infos['etat']);
+		    break;
+		case 'EST_ACTIF':
+		    $valeur_info = $plugin_infos['est_actif'];
+		    break;
+		case 'TOUT':
+		    $valeur_info = serialize($plugin_infos);
+		    break;
+		default:
+			$valeur_info = '';
+	}
+	return $valeur_info;
 }
 
 function formate_lien_plugin($lien) {
