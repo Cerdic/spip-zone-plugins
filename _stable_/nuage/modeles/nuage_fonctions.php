344ef6728b55fbf66971144a65a738505ecc9049
james@rezo.net <james@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-08-07 23:21:33 +0000

    forcément, j'ai tout mélangé :)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@14308 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/nuage/modeles/nuage_fonctions.php
+++ _stable_/nuage/modeles/nuage_fonctions.php
@@ -1,43 +1,5 @@
 <?php
 
-function critere_frequence_dist($idb, &$boucles, $crit) {
-	$not = $crit->not;
-
-	if ($not)
-		erreur_squelette(_T('zbug_info_erreur_squelette'), $crit->op);
-
-	//analyser chaque criteres de frequence
-	$boucle = &$boucles[$idb];
-	$nom = $boucle->type_requete;
-	$parent = $boucles[$idb]->id_parent;
-	$criteres = array();
-	while(list(,$p) = each($crit->param)) {
- 		$param = calculer_liste($p, array(), $boucles, $parent);
-		$type = preg_match(',^\(?\'(\w+)(\s*)?([!=<>]+)?(\s*)?,', $param, $regs) ? $regs[1] : 'articles';
-		$op = $regs[3] ? $regs[3] : '>=';
-		if($val = $regs[0] ? preg_replace(',' . preg_quote($regs[0]) . ',', '', $param) : 0) {
-			$val = preg_replace(',\'$,', '', $val);
-			$val = preg_replace(',^\'\s\.\s(.*)\)$,Um', '$1', $val);
-		}
-		//Trouver une jointure n:n (cad table spip_mots_articles)
-		if(in_array($type = $nom.'_'.$type, $boucle->jointures))
-			$criteres[] = array($op, $type, $val);
-	}
-	if(empty($criteres)) $criteres[0] = array('>=', 'articles', 0);
-	
-	//composer la requete pour chaque jointure
-//	$id_table = $boucle->id_table . '.' . $boucle->primary;
-	$cpt = 0;
-	foreach($criteres as $critere) {
-		$frequence = 'frequence'.(++$cpt);
-		$boucle->select[]= 'COUNT('.$frequence.'.'.$id.') AS '.$frequence;
-		$boucle->from[$frequence] = "spip_'.$type_requete.'_".$type."s";
-		$boucle->where[] = array("'='", "'".$id_table."'", "'freq.".$boucle->primary."'");
-		$boucle->group[] = $id_table;
-		$boucle->having[] = array("'".$op."'", "'frequence'",$op_val);
-	}
-}
-
 function balise_NUAGE_dist($p) {
   $filtre = chercher_filtre('nuage');
 	$p->interdire_scripts = false;
