b165e6b5fe8550c545e00a546126f4c5b0669dea
crovvfoot@gmail.com <crovvfoot@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-08-08 15:17:19 +0000

    Internationalisation des diminutifs des jour : <:agendax:lundi_court:>...<:agendax:dimanche_court:> pour les titres des colones
    Passage en squelette du script principale agendax.js pour permettre l'internationalisation
    
    Ajout de la possibilité de paramétrer les flèche, les styles des case (vide ou remplie) et le gif de  chargement.
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@14318 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/agendax/agendax.php
+++ _dev_/agendax/agendax.php
@@ -2,7 +2,7 @@
 
 	function Agendax_insert_head($flux) {
 		$flux .= '<link rel="stylesheet" href="'.find_in_path("agendax.css").'" type="text/css" />
-		<script src="'.find_in_path("agendax.js").'" type="text/javascript"></script>';
+		<script src="?page=agendax.js" type="text/javascript"></script>';
 		return $flux;
 	}
 
