c5e0a2f9d1f9b7d221c66fd8af12c10d89126081
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-08-10 21:26:07 +0000

    un peu mieux l'intégration à inscription2
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@14384 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _dev_/echoppe/lang/echoppe_fr.php
@@ -0,0 +1,14 @@
+<?php
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// formulaire inscription2
+'coord_livraison' => 'Coordonn&eacute;es de livraison',
+'adresse_livraison' => 'Adresse (livraison)',
+
+'xxx' => 'xxx'
+
+);
+
+
+?>
