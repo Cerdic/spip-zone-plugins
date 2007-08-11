7777ef33526c6c71e2071f7bf05e477348ca17b1
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-08-11 17:38:20 +0000

    les éléments d'échoppe pour un profil crayonnable
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@14412 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/echoppe/lang/echoppe_fr.php
+++ _dev_/echoppe/lang/echoppe_fr.php
@@ -6,6 +6,10 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 'coord_livraison' => 'Coordonn&eacute;es de livraison',
 'adresse_livraison' => 'Adresse (livraison)',
 
+// Formulaire profil : controleurs et vues
+'vos_contacts_livraison' => 'Vos contacts de livraison',
+'votre_adresse_livraison' => 'Votre adresse de livraison',
+
 'xxx' => 'xxx'
 
 );
