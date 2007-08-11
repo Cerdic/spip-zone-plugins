40e42ab5af034e048db544eb1b993e6bf7653ba6
booz@rezo.net <booz@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-08-11 18:37:57 +0000

    voila ca marche mieux
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@14419 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/abonnement/abonnement_options.php
+++ _dev_/abonnement/abonnement_options.php
@@ -16,24 +16,25 @@
 function traiter_message_banque($id_abonne,$validation_paiement){
 
 
-$abonne = spip_fetch_array(spip_query("SELECT a.nom_famille, a.prenom, a.adresse, a.code_postal, a.ville, a.pays, a.telephone, b.email FROM `spip_auteurs_elargis` a, `spip_auteurs` b WHERE a.id='$id_abonne' AND a.id_auteur = b.id_auteur") );
+$abonne = spip_fetch_array(spip_query("SELECT a.nom_famille, a.prenom, a.adresse, a.code_postal, a.ville, a.pays, a.telephone, a.commentaire, a.validite, b.email FROM `spip_auteurs_elargis` a, `spip_auteurs` b WHERE a.id='$id_abonne' AND a.id_auteur = b.id_auteur") );
 $abonnement = spip_fetch_array(spip_query("SELECT a.duree, a.montant, a.libelle FROM `spip_abonnements` a, `spip_auteurs_elargis_abonnements` b WHERE b.id_auteur_elargi = '$id_abonne' AND a.id_abonnement = b.id_abonnement") );
 
 $duree = $abonnement['duree'] ;
 $statut_abonnement = ($validation_paiement == "ok")? 'abonne' : 'prospect' ;
+$validite = ($validation_paiement == "ok")? "DATE_ADD(CURRENT_DATE, INTERVAL ".$duree." MONTH)" : "'".$abonne['validite']."'" ;
 
 //echo "UPDATE `spip_auteurs_elargis` SET statut_abonnement='$statut_abonnement', statut_paiement='$validation_paiement', validite = DATE_ADD(CURRENT_DATE, INTERVAL ".$duree." MONTH) WHERE id='$id_abonne'" ;
 
 // fixer la date de validite et le statut de paiement, et des zones acces restreint selon l'abonnement a l'occasion
-spip_query("UPDATE `spip_auteurs_elargis` SET statut_abonnement='$statut_abonnement', statut_paiement='$validation_paiement', validite = DATE_ADD(CURRENT_DATE, INTERVAL ".$duree." MONTH) WHERE id='$id_abonne'") ;
+spip_query("UPDATE `spip_auteurs_elargis` SET statut_abonnement='$statut_abonnement', statut_paiement='$validation_paiement', validite = $validite WHERE id='$id_abonne'") ;
 
 	//envoyer un mail a l'admin et a l'abonne
-	abonnement_envoyer_mails_confirmation($validation_paiement,$abonne);
+	abonnement_envoyer_mails_confirmation($validation_paiement,$abonne,$abonnement);
 
 }
 
 
-function abonnement_envoyer_mails_confirmation($validation_paiement,$abonne){
+function abonnement_envoyer_mails_confirmation($validation_paiement,$abonne,$abonnement){
 
 	include_spip('inc/charsets');
 	include_spip('inc/mail');
@@ -60,7 +61,7 @@ function abonnement_envoyer_mails_confirmation($validation_paiement,$abonne){
 
 	if($validation_paiement == "ok"){
 	
-			$sujet= "[".$nom_expediteur."-abo] Nouvel abonn&eacute; : ".$abonne['nom'] ;
+			$sujet= "[".$nom_expediteur."-abo] Nouvel abonn&eacute; : ".$abonne['nom_famille'] ;
 			//au webmaster
 			$message = "Un abonn&eacute; vient de valider son abonnement :\n\nNom : ".$abonne['nom_famille']."\nPr&eacute;nom : ".$abonne['prenom']."\n\nAdresse: \n".$abonne['adresse']."\n".$abonne['code_postal']." ".$abonne['ville']." ".$abonne['pays']
 			."\n\nEmail : ".$abonne['email']
@@ -77,13 +78,13 @@ function abonnement_envoyer_mails_confirmation($validation_paiement,$abonne){
 	
 	}elseif($validation_paiement == "erreur_bank"){
 	
-	$sujet= "[".$nom_expediteur."-abo] Echec abonnement : ".$abonne['nom'] ;
+	$sujet= "[".$nom_expediteur."-abo] Echec abonnement : ".$abonne['nom_famille'] ;
 			//au webmaster
-			$message = "Un abonn&eacute; n'a pas pu valider son abonnement (refus du paiement par la banque) :\n\nNom : ".$abonne['nom']."\nPr&eacute;nom : ".$abonne['prenom']."\n\nAdresse : \n".$abonne['rue']."\n".$abonne['code_postal']." ".$abonne['ville']." ".$abonne['pays']
+			$message = "Un abonn&eacute; n'a pas pu valider son abonnement (refus du paiement par la banque) :\n\nNom : ".$abonne['nom_famille']."\nPr&eacute;nom : ".$abonne['prenom']."\n\nAdresse : \n".$abonne['adresse']."\n".$abonne['code_postal']." ".$abonne['ville']." ".$abonne['pays']
 			."\n\nEmail : ".$abonne['email']
 			."\nT&eacute;l&eacute;phone : ".$abonne['telephone']
 			."\n\nAbonnement : ".$abonnement['libelle']
-			."\n\nCommentaire : ".$abonne['remarques'];
+			."\n\nCommentaire : ".$abonne['commentaire'];
 			envoyer_mail ( $adresse_expediteur, $sujet, $message, $from = $expediteur, $headers = $entetes );
 			
 			// au demandeur
