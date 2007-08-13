767c1532bf3c34e427cc690e9c5f7441937543d0
maieulrouquette@tele2.fr <maieulrouquette@tele2.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-08-13 00:20:58 +0000

    une fonctyion de "compactage" de resultat
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@14467 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _stable_/jeux/base/jeux_compacter.php
@@ -0,0 +1,17 @@
+<?
+function jeux_compacter_tout_jeu($id_jeu){
+	include_spip('public/assembler');
+	$liste = recuperer_fond('fonds/jeux_compacter',array('id_jeu'=>$id_jeu));
+	spip_query('DELETE FROM spip_jeux_resultats WHERE  id_resultat NOT IN '.$liste.' and id_jeu='.$id_jeu);
+	}
+function jeux_compacter_tout_auteur($id_auteur){
+	include_spip('public/assembler');
+	$liste = recuperer_fond('fonds/jeux_compacter',array('id_auteur'=>$id_auteur));
+	spip_query('DELETE FROM spip_jeux_resultats WHERE  id_resultat NOT IN '.$liste.'and id_auteur='.$id_auteur);	
+	}
+function jeux_compacter_tout_tout(){
+	include_spip('public/assembler');
+	$liste = recuperer_fond('fonds/jeux_compacter');
+	spip_query('DELETE FROM spip_jeux_resultats WHERE  id_resultat NOT IN '.$liste);
+	}
+?>
--- _stable_/jeux/exec/jeux_gerer_resultats.php
+++ _stable_/jeux/exec/jeux_gerer_resultats.php
@@ -8,9 +8,12 @@ function exec_jeux_gerer_resultats(){
 	// les boutons ... le pire ennemi des ados parait-il
 	$supprimer_tout = _request('supprimer_tout');
 	$supprimer_confirmer = _request('supprimer_confirmer');
-	
+	$compacter_tout 	 = _request('compacter_tout');
+	$compacter_confirmer = _request('compacter_confirmer');
 	($supprimer_confirmer) ? $bouton = 'supprimer_confirmer' : $bouton = '';
 	($bouton == '' and $supprimer_tout) ? $bouton = 'supprimer_tout' : $bouton = $bouton;
+	($bouton == '' and $compacter_tout) ? $bouton = 'compacter_tout' : $bouton = $bouton;
+	($bouton == '' and $compacter_confirmer) ? $bouton = 'compacter_confirmer' : $bouton = $bouton;
 	
 	$id_jeu 	= _request('id_jeu');
 	$id_auteur  = _request('id_auteur');
@@ -29,14 +32,19 @@ function gerer_resultat_tous($bouton){
 		include_spip('inc/headers');
 		redirige_par_entete(generer_url_ecrire('jeux_tous'));
 	}
-
+	if ($bouton == 'compacter_confirmer'){
+		include_spip('base/jeux_compacter');
+		jeux_compacter_tout_tout();
+		include_spip('inc/headers');
+		redirige_par_entete(generer_url_ecrire('jeux_tous'));
+	}	
 	debut_page(_T("jeux:gerer_resultats_tout"));
 	debut_gauche();
 	boite_infos_accueil();
 	
 	creer_colonne_droite();
 	debut_droite();
-	if ($bouton == 'supprimer_tout') gros_titre(_T("jeux:confirmation"));
+	if ($bouton == 'supprimer_tout'  or $bouton == 'compacter_tout') gros_titre(_T("jeux:confirmation"));
 	debut_cadre_relief();
 	gros_titre(_T("jeux:gerer_resultats_tout"));
 	formulaire_suppression($bouton, 'tout');
@@ -56,7 +64,7 @@ function gerer_resultats_auteur($id_auteur, $bouton){
 		fin_page();
 		return;
 	}
-
+	
 	// aïe, aïe, on efface tout pour $id_auteur
 	if ($bouton == 'supprimer_confirmer'){
 		include_spip('base/jeux_supprimer');
@@ -64,7 +72,12 @@ function gerer_resultats_auteur($id_auteur, $bouton){
 		include_spip('inc/headers');
 		redirige_par_entete(generer_url_ecrire('jeux_resultats_auteur', 'id_auteur='.$id_auteur, true));
 	}
-
+	if ($bouton == 'compacter_confirmer'){
+		include_spip('base/jeux_compacter');
+		jeux_compacter_tout_auteur($id_auteur);
+		include_spip('inc/headers');
+		redirige_par_entete(generer_url_ecrire('jeux_resultats_auteur', 'id_auteur='.$id_auteur, true));
+	}
 	debut_page(_T("jeux:gerer_resultats_auteur",array('nom'=>$nom)));
 		
 	debut_gauche();
@@ -74,7 +87,7 @@ function gerer_resultats_auteur($id_auteur, $bouton){
 
 	creer_colonne_droite();
 	debut_droite();
-	if ($bouton == 'supprimer_tout') gros_titre(_T("jeux:confirmation"));
+	if ($bouton == 'supprimer_tout'  or $bouton == 'compacter_tout') gros_titre(_T("jeux:confirmation"));
 	debut_cadre_relief();
 	gros_titre(_T("jeux:gerer_resultats_auteur",array('nom'=>$nom)));
 	formulaire_suppression($bouton, 'auteur');
@@ -101,6 +114,13 @@ function gerer_resultat_jeux($id_jeu, $bouton){
 		include_spip('inc/headers');
 		redirige_par_entete(generer_url_ecrire('jeux_resultats_jeu', 'id_jeu='.$id_jeu, true));
 	}
+	if ($bouton == 'compacter_confirmer'){
+		include_spip('base/jeux_compacter');
+		jeux_compacter_tout_jeu($id_jeu);
+		include_spip('inc/headers');
+		redirige_par_entete(generer_url_ecrire('jeux_resultats_jeu', 'id_jeu='.$id_jeu, true));
+	}
+
 
 	debut_page(_T("jeux:gerer_resultats_jeu",array('id'=>$id_jeu,'nom'=>$nom)));
 			
@@ -110,7 +130,7 @@ function gerer_resultat_jeux($id_jeu, $bouton){
 
 	creer_colonne_droite();
 	debut_droite();
-	if ($bouton == 'supprimer_tout') gros_titre(_T("jeux:confirmation"));
+	if ($bouton == 'supprimer_tout' or $bouton == 'compacter_tout') gros_titre(_T("jeux:confirmation"));
 	debut_cadre_relief();
 	
 	echo gros_titre(_T("jeux:gerer_resultats_jeu",array('id'=>$id_jeu,'nom'=>$nom)));
@@ -124,7 +144,7 @@ function formulaire_suppression($bouton, $type){
 	debut_cadre_formulaire();
 	echo "<form method='post'  name='supprimer_resultat'>";
 	debut_cadre_relief();	
-
+	
 	if ($bouton == 'supprimer_tout'){
 		$res = "<br/><input type='submit' name='supprimer_confirmer' value='"._T('jeux:supprimer_confirmer')."' class='fondo' />";
 		echo
@@ -137,7 +157,22 @@ function formulaire_suppression($bouton, $type){
 			$res,
 			fin_boite_alerte(),
 			"</div>";
-	} else {
+	} 
+	else if ($bouton == 'compacter_tout'){
+		$res = "<br/><input type='submit' name='compacter_confirmer' value='"._T('jeux:compacter_confirmer')."' class='fondo' />";
+		echo
+			_T('jeux:confirmation_compacter_'.$type),
+			"\n<div style='text-align: center'>",
+			debut_boite_alerte(),
+			"\n<div class='serif'>",
+			"\n<b>"._T('jeux:compacter_tout')."&nbsp;!</b>",
+			"\n</div>",
+			$res,
+			fin_boite_alerte(),
+			"</div>";
+	}
+	
+	else {
 		$res = "<br/><input type='submit' name='supprimer_tout' value='"._T('jeux:supprimer_tout_'.$type)."' class='fondo' />";
 		echo
 			_T('jeux:explication_supprimer_'.$type),
@@ -145,6 +180,18 @@ function formulaire_suppression($bouton, $type){
 			debut_boite_alerte(),
 			"\n<div class='serif'>",
 			"\n<b>"._T('avis_suppression_base')."&nbsp;!</b>",
+			"\n</div>",
+			$res,
+			fin_boite_alerte(),
+			"</div>";
+			
+		$res = "<br/><input type='submit' name='compacter_tout' value='"._T('jeux:compacter_tout_'.$type)."' class='fondo' />";
+		echo
+			_T('jeux:explication_supprimer_'.$type),
+			"\n<div style='text-align: center'>",
+			debut_boite_alerte(),
+			"\n<div class='serif'>",
+			"\n<b>"._T('jeux:compacter_tout')."&nbsp;!</b>",
 			"\n</div>",
 			$res,
 			fin_boite_alerte(),
