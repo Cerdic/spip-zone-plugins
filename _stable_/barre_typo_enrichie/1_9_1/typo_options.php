840d4f05575413ae898dd07a841251365dd78c2e
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-08-13 10:07:40 +0000

    La BTE passe en désuétude ;-)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@14484 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/barre_typo_enrichie/1_9_1/typo_options.php
+++
@@ -1,8 +0,0 @@
-<?php
-$GLOBALS['spip_pipeline']['BarreTypoEnrichie_boutonsavances']='';
-$GLOBALS['spip_pipeline']['BarreTypoEnrichie_toolbox']='';
-$GLOBALS['spip_pipeline']['BarreTypoEnrichie_tous']='';
-$GLOBALS['spip_pipeline']['BarreTypoEnrichie_avancees']='';
-$GLOBALS['spip_pipeline']['BarreTypoEnrichie_ecrire']='';
-$GLOBALS['spip_pipeline']['BarreTypoEnrichie_forum']='';
-?>
