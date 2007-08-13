5bb00e2a82a9ce6d95fec9537fc6a58a52060874
maieulrouquette@tele2.fr <maieulrouquette@tele2.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-08-13 11:22:51 +0000

    avec le fichier, c'est mieux ! et puis tant qu'on y est, on soumet aussi les icones à autorisation
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@14488 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/jeux/exec/inc_boites_infos.php
+++ _stable_/jeux/exec/inc_boites_infos.php
@@ -45,10 +45,13 @@ function jeux_navigation_pagination() {
 function boite_infos_auteur($id_auteur, $nom) {
 	debut_boite_info();
 	echo "<strong>$nom</strong><br />",
-		icone_horizontale(_T('jeux:infos_auteur'),generer_url_ecrire('auteur_infos','id_auteur='.$id_auteur),find_in_path('images/auteur-24.gif')),
-		(_request('exec')=='jeux_gerer_resultats'
-			?icone_horizontale(_T('jeux:voir_ses_resultats'),generer_url_ecrire('jeux_resultats_auteur','id_auteur='.$id_auteur),find_in_path('img/jeu-laurier.png'))
-			:icone_horizontale(_T('jeux:gerer_ses_resultats'),generer_url_ecrire('jeux_gerer_resultats','id_auteur='.$id_auteur),find_in_path('img/jeu-laurier.png')) );
+	icone_horizontale(_T('jeux:infos_auteur'),generer_url_ecrire('auteur_infos','id_auteur='.$id_auteur),find_in_path('images/auteur-24.gif'));
+	
+	if (_request('exec')=='jeux_gerer_resultats')
+		{icone_horizontale(_T('jeux:voir_ses_resultats'),generer_url_ecrire('jeux_resultats_auteur','id_auteur='.$id_auteur),find_in_path('img/jeu-laurier.png'));
+		}
+	else if(autoriser('gererresultats','auteur',$id_auteur)) {icone_horizontale(_T('jeux:gerer_ses_resultats'),generer_url_ecrire('jeux_gerer_resultats','id_auteur='.$id_auteur),find_in_path('img/jeu-laurier.png')) ;}
+	
 	fin_boite_info();
 }
 
@@ -62,10 +65,11 @@ function boite_infos_jeu($id_jeu, $nom) {
 		(_request('exec')=='jeux_edit'?'':
 			icone_horizontale(_T('jeux:modifier_ce_jeu'),generer_url_ecrire('jeux_edit','id_jeu='.$id_jeu),find_in_path('img/jeu-crayon.png')) ),
 		( (_request('exec')=='jeux_resultats_jeu') || !$nb_res?'':
-			icone_horizontale(_T('jeux:voir_ses_resultats'),generer_url_ecrire('jeux_resultats_jeu','id_jeu='.$id_jeu),find_in_path('img/jeu-laurier.png')) ),
-		( (_request('exec')=='jeux_gerer_resultats') || !$nb_res?'':
+			icone_horizontale(_T('jeux:voir_ses_resultats'),generer_url_ecrire('jeux_resultats_jeu','id_jeu='.$id_jeu),find_in_path('img/jeu-laurier.png')) );
+			
+		if (autoriser('gererresultats')){echo ( (_request('exec')=='jeux_gerer_resultats') || !$nb_res?'':
 			icone_horizontale(_T('jeux:gerer_ses_resultats'),generer_url_ecrire('jeux_gerer_resultats','id_jeu='.$id_jeu),find_in_path('img/jeu-laurier.png'))
-	);
+	);}
 	fin_boite_info();
 }
 
@@ -78,12 +82,13 @@ function boite_infos_accueil() {
 		icone_horizontale(_T('jeux:nouveau_jeu'),generer_url_ecrire('jeux_edit','nouveau=oui'),find_in_path('img/jeu-nouveau.png')) ),
 		// 'liste des jeux' sur les pages hors 'jeux_tous'
 		( _request('exec')=='jeux_tous'?'':
-		icone_horizontale(_T('jeux:jeux_tous'),generer_url_ecrire('jeux_tous'),find_in_path('img/jeux-tous.png')) ),
+		icone_horizontale(_T('jeux:jeux_tous'),generer_url_ecrire('jeux_tous'),find_in_path('img/jeux-tous.png')) );
 		// 'gerer les resultats' sur les pages hors 'jeux_gerer_resultats' si 1 jeu au moins est present
+		if (autoriser('gererresultats')){
 		( (_request('exec')=='jeux_gerer_resultats') || !$nb_res?'':
-			icone_horizontale(_T('jeux:gerer_resultats'),generer_url_ecrire('jeux_gerer_resultats'),find_in_path('img/jeu-laurier.png')) ),
+			icone_horizontale(_T('jeux:gerer_resultats'),generer_url_ecrire('jeux_gerer_resultats'),find_in_path('img/jeu-laurier.png')) );}
 		// 'configurer le plugin' uniquement sur la page 'jeux_tous' si lire_config() existe
-		( (_request('exec')!='jeux_tous') || !function_exists('lire_config')?'':
+		echo ( (_request('exec')!='jeux_tous') || !function_exists('lire_config')?'':
 		icone_horizontale(_T('jeux:configurer_jeux'),generer_url_ecrire('cfg','cfg=jeux'),find_in_path('img/jeu-cfg.png'))
 		);
 	fin_boite_info();
@@ -93,7 +98,7 @@ function boite_infos_jeux_tous() {
 	debut_boite_info();
 	echo icone_horizontale(_T('jeux:nouveau_jeu'),generer_url_ecrire('jeux_edit','nouveau=oui'),find_in_path('img/jeu-nouveau.png'));
 	echo icone_horizontale(_T('jeux:configurer_jeux'),generer_url_ecrire('cfg','cfg=jeux'),find_in_path('img/jeu-cfg.png'));
-	echo icone_horizontale(_T('jeux:gerer_resultats'),generer_url_ecrire('jeux_gerer_resultats'),find_in_path('img/jeu-laurier.png'));
+	if (autoriser('gererresultats')) {echo icone_horizontale(_T('jeux:gerer_resultats'),generer_url_ecrire('jeux_gerer_resultats'),find_in_path('img/jeu-laurier.png'));}
 	fin_boite_info();
 }
 
