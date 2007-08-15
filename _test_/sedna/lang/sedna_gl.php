a58bd5036f3ee646198d14ddf0ed672e22ecdbf2
berio@alg-a.org <berio@alg-a.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-08-15 15:30:11 +0000

    galician traduction
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@14606 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _test_/sedna/lang/sedna_gl.php
@@ -0,0 +1,40 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// pour la contrib sedna
+// esto es un archivo de idioma SPIP para la contrib sedna
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+'aff_resume' => 'Mostrar os resumes',
+'afficher_sources' => 'Mostrar esta fonte',
+'annee' => 'Ano',
+'articles_recents_court' => 'Art&iacute;gos recentes',
+'connexion' => 'Conexi&oacute;n/Inscribirse',
+'deconnexion' => 'Desconexi&oacute;n',
+'deuxjours' => 'Dous d&iacute;as',
+'derniere_syndication' => '&Uacute;ltima sindicaci&oacute;n deste sitio efectuada',
+'liens' => 'art&iacute;go',
+'liens_pluriel' => 'art&iacute;gos',
+'masquer_resume' => 'Ocultar os resumes',
+'masquer_sources' => 'Ocultar esta fonte',
+'mois' => 'Mes',
+'pas_articles' => '&iexcl;Non hai art&iacute;gos neste per&iacute;odo!',
+'pas_synchro' => 'Non sincronizar',
+'preferences' => 'Preferencias',
+'semaine' => 'Semana',
+'sources' => 'Fluxos',
+'synchro' => 'Sincronizar',
+'synchro_titre' => 'Gardar no sitio a lista de artígos lidos',
+'syndication_fait' => 'Sindicaci&oacute;n efectuada',
+'syndication_ajour' => 'Actualizar agora',
+'derniere_syndication' => '&Uacute;ltima sindicaci&oacute;n:&nbsp;',
+'toutes' => 'Todos',
+
+
+'probleme_de_syndication' => 'problema de sindicaci&oacute;n'
+
+);
+
+
+?>
