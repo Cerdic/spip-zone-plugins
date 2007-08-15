6d0d40abb4432e7b0e2abd1a62d5c1f6cde4f7ba
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-08-15 12:48:35 +0000

    "un exemple de bouton defini via plugin.xml"
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@14600 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/w3c_go_home/w3cgh_pipeline.php
+++ _stable_/w3c_go_home/w3cgh_pipeline.php
@@ -1,18 +1,20 @@
 <?php
 	/* public static */
 	function w3cgh_ajouter_boutons($boutons_admin) {
-		// si on est admin
-		if ($GLOBALS['connect_statut'] == "0minirezo" && $GLOBALS["connect_toutes_rubriques"]
-		AND $GLOBALS["options"]=="avancees") {
-			if (!defined(_DIR_PLUGIN_W3CGH))
-				$icone = find_in_path('/images/w3cgh-icone.gif');
-			else
-				$icone = _DIR_PLUGIN_W3CGH."/images/w3cgh-icone.gif";
-		  // on voit le bouton dans la barre "naviguer"
-			$boutons_admin['configuration']->sousmenu['w3c_go_home']= new Bouton(
-			$icone,  // icone
-			_T('w3cgh:titre_page')	// titre
-			);
+		if (version_compare($spip_version_code,'1.9260','<')){
+			// si on est admin
+			if ($GLOBALS['connect_statut'] == "0minirezo" && $GLOBALS["connect_toutes_rubriques"]
+			AND $GLOBALS["options"]=="avancees") {
+				if (!defined(_DIR_PLUGIN_W3CGH))
+					$icone = find_in_path('/images/w3cgh-icone.gif');
+				else
+					$icone = _DIR_PLUGIN_W3CGH."/images/w3cgh-icone.gif";
+			  // on voit le bouton dans la barre "naviguer"
+				$boutons_admin['configuration']->sousmenu['w3c_go_home']= new Bouton(
+				$icone,  // icone
+				_T('w3cgh:titre_page')	// titre
+				);
+			}
 		}
 		return $boutons_admin;
 	}
