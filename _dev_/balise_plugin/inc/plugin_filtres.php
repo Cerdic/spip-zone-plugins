0614bd37ae99006fd680123310fa6a04c7bcec0a
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-08-16 10:19:47 +0000

    Mise en cohérence de la balise avec le code SPIP de la prochaine version. Attention changements importants de son comportements (uniquement plugins actifs, infos non formatées, est_retroune 1 ou 0). Passage en 0.2
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@14630 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _dev_/balise_plugin/inc/plugin_filtres.php
@@ -0,0 +1,56 @@
+<?php
+// =======================================================================================================================================
+// Balise : #PLUGIN
+// =======================================================================================================================================
+// Auteur: Smellup
+// Fonction : retourne une info d'un plugin donne
+// =======================================================================================================================================
+//
+include_spip('inc/plugin');
+
+function calcul_info_plugin($plugin, $type_info) {
+	$plugin = strtoupper($plugin);
+	$plugins_actifs = liste_plugin_actifs();
+
+	if(!$plugin)
+		return serialize(array_keys($plugins_actifs));
+	if(!empty($plugins_actifs[$plugin]))
+		if($type_info == 'est_actif')
+			return $plugins_actifs[$plugin] ? 1 : 0;
+		else
+			return $plugins_actifs[$plugin][$type_info];
+}
+
+function formate_lien_plugin($lien) {
+	$ret = NULL;
+	if (trim($lien)) {
+		if (preg_match(',^https?://,iS', $lien))
+			$ret = propre("[->".$lien."]");
+		else
+			$ret = propre($lien);
+	}
+	return $ret;
+}
+
+function formate_etat_plugin($etat) {
+	$ret = NULL;
+	if (!isset($etat))
+		$etat = 'dev';
+	switch ($etat) {
+		case 'experimental':
+			$ret = _T('plugin_etat_experimental');
+			break;
+		case 'test':
+			$ret = _T('plugin_etat_test');
+			break;
+		case 'stable':
+			$ret = _T('plugin_etat_stable');
+			break;
+		default:
+			$ret = _T('plugin_etat_developpement');
+			break;
+	}
+	return $ret;
+}
+
+?>
