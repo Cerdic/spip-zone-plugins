73d043453ea69a2462f408a812b90ae1215cfdd1
dani@belvil.net <dani@belvil.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-08-19 21:21:41 +0000

    Traduction des fichiers de lang et de plugin.xml en es et en
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@14699 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _dev_/gis/lang/gis_en.php
@@ -0,0 +1,19 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP nomme admin_lang genere le NOW()
+// langue / language = fr
+// Traduction -- Pierre FICHES <pierre.fiches@free.fr>
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+//elements de formulaire pour editer les coordonnees
+'clave_engadida' => 'Added key: ',
+'clic_mapa' => 'Clic on the map to change coordinates',
+'clic_desplegar' => 'unfold',
+'configurar_gis' => 'Configure Gis Plugin',
+'conseguir' => '(obtain)',
+'cambiar' => 'change coordinates',
+'boton_actualizar' => 'update',
+'default_geoloc' => 'Default position of maps:'
+);
+
+?>
