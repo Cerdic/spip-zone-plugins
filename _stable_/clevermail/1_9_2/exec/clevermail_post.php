2cd69d3da25f4e5890416ff1b6d80f1e92695d95
tbeaumanoir@gmail.com <tbeaumanoir@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-08-23 09:46:39 +0000

    Simplification des différents tests
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@14766 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/clevermail/1_9_2/exec/clevermail_post.php
+++ _stable_/clevermail/1_9_2/exec/clevermail_post.php
@@ -9,25 +9,16 @@
 	 **/
 
 function exec_clevermail_post() {
-
 	if (isset($_GET['pst_id'])) {
-		if (isset($_GET['mode'])) {
-			$mode = $_GET['mode'];
-		} else {
-			$mode = 'text';
-		}
-		if ($mode == 'text') {
-			header('Content-type: text/plain; charset='.lire_meta('charset'));
-		} else {
-			header('Content-type: text/html; charset='.lire_meta('charset'));
-		}
 		$post = spip_fetch_array(spip_query("SELECT * FROM cm_posts WHERE pst_id = ".$_GET['pst_id']));
 		if (is_array($post)) {
 			$list = spip_fetch_array(spip_query("SELECT * FROM cm_lists WHERE lst_id = ".$post['lst_id']));
-			if ($mode == 'text') {
-				echo $post['pst_text'];
-			} else {
+			if (isset($_GET['mode']) && $_GET['mode']=='html') {
+				header('Content-type: text/html; charset='.lire_meta('charset'));
 				echo $post['pst_html'];
+			} else {
+				header('Content-type: text/plain; charset='.lire_meta('charset'));
+				echo $post['pst_text'];
 			}
 		} else {
 		    echo 'Invalid post identifier.';
