a635fede99f07ec82e5a9808039a0d8c0f955e5f
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-08-24 00:16:50 +0000

    grosse mise à jour ... on est plus uniquement censé tagger des articles ... les autres objets spip sont autorisés aussi
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@14792 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/spipicious_jquery/balise/formulaire_spipicious_ajax.php
+++ _dev_/spipicious_jquery/balise/formulaire_spipicious_ajax.php
@@ -3,43 +3,50 @@
 if (!defined("_ECRIRE_INC_VERSION")) return;	#securite
 
 function balise_FORMULAIRE_SPIPICIOUS_AJAX ($p) {
-	return calculer_balise_dynamique($p,'FORMULAIRE_SPIPICIOUS_AJAX', array('id_article'));
+	$p = calculer_balise_dynamique($p,'FORMULAIRE_SPIPICIOUS_AJAX', array('id_document','id_rubrique', 'id_forum', 'id_article', 'id_breve', 'id_syndic'));
+	return $p;
 }
 
-function balise_FORMULAIRE_SPIPICIOUS_AJAX_stat($args, $filtres) {  
-	// Pas d'id_article ? Erreur de squelette 
-	if (!$args[0])
-		return erreur_squelette(
-			_T('zbug_champ_hors_motif',
-				array ('champ' => '#FORMULAIRE_SPIPICIOUS_AJAX',
-					'motif' => 'ARTICLES')), '');
-	return $args;
-}
-
-function balise_FORMULAIRE_SPIPICIOUS_AJAX_dyn($id_article ) {
+function balise_FORMULAIRE_SPIPICIOUS_AJAX_dyn($id_document,$id_rubrique,$id_forum,$id_article,$id_breve,$id_syndic) {
 	$autorise = lire_config('spipicious/people');
 	
 	if (!$GLOBALS["auteur_session"] OR !in_array($GLOBALS['auteur_session']['statut'],$autorise)) {
 		return '';
 	} else {
-		$auteur_nom = $GLOBALS['auteur_session']['nom'];
-		$auteur_id = $GLOBALS['auteur_session']['email'];
+		$auteur_id = $GLOBALS['auteur_session']['id_auteur'];
 		$auteur_statut = $GLOBALS['auteur_session']['statut'];
 	}
 	
 	include_spip('spipicious_fonctions');
 
+	$ids = array();
+	if ($id_rubrique > 0 AND ($id_article OR $id_breve OR $id_syndic))
+		$id_rubrique = 0;
+	foreach (array('id_article', 'id_breve', 'id_rubrique', 'id_syndic', 'id_forum') as $o) {
+		if ($x = intval($$o)) {
+			$ids[$o] = $x;
+			$id = $x;
+			$type = str_replace('id_', '', $o);
+		}
+	}
+	
+	if ($id_document) {
+		$type = 'document';
+		$id = $id_document;
+		$ids['id_document'] = $id;
+	}
+	
+	$table_mot = 'spip_mots_'.$type.'s';
+	
 	//recuperation des variables utiles
 	$tags = _request('tags');
 	$id_groupe_tags = _request('select_groupe');
 	$type_groupe_tags = _request('type_groupe');
 	$add_tags = _request('add_tags');
 	
-	$autorise = lire_config('spipicious/people');
-	
 	if ($add_tags && $auteur_id){
 	
-	spip_query("DELETE FROM spip_spipicious WHERE id_auteur='$auteur_id' AND id_article='$id_article' "); // on efface les anciens triplets de cet auteur sur cet article 
+	spip_query("DELETE FROM spip_spipicious WHERE id_auteur='$auteur_id' AND id_$type='$id' "); // on efface les anciens triplets de cet auteur sur cet article 
 
 	if ($tags && $auteur_id) {
 		$tableau_tags = explode(",",$tags);
@@ -56,7 +63,7 @@ function balise_FORMULAIRE_SPIPICIOUS_AJAX_dyn($id_article ) {
 
 			if (spip_num_rows($result) == 0) { // creation tag
 				$exist = 'mot inexistant';
-				$sql = "INSERT INTO spip_mots (titre,id_groupe,type,idx) VALUES(".spip_abstract_quote(corriger_caracteres($tag)).",$id_groupe_tags,'$type_groupe_tags', 'oui')"; // FIXME encodage caractere ?
+				$sql = "INSERT INTO spip_mots (titre,id_groupe,type) VALUES(".spip_abstract_quote(corriger_caracteres($tag)).",$id_groupe_tags,'$type_groupe_tags')"; // FIXME encodage caractere ?
 				$result = spip_query($sql);
 				$id_tag = spip_insert_id();
 			} else {  // on recupere l'id du tag
@@ -67,13 +74,13 @@ function balise_FORMULAIRE_SPIPICIOUS_AJAX_dyn($id_article ) {
 					spip_log($id_tag,'spipicious');
 				}
 			}
-			// on lie le mot au couple article (uniquement si pas deja fait)
-			$result = spip_query("SELECT id_mot FROM spip_mots_articles WHERE id_mot=$id_tag AND id_article=$id_article");
+			// on lie le mot au couple type (uniquement si pas deja fait)
+			$result = spip_query("SELECT id_mot FROM $table_mot WHERE id_mot=$id_tag AND id_$type=$id");
 			if (spip_num_rows($result) == 0) {
-				spip_query("INSERT INTO spip_mots_articles(id_mot,id_article) VALUES('$id_tag','$id_article')");
+				spip_query("INSERT INTO $table_mot(id_mot,id_$type) VALUES('$id_tag','$id')");
 			}
 			
-			spip_query("INSERT INTO spip_spipicious(id_mot,id_auteur,id_article,position) VALUES('$id_tag','$auteur_id','$id_article','$position')");
+			spip_query("INSERT INTO spip_spipicious(id_mot,id_auteur,id_$type,position) VALUES('$id_tag','$auteur_id','$id','$position')");
 			$position++;
 			}
 		$tag_analysed[] = $tag;
@@ -86,9 +93,9 @@ function balise_FORMULAIRE_SPIPICIOUS_AJAX_dyn($id_article ) {
 	}
 	else{
 		return array('formulaires/formulaire_spipicious_ajax', 0,
-			array('id' => $id_article,
-				'auteur_nom' => $auteur_nom,
+			array('id' => $id,
 				'auteur_id' => $auteur_id,
+				'type' => $type,
 		));
 	}
 	
