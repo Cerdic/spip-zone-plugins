5cafec980d4a703a3a86d2ee7fd5ed13a22f7dbc
fil@rezo.net <fil@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-08-26 20:10:35 +0000

    faire marcher le tableau de controle dans des cas plus larges
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@14870 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/mutualisation/index.php
+++ _test_/mutualisation/index.php
@@ -1,45 +1,48 @@
 <?php
-	chdir('..');
+
+	// remonter l'arborescence jusqu'a trouver la racine du site
+	$profondeur=0;
+	while (!file_exists('ecrire/inc_version.php')
+	AND $profondeur++ < 4)
+		chdir('..');
 	require 'ecrire/inc_version.php';
+	include_spip('inc/minipres');
+	include_spip('inc/filtres');
 
 	// pas admin ? passe ton chemin (ce script est un vilain trou de securite)
 	if ( ($auteur_session['statut'] != '0minirezo') and ( $_SERVER["REMOTE_ADDR"]!='127.0.0.1'))
 		die('pas admin !');
-	chdir('sites');
+
+	$sites = array();
+	foreach(preg_files('sites', '.*/config/connect.php') as $s) {
+		$sites[] = preg_replace(',^sites/(.*)/config/connect.php,', '\1', $s);
+	}
+	sort($sites);
+
+	$titre = _L(count($sites).' '.'sites mutualis&#233;s');
+
+	$page = "<table>";
+	foreach ($sites as $v) {
+		if (lire_fichier('sites/'.$v.'/tmp/meta_cache.txt', $meta)
+		AND is_array($meta = @unserialize($meta))
+		AND $url = $meta['adresse_site']) {
+			$url .= '/';
+			$nom_site = sinon($meta['nom_site'], $v);
+		}
+		else
+			$url = 'http://'.$v.'/';
+		$page .= "<tr>
+			<td>$v</td>
+			<td><a href='$url'>$nom_site</a></td>
+			<td><a href='$url/ecrire/'>ecrire/</a></td>
+			<td><a href='$url/ecrire/?exec=statistiques_visites'>stats</a></td>
+			</tr>\n";
+	}
+	$page .= "</table>";
+
+	$page = minipres($titre, $page);
+
+	echo str_replace('<head>', '<head>
+		<base href="'.url_absolue(str_repeat('../', $profondeur)).'" />', $page);
+
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" lang="fr" xml:lang="fr" dir="ltr">
-<head>
-<title>SPIP mutualis&eacute;s</title>
-</head>
-<body>
-<?php
-// Création du tableau qui va contenir les fichiers et dossiers
-$files = array();
-
-// Ouverture du répetoire courant
-// Pour le changer utiliser chdir() avant opendir()
-$handle = opendir(".");
-
-// Parcours des fichiers et dossiers du répertoire courant
-while($file = readdir($handle)) {
-    if($file != "." && $file != ".." && is_dir($file)) {
-        $files[] = $file;
-    }
-}
-
-// Fermeture du répertoire courant
-closedir($handle);
-
-// Tri du tableaunat
-sort($files);
-
-// Affichage des fichiers et dossiers triés
-echo "<h1>Liste des sites install&eacute;s (".count($files).")</h1>\n<ul>";
-foreach($files as $v) {
-    echo "<li><a href='http://$v'>$v</a> — <a href='http://$v/ecrire/'>/ecrire/</a> — <a href='http://$v/ecrire/?exec=statistiques_visites'>Statistiques</a></li>\n";
-}
-echo "</ul>";
-?>
-</body>
-</html>
