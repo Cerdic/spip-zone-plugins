efe474598c1490ad833c17a0ba4ac915ed72d0db
marcimat@free.fr <marcimat@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-09-06 18:37:38 +0000

    La présence de CFG pouvait être bloquante (page blanche) si le plugin table des matières est, je suppose, chargé avant le plugin CFG : table des matières crée la fonction lire_config() avant que cfg ne l'ai fait ; du coup, gros boum lorsque cfg initialisait sa fonction lire_config(), déjà crée...
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@15105 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/table_matieres/tdm_options.php
+++ _stable_/table_matieres/tdm_options.php
@@ -1,16 +1,20 @@
 <?php
 
+$tm_lg_ancre  = 35;
+$tm_sep_ancre = '-';
+$tm_min_ancre = 3;
+
 #tester la presence de CFG
-if(!function_exists('lire_config')) {
-	function lire_config($texte) {
-		return 0;
-	}
+if(function_exists('lire_config')) {
+	$tm_lg_ancre  = ($t = lire_config('table_matieres/lg' )) ? $t :  $tm_lg_ancre;
+	$tm_sep_ancre = ($t = lire_config('table_matieres/sep')) ? $t :  $tm_sep_ancre;
+	$tm_min_ancre = ($t = lire_config('table_matieres/min')) ? $t :  $tm_min_ancre;
 }
 
-define( "_LG_ANCRE", ($t = lire_config('table_matieres/lg' )) ? $t :  35);
-define("_SEP_ANCRE", ($t = lire_config('table_matieres/sep')) ? $t : '-');
-define("_MIN_ANCRE", ($t = lire_config('table_matieres/min')) ? $t :   3);
-
+define( "_LG_ANCRE", $tm_lg_ancre);
+define("_SEP_ANCRE", $tm_sep_ancre);
+define("_MIN_ANCRE", $tm_min_ancre);
+	
 define("_RETOUR_TDM", '<a href="#tdm"><img src="' .
 	find_in_path('images/tdm.png') . 
 	'" /></a>');
