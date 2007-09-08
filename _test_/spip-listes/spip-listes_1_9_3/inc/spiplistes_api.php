d4f58315fc8cae6530c0c567ace999e59558318d
elrik75@free.fr <elrik75@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-09-08 17:08:04 +0000

    API spip_listes : ajout des fonctions utiles migration prochaine de spip-listes vers ces fonctions
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@15129 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/spip-listes/spip-listes_1_9_3/inc/spiplistes_api.php
+++ _test_/spip-listes/spip-listes_1_9_3/inc/spiplistes_api.php
@@ -38,8 +38,9 @@ function __exec_multi_queries($queries) {
 	}
 	$_queries = split(';', $queries);
 	while( list(,$val) = each($_queries)) {
-		spip_query($val);
+		$res = spip_query($val);
 	}
+	return $res;
 }
 
 /*
@@ -68,7 +69,8 @@ function spiplistes_remplir_liste_envois($id_courrier,$id_liste){
 
 // Nombre d'abonnes a une liste
 function spip_listes_nb_abonnes_liste($id_liste){
-	$row = spip_fetch_array(spip_query("SELECT COUNT(id_auteur) AS n FROM spip_auteurs_listes WHERE id_liste=$id_liste"));
+	$row = spip_fetch_array(spip_query("SELECT COUNT(id_auteur) AS n ".
+									   "FROM spip_auteurs_listes WHERE id_liste=$id_liste"));
 	$nb_abo = ( $row['n'] >1)?  $row['n']." abonn&eacute;s" :  $row['n']." abonn&eacute;";
 	return "(".$nb_abo.")";
 }
@@ -86,11 +88,25 @@ function spiplistes_desabonner($id_auteur){
 	$listes = spip_query ("SELECT * FROM spip_listes WHERE statut = 'liste'");
 			while($row = spip_fetch_array($listes)) {
 				$id_liste = $row['id_liste'] ;
-				$result=spip_query("DELETE FROM spip_auteurs_listes WHERE id_auteur="._q($id_auteur)." AND id_liste="._q($id_liste));
+				$result=spip_query("DELETE FROM spip_auteurs_listes WHERE id_auteur=".
+								   _q($id_auteur)." AND id_liste="._q($id_liste));
 			}
-	$result=spip_query("UPDATE `spip_auteurs_elargis` SET `spip_listes_format`='non' WHERE `id_auteur` ="._q($id_auteur));
+	$result=spip_query("UPDATE `spip_auteurs_elargis` ".
+					   "SET `spip_listes_format`='non' WHERE `id_auteur` ="._q($id_auteur));
 }
 
+/* desabonnement de certaines listes uniquement */
+function spiplistes_desabonner_des_listes($id_auteur, $ids_liste) {
+	if(!is_array($ids_liste)) {
+		$ids_liste = array($ids_liste);
+	}
+	$query = "";
+	while ( list(,$val) = each($ids_liste) ) {
+		$query .= "DELETE FROM spip_autheur_listes WHERE id_auteur=".
+			_q($id_auteur)." AND id_liste="._q($id_liste).";";
+	}
+	__exec_multi_queries($query);
+}
 
 //function spiplistes_propre($texte)
 // passe propre() sur un texte puis nettoye les trucs rajoutes par spip sur du html
@@ -115,6 +131,173 @@ function spiplistes_propre($texte){
 	return $texte;
 }
 
+/* Demande d'abonnement à une liste d'un visiteur
+ * NE VALIDE PAS L'ABONNEMENT : il faut confirmer
+ * Retourne une structure array('inser't ou 'update', id_auteur, cookie_oubli)
+ * @param $email : clé unique
+ * @param $nom : facultatif
+ * @param $prenom : facultatif concaténé avant le nom
+ * @param $ids_liste : entier ou array d'entier (id d'une spip_liste)
+ * @param $statut_abonnement : 'html' (defaut) ou 'texte'
+ */
+function spiplistes_ajout_inscription($email, $nom, $prenom, $ids_liste, $statut_abonnement) {
+	$email = email_valide($email);
+	$id_auteur = spiplistes_idauteur_depuis_email($email);
+	$cookie = creer_uniqid();
+	if ($id_auteur == 0) {
+		$nom = trim($nom);
+		$prenom = trim($prenom);
+		if (!$nom) {
+			$nom = $email;
+		} else if ($prenom) {
+			$nom = $prenom.' '.$nom;
+		}
+		$query = "INSERT INTO spip_auteurs (nom, email, statut, cookie_oubli)".
+			"VALUES ("._q($nom).", "._q($email).", '6forum', "._q($cookie).");";
+		spip_query($query);
+		$id_auteur = spiplistes_idauteur_depuis_email($email);
+		$ret = array('insert', $id_auteur, $cookie);
+		if ( ! $id_auteur ) return False; // erreur
+	} else if ($id_auteur) {
+		$query = "UPDATE spip_auteurs SET cookie_oubli="._q($cookie).
+		  " WHERE id_auteur = ".$id_auteur;
+		spip_query($query);
+		$ret = array('update', $id_auteur, $cookie);
+	} else { return False; } // erreur
+
+	if ($format_abonnement != 'html' 
+		or $format_abonnement != 'texte' 
+		or $format_abonnement != 'non') {
+		$format_abonnement = 'html';
+	}
+	spiplistes_MaJ_auteur_elargi($id_auteur, $format_abonnement);
+
+	if( ! is_array($ids_liste)) {
+		$ids_liste = array ($ids_liste);
+	}
+	while( list(,$val) = each($ids_liste) ){
+		$query = "DELETE FROM spip_auteurs_listes WHERE id_auteur=".
+			$id_auteur." AND id_liste="._q($val).";\n";
+		$query .= "INSERT INTO spip_auteurs_listes ".
+			"(id_auteur, id_liste, date_inscription, statut)".
+		"VALUES (".$id_auteur.", "._q($val).", now(), 'a_valider');\n";
+	}
+	//echo '<!-- '.$query.' -->'."\n";
+	__exec_multi_queries($query);
+	return $ret;
+}
+
+/* Savoir si l'auteur elargi a ete cree */
+function spiplistes_auteur_elargi_existe($id_auteur) {
+	$query = "SELECT 1 FROM spip_auteurs_elargis WHERE id_auteur="._q($id_auteur);
+	$result = spip_query($query);
+	if (spip_num_rows($result) == 0) { return False; }
+	else { return True; }
+}
+
+/*
+ * Mise a jour de l'auteur elargi (table spip-liste)
+ * @param $id_auteur : clé table auteur
+ * @param $format : non | texte | html
+ */
+function spiplistes_MaJ_auteur_elargi($id_auteur, $format) {
+	if (!$format) { $format='non'; }
+	if (spiplistes_auteur_elargi_existe($id_auteur)) {
+		$query = "INSERT INTO spip_auteurs_elargis (id_auteur, spip_listes_format)".
+			" VALUES ("._q($id_auteur)." , "._q($format)." ) ";
+	} else {
+		$query = "UPDATE spip_auteurs_elargis SET spip_listes_format=".q($format).
+			" WHERE id_auteur="._q($id_auteur);
+	}
+	// echo'<!-- '.$query.' -->'."\n";
+	spip_query($query);
+}
+
+/* retourne l'id auteur depuis l'email */
+function spiplistes_idauteur_depuis_email($email) {
+	$email = email_valide($email);
+	$query = "SELECT id_auteur FROM spip_auteurs WHERE email="._q($email).
+		" AND statut<>'5poubelle'";	
+	$result = spip_query($query);
+	if (spip_num_rows($result) == 0) { return 0; }
+	else if (spip_num_rows($result) == 1) {
+		$row = spip_fetch_array($result);
+		return $row['id_auteur'];
+	} else { echo "Erreur get id_auteur from email"; exit(); } //erreur
+}
+
+/* inscription directe - import 
+ * @param $type_abo = 'html' ou 'texte'
+ * @param $statut = 'valide' ou 'a_valider'
+ */
+function spiplistes_ajout_auteur_aux_listes($id_auteur, $ids_liste, $type_abo, $statut) {
+  if (!$type_abo) $type_abo = 'html';
+  else if ($type_abo == 'txt') $type_abo='texte';
+  if ($statut != 'valide')
+	$statut = 'a_valider';
+
+  spiplistes_MaJ_auteur_elargi($id_auteur, $type_abo);
+  
+  if(!is_array($ids_liste)){
+	$ids_liste = array($ids_liste);
+  }
+  $query = '';
+  while( list(,$val) = each($ids_liste) ) {
+	$query .= "DELETE FROM spip_auteurs_listes WHERE id_auteur=".
+	  _q($id_auteur)." AND id_liste="._q($val).";\n";
+	$query .= "INSERT INTO spip_auteurs_listes ".
+	  "(id_auteur,id_liste,format,date_inscription,statut)".
+	  " VALUES ("._q($id_auteur).","._q($val).", ".
+	  _q($type_abo).", now(), "._q($statut).");\n";
+  }
+  __exec_multi_queries($query);
+  return True;
+}
+
+/*
+ * validation de l'inscription d'un id_auteur
+ * Il faut deja etre inscrit !
+ */
+function spiplistes_valide_listes($id_auteur, $ids_liste) {
+	$query = '';
+	if(!is_array($ids_liste)) {
+		$ids_liste = array($ids_liste);
+	}
+	while( list(,$val) = each($ids_liste) ) {
+		$query .= "UPDATE spip_auteurs_listes SET ".
+			"statut='valide'".
+			"WHERE id_auteur="._q($id_auteur)." AND id_liste="._q($val);
+	}
+	__exec_multi_queries($query);
+}
+
+
+/* validation des inscriptions depuis cookie_oubli */
+function spiplistes_valide_listes_depuis_cookie($cookie) {
+  // better query, but works only whith mysql>=4 :( 
+  $query_mysql4 = "UPDATE spip_auteurs_listes, spip_auteurs".
+	" SET spip_auteurs_listes.statut = 'valide',".
+	" spip_auteurs.cookie_oubli = ''".
+	" WHERE spip_auteurs.cookie_oubli="._q($cookie).
+	" AND spip_auteurs_listes.id_auteur = spip_auteurs.id_auteur".
+	" AND spip_auteurs.statut<>'5poubelle'";
+
+  // standard queries  (works with mysql3)
+  $id_auteur = spiplistes_idauteur_depuis_cookie_oubli($cookie);
+  $queries = "UPDATE spip_auteurs_listes SET".
+	" statut = 'valide'".
+	" WHERE id_auteur = ".$id_auteur.";";
+  $queries .= "UPDATE spip_auteurs SET".
+	  " cookie_oubli = ''".
+	  " WHERE id_auteur = ".$id_auteur.";";
+
+  //echo '<!-- validation = '.$query.' -->'."\n";
+  $res = __exec_multi_queries($queries);
+  if (! $res) {
+	echo "Validation impossible !";
+	exit();
+  }
+}
 
 /******************************************************************************************/
 /* SPIP-Listes est un systeme de gestion de listes d'abonnes et d'envoi d'information     */
