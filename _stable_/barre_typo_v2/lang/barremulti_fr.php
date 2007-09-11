44960d4c27723f444c3863147c543a675fbd2977
apachot@openstudio.fr <apachot@openstudio.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-09-11 18:11:23 +0000

    ajout du fichier de langue français
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@15164 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _stable_/barre_typo_v2/lang/barremulti_fr.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// Aide des boutons supplémentaires de la barre typo
+'barre_multi_titre' => 'Configuration de l\'extension multilingue pour BTV2', 
+'barre_multi_langues' => 'Langues (s&eacute;par&eacute;es par des virgules)',
+'barre_multi_affiche_titres' => 'Afficher la barre typo sur les titres',
+'barre_multi_affiche_descriptifs' => 'Afficher la barre typo sur les descriptifs',
+'barre_multi_affiche_textes' => 'Afficher la barre typo sur les textes',
+'barre_multi_activer_articles' => 'Activer le multilinguisme sur les articles',
+'barre_multi_activer_breves' => 'Activer le multilinguisme sur les breves'
+);
+?>
