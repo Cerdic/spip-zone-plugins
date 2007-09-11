26ffd57394ad91b3c6e3798e9673db54755bae13
apachot@openstudio.fr <apachot@openstudio.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-09-11 18:29:31 +0000

    traduction en anglais du panneau de conf
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@15165 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _stable_/barre_typo_v2/lang/barremulti_en.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+
+'barre_multi_titre' => 'Multilingual typographical bar (multilingual extension for BTV2) Configuration', 
+'barre_multi_langues' => 'Languages separated by quotes symbol',
+'barre_multi_affiche_titres' => 'Display typographical bar on titles',
+'barre_multi_affiche_descriptifs' => 'Display typographical bar on descriptions',
+'barre_multi_affiche_textes' => 'Display typographical bar on texts',
+'barre_multi_activer_articles' => 'Enable multilingualism on articles',
+'barre_multi_activer_breves' => 'Enable multilingualism on news items'
+);
+?>
