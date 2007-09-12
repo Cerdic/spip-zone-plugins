07ee96f9cd66c05d67fcb235b12b875ff8660df5
vincent@ldd.fr <vincent@ldd.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-09-12 10:43:45 +0000

    bug d'erreur de frappe de cette saleté de Booz corrigé
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@15176 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/inscription2/inscription2_192/inscription2_mes_options.php
+++ _test_/inscription2/inscription2_192/inscription2_mes_options.php
@@ -56,8 +56,7 @@
 		$query = spip_fetch_array($query);
 		exit;
 		$GLOBALS['auteur_session'] = array_merge($query,$GLOBALS['auteur_session'] );
-	*/	
-	}
+	}*/
 	
 # autoriser les visiteurs a modifier leurs infos
 #define ('_DEBUG_AUTORISER', true);
