3898a23f4ef61d71fa1700acac8678cd1f46d682
apachot@openstudio.fr <apachot@openstudio.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-09-12 06:42:02 +0000

    séparation comas et non quotes
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@15173 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/barre_typo_v2/lang/barremulti_en.php
+++ _stable_/barre_typo_v2/lang/barremulti_en.php
@@ -5,7 +5,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 
 'barre_multi_titre' => 'Multilingual typographical bar (multilingual extension for BTV2) Configuration', 
-'barre_multi_langues' => 'Languages separated by quotes symbol',
+'barre_multi_langues' => 'Languages separated by comas',
 'barre_multi_affiche_titres' => 'Display typographical bar on titles',
 'barre_multi_affiche_descriptifs' => 'Display typographical bar on descriptions',
 'barre_multi_affiche_textes' => 'Display typographical bar on texts',
