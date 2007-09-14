2f161fb4d0fb64394653f7e06ac8cab19f326e8c
ventrea@gmail.com <ventrea@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-09-14 23:01:55 +0000

    remontee du code ctrlfroms, la surcharge de f_t (Forms_valide_conformite_champs_reponse_post) n'est pas incluse pour l'instant donc les retours d'erreur ne passent pas.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@15239 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _dev_/ctrlforms/inc/ctrlforms_types_champs.php
@@ -0,0 +1,104 @@
+<?php
+/* Ajout du champ dans la liste en mode création formulaire*/
+function ctrlforms_forms_types_champs($flux){
+	$flux['ctrlpass']=_T('ctrlforms:ctrl_pass');
+	return $flux;
+}
+
+
+function ctrlforms_forms_bloc_edition_champ($flux){
+	$row = $flux['args']['row'];
+	$type = $row['type'];
+
+	if (in_array($type,array('ctrlpass'))){
+
+		$action_link = $flux['args']['action_link'];
+		$redirect = $flux['args']['redirect'];
+		$idbloc = $flux['args']['idbloc'];
+		$id_form = $row['id_form'];
+		$champ = $row['champ'];
+		$titre = $row['titre'];
+		$obligatoire = $row['obligatoire'];
+		$extra_info = $row['extra_info'];
+		$specifiant = $row['specifiant'];
+		$public = $row['public'];
+		$aide = $row['aide'];
+		$html_wrap = $row['html_wrap'];
+		
+		
+		if (!$structure){
+			include_spip("inc/forms");
+			$structure = Forms_structure($id_form);
+		}
+		
+		$out = $flux['data'];
+		
+		$out .= "<label for='controle_$champ'>"._T('ctrlforms:sel_ctrlfield')."</label> :";
+		$out .= " &nbsp;<select name='controle_$champ' id='controle_$champ' class='fondo verdana2'>\n";
+		$out .= "<option value=''>"._T('ctrlforms:sel_ctrlfielddefault')."</option>\n";
+		foreach($structure as $champliste=>$infos){
+			$type = $infos['type'];
+			if($type!='ctrlpass'){	
+				$selected = ($champliste == $row['extra_info']) ? " selected='selected'": "";
+				$out .= "<option value='$champliste'$selected>".$type."</option>\n";
+			}
+		}
+		$out .= "</select>";
+		$out .= "<br />\n";
+		
+		$flux['data'] = $out;
+	}
+	return $flux;
+}
+
+
+function ctrlforms_forms_update_edition_champ($flux){
+	$row = $flux['args']['row'];
+	$type = $row['type'];
+	$champ = $row['champ'];
+	$id_form = $row['id_form'];
+	if (in_array($type,array('ctrlpass'))){
+		if ($s = _request("controle_$champ")){
+					if (!$structure){
+						include_spip("inc/forms");
+						$structure = Forms_structure($id_form);
+					}
+					$flux['data'] = "";
+					foreach($structure as $champliste=>$infos){
+						if($s == $champliste) $flux['data'] = $s;
+					}
+		}
+	}
+	return $flux;
+}
+
+
+function ctrlforms_forms_input_champs($flux){
+	static $vu=array();
+	$type = $flux['args']['type'];
+	if (in_array($type,array('ctrlpass')) AND (_DIR_RESTREINT OR _request('exec')!=='forms_edit')) {
+		$id_form = $flux['args']['id_form'];
+		$champ = $flux['args']['champ'];
+		$extra_info = $flux['args']['extra_info'];
+		$vu[$id_form][$type]=array(
+			'id'=>extraire_attribut($flux['data'],'id'),
+			'name'=>extraire_attribut($flux['data'],'name'),
+			'value'=>extraire_attribut($flux['data'],'value'),
+			'syst'=>$extra_info);
+			
+			$flux['data']="";
+			
+			$typefield = substr($vu[$id_form][$type][syst],0,8);
+			
+			if(strcmp($typefield,"password")==0){
+				$flux['data'].="<input type=\"password\" name='".$champ."' id='input-".$id_form."-".$champ."' value=\""._request($champ, $c)."\" class='password formo' size='40' />";
+			}
+			else{
+				$flux['data'].="<input type=\"text\" name='".$champ."' id='input-".$id_form."-".$champ."' value=\""._request($champ, $c)."\" class='password formo' size='40' />";
+			}
+			
+	}
+	return $flux;
+}
+
+?>
---
+++ _dev_/ctrlforms/public/ctrlforms_balises.php
@@ -0,0 +1,38 @@
+<?php
+/*
+ * ctrlforms
+ * Geolocalistion dans les tables et les formulaires
+ *
+ * Auteur :
+ * Cedric Morin
+ * (c) 2007 - Distribue sous licence GNU/GPL
+ *
+ */
+include_spip('inc/ctrlforms');
+
+function balise_LATITUDE_dist($p){
+	$_id_donnee = champ_sql('id_donnee', $p);
+	$_id_form = champ_sql('id_form', $p);
+	$p->code = "(ctrlforms_donnee_latitude($_id_form,$_id_donnee))";
+	$p->interdire_scripts = false; // securite assuree par la fonction
+
+	return $p;
+}
+function balise_LONGITUDE_dist($p){
+	$_id_donnee = champ_sql('id_donnee', $p);
+	$_id_form = champ_sql('id_form', $p);
+	$p->code = "(ctrlforms_donnee_longitude($_id_form,$_id_donnee))";
+	$p->interdire_scripts = false; // securite assuree par la fonction
+
+	return $p;
+}
+function balise_GEOICONE_dist($p){
+	$_id_donnee = champ_sql('id_donnee', $p);
+	$_id_form = champ_sql('id_form', $p);
+	$p->code = "(ctrlforms_donnee_icone($_id_form,$_id_donnee))";
+	$p->interdire_scripts = false; // securite assuree par la fonction
+
+	return $p;
+}
+
+?>
