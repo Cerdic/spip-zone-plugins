40df2b8752a6baaab622c436cdc0ea1412df03ac
ventrea@gmail.com <ventrea@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-09-14 23:04:29 +0000

    remontée du code de ftcaptcha, la surcharge de f_t (Forms_valide_conformite_champs_reponse_post) n'est pas incluse pour l'instant donc les retours d'erreur ne passent pas. En + risque de plantage aussi suite à renommage en ftcaptcha, captcha étant déjà pris
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@15240 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _dev_/forms_et_tables_captcha/base/ftcaptcha_install.php
@@ -0,0 +1,11 @@
+<?php
+include_spip('base/create');
+include_spip('base/abstract_sql');
+include_spip('base/captcha');
+
+creer_base();
+
+$current_version = 0.0;
+ecrire_meta('ftcaptcha/captcha_base_version',$current_version);
+ecrire_meta('ftcaptcha/captcha_activation','oui');
+?>
