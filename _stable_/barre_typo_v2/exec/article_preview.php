1db411a310e4d0c9ca6beb8d9d8da7adb546f123
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-09-15 17:32:02 +0000

    charger mes_fonctions
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@15254 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/barre_typo_v2/exec/article_preview.php
+++ _stable_/barre_typo_v2/exec/article_preview.php
@@ -3,6 +3,7 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 
 include_spip("inc/meta"); // Pour avoir le charset du site
 include_spip("inc/texte"); // Pour pouvoir utiliser propre
+include_spip("public/parametrer"); // pour mes_fonctions
 
 function exec_article_preview_dist()
 {
