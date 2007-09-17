0880e6d95a4915253abe06af66647448a614f90c
toutati@free.fr <toutati@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-09-17 22:43:52 +0000

    voici les debuts de walma_cfg desole si je pete des trucs ca fait longtemps et je suis sous kdesvn pas marrant à gere!!
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@15310 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _dev_/xspf/walma_cfg_SVN/lang/walma_en.php
@@ -0,0 +1,26 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+'titre_page_config_walma' => "Configuration of plugin Walma",
+'more_info' => 'More information', 
+'copidlibre' => "CopID free non commercial &copy; 07/2007 Alm &amp; Walk Galerie ",
+'copid_walma' => "CopID libre non marchand",
+'pour' => "for",
+'taille_image' => "Image size",
+'diaporama' => "Slideshow",
+'change_langue'=> "Passer en langue fran&#231;aise",
+'clic_fermer'=> "clic to close",
+'gauche'=> "Left",
+'droite'=> "Right", 
+'mosaiclic'=>"Mosa&#239;clic",
+'no_result'=> "No result!",
+'images'=> "images",
+'image'=> "image",
+'tout_portfolios'=>"All portfolios",
+'ce_portfolio'=>"This portfolio",
+);
+
+
+?>
