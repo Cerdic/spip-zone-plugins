1f2edb2ad121d90e1f21694aa950b94681f3dc07
horaciogd@vhplab.net <horaciogd@vhplab.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-09-20 22:48:21 +0000

    Changes to make markers visible in public area. There was a problem with URLbase, the javascript variable that stores the first part of all marker image url. There also was a prolbem with map ids, idmap didn´t existed when [(#ENV{id_carte_gis}|?{' ',''})var idmap = #EN{id_carte_gis};] result was empty.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@15379 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/googlemap_api/js/geomap.js
+++ _dev_/googlemap_api/js/geomap.js
@@ -31,14 +31,14 @@ function creaMarcador(point, html, icon, son, idmap) {
 	//creamos un obxecto GMarker e o gradamos nunha variable
 	var marcador = new GMarker(point, icon);
 	//engadimos un evento para que ao pulsar no marcador se abra a ventana co html indicado
-		var map = eval('map'+idmap);
+	var map = eval('map'+idmap);
 	GEvent.addListener(marcador, "click", function() {
 		marcador.openInfoWindowHtml(html);
 		//cando se abre a ventana do marcador executamos as seguintes intsruccions
 		GEvent.addListener(map,"infowindowopen", function() {
 			if(son){
 				//esta parte del codigo enbebe un obxecto flah na ventana creado con flashobject.js
-				var fo = new FlashObject(URLbase + "plugins/gis/img_pack/musicplayer.swf?autoplay=true&song_url="+son, "player_x", "17", "17", "6", "#FFFFFF");
+				var fo = new FlashObject(URLbase + "/gis/img_pack/musicplayer.swf?autoplay=true&song_url="+son, "player_x", "17", "17", "6", "#FFFFFF");
 				fo.write("player");
 			}
 		});
@@ -46,12 +46,12 @@ function creaMarcador(point, html, icon, son, idmap) {
 	return marcador;
 }
 
-function agregarMarcador (xmlItem, minZoom, maxZoom, idmanager) {
+function agregarMarcador (xmlItem, idmap, minZoom, maxZoom) {
 	//almacenamos en distintas variables la informacion contenida nen los chilNodes de cada item-marcador do xml
 	var xmlLat = $("geo_lat",xmlItem);
 	var xmlLng = $("geo_long",xmlItem);
 	var xmlSon = $("enclosure",xmlItem);
-	var marker = eval('markerManager'+idmanager);
+	var marker = eval('markerManager'+idmap);
 	if ((xmlLat.length == 0) || (xmlLng.length == 0)) return;
 	else {
 		var lat = parseFloat(xmlLat.text());
@@ -67,15 +67,15 @@ function agregarMarcador (xmlItem, minZoom, maxZoom, idmanager) {
 		
 		//creamos un icono para o marcador
 		var icono_categoria = new GIcon();
-		icono_categoria.image = (icon != "" ? icon : URLbase + "/plugins/gis/img_pack/correxir.png");
-		icono_categoria.shadow = URLbase + "/plugins/gis/img_pack/shadow.png";
+		icono_categoria.image = (icon != "" ? icon : URLbase + "/gis/img_pack/correxir.png");
+		icono_categoria.shadow = URLbase + "/gis/img_pack/shadow.png";
 		icono_categoria.iconSize = new GSize(20, 34);
 		icono_categoria.shadowSize = new GSize(22, 20);	
 		icono_categoria.iconAnchor = new GPoint(10, 34);
 		icono_categoria.infoWindowAnchor = new GPoint(5, 1);
 			
 		// creamos el marcador con los datos almacenados en las variables
-		var marcador = creaMarcador(point, html, icono_categoria, son, idmanager);
+		var marcador = creaMarcador(point, html, icono_categoria, son, idmap);
 		// recollemos a informacion que sexa necesaria en distintos arrays, usando como identificador a id do artigo
 		marcadores[id] = marcador;
 		contidosHTML[id] = html;
@@ -98,6 +98,6 @@ function abrirVentana(identificador) {
 	map.closeInfoWindow();
 	marcadores[identificador].openInfoWindowHtml(contidosHTML[identificador]);
 	//enbebemos o flash player do son
-	var fo = new FlashObject( URLbase + "/plugins/gis/img_pack/musicplayer.swf?autoplay=true&song_url=" + URLsons[identificador], "player_x", "17", "17", "6", "#FFFFFF");
+	var fo = new FlashObject( URLbase + "/img_pack/musicplayer.swf?autoplay=true&song_url=" + URLsons[identificador], "player_x", "17", "17", "6", "#FFFFFF");
 	fo.write("player");
 }
