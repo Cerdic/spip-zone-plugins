d1747f460489f96c8dc39458d66c3d1a6756ea69
paladin@quesaco.org <paladin@quesaco.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-09-21 06:58:40 +0000

    optimisation PHP + légères corrections XHTML
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@15387 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/spip-listes/spip-listes_1_9_3/exec/spip_listes.php
+++ _test_/spip-listes/spip-listes_1_9_3/exec/spip_listes.php
@@ -34,32 +34,38 @@ function spiplistes_afficher_pile_messages(){
 		if (!spiplistes_install('test'))
 			spiplistes_install('install');
 	}
-	$out = "";
+	
 	$list = spip_query ("SELECT * FROM spip_listes WHERE message_auto='oui' ");
 	$message_pile = spip_num_rows($list);
-	if ($message_pile == 0) return $out; 
-	$out .= debut_cadre_enfonce(_DIR_PLUGIN_SPIPLISTES.'img_pack/stock_timer.gif',true);
-	$out .= "<div  class='chapo' style='border-top:1px #cccccc;width:100%;font-weight:bold;font-size:14px'>"._T('spiplistes:Messages_automatiques')."</div>";
-	$out .= "<style>
+	if ($message_pile == 0) {
+		return (false); 
+	}
+	
+	$out = ""
+		. debut_cadre_enfonce(_DIR_PLUGIN_SPIPLISTES.'img_pack/stock_timer.gif',true)
+		. "<div  class='chapo' style='border-top:1px #cccccc;width:100%;font-weight:bold;font-size:14px'>"._T('spiplistes:Messages_automatiques')."</div>"
+		// déclaration css. Devrait plutot etre dans le head. A voir + tard
+		. "<style>
 	table.tab td {
 	text-align:center;
 	padding:3px;
 	width:33%;
-	background-color:#cccccc;
+	background-color:#ccc;
 	}
 	table.tab {
 	margin-top:5px;
 	}
 	tr.row_even {
-	background-color:#cccccc;
+	background-color:#ccc;
 	}
-	</style>";
-	$out .= "<table class='tab'>" ;	
-	$out .= "<tr style='padding:5px'>";
-	$out .= "<td style='font-weight:bold;background-color:#eeeecc'>"._T('spiplistes:envoi_patron')."</td>";
-	$out .= "<td style='font-weight:bold;background-color:#eeeecc'>"._T('spiplistes:sur_liste')."</td>";
-	$out .= "<td style='font-weight:bold;background-color:#eeeecc'>"._T('spiplistes:prochain_envoi_prevu')."</td>";
-	$out .= "</tr>";
+	</style>"
+		. "<table class='tab'>" 
+		. "<tr style='padding:5px'>"
+		. "<td style='font-weight:bold;background-color:#eec'>"._T('spiplistes:envoi_patron')."</td>"
+		. "<td style='font-weight:bold;background-color:#eec'>"._T('spiplistes:sur_liste')."</td>"
+		. "<td style='font-weight:bold;background-color:#eec'>"._T('spiplistes:prochain_envoi_prevu')."</td>"
+		. "</tr>"
+		;
 
 	while($row = spip_fetch_array($list)) {
 		$id_article = $row['id_liste'] ;
@@ -70,27 +76,35 @@ function spiplistes_afficher_pile_messages(){
 		if($i == 0){
 			$out .= "<tr style='padding:5px'>" ;
 			$i = 1 ;
-		}else {
+		}
+		else {
 			$out .= "<tr style='padding:5px' class='row_even'>" ;
 			$i = 0 ;
-		}
+		} // end else
 	
-  	$date_dernier = strtotime($row['maj']) ;
-    $date_dernier = date(_T('spiplistes:format_date'),$date_dernier) ;
+		$date_dernier = strtotime($row['maj']) ;
+		$date_dernier = date(_T('spiplistes:format_date'),$date_dernier) ;
 
-		$out .= "<td><a href='".generer_url_public('patron_switch',"patron=".$row['patron']."&date=".$date_dernier)."'> ".$row['patron']."</a><br />"._T('spiplistes:Tous_les')." ".$row['periode']." "._T('spiplistes:jours')."</td><td><a href='?exec=listes&id_liste=$id_article'>$titre</a><br />" ;
-		$out .= "</td>" ;
-		$out .= "<td>";
-		if($proch != 0)
-			$out .= _T('spiplistes:dans_jours')." <b>$proch</b> "._T('spiplistes:jours')."</td>";
-		else 
-			$out .= "<b>"._T('date_aujourdhui')."</b></td>";
-		$out .= "</tr>" ;
-	}
-	$out .= "</table>" ;
-	$out .= fin_cadre_enfonce(true);
-	return $out;
-}
+		$out .= ""
+			. "<td><a href='".generer_url_public('patron_switch',"patron=".$row['patron']."&date=".$date_dernier)."'> ".$row['patron']."</a><br />"._T('spiplistes:Tous_les')." ".$row['periode']." "._T('spiplistes:jours')."</td><td><a href='?exec=listes&id_liste=$id_article'>$titre</a><br />"
+			. "</td>"
+			. "<td>"
+			.	(
+				($proch != 0)
+				? _T('spiplistes:dans_jours')." <strong>$proch</strong> "._T('spiplistes:jours')."</td>"
+				: "<strong>"._T('date_aujourdhui')."</strong></td>"
+				)
+			. "</tr>"
+			;
+	} // end while
+	
+	$out .= ""
+		. "</table>"
+		. fin_cadre_enfonce(true)
+		;
+	return ($out);
+} // spiplistes_afficher_pile_messages()
+
 
 function exec_spip_listes() {
 	
@@ -103,32 +117,28 @@ function exec_spip_listes() {
 	include_spip ('inc/documents');
 	
 	
-	global $connect_statut;
-	global $connect_toutes_rubriques;
-	global $connect_id_auteur;
-	global $supp_dest;
+	global $connect_statut
+		, $connect_toutes_rubriques
+		, $connect_id_auteur
+		, $supp_dest
+		;
 	
-	$nomsite=lire_meta("nom_site"); 
-	$urlsite=lire_meta("adresse_site"); 
+//////////
+// PAGE CONTENU
+//////////
+
+	debut_page(_T('spiplistes:spip_listes'), "redacteurs", "spiplistes");
 	
-	// Admin SPIP-Listes
-	echo debut_page(_T('spiplistes:spip_listes'), "redacteurs", "spiplistes");
-	
-	if ($connect_statut != "0minirezo" ) {
-		echo "<p><b>"._T('spiplistes:acces_a_la_page')."</b></p>";
-		echo fin_page();
-		exit;
+	// la gestion des listes de courriers est réservée aux admins 
+	if($connect_statut != "0minirezo") {
+		die (spiplistes_terminer_page_non_authorisee() . fin_page());
 	}
 	
-	if (($connect_statut == "0minirezo") OR ($connect_id_auteur == $id_auteur)) {
-		$statut_auteur=$statut;
-		spip_listes_onglets("messagerie", _T('spiplistes:spip_listes'));
-	}
+	spip_listes_onglets("messagerie", _T('spiplistes:spip_listes'));
 	
 	debut_gauche();
 	spip_listes_raccourcis();
 	creer_colonne_droite();
-
 	debut_droite("messagerie");
 	
 	// MODE HISTORIQUE: Historique des envois --------------------------------------
@@ -139,30 +149,25 @@ function exec_spip_listes() {
 		spip_query("DELETE FROM spip_forum WHERE id_message="._q($detruire_message));
 	}
 	
-	/// afficher un tableau de messages
+	$page_result = ""
+		. spiplistes_afficher_en_liste(_T('spiplistes:aff_encours'), _DIR_PLUGIN_SPIPLISTES.'img_pack/24_send-receive.gif', 'messages', 'encour', '', 'position')
+		. spiplistes_afficher_en_liste(_T('spiplistes:aff_redac'), _DIR_PLUGIN_SPIPLISTES.'img_pack/stock_mail.gif', 'messages', 'redac', '', 'position')
+		// afficher les messages auto
+		. spiplistes_afficher_pile_messages()
+		//
+		. "<br /><br />"
+		//
+		. spiplistes_afficher_en_liste(_T('spiplistes:messages_auto_envoye'),_DIR_PLUGIN_SPIPLISTES.'img_pack/stock_mail.gif', 'messages', 'auto', '', 'position')
+		. spiplistes_afficher_en_liste(_T('spiplistes:aff_envoye'), _DIR_PLUGIN_SPIPLISTES.'img_pack/stock_mail.gif', 'messages', 'publie', '', 'position') 
+		;
 	
-	///
-	
-	$messages_vus = '';
-	
-	echo spiplistes_afficher_en_liste(_T('spiplistes:aff_encours'), _DIR_PLUGIN_SPIPLISTES.'img_pack/24_send-receive.gif', 'messages', 'encour', '', 'position') ;
-	echo spiplistes_afficher_en_liste(_T('spiplistes:aff_redac'), _DIR_PLUGIN_SPIPLISTES.'img_pack/stock_mail.gif', 'messages', 'redac', '', 'position') ;
-	
-	
-	// afficher les messages auto
-	echo spiplistes_afficher_pile_messages();
-	
-	echo "<br /><br />";
-
-	echo spiplistes_afficher_en_liste(_T('spiplistes:messages_auto_envoye'),_DIR_PLUGIN_SPIPLISTES.'img_pack/stock_mail.gif', 'messages', 'auto', '', 'position') ;
-	echo spiplistes_afficher_en_liste(_T('spiplistes:aff_envoye'), _DIR_PLUGIN_SPIPLISTES.'img_pack/stock_mail.gif', 'messages', 'publie', '', 'position') ;
-	
+	echo($page_result);
 	
 	// MODE HISTORIQUE FIN ---------------------------------------------------------
 	
 	echo __plugin_html_signature(true), fin_gauche(), fin_page();
 
-}
+} // exec_spip_listes()
 
 /******************************************************************************************/
 /* SPIP-listes est un syst�e de gestion de listes d'abonn� et d'envoi d'information     */
