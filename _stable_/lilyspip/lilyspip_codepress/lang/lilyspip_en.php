e3cf2bc2245496de871f4cc72493c6e0aa4a52af
crichard@jalmus.net <crichard@jalmus.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-09-23 15:01:00 +0000

    Internationalisation et amélioration du retour à la ligne de l'éditeur lilypond (merci Valentin)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@15463 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/lilyspip/lilyspip_codepress/lang/lilyspip_en.php
+++ _stable_/lilyspip/lilyspip_codepress/lang/lilyspip_en.php
@@ -7,7 +7,17 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 'parametrage_lilyspip' => 'Settings for Lilypond server',
 'info_message' => 'This page lets you specify Lilypond server address to embed music notation in all text.',
 'adresse_serveur' => 'Server address',
-'previsualisation' => 'Previsualisation :',
+'previsualisation' => 'Preview',
 'barre_lilypond' => 'Insert music notations',
+'code_lilypond' => 'Lilypond Code',
+'modeles' => 'Templates',
+'editeur' => 'LilyPond Assistant',
+'portee_simple' => 'Single Staff',
+'piano' => 'Piano',
+'quatuor' => 'String Quartet',
+'vocal' => 'Vocal Ensemble',
+'enregistrer' => 'Send',
+'previsualiser' => 'Preview',
+'code_invalide' => 'Invalid Code',
 );
 ?>
--- _stable_/lilyspip/lilyspip_codepress/lang/lilyspip_fr.php
+++ _stable_/lilyspip/lilyspip_codepress/lang/lilyspip_fr.php
@@ -7,7 +7,17 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 'parametrage_lilyspip' => 'Param&eacute;trages',
 'info_message' => "Cette page permet de sp&eacute;cifier l'adresse du serveur Lilypond pour l'insertion de notations musicales.",
 'adresse_serveur' => 'Adresse du serveur Lilypond (http://www.site.net/lilyserv.php) :',
-'previsualisation' => 'Pr&eacute;visualisation :',
+'previsualisation' => 'Pr&eacute;visualisation',
 'barre_lilypond' => 'Ins&eacute;rer des notations musicales',
+'code_lilypond' => 'Code Lilypond',
+'modeles' => 'Mod&egrave;les',
+'editeur' => 'Éditeur LilyPond',
+'portee_simple' => 'Port&eacute;e simple',
+'piano' => 'Piano',
+'quatuor' => 'Quatuor',
+'vocal' => 'Ensemble vocal',
+'enregistrer' => 'Enregistrer',
+'previsualiser' => 'Pr&eacute;visualiser',
+'code_invalide' => 'Ce code n\'est pas reconnu',
 );
 ?>
