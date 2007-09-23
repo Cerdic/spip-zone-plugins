b6ed259a91ae272d34ada73ef885e04b4c07599d
booz@rezo.net <booz@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-09-23 15:20:01 +0000

    envoyer un mail de relance apres l'echeance
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@15464 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/abonnement/inc/abonnement_cron.php
+++ _dev_/abonnement/inc/abonnement_cron.php
@@ -4,10 +4,10 @@ function cron_abonnement_cron($t){
 // ---------------------------------------------------------------------------------------------
 // Taches de fond
 		
-		spip_log("cron abonnement","abonnement");
+	spip_log("cron abonnement","abonnement");
 		
-		// fermer les zones aux echus
-		// attention s'il y en a beaucoup
+	// fermer les zones aux echus
+	// attention s'il y en a beaucoup
 		
 	$result = spip_query("
 	SELECT a.id_auteur FROM spip_auteurs_elargis a, spip_zones_auteurs b
@@ -23,6 +23,61 @@ function cron_abonnement_cron($t){
 		spip_query("DELETE FROM `spip_zones_auteurs` WHERE id_auteur='$id_auteur'");
 		}
 		
+	
+	// relancer les abonnes
+	// le cycle de relance comporte plusieurs phases
+	// avant l'echeance (1 à 3 messages) , à mettre en relation avec un compte pre-approvisionné.
+	// 1 après l'échéance traité ici.
+	// ne pas envoyer plusieurs fois le meme message (flaguer un peu)
+	
+	include_spip('inc/mail');
+	
+	$result = spip_query("
+	SELECT libelle, id_abonnement, periode FROM spip_abonnements 
+	");
+		
+		while($row = spip_fetch_array($result)){
+		$libelle = $row['libelle'] ;
+		$id_abonnement = $row['id_abonnement'] ;
+		$periode = $row['periode'] ;
+		
+		// a décliner sur les 4 phases de relance
+		
+		if($periode == "jours"){
+	 	$validite = "DATE_ADD(CURRENT_DATE, INTERVAL 0 DAY)" ;
+	 	}elseif($periode == "mois"){
+	 	$validite = "DATE_ADD(CURRENT_DATE, INTERVAL 0 DAY)" ;
+	 	}
+	 	
+	 	$sujet_relance = lire_config("abonnement_relances/sujet_relance4") ;
+	 	$texte_relance = lire_config("abonnement_relances/texte_relance4") ;
+		$adresse_expediteur = lire_config("abonnement_relances/email_envoi") ;
+
+		$result_abo = spip_query("
+		SELECT a.id_auteur, c.email FROM spip_auteurs_elargis a, spip_auteurs_elargis_abonnements b, spip_auteurs c
+		WHERE
+		a.id = b.id_auteur_elargi
+		and a.id_auteur = c.id_auteur
+		and b.id_abonnement = '$id_abonnement'
+		and a.validite <> '0000-00-00 00:00:00'
+		and a.validite < $validite
+		");
+		
+			while($row_abo = spip_fetch_array($result_abo)){
+			$id_auteur = $row_abo['id_auteur'] ;
+			$email_abonne = $row_abo['email'] ;
+	
+			spip_log($email_abonne."(".$id_auteur.") est a relancer\n","abonnement";
+			spip_log($sujet_relance,"abonnement") ;
+					
+			envoyer_mail ( $email_abonne , $sujet_relance, $texte_relance, $adresse_expediteur);
+	
+			}
+
+
+		}
+
+	
 		
 	return 1; 
 }
