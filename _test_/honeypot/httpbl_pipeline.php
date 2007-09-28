ba54887df7404ff7f15ee62556f6ab780207ed4e
pierre.andrews@gmail.com <pierre.andrews@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-09-28 20:28:52 +0000

    grrr svn est lourd... Donc, le commit d'avant ET celui ci ajoute l'option pour cacher les emails aux mechants...
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@15595 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _test_/honeypot/httpbl_pipeline.php
@@ -0,0 +1,24 @@
+<?php
+
+@define('_httpbl_mailcrypt_AUTORISE', '\!\#\$\%\&\'\*\+\-\/\=\?\^\_\`\.\{\|\}\~a-zA-Z0-9');
+@define('_httpbl_mailcrypt_REGEXPR', ',\b['._httpbl_mailcrypt_AUTORISE.']*@[a-zA-Z][a-zA-Z0-9-.]*\.[a-zA-Z]+(\?['._httpbl_mailcrypt_AUTORISE.']*)?,');
+
+function honeypot_affichage_final($flux) {
+  include_spip('inc/httpbl'); 
+  $info = httpbl_test($_SERVER['REMOTE_ADDR'],lire_config('honeypot/httpbl/apikey'));
+  //  $info = httpbl_test('127.1.40.1',lire_config('honeypot/httpbl/apikey'));
+  $config = lire_config('honeypot/httpbl');
+  if(min(intval($config['type'.$info['type'].'_threat']),255) <=  $info['threat']){
+	if(($config['type'.$info['type'].'_filter'] == 'cacheremail') || ($config['type'.$info['type'].'_filter'] == 'cachertout')){
+	  $flux = preg_replace(_httpbl_mailcrypt_REGEXPR, sinon($config['remplacement_email'],'NOSPAM'), $flux);	
+		//log pour l'instant, TODO faire mieux
+		spip_log("cacher les emails à ".$_SERVER['REMOTE_ADDR']." parce que ".$info['raw'],'httpbl');
+
+	}
+  }
+
+  return $flux;
+
+}
+
+?>
