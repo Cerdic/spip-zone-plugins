fbbc40bc17fde944493123ddf79ab5c0345fe983
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-09-29 19:48:50 +0000

    Ça marche en 1.9.3 10456. Est-ce que ça marche aussi en 1.9.2 ?
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@15624 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/spipBB/public/styliser192.php
+++ _dev_/spipBB/public/styliser192.php
@@ -10,30 +10,23 @@
  *  Pour plus de details voir le fichier COPYING.txt ou l'aide en ligne.   *
 \***************************************************************************/
 
-
 if (!defined("_ECRIRE_INC_VERSION")) return;
-
-global $spip_version_code;
-if (version_compare($spip_version_code,'1.925','<')){
-	include_spip('public/styliser192');; // SPIP 1.9.2
-} else { // SPIP 1.9.3
-
+include_spip('inc/spipbb');
 
 // Ce fichier doit imperativement definir la fonction ci-dessous:
 
-// Actuellement tous les squelettes se terminent par .html
-// pour des raisons historiques, ce qui est trompeur
-
-// http://doc.spip.org/@public_styliser_dist
-function public_styliser_dist($fond, $id_rubrique, $lang, $ext='html') {
+function public_styliser($fond, $id_rubrique, $lang) {
 	
+  // Actuellement tous les squelettes se terminent par .html
+  // pour des raisons historiques, ce qui est trompeur
+	$ext = 'html';
 	// Accrocher un squelette de base dans le chemin, sinon erreur
 	if (!$base = find_in_path("$fond.$ext")) {
 		include_spip('public/debug');
 		erreur_squelette(_T('info_erreur_squelette2',
 			array('fichier'=>"'$fond'")),
 			$GLOBALS['dossier_squelettes']);
-		$f = find_in_path(".$ext"); // on ne renvoie rien ici, c'est le resultat vide qui provoquere un 404 si necessaire
+		$f = find_in_path("404.$ext");
 		return array(substr($f, 0, -strlen(".$ext")),
 			     $ext,
 			     $ext,
@@ -45,16 +38,17 @@ function public_styliser_dist($fond, $id_rubrique, $lang, $ext='html') {
 
 	// traitement spipbb : on recherche un squelette defini
 	unset($sqel);
-	$spipbb_meta = @unserialize($GLOBALS['meta']['spipbb']);
+	$spipbb_meta=$GLOBALS['spipbb'];
 	$id_rubrique = intval($id_rubrique);
 
 	if ( is_array($spipbb_meta)
-	  AND ($fond=="article" OR $fond=="rubrique")
-	  AND $id_rubrique>0 ) {
-		spip_log("debug spipbb:".$id_rubrique.":sq:".$fond.":meta:".$spipbb_meta['spipbb_id_rubrique'], 'spipbb');
+	     AND ($fond=="article" OR $fond=="rubrique")
+	     AND $id_rubrique>0 )
+	{
+//		echo "debug spipbb:".$id_rubrique.":sq:".$fond.":meta:".$spipbb_meta['spipbb_id_rubrique']."<br>\n"  ;
 
 		if (empty($spipbb_meta['spipbb_squelette_filforum']) OR empty($spipbb_meta['spipbb_squelette_groupeforum']) ) spipbb_init_metas($id_rubrique);
-		$id_rub = $id_rubrique;
+		$id_rub=$id_rubrique;
 
 		while ($id_rub > 0 AND $id_rub!=intval($spipbb_meta['spipbb_id_rubrique'])) {
 			$id_rub = sql_parent($id_rub);
@@ -67,40 +61,39 @@ function public_styliser_dist($fond, $id_rubrique, $lang, $ext='html') {
 			if ( $squel=find_in_path("$sq.$ext") ) $squelette = substr($squel, 0, - strlen(".$ext"));
 		}
 	}
-	else spip_log("debug spipbb:".$id_rubrique.":sq:".$fond.":meta:".$spipbb_meta['spipbb_id_rubrique'], 'spipbb') ;
+//	else echo "debug spipbb:".$id_rubrique.":sq:".$fond.":meta:".$spipbb_meta['spipbb_id_rubrique'] ."<br>\n" ;
 
 	// traitement normal
-	if (!$squel) { 
-		// On selectionne, dans l'ordre :
-		// fond=10
-		$f = "$fond=$id_rubrique";
-		if (($id_rubrique > 0) AND ($squel=find_in_path("$f.$ext")))
-			$squelette = substr($squel, 0, - strlen(".$ext"));
-		else {
-			// fond-10 fond-<rubriques parentes>
-			while ($id_rubrique > 0) {
-				$f = "$fond-$id_rubrique";
-				if ($squel=find_in_path("$f.$ext")) {
-					$squelette = substr($squel, 0, - strlen(".$ext"));
-					break;
-				} else
-					$id_rubrique = quete_parent($id_rubrique);
+	if (!$squel)
+	{ 
+	// On selectionne, dans l'ordre :
+	// fond=10
+	$f = "$fond=$id_rubrique";
+	if (($id_rubrique > 0) AND ($squel=find_in_path("$f.$ext")))
+		$squelette = substr($squel, 0, - strlen(".$ext"));
+	else {
+		// fond-10 fond-<rubriques parentes>
+		while ($id_rubrique > 0) {
+			$f = "$fond-$id_rubrique";
+			if ($squel=find_in_path("$f.$ext")) {
+				$squelette = substr($squel, 0, - strlen(".$ext"));
+				break;
 			}
+			else
+				$id_rubrique = sql_parent($id_rubrique);
 		}
+	}
 	}
 
 	// Affiner par lang
 	if ($lang) {
-		$l = lang_select($lang);
+		lang_select($lang);
 		$f = "$squelette.".$GLOBALS['spip_lang'];
-		if ($l) lang_select();
+		lang_dselect();
 		if (@file_exists("$f.$ext"))
 			$squelette = $f;
 	}
 
 	return array($squelette, $ext, $ext, "$squelette.$ext");
 }
-
-} // fin de la condition de version de SPIP
-
 ?>
