56a385c4a47fcf4214f7c9ecf2664a30df012279
pierre.andrews@gmail.com <pierre.andrews@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-09-29 14:26:50 +0000

    on peut maintenant aussi bloquer les forums. option pour que les ips douteux soient renvoyes vers un 403 seulement quand ils essaient de poster un message de forum ou un mail a l'auteur.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@15602 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/honeypot/honeypot_httpbl_options.php
+++ _test_/honeypot/honeypot_httpbl_options.php
@@ -16,33 +16,56 @@
    *   You should have received a copy of the GNU General Public License
    *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
    */
+
+function httpbl_send403($info) {
+  include_spip('inc/headers');
+  http_status(403);
+  include_spip('public/assembler');
+  echo recuperer_fond("fonds/403",array(
+										'ip'=>$_SERVER['REMOTE_ADDR'],
+										'type'=>_T('honeypothttpbl:type'.$info['type']),
+										'threat'=>$info['threat'],
+										'age'=>$info['age'],
+										));
+  exit();
+}
+
 include_spip('cfg_options');
-if((_DIR_RACINE =='') && lire_config('honeypot/httpbl/apikey')) {
+
+if((_DIR_RACINE =='') && 
+   (strpos($_SERVER['PHP_SELF'],_DIR_RESTREINT_ABS) === false) &&
+   lire_config('honeypot/httpbl/apikey')) {
   include_spip('inc/httpbl'); 
   $info = httpbl_test($_SERVER['REMOTE_ADDR'],lire_config('honeypot/httpbl/apikey'));
   //pour le test
-  //$info = httpbl_test('127.1.40.1',lire_config('honeypot/httpbl/apikey'));
+  //  $info = httpbl_test('127.1.40.1',lire_config('honeypot/httpbl/apikey'));
   $config = lire_config('honeypot/httpbl');
   if($info) {
 	if(min(intval($config['type'.$info['type'].'_threat']),255) <=  $info['threat']){
 	  if($config['type'.$info['type'].'_filter'] == 'bloquer'){
-		include_spip('inc/headers');
-		http_status(403);
-		include_spip('public/assembler');
-		echo recuperer_fond("fonds/403",array(
-											  'ip'=>$_SERVER['REMOTE_ADDR'],
-											  'type'=>_T('honeypothttpbl:type'.$info['type']),
-											  'threat'=>$info['threat'],
-											  'age'=>$info['age'],
-											  ));
 		//log pour l'instant, TODO faire mieux
 		spip_log("bloquer ".$_SERVER['REMOTE_ADDR']." parce que ".$info['raw'],'httpbl');
-		exit();
+		httpbl_send403($info);		
 	  } else if($config['type'.$info['type'].'_filter'] == 'tohoneypot') {
 		include_spip('inc/headers');
 		//log pour l'instant, TODO faire mieux
 		spip_log("envoyer vers le pot de miel ".$_SERVER['REMOTE_ADDR']." parce que ".$info['raw'],'httpbl');
 		redirige_par_entete($GLOBALS['meta']['adresse_site'].'/'.lire_config('honeypot/hpfile').'.php');
+	  } else if(count($_POST) &&
+				($config['type'.$info['type'].'_filter'] == 'cacherforum' || $config['type'.$info['type'].'_filter'] == 'cachertout' )) {
+		// champs de formulaires a visiter (depuis couteau suisse)
+		//    un message en forum : texte, titre, auteur
+		//    un message a un auteur : texte_message_auteur_XX, sujet_message_auteur_XX, email_message_auteur_XX
+		$spam_POST_reg = ',^(texte|titre|sujet|auteur|email),i';
+	
+		// on regarde si c'est un post qui nous interesse
+		foreach (array_keys($_POST) as $key)
+		  if (preg_match($spam_POST_reg, $key)) {
+			//log pour l'instant, TODO faire mieux
+			spip_log("cacher forum ".$_SERVER['REMOTE_ADDR']." parce que ".$info['raw'],'httpbl');
+			httpbl_send403($info);		
+			break;
+		  }
 	  }
 	}
   }
