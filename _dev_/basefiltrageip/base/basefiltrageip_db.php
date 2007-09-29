1efe5ea4e3ed557a07bdb37c1db4ade6372875b7
pierre.andrews@gmail.com <pierre.andrews@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-09-29 20:23:38 +0000

    un debut pour un plugin de centralisation des fonctions pour les plugins de filtrage d'ip. Fournira la gestion de la whitelist, des stats et logs.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@15629 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _dev_/basefiltrageip/base/basefiltrageip_db.php
@@ -0,0 +1,59 @@
+<?php
+
+$GLOBALS['basefiltrageip_version'] = 1;
+
+$spip_basefiltrageip_whitelist = array(
+	"first_ip" 	=> "long NOT NULL",
+	"last_ip" 	=> "long NOT NULL",
+	"maj" 		=> "TIMESTAMP");
+
+$spip_basefiltrageip_whitelist_key = array(
+						 "PRIMARY KEY" => "first_ip",
+						 "UNIQ" => "last_ip");
+
+global $tables_principales;
+$tables_principales['spip_basefiltreageip_whitelist'] = array(
+	'field' => &$spip_basefiltrageip_whitelist,
+	'key' => &$spip_basefiltrageip_whitelist_key);
+
+
+$spip_basefiltrageip_log = array(
+								 "ip" 	=> "long NOT NULL",
+								 "reason_id" => "int NOT NULL",
+								 "count" => "biginy DEFAULT 0 NOT NULL",
+								 "firstseen" 		=> "datetime DEFAULT '0000-00-00 00:00:00' NOT NULL",
+								 "lastseen" 		=> "TIMESTAMP");
+
+$spip_basefiltrageip_log_key = array(
+						 "KEY" => "ip",
+						 "KEY" => "reason_id");
+
+$tables_principales['spip_basefiltreageip_log'] = array(
+	'field' => &$spip_basefiltrageip_log,
+	'key' => &$spip_basefiltrageip_log_key);
+
+
+$spip_basefiltrageip_reasons = array(
+								 "reason_id" => "int NOT NULL",
+								 "reason_string" => "text");
+
+$spip_basefiltrageip_reasons_key = array(
+						 "PRIMARY KEY" => "reason_id");
+
+$tables_principales['spip_basefiltreageip_reasons'] = array(
+	'field' => &$spip_basefiltrageip_reasons,
+	'key' => &$spip_basefiltrageip_reasons_key);
+
+
+
+//-- Relations ----------------------------------------------------
+global $tables_jointures;
+$tables_jointures['spip_basefiltreageip_reasons'][] = 'spip_basefiltreageip_log';
+
+global $table_des_tables;
+$table_des_tables['basefiltrageip_whitelist']='basefiltrageip_whitelist';
+$table_des_tables['basefiltrageip_log']='basefiltrageip_log';
+
+
+
+?>
---
+++ _dev_/basefiltrageip/base/basefiltrageip_upgrade.php
@@ -0,0 +1,41 @@
+<?php
+
+function basefiltrageip_droptables() {
+  spip_query("DROP TABLE IF EXISTS ".$GLOBALS['table_prefix']."_basefiltrageip_whitelist, ".$GLOBALS['table_prefix']."_basefiltrageip_log;");
+}
+
+function basefiltrageip_doupgrade() {
+  		include_spip('base/basefiltrageip_db');
+		$installe = unserialize($GLOBALS['meta']['basefiltrageip:installe']);
+	  $uptodate = $installe && ($installe == $GLOBALS['basefiltrageip_version']);
+	  if(!$uptodate) {
+		include_spip('base/create');
+		include_spip('base/abstract_sql');
+		creer_base();
+		ecrire_meta('basefiltrageip:installe',$GLOBALS['basefiltrageip_version']);
+	  }
+	  ecrire_metas();
+}
+
+function basefiltrageip_install($action){
+  switch ($action){
+	case 'test':
+	  include_spip('base/basefiltrageip_db');
+	  //Contrôle du plugin à chaque chargement de la page d'administration
+	  $installe = unserialize(lire_meta('basefiltrageip:installe'));
+	  $uptodate = $installe && ($installe == $GLOBALS['basefiltrageip_version']);
+	  return $uptodate;
+	  break;
+	case 'install':
+	  basefiltrageip_doupgrade();
+	  break;
+	case 'uninstall':
+	  //Appel de la fonction de suppression
+	  basefiltrageip_droptables();
+	  effacer_meta('basefiltrageip:installe');
+	  ecrire_metas();
+	  break;
+  }
+}
+
+?>
