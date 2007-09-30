1b3fe104780ee215b523721fb9f74fb927de03ac
spipcarto@gmail.com <spipcarto@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-09-30 14:50:54 +0000

    version 0.2 : - multidiapo - taille par defaut adaptee a la dist - diapo en relatif pour eviter scroll au changement d'image (? il faut sans doute calculer la hauteur max et l'affecter)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@15651 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/diapo/diapo.js
+++ _stable_/diapo/diapo.js
@@ -1,60 +1,61 @@
 
 var diapo_on=false;
 var center=true;
-function diaposlide(timeout){
+function diaposlide(timeout,mDiapo){
 	if (diapo_on){
-		mClass=$("#diapo_grand").show().attr('class').split('_');
+		mClass=$("#"+mDiapo+" .diapo .diapo_grand").show().attr('class').replace(' diapo_grand','').split('_');
 		mpage="diapo_img";
 		mid_article=mClass[1];
 		mnum=mClass[2];
-		$.get("spip.php",
+	    $.get("spip.php",
 			{page : mpage, id_article : mid_article, num : mnum},
 			function(txt){
 				debut=txt.indexOf("<!-- debut diapo_img"+mid_article+" -->");
     			fin=txt.lastIndexOf("<!-- fin diapo_img"+mid_article+" -->");
     			txt=txt.substring(debut,fin);
-				$("#diapo").html(txt);
+				$("#"+mDiapo+" .diapo").html(txt);
 			});
-		setTimeout('diaposlide('+timeout+')', timeout);
+		setTimeout('diaposlide('+timeout+',mDiapo)', timeout);
 	}
 }
 $.fn.diapo_mode = function() {
     return this.click(function() {
-    	$("#diapo_icones a").removeClass("selected");
+    	mDiapo=$(this).attr('rel');
+		$("#"+mDiapo+" .diapo_icones a").removeClass("selected");
 		$(this).addClass("selected");
-		mId=$(this).attr('id');
-		if (mId=="diapo_ico"){
-			$("#diapo_vignettes").attr("class","diapo_vignettes_invisible");
-			$("#diapo").attr("class","diapo_grand");
-			$("#diapo_petit").hide();
-			$("#diapo_grand").show();
+		mId=$(this).attr('class');
+		if (mId=="diapo_ico selected"){
+			$("#"+mDiapo+" .diapo_vignettes").attr("class","diapo_vignettes diapo_vignettes_invisible");
+			$("#"+mDiapo+" div.diapo").attr("class","diapo diapo_grand");
+			$("#"+mDiapo+" div.diapo img.diapo_petit").hide();
+			$("#"+mDiapo+" div.diapo img.diapo_grand").show();
 			diapo_on=!diapo_on;
 			center=true;
 			if ($(this).html()=='||')$(this).html('&gt;');
 			else $(this).html('||');
-			diaposlide(8000);
-		}else if (mId=="diapo_icoleft"){
-			$("#diapo_vignettes").attr("class","diapo_vignettes_left");
-			$("#diapo").attr("class","diapo_petit");
-			$("#diapo_grand").hide();
-			$("#diapo_petit").show();
-			$("#diapo_ico").html('&gt;');
+			diaposlide(8000,mDiapo);
+		}else if (mId=="diapo_icoleft selected"){
+			$("#"+mDiapo+" .diapo_vignettes").attr("class","diapo_vignettes diapo_vignettes_left");
+			$("#"+mDiapo+" div.diapo").attr("class","diapo diapo_petit");
+			$("#"+mDiapo+" div.diapo .diapo_grand").hide();
+			$("#"+mDiapo+" div.diapo .diapo_petit").show();
+			$("#"+mDiapo+" .diapo_ico").html('&gt;');
 			diapo_on=false;
 			center=false;
-		}else if (mId=="diapo_icoright"){
-			$("#diapo_vignettes").attr("class","diapo_vignettes_right");
-			$("#diapo").attr("class","diapo_petit");
-			$("#diapo_grand").hide();
-			$("#diapo_petit").show();
-			$("#diapo_ico").html('&gt;');
+		}else if (mId=="diapo_icoright selected"){
+			$("#"+mDiapo+" .diapo_vignettes").attr("class","diapo_vignettes diapo_vignettes_right");
+			$("#"+mDiapo+" div.diapo").attr("class","diapo diapo_petit");
+			$("#"+mDiapo+" div.diapo img.diapo_grand").hide();
+			$("#"+mDiapo+" div.diapo img.diapo_petit").show();
+			$("#"+mDiapo+" .diapo_ico").html('&gt;');
 			diapo_on=false;
 			center=false;
 		}else{
-			$("#diapo_vignettes").attr("class","diapo_vignettes");
-			$("#diapo").attr("class",'diapo_grand');
-			$("#diapo_petit").hide();
-			$("#diapo_grand").show();
-			$("#diapo_ico").html('&gt;');
+			$("#"+mDiapo+" .diapo_vignettes").attr("class","diapo_vignettes");
+			$("#"+mDiapo+" div.diapo").attr("class",'diapo diapo_grand');
+			$("#"+mDiapo+" div.diapo img.diapo_petit").hide();
+			$("#"+mDiapo+" div.diapo img.diapo_grand").show();
+			$("#"+mDiapo+" .diapo_ico").html('&gt;');
 			diapo_on=false;
 			center=true;
 		}
@@ -64,9 +65,10 @@ $.fn.diapo_mode = function() {
 $.fn.diapo_pagination = function() {
     return this.click(function() {
     	pagin="";
-    	mClass=$("#diapo_grand").attr('class').split('_');
+    	mDiapo=$(this).attr('rel');
+    	mClass=$("#"+mDiapo+" .diapo_grand").attr('class').split('_');
 		mPage="diapo";
-		malign=$("#diapo_icones a.selected").attr('id').replace("diapo_ico","");
+		malign=$("#"+mDiapo+" .diapo_icones a.selected").attr('class').replace("diapo_ico","").replace(" selected","");
 		mid_article=mClass[1];
 		tab=$(this).attr('href').split('#');
     	i=tab[0].lastIndexOf('debut_')
@@ -79,9 +81,9 @@ $.fn.diapo_pagination = function() {
     				fin=txt.lastIndexOf("<!-- fin diapo"+mid_article+" -->");
     				txt=txt.substring(debut,fin);
     				$("#diapo"+mid_article).html(txt);
-    				$("#diapo_icones a").diapo_mode();
-					$("div.diapo_menu a.lien_pagination").diapo_pagination();
-					$("div.diapo_vignette a").diapo_vignette();
+    				$("#diapo"+mid_article+" .diapo_icones a").diapo_mode();
+					$("#diapo"+mid_article+" div.diapo_menu a.lien_pagination").diapo_pagination();
+					$("#diapo"+mid_article+" div.diapo_vignette a").diapo_vignette();
     			});
 		return false;
     });
@@ -92,14 +94,14 @@ $.fn.diapo_vignette = function() {
  		mpage="diapo_img";
 		mid_article=mClass[1];
 		mnum=mClass[2]-1;
-		malign=$("#diapo_icones a.selected").attr('id').replace("diapo_ico","");
+		malign=$("#diapo"+mid_article+" .diapo_icones a.selected").attr('class').replace("diapo_ico","").replace(" selected","");
 		$.get("spip.php",
 				{page : mpage, id_article : mid_article, num : mnum, align : malign},
 				function(txt){
 					debut=txt.indexOf("<!-- debut diapo_img"+mid_article+" -->");
     				fin=txt.lastIndexOf("<!-- fin diapo_img"+mid_article+" -->");
     				txt=txt.substring(debut,fin);
-    				$("#diapo").html(txt);
+    				$("#diapo"+mid_article+" .diapo").html(txt);
 				});
 		return false;
     });
@@ -108,9 +110,7 @@ $.fn.diapo_center = function() {
 	return this.css("display")=="none";
 }
 $(document).ready(function(){
-	$("#diapo_icones a").diapo_mode();
+	$(".diapo_icones a").diapo_mode();
 	$("div.diapo_menu a.lien_pagination").diapo_pagination();
 	$("div.diapo_vignette a").diapo_vignette();
-	diapogrand=$("#diapo_grand").get(0);
-	if (diapogrand) center=!($("#diapo_grand").css("display")=="none");
 });
