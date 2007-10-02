ad80f59df52df6977c3a53c46b081ad149623dcb
pierre.andrews@gmail.com <pierre.andrews@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-10-02 23:57:32 +0000

    debut d'action
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@15699 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/blockreferer/blockreferer_pipeline.php
+++ _dev_/blockreferer/blockreferer_pipeline.php
@@ -1,14 +1,18 @@
 <?php
 
 function blockreferer_header_prive($flux) {
-
-  if(strpos('statistiques',_request('exec'))>=0) {
-	$script = '<script type="text/javascript"><!--
+  //  var_dump($auteur);
+  if((strpos('statistiques',_request('exec'))>=0)
+	  && ($GLOBALS['auteur_session']['statut'] == '0minirezo')) {
+  
+  $script = '<script type="text/javascript"><!--
 $(document).ready(function(){
-jQuery(\'.verdana1 > ul > li > a > span\').parent().each(function(i,e){$(e).append("<a href=\'"+encodeURI(this.href)+"\'>[X]</a>")});
+jQuery(\'.verdana1 > ul > li > a > span\').parent().each(function(i,e){$(e).after("&nbsp;<a href=\''.
+	generer_action_auteur("blockreferer_manage","referer",generer_url_ecrire(_request('exec')))
+.'&url_refer="+encodeURI(this.href)+"\'>[X]</a>")});
 });
 --></script>';
-	$flux .= $script;
+  $flux .= $script;
   }
 	return $flux;
 
