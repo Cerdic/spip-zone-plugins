838ac5a47cb62d3790dedf1edd2a84288e317c2b
horaciogd@vhplab.net <horaciogd@vhplab.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-10-02 22:51:05 +0000

    we have fixed some problems of map visualization in private area.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@15696 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/googlemap_api/inc/geomap_append_view_map.php
+++ _dev_/googlemap_api/inc/geomap_append_view_map.php
@@ -9,34 +9,41 @@
  *
  */
 
-function inc_geomap_append_view_map_dist($target_id,$view_lat,$view_long,$view_zoom=NULL,$Markers = NULL){
+function inc_geomap_append_view_map_dist($target_id,$view_lat,$view_long,$view_zoom=NULL,$Markers = NULL,$view_icon = NULL){
 	if (!strlen($view_zoom) OR !is_numeric($view_zoom)){
 		$view_zoom = isset($GLOBALS['meta']['geomap_default_zoom'])?$GLOBALS['meta']['geomap_default_zoom']:'8'; 
 		if (!strlen($view_zoom) OR !is_numeric($view_zoom)) $view_zoom='8';
 	}
 	$geomap_script_init = charger_fonction('geomap_script_init','inc');
-	return 
-	$out = $geomap_script_init();
+	$out .= $geomap_script_init();
 	$out .=
 		"<script type='text/javascript'>
 		/*<![CDATA[*/\n
 		if (GBrowserIsCompatible()) {
-		/* create the map*/
-			var map = new GMap2(document.getElementById('map'));
-			map.setCenter(new GLatLng(".$view_lat.",".$view_long."), ".$view_zoom.", G_MAP_TYPE);
-			icono = new GIcon();";
+			var viewMap = new GMap2(document.getElementById('".$target_id."'));
+			viewMap.setCenter(new GLatLng(".$view_lat.",".$view_long."), ".$view_zoom.", G_MAP_TYPE);
+			viewIcono = new GIcon();";
 	if (is_array($Markers) AND count($Markers)){
 		foreach($Markers as $point){
+			if ($view_icon){
+				$out .= "
+			viewIcono.image = \"/IMG/".$view_icon."\";
+			viewIcono.shadow = \""._DIR_PLUGIN_GEOMAP."img_pack/shadow.png\";";
+			
+			} else {
+				$out .= "
+			viewIcono.image = \""._DIR_PLUGIN_GEOMAP."img_pack/correxir.png\";
+			viewIcono.shadow = \""._DIR_PLUGIN_GEOMAP."img_pack/shadow.png\";";
+			}
+			
 			$out .= "
-			icono.image = \""._DIR_PLUGIN_GEOMAP."img_pack/correxir.png\";
-			icono.shadow = \"http://www.escoitar.org/loudblog/custom/templates/berio/shadow.png\";
-			icono.iconSize = new GSize(20, 34);
-			icono.shadowSize = new GSize(22, 20);
-			icono.iconAnchor = new GPoint(10, 34);
-			icono.infoWindowAnchor = new GPoint(5,1);
-			point = new GPoint(".$point['lon'].",".$point['lat'].");
-			marker = new GMarker(point, icono);
-			map.addOverlay(marker);";
+			viewIcono.iconSize = new GSize(20, 34);
+			viewIcono.shadowSize = new GSize(22, 20);
+			viewIcono.iconAnchor = new GPoint(10, 34);
+			viewIcono.infoWindowAnchor = new GPoint(5,1);
+			viewPoint = new GPoint(".$point['lonx'].",".$point['lat'].");
+			viewMarker = new GMarker(viewPoint, viewIcono);
+			viewMap.addOverlay(viewMarker);";
 		}
 	}
 	$out .= "
