1769f78ed23e8c727274c4727ce9f89735af6971
patfr@ifrance.com <patfr@ifrance.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-10-02 03:29:33 +0000

    Voici le glossaire interne sous deux modes différents : - La solution CSS, qui joue que la visibilité de blocs <span> sensibles au passage de la souris et insérés dans le texte d’origine à côté du mot à définir. - La solution Javascript/jQuery, qui joue sur l’insertion en temps réel de la définition du mot dans un bloc <div> qui apparait au niveau du mot survolé, grâce au passage de la souris. Ces deux solutions présentent l’avantage d’être automatiques. Aucune nouvelle fenêtre (popup) n’est ouverte.
    Des tests de compatibilité sur différents navigateurs sont les bienvenus !
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@15673 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/couteau_suisse/outils/glossaire.js
+++ _stable_/couteau_suisse/outils/glossaire.js
@@ -1,46 +1,36 @@
 var gloss_posx = 0;
 var gloss_posy = 0;
-var gloss_offy = 0
+var gloss_offy = 0;
 var gloss_el = null;
 var gloss_dt = null;
 var gloss_dd = null;
 
 jQuery(document).ready(function() {
   if($('span.gl_js').length) {
-    $('body').append('<div id="overDiv" style="position:absolute; display:none; visibility: hidden;" class="cs_glossaire"><span class="gl_dl"><span class="gl_dt">TITRE</span><span class="gl_dd">Definition</span></span></div>');
+    $('body').append('<div id="glossOverDiv" style="position:absolute; display:none; visibility: hidden;" class="cs_glossaire"><span class="gl_dl"><span class="gl_dt">TITRE</span><span class="gl_dd">Definition</span></span></div>');
 	$('span.gl_mot').bind('mouseout',
 		function(e) {
-			GlossHideOverDiv();
+			gloss_el.style.display    = 'none';
+			gloss_el.style.visibility = 'hidden';
 		}
 	);
 	$('span.gl_mot').bind('mouseover', 
 		function(e) {
 			GlossMouse(e);
-			GlossShowOverDiv(e);
+			gloss_el.style.top  = gloss_posy.toString()+"px";
+			gloss_el.style.left = gloss_posx.toString()+"px";
+			gloss_dt.innerHTML = e.target.nextSibling.title; // titre
+			gloss_dd.innerHTML = e.target.nextSibling.nextSibling.title; // definition
+			gloss_el.style.display    = 'block';
+			gloss_el.style.visibility = 'visible';
 		}
 	);
-	gloss_el = document.getElementById("overDiv");
+	gloss_el = document.getElementById('glossOverDiv');
+	gloss_el.style.fontSize = $('span.gl_mot').css('font-size');
 	gloss_dt = gloss_el.firstChild.firstChild;
 	gloss_dd = gloss_el.firstChild.lastChild;
   }
 });
-
-function GlossShowOverDiv(e) {
-	var yy = (gloss_offy < 200) ? (gloss_posy + 10) : (gloss_posy - 70);
-	var xx = (gloss_posx + 10);
-	gloss_el.style.top  = yy.toString()+"px";
-	gloss_el.style.left = xx.toString()+"px";
-	gloss_el.style.display    = 'block';
-	gloss_el.style.visibility = 'visible';
-	//gloss_el.html = gloss_el.style.top;
-	gloss_dt.innerHTML = e.target.nextSibling.title;
-	gloss_dd.innerHTML = e.target.nextSibling.nextSibling.title;
-}
-
-function GlossHideOverDiv() {
-	gloss_el.style.display    = 'none';
-	gloss_el.style.visibility = 'hidden';
-}
 
 function GlossMouse(e) {
 	if (document.all) {
