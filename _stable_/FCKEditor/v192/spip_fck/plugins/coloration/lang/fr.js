27748ce3a303d3ceee07656782052c0fa999ae8a
vincevg@free.fr <vincevg@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-10-04 12:14:33 +0000

    Copié à distance
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@15760 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _stable_/FCKEditor/v192/spip_fck/plugins/coloration/lang/fr.js
@@ -0,0 +1,19 @@
+/*
+ * Fichier de langage pour le plugin Geshi. Frank SAURET
+ */
+
+FCKLang.ColorationTitre = 'Coloration syntaxique.';
+FCKLang.ColorationBulle = 'Insérer un code pour le colorer.';
+FCKLang.ColorationInserer = 'Collez votre code source ici :';
+FCKLang.ColorationCodeSource = 'Code source.';
+FCKLang.ColorationLangage = 'Langage : ';
+FCKLang.ColorationNumerodeLigne = ' Afficher les numéros de ligne.';
+FCKLang.ColorationPatience = 'Patientez, Geshi bosse pour vous...';
+FCKLang.ColorationTabulation = 'Taille de la tabulation : '; 
+FCKLang.ColorationPremiernumero = 'Premier numéro de ligne : '; 
+FCKLang.ColorationTxtAdresse = 'Le site officiel de Geshi.'; 
+FCKLang.ColorationTxtLicence = 'Geshi est sous licence GPL.';
+FCKLang.ColorationNumeroNormaux = 'Normaux'; 
+FCKLang.ColorationNumeroColore = 'Lignes colorées'; 
+FCKLang.ColorationPasDeNumero = 'Pas de numéro';  
+FCKLang.ColorationInterLigne = 'Interligne :'; 
