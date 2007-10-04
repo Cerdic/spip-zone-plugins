fb743ae082085c363eeee25e89d107aefd428c19
vincevg@free.fr <vincevg@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-10-04 12:14:01 +0000

    Copié à distance
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@15758 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _dev_/fckeditor/v192/spip_fck/plugins/helppanel/lang/fr.js
@@ -0,0 +1,22 @@
+﻿/*
+ * FCKeditor - The text editor for internet
+ * Copyright (C) 2003-2006 Frederico Caldeira Knabben
+ * 
+ * Licensed under the terms of the GNU Lesser General Public License:
+ * 		http://www.opensource.org/licenses/lgpl-license.php
+ * 
+ * For further information visit:
+ * 		http://www.fckeditor.net/
+ * 
+ * "Support Open Source software. What about a donation today?"
+ * 
+ * File Name: fr.js
+ * 	Placholder Italian language file.
+ * 
+ * File Authors:
+ * 		Hubert Garrido (liane@users.sourceforge.net)
+ */
+ 
+// Fichier de langue française
+FCKLang.HelppanelBtn = 'Aide';
+FCKLang.HelppanelDlgTitle = 'Aide';
