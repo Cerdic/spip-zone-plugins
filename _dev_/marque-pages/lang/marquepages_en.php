7a48c3ae6252c67eeb16f55897e6dc57bb12fd2b
vincent@ldd.fr <vincent@ldd.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-10-05 12:27:21 +0000

    traduction anglais / faut repasser derrière moi !!
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@15785 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _dev_/marque-pages/lang/marquepages_en.php
@@ -0,0 +1,36 @@
+<?php
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// Pour l'affichage
+'titre_marquepages' => 'Bookmarks',
+'historique' => 'Website history',
+'aucun_marquepages' => 'There is no bookmarks.',
+'avec_etiquettes' => 'with tags',
+'poste_le' => 'post on',
+'et' => 'and',
+'autres' => 'other(s)',
+'vous' => 'you',
+'supprimer' => 'Delete',
+'copier' => 'Copy',
+'confirmer' => 'Are you sure ?',
+'oui' => 'yes',
+'non' => 'no',
+'de' => 'of',
+
+// Pour le formulaire de saisi
+'ajouter' => 'Add a bookmark',
+'a' => 'to',
+'modifier' => 'Modify a bookmark',
+'url' => 'URL',
+'titre' => 'Title',
+'description' => 'Description',
+'pas_le_droit' => 'You are not authorized. You have to log in with an authorized account.',
+'se_deconnecter' => 'You can log out by clicking here.',
+'enregistre' => 'Your bookmark have been successfully saved.',
+'revenir' => 'Come back from where you came',
+'nouveau' => 'Add a new bookmark'
+
+);
+
+?>
