c930bc2e164ea198127312e45354191d296e098a
vincent@ldd.fr <vincent@ldd.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-10-05 09:00:54 +0000

    ajout d'un paramètre optionnel "url de retour" lors de la confiramation de l'inscription. Pour l'utiliser #FORMULAIRE_INSCRIPTION2_CONFIRMATION{url} ou #FORMULAIRE_INSCRIPTION2_CONFIRMATION{mode}{url}
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@15779 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/inscription2/inscription2_193/balise/formulaire_inscription2_confirmation.php
+++ _test_/inscription2/inscription2_193/balise/formulaire_inscription2_confirmation.php
@@ -7,13 +7,22 @@ function balise_FORMULAIRE_INSCRIPTION2_CONFIRMATION ($p) {
 
 // args[0] peut valoir "redac" ou "forum" 
 function balise_FORMULAIRE_INSCRIPTION2_CONFIRMATION_stat($args, $filtres) {
-	//initialiser mode d'inscription
+	//initialiser mode d'inscription et adresse de retour
 	$mode = $args[0];
-	if(!$mode)
+	$retour = $args[1];
+	
+	if(!$mode || ($mode != 'redac' && $mode != 'forum')){
 		$mode = $GLOBALS['meta']['accepter_inscriptions'] == 'oui' ? 'redac' : 'forum'; 
-	return array($mode);}
+		$retour = $args[0];
+	}
+	
+	if(!$retour)
+		$retour = $GLOBALS['meta']["adresse_site"];
+	
+	return array($mode, $retour);
+}
 
-function balise_FORMULAIRE_INSCRIPTION2_CONFIRMATION_dyn($mode) {
+function balise_FORMULAIRE_INSCRIPTION2_CONFIRMATION_dyn($mode, $retour) {
 	//recuperer les infos inserees dans l'environnement
 	
 	$id = _request('id');
@@ -24,22 +33,24 @@ function balise_FORMULAIRE_INSCRIPTION2_CONFIRMATION_dyn($mode) {
 	if($id != '' and $mode != '' and $cle != '' and $pass == ''){
 		$n = confirmation_inscription2($id, $mode, $cle);
 		if ($n == 'pass'){			
-
-		return inclure_balise_dynamique(
-			array("formulaires/inscription2_confirmation", 0,
-			array(
-				'id' => $id,
-				'mode' => $mode,
-				'cle' => $cle,
-				'pass' => $pass,
-			)
-		), false);
-	
-	}elseif($n == 'sup'){
+			
+			return inclure_balise_dynamique(
+				array("formulaires/inscription2_confirmation", 0,
+					array(
+						'id' => $id,
+						'mode' => $mode,
+						'cle' => $cle,
+						'pass' => $pass,
+					)
+				),
+				false
+			);
+			
+		}elseif($n == 'sup'){
 			spip_query("DELETE FROM spip_auteurs WHERE id_auteur = '$id'");
 			spip_query("DELETE FROM spip_auteurs_elargis WHERE id_auteur = '$id'");
 			echo "<strong>"._T('inscription2:suppression_faite')."</strong>";
-	}else
+		}else
 			echo "rien a faire";
 	}else{
 		if ($GLOBALS['spip_version_code']>=1.9259){
@@ -51,16 +62,18 @@ function balise_FORMULAIRE_INSCRIPTION2_CONFIRMATION_dyn($mode) {
 		$htpass = generer_htpass($pass);
 		$statut = lire_config('inscription2/statut_nouveau');
 		spip_query("UPDATE spip_auteurs SET statut = '$statut', pass='$pass', htpass='$htpass', alea_actuel='' WHERE id_auteur = ".$id);
-		echo "<strong>"._T('pass_nouveau_enregistre')."</strong><p><a href='".$GLOBALS['meta']["adresse_site"]."'>Retour</a></p>";
+		echo "<strong>"._T('pass_nouveau_enregistre')."</strong><p><a href='".$retour."'>"._T('retour')."</a></p>";
 		$var_user = spip_query("SELECT nom, email, login FROM spip_auteurs WHERE id_auteur=".$id);
 		$var_user = spip_fetch_array($var_user);
 		if($var_user){
 			$nom_site_spip = nettoyer_titre_email($GLOBALS['meta']["nom_site"]);
 			$adresse_site = $GLOBALS['meta']["adresse_site"];
+			
 			$message = _T('inscription2:message_auto')."\n\n" 
 			. _T('inscription2:email_bonjour', array('nom'=>$var_user['nom']))."\n\n"
 			. _T('inscription2:texte_email_confirmation', array('login'=> $var_user['login'], 'nom_site' => $nom_site_spip));
-			if (envoyer_mail($var_user['email'],"[$nom_site_spip] "._T('inscription2:compte_active',array('nom_site' => $nom_site_spip)), $message))
+			
+			if (envoyer_mail($var_user['email'],"[$nom_site_spip] "._T('inscription2:compte_active', array('nom_site' => $nom_site_spip)), $message))
 				return;
 			else
 				return _T('inscription2:probleme_email');
