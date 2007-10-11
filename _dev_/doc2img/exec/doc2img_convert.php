8d1c946bb056334a26ba7f8028a4693b2004a180
zeraxp@gmail.com <zeraxp@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-10-11 14:38:22 +0000

    modif d'accents requete sql revue modif du pipeline affiche gauche
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@15932 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/doc2img/exec/doc2img_convert.php
+++ _dev_/doc2img/exec/doc2img_convert.php
@@ -1,53 +1,58 @@
 <?php
+
+function exec_doc2img_convert(){
+
     include_spip('base/compat193');
 
-    //racine du site c'est … dire url_site/ecrire/..
+    //racine du site c'est a dire url_site/ecrire/..
     $racine_site = getcwd().'/..';
-    //document … traiter
+    //document a traiter
     $id_document = _request('id_document');
-
+    spip_log('doc2img a convertir : '.$id_document ,'doc2img');
     //format d'exportation (issu de cfg)
     $id_type = 2; //PNG
- 
-    //on recup‚re l'url du document
-    $sql = "SELECT fichier FROM spip_documents WHERE id_document=".$id_document;	
+
+    //on recupere l'url du document
+    $sql = "SELECT fichier FROM spip_documents WHERE id_document=".$id_document;
     $res = sql_fetch(spip_query($sql));
-         
+
     //nom complet du fichier : recherche ce qui suit le dernier / et retire ce dernier
     $document['fullname'] = substr(strrchr($res['fichier'], "/"),1);
     //url relative du repertoire contenant le fichier
     $document['source_url'] = substr($res['fichier'],0,strlen($res['fichier'])-strlen($document['fullname']));
-    //d‚compose nom.extension
+    //decompose nom.extension
     $file_array = explode(".",$document['fullname']);
     $document['extension'] = $file_array[1];
     $document['name'] = $file_array[0];
-    
+
     spip_log('nom du fichier : '.$document['name'] ,'doc2img');
-    
-    //cr‚ation du repertoire cible
+
+    //creation du repertoire cible
     //url relative du repertoire cible
     $document['cible_url'] = 'IMG/doc2img/'.$document['name'];
     //si le repertoire existe on ne genere pas les images, url absolue
-    if (@mkdir($racine_site.'/'.$document['cible_url'])) {
-    
+    if (@mkdir($racine_site.'/'.$document['cible_url'])!==false) {
+
         //charge le document dans imagick
         spip_log('charge le document','doc2img');
         $handle = imagick_readimage($racine_site.'/'.$document['source_url'].'/'.$document['fullname']);
-        
+
         //genere l'ensemble des images dans un sous repertoire du nom du fichier parent
         imagick_writeimages($handle, $racine_site.'/'.$document['cible_url'].'/'.$document['name'].'.png');
-    
-        //chaque page est un fichier on sauve dans la table doc2img chacun des ces nouveaux fichier 
-    
+
+        //chaque page est un fichier on sauve dans la table doc2img chacun des ces nouveaux fichier
+
         for ($frame = 0 ; $frame < imagick_getlistsize($handle); $frame++ ) {
             //nom du fichier cible
             $document['frame'] = $document['cible_url'].'/'.$document['name'].'-'.$frame.'.png';
-            //sauvegarde les donn‚es dans la base
-            $sql = "INSERT INTO spip_doc2img VALUES('',".$id_document.",".$id_type.",'".$document['frame']."');";    
+            //sauvegarde les donnees dans la base
+            $sql = "INSERT INTO spip_doc2img (id_doc2img,id_document,id_type,fichier) VALUES('',".$id_document.",".$id_type.",'".$document['frame']."');";
+            //print $sql;
             spip_query($sql);
         }
     }
-    
-    //recharge la page appelante    
+
+    //recharge la page appelante
     header("Location: ".$_SERVER['HTTP_REFERER']);
+}
 ?>
--- _dev_/doc2img/inc/doc2img_espace_prive.php
+++ _dev_/doc2img/inc/doc2img_espace_prive.php
@@ -2,7 +2,7 @@
 
     include_spip('base/compat193');
     include_spip('public/assembler');
-   
+
     function affiche_liste_doc($id_article) {
 
         spip_log('genere liste doc','doc2img');
@@ -13,18 +13,18 @@
 
             //genere un bloc de données pour le dod2img
         	$flux .= debut_cadre('r');
-            
+
                 //definition du contexte
                 $contexte = array("id_article" => $id_article);
                 //chargement du fond demandé
-                $flux .= recuperer_fond("squelette/doc2img",$contexte);              
-                         	   
+                $flux .= recuperer_fond("squelette/doc2img",$contexte);
+
         	$flux .= fin_cadre('r');
         return $flux;
     }
 
 /*
-ajax_action_post( $action, $arg, $retour, $gra, $corps, $clic=’’, $atts_i=’’, $atts_span = "", $args_ajax=’’ )      
+ajax_action_post( $action, $arg, $retour, $gra, $corps, $clic="", $atts_i="", $atts_span = "", $args_ajax="" )
 */
 
 ?>
