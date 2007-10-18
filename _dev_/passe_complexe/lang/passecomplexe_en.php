d5e3feb1909e48ad0571caf2ad9d3b68598a135e
pierre.andrews@gmail.com <pierre.andrews@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-10-18 14:13:40 +0000

    on passe le fichier de langue en module plutot que d'utiliser local_ + ajout de traductions en glaise.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@16120 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _dev_/passe_complexe/lang/passecomplexe_en.php
@@ -0,0 +1,16 @@
+<?php
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+									   'tres_faible' => 'very weak',
+									   'faible' => 'weak',
+									   'moyen' => 'average',
+									   'fort' => 'strong',
+									   'tres_fort' => 'very strong',
+									   'nb_mini' => 'minimum number of characters : ',
+									   'simple' => 'password too simple!',
+									   'court' => 'too short'
+
+);
+
+?>
---
+++ _dev_/passe_complexe/lang/passecomplexe_fr.php
@@ -0,0 +1,16 @@
+<?php
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+									   'tres_faible' => 'tr&egrave;s faible',
+									   'faible' => 'faible',
+									   'moyen' => 'moyen',
+									   'fort' => 'fort',
+									   'tres_fort' => 'tr&egrave;s fort',
+									   'nb_mini' => 'nombre de caract&egrave;res minimum : ',
+									   'simple' => 'mot de passe trop simple!',
+									   'court' => 'trop court'
+
+);
+
+?>
