f1802d46e2bdf15394dfdd83e4051a4a2bbed833
pierre.andrews@gmail.com <pierre.andrews@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-10-18 16:02:42 +0000

    on passe les options js majeure dans le options pour eviter le cache. On peut ainsi: - utiliser les chaines traduites dans la langue de l'interface, - ne pas avoir la config coincee dans le cache, - ajouter le nom de l'auteur dans les mots communs
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@16125 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/passe_complexe/passe_complexe_options.php
+++ _dev_/passe_complexe/passe_complexe_options.php
@@ -1,11 +1,40 @@
 <?php
+
+function passe_complexe_quote_common($list) {
+  $commons = split('[ ,]',$list);
+  $return = '';
+  for ($i = 0; $i < count($commons); $i++) {
+	if($commons[$i] && count($commons[$i]) > 0)
+	   $return .= ",'".str_replace("'","\\'",$commons[$i])."'";
+  }
+  return $return;
+}
+
 function passe_complexe_insert_head($flux){
 	if (($r=_request('exec'))=='auteur_infos'){
-
 		$flux .= '<script type="text/javascript" src="'.generer_url_public('jquery.pstrength.1.2.js').'"></script>';
+		$common_cfg = lire_config('passe_complexe/common');
+		if(count($common_cfg) <= 0) $common_cfg = '';
+		else $common_cfg = ','.$common_cfg;
+
 		$flux .= '<script type="text/javascript"><!--
 		$(document).ready(function() {
-            $("input.formo").filter(function(el) {return $(this).attr("name") == "new_pass";}).pstrength({minchar: 6});
+            $("input.formo").filter(function(el) {return $(this).attr("name") == "new_pass";}).pstrength({
+             minchar: '.max(lire_config('passe_complexe/length',6),6).',
+             common: ["123456","123","spip","test"'
+		  .',"'.$GLOBALS['auteur_session']['nom'].'"'
+		  .passe_complexe_quote_common(_T('passecomplexe:common').$common_cfg).'],
+             verdects:	["'
+		  ._T('passecomplexe:tres_faible').'","'
+		  ._T('passecomplexe:faible').'","'
+		  ._T('passecomplexe:moyen').'","'
+		  ._T('passecomplexe:fort').'","'
+		  ._T('passecomplexe:tres_fort').'","'
+		  ._T('passecomplexe:court').'","'
+		  ._T('passecomplexe:simple')
+		  .'"],
+             minchar_label:"'._T('passecomplexe:nb_mini').'"
+            });
 		});
 		--></script>';
 	}
