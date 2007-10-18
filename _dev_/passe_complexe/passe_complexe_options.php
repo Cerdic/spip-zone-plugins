4f66971b370ced8b20ae62218e990210b83caffa
pierre.andrews@gmail.com <pierre.andrews@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-10-18 14:12:45 +0000

    a pas l'air de marcher avec l'attribut name, au moins en 1.9.2c, changement de la methode de recherche, on filtre sur attr(name). Si c'est pas correct, changer ;)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@16119 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/passe_complexe/passe_complexe_options.php
+++ _dev_/passe_complexe/passe_complexe_options.php
@@ -5,7 +5,7 @@ function passe_complexe_insert_head($flux){
 		$flux .= '<script type="text/javascript" src="'.generer_url_public('jquery.pstrength.1.2.js').'"></script>';
 		$flux .= '<script type="text/javascript"><!--
 		$(document).ready(function() {
-			$("#new_pass").pstrength({minchar: 6});
+            $("input.formo").filter(function(el) {return $(this).attr("name") == "new_pass";}).pstrength({minchar: 6});
 		});
 		--></script>';
 	}
