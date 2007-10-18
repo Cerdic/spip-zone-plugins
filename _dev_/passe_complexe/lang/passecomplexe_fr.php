ecedb5f7a34266c8f59427b04a30b93766e448f0
pierre.andrews@gmail.com <pierre.andrews@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-10-18 16:00:19 +0000

    traductions pour la page de config
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@16124 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/passe_complexe/lang/passecomplexe_fr.php
+++ _dev_/passe_complexe/lang/passecomplexe_fr.php
@@ -2,14 +2,20 @@
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-									   'tres_faible' => 'tr&egrave;s faible',
-									   'faible' => 'faible',
-									   'moyen' => 'moyen',
-									   'fort' => 'fort',
-									   'tres_fort' => 'tr&egrave;s fort',
-									   'nb_mini' => 'nombre de caract&egrave;res minimum : ',
-									   'simple' => 'mot de passe trop simple!',
-									   'court' => 'trop court'
+									   'tres_faible' => 'Tr&egrave;s Faible',
+									   'faible' => 'Faible',
+									   'moyen' => 'Moyen',
+									   'fort' => 'Fort',
+									   'tres_fort' => 'Tr&egrave;s fort',
+									   'nb_mini' => 'Nombre de caract&egrave;res minimum : ',
+									   'simple' => 'Mot de Passe Trop Simple!',
+									   'court' => 'Trop Court',
+									   'common' => 'motdepasse,azerty,qwertzui',
+									   'cfg_common' => 'Liste des mots communs interdis dans les mots de passe. <small>(S&eacute;par&eacute;s par des virgules)</small> :',
+									   'cfg_titre' => 'Testeur de Mot de Passe',
+									   'cfg_descriptif' => 'Le plugin testeur de mot de passe permet de tester la &quot;force&quot; d\'un mot de passe.<br/>Les tests sont fait sur diff&eacute;rents crit&egrave;s comme la longueur du mot de passe, s\'ils contiennent des caract&egrave;res sp&eacute;ciaux, etc... <br/>Vous pouvez aussi sp&eacute;cifier une liste de mots interdits dans un mot de passe.',
+									   'cfg_longueur' => 'Longueur minimum du mot de passe : '
+
 
 );
 
