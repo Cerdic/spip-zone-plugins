5b93b3f0242aebb7b0b2b3e3987ede6f84468478
patfr@ifrance.com <patfr@ifrance.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-10-19 00:35:10 +0000

    glossaire.js : simplification du code
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@16133 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/couteau_suisse/outils/glossaire.js
+++ _stable_/couteau_suisse/outils/glossaire.js
@@ -6,15 +6,9 @@ var gloss_dt = null;
 var gloss_dd = null;
 
 jQuery(document).ready(function() {
-  if($('span.gl_js').length) {
-    $('body').append('<div id="glossOverDiv" style="position:absolute; display:none; visibility: hidden;"><span class="gl_dl"><span class="gl_dt">TITRE</span><span class="gl_dd">Definition</span></span></div>');
-	$('span.gl_mot').bind('mouseout',
-		function(e) {
-			gloss_el.style.display    = 'none';
-			gloss_el.style.visibility = 'hidden';
-		}
-	);
-	$('span.gl_mot').bind('mouseover', 
+  if(jQuery('span.gl_js').length) {
+    jQuery('body').append('<div id="glossOverDiv" style="position:absolute; display:none; visibility: hidden;"><span class="gl_dl"><span class="gl_dt">TITRE</span><span class="gl_dd">Definition</span></span></div>');
+	jQuery('span.gl_mot').hover(
 		function(e) {
 			// cas du surligneur (SPIP 1.93)
 			if(this.firstChild.className=="spip_surligne") {
@@ -27,9 +21,13 @@ jQuery(document).ready(function() {
 			gloss_el.style.left = gloss_posx.toString()+"px";
 			gloss_dt.innerHTML = this.nextSibling.title; // titre
 			gloss_dd.innerHTML = this.nextSibling.nextSibling.title; // definition
-			gloss_el.style.fontSize = $(this).css('font-size');
+			gloss_el.style.fontSize = jQuery(this).css('font-size');
 			gloss_el.style.display    = 'block';
 			gloss_el.style.visibility = 'visible';
+		},
+		function(e) {
+			gloss_el.style.display    = 'none';
+			gloss_el.style.visibility = 'hidden';
 		}
 	);
 	gloss_el = document.getElementById('glossOverDiv');
