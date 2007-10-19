a2bc900f558abeb4617d938d6b3af7ac40648250
pierre.andrews@gmail.com <pierre.andrews@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-10-19 18:14:45 +0000

    si on est dans un fonds CFG et que le champs est marque class=type_pwd, ajouter le testeur passe_complexe (attention, avec la version actuelle du test CFG sur ce type, ca veut rien dire...
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@16152 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/passe_complexe/passe_complexe_options.php
+++ _test_/passe_complexe/passe_complexe_options.php
@@ -5,6 +5,9 @@ function passe_complexe_header_prive($flux){
   if (_request('exec')=='auteur_infos'){
 	include_spip('inc/passe_complexe');
 	$flux .= passe_complexe_generer_javascript("input.formo[@name=new_pass]");
+  } else if (_request('exec')=='cfg'){
+	include_spip('inc/passe_complexe');
+	$flux .= passe_complexe_generer_javascript("input.type_pwd");
   }
   return $flux;
 }
