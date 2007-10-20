b658f45148754a357b6cf8f8d4d2cb56df7b0dea
fdm@ufe.be <fdm@ufe.be@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-10-20 17:14:25 +0000

    Nettoyage et cosmétique
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@16176 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/Association/Association_1.9.2/inc_association.php
+++ _test_/Association/Association_1.9.2/inc_association.php
@@ -1,37 +1,50 @@
 <?php
-/**
-* Plugin Association
-*
-* Copyright (c) 2007
-* Bernard Blazin & François de Montlivault
-* http://www.plugandspip.com 
-* Ce programme est un logiciel libre distribue sous licence GNU/GPL.
-* Pour plus de details voir le fichier COPYING.txt.
-*  
-**/
+	/**
+	* Plugin Association
+	*
+	* Copyright (c) 2007
+	* Bernard Blazin & François de Montlivault
+	* http://www.plugandspip.com 
+	* Ce programme est un logiciel libre distribue sous licence GNU/GPL.
+	* Pour plus de details voir le fichier COPYING.txt.
+	*  
+	**/
 
-$p=explode(basename(_DIR_PLUGINS)."/",str_replace('\\','/',realpath(dirname(__FILE__))));
-define('_DIR_PLUGIN_ASSOCIATION',(_DIR_PLUGINS.end($p)));
+	$p=explode(basename(_DIR_PLUGINS)."/",str_replace('\\','/',realpath(dirname(__FILE__))));
+	define('_DIR_PLUGIN_ASSOCIATION',(_DIR_PLUGINS.end($p)));
 
-function association_ajouterBoutons($boutons_admin) {
-
-	// si on est admin
-	if ($GLOBALS['connect_statut'] == "0minirezo" && $GLOBALS["connect_toutes_rubriques"]) {
+	function association_ajouterBoutons($boutons_admin) {
+		// si on est admin
+		if ($GLOBALS['connect_statut'] == "0minirezo" && $GLOBALS["connect_toutes_rubriques"]) {
 		
-	  // on voit le bouton dans la barre "naviguer"		
+		// on voit le bouton dans la barre "naviguer"		
 		
-	  $boutons_admin['naviguer']->sousmenu['association']= new Bouton(
-		"../"._DIR_PLUGIN_ASSOCIATION."/img_pack/annonce.gif",  // icone
-		_T('asso:titre_menu_gestion_association') //titre
-		);
-		
+		$boutons_admin['naviguer']->sousmenu['association']= new Bouton(
+			"../"._DIR_PLUGIN_ASSOCIATION."/img_pack/annonce.gif",  // icone
+			_T('asso:titre_menu_gestion_association') //titre
+			);
+		}
+		return $boutons_admin;
 	}
-	return $boutons_admin;
-}
 
-function association_ajouterOnglets($flux) {
-	$rubrique = $flux['args'];
-	return $flux;
-}
+	function association_ajouterOnglets($flux) {
+		$rubrique = $flux['args'];
+		return $flux;
+	}
+	
+	function association_header_prive($flux){
+		$flux .= '<link rel="stylesheet" type="text/css" href="'.direction_css(find_in_path('association.css')).'" />';
+		return $flux;
+	}
+	
+	function association_affiche_milieu($flux) {
+		$exec =  $flux['args']['exec'];
+		if ($exec=='auteur_infos'){
+			include_spip('inc/association_adherents');
+			$id_auteur = $flux['args']['id_auteur'];
+			$flux['data'] .= association_adherents($id_auteur);
+		}
+		return $flux;
+	}	
 
 ?>
