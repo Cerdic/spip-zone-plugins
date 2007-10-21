2ef8bbfb5c00ce026d3b686011c32c253cc9aae4
fdm@ufe.be <fdm@ufe.be@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-10-21 12:15:46 +0000

    Correction si l'adhérent n'existe pas encore
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@16213 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/Association/Association_1.9.2/exec/action_cotisations.php
+++ _test_/Association/Association_1.9.2/exec/action_cotisations.php
@@ -22,14 +22,19 @@
 		$date= $_POST['date'];
 		$journal= $_POST['journal'];
 		$montant= $_POST['montant'];
+		$categorie= $_POST['categorie'];
 		$justification =$_POST['justification'];
 		$validite =$_POST['validite'];
 		$url_retour=$_POST['url_retour'];
 			
 		if($action=="ajoute") {
-			spip_query( "INSERT INTO spip_asso_comptes (date, journal, recette, justification, imputation, id_journal) VALUES ("._q($date).", "._q($journal).", "._q($montant).", "._q($justification).", ".lire_config('association/pc_cotisations').", "._q($id_inxscription)." )" );
+			spip_query( "INSERT INTO spip_asso_comptes (date, journal, recette, justification, imputation, id_journal) VALUES ("._q($date).", "._q($journal).", "._q($montant).", "._q($justification).", ".lire_config('association/pc_cotisations').", "._q($id_inscription)." )" );
 			spip_query( "UPDATE spip_auteurs_elargis SET statut_interne='ok' WHERE id="._q($id_inscription) );
-			spip_query( "UPDATE spip_asso_adherent SET statut_relance='ok', validite="._q($validite)." WHERE id="._q($id_inscription) );
+			$query=spip_query("SELECT * FROM spip_asso_adherents WHERE id_inscription='$id_inscription' ");
+			if($query) {
+				spip_query( "UPDATE spip_asso_adherent SET statut_relance='ok', validite="._q($validite)." WHERE id_inscription="._q($id_inscription) );
+			} else {
+				spip_query( "INSERT INTO spip_asso_adherent (id_inscription, date, montant, categorie, statut_relance, validite) VALUES ("._q($id_inscription) ", "._q($date) ", "._q($montant) ", "._q($categorie) ", 'ok', "._q($validite).".)");
 			header ('location:'.$url_retour);
 			exit;
 		}
