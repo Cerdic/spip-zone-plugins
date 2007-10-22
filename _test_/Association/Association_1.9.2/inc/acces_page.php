f421728861746ef50a7801d3c3151f3373b16a69
fdm@ufe.be <fdm@ufe.be@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-10-22 20:13:32 +0000

    On sait jamais!
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@16267 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/Association/Association_1.9.2/inc/acces_page.php
+++ _test_/Association/Association_1.9.2/inc/acces_page.php
@@ -1,4 +1,5 @@
-<? php
+<?php
+	global $connect_statut, $connect_toutes_rubriques;
 	if (!($connect_statut == '0minirezo' AND $connect_toutes_rubriques)) {
 		debut_page();
 		echo _T('avis_non_acces_page');
