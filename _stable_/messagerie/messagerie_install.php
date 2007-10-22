80f96634919acfc282b14216c7e5f5bfa87756e5
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-10-22 15:14:45 +0000

    vu etait deja vu ... :(
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@16260 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/messagerie/messagerie_install.php
+++ _stable_/messagerie/messagerie_install.php
@@ -5,10 +5,7 @@
 		$current_version = 0.0;
 		if (   (!isset($GLOBALS['meta'][$nom_meta_base_version]) )
 				|| (($current_version = $GLOBALS['meta'][$nom_meta_base_version])!=$version_cible)){
-			if (version_compare($current_version,'0.1.0.0','<')){
-				spip_query("ALTER TABLE spip_messages ADD vu varchar(3) NOT NULL DEFAULT 'non'");
 				ecrire_meta($nom_meta_base_version,$current_version='0.1.0.0','non');
-			}
 			ecrire_metas();
 		}
 	}
