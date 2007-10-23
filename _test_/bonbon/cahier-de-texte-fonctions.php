e73e67c2f13a8663886bac807f9dcd3d34d22498
bmarne@gmail.com <bmarne@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-10-23 08:44:28 +0000

    Corrections de beaucoup de bugs. Améliorations esthétiques. Meilleure spipisation (balises/filtres à la place du php/html) Optmisations (preg_match par PHP plutôt que regexp MySQL)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@16287 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _test_/bonbon/cahier-de-texte-fonctions.php
@@ -0,0 +1,19 @@
+<?php
+// Fonctions de «Bonbon !» le cahier de texte pour Spip.
+// Réalisé par Bertrand MARNE (bmarne à ac-creteil.fr)
+// Sous licence GPL (enfin bon, c'est bonbonware...)
+// CopyLeft en octobre 2007
+
+//Cette fonction matches les id_articles qui sont dans le PS des séances
+//Puis les mets dans une châine (séparés par des virgules, en vue d'un
+//explode)
+function bonbon_matches_id_article ($a_matcher) {
+	preg_match_all ("/->(\d+?)\]/",$a_matcher,$matches);
+	$key = key($matches[1]);
+	$val = current($matches[1]);
+	while(list ($key, $val) = each ($matches[1])) {
+	$sortie .= $val.",";
+ 	};
+	return $sortie;
+}
+?>
