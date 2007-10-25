6296132af571655b2ca7116a6e8018301ee9138c
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-10-25 16:44:43 +0000

    Pouvoir voter quand même avec sous IE qui ne respecte pas les normes du W3C. Nécessite jquery.
    (Pour la doc du bug : http://forum.alsacreations.com/topic-2-13291-1-Button-typequotsubmitquot-et-Internet-Explorer.html. Pour sa résolution : http://www.kopz.org/public/documents/css/multiple_buttons_ie_workaround.html)
    
    Pour l'intérêt de <button> : http://particletree.com/features/rediscovering-the-button-element/
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@16342 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/notation/notation.js
+++ _dev_/notation/notation.js
@@ -29,3 +29,18 @@ function notation_set_etoile(n, nb, id)
 	  for(i=n+1; i<=nb; i++) document.images['star-'+id+'-'+i].src = notation_img+"-off.gif";
   }
 }
+
+function buttonfix() {
+    var buttons = document.getElementsByTagName('button');
+    for (var i=0; i<buttons.length; i++) {
+        if(buttons[i].onclick) continue;
+        
+        buttons[i].onclick = function () {
+            for(j=0; j<this.form.elements.length; j++)
+                if( this.form.elements[j].tagName == 'BUTTON' )
+                    this.form.elements[j].disabled = true;
+            this.disabled=false;
+            this.value = this.attributes.getNamedItem("value").nodeValue ;
+        }
+    }
+}
