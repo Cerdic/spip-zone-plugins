fc0742413d22228e72a4b32104f17f0496e15811
bmarne@gmail.com <bmarne@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-11-03 16:10:12 +0000

    Nouvelle fonction: pour éviter de surcharger le répertoire IMG, il est possible de réutiliser des documents liés à d'autres fiches. En partique, les id_document des docs copiés sont inscrits dans le surtitre de l'article destination.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@16546 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/bonbon/cahier-de-texte-fonctions.php
+++ _test_/bonbon/cahier-de-texte-fonctions.php
@@ -5,10 +5,25 @@
 // CopyLeft en octobre 2007
 
 //Cette fonction matches les id_articles qui sont dans le PS des séances
-//Puis les mets dans une châine (séparés par des virgules, en vue d'un
+//Puis les mets dans une chaîne (séparés par des virgules, en vue d'un
 //explode)
 function bonbon_matches_id_article ($a_matcher) {
 	preg_match_all ("/->(\d+?)\]/",$a_matcher,$matches);
+	$key = key($matches[1]);
+	$val = current($matches[1]);
+	$virgule="";
+	while(list ($key, $val) = each ($matches[1])) {
+	$sortie .= $virgule.$val;
+	if ($virgule=="") $virgule=",";
+ 	};
+	return $sortie;
+}
+
+//Cette fonction matches les id_documents qui sont dans le SOUSTITRE des séances
+//Puis les mets dans une chaîne (séparés par des virgules, en vue d'un
+//explode)
+function bonbon_matches_id_document ($a_matcher) {
+	preg_match_all ("/<doc(\d+?)>/",$a_matcher,$matches);
 	$key = key($matches[1]);
 	$val = current($matches[1]);
 	$virgule="";
