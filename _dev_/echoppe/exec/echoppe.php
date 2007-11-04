265992f44bf58e2b7beeb8aba909d2f4e252d573
crovvfoot@gmail.com <crovvfoot@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-11-04 15:12:33 +0000

    test spip 192-193
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@16579 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/echoppe/exec/echoppe.php
+++ _dev_/echoppe/exec/echoppe.php
@@ -3,23 +3,34 @@
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 function exec_echoppe(){
-		
 	
+	if ($GLOBALS['meta']['version_installee'] <= '1.927'){
+		echo debut_page(_T('echoppe:echoppe'), "redacteurs", "echoppe");	
+	}else{
+		echo inc_commencer_page_dist(_T('echoppe:echoppe'), "redacteurs", "echoppe");
+	}
 	
-	echo debut_page(_T('echoppe:echoppe'), "redacteurs", "echoppe");
 	echo debut_gauche();
+	
+	
 		echo debut_boite_info();
+			echo (_T('echoppe:descriptif_echoppe'));
 			/* A coder :
 	
 			1) Verif des droits
-			2) Y a t il des catégorie de créer ?
-				-> non : on propose d'en créer une. Obligatoire.
+			2) Y a t il des categorie de creer ?
+				-> non : on propose d'en creer une. Obligatoire.
 				-> oui : on liste les categorie "racine".
 			
 			*/
 		echo fin_boite_info();
+		
+		$raccourcis = icone_horizontale(_T('echoppe:nouvelle_categorie'), generer_url_ecrire("echoppe_categorie","new=oui"), _DIR_PLUGIN_ECHOPPE."images/categorie-24.png","creer.gif", false);
+		echo bloc_des_raccourcis($raccourcis);
+		
+		
 		/* A coder :
-		1) Affichage des boutons de création des outils
+		1) Affichage des boutons de creation des outils
 		*/
 	
 	echo creer_colonne_droite();
@@ -30,9 +41,9 @@ function exec_echoppe(){
 	
 	1) Verif des droits
 		-> OK : goto point 2
-		-> NoOK : affichage d'une boite "Désolé poulette, t'as pas les droits."
-	2) Y a t il des catégorie de créer ?
-		-> non : on propose d'en créer une. Obligatoire.
+		-> NoOK : affichage d'une boite "Desole poulette, t'as pas les droits."
+	2) Y a t il des categorie de creer ?
+		-> non : on propose d'en creer une. Obligatoire.
 		-> oui : on liste les categorie "racine".
 	
 	*/
