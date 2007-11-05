a6eef5ee8a8fb482dce2c62ef5b594a64f37e9d3
crovvfoot@gmail.com <crovvfoot@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-11-05 18:08:04 +0000

    creation de categorie
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@16614 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/echoppe/action/echoppe_sauver_categorie.php
+++ _dev_/echoppe/action/echoppe_sauver_categorie.php
@@ -3,13 +3,19 @@
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 function action_echoppe_sauver_categorie(){
-	if (_request('new') == 'new'){
+	if (_request('new') == 'oui'){
 		$sql_insert_categorie = "INSERT INTO spip_echoppe_categories VALUES ('','"._request('id_parent')."')";
 		$res_insert_categorie = spip_query($sql_insert_categorie);
+		echo $sql_insert_categorie.'<hr />';
 		$new_id_categorie = spip_insert_id();
-		$sql_insert_categorie_descriptif = "INSERT INTO spip_echoppe_categories_descriptifs VALUES () ";
+		$sql_insert_categorie_descriptif = "INSERT INTO spip_echoppe_categories_descriptifs VALUES ('','".$new_id_categorie."','".$lang."','".$titre."','".$descriptif."','".$texte."','".$logo."','','".$statut."') ";
+		$res_insert_categorie_descriptif = spip_query($sql_insert_categorie_descriptif);
+		echo $sql_insert_categorie_descriptif.'<hr />';
+		
 	}else{
-	
+		$sql_update_categorie_descriptif = "UPDATE spip_echoppe_categories_descriptifs SET titre = '".$titre."', descriptif = '".$descriptif."', texet = '".$texte."', statut = '".$statut."' WHERE id_categorie_descriptif = '"._request('id_categorie_descriptif')."' ";
+		echo $sql_update_categorie_descriptif;
+		$res_update_categorie_descriptif = spip_query($sql_insert_categorie_descriptif);
 	}
 
 }
