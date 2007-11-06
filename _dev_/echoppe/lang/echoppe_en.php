e8b858ba888d7ca9b5965c6c0547297860bdcac2
matt_he_oh@yahoo.fr <matt_he_oh@yahoo.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-11-06 17:23:59 +0000

    Ajout Ficher langue en Anglais
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@16639 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _dev_/echoppe/lang/echoppe_en.php
@@ -0,0 +1,55 @@
+<?php
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// formulaire inscription2
+'coord_livraison' => 'Delivery coordinates',
+'adresse_livraison' => 'Address (delivery)',
+
+// Formulaire profil : controleurs et vues
+'contacts_livraison' => 'Delivery contact',
+'vos_contacts_livraison' => 'Your delivery contacts',
+'votre_adresse_livraison' => 'Your delivery address',
+
+// A
+'acces_non_autorise'=>'We are sorry,<br />you\'re not allowed to access this section.',
+
+// C
+'categorie_numero' => 'Category number :',
+'creer_nouvelle_categorie' => 'Create a new category',
+
+// D
+'descriptif'=>'Description :',
+'descriptif_echoppe' => 'Echoppe is an administration plugin for boutique online . It tries to be as generalist as possible.',
+'derniere_modification' => 'Last modifications',
+
+// E
+'echoppe'=>'Echoppe, the plugin.',
+'editer_version'=>'Edit the version',
+
+// G
+'gerer_echoppe' => 'Administer Echoppe',
+
+// L
+'les_categories' => 'The categories',
+
+// N
+'nouveau_produit' => 'Create a product',
+'nouvelle_cetegorie' => 'New category',
+
+// P
+'par_defaut' => 'default (setting)',
+'pas_encore_cree' => 'Doesn\'t exist yet.',
+'pas_encore_modifie' => 'Never modified;',
+
+// T
+'texte'=>'Text :',
+'titre_categorie' => 'Category title :',
+
+// X
+'xxx' => 'xxx'
+
+);
+
+
+?>
