f50836f73a9445838a0e15c6fd7075e51eb3798f
fdm@ufe.be <fdm@ufe.be@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-11-07 08:59:56 +0000

    Corection bug insertion de cotisation
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@16664 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/Association/Association_1.9.2/exec/action_adherents.php
+++ _test_/Association/Association_1.9.2/exec/action_adherents.php
@@ -29,10 +29,69 @@
 		
 		//MODIFICATION ADHERENT
 		if ($action=="modifie") {
-			spip_query("UPDATE spip_asso_adherents SET categorie="._q($categorie).", id_asso="._q($id_asso).", utilisateur1="._q($utilisateur1).", utilisateur2="._q($utilisateur2).", utilisateur3="._q($utilisateur3).", utilisateur4="._q($utilisateur4).", validite="._q($validite)." WHERE id_auteur="._q($id_auteur) );
-			spip_query("UPDATE spip_auteurs_elargis SET statut_interne="._q($statut_interne)." WHERE id_auteur="._q($id_auteur) );
+			$query=spip_query("SELECT * FROM spip_asso_adherents WHERE id_auteur=$id_auteur");
+			if($query) {
+				spip_query("UPDATE spip_asso_adherents SET id_asso="._q($id_asso).", utilisateur1="._q($utilisateur1).", utilisateur2="._q($utilisateur2).", utilisateur3="._q($utilisateur3).", utilisateur4="._q($utilisateur4).", validite="._q($validite)." WHERE id_auteur="._q($id_auteur) );			
+			} else{
+				spip_query("INSERT INTO spip_asso_adherents (id_auteur, id_asso, utilisateur1, utilisateur2, utilisateur3, utilisateur4, validite) VALUES ("._q($id_auteur).", "._q($id_asso).", "._q($utilisateur1).", "._q($utilisateur2).", "._q($utilisateur3).", "._q($utilisateur4).", "._q($validite));
+			}
+			spip_query("UPDATE spip_auteurs_elargis SET categorie="._q($categorie).", statut_interne="._q($statut_interne)." WHERE id_auteur="._q($id_auteur) );
 			header ('location:'.$url_retour);
 			exit;
 		}
+		
+		//SUPPRESSION PROVISOIRE ADHERENT
+		if (isset($_POST['delete'])) {
+			$url_retour = $_SERVER['HTTP_REFERER'];
+			
+			$delete_tab=(isset($_POST["delete"])) ? $_POST["delete"]:array();
+			$count=count ($delete_tab);
+			
+			echo '<p>'. _T('asso:adherent_message_confirmer_suppression').' : <br>';
+			echo '<table>';
+			echo '<form action="'.$url_action_adherents.'"  method="post">';
+			for ( $i=0 ; $i < $count ; $i++ ) {
+				$id = $delete_tab[$i];
+				$query = spip_query( "SELECT * FROM spip_asso_adherents where id_adherent='$id' " );
+				while($data = spip_fetch_array($query)) {
+					echo '<tr>';
+					echo '<td><strong>'.$data['nom'].' '.$data['prenom'].'</strong>';
+					echo '<td>';
+					echo '<input type=checkbox name="drop[]" value="'.$id.'" checked>';
+				}
+			}
+			echo '<tr>';
+			echo '<td colspan="2"><input name="url_retour" type="hidden" value="'.$url_retour.'">';
+			echo '<input name="submit" type="submit" value="'._T('asso:adherent_bouton_confirmer').'" class="fondo"></td></tr>';
+			echo '<table>';
+			echo '</p>';
+			
+		}
+
+	//  SUPPRESSION DEFINITIVE ADHERENTS
+	//---------------------------- 
+	if (isset($_POST['drop'])) {
+
+		$url_retour=$_POST['url_retour'];
+
+		$drop_tab=(isset($_POST["drop"])) ? $_POST["drop"]:array();
+		$count=count ($drop_tab);
+
+		for ( $i=0 ; $i < $count ; $i++ ) {
+			$id = $drop_tab[$i];
+			spip_query("DELETE FROM spip_asso_adherents WHERE id_adherent='$id'");
+		}
+		echo '<p><strong>'._T('asso:adherent_message_suppression_faite').'</strong></p>';
+
+		echo '<p>';
+		icone(_T('asso:bouton_retour'), $url_retour, '../'._DIR_PLUGIN_ASSOCIATION.'/img_pack/actif.png','rien.gif' );
+		echo '</p>';
+	}
+
+	fin_boite_info();
+  
+	fin_cadre_relief();
+
+	fin_page();
 	} 
 ?>
--- _test_/Association/Association_1.9.2/exec/action_cotisations.php
+++ _test_/Association/Association_1.9.2/exec/action_cotisations.php
@@ -24,17 +24,17 @@
 		$montant= $_POST['montant'];
 		$categorie= $_POST['categorie'];
 		$justification =$_POST['justification'];
+		$imputation=lire_config('association/pc_cotisations');
 		$validite =$_POST['validite'];
 		$url_retour=$_POST['url_retour'];
-			
+		
 		if($action=="ajoute") {
-			spip_query( "INSERT INTO spip_asso_comptes (date, journal, recette, justification, imputation, id_journal) VALUES ("._q($date).", "._q($journal).", "._q($montant).", "._q($justification).", ".lire_config('association/pc_cotisations').", "._q($id_auteur)." )" );
+			spip_query( "INSERT INTO spip_asso_comptes (date, journal, recette, justification, imputation, id_journal) VALUES ("._q($date).", "._q($journal).", "._q($montant).", "._q($justification).", "._q($imputation).", "._q($id_adherent)." )" );
 			spip_query( "UPDATE spip_auteurs_elargis SET statut_interne='ok' WHERE id_auteur="._q($id_auteur) );
-			$query=spip_query("SELECT * FROM spip_asso_adherents WHERE id_auteur='$id_auteur' ");
-			if($query) {
-				spip_query( "UPDATE spip_asso_adherent SET statut_relance='ok', validite="._q($validite)." WHERE id_auteur="._q($id_auteur) );
-			} else {
-				spip_query( "INSERT INTO spip_asso_adherent (id_auteur, date, montant, categorie, statut_relance, validite) VALUES ("._q($id_auteur ", "._q($date) ", "._q($montant) ", "._q($categorie) ", 'ok', "._q($validite).".)");
+			$query=spip_query( "UPDATE spip_asso_adherents SET validite="._q($validite)." WHERE id_auteur="._q($id_auteur) );
+			if(!$query) {
+				spip_query( "INSERT INTO spip_asso_adherents (id_auteur, validite) VALUES ( "._q($id_auteur).", "._q($validite)." )");
+			}
 			header ('location:'.$url_retour);
 			exit;
 		}
