0b3c5d8e2a28ed42bfe75ea5c71fce91d4c11196
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-11-09 23:25:14 +0000

    booster devient expresso, et c'est une premiere version complete qui marche
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@16726 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/booster/expresso_install.php
+++ _dev_/booster/expresso_install.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Booster
+ * Expresso
  * Gestion d'un cache statique Apache
  *
  * Auteurs :
@@ -10,24 +10,27 @@
  */
 
 	include_spip('inc/meta');
-	function booster_upgrade($nom_meta_base_version,$version_cible){
+	function expresso_upgrade($nom_meta_base_version,$version_cible){
 		$current_version = 0.0;
 		if (   (!isset($GLOBALS['meta'][$nom_meta_base_version]) )
 				|| (($current_version = $GLOBALS['meta'][$nom_meta_base_version])!=$version_cible)){
 			if ($current_version==0.0){
-				if ($d = sous_repertoire(_DIR_CACHE,"apache"))
+				if ($d = sous_repertoire(_DIR_VAR,"apache"))
 				ecrire_meta($nom_meta_base_version,$current_version=$version_cible,'non');
 			}
 			ecrire_metas();
 		}
 	}
 	
-	function booster_vider_tables($nom_meta_base_version) {
-		effacer_meta('booster');
-		$cache = preg_files(_DIR_CACHE."apache/");
+	function expresso_vider_tables($nom_meta_base_version) {
+		effacer_meta('expresso');
+		$cache = preg_files(_DIR_VAR."apache/");
 		foreach($cache as $f)
 			@unlink($f);
-		@unlink(_DIR_CACHE."apache");
+		@unlink(_DIR_VAR."apache");
+		lire_fichier('.htaccess',$htaccess);
+		$htaccess = preg_replace(",###EXPRESSO###.*###/EXPRESSO###,ms","###EXPRESSO###",$htaccess);
+		ecrire_fichier('.htaccess',$htaccess);
 		effacer_meta($nom_meta_base_version);
 		ecrire_metas();
 	}
---
+++ _dev_/booster/expresso_pipeline.php
@@ -0,0 +1,107 @@
+<?php
+/*
+ * Expresso
+ * Gestion d'un cache statique Apache
+ *
+ * Auteurs :
+ * Cedric Morin, Yterium.com
+ * (c) 2007 - Distribue sous licence GNU/GPL
+ *
+ */
+
+// ration de cache, exprime en 60e de temps :
+// 0 : pas de cache apache
+// 60 : toutes les requetes passent par apache (ne jamais faire ca !)
+// il faut laisser passer une partie des requetes pour que spip mette les pages a jour
+@define('_EXPRESSO_CACHE_RATIO',50);
+
+function expresso_fenetre_temps($duree){
+}
+function expresso_genere_htaccess(){
+	/* debug*/
+	include_spip('inc/meta');
+	ecrire_meta('expresso',$GLOBALS['meta']['expresso']);
+	ecrire_metas();
+	
+	lire_fichier('.htaccess',$htaccess);
+	if (strpos($htaccess,'###EXPRESSO###')!==FALSE) {
+		$express = "";
+		$liste_pages = explode("\n",$GLOBALS['meta']['expresso']);
+		foreach($liste_pages as $rewrite) {
+			$rewrite = explode('!',$rewrite);
+			if (count($rewrite)==2) {
+				# une fraction de temps donnee basse sur la valeur des secondes
+				# on sert par apache
+				$url = parse_url($rewrite[0]);
+				$query = $url['query'];
+				$host = $url['host'];
+				$url = substr($rewrite[0],strlen($GLOBALS['meta']['adresse_site']));
+				if ($url{0}=='/') $url = substr($url,1);
+				if (($p=strpos($url,"?"))!==FALSE)
+					$url = substr($url,0,$p);
+				$start = rand(0,59);
+				$end = modulo($start+round(_EXPRESSO_CACHE_RATIO),60);
+				$start--;
+				$end++;
+				if ($start<$end)
+					$express .= "RewriteCond %{HTTP_HOST} ^$host$ [NC]
+RewriteCond %{QUERY_STRING} ^$query$ [NC]
+RewriteCond %{TIME_SEC} >$start
+RewriteCond %{TIME_SEC} <$end
+RewriteRule ^$url$ ".$rewrite[1]." [L]
+	
+";
+				else
+					$express .= "RewriteCond %{HTTP_HOST} ^$host$ [NC]
+RewriteCond %{QUERY_STRING} ^$query$ [NC]
+RewriteCond %{TIME_SEC} >$start
+RewriteRule ^$url$ ".$rewrite[1]." [L]
+RewriteCond %{HTTP_HOST} ^$host$ [NC]
+RewriteCond %{QUERY_STRING} ^$query$ [NC]
+RewriteCond %{TIME_SEC} <$end
+RewriteRule ^$url$ ".$rewrite[1]." [L]
+	
+";
+			}
+		}
+		$htaccess = preg_replace(",###EXPRESSO###.*###/EXPRESSO###,ms","###EXPRESSO###",$htaccess);
+		$htaccess = str_replace("###EXPRESSO###","###EXPRESSO###\n$express###/EXPRESSO###",$htaccess);
+		ecrire_fichier('.htaccess',$htaccess);
+	}
+}
+
+function expresso_nettoie($flux){
+	$flux =	str_replace("</body>","<span style='font-size:xx-small;'>expresso</span></body>",$flux);
+	$flux = preg_replace(",<div[^>]*spip-admin[^>]*>.*</div>,Uims","",$flux);
+	return $flux;
+}
+
+function expresso_affichage_final($flux) {
+	if (isset($GLOBALS['page']['entetes']['X-Expresso'])
+	&& ($url = self(true))
+	//&& (strpos($url,"?")===FALSE)
+	) {
+		$url = "http://".$_SERVER['HTTP_HOST']. $url;
+		$nom_cache = _DIR_VAR . "apache/".md5($url).".html";
+		if ( 
+			($GLOBALS['var_mode']=='calcul')
+			OR ($GLOBALS['var_mode']=='recalcul')
+			OR !($e=file_exists($nom_cache))
+			OR ( ($d=filemtime($nom_cache)) AND ($d+$GLOBALS['page']['entetes']['X-Spip-Cache']<time()))
+			) {
+			spip_log("$url : $nom_cache",'boost');
+			ecrire_fichier($nom_cache,expresso_nettoie($flux));
+			# expresso est une simple chaine pour eviter la deserialisation a chaque hit et preferer un strpos plus rapide
+			# url!nom_cache\n
+			if (strpos($GLOBALS['meta']['expresso'],"$url!")===FALSE) {
+				$GLOBALS['meta']['expresso'] .= "$url!$nom_cache\n";
+				expresso_genere_htaccess();
+			}
+			elseif($GLOBALS['var_mode']=='recalcul')
+				expresso_genere_htaccess();
+		}
+	}
+	return $flux;
+}
+
+?>
