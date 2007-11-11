3bdb5a15861a1f520640968e7f5409a606a56a18
nicolas1.krebs2@netcourrier.com <nicolas1.krebs2@netcourrier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-11-11 10:36:17 +0000

    Mises à jour de fichiers plugin.xml (adresse de documentation)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@16760 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/passe_complexe/lang/passecomplexe_en.php
+++ _test_/passe_complexe/lang/passecomplexe_en.php
@@ -2,19 +2,19 @@
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-									   'tres_faible' => 'Very Weak',
-									   'faible' => 'Weak',
-									   'moyen' => 'Medium',
-									   'fort' => 'Strong',
-									   'tres_fort' => 'Very Strong',
-									   'nb_mini' => 'Minimum number of characters : ',
-									   'simple' => 'Password Too Simple!',
-									   'court' => 'Too Short',
-									   'common' => 'pass,word,password,sex,god,liverpool,letmein,qwerty,monkey',
-									   'cfg_common' => 'Common words to avoid in passwords. <small>(Separated by commas)</small>:',
-									   'cfg_titre' => 'Password Strength Tester',
-									   'cfg_descriptif' => 'This plugin tests the &quot;strength&quot; of the passwords chosen by the authors. <br/> The test is based on multiple criterion like the length of the password, if it contains special characters, etc. <br/> You can also provide a list of words that shouldn\'t be used in the passwords.',
-									   'cfg_longueur' => 'Password minimum length : '
+'tres_faible' => 'Very Weak',
+'faible' => 'Weak',
+'moyen' => 'Medium',
+'fort' => 'Strong',
+'tres_fort' => 'Very Strong',
+'nb_mini' => 'Minimum number of characters : ',
+'simple' => 'Password Too Simple!',
+'court' => 'Too Short',
+'common' => 'pass,word,password,sex,god,liverpool,letmein,qwerty,monkey',
+'cfg_common' => 'Common words to avoid in passwords. <small>(Separated by commas)</small>:',
+'cfg_titre' => 'Password Strength Tester',
+'cfg_descriptif' => 'This plugin tests the &quot;strength&quot; of the passwords chosen by the authors. <br/> The test is based on multiple criterion like the length of the password, if it contains special characters, etc. <br/> You can also provide a list of words that shouldn\'t be used in the passwords.',
+'cfg_longueur' => 'Password minimum length : '
 
 );
 
--- _test_/passe_complexe/lang/passecomplexe_nl.php
+++ _test_/passe_complexe/lang/passecomplexe_nl.php
@@ -2,24 +2,19 @@
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-                                                                           'tres_faible' => 'Zeer zwak',
-                                                                           'faible' => 'Zwak',
-                                                                           'moyen' => 'Middelmatig',
-                                                                           'fort' => 'Sterk',
-                                                                           'tres_fort' => 'Zeer sterk',
-                                                                           'nb_mini' => 'Aantal tekennen (min.) : ',
-                                                                           'simple' => 'Passwoord te eenvoudig !',
-                                                                           'court' => 'Te kort',
-                                                                           'common' => 'passwoord,azerty,qwertzui',
-                                                                           'cfg_common' => 'Lijst van verboden soortnamen in de  
-passwoorden. <small>(Gedeeld met kommas)</small> :',
-                                                                           'cfg_titre' => 'Passwoord tester',
-                                                                           'cfg_descriptif' => 'De paswword tester plugin controleert  
-de sterkte van een passwoord.<br/>De tests worden volgens  
-verschillende criterias uitgevooerd: lengte van de woord,  
-uitzonderlijke tekenen, enz. <br/>Je kan ook een lijst van verboden  
-woorden maken.',
-                                                                           'cfg_longueur' => 'Minimale lengte van de passwoord : '
+'tres_faible' => 'Zeer zwak',
+'faible' => 'Zwak',
+'moyen' => 'Middelmatig',
+'fort' => 'Sterk',
+'tres_fort' => 'Zeer sterk',
+'nb_mini' => 'Aantal tekennen (min.) : ',
+'simple' => 'Passwoord te eenvoudig !',
+'court' => 'Te kort',
+'common' => 'passwoord,azerty,qwertzui',
+'cfg_common' => 'Lijst van verboden soortnamen in de passwoorden. <small>(Gedeeld met kommas)</small> :',
+'cfg_titre' => 'Passwoord tester',
+'cfg_descriptif' => 'De paswword tester plugin controleert de sterkte van een passwoord.<br/>De tests worden volgens verschillende criterias uitgevooerd: lengte van de woord, uitzonderlijke tekenen, enz. <br/>Je kan ook een lijst van verboden woorden maken.',
+'cfg_longueur' => 'Minimale lengte van de passwoord : '
 
 
 );
