383ce58f711bb9509c5cfb800406f7fe43c749f4
crovvfoot@gmail.com <crovvfoot@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-11-13 14:28:59 +0000

    Première version ou on peut créer des produits. Mais on les vois pas encore dans les categories.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@16817 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/echoppe/action/echoppe_sauver_produit.php
+++ _dev_/echoppe/action/echoppe_sauver_produit.php
@@ -9,35 +9,54 @@ function action_echoppe_sauver_produit(){
 	$texte = _request('texte');
 	$ps = _request('ps');
 	$id_categorie = _request('id_categorie');
+	$id_produit = _request('id_produit');
 	$new = _request('new');
 	$ref_produit = _request('ref_produit');
+	$quantite_mini = _request('quantite_mini');
+	$tva = _request('tva');
 	
-	//echo _request('new');
+	
+	//echo $new.'<---';
 	
 	switch ($new){
 		case 'oui':
 			$sql_insert_produit = "INSERT INTO spip_echoppe_produits VALUES ('','".$date_debut."', '".$date_fin."', '".$poids."', '".$hauteur."', '".$largeur."', '".$longeur."', '".$colisage."', '".$ref_produit."', '".$prix_base_htva."', '', '".$statut."');";
-			$res_insert_produit = spip_query($sql_insert_categorie);
-			//echo $sql_insert_categorie.'<hr />';
+			$res_insert_produit = spip_query($sql_insert_produit);
+			//echo $sql_insert_produit.'<hr />';
+			
+			$id_produit = $new_id_produit;
+			
+			$sql_lien_produit_categorie = "INSERT INTO spip_echoppe_categories_produits VALUES ('".$id_categorie."', '".$id_produit."');";
+			$res_lien_produit_categorie = spip_query($sql_lien_produit_categorie);
+			
 			$new_id_produit = spip_insert_id();
 			$sql_insert_produit_descriptif = "INSERT INTO spip_echoppe_produits_descriptions VALUES ('','".$new_id_produit."','".$lang_produit."','".addslashes($titre)."','".addslashes($descriptif)."','".addslashes($texte)."','".addslashes($ps)."','".$tva."','".$quantite_mini."','') ";
-			$res_insert_produit_descriptif = spip_query($sql_insert_categorie_descriptif);
-			$id_produit = $new_id_produit;
+			$res_insert_produit_descriptif = spip_query($sql_insert_produit_descriptif);
+			
+			//echo $sql_insert_produit_descriptif.'<hr />';
+			
 			break;
 		
-		case 'description' :
-			$sql_insert_categorie_descriptif = "INSERT INTO spip_echoppe_categories_descriptions VALUES ('','".$id_categorie."','".$lang_categorie."','".addslashes($titre)."','".addslashes($descriptif)."','".addslashes($texte)."','".$logo."','".$lang_categorie."','".$statut."') ";
-			$res_insert_categorie_descriptif = spip_query($sql_insert_categorie_descriptif);
+		case 'ajout_description' :
+			$sql_insert_produit_descriptif = "INSERT INTO spip_echoppe_produits_descriptions VALUES ('','".$id_produit."','".$lang_produit."','".addslashes($titre)."','".addslashes($descriptif)."','".addslashes($texte)."','".addslashes($ps)."','".$tva."','".$quantite_mini."','') ";
+			//echo $sql_insert_produit_descriptif;
+			$res_insert_produit_descriptif = spip_query($sql_insert_produit_descriptif);
+			break;
+		
+		case 'maj_description' :
+			$sql_update_produit_descriptif = "UPDATE spip_echoppe_produits_descriptions SET titre = '".addslashes($titre)."', descriptif = '".addslashes($descriptif)."', texte = '".addslashes($texte)."',ps = '".addslashes($ps)."', tva = '".$tva."', quantite_mini = '".$quantite_mini."' WHERE id_produit = '".$id_produit."' AND lang = '".$lang_produit."' ";
+			//echo $sql_update_produit_descriptif;
+			$res_update_produit_descriptif = spip_query($sql_update_produit_descriptif);
 			break;
 		
 		default :
-			$sql_update_categorie_descriptif = "UPDATE spip_echoppe_categories_descriptions SET titre = '".addslashes($titre)."', descriptif = '".addslashes($descriptif)."', texte = '".addslashes($texte)."', statut = '".$statut."' WHERE id_categorie = '".$id_categorie."' AND lang = '".$lang_categorie."' ";
-			//echo $sql_update_categorie_descriptif;
-			$res_update_categorie_descriptif = spip_query($sql_update_categorie_descriptif);
+			$sql_update_produit_descriptif = "UPDATE spip_echoppe_produits_descriptions SET titre = '".addslashes($titre)."', descriptif = '".addslashes($descriptif)."', texte = '".addslashes($texte)."', statut = '".$statut."' WHERE id_produit = '".$id_produit."' AND lang = '".$lang_produit."' ";
+			//echo $sql_update_produit_descriptif;
+			$res_update_produit_descriptif = spip_query($sql_update_produit_descriptif);
 			break;
 		
 	}
-	$redirect = generer_url_ecrire('echoppe_produit', 'id_produit='.$id_produit,'&');
+	$redirect = generer_url_ecrire('echoppe', 'id_produit='.$id_produit,'&');
 	//echo $redirect;
 	redirige_par_entete($redirect);
 }
--- _dev_/echoppe/exec/echoppe_edit_produit.php
+++ _dev_/echoppe/exec/echoppe_edit_produit.php
@@ -9,19 +9,32 @@ include_spip('public/assembler');
 
 function exec_echoppe_edit_produit(){
 	
-	$id_produit = _request('id_produit');
-	$id_categorie = _request('id_categorie');
-	$lang_produit = _request('lang_produit');
-	$new = _request('new');
-
-	$sql_le_produit = "SELECT * FROM spip_echoppe_produits WHERE id_produit = '".$id_produit."';";
+	$contexte = array();
+	$contexte['id_produit'] = _request('id_produit');
+	$contexte['id_categorie'] = _request('id_categorie');
+	$contexte['lang_produit'] = _request('lang_produit');
+	$contexte['new'] = _request('new');
+	
+	
+	
+	$sql_le_produit = "SELECT * FROM spip_echoppe_produits WHERE id_produit = '".$contexte['id_produit']."';";
 	$res_le_produit = spip_query($sql_le_produit);
-	if (spip_num_rows($res_le_produit) == 0 && $new != "oui"){
+	if (spip_num_rows($res_le_produit) == 0 && $contexte['new'] != "oui"){
 		die(inc_commencer_page_dist(_T('echoppe:les_produits'), "redacteurs", "echoppe")._T('echoppe:pas_de_produit_ici').fin_page());
 	}
 	
-	(spip_num_rows($res_descriptif_produit) > 0)?$new = $new:$new = 'description';
+	$le_produit = spip_fetch_array($res_le_produit);
 	
+	
+	$sql_description_produit = "SELECT * FROM spip_echoppe_produits_descriptions WHERE id_produit = '".$id_produit."' AND lang = '".$lang_produit."';";
+	$res_description_produit = spip_query($sql_description_produit);
+	$description_produit = spip_fetch_array($res_description_produit);
+	//echo $contexte['new'];
+	
+	(spip_num_rows($res_description_produit) == 0 && $contexte['new'] != 'oui')?$contexte['new'] = 'ajout_description':$contexte['new'] = $contexte['new'];
+	(spip_num_rows($res_description_produit) > 0 && $contexte['new'] != 'oui')?$contexte['new'] = 'maj_descriptif':$contexte['new'] = $contexte['new'];
+	
+	//echo $contexte['new'];
 	
 	if ($GLOBALS['meta']['version_installee'] <= '1.927'){
 		echo debut_page(_T('echoppe:les_produits'), "redacteurs", "echoppe");	
@@ -35,9 +48,12 @@ function exec_echoppe_edit_produit(){
 	echo debut_droite(_T('echoppe:edition_de_produit'));
 	echo gros_titre(_T("echoppe:edition_de_produit"));
 	
-
+	(is_array($le_produit))?$contexte = array_merge($contexte, $le_produit):$contexte = $contexte;
+	(is_array($description_produit))?array_merge($contexte,$description_produit):$contexte = $contexte;
 	
-	echo recuperer_fond('fonds/echoppe_edit_produit');
+	$contexte['action'] = 'echoppe_sauver_produit';
+	
+	echo recuperer_fond('fonds/echoppe_edit_produit', $contexte);
 
 
 	
