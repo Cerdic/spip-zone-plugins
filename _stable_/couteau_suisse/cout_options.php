e8f2ec0b3cfc71d7b82578b5b9d5f8d435c322d4
patfr@ifrance.com <patfr@ifrance.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-11-16 02:29:30 +0000

    Compatibilité avec le plugin article_pdf : mode impression obligé pour toutes les lames Du coup et par exemple, un article découpé apparait en entier.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@16870 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/couteau_suisse/cout_options.php
+++ _stable_/couteau_suisse/cout_options.php
@@ -38,7 +38,7 @@ if(!isset($_GET['page']) OR !preg_match(',\.(css|js)$,', $_GET['page'])) {
 	define('_DIR_CS_TMP', sous_repertoire(_DIR_TMP, "couteau-suisse"));
 	define('_DIR_RSS_TMP', _DIR_TMP . 'rss_couteau_suisse.html');
 	// alias pour passer en mode impression
-	if(isset($_GET['page']) && in_array($_GET['page'], array('print', 'imprimer', 'imprimir_articulo', 'imprimir_breve')))
+	if(isset($_GET['page']) && in_array($_GET['page'], array('print', 'imprimer', 'imprimir_articulo', 'imprimir_breve', 'article_pdf')))
 		$_GET['cs']='print';
 	
 	// fonctions indispensables a l'execution
