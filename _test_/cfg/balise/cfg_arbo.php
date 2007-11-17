1b085f4bc1f92250bfd7ab00ced19466c610587a
marcimat@free.fr <marcimat@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-11-17 20:16:29 +0000

    Faire fonctionner #CFG_ARBO dans la partie publique ET privée j'ai dit !
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@16906 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/cfg/balise/cfg_arbo.php
+++ _test_/cfg/balise/cfg_arbo.php
@@ -24,47 +24,45 @@ function balise_CFG_ARBO($p) {
 	if (!$arg = interprete_argument_balise(1,$p)) {
 		$arg = "''";
 	}
+	//$p->interdire_scripts = false;
 	$p->code = 'affiche_arborescence(' . $arg . ')';
 	return $p;
 }
 
 function affiche_arborescence($cfg='') {
-	static $present = false;
+
 	$sortie = '';
+	$hash = substr(md5(rand()*rand()),0,6);
 	
 	// integration du css
-	if (!$present)
-		$sortie .= "<style type='text/css'>\n"
-				.  ".cfg_arbo{}\n"
-				.  ".cfg_arbo h5{padding:0.2em 0.2em; margin:0.2em 0;}\n"
-				.  ".cfg_arbo ul{border:1px solid #ccc; margin:0; padding:0.2em 0.5em; list-style-type:none;}\n"
-				.  "</style>\n";
+	$sortie .= "<style type='text/css'>\n"
+			.  ".cfg_arbo{}\n"
+			.  ".cfg_arbo h5{padding:0.2em 0.2em; margin:0.2em 0;}\n"
+			.  ".cfg_arbo ul{border:1px solid #ccc; margin:0; padding:0.2em 0.5em; list-style-type:none;}\n"
+			.  "</style>\n";
 	// integration du js	
-	if (!$present)
-		$sortie .= "<script type='text/javascript'>
-					$(document).ready(function(){
-						jQuery('.cfg_arbo ul').hide();
-						jQuery('.cfg_arbo h5')
-						.prepend('<b>[+] </b>')
-						.toggle(
-						  function () {
-							$(this).children('b').text('[-] ');
-							$(this).next('ul').show();
-						  },
-						  function () {
-							$(this).children('b').text('[+] ');
-							$(this).next('ul').hide();
-						  })
-					});
-					</script>\n";
-
-	$present = true;	
+	$sortie .= "<script type='text/javascript'>
+				$(document).ready(function(){
+					jQuery('.cfg_arbo_$hash ul').hide();
+					jQuery('.cfg_arbo_$hash h5')
+					.prepend('<b>[+] </b>')
+					.toggle(
+					  function () {
+						$(this).children('b').text('[-] ');
+						$(this).next('ul').show();
+					  },
+					  function () {
+						$(this).children('b').text('[+] ');
+						$(this).next('ul').hide();
+					  })
+				});
+				</script>\n";	
 	
 	$tableau = lire_config($cfg);
 	if (empty($cfg)) $cfg = 'spip_meta';
 	// parcours des donnees
 	$sortie .= 
-		"<div class='cfg_arbo'>\n" .
+		"<div class='cfg_arbo cfg_arbo_$hash'>\n" .
 		affiche_sous_arborescence($cfg, $tableau) .
 		"\n</div>\n";
 
