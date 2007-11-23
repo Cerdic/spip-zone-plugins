e3f2a093af3d372b12053c40cab7dc8fd3f7c2e9
marcimat@free.fr <marcimat@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-11-23 12:09:56 +0000

    Partage de code : utilisation de la fonction calculer_balise_formulaire_cfg() dans le pipeline affiche_milieu.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@17007 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/cfg_exemples/cfg_exemples_pipelines.php
+++ _test_/cfg_exemples/cfg_exemples_pipelines.php
@@ -5,27 +5,16 @@ function cfg_exemples_affiche_milieu($flux) {
 	if ($exec=='articles'){
 
 		// compiler #FORMULAIRE_CFG{...}
-		$cfg_vue		= 'exemple_prive_article';
-		$cfg_id			= $flux['args']['id_article'];
-		//$cfg_form		= 'formulaires/formulaire_cfg_vue';
-		$cfg_form		= 'formulaires/cfg_exemple_prive_article';
-		$cfg_form_ajax 	= 'oui';
-		$cfg_afficher_messages 	= 'non';
-		$cfg_hash = substr(md5($cfg_vue . $cfg_id . $cfg_form . $cfg_form_ajax),0,6);
-
-	
-		include_spip('public/assembler');
-		$contexte = array(
-				'cfg_nom' => $cfg_vue,
-				'cfg_vue' => $cfg_vue,
-				'cfg_hash' => $cfg_hash,
-				'cfg_form' => $cfg_form,
-				'cfg_form_ajax' => $cfg_form_ajax,
-				'cfg_id' => $cfg_id,
-				'cfg_afficher_messages' => $cfg_afficher_messages
-		);
-		$form = recuperer_fond('formulaires/formulaire_cfg', $contexte);
-		$flux['data'] .= $form;
+		include_spip('balises/formulaire_cfg');
+		$flux['data'] .= 
+			calculer_balise_formulaire_cfg(
+				$cfg_vue		= 'exemple_prive_article',
+				$cfg_id			= $flux['args']['id_article'],
+				//$cfg_form		= 'formulaires/formulaire_cfg_vue',
+				$cfg_form		= 'formulaires/cfg_exemple_prive_article',
+				$cfg_form_ajax 	= 'oui',
+				$cfg_afficher_messages 	= 'non'	
+			);
 	}
 	
 	return $flux;
