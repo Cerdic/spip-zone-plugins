dded18ba398630211fff42f422a16158c49e13ce
george@diwanalarab.com <george@diwanalarab.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-11-23 15:03:31 +0000

    Mise a jour de l'angalis
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@17012 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/acces_restreint/lang/accesrestreint_en.php
+++ _stable_/acces_restreint/lang/accesrestreint_en.php
@@ -3,25 +3,36 @@
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-'icone_menu_config' => 'restricted area',
-'page_zones_acces' => 'restricted area',
-'info_page' => "Set up page",
+'icone_menu_config' => 'restricted access',
+'page_zones_acces' => 'restricted acess',
+'info_page' => "This page allows you to create restricted areas in your site",
 'titre_zones_acces' => 'restricted area',
-'info_retirer_zone' => 'Move out',
+'info_retirer_zone' => 'Move out of area',
 'info_retirer_zones' => 'move out of all areas',
-'selectionner_une_zone' => 'Choose a restricted area',
+'info_ajouter_zones' => 'Add all areas',
+'selectionner_une_zone' => 'Select a restricted area',
 'titre_ajouter_zone' => 'add to a restricted area',
 'icone_creer_zone' => 'Create new restricted area',
 'colonne_id' => '#',
 'titre' => 'Title',
 'descriptif' => 'Description',
-'titre_table' => "Restricted areas",
-'bouton_creer_la_zone' => 'Create',
+'titre_table' => "All restricted areas",
+'bouton_creer_la_zone' => 'Create new area',
 'rubriques_zones_acces' => 'Restricted area\'s sections',
-'rubriques' => 'Sections',
-'auteurs' => 'Authors',
-'icone_supprimer_zone' => 'Delete',
-'ajouter_droits_auteur' => "Give me instant acces to this area"
+'rubriques' => 'sections',
+'auteurs' => 'authors',
+'icone_supprimer_zone' => 'Delete this area',
+'ajouter_droits_auteur' => "Give me instant acces to this area",
+
+'info_auteurs_lies_zone' => 'Authors with access to this area',
+'publique' => 'Public',
+'privee' => 'Private',
+'voir_toutes' => 'Show all areas',
+'creer_zone' => 'Create a new area',
+'zone_numero' => 'ZONE NUMBER:',
+'zone_restreinte_publique' => "Restrict access to this area in the public site",
+'zone_restreinte_espace_prive' => "Restrict access to this area in the private area"
+
 );
 
 ?>
