77a4f88013359cd72fac694cca8c66d8f25a1168
marcimat@free.fr <marcimat@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-11-23 12:09:09 +0000

    La balise Formulaire CFG n'a pas besoin d'être dynamique. (Un jour je comprendrais à quoi ça sert, un jour...)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@17006 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/cfg/balise/formulaire_cfg.php
+++ _test_/cfg/balise/formulaire_cfg.php
@@ -12,40 +12,41 @@ if (!defined("_ECRIRE_INC_VERSION")) return;	#securite
 
 
 function balise_FORMULAIRE_CFG ($p) {
-	return calculer_balise_dynamique($p, 'FORMULAIRE_CFG', array());
+	//return calculer_balise_dynamique($p, 'FORMULAIRE_CFG', array());
+	$cfg_vue 				= interprete_argument_balise(1, $p);
+	$cfg_id 				= sinon(interprete_argument_balise(2, $p),"''");
+	$cfg_form 				= sinon(interprete_argument_balise(3, $p),"'formulaires/formulaire_cfg_vue'");
+	$cfg_form_ajax 			= sinon(interprete_argument_balise(4, $p),"'oui'");
+	$cfg_afficher_messages 	= sinon(interprete_argument_balise(5, $p),"'oui'");
+	$p->code = "calculer_balise_formulaire_cfg($cfg_vue, $cfg_id, $cfg_form, $cfg_form_ajax, $cfg_afficher_messages)";
+	return $p;
 }
 
+function calculer_balise_formulaire_cfg(
+			$cfg_vue, 
+			$cfg_id = '', 
+			$cfg_form = 'formulaires/formulaire_cfg_vue', 
+			$cfg_form_ajax = 'oui', 
+			$cfg_afficher_messages = 'oui')
+{
 
-// Dans $args on recupere un array des valeurs collectees par balise_FORMULAIRE_CFG
-function balise_FORMULAIRE_CFG_stat($args, $filtres) {
-	return array($args[0], $args[1], $args[2], $args[3]);
-}
-
-
-//
-function balise_FORMULAIRE_CFG_dyn($cfg_vue, $cfg_id, $cfg_form, $cfg_form_ajax) {   
-	if (empty($cfg_form)) 
-		$cfg_form = 'formulaires/formulaire_cfg_vue';
-	if (empty($cfg_form_ajax)) 
-		$cfg_form_ajax = 'oui';
-		
+	if (empty($cfg_form)) 				$cfg_form = 'formulaires/formulaire_cfg_vue';
+	if (empty($cfg_form_ajax)) 			$cfg_form_ajax = 'oui';
+	if (empty($cfg_afficher_messages)) 	$cfg_afficher_messages = 'oui';
+				
 	$cfg_hash = substr(md5($cfg_vue . $cfg_id . $cfg_form . $cfg_form_ajax),0,6);
-	return
-		array(
-			// squelette
-			'formulaires/formulaire_cfg',
-			// delai
-			3600,
-			// contexte
-			array(
-				'cfg_nom' => $cfg_vue,
-				'cfg_vue' => $cfg_vue,
-				'cfg_hash' => $cfg_hash,
-				'cfg_form' => $cfg_form,
-				'cfg_form_ajax' => $cfg_form_ajax,
-				'cfg_id' => $cfg_id
-			)
-		);
+	
+	include_spip('public/assembler');
+	$contexte = array(
+			'cfg_nom' => $cfg_vue,
+			'cfg_vue' => $cfg_vue,
+			'cfg_hash' => $cfg_hash,
+			'cfg_form' => $cfg_form,
+			'cfg_form_ajax' => $cfg_form_ajax,
+			'cfg_id' => $cfg_id,
+			'cfg_afficher_messages' => $cfg_afficher_messages
+	);
+	return recuperer_fond('formulaires/formulaire_cfg', $contexte);	
 }
 
 ?>
--- _test_/cfg/cfg_options.php
+++ _test_/cfg/cfg_options.php
@@ -193,6 +193,7 @@ function cfg_charger_classe($nom, $dossier='inc', $continue=false) {
 }
 
 // Inclure les balises sinon SPIP ne voit pas les fonctions calculer_x()... meuh !
+include_spip('balise/formulaire_cfg');
 include_spip('balise/cfg_vue');
 include_spip('balise/cfg_arbo');
 include_spip('balise/cfg_traiter');
