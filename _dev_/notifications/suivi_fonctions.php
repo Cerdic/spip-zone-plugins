166bd188f773bd1cf8c137cc75c56428ce29ac10
fil@rezo.net <fil@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-11-26 09:01:02 +0000

    jeter le cookie_oubli apres usage ; permettre de se loger plutot que jeter les gens par un acces interdit
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@17069 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/notifications/suivi_fonctions.php
+++ _dev_/notifications/suivi_fonctions.php
@@ -1,13 +1,13 @@
 <?php
 
 // Si l'on se connecte sur cette page avec un parametre p=..., il faut valider
-// l'auteur, le connecter et activer sa session.
+// l'auteur, le connecter et activer sa session. (Pourrait aller dans le core)
 if (_request('p') !== null) {
 	$s = spip_query('SELECT * FROM spip_auteurs WHERE cookie_oubli='.sql_quote(_request('p')));
 	if (!$t = sql_fetch($s)
 	OR !$t['id_auteur']) {
-		include_spip('inc/minipres');
-		die(minipres());
+		include_spip('inc/headers');
+		redirige_par_entete(parametre_url(self(), 'p', '', '&'));
 	}
 
 	// $t est l'auteur souhaite : le valider si besoin
@@ -17,6 +17,9 @@ if (_request('p') !== null) {
 		$t['statut'] = acces_statut($t['id_auteur'], $t['statut'], $t['bio']);
 		sql_update('spip_forum', array('id_auteur', $t['id_auteur']), 'email_auteur='.sql_quote($t['email']));
 	}
+
+	// Invalider son cookie_oubli (pour eviter un vol de compte a partir de logs)
+	sql_update('spip_auteurs', array('cookie_oubli', ''), 'id_auteur='.$t['id_auteur']);
 
 	// Si ce n'est pas l'auteur connecte, se connecter sous son nom
 	if (!isset($GLOBALS['visiteur_session']['id_auteur'])
