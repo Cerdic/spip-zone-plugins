e1730ad19ea14cc8aba24c7cf18a9f6e4aa6c88c
fil@rezo.net <fil@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-11-26 00:35:59 +0000

    la timeline en include (bcp plus rapide), et reaffecter les forums signes mais non authentifies a l'auteur authentifie
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@17067 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/notifications/suivi_fonctions.php
+++ _dev_/notifications/suivi_fonctions.php
@@ -30,4 +30,17 @@ if (_request('p') !== null) {
 	}
 }
 
+if (!$GLOBALS['visiteur_session']['id_auteur']) {
+	include_spip('inc/headers');
+	redirige_par_entete(parametre_url(generer_url_public('login'), 'url', self(), '&'));
+} else {
+	// Si l'auteur a un email valide, on lui reaffecte tous les forums
+	// signes de son email
+	if (strlen($GLOBALS['visiteur_session']['email'])) {
+		include_spip('base/abstract_sql');
+		sql_update('spip_forum', array('id_auteur' => $GLOBALS['visiteur_session']['id_auteur']), 'id_auteur=0 AND email_auteur='.sql_quote($GLOBALS['visiteur_session']['email']));
+	}
+
+}
+
 ?>
