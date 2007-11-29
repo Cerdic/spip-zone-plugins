0d9a36e7aeed8bb512645d7838a98eb55886f47e
marcimat@free.fr <marcimat@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-11-29 20:23:54 +0000

    Il faut Et la balise dynamique et le cache 0 pour que tout aille correctement... c'est complexe !
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@17130 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/cfg/balise/cfg_vue.php
+++ _test_/cfg/balise/cfg_vue.php
@@ -13,19 +13,20 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 function balise_CFG_VUE($p){
 	$vue = 			sinon(interprete_argument_balise(1,$p), "''"); // indispensable neanmmoins
 	$id = 			sinon(interprete_argument_balise(2,$p), "''");
+	$messages = 	sinon(interprete_argument_balise(3,$p), "'oui'");
 
-	$p->code = "calculer_CFG_VUE($vue, $id)";
+	$p->code = "calculer_CFG_VUE($vue, $id, $messages)";
 	return $p;
 }
 
-function calculer_CFG_VUE($fond, $id, $afficher_messages = true){
+function calculer_CFG_VUE($fond, $id, $afficher_messages = 'oui'){
 	include_spip('inc/cfg');
 	$cfg = cfg_charger_classe('cfg');
 	$config = & new $cfg($fond, $fond, $id); 
 		
 	if ($config->autoriser()){
 		return 
-			(($afficher_messages) ? "<div class='cfg_message'>" . $config->message . "</div>" : "")
+			(($afficher_messages != 'non') ? "<div class='cfg_message'>" . $config->message . "</div>" : "")
 			. $config->formulaire();	
 	} else
 		return $config->refus;
--- _test_/cfg/balise/formulaire_cfg.php
+++ _test_/cfg/balise/formulaire_cfg.php
@@ -10,7 +10,48 @@
 if (!defined("_ECRIRE_INC_VERSION")) return;	#securite
 
 
+function balise_FORMULAIRE_CFG ($p) {
+	return calculer_balise_dynamique($p, 'FORMULAIRE_CFG', array());
+}
 
+
+// Dans $args on recupere un array des valeurs collectees par balise_FORMULAIRE_CFG
+function balise_FORMULAIRE_CFG_stat($args, $filtres) {
+	return array($args[0], $args[1], $args[2], $args[3], $args[4]);
+}
+
+
+//
+function balise_FORMULAIRE_CFG_dyn($cfg_vue, $cfg_id, $cfg_form, $cfg_form_ajax, $cfg_afficher_messages) {   
+	if (empty($cfg_form)) 
+		$cfg_form = 'formulaires/formulaire_cfg_vue';
+	if (empty($cfg_form_ajax)) 
+		$cfg_form_ajax = 'oui';
+		
+	$cfg_hash = substr(md5($cfg_vue . $cfg_id . $cfg_form . $cfg_form_ajax . $cfg_afficher_messages),0,6);
+	return
+		array(
+			// squelette
+			'formulaires/formulaire_cfg',
+			// delai
+			3600,
+			// contexte
+			array(
+				'cfg_nom' => $cfg_vue,
+				'cfg_vue' => $cfg_vue,
+				'cfg_hash' => $cfg_hash,
+				'cfg_form' => $cfg_form,
+				'cfg_form_ajax' => $cfg_form_ajax,
+				'cfg_id' => $cfg_id,
+				'cfg_afficher_messages' => $cfg_afficher_messages
+			)
+		);
+}
+
+
+
+
+/*
 function balise_FORMULAIRE_CFG ($p) {
 	$cfg_vue 				= interprete_argument_balise(1, $p);
 	$cfg_id 				= sinon(interprete_argument_balise(2, $p),"''");
@@ -20,7 +61,10 @@ function balise_FORMULAIRE_CFG ($p) {
 	$p->code = "calculer_balise_formulaire_cfg($cfg_vue, $cfg_id, $cfg_form, $cfg_form_ajax, $cfg_afficher_messages)";
 	return $p;
 }
+*/
 
+
+/* facilites pour les pipelines */
 function calculer_balise_formulaire_cfg(
 			$cfg_vue, 
 			$cfg_id = '', 
