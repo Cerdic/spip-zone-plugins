9ad32045d82fb0b8b06f9e50e7d4ff4c37ba10ed
booz@rezo.net <booz@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-12-01 17:15:06 +0000

    de quoi tagguer des documents en crayons
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@17193 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/tag-machine/lang/tagmachine_fr.php
+++ _dev_/tag-machine/lang/tagmachine_fr.php
@@ -3,7 +3,9 @@
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 'etiquettes' => '&Eacute;tiquettes',
-'etiquettes_populaires' => '&Eacute;tiquettes populaires'
+'etiquettes_populaires' => '&Eacute;tiquettes populaires',
+'tags_populaires' => 'Tags populaires'
+
 
 );
 
