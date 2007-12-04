da93c3b3de5ec27fd6213e004164c56059c35a5d
pierre.andrews@gmail.com <pierre.andrews@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-12-04 18:49:21 +0000

    traduction English
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@17237 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _stable_/cfg/lang/cfg_en.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ * Plugin CFG pour SPIP
+ * (c) toggg 2007, distribue sous licence GNU/GPL
+ * Documentation et contact: http://www.spip-contrib.net/
+ *
+ */
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+
+$GLOBALS['i18n_cfg_en'] = array(
+
+// boutons validation
+'ok' => 'OK',
+'reset' => 'Reset',
+'supprimer' => 'Erase',
+
+// textes cfg
+// C
+'cfg' => 'CFG',
+'choisir_module_a_configurer' => 'Choose the module to configure.',
+'configuration_modules' => 'Modules Configuration',
+'config_enregistree' => '<b>@nom@</b> has been saved', 
+'config_supprimee' => '<b>@nom@</b> has been erased',
+// E
+'erreur_lecture' => 'Impossible to read @nom@',
+'erreur_enregistrement' => 'An error occured while trying to save <b>@nom@</b>',
+'erreur_open_w_fichier' => 'Impossible to write to the file @fichier@',
+'erreur_suppression' => 'An error occured while trying to erase <b>@nom@</b>',
+// I
+'id_manquant' => 'Missing ID',
+// N
+'nom_table_manquant' => 'The name of the SQL table is missing',
+'nouveau' => 'New',
+// P
+'pas_de_changement' => 'No changes to <b>@nom@</b>',
+'pas_de_champs_dans' => 'No field availlable in @nom@'
+
+);
+
+?>
