d2992338aaa353e526ea8afed830ddcd98fd3215
cvallois@yahoo.fr <cvallois@yahoo.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-12-15 13:47:21 +0000

    corection bug sur filtre accord pluriel
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@17443 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/outils_article/boiteoutils_options.php
+++ _test_/outils_article/boiteoutils_options.php
@@ -1,37 +1,6 @@
 <?php
 
-/*
- *   +----------------------------------+
- *    Nom du Filtre :    accord_pluriel
- *   +----------------------------------+
- *    Date : mercredi 16 avril 2003
- *    Auteur :  Roustoubi (roustoubi@tiscali.fr)
- *   +-------------------------------------+
- *    Fonctions de ce filtre :
- *    Accorde le nom singulier passé en paramètre avec le nombre qui le quantifie
- *    Exemple : [#TOTAL_BOUCLE (#TOTAL_BOUCLE|accord_pluriel{article})]
- *   +-------------------------------------+ 
- *  
- * Pour toute suggestion, remarque, proposition d'ajout
- * reportez-vous au forum de l'article :
- * http://www.uzine.net/spip_contrib/article.php3?id_article=111
-*/
 
-function accord_pluriel ($nombre, $nom1='', $nom2='') {
-	if ($nom1=='') { return "Erreur filtre <b>&laquo; accord_pluriel &raquo;</b> : probl&egrave;me de param&egrave;tre"; }
-	if ($nom2!='') {
-		$nom2 = " ".$nom2;
-		$nom2s = $nom2."s";
-	}
-	if ($nombre == "0" OR $nombre == "1") {
-		$texte = $nombre."&nbsp;".$nom1.$nom2 ;
-	}
-	else {
-		$texte = $nombre."&nbsp;".$nom1."s".$nom2s ;
-	}
-	return $texte ; 
-}
-// FIN du Filtre accord_pluriel
 
 // Toute personne censee se detournerait de la rfc 822... et pourtant
 function date_rfc822($date_heure) {
@@ -66,4 +35,36 @@ function age_style($date) {
 	return 'background-color: #'.$couleur.';';
 }
 
+/*
+ *   +----------------------------------+
+ *    Nom du Filtre :    accord_pluriel
+ *   +----------------------------------+
+ *    Date : mercredi 16 avril 2003
+ *    Auteur :  Roustoubi (roustoubi@tiscali.fr)
+ *   +-------------------------------------+
+ *    Fonctions de ce filtre :
+ *    Accorde le nom singulier passé en paramètre avec le nombre qui le quantifie
+ *    Exemple : [#TOTAL_BOUCLE (#TOTAL_BOUCLE|accord_pluriel{article})]
+ *   +-------------------------------------+ 
+ *  
+ * Pour toute suggestion, remarque, proposition d'ajout
+ * reportez-vous au forum de l'article :
+ * http://www.uzine.net/spip_contrib/article.php3?id_article=111
+*/
+
+function accord_pluriel ($nombre, $nom1='', $nom2='') {
+	if ($nom1=='') { return "Erreur filtre <b>&laquo; accord_pluriel &raquo;</b> : probl&egrave;me de param&egrave;tre"; }
+	if ($nom2!='') {
+		$nom2 = " ".$nom2;
+		$nom2s = $nom2."s";
+	}
+	if ($nombre == "0" OR $nombre == "1") {
+		$texte = $nombre."&nbsp;".$nom1.$nom2 ;
+	}
+	else {
+		$texte = $nombre."&nbsp;".$nom1."s".$nom2s ;
+	}
+	return $texte ; 
+}
+// FIN du Filtre accord_pluriel
 ?>
