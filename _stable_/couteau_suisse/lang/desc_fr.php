0f8b2b5938aa98b7603fef543cf3b7448d0443c9
patfr@ifrance.com <patfr@ifrance.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-12-21 01:31:15 +0000

    outil SPIP et ses raccourcis : on gère la puce de SPIP et les styles de raccourcis à partir de 1.93 pour la ligne horizontale, c'est ok.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@17580 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/couteau_suisse/lang/desc_fr.php
+++ _stable_/couteau_suisse/lang/desc_fr.php
@@ -285,14 +285,15 @@ Attention : certains outils du Couteau Suisse n&eacute;cessitent les fonctions d
 // ---------------------------------------------------------------------------
 // TODO : a partir de 1.93 il faut changer (ul ol pour style_h et simplement li pour style_p)
 	'class_spip:nom' => 'SPIP et ses raccourcis&hellip;',
-	'class_spip:description' => (defined('_SPIP19300')?"#PUCE {{Les styles de SPIP}}. Jusqu'&agrave; la version 1.92 de SPIP, les raccourcis typographiques produisaient des balises syst&eacute;matiquement affubl&eacute;s du style &quot;spip&quot;. Par exemple : <code><p class=\"spip\"></code>. Vous pouvez ici d&eacute;finir le style de ces balises en fonction de vos feuilles de style. Une case vide signifie qu'aucun style particulier ne sera appliqu&eacute;.<blockquote>
+	'class_spip:description' => defined('_SPIP19300')?"#PUCE {{Les styles de SPIP}}. Jusqu'&agrave; la version 1.92 de SPIP, les raccourcis typographiques produisaient des balises syst&eacute;matiquement affubl&eacute;s du style &quot;spip&quot;. Par exemple : <code><p class=\"spip\"></code>. Vous pouvez ici d&eacute;finir le style de ces balises en fonction de vos feuilles de style. Une case vide signifie qu'aucun style particulier ne sera appliqu&eacute;.<blockquote>
 _ {{1.}} Balises &lt;p&gt;, &lt;i&gt;, &lt;strong&gt; et les listes (&lt;ol&gt;, &lt;ul&gt;, etc.) :[[Votre style :->%style_p%]]
 _ {{2.}} Balises &lt;tables&gt;, &lt;hr&gt;, &lt;h3&gt; et &lt;blockquote&gt; :[[Votre style :->%style_h%]]
 
 Attention : en modifiant ce deuxi&egrave;me param&egrave;tre, vous perdez alors les styles standards associ&eacute;s &agrave; ces balises.</blockquote>
-#PUCE {{Les raccourcis de SPIP}}. ":'')."D&eacute;finissez ici ce que SPIP doit mettre à la place des raccourcis suivants (une valeur vide &eacute;quivaut &agrave; utiliser la valeur par d&eacute;faut) : [[Ligne horizontale &laquo;<html>----</html>&raquo; :->%racc_hr%]][[Puce publique &laquo;<html>-</html>&raquo; :->%puce%]]
+#PUCE {{Les raccourcis de SPIP}}. D&eacute;finissez ici ce que SPIP doit mettre à la place des raccourcis suivants (une valeur vide &eacute;quivaut &agrave; utiliser la valeur par d&eacute;faut) : [[Ligne horizontale &laquo;<html>----</html>&raquo; :->%racc_hr%]][[Puce publique &laquo;<html>-</html>&raquo; :->%puce%]]
 Attention : en d&eacute;finissant le remplacement de la ligne horizontale, le style {{2.}} sp&eacute;cifi&eacute; plus haut ne sera pas appliqu&eacute; sur &lt;hr />."
-,//	."<p>Pour afiner un peu, vous pouvez maintenant d&eacute;finir le style particulier des balises suivantes :</p>",
+:"D&eacute;finissez ici ce que SPIP doit mettre à la place du raccourci suivant (une valeur vide &eacute;quivaut &agrave; utiliser la valeur par d&eacute;faut) : [[Ligne horizontale &laquo;<html>----</html>&raquo; :->%racc_hr%]]"
+,
 
 	'decoupe:nom' => 'D&eacute;coupe en pages et onglets',
 	'decoupe:description' => "D&eacute;coupe l'affichage public d'un article en plusieurs pages gr&acirc;ce &agrave; une pagination automatique. placez simplement dans votre article quatre signes plus cons&eacute;cutifs (<code>++++</code>) &agrave; l'endroit qui doit recevoir la coupure.
