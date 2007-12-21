d3374e66a2966fcde3921f13741f360b7c9633db
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-12-21 22:58:35 +0000

    plus besoin avec cfg récent...
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@17592 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/spipmotion/spipmotion_pipelines.php
+++ _dev_/spipmotion/spipmotion_pipelines.php
@@ -1,22 +1,5 @@
 <?php
 
-//    Fichier créé pour SPIP
-//    Distribué sans garantie sous licence GPL./
-//    Copyright (C) 2006  Quentin Drouet
-//
-//    This program is free software; you can redistribute it and/or modify
-//    it under the terms of the GNU General Public License as published by
-//    the Free Software Foundation; either version 2 of the License, or any later version.
-//
-//    This program is distributed in the hope that it will be useful,
-//    but WITHOUT ANY WARRANTY; without even the implied warranty of
-//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//    GNU General Public License for more details.
-//
-//    You should have received a copy of the GNU General Public License
-//    along with this program; if not, write to the Free Software
-//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-
 include_spip("inc/spipmotion");
 
 function spipmotion_affiche_droite($flux) {
@@ -24,16 +7,6 @@ function spipmotion_affiche_droite($flux) {
 		$spipmotion = charger_fonction('spipmotion', 'inc');
 		$flux['data'] .= $spipmotion($flux['arg']['id_article']);
 	}
-	return $flux;
-}
-
-function spipmotion_ajouter_onglets($flux) {
-	global $connect_statut, $connect_toutes_rubriques;
-	if($connect_statut == '0minirezo' AND $connect_toutes_rubriques)
-		if($flux['args']=='configuration')
-		$flux['data']['config_spipmotion']= new Bouton("../"._DIR_PLUGIN_SPIPMOTION."images/flv.png",
-		_T('spipmotion:titre'),
-		parametre_url(generer_url_ecrire('cfg'),'cfg','spipmotion'));
 	return $flux;
 }
 
