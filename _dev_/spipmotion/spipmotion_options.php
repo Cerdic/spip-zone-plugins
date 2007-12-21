6be00c58d0b13eba23defa11b8fab0d746aabd9d
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-12-21 22:54:54 +0000

    code mort ... sert plus à rien en svn
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@17591 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/spipmotion/spipmotion_options.php
+++ _dev_/spipmotion/spipmotion_options.php
@@ -1,7 +1,3 @@
 <?php
-if (!defined('_DIR_PLUGIN_SPIPMOTION')){
-	$p=explode(basename(_DIR_PLUGINS)."/",str_replace('\\','/',realpath(dirname(__FILE__))));
-	define('_DIR_PLUGIN_SPIPMOTION',(_DIR_PLUGINS.end($p)));
-}
 
 ?>
