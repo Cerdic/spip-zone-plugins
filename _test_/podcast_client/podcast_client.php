03616e6862699e52a5cfb3ae9deefa22d922b22c
nicolas@hoizey.com <nicolas@hoizey.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-12-25 11:26:20 +0000

    typo
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@17688 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/podcast_client/podcast_client.php
+++ _test_/podcast_client/podcast_client.php
@@ -18,7 +18,7 @@
 
 	function PodcastClient_podcast_client() {
 		PodcastClient_verifier_table_documents_syndic();
-	
+
 		list($le_lien, $id_syndic, $data) = func_get_arg(0);
 		//print_r($data);
 		//mettre les documents dans un tableau
@@ -35,12 +35,12 @@
 				if(!$data['enc_enclosure']['type']) $data['enc_enclosure']['type'] = substr($url, -3);
 			}
 
-		$data['enclosures_all'][] = $data['enc_enclosure'] ;	
+		$data['enclosures_all'][] = $data['enc_enclosure'] ;
 
-		// analyser le descriptif de l'article syndiqué
+		// analyser le descriptif de l'article syndiquï¿½
 		// trouver une image dans le descriptif (flickr)
 
-		// <img src="http://farm1.static.flickr.com/209/465887020_320ee68662_m.jpg" width="180" height="240" alt="Apéro Belleville 19/7/04" style="border: 1px solid #ddd;" />
+		// <img src="http://farm1.static.flickr.com/209/465887020_320ee68662_m.jpg" width="180" height="240" alt="Apï¿½ro Belleville 19/7/04" style="border: 1px solid #ddd;" />
 		$enclosure = $data['descriptif'] ;
 			if (($enc_regs_url = extraire_attribut($enclosure,'src'))) {
 				$url  = substr(urldecode($enc_regs_url), 0,255);
@@ -53,7 +53,7 @@
 
 				}
 			}
-			
+
 		$data['enclosures_all'][] = $data['enc_image'] ;
 
 		//trouver un flv chez  dailymotion ou une image chez flickr
@@ -72,9 +72,9 @@
 		}
 
 		// trouver une vignette miniature du media
-		// <media:thumbnail url="http://static-02.dailymotion.com/dyn/preview/320x240/2084714.jpg?20070218160100" height="240" width="320"/>	
+		// <media:thumbnail url="http://static-02.dailymotion.com/dyn/preview/320x240/2084714.jpg?20070218160100" height="240" width="320"/>
 
-		// <media:thumbnail url="http://farm1.static.flickr.com/169/465892205_c22c43b50d_s.jpg" height="75" width="75"/>		
+		// <media:thumbnail url="http://farm1.static.flickr.com/169/465892205_c22c43b50d_s.jpg" height="75" width="75"/>
 		if (preg_match(',(<media:thumbnail[^>]+\/>),i',
 		$data['item'], $match)) {
 			$go=str_replace('media:thumbnail','mediathumbnail',$match[1]);
@@ -83,19 +83,19 @@
 		}
 
 		// si le media est un flv, on prend le flv et aussi la miniature (a terme la mettre dans la vignette du doc distant spip)
-		if($data['enc_media']['type'] == 'video/x-flv'){	
+		if($data['enc_media']['type'] == 'video/x-flv'){
 			$data['enclosures_all'][] = $data['enc_media'] ;
 			$data['enclosures_all'][] = $data['enc_thumbnail'] ;
 		}
-		
-		
-		//trouver une video utube ou dalilymotion
-		
-		# <embed width="480" height="390" src="http://www.dailymotion.com/swf/1mt6qo4nfRX3Np77L" 
+
+
+		//trouver une video youtube ou dailymotion
+
+		# <embed width="480" height="390" src="http://www.dailymotion.com/swf/1mt6qo4nfRX3Np77L"
 		# type="application/x-shockwave-flash" allowfullscreen="true" allowscriptaccess="always">
-		
+
 		//var_dump($data['item']);
-		
+
 		//echo $item ;
 		if (preg_match(',(<embed[^>]*>),i',
 		$data['item'], $match)) {
@@ -105,14 +105,14 @@
 			$data['enc_emb']['width'] = extraire_attribut($go, 'width');
 			$data['enc_emb']['height'] = extraire_attribut($go, 'height');
 			$data['enc_emb']['type'] = trim(extraire_attribut($go,'type'));
-			
+
 			//var_dump($data['enc_emb']);
 			$data['enclosures_all'][] = $data['enc_emb'] ;
 
 		}
 
 
-		
+
 //var_dump($data['item']);var_dump($data['descriptif']);var_dump($data['enclosures_all']);die("coucou");
 
 		/**/
@@ -145,12 +145,12 @@
 	spip_log('podcast_client'.$le_lien.'\n\n');
 		if (sizeof($enclosures) == 0) return false ;
 		include_spip('inc/filtres'); # pour extraire_attribut
-	
+
 		list($id_syndic_article) = spip_fetch_array(spip_query(
 		"SELECT id_syndic_article FROM spip_syndic_articles
 		WHERE id_syndic=$id_syndic AND url='".addslashes($le_lien)."'"), SPIP_NUM);
-	
-		// Attention si cet article est deja vu, ne pas doubler les references 
+
+		// Attention si cet article est deja vu, ne pas doubler les references
 		spip_query("DELETE FROM spip_documents_syndic
 		WHERE id_syndic_article=$id_syndic_article");
 		spip_log("efface");
@@ -172,8 +172,8 @@
 					FROM spip_types_documents WHERE extension='$type'"), SPIP_NUM);
 				spip_log("id_type2 : ".$id_type) ;
 				}
-				
-				
+
+
 				if (!$id_type) {
 					spip_log("podcast_client: enclosure inconnue ($type) $url");
 					list($id_type) = spip_fetch_array(spip_query("SELECT id_type
@@ -184,7 +184,7 @@
 				}
 				spip_log("id_type : ".$id_type) ;
 				// length : optionnel (non bloquant)
-				
+
 				$taille = intval($enclosure['length']);
 				$largeur = intval($enclosure['width']);
 				$hauteur = intval($enclosure['height']);
@@ -202,9 +202,9 @@
 					VALUES ($id_type,'','$url',NOW(),'oui',$taille, 'document', $largeur, $hauteur)");
 					$id_document = spip_insert_id();
 					spip_log("podcast_client: '$url' => id_document=$id_document");
-					
+
 				}
-	
+
 				// lier avec l'article syndique
 				spip_query("INSERT INTO spip_documents_syndic
 				(id_document, id_syndic, id_syndic_article)
@@ -212,10 +212,10 @@
 				spip_log("lier doc->".$id_document."Sarticle".$id_syndic_article) ;
 				$n++;
 			}
-		
-		
+
+
 		}
-	
+
 		return $n; #nombre d'enclosures integrees
 	}
 
