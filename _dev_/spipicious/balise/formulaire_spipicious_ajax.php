c74da1dfe93f61fd43e5cc6f9b3a3713b067e243
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2007-12-29 23:46:55 +0000

    mmmh ca marche mieux là...
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@17773 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/spipicious/balise/formulaire_spipicious_ajax.php
+++ _dev_/spipicious/balise/formulaire_spipicious_ajax.php
@@ -3,47 +3,52 @@
 if (!defined("_ECRIRE_INC_VERSION")) return;	#securite
 
 function balise_FORMULAIRE_SPIPICIOUS_AJAX ($p) {
-	return calculer_balise_dynamique($p,'FORMULAIRE_SPIPICIOUS_AJAX', array('id_article'));
+	$p = calculer_balise_dynamique($p,'FORMULAIRE_SPIPICIOUS_AJAX', array('id_document','id_rubrique', 'id_forum', 'id_article', 'id_breve', 'id_syndic'));
+	return $p;
 }
 
-function balise_FORMULAIRE_SPIPICIOUS_AJAX_stat($args, $filtres) {  
-	// Pas d'id_article ? Erreur de squelette 
-	if (!$args[0])
-		return erreur_squelette(
-			_T('zbug_champ_hors_motif',
-				array ('champ' => '#FORMULAIRE_SPIPICIOUS_AJAX',
-					'motif' => 'ARTICLES')), '');
-	return $args;
-}
-
-function balise_FORMULAIRE_SPIPICIOUS_AJAX_dyn($id_article ) {
+function balise_FORMULAIRE_SPIPICIOUS_AJAX_dyn($id_document,$id_rubrique,$id_forum,$id_article,$id_breve,$id_syndic) {
 	$autorise = lire_config('spipicious/people');
+	if (!$autorise) $autorise == '0minirezo';
 	
 	if (!$GLOBALS["auteur_session"] OR !in_array($GLOBALS['auteur_session']['statut'],$autorise)) {
 		return '';
 	} else {
-		$auteur_nom = $GLOBALS['auteur_session']['nom'];
-		$auteur_id = $GLOBALS['auteur_session']['email'];
-		$auteur_statut = $GLOBALS['auteur_session']['statut'];
+		$auteur_id = $GLOBALS['auteur_session']['id_auteur'];
 	}
 
+	$ids = array();
+	if ($id_rubrique > 0 AND ($id_article OR $id_breve OR $id_syndic))
+		$id_rubrique = 0;
+	foreach (array('id_article', 'id_breve', 'id_rubrique', 'id_syndic', 'id_forum') as $o) {
+		if ($x = intval($$o)) {
+			$ids[$o] = $x;
+			$id = $x;
+			$type = str_replace('id_', '', $o);
+		}
+	}
+	
+	if ($id_document) {
+		$type = 'document';
+		$id = $id_document;
+		$ids['id_document'] = $id;
+	}
+	
+	$table_mot = 'spip_mots_'.$type.'s';
+	
 	//recuperation des variables utiles
 	$tags = _request('tags');
 	$id_groupe_tags = _request('select_groupe');
 	$type_groupe_tags = _request('type_groupe');
 	$add_tags = _request('add_tags');
 	
-	$autorise = lire_config('spipicious/people');
-	
 	if ($add_tags && $auteur_id){
-	
-	spip_query("DELETE FROM spip_spipicious WHERE id_auteur='$auteur_id' AND id_article='$id_article' "); // on efface les anciens triplets de cet auteur sur cet article 
 
 	if ($tags && $auteur_id) {
 		if (substr($tags, -1, 1) == ';'){
 			$tags = substr($tags,0,-1);
 		}
-		$tableau_tags = explode("; ",$tags);
+		$tableau_tags = explode(";",$tags);
 		if (is_array($tableau_tags)) {
 			$position = 0;
 			$tag_analysed = array();
@@ -53,28 +58,35 @@ function balise_FORMULAIRE_SPIPICIOUS_AJAX_dyn($id_article ) {
 			if ($tag!="" && !in_array($tag,$tag_analysed)) {
 
 			// doit on creer un nouveau tag ?
-			$result = spip_query("SELECT * FROM spip_mots AS mots WHERE titre='$tag' AND id_groupe=$id_groupe_tags");
-
-			if (spip_num_rows($result) == 0) { // creation tag
-				$exist = 'mot inexistant';
-				$sql = "INSERT INTO spip_mots (titre,id_groupe,type) VALUES(".spip_abstract_quote(corriger_caracteres($tag)).",$id_groupe_tags,'$type_groupe_tags')"; // FIXME encodage caractere ?
-				$result = spip_query($sql);
-				$id_tag = spip_insert_id();
-			} else {  // on recupere l'id du tag
-				$exist = 'mot existant';
-				$log = spip_log($exist,'spipicious');
-				while($row=spip_fetch_array($result)){
-					$id_tag = $row['id_mot'];
-					spip_log($id_tag,'spipicious');
+			$result = sql_select("*","spip_mots","titre='".$tag."' AND id_groupe=$id_groupe_tags");
+			
+				if (sql_count($result) == 0) { // creation tag
+					$exist = 'mot inexistant';
+					$log = spip_log($exist,'spipicious');
+					sql_insertq("spip_mots", array('titre' => $tag,'id_groupe' => $id_groupe_tags,'type' => $type_groupe_tags));
+					spip_log('creation du nouveau mot est '.$tag.'','spipicious');
+					$result2 = sql_select("*","spip_mots","titre = '".$tag."' AND id_groupe=$id_groupe_tags");
+					while($row=sql_fetch($result2)){
+						$id_tag = $row['id_mot'];
+						spip_log('id_mot du nouveau mot est '.$id_tag.'','spipicious');
+					}
+				} else {  // on recupere l'id du tag
+					$exist = 'mot existant';
+					$log = spip_log($exist,'spipicious');
+					while($row=sql_fetch($result)){
+						$id_tag = $row['id_mot'];
+						spip_log('id_mot est '.$id_tag.'','spipicious');
+					}
 				}
-			}
-			// on lie le mot au couple article (uniquement si pas deja fait)
-			$result = spip_query("SELECT id_mot FROM spip_mots_articles WHERE id_mot=$id_tag AND id_article=$id_article");
-			if (spip_num_rows($result) == 0) {
-				spip_query("INSERT INTO spip_mots_articles(id_mot,id_article) VALUES('$id_tag','$id_article')");
+		// on lie le mot au couple type (uniquement si pas deja fait)
+			$result = sql_select("id_mot","".$table_mot."","id_mot=".$id_tag." AND id_".$type."=".$id);
+			if (sql_count($result) == 0) {
+				sql_insertq("".$table_mot."",array('id_mot' => $id_tag,'id_'.$type.'' => $id));
+				$log = spip_log('insertion mot '.$id_tag.' in '.$table_mot.'','spipicious');
 			}
 			
-			spip_query("INSERT INTO spip_spipicious(id_mot,id_auteur,id_article,position) VALUES('$id_tag','$auteur_id','$id_article','$position')");
+			sql_insertq("spip_spipicious",array('id_mot' => $id_tag,'id_auteur' => $auteur_id,'id_'.$type.'' => $id, 'position' => $position));
+			$log = spip_log('insertion mot '.$id_tag.' in spip_spipicious','spipicious');
 			$position++;
 			}
 		$tag_analysed[] = $tag;
@@ -87,12 +99,15 @@ function balise_FORMULAIRE_SPIPICIOUS_AJAX_dyn($id_article ) {
 	}
 	else{
 		return array('formulaires/formulaire_spipicious_ajax', 0,
-			array('id' => $id_article,
-				'auteur_nom' => $auteur_nom,
+			array_merge($ids,
+			array(
+				'url' => $script, # ce sur quoi on fait le action='...'
+				'id' => $id,
 				'auteur_id' => $auteur_id,
-		));
+				'type' => $type,
+		))
+		);
 	}
-	
 }
 
 ?>
