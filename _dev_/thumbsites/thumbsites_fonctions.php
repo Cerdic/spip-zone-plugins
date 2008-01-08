e1d2e9d73af0c4b549476798dc1d9bab7221ba1e
cam.lafit@azerttyu.net <cam.lafit@azerttyu.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-01-08 12:53:55 +0000

    Oups erreur dans le commit le mauvais repertoire a été remonté ^^
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@17922 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _dev_/thumbsites/thumbsites_fonctions.php
@@ -0,0 +1,30 @@
+<?php
+    /*! \file thumbsites_fonctions.php 
+     *  \brief Fichier mes_fonctions propre au plugin
+     *         
+     *  Défini le filtre thumbsite
+     */
+
+    //charge cfg
+    include_spip('inc/cfg_config');
+
+    /*! \brief filtre à utiliser dans les squelettes
+     *
+     *  Définition de la fonction de filtre
+     *  Vérifie que le plugin est activé et qu'il n'existe pas ailleurs deja ce filtre
+     *  Rappel : dans le cadre d'une utilisation SPIP, il n'y a pas de paramétre à donner. $url correspond à la balise appelant le filtre
+     *  
+     * \param $url url du site à consulter
+     * \return url de l'image générée par le serveur
+     */
+    if ((lire_config('thumbsites/activer')=='on') AND !function_exists('thumbsite')) {
+        function thumbsite($url) {
+            //determine le serveur d'aperçu à utiliser
+            $serveur = lire_config('thumbsites/serveur');
+            //Charge le fichier de conf spécifique au serveur
+            include_spip('thumbsites/'.$serveur);
+            //execute la surcharge
+            return thumbsite_serveur($url);
+        }
+    }
+?>
