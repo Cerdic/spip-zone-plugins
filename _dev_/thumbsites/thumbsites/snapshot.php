a64a726d591e78d51d9556c48d51f9f892c7471b
cam.lafit@azerttyu.net <cam.lafit@azerttyu.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-01-09 10:42:19 +0000

    Ajout du serveur snapshot dans les possibilités : le fichier de configuration serveur
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@17930 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _dev_/thumbsites/thumbsites/snapshot.php
@@ -0,0 +1,23 @@
+<?php
+    /*! \file girafa.php 
+     *  \brief Fichier de fonctions personnalisées au serveur girafa
+     *         
+     *  Défini la surcharge de thumb
+     *  Le nom du fichier doit être obligatoirement celui déclaré dans le fond pour le paramétre thumbsites/serveur     
+     */
+
+
+    /*! \brief thumbsite_serveur() pour le serveur girafa
+     *
+     *  Surcharge de la fonction thumbs() exploitant le serveur d'aperçu de girafa
+     *  
+     * \param $url url du site à consulter
+     * \return url de l'image générée par le serveur
+     */
+    function thumbsite_serveur($url) {
+        //retourne l'url de la vignette
+        return "http://shots.snap.com/preview/?url=".$url;
+
+    }        
+    
+?>
