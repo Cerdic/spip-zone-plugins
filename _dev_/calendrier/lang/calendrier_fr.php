500a134573f9e66951513bf70e267e6734d9f5da
news.leon@free.fr <news.leon@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-01-12 16:44:10 +0000

    ajouts : internationalisation de l'interface de configuration
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@17983 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _dev_/calendrier/lang/calendrier_fr.php
@@ -0,0 +1,22 @@
+<?php
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	'Calendriers' => 'Calendriers',
+	'cal_par_mois' => 'calendrier mensuel',
+	'navigation' => 'navigation',
+	'par_mois' => 'par mois',
+	'par_annee' => 'par ann&eacute;e',
+	'premier_jour_culturel' => 'premier jour culturel',
+	'desc_pjc_auto' => '(automatique : le dimanche pour les pays anglo-saxons et le japon, le lundi sinon)',
+	'jour_semaine_abbr' => 'jour de la semaine',
+	'mot_entier' => 'mot complet',
+	'abreviation' => 'abr&eacute;viation',
+	'initiale' => 'initiale',
+	'une_lettre' => 'premi&egrave;re lettre',
+	'deux_lettres' => 'deux lettres',
+	'trois_lettres' => 'trois lettres',
+	'revenir_aujourdhui' => 'retour &agrave; la date courante',
+	'texte_precedent' => 'texte "pr&eacute;c&eacute;dent"',
+	'texte_suivant' => 'texte "suivant"',
+	'desc_texte_prec_suiv' => '(texte du lien vers la p&eacute;riode pr&eacute;c&eacute;dente / suivante &ndash; \'&#171;\' / \'&#187;\' par d&eacute;faut)'
+);
+?>
