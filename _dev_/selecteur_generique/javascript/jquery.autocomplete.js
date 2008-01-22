0caa2b07392387790ee94394885ca4611e4d6e59
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-01-22 23:10:35 +0000

    Un truc qui ne change rien au fonctionnement mais qui évite un bug si on utilise d'autre libs que jquery
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@18119 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/selecteur_generique/javascript/jquery.autocomplete.js
+++ _dev_/selecteur_generique/javascript/jquery.autocomplete.js
@@ -818,7 +818,7 @@ $.Autocompleter.Select = function (options, input, select, config) {
 					overflow: 'auto'
 				});
 				
-                if($.browser.msie && typeof document.body.style.maxHeight === "undefined") {
+                if(jQuery.browser.msie && typeof document.body.style.maxHeight === "undefined") {
 					var listHeight = 0;
 					listItems.each(function() {
 						listHeight += this.offsetHeight;
