0f0f8ebeed8270035d3177645cf2504329884b7f
apachot@openstudio.fr <apachot@openstudio.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-01-22 19:18:01 +0000

    un test sur la variable lang avant de l'affecter.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@18110 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/plugin-thelia/spip_thelia_fonct.php
+++ _dev_/plugin-thelia/spip_thelia_fonct.php
@@ -5,15 +5,12 @@ function spip_thelia_appeler_moteur_thelia($texte)
 	if ($_REQUEST['page'] == "forum") return $texte;	
 	include_once("classes/Navigation.class.php");
 	
-	session_name('thelia_session');
 	session_start();
-	$_SESSION['navig']->lang="";
+	if ($_SESSION['navig']->lang != '') $_SESSION['navig']->lang=0;
 	
 	
-	foreach ($_POST as $key => $value) $$key = $value;
-	foreach ($_GET as $key => $value) $$key = $value;
 	switch($page){
-		case 'panier' : $securise=0; $pageret=1; break;
+		case 'panier' : $securise=1; $pageret=1; break;
 		case 'adresse' : $securise=1; $pageret=1; break;
 		case 'cheque' : $securise=1; $pageret=1;$reset=1; break;
 		case 'commande' : $securise=1; $pageret=1; $panier=1; $transport=1; break;
