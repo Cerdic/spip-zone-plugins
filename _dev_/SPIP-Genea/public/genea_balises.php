c9c28a09811cd4ca16460c562d4562f9e49fff7b
xebiaut@free.fr <xebiaut@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-01-30 18:50:01 +0000

    Fichiers oubliés
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@18251 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _dev_/SPIP-Genea/public/genea_balises.php
@@ -0,0 +1,101 @@
+<?php
+
+/*******************************************************************
+ *
+ * Copyright (c) 2007-2008
+ * Xavier BUROT
+ * fichier : public/genea_balises
+ *
+ * Ce programme est un logiciel libre distribue sous licence GNU/GPL
+ *
+ * *******************************************************************/
+
+
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+include_spip('base/genea_base');
+include_spip('base/create');
+include_spip('inc/filtres');
+
+//
+// GESTION DES DATES
+//
+
+// -- Recupere et verif le droit a l'affichage des dates ----------------
+function genea_date_evt($id_individu, $type_evt, $fin=false, $second=false){
+	global $table_prefix, $connect_statut;
+	$date_evt='';
+	if ($id_individu) {
+		$q="SELECT ";
+		if ($fin==false) {
+			if (($second==false)) $q.="date_debut";
+			else $q.="date_debut2";
+		}else{
+			if (($second==false)) $q.="date_fin";
+			else $q.="date_fin2";
+		}
+		$q.=" FROM ".$table_prefix."_genea_evt WHERE (id_individu=$id_individu) AND (type_evt='$type_evt')";
+		$res=spip_query($q);
+		if ($row=spip_fetch_array($res)) {
+			if ($fin==false) {
+				if (($second==false)) $date_evt=$row['date_debut'];
+				else $date_evt=$row['date_debut2'];
+			}else{
+				if (($second==false)) $date_evt=$row['date_fin'];
+				else $date_evt=$row['date_fin2'];
+			}
+		}
+	}
+
+	// Transformer au format Date
+	$date_ret = "0000-00-00 00:00:00";
+//	$annee = substr($date_evt, 0, 4);
+//	$mois = substr($date_evt, 4, 2);
+//	$jour = substr($date_evt, 6, 2);
+//	$heure = substr($date_evt, 8,2);
+//	$minute = substr($date_evt, 10,2);
+
+	// Faire verification des droits de visualisation des dates
+	// si elles sont inferieures a 100 ans. Ne pas afficher, sauf
+	// pour les redacteurs et les administrateurs autorises.
+	$cejour= getdate();
+	$centans = intval($cejour['annee'])-100;
+	if (($centans>=date("Y", strtotime(normaliser_date($date_evt)))) || ($GLOBALS['auteur_session']['statut'] == "0minirezo") || ($GLOBALS['auteur_session']['statut'] == "1comite")) $date_ret = $date_evt;
+
+	return vider_date(normaliser_date($date_ret));
+}
+
+// -- Affiche la date de deces  -----------------------------------------
+function balise_DATE_DECES($p){
+	$p->code = "genea_date_evt(".champ_sql('id_individu',$p).", 'deat')";
+	$p->interdire_scripts = true;
+	return $p;
+}
+
+// -- Affiche la date de naissance --------------------------------------
+function balise_DATE_NAISSANCE($p){
+	$p->code = "genea_date_evt(".champ_sql('id_individu',$p).", 'birt')";
+	$p->interdire_scripts = true;
+	return $p;
+}
+
+// -- Recherche le numero SOSA d'un invidu ------------------------------
+function trouve_sosa($id_individu){
+	global $table_prefix;
+	$sosa='';
+	if($id_individu){
+		$q = "SELECT id_sosa FROM ".$table_prefix."_genea_sosa WHERE id_individu=$id_individu";
+		$res = spip_query($q);
+		if ($row = spip_fetch_array($res)) $sosa = $row['id_sosa'];
+	}
+	return $sosa;
+}
+
+// -- Affiche le numero SOSA d'un individu ------------------------------
+function balise_SOSA($p){
+	$p->code = "trouve_sosa(".champ_sql('id_individu', $p).")";
+	$p->interdire_scripts = true;
+	return $p;
+}
+?>
