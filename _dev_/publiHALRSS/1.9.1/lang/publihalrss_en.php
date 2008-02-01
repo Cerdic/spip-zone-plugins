9132e32d246884b9555d9c97bcf74a5fd1ce470f
bollonjm@yahoo.com <bollonjm@yahoo.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-02-01 10:16:16 +0000

    Modification pour affichage multilingue fr/en
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@18264 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _dev_/publiHALRSS/1.9.1/lang/publihalrss_en.php
@@ -0,0 +1,24 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+//A
+//D
+'de_l_annee' => 'of the year',
+'depuis' => 'from',
+//J
+'jusqu_a' => 'to',
+// L
+'les_publications' => 'Publications',
+'liste_de_publications' =>'Publications list',
+// P
+'publications' => 'Publications',
+'pour' => 'for',
+//R
+//T
+'tous_les_types_de_doc' => 'All types of documents'
+
+);
+
+?>
---
+++ _dev_/publiHALRSS/1.9.1/lang/publihalrss_fr.php
@@ -0,0 +1,23 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+//A
+//D
+'de_l_annee' => "de l'ann&eacute;e",
+'depuis' => 'depuis',
+//J
+'jusqu_a' => 'jusqu\'&agrave;',
+// L
+'les_publications' => 'Les publications',
+'liste_de_publications' =>'Liste de publications',
+// P
+'publications' => 'Publications',
+'pour' => 'pour',
+//R
+//T
+'tous_les_types_de_doc' => 'Tous les types de documents'
+);
+
+?>
