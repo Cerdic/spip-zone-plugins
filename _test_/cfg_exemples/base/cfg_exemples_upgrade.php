367d1f0e2d544ec2e4a59e404c715c91417f2e7b
marcimat@free.fr <marcimat@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-02-02 18:28:21 +0000

    - installation et suppression du plugin plus mieux
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@18288 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/cfg_exemples/base/cfg_exemples_upgrade.php
+++ _test_/cfg_exemples/base/cfg_exemples_upgrade.php
@@ -2,22 +2,40 @@
 
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
-function cfg_exemples_upgrade($nom_meta_base_version,$version_cible) {
+function cfg_exemples_upgrade($version_cible) {
 	spip_log("cfg_exemples_upgrade $nom_meta_base_version => $version_cible");
 	include_spip('base/create');
 	include_spip('base/abstract_sql');
 	include_spip('base/cfg_exemples');
 	creer_base();
 
-	ecrire_meta($nom_meta_base_version, $version_cible, 'non');
+	ecrire_meta('cfg_exemples_base_version', $version_cible, 'non');
 	ecrire_metas();
 }
 
-function cfg_exemples_vider_tables($nom_meta_base_version) {
+function cfg_exemples_vider_tables() {
 	spip_log("cfg_exemples_vider_tables");
 	spip_query("DROP TABLE spip_cfg_exemples");
-	effacer_meta($nom_meta_base_version);
+	effacer_meta('cfg_exemples_base_version');
 	ecrire_metas();
+}
+
+
+
+function cfg_exemples_install($action){
+	$version_base = $GLOBALS['cfg_exemples_base_version'];
+	switch ($action){
+		case 'test':
+			return (isset($GLOBALS['meta']['cfg_exemples_base_version']) 
+			AND ($GLOBALS['meta']['cfg_exemples_base_version']>=$version_base));
+			break;
+		case 'install':
+			cfg_exemples_upgrade($version_base);
+			break;
+		case 'uninstall':
+			cfg_exemples_vider_tables();
+			break;
+	}
 }
 
 ?>
