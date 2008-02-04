45e2ea5d09175d617d89907bb0dd504add1fa3f4
apachot@openstudio.fr <apachot@openstudio.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-02-04 18:30:24 +0000

    panier.html, chaines de langues
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@18318 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/plugin-thelia/lang/spip_thelia_fr.php
+++
@@ -1,6 +0,0 @@
-<?php
-$GLOBALS[$GLOBALS['idx_lang']] = array(
-'valider_panier' => 'Valider mon panier',
-'ajouter_panier' => 'Ajouter au panier',
-'catalogue_thelia' => 'Catalogue T&eacute;lia');
-?>
---
+++ _dev_/plugin-thelia/lang/spipthelia_fr.php
@@ -0,0 +1,14 @@
+<?php
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+'valider_panier' => 'Valider mon panier',
+'ajouter_panier' => 'Ajouter au panier',
+'mon_panier' => 'Contenu de mon panier',
+'references_produits' => 'R&eacute;f&eacute;rences produits',
+'prix_unitaire' => 'Prix unitaire',
+'quantite' => 'Quantit&eacute;',
+'total_ttc' => 'Total TTC',
+'supprimer' => 'Supprimer',
+'vider' => 'Vider',
+'catalogue_thelia' => 'Catalogue T&eacute;lia',
+'total_panier' => 'Total du panier');
+?>
