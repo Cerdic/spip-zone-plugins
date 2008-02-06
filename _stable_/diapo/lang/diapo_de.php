76c59e43346098b9a0d184c77b190cd2cab50e0d
baubiologie@gmail.com <baubiologie@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-02-06 22:40:57 +0000

    Rajout fichier allemand, mise à jour fichier plugin.xml pour allemand et liens pointants à la documentation
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@18374 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _stable_/diapo/lang/diapo_de.php
@@ -0,0 +1,20 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+'diapo'=>'Diaporama',
+'txt_icodiapo'=>'Diaporama play/pause',
+'txt_icoleft'=>'Vorschaubilder links',
+'txt_icoright'=>'Vorschaubilder rechts',
+'txt_icocenter'=>'Vorschaubilder oben (Grosses Bild)',
+'ico_diapo_play'=>'&gt;',
+'ico_diapo_pause'=>'||',
+'ico_left'=>'&nbsp;',
+'ico_right'=>'&nbsp;',
+'ico_center'=>'&nbsp;',
+'loading'=>'Das Bild wird geladen...'
+);
+
+
+?>
