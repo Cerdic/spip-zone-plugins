92e4259289d133c45cedab060a25cee3b58d3811
jlpnopub@gmail.com <jlpnopub@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-02-11 15:15:39 +0000

    Ajout fonctions d'archivage automatique (CRON) et sélection des répertoires à archiver
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@18427 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/archive/archive_options.php
+++ _dev_/archive/archive_options.php
@@ -14,4 +14,10 @@ array_merge(
     array('archive_date' => "datetime")
 );
 
+// Lancement des taches cron pour l'archivage
+function archive_taches_generales_cron($taches_generales){ 
+       $taches_generales['archive_cron']=1*24*3600;
+	    return $taches_generales;
+ }
+
 ?>
