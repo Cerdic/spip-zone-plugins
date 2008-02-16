e69b02193f65fb4ade5d7756f63aad3c896cd5e8
brunobergot@gmail.com <brunobergot@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-02-16 16:45:12 +0000

    on passe l'appel du script par ici maintenant comme ça on évite un hit sur un fichier js et on peut en plus accéder aux valeurs de CFG par php
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@18526 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/menu_accordeon/menuaccordeon_fonctions.php
+++ _dev_/menu_accordeon/menuaccordeon_fonctions.php
@@ -3,7 +3,13 @@
 function menuaccordeon_insert_head($flux){
 	$flux .= "<script type='text/javascript' src='".find_in_path('menu_accordeon.js')."'></script>\n";
 	$flux .= '<link rel="stylesheet" href="'.find_in_path('menu_accordeon.css').'" type="text/css" media="projection, screen" />';
-	$flux .= "<script type='text/javascript' src='".find_in_path('menu_accordeon_call.js')."'></script>\n";
+	//$flux .= "<script type='text/javascript' src='".find_in_path('menu_accordeon_call.js')."'></script>\n";
+	$flux .= "\n<script type='text/javascript'>\njQuery().ready(function(){jQuery('#listmenu').Accordion({header: 'a.hac',active: 'a.hac.on'";	
+	if(lire_config('menu-accordeon/event') == "hover"){
+		$flux .= ", event: 'mouseover'";
+	}
+	$flux .= "});});\n</script>\n";
+	
 	return $flux;
 }
 ?>
