80bd5d1ab335a2eb4e597c872d16df6cfd1b11d2
cousin-alain@wanadoo.fr <cousin-alain@wanadoo.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-02-22 10:50:12 +0000

    Ajouté un fichier à distance
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@18575 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _dev_/plugin-thelia/squelettes_multisaison/squelettes/nifty-bloog.js
@@ -0,0 +1,13 @@
+window.onload=function(){
+Nifty("div.menu_thelia,div.boite_cellule_droite,div.boite_cellule_gauche,div.boite,div.avatar,div.reponse,div.discussion,div.interne,p.interne,span.logorond","transparent");
+Nifty("div.formulaire_cellule_table,div.formulaire_cellule,div.grande_boite_cellule,ul#minipics li,div.grande_boite,div#content,div#footer");
+Nifty("div.interforum,div.interforumgrand","big");
+Nifty("div#header","top,big,transparent");
+Nifty("ul#deco h4,ul#decobas h4,ul#nav li","top");
+Nifty("div#surcontent","bottom,big");
+Nifty("#cel p","bottom,same-height");
+Nifty("div.cellule p","bottom,same-height");
+Nifty("div.cellule_produit p","bottom,same-height");
+Nifty("ul.cellule_deco h4","top");
+Nifty("#celbas p","bottom");
+}
