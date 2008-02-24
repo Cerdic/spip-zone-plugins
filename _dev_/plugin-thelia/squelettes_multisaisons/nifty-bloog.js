6d6c6455a98115298806461e5e11e1365eb06d46
cousin-alain@wanadoo.fr <cousin-alain@wanadoo.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-02-24 06:49:29 +0000

    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@18664 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/plugin-thelia/squelettes_multisaisons/nifty-bloog.js
+++ _dev_/plugin-thelia/squelettes_multisaisons/nifty-bloog.js
@@ -1,6 +1,6 @@
 window.onload=function(){
 Nifty("div.boite_cellule_droite,div.boite_cellule_gauche,div.boite,div.avatar,div.reponse,div.discussion,div.interne,p.interne,span.logorond","transparent");
-Nifty("div.formulaire_cellule_table,div.formulaire_cellule,div.grande_boite_cellule,ul#minipics li,div.grande_boite,div#content,div#footer");
+Nifty("div.formulaire_cellule,div.grande_boite_cellule,ul#minipics li,div.grande_boite,div#content,div#footer");
 Nifty("div.interforum,div.interforumgrand","big");
 Nifty("div#header","top,big,transparent");
 Nifty("ul#deco h4,ul#decobas h4,ul#nav li","top");
