ff8bb8499d48a8bf7ce212c292a72cd931fd8c8c
edd@no-log.org <edd@no-log.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-02-25 23:39:54 +0000

    Plus de boucle auteur_op, le compilo le fait tout seul Plus de boucle rubrique_op, mais un critère {openPublishing} à placer dans une boucle RUBRIQUES pour lister toutes les rubriques openPublishing
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@18750 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/openPublishing/op_fonctions.php
+++ _stable_/openPublishing/op_fonctions.php
@@ -2,31 +2,25 @@
 
 include_spip('inc/op_actions');
 
-/*
- * <BOUCLE(op_rubriques)>
- */
+// {openPublishing}
+function critere_openPublishing($idb, &$boucles, $crit) {
+	
+        $boucle = &$boucles[$idb];
+	$id_table = $boucle->id_table;
 
-function boucle_OP_RUBRIQUES_dist($id_boucle, &$boucles) {
-        $boucle = &$boucles[$id_boucle];
-        $id_table = $boucle->id_table;
+	$id_rubrique = $id_table.'.id_rubrique';
 
-        $boucle->from[$id_table] =  "spip_op_rubriques";
-	$boucle->from["rubriques"] =  "spip_rubriques";
-	$boucle->where[]= array("'='", "'rubriques.id_rubrique'", "'$id_table.op_rubrique'");
-	$boucle->group[] = $boucle->id_table . '.' . $boucle->primary;
-
-        return calculer_boucle($id_boucle, $boucles);
-}
-
-/*
- * <BOUCLE(op_auteurs)>
- */
-
-function boucle_OP_AUTEURS_dist($id_boucle, &$boucles) {
-        $boucle = &$boucles[$id_boucle];
-        $id_table = $boucle->id_table;
-        $boucle->from[$id_table] =  "spip_op_auteurs";
-        return calculer_boucle($id_boucle, $boucles);
+	$list = '';
+	foreach (lire_config('op') as $key => $val) {
+		if ((substr($key,0,3)) == "op_") {
+			if ($val == "openPublishing") {
+				$op_rub = substr($key,3,1);
+				$list .= $op_rub .',';
+			}
+		}
+	}
+	$list = substr($list,0,strlen($list)-1);
+	$boucle->where[] = array("'IN'", "'$id_rubrique'","'($list)'");
 }
 
 function balise_AGENDA($p) {
