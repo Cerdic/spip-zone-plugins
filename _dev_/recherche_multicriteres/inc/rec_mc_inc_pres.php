518f08c94e5b37510c11fa31cc30b6bd445175c4
dominique.lepaisant@ac-caen.fr <dominique.lepaisant@ac-caen.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-02-26 07:11:10 +0000

    ajout fonction menu_admin et suppression fonctions obsoletes
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@18754 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/recherche_multicriteres/inc/rec_mc_inc_pres.php
+++ _dev_/recherche_multicriteres/inc/rec_mc_inc_pres.php
@@ -8,14 +8,29 @@
 # Pour plus de details voir le fichier COPYING.txt                   #
 ######################################################################
 
-//
+// pointer la page courante dans bloc d'admin
+### note sur pointe_page() : le premier elem. du array doit être la page à atteindre
+function pointe_page($case_page, $fonction) {
+	$pg_exec = _request('exec');
+	// --> if() : cas de voir la fiche
+	if($fonction) {
+		echo "<br /><img border='0' src='"._DIR_IMG_PACK."fleche-right.png' />&nbsp;".
+			"<a href='".generer_url_ecrire($case_page[0])."'>".$fonction."</a>";
+	}
+
+	if (in_array($pg_exec,$case_page)) {
+		echo "&nbsp;<img border='0' src='"._DIR_IMG_PACK."fleche-left.png'>";
+	}
+	echo "<br />";
+}
+
 function debut_bloc_lien_page()
 	{
 	echo "<div style='margin-top:2px;' class='bouton36blanc' 
 		onMouseOver=\"changeclass(this,'bouton36gris')\"
 		onMouseOut=\"changeclass(this,'bouton36blanc')\">\n";
 	}
-//
+
 function fin_bloc()
 	{
 	echo "</div>\n";
@@ -47,59 +62,14 @@ function debut_boite_erreur($message)
 	echo $message;
 	echo "</div>";
 	}
-function debut_tableau_mots($id)
-	{
-	$sql="select * in spip_mots where id_groupe=$id";
-	$result=spip_query($sql);
-	echo "<table style=\"width:100%; text-align:left;\" border=\"1\" cellpadding=\"2\" cellspacing=\"2\">";
-	echo  "<tbody><tr>";
-		while ($row=spip_fetch_array($result)){
-			echo   "<td>".$row['titre']."</td>";
-		}
-	echo  "</tbody></tr>";
-	echo "</table>";
-	}
 
-function debut_table_mots()
-	{
-	global $couleur_foncee, $couleur_claire;
-	$sql="select titre from spip_mots where id_groupe=$id";
-	$result=spip_query($sql);
-	echo "<table style=\"width:100%; text-align:left; border:1px solid ".$couleur_foncee."; font-size:.8em\"  cellpadding=\"2\" cellspacing=\"0\">";
-	echo  "<tbody>";
-		while ($row=spip_fetch_array($result)){
-	echo  "<tr style=\"border:1px solid ".$couleur_foncee."; background-color:#ccc; font-wheight:small\"  >";
-			echo   "<td>".$row['titre']."</td><td></td>";
-	echo  "</tr>";
-		}
-	echo  "</tbody>";
-	echo "</table>";
-	}
-function debut_ligne_table_mots($titre)
-	{
-	global $couleur_foncee, $couleur_claire;
-	echo  "<tr style=\"border:1px solid ".$couleur_foncee."; background-color:#ccc; font-wheight:small\"  >";
-			echo   "<td>".$row['titre']."</td><td></td>";
-	echo  "</tr>";
-	}
-function fin_table_mots()
-	{
-	echo  "</tbody>";
-	echo "</table>";
+// --
+function menu_admin() {
+	debut_cadre_enfonce(_DIR_IMG_DW2."configure.gif");
+		echo "<div class='verdana2' style='padding:4px;'><b>"._T('rmc:conf')."<br />";
+			pointe_page(array("rec_mc"), _T('rmc:conf'));
+			pointe_page(array("rec_mc_conf_public"), _T('rmc:conf_public'));
+		echo "</b></div>";
+	fin_cadre_enfonce();
 }
-
-
-/*
-<table style="width: 100%; text-align:left;" border="1" cellpadding="2" cellspacing="2">
-<tbody>
-<tr>
-<td><br></td><td><br></td>
-</tr>
-</tbody>
-</table>
-*/
-
-
-
-
 ?>
