ce2b47c5f940e48676c7a5ebc0fa252691568a72
brunobergot@gmail.com <brunobergot@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-02-26 22:54:26 +0000

    inclure le fichier inc/editer_evenement pour rendre le formulaire public du plugin opérationnel (charger la fonction Agenda_action_formulaire_article())
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@18785 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/agenda/1_9_2/balise/formulaire_edition_evenement.php
+++ _stable_/agenda/1_9_2/balise/formulaire_edition_evenement.php
@@ -2,6 +2,8 @@
 
 if (!defined("_ECRIRE_INC_VERSION")) return;	#securite
 
+include_spip('inc/editer_evenement');
+
 // Pas besoin de contexte de compilation
 global $balise_FORMULAIRE_EDITION_EVENEMENT_collecte;
 $balise_FORMULAIRE_EDITION_EVENEMENT_collecte = array('id_evenement','id_article');
