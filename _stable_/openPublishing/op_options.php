0a4dbff6349a134f9514e4029719b80620b917d6
edd@no-log.org <edd@no-log.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-02-26 14:15:38 +0000

    définition des champs extras de la table spip_articles. Plus besoin d'une table spip_op_auteurs.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@18760 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/openPublishing/op_options.php
+++ _stable_/openPublishing/op_options.php
@@ -2,6 +2,15 @@
 $p=explode(basename(_DIR_PLUGINS)."/",str_replace('\\','/',realpath(dirname(__FILE__))));
 define('_DIR_PLUGIN_OPENPUBLISHING',(_DIR_PLUGINS.end($p)));
 
-include_spip('base/op_base');
+//
+// Definition des champs extra pour les articles openPublishing
+//
 
+$GLOBALS['champs_extra'] = Array (
+
+        'articles' => Array (
+                        "OP_pseudo" => "ligne|brut|pseudonyme du r&eacute;dacteur",
+                        "OP_mail" => "ligne|propre|mail du r&eacute;dacteur"
+                )
+        );
 ?>
