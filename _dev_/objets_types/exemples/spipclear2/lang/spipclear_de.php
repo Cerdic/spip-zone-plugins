0c8c7312275011dac6c4076e0e412e4585657af5
james@rezo.net <james@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-02-28 21:07:18 +0000

    un exemple un peu plus parlant : SpipClear avec des types. Plus besoin de fichiers rubrique-X.html et article-X.html, et le CFG permet de faire la synthèse des blogs du site.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@18839 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _dev_/objets_types/exemples/spipclear2/lang/spipclear_de.php
@@ -0,0 +1,105 @@
+<?php
+
+// This is a SPIP-forum module file  --  Ceci est un fichier module de SPIP-forum
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+//A
+'ajouter_commentaire' => 'Kommentar hinzug&uuml;gen',
+'archive' => 'Archiv', // Non utilise
+'archives' => 'Archive',
+'attime' => '&agrave;',
+'aucune_archive' => 'Kein Archiv', // Non utilise
+'aucune_categorie' => 'Keine Kategorie', // Non utilise
+'aucun_commentaire' => 'Kein Kommentar', // Non utilise
+'aucune_langue' => 'Kene Sprache', // Non utilise
+'aucun_lien' => 'Kein Link', // Non utilise
+'aucun_trackback' => 'Kein Trackback', // Non utilise
+
+//B
+'boss' => 'Administrator', // Oublie
+
+//C
+'calendrier' => 'Kalender', // Non utilise
+'categorie' => 'Kategorie', // Non utilise
+'categories' => 'Kategorien',
+'commentaire' => 'Kommentar',
+'commentaires' => 'Kommentare',
+'commentaires_pour' => 'Kommentare zu',
+'commentaires_fermes' => 'Die Kommetare zu diesem Eintrag sind beendet',
+'credits' => '<a href="http://www.spip.net/de">Angetrieben mit SPIP</a>,
+<a href="http://www.spip-contrib.net/Spip-Clear">beschleunigt mit Spip.Clear</a>',
+
+//D
+'date_jour_abbr_1' => 'So', // Non utilise
+'date_jour_abbr_2' => 'Mo', // Non utilise
+'date_jour_abbr_3' => 'Di', // Non utilise
+'date_jour_abbr_4' => 'Mi', // Non utilise
+'date_jour_abbr_5' => 'Do', // Non utilise
+'date_jour_abbr_6' => 'Fr', // Non utilise
+'date_jour_abbr_7' => 'Sa', // Non utilise
+'de' => 'de', // Non utilise
+
+//E
+
+//F
+'fil_rss' => 'RSS Stream',
+'fil_rss_commentaires' => 'Kommentar RSS-Stream',
+'fil_atom' => 'fil atom',
+'fil_atom_commentaires' => 'Kommentar ATOM-Stream',
+
+//G
+'go_main' => 'Zum Inhalt',
+'go_sidebar' => 'Zum Men&uuml;',
+'go_search' => 'Zur Suche',
+
+//H
+'hebergement' => 'Webhosting von',
+
+//I
+'info_navigation' => 'W&auml;en sie die Position der Navigation', // CFG
+'info_choix_theme' => 'W&auml;hlen sie das Layout-Thema f&uuml;r ihr Blog', // CFG
+
+//L
+'label_navigation_droite' => 'Navigation rechts (Standard)', // CFG
+'label_navigation_gauche' => 'Navigation links', // CFG
+'label_theme_defaut' => 'Standardthema (Typ DotClear)', // CFG
+'label_theme_perso' => 'Pers&ouml;nliches Thema (weiter unten definieren)', // CFG
+'label_nom_theme' => 'Geben sie den Namen des pers&ouml;nlichen Themas ein (entspricht dem Namen xxxx des Unterverzeichnisses squelettes/themes/xxxx/', // CFG
+'langue' => 'Sprache', // Non utilise
+'langues' => 'Sprachen',
+'legend_layout' => 'Pr&auml;sentation', // CFG
+'legend_theme' => 'Thema', // CFG
+'lien' => 'Link', // Non utilise
+'liens' => 'Links',
+
+//O
+'oksearch' => 'OK',
+
+//P
+'permalink_pour' => 'Permalink zu',
+
+//R
+'resultat_recherche' => 'Ergebnis ihrer Suche',
+'rss_pour' => 'RSS-Stream der Kommentare von',
+
+//S
+'selection' => 'merken',
+'site_comment' => 'Site',
+'syndication' => 'Syndikation',
+
+//T
+'texte_erreur1' => 'Suche nach',
+'texte_erreur2' => 'hat kein Ergebnis.',
+'titre_erreur' => 'Fehler:',
+'trackback' => 'Trackback', // Non utilise
+'trackbacks' => 'Trackbacks', // Non utilise
+'trackbacks_pour_faire' => 'Trackback zu diesem Eintrag anlegen', // Non utilise
+'trackbacks_fermes' => 'Die Trackbacks zu diesem Eintrag sind geschlossen.', // Non utilise
+
+//U
+'un_seul_mot' => 'ein einziges Wort', // Oubli
+
+);
+
+?>
