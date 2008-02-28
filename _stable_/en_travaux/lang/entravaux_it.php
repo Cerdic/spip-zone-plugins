8ef0e9eca5f678b10cb3696b63868b034fef58ef
renato@rezo.net <renato@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-02-28 11:27:39 +0000

    traduction italienne
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@18830 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _stable_/en_travaux/lang/entravaux_it.php
@@ -0,0 +1,13 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+'activer_message' => 'Attivare una sessione di manutenzione',
+'en_travaux' => 'In manutenzione',
+'parametrage_page_travaux' => 'COnfigurazione della pagina di manutenzione',
+'info_message' => 'Questa pagina consente di visualizzare un messaggio temporaneo su tutte le pagine del sito durante una sessione di manutenzione.',
+'message_temporaire' => 'Il vostro messaggio di manutenzione:',
+
+);
+?>
