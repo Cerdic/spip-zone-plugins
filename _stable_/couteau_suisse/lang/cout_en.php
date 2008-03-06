e36e69ca6e6ba01f5cb3a61d856ec78880aeca31
mathbouq@mathbouq.com <mathbouq@mathbouq.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-03-06 12:14:33 +0000

    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@18994 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/couteau_suisse/lang/cout_en.php
+++ _stable_/couteau_suisse/lang/cout_en.php
@@ -8,13 +8,13 @@
 $GLOBALS[$GLOBALS['idx_lang']] = array(
        'titre' => 'Swiss Knife',
        // Forcing Signings on forum posts
-       'nom_forum' => 'Please also specify your name !',
+       'nom_forum' => 'Please also specify your name!',
        // Private Boxes
        'variable_vide' => '(Empty)',
        'texte_formatspip' => 'ORIGINAL TEXT SPIP FORMATED', // meilleure traduction a determiner selon usage
        'textes_formatspip' => 'ORIGINAL TEXTS SPIP FORMATED', // meilleure traduction a determiner selon usage
        'derniers_connectes' => 'Last logged in :', // derniers connectes dans le sens derniere personne connectees ???
-       'non_confirmes' => 'Unconfirmed :',
+       'non_confirmes' => 'Unconfirmed:',
        'stats_auteur' => '@icon@ @nom@, on&nbsp;@date@', // gare au format de la date en anglais !!!
        'attente_confirmation' => "Waiting for confirmation",
        // Slices
@@ -22,16 +22,16 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
        'page_precedente' => 'Previous page', 
        'page_debut' => 'First page',
        'page_fin' => 'Last page', 
-       'page_lien' => 'Page @page@ : @title@',
+       'page_lien' => 'Page @page@: @title@',
        // Smileys
-       'smileys_dispos' => 'Available smileys :', 
+       'smileys_dispos' => 'Available smileys:', 
        // Summary
-       'sommaire_titre' => 'Summary', 
+       'sommaire_titre' => 'Table of content', 
        'sommaire_page' => ',&nbsp;p@page@',
        'sommaire_page_long' => ',&nbsp;page&nbsp;@page@',
        // SPAM Fighting
        'lutte_spam' => 'Fighting against Spam',
-       'explique_spam' => 'Warning, you have used forbidden tags, words or characters.<br /><br />Please go backward and correct yoour entries !',
+       'explique_spam' => 'Warning, you have used forbidden tags, words or characters.<br /><br />Please go backward and correct your entries!',
 );
 
 ?>
