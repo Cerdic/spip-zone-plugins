3152aa0a8549ceb6dcecfeda9f54b669a33eb90c
berio@alg-a.org <berio@alg-a.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-03-07 15:49:47 +0000

    galician translation of the crayons
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@19015 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _stable_/crayons/lang/crayons_gl.php
@@ -0,0 +1,21 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP -- Este e un ficheiro de idioma de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	'annuler' => 'Cancelar',
+	'editer_@type@_@id@' => 'Editar @type@ @id@ completamente',
+    'deja_modifie' => 'Xa modificado',
+    'donnees_mal_formatees' => 'Datos f—ra de formato',
+    'editer' => 'Mudar',
+    'editer_tout' => 'Mudar todo',
+    'modifie_par_ailleurs' => 'Mudado por outra persoa',
+    'non_autorise' => 'Non autorizado',
+    'non_implemente' => 'Non implementado',
+    'pas_de_modification' => 'Sen modificaci—ns',
+    'pas_de_valeur' => 'Sen valor',
+    'sauvegarder' => '&iquest;Gardar as modificaci—ns?',
+    'svp_copier_coller' => 'O lamentamos copie/pegue os cambios e comeze de novo',
+    'veuillez_patienter'=>'Por favor, espere...'
+    );
+?>
