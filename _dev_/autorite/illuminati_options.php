043ea3a01aae5e39cb207251aa0be23a219ead0b
cam.lafit@azerttyu.net <cam.lafit@azerttyu.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-03-14 08:48:21 +0000

    Compléte le commit 19109 qui n'aurait pas du agir sur autorité Maintenant que c'est fait autant finir la bétise du commit. On déplace la fonction de controle 'analyse_droit_rapide'
    Ceci devrait aider à la mise en place d'un wiki sans identification sans pété crayons.
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@19110 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _dev_/autorite/illuminati_options.php
@@ -0,0 +1,22 @@
+<?php
+
+
+$GLOBALS['autorite'] = @unserialize($GLOBALS['meta']['autorite']);
+
+##
+## Dire aux crayons si les visiteurs anonymes ont des droits
+##
+if (($GLOBALS['autorite']['espace_wiki']
+AND $GLOBALS['autorite']['espace_wiki_anonyme']) OR (
+    $GLOBALS['autorite']['espace_wiki_motsclef'] 
+AND $GLOBALS['autorite']['espace_wiki_motsclef_anonyme'])) {
+	if (!function_exists('analyse_droits_rapide')) {
+	function analyse_droits_rapide() {
+		return true;
+	}
+	}
+	else
+		$autorite_erreurs[] = 'analyse_droits_rapide';
+}
+
+?>
