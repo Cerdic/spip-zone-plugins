ec223c11a13b8d7ff3620e1be45956de41c00949
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-03-15 21:24:57 +0000

    Un pipeline plus nécessaire et on change l'appel aux nouveaux js
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@19144 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/inscription2/inscription2_193/inscription2_ajouter_onglets.php
+++ _test_/inscription2/inscription2_193/inscription2_ajouter_onglets.php
@@ -4,49 +4,34 @@
 
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
-function Inscription2_ajouter_boutons($boutons_admin){
-
-	if ($GLOBALS['connect_statut'] == "0minirezo" && $GLOBALS["connect_toutes_rubriques"]) {
-
-		$boutons_admin['auteurs']->sousmenu['inscription2_adherents']= new Bouton(
-		"../"._DIR_PLUGIN_INSCRIPTION2."images/inscription2_icone.png", // icone
-		_T("inscription2:adherents") //titre
-		);
-	}
-	return $boutons_admin;
-}
-
 function Inscription2_header_prive($flux){
 	if (_request('exec')=='ajouter_adherent'){
 		$flux .= "<link rel='stylesheet' href='"._DIR_PLUGIN_INSCRIPTION2."css/inscription2_forms.css' type='text/css' media='all' />\n";
 		$flux .= "<link rel='stylesheet' href='".direction_css(_DIR_PLUGIN_INSCRIPTION2."css/inscription2.css")."' type='text/css' media='all' />\n";
-		$flux .= "<script type='text/javascript' src='"._DIR_PLUGIN_INSCRIPTION2."javascript/jquery.validate.js'></script>\n";
+		$flux .= "<script type='text/javascript' src='".find_in_path('lib/jquery-validate/jquery.validate.pack.js')."'></script>\n";
 		$flux .= "<script type='text/javascript' src='"._DIR_PLUGIN_INSCRIPTION2."javascript/md5_inscription2.js'></script>\n";
 	}
 	return $flux;
 }
 
 function inscription2_affichage_final($page){
-
-    // regarder si la page contient le formulaire inscription2
-    if (!strpos($page, 'id="inscription"'))
-        return $page;
-
+	// regarder si la page contient le formulaire inscription2
+	if (!strpos($page, 'id="inscription"'))
+		return $page;
 	$page = inscription2_preparer_page($page);
-    return $page;
+		return $page;
 }
 
 function inscription2_preparer_page($page) {
 
 	$css = find_in_path('css/inscription2_forms.css');
-	$jqueryvalidate = find_in_path('javascript/jquery.validate.js');
+	$jqueryvalidate = find_in_path('lib/jquery-validate/jquery.validate.pack.js');
 
-    $incHead = <<<EOS
+	$incHead = <<<EOS
 <script type='text/javascript' src='$jqueryvalidate'></script>
 <link rel="stylesheet" href="$css" type="text/css" media="all" />
 EOS;
 	return substr_replace($page, $incHead, strpos($page, '</head>'), 0);
-
 }
 				
 ?>
