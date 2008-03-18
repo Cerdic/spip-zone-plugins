f3b8cc8dd31db113642a9d34e060118b21ec7625
djakoni@gmail.com <djakoni@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-03-18 02:24:38 +0000

    Licence
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@19224 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/siou/ezSQL/exec/ezsql.php
+++ _stable_/siou/ezSQL/exec/ezsql.php
@@ -1,22 +1,23 @@
 <?php
 /*
- *      Copyright 2008 Ghislain VLAVONOU, Yannick EDAHE, Cedric PROTIERE
- *      
- *      This program is free software; you can redistribute it and/or modify
- *      it under the terms of the GNU General Public License as published by
- *      the Free Software Foundation; either version 3 of the License, or
- *      (at your option) any later version.
- *      
- *      This program is distributed in the hope that it will be useful,
- *      but WITHOUT ANY WARRANTY; without even the implied warranty of
- *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *      GNU General Public License for more details.
- *      
- *      You should have received a copy of the GNU General Public License
- *      along with this program; if not, write to the Free Software
- *      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- *      MA 02110-1301, USA.
- */
+    This file is part of ezSQL.
+
+    ezSQL is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    ezSQL is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with SIOU; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+    
+    Copyright 2007, 2008 - Ghislain VLAVONOU, Yannick EDAHE, Cedric PROTIERE
+*/
 include_spip('inc/presentation');
 include_spip('inc/config');
 include_spip('inc/charsets');
