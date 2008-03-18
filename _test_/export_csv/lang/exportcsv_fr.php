4e26d67c22e5a9ee0670f3758be4021dc94a77ab
esse@free.fr <esse@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-03-18 11:13:41 +0000

    Ajouté un répertoire à distance
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@19236 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _test_/export_csv/lang/exportcsv_fr.php
@@ -0,0 +1,91 @@
+<?php
+/*##############################################################
+ * ExportCSV
+ * Export des articles / rubriques SPIP en fichiers CSV.
+ *
+ * Auteur :
+ * Stéphanie De Nadaï
+ * webdesigneuse.net
+ * © 2008 - Distribué sous licence GNU/GPL
+ *
+##############################################################*/
+
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+// A
+'apercu_data' => 'Aper&ccedil;u des donn&eacute;es extraites',
+'aux_rub' => 'aux rubriques',
+'aux_art' => 'aux articles',
+'aide' => 'Aide en ligne',
+
+// B
+
+// C
+'config' => 'Configuration',
+
+// D
+'description_cfg' => 'Ici vous pouvez configurer le plugin Export C.S.V.',
+'del_cfg' => 'Tout effacer',
+
+// E
+'extract_data' => 'Extraire les donn&eacute;es',
+'extraction_data' => 'Extraction des donn&eacute;es',
+'extraction_data_back' => 'Aller &agrave; l\'extraction des donn&eacute;es',
+'explications' => '<p><strong>Seuls les articles publi&eacute;s sont export&eacute;s</strong></p>
+<ol class="exportcsv_ol"><li>Cliquer sur &laquo;&nbsp;T&eacute;l&eacute;charger les donne&eacute;s&nbsp;&raquo; ci-dessus</li>
+		<li>Enregistrer le fichier sur votre ordinateur</li>
+		<li>Lancer le tableur (OpenOffice ou Excel<sup>&reg;</sup>)</li>
+		<li>Ouvrir le fichier t&eacute;l&eacute;charg&eacute; 
+		<ul>
+			<li>Avec OpenOffice, choisir comme s&eacute;paration le <strong>point-virgule</strong> et le <strong>texte par des guillemets</strong></li>
+			<li>Avec Excel, choisir &laquo;&nbsp;Tous les fichiers&nbsp;&raquo; dans <em>Type de fichiers</em></li>
+		</ul></li>
+		</ol>',
+'elements_a_extract' => '&Eacute;l&eacute;ments &agrave; extraire',
+'erreur_lien_config' => '<p><a href="?exec=cfg&amp;cfg=exportcsv">Aller &agrave; la configuration</a></p>',
+'erreur_admin_config' => '<p>Contactez un administrateur pour la configuration</p>',
+'erreur_pas_de_config' => '<p>La configuration n\'est pas faite</p>',
+'erreur_pas_de_rub' => '<p>Aucune rubrique n\'a &eacute;t&eacute; s&eacute;lectionn&eacute;e</p>',
+'erreur_pas_de_champ' => '<p>Aucun &eacute;l&eacute;ment &agrave; afficher n\'a &eacute;t&eacute; s&eacute;lectionn&eacute;</p>',
+
+// F
+// G
+// H
+// I
+'info_config_rub' => 'S&eacute;lectionnez les groupes de mots-cl&eacute;s que vous voulez attribuer aux rubriques.<br /> ',
+'info_config_art' => 'S&eacute;lectionnez les groupes de mots-cl&eacute;s que vous voulez attribuer aux articles.<br /> ',
+
+// J K
+// L
+// M
+'mc_associes' => 'Mots-cl&eacute;s associ&eacute;s',
+'mot_clef' => 'Mots-cl&eacute;s',
+
+// N 
+// O 
+'ok_cfg' => 'Ok',
+// P // Q 
+
+// R
+'rub_a_extraire' => 'Articles &agrave; extraire depuis les rubriques',
+'reset_cfg' => 'R&eacute;tablir',
+
+// S
+'statut_publie' => 'Publi&eacute;e',
+'statut_prive' => 'Non publi&eacute;e',
+'signature' => '<p><strong>Export C.S.V</strong></p> 
+<p><small>Extraction des donn&eacute;es de SPIP pour tableur au format C.S.V</small>.</p>
+<p>Plugin r&eacute;alis&eacute; par <a href="http://www.webdesigneuse.net/">St&eacute;phanie&nbsp;De&nbsp;Nada&iuml;</a>.</p>',
+
+// T
+'titre_page' => 'Export CSV',
+'titre_gros_page' => 'Export CSV',
+'telecharger_data' => 'T&eacute;l&eacute;charger les donn&eacute;es',
+
+// U
+// V
+// W X Y Z
+'z' => 'z'
+);
+
+?>
