a98a8feb0a3318fc0cb9b0a7dddb50e059d10c55
gaitan@gmail.com <gaitan@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-03-18 11:17:46 +0000

    added spanish language dict and fixed a typo in english one
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@19242 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/coloration_code/lang/colorationcode_es.php
+++ _stable_/coloration_code/lang/colorationcode_es.php
@@ -2,7 +2,7 @@
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-	   'telecharger' => 'Download'
+   'telecharger' => 'Descargar'
 );
 
 ?>
