0b001b2d444412d5205fe9193885f2b8726ec48c
fil@rezo.net <fil@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-03-20 15:11:46 +0000

    langues supplementaires
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@19338 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _stable_/cfg/lang/cfg_ca.php
@@ -0,0 +1,54 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier sur la zone **
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cfg' => 'CFG',
+	'choisir_module_a_configurer' => 'Escolliu el m&ograve;dul a configurar.',
+	'config_enregistree' => 'Enregistrament de <b>@nom@</b> realitzat',
+	'config_supprimee' => 'Suppressi&oacute; de <b>@nom@</b> realitzat',
+	'configuration_modules' => 'Configuraci&oacute; dels m&ograve;duls',
+
+	// E
+	'erreur_enregistrement' => 'Hi ha hagut un error durant l\'enregistrament de <b>@nom@</b>',
+	'erreur_lecture' => 'Impossible llegir @nom@',
+	'erreur_open_w_fichier' => 'Impossible obrir el fitxer @fichier@ en escriptura',
+	'erreur_suppression' => 'Ha ocorregut un errror durant la supressi&oacute; de <b>@nom@</b>',
+	'extraarticles' => 'Exemple de camp extra en un article',
+
+	// I
+	'id_manquant' => 'Falta identificador ',
+
+	// N
+	'nom_table_manquant' => 'Falta el nom de la taula SQL',
+	'nouveau' => 'Nou',
+
+	// O
+	'ok' => 'OK',
+
+	// P
+	'pas_de_champs_dans' => 'No s\'ha trobat cap camp a @nom@',
+	'pas_de_changement' => 'Cap canvi a <b>@nom@</b>',
+
+	// R
+	'reset' => 'Esborrar',
+
+	// S
+	'supprimer' => 'Suprimir',
+
+	// T
+	'testauteur' => 'Exemple en un camp extra/cfg de la taula autor',
+	'testcasier' => 'Exemple de casella',
+	'testclassic' => 'Exemple objectiu cl&agrave;ssic',
+	'testinclure' => 'Exemple d\'inclusi&oacute; d\'esquelet',
+	'testmulti' => 'Exemple entrades m&uacute;ltiples',
+	'testphp' => 'Exemple d\'emmagatzematge php',
+	'testsimple' => 'Exemple simple',
+	'testtable' => 'Exemple sobre camps d\'una taula table sql'
+);
+
+?>
---
+++ _stable_/cfg/lang/cfg_fr_tu.php
@@ -0,0 +1,54 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier sur la zone **
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cfg' => 'CFG',
+	'choisir_module_a_configurer' => 'Choisis le module &agrave; configurer.',
+	'config_enregistree' => 'Enregistrement de <b>@nom@</b> effectu&eacute;',
+	'config_supprimee' => 'Suppression de <b>@nom@</b> effectu&eacute;',
+	'configuration_modules' => 'Configuration des modules',
+
+	// E
+	'erreur_enregistrement' => 'Une erreur est survenue lors de l\'enregistrement de <b>@nom@</b>',
+	'erreur_lecture' => 'Impossible de lire @nom@',
+	'erreur_open_w_fichier' => 'Impossible d\'ouvrir le fichier @fichier@ en &eacute;criture',
+	'erreur_suppression' => 'Une erreur est survenue lors de la suppression de <b>@nom@</b>',
+	'extraarticles' => 'Exemple de champ extra sur un article',
+
+	// I
+	'id_manquant' => 'Identifiant manquant',
+
+	// N
+	'nom_table_manquant' => 'Nom de la table SQL manquant',
+	'nouveau' => 'Nouveau',
+
+	// O
+	'ok' => 'OK',
+
+	// P
+	'pas_de_champs_dans' => 'Aucun champs trouv&eacute; dans @nom@',
+	'pas_de_changement' => 'Pas de changement dans <b>@nom@</b>',
+
+	// R
+	'reset' => 'Effacer',
+
+	// S
+	'supprimer' => 'Supprimer',
+
+	// T
+	'testauteur' => 'Exemple sur champ extra/cfg de la table auteur',
+	'testcasier' => 'Exemple de casier',
+	'testclassic' => 'Exemple m&eacute;ta classique',
+	'testinclure' => 'Exemple d\'inclusion de squelette',
+	'testmulti' => 'Exemple entr&eacute;es multiples',
+	'testphp' => 'Exemple de stockage php',
+	'testsimple' => 'Exemple simple',
+	'testtable' => 'Exemple sur champs d\'une table sql'
+);
+
+?>
