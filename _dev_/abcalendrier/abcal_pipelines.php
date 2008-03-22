f85e6bf2080dea34aaa5d622119aa5879d8d0f40
mmmxvvv@tiscali.it <mmmxvvv@tiscali.it@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-03-22 16:28:50 +0000

    new version in _test_
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@19402 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/abcalendrier/abcal_pipelines.php
+++
@@ -1,7 +0,0 @@
-<?php
-function abcalendrier_insert_head($flux){
-   $css_link="<link rel=\"stylesheet\" href=\""._DIR_PLUGIN_ABCALENDRIER."abcalendrier.css\" type=\"text/css\" media=\"projection, screen\" />\n";
-      $flux .=  "\n<!-- Debut header du ABCalendrier -->\n$css_link\n<!-- Fin header du ABCalendrier -->\n\n";
-	return $flux;
-}
-?>
