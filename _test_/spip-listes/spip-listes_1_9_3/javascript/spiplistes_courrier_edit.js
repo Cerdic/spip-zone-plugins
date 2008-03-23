26f2f44d218d17d768d0c317f7941e7a7481761b
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-03-23 23:13:45 +0000

    éviter de potentiels clash entre les libs
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@19456 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/spip-listes/spip-listes_1_9_3/javascript/spiplistes_courrier_edit.js
+++ _test_/spip-listes/spip-listes_1_9_3/javascript/spiplistes_courrier_edit.js
@@ -1,37 +1,37 @@
 // spiplistes_courrier_edit.js
-// utilisé par _SPIPLISTES_EXEC_COURRIER_EDIT
+// utilisÃ© par _SPIPLISTES_EXEC_COURRIER_EDIT
 
 // $LastChangedRevision: 15830 $
 // $LastChangedBy: paladin@quesaco.org $
 // $LastChangedDate: 2007-10-07 05:41:13 +0200 (dim., 07 oct. 2007) $
 
-$(document).ready(function(){
+jQuery(document).ready(function(){
 	
-	$("#ajax-loader").hide();
+	jQuery("#ajax-loader").hide();
 	
-	$("#ajax-loader").ajaxStart(function(){
-			$(this).show();
+	jQuery("#ajax-loader").ajaxStart(function(){
+			jQuery(this).show();
 	});
 		
-	$("#ajax-loader").ajaxStop(function(){
-		$(this).hide();
+	jQuery("#ajax-loader").ajaxStop(function(){
+		jQuery(this).hide();
 	});
 
 	// deux boutons de validation dans la page
-	// sélectionne soit une validation du contenu titre texte
-	// soit valide le contenu généré par prévisu
-	$("#formulaire_courrier_edit").submit(function(){
-		if($("#btn_courrier_edit").val() == "oui") {
+	// sÃ©lectionne soit une validation du contenu titre texte
+	// soit valide le contenu gÃ©nÃ©rÃ© par prÃ©visu
+	jQuery("#formulaire_courrier_edit").submit(function(){
+		if(jQuery("#btn_courrier_edit").val() == "oui") {
 		// c'est le bouton du bas courrier_edit qui valide
 			return (true);
 		}
 		else {
 		// c'est le bouton de previsu qui valide
-			var	 data = $('input,textarea,radio,select, checkbox', this).serialize();
-			$.ajax({ type: "POST", 
+			var data = jQuery('input,textarea,radio,select, checkbox', this).serialize();
+			jQuery.ajax({ type: "POST", 
 						url: "./?exec=spiplistes_courrier_previsu", 
 						data: data, 
-						success: function(msg){  $("#apercu-courrier").html(msg); }
+						success: function(msg){  jQuery("#apercu-courrier").html(msg); }
 				});
 			}
 		return (false);
