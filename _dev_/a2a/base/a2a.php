aa0fd26fbf5a3f01b06852777e9bdc4361418e73
brunobergot@gmail.com <brunobergot@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-03-24 19:24:41 +0000

    on renomme la table article_articles en articles_lies pour être plus clair
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@19497 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/a2a/base/a2a.php
+++ _dev_/a2a/base/a2a.php
@@ -9,28 +9,28 @@ global $table_des_tables;
 $GLOBALS['a2a_version_base'] = 0.1;
 
 
-$spip_article_articles = array(
+$spip_articles_lies = array(
 	"id_article"	=> "bigint(21) NOT NULL",
 	"id_article_lie"	=> "bigint(21) NOT NULL"
 	);
 	
-$spip_article_articles_key = array(
+$spip_articles_lies_key = array(
 	"PRIMARY KEY"		=> "id_article, id_article_lie"
 	);
 
-$spip_article_articles_join = array(
+$spip_articles_lies_join = array(
 	"id_article"		=> "id_article",
 	"id_article_lie"		=> "id_article_lie"
 	);
 
-$tables_principales['spip_article_articles'] = array(
-	'field' => &$spip_article_articles,
-	'key' => &$spip_article_articles_key,
-	'join' => &$spip_article_articles_join
+$tables_principales['spip_articles_lies'] = array(
+	'field' => &$spip_articles_lies,
+	'key' => &$spip_articles_lies_key,
+	'join' => &$spip_articles_lies_join
 	);
 
 /*global $table_des_tables;
-$table_des_tables['article_articles'] = 'articles_lies';
+$table_des_tables['articles_lies'] = 'articles_lies';
 
 global $tables_jointures;
 $tables_jointures['articles'][]= 'articles_lies';*/
