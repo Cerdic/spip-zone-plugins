4c13248f1c6e44ed3ea1bcbbe19854be03d4a0fe
brunobergot@gmail.com <brunobergot@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-03-24 19:27:47 +0000

    toujours : on renomme la table article_articles en articles_lies
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@19500 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/a2a/action/supprimer_lien_a2a.php
+++ _dev_/a2a/action/supprimer_lien_a2a.php
@@ -7,7 +7,7 @@ function action_supprimer_lien_a2a(){
 	$contexte['id_article_cible'] = _request('id_article_cible');
 	$contexte['id_article'] = _request('id_article');
 	
-	$sql = "DELETE FROM spip_article_articles WHERE id_article = '".$contexte['id_article']."' AND id_article_lie = '".$contexte['id_article_cible']."';";
+	$sql = "DELETE FROM spip_articles_lies WHERE id_article = '".$contexte['id_article']."' AND id_article_lie = '".$contexte['id_article_cible']."';";
 	$res = spip_query($sql);
 	$redirect = generer_url_ecrire("articles", "id_article=".$contexte['id_article'], "&");
 	redirige_par_entete($redirect);
