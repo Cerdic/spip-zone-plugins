1f3f3ab668aa94266d6de9b2e156e0a9db815dfb
brunobergot@gmail.com <brunobergot@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-03-24 19:26:20 +0000

    et encore : on renomme la table article_articles en articles_lies
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@19499 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/a2a/action/a2a_lier_article.php
+++ _dev_/a2a/action/a2a_lier_article.php
@@ -6,10 +6,10 @@ function action_a2a_lier_article(){
 	$contexte['id_article_cible'] = _request('id_article');
 	$contexte['id_article_source'] = _request('id_article_orig');
 	
-	$sql_test_liaison = "SELECT * FROM spip_article_articles WHERE id_artile='".$contexte['id_article_source']."' AND id_article_cible='".$contexte['id_article_cible']."';";
+	$sql_test_liaison = "SELECT * FROM spip_articles_lies WHERE id_artile='".$contexte['id_article_source']."' AND id_article_cible='".$contexte['id_article_cible']."';";
 	$res_test_liaison = spip_query($sql_test_liaison);
 	if (spip_num_rows($res_test_liaison) == 0){
-		$sql_liaison = "INSERT INTO spip_article_articles VALUES ('".$contexte['id_article_source']."', '".$contexte['id_article_cible']."');";
+		$sql_liaison = "INSERT INTO spip_articles_lies VALUES ('".$contexte['id_article_source']."', '".$contexte['id_article_cible']."');";
 		$res_liaison = spip_query($sql_liaison);
 	}
 	
