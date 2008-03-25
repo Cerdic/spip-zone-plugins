7b5a36a3e0e21041f29cb345a11f59a12c40438c
brunobergot@gmail.com <brunobergot@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-03-25 00:57:47 +0000

    une balise #ARTICLES_LIES qui renvoie une array des articles liés à l'articles en cours
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@19520 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/a2a/a2a_fonction.php
+++ _dev_/a2a/a2a_fonction.php
@@ -1,3 +1,17 @@
 <?php
 
+function lister_articles_lies($id_article){
+	$liste = '';
+	$result = spip_query("SELECT id_article_lie FROM spip_articles_lies WHERE id_article=$id_article");
+	while($row = spip_fetch_array($result)) {
+		$liste[] = $row['id_article_lie'];	
+	}
+	return $liste;
+}
+function balise_ARTICLES_LIES($p) {
+	$id_article = champ_sql('id_article', $p);
+	$p->code = "lister_articles_lies($id_article)";
+	$p->type = 'php';  
+	return $p;
+}
 ?>
