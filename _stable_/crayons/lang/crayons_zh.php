aa5ae9e88c52f82caa41a9f988189db080afbeb7
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-03-30 17:03:32 +0000

    langues (crayons)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@19657 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _stable_/crayons/lang/crayons_zh.php
@@ -0,0 +1,41 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'annuler' => '<MODIF>&#21462;&#28040;',
+
+	// D
+	'deja_modifie' => '<MODIF>&#24050;&#32463;&#20462;&#25913;',
+	'donnees_mal_formatees' => '<MODIF>&#25968;&#25454;&#26684;&#24335;&#21270;&#38169;&#35823;',
+
+	// E
+	'editer' => '<MODIF>&#32534;&#36753;',
+	'editer_@type@_@id@' => '<MODIF>&#32534;&#36753;@type@@id@&#25152;&#26377;&#20869;&#23481;',
+	'editer_tout' => '<MODIF>&#32534;&#36753;&#25152;&#26377;&#20869;&#23481;',
+
+	// M
+	'modifie_par_ailleurs' => '<MODIF>&#34987;&#21035;&#20154;&#20462;&#25913;',
+
+	// N
+	'non_autorise' => '<MODIF>&#27809;&#26377;&#25480;&#26435;',
+	'non_implemente' => '<MODIF>&#20026;&#35843;&#35797;&#20132;&#26426;',
+
+	// P
+	'pas_de_modification' => '<MODIF>&#26080;&#20462;&#25913;&#20869;&#23481;',
+	'pas_de_valeur' => '<MODIF>&#27809;&#26377;&#20540;',
+
+	// S
+	'sauvegarder' => '<MODIF>&#20445;&#23384;&#24050;&#20462;&#25913;&#30340;&#20869;&#23481;&#21527;&#65311;',
+	'svp_copier_coller' => '<MODIF>&#23545;&#19981;&#36215;&#65292;&#22797;&#21046;/&#31896;&#36148;&#24050;&#32463;&#25913;&#21464;&#30340;&#20869;&#23481;&#65292;&#24182;&#37325;&#26032;&#24320;&#22987;',
+
+	// V
+	'veuillez_patienter' => '<MODIF>&#35831;&#31245;&#21518;...'
+);
+
+?>
