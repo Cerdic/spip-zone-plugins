135211c880d8bf03c00e58ab1f1c51c0eeee116c
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-03-31 04:07:15 +0000

    langues (cfg)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@19675 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/cfg/lang/cfg_de.php
+++ _stable_/cfg/lang/cfg_de.php
@@ -1,43 +1,44 @@
 <?php
-
-/*
- * Plugin CFG pour SPIP
- * (c) toggg 2007, distribue sous licence GNU/GPL
- * Documentation et contact: http://www.spip-contrib.net/
- *
- */
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
 
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
+$GLOBALS[$GLOBALS['idx_lang']] = array(
 
-$GLOBALS['i18n_cfg_de'] = array(
+	// C
+	'cfg' => 'CFG',
+	'choisir_module_a_configurer' => 'W&auml;hlen Sie das zu konfigurierende Modul',
+	'configuration_modules' => 'Konfiguration der Module',
+	'config_enregistree' => 'Speicherung von <b>@nom@</b> erfolgreich',
+	'config_supprimee' => 'L&ouml;schen von <b>@nom@</b> erfolgreich',
 
-// boutons validation
-'ok' => 'OK',
-'reset' => 'Reset',
-'supprimer' => 'L&ouml;schen',
+	// E
+	'erreur_enregistrement' => 'Ein Fehler ist aufgetreten beim Speichern von <b>@nom@</b>',
+	'erreur_lecture' => 'Fehler beim Lesen von @nom@',
+	'erreur_open_w_fichier' => 'Fehler beim &Ouml;ffnen der Datei @fichier@ zum Schreiben',
+	'erreur_suppression' => 'Ein Fehler ist aufgetreten beim L&ouml;schen von <b>@nom@</b>',
 
-// textes cfg
-// C
-'cfg' => 'CFG',
-'choisir_module_a_configurer' => 'W&auml;hlen Sie das zu konfigurierende Modul',
-'configuration_modules' => 'Konfiguration der Module',
-'config_enregistree' => 'Speicherung von <b>@nom@</b> erfolgreich',
-'config_supprimee' => 'L&ouml;schen von <b>@nom@</b> erfolgreich',
-// E
-'erreur_lecture' => 'Fehler beim Lesen von @nom@',
-'erreur_enregistrement' => 'Ein Fehler ist aufgetreten beim Speichern von <b>@nom@</b>',
-'erreur_open_w_fichier' => 'Fehler beim &Ouml;ffnen der Datei @fichier@ zum Schreiben',
-'erreur_suppression' => 'Ein Fehler ist aufgetreten beim L&ouml;schen von <b>@nom@</b>',
-// I
-'id_manquant' => 'Fehlende ID',
-// N
-'nom_table_manquant' => 'Fehlender Name der SQL Tabelle',
-'nouveau' => 'Neu',
-// P
-'pas_de_changement' => 'Keine &Auml;nderung in <b>@nom@</b>',
-'pas_de_champs_dans' => 'Kein Feld in @nom@ gefunden'
+	// I
+	'id_manquant' => 'Fehlende ID',
 
+	// N
+	'nom_table_manquant' => 'Fehlender Name der SQL Tabelle',
+	'nouveau' => 'Neu',
+
+	// O
+	'ok' => 'OK',
+
+	// P
+	'pas_de_champs_dans' => 'Kein Feld in @nom@ gefunden',
+	'pas_de_changement' => 'Keine &Auml;nderung in <b>@nom@</b>',
+
+	// R
+	'reset' => 'Reset',
+
+	// S
+	'supprimer' => 'L&ouml;schen'
 );
 
 ?>
