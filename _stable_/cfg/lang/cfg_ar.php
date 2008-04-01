5c0cca326eecb85df48329d3b3ca5bad6e421bd4
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-04-01 09:03:17 +0000

    langues (cfg)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@19699 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _stable_/cfg/lang/cfg_ar.php
@@ -0,0 +1,55 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cfg' => '&#1605;&#1581;&#1585;&#1603; &#1575;&#1604;&#1575;&#1593;&#1583;&#1575;&#1583; (CFG)',
+	'choisir_module_a_configurer' => '&#1575;&#1582;&#1578;&#1585; &#1575;&#1604;&#1605;&#1604;&#1581;&#1602; &#1575;&#1604;&#1584;&#1610; &#1578;&#1585;&#1610;&#1583; &#1573;&#1593;&#1583;&#1575;&#1583;&#1607;',
+	'configuration_modules' => '&#1573;&#1593;&#1583;&#1575;&#1583; &#1575;&#1604;&#1605;&#1604;&#1575;&#1581;&#1602;',
+	'config_enregistree' => '&#1578;&#1605; &#1578;&#1587;&#1580;&#1610;&#1604; <b>@nom@</b>',
+	'config_supprimee' => '&#1578;&#1605; &#1581;&#1584;&#1601; <b>@nom@</b>',
+
+	// E
+	'erreur_enregistrement' => '&#1581;&#1589;&#1604; &#1582;&#1591;&#1571; &#1582;&#1604;&#1575;&#1604; &#1578;&#1587;&#1580;&#1610;&#1604; <b>@nom@</b>',
+	'erreur_lecture' => '&#1604;&#1575; &#1610;&#1605;&#1603;&#1606; &#1602;&#1585;&#1575;&#1569;&#1577; @nom@',
+	'erreur_open_w_fichier' => '&#1604;&#1575; &#1610;&#1605;&#1603;&#1606; &#1601;&#1578;&#1581; &#1575;&#1604;&#1605;&#1604;&#1601; @fichier@ &#1601;&#1610; &#1608;&#1590;&#1593;&#1610;&#1577; &#1575;&#1604;&#1603;&#1578;&#1575;&#1576;&#1577;',
+	'erreur_suppression' => '&#1581;&#1589;&#1604; &#1582;&#1591;&#1571; &#1604;&#1583;&#1609; &#1581;&#1584;&#1601; <b>@nom@</b>',
+	'extraarticles' => '&#1605;&#1579;&#1575;&#1604; &#1581;&#1602;&#1604; &#1571;&#1590;&#1575;&#1601;&#1610; &#1601;&#1610; &#1575;&#1604;&#1605;&#1602;&#1575;&#1604;',
+
+	// I
+	'id_manquant' => '&#1605;&#1593;&#1585;&#1617;&#1601; &#1594;&#1610;&#1585; &#1605;&#1608;&#1580;&#1608;&#1583;',
+
+	// N
+	'nom_table_manquant' => '&#1575;&#1587;&#1605; &#1580;&#1583;&#1608;&#1604; SQL &#1594;&#1610;&#1585; &#1605;&#1608;&#1580;&#1608;&#1583;',
+	'nouveau' => '&#1580;&#1583;&#1610;&#1583;',
+
+	// O
+	'ok' => '&#1605;&#1608;&#1575;&#1601;&#1602;',
+
+	// P
+	'pas_de_champs_dans' => '<NEW>Aucun champs trouv&eacute; dans @nom@',
+	'pas_de_changement' => '<NEW>Pas de changement dans <b>@nom@</b>',
+
+	// R
+	'reset' => '<NEW>Effacer',
+
+	// S
+	'supprimer' => '<NEW>Supprimer',
+
+	// T
+	'testauteur' => '<NEW>Exemple sur champ extra/cfg de la table auteur',
+	'testcasier' => '<NEW>Exemple de casier',
+	'testclassic' => '<NEW>Exemple m&eacute;ta classique',
+	'testinclure' => '<NEW>Exemple d\'inclusion de squelette',
+	'testmulti' => '<NEW>Exemple entr&eacute;es multiples',
+	'testphp' => '<NEW>Exemple de stockage php',
+	'testsimple' => '<NEW>Exemple simple',
+	'testtable' => '<NEW>Exemple sur champs d\'une table sql'
+);
+
+?>
