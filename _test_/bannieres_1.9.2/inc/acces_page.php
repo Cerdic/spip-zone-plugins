690ddefbe9e228aad7c6ee50f19ec7db3571be1d
fdm@ufe.be <fdm@ufe.be@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-04-01 18:09:49 +0000

    Fichier/répertoire effacé
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@19715 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/bannieres_1.9.2/inc/acces_page.php
+++
@@ -1,9 +0,0 @@
-<?php
-	global $connect_statut, $connect_toutes_rubriques;
-	if (!($connect_statut == '0minirezo' AND $connect_toutes_rubriques)) {
-		debut_page();
-		echo _T('avis_non_acces_page');
-		fin_page();
-		exit;
-	}
-?>
