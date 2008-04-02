7eb469d645f597cbeebc284b01431310db3d403c
cvallois@yahoo.fr <cvallois@yahoo.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-04-02 12:39:50 +0000

    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@19727 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _test_/image-cycle/lang/imagecycle_fr.php
@@ -0,0 +1,15 @@
+<?php
+
+	$GLOBALS[$GLOBALS['idx_lang']] = array(
+		'titre'				=>	'titre',
+		'legende'			=>	'configuration du plugin',
+		'largeur'			=>	'largeur de l\'image en pixel',
+		'hauteur'			=>	'hauteur de l\'image en pixel',
+		'largeurcadre'		=>	'largeur du cadre en pixel',
+		'hauteurcadre'		=>	'hauteur du cadre en pixel',
+		'couleur'			=>	'couleur du cadre',
+		'vitesse'			=>	'vitesse de transition en mms',
+		'duree'				=>	'dur&eacute;e d\'affichage de la photo en mms',
+		'effet'				=>	'effet d&eacute;sir&eacute;',
+);
+?>
