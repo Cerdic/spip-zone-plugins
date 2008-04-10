6d790305234de26bba03646192cd4857cd2da8f1
vlavonoudg@yahoo.fr <vlavonoudg@yahoo.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-04-10 10:40:51 +0000

    correction du bug genere par fputcsv
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@19880 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/ezSQL/exec/inc-traitements.php
+++ _stable_/ezSQL/exec/inc-traitements.php
@@ -191,4 +191,66 @@ function code2utf($num)
     return '';
 }
 
+/**
+* Fonction recuperer sur php.net pour realiser la meme fonction que str_ireplace
+*/
+if(!function_exists('str_ireplace')){
+  function str_ireplace($search,$replace,$subject){
+    $token = chr(1);
+    $haystack = strtolower($subject);
+    $needle = strtolower($search);
+    while (($pos=strpos($haystack,$needle))!==FALSE){
+      $subject = substr_replace($subject,$token,$pos,strlen($search));
+      $haystack = substr_replace($haystack,$token,$pos,strlen($search));
+    }
+    $subject = str_replace($token,$replace,$subject);
+    return $subject;
+  }
+
+/**
+* Fonction recuperer sur php.net pour realiser la meme fonction que fputcsv
+*/
+if (!function_exists('fputcsv'))
+{
+ 
+  function fputcsv(&$handle, $fields = array(), $delimiter = ';', $enclosure = '"')
+  {
+    $str = '';
+    $escape_char = '\\';
+    foreach ($fields as $value)
+    {
+      if (strpos($value, $delimiter) !== false ||
+          strpos($value, $enclosure) !== false ||
+          strpos($value, "\n") !== false ||
+          strpos($value, "\r") !== false ||
+          strpos($value, "\t") !== false ||
+          strpos($value, ' ') !== false)
+      {
+        $str2 = $enclosure;
+        $escaped = 0;
+        $len = strlen($value);
+        for ($i=0;$i<$len;$i++)
+        {
+          if ($value[$i] == $escape_char)
+            $escaped = 1;
+          else if (!$escaped && $value[$i] == $enclosure)
+            $str2 .= $enclosure;
+          else
+            $escaped = 0;
+          $str2 .= $value[$i];
+        }
+        $str2 .= $enclosure;
+        $str .= $str2.$delimiter;
+      }
+      else
+        $str .= $value.$delimiter;
+    }
+    $str = substr($str,0,-1);
+    $str .= "\n";
+    return fwrite($handle, $str);
+  }
+}
+
+}
+
 ?>
