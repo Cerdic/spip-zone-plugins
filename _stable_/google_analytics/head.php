39c1305ea23be90e8b17a00d9e55c90b99b619f0
yaquoi@yaquoi.com <yaquoi@yaquoi.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-04-11 10:42:08 +0000

    Mis a jour du nouveau script google analytics, ga.js remplace urchin.js
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@19903 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/google_analytics/head.php
+++ _stable_/google_analytics/head.php
@@ -1,5 +1,6 @@
 <?php
 
+
 function GoogleAnalytics_insert_head($flux){
 $id_google = lire_config('googleanalytics/idGoogle');
 if (!$id_google || $id_google == '_' || $id_google == 'UA-xxxxxx') {
@@ -8,11 +9,14 @@ if (!$id_google || $id_google == '_' || $id_google == 'UA-xxxxxx') {
 	else {
 
 $flux .= '
-<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src=\'" + gaJsHost + "google-analytics.com/ga.js\' type=\'text/javascript\'%3E%3C/script%3E"));
 </script>
 <script type="text/javascript">
-_uacct = "'.$id_google.'";
-urchinTracker();
+var pageTracker = _gat._getTracker("'.$id_google.'");
+pageTracker._initData();
+pageTracker._trackPageview();
 </script>';
 return $flux;
 }
