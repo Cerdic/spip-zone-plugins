a624f3950178952d9b71ffab3e8509233441dd43
cam.lafit@azerttyu.net <cam.lafit@azerttyu.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-04-11 14:25:26 +0000

    La gestion des administrateurs SVN se fait depuis l'espace privé
    Cela requiert cfg, mais n'est pas encore imposé
     
    
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@19907 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _stable_/svn_update/svn_update_options.php
@@ -0,0 +1,8 @@
+<?php
+
+    if (!defined("_ECRIRE_INC_VERSION")) return;    #securite
+
+    if (!defined(SVN_UPDATE_AUTEURS)) {
+        define('_SVN_UPDATE_AUTEURS', implode(";",lire_config('svn_update/administrateurs')));
+    }
+?>
