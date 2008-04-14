288eb5e84ebdd1667a2b5f098e809df1f608ae7e
chryjs@free.fr <chryjs@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-04-14 15:54:30 +0000

    * ajout du danois (merci a tom@mir.dk et a scoty)  * ajouts en espagnol (merci a philippebessiere@free.fr)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@19968 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/spipBB/lang/spipbb_es.php
+++ _dev_/spipBB/lang/spipbb_es.php
@@ -1,99 +1,164 @@
 <?php
-// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// extrait automatiquement de http://www.spip.net/trad-lang/
-// ** ne pas modifier le fichier **
+#---------------------------------------------------------#
+#  Plugin  : spipbb - Licence : GPL                       #
+#  File    : spipbb_en : language file                    #
+#  Authors : Chryjs, 2007 and als (please read the notes) #
+#  Contact : chryjs¡@!free¡.!fr                           #
+#---------------------------------------------------------#
 
-if (!defined("_ECRIRE_INC_VERSION")) return;
+// This is a SPIP-forum module file  --  Ceci est un fichier module de SPIP-forum
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-	// A
-	'aecrit' => '<MODIF>ha escrito&nbsp;',
-	'annonce' => 'Anuncio',
-	'anonyme' => 'An&oacute;nimo',
-	'avatar' => 'Avatar',
 
-	// C
-	'citer' => 'Citar',
-	'creer_forum' => 'Crear un nuevo foro',
+// A
+'aecrit' => 'ha escrito&nbsp;',
+'anonyme' => 'An&oacute;nimo',
+'annonce' => 'Anuncio',
+'auteur' => 'Autor',
+'avatar' => 'Avatar',
+'admsurveiller' => 'Vigilar este tema',
+'admclore' => 'Cerrar este tema',
+'admbouger' => 'Mover este tema',
+'admeffacer' => 'Suprimir este tema',
+'admseparer' => 'Dividir este tema',
+'admsurveiller_forum' => 'Vigilar este foro',
+'admclore_forum' => 'Cerrar este foro',
+'admbouger_forum' => 'Mover este foro',
+'admeffacer_forum' => 'Suprimir este foro',
+'admseparer_forum' => 'Dividir este foro',
+// B
+'bio' => 'Peque&ntilde;a biograf&iacute;a',
 
-	// D
-	'dans_forum' => 'en el foro',
-	'dernier' => '<MODIF>&nbsp;&Uacute;ltimo<br />',
+//D
+'date' => 'Inscripto el',
+'deconnexion_' => 'Desconecci&oacute;n',
+'dans_forum' => 'en el foro',
+'dernier' => '<MODIF>&nbsp;&Uacute;ltimo<br />',
 
-	// E
-	'ecrirea' => '<MODIF>Escribir un email a',
-	'effacer' => 'Borrar',
-	'email' => 'E-mail',
-	'etplus' => '... y m&aacute;s ...',
+//C
+'credits'=>'Realizado con <a href="http://www.spip-contrib.net/Plugin-SpipBB#contributeurs" class="copyright">SpipBB</a>',
+'categorie'=>'Categor&iacute;a',
+'chercher' => 'Buscar',
+'contact' => 'Contacto',
+'citer' => 'Citar',
+'connexion' => 'Por favor ingresa tu identificante y tu contrase&ntilde;a para conectrarte.',
+'creer_forum' => 'Crear un nuevo foro',
 
-	// F
-	'fil_deplace' => 'Desplazar este hilo',
-	'forum_ferme' => '<MODIF>Este foro est&aacute; desactivado',
-	'forum_ferme_texte' => '<MODIF>Este foro est&aacute; desactivado. No puedes enviar mensajes.',
-	'forum_ouvrir' => 'Abrir este Foro',
+//E
+'email' => 'E-mail',
+'envmp' => 'Enviar un mensaje privado',
+'effacer' => 'Borrar',
+'ecrirea' => '<MODIF>Escribir un email a',
+'etplus' => '... y m&aacute;s ...',
 
-	// H
-	'haut_page' => 'Inicio de la p&aacute;gina',
+//F
+'faq' => 'FAQ',
+'forum_ferme' => 'Este foro est&aacute; desactivado',
+'forum_ferme_texte' => 'Este foro est&aacute; desactivado. Ya no puedes contribuir.',
+'ferme' => 'Esta discusi&oacute;n est&aacute; cerrada',
+'fil_deplace' => 'Desplazar este hilo',
+'forum_ferme' => '<MODIF>Este foro est&aacute; desactivado',
+'forum_ferme_texte' => '<MODIF>Este foro est&aacute; desactivado. No puedes enviar mensajes.',
+'forum_ouvrir' => 'Abrir este Foro',
 
-	// I
-	'icone_ferme' => 'Cerrar',
-	'info' => 'Informaci&oacute;n',
-	'info_confirmer_passe' => 'Confirmar la contrase&ntilde;a nueva:',
-	'info_plus_cinq_car' => 'm&aacute;s de 5 caracteres',
+// H
+'haut_page' => 'Inicio de la p&aacute;gina',
 
-	// L
-	'le' => 'El',
-	'login' => 'Conexi&oacute;n',
+//I
+'icone_ferme' => 'Cerrar',
+'info' => 'Informaci&oacute;n',
+'info_confirmer_passe' => 'Confirmar la contrase&ntilde;a nueva:',
+'info_plus_cinq_car' => 'm&aacute;s de 5 caracteres',
 
-	// M
-	'maintenance' => '<MODIF>No olvides quitar<br />"Cerrado por mantenimiento",<br />de los Foros afectados.',
-	'messages' => '<MODIF>Mensajes',
-	'messages_derniers' => '&Uacute;ltimos mensajes',
-	'messages_voir_dernier' => 'Ver el &uacute;ltimo mensaje',
-	'moderateurs' => 'Moderador(es)',
+//L
+'logout' => 'Desconecci&oacute;n',
+'lieu' => 'Localizaci&oacute;n',
+'liste_inscrits' => 'Lista de miembros',
+'luiecrire' => 'Escribirle',
+'le' => 'El',
+'login' => 'Conexi&oacute;n',
 
-	// N
-	'no_message' => 'No hay ning&uacute;n asunto o mensaje que corresponda con tus criterios de b&uacute;squeda',
+//M
+'membres_liste' => 'Lista de miembros',
+'membres_groupes' => 'Groupo de usuarios/as',
+'membres_profil' => 'Perfil',
+'message_date' => 'Publicado el ',
+'message_sujet' => 'Asunto ',
+'messages' => 'Mensajes',
+'message_s' => 'Mensajes',
+'messages_derniers' => '&Uacute;ltimos mensajes',
+'messages_nouveaux' => 'Nuevos mensajes',
+'messages_pas_nouveaux' => 'No hay nuevos mensajes',
+'messages_non_lus_grand' => 'No hay nuevos mensajes',
+'messages_repondre' => 'Nueva respuesta',
+'messages_voir_dernier' => 'Ver el &uacute;ltimo mensaje',
+'messages_voir_dernier_s' => 'Ver los nuevos mensajes',
+'moderateurs' => 'Moderador(es)/a(s)',
+'mp' => 'mensaje privado',
+'maintenance' => '<MODIF>No olvides quitar<br />"Cerrado por mantenimiento",<br />de los Foros afectados.',
+'messages_derniers' => '&Uacute;ltimos mensajes',
 
-	// P
-	'poste_valide' => 'Mensaje(s) por validar ...',
-	'profil' => '<MODIF>Perfil de',
+//n
+'nom' => 'Nombre de usuario/a',
+'nouveaux_messages' => 'Nuevos mensajes',
+'no_message' => 'No hay ning&uacute;n asunto o mensaje que corresponda con tus criterios de b&uacute;squeda',
 
-	// R
-	'recherche_elargie' => 'B&uacute;squeda extendida',
-	'reglement' => '<MODIF><p>Los administradores y moderadores de este foro se esforzar&aacute;n en suprimir o editar todos los mensajes de car&aacute;cter reprensible tan r&aacute;pidamente como sea posible. De cualquier modo, les resulta imposible revisar todos los mensajes. Por tanto, admites que todos los mensajes publicados en esto foros expresan el punto de vista y la opini&oacute;n de sus autores respectivos, y no la de los administradores, o moderadores, ou webmasters (excepto los mensages publicados por ellos mismos) y por consiguiente no pueden ser tomados como responsables de ellos.</p>
+//P
+'profil' => 'Perfil de',
+'paramprofil' => 'Modificar tus par&aacute;metros',
+'poste_valide' => 'Mensaje(s) por validar ...',
+
+//R
+'recherch' => 'B&uacute;squeda',
+'recherche_elargie' => 'B&uacute;squeda extendida',
+'revenir_haut' => 'Volver arriba de la p&aacute;gina',
+'reponse' => 'En respuesta al mensaje',
+'reponses' => 'Respuestas',
+'recherche_texte'=> 'La b&uacute;squeda se efectua en los asuntos y el cuerpo de los mensajes. No hay operadores booleanos, el operador l&oacute;gico impl&iacute; es m&aacute;s o menos un \'O\' l&oacute;gico. Se ignoran las palabras de tres letras o menos.',
+'recherche_titre' => 'Buscar un asunto',
+'recherche_auteur' => 'B&uacute;squeda por autor',
+'recherche_options' => 'Opciones de b&uacute;squeda',
+'reglement' => '<MODIF><p>Los administradores y moderadores de este foro se esforzar&aacute;n en suprimir o editar todos los mensajes de car&aacute;cter reprensible tan r&aacute;pidamente como sea posible. De cualquier modo, les resulta imposible revisar todos los mensajes. Por tanto, admites que todos los mensajes publicados en esto foros expresan el punto de vista y la opini&oacute;n de sus autores respectivos, y no la de los administradores, o moderadores, ou webmasters (excepto los mensages publicados por ellos mismos) y por consiguiente no pueden ser tomados como responsables de ellos.</p>
 
 <p>Consientes en no publicar mensajes injuriosos, obscenos, vulgares, difamatorios, amenazadores, sexuales o cualquier otro mensaje que viole las leyes aplicables. Hacerlo puede llevarte a ser bloqueado de forma inmediata y permanente (y se informar&aacute; a tu proveedor de acceso a internet). La direcci&oacute;n IP de cada mensaje queda registrada con el fin de ayudar a que se respeten estas condiciones. Quedas de acuerdo con el hecho de que el webmaster, el administrador y los moderadores de este foro tienen derecho a suprimir, editar, desplazar o cerrar cualquier asunto en discusi&oacute;n en todo momento. Como usuario, est&aacute;s de acuerdo con el hecho de que toda las informaci&oacute;n que facilites a continuaci&oacute;n se guardar&aacute; en una base de datos. No obstante, esa informaci&oacute;n no se divulgar&aacute; a ninguna tercera persona o sociedad sin tu acuerdo. El webmaster, el administrador, y los moderadores no se podr&aacute;n tener como responsables si un intento de pirateo inform&aacute;tico permite el acceso a estos datos.</p>
 
 <p>Este foro utiliza las cookies para guardar informaci&oacute;n sobre tu ordenador. Esas cookies no contendr&aacute;n ninguna informaci&oacute;n de la que introduzcas a continuaci&oacute;n, sirven &uacute;nicamente para mejorar la comodidad de uso. La direcci&oacute;n de e-mail &uacute;nicamente se utiliza para confirmar los detalles de tu registro al igual que tu contrase&ntilde;a (y tambi&eacute;n para enviarte una contrase&ntilde;a nueva en caso de que la olvides).</p><p>Al registrarte, te haces responsable del cumplimiento del reglamento anterior.</p>',
-	'repondre' => 'Responder',
-	'retour_forum' => 'Volver a la entrada del foro',
+'repondre' => 'Responder',
+'retour_forum' => 'Volver a la entrada del foro',
 
-	// S
-	'secteur_forum' => '<MODIF>SECTORES FORO',
-	'selection_efface' => '<MODIF>Borrar la selecci&oacute;n ...',
-	'sinscrire' => '<MODIF>registrarse',
-	'statut' => 'Estado',
-	'sujet' => '<MODIF>Asunto&nbsp;',
-	'sujets' => 'Asuntos',
-	'sujets_aucun' => 'De momento no se ha tratado ning&uacute;n asunto en este foro',
-	'sujet_auteur' => 'Autor',
-	'sujet_clos_texte' => 'Este asunto est&aacute; cerrado, no puedes escribir aqu&iacute;.',
-	'sujet_clos_titre' => 'Asunto Cerrado',
-	'sujet_nombre' => 'N&uacute;mero de asuntos',
-	'sujet_nouveau' => 'Asunto nuevo',
-	'sujet_rejete' => 'Asunto rechazado',
-	'sujet_valide' => 'Asunto por validar',
 
-	// T
-	'tous_forums' => 'Todos los foros',
+//S
+'site' => 'Sitio web',
+'sujet_auteur' => 'Autor',
+'sujet_repondre' => 'Nuevo tema',
+'sujet_visites' => 'Visitas',
+'sujets' => 'Asuntos',
+'sujet_s' => 'Asuntos',
+'sujet_clos_titre' => 'Asunto Cerrado',
+'sujet_clos_texte' => 'Este asunto est&aacute; cerrado, no puedes escribir aqu&iacute;.',
+'statut' => 'Estatuto',
+'secteur_forum' => '<MODIF>SECTORES FORO',
+'selection_efface' => '<MODIF>Borrar la selecci&oacute;n ...',
+'sinscrire' => '<MODIF>registrarse',
+'statut' => 'Estado',
+'sujet' => '<MODIF>Asunto&nbsp;',
+'sujets_aucun' => 'De momento no se ha tratado ning&uacute;n asunto en este foro',
+'sujet_nombre' => 'N&uacute;mero de asuntos',
+'sujet_nouveau' => 'Asunto nuevo',
+'sujet_rejete' => 'Asunto rechazado',
+'sujet_valide' => 'Asunto por validar',
 
-	// V
-	'voir' => 'VER',
 
-	// W
-	'www' => 'WWW'
+//T
+'tous_forums' => 'Todos los foros',
+'toutes_categories' => 'Todas las categor&iacute;as',
+'tout_apropos' => 'Todo acerca de',
+
+//V
+'voirprofil' => 'Ver el perfil de',
+'voir' => 'VER',
+
 );
 
 ?>
