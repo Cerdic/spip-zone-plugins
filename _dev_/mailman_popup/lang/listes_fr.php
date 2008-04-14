b3d2441b2e12e40f99d1997b92a6619a3213fd19
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-04-14 12:41:34 +0000

    langues (listes)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@19954 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/mailman_popup/lang/listes_fr.php
+++ _dev_/mailman_popup/lang/listes_fr.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier sur svn://zone.spip.org/spip-zone/_plugins_/_dev_/mailman_popup
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/_dev_/mailman_popup/lang/
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
