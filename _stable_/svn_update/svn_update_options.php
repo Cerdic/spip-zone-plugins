f039b4e9fd8368f85ffc3e253567a10ea5c67923
cam.lafit@azerttyu.net <cam.lafit@azerttyu.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-04-14 10:05:19 +0000

    coquille dans l'admin par defaut c'est l'user 1 et pas celui qui a servit a tester le code ....
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@19944 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/svn_update/svn_update_options.php
+++ _stable_/svn_update/svn_update_options.php
@@ -3,6 +3,6 @@
     if (!defined("_ECRIRE_INC_VERSION")) return;    #securite
 
     if (!defined(SVN_UPDATE_AUTEURS)) {
-        define('_SVN_UPDATE_AUTEURS', implode(";",lire_config('svn_update/administrateurs',array('35'))));
+        define('_SVN_UPDATE_AUTEURS', implode(";",lire_config('svn_update/administrateurs',array('1'))));
     }
 ?>
