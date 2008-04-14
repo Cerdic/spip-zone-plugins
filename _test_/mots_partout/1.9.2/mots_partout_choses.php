472e26b264cd72c4f1eb353cd003757124632b44
olivier.gendrin@free.fr <olivier.gendrin@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-04-14 14:26:10 +0000

    Déclarer automatiquement la nouvelle table à SPIP...
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@19963 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/mots_partout/1.9.2/mots_partout_choses.php
+++ _test_/mots_partout/1.9.2/mots_partout_choses.php
@@ -4,6 +4,19 @@
 /***********************************************************************/
 
 //==========================ARTICLES============================================
+global $tables_principales;
+$tables_principales['spip_mots_documents']['field'] = array(
+        "id_mot"    => "BIGINT (21) DEFAULT '0' NOT NULL",
+        "id_document"    => "BIGINT (21) DEFAULT '0' NOT NULL");
+
+$tables_principales['spip_mots_documents']['key'] = array(
+        "KEY id_mot"    => "id_mot",
+        "KEY id_document"    => "id_document");
+
+global $tables_relations;
+$tables_relations['mots']['id_document'] = 'mots_documents';
+$tables_relations['documents']['id_mot'] = 'mots_documents';
+
 global $choses_possibles,$statuts_possibles;
 $statuts_possibles[]='publie';
 $statuts_possibles[]='propose';
