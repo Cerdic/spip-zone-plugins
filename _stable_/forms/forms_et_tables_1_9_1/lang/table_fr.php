2e2b303243aac2975e41a228652abf1b46cb875d
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-04-15 22:08:57 +0000

    langues (table)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@20009 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/forms/forms_et_tables_1_9_1/lang/table_fr.php
+++ _stable_/forms/forms_et_tables_1_9_1/lang/table_fr.php
@@ -1,45 +1,53 @@
 <?php
-
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/_stable_/forms/forms_et_tables_1_9_1/lang/
+if (!defined("_ECRIRE_INC_VERSION")) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// page tables_tous
-'icone_creer_table' => "Cr&eacute;er une nouvelle table",
-'type_des_tables' => 'Tables',
-'toutes_tables' => "Toutes les tables",
+	// A
+	'articles_utilisant' => 'Articles utilisant cette table',
+	'aucune_reponse' => 'Aucune donn&eacute;e',
 
-// page donnees_tous
-'icone_ajouter_donnees' => "Ajouter des donn&eacute;es",
-'telecharger_reponses' => "T&eacute;l&eacute;chargez les donn&eacute;es",
-'importer_donnees_csv' => "Importer des donn&eacute;es",
+	// C
+	'champs_formulaire' => 'Champs de la table',
 
-// page donnees_edit
-'lien_retirer_donnee_liee' => "Retirer cette donn&eacute;e",
-'lien_retirer_donnee_liante' => "Retirer le lien depuis cette donn&eacute;e",
-'articles_utilisant'=> "Articles utilisant cette table",
-'suivi_reponses' => "Voir les donn&eacute;es",
-'aucune_reponse' => "Aucune donn&eacute;e",
-'une_reponse' => "Une donn&eacute;e",
-'nombre_reponses' => "@nombre@ donn&eacute;es",
-'texte_donnee_statut' => "Statut de cette donn&eacute;e",
-'texte_statut_prepa' => "Donn&eacute;e en cours de r&eacute;daction",
-'texte_statut_prop' => "Donn&eacute;e propos&eacute;e",
-'texte_statut_publie' => "Donn&eacute;e publi&eacute;e",
-'texte_statut_poubelle' => "Donn&eacute;e supprim&eacute;e",
-'texte_statut_refuse' => "Donn&eacute;e refus&eacute;e",
+	// F
+	'formulaire' => 'Table',
 
-// page forms_edit
-'champs_formulaire'=>"Champs de la table",
-'info_supprimer_formulaire' => "Voulez-vous vraiment supprimer cette table ?",
-'info_supprimer_formulaire_reponses' => "Des donn&eacute;es sont associ&eacute;es &agrave; cette table. Voulez-vous vraiment la supprimer ?",
+	// I
+	'icone_ajouter_donnees' => 'Ajouter des donn&eacute;es',
+	'icone_creer_table' => 'Cr&eacute;er une nouvelle table',
+	'importer_donnees_csv' => 'Importer des donn&eacute;es',
+	'info_supprimer_formulaire' => 'Voulez-vous vraiment supprimer cette table ?',
+	'info_supprimer_formulaire_reponses' => 'Des donn&eacute;es sont associ&eacute;es &agrave; cette table. Voulez-vous vraiment la supprimer ?',
 
-'formulaire' => "Table",
-'nouveau_formulaire' => "Nouvelle table",
-'supprimer_formulaire' => "Supprimer cette table",
-'titre_formulaire' => "Titre de la table",
+	// L
+	'lien_retirer_donnee_liante' => 'Retirer le lien depuis cette donn&eacute;e',
+	'lien_retirer_donnee_liee' => 'Retirer cette donn&eacute;e',
 
+	// N
+	'nombre_reponses' => '@nombre@ donn&eacute;es',
+	'nouveau_formulaire' => 'Nouvelle table',
+
+	// S
+	'suivi_reponses' => 'Voir les donn&eacute;es',
+	'supprimer_formulaire' => 'Supprimer cette table',
+
+	// T
+	'telecharger_reponses' => 'T&eacute;l&eacute;chargez les donn&eacute;es',
+	'texte_donnee_statut' => 'Statut de cette donn&eacute;e',
+	'texte_statut_poubelle' => 'Donn&eacute;e supprim&eacute;e',
+	'texte_statut_prepa' => 'Donn&eacute;e en cours de r&eacute;daction',
+	'texte_statut_prop' => 'Donn&eacute;e propos&eacute;e',
+	'texte_statut_publie' => 'Donn&eacute;e publi&eacute;e',
+	'texte_statut_refuse' => 'Donn&eacute;e refus&eacute;e',
+	'titre_formulaire' => 'Titre de la table',
+	'toutes_tables' => 'Toutes les tables',
+	'type_des_tables' => 'Tables',
+
+	// U
+	'une_reponse' => 'Une donn&eacute;e'
 );
-
 
 ?>
