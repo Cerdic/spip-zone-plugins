061ddf41195b805117623b4e3e351a3df8f61374
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-04-15 13:53:34 +0000

    langues (couteau)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@19987 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _stable_/couteau_suisse/lang/couteau_ro.php
@@ -0,0 +1,49 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'attente_confirmation' => '&Icirc;n a&#351;teptarea unei confirm&#259;ri',
+
+	// D
+	'derniers_connectes' => 'Ultimii conecta&#355;i:',
+
+	// E
+	'explique_spam' => 'Aten&#355;ie: a&#355;i folosit balize, caractere sau cuvinte interzise. &Icirc;ntoarce&#355;i-v&#259; pentru corectare !',
+
+	// L
+	'lutte_spam' => 'Lupta &icirc;mpotriva SPAM-ului',
+
+	// N
+	'nom_forum' => 'V&#259; rug&#259;m s&#259; specifica&#355;i numele dumneavoastr&#259; !',
+	'non_confirmes' => 'Neconfirmate:',
+
+	// P
+	'page_debut' => 'Prima pagin&#259;',
+	'page_fin' => 'Ultima pagin&#259;',
+	'page_lien' => 'Pagina @page@ : @title@',
+	'page_precedente' => 'Pagina anterioar&#259;',
+	'page_suivante' => 'Pagina urm&#259;toare',
+
+	// S
+	'smileys_dispos' => 'Emoticon-uri disponibile',
+	'sommaire_page' => ',&nbsp;p@page@',
+	'sommaire_page_long' => ',&nbsp;page&nbsp;@page@',
+	'sommaire_titre' => 'Sumar',
+	'stats_auteur' => '@icon@ @nom@, &nbsp;@date@',
+
+	// T
+	'textes_formatspip' => 'TEXTE ORIGINALE &Icirc;N FORMAT SPIP',
+	'texte_formatspip' => 'TEXT ORIGINAL &Icirc;N FORMAT SPIP',
+	'titre' => 'Cu&#355;itul Elve&#355;ian',
+
+	// V
+	'variable_vide' => '(Gol)'
+);
+
+?>
