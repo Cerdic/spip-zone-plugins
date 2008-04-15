bf9f84b514fe0c8cd4f6d9491183beb212fc19f6
apachot@openstudio.fr <apachot@openstudio.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-04-15 06:37:42 +0000

    version 3 du plugin qui permettra de gérer des visites virtuelles depuis l'espace privé, avec interactions entre les vues pour se déplacer d'une pièce à l'autre. pour se donner une idée du shmilblick : http://www.openstudio.fr/jquery-virtual-tour
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@19977 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _stable_/panoramas/panoramas_define_head.php
@@ -0,0 +1,10 @@
+<?php
+function panoramas_insertion_in_head($flux)
+{
+	return $flux."<link rel=\"stylesheet\" type=\"text/css\" href=\""._DIR_PLUGIN_PANORAMAS."css/jquery.panorama.css\" />
+	<script type=\"text/javascript\" src=\""._DIR_PLUGIN_PANORAMAS."js/jquery.panorama.js\"></script>";
+}
+		
+
+
+?>
