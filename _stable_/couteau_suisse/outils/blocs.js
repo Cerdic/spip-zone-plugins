24421d20fdfaf7d9f22e0e8b41908bc4cd912321
patfr@ifrance.com <patfr@ifrance.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-04-19 00:39:26 +0000

    Blocs dépliables : une fois qu'un bloc Ajax est en place, plus  besoin de le recharger ensuite (cette fois, ça marche...)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@20069 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/couteau_suisse/outils/blocs.js
+++ _stable_/couteau_suisse/outils/blocs.js
@@ -1,24 +1,32 @@
- /* if (window.jQuery) */
- jQuery(function(){
-	// clic sur un outil
-	jQuery('.blocs_resume').prev().toggleClass('blocs_click').click(function()	{
+if (window.jQuery) jQuery(function(){
+	// si un resume est present...
+	jQuery('div.blocs_resume').prev().toggleClass('blocs_click').click(function(){
 		jQuery(this).toggleClass('blocs_replie')
 		.next().toggleClass('blocs_invisible')
 		.next().toggleClass('blocs_invisible');
+		// annulation du clic
 		return false;
 		});
-		
-	 jQuery('h4.blocs_click').click( function() {
+
+	// sinon...
+	jQuery('h4.blocs_click').click( function(){
 		jQuery(this).toggleClass('blocs_replie')
 		.next().toggleClass('blocs_invisible');
 		// annulation du clic
 		return false;
 		});
-		
+
+	// si un bloc ajax est present...
 	jQuery('h4.blocs_ajax').click(function(){
 		var k=jQuery(this).children().attr("href");
-		jQuery(this).removeClass('blocs_ajax')
-		.parent().children(".blocs_destination").load(k);	
+		if(k=='javascript:;') return false;
+		// une fois le bloc ajax en place, plus besoin de le recharger ensuite
+		jQuery(this).children().attr("href", 'javascript:;');
+		// on charge !
+		jQuery(this).parent().children(".blocs_destination")
+//.animeajax()
+		.load(k);
+		return false;
 		});
 });
 
