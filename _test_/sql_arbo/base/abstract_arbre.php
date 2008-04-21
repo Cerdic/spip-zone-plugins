c3e92f13cbfa8ce819f3008126c57257248dac6d
cam.lafit@azerttyu.net <cam.lafit@azerttyu.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-04-21 11:06:28 +0000

    Test d'une api de gestion d'arbre à la sauce intervallaire.
    Prérequis : Creer une table dans la base spip à triturer, 
    
    Vous pouvez essayer sur une table existante comme spip_rubriques, mais il faudra modifier la requete d'insertion dans la fonction exec_sql_arbo
    
    Fonctionnement :
    Activé le plugin et accéder à la page ?exec=sql_arbo (attention aucune sécurité, donc à n'installer que sur de la dev/test)
    
    La table de test pour la quelle ?exec=sql_arbo est configurée est celle déclarée comme table d'autojointure sur http://sql.developpez.com/arborescence/
    
    Le plugin pour le moment ne fait que peu de chose :
    - conversion de la table de type autojointure en table intervallaire
    - extrait les elements feuilles 
    - insertion d'une feuille Roller
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@20100 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _test_/sql_arbo/base/abstract_arbre.php
@@ -0,0 +1,203 @@
+<?php
+/*! \file abstract_arbre.php
+ *  \brief Abstraction pour manipuler des tables en tant qu'arbres  intervallaire
+ *
+ *  Il est possible de :
+ *      -* créer une table de type autojointure en table intervallaire
+ *      -* de rechercher des feuilles et noeud
+ */
+ 
+ 
+/*! \brief Convertir une table de type autojointure en intervallaire
+ *
+ *  Formate la table donnée en argument en ajoutant les champs requis et parcours l'arbre afin de le remplir correctement
+ *  Il n'y a pas de notion d'ordre dans une fratrie, les noeuds sont remplis dans l'ordre de lecture de la base
+ *
+ *  \param $table table à convertir, nom de table complet
+ *  \param $champ_noeud nom du champ contenant l'index de table en général id_objet
+ *  \param $champ_parent nom du champs contenant l'index de l'element parent
+ *
+ *  \return booleen retourne un controle sur le nombre d'elements traités
+ */
+function sql_arbre_convertir($table, $champ_noeud, $champ_parent) {
+    //création des champs necessaires
+    sql_alter('TABLE '.$table.' ADD bord_gauche INT');
+    sql_alter('TABLE '.$table.' ADD bord_droit INT');
+        
+    //formatage intervallaire
+    $bord_gauche = sql_arbre_convertir_noeud(1,0,'spip_autojointure','FAM_ID','FAM_PERE');
+
+    //controle la conversion
+    //bord_gauche = 2 * nb d'element dans la table + 1
+    $nb_element = sql_countsel(
+        $table
+    );
+    
+    if ($nb_element * 2 == $bord_gauche - 1) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+
+
+/*! \brief Defini les bords d'un noeud et de sa descendande
+ *
+ *
+ *  \param $bord_gauche valeur du premier bord gauche à donner
+ *  \param $id_noeud identifiant du noeud à traiter
+ *  \param $table
+ *  \param $champ_noeud
+ *  \param $champ_parent
+ *
+ *  \return int retourne la valeur du prochain bord gauche à donner, pour le noeud frere par exemple
+ */
+function sql_arbre_convertir_noeud($bord_gauche,$id_noeud,$table,$champ_noeud,$champ_parent) {
+    
+    //on sauve le bord gauche du noeud en cours
+    $bord_gauche_noeud = $bord_gauche;
+    
+    //le prochain bord gauche 
+    $bord_gauche ++;
+
+    //recherche des enfants immédiats
+    $ressource = sql_select(
+        $champ_noeud,
+        $table,
+        "$champ_parent = $id_noeud"
+    );
+      
+    //parcours les enfants
+    while($fils = sql_fetch($ressource)) {
+        $id_fils = $fils[$champ_noeud];
+        $bord_gauche = sql_arbre_convertir_noeud($bord_gauche,$id_fils,$table,$champ_noeud,$champ_parent);
+    }
+    
+    //insert les bordures du du noeud
+    sql_updateq(
+        $table,
+        array(
+            'bord_gauche' => $bord_gauche_noeud,
+            'bord_droit' => $bord_gauche
+        ),
+        "$champ_noeud = $id_noeud"
+    );
+    
+    //le prochain bord_gauche
+    $bord_gauche ++;
+    
+    return $bord_gauche;
+}
+/*
+function sql_select (
+	$select = array(), $from = array(), $where = array(),
+	$groupby = array(), $orderby = array(), $limit = '', $having = array(),
+	$serveur='', $option=true) {
+*/
+
+/*! \brief Obtenir toutes les feuilles
+ *
+ *  Retourne une ressource qui contient tous les elements finaux d'un arbre
+ *
+ *  \param $from table à traiter
+ *  \param $serveur serveur sollicité
+ *  \param $option peut avoir 3 valeurs 
+ *      - true -> executer la requete, 
+ *      - false -> ne pas l'executer mais la retourner, 
+ *      - 'continue' -> ne pas echouer en cas de serveur sql indisponible
+ *
+ *  \return ressource une ressource à traiter par un sql_fetch
+ */
+function sql_arbre_get_feuilles($table ='', $serveur='', $option=true) {
+
+    return sql_select(
+        "*",
+        $table,
+        "bord_droit - bord_gauche = 1",
+        '',
+        '',
+        '',
+        '',
+        $serveur,
+        $option        
+    );
+}
+
+
+/*! \brief Inserer une feuille
+ *
+ *  Fait un sql_insertq dans l'abre
+ *
+ *  \param $table table à traiter
+ *  \param $serveur serveur sollicité
+ *  \param $option peut avoir 3 valeurs 
+ *      - true -> executer la requete, 
+ *      - false -> ne pas l'executer mais la retourner, 
+ *      - 'continue' -> ne pas echouer en cas de serveur sql indisponible
+ *
+ *  \return ressource une ressource à traiter par un sql_fetch
+ */
+function sql_arbre_set_feuille($table ='',$parent = array(), $couples= array(),  $serveur='', $option=true) {
+
+    $champ_parent = $parent['champ'];    
+    $id_parent = $parent['id'];
+
+    //recherche les bords du parent
+    $bordures = sql_fetsel(
+        array(
+            'bord_gauche',
+            'bord_droit'
+        ),
+        $table,
+        $parent,
+        '',
+        '',
+        '',
+        '',
+        $serveur
+    );
+
+    //mise à jour bord droit
+    sql_update(
+        $table,
+        array(
+            'bord_droit' => 'bord_droit + 2'
+        ),
+        "bord_droit >= ".$bordures['bord_droit'],
+        '',
+        $serveur
+    );
+
+    //mise à jour gauche
+    sql_update(
+        $table,
+        array(
+            'bord_gauche' => 'bord_gauche + 2'
+        ),
+        "bord_gauche >= ".$bordures['bord_droit'],
+        '',
+        $serveur
+    );
+    
+    $bords =array(
+        'bord_gauche' => $bordures['bord_droit'],
+        'bord_droit' => $bordures['bord_droit']+1 
+    );
+    
+    $couples = array_merge($couples,$bords);
+    
+    sql_insertq(
+        $table,
+        $couples,
+        '',
+        $serveur
+    );
+
+    return $bordures;
+}
+
+
+
+?>
