243cdf853a5993a38834a13f816eae30000e5b46
vincent@ldd.fr <vincent@ldd.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-04-22 11:29:19 +0000

    réglage pour les crayons, normalement plus d'erreurs SQL, la prochaine étape c'est d'essayer de faire un enregistrement tout automatique :)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@20137 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/etiquettes/etiquettes_options.php
+++ _test_/etiquettes/etiquettes_options.php
@@ -13,14 +13,26 @@ else @define('_SPIP19100', 1);
 
 function valeur_champ_tags($table, $id, $champ) {
 	
-	$r = spip_query('SELECT ALL titre FROM spip_mots AS m RIGHT JOIN spip_mots_'.$table.'s AS j ON m.id_mot=j.id_mot WHERE j.id_'.$table.'='.$id);
+	include_spip('base/connect_sql');
+	$table_sql = table_objet_sql($table);
+	$table_sql = preg_replace(',^spip_,', '', $table_sql);
+	$r = spip_query('SELECT ALL titre FROM spip_mots AS m RIGHT JOIN spip_mots_'.$table_sql.' AS j ON m.id_mot=j.id_mot WHERE j.id_'.$table.'='.$id);
 	$liste = array();
 	while($a = spip_fetch_array($r)){
 		array_push($liste,$a['titre']);
 	}
-	$liste = join(', ', $liste);
-	return $liste;
+	return empty($liste) ? "drfhdtrhrtfgh" : join(', ', $liste);
 	
+}
+
+function tags_revision($id_objet, $colonnes, $type_objet){
+
+	// Pour l'instant on ne fait rien ! On essaye pas de mettre à jour
+	// automatiquement, on fait ça à la main dans la vue.
+	// Cette fonction permet de ne pas avoir d'erreur SQL car sinon les crayons
+	// tentent de mettre la colonne "tags" à jour alors qu'elle n'existe pas.
+	return;
+
 }
 
 ?>
