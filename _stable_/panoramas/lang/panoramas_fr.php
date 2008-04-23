b48aeef8db5f534c51209578d0748d44e499112f
apachot@openstudio.fr <apachot@openstudio.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-04-23 10:33:48 +0000

    création des visites et lieux associés dans l'espace privé
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@20157 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/panoramas/lang/panoramas_fr.php
+++ _stable_/panoramas/lang/panoramas_fr.php
@@ -1,13 +1,21 @@
 <?php
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 'boite_info' => 'Bienvenue dans le module de visites virtuelles',
+'boite_info_lieu' => 'Ce panneau vous peret d\'ajouter des lieux &agrave; votre visite virtuelle',
 'icone_creer_visitevirtuelle' => 'Cr&eacute;er une nouvelle visite virtuelle',
 'toutes_visitesvirtuelles' => 'Toutes les visites virtuelles',
 'nouvelle_visite' => 'Nouvelle Visite',
 'supprimer_visite' => 'Supprimer la visite',
 'pas_de_visite' => 'Il n\'y a pas encore de visite virtuelle',
-'attention' => 'Attention cette visite n\'est pas vide !',
+'attention_visite' => 'Attention cette visite n\'est pas vide !',
+'attention_lieu' => 'Attention ce lieu n\'est pas vide !',
 'info_supprimer_visite' => 'Voulez-vous vraiement supprimer cette visite ?',
-'titre_visite' => 'Titre de la visite'
+'info_supprimer_lieu' => 'Voulez-vous vraiement supprimer ce lieu ?',
+'titre_visite' => 'Titre de la visite',
+'tous_lieux' => 'Tous les lieux associ&eacute;s &agrave; cette visite',
+'icone_creer_lieu' => 'Cr&eacute;er un nouveau lieu',
+'titre_lieu' => 'Titre du lieu',
+'nouveau_lieu' => 'Nouveau lieu'
+
 );
 ?>
