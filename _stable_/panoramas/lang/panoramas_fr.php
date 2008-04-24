53bd22cb1b6c8971b2aa87ea64fd2d5bfa5e4970
apachot@openstudio.fr <apachot@openstudio.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-04-24 11:43:46 +0000

    début de gestion des interactions dans l'espace privé
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@20177 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/panoramas/lang/panoramas_fr.php
+++ _stable_/panoramas/lang/panoramas_fr.php
@@ -1,21 +1,34 @@
 <?php
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 'boite_info' => 'Bienvenue dans le module de visites virtuelles',
-'boite_info_lieu' => 'Ce panneau vous peret d\'ajouter des lieux &agrave; votre visite virtuelle',
+'boite_info_lieu' => 'Ce panneau vous permet d\'ajouter des lieux &agrave; votre visite virtuelle',
+'boite_info_interaction' => 'Ce panneau vous permet d\'ajouter des interactions &agrave; ce lieu',
 'icone_creer_visitevirtuelle' => 'Cr&eacute;er une nouvelle visite virtuelle',
+'icone_creer_interaction' => 'Cr&eacute;er une nouvelle interaction',
 'toutes_visitesvirtuelles' => 'Toutes les visites virtuelles',
-'nouvelle_visite' => 'Nouvelle Visite',
 'supprimer_visite' => 'Supprimer la visite',
+'supprimer_interaction' => 'Supprimer l\'interaction',
 'pas_de_visite' => 'Il n\'y a pas encore de visite virtuelle',
 'attention_visite' => 'Attention cette visite n\'est pas vide !',
 'attention_lieu' => 'Attention ce lieu n\'est pas vide !',
-'info_supprimer_visite' => 'Voulez-vous vraiement supprimer cette visite ?',
-'info_supprimer_lieu' => 'Voulez-vous vraiement supprimer ce lieu ?',
+'info_supprimer_visite' => 'Voulez-vous vraiment supprimer cette visite ?',
+'info_supprimer_lieu' => 'Voulez-vous vraiment supprimer ce lieu ?',
+'info_supprimer_interaction' => 'Voulez-vous vraiment supprimer cette interaction ?',
 'titre_visite' => 'Titre de la visite',
+'titre_interaction' => 'Titre de l\'interaction',
 'tous_lieux' => 'Tous les lieux associ&eacute;s &agrave; cette visite',
+'toutes_interactions' => 'Toutes les interactions associ&eacute;es &agrave; ce lieu',
 'icone_creer_lieu' => 'Cr&eacute;er un nouveau lieu',
 'titre_lieu' => 'Titre du lieu',
-'nouveau_lieu' => 'Nouveau lieu'
-
+'nouvelle_visite' => 'Nouvelle Visite',
+'nouveau_lieu' => 'Nouveau lieu',
+'nouvelle_interaction' => 'Nouvelle interaction',
+'parametres' => 'Param&egrave;tres',
+'lieux_associes' => 'Lieux associ&eacute;s',
+'interactions_associees' => 'Interactions associ&eacute;es',
+'pas_d_interaction' => 'Pas d\'interaction',
+'visite_numero' => 'Visite num&eacute;ro',
+'lieu_numero' => 'Lieu num&eacute;ro',
+'interaction_numero' => 'Interaction num&eacute;ro'
 );
 ?>
