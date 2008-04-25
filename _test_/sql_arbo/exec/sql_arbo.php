8696bc9a1f35e99ae23da2cb06abe8cca4a251a7
cam.lafit@azerttyu.net <cam.lafit@azerttyu.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-04-25 13:29:41 +0000

    On liberer un peu la page exec.
    Il est ainsi possible de tester sur une table quelleconque, du moment que celle ci est en mode autojointure (rubriques par exemple)
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@20193 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/sql_arbo/exec/sql_arbo.php
+++ _test_/sql_arbo/exec/sql_arbo.php
@@ -25,26 +25,43 @@ function exec_sql_arbo() {
 	debut_droite();
 
     $table = _request('table');
+    $id_noeud = _request('id_noeud');
+    $champ_noeud = _request('champ_noeud');
+    $champ_parent = _request('champ_parent');
+    $champ_description = _request('champ_description');
+    
+    print_r($_GET);
 
-    if ($table) {
+    if (!is_null($table) && !is_null($id_noeud) && !is_null($champ_noeud) && !is_null($champ_parent)) {
         echo "<h1>Conversion autojointure -> intervallaire</h1>";
-        echo sql_arbre_convertir($table, 'FAM_ID', 'FAM_PERE');
+        
+        echo sql_arbre_convertir($table, $champ_noeud, $id_noeud, $champ_parent);
         
         echo "<h1>Obtenir toutes les feuilles</h1>";
         
         $ressource = sql_arbre_get_feuilles($table);
         
         while($r = sql_fetch($ressource)) {
-            echo $r['FAM_ID']."<br/>";
+            echo $r[$champ_noeud]."<br/>";
         }
         
         echo "<h1> Insertion d'une feuille Roller</h1>";
-        
-        print_r(sql_arbre_set_feuille($table,array('id'=>'1','champ' => 'FAM_ID'),'droit',array('FAM_LIB'=>'Roller')));
-        print_r(sql_arbre_set_feuille($table,array('id'=>'3','champ' => 'FAM_ID'),'gauche',array('FAM_LIB'=>'Parapente')));        
+        if ($champ_description ) {
+            print_r(sql_arbre_set_feuille($table,array('id'=>'1','champ' => $champ_noeud),'droit',array($champ_description=>'Roller')));
+            print_r(sql_arbre_set_feuille($table,array('id'=>'3','champ' => $champ_noeud),'gauche',array($champ_description=>'Parapente')));        
+        }
         
     } else {
-        echo "Veuillez donner en argument de la page la table à tester &table=matable";    
+        echo "Veuillez donner en argument de la page :  <br/>
+            <ul>
+                <li>la table à tester &table=matable</li>
+                <li>l'id du noeud de depart &id_noeud=id</li>
+                <li>le nom du champ des clefs &champ_noeud=champ</li>
+                <li>le nom du champ de la clef etrangére (parent) &champ_parent=champ</li>
+                <li>le nom du champ de description (pour tester les insertions) &champ_description</li>                
+                <li> <a href='?exec=sql_arbo&table=spip_autojointure&id_noeud=0&champ_noeud=FAM_ID&champ_parent=FAM_PERE&champ_description=FAM_LIB'> Exemple si la base autojointure de sql.developpez.com existe  </a></li>
+            </ul>
+        ";  
     }
     
 	fin_page();
