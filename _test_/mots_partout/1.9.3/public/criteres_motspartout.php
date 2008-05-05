1034d830cc1cf45f9cc784b3956284814c8d8081
cam.lafit@azerttyu.net <cam.lafit@azerttyu.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-05-05 13:40:44 +0000

    Entre les modifications d'api sql_*, de la gestion des pipelines et la possibilité d'utiliser des squelettes en espace privé. Il semble plus pertinent de faire une branche dédiée que d'essayer de travailler directement sur une compatibilité 192 et 193 dans le meme temps. En fonction de l'avancée du fork pour la 193, il sera peut etre bon de mettre en place à postériori la compat descendante.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@20316 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _test_/mots_partout/1.9.3/public/criteres_motspartout.php
@@ -0,0 +1,24 @@
+<?
+
+//ajout d'un critére branchemot basé sur critére branche
+// {branchemot ?}
+// http://www.spip.net/@branche
+// http://doc.spip.org/@critere_branche_dist
+function critere_branchemot($idb, &$boucles, $crit) {
+	$not = $crit->not;
+	$boucle = &$boucles[$idb];
+
+	$arg = calculer_argument_precedent($idb, 'id_groupe', $boucles);
+
+	$c = "calcul_mysql_in('" .
+	  $boucle->id_table .
+	  ".id_groupe', calcul_branchemot($arg), '')";
+	if ($crit->cond && true) $c = "($arg ? $c : 1)";
+			
+	if ($not)
+		$boucle->where[]= array("'NOT'", $c);
+	else
+		$boucle->where[]= $c;
+}	
+ 
+?>
