3098ab9306a4f22c71f53c3c9304854bfd260b89
cvallois@yahoo.fr <cvallois@yahoo.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-05-07 22:31:21 +0000

    ajout d'éléments de configuration: origine de l'article, mise en forme
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@20351 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/image-cycle/lang/imagecycle_fr.php
+++ _test_/image-cycle/lang/imagecycle_fr.php
@@ -3,13 +3,35 @@
 	$GLOBALS[$GLOBALS['idx_lang']] = array(
 		'titre'				=>	'titre',
 		'legende'			=>	'configuration du plugin',
-		'largeur'			=>	'largeur de l\'image en pixel',
-		'hauteur'			=>	'hauteur de l\'image en pixel',
-		'largeurcadre'		=>	'largeur du cadre en pixel',
-		'hauteurcadre'		=>	'hauteur du cadre en pixel',
+		
+		'origineimage'		=>	'origine de l\'image',
+		'aleatoire'			=>	'images al&eacute;atoires issues de toutes les images du site',
+		'article'			=>	'images d\'un article',
+		'renvart'			=>	'renvoi vers l\'article',
+		'idarticle'			=>	'choix de l\'article',
+		'renvdoc'			=>	'renvoi vers le document (compatible thickbox)',
+		'nombre'			=>	'nombre d\'image &acute; afficher',
+		
+		'paraimage'			=>	'Param&egrave;tres de l\'image',
+		'tailleimage'		=>	'Taille de l\'image',
+		'taillecadre'		=>	'Taille du cadre',
+		'largeur'			=>	'largeur',
+		'hauteur'			=>	'hauteur',
+		
+		'paraanim'			=>	'Param&egrave;tres de l\'animation',
 		'couleur'			=>	'couleur du cadre',
-		'vitesse'			=>	'vitesse de transition en mms',
-		'duree'				=>	'dur&eacute;e d\'affichage de la photo en mms',
+		'vitesse'			=>	'vitesse de transition',
+		'duree'				=>	'dur&eacute;e d\'affichage de la photo',
 		'effet'				=>	'effet d&eacute;sir&eacute;',
+		'couleurcadre'		=>	'Couleur du cadre',
+		'fond'				=>	'Couleur du fond',
+		'cadre'				=>	'Couleur de la bordure',
+		'taillefixe'		=>	'Taille fixe (avec d&eacute;formation de l\'image)',
+		'description'		=>	'<p>Bas&eacute; sur le script jquery image cycle.</p>
+								Affiches une noisette d\'images al&eacute;atoires',
+		'description2'		=>	'Configuration de la deuxi&egrave;me noisette pour un autre affichage dans le site',					
+								
+
+
 );
 ?>
