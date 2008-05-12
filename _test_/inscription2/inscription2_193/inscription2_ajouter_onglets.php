237fafabb0a08b7cbde681d4a558260c3e8cf357
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-05-12 22:54:33 +0000

    retour de la page des adhérents suite
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@20454 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/inscription2/inscription2_193/inscription2_ajouter_onglets.php
+++ _test_/inscription2/inscription2_193/inscription2_ajouter_onglets.php
@@ -3,6 +3,34 @@
 // Ajoute le bouton d'amin aux webmestres
 
 if (!defined("_ECRIRE_INC_VERSION")) return;
+function Inscription2_ajouter_boutons($boutons_admin){
+	if ($GLOBALS['connect_statut'] == "0minirezo" && $GLOBALS["connect_toutes_rubriques"]) {
+
+		$boutons_admin['auteurs']->sousmenu['inscription2_adherents']= new Bouton(
+		"../"._DIR_PLUGIN_INSCRIPTION2."images/inscription2_icone.png", // icone
+		_T("inscription2:adherents") //titre
+		);
+	}
+	return $boutons_admin;
+}
+
+function Inscription2_affiche_milieu($flux){
+	switch($flux['args']['exec']) {	
+			case 'auteur_infos':
+				include_spip('inc/inscription2_gestion');
+				$id_auteur = $flux['args']['id_auteur'];
+				$flux['data'] .= inscription2_ajouts($id_auteur);
+				break;
+			case 'auteurs_edit':
+				include_spip('inc/inscription2_fiche_adherent');
+				$id_auteur = $flux['args']['id_auteur'];
+				$flux['data'] .= inscription2_fiche_adherent($id_auteur);
+				break;
+			default:
+				break;
+		}
+	return $flux;
+}
 
 function Inscription2_header_prive($flux){
 	if (_request('exec')=='ajouter_adherent'){
