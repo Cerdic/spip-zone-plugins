1c3d216cc8c8e309cb362af8a957db79b482ed39
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-05-17 08:18:35 +0000

    langues (couteau)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@20542 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _stable_/couteau_suisse/lang/couteau_de.php
@@ -0,0 +1,50 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'attente_confirmation' => 'Auf Best&auml;tigung warten',
+
+	// D
+	'derniers_connectes' => 'Letzte Verbindungen:',
+
+	// E
+	'explique_spam' => 'Achtung: Sie haben reservierte Tag, Zeichen oder Worte verwendet.<br /><br />Bitte kehren Sie zum letzten Schritt zur&uuml;ck!',
+
+	// L
+	'lutte_spam' => 'SPAM-Bek&auml;mpfung',
+
+	// N
+	'nom_forum' => 'Danke, dass Sie Ihren Namen angegeben haben!',
+	'non_confirmes' => 'Nicht best&auml;tigt:',
+
+	// P
+	'page_debut' => 'Erste Seite',
+	'page_fin' => 'Letzte Seite',
+	'page_lien' => 'Seite @page@: @title@',
+	'page_precedente' => 'Vorige Seite',
+	'page_suivante' => 'N&auml;chste Seite',
+
+	// S
+	'smileys_dispos' => 'Verf&uuml;gbare Smileys:',
+	'sommaire_page' => ',&nbsp;S@page@',
+	'sommaire_page_long' => ',&nbsp;Seite&nbsp;@page@',
+	'sommaire_titre' => 'Inhalt',
+	'stats_auteur' => '@icon@ @nom@, am&nbsp;@date@',
+	'stats_date' => '@jour@.@mois@.@annee@&nbsp;um&nbsp;@h@h@m@',
+
+	// T
+	'texte_formatspip' => 'ORIGINALTEXT IM SPIP-FORMAT',
+	'textes_formatspip' => 'ORIGINALTEXTE IM SPIP-FORMAT',
+	'titre' => 'Schweizer Taschenmesser',
+
+	// V
+	'variable_vide' => '(Leer)'
+);
+
+?>
---
+++ _stable_/couteau_suisse/lang/couteau_pt_br.php
@@ -0,0 +1,50 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'attente_confirmation' => 'Aguardando confirma&ccedil;&atilde;o',
+
+	// D
+	'derniers_connectes' => '&Uacute;ltimos conectados:',
+
+	// E
+	'explique_spam' => 'Aten&ccedil;&atilde;o: voc&ecirc; usou tags, caracteres ou palavras proibidas.<br /><br />Retorne!',
+
+	// L
+	'lutte_spam' => 'Lute contra o SPAM',
+
+	// N
+	'nom_forum' => 'Obrigado por informar o seu nome!',
+	'non_confirmes' => 'Nomes confirmados:',
+
+	// P
+	'page_debut' => 'Primeira p&aacute;gina',
+	'page_fin' => '&Uacute;ltima p&aacute;gina',
+	'page_lien' => 'P&aacute;gina @page@: @title@',
+	'page_precedente' => 'P&aacute;gina anterior',
+	'page_suivante' => 'Pr&oacute;xima p&aacute;gina',
+
+	// S
+	'smileys_dispos' => 'Caricaturas dispon&iacute;veis:',
+	'sommaire_page' => ',&nbsp;p@page@',
+	'sommaire_page_long' => ',&nbsp;p&aacute;gina&nbsp;@page@',
+	'sommaire_titre' => '&Iacute;ndice',
+	'stats_auteur' => '@icon@ @nom@, @date@',
+	'stats_date' => '@jour@/@mois@/@annee@&nbsp;&agrave;s&nbsp;@h@h@m@',
+
+	// T
+	'texte_formatspip' => 'TEXTO ORIGINAL EM FORMATO SPIP',
+	'textes_formatspip' => 'TEXTOS ORIGINAIS EM FORMATO SPIP',
+	'titre' => 'Canivete Su&iacute;&ccedil;o',
+
+	// V
+	'variable_vide' => '(Vazio)'
+);
+
+?>
