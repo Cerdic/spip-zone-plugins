2ae8a8e68b0abeb60c6ec6b7e86bcf5e65025904
tbeaumanoir@gmail.com <tbeaumanoir@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-05-20 13:06:31 +0000

    coquille dans le nom du champ
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@20603 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/a2a/action/a2a_lier_article.php
+++ _dev_/a2a/action/a2a_lier_article.php
@@ -5,14 +5,14 @@ function action_a2a_lier_article(){
 	include_spip('public/assembler');
 	$contexte['id_article_cible'] = _request('id_article');
 	$contexte['id_article_source'] = _request('id_article_orig');
-	
-	$sql_test_liaison = "SELECT * FROM spip_articles_lies WHERE id_artile='".$contexte['id_article_source']."' AND id_article_cible='".$contexte['id_article_cible']."';";
+
+	$sql_test_liaison = "SELECT * FROM spip_articles_lies WHERE id_article='".$contexte['id_article_source']."' AND id_article_cible='".$contexte['id_article_cible']."';";
 	$res_test_liaison = spip_query($sql_test_liaison);
 	if (spip_num_rows($res_test_liaison) == 0){
 		$sql_liaison = "INSERT INTO spip_articles_lies VALUES ('".$contexte['id_article_source']."', '".$contexte['id_article_cible']."');";
 		$res_liaison = spip_query($sql_liaison);
 	}
-	
+
 	$redirect = generer_url_ecrire("articles", "id_article=".$contexte['id_article_source'], "&");
 	redirige_par_entete($redirect);
 }
