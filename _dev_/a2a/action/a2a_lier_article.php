114111ee04e5dd7e27ec9165ee6c16de37a593d8
brunobergot@gmail.com <brunobergot@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-05-25 22:16:50 +0000

    et hop maintenant qu'on a une table digne de ce nom on assigne un rang aux articles liés
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@20683 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/a2a/action/a2a_lier_article.php
+++ _dev_/a2a/action/a2a_lier_article.php
@@ -5,14 +5,20 @@ function action_a2a_lier_article(){
 	include_spip('public/assembler');
 	$contexte['id_article_cible'] = _request('id_article');
 	$contexte['id_article_source'] = _request('id_article_orig');
-
-	$sql_test_liaison = "SELECT * FROM spip_articles_lies WHERE id_article='".$contexte['id_article_source']."' AND id_article_cible='".$contexte['id_article_cible']."';";
+	//on verifie que cet article n'est pas deja lie
+	$sql_test_liaison = "SELECT * FROM spip_articles_lies WHERE id_article="._q($contexte['id_article_source'])." AND id_article_cible="._q($contexte['id_article_cible']);
 	$res_test_liaison = spip_query($sql_test_liaison);
 	if (spip_num_rows($res_test_liaison) == 0){
-		$sql_liaison = "INSERT INTO spip_articles_lies VALUES ('".$contexte['id_article_source']."', '".$contexte['id_article_cible']."');";
+		//on recupere le rang le plus haut pour definir celui de l'article a lier
+		$sql_rang = "SELECT MAX(rang) AS max FROM spip_articles_lies WHERE id_article="._q($contexte['id_article_source']);
+		$res_rang = spip_query($sql_rang);
+		$rang_max = spip_fetch_array($res_rang);
+		$rang = $rang_max['max'] + 1;
+		//on ajoute le lien vers l'article
+		$sql_liaison = "INSERT INTO spip_articles_lies VALUES ("._q($contexte['id_article_source']).", "._q($contexte['id_article_cible']).","._q($rang).");";
 		$res_liaison = spip_query($sql_liaison);
 	}
-
+	
 	$redirect = generer_url_ecrire("articles", "id_article=".$contexte['id_article_source'], "&");
 	redirige_par_entete($redirect);
 }
