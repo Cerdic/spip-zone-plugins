c3449241d3441239913ee28ec534a6dfb5037660
marcimat@free.fr <marcimat@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-05-29 20:03:58 +0000

    - plugin.xml cassait les zips - passage des fonctions en sql_*().
    Il y a un gros travail à faire sur exec/* afin d'utiliser des fonctions natives de SPIP.
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@20737 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/grenouille-193/meteo_fonctions.php
+++ _stable_/grenouille-193/meteo_fonctions.php
@@ -120,46 +120,46 @@
 			$url = "http://xoap.weather.com/weather/local/".$code."?cc=*&unit=s&dayf=".$jours;
 			$xml = meteo_lire_xml($url,true,"day d=.*",array("hi","low","part p=\"d\"","part p=\"n\""));
 			if ($xml) {
-				spip_query('UPDATE spip_meteo SET statut="publie", maj=NOW() WHERE id_meteo="'.$id_meteo.'"');
+				sql_update('spip_meteo', array('statut'=>sql_quote("publie"),'maj'=>'NOW()'),
+					'id_meteo=' . sql_quote($id_meteo));
 				for($i=0; $i<$jours; $i++) {
 				   $tmp = preg_split("/<\/?icon>/",$xml["part p=\"d\""][$i]);
 				   $xml["icon"][$i] = $tmp[1];
 				}
 				for($i=0; $i<$jours; $i++) {
+					$set = array();
 					$date = strftime("%Y-%m-%d 12:00:00", time() + $i * 24 * 3600);
-					if ($xml["hi"][$i] == "N/A") {
-						$max = "NA";
-						$set_max = '';
-					} else {
-						$max = meteo_convertir_fahrenheit_celsius($xml["hi"][$i]);
-						$set_max = 'maxima="'.$max.'", ';
+					if ($xml["hi"][$i] != "N/A") {
+						$set['maxima'] = meteo_convertir_fahrenheit_celsius($xml["hi"][$i]);
 					}
-					if ($xml["low"][$i] == "N/A") {
-						$min = "NA";
-						$set_min = '';
-					} else {
-						$min = meteo_convertir_fahrenheit_celsius($xml["low"][$i]);
-						$set_min = 'minima="'.$min.'", ';
+					if ($xml["low"][$i] != "N/A") {
+						$set['minima'] = meteo_convertir_fahrenheit_celsius($xml["low"][$i]);
 					}
-					if ($xml["icon"][$i] == 48) {
-						$icone = 48;
-						$set_temps = '';
-					} else {
-						$icone = $xml["icon"][$i];
-						$set_temps = 'id_temps="'.$xml["icon"][$i].'", ';
+					if ($xml["icon"][$i] != 48) {
+						$set['id_temps'] = $xml["icon"][$i];
 					}
 
-					$verif = spip_query('SELECT * FROM spip_previsions WHERE date="'.$date.'" AND id_meteo="'.$id_meteo.'" LIMIT 1');
-					if (spip_num_rows($verif) > 0) {
-						$bob = spip_fetch_array($verif);
-						$id_prevision = $bob['id_prevision'];
-						spip_query('UPDATE spip_previsions SET '.$set_max.$set_min.$set_temps.'maj=NOW() WHERE id_prevision="'.$id_prevision.'" AND id_meteo="'.$id_meteo.'"');
+					if ($id_prevision = sql_getfetsel('id_prevision', 'spip_previsions', 
+						array("date=". sql_quote($date), "id_meteo=". sql_quote($id_meteo)))) {
+							sql_updateq('spip_previsions', $set,  
+								array(
+								'id_prevision='. sql_quote($id_prevision), 
+								'id_meteo='. sql_quote($id_meteo)));
 					} else {
-						spip_query('INSERT INTO spip_previsions (id_meteo, date, maxima, minima, id_temps, maj) VALUES ("'.$id_meteo.'", "'.$date.'", "'.$max.'", "'.$min.'", "'.$icone.'", NOW())');
+						isset($set['maxima']) || $set['maxima']='NA';
+						isset($set['minima']) || $set['minima']='NA';
+						isset($set['id_temps']) || $set['minima']=48;
+						$set['id_meteo'] = $id_meteo;
+						$set['date'] = $date;
+						$set['date'] = $date;
+						$set['maj'] = 'MAJ()';
+						sql_insertq('spip_previsions', $set);
 					}
 				}
 			} else {
-				spip_query('UPDATE spip_meteo SET statut="en_erreur", maj=NOW() WHERE id_meteo="'.$id_meteo.'"');
+				sql_updateq('spip_meteo', 
+					array("statut"=>"en_erreur", "maj"=>"NOW()"),
+					"id_meteo=". sql_quote($id_meteo));
 			}
 		}
 		return true;
@@ -319,11 +319,9 @@
 	 **/
 	function meteo_calculer_temps($id_prevision) {
 		global $tableau_meteo;
-		$temps = spip_query('SELECT id_temps FROM spip_previsions WHERE id_prevision="'.$id_prevision.'"');
-		if (spip_num_rows($temps) == 0) {
+		if (!$id_temps = sql_getfetsel('id_temps','spip_previsions', 'id_prevision='. sql_quote($id_prevision))){
 			return '';
 		} else {
-			list($id_temps) = spip_fetch_array($temps, SPIP_NUM);
 			return $tableau_meteo[$id_temps];
 		}
 	}
