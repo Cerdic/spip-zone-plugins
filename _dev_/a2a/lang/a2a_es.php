e187c0479c60bf541f6577668b6f9f832d8150c2
prigent.yohann@gmail.com <prigent.yohann@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-06-05 21:27:18 +0000

    Traduction en espagnol, mais à vérifier comme même !
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@20851 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _dev_/a2a/lang/a2a_es.php
@@ -0,0 +1,29 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'articles_lies' => 'Connectar un art&iacute;culo',
+	
+	// L
+	'lier_un_article_a_celui_ci' => 'Connectar',
+	'lier_cet_article' => 'Connectar',
+	
+	// P
+	'pas_articles_lies' => 'No art&iacute;culo relacionado',
+	'pas_de_resultat' => 'No resultados',
+	
+	// R
+	'rechercher' => 'Buscar',
+	'rechercher_un_article_a_lier' => 'Buscar un art&iacute;culo',
+	
+	// S
+	'supprimer_le_lien' => 'Desconectar',
+	
+	// V
+	'voir' => 'Ver',
+);
+
+?>
