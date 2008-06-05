b9d8930c8aa8785fd63f9a2116f67fb58403e52a
prigent.yohann@gmail.com <prigent.yohann@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-06-05 21:08:26 +0000

    La traduction du fichier de langue en francais
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@20850 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _dev_/a2a/lang/a2a_en.php
@@ -0,0 +1,29 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'articles_lies' => 'Connect an article',
+	
+	// L
+	'lier_un_article_a_celui_ci' => 'Connect',
+	'lier_cet_article' => 'Connect',
+	
+	// P
+	'pas_articles_lies' => 'No related article',
+	'pas_de_resultat' => 'No results',
+	
+	// R
+	'rechercher' => 'Search',
+	'rechercher_un_article_a_lier' => 'Search an article',
+	
+	// S
+	'supprimer_le_lien' => 'Disconnect',
+	
+	// V
+	'voir' => 'See',
+);
+
+?>
