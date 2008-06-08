338f3d15e965aec2ffc88cb9db2982452d769b53
brunobergot@gmail.com <brunobergot@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-06-08 15:35:04 +0000

    On met à jour le rang des articles liés à l'article en cours lors de la suppression d'un article lié. Cela va permettre de mettre en place une fonction pour modifier l'ordre des articles liés.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@20887 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/a2a/action/supprimer_lien_a2a.php
+++ _dev_/a2a/action/supprimer_lien_a2a.php
@@ -7,6 +7,15 @@ function action_supprimer_lien_a2a(){
 	$id_article_cible = _request('id_article_cible');
 	$id_article = _request('id_article');
 	
+	$rang = sql_getfetsel('rang', 'spip_articles_lies', 'id_article=' . sql_quote($id_article) . 'AND id_article_lie=' . sql_quote($id_article_cible));
+
+	// on récupère les articles liés dont le rang est supérieur à celui à supprimer
+	$res = sql_select('*', 'spip_articles_lies', 'id_article=' . sql_quote($id_article) . 'AND rang>' . sql_quote($rang));
+	//on boucle sur le résultat et on met à jour le rang des articles lies avant suppression
+	while($r = sql_fetch($res)){
+		sql_update('spip_articles_lies', array('rang' => sql_quote(--$r["rang"])), 'id_article=' . sql_quote($r["id_article"]) . 'AND id_article_lie=' . sql_quote($r["id_article_lie"]));
+	}
+	
 	sql_delete('spip_articles_lies',  array(
 		'id_article = ' . sql_quote($id_article), 
 		'id_article_lie = ' . sql_quote($id_article_cible)
