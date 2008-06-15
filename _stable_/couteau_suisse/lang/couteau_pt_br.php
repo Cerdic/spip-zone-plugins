14834257cb8dc20aa15803c5991ab466da0feaea
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-06-15 09:49:08 +0000

    langues (couteau)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@20960 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/couteau_suisse/lang/couteau_pt_br.php
+++ _stable_/couteau_suisse/lang/couteau_pt_br.php
@@ -29,6 +29,17 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'page_lien' => 'P&aacute;gina @page@: @title@',
 	'page_precedente' => 'P&aacute;gina anterior',
 	'page_suivante' => 'Pr&oacute;xima p&aacute;gina',
+	'plugin_xml' => 'Re&uacute;ne, em um &uacute;nico plugin, diversas pequenas e &uacute;teis funcionalidades, que melhoram o gerenciamento do seu site SPIP.
+
+Cada uma destas ferramentas pode ser ativada ou desativada pelo usu&aacute;rio na [p&aacute;gina de administra&ccedil;&atilde;o do plugin->./?exec=admin_couteau_suisse] e gerar um certo n&uacute;mero de vari&aacute;veis: clique em {{Configuration}}, e depois escolha a guia {{Le Couteau Suisse}}.
+
+As categorias dispon&iacute;veis s&atilde;o: Administra&ccedil;&atilde;o, Melhoramentos tipogr&aacute;ficos, Atalhos tipogr&aacute;ficos, Exibi&ccedil;&atilde;o p&uacute;blica, Tags, filtros, crit&eacute;rios.
+
+Descobra neste plugin as suas ferramentas favoritas: {Sobrescritos}, {Aspas tipogr&aacute;ficas}, {Bullets melhorados}, {Um &iacute;ndice para os seus artigos}, {Smileys}, {Excluir o n&uacute;mero}, {URLs melhorados}, {SPIP e links externos}, {Sem f&oacute;runs an&ocirc;nimos}, {Dividir em p&aacute;ginas e guias}, etc., etc.
+
+N&atilde;o deixe de consultar os artigos de documenta&ccedil;&atilde;o do plugin, publicados em: [spip-contrib.net->http://www.spip-contrib.net/Le-Couteau-Suisse].
+
+Compatibilidade: SPIP em todas as vers&otilde;es 1.9x',
 
 	// S
 	'smileys_dispos' => 'Caricaturas dispon&iacute;veis:',
