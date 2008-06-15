fbd608419397bdf59c6c0d7557944b5166dc22cf
toutati@free.fr <toutati@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-06-15 22:28:42 +0000

    Bon, je recommence... début de AdminPublic, ce plugin utilise crayons, formulaire_upload, agenda, autorité, il étend les possibilités du formulaire administration, retours bienvenus!
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@20967 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _stable_/adminpublic/adminpublic_mes_fonctions.php
@@ -0,0 +1,58 @@
+<?php
+
+	function cree_puce_stat($statut){	
+	switch ($statut) {
+		case 'prepa': return  http_img_pack("puce-blanche.gif",_T('texte_statut_en_cours_redaction'));
+		case 'prop': return  http_img_pack("puce-orange.gif",_T('texte_statut_propose_evaluation'));
+		case 'publie': return  http_img_pack("puce-verte.gif",_T('texte_statut_publie'));
+		case 'refuse': return  http_img_pack("puce-rouge.gif",_T('texte_statut_refuse'));
+		case 'poubelle': return  http_img_pack("puce-poubelle.gif",_T('texte_statut_poubelle'));
+		};
+	}
+	
+/*
+* #IMG{fichier, alt} -> <img src='fichier' alt='alt' />
+*[(#IMG{images/blocsallesom.png}|image_reduire{280}|extraire_attribut{src})]
+*/
+function balise_IMG($p) {
+
+	if ($p->param && !$p->param[0][0]) {
+		$p->code =  calculer_liste($p->param[0][1],
+					$p->descr,
+					$p->boucles,
+					$p->id_boucle);
+		$alt =  calculer_liste($p->param[0][2],
+					$p->descr,
+					$p->boucles,
+					$p->id_boucle);
+
+		// autres filtres (???)
+		array_shift($p->param);
+	}
+
+	// recherche du chemin de l'image (comme #CHEMIN)
+	$p->code = 'find_in_path(' . $p->code .')';
+	// passage en image
+	$p->code = '"<img src=\'".' . $p->code .'."\' alt=\'".'.$alt.'."\' />"';
+
+	$p->interdire_scripts = true;
+	return $p;
+}
+
+
+// generer_url_public('style_prive', parametres_css_prive())
+// qu'il est alors possible de recuperer dans le squelette style_prive.html avec
+// #SET{claire,##ENV{couleur_claire,edf3fe}}
+// #SET{foncee,##ENV{couleur_foncee,3874b0}}
+// #SET{left,#ENV{ltr}|choixsiegal{left,left,right}}
+// #SET{right,#ENV{ltr}|choixsiegal{left,right,left}}
+// http://doc.spip.org/@parametres_css_prive
+function adminpublic_insert_head($flux){
+	$flux .= '<link rel="stylesheet" href="'._DIR_PLUGIN_ADMINPUBLIC.'adminpublic.css" type="text/css" media="projection, screen, tv" />';
+	$flux .= '<script type="text/javascript" src="'.generer_url_public('adminpublic.js').'"></script>';
+	$flux .= '<script type="text/javascript" src="'._DIR_PLUGIN_ADMINPUBLIC.'js/jquery-fieldselection.js"></script>';
+	return $flux;
+}
+	
+
+?>
---
+++ _stable_/adminpublic/lang/adminpublic_fr.php
@@ -0,0 +1,17 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	'modifier_les_textes' => 'Modifier les textes',
+	'positionner_le_curseur' => 'Dans la page, double-cliquez sur le crayon et positionnez votre curseur dans le texte! ou recopiez le raccourci',
+	'survoler_le_texte' => 'survoler le texte dans la page et double-cliquer dessus',
+	'ajouter_evenement'=> 'Ajouter un &eacute;v&eacute;nement'
+	
+	
+	
+);
+
+?>
