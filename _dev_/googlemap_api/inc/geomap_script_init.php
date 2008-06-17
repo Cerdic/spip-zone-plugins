66f93b4b4edfb9f1408929fd2f49483ed61723f0
renato@rezo.net <renato@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-06-17 13:23:32 +0000

    choisir la version de gmap et telecharger l'api dans la langue du contexte
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@20985 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/googlemap_api/inc/geomap_script_init.php
+++ _dev_/googlemap_api/inc/geomap_script_init.php
@@ -14,8 +14,9 @@ function inc_geomap_script_init_dist(){
 	static $deja_insere = false;
 	if ($deja_insere) return "";
 	$config = lire_meta('geomap_googlemapkey');
+	$version = lire_meta('geomap_googlemapversion');
 	$geomap = compacte_js(find_in_path('js/geomap.js'));
-	$gmap_script = compacte_js(recuperer_page('http://maps.google.com/maps?file=api&v=2&key='.$config));
+	$gmap_script = compacte_js(recuperer_page('http://maps.google.com/maps?file=api&v='.$version.'&key='.$config.'&hl='.$GLOBALS['spip_lang']));
 	return '<script type="text/javascript" src="'.$geomap.'"></script>'
 	.'<script type="text/javascript">'.$gmap_script.'</script>'.'<script type="text/javascript" src="'._DIR_PLUGIN_GEOMAP.'js/customControls.js"></script>';
 }
