2313d1c4ea86636145c97d0e9c868a59d5f9c477
prigent.yohann@gmail.com <prigent.yohann@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-06-19 14:45:14 +0000

    Traduction en anglais du fichier de lang de spipmotion.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@21010 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _dev_/spipmotion/lang/spipmotion_en.php
@@ -0,0 +1,26 @@
+<?php
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	'titre' => 'SPIPmotion',
+	'titrepage' => 'SPIPmotion : Easy Video Publication for Internet',
+	'gros_titre' => 'SPIPmotion : Easy Video Publication for Internet',
+	'chemin_executable' => 'Path to FFmpeg on the server :',
+	'width_video' => 'Width of the output in px : ',
+	'height_video' => 'Height of the output in px : ',
+	'bitrate' => 'Bitrate video of the output in kbps : ',
+	'framerate' => 'Image per second : ',
+	'bitrate_audio' => 'Bitrate audio of the output in kbps : ',
+	'frequence_audio' => 'Audio frequency : ',
+	'options_config' => 'Cofiguration of the encoding',
+	'bouton_ajouter_videos' => 'ADD A VIDEO',
+	'info_installer_encoder_ftp' => 'As Administrator, you can install (with FTP) .AVI .MPG .MP4 or .MOV files in the directory "tmp/upload" for encoding these files here.',
+	'info_installer_ftp' => 'As Administrator, you can install (with FTP) .FLV files in the directory "tmp/upload" for add these files in your articles here.',
+	'info_selectionner_fichier_encoder' => 'You can encode one of these files',
+	'info_selectionner_video' => 'You can insert one of these video :',
+	'bouton_encoder' => 'Encode',
+	'bouton_encoder_videos' => 'VIDEO TO ENCODE',
+	'verifier_droits' => 'Verify the right of "script_bash/spipmotion.sh". Right on this files have to be in 777.',
+	'encodage_en_cours' => 'ENCODING...',
+	'zzzz' => 'zzzz'
+);
+?>
