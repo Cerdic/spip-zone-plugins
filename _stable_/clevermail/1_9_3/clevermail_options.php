64c5344aa4b5e8b93a7fbf8818baf7888d9c2dad
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-06-20 20:11:33 +0000

    Optimisation : pas besoin de find_in_path et de 2 fichiers ici !
    !! Attention : il faut repasser par la page exec=admin_plugin puisque le plugin.xml est modifié !!
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@21044 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/clevermail/1_9_3/clevermail_options.php
+++ _stable_/clevermail/1_9_3/clevermail_options.php
@@ -26,4 +26,11 @@ function clevermail_taches_generales_cron($taches_generales){
 	$taches_generales['clevermail_cron'] = 10 ;
 	return $taches_generales;
 }
+
+function clevermail_header_prive($flux) {
+	// On ajoute un CSS et un JS pour le back-office
+	$flux .= "<script src=\""._DIR_PLUGIN_CLEVERMAIL."/js/functions.js\" type=\"text/javascript\"></script>";
+	$flux .= "<link rel=\"stylesheet\" type=\"text/css\" href=\""._DIR_PLUGIN_CLEVERMAIL."/css/styles.css\" />";
+	return $flux;
+}
 ?>
