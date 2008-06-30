b5a5693768de09d4caa453874ea8a9f27fb42254
berio@alg-a.org <berio@alg-a.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-06-30 19:05:26 +0000

    just checkig.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@21193 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/open_layers/geomap.js_fonctions.php
+++ _dev_/open_layers/geomap.js_fonctions.php
@@ -1,5 +1,5 @@
 <?php
-
+// gfd  gfd
 function geomap_cache_map($script){
 	if (preg_match(",http://maps\.google\.com/mapfiles/(.*)\.js,Uims",$script,$regs))
 		$script = str_replace($regs[0],generer_url_public('mapfiles.js','map='.$regs[1].".js",true),$script);
