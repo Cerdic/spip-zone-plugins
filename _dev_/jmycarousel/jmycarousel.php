9f2ce99b8a2d79b266efd7206940b40654d338e1
alexandra.guiderdoni@gmail.com <alexandra.guiderdoni@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-07-04 15:23:00 +0000

    Mise en plugin via des modeles de jmycarousel, portfolio en jquery carousel des doc spip
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@21252 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _dev_/jmycarousel/jmycarousel.php
@@ -0,0 +1,6 @@
+<?php
+function jmycarousel_insert_head($flux){
+		$flux .= '<link rel="stylesheet" href="'.find_in_path('css/jMyCarousel.css').'" type="text/css" media="projection, screen, tv" />'."\n";
+		return $flux;
+}
+
