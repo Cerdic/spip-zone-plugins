d8feb26cfee10c534922000671ad1d719b1ac1d3
arno@rezo.net <arno@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-07-04 12:36:41 +0000

    Compatibilite SPIP 2
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@21235 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/lire_aussi/exec/lire_aussi_interface.php
+++ _stable_/lire_aussi/exec/lire_aussi_interface.php
@@ -5,9 +5,6 @@ function exec_lire_aussi_interface() {
 		include_spip("inc/utils");
 		$contexte = array('id_article'=>$_GET["id_article"]);
 
-		$ret .= "A";
-
-
 		$p = evaluer_fond("lire_aussi_interface", $contexte);
 		$ret .= $p["texte"];
 		
