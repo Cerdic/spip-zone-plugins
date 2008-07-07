ddb6e463303359ee1fa234e02664a4701f750fad
paladin@quesaco.org <paladin@quesaco.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-07-07 06:04:40 +0000

    Curiosité: le bouton (barre naviguer) en png a droit à un encadré bord arrondi (sous SPIP-192D/Firefox en tt cas). Le changer en gif ne laisse pas apparaître cet encadré.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@21326 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/spip-listes/spip-listes_1_9_3/inc/spiplistes_pipeline_ajouter_boutons.php
+++ _test_/spip-listes/spip-listes_1_9_3/inc/spiplistes_pipeline_ajouter_boutons.php
@@ -18,7 +18,7 @@ function spiplistes_ajouterBoutons($boutons_admin) {
 	if($GLOBALS['connect_statut'] == "0minirezo") {
 	// affiche le bouton dans "Edition"
 		$boutons_admin['naviguer']->sousmenu['spiplistes'] = new Bouton(
-			_DIR_PLUGIN_SPIPLISTES_IMG_PACK."courriers_listes-24.png"  // icone
+			_DIR_PLUGIN_SPIPLISTES_IMG_PACK."courriers_listes-24.gif"  // icone
 			, _T('spiplistes:listes_de_diffusion_')	// titre
 			, _SPIPLISTES_EXEC_COURRIERS_LISTE
 		);
