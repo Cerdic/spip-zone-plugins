ff8752b259c7025f62183be44d920fbf711aee3c
webmaster@geomaticien.com <webmaster@geomaticien.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-07-08 21:34:32 +0000

    Cat-Google-analytics: UA var size increased
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@21382 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/acs/models/cat/composants/google-analytics/ecrire/lang/google-analytics_adm_fr.php
+++ _dev_/acs/models/cat/composants/google-analytics/ecrire/lang/google-analytics_adm_fr.php
@@ -4,6 +4,7 @@
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 'google-analytics_description' => 'Inclusion du script de Google-analytics',
+'google-analytics_help' => 'Il est conseillé de <a href="?exec=admin_vider">vider le cache</a> après activation de ce composant.',
 
 'acsGoogle-analyticsUA' => 'Compte utilisateur (UA-xxxxxx-x)',
 'google-analytics_on' => _T('item_oui'),
