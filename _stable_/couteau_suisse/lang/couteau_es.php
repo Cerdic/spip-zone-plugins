05aaf379b49ad01b45156fab2f510a90b865956b
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-07-14 09:52:09 +0000

    langues (couteau)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@21470 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _stable_/couteau_suisse/lang/couteau_es.php
@@ -0,0 +1,61 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'attente_confirmation' => 'En espera de confirmaci&oacute;n',
+
+	// D
+	'derniers_connectes' => '&Uacute;ltimos conectados:',
+
+	// E
+	'explique_spam' => 'Cuidado: has utilizado balizas, caracteres o palabras prohibidas.<br /><br />&iexcl;Vuelve atr&aacute;s!',
+
+	// L
+	'lutte_spam' => 'Lucha contra el SPAM',
+
+	// N
+	'nom_forum' => '&iexcl;Por favor, especifica tu nombre!',
+	'non_confirmes' => 'No confirmados:',
+
+	// P
+	'page_debut' => 'Primera p&aacute;gina',
+	'page_fin' => '&Uacute;ltima p&aacute;gina',
+	'page_lien' => 'P&aacute;gina @page@: @title@',
+	'page_precedente' => 'P&aacute;gina anterior',
+	'page_suivante' => 'P&aacute;gina siguiente',
+	'plugin_xml' => 'Re&uacute;ne en un solo plugin une liste de peque&ntilde;as caracter&iacute;sticas nuevas y &uacute;tiles para mejorar la gesti&oacute;n de tu sitio SPIP.
+
+Cada herramienta puede activarse o no por el usuario en [la p&aacute;gina de administraci&oacute;n del plugin->./?exec=admin_couteau_suisse] y manejar cierto n&uacute;mero de variables: pulsa en {{Configuraci&oacute;n}}, y selecciona la pesta&ntilde;a {{La Navaja Suiza}}.
+
+Las categor&iacute;as disponibles son: Administraci&oacute;n, Mejoras tipogr&aacute;ficas, Atajos tipogr&aacute;ficos, Publicaci&oacute;n, Balizas, filtros, criterios.
+
+Encuentra en este plugin tus herramientas favoritas: {Exponentes tipogr&aacute;ficos}, {Comillas tipogr&aacute;ficas}, {Marcas bonitas}, {Un sumario para tus art&iacute;culos}, {Smileys}, {Suprimir el n&uacute;mero}, {URLs elegantes}, {SPIP y los enlaces... externos}, {Foros no an&oacute;nimos}, {Dividir en p&aacute;ginas y pesta&ntilde;as}, etc., etc.
+
+No dudes en consultar los art&iacute;culos de documentaci&oacute;n del plugin publicados en: [spip-contrib.net->http://www.spip-contrib.net/Le-Couteau-Suisse].
+
+Compatibilidad: todas las versiones 1.9x de SPIP',
+
+	// S
+	'smileys_dispos' => 'Caritas disponibles:',
+	'sommaire_page' => ',&nbsp;p@page@',
+	'sommaire_page_long' => ',&nbsp;p&aacute;gina&nbsp;@page@',
+	'sommaire_titre' => 'Sumario',
+	'stats_auteur' => '@icon@ @nom@, el&nbsp;@date@',
+	'stats_date' => '@jour@/@mois@/@annee@&nbsp;a&nbsp;las&nbsp;@h@h@m@',
+
+	// T
+	'texte_formatspip' => 'TEXTO ORIGINAL EN FORMATO SPIP',
+	'textes_formatspip' => 'TEXTOS ORIGINALES EN FORMATO SPIP',
+	'titre' => 'La Navaja Suiza',
+
+	// V
+	'variable_vide' => '(Vac&iacute;o)'
+);
+
+?>
