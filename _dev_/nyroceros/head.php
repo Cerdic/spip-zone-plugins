c88a05552c7cf7186eb02eb81bf7542e89622f84
fil@rezo.net <fil@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-07-14 21:05:18 +0000

    nyro automatque sur tous les liens images (comme thicbox) ; et en mode galerie sur le portfolio
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@21498 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/nyroceros/head.php
+++ _dev_/nyroceros/head.php
@@ -1,15 +1,31 @@
 <?php
 
 function Nyro_insert_head($flux){
-	$flux = Nyro_header_prive($flux);
-	return $flux;
-}
-
-function Nyro_header_prive($flux) {
 	include_spip("inc/filtres");
 
 	$flux .='
 <script src=\''.url_absolue(find_in_path('js/jquery.nyroModal-1.2.8.js')).'\' type=\'text/javascript\'></script>
+<script type="text/javascript"><!--
+// Inside the function "this" will be "document" when called by ready() 
+// and "the ajaxed element" when called because of onAjaxLoad 
+if (window.jQuery)
+(function($){
+var init_f = function() {
+	// selectionner tous les liens vers des images
+	$("a[@type=\'image/jpeg\'],a[@type=\'image/png\'],a[@type=\'image/gif\']",this)
+	.addClass("nyroceros") // noter qu\'on l\'a vue
+	.attr("onclick","") // se debarrasser du onclick de SPIP
+	.nyroModal(); // activer le nyro
+
+	// passer le portfolio en mode galerie de nyro
+	$("#documents_portfolio .nyroceros", this)
+	.attr("rel","galerie-portfolio");
+}
+//onAjaxLoad is defined in private area only
+if(typeof onAjaxLoad == "function") onAjaxLoad(init_f);
+	$(init_f);
+ })(jQuery);
+ // --></script>
 <link rel="stylesheet" href="'.url_absolue(find_in_path('styles/nyroModal.full.css')).'" type="text/css" media="projection, screen, tv" />
 ';
 	return $flux;
