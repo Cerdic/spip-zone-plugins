0bfdfcf87e778acb132faf2484f937a88c96d435
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-07-14 09:52:19 +0000

    langues (accesrestreint)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@21474 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/acces_restreint/lang/accesrestreint_es.php
+++ _stable_/acces_restreint/lang/accesrestreint_es.php
@@ -16,6 +16,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// C
 	'colonne_id' => 'No',
+	'creer_zone' => 'Crear una zona nueva',
 
 	// D
 	'descriptif' => 'Descripci&oacute;n',
@@ -24,12 +25,16 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'icone_creer_zone' => 'Crear una nueva zona',
 	'icone_menu_config' => 'Acceso restringido',
 	'icone_supprimer_zone' => 'Suprimir esta zona',
+	'info_ajouter_zones' => 'A&ntilde;adir todas las zonas',
+	'info_auteurs_lies_zone' => 'Los autores con acceso a esta zona',
 	'info_page' => 'Esta p&aacute;gina permite administrar las zonas de acceso restringido en el sitio',
 	'info_retirer_zone' => 'Eliminar de la zona',
 	'info_retirer_zones' => 'Eliminar de todas las zonas',
 
 	// P
 	'page_zones_acces' => 'Acceso Restringido',
+	'privee' => 'Privado',
+	'publique' => 'P&uacute;blico',
 
 	// R
 	'rubriques' => 'secciones',
@@ -42,7 +47,15 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'titre' => 'T&iacute;tulo',
 	'titre_ajouter_zone' => 'A&ntilde;adir la zona',
 	'titre_table' => 'Todas las zonas de acceso restringido',
-	'titre_zones_acces' => 'Zonas de acceso restringido'
+	'titre_zones_acces' => 'Zonas de acceso restringido',
+
+	// V
+	'voir_toutes' => 'Ver todas las zonas',
+
+	// Z
+	'zone_numero' => 'ZONA N&Uacute;MERO&nbsp;:',
+	'zone_restreinte_espace_prive' => 'Restringir el acceso a esta zona en el espacio privado',
+	'zone_restreinte_publique' => 'Restringir el acceso a esta zona en la parte p&uacute;blica'
 );
 
 ?>
