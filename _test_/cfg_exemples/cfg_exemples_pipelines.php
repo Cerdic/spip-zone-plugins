c8b5a3db223729c0f4ea2160105b46197a851929
marcimat@free.fr <marcimat@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-07-14 18:44:23 +0000

    - Faire refonctionner l'exemple de cfg inclu dans la page article
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@21492 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/cfg_exemples/cfg_exemples_pipelines.php
+++ _test_/cfg_exemples/cfg_exemples_pipelines.php
@@ -1,21 +1,27 @@
 <?php
 # a modifier pour #formulaire_xx
 function cfg_exemples_affiche_milieu($flux) {
-	$exec =  $flux['args']['exec'];
-	if ($exec=='articles'){
-
-		// compiler #FORMULAIRE_CFG{...}
-		include_spip('public/assembler');
-
-		$flux['data'] .= recuperer_fond(
-				"noisettes/formulaire_cfg", 
-				array(
-					'cfg_fond'=>'exemple_prive_article',
-					'cfg_id' => $flux['args']['id_article']
-				));
+	if ($flux['args']['exec']=='articles'){
+		$flux['data'] .= cfg_ajoute_formulaire('exemple_article_prive', $flux['args']['id_article'],true);
 	}
-	
 	return $flux;
+}
+
+/* ajouter la balise #formulaire_xx */
+function cfg_ajoute_formulaire($nom,$id='', $ajax=false){
+	include_spip('inc/flock');
+	sous_repertoire(_DIR_CACHE,'cfg');
+	$base = _DIR_CACHE . 'cfg/formulaire_'.$nom;
+
+	if (!file_exists($f = $base . ".html")
+	OR  ($GLOBALS['var_mode']=='recalcul')) {
+		$c = '#FORMULAIRE_'.strtoupper($nom).'{#ENV{cfg_id}}';
+		if ($ajax) $c =  "<div class='ajax'>$c</div>";
+		ecrire_fichier($f, $c);
+	}
+
+	include_spip('public/assembler');
+	return recuperer_fond($base, array('cfg_id' => $id));
 }
 
 ?>
