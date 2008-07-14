1e892a96350df27e8397f93c5439cea0754e9185
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-07-14 09:52:11 +0000

    langues (form)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@21471 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _stable_/forms/forms_et_tables_1_9_1/lang/form_es.php
@@ -0,0 +1,56 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'articles_utilisant' => 'Art&iacute;culos que usan este formulario',
+	'aucune_reponse' => 'No hay respuestas',
+
+	// C
+	'champs_formulaire' => 'Campos del formulario',
+
+	// F
+	'formulaire' => 'Formulario',
+
+	// I
+	'icone_ajouter_donnees' => 'A&ntilde;adir respuestas',
+	'icone_creer_table' => 'Crear un formulario nuevo',
+	'importer_donnees_csv' => 'Importar respuestas',
+	'info_supprimer_formulaire' => '&iquest;Realmente quieres suprimir este formulario?',
+	'info_supprimer_formulaire_reponses' => 'Hay respuestas asociadas con el formulario. 
+&iquest;Realmente quieres suprimirlo?',
+
+	// L
+	'lien_retirer_donnee_liante' => 'Retirar el enlace de esta respuesta',
+	'lien_retirer_donnee_liee' => 'Retirar esta respuesta',
+
+	// N
+	'nombre_reponses' => '@nombre@ respuestas',
+	'nouveau_formulaire' => 'Nuevo formulario',
+
+	// S
+	'suivi_reponses' => 'Ver las respuestas',
+	'supprimer_formulaire' => 'Suprimir este formulario',
+
+	// T
+	'telecharger_reponses' => 'Descarga las respuestas',
+	'texte_donnee_statut' => 'Estado de esta respuesta',
+	'texte_statut_poubelle' => 'Respuesta suprimida',
+	'texte_statut_prepa' => 'Respuesta en proceso de redacci&oacute;n',
+	'texte_statut_prop' => 'Respuesta propuesta',
+	'texte_statut_publie' => 'Respuesta publicada',
+	'texte_statut_refuse' => 'Respuesta rehusada',
+	'titre_formulaire' => 'T&iacute;tulo del formulario',
+	'toutes_tables' => 'Todos los formularios',
+	'type_des_tables' => 'Formularios',
+
+	// U
+	'une_reponse' => 'Una respuesta'
+);
+
+?>
