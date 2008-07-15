57a327edb1a8509de5f0dfa3d33ffe8bfc32b5fb
brunobergot@gmail.com <brunobergot@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-07-15 12:28:23 +0000

    et hop le fichier de langue fr pour le rhino
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@21502 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _dev_/nyroceros/lang/nyroceros_fr.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	
+	//C
+	'cfg_boite_nyroceros' => 'Configuration du plugin Nyroceros',
+	'cfg_descr_nyroceros' => 'Nyroceros : Plugin jQuery Nyromodal pour SPIP',
+	'cfg_inf_selecteur' => 'Indiquez la cible des &eacute;l&eacute;ments qui d&eacute;clencheront Nyromodal.',
+	'cfg_lgd_selecteur' => 'Choix du s&eacute;l&eacute;cteur',
+	'cfg_titre_nyroceros' => 'Nyroceros',
+	
+);
+?>
