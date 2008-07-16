d65b9b3c639f91fa9535b6ab575501af6e6d72cf
webmaster@geomaticien.com <webmaster@geomaticien.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-07-16 04:44:25 +0000

    Cat-Pied: creation
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@21515 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _dev_/acs/models/cat/composants/entete/ecrire/lang/pied_adm_en.php
@@ -0,0 +1,13 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+'pied' => 'Footer',
+'pied_description' => 'Container for other components.',
+'pied_help' => 'By default, Pied contain Ours component.',
+
+'acsPied1' => 'Footer 1',
+'acsPied2' => 'Footer 2',
+'acsPied3' => 'Footer 3'
+);
+?>
