1ad938b020efb7072cb5e83573343d4cc47c2f9f
webmaster@geomaticien.com <webmaster@geomaticien.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-07-16 05:44:41 +0000

    - Màj du modèle Cat pour Acs version 0.4 - fusion des composants Forum et Derniers_commentaires en un seul composant: Forums. - composant Liste_articles devient Articles. - composant Acces inclut au composant Articles (simplification).
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@21538 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/acs/models/cat/composants/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Définit la liste et l'ordre d'affichage des composants
- * On met en premier tous les composants non optionnels,
- * puis les conteneurs,
- * et enfin les composants insérables dans les conteneurs (optionnels, donc).
- */
-$choixComposants = array('fond','rubnav','onglets','liste_articles', 'ours','','entete','col1','col3','','acces','agenda','auteurs','banniere','bandeau','breves','custom', 'custom2', 'custom3', 'democratie', 'edito', 'encart', 'formulaire_recherche','forums','google-analytics','syndic','','audio','playlist','repimages','video');
-?>
