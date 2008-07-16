3e9e161e35b8bd8238419f259646bb887c516c43
webmaster@geomaticien.com <webmaster@geomaticien.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-07-16 05:37:36 +0000

    Cat-Articles: add
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@21536 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _dev_/acs/models/cat/composants/articles/ecrire/lang/articles_adm_en.php
@@ -0,0 +1,31 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+'articles' => 'Articles lists',
+'articles_description' => 'Articles lists for rubrics, Une, abstract, sitemap, ...',
+'articles_info' => 'Colors 2 to 6 are for rubrics level 2 to 6 in sitemap
+ <br /><br />
+Access control limit access to articles tagged with keyword _ide, _aut, or _adm to registered members, authors, and administrors.
+3 lasts articles published with keyword "Communiques" displays "A la Une" (Home) '.
+												 'before list of last published articles.
+.',
+
+'acsArticlesTitresFondColor' => 'Title background color',
+'acsArticlesBordColor' => 'Border top',
+'acsArticlesMargeBas' => 'Margin bottom',
+'acsArticlesNbLettres' => 'Nb of letters before cut',
+'acsArticlesLogoTailleMax' => 'max logo size',
+'acsArticlesRubFond2' => '2',
+'acsArticlesRubFond3' => '3',
+'acsArticlesRubFond4' => '4',
+'acsArticlesRubFond5' => '5',
+'acsArticlesRubFond6' => '6',
+'acsArticlesPuce' => 'Puce',
+
+'acsArticlesDates' => 'Display articles dates',
+'acsArticlesAut' => 'Display articles authors',
+'acsArticlesLock' => 'Access control'
+);
+?>
---
+++ _dev_/acs/models/cat/composants/articles/ecrire/lang/articles_adm_fr.php
@@ -0,0 +1,33 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+'articles' => 'Listes d\'articles',
+'articles_description' => 'Listes d\'articles, pour rubriques, une, r&eacute;sum&eacute;, plan ...',
+'articles_info' => 'Les couleurs 2 &agrave; 6 concernent les rubriques de niveau 2 &agrave; 6 dans le plan du site:
+ elles d&eacute;finissent l\'&eacute;claircissement ou l\'assombrissement du fond selon la profondeur.
+ <br /><br />
+ La fonction contrôle d\'accès limite l\'accès aux articles avec mot-clé 
+_ide, _aut, ou _adm aux visiteurs enregistrés, aux rédacteurs, et aux administrateurs.
+<br /><br />
+ Les 3 derniers articles de la rubrique avec le mot-cl&eacute; "Communiques" s\'affichent à la Une avant la liste des derniers articles publi&eacute;s.
+ ',
+
+'acsArticlesTitresFondColor' => 'Fond titres',
+'acsArticlesBordColor' => 'Bord sup&eacute;rieur',
+'acsArticlesMargeBas' => 'Marge inf&eacute;rieure',
+'acsArticlesNbLettres' => 'Nb de lettres avant coupure',
+'acsArticlesLogoTailleMax' => 'Taille maxi du logo',
+'acsArticlesRubFond2' => '2',
+'acsArticlesRubFond3' => '3',
+'acsArticlesRubFond4' => '4',
+'acsArticlesRubFond5' => '5',
+'acsArticlesRubFond6' => '6',
+'acsArticlesPuce' => 'Puce',
+
+'acsArticlesDates' => 'Affiche les dates des articles',
+'acsArticlesAut' => 'Affiche les auteurs des articles',
+'acsArticlesLock' => 'Contrôle d\'accès'
+);
+?>
