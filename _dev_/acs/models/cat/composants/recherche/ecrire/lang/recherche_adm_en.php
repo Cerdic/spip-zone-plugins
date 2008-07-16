823dba3fc92b573d33f38474af05e92252a5cb39
webmaster@geomaticien.com <webmaster@geomaticien.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-07-16 05:45:53 +0000

    Cat-Recherche: add
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@21539 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _dev_/acs/models/cat/composants/recherche/ecrire/lang/recherche_adm_en.php
@@ -0,0 +1,13 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+'recherche' => 'Search form',
+'recherche_description' => 'Input search form.',
+'recherche_help' => 'Generate "formulaires/recherche.html" form style for active skeleton. Spip search engine must be activated.',
+
+'acsRechercheBordColor' => 'Border',
+'acsRechercheOngletColor' => 'Legend'
+);
+?>
---
+++ _dev_/acs/models/cat/composants/recherche/ecrire/lang/recherche_adm_fr.php
@@ -0,0 +1,13 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+'recherche' => 'Formulaire recherche',
+'recherche_description' => 'Petit formulaire de saisie d\'une recherche.',
+'recherche_help' => 'G&egrave;re le style du formulaire "formulaires/recherche.html" du squelette actif ou du squelette par d&eacute;faut. Le moteur de recherche intégré de spip doit &ecirc;tre actif.',
+
+'acsRechercheBordColor' => 'Bord',
+'acsRechercheOngletColor' => 'Onglet'
+);
+?>
