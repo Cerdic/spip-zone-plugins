99ed2cceda07c958867fb430d9c0e58bf430058f
webmaster@geomaticien.com <webmaster@geomaticien.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-07-16 05:12:18 +0000

    màj for 0.4
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@21524 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _dev_/acs/models/cat/composants/entete/ecrire/lang/entete_adm_en.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+'entete' => 'Header',
+'entete_description' => 'Container for other components.',
+'entete_help' => 'By default, Header contains Banner and Bandeau.',
+
+'acsEntete1' => 'Header 1',
+'acsEntete2' => 'Header 2',
+'acsEntete3' => 'Header 3',
+'acsEntete4' => 'Header 4',
+'acsEnteteFavicon' => 'Bookmark icon'
+);
+?>
