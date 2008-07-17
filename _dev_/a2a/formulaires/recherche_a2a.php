f3d9b4c6e3b0e63bc8395770ffd6f59f74884b02
brunobergot@gmail.com <brunobergot@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-07-17 03:12:41 +0000

    on remplace le formulaire de recherche "maison" par un formulaire CVT ajax (reste encore le problème de l'action lier_article à passer en ajax mais là je sèche)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@21556 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _dev_/a2a/formulaires/recherche_a2a.php
@@ -0,0 +1,30 @@
+<?php
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+// chargement des valeurs par defaut des champs du formulaire
+function formulaires_recherche_a2a_charger($id_article){
+	$recherche = _request('recherche');
+	$id_article_orig = $id_article;
+
+	return 
+		array(
+			'recherche' => $recherche,
+			'id_article_orig' => $id_article_orig
+		);
+}
+
+function formulaires_recherche_a2a_verifier($id_article){
+	$erreurs['message_erreur'] = _T('a2a:pas_de_resultat');
+	return $erreurs;
+}
+
+function formulaires_recherche_a2a_traiter($id_article){
+	return 
+		array(
+			'recherche' => $recherche,
+			'id_article_orig' => $id_article_orig
+		);
+}
+
+?>
