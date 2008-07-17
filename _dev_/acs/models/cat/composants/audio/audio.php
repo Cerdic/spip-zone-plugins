c6022e9bbdb1a3e0b0ef5a1649d82574c28258c9
webmaster@geomaticien.com <webmaster@geomaticien.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-07-17 06:03:54 +0000

    Cat-Audio: updated soundManager2
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@21560 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/acs/models/cat/composants/audio/audio.php
+++ _dev_/acs/models/cat/composants/audio/audio.php
@@ -7,8 +7,12 @@
 class acsAudio extends Composant{
   public function insert_head($flux) {
     $flux .= '<script type="text/javascript">var acsAudio_img_pack = "'._DIR_PLUGIN_ACS.'models/'.$GLOBALS['meta']['acsModel'].'/composants/audio/img_pack/";</script>';
-    $flux .= '<script type="text/javascript" src="'.find_in_path('composants/audio/js/soundmanager2-jsmin.js').'"></script>';
-    $flux .= '<script type="text/javascript">soundManager.debugMode = false;</script>';
+    if (false) { // soundManager2 debug mode
+      $flux .= '<script type="text/javascript" src="'.find_in_path('composants/audio/js/soundmanager2.js').'"></script>';
+      $flux .= '<script type="text/javascript">soundManager.debugMode = true;</script>';
+    }
+    else
+      $flux .= '<script type="text/javascript" src="'.find_in_path('composants/audio/js/soundmanager2-nodebug-jsmin.js').'"></script>';
     $flux .= '<script type="text/javascript" src="'.find_in_path('composants/audio/js/audio.js').'"></script>';
     $flux .= '<script type="text/javascript">soundManager.createMovie("'.url_absolue(find_in_path('composants/audio/swf/soundmanager2.swf')).'");</script>';
     return $flux;
