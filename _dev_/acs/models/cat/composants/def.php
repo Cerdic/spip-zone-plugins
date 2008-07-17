6fe3babbf16df6d13e37c5583fc4d31a11905c0f
webmaster@geomaticien.com <webmaster@geomaticien.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-07-17 04:25:45 +0000

    Cat: valeurs par défaut pour l'initialisation (composants/def.php)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@21559 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _dev_/acs/models/cat/composants/def.php
@@ -0,0 +1,44 @@
+<?php
+#              ACS
+#          (Plugin Spip)
+#     http://acs.geomaticien.org
+#
+# Copyright Daniel FAIVRE, 2007-2008
+# Copyleft: licence GPL - Cf. LICENCES.txt
+
+/**
+ *  ACS model default values
+ */
+$def = array(
+	'acsOnglets1' => 'sommaire',
+	'acsOnglets2' => 'resume',
+	'acsOnglets3' => 'plan',
+	'acsOnglets4' => 'sites',
+	'acsOnglets5' => 'forums',
+	'acsOngletsFondColor' => 'ffffff',
+	'acsOngletsBordColor' => 'dfdfdf',
+	'acsOngletsCouleurInactif' => 'efefef',
+	'acsOngletsCouleurSurvol' => 'ffffff',
+  'acs' => '',
+  'acs' => '',
+  'acs' => '',
+  'acs' => '',
+  'acs' => '',
+  'acs' => '',
+  'acs' => '',
+  'acs' => '',
+  'acs' => '',
+  'acs' => '',
+  'acs' => '',
+  'acs' => '',
+  'acs' => '',
+  'acs' => '',
+  'acs' => '',
+  'acs' => '',
+  'acs' => '',
+  'acs' => '',
+  'acs' => '',
+  'acs' => '',
+
+);
+?>
