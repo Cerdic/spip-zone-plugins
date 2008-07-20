d6e982bcb7ad166394c9e87ea1ef95346a38ce83
webmaster@geomaticien.com <webmaster@geomaticien.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-07-20 00:37:24 +0000

    Suppression de traductions : "adm" devient "ecrire", pour plus de cohérence avec SPIP-core.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@21594 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/acs/models/cat/composants/breves/ecrire/lang/breves_adm_en.php
+++
@@ -1,9 +0,0 @@
-<?php
-// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// ACS component specific - spécifique au composant ACS
-
-$GLOBALS[$GLOBALS['idx_lang']] = array(
-'breves' => 'News',
-'breves_description' => 'Usable only if Spip news are activated.',
-);
-?>
--- _dev_/acs/models/cat/composants/breves/ecrire/lang/breves_adm_fr.php
+++
@@ -1,9 +0,0 @@
-<?php
-// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// ACS component specific - spécifique au composant ACS
-
-$GLOBALS[$GLOBALS['idx_lang']] = array(
-'breves' => 'Br&egrave;ves',
-'breves_description' => 'Activable si les br&egrave;ves de Spip sont actives.',
-);
-?>
