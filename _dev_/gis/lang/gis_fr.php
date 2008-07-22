37baba106feab3acf4a93d621c6cc695a65c8a4a
brunobergot@gmail.com <brunobergot@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-07-22 17:15:21 +0000

    un formulaire CVT pour spip 2.0 qui permet de poster des articles depuis le public en les situants sune carte et d'y ajouter des documents
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@21635 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/gis/lang/gis_fr.php
+++ _dev_/gis/lang/gis_fr.php
@@ -5,15 +5,29 @@
 // Traduction -- Pierre FICHES <pierre.fiches@free.fr>
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-//elements de formulaire pour editer les coordonnees
-'clave_engadida' => 'Clef ajout&eacute;e: ',
-'clic_mapa' => 'Cliquer sur la carte pour changer les coordonn&eacute;es',
-'clic_desplegar' => 'd&eacute;plier',
-'configurar_gis' => 'Configurer le Plugin Gis',
-'conseguir' => '(obtenir)',
-'cambiar' => 'changer les coordonn&eacute;es',
-'boton_actualizar' => 'actualiser',
-'default_geoloc' => 'Position par defaut des cartes :'
+
+	//elements de formulaire pour editer les coordonnees
+	
+	"clave_engadida" => "Clef ajout&eacute;e: ",
+	"clic_mapa" => "Cliquer sur la carte pour changer les coordonn&eacute;es",
+	"clic_desplegar" => "d&eacute;plier",
+	"configurar_gis" => "Configurer le Plugin Gis",
+	"conseguir" => "(obtenir)",
+	"cambiar" => "changer les coordonn&eacute;es",
+	"boton_actualizar" => "actualiser",
+	"default_geoloc" => "Position par defaut des cartes :",
+	
+	//elements du formulaire public
+	"bouton_documents" => "Valider",
+	"document_joint" => "Documents joints",
+	"document_supprime" => "Document(s) supprim&eacute;(s)",
+	"erreur_ajout_article" => "Erreur lors de l'ajout de l'article",
+	"erreur_copie_impossible" => "Impossible de copier le document",
+	"erreur_formats_acceptes" => "Formats accept&#233;s : @formats@.",
+	"erreur_texte" => "Veuillez entrer un texte",
+	"erreur_upload" => "Erreur lors du chargement du fichier",
+	'ok_formulaire_soumis' => 'Votre article a &eacute;t&eacute; envoy&eacute; sur le site.',
+
 );
 
 ?>
