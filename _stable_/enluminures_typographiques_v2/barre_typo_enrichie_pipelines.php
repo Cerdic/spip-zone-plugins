004d58ea993b6cf6e297e2a2fbfc0cc62d58f50c
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-07-22 23:49:43 +0000

    Pour les enluminures typo aussi : retour de la prévisualisation après clic sur les boutons
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@21643 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/enluminures_typographiques_v2/barre_typo_enrichie_pipelines.php
+++ _stable_/enluminures_typographiques_v2/barre_typo_enrichie_pipelines.php
@@ -2,14 +2,14 @@
 define('_DIR_BTE_IMG', _DIR_PLUGIN_TYPOENLUMINEE.'/img_pack/icones_barre/');
 
 // toolbox pour les paragraphes
-function afficher_boutonsavances($champ, $champhelp) {
+function afficher_boutonsavances($champ, $champhelp, $num_barre) {
 	$reta = '';
-	$reta .= bouton_barre_racc("barre_raccourci('\n\n{2{','}2}\n\n',$champ)", _DIR_BTE_IMG.'intertitre2.png', _T('enlumtypo:barre_intertitre2'), $champhelp);
-	$reta .= bouton_barre_racc("barre_raccourci('\n\n{3{','}3}\n\n',$champ)", _DIR_BTE_IMG.'intertitre3.png', _T('enlumtypo:barre_intertitre3'), $champhelp);
-	$reta .= bouton_barre_racc("barre_raccourci('[|','|]',$champ)", _DIR_BTE_IMG.'center.png', _T('enlumtypo:barre_centrer'), $champhelp);
-	$reta .= bouton_barre_racc("barre_raccourci('[/','/]',$champ)", _DIR_BTE_IMG.'right.png', _T('enlumtypo:barre_alignerdroite'), $champhelp);
-	$reta .= bouton_barre_racc("barre_raccourci('[(',')]',$champ)", _DIR_BTE_IMG.'cadretexte.png', _T('enlumtypo:barre_encadrer'), $champhelp);
-	$reta .= bouton_barre_racc("barre_raccourci('<del>','</del>',$champ)", _DIR_BTE_IMG.'text_strikethrough.png', _T('enlumtypo:barre_barre'), $champhelp);
+	$reta .= bouton_barre_racc("barre_raccourci('\n\n{2{','}2}\n\n',$champ, $num_barre)", _DIR_BTE_IMG.'intertitre2.png', _T('enlumtypo:barre_intertitre2'), $champhelp);
+	$reta .= bouton_barre_racc("barre_raccourci('\n\n{3{','}3}\n\n',$champ, $num_barre)", _DIR_BTE_IMG.'intertitre3.png', _T('enlumtypo:barre_intertitre3'), $champhelp);
+	$reta .= bouton_barre_racc("barre_raccourci('[|','|]',$champ, $num_barre)", _DIR_BTE_IMG.'center.png', _T('enlumtypo:barre_centrer'), $champhelp);
+	$reta .= bouton_barre_racc("barre_raccourci('[/','/]',$champ, $num_barre)", _DIR_BTE_IMG.'right.png', _T('enlumtypo:barre_alignerdroite'), $champhelp);
+	$reta .= bouton_barre_racc("barre_raccourci('[(',')]',$champ, $num_barre)", _DIR_BTE_IMG.'cadretexte.png', _T('enlumtypo:barre_encadrer'), $champhelp);
+	$reta .= bouton_barre_racc("barre_raccourci('<del>','</del>',$champ, $num_barre)", _DIR_BTE_IMG.'text_strikethrough.png', _T('enlumtypo:barre_barre'), $champhelp);
 		
 	$tableau_formulaire = '
 <table class="spip_barre" style="width: auto; padding: 1px!important; border-top: 0px;" summary="">
@@ -30,14 +30,14 @@ function afficher_boutonsavances($champ, $champhelp) {
 }
 
 function TypoEnluminee_BT_toolbox($params) {
-	$params['flux'] .= afficher_boutonsavances($params['champ'], $params['help']);
+	$params['flux'] .= afficher_boutonsavances($params['champ'], $params['help'], $params['num']);
 	return $params;
 }
 
 function TypoEnluminee_BT_caracteres($params) {
-	$params['flux'] .= bouton_barre_racc("barre_raccourci('[*','*]',$params[champ])", _DIR_BTE_IMG.'miseenevidence.png', _T('enlumtypo:barre_miseenevidence'), $params['help'])
-		. bouton_barre_racc("barre_raccourci('&lt;sup&gt;','&lt;/sup&gt;',$params[champ])", _DIR_BTE_IMG.'exposant.png', _T('enlumtypo:barre_exposant'), $params['help'])
-		. bouton_barre_racc("barre_raccourci('&lt;sc&gt;','&lt;/sc&gt;',$params[champ])", _DIR_BTE_IMG.'petitescapitales.png', _T('enlumtypo:barre_petitescapitales'), $params['help']);
+	$params['flux'] .= bouton_barre_racc("barre_raccourci('[*','*]',$params[champ], $params[num])", _DIR_BTE_IMG.'miseenevidence.png', _T('enlumtypo:barre_miseenevidence'), $params['help'])
+		. bouton_barre_racc("barre_raccourci('&lt;sup&gt;','&lt;/sup&gt;',$params[champ], $params[num])", _DIR_BTE_IMG.'exposant.png', _T('enlumtypo:barre_exposant'), $params['help'])
+		. bouton_barre_racc("barre_raccourci('&lt;sc&gt;','&lt;/sc&gt;',$params[champ], $params[num])", _DIR_BTE_IMG.'petitescapitales.png', _T('enlumtypo:barre_petitescapitales'), $params['help']);
     return $params;
 }
 
