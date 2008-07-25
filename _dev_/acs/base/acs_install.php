105f89f5db0ad9df57469c6624b3cb4350bcf772
webmaster@geomaticien.com <webmaster@geomaticien.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-07-25 02:51:00 +0000

    Init
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@21699 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/acs/base/acs_install.php
+++ _dev_/acs/base/acs_install.php
@@ -10,8 +10,7 @@ function acs_install($action){
   switch ($action)
   {
     case 'test':
-      return false;
-      //return isset($GLOBALS['meta']['acsOnglets1']); // TODO: remove any Model's components dependency
+      return isset($GLOBALS['meta']['acsModel']); // TODO: remove any Model's components dependency
       break;
 
     case 'install':
@@ -31,11 +30,12 @@ function acs_set_default_values() {
     include $defaults;
   if (is_array($def)) {
     foreach($def as $var=>$value) {
-      ecrire_meta($var, $value);      
+      ecrire_meta($var, $value);
     }
     ecrire_metas();
   }
 }
+
 function acs_reset_vars() {
   spip_query("delete FROM spip_meta where left(nom,3)='acs'");
 }
--- _dev_/acs/models/cat/composants/def.php
+++ _dev_/acs/models/cat/composants/def.php
@@ -6,10 +6,42 @@
 # Copyright Daniel FAIVRE, 2007-2008
 # Copyleft: licence GPL - Cf. LICENCES.txt
 
+// On initialise les valeurs des variables du modèle 
+
 /**
  *  ACS model default values
  */
 $def = array(
+	'acsAgendaUse' => 'oui',
+	'acsArticlesUse' => 'oui',
+  'acsAudioUse' => 'oui',
+	'acsBanniereUse' => 'oui',
+	'acsBanniereUseLogo' => 'oui',
+	'acsBandeauUse' => 'oui',
+	'acsBandeauContenu' => 'Under construction',
+  'acsCol1Use' => 'oui',
+  'acsCol11' => 'recherche',
+	'acsCol12' => 'rubnav',
+  'acsCol13' => 'forums',
+  'acsCol15' => 'syndic',
+  'acsCol3Use' => 'oui',
+  'acsCol31' => 'agenda',
+	'acsCol32' => 'playlist',
+	'acsFondUse' => 'oui',
+	'acsFondColor' => 'f4f4f4',
+	'acsFondBordColor' => 'cfcfcf',
+	'acsFondBordLargeur' => 'thin',
+	'acsFondBordStyle' => 'solid',
+	'acsFondTexte' => '002200',
+	'acsFondLink' => '00008d',
+	'acsFondlinkHover' => '0000f4',
+	'acsForumsUse' => 'oui',
+  'acsEditoUse' => 'oui',
+  'acsEnteteUse' => 'oui',
+  'acsEntete1' => 'banniere',
+  'acsEntete2' => 'bandeau',
+  'acsEnteteFavicon' => 'favicon.ico',
+  'acsOngletsUse' => 'oui',
 	'acsOnglets1' => 'sommaire',
 	'acsOnglets2' => 'resume',
 	'acsOnglets3' => 'plan',
@@ -19,6 +51,20 @@ $def = array(
 	'acsOngletsBordColor' => 'dfdfdf',
 	'acsOngletsCouleurInactif' => 'efefef',
 	'acsOngletsCouleurSurvol' => 'ffffff',
+	'acsOursUse' => 'oui',
+	'acsPiedUse' => 'oui',
+	'acsPied1' => 'ours',
+  'acsPlayListUse' => 'no',
+	'acsRechercheUse' => 'oui',
+	'acsRubnavUse' => 'oui',
+	'acsRubnavFond' => 'f4f4ff',
+	'acsRubnavFond2' => 'efefff',
+	'acsRubnavFond3' => 'e4e4ff',
+	'acsRubnavFondHover' => 'f8f8df',
+	'acsRubnavSep' => 'dfdfdf',
+  'acsSyndicUse' => 'oui',
+	'acsVideoUse' => 'oui',
+
   'acs' => '',
   'acs' => '',
   'acs' => '',
@@ -28,17 +74,8 @@ $def = array(
   'acs' => '',
   'acs' => '',
   'acs' => '',
-  'acs' => '',
-  'acs' => '',
-  'acs' => '',
-  'acs' => '',
-  'acs' => '',
-  'acs' => '',
-  'acs' => '',
-  'acs' => '',
-  'acs' => '',
-  'acs' => '',
-  'acs' => '',
+  'acsDerniereModif' => time(),
+  'acsModel' => 'cat',
 
 );
 ?>
