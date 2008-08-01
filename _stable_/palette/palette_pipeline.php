e08afe376db78403bc038b40114f604ad60d58fd
maieulrouquette@tele2.fr <maieulrouquette@tele2.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-08-01 20:04:12 +0000

    a l'install, remplit a "on" les champs de CFG de 'palette'
    a la desintall, doit normalement vider les champs CFG de 'palette'
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@21842 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/palette/palette_pipeline.php
+++ _stable_/palette/palette_pipeline.php
@@ -1,5 +1,28 @@
 <?php
 
+function palette_install($action){
+	
+	switch($action){
+		
+		case 'install':
+			
+			if (function_exists('ecrire_config')){
+				ecrire_config('palette/palette_public','on');
+				ecrire_config('palette/palette_ecrire','on');
+				}
+			break;
+			
+		case 'uninstall':
+			
+			if (function_exists('effacer_config')){
+				effacer_config('palette/palette_public');
+				effacer_config('palette/palette_ecrire');
+			}
+			break;
+			
+	}
+}
+
 function palette_insert_head($flux) {
   $cfg = unserialize($GLOBALS['meta']['palette']);
   if ($cfg['palette_public'] =='on')
