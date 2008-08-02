df194d066492b383c6b0a089a934d87cd95ce9e2
webmaster@geomaticien.com <webmaster@geomaticien.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-08-02 23:31:00 +0000

    Palette aux couleurs de l'interface privée (partie privée: sur le site public, la palette adopte les mêmes couleurs par défaut que prive/style_prive.html)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@21860 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/palette/inc/palette_header.php
+++ _stable_/palette/inc/palette_header.php
@@ -4,7 +4,9 @@
  * Définit l'entête par défaut pour le plugin Palette
  */
 function inc_palette_header_dist() {
-	return '<link rel="stylesheet" href="'.find_in_path('palette.css').'" type="text/css" />'."\n".
+  if ($GLOBALS['couleur_foncee']) // Couleurs de la palette pour l'espace prive
+    $couleurs_admin = '&couleur_foncee='.substr($GLOBALS['couleur_foncee'],1).'&couleur_claire='.substr($GLOBALS['couleur_claire'],1);
+	return '<link rel="stylesheet" href="'._DIR_RACINE.'spip.php?page=palette.css'.$couleurs_admin.'" type="text/css" />'."\n".
 				'<link rel="stylesheet" href="'.find_in_path('lib/farbtastic/farbtastic.css').'" type="text/css" />'."\n".
 				'<script type="text/javascript" src="'._DIR_RACINE.'spip.php?page=palette.js"></script>'."\n";
 }
