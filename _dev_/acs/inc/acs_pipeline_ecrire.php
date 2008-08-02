28f668e8698d3f224e960078c464890a65d8b4a6
webmaster@geomaticien.com <webmaster@geomaticien.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-08-02 23:10:16 +0000

    style prive
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@21859 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/acs/inc/acs_pipeline_ecrire.php
+++ _dev_/acs/inc/acs_pipeline_ecrire.php
@@ -33,7 +33,7 @@ function acs_ajouterBouton($boutons_admin) {
 }
 
 function acs_header_prive($flux) {
-  $url_css = '../spip.php?page=acs_style_prive.css&couleur_foncee='.substr($GLOBALS['couleur_foncee'], 1).'&couleur_claire='.substr($GLOBALS['couleur_claire'], 1);
+  $url_css = '../spip.php?page=acs_style_prive.css&couleur_foncee='.substr($GLOBALS['couleur_foncee'],1).'&couleur_claire='.substr($GLOBALS['couleur_claire'],1);
   return $flux.'<link rel="stylesheet" href="'.$url_css.'" type="text/css" media="projection, screen, tv" />'.
 '<script type="text/javascript" src="'._DIR_ACS.'javascript/jquery.debug.js"></script>'.
 '<script type="text/javascript">var DEBUG = '.
