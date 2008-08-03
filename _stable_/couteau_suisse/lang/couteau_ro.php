e10f78402cc52289de49095f6ab7778b6b0f9f98
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-08-03 16:25:39 +0000

    langues (couteau)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@21868 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/couteau_suisse/lang/couteau_ro.php
+++ _stable_/couteau_suisse/lang/couteau_ro.php
@@ -29,6 +29,17 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'page_lien' => 'Pagina @page@ : @title@',
 	'page_precedente' => 'Pagina anterioar&#259;',
 	'page_suivante' => 'Pagina urm&#259;toare',
+	'plugin_xml' => 'Reune&#351;te &icirc;ntr-un singur plugin o list&#259; de mici func&#355;ionalit&#259;&#355;i noi &#351;i utile care amelioreaz&#259; gestiunea site-ului dumneavoastr&#259; SPIP.
+
+Fiecare dintre aceste utilit&#259;&#355;i poate fi activat&#259; de c&#259;tre utilizator &icirc;n [pagina de administrare a plug-in-ului->./?exec=admin_couteau_suisse] &#351;i poate gestiona un anumit num&#259;r de variabile: face&#355;i Clic pe {{Configurare}}, dup&#259; aceea alege&#355;i pagina {{Cu&#355;itul Elve&#355;ian}}.
+
+Categoriile disponibile sunt: Administrare, Amelior&#259;ri tipografice, Scurt&#259;turi tipografice, Afi&#351;aj public, Balize, filtre, criterii.
+
+Descoperi&#355;i &icirc;n aces plug-in tool-urile dumneavoastr&#259; favorite: {Ghilimele tipografice}, {Fonturi dr&#259;gu&#355;e}, etc., etc.
+
+Nu ezita&#355;i s&#259; consulta&#355;i articolele de documenta&#355;ie publicate la : [spip-contrib.net->http://www.spip-contrib.net/Le-Couteau-Suisse].
+
+Compatibilitate : SPIP toate versiunile 1.9x',
 
 	// S
 	'smileys_dispos' => 'Emoticon-uri disponibile',
@@ -36,7 +47,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'sommaire_page_long' => ',&nbsp;page&nbsp;@page@',
 	'sommaire_titre' => 'Sumar',
 	'stats_auteur' => '@icon@ @nom@, &nbsp;@date@',
-	'stats_date' => '<NEW>@jour@/@mois@/@annee@&nbsp;&agrave;&nbsp;@h@h@m@',
+	'stats_date' => '@jour@/@mois@/@annee@&nbsp;la&nbsp;@h@h@m@',
 
 	// T
 	'texte_formatspip' => 'TEXT ORIGINAL &Icirc;N FORMAT SPIP',
