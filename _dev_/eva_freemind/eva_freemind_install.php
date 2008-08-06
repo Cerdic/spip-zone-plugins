82ac3400bd563012831301db8509ca8cc1dbedce
olivier.gautier@ac-rouen.fr <olivier.gautier@ac-rouen.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-08-06 09:15:33 +0000

    Eva_freemind en développement pour SPIP 2.0 en prévision d'ajouts de Dominique Lepaisan
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@21951 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _dev_/eva_freemind/eva_freemind_install.php
@@ -0,0 +1,35 @@
+<?php
+/******************************************************************
+***  Ce plugin eva_freemind, créé par Olivier Gautier et ***
+*** Dominique Lepaisan est mis à disposition sous un ***
+*** contrat GNU/GPL consultable à l'adresse                    ***
+***      http://www.april.org/gnu/gpl_french.html		     ***
+******************************************************************/
+function eva_freemind_install($action){
+	
+	switch ($action){
+	
+	case 'test':
+	$test_req=sql_select('inclus','spip_types_documents',"extension = 'mm'");
+	$test_ta=sql_fetch($test_req);
+	$test=$test_ta['inclus'];
+	if ((!@opendir(_DIR_IMG."icones")) OR (!@fopen(_DIR_IMG."icones/mm.png", "r")) OR !$test) {return false;}
+	else {return true;}
+	break;
+
+	case 'install':
+	if (!@opendir(_DIR_IMG."icones")) {mkdir(_DIR_IMG."icones");}
+	if (!@fopen(_DIR_IMG."icones/mm.png", "r")) {copy(_DIR_PLUGIN_EVA_FREEMIND.'img_pack/mm.png',_DIR_IMG.'icones/mm.png');}
+	$test_req=sql_select('inclus','spip_types_documents',"extension = 'mm'");
+	$test_ta=sql_fetch($test_req);
+	$test=$test_ta['inclus'];
+	if (!$test) {
+	sql_insertq('spip_types_documents',array('extension' => 'mm','mime_type' => 'application/x-freemind','titre' => 'Freemind','inclus' => 'embed','upload' => 'oui'));
+	}
+	break;
+       
+	case 'uninstall':
+	break;
+	}
+}
+?>
