674ad8c687a6528161eed3a156327eaa12cfe128
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-08-09 20:17:01 +0000

    langues (couteau)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@22005 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _stable_/couteau_suisse/lang/couteau_ast.php
@@ -0,0 +1,61 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'attente_confirmation' => 'N\'espera de confirmaci&oacute;n',
+
+	// D
+	'derniers_connectes' => 'Caberos coneut&aacute;os:',
+
+	// E
+	'explique_spam' => 'Atenci&oacute;n: usaste etiquetes, carauteres o pallabres torgaos.<br /><br />&iexcl;Vuelve p\'atr&aacute;s!',
+
+	// L
+	'lutte_spam' => 'Llucha escontra\'l SPAM',
+
+	// N
+	'nom_forum' => '&iexcl;Ties que poner el to nome!',
+	'non_confirmes' => 'Nun confirm&aacute;os:',
+
+	// P
+	'page_debut' => 'Primera p&aacute;xina',
+	'page_fin' => 'Cabera p&aacute;xina',
+	'page_lien' => 'P&aacute;xina @page@: @title@',
+	'page_precedente' => 'P&aacute;xina anterior',
+	'page_suivante' => 'P&aacute;xina siguiente',
+	'plugin_xml' => 'Xunta n\'un solu plugin una llista de peque&ntilde;es funciones nueves y ferramientes pa meyorar la xesti&oacute;n del to sitiu SPIP.
+
+Cauna d\'estes ferramientes puede activase o non pol usuariu so [la p&aacute;xina d\'alministraci&oacute;n del plugin->./?exec=admin_couteau_suisse] y manexar ciertu n&uacute;mberu de variables : picar en {{Afitamientu}}, llueu escoyer la pesta&ntilde;a {{La Navaya Suiza}}.
+
+Les categor&iacute;es disponibles son: Alministraci&oacute;n, Meyores tipogr&aacute;fiques, Atayos tipogr&aacute;ficos, Aspeutu p&uacute;blicu, Balices, filtros, criterios.
+
+Descubre nesti plugin les tos ferramientes favorites: {Esponentes tipogr&aacute;ficos}, {Comilles tipogr&aacute;fiques}, {Marques de llista guapes}, {Un sumariu pa los tos art&iacute;culos}, {Caruques}, {Suprimir el n&uacute;mberu}, {URLs guapes}, {SPIP y los enllaces... esternos}, {Ensin foros an&oacute;nimos}, {Divide en p&aacute;xines y pesta&ntilde;es}, etc., etc.
+
+Si quies consultar los art&iacute;culos de la documentaci&oacute;n del plugin, tan espublizaos en: [spip-contrib.net->http://www.spip-contrib.net/Le-Couteau-Suisse].
+
+Compatibilid&aacute;: SPIP toes les versiones 1.9x',
+
+	// S
+	'smileys_dispos' => 'Caruques disponibles:',
+	'sommaire_page' => ',&nbsp;p@page@',
+	'sommaire_page_long' => ',&nbsp;p&aacute;xina&nbsp;@page@',
+	'sommaire_titre' => 'Sumariu',
+	'stats_auteur' => '@icon@ @nom@, el&nbsp;@date@',
+	'stats_date' => '@jour@/@mois@/@annee@&nbsp;a les&nbsp;@h@:@m@&nbsp;h.',
+
+	// T
+	'texte_formatspip' => 'TESTU ORIXINAL EN FORMATU SPIP',
+	'textes_formatspip' => 'TESTOS ORIXINALES EN FORMATU SPIP',
+	'titre' => 'La Navaya Suiza',
+
+	// V
+	'variable_vide' => '(Vac&iacute;u)'
+);
+
+?>
