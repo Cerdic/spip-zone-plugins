bd5f92f9dd145275165651c282e10b794b92c0dc
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-08-15 17:56:23 +0000

    renommage, mise au point des textes, nouveaux filtres et traitement des unites
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@22056 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/rainette/inc/rainette_filtres.php
+++ _stable_/rainette/inc/rainette_filtres.php
@@ -1,34 +1,50 @@
 <?php
 /**
- * filtre icone_meteo
+ * filtre rainette_icone_meteo
  *
  * @param string temps
  * @param string chemin
  * @param string extension
- * @return string image correspondant Ì  la prÌ©vision
+ * @return string image correspondant au code de temps
  * @author Pierre Basson
  **/
 # cf pour le choix des icones http://liquidweather.net/icons.php
-function rainette_icone_meteo($icone, $chemin='', $extension="png"){
+function rainette_icone_meteo($code, $chemin='', $extension="png"){
+	$html_icone = '';
+	include_spip('inc/rainette_utils');
+
 	if (!$chemin) $chemin = _RAINETTE_ICONES_PATH;
-	include_spip('inc/rainette_utils');
-	$temps = rainette_decode_icone($icone);
-	if ($img = find_in_path($chemin.$icone.'.'.$extension)) {
+	$temps = code2icone($code);
+
+	// Le dossier personnalise ou le dossier passe en argument a bien l'icone requise
+	if ($img = find_in_path($chemin.$temps.'.'.$extension)) {
 		list ($h,$l) = taille_image($img);
-		return '<img src="'.$img.'" alt="'.rainette_traduire_temps($temps).'" title="'.rainette_traduire_temps($temps).'" width="'.$l.'" height="'.$h.'" />';
-	} elseif (
-	  ($chemin = 'img_meteo/')
-	  AND	($img = find_in_path($chemin.$temps.'.'.$extension))) {
-		#alors le dossier /grenouille n'a pas d'image, on reprend la fonction de depart (avec images de img_meteo)
+		$html_icone = '<img src="'.$img.'" alt="'.rainette_resume_meteo($code).'" title="'.rainette_resume_meteo($code).'" width="'.$l.'" height="'.$h.'" />';
+	} 
+	// Le dossier personnalise n'a pas d'image, on prend l'icone par defaut dans le repertoire img_meteo/
+	elseif (($chemin = 'img_meteo/') && ($img = find_in_path($chemin.$temps.'.'.$extension))) {
 		list ($h,$l) = taille_image($img);
-		return '<img src="'.$img.'" alt="'.rainette_traduire_temps($temps).'" title="'.rainette_traduire_temps($temps).'" width="'.$l.'" height="'.$h.'" />';
+		$html_icone = '<img src="'.$img.'" alt="'.rainette_resume_meteo($code).'" title="'.rainette_resume_meteo($code).'" width="'.$l.'" height="'.$h.'" />';
 	}
-	return "";
+	return $html_icone;
 }
-function rainette_icone_details($icone){
+
+function rainette_resume_meteo($code){
 	include_spip('inc/rainette_utils');
-	$temps = rainette_decode_icone($icone);
-	return rainette_traduire_temps($temps);
+	$resume = _T('rainette:meteo_'.code2icone($code));
+	return $resume;
+}
+
+function rainette_texte_direction($direction){
+	return _T('rainette:direction_'.$direction);
+}
+
+function rainette_afficher_unite($valeur, $type_valeur=''){
+	$suffixe = (_RAINETTE_SYSTEME_MESURE == 'm') ? 'metrique' : 'standard';
+	$espace = (($type_valeur == 'temperature') || 
+			   ($type_valeur == 'humidite') || ($type_valeur == 'angle')) ? '' : '&nbsp;';
+	$valeur_affichee = strval($valeur).$espace._T('rainette:unite_'.$type_valeur.'_'.$suffixe);
+	return $valeur_affichee;
 }
 
 /**
@@ -40,13 +56,16 @@ function rainette_icone_details($icone){
  * @return string
  * @author Cedric Morin
  */
-function rainette_croaaaaa_previsions($code_frxx, $nb_jours_affiche=99, $modele='previsions_jour'){
+function rainette_croaaaaa_previsions($code_frxx, $nb_jours_affiche=0, $modele='previsions_jour'){
 	include_spip('inc/rainette_utils');
-	$nom_fichier = rainette_charge_meteo($code_frxx, 'previsions');
+	if ($nb_jours_affiche == 0) $nb_jours_affiche = _RAINETTE_JOURS_PREVISION;
+	$nb_jours_affiche = min($nb_jours_affiche, _RAINETTE_JOURS_PREVISION);
+	
+	$nom_fichier = charger_meteo($code_frxx, 'previsions');
 	lire_fichier($nom_fichier,$tableau);
 	$tableau = unserialize($tableau);
 	$texte = "";
-	while (count($tableau) AND $nb_jours_affiche--){
+	while (count($tableau) && $nb_jours_affiche--){
 		$page = evaluer_fond("modeles/$modele", array_shift($tableau));			
 		$texte .= $page['texte'];
 	}
@@ -55,7 +74,7 @@ function rainette_croaaaaa_previsions($code_frxx, $nb_jours_affiche=99, $modele=
 
 function rainette_croaaaaa_conditions($code_frxx, $modele='conditions_tempsreel'){
 	include_spip('inc/rainette_utils');
-	$nom_fichier = rainette_charge_meteo($code_frxx, 'conditions');
+	$nom_fichier = charger_meteo($code_frxx, 'conditions');
 	lire_fichier($nom_fichier,$tableau);
 	$tableau = unserialize($tableau);
 	$page = evaluer_fond("modeles/$modele", $tableau);			
--- _stable_/rainette/inc/rainette_utils.php
+++ _stable_/rainette/inc/rainette_utils.php
@@ -1,81 +1,47 @@
 <?php
 
-function rainette_decode_icone($code){
- 	$tableau_meteo	= array(
-							"1"	=> "pluie",
-							"2"	=> "pluie",
-							"3"	=> "orage",
-							"4"	=> "orage",
-							"5"	=> "pluie",
-							"6"	=> "neige",
-							"7"	=> "verglas",
-							"8"	=> "pluie",
-							"9"	=> "pluie",
-							"10"	=> "pluie",
-							"11"	=> "pluie",
-							"12"	=> "pluie",
-							"13"	=> "neige",
-							"14"	=> "neige",
-							"15"	=> "neige",
-							"16"	=> "neige",
-							"17"	=> "orage",
-							"18"	=> "neige",
-							"19"	=> "brouillard",
-							"20"	=> "brouillard",
-							"21"	=> "brouillard",
-							"22"	=> "brouillard",
-							"23"	=> "vent",
-							"24"	=> "vent",
-							"25"	=> "vent",
-							"26"	=> "nuages",
-							"27"	=> "lune-nuages",
-							"28"	=> "soleil-nuages",
-							"29"	=> "lune-nuage",
-							"30"	=> "soleil-nuage",
-							"31"	=> "lune",
-							"32"	=> "soleil",
-							"33"	=> "lune-nuage",
-							"34"	=> "soleil-nuage",
-							"35"	=> "orage",
-							"36"	=> "soleil",
-							"37"	=> "orage",
-							"38"	=> "orage",
-							"39"	=> "pluie",
-							"40"	=> "pluie",
-							"41"	=> "neige",
-							"42"	=> "neige",
-							"43"	=> "neige",
-							"44"	=> "soleil-nuage",
-							"45"	=> "pluie",
-							"46"	=> "neige",
-							"47"	=> "orage",
-							"48"	=> "inconnu",
-						);
-	return isset($tableau_meteo[$code])?$tableau_meteo[$code]:$tableau_meteo[48];
+function code2icone($icon){
+	$r = "na";
+	if (($icon >= 1) && ($icon < 48)) $r = strval($icon);
+	return $r;
 }
 
-/**
- * filtre traduire_meteo
- *
- * @param string temps
- * @return string traduction
- * @author Pierre Basson
- **/
-function rainette_traduire_temps($temps) {
-	if (!$temps) return '';
-	return _T('grenouille:meteo_'.$temps);
+function angle2direction($degre){
+	$dir = '';
+	switch(round($degre / 22.5) % 16)
+	{
+		case 0:  $dir = 'N'; break;
+		case 1:  $dir = 'NNE'; break;
+		case 2:  $dir = 'NE'; break;
+		case 3:  $dir = 'ENE'; break;
+		case 4:  $dir = 'E'; break;
+		case 5:  $dir = 'ESE'; break;
+		case 6:  $dir = 'SE'; break;
+		case 7:  $dir = 'SSE'; break;
+		case 8:  $dir = 'S'; break;
+		case 9:  $dir = 'SSW'; break;
+		case 10: $dir = 'SW'; break;
+		case 11: $dir = 'WSW'; break;
+		case 12: $dir = 'W'; break;
+		case 13: $dir = 'WNW'; break;
+		case 14: $dir = 'NW'; break;
+		case 15: $dir = 'NNW'; break;
+	}
+	return $dir;
 }
+
 
 /**
  * lire le xml fournit par le service meteo et en extraire les infos interessantes
  * retournees en tableau jour par jour
  * utilise le parseur xml de Spip
  *
+ * ne gere pas encore le jour et la nuit de la date courante suivant l'heure!!!!
  * @param array $xml
  * @return array
  * @author Cedric Morin
  */
-function rainette_xml2tab_previsions($xml){
+function xml2tab_previsions($xml){
 	$tableau = array();
 	$n = spip_xml_match_nodes(",^dayf,",$xml,$previsions);
 	if ($n==1){
@@ -88,10 +54,10 @@ function rainette_xml2tab_previsions($xml){
 				$jour = date('Y-m-d',$date+$regs[1]*24*3600);
 				$p = reset($p);
 				$tableau[$jour]['date'] = $jour;
-				$tableau[$jour]['maxima']=intval($p['hi'][0])?intval($p['hi'][0]):'';
-				$tableau[$jour]['minima']=intval($p['low'][0])?intval($p['low'][0]):'';
-				$tableau[$jour]['icone']=intval($p['part p="d"'][0]['icon'][0]);
-				$tableau[$jour]['humidite']=intval($p['hmid'][0])?intval($p['hmid'][0]):'';
+				$tableau[$jour]['maxima'] = ($p['hi'][0] <> 'N/A') ? intval($p['hi'][0]) : 'N/A';
+				$tableau[$jour]['minima'] = ($p['low'][0] <> 'N/A') ? intval($p['low'][0]) : 'N/A';
+				$tableau[$jour]['code_icone'] = intval($p['part p="d"'][0]['icon'][0]);
+				$tableau[$jour]['humidite'] = ($p['part p="d"'][0]['hmid'][0] <> 'N/A') ? intval($p['part p="d"'][0]['hmid'][0]) : 'N/A';
 			}
 		}
 		// trier par date
@@ -100,7 +66,7 @@ function rainette_xml2tab_previsions($xml){
 	return $tableau;
 }
 
-function rainette_xml2tab_conditions($xml){
+function xml2tab_conditions($xml){
 	$tableau = array();
 	$n = spip_xml_match_nodes(",^cc,",$xml,$conditions);
 	if ($n==1){
@@ -110,21 +76,19 @@ function rainette_xml2tab_conditions($xml){
 		$date = strtotime(preg_replace(',\slocal\s*time\s*,ims','',$date));
 		$jour = date('Y-m-d',$date);
 		$tableau['date'] = $jour;
-		$tableau['t_reelle']=intval($conditions['tmp'][0])?intval($conditions['tmp'][0]):'';
-		$tableau['t_ressentie']=intval($conditions['flik'][0])?intval($conditions['flik'][0]):'';
-		$tableau['icone']=intval($conditions['icon'][0]);
-		$tableau['pression']=intval($conditions['bar'][0]['r'][0]);
-		$tableau['tendance_pression']=$conditions['bar'][0]['d'][0];
-		$tableau['vitesse_vent']=intval($conditions['wind'][0]['s'][0]);
-		$tableau['degre_vent']=intval($conditions['wind'][0]['d'][0]);
-		$tableau['direction_vent']=$conditions['wind'][0]['t'][0];
-		$tableau['humidite']=intval($conditions['hmid'][0])?intval($conditions['hmid'][0]):'';
-		$tableau['point_rosee']=intval($conditions['dewp'][0])?intval($conditions['dewp'][0]):'';
-		// trier par date
+		$tableau['temperature_reelle'] = intval($conditions['tmp'][0]);
+		$tableau['temperature_ressentie'] = intval($conditions['flik'][0]);
+		$tableau['code_icone'] = intval($conditions['icon'][0]);
+		$tableau['pression'] = intval($conditions['bar'][0]['r'][0]);
+		$tableau['tendance_pression'] = $conditions['bar'][0]['d'][0];
+		$tableau['vitesse_vent'] = intval($conditions['wind'][0]['s'][0]);
+		$tableau['angle_vent'] = intval($conditions['wind'][0]['d'][0]);
+		$tableau['direction_vent'] = $conditions['wind'][0]['t'][0];
+		$tableau['humidite'] = intval($conditions['hmid'][0]);
+		$tableau['point_rosee'] = intval($conditions['dewp'][0]);
 	}
 	return $tableau;
 }
-
 
 /**
  * charger le fichier des infos meteos correspondant au code
@@ -135,7 +99,7 @@ function rainette_xml2tab_conditions($xml){
  * @return string
  * @author Cedric Morin
  */
-function rainette_charge_meteo($code_frxx, $mode='previsions'){
+function charger_meteo($code_frxx, $mode='previsions'){
 	$dir = sous_repertoire(_DIR_CACHE,"rainette");
 	$dir = sous_repertoire($dir,substr(md5($code_frxx),0,1));
 	$f = $dir . $code_frxx . "_".$mode . ".txt";
@@ -147,11 +111,23 @@ function rainette_charge_meteo($code_frxx, $mode='previsions'){
 		$flux .= ($mode == 'previsions') ? "&dayf="._RAINETTE_JOURS_PREVISION : "&cc=*";
 		include_spip('inc/xml');
 		$xml = spip_xml_load($flux);
-		$tableau = ($mode == 'previsions') ? rainette_xml2tab_previsions($xml) : rainette_xml2tab_conditions($xml);
+		$tableau = ($mode == 'previsions') ? xml2tab_previsions($xml) : xml2tab_conditions($xml);
 		ecrire_fichier($f,serialize($tableau));
 	}
 	return $f;
 }
+
+/**
+ * filtre traduire_meteo
+ *
+ * @param string temps
+ * @return string traduction
+ * @author Pierre Basson
+ **/
+// function rainette_traduire_temps($temps) {
+	// if (!$temps) return '';
+	// return _T('grenouille:meteo_'.$temps);
+// }
 
 /**
  * rainette_fahrenheit2celsius
@@ -164,5 +140,58 @@ function rainette_charge_meteo($code_frxx, $mode='previsions'){
 	return round( ($t - 32) * 5 / 9 );
 }
  */
+// function traduire_iconcode($code){
+ 	// $tableau_meteo	= array(
+							// "1"	=> "pluie",
+							// "2"	=> "pluie",
+							// "3"	=> "orage",
+							// "4"	=> "orage",
+							// "5"	=> "pluie",
+							// "6"	=> "neige",
+							// "7"	=> "verglas",
+							// "8"	=> "pluie",
+							// "9"	=> "pluie",
+							// "10"	=> "pluie",
+							// "11"	=> "pluie",
+							// "12"	=> "pluie",
+							// "13"	=> "neige",
+							// "14"	=> "neige",
+							// "15"	=> "neige",
+							// "16"	=> "neige",
+							// "17"	=> "orage",
+							// "18"	=> "neige",
+							// "19"	=> "brouillard",
+							// "20"	=> "brouillard",
+							// "21"	=> "brouillard",
+							// "22"	=> "brouillard",
+							// "23"	=> "vent",
+							// "24"	=> "vent",
+							// "25"	=> "vent",
+							// "26"	=> "nuages",
+							// "27"	=> "lune-nuages",
+							// "28"	=> "soleil-nuages",
+							// "29"	=> "lune-nuage",
+							// "30"	=> "soleil-nuage",
+							// "31"	=> "lune",
+							// "32"	=> "soleil",
+							// "33"	=> "lune-nuage",
+							// "34"	=> "soleil-nuage",
+							// "35"	=> "orage",
+							// "36"	=> "soleil",
+							// "37"	=> "orage",
+							// "38"	=> "orage",
+							// "39"	=> "pluie",
+							// "40"	=> "pluie",
+							// "41"	=> "neige",
+							// "42"	=> "neige",
+							// "43"	=> "neige",
+							// "44"	=> "soleil-nuage",
+							// "45"	=> "pluie",
+							// "46"	=> "neige",
+							// "47"	=> "orage",
+							// "48"	=> "inconnu",
+						// );
+	// return isset($tableau_meteo[$code])?$tableau_meteo[$code]:$tableau_meteo[48];
+// }
 
 ?>
