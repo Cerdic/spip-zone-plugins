249d0475cb5b628c771e18f64d3910ed70247c9d
prigent.yohann@gmail.com <prigent.yohann@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-08-15 16:04:39 +0000

    Premier plugin ! Veuillez m'excuser pour les 2 derniers commits, c'était une grosse erreur de ma part ! (Premiere fois que j'uploade un plugin !)
    Ce plugin est un peu simple est à encore un petit probleme...
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@22055 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _dev_/moderation_moderee/lang/moderation_fr.php
@@ -0,0 +1,24 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	
+	//C
+	"titre" => "Modération Modérée",
+	"config_false_1" => "Ce plugin n'est pas fait pour votre configuration...",
+	"config_false_2" => "Ce plugin n'est pas fait pour votre configuration... Il est fait seulement pour les forums à priori ! Vous êtes à postériori",
+	"config_false_3" => "Ce plugin n'est pas fait pour votre configuration... Il est fait seulement pour les forums à priori ! Votre configuration pour les forums est reglé avec abonnement",
+	"config_true" => "<div style='text-align: center;'>Ce plugin vous permet de modérér la modération des forums publics.</div>",
+	"admin_config" => "Activer la Modération Modérée pour les administrateurs enregistrés :",
+	"redac_config" => "Activer la Modération Modérée pour les rédacteurs enregistrés :",
+	"visit_config" => "Activer la Modération Modérée pour les visiteurs enregistrés :",
+	"oui" => "Oui",
+	"non" => "Non",
+	"valider" => "Valider",
+	"install" => "Installer le plugin !",
+	"installer" => "Installer",
+	"explain_install" => "<br />En cliquant sur le bouton ci dessous, le plugin écrira 3 entrées dans la table <b>spip_metas</b>. Elles permettront de gérer les autorisations de validation.<br /><br />",
+
+	);
+?>
