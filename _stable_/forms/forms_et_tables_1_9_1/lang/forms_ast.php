196d6087ddc40c26c92f2046a8aa3fe1833aa175
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-08-19 10:34:32 +0000

    langues (forms)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@22077 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _stable_/forms/forms_et_tables_1_9_1/lang/forms_ast.php
@@ -0,0 +1,222 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'activer_barre_typo' => 'Activar la barra tipogr&aacute;fica',
+	'adresse_invalide' => 'Esta direici&oacute;n nun ye v&aacute;lida.',
+	'afficher' => 'Amosar',
+	'aide_contextuelle' => 'Aida contestual',
+	'ajouter_champ' => 'A&ntilde;adir un campu',
+	'ajouter_champ_type' => 'Crear un campu de tipu:',
+	'ajouter_choix' => 'A&ntilde;ader una seleici&oacute;n',
+	'apparence_formulaire' => 'Aspeutu del formulariu d\'edici&oacute;n',
+	'article_inserer_un_formulaire' => 'Amestar un formulariu',
+	'article_inserer_un_formulaire_detail' => 'Vas poder amestar formularios a los tos art&iacute;culos pa permiti-yos a los visitantes inxertar informaci&oacute;n. Escueye un formulariu de la llista d\'embaxo y copia l\'atayu nel testu de l\'art&iacute;culu.',
+	'article_recopier_raccourci' => 'Copia esti atayu nel testu de l\'art&iacute;culu pa amesta-y esti formulariu',
+	'articles_utilisant' => 'Art&iacute;culos que tan usando esti formulariu',
+	'attention' => 'Atenci&oacute;n:',
+	'aucune_reponse' => 'deng&uacute;n datu',
+	'avis_message_confirmation' => 'Un mensaxe de confirmaci&oacute;n ta acabante d\'unviase a @mail@',
+
+	// B
+	'boite_info' => 'Pica enriba los iconos pa ver los datos asociaos con un formulariu, editalu, duplicalu, esportalu, vacialu de datos o suprimilu.',
+
+	// C
+	'cfg_activer' => 'Si',
+	'cfg_associer_donnees' => 'Rellacionar los datos de les tables',
+	'cfg_associer_donnees_articles' => 'Permitir rellacionar los datos colos art&iacute;culos:',
+	'cfg_associer_donnees_auteurs' => 'Permitir rellacionar los datos colos autores:',
+	'cfg_associer_donnees_rubriques' => 'Permitir rellacionar los datos coles seiciones:',
+	'cfg_bouton_type_image' => 'bot&oacute;n de tipu imaxe',
+	'cfg_bouton_type_texte' => 'bot&oacute;n de tipu testu (unviar)',
+	'cfg_bouton_valider' => 'Tipu de bot&oacute;n validar',
+	'cfg_bouton_valider_texte' => 'Puedes escoyer qu&eacute; tipu de bot&oacute;n validar va utilizase nos formularios:',
+	'cfg_desactiver' => 'Non',
+	'cfg_inserer_head' => 'Inxertar nel &lt;head&gt;',
+	'cfg_inserer_head_texte' => 'Inxertar autom&aacute;ticamente les llam&aacute;es a los ficheros siguientes na seici&oacute;n &lt;head&gt; de les p&aacute;xines del sitiu (encamentao):',
+	'champ_descendre' => 'baxar',
+	'champ_email_details' => 'Hai qu\'escribir unes se&ntilde;es d\'e-mail v&aacute;lides (del tipu nome@eltocorreu.com).',
+	'champ_listable_admin' => 'Amosar esti campu nes llistes de l\'espaciu privao',
+	'champ_listable_publique' => 'Amosar esti campu nes llistes p&uacute;bliques',
+	'champ_monter' => 'xubir',
+	'champ_necessaire' => 'Hai qu\'enllenar esti campu.',
+	'champ_nom' => 'Nome del campu',
+	'champ_nom_bloc' => 'Nome del bloque',
+	'champ_nom_groupe' => 'Grupu',
+	'champ_nom_texte' => 'Testu',
+	'champ_public' => 'Esti campu ye visible dende l\'espaciu p&uacute;blicu',
+	'champ_saisie_desactivee' => 'Desactivar la edici&oacute;n d\'esti campu',
+	'champ_specifiant' => 'Esti campu clasifica\'l datu (orden, filtru, descripci&oacute;n)',
+	'champ_table_jointure_type' => 'Tipu de tabla pa les xuntures',
+	'champ_type_date' => 'Fecha',
+	'champ_type_email' => 'Se&ntilde;es d\'e-mail',
+	'champ_type_fichier' => 'Ficheru a descargar',
+	'champ_type_joint' => 'Xuntura con otra tabla',
+	'champ_type_ligne' => 'Ll&iacute;nia de testu',
+	'champ_type_monnaie' => 'Monetariu',
+	'champ_type_mot' => 'Pallabres-clave',
+	'champ_type_multiple' => 'Escoyeta m&uacute;ltiple',
+	'champ_type_numerique' => 'Numb&eacute;ricu',
+	'champ_type_password' => 'Contrase&ntilde;a',
+	'champ_type_select' => 'Escoyeta &uacute;nica',
+	'champ_type_separateur' => 'Nuevu bloque d\'entrugues',
+	'champ_type_texte' => 'Testu',
+	'champ_type_textestatique' => 'Mensaxe d\'esplicaci&oacute;n',
+	'champ_type_url' => 'Se&ntilde;es del sitiu Web',
+	'champ_url_details' => 'Hai qu\'inxertar unes se&ntilde;es Web v&aacute;lides (del tipu http://www.elmiositiu.com/...).',
+	'champs_formulaire' => 'Campos del formulariu',
+	'changer_choix_multiple' => 'Cambear a escoyeta m&uacute;ltiple',
+	'changer_choix_unique' => 'Cambear a escoyeta &uacute;nica',
+	'choisir_email' => 'Escoyer l\'e-mail en funci&oacute;n de',
+	'confirm_supprimer_champ' => '&iquest;Est&aacute;s seguru de que quies desaniciar el campu \'@champ@\'?',
+	'confirm_supprimer_donnee' => '&iquest;De xuru quies desaniciar el datu \'@donnee@\'?',
+	'confirm_vider_table' => '&iquest;De xuru quies vaciar el conten&iacute;u de la tabla \'@table@\'?',
+	'confirmer_champ_password' => 'Testu pa la escritura doble',
+	'confirmer_password' => 'Confirmaci&oacute;n',
+	'confirmer_reponse' => 'Enviar un corr&eacute;u pa confirmar la receici&oacute;n con:',
+	'csv_classique' => 'CSV cl&aacute;sicu (,)',
+	'csv_excel' => 'CSV pa Excel (;)',
+	'csv_tab' => 'CSV con tabulaciones',
+
+	// D
+	'date' => 'Fecha',
+	'date_invalide' => 'Formatu fecha non v&aacute;lidu',
+	'donnees_modifiable' => 'Datos iguables pel usuariu.',
+	'donnees_multiple' => 'Respuestes m&uacute;ltiples.',
+	'donnees_nonmodifiable' => 'Datos non iguables pel usuariu.',
+	'donnees_nonmultiple' => 'Respuesta &uacute;nica.',
+	'donnees_prot' => 'Datos protex&iacute;os. Los datos grabaos nun van ser accesibles m&aacute;s que dende l\'interfaz privao.',
+	'donnees_pub' => 'Datos p&uacute;blicos. Los datos grabaos tar&aacute;n accesibles pa les visites del sitiu.',
+	'dupliquer' => 'Duplicar',
+
+	// E
+	'echec_upload' => 'Fallu na tresferencia del ficheru.',
+	'edit_champ_obligatoire' => 'Esti campu ye obligatoriu',
+	'editer' => 'Iguar',
+	'email_independant' => 'Email independiente de la respuesta',
+	'exporter' => 'Esportar',
+	'exporter_article' => 'Esportar nun art&iacute;culu',
+
+	// F
+	'fichier_trop_gros' => 'Esti ficheru ye demasiao grande.',
+	'fichier_type_interdit' => 'Esti tipu de ficheru ta torgau.',
+	'form_erreur' => 'Fallu:',
+	'format_fichier' => '<NEW>Format du fichier&nbsp;:',
+	'format_liste' => '<NEW>d&eacute;roulante',
+	'format_liste_ou_radio' => '<NEW>Format de la liste',
+	'format_radio' => '<NEW>boutons radio',
+	'forms_obligatoires' => '<NEW>Formulaires obligatoires pour la saisie de celui-ci&nbsp;:',
+	'formulaire' => '<NEW>Formulaire',
+	'formulaire_aller' => '<NEW>Aller au formulaire',
+	'formulaires_copie' => '<NEW>Copie de @nom@',
+	'formulaires_sondages' => '<NEW>Formulaires et sondages',
+
+	// H
+	'html_wrapper' => '<NEW>Encapsuler le champ dans le code html',
+
+	// I
+	'icone_ajouter_donnees' => '<NEW>Ajouter des r&eacute;ponses',
+	'icone_creer_formulaire' => '<NEW>Cr&eacute;er un nouveau formulaire',
+	'icone_creer_table' => '<NEW>Cr&eacute;er un nouveau formulaire',
+	'importer_form' => '<NEW>Importer un formulaire',
+	'info_apparence' => '<NEW>Voici une pr&eacute;visualisation du formulaire tel qu\'il appara&icirc;tra aux visiteurs du site public.',
+	'info_articles_lies_donnee' => '<NEW>Les articles li&eacute;s',
+	'info_champs_formulaire' => '<NEW>Vous pouvez ici cr&eacute;er et modifier les champs que les visiteurs pourront remplir.',
+	'info_obligatoire_02' => '<NEW>[Obligatoire]',
+	'info_rubriques_liees_donnee' => '<NEW>Les rubriques li&eacute;es',
+	'info_sondage' => '<NEW>Si votre formulaire est un sondage, les r&eacute;sultats des champs de type &laquo; s&eacute;lection &raquo; seront additionn&eacute;s et affich&eacute;s.',
+	'info_supprimer_formulaire' => '<NEW>Voulez-vous vraiment supprimer ce formulaire ?',
+	'info_supprimer_formulaire_reponses' => '<NEW>Des donn&eacute;es sont associ&eacute;es &agrave; ce formulaire. Voulez-vous vraiment le supprimer ?',
+
+	// L
+	'lien_apercu' => '<NEW>Aper&ccedil;u',
+	'lien_champ' => '<NEW>Champs',
+	'lien_propriete' => '<NEW>Propri&eacute;t&eacute;s',
+	'lien_retirer_donnee_liante' => '<NEW>Retirer le lien',
+	'lien_retirer_donnee_liee' => '<NEW>Retirer le lien',
+	'lier_articles' => '<NEW>Permettre d\'associer les donn&eacute;es aux articles',
+	'lier_documents' => '<NEW>Permettre de joindre des documents aux donn&eacute;es',
+	'lier_documents_mail' => '<NEW>Joindre les documents &agrave; l\'email',
+	'liste_choix' => '<NEW>Liste des choix propos&eacute;s',
+
+	// M
+	'moderation_donnees' => '<NEW>Valider les donn&eacute;es avant publication :',
+	'modifiable_donnees' => '<NEW>Donn&eacute;s modifiables dans l\'espace public&nbsp;:',
+	'monetaire_invalide' => '<NEW>Champ mon&eacute;taire invalide',
+	'monnaie_euro' => '<NEW>Euro (&euro;)',
+	'multiple_donnees' => '<NEW>Saisie des donn&eacute;es dans l\'espace public&nbsp;:',
+
+	// N
+	'nb_decimales' => '<NEW>Nombre de d&eacute;cimales',
+	'nombre_reponses' => '<NEW>@nombre@ donn&eacute;es',
+	'nouveau_champ' => '<NEW>Nouveau champ',
+	'nouveau_choix' => '<NEW>Nouveau choix',
+	'nouveau_formulaire' => '<NEW>Nouveau formulaire',
+	'numerique_invalide' => '<NEW>Champ num&eacute;rique invalide',
+
+	// P
+	'page' => 'P&aacute;xina',
+	'pas_mail_confirmation' => '<NEW>Pas de mail confirmation',
+	'probleme_technique' => '<NEW>Probl&egrave;me technique. Votre saisie n\'a pas pu &ecirc;tre prise en compte.',
+	'probleme_technique_upload' => '<NEW>Probl&egrave;me technique. Le transfert du fichier a &eacute;chou&eacute;.',
+	'publication_donnees' => '<NEW>Publication des donn&eacute;es',
+
+	// R
+	'rang' => '<NEW>Rang',
+	'remplir_un_champ' => 'Ties que llenar polo menos un campu.',
+	'reponse' => 'Respuesta @id_reponse@',
+	'reponse_depuis' => '<NEW>Depuis la page ',
+	'reponse_enregistree' => '<NEW>Votre saisie a &eacute;t&eacute; enregistr&eacute;e.',
+	'reponse_envoyee' => '<NEW>R&eacute;ponse envoy&eacute;e le ',
+	'reponse_envoyee_a' => '<NEW>&agrave;',
+	'reponse_retrovez' => '<NEW>Retrouvez cette r&eacute;ponse dans l\'interface d\'administration :',
+	'reponses' => 'respuestes',
+	'resultats' => 'Resultaos: ',
+
+	// S
+	'site_introuvable' => 'Esti sitiu nun s\'alcuentra.',
+	'sondage_deja_repondu' => '<NEW>Vous avez d&eacute;j&agrave; r&eacute;pondu a ce sondage.',
+	'sondage_non' => '<NEW>Ce formulaire n\'est pas un sondage',
+	'sondage_oui' => '<NEW>Ce formulaire est un sondage.',
+	'suivi_formulaire' => '<NEW>Suivi du formulaire',
+	'suivi_formulaires' => '<NEW>Suivi des formulaires',
+	'suivi_reponses' => '<NEW>Suivi des r&eacute;ponses',
+	'supprimer' => '<NEW>Supprimer',
+	'supprimer_champ' => '<NEW>Supprimer ce champ',
+	'supprimer_choix' => '<NEW>supprimer ce choix',
+	'supprimer_formulaire' => '<NEW>Supprimer ce formulaire',
+	'supprimer_reponse' => '<NEW>Supprimer cette r&eacute;ponse',
+
+	// T
+	'tables' => 'Tables',
+	'taille_max' => '<NEW>Taille maximale (en ko)',
+	'telecharger' => '<NEW>T&eacute;l&eacute;charger',
+	'telecharger_reponses' => '<NEW>T&eacute;l&eacute;charger les r&eacute;ponses',
+	'titre_formulaire' => '<NEW>Titre du formulaire',
+	'total_votes' => '<NEW>Total des votes',
+	'tous_formulaires' => '<NEW>Tous les formulaires',
+	'tous_sondages' => '<NEW>Tous les sondages',
+	'tous_sondages_proteges' => '<NEW>Tous les sondages prot&eacute;g&eacute;s',
+	'tous_sondages_public' => '<NEW>Tous les sondages publics',
+	'toutes_tables' => '<NEW>Tous les formulaires',
+	'type_form' => '<NEW>Type de formulaire',
+
+	// U
+	'une_reponse' => 'un datu',
+	'unite_monetaire' => 'Unid&aacute; monetaria',
+
+	// V
+	'valider' => '<NEW>Valider',
+	'verif_web' => '<NEW>v&eacute;rifier l\'existence du site Web',
+	'vider' => '<NEW>Vider',
+	'voir_article' => '<NEW>Voir l\'article',
+	'voir_resultats' => 'Ver los resultaos'
+);
+
+?>
