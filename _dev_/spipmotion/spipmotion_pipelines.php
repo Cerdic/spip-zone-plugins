22796ca3f7eaf23fecb77de3f09bdc33cb452f95
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-08-21 19:53:46 +0000

    update des pipelines
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@22147 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/spipmotion/spipmotion_pipelines.php
+++ _dev_/spipmotion/spipmotion_pipelines.php
@@ -2,22 +2,13 @@
 
 include_spip("inc/spipmotion");
 
-function spipmotion_affiche_droite($flux) {
-	if ($flux['args']['exec'] =='articles_edit'){
-		$spipmotion = charger_fonction('spipmotion', 'inc');
-		$flux['data'] .= $spipmotion($flux['arg']['id_article']);
-	}
-	return $flux;
-}
-
 function spipmotion_editer_contenu_objet($flux){
-	$type = _request('exec');
-	$type = substr($type, 0, -1);
-	$id = _request('id_'.$type);
+	$id_document = $flux['args']['id'];
 	if($flux['args']['type']=='case_document'){
-		$id_document = $flux['args']['id'];
-		$document = sql_fetsel("docs.id_document, docs.id_vignette,docs.extension,docs.fichier,docs.mode,docs.distant, docs.date, L.vu", "spip_documents AS docs INNER JOIN spip_documents_liens AS L ON L.id_document=docs.id_document","L.id_document=".sql_quote($id_document));
+		$document = sql_fetsel("docs.id_document, docs.extension, L.vu,L.objet,L.id_objet", "spip_documents AS docs INNER JOIN spip_documents_liens AS L ON L.id_document=docs.id_document","L.id_document=".sql_quote($id_document));
 		$extension = $document['extension'];
+		$type = $document['objet'];
+		$id = $document['id_objet'];
 		if(($extension == 'mov')||($extension == 'flv')||($extension == 'avi')){
 			$infos_videos = charger_fonction('infos_videos', 'inc');
 			$flux['data'] .= $infos_videos($id,$id_document,$type);
@@ -27,9 +18,6 @@ function spipmotion_editer_contenu_objet($flux){
 }
 
 function spipmotion_post_edition($flux){
-	spip_log('spipmotion post edition');
-	spip_log('operation = '.$flux['args']['operation']);
-	spip_log('type_image = '.$flux['args']['type_image']);
 	$id_document = $flux['args']['id_objet'];
 	if($flux['args']['operation'] == 'ajouter_document'){
 			$document = sql_fetsel("docs.id_document, docs.extension,docs.fichier,docs.mode,docs.distant, L.vu, L.objet, L.id_objet", "spip_documents AS docs INNER JOIN spip_documents_liens AS L ON L.id_document=docs.id_document","L.id_document=".sql_quote($id_document));
@@ -61,12 +49,10 @@ function spipmotion_post_edition($flux){
 				$mode = 'vignette';
 				
 				$ajouter_documents = charger_fonction('ajouter_documents', 'inc');
-			
-					spip_log("on ajoute $img_finale","spipmotion");	
-					// verifier l'extension du fichier en fonction de son type mime
-					//list($extension,$arg) = fixer_extension_document($arg);
-					$x = $ajouter_documents($img_finale, $img_finale, 
-							    $type, $id, $mode, $id_document, $actifs);
+				
+				// verifier l'extension du fichier en fonction de son type mime
+				//list($extension,$arg) = fixer_extension_document($arg);
+				$x = $ajouter_documents($img_finale, $img_finale, $type, $id, $mode, $id_document, $actifs);
 				
 				sql_updateq('spip_documents',array('hauteur'=> $height, 'largeur'=>$width),'id_document='.sql_quote($id_document));
 				
@@ -78,7 +64,6 @@ function spipmotion_post_edition($flux){
 				return $x;
 			}
 			else{
-				spip_log("id_document=$id_document - extension = ".$document['extension']);
 				return;
 			}
 		}
