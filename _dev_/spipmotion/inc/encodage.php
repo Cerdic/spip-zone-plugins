a6455f39be024933ef9827ebdf2e821ac5bad0e0
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-08-21 23:13:26 +0000

    L'action d'encodage des videos
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@22154 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/spipmotion/inc/encodage.php
+++ _dev_/spipmotion/inc/encodage.php
@@ -15,22 +15,54 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 //
 // Faire l'encodage d'une video en .flv
 //
-include_spip('inc/charsets'); # pour le nom de fichier
-include_spip('base/abstract_sql');
-include_spip('inc/actions');
 
-function inc_encodage_dist ($sources, $file, $type, $id, $mode, $id_document, &$actifs, $hash='', $redirect='', $iframe_redirect='')
-{
-	  return encodage($sources, $file, $type, $id, $id_document, $actifs);
+function inc_encodage_dist($source,$attente){
+	  return encodage($source,$attente);
 }
 
-function encodage($sources, $file, $type_lien, $id_lien, $id_document, &$documents_actifs){
-		
-	$dir_ftp = determine_upload();
-	spip_log ("encodage de la video $dir_ftp$file  (T '$type_lien' L '$id_lien' D '$id_document')");
-	$encodageflv = _DIR_PLUGIN_SPIPMOTION.'script_bash/spipmotion.sh --e '.$dir_ftp.$file.' --s '.$dir_ftp.$file.'.flv --bitrate '.lire_config("spipmotion/bitrate","448").' --audiobitrate '.lire_config("spipmotion/bitrate_audio","64").' --audiofreq '. lire_config("spipmotion/frequence_audio","22050").' --fps '.lire_config("spipmotion/fps","15").' --p '.lire_config("spipmotion/chemin","/usr/local/bin/ffmpeg");
+function encodage($source,$attente){
+	include_spip('inc/documents');
+	$fichier = basename($source['fichier']);
+	$chemin = get_spip_doc($source['fichier']);
+
+	// Calcul de la hauteur en fonction de la largeur souhaitée et de la taille de la video originale	
+	$width_finale = 480;
+	$width = $source['largeur'];
+	
+	$height = $source['hauteur'];
+	$height_finale = $source['hauteur']/($source['largeur']/$width_finale);
+	
+	spip_log("document original ($chemin) = $width/$height - document final = $width_finale/$height_finale");
+	
+	$string = "$fichier-$width-$height";
+	$query = md5($string);
+	$dossier = _DIR_VAR;
+	$fichier_temp = "$dossier$query.flv";
+	
+	$encodageflv = find_in_path('script_bash/spipmotion.sh').' --e '.$chemin.' --s '.$fichier_temp.' --size '.$width_finale.'x'.$height_finale.' --bitrate '.lire_config("spipmotion/bitrate","448").' --audiobitrate '.lire_config("spipmotion/bitrate_audio","64").' --audiofreq '. lire_config("spipmotion/frequence_audio","22050").' --fps '.lire_config("spipmotion/fps","15").' --p '.lire_config("spipmotion/chemin","/usr/local/bin/ffmpeg");
 	spip_log("$encodageflv");
-	shell_exec($encodageflv);
+	$lancement_encodage =shell_exec($encodageflv);
+	spip_log($lancement_encodage);
+	spip_log("l'encodage est terminé");
+	
+	$mode = 'document';
+	$invalider = true;
+	$type_doc = 'article';
+	
+	$ajouter_documents = charger_fonction('ajouter_documents', 'inc');
+	$x = $ajouter_documents($fichier_temp, $fichier_temp, $type_doc, $attente['id_article'], $mode, $id_document, $actifs);
+	spip_log("on ajoute le nouveau fichier qui devient $x");
+	
+	unlink($fichier_temp);
+	
+	sql_updateq('spip_spipmotion_attentes',array('encode'=>'oui'),'id_spipmotion_attente='.$attente['id_spipmotion_attente']);
+	// la récupération des infos et du logo est faite automatiquement par le pipeline post-edition
+	
+		if ($invalider) {
+			include_spip('inc/invalideur');
+			suivre_invalideur("0",true);
+			spip_log('invalider', 'spipmotion');
+		}
 	return;
 }
 ?>
