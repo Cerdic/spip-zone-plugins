6b9c961dbeb7a8ee236c8df76f20835d9d9d125a
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-08-21 12:37:01 +0000

    Voila, une premiere version complète Il semble necessaire de revoir quand meme le contexte passé aux modeles afin de le limiter et de revoir les résumé méteo issus de Grenouille et qui ne sont pas toujours pertinents.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@22131 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/rainette/rainette_options.php
+++ _stable_/rainette/rainette_options.php
@@ -9,6 +9,6 @@ define ('_RAINETTE_RELOAD_TIME_PREVISIONS',2*3600); // pas la peine de recharger
 define ('_RAINETTE_RELOAD_TIME_CONDITIONS',1800); // pas la peine de recharger un flux de moins de 30mn
 define ('_RAINETTE_JOURS_PREVISION', 10);
 define ('_RAINETTE_SYSTEME_MESURE','m');
-// Valeur utilisee pour definir une donnee non determinee dans les fichiers (inutile de la modifier)
+// Valeur utilisee pour definir une donnee non determinee dans les fichiers (NE PAS MODIFIER)
 define ('_RAINETTE_VALEUR_INDETERMINEE','N/D');
 ?>
