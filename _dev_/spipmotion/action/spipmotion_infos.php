290a1aa134a6408f57575d1c715365d448d8104f
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-08-24 23:11:49 +0000

    un include en trop
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@22206 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/spipmotion/action/spipmotion_infos.php
+++ _dev_/spipmotion/action/spipmotion_infos.php
@@ -3,7 +3,6 @@
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 include_spip('inc/charsets');	# pour le nom de fichier
-include_spip('base/abstract_sql');
 include_spip('inc/actions');
 
 function action_spipmotion_infos_dist(){
@@ -25,20 +24,12 @@ function action_spipmotion_infos_dist(){
 
 function action_spipmotion_infos_post($r){
 	list(, $sign, $id_objet, $objet, $id_document, $suite) = $r;
-	include_spip('inc/documents');
-	$document = sql_fetsel("docs.id_document,docs.extension,docs.fichier,docs.taille,docs.mode", "spip_documents AS docs INNER JOIN spip_documents_liens AS L ON L.id_document=docs.id_document","L.id_document=".sql_quote($id_document));
-	$chemin = $document['fichier'];
-	$movie = get_spip_doc($chemin);
-	spip_log("on travail sur $movie","spipmotion");	
 	
-	$movie = @new ffmpeg_movie($movie, 0);
-	$height = $movie->getFrameHeight();
-	$width = $movie->getFrameWidth();
-	$duree = $movie->getDuration();
+	$recuperer_infos = charger_fonction('spipmotion_recuperer_infos','inc');
+	$infos = $recuperer_infos($id_document);
 	
-	sql_updateq('spip_documents',array('hauteur'=> $height, 'largeur'=>$width),'id_document='.sql_quote($id_document));
 	if(_request("iframe") == 'iframe') {
-		$redirect = parametre_url(urldecode($iframe_redirect),"show_docs",join(',',$documents_actifs),'&')."&iframe=iframe";
+		$redirect = parametre_url(urldecode($iframe_redirect),"show_video_infos",join(',',$documents_actifs),'&')."&iframe=iframe";
 	}
 	return $redirect;
 }
