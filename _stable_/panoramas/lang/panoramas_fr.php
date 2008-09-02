593c97e929a4f9ab56c31fa1e4edc9daee408277
apachot@openstudio.fr <apachot@openstudio.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-09-02 15:59:54 +0000

    les icones sur la carte sont directement cliquables
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@22375 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/panoramas/lang/panoramas_fr.php
+++ _stable_/panoramas/lang/panoramas_fr.php
@@ -42,6 +42,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 'liste_lieux_visites' => 'Liste des lieux visit&eacute;s',
 'objets_ramasses' => 'Liste des objets ramass&eacute;s',
 'jeux_reussis' => 'Liste des jeux r&eacute;ussis',
-'votre_profil' => 'Votre profil'
+'votre_profil' => 'Votre profil',
+'pas_d_objets' => 'Votre panier est vide'
 );
 ?>
