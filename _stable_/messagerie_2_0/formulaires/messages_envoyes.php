b60430f74202e5db835ae13e21e8e846a171553b
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-09-09 09:48:20 +0000

    invalidation apres envoi message
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@22453 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/messagerie_2_0/formulaires/messages_envoyes.php
+++ _stable_/messagerie_2_0/formulaires/messages_envoyes.php
@@ -7,9 +7,17 @@
  */
 
 
-include_spip('base/abstract_sql');
-include_spip('inc/texte');
-include_spip('base/abstract_sql');
+/**
+ * Chargement des valeurs par defaut de #FORMULAIRE_MESSAGES_ENVOYES
+ *
+ * @return array
+ */
+function formulaires_messages_envoyes_charger_dist(){
+	$valeurs = array();
+
+	return $valeurs;
+}
+
 
 /**
  * Traitement de la saisie de #FORMULAIRE_MESSAGES_ENVOYES
@@ -17,11 +25,16 @@ include_spip('base/abstract_sql');
  * @return string
  */
 function formulaires_messages_envoyes_traiter_dist(){
+	include_spip('base/abstract_sql');
+	
 	$liste = _request('selectionne');
 	$id_auteur = $GLOBALS['visiteur_session']['id_auteur'];
 	if (is_array($liste) && count($liste)) {
 		if (_request('effacer')){
 			sql_updateq('spip_messages',array('statut'=>'poub'),array('id_auteur='.intval($id_auteur),'id_message IN ('.implode(',',$liste).')'));
+			
+			include_spip('inc/invalideur');
+			suivre_invalideur("message/".implode(',',$liste));
 		}
 	}
 	
