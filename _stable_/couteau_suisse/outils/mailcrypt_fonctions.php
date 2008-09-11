3741dbb41131c5787877b39a6a465c7ce27188b2
patfr@ifrance.com <patfr@ifrance.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-09-11 22:43:10 +0000

    mailcrypt : éviter les doubles titles, non conformes (RealET)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@22526 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/couteau_suisse/outils/mailcrypt_fonctions.php
+++ _stable_/couteau_suisse/outils/mailcrypt_fonctions.php
@@ -5,6 +5,8 @@ include_spip('outils/inc_cs_liens');
 
 // tip visible onMouseOver
 @define('_mailcrypt_AROBASE_JS', '..&aring;t..');
+@define('_mailcrypt_AROBASE_JSQ', preg_quote(_mailcrypt_AROBASE_JS,','));
+
 // span ayant l'arobase en background
 @define('_mailcrypt_AROBASE', '<span class="spancrypt">&nbsp;</span>');
 @define('_mailcrypt_REGEXPR1', ',\b['._cs_liens_AUTORISE.']*@[a-zA-Z][a-zA-Z0-9-.]*\.[a-zA-Z]+(\?['._cs_liens_AUTORISE.']*)?,');
@@ -13,13 +15,15 @@ include_spip('outils/inc_cs_liens');
 function mailcrypt($texte) {
 	if (strpos($texte, '@')===false) return $texte;
 
-	// liens HTML
+	// protection des liens HTML
 	$lien = '$1' . _mailcrypt_AROBASE_JS . '$2';
 	$texte = preg_replace(",[\"\']mailto:([^@\"']+)@([^\"']+)[\"\'],", 
 		'"#" title="'.$lien.'" onclick="location.href=lancerlien(\'$1\',\'$2\'); return false;"', $texte);
-	if (strpos($texte, '@')===false) return $texte;
+	// retrait des titles en doublon... un peu sale, mais en attendant mieux ?
+	$texte = preg_replace(',title="[^"]+'._mailcrypt_AROBASE_JSQ.'[^"]+"([^>]+title=[\"\']),', '$1', $texte);
 
 	// protection de tout le reste...
+	if (strpos($texte, '@')===false) return $texte;
 	$texte = preg_replace(_mailcrypt_REGEXPR2, '$1'._mailcrypt_AROBASE.'$2', $texte);
 	return $texte;
 }
