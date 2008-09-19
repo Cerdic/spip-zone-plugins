0256e8c4e531622af124bbca18592cd5580bcb60
fil@rezo.net <fil@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-09-19 10:31:46 +0000

    passage a Akismet.class.php v 0.4 http://www.achingbrain.net/stuff/php/akismet
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@22656 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/akismet/Akismet.class.php
+++ _dev_/akismet/Akismet.class.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Akismet anti-comment spam service
  *
@@ -6,7 +7,7 @@
  *
  * This service performs a number of checks on submitted data and returns whether or not the data is likely to be spam.
  *
- * Please note that in order to use this class, you must have a vaild {@link http://wordpress.com/api-keys/ WordPress API key}.  They are free for non/small-profit types and getting one will only take a couple of minutes.
+ * Please note that in order to use this class, you must have a vaild {@link http://wordpress.com/api-keys/ WordPress API key}.  They are free for non/small-profit types and getting one will only take a couple of minutes.  
  *
  * For commercial use, please {@link http://akismet.com/commercial/ visit the Akismet commercial licensing page}.
  *
@@ -14,15 +15,15 @@
  *
  * See the Akismet class documentation page linked to below for usage information.
  *
- * @package Akismet
- * @author Alex Potsides, {@link http://www.achingbrain.net http://www.achingbrain.net}, Bret Kuhns {@link http://www.miphp.net}
- * @version 0.1
- * @copyright Alex Potsides, {@link http://www.achingbrain.net http://www.achingbrain.net}
- * @license http://www.gnu.org/licenses/gpl.html GNU General Public License
+ * @package		akismet
+ * @author		Alex Potsides, {@link http://www.achingbrain.net http://www.achingbrain.net}
+ * @version		0.4
+ * @copyright	Alex Potsides, {@link http://www.achingbrain.net http://www.achingbrain.net}
+ * @license		http://www.opensource.org/licenses/bsd-license.php BSD License
  */
 
 /**
- *	The Akismet PHP4 Class
+ *	The Akismet PHP5 Class
  *
  *  This class takes the functionality from the Akismet WordPress plugin written by {@link http://photomatt.net/ Matt Mullenweg} and allows it to be integrated into any PHP5 application or website.
  *
@@ -42,94 +43,111 @@
  *      // store the comment normally
  *  </code>
  *
+ *  Optionally you may wish to check if your WordPress API key is valid as in the example below.
+ * 
+ * <code>
+ *   $akismet = new Akismet('http://www.example.com/blog/', 'aoeu1aoue');
+ *   
+ *   if($akismet->isKeyValid()) {
+ *     // api key is okay
+ *   } else {
+ *     // api key is invalid
+ *   }
+ * </code>
+ *
  *	@package	akismet
  *	@name		Akismet
- *	@version	0.2
- *  @author		Alex Potsides (converted to PHP4 by Bret Kuhns)
+ *	@version	0.4
+ *  @author		Alex Potsides
  *  @link		http://www.achingbrain.net/
  */
-class Akismet {
-	var $version = '0.2';
-	var $wordPressAPIKey;
-	var $blogURL;
-	var $comment;
-	var $apiPort;
-	var $akismetServer;
-	var $akismetVersion;
-
+class Akismet
+	{
+	private $version = '0.4';
+	private $wordPressAPIKey;
+	private $blogURL;
+	private $comment;
+	private $apiPort;
+	private $akismetServer;
+	private $akismetVersion;
+	
 	// This prevents some potentially sensitive information from being sent accross the wire.
-	var $ignore = array(
-			'HTTP_COOKIE',
-			'HTTP_X_FORWARDED_FOR',
-			'HTTP_X_FORWARDED_HOST',
-			'HTTP_MAX_FORWARDS',
-			'HTTP_X_FORWARDED_SERVER',
-			'REDIRECT_STATUS',
-			'SERVER_PORT',
-			'PATH',
-			'DOCUMENT_ROOT',
-			'SERVER_ADMIN',
-			'QUERY_STRING',
-			'PHP_SELF'
-		);
-
-
+	private $ignore = array('HTTP_COOKIE', 
+							'HTTP_X_FORWARDED_FOR', 
+							'HTTP_X_FORWARDED_HOST', 
+							'HTTP_MAX_FORWARDS', 
+							'HTTP_X_FORWARDED_SERVER', 
+							'REDIRECT_STATUS', 
+							'SERVER_PORT', 
+							'PATH',
+							'DOCUMENT_ROOT',
+							'SERVER_ADMIN',
+							'QUERY_STRING',
+							'PHP_SELF' );
+	
 	/**
-	 *	@throws	Exception	An exception is thrown if your API key is invalid.
-	 *	@param	string	Your WordPress API key.
 	 *	@param	string	$blogURL			The URL of your blog.
+	 *	@param	string	$wordPressAPIKey	WordPress API key.
 	 */
-	function Akismet($blogURL, $wordPressAPIKey) {
+	public function __construct($blogURL, $wordPressAPIKey) {
 		$this->blogURL = $blogURL;
 		$this->wordPressAPIKey = $wordPressAPIKey;
-
+		
 		// Set some default values
 		$this->apiPort = 80;
 		$this->akismetServer = 'rest.akismet.com';
 		$this->akismetVersion = '1.1';
-
+		
 		// Start to populate the comment data
 		$this->comment['blog'] = $blogURL;
 		$this->comment['user_agent'] = $_SERVER['HTTP_USER_AGENT'];
-		$this->comment['referrer'] = $_SERVER['HTTP_REFERER'];
-
-		// This is necessary if the server PHP5 is running on has been set up to run PHP4 and
-		// PHP5 concurently and is actually running through a separate proxy al a these instructions:
-		// http://www.schlitt.info/applications/blog/archives/83_How_to_run_PHP4_and_PHP_5_parallel.html
-		// and http://wiki.coggeshall.org/37.html
-		// Otherwise the user_ip appears as the IP address of the PHP4 server passing the requests to the
-		// PHP5 one...
-		$this->comment['user_ip'] = $_SERVER['REMOTE_ADDR'] != getenv('SERVER_ADDR') ? $_SERVER['REMOTE_ADDR'] : getenv('HTTP_X_FORWARDED_FOR');
-
-		// Check to see if the key is valid
-		$response = $this->http_post('key=' . $this->wordPressAPIKey . '&blog=' . $this->blogURL, $this->akismetServer, '/' . $this->akismetVersion . '/verify-key');
-
-		if($response[1] != 'valid') {
-			// Whoops, no it's not.  Throw an exception as we can't proceed without a valid API key.
-			trigger_error('Invalid API key.  Please obtain one from http://wordpress.com/api-keys/', E_USER_ERROR);
+		
+		if(isset($_SERVER['HTTP_REFERER'])) {
+			$this->comment['referrer'] = $_SERVER['HTTP_REFERER'];
 		}
+		
+		/* 
+		 * This is necessary if the server PHP5 is running on has been set up to run PHP4 and
+		 * PHP5 concurently and is actually running through a separate proxy al a these instructions:
+		 * http://www.schlitt.info/applications/blog/archives/83_How_to_run_PHP4_and_PHP_5_parallel.html
+		 * and http://wiki.coggeshall.org/37.html
+		 * Otherwise the user_ip appears as the IP address of the PHP4 server passing the requests to the 
+		 * PHP5 one...
+		 */
+		$this->comment['user_ip'] = $_SERVER['REMOTE_ADDR'] != getenv('SERVER_ADDR') ? $_SERVER['REMOTE_ADDR'] : getenv('HTTP_X_FORWARDED_FOR');
 	}
-
-	function http_post($request, $host, $path) {
-		$http_request  = 
-				"POST " . $path . " HTTP/1.1\r\n" .
-				"Host: " . $host . "\r\n" .
-				"Content-Type: application/x-www-form-urlencoded; charset=utf-8\r\n" .
-				"Content-Length: " . strlen($request) . "\r\n" .
-				"User-Agent: Akismet PHP5 Class " . $this->version . " | Akismet/1.11\r\n" .
-				"Connection: close\r\n" . // cf. http://blog.akismet.com/2008/02/25/using-http-11/
-				"\r\n" .
-				$request
-			;
-
+	
+	/**
+	 * Makes a request to the Akismet service to see if the API key passed to the constructor is valid.
+	 * 
+	 * Use this method if you suspect your API key is invalid.
+	 * 
+	 * @return bool	True is if the key is valid, false if not.
+	 */
+	public function isKeyValid() {
+		// Check to see if the key is valid
+		$response = $this->sendRequest('key=' . $this->wordPressAPIKey . '&blog=' . $this->blogURL, $this->akismetServer, '/' . $this->akismetVersion . '/verify-key');
+		return $response[1] == 'valid';
+	}
+	
+	// makes a request to the Akismet service
+	private function sendRequest($request, $host, $path) {
+		$http_request  = "POST " . $path . " HTTP/1.0\r\n";
+		$http_request .= "Host: " . $host . "\r\n";
+		$http_request .= "Content-Type: application/x-www-form-urlencoded; charset=utf-8\r\n";
+		$http_request .= "Content-Length: " . strlen($request) . "\r\n";
+		$http_request .= "User-Agent: Akismet PHP5 Class " . $this->version . " | Akismet/1.11\r\n";
+		$http_request .= "\r\n";
+		$http_request .= $request;
+		
 		$socketWriteRead = new SocketWriteRead($host, $this->apiPort, $http_request);
 		$socketWriteRead->send();
-
+		
 		return explode("\r\n\r\n", $socketWriteRead->getResponse(), 2);
 	}
-
-	// Formats the data for transmission	echo $sql;
-	function getQueryString() {
+	
+	// Formats the data for transmission
+	private function getQueryString() {
 		foreach($_SERVER as $key => $value) {
 			if(!in_array($key, $this->ignore)) {
 				if($key == 'REMOTE_ADDR') {
@@ -141,24 +159,31 @@ class Akismet {
 		}
 
 		$query_string = '';
-
+		
 		foreach($this->comment as $key => $data) {
-			$query_string .= $key . '=' . urlencode(stripslashes($data)) . '&';
+			if(!is_array($data)) {
+				$query_string .= $key . '=' . urlencode(stripslashes($data)) . '&';
+			}
 		}
-
+		
 		return $query_string;
 	}
-
+	
 	/**
 	 *	Tests for spam.
 	 *
 	 *	Uses the web service provided by {@link http://www.akismet.com Akismet} to see whether or not the submitted comment is spam.  Returns a boolean value.
 	 *
 	 *	@return		bool	True if the comment is spam, false if not
+	 *  @throws		Will throw an exception if the API key passed to the constructor is invalid.
 	 */
-	function isSpam() {
-		$response = $this->http_post($this->getQueryString(), $this->wordPressAPIKey . '.rest.akismet.com', '/' . $this->akismetVersion . '/comment-check');
-
+	public function isCommentSpam() {
+		$response = $this->sendRequest($this->getQueryString(), $this->wordPressAPIKey . '.rest.akismet.com', '/' . $this->akismetVersion . '/comment-check');
+		
+		if($response[1] == 'invalid' && !$this->isKeyValid()) {
+			throw new exception('The Wordpress API key passed to the Akismet constructor is invalid.  Please obtain a valid one from http://wordpress.com/api-keys/');
+		}
+		
 		return ($response[1] == 'true');
 	}
 
@@ -167,107 +192,106 @@ class Akismet {
 	 *
 	 *	Using this function will make you a good citizen as it helps Akismet to learn from its mistakes.  This will improve the service for everybody.
 	 */
-	function submitSpam() {
-		$this->http_post($this->getQueryString(), $this->wordPressAPIKey . '.' . $this->akismetServer, '/' . $this->akismetVersion . '/submit-spam');
+	public function submitSpam() {
+		$this->sendRequest($this->getQueryString(), $this->wordPressAPIKey . '.' . $this->akismetServer, '/' . $this->akismetVersion . '/submit-spam');
 	}
-
+	
 	/**
 	 *	Submit ham that is incorrectly tagged as spam.
 	 *
 	 *	Using this function will make you a good citizen as it helps Akismet to learn from its mistakes.  This will improve the service for everybody.
 	 */
-	function submitHam() {
-		$this->http_post($this->getQueryString(), $this->wordPressAPIKey . '.' . $this->akismetServer, '/' . $this->akismetVersion . '/submit-ham');
+	public function submitHam() {
+		$this->sendRequest($this->getQueryString(), $this->wordPressAPIKey . '.' . $this->akismetServer, '/' . $this->akismetVersion . '/submit-ham');
 	}
-
+	
 	/**
 	 *	To override the user IP address when submitting spam/ham later on
 	 *
 	 *	@param string $userip	An IP address.  Optional.
 	 */
-	function setUserIP($userip) {
+	public function setUserIP($userip) {
 		$this->comment['user_ip'] = $userip;
 	}
-
+	
 	/**
 	 *	To override the referring page when submitting spam/ham later on
 	 *
 	 *	@param string $referrer	The referring page.  Optional.
 	 */
-	function setReferrer($referrer) {
+	public function setReferrer($referrer) {
 		$this->comment['referrer'] = $referrer;
 	}
-
+	
 	/**
 	 *	A permanent URL referencing the blog post the comment was submitted to.
 	 *
 	 *	@param string $permalink	The URL.  Optional.
 	 */
-	function setPermalink($permalink) {
+	public function setPermalink($permalink) {
 		$this->comment['permalink'] = $permalink;
 	}
-
+	
 	/**
-	 *	The type of comment being submitted.
+	 *	The type of comment being submitted.  
 	 *
 	 *	May be blank, comment, trackback, pingback, or a made up value like "registration" or "wiki".
 	 */
-	function setType($commentType) {
+	public function setCommentType($commentType) {
 		$this->comment['comment_type'] = $commentType;
 	}
-
+	
 	/**
 	 *	The name that the author submitted with the comment.
 	 */
-	function setAuthor($commentAuthor) {
+	public function setCommentAuthor($commentAuthor) {
 		$this->comment['comment_author'] = $commentAuthor;
 	}
-
+	
 	/**
 	 *	The email address that the author submitted with the comment.
 	 *
 	 *	The address is assumed to be valid.
 	 */
-	function setAuthorEmail($authorEmail) {
+	public function setCommentAuthorEmail($authorEmail) {
 		$this->comment['comment_author_email'] = $authorEmail;
 	}
-
+	
 	/**
 	 *	The URL that the author submitted with the comment.
-	 */
-	function setAuthorURL($authorURL) {
+	 */	
+	public function setCommentAuthorURL($authorURL) {
 		$this->comment['comment_author_url'] = $authorURL;
 	}
-
+	
 	/**
 	 *	The comment's body text.
 	 */
-	function setContent($commentBody) {
+	public function setCommentContent($commentBody) {
 		$this->comment['comment_content'] = $commentBody;
 	}
-
+	
 	/**
 	 *	Defaults to 80
 	 */
-	function setAPIPort($apiPort) {
+	public function setAPIPort($apiPort) {
 		$this->apiPort = $apiPort;
 	}
-
+	
 	/**
 	 *	Defaults to rest.akismet.com
 	 */
-	function setAkismetServer($akismetServer) {
+	public function setAkismetServer($akismetServer) {
 		$this->akismetServer = $akismetServer;
 	}
-
+	
 	/**
 	 *	Defaults to '1.1'
 	 */
-	function setAkismetVersion($akismetVersion) {
+	public function setAkismetVersion($akismetVersion) {
 		$this->akismetVersion = $akismetVersion;
 	}
 }
-
 
 /**
  *	Utility class used by Akismet
@@ -285,21 +309,21 @@ class Akismet {
  *  @link		http://www.achingbrain.net/
  */
 class SocketWriteRead {
-	var $host;
-	var $port;
-	var $request;
-	var $response;
-	var $responseLength;
-	var $errorNumber;
-	var $errorString;
-
+	private $host;
+	private $port;
+	private $request;
+	private $response;
+	private $responseLength;
+	private $errorNumber;
+	private $errorString;
+	
 	/**
 	 *	@param	string	$host			The host to send/receive data.
 	 *	@param	int		$port			The port on the remote host.
 	 *	@param	string	$request		The data to send.
 	 *	@param	int		$responseLength	The amount of data to read.  Defaults to 1160 bytes.
 	 */
-	function SocketWriteRead($host, $port, $request, $responseLength = 1160) {
+	public function __construct($host, $port, $request, $responseLength = 1160) {
 		$this->host = $host;
 		$this->port = $port;
 		$this->request = $request;
@@ -307,41 +331,41 @@ class SocketWriteRead {
 		$this->errorNumber = 0;
 		$this->errorString = '';
 	}
-
+	
 	/**
 	 *  Sends the data to the remote host.
 	 *
 	 * @throws	An exception is thrown if a connection cannot be made to the remote host.
 	 */
-	function send() {
+	public function send() {
 		$this->response = '';
-
+		
 		$fs = fsockopen($this->host, $this->port, $this->errorNumber, $this->errorString, 3);
-
+		
 		if($this->errorNumber != 0) {
-			trigger_error('Error connecting to host: ' . $this->host . ' Error number: ' . $this->errorNumber . ' Error message: ' . $this->errorString, E_USER_ERROR);
+			throw new Exception('Error connecting to host: ' . $this->host . ' Error number: ' . $this->errorNumber . ' Error message: ' . $this->errorString);
 		}
-
+		
 		if($fs !== false) {
 			@fwrite($fs, $this->request);
-
+			
 			while(!feof($fs)) {
 				$this->response .= fgets($fs, $this->responseLength);
 			}
-
+			
 			fclose($fs);
 		}
 	}
-
+	
 	/**
 	 *  Returns the server response text
 	 *
 	 *  @return	string
 	 */
-	function getResponse() {
+	public function getResponse() {
 		return $this->response;
 	}
-
+	
 	/**
 	 *	Returns the error number
 	 *
@@ -349,10 +373,10 @@ class SocketWriteRead {
 	 *
 	 *	@return int
 	 */
-	function getErrorNumner() {
+	public function getErrorNumner() {
 		return $this->errorNumber;
 	}
-
+	
 	/**
 	 *	Returns the error string
 	 *
@@ -360,8 +384,9 @@ class SocketWriteRead {
 	 *
 	 *	@return string
 	 */
-	function getErrorString() {
+	public function getErrorString() {
 		return $this->errorString;
 	}
 }
+
 ?>
--- _dev_/akismet/inc/forum_insert.php
+++ _dev_/akismet/inc/forum_insert.php
@@ -23,13 +23,13 @@ function inc_forum_insert() {
 		// Utilisez votre propre cle de developpeur Wordress, 
 		// disponible sur http://akismet.com/personal/ pour un usage personnel / non commercial
 		// ou http://akismet.com/commercial/ sinon
-		$akismet->setAuthor($_POST['auteur']);
-		$akismet->setAuthorEmail($_POST['email_auteur']);
-		$akismet->setAuthorURL($_POST['url_site']);
-		$akismet->setContent($_POST['texte']);
-		$akismet->setType('comment');
-		$akismet->setPermalink('http://' . $GLOBALS['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
-		if ($akismet->isSpam()) {
+		$akismet->setCommentAuthor($_POST['auteur']);
+		$akismet->setCommentAuthorEmail($_POST['email_auteur']);
+		$akismet->setCommentAuthorURL($_POST['url_site']);
+		$akismet->setCommentContent($_POST['texte']);
+		$akismet->setCommentType('comment');
+		$akismet->setCommentPermalink('http://' . $GLOBALS['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
+		if ($akismet->isCommentSpam()) {
 			// sauvegarde le commentaire mais le garde comme spam
 			// (en cas de faux positif)
 			tracer_erreur_forum('Akismet a d&#233;tect&#233; un SPAM');
