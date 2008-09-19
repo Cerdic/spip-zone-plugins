4693711689c94ee733de8a0770b418305115668a
webmaster@geomaticien.com <webmaster@geomaticien.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-09-19 15:54:58 +0000

    Removed (-> Fond)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@22660 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/acs/models/cat/composants/pied/ecrire/lang/pied_ecrire_en.php
+++
@@ -1,9 +0,0 @@
-<?php
-// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
-$GLOBALS[$GLOBALS['idx_lang']] = array(
-'nom' => 'Footer',
-'description' => 'Container for other components, and end of page.',
-'help' => 'By default, Pied contain Ours component.',
-);
-?>
