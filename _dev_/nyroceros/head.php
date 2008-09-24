e178b8beab33f810a1ad89cca7f26bfced9720ff
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-09-24 10:39:42 +0000

    "un position:relative/z-index sur #nyromodal pour etre sur de passer au dessus de tout deporter le js dans un script separe qui peut etre compacte, et ne garder que le mini inline"
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@22719 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/nyroceros/head.php
+++ _dev_/nyroceros/head.php
@@ -15,62 +15,20 @@ function Nyro_insert_head($flux){
 
 	$flux .='
 <script src=\''.url_absolue(find_in_path('js/jquery.nyroModal-1.2.8.js')).'\' type=\'text/javascript\'></script>
-<script type="text/javascript"><!--
-// Inside the function "this" will be "document" when called by ready() 
-// and "the ajaxed element" when called because of onAjaxLoad 
-if (window.jQuery)
-(function($){
-var init_f = function() {';
-
-if ($config['traiter_toutes_images'] == 'oui') {
-	$flux .='
-// selectionner tous les liens vers des images
-$("a[@type=\'image/jpeg\'],a[@type=\'image/png\'],a[@type=\'image/gif\']",this)
-.addClass("nyroceros") // noter qu\'on l\'a vue
-.attr("onclick","") // se debarrasser du onclick de SPIP
-.nyroModal({bgColor: "'.$config['bgcolor'].'"}); // activer le nyro
-';
-}
-
-$flux .= '
-// passer le portfolio en mode galerie de nyro
-$("'.$config['selecteur_galerie'].'", this)
-.attr("rel","galerie-portfolio");
-
-// charger nyro sur autre chose
-$("'.$config['selecteur_commun'].'").nyroModal({bgColor: "'.$config['bgcolor'].'"});
-
-'
-. (($config['preload'] == 'non')
-  ? ''
-  : '
-  // preload images
-  $.fn.preload = function() {
-    var url;
-    return this.each(function() {
-      if ((url = $(this).attr("href")) && url.match(/\.(jpg|jpeg|png|gif)$/)) {
-        var img = new Image;
-        img.src = url;
-      }
-    });
-  }
-
-  $.fn.nyroModal.settings.endShowContent = function(elts,settings) {
-    $(".nyroModalNext").preload();
-  }
-  
-  $(".nyroceros[@rel]:eq(0)").preload();
-  ')
-
-.'
-}
-
+<script src=\''.url_absolue(find_in_path('js/nyromodal.js')).'\' type=\'text/javascript\'></script>
+<script type="text/javascript">/* <![CDATA[ */
+var nyro_traiter_toutes_images="'.($config['traiter_toutes_images'] == 'oui'?'true':'false').'";
+var nyro_bgcolor="'.$config['bgcolor'].'";
+var nyro_selecteur_galerie="'.$config['selecteur_galerie'].'";
+var nyro_selecteur_commun="'.$config['selecteur_commun'].'";
+var nyro_preload="'.($config['preload'] == 'non'?'false':'true').'";
 //onAjaxLoad is defined in private area only
-if(typeof onAjaxLoad == "function") onAjaxLoad(init_f);
+if (window.jQuery)
+(function($){if(typeof onAjaxLoad == "function") onAjaxLoad(init_f);
 	$(init_f);
  })(jQuery);
- // --></script>
-<link rel="stylesheet" href="'.url_absolue(find_in_path('styles/nyroModal.full.css')).'" type="text/css" />
+/* ]]> */</script>
+<link rel="stylesheet" href="'.url_absolue(find_in_path('styles/nyroModal.full.css')).'" type="text/css" media="all"/>
 ';
 	return $flux;
 }
---
+++ _dev_/nyroceros/js/nyromodal.js
@@ -0,0 +1,34 @@
+// Inside the function "this" will be "document" when called by ready() 
+// and "the ajaxed element" when called because of onAjaxLoad 
+var init_f = function() {
+	if (nyro_traiter_toutes_images) {
+		// selectionner tous les liens vers des images
+		$("a[@type=\'image/jpeg\'],a[@type=\'image/png\'],a[@type=\'image/gif\']",this)
+		.addClass("nyroceros") // noter qu\'on l\'a vue
+		.attr("onclick","") // se debarrasser du onclick de SPIP
+		.nyroModal({bgColor: nyro_bgcolor}); // activer le nyro
+	}
+	// passer le portfolio en mode galerie de nyro
+	$(nyro_selecteur_galerie, this)
+	.attr("rel","galerie-portfolio");
+
+	// charger nyro sur autre chose
+	$(nyro_selecteur_commun).nyroModal({bgColor: nyro_bgcolor});
+
+  // preload images
+	if (nyro_preload) {
+	  $.fn.preload = function() {
+	    var url;
+	    return this.each(function() {
+	      if ((url = $(this).attr("href")) && url.match(/\.(jpg|jpeg|png|gif)$/ )) {
+	        var img = new Image;
+	        img.src = url;
+	      }
+	    });
+	  }
+	  $.fn.nyroModal.settings.endShowContent = function(elts,settings) {
+	    $(".nyroModalNext").preload();
+	  } 
+	  $(".nyroceros[@rel]:eq(0)").preload();
+	}
+};
