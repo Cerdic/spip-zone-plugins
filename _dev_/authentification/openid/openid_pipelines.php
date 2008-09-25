8163afd4e4d5df208e90ceb8e2f54c1a7a38ea78
marcimat@free.fr <marcimat@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-09-25 17:44:33 +0000

    -* Bien enregistrer le champ openid dans l'interface auteur -* déclaration des tables avec api spip 2
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@22751 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/authentification/openid/openid_pipelines.php
+++ _dev_/authentification/openid/openid_pipelines.php
@@ -19,11 +19,13 @@ function openid_formulaire_verifier($flux){
 function openid_editer_contenu_objet($flux){
 	if ($flux['args']['type']=='auteur') {
 		include_spip('public/assembler');
-		$openid = recuperer_fond('formulaires/inc-openid', $flux['args']);
+		$flux['args']['contexte']['openid'] = sql_getfetsel('openid','spip_auteurs','id_auteur='.sql_quote($flux['args']['contexte']['id_auteur']));
+		$openid = recuperer_fond('formulaires/inc-openid', $flux['args']['contexte']);
 		$flux['data'] = preg_replace('%(<li class="editer_email(.*?)</li>)%is', '$1'."\n".$openid, $flux['data']);
 	}
 	return $flux;
 }
+
 
 
 
