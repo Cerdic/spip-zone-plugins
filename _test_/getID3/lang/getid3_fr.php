ac0940e7bcee20f13290af72fc927c2553bc8b5c
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-09-25 16:01:34 +0000

    Ce qu'il manquait
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@22747 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _test_/getID3/lang/getid3_fr.php
@@ -0,0 +1,24 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	'recuperer_infos' => 'R&eacute;cup&eacute;rer les informations du fichier',
+	
+	'son_infos_techniques' => 'Infos techniques',
+	'son_nom_fichier' => 'Nom du fichier',
+	'son_extension' => 'Extension :',
+	'son_format' => 'Mime-Type :',
+	'son_duree' => 'Dur&eacute;e :',
+	'son_bitrate' => 'Bitrate :', 
+	'son_bitratemode' => 'Mode :',
+	'son_bitrate_cbr' => 'Bitrate constant',
+	'son_bitrate_vbr' => 'Bitrate variable',
+	'son_audiosamplerate' => 'Audio Samplerate :',
+	'son_encodeur' => 'Encodeur :',
+	'verifier_formulaire' => 'V&eacute;rifiez le remplissage du formulaire.'
+	
+);
+
+
+?>
