724119ac6daaa48ace5052a08fe740942b618d9a
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-09-26 05:07:39 +0000

    Genre ou Comment dans le descriptif
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@22765 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/getID3/inc/getid3_recuperer_infos.php
+++ _test_/getID3/inc/getid3_recuperer_infos.php
@@ -10,15 +10,20 @@
 		$recuperer_id3 = charger_fonction('recuperer_id3','inc');
 		$id3 = $recuperer_id3($son_chemin);
 		if($document['titre']==''){
-			$document['titre'] = ereg_replace('_',' ',$id3['titre']);
+			$document['titre'] = ereg_replace('_',' ',utf8_encode($id3['titre']));
 		}
 		if($document['titre'] == ''){
 			$titre = substr(basename($document['fichier']), 0, -4);
 			$document['titre'] = ereg_replace('_',' ',$titre);
 		}
 		if($document['descriptif']==''){
-			$document['descriptif'] = $id3['artiste']?utf8_encode($id3['artiste']).' - ':'';
-			$document['descriptif'] .= utf8_encode($id3['album']);
+			
+			if($id3['comment']){
+				$document['descriptif'] = utf8_encode($id3['comment']);
+			}
+			else{
+				$document['descriptif'] = utf8_encode($id3['genre']);
+			}
 		}
 		sql_updateq('spip_documents',array('titre'=>$document['titre'],'descriptif'=>$document['descriptif'],'duree'=> $id3['duree'], 'bitrate' => $id3['bitrate'], 'bitrate_mode'=>$id3['bitrate_mode'],'audiosamplerate'=>$id3['audiosamplerate'], 'encodeur'=>$id3['encoded_by']),'id_document='.sql_quote($id_document));
 		return;
