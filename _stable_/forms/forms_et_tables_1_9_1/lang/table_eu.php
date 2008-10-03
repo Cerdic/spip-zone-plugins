08fb9b9ee61179437bb1fda336b518754c8ef42a
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-10-03 10:21:00 +0000

    langues (table)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@22895 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _stable_/forms/forms_et_tables_1_9_1/lang/table_eu.php
@@ -0,0 +1,55 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'articles_utilisant' => 'Mahai hori erabiltzen dituzten artikuluak',
+	'aucune_reponse' => 'Daturik ez',
+
+	// C
+	'champs_formulaire' => 'Mahaiaren eremuak',
+
+	// F
+	'formulaire' => 'Mahaia',
+
+	// I
+	'icone_ajouter_donnees' => 'Datuak gehitu',
+	'icone_creer_table' => 'Mahai berri bat gehitu',
+	'importer_donnees_csv' => 'Datuak inportatu',
+	'info_supprimer_formulaire' => 'Mahai hori ezabatu nahi duzula segur zirea ?',
+	'info_supprimer_formulaire_reponses' => 'datu batzu mahai huni lotuak dira. Ezabatu nahi duzula segur zira ?',
+
+	// L
+	'lien_retirer_donnee_liante' => 'Datu hunengandik lotura kendu',
+	'lien_retirer_donnee_liee' => 'Datu hau kendu',
+
+	// N
+	'nombre_reponses' => '@nombre@ datu',
+	'nouveau_formulaire' => 'Mahai berria',
+
+	// S
+	'suivi_reponses' => 'Datuak ikusi',
+	'supprimer_formulaire' => 'Mahai hau ezabatu',
+
+	// T
+	'telecharger_reponses' => 'Datuak telekargatu',
+	'texte_donnee_statut' => 'Datu hunen estatusa',
+	'texte_statut_poubelle' => 'Kendutako datua',
+	'texte_statut_prepa' => 'Datua idazterakoan',
+	'texte_statut_prop' => 'Proposatutako datua',
+	'texte_statut_publie' => 'Argitaratutako datua',
+	'texte_statut_refuse' => 'Errefusatutako datua',
+	'titre_formulaire' => 'Mahaiaren titulua',
+	'toutes_tables' => 'Mahai guziak',
+	'type_des_tables' => 'Mahaiak',
+
+	// U
+	'une_reponse' => 'Datu bat'
+);
+
+?>
