ca47ef33aead1ae86438613ae59bf1a7019cebbd
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-10-03 10:21:05 +0000

    langues (sedna)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@22897 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _test_/sedna/lang/sedna_eu.php
@@ -0,0 +1,51 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'aff_resume' => 'Bildumak erakutsi',
+	'afficher_sources' => 'Iturri hau erakutsi',
+	'annee' => 'Urte',
+	'articles_recents_court' => 'Azken artikuluak',
+
+	// C
+	'connexion' => 'Konexioa',
+
+	// D
+	'deconnexion' => 'Deskonexioa',
+	'derniere_syndication' => '<NEW>Derni&egrave;re syndication de ce site effectu&eacute;e',
+	'deuxjours' => 'Bi egun',
+
+	// L
+	'liens' => 'artikulua',
+	'liens_pluriel' => 'artikuluak',
+
+	// M
+	'masquer_resume' => 'Bildumak maskatu',
+	'masquer_sources' => 'Iturri hau maskatu',
+	'mois' => 'Hilabete',
+
+	// P
+	'pas_articles' => 'Garai hunetan ez dago artikulurik&nbsp;!',
+	'pas_synchro' => '<NEW>Ne pas synchroniser',
+	'preferences' => 'Hobespenak',
+	'probleme_de_syndication' => '<NEW>probl&egrave;me de syndication',
+
+	// S
+	'semaine' => 'Astea',
+	'sources' => 'Iturriak',
+	'synchro' => '<NEW>Synchroniser',
+	'synchro_titre' => '<NEW>Enregistrer sur le site la liste des articles lus',
+	'syndication_ajour' => '<NEW>Mettre &agrave; jour maintenant',
+	'syndication_fait' => '<NEW>Syndication effectu&eacute;e',
+
+	// T
+	'toutes' => 'Denak'
+);
+
+?>
