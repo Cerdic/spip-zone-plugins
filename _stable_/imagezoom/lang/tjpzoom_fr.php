c4df74e6ae20053233aa70e8d67efe6e766dc8ed
pierre.andrews@gmail.com <pierre.andrews@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-10-03 16:43:55 +0000

    loupe pour les images. un nouveau modele <imgX|zoom> permet d'inserer des images avec une loupe qui affiche une portion de l'image originale au dessus de la vignette de l'image dans le texte.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@22900 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _stable_/imagezoom/lang/tjpzoom_fr.php
@@ -0,0 +1,25 @@
+<?php
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+									   //D
+									   'descriptif' => 'Cette page permet de configurer le style de la loupe. Un style personalis&eacute; peut &ecirc;tre d&eacute;clar&eacute;; voir <a href="http://plugins.spip.net/?page=article&id_article=48">la documentation en ligne</a>.'
+
+									   //E
+									   'example' => 'R&eacute;sultat graphique',
+
+									   //N
+									   'nostyle' => 'Sans Style',
+
+									   //O
+									   'other' => 'Autre',
+
+									   //S
+									   'style' => 'Style de la Loupe',
+
+									   //T
+									   'titre' => 'Configuration de la Loupe',
+									   );
+
+?>
---
+++ _stable_/imagezoom/tjpzoom_pipelines_public.php
@@ -0,0 +1,49 @@
+<?php
+
+//    Fichier créé pour SPIP.
+//    Distribué sans garantie sous licence GPL.
+//    Copyright (C) 2008  Pierre ANDREWS
+//
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program; if not, write to the Free Software
+//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+  //pipeline pour ajouter des scripts dans la page public
+function tjpzoom_insert_head($flux){
+  //trouver la lib tjpzoom
+  $tjp = find_in_path('lib/tjpzoom/tjpzoom.js');
+  if(!$tjp) $tjp = find_in_path('tjpzoom/tjpzoom.js'); //cas 1.9.2
+  $toRet = $flux.'<script type="text/javascript" src="'.$tjp.'"></script>';
+
+  //chercher les configs de la loupe
+  if(($style = lire_config('tjpzoom/zoomstyle')) != '') {
+	if($style == 'other') $style = lire_config('tjpzoom/zoomstyle_o'); //si le style est autre
+	$conf = find_in_path("lib/tjpzoom/tjpzoom_config_$style.js"); 
+	if(!$conf) $conf = find_in_path("tjpzoom_config_$style.js"); // cas 1.9.2
+	$toRet .= '<script type="text/javascript" src="'.$conf.'"></script>';
+
+	if($style == 'default' || $style == 'smart' || $style == 'relative'){
+	  $shadow = find_in_path("lib/tjpzoom/dropshadow");
+	  if(!$shadow) $shadow = find_in_path("tjpzoom/dropshadow");
+	} else {
+	  $shadow = find_in_path("lib/tjpzoom/".$style);
+	  if(!$shadow) $shadow = find_in_path("tjpzoom/$style"); //cas 1.9.2
+	  if(!$shadow) $shadow = find_in_path($style); // style perso direct dans squelettes
+	}
+	if($shadow)
+	  $toRet .= '<script type="text/javascript">var TJPshadow="'.$shadow.'/"</script>';
+  }
+  return $toRet;
+}
+
+?>
