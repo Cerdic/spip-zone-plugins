52897fa9fdc6312d1e0a01d72cb300074ea2356c
patfr@ifrance.com <patfr@ifrance.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-10-04 22:25:33 +0000

    Typo exposants : ajout de Vve(s), Mn(s), Md(s), Bd(s), Cie(s), 1o, 2o, etc. + qq optimisations du code (assertions)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@22925 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/couteau_suisse/outils/typo_exposants.php
+++ _stable_/couteau_suisse/outils/typo_exposants.php
@@ -7,49 +7,49 @@ include_spip('inc/charsets');
 // en principe, pas besoin de : caractere_utf_8(232)
 define('_TYPO_EGRAVE', unicode2charset('&#232;').'|&#232;|&egrave;');
 define('_TYPO_sup', '<sup class="typo_exposants">\\1</sup>');
-define('_TYPO_Msup', 'M'._TYPO_sup);
-define('_TYPO_Psup', '\\1<sup class="typo_exposants">\\2</sup>');
-define('_TYPO_Dsup', _TYPO_Psup.'\\3');
+define('_TYPO_sup2', '\\1<sup class="typo_exposants">\\2</sup>');
 
 // cette fonction ne fonctionne que pour le francais
 // elle n'est pas appelee dans les balises html : html|code|cadre|frame|script|acronym|cite
 function typo_exposants_fr($texte){
-	$trouve = array(
-		'/\bMe?(lles?)\b/',		// Mlle(s), Mme(s) et erreurs Melle(s)
-		'/\bM(mes?)\b/',
-		'/\bM(gr)\b/',		// Mgr
+	static $typo;
+	if(!$typo) $typo = array( array(
+		'/(?<=\bM)e?(lles?)\b/',		// Mlle(s), Mme(s) et erreurs Melle(s)
+		'/(?<=\bM)(gr|mes?)\b/',	// Mme(s) et Mgr
 
-		'/\b(D|P)(rs?)([\s\.-])/',	// Dr(s), Pr(s) suivis d'un espace d'un point ou d'un tiret
-		'/\b(S)(te?s?)([\s\.-])/',  // St(e)(s) suivis d'un espace d'un point ou d'un tiret
-		'/\b(B)(x|se|ses)([\s\.-])/',  // Bx, Bse(s) suivis d'un espace d'un point ou d'un tiret
+		'/(?<=\b[DP])(rs?)(?=[\s\.-])/',	// Dr(s), Pr(s) suivis d'un espace d'un point ou d'un tiret
+		'/(?<=\bS)(te?s?)(?=[\s\.-])/',  // St(e)(s) suivis d'un espace d'un point ou d'un tiret
+		'/(?<=\bB)(x|se|ses)(?=[\s\.-])/',  // Bx, Bse(s) suivis d'un espace d'un point ou d'un tiret
 
-		'/\bm(2|3)\b/',	 // m2, m3, m²
-		'/\bm²\b/',
+		'/\bm²\b/', '/(?<=\bm)([23])\b/',	 // m2, m3, m²
+		'/(?<=\b[Mm])([nd]s?)\b/',	// millions, milliards
+		'/(?<=\bV)(ves?)\b/', '/(?<=\b[Bb])(ds?)\b/', '/(?<=\bC)(ies?)\b/',	// veuves, boulevard(s) et Cie(s)
 
-		'/(\\b[1I])i?(ers?)\\b/',	// Erreurs ier, iers
-		'/(\\b[1I])i?(?:'._TYPO_EGRAVE.')(res?)\\b/',	// Erreurs ère(s), ière(s)
-		'/(\\b1)(r?es?)\\b/', // 1e(s), 1re(s)
-		'/\\b2(nde?s?)\\b/',	// 2nd(e)(s)
+		'/(?<=\b[1I])i?(ers?)\b/',	// Erreurs ier, iers
+		'/(?<=\b[1I])i?(?:'._TYPO_EGRAVE.')(res?)\b/',	// Erreurs ère(s), ière(s)
+		'/(?<=\b1)(r?es?)\b/', // 1e(s), 1re(s)
+		'/(?<=\b2)(nde?s?)\b/',	// 2nd(e)(s)
 
-		'/(\\b[0-9IVX]+)i?(?:e|'._TYPO_EGRAVE.')?me(s?)\\b/', // Erreurs me, eme, ème, ième + pluriels
+		'/(\b[0-9IVX]+)i?(?:e|'._TYPO_EGRAVE.')?me(s?)\b/', // Erreurs me, eme, ème, ième + pluriels
 		'/\b([0-9IVX]+)(es?)\b/', // 2e(s), IIIe(s)... (les 1(e?r?s?) ont deja ete remplaces)
-	);
-	$remplace = array(
-		_TYPO_Msup, _TYPO_Msup, _TYPO_Msup,		// Mlle(s), Mme(s)
+		'/\b(\d+)o\b/', // primo, secondo, etc.
+	), array(
+		_TYPO_sup, _TYPO_sup,		// Mlle(s), Mme(s), Mgr
 
-		_TYPO_Dsup, _TYPO_Dsup, _TYPO_Dsup,	// Dr(s), Pr(s), St(e)(s), Bx, Bse(s)
+		_TYPO_sup, _TYPO_sup, _TYPO_sup,	// Dr(s), Pr(s), St(e)(s), Bx, Bse(s)
 
-		'm'._TYPO_sup,	// m2, m3, m²
-		'm<sup class="typo_exposants">2</sup>',
+		'm<sup class="typo_exposants">2</sup>',	_TYPO_sup,	// m2, m3, m²
+		_TYPO_sup, _TYPO_sup, _TYPO_sup, _TYPO_sup,	// Vve(s), Mn(s), Md(s), Bd(s), Cie(s)
 
-		_TYPO_Psup, _TYPO_Psup, _TYPO_Psup, // 1er et Cie
-		'2'._TYPO_sup,	// 2nd(e)(s)
+		_TYPO_sup, _TYPO_sup, _TYPO_sup, // 1er et Cie
+		_TYPO_sup,	// 2nd(e)(s)
 
-		'\\1<sup class="typo_exposants">e\\2</sup>', // Erreurs me, eme, ème, ième + pluriels
-		_TYPO_Psup, // 2e(s), IIIe(s)...
-	);
+		'$1<sup class="typo_exposants">e$2</sup>', // Erreurs me, eme, ème, ième + pluriels
+		_TYPO_sup2, // 2e(s), IIIe(s)...
+		'$1<sup class="typo_exposants">o</sup>', // 1o, 2o, etc.
+	));
 
-	return preg_replace($trouve, $remplace, $texte);
+	return preg_replace($typo[0], $typo[1], $texte);
 }
 
 function typo_exposants_echappe_balises_callback($matches) {
