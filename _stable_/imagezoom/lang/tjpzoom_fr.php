eb2329b930fd992b577fa406fe577d6f3aa8b7e0
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-10-04 07:56:19 +0000

    langues (tjpzoom)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@22915 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/imagezoom/lang/tjpzoom_fr.php
+++ _stable_/imagezoom/lang/tjpzoom_fr.php
@@ -1,25 +1,27 @@
 <?php
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/_stable_/imagezoom/lang/
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-									   //D
-									   'descriptif' => 'Cette page permet de configurer le style de la loupe. Un style personalis&eacute; peut &ecirc;tre d&eacute;clar&eacute;; voir <a href="http://plugins.spip.net/?page=article&id_article=48">la documentation en ligne</a>.'
 
-									   //E
-									   'example' => 'R&eacute;sultat graphique',
+	// D
+	'descriptif' => 'Cette page permet de configurer le style de la loupe. Un style personalis&eacute; peut &ecirc;tre d&eacute;clar&eacute;; voir <a href="http://plugins.spip.net/?page=article&id_article=48">la documentation en ligne</a>.',
 
-									   //N
-									   'nostyle' => 'Sans Style',
+	// E
+	'example' => 'R&eacute;sultat graphique',
 
-									   //O
-									   'other' => 'Autre',
+	// N
+	'nostyle' => 'Sans Style',
 
-									   //S
-									   'style' => 'Style de la Loupe',
+	// O
+	'other' => 'Autre',
 
-									   //T
-									   'titre' => 'Configuration de la Loupe',
-									   );
+	// S
+	'style' => 'Style de la Loupe',
+
+	// T
+	'titre' => 'Configuration de la Loupe'
+);
 
 ?>
