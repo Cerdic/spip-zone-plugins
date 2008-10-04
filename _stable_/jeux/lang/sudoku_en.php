18e8cdef1426dcad9023fb97316e2724c1937786
patfr@ifrance.com <patfr@ifrance.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-10-04 00:29:42 +0000

    scorie... à traduire ;)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@22904 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/jeux/lang/sudoku_en.php
+++ _stable_/jeux/lang/sudoku_en.php
@@ -1,5 +1,3 @@
-'regle'		=> "R&amp;egrave;gles du jeu :<br />
-- En partant des chiffres d&eacute;j&agrave; inscrits, remplissez la grille de mani&egrave;re que chaque ligne, chaque colonne et chaque carr&eacute; de @largeur@ par @hauteur@ contienne une seule fois tous les chiffres de 1 &agrave; @max@."
 <?php
 
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
