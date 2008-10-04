07f971eb7e0aa6a3dc217a0c23fc19f312d7047f
pierre.andrews@gmail.com <pierre.andrews@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-10-04 19:01:51 +0000

    correction du bug d'oubli de mot de passe.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@22922 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/passe_complexe/action/pass.php
+++ _test_/passe_complexe/action/pass.php
@@ -7,17 +7,19 @@ function action_pass()
 {
 	utiliser_langue_visiteur();
 
-	include_spip('inc/passe_complexe');
-	//avec minipres, on n'a pas de pipeline insert_head ou header_prive, donc on triche...
-	//on integre le script apres, mais il faut aussi ajouter jquery qui n'est pas la normalement
-	$flux = '<script type="text/javascript" src="'.generer_url_public('jquery.js').'"></script>';
-	$flux .= passe_complexe_generer_javascript('#oubli');
+	if(_request('p')) {
+	  include_spip('inc/passe_complexe');
+	  //avec minipres, on n'a pas de pipeline insert_head ou header_prive, donc on triche...
+	  //on integre le script apres, mais il faut aussi ajouter jquery qui n'est pas la normalement
+	  $flux = '<script type="text/javascript" src="'.generer_url_public('jquery.js').'"></script>';
+	  $flux .= passe_complexe_generer_javascript('#oubli');
 
-	echo str_replace('</head>',$flux."\n</head>",
-					 install_debut_html(_T('pass_mot_oublie'), " class='pass'")
-					 );
+	  echo str_replace('</head>',$flux."\n</head>",
+					   install_debut_html(_T('pass_mot_oublie'), " class='pass'")
+					   );
+	}
 
-	inclure_balise_dynamique(formulaire_oubli_dyn('test', _request('oubli')));
+	inclure_balise_dynamique(formulaire_oubli_dyn(_request('p'), _request('oubli')));
 	echo install_fin_html();
 }
 
