3dab82c75916a9421e68f461a31f694acb941b7b
pierre.andrews@gmail.com <pierre.andrews@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-10-05 21:32:35 +0000

    ready spip2... ou presque
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@22947 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/passe_complexe/passe_complexe_options.php
+++ _test_/passe_complexe/passe_complexe_options.php
@@ -4,11 +4,14 @@
 function passe_complexe_header_prive($flux){
   if (_request('exec')=='auteur_infos'){
 	include_spip('inc/passe_complexe');
-	$flux .= passe_complexe_generer_javascript("input.formo[@name=new_pass]");
+	$flux .= passe_complexe_generer_javascript("input[@name=new_pass]");
   } else if (_request('exec')=='cfg'){
 	include_spip('inc/passe_complexe');
 	$flux .= passe_complexe_generer_javascript("input.type_pwd");
-  }
+  } else if (_request('p')){
+	include_spip('inc/passe_complexe');
+	$flux .= passe_complexe_generer_javascript("input#oubli");
+}
   return $flux;
 }
 
