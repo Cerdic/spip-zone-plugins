f3e7a9fe30ef29299143671d90c8a3fb748e4452
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-10-06 15:32:22 +0000

    on n'a plus d'id mais id_auteur on mets les fonctions dans un fichier séparé si des fois on a plusieurs formulaires différents
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@22963 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/inscription2/inscription2_193/formulaires/inscription2_ajax.php
+++ _test_/inscription2/inscription2_193/formulaires/inscription2_ajax.php
@@ -12,8 +12,10 @@
 
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
-//charger cfg
-include_spip('cfg_options');   
+// charger cfg
+include_spip('cfg_options');
+// charger les fonctions de formilaires
+include_spip('inc/inscription2_form_fonctions');
 
 // chargement des valeurs par defaut des champs du formulaire
 function formulaires_inscription2_ajax_charger_dist($id_auteur = NULL){
@@ -170,7 +172,7 @@ function formulaires_inscription2_ajax_traiter_dist($id_auteur = NULL){
 	$val = array_intersect_key($valeurs,$clefs);
 	
 	//recherche la presence d'un complément sur l'auteur
-	$id_elargi = sql_getfetsel('id','spip_auteurs_elargis','id_auteur='.$id_auteur);
+	$id_elargi = sql_getfetsel('id_auteur','spip_auteurs_elargis','id_auteur='.$id_auteur);
 	
 	if ($id_elargi) {
 		$where = 'id_auteur = '.$id_auteur;
@@ -197,69 +199,4 @@ function formulaires_inscription2_ajax_traiter_dist($id_auteur = NULL){
     return $message;
 }
 
-/*
- *  ! brief Determine les champs de formulaire à traiter
- */
-function inscription2_champs_formulaire() {
-
-	//charge les valeurs de chaque champs proposés dans le formulaire   
-	foreach (lire_config('inscription2/') as $clef => $valeur) {
-		/* Il faut retrouver les noms des champ, 
-		* par défaut inscription2 propose pour chaque champ le cas champ_obligatoire
-		*  On retrouve donc les chaines de type champ_obligatoire
-		*  Ensuite on verifie que le champ est proposé dans le formulaire
-		*  Remplissage de $valeurs[]
-		*/
-		//decoupe la clef sous le forme $resultat[0] = $resultat[1] ."_obligatoire"
-		//?: permet de rechercher la chaine sans etre retournée dans les résultats
-		preg_match('/^(.*)(?:_obligatoire)/i', $clef, $resultat);
-	
-		if ((!empty($resultat[1])) && (lire_config('inscription2/'.$resultat[1]) == 'on')) {
-			$valeurs[] = $resultat[1];
-			$valeur = _request($resultat[1]);
-			$valeurs[$resultat[1]] = $valeur;
-		}
-	}
-	return $valeurs;
-}
-/*
-// http://doc.spip.org/@test_inscription_dist
-function test_inscription_dist($mode, $mail, $nom, $id=0) {
-
-    include_spip('inc/filtres');
-    $nom = trim(corriger_caracteres($nom));
-    if (!$nom || strlen($nom) > 64)
-        return _T('ecrire:info_login_trop_court');
-    if (!$r = email_valide($mail)) return _T('info_email_invalide');
-    return array('email' => $r, 'nom' => $nom, 'bio' => $mode);
-}
-*/
-
-function inscription2_valide_cp($cp){
-	if(!$cp){
-		return;
-	}
-	else{
-		if(preg_match('/^[A-Z]{1,2}[-|\s][0-9]{3,6}$|^[0-9]{3,6}$|^[0-9|A-Z]{2,5}[-|\s][0-9|A-Z]{2,4}$|^[A-Z]{1,2} [0-9|A-Z]{2,5}[-|\s][0-9|A-Z]{2,4}/i',$cp)){
-			return;
-		}
-		else{
-			return _T('inscription2:cp_valide');
-		}
-	}
-}
-
-function inscription2_valide_numero($numero){
-	if(!$numero){
-		return;
-	}
-	else{
-		if(preg_match('/^[0-9\+\. \-]+$/',$numero)){
-			return;
-		}
-		else{
-			return _T('inscription2:numero_tva_valide');
-		}
-	}
-}
 ?>
---
+++ _test_/inscription2/inscription2_193/inc/inscription2_form_fonctions.php
@@ -0,0 +1,80 @@
+<?php
+/*
+ *  ! brief Determine les champs de formulaire à traiter
+ */
+function inscription2_champs_formulaire() {
+
+	//charge les valeurs de chaque champs proposés dans le formulaire   
+	foreach (lire_config('inscription2/') as $clef => $valeur) {
+		/* Il faut retrouver les noms des champ, 
+		* par défaut inscription2 propose pour chaque champ le cas champ_obligatoire
+		*  On retrouve donc les chaines de type champ_obligatoire
+		*  Ensuite on verifie que le champ est proposé dans le formulaire
+		*  Remplissage de $valeurs[]
+		*/
+		//decoupe la clef sous le forme $resultat[0] = $resultat[1] ."_obligatoire"
+		//?: permet de rechercher la chaine sans etre retournée dans les résultats
+		preg_match('/^(.*)(?:_obligatoire)/i', $clef, $resultat);
+	
+		if ((!empty($resultat[1])) && (lire_config('inscription2/'.$resultat[1]) == 'on')) {
+			$valeurs[] = $resultat[1];
+			$valeur = _request($resultat[1]);
+			$valeurs[$resultat[1]] = $valeur;
+		}
+	}
+	return $valeurs;
+}
+/*
+// http://doc.spip.org/@test_inscription_dist
+function test_inscription_dist($mode, $mail, $nom, $id=0) {
+
+    include_spip('inc/filtres');
+    $nom = trim(corriger_caracteres($nom));
+    if (!$nom || strlen($nom) > 64)
+        return _T('ecrire:info_login_trop_court');
+    if (!$r = email_valide($mail)) return _T('info_email_invalide');
+    return array('email' => $r, 'nom' => $nom, 'bio' => $mode);
+}
+*/
+
+function inscription2_valide_cp($cp){
+	if(!$cp){
+		return;
+	}
+	else{
+		if(preg_match('/^[A-Z]{1,2}[-|\s][0-9]{3,6}$|^[0-9]{3,6}$|^[0-9|A-Z]{2,5}[-|\s][0-9|A-Z]{2,4}$|^[A-Z]{1,2} [0-9|A-Z]{2,5}[-|\s][0-9|A-Z]{2,4}/i',$cp)){
+			return;
+		}
+		else{
+			return _T('inscription2:cp_valide');
+		}
+	}
+}
+
+function inscription2_valide_numero($numero){
+	if(!$numero){
+		return;
+	}
+	else{
+		if(preg_match('/^[0-9\+\. \-]+$/',$numero)){
+			return;
+		}
+		else{
+			return _T('inscription2:numero_tva_valide');
+		}
+	}
+}
+
+function inscription2_valid_login($login,$nom) {
+	if(!isset($login))
+		$login=$nom;
+	for ($i = 1; ; $i++) {
+		$n = sql_count(sql_select("id_auteur","spip_auteurs","login='$login' LIMIT 1"));
+		if (!$n){
+			return $login;
+		}
+		$login = $login.$i;
+	}
+	return $login;
+}
+?>
