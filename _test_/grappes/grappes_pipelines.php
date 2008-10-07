aa6931e13f645dc68020d5e4cb6c94736f8659e4
marcimat@free.fr <marcimat@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-10-07 20:19:41 +0000

    Suivi des améliorations sur le sélecteur générique
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@23021 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/grappes/grappes_pipelines.php
+++ _test_/grappes/grappes_pipelines.php
@@ -8,43 +8,36 @@
 function grappes_inserer_js_recherche_objet(){
 	return <<<EOS
 		
-	function rechercher_objet(id_selecteur, page_selection) {
-
-		// chercher l'input de saisie
-		jQuery(id_selecteur+' input[@name=nom_objet]')
-		.not('[@autoCFG]')
-		.each(function() {
-			var me = this;
-			jQuery(this)
-			.Autocomplete({
-				'source': page_selection,
-				'delay': 200,
-				'autofill': false,
-				'helperClass': "autocompleter",
-				'selectClass': "selectAutocompleter",
-				'minchars': 1,
-				'mustMatch': true,
-				'inputWidth': false,
-				'cacheLength': 20,
-				'multiple' : false,
-				'multipleSeparator' : ";",
-				fx : {type: "fade", duration: 400},
-				'onShow' : function(suggestionBox, suggestionIframe) {
-					jQuery('.autocompleter, .selectAutocompleter').fadeTo(300,1);
-				},
-				'onSelect': 
-				function(li) {
-					if (li.id > 0) {
-						var id = li.id;
-						jQuery(id_selecteur + ' #pid_objet').val(id);
+		function rechercher_objet(id_selecteur, page_selection) {
+			// chercher l'input de saisie
+			var me = jQuery(id_selecteur+' input[@name=nom_objet]');
+			me.autocomplete(page_selection,
+					{
+						delay: 200,
+						autofill: false,
+						minChars: 1,
+						multiple:false,
+						multipleSeparator:";",
+						formatItem: function(data, i, n, value) {
+							return data[0];
+						},
+						formatResult: function(data, i, n, value) {
+							return data[1];
+						},
+					}
+				);
+				me.result(function(event, data, formatted) {
+					if (data[2] > 0) {
+						jQuery(id_selecteur + ' #pid_objet').val(data[2]);
 						jQuery(id_selecteur + ' input[type="submit"]').focus();
 						jQuery(me)
 						.end();
 					}
-				}
-			});
-		});
-	}
+					else{
+						return data[1];
+					}
+				});
+			};
 EOS;
 }
 
@@ -53,12 +46,7 @@ function grappes_inserer_javascript($flux){
 	$js = grappes_inserer_js_recherche_objet();
 	$js = 
 		'<script type="text/javascript" src="'
-		. find_in_path('javascript/iautocompleter.js')
-		. '"></script>'
-		. "\n"
-
-		. '<script type="text/javascript" src="'
-		. find_in_path('javascript/iutil.js')
+		. find_in_path('javascript/jquery.autocomplete.js')
 		. '"></script>'
 		. "\n"
 
