6b8623d7288179d95696a22675c54d2c9e3e3eb1
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-10-08 12:56:33 +0000

    on utilise la fonction fournie par selecteur générique pour être sur de ne pas charger 2 fois les libs js
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@23049 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/spipicious_jquery/spipicious_head.php
+++ _dev_/spipicious_jquery/spipicious_head.php
@@ -1,62 +1,60 @@
 <?php
 
-function spipicious_affichage_final($page){
+function spipicious_insert_head($flux){
+	global $visiteur_session;
+	
+	if(!$visiteur_session['id_auteur'])
+		return;
 
-	if (!strpos($page, 'formulaire_spipicious_ajax'))
-		return $page;
-
-	$autocompleter = find_in_path('javascript/jquery.autocomplete.js');
-	$autocompletecss = find_in_path('jquery.autocomplete.css');
-
+	include_spip('selecteurgenerique_fonctions');
+	$flux .= selecteurgenerique_verifier_js($flux);
+	
 	$selecteur = generer_url_public('selecteurs_tags');
-
-    $incHead = <<<EOS
-		<script type='text/javascript' src='$autocompleter'></script>
-		<link rel="stylesheet" href="$autocompletecss" type="text/css" media="all" />
-		<script  type="text/javascript"><!--
+    
+	$flux .= <<<EOS
+		<script type="text/javascript"><!--
 		
 	(function($) {
 	var appliquer_selecteur_cherche_mot = function() {
 
 		// chercher l'input de saisie
-		var me = jQuery('input[@name=tags][autocomplete!=off]');
+		var spipicious = jQuery('input[@name=tags][autocomplete!=off]');
 		var id_groupe = jQuery("#select_groupe").val();
 		var id_article = jQuery("#spipicious_id").val();
-		me.autocomplete('$selecteur',
-					{
-						extraParams:{id_article:id_article,id_groupe:id_groupe},
-						delay: 200,
-						autofill: false,
-						minChars: 1,
-						multiple:true,
-						multipleSeparator:";",
-						formatItem: function(data, i, n, value) {
-							return data[0];
-						},
-						formatResult: function(data, i, n, value) {
-							return data[1];
-						},
-					}
-				);
-				me.result(function(event, data, formatted) {
-					if (data[2] > 0) {
-						jQuery(me)
-						.end();
-					}
-					else{
-						return data[1];
-					}
-				});
-			};
-			$(function(){
-				appliquer_selecteur_cherche_mot();
-				onAjaxLoad(appliquer_selecteur_cherche_mot);
-			});
-		})(jQuery);
+		spipicious.autocomplete('$selecteur',
+			{
+				extraParams:{id_article:id_article,id_groupe:id_groupe},
+				delay: 200,
+				autofill: false,
+				minChars: 1,
+				multiple:true,
+				multipleSeparator:";",
+				formatItem: function(data, i, n, value) {
+					return data[0];
+				},
+				formatResult: function(data, i, n, value) {
+					return data[1];
+				},
+			}
+		);
+		spipicious.result(function(event, data, formatted) {
+			if (data[2] > 0) {
+				jQuery(me)
+				.end();
+			}
+			else{
+				return data[1];
+			}
+		});
+	};
+		$(function(){
+			appliquer_selecteur_cherche_mot();
+			onAjaxLoad(appliquer_selecteur_cherche_mot);
+		});
+	})(jQuery);
 // --></script>
 EOS;
-
-	return substr_replace($page, $incHead, strpos($page, '</head>'), 0);
+	return $flux;
 }
 
 ?>
