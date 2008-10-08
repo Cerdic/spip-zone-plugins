bb9e327ebf9193c0811ff92384cc51cf5eaee0f1
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-10-08 18:58:02 +0000

    oups... ne pa aller trop vite
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@23063 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/selecteur_generique/selecteurgenerique_fonctions.php
+++ _dev_/selecteur_generique/selecteurgenerique_fonctions.php
@@ -3,7 +3,7 @@
 function selecteurgenerique_verifier_js($flux){
     if(strpos($flux,'jquery.autocomplete.js')===FALSE){
 		$autocompleter = find_in_path('javascript/jquery.autocomplete.js');
-		$autocompletecss = find_in_path('jquery.autocomplete.css');
+		$autocompletecss = find_in_path('iautocompleter.css');
 		$flux .= "<script type='text/javascript' src='$autocompleter'></script>";
 		$flux .= "<link rel='stylesheet' href='$autocompletecss' type='text/css' media='all' />";
 	};
