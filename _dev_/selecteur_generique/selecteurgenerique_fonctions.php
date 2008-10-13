b304a0084b0961cee8cd42deb31771f6e282f941
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-10-13 16:34:27 +0000

    ne pas retourner plusieurs fois le $flux
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@23225 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/selecteur_generique/selecteurgenerique_fonctions.php
+++ _dev_/selecteur_generique/selecteurgenerique_fonctions.php
@@ -1,12 +1,13 @@
 <?php
 
 function selecteurgenerique_verifier_js($flux){
+	$contenu = "";
     if(strpos($flux,'jquery.autocomplete.js')===FALSE){
 		$autocompleter = find_in_path('javascript/jquery.autocomplete.js');
 		$autocompletecss = find_in_path('iautocompleter.css');
-		$flux .= "<script type='text/javascript' src='$autocompleter'></script>";
-		$flux .= "<link rel='stylesheet' href='$autocompletecss' type='text/css' media='all' />";
+		$contenu .= "<script type='text/javascript' src='$autocompleter'></script>";
+		$contenu .= "<link rel='stylesheet' href='$autocompletecss' type='text/css' media='all' />";
 	};
-	return $flux;
+	return $contenu;
 }
 ?>
