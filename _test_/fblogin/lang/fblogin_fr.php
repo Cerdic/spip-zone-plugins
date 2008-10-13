776ce29f28fe82316f3d46ae5f20a6a99b596914
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-10-13 22:01:20 +0000

    message plus court
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@23241 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/fblogin/lang/fblogin_fr.php
+++ _test_/fblogin/lang/fblogin_fr.php
@@ -5,7 +5,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 'je_veux_utiliser_fb_insc' => 'Je veux utiliser mon compte FaceBook pour m\'inscrire',
 'je_veux_utiliser_fb' => 'Je veux &ecirc;tre reconnu quand je viens de Facebook',
 'acceder_a_mon_profil_fb' => 'Mon profil Facebook',
-'auth_fb_ok_connectez_vous' => 'Vous avez &eacute;t&eacute; identifi&eacute; par Facebook, mais votre compte n\'est pas encore associ&eacute; &agrave; votre compte Facebook. Indiquez votre login et votre mot de passe pour finir.',
+'auth_fb_ok_connectez_vous' => 'Vous avez &eacute;t&eacute; identifi&eacute; par Facebook. Indiquez votre login et votre mot de passe pour finir.',
 'ajouter_application' => 'Ajouter l\'application sur Facebook',
 
 );
