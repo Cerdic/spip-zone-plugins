4063a6e17cbc9964f2c25691af5c4e924c643e43
sam@poongaloo.org <sam@poongaloo.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-10-14 13:04:02 +0000

    Premier ajout du plugin permettant d'inserer des activtes jclic dans spip
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@23259 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _test_/eva_jclic19x/lang/evajclic_fr.php
@@ -0,0 +1,46 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+'article_num' => "Article n &#176;",
+
+'clic_editer' => "Cliquez ici pour &eacute;diter",
+'competences' => "<strong>Comp&eacute;tences vis&eacute;es : </strong>",
+'creation_terminee' => "Cr&eacute;ation du mot-cl&eacute; jclic termin&eacute;e<br/><br/>",
+
+'descriptif' => "<strong>Descriptif : </strong>",
+'discipline' => "<strong>Discipline : </strong>",
+
+'gros_titre_page' => "Liste des activit&eacute;s jclic",
+'groupe_non' => "<br/> Le groupe Activites n'existe pas",
+
+'presentation' => "JClic est un ensemble de logiciels de cr&eacute;ation, d'&eacute;valuation et de visionneuses d'activit&eacute;s multim&eacute;dia scolaire.
+   Tous ces modules sont d&eacute;velopp&eacute;s en langage Java. C'est un projet multi-plateforme, sous licence libre et bas&eacute; sur des standards ouverts.
+   Jclic fonctionne sous Linux, Mac OS X, Windows et Solaris.<br/>",
+'probleme_base' => "Il y a un problème dans votre base de donn&eacute;es.<br/>",
+
+'lienoff1' => "<a href='http://clic.xtec.net/en/jclic/index.htm' target='_blank'>http://clic.xtec.net/en/jclic/index.htm</a><br/>",
+'lienoff2' => "<a href='http://jclic.clicapplic.net/' target='_blank' >http://jclic.clicapplic.net/</a><br/>",
+'liste_activites' => "<br/><strong>Liste des activit&eacute;s Jclic propos&eacute;es dans ce site :</strong><br/>",
+
+'mot_deja' => "Le mot-cl&eacute; jclic est d&eacute;j&agrave; cr&eacute;&eacute;.<br/>",
+'mot_pas' => "Le mot-cl&eacute; jclic n'existe pas encore dans votre liste. <br/>",
+
+'nb_act1' => "<br/><strong>Ce site contient ",
+'nb_act2' => " activit&eacute;s jclic.</strong><br/>",
+
+'siteoff1' => "<strong>Site officiel :</strong><br/>",
+'siteoff2' => "<strong>Portail francophone des activit&eacute;s jclic pour l'&eacute;cole : </strong><br/>",
+
+'texte_article' => "<strong>Texte de l'article : </strong>",
+'titre_activite' => "<strong>Titre de l'activit&eacute; : </strong>",
+'titre_page' => "JCLIC",
+'titre_boite_principale' => "Voici la liste de toutes les activit&eacute;s r&eacute;pertori&eacute;es dans ce site :",
+'titre_presentation' => "<strong>Pr&eacute;sentation de Jclic</strong><br/>",
+
+'verification_mot' => "<strong>V&eacute;rification du mot-cl&eacute; :</strong><br/>",
+
+'' => "",
+);
+?>
