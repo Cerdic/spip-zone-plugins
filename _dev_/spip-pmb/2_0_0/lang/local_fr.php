44c8cf82e826580badac5f87af6fcc2361075548
apachot@openstudio.fr <apachot@openstudio.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-10-16 12:54:20 +0000

    Développement d'un portail web pour les biliothèques et centres de documentations qui utilisent pmb
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@23361 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _dev_/spip-pmb/2_0_0/lang/local_fr.php
@@ -0,0 +1,26 @@
+<?php
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+
+'accueil_site' => 'Accueil',
+'conferences' =>'P.U.L.P.',
+'derniers_articles' => 'Actualit&eacute;s',
+'newsletter' => 'Newsletter',
+'toute_actualite' => 'toute l\'actualit&eacute;',
+'lire_la_suite' => 'Lire la suite',
+'nouveautes' => 'Nouveaut&eacute;s',
+'toutes_nouveautes' => 'toutes les nouveaut&eacute;s',
+'auteur' => 'Auteur',
+'tous_coupsdecoeur' => 'tous les coups de c&oelig;urs',
+'serie' => 'S&eacute;rie',
+'type' => 'Type',
+'editeur' => 'Editeur',
+'format' =>'Format',
+'annee_publication' => 'Ann&eacute;e de publication',
+'importance' => 'Importance',
+'presentation' => 'Pr&eacute;sentation',
+'rubriques' => 'Qui sommes-nous ?'
+
+);
+
+?>
