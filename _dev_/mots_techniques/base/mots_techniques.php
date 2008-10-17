5ae22f74295073c41f72f2f71af05e2f01402d15
marcimat@free.fr <marcimat@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-10-17 22:01:37 +0000

    On casse les mots techniques
    Opération 1) Mettre l'install/désinstall en version 2.0 beta
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@23398 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/mots_techniques/base/mots_techniques.php
+++ _dev_/mots_techniques/base/mots_techniques.php
@@ -1,54 +1,19 @@
 <?php
 
-include_spip('base/serial'); // pour eviter une reinit posterieure des tables modifiees
+if (!defined("_ECRIRE_INC_VERSION")) return;
 
-$spip_mots = array(
-		"id_mot"	=> "bigint(21) NOT NULL",
-		"titre"	=> "text DEFAULT '' NOT NULL",
-		"descriptif"	=> "text DEFAULT '' NOT NULL",
-		"texte"	=> "longtext DEFAULT '' NOT NULL",
-		"id_groupe"	=> "bigint(21) NOT NULL",
-		"type"	=> "text DEFAULT '' NOT NULL",
-		"extra"	=> "longtext NULL",
-		"maj"	=> "TIMESTAMP");
+function mots_techniques_declarer_tables_principales($tables_principales){
+	// ajout de la jointure pour {technique=...} sur boucle MOT
+	$tables_principales['spip_mots']['join']["id_groupe"] = "id_groupe";
+	$tables_principales['spip_mots']['join']["id_mot"] = "id_mot";
+	// jout du champ technique
+	$tables_principales['spip_groupes_mots']['field']["technique"] = "text DEFAULT '' NOT NULL";
+	return $tables_principales;
+}
 
-$spip_mots_key = array(
-		"PRIMARY KEY"	=> "id_mot",
-);
-// ajout de la jointure pour {technique=...} sur boucle MOT
-$spip_mots_join = array(
-		"id_mot"=>"id_mot",
-		"id_groupe"=>"id_groupe");
-		
-$spip_groupes_mots = array(
-		"id_groupe"	=> "bigint(21) NOT NULL",
-		"titre"	=> "text DEFAULT '' NOT NULL",
-		"descriptif"	=> "text DEFAULT '' NOT NULL",
-		"texte"	=> "longtext DEFAULT '' NOT NULL",
-		"unseul"	=> "varchar(3) DEFAULT '' NOT NULL",
-		"obligatoire"	=> "varchar(3) DEFAULT '' NOT NULL",
-		"articles"	=> "varchar(3) DEFAULT '' NOT NULL",
-		"breves"	=> "varchar(3) DEFAULT '' NOT NULL",
-		"rubriques"	=> "varchar(3) DEFAULT '' NOT NULL",
-		"syndic"	=> "varchar(3) DEFAULT '' NOT NULL",
-		"minirezo"	=> "varchar(3) DEFAULT '' NOT NULL",
-		"comite"	=> "varchar(3) DEFAULT '' NOT NULL",
-		"forum"	=> "varchar(3) DEFAULT '' NOT NULL",
-		"maj"	=> "TIMESTAMP",
-		"technique"	=> "text DEFAULT '' NOT NULL");
-
-$spip_groupes_mots_key = array(
-		"PRIMARY KEY"	=> "id_groupe");
-
-global $tables_principales;
-$tables_principales['spip_mots']     =
-	array('field' => &$spip_mots, 'key' => &$spip_mots_key ,'join' => &$spip_mots_join);
-$tables_principales['spip_groupes_mots'] =
-	array('field' => &$spip_groupes_mots, 'key' => &$spip_groupes_mots_key );	
-
-// dans public/interfaces
-global $tables_jointures;
-$tables_jointures['spip_mots'][]= 'groupes_mots';
-
+function mots_techniques_declarer_tables_interfaces($interface){
+	$interface['tables_jointures']['spip_mots'][] = 'groupes_mots';		
+	return $interface;
+}
 
 ?>
