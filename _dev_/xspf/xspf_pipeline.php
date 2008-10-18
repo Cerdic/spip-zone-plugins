7bcc59ebc1c7adc6abb3aeea1ba610d7c8044543
marcimat@free.fr <marcimat@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-10-18 21:09:39 +0000

    Prise en compte des nouveaux chemins pour les librairies à télécharger (nouvelles libs non testés) Suppression de Farbastic inclus dans le plugin : utiliser le plugin "Palette"
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@23433 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/xspf/xspf_pipeline.php
+++ _dev_/xspf/xspf_pipeline.php
@@ -9,20 +9,15 @@ function xspf_header_prive($texte) {
 	$pos = strpos($cfg, 'xspf');
 	if ($pos === 0){
 		$tooltipcss = find_in_path('javascript/jquery.tooltip.css');
-		$farbcss = url_absolue_css(direction_css(find_in_path('css/farbtastic.css')));
-		
 		$bgiframejs = find_in_path('javascript/jquery.bgiframe.js');
 		$dimensionjs = find_in_path('javascript/jquery.dimensions.pack.js');
 		$tooltipjs = find_in_path('javascript/jquery.tooltip.pack.js');
-		$farbjs = find_in_path('javascript/farbtastic.js');
 				
 		$texte.= "
 			<link rel='stylesheet' type='text/css' href='$tooltipcss' />\n
-			<link rel='stylesheet' type='text/css' href='$farbcss' />\n
 			<script type='text/javascript' src='$bgiframejs'></script>\n
 			<script type='text/javascript' src='$dimensionjs'></script>\n
-			<script type='text/javascript' src='$tooltipjs'></script>\n
-			<script type='text/javascript' src='$farbjs'></script>" . "\n";
+			<script type='text/javascript' src='$tooltipjs'></script>\n";
 
 		$texte.= "
 			<script type='text/javascript'>
@@ -34,30 +29,6 @@ function xspf_header_prive($texte) {
 						showBody: ' - '
 					});
 				});
-	
-			$(document).ready(function() {
-				$('.picker').css('opacity', 0.25);
-				var selected;
-				$('.colorwell')
-					.each(function () { 
-						var pick = $(this).parent().children('.picker');
-						$.farbtastic(pick).linkTo(this);$(this).css('opacity', 0.75);})
-				
-					.focus(function() {
-						if (selected) {
-							$(selected).css('opacity', 0.75).removeClass('colorwell-selected');
-						}
-						var pick = $(this).parent().children('.picker');
-						$.farbtastic(pick).linkTo(this);
-						pick.css('opacity', 1);
-						$(selected = this).css('opacity', 1).addClass('colorwell-selected');
-					})
-					.blur(function() {
-						var pick = $(this).parent().children('.picker');
-						pick.css('opacity', 0.25);
-						$(selected = this).css('opacity', 0.75).removeClass('colorwell-selected');
-					});
-			});
 			</script>\n";
 	}
 	return $texte;
