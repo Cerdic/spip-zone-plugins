43bc8a54c7b80e3994ec1a8f775b756d313d672a
dominique.lepaisant@ac-caen.fr <dominique.lepaisant@ac-caen.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-10-18 18:12:11 +0000

    Menu rosace - depot
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@23416 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _dev_/lang/rosace_fr.php
@@ -0,0 +1,70 @@
+<?php
+######################################################################
+# Menu Rosace                                   
+# Auteur: Dominique (Dom) Lepaisant - Frederic TARAUD - OCTOBRE 2008                 
+# Ce programme est un logiciel libre distribue sous licence GNU/GPL. 
+# Pour plus de details voir le fichier COPYING.txt                   
+######################################################################
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+//1
+''=>'',
+//2
+
+//3
+''=>'',
+
+//A
+''=>'',
+//B
+''=>'',
+
+//C
+''=>'',
+
+//D
+''=>'',
+//E
+''=>'',
+
+//F
+''=>'',
+
+//G
+''=>'',
+
+//H
+''=>'',
+
+//I
+''=> '',
+
+//J
+
+//L
+
+//M
+''=> '',
+
+//N
+
+//O
+
+//P
+''=> '',
+
+//R
+'titre_plugin_rosace'=>'Menu Rosace',
+//S
+''=> '',
+//T
+''=> '',
+
+//V
+''=> '',
+
+//Z
+'z' =>'z'
+);
+
+?>
