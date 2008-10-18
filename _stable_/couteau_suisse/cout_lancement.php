1439336d3bb0e34ebe477597082dcaa4c2d46bdf
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-10-18 20:20:55 +0000

    Suite à discussion sur la zone (http://thread.gmane.org/gmane.comp.web.spip.zone/10346), passage de l'écriture dans mes_options.php en define avec un drapeau_rouge.
    Par défaut, cette écriture est désactivée et ne peut être activée que par un define idoine dans mes_options.php.
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@23428 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/couteau_suisse/cout_lancement.php
+++ _stable_/couteau_suisse/cout_lancement.php
@@ -98,7 +98,10 @@ cs_log("$rand -- cs_initialise_includes()... cout_fonctions.php sera probablemen
 	// creer les includes (config/mes_options, mes_options et mes_fonctions) et le fichier de controle pipelines.php
 	cs_initialise_includes(count($metas_outils));
 	// verifier le fichier d'options _FILE_OPTIONS (ecrire/mes_options.php ou config/mes_options.php)
-	cs_verif_FILE_OPTIONS(true);
+	// De'sactive' par de'faut, cf : http://permalink.gmane.org/gmane.comp.web.spip.zone/10346
+	if (_CS_ECRIRE_MES_OPTIONS=='drapeau_rouge') {
+		cs_verif_FILE_OPTIONS(true);
+	}
 	// sauver la configuration
 	cs_sauve_configuration();
 	// en metas : outils actifs
