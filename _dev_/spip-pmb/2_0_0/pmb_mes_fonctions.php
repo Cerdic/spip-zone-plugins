386afebeeaafff0340d436b212295dc3685b1594
apachot@openstudio.fr <apachot@openstudio.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-10-20 20:41:17 +0000

    Parsing des pages PMB avec PHP Simple HTML DOM Parser http://simplehtmldom.sourceforge.net/
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@23522 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/spip-pmb/2_0_0/pmb_mes_fonctions.php
+++ _dev_/spip-pmb/2_0_0/pmb_mes_fonctions.php
@@ -1,3 +1,27 @@
 <?php
 include_spip('base/pmb_tables');
+
+function extraire_element_distant ($url, $selector) {
+			$resultat_recherche_locale = copie_locale($url,'auto');
+			if($resultat_recherche_locale != false) {
+					$resultat_recherche_html = unicode2charset(charset2unicode(file_get_contents($resultat_recherche_locale), 'iso-8859-1'),'utf-8');
+					
+					$resultat_recherche_html = str_replace("addtags.php", "#PMB_URL_BASEaddtags.php", $resultat_recherche_html);
+					$resultat_recherche_html = str_replace("avis.php", "#PMB_URL_BASEavis.php", $resultat_recherche_html);
+					$resultat_recherche_html = str_replace("./do_resa.php", "#PMB_URL_BASEdo_resa.php", $resultat_recherche_html);
+					
+					$resultat_recherche_html = str_replace("index.php?lvl=", "index.php?page=", $resultat_recherche_html);
+					
+					require(find_in_path('simple_html_dom.php'));
+					$htmldom = str_get_html($resultat_recherche_html);
+					
+					foreach($htmldom->find($selector) as $e) {
+  		 			 	return $e->innertext;
+					}
+					
+					
+			}
+		}
+
+
 ?>
