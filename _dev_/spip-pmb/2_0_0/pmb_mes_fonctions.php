3f65e0e054f8993feca364bdf5fed5d9b2bf0a7c
apachot@openstudio.fr <apachot@openstudio.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-10-20 21:00:31 +0000

    ajout de la page collection (coll_see)
    et bug sur les liens "réserver l'ouvrage"
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@23524 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/spip-pmb/2_0_0/pmb_mes_fonctions.php
+++ _dev_/spip-pmb/2_0_0/pmb_mes_fonctions.php
@@ -1,14 +1,14 @@
 <?php
 include_spip('base/pmb_tables');
 
-function extraire_element_distant ($url, $selector) {
-			$resultat_recherche_locale = copie_locale($url,'auto');
+function extraire_element_distant ($url_base, $file, $selector) {
+			$resultat_recherche_locale = copie_locale($url_base.$file,'auto');
 			if($resultat_recherche_locale != false) {
 					$resultat_recherche_html = unicode2charset(charset2unicode(file_get_contents($resultat_recherche_locale), 'iso-8859-1'),'utf-8');
 					
-					$resultat_recherche_html = str_replace("addtags.php", "#PMB_URL_BASEaddtags.php", $resultat_recherche_html);
-					$resultat_recherche_html = str_replace("avis.php", "#PMB_URL_BASEavis.php", $resultat_recherche_html);
-					$resultat_recherche_html = str_replace("./do_resa.php", "#PMB_URL_BASEdo_resa.php", $resultat_recherche_html);
+					$resultat_recherche_html = str_replace("addtags.php", $url_base."addtags.php", $resultat_recherche_html);
+					$resultat_recherche_html = str_replace("avis.php", $url_base."avis.php", $resultat_recherche_html);
+					$resultat_recherche_html = str_replace("./do_resa.php", $url_base."do_resa.php", $resultat_recherche_html);
 					
 					$resultat_recherche_html = str_replace("index.php?lvl=", "index.php?page=", $resultat_recherche_html);
 					
