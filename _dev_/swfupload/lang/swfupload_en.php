17b63fa4616e89301a78027ff7558ce686e5fbaa
erational@erational.org <erational@erational.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-10-20 06:39:41 +0000

    slashes
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@23486 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/swfupload/lang/swfupload_en.php
+++ _dev_/swfupload/lang/swfupload_en.php
@@ -4,7 +4,7 @@
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 'file_size_limit' => '<strong>File size limit</strong><br/>The file_size_limit parameter will accept a unit. Valid units are B, KB, MB, and GB. The default unit is KB.Examples: 2147483648 B, 2097152, 2097152KB, 2048 MB, 2 GB',
-'file_types' => '<strong>File types</strong> setting accepts a semi-colon separated list of file extensions that are allowed to be selected by the user. Use '*.*' to allow all file types',
+'file_types' => '<strong>File types</strong> setting accepts a semi-colon separated list of file extensions that are allowed to be selected by the user. Use \'*.*\' to allow all file types',
 'file_upload_limit' => '<strong>File upload limit</strong><br/>Dynamically modifies the file_upload_limit setting. The special value zero (0) indicates "no limit".',
 'debug' => 'Enables or disables debug output',
 'texte_uploadqueue' => 'Upload queue',
