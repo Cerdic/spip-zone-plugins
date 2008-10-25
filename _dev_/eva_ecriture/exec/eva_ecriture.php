1c94add82679dd969f04d2f90d20fed53ecaa22c
sam@poongaloo.org <sam@poongaloo.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-10-25 14:02:00 +0000

    Permettre l'ajout de polices dans le répertoire polices. A terminer
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@23623 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/eva_ecriture/exec/eva_ecriture.php
+++ _dev_/eva_ecriture/exec/eva_ecriture.php
@@ -34,14 +34,23 @@ echo debut_boite_info(true);
 	echo '<br/><br/>';
   	echo _T('evaecriture:siteoff1');
 	echo _T('evaecriture:lienoff1');
+	
+    echo debut_cadre_trait_couleur(_DIR_PLUGIN_EVA_ECRITURE."img_pack/eva.gif", true, '', '');	
+	echo bouton_block_depliable('Ajouter des polices',false,'');
+   echo debut_block_depliable(false);
+   echo '<br />&nbsp;<br />';
+   echo '<form action="'.generer_url_ecrire("eva_ecriture").'" method="post" enctype="multipart/form-data">';
+   echo '<input type="file" name="image_eva_habillage_envoi" />';
+   echo '<br />&nbsp;<br /><input type="submit" value="Envoyer" /></form></center>';
+	echo fin_block();
+	echo fin_cadre_trait_couleur(true);
 echo fin_boite_info(true);
 
-echo debut_droite('', true);
-
 ################
-echo debut_cadre_enfonce('', false, '', _T('evaecriture:eva_ecriture'));
+echo debut_droite('', true);
+echo debut_cadre_enfonce($icone, false, '', _T('evaecriture:eva_ecriture'));
 	echo _T('evaecriture:presentation');
-	echo "<ecriture1|texte=Ma première phrase.|police=Boite_pleine.ttf|left>";
+	//echo "<ecriture1|texte=Ma première phrase.|police=Boite_pleine.ttf|left>";
         
 	echo debut_cadre_trait_couleur('',true);
 		//echo bouton_block_invisible($bloc);
@@ -52,11 +61,17 @@ $rep = _DIR_PLUGIN_EVAECRITURE."/polices/";
 echo "répertoire d'installation des polices :".$rep;
 $dir = opendir($rep);
 
+	//if (($nom_fichier!='.') AND ($nom_fichier!='..') AND ((strpos($nom_fichier,'.PNG')))) {
+   echo '<br />';
+   
+//}
+
 while ($f = readdir($dir)) {
-   if(is_file($rep.$f)) {
+   if(is_file($rep.$f) AND ($f!='.') AND ($f!='..') AND (strpos($f,'.ttf'))) {
       echo "<li>Nom de la police: <strong>".$f."</strong></li>";
       echo "<li>Taille du fichier: ".filesize($rep.$f)." octets</li>";
       echo "<li>Exemple : bientot ici</li>";
+      echo "<li>[(#TEXTE|police=".$f.")]</li>";
       echo "<br><br>";
    }
    }
