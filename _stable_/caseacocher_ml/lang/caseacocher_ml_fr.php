34db6f47937707bc0865798768cbcf7e646c02f2
yaquoi@yaquoi.com <yaquoi@yaquoi.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-10-26 18:08:50 +0000

    case a cocher newsletter
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@23673 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _stable_/caseacocher_ml/lang/caseacocher_ml_fr.php
@@ -0,0 +1,11 @@
+<?php
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	'inscription_mailing_list' => 'S\'inscrire &agrave; la newsletter',
+	'mail_inscription_mailinglist' => 'Adresse email d\'inscription &agrave; la liste',
+	'sujet_inscription_mailinglist' => 'Sujet du mail pour l\'inscription &agrave; la liste',
+	'sujet_inscription_mailinglist_explication' => 'sujet de l\'email envoy&eacute; &agrave; la liste et correspondant &agrave; la requ&ecirc;te de la demande d\'inscription.',
+	'mail_inscription_mailinglist_explication' => 'adresse email &agrave; laquelle la demande d\'inscription &agrave; la liste doit &ecirc;tre envoy&eacute;e',
+);
+
+?>
