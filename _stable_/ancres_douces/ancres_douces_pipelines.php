dc88a4aba90e56c0eb071b0dd6a434679e5a3438
fil@rezo.net <fil@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-10-27 17:49:36 +0000

    retour a la version 1.0
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@23708 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/ancres_douces/ancres_douces_pipelines.php
+++ _stable_/ancres_douces/ancres_douces_pipelines.php
@@ -2,13 +2,13 @@
 function ancres_douces_insert_head($flux){
 
 // Inclusion des scripts jquery
-$flux .= '<script src="'.url_absolue(find_in_path("js/jquery.ancresdouces.js")).'" type="text/javascript"></script>';
+$flux .= '<script src="'.url_absolue(find_in_path("js/jquery.scrollto.js")).'" type="text/javascript"></script>';
+$flux .= '<script src="'.url_absolue(find_in_path("js/jquery.localscroll.js")).'" type="text/javascript"></script>';
 
 // Code d'init
 $flux .= '<script type="text/javascript"><!--
 if(window.jQuery)jQuery(document).ready(function(){
-if(typeof jQuery.fn.ancresdouces=="function") jQuery("a[href*=#]").ancresdouces();
-});
+if(typeof jQuery.localScroll=="function")jQuery.localScroll({hash:true})});
 // --></script>';
 
 return $flux;
