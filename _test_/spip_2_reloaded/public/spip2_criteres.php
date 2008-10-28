2f413406efef878d78bcfe528089823f83236b61
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-10-28 10:41:04 +0000

    "un critere compteur et sa balise associee d'apres http://www.spip-contrib.net/Classer-les-articles-par-nombre-de#forum409210 "
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@23725 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/spip_2_reloaded/public/spip2_criteres.php
+++ _test_/spip_2_reloaded/public/spip2_criteres.php
@@ -25,4 +25,65 @@ function critere_CONDITION_si_dist($idb, &$boucles, $crit) {
 	}
 }
 
+/**
+ * http://www.spip-contrib.net/Classer-les-articles-par-nombre-de#forum409210
+ * Permet de faire un comptage par table liee
+ * exemple
+ * <BOUCLE1(AUTEURS){compteur articles}{par compteur_articles}>
+ * #ID_AUTEUR : #COMPTEUR{articles}
+ * </BOUCLE1>
+ * pour avoir les auteurs classes par articles et le nombre d'article de chacun
+ *
+ * @param unknown_type $idb
+ * @param unknown_type $boucles
+ * @param unknown_type $crit
+ */
+function critere_compteur($idb, &$boucles, $crit){
+	$boucle = &$boucles[$idb];
+	
+	$params = $crit->param;
+	$table = array_shift($params);
+	$table = $table[0]->texte;
+	if(preg_match(',^(\w+)([<>=])([0-9]+)$,',$table,$r)){
+		$table=$r[1];
+		$op=$r[2];
+		$op_val=$r[3];
+	}
+	$type = objet_type($table);
+	$type_id = id_table_objet($type);
+	$table_sql = table_objet_sql($type);
+	
+	
+	$trouver_table = charger_fonction('trouver_table','base');
+	$arrivee = array($table, $trouver_table($table, $boucle->sql_serveur));
+	$depart = array($boucle->id_table,$trouver_table($boucle->id_table, $boucle->sql_serveur));
+
+	if ($compt = calculer_jointure($boucle,$depart,$arrivee)){
+		// en cas de jointure, on ne veut pas du group_by sur la cle primaire !
+		// cela casse le compteur !
+		foreach($boucle->group as $k=>$group)
+			if ($group == $boucle->id_table.'.'.$boucle->primary)
+				unset($boucle->group[$k]);
+
+		$boucle->select[]= "COUNT($compt.$type_id) AS compteur_$table";	
+		if ($op)
+			$boucle->having[]= array("'".$op."'", "'compteur_".$table."'",$op_val);
+	}
+}
+
+/**
+ * Balise #COMPTEUR associee au critere compteur
+ *
+ * @param unknown_type $p
+ * @return unknown
+ */
+function balise_COMPTEUR_dist($p) {
+	$p->code = '';
+	if (isset($p->param[0][1][0])
+	AND $champ = ($p->param[0][1][0]->texte))
+		return rindex_pile($p, "compteur_$champ", 'compteur');
+  return $p;
+}
+
+
 ?>
