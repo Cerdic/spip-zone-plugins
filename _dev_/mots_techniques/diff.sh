cddd6fafc5d21b446409c7c552f9bf680010cee0
marcimat@free.fr <marcimat@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-10-28 09:22:08 +0000

    Suivre la svn http://trac.rezo.net/trac/spip/changeset/13050
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@23721 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/mots_techniques/diff.sh
+++
@@ -1,9 +0,0 @@
-#dir=../../..
-dir=/home/marcimat/www/spip
-
-# -w n'affiche pas les differrences d'espacement ou de ligne vide
-diff -w exec/mots_edit.php $dir/ecrire/exec/mots_edit.php > diff/exec_mots_edit.diff
-diff -w exec/mots_tous.php $dir/ecrire/exec/mots_tous.php > diff/exec_mots_tous.diff
-diff -w exec/mots_type.php $dir/ecrire/exec/mots_type.php > diff/exec_mots_type.diff
-diff -w inc/editer_mot.php $dir/ecrire/inc/editer_mot.php > diff/inc_editer_mot.diff
-diff -w action/instituer_groupe_mots.php $dir/ecrire/action/instituer_groupe_mots.php > diff/action_instituer_groupe_mots.diff
