b042129018fbfde9d1f5994264efdf1db8e9787e
jsb92@laposte.net <jsb92@laposte.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-10-28 10:34:09 +0000

    plugin spip pour piwik, basé sur le même principe que google analytics www.piwik.org
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@23724 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _test_/spip_piwik/head.php
@@ -0,0 +1,27 @@
+<?php
+
+
+function piwik_insert_head($flux){
+$id_piwik=lire_config('piwik/idPiwik');
+$url_piwik=lire_config('piwik/UrlPiwik');
+
+
+	$flux='
+	<script type="text/javascript">
+	var pkBaseURL = (("https:" == document.location.protocol) ? "'.$url_piwik.'" : "'.$url_piwik.'");
+document.write(unescape("%3Cscript src=\'" + pkBaseURL + "piwik.js\' type=\'text/javascript\'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+<!--
+piwik_action_name = \'\';
+piwik_idsite = '.$id_piwik.';
+piwik_url = pkBaseURL + "piwik.php";
+piwik_log(piwik_action_name, piwik_idsite, piwik_url);
+//-->
+</script>';
+
+return $flux;
+
+}
+
+?>
---
+++ _test_/spip_piwik/lang/piwik_fr.php
@@ -0,0 +1,12 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+'textes_id_piwik' => 'Votre identifiant piwik',
+'textes_url_piwik' => 'Votre serveur piwik',
+'texte_votre_identifiant' => 'Votre identifiant',
+'retour_a_spip' => 'Retour &agrave; la page compl&egrave;te',
+'adresse_du_serveur_web' => 'url de serveur',
+);
+?>
