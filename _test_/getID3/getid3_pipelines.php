d573bcf104a94efd1cb9214f478d4d302f2a835f
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-11-01 18:31:21 +0000

    ca a l'air de mieux marcher
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@23895 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/getID3/getid3_pipelines.php
+++ _test_/getID3/getid3_pipelines.php
@@ -1,20 +1,16 @@
 <?php
 
 include_spip("inc/getid3");
-
-$son = array(
-	0=> "mp3",
-	1=> "ogg",
-	2=> "flac");
 				
 function getid3_editer_contenu_objet($flux){
 	$id_document = $flux['args']['id'];
 	if($flux['args']['type']=='case_document'){
+		$son = array("mp3","ogg","flac");
 		$document = sql_fetsel("docs.id_document, docs.extension, L.vu,L.objet,L.id_objet", "spip_documents AS docs INNER JOIN spip_documents_liens AS L ON L.id_document=docs.id_document","L.id_document=".sql_quote($id_document));
 		$extension = $document['extension'];
 		$type = $document['objet'];
 		$id = $document['id_objet'];
-		if(in_array($son,$extension)){
+		if(in_array($extension,$son)){
 			$infos_son = charger_fonction('infos_son', 'inc');
 			$flux['data'] .= $infos_son($id,$id_document,$type);
 		}
@@ -25,16 +21,17 @@ function getid3_editer_contenu_objet($flux){
 function getid3_post_edition($flux){
 	$id_document = $flux['args']['id_objet'];
 	if($flux['args']['operation'] == 'ajouter_document'){
-			$document = sql_fetsel("docs.id_document, docs.extension,docs.fichier,docs.mode,docs.distant, L.vu, L.objet, L.id_objet", "spip_documents AS docs INNER JOIN spip_documents_liens AS L ON L.id_document=docs.id_document","L.id_document=".sql_quote($id_document));
-			$extension = $document['extension'];
-			if(in_array($son,$extension)){
-				$recuperer_infos = charger_fonction('getid3_recuperer_infos','inc');
-				$infos = $recuperer_infos($id_document);
-			}
-			else{
-				return;
-			}
+		$son = array("mp3","ogg","flac");
+		$document = sql_fetsel("docs.id_document, docs.extension,docs.fichier,docs.mode,docs.distant, L.vu, L.objet, L.id_objet", "spip_documents AS docs INNER JOIN spip_documents_liens AS L ON L.id_document=docs.id_document","L.id_document=".sql_quote($id_document));
+		$extension = $document['extension'];
+		if(in_any($extension,$son)){
+			$recuperer_infos = charger_fonction('getid3_recuperer_infos','inc');
+			$infos = $recuperer_infos($id_document);
 		}
+		else{
+			return;
+		}
+	}
 	else{
 		return;
 	}
