3ef1646b9852d8ab41ec0c9e547ca4eb0c286bd7
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-11-01 18:14:39 +0000

    "une icone moins conforme aux cgu mais plus discrete"
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@23890 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/fblogin/lang/fblogin_fr.php
+++ _test_/fblogin/lang/fblogin_fr.php
@@ -3,6 +3,7 @@
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 'je_veux_utiliser_fb_insc' => 'Je veux utiliser mon compte FaceBook pour m\'inscrire',
+'je_veux_utiliser_fb_login' => 'Utiliser mon compte FaceBook pour m\'identifier',
 'je_veux_utiliser_fb' => 'Je veux &ecirc;tre reconnu quand je viens de Facebook',
 'acceder_a_mon_profil_fb' => 'Mon profil Facebook',
 'auth_fb_ok_connectez_vous' => 'Vous avez &eacute;t&eacute; identifi&eacute; par Facebook. Indiquez votre login et votre mot de passe pour finir.',
