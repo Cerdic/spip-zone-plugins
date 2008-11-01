f974dcad7b5ee5744c707117e8f4f4a9cffead1a
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-11-01 01:52:15 +0000

    encore plus simple...
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@23861 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/getID3/getid3_fonctions.php
+++ _test_/getID3/getid3_fonctions.php
@@ -1,17 +1,15 @@
 <?php
 
-function recuperer_id3_doc($id_document,$info,$mime){
-include_spip('inc/recuperer_id3');
+function recuperer_id3_doc($id_document,$info = "", $mime = ""){
 include_spip('inc/documents');
 
-	$result = sql_select("fichier","spip_documents","id_document = " .sql_quote($id_document));
-
-	if(sql_num_rows($result)>0){
-		$document=sql_fetch($result);
-		$fichier = get_spip_doc($document['fichier']);
-		$fichier = ereg_replace(" ","%20",$fichier);
-		return recuperer_id3($fichier,$info,$mime);
-	}
+	$fichier = sql_getfetsel("fichier","spip_documents","id_document = " .sql_quote($id_document));
+	$recuperer_id3 = charger_fonction('recuperer_id3','inc');
+	$fichier = get_spip_doc($fichier);
+	$fichier = ereg_replace(" ","%20",$fichier);
+	$id3_content = $recuperer_id3($fichier,$info,$mime);
+	
+	return $id3_content;
 }
 
 ?>
