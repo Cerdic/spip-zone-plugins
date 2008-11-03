54ca795a95573972e24d2c9b788e2a549e274743
crovvfoot@gmail.com <crovvfoot@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-11-03 00:48:57 +0000

    on continue le remodling
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@23925 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/echoppe/action/echoppe_sauver_categorie.php
+++ _dev_/echoppe/action/echoppe_sauver_categorie.php
@@ -5,31 +5,31 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 function action_echoppe_sauver_categorie(){
 	include_spip('inc/echoppe');
 	
-	$lang_categorie = _request('lang_categorie');
-	$titre = _request('titre');
-	$descriptif = _request('descriptif');
-	$texte = _request('texte');
-	$logo = _request('logo');
-	$id_categorie = _request('id_categorie');
-	$id_parent = _request('id_parent');
+	$categorie = array();
+	$categorie['titre'] = _request('titre_categorie');
+	$categorie['descriptif'] = _request('descriptif_categorie');
+	$categorie['texte'] = _request('texte');
+	$categorie['id_categorie'] = _request('id_categorie');
+	$categorie['id_parent'] = _request('id_parent');
+	
 	$new = _request('new');
 	$id_secteur = recuperer_id_secteur($id_parent, $id_categorie, 'categorie');
-	$categorie = _request('categorie');
+	$lang_categorie = _request('lang_categorie');
+	$logo = _request('logo');
 	
 	switch ($new){
 		
 		case 'oui':
 			$valeur = array('id_categorie'=>'','id_parent'=>$id_parent,'id_secteur'=>$id_secteur);
 			$new_id_categorie = sql_insertq('spip_echoppe_categories',$categorie);
+			sql_updateq('spip_echoppe_categories',array('id_secteur' => recuperer_id_secteur($categorie['id_parent'],'categorie')), 'id_categorie = '.$new_id_categorie);
 			$id_categorie = $new_id_categorie;
 			break;
 		
 		
 		default :
-			/*$sql_update_categorie_descriptif = "UPDATE spip_echoppe_categories_descriptions SET titre = '".addslashes($titre)."', descriptif = '".addslashes($descriptif)."', texte = '".addslashes($texte)."', statut = '".$statut."' WHERE id_categorie = '".$id_categorie."' AND lang = '".$lang_categorie."' ";
-			//echo $sql_update_categorie_descriptif;
-			$res_update_categorie_descriptif = spip_query($sql_update_categorie_descriptif);*/
 			$sav_update = sql_updateq('spip_echoppe_categories',$categorie,'id_categorie = '.$categorie['id_categorie']);
+			sql_updateq('spip_echoppe_categories',array('id_secteur' => recuperer_id_secteur($categorie['id_parent'],'categorie')), 'id_categorie = '.$categorie['id_categorie']);
 			$id_categorie = $categorie['id_categorie'];
 			break;
 		
--- _dev_/echoppe/inc/echoppe.php
+++ _dev_/echoppe/inc/echoppe.php
@@ -8,13 +8,14 @@ function echoppe_echec_autorisation(){
 	echo fin_boite_alerte();
 }
 
-function recuperer_id_secteur($id, $id_objet, $objet){
+function recuperer_id_secteur($id, $objet){
 	switch ($objet){
 		case 'categorie':
 			if($id != 0){
-				$sql_recup_id_secteur = ("SELECT id_secteur FROM spip_echoppe_categories WHERE id_categorie = '".$id."';");
-				$res_recup_id_secteur = spip_query($sql_recup_id_secteur);
-				$id_secteur = spip_fetch_array($res_recup_id_secteur);
+				//$sql_recup_id_secteur = ("SELECT id_secteur FROM spip_echoppe_categories WHERE id_categorie = '".$id."';");
+				$res_recup_id_secteur = sql_select(array("id_secteur"),array("spip_echoppe_".$objet."s"), array("id_".$objet." = ".$id));
+				//$res_recup_id_secteur = spip_query($sql_recup_id_secteur);
+				$id_secteur = sql_fetch($res_recup_id_secteur);
 				return $id_secteur['id_secteur'];				
 			}else{
 				return $id;
