b2b735cc81502777b0d07be3dd6f18a9e6389621
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-11-03 16:45:17 +0000

    ca ne sert à rien d'avoir deux versions qui se balladent...
    Dont une qui marche mal en plus...
    
    maintenant tout est dans _plugins_/_dev_/
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@23946 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/gis/js/gis.js
+++
@@ -1,9 +0,0 @@
-function coordenadas (articulo){
-	$.ajax({
-		type: "POST",
-		url: "'.generer_url_public('cambiar_coordenadas').'",
-		data: "id_article="+articulo+"&lat="+document.forms.formulaire_coordenadas.lat.value+"&lonx="+document.forms.formulaire_coordenadas.lonx.value,
-		success: function() {
-		}
-	});
-}
