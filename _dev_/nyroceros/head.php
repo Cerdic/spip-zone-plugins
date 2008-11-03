c91abb37fca06f7a0cfeee85b810ea678f528c36
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-11-03 20:10:57 +0000

    Correction d'une erreur dans la chaine $flux qui empechait de désactiver le traitement automatique de toutes les images et le preload
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@23949 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/nyroceros/head.php
+++ _dev_/nyroceros/head.php
@@ -14,14 +14,14 @@ function Nyro_insert_head($flux){
 	), $config);
 
 	$flux .='
-<script src=\''.url_absolue(find_in_path('js/jquery.nyroModal-1.2.8.js')).'\' type=\'text/javascript\'></script>
-<script src=\''.url_absolue(find_in_path('js/nyromodal.js')).'\' type=\'text/javascript\'></script>
+<script src="'.url_absolue(find_in_path('js/jquery.nyroModal-1.2.8.js')).'" type="text/javascript"></script>
+<script src="'.url_absolue(find_in_path('js/nyromodal.js')).'" type="text/javascript"></script>
 <script type="text/javascript">/* <![CDATA[ */
-var nyro_traiter_toutes_images="'.($config['traiter_toutes_images'] == 'oui'?'true':'false').'";
+var nyro_traiter_toutes_images='.($config['traiter_toutes_images'] == 'oui'?'true':'false').';
 var nyro_bgcolor="'.$config['bgcolor'].'";
 var nyro_selecteur_galerie="'.$config['selecteur_galerie'].'";
 var nyro_selecteur_commun="'.$config['selecteur_commun'].'";
-var nyro_preload="'.($config['preload'] == 'non'?'false':'true').'";
+var nyro_preload='.($config['preload'] == 'non'?'false':'true').';
 //onAjaxLoad is defined in private area only
 if (window.jQuery)
 (function($){if(typeof onAjaxLoad == "function") onAjaxLoad(nyro_init);
