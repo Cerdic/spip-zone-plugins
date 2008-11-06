783bba746788aa354d3cddefdbd9c406e1c5a207
crovvfoot@gmail.com <crovvfoot@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-11-06 17:55:40 +0000

    Refonte de l'edition des produits suite.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@24058 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/echoppe/exec/echoppe_produit.php
+++ _dev_/echoppe/exec/echoppe_produit.php
@@ -12,26 +12,7 @@ function exec_echoppe_produit(){
 	$contexte = array();
 	$contexte['id_produit'] = _request('id_produit');
 	$contexte['lang_produit'] = _request('lang_produit');
-	/*
-	$sql_lien_produit_categorie = "SELECT id_categorie FROM spip_echoppe_categories_produits WHERE id_produit = '".$contexte['id_produit']."';";
-	//echo $sql_lien_produit_categorie;
-	$res_lien_produit_categorie = spip_query($sql_lien_produit_categorie);
-	$lien_produit_categorie = spip_fetch_array($res_lien_produit_categorie);
-	$contexte['id_categorie'] = $lien_produit_categorie['id_categorie'];
-
-	$sql_le_produit = "SELECT * FROM spip_echoppe_produits WHERE id_produit = '".$contexte['id_produit']."';";
-	$res_le_produit = spip_query($sql_le_produit);
-	$le_produit = spip_fetch_array($res_le_produit);
-	(is_array($le_produit))?$contexte = array_merge($contexte, $le_produit):$contexte = $contexte;
-	
-	$sql_description_produit = "SELECT * FROM spip_echoppe_produits_descriptions WHERE id_produit = '".$contexte['id_produit']."' AND lang = '".$contexte['lang_produit']."';";
-	$res_description_produit = spip_query($sql_description_produit);
-	$description_produit = spip_fetch_array($res_description_produit);
-	(is_array($description_produit))?$contexte = array_merge($contexte,$description_produit):$contexte = $contexte;
-	
-	$contexte['action'] = 'echoppe_sauve_general_produit';
-	
-	*/
+	$contexte['onglet'] = _request('onglet');
 	
 	$res_leproduit = sql_select('*','spip_echoppe_produits','id_produit = '.$contexte['id_produit']);
 	$contexte = array_merge($contexte,sql_fetch($res_leproduit));
@@ -43,9 +24,6 @@ function exec_echoppe_produit(){
 
 	echo inc_commencer_page_dist($contexte['titre'], "redacteurs", "echoppe");
 	
-	/*echo debut_grand_cadre();
-	echo recuperer_fond('fonds/echoppe_chemin_categorie',$contexte);
-	echo fin_grand_cadre();*/
 	
 	debut_grand_cadre(true);
 		echo recuperer_fond('fonds/echoppe_chemin_categorie',$contexte);
@@ -66,10 +44,31 @@ function exec_echoppe_produit(){
 	echo creer_colonne_droite(true);
 	
 	echo debut_droite(false,_T('echoppe:visualisation_d_un_produit'),'');
-	//echo gros_titre($contexte['titre']);
+	echo recuperer_fond('fonds/echoppe_onglets_produit',$contexte);
+	switch ($contexte['onglet']){
+		case 'options' :
+			echo recuperer_fond('fonds/echoppe_produit_options', $contexte);
+		break;
+		
+		case 'stocks' :
+			echo recuperer_fond('fonds/echoppe_produit_stocks', $contexte);
+		break;
+		
+		case 'statistiques' :
+			echo recuperer_fond('fonds/echoppe_produit_statistiques', $contexte);
+		break;
+		
+		case 'infos' :
+			echo recuperer_fond('fonds/echoppe_produit_infos', $contexte);
+		break;
+		
+		default :
+			echo recuperer_fond('fonds/echoppe_produit', $contexte);
+		break;
+	}
 	
-	echo recuperer_fond('fonds/echoppe_produit', $contexte);
-	echo recuperer_fond('fonds/echoppe_options_produit', $contexte);
+	
+	
 	echo fin_gauche(true);
 	echo fin_page(true);
 	
