259c3c68049a15f9af9cf88279e917438383cb78
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-11-07 16:11:53 +0000

    Plus de pipeline affiche_droite Une file d'attente d'encodage qui devrait mieux fonctionner
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@24096 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/spipmotion/genie/spipmotion_file.php
+++ _dev_/spipmotion/genie/spipmotion_file.php
@@ -4,10 +4,11 @@ function genie_spipmotion_file($time)  {
 	spip_log("Il y a $nb_videos vidéo(s) à encoder","spipmotion");
 	if($nb_videos>0){
 		$encoder = charger_fonction('encodage','inc');
-		$video_attente = sql_fetsel("*","spip_spipmotion_attentes","encode='non'","","maj","1");
-		$id_video_attente = $video_attente['id_document'];
-		$document = sql_fetsel('*','spip_documents','id_document='.sql_quote($id_video_attente));
-		$encoder($document,$video_attente);
+		$video_attente = sql_fetsel("id_spipmotion_attente,id_document","spip_spipmotion_attentes","encode='non'","","maj","1");
+		$id_document = $video_attente['id_document'];
+		$id_video_attente = $video_attente['id_spipmotion_attente'];
+		$document = sql_fetsel('*','spip_documents','id_document='.sql_quote($id_document));
+		$encoder($document,$id_video_attente);
 	}
 }
 ?>
--- _dev_/spipmotion/inc/encodage.php
+++ _dev_/spipmotion/inc/encodage.php
@@ -20,11 +20,12 @@ function inc_encodage_dist($source,$attente){
 	  return encodage($source,$attente);
 }
 
-function encodage($source,$attente){
+function encodage($source,$video_attente){
 	include_spip('inc/documents');
-	$fichier = basename($source['fichier']);
 	$chemin = get_spip_doc($source['fichier']);
 
+	spip_log("encodage de $chemin","spipmotion");
+	
 	// Calcul de la hauteur en fonction de la largeur souhaitée et de la taille de la video originale	
 	$width = $source['largeur'];
 	$height = $source['hauteur'];
@@ -40,14 +41,16 @@ function encodage($source,$attente){
 	
 	spip_log("document original ($chemin) = $width/$height - document final = $width_finale/$height_finale");
 	
+	$fichier = basename($source['fichier']);
 	$string = "$fichier-$width-$height";
 	$query = md5($string);
 	$dossier = _DIR_VAR;
 	$fichier_temp = "$dossier$query.flv";
+	spip_log("le nom temporaire durant l'encodage est $fichier_temp");
 	
 	$encodageflv = find_in_path('script_bash/spipmotion.sh').' --e '.$chemin.' --s '.$fichier_temp.' --size '.$width_finale.'x'.$height_finale.' --bitrate '.lire_config("spipmotion/bitrate","448").' --audiobitrate '.lire_config("spipmotion/bitrate_audio","64").' --audiofreq '. lire_config("spipmotion/frequence_audio","22050").' --fps '.lire_config("spipmotion/fps","15").' --p '.lire_config("spipmotion/chemin","/usr/local/bin/ffmpeg");
 	spip_log("$encodageflv");
-	$lancement_encodage =shell_exec($encodageflv);
+	$lancement_encodage = shell_exec($encodageflv);
 	spip_log($lancement_encodage);
 	spip_log("l'encodage est terminé");
 	
@@ -55,15 +58,17 @@ function encodage($source,$attente){
 	
 	$mode = 'document';
 	$invalider = true;
-	$type_doc = 'article';
+	
+	$attente = sql_fetsel("*","spip_spipmotion_attentes","id_spipmotion_attente=$video_attente");
+	$type_doc = $attente['objet'];
+	$id_objet = $attente['id_objet'];
 	
 	$ajouter_documents = charger_fonction('ajouter_documents', 'inc');
-	$x = $ajouter_documents($fichier_temp, $fichier_final, $type_doc, $attente['id_article'], $mode, $id_document, $actifs);
+	$x = $ajouter_documents($fichier_temp, $fichier_final, $type_doc, $id_objet, $mode, $id_document, $actifs);
 	spip_log("on ajoute le nouveau fichier qui devient $x");
-	
 	unlink($fichier_temp);
 	
-	sql_updateq('spip_spipmotion_attentes',array('encode'=>'oui'),'id_spipmotion_attente='.$attente['id_spipmotion_attente']);
+	sql_updateq("spip_spipmotion_attentes",array('encode'=>'oui'),"id_spipmotion_attente=$video_attente");
 	// la récupération des infos et du logo est faite automatiquement par le pipeline post-edition
 	
 		if ($invalider) {
