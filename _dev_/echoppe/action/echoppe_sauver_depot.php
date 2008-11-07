7233bb6160110428d8caab1018156bebca3928ec
pierre.fiches@free.fr <pierre.fiches@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-11-07 09:56:00 +0000

    Suivre l'evolution de produit
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@24075 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/echoppe/action/echoppe_sauver_depot.php
+++ _dev_/echoppe/action/echoppe_sauver_depot.php
@@ -1,25 +1,42 @@
 <?php
 
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
 function action_echoppe_sauver_depot(){
-	include_spip('inc/utils');
-	$contexte = Array();
-	$contexte['new'] = _request('new');
-	$contexte['id_depot'] = _request('id_depot');
-	$contexte['titre'] = _request('titre');
-	$contexte['description'] = _request('texte');
-	$contexte['adresse'] = serialize(_request('adresse'));
+	//include_spip('inc/utils');  necessaire ?
+	$new = _request('new');
+	$id_depot = _request('id_depot');
+	$titre = _request('titre');
+	$description = _request('texte');
+	$adresse = serialize(_request('adresse'));
 	
-	if ($contexte['new'] == "oui"){
-		$sql_sauver_depot = "INSERT INTO spip_echoppe_depots VALUES ('', '".addslashes($contexte['titre'])."', '".addslashes($contexte['description'])."', '".addslashes($contexte['adresse'])."', NOW());";
-		$res_sauver_depot = spip_query($sql_sauver_depot);
-		$contexte['id_depot'] == spip_insert_id();
+	switch ($new){
+		case 'oui':
+			$arg_inser_depot = array(
+			'id_depot' => '',
+			'titre' => addslashes($titre), 
+			'descriptif' => addslashes($description), 
+			'adresse' => $adresse, 
+			'maj' => 'NOW()'
+			);
+			$id_depot = sql_insertq('spip_echoppe_depots',$arg_inser_depot);
+	
+		break;
+	
+		default :
+			$arg_inser_depot = array(
+			'id_depot' => $id_depot,
+			'titre' => $titre, 
+			'descriptif' => $description,
+			'adresse' => $adresse, 
+			'maj' => 'NOW()'
+			);
+			sql_updateq("spip_echoppe_depots",$arg_inser_depot,"id_depot=$id_depot");
+			
+		break;
 		
-	}else{
-		$sql_sauver_depot = "UPDATE spip_echoppe_depots SET titre='".addslashes($contexte['titre'])."', descriptif='".addslashes($contexte['description'])."', adresse='".addslashes($contexte['adresse'])."', maj = NOW() WHERE id_depot='".$contexte['id_depot']."';";
-		$res_sauver_depot = spip_query($sql_sauver_depot);
 	}
 	//echo "SQL1 - ".$sql_sauver_depot;
-	
 	$redirect = generer_url_ecrire('echoppe_gerer_depots');
 	redirige_par_entete($redirect);
 	
