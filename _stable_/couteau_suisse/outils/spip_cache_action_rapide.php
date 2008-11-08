60fcaa47d9ff9c33ebda74f1247d481f2114ecad
patfr@ifrance.com <patfr@ifrance.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-11-08 02:32:29 +0000

    Compatibilité des actions rapides pour SPIP 1.92
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@24108 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/couteau_suisse/outils/spip_cache_action_rapide.php
+++ _stable_/couteau_suisse/outils/spip_cache_action_rapide.php
@@ -3,8 +3,22 @@
 // module inclu dans la description de l'outil en page de configuration
 // ici, un bouton : "vider le cache"
 
+include_spip('inc/invalideur');
+include_spip('inc/actions');
+
+// Compatibilite SPIP < 2.0
+if(!defined('_SPIP19300')) {
+	function taille_du_cache() {
+		$cpt = spip_fetch_array(spip_query("SELECT SUM(taille) AS n FROM spip_caches WHERE type='t'"));
+		return $cpt['n'];
+	}
+	function redirige_action_post($action, $arg, $ret, $gra, $corps, $att='') {
+		$r = _DIR_RESTREINT_ABS . generer_url_ecrire($ret, $gra, true, true);
+		return generer_action_auteur($action, $arg, $r, $corps, $att . " method='post'");
+	}
+}
+
 function spip_cache_action_rapide() {
-	include_spip('inc/invalideur');
 	if ($n = taille_du_cache())
 	  $info = _T('taille_cache_octets', array('octets' => taille_en_octets($n)));
 	else
