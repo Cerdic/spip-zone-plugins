546ea2076cb3388e8e5a6762e05ce460bd8dd73e
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-11-08 17:58:22 +0000

    Ça ne marchait plus, mise à jour pour SPIP 2. Merci Valéry-Xavier
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@24120 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/layoutgala/layoutgala_pipelines.php
+++ _stable_/layoutgala/layoutgala_pipelines.php
@@ -1,6 +1,6 @@
 <?php
 // insert le css pour les styles supplementaires de LayoutGala dans le <head> du document (#INSERT_HEAD)
-function LayoutGala_insert_head($flux) {
+function layoutgala_insert_head($flux) {
 	$incHead = '<link rel="stylesheet" href="spip.php?page=layoutgala" type="text/css" media="all" />';
 	return preg_replace('#(</head>)?$#i', $incHead . "\$1\n", $flux, 1);
 }
