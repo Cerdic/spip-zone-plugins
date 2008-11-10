eb79b6d8fb1ac8ec65740a61c888190f1174bcaf
apachot@openstudio.fr <apachot@openstudio.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-11-10 15:42:58 +0000

    ajout des liens vers le compte de lecteur - Réservez cet ouvrage - Etiquetez cet ouvrage - Donnez votre avis
    affichage dans une fenêtre js avec nyroModal
    
    
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@24158 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/spip-pmb/2_0_0/lang/pmb_fr.php
+++ _dev_/spip-pmb/2_0_0/lang/pmb_fr.php
@@ -3,6 +3,12 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 'parametrage_plugin' => 'Param&eacute;trage du plugin',
 'url' => 'Adresse',
 'parametrage_catalogue' => 'Param&eacute;trage du catalogue',
-'pas_d_ouvrages_trouves' => 'Pas d\'ouvrages trouv&eacute;s dans le catalogue'
+'pas_d_ouvrages_trouves' => 'Pas d\'ouvrages trouv&eacute;s dans le catalogue',
+'donner_avis' => 'Donnez votre avis',
+'ajouter_tag' => 'Etiquetez cet ouvrage',
+'reserver_ouvrage' => 'R&eacute;servez cet ouvrage',
+'exemplaires' => 'Exemplaires',
+'autres_lecteurs' => 'Les abonn&eacute;s qui ont emprunt&eacute; ce document ont &eacute;galement emprunt&eacute;',
+'compte_lecteur' => 'Votre compte de lecteur'
 );
 ?>
