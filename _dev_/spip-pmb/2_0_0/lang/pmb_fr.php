b10cf011d54e5538bbcf04327f2720701511fc68
apachot@openstudio.fr <apachot@openstudio.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-11-10 13:25:24 +0000

    recherche dans le catalogue : remplacer le php par des boucles et balises spip
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@24152 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/spip-pmb/2_0_0/lang/pmb_fr.php
+++ _dev_/spip-pmb/2_0_0/lang/pmb_fr.php
@@ -2,6 +2,7 @@
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 'parametrage_plugin' => 'Param&eacute;trage du plugin',
 'url' => 'Adresse',
-'parametrage_catalogue' => 'Param&eacute;trage du catalogue'
+'parametrage_catalogue' => 'Param&eacute;trage du catalogue',
+'pas_d_ouvrages_trouves' => 'Pas d\'ouvrages trouv&eacute;s dans le catalogue'
 );
 ?>
