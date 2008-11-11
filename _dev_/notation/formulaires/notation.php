937c7938b6059befd66f8916705b17de722bce27
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-11-11 23:06:22 +0000

    "invalidons le cache, subtilement si possible"
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@24250 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/notation/formulaires/notation.php
+++ _dev_/notation/formulaires/notation.php
@@ -203,7 +203,14 @@ function notation_recalculer_total($objet,$id_objet){
 			"note_ponderee" => $note_ponderee,
 			"nombre_votes" => $total
 			));
+		include_spip('inc/invalideur');
+		suivre_invalideur("notation/$objet/$id_objet");
+			
 	} else {
+		$anc_note_ponderee = sql_getfetsel('note_ponderee','spip_notations_objets',array(
+				"objet=" . sql_quote($objet),
+				"id_objet=" . sql_quote($id_objet)
+			));
 		// Mettre ajour dans les autres cas
 		sql_updateq("spip_notations_objets", array(
 			"note" => $note,
@@ -213,6 +220,11 @@ function notation_recalculer_total($objet,$id_objet){
 				"objet=" . sql_quote($objet),
 				"id_objet=" . sql_quote($id_objet)
 			));
+		// on optimise en n'invalidant que si la notre ponderee change (sinon ca ne se verra pas)
+		if (round($anc_note_ponderee)!=$note_ponderee){
+			include_spip('inc/invalideur');
+			suivre_invalideur("notation/$objet/$id_objet");
+		}
 	}
 }
 
