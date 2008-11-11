6afcec247e19391b766da220db203e9e95f700e1
JN.jamesnicolas@gmail.com <JN.jamesnicolas@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-11-11 17:33:13 +0000

    Ce plugin permet , si le referer du visiteur correspond à l'url d'un site syndiqué, d'augmenter son compteur.
    Le compteur de chaque site est #TOPSITE
    
    (ce plugin n'est pas complètement opérationnel, n'hésitez pas à regarder ;) Il pourrait peut-être utiliser ce qui est dans le core pour les stats...)
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@24219 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _dev_/topsite/base/topsite_init.php
@@ -0,0 +1,52 @@
+<?php
+#------------------------------------------------------------------------#
+#  Plugin  : compte_clics - Licence : GPL                                #
+#  File    : compteclics_init : installation/desinstallation du plugin   #
+#  Authors : Chryjs, 2007 +                                              #
+#  based on: http://www.spip-contrib.net/Compter-les-clics-sur-les-liens #
+#  and     : http://www.plugandspip.com/spip.php?article37               #
+#  Contact : chryjs¡@!free¡.!fr                                          #
+#------------------------------------------------------------------------#
+
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program; if not, write to the Free Software
+//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+if (!function_exists("spip_query")) include_ecrire("inc/utils");
+
+function topsite_install($action)
+{
+switch ($action) {
+ case 'test' : /* test pour savoir si les actions sont necessaires */
+	include_ecrire ('base/abstract_sql.php');
+	$desc = spip_abstract_showtable("spip_syndic", '', true);
+	return (isset($desc['field']['topsite']));
+	break;
+ case 'install' :
+	$res =@spip_query("ALTER TABLE spip_syndic ADD COLUMN topsite INTEGER DEFAULT 0");
+	return $res;
+	break;
+ case 'uninstall' :
+	$res = @spip_query("ALTER TABLE spip_syndic DROP COLUMN topsite");
+	return $res;
+	break;
+ }
+} /* compteclics_install */
+
+/* rend disponible l'icone de désinstallation */
+/* ca doit certainement permettre d'effacer les fichiers et autres */
+function topsite_uninstall(){
+}
+
+?>
---
+++ _dev_/topsite/topsite_fonctions.php
@@ -0,0 +1,37 @@
+<?php
+#------------------------------------------------------------------------#
+#  Plugin  : TOP site - Licence : GPL                                    #
+#  File    : topsite_fonctions - #CLIC_COMPTEUR                          #
+#  Authors : JN, 2008 +                                                  #
+#  based on: http://www.spip-contrib.net/Compter-les-clics-sur-les-liens #
+#  Contact : JN.jamesnicolas@gmail.com                                   #
+#------------------------------------------------------------------------#
+
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program; if not, write to the Free Software
+//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+
+function topsite_ajout($flux) {
+	verifier_visiteur(); // securite...
+	$site = $_SERVER['HTTP_REFERER'];
+	preg_split(",\t,", $site, 3);
+	if(spip_query("SELECT url_site  FROM spip_syndic WHERE url_site='$site' LIMIT 1"))
+	{
+
+		spip_query("UPDATE LOW_PRIORITY spip_syndic SET topsite = topsite + 1 WHERE url_site='$site' LIMIT 1");
+	
+	}
+	
+}
+?>
