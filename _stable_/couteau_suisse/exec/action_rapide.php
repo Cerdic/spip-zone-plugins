8f27047ca0aeb58d37d650f7a078ad3e6da53194
patfr@ifrance.com <patfr@ifrance.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-11-12 06:30:57 +0000

    bricole
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@24268 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/couteau_suisse/exec/action_rapide.php
+++ _stable_/couteau_suisse/exec/action_rapide.php
@@ -85,7 +85,7 @@ cs_log(" FIN : exec_description_outil_dist() - Appel maintenant de ajax_retour()
 		// chercher dans la table des URLS
 		include_spip('base/abstract_sql');
 		//  Recuperer une URL propre correspondant a l'objet.
-		$row = sql_fetsel("U.url, O.$champ_titre", "$table AS O LEFT JOIN spip_urls AS U ON (U.type='$type' AND U.id_objet=O.$col_id)", "O.$col_id=$id_objet", '', 'date DESC', 1);
+		$row = sql_fetsel("U.url, O.$champ_titre", "$table AS O LEFT JOIN spip_urls AS U ON (U.type='$type' AND U.id_objet=O.$col_id)", "O.$col_id=$id_objet", '', 'U.date DESC', 1);
 
 		if (!$row) return false; # Quand $id_objet n'est pas un numero connu
 		list($champ_titre,) = explode(',', $champ_titre, 2);
