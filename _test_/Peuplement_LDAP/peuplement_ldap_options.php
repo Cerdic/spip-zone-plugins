f25278c96c1a3ba30319a7d752057ed5849bf996
benjamin.revel@free.fr <benjamin.revel@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-11-12 22:24:44 +0000

    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@24302 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _test_/Peuplement_LDAP/peuplement_ldap_options.php
@@ -0,0 +1,11 @@
+<?php
+
+$peuplement_ldap_filtre_defaut = "(&(mail=*)(objectClass=*))";
+
+// Images
+$peuplement_ldap_icon = "system-users.png";
+$peuplement_ldap_insert_ok = "contact-new.png";
+$peuplement_ldap_insert_ko = "dialog-error.png";
+$peuplement_ldap_insert_doublon = "dialog-warning.png";
+
+?>
