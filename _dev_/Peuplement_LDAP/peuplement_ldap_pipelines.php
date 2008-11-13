5fc7b68938e28a3e54fbb9bd35874e485575ce6b
benjamin.revel@free.fr <benjamin.revel@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-11-13 18:51:05 +0000

    v 0.9
    * Message d'alerte concernant le filtre par défaut
    * Correction de caracteres HTML
    * Acces au plugin uniquement si acces LDAP correctement configure
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@24322 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/Peuplement_LDAP/peuplement_ldap_pipelines.php
+++ _dev_/Peuplement_LDAP/peuplement_ldap_pipelines.php
@@ -1,5 +1,5 @@
 <?php
-// Essai
+
 if (!defined('_DIR_PLUGIN_PEUPLEMENTLDAP')){ // definie automatiquement en 1.9.2
         $p=explode(basename(_DIR_PLUGINS)."/",str_replace('\\','/',realpath(dirname(__FILE__))));
         define('_DIR_PLUGIN_PEUPLEMENTLDAP',(_DIR_PLUGINS.end($p)));
@@ -8,13 +8,14 @@ if (!defined('_DIR_PLUGIN_PEUPLEMENTLDAP')){ // definie automatiquement en 1.9.2
  * Affiche le bouton d'accès aux formulaires de peuplement
  * 
  * Le bouton de configuration n'est affiché que 
- * si l'on est administrateur et en interface complète
+ * si l'on est administrateur, en interface complète et avec
+ * l'accès LDAP correctement configuré.
  *
  * @param Array $boutons_admin Tableau contenant les boutons Spip
  * @return Array Tableau contenant les bouton Spip dont celui du peuplement
  */
 function PeuplementLdap_ajouterBoutons($boutons_admin) {
-	if ($GLOBALS['connect_statut'] == "0minirezo" AND $GLOBALS["options"]=="avancees") { // Controle sur les droits de l'auteur
+	if ($GLOBALS['connect_statut'] == "0minirezo" AND $GLOBALS["options"]=="avancees" AND $GLOBALS["ldap_present"]) { // Controle sur les droits de l'auteur
 		$boutons_admin['auteurs']->sousmenu['peuplement_ldap']= new Bouton(
         	"../"._DIR_PLUGIN_PEUPLEMENTLDAP."/img_pack/".$GLOBALS['peuplement_ldap_icon'],
 			_T('peuplementldap:icone_menu_config')
