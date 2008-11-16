0e01c3ecac0a8ea64262079c4acba67b61e8d8d1
paladin@quesaco.org <paladin@quesaco.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-11-16 05:53:05 +0000

    respecter la licence GNU
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@24381 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/spip-listes/spip-listes_1_9_3/inc/spiplistes_listes_selectionner_auteur.php
+++ _test_/spip-listes/spip-listes_1_9_3/inc/spiplistes_listes_selectionner_auteur.php
@@ -2,6 +2,26 @@
 
 // inc/spiplistes_listes_selectionner_auteur.php
 
+/******************************************************************************************/
+/* SPIP-Listes est un systeme de gestion de listes d'abonnes et d'envoi d'information     */
+/* par email pour SPIP. http://bloog.net/spip-listes                                      */
+/* Copyright (C) 2004 Vincent CARON  v.caron<at>laposte.net                               */
+/*                                                                                        */
+/* Ce programme est libre, vous pouvez le redistribuer et/ou le modifier selon les termes */
+/* de la Licence Publique Generale GNU publiee par la Free Software Foundation            */
+/* (version 2).                                                                           */
+/*                                                                                        */
+/* Ce programme est distribue car potentiellement utile, mais SANS AUCUNE GARANTIE,       */
+/* ni explicite ni implicite, y compris les garanties de commercialisation ou             */
+/* d'adaptation dans un but specifique. Reportez-vous a la Licence Publique Generale GNU  */
+/* pour plus de details.                                                                  */
+/*                                                                                        */
+/* Vous devez avoir recu une copie de la Licence Publique Generale GNU                    */
+/* en meme temps que ce programme ; si ce n'est pas le cas, ecrivez a la                  */
+/* Free Software Foundation,                                                              */
+/* Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, Etats-Unis.                   */
+/******************************************************************************************/
+
 // $LastChangedRevision$
 // $LastChangedBy$
 // $LastChangedDate$
