c1793d9dad7431c68263dfcb59ade3cf12976fd8
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-11-18 10:25:34 +0000

    cas de la nouvelle video
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@24425 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/kaltura/lang/kaltura_fr.php
+++ _dev_/kaltura/lang/kaltura_fr.php
@@ -6,6 +6,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// B
   'bouton_reset_video' => 'Effacer cette vid&eacute;o et recommencer',
   'bouton_ajouter_video' => 'Enregistrer cette vid&eacute;o',
+  'bouton_nouvelle_video' => 'Commencer une nouvelle vid&eacute;o',
 
 	// N
   'nouvelle_video' => 'Nouvelle vid&eacute;o',
