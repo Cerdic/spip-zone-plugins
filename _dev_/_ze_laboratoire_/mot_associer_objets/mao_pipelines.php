fa744af9c8cae6e814676ae5c1ed3ffdadfec06e
cam.lafit@azerttyu.net <cam.lafit@azerttyu.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-11-18 11:39:49 +0000

    recuperer_fond est l'api officielle depuis septembre 2008
    cf : http://trac.rezo.net/trac/spip/changeset/12503
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@24430 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/_ze_laboratoire_/mot_associer_objets/mao_pipelines.php
+++ _dev_/_ze_laboratoire_/mot_associer_objets/mao_pipelines.php
@@ -4,7 +4,7 @@ function mao_affiche_milieu($flux){
 	if ($flux['args']['exec']=='mots_edit'){
 		$id_mot = $flux['args']['id_mot'];
 		$id_groupe = sql_getfetsel('id_groupe','spip_mots','id_mot='.intval($id_mot));
-		$page = evaluer_fond("prive/contenu/mot", array('id_mot'=>$id_mot,'id_groupe'=>$id_groupe), $connect);
+		$page = recuperer_fond("prive/contenu/mot", array('id_mot'=>$id_mot,'id_groupe'=>$id_groupe), $connect);
 		
 		$flux['data'] .= $page['texte'];
 	}
