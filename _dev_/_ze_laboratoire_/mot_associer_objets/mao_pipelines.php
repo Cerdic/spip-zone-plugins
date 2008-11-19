9805a3265edd179c1f36c22120e81e4adf0af0a4
cam.lafit@azerttyu.net <cam.lafit@azerttyu.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-11-19 12:12:49 +0000

    revert [24450]
    cf http://comments.gmane.org/gmane.comp.web.spip.zone.cvs/19729
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@24451 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/_ze_laboratoire_/mot_associer_objets/mao_pipelines.php
+++ _dev_/_ze_laboratoire_/mot_associer_objets/mao_pipelines.php
@@ -1,11 +1,14 @@
 <?php
-function mao_afficher_contenu_objet($flux){
+function mao_affiche_milieu($flux){
 
-	$id_mot = $flux['args']['id_objet'];
-	$id_groupe = sql_getfetsel('id_groupe','spip_mots','id_mot='.intval($id_mot));
-	$page = recuperer_fond("prive/contenu/mot", array('id_mot'=>$id_mot,'id_groupe'=>$id_groupe),true,$connect);
+    if ($flux['args']['exec']=='mots_edit'){
+    
+	    $id_mot = $flux['args']['id_mot'];
+	    $id_groupe = sql_getfetsel('id_groupe','spip_mots','id_mot='.intval($id_mot));
+	    $page = recuperer_fond("prive/contenu/mot", array('id_mot'=>$id_mot,'id_groupe'=>$id_groupe),true,$connect);
 	
-	$flux['data'] .= $page;
+	    $flux['data'] .= $page;
+	}
 
 	return $flux;
 }
