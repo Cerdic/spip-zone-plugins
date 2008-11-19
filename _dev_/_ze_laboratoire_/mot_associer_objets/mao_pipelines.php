75f9086d1a83ef0a73ef3eecf06052b7816d15f6
cam.lafit@azerttyu.net <cam.lafit@azerttyu.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-11-19 11:35:01 +0000

    Changement de pipeline
    La gestion des mots clefs propose un pipeline plus spécifique autant en profiter pour agir là où c'est necessaire
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@24450 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/_ze_laboratoire_/mot_associer_objets/mao_pipelines.php
+++ _dev_/_ze_laboratoire_/mot_associer_objets/mao_pipelines.php
@@ -1,12 +1,11 @@
-<?
-function mao_affiche_milieu($flux){
+<?php
+function mao_afficher_contenu_objet($flux){
 
-	if ($flux['args']['exec']=='mots_edit'){
-		$id_mot = $flux['args']['id_mot'];
-		$id_groupe = sql_getfetsel('id_groupe','spip_mots','id_mot='.intval($id_mot));
-		$page = recuperer_fond("prive/contenu/mot", array('id_mot'=>$id_mot,'id_groupe'=>$id_groupe),true,$connect);
-		
-		$flux['data'] = $page;
-	}
+	$id_mot = $flux['args']['id_objet'];
+	$id_groupe = sql_getfetsel('id_groupe','spip_mots','id_mot='.intval($id_mot));
+	$page = recuperer_fond("prive/contenu/mot", array('id_mot'=>$id_mot,'id_groupe'=>$id_groupe),true,$connect);
+	
+	$flux['data'] .= $page;
+
 	return $flux;
 }
