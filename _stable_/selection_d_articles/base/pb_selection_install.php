832e30ad8782ec79b1927b540230c1ecb00b90fb
arno@rezo.net <arno@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-11-24 18:47:44 +0000

    Installation de la base
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@24599 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/selection_d_articles/base/pb_selection_install.php
+++ _stable_/selection_d_articles/base/pb_selection_install.php
@@ -5,8 +5,6 @@
 //
 
 function pb_selection_declarer_tables_principales($tables_principales){
-
-	global $tables_principales;
 	
 
 	$spip_pb_selection = array(
@@ -22,23 +20,9 @@ function pb_selection_declarer_tables_principales($tables_principales){
 	$tables_principales['spip_pb_selection'] = array(
 		'field' => &$spip_pb_selection,
 		'key' => &$spip_pb_selection_key);
-
-	// Bizarre: il faut encore demander a creer la base?
-	include_spip('base/create');
-	include_spip('base/abstract_sql');
-	creer_base();
-	
-	
 	return $tables_principales;
 	
 	}
 
-/*
-	function pb_selection_vider_tables($nom_meta_base_version) {
-		spip_query("DROP TABLE spip_pb_selection");
-		effacer_meta("pb_selection_version");
-		ecrire_metas();
-	}
-*/
 
 ?>
