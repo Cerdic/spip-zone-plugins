ad7ab5a51c3877a189971a5cff62f415081b173d
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-11-26 13:13:47 +0000

    langues (flickr_cc)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@24663 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _test_/flickr_cc/lang/flickr_cc_cc_cc_gl.php
@@ -0,0 +1,37 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'ajouter_image_flickr' => 'Engadir unha imaxe de Flickr',
+
+	// C
+	'cc_commerciale' => 'CC - Uso comercial aceptado',
+	'cc_modification' => 'CC - Modificaci&oacute;n aceptada',
+	'cc_pas_de_restriction' => 'Non se declara ningunha restrici&oacute;n',
+	'cc_paternite' => 'Creative Commons - Autor&iacute;a',
+
+	// F
+	'fermer' => 'Pechar',
+	'flickr_cc_explication' => '<p>Procurar imaxes con licenza Creative Commons sobre Flickr indicando os termos da procura seguintes. Pode precisar a natureza das licenzas &aacute;s que est&aacute;n sometidas as imaxes.</p>
+<p>Prema sobre a (ou as) imaxes(s) escollida(s) entre os resultados da procura&nbsp;: est&aacute; inmediatamente asociada ao seu artigo. Prema sobre o nome do autor para acceder &aacute; p&aacute;xina de Flickr correspondente a esta imaxe.</p>
+<p>Estas imaxes ofrecidas polos seus autores respectivos&nbsp;: para respectar os seus dereitos de autor e o seu traballo,</p>
+<ul><li>non suprima a menci&oacute;n do autor nin a ligaz&oacute;n sobre a s&uacute;a p&aacute;xina de Flick&nbsp;;</li>
+<li>respecte rigorosamente a licenza Creative Commons asociada &aacute; cada imaxe (en particular, non empregue unha imaxe nun &aacute;mbito comercial se a licenza non o autoriza&nbsp;;</li>
+<li>se o autor da imaxe llo demanda, debe retirar a imaxe do seu web nun prazo de 24 horas.</li>
+</ul>
+<p>Este produto usa a API de Flickr mais non es&aacute; sustentado, nin certificado por Flickr.</p>',
+
+	// R
+	'rechercher_flickr' => 'Procurar sobre Flickr',
+	'resultat_par_date' => 'por data',
+	'resultat_par_interet' => 'por interese',
+	'resultat_par_pertinence' => 'por pertinencia'
+);
+
+?>
---
+++ _test_/flickr_cc/lang/flickr_cc_cc_es.php
@@ -0,0 +1,37 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'ajouter_image_flickr' => 'A&ntilde;adir una imagen de Flickr',
+
+	// C
+	'cc_commerciale' => 'CC - Uso comercial posible',
+	'cc_modification' => 'CC - Modificaci&oacute;n posible',
+	'cc_pas_de_restriction' => 'Sin restricciones conocidas',
+	'cc_paternite' => 'Creative Commons - Reconocimiento',
+
+	// F
+	'fermer' => 'Cerrar',
+	'flickr_cc_explication' => '<p>Buscar im&aacute;genes con licencia Creative Commons en Flickr indicando los t&eacute;rminos de b&uacute;squeda m&aacute;s arriba. Indica si es el caso la naturaleza de las licencias a las que se acogen las im&aacute;genes.</p>
+<p>Pulsa en la (o las) imagen(es) elegida(s) entre los resultados de la b&uacute;squeda: la imagen se asocia inmediatamente con el art&iacute;culo. Pulsa en el nombre del autor para acceder a la p&aacute;gina en Flickr de esta imagen.</p>
+<p>Estas im&aacute;genes se facilitan por sus respectivos autores: para respetar sus derechos y su trabajo,</p>
+<ul><li>no suprimas la menci&oacute;n del autor y el enlace a su p&aacute;gina en Flickr&nbsp;;</li>
+<li>respeta rigurosamente la licencia Creative Commons asociada con cada imagen (en particular, no uses una imagen en un sitio comercial si la licencia no lo autoriza;</li>
+<li>si el autor de la imagen te lo pide, debes retirar la imagen de tu sitio en un plazo de 24 horas.</li>
+</ul>
+<p>Este producto utiliza el API Flickr pero no est&aacute; ni soportado, ni certificado por Flickr.</p>',
+
+	// R
+	'rechercher_flickr' => 'Buscar en Flickr',
+	'resultat_par_date' => 'por fecha',
+	'resultat_par_interet' => 'por inter&eacute;s',
+	'resultat_par_pertinence' => 'por pertinencia'
+);
+
+?>
