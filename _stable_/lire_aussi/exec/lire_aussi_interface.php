6dd7c96e12030549e072c520a3a8efd9236a9f00
aurelien.levy@free.fr <aurelien.levy@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-12-01 13:18:21 +0000

    compatibilite 2.0 RC1
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@24829 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/lire_aussi/exec/lire_aussi_interface.php
+++ _stable_/lire_aussi/exec/lire_aussi_interface.php
@@ -4,9 +4,7 @@
 function exec_lire_aussi_interface() {
 		include_spip("inc/utils");
 		$contexte = array('id_article'=>$_GET["id_article"]);
-
-		$p = evaluer_fond("lire_aussi_interface", $contexte);
-		$ret .= $p["texte"];
+		$ret .= recuperer_fond("lire_aussi_interface", $contexte);
 		
 		
 		echo $ret;
