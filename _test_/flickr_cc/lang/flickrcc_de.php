c101d88fe4d9fd4cef1cf69a5c624bc60e84979a
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-12-06 19:06:06 +0000

    langues (flickrcc)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@25000 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _test_/flickr_cc/lang/flickrcc_de.php
@@ -0,0 +1,30 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'ajouter_image_flickr' => 'Ein Bild aus Flickr hinzuf&uuml;gen',
+
+	// C
+	'cc_commerciale' => 'CC - Gewerbliche Verwendung gestattet',
+	'cc_modification' => 'CC - Bearbeitung gestattet',
+	'cc_pas_de_restriction' => 'Keine bekannten Beschr&auml;nkungen',
+	'cc_paternite' => 'Creative Commons - Autorenangaben erforderlich',
+
+	// F
+	'fermer' => 'Schlie&szlig;en',
+	'flickr_cc_explication' => '<p>Suchen Sie auf Flickr nach Bilder unter der <i>Creative Commons Lizenz</i>, indem Sie unten Ihre Suchbegriffe eingeben. Geben Sie falls erforderlich den gew&uuml;nschten Lizenztyp an.</p><p>Klicken Sie auf das Bild oder die Bilder der Fundstellenliste. Sie werden Ihrem Artikel sofort zugeordnet. Klicken Sie auf den Autorennamen, um auf die Flickr Seite zu gelangen.</p><p>Diese Bilder werden von den Urhebern kostenlos bereitgestellt. Bitte respektieren Sie ihre Rechnte und</p><ul><li>entfernen keine Angaben zum Urheber und seiner Seite auf Flickr.</li><li>halten Sie sich bitte an die Lizenzbedingungen (unterlassen Sie es insbesondere, Bilder kommerziell zu verwenden, f&uuml;r die es nicht gestattet ist.)</li><li>falls ein Urheber es verlangt, m&uuml;s&amp;uuml;sen Sie seine Bilder unverz&uuml;glich aus ihrer Website entfernen</li></ul><p>Dieses Plugin verwendet die Flickr API, wird von Flickr jedoch nicht supportet.</p>',
+
+	// R
+	'rechercher_flickr' => 'Auf Flickr suchen',
+	'resultat_par_date' => 'Nach Datum',
+	'resultat_par_interet' => 'Nach Interesse',
+	'resultat_par_pertinence' => 'Nach Relevanz'
+);
+
+?>
