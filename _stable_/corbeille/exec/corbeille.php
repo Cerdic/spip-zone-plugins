69dece9fe8de965e0155fcf43730518e9a1db671
patfr@ifrance.com <patfr@ifrance.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-12-07 18:24:43 +0000

    Les plugins peuvent ajouter des noisettes dans "prive/listes/"
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@25027 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/corbeille/exec/corbeille.php
+++ _stable_/corbeille/exec/corbeille.php
@@ -19,21 +19,21 @@ function exec_corbeille_dist(){
 	echo debut_boite_info(true);
 	echo propre(_T('corbeille:readme'));  	
 	echo fin_boite_info(true);
-	
+
 	echo debut_droite('',true);
 	echo gros_titre(_T('corbeille:corbeille'),'',false);
-	
-	
+
+
 	echo formulaire_recherche('corbeille');
 	// recuperer toutes les noisettes d'admin existantes
-	$liste = preg_files(_DIR_PLUGIN_CORBEILLE."prive/listes/","/corbeille_([^.]*)[.]html");
+	$liste = find_all_in_path("prive/listes/","/corbeille_[^.]*[.]html");
 	foreach ($liste as $noisette) {
 		if (preg_match(',^corbeille_([^.]*)$,',basename($noisette,'.html'),$regs)){
 			$table = $regs[1];
 			echo recuperer_fond("prive/inc-corbeille",array_merge($_GET,array('table'=>$table)));
 		}
 	}
-	
+
 	echo fin_gauche(),fin_page();
 }
 
