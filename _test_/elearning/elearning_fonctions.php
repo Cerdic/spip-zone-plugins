8e955c39efc020f6a32779855cd405aed8cd71ef
vincent@ldd.fr <vincent@ldd.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-12-09 16:27:41 +0000

    il manquait une transformation pour le sélecteur ajax (lorsque plus de 20 rubriques par défaut)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@25047 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/elearning/elearning_fonctions.php
+++ _test_/elearning/elearning_fonctions.php
@@ -12,7 +12,8 @@
 function elearning_chercher_rubrique($msg, $rubrique_elearning, $name){
 	
 	$select = chercher_rubrique($msg, 0, $rubrique_elearning, 'article', 0, '', 0, 'form_simple');
-	$select = preg_replace('/<select.*?>/is', '<select name="'.$name.'" id="'.$name.'"', $select);
+	$select = preg_replace('/<select.*?>/is', '<select name="'.$name.'" id="'.$name.'">', $select);
+	$select = preg_replace('/<input[[:blank:]]+type=[\'"]hidden[\'"].*?id=[\'"]id_parent[\'"].*?\/>/is', '<input type="hidden" name="'.$name.'" id="id_parent" value="'.$rubrique_elearning.'" />', $select);
 	return $select;
 	
 }
