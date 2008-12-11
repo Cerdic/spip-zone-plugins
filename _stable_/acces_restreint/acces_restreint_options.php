bfebea2cc02fcbee0422048e1fc28f7f31127cff
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-12-11 14:28:42 +0000

    "acces restreint 3 ne marche qu'avec le vrai spip-bonux"
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@25110 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/acces_restreint/acces_restreint_options.php
+++ _stable_/acces_restreint/acces_restreint_options.php
@@ -27,4 +27,17 @@ if (!isset($GLOBALS['marqueur'])) $GLOBALS['marqueur'] = '';
 $GLOBALS['marqueur'] .= ":accesrestreint_zones_autorisees="
 	.$GLOBALS['accesrestreint_zones_autorisees'];
 
+if (test_espace_prive()
+  AND _request('exec')=='admin_plugin'){
+  include_spip('spip_bonux_fonctions');
+  // verifier qu'on a bien le bon bonux, avec la css qui marche
+  if (!file_exists($f=(_DIR_PLUGIN_SPIP_BONUX.'style_prive_formulaires.html'))
+  OR !lire_fichier($f,$contenu)
+  OR md5($contenu)!==_BONUX_CSS_MD5_FORMULAIRES){
+  	include_spip('inc/plugin');
+  	ecrire_plugin_actifs(array('AGENDA'=>substr(rtrim(_DIR_PLUGIN_AGENDA,'/'),strlen(_DIR_PLUGINS))),false,'enleve');
+  	ecrire_meta('plugin_erreur_activation','Agenda necessite le vrai SPIP-Bonux');
+  	//die('echec:'.md5($contenu).":"._BONUX_CSS_MD5_FORMULAIRES);	
+  }
+}
 ?>
