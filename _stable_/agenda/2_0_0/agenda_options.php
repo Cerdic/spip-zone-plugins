f3830f596ce486121c8e06c030c29e95498c63ff
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-12-12 07:25:55 +0000

    "ca ne sert a rien"
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@25132 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/agenda/2_0_0/agenda_options.php
+++ _stable_/agenda/2_0_0/agenda_options.php
@@ -21,9 +21,7 @@ if (test_espace_prive()
   AND _request('exec')=='admin_plugin'){
   include_spip('spip_bonux_fonctions');
   // verifier qu'on a bien le bon bonux, avec la css qui marche
-  if (!file_exists($f=(_DIR_PLUGIN_SPIP_BONUX.'style_prive_formulaires.html'))
-  OR !lire_fichier($f,$contenu)
-  OR md5($contenu)!==_BONUX_CSS_MD5_FORMULAIRES){
+  if (!file_exists($f=(_DIR_PLUGIN_SPIP_BONUX.'style_prive_formulaires.html'))){
   	include_spip('inc/plugin');
   	ecrire_plugin_actifs(array('AGENDA'=>substr(rtrim(_DIR_PLUGIN_AGENDA,'/'),strlen(_DIR_PLUGINS))),false,'enleve');
   	ecrire_meta('plugin_erreur_activation','Agenda necessite le vrai SPIP-Bonux');
