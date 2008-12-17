de354bc0784d226373101d9fc671690e4bc95fde
bernard.blazin@free.fr <bernard.blazin@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-12-17 18:05:47 +0000

    Mise à jour vers les nouvelles icones
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@25271 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/Association/Association_2.0/exec/association.php
+++ _test_/Association/Association_2.0/exec/association.php
@@ -10,6 +10,12 @@
 	*  
 	**/
 	
+	//juste pour essai
+	sql_alter("TABLE spip_auteurs_elargis ADD statut_interne TEXT  NOT NULL");
+	spip_query("UPDATE spip_auteurs_elargis SET statut_interne='ok'");
+	sql_alter("TABLE spip_auteurs_elargis ADD categorie TEXT  NOT NULL");
+	sql_alter("TABLE spip_auteurs_elargis ADD validite DATE DEFAULT '2008-12-15' NOT NULL");
+	//fin essai
 	include_spip('inc/presentation');
 	include_spip('inc/gestion_base');
 	include_spip ('inc/navigation_modules');
@@ -32,9 +38,9 @@
 		echo propre(_T('asso:info_doc'));  	
 		echo fin_boite_info(true);
 		
-		$res=icone_horizontale(_L('asso:Profil de l\'association'), '?exec=cfg&cfg=association', '../'._DIR_PLUGIN_ASSOCIATION.'/img_pack/ecole.gif', 'rien.gif',false);
-			$res.=icone_horizontale(_L('asso:Cat&eacute;gories de cotisations'), generer_url_ecrire("categories"), '../'._DIR_PLUGIN_ASSOCIATION.'/img_pack/calculatrice.gif', '',false);
-	$res.=icone_horizontale(_L('asso:plan_comptable'), generer_url_ecrire("plan"), '../'._DIR_PLUGIN_ASSOCIATION.'/img_pack/EuroOff.gif', '',false);	
+		$res=icone_horizontale(_T('asso:Profil de l\'association'), '?exec=cfg&cfg=association', '../'._DIR_PLUGIN_ASSOCIATION.'/img_pack/assoc_qui.png', 'rien.gif',false);
+			$res.=icone_horizontale(_T('asso:Cat&eacute;gories de cotisations'), generer_url_ecrire("categories"), '../'._DIR_PLUGIN_ASSOCIATION.'/img_pack/cotisation.png', '',false);
+	$res.=icone_horizontale(_T('asso:plan_comptable'), generer_url_ecrire("plan"), '../'._DIR_PLUGIN_ASSOCIATION.'/img_pack/plan_compte.png', '',false);	
 	echo bloc_des_raccourcis($res);
 		
 		echo debut_droite("",true);	
