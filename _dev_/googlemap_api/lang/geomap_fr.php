49d9f417df779954ba634e366ff7b2ac63badb41
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-12-19 17:07:49 +0000

    On ne concatène le numéro de version que si on utilise la version de map adéquate... Validité xhtml du cfg + le champs de numéro de version de map affiché si c'est le type choisi... Des liens de type spip_out avec traitement en js pour éviter les horribles target="blank" dans le fichier de langue... Modification de locutions dans le fichier de langue
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@25336 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/googlemap_api/lang/geomap_fr.php
+++ _dev_/googlemap_api/lang/geomap_fr.php
@@ -13,15 +13,16 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'conseguir' => '(obtenir)',
 	'default_geoloc' => 'Position par d&eacute;faut des cartes :',
 	'desactiver_custom_control' => 'D&eacute;sactiver custom_control.js',
-	'explication_api_version' => 'API Version <a href="http://code.google.com/apis/maps/documentation/index.html#API_Updates" target="_blank">info</a>:',
-	'explication_cle' => 'Google Map API Key : <a href="http://www.google.com/apis/maps/signup.html" target="_blank" >Obtenir une cl&eacute;</a>',
+	'explication_api_version' => 'API Version <a href="http://code.google.com/apis/maps/documentation/index.html#API_Updates" class="spip_out">info</a>:',
+	'explication_cle' => 'Google Map API Key : <a href="http://www.google.com/apis/maps/signup.html" class="spip_out">Obtenir une cl&eacute;</a>',
 	'explication_custom_control' => 'D&eacute;sactiver les contr&ocirc;les personnalis&eacute;s :',
 	'latitude' => 'Latitude',
 	'longitude' => 'Longitude',
 	'miseajour' => 'Mise &agrave; jour',
 	'pages_public' => 'Pages publiques',
 	'rechercher' => 'Rechercher',
-	'zoom' => 'Zoom'
+	'zoom' => 'Zoom',
+	'configuration_titre' => 'Configuration du plugin GoogleMap Api'
 );
 
 ?>
