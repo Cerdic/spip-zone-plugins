d33e96c6818ea157275c23e231a8570036c14a9c
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-12-21 22:55:57 +0000

    langues (tjpzoom)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@25379 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _stable_/imagezoom/lang/tjpzoom_de.php
@@ -0,0 +1,30 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// D
+	'descriptif' => 'Auf dieser Seite wird der Stil der Lupe eingestellt. Ein individueller Stil kann angegebn werden - siehe <a href="http://plugins.spip.net/?page=article&amp;id_article=48">Dokumentation (fr)</a>.',
+
+	// E
+	'example' => 'Grafisches Ergebnis',
+
+	// N
+	'nostyle' => 'Ohne Stil',
+
+	// O
+	'other' => 'Andere',
+
+	// S
+	'size_vignette' => 'Maximale Gr&ouml;&szlig;e des Thumbnails',
+	'style' => 'Stil der Lupe',
+
+	// T
+	'titre' => 'Konfiguration der Lupe'
+);
+
+?>
