fb5779ea15fd452058ee89b2f3a9e7fd02fdcf5b
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-12-23 20:01:00 +0000

    celle la etait facile, elle utilisait deja l'api !
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@25456 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/forms/forms_et_tables_2_0_0/core/action/table_donnee_deplace.php
+++ _stable_/forms/forms_et_tables_2_0_0/core/action/table_donnee_deplace.php
@@ -13,57 +13,51 @@
 include_spip('inc/forms');
 include_spip('inc/autoriser');
 
-function action_table_donnee_deplace(){
-	global $auteur_session;
-	$args = _request('arg');
-	$hash = _request('hash');
-	$id_auteur = $auteur_session['id_auteur'];
-	$redirect = urldecode(_request('redirect'));
-	if (!include_spip("inc/securiser_action"))
-		include_spip("inc/actions");
-	if (verifier_action_auteur("table_donnee_deplace-$args",$hash,$id_auteur)==TRUE) {
-		$args = explode("-",$args);
-		$id_form = $args[0];
-		$id_donnee = $args[1];
-		$ordre = _request('ordre');
-		$rang_nouv = 0;
-		if ($ordre){
-			$table_sort = explode("&",$ordre);
-			$last_rang = 0;
-			foreach($table_sort as $item){
-				$item = explode("=",$item);
-				$item = explode("-",end($item));
-				$donnees[] = reset($item);
-				$rangs[] = end($item);
-				if (($n = count($rangs))>=2){
-					if ($rangs[$n-1]<$rangs[$n-2]){ // irregularite
-						if ($n>=3){
-							if ($rangs[$n-1]<$rangs[$n-3]) 
-								{$id_donnee = $donnees[$n-1];$rang_nouv = $rangs[$n-2];}
-							else
-								{$id_donnee = $donnees[$n-2];$rang_nouv = $rangs[$n-1];}
-						}
+function action_table_donnee_deplace_dist(){
+	$securiser_action = charger_fonction('securiser_action','inc');
+	$args = $securiser_action();
+
+	$args = explode("-",$args);
+	$id_form = $args[0];
+	$id_donnee = $args[1];
+	$ordre = _request('ordre');
+	$rang_nouv = 0;
+	// cas ou on a passe une liste d'id ordonnes
+	if ($ordre){
+		$table_sort = explode("&",$ordre);
+		$last_rang = 0;
+		foreach($table_sort as $item){
+			$item = explode("=",$item);
+			$item = explode("-",end($item));
+			$donnees[] = reset($item);
+			$rangs[] = end($item);
+			if (($n = count($rangs))>=2){
+				if ($rangs[$n-1]<$rangs[$n-2]){ // irregularite
+					if ($n>=3){
+						if ($rangs[$n-1]<$rangs[$n-3]) 
+							{$id_donnee = $donnees[$n-1];$rang_nouv = $rangs[$n-2];}
 						else
 							{$id_donnee = $donnees[$n-2];$rang_nouv = $rangs[$n-1];}
-						continue;
 					}
+					else
+						{$id_donnee = $donnees[$n-2];$rang_nouv = $rangs[$n-1];}
+					continue;
 				}
 			}
-			if ($rang_nouv)
-				if (autoriser('modifier','donnee',$id_donnee,NULL,array('id_form'=>$id_form))){
-					include_spip("base/forms_base_api");
-					Forms_ordonner_donnee($id_donnee,$rang_nouv);
-				}
 		}
-		else {
+		if ($rang_nouv)
 			if (autoriser('modifier','donnee',$id_donnee,NULL,array('id_form'=>$id_form))){
-				$rang_nouv = _request('rang_nouv');
 				include_spip("base/forms_base_api");
 				Forms_ordonner_donnee($id_donnee,$rang_nouv);
 			}
+	}
+	else {
+		if (autoriser('modifier','donnee',$id_donnee,NULL,array('id_form'=>$id_form))){
+			$rang_nouv = _request('rang_nouv');
+			include_spip("base/forms_base_api");
+			Forms_ordonner_donnee($id_donnee,$rang_nouv);
 		}
 	}
-	redirige_par_entete(str_replace("&amp;","&",$redirect));
 }
 
 ?>
