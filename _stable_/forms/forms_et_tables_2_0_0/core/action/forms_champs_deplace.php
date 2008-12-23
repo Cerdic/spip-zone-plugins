9b5a0f55c3db1d83013b307c0482d162cc0980cc
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-12-23 18:02:03 +0000

    une action
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@25444 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/forms/forms_et_tables_2_0_0/core/action/forms_champs_deplace.php
+++ _stable_/forms/forms_et_tables_2_0_0/core/action/forms_champs_deplace.php
@@ -14,35 +14,28 @@ include_spip('inc/forms');
 include_spip('inc/autoriser');
 
 function action_forms_champs_deplace(){
-	global $auteur_session;
-	$args = _request('arg');
-	$hash = _request('hash');
-	$id_auteur = $auteur_session['id_auteur'];
-	$redirect = _request('redirect');
-	if ($redirect==NULL) $redirect="";
-	if (!include_spip("inc/securiser_action"))
-		include_spip("inc/actions");
-	if (verifier_action_auteur("forms_champs_deplace-$args",$hash,$id_auteur)==TRUE) {
-			$args = explode("-",$args);
-			$id_form = $args[0];
-			$champ = $args[1];
-			$action = $args[2];
-			if (autoriser('modifier','form',$id_form)){
-				// Monter / descendre un champ
-				if (($action == 'monter') OR ($action == 'descendre')) {
-					if ($row = spip_fetch_array(spip_query("SELECT rang FROM spip_forms_champs WHERE id_form="._q($id_form)." AND champ="._q($champ)))) {
-						$rang1 = intval($row['rang']);
-						if ($action == 'monter') $order = "rang<$rang1 ORDER BY rang DESC";
-						else $order = "rang>$rang1 ORDER BY rang";
-						if (($row = spip_fetch_array(spip_query("SELECT rang FROM spip_forms_champs WHERE id_form="._q($id_form)." AND $order LIMIT 0,1")))){
-							$rang2 = intval($row['rang']);
-							spip_query("UPDATE spip_forms_champs SET rang=$rang1+$rang2-rang WHERE id_form="._q($id_form)." AND rang IN ($rang1,$rang2)");
-						}
-					}
-				}
+	$securiser_action = charger_fonction('securiser_action','inc');
+	$args = $securiser_action();
+	
+	$args = explode("-",$args);
+	$id_form = intval($args[0]);
+	$champ = $args[1];
+	$action = $args[2];
+	if (autoriser('modifier','form',$id_form)
+		// Monter / descendre un champ
+	  AND in_array($action,array('monter','descendre'))){
+		if ($row = sql_fetsel("rang","spip_forms_champs","id_form=".intval($id_form)." AND champ=".sql_quote($champ))) {
+			$rang1 = intval($row['rang']);
+			if ($action == 'monter')
+				$row = sql_fetsel("rang","spip_forms_champs","id_form=".intval($id_form)." AND rang<$rang1","","rang DESC","0,1");
+			else
+				$row = sql_fetsel("rang","spip_forms_champs","id_form=".intval($id_form)." AND rang>$rang1","","rang","0,1");
+			if ($row){
+				$rang2 = intval($row['rang']);
+				sql_update("spip_forms_champs",array("rang"=>"$rang1+$rang2-rang"),"id_form=".intval($id_form)." AND". sql_in("rang",array($rang1,$rang2)));
 			}
+		}
 	}
-	redirige_par_entete(str_replace("&amp;","&",urldecode($redirect)));
 }
 
 ?>
