463aebb3f06cc575d31039ad03aba9cff732f0f2
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-12-23 18:06:15 +0000

    une action
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@25446 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/forms/forms_et_tables_2_0_0/core/action/forms_donnee_supprime.php
+++ _stable_/forms/forms_et_tables_2_0_0/core/action/forms_donnee_supprime.php
@@ -11,26 +11,20 @@
  *
  */
 include_spip('inc/forms');
+include_spip('inc/autoriser');
 
 function action_forms_donnee_supprime(){
-	global $auteur_session;
-	$arg = _request('arg');
-	$hash = _request('hash');
-	$id_auteur = $auteur_session['id_auteur'];
-	$redirect = _request('redirect');
-	if ($redirect==NULL) $redirect="";
-	if (!include_spip("inc/securiser_action"))
-		include_spip("inc/actions");
-	if (verifier_action_auteur("forms_donnee_supprime-$arg",$hash,$id_auteur)==TRUE) {
-		list($id_form,$id_donnee) = explode(':',$arg);
-		include_spip('inc/autoriser');
-		if (autoriser('supprimer','donnee',$id_donnee,NULL,array('id_form'=>$id_form))){
-			if ($result = spip_query("DELETE FROM spip_forms_donnees WHERE id_form="._q($id_form)." AND id_donnee="._q($id_donnee)))
-				$result = spip_query("DELETE FROM spip_forms_donnees_champs WHERE id_donnee="._q($id_donnee));
-		}
+	$securiser_action = charger_fonction('securiser_action','inc');
+	$args = $securiser_action();
+
+	list($id_form,$id_donnee) = explode(':',$args);
+	$id_form = intval($id_form);
+	$id_donnee = intval($id_donnee);
+	if (autoriser('supprimer','donnee',$id_donnee,NULL,array('id_form'=>$id_form))){
+		if (sql_delete("spip_forms_donnees","id_form=".intval($id_form)." AND id_donnee=".intval($id_donnee)))
+			sql_delete("spip_forms_donnees_champs","id_donnee=".intval($id_donnee));
 	}
-	if ($redirect)
-		redirige_par_entete(str_replace("&amp;","&",urldecode($redirect)));
+
 }
 
 ?>
