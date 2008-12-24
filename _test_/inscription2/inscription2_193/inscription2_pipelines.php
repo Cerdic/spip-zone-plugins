9b53c267e6b2140828a92df035bbb20367bc08a9
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-12-24 17:49:42 +0000

    On enlève la dépendance à jquery validate car c'est CVT qui se charge de la validité des contenus dans le formulaire public ... Du coup on utilise un pipeline de moins On ne traite que les champs de spip_auteurs_elargis lors de la soumission du formulaire de modification des auteurs dans le privé
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@25501 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/inscription2/inscription2_193/inscription2_pipelines.php
+++ _test_/inscription2/inscription2_193/inscription2_pipelines.php
@@ -3,7 +3,7 @@
 // Ajoute le bouton d'amin aux webmestres
 
 if (!defined("_ECRIRE_INC_VERSION")) return;
-function Inscription2_ajouter_boutons($boutons_admin){
+function inscription2_ajouter_boutons($boutons_admin){
 	if ($GLOBALS['connect_statut'] == "0minirezo" && $GLOBALS["connect_toutes_rubriques"]) {
 
 		$boutons_admin['auteurs']->sousmenu['inscription2_adherents']= new Bouton(
@@ -14,7 +14,7 @@ function Inscription2_ajouter_boutons($boutons_admin){
 	return $boutons_admin;
 }
 
-function Inscription2_affiche_milieu($flux){
+function inscription2_affiche_milieu($flux){
 	switch($flux['args']['exec']) {	
 			case 'auteur_infos':
 				include_spip('inc/inscription2_gestion');
@@ -27,30 +27,12 @@ function Inscription2_affiche_milieu($flux){
 	return $flux;
 }
 
-function Inscription2_header_prive($flux){
+function inscription2_header_prive($flux){
 	if ((_request('exec')=='ajouter_adherent') || (_request('exec')=='inscription2_adherents')){
 		$flux .= "<script type='text/javascript' src='".find_in_path('lib/jquery-validate/jquery.validate.pack.js')."'></script>\n";
 		$flux .= "<script type='text/javascript' src='"._DIR_PLUGIN_INSCRIPTION2."javascript/md5_inscription2.js'></script>\n";
 	}
 	return $flux;
-}
-
-function inscription2_affichage_final($page){
-	// regarder si la page contient le formulaire inscription2
-	if (!strpos($page, 'id="inscription"'))
-		return $page;
-	$page = inscription2_preparer_page($page);
-		return $page;
-}
-
-function inscription2_preparer_page($page) {
-
-	$jqueryvalidate = find_in_path('lib/jquery-validate/jquery.validate.pack.js');
-
-	$incHead = <<<EOS
-<script type='text/javascript' src='$jqueryvalidate'></script>
-EOS;
-	return substr_replace($page, $incHead, strpos($page, '</head>'), 0);
 }
 
 // ajouter les champs I2 sur le formulaire CVT editer_auteur
@@ -73,19 +55,17 @@ function inscription2_post_edition($flux){
 				foreach(lire_config('inscription2',array()) as $cle => $val){
 					if($val!='' and !ereg("^(accesrestreint|categories|zone|news).*$", $cle)){
 						$cle = ereg_replace("_(obligatoire|fiche|table).*$", "", $cle);
-						if($cle == 'nom' or $cle == 'email' or $cle == 'login')
-							$var_user['a.'.$cle] = sql_quote(_request($cle));
-						elseif($cle == 'statut_nouveau'){
+						if($cle == 'nom' or $cle == 'email' or $cle == 'login' or $cle == 'statut_nouveau'){
 						}
 						else
-							$var_user['b.'.$cle] = sql_quote(_request($cle));
+							$var_user[$cle] = sql_quote(_request($cle));
 					}
 				}
 				if (!sql_getfetsel('id_auteur','spip_auteurs_elargis','id_auteur='.$id_auteur)){
 					//insertion de l'id_auteur dans spip_auteurs_elargis sinon on peut pas proceder a l'update
 					$id_elargi = sql_insertq("spip_auteurs_elargis",array('id_auteur'=> $id_auteur));
 				}
-				sql_update("spip_auteurs a left join spip_auteurs_elargis b on a.id_auteur=b.id_auteur",$var_user, "b.id_auteur=$id_auteur");
+				sql_update("spip_auteurs_elargis",$var_user,"id_auteur=$id_auteur");
 			
 			// Notifications, gestion des revisions, reindexation...
 			pipeline('post_edition',
