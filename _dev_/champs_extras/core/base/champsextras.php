ab6d3c27df7df923e4f36cc558abdfcda9378a30
marcimat@free.fr <marcimat@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-12-27 14:24:02 +0000

    Eviter les pages blanches au renouvellement de l'aléa (solution temporaire ?)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@25549 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/champs_extras/core/base/champsextras.php
+++ _dev_/champs_extras/core/base/champsextras.php
@@ -4,6 +4,14 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 function champsextras_declarer_tables_principales($tables_principales){
 	// pouvoir utiliser la class ChampExtra
 	include_spip('inc/champsextras');
+	
+	// lors du renouvellement de l'alea, au demarrage de SPIP
+	// les chemins de plugins ne sont pas encore connus.
+	// il faut se mefier et charger tout de meme la fonction, sinon page blanche.
+	if (!function_exists('declarer_champs_extras')) {
+		include_once(dirname(__file__).'/../inc/champsextras.php');
+	}
+	
 	// recuperer les champs crees par les plugins
 	$champs = pipeline('declarer_champs_extras', array());
 	// ajouter les champs au tableau spip
--- _dev_/champs_extras/extensions/interface/base/iextra.php
+++ _dev_/champs_extras/extensions/interface/base/iextra.php
@@ -3,6 +3,14 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 
 function iextra_declarer_champs_extras($champs=array()) {
 	include_spip('inc/iextra');
+	
+	// lors du renouvellement de l'alea, au demarrage de SPIP
+	// les chemins de plugins ne sont pas encore connus.
+	// il faut se mefier et charger tout de meme la fonction, sinon page blanche.
+	if (!function_exists('iextra_get_extras')) {
+		include_once(dirname(__file__).'/../inc/iextra.php');
+	}
+	
 	$extras = iextra_get_extras();
 	foreach($extras as $e) {
 		$champs[] = new ChampExtra($e);
