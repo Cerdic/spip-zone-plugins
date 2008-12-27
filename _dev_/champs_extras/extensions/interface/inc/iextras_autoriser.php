65487ee7eb07ee59dd44e745712021613664716a
marcimat@free.fr <marcimat@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-12-27 17:53:41 +0000

    Autorisations dans le pipeline adéquat !
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@25558 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/champs_extras/extensions/interface/inc/iextras_autoriser.php
+++ _dev_/champs_extras/extensions/interface/inc/iextras_autoriser.php
@@ -1,4 +1,10 @@
 <?php
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+// fonction pour le pipeline, n'a rien a effectuer
+function iextras_autoriser(){}
+
+// declarations d'autorisations
 function autoriser_iextras_onglet_dist($faire, $type, $id, $qui, $opt) {
 	return autoriser('configurer', 'iextra', $id, $qui, $opt);
 }
