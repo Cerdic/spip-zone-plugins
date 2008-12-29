8a184b8213fc28abe46a5c0669b93910e0f4ee64
patfr@ifrance.com <patfr@ifrance.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2008-12-29 12:22:10 +0000

    Etranges parenthèses...
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@25599 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/couteau_suisse/cout_utils.php
+++ _stable_/couteau_suisse/cout_utils.php
@@ -261,9 +261,10 @@ function cs_sauve_configuration() {
 include_spip('inc/charset');
 $sauve .= $temp = "\n######## "._T('couteauprive:pack_actuel_titre')." #########\n\n// "
 	. filtrer_entites(_T('couteauprive:pack_actuel_avert')."\n\n"
-		. "\$GLOBALS['cs_installer']['"._T('couteauprive:pack_actuel', array('date'=>cs_date())."'] = array(\n\n\t// "._T('couteauprive:pack_outils_defaut')."\n"
+		. "\$GLOBALS['cs_installer']['"._T('couteauprive:pack_actuel', array('date'=>cs_date()))."'] = array(\n\n\t// "
+		. _T('couteauprive:pack_outils_defaut')."\n"
 		. "\t'outils' =>\n\t\t'".join(",\n\t\t", $actifs)."',\n"
-		. "\n\t// "._T('couteauprive:pack_variables_defaut')."\n"))
+		. "\n\t// "._T('couteauprive:pack_variables_defaut')."\n")
 	. "\t'variables' => array(\n\t" . join(",\n\t", $metas_actifs) . "\n\t)\n);\n";
 
 	ecrire_fichier(_DIR_CS_TMP.'config.php', '<'."?php\n// Configuration de controle pour le plugin 'Couteau Suisse'\n\n$sauve?".'>');
