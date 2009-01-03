125e379a51d415b5ebb77bb8d01e477e906ffa71
sebbou@gmail.com <sebbou@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-01-03 07:52:52 +0000

    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@25697 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _dev_/lang/picasa_en.php
@@ -0,0 +1,34 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'agrandir' => 'Open image',
+	'appareil' => 'Taken with:',
+
+	// C
+	'chargement' => 'Loading...',
+	'commentaires' => 'Comments :',
+	
+	// D
+
+	// E
+
+	// M
+	'miniatures' => 'Pictures',
+
+	// N
+
+	// P
+
+	// S
+
+	// V
+);
+
+?>
