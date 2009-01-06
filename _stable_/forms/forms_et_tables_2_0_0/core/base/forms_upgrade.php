d964ffa47c98daa279628087dcf34a9626e43987
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-01-06 16:42:15 +0000

    "un bug sur les mises a jour et les numero de version base"
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@25785 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/forms/forms_et_tables_2_0_0/core/base/forms_upgrade.php
+++ _stable_/forms/forms_et_tables_2_0_0/core/base/forms_upgrade.php
@@ -82,10 +82,18 @@ function forms_allstructure2table($clean=false){
  * @param string $version_cible
  */
 function forms_upgrade($nom_meta_base_version,$version_cible){
-	$current_version = 0.0;
+	$current_version = '0.0';
 	if (   (!isset($GLOBALS['meta'][$nom_meta_base_version]) )
 			|| (($current_version = $GLOBALS['meta'][$nom_meta_base_version])!=$version_cible)){
 
+		// bug sur l'ecriture numerique anterieure des vieilles versions
+		if ($current_version == 0.2)
+			$current_version = '0.20';
+		if ($current_version == 0.3)
+			$current_version = '0.30';
+		if ($current_version == 0.4)
+			$current_version = '0.40';
+		
 		include_spip('base/serial');
 		include_spip('base/aux');
 		if (version_compare($current_version,'0.0','<=')){
@@ -93,14 +101,14 @@ function forms_upgrade($nom_meta_base_version,$version_cible){
 			include_spip('base/abstract_sql');
 			// attention on vient peut etre d'une table spip-forms 1.8
 			if ($desc = sql_showtable("spip_forms"))
-				$current_version=0.1;
+				$current_version='0.10';
 			else {
 				creer_base();
 				ecrire_meta($nom_meta_base_version,$current_version=$version_cible);
 				ecrire_meta('forms_et_tables',serialize(array('associer_donnees_articles'=>0,'associer_donnees_rubriques'=>0,'associer_donnees_auteurs'=>0)));
 			}
 		}
-		if (version_compare($current_version,0.11,'<')){
+		if (version_compare($current_version,'0.11','<')){
 			include_spip('base/create');
 			include_spip('base/abstract_sql');
 			creer_base();
@@ -114,9 +122,9 @@ function forms_upgrade($nom_meta_base_version,$version_cible){
 					sql_update("spip_forms",array('email'=>"'".addslashes($email)."'"),"id_form=".intval($id_form));
 				}
 			}
-			ecrire_meta($nom_meta_base_version,$current_version=0.11);
+			ecrire_meta($nom_meta_base_version,$current_version='0.11');
 		}
-		if (version_compare($current_version,0.12,'<')){
+		if (version_compare($current_version,'0.12','<')){
 			include_spip('base/create');
 			include_spip('base/abstract_sql');
 			creer_base();
@@ -124,22 +132,22 @@ function forms_upgrade($nom_meta_base_version,$version_cible){
 			sql_alter("TABLE spip_forms CHANGE schema schema TEXT");
 			sql_alter("TABLE spip_forms CHANGE email email TEXT");
 			sql_alter("TABLE spip_forms CHANGE texte texte TEXT");
-			ecrire_meta($nom_meta_base_version,$current_version=0.12);
+			ecrire_meta($nom_meta_base_version,$current_version='0.12');
 		}
-		if (version_compare($current_version,0.13,'<')){
+		if (version_compare($current_version,'0.13','<')){
 			sql_alter("TABLE spip_forms CHANGE schema structure TEXT");
-			ecrire_meta($nom_meta_base_version,$current_version=0.13);
+			ecrire_meta($nom_meta_base_version,$current_version='0.13');
 		}
-		if (version_compare($current_version,0.14,'<')){
+		if (version_compare($current_version,'0.14','<')){
 			sql_alter("TABLE spip_reponses ADD id_article_export BIGINT( 21 ) NOT NULL AFTER id_auteur ");
-			ecrire_meta($nom_meta_base_version,$current_version=0.14);
+			ecrire_meta($nom_meta_base_version,$current_version='0.14');
 		}
-		if (version_compare($current_version,0.15,'<')){
+		if (version_compare($current_version,'0.15','<')){
 			sql_alter("TABLE spip_reponses ADD url VARCHAR(255) NOT NULL AFTER id_article_export ");
-			ecrire_meta($nom_meta_base_version,$current_version=0.15);
+			ecrire_meta($nom_meta_base_version,$current_version='0.15');
 		}
 		// maj en version 0.16 annulee et remplacee par 0.17
-		if (version_compare($current_version,0.17,'<')){
+		if (version_compare($current_version,'0.17','<')){
 			if (sql_count(sql_select("structure","spip_forms"))){
 					// virer les tables temporaires crees manuellement sur les serveurs ou ca foirait
 					sql_drop_table("spip_forms_champs");
@@ -187,9 +195,9 @@ function forms_upgrade($nom_meta_base_version,$version_cible){
 				sql_alter("TABLE spip_forms_donnees ADD statut VARCHAR(10) NOT NULL AFTER confirmation");
 				sql_update("spip_forms_donnees",array("statut"=>"'publie'")); // par securite
 			}
-			ecrire_meta($nom_meta_base_version,$current_version=0.17);
+			ecrire_meta($nom_meta_base_version,$current_version='0.17');
 		}
-		if (version_compare($current_version,0.18,'<')){
+		if (version_compare($current_version,'0.18','<')){
 			sql_alter("TABLE spip_forms ADD linkable ENUM('non', 'oui') DEFAULT 'non' NOT NULL AFTER public");
 
 			// init la valeur par defaut de extra_info sur les champs select (aurait du etre fait en 0.17
@@ -203,41 +211,41 @@ function forms_upgrade($nom_meta_base_version,$version_cible){
 					sql_update("spip_forms_champs",array("extra_info"=>"'".addslashes($extra_info)."'"),"id_form=".intval($row['id_form'])." AND champ=".sql_quote($row['champ']));
 				}
 			}
-			ecrire_meta($nom_meta_base_version,$current_version=0.18);
+			ecrire_meta($nom_meta_base_version,$current_version='0.18');
 		}
-		if (version_compare($current_version,0.19,'<')){
+		if (version_compare($current_version,'0.19','<')){
 			sql_alter("TABLE spip_forms ADD html_wrap text AFTER linkable");
-			ecrire_meta($nom_meta_base_version,$current_version=0.19);
+			ecrire_meta($nom_meta_base_version,$current_version='0.19');
 		}
-		if (version_compare($current_version,0.20,'<')){
+		if (version_compare($current_version,'0.20','<')){
 			sql_alter("TABLE spip_forms_champs CHANGE champ champ varchar(100) NOT NULL");
 			sql_alter("TABLE spip_forms_champs_choix CHANGE champ champ varchar(100) NOT NULL");
 			// on rappelle creer base car la creation de forms_champs et forms_champs_choix a pu echouer sur mysql 3.23
 			include_spip('base/create');
 			include_spip('base/abstract_sql');
 			creer_base();
-			ecrire_meta($nom_meta_base_version,$current_version=0.20);
+			ecrire_meta($nom_meta_base_version,$current_version='0.20');
 		}
-		if (version_compare($current_version,0.21,'<')){
+		if (version_compare($current_version,'0.21','<')){
 			sql_alter("TABLE spip_forms ADD forms_obligatoires VARCHAR(255) DEFAULT '' AFTER type_form");
 			sql_alter("TABLE spip_forms ADD modifiable ENUM('non', 'oui') DEFAULT 'non' AFTER type_form");
 			sql_alter("TABLE spip_forms ADD multiple ENUM('non', 'oui') DEFAULT 'non' AFTER type_form");
-			ecrire_meta($nom_meta_base_version,$current_version=0.21);
+			ecrire_meta($nom_meta_base_version,$current_version='0.21');
 		}
-		if (version_compare($current_version,0.22,'<')){
+		if (version_compare($current_version,'0.22','<')){
 			// creer toutes la nouvelle table spip_documents_donnees
 			include_spip('base/forms');
 			include_spip('base/create');
 			include_spip('base/abstract_sql');
 			creer_base();
 			ecrire_meta('documents_donnee','oui');
-			ecrire_meta($nom_meta_base_version,$current_version=0.22);
+			ecrire_meta($nom_meta_base_version,$current_version='0.22');
 		}
-		if (version_compare($current_version,0.23,'<')){
+		if (version_compare($current_version,'0.23','<')){
 			sql_alter("TABLE spip_forms ADD documents ENUM('non', 'oui') DEFAULT 'non' NOT NULL AFTER linkable");
-			ecrire_meta($nom_meta_base_version,$current_version=0.23);
+			ecrire_meta($nom_meta_base_version,$current_version='0.23');
 		}
-		if (version_compare($current_version,0.24,'<')){
+		if (version_compare($current_version,'0.24','<')){
 			if (!sql_select("rang","spip_forms_donnees")){
 				sql_alter("TABLE spip_forms_donnees ADD rang bigint(21) NOT NULL AFTER cookie");
 				$rows = sql_allfetsel("id_form","spip_forms");
@@ -248,31 +256,31 @@ function forms_upgrade($nom_meta_base_version,$version_cible){
 						sql_update("spip_forms_donnees",array("rang"=>$rang++),"id_donnee=".intval($row2['id_donnee']));
 				}
 			}
-			ecrire_meta($nom_meta_base_version,$current_version=0.24,'non');
+			ecrire_meta($nom_meta_base_version,$current_version='0.24','non');
 		}
-		if (version_compare($current_version,0.25,'<')){
+		if (version_compare($current_version,'0.25','<')){
 			include_spip('base/create');
 			include_spip('base/abstract_sql');
 			creer_base();
-			ecrire_meta($nom_meta_base_version,$current_version=0.25,'non');
+			ecrire_meta($nom_meta_base_version,$current_version='0.25','non');
 		}
-		if (version_compare($current_version,0.26,'<')){
+		if (version_compare($current_version,'0.26','<')){
 			include_spip('base/create');
 			include_spip('base/abstract_sql');
 			creer_base();
 			echo "forms update @ 0.26<br/>";
-			ecrire_meta($nom_meta_base_version,$current_version=0.26,'non');
+			ecrire_meta($nom_meta_base_version,$current_version='0.26','non');
 		}
-		if (version_compare($current_version,0.27,'<')){
+		if (version_compare($current_version,'0.27','<')){
 			sql_alter("TABLE spip_forms_donnees_articles ADD article_ref ENUM('non', 'oui') DEFAULT 'non' NOT NULL AFTER id_article");
 			sql_alter("TABLE spip_forms_donnees_donnees ADD donnee_ref ENUM('non', 'oui') DEFAULT 'non' NOT NULL AFTER id_donnee");
 			include_spip('base/create');
 			include_spip('base/abstract_sql');
 			creer_base();
 			echo "forms update @ 0.27<br/>";
-			ecrire_meta($nom_meta_base_version,$current_version=0.27,'non');
+			ecrire_meta($nom_meta_base_version,$current_version='0.27','non');
 		}
-		if (version_compare($current_version,0.29,'<')){
+		if (version_compare($current_version,'0.29','<')){
 			include_spip('base/create');
 			include_spip('base/abstract_sql');
 			creer_base();
@@ -280,55 +288,55 @@ function forms_upgrade($nom_meta_base_version,$version_cible){
 			sql_alter("TABLE spip_forms_donnees ADD bdte bigint(21) NOT NULL AFTER bgch");
 			sql_alter("TABLE spip_forms_donnees ADD niveau bigint(21) DEFAULT '0' NOT NULL AFTER bdte");
 			echo "forms update @ 0.29<br/>";
-			ecrire_meta($nom_meta_base_version,$current_version=0.29,'non');
+			ecrire_meta($nom_meta_base_version,$current_version='0.29','non');
 		}
-		if (version_compare($current_version,0.31,'<')){
+		if (version_compare($current_version,'0.31','<')){
 			sql_alter("TABLE spip_forms_champs ADD listable ENUM('non', 'oui') DEFAULT 'oui' NOT NULL AFTER specifiant");
 			echo "forms update @ 0.31<br/>";
-			ecrire_meta($nom_meta_base_version,$current_version=0.31,'non');
+			ecrire_meta($nom_meta_base_version,$current_version='0.31','non');
 		}
-		if (version_compare($current_version,0.32,'<')){
+		if (version_compare($current_version,'0.32','<')){
 			sql_alter("TABLE spip_forms_champs CHANGE listable listable_admin ENUM('non', 'oui') DEFAULT 'oui' NOT NULL");
 			if (!sql_getfetsel("listable","spip_forms_champs","","","","0,1")){
 				sql_alter("TABLE spip_forms_champs ADD listable ENUM('non', 'oui') DEFAULT 'oui' NOT NULL AFTER listable_admin");
 				sql_update("spip_forms_champs",array("listable"=>"'specifiant'")); // valeur par defaut pour iso fonctionnalite cote public
 			}
 			echo "forms update @ 0.32<br/>";
-			ecrire_meta($nom_meta_base_version,$current_version=0.32,'non');
+			ecrire_meta($nom_meta_base_version,$current_version='0.32','non');
 		}
-		if (version_compare($current_version,0.33,'<')){
+		if (version_compare($current_version,'0.33','<')){
 			sql_alter("TABLE spip_forms_donnees_champs CHANGE valeur valeur TEXT NOT NULL");
 			echo "forms update @ 0.33<br/>";
-			ecrire_meta($nom_meta_base_version,$current_version=0.33,'non');
+			ecrire_meta($nom_meta_base_version,$current_version='0.33','non');
 		}
-		if (version_compare($current_version,0.34,'<')){
+		if (version_compare($current_version,'0.34','<')){
 			sql_alter("TABLE spip_forms_donnees_champs DROP INDEX champ , ADD UNIQUE champ ( champ ( 128 ) , id_donnee , valeur ( 128 ) )");
 			echo "forms update @ 0.34<br/>";
-			ecrire_meta($nom_meta_base_version,$current_version=0.34,'non');
+			ecrire_meta($nom_meta_base_version,$current_version='0.34','non');
 		}
-		if (version_compare($current_version,0.35,'<')){
+		if (version_compare($current_version,'0.35','<')){
 			sql_alter("TABLE spip_forms ADD arborescent ENUM('non', 'oui') DEFAULT 'non' NOT NULL AFTER documents");
 			echo "forms update @ 0.35<br/>";
-			ecrire_meta($nom_meta_base_version,$current_version=0.35,'non');
+			ecrire_meta($nom_meta_base_version,$current_version='0.35','non');
 		}
-		if (version_compare($current_version,0.36,'<')){
+		if (version_compare($current_version,'0.36','<')){
 			include_spip('base/create');
 			include_spip('base/abstract_sql');
 			creer_base();
 			echo "forms update @ 0.36<br/>";
-			ecrire_meta($nom_meta_base_version,$current_version=0.36,'non');
+			ecrire_meta($nom_meta_base_version,$current_version='0.36','non');
 		}
-		if (version_compare($current_version,0.37,'<')){
+		if (version_compare($current_version,'0.37','<')){
 			sql_alter("TABLE spip_forms_champs ADD taille bigint(21) NOT NULL NULL AFTER type");
 			echo "forms update @ 0.37<br/>";
-			ecrire_meta($nom_meta_base_version,$current_version=0.37,'non');
+			ecrire_meta($nom_meta_base_version,$current_version='0.37','non');
 		}
-		if (version_compare($current_version,0.38,'<')){
+		if (version_compare($current_version,'0.38','<')){
 			ecrire_meta('forms_et_tables',serialize(array('associer_donnees_articles'=>1,'associer_donnees_rubriques'=>0,'associer_donnees_auteurs'=>0)));
 			echo "forms update @ 0.38<br/>";
-			ecrire_meta($nom_meta_base_version,$current_version=0.38,'non');
+			ecrire_meta($nom_meta_base_version,$current_version='0.38','non');
 		}
-		if (version_compare($current_version,0.39,'<')){
+		if (version_compare($current_version,'0.39','<')){
 			sql_alter("TABLE spip_forms_articles DROP INDEX id_form");
 			sql_alter("TABLE spip_forms_articles ADD PRIMARY KEY ( id_form , id_article )");
 			sql_alter("TABLE forms_donnees_articles DROP INDEX id_donnee");
@@ -340,17 +348,17 @@ function forms_upgrade($nom_meta_base_version,$version_cible){
 			sql_alter("TABLE forms_donnees_auteurs DROP INDEX id_donnee");
 			sql_alter("TABLE forms_donnees_auteurs ADD PRIMARY KEY ( id_donnee , id_auteur )");
 			echo "forms update @ 0.39<br/>";
-			ecrire_meta($nom_meta_base_version,$current_version=0.39,'non');
+			ecrire_meta($nom_meta_base_version,$current_version='0.39','non');
 		}
-		if (version_compare($current_version,0.40,'<')){
+		if (version_compare($current_version,'0.40','<')){
 			sql_alter("TABLE spip_forms_champs ADD saisie ENUM('non', 'oui') DEFAULT 'oui' NOT NULL AFTER public");
 			echo "forms update @ 0.40<br/>";
-			ecrire_meta($nom_meta_base_version,$current_version=0.40,'non');
+			ecrire_meta($nom_meta_base_version,$current_version='0.40','non');
 		}
-		if (version_compare($current_version,0.41,'<')){
+		if (version_compare($current_version,'0.41','<')){
 			sql_alter("TABLE spip_forms ADD documents_mail ENUM('non', 'oui') DEFAULT 'non' NOT NULL AFTER documents");
 			echo "forms update @ 0.41<br/>";
-			ecrire_meta($nom_meta_base_version,$current_version=0.41,'non');
+			ecrire_meta($nom_meta_base_version,$current_version='0.41','non');
 		}
 		
 		$trouver_table = charger_fonction("trouver_table","base");
