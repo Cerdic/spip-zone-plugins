08e7e48809cb838428d2a9455a30d3bf012766d3
patfr@ifrance.com <patfr@ifrance.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-01-06 13:47:27 +0000

    Traduction en allemand (Alexandra)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@25779 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _stable_/jeux/lang/jeux_de.php
@@ -0,0 +1,142 @@
+<?php
+
+// This is a SPIP language file
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+'votre_choix'    => "Ihre Wahl: ",
+'votre_reponse' => "Ihre Antwort: ",
+'corriger'        => "Korrigieren",
+'verifier'         => '&#220;berpr&#252;fen',
+'verifier_validite'     => 'Validit&#228;t &#252;berpr&#252;fen ',
+
+'afficher_solution'     =>'L&#246;sung anzeigen',
+'solution'        =>'L&#246;sung',
+'reponse'        =>'Antwort',
+'indices'        => 'Hinweise',
+'score'            => 'Ergebnis:',
+
+'point' => "&nbsp;Pkt",
+'points' => "&nbsp;Pkte",
+
+'ligne_n'        =>'Zeile @n@',
+'colonne_n'        =>'Spalte @n@',
+
+'reinitialiser'        => 'Nullstellen',
+'recommencer'        => 'Neustarten',
+'rejouer'        => 'Nochmals spielen',
+'reset'        => 'Nullstellen',
+
+'grille_valide'        =>'G&#252;ltiges Netz',
+'grille_invalide'    =>'Ung&#252;ltiges Netz',
+'aucune_erreur'        =>'Keine Fehler',
+'une_erreur'        => 'Ein Fehler',
+'n_erreurs'    => '@n@ Fehler',
+'n_vides'        => '@n@ leere Zellen',
+'une_vide'            => 'Eine leere Zelle',
+'bravo'            => "Herzlichen Gl&#252;ckwunsch!",
+
+'reponseJuste' => "Richtig!",
+'reponseFausse' => "Falsch!",
+'reponseNulle' => "Unbeantwortet!",
+'bonneReponse' => "Die richtige Antwort ist:",
+'Correction' => "L&#246;sung:",
+
+'erreur_taille_grille'    => "Fehler: Ung&#252;ltige Gr&#246;§e",
+'erreur_syntaxe'    => "Syntaxfehler",
+'erreur_spip'    => "Diese Spip Version ist zu alt f&#252;r dieses Spiel!",
+'erreur_scripts'    => "Skripts verboten: Dieses Spiel kann nicht ausgef&#252;hrt zerden!",
+
+// espace prive
+
+'jeux'		=> 'Jeux',
+'jeux_' 	=> 'Jeux&nbsp;:',
+'jeu_numero' 	=> 'Jeu num&eacute;ro @id@ (@nom@)',
+'jeu_court'	=> 'Jeu n&deg;@id@ (@nom@)',
+'jeu_numero_court' 	=> 'ID',
+'jeu_type'	=> 'Type',
+'jeu_titre'	=> 'Titre',
+'auteur'	=> 'Auteur',
+
+'nouveau_jeu'	=> 'Nouveau jeu',
+'explication_jeu'=> 'Le titre priv&eacute; sert simplement &agrave; identifier le jeu en partie priv&eacute;e.<br /><br />Le contenu du jeu doit suivre la syntaxe d&eacute;crite dans <a href="http://www.spip-contrib.net/Des-jeux-dans-vos-articles">la&nbsp;documentation</a> du plugin.<br /><br />Ici, les balises &lt;jeux> et &lt;/jeux> sont inutiles.',
+'modifier_jeu'	=> 'Modifier le jeu num&eacute;ro @id@ (@nom@)',
+'modifier_ce_jeu'	=> 'Modifier ce jeu',
+'voir_jeu'			=> 'Voir le jeu',
+'liste_jeux'			=> 'Liste des jeux',
+'retourner_jeu'		=> 'Retourner au jeu',
+'jeu_titre_prive'	=> 'Titre priv&eacute;',
+'jeu_titre_public'	=> 'Titre public',
+'jeu_titre_prive_'	=> 'Titre priv&eacute; :',
+'jeu_titre_public_'	=> 'Titre public :',
+'jeu_contenu'		=> 'Contenu du jeu',
+'jeu_vide'			=> 'VIDE',
+
+'derniere_modif'	=> 'Derni&egrave;re modification',
+'resultat'			=> 'R&eacute;sultat',
+'resultat_unique'	=> 'R&eacute;sultat unique',
+'resultats'			=> 'R&eacute;sultats',
+'resultats_auteur'	=> 'Les r&eacute;sultats de @nom@',
+'resultats_auteur_'	=> "R&eacute;sultats de l&#8217;auteur",
+'voir_ses_resultats'	=> 'Voir ses r&eacute;sultats',
+'resultats_jeu'		=> 'Les r&eacute;sultats du jeu num&eacute;ro @id@ (@nom@)',
+'resultats_jeu_'		=> 'R&eacute;sultats du jeu',
+'nb_resultats'		=> 'Nombre de r&eacute;sultats',
+'gerer_resultats'	=> 'G&eacute;rer les r&eacute;sultats',
+'gerer_ses_resultats'	=> 'G&eacute;rer ses r&eacute;sultats',
+'voir_resultats'	=> 'Voir les r&eacute;sultats',
+'enregistrer_resultat' => 'Enregistrer les r&eacute;sultats&nbsp;?',
+'gerer_resultats_jeu' => 'G&eacute;rer les r&eacute;sultats du jeu num&eacute;ro @id@ (@nom@)',
+'aucun_resultat' => 'Aucun r&eacute;sultat',
+
+'supprimer_tout_tout'			=> 'Effacer TOUS les r&eacute;sultats',
+'supprimer_tout_jeu'		=> 'Effacer TOUS les r&eacute;sultats du jeu',
+'supprimer_tout_auteur'		=> "Effacer TOUS les r&eacute;sultats de l&#8217;auteur",
+'supprimer_confirmer'	=> 'Confimer la suppression de ces r&eacute;sultats',
+'explication_supprimer_tout'=> 'Cette commande efface <i>tous</i> les r&eacute;sultats des diff&eacute;rents jeux&hellip;',
+'explication_supprimer_jeu'=> 'Cette commande efface <i>tous</i> les r&eacute;sultats du jeu&hellip;',
+'explication_supprimer_auteur'=> 'Cette commande efface <i>tous</i> les r&eacute;sultats de l&#8217;auteur&hellip;',
+'confirmation' => 'Confirmation',
+'confirmation_supprimer_tout' => '&Ecirc;tes vous r&eacute;ellement s&ucirc;r de vouloir effacer TOUS les r&eacute;sultats&nbsp;?',
+'confirmation_supprimer_jeu' => '&Ecirc;tes vous r&eacute;ellement s&ucirc;r de vouloir effacer TOUS les r&eacute;sultats du jeu&nbsp;?',
+'confirmation_supprimer_auteur' => '&Ecirc;tes vous r&eacute;ellement s&ucirc;r de vouloir effacer TOUS les r&eacute;sultats de l&#8217;auteur&nbsp;?',
+'gerer_resultats_auteur'	=> 'G&eacute;rer les r&eacute;sultats de @nom@',
+'infos_auteur'				=> 'Informations',
+'gerer_resultats_tout' 		=> 'G&eacute;rer les r&eacute;sultats de tous les jeux',
+'ecraser_resultat'			=> 'Ecraser les r&eacute;sultats&nbsp;?',
+'dernier_resultat'			=> 'Garder le dernier r&eacute;sultat',
+'meilleur_resultat'			=> 'Garder le meilleur r&eacute;sultat',
+'configurer_jeux'			=> 'Configurer le plugin Jeux',
+
+'sans_titre_prive'	=> 'Sans titre priv&eacute;',
+'sans_type'		=> 'Sans type',
+'introuvable'	=> 'Aucun jeu dans le contenu !',
+'pas_de_jeu'	=> "D&eacute;sol&eacute;, ce jeu n'existe pas !",
+'pas_d_auteur'	=> "D&eacute;sol&eacute;, cet auteur est inconnu !",
+'inserer_jeu'	=> "Ins&eacute;rer des jeux",
+'inserer_jeu_explication'	=>"Vous pouvez ins&eacute;rer des jeux en &eacute;crivant &#60;jeuyyy&#62;, o&ugrave; yyy est l'ID du jeu",
+
+'compacter_tout_tout'			=> 'Compacter TOUS les r&eacute;sultats',
+'compacter_tout_jeu'		=> 'Compacter TOUS les r&eacute;sultats du jeu',
+'compacter_tout_auteur'		=> "Compacter TOUS les r&eacute;sultats de l&#8217;auteur",
+'explication_compacter_tout'=>"Cette commande  efface <i>tous</i> les r&eacute;sultats des diff&eacute;rents jeux, sauf le dernier pour chaque couple jeu/auteur",
+'explication_compacter_jeu'=>"Cette commande  efface <i>tous</i> les r&eacute;sultats de ce jeu, sauf le dernier pour chaque auteur",
+'explication_compacter_auteur'=>"Cette commande  efface <i>tous</i> les r&eacute;sultats de cet auteur, sauf le dernier pour chaque jeu",
+'confirmation_compacter_tout' 	=> '&Ecirc;tes vous r&eacute;ellement s&ucirc;r de vouloir compacter TOUS les r&eacute;sultats&nbsp;?',
+'confirmation_compacter_auteur' 	=> '&Ecirc;tes vous r&eacute;ellement s&ucirc;r de vouloir compacter les r&eacute;sultats de cet auteur&nbsp;?',
+'confirmation_compacter_jeu' 	=> '&Ecirc;tes vous r&eacute;ellement s&ucirc;r de vouloir compacter les r&eacute;sultats de ce jeu&nbsp;?',
+'compacter_confirmer'			=> 'Confirmer le compactage de ces r&eacute;sultats',
+'statut_jeu'					=>'Statut du jeu&nbsp;: ',
+
+'doc_jeux' => 'Documentation générale',
+'doc_scores' => 'Documentation sur les scores',
+
+// Corbeille
+  'jeux_corbeille_tous' => "@nb@ jeux dans la corbeille",
+  'jeux_corbeille_un' => "1 jeu dans la corbeille",
+  'jeux_tous' => 'Tous les jeux',
+
+);
+
+
+?>
