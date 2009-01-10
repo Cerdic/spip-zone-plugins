86a4763558f5af5f7593050a8572889f4c73a34a
marcimat@free.fr <marcimat@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-01-10 14:00:47 +0000

    1 oups, 1 debug...
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@25869 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/bisous/base/bisous_tables.php
+++ _dev_/bisous/base/bisous_tables.php
@@ -32,28 +32,6 @@ function bisous_declarer_tables_principales($tables_principales){
 		'key' => &$spip_bisous_cles
 	);
 	
-/*	
-	$spip_bisous = array(
-		'id_bisou' => 'bigint(21) NOT NULL',
-		'id_donneur' => 'bigint(21) DEFAULT "0" NOT NULL',
-		"objet"	=> "VARCHAR (25) DEFAULT '' NOT NULL",
-		'id_objet' => 'bigint(21) DEFAULT "0" NOT NULL',
-		'date' => 'datetime DEFAULT "0000-00-00 00:00:00" NOT NULL'
-	);
-	
-	$spip_bisous_cles = array(
-		'PRIMARY KEY' => 'id_bisou',
-		'KEY id_donneur' => 'id_donneur',
-	);
-	
-	$tables_principales['spip_bisous'] = array(
-		'field' => &$spip_bisous,
-		'key' => &$spip_bisous_cles,
-		'join' => array(
-			'id_donneur'=>array('auteurs','id_auteur')
-		)
-	);	
-*/
 	return $tables_principales;
 
 }
