d9066c44e751bc5eb24cc048e7c6e723a8434ead
xebiaut@free.fr <xebiaut@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-01-11 16:57:39 +0000

    Correction petit bug dans l'appel du fichier CSS
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@25885 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/SPIP-album/album_pipeline.php
+++ _test_/SPIP-album/album_pipeline.php
@@ -21,8 +21,8 @@ function album_entete($public=true){
 	$entete ='
 
 <!-- '. _T('album:head_debut') . ' -->
-<!-- <link rel="stylesheet" href="'.direction_css(compacte(find_in_path('css/album.css'))).'" type="text/css" media="projection, screen, tv" /> -->
-<link rel="stylesheet" href="'.generer_url_public(find_in_path('css/album.css')).'" type="text/css" media="projection, screen, tv" />
+<link rel="stylesheet" href="'.direction_css(compacte(find_in_path('css/album.css'))).'" type="text/css" media="projection, screen, tv" />
+<!-- <link rel="stylesheet" href="'.generer_url_public(find_in_path('css/album.css')).'" type="text/css" media="projection, screen, tv" /> -->
 <script type="text/javascript" src="'.find_in_path('javascript/interface.js').'"></script>
 <script type="text/javascript">
 var init_ib = function(){
