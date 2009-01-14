b2d9740d576cd701e5f64a96a6bc90477068a39d
marcimat@free.fr <marcimat@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-01-14 12:15:33 +0000

    Coquille dans l'autorisation de l'onglet Champs Extras
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@25956 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/champs_extras2/extensions/interface/inc/iextras_autoriser.php
+++ _stable_/champs_extras2/extensions/interface/inc/iextras_autoriser.php
@@ -6,7 +6,7 @@ function iextras_autoriser(){}
 
 // declarations d'autorisations
 function autoriser_iextras_onglet_dist($faire, $type, $id, $qui, $opt) {
-	return autoriser('configurer', 'iextra', $id, $qui, $opt);
+	return autoriser('configurer', 'iextras', $id, $qui, $opt);
 }
 
 function autoriser_iextras_configurer_dist($faire, $type, $id, $qui, $opt) {
