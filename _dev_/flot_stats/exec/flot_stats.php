f6f826f84e9e19d864b8bc6491a0519dc0656a41
prigent.yohann@gmail.com <prigent.yohann@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-01-17 21:53:04 +0000

    Intégration du zooooooooooom
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@26011 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/flot_stats/exec/flot_stats.php
+++ _dev_/flot_stats/exec/flot_stats.php
@@ -21,45 +21,62 @@ function exec_flot_stats() {
 	?>
 	<script id="source" language="javascript" type="text/javascript">
 		$(function () {
-		var stats =  <?php $select = sql_select("*", "spip_visites");
-	$nstats = sql_countsel('spip_visites');
-	echo '[';
-	$coun = 1;
-	while ($ele=sql_fetch($select)){
-		$date = $ele['date'];
-		$nvis = $ele['visites'];
-		$d = explode("-", $date);
-		$times = mktime(0, 0, 0, $d[1], $d[2], $d[0]);
-		$times = intval($times);
-		$times = $times * 1000;
-		
-		echo '['.$times.', '.$nvis.']';
-		if ($coun<$nstats) {
-			$coun++;
-			echo ",";
-		}
-	}
-	echo ']';
-	?>;
-	
-    $.plot($("#conteneur"),
-           [ { data: stats, label: "Visites",lines: { show: true }, points: { show: <?php
-		   $nstats = sql_countsel('spip_visites');
-	if ($nstats > 365) {
-	echo "false";
-	}
-	else {
-	echo "true";
-	}
-	?> } },],
-           { xaxis: { mode: 'time', timeformat: "%y"   },
-             yaxis: { min: 0  },
-             legend: { position: 'ne'} });
-});
-</script>
+			var stats =  [{
+				label: "Visites",
+				data: <?php $select = sql_select("*", "spip_visites");
+				$nstats = sql_countsel('spip_visites');
+				echo '[';
+				$coun = 1;
+				while ($ele=sql_fetch($select)){
+					$date = $ele['date'];
+					$nvis = $ele['visites'];
+					$d = explode("-", $date);
+					$times = mktime(0, 0, 0, $d[1], $d[2], $d[0]);
+					$times = intval($times);
+					$times = $times * 1000;
+					echo '['.$times.', '.$nvis.']';
+						if ($coun<$nstats) {
+							$coun++;
+							echo ",";
+						}
+					}
+				echo ']';
+				?> 
+			}];
+
+			var options = { 
+				lines: { show: true }, 
+				points: { show: <?php $nstats = sql_countsel('spip_visites'); if ($nstats > 365) { echo "false";} else { echo "true";}?> },
+				xaxis: { mode: 'time', timeformat: "%d/%m/%y"   },
+				yaxis: { min: 0  },
+				legend: { position: 'ne'},
+				selection: { mode: "xy" }
+			};
+			
+			var conteneur = $("#conteneur");
+
+			conteneur.bind("plotselected", function (event, ranges) {
+				$("#selection").text(ranges.xaxis.from.toFixed(1) + " to " + ranges.xaxis.to.toFixed(1));
+				var zoom = $("#zoom").attr("checked");
+				if (zoom)
+					plot = $.plot(conteneur, stats,
+					$.extend(true, {}, options, {
+						xaxis: { min: ranges.xaxis.from, max: ranges.xaxis.to }
+					}));
+			});
+			
+			var plot = $.plot(conteneur, stats, options);
+
+			$("#clearSelection").click(function () {
+				plot.clearSelection();
+			});
+		});
+	</script>
 	<?php
 	echo '<br />';
 	echo '<div id="conteneur" style="width:600px;height:300px;border: 1px solid; margin: auto;"></div>';
-
+	echo '<div id="overview" style="width:166px;height:100px"></div>';
+	echo '<p><input id="clearSelection" type="button" value="Effacer la selection" />';
+	echo '<p><input id="zoom" type="checkbox">Zoomer sur la selection</input></p>';
 }
 ?>
