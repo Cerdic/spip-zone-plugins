7b6bba61928a19da60e7143847b3de7f8dd8b1ab
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-01-19 22:46:32 +0000

    langues (flickrcc)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@26063 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/flickr_cc/lang/flickrcc_ast.php
+++ _test_/flickr_cc/lang/flickrcc_ast.php
@@ -8,7 +8,7 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
-	'ajouter_image_flickr' => 'Amestar una imaxen de Flickr',
+	'ajouter_image_flickr' => 'Amestar una imaxe de Flickr',
 
 	// C
 	'cc_commerciale' => 'CC - Usu comercial posible',
@@ -18,12 +18,12 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// F
 	'fermer' => 'Zarrar',
-	'flickr_cc_explication' => '<p>Restola im&aacute;xenes de Creative Commons en Flickr enxertando enriba los t&eacute;rminos a guetar. Precisa, si ye\'l casu, los tipos de llicencia embaxo los que tan les im&aacute;xenes.</p>
-<p>Calca na (o les) im&aacute;xen(es) escoy&iacute;a(es) ente los resultaos de la gueta: la imaxen am&eacute;stase de secute col to art&iacute;culu. Calca enriba\'l nome de l\'autor pa dir a la p&aacute;xina en Flickr d\'esa imaxen.</p>
-<p>Estes im&aacute;xenes &uacute;frense polos sos respeutivos autores: col env&iacute;s de respetar los sos derechos y el so trabayu,</p>
+	'flickr_cc_explication' => '<p>Restola imaxes de Creative Commons en Flickr enxertando enriba los t&eacute;rminos a guetar. Precisa, si ye\'l casu, los tipos de llicencia embaxo los que tan les im&aacute;xenes.</p>
+<p>Calca na (o nes) imaxe(s) escoy&iacute;a(es) ente los resultaos de la gueta: la imaxe am&eacute;stase de secute col to art&iacute;culu. Calca enriba\'l nome de l\'autor pa dir a la p&aacute;xina en Flickr d\'esa imaxen.</p>
+<p>Estes imaxes &uacute;frense polos sos respeutivos autores: col env&iacute;s de respetar los sos derechos y el so trabayu,</p>
 <ul><li>nun desanicies la menci&oacute;n de l\'autor y l\'enllace a la so p&aacute;xina en Flickr&nbsp;;</li>
-<li>respeta rigurosamente la llicencia Creative Commons asociada con cada imaxen (en particular, nun utilizar une imaxen nun proyeutu comercial si la llicencia nun lo permite;</li>
-<li>si l\'autor de la imaxen te lo pide, debes retirar la imaxen del to sitiu nun plazu de 24 hores.</li>
+<li>respeta rigurosamente la llicencia Creative Commons asociada con cada imaxe (en particular, nun utilizar una imaxe nun proyeutu comercial si la llicencia nun lo permite;</li>
+<li>si l\'autor de la imaxe te lo pidiere, debes desaniciar la imaxe del to sitiu nun plazu de 24 hores.</li>
 </ul>
 <p>Esti productu utiliza l’API Flickr pero nun ta nin sofit&aacute;u, nin certific&aacute;u por Flickr.</p>',
 
