b4a806456ca89c78acd8a2274951ed4274119dcf
george@diwanalarab.com <george@diwanalarab.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-01-20 10:47:50 +0000

    Quelques corrections
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@26073 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/barre_typo_v2/lang/bartypenr_fr.php
+++ _stable_/barre_typo_v2/lang/bartypenr_fr.php
@@ -43,7 +43,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 'barre_stats' => 'Afficher les statistiques du texte',
 
 'barre_code' => 'Mettre en forme un &lt;code&gt;code informatique&lt;/code&gt;',
-'barre_cadre' => 'Placer dans une &lt;cadre&gt;zone de saisie de texte&lt;/poesie&gt;',
+'barre_cadre' => 'Placer dans une &lt;cadre&gt;zone de saisie de texte&lt;/cadre&gt;',
 'barre_poesie' => 'Mettre en forme comme une &lt;poesie&gt;po&eacute;sie&lt;/poesie&gt;',
 
 'barre_ancres' => 'Gestion des ancres',
