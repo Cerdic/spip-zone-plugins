a0710bc6789bdeaf8399c2d835e3fa75ae8de5c9
cam.lafit@azerttyu.net <cam.lafit@azerttyu.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-01-21 16:41:45 +0000

    Proteger le php
    On ne charge pas comme on veut un bout de php
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@26114 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/innerfade/innerfade_pipeline.php
+++ _dev_/innerfade/innerfade_pipeline.php
@@ -1,5 +1,7 @@
 <?php 
 
+    if (!defined("_ECRIRE_INC_VERSION")) return;
+    
     function innerfade_insert_head($flux){
         $flux .= '<link rel="stylesheet" href="'.find_in_path('css/diaporama_innerfade.css').'" type="text/css" media="projection, screen, tv" />'."\n";
         $flux .= '<script src="'.find_in_path('js/jquery.innerfade.js').'" type="text/javascript"></script>'."\n";
