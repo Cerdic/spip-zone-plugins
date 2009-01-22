974cc14f1291ba0cfe50328fb9c006f7d887b356
cam.lafit@azerttyu.net <cam.lafit@azerttyu.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-01-22 15:30:07 +0000

    Passage de l'appel js dans les '''<head>'''
    On deplace l'appel au script '''slideshow.js''' au niveau du '''<head>'''
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@26142 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/innerfade/innerfade_pipeline.php
+++ _dev_/innerfade/innerfade_pipeline.php
@@ -5,6 +5,7 @@
     function innerfade_insert_head($flux){
         $flux .= '<link rel="stylesheet" href="'.find_in_path('css/diaporama_innerfade.css').'" type="text/css" media="projection, screen, tv" />'."\n";
         $flux .= '<script src="'.find_in_path('js/jquery.innerfade.js').'" type="text/javascript"></script>'."\n";
+        $flux .= '<script src="'.find_in_path('js/slideshow.js').'" type="text/javascript"></script>'."\n";
         return $flux;
 
     }
@@ -12,6 +13,7 @@
     function innerfade_header_prive($flux){
         $flux .= '<link rel="stylesheet" href="'.find_in_path('css/diaporama_innerfade.css').'" type="text/css" media="projection, screen, tv" />'."\n";
         $flux .= '<script src="'.find_in_path('js/jquery.innerfade.js').'" type="text/javascript"></script>'."\n";
+        $flux .= '<script src="'.find_in_path('js/slideshow.js').'" type="text/javascript"></script>'."\n";        
         return $flux;    
     }
 
