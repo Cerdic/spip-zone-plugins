33613bbcf03c5e9bc1799b4404073207cfa51f84
prigent.yohann@gmail.com <prigent.yohann@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-01-24 20:39:00 +0000

    Joli :)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@26185 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/flot_stats/exec/flot_stats.php
+++ _dev_/flot_stats/exec/flot_stats.php
@@ -18,25 +18,6 @@ include_spip('inc/statistiques_new');
 function exec_flot_stats() {
 	$commencer_page = charger_fonction('commencer_page', 'inc');
 	echo $commencer_page("Statistiques", "", "");
-	echo '<div id="conteneur" style="width:600px;height:300px;border: 1px solid; margin: auto;"></div>';
-	echo 'Total : '.$mtotal.'<br />';
-	$select = sql_select("*", "spip_visites","","","`date` DESC",1);
-					while ($ojr=sql_fetch($select)){
-						echo ' Aujourd\'hui : '.$ojr['visites'].'<br />';
-					}
-	$select = sql_select("*", "spip_visites","","","`visites` DESC",1);
-					while ($ojr=sql_fetch($select)){
-						echo ' Maximum : '.$ojr['visites'].'<br />';
-					}
-	$select = sql_select("*", "spip_visites","visites!=0","","`visites` ASC",1);
-					while ($ojr=sql_fetch($select)){
-						echo ' Minimum : '.$ojr['visites'].'<br />';
-					}
-	$mcompte = sql_countsel('spip_visites','visites!=0');
-	$moyenne = $mtotal / $mcompte;
-	$moyenne = round($moyenne);
-	echo ' Moyenne : '.$moyenne;
-	echo '<p><input id="clearSelection" type="button" value="Effacer la selection" />';
-	echo '<p><input id="zoom" type="checkbox">Zoomer sur la selection</input></p>';
+	echo recuperer_fond('prive/stats');
 }
 ?>
