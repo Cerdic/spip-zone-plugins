f35a238e004529d87e0a05d3507409854947931f
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-01-25 20:27:42 +0000

    langues (flickrcc)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@26202 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _test_/flickr_cc/lang/flickrcc_lb.php
@@ -0,0 +1,37 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'ajouter_image_flickr' => 'E Flickr-Bild derb&auml;i setzen',
+
+	// C
+	'cc_commerciale' => 'CC - Kommerziellt Benotzen erlaabt',
+	'cc_modification' => 'CC - &Auml;nnerung erlaabt',
+	'cc_pas_de_restriction' => 'K&eacute;ng bekannte Restriktiounen',
+	'cc_paternite' => 'Creative Commons - Credit',
+
+	// F
+	'fermer' => 'Zoumaachen',
+	'flickr_cc_explication' => '<p>Sicht Creative Commons Biller op Flickr andeem dir hei eng Sich agidd. Pr&auml;zis&eacute;iert och w&eacute;i eng Lizenz d\'Biller sollen hun.</p>
+<p>Klickt op d\'Bild oder op d\'Biller vum Resultat: si ginn direkt un &auml;ren Artikel ugebonnen. Klickt op de Numm vum Auteur fir op d\'Flickr-S&auml;it vun d&euml;sem Bild ze kommen.</p>
+<p>D&euml;s Biller ginn vun den Auteuren zur Verf&uuml;gung gestallt: fir hier Rechter an hier Aarbecht ze respekt&eacute;ieren,</p>
+<ul><li>l&auml;scht d\'Mentioun vum Auteur an de Link op d\'Flickr-S&auml;it n&euml;t;</li>
+<li>respekt&eacute;iert genee d\'Creative Commons Lizenz vun all Bild;</li>
+<li>wann den Auteur dat verlaangt musst dir innerhalb vu 24 Stonnen d\'Bild vun &auml;rem Site l&auml;schen.</li>
+</ul>
+<p>D&euml;st Produkt benotzt d\'API vu Flickr mais ass awer n&euml;t vu Flickr &euml;nnerst&euml;tzt oder zertifi&eacute;iert.</p>',
+
+	// R
+	'rechercher_flickr' => 'Op Flickr sichen',
+	'resultat_par_date' => 'no Datum',
+	'resultat_par_interet' => 'no Int&eacute;r&ecirc;t',
+	'resultat_par_pertinence' => 'no Warscheinlechkeet'
+);
+
+?>
