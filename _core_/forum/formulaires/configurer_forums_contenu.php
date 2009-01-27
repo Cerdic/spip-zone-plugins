26f80a9b01665cc3b182593a169f1d6dc6dae00a
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-01-27 15:06:12 +0000

    "des coquilles (d'e, pour ceux qui suivent)"
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@26232 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _core_/forum/formulaires/configurer_forums_contenu.php
+++ _core_/forum/formulaires/configurer_forums_contenu.php
@@ -15,10 +15,10 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 function formulaires_configurer_forums_contenu_charger_dist(){
 
 	return array(
-		'forums_titre' => $GLOBALS['meta']["forums_titre"]=='oui',
-		'forums_texte' => $GLOBALS['meta']["forums_texte"]=='oui',
-		'forums_urlref' => $GLOBALS['meta']["forums_urlref"]=='oui',
-		'forums_afficher_barre' => $GLOBALS['meta']["forums_afficher_barre"]=='oui',
+		'forums_titre' => $GLOBALS['meta']["forums_titre"],
+		'forums_texte' => $GLOBALS['meta']["forums_texte"],
+		'forums_urlref' => $GLOBALS['meta']["forums_urlref"],
+		'forums_afficher_barre' => $GLOBALS['meta']["forums_afficher_barre"],
 		'formats_documents_forum' => $GLOBALS['meta']['formats_documents_forum'],
 	);
 	
@@ -26,21 +26,21 @@ function formulaires_configurer_forums_contenu_charger_dist(){
 
 function formulaires_configurer_forums_contenu_verifier_dist(){
 	$erreurs = array();
-	
-	foreach(array('forums_titre', 'forums_texte',	'forums_urlref','forums_afficher_barre') as $champ)
-		if (_request($champ)===NULL)
-			set_request($champ,'0');
-			
+
 	if (!_request('forums_titre') AND !_request('forums_texte') AND !_request('forums_urlref'))
 		$erreurs['forums_titre'] = _T('info_obligatoire');
-		
+
+	foreach(array('forums_titre', 'forums_texte',	'forums_urlref','forums_afficher_barre') as $champ)
+		if (_request($champ)!=='oui')
+			set_request($champ,'non');
+	
 	return $erreurs;
 }
 
 function formulaires_configurer_forums_contenu_traiter_dist(){
 	include_spip('inc/config');
 	appliquer_modifs_config();
-		
+
 	return array('message_ok'=>_T('config_info_enregistree'));
 }
 
