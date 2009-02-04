4bc3a757f4f6ab938a68b3b530c6b91e8361bb6b
mmmxvvv@tiscali.it <mmmxvvv@tiscali.it@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-02-04 19:42:55 +0000

    translations corrected and added
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@26401 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _test_/abcalendrier/lang/abcalendrier_en.php
@@ -0,0 +1,13 @@
+<?php
+// Language file 
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+   'agenda' => 'Calendar',
+   'Mois_precedent' => 'Previous Month',
+   'Mois_suivant' => 'Next month',
+   'aucun_evenement' => 'There are no upcoming events for this month in the calendar',
+   'date_evenement' => 'Date of Event : ',
+);
+
+
+
+?>
--- _test_/abcalendrier/lang/abcalendrier_fr.php
+++ _test_/abcalendrier/lang/abcalendrier_fr.php
@@ -1,10 +1,11 @@
 <?php
-// File di lingua 
+// Fichier de langue 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
    'agenda' => 'Calendrier',
-   'Mois_precedent' => "Mois pr&eacute;c&eacute;dent",
+   'Mois_precedent' => 'Mois pr&#233;c&#233;dent',
    'Mois_suivant' => 'Mois suivant',
-   'aucun_evenement' => "Il n'y a aucun &eacute;v&egrave;nement &agrave; venir pour ce mois dans l'agenda",
+   'aucun_evenement' => 'Il n\'y a aucun &#233;v&#232;nement &agrave; venir pour ce mois dans l\'agenda',
+   'date_evenement' => 'Date de l\'&#233;v&#232;nement : ',
 );
 
 
