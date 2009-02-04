1f90c3543ec87c556645718788da52c4f7625f8d
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-02-04 14:56:29 +0000

    "redondance"
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@26397 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/gestion_documents/gestdoc_fonctions.php
+++ _stable_/gestion_documents/gestdoc_fonctions.php
@@ -6,18 +6,6 @@
  */
 
 /**
- * Afficher un message "une truc"/"N trucs"
- *
- * @param int $nb
- * @return string
- */
-function gestdoc_affiche_un_ou_plusieurs($nb,$chaine_un,$chaine_plusieurs,$var='nb'){
-	if (!$nb=intval($nb)) return "";
-	if ($nb>1) return _T($chaine_plusieurs, array($var => $nb));
-	else return _T($chaine_un);
-}
-
-/**
  * Enter description here...
  *
  * @param unknown_type $id_document
