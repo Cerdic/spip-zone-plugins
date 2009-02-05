1c3ca4eee6a349e823b0ed0c803b22c7e101ac67
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-02-05 16:30:46 +0000

    amélioration des listes
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@26406 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/spipicious_jquery/spipicious_head.php
+++ _dev_/spipicious_jquery/spipicious_head.php
@@ -13,10 +13,11 @@
  */
  
 function spipicious_insert_head($flux){
-	$contenu = " ";
 	global $visiteur_session;
+	$contenu = " ";
+	$autorise = lire_config('spipicious/people');
 	
-	if($visiteur_session['id_auteur']){
+	if($visiteur_session['id_auteur'] && in_array($visiteur_session['statut'],$autorise)){
 
 	include_spip('selecteurgenerique_fonctions');
 	$flux .= selecteurgenerique_verifier_js($flux);
@@ -26,62 +27,65 @@ function spipicious_insert_head($flux){
 	
 	$flux .= <<<EOS
 		<script type="text/javascript"><!--
-			function deletetag(tag){
-				var tag = tag;
+		
+		var deletetag = function(tag){
 				jQuery('input#remove_tag').val(tag).parents('form').submit().end();
 				return false;
-			}
-	(function($) {
-	var appliquer_selecteur_spipicious = function() {
+		};
 		
-		var spipicious = $('input[name=spipicious_tags][autocomplete!=off]');
-		// chercher l'input de saisie
-		var id_objet = $("input#spipicious_id").val();
-		var type = $("input#spipicious_type").val();
-		if((spipicious.size()>0)&&(type!='')){
-			$.ajax({
-				type: "GET",
-				url:'$tags_link&id_'+type+'='+id_objet,
-				success:function(data,status){
-					var newdata = jQuery(data+' #tags').html();
-					jQuery('#tags').addClass('loading').html(newdata).removeClass('loading');
+	(function($) {
+		var appliquer_selecteur_spipicious = function() {
+			
+			// chercher l'input de saisie
+			var spipicious = $('input[name=spipicious_tags][autocomplete!=off]');
+			
+			var id_objet = $("input#spipicious_id").val();
+			var type = $("input#spipicious_type").val();
+			if((spipicious.size()>0)&&(type!='')){
+				$.ajax({
+					type: "GET",
+					url:'$tags_link',
+					data: 'id_'+type+'='+id_objet,
+					success:function(data,status){
+						var newdata = jQuery(data+' #tags').html();
+						$('.tags').addClass('loading').html(newdata).removeClass('loading');
+					}
+				});
+			}
+			
+			//.addClass('loading').load('$tags_link&id_'+type+'='+id_objet).removeClass('loading');
+			spipicious.autocomplete('$selecteur',
+				{
+					extraParams:{
+						id_objet:id_objet,
+						type:type
+					},
+					delay: 200,
+					autofill: false,
+					minChars: 1,
+					multiple:true,
+					multipleSeparator:";",
+					formatItem: function(data, i, n, value) {
+						return data[0];
+					},
+					formatResult: function(data, i, n, value) {
+						return data[1];
+					},
+				}
+			);
+			spipicious.result(function(event, data, formatted) {
+				if (data[2] > 0) {
+					$(spipicious)
+					.end();
+				}
+				else{
+					return data[1];
 				}
 			});
-		}
-		
-		//.addClass('loading').load('$tags_link&id_'+type+'='+id_objet).removeClass('loading');
-		spipicious.autocomplete('$selecteur',
-			{
-				extraParams:{
-					id_objet:id_objet,
-					type:type
-				},
-				delay: 200,
-				autofill: false,
-				minChars: 1,
-				multiple:true,
-				multipleSeparator:";",
-				formatItem: function(data, i, n, value) {
-					return data[0];
-				},
-				formatResult: function(data, i, n, value) {
-					return data[1];
-				},
-			}
-		);
-		spipicious.result(function(event, data, formatted) {
-			if (data[2] > 0) {
-				jQuery(spipicious)
-				.end();
-			}
-			else{
-				return data[1];
-			}
-		});
-		// Hack pour le focus obligatoire de positionner
-		// Le selecteur generique ne se rechargeait pas
-		spipicious.blur().focus();
-	};
+			// Hack pour le focus obligatoire de positionner
+			// Le selecteur generique ne se rechargeait pas
+			spipicious.blur().focus();
+		};
 	
 		$(function(){
 			appliquer_selecteur_spipicious();
