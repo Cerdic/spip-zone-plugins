a7aa0925a49a3e6826e0fc1299331f5ecb2abb78
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-02-08 18:28:47 +0000

    langues (cfg)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@26465 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/cfg/lang/cfg_ca.php
+++ _stable_/cfg/lang/cfg_ca.php
@@ -47,6 +47,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'pas_de_changement' => 'Cap canvi a <b>@nom@</b>',
 
 	// R
+	'refus_configuration_webmestre' => 'Nom&eacute;s els webmestres del lloc estan autoritzats per modificar aquests par&agrave;metres. ',
 	'reset' => 'Esborrar',
 
 	// S
