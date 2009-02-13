ddec2c87aabbbced6c3fc29221ab8a9cf33eb37e
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-02-13 11:28:27 +0000

    "complement du bandeau : peuplement, et fonctions autoriser correspondantes, a remplir"
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@26571 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _core_/bandeau/inc/bandeau.php
+++ _core_/bandeau/inc/bandeau.php
@@ -24,135 +24,6 @@ include_spip('inc/boutons');
 // http://doc.spip.org/@definir_barre_boutons
 function definir_barre_boutons() {
 	$boutons_admin=array();
-/*
-	$boutons_admin['naviguer'] = new Bouton(
-		"documents-48$spip_lang_rtl.png", 'icone_edition_site');
-
-	$boutons_admin['auteurs'] = new Bouton(
-		'redacteurs-48.png', 'icone_auteurs');
-
-	// autoriser('configurer') => forcement admin complet (ou webmestre)
-	if (autoriser('configurer')) {
-		$boutons_admin['configuration']=
-		  new Bouton('administration-48.png', 'icone_configuration_site');
-	}
-	// autres admins (restreints ou non webmestres) peuvent aller sur les backups
-	else
-	if (autoriser('sauvegarder', 'admin_tech')) {
-		$boutons_admin['admin_tech']=
-		  new Bouton('administration-48.png', 'texte_sauvegarde_base');
-	}
-
-	$boutons_admin['espacement']=null;
-
-	$urlAide= generer_url_ecrire('aide_index')."&amp;var_lang=$spip_lang";
-	$boutons_admin['aide_index']=
-		  new Bouton('aide-48'.$spip_lang_rtl.'.png', 'icone_aide_ligne',
-					 $urlAide, null, "javascript:window.open('$urlAide', 'spip_aide', 'scrollbars=yes,resizable=yes,width=740,height=580');", 'aide_spip');
-
-	$boutons_admin['visiter']=
-		new Bouton("visiter-48$spip_lang_rtl.png", 'icone_visiter_site',
-		url_de_base());
-
-	// les sous menu des boutons, que si on est admin
-	if ($GLOBALS['connect_statut'] == '0minirezo'
-	AND $GLOBALS['connect_toutes_rubriques']) {
-
-	// sous menu edition
-
-	$sousmenu=array();
-
-	$nombre_articles = sql_fetsel('id_article', 'spip_auteurs_articles', "id_auteur=".$GLOBALS['connect_id_auteur']);
-
-	if ($nombre_articles > 0) {
-		$sousmenu['articles_page']=
-		  new Bouton('article-24.gif', 'icone_tous_articles');
-	}
-
-	if ($GLOBALS['meta']["activer_breves"] != "non") {
-		$sousmenu['breves']=
-		  new Bouton('breve-24.gif', 'icone_breves');
-	}
-
-	$articles_mots = $GLOBALS['meta']['articles_mots'];
-	if ($articles_mots != "non") {
-			$sousmenu['mots_tous']=
-			  new Bouton('mot-cle-24.gif', 'icone_mots_cles');
-	}
-
-	$activer_sites = $GLOBALS['meta']['activer_sites'];
-	if ($activer_sites<>'non')
-			$sousmenu['sites_tous']=
-			  new Bouton('site-24.gif', 'icone_sites_references');
-
-	$n = sql_countsel('spip_documents_liens', 'id_objet>0 AND objet=\'rubrique\'');
-	if ($n) {
-			$sousmenu['documents_liste']=
-			  new Bouton('doc-24.gif', 'icone_doc_rubrique');
-	}
-	$boutons_admin['naviguer']->sousmenu= $sousmenu;
-
-	// sous menu forum
-	$sousmenu=array();
-
-	if (sql_countsel('spip_signatures'))
-		$sousmenu['controle_petition']=
-			new Bouton("suivi-petition-24.gif", "icone_suivi_pettions");
-
-	// Si le forum a ete desactive, mais qu'il y a un sous-menu de suivi
-	// des forums ou des petitions, on colle ce suivi sous le menu "a suivre"
-	if ($sousmenu) {
-		if (isset($boutons_admin['forum']))
-			$boutons_admin['forum']->sousmenu= $sousmenu;
-		else
-			$boutons_admin['accueil']->sousmenu= $sousmenu;
-	}
-
-
-
-	// sous menu auteurs
-
-	$sousmenu=array();
-
-	if (avoir_visiteurs(true))
-		$sousmenu['auteurs'] =
-			new Bouton("fiche-perso.png", 'icone_afficher_visiteurs', null, "statut=!1comite,0minirezo,nouveau");
-
-	$sousmenu['auteur_infos']=
-		new Bouton("auteur-24.gif", "icone_creer_nouvel_auteur", null, 'new=oui');
-
-	$boutons_admin['auteurs']->sousmenu= $sousmenu;
-
-
-	// sous menu configuration
-	$sousmenu = array();
-	if (autoriser('configurer', 'lang')) {
-		$sousmenu['config_lang'] =
-			new Bouton("langues-24.gif", "icone_gestion_langues");
-		//$sousmenu['espacement'] = null; // les espacements debloquent si on a des icones sur 2 lignes
-	}
-
-	if (autoriser('sauvegarder')) {
-		$sousmenu['admin_tech']=
-			new Bouton("base-24.gif", "icone_maintenance_site");
-	}
-	if (autoriser('configurer', 'admin_vider')) {
-		$sousmenu['admin_vider']=
-			new Bouton("cache-24.gif", "onglet_vider_cache");
-	}
-
-	// Si _DIR_PLUGINS est definie a '', pas de bouton
-	if (_DIR_PLUGINS
-	AND autoriser('configurer', 'admin_plugin')) {
-		$sousmenu['admin_plugin']=
-			new Bouton("plugin-24.gif", "icone_admin_plugin");
-	}
-
-	if ($sousmenu)
-		$boutons_admin['configuration']->sousmenu= $sousmenu;
-
-	} // fin si admin
-	*/
 
 	// ajouter les boutons issus des plugin via plugin.xml
 	// avant l'icone de configuration
@@ -172,14 +43,14 @@ function definir_barre_boutons() {
 				AND (!in_array($id,array('forum','statistiques_visites')))
 				
 				) {
-					$position = $infos['position']?$infos['position']:-4;
-					$boutons_admin += 
-					array($id=> new Bouton(
+					$position = $infos['position']?$infos['position']:count($boutons_admin);
+					$boutons_admin = array_slice($boutons_admin,0,$position)
+					+array($id=> new Bouton(
 					  find_in_path($infos['icone']),  // icone
 					  $infos['titre'],	// titre
 					  $infos['url']?generer_url_ecrire($infos['url'],$infos['args']?$infos['args']:''):null
-					  ));
-
+					  ))
+					+ array_slice($boutons_admin,$position,100);
 				}
 			}
 		}
