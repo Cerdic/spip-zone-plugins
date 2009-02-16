e4ed94ee170295df1f1eefd20ab35120e9b4a78c
sam@poongaloo.org <sam@poongaloo.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-02-16 15:54:55 +0000

    Nettoyage dans les commentaires du code et ajout d'un prefixe aux fonctions
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@26686 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/jflipbook/exec/jflipbook.php
+++ _test_/jflipbook/exec/jflipbook.php
@@ -30,10 +30,6 @@ $descriptifmot = 'mot-clé permettant de transformer un article contenant des im
    echo '<br /><br /> Documentation officielle EVA-WEB :';
    echo '<br /><a href="http://eva-web.edres74.net/spip.php?rubrique4" target="_blank" >Documentation eva-web</a>';
    echo fin_boite_info(true);
-	
-   //echo debut_raccourcis();
-   //echo 'contenu de la boite des raccourcis du plugin';
-   //echo fin_raccourcis();
 		
    echo debut_droite('',true);
    echo debut_cadre_trait_couleur($icone, true,'', _T('jflipbook:titre_boite_principale'));
@@ -45,7 +41,7 @@ $descriptifmot = 'mot-clé permettant de transformer un article contenant des im
 //Vérification de la configuration du site
    echo _T('jflipbook:verification_conf');
 	echo _T('jflipbook:conf_mots');	
-	$confmot = test_conf('articles_mots');
+	$confmot = jflipbook_test_conf('articles_mots');
 	if ($confmot == 'oui'){
 	   echo 'La configuration des mots-clés est correcte';
 	   }else{
@@ -55,7 +51,7 @@ $descriptifmot = 'mot-clé permettant de transformer un article contenant des im
 	echo "<br />";
 	
 	echo _T('jflipbook:conf_docs');
-	$confdoc = test_conf('documents_article');
+	$confdoc = jflipbook_test_conf('documents_article');
 	if ($confdoc == 'oui'){
 	   echo 'La configuration des documents joints aux articles est correcte';
 	   }else{
@@ -67,13 +63,12 @@ $descriptifmot = 'mot-clé permettant de transformer un article contenant des im
 //Gestion du mot-clé jflip   
 	echo _T('jflipbook:texte_motcle');
 	echo "Mot-clé nécessaire : ".$jflip."<br />";
-	$presence = test_mots($jflip);
+	$presence = jflipbook_test_mots($jflip);
 	
 	if ($presence == false){
 		echo 'il faut créer '.$jflip.' mots-clés<br/>';
 			if (_request('creermot')!=NULL){
-				crea_mots($jflip,$descriptifmot);
-				#echo 'id aff ='.$id_aff.'<br/>';
+				jflipbook_crea_mots($jflip,$descriptifmot);
 			}
 			echo '<form method="POST" action="'.generer_url_ecrire("jflipbook").'">';
 			echo "<input type='submit' name='creermot' value='"._T('jflipbook:creer_mots')."' class='fondo'>";
@@ -105,51 +100,32 @@ $descriptifmot = 'mot-clé permettant de transformer un article contenant des im
 }
 
 ####################
-function test_mots($jflip){
-
-//echo 'jflip = '.$jflip.'<br/>';
+function jflipbook_test_mots($jflip){
 
 $resultat = sql_select('id_mot','spip_mots',"titre='".$jflip."'");
 $nb = sql_count($resultat);
 
-//echo 'nombre trouve pour '.$jflip.'= '.$nb.'<br>';
-
 if ($nb == 0){
-	//echo 'il manque '.$jflip.' mots-clés<br/>';
 	return false;
 	}else{
-	//echo 'C\'est OK.<br/>';
 	return true;
 	}
-#echo '----------------------------<br/>';
 }
 ####################
-function crea_mots ($jflip, $descriptifmot){
-#echo 'Dans la fonction, id = '.$id.'<br/>';
-
-#echo "<br/>-----------------------------------<br/>";
-
-#echo "Valeur de id du groupe =".$id."<br/>";
+function jflipbook_crea_mots($jflip, $descriptifmot){
 echo 'Cr&eacute;ation du mots-cl&eacute;s '.$jflip.' <br/>';
-
 $ajout_mot = sql_insertq('spip_mots',array('titre'=>$jflip,'descriptif'=>$descriptifmot));
 
 }
 ####################	
-function	test_conf($nom){
+function	jflipbook_test_conf($nom){
 $resultat = sql_select('valeur','spip_meta',"nom='".$nom."'");
 $nb = sql_count($resultat);
 $row = sql_fetch($resultat);
 
 if ($nb == 1){
-	//echo 'nb = '.$nb;
-	//echo '<br />';
-	//echo 'resultat = '.$resultat;
-	//echo '<br />';
-	//echo 'row = '.$row['valeur'];
 	return $row['valeur'];
 	}else{
-	//echo 'ERREUR';
 	return false;
 	}
 }
