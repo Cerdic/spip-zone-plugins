69ff8d3850a4674dc8cac7fe372057897cdeb107
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-02-18 11:28:59 +0000

    debugguer la gestion des tempos dans les menu deroulants, et eviter de mettre N fois une fonction sur l'evenement hover d'un li (N etant le nombre de liens contenu dans le li) la navigation clavier est fonctionnelle sur le menu deroulant boussole, mais lente. Voir si on la garde ou si on la zappe en repassant le deroulant en ajax charge uniquement au hover et pas au focus ...
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@26728 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _core_/bandeau/javascript/gadgets.js
+++ _core_/bandeau/javascript/gadgets.js
@@ -9,42 +9,40 @@ jQuery(document).ready(function(){
 	// deplier le menu au focus clavier,
 	// enlever ce depliement si passage a la souris,
 	// delai de fermeture.
-	var timer; // pour timeout du menu...
 	jQuery.fn.menuFocus = function(){
+		jQuery(this)
+		// le replier si un hover de souris sur un autre onglet,
+		// timer sur la fermeture des onglets pour ne pas que ca aille trop vite
+		.hover(
+			function(){
+				/*jQuery(this).parents('ul').find('>li.actif').removeClass('actif');*/
+				jQuery(this)
+					.addClass('actif')
+					.parents('li').addClass('actif');
+				jQuery(this).siblings('li').removeClass('actif_tempo');
+			}
+			,
+			function(){
+				var me = jQuery(this).removeClass('actif').addClass('actif_tempo');
+				setTimeout(function(){
+					me.removeClass('actif_tempo');
+				}, 600);
+			}
+		)
+		// navigation au clavier :
 		// deplier le ul enfant
-		jQuery(this).focus(function(){
-			jQuery('#bando_navigation li.actif').removeClass('actif');
-			jQuery(this).parent().addClass('actif');
+		.find('>a').focus(function(){
+			//jQuery(this).parents('ul').find('>li.actif').removeClass('actif');
+			jQuery(this).parents('li').addClass('actif');
 		})
 		// cacher en partant de l'onglet...
 		.blur(function(){
-			jQuery(this).parents('li.actif').removeClass('actif');
-		})
-		// remonter au li parent
-		.parent()
-		// le replier si un hover de souris sur un autre onglet,
-		// timer sur la fermeture des onglets pour ne pas que ca aille trop vite
-		.hover(function(){
-			clearTimeout(timer);
-			if (jQuery(this).parent().parent().is('li'))
-				jQuery(this).parent().parent().addClass('actif').siblings('li').removeClass('actif');
-			jQuery(this).addClass('actif').siblings('li').removeClass('actif');
-		}, function(){
-			var me = jQuery(this);
-			timer = setTimeout(function(){
-				me.removeClass('actif');
-			}, 600);
-		})
-
-		// afficher le menu de l'onglet si un lien enfant devient actif
-		.find('li > a').focus(function(){
-			jQuery('#bando_navigation li.actif').removeClass('actif');
-			jQuery(this).parent().parent().parent().addClass('actif');
+			jQuery(this).parents('li').removeClass('actif');
 		});
 		return this;
 	}
-	jQuery('#bando_navigation li >a').menuFocus();
-	jQuery('#bando_outils ul.bandeau_rubriques li >a').menuFocus();
+	jQuery('#bando_navigation li').menuFocus();
+	jQuery('#bando_outils ul.bandeau_rubriques li').menuFocus();
 
 	jQuery('#bandeau_haut #formRecherche input').hover(function(){
 		jQuery('#bandeau_haut ul.actif').trigger('mouseout');
