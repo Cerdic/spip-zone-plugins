23240719d3756673c02d0a144cfc7ebb221edc4d
sam@poongaloo.org <sam@poongaloo.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-02-20 10:44:03 +0000

    remplacement de la librairie jflip par une librairie plus compacte
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@26764 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/jflipbook/jflipbook_head.php
+++ _test_/jflipbook/jflipbook_head.php
@@ -7,28 +7,28 @@
 //				"fit" big images are resized to be completely visible
 // 			"fill" all images are resized keeping aspect ratio to fill the canvas
 
-	function jflipbook_insert_head($flux){
-		
-		$largeur = lire_config('jflipbook/largeur');
-		$hauteur = lire_config('jflipbook/hauteur');
-		$couleur = lire_config('jflipbook/couleur');
-		$corners = lire_config('jflipbook/corners');
-		$scale = lire_config('jflipbook/scale');	
-		$flux = '<link rel="stylesheet" TYPE="text/css" href="'._DIR_PLUGIN_JFLIPBOOK.'css/jflip_style.css"></link>';
-		$flux .= '<!--[if IE]><script type="text/javascript" src="'._DIR_PLUGIN_JFLIPBOOK.'javascript/excanvasX.js"></script><![endif]-->';
-		$flux .= '<script type="text/javascript" src="'._DIR_PLUGIN_JFLIPBOOK.'javascript/jquery-1.2.6.min.js"></script>';
-		$flux .= '<script type="text/javascript" src="'._DIR_PLUGIN_JFLIPBOOK.'javascript/jquery.jflip.js"></script>';
-		$flux .= '<script type="text/javascript">
-	  					(function($){    
-	      				$(function(){
-	      					$("#g1").jFlip('.$largeur.','.$hauteur.',{background:"'.$couleur.'",cornersTop:'.$corners.',scale:"'.$scale.'"});
-	      				});
-	  					})(jQuery);';
-		$flux .= '</script>';
-		$flux .= '<style type="text/css">
-	  					.flip_gallery {margin:10px}
-	  				</style>';
-	  				
-		return $flux;
-	}
+function jflipbook_insert_head($flux){
+	
+	$largeur = lire_config('jflipbook/largeur');
+	$hauteur = lire_config('jflipbook/hauteur');
+	$couleur = lire_config('jflipbook/couleur');
+	$corners = lire_config('jflipbook/corners');
+	$scale = lire_config('jflipbook/scale');
+	$flux = '<link rel="stylesheet" TYPE="text/css" href="'._DIR_PLUGIN_JFLIPBOOK.'css/jflip_style.css"></link>';
+	$flux .= '<!--[if IE]><script type="text/javascript" src="'._DIR_PLUGIN_JFLIPBOOK.'javascript/excanvasX.js"></script><![endif]-->';
+	$flux .= '<script type="text/javascript" src="'._DIR_PLUGIN_JFLIPBOOK.'javascript/jquery-1.2.6.min.js"></script>';
+	$flux .= '<script type="text/javascript" src="'._DIR_PLUGIN_JFLIPBOOK.'javascript/jquery.jflip-0.3.min.js"></script>';
+	$flux .= '<script type="text/javascript">
+  					(function($){    
+      				$(function(){
+      					$("#g1").jFlip('.$largeur.','.$hauteur.',{background:"'.$couleur.'",cornersTop:'.$corners.',scale:"'.$scale.'"});
+      				});
+  					})(jQuery);';
+	$flux .= '</script>';
+	$flux .= '<style type="text/css">
+  					.flip_gallery {margin:10px}
+  				</style>';
+  				
+	return $flux;
+}
 ?>
