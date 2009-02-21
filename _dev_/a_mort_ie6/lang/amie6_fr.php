5e40cbe12aa8ce1b0c2acacba375a3eb97fa68a2
cy_altern@yahoo.fr <cy_altern@yahoo.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-02-21 11:30:23 +0000

    les fichiers pour "A mort IE6!"
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@26791 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _dev_/a_mort_ie6/lang/amie6_fr.php
@@ -0,0 +1,29 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	
+  // A
+  'autre_navigateurs' => 'Si vous le souhaitez, vous pouvez aussi essayer d\'autres navigateurs Web populaires comme par exemple 
+    <a class="ie6expl" href="http://mozilla.com">FireFox</a>, 
+    <a class="ie6expl" href="http://www.opera.com">Opera</a> 
+    ou <a class="ie6expl" href="http://www.apple.com/safari/download/">Safari</a>',
+
+
+  // M
+  'mise_a_jour_navigateur' => 'Pour naviguer de la mani&egrave;re la plus satisfaisante sur notre site, 
+    nous recommandons que vous proc&eacute;diez &agrave; une mise &agrave; jour de votre navigateur. 
+    La version actuelle est <a class="getie7" href="http://www.microsoft.com/windows/downloads/ie/getitnow.mspx">Internet Explorer&nbsp;7</a> 
+    et <a class="getie7" href="http://www.microsoft.com/windows/Internet-explorer/beta/default.aspx">Internet Explorer&nbsp;8</a> 
+    est disponible en version beta. La mise &agrave; jour est gratuite. 
+    Si vous utilisez un PC au travail, veuillez contacter votre service informatique.',
+
+  
+  // N
+  'navigateur_obsolete'=> 'Savez-vous que votre navigateur est obsol&egrave;te&nbsp;?',
+  
+  
+);
+
+?>
