46c49a8396eac4d9ebf62969ddb8764485621d71
webmaster@geomaticien.com <webmaster@geomaticien.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-02-21 04:28:07 +0000

    Patch compatibilité avec mutualisation
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@26782 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/acs/acs_options.php
+++ _dev_/acs/acs_options.php
@@ -8,7 +8,7 @@
 
 // Dossier des paramètres et images utilisateur
 // User images and parameters
-$GLOBALS['ACS_CHEMIN'] = 'IMG/_acs';
+$GLOBALS['ACS_CHEMIN'] = _NOM_PERMANENTS_ACCESSIBLES.'_acs';
 
 // Pages de l'espace ecrire à accès contrôlé comme ACS - ACS access rules controlled pages
 // Les administrateurs sont ceux d'ACS
@@ -28,6 +28,13 @@ if(in_array('openPublishing', $GLOBALS['plugins'])) {
   Ne PAS modifier ce qui suit - Do NOT modify anything after this
   __________________________________________________________________
 */
+// Dossier des paramètres et images utilisateur
+// User images and parameters
+// compatible mutualisation (_DIR_SITE defini)
+$dir_site = _DIR_RACINE ? substr(_DIR_SITE, 3) : _DIR_SITE ;
+$GLOBALS['ACS_CHEMIN'] = $dir_site._NOM_PERMANENTS_ACCESSIBLES.'_acs';
+
+// Chargement - Loading
 require_once _DIR_PLUGIN_ACS.'inc/acs_onload.php';
 
 ?>
