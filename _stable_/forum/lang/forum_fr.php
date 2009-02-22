9007e352e32d21608434b29ff2a9cd6cea76227d
romy@tetue.net <romy@tetue.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-02-22 12:45:19 +0000

    Vocabulaire : 'refuser' plutot que 'supprimer'
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@26832 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/forum/lang/forum_fr.php
+++ _stable_/forum/lang/forum_fr.php
@@ -14,7 +14,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 'icone_valider_messages' => 'Valider ces messages',
 'icone_bruler_messages' => "Signaler comme Spam",
 'icone_legitimer_message' => "Signaler comme licite",
-'icone_supprimer_messages' => 'Supprimer ces messages',
+'icone_supprimer_messages' => 'Refuser ces messages',
 
 'messages_meme_auteur' => 'Tous les messages de cet auteur',
 'messages_meme_email' => 'Tous les messages de cet email',
@@ -25,10 +25,10 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 'messages_publie' => 'Publi&eacute;s',
 'messages_prop' => 'Propos&eacute;s',
 'messages_spam' => 'Spam',
-'messages_off' => 'Supprim&eacute;s',
+'messages_off' => 'Refus&eacute;s',
 
 'messages_prive' => 'Priv&eacute;s',
-'messages_privoff' => 'Supprim&eacute;s',
+'messages_privoff' => 'Refus&eacute;s',
 'messages_privrac' => 'G&eacute;n&eacute;raux',
 'messages_privadm' => 'Administrateurs',
 
@@ -37,7 +37,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 'statut_prop' => 'Propos&eacute;',
 'statut_publie' => 'Publi&eacute;',
 'statut_spam' => 'Spam',
-'statut_off' => 'Supprim&eacute;',
+'statut_off' => 'Refus&eacute;',
 
 'tout_voir' => 'Voir tous les messages',
 );
