75680c703cb53d1e4c1e6e94ce077817fb0bc2c8
fil@rezo.net <fil@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-02-22 15:55:48 +0000

    se compacter avec les copains
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@26850 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/socialtags/socialtags_fonctions.php
+++ _stable_/socialtags/socialtags_fonctions.php
@@ -8,7 +8,7 @@ if (!defined('_DIR_PLUGIN_SOCIALTAGS')){ // defini automatiquement par SPIP 1.9.
 // ajout feuille de stylle
 //
 function socialtags_insert_head($flux){
-  $flux .= '<link rel="stylesheet" type="text/css" href="'.find_in_path('socialtags.css').'" media="all" />';
+  $flux .= '<link rel="stylesheet" type="text/css" href="'.find_in_path('socialtags.css').'" media="projection, screen, tv" />';
 
   if (lire_config('socialtags/jshead')) {
   	$jsFile = generer_url_public('socialtags.js');
