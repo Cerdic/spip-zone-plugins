4a8ae19f163eb114f8d55596ba95086c9a8894eb
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-02-23 10:06:31 +0000

    langues (noie)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@26875 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _stable_/noie/lang/noie_da.php
@@ -0,0 +1,21 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// I
+	'ie6msg_info' => '<NEW>For at f&aring; den bedst mulige oplevelse med at bruge websider, anbefales det at du opgraderer til <a href="http://www.microsoft.com/danmark/windows/internet-explorer/download-ie.aspx">nyeste version af Internet Explorer</a>. Alternativt kan du installere en alternativ browser, fx <a href="http://www.mozilla-europe.org/da/firefox/">Mozilla Firefox</a>. Hvis du bruger en arbejds-pc, b&oslash;r du kontakte den it-ansvarlige.<br /><a href="http://drop-ie6.dk/hvorfor">L&aelig;s mere om hvorfor det er en d&aring;rlig id&eacute; at bruge Internet Explorer&amp;nbsp;6</a>.',
+	'ie6msg_titre' => '<NEW>Servicemeddelelse: Du bruger en &aelig;ldre version af browseren Internet Explorer',
+
+	// T
+	'toocool_alt' => '<NEW>Trop cool pour IE',
+	'toocool_info' => '<NEW>Le site que vous tentez d\'afficher a &eacute;t&eacute; jug&eacute; &laquo;&nbsp;trop bien pour Internet Explorer&nbsp;&raquo;.
+<br />Prenez, s\'il vous pla&icirc;t, un moment afin d\'essayer l\'un de ces excellents navigateurs : <a href="http://www.w3junkies.com/toocool/index.php?language=fr">http://www.w3junkies.com/toocool/</a>',
+	'toocool_titre' => '<NEW>Trop Cool pour Internet Explorer'
+);
+
+?>
