99fe797a9f358caf2c8a0beef610fc300335147d
marcimat@free.fr <marcimat@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-02-25 00:10:54 +0000

    CFG, version 2, en développement potentiel. (NE PAS UTILISER, merci !)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@26913 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _stable_/cfg2/extensions/compat/cfg_compat_pipelines.php
@@ -0,0 +1,54 @@
+<?php
+/*
+ * Plugin CFG pour SPIP
+ * (c) toggg, marcimat 2009, distribue sous licence GNU/GPL
+ *
+ */
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+
+/*
+ *  Ajoute le bouton d'amin aux webmestres
+ */
+function cfg_ajouter_boutons($flux) {
+	// si on est admin
+	// et pas en SPIP 2 (qui gere deja le bouton)
+	if (defined('_COMPAT_CFG_192') AND autoriser('configurer','cfg')) {
+	  // on voit le bouton dans la barre "configuration"
+		$flux['configuration']->sousmenu['cfg']= new Bouton(
+		"../"._DIR_PLUGIN_CFG."cfg-22.png",  // icone
+		_T('cfg:CFG'));
+	}
+	return $flux;
+}
+
+
+/*
+ * - Gerer l'option <!-- head= xxx --> (/!\ DEPRECIE) 
+ *   des fonds CFG uniquement dans le prive, 
+ * - ajouter la css pour cfg_arbo
+ * 
+ */
+function cfg_header_prive($flux){
+	
+	if (!_request('cfg') || (!_request('exec') == 'cfg')) {
+		return $flux;
+	}
+
+	// Ajout des css de cfg (uniquement balise arbo pour l'instant) dans le header prive
+	$flux .= '<link rel="stylesheet" href="'._DIR_PLUGIN_CFG.'css/cfg.css" type="text/css" media="all" />';
+
+	include_spip('inc/filtres');
+	include_spip('inc/cfg_formulaire');
+	$config = &new cfg_formulaire(
+				sinon(_request('cfg'), ''),
+				sinon(_request('cfg_id'),''));
+	
+	if ($config->param['head']) 
+		$flux .= "\n".$config->param['head'];
+	
+	return $flux;
+}
+
+?>
---
+++ _stable_/cfg2/extensions/compat/inc/cfg_analyse_args.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ * Plugin CFG pour SPIP
+ * (c) toggg, marcimat 2009, distribue sous licence GNU/GPL
+ *
+ */
+
+// surcharge de la fonction du core de CFG
+// pour autoriser les raccourcis lire_config('~toto')
+// DEPRECIE : declarer le depot systematiquement si different de meta ou metapack
+// lire_config('extrapack::toto')
+//
+// en l'absence du nom de depot (gauche des ::) cette fonction prendra comme suit :
+// ~ en premier caractere : tablepack
+// : present avant un / : tablepack
+// / present : metapack
+// sinon meta
+//
+function inc_cfg_analyse_args($args) {
+	list($depot, $args) = explode('::',$args,2);
+
+	// si un seul argument, il faut trouver le depot
+	if (!$args) {
+		$args = $depot;
+		if ($args[0] == '~'){
+			$depot = 'tablepack';	
+		} elseif (
+			(list($head, $body) = explode('/',$args,2)) &&
+			(strpos($head,':') !== false)) {
+				$depot = 'tablepack';
+		} else {
+			if (strpos($args,'/') !== false)
+				$depot = 'metapack';
+			else 
+				$depot = 'meta';
+		}
+	}
+	
+	return array($depot, $args);	
+}
+
+?>
