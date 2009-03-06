0531f4e012a68f40c207517ef257fc3a4fd6ba83
marcimat@free.fr <marcimat@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-03-06 21:23:51 +0000

    - corrections de SPIP Clear pour ne pas forcer "squelette-dist" si on n'est pas dans un sectur de Spip CLear.
    => SURCHARGE de public/styliser.php pour ajouter un pipeline "styliser" ...
    .. il faudra voir pour reporter ce pipeline, s'il est correct, soit dans le core de SPIP,
    soit dans un plugin séparé.
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@27110 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _dev_/spipclear/public/styliser.php
@@ -0,0 +1,101 @@
+<?php
+
+/*
+ * surcharge de SPIP 2.0.5
+ * pour tester un pipeline
+ * permettant de selectionner d'autres fond
+ * que celui par defaut dans SPIP
+ */
+
+/***************************************************************************\
+ *  SPIP, Systeme de publication pour l'internet                           *
+ *                                                                         *
+ *  Copyright (c) 2001-2009                                                *
+ *  Arnaud Martin, Antoine Pitrou, Philippe Riviere, Emmanuel Saint-James  *
+ *                                                                         *
+ *  Ce programme est un logiciel libre distribue sous licence GNU/GPL.     *
+ *  Pour plus de details voir le fichier COPYING.txt ou l'aide en ligne.   *
+\***************************************************************************/
+
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+// Ce fichier doit imperativement definir la fonction ci-dessous:
+
+// Actuellement tous les squelettes se terminent par .html
+// pour des raisons historiques, ce qui est trompeur
+
+// http://doc.spip.org/@public_styliser_dist
+function public_styliser_dist($fond, $id_rubrique, $lang='', $connect='', $ext='html') {
+	
+	// Trouver un squelette de base dans le chemin
+	if (!$base = find_in_path("$fond.$ext")) {
+		// Si pas de squelette regarder si c'est une table
+		$trouver_table = charger_fonction('trouver_table', 'base');
+		if (preg_match('/^table:(.*)$/', $fond, $r)
+		AND $table = $trouver_table($r[1], $connect)
+		AND include_spip('inc/autoriser')
+		AND autoriser('webmestre')
+		) {
+				$fond = $r[1];
+				$base = _DIR_TMP . 'table_'.$fond . ".$ext";
+				if (!file_exists($base)
+				OR  $GLOBALS['var_mode']) {
+					$vertebrer = charger_fonction('vertebrer', 'public');
+					ecrire_fichier($base, $vertebrer($table));
+				}
+		} else { // on est gentil, mais la ...
+			include_spip('public/debug');
+			erreur_squelette(_T('info_erreur_squelette2',
+				array('fichier'=>"'$fond'")),
+				$GLOBALS['dossier_squelettes']);
+			$f = find_in_path(".$ext"); // on ne renvoie rien ici, c'est le resultat vide qui provoquere un 404 si necessaire
+			return array(substr($f, 0, -strlen(".$ext")), $ext, $ext, $f);
+		}
+	}
+
+	// supprimer le ".html" pour pouvoir affiner par id_rubrique ou par langue
+	$squelette = substr($base, 0, - strlen(".$ext"));
+
+	// pipeline styliser
+	$squelette = pipeline('styliser', array(
+		'args' => array(
+			'id_rubrique' => $id_rubrique,
+			'ext' => $ext,
+			'fond' => $fond,
+			'lang' => $lang,
+			'connect' => $connect
+		),
+		'data' => $squelette,
+	));
+	
+	// On selectionne, dans l'ordre :
+	// fond=10
+	if ($id_rubrique) {
+		$f = "$squelette=$id_rubrique";
+		if (@file_exists("$f.$ext"))
+			$squelette = $f;
+		else {
+			// fond-10 fond-<rubriques parentes>
+			do {
+				$f = "$squelette-$id_rubrique";
+				if (@file_exists("$f.$ext")) {
+					$squelette = $f;
+					break;
+				}
+			} while ($id_rubrique = quete_parent($id_rubrique));
+		}
+	}
+
+	// Affiner par lang
+	if ($lang) {
+		$l = lang_select($lang);
+		$f = "$squelette.".$GLOBALS['spip_lang'];
+		if ($l) lang_select();
+		if (@file_exists("$f.$ext"))
+			$squelette = $f;
+	}
+
+	return array($squelette, $ext, $ext, "$squelette.$ext");
+}
+?>
---
+++ _dev_/spipclear/spipclear_pipelines.php
@@ -0,0 +1,50 @@
+<?php
+
+// utiliser le pipeline 'styliser' pour
+// définir le squelette a utiliser si on est dans le cas
+// d'une rubrique de spipClear
+
+function spipclear_styliser($flux){
+	// si article, rubrique ou sommaire,
+	// on cherche si spip clear doit s'activer
+	if (($fond = $flux['args']['fond'])
+	AND in_array($fond, array('article','rubrique','sommaire'))) {
+		
+		$ext = $flux['args']['ext'];
+		spip_log($fond,'clear');
+
+spip_log('sommaire','clear');
+spip_log(lire_config('spipclear/sommaire_spipclear'),'clear');
+spip_log(lire_config('spipclear/secteurs'),'clear');
+
+		// cas du sommaire
+		if ($fond == 'sommaire') {
+			// uniquement si configuration de spipClear pour le sommaire
+			if (lire_config('spipclear/sommaire_spipclear') == 'on') {
+				if ($squelette = test_squelette_spipclear($fond, $ext)) {
+					$flux['data'] = $squelette;
+				}
+			}
+		}
+		
+		// cas dans une rubrique
+		// uniquement si configuration de spipClear pour le secteur en question
+		elseif ($id_rubrique = $flux['args']['id_rubrique']) {
+			$id_secteur = sql_getfetsel('id_secteur', 'spip_rubriques', 'id_rubrique=' . intval($id_rubrique));
+			if (in_array($id_secteur, lire_config('spipclear/secteurs', 1))) {
+				if ($squelette = test_squelette_spipclear($fond, $ext)) {
+					$flux['data'] = $squelette;
+				}
+			}
+		}
+	}
+	return $flux;
+}
+
+function test_squelette_spipclear($fond, $ext) {
+	if ($squelette = find_in_path($fond."_spipclear.$ext")) {
+		return substr($squelette, 0, -strlen(".$ext"));
+	}
+	return false;
+}
+?>
