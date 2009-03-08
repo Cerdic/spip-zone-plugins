904411210d28daa46af5a3342608d06d0bcffc68
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-03-08 21:38:43 +0000

    On prend en compte le nouveau pipeline sur image_graver...
    Du coup uniquement les images dans le public en bénéficieront puisque je doute que les vignettes du privé passent par cette fonction
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@27150 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/smush_images/smush_pipelines.php
+++ _dev_/smush_images/smush_pipelines.php
@@ -1,16 +1,12 @@
 <?php
 
-function smush_post_ecrire_fichier($flux) {
-
-    spip_log('SMUSH : pipeline post transformation','smush');
-    
-    $chemin = $flux['args']['chemin'];
-    if(preg_match(',^image_,',$flux['args']['action'])){
-		spip_log('SMUSH : pipeline post transformation sur :'.$chemin,'smush');
-		//charge les fonctions necessaire
-		$smush_image = charger_fonction('smush_image','inc');
-		$smush_image($chemin);
-    }
+function smush_post_image_filtrer($flux) {
+	spip_log('SMUSH : pipeline post_image_filtrer sur :'.$flux,'smush');
+	
+	//charge la fonction necessaire
+	$smush_image = charger_fonction('smush_image','inc');
+	$smush_image($flux);
+	
 	return $flux;
 }
 ?>
