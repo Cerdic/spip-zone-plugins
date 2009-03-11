036966058f83e1f8664917a358b262552126d5a3
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-03-11 18:30:57 +0000

    on continue à séparer les choses...
    La fonction de traitement d'image part dans inc/ et là on est clean pour séparer la génération en plusieurs fonctions...
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@27229 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/odt2spip/version_0.2_dev/action/odt2spip_importe.php
+++ _stable_/odt2spip/version_0.2_dev/action/odt2spip_importe.php
@@ -1,7 +1,7 @@
 <?
 
 function action_odt2spip_importe() {
-    global $visiteur_session, $spip_version_code;;
+    global $visiteur_session;
     
     $id_auteur = $visiteur_session['id_auteur'];
     $arg = _request('arg');
@@ -18,7 +18,7 @@ function action_odt2spip_importe() {
     include_spip("inc/securiser_action");
     
     if (!autoriser('creerarticledans', 'rubrique', $id_rubrique)) die(_T('avis_non_acces_page'));
-		
+
 	// les chemins a utiliser
 	$rep_IMG = _DIR_RACINE._NOM_PERMANENTS_ACCESSIBLES;
     
@@ -43,15 +43,15 @@ function action_odt2spip_importe() {
   // dezipper le fichier odt a la mode SPIP
     include_spip("inc/pclzip");
     $zip = new PclZip($rep_dezip.$fichier_zip);
-	  $ok = $zip->extract(
-        PCLZIP_OPT_PATH, $rep_dezip,
-        PCLZIP_OPT_SET_CHMOD, _SPIP_CHMOD,
-        PCLZIP_OPT_REPLACE_NEWER
-	  );
-	  if ($zip->error_code < 0) {
-		    spip_log('charger_decompresser erreur zip ' . $zip->error_code .' pour fichier ' . $rep_dezip.$fichier_zip);
-		    die($zip->errorName(true));  //$zip->error_code
-	  }
+	$ok = $zip->extract(
+		PCLZIP_OPT_PATH, $rep_dezip,
+		PCLZIP_OPT_SET_CHMOD, _SPIP_CHMOD,
+		PCLZIP_OPT_REPLACE_NEWER
+	);
+	if ($zip->error_code < 0) {
+		spip_log('charger_decompresser erreur zip ' . $zip->error_code .' pour fichier ' . $rep_dezip.$fichier_zip);
+		die($zip->errorName(true));  //$zip->error_code
+	}
     
 	// variables en dur pour xml en entree et xslt utilisee
 	// $xml_entre = $rep_dezip.'content.xml';  // chemin du fichier xml a lire  !!! ce chemin absolu ne fonctionne pas pour PHP4 !!!
@@ -62,9 +62,9 @@ function action_odt2spip_importe() {
 	$fichier_sortie = $rep_dezip.'snippet_odt2spip.xml';
 
 	// determiner si le plugin enluminure_typo ou intertitres_enrichis est present & actif
-    include_spip('inc/plugin');
-    $Tplugins = liste_plugin_actifs();
-    $intertitres_riches = ((array_key_exists('TYPOENLUMINEE', $Tplugins) OR array_key_exists('INTERTITRESTDM', $Tplugins)) ? 'oui' : 'non'); 
+	include_spip('inc/plugin');
+	$Tplugins = liste_plugin_actifs();
+	$intertitres_riches = ((array_key_exists('TYPOENLUMINEE', $Tplugins) OR array_key_exists('INTERTITRESTDM', $Tplugins)) ? 'oui' : 'non'); 
     
 	// appliquer la transformation XSLT sur le fichier content.xml
 	// daterminer si on est en php 4 ou php 5 pour choisir les fonctions xslt a utiliser
@@ -74,7 +74,7 @@ function action_odt2spip_importe() {
         $proc = new XSLTProcessor();
 
 		// passage d'un parametre a la xslt
-        $proc->setParameter(null, 'IntertitresRiches', $intertitres_riches);   
+		$proc->setParameter(null, 'IntertitresRiches', $intertitres_riches);   
         
         $xml = new DOMDocument();
         $xml->load($xml_entre);
@@ -94,12 +94,16 @@ function action_odt2spip_importe() {
     // virer les sauts de ligne multiples
     $xml_sortie = preg_replace('/([\r\n]{2})[ \r\n]*/m', "$1", $xml_sortie);
         
-    // traiter les images: dans tous les cas il faut les integrer dans la table documents 
-    // en 2.0 c'est mode image + les fonctions de snippets font la liaison => on bloque la liaison en filant un id_article vide
-	$id_article_tmp = '';    
+	// traiter les images: dans tous les cas il faut les integrer dans la table documents 
+	// en 2.0 c'est mode image + les fonctions de snippets font la liaison => on bloque la liaison en filant un id_article vide
+
 	preg_match_all('/<img([;a-zA-Z0-9\.]*)/', $xml_sortie, $match, PREG_PATTERN_ORDER);
+
 	if (@count($match) > 0) {
-		include_spip('inc/ajouter_document');
+		if(!isset($odt2spip_retailler_img))
+            $odt2spip_retailler_img = charger_fonction('odt2spip_retailler_img','inc');
+		if(!isset($ajouter_documents))
+			$ajouter_documents = charger_fonction('ajouter_documents','inc');
         $T_images = array();
         foreach($match[1] as $ch) {
             $Tdims = explode(';;;', $ch);
@@ -108,11 +112,9 @@ function action_odt2spip_importe() {
               // retailler l'image en fct des parametres ;;;largeur;;;hauteur;;;
                 $largeur = round($Tdims[1]*$conversion_image);
                 $hauteur = round($Tdims[2]*$conversion_image);
-                odt2spip_retailler_img($rep_pictures.$img, $largeur, $hauteur);
-				// integrer l'image comme document spip
-                $ajouter_documents = charger_fonction('ajouter_documents','inc');
+                $odt2spip_retailler_img($rep_pictures.$img, $largeur, $hauteur);
                 $type = 'image';
-                if ($id_document = $ajouter_documents($rep_pictures.$img, $img, "article", $id_article_tmp, $type, 0, $toto='')) { 
+                if ($id_document = $ajouter_documents($rep_pictures.$img, $img, "article", "", $type, 0, $toto='')) { 
 				// uniformiser la sortie: si on est en 1.9.2 inc_ajouter_documents_dist() retourne le type de fichier (extension) alors qu'en 2.0 c'est l'id_document
 					if (!is_numeric($id_document)) {
 						$Ttmp = explode('.', $img);
@@ -164,50 +166,5 @@ function action_odt2spip_importe() {
     
 	// aller sur la page de l'article qui vient d'etre cree
     redirige_par_entete(parametre_url(str_replace("&amp;","&",urldecode($redirect)),'id_article',$id_article,'&'));
-}
-
-// retailler une image : (ne gere que les images GIF, JPG et PNG)
-// $img_ini = CHEMIN+NOM_FICHIER de l'img initiale, $l et $h = largeur et hauteur max de l'image finale
-// gestion de la transparence des PNG : code de matt1walsh@gmail.com sur http://fr2.php.net/manual/fr/function.imagecopyresampled.php
-function odt2spip_retailler_img($img_ini, $l = '', $h = 400) {
-    if (!file_exists($img_ini)) return 'Le fichier '.$img_ini.' n\'existe pas';
-	// determiner le type de fonction de creation d'image a utiliser 
-    $param_img = getimagesize($img_ini);
-    $type_img = $param_img[2];
-    switch ($type_img) {
-        case 1 :
-            $fct_creation_ext = 'imagecreatefromgif';
-            $fct_ecrire = 'imagegif';
-        break;
-        case 2 :
-            $fct_creation_ext = 'imagecreatefromjpeg';
-            $fct_ecrire = 'imagejpeg';
-        break;
-        case 3 :
-            $fct_creation_ext = 'imagecreatefrompng';
-            $fct_ecrire = 'imagepng';
-        break;
-        default :
-            return;
-        break;
-    } 
-	// calculer le ratio a appliquer aux dimensions initiales
-    $l_ini = $param_img[0];
-    $h_ini = $param_img[1];
-    $ratio = ($l != '' ? (abs($l_ini - $l) >= abs($h_ini - $h) ? $l/$l_ini : $h/$h_ini) : $h/$h_ini); 
-    $img_nv = imagecreatetruecolor($l_ini*$ratio, $h_ini*$ratio); 
-    $img_acopier = $fct_creation_ext($img_ini);
-	
-    // gerer la transparence pour les images PNG (le mec qui a trouve ce code est genial! :-)
-    if ($type_img == 3) {
-        imagecolortransparent($img_nv, imagecolorallocate($img_nv, 0, 0, 0));
-        imagealphablending($img_nv, false);
-        imagesavealpha($img_nv, true);
-    }
-    imagecopyresampled($img_nv, $img_acopier, 0, 0, 0, 0, $l_ini*$ratio, $h_ini*$ratio, $l_ini, $h_ini);                     
-	// sauvegarder l'image et eventuellement detruire le fichier image initial
-    $fct_ecrire($img_nv, $img_ini);
-    imagedestroy($img_nv);
-    imagedestroy($img_acopier);
 }
 ?>
