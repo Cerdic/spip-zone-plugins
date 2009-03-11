ffc3cccb95aefaee341bcf7ca61415e19e3484ed
cy_altern@yahoo.fr <cy_altern@yahoo.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-03-11 00:00:58 +0000

    dépot initial de la version de dev.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@27207 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _stable_/odt2spip/version_0.2_dev/odt2spip_pipelines.php
@@ -0,0 +1,39 @@
+<?php
+		 $p = explode(basename(_DIR_PLUGINS)."/", str_replace('\\','/',realpath(dirname(__FILE__))));
+		 define('_DIR_PLUGIN_ODT2SPIP',(_DIR_PLUGINS.end($p)));
+
+function odt2spip_affiche_droite($flux){
+	global $spip_version_code;
+  $args = $flux['args'];
+	$out = "";
+  $id_rubrique = $args['id_rubrique'];
+	if ($args['exec']=='naviguer' AND $args['id_rubrique'] AND $args['id_rubrique'] != 0) {
+    $retour_final = ($spip_version_code > 2 ? './' : _DIR_RESTREINT_ABS).'?exec=articles&id_article=';
+//    $retour = generer_action_auteur('snippet_importe',"articles:articles:id_rubrique=$id_rubrique:$fichier_odt2spip",$retour_final);
+    $action = generer_action_auteur('odt2spip_importe',"id_rubrique=$id_rubrique",$retour_final);
+    $out .= icone_horizontale(_T("odtspip:importer_fichier"), "#", "", _DIR_PLUGIN_ODT2SPIP."images/odt-24.png", false, "onclick='$(\"#boite_odt2spip\").slideToggle(\"fast\");'");
+    $out .= "<div id='boite_odt2spip' style='display:none;' >\n";
+    $out .= debut_cadre_relief('',true);
+    $out .= "<form action='$action' method='POST' enctype='multipart/form-data'>";
+    $out .= form_hidden($action);
+    $out .= "<strong><label for='id_article'>"._T("odtspip:choix_fichier")."</label></strong> ";
+    $out .= "<br />";
+    $out .= "<input type='file' name='fichier_odt' id='fichier_odt' class='formo'>";
+    $out .= "<br /><small>";
+    $out .= "<strong>"._T("odtspip:attacher_fichier_odt")."</strong> ";
+    $out .= "<label for='attacher_oui'>"._T("odtspip:oui")."</label>";
+    $out .= "<input type='radio' name='attacher_odt' value='1' id='attacher_oui' checked='checked'/>";
+    $out .= "<input type='radio' name='attacher_odt' value='0' id='attacher_non'/>";
+    $out .= "<label for='attacher_non'>"._T("odtspip:non")."</label>";
+    $out .= "</small><br />";
+    $out .= "<div style='text-align:$spip_lang_right'>";
+    $out .= "<input type='submit' name='Valider' value='"._T('bouton_valider')."' class='fondo'>";
+    $out .= "</div>";
+    $out .= "</form>\n";
+	  $out .= fin_cadre_relief(true)."</div>";
+	}
+	$flux['data'].= $out;
+	return $flux;
+}
+
+?>
