7f71bc9045b15b64bfafaabcb1460d477924df6a
pierre.basson@gmail.com <pierre.basson@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-03-11 16:00:56 +0000

    spip-lettres : langues
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@27228 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/spip-lettres/lang/lettres_de.php
+++ _dev_/spip-lettres/lang/lettres_de.php
@@ -5,15 +5,13 @@
 	 * SPIP-Lettres
 	 *
 	 * Copyright (c) 2006-2009
-	 * Agence ArtÃ©go http://www.artego.fr
+	 * Agence Artégo http://www.artego.fr
 	 *  
 	 * Ce programme est un logiciel libre distribue sous licence GNU/GPLv3.
 	 * Pour plus de details voir http://www.gnu.org/licenses/gpl-3.0.html
 	 *  
-	 *  
 	 * fichier de langue allemand traduit par Objectif Sciences
 	 * http://asso.objectif-sciences.com
-	 *  
 	 *  
 	 **/
 
@@ -26,9 +24,6 @@
 		'themes' => "Themen",
 		'racine' => "Wurzel der Website",
 		'tout_le_site' => "Komplette Website",
-		'aucun_theme_disponible' => "Momentan kein Thema vorhanden",
-		'champs_obligatoires_erreur' => "Gewisse Pflicht-Felder (*) wurden nicht korrekt ausgefüllt",
-		'champs_obligatoires' => "Pflicht-Felder",
 		'email' => "Email",
 		'nom' => "Name",
 		'format' => "Format",
@@ -44,13 +39,13 @@
 		// formulaire lettres messages
 		'validation_abonnements_succes' => "Ihre Abonnements wurden bestätigt.",
 		'validation_abonnements_erreur' => "Ihre Abonnements konnten nicht bestätigt werden.",
-		'validation_desabonnements_succes' => "Ihre Abonnements wurden abbestellt:",
+		'validation_desabonnements_succes' => "Ihre Abonnements wurden abbestellt.",
 		'validation_desabonnements_erreur' => "Ihre Abonnements konnten nicht abbestellt werden.",
 		'validation_changement_format_succes' => "Ihre Änderungen wurden übernommen.",
 		'validation_changement_format_erreur' => "Ihre Änderungen konnten nicht übernommen werden.",
-		'envoi_abonnements_succes' => "Sie erhalten eine Mail zur Bestätigung Ihres Abonnements:",
+		'envoi_abonnements_succes' => "Sie erhalten eine Mail zur Bestätigung Ihres Abonnements.",
 		'envoi_abonnements_erreur' => "Die Mail zur Bestätigung Ihres Abonnements konnte nicht versendet werden.",
-		'envoi_desabonnements_succes' => "Sie erhalten eine Mail zur Bestätigung Ihres Abmeldung Ihres Abonnements:",
+		'envoi_desabonnements_succes' => "Sie erhalten eine Mail zur Bestätigung Ihres Abmeldung Ihres Abonnements.",
 		'envoi_desabonnements_erreur' => "Die Mail zur Bestätigung der Abmeldung Ihres Abonnements konnte nicht versendet werden.",
 		'envoi_changement_format_succes' => "Sie erhalten eine Mail zur Bestätigung Ihrer Formatänderungen.",
 		'envoi_changement_format_erreur' => "Die Mail zur Bestätigung der Formatänderungen konnte nicht versendet werden.",
