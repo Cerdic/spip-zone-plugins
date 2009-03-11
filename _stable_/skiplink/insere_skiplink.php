c81cc1970c61cd9f43e37bdaa024a17c0401a5b6
romy@tetue.net <romy@tetue.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-03-11 02:33:28 +0000

    vous allez pouvoir jouer au yoyo
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@27214 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _stable_/skiplink/insere_skiplink.php
@@ -0,0 +1,24 @@
+<?php
+
+# inserer les liens dans la page
+function skiplink_affichage_final(&$page) {
+	// ne pas se fatiguer si pas HTML
+	if (!($GLOBALS['html']
+	AND !strpos($page, '<p id="raccourcis">') # pas deux fois, au cas ou !
+	))
+		return $page;
+  
+	if (!function_exists('recuperer_fond')) include_spip('public/assembler');
+	$raccourcis = recuperer_fond('raccourcis', array('lang'=>$GLOBALS['spip_lang']));
+	preg_match(',<body\b.*?>,i', $page, $regs);
+	if ($regs)
+		$page = substr_replace($page, $raccourcis, strpos($page, $regs[0]), 0);
+	$remonter = recuperer_fond('remonter', array('lang'=>$GLOBALS['spip_lang']));
+	preg_match(',<\/body\b.*?>,i', $page, $regs);
+	if ($regs)
+		$page = substr_replace($page, $remonter, strpos($page, $regs[0]), 0);
+
+	return $page;
+}
+
+?>
