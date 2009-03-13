1ca3212edc19aa126ba1150654e1c54c30d83ada
vincent@ldd.fr <vincent@ldd.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-03-13 15:38:37 +0000

    Et hop fonctionnalité supplémentaire : - d'abord j'ai rajouté une option permettant d'envoyé le mail à TOUS les auteurs sans que le visiteur choisisse - ensuite et surtout j'ai rajouté la possibilité d'avoir des champs supplémentaires, et à l'infini puisque ça passe dans un pipeline() assez simple d'utilisation
    Étape suivante : PHPMailer et pièces jointes.
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@27278 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/contact_avance/formulaires/contact_avance.php
+++ _stable_/contact_avance/formulaires/contact_avance.php
@@ -6,27 +6,53 @@ function formulaires_contact_avance_charger_dist(){
 	$valeurs['email_contact'] = '';
 	$valeurs['sujet_contact'] = '';
 	$valeurs['texte_contact'] = '';
-	$valeurs['destinataire'] = '';
+	$valeurs['destinataire'] = array();
 	$valeurs['choix_destinataires'] = '';
 	
+	// La liste dans laquelle on pourra éventuellement choisir
 	$choix_destinataires = lire_config('contactavance/choix_destinataires');
+	// Le type de choix
+	$valeurs['type_choix'] = $type_choix = lire_config('contactavance/type_choix');
+	
 	// Rien n'a été défini, on utilise l'auteur 1
 	if (count($choix_destinataires) == 0){
-		$valeurs['destinataire'] = 1;
+		$valeurs['destinataire'][] = 1;
 	}
-	// S'il n'y a qu'un seul choix, on l'utilise directement
-	else if (count($choix_destinataires) == 1){
-		$valeurs['destinataire'] = intval($choix_destinataires[0]);
+	// S'il n'y a qu'un seul choix OU que le type est "tous", on l'utilise directement
+	else if ((count($choix_destinataires) == 1) or ($type_choix == 'tous')){
+		$valeurs['destinataire'] = $choix_destinataires;
 	}
 	// S'il y a plusieurs choix, on s'assure que ce sont tous des entiers
 	else{
 		$valeurs['choix_destinataires'] = array_map('intval', $choix_destinataires);
 	}
 	
-	if (lire_config('contactavance/plusieurs_destinataires') == 'oui')
-		$valeurs['plusieurs_destinataires'] = true;
+	// Les infos supplémentaires
+	$champs_possibles = contactavance_infos_supplementaires();
+	if (!is_array($champs_choisis = lire_config('contactavance/champs')))
+		$valeurs['champs'] = false;
+	else{
+		// On envoie un talbeau contenant tous les champs choisis et leur titre
+		// DANS L'ORDRE de ce qu'on a récupéré de CFG
+		$champs_choisis = array_flip($champs_choisis);
+		foreach ($champs_choisis as $cle => $valeur){
+			$champs_choisis[$cle] = $champs_possibles[$cle];
+		}
+		$valeurs['champs'] = $champs_choisis;
+		// Mais aussi tous les champs un par un
+		$valeurs = array_merge(
+			$valeurs,
+			array_map(
+				create_function('', 'return "";'),
+				$champs_choisis
+			)
+		);
+	}
+	
+	if (!is_array($champs_obligatoires = lire_config('contactavance/obligatoires')))
+		$valeurs['obligatoires'] = false;
 	else
-		$valeurs['plusieurs_destinataires'] = false;
+		$valeurs['obligatoires'] = $champs_obligatoires;
 	
 	return $valeurs;
 }
@@ -35,11 +61,22 @@ function formulaires_contact_avance_verifier_dist(){
 	$erreurs = array();
 	include_spip('inc/filtres');
 	
+	if (!_request('destinataire'))
+		$erreurs['destinataire'] = _T("info_obligatoire");
 	if (!$adres = _request('email_contact'))
 		$erreurs['email_contact'] = _T("info_obligatoire");
 	elseif(!email_valide($adres))
 		$erreurs['email_contact'] = _T('form_prop_indiquer_email');
-
+	
+	$champs_choisis = lire_config('contactavance/champs');
+	$champs_obligatoires = lire_config('contactavance/obligatoires');
+	if (is_array($champs_choisis) and is_array($champs_obligatoires)){
+		foreach($champs_choisis as $champ){
+			if (!_request($champ) and in_array($champ, $champs_obligatoires))
+				$erreurs[$champ] = _T("info_obligatoire");
+		}
+	}
+	
 	if (!$sujet=_request('sujet_contact'))
 		$erreurs['sujet_contact'] = _T("info_obligatoire");
 	elseif(!(strlen($sujet)>3))
@@ -61,9 +98,12 @@ function formulaires_contact_avance_verifier_dist(){
 
 function formulaires_contact_avance_traiter_dist(){
 	
+	include_spip('base/abstract_sql');
+	
 	$adres = _request('email_contact');
-	$sujet=_request('sujet_contact');
-	$texte=_request('texte_contact');
+	$sujet = _request('sujet_contact');
+	$texte = "\n\n"._request('texte_contact');
+	$infos = '';
 	
 	// On récupère à qui ça va être envoyé
 	$destinataire = _request('destinataire');
@@ -78,7 +118,18 @@ function formulaires_contact_avance_traiter_dist(){
 	$mail = array_map('reset', $mail);
 	$mail = join(', ', $mail);
 	
+	// Les infos supplémentaires
+	$champs_possibles = contactavance_infos_supplementaires();
+	$champs_choisis = lire_config('contactavance/champs');
+	if (is_array($champs_choisis)){
+		foreach($champs_choisis as $champ){
+			$infos .= "\n".$champs_possibles[$champ]." : "._request($champ);
+		}
+	}
+	$texte = $infos.$texte;
 	$texte .= "\n\n-- "._T('envoi_via_le_site')." ".supprimer_tags(extraire_multi($GLOBALS['meta']['nom_site']))." (".$GLOBALS['meta']['adresse_site']."/) --\n";
+	
+	spip_log($texte);
 	$envoyer_mail = charger_fonction('envoyer_mail','inc');
 	$envoyer_mail($mail, $sujet, $texte, $adres,
 				"X-Originating-IP: ".$GLOBALS['ip']);
--- _stable_/contact_avance/lang/contactavance_fr.php
+++ _stable_/contact_avance/lang/contactavance_fr.php
@@ -3,14 +3,32 @@
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-
+	'champ_obligatoire' => 'Obligatoire ?',
+	'champ_prenom' => 'Pr&eacute;nom',
+	'champ_nom' => 'Nom',
+	'champ_organisation' => 'Organisation',
+	'champ_telephone' => 'T&eacute;l&eacute;phone',
+	'champ_portable' => 'Portable',
+	'champ_adresse' => 'Adresse',
+	'champ_code_postal' => 'Code postal',
+	'champ_ville' => 'Ville',
+	'champ_pays' => 'Pays',
+	
 	'configuration_description' => 'Permet de configurer le formulaire de contact.',
 	'configuration_destinataires' => 'Destinataires',
 	'configuration_destinataires_explication' => 'Choisissez un ou plusieurs auteurs que les visiteurs pourront contacter. Si rien n\'est d&eacute;fini, c\'est l\'auteur num&eacute;ro 1 (celui qui a install&eacute; le site) qui sera utilis&eacute;.',
+	'configuration_destinataires_tous' => 'Mettre <b>tous</b> ces auteurs en destinataires du courriel',
+	'configuration_destinataires_un' => 'Permettre aux visiteurs de choisir <b>un seul</b> destinataire',
+	'configuration_destinataires_plusieurs' => 'Permettre aux visiteurs de choisir <b>plusieurs</b> destinataires',
 	'configuration_intro' => 'Introduction',
 	'configuration_intro_explication' => '&Eacute;crivez &eacute;ventuellement un texte qui introduira le formulaire.',
-	'configuration_plusieurs_destinataires' => 'Autoriser les visiteurs &agrave; &eacute;crire &agrave; plusieurs personnes en m&ecirc;me temps',
-	'form_destinataire' => 'Destinataire',
+	
+	'courriel_de' => 'De',
+	'courriel_pour' => 'Pour',
+	
+	'form_destinataire' => 'Choisissez un destinataire',
+	'form_destinataires' => 'Choisissez les destinataires',
+	'infos_supplementaires' => 'Informations suppl&eacute;mentaires',
 	'titre' => 'Formulaire de contact'
 );
 
