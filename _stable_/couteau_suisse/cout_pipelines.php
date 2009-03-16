e3ea4f52b0015e546ec6aa97ecd9586d5589327a
patfr@ifrance.com <patfr@ifrance.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-03-16 12:05:53 +0000

    include de précaution
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@27354 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/couteau_suisse/cout_pipelines.php
+++ _stable_/couteau_suisse/cout_pipelines.php
@@ -23,6 +23,7 @@ cs_log("couteau_suisse_install($action)");
 			// affichage d'un lien ici, puisque le pipeline 'affiche_gauche' n'est pas pris en compte dans 'admin_plugin'...
 			if (_request('exec') == 'admin_plugin') {
 				if(!defined('_SPIP19300')) echo '<br/>';
+				include_spip('inc/presentation');
 				echo debut_cadre_enfonce('', true),
 					icone_horizontale(_T('couteau:titre'), generer_url_ecrire('admin_couteau_suisse'), find_in_path('img/couteau-24.gif'), '', false),
 					fin_cadre_enfonce(true);
