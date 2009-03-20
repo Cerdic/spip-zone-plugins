286e55fac9de3f738db9f560335c33e950cdcbae
brunobergot@gmail.com <brunobergot@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-03-20 15:14:08 +0000

    mise à jour du formulaire_editer_gis : - gestion du zoom - possibilité d'afficher le champ de recherche d'adresse dans le formulaire en lui passant le paramètre recherche=oui lors de l'appel (à documenter)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@27442 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/gis/formulaires/editer_gis.php
+++ _dev_/gis/formulaires/editer_gis.php
@@ -1,24 +1,27 @@
 <?php
 
-function formulaires_editer_gis_charger_dist($id_article){
+function formulaires_editer_gis_charger_dist($id_article, $recherche=''){
 
 	$valeurs = array(
 		'lat'=> '',
 		'lonx'=> '',
+		'zoom'=> '',
+		'recherche'=> $recherche,
 		'editable'=>true
 	);
 	
-	$row = sql_fetsel("lat,lonx", "spip_gis", "id_article=$id_article");
+	$row = sql_fetsel("lat,lonx,zoom", "spip_gis", "id_article=$id_article");
 	if ($row) {
 		$valeurs['lat'] = $row['lat'];
 		$valeurs['lonx'] = $row['lonx'];
+		$valeurs['zoom'] = $row['zoom'];
 	}
 	
 	return $valeurs;
 }
 
 
-function formulaires_editer_gis_verifier_dist($id_article){
+function formulaires_editer_gis_verifier_dist($id_article, $recherche=''){
 
 	$erreurs = array();
 
@@ -26,23 +29,24 @@ function formulaires_editer_gis_verifier_dist($id_article){
 }
 
 
-function formulaires_editer_gis_traiter_dist($id_article){
+function formulaires_editer_gis_traiter_dist($id_article, $recherche=''){
 	$res = array('editable'=>' ');
 	
 	// recuperation des donnees
 	$lat = _request('lat');
 	$lonx = _request('lonx');
+	$zoom = _request('zoom');
 	
 	include_spip('base/abstract_sql');
 	
 	// mise a jour ou creation ?
 	if ($id_gis = sql_getfetsel("id_gis", "spip_gis", "id_article=$id_article")) {
 		sql_updateq("spip_gis",
-					array("id_article" => $id_article , "lat" => $lat, "lonx" => $lonx),
+					array("id_article" => $id_article , "lat" => $lat, "lonx" => $lonx, "zoom" => $zoom),
 					"id_gis=$id_gis");
 		$res['message_ok'] = _T('gis:coord_maj');
 	}else{
-		sql_insertq("spip_gis",  array("id_article" => $id_article , "lat" => $lat, "lonx" => $lonx));
+		sql_insertq("spip_gis",  array("id_article" => $id_article , "lat" => $lat, "lonx" => $lonx, "zoom" => $zoom));
 		$res['message_ok'] = _T('gis:coord_enregistre');
 	}
 	return $res;
