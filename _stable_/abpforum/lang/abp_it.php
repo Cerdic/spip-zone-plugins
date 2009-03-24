2253873df7e4be893e60c429c0ab8c14deaa0334
mmmxvvv@tiscali.it <mmmxvvv@tiscali.it@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-03-24 12:56:33 +0000

    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@27502 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _stable_/abpforum/lang/abp_it.php
@@ -0,0 +1,9 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// Aide des boutons supplémentaires de la barre typo
+'bouton_radio_abp' => 'Su Abbonamento &amp; a Priori'
+);
+?>
