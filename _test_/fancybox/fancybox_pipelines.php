e37a349d758679706bfa6a4e0313b22799d8778d
brunobergot@gmail.com <brunobergot@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-03-24 12:37:37 +0000

    et hop un plugin fancybox pompé sur nyroceros
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@27498 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _test_/fancybox/fancybox_pipelines.php
@@ -0,0 +1,46 @@
+<?php
+
+function fancybox_insert_head($flux){
+	include_spip("inc/filtres");
+	$config = @unserialize($GLOBALS['meta']['fancybox']);
+	if (!is_array($config))
+		$config = array();
+	$config = array_merge(array(
+		'selecteur_galerie' => '#documents_portfolio .fancybox',
+		'selecteur_commun' => '.fancybox',
+		'padding' => '10',
+		'imagescale' => 'true',
+		'overlayshow' => 'true',
+		'overlayopacity' => '0.3',
+		'hideoncontentclick' => 'true'
+	), $config);
+
+	$flux .='<script src="'.url_absolue(find_in_path('javascript/jquery.fancybox-1.2.1.js')).'" type="text/javascript"></script>';
+	
+	$flux .='
+<script type="text/javascript">/* <![CDATA[ */
+(function($){
+	$(function(){
+		$("a[type=\'image/jpeg\'],a[type=\'image/png\'],a[type=\'image/gif\']")
+			.addClass("fancybox")
+			.attr("onclick","")
+			.fancybox();
+		$("'.$config['selecteur_galerie'].'").attr("rel","galerie-portfolio");
+		$("'.$config['selecteur_commun'].'")
+			.fancybox({
+				"padding": '.$config['padding'].',
+				"imageScale": '.$config['imagescale'].',
+				"overlayShow": '.$config['overlayshow'].',
+				"overlayOpacity": '.$config['overlayopacity'].',
+				"hideOnContentClick": '.$config['hideoncontentclick'].'
+			});
+	});
+})(jQuery);
+/* ]]> */</script>';
+
+	$flux .='<link rel="stylesheet" href="'.url_absolue(find_in_path('styles/jquery.fancybox.css')).'" type="text/css" />';
+
+	return $flux;
+}
+
+?>
---
+++ _test_/fancybox/lang/fancybox_fr.php
@@ -0,0 +1,29 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	
+	//C
+	'cfg_boite_fancybox' => 'Configuration du plugin FancyBox',
+	'cfg_descr_fancybox' => 'FancyBox : Plugin jQuery FancyBox pour SPIP',
+	'cfg_inf_selecteur_commun' => 'Indiquez la cible des &eacute;l&eacute;ments qui d&eacute;clencheront FancyBox (expression CSS ou &eacute;tendue jQuery).',
+	'cfg_inf_selecteur_galerie' => 'Indiquez la cible des &eacute;l&eacute;ments qui d&eacute;clencheront une galerie avec FancyBox  (expression CSS ou &eacute;tendue jQuery).',
+	'cfg_inf_padding' => 'Padding autour du contenu de la fen&ecirc;tre (en pixels).',
+	'cfg_inf_imagescale' => 'Redimensionner automatiquement les images &agrave; la taille de la fen&ecirc;tre ?',
+	'cfg_inf_overlayshow' => 'Afficher l\'arri&egrave;re plan de la fen&ecirc;tre ?',
+	'cfg_inf_overlayopacity' => 'Opacit&eacute; de l\'arri&egrave;re-plan (de 0 &agrave; 1).',
+	'cfg_inf_hideoncontentclick' => 'Fermer la fen&ecirc;tre lors d\'un clic sur celle-ci ou à l\'ext&eacute;rieur ?',
+
+	
+	'cfg_lbl_selecteur_commun' => 'En g&eacute;n&eacute;ral',
+	'cfg_lbl_selecteur_galerie' => 'En galerie',
+	'cfg_lbl_padding' => 'Padding',
+	'cfg_lbl_imagescale' => '&Eacute;chelle',
+	'cfg_lbl_overlayshow' => 'Arri&egrave;re-plan',
+	'cfg_lbl_overlayopacity' => 'Opacit&eacute;',
+	'cfg_lbl_hideoncontentclick' => 'Fermer au clic',
+	'cfg_titre_fancybox' => 'FancyBox'
+	
+);
+?>
