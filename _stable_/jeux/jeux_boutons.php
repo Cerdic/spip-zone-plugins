4350794c5f26d1bccd930951c2f26b821dd4cdc4
maieulrouquette@@sfr.fr <maieulrouquette@@sfr.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-03-26 15:02:11 +0000

    avec le plugin bando on déplace ailleurs
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@27531 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/jeux/jeux_boutons.php
+++ _stable_/jeux/jeux_boutons.php
@@ -1,9 +1,12 @@
 <?php
 
-
 function jeux_ajouterBoutons($boutons_admin) {
-		
-		
+    //test si le plugin bando est activé
+	$liste_plugin = unserialize($GLOBALS['meta']['plugin']);
+
+	if (array_key_exists('BANDO',$liste_plugin)==true){
+	   return $boutons_admin;
+	}	
 		  // on voit le bouton dans la barre "naviguer"
 		  $boutons_admin['naviguer']->sousmenu["jeux_tous"]= new Bouton(
 			find_in_path("img/jeu-24.png"),  // icone
@@ -12,6 +15,7 @@ function jeux_ajouterBoutons($boutons_admin) {
 		
 		return $boutons_admin;
 }
+
 
 
 ?>
