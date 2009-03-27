c83b6e3155baf2cd36c9ebfe216d24a8bc0f53aa
patfr@ifrance.com <patfr@ifrance.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-03-27 23:45:44 +0000

    + Mise à jour : jQuery v1.3.2  + Fond pour CFG afin de choisir à la volée la version de jQuery à utiliser pour le site public :     - jQuery v1.1.1 (SPIP v1.9.2 dist)     - jQuery v1.2.6 (SPIP v2.0.x dist)     - jQuery v1.3.2 (SPIP v2.1.x dist)
    Ceci pourra répondre à tous les besoins de sites anciens sous SPIP 1.92.
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@27554 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/jquery_192/jquery_192_options.php
+++ _stable_/jquery_192/jquery_192_options.php
@@ -20,16 +20,17 @@ function f_jQuery_192($texte) {
 			array("#INSERT_HEAD",_T('double_occurrence')))
 		) . $texte;
 	} else {
-		$x = '';
+		$x = "\n";
+		$choix_version = function_exists('lire_config')?lire_config('jquery_192/choix_version','_126'):'_126';
 		foreach (pipeline('jquery_plugins',
 		array(
 			// clefs obligees sans quoi "in_array('data', array_keys($val))" retourne true !!
-			'jquery_'=>'javascript/jquery_.js',
-			'jquery.form_'=>'javascript/jquery.form_.js',
-			'ajaxCallback_'=>'javascript/ajaxCallback_.js'
+			'jquery_'=>"javascript/jquery$choix_version.js",
+			'jquery.form_'=>"javascript/jquery.form$choix_version.js",
+			'ajaxCallback_'=>"javascript/ajaxCallback$choix_version.js"
 		)) as $script)
 			if ($script = find_in_path($script))
-				$x .= "\n<script src=\"$script\" type=\"text/javascript\"></script>\n";
+				$x .= "<script src=\"$script\" type=\"text/javascript\"></script>\n";
 		$texte = $x.$texte;
 	}
 	return $texte;
