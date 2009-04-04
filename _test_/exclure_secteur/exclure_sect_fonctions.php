8181116eeb9d5c457cf87bad079b382a8dc6f354
maieulrouquette@@sfr.fr <maieulrouquette@@sfr.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-04-04 13:29:02 +0000

    un critere tout configurable + breves + id_secteur permet de changer
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@27652 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/exclure_secteur/exclure_sect_fonctions.php
+++ _test_/exclure_secteur/exclure_sect_fonctions.php
@@ -1,34 +1,71 @@
 <?php
+/* un cfg qui marche sur 19x*/
+
+/* criteres*/
 if (!function_exists('critere_tout_secteur_dist')){
 function critere_tout_secteur_dist($idb, &$boucles, $crit) {   
     $boucle = &$boucles[$idb];
     $boucle->modificateur['tout_secteur'] = true;
 }
 }
+function secteur_explicite($crit){
+    
+    foreach($crit as $critere){   
+        if ($critere->param[0][0]->texte == 'id_secteur') {
+                switch ($critere->op){
+                    case '=' :
+                        return true;
+                    case '==':
+                        return true;
+                    case 'IN':
+                        return true;                
+                }                   
+        }
+    }
+    return false;
+}
 
-function exclure_sect_choisir(){
-  $cfg =lire_config('secteur/exclure_sect');
-  if ( $cfg = array_map('sql_quote', $cfg) ) {
-    $cfg = implode($cfg, ',');
-  }
-  else {
-    $cfg = sql_quote('z');
-  }
-
-  return '('.$cfg.')';
+function exclure_sect_choisir($crit){
+    $cfg =lire_config('secteur/exclure_sect');
+    $sect_afficher = secteur_explicite($crit);
+    if ($cfg = array_map('sql_quote', $cfg) and !$sect_afficher ) {
+        $cfg = implode($cfg, ',');
+    }
+    else {
+        $cfg = sql_quote('z');
+    }
+    
+   
+    return '('.$cfg.')';
 
 }
+/* surcharges de boucles*/
+function boucle_BREVES($id_boucle, &$boucles) {
+    
+	$boucle = &$boucles[$id_boucle];
+	$id_table = $boucle->id_table;
+	
+    if(!$boucle->modificateur['tout_secteur'] and !($boucle->modificateur['tout'] and lire_config('secteur/tout') == 'oui')){
+        $exclut = exclure_sect_choisir($crit);       
+        $boucle->where[] = array("'NOT IN'", "'$id_table.id_secteur'",sql_quote($exclut));        
+    }
+
+	return boucle_BREVES_dist($id_boucle, $boucles);
+}
+
 
 
 function boucle_ARTICLES($id_boucle, &$boucles) {
     
 	$boucle = &$boucles[$id_boucle];
 	$id_table = $boucle->id_table;
-	
-    if(!$boucle->modificateur['tout_secteur']){
-        $boucle->where[] = array("'NOT IN'", "'$id_table.id_secteur'", 'exclure_sect_choisir()');    
+	$crit = $boucle->criteres;
+   
+    if(!$boucle->modificateur['tout_secteur'] and !($boucle->modificateur['tout'] and lire_config('secteur/tout') == 'oui')){
+        $exclut = exclure_sect_choisir($crit);       
+        $boucle->where[] = array("'NOT IN'", "'$id_table.id_secteur'",sql_quote($exclut));    
     }
-
+  
 	return boucle_ARTICLES_dist($id_boucle, $boucles);
 }
 
@@ -37,8 +74,9 @@ function boucle_RUBRIQUES($id_boucle, &$boucles) {
 	$boucle = &$boucles[$id_boucle];
 	$id_table = $boucle->id_table;
 	
-    if(!$boucle->modificateur['tout_secteur']){
-        $boucle->where[] = array("'NOT IN'", "'$id_table.id_secteur'", 'exclure_sect_choisir()');
+    if(!$boucle->modificateur['tout_secteur'] and !($boucle->modificateur['tout'] and lire_config('secteur/tout') == 'oui')){
+        $exclut = exclure_sect_choisir($crit);       
+        $boucle->where[] = array("'NOT IN'", "'$id_table.id_secteur'",sql_quote($exclut));    
     
     }
 
@@ -50,8 +88,9 @@ function boucle_SYNDICATION($id_boucle, &$boucles) {
 	$boucle = &$boucles[$id_boucle];
 	$id_table = $boucle->id_table;
 	
-    if(!$boucle->modificateur['tout_secteur']){
-        $boucle->where[] = array("'NOT IN'", "'$id_table.id_secteur'", 'exclure_sect_choisir()');
+    if(!$boucle->modificateur['tout_secteur'] and !($boucle->modificateur['tout'] and lire_config('secteur/tout') == 'oui')){
+        $exclut = exclure_sect_choisir($crit);       
+        $boucle->where[] = array("'NOT IN'", "'$id_table.id_secteur'",sql_quote($exclut));    
     
     }
 
