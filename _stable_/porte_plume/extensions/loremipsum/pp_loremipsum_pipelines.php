68d7cb23d2e5e0fdfab3833c77d0eb2b313ce8fa
marcimat@free.fr <marcimat@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-04-04 08:54:09 +0000

    - lorem ipsum qui ne prend qu'un bouton
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@27645 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/porte_plume/extensions/loremipsum/pp_loremipsum_pipelines.php
+++ _stable_/porte_plume/extensions/loremipsum/pp_loremipsum_pipelines.php
@@ -27,49 +27,51 @@ function pp_loremipsum_porte_plume_barre_pre_charger($barres){
 						. "Proin feugiat, augue non elementum posuere, metus purus iaculis lectus, "
 						. "et tristique ligula justo vitae magna.\n\n",
 		"display"     => true,
+		"dropMenu" => array(
+			// lorem ipsum 3 paragraphes
+			array(
+				"id"          => 'lorem_ipsum_big',
+				"name"        => _T('barre_outils:barre_lorem_ipsum_3'),
+				"className"   => "outil_lorem_ipsum_big", 
+				"replaceWith" => "\n\nLorem ipsum dolor sit amet, consectetuer adipiscing elit. "
+								. "Curabitur lacus mi, varius sit amet, suscipit in, hendrerit "
+								. "sit amet, turpis. Duis in odio. Fusce mauris. Nulla quis ante. "
+								. "Vestibulum id dui. Curabitur quis est ac quam euismod ullamcorper. "
+								. "Phasellus nec justo. Vestibulum id erat sed odio ultrices hendrerit. "
+								. "Duis fermentum, velit ut pretium fermentum, felis turpis rhoncus justo, "
+								. "vel adipiscing nulla lectus sed eros. In hac habitasse platea dictumst. "
+								. "In hac habitasse platea dictumst. Curabitur tellus velit, consequat nec, "
+								. "tincidunt sit amet, posuere vel, ligula. Aenean auctor mollis mi. "
+								. "In adipiscing dolor vel diam. Morbi justo. Maecenas eu risus id mi tincidunt "
+								. "vestibulum.\n\n"
+								. "Maecenas lacinia. Sed aliquet bibendum nisl. Vivamus vulputate, "
+								. "sapien ut molestie iaculis, diam libero porttitor dolor, eget semper orci orci "
+								. "ut sem. Nunc venenatis. Curabitur adipiscing, velit at iaculis dictum, "
+								. "lacus nulla adipiscing mauris, id rhoncus velit nisl ac mauris. Aliquam "
+								. "egestas, sapien sed placerat lacinia, tellus erat tempor quam, at sollicitudin "
+								. "ligula eros sit amet sapien. Nam at dui id libero vehicula sodales. Vestibulum "
+								. "dictum risus eget metus. Cras lorem. Pellentesque lobortis sodales ipsum. "
+								. "Vivamus convallis lectus in nunc. Vivamus metus libero, ullamcorper in, "
+								. "porttitor nec, dapibus id, est. Praesent pede. Sed viverra consequat leo. "
+								. "Mauris pharetra tortor a orci.\n\n"
+								. "Maecenas sed lacus. Phasellus iaculis risus et elit. Morbi sagittis nunc vitae "
+								. "sem. Aliquam ac lorem vel magna ornare malesuada. Pellentesque habitant "
+								. "morbi tristique senectus et netus et malesuada fames ac turpis egestas. "
+								. "Mauris est dolor, aliquam eget, feugiat ut, tempus at, arcu. Duis porta, "
+								. "pede sed hendrerit pellentesque, orci dolor consectetuer risus, id scelerisque "
+								. "tellus ipsum quis felis. Sed ultrices. Nullam eleifend dui sodales massa. "
+								. "Morbi consectetuer pellentesque dui. Vestibulum urna. Fusce congue velit ut "
+								. "erat. Aliquam quis odio sollicitudin ipsum euismod porta. Vivamus pharetra, "
+								. "lacus eu tempor lobortis, diam nisi vulputate lorem, ut aliquet dui neque eu "
+								. "sem. Morbi varius, nisi ac laoreet mollis, pede odio cursus nisi, in imperdiet "
+								. "dolor enim at metus. Nunc pretium pulvinar tortor. Vestibulum euismod ultrices "
+								. "est. Etiam lobortis, enim ut bibendum dictum, urna orci lacinia tortor, "
+								. "at eleifend pede sem eu sem. Morbi a neque. Vestibulum cursus.\n\n", 
+				"display"     => true,
+			),
+		),
 	));
 	
-	// lorem ipsum 3 paragraphes
-	$barre->ajouterApres('lorem_ipsum', array(
-		"id"          => 'lorem_ipsum_big',
-		"name"        => _T('barre_outils:barre_lorem_ipsum_3'),
-		"className"   => "outil_lorem_ipsum_big", 
-		"replaceWith" => "\n\nLorem ipsum dolor sit amet, consectetuer adipiscing elit. "
-						. "Curabitur lacus mi, varius sit amet, suscipit in, hendrerit "
-						. "sit amet, turpis. Duis in odio. Fusce mauris. Nulla quis ante. "
-						. "Vestibulum id dui. Curabitur quis est ac quam euismod ullamcorper. "
-						. "Phasellus nec justo. Vestibulum id erat sed odio ultrices hendrerit. "
-						. "Duis fermentum, velit ut pretium fermentum, felis turpis rhoncus justo, "
-						. "vel adipiscing nulla lectus sed eros. In hac habitasse platea dictumst. "
-						. "In hac habitasse platea dictumst. Curabitur tellus velit, consequat nec, "
-						. "tincidunt sit amet, posuere vel, ligula. Aenean auctor mollis mi. "
-						. "In adipiscing dolor vel diam. Morbi justo. Maecenas eu risus id mi tincidunt "
-						. "vestibulum.\n\n"
-						. "Maecenas lacinia. Sed aliquet bibendum nisl. Vivamus vulputate, "
-						. "sapien ut molestie iaculis, diam libero porttitor dolor, eget semper orci orci "
-						. "ut sem. Nunc venenatis. Curabitur adipiscing, velit at iaculis dictum, "
-						. "lacus nulla adipiscing mauris, id rhoncus velit nisl ac mauris. Aliquam "
-						. "egestas, sapien sed placerat lacinia, tellus erat tempor quam, at sollicitudin "
-						. "ligula eros sit amet sapien. Nam at dui id libero vehicula sodales. Vestibulum "
-						. "dictum risus eget metus. Cras lorem. Pellentesque lobortis sodales ipsum. "
-						. "Vivamus convallis lectus in nunc. Vivamus metus libero, ullamcorper in, "
-						. "porttitor nec, dapibus id, est. Praesent pede. Sed viverra consequat leo. "
-						. "Mauris pharetra tortor a orci.\n\n"
-						. "Maecenas sed lacus. Phasellus iaculis risus et elit. Morbi sagittis nunc vitae "
-						. "sem. Aliquam ac lorem vel magna ornare malesuada. Pellentesque habitant "
-						. "morbi tristique senectus et netus et malesuada fames ac turpis egestas. "
-						. "Mauris est dolor, aliquam eget, feugiat ut, tempus at, arcu. Duis porta, "
-						. "pede sed hendrerit pellentesque, orci dolor consectetuer risus, id scelerisque "
-						. "tellus ipsum quis felis. Sed ultrices. Nullam eleifend dui sodales massa. "
-						. "Morbi consectetuer pellentesque dui. Vestibulum urna. Fusce congue velit ut "
-						. "erat. Aliquam quis odio sollicitudin ipsum euismod porta. Vivamus pharetra, "
-						. "lacus eu tempor lobortis, diam nisi vulputate lorem, ut aliquet dui neque eu "
-						. "sem. Morbi varius, nisi ac laoreet mollis, pede odio cursus nisi, in imperdiet "
-						. "dolor enim at metus. Nunc pretium pulvinar tortor. Vestibulum euismod ultrices "
-						. "est. Etiam lobortis, enim ut bibendum dictum, urna orci lacinia tortor, "
-						. "at eleifend pede sem eu sem. Morbi a neque. Vestibulum cursus.\n\n", 
-		"display"     => true,
-	));
 
 	return $barres;
 }
