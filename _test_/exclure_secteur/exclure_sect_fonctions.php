8807539526e439e181b950fcc9af201366ef5ab9
maieulrouquette@@sfr.fr <maieulrouquette@@sfr.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-04-04 00:16:37 +0000

    éviter les problèmes si pas de secteur choisis + le filtre s'appel tout_secteur pour forcer
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@27642 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/exclure_secteur/exclure_sect_fonctions.php
+++ _test_/exclure_secteur/exclure_sect_fonctions.php
@@ -1,23 +1,32 @@
 <?php
+if (!function_exists('critere_tout_secteur_dist')){
+function critere_tout_secteur_dist($idb, &$boucles, $crit) {   
+    $boucle = &$boucles[$idb];
+    $boucle->modificateur['tout_secteur'] = true;
+}
+}
+
 function exclure_sect_choisir(){
-    $cfg =lire_config('secteur/exclure_sect');
-    $cfg = array_map('sql_quote',$cfg);
-    $cfg = implode($cfg,',');
-    
-    gettype($cfg)=='NULL' ? $cfg = 0 : $cfg = $cfg;
-    
-    return '('.$cfg.')';
+  $cfg =lire_config('secteur/exclure_sect');
+  if ( $cfg = array_map('sql_quote', $cfg) ) {
+    $cfg = implode($cfg, ',');
+  }
+  else {
+    $cfg = sql_quote('z');
+  }
+
+  return '('.$cfg.')';
 
 }
+
 
 function boucle_ARTICLES($id_boucle, &$boucles) {
     
 	$boucle = &$boucles[$id_boucle];
 	$id_table = $boucle->id_table;
 	
-    if(!$boucle->modificateur['tout']){
-        $boucle->where[] = array("NOT IN", "'$id_table.id_secteur'", 'sql_quote(exclure_sect_choisir())');
-    
+    if(!$boucle->modificateur['tout_secteur']){
+        $boucle->where[] = array("'NOT IN'", "'$id_table.id_secteur'", 'exclure_sect_choisir()');    
     }
 
 	return boucle_ARTICLES_dist($id_boucle, $boucles);
@@ -28,7 +37,7 @@ function boucle_RUBRIQUES($id_boucle, &$boucles) {
 	$boucle = &$boucles[$id_boucle];
 	$id_table = $boucle->id_table;
 	
-    if(!$boucle->modificateur['tout']){
+    if(!$boucle->modificateur['tout_secteur']){
         $boucle->where[] = array("'NOT IN'", "'$id_table.id_secteur'", 'exclure_sect_choisir()');
     
     }
@@ -41,7 +50,7 @@ function boucle_SYNDICATION($id_boucle, &$boucles) {
 	$boucle = &$boucles[$id_boucle];
 	$id_table = $boucle->id_table;
 	
-    if(!$boucle->modificateur['tout']){
+    if(!$boucle->modificateur['tout_secteur']){
         $boucle->where[] = array("'NOT IN'", "'$id_table.id_secteur'", 'exclure_sect_choisir()');
     
     }
