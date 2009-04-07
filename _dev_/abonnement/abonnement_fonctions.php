b7176fba8d58ce9071848a1b4f4a6c844c774ae9
marcimat@free.fr <marcimat@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-04-07 12:46:51 +0000

    - abonnement_fonctions en sql_quote(), et déplacement d'une dedans en plus
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@27697 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/abonnement/abonnement_fonctions.php
+++ _dev_/abonnement/abonnement_fonctions.php
@@ -1,19 +1,39 @@
 <?php
 
 function id_abonnement_to_libelle($id_abonnement){
-$abonnement = spip_fetch_array(spip_query("select libelle from spip_abonnements where id_abonnement ='$id_abonnement'")) ;
-return $abonnement['libelle'] ;
+	return sql_getfetsel('libelle', 'spip_abonnements','id_abonnement='.sql_quote($id_abonnement));
 }
 
 function id_abonnement_to_montant($id_abonnement){
-$abonnement = spip_fetch_array(spip_query("select montant from spip_abonnements where id_abonnement ='$id_abonnement'")) ;
-return $abonnement['montant'] ;
+	return sql_getfetsel('montant', 'spip_abonnements','id_abonnement='.sql_quote($id_abonnement));
 }
 
 function id_article_to_titre($id_article){
-$article = spip_fetch_array(spip_query("select titre from spip_articles where id_article ='$id_article'")) ;
-return $article['titre'] ;
+	return sql_getfetsel('titre', 'spip_articles','id_article='.sql_quote($id_article));
 }
 
+
+function article_visible_par_abonne($id_auteur,$id_article){
+	$res = sql_getfetsel(
+		"a.id_article",
+		array(
+			"spip_auteurs_elargis_articles AS a",
+			"spip_auteurs_elargis AS b",
+			"spip_auteurs AS c"
+		),
+		array(
+			"a.id_auteur_elargi = b.id",
+			"b.id_auteur = c.id_auteur",
+			"c.id_auteur = " . sql_quote($id_auteur),
+			"a.id_article = " . sql_quote($id_article),
+			"a.statut_paiement = " . sql_quote("ok"),
+		)
+	);
+
+	if($res) 
+		return true ;
+	else
+		return false ;	
+}
 
 ?>
