0ba7ae6560a0462349b89156e749c5d4994a1851
webmaster@geomaticien.com <webmaster@geomaticien.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-04-08 23:19:06 +0000

    Mutualisation
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@27731 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/acs/acs_options.php
+++ _dev_/acs/acs_options.php
@@ -6,10 +6,6 @@
 # Copyright Daniel FAIVRE, 2007-2008
 # Copyleft: licence GPL - Cf. LICENCES.txt
 
-// Dossier des paramètres et images utilisateur
-// User images and parameters
-$GLOBALS['ACS_CHEMIN'] = _NOM_PERMANENTS_ACCESSIBLES.'_acs';
-
 // Pages de l'espace ecrire à accès contrôlé comme ACS - ACS access rules controlled pages
 // Les administrateurs sont ceux d'ACS
 $GLOBALS['ACS_ACCES'] = array('mots_type');
@@ -31,7 +27,12 @@ if(in_array('openPublishing', $GLOBALS['plugins'])) {
 // Dossier des paramètres et images utilisateur
 // User images and parameters
 // compatible mutualisation (_DIR_SITE defini)
-$dir_site = _DIR_RACINE ? substr(_DIR_SITE, 3) : _DIR_SITE ;
+if (_DIR_SITE == '_DIR_SITE') {
+  $dir_site ='';
+}
+else {
+  $dir_site = _DIR_RACINE ? substr(_DIR_SITE, 3) : _DIR_SITE ;
+}
 $GLOBALS['ACS_CHEMIN'] = $dir_site._NOM_PERMANENTS_ACCESSIBLES.'_acs';
 
 // Chargement - Loading
