d3c4e2d332dc18284603400ed5dde36d9bbefb64
maieulrouquette@@sfr.fr <maieulrouquette@@sfr.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-04-08 21:25:31 +0000

    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@27727 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/bisous/formulaires/bisous.php
+++ _dev_/bisous/formulaires/bisous.php
@@ -9,9 +9,9 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 include_spip('base/abstract_sql');
 
 function formulaires_bisous_charger_dist($id_auteur){
-
+    
 	if (
-		$GLOBALS['visiteur_session']['id_auteur'] > 0
+		($GLOBALS['visiteur_session']['id_auteur'] > 0
 		and !sql_getfetsel(
 			'id_bisou',
 			'spip_bisous',
@@ -19,9 +19,15 @@ function formulaires_bisous_charger_dist($id_auteur){
 				array('=', 'id_donneur', intval($GLOBALS['visiteur_session']['id_auteur'])),
 				array('=', 'id_receveur', intval($id_auteur))
 			)
+         
 		)
+	   or lire_config('bisous/multiple'))
+	   
+	   and (lire_config('bisous/self') or $GLOBALS['visiteur_session']['id_auteur']!=$id_auteur )  // il faudrait voir à pas se donner à soi même un bisous
 	)
-		return array();
+         
+        return array();
+
 	else
 		return false;
 
@@ -36,15 +42,16 @@ function formulaires_bisous_verifier_dist($id_auteur){
 function formulaires_bisous_traiter_dist($id_auteur){
 
 	// On teste si ya pas déjà un bisou
-	$bisou = sql_getfetsel(
-		'id_bisou',
-		'spip_bisous',
-		array(
-			array('=', 'id_donneur', intval($GLOBALS['visiteur_session']['id_auteur'])),
-			array('=', 'id_receveur', intval($id_auteur))
-		)
-	);
-	
+	if (!lire_config('bisous/multiple')){
+           $bisou = sql_getfetsel(
+            'id_bisou',
+            'spip_bisous',
+            array(
+                array('=', 'id_donneur', intval($GLOBALS['visiteur_session']['id_auteur'])),
+                array('=', 'id_receveur', intval($id_auteur))
+            )
+        );
+	}
 	// On ajoute un bisou si c'est pas déjà fait
 	if (!$bisou)
 		sql_insertq(
--- _dev_/bisous/lang/bisous_fr.php
+++ _dev_/bisous/lang/bisous_fr.php
@@ -3,12 +3,14 @@
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-    'aucun_bisou_donne'   =>'Aucun bisou donnes. :(',
-    'aucun_bisou_recu'   =>'Aucun bisou reçu. :(',
+    'aucun_bisou_donne' =>'Aucun bisou donnés. :(',
+    'aucun_bisou_recu'  =>'Aucun bisou reçu. :(',
     'bisous_donnes'     =>'Bisous donnés',
     'bisous_recus'      =>'Bisous reçus',
     'cfg_bisous'        =>'Configurer le plugin bisous',  
-    'donnez_bisou'    =>'Donnez un bisou !'
+    'donnez_bisou'      =>'Donnez un bisou !',
+    'multiple'          =>'On peut donner plusieur bisous',
+    'self'              =>'On peut se donner des bisous &agrave; soi m&ecirc;me'
     
 );
 
