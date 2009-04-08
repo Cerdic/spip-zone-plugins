bc02b20c5e65716863cf8d505cd24d0bbd7610f3
maieulrouquette@@sfr.fr <maieulrouquette@@sfr.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-04-08 19:00:48 +0000

    internationalisons les bisous !
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@27720 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _dev_/bisous/lang/bisous_fr.php
@@ -0,0 +1,12 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+    'bisous_donnes'     =>'Bisous donnés',
+    'bisous_recus'      =>'Bisous reçus',    
+    'donnez_bisou'    =>'Donnez un bisou !'
+    
+);
+
+?>
