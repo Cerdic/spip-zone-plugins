8dadb91957282ea20d39178ca619b24d92dc3438
maieulrouquette@@sfr.fr <maieulrouquette@@sfr.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-04-08 20:39:20 +0000

    des bisous dans l'espace privé + correction sur pas de bisous (mais comme dirait les bisounours : "des bisous partout")
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@27723 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/bisous/lang/bisous_fr.php
+++ _dev_/bisous/lang/bisous_fr.php
@@ -3,6 +3,8 @@
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+    'aucun_bisou_donne'   =>'Aucun bisou donnes. :(',
+    'aucun_bisou_recu'   =>'Aucun bisou reçu. :(',
     'bisous_donnes'     =>'Bisous donnés',
     'bisous_recus'      =>'Bisous reçus',    
     'donnez_bisou'    =>'Donnez un bisou !'
