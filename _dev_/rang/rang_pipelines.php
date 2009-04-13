3553086796fd6210f474e72294fbb8946473f586
cam.lafit@azerttyu.net <cam.lafit@azerttyu.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-04-13 15:09:04 +0000

    Gerer le rang pour les articles
     * De la meme façon on peut definir le rang d'un article avec '''XXX. titre'''
     * On extrait les 2 fonctions de calculs du rangs
      * '''update_rang''' pour metre à jour le rang d'un objet pour un id_objet donné
      * '''extraire_rang''' extrait le rang (si il existe) d'une chaine de texte
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@27822 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/rang/rang_pipelines.php
+++ _dev_/rang/rang_pipelines.php
@@ -2,23 +2,26 @@
 
 function prang_formulaire_charger($flux) {
 
-    if ($flux['args']['form'] == 'editer_rubrique') {
-        if ($flux['data']['rang']) {
-            $flux['data']['titre'] = $flux['data']['rang'].'. '.$flux['data']['titre'];         
-        }
+    switch ($flux['args']['form']) {
+        case 'editer_rubrique' :
+        case 'editer_article' :
+            if ($flux['data']['rang']) {
+                $flux['data']['titre'] = $flux['data']['rang'].'. '.$flux['data']['titre'];         
+            }
+            break;
     }
-    
     return $flux;
 }
 
 function prang_formulaire_verifier($flux) {
-    
-    if ($flux['args']['form'] == 'editer_rubrique') {
-        list($rang,$titre) = explode(".", _request('titre'), 2);
-        if (is_numeric(trim($rang))) {
-            set_request('titre',trim($titre));
-            set_request('rang',trim($rang));
-        }
+        
+    switch ($flux['args']['form'] ) {
+        case 'editer_rubrique' :
+        case 'editer_article' :
+            $array = extraire_rang(_request('titre'));
+            set_request('titre',$array['titre']);
+            set_request('rang',$array['rang']);
+            break;
     }
     return $flux;
 }
@@ -27,13 +30,44 @@ function prang_formulaire_verifier($flux) {
 
 //ajouter la valeur de rang dans la base
 function prang_formulaire_traiter($flux) {
-    
-    //var_export($flux['data']['id_rubrique']);
-    
-    if ($flux['args']['form'] == 'editer_rubrique') {
-        sql_updateq('spip_rubriques', array('rang' => _request('rang')), 'id_rubrique='.$flux['data']['id_rubrique']);
+        
+    switch($flux['args']['form']) {
+        case 'editer_rubrique' :
+            update_rang(_request('rang'),'rubrique',$flux['data']['id_rubrique']);
+            break;
+        case 'editer_article' :
+            update_rang(_request('rang'),'article',$flux['data']['id_article']);
+            break;
     }
     
     return $flux;
 }
+
+function update_rang($rang,$objet,$id_objet) {
+
+    switch($objet) {
+        case 'rubrique' :
+            $id_table = 'id_rubrique';
+            break;
+        case 'article' :
+            $id_table = 'id_article';
+            break;
+    }
+
+    sql_updateq("spip_".$objet."s", array('rang' => $rang), "id_".$objet.'='.$id_objet);    
+}
+
+function extraire_rang($texte) {
+    list($rang,$titre) = explode(".", $texte, 2);
+    $rang = trim($rang);
+    $titre = trim($titre);
+    
+    if (!is_numeric($rang)) {
+        $rang = null;
+        $titre = $texte;
+    }
+    
+    return array('rang'=>$rang,'titre'=>$titre);
+}
+
 ?>
