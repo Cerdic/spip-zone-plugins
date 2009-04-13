5b2c3a9b7b95016a47d4eb5f261a7827176cbf65
cam.lafit@azerttyu.net <cam.lafit@azerttyu.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-04-13 17:58:11 +0000

    Isoler les fonctions de traitement du rang
     * Serar utile pour la conversion des bases à l'activation du plugin
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@27829 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/rang/rang_pipelines.php
+++ _dev_/rang/rang_pipelines.php
@@ -1,5 +1,7 @@
 <?php
 
+include_spip('inc/rang');
+
 function prang_formulaire_charger($flux) {
 
     switch ($flux['args']['form']) {
@@ -42,32 +44,4 @@ function prang_formulaire_traiter($flux) {
     
     return $flux;
 }
-
-function update_rang($rang,$objet,$id_objet) {
-
-    switch($objet) {
-        case 'rubrique' :
-            $id_table = 'id_rubrique';
-            break;
-        case 'article' :
-            $id_table = 'id_article';
-            break;
-    }
-
-    sql_updateq("spip_".$objet."s", array('rang' => $rang), "id_".$objet.'='.$id_objet);    
-}
-
-function extraire_rang($texte) {
-    list($rang,$titre) = explode(".", $texte, 2);
-    $rang = trim($rang);
-    $titre = trim($titre);
-    
-    if (!is_numeric($rang)) {
-        $rang = null;
-        $titre = $texte;
-    }
-    
-    return array('rang'=>$rang,'titre'=>$titre);
-}
-
 ?>
