a8c5d071910dea17cf3534f576c629126559ea62
webmaster@geomaticien.com <webmaster@geomaticien.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-04-13 15:44:53 +0000

    Nouveau composant du modèle Cat : Tags (nuage de mots-clés) New cat's model component : Tags (Cloud of keywords)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@27824 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _stable_/acs/models/cat/composants/tags/ecrire/lang/tags_ecrire_en.php
@@ -0,0 +1,20 @@
+<?php
+// This is a SPIP-ACS language file  --  Ceci est un fichier langue de SPIP-ACS
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+'nom' => 'Tags',
+'description' => 'Display a cloud of keywords.',
+'info' => 'Display a cloud of keywords sized to their frequency of use.',
+'help' => 'Keywords beginning by "_" are not displayed',
+
+'Bord' => 'Border',
+'Fond' => 'Background',
+'TitreFond' => 'Title',
+'Align' => 'Alignment',
+'AlignCenter' => 'center',
+'AlignJustify' => 'justify',
+'Titre' => 'Display block title',
+'Sep' => 'Separator'
+);
+?>
