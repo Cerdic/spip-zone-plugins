91b2ae4b951df4489053e14cda00417e60b1806f
marcimat@free.fr <marcimat@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-04-15 19:34:52 +0000

    - refonte des fonds CFG pour utiliser la syntaxe SPIP 2.0/CVT
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@27875 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _dev_/abonnement/lang/abo_fr.php
@@ -0,0 +1,25 @@
+<?php
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	// CFG
+	"config_description_abonnement" => "Cette page permet de d&eacute;finir les options d'abonnement de votre site.",
+	"config_description_abonnement_relances" => "Cette page permet de d&eacute;finir les options de relances d'abonnement de votre site.",
+
+	// Formulaires CFG
+	"fieldset_expediteur" => "Expediteur des mails automatiques",
+	"fieldset_confirmation" => "Mail de confirmation d'abonnement",
+	"fieldset_echec" => "Mail d'&eacute;chec de l'abonnement",
+	"fieldset_articles_payants" => "Articles payants",
+	"fieldset_relance" => "Email de relance a &eacute;ch&eacute;ance",
+	"fieldset_relance_avant_1" => "Email de relance avant &eacute;ch&eacute;ance 1",
+	"fieldset_relance_avant_2" => "Email de relance avant &eacute;ch&eacute;ance 2",
+	"fieldset_relance_apres" => "Email de relance apr&egrave;s &eacute;ch&eacute;ance",
+	
+	"label_nom" => "Nom",
+	"label_email" => "Email",
+	"label_sujet" => "Sujet",
+	"label_texte" => "Texte",
+	"label_tarif" => "Tarif",
+	
+	"explication_tarif" => "Indiquer le prix en euros.",
+);
+?>
