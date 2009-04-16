46581686a49e6c7542830236bb15b0abc93476f8
webmaster@geomaticien.com <webmaster@geomaticien.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-04-16 19:14:21 +0000

    Test pour ne pas compacter deux fois les javascripts des crayons lorsqu'ils sont déjà compactés par SPIP. Non seulement le "double compactage" est inutile et fait perdre du temps, mais en plus il génère des bugs.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@27890 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/crayons/crayons.js_fonctions.php
+++ _stable_/crayons/crayons.js_fonctions.php
@@ -9,8 +9,11 @@ function pack_cQuery($chemin) {
 	$flux = spip_file_get_contents($chemin);
 	$flux = str_replace('jQuery', 'cQuery', $flux);
 
+// On ne compacte PAS deux fois (c'est inutile et en plus ça bugge)
 	if (!strlen($flux)
-	OR _request('debug_crayons'))
+	OR _request('debug_crayons')
+	OR ($GLOBALS['meta']['auto_compress_js'] == 'oui')
+	OR (test_espace_prive()))
 		return $flux;
 
 	include_spip('lib/JavaScriptPacker/class.JavaScriptPacker');
