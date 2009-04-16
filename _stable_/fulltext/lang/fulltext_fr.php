f249f24c5d9abd2806fa386d1c3c546db07f54ea
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-04-16 22:16:35 +0000

    langues (fulltext)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@27899 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/fulltext/lang/fulltext_fr.php
+++ _stable_/fulltext/lang/fulltext_fr.php
@@ -4,64 +4,63 @@
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-    // A
-    'accents_pas_pris' => 'Les accents ne sont pas pris en compte (&laquo;&nbsp;d&#233;j&#224;&nbsp;&raquo; ou &laquo;&nbsp;deja&nbsp;&raquo;, retourneront &#224; l\'identique &laquo;&nbsp;d&#233;j&#224;&nbsp;&raquo;, &laquo;&nbsp;dej&#224;&nbsp;&raquo;, &laquo;&nbsp;d&#233;ja&nbsp;&raquo;...)',
-    'asterisque_terminale' => 'ne retournera rien: l\'ast&#233;risque * doit &#234;tre terminale',
-    'aussi' => 'aussi',
-    
-    // C
-    'casse_indifferente' => 'La casse (minuscule/majuscule) des mots recherch&#233;s est indiff&#233;rente.',
-    'convertir_myisam' => 'Convertir en MyISAM',
-    'convertir_toutes' => 'Convertir toutes les tables en MyISAM',
-    'convertir_utf8' => 'convertir en UTF-8 pour restaurer la coh&#233;rence',
-    'creer_tous' => 'Cr&#233;er tous les index FULLTEXT sugg&#233;r&#233;s',
-    
-    // E
-    'et' => 'ET',
-    'exemples' => 'Exemples d\'utilisation',
-    
-    // F
-    'fonctionnement_recherche' => 'Fonctionnement du moteur de recherche de ce site',
-    'fulltext_cree' => 'FULLTEXT cr&#233;&#233;',
-    
-    // I
-    'il_faut_myisam' => 'il faut MyISAM',
-    'incoherence_charset' => 'Une incoh&#233;rence entre le charset de votre site et celui des tables de votre base de donn&#233;es risque de fausser les recherches avec caract&#232;res accentu&#233;s:',
-            
-    'index_supprime' => 'index supprim&eacute;',
-    'index_regenere' => 'index de la table r&#233;g&#233;n&#233;r&#233;s',
-    
-    // L
-    'liste_tables_connues' => 'Voici la liste des tables connues de la recherche. Vous pouvez y ajouter des &#233;l&#233;ments FULLTEXT, cf. documentation &#224; l\'adresse',
-    
-    // M
-    'mais_pas' => 'mais PAS',
-    
-    // O
-    'ou_bien' => 'ou bien',
-    
-    // P
-    'pas_index' => 'Pas d\'index FULLTEXT',
-    
-    // R
-    'regenerer_tous' => 'R&#233;g&#233;n&#233;rer tous les index FULLTEXT',
-    'reserve_webmestres' => 'Page r&#233;serv&#233;e aux webmestres',
-    'retourne' => 'Retourne les textes qui contiennent',
-    
-    // S
-    'sequence_exacte' => 'exactement la s&#233;quence de mots',
-    'soit' => 'SOIT',
-    'supprimer' => 'Supprimer',
-    
-    // T
-    'table_convertie' => 'table convertie en MyISAM',
-    'table_format' => 'Cette table est au format',
-    'table_non_reconnue' => 'table non reconnue',
-    'textes_premier' => 'mais pr&#233;sente en premier les textes qui contiennent',
-    
-    // U
-    'utiliser_operateurs_logiques' => 'La recherche utilise les op&eacute;rateurs logiques les plus courants.'
-    
+
+	// A
+	'accents_pas_pris' => 'Les accents ne sont pas pris en compte (&laquo;&nbsp;d&#233;j&#224;&nbsp;&raquo; ou &laquo;&nbsp;deja&nbsp;&raquo;, retourneront &#224; l\'identique &laquo;&nbsp;d&#233;j&#224;&nbsp;&raquo;, &laquo;&nbsp;dej&#224;&nbsp;&raquo;, &laquo;&nbsp;d&#233;ja&nbsp;&raquo;...)',
+	'asterisque_terminale' => 'ne retournera rien: l\'ast&#233;risque * doit &#234;tre terminale',
+	'aussi' => 'aussi',
+
+	// C
+	'casse_indifferente' => 'La casse (minuscule/majuscule) des mots recherch&#233;s est indiff&#233;rente.',
+	'convertir_myisam' => 'Convertir en MyISAM',
+	'convertir_toutes' => 'Convertir toutes les tables en MyISAM',
+	'convertir_utf8' => 'convertir en UTF-8 pour restaurer la coh&#233;rence',
+	'creer_tous' => 'Cr&#233;er tous les index FULLTEXT sugg&#233;r&#233;s',
+
+	// E
+	'et' => 'ET',
+	'exemples' => 'Exemples d\'utilisation',
+
+	// F
+	'fonctionnement_recherche' => 'Fonctionnement du moteur de recherche de ce site',
+	'fulltext_cree' => 'FULLTEXT cr&#233;&#233;',
+
+	// I
+	'il_faut_myisam' => 'il faut MyISAM',
+	'incoherence_charset' => 'Une incoh&#233;rence entre le charset de votre site et celui des tables de votre base de donn&#233;es risque de fausser les recherches avec caract&#232;res accentu&#233;s:',
+	'index_regenere' => 'index de la table r&#233;g&#233;n&#233;r&#233;s',
+	'index_supprime' => 'index supprim&eacute;',
+
+	// L
+	'liste_tables_connues' => 'Voici la liste des tables connues de la recherche. Vous pouvez y ajouter des &#233;l&#233;ments FULLTEXT, cf. documentation &#224; l\'adresse',
+
+	// M
+	'mais_pas' => 'mais PAS',
+
+	// O
+	'ou_bien' => 'ou bien',
+
+	// P
+	'pas_index' => 'Pas d\'index FULLTEXT',
+
+	// R
+	'regenerer_tous' => 'R&#233;g&#233;n&#233;rer tous les index FULLTEXT',
+	'reserve_webmestres' => 'Page r&#233;serv&#233;e aux webmestres',
+	'retourne' => 'Retourne les textes qui contiennent',
+
+	// S
+	'sequence_exacte' => 'exactement la s&#233;quence de mots',
+	'soit' => 'SOIT',
+	'supprimer' => 'Supprimer',
+
+	// T
+	'table_convertie' => 'table convertie en MyISAM',
+	'table_format' => 'Cette table est au format',
+	'table_non_reconnue' => 'table non reconnue',
+	'textes_premier' => 'mais pr&#233;sente en premier les textes qui contiennent',
+
+	// U
+	'utiliser_operateurs_logiques' => 'La recherche utilise les op&eacute;rateurs logiques les plus courants.'
 );
 
 ?>
