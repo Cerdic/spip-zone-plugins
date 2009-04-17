6fe9b046a3279d7ee5c8d8d6a39aa7abd6b052ff
marcimat@free.fr <marcimat@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-04-17 14:22:13 +0000

    La recherche privée en SPIP 2.0 (?exec=recherche&recherche=112) plantait car SPIP ne trouvait pas la clé primaire des tables de géographie. En conséquence : passage de geographie en écriture SPIP 2.0 pour les déclarations de tables.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@27927 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/geographie/base/geographie.php
+++ _stable_/geographie/base/geographie.php
@@ -1,57 +1,65 @@
 <?php
-include_spip('base/serial');
-$spip_geo_pays = array(
-		"id_pays"	=> "smallint NOT NULL",
-		"nom"	=> "text DEFAULT '' NOT NULL",
-);
-$spip_geo_pays_key = array(
-		"PRIMARY KEY"		=> "id_pays"
-);
-$spip_geo_regions = array(
-		"id_region"	=> "smallint NOT NULL",
-		"id_pays"	=> "smallint NOT NULL",
-		"nom"	=> "tinytext DEFAULT '' NOT NULL",
-);
-$spip_geo_regions_key = array(
-		"PRIMARY KEY"		=> "id_region"
-);
-$spip_geo_departements = array(
-		"id_departement"	=> "smallint NOT NULL",
-		"abbr"	=> "varchar(5) default '' NOT NULL",
-		"id_region"	=> "smallint NOT NULL",
-		"nom"	=> "tinytext DEFAULT '' NOT NULL",
-);
-$spip_geo_departements_key = array(
-		"PRIMARY KEY"		=> "id_departement"
-);
+#include_spip('base/serial');
 
-$spip_geo_communes = array(
-		"id_commune"	=> "bigint(21) NOT NULL",
-		"insee"	=> "char(6) default '' NOT NULL",
-		"id_departement"	=> "smallint NOT NULL",
-		"id_pays"	=> "smallint NOT NULL",
-		"code_postal"	=> "char(5) default '' NOT NULL",
-		"nom"	=> "tinytext DEFAULT '' NOT NULL",
-		"longitude"	=> "varchar(15) default '' NOT NULL",
-		"latitude"	=> "varchar(15) default '' NOT NULL",
-);
-$spip_geo_communes_key = array(
-		"PRIMARY KEY"		=> "id_commune",
-		"INDEX insee"		=> "insee",
-		"INDEX id_pays"		=> "id_pays"
-);
+function geographie_declarer_tables_principales($tables_principales){
+	$spip_geo_pays = array(
+			"id_pays"	=> "smallint NOT NULL",
+			"nom"	=> "text DEFAULT '' NOT NULL",
+	);
+	$spip_geo_pays_key = array(
+			"PRIMARY KEY"		=> "id_pays"
+	);
+	$spip_geo_regions = array(
+			"id_region"	=> "smallint NOT NULL",
+			"id_pays"	=> "smallint NOT NULL",
+			"nom"	=> "tinytext DEFAULT '' NOT NULL",
+	);
+	$spip_geo_regions_key = array(
+			"PRIMARY KEY"		=> "id_region"
+	);
+	$spip_geo_departements = array(
+			"id_departement"	=> "smallint NOT NULL",
+			"abbr"	=> "varchar(5) default '' NOT NULL",
+			"id_region"	=> "smallint NOT NULL",
+			"nom"	=> "tinytext DEFAULT '' NOT NULL",
+	);
+	$spip_geo_departements_key = array(
+			"PRIMARY KEY"		=> "id_departement"
+	);
 
-global $tables_principales;
-$tables_principales['spip_geo_pays'] = array('field'=>&$spip_geo_pays,'key'=>$spip_geo_pays_key);
-$tables_principales['spip_geo_regions'] = array('field'=>&$spip_geo_regions,'key'=>$spip_geo_regions_key);
-$tables_principales['spip_geo_departements'] = array('field'=>&$spip_geo_departements,'key'=>$spip_geo_departements_key);
-$tables_principales['spip_geo_communes'] = array('field'=>&$spip_geo_communes,'key'=>$spip_geo_communes_key);
+	$spip_geo_communes = array(
+			"id_commune"	=> "bigint(21) NOT NULL",
+			"insee"	=> "char(6) default '' NOT NULL",
+			"id_departement"	=> "smallint NOT NULL",
+			"id_pays"	=> "smallint NOT NULL",
+			"code_postal"	=> "char(5) default '' NOT NULL",
+			"nom"	=> "tinytext DEFAULT '' NOT NULL",
+			"longitude"	=> "varchar(15) default '' NOT NULL",
+			"latitude"	=> "varchar(15) default '' NOT NULL",
+	);
+	$spip_geo_communes_key = array(
+			"PRIMARY KEY"		=> "id_commune",
+			"INDEX insee"		=> "insee",
+			"INDEX id_pays"		=> "id_pays"
+	);
 
-global $table_des_tables;
-$table_des_tables['geo_pays'] = 'geo_pays';
-$table_des_tables['geo_regions'] = 'geo_regions';
-$table_des_tables['geo_departements'] = 'geo_departements';
-$table_des_tables['geo_communes'] = 'geo_communes';
+	$tables_principales['spip_geo_pays'] = array('field'=>&$spip_geo_pays,'key'=>$spip_geo_pays_key);
+	$tables_principales['spip_geo_regions'] = array('field'=>&$spip_geo_regions,'key'=>$spip_geo_regions_key);
+	$tables_principales['spip_geo_departements'] = array('field'=>&$spip_geo_departements,'key'=>$spip_geo_departements_key);
+	$tables_principales['spip_geo_communes'] = array('field'=>&$spip_geo_communes,'key'=>$spip_geo_communes_key);
+
+	return $tables_principales;
+}
+
+function geographie_declarer_tables_interfaces($interface){
+	$interface['table_des_tables']['geo_pays'] = 'geo_pays';
+	$interface['table_des_tables']['geo_regions'] = 'geo_regions';
+	$interface['table_des_tables']['geo_departements'] = 'geo_departements';
+	$interface['table_des_tables']['geo_communes'] = 'geo_communes';
+
+	return $interface;
+}
+
 
 function geographie_lister_tables_noexport($liste){
 	$liste[] = 'spip_geo_communes';
@@ -60,6 +68,7 @@ function geographie_lister_tables_noexport($liste){
 	$liste[] = 'spip_geo_pays';
 	return $liste;
 }
+
 global $IMPORT_tables_noerase;
 $IMPORT_tables_noerase[]='spip_geo_communes';
 $IMPORT_tables_noerase[]='spip_geo_departements';
