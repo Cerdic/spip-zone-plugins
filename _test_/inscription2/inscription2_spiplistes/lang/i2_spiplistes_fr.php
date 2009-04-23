2ca5e62839771a24b0b93112008b6ea62d9f998e
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-04-23 14:37:31 +0000

    On repose tout au propre... pour I2 et spip-listes
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@28056 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _test_/inscription2/inscription2_spiplistes/lang/i2_spiplistes_fr.php
@@ -0,0 +1,30 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://trac.rezo.net/spip-zone/_plugins_/_test_/inscription2/inscription2_2_0/lang
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'choisir_listes' => 'Choisissez ci-dessous les listes de diffusion &#224; proposer aux nouveaux adh&eacute;rents&nbsp;: ',
+	
+	// E
+	'exp_newsletter' => 'Choisissez ci-dessous les listes de diffusion auxquelles vous voulez vous abonner',
+	
+	// D
+	'deselect_listes' => '> tout d&#233;s&eacute;lectionner',
+			
+	// N
+	'newsletter' => 'Listes de diffusion',
+	
+	// O
+	'optout' => 'Cases &agrave; coch&#233;es non pr&#233;coch&#233;es (optout)',
+	
+	// S
+	'select_listes' => '(vous pouvez s&#233;lectionner plusieurs listes avec la touche shift)',
+		
+	// Z
+	'z' => 'zzz'
+);
+
+?>
