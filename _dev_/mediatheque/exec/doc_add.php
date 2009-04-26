27c5bfe54a1ede3dbd3123e5211b0985aa1924ea
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-04-26 18:51:56 +0000

    un plugin visiblement mort né, pas vraiement fonctionnel, et qui induit les utilisateurs en erreur avec le plugin mediatheque qui marche (gestion_document)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@28107 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/mediatheque/exec/doc_add.php
+++
@@ -1,9 +0,0 @@
-<?php
-// ---------------------------------------------
-//	simeray@tektonika.com
-//	last modification: 24/11/06
-// ---------------------------------------------
-function exec_doc_add() {
-	print "ajouter document";
-}
-?>
