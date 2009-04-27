f01c46870c48fcce03c777c5e472715be4b0f3f7
marcimat@free.fr <marcimat@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-04-27 13:03:16 +0000

    le genie en sql_* (a tester)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@28146 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/abonnement/genie/abonnement.php
+++ _dev_/abonnement/genie/abonnement.php
@@ -11,20 +11,25 @@ function genie_abonnement_dist($t){
 		
 	// fermer les zones aux echus
 	// attention s'il y en a beaucoup
+	$result = sql_select(
+		"a.id_auteur",
+		array(
+			"spip_auteurs_elargis AS a",
+			"spip_zones_auteurs AS b",
+			"spip_auteurs_elargis_abonnements AS c"
+		),
+		array(
+			"a.id_auteur = b.id_auteur",
+			"a.id = c.id_auteur_elargi",
+			"c.validite <> '0000-00-00 00:00:00'",
+			"c.validite < " . date('Y-m-d H:i:s')
+		),
+	);
 		
-	$result = spip_query("
-	SELECT a.id_auteur FROM spip_auteurs_elargis a, spip_zones_auteurs b, spip_auteurs_elargis_abonnements c
-	WHERE
-	a.id_auteur = b.id_auteur
-	and a.id = c.id_auteur_elargi
-	and c.validite <> '0000-00-00 00:00:00' 
-	and c.validite < NOW()
-	");
-		
-	while ($row = spip_fetch_array($result)){
+	while ($row = sql_fetch($result)){
 		$id_auteur = $row['id_auteur'] ;
 		spip_log("$id_auteur est echu (salo), il perd sa (ses) zone(s)", "abonnement");		
-		spip_query("DELETE FROM `spip_zones_auteurs` WHERE id_auteur='$id_auteur'");
+		sql_delete("spip_zones_auteurs", "id_auteur = " . sql_quote($id_auteur));
 	}
 		
 	
@@ -34,11 +39,9 @@ function genie_abonnement_dist($t){
 	// 1 après l'échéance traité ici.
 	// ne pas envoyer plusieurs fois le meme message (flaguer un peu)
 	
-
+	$result = sql_select(array("libelle", "id_abonnement", "periode"),"spip_abonnements");
 	
-	$result = spip_query("SELECT libelle, id_abonnement, periode FROM spip_abonnements");
-		
-	while ($row = spip_fetch_array($result)){
+	while ($row = sql_fetch($result)){
 		$libelle = $row['libelle'] ;
 		$id_abonnement = $row['id_abonnement'] ;
 		$periode = $row['periode'] ;
@@ -55,19 +58,24 @@ function genie_abonnement_dist($t){
 		$texte_relance = lire_config("abonnement_relances/texte_relance4") ;
 		$adresse_expediteur = lire_config("abonnement_relances/email_envoi") ;
 
-		$result_abo = spip_query("
-		SELECT a.id_auteur, a.id, c.email FROM spip_auteurs_elargis a, spip_auteurs_elargis_abonnements b, spip_auteurs c
-		WHERE
-		a.id = b.id_auteur_elargi
-		and a.id_auteur = c.id_auteur
-		and b.id_abonnement = '$id_abonnement'
-		and b.validite <> '0000-00-00 00:00:00'
-		and b.validite < $validite
-		and b.stade_relance < 4
-		");
-		
+		$result_abo = sql_select(
+			array("a.id_auteur", "a.id", "c.email"),
+			array(
+				"spip_auteurs_elargis AS a",
+				"spip_auteurs_elargis_abonnements AS b",
+				"spip_auteurs AS c"
+			),
+			array(
+				"a.id = b.id_auteur_elargi",
+				"a.id_auteur = c.id_auteur",
+				"b.id_abonnement = " . sql_quote($id_abonnement),
+				"b.validite <> '0000-00-00 00:00:00'",
+				"b.validite < " . $validite,
+				"b.stade_relance < 4"
+			),
+		);
 
-		while($row_abo = spip_fetch_array($result_abo)){
+		while($row_abo = sql_fetch($result_abo)){
 			$id_auteur = $row_abo['id_auteur'] ;
 			$email_abonne = $row_abo['email'] ;
 			$id_auteur_elargi = $row_abo['id'] ;
@@ -79,7 +87,7 @@ function genie_abonnement_dist($t){
 			include_spip('inc/mail'); 
 						
 			if(envoyer_mail($email_abonne, $sujet_relance, $texte_relance, $adresse_expediteur)){
-				spip_query("UPDATE `spip_auteurs_elargis_abonnements` SET stade_relance = 4 WHERE id_auteur_elargi = '$id_auteur_elargi'");
+				sql_updateq("spip_auteurs_elargis_abonnements", array("stade_relance"=>4), "id_auteur_elargi = " . sql_quote($id_auteur_elargi));
 				spip_log("relance faite","abonnement") ;
 			}
 		}
