15a9c590bd1c240574358a8c96aa3eeaf0ce32e5
booz@rezo.net <booz@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-04-27 22:09:44 +0000

    code mort
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@28163 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/inscription2/inscription2_2_0/inscription2_mes_fonctions.php
+++ _test_/inscription2/inscription2_2_0/inscription2_mes_fonctions.php
@@ -1,21 +1,6 @@
 <?php
 include_spip('base/abstract_sql');
 
-function confirmation_inscription2($id, $mode, $cle){
-	$q = sql_fetsel("statut, alea_actuel","spip_auteurs","id_auteur = '$id'");
-	$statuts_autorises = array( 
- 		"aconfirmer", 
-	 	"6forum" 
- 	 ); 
- 		        	         
- 	if(in_array($q['statut'],$statuts_autorises) and $mode == 'conf' and $cle ==  $q['alea_actuel']){ 		
-	 	return 'pass';
-	}elseif($q['statut'] == 'aconfirmer' and $mode == 'sup' and $cle ==  $q['alea_actuel']){
-		return 'sup';
-	}else
-		return 'rien';
-}
-
 // Filtres
 function n_to_br($texte){
 	$texte = str_replace("\n", "<br />", $texte);
