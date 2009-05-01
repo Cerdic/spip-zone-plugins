232352b9ddaa5918138cc92290e7cb0508ded9c7
marcimat@free.fr <marcimat@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-05-01 10:26:59 +0000

    - upgrade en sql_* et en mode SPIP 2 et en bonus : qui fonctionne !
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@28315 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/abonnement/base/abonnement_upgrade.php
+++ _dev_/abonnement/base/abonnement_upgrade.php
@@ -10,137 +10,92 @@
 **/
 
 //version actuelle du plugin à changer en cas de maj
-	$GLOBALS['abonnement_base_version'] = 0.74;
-	
-	function abonnement_upgrade(){
-		$version_base = $GLOBALS['abonnement_base_version'];
-		$current_version = 0.0;
-		if (   (isset($GLOBALS['meta']['abonnement_base_version']) )
-				&& (($current_version = $GLOBALS['meta']['abonnement_base_version'])==$version_base))
-			return;
 
-		include_spip('base/abonnement');
-		if ($current_version==0.0){
-			include_spip('base/create');
-			include_spip('base/abstract_sql');
-			creer_base();
-			//echo "creation des tables spip_abonnements";
+function abonnement_upgrade($nom_meta_base_version,$version_cible){
 
-			ecrire_meta('abonnement_base_version',$current_version=$version_base);
-			ecrire_metas();
-		}
-		
-		if ($current_version < 0.2){
-			include_spip('base/create');
-			include_spip('base/abstract_sql');
-			creer_base();
-			echo "Maj 0.2 des tables spip_abonnements";
-			
-			ecrire_meta('abonnement_base_version',$current_version=$version_base);
-			ecrire_metas();
-		}
-		
-		if ($current_version < 0.3){
-			include_spip('base/create');
-			include_spip('base/abstract_sql');
-			creer_base();
-			echo "Maj 0.3 des tables spip_auteurs_elargis_articles";
-			
-			ecrire_meta('abonnement_base_version',$current_version=$version_base);
-			ecrire_metas();
-		}
-		
-		if ($current_version < 0.4){
-			include_spip('base/create');
-			include_spip('base/abstract_sql');
-			// faudrait virer le autoincrement aussi
-			spip_query("ALTER TABLE `spip_auteurs_elargis_articles` ADD INDEX id_auteur_elargi (id_auteur_elargi)");
-			spip_query("ALTER TABLE `spip_auteurs_elargis_articles` DROP PRIMARY KEY");
-			echo "Maj 0.4 des index `spip_auteurs_elargis_articles`";
-			ecrire_meta('abonnement_base_version',$current_version=$version_base);
-			ecrire_metas();
-		}
-		
-		if ($current_version < 0.5){
-			include_spip('base/create');
-			include_spip('base/abstract_sql');
-			spip_query("ALTER TABLE `spip_abonnements` ADD periode text NOT NULL default '';");
-			echo "Maj 0.5 de `spip_abonnements` (periode)";
-			ecrire_meta('abonnement_base_version',$current_version=$version_base);
-			ecrire_metas();
-		}
-		
-		if ($current_version < 0.6){
-			include_spip('base/create');
-			include_spip('base/abstract_sql');
-			spip_query("ALTER TABLE `spip_auteurs_elargis_abonnements` ADD validite datetime DEFAULT '0000-00-00 00:00:00' NOT NULL");
-			spip_query("ALTER TABLE `spip_auteurs_elargis_abonnements` ADD montant int(10) unsigned NOT NULL");
-			echo "Maj 0.6 de `spip_auteurs_elargis_abonnements` (validite, montant)";
-			ecrire_meta('abonnement_base_version',$current_version=$version_base);
-			ecrire_metas();
-		}
-		
-		if ($current_version < 0.61){
-			include_spip('base/create');
-			include_spip('base/abstract_sql');
-			spip_query("ALTER TABLE `spip_auteurs_elargis_abonnements` ADD stade_relance int(10) unsigned NOT NULL");
-			echo "Maj 0.61 de `spip_auteurs_elargis_abonnements` (stade_relance)";
-			ecrire_meta('abonnement_base_version',$current_version=$version_base);
-			ecrire_metas();
-		}
-		
-		if ($current_version < 0.62){
-			include_spip('base/create');
-			include_spip('base/abstract_sql');
-			spip_query("ALTER TABLE `spip_auteurs_elargis_articles` ADD montant int(10) unsigned NOT NULL");
-			echo "Maj 0.62 de `spip_auteurs_elargis_articles` (montant)";
-			ecrire_meta('abonnement_base_version',$current_version=$version_base);
-			ecrire_metas();
-		}
-		
-		if ($current_version < 0.7){
-			include_spip('base/create');
-			include_spip('base/abstract_sql');
-			spip_query("ALTER TABLE `spip_auteurs_elargis_abonnements` CHANGE `date` `date` DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00'");
-			spip_query("ALTER TABLE `spip_auteurs_elargis_abonnements` CHANGE `id_auteur_elargi` `id_auteur` INT( 10 ) UNSIGNED NOT NULL"); 
-			echo "Maj 0.7 de `spip_auteurs_elargis_abonnements` (date)";
-			ecrire_meta('abonnement_base_version',$current_version=$version_base);
-			ecrire_metas();
-		}
-		
-		if ($current_version < 0.75){
-			include_spip('base/create');
-			include_spip('base/abstract_sql');
-			creer_base();
-			ecrire_meta('abonnement_base_version',$current_version=$version_base);
-			ecrire_metas();
-		}
+	$current_version = 0.0;
+	if (   (isset($GLOBALS['meta']['abonnement_base_version']) )
+			&& (($current_version = $GLOBALS['meta'][$nom_meta_base_version])==$version_cible))
+		return;
 
+	include_spip('base/create');
+	include_spip('base/abstract_sql');
 
+	if ($current_version==0.0){
+		creer_base();
+		//echo "creation des tables spip_abonnements";
+		ecrire_meta($nom_meta_base_version,$current_version=$version_cible);
 	}
 	
-	function abonnement_vider_tables() {
-		spip_query("DROP TABLE spip_abonnements");
-		spip_query("DROP TABLE spip_auteurs_elargis_abonnements");
-		spip_query("DROP TABLE spip_auteurs_elargis_articles");
-		effacer_meta('abonnement_base_version');
-		ecrire_metas();
+	if ($current_version < 0.2){
+		creer_base();
+		echo "Maj 0.2 des tables spip_abonnements";	
+		ecrire_meta($nom_meta_base_version,$current_version=0.2);
 	}
 	
-	function abonnement_install($action){
-		global $abonnement_base_version;
-		switch ($action){
-			case 'test':
-				return (isset($GLOBALS['meta']['abonnement_base_version']) AND ($GLOBALS['meta']['abonnement_base_version']>=$abonnement_base_version));
-				break;
-			case 'install':
-				abonnement_upgrade();
-				break;
-			case 'uninstall':
-				abonnement_vider_tables();
-				break;
-		}
+	if ($current_version < 0.3){
+		creer_base();
+		echo "Maj 0.3 des tables spip_auteurs_elargis_articles";
+		ecrire_meta($nom_meta_base_version,$current_version=0.3);
 	}
+	
+	if ($current_version < 0.4){
+		// faudrait virer le autoincrement aussi
+		sql_alter("TABLE spip_auteurs_elargis_articles ADD INDEX id_auteur_elargi (id_auteur_elargi)");
+		sql_alter("TABLE spip_auteurs_elargis_articles DROP PRIMARY KEY");
+		echo "Maj 0.4 des index `spip_auteurs_elargis_articles`";
+		ecrire_meta($nom_meta_base_version,$current_version=0.4);
+	}
+	
+	if ($current_version < 0.5){
+		sql_alter("TABLE spip_abonnements ADD periode text NOT NULL default '';");
+		echo "Maj 0.5 de `spip_abonnements` (periode)";
+		ecrire_meta($nom_meta_base_version,$current_version=0.5);
+	}
+	
+	if ($current_version < 0.6){
+		sql_alter("TABLE spip_auteurs_elargis_abonnements ADD validite datetime DEFAULT '0000-00-00 00:00:00' NOT NULL");
+		sql_alter("TABLE spip_auteurs_elargis_abonnements ADD montant int(10) unsigned NOT NULL");
+		echo "Maj 0.6 de `spip_auteurs_elargis_abonnements` (validite, montant)";
+		ecrire_meta($nom_meta_base_version,$current_version=0.6);
+	}
+	
+	if ($current_version < 0.61){
+		sql_alter("TABLE spip_auteurs_elargis_abonnements ADD stade_relance int(10) unsigned NOT NULL");
+		echo "Maj 0.61 de `spip_auteurs_elargis_abonnements` (stade_relance)";
+		ecrire_meta($nom_meta_base_version,$current_version=0.61);
+	}
+	
+	if ($current_version < 0.62){
+		sql_alter("TABLE spip_auteurs_elargis_articles ADD montant int(10) unsigned NOT NULL");
+		echo "Maj 0.62 de `spip_auteurs_elargis_articles` (montant)";
+		ecrire_meta($nom_meta_base_version,$current_version=0.62);
+	}
+	
+	if ($current_version < 0.70){
+		sql_alter("TABLE spip_auteurs_elargis_abonnements CHANGE `date` `date` DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00'");
+		sql_alter("TABLE spip_auteurs_elargis_abonnements CHANGE `id_auteur_elargi` `id_auteur` INT( 10 ) UNSIGNED NOT NULL"); 
+		echo "Maj 0.7 de `spip_auteurs_elargis_abonnements` (date)";
+		ecrire_meta($nom_meta_base_version,$current_version=0.70);
+	}
+	
+	if ($current_version < 0.75){
+		maj_tables(array('spip_auteurs_elargis_abonnements'));
+		echo "Maj 0.75 de `spip_auteurs_elargis_abonnements` (statut_paiement, hash)";
+		ecrire_meta($nom_meta_base_version,$current_version=0.75);
+	}
+
+
+}
+
+function abonnement_vider_tables($nom_meta_base_version) {
+	sql_drop_table("spip_abonnements");
+	sql_drop_table("spip_auteurs_elargis_abonnements");
+	sql_drop_table("spip_auteurs_elargis_articles");
+	effacer_meta($nom_meta_base_version);
+}
+	
+
 		
 
 ?>
