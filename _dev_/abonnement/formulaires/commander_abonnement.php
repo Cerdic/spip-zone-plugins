0ced62177d76773921391b62c288e321c2024776
marcimat@free.fr <marcimat@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-05-01 15:53:31 +0000

    - on renomme le formulaire de commande en commander_abonnement - suppression du code d'ajout d'auteur dans une zone (que'est-ce que ça faisait là ?)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@28323 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _dev_/abonnement/formulaires/commander_abonnement.php
@@ -0,0 +1,99 @@
+<?php
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+/**
+ * 
+ * Chargement des valeurs par defaut des champs du formulaire
+ * 
+ * @return array L'ensemble des champs et de leur valeurs
+ * @param int $id_auteur[optional] Si cette valeur est utilisée, on entre dans le cadre de
+ * la modification d'un auteur, et plus dans la création
+ */
+function formulaires_commander_abonnement_charger_dist($id_auteur = NULL){
+	
+	// si pas d'id_auteur, on prend la personne identifiee si elle existe
+	// sinon c'est une inscription.
+	if (!$id_auteur) {
+		$id_auteur = $GLOBALS['auteur_session']['id_auteur'];
+		if (!$id_auteur) {
+			
+		}
+	}
+
+	// creer un hash unique pour la transaction
+	include_spip('inc/acces');
+	$hash = creer_uniqid();
+		
+	$champs = array(
+		"hash" => $hash,
+		"id_auteur" => $id_auteur,
+		"abonnement" => "",
+		"reabo" => "", // ? kesako
+	);
+
+
+	/*
+	// on n'y touche pas aux articles pour le moment
+	if(_request('reachat')){	
+		$champs['id_article'] = _request('id_article') ;				
+	}
+	*/
+	return $champs;
+}
+
+function formulaires_commander_abonnement_verifier_dist($id_auteur = NULL){
+   
+	//initialise le tableau des erreurs
+	$erreurs = array();
+	
+	//if(!_request('abonnement') OR !_request('id_auteur') )
+	//   	$erreurs['abonnement_pas_content'] = "oui";
+
+	// erreurs sur l'abonnement
+	if(!_request('abonnement')) {
+		$erreurs['abonnement'] = _T("abo:erreur_selection_abonnement");
+		$erreurs['message_erreur'] = _T("abo:erreur_presente");
+	}
+
+    return $erreurs; // si c'est vide, traiter sera appele, sinon le formulaire sera resoumis
+}
+
+
+function formulaires_commander_abonnement_traiter_dist($id_auteur = NULL){
+	spip_log('traiter','paiement-form');
+	global $tables_principales;
+	
+	$hash = _request('hash');
+	$message = " ";
+	
+	/* c'est un abonnement */
+	$id_abonnement = _request('abonnement');
+	$id_auteur = _request('id_auteur');
+	
+	// enregistrer l'abonnement
+	// todo ? [attention aux doublons...]
+	sql_insertq("spip_auteurs_elargis_abonnements", array(
+		"id_auteur" => $id_auteur,
+		"id_abonnement" => $id_abonnement,
+		"hash" => $hash,
+		"date" => date("Y-m-d H:i:s"),
+	));
+
+	return array('editable'=>false,'message_ok' => $message);
+
+	
+	// a deplacer...
+	// a maj
+	if($value = _request('reachat_valide_hash')){		
+		sql_insertq("spip_auteurs_elargis_articles", array(
+			"id_auteur" => $id_article,
+			"id_article" => $id_article,
+			"statut_paiement" => 'a_confirmer',
+			"hash" => $hash,
+		));
+	}
+
+    return array('editable'=>false, 'message_ok' => $message);
+}
+?>
