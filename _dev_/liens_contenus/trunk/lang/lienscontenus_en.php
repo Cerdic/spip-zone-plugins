802158783e89774aa9e5446a386266c87232be08
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-05-03 20:09:49 +0000

    langues (lienscontenus)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@28370 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/liens_contenus/trunk/lang/lienscontenus_en.php
+++ _dev_/liens_contenus/trunk/lang/lienscontenus_en.php
@@ -8,14 +8,14 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
-	'alerte_publie_contenant' => '<MODIF>Be careful, this content is published, but contains links to other contents that are not!',
+	'alerte_publie_contenant' => 'Be careful, this content is published, but contains links to other contents that are not!',
 	'aucun_objets_avec_lien_depuis_courant' => 'This content doesn\'t contain any link to another content.',
 	'aucun_objets_avec_lien_vers_courant' => 'No other content contain link to this one.',
 
 	// C
-	'confirmation_depublication' => '<MODIF>Be careful, a published content contains at least one link to this one and will be affected if you change its status!\\n\\nDo you still want to change the status?',
-	'confirmation_publication' => '<MODIF>Be careful, this content links to at least one other content that is not yet published!\\n\\nDo you still want to change the status?',
-	'confirmation_suppression' => '<MODIF>Be careful, a published content contains at least one link to this one and will be affected if you delete it!\\n\\nDo you still want to delete it?',
+	'confirmation_depublication' => 'Be careful, a published content contains at least one link to this one and will be affected if you change its status!nnDo you still want to change the status?',
+	'confirmation_publication' => 'Be careful, this content links to at least one other content that is not published!nnDo you still want to change the status?',
+	'confirmation_suppression' => 'Be careful, a published content contains at least one link to this one and will be affected if you delete it!nnDo you still want to delete it?',
 
 	// I
 	'information_element_contenu' => 'Be careful, another content links to this one!',
