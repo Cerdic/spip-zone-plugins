cab2e800db9d23a3a89a6857590a71667f47cb62
romy@tetue.net <romy@tetue.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-05-04 18:54:56 +0000

    Mise à jour du message francophone, pour n'avoir plus à mettre à jour à chaque version d'IE ;-)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@28389 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/noie/lang/noie_fr.php
+++ _stable_/noie/lang/noie_fr.php
@@ -6,10 +6,8 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// I
-	'ie6msg_info' => 'Pour naviguer de la mani&egrave;re la plus satisfaisante sur ce site (et sur le reste du Web), nous vous recommandons de proc&eacute;der &agrave; une mise &agrave; jour de votre navigateur.
-La&nbsp;version actuelle est <a href="http://www.microsoft.com/france/windows/downloads/ie/getitnow.mspx">Internet Explorer&nbsp;8</a>. La mise &agrave; jour est gratuite.
-<br />Si&nbsp;vous utilisez un PC au travail, veuillez contacter votre service informatique.
-<br />Si&nbsp;vous le souhaitez, vous pouvez aussi essayer d\'autres navigateurs web populaires comme par exemple <a href="http://www.mozilla-europe.org/fr/firefox/">Firefox</a>, <a href="http://www.opera.com">Opera</a> ou <a href="http://www.apple.com/fr/safari/download/">Safari</a>.',
+	'ie6msg_info' => 'Pour naviguer de fa&ccedil;on plus satisfaisante sur ce site (et sur le reste du Web), nous vous recommandons de proc&eacute;der &agrave; une mise &agrave; jour de votre navigateur. Actualisez gratuitement votre <a href="http://www.microsoft.com/france/windows/products/winfamily/ie/">Internet Explorer</a>. Vous&nbsp;pouvez aussi essayer d\'autres navigateurs populaires comme par exemple <a href="http://www.mozilla-europe.org/fr/firefox/">Firefox</a>, <a href="http://www.opera.com/">Opera</a> ou <a href="http://www.apple.com/fr/safari/">Safari</a>.
+<br />Si&nbsp;vous utilisez un PC au travail, veuillez contacter votre service informatique.',
 	'ie6msg_titre' => 'Savez-vous que votre navigateur est obsol&egrave;te&nbsp;?',
 
 	// T
