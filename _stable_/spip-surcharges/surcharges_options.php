b56b54a12ff8da25ece1d96fdf1e665deff5b8b3
pierre.basson@gmail.com <pierre.basson@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-05-05 08:51:43 +0000

    spip-surcharges : warning options
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@28403 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/spip-surcharges/surcharges_options.php
+++ _stable_/spip-surcharges/surcharges_options.php
@@ -8,9 +8,11 @@
 	 **/
 
 
-	$p = explode(basename(_DIR_PLUGINS)."/",str_replace('\\','/',realpath(dirname(__FILE__))));
-	define('_DIR_PLUGIN_SURCHARGES', (_DIR_PLUGINS.end($p)));
-	define('_NOM_PLUGIN_SURCHARGES', (end($p)));
+	if (!defined('_DIR_PLUGIN_SURCHARGES')) {
+		$p = explode(basename(_DIR_PLUGINS)."/",str_replace('\\','/',realpath(dirname(__FILE__))));
+		define('_DIR_PLUGIN_SURCHARGES', (_DIR_PLUGINS.end($p)));
+		define('_NOM_PLUGIN_SURCHARGES', (end($p)));
+	}
 
 
 ?>
