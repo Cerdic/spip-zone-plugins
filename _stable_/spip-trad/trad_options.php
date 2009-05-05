e0e7413bb6504a611c46e671a928b9d046a7b6fd
pierre.basson@gmail.com <pierre.basson@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-05-05 08:52:16 +0000

    spip-trad : warning options
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@28405 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/spip-trad/trad_options.php
+++ _stable_/spip-trad/trad_options.php
@@ -13,9 +13,11 @@
 	 **/
 
 
-	$p = explode(basename(_DIR_PLUGINS)."/",str_replace('\\','/',realpath(dirname(__FILE__))));
-	define('_DIR_PLUGIN_TRAD', (_DIR_PLUGINS.end($p)));
-	define('_NOM_PLUGIN_TRAD', (end($p)));
+	if (!defined('_DIR_PLUGIN_TRAD')) {
+		$p = explode(basename(_DIR_PLUGINS)."/",str_replace('\\','/',realpath(dirname(__FILE__))));
+		define('_DIR_PLUGIN_TRAD', (_DIR_PLUGINS.end($p)));
+		define('_NOM_PLUGIN_TRAD', (end($p)));
+	}
 
 
 ?>
