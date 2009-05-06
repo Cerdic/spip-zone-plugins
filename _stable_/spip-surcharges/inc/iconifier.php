7e7fccfc61cf342f11ae2e098bd26dd32f3954d5
pierre.basson@gmail.com <pierre.basson@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-05-06 13:13:23 +0000

    spip-surcharges : exit le code de spip-plans
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@28439 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/spip-surcharges/inc/iconifier.php
+++ _stable_/spip-surcharges/inc/iconifier.php
@@ -89,8 +89,6 @@ $logo_libelles = array(
 		       'id_lettre' => _T('lettresprive:logo_lettre'),
 		       'id_formulaire' => _T('formulairesprive:logo_formulaire'),
 		       'id_sondage' => _T('sondagesprive:logo_sondage'),
-		       'id_plan' => _T('plans:logo_plan'),
-		       'id_point' => _T('plans:logo_point'),
 		       'id_notification' => _T('facteur:logo_notifications')
 		       );
 
