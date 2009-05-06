a0817495e3cdfa6325615b3a6cfac11199430e78
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-05-06 15:51:47 +0000

    y voir plus clair dans ce fichier de langue avant de le donner à manger à salvatore
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@28453 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/googlemap_api/lang/geomap_fr.php
+++ _dev_/googlemap_api/lang/geomap_fr.php
@@ -6,27 +6,48 @@
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
+	// A
 	'api_version' => 'Version de l\'API',
+	
+	// C
 	'clave_engadida' => 'Clef ajout&eacute;e: ',
 	'cle' => 'Cl&eacute;',
+	'compacte' => 'Compacte',
 	'configuration' => 'Configuration',
+	'configuration_titre' => 'Configuration du plugin GoogleMap Api',
 	'conseguir' => '(obtenir)',
+	
+	// D
 	'default_geoloc' => 'Position par d&eacute;faut des cartes :',
 	'desactiver_custom_control' => 'D&eacute;sactiver custom_control.js',
+	
+	// E
 	'explication_api_version' => 'API Version <a href="http://code.google.com/apis/maps/documentation/index.html#API_Updates" class="spip_out">info</a>:',
 	'explication_cle' => 'Google Map API Key : <a href="http://www.google.com/apis/maps/signup.html" class="spip_out">Obtenir une cl&eacute;</a>',
+	'explication_compacte' => '&Eacute;viter le compactage du javascript',
 	'explication_custom_control' => 'D&eacute;sactiver les contr&ocirc;les personnalis&eacute;s :',
+	
+	// L
 	'latitude' => 'Latitude',
 	'longitude' => 'Longitude',
+	
+	// M
 	'miseajour' => 'Mise &agrave; jour',
-	'pages_public' => 'Pages publiques',
-	'rechercher' => 'Rechercher',
-	'zoom' => 'Zoom',
-	'configuration_titre' => 'Configuration du plugin GoogleMap Api',
-	'compacte' => 'Compacte',
-	'explication_compacte' => 'eviter le compactage du javascript',
+	
+	// N
 	'non' => 'non',
-	'oui' => 'oui'
+	
+	// O
+	'oui' => 'oui',
+	
+	// P
+	'pages_public' => 'Pages publiques',
+	
+	// R
+	'rechercher' => 'Rechercher',
+	
+	// Z
+	'zoom' => 'Zoom'
 	
 );
 
