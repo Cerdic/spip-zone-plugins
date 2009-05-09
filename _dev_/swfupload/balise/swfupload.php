97f803a92da0dae735fe22acea1b9fa69ab2130d
yaquoi@yaquoi.com <yaquoi@yaquoi.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-05-09 21:13:31 +0000

    Telechargement multiple par lot directement dans les articles. testé sous ff3 et ie7 ...
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@28517 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/swfupload/balise/swfupload.php
+++ _dev_/swfupload/balise/swfupload.php
@@ -3,7 +3,7 @@
 if (!defined("_ECRIRE_INC_VERSION")) return;	#securite
 
 function balise_SWFUPLOAD ($p) {
-	$p = calculer_balise_dynamique($p,'SWFUPLOAD', array());
+	$p = calculer_balise_dynamique($p,'SWFUPLOAD', array(''));
 	return $p;
 }
 
@@ -14,7 +14,7 @@ function balise_SWFUPLOAD_stat($args, $filtres) {
 
 // dyn
 function balise_SWFUPLOAD_dyn($str="")
-{
+{	  
   
   if (isset($GLOBALS['auteur_session']['statut']) &&
 		 ($GLOBALS['auteur_session']['statut']!="0minirezo")) {
@@ -43,9 +43,23 @@ function balise_SWFUPLOAD_dyn($str="")
 	spip_log("swfupload: $nom");  
 
 	$file_id = md5($_FILES["Filedata"]["tmp_name"] + rand()*100000);
-	return $file_id;
 
-  }
+	
+  
+    $id_article=intval(_request('id_article'));
+	
+	if ($id_article!=0)	{
+	$files = array(array (
+			'name' => $_FILES['Filedata']['name'],
+			'tmp_name' => $nom)
+			);
+	$type = "article";
+	$mode = 'document';
+	include_spip('action/joindre');
+	joindre_documents($files, $mode, $type, $id_article, 0, $hash, $redirect, $actifs, $iframe_redirect);
+
+					}
+	}
 }
 
 ?>
--- _dev_/swfupload/swfupload_pipelines.php
+++ _dev_/swfupload/swfupload_pipelines.php
@@ -16,6 +16,8 @@ function swfupload_ajouterBoutons($boutons_admin) {
 function swfupload_header_prive($flux) {
 global $connect_statut,$connect_toutes_rubriques;
 
+$id_article=intval(_request('id_article'));
+
 if ($GLOBALS['connect_statut'] == "0minirezo" || $GLOBALS["connect_toutes_rubriques"]) {
 $session = session_id();
 
@@ -47,7 +49,10 @@ $flux .= '<!-- swfupload -->
 				// Backend Settings - Relative to the SWF file, utiliser url absolue cest mieux					
 				upload_url: "'.$GLOBALS['meta']["adresse_site"].'/?page=swfupload",
 
-				post_params: {"PHPSESSID": "'.$session.'"},
+				post_params: {
+				"PHPSESSID": "'.$session.'",
+				"id_article": "'.$id_article.'"
+				},
 				// File Upload Settings
 				file_size_limit : "'.$file_size_limit.'",	// 2MB
 				file_types : "'.$file_types.'",
@@ -84,5 +89,42 @@ $flux .= '<!-- swfupload -->
 	</script>';
 }
 	return $flux;
+}
+
+function swfupload_affiche_milieu($flux){
+global $connect_statut,$connect_toutes_rubriques;
+
+ $args = $flux['args'];
+ $out = "";
+ $id_article = $args['id_article'];
+if ($GLOBALS['connect_statut'] == "0minirezo" || $GLOBALS["connect_toutes_rubriques"]) {
+	if ($args['exec']=='articles') {
+	$bouton = _T('swfupload:titre_swfupload');
+	$out .= debut_cadre_enfonce(_DIR_PLUGIN_SWFUPLOAD.'images/swfupload-24.png', true, "", 
+			bouton_block_depliable($bouton,$flag === 'ajax','swfupload'))
+		. debut_block_depliable($flag === 'ajax','swfupload');
+	
+
+$out .= '<form id="form1" action="index.php" method="post" enctype="multipart/form-data">
+		<div>'._T('swfupload:texte_swfupload_article').'</div>
+<input type="hidden" value='.$id_article.' name="id_article" />
+		<div class="content">
+			<fieldset class="flash" id="fsUploadProgress">
+				<legend>'._T('swfupload:texte_uploadqueue').'</legend>
+			</fieldset>
+			<div id="divStatus">0 '._T('swfupload:texte_filesupload').'</div>
+			<div style="padding-left: 5px;">
+				<span id="spanButtonPlaceholder"></span>
+				<input id="btnCancel" type="button" value="'._T('swfupload:texte_cancelupload').'" onclick="cancelQueue(swfu);" disabled="disabled"  style="margin-left: 2px; height: 22px; font-size: 8pt;" /><br />
+			</div>
+		</div>
+	</form>';
+	
+$out .= fin_block()
+		. fin_cadre_enfonce(true);
+	}
+	$flux['data'].= $out;
+	}
+	return $flux;	
 }
 ?>
