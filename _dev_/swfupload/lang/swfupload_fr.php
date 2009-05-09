1eb0054dcfcf0089fe66f8739b2d80910381d046
yaquoi@yaquoi.com <yaquoi@yaquoi.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-05-09 22:00:04 +0000

    un peu d anglais
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@28520 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _dev_/swfupload/lang/swfupload_fr.php
+++ _dev_/swfupload/lang/swfupload_fr.php
@@ -3,7 +3,7 @@
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-'titre_swfupload' => 'SWFupload - T&eacute;l&eacute;chargement de fichiers',
+'titre_swfupload' => 'SWFupload - T&eacute;l&eacute;chargement de fichiers par lot',
 'texte_swfupload' => 'Vous pouvez t&eacute;l&eacute;charger un lot de fichiers dans votre r&eacute;pertoire tmp/upload.<br />Ces fichiers seront alors accessibles lors de l\'ajout de documents joints.',
 'texte_swfupload_article' => 'Vous pouvez t&eacute;l&eacute;charger un lot de fichiers dans votre article.<br />Actualisez la page pour visualiser les fichiers ajout&eacute;s.',
 'file_size_limit' => '<strong>Taille limite des fichiers</strong><br/>Ce param&egrave;tre accepte les unit&eacute;s.<br />Les unit&eacute;s valides sont B, KB, MB, and GB. L\'unit&eacute; par d&eacute;faut est KB.<br />Exemples: 2147483648 B, 2097152, 2097152KB, 2048 MB, 2 GB',
