e1fd19595d157aa68e4c3e6327948099f3796561
paladin@quesaco.org <paladin@quesaco.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-05-10 18:09:46 +0000

    L'icone du plugin. Le fichier de lanf. Pfff...
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@28565 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _stable_/mon_diplome/lang/plom_fr.php
@@ -0,0 +1,27 @@
+<?php
+
+// lang/plom_fr.php
+
+
+// $LastChangedRevision$
+// $LastChangedBy$
+// $LastChangedDate$
+ 
+$GLOBALS['i18n_plom_fr'] = array(
+
+	'mon_diplome' => "Mon diplôme"
+	, 'obtenir_mon_diplome' => "Obtenir mon diplôme"
+	, 'inconnu' => "Inconnu"
+	
+	, 'diplome_ligne_1' => "Les responsables de Spip-ville ont l'immense honneur de d&#233;cerner ce jour le"
+	, 'diplome_etudes_sup' => "Dipl&#244;me d'&#233;tudes sup&#233;rieures spipiennes"
+	, 'diplome_ligne_2' => "&#224;"
+	, 'diplome_ligne_3' => "Fait &#224; Dijon le "
+	, 'diplome_ligne_4' => "pour en jouir avec les droits et pr&#233;rogatives qui y sont attach&#233;s."
+	
+	, 'erreur_diplome_manquant_i' => "Votre dipl&#244;me est introuvable (code erreur #ID_@i@)"
+	, 'erreur_fichier_s_manquant' => "Le fichier @s@ est introuvable."
+	
+);
+
+?>
