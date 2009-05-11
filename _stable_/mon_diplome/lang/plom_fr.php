8424d34d07088f4b08a6978a5992fe55e912b872
paladin@quesaco.org <paladin@quesaco.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-05-11 04:15:07 +0000

    Des entités HTML pour les diacritiques.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@28586 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/mon_diplome/lang/plom_fr.php
+++ _stable_/mon_diplome/lang/plom_fr.php
@@ -9,14 +9,14 @@
  
 $GLOBALS['i18n_plom_fr'] = array(
 
-	'mon_diplome' => "Mon diplôme"
-	, 'obtenir_mon_diplome' => "Obtenir mon diplôme"
+	'mon_diplome' => "Mon dipl&#244;me"
+	, 'obtenir_mon_diplome' => "Obtenir mon dipl&#244;me"
 	, 'inconnu' => "Inconnu"
 	
 	, 'diplome_ligne_1' => "Les responsables de Spip-ville ont l'immense honneur de d&#233;cerner ce jour le"
 	, 'diplome_etudes_sup' => "Dipl&#244;me d'&#233;tudes sup&#233;rieures spipiennes"
 	, 'diplome_ligne_2' => "&#224;"
-	, 'diplome_ligne_3' => "Fait &#224; Dijon le "
+	, 'diplome_ligne_3' => "Fait &#224; Spip-ville le "
 	, 'diplome_ligne_4' => "pour en jouir avec les droits et pr&#233;rogatives qui y sont attach&#233;s."
 	
 	, 'erreur_diplome_manquant_i' => "Votre dipl&#244;me est introuvable (code erreur #ID_@i@)"
