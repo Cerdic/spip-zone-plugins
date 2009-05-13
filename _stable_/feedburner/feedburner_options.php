e3a9e1cb61986c6a30f67aa595b87b2cfc7aa88b
fil@rezo.net <fil@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-05-13 08:23:21 +0000

    detecter la visite de l'agent feedburner, pas seulement avec ?var_feedburner=oui (qui devient un outil de debug et de compat ascendante). rendre optionnelle la redirection d'url sur ?page=backend
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@28621 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/feedburner/feedburner_options.php
+++ _stable_/feedburner/feedburner_options.php
@@ -1,10 +1,17 @@
 <?php
 
-if (_request('page')=='backend' AND _request('var_feedburner')!=='oui' AND lire_config('feedburner/feedId')) {
+//
+// Si la page est backend, si le visiteur n'est pas feedburner
+// et si on a precise une URL de redirection de flux, rediriger
+// Attention pas compatible avec les flux specialises ?page=backend&id_mot=12
+//
+if (_request('page')=='backend'
+AND !(_request('var_feedburner') == 'oui'
+	OR strstr($_SERVER['HTTP_USER_AGENT'], 'FeedBurner'))
+AND function_exists('lire_config')
+AND lire_config('feedburner/url')) {
 	include_spip('inc/headers');
-	$url=lire_config('feedburner/url');
-	if(!$url) $url='http://feeds.feedburner.com/~e?ffid='.lire_config('feedburner/feedId');
-	redirige_par_entete($url);
+	redirige_par_entete(lire_config('feedburner/url'));
 }
 
 ?>
