2d9ce5aec6203e7efb082659d82761cc3b6769b4
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-05-13 17:56:40 +0000

    Une version anglaise certainement à revoir
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@28627 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _dev_/abomailmans/lang/abomailmans_en.php
@@ -0,0 +1,71 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// $Id: abomailmans_fr.php 26976 2009-03-01 00:33:38Z kent1@arscenic.info $
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+		'abomailmans' 					=> 'Mailmans',
+		'icone_ajouter_liste'			=> 'Add a new list',
+		'icone_envoyer_mail_liste'		=> 'Send an e-mail to the lists from the content of this site',
+		'les_listes_mailmans'			=> 'The lists of Mailman or Sympa filled',
+		
+		
+		'titre_abomailman'				=> 'Title of the list',
+		'emailliste_abomailman'			=> 'The email address of the list',
+		'supprimer'						=> 'Delete',
+
+		'nom'							=> 'Name and first name (optional)',
+		'prenom'						=> 'First name',
+		'email_abonnement'				=> 'Your email address',
+		'votre_email'					=> 'Your email',
+		
+		'je_m_abonne'					=> 'Check to confirm the subscription or unsubscription.',
+
+		'message_confirmation_a'      	=> 'A subscription request to the following lists has been sent :',
+		'message_confirmation_unique_a' => 'A subscription request to the following list has been sent :',
+		'message_confirmation_d'        => 'An unsubscribing request from the lists below has been sent. ',
+		'message_confirmation_unique_d'	=> 'An unsubscribing request from the list below has been sent. ',
+		'message_confirm_suite'         => 'To validate your request, please reply to the confirmation email that you will receive.',
+		
+		'email_oublie'					=> 'You forgot your email address',
+		
+		'envoyer_courier'				=> 'Send an email',
+		'template'						=> 'Choose a template and his content',
+		'sujet'							=> 'Subject of the mail',
+		'rubrique'						=> 'And list the articles of the section',
+		'mot'							=> 'And list the articles linked to the keyword',
+		'message'						=> 'Introduction of your email, before the site\'s content',
+		'envoi_apercu'					=> 'Previsualization',
+		'envoi_confirmer'				=> 'Confirm and send',
+		'envoyer_mailmans'				=> 'Select the template and his content',
+		'sujet_obligatoire'				=> 'The subject is necessary.',
+		'email_envoye'					=> 'The email has been sent to the list : @liste@.',
+		
+		'creation_droits_insuffisants'	=>	'Your rights are insufficients',
+		'emailliste_abosympa'           =>	'The email address of the Sympa admin',
+		'info_sisympa'                  =>	'[Necessary for a Sympa list]',
+		'btn_abonnement'                =>	'Subscribe',
+		'btn_desabonnement'             =>	'Unsubscribe',
+		'liste_oublie'                  =>	'You forgot to check a list.',
+		'active'                        =>	'Active',
+		'desactive'                     =>	'Deactivated',
+		'supprimer'						=>	'Delete',
+		'langue_liste'					=>	'Language of the list',
+		'aucune_langue'					=>	'None specified',
+		'sympa_message_confirmation'	=>	'A validation email has been sent to the address : ',
+		'verifier_formulaire'			=>	'Please verify the filling of the form.',
+		'liste_updatee'					=>	'The list number @id@ (@titre@) has been updated.',
+		'liste_creee'					=>	'The list number @id@ (@titre@) has been created.',
+		'liste_supprimee'				=>	'The list number @id@ (@titre@) has been deleted.',
+		'liste_non_existante'			=>	'The asked list doesn\'t exist or has been deleted',
+
+		'envoyer_courier_liste'			=>	'Send this content to this list :',
+
+		'contenu_date'					=>	'Content since this date',
+
+		// #FORMULAIRE_ABOMAILMAN
+		'insciption_listes_legende'		=> 'Subscription to the mailing lists',
+		'choisir_liste'					=> 'You should choose a list.',
+);
+
+?>
