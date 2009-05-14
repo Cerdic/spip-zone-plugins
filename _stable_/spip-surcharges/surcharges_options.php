a08458629420bff802700127b46533d956e3c900
pierre.basson@gmail.com <pierre.basson@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-05-14 12:13:17 +0000

    spip-surcharges : exit test define options
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@28642 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/spip-surcharges/surcharges_options.php
+++ _stable_/spip-surcharges/surcharges_options.php
@@ -8,11 +8,9 @@
 	 **/
 
 
-	if (!defined('_DIR_PLUGIN_SURCHARGES')) {
-		$p = explode(basename(_DIR_PLUGINS)."/",str_replace('\\','/',realpath(dirname(__FILE__))));
-		define('_DIR_PLUGIN_SURCHARGES', (_DIR_PLUGINS.end($p)));
-		define('_NOM_PLUGIN_SURCHARGES', (end($p)));
-	}
+	$p = explode(basename(_DIR_PLUGINS)."/",str_replace('\\','/',realpath(dirname(__FILE__))));
+	define('_DIR_PLUGIN_SURCHARGES', (_DIR_PLUGINS.end($p)));
+	define('_NOM_PLUGIN_SURCHARGES', (end($p)));
 
 
 ?>
