ae7e0f253b05c396b52df4bc48f339a287841641
pierre.basson@gmail.com <pierre.basson@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-05-14 12:12:19 +0000

    spip-lettres : exit test define options
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@28638 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/spip-lettres/lettres_options.php
+++ _stable_/spip-lettres/lettres_options.php
@@ -13,12 +13,10 @@
 	 **/
 
 
-	if (!defined('_DIR_PLUGIN_LETTRE_INFORMATION')) {
-		$p = explode(basename(_DIR_PLUGINS)."/",str_replace('\\','/',realpath(dirname(__FILE__))));
-		define('_DIR_PLUGIN_LETTRE_INFORMATION', (_DIR_PLUGINS.end($p)));
-		define('_NOM_PLUGIN_LETTRE_INFORMATION', (end($p)));
-		define('_DIR_LETTRES', _DIR_IMG.'lettres/');
-	}
+	$p = explode(basename(_DIR_PLUGINS)."/",str_replace('\\','/',realpath(dirname(__FILE__))));
+	define('_DIR_PLUGIN_LETTRE_INFORMATION', (_DIR_PLUGINS.end($p)));
+	define('_NOM_PLUGIN_LETTRE_INFORMATION', (end($p)));
+	define('_DIR_LETTRES', _DIR_IMG.'lettres/');
 
 
 ?>
