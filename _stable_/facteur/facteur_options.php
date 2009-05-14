0e3c7f31c84bef9e65c34488fb6e5064d772aabf
pierre.basson@gmail.com <pierre.basson@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-05-14 12:13:50 +0000

    facteur : exit test define options
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@28644 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/facteur/facteur_options.php
+++ _stable_/facteur/facteur_options.php
@@ -1,11 +1,9 @@
 <?php
 
 
-	if (!defined('_DIR_PLUGIN_FACTEUR')) {
-		$p = explode(basename(_DIR_PLUGINS)."/",str_replace('\\','/',realpath(dirname(__FILE__))));
-		define('_DIR_PLUGIN_FACTEUR', (_DIR_PLUGINS.end($p)));
-		define('_NOM_PLUGIN_FACTEUR', (end($p)));
-	}
+	$p = explode(basename(_DIR_PLUGINS)."/",str_replace('\\','/',realpath(dirname(__FILE__))));
+	define('_DIR_PLUGIN_FACTEUR', (_DIR_PLUGINS.end($p)));
+	define('_NOM_PLUGIN_FACTEUR', (end($p)));
 
 
 ?>
