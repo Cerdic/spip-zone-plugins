00a204911302da4a2f7c8c80c444a458bf538996
webmaster@geomaticien.com <webmaster@geomaticien.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-05-16 02:44:10 +0000

    Notice pour répondre à un article.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@28673 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _stable_/acs/models/cat/composants/forums/lang/forums_en.php
@@ -0,0 +1,8 @@
+<?php
+// This is a SPIP-ACS language file  --  Ceci est un fichier langue de SPIP-ACS
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+'repondre_article_notice' => 'You must be member or have a valid email'
+);
+?>
---
+++ _stable_/acs/models/cat/composants/forums/lang/forums_fr.php
@@ -0,0 +1,8 @@
+<?php
+// This is a SPIP-ACS language file  --  Ceci est un fichier langue de SPIP-ACS
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+'repondre_article_notice' => 'Vous devez &ecirc;tre membre du site ou avoir un email valide'
+);
+?>
