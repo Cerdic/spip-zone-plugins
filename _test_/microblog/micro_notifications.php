f6251aa302c93efbad1d249bd5bc04aac6d5a311
fil@rezo.net <fil@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-05-23 21:49:09 +0000

    on separe la partie notifications de la lib d'envoi + indentation tabulee
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@28786 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _test_/microblog/micro_notifications.php
@@ -0,0 +1,69 @@
+<?php
+
+/*****************************************************************\
+ * spip|microblog
+ *                      (c) Fil 2009
+ *
+ * envoyer des micromessages depuis SPIP vers twitter ou laconica
+ * distribue sous licence GNU/LGPL
+ *
+\*****************************************************************/
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+
+
+/*
+ * Buzzer les notifications
+ */
+
+function Microblog_notifications($x) {
+  include_spip('inc/filtres_mini');
+  include_spip('inc/texte');
+
+	$status = null;
+	$cfg = @unserialize($GLOBALS['meta']['microblog']);
+	switch($x['args']['quoi']) {
+		case 'forumposte':      // post forums
+			if ($cfg['evt_forumposte']
+			AND $id = intval($x['args']['id'])) {
+				$url = url_absolue(generer_url_entite($id, 'forum'));
+				$t = sql_fetsel('titre,texte', 'spip_forum', 'id_forum='.$id);
+				$titre = couper(typo($t['titre'].' | '.$t['texte']),
+					120 - strlen('#forum  ') - strlen($url));
+				$status = "$titre #forum $url";
+			}
+			break;
+		
+		case 'instituerarticle':    // publier | proposer articles
+		if ($id = intval($x['args']['id'])
+			AND ( ($cfg['evt_publierarticles'] 
+				AND $x['args']['options']['statut'] == 'publie') // publier
+			OR ($cfg['evt_proposerarticles'] 
+				AND $x['args']['options']['statut'] == 'prop' 
+				AND $x['args']['options']['statut_ancien'] != 'publie') )  // proposer
+		) {
+			$url = str_replace('amp;','',url_absolue(generer_url_entite($id, 'article')));
+			$t = sql_fetsel('titre,descriptif,texte', 'spip_articles', 'id_article='.$id);
+			$etat = str_replace(array('prop','publie'),
+				array(_T('microblog:propose'),_T('microblog:publie')),
+				$x['args']['options']['statut']
+			);
+			$titre = couper(typo($t['titre']
+				.' | '._T('microblog:article').' '.$etat
+				.' | '.($t['descriptif'] != '' ? $t['descriptif'].' | ' : '')
+				.$t['texte']),
+				120 - strlen($url));
+			$status = "$titre $url";
+		}
+		break;
+	}
+
+	if (!is_null($status)) {
+		include_spip('inc/microblog');
+		microblog($status);
+	}
+
+	return $x;
+}
+
