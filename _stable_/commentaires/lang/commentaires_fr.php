a0e0e3e5201ab405d4c691242e2dd5e0b523122a
romy@tetue.net <romy@tetue.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-05-28 16:42:56 +0000

    Ebauche d'un plugin servant a enregistrer et afficher (formulaire et noisette) des commentaires.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@28871 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ _stable_/commentaires/lang/commentaires_fr.php
@@ -0,0 +1,30 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/_stable_/noie/lang/
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// Liste des commentaires
+'message' => 'commentaire',
+'messages_forum' => 'commentaires',
+'commentaire' => 'commentaire',
+'commentaires' => 'commentaires',
+
+// Formulaire de commentaires
+'form_pet_message_commentaire' => 'Ajoutez votre commentaire ici',
+'forum_info_modere' => 'Attention, les commentaires sont mod&eacute;r&eacute;s&nbsp;: votre message n\'appara&icirc;tra qu\'apr&egrave;s avoir &eacute;t&eacute; relu et approuv&eacute;.',
+
+'forum_qui_etes_vous' => 'Qui &ecirc;tes-vous&nbsp;?',
+'forum_votre_nom' => 'Nom',
+'forum_votre_email' => 'Courriel',
+'forum_url' => 'Votre site web',
+
+'info_creation_paragraphe' => 'Ce formulaire accepte les <a rel="external nofollow" href="http://www.spip.net/fr_article1578.html" class="spip_out">raccourcis&nbsp;SPIP</a> et le code HTML. Pour cr&eacute;er des paragraphes, laissez simplement des lignes vides.',
+
+'forum_voir_avant' => 'Pr&eacute;visualiser',
+'forum_message_definitif' => 'Confirmer l\'envoi',
+
+);
+
+?>
