adc680861b1f818f6587b35648321df3ee0d2e00
webmaster@geomaticien.com <webmaster@geomaticien.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-05-28 05:44:34 +0000

    Utilisation de formulaires de la Dist SPIP et MàJ css
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@28862 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _stable_/acs/models/cat/composants/forums/lang/forums_en.php
+++
@@ -1,8 +0,0 @@
-<?php
-// This is a SPIP-ACS language file  --  Ceci est un fichier langue de SPIP-ACS
-
-$GLOBALS[$GLOBALS['idx_lang']] = array(
-
-'repondre_article_notice' => 'You must be member or have a valid email'
-);
-?>
--- _stable_/acs/models/cat/composants/forums/lang/forums_fr.php
+++
@@ -1,8 +0,0 @@
-<?php
-// This is a SPIP-ACS language file  --  Ceci est un fichier langue de SPIP-ACS
-
-$GLOBALS[$GLOBALS['idx_lang']] = array(
-
-'repondre_article_notice' => 'Vous devez &ecirc;tre membre du site ou avoir un email valide'
-);
-?>
