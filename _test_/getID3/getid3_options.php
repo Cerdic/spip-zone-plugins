940389cc2a748f955e34c277217f8f7cf48ddbe7
ben.spip@gmail.com <ben.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-05-30 23:42:20 +0000

    on bouge
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@28922 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- _test_/getID3/getid3_options.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if (!defined('_DIR_PLUGIN_GETID3')){
-	$p=explode(basename(_DIR_PLUGINS)."/",str_replace('\\','/',realpath(dirname(__FILE__))));
-	define('_DIR_PLUGIN_GETID3',(_DIR_PLUGINS.end($p)).'/');
-}
-
-?>
