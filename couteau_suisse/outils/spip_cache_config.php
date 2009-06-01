e34ae88f8027651abcb37595d56e7fadf0b97ba2
patfr@ifrance.com <patfr@ifrance.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-06-01 00:44:42 +0000

    Poursuite du changement de méthode de config pour les outils suivants : - SPIP et ses raccourcis… - SPIP et le cache…
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@28948 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ couteau_suisse/outils/spip_cache_config.php
@@ -0,0 +1,66 @@
+<?php
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+# Fichier de configuration pris en compte par config_outils.php et specialement dedie a la configuration du cache SPIP
+# --------------------------------------------------------------------------------------------------------------------
+
+// Fonction d'ajout de l'outil 'spip_cache'
+function spip_cache_add_outil() { return array(
+	'code:spip_options' => "%%radio_desactive_cache".(defined('_SPIP19300')?'4':'3')
+		."%%%%duree_cache%%%%duree_cache_mutu%%%%quota_cache%%%%derniere_modif_invalide%%",
+	'categorie' => 'admin',
+	'description' => (defined('_SPIP19300')?'<:spip_cache:2:>':'<:spip_cache:1:>').'<:spip_cache::>',
+);}
+
+# Definition des variables utilisee ci-dessus
+# -------------------------------------------
+
+// SPIP<=1.92
+add_variable( array(
+	'nom' => 'radio_desactive_cache3',
+	'format' => _format_NOMBRE,
+	'radio' => array(0 => 'couteauprive:cache_nornal', 1 => 'couteauprive:cache_sans'),
+	'defaut' => 0,
+	// si la variable est egale a 1, on code (jquery.js et forms_styles.css restent en cache)
+	'code:%s' => defined('_SPIP19300')?'':"\$cs_fond = isset(\$GLOBALS['fond'])?\$GLOBALS['fond']:_request('page');
+if (!in_array(\$cs_fond, array('jquery.js','forms_styles.css'))) \$_SERVER['REQUEST_METHOD']='POST';\n",
+));
+/*
+pour SPIP 2.0 :
+ define('_NO_CACHE',0); -> toujours prendre tous les fichiers en cache
+ define('_NO_CACHE',-1); -> ne jamais utiliser le cache ni meme creer les fichiers cache
+ define('_NO_CACHE',1); -> ne pas utiliser le fichier en cache, mais stocker le resultat du calcul dans le fichier cache
+ La fonction cache_valide() retourne :
+	'1' si il faut mettre le cache a jour, '0' si le cache est valide, '-1' s'il faut calculer sans stocker en cache
+*/
+add_variables( array(
+	'nom' => 'radio_desactive_cache4',
+	'format' => _format_NOMBRE,
+	'radio' => array(2 => 'couteauprive:cache_nornal', 0 => 'couteauprive:cache_permanent', -1 => 'couteauprive:cache_sans', 1 => 'couteauprive:cache_controle'),
+	'radio/ligne' => 2,
+	'defaut' => 2,
+	'code:%s!=2' => "define('_NO_CACHE',%s);\n",
+), array(
+	'nom' => 'duree_cache',
+	'format' => _format_NOMBRE,
+	'defaut' => "24", // 1 jour
+	'code' => "\$GLOBALS['delais']=%s*3600;\n",
+), array(
+	'nom' => 'duree_cache_mutu',
+	'format' => _format_NOMBRE,
+	'defaut' => "24", // 1 jour
+	'code' => "define('_DUREE_CACHE_DEFAUT', %s*3600);\n",
+), array(
+	'nom' => 'quota_cache',
+	'format' => _format_NOMBRE,
+	'defaut' => 10, // 10 Mo
+	'code' => "\$GLOBALS['quota_cache']=%s;\n",
+), array(
+	'nom' => 'derniere_modif_invalide',
+	'format' => _format_NOMBRE,
+	'radio' => array(0 => 'item_oui', 1 => 'item_non'),
+	'defaut' => 0,
+	'code' => "\$GLOBALS['derniere_modif_invalide']=%s;\n",
+));
+
+?>
