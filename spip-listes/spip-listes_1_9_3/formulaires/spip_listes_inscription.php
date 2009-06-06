b354f9138b03399bfd81a374e5b231a58be5b86c
booz@rezo.net <booz@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-06-06 22:12:37 +0000

    formulaire d'inscription simple en CVT #FOMULAIRE_SPIP_LISTES_INSCRIPTION
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@29038 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ spip-listes/spip-listes_1_9_3/formulaires/spip_listes_inscription.php
@@ -0,0 +1,79 @@
+<?php
+
+function formulaires_spip_listes_inscription_charger_dist(){
+	$valeurs = array('email'=>'');
+	
+	return $valeurs;
+}
+
+function formulaires_spip_listes_inscription_verifier_dist(){
+	$erreurs = array();
+	// verifier que les champs obligatoires sont bien la :
+	foreach(array('email') as $obligatoire)
+		if (!_request($obligatoire)) $erreurs[$obligatoire] = 'Ce champ est obligatoire';
+	
+	if(!in_array(_request('format_abo'),array('html','texte')))
+		$erreurs['format'] = "format inconnu";
+	
+	$listes = _request('listes') ;
+	if(is_array($listes))
+	 foreach($listes as $liste)
+		if(!intval($liste))
+			$erreurs['liste'] = "liste inconnue";
+
+
+	// verifier que si un email a été saisi, il est bien valide :
+	include_spip('inc/filtres');
+	if (_request('email') AND !email_valide(_request('email')))
+		$erreurs['email'] = 'Cet email n\'est pas valide';
+	
+	// Verifier si le mail est déjà connu
+	$email = _request('email') ;
+	if(email_valide(_request('email')))
+		if (sql_getfetsel("id_auteur","spip_auteurs","id_auteur !='".intval($id_auteur)."' AND email = '$email'")) {
+			$erreurs['email'] = 'Cet email et déja enregistré.';
+		}
+	
+
+	if (count($erreurs))
+		$erreurs['message_erreur'] = 'Votre saisie contient des erreurs !';
+		
+    return $erreurs; // si c'est vide, traiter sera appele, sinon le formulaire sera resoumis
+}
+
+
+function formulaires_spip_listes_inscription_traiter_dist(){
+	
+	// enregistre dans spip_auteurs, spip_auteurs_elargis, spip_auteurs_listes			
+			
+			$val['email'] = _request('email');
+			include_spip('inc/acces');
+			$alea_actuel = creer_uniqid();
+			$alea_futur = creer_uniqid();
+			$val['alea_actuel'] = $alea_actuel;
+			$val['alea_futur'] = $alea_futur;
+			$val['low_sec'] = '';
+			$val['statut'] = 'aconfirmer';
+
+			$format = _request('format_abo') ;
+			$listes = _request('listes');
+
+			$id_auteur = sql_insertq("spip_auteurs",$val);			
+			$ok = sql_insertq("spip_auteurs_elargis",array('id_auteur'=>$id_auteur,'spip_listes_format'=>$format));
+			
+			foreach($listes as $liste)
+				$ok = sql_insertq("spip_auteurs_listes",array('id_auteur'=>$id_auteur,'id_liste'=>$liste));
+			
+	// envoyer mail de confirmation
+	
+	$envoyer_mail = charger_fonction('envoyer_mail','inc');
+	$email_to = _request('email');
+	$email_from = _request('email');
+	$sujet = "Confirmation d'inscription";
+	$message = "coucou" ;
+	$envoyer_mail($email_to,$sujet,$message,$email_from);
+	return array('message_ok'=>'Votre demande a bien été prise en compte. Vous recevrez prochainement une confirmation.','editable' => false,);
+}
+
+
+?>
