d6076816a36083179ad454b2eea32b1420bd110b
maieulrouquette@sfr.fr <maieulrouquette@sfr.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-06-09 17:48:42 +0000

    Un peu de typo pour voir plus facilement les listes avec des élus
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@29081 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ scrutin_liste_proportionnel/scrut_prop_pip.php
@@ -0,0 +1,10 @@
+<?php
+
+function scrut_prop_insert_head($flux){
+    return $flux.'<link rel="stylesheet" href="'.find_in_path('scrut_prop.css').'" type="text/css" media="projection, screen, tv" />';
+    
+
+
+}
+
+?>
