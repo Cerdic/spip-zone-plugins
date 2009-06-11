485960635c2b6e164c7ba91ec17a208093085985
cyril@cym.fr <cyril@cym.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-06-11 17:33:39 +0000

    optimisations en vue de la suppression de l'erreur (table).tablesorter is not a function...
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@29124 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- tablesorter/inc-head.php
+++ tablesorter/inc-head.php
@@ -1,8 +1,9 @@
 
 
-<!-- insertion par le plugin tablesorter -->
+<!-- insertion dans le head par le plugin tablesorter pour SPIP -->
 
 <link type="text/css" rel="stylesheet" href="./plugins/auto/tablesorter/styles/tablesorter.css" media="projection, screen, tv" />
+<script type="text/javascript" src="./plugins/auto/tablesorter/scripts/jquery-1.2.6.js"></script>
 
 <script type="text/javascript" src="./plugins/auto/tablesorter/scripts/jquery.tablesorter.js"></script>
 
@@ -12,3 +13,4 @@
 			.tablesorter({widthFixed: true});
 	}); 
 </script>
+
