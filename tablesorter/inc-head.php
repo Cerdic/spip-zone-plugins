7a1d50010ac7d1c776f0ff4e82906469bb93cf4c
cyril@cym.fr <cyril@cym.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-06-11 16:53:17 +0000

    Premier dépot en version test.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@29122 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ tablesorter/inc-head.php
@@ -0,0 +1,14 @@
+
+
+<!-- insertion par le plugin tablesorter -->
+
+<link type="text/css" rel="stylesheet" href="./plugins/auto/tablesorter/styles/tablesorter.css" media="projection, screen, tv" />
+
+<script type="text/javascript" src="./plugins/auto/tablesorter/scripts/jquery.tablesorter.js"></script>
+
+<script type="text/javascript">
+	$(document).ready(function() { 
+		$("table")
+			.tablesorter({widthFixed: true});
+	}); 
+</script>
