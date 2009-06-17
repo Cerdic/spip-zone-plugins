63eb7f5ffa6af1634f5dd432c3fdcc9292c18415
cyril@cym.fr <cyril@cym.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-06-17 17:45:32 +0000

    Localisation du problème "tablesorter is not a function"; il s'agit de l'ordre dans lequel sont appelés les plugins; à régler.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@29230 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- tablesorter/inc-head.php
+++ tablesorter/inc-head.php
@@ -1,11 +1,10 @@
-
-
 <!-- insertion dans le head par le plugin tablesorter pour SPIP -->
 
 <link type="text/css" rel="stylesheet" href="./plugins/auto/tablesorter/styles/tablesorter.css" media="projection, screen, tv" />
-<script type="text/javascript" src="./plugins/auto/tablesorter/scripts/jquery-1.2.6.js"></script>
 
-<script type="text/javascript" src="./plugins/auto/tablesorter/scripts/jquery.tablesorter.js"></script>
+<script type="text/javascript" src="./plugins/auto/tablesorter/scripts/jquery-1.2.6.js" ></script>
+
+<script type="text/javascript" src="./plugins/auto/tablesorter/scripts/jquery.tablesorter.js" ></script>
 
 <script type="text/javascript">
 	$(document).ready(function() { 
@@ -14,3 +13,4 @@
 	}); 
 </script>
 
+<!-- fin de l'insertion par le plugin tablesorter pour SPIP -->
