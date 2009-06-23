43f6af200a358f653bf34abe9b5e36702bb84846
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-06-23 19:57:31 +0000

    langues (tjpzoom)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@29358 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ imagezoom/lang/tjpzoom_ro.php
@@ -0,0 +1,30 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// D
+	'descriptif' => 'Aceast&#259; pagin&#259; permite configurarea stilului lupei. Un stil personalizat poate fi declarat; vede&#355;i <a href="http://plugins.spip.net/?page=article&amp;id_article=48">documenta&#355;ia on-line</a>.',
+
+	// E
+	'example' => 'Rezultat grafic',
+
+	// N
+	'nostyle' => 'F&#259;r&#259; stil',
+
+	// O
+	'other' => 'Altul',
+
+	// S
+	'size_vignette' => 'M&#259;rimea maxim&#259; a vinietei',
+	'style' => 'Stilul lupei',
+
+	// T
+	'titre' => 'Configura&#355;ia lupei'
+);
+
+?>
