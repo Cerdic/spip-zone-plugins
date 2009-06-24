a26dac9f013f282385a22b6117be37969b713a50
nicolas@hoizey.com <nicolas@hoizey.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-06-24 07:10:15 +0000

    CVT, ça roxe ! (mais faut deviner certaines choses)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@29376 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ clevermail/2_0/formulaires/clevermail_list.php
@@ -0,0 +1,76 @@
+<?php
+function formulaires_clevermail_list_charger_dist($lst_id = -1) {
+	if ($lst_id == -1 || !$valeurs = sql_fetsel('*', 'spip_cm_lists', 'lst_id='.$lst_id)) {
+    $cm_mail_admin = sql_getfetsel('set_value', 'spip_cm_settings', 'set_name="CM_MAIL_ADMIN"');
+		$valeurs = array(
+			'lst_id' => -1,
+			'lst_name' => '',
+			'lst_comment' => '',
+			'lst_moderation' => 'closed',
+			'lst_moderator_email' => $cm_mail_admin,
+			'lst_subscribe_subject' => _T('clevermail:confirmation_votre_inscription'),
+			'lst_subscribe_text' => _T('clevermail:confirmation_votre_inscription_text'),
+			'lst_subject' => '',
+			'lst_unsubscribe_subject' => _T('clevermail:confirmation_votre_desinscription'),
+			'lst_unsubscribe_text' => _T('clevermail:confirmation_votre_desinscription_text'),
+			'lst_subject_tag' => 1,
+			'lst_url_html' => "http://",
+			'lst_url_text' => "http://"
+		);
+	}
+	return $valeurs;
+}
+
+function formulaires_clevermail_list_verifier_dist($lst_id = -1) {
+	$erreurs = array();
+	foreach(array('lst_name', 'lst_moderator_email', 'lst_url_html', 'lst_url_text') as $obligatoire) {
+		if (!_request($obligatoire)) {
+			$erreurs[$obligatoire] = 'Ce champ est obligatoire.';
+		}
+	}
+	$nb = sql_countsel("spip_cm_lists", "lst_id != "._request('lst_id')." AND lst_name = '"._request('lst_name')."'");
+  if ($nb > 0) {
+  	$erreurs['lst_name'] = _T('clevermail:lettre_meme_nom');
+  }
+	include_spip('inc/filtres');
+	if (_request('lst_moderator_email') && !email_valide(_request('lst_moderator_email'))) {
+		$erreurs['lst_moderator_email'] = 'Cette adresse e-mail n\'est pas valide.';
+	}
+	if (count($erreurs)) {
+		$erreurs['message_erreur'] = 'Veuillez corriger votre saisie.';
+	}
+	return $erreurs;
+}
+
+function formulaires_clevermail_list_traiter_dist($lst_id = -1) {
+  $champs = array(
+    'lst_name' => _request('lst_name'),
+    'lst_comment' => _request('lst_comment'),
+    'lst_moderation' => _request('lst_moderation'),
+    'lst_moderator_email' => _request('lst_moderator_email'),
+    'lst_subscribe_subject' => _request('lst_subscribe_subject'),
+    'lst_subscribe_text' => _request('lst_subscribe_text'),
+    'lst_subject' => _request('lst_subject'),
+    'lst_unsubscribe_subject' => _request('lst_unsubscribe_subject'),
+    'lst_unsubscribe_text' => _request('lst_unsubscribe_text'),
+    'lst_subject_tag' => _request('lst_subject_tag'),
+    'lst_url_html' => _request('lst_url_html'),
+    'lst_url_text' => _request('lst_url_text')
+  );
+
+  // Handle checkbox value
+  if (isset($list['lst_subject_tag']) && ($list['lst_subject_tag'] == 'on' || $list['lst_subject_tag'] == 1)) {
+    $champs['lst_subject_tag'] = 1;
+  }
+  
+  if (_request('lst_id') == -1) {
+    sql_insertq('spip_cm_lists', $champs);
+    spip_log('Nouvelle liste « '._request('lst_name').' »', 'clevermail');
+  } else {
+  	sql_updateq('spip_cm_lists', $champs, "lst_id = "._request('lst_id'));
+    spip_log('Modification de la liste « '._request('lst_name').' » (id = '._request('lst_id').')', 'clevermail');
+  }
+
+ 	return array('message_ok' => 'ok', 'redirect' => generer_url_ecrire('clevermail'));
+}
+?>
