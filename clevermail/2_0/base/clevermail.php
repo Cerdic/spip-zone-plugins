209f5c54343468f882458fe29a04866a1e46928b
nicolas@hoizey.com <nicolas@hoizey.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-06-26 07:42:27 +0000

    cosmetique
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@29412 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- clevermail/2_0/base/clevermail.php
+++ clevermail/2_0/base/clevermail.php
@@ -170,12 +170,12 @@ function clevermail_declarer_tables_principales($tables_principales) {
 
 function clevermail_upgrade($nom_meta_base_version, $version_cible) {
   $current_version = 0.0;
-  if (   (!isset($GLOBALS['meta'][$nom_meta_base_version]) )
+  if ((!isset($GLOBALS['meta'][$nom_meta_base_version]))
       || (($current_version = $GLOBALS['meta'][$nom_meta_base_version])!=$version_cible)){
     if (version_compare($current_version,'0.1','<')) {
       include_spip('base/abstract_sql');
       include_spip('base/create');
-      creer_base();      
+      creer_base();
       sql_insertq('spip_cm_settings',  array('set_name' => 'CM_MAIL_FROM', 'set_value' => $GLOBALS['meta']['email_webmaster']));
       sql_insertq('spip_cm_settings',  array('set_name' => 'CM_MAIL_ADMIN', 'set_value' => $GLOBALS['meta']['email_webmaster']));
       sql_insertq('spip_cm_settings',  array('set_name' => 'CM_MAIL_RETURN', 'set_value' => $GLOBALS['meta']['email_webmaster']));
