d8ed604046aa1227135fc38ae44cc0c305642309
nicolas@hoizey.com <nicolas@hoizey.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-06-30 21:26:11 +0000

    des / en trop
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@29526 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- clevermail/2_0/clevermail_pipelines.php
+++ clevermail/2_0/clevermail_pipelines.php
@@ -6,8 +6,8 @@ function clevermail_taches_generales_cron($taches_generales) {
 
 function clevermail_header_prive($flux) {
 	// On ajoute un CSS et un JS pour le back-office
-	$flux .= "<script src=\""._DIR_PLUGIN_CLEVERMAIL."/js/functions.js\" type=\"text/javascript\"></script>";
-	$flux .= "<link rel=\"stylesheet\" type=\"text/css\" href=\""._DIR_PLUGIN_CLEVERMAIL."/css/styles.css\" />";
+	$flux .= "<script src=\""._DIR_PLUGIN_CLEVERMAIL."js/functions.js\" type=\"text/javascript\"></script>";
+	$flux .= "<link rel=\"stylesheet\" type=\"text/css\" href=\""._DIR_PLUGIN_CLEVERMAIL."css/styles.css\" />";
 	return $flux;
 }
 ?>
