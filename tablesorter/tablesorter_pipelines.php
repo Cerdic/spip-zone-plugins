754a1beb7f4ab795f2e50625fb87b48108f472e3
cyril@cym.fr <cyril@cym.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-07-02 17:10:24 +0000

    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@29610 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- tablesorter/tablesorter_pipelines.php
+++ tablesorter/tablesorter_pipelines.php
@@ -17,4 +17,20 @@ function tablesorter_insert_head($flux){
 	return $flux;
 }
 
+function tablesorter_header_prive($flux){
+	// Insertion des librairies js
+	$flux .='<script src="'.url_absolue(find_in_path('scripts/jquery.tablesorter.js')).'" type="text/javascript"></script>';
+	// Inclusion des styles du plugin
+	$flux .='<link rel="stylesheet" href="'.url_absolue(find_in_path('styles/tablesorter.css')).'" type="text/css" />';
+	// Init de tablesorter
+	$flux .='
+	<script type="text/javascript">/* <![CDATA[ */
+	(function($){
+		$(function(){
+			$("table.spip").tablesorter();
+		});
+	})(jQuery);
+	/* ]]> */</script>';
+	return $flux;
+}
 ?>
