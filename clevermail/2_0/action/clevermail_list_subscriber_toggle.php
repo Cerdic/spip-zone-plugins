bcf2d4136c10a13cc0a789b1fcbb908b1f059000
nicolas@hoizey.com <nicolas@hoizey.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-07-02 16:21:01 +0000

    trop de quotes tue les quotes (plus besoin des miennes, sql_quote suffit)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@29603 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- clevermail/2_0/action/clevermail_list_subscriber_toggle.php
+++ clevermail/2_0/action/clevermail_list_subscriber_toggle.php
@@ -3,12 +3,12 @@ function action_clevermail_list_subscriber_toggle_dist() {
 	$securiser_action = charger_fonction('securiser_action', 'inc');
   $arg = $securiser_action();
   $lsr_id = sql_quote($arg);
-  $mode = 1 - sql_getfetsel("lsr_mode", "spip_cm_lists_subscribers", "lsr_id='".$lsr_id."'");
+  $mode = 1 - sql_getfetsel("lsr_mode", "spip_cm_lists_subscribers", "lsr_id=".$lsr_id);
   
-  if (sql_countsel("spip_cm_lists_subscribers", "lsr_id='".$lsr_id."'") == 1) {
+  if (sql_countsel("spip_cm_lists_subscribers", "lsr_id=".$lsr_id) == 1) {
 	  include_spip('inc/autoriser');
 	  if (autoriser('toggle','cm_list_subscriber',$pst_id)) {
-	    sql_updateq("spip_cm_lists_subscribers", array('lsr_mode' => $mode), "lsr_id='".$lsr_id."'");
+	    sql_updateq("spip_cm_lists_subscribers", array('lsr_mode' => $mode), "lsr_id=".$lsr_id);
 	    spip_log('Changement de mode de l\'abonnement de « '.$abonne.' » à la liste « '.$liste.' » (id='.$abonnement['lst_id'].')', 'clevermail');
 	  }
   }
