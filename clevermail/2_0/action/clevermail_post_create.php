42df3be0f03e593e6a600e4f84362b8527ecb283
nicolas@hoizey.com <nicolas@hoizey.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-07-03 12:08:38 +0000

    simplification
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@29628 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- clevermail/2_0/action/clevermail_post_create.php
+++ clevermail/2_0/action/clevermail_post_create.php
@@ -9,16 +9,15 @@ function action_clevermail_post_create_dist() {
   	$list = sql_fetsel("*", "spip_cm_lists", "lst_id = ".intval($lst_id));
   	$post = array('lst_id' => intval($lst_id), 'pst_date_create' => time());
   	include_spip('inc/distant');
-  	$html = recuperer_page($list['lst_url_html']);
-  	if (eregi("<title>(.*)</title>", $html, $regs)) {
-      $post['pst_subject'] = trim($regs[1]);
-  	} else {
-  		$post['pst_subject'] = 'Aucun sujet';
-  	}
-  	$post['pst_html'] = $html;
+  	$post['pst_html'] = recuperer_page($list['lst_url_html']);
     $post['pst_text'] = recuperer_page($list['lst_url_text']);
+    if (eregi("<title>(.*)</title>", $post['pst_html'], $regs)) {
+      $post['pst_subject'] = trim($regs[1]);
+    } else {
+      $post['pst_subject'] = 'Aucun sujet';
+    }
     sql_insertq("spip_cm_posts", $post);
-    spip_log('Création du message « '.$post['pst_subject'].' » dans la liste « '.sql_getfetsel("lst_name", "spip_cm_lists", "lst_id=".$lst_id).' » (id = '.$lst_id.')', 'clevermail');
+    spip_log('Création du message « '.$post['pst_subject'].' » dans la liste « '.$list['lst_name'].' » (id = '.$lst_id.')', 'clevermail');
   }
 }
 ?>
