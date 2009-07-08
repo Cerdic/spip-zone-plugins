f20ed8ee4cfb7ec5eda237f29ceebafdf6d04fcc
vincent@ldd.fr <vincent@ldd.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-07-08 20:09:41 +0000

    chaine de langue supplémentaire pour les "related tags"
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@29776 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- etiquettes/lang/etiquettes_fr.php
+++ etiquettes/lang/etiquettes_fr.php
@@ -8,6 +8,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 'etiquettes' => '&Eacute;tiquettes',
 'etiquettes_populaires' => '&Eacute;tiquettes populaires',
 'toutes_les_etiquettes' => 'Toutes les &eacute;tiquettes',
+'etiquettes_liees' => '&Eacute;tiquettes li&eacute;es',
 'explication' => '&Eacute;tiquettes s&eacute;par&eacute;es par des espaces : <em>machin chose "plusieurs mots ensemble" truc "autre chose"</em>.',
 
 'modifier_etiquettes' => 'Modifier les &eacute;tiquettes',
