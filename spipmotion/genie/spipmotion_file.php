b4891bc205026a937b98db057461af5f2b78b67c
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-07-08 01:05:15 +0000

    Gestion de la file d'attente
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@29746 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spipmotion/genie/spipmotion_file.php
+++ spipmotion/genie/spipmotion_file.php
@@ -1,10 +1,28 @@
 <?php
+/**
+* Plugin SPIPmotion
+* par kent1 (http://kent1.sklunk.net)
+* 
+* Copyright (c) 2007-2009
+* Logiciel libre distribué sous licence GNU/GPL.
+*  
+**/
+
+/**
+ * Fonction appelée par le génie de SPIP à intervalle régulier
+ * 
+ * Consulte la file d'attente pour savoir si des videos sont à encoder.
+ * S'il existe au moins une vidéo à encoder on lance la première et ainsi de suite
+ * 
+ * @return 
+ * @param object $time
+ */
 function genie_spipmotion_file($time)  {
 	$nb_videos = sql_countsel('spip_spipmotion_attentes', "encode='non'");
 	spip_log("Il y a $nb_videos vidéo(s) à encoder","spipmotion");
 	if($nb_videos>0){
 		$encoder = charger_fonction('encodage','inc');
-		$video_attente = sql_fetsel("id_spipmotion_attente,id_document","spip_spipmotion_attentes","encode='non'","","maj","1");
+		$video_attente = sql_fetsel("*","spip_spipmotion_attentes","encode='non'","","maj DESC","1");
 		$id_document = $video_attente['id_document'];
 		$id_video_attente = $video_attente['id_spipmotion_attente'];
 		$document = sql_fetsel('*','spip_documents','id_document='.sql_quote($id_document));
