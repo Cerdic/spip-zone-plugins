46ce2ba85cb1f96a1c69c187b9a60fa7d6faa5d8
webmaster@geomaticien.com <webmaster@geomaticien.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-07-09 19:47:57 +0000

    Cat - Video : suppression de la video de demo, qui est remplacée par une video du site, au hasard.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@29796 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- acs/models/cat/composants/video/ecrire/lang/video_ecrire_en.php
+++ acs/models/cat/composants/video/ecrire/lang/video_ecrire_en.php
@@ -6,7 +6,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 'nom' => 'Video',
 'description' => 'Video flv player.',
 'info' => 'To embed the player in an article, add filter player to tag &lt;doc123&gt; of a flv document. Examples: &lt;doc8|player&gt; ou &lt;doc123|player|autostart=yes&gt;',
-'help' => 'Use Flash player.',
+'help' => 'Preview display nothing since any video is uploaded on the site. Use Flash player.',
 
 'mp3bgcolor' => 'Background',
 'mp3Boutons' => 'Buttons',
--- acs/models/cat/composants/video/ecrire/lang/video_ecrire_fr.php
+++ acs/models/cat/composants/video/ecrire/lang/video_ecrire_fr.php
@@ -6,7 +6,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 'nom' => 'Video',
 'description' => 'Lecteur de documents flv ins&eacute;rable dans les articles.',
 'info' => 'Dans un article, ajouter le filtre player &agrave; la balise &lt;doc123&gt; d\'un document mp3 ou flv suffit à ins&eacute;rer le lecteur. Exemples: &lt;doc8|player&gt; ou &lt;doc123|player|autostart=yes&gt;',
-'help' => 'Utilise le lecteur Flash.',
+'help' => 'La pr&eacute;visualisation n\'affiche rien avant que le site ne contienne au moins une vid&eacute;o. Utilise le lecteur Flash.',
 
 'mp3bgcolor' => 'Fond',
 'mp3Boutons' => 'Boutons',
