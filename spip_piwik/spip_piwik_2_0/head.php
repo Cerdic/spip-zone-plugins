1767c5de72b2baeb7baf7487ce7294d952b176d0
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-07-17 01:28:12 +0000

    Premier commit qui casse un peu la compat avec l'ancienne version ...
    On passe la configuration en #FORMULAIRE_CONFIG_
    On prépare la possibilité d'avoir des statistiques du privé et d'enlever des statistiques certains statuts
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@29932 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spip_piwik/spip_piwik_2_0/head.php
+++ spip_piwik/spip_piwik_2_0/head.php
@@ -1,25 +1,62 @@
 <?php
 
-
+/**
+ * Insertion dans le pipeline insert_head
+ * 
+ * @param object $flux
+ * @return 
+ */
 function piwik_insert_head($flux){
-	$id_piwik=lire_config('piwik/idPiwik');
-	$url_piwik=lire_config('piwik/UrlPiwik');
+	$id_piwik=lire_config('piwik/idpiwik');
+	$url_piwik=lire_config('piwik/urlpiwik');
 	
 	if($url_piwik && $id_piwik){
+		if(lire_config('piwik/restreindre_statut')){
+			$statut = $GLOBALS['visiteur_session']['statut'];
+			if(in_array($statut,lire_config('piwik/restreindre_statut')));
+		}
 		$flux .= '
 		<script type="text/javascript">
-		var pkBaseURL = (("https:" == document.location.protocol) ? "https://'.$url_piwik.'" : "http://'.$url_piwik.'"); document.write(unescape("%3Cscript src=\'" + pkBaseURL + "piwik.js\' type=\'text/javascript\'%3E%3C/script%3E"));
+		var pkBaseURL = (("https:" == document.location.protocol) ? "https://'.$url_piwik.'/" : "http://'.$url_piwik.'/");
+			document.write(unescape("%3Cscript src=\'" + pkBaseURL + "piwik.js\' type=\'text/javascript\'%3E%3C/script%3E"));
 		</script>
 		<script type="text/javascript">
-		<!--
-		piwik_action_name = \'\';
-		piwik_idsite = '.$id_piwik.';
-		piwik_url = pkBaseURL + "piwik.php";
-		piwik_log(piwik_action_name, piwik_idsite, piwik_url);
-		//-->
+		try {
+		var piwikTracker = Piwik.getTracker(pkBaseURL + "piwik.php", '.$id_piwik.');
+		piwikTracker.trackPageView();
+		piwikTracker.enableLinkTracking();
+		} catch( err ) {}
 		</script>';
 	}		
 	return $flux;
 }
 
+/**
+ * Insertion dans le pipeline header_prive
+ * 
+ * @param object $flux
+ * @return 
+ */
+function piwik_header_prive($flux){
+	if(lire_config('piwik/piwik_prive')){
+		$id_piwik=lire_config('piwik/idpiwik');
+		$url_piwik=lire_config('piwik/urlpiwik');
+		
+		if($url_piwik && $id_piwik){
+			$flux .= '
+			<script type="text/javascript">
+			var pkBaseURL = (("https:" == document.location.protocol) ? "https://'.$url_piwik.'/" : "http://'.$url_piwik.'/");
+				document.write(unescape("%3Cscript src=\'" + pkBaseURL + "piwik.js\' type=\'text/javascript\'%3E%3C/script%3E"));
+			</script>
+			<script type="text/javascript">
+			try {
+			var piwikTracker = Piwik.getTracker(pkBaseURL + "piwik.php", '.$id_piwik.');
+			piwikTracker.trackPageView();
+			piwikTracker.enableLinkTracking();
+			} catch( err ) {}
+			</script>';
+		}
+	}
+	return $flux;
+}
 ?>
