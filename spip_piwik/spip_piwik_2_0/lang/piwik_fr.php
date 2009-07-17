874db919b51173fc00fd9d9c0f1f2d35cfa80cf3
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-07-17 13:56:17 +0000

    On peut comptabiliser les pages du privé
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@29944 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spip_piwik/spip_piwik_2_0/lang/piwik_fr.php
+++ spip_piwik/spip_piwik_2_0/lang/piwik_fr.php
@@ -3,10 +3,14 @@
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-'textes_id_piwik' => 'Votre identifiant piwik',
-'textes_url_piwik' => 'Votre serveur piwik',
-'texte_votre_identifiant' => 'Votre identifiant',
-'retour_a_spip' => 'Retour &agrave; la page compl&egrave;te',
-'adresse_du_serveur_web' => 'url du serveur (https:// ou http://)',
+	'adresse_du_serveur_web' => 'url du serveur (https:// ou http://)',
+	
+	//L
+	'label_comptabiliser_prive' => 'Comptabiliser les visites de l\'espace priv&eacute;',
+	
+	'textes_id_piwik' => 'Votre identifiant piwik',
+	'textes_url_piwik' => 'Votre serveur piwik',
+	'texte_votre_identifiant' => 'Votre identifiant',
+	'retour_a_spip' => 'Retour &agrave; la page compl&egrave;te',
 );
 ?>
