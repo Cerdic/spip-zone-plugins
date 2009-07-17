1302bb90c3cff197db750eacb3a9e6a247e2d665
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-07-17 00:31:53 +0000

    On réutilise l'ancien code pour débuter notre branche
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@29928 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ spip_piwik/spip_piwik_2_0/head.php
@@ -0,0 +1,25 @@
+<?php
+
+
+function piwik_insert_head($flux){
+	$id_piwik=lire_config('piwik/idPiwik');
+	$url_piwik=lire_config('piwik/UrlPiwik');
+	
+	if($url_piwik && $id_piwik){
+		$flux .= '
+		<script type="text/javascript">
+		var pkBaseURL = (("https:" == document.location.protocol) ? "https://'.$url_piwik.'" : "http://'.$url_piwik.'"); document.write(unescape("%3Cscript src=\'" + pkBaseURL + "piwik.js\' type=\'text/javascript\'%3E%3C/script%3E"));
+		</script>
+		<script type="text/javascript">
+		<!--
+		piwik_action_name = \'\';
+		piwik_idsite = '.$id_piwik.';
+		piwik_url = pkBaseURL + "piwik.php";
+		piwik_log(piwik_action_name, piwik_idsite, piwik_url);
+		//-->
+		</script>';
+	}		
+	return $flux;
+}
+
+?>
