078fce281ff657d75da7c51bc30d203f33b6763b
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-07-20 14:41:34 +0000

    Suite aux retours dans le forum, mise à jour complète du plugin.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@30010 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- woopra/woopra_options.php
+++ woopra/woopra_options.php
@@ -1,17 +1,15 @@
 <?php
 
-// Recalculer le cache si la config du site change
-$GLOBALS['marqueur'] .= ":".md5($GLOBALS['meta']['woopra']); // Sur un conseil de Cedric : http://permalink.gmane.org/gmane.comp.web.spip.zone/6258
-
-function woopra_affichage_final($texte){
-	$wroopa_id = lire_config('woopra/woopra_id','');
-	if ($wroopa_id<>'') {
-		$code = "<script type=\"text/javascript\">
-	var woopra_id = '".$wroopa_id."';
-	</script>
-	<script src=\"http://static.woopra.com/js/woopra.js\" type=\"text/javascript\"></script>";
-		$texte=preg_replace(",(</body>),i","$code\n</body>",$texte);
-	}
-	return $texte;
+function woopra_insert_head($flux) {
+	$script_woopra = <<<EOF
+<!-- Woopra Code Start -->
+<script type="text/javascript">
+    var _wh = ((document.location.protocol=='https:') ? "https://sec1.woopra.com" : "http://static.woopra.com");
+    document.write(unescape("%3Cscript src='" + _wh + "/js/woopra.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<!-- Woopra Code End -->
+EOF;
+	return $flux.$script_woopra;
 }
+
 ?>
