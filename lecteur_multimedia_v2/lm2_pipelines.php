8408502900389297b76509c8d0a1d249985efe68
booz@rezo.net <booz@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-07-20 21:42:06 +0000

    retour des liens sonores - dites moi si ca gene
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@30029 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- lecteur_multimedia_v2/lm2_pipelines.php
+++ lecteur_multimedia_v2/lm2_pipelines.php
@@ -2,8 +2,10 @@
 function lm2_insert_head($flux){
 	$flux .= '<script type="text/javascript" src="'.find_in_path(_DIR_LIB_SM.'script/soundmanager2.js').'"></script>'."\n";
 	$flux .= '<script type="text/javascript" src="'.find_in_path('javascript/lm2_playlist_jquery.js').'"></script>'."\n";
+	$flux .= '<script type="text/javascript" src="'.find_in_path('javascript/lm2_inlineplayer.js').'"></script>'."\n";
 	$flux .= '<link rel="stylesheet" href="'.generer_url_public('lm2_player.css').'" type="text/css" media="all" />'."\n";
 	$flux .= '<script type="text/javascript">soundManager.debugMode = false;</script>';
 	return $flux;
 }
+
 ?>
