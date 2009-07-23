2a7f9e1ee68f6dece7e40d06686c931c27fdb4d6
zeraxp@gmail.com <zeraxp@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-07-23 14:00:41 +0000

    Ajout d'une balise #RUBRIQUE_PDF qui fonctionne exactement de la meme facon que la balise #ARTICLE_PDF
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@30134 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- article_pdf/article_pdf_mes_options.php
+++ article_pdf/article_pdf_mes_options.php
@@ -2,14 +2,14 @@
 function balise_ARTICLE_PDF_dist($p) {
 		if (!is_array($p->param))
 			$p->param=array();
-		
+
 		// Produire le premier argument {article_pdf}
 		$texte = new Texte;
 		$texte->type='texte';
 		$texte->texte='article_pdf';
 		$param = array(0=>NULL, 1=>array(0=>$texte));
 		array_unshift($p->param, $param);
-		
+
 		// Transformer les filtres en arguments
 		for ($i=1; $i<count($p->param); $i++) {
 			if ($p->param[$i][0]) {
@@ -23,7 +23,37 @@ function balise_ARTICLE_PDF_dist($p) {
 				$p->param[$i][0] = NULL;
 			}
 		}
-		
+
+		// Appeler la balise #MODELE{article_pdf}{arguments}
+		if (!function_exists($f = 'balise_modele'))
+			$f = 'balise_modele_dist';
+		return $f($p);
+	}
+function balise_RUBRIQUE_PDF_dist($p) {
+		if (!is_array($p->param))
+			$p->param=array();
+
+		// Produire le premier argument {article_pdf}
+		$texte = new Texte;
+		$texte->type='texte';
+		$texte->texte='rubrique_pdf';
+		$param = array(0=>NULL, 1=>array(0=>$texte));
+		array_unshift($p->param, $param);
+
+		// Transformer les filtres en arguments
+		for ($i=1; $i<count($p->param); $i++) {
+			if ($p->param[$i][0]) {
+				if (!strstr($p->param[$i][0], '='))
+					break;# on a rencontre un vrai filtre, c'est fini
+				$texte = new Texte;
+				$texte->type='texte';
+				$texte->texte=$p->param[$i][0];
+				$param = array(0=>$texte);
+				$p->param[$i][1] = $param;
+				$p->param[$i][0] = NULL;
+			}
+		}
+
 		// Appeler la balise #MODELE{article_pdf}{arguments}
 		if (!function_exists($f = 'balise_modele'))
 			$f = 'balise_modele_dist';
