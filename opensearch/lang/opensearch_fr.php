fb56daf87f17f9354c744c8705b23f2492b53c23
brunobergot@gmail.com <brunobergot@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-07-24 20:44:58 +0000

    une page CFG/CVT qui necessite le plugin saisies, un fichier de langue et du nettoyage
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@30184 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ opensearch/lang/opensearch_fr.php
@@ -0,0 +1,17 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	
+	//C
+	'cfg_boite_opensearch' => 'Configuration du plugin OpenSearch',
+	'cfg_descr_opensearch' => 'OpenSearch : Moteur de recherche personnalisé pour votre site.',
+	'cfg_inf_id_rubrique' => 'S&eacute;lectionnez la rubrique dans laquelle effectuer la recherche.',
+	'cfg_inf_page_recherche' => 'Indiquez le squelette &agrave; utiliser pour la recherche sur le site (exemple : recherche pour le squelette recherche.html)',
+	'cfg_lbl_id_rubrique' => 'Rubrique cible',
+	'cfg_lbl_page_recherche' => 'Squelette de recherche',
+	'cfg_option_tout' => 'Tout le site',
+	'cfg_titre_opensearch' => 'OpenSearch'
+);
+?>
