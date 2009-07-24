0e32f9c4f45793c463d63e1bb7665b29b04c524b
brunobergot@gmail.com <brunobergot@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-07-24 18:33:00 +0000

    un plugin opensearch pour proposer d'ajouter son site aux moteurs personnalisés des navigateurs qui supportent ce protocole
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@30177 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ opensearch/opensearch_pipelines.php
@@ -0,0 +1,10 @@
+<?php
+
+function opensearch_insert_head($flux){
+	$nom_site = textebrut(typo($GLOBALS['meta']['nom_site']));
+	$url_opensearch = url_absolue(generer_url_public('opensearch.xml'));
+	$flux .= '<link rel="search" type="application/opensearchdescription+xml" title="'.$nom_site.'" href="'.$url_opensearch.'" />';
+	return $flux;
+}
+
+?>
