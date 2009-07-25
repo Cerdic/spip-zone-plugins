1366b8ac2fbb6bac8393c0711f4c4a00fde41881
gilles.vincent@gmail.com <gilles.vincent@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-07-25 14:11:32 +0000

    Le filtre qui teste les caracteres 'exotiques' doit eliminer ceux qui sont saisis sous la forme &#1234;
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@30225 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- captcha2/captcha2_fonctions.php
+++ captcha2/captcha2_fonctions.php
@@ -5,8 +5,12 @@
 function captcha2_ratio_ascii($str) {
   if (strlen($str)==0) return 0;
   $str = strip_tags($str); // on supprime les tags (ex. code) qui faussent le ratio
+  // On supprime les caracteres accentues de type &#1234;
+  $pattern = '/&#(\d+);/i';
+  $replacement = '';
+  $string = preg_replace($pattern, $replacement, $str);
   $c = 0;
-  foreach (count_chars($str, 1) as $i => $val) {
+  foreach (count_chars($string, 1) as $i => $val) {
        if ($i<127)  $c += $val; 
   }
   $l = strlen($str);  
