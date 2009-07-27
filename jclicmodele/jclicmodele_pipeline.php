1fb2d45d54484936bc0a6323c2deb9fd90cd206e
olivier.gautier@ac-rouen.fr <olivier.gautier@ac-rouen.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-07-27 16:21:32 +0000

    Plugin proposant un modèle pour lire les activités Jclic
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@30269 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ jclicmodele/jclicmodele_pipeline.php
@@ -0,0 +1,10 @@
+<?php
+function jclicmodele_insert_head($flux){
+$flux .= "<script type='text/javascript' src='"._DIR_PLUGIN_JCLICMODELE."scripts/jclicplugin.js'></script>";
+return $flux;
+}
+function jclicmodele_header_prive($flux){
+$flux .= "<script type='text/javascript' src='"._DIR_PLUGIN_JCLICMODELE."scripts/jclicplugin.js'></script>";
+return $flux;
+}
+?>
