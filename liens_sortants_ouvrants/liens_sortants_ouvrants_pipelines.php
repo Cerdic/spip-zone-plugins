7a6cf07000bc892efbc53bbf7a4cfcb1ab57bf3c
romy@tetue.net <romy@tetue.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-07-29 11:16:16 +0000

    Signalons les liens externes par un picto consensuel (cf.: http://www.ergologique.com/conseils/conseils.php?id_tip=808 )
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@30314 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- liens_sortants_ouvrants/liens_sortants_ouvrants_pipelines.php
+++ liens_sortants_ouvrants/liens_sortants_ouvrants_pipelines.php
@@ -2,6 +2,7 @@
 
 function liens_sortants_ouvrants_insert_head($flux) {
 	$flux .= '<script  src="'._DIR_PLUGIN_LIENS_SORTANTS_OUVRANTS.'liens_sortants_ouvrants.js" type="text/javascript"></script>';
+	$flux .= '<link rel="stylesheet" href="'._DIR_PLUGIN_LIENS_SORTANTS_OUVRANTS.'liens_sortants_ouvrants.css" type="text/css" media="all" />';
 	return $flux;
 }
 
