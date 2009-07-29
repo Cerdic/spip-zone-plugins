509396b326cd25b4e18f8e0b04680c00436143ed
cy_altern@yahoo.fr <cy_altern@yahoo.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-07-29 23:51:32 +0000

    fusion avec le plugin gallery-embed :  - modification de la creation des auteurs  - gestion des imageBlock des modeles par la fonction call_imageblock() histoire d'assurer le coup si le module correspondant de Gallery n’est pas installe... Du coup ca merite le changement de version: passage a la 1.2.0
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@30333 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- gallery2/gallery_fonctions.php
+++ gallery2/gallery_fonctions.php
@@ -1,5 +1,4 @@
 <?php
-// MODIF-220709	: ajout de la fonction call_imageblock()
 // fonction pour récuperer le block HTML produit par gallery
 	function call_imageblock($params) {
 	// RQ:  getBlock n'existe qu'a partir de la version 1.1.9 du plugin "image block"
@@ -17,8 +16,8 @@
 		}
 		return $html;
 	}
-// Finde MODIF
 
+  
 // fonction pour lister les ss-reps de /squelettes du plugin et retourner un array
     function liste_ssreps() {
         $Treps = array();
@@ -32,31 +31,6 @@
         return $Treps;
     }
 
-// MODIF-220709	: suppression de la fonction trouveid()
-/*
-// fonction pour tester si un user SPIP est user de Gallery2
-    function trouveid($userName) {
-        global $gallery;
-
-        $query = 'SELECT [GalleryUser::id]
-                  FROM [GalleryUser]
-                  WHERE [GalleryUser::userName] = ?
-        ';
-
-        // Check to see if we have a collision //
-        list($ret, $results) =
-            $gallery->search($query, array($userName), array('limit' => array('count' => 1)));  
-
-        $result = $results->nextResult();
-        if ($result[0] > 0) 
-            $userspip_exist_in_gallery = array( true, $result[0] );  
-        else 
-           $userspip_exist_in_gallery = array( false, 0 );  
-       
-        return($userspip_exist_in_gallery);
-    }
-*/  
-// fin de MODIF
   
 // fonction d'initialisation de gallery
     function  gallery_init() {
@@ -68,9 +42,8 @@
         if ($cfg['chemin_spip'] != '/') $cfg['chemin_spip'] = '/'.trim(rtrim($cfg['chemin_spip'], '/'), '/').'/';
         $cfg['chemin_gallery'] = '/'.trim(rtrim($cfg['chemin_gallery'], '/'), '/').'/';
 
-//MODIF-220709 : voir autre proposition ci-dessous
-/*      // mauvaise bidouille pour gérer l'include du fichier embed.php de Gallery 
-      	// selon qu'on est dans le public ou le privé...
+      // mauvaise bidouille pour gérer l'include du fichier embed.php de Gallery 
+      // selon qu'on est dans le public ou le privé...
         $chem_inclure = trim($cfg['chemin_gallery'],'/').'/';
         $chem_inclure = _DIR_RACINE.$chem_inclure;
         if (file_exists($chem_inclure.'embed.php')) 
@@ -79,13 +52,6 @@
             include_once('../'.$chem_inclure.'embed.php');
         else 
             die(_T('gallery:fichier_embed_pas_trouve'));
-*/
-        $chem_inclure = '/../../../'.trim($cfg['chemin_gallery'],'/').'/';
-		//echo dirname(__FILE__).'/../../../'.$chem_inclure.'embed.php';
-		if (file_exists(dirname(__FILE__) .$chem_inclure.'embed.php'))
-		include_once(dirname(__FILE__) .$chem_inclure.'embed.php');
-		else die(_T('gallery:fichier_embed_pas_trouve'));
-// fin de MODIF
 		 
         $lang = $GLOBALS['auteur_session']['lang'] ;
         if( $lang =='' ) $lang = 'fr';
@@ -104,11 +70,6 @@
                                          'fullInit' => true,
                                          'loginRedirect' => $cfg['chemin_spip'],  
                                          'activeUserId' => $GLOBALS['auteur_session']['id_auteur'] ));
-		
-//MODIF-220709
-		//if ($ret) print 'GalleryEmbed::init failed, here is the error message: ' . $ret->getAsHtml();  
-// fin de MODIF
-
         return $ret;
     }
     
@@ -116,53 +77,32 @@
 // le nécessaire pour faire tourner gallery en mode "embed" dans son squelette SPIP
     function gallery2(){    
         $ret=gallery_init();
-
-//MODIF-220709 : nouvelle méthode de synchronistation SPIP/GALLERY sans utilisation de trouveid()
-		/*        $ret = GalleryEmbed::checkActiveUser( $GLOBALS['auteur_session']['id_auteur']);    
-        if($ret) {
-             $extUserId = $GLOBALS['auteur_session']['id_auteur'];
-//             $extUserId = $id_auteur;
-             $args = array('username' => $GLOBALS['auteur_session']['login']);
-//             $args = array('username' => $login_auteur);
-             $spipgallery = trouveid( $args['username']) ;
-             if(  $spipgallery[0] ) {
-                $ret = GalleryEmbed::addExternalIdMapEntry($extUserId, $spipgallery[1], 'GalleryUser') ;
-             } 
-             else {
-                $ret = GalleryEmbed::createUser($extUserId, $args) ;
-             }
-         }*/
-		 
-         /* Now you *could* do something with $g2moddata['themeData'] */
-        //$g2data = GalleryEmbed::handleRequest();
-        
-	if ($ret) {
-		 /* Error! */
-		 /* Did we get an error because the user doesn't exist in g2 yet? */	 
-		$ret2 =GalleryEmbed::isExternalIdMapped($GLOBALS['auteur_session']['id_auteur'],'GalleryUser');   
-		 if ($ret2 && $ret2->getErrorCode() & ERROR_MISSING_OBJECT) {
-			 /* The user does not exist in G2 yet. Create in now on-the-fly */		 
-				$extUserId = $GLOBALS['auteur_session']['id_auteur'];
-				$args = array('username' => $GLOBALS['auteur_session']['login']);
-				$ret = GalleryEmbed::createUser($extUserId, $args) ;															
-			 if ($ret) {
-			 /* An error during user creation. Not good, print an error or do whatever is appropriate * in your emApp when an error occurs */
-				 print "An error occurred during the on-the-fly user creation <br>";
-				 print $ret->getAsHtml();
-				 exit;
-			 }
-		 } else {
-			 /* The error we got wasn't due to a missing user, it was a real error */
-			 if ($ret2) {
-				 print "An error occurred while checking if a user already exists<br>";
-				 print $ret2->getAsHtml();
-			 }
-			 print "An error occurred while trying to initialize G2<br>";
-			 print $ret->getAsHtml();
-			 exit;
-		 }
+    	if ($ret) {
+    		 /* Error! */
+    		 /* Did we get an error because the user doesn't exist in g2 yet? */	 
+    		$ret2 =GalleryEmbed::isExternalIdMapped($GLOBALS['auteur_session']['id_auteur'],'GalleryUser');   
+    		 if ($ret2 && $ret2->getErrorCode() & ERROR_MISSING_OBJECT) {
+    			 /* The user does not exist in G2 yet. Create in now on-the-fly */		 
+    				$extUserId = $GLOBALS['auteur_session']['id_auteur'];
+    				$args = array('username' => $GLOBALS['auteur_session']['login']);
+    				$ret = GalleryEmbed::createUser($extUserId, $args) ;															
+    			 if ($ret) {
+    			 /* An error during user creation. Not good, print an error or do whatever is appropriate * in your emApp when an error occurs */
+    				 print "An error occurred during the on-the-fly user creation <br>";
+    				 print $ret->getAsHtml();
+    				 exit;
+    			 }
+    		 } else {
+    			 /* The error we got wasn't due to a missing user, it was a real error */
+    			 if ($ret2) {
+    				 print "An error occurred while checking if a user already exists<br>";
+    				 print $ret2->getAsHtml();
+    			 }
+    			 print "An error occurred while trying to initialize G2<br>";
+    			 print $ret->getAsHtml();
+    			 exit;
+    		 }
 	 }
-// fin de MODIF 
 	 
       // balancer le résultat de GalleryEmbed::handleRequest(); dans une GLOBALS pour pouvoir interroger par des filtres plus tard
         $GLOBALS['g2data'] = GalleryEmbed::handleRequest();
@@ -217,26 +157,14 @@
          $html = '';
        // si il existe une référence d'item on l'utilise pour envoyer une image unique
          if ($item_id != '' AND intval($item_id)!= 0){ 
-
-// MODIF-220709																		   
-/*             list($ret,$html,$head1) = GalleryEmbed::getBlock('imageblock', 'ImageBlock', array(
-                  'blocks' => 'specificItem',
-                  'show' => $show,
-                  'link' => $lien,
-                  'itemId' => intval($item_id),
-                  'maxSize' => $taille));
-            if ($ret) {
-                return "<blink><span style='color: red;'>"._T('gallery:erreur_insertion')." ".$item_id." ".$ret."</span></blink>";
-            }*/
-			$params= array(
-                  'blocks' => 'specificItem',
-                  'show' => $show,
-                  'link' => $lien,
-                  'itemId' => intval($item_id),
-                  'maxSize' => $taille);			
-			$html=call_imageblock($params);
+            $params= array(
+                          'blocks' => 'specificItem',
+                          'show' => $show,
+                          'link' => $lien,
+                          'itemId' => intval($item_id),
+                          'maxSize' => $taille);			
+            $html=call_imageblock($params);
         }
-// fin de MODIF 
 
       // si pas référence d'item mais un nbe de dernières photos, on envoie les X dernières
         elseif ($nb_dernier != '' AND intval($nb_dernier)!= 0) {
@@ -246,48 +174,25 @@
                 $ch_last .= $sep.$ch_type;
                 $sep = '|';
             }
-			
-//MODIF-220709 : appel de la fn call_imageblock()
-/*             list($ret,$html,$head1) = GalleryEmbed::getBlock('imageblock', 'ImageBlock', array(
-                  'blocks' => $ch_last,
-                  'show' => $show,
-                  'link' => $lien,
-                  'maxSize' => $taille));
-            if ($ret) {
-                return "<blink><span style='color: red;'>"._T('gallery:erreur_insertion')." ".$ret."</span></blink>";
-            }*/
-			$params=  array(
-                  'blocks' => $ch_last,
-                  'show' => $show,
-                  'link' => $lien,
-                  'maxSize' => $taille);			
-			$html=call_imageblock($params);
+            $params=  array(
+                          'blocks' => $ch_last,
+                          'show' => $show,
+                          'link' => $lien,
+                          'maxSize' => $taille);			
+            $html=call_imageblock($params);
         }
-// fin de MODIF 
 
       // si aucun paramètre on envoie une photo au hazard
         else {
             $ch_type =  ($type == 'album' ? 'randomAlbum' :  'randomImage');
-			
-//MODIF-220709 : appel de la fn call_imageblock()
-/*            list($ret,$html,$head1) = GalleryEmbed::getBlock('imageblock', 'ImageBlock', array(
-                  'blocks' => $ch_type,
-                  'show' => $show,
-                  'link' => $lien,
-                  'maxSize' => $taille));
-            if ($ret) {
-                return "<blink><span style='color: red;'>"._T('gallery:erreur_insertion')." ".$ret."</span></blink>";
-            }*/
-			$params= array(
-                  'blocks' => $ch_type,
-                  'show' => $show,
-                  'link' => $lien,
-                  'maxSize' => $taille);			
-			$html=call_imageblock($params);
+            $params= array(
+                          'blocks' => $ch_type,
+                          'show' => $show,
+                          'link' => $lien,
+                          'maxSize' => $taille);			
+            $html=call_imageblock($params);
         }
-// fin de MODIF 
 
-//echo '<br>brut= <br>'.$html.'<br>';    
       // retourner des blocs formatés comme les <docXX> de SPIP  
         // supprimer le <div class="block-imageblock-ImageBlock"> englobant les résultats
         $html = preg_replace('/^[\s ]*<div.*?class.*?block-imageblock-ImageBlock.*?>(.*)<\/div>[\s ]*$/is', '$1', $html);
@@ -348,24 +253,13 @@
              $taille = intval($cfg['gimg_taille']);
          else $taille = 640;
          
-//MODIF-220709 : appel de la fn call_imageblock()																	
-/*         list($ret,$html,$head1) = GalleryEmbed::getBlock('imageblock', 'ImageBlock', array(
-              'blocks' => 'specificItem',
-              'show' => 'none',
-              'link' => 'none',
-              'maxSize' => $taille,
-              'itemId' => intval($item_id)
-              ));
-        if ($ret) {
-            return "<blink><span style='color: red;'>"._T('gallery:erreur_insertion')." ".$item_id." ".$ret."</span></blink>"  };*/
-		$params=  array(
-              'blocks' => 'specificItem',
-              'show' => 'none',
-              'link' => 'none',
-              'maxSize' => $taille,
-              'itemId' => intval($item_id));			
-		$html=call_imageblock($params);   
-// fin de MODIF 
+        $params=  array(
+                  'blocks' => 'specificItem',
+                  'show' => 'none',
+                  'link' => 'none',
+                  'maxSize' => $taille,
+                  'itemId' => intval($item_id));			
+        $html=call_imageblock($params);   
         
       	// triturer le html de retour pour retourner la forme voulue
         // recup les atributs et construire la balise <img>
