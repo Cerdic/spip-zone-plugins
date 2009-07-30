529b5dc9e37993aecc8f40552730ef0a64ada8a5
cyril@cym.fr <cyril@cym.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-07-30 18:30:19 +0000

    Ajout de la structure des tables; bute toujours sur la liaison entre les blocs et les pages via la table spip_mine_blocs_relations.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@30345 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spip_mine/spip_mine_pages/spip_mine_pages_options.php
+++ spip_mine/spip_mine_pages/spip_mine_pages_options.php
@@ -20,4 +20,28 @@ global $table_date;
 $table_date['spip_mine_pages']='date_creation';
 
 
+
+global $tables_jointures;
+$tables_jointures['spip_mine_pages'][]= 'spip_mine_blocs_relations';
+$tables_jointures['spip_mine_blocs'][]= 'spip_mine_blocs_relations';
+
+
+global $exceptions_des_jointures;
+$exceptions_des_jointures['id_page'] = array('spip_mine_blocs', 'id_parent')
+// Attention ! le id_parent peut être un id_bloc si type parent = bloc
+
+/*
+$tables_auxiliaires['spip_mine_blocs_relations'] = array(
+	'field' => &$spip_mine_blocs_relations,
+	'key' => &$spip_mine_blocs_relations);
+
+
+$tables_jointures['spip_mine_blocs']['id_bloc']= 'spip_mine_blocs_relations';
+$tables_jointures['spip_mine_blocs']['id_parent']= 'spip_mine_blocs_relations';
+$tables_jointures['spip_mine_pages']['id_parent']= 'spip_mine_blocs_relations';
+$tables_jointures['spip_mine_pages']['id_section']= 'spip_mine_sections';
+*/
+
+// echo "<script language=\"javascript\">alert('Pouet !')</script>";
+
 ?>
