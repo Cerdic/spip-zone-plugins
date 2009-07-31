c742a421ad9080b1fd35f462e28ba8bbe9b0c87e
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-07-31 14:46:57 +0000

    init on load
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@30367 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- liens_sortants_ouvrants/liens_sortants_ouvrants.js
+++ liens_sortants_ouvrants/liens_sortants_ouvrants.js
@@ -1,16 +1,20 @@
-var site = '#URL_SITE_SPIP';
-
 function targetLinks() {
-var str;
-var where;
-where="_blank";
+	var str;
+	var where;
+	where="_blank";
 
-for(var i=0;i<=(document.links.length-1);i++) {
- str=document.links[i].href;
- if((str.search(site)==-1)&&((str.search('http://')!=-1)
- ||(str.search('www.')!=-1)
- ||(str.search('ftp://')!=-1))){
-  document.links[i].target=where;
- }
+	for(var i=0;i<=(document.links.length-1);i++) {
+	 str=document.links[i].href;
+	 if((str.search(liens_sortants_site)==-1)&&((str.search('http://')!=-1)
+	 ||(str.search('www.')!=-1)
+	 ||(str.search('ftp://')!=-1))){
+		document.links[i].target=where;
+	 }
+	}
 }
-}
+if (window.jQuery)
+	(function($){
+		if(typeof onAjaxLoad == "function") onAjaxLoad(targetLinks);
+		$('document').ready(targetLinks);
+	})(jQuery);
+
