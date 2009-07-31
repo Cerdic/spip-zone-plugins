014f4826a1eec210d89601218aaa04cfe8e60397
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-07-31 15:00:33 +0000

    en tout jquery, mais en ignorant les liens mals ecrits
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@30369 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- liens_sortants_ouvrants/liens_sortants_ouvrants.js
+++ liens_sortants_ouvrants/liens_sortants_ouvrants.js
@@ -1,16 +1,11 @@
 function targetLinks() {
-	var str;
 	var where;
 	where="_blank";
 
-	for(var i=0;i<=(document.links.length-1);i++) {
-	 str=document.links[i].href;
-	 if((str.search(liens_sortants_site)==-1)&&((str.search('http://')!=-1)
-	 ||(str.search('www.')!=-1)
-	 ||(str.search('ftp://')!=-1))){
-		document.links[i].target=where;
-	 }
-	}
+	$("a[href*='://']:not([href^="+liens_sortants_site+"])")
+	  .attr('target',where)
+		.attr('rel','external')
+		.addClass('external');
 }
 if (window.jQuery)
 	(function($){
