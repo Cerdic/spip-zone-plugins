cb3f6b0ca2aaec2b490d209f64340ef968f7cd65
fil@rezo.net <fil@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-08-03 16:49:06 +0000

    typo des dates
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@30463 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ typo_mois/typo_exposants_fonctions.php
@@ -0,0 +1,20 @@
+<?php
+
+function typo_mois($texte){
+	static $typo;
+	if(!$typo) {
+		$typo = array();
+		for ($m=1; $m<=12; $m++)
+			$typo[] = _T('date_mois_'.$m);
+		$pre1 = _T('date_jnum1');
+		$pre2 = _T('date_jnum2');
+		$pre3 = _T('date_jnum3');
+		$typo = ",([1-3]?[0-9]|$pre1|$pre2|$pre3) (".join('|', $typo).'),UimsS';
+		include_spip('inc/charsets');
+		$typo = unicode2charset(html2unicode($typo));
+	}
+
+	return preg_replace($typo, '\1&nbsp;\2', $texte);
+}
+
+?>
