0016c04d80697e3c1c906ed9ba31cd5315b09973
damazone72-spip@yahoo.fr <damazone72-spip@yahoo.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-08-04 07:37:55 +0000

    Traduction en néerlandais
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@30499 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ boutonstexte/lang/boutonstexte_nl.php
@@ -0,0 +1,15 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+'textes_icones' => 'Teksten van icoonvelden',
+'texte_seulement' => 'Alleen tekst',
+'retour_a_spip' => 'Terug naar de volledige pagina',
+'augmenter_police' => 'Lettertype vergroten',
+'diminuer_police' => 'Lettertype verkleinen',
+'selecteur' => 'Invoerveld van tekst te behandelen',
+
+'donnees_techniques' => 'Technnische gegevens niet te veranderen'
+);
+?>
