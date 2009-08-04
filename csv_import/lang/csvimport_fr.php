621aec597ad7b26eb2ab0c7ad5ce6a935a37145a
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-08-04 07:52:26 +0000

    langues (csvimport)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@30507 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- csv_import/lang/csvimport_fr.php
+++ csv_import/lang/csvimport_fr.php
@@ -1,18 +1,19 @@
 <?php
-
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/csv_import/lang/
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+
 	// A
 	'administrer_tables' => 'Administrer les tables',
 	'ajouter_donnees' => 'Ajouter les donn&eacute;es',
 	'ajouter_table' => 'Ajouter &agrave; la table',
 	'aucune_donnee' => 'Il n\'y a aucune donn&eacute;e dans le fichier.',
 	'aucune_table_declaree' => 'Aucune table n\'est d&eacute;clar&eacute;e pour l\'import CSV',
-	'avertissement_remplacement' => 'Cette op&eacute;ration va entra&icirc;ner la suppression de toutes les donn&eacute;es pr&eacute;sentes dans la table. Les donn&eacute;es du fichier seront ins&eacute;r&eacute;es comme ci-dessous :',
 	'avertissement_ajout' => 'Les donn&eacute;es du fichier CSV vont &ecirc;tre ajout&eacute;es &agrave; la table "@table@" comme illustr&eacute; ci-dessous.',
-	
+	'avertissement_remplacement' => 'Cette op&eacute;ration va entra&icirc;ner la suppression de toutes les donn&eacute;es pr&eacute;sentes dans la table. Les donn&eacute;es du fichier seront ins&eacute;r&eacute;es comme ci-dessous :',
+
 	// C
 	'caracteres_separation' => 'Caract&egrave;re de s&eacute;paration',
 	'champs_csv' => 'Champs du fichier CSV',
@@ -20,11 +21,11 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'correspondance_incomplete' => 'Correspondances CSV-Table incompl&egrave;tes',
 	'correspondance_indefinie' => 'Correspondances CSV-Table non d&eacute;finies',
 	'csvimport' => 'Import CSV',
-	
+
 	// D
 	'delimiteur_indefini' => 'D&eacute;limiteur non d&eacute;fini',
 	'description_table_introuvable' => 'Description de la table introuvable',
-	
+
 	// E
 	'erreurs_ajout_base' => 'Il y a eu @nb@ erreurs lors de l\'ajout dans la base.',
 	'export_classique' => 'CSV classique (,)',
@@ -33,41 +34,41 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'export_table' => 'Export de la table : @table@',
 	'export_tabulation' => 'CSV avec tabulations',
 	'exporter' => 'Exporter',
-	
+
 	// F
 	'fichier_absent' => 'Fichier absent',
 	'fichier_choisir' => 'Fichier CSV &agrave; importer',
 	'fichier_vide' => 'Fichier vide',
-	
+
 	// I
 	'import_csv' => 'Import CSV : @table@',
 	'import_export_tables' => 'Import / Export dans les tables',
-	
+
 	// L
 	'ligne_entete' => '1<sup>&egrave;re</sup> ligne d\'en-t&ecirc;te',
+	'lignes_table' => 'Il y a @nb_resultats@ lignes dans la table "@table@".',
 	'lignes_totales' => '@nb@ lignes au total.',
 	'lignes_totales_csv' => '@nb@ lignes au total dans le fichier CSV.',
-	'lignes_table' => 'Il y a @nb_resultats@ lignes dans la table "@table@".',
-	
+
 	// N
-	'nb_enregistrements' => "@nb@ enregistrements",
-	
+	'nb_enregistrements' => '@nb@ enregistrements',
+
 	// P
 	'pas_importer' => 'Ne pas importer',
 	'premieres_lignes' => '@nb@ premi&egrave;res lignes du fichier.',
 	'probleme_chargement_fichier' => 'Probl&egrave;me lors du chargement du fichier',
 	'probleme_chargement_fichier_erreur' => 'Probl&egrave;me lors du chargement du fichier (erreur @erreur@).',
 	'probleme_inextricable' => 'Probl&egrave;me inextricable...',
-	
+
 	// R
 	'remplacer_toute_table' => 'Remplacer toute la table',
-	
+
 	// T
+	'table_vide' => 'La table "@table@" est vide.',
 	'tables_declarees' => 'Tables d&eacute;clar&eacute;es',
 	'tables_presentes' => 'Tables pr&eacute;sentes dans la base',
-	'table_vide' => 'La table "@table@" est vide.',
 	'tout_remplacer' => 'Tout remplacer',
-	
+
 	// Z
 	'z' => 'zzz'
 );
