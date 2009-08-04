84e92d16049743373f37873a1e05e22df726ae87
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-08-04 07:52:24 +0000

    langues (noie)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@30506 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- noie/lang/noie_ca.php
+++ noie/lang/noie_ca.php
@@ -8,10 +8,8 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// I
-	'ie6msg_info' => '<MODIF>Per navegar de la manera m&eacute;s satisfact&ograve;ria per aquest lloc (i per la resta de la Web), us recomanem que actualitzeu el vostre navegador.
-La&nbsp;versi&oacute; actual &eacute;s <a href="http://www.microsoft.com/france/windows/downloads/ie/getitnow.mspx">Internet Explorer&nbsp;7</a> i <a href="http://www.microsoft.com/windows/Internet-explorer/beta/">Internet Explorer&nbsp;8</a> est&agrave; disponible en versi&oacute; beta. L\'actualitzaci&oacute; &eacute;s gratu&iuml;ta.
-<br />Si&nbsp;utilitzeu un PC al vostre treball, contacteu amb el vostre servei inform&agrave;tic.
-<br />Si&nbsp;ho desitgeu, podeu tamb&eacute; provar altres navegadors web populars com per exemple <a href="http://www.mozilla-europe.org/fr/firefox/">Firefox</a>, <a href="http://www.opera.com">Opera</a> o <a href="http://www.apple.com/fr/safari/download/">Safari</a>.',
+	'ie6msg_info' => 'Per navegar de la manera m&eacute;s satisfact&ograve;ria per aquest lloc (i per la resta de la Web), us recomanem que actualitzeu el vostre navegador. Actualitzeu-lo gratu&iuml;tament a <a href="http://www.microsoft.com/spain/windows/internet-explorer/worldwide-sites.aspx</a>. Tamb&eacute; podeu provar altres navegadors populars com per exemple <a href="http://www.mozilla-europe.org/ca/">Firefox</a>, <a href="http://www.opera.com/">&Ograve;pera</a> o <a href="http://www.apple.com/es/safari/">Safari</a>.
+<br />Si&nbsp;utilitzeu un PC al vostre treball, contacteu amb el vostre servei inform&agrave;tic.',
 	'ie6msg_titre' => 'Sabeu que el vostre navegador &eacute;s obsolet?',
 
 	// T
