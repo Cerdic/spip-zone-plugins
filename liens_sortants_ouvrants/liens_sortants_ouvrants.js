f4881f14f47c749f99efcd2ee4d1324742f0c7cd
romy@tetue.net <romy@tetue.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-08-04 09:18:00 +0000

    Completer le title en signalant l'ouverture de fenêtre
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@30515 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- liens_sortants_ouvrants/liens_sortants_ouvrants.js
+++ liens_sortants_ouvrants/liens_sortants_ouvrants.js
@@ -5,11 +5,15 @@ function targetLinks() {
 	$("a[href*='://']:not([href^="+liens_sortants_site+"])")
 	  .attr('target',where)
 		.attr('rel','external')
-		.addClass('external');
+		.addClass('external')
+		.each(function(){
+			title =  "(nouvelle fenetre)";
+			if($(this).attr("title")) title = $(this).attr("title") + " (nouvelle fenetre)";
+			$(this).attr("title",title);
+		});
 }
 if (window.jQuery)
 	(function($){
 		if(typeof onAjaxLoad == "function") onAjaxLoad(targetLinks);
 		$('document').ready(targetLinks);
 	})(jQuery);
-
