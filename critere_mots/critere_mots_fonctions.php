32268886d4327be00d685468db939fb25ffd75b4
maieulrouquette@sfr.fr <maieulrouquette@sfr.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-08-04 08:44:08 +0000

    trois syntaxe {mots 66%} -> 66% de mots communs {mots 0.66} -> 66% de mots communs {mot 2}	-> 2 mots en communs
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@30514 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- critere_mots/critere_mots_fonctions.php
+++ critere_mots/critere_mots_fonctions.php
@@ -4,12 +4,14 @@
 // {mots?} ne s'applique que si au moins un mot est demande
 // on passe dans l'url &mots[]=titre1&mots[]=titre2
 // et/ou &mots[]=11 etc
-// parametre optionnel : {mots score%} ou score est un nombre entre 0 et 1
-// qui indique le pourcentage de mots a valider (ou entre 0 et 100)
-//ou encore {mots score} qui indique le nombre de mots communs (par exemple 2)
-// ex: {mots? 0.66%} selectionne tous les articles qui ont au moins 2/3
+// parametre optionnel : {mots score} ou score est un nombre entre 0 et 1
+// qui indique le pourcentage de mots a valider
+// ex: {mots? 0.66} selectionne tous les articles qui ont au moins 2/3
+// ou encore {mots score} qui indique le nombre de mots communs (par exemple 2) si score >= 1
+// ex: {mots 2} indique qu'il doit y avoir 2 mots communs
+// ou encore {mots 66%} indique qu'on doit avoir 66% de mot en commun (identique à {mots 0.66} 
 // de mots en commun avec ceux demandes par le contexte (ou l'URL)
-// par defaut score=1 (tous les mots demandes doivent figurer)
+// par defaut score=100% (tous les mots demandes doivent figurer)
 function critere_mots_dist($idb, &$boucles, $crit) {
 
 	$boucle = &$boucles[$idb];
@@ -72,13 +74,13 @@ function inc_prepare_mots_dist($mots, $table='articles', $cond=false, $score, $s
 	// sans regarder spip_mots ni les groupes
 	// (=> faire attention si on utilise les mots techniques)
     
-	// si on a un % dans le score, c'est que c'est une fraction ou %age
+	// si on a un % dans le score, c'est que c'est un %age
 	if (substr($score,-1)=='%'){
         
-	       $score = str_replace('%','',$score);
-	       if ($score>1){
-	           $score = $score/100;
-	       } // si exprime en %
+	   $score = str_replace('%','',$score);
+	   $having = ' HAVING SUM(1) >= '.ceil($score/100 * count($where)) ;
+	}
+	elseif ((0 < $score) and ($score < 1)){
 	   $having = ' HAVING SUM(1) >= '.ceil($score * count($where)) ;
 	}
 	else{
