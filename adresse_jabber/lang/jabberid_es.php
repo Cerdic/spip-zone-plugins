b96bc561580605a21f1e6768b6006e794f4e752c
da@weeno.net <da@weeno.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-08-08 20:11:05 +0000

    Nouveau plugin permettant de spécifier l'adresse Jabber d'un auteur.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@30733 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ adresse_jabber/lang/jabberid_es.php
@@ -0,0 +1,5 @@
+<?php
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	'adresse_jabber' => 'JID',
+	'adresse_jabber_precisions' => 'Tu direcci&oacute;n de Jabber (por ejemplo: Gmail) para la mensajer&iacute;a instant&aacute;nea.',
+);
---
+++ adresse_jabber/lang/jabberid_fr.php
@@ -0,0 +1,5 @@
+<?php
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	'adresse_jabber' => 'JID',
+	'adresse_jabber_precisions' => 'Votre adresse Jabber (exemple&nbsp;: Gmail) pour la messagerie instantan&eacute;e.',
+);
