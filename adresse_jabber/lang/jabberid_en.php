cd3ea210d9d330ef5f77f300f14b6dcc5a60a44a
da@weeno.net <da@weeno.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-08-10 20:00:47 +0000

    Gros contre-sens sur la traduction en anglais.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@30798 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- adresse_jabber/lang/jabberid_en.php
+++ adresse_jabber/lang/jabberid_en.php
@@ -1,5 +1,5 @@
 <?php
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'adresse_jabber' => 'JID',
-	'adresse_jabber_precisions' => 'Your Jabber address (i.e. Gmail) for instant messaging.',
+	'adresse_jabber_precisions' => 'Your Jabber address (e.g. on Gmail) for instant messaging.',
 );
