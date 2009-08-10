99ccf4f606305acdeafbee2532736b06fe6bd2f3
prigent.yohann@gmail.com <prigent.yohann@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-08-10 20:53:57 +0000

    Quelques corrections...
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@30801 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- savefg/action/restaurer_savefg.php
+++ savefg/action/restaurer_savefg.php
@@ -2,14 +2,11 @@
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 function action_restaurer_cfg() {
+	include_spip('inc/meta');
 	include_spip('inc/filtres');
 	$fond = _request('arg');
 	$sfg = sql_getfetsel('valeur', 'spip_savefg', 'fond='.sql_quote($fond));
-	if (sql_countsel('spip_meta', 'nom='.sql_quote($fond)) == 0) {
-		sql_insertq('spip_meta', array('nom' => $fond, 'valeur' => $sfg))
-	}
-	else {
-		sql_updateq('spip_meta', array('valeur' => $sfg), 'nom='.sql_quote($fond));
-	}
+	ecrire_meta($fond, $sfg);
+	ecrire_metas();
 }
 ?>
---
+++ savefg/action/supprimer_savefg.php
@@ -0,0 +1,9 @@
+<?php
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+function action_supprimer_savefg() {
+	include_spip('inc/filtres');
+	$fond = _request('arg');
+	sql_delete('spip_savefg', 'fond='.sql_quote($fond));
+}
+?>
