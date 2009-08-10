479cfa75e48c3fd783cc41ae1867743492eb6915
prigent.yohann@gmail.com <prigent.yohann@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-08-10 21:53:57 +0000

    Petites corrections + utilisation de id_savefg + dépendance envers bonux
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@30805 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- savefg/action/restaurer_savefg.php
+++ savefg/action/restaurer_savefg.php
@@ -5,7 +5,7 @@ function action_restaurer_savefg() {
 	include_spip('inc/meta');
 	include_spip('inc/filtres');
 	$fond = _request('arg');
-	$sfg = sql_getfetsel('valeur', 'spip_savefg', 'fond='.sql_quote($fond));
+	$sfg = sql_getfetsel('valeur', 'spip_savefg', 'id_savefg='.sql_quote(_request('id_savefg')).' AND fond='.sql_quote($fond));
 	ecrire_meta($fond, $sfg);
 	ecrire_metas();
 }
--- savefg/action/supprimer_savefg.php
+++ savefg/action/supprimer_savefg.php
@@ -4,6 +4,6 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 function action_supprimer_savefg() {
 	include_spip('inc/filtres');
 	$fond = _request('arg');
-	sql_delete('spip_savefg', 'fond='.sql_quote($fond));
+	sql_delete('spip_savefg', 'id_savefg='.sql_quote(_request('id_savefg')).' AND fond='.sql_quote($fond));
 }
 ?>
