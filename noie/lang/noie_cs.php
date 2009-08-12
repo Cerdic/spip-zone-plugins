3da4acd2b2fb488f204c2373bd8d3f8cc64097bc
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-08-12 10:19:15 +0000

    langues (noie)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@30843 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ noie/lang/noie_cs.php
@@ -0,0 +1,22 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// I
+	'ie6msg_info' => 'Pro snadn&eacute; pou&#382;&iacute;v&aacute;n&iacute; tphoto webu v&aacute;m doporu&#269;ujeme aktulizovat v&aacute;&#353; prohl&iacute;&#382;e&#269; na nov&#283;j&#353;&iacute; verzi.Aktualizujte <a href="http://www.microsoft.com/windows/internet-explorer/default.aspx">Internet Explorer</a> nebo zkuste jin&eacute; popul&aacute;rn&iacute; prohl&iacute;&#382;e&#269;e jako nap&#345;&iacute;klad <a href="http://mozilla.com/en-US/firefox/firefox.html">FireFox</a>, <a href="http://www.opera.com">Opera</a> nebo <a href="http://www.apple.com/safari/">Safari</a>.
+<br />Pokud pracujete na PC v zam&#283;stn&aacute;n&iacute;, po&#382;&aacute;dejte sv&eacute;ho spr&aacute;vce IT.',
+	'ie6msg_titre' => 'Je v&aacute;m zn&aacute;mo, &#382;e v&aacute;&#353; prohl&iacute;&#382;e&#269; je zastaral&yacute;?',
+
+	// T
+	'toocool_alt' => 'P&#345;&iacute;li&#353; cool pro IE',
+	'toocool_info' => 'Web, kter&yacute; si chcete prohl&iacute;&#382;et, je ozna&#269;en "Too Cool for Internet Explorer".
+<br />Pros&iacute;m najd&#283;te si chvilku na vyzkou&#353;en&iacute; n&#283;kter&eacute;ho z obl&iacute;ben&yacute;ch prohl&iacute;&#382;e&#269;&#367;: <a href="http://www.w3junkies.com/toocool/index.php?language=en">http://www.w3junkies.com/toocool/</a>',
+	'toocool_titre' => 'P&#345;&iacute;li&#353; cool pro Internet Explorer'
+);
+
+?>
