63242b190613a0742983701f568cdb275e29f25d
da@weeno.net <da@weeno.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-08-16 05:30:23 +0000

    Prendre en compte la langue du contexte dans la balise #JABBER_ACTIVITY_ICON pour la balise "alt".
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@30941 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- omnipresence/balise/jabber_activity_icon.php
+++ omnipresence/balise/jabber_activity_icon.php
@@ -2,20 +2,20 @@
 include_spip('inc/omnipresence');
 
 function balise_JABBER_ACTIVITY_ICON($p) {
-	return calculer_balise_dynamique($p, 'JABBER_ACTIVITY_ICON', array(CHAMP_JID, CHAMP_SERVEUR_OMNIPRESENCE));
+	return calculer_balise_dynamique($p, 'JABBER_ACTIVITY_ICON', array(CHAMP_JID, CHAMP_SERVEUR_OMNIPRESENCE, 'lang'));
 }
 
 function balise_JABBER_ACTIVITY_ICON_stat($args, $filtres) {
 	return array(
-		isset($args[2]) ? $args[2] : $args[0],
+		isset($args[3]) ? $args[3] : $args[0],
 		$args[1],
+		$args[2],
 	);
 }
 
-function balise_JABBER_ACTIVITY_ICON_dyn($jid, $host) {
-	#TODO: Request value in the context's locale
+function balise_JABBER_ACTIVITY_ICON_dyn($jid, $host, $locale) {
 	include_spip('inc/filtres');
-	$status = demander_action('pep/activity/value.txt', $jid, $host);
+	$status = demander_action("pep/activity/value-$locale.txt", $jid, $host);
 	return inserer_attribut(demander_action('pep/activity/image.png'.$theme, $jid, $host), "alt", $status);
 }
 ?>
