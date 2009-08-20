374968c77b0bcb8a0707b80e6182da9810ff5e61
bennetteson@free.fr <bennetteson@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-08-20 17:06:29 +0000

    Suppression du '<br />' dans la chaine de langue qui apparaissait dans le label complémentaire du champ extra
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@31029 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- multidomaines/lang/multidomaines_fr.php
+++ multidomaines/lang/multidomaines_fr.php
@@ -2,7 +2,7 @@
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'titre_multidomaines' => 'Multidomaine',
 	'label_url' => 'Url du secteur',
-	'precisions_url' => "l'url doit être valide,ne contenir qu'un domaine et se terminer par un slash<br /> ex: https://sous-dmomaine.domaine.com/",
+	'precisions_url' => "l'url doit être valide,ne contenir qu'un domaine et se terminer par un slash. ex: https://sous-dmomaine.domaine.com/",
 	'label_squelette' => 'Dossier des squelettes',
 	'precisions_squelette' => 'ex : squelettes',
 	'label_editer_url' => 'Url par defaut',
