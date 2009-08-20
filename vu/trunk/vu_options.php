33657cb6aa3180effc2a078130706418059dad22
jo.chibois@gmail.com <jo.chibois@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-08-20 12:40:39 +0000

    L'interface privé pour le plugin est là. Qasi fonctionnelle, reste des broutilles à régler. Permet de s'affranchir du plugin TableData. Attention, le plugin bride actuellement les évenements et les publications
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@31025 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- vu/trunk/vu_options.php
+++ vu/trunk/vu_options.php
@@ -1,5 +1,14 @@
 <?php
 
+/* Definir une bonne fois pour touts les repertoires par defaut */
+	// le repertoire parent : semble poser probleme... et apres tout quelle utilite ?
+		//	$p=explode(basename(_DIR_PLUGINS)."/",str_replace('\\','/',realpath(dirname(__FILE__))));
+		//	define('_DIR_VU',(_DIR_PLUGINS.end($p)));
+
+	// le repertoire des images
+	define("_DIR_VU_IMG_PACK", _DIR_PLUGIN_VU."img_pack/");
+	// le repertoire prive
+	define("_DIR_VU_PRIVE", _DIR_PLUGIN_VU."prive/");
 
 
 
