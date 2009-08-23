49e944bba5704abb5fa9cd62c0c9b5bd3f41e010
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-08-23 08:46:37 +0000

    langues (thumbsites)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@31070 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ thumbsites/lang/thumbsites_ca.php
@@ -0,0 +1,46 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'bouton_effacer' => 'Esborrar',
+	'bouton_valider' => 'Validar',
+	'bulle_bouton_effacer' => 'Esborrar les entrades i tornar als valors inicials',
+	'bulle_bouton_valider' => 'Validar les dades',
+
+	// C
+	'cfg_descriptif' => 'Aquesta p&agrave;gina us permet configurar el plugin Thumbsites, i, en particular, escollir el servidor de vinyetes. Per saber-ne m&eacute;s, consulteu la <a href="http://www.spip-contrib.net/?article2584">documentaci&oacute; a contrib</a>',
+	'cfg_inf_girafa_description' => 'Les vinyetes generades seran com a molt de 160 x 120 p&iacute;xels (aquest valor es pot modificar al lloc de Girafa)',
+	'cfg_inf_girafa_parametres' => 'Introdu&iuml;u les vostres dades personals:',
+	'cfg_inf_girafa_presentation' => 'Per utilitzar aquest servei haureu d\'estar registrats al lloc <a href="http://www.girafa.com">Girafa</a>. Si voleu fer menys de 2000 captures per dia, hi ha una versi&oacute; gratu&iuml;ta.',
+	'cfg_inf_rotothumb_parametres' => 'Introdu&iuml;u les vostres dades personals:',
+	'cfg_inf_rotothumb_presentation' => 'Per utilitzar aquest servei heu de registrar-vos per&ograve; haureu de posar un enlla&ccedil; cap al lloc <a href="http://www.robothumb.com">Robothumb</a>. La pres&egrave;ncia d\'aquest vincle &eacute;s verificada peri&ograve;dicament per la seva cura. ',
+	'cfg_inf_thumbshots_de_description' => 'Les vinyetes generades seran de 120 x 90 p&iacute;xels.',
+	'cfg_inf_thumbshots_de_presentation' => '<a href="http://www.thumbshots.de">Thumbshots.de</a> proposa un servei equivalent a Thumbshots.com. Encara que els serveis siguin id&egrave;ntics, s&oacute;n 2 llocs ben diferents.',
+	'cfg_inf_thumbshots_description' => 'Les vinyetes generades seran de 120 x 90 p&iacute;xels.',
+	'cfg_inf_thumbshots_presentation' => '<a href="http://www.thumbshots.com">Thumbshots.com</a> proposa un servei gratu&iuml;t que no necessita identificaci&oacute;.',
+	'cfg_inf_websnapr_parametres' => 'Vulgueu introduir les vostres dades personals:',
+	'cfg_inf_websnapr_presentation' => 'Per utilitzar aquest servei heu d\'estar registrat al lloc <a href="http://www.websnapr.com">Websnapr</a>. La versi&oacute; gratu&iuml;ta permet processar unes 250.000 captures del lloc per mes, a un ritme de 80 captures per hora.',
+	'cfg_itm_serveur_girafa' => 'Girafa',
+	'cfg_itm_serveur_robothumb' => 'Robothumb',
+	'cfg_itm_serveur_thumbshots' => 'Thumbshots.com',
+	'cfg_itm_serveur_thumbshots_de' => 'Thumbshots.de',
+	'cfg_itm_serveur_websnapr' => 'Websnapr',
+	'cfg_itm_taille_vignette' => '@taille@ p&iacute;xels',
+	'cfg_lbl_choix_serveur' => 'Escolliu el servidor de vinyetes',
+	'cfg_lbl_cle' => 'La vostra clau',
+	'cfg_lbl_identifiant' => 'El vostre identificador ',
+	'cfg_lbl_signature' => 'La vostra signatura',
+	'cfg_lbl_taille_vignette' => 'Mida de les vinyetes ',
+	'cfg_lgd_choix_serveur' => 'Servidor',
+
+	// T
+	'titre_thumbshot_site' => 'VINYETA DEL LLOC'
+);
+
+?>
---
+++ thumbsites/lang/thumbsites_de.php
@@ -0,0 +1,46 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'bouton_effacer' => 'L&ouml;schen',
+	'bouton_valider' => 'Best&auml;tigen',
+	'bulle_bouton_effacer' => 'Eingaben l&ouml;schen und mit Standardwerten neu beginnen',
+	'bulle_bouton_valider' => 'Eingaben best&auml;tigen',
+
+	// C
+	'cfg_descriptif' => 'Auf dieser Seite k&ouml;nnen sie das Plugin Thumbsites konfigurieren und den Thumbnailserver einstellen. Weitere Informationen: <a href="http://www.spip-contrib.net/?article2584">Dokumentation auf Spip-Contrib</a>',
+	'cfg_inf_girafa_description' => 'Die Thumbnails sind maximal 160 x 120 Pixel gro&szlig; (Dieser Wert kann auf der Website Girafa modifiziert werden.)',
+	'cfg_inf_girafa_parametres' => 'Bitte geben sie ihre pers&ouml;nlichen Daten ein:',
+	'cfg_inf_girafa_presentation' => 'Um diesen Dienst zu nutzen, m&uuml;ssen sie eingetragener Nutzer der Website <a href="http://www.girafa.com">Girafa</a> sein. Es gibt eine kostenlose Version f&uuml;r bis zu 200 Screenshots am Tag.',
+	'cfg_inf_rotothumb_parametres' => 'Bitte geben sie ihre pers&ouml;nlichen Daten ein:',
+	'cfg_inf_rotothumb_presentation' => 'Um diesen Dienst zu nutzen, brauchen sie sich nicht anzumelden, abe sie m&uuml;ssen einen Link zur Site <a href="http://www.robothumb.com">Robothumb</a> setzen. Die Existenz dieses Links wird vom Betreiber &uuml;berpr&uuml;ft.',
+	'cfg_inf_thumbshots_de_description' => 'Die erzeugten Thumbnails haben 120 x 90 Pixels.',
+	'cfg_inf_thumbshots_de_presentation' => '<a href="http://www.thumbshots.de">Thumbshots.de</a> bietet einen Dienst, der Thumbshots.com entspricht. Auch wenn die Dienste identisch sind, handelt es sich doch um zwei Websites.',
+	'cfg_inf_thumbshots_description' => 'Die erzeugten Thumbnails haben 120 x 90 Pixel.',
+	'cfg_inf_thumbshots_presentation' => '<a href="http://www.thumbshots.com">Thumbshots.com</a> bietet einen kostenlosen Dienst ohne Anmeldung.',
+	'cfg_inf_websnapr_parametres' => 'Bitte geben sie ihre pers&ouml;nlichen Daten ein:',
+	'cfg_inf_websnapr_presentation' => 'Um diesen Dienst zu nutzen, m&uuml;ssen sie sich bei <a href="http://www.websnapr.com">Websnapr</a> anmelden. Die kostenlose Version erlaubt 250.000 Screenshots pro Monat und schafft bis zu 80 neue Screenshots pro Stunde.',
+	'cfg_itm_serveur_girafa' => 'Girafa',
+	'cfg_itm_serveur_robothumb' => 'Robothumb',
+	'cfg_itm_serveur_thumbshots' => 'Thumbshots.com',
+	'cfg_itm_serveur_thumbshots_de' => 'Thumbshots.de',
+	'cfg_itm_serveur_websnapr' => 'Websnapr',
+	'cfg_itm_taille_vignette' => '@taille@ pixels',
+	'cfg_lbl_choix_serveur' => 'Bitte w&auml;hlen sie den Thumbnail-Server',
+	'cfg_lbl_cle' => 'Ihr Schl&uuml;ssel',
+	'cfg_lbl_identifiant' => 'Ihre ID',
+	'cfg_lbl_signature' => 'Ihre Unterschrift',
+	'cfg_lbl_taille_vignette' => 'Gr&ouml;&szlig;e der Thumbnails',
+	'cfg_lgd_choix_serveur' => 'Server',
+
+	// T
+	'titre_thumbshot_site' => 'THUMBNAIL DER SITE'
+);
+
+?>
