e074d52d776d3f0b7411ddf640c1b9d805718bd6
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-08-23 08:46:38 +0000

    langues (rainette)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@31071 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ rainette/lang/rainette_ca.php
@@ -0,0 +1,142 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'coucher_soleil' => 'posta de sol',
+
+	// D
+	'demain' => 'dem&agrave;',
+	'derniere_maj' => 'Actualitzaci&oacute;',
+	'direction_E' => 'est',
+	'direction_ENE' => 'est nord-est',
+	'direction_ESE' => 'est sud-est',
+	'direction_N' => 'nord',
+	'direction_NE' => 'nord-est',
+	'direction_NNE' => 'nord nord-est',
+	'direction_NNW' => 'nord nord-oest',
+	'direction_NW' => 'nord-oest',
+	'direction_S' => 'sud',
+	'direction_SE' => 'sud-est',
+	'direction_SSE' => 'sud sud-est',
+	'direction_SSW' => 'sud sud-oest',
+	'direction_SW' => 'sud-oest',
+	'direction_W' => 'oest',
+	'direction_WNW' => 'oest nord-oest',
+	'direction_WSW' => 'oest sud-oest',
+
+	// H
+	'humidite' => 'humitat',
+
+	// J
+	'jour' => 'dia',
+
+	// L
+	'lever_soleil' => 'alba',
+
+	// M
+	'meteo' => 'temps',
+	'meteo_0' => '<MODIF>Pluja',
+	'meteo_1' => '<MODIF>Pluja',
+	'meteo_10' => '<MODIF>Pluja',
+	'meteo_11' => '<MODIF>Pluja',
+	'meteo_12' => '<MODIF>Pluja',
+	'meteo_13' => '<MODIF>Neu',
+	'meteo_14' => '<MODIF>Neu',
+	'meteo_15' => '<MODIF>Neu',
+	'meteo_16' => '<MODIF>Neu',
+	'meteo_17' => '<MODIF>Tempesta',
+	'meteo_18' => '<MODIF>Neu',
+	'meteo_19' => '<MODIF>Boira',
+	'meteo_2' => '<MODIF>Pluja',
+	'meteo_20' => '<MODIF>Boira',
+	'meteo_21' => '<MODIF>Boira',
+	'meteo_22' => '<MODIF>Boira',
+	'meteo_23' => '<MODIF>Vent',
+	'meteo_24' => '<MODIF>Vent',
+	'meteo_25' => '<MODIF>Vent',
+	'meteo_26' => '<MODIF>N&uacute;vols',
+	'meteo_27' => '<MODIF>Lluna plena i n&uacute;vols',
+	'meteo_28' => '<MODIF>Sol i n&uacute;vols',
+	'meteo_29' => '<MODIF>Lluna plena i n&uacute;vols dispersos ',
+	'meteo_3' => '<MODIF>Tempesta',
+	'meteo_30' => '<MODIF>Sol i n&uacute;vols dispersos',
+	'meteo_31' => '<MODIF>Lluna plena',
+	'meteo_32' => '<MODIF>Sol',
+	'meteo_33' => '<MODIF>Lluna plena i n&uacute;vols dispersos',
+	'meteo_34' => '<MODIF>Sol i n&uacute;vols dispersos',
+	'meteo_35' => '<MODIF>Tempesta',
+	'meteo_36' => '<MODIF>Sol',
+	'meteo_37' => '<MODIF>Tempesta',
+	'meteo_38' => '<MODIF>Tempesta',
+	'meteo_39' => '<MODIF>Pluja',
+	'meteo_4' => '<MODIF>Tempesta',
+	'meteo_40' => '<MODIF>Pluja',
+	'meteo_41' => '<MODIF>Neu',
+	'meteo_42' => '<MODIF>Neu',
+	'meteo_43' => '<MODIF>Neu',
+	'meteo_44' => '<MODIF>Sol i n&uacute;vols dispersos',
+	'meteo_45' => '<MODIF>Pluja',
+	'meteo_46' => '<MODIF>Neu',
+	'meteo_47' => '<MODIF>Tempesta',
+	'meteo_5' => '<MODIF>Pluja',
+	'meteo_6' => '<MODIF>Neu',
+	'meteo_7' => '<MODIF>Gel',
+	'meteo_8' => '<MODIF>Pluja',
+	'meteo_9' => '<MODIF>Pluja',
+	'meteo_conditions' => 'condicions actuals',
+	'meteo_na' => '<MODIF>Desconegut',
+	'meteo_previsions' => 'previsions del ',
+
+	// N
+	'nuit' => 'nit',
+
+	// P
+	'point_rosee' => 'punt de rosada',
+	'pression' => 'pressi&oacute; ',
+
+	// R
+	'risque_precipitation' => 'risc de precip.',
+
+	// S
+	'station_observation' => 'estaci&oacute;',
+
+	// T
+	'temperature_max' => '<MODIF>m&agrave;x.',
+	'temperature_min' => '<MODIF>m&iacute;n.',
+	'temperature_ressentie' => 'sensaci&oacute;',
+	'tendance_symbole_falling' => '&#8595;',
+	'tendance_symbole_rising' => '&#8593;',
+	'tendance_symbole_steady' => '&#8594;',
+	'tendance_texte_falling' => 'en descens',
+	'tendance_texte_rising' => 'cap amunt',
+	'tendance_texte_steady' => 'estable',
+
+	// U
+	'unite_angle_metrique' => '&deg;',
+	'unite_angle_standard' => '&deg;',
+	'unite_distance_metrique' => 'km',
+	'unite_distance_standard' => 'milles',
+	'unite_pourcentage_metrique' => '%',
+	'unite_pourcentage_standard' => '%',
+	'unite_precipitation_metrique' => 'mm',
+	'unite_precipitation_standard' => 'polzades',
+	'unite_pression_metrique' => 'mbar',
+	'unite_pression_standard' => 'polzades',
+	'unite_temperature_metrique' => '&deg;C',
+	'unite_temperature_standard' => '&deg;F',
+	'unite_vitesse_metrique' => 'km/h',
+	'unite_vitesse_standard' => 'mph',
+
+	// V
+	'valeur_indeterminee' => 'N/D',
+	'vent' => 'vent',
+	'visibilite' => 'visibilitat'
+);
+
+?>
