df13d79b948b1ceb194880715429c62c109af5f7
maieulrouquette@sfr.fr <maieulrouquette@sfr.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-08-24 12:14:54 +0000

    permettre que si on passe explicitement l'id cela annule l'exclusion par secteur->c'est configurable (à documenter)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@31090 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- exclure_secteur/exclurepip.php
+++ exclure_secteur/exclurepip.php
@@ -2,16 +2,17 @@
 include_spip('inc/exclure_utils');
 
 function exclure_sect_pre_boucle(&$boucle){
+    
     if ($boucle->modificateur['tout_voir'] or ($boucle->modificateur['tout'] and lire_config('secteur/tout') == 'oui') or test_espace_prive()==1){
         return $boucle;
     }
     $type = $boucle->id_table;
-    
+
     
     if ($type == 'articles' or $type == 'rubriques' or $type == 'syndic'){
     
         $crit = $boucle->criteres;
-        $exclut = exclure_sect_choisir($crit);       
+        $exclut = exclure_sect_choisir($crit,$type);       
         $boucle->where[] = array("'NOT IN'", "'$type.id_secteur'",sql_quote($exclut));    
          
     }
@@ -19,14 +20,14 @@ function exclure_sect_pre_boucle(&$boucle){
     if ($type == 'breves'){
     
         $crit = $boucle->criteres;
-        $exclut = exclure_sect_choisir($crit);       
+        $exclut = exclure_sect_choisir($crit,$type);       
         $boucle->where[] = array("'NOT IN'", "'$type.id_rubrique'",sql_quote($exclut));    
          
     }
     
     if ($type == 'forum'){
         $crit = $boucle->criteres;
-        $exclut = exclure_sect_choisir($crit);        
+        $exclut = exclure_sect_choisir($crit,$type);        
         global $table_prefix;
         $boucle->from['L1']=$table_prefix.'_articles';
         $boucle->where[] = array("'NOT IN'", "'L1.id_secteur'",sql_quote($exclut));  
--- exclure_secteur/inc/exclure_utils.php
+++ exclure_secteur/inc/exclure_utils.php
@@ -22,10 +22,46 @@ function secteur_explicite($crit){
     return false;
 }
 
-function exclure_sect_choisir($crit){
+function id_explicite($crit,$type){
+    //test de cfg
+    $cfg = lire_config('secteur/idexplicite');
+
+    
+    if ($cfg == false){
+        return false;
+    }
+    $id = 'id_'.substr($type,0,-1);
+    
+   
+    foreach($crit as $critere){
+        if ($critere->param[0][0]->texte == $id and $critere->not!='!') {
+                
+                switch ($critere->op){
+                    case '=' :
+                        return true;
+                    case '==':
+                        return true;
+                    case 'IN':
+                        return true;                
+                }                   
+        }
+        if ($critere->op == $id and $critere->not!='!'){
+            return true;
+        
+        }
+    }
+    return false;
+
+    
+    
+    //var_dump($crit);
+}
+
+function exclure_sect_choisir($crit,$type){
     $cfg =lire_config('secteur/exclure_sect');
     $sect_afficher = secteur_explicite($crit);
-    if ($cfg = array_map('sql_quote', $cfg) and !$sect_afficher ) {
+    $id_explicite = id_explicite($crit,$type); //l'id de la table sur laquelle on boucle est-il explicite ?
+    if ($cfg = array_map('sql_quote', $cfg) and !$sect_afficher and !$id_explicite) {
         $cfg = implode($cfg, ',');
     }
     else {
