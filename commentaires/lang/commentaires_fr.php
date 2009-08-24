6c7c139d38b3d9a1103cd82fb44608ef1bf05fc6
romy@tetue.net <romy@tetue.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-08-24 15:26:29 +0000

    Je me bats avec les chaines de langue, pfiou !
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@31093 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- commentaires/lang/commentaires_fr.php
+++ commentaires/lang/commentaires_fr.php
@@ -1,7 +1,6 @@
 <?php
-// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/_stable_/noie/lang/
-if (!defined("_ECRIRE_INC_VERSION")) return;
+
+// This is a SPIP-forum module file  --  Ceci est un fichier module de SPIP-forum
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
@@ -23,7 +22,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 'forum_votre_email' => 'Courriel (non&nbsp;publi&eacute;)',
 'forum_url' => 'Votre site web',
 
-'info_creation_paragraphe' => 'Ce formulaire accepte les raccourcis&nbsp;SPIP et le code&nbsp;HTML. Pour cr&eacute;er des paragraphes, laissez simplement des lignes vides.',
+'info_creation_paragraphe' => 'Ce formulaire accepte les <a href="http://www.spip.net/fr_article1578.html" rel="external nofollow" class="spip_out">raccourcis&nbsp;SPIP</a> et le code&nbsp;HTML. Pour cr&eacute;er des paragraphes, laissez simplement des lignes vides.',
 
 'forum_voir_avant' => 'Pr&eacute;visualiser',
 'forum_message_definitif' => 'Confirmer l\'envoi',
