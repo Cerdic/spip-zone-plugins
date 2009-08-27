8c56deef6be345d664cd80c171b8d9719a8f3f09
romy@tetue.net <romy@tetue.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-08-27 10:06:22 +0000

    Message d'info moins verbeux, sur les gravatars des commentaires
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@31141 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- gravatar/lang/gravatar_fr.php
+++ gravatar/lang/gravatar_fr.php
@@ -1,12 +1,11 @@
 <?php
+
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/_stable_/noie/lang/
-if (!defined("_ECRIRE_INC_VERSION")) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 'gravatar_info' => 'Pour afficher votre trombine ici, enregistrez-la d\'abord sur <a href="http://www.gravatar.com/" rel="external nofollow" class="spip_out">gravatar.com</a> (gratuit et indolore).',
-'gravatar_info_forum' => 'Pour afficher votre trombine avec votre message, enregistrez-la d\'abord sur <a href="http://www.gravatar.com/" rel="external nofollow" class="spip_out">gravatar.com</a> (gratuit et indolore). Postez ensuite votre message ici sans oublier d\'indiquer votre adresse e-mail (non publi&eacute;e).',
+'gravatar_info_forum' => 'Pour afficher votre trombine avec votre message, enregistrez-la d\'abord sur <a href="http://www.gravatar.com/" rel="external nofollow" class="spip_out">gravatar.com</a> (gratuit et indolore) et n\'oubliez pas d\'indiquer votre adresse e-mail ici.',
 'titre_gravatar_auteur' => 'GRAVATAR DE L\'AUTEUR',
 
 );
