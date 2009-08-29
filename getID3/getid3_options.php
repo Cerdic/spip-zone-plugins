ba68232733a4568f76a65d6e1301bd7652014e97
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-08-29 15:49:26 +0000

    Un bitrate c'est en kbps...
    C'est un plugin pour spip 2.0 donc on fait sauter la définition de _DIR_PLUGIN_GETID3
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@31197 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- getID3/getid3_options.php
+++ getID3/getid3_options.php
@@ -1,8 +1,5 @@
 <?php
 
-if (!defined('_DIR_PLUGIN_GETID3')){
-	$p=explode(basename(_DIR_PLUGINS)."/",str_replace('\\','/',realpath(dirname(__FILE__))));
-	define('_DIR_PLUGIN_GETID3',(_DIR_PLUGINS.end($p)).'/');
-}
+$table_des_traitements['BITRATE'][]= 'number_format(div(typo(%s),1000),0,""," ")';
 
 ?>
