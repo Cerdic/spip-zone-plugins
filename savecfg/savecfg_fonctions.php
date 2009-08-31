8947a1ec6ecceb5feca71d9008ef4ffd6727dd71
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-08-31 19:31:46 +0000

    Ne pas afficher les formulaire de restauration et d'export quand il n'y a rien de stocké.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@31240 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- savecfg/savecfg_fonctions.php
+++ savecfg/savecfg_fonctions.php
@@ -1,7 +1,7 @@
 <?php
 function savecfg_afficher_tout($flux) {
 	if($flux['args']['exec'] == 'cfg' AND _request('cfg')) {
-		$flux['data'] = debut_boite_info(true) . recuperer_fond('prive/formulaires_savecfg') . '<hr />' . recuperer_fond('prive/formulaire_savecfg_import') . fin_boite_info(true);
+		$flux['data'] = debut_boite_info(true) . recuperer_fond('prive/formulaires_savecfg') . recuperer_fond('prive/formulaire_savecfg_import') . fin_boite_info(true);
 	}
 	return $flux;
 }
