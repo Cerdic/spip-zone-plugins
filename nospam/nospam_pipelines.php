b04f8130df4c8db5b8da5a3f3b637b8ce3fc848c
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-08-31 10:31:44 +0000

    surveiller tous les champs du forum, avec des seuils possiblement adaptes a chaque cas, et un peu plus de severite sur les messages moderes
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@31218 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- nospam/nospam_pipelines.php
+++ nospam/nospam_pipelines.php
@@ -40,6 +40,7 @@ function nospam_formulaire_verifier($flux){
 		// le jeton prend en compte l'heure et l'ip de l'internaute
 		if (_request('nobot') // trop facile !
 		OR (!verifier_jeton($jeton, $form))){
+			#spip_log('pas de jeton pour '.var_export($flux,true),'nospam');
 			$flux['data']['message_erreur'] .= _T('nospam:erreur_jeton');
 			if ($form=='forum')
 				unset($flux['data']['previsu']);
@@ -77,36 +78,53 @@ function nospam_pre_edition($flux){
 		include_spip('inc/autoriser');
 	  if ($flux['data']['statut'] == 'publie'
 		  AND (!isset($GLOBALS['visiteur_session']['id_auteur']) OR !autoriser('modererforum'))){
-	  	// si c'est un message bourre de liens, on le modere
-	  	$texte = propre($flux['data']['texte']);
-	  	$liens = extraire_balises($texte,'a');
-	  	if (count($liens)>20)
-	  		$flux['data']['statut']='spam';
-	  	elseif (count($liens)>=4)
-	  		$flux['data']['statut']='prop';
 
-			// verifier que cette ip et/ou cet email n'a pas deja envoye du spam
-			// auquel cas on modere des qu'il y a un lien
-			if ($flux['data']['statut'] == 'publie'){
-				$email = $flux['data']['email_auteur'] ? " OR email_auteur=".sql_quote($flux['data']['email_auteur']):"";
-				if (sql_countsel('spip_forum','(ip='.sql_quote($GLOBALS['ip'])."$email) AND statut='spam'")>0){
-					if (count($liens)>10)
-						$flux['data']['statut']='spam';
-					elseif (count($liens)>=1)
-						$flux['data']['statut']='prop';
+			$email = strlen($flux['data']['email_auteur']) ? " OR email_auteur=".sql_quote($flux['data']['email_auteur']):"";
+			$spammeur_connu = (sql_countsel('spip_forum','(ip='.sql_quote($GLOBALS['ip'])."$email) AND statut='spam'")>0);
+
+			// si c'est un message bourre de liens, on le modere
+			// le seuil varie selon le champ et le fait que le spammeur est deja connu ou non
+			$seuils = array(
+				// seuils par defaut
+				0=>array(
+					0=>array(1=>'prop',3=>'spam'), // seuils par defaut
+					'url_site' => array(2=>'spam'), // 2 liens dans le champ url, c'est vraiment louche
+					'texte'=>array(4=>'prop',20=>'spam') // pour le champ texte
+				),
+				// seuils severises pour les spammeurs connus
+				'spammeur'=>array(
+					0=>array(1=>'spam'),
+					'url_site' => array(2=>'spam'), // 2 liens dans le champ url, c'est vraiment louche
+					'texte'=>array(1=>'prop',5=>'spam')
+				)
+			);
+
+			$seuils = $spammeur_connu?$seuils['spammeur']:$seuils[0];
+			foreach($flux['data'] as $champ=>$valeur){
+				$valeur = propre($valeur); // on passe propre a chaque fois, ce qui recupere aussi les adresse de site non linkees
+				if ($liens = extraire_balises($valeur,'a')>0){
+					if (isset($seuils[$champ]))
+						$seuil = $seuils[$champ];
+					else
+						$seuil = $seuils[0];
+
+					foreach($seuil as $s=>$stat)
+						if ($liens>=$s)
+							$flux['data']['statut'] = $stat;
 				}
 			}
 
 			// verifier qu'un message identique n'a pas ete publie il y a peu
-			if ($flux['data']['statut'] == 'publie'){
+			if ($flux['data']['statut'] != 'spam'){
 				if (sql_countsel('spip_forum','texte='.sql_quote($flux['data']['texte'])." AND statut IN ('publie','off','spam')")>0)
 					$flux['data']['statut']='spam';
 			}
 			// verifier que cette ip n'en est pas a son N-ieme post en peu de temps
 			// plus de 5 messages en 5 minutes c'est suspect ...
-			if ($flux['data']['statut'] == 'publie'){
-				if (sql_countsel('spip_forum','ip='.sql_quote($GLOBALS['ip']).' AND maj>DATE_SUB(NOW(),INTERVAL 5 minute)')>5)
+			if ($flux['data']['statut'] != 'spam'){
+				if (($nb=sql_countsel('spip_forum','ip='.sql_quote($GLOBALS['ip']).' AND maj>DATE_SUB(NOW(),INTERVAL 5 minute)'))>5)
 					$flux['data']['statut']='spam';
+				#spip_log("$nb post pour l'ip ".$GLOBALS['ip']." dans les 5 dernieres minutes",'nospam');
 			}
 	  }
 	}
