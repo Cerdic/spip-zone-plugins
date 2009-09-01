0f09fe5e84c9f2b91f592d4e3e43c2f6b921f742
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-09-01 08:02:54 +0000

    langues (noie)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@31256 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- noie/lang/noie_zh.php
+++ noie/lang/noie_zh.php
@@ -1,13 +1,15 @@
-﻿<?php
+<?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/noie/lang/
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// I
-	'ie6msg_info' => '为了更轻松地浏览我们的网站内容（以及其它网站），我们建议您尽快更新的浏览器。免费更新<a href="http://www.microsoft.com/china/windows/internet-explorer/">Internet Explorer。您也可以尝试使用其它较为流行的浏览器，比如 <a href="http://www.mozillaonline.com/">Firefox</a>、 <a href="http://www.opera.com/">Opera</a> 或 <a href="http://www.apple.com.cn/safari/">Safari</a>. <br />如果您使用的是工作电脑，请向本单位的计算机管理员要求协助。',
-	'ie6msg_titre' => '很遗憾，您的浏览器已经过期。',
+	'ie6msg_info' => '??????????????????????????????????????????<a href="http://www.microsoft.com/china/windows/internet-explorer/">Internet Explorer?????????????????????? <a href="http://www.mozillaonline.com/">Firefox</a>? <a href="http://www.opera.com/">Opera</a> ? <a href="http://www.apple.com.cn/safari/">Safari</a>. <br />?????????????????????????????',
+	'ie6msg_titre' => '??????????????',
 
 	// T
 	'toocool_alt' => 'Too Cool for IE',
