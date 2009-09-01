7c20264c02981a0deaca104382a5e324d8dc49e4
scamerlo.work@gmail.com <scamerlo.work@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-09-01 03:40:57 +0000

    - Traduction des messages concernant IE6 en chinois simplifié (50+% d'IE6 encore installés en Chine !) - Toocool reste en anglais (pas d'équivalent en chinois à ma connaissance) - Fichier en utf-8 pour les caractères chinois
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@31250 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ noie/lang/noie_zh.php
@@ -0,0 +1,19 @@
+﻿<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/noie/lang/
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// I
+	'ie6msg_info' => '为了更轻松地浏览我们的网站内容（以及其它网站），我们建议您尽快更新的浏览器。免费更新<a href="http://www.microsoft.com/china/windows/internet-explorer/">Internet Explorer。您也可以尝试使用其它较为流行的浏览器，比如 <a href="http://www.mozillaonline.com/">Firefox</a>、 <a href="http://www.opera.com/">Opera</a> 或 <a href="http://www.apple.com.cn/safari/">Safari</a>. <br />如果您使用的是工作电脑，请向本单位的计算机管理员要求协助。',
+	'ie6msg_titre' => '很遗憾，您的浏览器已经过期。',
+
+	// T
+	'toocool_alt' => 'Too Cool for IE',
+	'toocool_info' => 'The site you are trying to view has been deemed "Too Cool for Internet Explorer".
+<br />Please Take a Moment to Try One of These Great Browsers: <a href="http://www.w3junkies.com/toocool/index.php?language=en">http://www.w3junkies.com/toocool/</a>',
+	'toocool_titre' => 'Too Cool for Internet Explorer'
+);
+
+?>
