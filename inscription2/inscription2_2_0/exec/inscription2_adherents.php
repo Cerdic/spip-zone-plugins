6cd136ee70fe97e8f6bcf184d36c1e83f4138d77
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-09-05 01:39:36 +0000

    Rien ne sert de charger deux fois la fonction ici
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@31346 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- inscription2/inscription2_2_0/exec/inscription2_adherents.php
+++ inscription2/inscription2_2_0/exec/inscription2_adherents.php
@@ -7,7 +7,7 @@
 * Sergio and co
 * Ce programme est un logiciel libre distribue sous licence GNU/GPL.
 * Pour plus de details voir le fichier COPYING.txt.
-*  
+*
 **/
 
 function exec_inscription2_adherents() {
@@ -19,20 +19,18 @@ function exec_inscription2_adherents() {
 		fin_page();
 		exit;
 	}
-	
-	$commencer_page = charger_fonction('commencer_page', 'inc');
-	
+
 	$contexte['case'] = _request('case');
 	$contexte['valeur'] = _request('valeur');
 
 	$contexte = array_merge($contexte,$_GET);
-	
+
 	$commencer_page = charger_fonction('commencer_page', 'inc');
-	
+
 	pipeline('exec_init',array('args'=>$_GET,'data'=>''));
-	
+
 	echo $commencer_page(_T('inscription2:gestion_adherents'), "", "", "");
-	
+
 	echo recuperer_fond('prive/table_adherents',$contexte);
 }
 ?>
