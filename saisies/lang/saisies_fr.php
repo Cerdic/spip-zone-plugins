4cc7448b09af149c82ed280e57b3395f62e64999
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-09-08 01:37:24 +0000

    Avoir une 4ème options dans la saisie statuts_auteurs correspondant à l'ensemble des visiteurs (même et surtout non connectés)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@31407 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ saisies/lang/saisies_fr.php
@@ -0,0 +1,15 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// T
+	'tous_visiteurs' => 'Tous les visiteurs (m&ecirc;me non enregistr&eacute;s)',
+
+	// Z
+	'z' => 'zzz'
+);
+
+?>
