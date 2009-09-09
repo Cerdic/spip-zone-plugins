5450260c608aef9bfaac3a6b91126fa52205325d
olivier.sallou@irisa.fr <olivier.sallou@irisa.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-09-09 16:21:39 +0000

    Ajout plugin mesfavoris
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@31450 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ mesfavoris/action/action_mesfavoris_fonction.php
@@ -0,0 +1,12 @@
+<?php
+
+//spip.php?articleXXX
+
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+function action_mesfavoris_dist(){
+
+}
+
+?>
---
+++ mesfavoris/lang/mesfavorispublic_fr.php
@@ -0,0 +1,20 @@
+<?php
+
+
+	/**
+	 * SPIP-Formulaires
+	 *
+	 * Copyright (c) 2006-2009
+	 * Agence Artégo http://www.artego.fr
+	 *  
+	 * Ce programme est un logiciel libre distribue sous licence GNU/GPLv3.
+	 * Pour plus de details voir http://www.gnu.org/licenses/gpl-3.0.html
+	 *  
+	 **/
+
+
+	$GLOBALS[$GLOBALS['idx_lang']] = array(
+		'login_first' => 'Désolé l\'action requiert d\être enregistré, authentifiez-vous d\'abord'
+	);
+
+?>
