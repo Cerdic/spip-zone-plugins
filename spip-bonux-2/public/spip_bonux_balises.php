d7fab3baf3bc8e2528006d9faf4858667d5541c1
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-09-14 12:45:56 +0000

    Une nouvelle fonctionnalité générique dans bonux : optenir les infos de n'importe quel type d'objet.
    #INFO_<QUOI>{type_objet, id_objet}
    
    Exemple : 
    #INFO_TITRE{article, 2}
    #INFO_TITRE{patate, #ENV{id_patate}}
    
    Il y a une fontion PHP associé qu'on peut donc utiliser dans ses scripts : generer_info_entite($id_objet, $type_objet, $info).
    
    Par défaut une info est tout simplement le champ SQL de l'objet mais c'est personnalisable par une fonction generer_<quoi>_entite().
    En effet, il y a des infos qui sont calculées et ne sont donc pas le champ directement.
    
    De plus, deux infos sont prisent en compte en interne par la fonction : l'URL via generer_url_entite() et le titre car SPIP permet de déclarer un titre autre que le champ SQL "titre".
    
    La fonction respecte la magie de SPIP : l'information demandée va être automatiquement transformée s'il y a des traitements automatiques de déclarés pour cette info (cf. la gobale $table_des_traitements). Notamment pour gérer directement les textes <multi> et autres transformations.
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@31574 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spip-bonux-2/public/spip_bonux_balises.php
+++ spip-bonux-2/public/spip_bonux_balises.php
@@ -310,4 +310,30 @@ function balise_BOUTON_ACTION($p){
 	$p->interdire_scripts = false;
 	return $p;
 }
+
+/**
+ * Generer n'importe quel info pour un objet : #INFO_ARTICLE{#ENV{id_article}, titre}
+ * Utilise la fonction generer_info_entite(), se reporter a sa documentation
+ */
+function balise_INFO__dist($p){
+	$info = $p->nom_champ;
+	$type_objet = interprete_argument_balise(1,$p);
+	$id_objet = interprete_argument_balise(2,$p);
+	if ($info === 'INFO_' or !$type_objet or !$id_objet) {
+		$msg = _T('zbug_balise_sans_argument', array('balise' => ' INFO_'));
+		erreur_squelette($msg, $p);
+		$p->interdire_scripts = false;
+		return $p;
+	} elseif ($f = charger_fonction($nom, 'balise', true)) {
+		return $f($p);
+	}else {
+		$p->code = champ_sql($info, $p);
+		if (strpos($p->code, '@$Pile[0]') !== false) {
+			$info = strtolower(substr($info,5));
+			$p->code = "generer_info_entite($id_objet, $type_objet, '$info')";
+		}
+		$p->interdire_scripts = false;
+		return $p;
+	}
+}
 ?>
