5ef99bb32fbbf1b1f3bf0491b2a8044358b5ba11
romy@tetue.net <romy@tetue.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-09-16 08:34:21 +0000

    Traduction en anglais !
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@31608 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ recommander/lang/recommander_en.php
@@ -0,0 +1,14 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+'recommander' => 'Send this article',
+'destinataire' => "Recipient :",
+'recommander_titre' => 'To read on @nom_site@ --',
+'recommander_lecture'=> "Hello, @from@ recommend reading this article : ",
+'recommander_message'=> "Send"
+);
+
+
+?>
