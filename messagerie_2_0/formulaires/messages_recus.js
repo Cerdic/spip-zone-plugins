bcebea7ffba8f21cdd42e8ec26960851ca55f14e
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-09-16 18:22:33 +0000

    recliquer sur le titre du message ferme également le message ... toggleClass au lieu d'un simple addClass
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@31614 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- messagerie_2_0/formulaires/messages_recus.js
+++ messagerie_2_0/formulaires/messages_recus.js
@@ -1,5 +1,5 @@
 function lit_message(id_message,url_mark){
-	jQuery('#message_'+id_message).addClass('on');
+	jQuery('#message_'+id_message).toggleClass('on');
 	if (jQuery('#message_'+id_message+'>div.message').is('.new')){
 		jQuery('#message_'+id_message+'>div.message').removeClass('new');
 		jQuery.get(url_mark);
