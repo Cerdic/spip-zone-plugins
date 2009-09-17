5a1fc99fbdf8c366bb48c95a063e296c8d2b0b57
gaitan@gmail.com <gaitan@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-09-17 18:49:05 +0000

    spanish language file updated
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@31629 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- agenda/2_0_0/lang/agenda_es.php
+++ agenda/2_0_0/lang/agenda_es.php
@@ -3,20 +3,33 @@
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+'absence_prise_en_compte' => 'Tu ausencia ha sido registrada',
+'activite_editoriale' => 'Actividad Editorial',
+'afficher_calendrier' => 'Mostrar el calendario',
+'ajouter_un_evenement' => 'A&ntilde;adir un evento a este art&iacute;culo',
+'ajouter_repetition' => 'A&ntilde;adir repeticiones',
+'ajouter_mots_cles' => 'A&ntilde;adir palabras clave',
+'agenda' => 'Agenda',
+'annee_precedente' => 'A&ntilde;o anterior',
+'annee_suivante' => 'A&ntilde;o siguiente',
+'aucun_evenement' => 'Ning&uacute;n evento',
+'aucune_rubrique_mode_agenda' => 'Por defecto, se puede crear eventos sobre cualquier secci&oacute;n. Si activas el modo agenda sobre una o m&aacute;s secciones, la gesti&oacute;n la creaci&oacute;n de eventos se limitar&aacute; a esa/s ramas.',
+
 'bouton_annuler' => 'Cancelar',
-'activite_editoriale' => 'Actividad editorial',
-'ajouter_un_evenement' => 'A&ntilde;adir un evento a &eacute;ste art&iacute;culo',
-'texte_agenda' => 'AGENDA',
-'titre_cadre_ajouter_evenement' => 'A&ntilde;adir un evento',
-'titre_cadre_modifier_evenement' => 'Modificar un evento',
-'item_mots_cles_association_evenements' => 'a los eventos',
-'info_un_evenement' => 'un evento,',
-'info_nombre_evenements' => '@nb@ eventos,',
-'info_evenements' => 'Eventos',
-'evenements' => 'Eventos',
-'evenement_titre' => 'T&iacute;tulo',
+'bouton_supprimer' => 'Eliminar',
+
+'creer_evenement' => 'Crear un evento',
+
+'erreur_article_manquant' => 'Debes indicar un art&iacute;culo',
+'erreur_article_interdit' => 'No tiene permiso para asociar este evento al art&iacute;culo',
+
+'evenement_article' => 'Asoaciar al art&iacute;culo',
+'evenement_autres_occurences' => 'Otras ocurrencias :',
 'evenement_descriptif' => 'Descripci&oacute;n',
-'evenement_lieu' => 'Lugar',
+'evenement_horaire' => 'Todo el d&iacute;a',
+'evenement_lieu' => 'Ubicaci&oacute;n',
+'evenement_adresse' => 'Direcci&oacute;n',
+'evenement_titre' => 'T&iacute;tulo',
 'evenement_date' => 'Fecha',
 'evenement_date_de' => 'De ',
 'evenement_date_du' => 'Del ',
@@ -25,19 +38,61 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 'evenement_date_a_immediat' => 'a las ',
 'evenement_date_debut' => 'Fecha de comienzo',
 'evenement_date_fin' => 'Fecha de fin',
-'lien_retirer_evenement' => 'Suprimir',
-'evenement_horaire' => 'Horario',
-'titre_sur_l_agenda' => "Sobre la agenda",
-'icone_creer_evenement' => 'Creaci&oacute;n de un nuevo evento',
 'evenement_repetitions' => 'Repeticiones',
-'nb_repetitions' => '@nb@&nbsp;repeticiones',
+'evenements' => 'Eventos',
+'evenements_a_venir'=>'pr&oacute;ximos',
+'evenements_depuis_debut'=>'Todos',
+
+'fermer' => 'cerrar',
+
+'groupes_mots'=>'Grupos de palabras clave',
+
+'icone_creer_evenement' => 'Crear un nuevo evento',
+'item_mots_cles_association_evenements' => 'para eventos',
+'info_un_evenement' => '1 evento',
+'info_nombre_evenements' => '@nb@ eventos',
+'info_aucun_evenement' => 'Ning&uacute;n evento',
+'info_evenements' => 'Eventos',
+
+'lien_retirer_evenement' => 'Eliminar',
+'label_inscription' => 'Inscripci&oacute;n en l&iacute;nea ',
+'label_places' => 'Limitar el n&uacute;mero de lugares',
+'label_reponse_jyparticipe' => 'Asistir&eacute;',
+'label_reponse_jyparticipe_peutetre' => 'Tal vez asista',
+'label_reponse_jyparticipe_pas' => 'No asistir&eacute;',
+'label_vous_inscrire' => 'Tu participaci&oacute;n',
+
+'mois_precedent' => 'mes anterior',
+'mois_suivant' => 'mes siguiente',
+
+'nb_mots_clefs' => '@nb@&nbsp; palabras clave',
+'nb_repetitions' => '@nb@&nbsp; repeticiones',
+
+'probleme_technique' => 'Ha habido un problema t&eacute;cnico. Por favor, intenta m&aacute;s tarde.',
+'participation_prise_en_compte' => 'Tu participaci&oacute;n ha sido registrada',
+'participation_incertaine_prise_en_compte' => 'Tu posible participaci&oacute;n ha sido registrada',
+
+'rubriques' => 'Secciones Agenda',
+'rubrique_dans_une_rubrique_mode_agenda' => 'Esta secci&oacute;n permite utilizar eventos',
+'rubrique_sans_gestion_evenement' => 'No est&aacute; activado el modo agenda para esta secci&oacute;n',
+'rubrique_mode_agenda' => 'El modo agenda est&aacute; activado para esta secci&oacute;n y sus ramas.',
+'rubrique_activer_agenda' => 'Activar el modo agenda para esa secci&oacute;n',
+'rubrique_desactiver_agenda' => 'Desactivar el modo agenda para esta secci&oacute;n',
+
+'sans_titre' => '(sin t&iacute;tulo)',
+
+'toutes_rubriques' => 'Todas',
+'texte_agenda' => 'AGENDA',
+'titre_sur_l_agenda' => 'En la agenda',
+'titre_cadre_ajouter_evenement' => 'A&ntilde;adir un evento',
+'titre_cadre_modifier_evenement' => 'Modificar un evento',
+'repetition_de' => 'Repetici&oacute;n de',
+
+'un_mot_clef' => '1&nbsp;palabra clave',
 'une_repetition' => '1&nbsp;repetici&oacute;n',
-'repetition_de' => "Repetici&oacute;n de",
-'nb_mots_clefs' => '@nb@&nbsp;palabras&nbsp;clave',
-'un_mot_clef' => '1&nbsp;palabra&nbsp;clave',
-'sans_titre' => "(con t&iacute;tulo)",
-'evenement_autres_occurences' => "Otras ocurrencias:",
-'bouton_supprimer' => "Suprimir",
+
+'voir_evenements_rubrique' => 'Ver los eventos de la secci&oacute;n',
 );
+
 
 ?>
