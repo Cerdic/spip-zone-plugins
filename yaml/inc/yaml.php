a5673b65a24f9e4cbe0c5fdad2199e4baa496502
marcimat@free.fr <marcimat@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-09-18 12:28:25 +0000

    - La librairie sfYaml n'arrive pas à parser un exemple du site de Yaml : http://www.yaml.org/spec/1.2/spec.html#id2559548, exemple 2.12. La librairie SPYC (http://code.google.com/p/spyc/) s'en occupe quand à elle très bien. Par contre cette librairie n'a pas de gestion d'exception en cas d'erreur. On permet de la tester (pas active par défaut) avec {{{define('_LIB_YAML','spyc');}}} . Si elle convient, on la gardera.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@31640 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- yaml/inc/yaml.php
+++ yaml/inc/yaml.php
@@ -7,6 +7,9 @@
 #
 # Regle de dev: ne pas se rendre dependant de la lib sous-jacente
 
+// temporaire le temps de tester spyc
+define('_LIB_YAML','sfyaml'); 
+#define('_LIB_YAML','spyc'); 
 
 /*
  * Encode n'importe quelle structure en yaml
@@ -14,6 +17,11 @@
  * @return string
  */
 function yaml_encode($struct, $opt = array()) {
+	// test temporaire
+	if (_LIB_YAML == 'spyc') {
+		require_once _DIR_PLUGIN_YAML.'spyc/spyc.php';
+		return Spyc::YAMLDump($struct);
+	}
 	require_once _DIR_PLUGIN_YAML.'sfyaml/sfYaml.php';
 	require_once _DIR_PLUGIN_YAML.'sfyaml/sfYamlDumper.php';
 	$opt = array_merge(
@@ -29,6 +37,11 @@ function yaml_encode($struct, $opt = array()) {
  * @param string $input
  */
 function yaml_decode($input) {
+	// test temporaire
+	if (_LIB_YAML == 'spyc') {
+		require_once _DIR_PLUGIN_YAML.'spyc/spyc.php';
+		return Spyc::YAMLLoad($input);
+	}
 	require_once _DIR_PLUGIN_YAML.'sfyaml/sfYaml.php';
 	require_once _DIR_PLUGIN_YAML.'sfyaml/sfYamlParser.php';
 
