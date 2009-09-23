dfa467494d1df97a33a67437861327963ba4273c
apachot@openstudio.fr <apachot@openstudio.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-09-23 11:21:33 +0000

    - n'obtenir la récompense d'un jeu qu'une seule fois
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@31763 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- panoramas/panoramas_mes_fonctions.php
+++ panoramas/panoramas_mes_fonctions.php
@@ -17,21 +17,21 @@ function contient($texte, $findme) {
 
 function memoriser_resultat_jeu($texte, $id_jeu) {
 
-	//mise à jour de la liste des jeux réussis
-		$tab_jeux_reussis = explode(",", $_COOKIE['jeux_reussis']);
+
+		$tab_jeux_reussis = split(",", $_COOKIE['jeux_reussis']);
 		if (!in_array($id_jeu, $tab_jeux_reussis)) {
-			setcookie("jeux_reussis",implode(",", $tab_jeux_reussis).",".$id_jeu);
+			return "Resultat m&eacute;moris&eacute;";
 		}
-	return "Resultat m&eacute;moris&eacute;";
+	return "Vous avez d&eacute;j&agrave; r&eacute;ussi ce jeu";
 }
 function remplir_sacoche($texte, $id_objet, $img_src) {
 
-	//mise à jour de la liste des objets ramassés
+	//mise ï¿½ jour de la liste des objets ramassï¿½s
 	
-	//$objet_ajoute = "<li><div class=\"objet".$id_objet."\"><img src=\"".$img_src."\" /><span style=\"display: none;\">".$id_objet."</span></div></li>";
-	//if ($_COOKIE['objets_ramasses']) setcookie("objets_ramasses", $_COOKIE['objets_ramasses'].$objet_ajoute);
-	//else setcookie("objets_ramasses", $objet_ajoute);
-	return ""; // - Un objet a &eacute;t&eacute; donn&eacute; en r&eacute;compense";
+	$objet_ajoute = "<li><div class='objet".$id_objet."'><img src='".$img_src."' /><span style='display: none;'>".$id_objet."</span></div></li>";
+	if ($_COOKIE['objets_ramasses']) setcookie("objets_ramasses", $_COOKIE['objets_ramasses'].$objet_ajoute);
+	else setcookie("objets_ramasses", $objet_ajoute);
+	return " - Un objet a &eacute;t&eacute; donn&eacute; en r&eacute;compense <br /><ul>".$_COOKIE['objets_ramasses']."</ul>";
 }
 
 ?>
