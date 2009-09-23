6884133dd828542c29489de7c4b13fbe1decf1ff
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-09-23 17:21:10 +0000

    Une traduction rapide en anglais
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@31770 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ grappes/lang/grappes_en.php
@@ -0,0 +1,66 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// B
+'bouton_ajouter' => 'Add',
+'bouton_chercher' => 'Search',
+'bouton_checkbox_qui_administrateurs' => 'Administrators',
+'bouton_checkbox_qui_redacteurs' => 'Editors',
+
+// D
+'delier' => 'Unlink',
+
+// I
+'icone_creation_grappe' => 'Create a new grape',
+'icone_modif_grappe' => 'Modify this grape',
+'icone_supprimer_grappe' => 'Delete this grape',
+'icone_voir_toutes_grappes' => 'View all grapes',
+'info_acces_0minirezo' => 'Administrators',
+'info_acces_1comite' => 'Editor',
+'info_changer_nom_grappe' => 'Change the name of the grape',
+'info_creation_grappes' => 'Create or modify elements grapes',
+'info_grappes_association' => 'What kind of objects can be linked to the grape ?',
+'info_lier_auteurs' => 'Authors',
+'info_lier_articles' => 'Articles',
+'info_lier_breves' => 'News items',
+'info_lier_documents' => 'Documents',
+'info_lier_mots' => 'Keywords',
+'info_lier_grappes' => 'Grapes',
+'info_lier_rubriques' => 'Sections',
+'info_lier_sites' => 'Websites',
+'info_lier_syndic' => 'Websites',
+'info_nom_grappe' => 'Name of the grape',
+'info_qui_peut_lier' => 'Who can link objects to the grape ?',
+'info_rechercher' => 'Search', // chercher de nouveaux objets a lier
+
+'item_groupes_association_articles' => 'The articles',
+'item_groupes_association_auteurs' => 'The authors',
+'item_groupes_association_breves' => 'The news items',
+'item_groupes_association_documents' => 'The documents',
+'item_groupes_association_grappes' => 'The grapes',
+'item_groupes_association_groupes_mots' => 'The keyword groups',
+'item_groupes_association_mots' => 'The keywords',
+'item_groupes_association_rubriques' => 'The sections',
+'item_groupes_association_syndic' => 'The websites',
+
+// L
+'lier' => 'Link',
+
+// P
+'pas_de_identifiant' => 'Name unknown',
+
+// T
+'texte_descriptif' => 'Description',
+
+'titre_grappe' => 'Grape',
+'titre_nouvelle_grappe' => 'New cluster',
+'titre_page_grappes' => 'Grapes',
+
+// V
+'voir' => 'View',
+);
+
+?>
