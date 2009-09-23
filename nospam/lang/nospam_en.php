e1ebf610abd1c29d95c9c0771cd91f55586f318d
superyms@yahoo.fr <superyms@yahoo.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-09-23 15:11:28 +0000

    ma petite contrib du jour : ajout du fichier de langue en anglais
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@31766 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ nospam/lang/nospam_en.php
@@ -0,0 +1,11 @@
+﻿<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	'erreur_jeton' => 'Unable to take account of your message. Thank you to resubmit!',
+	'erreur_spam' => 'Unable to take account of your message!',
+
+);
+
+?>
