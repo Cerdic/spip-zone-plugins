1fbc3bf6550dab59c84ece4654e33d61eb8e7b81
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-09-24 10:52:05 +0000

    Finalement, pas la peine de faire une version propre à la mutualisation.
    Usage : dans le config/mes_options.php du site concerné (en mutualisation facile ou non) :
    define('SWITCHER_REPERTOIRE_SQUELETTES_ALTERNATIFS',$GLOBALS['dossier_squelettes'].'_test');
    
    (Sous réserve, bien sûr, de n'avoir défini qu'un seul dossier squelettes, sinon, s'adpater...).
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@31779 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- switcher/inc/switcher_config.php
+++ switcher/inc/switcher_config.php
@@ -3,7 +3,11 @@ global $repertoire_squelettes_alternatifs;
 global $styleListeSwitcher;
 
 // Repertoire contenant les repertoires squelettes a tester
-$repertoire_squelettes_alternatifs ='squelettes-test';
+if (defined('SWITCHER_REPERTOIRE_SQUELETTES_ALTERNATIFS')) {
+	$repertoire_squelettes_alternatifs = SWITCHER_REPERTOIRE_SQUELETTES_ALTERNATIFS;
+} else {
+	$repertoire_squelettes_alternatifs ='squelettes-test';
+}
 
 // Style css associe a la liste deroulante
 $styleListeSwitcher="font-size: 10px;background-color: #FFF;color: #0C479D;border-top: 1px solid #CECECE; border-bottom: 2px solid #4A4A4A; border-left: 1px solid #CECECE; border-right: 1px solid #CECECE;margin:2px .5em;";
