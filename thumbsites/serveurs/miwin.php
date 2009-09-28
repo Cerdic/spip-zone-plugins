273dcb060bb4a232d1b118ff3193d92fea9c8edc
pierrekuhn82@gmail.com <pierrekuhn82@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-09-28 20:35:37 +0000

    Rajout d'un serveur miwin
    /!\ Il faut mettre un liens vers le site miwin.fr (décris dans la config du plugins)
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@31856 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ thumbsites/serveurs/miwin.php
@@ -0,0 +1,21 @@
+<?php
+    /*! \file miwin.php 
+     *  \brief Fichier de fonctions personnalisées au serveur miwin
+     *         
+     *  Défini la surcharge de thumb
+     *  Le nom du fichier doit être obligatoirement celui déclaré dans le fond pour le paramétre thumbsites/serveur     
+     */
+
+
+    /*! \brief thumbsite_serveur() pour le serveur miwin
+     *
+     *  Surcharge de la fonction thumbs() exploitant le serveur d'aperçu de miwin
+     *  
+     * \param $url_site url du site à consulter
+     * \return url de l'image générée par le serveur
+     */
+    function url_thumbsite_serveur($url_site) {
+        //retourne l'url de la vignette
+        return "http://thumbs.miwim.fr/img.php?url=".$url_site."&ext=jpg&size=120x90&remplace=http://www.miwim.fr/templates/miwim/img/no-preview.gif";
+    }        
+?>
