12820e93a1bf4556c4d7b0c06c5eab13ea4150de
cy_altern@yahoo.fr <cy_altern@yahoo.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-10-03 14:19:42 +0000

    Extension du plugin Multi-Dossier qui permet de ne pas avoir a modifier le mes_options d'un SPIP qui utilise un dossier /plugins supplementaire Les SPIP autorises a avoir un dossier /plugins specifique doivent etres declares dans le mes_options.php central par: $GLOBALS['multiplug_autorise'] = array('nom_dossier_spip1','nom_dossier_spip2', ...);
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@31919 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ multiplug_auto/multiplug_auto_options.php
@@ -0,0 +1,9 @@
+<?php
+if (isset($GLOBALS['multiplug_autorise'])
+    AND is_dir(_DIR_RACINE.$GLOBALS['mutualisation_dir'].'/'.$GLOBALS['site'].'/'.'plugins/')
+    AND in_array($GLOBALS['site'], $GLOBALS['multiplug_autorise']))
+  define('_DIR_PLUGINS_SUPPL', _DIR_RACINE.$GLOBALS['mutualisation_dir'].'/'.$GLOBALS['site'].'/'.'plugins/');
+  
+//define('_DIR_PLUGINS_SUPPL', _DIR_RACINE.'sites/'.'mamutu1.mon-site.tld/'.'plugins/'.':'._DIR_RACINE.'sites/'.'mamutu1.mon-site.tld/'.'core/');
+
+?>
