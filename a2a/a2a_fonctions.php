a6423a913d3870cbf864398eb0fdd9dd52f71dfc
brunobergot@gmail.com <brunobergot@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-10-03 11:12:03 +0000

    la balise ARTICLES_LIES accepte un paramètre "inverse" pour le classement des éléments qu'elle renvoie (merci tonton)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@31916 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- a2a/a2a_fonctions.php
+++ a2a/a2a_fonctions.php
@@ -1,13 +1,17 @@
 <?php
 
-function lister_articles_lies($id_article){
-	return sql_allfetsel('id_article_lie','spip_articles_lies','id_article=' . sql_quote($id_article));
+function lister_articles_lies($id_article, $ordre){
+	return sql_allfetsel('id_article_lie','spip_articles_lies','id_article=' . sql_quote($id_article),'',"rang $ordre");
 }
 
 function balise_ARTICLES_LIES($p) {
 	$id_article = champ_sql('id_article', $p);
-	$p->code = "lister_articles_lies($id_article)";
+	$ordre = 'ASC';
+	if($inverse = interprete_argument_balise(1,$p))
+		$ordre = 'DESC';
+	$p->code = "lister_articles_lies($id_article, $ordre)";
 	$p->type = 'php';  
 	return $p;
 }
+
 ?>
