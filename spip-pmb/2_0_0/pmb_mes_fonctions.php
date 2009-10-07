4ef1db0b9d8e4f34f4912cf5d781b3410b5e1a63
apachot@openstudio.fr <apachot@openstudio.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-10-07 11:19:16 +0000

    Licence et lien vers pmb Ne pas afficher les nouveautés en page d'accueil qui n'ont pas de descriptif
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@31958 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spip-pmb/2_0_0/pmb_mes_fonctions.php
+++ spip-pmb/2_0_0/pmb_mes_fonctions.php
@@ -1,4 +1,28 @@
 <?php
+/*************************************************************************************/
+/*                                                                                   */
+/*      Portail web pour PMB	                                                            		 */
+/*                                                                                   */
+/*      Copyright (c) OpenStudio		                                     */
+/*	email : info@openstudio.fr		        	                             	 */
+/*      web : http://www.openstudio.fr						   							 */
+/*                                                                                   */
+/*      This program is free software; you can redistribute it and/or modify         */
+/*      it under the terms of the GNU General Public License as published by         */
+/*      the Free Software Foundation; either version 3 of the License, or            */
+/*      (at your option) any later version.                                          */
+/*                                                                                   */
+/*      This program is distributed in the hope that it will be useful,              */
+/*      but WITHOUT ANY WARRANTY; without even the implied warranty of               */
+/*      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                */
+/*      GNU General Public License for more details.                                 */
+/*                                                                                   */
+/*      You should have received a copy of the GNU General Public License            */
+/*      along with this program; if not, write to the Free Software                  */
+/*      Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA    */
+/*                                                                                   */
+/*************************************************************************************/
+
 include_spip('base/pmb_tables');
 
 
@@ -211,7 +235,7 @@ function pmb_recherche_extraire($recherche, $url_base, $look_FIRSTACCESS='', $lo
 	}
 	
 	
-	//récupérer le résultat d'une recherche simple via les webservices
+	//récupérer le résultat d'une recherchevia les webservices
 	
 	global $gtresultat;
 	$gtresultat = array();
