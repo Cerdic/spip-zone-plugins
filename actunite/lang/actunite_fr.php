94e495340fdb231eeb5e4deca42c38f065c5658c
cyril@cym.fr <cyril@cym.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-10-10 20:43:27 +0000

    Aer dépot du plugin
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@32016 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ actunite/lang/actunite_fr.php
@@ -0,0 +1,22 @@
+
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+'cfg_label_titre_cfg_boucle' 	=> 'Param&egrave;tres de la boucle',
+'cfg_label_rubriques' 			=> 'Rubriques :',
+'cfg_label_mots_cle' 			=> 'Mots cl&eacute; :',
+'cfg_label_critere_tri' 		=> 'Crit&egrave;res de tri :',
+'cfg_info_boucle'				=> 'La boucle qui permet d\'extraire les articles du site est param&eacute;trable. On peut tout d\'abord sp&eacute;cifier la ou les rubriques desquelles seront extraites les articles (indiquer les num&eacute;ros de rubriques s&eacute;par&eacute;s par des virgules). Ensuite on peut indiquer si la s&eacute;lection des articles s\'effectue selon certains momts cl&eacute;s (idem, liste des id_mots s&eacute;par&eacute;s par des virgules). Enfin, on peut indiquer les &eacute;ventuels crit&egrave;res de tri pour la boucle (par date, par num titre, etc.).',
+
+'cfg_label_titre_insertion'		=> 'Position d\'insertion',
+'cfg_label_bloc_insertion'		=> 'Insertion',
+'cfg_info_insertion'			=> 'Indiquez ici le nom du bloc et la position d\'insertion'
+
+);
+
+
+
+?>
