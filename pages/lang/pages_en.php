2ce44257f3ae80b647549111e6b141116adba5f7
superyms@yahoo.fr <superyms@yahoo.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-10-11 05:49:43 +0000

    ajout des fichiers anglais et arabe... @+
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@32020 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ pages/lang/pages_en.php
@@ -0,0 +1,14 @@
+<?php
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+'aucune_page' => 'There are no pages at the moment.',
+'creer_page' => 'Create a new page',
+'input_page' => 'une_page',
+'modifier_page' => 'Edit page:',
+'titre_page' => 'Page',
+'toutes_les_pages' => 'All pages'
+
+);
+
+?>
