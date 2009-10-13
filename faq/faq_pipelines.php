d936f14f323cac41df026168bbac3eb62ba4b2b6
romy@rezo.net <romy@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-10-13 10:11:39 +0000

    Les reponses sont imprimables, meme lorsqu'elles sont repliees a l'ecran.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@32068 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- faq/faq_pipelines.php
+++ faq/faq_pipelines.php
@@ -1,8 +1,8 @@
 <?php
 
 function faq_insert_head($flux) {
-	$flux .= '<script  src="'.find_in_path('faq.js').'" type="text/javascript"></script>';
-	$flux .= '<link rel="stylesheet" href="'.find_in_path('faq.css').'" type="text/css" media="all" />';
+	$flux .= '<script src="'.find_in_path('faq.js').'" type="text/javascript"></script>';
+	$flux .= '<link rel="stylesheet" href="'.find_in_path('faq.css').'" type="text/css" />';
 	return $flux;
 }
 
