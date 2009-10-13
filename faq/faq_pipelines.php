70f380a8520a580ef504310683938748a6fcb237
romy@rezo.net <romy@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-10-13 15:39:00 +0000

    Il faut indiquer le media de la feuille de style pour qu'elle soit compactee avec les autres, grml !
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@32091 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- faq/faq_pipelines.php
+++ faq/faq_pipelines.php
@@ -2,7 +2,7 @@
 
 function faq_insert_head($flux) {
 	$flux .= '<script src="'.find_in_path('faq.js').'" type="text/javascript"></script>';
-	$flux .= '<link rel="stylesheet" href="'.find_in_path('faq.css').'" type="text/css" />';
+	$flux .= '<link rel="stylesheet" href="'.find_in_path('faq.css').'" type="text/css" media="all" />';
 	return $flux;
 }
 
