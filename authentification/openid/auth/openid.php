a5a0a5606654a45ad7977b8351b5c50144908637
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-10-13 09:42:08 +0000

    ajouter http:// devant le login openid si rien indique, c'est plus smart avec les utilisateurs
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@32064 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- authentification/openid/auth/openid.php
+++ authentification/openid/auth/openid.php
@@ -17,7 +17,14 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 // http://doc.spip.org/@inc_auth_ldap_dist
 function auth_openid_dist ($login) {
 
-	if (!$login) return false;
+	// il faut un login non vide et qui contient au moins un point
+	// car c'est cense etre une url
+	if (!$login
+		OR !preg_match(';[.];',$login)) return false;
+
+	// si pas de protocole, mettre http://
+	if (!preg_match(';^[a-z]{3,6}://;i',$login))
+		$login = "http://".$login;
 
 	// Si l'utilisateur figure deja dans la base, y recuperer les infos
 	$result = sql_fetsel("*", "spip_auteurs", array("statut!=".sql_quote("5poubelle") , "openid=" . sql_quote($login)));
