11d934b93859bd243b646f3b98628954ca4b8970
renato@rezo.net <renato@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-10-14 22:49:40 +0000

    noveau balise generique #EXPRESSION pour ajouter a la select une exression sql et une example de balise #SUM qui urtilise #EXPRESSION
    #EXPRESION(expression,alias)
    
    Utiliser {par alias} pout trier
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@32127 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- agregateurs_sql/public/agregateurs_sql_balises.php
+++ agregateurs_sql/public/agregateurs_sql_balises.php
@@ -63,4 +63,37 @@ function calculer_balise_criteres($nom, $p, $balise="") {
   return $p;
 }
 
+function balise_SUM($p) {
+  $t = $p->param[0][1][0]->texte;
+  $p->param[0][1][0]->texte = "SUM($t)";
+  $p->param[0][2][0]->texte = "sum_$t";
+  return balise_EXPRESSION($p); 
+}
+
+function balise_EXPRESSION($p) {
+  static $num = 1;
+  $b = $p->nom_boucle ? $p->nom_boucle : $p->descr['id_mere'];
+  if ($b === '' || !isset($p->boucles[$b])) {
+     erreur_squelette(
+       _T('zbug_champ_hors_boucle',
+       array('champ' => "#$b" . 'EXPRESSION')
+     ), $p->id_boucle);
+     $p->code = "''";
+  } else {
+    if (isset($p->param[0][1][0])
+    AND $champ = ($p->param[0][1][0]->texte)) {
+      if(isset($p->param[0][2][0]) AND $p->param[0][2][0]->texte)
+        $alias = $p->param[0][2][0]->texte;
+      else {
+        $alias = "expr_$num";
+        $num++;
+      }
+      $p->code = "\$Pile[\$SP]['$alias']";
+      $p->boucles[$b]->select[] = "$champ as $alias";
+      $p->interdire_scripts = true;
+    }
+  }
+  return $p;
+}
+
 ?>
