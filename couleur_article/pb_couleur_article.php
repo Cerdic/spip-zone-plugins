c3eb8c40aa50c8ca26b3d195f58ed6032f6c9cb4
arno@rezo.net <arno@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-10-18 12:58:42 +0000

    Nouveau plugin: couleur_article. Meme principe que pour couleur_rubrique, mais applique aux articles.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@32199 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ couleur_article/pb_couleur_article.php
@@ -0,0 +1,102 @@
+<?php
+
+
+function pb_couleur_article_gauche($vars){
+
+
+		if ($_POST["pb_couleur_article"] && $GLOBALS['connect_statut'] == "0minirezo" && $GLOBALS["connect_toutes_rubriques"]) {
+			$couleur = str_replace("#", "", $_POST["pb_couleur_article"]);
+			$id_article = $_GET["id_article"];
+			
+			
+			ecrire_meta("pb_couleur_article$id_article",$couleur);
+			if ($_POST["supprimer"]) ecrire_meta("pb_couleur_article$id_article","");
+			ecrire_metas();
+			
+		}
+
+
+		$exec = $vars["args"]["exec"];
+		$id_article = $vars["args"]["id_article"];
+		$data =	$vars["data"];
+		
+		$ret = "";
+		
+		
+		if ($exec == "articles" && $GLOBALS['connect_statut'] == "0minirezo" && $GLOBALS["connect_toutes_rubriques"]) {
+
+			$deplier = true;
+			$pb_couleur_article = pb_couleur_article($id_article);
+			if (!$pb_couleur_article) {
+				$pb_couleur_article = "999999";
+				$deplier = false;
+			}
+			
+
+			$titre = "Couleur de l&rsquo;article";
+		
+			$ret .= debut_cadre_enfonce(_DIR_PLUGIN_PB_COULEUR_ARTICLE."img_pack/icon-24.png", true, "rien.gif", bouton_block_depliable($titre, $deplier, "selection_couleur"));
+
+			$ret .= debut_block_depliable($deplier, "selection_couleur");
+		    $ret .= "<form method='post' action='index.php?exec=articles&id_article=$id_article'>";
+
+			$ret .= "<div id='picker' style='margin-left: -5px;'></div>";
+			$ret .= "<div style='float: right;'><input class='fondo' type='submit' value='"._L("Enregistrer")."' /></div>";
+			$ret .= "<div style='text-align: left; padding-top: 2px;'><input type='text' id='pb_couleur_article' name='pb_couleur_article' value='#$pb_couleur_article'  class='colorwell' style='width: 70px;' /></div>\n"; 
+
+			if ($deplier) {
+				$ret .= "<input type='submit' class='fondl' name='supprimer' value='Supprimer la couleur' />";
+			}
+			
+			$ret .= "</form>";
+			$ret .= fin_block();
+			$ret .= fin_cadre_enfonce(true);
+
+
+
+    
+    
+    
+
+			
+		}
+
+
+		$data .= $ret;
+	
+		$vars["data"] = $data;
+		return $vars;
+}
+
+
+
+function pb_couleur_article_header($flux){
+	if ($_GET["exec"]=="articles") {
+		$flux .= "<link rel='stylesheet' type='text/css' href='"._DIR_FARBTASTIC_1_3_LIB."farbtastic.css' />\n";     
+		$flux .= "<script src='"._DIR_FARBTASTIC_1_3_LIB."farbtastic.js' type=\"text/javascript\"></script>\n";
+		
+		
+		$flux .= "<script type=\"text/javascript\">
+$(document).ready(function() {
+    var f = $.farbtastic('#picker');
+    var p = $('#picker').css('opacity', 1);
+    var selected;
+    $('.colorwell')
+      .each(function () { f.linkTo(this); $(this).css('opacity', 0.75); })
+      .focus(function() {
+        if (selected) {
+          $(selected).css('opacity', 0.75).removeClass('colorwell-selected');          
+        }
+        f.linkTo(this);
+        p.css('opacity', 1);
+        $(selected = this).css('opacity', 1).addClass('colorwell-selected');
+      });
+});</script>
+";
+		
+	}
+	return $flux;
+
+}
+
+?>
---
+++ couleur_article/pb_couleur_article_options.php
@@ -0,0 +1,29 @@
+<?php
+//
+// ajout bouton
+// 
+$p=explode(basename(_DIR_PLUGINS)."/",str_replace('\\','/',realpath(dirname(__FILE__))));
+define('_DIR_PLUGIN_PB_COULEUR_ARTICLE',(_DIR_PLUGINS.end($p)));
+
+if (!defined('_DIR_LIB')) define('_DIR_LIB', _DIR_RACINE . 'lib/');
+define('_DIR_FARBTASTIC_1_3_LIB', _DIR_LIB . 'farbtastic_1_3/'); 
+ 
+
+
+ 
+//
+// functions
+//
+
+function pb_couleur_article($id_article) {
+			$pb_couleur_article = lire_meta("pb_couleur_article$id_article");
+//			if (!$pb_couleur_article) $pb_couleur_article = "#999999";
+	
+	return $pb_couleur_article;
+}
+
+function couleur_article($id_article) {
+	return pb_couleur_article($id_article);
+}
+
+?>
