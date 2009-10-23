e8b6e80fbeecaa523ca0c66b3cc9ff43954e5ffc
superyms@yahoo.fr <superyms@yahoo.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-10-23 07:58:16 +0000

    - ajout multilinguisme pour plugin.xml  - fichiers de langue : arabe, allemand, espagnol et italien.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@32300 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ visiteurs_connectes/lang/local_ar.php
@@ -0,0 +1,7 @@
+<?php
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+ 	// Bare de nav
+	'visiteurs_en_ce_moment' => '@nb@ &#1605;&#1606; &#1575;&#1604;&#1586;&#1608;&#1575;&#1585; &#1575;&#1604;&#1570;&#1606;'
+	);
+?>
