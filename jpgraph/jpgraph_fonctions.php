0eb96a3c871a2eefe34b3bdf4f97b3283cade7a1
erational@erational.org <erational@erational.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-10-23 07:20:51 +0000

    1 seul modele 1 seule fonction 1 exemple avec des boucles
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@32296 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- jpgraph/jpgraph_fonctions.php
+++ jpgraph/jpgraph_fonctions.php
@@ -6,7 +6,7 @@ define('_DIR_JPGRAPH_LIB', _DIR_LIB . 'jpgraph-3.0.6/');
 
 //
 // cree un hash unique 
-//
+// (a ameliorer activer le cache de jpgraph ou celui de spip ?)
 function jpgraph_name_hash($type="graph",$largeur,$hauteur,$donnee) {
     // repertoire IMG/jpgraph dispo ?
     if (!is_dir(_DIR_IMG."jpgraph/")) {                                     
@@ -25,15 +25,11 @@ function jpgraph_name_hash($type="graph",$largeur,$hauteur,$donnee) {
 //
 // filtre pour creer des courbes simples
 //
-function filtre_jpgraph_courbe($str,$donnee="",$legende="",$largeur=400,$hauteur=300,$titre="",$couleur="orange"){
-    // retrouver jpgraph 
-  	$cwd = getcwd();
-  	chdir(realpath(_DIR_JPGRAPH_LIB));
-    require_once ('src/jpgraph.php');
-    require_once ('src/jpgraph_line.php');
-  	chdir($cwd);
+function filtre_jpgraph($str,$type_graphe="courbe",$donnee="",$legende="",$largeur=400,$hauteur=300,$titre="",$couleur="orange"){
    
     // traiter les donnees
+    $type_graphe = strtolower(trim($type_graphe));  // pour pb avec les modeles si du blanc en fin de ligne
+    $couleur = trim($couleur);    
     $donnee =  explode(";", $donnee);    
     foreach ($donnee as $key => $value)
          $donnee[$key] = (float) $value;    
@@ -46,126 +42,61 @@ function filtre_jpgraph_courbe($str,$donnee="",$legende="",$largeur=400,$hauteur
     $largeur = (int) $largeur;    if ($largeur<=0) $largeur = 400;
     $hauteur = (int) $hauteur;    if ($hauteur<=0) $hauteur = 300;
     
-    // Create the graph. These two calls are always required
-    $graph = new Graph($largeur,$hauteur);
-    $graph->SetScale('textlin');
-    
-    // Create the linear plot
-    $lineplot=new LinePlot($donnee);
-    $lineplot->SetColor($couleur);
-    
-    // titre & legende 
-    $graph->title->Set(utf8_decode($titre));
-    if (count($legende)>1) {
-        $graph->xaxis->SetTickLabels($legende);
-    }
-    
-    // Add the plot to the graph
-    $graph->Add($lineplot);
-    
-    // Save the graph   
-    $filename = jpgraph_name_hash("courbe",$largeur,$hauteur,$donnee);
-    @unlink($filename); // http://jpgraph.intellit.nl/index.php?topic=4547.msg11823
-    $graph->Stroke($filename);
-    
-    
-    return "<span class='spip_documents jgraph'><img src='$filename' alt='' width='$largeur' height='$hauteur' /></span>";
-
-}
-
-//
-// filtre pour creer des barres horizontales
-//
-function filtre_jpgraph_barre($str,$donnee="",$legende="",$largeur=400,$hauteur=300,$titre="",$couleur="orange"){
     // retrouver jpgraph 
   	$cwd = getcwd();
   	chdir(realpath(_DIR_JPGRAPH_LIB));
     require_once ('src/jpgraph.php');
-    require_once ('src/jpgraph_bar.php');
-  	chdir($cwd);
-   
-    // traiter les donnees
-    $donnee =  explode(";", $donnee);    
-    foreach ($donnee as $key => $value)
-         $donnee[$key] = (float) $value;
-    if (count($donnee)<2) 
-        $donnee[] = "1";   // securite pour empecher les erreurs si donnnee pas renseigne  
+    switch($type_graphe) {
+        case "courbe":      require_once ('src/jpgraph_line.php'); break;
+        case "barre":       require_once ('src/jpgraph_bar.php');  break;
+        case "camembert":   require_once ('src/jpgraph_pie.php');  break;
+        default:            $type_graphe = "courbe";
+                            require_once ('src/jpgraph_line.php');  
+                            break;        
+    }
+    chdir($cwd);
     
-    $legende =  explode(";", $legende);  
-    foreach ($legende as $key => $value)  
-            $legende[$key] = utf8_decode($value); 
-    $largeur = (int) $largeur;    if ($largeur<=0) $largeur = 400;
-    $hauteur = (int) $hauteur;    if ($hauteur<=0) $hauteur = 300;
     
-    // Create the graph. These two calls are always required
-    $graph = new Graph($largeur,$hauteur);
-    $graph->SetScale('textlin');
-    
-    // Create the linear plot
-    $bplot = new BarPlot($donnee);
-    $bplot->SetFillColor($couleur);
-    
-    // titre & legende 
-    $graph->title->Set(utf8_decode($titre));
-    if (count($legende)>1) {
-        $graph->xaxis->SetTickLabels($legende);
+    // creation du graphe
+    switch($type_graphe) {
+        case "courbe":      $graph = new Graph($largeur,$hauteur);
+                            $graph->SetScale('textlin');
+                            // Create the linear plot
+                            $plot=new LinePlot($donnee);
+                            $plot->SetColor($couleur);
+                            // titre & legende 
+                            $graph->title->Set(utf8_decode($titre));
+                            if (count($legende)>1) 
+                                $graph->xaxis->SetTickLabels($legende);  
+                            break;
+                            
+        case "barre":       $graph = new Graph($largeur,$hauteur);
+                            $graph->SetScale('textlin');
+                            // Create the linear plot
+                            $plot = new BarPlot($donnee);
+                            $plot->SetFillColor($couleur);    
+                            // titre & legende 
+                            $graph->title->Set(utf8_decode($titre));
+                            if (count($legende)>1)
+                                $graph->xaxis->SetTickLabels($legende);                                                     
+                            break;
+                            
+         case "camembert":  $graph = new PieGraph($largeur,$hauteur);    
+                            // Create the linear plot
+                            $plot = new PiePlot($donnee);
+                            $plot->SetTheme("earth");    
+                            // titre & legende 
+                            $graph->title->Set(utf8_decode($titre));
+                            if (count($legende)>1) 
+                                $plot->SetLegends($legende);        
+                            break;
     }
     
-    // Add the plot to the graph
-    $graph->Add($bplot);
+    // Attacher le trace au graph
+    $graph->Add($plot);  
     
-    // Save the graph   
-    $filename = jpgraph_name_hash("barre",$largeur,$hauteur,$donnee);
-    @unlink($filename); // http://jpgraph.intellit.nl/index.php?topic=4547.msg11823
-    $graph->Stroke($filename);
-    
-    
-    return "<span class='spip_documents jgraph'><img src='$filename' alt='' width='$largeur' height='$hauteur' /></span>";
-
-}
-
-//
-// filtre pour creer des barres horizontales
-//
-function filtre_jpgraph_camembert($str,$donnee="",$legende="",$largeur=400,$hauteur=300,$titre="",$couleur="orange"){
-    // retrouver jpgraph 
-  	$cwd = getcwd();
-  	chdir(realpath(_DIR_JPGRAPH_LIB));
-    require_once ('src/jpgraph.php');
-    require_once ('src/jpgraph_pie.php');
-  	chdir($cwd);
-   
-    // traiter les donnees
-    $donnee =  explode(";", $donnee);    
-    foreach ($donnee as $key => $value)
-         $donnee[$key] = (float) $value;
-    if (count($donnee)<2) 
-        $donnee[] = "1";   // securite pour empecher les erreurs si donnnee pas renseigne  
-    
-    $legende =  explode(";", $legende);  
-    foreach ($legende as $key => $value)  
-            $legende[$key] = utf8_decode($value); 
-    $largeur = (int) $largeur;    if ($largeur<=0) $largeur = 400;
-    $hauteur = (int) $hauteur;    if ($hauteur<=0) $hauteur = 300;
-    
-    // Create the graph. These two calls are always required
-    $graph = new PieGraph($largeur,$hauteur);
-    
-    // Create the linear plot
-    $bplot = new PiePlot($donnee);
-    $bplot->SetTheme("earth");
-    
-    // titre & legende 
-    $graph->title->Set(utf8_decode($titre));
-    if (count($legende)>1) {
-        $bplot->SetLegends($legende);            
-    }
-    
-    // Add the plot to the graph
-    $graph->Add($bplot);
-    
-    // Save the graph   
-    $filename = jpgraph_name_hash("pie",$largeur,$hauteur,$donnee);
+    // export du graphe dans un fichier   
+    $filename = jpgraph_name_hash($type_graphe,$largeur,$hauteur,$donnee);
     @unlink($filename); // http://jpgraph.intellit.nl/index.php?topic=4547.msg11823
     $graph->Stroke($filename);
     
