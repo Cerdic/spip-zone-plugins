018dccb163055371368036cb02dd65a80b974f13
marcimat@free.fr <marcimat@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-10-26 20:13:49 +0000

    Cet include n'est plus utile en spip 2
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@32388 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- authentification/openid/openid_pipelines.php
+++ authentification/openid/openid_pipelines.php
@@ -13,7 +13,6 @@
  */
 function openid_editer_contenu_objet($flux){
 	if ($flux['args']['type']=='auteur') {
-		include_spip('public/assembler');
 		$openid = recuperer_fond('formulaires/inc-openid', $flux['args']['contexte']);
 		$flux['data'] = preg_replace('%(<li class="editer_email(.*?)</li>)%is', '$1'."\n".$openid, $flux['data']);
 	}
