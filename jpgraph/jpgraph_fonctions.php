9e88136389db67b22da1113be112916af53d0a00
olivier.gautier@ac-rouen.fr <olivier.gautier@ac-rouen.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-10-27 16:26:25 +0000

    On complète les possibilités de personnalisation du modèle "courbe". On peut désormais : - définir des marqueurs (forme, taille, couleur de contour et couleur de fond) - définir la couleur de fond - définir un dégradé de 2 couleurs comme couleur de fond - afficher la courbe en marches (lignes verticales et horizontales)
    Au passage, on limite la hauteur et largeur à 1600px pour ne pas faire planter les serveurs.
    Je pense qu'on pourra réduire encore... ?
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@32400 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- jpgraph/jpgraph_fonctions.php
+++ jpgraph/jpgraph_fonctions.php
@@ -21,26 +21,61 @@ function jpgraph_name_hash($type="graph",$largeur,$hauteur,$donnee) {
     return _DIR_IMG."jpgraph/$type-$hash.png";
 }
 
+function jpgraph_nom_marqueur($nom) {
+	
+}
 
 //
 // filtre pour creer des courbes simples
 //
-function filtre_jpgraph($str,$type_graphe="courbe",$donnee="",$legende="",$largeur=400,$hauteur=300,$titre="",$couleur="orange"){
+function filtre_jpgraph($str,
+	$type_graphe="courbe",
+	$donnee="",
+	$legende="",
+	$largeur=400,
+	$hauteur=300,
+	$titre="",
+	$couleur="orange",
+	$couleur_fond="",
+	$fond_degrade="",
+	$epaisseur="",
+	$marqueur_forme="",
+	$marqueur_couleur="",
+	$marqueur_epaisseur="",
+	$marqueur_couleur_fond="",
+	$style=""){
    
     // traiter les donnees
     $type_graphe = strtolower(trim($type_graphe));  // pour pb avec les modeles si du blanc en fin de ligne
-    $couleur = trim($couleur);    
+    $couleur = trim($couleur);
+    $couleur_fond = trim($couleur_fond);
+    $epaisseur = (int) $epaisseur;
+    $marqueur_forme = trim($marqueur_forme);
+    $marqueur_couleur=trim($marqueur_couleur);
+    $marqueur_epaisseur=(int) $marqueur_epaisseur;
+    $marqueur_couleur_fond = trim($marqueur_couleur_fond);
+    $style = trim ($style);
+    
     $donnee =  explode(";", $donnee);    
     foreach ($donnee as $key => $value)
          $donnee[$key] = (float) $value;    
     if (count($donnee)<2) 
         $donnee[] = "1";   // securite pour empecher les erreurs si donnnee pas renseigne  
     
+    if ($fond_degrade) {
+	$fond_degrade = explode(";", $fond_degrade);
+	foreach ($fond_degrade as $key => $value) $fond_degrade[$key] = trim($value);
+	if (count($fond_degrade)<2) {
+		$fond_degrade[0]='white@0.5';
+		$fond_degrade[1]='orange@0.5';
+	}
+    }
+    
     $legende =  explode(";", $legende); 
     foreach ($legende as $key => $value)  
             $legende[$key] = utf8_decode($value);
-    $largeur = (int) $largeur;    if ($largeur<=0) $largeur = 400;
-    $hauteur = (int) $hauteur;    if ($hauteur<=0) $hauteur = 300;
+    $largeur = (int) $largeur;    if (($largeur<=0) OR ($largeur>1600)) $largeur = 400;
+    $hauteur = (int) $hauteur;    if (($hauteur<=0) OR ($hauteur>1600)) $hauteur = 300;
     
     // retrouver jpgraph 
   	$cwd = getcwd();
@@ -63,7 +98,29 @@ function filtre_jpgraph($str,$type_graphe="courbe",$donnee="",$legende="",$large
                             $graph->SetScale('textlin');
                             // Create the linear plot
                             $plot=new LinePlot($donnee);
-                            $plot->SetColor($couleur);
+			    $plot->SetColor($couleur);
+			    if ($style=='marches') $plot->SetStepStyle();
+			    if ($fond_degrade) $plot->SetFillGradient($fond_degrade[0],$fond_degrade[1]);
+			    if ($couleur_fond) $plot->SetFillColor($couleur_fond);
+			    if ($epaisseur) $plot->SetWeight($epaisseur);
+			    if ($marqueur_forme) {
+			    switch($marqueur_forme) {
+			    	case 'carre': $plot->mark->SetType(MARK_SQUARE); break;
+				case 'triangle': $plot->mark->SetType(MARK_UTRIANGLE); break;
+				case 'triangle_bas': $plot->mark->SetType(MARK_DTRIANGLE); break;
+				case 'losange': $plot->mark->SetType(MARK_DIAMOND); break;
+				case 'cercle': $plot->mark->SetType(MARK_CIRCLE); break;
+				case 'cercle_plein': $plot->mark->SetType(MARK_FILLEDCIRCLE); break;
+				case 'croix': $plot->mark->SetType(MARK_CROSS); break;
+				case 'croix_x': $plot->mark->SetType(MARK_X); break;
+				case 'etoile': $plot->mark->SetType(MARK_STAR); break;
+				default: $plot->mark->SetType(MARK_SQUARE); break;
+			}
+			    if ($marqueur_couleur) $plot->mark->SetColor($marqueur_couleur);
+			    if ($marqueur_couleur_fond) $plot->mark->SetFillColor($marqueur_couleur_fond);
+			    if ($marqueur_epaisseur)$plot->mark->SetWidth($marqueur_epaisseur);
+			    }
+			    
                             // titre & legende 
                             $graph->title->Set(utf8_decode($titre));
                             if (count($legende)>1) 
@@ -100,7 +157,7 @@ function filtre_jpgraph($str,$type_graphe="courbe",$donnee="",$legende="",$large
     @unlink($filename); // http://jpgraph.intellit.nl/index.php?topic=4547.msg11823
     $graph->Stroke($filename);
     
-    $titre = str_replace("'","&#039",$titre);    
+    $titre = str_replace("'","&#039",$titre);
     return "<span class='spip_documents jgraph'><img src='$filename' alt='$titre' width='$largeur' height='$hauteur' /></span>";
 
 }
