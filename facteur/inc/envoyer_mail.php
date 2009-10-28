ad8423ba331c2568c4d41524f43d85547e2f0616
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-10-28 14:59:50 +0000

    Le monde n'est pas toujours rose ...
    Il se peut que l'on envoie des mails en texte toujours et il est donc nécessaire de conserver les traitements standards sur le contenu
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@32419 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- facteur/inc/envoyer_mail.php
+++ facteur/inc/envoyer_mail.php
@@ -9,11 +9,12 @@
 		$message_texte	= '';
 		if (is_array($corps)) {
 			$message_html	= $corps['html'];
-			$message_texte	= $corps['texte'];
+			$message_texte	= nettoyer_caracteres_mail($corps['texte']);
 			$pieces_jointes	= $corps['pieces_jointes'];
 		} else {
-			$message_texte	= $corps;
+			$message_texte	= nettoyer_caracteres_mail($corps);
 		}
+		$sujet = nettoyer_titre_email($sujet);
 		$notification = new Facteur($destinataire, $sujet, $message_html, $message_texte);
 		if (!empty($from))
 			$notification->From = $from;
@@ -35,5 +36,24 @@ function nettoyer_titre_email($titre) {
 	return str_replace("\n", ' ', supprimer_tags(extraire_multi($titre)));
 }
 
+// http://doc.spip.org/@nettoyer_caracteres_mail
+function nettoyer_caracteres_mail($t) {
+
+	$t = filtrer_entites($t);
+
+	if ($GLOBALS['meta']['charset'] <> 'utf-8') {
+		$t = str_replace(
+			array("&#8217;","&#8220;","&#8221;"),
+			array("'",      '"',      '"'),
+		$t);
+	}
+
+	$t = str_replace(
+		array("&mdash;", "&endash;"),
+		array("--","-" ),
+	$t);
+
+	return $t;
+}
 
 ?>
