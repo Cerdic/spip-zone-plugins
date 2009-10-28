b2a31647027407bcc632d426679725bd5c097885
fil@rezo.net <fil@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-10-28 22:08:44 +0000

    un dernier bug pour la route : la config ne modifiait pas le _FC_LANCEUR ; en fait on ne peut pas le modifier, mais il suffit de l'effacer
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@32443 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- fastcache/fonds/cfg_fastcache_fonctions.php
+++ fastcache/fonds/cfg_fastcache_fonctions.php
@@ -2,5 +2,8 @@
 
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
-include_spip('creer_fastcache');
-creer_fastcache();
+# code en double, cf spip.php
+$site = str_replace('www.', '', $_SERVER['HTTP_HOST']);
+list($site) = explode(':', $site); // supprimer le :80 (flash)
+define('_FC_LANCEUR', _DIR_RACINE.'tmp/fcconfig_' . $site . '.inc');
+unlink(_FC_LANCEUR); # on ne peut pas le creer depuis l'espace prive, les chemins sont faux !
