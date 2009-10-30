b958db6dda882e14d161e98a69a5e2b9998446d0
renato@rezo.net <renato@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-10-30 23:58:44 +0000

    il Coltellino Svizzero in italiano
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@32510 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ couteau_suisse/lang/couteau_it.php
@@ -0,0 +1,115 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// 2
+	'2pts' => '@objet@&nbsp;:',
+
+	// A
+	'attente_confirmation' => 'In attesa di conferma',
+
+	// C
+	'configurer' => 'Configura il Coltello Svizzero',
+	'connections' => 'Connessioni',
+
+	// D
+	'date_court' => '@jour@/@mois@/@annee@',
+	'derniers_connectes' => 'Ultimi connessi:',
+
+	// E
+	'email_forum' => 'Si prega di inserire l\'indirizzo email!',
+	'explique_spam' => 'Attenzione: hai utilizzato dei tag, dei catarreti o delle parole vietate.<br /><br />Riprova!',
+
+	// L
+	'lutte_spam' => 'Lotta contro lo SPAM',
+
+	// M
+	'maj_actu' => 'Forza l\'aggiornamento',
+	'maj_liste' => 'Elenco dei plugin rilevati:',
+	'maj_maj' => 'Aggiorna il plugin selezionato',
+	'maj_ok' => 'Questo [plugin->@zip@] sembra [aggiornato->@url@].',
+	'maj_rev' => 'Rev.&nbsp;@revision@',
+	'maj_rev_ko' => 'La [revisione remota->@url@] non &egrave; stata trovata.',
+	'maj_rev_ok' => 'La revisione [@revision@->@url@] &egrave; [disponibile->@zip@].',
+	'maj_svn' => 'Questo plugin SVN sembra [aggiornato->@url@].',
+	'maj_verif' => 'Verifica prima l\'archivio:',
+	'maj_verif2' => 'Attenzione: dopo aver cliccato sul tasto qui in basso, verifica attentamente che l\'archivio scaricato corrisponda al plugin da aggiornare.',
+
+	// N
+	'nom_email_forum' => 'Si prega di inserire il nome o l\'indirizzo email!',
+	'nom_forum' => 'Si prega di inserire il nome!',
+	'non_confirmes' => 'Non confermati:',
+
+	// O
+	'objet_article' => 'Articolo',
+	'objet_articles' => 'Articoli',
+	'objet_auteur' => 'Autore',
+	'objet_auteurs' => 'Autori',
+	'objet_breve' => 'Breve',
+	'objet_breves' => 'Brevi',
+	'objet_mot' => 'Parola chiave',
+	'objet_mots' => 'Parole chiave',
+	'objet_petition' => 'Petizione',
+	'objet_petitions' => 'Petizioni',
+	'objet_rubrique' => 'Rubrica',
+	'objet_rubriques' => 'Rubriche',
+	'objet_syndic' => 'Sito',
+	'objet_syndics' => 'Siti',
+
+	// P
+	'page_debut' => 'Prima pagina',
+	'page_fin' => 'Ultima pagina',
+	'page_lien' => 'Pagina @page@ : @title@',
+	'page_precedente' => 'Pagina precedente',
+	'page_suivante' => 'Pagina successiva',
+	'plugin_xml' => 'Raccoglie in un solo plugin una serie di piccole funzioni nuove ed utili a migliorare la gestione del tuo sito SPIP.
+
+Ciascuna di queste funzioni pu&ograve; essere attivata o meno dall\'utente sulla [pagina di amministrazione dei plugin->./?exec=admin_couteau_suisse] ed impostata con un certo numero di variabili: clicca su {{Configurazione}}, poi scegli la scheda {{Coltellino Svizzero}}.
+
+Le categorie disponibili sono:Amministrazione, Miglioramenti tipografici, Scorciatoie tipografiche, Visualizzazione del sito pubblico, Segnaposti, filtri, criteri.
+
+Scopri in questo plugin le tue funzioni preferite: {Sopprimi il numero}, {Formato degli URL}, {Evidenziatori tipografici}, {Virgolette tipografiche}, {Simboli di spunta}, {Lotta contro lo SPAM}, {Mailcrypt}, {Belle URL}, {SPIP e i link... esterni}, {Smileys}, {Un sommario per i tuoi articoli}, {Dividi in pagine e schede}, ecc., ecc.
+
+Non esitare a leggere gli articoli di documentazione del plugin pubblicati su: [spip-contrib.net->http://www.spip-contrib.net/Le-Couteau-Suisse].
+
+Compatibilit&agrave; : SPIP v1.92x e v2.0.x',
+	'plug_actifs' => 'Plugins attivi',
+	'plug_inactifs' => 'Plugins non attivi',
+
+	// S
+	'smileys_dispos' => 'Smiley disponibili:',
+	'sommaire_page' => ',&nbsp;p@page@',
+	'sommaire_page_long' => '&nbsp;page&nbsp;@page@',
+	'sommaire_titre' => 'Sommario',
+	'stats_auteur' => '@icon@ @nom@, l&igrave;&nbsp;@date@',
+	'stats_date' => '@jour@/@mois@/@annee@&nbsp;alle&nbsp;@h@h@m@',
+
+	// T
+	'textes_formatspip' => 'TESTI ORIGINALI NEL FORMATO SPIP',
+	'texte_formatspip' => 'TESTO ORIGINALE NEL FORMATO SPIP',
+	'titre' => 'Coltellino svizzero',
+	'tri_auteurs' => 'ORDINE DEGLI AUTORI',
+	'tri_descendre' => 'Sposta in basso questo autore',
+	'tri_monter' => 'Sposta in alto questo autore',
+
+	// U
+	'urls_propres_erreur' => 'Questo formato non tiene conto delle URL "propres" elencate qui sotto.',
+	'urls_propres_format' => 'il formato attuale delle URL &egrave;: {{&laquo;&nbsp;@format@&nbsp;&raquo;}}. [<span>[Configruazione->@url@]</span>]',
+	'urls_propres_lien' => 'Link pubblico di accesso',
+	'urls_propres_objet' => 'Qui sono elencate le URL proprie memorizzate in database grazie alle quali i visitatori possono navigare sul tuo sito.',
+	'urls_propres_titre' => 'Url proprie',
+
+	// V
+	'variable_vide' => '(Vuoto)',
+	'visiteurs_connectes' => 'Visitatori collegati: @nb@',
+
+	// W
+	'webmestres' => 'Webamaster SPIP'
+);
+
+?>
