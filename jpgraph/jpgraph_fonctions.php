30a5d3fa2d95dad869dcc72748092c2a45f1ce4d
olivier.gautier@ac-rouen.fr <olivier.gautier@ac-rouen.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-10-31 11:36:11 +0000

    Les paramètres ne doivent pas être nommés donnee2, donnee3, etc. car SPIP interprète 2 et 3 comme une variable id On renomme donc tout cela en donneedeux, donneetrois, etc.
    Ajout de la gestion des barres en deux et trois séries de données.
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@32523 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- jpgraph/jpgraph_fonctions.php
+++ jpgraph/jpgraph_fonctions.php
@@ -67,19 +67,19 @@ function filtre_jpgraph($str,
 	$type_graphe="courbe",
 	$titre="",
 	$donnee="",
-	$donnee2="",
-	$donnee3="",
+	$donneedeux="",
+	$donneetrois="",
 	$legende="",
-	$legende2="",
-	$legende3="",
+	$legendedeux="",
+	$legendetrois="",
 	$largeur=400,
 	$hauteur=300,
 	$couleur="orange",
-	$couleur2="",
-	$couleur3="",
+	$couleurdeux="",
+	$couleurtrois="",
 	$marqueur="",
-	$marqueur2="",
-	$marqueur3="")
+	$marqueurdeux="",
+	$marqueurtrois="")
 {
 	
 	  // constantes
@@ -96,19 +96,19 @@ function filtre_jpgraph($str,
     // traiter les parametres en entree
     $type_graphe = strtolower(trim($type_graphe));  // pour pb avec les modeles si du blanc en fin de ligne
     $donnee = jpgraph_traitement_donnees($donnee);
-    if ($donnee2) $donnee2 = jpgraph_traitement_donnees($donnee2);
-    if ($donnee3) $donnee3 = jpgraph_traitement_donnees($donnee3);
+    if ($donneedeux) $donneedeux = jpgraph_traitement_donnees($donneedeux);
+    if ($donneetrois) $donneetrois = jpgraph_traitement_donnees($donneetrois);
     if ($legende) $legende = jpgraph_traitement_legendes($legende);
-    if ($legende2) $legende2 = jpgraph_traitement_legendes($legende2);
-    if ($legende3) $legende3 = jpgraph_traitement_legendes($legende2);
-    $largeur = (int) $largeur;    if (($largeur<=0) OR ($largeur>1600)) $largeur = 400;
-    $hauteur = (int) $hauteur;    if (($hauteur<=0) OR ($hauteur>1600)) $hauteur = 300;
+    if ($legendedeux) $legendedeux = jpgraph_traitement_legendes($legendedeux);
+    if ($legendetrois) $legendetrois = jpgraph_traitement_legendes($legendetrois);
+    $largeur = (int) $largeur;    if (($largeur<=10) OR ($largeur>1600)) $largeur = 400;
+    $hauteur = (int) $hauteur;    if (($hauteur<=10) OR ($hauteur>1600)) $hauteur = 300;
     $couleur = jpgraph_traitement_couleurs($couleur);
-    if ($couleur2) $couleur2 = jpgraph_traitement_couleurs($couleur2);
-    if ($couleur3) $couleur3 = jpgraph_traitement_couleurs($couleur3);
+    if ($couleurdeux) $couleurdeux = jpgraph_traitement_couleurs($couleurdeux);
+    if ($couleurtrois) $couleurtrois = jpgraph_traitement_couleurs($couleurtrois);
     if ($marqueur) $marqueur = jpgraph_traitement_marqueur($marqueur);
-    if ($marqueur2) $marqueur2 = jpgraph_traitement_marqueur($marqueur2);
-    if ($marqueur3) $marqueur3 = jpgraph_traitement_marqueur($marqueur3);
+    if ($marqueurdeux) $marqueurdeux = jpgraph_traitement_marqueur($marqueurdeux);
+    if ($marqueurtrois) $marqueurtrois = jpgraph_traitement_marqueur($marqueurtrois);
 
     
     // retrouver jpgraph 
@@ -152,22 +152,41 @@ function filtre_jpgraph($str,
         case "barre":       $graph = new Graph($largeur,$hauteur);
                             $graph->SetScale('textlin');
                             // Create the linear plot
-                            $plot = new BarPlot($donnee);
+                            $plot1 = new BarPlot($donnee);
                             // style & couleur
-                      			if ($couleur['contour']) $plot->SetColor($couleur['contour']);
+                      			if ($couleur['contour']) $plot1->SetColor($couleur['contour']);
                       			// Le degrade pour les barres est tres specifique (un peu comme le modele de marqueur) et doit etre traite dans une fonction supplementaire, sera fait ulterieurement
                       			// if ($couleur['degrade']) $plot->SetFillGradient($couleur['fond'],$couleur['degrade']);
                       			
                       			//petit patch en attendant d'uniformiser la doc : pour l'instant la doc indique que couleur=blue doit remplir en bleu les barres, alors que l'uniformisation des
                       			// couleurs indiquera plutot qu'il s'agit d'une couleur de contour et non une couleur de fond
-                      			if (($couleur['contour']) AND (!$couleur['fond'])) $plot->SetFillColor($couleur['contour']);
+                      			if (($couleur['contour']) AND (!$couleur['fond'])) $plot1->SetFillColor($couleur['contour']);
                       			//Devra etre probablement supprime ulterieurement, ou alors on garde cela, dans le cas d'oubli de la couleur de fond
                       			
-                      			if ($couleur['fond']) $plot->SetFillColor($couleur['fond']);                             
-                            // titre & legende 
+                      			if ($couleur['fond']) $plot1->SetFillColor($couleur['fond']);                             
+                           $group_plot[0]= $plot1;
+			   
+			   if ($donneedeux) {
+				$plot2 = new BarPlot($donneedeux);
+				if ($couleurdeux['contour']) $plot2->SetColor($couleurdeux['contour']);
+				if (($couleurdeux['contour']) AND (!$couleurdeux['fond'])) $plot2->SetFillColor($couleurdeux['contour']);
+                      		if ($couleurdeux['fond']) $plot2->SetFillColor($couleurdeux['fond']);
+				$group_plot[1]= $plot2;
+			   }
+
+			   if ($donneetrois) {
+				$plot3 = new BarPlot($donneetrois);
+				if ($couleurtrois['contour']) $plot3->SetColor($couleurtrois['contour']);
+				if (($couleurtrois['contour']) AND (!$couleurtrois['fond'])) $plot3->SetFillColor($couleurtrois['contour']);
+                      		if ($couleurtrois['fond']) $plot3->SetFillColor($couleurtrois['fond']);
+				$group_plot[2]= $plot3;
+			   }
+
+			    $plot = new  GroupBarPlot ($group_plot);
+			    // titre & legende 
                             $graph->title->Set(utf8_decode($titre));
                             if (count($legende)>1)
-                                $graph->xaxis->SetTickLabels($legende);                                                     
+                                $graph->xaxis->SetTickLabels($legende);                                                
                             break;
                             
          case "camembert":  $graph = new PieGraph($largeur,$hauteur);    
