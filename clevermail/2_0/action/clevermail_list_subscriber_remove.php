12d8268ca2b07339e24b9fc91d44ca4faf62a060
nicolas@hoizey.com <nicolas@hoizey.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-11-02 09:01:31 +0000

    menage
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@32605 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- clevermail/2_0/action/clevermail_list_subscriber_remove.php
+++ clevermail/2_0/action/clevermail_list_subscriber_remove.php
@@ -14,7 +14,6 @@ function action_clevermail_list_subscriber_remove_dist() {
       sql_delete("spip_cm_posts_queued", "sub_id = ".intval($abonnement['sub_id']));
       if (sql_countsel("spip_cm_lists_subscribers", "sub_id=".intval($abonnement['sub_id'])) == 0) {
       	// No more subscription, subscriber address is removed
-        //sql_delete("spip_cm_subscribers", "sub_id = ".intval($abonnement['sub_id']));
         sql_updateq("spip_cm_subscribers", array('sub_email' => md5($abonne).'@example.com'), "sub_id = ".intval($abonnement['sub_id']));
       }
       	spip_log('Suppression du l\'abonnement de « '.$abonne.' » à la liste « '.$liste.' » (id='.$abonnement['lst_id'].')', 'clevermail');
