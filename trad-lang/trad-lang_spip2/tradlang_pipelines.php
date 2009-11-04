0b526182a7ee620e194fbf87113b79894f555fbe
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-11-04 22:37:29 +0000

    On s'insère dans le gouverneur et dans la liste des objets révisables pour les traductions des chaines de langue
    On ajoute des petites icones
    
    On ajoute également l'insertion de id comme titre dans table_titre
    
    On déclare tradlang comme surnom pour tradlang sinon trouver_table nous donnait tradlangs et ca cassait
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@32672 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- trad-lang/trad-lang_spip2/tradlang_pipelines.php
+++ trad-lang/trad-lang_spip2/tradlang_pipelines.php
@@ -34,4 +34,38 @@ function tradlang_declarer_tables_objets_surnoms($flux){
 	return $flux;
 }
 
+/**
+ * Insertion dans le pipeline gouverneur_infos_tables_versions
+ * (utile pour le plugin revisions en 2.1)
+ * Permet de gérer les révisions sur les chaines de langue
+ *
+ * @param object $array
+ * @return
+ */
+function tradlang_gouverneur_infos_tables($array){
+	$array['spip_tradlang'] = array(
+								'table_objet' => 'tradlang',
+								'type' => 'tradlang',
+								'url_voir' => '',
+								'texte_retour' => '',
+								'url_edit' => '',
+								'texte_modifier' => '',
+								'icone_objet' => 'spip_lang',
+								'texte_unique' => 'tradlang:chaine_langue',
+								'texte_multiple' => 'tradlang:chaines_langue',
+								'champs_versionnes' => array('str','comm', 'status')
+							);
+	return $array;
+}
+
+/**
+ * Insertion dans le pipeline revisions_liste_objets du plugin revisions (2.1)
+ * Definir la liste des tables possibles
+ * @param object $array
+ * @return
+ */
+function tradlang_revisions_liste_objets($array){
+	$array['tradlang'] = 'tradlang:chaines_langue';
+	return $array;
+}
 ?>
