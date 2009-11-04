719ab9c257f202695ac81b16571adeed9383873b
alesk@spip.org <alesk@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-11-04 13:57:47 +0000

    Ajout du critère is_null
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@32650 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- agregateurs_sql/public/agregateurs_sql_criteres.php
+++ agregateurs_sql/public/agregateurs_sql_criteres.php
@@ -87,6 +87,23 @@ function critere_stats($idb, &$boucles, $crit){
 	), $idb, $boucles, $crit);
 }
 
+
+
+/**  Critere {is_null champ} - Franck Van Lancker - */
+function critere_is_null($idb, &$boucles, $crit)
+{
+	
+	$not = ($crit->not ? ' NOT' : '');
+	$param = "IS$not NULL";
+	$boucle = &$boucles[$idb];
+	$field = $crit->param[0][0]->texte;
+	
+	if( preg_match('~\s~si', $field) )
+		erreur_squelette(_T('zbug_info_erreur_squelette'), $crit->op);
+	
+	$boucle->where[]= array("'$param'", "'$boucle->id_table." . "$field'", "''");
+}
+
 /* $func : array(FUNC => balise) */
 function calcul_critere_fonctions($func, $idb, &$boucles, $crit) {
 	$boucle = &$boucles[$idb];
