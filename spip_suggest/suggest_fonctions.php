138d2aef489a7950bb1db8442f8e41a6a83e6c0c
prigent.yohann@gmail.com <prigent.yohann@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-11-09 20:34:04 +0000

    correction du bug singulier pluriel, pas encore fonctionnel
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@32787 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spip_suggest/suggest_fonctions.php
+++ spip_suggest/suggest_fonctions.php
@@ -14,43 +14,28 @@ function spip_suggest_complete ($q) {
 	foreach (array_map('strtolower',$res) as $key=>$value) {
 			echo "$key|$value\n";
 	}
-	/*include_spip('inc/recherche');
-	if ($s = sql_query("SHOW CREATE TABLE ".table_objet_sql('article'))
-	AND $t = sql_fetch($s)
-	AND $create = array_pop($t)
-	AND preg_match_all('/,\s*FULLTEXT\sKEY.*`(.*)`\s+[(](.*)[)]/i', $create, $keys, PREG_SET_ORDER)) {
-		$cles = array();
-		foreach ($keys as $key) {
-			$cle = $key[2];
-			if ($prefix)
-				$cle = preg_replace(',`.*`,U', $prefix.'.$0', $cle);
-			$cles[$key[1]] = $cle;
-		}
-	}
-	echo fulltext_keys('rubrique', 't');
-	var_dump($s);*/
 }
 function spip_suggest_insert_head ($flux) {
 	$flux .= '<script src="'.find_in_path("javascript/jquery.autocomplete.js").'" type="text/javascript"></script>';
 	$flux .= '
 <script>
-  $(document).ready(function(){
-  function formatItem(row) {
-if (row[0]=="1") {
-return row[0] + " (<strong>" + row[1] + " r&eacute;sultat</strong>)";
-}
-else {
-return row[0] + " (<strong>" + row[1] + " r&eacute;sultats</strong>)";
-} 
- }
-   $("#recherche").autocomplete("'.generer_url_public("suggest").'", {
-width: 260,
-matchContains: true, 
-selectFirst: false,
-formatItem: formatItem
-}); 
-  });
-  </script>';
+	$(document).ready(function(){
+		function formatItem(row) {
+			if (row[1] == 1) {
+				return row[0] + " (<strong>" + row[1] + " r&eacute;sultat</strong>)";
+			}
+			else if (row[1] > 1) {
+				return row[0] + " (<strong>" + row[1] + " r&eacute;sultats</strong>)";
+			} 
+		 }
+		$("#recherche").autocomplete("'.generer_url_public("suggest").'", {
+			width: 260,
+			matchContains: true, 
+			selectFirst: false,
+			formatItem: formatItem
+		}); 
+	});
+</script>';
   $flux .= '<link rel="stylesheet" href="'.find_in_path("javascript/jquery.autocomplete.css").'" type="text/css" media="all" />';
   return $flux;
 }
