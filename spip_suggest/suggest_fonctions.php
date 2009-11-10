d000964bc4479880436ce7f87f26cb3b3284981d
prigent.yohann@gmail.com <prigent.yohann@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-11-10 23:24:16 +0000

    SPIP Suggest dans les bacs en version stable
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@32802 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spip_suggest/suggest_fonctions.php
+++ spip_suggest/suggest_fonctions.php
@@ -1,13 +1,26 @@
 <?php
 function spip_suggest_complete ($q) {
 	define('_MYSQL_DIR', '/var/lib/mysql/');
+	// lire la base de donnees
 	$s = lire_config('spip_suggest/db_name');
-	$services = array($s => array('spip_articles' => array('num' => 7)));
-	$t = 'spip_articles';
-	$num = $services[$s][$t]['num'];
-	# query a nettoyer (charset...)
+	// lire les cles fulltext
+	$k = lire_config('spip_suggest/db_keys');
+
+	$tout = array();
+	// on parse les donnes pour stocker tout dans un tableau
+	foreach(array_filter(explode(',', $k)) as $table) {
+		// securite
+		$table = explode(':', str_replace(' ', '', $table));
+		$tout[$table[0]][] = $table[1];
+	}
+	// on execute le programme
+	foreach ($tout as $key=>$value) {
+		foreach($value as $cle) {
+			exec($c = 'myisam_suggest '._MYSQL_DIR.$s.'/'.$key.' '.$cle.' '.escapeshellarg($q), $lines);
+		}
+	}
 	
-	exec($c = 'myisam_suggest '._MYSQL_DIR.$s.'/'.$t.' '.$num.' '.escapeshellarg($q), $lines);
+	// les resultats maintenant
 	$res = array();
 	foreach (array_map('strtolower',$lines) as $line)
 		$res[trim($line)]++;
@@ -16,9 +29,9 @@ function spip_suggest_complete ($q) {
 	}
 }
 function spip_suggest_insert_head ($flux) {
-	$flux .= '<script src="'.find_in_path("javascript/jquery.autocomplete.js").'" type="text/javascript"></script>';
+	$flux .= '<script type="text/javascript" src="'.find_in_path("javascript/jquery.autocomplete.js").'" type="text/javascript"></script>';
 	$flux .= '
-<script>
+<script type="text/javascript">
 	$(document).ready(function(){
 		function formatItem(row) {
 			if (row[1] == 1) {
@@ -28,8 +41,8 @@ function spip_suggest_insert_head ($flux) {
 				return row[0] + " (<strong>" + row[1] + " r&eacute;sultats</strong>)";
 			} 
 		 }
-		$("#recherche").autocomplete("'.generer_url_public("suggest").'", {
-			width: 260,
+		$("'.lire_config("spip_suggest/suggest_selecteur", "#recherche").'").autocomplete("'.generer_url_public("suggest").'", {
+			width: '.lire_config("spip_suggest/suggest_width", 205).',
 			matchContains: true, 
 			selectFirst: false,
 			formatItem: formatItem
