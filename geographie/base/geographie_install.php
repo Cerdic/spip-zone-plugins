a707d1b665ab2a52cc2213eb98930bee355f8171
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-11-12 21:25:32 +0000

    Mise à jour de la base (devrait fonctionner)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@32873 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- geographie/base/geographie_install.php
+++ geographie/base/geographie_install.php
@@ -15,6 +15,16 @@
 				}
 				else return;
 			}
+			else if ($current_version<0.2){
+				if (include_spip('base/geographie')){
+					sql_drop_table("spip_geo_pays");
+					creer_base();
+					echo "Mise &agrave; des pays<br/>";
+					reimport_pays();
+					ecrire_meta($nom_meta_base_version,$current_version=0.2,'non');
+				}
+				else return;
+			}
 			ecrire_metas();
 		}
 	}
@@ -28,4 +38,10 @@
 		ecrire_metas();
 	}
 
+	function reimport_pays(){
+		include_spip('imports/pays');
+		include_spip('inc/charset');
+		foreach($GLOBALS['liste_pays'] as $k=>$p)
+			sql_insertq('spip_geo_pays',array('id_pays'=>$k,'nom'=>unicode2charset(html2unicode($p))));
+	}
 ?>
