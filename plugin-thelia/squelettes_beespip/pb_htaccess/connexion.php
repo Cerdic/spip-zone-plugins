1a8fea9983886111170fd2bf7174ca799055630e
apachot@openstudio.fr <apachot@openstudio.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-11-12 13:29:02 +0000

    suppression des squelettes beespip non maintenus
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@32850 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- plugin-thelia/squelettes_beespip/pb_htaccess/connexion.php
+++
@@ -1,7 +0,0 @@
-<?php
-$nouvelleAdresse='spip.php?page=connexion_thelia'; 
-if (isset($_SERVER['QUERY_STRING'])&&($_SERVER['QUERY_STRING']!=''))
- 	$nouvelleAdresse.='&'.substr($_SERVER['QUERY_STRING'],0,2048); //Récupère les paramètres
-header('Location: '.$nouvelleAdresse); //Redirection HTTP
-exit;
-?>
