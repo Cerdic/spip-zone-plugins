4c13702a6f260a968a6e4a83e4faadcd723cf199
apachot@openstudio.fr <apachot@openstudio.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-11-13 19:35:03 +0000

    Vers une authentification unique spip et thélia
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@32902 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- plugin-thelia/spip_thelia_mes_options.php
+++ plugin-thelia/spip_thelia_mes_options.php
@@ -8,5 +8,6 @@ if (!defined('_DIR_PLUGIN_SPIP_THELIA')){
 	define('_DIR_PLUGIN_SPIP_THELIA',(_DIR_PLUGINS.end($p)));
 }
 include_spip('base/spip_thelia_produits_associes');
-
+$GLOBALS['liste_des_authentifications']['thelia'] = 'thelia';
+$GLOBALS['thelia_statut_nouvel_auteur'] = '1comite';
 ?>
