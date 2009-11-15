92ac8cd98cdeae830975c9c052c3c2bd44abac5e
devmed@hotmail.fr <devmed@hotmail.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-11-15 09:56:12 +0000

    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@32957 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ skiplink/lang/skiplink_ar.php
@@ -0,0 +1,24 @@
+﻿<?php
+
+// This is a SPIP-forum module file  --  Ceci est un fichier module de SPIP-forum
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+'go_content' => 'تنقل إلى المحتوى',
+'go_nav' => 'تنقل إلى القائمة',
+'go_search' => 'تنقل إلى محرك البحث',
+
+'ancre_haute' => 'أعلى',
+'ancre_basse' => 'أسفل',
+
+'haut' => 'أعلى',
+'remonter' => 'طلوع إلى أعلى الصفحة',
+'bas' => 'أسفل',
+'descendre' => 'نزول إلى أسفل الصفحة',
+
+// Configuration CFG
+'go_plus_label' => 'إضافة وصلة إلى المقال رقم :'
+
+);
+
+?>
