0c393f3bcfd565c8738e291f6c85cc5c28eb8840
devmed@hotmail.fr <devmed@hotmail.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-11-15 12:27:46 +0000

    traduction arabe du plugin
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@32959 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ rss_article/lang/rssarticle_ar.php
@@ -0,0 +1,31 @@
+﻿<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+  // A
+  'article_origine' => 'هذا المقال منقول من موقع', 
+
+  // C
+  'citer_source' => 'علق على أصل المقال المستورد',
+  'citer_source_oui' => 'إدماج عنوان موقع المقال الأصلي في المقال المستورد',
+  'copie_logo' => 'إستنساخ صورة الموقع كصورة للمقال المستورد',
+
+  // S
+  'statut_article_importe' => 'حالة المقالات المستوردة',
+  'suivi_syndic' => 'تتبع التصديقات',
+  
+  // E
+  'email_alerte' => 'إنذار بالبريد الإلكتروني عند كل تصديق في مقال؟',
+  'email_alerte_email' => 'إذ نعم على أي عنوان يرسل الإنذار؟',
+  
+  // I
+  'install_rssarticle' => 'تم إنشاء الطاولة  --- spip_articles_syndic'
+  
+
+);
+
+
+?>
