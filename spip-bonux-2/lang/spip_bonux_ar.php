223b7047c72e280bc5e9fd190ed5b2041d3665d3
devmed@hotmail.fr <devmed@hotmail.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-11-18 02:33:06 +0000

    abra kadabra! et hop traduit en arabe..
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@33049 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ spip-bonux-2/lang/spip_bonux_ar.php
@@ -0,0 +1,23 @@
+﻿<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+'afficher_calendrier' => 'إظهار الأجندة',
+'annee_precedente' => 'السنة الفائتة',
+'annee_suivante' => 'السنة القادمة',
+'annuler_recherche' => 'إلغاء البحث',
+
+'bouton_fermer' => "إغلاق",
+
+'mois_precedent' => 'الشهر الفائت',
+'mois_suivant' => 'الشهر القادم',
+'id_rapide' => 'إضافة سريعة',
+'pages' => 'الصفحات',
+
+
+);
+
+
+?>
