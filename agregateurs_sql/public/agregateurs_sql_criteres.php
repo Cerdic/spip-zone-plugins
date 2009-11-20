e45af43009f9d510b91a08de0639bf8b9bf76f4f
booz@rezo.net <booz@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-11-20 15:55:56 +0000

    le plugin devient completement autonome de bonux vu qu'il fait la meme chose differement (suite)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@33093 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- agregateurs_sql/public/agregateurs_sql_criteres.php
+++ agregateurs_sql/public/agregateurs_sql_criteres.php
@@ -15,27 +15,6 @@ function critere_is_null($idb, &$boucles, $crit)
 	$boucle->where[]= array("'$param'", "'$boucle->id_table." . "$field'", "''");
 }
 
-/* $func : array(FUNC => balise) */
-function calcul_critere_fonctions($func, $idb, &$boucles, $crit) {
-	$boucle = &$boucles[$idb];
-	$_fusion = calculer_liste($crit->param[1], array(), $boucles, $boucle->id_parent);
-
-	$params = $crit->param;
-	$champ = reset($params);
-	$champ = $champ[0]->texte;
-
-	// option DISTINCT {compte DISTINCT(id_article) }
-	$filter="";
-	if (preg_match('/^([a-zA-Z]+)\(\s*([a-zA-Z_]+)\s*\)$/', trim($champ), $r)) {
-		$filter = $r[1]; // DISTINCT
-		$champ = $r[2]; // id_article
-	}
-	
-	$sel = $filter ? "$filter($champ)" : $champ;
-	foreach ($func as $f => $as) {
-		$boucle->select[]= "$f($sel) AS $as" . "_$champ";
-	}
-}
 
 
 ?>
