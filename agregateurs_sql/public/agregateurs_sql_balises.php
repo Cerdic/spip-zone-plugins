be48d44693b7743a71a0d3a551b22e4f042e7fdd
booz@rezo.net <booz@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-11-20 13:51:32 +0000

    compatibilite spip-bonux retablie
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@33091 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- agregateurs_sql/public/agregateurs_sql_balises.php
+++ agregateurs_sql/public/agregateurs_sql_balises.php
@@ -1,67 +1,13 @@
 <?php
 /**
- * D'apres le Plugin Spip-Bonux
- * Le plugin qui lave plus SPIP que SPIP
- * (c) 2008 Mathieu Marcillaud, Cedric Morin, Romy Tetue
+ * Balise #EXPRESSION
+ * 
+ * (c) 2009 Renato
  * Licence GPL
  *
  */
 
-/**
- * Balise #COMPTEUR associee au critere compteur
- *
- * @param unknown_type $p
- * @return unknown
- */
-function balise_COMPTEUR_dist($p) {
-	return calculer_balise_criteres('compteur', $p);
-}
 
-/** Balise #SOMME associee au critere somme */
-function balise_SOMME_dist($p) {
-	return calculer_balise_criteres('somme', $p);
-}
-
-/** Balise #COMPTE associee au critere compte */
-function balise_COMPTE_dist($p) {
-	return calculer_balise_criteres('compte', $p);
-}
-
-/** Balise #MOYENNE associee au critere moyenne */
-function balise_MOYENNE_dist($p) {
-	return calculer_balise_criteres('moyenne', $p);
-}
-
-/** Balise #MINIMUM associee au critere moyenne */
-function balise_MINIMUM_dist($p) {
-	return calculer_balise_criteres('minimum', $p);
-}
-
-/** Balise #MAXIMUM associee au critere moyenne */
-function balise_MAXIMUM_dist($p) {
-	return calculer_balise_criteres('maximum', $p);
-}
-
-/** Balise #STATS associee au critere stats
- * #STATS{id_article,moyenne}
- */
-function balise_STATS_dist($p) {
-	if (isset($p->param[0][2][0])
-	AND $nom = ($p->param[0][2][0]->texte)) {
-		return calculer_balise_criteres($nom, $p, 'stats');
-	}
-	return $p;
-}
-
-function calculer_balise_criteres($nom, $p, $balise="") {
-	$p->code = '';
-	$balise = $balise ? $balise : $nom;
-	if (isset($p->param[0][1][0])
-	AND $champ = ($p->param[0][1][0]->texte)) {
-		return rindex_pile($p, $nom."_$champ", $balise);
-	}
-  return $p;
-}
 
 function balise_SUM($p) {
   $t = $p->param[0][1][0]->texte;
