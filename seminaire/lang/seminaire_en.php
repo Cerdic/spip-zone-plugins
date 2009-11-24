ba6c35a95d647b2c207d191f646d0f35cfc71022
amaury.adon@laposte.net <amaury.adon@laposte.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-11-24 21:02:18 +0000

    commit initial
    Plugin fait pour gérer les séminaires d'un labo de maths mais il devrait pouvoir servir aussi pour une galerie d'art ou tout autre chose en changeant les mots clés.
    
    S'appuie fortement sur les conseils et le travail de beaucoup de monde sur le channel irc. Merci les gens !
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@33236 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ seminaire/lang/seminaire_en.php
@@ -0,0 +1,77 @@
+<?php
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+    // entete.html
+
+    'credits'           =>      'Credits',
+    'aller_menu'        =>      'Go to menu',
+    'aller_recherche'   =>      'Go to search',
+    'aller_contenu'     =>      'Go to content',
+    'aller_navigation'  =>      'Go to navigation',
+    'presentation_cnrs' =>      'About the CNRS',
+    'url_presentation_cnrs' =>  'http://www2.cnrs.fr/en/8.htm',
+    'le_cnrs'           =>      'The CNRS',
+    'sites_cnrs'        =>      'The CNRS websites',
+    'autres_sites_cnrs' =>      'Other CNRS websites',
+    'url_autres_sites_cnrs' =>  'http://www2.cnrs.fr/en/643.htm',
+    'menu'              =>      'Menu',
+    'sur_ce_site'       =>      'On this website',
+    'sur_web_cnrs'      =>      'On the whole CNRS Web',
+    'url_cnrs_rechercher' =>    'http://www.cnrs.fr/rechercher/english.php',
+
+    // pied.html
+
+    'imprimer'          =>      'Print this page',
+    'nous_contacter'    =>      'Contact us',
+    'fil_rss_site'      =>      'RSS feed',
+
+    // article-6.html
+
+    'bibliographie'     =>      'Bibliography / Publications',
+    'blog_personnel'    =>      'Personal blog',
+    'post_scriptum'     =>      'Postscript',
+    'documents_joints'  =>      'Attached documents',
+
+    // auteur.html
+
+    'envoyer_un_message' =>     'Send a message',
+    'envoyer'           =>      'Send',
+    'votre_mail'        =>      'Your email',
+    'sujet_message'     =>      'Message subject',
+    'texte_message'     =>      'Message body',
+
+    // plan.hmtl
+
+    'articles_et_sous_rubriques' =>  'Articles and subsections',
+
+    // recherche.html
+
+    'resultats_recherche_sur'    =>  'Search results for ',
+    'articles_trouves'           =>  'Articles found',
+    'aucun_article_trouve'       =>  'No article found',
+    'rubriques_trouvees'         =>  'Sections found',
+    'aucune_rubrique_trouvee'    =>  'No section found',
+    'breves_trouvees'            =>  'No news item found',
+    'sites_web_trouves'          =>  'Websites found',
+    'messages_trouves'           =>  'Messages found',
+    'mots_cles_articles_trouves' =>  'Keywords found and related articles',
+
+    // rubrique-20.html
+
+    'derniers_billets_publies'   =>  'Recent posts',
+    'categories'                 =>  'Categories',
+    'archives'                   =>  'Archives',
+    'fil_rss_blogue'             =>  'RSS feed for this blog',
+
+    // rubrique=20.html
+
+    'liste_blogues_dispos'       =>  'Available blogs on this website',
+
+    // sommaire.html
+
+    'lire_la_suite'              =>  'Read more',    
+
+);
+
+
+?>
---
+++ seminaire/lang/seminaire_fr.php
@@ -0,0 +1,29 @@
+<?php
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	//pour le formulaire de séminaires
+	'name'						 => 	'Nom de l&rsquo;intervenant (Name)',
+	'origin'					 => 	'Institut d&rsquo;origine de l&rsquo;intervenant',
+	'abstract'					 => 	'Résumé de l&rsquo;intervention (abstract)',
+	'notes'						 =>		'Notes de dernières minutes',
+	'lieu'						 =>		'Lieu',
+	'precisions_name'			 =>		'le nom sera affiché dans le calendrier et dans le mini calendrier',
+	'precisions_origin'			 =>		'N&rsquo;apparaît que dans les détails de l&rsquo;événement',
+	'precisions_abstract'		 =>		'Ne pas oublier les balises <math></math> si on veut afficher du LaTeX (ex : <math>$\frac{1}{2}$</math>)',
+	'precisions_notes'			 =>		'N&rsquo;apparaît que dans les détails de l&rsquo;événement',
+	'apreciser'					 =>		'A pr&eacute;ciser',
+	'plus'						 =>		'En savoir plus',
+	'a_venir'					 =>		'Les prochains s&eacute;minaires',
+	'attention_type'			 =>		'Le choix du type d&rsquo;&eacute;&eacute;nement d&eacute;termine le tri qui lui sera appliqu&eacute;.',
+	'oubli_titre'				 =>		'Ne pas oublier de mettre le titre de l&rsquo;&eacute;v&eacute;nement',
+	
+	//pour les groupes de mots clés
+	'mots_categorie_kitcnrs'	 => 	'Les mots clés à affecter aux articles pour que les événements soient encore mieux triés',
+	'mots_cles_techniques_kitcnrs'=>	'Les mots clés pour repérer plus finement les différents types d&rsquo;événements',
+	'type'						 =>		'Type d&rsquo;&eacute;v&eacute;nement',
+	'evenements_depuis_debut'	 =>		'Archives',	
+	'creer_evenement'			 =>		'Ajouter un événement', 
+	'erreur_install_groupe_categories' => 'erreur &agrave; la cr&eacute;ation du groupe',
+	'mots_cles_categories'		 =>		'Les différentes cat&eacute;gories de s&eacute;minaires, groupes de travail et &eacute;v&eacute;nements exceptionnels',
+);
+?>
