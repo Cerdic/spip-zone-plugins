2bae8652d050fe349ae234036a3b3f3075454322
apachot@openstudio.fr <apachot@openstudio.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-11-24 10:46:24 +0000

    Suite de la mise en zpip : - nettoyage des fichiers de zpip non surchargés - ajout des pages pmb : auteur, éditeur, collection
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@33205 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ spip-pmb/2_0_0/squelettes_zpip/lang/zpip_fr.php
@@ -0,0 +1,41 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/_stable_/acces_restreint/lang/
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+
+	// B
+
+	// C
+	'conception_graphique_par' => 'Habillage visuel (c)',
+	'commentaire' => 'commentaire',
+	'commentaires' => 'commentaires',
+
+	// D
+
+	// I
+
+	// L
+	'lire_la_suite' => 'Lire la suite',
+	'lire_la_suite_de' => ' de ',
+
+	// M
+
+	// P
+
+	// R
+
+	// S
+	'sous_licence' => 'sous Licence',
+
+	// T
+
+	// V
+
+	// Z
+);
+
+?>
