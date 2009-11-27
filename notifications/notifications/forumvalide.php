1cb11d754c28e9aafbb5cf841a2c7031695fde66
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-11-27 13:15:40 +0000

    generalisation de forum_valide pour eviter le fork propre au plugin
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@33318 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- notifications/notifications/forumvalide.php
+++ notifications/notifications/forumvalide.php
@@ -21,17 +21,20 @@ function notifications_forumvalide_dist($quoi, $id_forum, $options) {
 
 	$t = sql_fetsel("*", "spip_forum", "id_forum=".intval($id_forum));
 	if (!$t
-	  OR !$id_article = $t['id_article']
 		// forum sur un message prive : pas de notification ici (cron)
 		OR @$t['statut'] == 'perso')
 		return;
 
-	$s = sql_getfetsel('accepter_forum','spip_articles',"id_article=" . $t['id_article']);
-	if (!$s)  $s = substr($GLOBALS['meta']["forums_publics"],0,3);
+	// plugin notification si present
+	$prevenir_auteurs = isset($GLOBALS['notifications']['prevenir_auteurs']) AND $GLOBALS['notifications']['prevenir_auteurs'];
+	// sinon voie normale
+	if ($t['id_article'] AND !$prevenir_auteurs){
+		$s = sql_getfetsel('accepter_forum','spip_articles',"id_article=" . $t['id_article']);
+		if (!$s)  $s = substr($GLOBALS['meta']["forums_publics"],0,3);
 
-	if (strpos(@$GLOBALS['meta']['prevenir_auteurs'],",$s,")===false
-	AND @$GLOBALS['meta']['prevenir_auteurs'] !== 'oui') // compat
-		return;
+		$prevenir_auteurs = (strpos(@$GLOBALS['meta']['prevenir_auteurs'],",$s,")!==false
+			OR @$GLOBALS['meta']['prevenir_auteurs'] === 'oui'); // compat
+	}
 
 	include_spip('inc/texte');
 	include_spip('inc/filtres');
@@ -39,20 +42,23 @@ function notifications_forumvalide_dist($quoi, $id_forum, $options) {
 
 	// Qui va-t-on prevenir ?
 	$tous = array();
-	// Ne pas ecrire au posteur du message, ni au moderateur qui active le mail,
+	// Ne pas ecrire au posteur du message, ni au moderateur qui valide le forum,
 	$pasmoi = array($t['email_auteur'],$GLOBALS['visiteur_session']['email']);
 
-	// 1. Les auteurs de l'article qui n'ont pas le droit de le moderer
-	// (les autres l'ont recu plus tot)
+	// 1. Les auteurs de l'article ; si c'est un article, ceux qui n'ont
+	// pas le droit de le moderer (les autres l'ont recu plus tot)
+	if ($t['id_article']
+	AND $prevenir_auteurs) {
+		$result = sql_select("auteurs.*","spip_auteurs AS auteurs, spip_auteurs_articles AS lien","lien.id_article=".intval($t['id_article'])." AND auteurs.id_auteur=lien.id_auteur");
 
-	$result = sql_select("auteurs.id_auteur, auteurs.email", "spip_auteurs AS auteurs, spip_auteurs_articles AS lien", "lien.id_article=".sql_quote($t['id_article'])." AND auteurs.id_auteur=lien.id_auteur");
-
-	while ($qui = sql_fetch($result)) {
-		if (!autoriser('modererforum', 'article', $t['id_article'], $qui['id_auteur']))
+		while ($qui = sql_fetch($result)) {
+			if (!autoriser('modererforum', 'article', $t['id_article'], $qui['id_auteur']))
 				$tous[] = $qui['email'];
-		else
+			else
 				// Ne pas ecrire aux auteurs deja notifies precedemment
 				$pasmoi[] = $qui['email'];
+
+		}
 	}
 
 
