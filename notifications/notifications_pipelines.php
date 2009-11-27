79ab4ae827dfd4ad1588a4ab3c2943c8c24359e6
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-11-27 10:43:09 +0000

    passage en pipeline de la notifications aux auteurs d'un article lors de la validation
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@33313 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- notifications/notifications_pipelines.php
+++ notifications/notifications_pipelines.php
@@ -7,67 +7,30 @@
  */
 
 
-function notifier_publication_auteurs_article($id_article) {
-	include_spip('base/abstract_sql');
-	$nom_site_spip = nettoyer_titre_email($GLOBALS['meta']["nom_site"]);
-	$suivi_edito = $GLOBALS['meta']["suivi_edito"];
+/**
+ * Ajouter des destinataires dans une notification en lot
+ *
+ * @param array $flux
+ * @return array
+ */
+function notifications_notifications_destinataires($flux) {
+	$quoi = $flux['args']['quoi'];
+	if ($quoi=='instituer_article'
+	  AND $GLOBALS['notifications']['prevenir_auteurs_articles']){
+		$id_article = $flux['args']['id'];
+		$options = $flux['args']['options'];
 
-	if ($GLOBALS['notifications']['prevenir_auteurs_articles']) {
-		$row = sql_fetsel("*", "spip_articles", "id_article = $id_article");
-		if ($row) {
+		include_spip('base/abstract_sql');
 
-			$l = lang_select($row['lang']);
+		// Qui va-t-on prevenir en plus ?
+		$result_email = sql_select("auteurs.email", "spip_auteurs AS auteurs, spip_auteurs_articles AS lien", "lien.id_article=".intval($id_article)." AND auteurs.id_auteur=lien.id_auteur");
 
-			// URL de l'article
-			if (function_exists('generer_url_entite_absolue')) {
-				$url = generer_url_entite_absolue($id_article, 'article');
-			} else {
-				charger_generer_url(false);
-				$url = url_absolue(suivre_lien(_DIR_RACINE,generer_url_article($id_article, '','', 'publie')));
-			}
-
-			$titre = nettoyer_titre_email($row['titre']);
-
-			$sujet = _T('info_publie_1', array('nom_site_spip' => $nom_site_spip, 'titre' => $titre));
-			$courr = _T('info_publie_2')."\n\n";
-
-			$nom = $GLOBALS['visiteur_session']['nom'];
-			$nom = trim(supprimer_tags(typo($nom)));
-			$courr .= _T('info_publie_01', array('titre' => $titre, 'connect_nom' => $nom))
-				. "\n\n"
-				. "-> " . $url
-				. "\n";
-
-		// Qui va-t-on prevenir ?
-			$tous = array();
-
-			$result_email = sql_select("auteurs.email", "spip_auteurs AS auteurs, spip_auteurs_articles AS lien", "lien.id_article=$id_article AND auteurs.id_auteur=lien.id_auteur");
-
-			while ($qui = sql_fetch($result_email)) {
-				$tous[] = $qui['email'];
-			}
-
-			// Nettoyer le tableau
-			// Ne pas ecrire au posteur du message !
-			$destinataires = array();
-			foreach ($tous as $m) {
-				if ($m = email_valide($m))
-					$destinataires[$m]++;
-			}
-
-			//
-			// Envoyer les emails
-			//
-			$envoyer_mail = charger_fonction('envoyer_mail','inc');
-			foreach (array_keys($destinataires) as $email) {
-				if (!function_exists('job_queue_add'))
-					$envoyer_mail($email, $sujet, $courr);
-				else
-					job_queue_add('envoyer_mail',">$email : $sujet",array($email,$sujet,$courr),'inc/');
-			}
-
-			if ($l) lang_select();
+		while ($qui = sql_fetch($result_email)) {
+			$flux['data'][] = $qui['email'];
 		}
+
 	}
+	
+	return $flux;
 }
 ?>
