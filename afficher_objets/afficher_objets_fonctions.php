c0b107aa14dfcfd995c748ba85c43f7f37543484
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-12-06 14:03:56 +0000

    {where?} permet la translation simple de la fonction inc_afficher_objets_dist vers l'appel au squelette prive/liste/{table} est present. On garde le reste du fichier tant que toutes les listes ne sont pas squeletisees, mais tout cela pourra disparaitre corps et ames ensuite, permettant a un plugin qui ajoute un objet de n'avoir a fournir que le squelette en question.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@33545 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- afficher_objets/afficher_objets_fonctions.php
+++ afficher_objets/afficher_objets_fonctions.php
@@ -5,4 +5,29 @@ function filtre_puce_statut_dist($id_objet, $statut, $id_rubrique, $type){
 	return $puce_statut($id_objet, $statut, $id_rubrique, $type);
 }
 
+
+/**
+ * {where}
+ * tout simplement
+ *
+ * @param <type> $idb
+ * @param <type> $boucles
+ * @param <type> $crit
+ */
+function critere_where_dist($idb, &$boucles, $crit) {
+	$boucle = &$boucles[$idb];
+	if (isset($crit->param[0]))
+		$_where = calculer_liste($crit->param[0], array(), $boucles, $boucles[$idb]->id_parent);
+	else
+		$_where = '@$Pile[0]["where"]';
+
+	if ($crit->cond)
+		$_where = "(($_where) ? ($_where) : '')";
+	
+	if ($crit->not)
+		$_where = "array('NOT',$_where)";
+
+	$boucle->where[]= $_where;
+}
+
 ?>
