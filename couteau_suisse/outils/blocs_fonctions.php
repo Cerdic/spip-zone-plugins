2c737c54543861c0324dcd5be782709c1d7d5581
patfr@ifrance.com <patfr@ifrance.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-12-16 20:31:15 +0000

    Blocs dépliables : grosse refonte du code des balises de squelette
    1. Les balises sont désormais calculées en php, recentrée vers une fonction unique : blocs_balises($type, $args=array())
       Cette fonction contrôle le bon ordre des balises et décrypte les arguments de #BLOC_TITRE
    
    2. Les balises #BLOC_TITRE_NUM, #BLOC_VISIBLE_TITRE et #balise_BLOC_VISIBLE_TITRE_NUM sont obsolètes désormais
       Elles sont remplacées par #BLOC_TITRE{9999}, #BLOC_TITRE{visible} et #BLOC_TITRE{9999, visible}
    
    3. L'ordre des arguments de #BLOC_TITRE importe peu.
       Arguments autorisés :
         - 'visible' : crée un bloc déplié
         - 'invisible' : crée un bloc replié (état par défaut)
         - un nombre : crée une balise numérotée
         - un texte (URL si possible !) : crée un bloc AJAX
    
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@33780 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- couteau_suisse/outils/blocs_fonctions.php
+++ couteau_suisse/outils/blocs_fonctions.php
@@ -2,84 +2,59 @@
 	
 /* Trois ou quatre balises pour creer des blocs depliables : 
 
-#BLOC_TITRE{fragment}     / fragment si ajax, pas de fragment sinon /
+#BLOC_TITRE     		/ {une_URL} si ajax, {un_numero} si bloc numerote, {visible} si bloc deplie /
 Mon titre
-#BLOC_RESUME              / facultatif /
+#BLOC_RESUME			/ facultatif /
 Mon resume qui disparait si on clique
 #BLOC_DEBUT
-Mon bloc depliable        / qui est aussi l'emplacement pour l'Ajax si le fragment est donne /
+Mon bloc depliable		/ qui est aussi l'emplacement pour l'Ajax si le fragment est donne /
 #BLOC_FIN
 
 */
 
 @define('_BLOC_TITRE_H', 'h4');
 
-// Un bloc replie, titre numerote : #BLOC_TITRE_NUM{numero} ou #BLOC_TITRE_NUM{numero, fragment}
-function balise_BLOC_TITRE_NUM($p) {
-	$p->code = blocs_balise_titre(interprete_argument_balise(2, $p), interprete_argument_balise(1, $p));
-	$p->interdire_scripts = false;
-	return $p;
-}
-// Un bloc deplie, titre numerote : #BLOC_VISIBLE_TITRE_NUM{numero} ou #BLOC_VISIBLE_TITRE_NUM{numero, fragment}
-function balise_BLOC_VISIBLE_TITRE_NUM($p) {
-	$p->code = blocs_balise_titre(interprete_argument_balise(2, $p), interprete_argument_balise(1, $p), -1);
+// Pour la balise suivante, l'ordre des arguments importe peu
+// Un bloc replie, titre simple : #BLOC_TITRE
+// Un bloc replie AJAX : #BLOC_TITRE{fragment} (fragment est une URLs)
+// Un bloc replie numerote : #BLOC_TITRE{numero} (numero est un nombre entier)
+// Un bloc deplie ou replie : ajouter l'argument 'visible' ou 'invisible' : #BLOC_TITRE{visible}
+// Par defaut : les blocs sont replies
+function balise_BLOC_TITRE($p) {
+	// Les arguments : 'visible' ou 'invisible', un numero, une URL
+	$i = 0; $args = array();
+	while(($a = interprete_argument_balise(++$i,$p)) != NULL) $args[] = $a;
+	$p->code = "blocs_balises('titre', array(".join(",",$args).'))';
 	$p->interdire_scripts = false;
 	return $p;
 }
 
-// Un bloc replie, titre simple : #BLOC_TITRE ou #BLOC_TITRE{fragment}
-function balise_BLOC_TITRE($p) {
-	$p->code = blocs_balise_titre(interprete_argument_balise(1, $p));
-	$p->interdire_scripts = false;
-	return $p;
-}
-// Un bloc deplie, titre simple : #BLOC_VISIBLE_TITRE ou #BLOC_VISIBLE_TITRE{fragment}
+// Balise obsolete
+function balise_BLOC_TITRE_NUM($p) { return balise_BLOC_TITRE($p);}
+// Balise obsolete
+function balise_BLOC_VISIBLE_TITRE_NUM($p) { return balise_BLOC_VISIBLE_TITRE($p);}
+// Balise obsolete
 function balise_BLOC_VISIBLE_TITRE($p) {
-	$p->code = blocs_balise_titre(interprete_argument_balise(1, $p), NULL, -1);
-	$p->interdire_scripts = false;
-	return $p;
+	// Produire le premier argument {visible}
+	$texte = new Texte; $texte->type='texte'; $texte->texte='visible';
+	array_unshift($p->param, array(0=>NULL, 1=>array(0=>$texte)));
+	return balise_BLOC_TITRE($p);
 }
 
 function balise_BLOC_RESUME($p) {
-	global $bloc_stade;
-	$k=count($bloc_stade)-1;
-	$stade=abs($bloc_stade[$k]);
-	$class=$bloc_stade[$k]>0?'':' blocs_invisible blocs_slide';
-	
-	if($stade<1 || $stade>2)
-		die("erreur de compilation #BLOC_RESUME sans #BLOC_TITRE_($stade)");
-	$p->code = "'</a></"._BLOC_TITRE_H."><div class=\"blocs_resume$class\">'";
-	$bloc_stade[$k]=$bloc_stade[$k]>0?3:-3;	/* 3 = resume */
+	$p->code = "blocs_balises('resume')";
 	$p->interdire_scripts = false;
 	return $p;
 }
 
 function balise_BLOC_DEBUT($p) {
-	global $bloc_stade;
-	$k=count($bloc_stade)-1;
-	$stade=abs($bloc_stade[$k]);
-	$class=$bloc_stade[$k]<0?'':' blocs_invisible blocs_slide';
-
-	if($stade == 3)	/* on arrive du resume, fermer la div resume seulement */
-		$p->code = "'</div><div class=\"blocs_destination$class\">'";
-	else	{
-		if($stade<1 || $stade>2)
-					/* on DOIT arriver de titre */
-			die("erreur de compilation #BLOC_DEBUT sans #BLOC_TITRE_($stade)");
-		$p->code = "'</a></"._BLOC_TITRE_H."><div class=\"blocs_destination$class\">'";
-		}
-	$bloc_stade[$k]=$bloc_stade[$k]>0?4:-4; /* 4=debut */
+	$p->code = "blocs_balises('debut')";
 	$p->interdire_scripts = false;
 	return $p;
 }
 
 function balise_BLOC_FIN($p) {
-	global $bloc_stade;
-	$k=abs(array_pop($bloc_stade));
-	if($k!=4)	
-		die("erreur de compilation #BLOC_FIN sans #BLOC_DEBUT_($k)");
-
-	$p->code = "'</div></div>'";
+	$p->code = "blocs_balises('fin')";
 	$p->interdire_scripts = false;
 	return $p;
 }
@@ -101,7 +76,7 @@ function balise_BLOC_DEPLIER_NUM($p) {
 	return $p;
 }
 
-// Renvoie un code JQuery pour courcuiter la variable cnfigurant les blocs uniques
+// Renvoie un code JQuery pour courcuiter la variable configurant les blocs uniques
 // Argument : oui/non ou 0/1
 function balise_BLOC_UNIQUE($p) {
 	$arg = interprete_argument_balise(1, $p);
@@ -124,26 +99,55 @@ function bloc_unique_script($num=1) {
 	return http_script("var blocs_replier_tout = $num;");
 }
 
-// fonction pour le calcul de la balise #BLOC_TITRE_NUM
-function bloc_is_num($num) {
-	 return preg_match(',^\s*(\d+)\s*$,', $num, $regs)?" cs_bloc$regs[1]":"''";
-}
-
-// fonction pour le calcul des 4 balises de type #BLOC_TITRE
-function blocs_balise_titre($fragment, $numero=NULL, $replie=1) {
+// fonction pour le calcul des balises de type #BLOC_XXX
+// $args ne sert que pour #BLOC_TITRE et contient les arguments de la balise sous forme de tableau
+function blocs_balises($type, $args=array()) {
 	// statut binaire : bit1=ajax bit2=titre bit3=resume bit4=debut
-	global  $bloc_stade; /* 2 = ajax; 1 = titre pas d'ajax; idem negatif = bloc visible */
-	if ($fragment!==NULL){
-		$ajax=' blocs_ajax ';
-		$bloc_stade[]=2*$replie;
-	} else {
-		$fragment="'javascript:;'";
-		$ajax="";
-		$bloc_stade[]=1*$replie;
+	// 2 = ajax; 1 = titre pas d'ajax; idem negatif = bloc visible
+	static $bloc_stade;
+	$k=isset($bloc_stade)?count($bloc_stade):0;
+	$stade=$k?abs($bloc_stade[--$k]):0;
+	switch($type) {
+	case 'titre':
+		$replie = 1; $numero = '';
+		foreach($args as $a) {	
+			if(is_numeric($a=trim($a))) $numero = $a;	
+			elseif($a=='visible') $replie = -1;
+			elseif($a=='invisible') $replie = 1;
+			elseif(strlen($a)) $fragment = $a;
+		}
+		if (isset($fragment)){
+			$ajax=' blocs_ajax ';
+			$bloc_stade[]=2*$replie;
+		} else {
+			$fragment="javascript:;";
+			$ajax='';
+			$bloc_stade[]=1*$replie;
+		}
+		$replie = $replie>0?' blocs_replie':'';
+		return "<div class=\"cs_blocs$numero\"><"._BLOC_TITRE_H." class=\"blocs_titre$replie $ajax\"><a href=\"$fragment\">";
+	case 'resume':
+		$class=$bloc_stade[$k]>0?'':' blocs_invisible blocs_slide';
+		if($stade<1 || $stade>2) // on DOIT arriver de titre
+			die("erreur de compilation #BLOC_RESUME sans #BLOC_TITRE ($stade)");
+		$bloc_stade[$k]=$bloc_stade[$k]>0?3:-3;	// 3 = resume
+		return "</a></"._BLOC_TITRE_H."><div class=\"blocs_resume$class\">";
+	case 'debut':
+		$class=$bloc_stade[$k]<0?'':' blocs_invisible blocs_slide';
+		$bloc_stade[$k]=$bloc_stade[$k]>0?4:-4; // 4=debut
+		if($stade == 3)	// on arrive du resume, fermer la div resume seulement
+			return "</div><div class=\"blocs_destination$class\">";
+		else {
+			if($stade<1 || $stade>2) // on DOIT arriver de titre
+				die("erreur de compilation #BLOC_DEBUT sans #BLOC_TITRE ($stade)");
+			return "</a></"._BLOC_TITRE_H."><div class=\"blocs_destination$class\">";
+		}
+	case 'fin':
+		$k=isset($bloc_stade)?abs(array_pop($bloc_stade)):0;
+		if($k!=4)	
+			die("erreur de compilation #BLOC_FIN sans #BLOC_DEBUT ($k)");
+		return "</div></div>";
 	}
-	$numero = $numero===NULL?"''":"bloc_is_num($numero)";
-	$replie = $replie>0?' blocs_replie':'';
- 	return " '<div class=\"cs_blocs'.$numero.'\"><"._BLOC_TITRE_H." class=\"blocs_titre$replie $ajax\"><a href=\"'.".$fragment.".'\">' ";
 }
 
 ?>
