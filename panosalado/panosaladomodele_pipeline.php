7320ae40dca2f31e06efac55b14c91f8478bcbcd
fbarbenoire@yahoo.fr <fbarbenoire@yahoo.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-12-17 22:43:44 +0000

    Importation du plugin panosalado
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@33802 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ panosalado/panosaladomodele_pipeline.php
@@ -0,0 +1,10 @@
+<?php
+function panosaladomodele_insert_head($flux){
+$flux .= "<script type='text/javascript' src='"._DIR_PLUGIN_PANOSALADOMODELE."scripts/swfobject.js'></script>\n";
+return $flux;
+}
+function panosaladomodele_header_prive($flux){
+$flux .= "<script type='text/javascript' src='"._DIR_PLUGIN_PANOSALADOMODELE."scripts/swfobject.js'></script>\n";
+return $flux;
+}
+?>
