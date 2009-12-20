2a2764e88aa6ca5b996e23778887572b7baab11a
fil@rezo.net <fil@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-12-20 21:58:26 +0000

    pour tester permettre de passer {{{ ?HTTP_REFERER=www.xxxxx.tld }}} dans l'url
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@33875 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- socialtags/socialtags_options.php
+++ socialtags/socialtags_options.php
@@ -5,12 +5,15 @@ if (is_array($cfg = @unserialize($GLOBALS['meta']['socialtags']))
 AND $cfg['ifreferer']) {
 	define('SOCIALTAGS_SOURCES', 'facebook|yahoo|google');
 	# $dom = domaine referent
-	if (isset($_SERVER['HTTP_REFERER'])) {
-		list(,,$s_ref) = explode('/', $_SERVER['HTTP_REFERER']);
-		if (preg_match(','.SOCIALTAGS_SOURCES.',i', $s_ref, $r)) {
-			$coo = 'social_'.$r[0];
-			setcookie($coo, $_COOKIE[$coo] = 1);
-		}
+	$ref = isset($_GET['HTTP_REFERER'])
+		? $_GET['HTTP_REFERER']
+		: (isset($_SERVER['HTTP_REFERER'])
+			? $_SERVER['HTTP_REFERER']
+			: '');
+	list(,,$s_ref) = explode('/', $ref);
+	if (preg_match(','.SOCIALTAGS_SOURCES.',i', $s_ref, $r)) {
+		$coo = 'social_'.$r[0];
+		setcookie($coo, $_COOKIE[$coo] = 1);
 	}
 }
 
