36ab853f583ab3730214e1b4be0730af0eff9485
fil@rezo.net <fil@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-12-20 21:18:18 +0000

    commit precedent etait un peu trop rapide ; cette fois ca fonctionne
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@33873 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- socialtags/socialtags_options.php
+++ socialtags/socialtags_options.php
@@ -2,13 +2,12 @@
 
 # definir la source de trafic et l'indiquer dans un cookie
 if (is_array($cfg = @unserialize($GLOBALS['meta']['socialtags']))
-AND $cfg['badgereferer']) {
+AND $cfg['ifreferer']) {
 	define('SOCIALTAGS_SOURCES', 'facebook|yahoo|google');
-
 	# $dom = domaine referent
 	if (isset($_SERVER['HTTP_REFERER'])) {
 		list(,,$s_ref) = explode('/', $_SERVER['HTTP_REFERER']);
-		if (preg_match(','.SOURCES.',i', $s_ref, $r)) {
+		if (preg_match(','.SOCIALTAGS_SOURCES.',i', $s_ref, $r)) {
 			$coo = 'social_'.$r[0];
 			setcookie($coo, $_COOKIE[$coo] = 1);
 		}
