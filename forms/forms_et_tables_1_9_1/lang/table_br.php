b70b1e877b585008a10716b6974240c055d79754
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-12-29 12:07:46 +0000

    langues (table)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@34037 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ forms/forms_et_tables_1_9_1/lang/table_br.php
@@ -0,0 +1,55 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'articles_utilisant' => '<NEW>Pennado&ugrave; a implij an daolenn-se',
+	'aucune_reponse' => '<NEW>Roadenn ebet',
+
+	// C
+	'champs_formulaire' => '<NEW>Maezienno&ugrave; an daolenn',
+
+	// F
+	'formulaire' => '<NEW>Taolenn',
+
+	// I
+	'icone_ajouter_donnees' => '<NEW>Ouzhpenna&ntilde; roadenno&ugrave;',
+	'icone_creer_table' => '<NEW>Kroui&ntilde; un daolenn nevez',
+	'importer_donnees_csv' => '<NEW>Enporzhia&ntilde; roadenno&ugrave;',
+	'info_supprimer_formulaire' => '<NEW>Ha sur oc\'h e fell deoc\'h diverka&ntilde; an daolenn-ma&ntilde;?',
+	'info_supprimer_formulaire_reponses' => '<NEW>Roadenno&ugrave; zo stag ouzh an daolenn-ma&ntilde;. Diverka&ntilde; memes tra?',
+
+	// L
+	'lien_retirer_donnee_liante' => '<NEW>Lemel an liamm kuit adalek ar roadenn-ma&ntilde;',
+	'lien_retirer_donnee_liee' => '<NEW>Lemel ar roadenn-ma&ntilde; kuit',
+
+	// N
+	'nombre_reponses' => '<NEW>@nombre@ roadenn',
+	'nouveau_formulaire' => '<NEW>Taolenn nevez',
+
+	// S
+	'suivi_reponses' => '<NEW>Gwelet ar roadenno&ugrave;',
+	'supprimer_formulaire' => '<NEW>Diverka&ntilde; an daolenn-ma&ntilde;',
+
+	// T
+	'telecharger_reponses' => '<NEW>Pellgarga&ntilde; ar roadenno&ugrave;',
+	'texte_donnee_statut' => '<NEW>Statud ar roadenn-ma&ntilde;',
+	'texte_statut_poubelle' => '<NEW>Roadenn bet diverket',
+	'texte_statut_prepa' => '<NEW>O skridaoza&ntilde; ar roadenn',
+	'texte_statut_prop' => '<NEW>Roadenn kinniget',
+	'texte_statut_publie' => '<NEW>Roadenn embannet',
+	'texte_statut_refuse' => '<NEW>Roadenn nac\'het',
+	'titre_formulaire' => '<NEW>Titl an daolenn',
+	'toutes_tables' => '<NEW>An holl daolenno&ugrave;',
+	'type_des_tables' => '<NEW>Taolenno&ugrave;',
+
+	// U
+	'une_reponse' => '<NEW>Ur roadenn'
+);
+
+?>
