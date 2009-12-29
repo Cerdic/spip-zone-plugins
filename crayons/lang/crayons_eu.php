47e27f513e14340066781635f1d7e2aae30f99f2
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-12-29 12:07:48 +0000

    langues (crayons)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@34038 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ crayons/lang/crayons_eu.php
@@ -0,0 +1,44 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'annuler' => 'Ezeztatu',
+
+	// D
+	'deja_modifie' => 'Jadanik aldatua',
+	'donnees_mal_formatees' => 'Gaizki formatutako datuak',
+
+	// E
+	'editer' => 'Editatu',
+	'editer_@type@_@id@' => '@type@ @id@ osoki editatu',
+	'editer_tout' => 'Dena editatu',
+
+	// M
+	'modifie_par_ailleurs' => 'Bestalde aldatua',
+
+	// N
+	'non_autorise' => 'Ez baimendua',
+	'non_implemente' => 'Ez inpliment atua',
+
+	// P
+	'pas_de_modification' => 'Aldaketarik ez',
+	'pas_de_valeur' => 'Balorerik ez',
+
+	// S
+	'sauvegarder' => 'Aldaketak grabatu ?',
+	'svp_copier_coller' => '<NEW>D&eacute;sol&eacute;, copiez/collez vos changements et recommencez',
+
+	// U
+	'update_impossible' => '<NEW>La mise &agrave; jour a &eacute;chou&eacute;.',
+
+	// V
+	'veuillez_patienter' => '<NEW>Veuillez patienter...'
+);
+
+?>
---
+++ crayons/lang/crayons_km.php
@@ -0,0 +1,44 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'annuler' => '&#6036;&#6084;&#6087;&#6036;&#6020;&#6091;',
+
+	// D
+	'deja_modifie' => '<NEW>Already modified',
+	'donnees_mal_formatees' => '<NEW>Invalid request format',
+
+	// E
+	'editer' => '&#6016;&#6082;&#6036;&#6098;&#6042;&#6082;',
+	'editer_@type@_@id@' => '<NEW>Edit the whole @type@ @id@',
+	'editer_tout' => '&#6016;&#6082;&#6036;&#6098;&#6042;&#6082; &#6033;&#6070;&#6086;&#6020;&#6050;&#6047;&#6091;',
+
+	// M
+	'modifie_par_ailleurs' => '<NEW>Was modified elsewhere in the meantime',
+
+	// N
+	'non_autorise' => '<NEW>&#6018;&#6098;&#6040;&#6070;&#6035;&#6050;&#6035;&#6075;&#6025;&#6098;&#6025;&#6070;&#6031;',
+	'non_implemente' => '<NEW>Not implemented',
+
+	// P
+	'pas_de_modification' => '<NEW>&#6018;&#6098;&#6040;&#6070;&#6035;&#6036;&#6086;&#6043;&#6070;&#6047;&#6091;&#6036;&#6098;&#6031;&#6076;&#6042;',
+	'pas_de_valeur' => '<NEW>&#6018;&#6098;&#6040;&#6070;&#6035;&#6031;&#6086;&#6043;&#6083; &#6047;&#6086;&#6042;&#6070;&#6036;&#6091;&#6050;&#6031;&#6098;&#6031;&#6047;&#6025;&#6098;&#6025;&#6070; (ID) &#6035;&#6081;&#6087;',
+
+	// S
+	'sauvegarder' => '<NEW>&#6042;&#6016;&#6098;&#6047;&#6070;&#6033;&#6075;&#6016; &#6036;&#6086;&#6043;&#6070;&#6047;&#6091;&#6036;&#6098;&#6031;&#6076;&#6042;?',
+	'svp_copier_coller' => '<NEW>Sorry, copy/paste your changes and retry',
+
+	// U
+	'update_impossible' => '&#6016;&#6070;&#6042;&#6036;&#6035;&#6098;&#6033;&#6070;&#6035;&#6091;&#6047;&#6040;&#6096;&#6041; &#6036;&#6070;&#6035;&#6036;&#6042;&#6070;&#6023;&#6096;&#6041;&#6100;',
+
+	// V
+	'veuillez_patienter' => '&#6016;&#6086;&#6038;&#6075;&#6020;&#6047;&#6098;&#6044;&#6082;&#6020;&#6042;&#6016; ...'
+);
+
+?>
