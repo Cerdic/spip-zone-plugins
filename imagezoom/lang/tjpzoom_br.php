85bdce9b10df44fec339f614b5beab15e712b215
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-12-29 12:07:54 +0000

    langues (tjpzoom)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@34041 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ imagezoom/lang/tjpzoom_br.php
@@ -0,0 +1,30 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// D
+	'descriptif' => '<NEW>Dre ar bajenn-ma&ntilde; e c\'haller keflunia&ntilde; stil ar werenn-greski&ntilde;. Gallout a c\'haller diskl&ecirc;ria&ntilde; ur stil personelaet; gwelet <a href="http://plugins.spip.net/?page=article&amp;id_article=48">an teulio&ugrave; skoazell enlinenn</a>.',
+
+	// E
+	'example' => '<NEW>Disoc\'h grafek',
+
+	// N
+	'nostyle' => '<NEW>Distil',
+
+	// O
+	'other' => '<NEW>Trao&ugrave; all',
+
+	// S
+	'size_vignette' => '<NEW>Ledander brasa&ntilde; ar vignetenn',
+	'style' => '<NEW>Stil ar werenn-greski&ntilde;',
+
+	// T
+	'titre' => '<NEW>Kefluniadur ar werenn-greski&ntilde;'
+);
+
+?>
