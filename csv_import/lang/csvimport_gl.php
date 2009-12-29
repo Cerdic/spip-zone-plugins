b1a1424edee37a4247d30b543b1f1515dbf8aff0
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-12-29 12:07:58 +0000

    langues (csvimport)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@34044 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ csv_import/lang/csvimport_gl.php
@@ -0,0 +1,78 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'administrer_tables' => 'Administrar as t&aacute;boas',
+	'ajouter_donnees' => 'Engadir os datos',
+	'ajouter_table' => 'Engadir &aacute; t&aacute;boa',
+	'aucune_donnee' => 'Non hai ning&uacute;n dato dentro do ficheiro',
+	'aucune_table_declaree' => 'Ningunha t&aacute;boa foi declarada para a importaci&oacute;n CSV',
+	'avertissement_ajout' => 'Os datos do ficheiro CSV vanse engadir &aacute; t&aacute;boa "@table@" como se ilustra a seguir.',
+	'avertissement_remplacement' => 'Esta operaci&oacute;n sup&oacute;n a supresi&oacute;n de todos os datos presentes na t&aacute;boa. Os datos do ficheiro inseriranse a seguir:',
+
+	// C
+	'caracteres_separation' => 'Car&aacute;cter de separaci&oacute;n',
+	'champs_csv' => 'Campos do ficheiro CSV',
+	'champs_table' => 'Campos da t&aacute;boa',
+	'correspondance_incomplete' => 'As correspondencias CSV-T&aacute;boa son incompletas',
+	'correspondance_indefinie' => 'As correspondencias entre CSV-T&aacute;boa non est&aacute;n definidas',
+	'csvimport' => 'Importar un CSV',
+
+	// D
+	'delimiteur_indefini' => 'Delimitador non definido',
+	'description_table_introuvable' => 'Non se pode encontrar a descrici&oacute;n da t&aacute;boa',
+
+	// E
+	'erreurs_ajout_base' => 'Houbo @nb@ erros logo de engadilos na base.',
+	'export_classique' => 'CSV cl&aacute;sico (,)',
+	'export_excel' => 'CSV para Excel (;)',
+	'export_format' => 'Formato do ficheiro:',
+	'export_table' => 'Exportaci&oacute;n da t&aacute;boa: @table@',
+	'export_tabulation' => 'CSV con tabulaci&oacute;n',
+	'exporter' => 'Exportar',
+
+	// F
+	'fichier_absent' => 'Ficheiro ausente',
+	'fichier_choisir' => 'Ficheiro CSV para importar',
+	'fichier_vide' => 'Ficheiro baleiro',
+
+	// I
+	'import_csv' => 'Importar o CSV : @table@',
+	'import_export_tables' => 'Importar / Exportar nas t&aacute;boas',
+
+	// L
+	'ligne_entete' => '1<sup>ro</sup> li&ntilde;a de cabeceira',
+	'lignes_table' => 'Hai @nb_resultats@ li&ntilde;as na t&aacute;boa "@table@".',
+	'lignes_totales' => '@nb@ li&ntilde;as en total.',
+	'lignes_totales_csv' => '@nb@ li&ntilde;as en total no ficheiro CSV.',
+
+	// N
+	'nb_enregistrements' => '@nb@ rexistros',
+
+	// P
+	'pas_importer' => 'Non importar',
+	'premieres_lignes' => '@nb@ primeiras li&ntilde;as do ficheiro.',
+	'probleme_chargement_fichier' => 'Problema logo da carga do ficheiro',
+	'probleme_chargement_fichier_erreur' => 'Problema logo da carga do ficheiro (erro @erreur@).',
+	'probleme_inextricable' => 'Problema inextricable...',
+
+	// R
+	'remplacer_toute_table' => 'Substitu&iacute;r toda a t&aacute;boa',
+
+	// T
+	'table_vide' => 'A t&aacute;boa "@table@" est&aacute; baleira.',
+	'tables_declarees' => 'T&aacute;boas declaradas',
+	'tables_presentes' => 'T&aacute;boas presentes na t&aacute;boa',
+	'tout_remplacer' => 'Substitu&iacute;r todo',
+
+	// Z
+	'z' => 'zzz'
+);
+
+?>
---
+++ csv_import/lang/csvimport_km.php
@@ -0,0 +1,78 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'administrer_tables' => '&#6050;&#6039;&#6071;&#6036;&#6070;&#6043; &#6038;&#6077;&#6016;&#6031;&#6070;&#6042;&#6070;&#6020;',
+	'ajouter_donnees' => '&#6036;&#6035;&#6098;&#6032;&#6082;&#6040;&#6033;&#6071;&#6035;&#6098;&#6035;&#6035;&#6096;&#6041;',
+	'ajouter_table' => '&#6036;&#6035;&#6098;&#6032;&#6082;&#6040; &#6033;&#6085;&#6031;&#6070;&#6042;&#6070;&#6020;',
+	'aucune_donnee' => '&#6018;&#6098;&#6040;&#6070;&#6035;&#6033;&#6071;&#6035;&#6098;&#6035;&#6035;&#6096;&#6041; &#6016;&#6098;&#6035;&#6075;&#6020;&#6063;&#6016;&#6047;&#6070;&#6042;&#6100;',
+	'aucune_table_declaree' => '<NEW>Aucune table n\'est d&eacute;clar&eacute;e pour l\'import CSV',
+	'avertissement_ajout' => '<NEW>Les donn&eacute;es du fichier CSV vont &ecirc;tre ajout&eacute;es &agrave; la table "@table@" comme illustr&eacute; ci-dessous.',
+	'avertissement_remplacement' => '<NEW>Cette op&eacute;ration va entra&icirc;ner la suppression de toutes les donn&eacute;es pr&eacute;sentes dans la table. Les donn&eacute;es du fichier seront ins&eacute;r&eacute;es comme ci-dessous :',
+
+	// C
+	'caracteres_separation' => '<NEW>Caract&egrave;re de s&eacute;paration',
+	'champs_csv' => '<NEW>Champs du fichier CSV',
+	'champs_table' => '&#6044;&#6070;&#6043; &#6035;&#6083;&#6031;&#6070;&#6042;&#6070;&#6020;',
+	'correspondance_incomplete' => '<NEW>Correspondances CSV-Table incompl&egrave;tes',
+	'correspondance_indefinie' => '<NEW>Correspondances CSV-Table non d&eacute;finies',
+	'csvimport' => '&#6035;&#6070;&#6086;&#6021;&#6076;&#6043; CSV',
+
+	// D
+	'delimiteur_indefini' => '<NEW>D&eacute;limiteur non d&eacute;fini',
+	'description_table_introuvable' => '<NEW>Description de la table introuvable',
+
+	// E
+	'erreurs_ajout_base' => '<NEW>Il y a eu @nb@ erreurs lors de l\'ajout dans la base.',
+	'export_classique' => '<NEW>CSV classique (,)',
+	'export_excel' => '<NEW>CSV pour Excel (;)',
+	'export_format' => '&#6033;&#6098;&#6042;&#6020;&#6091;&#6033;&#6098;&#6042;&#6070;&#6041; &#6035;&#6083;&#6063;&#6016;&#6047;&#6070;&#6042;&nbsp;&#6102;',
+	'export_table' => '&#6016;&#6070;&#6042;&#6035;&#6070;&#6086;&#6021;&#6081;&#6025; &#6035;&#6083;&#6031;&#6070;&#6042;&#6070;&#6020;&#6102; @table@',
+	'export_tabulation' => '<NEW>CSV avec tabulations',
+	'exporter' => '&#6035;&#6070;&#6086;&#6021;&#6081;&#6025;',
+
+	// F
+	'fichier_absent' => '&#6063;&#6016;&#6047;&#6070;&#6042;&#6050;&#6044;&#6031;&#6098;&#6031;&#6040;&#6070;&#6035;',
+	'fichier_choisir' => '&#6063;&#6016;&#6047;&#6070;&#6042; CSV &#6031;&#6098;&#6042;&#6076;&#6044;&#6035;&#6070;&#6086;&#6021;&#6076;&#6043;',
+	'fichier_vide' => '&#6063;&#6016;&#6047;&#6070;&#6042;&#6033;&#6033;&#6081;',
+
+	// I
+	'import_csv' => '&#6035;&#6070;&#6086;&#6021;&#6076;&#6043; CSV &#6102; @table@',
+	'import_export_tables' => '&#6035;&#6070;&#6086;&#6021;&#6076;&#6043; / &#6035;&#6070;&#6086;&#6021;&#6081;&#6025; &#6016;&#6098;&#6035;&#6075;&#6020;&#6031;&#6070;&#6042;&#6070;&#6020;',
+
+	// L
+	'ligne_entete' => '<NEW>1<sup>&egrave;re</sup> ligne d\'en-t&ecirc;te',
+	'lignes_table' => '<NEW>Il y a @nb_resultats@ lignes dans la table "@table@".',
+	'lignes_totales' => '&#6047;&#6042;&#6075;&#6036; @nb@ &#6036;&#6035;&#6098;&#6033;&#6070;&#6031;&#6091;&#6100;',
+	'lignes_totales_csv' => '&#6047;&#6042;&#6075;&#6036; @nb@ &#6036;&#6035;&#6098;&#6033;&#6070;&#6031;&#6091; &#6016;&#6098;&#6035;&#6075;&#6020;&#6063;&#6016;&#6047;&#6070;&#6042; CSV&#6100;',
+
+	// N
+	'nb_enregistrements' => '<NEW>@nb@ enregistrements',
+
+	// P
+	'pas_importer' => '&#6018;&#6098;&#6040;&#6070;&#6035;&#6031;&#6098;&#6042;&#6076;&#6044;&#6035;&#6070;&#6086;&#6021;&#6076;&#6043;',
+	'premieres_lignes' => '@nb@ &#6036;&#6035;&#6098;&#6033;&#6070;&#6031;&#6091;&#6026;&#6086;&#6036;&#6076;&#6020; &#6035;&#6083;&#6063;&#6016;&#6047;&#6070;&#6042;&#6100;',
+	'probleme_chargement_fichier' => '&#6036;&#6025;&#6098;&#6048;&#6070; &#6038;&#6081;&#6043;&#6033;&#6070;&#6025;&#6041;&#6016;&#6063;&#6016;&#6047;&#6070;&#6042;',
+	'probleme_chargement_fichier_erreur' => '&#6036;&#6025;&#6098;&#6048;&#6070; &#6038;&#6081;&#6043;&#6033;&#6070;&#6025;&#6041;&#6016;&#6063;&#6016;&#6047;&#6070;&#6042; (&#6016;&#6086;&#6048;&#6075;&#6047; @erreur@)&#6100;',
+	'probleme_inextricable' => '<NEW>Probl&egrave;me inextricable...',
+
+	// R
+	'remplacer_toute_table' => '<NEW>Remplacer toute la table',
+
+	// T
+	'table_vide' => '&#6031;&#6070;&#6042;&#6070;&#6020; "@table@" &#6033;&#6033;&#6081;&#6100;',
+	'tables_declarees' => '<NEW>Tables d&eacute;clar&eacute;es',
+	'tables_presentes' => '<NEW>Tables pr&eacute;sentes dans la base',
+	'tout_remplacer' => '&#6023;&#6086;&#6035;&#6077;&#6047;&#6033;&#6070;&#6086;&#6020;&#6050;&#6047;&#6091;',
+
+	// Z
+	'z' => 'zzz'
+);
+
+?>
