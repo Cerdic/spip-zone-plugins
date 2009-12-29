13de182fb5a3869af8d81e4e571d4489c0d1b794
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-12-29 12:07:55 +0000

    langues (flickrcc)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@34042 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ flickr_cc/lang/flickrcc_km.php
@@ -0,0 +1,17 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'ajouter_image_flickr' => '&#6036;&#6035;&#6098;&#6032;&#6082;&#6040; &#6040;&#6077;&#6041;&#6042;&#6076;&#6036;&#6039;&#6070;&#6038; Flickr',
+
+	// F
+	'fermer' => '&#6036;&#6071;&#6033;'
+);
+
+?>
