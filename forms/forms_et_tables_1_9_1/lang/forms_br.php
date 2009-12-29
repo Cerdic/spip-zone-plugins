e57271210d27dfd5319e9c73555516ea2d84ab40
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-12-29 12:07:44 +0000

    langues (forms)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@34036 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ forms/forms_et_tables_1_9_1/lang/forms_br.php
@@ -0,0 +1,18 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'activer_barre_typo' => 'Gweredekaat ar varrenn dipografek',
+	'adresse_invalide' => 'Direizh eo ar chomlec\'h-ma&ntilde;.',
+
+	// V
+	'voir_article' => 'Gwelet ar pennad'
+);
+
+?>
