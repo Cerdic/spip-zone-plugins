fdd32d559057394abdbcd715932c2dc0c06f49d4
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-12-29 12:08:12 +0000

    langues (listes)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@34052 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ mailman_popup/lang/listes_eu.php
@@ -0,0 +1,56 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'ajoutee' => 'Egiaztatu ondonan &lt;@var_email@&gt; helbidea &lt;@liste@&gt; zerrendan gehitua izanen da. Igorria izan zaizun mezuari erantzutea eskertzen zaitugu.',
+
+	// C
+	'confirm' => 'Egiaztatze eskaera bat helbide honetara igorria izan da :&lt;@var_email@&gt;.',
+
+	// D
+	'deja' => '&lt;@var_email@&gt;&nbsp;: &lt;@liste@&gt; zerrendan jadanik harpidetua zira.',
+	'desabo' => 'Harpidetza ezeztatua da.',
+
+	// F
+	'fermer' => 'hetsi',
+
+	// I
+	'inscription' => '&lt;@liste@&gt; zerrendara harpidetza',
+
+	// M
+	'mail_removed' => '
+
+<@var_email@> helbidea @liste@ zerrendatik kendua izan da.
+
+Arazo bat balitz edo ez badezu zu honek harpidetza hori ezeztatzea eskatu,
+<@responsable@>-i idaztea eskertzen dizugu.
+
+Agur eta milesker.
+',
+	'merci' => 'Milesker.',
+
+	// P
+	'pasabo' => '&lt;@var_email@&gt; helbidea ez da &lt;@liste@&gt; zerrendari harpidetua.',
+	'patientez' => 'Igurikatu mesedez...',
+
+	// Q
+	'quitter' => 'Harpidetza ezeztatu',
+
+	// S
+	'subject_removed' => 'Zure helbidea @liste@ zerrendatik kendua izan da.',
+
+	// T
+	'titrefenetre' => 'Harpidetza',
+
+	// V
+	'veuillez' => 'Zure helbidea zehaztea eskertzen dizugu.',
+	'votreemail' => 'Zure emaila&nbsp;:'
+);
+
+?>
