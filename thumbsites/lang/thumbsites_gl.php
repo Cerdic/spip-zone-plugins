eb51e22cfdafc2be90686a723d56f29ba20899c2
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-12-29 12:08:00 +0000

    langues (thumbsites)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@34045 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ thumbsites/lang/thumbsites_gl.php
@@ -0,0 +1,20 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'bouton_effacer' => 'Desfacer',
+	'bouton_valider' => 'Validar',
+	'bulle_bouton_effacer' => 'Desfacer os resultados de volver aos valores de partida',
+	'bulle_bouton_valider' => 'Validar as sa&iacute;das',
+
+	// T
+	'titre_thumbshot_site' => 'VI&Ntilde;ETA DO SITIO'
+);
+
+?>
