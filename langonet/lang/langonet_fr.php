fcd8e11e03605df57accd62ef0b0d7c1fbe4e887
denisb@laposte.net <denisb@laposte.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-01-01 23:59:59 +0000

    des messages retournés plus précis, plus jolis.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@34110 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- langonet/lang/langonet_fr.php
+++ langonet/lang/langonet_fr.php
@@ -9,12 +9,12 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'bouton_verifier' => 'V&eacute;rifier',
 
 // I
-	'info_chemin_fichier' => 'Racine de l\'arborescence des fichiers : <em>ecrire/</em> ou <em>plugins/rainette/</em>.',
-	'info_chemin_langue' => 'Dossier dans lequel est install&eacute; le fichier de langue : <em>ecrire/lang/</em> ou <em>plugins/rainette/lang/</em>.',
+	'info_chemin_fichier' => 'Racine de l\'arborescence des fichiers (exemple : <em>plugins/rainette/</em>, ou <em>ecrire/</em>)',
+	'info_chemin_langue' => 'Dossier dans lequel est install&eacute; le fichier de langue (exemple : <em>plugins/rainette/lang/</em>, ou <em>ecrire/lang/</em>)',
 	'info_generer' => 'Cette option vous permet de g&eacute;n&eacute;rer, &agrave; partir d\'une langue source, le fichier de langue d\'un module donn&eacute; dans une langue cible. Si le fichier cible existe d&eacute;j&agrave; son contenu est r&eacute;utilis&eacute; pour construire le nouveau fichier.',
-	'info_langue' => 'Abr&eacute;viation de la langue: <em>fr</em>, <em>en</em>, <em>es</em>...',
+	'info_langue' => 'Abr&eacute;viation de la langue (exemple : <em>fr</em>, <em>en</em>, <em>es</em>...)',
 	'info_mode' => 'Correspond &agrave; la chaine qui sera ins&eacute;r&eacute;e lors de la cr&eacute;ation d\'un nouvel item pour la langue cible.',
-	'info_module' => 'Correspond au pr&eacute;fixe du fichier de langue hors abr&eacute;viation de la langue: <em>rainette</em> pour le plugin de m&ecirc;me nom ou <em>ecrire</em>, par exemple, pour SPIP.',
+	'info_module' => 'Correspond au pr&eacute;fixe du fichier de langue hors abr&eacute;viation de la langue (exemple : <em>rainette</em> pour le plugin de m&ecirc;me nom, ou <em>ecrire</em> pour SPIP)',
 	'info_verifier' => 'Cette option vous permet de v&eacute;rifier les fichiers de langue d\'un module donn&eacute; sous deux angles compl&eacute;mentaires. Il est possible, soit de v&eacute;rifier si des items de langue utilis&eacute;s dans un groupe de fichiers (un plugin, par exemple) ne sont pas d&eacute;finis dans le fichier de langue idoine, soit que certains items de langue d&eacute;finis ne sont plus utilis&eacute;s.',
 
 // L
@@ -33,31 +33,30 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 // M
 	'message_nok_champ_obligatoire' => 'Ce champ est obligatoire',
-	'message_nok_ecriture_fichier' => 'Le fichier de langue @langue@ du module @module@ n\'a pas &eacute;t&eacute; cr&eacute;&eacute; car une erreur s\'est produite lors de son &eacute;criture !',
-	'message_nok_fichier_langue' => 'Le fichier de langue @langue@ du module @module@ est introuvable sous @dossier@ !',
+	'message_nok_ecriture_fichier' => 'Le fichier de langue &laquo;<em>@langue@</em>&raquo; du module &laquo;<em>@module@</em>&raquo; n\'a pas &eacute;t&eacute; cr&eacute;&eacute; car une erreur s\'est produite lors de son &eacute;criture !',
+	'message_nok_fichier_langue' => 'Le fichier de langue &laquo;<em>@langue@</em>&raquo; du module &laquo;<em>@module@</em>&raquo; est introuvable sous &laquo;<em>@dossier@</em>&raquo; !',
 	'message_ok_0_defini_incertain' => 'Aucun item de langue n\'est utilis&eacute; dans un contexte complexe.',
 	'message_ok_0_non_defini' => 'Tous les items de langue utilis&eacute;s sont bien d&eacute;finis dans le fichier de langue choisi.',
 	'message_ok_0_non_utilise' => 'Tous les items de langue d&eacute;finis dans le fichier de langue choisi sont bien utilis&eacute;s.',
 	'message_ok_0_utilise_incertain' => 'Aucun item de langue n\'est utilis&eacute; dans un contexte complexe.',
-	'message_ok_fichier_genere' => 'Le fichier de langue @langue@ du module @module@ a &eacute;t&eacute; g&eacute;n&eacute;r&eacute; correctement (voir fichier @fichier@).',
+	'message_ok_fichier_genere' => 'Le fichier de langue &laquo;<em>@langue@</em>&raquo; du module &laquo;<em>@module@</em>&raquo; a &eacute;t&eacute; g&eacute;n&eacute;r&eacute; correctement (voir fichier &laquo;<em>@fichier@</em>&raquo;).',
 	'message_ok_n_definis_incertains' => 'Les @nberr@ items de langue ci-dessous sont utilis&eacute;s dans un contexte complexe et pourraient &ecirc;tre non d&eacute;finis dans le fichier de langue choisi. Nous vous invitons &agrave; les v&eacute;rifier un par un :',
 	'message_ok_n_non_definis' => 'Les @nberr@ items de langue ci-dessous sont utilis&eacute;s mais ne sont pas d&eacute;finis dans le fichier de langue choisi:',
-	'message_ok_n_non_utilises' => 'Les @nberr@ items de langue ci-dessous sont d&eacute;finis dans le fichier de langue choisi mais ne sont pas utilis&eacute;s :',
+	'message_ok_n_non_utilises' => 'Les @nberr@ items de langue ci-dessous sont bien d&eacute;finis dans le fichier de langue choisi, mais ne sont pas utilis&eacute;s dans l\'arborescence &agrave; v&eacute;rifier :',
 	'message_ok_n_utilises_incertains' => 'Les @nberr@ items de langue ci-dessous sont peut-&ecirc;tre utilis&eacute;s dans un contexte complexe. Nous vous invitons &agrave; les v&eacute;rifier un par un :',
-	'message_nok_plugin_inactif' => 'Le plugin de pr&eacute;fixe @module@ n\'est pas activ&eacute;. Activez le avant de continuer les v&eacute;rifications',
+	'message_nok_plugin_inactif' => 'Le plugin &laquo;<em>@arbo_verif@</em>&raquo; n\'est pas activ&eacute;. Activez-le avant de continuer les v&eacute;rifications.',
 
 // O
 	'option_mode_index' => 'Item de la langue source',
 	'option_mode_new_index' => 'Item de la langue source pr&eacute;c&eacute;d&eacute; de &lt;NEW&gt;',
 	'option_mode_new_valeur' => 'Chaine dans la langue source pr&eacute;c&eacute;d&eacute;e de &lt;NEW&gt;',
 	'option_mode_new' => 'Balise &lt;NEW&gt; uniquement',
-	'option_mode_pas_item' => 'Ne pas cr&eacute;er d\'item',
 	'option_mode_valeur' => 'Chaine dans la langue source',
 	'option_mode_vide' => 'Une chaine vide',
 
 // T
 	'titre_onglet' => 'LangOnet',
-	'titre_page_navigateur' => 'LangOnet',
+	'titre_page_navigateur' => 'LangOnet'
 
 );
 
