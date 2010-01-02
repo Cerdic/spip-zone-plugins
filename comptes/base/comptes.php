f421c2ca6f655276b6593eac1522bb7da9fdff0b
cyril@cym.fr <cyril@cym.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-01-02 18:09:36 +0000

    Définition des 4 tables : - spip_contacts liée à spip_auteurs - spip_comptes : regroupe les contacts - spip_coordonnees : comme son nom l'indique, et pour ne pas surcharger inutilement les tables - spip_coordonnees_liens : inspiré des spip_documents_liens Création / désinstallation testé OK A faire : peupler la base
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@34130 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- comptes/base/comptes.php
+++ comptes/base/comptes.php
@@ -5,4 +5,102 @@
  */
 
 
+function comptes_declarer_tables_interfaces($interface){
+	
+	$interface['table_des_tables']['comptes'] = 'comptes';
+	$interface['table_des_tables']['contacts'] = 'contacts';
+	$interface['table_des_tables']['liens'] = 'liens';
+	
+	/**
+	 * Objectif : pouvoir utiliser les champs liés dans les boucles...
+	 *
+	 */
+	$interface['tables_jointures']['spip_auteurs']['id_auteur']= 'contacts';
+	$interface['tables_jointures']['spip_contacts']['id_auteur']= 'auteurs';
+	$interface['tables_jointures']['spip_comptes']['id_compte']= 'contacts';
+	$interface['tables_jointures']['spip_contacts']['id_compte']= 'comptes';
+
+	
+	/**
+	 * Objectif : autoriser les traitements SPIP sur certains champs texte...
+	 *
+	 */
+	$interface['table_des_traitements']['NOM'][] = _TRAITEMENT_TYPO;
+	$interface['table_des_traitements']['PRENOM'][] = _TRAITEMENT_TYPO;
+
+	return $interface;
+}
+
+
+function comptes_declarer_tables_principales($tables_principales){
+	//-- Table contacts ------------------------------------------
+	$contacts = array(
+		"id_contact" 	=> "bigint(21) NOT NULL auto_increment",
+		"id_auteur" 	=> "bigint(21) NOT NULL DEFAULT 0",
+		"id_compte" 	=> "bigint(21) NOT NULL DEFAULT 0",
+		"civilite" 		=> "tinytext DEFAULT '' NOT NULL",
+		"nom" 			=> "tinytext DEFAULT '' NOT NULL",
+		"prenom"		=> "VARCHAR(10) NOT NULL DEFAULT 0",
+		"naissance"		=> "datetime NOT NULL default '0000-00-00 00:00:00'",
+		"descriptif" 	=> "tinytext DEFAULT '' NOT NULL",
+		"date" 			=> "datetime NOT NULL default '0000-00-00 00:00:00'",
+		"maj"			=> "TIMESTAMP"
+		);
+	$contacts_key = array(
+		"PRIMARY KEY"	=> "id_contact",
+		"KEY id_auteur" => "id_auteur"
+		);
+	$tables_principales['spip_contacts'] =
+		array('field' => &$contacts, 'key' => &$contacts_key);
+
+
+	//-- Table comptes ------------------------------------------
+	$comptes = array(
+		"id_compte" 	=> "bigint(21) NOT NULL auto_increment",
+		"titre" 		=> "tinytext DEFAULT '' NOT NULL",
+		"descriptif"	=> "tinytext DEFAULT '' NOT NULL",
+		"date" 			=> "datetime NOT NULL default '0000-00-00 00:00:00'",
+		"maj"			=> "TIMESTAMP"
+		);
+	$comptes_key = array(
+		"PRIMARY KEY"	=> "id_compte"
+		);
+	$tables_principales['spip_comptes'] =
+		array('field' => &$comptes, 'key' => &$comptes_key);
+
+
+	//-- Table coordonnees ------------------------------------------
+	$coordonnees = array(
+		"id_coordonnee"	=> "bigint(21) NOT NULL auto_increment",
+		"type"			=> "VARCHAR(10) DEFAULT '' NOT NULL",
+		"titre" 		=> "tinytext DEFAULT '' NOT NULL",
+		"descriptif"	=> "tinytext DEFAULT '' NOT NULL",
+		"date" 			=> "datetime NOT NULL default '0000-00-00 00:00:00'",
+		"maj"			=> "TIMESTAMP"
+		);
+	$coordonnees_key = array(
+		"PRIMARY KEY"	=> "id_coordonnee"
+		);
+	$tables_principales['spip_coordonnees'] =
+		array('field' => &$coordonnees, 'key' => &$coordonnees_key);
+
+
+	//-- Table coordonnees_liens ------------------------------------------
+	$coordonnees_liens = array(
+		"id_coordonnee"	=> "bigint(21) NOT NULL DEFAULT 0",
+		"id_objet"		=> "bigint(21) NOT NULL DEFAULT 0", 
+		"objet"			=> "tinytext DEFAULT '' NOT NULL" // peut etre un contact ou un compte
+		);
+	$coordonnees_liens_key = array(
+		"PRIMARY KEY"	=> "id_coordonnee, id_objet"
+		);
+	$tables_principales['spip_coordonnees_liens'] =
+		array('field' => &$coordonnees_liens, 'key' => &$coordonnees_liens_key);
+
+
+	return $tables_principales;
+
+}
+
+
 ?>
--- comptes/base/comptes_upgrade.php
+++ comptes/base/comptes_upgrade.php
@@ -4,5 +4,39 @@
  * Licence GPL (c) 2010 - Ateliers CYM
  */
 
+include_spip('inc/meta');
+include_spip('base/create');
+include_spip('base/catalogue_peupler_base');
+
+
+function comptes_upgrade($nom_meta_base_version, $version_cible){
+	$current_version = "0.0";
+	if (isset($GLOBALS['meta'][$nom_meta_base_version]))
+		$current_version = $GLOBALS['meta'][$nom_meta_base_version];
+	
+	if ($current_version=="0.0") {
+		creer_base();
+		// comptes_peupler_base();
+		ecrire_meta($nom_meta_base_version, $current_version=$version_cible);
+	}
+	if (version_compare($current_version,"1.6","<")){
+		maj_tables('spip_comptes');
+		maj_tables('spip_contacts');
+		maj_tables('spip_coordonnees');
+		maj_tables('spip_coordonnees_liens');
+		
+		ecrire_meta($nom_meta_base_version,$current_version="1.0");
+	}
+}
+
+
+function comptes_vider_tables($nom_meta_base_version) {
+	sql_drop_table("spip_comptes");
+	sql_drop_table("spip_contacts");
+	sql_drop_table("spip_coordonnees");
+	sql_drop_table("spip_coordonnees_liens");
+
+	effacer_meta($nom_meta_base_version);
+}
 
 ?>
