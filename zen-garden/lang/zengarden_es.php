6a3e35e90ef104c701fe5bf93aae569675e44597
joseluis@digital77.com <joseluis@digital77.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-01-03 13:15:08 +0000

    en espagnol :-)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@34138 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ zen-garden/lang/zengarden_es.php
@@ -0,0 +1,45 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/_stable_/acces_restreint/lang/
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'apercevoir_ce_theme' => 'Previsualizar',
+
+	// B
+	'bandeau_personalisable' => 'Personalizar la imagen de la cabecera para este tema',
+
+	// C
+	'choix_theme' => 'Seleccionar un tema',
+	'choisir_ce_theme' => 'Seleccionar',
+	'conception_graphique_par' => 'Dise&ntilde;o gr&aacute;fico (c)',
+
+	// D
+	'desactiver_ce_theme' => 'Desactivar el tema y volver al estilo inicial del sitio',
+
+	// I
+	'info_page' => 'Puedes seleccionar uno de los temas propuestos, o s&oacute;lo previsualizarlos sin modificar la visualizaci&oacute;n actual del sitio',
+	'intitule_compatiblite_squelette' => 'S&oacute;lo es compatible con un esqueleto',
+	'intitule_version' => 'versi&oacute;n',
+
+	// M
+
+	// P
+
+	// R
+
+	// S
+	'theme_actif' => 'Este tema se utiliza actualmente',
+	'sous_licence' => 'con Licencia',
+
+	// T
+	'themes' => 'Temas',
+
+	// V
+
+	// Z
+);
+
+?>
