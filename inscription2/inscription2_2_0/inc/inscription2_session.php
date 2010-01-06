0996bc992499beed75639157493ff8335d7ba6bb
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-01-06 17:48:28 +0000

    Je pense que des espaces inutiles
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@34231 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- inscription2/inscription2_2_0/inc/inscription2_session.php
+++ inscription2/inscription2_2_0/inc/inscription2_session.php
@@ -1,28 +1,28 @@
 <?php
 
 function i2_poser_session($id_auteur){
-	// on pose une session permettant d'identifier l'abonne 
+	// on pose une session permettant d'identifier l'abonne
 	// si desfois il lui prenait l'idée de faire "retour" avec son navigateur
 	// on prend la date pour permettre la manip que pendant deux minutes
 	// voir un exemple dans le plugin abonnement (pipeline)
 	include_spip("inc/session");
-	session_set("id_inscrit", $id_auteur) ;	
+	session_set("id_inscrit", $id_auteur) ;
 	session_set("date_inscription", time()) ;
-	return ;	
+	return ;
 }
 
 function i2_session_valide(){
 	include_spip("inc/session");
-			
+
 	if($id_inscrit = session_get("id_inscrit")){
 		$time = time() - session_get("date_inscription") ;
 		spip_log($time,"session_lol");
 		// valide pendant quelques minutes
 		if($time < 4*60)
 			return $id_inscrit ;
-	}	
-	
-	return false ;
+	}
+
+	return false;
 }
 
 ?>
