5351e56b5a06044fa47f1ac1db8c31f6ade72df1
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-01-06 17:43:31 +0000

    la table spip_auteurs aussi doit être vérifiée
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@34230 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- inscription2/inscription2_2_0/inc/inscription2_inscrit_partiel.php
+++ inscription2/inscription2_2_0/inc/inscription2_inscrit_partiel.php
@@ -1,24 +1,35 @@
 <?php
 
+/**
+ * Vérification que nous sommes dans une inscription partielle
+ * Est-ce un email connu à qui il manque des champs obligatoires
+ * (inscrit partiellement par spip listes par ex) ?
+ *
+ * @param int $id
+ * @return bool
+ */
 function i2_inscrit_partiel($id){
-	// est-ce un email connu à qui il manque des champs obligatoires (inscrit partiellement par spip listes par ex) ?					
-				
-	// champs obligatoires
+	/**
+	 * Récupération de la liste des champs obligatoires
+	 */
 	$chercher_champs = charger_fonction('inscription2_champs_obligatoires','inc');
 	$champs = $chercher_champs();
 
-	// champs de l'inscrit
-	$res = sql_fetsel('*',"spip_auteurs_elargis","id_auteur = $id");
-															
+	/**
+	 * Champs de l'inscrit dans les tables spip_auteurs et spip_auteurs_elargis
+	 */
+	$res = sql_fetsel($champs,"spip_auteurs as aut LEFT JOIN spip_auteurs_elargis as autelar USING(id_auteur)","aut.id_auteur = $id");
+
 	foreach($champs as $val){
-		// s'il en manque un, on pose une session et on laisse passer
+		/**
+		 * Si un champs manque, on pose une session et on laisse passer
+		 */
 		if($res[$val]==''){
-		i2_poser_session($id);
-		return true ;
+			i2_poser_session($id);
+			return;
 		}
 	}
-	return false ;	
+	return false;
 }
-
 
 ?>
