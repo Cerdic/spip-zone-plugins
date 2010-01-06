9ba047a81ed627bc6934e48bae782fbbfcb10f67
cyril@cym.fr <cyril@cym.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-01-06 14:56:32 +0000

    Livraison du logo, du fichier lang, et ajout page d'explications dans l'interface privée.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@34227 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- comptes/comptes_autorisations.php
+++ comptes/comptes_autorisations.php
@@ -4,5 +4,18 @@
  * Licence GPL (c) 2010 - Ateliers CYM
  */
 
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+// fonction pour le pipeline, n'a rien a effectuer
+function comptes_autoriser(){}
+
+// declarations d'autorisations
+function autoriser_comptes_bouton_dist($faire, $type, $id, $qui, $opt) {
+	return autoriser('voir', 'comptes', $id, $qui, $opt);
+}
+
+function autoriser_comptes_voir_dist($faire, $type, $id, $qui, $opt) {
+	return true;
+}
 
 ?>
