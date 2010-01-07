2eb983d032d4ec59dfc746ba68f23cb7f8cc31fc
superyms@yahoo.fr <superyms@yahoo.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-01-07 19:51:56 +0000

    fichier de langue arabe
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@34262 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ slogan/lang/slogan_ar.php
@@ -0,0 +1,12 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	'info_slogan_site' => "&#1588;&#1593;&#1575;&#1585; &#1604;&#1605;&#1608;&#1602;&#1593;&#1603;",
+	'titre_identite_site' => '&#1607;&#1608;&#1610;&#1577; &#1575;&#1604;&#1605;&#1608;&#1602;&#1593;',
+
+);
+
+?>
