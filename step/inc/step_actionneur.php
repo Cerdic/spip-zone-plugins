67e6345bc9752705639b6aa66d7d9062e682fe6e
marcimat@free.fr <marcimat@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-01-07 15:04:58 +0000

    Une fonction scandir pour PHP4 (non testé)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@34253 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- step/inc/step_actionneur.php
+++ step/inc/step_actionneur.php
@@ -650,7 +650,24 @@ class Actionneur {
 	}
 	
 }
+}
 
 
+// scandir pour php4
+// http://fr2.php.net/manual/fr/function.scandir.php#73062
+if (!function_exists('scandir')) {
+function scandir($dir, $listDirectories=false, $skipDots=true) {
+    $dirArray = array();
+    if ($handle = opendir($dir)) {
+        while (false !== ($file = readdir($handle))) {
+            if (($file != "." && $file != "..") || $skipDots == true) {
+                if($listDirectories == false) { if(is_dir($file)) { continue; } }
+                array_push($dirArray,basename($file));
+            }
+        }
+        closedir($handle);
+    }
+    return $dirArray;
+}
 
 ?>
