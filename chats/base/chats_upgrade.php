7797ca759c94d7ad1e415d57bd82aab69fcfee6f
marcimat@free.fr <marcimat@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-01-08 10:18:08 +0000

    Il faut définir $current_version si on le connait !
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@34276 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- chats/base/chats_upgrade.php
+++ chats/base/chats_upgrade.php
@@ -5,6 +5,10 @@ include_spip('base/create');
 function chats_upgrade($nom_meta_base_version, $version_cible){
 	$current_version = "0.0";
 
+	if (isset($GLOBALS['meta'][$nom_meta_base_version])) {
+		$current_version = $GLOBALS['meta'][$nom_meta_base_version];
+	}
+
 	if ($current_version=="0.0") {
 		creer_base();
 		ecrire_meta($nom_meta_base_version, $current_version=$version_cible);
