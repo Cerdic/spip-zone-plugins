b05358229b7f9208c27a8b7cea145f63fe88f5be
denisb@laposte.net <denisb@laposte.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-01-09 10:11:02 +0000

    la fonction de vérification des utilisations retourne plus d'informations.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@34330 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- langonet/inc/langonet_verifier_utilisation.php
+++ langonet/inc/langonet_verifier_utilisation.php
@@ -1,47 +1,56 @@
 <?php
 /**
- * Vérification de l'utilisation dans le module des items de langue definis
+ * Vérification de l'utilisation des items de langue definis
  * 
- * @param object $module
- * @param object $langue
- * @param object $ou_langue
- * @param object $ou_fichiers
- * @return 
+ * @param string $rep
+ * @param string $module
+ * @param string $langue
+ * @param string $ou_langue
+ * @param string $ou_fichier
+ * @return array
  */
-function inc_langonet_verifier_utilisation($module, $langue, $ou_langue, $ou_fichiers) {
 
-	// On charge le fichier de langue a verifier si il existe dans l'arborescence $ou_langue 
+// $rep        => nom du repertoire parent de lang/
+// $module     => prefixe du fichier de langue
+// $langue     => index du nom de langue
+// $ou_lang    => chemin vers le fichier de langue a verifier
+// $ou_fichier => racine de l'arborescence a verifier
+function inc_langonet_verifier_utilisation($rep, $module, $langue, $ou_langue, $ou_fichier) {
+
+	// On charge le fichier de langue a verifier
+	// si il existe dans l'arborescence $ou_langue 
 	// (evite le mecanisme standard de surcharge SPIP)
 	include_spip('inc/traduire');
 	$var_source = "i18n_".$module."_".$langue;
-	if (empty($GLOBALS[$var_source]))
-		if (find_in_path($module.'_'.$langue.'.php', $ou_langue)) {
-			charger_langue($langue, $module);
-			if (!$GLOBALS[$var_source]) {
-				$resultats['statut'] = false;
-				$resultats['erreur'] = _T('langonet:message_nok_plugin_inactif', 
-										array('arbo_verif' => $ou_fichiers));
-				return $resultats;
+	if (empty($GLOBALS[$var_source])) {
+		$GLOBALS['idx_lang'] = $var_source;
+		include(_DIR_RACINE.$ou_langue.$module.'_'.$langue.'.php');
+	}
+
+	// On cherche l'ensemble des items utilises dans l'arborescence $ou_fichier
+	$utilises_brut = array('items' => array(), 'suffixes' => array());
+	// On ne scanne pas dans les ultimes sous-repertoires charsets/ ,
+	// lang/ , req/ . On ne scanne que les fichiers php, html ou xml
+	// (voir le fichier regexp.txt).
+	foreach (preg_files(_DIR_RACINE.$ou_fichier, '(?<!/charsets|/lang|/req)(/[^/]*\.(html|php|xml))$') as $_fichier) {
+		foreach ($contenu = file($_fichier) as $ligne => $texte) {
+			if (strpos($_fichier, '.xml')) {
+				$trouver_item = _TROUVER_ITEM_X;
+			}
+			else {
+				$trouver_item = _TROUVER_ITEM_HP;
+			}
+			if (preg_match_all($trouver_item, $texte, $matches)) {
+				$utilises_brut['items'] = array_merge($utilises_brut['items'], $matches[2]);
+				$utilises_brut['suffixes'] = array_merge($utilises_brut['suffixes'], $matches[3]);
+				foreach ($matches[2] as $item_val) {
+					$item_tous[$item_val][$_fichier][$ligne][] = trim($texte);
+				}
 			}
 		}
-		else{
-			$resultats['statut'] = false;
-			$resultats['erreur'] = _T('langonet:message_nok_fichier_langue', 
-									array('langue' => $langue, 'module' => $module, 'dossier' => $ou_langue));
-			return $resultats;
-		}
-		
-	// On cherche l'ensemble des items utilises dans l'arborescence $ou_fichiers
-	$utilises_brut = array('items' => array(), 'suffixes' => array());
-//	$regexp = ",(=\"$module:|='$module:|<\w+>$module:|<:$module:|_T\('$module:|_U\('$module:)(\w*)('\s*\.\s*\\$*\w*)*,im";
-	foreach (preg_files(_DIR_RACINE.$ou_fichiers,'\.(html|php|xml)$') as $_fichier) {
-		lire_fichier($_fichier, $contenu);
-		if (preg_match_all(_TROUVER_ITEM, $contenu, $matches)) {
-			$utilises_brut['items'] = array_merge($utilises_brut['items'], $matches[2]);
-			$utilises_brut['suffixes'] = array_merge($utilises_brut['suffixes'], $matches[3]);
-		}
 	}
-	// On rafine le tableau resultant en virant les doublons
+
+	// On affine le tableau resultant en supprimant les doublons
 	$utilises = array('items' => array(), 'suffixes' => array());
 	foreach ($utilises_brut['items'] as $_cle => $_valeur) {
 		if (!in_array($_valeur, $utilises['items'])) {
@@ -49,6 +58,7 @@ function inc_langonet_verifier_utilisation($module, $langue, $ou_langue, $ou_fic
 			$utilises['suffixes'][] = (!$utilises_brut['suffixes'][$_cle]) ? false : true;
 		}
 	}
+
 	// On construit la liste des items definis mais plus utilises
 	$non_utilises = array();
 	$a_priori_utilises = array();
@@ -79,14 +89,21 @@ function inc_langonet_verifier_utilisation($module, $langue, $ou_langue, $ou_fic
 		}
 		if (!$avec_certitude) {
 			$a_priori_utilises[] = $_item;
+			if (is_array($item_tous[$_valeur])) {
+				$item_peut_etre[$_item] = $item_tous[$_valeur];
+			}
 		}
 	}
 
 	// On prepare le tableau des resultats
+	$resultats['module'] = $module;
+	$resultats['ou_fichier'] = $ou_fichier;
+	$resultats['langue'] = $ou_langue.$module.'_'.$langue.'.php';
 	$resultats['non_utilises'] = $non_utilises;
 	$resultats['a_priori_utilises'] = $a_priori_utilises;
+	$resultats['fichier_peut_etre'] = $item_peut_etre;
 	$resultats['statut'] = true;
-	
+
 	return $resultats;
 }
 ?>
