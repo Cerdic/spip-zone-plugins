bdcd88da50bbedee1d4da978b3b0c74df83ab858
romy@rezo.net <romy@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-01-10 19:25:03 +0000

    Formulaire de commentaires pour SPIP 1.9
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@34374 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ comments/comments-190/comments_fr.php
@@ -0,0 +1,34 @@
+<?php
+
+// This is a SPIP-forum module file  --  Ceci est un fichier module de SPIP-forum
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// Liste des commentaires
+'message' => 'commentaire',
+'messages_forum' => 'commentaires',
+'commentaire' => 'commentaire',
+'commentaires' => 'commentaires',
+'commentaires_titre' => 'Vos commentaires',
+'commentaires_tout' => 'Tous vos commentaires',
+'permalink_pour' => 'Lien permanent vers',
+'le_date_jour' => 'Le', /* verifier que ca n'existe pas deja dans SPIP */
+'a_date_heure' => '&agrave;', /* verifier que ca n'existe pas deja dans SPIP */
+
+// Formulaire de commentaires
+'form_pet_message_commentaire' => 'Un message, un commentaire ?',
+'forum_info_modere' => 'Attention, votre message n\'appara&icirc;tra qu\'apr&egrave;s avoir &eacute;t&eacute; relu et approuv&eacute;.',
+
+'forum_votre_nom' => 'Nom ou pseudo&nbsp;:',
+'forum_votre_email' => 'Courriel (non&nbsp;publi&eacute;)&nbsp;:',
+'forum_url' => 'Votre site web (facultatif)&nbsp;:',
+
+'forum_texte' => 'Message&nbsp;:',
+'info_creation_paragraphe' => 'Ce formulaire accepte les <a href="http://www.spip.net/fr_article1578.html" rel="external nofollow" class="spip_out">raccourcis&nbsp;SPIP</a> et le code&nbsp;HTML. Pour cr&eacute;er des paragraphes, laissez simplement des lignes vides.',
+
+'forum_voir_avant' => 'Pr&eacute;visualiser',
+'forum_message_definitif' => 'Confirmer l\'envoi',
+
+);
+
+?>
