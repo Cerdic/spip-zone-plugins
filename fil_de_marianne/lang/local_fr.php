2d1bba4311e058a1e613ab4ef12a7146af5386ce
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-01-10 23:11:45 +0000

    Dépôt du plugin "Fil de Marianne". Correction et mise en plugin du squelette dédié aux articles syndiqués
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@34390 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ fil_de_marianne/lang/local_fr.php
@@ -0,0 +1,48 @@
+<?php
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// A
+'articles_recents' => 'Articles r&eacute;cents',
+'articles_moins_consultes' => 'Articles les moins consult&eacute;s',
+'articles_plus_consultes' => 'Articles les plus consult&eacute;s',
+'adaptation_pour_spip_par' => 'Adaptation graphique pour SPIP par ',
+'administration' => 'Administration',
+'archives' => 'Archives',
+
+// C
+'conception_graphique_par' => 'Habillage visuel (c)',
+'commentaire' => 'commentaire',
+'commentaires' => 'commentaires',
+
+// F
+'featured' => 'En vedette',
+
+// H
+'headline' => '&Agrave; la une',
+
+// L
+'lire_la_suite' => 'Lire la suite',
+'lire_la_suite_de' => ' de ',
+
+// M
+'mots_cles' => 'Mots cl&eacute;s',
+
+// P
+'parcourir_rubriques' => 'Parcourir les rubriques',
+'parcourir_sites' => 'Parcourir les sites r&eacute;f&eacute;renc&eacute;s',
+'propulse_par' => 'Propuls&eacute; par ',
+
+// S
+'site_pas_description' => 'Ce site n\'a pas de description',
+'sous_licence' => 'sous Licence',
+'sur' => ' sur :',
+'sites_dans_la_rubrique' => 'Sites dans la rubrique',
+
+// V
+'visites' => 'visites',
+'voir_tous_articles_dans' => 'Voir tous les articles dans',
+'voir_en_ligne' => 'Voir en ligne',
+);
+?>
