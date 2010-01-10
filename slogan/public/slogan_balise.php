5477fd6fa01f7c773dd8f8db94dffa67514507c0
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-01-10 16:26:40 +0000

    un filtre slogan_aleatoire qui genere un slogan du meilleur effet : [<h1>(#VAL{SPIP}|slogan_aleatoire)</h1>]
    On peut lui passer une langue en argument :
    [<h1>(#VAL{SPIP}|slogan_aleatoire{fr})</h1>]
    [<h1>(#VAL{SPIP}|slogan_aleatoire{en})</h1>]
    [<h1>(#VAL{SPIP}|slogan_aleatoire{de})</h1>]
    
    seuls fr,de et en sont implementes, mais le calcul d'un slogan dans une langue peut etre personalise ou enrichi par une fonction surchargeable.
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@34362 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- slogan/public/slogan_balise.php
+++ slogan/public/slogan_balise.php
@@ -15,4 +15,61 @@ function balise_SLOGAN_SITE_SPIP_dist($p) {
 	return $p;
 }
 
+function slogan_aleatoire($titre,$lang=null){
+	static $slogan = array();
+	if (is_null($lang)){
+		$lang = $GLOBALS['spip_lang'];
+	}
+	if (isset($slogan[$lang]))
+		return $slogan[$lang];
+	if (!$slogan_aleatoire = charger_fonction("slogan_aleatoire_".$lang,"public",true)){
+		$slogan_aleatoire = charger_fonction("slogan_aleatoire_fr","public");
+		return $slogan[$lang] = "<span lang='$lang'>".$slogan_aleatoire($titre)."</span>";
+	}
+	return $slogan[$lang] = $slogan_aleatoire($titre);
+}
+
+function public_slogan_aleatoire_fr_dist($titre){
+	define('_URL_SLOGAN_ALEATOIRE_FR',"http://hellday.free.fr/slogans/sloganfr.php?chaine=%s");
+	$slogan = "";
+
+	$url = str_replace("%s", urlencode($titre), _URL_SLOGAN_ALEATOIRE_FR);
+	include_spip('inc/distant');
+	if ($res = recuperer_page($url)){
+		$res = extraire_balises($res,'body');
+		$res = reset($res);
+		$res = charset2unicode($res,"iso-8859-1");
+		$res = preg_replace(",<style[^>]*>.*</style>,Uims","",$res);
+		$res = preg_replace(",<h1[^>]*>.*</h1>,Uims","",$res);
+		$res = preg_replace(",<form[^>]*>.*</form>,Uims","",$res);
+		$slogan = trim(textebrut($res));
+	}
+	return $slogan;
+}
+
+function public_slogan_aleatoire_en_dist($titre){
+	define('_URL_SLOGAN_ALEATOIRE_EN',"http://www.sloganizer.net/en/outbound.php?slogan=%s");
+	$slogan = "";
+
+	$url = str_replace("%s", urlencode($titre), _URL_SLOGAN_ALEATOIRE_EN);
+	include_spip('inc/distant');
+	if ($res = recuperer_page($url)){
+		$slogan = trim(textebrut($res));
+	}
+	return $slogan;
+}
+
+function public_slogan_aleatoire_de_dist($titre){
+	define('_URL_SLOGAN_ALEATOIRE_DE',"http://www.sloganizer.net/outbound.php?slogan=%s");
+	$slogan = "";
+
+	$url = str_replace("%s", urlencode($titre), _URL_SLOGAN_ALEATOIRE_DE);
+	include_spip('inc/distant');
+	if ($res = recuperer_page($url)){
+		$slogan = trim(textebrut($res));
+	}
+	return $slogan;
+}
+
+
 ?>
