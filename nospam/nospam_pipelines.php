e8f9bc66a737ff13ce4be4696e95bf1fc04cef0d
brunobergot@gmail.com <brunobergot@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-01-12 20:06:49 +0000

    version 0.7 : on branche nospam sur le formulaire_ecrire_auteur
    // moins de 10 caracteres dans le texte sans les liens = spam !
    // si un lien dans le sujet = spam !
    // si un lien a un titre de moins de 3 caracteres = spam !
    // si le texte contient plus de trois liens = spam !
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@34414 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- nospam/nospam_pipelines.php
+++ nospam/nospam_pipelines.php
@@ -8,6 +8,7 @@
 
 // pour verifier le nobot et le jeton sur un formulaire, l'ajouter a cette globale
 $GLOBALS['formulaires_no_spam'][] = 'forum';
+$GLOBALS['formulaires_no_spam'][] = 'ecrire_auteur';
 
 
 /**
@@ -79,6 +80,38 @@ function nospam_formulaire_verifier($flux){
 			if ($caracteres < 10){
 				$flux['data']['texte'] = _T('forum_attention_dix_caracteres');
 				unset($flux['data']['previsu']);
+			}
+		}
+	}
+	if ($form=='ecrire_auteur'){
+		if (!isset($flux['data']['texte_message_auteur'])){
+			include_spip("inc/nospam");
+			include_spip("inc/texte");
+			// regarder si il y a du contenu en dehors des liens !
+			$texte_message_auteur = _request('texte_message_auteur');
+			$caracteres = compter_caracteres_utiles($texte_message_auteur);
+			if ($caracteres < 10){
+				$flux['data']['texte_message_auteur'] = _T('forum_attention_dix_caracteres');
+				unset($flux['data']['previsu']);
+			}
+			// on analyse le sujet
+			$infos_sujet = analyser_spams(_request('sujet_message_auteur'));
+			// si un lien dans le sujet = spam !
+			if ($infos_sujet['nombre_liens'] > 0){
+				$flux['data']['sujet_message_auteur'] = _T('nospam:erreur_spam');
+				unset($flux['data']['previsu']);
+			}
+
+			// on analyse le texte
+			$infos_texte = analyser_spams($texte_message_auteur);
+			if ($infos_texte['nombre_liens'] > 0) {
+				// si un lien a un titre de moins de 3 caracteres = spam !
+				if ($infos_texte['caracteres_texte_lien_min'] < 3) {
+					$flux['data']['texte_message_auteur'] = _T('nospam:erreur_spam');
+				}
+				// si le texte contient plus de trois liens = spam !
+				if ($infos_texte['nombre_liens'] >= 3)
+					$flux['data']['texte_message_auteur'] = _T('nospam:erreur_spam');
 			}
 		}
 	}
