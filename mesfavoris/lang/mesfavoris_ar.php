b3c661a6cceed2511035d315bfc45f10ec48bd57
devmed@hotmail.fr <devmed@hotmail.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-01-13 13:40:16 +0000

    une traduction de plus...
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@34428 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ mesfavoris/lang/mesfavoris_ar.php
@@ -0,0 +1,29 @@
+﻿<?php
+
+
+	/**
+	 * SPIP-Formulaires
+	 *
+	 * Copyright (c) 2006-2009
+	 * Agence Artégo http://www.artego.fr
+	 *  
+	 * Ce programme est un logiciel libre distribue sous licence GNU/GPLv3.
+	 * Pour plus de details voir http://www.gnu.org/licenses/gpl-3.0.html
+	 *  
+	 **/
+
+
+	$GLOBALS[$GLOBALS['idx_lang']] = array(
+		'add' => 'إضافة',
+		'remove' => 'سحب',
+
+		'add_to' => 'إضافة إلى قائمة المفضلات',
+		'remove_from' => 'سحب من قائمة المفضلات',
+
+		'mes_favoris' => 'قائمة المفضلات',
+
+		'login_first' => '.المعذرة، يجب عليك التسجيل قبل القيام بهذه الخطوة. الرجاء إدخال إسمك وشفرتك السرية'
+		
+	);
+
+?>
