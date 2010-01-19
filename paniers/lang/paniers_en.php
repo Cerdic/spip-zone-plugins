74e17f3e71bd2d07bac44638e8709b1b2de57705
cyril@cym.fr <cyril@cym.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-01-19 10:38:39 +0000

    1er dépot; fichiers de base vides.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@34573 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ paniers/lang/paniers_en.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Paniers pour Spip 2.0
+ * Licence GPL
+ * Auteur Cyril Marion - Ateliers CYM
+ *
+ */
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+    // P
+	'paniers' => 'Caddie'
+
+);
+
+?>
---
+++ paniers/lang/paniers_fr.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Paniers pour Spip 2.0
+ * Licence GPL
+ * Auteur Cyril Marion - Ateliers CYM
+ *
+ */
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+    // P
+	'paniers' => 'Paniers'
+
+);
+
+?>
