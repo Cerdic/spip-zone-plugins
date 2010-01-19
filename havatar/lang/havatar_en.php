cb6694c5388143114b9c13f56573550561051800
da@weeno.net <da@weeno.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-01-19 08:06:35 +0000

    Traduction des fichiers de langue en espagnol et anglais
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@34569 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ havatar/lang/havatar_en.php
@@ -0,0 +1,13 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+'havatar_info' => 'Your avatar will be fetched from the URL of the website you indicated in your author profile, provided that it uses <a rel="nofollow external" href="http://microformats.org/" hreflang="en" rel="nofollow">microformats</a>.',
+'havatar_info_forum' => 'Your avatar will be fetched from your website, provided it uses <a rel="nofollow external" href="http://microformats.org/" hreflang="en" rel="nofollow">microformats</a>.',
+'titre_havatar_auteur' => 'AUTHOR\'S HAVATAR',
+
+);
+
+?>
---
+++ havatar/lang/havatar_es.php
@@ -0,0 +1,13 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+'havatar_info' => 'Su avatar ser&aacute; bajado siguiendo la direcci&oacute;n indicada en su perfil para su sitio web, a condici&oacute;n de que &eacute;ste use los <a rel="nofollow external" href="http://microformats.org/" hreflang="en" rel="nofollow">microformatos</a>.',
+'havatar_info_forum' => 'Su avatar ser&aacute; bajado desde su sitio web, a condici&oacute;n de que &eacute;ste use los <a rel="nofollow external" href="http://microformats.org/" hreflang="en" rel="nofollow">microformatos</a>.',
+'titre_havatar_auteur' => 'HAVATAR DE LA AUTORA O DEL AUTOR',
+
+);
+
+?>
