f5cc25c09cb1bf21f4347db157d744738c7ea529
da@weeno.net <da@weeno.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-01-19 07:56:30 +0000

    Utilisation d'entités HTML dans le fichier de langue
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@34568 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- havatar/lang/havatar_fr.php
+++ havatar/lang/havatar_fr.php
@@ -4,8 +4,8 @@
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-'havatar_info' => 'Votre avatar sera récupéré d\'après l\'URL que vous avez indiquée dans votre profil, à condition que celui-ci utilise les microformats.',
-'havatar_info_forum' => 'Votre avatar sera récupéré sur votre site, à condition que celui-ci utilise les microformats.',
+'havatar_info' => 'Votre avatar sera r&eacute;cup&eacute;r&eacute; d\'apr&egrave;s l\'URL que vous avez indiqu&eacute;e dans votre profil, &agrave; condition que celui-ci utilise les <a rel="nofollow external" href="http://microformats.org/" rel="nofollow">microformats</a>.',
+'havatar_info_forum' => 'Votre avatar sera r&eacute;cup&eacute;r&eacute; sur votre site, &agrave; condition que celui-ci utilise les <a rel="nofollow external" href="http://microformats.org/" rel="nofollow">microformats</a>.',
 'titre_havatar_auteur' => 'HAVATAR DE L\'AUTEUR',
 
 );
