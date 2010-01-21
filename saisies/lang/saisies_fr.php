9f759ff39a4b032727ff16728dd96c4096c550ac
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-01-21 09:04:10 +0000

    Une coquille dans generer_saisies + les chaines de langue de la saisie Destinataires.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@34621 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- saisies/lang/saisies_fr.php
+++ saisies/lang/saisies_fr.php
@@ -7,6 +7,8 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// O
 	'option_attention_explication' => 'Un message plus important que l\'explication.',
 	'option_attention_label' => 'Avertissement',
+	'option_choix_destinataires_label' => 'Destinataires possibles',
+	'option_choix_destinataires_explication' => 'Un ou plusieurs auteurs parmis lesquels l\'utilisateur pourra faire son choix. Si rien n\'est s&eacute;lectionn&eacute;, c\'est l\'auteur qui a install&eacute; le site qui sera choisi.',
 	'option_class_label' => 'Classes CSS suppl&eacute;mentaires',
 	'option_defaut_label' => 'Valeur par d&eacute;faut',
 	'option_disable_avec_post_explication' => 'Identique &agrave; l\'option pr&eacute;c&eacute;dente mais poste quand m&ecirc;me la valeur dans un champ cach&eacute;.',
@@ -21,7 +23,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'option_label_label' => 'Label',
 	'option_maxlength_explication' => 'L\'utilisateur ne pourra pas taper plus de caract&egrave;res que ce nombre.',
 	'option_maxlength_label' => 'Nombre de caract&egrave;res maximum',
-	'option_nom_explication' => 'La valeur de l\'attribut "name".',
+	'option_nom_explication' => 'Un nom informatique qui identifiera le champ. Il ne doit contenir que des caractères alpha-numériques minuscules ou le caractère "_".',
 	'option_nom_label' => 'Nom du champ',
 	'option_obligatoire_label' => 'Champ obligatoire',
 	'option_readonly_explication' => 'Le champ peut &ecirc;tre lu, s&eacute;lectionn&eacute;, mais pas modifi&eacute;.',
@@ -29,6 +31,9 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'option_size_explication' => 'Largeur du champ en nombre de caract&egrave;res. Cette option n\'est pas toujours appliqu&eacute;e car les styles CSS de votre site peuvent l\'annuler.',
 	'option_size_label' => 'Taille du champ',
 	'option_type_explication' => 'En mode "mot de passe", le contenu du champ sera masqu&eacute;.',
+	'option_type_choix_un' => 'Permettre à l\'utilisateur de choisir <strong>un seul</strong> destinataire.',
+	'option_type_choix_plusieurs' => 'Permettre à l\'utilisateur de choisir <strong>plusieurs</strong> destinataires.',
+	'option_type_choix_tous' => 'Mettre <strong>tous</strong> ces auteurs en destinataires. L\'utilisateur n\'aura aucun choix.',
 	'option_type_label' => 'Type du champ',
 	'option_type_text' => 'Normal',
 	'option_type_password' => 'Mot de passe',
@@ -36,6 +41,8 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// S
 	'saisie_base_titre' => 'Param&egrave;tres g&eacute;n&eacute;raux',
 	'saisie_base_plus_titre' => 'Param&egrave;tres suppl&eacute;mentaires',
+	'saisie_destinataires_titre' => 'Destinataires',
+	'saisie_destinataires_explication' => 'Permet de choisir un ou plusieurs destinataires parmis des auteurs pr&eacute;-s&eacute;lectionn&eacute;e.',
 	'saisie_input_explication' => 'Une simple ligne de texte, pouvant &ecirc;tre visible ou masqu&eacute;e (mot de passe).',
 	'saisie_input_titre' => 'Texte simple',
 	
