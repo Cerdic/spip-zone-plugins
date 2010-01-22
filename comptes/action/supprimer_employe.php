be6839a14b52854a15c7abf831d9fa703e322f52
xdjuj@apsulis.com <xdjuj@apsulis.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-01-22 18:50:13 +0000

    Poser les bases d'une gestion des utilisateurs depuis la partie privée : - ajout de bonhommes - suppression de bonhommes - affichage/edition des bonhommes - merci les crayons
    TODO : adapter au projet présent, tables et fichiers de langues à adapter
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@34647 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ comptes/action/supprimer_employe.php
@@ -0,0 +1,32 @@
+<?php
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+/**
+ * Supprimer un employé comptes
+ *
+ */
+/*
+	TODO Peut etre gerer en deux temps :
+	1) Modale pour dire "etes vous surs"
+	2) Bouton d'action dans la modale
+*/
+
+function action_supprimer_employe_dist() {
+	$securiser_action = charger_fonction('securiser_action','inc');
+	$id = $securiser_action();
+	supprimer_employe($id);
+	return true;
+}
+
+function supprimer_employe($comptes){
+	include_spip('base/abstract_sql');
+
+	$n = sql_delete("spip_auteurs_comptes_specifique", "id_comptes=".intval($comptes));
+
+	if($n){
+		spip_log("Suppression de l'employé $comptes");
+		echo _T('gestion:suppression_employe', array('id'=>$comptes));
+		return true;
+	}	
+	return false;
+}
