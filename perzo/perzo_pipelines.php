35392e8270000f7dddc197eeae5d01fe341a4022
cy_altern@yahoo.fr <cy_altern@yahoo.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-01-27 01:28:49 +0000

    remplacement de perso.css par sa version calculee perso.css.html
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@34748 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- perzo/perzo_pipelines.php
+++ perzo/perzo_pipelines.php
@@ -1,7 +1,7 @@
 <?php
 
 function perzo_insert_head($flux){
-		$flux .= "\r\n".'<link rel="stylesheet" href="'.find_in_path('perso.css').'" type="text/css" media="all" />'."\r\n";
+		$flux .= "\r\n".'<link rel="stylesheet" href="spip.php?page=perso.css" type="text/css" media="all" />'."\r\n";
 	  return $flux;
 }
 
