b08c8c4e74ba75f752be571c2a8b99da13e2cf56
marcimat@rezo.net <marcimat@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-01-31 12:43:39 +0000

    Compatibilité de l'installateur en version 2.1. Quelle simplification !
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@34812 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- pages/base/pages_installation.php
+++ pages/base/pages_installation.php
@@ -9,70 +9,32 @@
 
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
-// La fonction de base appelée par le gestionnaire de plugins
-function pages_install($install){
-	
-	//recupère les informations de plugin.xml
-	find_in_path('plugin.php', 'inc/', true);
-	$infos = plugin_get_infos(_DIR_PLUGIN_PAGES);
-	$version_du_plugin = $infos['version'];
-	
-	switch($install) {
-		case 'test':
-			return isset($GLOBALS['meta']['pages_version_base']) AND ($GLOBALS['meta']['pages_version_base']>=$version_du_plugin);
-			return true;
-		case 'install':
-			pages_maj();
-			break;
-		case 'uninstall':
-			pages_vider_tables();
-			break;
-	}
-	
-}
+include_spip('inc/meta');
 
-// Supprimer les tables du plugin
-function pages_vider_tables() {
-	find_in_path('abstract_sql.php', 'base/', true);
-	sql_alter('table spip_articles drop page');
-	effacer_meta('pages_version_base');
-}
 
-// Met à jour le numéro de version dans les métas
-function pages_maj_version(&$v1, $v2) {
-	echo "MAJ Pages : $v1 =&gt; $v2<br />";
-	ecrire_meta('pages_version_base', $v1=$v2, 'non');
-}
+function pages_upgrade($nom_meta_base_version,$version_cible){
+	$current_version = 0.0;
 
-// Là où se trouve les mises à jour
-function pages_maj(){
-	
-	//recupère les informations de plugin.xml
-	find_in_path('plugin.php', 'inc/', true);
-	$infos = plugin_get_infos(_DIR_PLUGIN_PAGES);
-	$version_du_plugin = $infos['version'];
-	$version_en_cours = isset($GLOBALS['meta']['pages_version_base'])
-		?$GLOBALS['meta']['pages_version_base']
-		:0.0;
-	
-	// On met à jour seulement si les versions sont différentes
-	if ($version_en_cours != $version_du_plugin){
-		
-		find_in_path('create.php', 'base/', true);
-		include_spip('abstract_sql.php', 'base/', true);
-				
-		if ($version_en_cours == 0.0){
-			creer_base();
-			sql_alter('table spip_articles add `page` varchar(20) not null');
-			pages_maj_version($version_en_cours, 0.1);
+	if ((!isset($GLOBALS['meta'][$nom_meta_base_version]) )
+			|| (($current_version = $GLOBALS['meta'][$nom_meta_base_version])!=$version_cible)) {
+		if ($current_version==0.0) {
+			include_spip('base/create');
+			maj_tables('spip_articles');
+			ecrire_meta($nom_meta_base_version, $current_version=$version_cible, 'non');
 		}
-		
-		/*if ($version_en_cours < ($version_de_test = 0.2)){
-			//$desc = $showtable("spip_jeux", true);
-			
-			pages_maj_version($version_en_cours, $version_de_test);
-		}*/
+		// remise a jour du nouveau version base pour les anciennes installations.
+		if ($current_version<1.0){
+			ecrire_meta($nom_meta_base_version, $current_version=$version_cible, 'non');				
+		}
 	}
 }
+
+// Supprimer la colonne 'page' du plugin
+function pages_vider_tables($nom_meta_base_version) {
+	sql_alter("TABLE spip_articles DROP page");
+	effacer_meta($nom_meta_base_version);
+}
+
+
 
 ?>
