b6f5ad43d70427898ec99eaefd7e1cb3cadc7245
fil@rezo.net <fil@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-02-05 09:05:48 +0000

    corrections anglais
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@34932 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- authentification/openid/lang/openid_en.php
+++ authentification/openid/lang/openid_en.php
@@ -6,22 +6,22 @@
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// E
   'erreur_openid' => 'OpenID authentication error: have you entered a valid OpenID?',
-	'erreur_librairies' => 'Unable to found OpenID authentification Libraries',
+	'erreur_librairies' => 'Unable to locate OpenID authentification libraries',
 	// F
 	'form_login_openid' => 'You can use your <a href=\'http://openid.net/\'>OpenID</a> as login',
-	'form_forum_indiquer_openid' => 'You can sign up on this site with your OpenID adress',
-	'form_login_openid_ok' => 'This login use OpenID.',
+	'form_forum_indiquer_openid' => 'You can sign up on this site with your OpenID address',
+	'form_login_openid_ok' => 'This login uses OpenID.',
 	'form_login_openid_pass' => 'Connect with password.',
 	'form_forum_openid' => 'Sign up with OpenID',
-	'form_pet_votre_openid' => 'OpenID Login',
+	'form_pet_votre_openid' => 'OpenID login',
 
 	// L
-  'login_login2' => 'Login (identifier for connection to the site) or OpenID&nbsp;:',
+  'login_login2' => 'Login (identifier for connection to the site) or OpenID:',
 	// O
   'openid' => 'OpenID',
 	// U
-  'utilisateur_inconnu' => 'User unknown on this site',
+  'utilisateur_inconnu' => 'Unknown user'
 	// V
-  'verif_refusee' => 'Authentication request canceled',
+  'verif_refusee' => 'Authentication request cancelled',
 	
 );
