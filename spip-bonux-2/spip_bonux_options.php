991e9e3b752a53d1fd81ffe6cc48243e5e066966
joseph@larmarange.net <joseph@larmarange.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-02-05 15:52:24 +0000

    ajout d'une fonction _TT() pour les chaines de langue dans les fichiers de conf
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@34946 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ spip-bonux-2/spip_bonux_options.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Plugin Spip-Bonux
+ * Le plugin qui lave plus SPIP que SPIP
+ * (c) 2008 Mathieu Marcillaud, Cedric Morin, Romy Tetue
+ * Licence GPL
+ * 
+ */
+
+ /**
+ * une fonction qui regarde si $texte est une chaine de langue
+ * de la forme <:qqch:>
+ * si oui applique _T()
+ * si non applique typo()
+ */
+function _TT($texte, $args=array()) {
+	
+	if (preg_match('/^\<:(.*?):\>$/',$texte,$match)) {
+		static $traduire=false ;
+		if (!$traduire) {
+			$traduire = charger_fonction('traduire', 'inc');
+			include_spip('inc/lang');
+		}
+		$text = $traduire($match[1],$GLOBALS['spip_lang']);
+	}
+	
+	else {
+		include_spip('inc/texte');
+		$text = typo($texte);
+	}
+
+	if (is_array($args))
+	foreach ($args as $name => $value)
+		$text = str_replace ("@$name@", $value, $text);
+
+	return $text;
+
+}
+
+?>
