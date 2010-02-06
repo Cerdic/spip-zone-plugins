8d7b5cde05daced20972d4a8ae77c879f9d4de02
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-02-06 17:14:46 +0000

    Généralisation aussi sur la Bio des auteurs
    Et hauteur de 320px effective sur le Texte des article.
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@34979 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- porte_plume_extras/partout/javascript/barre_generalisee.js
+++ porte_plume_extras/partout/javascript/barre_generalisee.js
@@ -2,7 +2,7 @@
 $(document).ready(function(){
 	function addbarre(){
 	$('.formulaire_spip').not('.formulaire_spip_compact')
-		.find('.editer_ps,.editer_descriptif,.editer_chapo')
+		.find('.editer_ps,.editer_descriptif,.editer_chapo,.editer_bio')
 		.find('textarea')
 		.barre_outils('edition');
 	}
