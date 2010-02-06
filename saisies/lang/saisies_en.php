6d49217ab744c7c8a73fa47928d4a6286d99723a
superyms@yahoo.fr <superyms@yahoo.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-02-06 08:43:04 +0000

    début du multilinguisme, anglais (espagnol et arabe plus tard).
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@34972 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ saisies/lang/saisies_en.php
@@ -0,0 +1,56 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	// O
+	'option_attention_explication' => 'A message more important than the explanation.',
+	'option_attention_label' => 'Warning',
+	'option_choix_destinataires_label' => 'Possible recipients',
+	'option_choix_destinataires_explication' => 'One or several authors among which the user can make his choice. If nothing selected, it will be the author who installed the site to be chosen.',
+	'option_class_label' => 'Additional CSS Classes',
+	'option_defaut_label' => 'Default value',
+	'option_disable_avec_post_explication' => 'Same as previous option position but still post value in a hidden field.',
+	'option_disable_avec_post_label' => 'Disabled but posted.',
+	'option_disable_explication' => 'The field can not get the focus.',
+	'option_disable_label' => 'Disable the field',
+	'option_explication_explication' => 'If necessary, a short sentence describing the subject field.',
+	'option_explication_label' => 'Explanation',
+	'option_info_obligatoire_explication' => 'You can modify (or delete) the default indication of obligation.',
+	'option_info_obligatoire_label' => 'Indication of obligation',
+	'option_label_explication' => 'The title that will be displayed.',
+	'option_label_label' => 'Label',
+	'option_maxlength_explication' => 'The user can not type more characters than this number.',
+	'option_maxlength_label' => 'Maximum number of characters',
+	'option_nom_explication' => 'A digital name that identifies the field. It should contain only lowercase alphanumeric characters or the character "_".',
+	'option_nom_label' => 'Field name',
+	'option_obligatoire_label' => 'Required field',
+	'option_readonly_explication' => 'The field can be viewed, selected, but not modified.',
+	'option_readonly_label' => 'Read only',
+	'option_size_explication' => 'Field width in characters. This option is not always applied/used because the CSS styles of your site can cancel it.',
+	'option_size_label' => 'Field size',
+	'option_type_explication' => 'Mode "password", the contents of the field will be hidden.',
+	'option_type_choix_un' => 'Allow the user to choose <strong>one</ strong> recipient.',
+	'option_type_choix_plusieurs' => 'Allow the user to choose <strong>many</ strong> recipients.',
+	'option_type_choix_tous' => 'Make <strong>all</ strong> these authors as recipients. The user will not have choice.',
+	'option_type_label' => 'Field type',
+	'option_type_text' => 'Normal',
+	'option_type_password' => 'Password',
+	
+	// S
+	'saisie_base_titre' => 'General settings',
+	'saisie_base_plus_titre' => 'Additional settings',
+	'saisie_destinataires_titre' => 'Recipients',
+	'saisie_destinataires_explication' => 'Allows you to select one or more recipients among the pre-selected authors .',
+	'saisie_input_explication' => 'A simple line of text that can be visible or hidden (password).',
+	'saisie_input_titre' => 'Plain text',
+	
+	// T
+	'tous_visiteurs' => 'All visitors (even if not registered)',
+
+	// Z
+	'z' => 'zzz'
+);
+
+?>
