c10dab46687e57eb9ad8946d9264604205718c5b
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-02-07 16:41:17 +0000

    Comme ça, la dimension du champ #TEXTE est bien fixée par PPP (Porte Plume Partout). Mais c'est au prix d'un fork sur un fichier du core de SPIP (juste une suppression d'un javascript inline qui n'a sans doute pas sa place en inline. Et qui surtout, n'a plus de raison d'être depuis que PP est intégré à SPIP 2.1 (puisque PP permet de redimensionner manuellement le champd de texte).
    Si un core dev a le cœur à enlever ce js inline du core...
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@34983 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- porte_plume_extras/partout/javascript/barre_generalisee.js
+++ porte_plume_extras/partout/javascript/barre_generalisee.js
@@ -5,6 +5,8 @@ $(document).ready(function(){
 		.find('.editer_ps,.editer_descriptif,.editer_chapo,.editer_bio')
 		.find('textarea')
 		.barre_outils('edition');
+	$('#text_area')
+		.height(($(window).height()/2.2)+'px');
 	}
 	addbarre();
 	onAjaxLoad(addbarre);
