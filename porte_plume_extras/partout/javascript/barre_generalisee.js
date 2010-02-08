3237e7b5e669735f01a4f5e18c1826ae3ffacb56
marcimat@rezo.net <marcimat@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-02-08 08:48:06 +0000

    Pas de redimensionnement auto du champ. D'une part ce n'est pas l'objet de ce plugin, d'autre part, il y a une poignée. On garde le code pour exemple cependant.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@34994 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- porte_plume_extras/partout/javascript/barre_generalisee.js
+++ porte_plume_extras/partout/javascript/barre_generalisee.js
@@ -5,8 +5,7 @@ $(document).ready(function(){
 		.find('.editer_ps,.editer_descriptif,.editer_chapo,.editer_bio')
 		.find('textarea')
 		.barre_outils('edition');
-	$('#text_area')
-		.height(($(window).height()/2.2)+'px');
+	/*$('#text_area').height(($(window).height()/2.2)+'px');*/
 	}
 	addbarre();
 	onAjaxLoad(addbarre);
