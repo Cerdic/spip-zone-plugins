66c49be1166f727355b75d24f0a8a2b3ed6dac97
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-02-08 15:35:43 +0000

    Tenir compte de la CFG (il faut vider le cache de SPIP pour voir la différence)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@35015 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- porte_plume_extras/partout/ppp_pipelines.php
+++ porte_plume_extras/partout/ppp_pipelines.php
@@ -1,8 +1,5 @@
 <?php
 
-// Recalculer le cache si la config du site change
-$GLOBALS['marqueur'] .= ":".md5($GLOBALS['meta']['ppp']); // Sur un conseil de Cedric : http://permalink.gmane.org/gmane.comp.web.spip.zone/6258
-
 //Appel du pipeline qui ajoute des barres de porte-plume partout dans Descriptif, chapo, PS, Bio et Descriptif du site
 function ppp_insert_head_prive($flux){
 	$js = generer_url_public('barre_generalisee.js');
