0f0d335ebfc5b14aa26d53ebca003dc3d853a0af
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-02-08 14:30:15 +0000

    Rendre le js dynamique.
    J'ai dû le passer dans le insert head privé parce que sinon, il n'était pas inclus :(
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@35011 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- porte_plume_extras/partout/ppp_pipelines.php
+++ porte_plume_extras/partout/ppp_pipelines.php
@@ -1,11 +1,13 @@
 <?php
 
-//Appel du pipeline qui ajoute des barres de porte-plume partout dans Descriptif, chapo et PS
+// Recalculer le cache si la config du site change
+$GLOBALS['marqueur'] .= ":".md5($GLOBALS['meta']['ppp']); // Sur un conseil de Cedric : http://permalink.gmane.org/gmane.comp.web.spip.zone/6258
 
-// function préfixe_pipelineexistant ($nomdeboucle) {}
-function ppp_jquery_plugins($js) {
-	$js[] = 'javascript/barre_generalisee.js';
-	return $js;
+//Appel du pipeline qui ajoute des barres de porte-plume partout dans Descriptif, chapo, PS, Bio et Descriptif du site
+function ppp_insert_head_prive($flux){
+	$js = generer_url_public('barre_generalisee.js');
+	$flux .= "<script type='text/javascript' src='$js'></script>\n";
+	return $flux;
 }
 
 ?>
