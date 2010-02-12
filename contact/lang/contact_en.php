8cfa904afe7811a60a5f3bc94b0a0f353f29969f
superyms@yahoo.fr <superyms@yahoo.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-02-12 21:09:18 +0000

    Mise à jour fichiers de langue (anglais + arabe)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@35118 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- contact/lang/contact_en.php
+++ contact/lang/contact_en.php
@@ -21,22 +21,41 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'champ_mail' => 'Mail',
 	'champ_impose' => 'field imposed',
 	
+	'configuration_barre_oui' => 'View the complete bar typo.',
+	'configuration_no_previsualisation' => 'Show typo toolbar without preview.',
+	'configuration_no_barre' => 'Do not show typo toolbar.',
+	'configuration_champs_message' => 'PenHolder Settings.',
+	'configuration_champs_message_explication' => 'Typo toolbar settings, associated to the text\'s message.',
 	'configuration_description' => 'Customize your contact form.',
+
 	'configuration_destinataires' => 'Recipients',
 	'configuration_destinataires_explication' => 'Choose one or more authors recipients your visitors may contact. When not defined, the first author will be used (the person who installed the website).',
-	'configuration_destinataires_tous' => 'Select <b>all</b> authors as contact email recipients',
+	'configuration_regle_liste_explication' => 'Select a calculation rule from the list of potential recipients and the selection method proposed to the visitors.',
+	'configuration_destinataires_tous' => 'Make <b>all</b> authors as contact email recipients',
+	'configuration_destinataires_tous_ou' => 'Make <b>all</b> these authors as recipients of the email or only those passed to the form',
+	'configuration_destinataires_tous_et' => 'Make <b>all</b> these authors as recipients of the email and add the group of authors passed to the form',
 	'configuration_destinataires_un' => 'Allow visitors to choose <b>a single</b> recipient author',
+	'configuration_destinataires_un_ou' => 'Force visitors to choose <b>a single</b> recipient from the selected list above or from the group of authors eventually passed to the form',
+	'configuration_destinataires_un_et' => 'Force visitors to choose <b>a single</b> recipient from the selected list above andadd those from the group of authors eventually passed to the form.',
 	'configuration_destinataires_plusieurs' => 'Allow visitors to choose among <b>several</b> recipient authors',
+	'configuration_destinataires_plusieurs_ou' => 'Allow visitors to choose among <b>several</b> recipients from the selected list above or from the group of authors eventually passed to the form',
+	'configuration_destinataires_plusieurs_et' => 'Allow visitors to choose among <b>several</b> recipients from the selected list above andadd those from the group of authors eventually passed to the form.',
+	
+	
+	
+	
 	'configuration_intro' => 'Introduction',
 	'configuration_intro_explication' => 'You may here add a text that will introduce your contact form.',
+
 	'configuration_pj' => 'Attachement',
 	'configuration_pj_autoriser' => 'Allow adding attachement',
 	'configuration_pj_nb_max' => 'Maximum number of attachments',
 	'configuration_pj_facteur_absent' => 'You have to install the Facteur plugin to use this functionality.',
-	
+	'consulter_memoire' => 'This message is saved on your site:',
 	'courriel_de' => 'From',
 	'courriel_pour' => 'To',
 	
+	'deplacement_possible' => 'Vertical displacement by drag and drop',
 	'form_destinataire' => 'Choose a recipient',
 	'form_destinataires' => 'Choose recipients',
 	'form_pj_ajouter_pluriel' => 'Add attachements',
@@ -49,15 +68,36 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'form_pj_titre' => 'File title',
 	'forum_attention_nbre_caracteres' => '<b>Warning!</b> Your message needs to be at least @nbre_caract@ characters in length.',
 	
+
+
+
 	'horodatage' => 'Form posted on: @horodatage@.',
+	'inforigine' => 'Information from the form: ',
 	'infos_collecte' => 'Information collected',
 	'infos_supplementaires' => 'Additional Infos',
 	'informez_moi' => 'I agree to receive, by e-mail, information from your organisation.',
 	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	'sauv_message' => 'Save contact messages',
 	'sauv_message_explication' => 'Allow to save messages from contacts, and display them in a dedicated interface.',
 	'sauv_message_ok' => 'Save contact messages',
-	'succes' => 'Merci, votre message a bien &#233;t&#233; envoy&#233;.<br />Nous vous r&#233;pondrons d&#232;s que possible.<br />L\'&#233;quipe de @equipe_site@',
+	'succes' => 'Thank you, your message has been sent.<br />We will reply as soon as possible.<br />The team of @equipe_site@',
 	
 	'madame' => 'Madam',
 	'monsieur' => 'Mister',
@@ -71,7 +111,9 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'msg_supprimer_message' => 'Delete this message',
 	'msg_messagerie' => 'Contact messaging',
 	
+	
 	'titre' => 'Contact Form'
-);
+
+	);
 
 ?>
