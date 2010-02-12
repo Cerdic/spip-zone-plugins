5e9af134d08f025bfc729ffa2a8c979ac9118860
berio@alg-a.org <berio@alg-a.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-02-12 20:11:58 +0000

    spanish translation
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@35115 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ formulaire_upload/lang/formupload_es.php
@@ -0,0 +1,34 @@
+<?php
+
+	$GLOBALS[$GLOBALS['idx_lang']] = array(
+	'cfg_boite_formupload' => 'Configuración del plugin Formulaire upload',
+	'cfg_descr_formupload' => 'Este plugin permite introducir un formulario en la parte pública para adjuntar archivos a los elementos de SPIP.',
+	'cfg_lbl_files_accepted' => 'Formatos de los archivos aceptados',
+	'cfg_lbl_insert_code' => 'Código de inserción',
+	'cfg_lbl_files_number' => 'Nombre de los archivos aceptados',
+	'cfg_inf_files_accepted' => 'Separe los formatos de los arhivos mediante barras horizontales: pdf|txt|odt ou gif|jpg|png',
+	'cfg_inf_insert_code' => 'Ver el código de inserción de los documentos (útil si hay otro formulario para crear artículos, por ejemplo)',
+	'cfg_inf_files_number' => 'L\'utilisateur ne pourra uploader que ce nombre de fichiers au maximum',
+	'cfg_titre_formupload' => 'Formulaire upload',
+	'choosefiles' => 'Selecciona los archivos para subir',
+	'nodocs' => 'Pas encore de document sur la zone de t&eacute;l&eacute;chargement',
+	'insert_code' => 'Código para insertar en el campo de texto:',
+	'doc_delete' => 'Suprimir el archivo',
+	'doc_left' => 'Archivo flotando a la izquierda',
+	'doc_center' => 'Archivo centrado',
+	'doc_right' => 'Archivo flotando a la derecha',
+	'img_left' => 'Imagen flotante a la izquierda',
+	'img_center' => 'Imagen centrada',
+	'img_right' => 'Imagen flotando a la derecha',
+	'msg_doc_added' => 'Archivos subidos',
+	'msg_doc_deleted' => 'Archivos suprimidos',
+	'msg_fichier_doublon' => 'Ya has seleccionado el archivo.',
+	'msg_format_non_autorise' => 'Formato de archivo no autorizado.',
+	'msg_nothing_to_do' => 'No hay nada que hacer',
+	'remove' => 'suprimir',
+	'yourfiles' => 'Tus archivos',
+	'Z' => 'ZZzZZzzz'
+
+);
+
+?>
