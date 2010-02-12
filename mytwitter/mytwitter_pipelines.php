97ca6666e1c1965c1d358c654299d2f38f8fe734
yves.costiou@gmail.com <yves.costiou@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-02-12 23:45:31 +0000

    Le plugin mytwitter fournit un bloc de présentation de votre compte twitter. Les pramètres de personnalisation et de réglage sont accessible via un formulaire cfg (paramétrage du compte et des options, réglage de l'affichage) (fonctionnel, à compléter) le bloc est accessible : - via l'insertion d'un modèle (qui offre la possibilité de passer en paramètre un autre compte que celui décrit par cfg) (fonctionnel par #modele mais correction en cours pour insertion par <mytwitter> - via une page dédié pour les tuilisateurs de squelette de type Z (page=mytwitter)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@35125 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ mytwitter/mytwitter_pipelines.php
@@ -0,0 +1,30 @@
+<?php
+
+function mytwitter_insert_head($flux){
+	$flux	.= '<link rel="stylesheet" type="text/css" href="'.find_in_path('mytwitter.css').'" media="all" />'."\n"
+		.  "<script type='text/javascript' src='http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js'></script>\n"
+		.  "<script type='text/javascript' src='http://twitter-friends-widget.googlecode.com/files/jquery.twitter-friends-1.0.min.js'></script>\n";
+	return $flux;
+
+}
+
+
+
+function mytwitter_ajouter_boutons($flux) {
+	// si on est admin
+	if (autoriser('configurer','mytwitter')) {
+		$menu = "configuration";
+		$icone = "twitter-32.png";
+
+		// on voit le bouton dans la barre "configuration"
+		$flux[$menu]->sousmenu['cfg&cfg=mytwitter']= new Bouton(
+		_DIR_PLUGIN_MYTWITTER.$icone,  // icone
+		_T('mytwitter:my twitter'));
+	}
+	return $flux;
+}
+?>
+
+
+
+
