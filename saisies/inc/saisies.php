41c2eae824fb9ee88195dc091b9b0614dc12970c
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-02-13 22:51:26 +0000

    Nouvelles fonctions pratiques pour lister toutes les saisies disponibles (une saisie disponible c'est une saisie qui a été décrite correctement).
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@35154 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- saisies/inc/saisies.php
+++ saisies/inc/saisies.php
@@ -9,8 +9,8 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
  * Prend la description complète du contenu d'un formulaire et retourne
  * uniquement les saisies.
  *
- * @param array $contenu : le contenu d'un formulaire
- * @return array : un tableau avec uniquement les saisies
+ * @param array $contenu Le contenu d'un formulaire
+ * @return array Un tableau avec uniquement les saisies
  */
 function saisies_recuperer_saisies($contenu){
 	$saisies = array();
@@ -35,12 +35,60 @@ function saisies_recuperer_saisies($contenu){
  * Prend la description complète du contenu d'un formulaire et retourne
  * une liste des noms des champs du formulaire.
  *
- * @param array $contenu : le contenu d'un formulaire
- * @return array : un tableau listant les noms des champs
+ * @param array $contenu Le contenu d'un formulaire
+ * @return array Un tableau listant les noms des champs
  */
 function saisies_recuperer_champs($contenu){
 	$saisies = saisies_recuperer_saisies($contenu);
 	return array_keys($saisies);
+}
+
+/*
+ * Liste toutes les saisies configurables (ayant une description)
+ *
+ * @return array Un tableau listant des saisies et leurs options
+ */
+function saisies_lister_disponibles(){
+	static $saisies = null;
+	
+	if (is_null($saisies)){
+		$saisies = array();
+		$liste = find_all_in_path('saisies/', '.+[.]yaml$');
+		
+		if (count($liste)){
+			foreach ($liste as $fichier=>$chemin){
+				$type_saisie = preg_replace(',[.]yaml$,i', '', $fichier);
+				$dossier = str_replace($fichier, '', $chemin);
+				// On ne garde que les saisies qui ont bien le HTML avec !
+				if (file_exists("$dossier$type_saisie.html")
+					and (
+						is_array($saisie = saisies_charger_infos($type_saisie))
+					)
+				){
+					$saisies[$type_saisie] = $saisie;
+				}
+			}
+		}
+	}
+	
+	return $saisies;
+}
+
+/**
+ * Charger les informations contenues dans le yaml d'une saisie
+ *
+ * @param string $type_saisie Le type de la saisie
+ * @return array Un tableau contenant le YAML décodé
+ */
+function saisies_charger_infos($type_saisie){
+	include_spip('inc/yaml');
+	$fichier = find_in_path("saisies/$type_saisie.yaml");
+	$saisie = yaml_decode_file($fichier);
+	if (is_array($saisie)){
+		$saisie['titre'] = _T_ou_typo($saisie['titre']);
+		$saisie['explication'] = _T_ou_typo($saisie['explication']);
+	}
+	return $saisie;
 }
 
 ?>
