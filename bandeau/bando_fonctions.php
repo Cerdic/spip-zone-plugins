4b7379c9e3aaa4c05a015c28019e6b6086c89263
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-02-13 19:57:04 +0000

    ces deux filtres apparaissent autre part, les mettre dans le fichier fonctions
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@35151 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- bandeau/bando_fonctions.php
+++ bandeau/bando_fonctions.php
@@ -7,4 +7,31 @@
  */
 include_spip("public/slogan_balise");
 
+
+if (!function_exists('bando_images_background')){
+function bando_images_background(){
+	include_spip('inc/bandeau');
+	// recuperer tous les boutons et leurs images
+	$boutons = definir_barre_boutons(definir_barre_contexte(),true,false);
+
+	$res = "";
+	foreach($boutons as $page => $detail){
+		if ($detail->icone AND strlen(trim($detail->icone)))
+			$res .="\n.navigation_avec_icones #bando1_$page {background-image:url(".$detail->icone.");}";
+		$selecteur = ($page=='outils_rapides'?"":".navigation_avec_icones ");
+		if (is_array($detail->sousmenu))
+			foreach($detail->sousmenu as $souspage=>$sousdetail)
+				if ($sousdetail->icone AND strlen(trim($sousdetail->icone)))
+					$res .="\n$selecteur#bando2_$souspage {background-image:url(".$sousdetail->icone.");}";
+	}
+	return $res;
+}
+}
+if (!function_exists('bando_style_prive_theme')){
+function bando_style_prive_theme() {
+	if ($f = find_in_theme('style_prive_theme.html'))
+		return preg_replace(',[.]html$,Ui','',$f);
+	return '';
+}
+}
 ?>
