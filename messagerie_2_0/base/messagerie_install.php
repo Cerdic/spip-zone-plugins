bd04dc88062dd43fe2cd81b24f6e63a6227a39b2
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-02-13 18:36:07 +0000

    icone et version base
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@35145 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- messagerie_2_0/base/messagerie_install.php
+++ messagerie_2_0/base/messagerie_install.php
@@ -18,7 +18,7 @@
 		$current_version = 0.0;
 		if (   (!isset($GLOBALS['meta'][$nom_meta_base_version]) )
 				|| (($current_version = $GLOBALS['meta'][$nom_meta_base_version])!=$version_cible)){
-				ecrire_meta($nom_meta_base_version,$current_version='0.1.0.0','non');
+				ecrire_meta($nom_meta_base_version,$current_version='1.0.0','non');
 		}
 	}
 	
