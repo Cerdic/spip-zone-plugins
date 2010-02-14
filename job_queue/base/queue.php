33af4af9c3fbec65729e1611e94bf9e8c3d81c40
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-02-14 18:22:10 +0000

    une coquille dans l'install
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@35175 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- job_queue/base/queue.php
+++ job_queue/base/queue.php
@@ -64,7 +64,7 @@ function queue_upgrade($nom_meta_base_version,$version_cible){
 	if (   (!isset($GLOBALS['meta'][$nom_meta_base_version]) )
 			|| (($current_version = $GLOBALS['meta'][$nom_meta_base_version])!=$version_cible)){
 		include_spip('base/abstract_sql');
-		if (version_compare($current_version,"0.2.0"<=0.0)){
+		if (version_compare($current_version,"0.2.0",'<')){
 			include_spip('base/serial');
 			include_spip('base/auxiliaires');
 			include_spip('base/create');
