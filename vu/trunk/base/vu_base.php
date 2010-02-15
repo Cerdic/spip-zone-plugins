082687d1a388abe65a4b7a98f822c15d2c752408
jo.chibois@gmail.com <jo.chibois@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-02-15 12:59:28 +0000

    Proposer un flux rss pour suivre l'activité : premier jet + une broutille de mise en forme sur 'vu_base'
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@35205 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- vu/trunk/base/vu_base.php
+++ vu/trunk/base/vu_base.php
@@ -12,62 +12,6 @@
 
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
-// ---------- 3/3 - Declatation des interfaces, modalites d'utilisation des tables
-
-function vu_declarer_tables_interfaces($interface){
-
-	// Definir le nom des nouvelles boucles
-		// La table ['vu_*'] aura en fait pour nom 'vu_*'
-		$interface['table_des_tables']['vu_annonces']= 'vu_annonces';
-		$interface['table_des_tables']['vu_evenements'] = 'vu_evenements';
-		$interface['table_des_tables']['vu_publications'] = 'vu_publications';
-
-	// Indiquer les jointures possibles
-		// de tables vu_* vers mots_vu_* : sur le champ id_*
-		$interface['tables_jointures']['spip_vu_annonces']['id_annonce'] = 'mots_vu_annonces';
-        	$interface['tables_jointures']['spip_vu_evenements']['id_evenement'] = 'mots_vu_evenements';
-        	$interface['tables_jointures']['spip_vu_publications']['id_publication'] = 'mots_vu_publications';
-
-		// de tables mots_vu_* vers vu_* : sur le champ id_*
-	        $interface['tables_jointures']['spip_mots_vu_annonces']['id_annonce'] = 'vu_annonces';
-	        $interface['tables_jointures']['spip_mots_vu_evenements']['id_evenement'] = 'vu_evenements';
-	        $interface['tables_jointures']['spip_mots_vu_publications']['id_publication'] = 'vu_publications';
-
-		// de spip_mots vers mots_vu_* : sur le champ id_mot
-	        $interface['tables_jointures']['spip_mots']['id_mot'] = 'spip_mots_vu_annonces';
-	        $interface['tables_jointures']['spip_mots']['id_mot'] = 'spip_mots_vu_evenements';
-	        $interface['tables_jointures']['spip_mots']['id_mot'] = 'spip_mots_vu_publications';
-
-		// de vu_*_mots vers spip_mots : sur le champ id_mot
-		$interface['tables_jointures']['spip_mots_vu_annonces']['id_mot'] = 'spip_mots';
-		$interface['tables_jointures']['spip_mots_vu_evenements']['id_mot'] = 'spip_mots';
-		$interface['tables_jointures']['spip_mots_vu_publications']['id_mot'] = 'spip_mots';
-
-        // Titre pour url
-        $interface['table_titre']['spip_vu_annonces'] = "titre, '' AS lang";
-        $interface['table_titre']['spip_vu_evenements'] = "titre, '' AS lang";
-        $interface['table_titre']['spip_vu_publications'] = "titre, '' AS lang";
-
-	// On definit les traitements par défaut sur les champs qui le necessitent
-	//$interface['table_des_traitements']['MON_CHAMPS'][]= 'ma_fonction(%s)';
-	$interfaces['PEREMPTION'][]= 'normaliser_date(%s)';
-	$interfaces['DATE_EVENEMENT'][]= 'normaliser_date(%s)';
-	$interfaces['DATE_PUBLICATION'][]= 'normaliser_date(%s)';
-
-	// On indique les champs 'date' pour accelerer les requetes SQL
-	// Autorise l'utilisation des criteres 'age' et 'age_relatif' sur les nouveaux objets
-		$interface['table_date']['vu_annonces'] = 'peremption';
-		$interface['table_date']['vu_annonces'] = 'date';
-
-		$interface['table_date']['vu_evenements'] = 'date_evenement';
-		$interface['table_date']['vu_evenements'] = 'date';
-
-		$interface['table_date']['vu_publications'] = 'date_publication';
-		$interface['table_date']['vu_publications'] = 'date';
-
-       return $interface;
-
-
 // ---------- 1/3 - Declaration des tables principales
 function vu_declarer_tables_principales($tables_principales){
 	
@@ -212,6 +156,60 @@ function vu_declarer_tables_auxiliaires($tables_auxiliaires){
         return $tables_auxiliaires;
 }
 
+// ---------- 3/3 - Declatation des interfaces, modalites d'utilisation des tables
+
+function vu_declarer_tables_interfaces($interface){
+
+	// Definir le nom des nouvelles boucles
+		// La table ['vu_*'] aura en fait pour nom 'vu_*'
+		$interface['table_des_tables']['vu_annonces']= 'vu_annonces';
+		$interface['table_des_tables']['vu_evenements'] = 'vu_evenements';
+		$interface['table_des_tables']['vu_publications'] = 'vu_publications';
+
+	// Indiquer les jointures possibles
+		// de tables vu_* vers mots_vu_* : sur le champ id_*
+		$interface['tables_jointures']['spip_vu_annonces']['id_annonce'] = 'mots_vu_annonces';
+        	$interface['tables_jointures']['spip_vu_evenements']['id_evenement'] = 'mots_vu_evenements';
+        	$interface['tables_jointures']['spip_vu_publications']['id_publication'] = 'mots_vu_publications';
+
+		// de tables mots_vu_* vers vu_* : sur le champ id_*
+	        $interface['tables_jointures']['spip_mots_vu_annonces']['id_annonce'] = 'vu_annonces';
+	        $interface['tables_jointures']['spip_mots_vu_evenements']['id_evenement'] = 'vu_evenements';
+	        $interface['tables_jointures']['spip_mots_vu_publications']['id_publication'] = 'vu_publications';
+
+		// de spip_mots vers mots_vu_* : sur le champ id_mot
+	        $interface['tables_jointures']['spip_mots']['id_mot'] = 'spip_mots_vu_annonces';
+	        $interface['tables_jointures']['spip_mots']['id_mot'] = 'spip_mots_vu_evenements';
+	        $interface['tables_jointures']['spip_mots']['id_mot'] = 'spip_mots_vu_publications';
+
+		// de vu_*_mots vers spip_mots : sur le champ id_mot
+		$interface['tables_jointures']['spip_mots_vu_annonces']['id_mot'] = 'spip_mots';
+		$interface['tables_jointures']['spip_mots_vu_evenements']['id_mot'] = 'spip_mots';
+		$interface['tables_jointures']['spip_mots_vu_publications']['id_mot'] = 'spip_mots';
+
+        // Titre pour url
+        $interface['table_titre']['spip_vu_annonces'] = "titre, '' AS lang";
+        $interface['table_titre']['spip_vu_evenements'] = "titre, '' AS lang";
+        $interface['table_titre']['spip_vu_publications'] = "titre, '' AS lang";
+
+	// On definit les traitements par défaut sur les champs qui le necessitent
+	//$interface['table_des_traitements']['MON_CHAMPS'][]= 'ma_fonction(%s)';
+	$interfaces['PEREMPTION'][]= 'normaliser_date(%s)';
+	$interfaces['DATE_EVENEMENT'][]= 'normaliser_date(%s)';
+	$interfaces['DATE_PUBLICATION'][]= 'normaliser_date(%s)';
+
+	// On indique les champs 'date' pour accelerer les requetes SQL
+	// Autorise l'utilisation des criteres 'age' et 'age_relatif' sur les nouveaux objets
+		$interface['table_date']['vu_annonces'] = 'peremption';
+		$interface['table_date']['vu_annonces'] = 'date';
+
+		$interface['table_date']['vu_evenements'] = 'date_evenement';
+		$interface['table_date']['vu_evenements'] = 'date';
+
+		$interface['table_date']['vu_publications'] = 'date_publication';
+		$interface['table_date']['vu_publications'] = 'date';
+
+       return $interface;
 
 }
 
