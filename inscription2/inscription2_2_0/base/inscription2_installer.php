0f72923257da602d7b4ba67d9b320286d4d6acfc
pierre.fiches@free.fr <pierre.fiches@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-02-15 04:21:29 +0000

    Permettre la surcharge de la configuration initiale
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@35197 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- inscription2/inscription2_2_0/base/inscription2_installer.php
+++ inscription2/inscription2_2_0/base/inscription2_installer.php
@@ -43,33 +43,8 @@ function inscription2_upgrade($nom_meta_base_version,$version_cible){
 	if ($current_version==0.0){
 		spip_log('INSCRIPTION2 : installation neuve');
 		if(!$inscription2_meta){
-		ecrire_meta(
-			'inscription2',
-				serialize(array(
-					'nom' => 'on',
-					'nom_obligatoire' => 'on',
-					'nom_fiche_mod' => 'on',
-					'email' => 'on',
-					'email_obligatoire' => 'on',
-					'nom_famille' => 'on',
-					'nom_famille_table' => 'on',
-					'prenom' => 'on',
-					'prenom_table' => 'on',
-					'login' => 'on',
-					'login_fiche_mod' => 'on',
-					'adresse' => 'on',
-					'adresse_fiche_mod' => 'on',
-					'code_postal' => 'on',
-					'code_postal_fiche_mod' => 'on',
-					'ville' => 'on',
-					'ville_fiche_mod' => 'on',
-					'ville_table' => 'on',
-					'telephone' => 'on',
-					'telephone_fiche_mod' => 'on',
-					'statut_nouveau' => '6forum',
-					'statut_interne' => ''
-				))
-			);
+			$i2_configuration_initiale = charger_fonction('i2_configuration_initiale','inc');
+			$i2_configuration_initiale();
 		}
 		
 		// Creation de la table et des champs
