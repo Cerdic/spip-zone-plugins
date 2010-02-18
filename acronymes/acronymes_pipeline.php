d9d6247158f94203b1895b91a66f619045c2bad8
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-02-18 18:14:55 +0000

    Compatibilité avec le plugin Bandeau
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@35311 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- acronymes/acronymes_pipeline.php
+++ acronymes/acronymes_pipeline.php
@@ -12,7 +12,13 @@
 function acronymes_ajouter_boutons($boutons_admin) {
 	if (autoriser('administrer','form')) {
 	  // on voit le bouton dans la barre "naviguer"
-		$boutons_admin['naviguer']->sousmenu["sigles_tous"]= new Bouton(
+		$menu = "naviguer";
+		$icone = "img_pack/acronym-24.png";
+		if (isset($boutons_admin['bando_edition'])){
+			$menu = "bando_edition";
+			$icone = "img_pack/acronym-16.png";
+		}
+		$boutons_admin[$menu]->sousmenu["sigles_tous"]= new Bouton(
 		_DIR_PLUGIN_ACRONYMES."img_pack/acronym-24.png",  // icone
 		_L("Sigles &lt;acronym&gt; &lt;abbr&gt;") //titre
 		);
