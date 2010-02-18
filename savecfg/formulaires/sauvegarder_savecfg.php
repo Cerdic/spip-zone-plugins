ae92538fe85c9776a92fbab6f962ef03bc9d65bb
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-02-18 09:24:16 +0000

    Grosse amélioration : on peut désormais mettre à jour une sauvegarde (critère : porte le même nom).
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@35298 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- savecfg/formulaires/sauvegarder_savecfg.php
+++ savecfg/formulaires/sauvegarder_savecfg.php
@@ -19,8 +19,16 @@ function formulaires_sauvegarder_savecfg_traiter() {
 function sauvegarder_savecfg($fond, $titre) {
 	if (sql_countsel('spip_meta', 'nom='.sql_quote($fond)) == 1) {
 		$sfg = sql_getfetsel('valeur', 'spip_meta', 'nom='.sql_quote($fond));
-		sql_insertq('spip_savecfg', array('id_savecfg' => '', 'fond' => $fond, 'valeur' => $sfg, 'titre' => $titre, 'date' => date('Y-m-d H:m:s')));
+		// Insert ou Update ?
+		$id_savecfg = sql_getfetsel('id_savecfg', 'spip_savecfg', 'titre='.sql_quote($titre).' AND fond='.sql_quote($fond));
+		if ($id_savecfg > 0) { // Update
+			sql_updateq('spip_savecfg', array( 'valeur' => $sfg, 'date' => date('Y-m-d H:m:s')), 'id_savecfg='.$id_savecfg);
+			return _T('savecfg:miseajour_ok',array('titre'=>$titre));
+		} else { // Insert
+			sql_insertq('spip_savecfg', array('id_savecfg' => '', 'fond' => $fond, 'valeur' => $sfg, 'titre' => $titre, 'date' => date('Y-m-d H:m:s')));
+			return _T('savecfg:sauvegarde_ok',array('titre'=>$titre));
+		}
 	}
-	return _T('savecfg:sauvegarde_ok');
+	return _T('savecfg:sauvegarde_pas_ok');
 }
 ?>
