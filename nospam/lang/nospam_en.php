073233552816d1faa15bad24f1c22e0f4f8f67db
superyms@yahoo.fr <superyms@yahoo.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-02-19 07:09:43 +0000

    le plugin est plus multilangue, finition de langue anglaise, ajout des langues espagnole et arabe.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@35324 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- nospam/lang/nospam_en.php
+++ nospam/lang/nospam_en.php
@@ -5,6 +5,8 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	'erreur_jeton' => 'Unable to take account of your message. Thank you to resubmit!',
 	'erreur_spam' => 'Unable to take account of your message!',
+	'erreur_spam_doublon' => 'An identical message already exists!',
+	'erreur_spam_ip' => 'Too many comments detrimental to the quality!',
 
 );
 
