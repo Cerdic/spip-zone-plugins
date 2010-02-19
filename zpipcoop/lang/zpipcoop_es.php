de5dcbac17a4d8a74deeba7e75709ed846b43398
pierrekuhn82@gmail.com <pierrekuhn82@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-02-19 22:40:47 +0000

    Premier jet de zpipcoop, je suis parti de spipcoop pour arriver sur un squelettes Z.
    Plugins inutilisable et non sécurisé.
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@35344 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ zpipcoop/lang/zpipcoop_es.php
@@ -0,0 +1,67 @@
+<?php
+// Fichier de langue spécifique au squelette
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+//A
+'accueil_site' => 'Portada',
+'adresse_mail' => 'Email',
+'agenda' => 'Agenda',
+'ajouter_commentaire' => 'Comentar',
+'annuaire' => 'Participantes',
+'articles_auteur' => 'Mensajes de este autor o autora',
+'aucun_document' => 'No hay documentos',
+'aucun_commentaire' => 'No hay comentarios',
+
+//C
+'categories' => 'Categor&iacute;as',
+'conception_graphique_par' => 'Dise&ntilde;o gr&aacute;fico (c)',
+'commentaire' => 'comentario',
+'commentaires' => 'comentarios',
+'connecter_comme' => 'Conectada como',
+
+//D
+'dans' => 'en',
+'derniers_articles' => '&Uacute;ltimos mensajes',
+'derniers_visiteurs' => '&Uacute;ltimas visitas',
+'design' => 'Dise&ntilde;o de',
+'documents' => 'Documentos',
+
+//F
+'flux_forum_article' => 'Seguir por RSS estos comentarios',
+
+//G
+'groupes_travail' => 'Tus grupos',
+
+//H
+'haut_page' => 'Subir',
+
+//I
+'icons' => 'Im&aacute;genes de',
+'identifiant' => 'Identificante',
+'identifiant_compte' => 'Identificante de esta cuenta',
+
+//L
+'laissez_commentaire' => 'Comentar',
+'lire_la_suite' => 'Seguir leyendo',
+'lire_la_suite_de' => '',
+	
+//M
+'messages' => 'Mensajes',
+
+//N
+'nouveau_message' => 'Nuevo mensaje',
+'nouveau_site' => 'Nuevo sitio',
+
+//P
+'publier' => 'Publicado el',
+
+//S
+'sous_licence' => 'bajo Licencia',
+'surnom' => 'Nombre',
+
+);
+
+?>
---
+++ zpipcoop/lang/zpipcoop_fr.php
@@ -0,0 +1,73 @@
+<?php
+// Fichier de langue spécifique au squelette
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+//A
+'abonnement' => 'Abonnement',
+'accueil_site' => 'Accueil',
+'adresse_mail' => 'Adresse mail',
+'agenda' => 'Agenda',
+'ajouter_commentaire' => 'Ajouter un commentaire',
+'annuaire' => 'Annuaire',
+'antispam_champ_vide' => 'Antispam champ vide',
+'aucun_document' => 'Aucun document',
+
+//B
+'bouton_ajouter_document' => 'Ajouter un document',
+'bouton_radio_modere_priori' => 'Mod&ecute;r&ecute; &agrave; priori',
+'bouton_telecharger' => 'T&ecute;l&ecute;charger',
+'bouton_valider' => 'Valider',
+
+//C
+'categories' => 'Cat&ecute;gories',
+'commentaire' => 'commentaire',
+'commentaires' => 'commentaires',
+'connecter_comme' => 'Connect&ecute; comme',
+'conception_graphique_par' => 'Habillage visuel (c)',
+
+//D
+'dans' => 'dans',
+'derniers_visiteurs' => 'Derniers visiteurs',
+'design' => 'Charte graphique par',
+'documents' => 'Documents',
+
+//F
+'flux_forum_article' => 'Flux rss du forum de cet article',
+
+//G
+'groupes_travail' => 'Vos groupes de travail',
+
+//H
+'haut_page' => 'Haut de la page',
+
+//I
+'icons' => 'Images par',
+'identifiant' => 'Identifiant',
+'identifiant_compte' => 'Identifiant de ce compte',
+
+//L
+'laissez_commentaire' => 'Laissez un commentaire',
+'lire_la_suite' => 'Lire la suite',
+'lire_la_suite_de' => ' de ',
+	
+//M
+'messages' => 'Messages',
+
+//N
+'nouveau_message' => 'Nouveau message',
+'nouveau_site' => 'Nouveau site',
+
+//P
+'personaliser_nav' => 'Personnaliser ce menu',
+'publier' => 'Publi&eacute; le',
+
+//S
+'sous_licence' => 'sous Licence',
+'surnom' => 'Surnom',
+
+);
+
+?>
