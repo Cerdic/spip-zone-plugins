b05e4e842f24dcf174fcb4544b2cc97c57ac3a3b
superyms@yahoo.fr <superyms@yahoo.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-02-19 08:01:53 +0000

    ajout du multilinguisme + langues anglaise et arabe + petite correction.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@35325 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ forum/lang/forum_en.php
@@ -0,0 +1,46 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+'aucun_post_forum' => 'No post forum',
+'un_post_forum' => '1 post forum',
+'nombre_posts_forum' => '@nb@ forum posts',
+
+'icone_bruler_post' => "Report as Spam",
+'icone_valider_repondre_post' => 'Confirm & Reply to this post',
+
+'icone_valider_posts' => 'Confirm these posts',
+'icone_bruler_posts' => "Report as Spam",
+'icone_legitimer_post' => "Report as lawful",
+'icone_supprimer_posts' => 'Refuse these posts',
+
+'posts_meme_auteur' => 'All posts by this author',
+'posts_meme_email' => 'All posts in this email',
+'posts_meme_ip' => 'All posts of this IP',
+
+// statuts
+'posts_tous' => 'All',
+'posts_publie' => 'Published',
+'posts_prop' => 'Proposed',
+'posts_spam' => 'Spam',
+'posts_off' => 'Refused',
+
+'posts_prive' => 'Private',
+'posts_privoff' => 'Refused',
+'posts_privrac' => 'General',
+'posts_privadm' => 'Administrators',
+
+'posts_perso' => 'Personal',
+
+'statut_prop' => 'Proposed',
+'statut_publie' => 'Published',
+'statut_spam' => 'Spam',
+'statut_off' => 'Refused',
+
+'tout_voir' => 'See all posts',
+);
+
+
+?>
