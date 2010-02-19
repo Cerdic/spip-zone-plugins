aa163aba96be057ef1768a946e876eb13d157b34
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-02-19 14:08:25 +0000

    On passe les directives de configuration dans un squelette séparé et non en dur dans insert_head.
    Avantages :
    - Evite une erreur dans le compactage des js qui fait passer ces définitions après l'endroit où elles sont appelées
    - Permet de surcharger ces directives facilement
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@35331 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- lecteur_multimedia_v2/lm2_pipelines.php
+++ lecteur_multimedia_v2/lm2_pipelines.php
@@ -3,31 +3,28 @@ function lm2_insert_head($flux){
 	$flux .= '<script type="text/javascript" src="'.find_in_path(_DIR_LIB_SM.'script/soundmanager2.js').'"></script>'."\n";
 	$flux .= '<script type="text/javascript" src="'.find_in_path('javascript/lm2_playlist_jquery.js').'"></script>'."\n";
 	$flux .= '<link rel="stylesheet" href="'.generer_url_public('lm2_player.css').'" type="text/css" media="all" />'."\n";
-	$flux .= '<script type="text/javascript">'."\n".
-	'soundManager.debugMode = false;'."\n".
-	'var REPSWF ="' . _DIR_LIB_SM . 'swf/";'."\n".
-	'</script>'."\n" ;
+	$flux .= '<script type="text/javascript" src="'.generer_url_public('lm2_config.js').'"></script>'."\n";
 	$flux .= '<script type="text/javascript" src="'.find_in_path('javascript/lm2_inlineplayer.js').'"></script>'."\n";
 
 	return $flux;
 }
 
 /**
- * enclosures 
+ * enclosures
  * ajout d'un rel="enclosure" sur les liens mp3 absolus
- * permet de traiter les [mon son->http://monsite/mon_son.mp3] dans un texte. 
+ * permet de traiter les [mon son->http://monsite/mon_son.mp3] dans un texte.
  * Le filtre peut etre appele dans un squelette apres |liens_absolus
  */
- 
+
  function lm2_post_propre($texte) {
 
 	$reg_formats="mp3";
 
 	$texte = preg_replace(
 		",<a(\s[^>]*href=['\"]?(http:\/\/[a-zA-Z0-9\s()\/\:\._%\?+'=~-]*\.($reg_formats))['\"]?[^>]*)>(.*)</a>,Uims",
-		'<a$1 rel="enclosure">$4</a>', 
+		'<a$1 rel="enclosure">$4</a>',
 		$texte);
-	
+
 	return $texte;
 }
 
