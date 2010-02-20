d5ee78cb510c2441441e9834d651944a02d5e90e
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-02-20 22:35:06 +0000

    eviter de charger le fichier fonctions a chaque calcul si il ne sert que dans l'espace prive, sur la page themes
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@35385 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- zen-garden/zengarden_pip.php
+++ zen-garden/zengarden_pip.php
@@ -1,21 +1,26 @@
 <?php
+/**
+ * Plugin Zen-Garden pour Spip 2.0
+ * Licence GPL (c) 2006-2009 Cedric Morin
+ *
+ */
+
 function zengarden_affichage_final($texte){
-    global $html;
-    if ($html and lire_config('zengarden/switcher')){
-        include_spip('inc/utils');
-        $code = recuperer_fond('inc/switcher_zen');
-        // On rajoute le code du selecteur de squelettes avant la balise </body>
+	global $html;
+	if ($html and lire_config('zengarden/switcher')){
+		include_spip('prive/zengarden_theme_fonctions');
+		$code = recuperer_fond('inc/switcher_zen');
+		// On rajoute le code du selecteur de squelettes avant la balise </body>
 		$texte=str_replace("</body>",$code."</body>",$texte);
-    
-    }
-    return $texte;
+	}
+	return $texte;
 }
 
 function zengarden_insert_head($flux){
-    if(lire_config('zengarden/switcher')){
-        //$flux .= "<script src='".find_in_path('switcher_zen.js')."' type='text/javascript'></script>\n";
-        $flux .= "<link type='text/css' href='".find_in_path('switcher_zen.css')."' rel='stylesheet' />";
-    }
-    return $flux;
+	if(lire_config('zengarden/switcher')){
+		//$flux .= "<script src='".find_in_path('switcher_zen.js')."' type='text/javascript'></script>\n";
+		$flux .= "<link type='text/css' href='".find_in_path('switcher_zen.css')."' rel='stylesheet' />";
+	}
+	return $flux;
 }    
 ?>
