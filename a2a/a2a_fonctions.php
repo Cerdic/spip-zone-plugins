ca7078bb4634892b0578576d69991507fcf32277
brunobergot@gmail.com <brunobergot@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-02-21 12:07:36 +0000

    on affiche le statut des articles liés avec un filtre puce_statut (pas de _dist pour ne pas clasher avec le plugin afficher_objets)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@35400 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- a2a/a2a_fonctions.php
+++ a2a/a2a_fonctions.php
@@ -14,4 +14,9 @@ function balise_ARTICLES_LIES($p) {
 	return $p;
 }
 
+function filtre_puce_statut($id_objet, $statut, $id_rubrique, $type){
+	$puce_statut = charger_fonction('puce_statut', 'inc');
+	return $puce_statut($id_objet, $statut, $id_rubrique, $type);
+}
+
 ?>
