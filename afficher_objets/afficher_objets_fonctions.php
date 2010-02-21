cce90757e54005689a0379731e837603a465a510
marcimat@rezo.net <marcimat@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-02-21 12:08:31 +0000

    icone pour les rubriques, et pas la peine de réinventer une fonction qui existe déjà dans le core
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@35401 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- afficher_objets/afficher_objets_fonctions.php
+++ afficher_objets/afficher_objets_fonctions.php
@@ -1,10 +1,5 @@
 <?php
 
-function filtre_puce_statut_dist($id_objet, $statut, $id_rubrique, $type){
-	$puce_statut = charger_fonction('puce_statut', 'inc');
-	return $puce_statut($id_objet, $statut, $id_rubrique, $type);
-}
-
 function filtre_test_syndic_article_miroir($id){
 	global $my_sites;
 	if (isset($my_sites[$id]['miroir']) AND $my_sites[$id]['miroir'] == 'oui')
