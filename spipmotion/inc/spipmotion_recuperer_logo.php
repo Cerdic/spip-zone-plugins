64152570ed9e8e475515e61ea074dd62d85f6001
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-02-23 20:05:18 +0000

    une erreur grossière de caractères en trop
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@35507 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spipmotion/inc/spipmotion_recuperer_logo.php
+++ spipmotion/inc/spipmotion_recuperer_logo.php
@@ -5,11 +5,11 @@
 		}
 		include_spip('inc/documents');
 		$mode= 'vignette';
-		
+
 		$document = sql_fetsel("docs.id_document,docs.fichier", "spip_documents AS docs INNER JOIN spip_documents_liens AS L ON L.id_document=docs.id_document","L.id_document=".sql_quote($id_document));
 		$chemin_court = $document['fichier'];
-		$chemin = get_spip_doc($chemin_court););
-		
+		$chemin = get_spip_doc($chemin_court);
+
 		$movie = new ffmpeg_movie($chemin,0);
 		if($movie->hasVideo()){
 			$frame1 = $movie->getFrame(100);
@@ -19,20 +19,20 @@
 				$dossier_temp = _DIR_VAR;
 				$fichier_temp = "$dossier_temp$query.jpg";
 				spip_log("fichier temporaire = $fichier_temp","spipmotion");
-				
+
 				spip_log('frame1 existe',"spipmotion");
 				$img_temp = $frame1->toGDImage();
 				imagejpeg($img_temp, $fichier_temp);
 				$img_finale = $fichier_temp;
 				$mode = 'vignette';
-				
+
 				$ajouter_documents = charger_fonction('ajouter_documents', 'inc');
-				spip_log("on ajoute $img","spipmotion");	
+				spip_log("on ajoute $img","spipmotion");
 				// verifier l'extension du fichier en fonction de son type mime
 				list($extension,$arg) = fixer_extension_document($arg);
-				$x = $ajouter_documents($img_finale, $img_finale, 
+				$x = $ajouter_documents($img_finale, $img_finale,
 						    $type, $id, $mode, $id_document, $actifs);
-				
+
 				imagedestroy($img_temp);
 				unlink($img_finale);
 			}
