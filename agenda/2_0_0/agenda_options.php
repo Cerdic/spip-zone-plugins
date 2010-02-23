9d9b8c4b91e1d6c19c073ef30d140a7e27cf4e0a
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-02-23 13:52:23 +0000

    Agenda ne teste plus que Bonux n'a pas été modifié.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@35491 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- agenda/2_0_0/agenda_options.php
+++ agenda/2_0_0/agenda_options.php
@@ -18,15 +18,4 @@ function exec_calendrier(){
 		$var_f();
 	}
 }
-if (test_espace_prive()
-  AND _request('exec')=='admin_plugin'){
-  include_spip('spip_bonux_fonctions');
-  // verifier qu'on a bien le bon bonux, avec la css qui marche
-  if (!file_exists($f=(_DIR_PLUGIN_SPIP_BONUX.'style_prive_formulaires.html'))){
-  	include_spip('inc/plugin');
-  	ecrire_plugin_actifs(array('AGENDA'=>substr(rtrim(_DIR_PLUGIN_AGENDA,'/'),strlen(_DIR_PLUGINS))),false,'enleve');
-  	ecrire_meta('plugin_erreur_activation','Agenda necessite le vrai SPIP-Bonux');
-  	//die('echec:'.md5($contenu).":"._BONUX_CSS_MD5_FORMULAIRES);	
-  }
-}
 ?>
