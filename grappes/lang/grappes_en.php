05cc7951108be4086719ca7d46cf27f3caf9be63
da@weeno.net <da@weeno.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-02-23 21:19:41 +0000

    Traduction de plugin.xml en anglais, et correction du fichier de langue: en anglais, grappe ne se traduit pas par "grape" qui signifie "raisin", mais par "bunch of grapes" ou encore "bunch".
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@35510 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- grappes/lang/grappes_en.php
+++ grappes/lang/grappes_en.php
@@ -17,33 +17,33 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 'delier' => 'Unlink',
 
 // I
-'icone_creation_grappe' => 'Create a new grape',
-'icone_modif_grappe' => 'Modify this grape',
-'icone_supprimer_grappe' => 'Delete this grape',
-'icone_voir_toutes_grappes' => 'View all grapes',
+'icone_creation_grappe' => 'Create a new bunch',
+'icone_modif_grappe' => 'Modify this bunch',
+'icone_supprimer_grappe' => 'Delete this bunch',
+'icone_voir_toutes_grappes' => 'View all bunches',
 'info_acces_0minirezo' => 'Administrators',
 'info_acces_1comite' => 'Editor',
-'info_changer_nom_grappe' => 'Change the name of the grape',
-'info_creation_grappes' => 'Create or modify elements grapes',
-'info_grappes_association' => 'What kind of objects can be linked to the grape ?',
+'info_changer_nom_grappe' => 'Change the name of the bunch',
+'info_creation_grappes' => 'Create or modify elements bunches',
+'info_grappes_association' => 'What kind of objects can be linked to the bunch?',
 'info_lier_auteurs' => 'Authors',
 'info_lier_articles' => 'Articles',
 'info_lier_breves' => 'News items',
 'info_lier_documents' => 'Documents',
 'info_lier_mots' => 'Keywords',
-'info_lier_grappes' => 'Grapes',
+'info_lier_grappes' => 'Bunches',
 'info_lier_rubriques' => 'Sections',
 'info_lier_sites' => 'Websites',
 'info_lier_syndic' => 'Websites',
-'info_nom_grappe' => 'Name of the grape',
-'info_qui_peut_lier' => 'Who can link objects to the grape ?',
-'info_rechercher' => 'Search', // chercher de nouveaux objets a lier
+'info_nom_grappe' => 'Name of the bunch',
+'info_qui_peut_lier' => 'Who can link objects to the bunch?',
+'info_rechercher' => 'Search', // search new objects to link
 
 'item_groupes_association_articles' => 'The articles',
 'item_groupes_association_auteurs' => 'The authors',
 'item_groupes_association_breves' => 'The news items',
 'item_groupes_association_documents' => 'The documents',
-'item_groupes_association_grappes' => 'The grapes',
+'item_groupes_association_grappes' => 'The bunches',
 'item_groupes_association_groupes_mots' => 'The keyword groups',
 'item_groupes_association_mots' => 'The keywords',
 'item_groupes_association_rubriques' => 'The sections',
@@ -58,9 +58,9 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 // T
 'texte_descriptif' => 'Description',
 
-'titre_grappe' => 'Grape',
-'titre_nouvelle_grappe' => 'New cluster',
-'titre_page_grappes' => 'Grapes',
+'titre_grappe' => 'Bunch',
+'titre_nouvelle_grappe' => 'New bunch',
+'titre_page_grappes' => 'Bunches',
 
 // V
 'voir' => 'View',
