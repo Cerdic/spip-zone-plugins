3e175bad3c3053089acc2a50c03688625844a7d4
pierre.fiches@free.fr <pierre.fiches@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-02-25 13:39:42 +0000

    utiliser une fonction spip
    empêcher les angles supérieurs de titrem de couvrir les arrondis de son  contenant
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@35568 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- themes_interface_privee/themes_interface_privee_pipelines.php
+++ themes_interface_privee/themes_interface_privee_pipelines.php
@@ -27,11 +27,11 @@ function themes_interface_privee_header_prive($flux) {
 	$bouton["$interface"] = "<img src='".chemin("images/puce-verte-breve.gif")."' alt='selectionnee' />";
 	
 	$entete = "<div class='titrem' style='text-align: center;'>Choix de l’interface<\/div>";
-	$liens = "<tr class='tr_liste'><td>".$bouton["standard"]."<\/td><td class='arial2'><a href='?action=preferer_interface&amp;arg=standard&amp;redirect=".urlencode($url)."' class='lien_sous'>Standard<\/a><\/td><\/tr>";
-	$liens .= "<tr class='tr_liste'><td>".$bouton["degrades"]."<\/td><td class='arial2'><a href='?action=preferer_interface&amp;arg=degrades&amp;redirect=".urlencode($url)."' class='lien_sous'>Standard avec effets<\/a><\/td><\/tr>";
-	$liens .= "<tr class='tr_liste'><td>".$bouton["bonux"]."<\/td><td class='arial2'><a href='?action=preferer_interface&amp;arg=bonux&amp;redirect=".urlencode($url)."' class='lien_sous'>Contrast&eacute;e<\/a><\/td><\/tr>";
-	$liens .= "<tr class='tr_liste'><td>".$bouton["wpip"]."<\/td><td class='arial2'><a href='?action=preferer_interface&amp;arg=wpip&amp;redirect=".urlencode($url)."' class='lien_sous'>&Agrave; la fa&ccedil;on de Wordpress<\/a><\/td><\/tr>";
-	$liens .= "<tr class='tr_liste'><td>".$bouton["blanche"]."<\/td><td class='arial2'><a href='?action=preferer_interface&amp;arg=blanche&amp;redirect=".urlencode($url)."' class='lien_sous'>Blanche<\/a><\/td><\/tr>";
+	$liens = "<tr class='tr_liste'><td>".$bouton["standard"]."<\/td><td class='arial2'><a href='".generer_url_action('preferer_interface',"arg=standard&redirect=".urlencode($url))."' class='lien_sous'>Standard<\/a><\/td><\/tr>";
+	$liens .= "<tr class='tr_liste'><td>".$bouton["degrades"]."<\/td><td class='arial2'><a href='".generer_url_action('preferer_interface',"arg=degrades&redirect=".urlencode($url))."' class='lien_sous'>Standard avec effets<\/a><\/td><\/tr>";
+	$liens .= "<tr class='tr_liste'><td>".$bouton["bonux"]."<\/td><td class='arial2'><a href='".generer_url_action('preferer_interface',"arg=bonux&redirect=".urlencode($url))."' class='lien_sous'>Contrast&eacute;e<\/a><\/td><\/tr>";
+	$liens .= "<tr class='tr_liste'><td>".$bouton["wpip"]."<\/td><td class='arial2'><a href='".generer_url_action('preferer_interface',"arg=wpip&redirect=".rawurlencode($url))."' class='lien_sous'>&Agrave; la fa&ccedil;on de Wordpress<\/a><\/td><\/tr>";
+	$liens .= "<tr class='tr_liste'><td>".$bouton["blanche"]."<\/td><td class='arial2'><a href='".generer_url_action('preferer_interface',"arg=blanche&redirect=".urlencode($url))."' class='lien_sous'>Blanche<\/a><\/td><\/tr>";
 	
 	$flux .= "
 		<script type='text/javascript'><!--
