b5f98e228ad0f356edab0cba2f782d9e3ad83783
pierre.fiches@free.fr <pierre.fiches@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-02-25 10:31:51 +0000

    Validité et utilisation de fonctions spip
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@35557 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- themes_interface_privee/themes_interface_privee_pipelines.php
+++ themes_interface_privee/themes_interface_privee_pipelines.php
@@ -1,9 +1,5 @@
 <?php
 
-$p=explode(basename(_DIR_PLUGINS)."/",str_replace('\\','/',realpath(dirname(__FILE__))));
-define('_DIR_PLUGIN_THEMES_INTERFACE_PRIVEE',(_DIR_PLUGINS.end($p)));
-
-
 function themes_interface_privee_header_prive($flux) {
 	global $visiteur_session, $REQUEST_URI;
 	$c = (is_array($visiteur_session)
@@ -16,12 +12,12 @@ function themes_interface_privee_header_prive($flux) {
 	$interface = $GLOBALS['visiteur_session']['prefs']['interface_privee'];
 	if (strlen($interface) == 0) $interface = "standard";
 	
-	
-	if ($interface == "blanche") $flux .= '<link rel="stylesheet" type="text/css" href="../spip.php?page=style_prive_blanche&amp;ltr=' . $GLOBALS['spip_lang_left'] . '&'. $couleurs($c).'" id="css_blanche" />';
-	else if ($interface == "wpip") $flux .= '<link rel="stylesheet" type="text/css" href="../spip.php?page=style_prive_wpip&amp;ltr=' . $GLOBALS['spip_lang_left'] . '&'. $couleurs($c).'" id="css_wpip" />';
-	else if ($interface == "degrades") $flux .= '<link rel="stylesheet" type="text/css" href="../spip.php?page=style_prive_degrades&amp;ltr=' . $GLOBALS['spip_lang_left'] . '&'. $couleurs($c).'" id="css_degrades" />';
+	//generer_url_public('style_prive', parametres_css_prive())
+	if ($interface == "blanche") $flux .= '<link rel="stylesheet" type="text/css" href="'.generer_url_public('style_prive_blanche', parametres_css_prive()).'" id="css_blanche" />';
+	else if ($interface == "wpip") $flux .= '<link rel="stylesheet" type="text/css" href="'.generer_url_public('style_prive_wpip', parametres_css_prive()).'" id="css_wpip" />';
+	else if ($interface == "degrades") $flux .= '<link rel="stylesheet" type="text/css" href="'.generer_url_public('style_prive_degrades', parametres_css_prive()).'" id="css_degrades" />';
 	else if ($interface == "bonux") {
-		$flux .= '<link rel="stylesheet" type="text/css" href="../spip.php?page=style_prive_bonux&amp;ltr=' . $GLOBALS['spip_lang_left'] . '&'. $couleurs($c).'" id="css_degrades" />';
+		$flux .= '<link rel="stylesheet" type="text/css" href="'.generer_url_public('style_prive_bonux', parametres_css_prive()).'" id="css_degrades" />';
 	}
 
 	
@@ -30,17 +26,17 @@ function themes_interface_privee_header_prive($flux) {
 	// Ajouter les liens pour changer d'interface
 	$bouton["$interface"] = "<img src='".chemin("images/puce-verte-breve.gif")."' alt='selectionnee' />";
 	
-	$liens = "<div class='titrem' style='text-align: center;'>Choix de l’interface</div>";
-	$liens .= "<tr class='tr_liste'><td>".$bouton["standard"]."</td><td class='arial2'><a href='?action=preferer_interface&amp;arg=standard&amp;redirect=".urlencode($url)."' class='lien_sous'>Standard</a></td></tr>";
-	$liens .= "<tr class='tr_liste'><td>".$bouton["degrades"]."</td><td class='arial2'><a href='?action=preferer_interface&amp;arg=degrades&amp;redirect=".urlencode($url)."' class='lien_sous'>Standard avec effets</a></td></tr>";
-	$liens .= "<tr class='tr_liste'><td>".$bouton["bonux"]."</td><td class='arial2'><a href='?action=preferer_interface&amp;arg=bonux&amp;redirect=".urlencode($url)."' class='lien_sous'>Contrast&eacute;e</a></td></tr>";
-	$liens .= "<tr class='tr_liste'><td>".$bouton["wpip"]."</td><td class='arial2'><a href='?action=preferer_interface&amp;arg=wpip&amp;redirect=".urlencode($url)."' class='lien_sous'>&Agrave; la fa&ccedil;on de Wordpress</a></td></tr>";
-	$liens .= "<tr class='tr_liste'><td>".$bouton["blanche"]."</td><td class='arial2'><a href='?action=preferer_interface&amp;arg=blanche&amp;redirect=".urlencode($url)."' class='lien_sous'>Blanche</a></td></tr>";
+	$liens = "<div class='titrem' style='text-align: center;'>Choix de l’interface<\/div>";
+	$liens .= "<tr class='tr_liste'><td>".$bouton["standard"]."<\/td><td class='arial2'><a href='?action=preferer_interface&amp;arg=standard&amp;redirect=".urlencode($url)."' class='lien_sous'>Standard<\/a><\/td><\/tr>";
+	$liens .= "<tr class='tr_liste'><td>".$bouton["degrades"]."<\/td><td class='arial2'><a href='?action=preferer_interface&amp;arg=degrades&amp;redirect=".urlencode($url)."' class='lien_sous'>Standard avec effets<\/a><\/td><\/tr>";
+	$liens .= "<tr class='tr_liste'><td>".$bouton["bonux"]."<\/td><td class='arial2'><a href='?action=preferer_interface&amp;arg=bonux&amp;redirect=".urlencode($url)."' class='lien_sous'>Contrast&eacute;e<\/a><\/td><\/tr>";
+	$liens .= "<tr class='tr_liste'><td>".$bouton["wpip"]."<\/td><td class='arial2'><a href='?action=preferer_interface&amp;arg=wpip&amp;redirect=".urlencode($url)."' class='lien_sous'>&Agrave; la fa&ccedil;on de Wordpress<\/a><\/td><\/tr>";
+	$liens .= "<tr class='tr_liste'><td>".$bouton["blanche"]."<\/td><td class='arial2'><a href='?action=preferer_interface&amp;arg=blanche&amp;redirect=".urlencode($url)."' class='lien_sous'>Blanche<\/a><\/td><\/tr>";
 	
 	$flux .= "
 		<script type='text/javascript'><!--
 			$(document).ready(function() {
-				$('#bandeauinterface').append(\"<div id='bandeauprefinterface' class='cadre cadre-liste'><table width='100%' cellpadding='2' cellspacing='0' border='0'>$liens</table></div>\");
+				$('#bandeauinterface').append(\"<div id='bandeauprefinterface' class='cadre cadre-liste'><table width='100%' cellpadding='2' cellspacing='0' border='0'>$liens<\/table><\/div>\");
 			});
 		--></script>
 	";
