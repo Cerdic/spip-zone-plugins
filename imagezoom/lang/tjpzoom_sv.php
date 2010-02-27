2108cc7ec15522ceff0b7591fb2da78c150a58ba
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-02-27 18:54:17 +0000

    langues (tjpzoom)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@35663 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- imagezoom/lang/tjpzoom_sv.php
+++ imagezoom/lang/tjpzoom_sv.php
@@ -7,8 +7,24 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
+	// D
+	'descriptif' => 'Cette page permet de configurer le style de la loupe. Un style personalis&eacute; peut &ecirc;tre d&eacute;clar&eacute;; voir <a href="http://plugins.spip.net/?page=article&amp;id_article=48">la documentation en ligne</a>.', # NEW
+
+	// E
+	'example' => 'R&eacute;sultat graphique', # NEW
+
+	// N
+	'nostyle' => 'Sans Style', # NEW
+
 	// O
-	'other' => 'Annan'
+	'other' => 'Annan',
+
+	// S
+	'size_vignette' => 'Largeur Maximale de la Vignette', # NEW
+	'style' => 'Style de la Loupe', # NEW
+
+	// T
+	'titre' => 'Configuration de la Loupe', # NE
 );
 
 ?>
