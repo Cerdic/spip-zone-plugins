dd3de7fff78fc4d65eaef5d5292c2693a65bc597
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-02-27 18:54:25 +0000

    langues (csvimport)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@35667 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- csv_import/lang/csvimport_km.php
+++ csv_import/lang/csvimport_km.php
@@ -12,29 +12,29 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'ajouter_donnees' => '&#6036;&#6035;&#6098;&#6032;&#6082;&#6040;&#6033;&#6071;&#6035;&#6098;&#6035;&#6035;&#6096;&#6041;',
 	'ajouter_table' => '&#6036;&#6035;&#6098;&#6032;&#6082;&#6040; &#6033;&#6085;&#6031;&#6070;&#6042;&#6070;&#6020;',
 	'aucune_donnee' => '&#6018;&#6098;&#6040;&#6070;&#6035;&#6033;&#6071;&#6035;&#6098;&#6035;&#6035;&#6096;&#6041; &#6016;&#6098;&#6035;&#6075;&#6020;&#6063;&#6016;&#6047;&#6070;&#6042;&#6100;',
-	'aucune_table_declaree' => '<NEW>Aucune table n\'est d&eacute;clar&eacute;e pour l\'import CSV',
-	'avertissement_ajout' => '<NEW>Les donn&eacute;es du fichier CSV vont &ecirc;tre ajout&eacute;es &agrave; la table "@table@" comme illustr&eacute; ci-dessous.',
-	'avertissement_remplacement' => '<NEW>Cette op&eacute;ration va entra&icirc;ner la suppression de toutes les donn&eacute;es pr&eacute;sentes dans la table. Les donn&eacute;es du fichier seront ins&eacute;r&eacute;es comme ci-dessous :',
+	'aucune_table_declaree' => 'Aucune table n\'est d&eacute;clar&eacute;e pour l\'import CSV', # NEW
+	'avertissement_ajout' => 'Les donn&eacute;es du fichier CSV vont &ecirc;tre ajout&eacute;es &agrave; la table "@table@" comme illustr&eacute; ci-dessous.', # NEW
+	'avertissement_remplacement' => 'Cette op&eacute;ration va entra&icirc;ner la suppression de toutes les donn&eacute;es pr&eacute;sentes dans la table. Les donn&eacute;es du fichier seront ins&eacute;r&eacute;es comme ci-dessous :', # NEW
 
 	// C
-	'caracteres_separation' => '<NEW>Caract&egrave;re de s&eacute;paration',
-	'champs_csv' => '<NEW>Champs du fichier CSV',
+	'caracteres_separation' => 'Caract&egrave;re de s&eacute;paration', # NEW
+	'champs_csv' => 'Champs du fichier CSV', # NEW
 	'champs_table' => '&#6044;&#6070;&#6043; &#6035;&#6083;&#6031;&#6070;&#6042;&#6070;&#6020;',
-	'correspondance_incomplete' => '<NEW>Correspondances CSV-Table incompl&egrave;tes',
-	'correspondance_indefinie' => '<NEW>Correspondances CSV-Table non d&eacute;finies',
+	'correspondance_incomplete' => 'Correspondances CSV-Table incompl&egrave;tes', # NEW
+	'correspondance_indefinie' => 'Correspondances CSV-Table non d&eacute;finies', # NEW
 	'csvimport' => '&#6035;&#6070;&#6086;&#6021;&#6076;&#6043; CSV',
 
 	// D
-	'delimiteur_indefini' => '<NEW>D&eacute;limiteur non d&eacute;fini',
-	'description_table_introuvable' => '<NEW>Description de la table introuvable',
+	'delimiteur_indefini' => 'D&eacute;limiteur non d&eacute;fini', # NEW
+	'description_table_introuvable' => 'Description de la table introuvable', # NEW
 
 	// E
-	'erreurs_ajout_base' => '<NEW>Il y a eu @nb@ erreurs lors de l\'ajout dans la base.',
-	'export_classique' => '<NEW>CSV classique (,)',
-	'export_excel' => '<NEW>CSV pour Excel (;)',
+	'erreurs_ajout_base' => 'Il y a eu @nb@ erreurs lors de l\'ajout dans la base.', # NEW
+	'export_classique' => 'CSV classique (,)', # NEW
+	'export_excel' => 'CSV pour Excel (;)', # NEW
 	'export_format' => '&#6033;&#6098;&#6042;&#6020;&#6091;&#6033;&#6098;&#6042;&#6070;&#6041; &#6035;&#6083;&#6063;&#6016;&#6047;&#6070;&#6042;&nbsp;&#6102;',
 	'export_table' => '&#6016;&#6070;&#6042;&#6035;&#6070;&#6086;&#6021;&#6081;&#6025; &#6035;&#6083;&#6031;&#6070;&#6042;&#6070;&#6020;&#6102; @table@',
-	'export_tabulation' => '<NEW>CSV avec tabulations',
+	'export_tabulation' => 'CSV avec tabulations', # NEW
 	'exporter' => '&#6035;&#6070;&#6086;&#6021;&#6081;&#6025;',
 
 	// F
@@ -47,28 +47,28 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'import_export_tables' => '&#6035;&#6070;&#6086;&#6021;&#6076;&#6043; / &#6035;&#6070;&#6086;&#6021;&#6081;&#6025; &#6016;&#6098;&#6035;&#6075;&#6020;&#6031;&#6070;&#6042;&#6070;&#6020;',
 
 	// L
-	'ligne_entete' => '<NEW>1<sup>&egrave;re</sup> ligne d\'en-t&ecirc;te',
-	'lignes_table' => '<NEW>Il y a @nb_resultats@ lignes dans la table "@table@".',
+	'ligne_entete' => '1<sup>&egrave;re</sup> ligne d\'en-t&ecirc;te', # NEW
+	'lignes_table' => 'Il y a @nb_resultats@ lignes dans la table "@table@".', # NEW
 	'lignes_totales' => '&#6047;&#6042;&#6075;&#6036; @nb@ &#6036;&#6035;&#6098;&#6033;&#6070;&#6031;&#6091;&#6100;',
 	'lignes_totales_csv' => '&#6047;&#6042;&#6075;&#6036; @nb@ &#6036;&#6035;&#6098;&#6033;&#6070;&#6031;&#6091; &#6016;&#6098;&#6035;&#6075;&#6020;&#6063;&#6016;&#6047;&#6070;&#6042; CSV&#6100;',
 
 	// N
-	'nb_enregistrements' => '<NEW>@nb@ enregistrements',
+	'nb_enregistrements' => '@nb@ enregistrements', # NEW
 
 	// P
 	'pas_importer' => '&#6018;&#6098;&#6040;&#6070;&#6035;&#6031;&#6098;&#6042;&#6076;&#6044;&#6035;&#6070;&#6086;&#6021;&#6076;&#6043;',
 	'premieres_lignes' => '@nb@ &#6036;&#6035;&#6098;&#6033;&#6070;&#6031;&#6091;&#6026;&#6086;&#6036;&#6076;&#6020; &#6035;&#6083;&#6063;&#6016;&#6047;&#6070;&#6042;&#6100;',
 	'probleme_chargement_fichier' => '&#6036;&#6025;&#6098;&#6048;&#6070; &#6038;&#6081;&#6043;&#6033;&#6070;&#6025;&#6041;&#6016;&#6063;&#6016;&#6047;&#6070;&#6042;',
 	'probleme_chargement_fichier_erreur' => '&#6036;&#6025;&#6098;&#6048;&#6070; &#6038;&#6081;&#6043;&#6033;&#6070;&#6025;&#6041;&#6016;&#6063;&#6016;&#6047;&#6070;&#6042; (&#6016;&#6086;&#6048;&#6075;&#6047; @erreur@)&#6100;',
-	'probleme_inextricable' => '<NEW>Probl&egrave;me inextricable...',
+	'probleme_inextricable' => 'Probl&egrave;me inextricable...', # NEW
 
 	// R
-	'remplacer_toute_table' => '<NEW>Remplacer toute la table',
+	'remplacer_toute_table' => 'Remplacer toute la table', # NEW
 
 	// T
 	'table_vide' => '&#6031;&#6070;&#6042;&#6070;&#6020; "@table@" &#6033;&#6033;&#6081;&#6100;',
-	'tables_declarees' => '<NEW>Tables d&eacute;clar&eacute;es',
-	'tables_presentes' => '<NEW>Tables pr&eacute;sentes dans la base',
+	'tables_declarees' => 'Tables d&eacute;clar&eacute;es', # NEW
+	'tables_presentes' => 'Tables pr&eacute;sentes dans la base', # NEW
 	'tout_remplacer' => '&#6023;&#6086;&#6035;&#6077;&#6047;&#6033;&#6070;&#6086;&#6020;&#6050;&#6047;&#6091;',
 
 	// Z
