3c2967eb18aaec15fc0591bbcaaa59b71a8926ce
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-02-27 18:54:07 +0000

    langues (forms)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@35658 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- forms/forms_et_tables_1_9_1/lang/forms_km.php
+++ forms/forms_et_tables_1_9_1/lang/forms_km.php
@@ -8,45 +8,185 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
+	'activer_barre_typo' => 'Activer la barre typographique', # NEW
 	'adresse_invalide' => '&#6050;&#6070;&#6047;&#6041;&#6026;&#6098;&#6027;&#6070;&#6035;&#6035;&#6081;&#6087; &#6018;&#6098;&#6040;&#6070;&#6035;&#6047;&#6075;&#6038;&#6043;&#6039;&#6070;&#6038;&#6100;',
 	'afficher' => '&#6036;&#6020;&#6098;&#6048;&#6070;&#6025;',
+	'aide_contextuelle' => 'Aide contextuelle', # NEW
 	'ajouter_champ' => '&#6036;&#6035;&#6098;&#6032;&#6082;&#6040; &#6040;&#6077;&#6041;&#6044;&#6070;&#6043;',
 	'ajouter_champ_type' => '&#6036;&#6020;&#6098;&#6016;&#6078;&#6031; &#6040;&#6077;&#6041;&#6044;&#6070;&#6043; &#6040;&#6070;&#6035;&#6036;&#6098;&#6042;&#6039;&#6081;&#6033;&#6102;',
 	'ajouter_choix' => '&#6036;&#6035;&#6098;&#6032;&#6082;&#6040; &#6040;&#6077;&#6041;&#6023;&#6086;&#6042;&#6078;&#6047; ',
+	'apparence_formulaire' => 'Apparence du formulaire de saisie', # NEW
 	'article_inserer_un_formulaire' => '&#6036;&#6025;&#6098;&#6021;&#6076;&#6043; &#6040;&#6077;&#6041;&#6036;&#6082;&#6036;&#6036;&#6033;',
+	'article_inserer_un_formulaire_detail' => 'Vous pouvez ins&eacute;rer des formulaires dans vos articles afin de permettre aux visiteurs d\'entrer des informations. Choisissez un formulaire dans la liste ci-dessous et recopiez le raccourci dans le texte de l\'article.', # NEW
+	'article_recopier_raccourci' => 'Recopiez ce raccourci dans le texte de l\'article pour ins&eacute;rer ce formulaire', # NEW
+	'articles_utilisant' => 'Articles utilisant ce formulaire', # NEW
 	'attention' => '&#6036;&#6098;&#6042;&#6041;&#6031;&#6098;&#6035;&#6102;',
 	'aucune_reponse' => '&#6018;&#6098;&#6040;&#6070;&#6035;&#6033;&#6071;&#6035;&#6098;&#6035;&#6035;&#6096;&#6041;',
+	'avis_message_confirmation' => 'Un message de confirmation est envoy&eacute; &agrave; @mail@', # NEW
+
+	// B
+	'boite_info' => 'Cliquez sur les icones pour voir les donn&eacute;es associ&eacute;es a un formulaire, l\'editer, le dupliquer, l\'exporter, vider les donn&eacute;es ou le supprimer.', # NEW
 
 	// C
 	'cfg_activer' => '&#6036;&#6070;&#6033;/&#6021;&#6070;&#6047;',
+	'cfg_associer_donnees' => 'Associer les donn&eacute;es des tables', # NEW
+	'cfg_associer_donnees_articles' => 'Permettre d\'associer les donnees aux articles :', # NEW
+	'cfg_associer_donnees_auteurs' => 'Permettre d\'associer les donnees aux auteurs :', # NEW
+	'cfg_associer_donnees_rubriques' => 'Permettre d\'associer les donnees aux rubriques :', # NEW
 	'cfg_bouton_type_image' => '&#6036;&#6098;&#6042;&#6050;&#6036;&#6091; &#6023;&#6070;&#6036;&#6098;&#6042;&#6039;&#6081;&#6033;&#6042;&#6076;&#6036;&#6039;&#6070;&#6038;',
 	'cfg_bouton_type_texte' => '&#6036;&#6098;&#6042;&#6050;&#6036;&#6091; &#6023;&#6070;&#6036;&#6098;&#6042;&#6039;&#6081;&#6033;&#6050;&#6031;&#6098;&#6032;&#6036;&#6033; (&#6026;&#6070;&#6016;&#6091;&#6047;&#6098;&#6035;&#6078;)',
+	'cfg_bouton_valider' => 'Type de bouton valider', # NEW
+	'cfg_bouton_valider_texte' => 'Vous pouvez pr&eacute;ciser quel type de bouton valider utiliser dans les formulaires&nbsp;:', # NEW
 	'cfg_desactiver' => '&#6033;&#6081;',
+	'cfg_inserer_head' => 'Insertion dans le &lt;head&gt;', # NEW
+	'cfg_inserer_head_texte' => 'Inserer automatiquement les appels aux fichiers suivants dans la section &lt;head&gt; des pages du site (recommand&eacute;) :', # NEW
 	'champ_descendre' => '&#6021;&#6075;&#6087;',
 	'champ_email_details' => '&#6047;&#6076;&#6040;&#6036;&#6025;&#6098;&#6021;&#6076;&#6043; &#6040;&#6077;&#6041;&#6050;&#6070;&#6047;&#6041;&#6026;&#6098;&#6027;&#6070;&#6035;&#6050;&#6090;&#6072;&#6040;&#6082;&#6044;&#6043;&#6093; &#6047;&#6075;&#6038;&#6043;&#6039;&#6070;&#6038; (&#6023;&#6070;&#6042;&#6070;&#6020; &#6024;&#6098;&#6040;&#6084;&#6087;&#6050;&#6098;&#6035;&#6016;@&#6016;&#6098;&#6042;&#6075;&#6040;&#6048;&#6090;&#6075;&#6035;&#6037;&#6098;&#6031;&#6043;&#6091;&#6047;&#6081;&#6044;&#6070;.com )',
+	'champ_listable_admin' => 'Afficher ce champ dans les listes de l\'espace priv&eacute;', # NEW
 	'champ_listable_publique' => '&#6036;&#6020;&#6098;&#6048;&#6070;&#6025; &#6044;&#6070;&#6043;&#6035;&#6081;&#6087; &#6016;&#6098;&#6035;&#6075;&#6020;&#6036;&#6025;&#6098;&#6023;&#6072;&#6047;&#6070;&#6034;&#6070;&#6042;&#6030;&#6087;',
+	'champ_monter' => 'monter', # NEW
+	'champ_necessaire' => 'Ce champ doit &ecirc;tre rempli.', # NEW
+	'champ_nom' => 'Nom du champ', # NEW
+	'champ_nom_bloc' => 'Nom du bloc', # NEW
+	'champ_nom_groupe' => 'Groupe', # NEW
+	'champ_nom_texte' => 'Texte', # NEW
+	'champ_public' => 'Ce champ est visible dans l\'espace public', # NEW
+	'champ_saisie_desactivee' => 'D&eacute;sactiver la saisie de ce champ', # NEW
+	'champ_specifiant' => 'Ce champ qualifie la donn&eacute;e (tri, filtre, description)', # NEW
+	'champ_table_jointure_type' => 'Type de table pour les jointures', # NEW
+	'champ_type_date' => 'Date', # NEW
+	'champ_type_email' => 'Adresse e-mail', # NEW
+	'champ_type_fichier' => 'Fichier &agrave; t&eacute;l&eacute;charger', # NEW
+	'champ_type_joint' => 'Jointure avec une autre table', # NEW
+	'champ_type_ligne' => 'Ligne de texte', # NEW
+	'champ_type_monnaie' => 'Mon&eacute;taire', # NEW
+	'champ_type_mot' => 'Mots-cl&eacute;s', # NEW
+	'champ_type_multiple' => 'Choix multiple', # NEW
+	'champ_type_numerique' => 'Num&eacute;rique', # NEW
+	'champ_type_password' => 'Mot de passe', # NEW
+	'champ_type_select' => 'Choix unique', # NEW
+	'champ_type_separateur' => 'Nouveau bloc de questions', # NEW
+	'champ_type_texte' => 'Texte', # NEW
+	'champ_type_textestatique' => 'Message d\'explication', # NEW
+	'champ_type_url' => 'Adresse de site Web', # NEW
+	'champ_url_details' => 'Veuillez entrer une adresse Web valide (de type http://www.monsite.com/...).', # NEW
+	'champs_formulaire' => 'Champs du formulaire', # NEW
+	'changer_choix_multiple' => 'Changer en choix multiple', # NEW
+	'changer_choix_unique' => 'Changer en choix unique', # NEW
+	'choisir_email' => 'Choisir l\'email en fonction de', # NEW
+	'confirm_supprimer_champ' => 'Etes-vous certain de vouloir supprimer le champ \'@champ@\' ?', # NEW
+	'confirm_supprimer_donnee' => 'Etes-vous certain de vouloir supprimer la donnee \'@donnee@\' ?', # NEW
+	'confirm_vider_table' => 'Etes-vous certain de vouloir vider le contenu de la table \'@table@\' ?', # NEW
+	'confirmer_champ_password' => 'Libelle si double saisie', # NEW
+	'confirmer_password' => 'Confirmation', # NEW
+	'confirmer_reponse' => 'Envoyer un mail d\'accus&eacute; de r&eacute;ception avec :', # NEW
+	'csv_classique' => 'CSV classique (,)', # NEW
+	'csv_excel' => 'CSV pour Excel (;)', # NEW
+	'csv_tab' => 'CSV avec tabulations', # NEW
 
 	// D
 	'date' => '&#6032;&#6098;&#6020;&#6083;&#6017;&#6082;&#6022;&#6098;&#6035;&#6070;&#6086;',
+	'date_invalide' => 'Format de la date invalide', # NEW
+	'donnees_modifiable' => 'Donn&eacute;es modifiables par l\'utilisateur.', # NEW
+	'donnees_multiple' => 'R&eacute;ponses multiples.', # NEW
+	'donnees_nonmodifiable' => 'Donn&eacute;es non modifiables par l\'utilisateur.', # NEW
+	'donnees_nonmultiple' => 'R&eacute;ponse unique.', # NEW
+	'donnees_prot' => 'Donn&eacute;es prot&eacute;g&eacute;es. Les donn&eacute;es enregistr&eacute;es ne seront accessibles que depuis l\'interface priv&eacute;e.', # NEW
+	'donnees_pub' => 'Donn&eacute;es publiques. Les donn&eacute;es enregitr&eacute;es seront accessibles aux visiteurs du site.', # NEW
+	'dupliquer' => 'Dupliquer', # NEW
 
 	// E
 	'echec_upload' => '&#6036;&#6035;&#6098;&#6033;&#6081;&#6042;&#6063;&#6016;&#6047;&#6070;&#6042; &#6036;&#6070;&#6035;&#6036;&#6042;&#6070;&#6023;&#6096;&#6041;',
+	'edit_champ_obligatoire' => 'Ce champ est obligatoire', # NEW
 	'editer' => '&#6016;&#6082;&#6036;&#6098;&#6042;&#6082;',
+	'email_independant' => 'Email independant de la reponse', # NEW
 	'exporter' => '&#6035;&#6070;&#6086;&#6021;&#6081;&#6025;',
+	'exporter_article' => 'Exporter dans un article', # NEW
 
 	// F
 	'fichier_trop_gros' => '&#6063;&#6016;&#6047;&#6070;&#6042;&#6035;&#6081;&#6087; &#6040;&#6070;&#6035;&#6033;&#6086;&#6048;&#6086;&#6034;&#6086;&#6038;&#6081;&#6016;&#6100;',
+	'fichier_type_interdit' => 'Ce type de fichier est interdit.', # NEW
+	'form_erreur' => 'Erreur&nbsp;:', # NEW
+	'format_fichier' => 'Format du fichier&nbsp;:', # NEW
+	'format_liste' => 'd&eacute;roulante', # NEW
+	'format_liste_ou_radio' => 'Format de la liste', # NEW
+	'format_radio' => 'boutons radio', # NEW
+	'forms_obligatoires' => 'Formulaires obligatoires pour la saisie de celui-ci&nbsp;:', # NEW
+	'formulaire' => 'Formulaire', # NEW
+	'formulaire_aller' => 'Aller au formulaire', # NEW
+	'formulaires_copie' => 'Copie de @nom@', # NEW
+	'formulaires_sondages' => 'Formulaires et sondages', # NEW
+
+	// H
+	'html_wrapper' => 'Encapsuler le champ dans le code html', # NEW
+
+	// I
+	'icone_ajouter_donnees' => 'Ajouter des r&eacute;ponses', # NEW
+	'icone_creer_formulaire' => 'Cr&eacute;er un nouveau formulaire', # NEW
+	'icone_creer_table' => 'Cr&eacute;er un nouveau formulaire', # NEW
+	'importer_form' => 'Importer un formulaire', # NEW
+	'info_apparence' => 'Voici une pr&eacute;visualisation du formulaire tel qu\'il appara&icirc;tra aux visiteurs du site public.', # NEW
+	'info_articles_lies_donnee' => 'Les articles li&eacute;s', # NEW
+	'info_champs_formulaire' => 'Vous pouvez ici cr&eacute;er et modifier les champs que les visiteurs pourront remplir.', # NEW
+	'info_obligatoire_02' => '[Obligatoire]', # NEW
+	'info_rubriques_liees_donnee' => 'Les rubriques li&eacute;es', # NEW
+	'info_sondage' => 'Si votre formulaire est un sondage, les r&eacute;sultats des champs de type &laquo; s&eacute;lection &raquo; seront additionn&eacute;s et affich&eacute;s.', # NEW
+	'info_supprimer_formulaire' => 'Voulez-vous vraiment supprimer ce formulaire ?', # NEW
+	'info_supprimer_formulaire_reponses' => 'Des donn&eacute;es sont associ&eacute;es &agrave; ce formulaire. Voulez-vous vraiment le supprimer ?', # NEW
+
+	// L
+	'lien_apercu' => 'Aper&ccedil;u', # NEW
+	'lien_champ' => 'Champs', # NEW
+	'lien_propriete' => 'Propri&eacute;t&eacute;s', # NEW
+	'lien_retirer_donnee_liante' => 'Retirer le lien', # NEW
+	'lien_retirer_donnee_liee' => 'Retirer le lien', # NEW
+	'lier_articles' => 'Permettre d\'associer les donn&eacute;es aux articles', # NEW
+	'lier_documents' => 'Permettre de joindre des documents aux donn&eacute;es', # NEW
+	'lier_documents_mail' => 'Joindre les documents &agrave; l\'email', # NEW
+	'liste_choix' => 'Liste des choix propos&eacute;s', # NEW
+
+	// M
+	'moderation_donnees' => 'Valider les donn&eacute;es avant publication :', # NEW
+	'modifiable_donnees' => 'Donn&eacute;s modifiables dans l\'espace public&nbsp;:', # NEW
+	'monetaire_invalide' => 'Champ mon&eacute;taire invalide', # NEW
+	'monnaie_euro' => 'Euro (&euro;)', # NEW
+	'multiple_donnees' => 'Saisie des donn&eacute;es dans l\'espace public&nbsp;:', # NEW
+
+	// N
+	'nb_decimales' => 'Nombre de d&eacute;cimales', # NEW
+	'nombre_reponses' => '@nombre@ donn&eacute;es', # NEW
+	'nouveau_champ' => 'Nouveau champ', # NEW
+	'nouveau_choix' => 'Nouveau choix', # NEW
+	'nouveau_formulaire' => 'Nouveau formulaire', # NEW
+	'numerique_invalide' => 'Champ num&eacute;rique invalide', # NEW
+
+	// P
+	'page' => 'Page', # NEW
+	'pas_mail_confirmation' => 'Pas de mail confirmation', # NEW
+	'probleme_technique' => 'Probl&egrave;me technique. Votre saisie n\'a pas pu &ecirc;tre prise en compte.', # NEW
+	'probleme_technique_upload' => 'Probl&egrave;me technique. Le transfert du fichier a &eacute;chou&eacute;.', # NEW
+	'publication_donnees' => 'Publication des donn&eacute;es', # NEW
 
 	// R
+	'rang' => 'Rang', # NEW
 	'remplir_un_champ' => '&#6047;&#6076;&#6040;&#6036;&#6086;&#6038;&#6081;&#6025; &#6041;&#6089;&#6070;&#6020;&#6031;&#6071;&#6021; &#6040;&#6077;&#6041;&#6044;&#6070;&#6043;&#6100;',
 	'reponse' => '&#6021;&#6086;&#6043;&#6078;&#6041; @id_reponse@',
 	'reponse_depuis' => '&#6038;&#6072; &#6033;&#6086;&#6038;&#6096;&#6042;',
+	'reponse_enregistree' => 'Votre saisie a &eacute;t&eacute; enregistr&eacute;e.', # NEW
 	'reponse_envoyee' => '&#6021;&#6086;&#6043;&#6078;&#6041; &#6031;&#6098;&#6042;&#6076;&#6044;&#6036;&#6070;&#6035;&#6037;&#6098;&#6025;&#6078;',
+	'reponse_envoyee_a' => '&agrave;', # NEW
+	'reponse_retrovez' => 'Retrouvez cette r&eacute;ponse dans l\'interface d\'administration :', # NEW
 	'reponses' => '&#6021;&#6086;&#6043;&#6078;&#6041;',
 	'resultats' => '&#6043;&#6033;&#6098;&#6034;&#6037;&#6043;&#6102;',
 
 	// S
 	'site_introuvable' => '&#6018;&#6098;&#6040;&#6070;&#6035;&#6042;&#6016;&#6019;&#6078;&#6025; &#6047;&#6083;&#6032;&#6093;&#6035;&#6081;&#6087;&#6100;',
+	'sondage_deja_repondu' => 'Vous avez d&eacute;j&agrave; r&eacute;pondu a ce sondage.', # NEW
+	'sondage_non' => 'Ce formulaire n\'est pas un sondage', # NEW
+	'sondage_oui' => 'Ce formulaire est un sondage.', # NEW
+	'suivi_formulaire' => 'Suivi du formulaire', # NEW
+	'suivi_formulaires' => 'Suivi des formulaires', # NEW
+	'suivi_reponses' => 'Suivi des r&eacute;ponses', # NEW
 	'supprimer' => '&#6043;&#6075;&#6036;&#6021;&#6081;&#6025;',
 	'supprimer_champ' => '&#6043;&#6075;&#6036;&#6021;&#6081;&#6025; &#6044;&#6070;&#6043;&#6035;&#6081;&#6087;',
 	'supprimer_choix' => '&#6043;&#6075;&#6036;&#6021;&#6081;&#6025; &#6023;&#6086;&#6042;&#6078;&#6047;&#6035;&#6081;&#6087;',
@@ -58,9 +198,22 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'taille_max' => '&#6033;&#6086;&#6048;&#6086;&#6050;&#6031;&#6071;&#6036;&#6042;&#6071;&#6040;&#6070; (&#6023;&#6070; &#6018;&#6072;&#6049;&#6076;&#6036;&#6083;)',
 	'telecharger' => '&#6033;&#6070;&#6025;&#6041;&#6016;',
 	'telecharger_reponses' => '&#6033;&#6070;&#6025;&#6041;&#6016; &#6021;&#6086;&#6043;&#6078;&#6041;',
+	'titre_formulaire' => 'Titre du formulaire', # NEW
+	'total_votes' => 'Total des votes', # NEW
+	'tous_formulaires' => 'Tous les formulaires', # NEW
+	'tous_sondages' => 'Tous les sondages', # NEW
+	'tous_sondages_proteges' => 'Tous les sondages prot&eacute;g&eacute;s', # NEW
+	'tous_sondages_public' => 'Tous les sondages publics', # NEW
+	'toutes_tables' => 'Tous les formulaires', # NEW
+	'type_form' => 'Type de formulaire', # NEW
+
+	// U
+	'une_reponse' => 'une donn&eacute;e', # NEW
+	'unite_monetaire' => 'Unit&eacute; mon&eacute;taire', # NEW
 
 	// V
 	'valider' => '&#6026;&#6070;&#6016;&#6091;&#6047;&#6098;&#6035;&#6078;',
+	'verif_web' => 'v&eacute;rifier l\'existence du site Web', # NEW
 	'vider' => '&#6043;&#6075;&#6036;&#6047;&#6086;&#6050;&#6070;&#6031;',
 	'voir_article' => '&#6040;&#6078;&#6043; &#6050;&#6031;&#6098;&#6032;&#6036;&#6033;',
 	'voir_resultats' => '&#6040;&#6078;&#6043; &#6043;&#6033;&#6098;&#6034;&#6037;&#6043;'
