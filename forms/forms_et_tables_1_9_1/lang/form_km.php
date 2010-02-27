a70e7fce358c8170588922695db67cbd88dc45ba
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-02-27 18:54:05 +0000

    langues (form)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@35657 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- forms/forms_et_tables_1_9_1/lang/form_km.php
+++ forms/forms_et_tables_1_9_1/lang/form_km.php
@@ -8,6 +8,7 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
+	'articles_utilisant' => 'Articles utilisant ce formulaire', # NEW
 	'aucune_reponse' => '&#6018;&#6098;&#6040;&#6070;&#6035;&#6021;&#6086;&#6043;&#6078;&#6041;',
 
 	// C
@@ -20,6 +21,13 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'icone_ajouter_donnees' => '&#6036;&#6035;&#6098;&#6032;&#6082;&#6040; &#6021;&#6086;&#6043;&#6078;&#6041;',
 	'icone_creer_table' => '&#6036;&#6020;&#6098;&#6016;&#6078;&#6031; &#6040;&#6077;&#6041;&#6036;&#6082;&#6036;&#6033;&#6032;&#6098;&#6040;&#6072;',
 	'importer_donnees_csv' => '&#6035;&#6070;&#6086;&#6021;&#6076;&#6043; &#6021;&#6086;&#6043;&#6078;&#6041;',
+	'info_supprimer_formulaire' => 'Voulez-vous vraiment supprimer ce formulaire ?', # NEW
+	'info_supprimer_formulaire_reponses' => 'Des R&eacute;ponse sont associ&eacute;es &agrave; ce formulaire. 
+Voulez-vous vraiment le supprimer ?', # NEW
+
+	// L
+	'lien_retirer_donnee_liante' => 'Retirer le lien depuis cette r&eacute;ponse', # NEW
+	'lien_retirer_donnee_liee' => 'Retirer cette r&eacute;ponse', # NEW
 
 	// N
 	'nombre_reponses' => '@nombre@ &#6021;&#6086;&#6043;&#6078;&#6041;',
@@ -27,12 +35,19 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// S
 	'suivi_reponses' => '&#6040;&#6078;&#6043; &#6042;&#6070;&#6043;&#6091;&#6021;&#6086;&#6043;&#6078;&#6041;',
+	'supprimer_formulaire' => 'Supprimer ce formulaire', # NEW
 
 	// T
+	'telecharger_reponses' => 'T&eacute;l&eacute;chargez les r&eacute;ponses', # NEW
 	'texte_donnee_statut' => '&#6047;&#6098;&#6032;&#6070;&#6035;&#6039;&#6070;&#6038; &#6035;&#6083;&#6021;&#6086;&#6043;&#6078;&#6041;&#6035;&#6081;&#6087;',
 	'texte_statut_poubelle' => '&#6021;&#6086;&#6043;&#6078;&#6041; &#6031;&#6098;&#6042;&#6076;&#6044;&#6036;&#6070;&#6035;&#6043;&#6075;&#6036;&#6021;&#6081;&#6025;',
+	'texte_statut_prepa' => 'R&eacute;ponse en cours de r&eacute;daction', # NEW
+	'texte_statut_prop' => 'R&eacute;ponse propos&eacute;e', # NEW
+	'texte_statut_publie' => 'R&eacute;ponse publi&eacute;e', # NEW
+	'texte_statut_refuse' => 'R&eacute;ponse refus&eacute;e', # NEW
 	'titre_formulaire' => '&#6021;&#6086;&#6030;&#6020;&#6023;&#6078;&#6020; &#6035;&#6083;&#6036;&#6082;&#6036;&#6036;&#6033;',
 	'toutes_tables' => '&#6018;&#6098;&#6042;&#6036;&#6091;&#6036;&#6082;&#6036;&#6036;&#6033;',
+	'type_des_tables' => 'Formulaires', # NEW
 
 	// U
 	'une_reponse' => '&#6040;&#6077;&#6041;&#6021;&#6086;&#6043;&#6078;&#6041;'
--- forms/forms_et_tables_1_9_1/lang/form_sv.php
+++ forms/forms_et_tables_1_9_1/lang/form_sv.php
@@ -7,8 +7,50 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
+	// A
+	'articles_utilisant' => 'Articles utilisant ce formulaire', # NEW
+	'aucune_reponse' => 'Aucune r&eacute;ponse', # NEW
+
+	// C
+	'champs_formulaire' => 'Champs du formulaire', # NEW
+
+	// F
+	'formulaire' => 'Formulaire', # NEW
+
 	// I
-	'info_supprimer_formulaire' => 'Vill du verkligen radera formul&auml;ret ?'
+	'icone_ajouter_donnees' => 'Ajouter des r&eacute;ponses', # NEW
+	'icone_creer_table' => 'Cr&eacute;er un nouveau formulaire', # NEW
+	'importer_donnees_csv' => 'Importer des r&eacute;ponses', # NEW
+	'info_supprimer_formulaire' => 'Vill du verkligen radera formul&auml;ret ?',
+	'info_supprimer_formulaire_reponses' => 'Des R&eacute;ponse sont associ&eacute;es &agrave; ce formulaire. 
+Voulez-vous vraiment le supprimer ?', # NEW
+
+	// L
+	'lien_retirer_donnee_liante' => 'Retirer le lien depuis cette r&eacute;ponse', # NEW
+	'lien_retirer_donnee_liee' => 'Retirer cette r&eacute;ponse', # NEW
+
+	// N
+	'nombre_reponses' => '@nombre@ r&eacute;ponses', # NEW
+	'nouveau_formulaire' => 'Nouveau formulaire', # NEW
+
+	// S
+	'suivi_reponses' => 'Voir les r&eacute;ponses', # NEW
+	'supprimer_formulaire' => 'Supprimer ce formulaire', # NEW
+
+	// T
+	'telecharger_reponses' => 'T&eacute;l&eacute;chargez les r&eacute;ponses', # NEW
+	'texte_donnee_statut' => 'Statut de cette r&eacute;ponse', # NEW
+	'texte_statut_poubelle' => 'R&eacute;ponse supprim&eacute;e', # NEW
+	'texte_statut_prepa' => 'R&eacute;ponse en cours de r&eacute;daction', # NEW
+	'texte_statut_prop' => 'R&eacute;ponse propos&eacute;e', # NEW
+	'texte_statut_publie' => 'R&eacute;ponse publi&eacute;e', # NEW
+	'texte_statut_refuse' => 'R&eacute;ponse refus&eacute;e', # NEW
+	'titre_formulaire' => 'Titre du formulaire', # NEW
+	'toutes_tables' => 'Tous les formulaires', # NEW
+	'type_des_tables' => 'Formulaires', # NEW
+
+	// U
+	'une_reponse' => 'Une r&eacute;ponse', # NE
 );
 
 ?>
