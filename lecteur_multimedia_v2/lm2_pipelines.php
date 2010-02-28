0f51be463a25f992908c0c4b628d40e84873f294
booz@rezo.net <booz@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-02-28 23:44:56 +0000

    Pour traiter les liens [xx->.mp3] dans les textes on passe par le pipeline pre_liens, c'est mieux que par post_propre qui avait le defaut de traiter des url dans les modeles de b_b. Et puis c'est plus logique finalement.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@35693 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- lecteur_multimedia_v2/lm2_pipelines.php
+++ lecteur_multimedia_v2/lm2_pipelines.php
@@ -12,18 +12,23 @@ function lm2_insert_head($flux){
 /**
  * enclosures
  * ajout d'un rel="enclosure" sur les liens mp3 absolus
- * permet de traiter les [mon son->http://monsite/mon_son.mp3] dans un texte.
- * Le filtre peut etre appele dans un squelette apres |liens_absolus
+ * appele en pipeline apres propre pour traiter les [mon son->http://monsite/mon_son.mp3]
+ * peut etre appele dans un squelette apres |liens_absolus
+ * pète cependant avec les cas (tordus) suivants :
+ * [{{Une histoire d'amour}}->documents/sons/PIRATAGE/01 UNE HISTOIRE D'AMOUR.mp3]
+ * [{{Une histoire d'amour à trois}}->documents/sons/PIRATAGE/02 UNE HISTOIRE D'AMOUR A TROIS[2].mp3]
  */
+ 
+function lm2_pre_liens($texte) {
+	define('_RACCOURCI_LIEN_MP3', "/\[([^][]*?([[]\w*[]][^][]*)*)->(>?)([^]\.mp3]*)\]/msS");
 
- function lm2_post_propre($texte) {
-
-	$reg_formats="mp3";
-
-	$texte = preg_replace(
-		",<a(\s[^>]*href=['\"]?(http:\/\/[a-zA-Z0-9\s()\/\:\._%\?+'=~-]*\.($reg_formats))['\"]?[^>]*)>(.*)</a>,Uims",
-		'<a$1 rel="enclosure">$4</a>',
-		$texte);
+	if (preg_match_all(_RACCOURCI_LIEN, $texte, $regs, PREG_SET_ORDER)) {
+		foreach ($regs as $k => $reg) {
+		$l = "<a href='$reg[4]' rel='enclosure'>$reg[1]</a>";
+		$p = $reg[0] ;
+		$texte = str_replace($p,$l,$texte);
+		}	
+	}
 
 	return $texte;
 }
