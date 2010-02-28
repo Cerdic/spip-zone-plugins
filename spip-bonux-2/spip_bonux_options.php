aba92d2983f650deec720a8ff3637c2fdd3286f2
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-02-28 13:26:14 +0000

    On applique la transformation de la chaine seulement si le paramètre est effectivement une chaine.
    Et si la valeur est un tableau, on ne fait pas rien, on applique la fonction récursivement dessus.
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@35684 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spip-bonux-2/spip_bonux_options.php
+++ spip-bonux-2/spip_bonux_options.php
@@ -13,16 +13,27 @@
  * si oui applique _T()
  * si non applique typo()
  */
-function _T_ou_typo($texte, $args=array()) {
+function _T_ou_typo($valeur, $args=array()) {
 	
-	if (preg_match('/^\<:(.*?):\>$/',$texte,$match)) 
-		$text = _T($match[1],$args);
-	else {
-		include_spip('inc/texte');
-		$text = typo($texte);
+	// Si la valeur est bien une chaine (et pas non plus un entier déguisé)
+	if (is_string($valeur) and !intval($valeur)){
+		// Si la chaine est du type <:truc:> on passe à _T()
+		if (preg_match('/^\<:(.*?):\>$/', $valeur, $match)) 
+			$valeur = _T($match[1], $args);
+		// Sinon on la passe a typo()
+		else {
+			include_spip('inc/texte');
+			$valeur = typo($valeur);
+		}
+	}
+	// Si c'est un tableau, on reapplique la fonction recursivement
+	elseif (is_array($valeur)){
+		foreach ($valeur as $cle => $valeur2){
+			$valeur[$cle] = _T_ou_typo($valeur2, $args);
+		}
 	}
 
-	return $text;
+	return $valeur;
 
 }
 if (defined('_BONUX_STYLE'))
