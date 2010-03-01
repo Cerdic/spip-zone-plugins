b00fc0545daa0fc41247265138b4053a15d2b6a9
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-03-01 15:32:03 +0000

    On enlève le deuxième argument de _T_ou_typo() et on le remplace par un choix du mode d'application de typo(). Soit "toujours", soit "jamais", soit "multi" cad que lorsqu'il y a des <multi> dans la chaine
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@35704 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spip-bonux-2/spip_bonux_options.php
+++ spip-bonux-2/spip_bonux_options.php
@@ -11,25 +11,34 @@
  * une fonction qui regarde si $texte est une chaine de langue
  * de la forme <:qqch:>
  * si oui applique _T()
- * si non applique typo()
+ * si non applique typo() suivant le mode choisi
+ *
+ * @param unknown_type $valeur Une valeur à tester. Si c'est un tableau, la fonction s'appliquera récursivement dessus.
+ * @param string $mode_typo Le mode d'application de la fonction typo(), avec trois valeurs possibles "toujours", "jamais" ou "multi".
+ * @return unknown_type Retourne la valeur éventuellement modifiée.
  */
-function _T_ou_typo($valeur, $args=array()) {
+function _T_ou_typo($valeur, $mode_typo='toujours') {
 	
 	// Si la valeur est bien une chaine (et pas non plus un entier déguisé)
 	if (is_string($valeur) and !intval($valeur)){
 		// Si la chaine est du type <:truc:> on passe à _T()
 		if (preg_match('/^\<:(.*?):\>$/', $valeur, $match)) 
-			$valeur = _T($match[1], $args);
+			$valeur = _T($match[1]);
 		// Sinon on la passe a typo()
 		else {
-			include_spip('inc/texte');
-			$valeur = typo($valeur);
+			if (!in_array($mode_typo, array('toujours', 'multi', 'jamais')))
+				$mode_typo = 'toujours';
+			
+			if ($mode_typo == 'toujours' or ($mode_typo == 'multi' and strpos($valeur, '<multi>') !== false)){
+				include_spip('inc/texte');
+				$valeur = typo($valeur);
+			}
 		}
 	}
-	// Si c'est un tableau, on reapplique la fonction recursivement
+	// Si c'est un tableau, on reapplique la fonction récursivement
 	elseif (is_array($valeur)){
 		foreach ($valeur as $cle => $valeur2){
-			$valeur[$cle] = _T_ou_typo($valeur2, $args);
+			$valeur[$cle] = _T_ou_typo($valeur2);
 		}
 	}
 
