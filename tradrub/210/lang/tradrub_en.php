a3f560e14f35e412a9f5aac7d314601ea1ad0f18
paolo2@taize.fr <paolo2@taize.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-03-02 18:17:09 +0000

    vraiment ajouter anglais
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@35731 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ tradrub/210/lang/tradrub_en.php
@@ -0,0 +1,18 @@
+<?php
+
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	'traduction_rubrique' => "Translations of this section",
+	'info_toutes_traductions_rubriques' => "Translations of the section",
+	'langue' => "Language",
+	'label_id_trad' => "Translation",
+	//'explication_id_trad_rubrique' => "D&eacute;finir l'origine de cette traduction",
+	'enregistrement_de_la_traduction_ok' => "The translation link has been saved.",
+	'trad_rubrique_traduction' => "All versions of this section:",
+	'trad_new_rubrique' => "Create a new translation of this section",
+	'trad_delier_rubrique' => "Unlink this section from these translations",
+	'rubrique_de_reference' => "Reference section"
+);
+
+?>
