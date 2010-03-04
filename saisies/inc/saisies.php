c1004431cdc45ff4e2ee3f21d979c2e05ca44f5e
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-03-04 17:35:38 +0000

    On renomme certaines fonctions et on ajoute aussi saisies_chercher() pour chercher une saisie suivant son nom de champ. Et ça renvoie soit la saisie, soit son chemin dans l'arborescence.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@35796 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- saisies/inc/saisies.php
+++ saisies/inc/saisies.php
@@ -3,27 +3,84 @@
 // Sécurité
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
-// Une librairie pour manipuler ou obtenir des infos sur un tableau de saisies
+/*
+ * Une librairie pour manipuler ou obtenir des infos sur un tableau de saisies
+ *
+ * saisies_lister_par_nom()
+ * saisies_lister_champs()
+ * saisies_chercher()
+ * saisies_chemin()
+ * saisies_inserer_apres()
+ * saisies_supprimer()
+ * saisies_verifier()
+ * saisies_generer_html()
+ * saisies_generer_nom()
+ * saisies_inserer_html()
+ * saisies_lister_disponibles()
+ * saisies_autonomes()
+ */
 
 
 /*
- * Quelles sont les saisies qui se débrouillent toutes seules, sans le _base commun
+ * Prend la description complète du contenu d'un formulaire et retourne
+ * les saisies "à plat" classées par nom.
  *
- * @return array Retourne un tableau contenant les types de saisies qui ne doivent pas utiliser le _base.html commun
+ * @param array $contenu Le contenu d'un formulaire
+ * @return array Un tableau avec uniquement les saisies
  */
-function saisies_autonomes(){
-	$saisies_autonomes = pipeline(
-		'saisies_autonomes',
-		array(
-			'fieldset',
-			'hidden',
-			'destinataires'
-		)
-	);
-
-	return $saisies_autonomes;
+function saisies_lister_par_nom($contenu){
+	$saisies = array();
+	
+	if (is_array($contenu)){
+		foreach ($contenu as $ligne){
+			if (is_array($ligne)){
+				if (array_key_exists('saisie', $ligne)){
+					$saisies[$ligne['options']['nom']] = $ligne;
+				}
+				if (is_array($ligne['saisies'])){
+					$saisies = array_merge($saisies, saisies_lister_par_nom($ligne['saisies']));
+				}
+			}
+		}
+	}
+	
+	return $saisies;
 }
 
+/*
+ * Prend la description complète du contenu d'un formulaire et retourne
+ * une liste des noms des champs du formulaire.
+ *
+ * @param array $contenu Le contenu d'un formulaire
+ * @return array Un tableau listant les noms des champs
+ */
+function saisies_lister_champs($contenu){
+	$saisies = saisies_lister_par_nom($contenu);
+	return array_keys($saisies);
+}
+
+/*
+ * Cherche une saisie par son nom et renvoie soit la saisie, soit son chemin
+ *
+ * @param array $saisies Un tableau décrivant les saisies
+ * @param string $nom Le nom de la saisie à chercher
+ * @param bool $retourner_chemin Indique si on retourne non pas la saisie mais son chemin
+ * @return array Retourne soit la saisie, soit son chemin, soit null
+ */
+function saisies_chercher($saisies, $nom, $retourner_chemin=false){
+	if (is_array($saisies) and $nom){
+		foreach($saisies as $cle => $saisie){
+			$chemin = array($cle);
+			if ($saisie['options']['nom'] == $nom)
+				return $retourner_chemin ? $chemin : $saisie;
+			elseif ($saisie['saisies'] and is_array($saisie['saisies']) and ($retour = saisies_chercher($saisie['saisies'], $nom, $retourner_chemin))){
+				return $retourner_chemin ? array_merge($chemin, $retour) : $retour;
+			}
+		}
+	}
+	
+	return null;
+}
 
 /*
  * Génère une saisie à partir d'un tableau la décrivant et de l'environnement
@@ -39,17 +96,17 @@ function saisies_autonomes(){
  *		2 => array(etc)
  * )
  * Les options de la saisie ont une clé textuelle, tandis que les éventuelles saisies enfants ont des clés numériques indiquant leur rang.
- *
+ * 
  * @param array $saisie La description d'une saisie sous la forme d'un tableau
  * @param arary $env L'environnement dans lequel sera construit la saisie (sert notamment pour récupérer la valeur du champ)
  * @return string Retourne le HTML de la saisie
  */
 function saisies_generer_html($saisie, $env=array()){
-
+	
 	// Si le parametre n'est pas bon, on genere du vide
 	if (!is_array($saisie))
 		return '';
-
+	
 	// On sépare les options et les saisies enfants
 	$cles = array_keys($saisie);
 	// Les saisies enfants ce sont les clés numériques
@@ -62,15 +119,15 @@ function saisies_generer_html($saisie, $env=array()){
 	// On récupère chaque morceaux
 	$options = array_intersect_key($saisie, $cles_options);
 	$enfants = array_intersect_key($saisie, $cles_enfants);
-
+	
 	// Le contexte c'est d'abord les options de la saisie
 	// Peut-être des transformations à faire sur les options textuelles
 	$contexte = _T_ou_typo($options, 'multi');
-
+	
 	// Si on ne trouve pas de type de saisie on met input par defaut
 	if (!$contexte['type_saisie'])
 		$contexte['type_saisie'] = 'input';
-
+	
 	// Si env est définie dans les options, on ajoute tout l'environnement
 	if(isset($contexte['env'])){
 		unset($contexte['env']);
@@ -82,7 +139,7 @@ function saisies_generer_html($saisie, $env=array()){
 		// On récupère la liste des erreurs
 		$contexte['erreurs'] = $env['erreurs'];
 	}
-
+	
 	// Dans tous les cas on récupère de l'environnement la valeur actuelle du champ
 	// Si le nom du champ est un tableau indexé, il faut parser !
 	if (preg_match('/([\w]+)((\[[\w]+\])+)/', $contexte['nom'], $separe)){
@@ -96,56 +153,18 @@ function saisies_generer_html($saisie, $env=array()){
 	// Sinon la valeur est juste celle du nom
 	else
 		$contexte['valeur'] = $env[$contexte['nom']];
-
+	
 	// S'il y a des saisies enfants, on les ajoute au contexte
 	if ($enfants and is_array($enfants)){
 		$contexte['saisies'] = $enfants;
 	}
-
+	
 	// On génère la saisie
 	return recuperer_fond(
 		'saisies/_base',
 		$contexte
 	);
-
-}
-
-/*
- * Prend la description complète du contenu d'un formulaire et retourne
- * les saisies "à plat" classées par nom.
- *
- * @param array $contenu Le contenu d'un formulaire
- * @return array Un tableau avec uniquement les saisies
- */
-function saisies_recuperer_saisies($contenu){
-	$saisies = array();
-
-	if (is_array($contenu)){
-		foreach ($contenu as $ligne){
-			if (is_array($ligne)){
-				if (array_key_exists('saisie', $ligne)){
-					$saisies[$ligne['options']['nom']] = $ligne;
-				}
-				if (is_array($ligne['saisies'])){
-					$saisies = array_merge($saisies, saisies_recuperer_saisies($ligne['saisies']));
-				}
-			}
-		}
-	}
-
-	return $saisies;
-}
-
-/*
- * Prend la description complète du contenu d'un formulaire et retourne
- * une liste des noms des champs du formulaire.
- *
- * @param array $contenu Le contenu d'un formulaire
- * @return array Un tableau listant les noms des champs
- */
-function saisies_recuperer_champs($contenu){
-	$saisies = saisies_recuperer_saisies($contenu);
-	return array_keys($saisies);
+	
 }
 
 /*
@@ -154,16 +173,16 @@ function saisies_recuperer_champs($contenu){
  * @return array Retourne un tableau d'erreurs
  */
 function saisies_verifier($formulaire){
+	include_spip('inc/verifier');
 	$erreurs = array();
-
 	$verif_fonction = charger_fonction('verifier','inc',true);
-
-	$saisies = saisies_recuperer_saisies($formulaire);
+	
+	$saisies = saisies_lister_par_nom($formulaire);
 	foreach ($saisies as $saisie){
 		$obligatoire = $saisie['options']['obligatoire'];
 		$champ = $saisie['options']['nom'];
 		$verifier = $saisie['verifier'];
-
+		
 		// Si le nom du champ est un tableau indexé, il faut parser !
 		if (preg_match('/([\w]+)((\[[\w]+\])+)/', $champ, $separe)){
 			$valeur = _request($separe[1]);
@@ -176,11 +195,11 @@ function saisies_verifier($formulaire){
 		// Sinon la valeur est juste celle du nom
 		else
 			$valeur = _request($champ);
-
+		
 		// On regarde d'abord si le champ est obligatoire
 		if ($obligatoire and $obligatoire != 'non' and ($valeur == ''))
 			$erreurs[$champ] = _T('info_obligatoire');
-
+		
 		// On continue seulement si ya pas d'erreur d'obligation et qu'il y a une demande de verif
 		if (!$erreurs[$champ] and is_array($verifier) and $verif_fonction){
 			// Si le champ n'est pas valide par rapport au test demandé, on ajoute l'erreur
@@ -188,8 +207,47 @@ function saisies_verifier($formulaire){
 				$erreurs[$champ] = $erreur_eventuelle;
 		}
 	}
-
+	
 	return $erreurs;
+}
+
+/**
+ * Génère un nom unique pour un champ d'un formulaire donné
+ *
+ * @param array $formulaire Le formulaire à analyser 
+ * @param string $type_saisie Le type de champ dont on veut un identifiant 
+ * @return string Un nom unique par rapport aux autres champs du formulaire
+ */
+function saisies_generer_nom($formulaire, $type_saisie){
+	$champs = saisies_lister_champs($formulaire);
+	
+	// Tant que type_numero existe, on incrémente le compteur
+	$compteur = 1;
+	while (array_search($type_saisie.'_'.$compteur, $champs) !== false)
+		$compteur++;
+	
+	// On a alors un compteur unique pour ce formulaire
+	return $type_saisie.'_'.$compteur;
+}
+
+/**
+ * Insère du HTML au début ou à la fin d'une saisie
+ *
+ * @param array $saisie La description d'une seule saisie
+ * @param string $insertion Du code HTML à insérer dans la saisie 
+ * @param string $ou L'endroit où insérer le HTML : "debut" ou "fin"
+ * @return array Retourne la description de la saisie modifiée
+ */
+function saisies_inserer_html($saisie, $insertion, $ou='fin'){
+	if (!in_array($ou, array('debut', 'fin')))
+		$ou = 'fin';
+	
+	if ($ou == 'debut')
+		$saisie['options']['inserer_debut'] = $insertion.$saisie['options']['inserer_debut'];
+	elseif ($ou == 'fin')
+		$saisie['options']['inserer_fin'] = $saisie['options']['inserer_fin'].$insertion;
+	
+	return $saisie;
 }
 
 /*
@@ -199,11 +257,11 @@ function saisies_verifier($formulaire){
  */
 function saisies_lister_disponibles(){
 	static $saisies = null;
-
+	
 	if (is_null($saisies)){
 		$saisies = array();
 		$liste = find_all_in_path('saisies/', '.+[.]yaml$');
-
+		
 		if (count($liste)){
 			foreach ($liste as $fichier=>$chemin){
 				$type_saisie = preg_replace(',[.]yaml$,i', '', $fichier);
@@ -219,7 +277,7 @@ function saisies_lister_disponibles(){
 			}
 		}
 	}
-
+	
 	return $saisies;
 }
 
@@ -241,43 +299,22 @@ function saisies_charger_infos($type_saisie){
 	return $saisie;
 }
 
-/**
- * Génère un nom unique pour un champ d'un formulaire donné
+/*
+ * Quelles sont les saisies qui se débrouillent toutes seules, sans le _base commun
  *
- * @param array $formulaire Le formulaire à analyser
- * @param string $type_saisie Le type de champ dont on veut un identifiant
- * @return string Un nom unique par rapport aux autres champs du formulaire
+ * @return array Retourne un tableau contenant les types de saisies qui ne doivent pas utiliser le _base.html commun
  */
-function saisies_generer_nom($formulaire, $type_saisie){
-	$champs = saisies_recuperer_champs($formulaire);
-
-	// Tant que type_numero existe, on incrémente le compteur
-	$compteur = 1;
-	while (array_search($type_saisie.'_'.$compteur, $champs) !== false)
-		$compteur++;
-
-	// On a alors un compteur unique pour ce formulaire
-	return $type_saisie.'_'.$compteur;
-}
-
-/**
- * Insère du HTML au début ou à la fin d'une saisie
- *
- * @param array $saisie La description d'une seule saisie
- * @param string $insertion Du code HTML à insérer dans la saisie
- * @param string $ou L'endroit où insérer le HTML : "debut" ou "fin"
- * @return array Retourne la description de la saisie modifiée
- */
-function saisies_inserer_html($saisie, $insertion, $ou='fin'){
-	if (!in_array($ou, array('debut', 'fin')))
-		$ou = 'fin';
-
-	if ($ou == 'debut')
-		$saisie['options']['inserer_debut'] = $insertion.$saisie['options']['inserer_debut'];
-	elseif ($ou == 'fin')
-		$saisie['options']['inserer_fin'] = $saisie['options']['inserer_fin'].$insertion;
-
-	return $saisie;
+function saisies_autonomes(){
+	$saisies_autonomes = pipeline(
+		'saisies_autonomes',
+		array(
+			'fieldset',
+			'hidden',
+			'destinataires'
+		)
+	);
+	
+	return $saisies_autonomes;
 }
 
 ?>
