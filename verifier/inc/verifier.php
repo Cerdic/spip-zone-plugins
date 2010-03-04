6df1c57ac8cf3f2f2cfef41b1076d3ff9f905233
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-03-04 15:37:12 +0000

    On passe en inc_verifier_dist permettant de le squeezer si le plugin n'est pas présent en faisant :
    $verifier = charger_fonction('verifier','inc',true);
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@35793 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- verifier/inc/verifier.php
+++ verifier/inc/verifier.php
@@ -10,20 +10,20 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
  * @param array $options Un eventuel tableau d'options suivant le type.
  * @return string Retourne une chaine vide si c'est valide, sinon une chaine expliquant l'erreur.
  */
-function verifier($valeur, $type, $options=null){
+function inc_verifier_dist($valeur, $type, $options=null){
 
 	// On vérifie que les options sont bien un tableau
 	if (!is_array($options))
 		$options = array();
-	
+
 	// Si la valeur est vide, il n'y a rien a verifier donc c'est bon
 	if (!$valeur) return '';
-	
+
 	// On cherche si une fonction correspondant au type existe
-	if ($verifier = charger_fonction($type, 'verifier/')){
+	if ($verifier = charger_fonction($type, 'verifier',true)){
 		$erreur = $verifier($valeur, $options);
 	}
-	
+
 	// On passe le tout dans le pipeline du meme nom
 	$erreur = pipeline(
 		'verifier',
@@ -36,7 +36,7 @@ function verifier($valeur, $type, $options=null){
 			'data' => $erreur
 		)
 	);
-	
+
 	return $erreur;
 }
 
