9c3ad70e039f68ee0171bede30b39a396a6c3e5b
scaron@dumx.com <scaron@dumx.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-03-04 15:30:46 +0000

    Plugin Masquer : Amelioration sur la liste des rubriques masquer.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@35791 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- masquer/masquer_pipelines.php
+++ masquer/masquer_pipelines.php
@@ -8,73 +8,88 @@ function masquer_pre_boucle($boucle) {
   if (test_espace_prive() || isset($boucle->modificateur['tout_voir'])) {
     return $boucle;
   }
-  
-  // Récupération de l'id_mot du mot dont le titre est "masquer"
-  if (!isset($id_mot)) {
-    $id_mot = sql_getfetsel("id_mot", "spip_mots", "titre='masquer'");
-  }
-  if (!isset($id_mot)) {
-    spip_log('[Plugin « Masquer »] Il n\'y a pas de mot clef « masquer »');
-    return $boucle;
-  }
-  
-  // Récupération de la liste des rubriques masquées et leurs branches
-  // Hors du cas de la boucle RUBRIQUE, car utilisé aussi par le cas de la boucle ARTICLES
-  if (!isset($liste_rubriques)) {
-    $liste_rubriques = array();
-    // On masque les rubriques qui ont le mot clef
-    $res = sql_select("id_rubrique", "spip_mots_rubriques", "id_mot=".$id_mot); 
-    while($row = sql_fetch($res)) {
-      $liste_rubriques = array_merge($liste_rubriques, liste_rubriques_branche($row['id_rubrique']));
-    }
-  }
-  
+
   // Cas de la boucle RUBRIQUES
   if ($boucle->type_requete == 'rubriques') {
-    if (count($liste_rubriques) == 0) {
-      return $boucle;
-    }
     $rub = $boucle->id_table . '.id_rubrique';
-    $boucle->where[] = "array('NOT IN', '$rub', '(".implode(',', $liste_rubriques).")')";
+    $boucle->where[] = masquer_rubriques_where($rub);
   }
 
   // Cas de la boucle ARTICLES
   if ($boucle->type_requete == 'articles') {
-    // On ne récupère la liste des articles masqués que si on a une boucle ARTICLES
-    if (!isset($liste_articles)) {
-      $liste_articles = array();
-      // On masque les articles qui ont le mot clef
-      $res = sql_select("id_article", "spip_mots_articles", "id_mot=".$id_mot); 
-      while($row = sql_fetch($res)) {
-        $liste_articles[] = $row['id_article'];
-      }
-      // On masque les articles qui sont dans des rubriques masquées
-      if (count($liste_rubriques) != 0) {
-        $res = sql_select("id_article", "spip_articles", "id_rubrique IN (".implode(',', $liste_rubriques).")"); 
-        while($row = sql_fetch($res)) {
-          $liste_articles[] = $row['id_article'];
-        }
-      }
-    }
-    if (count($liste_articles) == 0) {
-      return $boucle;
-    }
     $art = $boucle->id_table . '.id_article';
-    $boucle->where[] = "array('NOT IN', '$art', '(".implode(',', $liste_articles).")')";
-  }    
+    $boucle->where[] = masquer_articles_accessibles_where($art);
+  }
 
   return $boucle;
 }
 
-// Récupération de la liste des rubriques d'une branche
-function liste_rubriques_branche($id_rubrique) {
-  $liste = array($id_rubrique);
-  $res = sql_select("id_rubrique", "spip_rubriques", "id_parent=".$id_rubrique); 
-  while($row = sql_fetch($res)) {
-    $liste[] = $row['id_rubrique'];
-    $sous_rubriques = liste_rubriques_branche($row['id_rubrique']);
-    $liste = array_merge($liste, $sous_rubriques);
-  }
-  return $liste;
+/**
+ * Renvoyer le code de la condition where pour la liste des rubriques masquées
+ *
+ * @param string $primary
+ * @return string
+ */
+function masquer_rubriques_where($primary, $_publique=''){
+	# hack : on utilise zzz pour eviter que l'optimiseur ne confonde avec un morceau de la requete principale
+	return "array('NOT IN','$primary','('.sql_get_select('zzzr.id_rubrique','spip_mots_rubriques as zzzr, spip_mots as zzzm',' zzzr.id_mot=zzzm.id_mot AND zzzm.titre=\'masquer\'','','','','',\$connect).')')";
 }
+
+/**
+ * Renvoyer le code de la condition where pour la liste des rubriques accessibles
+ *
+ * @param string $primary
+ * @return string
+ */
+function masquer_rubriques_accessibles_where($primary,$not='NOT', $_publique=''){
+	return "sql_in('$primary','".implode(',', masquer_liste_rubriques($_publique))."', '$not')";
+}
+
+/**
+ * liste des rubriques masquer, directement ou par heritage.
+ *
+ * @param int/string $id_zone
+ * @return array
+ */
+function masquer_liste_rubriques($publique=true){
+	// cache static
+	static $liste_rubriques = array();
+	include_spip('inc/rubriques');
+	$liste_rubriques = masquer_liste_rub_direct();
+	if (!count($liste_rubriques))
+		return $liste_rubriques;
+	$liste_rubriques = calcul_branche_in(join(',',$liste_rubriques));
+	if (!strlen($liste_rubriques))
+		return array();
+	$liste_rubriques = explode(',',$liste_rubriques);
+	return $liste_rubriques;
+}
+
+/**
+ * liste des rubriques masquer directement.
+ *
+ * @return array
+ */
+function masquer_liste_rub_direct(){
+	$liste_rubriques=array();
+	// liste des rubriques directement masquer
+	$where = array();
+	include_spip('base/abstract_sql');
+	$liste_rubriques = sql_allfetsel('id_rubrique','spip_mots_rubriques AS mr INNER JOIN spip_mots AS m ON mr.id_mot=m.id_mot','m.titre=\'masquer\'');
+	$liste_rubriques = array_map('reset',$liste_rubriques);
+	$liste_rubriques = array_unique($liste_rubriques);
+	return $liste_rubriques;
+}
+
+/**
+ * Renvoyer la condition where pour la liste des articles masquer
+ *
+ * @param string $primary
+ * @return string
+ */
+function masquer_articles_accessibles_where($primary, $_publique=''){
+	# hack : on utilise zzz pour eviter que l'optimiseur ne confonde avec un morceau de la requete principale
+	return "array('NOT IN','$primary','('.sql_get_select('zzza.id_article','spip_articles as zzza',".masquer_rubriques_accessibles_where('zzza.id_rubrique','',$_publique).",'','','','',\$connect).')')";
+}
+
 ?>
