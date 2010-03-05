c3161ae7b5e7544cf3ddb19735fcc691ae5f1dcf
nicolas@hoizey.com <nicolas@hoizey.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-03-05 15:47:00 +0000

    le handler onChange n'etait plus pris en compte
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@35839 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- clevermail/2_0/js/clevermail_list_edit.js
+++ clevermail/2_0/js/clevermail_list_edit.js
@@ -1,26 +1,27 @@
-$(document).ready(function() {
-	function updateMode() {
-		mode = $('#lst_auto_mode > option[selected]').attr('value');
-		if (mode == 'none') {
-			$('#lst_auto_hour').parent('li').hide();
+function clevermailUpdateMode() {
+	mode = $('#lst_auto_mode > option[selected]').attr('value');
+	if (mode == 'none') {
+		$('#lst_auto_hour').parent('li').hide();
+		$('#lst_auto_week_days').parent('li').hide();
+		$('#lst_auto_month_day').parent('li').hide();
+	} else {
+		$('#lst_auto_hour').parent('li').show();
+		if (mode == 'day') {
 			$('#lst_auto_week_days').parent('li').hide();
 			$('#lst_auto_month_day').parent('li').hide();
 		} else {
-			$('#lst_auto_hour').parent('li').show();
-			if (mode == 'day') {
-				$('#lst_auto_week_days').parent('li').hide();
+			if (mode == 'week') {
+				$('#lst_auto_week_days').parent('li').show();
 				$('#lst_auto_month_day').parent('li').hide();
 			} else {
-				if (mode == 'week') {
-					$('#lst_auto_week_days').parent('li').show();
-					$('#lst_auto_month_day').parent('li').hide();
-				} else {
-					$('#lst_auto_week_days').parent('li').hide();
-					$('#lst_auto_month_day').parent('li').show();
-				}
+				$('#lst_auto_week_days').parent('li').hide();
+				$('#lst_auto_month_day').parent('li').show();
 			}
 		}
 	}
-	$('#lst_auto_mode').bind('change', updateMode);
-	updateMode();
+}
+
+$(document).ready(function() {
+	$('#lst_auto_mode').bind('change', clevermailUpdateMode);
+	clevermailUpdateMode();
 });
