557fe8e71dbdbb7d64acacc548209b3414cc0aab
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-03-07 14:41:45 +0000

    Une petite modif dans le nettoyage du texte des emails car on peut forcer le mail en iso-...
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@35901 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- facteur/inc/envoyer_mail.php
+++ facteur/inc/envoyer_mail.php
@@ -36,15 +36,26 @@ function nettoyer_titre_email($titre) {
 	return str_replace("\n", ' ', supprimer_tags(extraire_multi($titre)));
 }
 
-// http://doc.spip.org/@nettoyer_caracteres_mail
+/**
+ * Modification de la fonction SPIP nettoyer_caracteres_mail
+ * http://doc.spip.org/@nettoyer_caracteres_mail
+ *
+ * Ici les mails peuvent etre envoyes en ISO-8859-1,
+ * On tente un remplacement de caracteres problematiques avant de passer dans filtrer_entites
+ *
+ * @param string $t
+ */
 function nettoyer_caracteres_mail($t) {
 
-	$t = filtrer_entites($t);
-
-	if ($GLOBALS['meta']['charset'] <> 'utf-8') {
+	/**
+	 * Si on n'est pas en utf-8 et que l'on enverra au final en iso-8859-1
+	 * On remplace certains caracteres qui poseront probleme par la suite avant
+	 * filtrer_entites qui les remplace et casse leur remise en place par la suite
+	 */
+	if (($GLOBALS['meta']['charset'] <> 'utf-8') OR $GLOBALS['meta']['facteur_filtre_iso_8859']) {
 		$t = str_replace(
 			array("&#8217;","&#8220;","&#8221;"),
-			array("'",      '"',      '"'),
+			array("'",'"','"'),
 		$t);
 	}
 
@@ -52,6 +63,8 @@ function nettoyer_caracteres_mail($t) {
 		array("&mdash;", "&endash;"),
 		array("--","-" ),
 	$t);
+
+	$t = filtrer_entites($t);
 
 	return $t;
 }
