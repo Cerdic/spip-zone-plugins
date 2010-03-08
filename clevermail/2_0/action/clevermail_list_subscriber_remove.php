28fc90e23ddec6a95237a34af26e3e2f76132713
scaron@dumx.com <scaron@dumx.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-03-08 10:42:57 +0000

    Clevermail : Envoi d'un mail de desinscription au moderateur de la lettre lorsqu'un abonne est desinscrit depuis le BO
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@35934 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- clevermail/2_0/action/clevermail_list_subscriber_remove.php
+++ clevermail/2_0/action/clevermail_list_subscriber_remove.php
@@ -9,15 +9,25 @@ function action_clevermail_list_subscriber_remove_dist() {
 	  if (autoriser('supprimer','cm_list_subscriber',sql_quote($lsr_id))) {
 	    $abonnement = sql_fetsel("sub_id, lst_id", "spip_cm_lists_subscribers", "lsr_id=".sql_quote($lsr_id));
 	  	$abonne = sql_getfetsel("sub_email", "spip_cm_subscribers", "sub_id=".intval($abonnement['sub_id']));
-	  	$liste = sql_getfetsel("lst_name", "spip_cm_lists", "lst_id=".intval($abonnement['lst_id']));
+	  	$liste = sql_fetsel("lst_moderator_email, lst_name", "spip_cm_lists", "lst_id=".intval($abonnement['lst_id']));
 	    sql_delete("spip_cm_lists_subscribers", "lsr_id = ".sql_quote($lsr_id));
 	    sql_delete("spip_cm_posts_queued", "sub_id = ".intval($abonnement['sub_id']));
 	    if (sql_countsel("spip_cm_lists_subscribers", "sub_id=".intval($abonnement['sub_id'])) == 0) {
 	    	// No more subscription, subscriber address is removed
-			sql_delete("spip_cm_pending","sub_id = ".intval($abonnement['sub_id']));
-	        sql_updateq("spip_cm_subscribers", array('sub_email' => md5($abonne).'@example.com'), "sub_id = ".intval($abonnement['sub_id']));
-	      }
-      	spip_log('Suppression du l\'abonnement de « '.$abonne.' » à la liste « '.$liste.' » (id='.$abonnement['lst_id'].')', 'clevermail');
+				sql_delete("spip_cm_pending","sub_id = ".intval($abonnement['sub_id']));
+    		sql_updateq("spip_cm_subscribers", array('sub_email' => md5($abonne).'@example.com'), "sub_id = ".intval($abonnement['sub_id']));
+	    }
+	    $destinataire = $liste['lst_moderator_email'];
+	    $sujet = '['.addslashes($liste['lst_name']).'] Désinscription de '.addslashes($sub['sub_email']);
+	    $corps = _T('clevermail:mail_info_desinscription_corps', array('nom_site' => $GLOBALS['meta']['nom_site'], 'url_site' => $GLOBALS['meta']['adresse_site'], 'sub_email' => addslashes($sub['sub_email']), 'lst_name' => addslashes($list['lst_name'])));
+	    $expediteur = sql_getfetsel("set_value", "spip_cm_settings", "set_name='CM_MAIL_FROM'");
+	    $envoyer_mail = charger_fonction('envoyer_mail', 'inc');
+	    if ($envoyer_mail($destinataire, $sujet, $corps, $expediteur)) {
+		       spip_log('Envoie du mail OK','clevermail');
+		  } else {
+		       spip_log('Envoie du mail KO','clevermail');
+		  }
+      spip_log('Suppression du l\'abonnement de « '.$abonne.' » à la liste « '.$liste['lst_name'].' » (id='.$abonnement['lst_id'].')', 'clevermail');
 	  }
   }
 }
