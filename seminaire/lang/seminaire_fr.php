e1cc20478e5ca551a0eb948f018c3c12ffb1957a
amaury.adon@laposte.net <amaury.adon@laposte.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-03-08 11:09:28 +0000

    oups
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@35938 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- seminaire/lang/seminaire_fr.php
+++ seminaire/lang/seminaire_fr.php
@@ -1,27 +1,27 @@
 <?php
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-	//pour le formulaire de séminaires
+	//pour le formulaire de s&eacute;minaires
 	'evenement_titre'			 =>		'Titre de l&rsquo;expos&eacute;',
 	'name'						 => 	'Nom de l&rsquo;intervenant (Name)',
 	'origin'					 => 	'Institut d&rsquo;origine de l&rsquo;intervenant',
-	'abstract'					 => 	'Résumé',
+	'abstract'					 => 	'R&eacute;sum&eacute;',
 	'notes'						 =>		'Notes de dernières minutes',
 	'lieu'						 =>		'Lieu',
 	'precisions_name'			 =>		'le nom sera affich&eacute; dans le calendrier et dans le mini calendrier',
 	'precisions_origin'			 =>		'N&rsquo;apparaît que dans les d&eacute;tails de l&rsquo;&eacute;v&eacute;nement',
 	'precisions_abstract'		 =>		'pour mettre du code LATEX, ajoutez les balises <math> </math> en d&eacute;but et fin de texte (ex :  <math>La fraction $\frac{1}{2}$</math>)',
-	'precisions_notes'			 =>		'N&rsquo;apparaît que dans les détails de l&rsquo;&eacute;v&eacute;nement',
+	'precisions_notes'			 =>		'N&rsquo;apparaît que dans les d&eacute;tails de l&rsquo;&eacute;v&eacute;nement',
 	'apreciser'					 =>		'A pr&eacute;ciser',
 	'plus'						 =>		'En savoir plus',
 	'a_venir'					 =>		'Les prochains s&eacute;minaires',
 	'attention_type'			 =>		'Le choix du type d&rsquo;&eacute;v&eacute;nement d&eacute;termine le tri qui lui sera appliqu&eacute;.',
 	'oubli_titre'				 =>		'Ne pas oublier de mettre le titre de l&rsquo;&eacute;v&eacute;nement',
-	'exemple'					 =>		'Par exemple : CMI - Salle R164'
+	'exemple'					 =>		'Par exemple : CMI - Salle R164',
 	
-	//pour les groupes de mots clés
-	'mots_categorie_kitcnrs'	 => 	'Les mots clés à affecter aux articles pour que les événements soient encore mieux triés',
-	'mots_cles_techniques_kitcnrs'=>	'Les mots clés pour repérer plus finement les différents types d&rsquo;événements',
+	//pour les groupes de mots cl&eacute;s
+	'mots_categorie_kitcnrs'	 => 	'Les mots cl&eacute;s à affecter aux articles pour que les &eacute;v&eacute;nements soient encore mieux tri&eacute;s',
+	'mots_cles_techniques_kitcnrs'=>	'Les mots cl&eacute;s pour rep&eacute;rer plus finement les diff&eacute;rents types d&rsquo;&eacute;v&eacute;nements',
 	'type'						 =>		'Type d&rsquo;&eacute;v&eacute;nement',
 	'evenements_depuis_debut'	 =>		'Archives',	
 	'creer_evenement'			 =>		'Ajouter un &eacute;v&eacute;nement', 
