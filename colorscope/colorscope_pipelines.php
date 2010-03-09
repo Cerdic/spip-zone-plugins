6c4a4786f7f1431cf9ca1b98664b85478174f60f
cyril@cym.fr <cyril@cym.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-03-09 15:38:29 +0000

    Intégration des filtres couleur_extreme suite suggestion d'Arno*
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@35993 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- colorscope/colorscope_pipelines.php
+++ colorscope/colorscope_pipelines.php
@@ -3,13 +3,23 @@
  * Plugin Spipmine pour Spip 2.0
  * Licence GPL (c) 2009 - 2010
  * Auteur Cyril MARION - Ateliers CYM
+ * Merci a Arno* pour les filtres couleur :-)
  *
  */
+include_spip('inc/filtres_images_mini');
+include_spip('inc/filtres_images');
 
 function colorscope_post_typo($texte) {
 	$motif = '`(#[a-fA-F0-9]{6})`';
-	$texte = preg_replace($motif, '<span style="display:inline-block;text-align:center;width:90px;height:20px;background-color:$1;color:white">$1</span>', $texte);
+	$texte = preg_replace_callback($motif, 'bloc_colorscope', $texte);
 	return $texte;
 }
+
+function bloc_colorscope($couleur) {
+	$fond = '#'.couleur_inverser(couleur_extreme($couleur[0]));
+	$bloc = '<span style="display:inline-block;text-align:center;width:90px;height:20px;background-color:'.$couleur[0].';color:'.$fond.'">'.$couleur[0].'</span>';
+	return $bloc;
+}
+
 
 ?>
