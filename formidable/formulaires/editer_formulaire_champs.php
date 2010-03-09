11d5bfd9f47e720165e3a4953ca643c855c240eb
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-03-09 22:26:23 +0000

    Et quand même, on peut enfin enregistrer vraiment dans la base de données le formulaire modifié.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@36034 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- formidable/formulaires/editer_formulaire_champs.php
+++ formidable/formulaires/editer_formulaire_champs.php
@@ -13,7 +13,7 @@ function formulaires_editer_formulaire_champs_charger($id_formulaire){
 		$contenu = unserialize($formulaire['contenu']);
 		if (!is_array($contenu)) $contenu = array();
 		$contexte['_contenu'] = $contenu;
-		$contexte['id_formulaire'] = $id_formulaire;
+		$contexte['id'] = $id_formulaire;
 	}
 	
 	return $contexte;
@@ -27,6 +27,25 @@ function formulaires_editer_formulaire_champs_verifier($id_formulaire){
 
 function formulaires_editer_formulaire_champs_traiter($id_formulaire){
 	$retours = array();
+	$id_formulaire = intval($id_formulaire);
+	
+	// On récupère le formulaire dans la session
+	$contenu = session_get("constructeur_formulaire_formidable_$id_formulaire");
+	$contenu = serialize($contenu);
+	
+	// On l'envoie dans la table
+	$ok = sql_updateq(
+		'spip_formulaires',
+		array(
+			'contenu' => $contenu
+		),
+		'id_formulaire = '.$id_formulaire
+	);
+	
+	if ($ok){
+		$retours['message_ok'] = 'Le formulaire a bien été enregistré.';
+		$retours['editable'] = true;
+	}
 	
 	return $retours;
 }
