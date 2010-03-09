a6a5b84bbe2aee66812c746ce3c27233356b43ea
nicolas1.krebs2@netcourrier.com <nicolas1.krebs2@netcourrier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-03-09 22:48:14 +0000

    modification du plugin css_petits_ecrans_pipelines simplifiant css_petits_ecrans_pipelines.php en requerant SPIP 1.9.2
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@36036 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- css_petits_ecrans/css_petits_ecrans_pipelines.php
+++ css_petits_ecrans/css_petits_ecrans_pipelines.php
@@ -1,11 +1,7 @@
 <?php
 
 
-
 function css_petits_ecrans_insert_head($flux) {
-
-	$p=explode(basename(_DIR_PLUGINS)."/",str_replace('\\','/',realpath(dirname(__FILE__))));
-	define('_DIR_PLUGIN_CSS_PETITS_ECRANS',(_DIR_PLUGINS.end($p)));
 
 	$flux .= '<link rel="stylesheet" href="'._DIR_PLUGIN_CSS_PETITS_ECRANS.'css_petits_ecrans.css" type="text/css" media="all" />';
 	
