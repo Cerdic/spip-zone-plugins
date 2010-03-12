75e363cdad91b835fb7c4fcb4bc9c38d0427970d
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-03-12 17:20:51 +0000

    Le bon chemin pour le js (ça ne marchait pas en ayant mis le plugin dans les extensions/ de SPIP 2.1.0-rc)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@36150 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- protection_formulaires/protection_formulaires.php
+++ protection_formulaires/protection_formulaires.php
@@ -1,11 +1,7 @@
 <?php
 
-$p=explode(basename(_DIR_PLUGINS)."/",str_replace('\\','/',realpath(dirname(__FILE__))));
-define('_DIR_PLUGIN_PROTEGER_FORMULAIRES',(_DIR_PLUGINS.end($p)));
-
-
 function protection_formulaires_proteger($data) {
-	$ret = '<script src="'._DIR_PLUGIN_PROTEGER_FORMULAIRES.'/javascript/proteger_formulaires.js" type="text/javascript"></script>';
+	$ret = '<script src="'._DIR_PLUGIN_PROTECTION_FORMULAIRES.'/javascript/proteger_formulaires.js" type="text/javascript"></script>';
 
 
 	return $data.$ret;
