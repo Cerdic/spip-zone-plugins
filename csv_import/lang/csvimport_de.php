8b060ca83150d87ea8d8b4c1041657ba6a98bced
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-03-12 08:19:10 +0000

    langues (csvimport)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@36128 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ csv_import/lang/csvimport_de.php
@@ -0,0 +1,78 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'administrer_tables' => 'Tabellen verwalten',
+	'ajouter_donnees' => 'Daten hinzuf&uuml;gen',
+	'ajouter_table' => 'Tabelle hinzuf&uuml;gen',
+	'aucune_donnee' => 'Diese Datei enth&auml;lt keine Daten.',
+	'aucune_table_declaree' => 'Es wurde keine Tabelle f&uuml;r den CSV-Import deklariert',
+	'avertissement_ajout' => 'Die Daten der CSV-Datei werden, wie weiter unten gezeigt, der Tabelle "@table@" hinzugrf&uuml;gt.',
+	'avertissement_remplacement' => 'Dieser Vorgang wird alle in der Tabelle vorhandenen Daten l&ouml;schen. Die Daten aus der Datei werden wie weiter unten gezeigt an ihrer Stelle eingef&uuml;gt.',
+
+	// C
+	'caracteres_separation' => 'Trennzeichen',
+	'champs_csv' => 'Feld der CSV-Datei',
+	'champs_table' => 'Felder der Tabelle',
+	'correspondance_incomplete' => 'Relationen CSV-Tabelle unvollst&auml;ndig',
+	'correspondance_indefinie' => 'Relationen CSV-Tabelle nicht definiert',
+	'csvimport' => 'CSV-Import',
+
+	// D
+	'delimiteur_indefini' => 'Trennzeichen nicht definiert',
+	'description_table_introuvable' => 'Beschreibung der unauffindbaren Tabelle',
+
+	// E
+	'erreurs_ajout_base' => 'Beim hinzuf&uuml;gen der Daten in die Tabelle sind @nb@  Fehler aufgetreten.',
+	'export_classique' => 'Klassisches CSV (,)',
+	'export_excel' => 'CSV f&uuml;r Excel (;)',
+	'export_format' => 'Format der Datei:',
+	'export_table' => 'Export der Tabelle:  @table@',
+	'export_tabulation' => 'CSV mit Tabulatoren',
+	'exporter' => 'exportieren',
+
+	// F
+	'fichier_absent' => 'Datei nicht vorhanden',
+	'fichier_choisir' => 'CSV-Date f&uuml;r den Import',
+	'fichier_vide' => 'leere Datei',
+
+	// I
+	'import_csv' => 'CSV-Import: @table@',
+	'import_export_tables' => 'Import / Export in die Tabellen',
+
+	// L
+	'ligne_entete' => 'erste Zeile ist Kopfzeile',
+	'lignes_table' => 'Es gibt @nb_resultats@ Zeilen in der Tabelle "@table@."',
+	'lignes_totales' => '@nb@ Zeilen insgesamt',
+	'lignes_totales_csv' => 'Die CSV-Datei hat @nb@ Zeilen insgesamt.',
+
+	// N
+	'nb_enregistrements' => '@nb@ gespeichert',
+
+	// P
+	'pas_importer' => 'Nicht importieren',
+	'premieres_lignes' => '@nb@ erste Zeilen der Datei',
+	'probleme_chargement_fichier' => 'Ein Problem bei Laden der Datei ist aufgetreten',
+	'probleme_chargement_fichier_erreur' => 'Ein Problem bei Laden der Datei ist aufgetreten (Fehler @erreur@).',
+	'probleme_inextricable' => 'unl&ouml;sbares Problem',
+
+	// R
+	'remplacer_toute_table' => 'Ganze Tabelle ersetzen',
+
+	// T
+	'table_vide' => 'Die Tabelle "@table@" ist leer.',
+	'tables_declarees' => 'Deklarierte Tabellen',
+	'tables_presentes' => 'Tabellen in der Datenbank',
+	'tout_remplacer' => 'Alles ersetzen',
+
+	// Z
+	'z' => 'zzz'
+);
+
+?>
