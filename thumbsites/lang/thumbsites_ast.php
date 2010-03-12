aa50dca8e27afad1092246a51f82599ca9130d8c
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-03-12 08:19:11 +0000

    langues (thumbsites)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@36129 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ thumbsites/lang/thumbsites_ast.php
@@ -0,0 +1,49 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'bouton_effacer' => 'Desaniciar',
+	'bouton_valider' => 'Validar',
+	'bulle_bouton_effacer' => 'Desfacer lo editao y tornar a los valores d\'aniciu',
+	'bulle_bouton_valider' => 'Validar lo editao',
+
+	// C
+	'cfg_descriptif' => 'Esta p&aacute;xina permite configurar el complementu Thumbsites, y, n\'especial, escoyer el sirvidor de vi&ntilde;etes. Pa saber m&aacute;s, consulte la <a href="http://www.spip-contrib.net/?article2584">documentaci&oacute;n en contrib</a>',
+	'cfg_inf_girafa_description' => 'Les vi&ntilde;etes xeneraes tendr&aacute;n un m&aacute;simu de 160 x 120 pixels (esti valor puede camudase nel sitiu de Girafa)',
+	'cfg_inf_girafa_parametres' => 'Escriba los sos datos personales:',
+	'cfg_inf_girafa_presentation' => 'Pa utilizar esti serviciu tien d\'inscribise nel sitiu <a href="http://www.girafa.com">Girafa</a>. Esiste una versi&oacute;n de baldre si necesita menos de 2000 captures por d&iacute;a.',
+	'cfg_inf_miwin_parametres' => 'Veuillez saisir vos donn&eacute;es personnelles:', # NEW
+	'cfg_inf_miwin_presentation' => 'Pour utiliser ce service vous devez mettre un lien vers le site <a href="http://thumbs.miwim.fr">Miwin</a>. La pr&eacute;sence de ce lien est v&eacute;rifi&eacute; p&eacute;riodiquement par un script.', # NEW
+	'cfg_inf_rotothumb_parametres' => 'Veuillez saisir vos donn&eacute;es personnelles:', # NEW
+	'cfg_inf_rotothumb_presentation' => 'Pour utiliser ce service vous ne devez pas vous enregistrer mais vous devez mettre un lien vers le site <a href="http://www.robothumb.com">Robothumb</a>. La pr&eacute;sence de ce lien est v&eacute;rifi&eacute; p&eacute;riodiquement par leur soin.', # NEW
+	'cfg_inf_thumbshots_de_description' => 'Les vignettes g&eacute;n&eacute;r&eacute;es seront de 120 x 90 pixels.', # NEW
+	'cfg_inf_thumbshots_de_presentation' => '<a href="http://www.thumbshots.de">Thumbshots.de</a> propose un service &eacute;quivalent &agrave; Thumbshots.com. Bien que les services soient identiques, ce sont bien 2 sites distincts.', # NEW
+	'cfg_inf_thumbshots_description' => 'Les vignettes g&eacute;n&eacute;r&eacute;es seront de 120 x 90 pixels.', # NEW
+	'cfg_inf_thumbshots_presentation' => '<a href="http://www.thumbshots.com">Thumbshots.com</a> propose un service gratuit qui ne n&eacute;cessite pas d\'identification.', # NEW
+	'cfg_inf_websnapr_parametres' => 'Veuillez saisir vos donn&eacute;es personnelles:', # NEW
+	'cfg_inf_websnapr_presentation' => 'Pour utiliser ce service vous devez &ecirc;tre inscrit sur le site <a href="http://www.websnapr.com">Websnapr</a>. La version gratuite permet de traiter de 250.000 captures de site par mois, &agrave; un rythme de 80 captures par heure.', # NEW
+	'cfg_itm_serveur_girafa' => 'Girafa', # NEW
+	'cfg_itm_serveur_miwin' => 'Miwin', # NEW
+	'cfg_itm_serveur_robothumb' => 'Robothumb', # NEW
+	'cfg_itm_serveur_thumbshots' => 'Thumbshots.com', # NEW
+	'cfg_itm_serveur_thumbshots_de' => 'Thumbshots.de', # NEW
+	'cfg_itm_serveur_websnapr' => 'Websnapr', # NEW
+	'cfg_itm_taille_vignette' => '@taille@ pixels', # NEW
+	'cfg_lbl_choix_serveur' => 'Choisissez le serveur de vignettes', # NEW
+	'cfg_lbl_cle' => 'Votre cl&eacute;', # NEW
+	'cfg_lbl_identifiant' => 'Votre identifiant', # NEW
+	'cfg_lbl_signature' => 'Votre signature', # NEW
+	'cfg_lbl_taille_vignette' => 'Taille des vignettes', # NEW
+	'cfg_lgd_choix_serveur' => 'Serveur', # NEW
+
+	// T
+	'titre_thumbshot_site' => 'VIGNETTE DU SITE', # NE
+);
+
+?>
