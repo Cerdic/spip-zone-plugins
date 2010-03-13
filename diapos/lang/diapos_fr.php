cc97dd508876a976514ea33935666bf24ac94ca4
lspeciale@gmail.com <lspeciale@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-03-13 18:09:56 +0000

    Oups Raté
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@36200 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ diapos/lang/diapos_fr.php
@@ -0,0 +1,17 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined("_ECRIRE_INC_VERSION")) return;
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+    'cfg'                       =>'Configuration de Diapos',
+    'cfg_largeur_imagespa'    =>'Largeur',
+    'cfg_hauteur_imagespa'             =>'Hauteur',
+    'cfg_descriptifpa'            =>'<b>Diapos paysage</b><br />Choissisez ici la taille de vos images qui seront affich&eacute;es par le module paysage. Vous pouvez la renseigner soit en pixels soit en ems (px ou em, sans espace entre le chiffre et la définition).',
+     'cfg_marges_imagespati'    =>'Marges portrait',
+    'cfg_marges_imagespa'    =>'La premi&egrave;re chiffre correspond &agrave; la marge sup&eacute;rieure, puis dans le sens des aiguiles d\'une montre, marge droite, inf&eacute;rieure et gauche. Vous pouvez la renseigner soit en pixels soit en ems.',
+    'cfg_largeur_imagespo'    =>'Largeur',
+    'cfg_hauteur_imagespo'             =>'Hauteur',
+    'cfg_descriptifpo'            =>'<b>Diapos portrait</b><br />Choissisez ici la taille de vos images qui seront affich&eacute;es par le module portrait (en pixels ou ems).',
+     'cfg_marges_imagespoti'    =>'Marges portrait',
+    'cfg_valider'        =>'Valider',
+);  
+?>
