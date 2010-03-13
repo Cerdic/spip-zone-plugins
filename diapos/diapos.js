74d39e9d87c6afb6beb90efb751940645f57cf7a
lspeciale@gmail.com <lspeciale@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-03-13 18:08:15 +0000

    Ajouts : Config CFG. On peut choisir la taille de deux modules : portrait ou paysage. On peut renseigner les marges pour l'insertion dans le texte.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@36199 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- diapos/diapos.js
+++ diapos/diapos.js
@@ -1,32 +1,32 @@
 $(document).ready(function() {
-
 	//Met la premiere image en active
 	$(".choix_image").show();
 	$(".choix_image a:first").addClass("active");
-		
 	//Cherche la largeur et la quantite des images 
-	var imageWidth = $(".fenetre").width();
-	var imageSum = $(".cadre_image img").size();
-	var imageReelWidth = imageWidth * imageSum;
-	
+	var imageWidthPa = $(".fenetrepa").width();
+	var imageWidthPo = $(".fenetrepo").width();
+	var imageSumPa = $(".cadre_imagepa img").size();
+	var imageSumPo = $(".cadre_imagepo img").size();
+	var imageReelWidthPa = imageWidthPa * imageSumPa;
+	var imageReelWidthPo = imageWidthPo * imageSumPo;
 	//Donne la largeur a cadre_image.
-	$(".cadre_image").css({'width' : imageReelWidth});
-	
+	$(".cadre_imagepa").css({'width' : imageReelWidthPa});
+	$(".cadre_imagepo").css({'width' : imageReelWidthPo});
 	//choix_image + diapos function
 	rotate = function(){	
 		var triggerID = $active.attr("rel") - 1; //Quantite des images
-		var cadre_imagePosition = triggerID * imageWidth; //Determine la largeur a cadre_image
-
+		var cadre_imagePositionPa = triggerID * imageWidthPa; //Determine la largeur a cadre_image
+		var cadre_imagePositionPo = triggerID * imageWidthPo; //Determine la largeur a cadre_image
 		$(".choix_image a").removeClass('active'); //Nettoie
 		$active.addClass('active'); //Ajoute la classe active
-		
 		//Animation
-		$(".cadre_image").animate({ 
-			left: -cadre_imagePosition
+		$(".cadre_imagepa").animate({ 
+			left: -cadre_imagePositionPa
 		}, 500 );
-		
+		$(".cadre_imagepo").animate({ 
+			left: -cadre_imagePositionPo
+		}, 500 );
 	}; 
-	
 	//Rotation et temps
 	rotateSwitch = function(){		
 		play = setInterval(function(){ //Temps de vision
@@ -37,16 +37,18 @@ $(document).ready(function() {
 			rotate();
 		}, 7000); //On peut ajuster le tems en milisecondes
 	};
-	
 	rotateSwitch(); //Demarre au chargement de la page
-	
 	//Survoler une image arrete le diapos
-	$(".cadre_image a").hover(function() {
+	$(".cadre_imagepa a").hover(function() {
 		clearInterval(play);
 	}, function() {
 		rotateSwitch();
 	});	
-	
+	$(".cadre_imagepo a").hover(function() {
+		clearInterval(play);
+	}, function() {
+		rotateSwitch();
+	});	
 	//Choisir l'image
 	$(".choix_image a").click(function() {	
 		$active = $(this); //Active l'imageimage
@@ -55,5 +57,4 @@ $(document).ready(function() {
 		rotateSwitch(); 
 		return false;
 	});	
-	
 });
--- diapos/diapos_pipelines.php
+++ diapos/diapos_pipelines.php
@@ -1,12 +1,10 @@
 <?php
 function diapos_insert_head($flux){
 	$flux .= '<script type="text/javascript" src="'.find_in_path('diapos.js').'"></script>';
-	$flux .= '<link rel="stylesheet" href="'.find_in_path('diapos.css').'" type="text/css" media="all" />';
 	return $flux;
 }
 function diapos_header_prive($flux){
 	$flux .= '<script type="text/javascript" src="'.find_in_path('diapos.js').'"></script>';
-	$flux .= '<link rel="stylesheet" href="'.find_in_path('diapos.css').'" type="text/css" media="all" />';
 	return $flux;
 }
 ?>
