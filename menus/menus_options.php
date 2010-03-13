4e3ca755e6ea70e4858dda5217ed6250155bb537
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-03-13 22:17:03 +0000

    en attendant mieux dans la 2.1, ça évite de tout faire planter
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@36212 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- menus/menus_options.php
+++ menus/menus_options.php
@@ -4,6 +4,6 @@
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 // On ajoute le nouveau type d'objet
-define('_URL_OBJETS', _URL_OBJETS.'|menu');
+define('_URL_OBJETS', 'article|breve|rubrique|mot|auteur|site|syndic|menu');
 
 ?>
