c43f108a07e2ee5871c808872e41a2985ccf2a9c
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-03-15 22:29:22 +0000

    Simplifions : plus de 2e paramètre, et faisons un vrai CVT et pas comme le seul formulaire de la dist qui n'est pas un CVT (recherche) (cerdic)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@36286 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- critere_mots/formulaires/recherche_par_mots.php
+++ critere_mots/formulaires/recherche_par_mots.php
@@ -1,20 +1,11 @@
 <?php
 
-function formulaires_recherche_par_mots_charger_dist($filtre_groupes = NULL, $lien = NULL){
-	if ($GLOBALS['spip_lang'] != $GLOBALS['meta']['langue_site'])
-		$lang = $GLOBALS['spip_lang'];
-	else
-		$lang='';
-		
-	$mots = _request('mots');
-	
+function formulaires_recherche_par_mots_charger_dist($filtre_groupes = NULL){
 	return 
 		array(
-			'action' => ($lien ? $lien : generer_url_public(self())), # action specifique, ne passe pas par Verifier, ni Traiter
 			'filtre_groupes' => $filtre_groupes,
-			'lang' => $lang,
 			'id_groupe'=>_request('id_groupe'),
-			'mots'=>$mots
+			'mots'=>_request('mots')
 		);
 }
 
