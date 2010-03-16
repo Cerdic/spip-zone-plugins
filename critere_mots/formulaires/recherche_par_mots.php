7d2f020543dd3fc90a85341e250217a6a96b75eb
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-03-16 11:18:14 +0000

    Peut-être pas très orthodoxe, mais ça permet :  * de recharger en ajax la liste des mots du groupe sélectionné  * d'être redirigé automatiquement sur la page de résultat.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@36304 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- critere_mots/formulaires/recherche_par_mots.php
+++ critere_mots/formulaires/recherche_par_mots.php
@@ -9,6 +9,21 @@ function formulaires_recherche_par_mots_charger_dist($filtre_groupes = NULL){
 		);
 }
 
+function formulaires_recherche_par_mots_verifier_dist($filtre_groupes = NULL){
+	$erreurs = array();
+	if (_request('le_groupe') && _request('choixmot'))
+		$erreurs['message_erreur'] = 'Choisissez un mot';
+	return $erreurs;
+}
+
+function formulaires_recherche_par_mots_traiter_dist($filtre_groupes = NULL){
+	return array('redirect' => parametre_url(
+									self(),'mots',_request('mots')
+									)
+					);
+}
+
+
 function critere_mots_enleve_mot_de_liste($listemots, $id_mot) {
 	unset($listemots[array_search($id_mot,$listemots)]);
 	return $listemots;
