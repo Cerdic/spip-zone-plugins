31f73e24971a611e70cb7768a9256522d9b69d53
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-03-16 14:33:27 +0000

    Bien nettoyer les URL pour n'avoir qu'une fois chaque identifiant de mots.
    Et gérer aussi le cas où l'on change de groupe sans sélectionner de mot.
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@36311 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- critere_mots/formulaires/recherche_par_mots.php
+++ critere_mots/formulaires/recherche_par_mots.php
@@ -1,6 +1,6 @@
 <?php
 
-function formulaires_recherche_par_mots_charger_dist($filtre_groupes = NULL, $url_resultat = NULL){
+function formulaires_recherche_par_mots_charger_dist($filtre_groupes = null, $url_resultat = null){
 	return 
 		array(
 			'id_groupe' => $filtre_groupes,
@@ -9,25 +9,32 @@ function formulaires_recherche_par_mots_charger_dist($filtre_groupes = NULL, $ur
 		);
 }
 
-function formulaires_recherche_par_mots_verifier_dist($filtre_groupes = NULL, $url_resultat = NULL){
+function formulaires_recherche_par_mots_verifier_dist($filtre_groupes = null, $url_resultat = null){
 	$erreurs = array();
 	if (_request('le_groupe') && _request('choixmot'))
 		$erreurs['message_erreur'] = 'Choisissez un mot';
+	$mots = _request('mots');
+	if (is_array($mots) && array_search('',$mots))
+		$erreurs['message_erreur'] = 'Changement de groupe';
 	return $erreurs;
 }
 
-function formulaires_recherche_par_mots_traiter_dist($filtre_groupes = NULL, $url_resultat = NULL){
+function formulaires_recherche_par_mots_traiter_dist($filtre_groupes = null, $url_resultat = null){
 	if (!$url_resultat) 
 		$url_resultat = self();
-	return array('redirect' => parametre_url(
-									$url_resultat,'mots',_request('mots')
-									)
-					);
+	// Nettoyer l'URL des mots[] qu'elle contiendrait deja
+	$url_resultat = parametre_url($url_resultat,'mots','');
+	// Mettre dans l'URL 
+	$url_resultat = parametre_url(
+						$url_resultat,'mots',array_unique(_request('mots'))
+						);
+	return array('redirect' => $url_resultat);
 }
 
 
 function critere_mots_enleve_mot_de_liste($listemots, $id_mot) {
-	while (array_search($id_mot,$listemots)!==FALSE)
+	$listemots = array_unique($listemots);
+	while (array_search($id_mot,$listemots)!==false)
 		unset($listemots[array_search($id_mot,$listemots)]);
 	return $listemots;
 }
