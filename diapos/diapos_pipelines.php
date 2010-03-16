b504c9a09beb52fffb2f69c2ccfec8e725f94ef5
lspeciale@gmail.com <lspeciale@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-03-16 16:16:28 +0000

    On ne dépend plus de zspip (honte à moi) grâce à generer_url_public (Bruno !!!!)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@36312 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- diapos/diapos_pipelines.php
+++ diapos/diapos_pipelines.php
@@ -1,6 +1,7 @@
 <?php
 function diapos_insert_head($flux){
 	$flux .= '<script type="text/javascript" src="'.find_in_path('diapos.js').'"></script>';
+	$flux .= '<link rel="stylesheet" href="'.generer_url_public("diapos.css").'" type="text/css" media="projection, screen, tv" />'; 
 	return $flux;
 }
 function diapos_header_prive($flux){
