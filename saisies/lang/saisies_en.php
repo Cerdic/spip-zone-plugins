16ea87695f66af7c9dd1ec7d0028f06c272fd14f
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-03-18 09:04:40 +0000

    langues (saisies)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@36380 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- saisies/lang/saisies_en.php
+++ saisies/lang/saisies_en.php
@@ -1,14 +1,17 @@
 <?php
-
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+
 	// O
 	'option_attention_explication' => 'A message more important than the explanation.',
 	'option_attention_label' => 'Warning',
-	'option_choix_destinataires_label' => 'Possible recipients',
 	'option_choix_destinataires_explication' => 'One or several authors among which the user can make his choice. If nothing selected, it will be the author who installed the site to be chosen.',
+	'option_choix_destinataires_label' => 'Possible recipients',
 	'option_class_label' => 'Additional CSS Classes',
 	'option_cols_explication' => 'Field width in characters. This option is not always applied/used because the CSS styles of your site can cancel it.',
 	'option_cols_label' => 'Width',
@@ -23,7 +26,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'option_groupe_description' => 'Description',
 	'option_groupe_utilisation' => 'Usage',
 	'option_groupe_validation' => 'Validation',
-	'option_info_obligatoire_explication' => 'You can modify the default indication of obligation: <i>'._T('info_obligatoire_02').'</i>.',
+	'option_info_obligatoire_explication' => 'You can modify the default indication of obligation: <i>[Obligatoire]</i>.',
 	'option_info_obligatoire_label' => 'Indication of obligation',
 	'option_label_explication' => 'The title that will be displayed.',
 	'option_label_label' => 'Label',
@@ -32,32 +35,34 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'option_nom_explication' => 'A digital name that identifies the field. It should contain only lowercase alphanumeric characters or the character "_".',
 	'option_nom_label' => 'Field name',
 	'option_obligatoire_label' => 'Required field',
+	'option_pliable_label' => 'Le groupe de champs pourra &ecirc;tre repli&eacute;.', # NEW
+	'option_plie_label' => 'Si le groupe de champs est pliable, il sera d&eacute;j&agrave; pli&eacute; &agrave; l\'affichage du formulaire.', # NEW
 	'option_readonly_explication' => 'The field can be viewed, selected, but not modified.',
 	'option_readonly_label' => 'Read only',
 	'option_rows_explication' => 'Field height in lines. This option is not always applied/used because the CSS styles of your site can cancel it.',
 	'option_rows_label' => 'Lines number',
 	'option_size_explication' => 'Field width in characters. This option is not always applied/used because the CSS styles of your site can cancel it.',
 	'option_size_label' => 'Field size',
-	'option_type_explication' => 'Mode "password", the contents of the field will be hidden.',
-	'option_type_choix_un' => 'Allow the user to choose <strong>one</ strong> recipient.',
 	'option_type_choix_plusieurs' => 'Allow the user to choose <strong>many</ strong> recipients.',
 	'option_type_choix_tous' => 'Make <strong>all</ strong> these authors as recipients. The user will not have choice.',
+	'option_type_choix_un' => 'Allow the user to choose <strong>one</ strong> recipient.',
+	'option_type_explication' => 'Mode "password", the contents of the field will be hidden.',
 	'option_type_label' => 'Field type',
-	'option_type_text' => 'Normal',
 	'option_type_password' => 'Password',
-	
+	'option_type_text' => 'Normal',
+
 	// S
-	'saisie_base_titre' => 'General settings',
 	'saisie_base_plus_titre' => 'Additional settings',
-	'saisie_destinataires_titre' => 'Recipients',
+	'saisie_base_titre' => 'General settings',
 	'saisie_destinataires_explication' => 'Allows you to select one or more recipients among the pre-selected authors .',
-	'saisie_fieldset_titre' => 'Fieldset',
+	'saisie_destinataires_titre' => 'Recipients',
 	'saisie_fieldset_explication' => 'A frame which may include several fields.',
+	'saisie_fieldset_titre' => 'Fieldset',
 	'saisie_input_explication' => 'A simple line of text that can be visible or hidden (password).',
 	'saisie_input_titre' => 'Textfield',
 	'saisie_textarea_explication' => 'A multilines text field.',
 	'saisie_textarea_titre' => 'Textarea',
-	
+
 	// T
 	'tous_visiteurs' => 'All visitors (even if not registered)',
 
--- saisies/lang/saisies_fr.php
+++ saisies/lang/saisies_fr.php
@@ -1,14 +1,15 @@
 <?php
-
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/saisies/lang/
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+
 	// O
 	'option_attention_explication' => 'Un message plus important que l\'explication.',
 	'option_attention_label' => 'Avertissement',
-	'option_choix_destinataires_label' => 'Destinataires possibles',
 	'option_choix_destinataires_explication' => 'Un ou plusieurs auteurs parmis lesquels l\'utilisateur pourra faire son choix. Si rien n\'est s&eacute;lectionn&eacute;, c\'est l\'auteur qui a install&eacute; le site qui sera choisi.',
+	'option_choix_destinataires_label' => 'Destinataires possibles',
 	'option_class_label' => 'Classes CSS suppl&eacute;mentaires',
 	'option_cols_explication' => 'Largeur du bloc en nombre de caractères. Cette option n\'est pas toujours appliqu&eacute;e car les styles CSS de votre site peuvent l\'annuler.',
 	'option_cols_label' => 'Largeur',
@@ -23,7 +24,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'option_groupe_description' => 'Description',
 	'option_groupe_utilisation' => 'Utilisation',
 	'option_groupe_validation' => 'Validation',
-	'option_info_obligatoire_explication' => 'Vous pouvez modifier l\'indication d\'obligation par d&eacute;faut : <i>'._T('info_obligatoire_02').'</i>.',
+	'option_info_obligatoire_explication' => 'Vous pouvez modifier l\'indication d\'obligation par d&eacute;faut : <i>[Obligatoire]</i>.',
 	'option_info_obligatoire_label' => 'Indication d\'obligation',
 	'option_label_explication' => 'Le titre qui sera affich&eacute;.',
 	'option_label_label' => 'Label',
@@ -34,32 +35,32 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'option_obligatoire_label' => 'Champ obligatoire',
 	'option_pliable_label' => 'Le groupe de champs pourra &ecirc;tre repli&eacute;.',
 	'option_plie_label' => 'Si le groupe de champs est pliable, il sera d&eacute;j&agrave; pli&eacute; &agrave; l\'affichage du formulaire.',
-	'option_rows_explication' => 'Hauteur du bloc en nombre de ligne. Cette option n\'est pas toujours appliqu&eacute;e car les styles CSS de votre site peuvent l\'annuler.',
-	'option_rows_label' => 'Nombre de lignes',
 	'option_readonly_explication' => 'Le champ peut &ecirc;tre lu, s&eacute;lectionn&eacute;, mais pas modifi&eacute;.',
 	'option_readonly_label' => 'Lecture seule',
+	'option_rows_explication' => 'Hauteur du bloc en nombre de ligne. Cette option n\'est pas toujours appliqu&eacute;e car les styles CSS de votre site peuvent l\'annuler.',
+	'option_rows_label' => 'Nombre de lignes',
 	'option_size_explication' => 'Largeur du champ en nombre de caract&egrave;res. Cette option n\'est pas toujours appliqu&eacute;e car les styles CSS de votre site peuvent l\'annuler.',
 	'option_size_label' => 'Taille du champ',
-	'option_type_explication' => 'En mode "mot de passe", le contenu du champ sera masqu&eacute;.',
-	'option_type_choix_un' => 'Permettre à l\'utilisateur de choisir <strong>un seul</strong> destinataire.',
 	'option_type_choix_plusieurs' => 'Permettre à l\'utilisateur de choisir <strong>plusieurs</strong> destinataires.',
 	'option_type_choix_tous' => 'Mettre <strong>tous</strong> ces auteurs en destinataires. L\'utilisateur n\'aura aucun choix.',
+	'option_type_choix_un' => 'Permettre à l\'utilisateur de choisir <strong>un seul</strong> destinataire.',
+	'option_type_explication' => 'En mode "mot de passe", le contenu du champ sera masqu&eacute;.',
 	'option_type_label' => 'Type du champ',
-	'option_type_text' => 'Normal',
 	'option_type_password' => 'Mot de passe',
-	
+	'option_type_text' => 'Normal',
+
 	// S
-	'saisie_base_titre' => 'Param&egrave;tres g&eacute;n&eacute;raux',
 	'saisie_base_plus_titre' => 'Param&egrave;tres suppl&eacute;mentaires',
-	'saisie_destinataires_titre' => 'Destinataires',
+	'saisie_base_titre' => 'Param&egrave;tres g&eacute;n&eacute;raux',
 	'saisie_destinataires_explication' => 'Permet de choisir un ou plusieurs destinataires parmis des auteurs pr&eacute;-s&eacute;lectionn&eacute;e.',
-	'saisie_fieldset_titre' => 'Groupe de champs',
+	'saisie_destinataires_titre' => 'Destinataires',
 	'saisie_fieldset_explication' => 'Un cadre qui pourra englober plusieurs champs.',
+	'saisie_fieldset_titre' => 'Groupe de champs',
 	'saisie_input_explication' => 'Une simple ligne de texte, pouvant &ecirc;tre visible ou masqu&eacute;e (mot de passe).',
 	'saisie_input_titre' => 'Ligne de texte',
 	'saisie_textarea_explication' => 'Un champ de texte sur plusieurs lignes.',
 	'saisie_textarea_titre' => 'Bloc de texte',
-	
+
 	// T
 	'tous_visiteurs' => 'Tous les visiteurs (m&ecirc;me non enregistr&eacute;s)',
 
