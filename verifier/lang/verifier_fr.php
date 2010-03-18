93023c160e66e2b380668b994ce33429fd9a72e7
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-03-18 09:04:43 +0000

    langues (verifier)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@36381 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- verifier/lang/verifier_fr.php
+++ verifier/lang/verifier_fr.php
@@ -1,51 +1,55 @@
 <?php
-
-// Sécurité
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/verifier/lang/
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-'erreur_date' => "Le format de la date n'est pas accept&eacute;.",
-'erreur_email' => 'L\'adresse de courriel n\'a pas un format valide.',
-'erreur_email_nondispo'=>"L'adresse de courriel est d&eacute;j&agrave; enregistr&eacute;.",
-'erreur_entier' => 'La valeur doit &ecirc;tre un entier.',
-'erreur_entier_entre' => 'La valeur doit &ecirc;tre comprise entre @min@ et @max@.',
-'erreur_entier_max' => 'La valeur doit &ecirc;tre inf&eacute;rieure &agrave; @max@.',
-'erreur_entier_min' => 'La valeur doit &ecirc;tre supp&eacute;rieure &agrave; @min@.',
-'erreur_numerique' => 'Le format du nombre n\'est pas valide.',
-'erreur_regex' => 'Le format de la cha&icirc;ne n\'est pas valide.',
-'erreur_siren' => 'Le num&eacute;ro de SIREN n\'est pas valide.',
-'erreur_siret' => 'Le num&eacute;ro de SIRET n\'est pas valide.',
-'erreur_taille_entre' => 'La valeur doit comprendre entre @min@ et @max@ caract&egrave;res.',
-'erreur_taille_egal' => 'La valeur doit comprendre exactement @egal@ caract&egrave;res.',
-'erreur_taille_max' => 'La valeur doit comprendre au maximum @max@ caract&egrave;res.',
-'erreur_taille_min' => 'La valeur doit comprendre au minimum @min@ caract&egrave;res.',
-'erreur_telephone' => "Le numéro n'est pas valide.",
-'erreur_url' => "L'adresse n\'est pas valide.",
 
-'option_entier_max_label' => 'Valeur maximum',
-'option_entier_min_label' => 'Valeur minimum',
-'option_regex_modele_label' => 'La valeur doit correspondre au masque suivant',
-'option_taille_max_label' => 'Taille maximum',
-'option_taille_min_label' => 'Taille minimum',
+	// E
+	'erreur_date' => 'Le format de la date n\'est pas accept&eacute;.',
+	'erreur_email' => 'L\'adresse de courriel n\'a pas un format valide.',
+	'erreur_email_nondispo' => 'L\'adresse de courriel est d&eacute;j&agrave; enregistr&eacute;.',
+	'erreur_entier' => 'La valeur doit &ecirc;tre un entier.',
+	'erreur_entier_entre' => 'La valeur doit &ecirc;tre comprise entre @min@ et @max@.',
+	'erreur_entier_max' => 'La valeur doit &ecirc;tre inf&eacute;rieure &agrave; @max@.',
+	'erreur_entier_min' => 'La valeur doit &ecirc;tre supp&eacute;rieure &agrave; @min@.',
+	'erreur_numerique' => 'Le format du nombre n\'est pas valide.',
+	'erreur_regex' => 'Le format de la cha&icirc;ne n\'est pas valide.',
+	'erreur_siren' => 'Le num&eacute;ro de SIREN n\'est pas valide.',
+	'erreur_siret' => 'Le num&eacute;ro de SIRET n\'est pas valide.',
+	'erreur_taille_egal' => 'La valeur doit comprendre exactement @egal@ caract&egrave;res.',
+	'erreur_taille_entre' => 'La valeur doit comprendre entre @min@ et @max@ caract&egrave;res.',
+	'erreur_taille_max' => 'La valeur doit comprendre au maximum @max@ caract&egrave;res.',
+	'erreur_taille_min' => 'La valeur doit comprendre au minimum @min@ caract&egrave;res.',
+	'erreur_telephone' => 'Le numéro n\'est pas valide.',
+	'erreur_url' => 'L\'adresse n\\\'est pas valide.',
 
-'type_date' => 'Date',
-'type_date_description' => 'V&eacute;rifie que la valeur est une date au format JJ/MM/AAAA. Le s&eacute;parateur est libre (&quot;.&quot;, &quot;/&quot;, etc).',
-'type_email' => 'Adresse de courriel',
-'type_email_description' => 'V&eacute;rifie que l\'adresse de courriel a un format correct.',
-'type_email_disponible' => 'Disponibilit&eacute; d\'une adresse de courriel',
-'type_email_disponible_description' => 'V&eacute;rifie que l\'adresse de courriel n\'est pas d&eacute;j&agrave; utilis&eacute; par un autre utilisateur du syst&egrave;me.',
-'type_entier' => 'Entier',
-'type_entier_description' => 'V&eacute;rifie que la valeur est un entier, avec la possibilit&eacute; de restreindre entre deux valeurs.',
-'type_regex' => 'Expression r&eacute;guli&egrave;re',
-'type_regex_description' => 'V&eacute;rifie que la valeur correspond au masque demand&eacute;. Pour l\'utilisation des masques, reportez-vous à <a href="http://fr2.php.net/manual/fr/reference.pcre.pattern.syntax.php">l\'aide en ligne de PHP</a>.',
-'type_siren' => 'SIREN',
-'type_siren_description' => 'V&eacute;rifie que la valeur est un num&eacute;ro valide du <a href="http://fr.wikipedia.org/wiki/SIREN">Syst&egrave;me d&rsquo;Identification du R&eacute;pertoire des ENtreprises</a> fran&ccedil;ais.',
-'type_siret' => 'SIRET',
-'type_siret_description' => 'V&eacute;rifie que la valeur est un num&eacute;ro valide du <a href="http://fr.wikipedia.org/wiki/SIRET">Syst&egrave;me d&rsquo;Identification du R&eacute;pertoire des ETablissements</a> fran&ccedil;ais.',
-'type_taille' => 'Taille',
-'type_taille_description' => 'V&eacute;rifie que la taille de la valeur correspond au minimum et/ou au maximum demand&eacute;.',
-'type_telephone_fr' => 'Num&eacute;ro de t&eacute;l&eacute;phone fran&ccedil;ais',
-'type_telephone_fr_description' => 'V&eacute;rifie que le num&eacute;ro de t&eacute;l&eacute;phone correspond au sh&eacute;ma fran&ccedil;ais.'
+	// O
+	'option_entier_max_label' => 'Valeur maximum',
+	'option_entier_min_label' => 'Valeur minimum',
+	'option_regex_modele_label' => 'La valeur doit correspondre au masque suivant',
+	'option_taille_max_label' => 'Taille maximum',
+	'option_taille_min_label' => 'Taille minimum',
+
+	// T
+	'type_date' => 'Date',
+	'type_date_description' => 'V&eacute;rifie que la valeur est une date au format JJ/MM/AAAA. Le s&eacute;parateur est libre (&quot;.&quot;, &quot;/&quot;, etc).',
+	'type_email' => 'Adresse de courriel',
+	'type_email_description' => 'V&eacute;rifie que l\'adresse de courriel a un format correct.',
+	'type_email_disponible' => 'Disponibilit&eacute; d\'une adresse de courriel',
+	'type_email_disponible_description' => 'V&eacute;rifie que l\'adresse de courriel n\'est pas d&eacute;j&agrave; utilis&eacute; par un autre utilisateur du syst&egrave;me.',
+	'type_entier' => 'Entier',
+	'type_entier_description' => 'V&eacute;rifie que la valeur est un entier, avec la possibilit&eacute; de restreindre entre deux valeurs.',
+	'type_regex' => 'Expression r&eacute;guli&egrave;re',
+	'type_regex_description' => 'V&eacute;rifie que la valeur correspond au masque demand&eacute;. Pour l\'utilisation des masques, reportez-vous à <a href="http://fr2.php.net/manual/fr/reference.pcre.pattern.syntax.php">l\'aide en ligne de PHP</a>.',
+	'type_siren' => 'SIREN',
+	'type_siren_description' => 'V&eacute;rifie que la valeur est un num&eacute;ro valide du <a href="http://fr.wikipedia.org/wiki/SIREN">Syst&egrave;me d&rsquo;Identification du R&eacute;pertoire des ENtreprises</a> fran&ccedil;ais.',
+	'type_siret' => 'SIRET',
+	'type_siret_description' => 'V&eacute;rifie que la valeur est un num&eacute;ro valide du <a href="http://fr.wikipedia.org/wiki/SIRET">Syst&egrave;me d&rsquo;Identification du R&eacute;pertoire des ETablissements</a> fran&ccedil;ais.',
+	'type_taille' => 'Taille',
+	'type_taille_description' => 'V&eacute;rifie que la taille de la valeur correspond au minimum et/ou au maximum demand&eacute;.',
+	'type_telephone_fr' => 'Num&eacute;ro de t&eacute;l&eacute;phone fran&ccedil;ais',
+	'type_telephone_fr_description' => 'V&eacute;rifie que le num&eacute;ro de t&eacute;l&eacute;phone correspond au sh&eacute;ma fran&ccedil;ais.'
 );
 
 ?>
