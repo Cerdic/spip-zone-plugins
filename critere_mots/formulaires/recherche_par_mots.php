e0a578b8ad8cf557135e5b82db3b0d2fba6ecf0c
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-03-18 14:18:31 +0000

    Le formulaire n'est jamais traité en ajax car il s'achève par une redirection vers la bonne page qui doit être réaffichée dans son ensemble.
    Astuce trouvée ici : http://trac.rezo.net/trac/spip/changeset/15510
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@36391 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- critere_mots/formulaires/recherche_par_mots.php
+++ critere_mots/formulaires/recherche_par_mots.php
@@ -20,7 +20,11 @@ function formulaires_recherche_par_mots_verifier_dist($filtre_groupes = null, $u
 }
 
 function formulaires_recherche_par_mots_traiter_dist($filtre_groupes = null, $url_resultat = null){
-	if (!$url_resultat) 
+    // le formulaire n'est jamais traite en ajax car il s'acheve par une redirection vers 
+ 	// la bonne page qui doit etre reaffichee dans son ensemble 
+ 	refuser_traiter_formulaire_ajax();
+
+ 	if (!$url_resultat) 
 		$url_resultat = self();
 	// Nettoyer l'URL des mots[] qu'elle contiendrait deja
 	$url_resultat = parametre_url($url_resultat,'mots','');
