d1d97397bcb78ddbf8e17498d2bcc88a9f8f406f
lspeciale@gmail.com <lspeciale@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-03-19 00:26:01 +0000

    Surcharge  filtre_text_csv_dist Plus de précision dans les CSS
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@36431 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- tablos/tablos_pipelines.php
+++ tablos/tablos_pipelines.php
@@ -1,6 +1,6 @@
 <?php
 function tablos_insert_head($texte){
-	$texte .= '<link rel="stylesheet" type="text/css" href="'.find_in_path('prive/spip_style.css').'" media="all" />'."\n";
+	$texte .= '<link rel="stylesheet" type="text/css" href="'.find_in_path('tablos.css').'" media="all" />'."\n";
 	return $texte;
 }
 ?>
