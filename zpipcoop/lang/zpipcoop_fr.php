fc3fc9c8a85044df8653ec52b85c76a68f0f1ea7
pierrekuhn82@gmail.com <pierrekuhn82@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-03-20 21:12:19 +0000

    Rajout de la fonction de création de rubrique (et modification) Petit retour sur [36495]
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@36496 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- zpipcoop/lang/zpipcoop_fr.php
+++ zpipcoop/lang/zpipcoop_fr.php
@@ -36,6 +36,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 //E
 'editer_message' => '&Eacute;diter le message',
+'editer_rubrique' => '&Eacute;diter la rubrique',
 'editer_site' => '&Eacute;diter le site',
 
 //F
@@ -67,6 +68,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 //N
 'nouveau_message' => 'Nouveau message',
 'nouveau_site' => 'Nouveau site',
+'nouvelle_rubrique' => 'Nouvelle rubrique',
 
 //P
 'pas_le_droit_ecrire' => 'Vous n\'avez pas d\'autorisation en &eacute;criture',
