24be55386a597c72e68977fed15ed3fedbbb110d
cy_altern@yahoo.fr <cy_altern@yahoo.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-03-21 21:56:39 +0000

    ajouter la possibilite de choisir le fichier RSS utilise pour generer les marqueurs sur la carte (histoire de ne pas devoir forker le plugin pour permettre le multilinguisme par exemple)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@36533 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- googlemap_api/lang/geomap_fr.php
+++ googlemap_api/lang/geomap_fr.php
@@ -25,7 +25,11 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'explication_cle' => 'Google Map API Key : <a href="http://www.google.com/apis/maps/signup.html" class="spip_out">Obtenir une cl&eacute;</a>',
 	'explication_compacte' => '&Eacute;viter le compactage du javascript',
 	'explication_custom_control' => 'D&eacute;sactiver les contr&ocirc;les personnalis&eacute;s :',
+  'explication_nom_fichier_rss_gis'=>'nom du fichier RSS utilis&eacute; pour g&eacute;n&eacute;rer les marqueurs',
 	'erreur_api_browser' => 'D&eacute;sol&eacute;, l\'API Google Maps n\'est pas compatible avec votre navigateur',
+
+  // F
+  'fichier_rss_gis'=>'Fichier RSS GIS',
 	
 	// L
 	'latitude' => 'Latitude',
