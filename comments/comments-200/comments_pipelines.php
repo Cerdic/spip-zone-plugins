fb7381b4d5b9e458106f99f9f6140cfb8c0d352b
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-03-23 21:11:51 +0000

    une balise #BOUTON_ADMIN_FORUM pour generer les boutons d'admin des forums (dispo en 2.1 uniquement)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@36578 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- comments/comments-200/comments_pipelines.php
+++ comments/comments-200/comments_pipelines.php
@@ -1,4 +1,44 @@
 <?php
+/*
+ * Plugin Comments
+ * (c) 2010 xxx
+ * Distribue sous licence GPL
+ *
+ */
+
+/**
+ * Generer les boutons d'admin des forum selon les droits du visiteur
+ * en SPIP 2.1 uniquement
+ * 
+ * @param <type> $p
+ * @return <type>
+ */
+function balise_BOUTONS_ADMIN_FORUM_dist($p) {
+	if (($_id = interprete_argument_balise(1,$p))===NULL)
+		$_id = champ_sql('id_forum', $p);
+
+	include_spip('inc/plugin');
+	if (function_exists('spip_version_compare')
+		AND spip_version_compare($GLOBALS['spip_version_code'],"2.1.0-rc",">="))
+		$p->code = "
+'<'.'?php
+	if (\$GLOBALS[\'visiteur_session\'][\'statut\']==\'0minirezo\'
+		AND (\$id = '.intval($_id).')
+		AND	include_spip(\'inc/autoriser\')
+		AND autoriser(\'moderer\',\'forum\',\$id)) {
+			include_spip(\'inc/actions\');
+			echo \"<div class=\'actions modererforum\'>\"
+			. bouton_action(_T(\'icone_supprimer_message\'),generer_action_auteur(\'instituer_forum\',\$id.\'-off\',ancre_url(self(),\'forum\')))
+			. bouton_action(_T(\'SPAM\'),generer_action_auteur(\'instituer_forum\',\$id.\'-spam\',ancre_url(self(),\'forum\')))
+			. \"</div>\";
+		}
+?'.'>'";
+		$p->interdire_scripts = false;
+
+	return $p;
+
+}
+
 
 // surcharger les boucles FORUMS
 // pour afficher uniquement les forums public meme en preview
