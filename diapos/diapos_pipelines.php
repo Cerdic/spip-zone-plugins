a50734da373722143b9f129ce3e54f8597d11241
lspeciale@gmail.com <lspeciale@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-03-23 19:51:38 +0000

    Super, désolé d'avoir pesté tout à l'heure. J'ai cannibalisé ton code et pondu un petit
    	find_in_path('diapos.css.html'); 
    	if (find_in_path('diapos.css.html')){
    		$flux .= recuperer_fond('diapos-insert-head',array());
    	}
    Et ça fonctionne avec collezion qui utilise inc-insert-head.
    
    C'est bien ?
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@36572 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- diapos/diapos_pipelines.php
+++ diapos/diapos_pipelines.php
@@ -1,9 +1,18 @@
 <?php
 function diapos_insert_head($flux){
 	$flux .= '<script type="text/javascript" src="'.find_in_path('diapos.js').'"></script>';
-	$flux .= '<link rel="stylesheet" href="'.generer_url_public("diapos.css").'" type="text/css" media="projection, screen, tv" />'; 
+	
+	find_in_path('diapos.css.html'); 
+	if (find_in_path('diapos.css.html')){
+		$flux .= recuperer_fond('diapos-insert-head',array());
+	}
+	
+	//if (find_in_path('diapos.css.html')){
+		//$flux .= '<link rel="stylesheet" href="'.generer_url_public("diapos.css").'" type="text/css" media="projection, screen, tv" />';
+	//}
 	return $flux;
 }
+
 function diapos_header_prive($flux){
 	$flux .= '<script type="text/javascript" src="'.find_in_path('diapos.js').'"></script>';
 	return $flux;
