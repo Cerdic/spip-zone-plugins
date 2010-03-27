d971a656853973bdead5e18e82a453730c8366ce
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-03-27 10:44:15 +0000

    Nouvelle lib soundManager
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@36673 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- lecteur_multimedia_v2/lm2_options.php
+++ lecteur_multimedia_v2/lm2_options.php
@@ -1,7 +1,7 @@
 <?php
 
 if (!defined('_DIR_LIB_SM'))
-	define('_DIR_LIB_SM', _DIR_RACINE . 'lib/soundmanagerv295b-20100101/');
+	define('_DIR_LIB_SM', _DIR_RACINE . 'lib/soundmanagerv295b-20100323/');
 
 
 ?>
