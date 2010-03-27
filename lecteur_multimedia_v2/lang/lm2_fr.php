003a417e1309e3161617c0f9f4d7faf7b493a4f4
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-03-27 10:33:18 +0000

    Un cfg un peu moins dégueux
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@36672 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- lecteur_multimedia_v2/lang/lm2_fr.php
+++ lecteur_multimedia_v2/lang/lm2_fr.php
@@ -1,12 +1,20 @@
 <?php
-// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/acces_restreint/lang/
+
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+	// C
+	'cfg_descriptif_titre' => 'Configuration du plugin Lecteur multimedia',
+	'cfg_explications_lecteur_video' => 'Choisir parmi les lecteurs propos&eacute;s pour remplacer &lt;docX|video&gt;.',
+	'cfg_label_lecteur_audio' => 'Lecteur audio',
+	'cfg_label_lecteur_video' => 'Lecteur vid&eacute;o',
+	'cfg_label_lecteur_video_lecteur' => 'Lecteur vid&eacute;o',
+	'cfg_legende_documents_textes' => 'Lecteur pour les documents dans les textes',
+	'cfg_lien_doc' => '<a href="http://multimedia.spip-zone.info" class="spip_out">Documentation</a>',
 
+	// T
 	'titre_liste_lecture' => 'Liste de lecture'
-	
+
 );
 
 ?>
