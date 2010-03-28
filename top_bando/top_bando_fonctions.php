b24782d971f1921b350c55c77cdb1b86a8c85dd0
gilles.vincent@gmail.com <gilles.vincent@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-03-28 23:16:34 +0000

    Documentation au format PHPDoc + indentation + suppression des accents des commentaires pour les fichiers qui ne sont pas en utf8
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@36736 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- top_bando/top_bando_fonctions.php
+++ top_bando/top_bando_fonctions.php
@@ -1,20 +1,38 @@
 <?php
-// supprimer le # dans une chaîne (cf couleurs pour image_typo
+/**
+ * Ce plugin permet de g&#233;rer le bandeau de sommet des pages: hauteur et images.
+ *
+ * @author     cy_altern
+ * @license    GNU/GPL
+ * @package    plugins
+ * @subpackage top_bando
+ * @category   Interface Privee
+ * @version    $Id$
+ */
+
+
+/**
+ * supprimer le # dans une chaine (cf couleurs pour image_typo)
+ * @param string $txt
+ * @return string
+ */
 function top_bando_suprime_diese($txt) {
-    return str_replace('#', '', $txt);
+	return str_replace('#', '', $txt);
 }
 
+/**
+ * Modifier le fichier d'upload
+ * @return string
+ */
 function affiche_upload(){
 	$ret = '';
 	$iconifier = charger_fonction('iconifier', 'inc');
 	$ret .= $iconifier('id_syndic', 0, 'cfg&amp;cfg=top_bando');
-  
-  
-  $Trech = array('#<div([^>]*?cadre.*?)>.*?<img[^>]*?cadre-icone[^>]*?>#Uim', '#<\/div>(\W*?<\/div>\W*?<script.*?)$#Uim', '#<a[^>]*?aide[^>]*?>[\W]*<img[^>]*?>[\W]*<\/a>#Uim');
-  $Tremp = array('<fieldset $1><legend>'._T('topbando:choix_images').'</legend><p>'._T('topbando:explication_choix_images').'</p>', '</fieldset> $1','');
+	$Trech = array('#<div([^>]*?cadre.*?)>.*?<img[^>]*?cadre-icone[^>]*?>#Uim', '#<\/div>(\W*?<\/div>\W*?<script.*?)$#Uim', '#<a[^>]*?aide[^>]*?>[\W]*<img[^>]*?>[\W]*<\/a>#Uim');
+	$Tremp = array('<fieldset $1><legend>'._T('topbando:choix_images').'</legend><p>'._T('topbando:explication_choix_images').'</p>', '</fieldset> $1','');
 
-  $ret = preg_replace($Trech, $Tremp, $ret);
-  return $ret;
+	$ret = preg_replace($Trech, $Tremp, $ret);
+	return $ret;
 }
 
 ?>
--- top_bando/top_bando_pipelines.php
+++ top_bando/top_bando_pipelines.php
@@ -1,31 +1,48 @@
 <?php
-// ajouter js et css necessaires pour utliser jQuery UI dans l'interface de config CFG
+/**
+ * Ce plugin permet de g&#233;rer le bandeau de sommet des pages: hauteur et images.
+ *
+ * @author     cy_altern
+ * @license    GNU/GPL
+ * @package    plugins
+ * @subpackage top_bando
+ * @category   Interface Privee
+ * @version    $Id$
+ */
+
+
+/**
+ * ajouter js et css necessaires pour utiliser jQuery UI dans l'interface de config CFG
+ *
+ * @param string $flux # le code HTML a modifier
+ * @return string
+ */
 function top_bando_header_prive($flux){
 	// Initialisation des valeurs de config
 	$config = @unserialize($GLOBALS['meta']['top_bando']);
  
-// si on veut pouvoir manipuler la zone de recadrage en drag/resize dans la page CFG il faut les jQuery UI
-  if (_request('exec') == 'cfg' AND _request('cfg') == 'top_bando'){
+	// si on veut pouvoir manipuler la zone de recadrage en drag/resize dans la page CFG il faut les jQuery UI
+	if (_request('exec') == 'cfg' AND _request('cfg') == 'top_bando'){
 
-    // Insertion des librairies js en fonction de la version de jQuery donc de celle de SPIP...
-      // en SPIP 2.1.* jQuery est en version 1.3.+ donc ui en version 1.7.2
-      if (strpos('2.1', $GLOBALS['spip_version_affichee'])) {
-          $flux .='<link type="text/css" href="'.url_absolue(find_in_path('lib/jquery-ui-themes-1.7.2/themes/base/jquery-ui.css')).'" rel="Stylesheet"/>'."\r\n";
-          $flux .='<script src="'.url_absolue(find_in_path('lib/jquery-ui-1.7.2/ui/jquery-ui.js')).'"></script>'."\r\n";
-          $flux .='<script src="'.find_in_path('lib/jquery-ui-1.7.2/ui/ui.resizable.js').'"></script>'."\r\n";
-          $flux .='<script src="'.find_in_path('lib/jquery-ui-1.7.2/ui/ui.draggable.js').'"></script>'."\r\n";
-      }
-      // en SPIP 2.0.* jQuery est en version 1.2.6 donc ui en version 1.6
-      else {
-          $flux .='<link type="text/css" href="'.find_in_path('lib/jquery.ui-1.6/themes/default/ui.all.css').'" rel="Stylesheet"/>'."\r\n";
-          $flux .='<script src="'.find_in_path('lib/jquery.ui-1.6/ui/ui.core.js').'"></script>'."\r\n";
-          $flux .='<script src="'.find_in_path('lib/jquery.ui-1.6/ui/ui.resizable.js').'"></script>'."\r\n";
-          $flux .='<script src="'.find_in_path('lib/jquery.ui-1.6/ui/ui.draggable.js').'"></script>'."\r\n";
-      }
+		// Insertion des librairies js en fonction de la version de jQuery donc de celle de SPIP...
+		// en SPIP 2.1.* jQuery est en version 1.3.+ donc ui en version 1.7.2
+		if (strpos('2.1', $GLOBALS['spip_version_affichee'])) {
+			$flux .='<link type="text/css" href="'.url_absolue(find_in_path('lib/jquery-ui-themes-1.7.2/themes/base/jquery-ui.css')).'" rel="Stylesheet"/>'."\r\n";
+			$flux .='<script src="'.url_absolue(find_in_path('lib/jquery-ui-1.7.2/ui/jquery-ui.js')).'"></script>'."\r\n";
+			$flux .='<script src="'.find_in_path('lib/jquery-ui-1.7.2/ui/ui.resizable.js').'"></script>'."\r\n";
+			$flux .='<script src="'.find_in_path('lib/jquery-ui-1.7.2/ui/ui.draggable.js').'"></script>'."\r\n";
+		}
+		// en SPIP 2.0.* jQuery est en version 1.2.6 donc ui en version 1.6
+		else {
+			$flux .='<link type="text/css" href="'.find_in_path('lib/jquery.ui-1.6/themes/default/ui.all.css').'" rel="Stylesheet"/>'."\r\n";
+			$flux .='<script src="'.find_in_path('lib/jquery.ui-1.6/ui/ui.core.js').'"></script>'."\r\n";
+			$flux .='<script src="'.find_in_path('lib/jquery.ui-1.6/ui/ui.resizable.js').'"></script>'."\r\n";
+			$flux .='<script src="'.find_in_path('lib/jquery.ui-1.6/ui/ui.draggable.js').'"></script>'."\r\n";
+		}
       
-  }
-  
-  return $flux;
+	}
+
+	return $flux;
 }
 
 ?>
