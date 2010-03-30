59d3b5be1facd397dd135a8f27908cfcba0ec23d
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-03-30 15:20:46 +0000

    On s'insère dans le cron
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@36765 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- saveauto/2.1/genie/saveauto.php
+++ saveauto/2.1/genie/saveauto.php
@@ -1,18 +1,25 @@
 <?php
 
-//
-// Alerte sur les articles publies post-dates
-//
 
+/**
+ * La fonction à exécuter par le cron
+ * @param unknown_type $last
+ */
 function genie_saveauto_dist($last) {
 	$saveauto_creation = $GLOBALS['meta']['saveauto_creation'] ? $GLOBALS['meta']['saveauto_creation'] : time();
-	if ($GLOBALS['meta']['derniere_modif'] > $saveauto_creation){
+	if (($GLOBALS['meta']['derniere_modif'] > $saveauto_creation) AND $last){
 		$saveauto = charger_fonction('saveauto','inc');
 		$saveauto();
 	}
 	return 1;
 }
 
+/**
+ * On s'insère dans le cron de SPIP
+ * Par défaut une fois par jour (peut être modifié dans la conf)
+ *
+ * @param array $taches_generales
+ */
 function saveauto_taches_generales_cron($taches_generales){
 	if ($cfg = @unserialize($GLOBALS['meta']['saveauto'])){
 		$taches_generales['saveauto'] = $cfg['frequence_maj']*24*3600;
