a1bf610b9e2a1dd56b7ab06e26826b6bc6399da4
romy@rezo.net <romy@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-03-31 20:53:31 +0000

    Ce n'est pas satisfaisant, car Pétronille et son reset CSS doivent être appelés en premier, mais ça sera mieux dans insert_head_css
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@36805 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- petronille/petronille_pipelines.php
+++ petronille/petronille_pipelines.php
@@ -1,7 +1,9 @@
 <?php
 
-function petronille_insert_head($flux) {
-	$flux .= '<link rel="stylesheet" href="'.find_in_path('petronille.css').'" type="text/css" media="all" />';
+function petronille_insert_head_css($flux) {
+	$flux .= '<link rel="stylesheet" href="'.find_in_path('reset.css').'" type="text/css" media="all" />'."\n";
+	$flux .= '<link rel="stylesheet" href="'.find_in_path('typo.css').'" type="text/css" media="all" />'."\n";
+	$flux .= '<link rel="stylesheet" href="'.find_in_path('spip.css').'" type="text/css" media="all" />';
 	return $flux;
 }
 
