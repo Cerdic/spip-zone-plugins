0afe9cdc3f0b1f9e95ed773b9ea0c5d3f37ee6e8
erational@erational.org <erational@erational.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-04-01 08:53:20 +0000

    fichier lang: english
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@36818 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ ajaxforms/lang/ajaxform_en.php
@@ -0,0 +1,23 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// formulaire editer logo
+'logo' => 'Logo',
+'logo_survol' => 'Rollover Logo',
+'logo_maj'=>'The logo has been updated.',
+'confirmer_suppression'=>'Remove completed',
+
+// formulaire ajouter document
+'aucun_fichier_recu' => 'Oups ! You have to send something !',
+'document_ajoute' => 'The document has been added.',
+'erreur_ajout_document' => 'An error has occured while adding the document.',
+'mauvaise_reception' => 'Oups ! Error in receiving the file. Please try again ...',
+
+// formulaire modifier document
+'changer_document'=>'Change the document',
+
+);
+?>
