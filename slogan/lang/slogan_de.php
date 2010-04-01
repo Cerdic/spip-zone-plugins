67bfaf79982f74a37ea635da075a3a327f73695e
klaus@spip.de <klaus@spip.de@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-04-01 20:23:36 +0000

    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@36832 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ slogan/lang/slogan_de.php
@@ -0,0 +1,13 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	'info_slogan_site' => "Slogan Ihrer Website",
+	'titre_identite_site' => 'Identit&auml;t der Website',
+	'config_info_enregistree' => 'Die Konfiguration wurde gespeichert'
+
+);
+
+?>
