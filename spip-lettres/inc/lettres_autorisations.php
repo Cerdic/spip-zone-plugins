c91019ac9ebeb2da51da48eadebc54f22f8220d4
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-04-05 10:50:39 +0000

    retour du dater avec la bonne autorisation
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@36947 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spip-lettres/inc/lettres_autorisations.php
+++ spip-lettres/inc/lettres_autorisations.php
@@ -1,39 +1,63 @@
 <?php
+/**
+ * SPIP-Lettres
+ *
+ * Copyright (c) 2006-2009
+ * Agence Artégo http://www.artego.fr
+ *
+ * Ce programme est un logiciel libre distribue sous licence GNU/GPLv3.
+ * Pour plus de details voir http://www.gnu.org/licenses/gpl-3.0.html
+ *
+ **/
 
 
-	/**
-	 * SPIP-Lettres
-	 *
-	 * Copyright (c) 2006-2009
-	 * Agence Artégo http://www.artego.fr
-	 *  
-	 * Ce programme est un logiciel libre distribue sous licence GNU/GPLv3.
-	 * Pour plus de details voir http://www.gnu.org/licenses/gpl-3.0.html
-	 *  
-	 **/
+function lettres_autoriser() {}
 
 
-	function lettres_autoriser() {}
-	
-	
-	function autoriser_lettres_dist($faire, $type, $id, $qui, $opt) {
-		switch ($faire) {
-			case 'bouton':
-			case 'onglet':
-			case 'configurer':
-			case 'voir':
-			case 'exporter':
-			case 'importer':
-			case 'purger':
-			case 'joindre':
-			case 'editer':
-				return ($qui['statut'] == '0minirezo');
-				break;
-			default:
-				return false;
-				break;
-		}
+function autoriser_lettres_dist($faire, $type, $id, $qui, $opt) {
+	switch ($faire) {
+		case 'bouton':
+		case 'onglet':
+		case 'configurer':
+		case 'voir':
+		case 'exporter':
+		case 'importer':
+		case 'purger':
+		case 'joindre':
+		case 'editer':
+			return ($qui['statut'] == '0minirezo');
+			break;
+		default:
+			return false;
+			break;
 	}
+}
 
+
+function autoriser_dater_dist($faire, $type, $id, $qui, $opt) {
+	if (!isset($opt['statut'])){
+		$table = table_objet($type);
+		$trouver_table = charger_fonction('trouver_table','base');
+		$desc = $trouver_table($table);
+		if (!$desc)
+			return false;
+		if (isset($desc['field']['statut'])){
+			$statut = sql_getfetsel("statut", $desc['table'], id_table_objet($type)."=".intval($id));
+		}
+		else
+			$statut = 'publie'; // pas de statut => publie
+	}
+	else
+		$statut = $opt['statut'];
+
+	if ($statut == 'publie'
+	 OR ($statut == 'prop' AND $type=='article' AND $GLOBALS['meta']["post_dates"] == "non"))
+		return autoriser('modifier', $type, $id);
+
+	if ($type=='lettre' AND($statut == 'brouillon' OR $statut == 'en_ligne' OR $statut == 'hors_ligne'))
+		return autoriser('modifier', $type, $id);
+
+	return false;
+}
 
 ?>
