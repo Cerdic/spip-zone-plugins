e90ac79a1a2c6adf9448fd43d027175d19267405
romy@rezo.net <romy@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-04-05 17:02:34 +0000

    Petronille se mêle des formulaires
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@36970 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- petronille/petronille_pipelines.php
+++ petronille/petronille_pipelines.php
@@ -1,9 +1,10 @@
 <?php
 
 function petronille_insert_head_css($flux) {
-	$flux .= '<link rel="stylesheet" href="'.find_in_path('reset.css').'" type="text/css" media="all" />'."\n";
-	$flux .= '<link rel="stylesheet" href="'.find_in_path('typo.css').'" type="text/css" media="all" />'."\n";
-	$flux .= '<link rel="stylesheet" href="'.find_in_path('spip.css').'" type="text/css" media="all" />';
+	$flux .= '<link rel="stylesheet" type="text/css" href="'.find_in_path('reset.css').'" media="all" />'."\n";
+	$flux .= '<link rel="stylesheet" type="text/css" href="'.find_in_path('typo.css').'" media="all" />'."\n";
+	$flux .= '<link rel="stylesheet" type="text/css" href="'.find_in_path('forms.css').'" media="all" />'."\n";
+	$flux .= '<link rel="stylesheet" type="text/css" href="'.find_in_path('spip.css').'" media="all" />';
 	return $flux;
 }
 
