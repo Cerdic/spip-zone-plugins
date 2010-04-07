467435434721a6674d532663f1f678d3ddfe35a0
brunobergot@gmail.com <brunobergot@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-04-07 11:07:00 +0000

    report de [30806] [31054] [31212] [32204] [32251] et [34143] pour bénéficier de l'interface de gestion des forums "à la gmail" en 2.0.x
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@37020 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- forum/lang/forum_fr.php
+++ forum/lang/forum_fr.php
@@ -15,6 +15,8 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 'icone_bruler_messages' => "Signaler comme Spam",
 'icone_legitimer_message' => "Signaler comme licite",
 'icone_supprimer_messages' => 'Refuser ces messages',
+'interface_onglets' => 'Interface avec onglets',
+'interface_formulaire' => 'Interface formulaire',
 
 'messages_meme_auteur' => 'Tous les messages de cet auteur',
 'messages_meme_email' => 'Tous les messages de cet email',
@@ -22,6 +24,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 // statuts
 'messages_tous' => 'Tous',
+'messages_aucun' => 'Aucun',
 'messages_publie' => 'Publi&eacute;s',
 'messages_prop' => 'Propos&eacute;s',
 'messages_spam' => 'Spam',
