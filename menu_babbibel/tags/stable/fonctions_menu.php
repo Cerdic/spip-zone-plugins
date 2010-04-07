843733ccecd1ac7677be1c0d9a2e948f8ef7362e
babbibel@gmail.com <babbibel@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-04-07 18:55:20 +0000

    supression du vieux css
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@37034 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- menu_babbibel/tags/stable/fonctions_menu.php
+++ menu_babbibel/tags/stable/fonctions_menu.php
@@ -2,7 +2,7 @@
 
 function babbi_headeur($flux){
 	$flux .= "<script type='text/javascript' src='".find_in_path('js/menu_babbi.js')."'></script>\n";
-	$flux .= '<link rel="stylesheet" href="'.find_in_path('css/menu_babbi.css').'" type="text/css" media="projection, screen" />';
+	$flux .= '<link rel="stylesheet" href="'.find_in_path('menu_babbi.css').'" type="text/css" media="projection, screen" />';
 	return $flux;
 }
 ?>
--- menu_babbibel/tags/stable/js/menu_babbi.js
+++ menu_babbibel/tags/stable/js/menu_babbi.js
@@ -1,6 +1,6 @@
 function mainmenu(){
-$(" #nav ul ").css({display: "none"}); // Opera Fix
-$(" #nav li").hover(function(){
+$(" .nav ul ").css({display: "none"}); // Opera Fix
+$(" .nav li").hover(function(){
 		$(this).find('ul:first').css({visibility: "visible",display: "none"}).show(400);
 		},function(){
 		$(this).find('ul:first').css({visibility: "hidden"});
