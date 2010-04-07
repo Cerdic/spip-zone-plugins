d6aeadec8444d2d50b6fbb3fbe446280770c7591
joseph@larmarange.net <joseph@larmarange.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-04-07 17:26:11 +0000

    Les noisettes sont inserees en Ajax. Pratique pour les listes paginees
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@37030 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- noiZetier/balise/noizetier_afficher_noisette.php
+++ noiZetier/balise/noizetier_afficher_noisette.php
@@ -11,7 +11,8 @@ function balise_NOIZETIER_AFFICHER_NOISETTE_dist($p) {
 	
 	$p->code =  "(!$id_noisette) ? _T('zbug_champ_hors_motif', array('champ'=>'ID_NOISETTE', 'motif'=>'NOISETTES')) : recuperer_fond(
 		'noisettes/'.$noisette,
-		array_merge(unserialize($parametres), \$Pile[0])
+		array_merge(unserialize($parametres), \$Pile[0]),
+		array('ajax'=>true)
 	)";
 	
 	return $p;
