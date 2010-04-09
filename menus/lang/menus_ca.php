82abc3a9306b8ed5a2ab8090017b3d132e8becf3
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-04-09 20:56:38 +0000

    langues (menus)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@37082 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ menus/lang/menus_ca.php
@@ -0,0 +1,100 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// D
+	'description_menu_accueil' => 'Enlla&ccedil; cap a la p&agrave;gina d\'inici del lloc.',
+	'description_menu_deconnecter' => 'Si el visitant est&agrave; connectat, afegeix una entrada proposant-li la desconnexi&oacute;.',
+	'description_menu_groupes_mots' => 'Mostra autom&agrave;ticament un men&uacute; que llista les paraules del grup i els articles lligats. Per defecte, mostra la llista dels grups de paraules i les paraules lligades. Si un esquelet grups_paraules.html existeix, s\'utilitzar&agrave; l\'enlla&ccedil; cap al grup.',
+	'description_menu_lien' => 'Afegeix un enlla&ccedil; arbitrari, intern (URL relativa) o extern (http://...).',
+	'description_menu_mapage' => 'Si el visitant est&agrave; connectat, afegeix un enlla&ccedil; cap a la p&agrave;gina de l\'autor.',
+	'description_menu_mots' => 'Mostra autom&agrave;ticament un men&uacute; que llista els articles lligats a la paraula clau.',
+	'description_menu_objet' => 'Crea un enlla&ccedil; cap a un objecte d\'SPIP: article, secci&oacute; o un altre. Per defecte, l\'entrada tindr&agrave; el t&iacute;tol de l\'objecte.',
+	'description_menu_page_speciale' => 'Afegeix un enlla&ccedil; cap a un esquelet accessible per un URL del tipus <code>spip.php?page=nom&amp;param1=xx&amp;param2=yyy...</code> Aquestes p&agrave;gines sovint s&oacute;n subministrades per connectors.',
+	'description_menu_rubriques' => 'Mostra una llista de seccions i, si es vol, les subseccions a diferents nivells. Per defecte, mostra totes les seccions des de l\'arrel, ordenats per t&iacute;tol (num&egrave;ricament i despr&eacute;s afab&egrave;ticament).',
+	'description_menu_secteurlangue' => 'Aquesta entrada &eacute;s espec&iacute;fica pels llocs que utilitzen un sector per llengua. Mostra autom&agrave;ticament un men&uacute; que llista les seccions del sector corresponent a la llengua de la p&agrave;gina i, si es vol, les subseccions a diferents nivells. Per defecte, mostra totes les seccions des de l\'arrel, ordenades per t&iacute;tol (num&egrave;ricament i despr&eacute;s alfab&egrave;ticament).',
+
+	// E
+	'editer_menus_editer' => 'Modificar aquest men&uacute;',
+	'editer_menus_explication' => 'Creeu i configureu els men&uacute;s del vostre lloc.',
+	'editer_menus_exporter' => 'Exportar aquest men&uacute;',
+	'editer_menus_nouveau' => 'Creeu un nou men&uacute;',
+	'editer_menus_titre' => 'Men&uacute;s del lloc',
+	'entree_aucun' => 'Cap',
+	'entree_choisir' => 'Escolliu el tipus d\'entrada que voleu afegir:',
+	'entree_css' => 'Classes CSS de l\'entrada',
+	'entree_id_groupe' => 'N&uacute;mero del grup de paraula clau',
+	'entree_id_mot' => 'N&uacute;mero de la paraula clau',
+	'entree_id_objet' => 'N&uacute;mero',
+	'entree_id_rubrique' => 'N&uacute;mero de la secci&oacute; pare',
+	'entree_infini' => 'Fins l\'infinit',
+	'entree_mapage' => 'La meva p&agrave;gina personal',
+	'entree_niveau' => 'Nivell de les subseccions',
+	'entree_nombre_articles' => 'N&uacute;mero d\'articles com a m&agrave;xim (0 per defecte)',
+	'entree_page' => 'Nom de la p&agrave;gina',
+	'entree_parametres' => 'Llista dels par&agrave;metres',
+	'entree_sur_n_articles' => '@n@ articles mostrat(s)',
+	'entree_sur_n_mots' => '@n@ paraules mostrada(es)',
+	'entree_sur_n_niveaux' => 'A @n@ nivell(s)',
+	'entree_titre' => 'T&iacute;tol',
+	'entree_tri_alpha' => 'Criteri d\'ordenaci&oacute; (alfab&egrave;tic)',
+	'entree_tri_num' => 'Criteri d\'ordenaci&oacute; (num&egrave;ric)',
+	'entree_type_objet' => 'Tipus d\'objecte',
+	'entree_url' => 'Adre&ccedil;a',
+	'erreur_aucun_type' => 'No s\'ha trobat cap entrada.',
+	'erreur_autorisation' => 'No est&agrave;s autoritzat per modificar els men&uacute;s.',
+	'erreur_identifiant_deja' => 'Aquest identificador ja es utilitzat per un men&uacute;.',
+	'erreur_identifiant_forme' => 'L\'identificador nom&eacute;s pot contenir lletres, xifres o el car&agrave;cter subratllat.',
+	'erreur_menu_inexistant' => 'El men&uacute; demanat n&uacute;mero @id@ no existeix.',
+	'erreur_mise_a_jour' => 'S\'ha produ&iuml;t un error durant l\'actualitzaci&oacute; de la base de dades .',
+	'erreur_type_menu' => 'Has d\'escollir un tipus de men&uacute;',
+
+	// F
+	'formulaire_ajouter_entree' => 'Afegir una entrada',
+	'formulaire_ajouter_sous_menu' => 'Crear un submen&uacute;',
+	'formulaire_css' => 'Classes CSS',
+	'formulaire_css_explication' => 'Podeu afegir al men&uacute; eventuals classes CSS suplement&agrave;ries.',
+	'formulaire_deplacer_bas' => 'Despla&ccedil;ar avall',
+	'formulaire_deplacer_haut' => 'Despla&ccedil;ar amunt',
+	'formulaire_facultatif' => 'Facultatiu',
+	'formulaire_identifiant' => 'Identificador',
+	'formulaire_identifiant_explication' => 'Doneu una paraula clau &uacute;nica que us permetr&agrave; cridar f&agrave;cilment el vostre men&uacute;.',
+	'formulaire_importer' => 'Importar un men&uacute;',
+	'formulaire_importer_explication' => 'Si heu exportar un men&uacute; a dins d\'un fitxer, ara el podreu importar.',
+	'formulaire_modifier_entree' => 'Modificar aquesta entrada',
+	'formulaire_modifier_menu' => 'Modificar el men&uacute;:',
+	'formulaire_nouveau' => 'Nou men&uacute;',
+	'formulaire_partie_construction' => 'Construcci&oacute; del men&uacute;',
+	'formulaire_partie_identification' => 'Identificaci&oacute; del men&uacute;',
+	'formulaire_supprimer_entree' => 'Suprimir aquesta entrada',
+	'formulaire_supprimer_menu' => 'Suprimir el men&uacute;',
+	'formulaire_supprimer_sous_menu' => 'Suprimir aquest submen&uacute;',
+	'formulaire_titre' => 'T&iacute;tol',
+
+	// I
+	'info_numero_menu' => 'MEN&Uacute; N&Uacute;MERO:',
+	'info_page_speciale' => 'Enlla&ccedil; cap a la p&agrave;gina &laquo;@page@&raquo;',
+	'info_tous_groupes_mots' => 'Tots els grups de paraules',
+	'info_tri' => 'Ordena:',
+	'info_tri_alpha' => '(alfab&egrave;tica)',
+	'info_tri_num' => '(num&egrave;rica)',
+
+	// N
+	'nom_menu_accueil' => 'Inici',
+	'nom_menu_deconnecter' => 'Desconnectar-se',
+	'nom_menu_groupes_mots' => 'Paraules clau i Articles d\'un Grup de paraules',
+	'nom_menu_lien' => 'Enlla&ccedil; arbitrari',
+	'nom_menu_mapage' => 'La meva p&agrave;gina',
+	'nom_menu_mots' => 'Articles d\'una Paraula clau',
+	'nom_menu_objet' => 'Article, secci&oacute; o un altre objecte SPIP',
+	'nom_menu_page_speciale' => 'Enlla&ccedil; cap a una p&agrave;gina esquelet',
+	'nom_menu_rubriques' => 'Llista o arboresc&egrave;ncia de seccions',
+	'nom_menu_secteurlangue' => 'Sectors de llengua'
+);
+
+?>
