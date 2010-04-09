b90c1a8df6ff4ac471c714b50c9b1f945fa4c213
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-04-09 09:24:19 +0000

    Un problème en 2.1 sur compacte_head... on teste son existence
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@37072 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- notation/notation_pipelines.php
+++ notation/notation_pipelines.php
@@ -17,8 +17,10 @@ function notation_affichage_final($flux){
 	$incHead .= "<script src='"._DIR_PLUGIN_NOTATION."javascript/jquery.MetaData.js' type='text/javascript'></script>\n";
 	$incHead .= "<script src='"._DIR_PLUGIN_NOTATION."javascript/jquery.rating.js' type='text/javascript'></script>\n";
 	$incHead .= "<script src='"._DIR_PLUGIN_NOTATION."javascript/notation.js' type='text/javascript'></script>\n";
-	include_spip('inc_filtres');
-	$incHead = compacte_head($incHead);
+	include_spip('inc/filtres');
+	if(function_exists('compacte_head')){
+		$incHead = compacte_head($incHead);
+	}
 	return substr_replace($flux, $incHead, strpos($flux, '</head>'), 0);
 
 }
