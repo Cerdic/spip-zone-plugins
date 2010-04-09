8151620765660ddc866e303d4341feaa87926489
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-04-09 20:56:45 +0000

    langues (verifier)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@37086 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ verifier/lang/verifier_ca.php
@@ -0,0 +1,58 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// E
+	'erreur_date' => 'El format de la data no &eacute;s correcte.',
+	'erreur_email' => 'El format de l\'adre&ccedil;a de correu electr&ograve;nic no &eacute;s v&agrave;lid.',
+	'erreur_email_nondispo' => 'Aquest adre&ccedil;a de correu electr&ograve;nic ja est&agrave; registrada.',
+	'erreur_entier' => 'El valor ha de ser un nombre enter.',
+	'erreur_entier_entre' => 'El valor ha d\'estar compr&egrave;s entre @min@ i @max@.',
+	'erreur_entier_max' => 'El valor ha de ser inferior a @max@.',
+	'erreur_entier_min' => 'El valor ha de ser superior a @min@.', # MODIF
+	'erreur_numerique' => 'El format del n&uacute;mero no &eacute;s v&agrave;lid.',
+	'erreur_regex' => 'El format de la cadena no &eacute;s v&agrave;lida.',
+	'erreur_siren' => 'N&uacute;mero SIREN no v&agrave;lid.',
+	'erreur_siret' => 'N&uacute;mero SIRET no v&agrave;lid.',
+	'erreur_taille_egal' => 'El valor ha de tenir exactament @egal@ car&agrave;cters.',
+	'erreur_taille_entre' => 'El valor ha d\'estar compr&egrave;s entre @min@ i @max@ car&agrave;cters.',
+	'erreur_taille_max' => 'El valor ha de comprendre com a molt @max@ car&agrave;cters.',
+	'erreur_taille_min' => 'El valor ha de tenir com a m&iacute;nim @min@ car&agrave;cters.',
+	'erreur_telephone' => 'El n&uacute;mero no &eacute;s v&agrave;lid.',
+	'erreur_url' => 'L\'adre&ccedil;a no &eacute;s v&agrave;lida.',
+
+	// O
+	'option_entier_max_label' => 'Valor m&agrave;xim',
+	'option_entier_min_label' => 'Valor m&iacute;nim',
+	'option_regex_modele_label' => 'El valor ha de coincidir amb la m&agrave;scara de la seg&uuml;ent',
+	'option_siren_siret_mode' => 'Que voulez-vous vérifier ?', # NEW
+	'option_siren_siret_mode_siren' => 'le SIREN', # NEW
+	'option_siren_siret_mode_siret' => 'le SIRET', # NEW
+	'option_taille_max_label' => 'Mida m&agrave;xima',
+	'option_taille_min_label' => 'Mida m&iacute;nima',
+
+	// T
+	'type_date' => 'Data',
+	'type_date_description' => 'Verifica que el valor &eacute;s una data en format JJ/MM/AAAA. El separador &eacute;s lliure (".", "/", etc.).',
+	'type_email' => 'Adre&ccedil;a de correu electr&ograve;nic',
+	'type_email_description' => 'Verifica que el format de l\'adre&ccedil;a de correu electr&ograve;nica sigui correcte.',
+	'type_email_disponible' => 'Disponibilitat d\'una adre&ccedil;a de correu electr&ograve;nic',
+	'type_email_disponible_description' => 'Verifica que l\'adre&ccedil;a de correu electr&ograve;nica no sigui utilitzada ja per un altre usuari del sistema.',
+	'type_entier' => 'N&uacute;mero enter',
+	'type_entier_description' => 'Verifica que el valor sigui un n&uacute;mero enter, amb la possibilitat de restringir entre dos valors.',
+	'type_regex' => 'Expressi&oacute; regular ',
+	'type_regex_description' => 'Verifica que el valor correspon a la m&agrave;scara demanada. Per l\'&uacute;s de m&agrave;scares, aneu a <a href="http://fr2.php.net/manual/fr/reference.pcre.pattern.syntax.php">l\'ajuda en l&iacute;nia de PHP</a>.',
+	'type_siren_siret' => 'SIREN ou SIRET', # NEW
+	'type_siren_siret_description' => 'V&eacute;rifie que la valeur est un num&eacute;ro valide du <a href="http://fr.wikipedia.org/wiki/SIREN">Syst&egrave;me d&rsquo;Identification du R&eacute;pertoire des ENtreprises</a> fran&ccedil;ais.', # NEW
+	'type_taille' => 'Mida',
+	'type_taille_description' => 'Verifica que la mida del valor correspon al m&iacute;nim i/o al m&agrave;xim demanat.',
+	'type_telephone' => 'Num&eacute;ro de t&eacute;l&eacute;phone', # NEW
+	'type_telephone_description' => 'V&eacute;rifie que le num&eacute;ro de t&eacute;l&eacute;phone correspond &agrave; un sch&eacute;ma reconnu.', # NE
+);
+
+?>
---
+++ verifier/lang/verifier_de.php
@@ -0,0 +1,58 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// E
+	'erreur_date' => 'Fromat des Datums ung&uuml;ltig',
+	'erreur_email' => 'Mailadresse ung&uuml;ltig.',
+	'erreur_email_nondispo' => 'Mailadresse wird bereits verwendet.',
+	'erreur_entier' => 'Der Wert mu&szlig; eine ganze Zahl sein.',
+	'erreur_entier_entre' => 'Der Wert mu&szlig; zwischen  @min@ und @max@ liegen.',
+	'erreur_entier_max' => 'Der Wert mu&szlig; kleiner als @max@ sein.',
+	'erreur_entier_min' => 'Der Wert mu&szlig; gr&ouml;&szlig;er als @min@ sein.', # MODIF
+	'erreur_numerique' => 'Zahlenformat ung&uuml;ltig',
+	'erreur_regex' => 'Zeichenkettenformat ung&uuml;ltig',
+	'erreur_siren' => 'SIREN Nummer ung&uuml;ltig',
+	'erreur_siret' => 'SIRET Nummer ung&uuml;ltig',
+	'erreur_taille_egal' => 'Der Wert mu&szlig; exakt @egal@ Zeichen haben.',
+	'erreur_taille_entre' => 'Der Wert mu&szlig; zwischen @min@ und @max@ Zeichen haben.',
+	'erreur_taille_max' => 'Der Wert darf maximal @max@ Zeichen haben.',
+	'erreur_taille_min' => 'Der Wert mu&szlig; mindestens @min@ Zeichen haben.',
+	'erreur_telephone' => 'Zahl ung&uuml;ltig',
+	'erreur_url' => 'Adresse ung&uuml;ltig',
+
+	// O
+	'option_entier_max_label' => 'Maximalwert',
+	'option_entier_min_label' => 'Minimalwert',
+	'option_regex_modele_label' => 'Der Wert mu&szlig; mit der folgenden Maske &uuml;bereinstimmen.',
+	'option_siren_siret_mode' => 'Que voulez-vous vérifier ?', # NEW
+	'option_siren_siret_mode_siren' => 'le SIREN', # NEW
+	'option_siren_siret_mode_siret' => 'le SIRET', # NEW
+	'option_taille_max_label' => 'Maximalgr&ouml;&szlig;e',
+	'option_taille_min_label' => 'Minimalgr&ouml;&szlig;e',
+
+	// T
+	'type_date' => 'Datum',
+	'type_date_description' => '&Uuml;berpr&uuml;ft den Wert auf das Datumsformat  JJ/MM/AAAA. Verschiedene Trenner sind m&ouml;glich (".", "/", etc).',
+	'type_email' => 'Mailadresse',
+	'type_email_description' => '&Uuml;berpr&uuml;ft das Format der Mailadresse',
+	'type_email_disponible' => 'Verf&uuml;gbarkeit einer Mailadresse',
+	'type_email_disponible_description' => '&Uuml;berpr&uuml;ft ob die Mailadresse bereits von einem anderen Nutzer des System verwendet wird.',
+	'type_entier' => 'Ganzzahl',
+	'type_entier_description' => '&Uuml;berpr&uuml;ft ob der Wert eine Ganzzahl ist; bietet die M&ouml;glichkeit, einen Bereich zwischen zwei Zahlen anzugeben.',
+	'type_regex' => 'Regul&auml;rer Ausdruck',
+	'type_regex_description' => 'Pr&uuml;ft ob der Wert mit der vorgegebenen Maske &uuml;bereinstimmt. Zur Verwendung der Masken <a href="http://www.php.net/manual/de/reference.pcre.pattern.syntax.php">lesen sie bitte die PHP Dokumentation</a>.',
+	'type_siren_siret' => 'SIREN ou SIRET', # NEW
+	'type_siren_siret_description' => 'V&eacute;rifie que la valeur est un num&eacute;ro valide du <a href="http://fr.wikipedia.org/wiki/SIREN">Syst&egrave;me d&rsquo;Identification du R&eacute;pertoire des ENtreprises</a> fran&ccedil;ais.', # NEW
+	'type_taille' => 'Gr&ouml;&szlig;e',
+	'type_taille_description' => '&Uuml;berpr&uuml;ft ob der Wert zum geforderten Minimal- oder Maximalwert pa&szlig;t.',
+	'type_telephone' => 'Num&eacute;ro de t&eacute;l&eacute;phone', # NEW
+	'type_telephone_description' => 'V&eacute;rifie que le num&eacute;ro de t&eacute;l&eacute;phone correspond &agrave; un sch&eacute;ma reconnu.', # NE
+);
+
+?>
