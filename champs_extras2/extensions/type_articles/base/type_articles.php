ab509fe1fe49dbd54409e9425ae75bebfeda98e8
xdjuj@apsulis.com <xdjuj@apsulis.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-04-10 17:40:23 +0000

    Un champ TYPE on a dit...
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@37123 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- champs_extras2/extensions/type_articles/base/type_articles.php
+++ champs_extras2/extensions/type_articles/base/type_articles.php
@@ -4,7 +4,7 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 function type_articles_declarer_champs_extras($champs = array()){
 	$champs[] = new ChampExtra(array(
 		'table' => 'article',					// Table (au sens Champs Extra 2)
-		'champ' => 'type_articles',				// Nom du champs dans la base de donnée
+		'champ' => 'type',						// Nom du champs dans la base de donnée
 		'label' => 'type_articles:titre',		// Nom dans le formulaire dans l'espace privé
 		// 'precisions'=>'',						// 
 		'obligatoire'=>'on',					// Champs obligatoire ?
