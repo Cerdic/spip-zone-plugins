2c861477fc7d9dfdf135b8b8d3dd32bac218c7e3
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-04-12 09:22:08 +0000

    On ajoute un tout petit javascript et une toute petite CSS à la fois dans le privé et dans le public, pour gérer automatiquement les fieldsets pliables.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@37158 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ saisies/saisies_pipelines.php
@@ -0,0 +1,22 @@
+<?php
+
+// Sécurité
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+function saisies_header_prive($flux){
+	$js = find_in_path('javascript/saisies.js');
+	$css = generer_url_public('saisies.css');
+	$flux .= "\n<script type='text/javascript' src='$js'></script>\n";
+	$flux .= "\n<link rel='stylesheet' href='$css' type='text/css' />\n";
+	return $flux;
+}
+
+function saisies_insert_head($flux){
+	$js = find_in_path('javascript/saisies.js');
+	$css = generer_url_public('saisies.css');
+	$flux .= "\n<script type='text/javascript' src='$js'></script>\n";
+	$flux .= "\n<link rel='stylesheet' href='$css' type='text/css' media='projection, screen, tv' />\n";
+	return $flux;
+}
+
+?>
