43765a2548358f318086ae1ed8473f3a288f7ccd
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-04-14 21:09:06 +0000

    Completude des select Validation XHTML C'est une premiere correction pas forcément parfaite d'un point de vue HTML
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@37277 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- langonet/langonet_options.php
+++ langonet/langonet_options.php
@@ -17,7 +17,20 @@
 // $err_d  => affecte si pas de choix de repertoire
 function langonet_creer_selects($sel_l = '0', $sel_d = '0', $err_l, $err_d) {
 	include_spip('inc/plugin');
-	$rep_normal = liste_plugin_files();
+
+	// Recuperation des repertoires plugins possedant un dossier lang/
+	$rep_plugins = liste_plugin_files();
+	$rep_normal = array();
+	foreach ($rep_plugins as $rep) {
+		$reel_dir = _DIR_PLUGINS . $rep;
+		$rep_normal[] = $rep;
+		if ($sous_rep = glob($reel_dir.'/*/lang', GLOB_ONLYDIR)) {
+			for ($i = 0; $i < count($sous_rep); $i++) {
+    			$rep_normal[] = str_replace(_DIR_PLUGINS, '', str_replace('/lang', '', $sous_rep[$i]));
+			}
+		}
+	}
+	// Recuperation des repertoires SPIP et squelettes
 	if (strlen($GLOBALS['dossier_squelettes'])) {
 		$rep_complet = explode(':', $GLOBALS['dossier_squelettes']);
 	}
@@ -28,7 +41,7 @@ function langonet_creer_selects($sel_l = '0', $sel_d = '0', $err_l, $err_d) {
 	$rep_complet[] = 'prive';
 	$rep_complet[] = 'squelettes-dist';
 	$rep_scan = array_merge($rep_complet, $rep_normal);
-
+	
 	// construction des <select>
 	$sel_lang = '<p class='.($err_l?'"erreur_message"':'"explication"').'>'.
 				"\n"._T('langonet:message_choisir_langue')."</p>\n".
@@ -75,6 +88,7 @@ function langonet_creer_selects($sel_l = '0', $sel_d = '0', $err_l, $err_d) {
 				$sel_lang .= ($value == $sel_l) ? '" selected="selected">' : '">';
 				$sel_lang .= str_replace('.php', '', str_replace($reel_dir . '/lang/', '', $le_module)) . '</option>' . "\n";
 			}
+			$sel_lang .= '</optgroup>' . "\n";
 		}
 		$sel_dossier .= '<option value="' . $ou_fichier;
 		$sel_dossier .= ($sel_d == $ou_fichier) ? '" selected="selected">' : '">';
