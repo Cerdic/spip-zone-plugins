d08de83081d893ddc2e69367dbab0ad35b237b94
joseph@larmarange.net <joseph@larmarange.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-04-14 14:24:43 +0000

    seul le webmestre a les droits
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@37256 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- noiZetier/noizetier_autoriser.php
+++ noiZetier/noizetier_autoriser.php
@@ -9,17 +9,11 @@ function noizetier_autoriser(){}
 
 function autoriser_noizetier_configurer_dist($faire, $type, $id, $qui, $opt) {
 	$config = unserialize($GLOBALS['meta']['noizetier']);
-	if($config['webmestre_seulement']=='oui')
-		return
-			$qui['webmestre']=='oui'
-			AND $qui['statut'] == '0minirezo'
-			AND !$qui['restreint']
-			;
-	else
-		return
-			$qui['statut'] == '0minirezo'
-			AND !$qui['restreint']
-			;
+	return
+		$qui['webmestre']=='oui'
+		AND $qui['statut'] == '0minirezo'
+		AND !$qui['restreint']
+		;
 }
 
 ?>
