762f608db3a2be835b28eab477745159dfcfb512
joseph@larmarange.net <joseph@larmarange.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-04-15 22:25:41 +0000

    Ajout de la prise en compte d'une constante _NOIZETIER_RECUPERER_FOND si elle existe
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@37320 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- noiZetier/noizetier_pipelines.php
+++ noiZetier/noizetier_pipelines.php
@@ -17,21 +17,22 @@ function noizetier_header_prive($flux){
  * @return array
  */
 function noizetier_recuperer_fond($flux){
-	include_spip('inc/noizetier');
-	$fond = $flux['args']['fond'];
-	$composition = $flux['args']['contexte']['composition'];
-	// Si une composition est définie et si elle n'est pas déjà dans le fond, on l'ajoute au fond
-	// sauf s'il s'agit d'une page de type page (les squelettes page.html assurant la redirection)
-	if ($composition!='' AND noizetier_page_composition($fond)=='' AND noizetier_page_type($fond)!='page')
-		$fond .= '-'.$composition;
-	
-	if (in_array($fond,noizetier_lister_blocs_avec_noisettes())) {
-		$contexte = $flux['data']['contexte'];
-		$contexte['bloc'] = substr($fond,0,strpos($fond,'/'));
-		$complements = recuperer_fond('noizetier-generer-bloc',$contexte,array('raw'=>true));
-		$flux['data']['texte'] .= $complements['texte'];
+	if (defined('_NOIZETIER_RECUPERER_FOND')?_NOIZETIER_RECUPERER_FOND:true) {
+		include_spip('inc/noizetier');
+		$fond = $flux['args']['fond'];
+		$composition = $flux['args']['contexte']['composition'];
+		// Si une composition est définie et si elle n'est pas déjà dans le fond, on l'ajoute au fond
+		// sauf s'il s'agit d'une page de type page (les squelettes page.html assurant la redirection)
+		if ($composition!='' AND noizetier_page_composition($fond)=='' AND noizetier_page_type($fond)!='page')
+			$fond .= '-'.$composition;
+		
+		if (in_array($fond,noizetier_lister_blocs_avec_noisettes())) {
+			$contexte = $flux['data']['contexte'];
+			$contexte['bloc'] = substr($fond,0,strpos($fond,'/'));
+			$complements = recuperer_fond('noizetier-generer-bloc',$contexte,array('raw'=>true));
+			$flux['data']['texte'] .= $complements['texte'];
+		}
 	}
-	
 	return $flux;
 }
 
