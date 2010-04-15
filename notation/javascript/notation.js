12ecae335639ea46046a588e617f5646a858482a
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-04-15 17:41:03 +0000

    Les float dans une div sont difficiles à styler sans clear derrière...
    Comme ces floats sont ajoutés via js ... on insère un clear juste derrière pour pouvoir repartir sur de bonnes base à l'élément suivant
    
    Je pense que cela ne doit rien casser réellement et doit éviter de beaux hacks
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@37307 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- notation/javascript/notation.js
+++ notation/javascript/notation.js
@@ -5,7 +5,8 @@ jQuery(function(){ jQuery('input[type=radio].star').rating(); });
 jQuery('.auto-submit-star').rating({
 	required: true,
 	callback: function(value, link){
-	jQuery(this.form).submit();
+		jQuery(this.form).submit();
 	}});
-});}
+});
+jQuery('.star-rating-control').append('<span style="clear:both">&nbsp;</span>');}
 jQuery(function(){notation_init.apply(document); onAjaxLoad(notation_init);});
