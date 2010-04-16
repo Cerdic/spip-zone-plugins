f95676f349b60ad0784fbcd748dab928f263940d
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-04-16 13:23:06 +0000

    langues (licence)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@37336 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ licence/lang/licence_en.php
@@ -0,0 +1,53 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cfg_boite_licence' => 'License plugin configuration',
+	'cfg_descr_licence' => 'Licenses for your articles',
+	'cfg_lbl_licence_defaut' => 'Default license',
+	'cfg_titre_licence' => 'License',
+
+	// D
+	'description_art_libre' => 'Free Art License',
+	'description_cc_by' => 'Creative Commons - Attribution',
+	'description_cc_by_nc' => 'Creative Commons - Attribution Non-Commercial',
+	'description_cc_by_nc_nd' => 'Creative Commons - Attribution Non-Commercial No Derivatives',
+	'description_cc_by_nc_sa' => 'Creative Commons - Attribution Non-Commercial Share Alike',
+	'description_cc_by_nd' => 'Creative Commons - Attribution No Derivatives',
+	'description_cc_by_sa' => 'Creative Commons - Attribution Share Alike',
+	'description_copyright' => '&copy; copyright the article\'s author',
+	'description_gpl' => 'GPL License',
+
+	// L
+	'label_select_licence' => 'Choose a license',
+	'lien_art_libre' => 'http://artlibre.org/licence/lal/en',
+	'lien_cc_by' => 'http://creativecommons.org/licenses/by/2.0/en/',
+	'lien_cc_by_nc' => 'http://creativecommons.org/licenses/by-nc/2.0/en/',
+	'lien_cc_by_nc_nd' => 'http://creativecommons.org/licenses/by-nc-nd/2.0/en/',
+	'lien_cc_by_nc_sa' => 'http://creativecommons.org/licenses/by-nc-sa/2.0/en/',
+	'lien_cc_by_nd' => 'http://creativecommons.org/licenses/by-nd/2.0/en/',
+	'lien_cc_by_sa' => 'http://creativecommons.org/licenses/by-sa/2.0/en/',
+	'lien_gpl' => 'http://www.gnu.org/copyleft/gpl.html',
+
+	// S
+	'sans_licence' => 'No license',
+
+	// T
+	'titre_art_libre' => 'FAL',
+	'titre_cc_by' => 'CC by',
+	'titre_cc_by_nc' => 'CC by-nc',
+	'titre_cc_by_nc_nd' => 'CC by-nc-nd',
+	'titre_cc_by_nc_sa' => 'CC by-nc-sa',
+	'titre_cc_by_nd' => 'CC by-nd',
+	'titre_cc_by_sa' => 'CC by-sa',
+	'titre_copyright' => 'Copyright',
+	'titre_gpl' => 'Gnu GPL'
+);
+
+?>
---
+++ licence/lang/licence_it.php
@@ -0,0 +1,53 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cfg_boite_licence' => 'Configurazione del plugin licenza',
+	'cfg_descr_licence' => 'Licenze per i vostri articoli',
+	'cfg_lbl_licence_defaut' => 'Licenza di default',
+	'cfg_titre_licence' => 'Licenza',
+
+	// D
+	'description_art_libre' => 'Licenza Arte Libera',
+	'description_cc_by' => 'Creative Commons - Attribuzione',
+	'description_cc_by_nc' => 'Creative Commons - Attribuzione Non uso commerciale',
+	'description_cc_by_nc_nd' => 'Creative Commons - Attribuzione Non uso commerciale Non opere derivate',
+	'description_cc_by_nc_sa' => 'Creative Commons - Attribuzione Non uso commerciale Condividi allo stesso modo',
+	'description_cc_by_nd' => 'Creative Commons - Attribuzione Non opere derivate',
+	'description_cc_by_sa' => 'Creative Commons - Attribuzione Condividi allo stesso modo',
+	'description_copyright' => '&copy; copyright autore dell\'articolo',
+	'description_gpl' => 'licenza GPL',
+
+	// L
+	'label_select_licence' => 'Scegliete una licenza',
+	'lien_art_libre' => 'http://artlibre.org/',
+	'lien_cc_by' => 'http://creativecommons.org/licenses/by/2.0/it/',
+	'lien_cc_by_nc' => 'http://creativecommons.org/licenses/by-nc/2.0/it/',
+	'lien_cc_by_nc_nd' => 'http://creativecommons.org/licenses/by-nc-nd/2.0/it/',
+	'lien_cc_by_nc_sa' => 'http://creativecommons.org/licenses/by-nc-sa/2.0/it/',
+	'lien_cc_by_nd' => 'http://creativecommons.org/licenses/by-nd/2.0/it/',
+	'lien_cc_by_sa' => 'http://creativecommons.org/licenses/by-sa/2.0/it/',
+	'lien_gpl' => 'http://www.gnu.org/copyleft/gpl.html',
+
+	// S
+	'sans_licence' => 'Senza licenza',
+
+	// T
+	'titre_art_libre' => 'LAL',
+	'titre_cc_by' => 'CC by',
+	'titre_cc_by_nc' => 'CC by-nc',
+	'titre_cc_by_nc_nd' => 'CC by-nc-nd',
+	'titre_cc_by_nc_sa' => 'CC by-nc-sa',
+	'titre_cc_by_nd' => 'CC by-nd',
+	'titre_cc_by_sa' => 'CC by-sa',
+	'titre_copyright' => 'Copyright',
+	'titre_gpl' => 'Gnu GPL'
+);
+
+?>
