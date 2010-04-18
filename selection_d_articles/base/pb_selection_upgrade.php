a2c5434b41b765428e57f92aab4cba8dc10c2753
arno@rezo.net <arno@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-04-18 12:07:55 +0000

    Compat 2.1
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@37401 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- selection_d_articles/base/pb_selection_upgrade.php
+++ selection_d_articles/base/pb_selection_upgrade.php
@@ -1,7 +1,6 @@
 <?php
 
 function pb_selection_install ($action,$prefix,$version_cible) {
-	if ($action == "install") {
 		global $tables_principales;
 
 		include_spip("base/pb_selection_install");
@@ -10,10 +9,8 @@ function pb_selection_install ($action,$prefix,$version_cible) {
 		include_spip('base/create');
 		include_spip('base/abstract_sql');
 		creer_base();
-	};
-	if ($action == "test") {
+		
 		return true;
-	};
 }
 
 ?>
