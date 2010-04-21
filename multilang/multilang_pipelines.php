918f93395cb382996c7bcf2e444514c8757999f3
joseph@larmarange.net <joseph@larmarange.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-04-21 15:25:59 +0000

    Attention, le pipeline doit renvoyer tout le flux et pas seulement data. D'autres plugins peuvent utiliser ce pipeline.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@37495 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- multilang/multilang_pipelines.php
+++ multilang/multilang_pipelines.php
@@ -2,16 +2,15 @@
 
 function multilang_recuperer_fond($flux) {
   $args = $flux["args"];
-  $data = $flux["data"];
   if($args["fond"]=="prive/editer/rubrique") {
-    $data["texte"] .= "<script type=\"text/javascript\" src=\"".find_in_path("javascript/multilang.js")."\"></script>\n".
+    $flux["data"]["texte"] .= "<script type=\"text/javascript\" src=\"".find_in_path("javascript/multilang.js")."\"></script>\n".
              "<script type=\"text/javascript\">\n".
              "var multilang_avail_langs = '".$GLOBALS["meta"]["langues_multilingue"]."'.split(','),\n". 
              "multilang_def_lang = '".$GLOBALS["meta"]["langue_site"]."';\n".
              "multilang_init_lang({fields:':text,textarea',root:'div.cadre-formulaire-editer'});\n".
              "</script>\n";
   }
-  return $data;
+  return $flux;
 }
 
 ?>
