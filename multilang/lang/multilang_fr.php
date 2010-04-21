8ab1b5cb47b1c09f951533ac1085321a805db842
yffic@lefourneau.com <yffic@lefourneau.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-04-21 18:41:31 +0000

    Evolution du plugin : Tous les champs texte et textarea sont traites dans : - articles - rubriques - configuration du site - auteurs (interessant uniquement pour le champs BIO) - documents dans les colonnes de gauche des pages d'edition d'articles et de rubriques - documents dans les parties basses des pages de presentation des articles et rubriques - groupe de mots cles - mots cles - sites - Breves
    Integration dans d'autres plugins :
    - Mediatheque
    	- documents
    	- documents dans popup
    - Forms et Tables. Proposer d'utiliser ce plugin pour ne pas avoir a gerer 2 js
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@37509 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ multilang/lang/multilang_fr.php
@@ -0,0 +1,25 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	'cfg_boite_multilang' => 'Configuration du plugin Multilang',
+	'cfg_descr_multilang' => 'Multilang : Plugin de gestion des tags multi dans l\'espace priv&eacute;',
+	
+	'cfg_activer' => 'Activer Multilang sur la page d\'&eacute;dition :',
+	'cfg_lbl_siteconfig' => 'de la configuration du site',
+	'cfg_lbl_article' => 'des articles',
+	'cfg_lbl_breve' => 'des breves',
+	'cfg_lbl_rubrique' => 'des rubriques',
+	'cfg_lbl_auteur' => 'des auteurs',
+	'cfg_lbl_document' => 'des documents',
+	'cfg_lbl_motcle' => 'des mots cl&eacute;s',
+	'cfg_lbl_site' => 'des sites',
+	'cfg_lbl_formstables' => 'des forms&tables',
+
+	'cfg_effacer' => 'Re-initialiser',
+	'cfg_titre_multilang' => 'Multilang'
+	
+);
+?>
--- multilang/multilang_pipelines.php
+++ multilang/multilang_pipelines.php
@@ -1,16 +1,81 @@
 <?php
 
-function multilang_recuperer_fond($flux) {
-  $args = $flux["args"];
-  if($args["fond"]=="prive/editer/rubrique") {
-    $flux["data"]["texte"] .= "<script type=\"text/javascript\" src=\"".find_in_path("javascript/multilang.js")."\"></script>\n".
-             "<script type=\"text/javascript\">\n".
-             "var multilang_avail_langs = '".$GLOBALS["meta"]["langues_multilingue"]."'.split(','),\n". 
-             "multilang_def_lang = '".$GLOBALS["meta"]["langue_site"]."';\n".
-             "multilang_init_lang({fields:':text,textarea',root:'div.cadre-formulaire-editer'});\n".
-             "</script>\n";
-  }
-  return $flux;
+function multilang_insert_head_prive($flux){
+
+	// Si la config existe deja, on ne l'ecrase pas
+	// Si evolution de la config, les nouveaux champs sont rajoutes
+	$config = lire_config('multilang');
+	if (!is_array($config)) {
+		$config = array();
+	}
+	$config = array_merge(array(
+			'siteconfig' => 'on',
+			'article' => '',
+			'breve' => '',
+			'rubrique' => 'on',
+			'auteur' => 'on',
+			'document' => 'on',
+			'motcle' => '',
+			'site' => '',
+			'formstables' => ''
+	), $config);
+	ecrire_config('multilang', serialize($config));
+
+	// Insertion de la css
+	$flux .= "\n".'<link rel="stylesheet" href="'.url_absolue(find_in_path('css/multilang.css')).'" type="text/css" media="all" />';
+	$exec = _request('exec') ;
+	if($config['siteconfig'] && $exec=='configuration'){
+		$flux .= multilang_traiter('div#configurer-accueil') ; // Config Site
+	} else if($config['article'] && $exec=='articles_edit') { // Articles
+		$flux .= multilang_traiter('div.cadre-formulaire-editer') ;
+	} else if($config['breve'] && $exec=='breves_edit') { // Breves
+		$flux .= multilang_traiter('div.cadre-formulaire-editer') ;
+	} else if($config['rubrique'] && $exec=='rubriques_edit') { // Rubriques
+		$flux .= multilang_traiter('div.cadre-formulaire-editer') ;
+	} else if($config['auteur'] && $exec=='auteur_infos') { // Auteurs
+		$flux .= multilang_traiter('div.cadre-formulaire-editer') ;
+	} else if($config['document'] && ($exec=='naviguer' ||
+																	$exec=='articles')) {  // Docs dans page de presentation rubrique ou article,
+		$flux .= multilang_traiter('div#portfolio,div#documents,div.formulaire_editer_document') ; //avec ou sans Mediatheque
+	} else if($config['document'] && $exec=='documents_edit') {// Mediatheque document
+		$flux .= multilang_traiter('div.formulaire_editer_document') ;
+	} else if($config['site'] && $exec=='sites_edit') { // Sites
+		$flux .= multilang_traiter('div.cadre-formulaire-editer') ;
+	} else if($config['motcle'] && ($exec=='mots_type' ||
+																 $exec=='mots_edit')) { // Mots
+		$flux .= multilang_traiter('div.cadre-formulaire-editer') ;
+	} else if($config['formstables'] && $exec=='forms_edit'){
+		$flux .= multilang_traiter('div#champs') ; // Création d'un formulaire
+	} else if($config['formstables'] && $exec=='donnees_edit'){
+		$flux .= multilang_traiter('div.spip_forms') ; // Remplissage d'un formulaire
+	}
+	// Docs traites a part dans pages d'edition d'articles et de rubriques
+	if($config['document'] && ($exec=='rubriques_edit' || $exec=='articles_edit')){
+		$flux .= multilang_traiter('div#liste_documents,div.formulaire_editer_document') ; // avec ou sans Mediatheque
+	}
+	
+	return $flux;
 }
 
+function multilang_traiter($obj){
+
+	// Appel de multilang_init_lang si
+	// - document.ready 
+	// - onAjaxLoad (cas des docs et de la configuration du site)
+
+	$out = '<script type="text/javascript" src="'.find_in_path("javascript/multilang.js").'"></script>
+			  <script type="text/javascript">
+			  var multilang_avail_langs = "'.$GLOBALS["meta"]["langues_multilingue"].'".split(\',\'),
+			  multilang_def_lang = "'.$GLOBALS["meta"]["langue_site"].'";
+			  jQuery(document).ready(function(){
+					function multilang_init(){
+						multilang_init_lang({fields:":text,textarea",root:"'.$obj.'"});
+					} 
+					multilang_init();
+					if(typeof onAjaxLoad == "function") onAjaxLoad(multilang_init);
+			  }); 
+			  </script>' ;
+
+	return $out;
+}
 ?>
