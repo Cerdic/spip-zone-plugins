32748ef0519fcd51bd5e7e2949ec5632d4f45d95
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-04-21 19:44:29 +0000

    La page Mes préférences du plugin Bandeau n'a pas un comportement habituel pour la prise en compte des changements dans le formulaire.
    Du coup, on avait autant de demande de confirmation avant de pouvoir quitter la page.
    
    Idéalement, il faudrait pouvoir mettre une liste de page à exclure.
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@37512 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- protection_formulaires/protection_formulaires.php
+++ protection_formulaires/protection_formulaires.php
@@ -1,11 +1,8 @@
 <?php
-
 function protection_formulaires_proteger($data) {
-	$ret = '<script src="'._DIR_PLUGIN_PROTECTION_FORMULAIRES.'/javascript/proteger_formulaires.js" type="text/javascript"></script>';
-
-
+	$ret = '';
+	if (_request('exec') != 'config_preferences') 
+		$ret = '<script src="'._DIR_PLUGIN_PROTECTION_FORMULAIRES.'/javascript/proteger_formulaires.js" type="text/javascript"></script>';
 	return $data.$ret;
-
 }
-
 ?>
