1a48bc8add80aebc5076b336b88121b73baf32e2
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-04-23 13:34:05 +0000

    Une ligne de titre pour le CSV et on affiche les vraies valeurs humainement lisibles.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@37562 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- formidable/action/exporter_formulaires_reponses.php
+++ formidable/action/exporter_formulaires_reponses.php
@@ -21,7 +21,20 @@ function action_exporter_formulaires_reponses_dist($arg=null) {
 		and $formulaire = sql_fetsel('*','spip_formulaires','id_formulaire = '.$id_formulaire)
 		and $reponses = sql_allfetsel('*', 'spip_formulaires_reponses', 'id_formulaire = '.$id_formulaire.' and statut = '.sql_quote('publie'))
 	) {
+		include_spip('inc/saisies');
+		include_spip('classes/facteur');
+		include_spip('inc/filtres');
 		$reponses_completes = array();
+		
+		// La première ligne des titres
+		$titres = array(_T('date'), _T('formidable:reponses_auteur'), _T('formidable:reponses_ip'));
+		$saisies = saisies_lister_par_nom(unserialize($formulaire['saisies']), false);
+		foreach ($saisies as $nom=>$saisie){
+			$options = $saisie['options'];
+			$titres[] = sinon($options['label_case'], sinon($options['label'], $nom));
+		}
+		$reponses_completes[] = $titres;
+		
 		// On parcourt chaque réponse
 		foreach ($reponses as $reponse){
 			// Est-ce qu'il y a un auteur avec un nom
@@ -35,17 +48,27 @@ function action_exporter_formulaires_reponses_dist($arg=null) {
 			$reponse_complete = array($reponse['date'], $nom_auteur, $reponse['ip']);
 			
 			// Ensuite tous les champs
-			include_spip('inc/saisies');
-			$champs = saisies_lister_champs(unserialize($formulaire['saisies']), false);
-			foreach ($champs as $nom){
+			foreach ($saisies as $nom=>$saisie){
 				$valeur = sql_getfetsel(
 					'valeur',
 					'spip_formulaires_reponses_champs',
 					'id_formulaires_reponse = '.intval($reponse['id_formulaires_reponse']).' and nom = '.sql_quote($nom)
 				);
 				if (is_array(unserialize($valeur)))
-					$valeur = join(', ', unserialize($valeur));
-				$reponse_complete[] = $valeur;
+					$valeur = unserialize($valeur);
+				$reponse_complete[] = Facteur::html2text(
+					recuperer_fond(
+						'saisies-vues/_base',
+						array_merge(
+							array(
+								'valeur_uniquement' => 'oui',
+								'type_saisie' => $saisie['saisie'],
+								'valeur' => $valeur
+							),
+							$saisie['options']
+						)
+					)
+				);
 			}
 			
 			// On ajoute la ligne à l'ensemble des réponses
@@ -53,18 +76,10 @@ function action_exporter_formulaires_reponses_dist($arg=null) {
 		}
 		
 		if ($reponses_completes and $exporter_csv = charger_fonction('exporter_csv', 'inc/', true)){
-			echo $exporter_csv("export-form-$id_formulaire", $reponses_completes);
+			echo $exporter_csv('reponses-formulaire-'.$formulaire['identifiant'], $reponses_completes);
 			exit();
 		}
 	}
-
-	if (_request('redirect')) {
-		$redirect = urldecode(_request('redirect'));
-		include_spip('inc/headers');
-		redirige_par_entete($redirect);
-	}
-	else
-		return $ok;
 }
 
 ?>
