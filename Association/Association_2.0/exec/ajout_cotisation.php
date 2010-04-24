874be8705ecd0f25256adc80ab52c5d4901497a7
esj@rezo.net <esj@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-04-24 09:25:19 +0000

    Association: un commencer_page dans un While, faut oser.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@37610 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- Association/Association_2.0/exec/ajout_cotisation.php
+++ Association/Association_2.0/exec/ajout_cotisation.php
@@ -16,7 +16,7 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 	
 	include_spip ('inc/navigation_modules');
 
-	function exec_ajout_cotisation(){
+function exec_ajout_cotisation(){
 		
 		include_spip('inc/autoriser');
 		if (!autoriser('configurer')) {
@@ -29,6 +29,26 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 		$url_retour = $_SERVER['HTTP_REFERER'];
 		
 		$id_auteur=intval($_GET['id']);
+			
+		//debut_page(_T('Ajout de cotisation'), "", "");
+		$commencer_page = charger_fonction('commencer_page', 'inc');
+		echo $commencer_page(_T('Ajout de cotisation')) ;
+		association_onglets();
+			
+		echo debut_gauche("",true);
+			
+		echo debut_boite_info(true);
+		
+		echo '<p>';
+		echo 'Adh&eacute;rent :<strong>'.$nom_famille.' '.$prenom.'</strong><br />';
+		echo 'Cat&eacute;gorie :<strong>'.$categorie.'</strong></p>';
+		association_date_du_jour();	
+		echo fin_boite_info(true);
+			
+		echo debut_droite("",true);
+
+		echo debut_cadre_relief(  "", false, "", $titre = _T('asso:Nouvelle cotisation'));
+
 		$query = association_auteurs_elargis_select("*",'', "id_auteur=$id_auteur");
 		while($data = spip_fetch_array($query)) {
 			$nom_famille=$data['nom_famille'];
@@ -39,31 +59,12 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 			$annee = $split[0]; 
 			$mois = $split[1]; 
 			$jour = $split[2]; 
-			
-			//debut_page(_T('Ajout de cotisation'), "", "");
-			 $commencer_page = charger_fonction('commencer_page', 'inc');
-		echo $commencer_page(_T('Ajout de cotisation')) ;
-			association_onglets();
-			
-			echo debut_gauche("",true);
-			
-			echo debut_boite_info(true);
-				
-			echo '<p>';
-			echo 'Adh&eacute;rent :<strong>'.$nom_famille.' '.$prenom.'</strong><br />';
-			echo 'Cat&eacute;gorie :<strong>'.$categorie.'</strong></p>';
-			association_date_du_jour();	
-			echo fin_boite_info(true);
-			
-			echo debut_droite("",true);
-			
-			echo debut_cadre_relief(  "", false, "", $titre = _T('asso:Nouvelle cotisation'));
-			
+
 			echo '<form action="'.$url_action_cotisations.'" method="POST">';
 			echo '<label for="date"><strong>'._T('asso:Date du paiement (AAAA-MM-JJ)').' :</strong></label>';
 			echo '<input name="date" type="text" value="'.date('Y-m-d').'" id="date" class="formo" />';
 			echo '<label for="montant"><strong>'._T('asso:Montant paye (en euros)').' :</strong></label>';
-			$sql = spip_query( "SELECT * FROM spip_asso_categories WHERE id_categorie='$categorie' ");
+			$sql = sql_select("*", "spip_asso_categories", "id_categorie='$categorie' ");
 			while($categorie = spip_fetch_array($sql)) {
 				$duree=$categorie['duree'];
 				$mois=$mois+$duree;
@@ -98,6 +99,6 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 		echo '</fieldset>';
 		
 		echo fin_cadre_relief(true);  
-		//fin_page();
-	}
+		echo fin_gauche(), fin_page();
+}
 ?>
