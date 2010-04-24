d6923a0efae451d8a852e1cfc78a043e046a23e5
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-04-24 10:35:24 +0000

    Une API pour SaveCFG permettant de créer une sauvegarde depuis un script.
    Application dans le prochain dépôt sur SoyezCreateurs
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@37615 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- savecfg/formulaires/sauvegarder_savecfg.php
+++ savecfg/formulaires/sauvegarder_savecfg.php
@@ -13,22 +13,15 @@ function formulaires_sauvegarder_savecfg_verifier(){
 	return $erreurs;
 }
 function formulaires_sauvegarder_savecfg_traiter() {
-	$message = sauvegarder_savecfg(_request('fond'), _request('titre'));
-	return $message;
-}
-function sauvegarder_savecfg($fond, $titre) {
+	$fond = _request('fond');
 	if (sql_countsel('spip_meta', 'nom='.sql_quote($fond)) == 1) {
 		$sfg = sql_getfetsel('valeur', 'spip_meta', 'nom='.sql_quote($fond));
-		// Insert ou Update ?
-		$id_savecfg = sql_getfetsel('id_savecfg', 'spip_savecfg', 'titre='.sql_quote($titre).' AND fond='.sql_quote($fond));
-		if ($id_savecfg > 0) { // Update
-			sql_updateq('spip_savecfg', array( 'valeur' => $sfg, 'date' => date('Y-m-d H:m:s')), 'id_savecfg='.$id_savecfg);
-			return _T('savecfg:miseajour_ok',array('titre'=>$titre));
-		} else { // Insert
-			sql_insertq('spip_savecfg', array('id_savecfg' => '', 'fond' => $fond, 'valeur' => $sfg, 'titre' => $titre, 'date' => date('Y-m-d H:m:s')));
-			return _T('savecfg:sauvegarde_ok',array('titre'=>$titre));
-		}
+
+		include_spip('inc/sauvegarder_savecfg');
+		$message = sauvegarder_savecfg($fond, _request('titre'), $sfg);
+	} else {
+		$message = _T('savecfg:sauvegarde_pas_ok');
 	}
-	return _T('savecfg:sauvegarde_pas_ok');
+	return $message;
 }
 ?>
