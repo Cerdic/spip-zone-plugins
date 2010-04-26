dedab555ad8f0ad4ef3ce77929e539c9935700e3
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-04-26 21:24:40 +0000

    langues (comments)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@37715 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ comments/comments-200/lang/comments_ca.php
@@ -0,0 +1,47 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'comment' => 'comentari',
+	'comments' => 'comentaris',
+	'comments_h' => 'Els teus comentaris',
+
+	// D
+	'date_heure_a' => 'a',
+	'date_jour_le' => 'El',
+
+	// F
+	'forum_longueur' => 'Llargada dels missatges',
+	'forum_longueur_maxi' => 'Llargada m&agrave;xima',
+	'forum_longueur_mini' => 'Llargada m&iacute;nima',
+	'forum_qui_etes_vous' => 'Qui ets?',
+
+	// L
+	'label_email' => 'Correu electr&ograve;nic (no&nbsp;publicat)',
+	'label_nom' => 'Nom',
+	'label_url' => 'El teu lloc Web',
+
+	// M
+	'moderation_info' => 'Atenci&oacute;, el teu missatge no apareixer&agrave; fins que no hagi estat rellegit i aprovat. ',
+
+	// P
+	'permalink_to' => 'Enlla&ccedil; permanent cap el comentari',
+
+	// R
+	'reponse_comment_modere' => 'El teu comentari s\'ha enregistrat correctament i est&agrave; esperant la relectura abans de ser publicat.',
+	'reponse_comment_ok' => 'Gr&agrave;cies pel comentari!',
+
+	// S
+	'saisie_texte_info' => 'Aquest formulari accepta les dreceres SPIP <code>[-&gt;url] {{italique}} {italique} &lt;quote&gt; &lt;code&gt;</code> i el codi HTML <code>&lt;q&gt; &lt;del&gt; &lt;ins&gt;</code>. Per crear par&agrave;grafs, deixeu simplement l&iacute;nies buides.',
+	'saisie_texte_legend' => 'Afegeix el comentari aqu&iacute;',
+	'submit1' => 'Visualitzaci&oacute; pr&egrave;via',
+	'submit2' => 'Confirmar l\'enviament'
+);
+
+?>
---
+++ comments/comments-200/lang/comments_fr_tu.php
@@ -0,0 +1,47 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'comment' => 'commentaire',
+	'comments' => 'commentaires',
+	'comments_h' => 'Tes commentaires',
+
+	// D
+	'date_heure_a' => '&agrave;',
+	'date_jour_le' => 'Le',
+
+	// F
+	'forum_longueur' => 'Longueur des messages',
+	'forum_longueur_maxi' => 'Longueur maximale',
+	'forum_longueur_mini' => 'Longueur minimale',
+	'forum_qui_etes_vous' => 'Qui es-tu&nbsp;?',
+
+	// L
+	'label_email' => 'Courriel (non&nbsp;publi&eacute;)',
+	'label_nom' => 'Nom',
+	'label_url' => 'Ton site web',
+
+	// M
+	'moderation_info' => 'Attention, ton message n\'appara&icirc;tra qu\'apr&egrave;s avoir &eacute;t&eacute; relu et approuv&eacute;.',
+
+	// P
+	'permalink_to' => 'Lien permanent vers le commentaire',
+
+	// R
+	'reponse_comment_modere' => 'Ton commentaire a bien &eacute;t&eacute; enregistr&eacute; et est en attente de relecture avant publication.',
+	'reponse_comment_ok' => 'Merci pour ton commentaire&nbsp;!',
+
+	// S
+	'saisie_texte_info' => 'Ce formulaire accepte les raccourcis&nbsp;SPIP <code>[-&gt;url] {{gras}} {italique} &lt;quote&gt; &lt;code&gt;</code> et le code&nbsp;HTML <code>&lt;q&gt; &lt;del&gt; &lt;ins&gt;</code>. Pour cr&eacute;er des paragraphes, laisse simplement des lignes vides.',
+	'saisie_texte_legend' => 'Ajoute ton commentaire ici',
+	'submit1' => 'Pr&eacute;visualiser',
+	'submit2' => 'Confirmer l\'envoi'
+);
+
+?>
