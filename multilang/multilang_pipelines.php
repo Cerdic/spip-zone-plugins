e15fc2ef755a564f1b07c49168c0bba818b68808
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-04-26 13:04:29 +0000

    L'installation se fait dans les fonctions d'installation adéquates et pas dans le pipeline header_prive
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@37687 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- multilang/multilang_pipelines.php
+++ multilang/multilang_pipelines.php
@@ -1,30 +1,20 @@
 <?php
 
+/**
+ * Insertion dans le pipeline insert_head_prive
+ * Ajoute css et javascript dans le <head> privé
+ *
+ * @param string $flux Le contenu du head privé
+ */
 function multilang_insert_head_prive($flux){
-
-	// Si la config existe deja, on ne l'ecrase pas
-	// Si evolution de la config, les nouveaux champs sont rajoutes
 	$config = lire_config('multilang');
-	if (!is_array($config)) {
-		$config = array();
-	}
-	$config = array_merge(array(
-			'siteconfig' => 'on',
-			'article' => '',
-			'breve' => '',
-			'rubrique' => 'on',
-			'auteur' => 'on',
-			'document' => 'on',
-			'motcle' => '',
-			'site' => ''
-	), $config);
-	ecrire_config('multilang', serialize($config));
+
 
 	// Insertion de la css
 	$flux .= "\n".'<link rel="stylesheet" href="'.url_absolue(find_in_path('css/multilang.css')).'" type="text/css" media="all" />';
 	$exec = _request('exec') ;
 	$root = '' ;
-	
+
 	if($config['siteconfig'] && $exec=='configuration'){
 		$root .= 'div#configurer-accueil' ; // Config Site
 	} else if($config['article'] && $exec=='articles_edit') { // Articles
@@ -35,13 +25,11 @@ function multilang_insert_head_prive($flux){
 		$root .= 'div.cadre-formulaire-editer' ;
 	} else if($config['auteur'] && $exec=='auteur_infos') { // Auteurs
 		$root .= 'div.cadre-formulaire-editer' ;
-	} else if($config['document'] && ($exec=='naviguer' ||
-												 $exec=='articles')) {  // Docs dans page de presentation rubrique ou article,
-		$root .= 'div#portfolio_portfolio,div#portfolio_documents' ; 
+	} else if($config['document'] && ($exec=='naviguer' || $exec=='articles')) {  // Docs dans page de presentation rubrique ou article,
+		$root .= 'div#portfolio_portfolio,div#portfolio_documents' ;
 	} else if($config['site'] && $exec=='sites_edit') { // Sites
 		$root .= 'div.cadre-formulaire-editer' ;
-	} else if($config['motcle'] && ($exec=='mots_type' ||
-																 $exec=='mots_edit')) { // Mots
+	} else if($config['motcle'] && ($exec=='mots_type' || $exec=='mots_edit')) { // Mots
 		$root .= 'div.cadre-formulaire-editer' ;
 	}
 
@@ -49,9 +37,9 @@ function multilang_insert_head_prive($flux){
 	if($config['document'] && ($exec=='rubriques_edit' || $exec=='articles_edit')){
 		$root .= ',div#liste_documents,div.formulaire_editer_document' ; // avec ou sans Mediatheque
 	}
-	
+
 	// Appel de multilang_init_lang si
-	// - document.ready 
+	// - document.ready
 	// - onAjaxLoad (cas des docs et de la configuration du site)
 
 	$flux .= '<script type="text/javascript" src="'.find_in_path("javascript/multilang.js").'"></script>
@@ -61,24 +49,22 @@ function multilang_insert_head_prive($flux){
 			  dir_plugin = "'._DIR_PLUGIN_MULTILANG.'";
 			  jQuery(document).ready(function(){
 					function multilang_init(){
-						
+
 				';
 	if($root) {
-		$flux .= '      multilang_init_lang({fields:":text,textarea",root:"'.$root.'"});
-					';
+		$flux .= 'multilang_init_lang({fields:":text,textarea",root:"'.$root.'"});';
 	}
+
 	// Pour toutes les forms de class multilang (pour les autres plugins)
-	$flux .= '     forms_selector = $(".multilangclass").parents("form") ;
+	$flux .= 'forms_selector = $(".multilangclass").parents("form") ;
 					   multilang_init_lang({fields:".multilangclass",forms:forms_selector});
 					} // end multilang_init
 					multilang_init();
 					if(typeof onAjaxLoad == "function") onAjaxLoad(multilang_init);
-			  }); 
+			  });
 			  </script>
 			  ' ;
 
 	return $flux;
 }
-
-
 ?>
