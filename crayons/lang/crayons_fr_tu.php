ebaaa750f43632c38ac9082a3bec830a13d2d490
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-04-26 21:24:46 +0000

    langues (crayons)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@37718 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- crayons/lang/crayons_fr_tu.php
+++ crayons/lang/crayons_fr_tu.php
@@ -8,7 +8,7 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
-	'activation_barre_impossible' => 'Un autre plugin interdit l\'usage du porte-plume dans l\'espace public.', # NEW
+	'activation_barre_impossible' => 'Un autre plugin interdit l\'usage du porte-plume dans l\'espace public.',
 	'annuler' => 'Annuler',
 
 	// D
