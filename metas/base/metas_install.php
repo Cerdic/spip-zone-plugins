6b2c64e8bdd1a57fd165064eb88b43e32d12e239
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-04-26 08:21:32 +0000

    reparer l'installation sous SPIP 2.0.x et virer du code inutile qui cassait la compat.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@37676 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- metas/base/metas_install.php
+++ metas/base/metas_install.php
@@ -3,43 +3,21 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 
 include_spip('inc/meta');
 
-function metas_install($action){
-	include_spip('inc/plugin');
-
-	//recupère les informations de plugin.xml
-	$infos = charger_fonction('get_infos','plugins');
-	$mes_infos = $infos('metas', false, '_DIR_PLUGIN_METAS');
-	$version_cible = $mes_infos['version'];
-
-	switch ($action){
-		case 'test':
-			//Appel de la fonction d'installation. Lors du clic sur l'icône depuis le panel.
-			//quand le plugin est activé et test retourne false
-			$current_version = 0.0;
-			if ((!isset($GLOBALS['meta']['spip_metas_version'])) || (($current_version = $GLOBALS['meta']['spip_metas_version'])!=$version_cible))
-				return false;
-			else
-				return true;
-		break;
-		case 'install':
-			//Appel de la fonction d'installation. Lors du clic sur l'icône depuis le panel.
-			//quand le plugin est activé et test retourne false
-			metas_upgrade('spip_metas_version', $version_cible);
-		break;
-		case 'uninstall':
-			//Appel de la fonction de suppression
-			//quand l'utilisateur clique sur "supprimer tout" (disponible si test retourne true)
-			metas_vider_tables();
-		break;
-	}
-}
-
 // fonction d'installation, mise a jour de la base
 function metas_upgrade($nom_meta_base_version, $version_cible){
-	$current_version = 0.0;
-	if ((!isset($GLOBALS['meta']['spip_metas_version']))
-			|| (($current_version = $GLOBALS['meta']['spip_metas_version'])!=$version_cible)){
-		include_spip('base/metas');
+	include_spip('inc/meta');
+	// migration depuis l'ancien systeme de maj
+	if (isset($GLOBALS['meta']['spip_metas_version'])
+		AND !isset($GLOBALS['meta'][$nom_meta_base_version])){
+		ecrire_meta($nom_meta_base_version,$GLOBALS['meta']['spip_metas_version'],'non');
+		effacer_meta('spip_metas_version');
+	}
+	
+	$current_version = '0.0';
+	if ((!isset($GLOBALS['meta'][$nom_meta_base_version]))
+			|| (($current_version = $GLOBALS['meta'][$nom_meta_base_version])!=$version_cible)){
+		include_spip('base/serial');
+		include_spip('base/aux');
 		if (version_compare($current_version,'0.0','<=')){
 			include_spip('base/create');
 			include_spip('base/abstract_sql');
@@ -47,7 +25,7 @@ function metas_upgrade($nom_meta_base_version, $version_cible){
 			// ou ajoute des champs declares, manquants
 			creer_base();
 			echo "Installation du plugin M&eacute;tas effectu&eacute;e correctement !<br/>";
-			ecrire_meta('spip_metas_version',$current_version=$version_cible,'non');
+			ecrire_meta($nom_meta_base_version,$current_version=$version_cible,'non');
 		}
 	}
 }
@@ -56,10 +34,10 @@ function metas_upgrade($nom_meta_base_version, $version_cible){
 function metas_vider_tables($nom_meta_base_version) {
 	sql_drop_table("spip_metas");
 	sql_drop_table("spip_metas_liens");
-	effacer_meta('spip_metas_version');
 	effacer_meta('spip_metas_title');
 	effacer_meta('spip_metas_description');
 	effacer_meta('spip_metas_mots_importants');
 	effacer_meta('spip_metas_mots_keywords');
+	effacer_meta($nom_meta_base_version);
 }
 ?>
