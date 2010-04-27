ca609ece5e6c5d0c5f96c5e36a906d1c7003dc11
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-04-27 11:28:49 +0000

    On ne se prend pas la tête à maintenir deux fonctions qui au final sont plus ou moins identiques
    La class pour ajouter de nouveaux formulaire est multilang / pour ne pas prendre en compte un input ou textarea, il faut lui donner la class .nomulti
    
    Une traduction en anglais rapide pour tester l'objet js de langue
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@37740 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- multilang/multilang_pipelines.php
+++ multilang/multilang_pipelines.php
@@ -7,7 +7,37 @@
  * @param string $flux Le contenu du head privé
  */
 function multilang_insert_head_prive($flux){
-	$config = lire_config('multilang');
+	$config = lire_config('multilang',array());
+
+	$flux = multilang_inserer_head($flux,$config);
+
+	return $flux;
+}
+
+/**
+ * Insertion dans le pipeline insert_head
+ * si on a configuré multilang pour s'insérer dans l'espace public
+ *
+ * @param $flux
+ */
+function multilang_insert_head($flux){
+	$config = lire_config('multilang',array());
+
+	if($config['multilang_public'] == 'on'){
+		$flux = multilang_inserer_head($flux,$config);
+	}
+
+	return $flux;
+}
+
+/**
+ * La fonction de modification du $flux pour l'insertion dans le head qu'il
+ * soit privé ou public
+ *
+ * @param string $flux Le head de la page où l'on se trouve
+ * @param array $config La configuration du plugin
+ */
+function multilang_inserer_head($flux,$config=array()){
 
 	// Insertion de la css
 	$flux .= "\n".'<link rel="stylesheet" href="'.url_absolue(find_in_path('css/multilang.css')).'" type="text/css" media="all" />';
@@ -58,89 +88,18 @@ function multilang_insert_head_prive($flux){
 
 				';
 	if($root) {
-		$flux .= 'multilang_init_lang({fields:"textarea,input:text:not(input#id_parent,input.password,input#new_login,#titreparent)",root:"'.$root.'"});';
+		$flux .= 'multilang_init_lang({fields:"textarea,input:text:not(input#id_parent,input.password,input#new_login,#titreparent,*.nomulti)",root:"'.$root.'"});';
 	}
 
 	// Pour toutes les forms de class multilang (pour les autres plugins)
-	$flux .= 'forms_selector = $(".multilangclass").parents("form") ;
-					   multilang_init_lang({fields:".multilangclass",forms:forms_selector});
+	$flux .= 'forms_selector = $(".multilang").parents("form") ;
+					   multilang_init_lang({fields:".multilang",forms:forms_selector});
 					} // end multilang_init
 					multilang_init();
 					if(typeof onAjaxLoad == "function") onAjaxLoad(multilang_init);
 			  });
 			  </script>
-			  ' ;
-
-	return $flux;
-}
-
-function multilang_insert_head($flux){
-	$config = lire_config('multilang');
-
-	if($config['multilang_public'] == 'on'){
-		// Insertion de la css
-		$flux .= "\n".'<link rel="stylesheet" href="'.url_absolue(find_in_path('css/multilang.css')).'" type="text/css" media="all" />';
-		$root = '' ;
-
-		if($config['siteconfig']){
-			$root .= 'div#configurer-accueil' ; // Config Site
-		}
-		if($config['article']) { // Articles
-			$root .= ',div.formulaire_editer_article' ;
-		}
-		if($config['breve']) { // Breves
-			$root .= ',div.formulaire_editer_breve' ;
-		}
-		if($config['rubrique']) { // Rubriques
-			$root .= ',div.formulaire_editer_rubrique' ;
-		}
-		if($config['auteur']) { // Auteurs
-			$root .= ',div.formulaire_editer_auteur';
-		}
-		if($config['document']) {  // Docs dans page de presentation rubrique ou article,
-			$root .= ',div#portfolio_portfolio,div#portfolio_documents' ;
-		}
-		if($config['site']) { // Sites
-			$root .= ',div.cadre-formulaire-editer' ;
-		}
-		if($config['motcle']) { // Mots
-			$root .= ',div.formulaire_editer_groupe_mot,div.formulaire_editer_mot' ;
-		}
-
-		// Docs traites a part dans pages d'edition d'articles et de rubriques
-		if($config['document']){
-			$root .= ',div#liste_documents,div.formulaire_editer_document' ; // avec ou sans Mediatheque
-		}
-
-		// Appel de multilang_init_lang si
-		// - document.ready
-		// - onAjaxLoad (cas des docs et de la configuration du site)
-
-		$flux .= '<script type="text/javascript" src="'.generer_url_public("multilang_lang.js").'"></script>
-				  <script type="text/javascript" src="'.find_in_path("javascript/multilang.js").'"></script>
-				  <script type="text/javascript">
-				  var multilang_avail_langs = "'.$GLOBALS["meta"]["langues_multilingue"].'".split(\',\'),
-				  multilang_def_lang = "'.$GLOBALS["meta"]["langue_site"].'",
-				  dir_plugin = "'._DIR_PLUGIN_MULTILANG.'";
-				  jQuery(document).ready(function(){
-						function multilang_init(){
-
-					';
-		if($root) {
-			$flux .= 'multilang_init_lang({fields:":text,textarea",root:"'.$root.'"});';
-		}
-
-		// Pour toutes les forms de class multilang (pour les autres plugins)
-		$flux .= 'forms_selector = $(".multilangclass").parents("form") ;
-						   multilang_init_lang({fields:".multilangclass",forms:forms_selector});
-						} // end multilang_init
-						multilang_init();
-						if(typeof onAjaxLoad == "function") onAjaxLoad(multilang_init);
-				  });
-				  </script>
-				  ' ;
-	}
-
+			  ';
 	return $flux;
 }
 ?>
