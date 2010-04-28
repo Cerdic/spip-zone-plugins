103a62473612b963520c98fd1c14357dec674bee
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-04-28 21:03:44 +0000

    Une partie de configuration un peu plus moderne
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@37785 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ socialtags/formulaires/config_socialtags_fonctions.php
@@ -0,0 +1,29 @@
+<?php
+
+function socialtags_choix(){
+	include_spip('socialtags_fonctions');
+	global $couleur_fonce;
+	$cfg = is_array($cfg = lire_config('socialtags/tags')) ? $cfg : array();
+
+	$retour = '';
+	foreach (socialtags_liste() as $service) {
+		$t = $service['titre'];
+		$u = $service['url'];
+		$a = $service['lesauteurs'];
+		$d = $service['descriptif'];
+
+		$image = find_in_path('images/'.$a.'.png');
+		$checked = in_array($a, $cfg) ? ' checked="checked"' : '';
+
+		$retour .= "<div class='choix'>
+				<input type='checkbox' id='choix_{$a}' name='tags[]' value='{$a}'{$checked} />
+				<label for='choix_{$a}'>
+					<img src='{$image}' title='".texte_script($t)."' alt='' />
+					" . ($checked ? "<strong>$t</strong>" : $t)
+					. ($d ? "&nbsp;<span style='color:$couleur_fonce;font-size:90%'>$d</span>" : "") . "
+				</label>
+			</div>";
+	}
+	return $retour;
+}
+?>
--- socialtags/lang/socialtags_fr.php
+++ socialtags/lang/socialtags_fr.php
@@ -3,31 +3,32 @@
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-	
+
 	//C
 	'cfg_1' => 'Configuration du plugin Social tags',
 	'cfg_2' => 'Ici vous pouvez choisir les sites de partages de vos articles.',
 	'cfg_3' => 'Documentation',
-	'cfg_tout_cocher' => 'Tout cocher',
-	'cfg_tout_decocher' => 'Tout d&#233;cocher',
-	'cfg_insertion' => 'Insertion dans les pages :',
-	'cfg_inserer' => 'Ins&#233;rer le bloc social-tags en-dessous du bloc suivant :',
-	'cfg_selector' => 'S&#233;lecteur :',
-	'cfg_exemples' => 'Exemples (sur les squelettes dist) :',
-	'cfg_en_bas' => 'En bas de chaque page',
-	'cfg_sous_bloc' => 'Sous le bloc de titre des pages article',
-	'cfg_sous_div' => 'Sous la div nomm&#233;e id=socialtags',
 	'cfg_badge_fb' => 'Badge/Widget Facebook',
-	'cfg_badge_fb_existe' => 'Si vous avez un Badge Facebook (',
-	'cfg_badge_fb_aide' => 'aide sur le site FB',
-	'cfg_badge_fb_style' => '), indiquez ci-dessous son contenu, soit en JS, soit en HTML.',
+	'cfg_badge_fb_existe' => 'Si vous avez un Badge Facebook (<a href="http://www.facebook.com/facebook-widgets/" class="spip_out">aide sur le site FB</a>), indiquez ci-dessous son contenu, soit en JS, soit en HTML.',
 	'cfg_badge_html' => 'Badge HTML',
 	'cfg_badge_html_ajouter_1' => '(ajouter dans votre squelette',
 	'cfg_badge_html_ajouter_2' => ', ou dans un article',
 	'cfg_badge_js' => 'Badge JS',
-	'cfg_badge_js_1' => ' (automatiquement inclus dans ',
+	'cfg_badge_js_1' => ' (automatiquement inclus dans <code>@code@</code)',
+	'cfg_en_bas' => 'En bas de chaque page',
+	'cfg_exemples' => 'Exemples (sur les squelettes dist) :',
+	'cfg_explication_boite_info' => 'Ici vous pouvez choisir les sites de partages de vos articles.',
+	'cfg_inserer' => 'Ins&#233;rer le bloc social-tags en-dessous du bloc suivant :',
+	'cfg_insertion' => 'Insertion dans les pages :',
+	'cfg_liste_site' => 'Liste des sites sociaux',
 	'cfg_referer' => 'N\'afficher le badge et les boutons que pour les visiteurs originaires des sites concern&#233;s.',
-	'cfg_titre_socialtags' => 'Social tags'
-	
+	'cfg_selector' => 'S&#233;lecteur :',
+	'cfg_sous_bloc' => 'Sous le bloc de titre des pages article',
+	'cfg_sous_div' => 'Sous la div nomm&#233;e id=socialtags',
+	'cfg_titre_boite_info' => 'Configuration du plugin Social tags',
+	'cfg_titre_socialtags' => 'Social tags',
+	'cfg_tout_cocher' => 'Tout cocher',
+	'cfg_tout_decocher' => 'Tout d&#233;cocher',
+
 );
 ?>
