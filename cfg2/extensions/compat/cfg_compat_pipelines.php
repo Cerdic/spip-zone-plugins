f43d6a0bd67ddf0cd341599f48c44f6eb6d9a0a7
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-04-28 15:08:49 +0000

    Aller chercher la css au bon endroit
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@37772 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- cfg2/extensions/compat/cfg_compat_pipelines.php
+++ cfg2/extensions/compat/cfg_compat_pipelines.php
@@ -25,29 +25,29 @@ function cfg_compat_ajouter_boutons($flux) {
 
 
 /*
- * - Gerer l'option <!-- head= xxx --> (/!\ DEPRECIE) 
- *   des fonds CFG uniquement dans le prive, 
+ * - Gerer l'option <!-- head= xxx --> (/!\ DEPRECIE)
+ *   des fonds CFG uniquement dans le prive,
  * - ajouter la css pour cfg_arbo
- * 
+ *
  */
 function cfg_compat_header_prive($flux){
-	
+
 	if (!_request('cfg') || (!_request('exec') == 'cfg')) {
 		return $flux;
 	}
 
 	// Ajout des css de cfg (uniquement balise arbo pour l'instant) dans le header prive
-	$flux .= '<link rel="stylesheet" href="'._DIR_PLUGIN_CFG.'css/cfg.css" type="text/css" media="all" />';
+	$flux .= '<link rel="stylesheet" href="'._DIR_PLUGIN_CFG_COMPAT.'css/cfg.css" type="text/css" media="all" />';
 
 	include_spip('inc/filtres');
 	include_spip('inc/cfg_formulaire');
 	$config = new cfg_formulaire(
 				sinon(_request('cfg'), ''),
 				sinon(_request('cfg_id'),''));
-	
-	if ($config->param['head']) 
+
+	if ($config->param['head'])
 		$flux .= "\n".$config->param['head'];
-	
+
 	return $flux;
 }
 
