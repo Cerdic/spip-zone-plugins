b9b0155093fc97451ea6cb7d7c8a9983cbf8c94c
erational@erational.org <erational@erational.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-04-28 10:52:29 +0000

    info bulle du mini-cal en multilang
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@37766 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- agenda/2_0_0/agenda_fonctions.php
+++ agenda/2_0_0/agenda_fonctions.php
@@ -71,7 +71,7 @@ function http_calendrier_mini($annee, $mois, $jour, $echelle, $partie_cal, $scri
 		if ($evts) {
 			$nb_elmts= @count($evts);
 			if ($nb_elmts>1){
-				$evts = "<a href='".$evts[0]['URL']."' title='".$nb_elmts." ".utf8_encode("�v�nements")."'>".intval($jour)."</a>";
+				$evts = "<a href='".$evts[0]['URL']."' title='".$nb_elmts." ".utf8_encode(_T('agenda:evenements'))."'>".intval($jour)."</a>";
 			}
 			else{
 				$evts = "<a href='".$evts[0]['URL']."' title='".$evts[0]['SUMMARY'].
