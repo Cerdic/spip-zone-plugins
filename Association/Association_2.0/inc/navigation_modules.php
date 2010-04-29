3b9ba618c71eba4088b76f5fc289e5e243bb6a29
esj@rezo.net <esj@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-04-29 10:00:31 +0000

    Association: pas de {{{^M}}} dans le code.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@37804 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- Association/Association_2.0/inc/navigation_modules.php
+++ Association/Association_2.0/inc/navigation_modules.php
@@ -9,35 +9,35 @@
 	* Pour plus de details voir le fichier COPYING.txt.
 	*  
 	**/
+
+function association_onglets(){
 	
-	function association_onglets(){
+	echo debut_onglet();
 		
-		echo debut_onglet();
-			
-		$link1= generer_url_ecrire('adherents');
-		$link2= generer_url_ecrire('dons');
-		$link4= generer_url_ecrire('ventes');
-		$link5= generer_url_ecrire('activites');
-		$link6= generer_url_ecrire('comptes');
-		$link7= generer_url_ecrire('ressources');
-		
-		echo onglet(_T('asso:menu2_titre_gestion_membres'), $link1, '', 'Membres', _DIR_PLUGIN_ASSOCIATION_ICONES.'annonce.gif','rien.gif' );  
-		if (lire_config("association/dons")) {
-			echo onglet(_T('asso:menu2_titre_gestion_dons'), $link2, '', 'Dons', _DIR_PLUGIN_ASSOCIATION_ICONES.'dons.gif','rien.gif' ); 
-		}
-		if (lire_config("association/ventes")) {
-			echo onglet(_T('asso:menu2_titre_ventes_asso'), $link4, '', 'Ventes', _DIR_PLUGIN_ASSOCIATION_ICONES.'ventes.gif','rien.gif' ); 
-		}
-		if (lire_config("association/activites")) {
-			echo onglet(_T('asso:menu2_titre_gestion_activites'), $link5, '', 'Activites', _DIR_PLUGIN_ASSOCIATION_ICONES.'activites.gif','rien.gif' ); 
-		}
-		if (lire_config("association/prets")) {
-			echo onglet(_T('asso:menu2_titre_gestion_prets'), $link7, '', 'Prets', _DIR_PLUGIN_ASSOCIATION_ICONES.'pret1.gif','rien.gif' ); 
-		}
-		if (lire_config("association/comptes")) {
-			echo onglet(_T('asso:menu2_titre_livres_comptes'), $link6, '', 'Comptes', _DIR_PLUGIN_ASSOCIATION_ICONES.'comptes.gif','rien.gif' ); 
-		}
-		
-		echo fin_onglet();
+	$link1= generer_url_ecrire('adherents');
+	$link2= generer_url_ecrire('dons');
+	$link4= generer_url_ecrire('ventes');
+	$link5= generer_url_ecrire('activites');
+	$link6= generer_url_ecrire('comptes');
+	$link7= generer_url_ecrire('ressources');
+	
+	echo onglet(_T('asso:menu2_titre_gestion_membres'), $link1, '', 'Membres', _DIR_PLUGIN_ASSOCIATION_ICONES.'annonce.gif','rien.gif' );  
+	if (lire_config('association/dons')) {
+		echo onglet(_T('asso:menu2_titre_gestion_dons'), $link2, '', 'Dons', _DIR_PLUGIN_ASSOCIATION_ICONES.'dons.gif','rien.gif' ); 
+	}
+	if (lire_config('association/ventes')) {
+		echo onglet(_T('asso:menu2_titre_ventes_asso'), $link4, '', 'Ventes', _DIR_PLUGIN_ASSOCIATION_ICONES.'ventes.gif','rien.gif' ); 
+	}
+	if (lire_config('association/activites')) {
+		echo onglet(_T('asso:menu2_titre_gestion_activites'), $link5, '', 'Activites', _DIR_PLUGIN_ASSOCIATION_ICONES.'activites.gif','rien.gif' ); 
+	}
+	if (lire_config('association/prets')) {
+		echo onglet(_T('asso:menu2_titre_gestion_prets'), $link7, '', 'Prets', _DIR_PLUGIN_ASSOCIATION_ICONES.'pret1.gif','rien.gif' ); 
+	}
+	if (lire_config('association/comptes')) {
+		echo onglet(_T('asso:menu2_titre_livres_comptes'), $link6, '', 'Comptes', _DIR_PLUGIN_ASSOCIATION_ICONES.'comptes.gif','rien.gif' ); 
+	}
+	
+	echo fin_onglet();
 	}
 ?>
