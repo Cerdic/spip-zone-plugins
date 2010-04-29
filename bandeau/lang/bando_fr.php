76e0825c79b87a25ebc7c8d1260c087704131bdc
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-04-29 12:12:38 +0000

    langues (bando)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@37810 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- bandeau/lang/bando_fr.php
+++ bandeau/lang/bando_fr.php
@@ -1,49 +1,34 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/_stable_/acces_restreint/lang/
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/bandeau/lang/
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// I
-	'icone_visiter_site' => 'Voir le site public',
-	
 	'icone_accueil' => 'Accueil',
-	'icone_edition' => '&Eacute;dition',
-	'icone_suivi_edito' => 'Publication',
-	'icone_reactions' => 'Activit&eacute;',
-	'icone_configuration' => 'Configuration',
 	'icone_administration' => 'Administration',
-	'icone_suivi_publication' => 'Suivi de la publication',
-	'icone_preferences' => 'Mes pr&eacute;f&eacute;rences',
-	'icone_mes_infos' => 'Mes informations',
+	'icone_configuration' => 'Configuration',
+	'icone_edition' => '&Eacute;dition',
 	'icone_langage' => 'Ma langue',
+	'icone_mes_infos' => 'Mes informations',
+	'icone_preferences' => 'Mes pr&eacute;f&eacute;rences',
+	'icone_reactions' => 'Activit&eacute;',
+	'icone_suivi_edito' => 'Publication',
+	'icone_suivi_publication' => 'Suivi de la publication',
+	'icone_visiter_site' => 'Voir le site public',
 
 	// L
-	'label_slogan_site' => 'Slogan du site',
 	'label_choix_langue' => 'Selectionnez votre langue',
+	'label_slogan_site' => 'Slogan du site',
 	'label_taille_ecran' => 'Largeur de l\'ecran',
 	'label_texte_et_icones_navigation' => 'Menu de navigation',
 	'label_texte_et_icones_page' => 'Affichage dans la page',
 
-	// M
-
-	// N
-	
-	// P
-
-	// R
-
-	// S
-
 	// T
-	'titre_identite_site' => 'Identit&eacute; du site',
 	'titre_config_langage' => 'Configurer la langue',
 	'titre_configurer_preferences' => 'Configurer vos pr&eacute;f&eacute;rences',
-
-	// V
-
-	// Z
+	'titre_identite_site' => 'Identit&eacute; du site'
 );
 
 ?>
