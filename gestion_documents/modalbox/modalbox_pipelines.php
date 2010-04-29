8f8d0dfa397303534cc407f0e403f7b4f38a016b
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-04-29 15:00:18 +0000

    ne pas casser la mediatheque en SPIP 2.0.x
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@37819 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- gestion_documents/modalbox/modalbox_pipelines.php
+++ gestion_documents/modalbox/modalbox_pipelines.php
@@ -1,7 +1,7 @@
 <?php
 
 function gestdoc_modalbox_insert_head($flux){
-	if (!test_plugin_actif('mediabox')) {
+	if (!defined('_DIR_PLUGIN_MEDIABOX')) {
 		// Insertion des librairies js
 		$flux .='<script src="'.find_in_path('modalbox/jquery.simplemodal-1.3.3.js').'" type="text/javascript"></script>';
 		$flux .='<script src="'.find_in_path('modalbox/modalbox.js').'" type="text/javascript"></script>';
