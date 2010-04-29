ed13f820aabdcc860a08f968656d20f6782e6211
pierrekuhn82@gmail.com <pierrekuhn82@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-04-29 20:26:34 +0000

    On passe langonet et on fait des corrections qui dates du passages a Z
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@37841 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- zpipcoop/lang/zpipcoop_fr.php
+++ zpipcoop/lang/zpipcoop_fr.php
@@ -8,30 +8,23 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 //A
 'abonnement' => 'Abonnement',
 'accueil_site' => 'Accueil',
-'adresse_mail' => 'Adresse mail',
 'agenda' => 'Agenda',
-'ajouter_commentaire' => 'Ajouter un commentaire',
 'annuaire' => 'Annuaire',
 'antispam_champ_vide' => 'Antispam champ vide',
 'aucun_document' => 'Aucun document',
+'aucun_message' => 'Aucun message',
 
 //B
 'bouton_ajouter_document' => 'Ajouter un document',
 'bouton_radio_modere_priori' => 'Mod&ecute;r&ecute; &agrave; priori',
 'bouton_telecharger' => 'T&ecute;l&ecute;charger',
-'bouton_valider' => 'Valider',
 
 //C
 'categories' => 'Cat&ecute;gories',
-'commentaire' => 'Commentaire',
 'commentaires' => 'Commentaires',
-'connecter_comme' => 'Connect&ecute; comme',
-'conception_graphique_par' => 'Habillage visuel (c)',
 
 //D
-'dans' => 'dans',
 'derniers_visiteurs' => 'Derniers visiteurs',
-'design' => 'Charte graphique par',
 'documents' => 'Documents',
 
 //E
@@ -40,18 +33,13 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 'editer_site' => '&Eacute;diter le site',
 
 //F
-'flux_forum_article' => 'Flux rss du forum de cet article',
 
 //G
 'groupes_travail' => 'Vos groupes de travail',
 
 //H
-'haut_page' => 'Haut de la page',
 
 //I
-'icons' => 'Images par',
-'identifiant' => 'Identifiant',
-'identifiant_compte' => 'Identifiant de ce compte',
 'info_cours_edition' => 'Vos messages en cours de r&eacute;daction',
 'info_cours_prop' => 'Vos messages propos&eacute; &agrave la publication',
 'info_messages_publies' => 'Vos messages publi&eacute;s',
@@ -63,7 +51,6 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	
 //M
 'messages' => 'Messages',
-'mon_profil' => 'Mon profil',
 
 //N
 'nouveau_groupe' => 'Nouveau groupe',
@@ -71,27 +58,20 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 'nouveau_site' => 'Nouveau site',
 
 //P
-'pas_le_droit_ecrire' => 'Vous n\'avez pas d\'autorisation en &eacute;criture',
-'personaliser_nav' => 'Personnaliser ce menu',
-'publier' => 'Publi&eacute; le',
+'pas_le_droit_ecrire' => 'Vous n\'avez pas d\'autorisation en &eacute;crire',
 
 //R
 'redaction' => 'R&eacute;daction',
 
 //S
 'sites' => 'Sites',
-'sous_licence' => 'sous Licence',
-'surnom' => 'Surnom',
 
 //T
-'tableau_de_bord' => 'Tableau de bord',
 'tous_les_sites' => 'Tous les sites',
 'tous_vos_messages' => 'Tous vos messages',
 
 //V
-'voir' => 'Voir',
-'voir_mon_profil' => 'Voir mon profil',
-'voir_votre_profil' => 'Voir voir profil',
+'voir_votre_profil' => 'Voir votre profil',
 'votre_profil' => 'Votre profil',
 
 );
