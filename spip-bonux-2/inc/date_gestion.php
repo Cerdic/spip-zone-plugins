fa0f586a2af6100ba36dfd77bace1efe2b91db7d
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-04-30 11:33:19 +0000

    eviter des warnings sur une saisie vide
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@37879 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spip-bonux-2/inc/date_gestion.php
+++ spip-bonux-2/inc/date_gestion.php
@@ -19,6 +19,8 @@ function verifier_corriger_date_saisie($suffixe,$horaire,&$erreurs){
 	include_spip('inc/filtres');
 	$date = _request("date_$suffixe").($horaire?' '.trim(_request("heure_$suffixe")).':00':'');
 	$date = recup_date($date);
+	if (!$date)
+		return '';
 	$ret = null;
 	if (!$ret=mktime(0,0,0,$date[1],$date[2],$date[0]))
 		$erreurs["date_$suffixe"] = _T('spip_bonux:erreur_date');
