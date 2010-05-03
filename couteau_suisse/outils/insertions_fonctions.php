0e492ff01d8419c8828ea43fa6a23ccbadf502f3
patfr@ifrance.com <patfr@ifrance.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-05-03 22:38:25 +0000

    Corrections automatiques : attention aux liens SPIP !
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@37964 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- couteau_suisse/outils/insertions_fonctions.php
+++ couteau_suisse/outils/insertions_fonctions.php
@@ -8,8 +8,15 @@ function insertions_rempl($texte) {
 	return preg_replace($ins[1][0], $ins[1][1], $texte);
 }
 
+function insertions_callback($m) {
+	return $m[1].cs_code_echappement($m[2], 'CS');
+}
+
 // Fonctions de traitement sur #TEXTE
 function insertions_pre_propre($texte) {
+	// prudence : on protege les liens de raccourcis de liens SPIP
+	if (strpos($texte, '[')!==false) 
+		$texte = preg_replace_callback(',(\[[^][]*->>?)([^]]*)(?=\]),msS', 'insertions_callback', $texte);
 	return cs_echappe_balises('', 'insertions_rempl', $texte);
 }
 
