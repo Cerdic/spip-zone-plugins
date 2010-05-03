75632302566fcc8872341756235ed279fbdea9e7
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-05-03 11:09:46 +0000

    plugin qui n'a plus lieu d'être
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@37946 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- lister_extensions/lister_extensions_pipelines.php
+++
@@ -1,9 +0,0 @@
-<?php
-	function lister_extensions_affiche_gauche($flux){
-		if($flux['args']['exec'] == 'admin_plugin'){
-			$lister_extensions_liste = charger_fonction('lister_extensions','inc');
-			$flux['data'] .= $lister_extensions_liste();	
-		}
-		return $flux;
-	}
-?>
