11528ca462d8a018f9f845d7e1d1618441c7bb89
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-05-04 17:33:10 +0000

    oups
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@37991 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ getID3/lang/getid3_en.php
@@ -0,0 +1,69 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	// I
+	'info_artist' => 'Artist',
+	'info_album' => 'Album',
+	'info_bitrate' => 'Bitrate',
+	'info_bitrate_mode' => 'Mode',
+	'info_bits' => 'Resolution (bits)',
+	'info_channels' => 'Channels number',
+	'info_channel_mode' => 'Mode (channel)',
+	'info_codec' => 'Codec',
+	'info_comment' => 'Comment',
+	'info_comments' => 'Comments',
+	'info_commercial_information' => 'Sales informations',
+	'info_copyright' => 'Copyright',
+	'info_copyright_message' => 'Copyright message',
+	'info_duree' => 'Duration',
+	'info_duree_secondes' => 'Duration (seconds)',
+	'info_encoded_by' => 'Encoded by',
+	'info_encoding_time' => 'Encoding date',
+	'info_format' => 'Format',
+	'info_genre' => 'Genre',
+	'info_lossless' => 'Lossless',
+	'info_mime' => 'Mime type',
+	'info_original_filename' => 'Original name',
+	'info_original_release_time' => 'Original creation date',
+	'info_sample_rate' => 'Sample rate',
+	'info_source' => 'Source',
+	'info_title' => 'Title',
+	'info_totaltracks' => 'Total tracks number',
+	'info_track' => 'Track',
+	'info_track_number' => 'Track number',
+	'info_url_artist' => 'Artist Url',
+	'info_url_file' => 'File Url',
+	'info_url_payment' => 'Payment Url',
+	'info_url_publisher' => 'Publishing site Url',
+	'info_url_source' => 'Source Url',
+	'info_url_station' => 'Station Url',
+	'info_year' => 'Year',
+	'install_mise_a_jour_base' => 'Update of the getid3 database at @version@',
+	'install_ajout_champs_documents' => 'GetID3 : Adding fields on the spip_documents database table',
+
+	// S
+	'son_infos_techniques' => 'Technical informations',
+	'son_nom_fichier' => 'File name',
+	'son_extension' => 'Extension :',
+	'son_format' => 'Mime-Type :',
+	'son_duree' => 'Duration :',
+	'son_bitrate' => 'Bitrate :',
+	'son_bitratemode' => 'Mode :',
+	'son_bitrate_cbr' => 'Constant bitrate',
+	'son_bitrate_vbr' => 'Variable bitrate',
+	'son_audiosamplerate' => 'Audio Samplerate :',
+	'son_encodeur' => 'Encoder :',
+	'son_bits' => 'Bits :',
+
+	// R
+	'recuperer_infos' => 'Retrieve file informations',
+
+	// V
+	'verifier_formulaire' => 'Verify the content of the form',
+
+);
+
+
+?>
---
+++ getID3/lang/getid3_fr.php
@@ -0,0 +1,69 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	// I
+	'info_artist' => 'Artiste',
+	'info_album' => 'Album',
+	'info_bitrate' => 'Bitrate',
+	'info_bitrate_mode' => 'Mode',
+	'info_bits' => 'R&eacute;solution (bits)',
+	'info_channels' => 'Nombre de canaux',
+	'info_channel_mode' => 'Mode (channel)',
+	'info_codec' => 'Codec',
+	'info_comment' => 'Commentaire',
+	'info_comments' => 'Commentaires',
+	'info_commercial_information' => 'Informations commerciales',
+	'info_copyright' => 'Copyright',
+	'info_copyright_message' => 'Message de copyright',
+	'info_duree' => 'Dur&eacute;e',
+	'info_duree_secondes' => 'Dur&eacute;e (en secondes)',
+	'info_encoded_by' => 'Encod&eacute; par',
+	'info_encoding_time' => 'Date d\'encodage',
+	'info_format' => 'Format',
+	'info_genre' => 'Genre',
+	'info_lossless' => 'Aucune perte de compression',
+	'info_mime' => 'Type mime',
+	'info_original_filename' => 'Nom original',
+	'info_original_release_time' => 'Date de cr&eacute;ation originale',
+	'info_sample_rate' => 'Sample rate',
+	'info_source' => 'Source',
+	'info_title' => 'Titre',
+	'info_totaltracks' => 'Nombre total de pistes',
+	'info_track' => 'Piste',
+	'info_track_number' => 'Piste num&eacute;ro',
+	'info_url_artist' => 'Url de l\'artiste',
+	'info_url_file' => 'Url du fichier',
+	'info_url_payment' => 'Url de paiement',
+	'info_url_publisher' => 'Url du site de publication',
+	'info_url_source' => 'Url de la source',
+	'info_url_station' => 'Url de station (?)',
+	'info_year' => 'Ann&eacute;e',
+	'install_mise_a_jour_base' => 'Mise &agrave; jour de la base de getid3 en @version@',
+	'install_ajout_champs_documents' => 'GetID3 : Ajout des champs sur spip_documents',
+
+	// S
+	'son_infos_techniques' => 'Infos techniques',
+	'son_nom_fichier' => 'Nom du fichier',
+	'son_extension' => 'Extension :',
+	'son_format' => 'Mime-Type :',
+	'son_duree' => 'Dur&eacute;e :',
+	'son_bitrate' => 'Bitrate :',
+	'son_bitratemode' => 'Mode :',
+	'son_bitrate_cbr' => 'Bitrate constant',
+	'son_bitrate_vbr' => 'Bitrate variable',
+	'son_audiosamplerate' => 'Audio Samplerate :',
+	'son_encodeur' => 'Encodeur :',
+	'son_bits' => 'Bits :',
+
+	// R
+	'recuperer_infos' => 'R&eacute;cup&eacute;rer les informations du fichier',
+
+	// V
+	'verifier_formulaire' => 'V&eacute;rifiez le remplissage du formulaire.',
+
+);
+
+
+?>
