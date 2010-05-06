3c7aedc8fc8e0971eb7a2c921bb4cd7ec96fe21d
klaus@spip.de <klaus@spip.de@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-05-06 18:44:37 +0000

    Traduction allemande pour l'interface des forums
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@38037 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ forum/lang/forum_de.php
@@ -0,0 +1,49 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+'aucun_message_forum' => 'Keine Beitr&auml;ge',
+'un_message_forum' => '1 Beitrag im Forum',
+'nombre_messages_forum' => '@nb@  Beitr&auml;ge im Forum',
+
+'icone_bruler_message' => "als Spam kennzeichnen",
+'icone_valider_repondre_message' => 'Beitrag freischalten ohne zu antworten',
+
+'icone_valider_messages' => 'Diese Beitr&auml;ge freischalten',
+'icone_bruler_messages' => "Als Spam markieren",
+'icone_legitimer_message' => "Kein Spam",
+'icone_supprimer_messages' => 'Diese Beit&auml;ge l&ouml;schen',
+'interface_onglets' => 'Darstellung mirt Reitern',
+'interface_formulaire' => 'Darstellung als Formular',
+
+'messages_meme_auteur' => 'Alle Beit&auml;ge dieses Autors',
+'messages_meme_email' => 'Alle Beit&auml;ge mit diese E-Mail',
+'messages_meme_ip' => 'Alle Beit&auml;ge mit dieser IP',
+
+// statuts
+'messages_tous' => 'Alle',
+'messages_aucun' => 'Kein',
+'messages_publie' => 'Ver&ouml;ffentlicht',
+'messages_prop' => 'Vorgeschlagen',
+'messages_spam' => 'Spam',
+'messages_off' => 'Abgelehnt',
+
+'messages_prive' => 'Private',
+'messages_privoff' => 'Abgelehnte',
+'messages_privrac' => 'Allgemeine',
+'messages_privadm' => 'Administratoren',
+
+'messages_perso' => 'Pers&ouml;nliche',
+
+'statut_prop' => 'Vorgeschlagen',
+'statut_publie' => 'Ver&ouml;ffentlicht',
+'statut_spam' => 'Spam',
+'statut_off' => 'Abgelehnt',
+
+'tout_voir' => 'Alle Beit&auml;ge anzeigen',
+);
+
+
+?>
