db94690c22de78ab9812136bb1f2a7a76364a499
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-05-06 13:19:08 +0000

    Un traduction rapide en anglais
    On vire le necessite sur jquery-ui (je ne retrouve pas où c'est utilisé)
    
    str_replace plutot que ereg déprécié
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@38031 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ lecteur_multimedia_v2/lang/lm2_en.php
@@ -0,0 +1,23 @@
+<?php
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	// C
+	'cfg_descriptif_titre' => 'Configuration of the Media Player plugin',
+	'cfg_explications_lecteur_son_inline' => 'Each sound file in the texts having the attribut <code>rel="enclosure"</code> may be replaced by a sound player',
+	'cfg_explications_lecteur_video' => 'Choose among the players below to replace &lt;docX|video&gt;.',
+	'cfg_label_aucun' => 'None',
+	'cfg_label_lecteur_audio' => 'Audio player',
+	'cfg_label_lecteur_son_inline' => 'Audio player in the text',
+	'cfg_label_lecteur_video' => 'Video player',
+	'cfg_label_lecteur_video_lecteur' => 'Video player',
+	'cfg_legende_documents_textes' => 'Player for the documents in the texts',
+	'cfg_lien_doc' => '<a href="http://multimedia.spip-zone.info" class="spip_out">Documentation</a>',
+
+	// T
+	'titre_liste_lecture' => 'Playliste'
+
+);
+
+?>
--- lecteur_multimedia_v2/lm2_fonctions.php
+++ lecteur_multimedia_v2/lm2_fonctions.php
@@ -1,11 +1,11 @@
 <?php
 function lm2_joli_titre($titre){
 	$titre = basename($titre);
-	$titre = ereg_replace('.mp3','',$titre);
-	$titre = ereg_replace('^ ','',$titre);
-	$titre = eregi_replace("_"," ", $titre );
-	$titre = eregi_replace("'"," ",$titre );
-	
+	$titre = str_replace('.mp3','',$titre);
+	$titre = str_replace('^ ','',$titre);
+	$titre = str_replace("_"," ", $titre );
+	$titre = str_replace("'"," ",$titre );
+
 	return $titre ;
 }
 ?>
