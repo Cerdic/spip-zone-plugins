9b703d7717faa7e4d608ebbe732fbc88f4827a66
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-05-11 09:25:35 +0000

    Ne pas prendre exclusivement les class text car certains (configuration du site) ne l'ont pas (encore)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@38136 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- multilang/multilang_pipelines.php
+++ multilang/multilang_pipelines.php
@@ -100,7 +100,7 @@ function multilang_inserer_head($flux,$config=array()){
 			  	jQuery(document).ready(function(){
 					function multilang_init(){
 						root = "'.$root.'";
-						fields_selector = "textarea,input.text:text:not(input#new_login,*.nomulti),.multilang";
+						fields_selector = "textarea,input:text:not(input#new_login,*.nomulti),.multilang";
 						forms_selector = "form[class!=\'form_upload\'][class!=\'form_upload_icon\']";
 						root_opt = "div:has(> form:has(.multilang))";
 						forms_selector_opt = ".multilang";
