087b8068e949113b7e02e19dad1adafee5ddb143
maieulrouquette@sfr.fr <maieulrouquette@sfr.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-05-15 16:32:53 +0000

    tout en fichier de langue
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@38199 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- activite_editoriale/genie/activite_editoriale_alerte.php
+++ activite_editoriale/genie/activite_editoriale_alerte.php
@@ -4,19 +4,19 @@ function genie_activite_editoriale_alerte_dist() {
 	if ($rubLists = sql_select("*", "spip_rubriques", "`extras_delai` != '' and TO_DAYS(NOW()) - TO_DAYS(maj) >= `extras_delai`")) {
 		while($list = sql_fetch($rubLists)) {
 			$envoyer_mail = charger_fonction('envoyer_mail', 'inc');
-			$subject = 'Une rubrique doit etre mise a jour';
+			$subject = _T('activite_editoriale:rubrique_doit_maj');
 			include_spip('activite_editoriale_fonctions');
 			$url = $GLOBALS['meta']['adresse_site'].'/ecrire/?exec=naviguer&id_rubrique='.$list['id_rubrique'];
-			$body = 'Attention, la rubique "'.$list['titre'].'" n\'a pas ete mise a jour depuis '.age_rubrique($list['maj']).' jours. '."\n\n";
-			$body = $body.'Vous etes identifie comme faisant partie des gestionnaires de la rubrique'."\n\n";
+			$body = _T('activite_editoriale:rubrique_pas_maj',array('titre'=>$list['titre'],'jour'=>age_rubrique($list['maj']))."\n\n";
+			$body = $body._T('activite_editoriale:gestionnaire')."\n\n";
 			$body = $body.$url;
 			if ($auteurLists = sql_select("*", "spip_auteurs", "id_auteur in (".$list['extras_identifiants'].")")) {
 				while($auteurs = sql_fetch($auteurLists)) {
 					$to = $auteurs['email'];
 					if ($envoyer_mail($to, $subject, $body)) {
-						spip_log("Message sent to ".$to, "activite_editoriale");
+						spip_log("Message envoyé à".$to, "activite_editoriale");
 					} else {
-						spip_log("Message could not be sent to ".$to, "activite_editoriale");
+						spip_log("Message n'a pu être envoyé à ".$to, "activite_editoriale");
 					}
 				}
 			}
--- activite_editoriale/lang/activite_editoriale_fr.php
+++ activite_editoriale/lang/activite_editoriale_fr.php
@@ -4,9 +4,23 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	//D
 	'delai' => 'D&eacute;lai',
 	'delai_precisions' => '(en nombre de jours)',
+	'derniere_maj'     => 'Dernière MàJ',
+	
+	
+	//G
+	'gestionnaire'     => "Vous êtes identifié comme un gestionnaire de la rubrique",
 
 	//I
 	'identifiants' => 'Identifiants des auteurs &agrave; alerter',
 	'identifiants_precisions' => '(id_auteur s&eacute;par&eacute;s par des ",")',
+	
+	//J
+	'jours'        =>'jour(s)'
+	
+	//R
+	'responsables'             =>  'Responsables',
+	'rubrique_doit_maj'        => 'Une rubrique doit être mise a jour',
+	'rubrique_pas_maj'         => "Attention, la rubrique @titre n'a pas ete mise a jour depuis @jours jours".
+	
 
 );
