39f6084c95970d6995da1efd2629221229a320bb
esj@rezo.net <esj@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-05-16 09:21:22 +0000

    [source:_plugins_/Association/Association_2.0 Association]: mauvaise URL, et conformité XHTML.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@38214 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- Association/Association_2.0/exec/voir_adherent.php
+++ Association/Association_2.0/exec/voir_adherent.php
@@ -41,11 +41,11 @@ function exec_voir_adherent(){
 		echo debut_gauche("",true);
 		
 		echo debut_boite_info(true);
-		echo '<div style="font-weight: bold; text-align: center" class="verdana1 spip_xx-small">'.propre(_T('asso:adherent_libelle_numero')).'<br />';
+		echo '<div style="font-weight: bold; text-align: center;" class="verdana1 spip_xx-small">'.propre(_T('asso:adherent_libelle_numero')).'<br />';
 		echo '<span class="spip_xx-large">';
 		if($indexation=="id_asso"){echo $id_asso;} else {echo $id_auteur;}
 		echo '</span></div>';
-		echo '<br /><div style="font-weight: bold; text-align: center" class="verdana1 spip_xx-small">'.$nom_famille.' '.$prenom.'</div>';
+		echo '<br /><div style="font-weight: bold; text-align: center;" class="verdana1 spip_xx-small">'.$nom_famille.' '.$prenom.'</div>';
 		echo '<br /><div style="text-align:center;">'.association_date_du_jour().'</div>';	
 		 echo fin_boite_info(true);
 		
@@ -59,7 +59,7 @@ function exec_voir_adherent(){
 		// FICHE HISTORIQUE COTISATIONS
 		echo '<fieldset><legend>'._T('asso:adherent_titre_historique_cotisations').'</legend>';
 		echo "<table border='0' cellpadding='2' cellspacing='0' width='100%' class='arial2' style='border: 1px solid #aaaaaa;'>\n";
-		echo "<tr style='background-gcolor: #DBE1C5'>\n";
+		echo "<tr style='background-color: #DBE1C5'>\n";
 		echo '<td style="text-align:right;"><strong>'._T('asso:adherent_entete_id').'</strong></td>';
 		echo '<td><strong>'._T('asso:adherent_entete_date').'</strong></td>';
 		echo '<td style="text-align:right;"><strong>'._T('asso:adherent_entete_paiement').'</strong></td>';
@@ -70,11 +70,11 @@ function exec_voir_adherent(){
 		$query = sql_select("*", "spip_asso_comptes", "id_journal=$id_auteur ", '', "date DESC" );
 		while ($data = sql_fetch($query)) {
 			echo '<tr style="background-color: #EEEEEE;">';
-			echo '<td class="arial11 border1" style="text-align:right;">'.$data['id_compte'].'</td>';
-			echo '<td class="arial11 border1">'.association_datefr($data['date']).'</td>';
+			echo '<td class="arial11 border1" style="text-align:right;">'.$data['id_compte']."</td>\n";
+			echo '<td class="arial11 border1">'.association_datefr($data['date'])."</td>\n";
 			echo '<td class="arial11 border1" style="text-align:right;">'.$data['recette'].' &euro;</td>';
-			echo '<td class="arial11 border1">'.propre($data['justification']).'</td>';
-			echo '<td class="arial11 border1">'.$data['journal'].'</td>';
+			echo '<td class="arial11 border1">'.propre($data['justification'])."</td>\n";
+			echo '<td class="arial11 border1">'.$data['journal']."</td>\n";
 			echo '</tr>';
 		}
 		echo '</table>';
@@ -84,12 +84,12 @@ function exec_voir_adherent(){
 		if ($GLOBALS['asso_metas']['activites']=="on"){
 			echo '<fieldset><legend>'._T('asso:adherent_titre_historique_activites').'</legend>';
 			echo "<table border=0 cellpadding=2 cellspacing=0 width='100%' class='arial2' style='border: 1px solid #aaaaaa;'>\n";
-			echo '<tr bgcolor="#DBE1C5">';
-			echo '<td style="text-align:right;"><strong>'._T('asso:adherent_entete_id').'</strong></td>';
-			echo '<td><strong>'._T('asso:adherent_entete_date').'</strong></td>';
-			echo '<td><strong>'._T('asso:adherent_entete_activite').'</strong></td>';
-			echo '<td style="text-align:right;"><strong>'._T('asso:adherent_entete_inscrits').'</strong></td>';
-			echo '<td><strong>'._T('asso:adherent_entete_statut').'</strong></td>';
+			echo '<tr style="background-color: #DBE1C5">';
+			echo '<td style="text-align:right;"><strong>'._T('asso:adherent_entete_id')."</strong></td>\n";
+			echo '<td><strong>'._T('asso:adherent_entete_date')."</strong></td>\n";
+			echo '<td><strong>'._T('asso:adherent_entete_activite')."</strong></td>\n";
+			echo '<td style="text-align:right;"><strong>'._T('asso:adherent_entete_inscrits')."</strong></td>\n";
+			echo '<td><strong>'._T('asso:adherent_entete_statut')."</strong></td>\n";
 			echo '<td><strong>&nbsp;</strong></td>';
 			echo '</tr>';
 			$critere='id_adherent='.$id_auteur;
@@ -98,17 +98,17 @@ function exec_voir_adherent(){
 			while ($data = sql_fetch($query)) {
 				$id_evenement=$data['id_evenement'];
 				echo '<tr style="background-color: #EEEEEE;">';
-				echo '<td class="arial11 border1" style="text-align:right;">'.$data['id_activite'].'</td>';
+				echo '<td class="arial11 border1" style="text-align:right;">'.$data['id_activite']."</td>\n";
 				$sql = sql_select("*", "spip_evenements", "id_evenement=$id_evenement" );
 				while ($evenement = sql_fetch($sql)) {
 					$date = substr($evenement['date_debut'],0,10);
-					//echo '<td class="arial11 border1">'.association_datefr($date).'</td>';
-					echo '<td class="arial11 border1">'.association_datefr($date).'</td>';
-					echo '<td class="arial11 border1">'.$evenement['titre'].'</td>';
+					//echo '<td class="arial11 border1">'.association_datefr($date)."</td>\n";
+					echo '<td class="arial11 border1">'.association_datefr($date)."</td>\n";
+					echo '<td class="arial11 border1">'.$evenement['titre']."</td>\n";
 				}
-				echo '<td class="arial11 border1" style="text-align:right;">'.$data['inscrits'].'</td>';
-				echo '<td class="arial11 border1">'.$data['statut'].'</td>';
-				echo '<td class="arial11 border1" style="text-align:center"><a href="'.$url_edit_activite.'&id='.$data['id_activite'].'"><img src="'._DIR_PLUGIN_ASSOCIATION_ICONES.'edit-12.gif" title="'._T('asso:adherent_bouton_maj_inscription').'"></a></td>';
+				echo '<td class="arial11 border1" style="text-align:right;">'.$data['inscrits']."</td>\n";
+				echo '<td class="arial11 border1">'.$data['statut']."</td>\n";
+				echo '<td class="arial11 border1" style="text-align:center;"><a href="'.$url_edit_activite.'&id='.$data['id_activite'].'"><img src="'._DIR_PLUGIN_ASSOCIATION_ICONES.'edit-12.gif" title="'._T('asso:adherent_bouton_maj_inscription').'"></a></td>';
 				echo '</tr>';
 			}
 			echo '</table>';
@@ -119,25 +119,25 @@ function exec_voir_adherent(){
 		if ($GLOBALS['asso_metas']['ventes']=="on"){
 			echo '<fieldset><legend>'._T('asso:adherent_titre_historique_ventes').'</legend>';
 			echo "<table border=0 cellpadding=2 cellspacing=0 width='100%' class='arial2' style='border: 1px solid #aaaaaa;'>\n";
-			echo '<tr bgcolor="#DBE1C5">';
-			echo '<td style="text-align:right;"><strong>'._T('asso:vente_entete_id').'</strong></td>';
-			echo '<td><strong>'._T('asso:vente_entete_date').'</strong></td>';
-			echo '<td><strong>'._T('asso:vente_entete_article').'</strong></td>';
-			echo '<td style="text-align:right;"><strong>'._T('asso:vente_entete_quantites').'</strong></td>';
-			echo '<td><strong>'._T('asso:vente_entete_date_envoi').'</strong></td>';
-			echo '<td><strong>&nbsp;</strong></td>';
+			echo '<tr style="background-color: #DBE1C5">';
+			echo '<td style="text-align:right;"><strong>'._T('asso:vente_entete_id')."</strong></td>\n";
+			echo '<td><strong>'._T('asso:vente_entete_date')."</strong></td>\n";
+			echo '<td><strong>'._T('asso:vente_entete_article')."</strong></td>\n";
+			echo '<td style="text-align:right;"><strong>'._T('asso:vente_entete_quantites')."</strong></td>\n";
+			echo '<td><strong>'._T('asso:vente_entete_date_envoi')."</strong></td>\n";
+			echo "<td><strong>&nbsp;</strong></td>\n";
 			echo '</tr>';
 			$critere='id_acheteur='.$id_auteur;
 			if($indexation=='id_asso'){$critere='id_acheteur='._q($id_asso);} 
 			$query = sql_select("*", "spip_asso_ventes", $critere, '', "date_vente DESC" );			
 			while ($data = sql_fetch($query)) {
 				echo '<tr style="background-color: #EEEEEE;">';
-				echo '<td class="arial11 border1" style="text-align:right;">'.$data['id_vente'].'</td>';
-				echo '<td class="arial11 border1" style="text-align:right;">'.association_datefr($data['date_vente']).'</td>';
-				echo '<td class="arial11 border1">'.$data['article'].'</td>';
-				echo '<td class="arial11 border1" style="text-align:right;">'.$data['quantite'].'</td>';
-				echo '<td class="arial11 border1" style="text-align:right;">'.association_datefr($data['date_envoi']).'</td>';
-				echo '<td class="arial11 border1" style="text-align:center"><a href="'.$url_edit_vente.'&id='.$data['id_vente'].'"><img src="'._DIR_PLUGIN_ASSOCIATION_ICONES.'edit-12.gif" title="'._T('asso:adherent_bouton_maj_vente').'"></a></td>';
+				echo '<td class="arial11 border1" style="text-align:right;">'.$data['id_vente']."</td>\n";
+				echo '<td class="arial11 border1" style="text-align:right;">'.association_datefr($data['date_vente'])."</td>\n";
+				echo '<td class="arial11 border1">'.$data['article']."</td>\n";
+				echo '<td class="arial11 border1" style="text-align:right;">'.$data['quantite']."</td>\n";
+				echo '<td class="arial11 border1" style="text-align:right;">'.association_datefr($data['date_envoi'])."</td>\n";
+				echo '<td class="arial11 border1" style="text-align:center;">', association_bouton(_T('asso:adherent_bouton_maj_vente'), 'edit-12.gif', 'edit_vente','id='.$data['id_vente']), "</td>\n";
 				echo '</tr>';
 			}
 			echo '</table>';
@@ -155,10 +155,10 @@ function exec_voir_adherent(){
 			echo "<table border='0' cellpadding='2' cellspacing='0' width='100%' class='arial2' style='border: 1px solid #aaaaaa;'>\n";
 			echo "\n<tr style='background-color: #DBE1C5'>";
 			echo '<td>&nbsp;</td>';
-			echo '<td style="text-align:right;"><strong>'._T('asso:pret_entete_id').'</strong></td>';
-			echo '<td><strong>'._T('asso:pret_entete_article').'</strong></td>';
-			echo '<td style="text-align:right;"><strong>'._T('asso:pret_entete_date_sortie').'</strong></td>';
-			echo '<td style="text-align:right;"><strong>'._T('asso:pret_entete_date_retour').'</strong></td>';
+			echo '<td style="text-align:right;"><strong>'._T('asso:pret_entete_id')."</strong></td>\n";
+			echo '<td><strong>'._T('asso:pret_entete_article')."</strong></td>\n";
+			echo '<td style="text-align:right;"><strong>'._T('asso:pret_entete_date_sortie')."</strong></td>\n";
+			echo '<td style="text-align:right;"><strong>'._T('asso:pret_entete_date_retour')."</strong></td>\n";
 			echo '<td><strong>&nbsp;</strong></td>';
 			echo '</tr>';
 			if($indexation=='id_asso'){$critere='id_emprunteur='._q($id_asso);} else {$critere='id_emprunteur='._q($id_auteur);}
@@ -173,9 +173,9 @@ function exec_voir_adherent(){
 				echo "\n<tr style='background-color: #EEEEEE;'>";
 				echo '<td class="arial11 border1">';
 				echo '<img src="' . _DIR_PLUGIN_ASSOCIATION_ICONES . 'puce-'.$puce. ".gif\" /></td>\n";
-				echo '<td class="arial11 border1" style="text-align:right;">'.$data['id_pret'].'</td>';
-				echo '<td class="arial11 border1">'.$data['intitule'].'</td>';
-				echo '<td class="arial11 border1" style="text-align:right;">'.association_datefr($data['date_sortie']).'</td>';
+				echo '<td class="arial11 border1" style="text-align:right;">'.$data['id_pret']."</td>\n";
+				echo '<td class="arial11 border1">'.$data['intitule']."</td>\n";
+				echo '<td class="arial11 border1" style="text-align:right;">'.association_datefr($data['date_sortie'])."</td>\n";
 				echo '<td class="arial11 border1" style="text-align:right;">';
 				if($data['date_retour']=="0000-00-00"){echo '&nbsp;';} else {echo association_datefr($data['date_retour']);}
 				echo "</td>\n";
