cc24b476e983ccaf1b01b2e58719f1dd643c1781
apachot@openstudio.fr <apachot@openstudio.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-05-19 14:10:32 +0000

    pouvoir prévisualiser les lieux depuis l'espace privé
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@38268 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- panoramas/lang/panoramas_fr.php
+++ panoramas/lang/panoramas_fr.php
@@ -48,6 +48,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 'liste_objets_jeu' => 'Liste des objets &agrave; avoir dans la sacoche pour terminer le jeu (id s&eacute;par&eacute;s par des virgules)',
 'bravo' => 'F&eacute;licitation NOM_JOUEUR ! clique ici pour recommencer',
 'nom_joueur' => 'Inscris ton nom',
-'nom_obligatoire' => 'Il faut d\'abord inscrire ton nom pour pouvoir jouer'
+'nom_obligatoire' => 'Il faut d\'abord inscrire ton nom pour pouvoir jouer',
+'apercu' => 'Aper&ccedil;u'
 );
 ?>
