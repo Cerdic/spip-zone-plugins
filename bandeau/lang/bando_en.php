9fee7fef59b24d5ebed5437adde1ed70d5ad1a24
lspeciale@gmail.com <lspeciale@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-05-25 16:01:21 +0000

    Langues : es et en
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@38370 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ bandeau/lang/bando_en.php
@@ -0,0 +1,34 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/bandeau/lang/
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// I
+	'icone_accueil' => 'Launch pad',
+	'icone_administration' => 'Administration',
+	'icone_configuration' => 'Configuration',
+	'icone_edition' => 'Site edit',
+	'icone_langage' => 'My language',
+	'icone_mes_infos' => 'My informations',
+	'icone_preferences' => 'My preferences',
+	'icone_reactions' => 'Activity',
+	'icone_suivi_edito' => 'Publication',
+	'icone_suivi_publication' => 'To follow the publication',
+	'icone_visiter_site' => 'Visit',
+
+	// L
+	'label_choix_langue' => 'Manage your language ',
+	'label_slogan_site' => 'Slogan',
+	'label_taille_ecran' => 'Display',
+	'label_texte_et_icones_navigation' => 'Navigation menu',
+	'label_texte_et_icones_page' => 'Page display',
+
+	// T
+	'titre_config_langage' => 'Configurer la langue',
+	'titre_configurer_preferences' => 'Configure your preferences',
+	'titre_identite_site' => 'Site\'s identity'
+);
+
+?>
---
+++ bandeau/lang/bando_es.php
@@ -0,0 +1,34 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/bandeau/lang/
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// I
+	'icone_accueil' => 'Mis tareas',
+	'icone_administration' => 'Administraci&oacute;n',
+	'icone_configuration' => 'Configuraci&oacute;n',
+	'icone_edition' => 'Edici&oacute;n',
+	'icone_langage' => 'Mi lengua',
+	'icone_mes_infos' => 'Mis informaciones',
+	'icone_preferences' => 'Mis preferencias',
+	'icone_reactions' => 'Actividad',
+	'icone_suivi_edito' => 'Publicaci&oacute;n',
+	'icone_suivi_publication' => 'Seguir la publicaci&oacute;n',
+	'icone_visiter_site' => 'Ver el sitio p&uacute;blico',
+
+	// L
+	'label_choix_langue' => 'Seleccine su lengua',
+	'label_slogan_site' => 'Slogan del sitio',
+	'label_taille_ecran' => 'Largo de la pantalla',
+	'label_texte_et_icones_navigation' => 'Menu de navigai&oacute;n',
+	'label_texte_et_icones_page' => 'Afichar en la p&aacute;gina',
+
+	// T
+	'titre_config_langage' => 'Configurar la lengua',
+	'titre_configurer_preferences' => 'Configurar sus preferencias',
+	'titre_identite_site' => 'Identidad del sitio'
+);
+
+?>
