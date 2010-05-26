abd99a586098a184aee74ff18731e6640157ddc1
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-05-26 16:49:01 +0000

    langues (spipicious)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@38392 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ spipicious_jquery/lang/spipicious_ca.php
@@ -0,0 +1,63 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'add_tags' => 'Afegir etiquetes',
+	'aucun_tags' => 'Cap etiqueta associada.',
+	'auteur_tag_usage' => 'Aquest usuari utilitza @nb@ vegades aquesta etiqueta',
+
+	// B
+	'by' => 'Per',
+
+	// C
+	'cfg_description' => 'Aquest connector permet afegir f&agrave;cilment etiquetes als articles.',
+
+	// E
+	'explication_ajout_tag' => 'Per afegir diverses etiquetes a la vegada, utilitzeu el separador: "@separateur@".',
+	'explication_suppression_tag' => 'Seleccioneu les vostres etiquetes de la llista que hi ha a sota i valideu per suprimir-les.',
+
+	// I
+	'identifier' => 'Per afegir etiquetes a aquest article, us heu d\'identificar',
+
+	// L
+	'label_tags' => 'Etiquetes:',
+	'liste_auteurs_tag' => 'Aquesta etiqueta es fa servir per:',
+
+	// M
+	'message_aucun_tags' => 'No hi ha cap etiqueta associada',
+	'message_installation_activation' => 'Instal&middot;laci&oacute; de taules i configuraci&oacute; per defecte de spip.icio.us',
+	'message_upgrade_database' => 'Actualitzaci&oacute; de la base de dades de spip.ici.ous a la versi&oacute; @version@<br/>',
+
+	// N
+	'no_tags_associes' => 'No teniu cap paraula clau associada',
+	'nom_groupe_tag' => 'Grup de paraules clau associades',
+
+	// S
+	'spipicious' => 'Spip.icio.us',
+
+	// T
+	'tag_ajoute' => 'Heu afegit una etiqueta:<br />@name@',
+	'tag_deja_present' => 'Ja havies afegit aquesta etiqueta.',
+	'tag_supprime' => 'Heu suprimit el tag:<br />@name@',
+	'tags' => 'Etiquetes:',
+	'tags_ajoutes' => 'Heu afegit diverses etiquetes:<br />@name@',
+	'tags_supprimes' => 'Heu suprimit diverses etiquetes:<br />@name@',
+	'title_tag_utilise_nb' => 'Aquesta paraula clau est&agrave; associada per @nb@ persones.',
+	'title_tag_utilise_un' => 'Aquesta paraula clau est&agrave; associada per una persona.',
+	'to' => 'amb',
+	'types_utilisateurs' => 'Qui t&eacute; acc&eacute;s al formulari?',
+
+	// V
+	'vos_tags' => 'Les vostres etiquetes (modificaci&oacute;)',
+
+	// W
+	'waiting' => 'cerca de les vostres etiquetes'
+);
+
+?>
