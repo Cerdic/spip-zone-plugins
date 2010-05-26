c9e1f06f36444539586cbe15d6f8f41e7bfbc016
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-05-26 12:39:29 +0000

    eviter de declencher un set_magic_quotes_runtime() deprecie lorsque la fonction est de toute facon a OFF
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@38376 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- facteur/phpmailer-php5/class.phpmailer.php
+++ facteur/phpmailer-php5/class.phpmailer.php
@@ -1461,18 +1461,18 @@ class PHPMailer {
       if (!is_readable($path)) {
         throw new phpmailerException($this->Lang('file_open') . $path, self::STOP_CONTINUE);
       }
-      if (function_exists('get_magic_quotes')) {
+      if (!function_exists('get_magic_quotes')) {
         function get_magic_quotes() {
           return false;
         }
       }
-      if (PHP_VERSION < 6) {
-        $magic_quotes = get_magic_quotes_runtime();
+      if (PHP_VERSION < 6
+			  AND $magic_quotes = get_magic_quotes_runtime()) {
         set_magic_quotes_runtime(0);
       }
       $file_buffer  = file_get_contents($path);
       $file_buffer  = $this->EncodeString($file_buffer, $encoding);
-      if (PHP_VERSION < 6) { set_magic_quotes_runtime($magic_quotes); }
+      if (PHP_VERSION < 6 AND $magic_quotes) { set_magic_quotes_runtime($magic_quotes); }
       return $file_buffer;
     } catch (Exception $e) {
       $this->SetError($e->getMessage());
