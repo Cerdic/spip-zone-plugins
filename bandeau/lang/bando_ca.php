8d3b98afef3814b4940cc8be1a97798122fd85ee
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-05-26 16:48:38 +0000

    langues (bando)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@38381 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ bandeau/lang/bando_ca.php
@@ -0,0 +1,36 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// I
+	'icone_accueil' => 'Inici',
+	'icone_administration' => 'Administraci&oacute;',
+	'icone_configuration' => 'Configuraci&oacute;',
+	'icone_edition' => 'Edici&oacute;',
+	'icone_langage' => 'La meva llengua ',
+	'icone_mes_infos' => 'Les meves informacions',
+	'icone_preferences' => 'Les meves prefer&egrave;ncies',
+	'icone_reactions' => 'Activitat',
+	'icone_suivi_edito' => 'Publicaci&oacute;',
+	'icone_suivi_publication' => 'Seguiment de la publicaci&oacute;',
+	'icone_visiter_site' => 'Veure el lloc p&uacute;blic',
+
+	// L
+	'label_choix_langue' => 'Seleccioneu la vostra llengua',
+	'label_slogan_site' => 'Esl&ograve;gan del lloc',
+	'label_taille_ecran' => 'Amplada de la pantalla',
+	'label_texte_et_icones_navigation' => 'Men&uacute; de navegaci&oacute;',
+	'label_texte_et_icones_page' => 'Visualitzaci&oacute; a la p&agrave;gina',
+
+	// T
+	'titre_config_langage' => 'Configurar la llengua',
+	'titre_configurer_preferences' => 'Configurar les teves prefer&egrave;ncies',
+	'titre_identite_site' => 'Identitat del lloc'
+);
+
+?>
