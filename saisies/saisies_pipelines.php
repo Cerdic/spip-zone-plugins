e9e229355e2b39b62f4b0410121c1f2c0ad7df85
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-05-28 20:37:35 +0000

    media='all' pour que la css soit compressée avec les autres
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@38441 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- saisies/saisies_pipelines.php
+++ saisies/saisies_pipelines.php
@@ -7,7 +7,7 @@ function saisies_header_prive($flux){
 	$js = find_in_path('javascript/saisies.js');
 	$css = generer_url_public('saisies.css');
 	$flux .= "\n<script type='text/javascript' src='$js'></script>\n";
-	$flux .= "\n<link rel='stylesheet' href='$css' type='text/css' />\n";
+	$flux .= "\n<link rel='stylesheet' href='$css' type='text/css' media='all' />\n";
 	return $flux;
 }
 
