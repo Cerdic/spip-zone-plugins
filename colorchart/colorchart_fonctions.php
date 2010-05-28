92e63bd3afd030f3b9abd76c99433cd525a0f107
fil@rezo.net <fil@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-05-28 16:26:27 +0000

    colorchart pour bidouiller
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@38432 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ colorchart/colorchart_fonctions.php
@@ -0,0 +1,32 @@
+<?php
+
+function couleurs_extraire($src, $a) {
+	for ($i = 1; $i <= $a; $i++)
+	for ($j = 1; $j <= $a; $j++) {
+		$e = extraire_image_couleur($src, $j*floor(20/($a+1)), $i*floor(20/($a+1)));
+		$b[] = couleur_web($e);
+	}
+
+	return join('-', $b);
+}
+
+function pixellate($img, $a=4) {
+	include_spip('images_fonctions');
+	include_spip('inc/filtres_images');
+
+	$src = extraire_attribut($img, 'src');
+
+	$k = couleurs_extraire($src, $a);
+	$r = "<table>";
+
+	foreach (explode('-', $k) as $i => $c) {
+		$c = str_replace(':', 'a', preg_replace(',(.).,e', 'chr(2*ceil(ord("\1")/2))', $c));
+		if ($i%$a == 0) $r .= "<tr>";
+		$r .= "<td style='background:#$c;' title='$c' />";
+	}
+	$r .= "</table>\n";
+
+	return $r;
+}
+
+?>
