e80df69893963e0c699118f6f552fa811b4f59a4
erational@erational.org <erational@erational.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-05-31 13:03:03 +0000

    SPIP 2.1 repare lien bouton menu
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@38494 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- ma-lettre/inc_malettre.php
+++ ma-lettre/inc_malettre.php
@@ -1,15 +1,12 @@
 <?php
 //
 // ajout bouton 
-$p=explode(basename(_DIR_PLUGINS)."/",str_replace('\\','/',realpath(dirname(__FILE__))));
-define('_DIR_PLUGIN_MALETTRE',(_DIR_PLUGINS.end($p)));
- 
 function malettre_ajouterBoutons($boutons_admin) {
 		// si on est admin (deactive)		
 		if ($GLOBALS['connect_statut'] == "0minirezo" && $GLOBALS["connect_toutes_rubriques"]) { // admin full
 		  // on voit le bouton dans la barre "naviguer"
 		  $boutons_admin['naviguer']->sousmenu['malettre']= new Bouton(
-			"../"._DIR_PLUGIN_MALETTRE."/img/icon_malettre.png",  // icone
+			_DIR_PLUGIN_MALETTRE."/img/icon_malettre.png",  // icone
 			_T("malettre:ma_lettre")	// titre
 			);
 		}
