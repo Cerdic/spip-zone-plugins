40a850f05baaa0c8cf690eb45badb8f2da2b92af
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-05-31 21:24:45 +0000

    suivre le core qui introduit et utilise la fonction tester_url_ecrire dans #URL_ECRIRE, sans casser la compat avec les versions precedentes
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@38506 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spip-bonux-2/public/spip_bonux_balises.php
+++ spip-bonux-2/public/spip_bonux_balises.php
@@ -62,16 +62,18 @@ function balise_URL_ECRIRE($p) {
 		return $p;
 	}
 
-	$p->code = interprete_argument_balise(1,$p);
+	$code = interprete_argument_balise(1,$p);
+	if (preg_match("/^'[^']*'$/", $code))
+		$fonc = $code;
+	else {$code = "(\$f = $code)"; $fonc = '$f';}
 	$args = interprete_argument_balise(2,$p);
 	if ($args != "''" && $args!==NULL)
-		$p->code .= ','.$args;
+		$fonc .= ',' . $args;
 
-	// autres filtres (???)
-	array_shift($p->param);
-
-	$p->code = 'generer_url_ecrire(' . $p->code .')';
-
+	if (function_exists('tester_url_ecrire'))
+		$p->code = '(tester_url_ecrire(' . $code . ') ? generer_url_ecrire(' . $fonc .') : "")';
+	else
+		$p->code = 'generer_url_ecrire(' . $fonc .')';
 	#$p->interdire_scripts = true;
 	return $p;
 }
