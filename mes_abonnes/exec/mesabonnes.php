66e5fe3b8ca1101614a7f7005b598ec1eed57b31
erational@erational.org <erational@erational.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-06-03 08:53:14 +0000

    nettoyage superflu
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@38562 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- mes_abonnes/exec/mesabonnes.php
+++ mes_abonnes/exec/mesabonnes.php
@@ -3,26 +3,13 @@
 //  Mes abonnés
 // ---------------------------------------------------------
 
-
-include_spip('inc/presentation');
-include_spip('inc/distant');
-include_spip('inc/affichage');
-include_spip('inc/meta');
-include_spip('inc/filtres');
-include_spip('inc/lang');
-
-
 function exec_mesabonnes(){ 
   global $connect_statut;
 	global $connect_toutes_rubriques;
- 
-  include_spip("inc/charsets"); 
-  include_spip("inc_presentation");
-  
 
   // main ------------------------------------------------------  
 	$commencer_page = charger_fonction('commencer_page', 'inc');
-  echo $commencer_page(_T('malettre:ma_lettre'),_T('malettre:ma_lettre'),_T('malettre:ma_lettre'));	  
+  echo $commencer_page(_T('mesabonnes:mes_abonnes'),_T('mesabonnes:mes_abonnes'),_T('mesabonnes:mes_abonnes'));	  
  
 	if ($connect_statut == "0minirezo" && $connect_toutes_rubriques) {	  // admin restreint (connect_toutes_rubriques si admin)
 		echo gros_titre(_T('mesabonnes:mes_abonnes'),'', false);
@@ -31,15 +18,15 @@ function exec_mesabonnes(){
     echo debut_droite('', true);
     echo "<p>"._T('mesabonnes:export_abonnes')."</p>";
     
-    // inspi ecrire/inc/stastiques.php
-		
-		include_spip('inc/acces');
+    // inspi ecrire/inc/stastiques.php		
+		include_spip('inc/acces');		
 		$args = array();
 		$fond = "mesabonnes";
 	  $args = param_low_sec($fond, $args, '', 'transmettre');
 	  $url = generer_url_public('transmettre', $args);
 	  echo "<ul><li><a href='$url'>"._T('mesabonnes:export_abonnes_csv')."</a></li>";
 	  
+	  $args = array();
 	  $fond = "mesabonnes_maxbulk";
 	  $args = param_low_sec($fond, $args, '', 'transmettre');
 	  $url = generer_url_public('transmettre', $args);
