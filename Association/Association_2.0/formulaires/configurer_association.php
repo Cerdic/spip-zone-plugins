36e355231b770f49a373448a164862cdf22a518c
esj@rezo.net <esj@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-06-03 12:49:28 +0000

    [source:_plugins_/Association/Association_2.0 Association]: reprendre la RegExp de CFG pour déterminer les meta à sauver plutôt que de le faire à la main.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@38569 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- Association/Association_2.0/formulaires/configurer_association.php
+++ Association/Association_2.0/formulaires/configurer_association.php
@@ -16,32 +16,22 @@ function formulaires_configurer_association_charger_dist(){
 	return $GLOBALS['asso_metas'];
 }
 
+// version amelioree de la RegExp de cfg_formulaire.
+define('_EXTRAIRE_SAISIES', 
+	'#<(?:(select|textarea)|input type=["\'](text|password|checkbox|radio|hidden|file)["\']) name=["\'](\w+)(\[\w*\])?["\'](?: class=["\']([^\'"]*)["\'])?( multiple=)?[^>]*?>#ims');
+
 function formulaires_configurer_association_traiter_dist(){
-	foreach (array(
-		       "activites",
-		       "classe_banques",
-		       "comptes",
-		       "cp",
-		       "declaration",
-		       "dons",
-		       "email",
-		       "indexation",
-		       "nom",
-		       "pc_activites",
-		       "pc_cotisations",
-		       "pc_dons",
-		       "pc_prets",
-		       "pc_ventes",
-		       "prefet",
-		       "prets",
-		       "rue",
-		       "siret",
-		       "telephone",
-		       "ventes",
-		       "ville") as $k) {
-		$v = _request($k);
-		ecrire_meta($k, $v, 'oui', 'asso_metas');
-  }
+
+	$form = _DIR_PLUGINS . 'Association_2.0'.'/formulaires/configurer_association.html';
+	$form = $form ? file_get_contents($form) : '';
+	if (!$form) spip_log('configurer_association sans formulaire');
+	if (preg_match_all(_EXTRAIRE_SAISIES, $form, $r, PREG_SET_ORDER)) {
+		foreach($r as $regs) {
+			$k = $regs[3];
+			$v = _request($k);
+			ecrire_meta($k, $v, 'oui', 'asso_metas');
+		}
+	}
 	return array('redirect' => generer_url_ecrire('association'));
 }
 ?>
