f433402dc6c43599e210d32fed0c56d44f61fe29
marcimat@rezo.net <marcimat@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-06-04 12:34:20 +0000

    Traduction anglaise (Mark Baber)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@38593 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ champs_extras2/core/lang/cextras_en.php
@@ -0,0 +1,20 @@
+<?php
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cextra_par_defaut' => 'Default value',
+	
+	//T
+	'table_article' => 'Articles',
+	'table_auteur' => 'Authors',
+	'table_breve' => 'News items',
+	'table_groupes_mot' => 'Keyword groups',
+	'table_mot' => 'Keywords',
+	'table_rubrique' => 'Sections',
+	'table_site' => 'Sites',
+	
+	'type' => '@type@',
+#	'type_input'=>'Afficher une saisie input de type texte',
+#	'type_textarea'=>'Afficher une saisie textarea',
+);
+?>
