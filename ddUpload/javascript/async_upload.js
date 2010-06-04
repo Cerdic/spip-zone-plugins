759a0c873d7e17c623d4a34e1caa17ff62f3c714
renato@rezo.net <renato@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-06-04 22:29:28 +0000

    corrige dragover
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@38598 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- ddUpload/javascript/async_upload.js
+++ ddUpload/javascript/async_upload.js
@@ -239,8 +239,10 @@ jQuery.fn.ddUpload = function(success) {
     if($(e.target).is(".ddArea") || $(e.target).closest(".ddArea").length) { 
       if(!!($.browser.safari || $.browser.msie || $.browser.opera)==false) {
         e.originalEvent.dataTransfer.dropEffect = undefined;
+        return false;
+      } else {
+        return true;
       }
-      return true;
     } else {
       e.originalEvent.dataTransfer.dropEffect = 'none';
     }
