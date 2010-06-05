51368d3d1be93a27f5d1121db7855986341a653b
xdjuj@apsulis.com <xdjuj@apsulis.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-06-05 22:03:08 +0000

    Parfaitement inutile mais pour l'indentation...
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@38615 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- seo/seo_fonctions.php
+++ seo/seo_fonctions.php
@@ -47,10 +47,9 @@ function generer_google_analytics(){
 	_gaq.push(['_trackPageview']);
 	(function() {
 		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-	  })();
-
+		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	})();
 </script>
 ";
 		
