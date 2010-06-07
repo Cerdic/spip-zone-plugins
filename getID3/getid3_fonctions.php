a44494ae002ba75d3ca57b8cf2449fbb782daea7
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-06-07 13:42:29 +0000

    Une fonction de convertion du temps en seconde vers la forme hh:mm:ss ou mm:ss
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@38660 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- getID3/getid3_fonctions.php
+++ getID3/getid3_fonctions.php
@@ -19,4 +19,23 @@ function recuperer_id3_doc($id_document,$info = "", $mime = "",$retour='oui'){
 	return $output;
 }
 
+/**
+ * Converti une durée en secondes en une durée affichable et lisible hh:mm:ss ou mm:ss
+ * @param int/float $temps_secondes le nombre de secondes
+ */
+function getid3_duree($temps_secondes){
+	$diff_hours    = floor($temps_secondes/3600);
+	$temps_secondes -= $diff_hours   * 3600;
+	$diff_hours = (($diff_hours > 0) && ($diff_hours < 10)) ? '0'.$diff_hours : $diff_hours;
+
+	$diff_minutes  = floor($temps_secondes/60);
+	$temps_secondes -= $diff_minutes * 60;
+	$diff_minutes = (($diff_minutes > 0) && ($diff_minutes < 10)) ? '0'.$diff_minutes : $diff_minutes;
+
+	$temps_secondes = (($temps_secondes > 0) && ($temps_secondes < 10)) ? '0'.$temps_secondes : $temps_secondes;
+
+	$str = (($diff_hours > 0) ? $diff_hours.':':'').(($diff_minutes > 0) ? $diff_minutes:'00').':'.floor($temps_secondes);
+
+	return $str;
+}
 ?>
