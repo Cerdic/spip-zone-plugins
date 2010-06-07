c7356955d09490533b66307d468c8122a423dbd3
pierrekuhn82@gmail.com <pierrekuhn82@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-06-07 17:40:50 +0000

    Gestion des forum pour les sites et rajout dans la boucle forum du coup.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@38674 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- zpipcoop/lang/zpipcoop_fr.php
+++ zpipcoop/lang/zpipcoop_fr.php
@@ -91,6 +91,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 //R
 'redaction' => 'R&eacute;daction',
+'repondre_site' => 'R&eacute;pondre &agrave; ce site',
 'retour' => 'Retour',
 
 //S
@@ -98,6 +99,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 //T
 'texte_modifier_breve' => 'Modifier la br&egrave;ve :',
+'tickets' => 'Tickets',
 'toutes_les_breves' => 'Toutes les br&egrave;ves',
 'tous_les_sites' => 'Tous les sites',
 'tous_les_tickets' => 'Tous les tickets',
