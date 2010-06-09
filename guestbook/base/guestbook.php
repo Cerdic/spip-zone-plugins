ef24269602204d94944a9e8bfd6b77e711ce8d93
prigent.yohann@gmail.com <prigent.yohann@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-06-09 23:07:59 +0000

    Renomage un peu plus logique de la création de tables. D'ailleurs, j'ai oublié de dire qu'on utilise insert_head_css et non plus insert_head pour introduire le seul fichier css qu'on a dans le head.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@38727 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- guestbook/base/guestbook.php
+++ guestbook/base/guestbook.php
@@ -8,37 +8,56 @@
 	 * Pour plus de details voir le fichier COPYING.txt.
 	 *  
 	 **/
-	 
-include_spip('inc/meta');
-include_spip('base/create');
+if (!defined("_ECRIRE_INC_VERSION")) return;
 
-function guestbook_upgrade($nom_meta_base_version,$version_cible){
-	$current_version = 0.0;
-	if (isset($GLOBALS['meta'][$nom_meta_base_version])) {
-		$current_version = $GLOBALS['meta'][$nom_meta_base_version];
-	}
-	if ((!isset($GLOBALS['meta'][$nom_meta_base_version])) || (($current_version = $GLOBALS['meta'][$nom_meta_base_version])!=$version_cible)){
-		if ($current_version==0.0){
-			creer_base();
-			ecrire_meta($nom_meta_base_version, $version_cible,'non');
-		}
-		if (version_compare($current_version,'2.0','<')){
-			maj_tables('spip_guestbook');
-			maj_tables('spip_guestbook_reponses');
-			ecrire_meta($nom_meta_base_version,$current_version='2.0');
-		}
-		if (version_compare($current_version,'2.1','<')){
-			maj_tables('spip_guestbook');
-			maj_tables('spip_guestbook_reponses');
-			ecrire_meta($nom_meta_base_version,$current_version='2.1');
-		}
-		ecrire_metas();
-	}
+function guestbook_declarer_tables_interfaces($interface){
+	$interface['table_des_tables']['guestbook'] = 'guestbook';
+	$interface['table_des_tables']['guestbook_reponses'] = 'guestbook_reponses';
+	return $interface;
 }
-function guestbook_vider_tables($nom_meta_base_version) {
-	sql_drop_table("spip_guestbook");
-	sql_drop_table("spip_guestbook_reponses");
-	effacer_meta($nom_meta_base_version);
-	ecrire_metas();
+
+function guestbook_declarer_tables_principales($tables_principales){
+	// TABLE spip_guestbook
+	$spip_guestbook = array(
+		"id_message" 	=> "INT(10) NOT NULL AUTO_INCREMENT",
+		"message" 	=> "text NOT NULL",
+		"email" 	=> "text NOT NULL",
+		"nom" => "VARCHAR(100) NOT NULL DEFAULT '0'",
+		"prenom" => "text NOT NULL",
+		"pseudo" => "text NOT NULL",
+		"ville" => "text NOT NULL",
+		"statut" => "VARCHAR(8) NOT NULL",
+		"ip" => "VARCHAR(15) NOT NULL",
+		"note" => "INT(2) NOT NULL",
+		"date"	=> "DATETIME");
+	
+	$spip_guestbook_key = array(
+		"PRIMARY KEY" => "id_message");
+	
+	$tables_principales['spip_guestbook'] = array(
+		'field' => &$spip_guestbook,
+		'key' => &$spip_guestbook_key);
+		
+	return $tables_principales;
+}
+function guestbook_declarer_tables_auxiliaires($tables_auxiliaires){
+	// TABLE spip_guestbook_reponses
+	$spip_guestbook_reponses = array(
+		"id_reponse" => "INT(10) NOT NULL AUTO_INCREMENT",
+		"id_message" => "INT(10) NOT NULL",
+		"id_auteur" => "INT(10) NOT NULL",
+		"message" =>  "TEXT",
+		"statut" => "VARCHAR(8) NOT NULL",
+		"date"	=> "DATETIME");
+	
+	$spip_guestbook_reponses_key = array(
+		"PRIMARY KEY" 	=> "id_reponse, id_message",
+		"KEY id_auteur" => "id_auteur");
+	
+	$tables_auxiliaires['spip_guestbook_reponses'] = array(
+		'field' => &$spip_guestbook_reponses,
+		'key' => &$spip_guestbook_reponses_key);
+
+	return $tables_auxiliaires;
 }
 ?>
