eb7baba39468ec3b6e6bf9c5ee71ff9642d88005
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-06-09 13:53:46 +0000

    neolao_audio et non pas neolao tout seul pour le nom du player audio par défaut
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@38703 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- lecteur_multimedia_v2/base/lm2_upgrade.php
+++ lecteur_multimedia_v2/base/lm2_upgrade.php
@@ -3,22 +3,22 @@ include_spip('inc/meta');
 
 function lm2_upgrade($nom_meta_base_version,$version_cible){
 	$current_version = "0.0";
-		
+
 	if (isset($GLOBALS['meta'][$nom_meta_base_version]))
 		$current_version = $GLOBALS['meta'][$nom_meta_base_version];
-		
+
 	if ($current_version=="0.0") {
 		// players par defaut
 		ecrire_meta(
 			'lecteur_multimedia',
 				serialize(array(
-					'lecteur_audio' => 'neolao',
+					'lecteur_audio' => 'neolao_audio',
 					'lecteur_video' => 'neolao_video'
 				))
 			);
 		ecrire_meta($nom_meta_base_version,$current_version=$version_cible);
 	}
-		
+
 }
 
 function lm2_vider_tables($nom_meta_base_version) {
