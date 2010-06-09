533d6236d5aabb146254a9208d56fb25b99d1450
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-06-09 19:02:24 +0000

    Un formulaire de configuration un peu plus compliant
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@38712 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ doc2img/lang/doc2img_fr.php
@@ -0,0 +1,19 @@
+<?php
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cfg_explication_format' => 'Utilisez une virgule "," pour s&eacute;parer les formats',
+	'cfg_label_agrandissements' => 'Autoriser les agrandissements',
+	'cfg_label_conversion_auto' => 'Conversion automatique lorsque qu\'un document est joint',
+	'cfg_label_format' => 'Extensions de fichiers &agrave; traiter (pdf, tiff, ...)',
+	'cfg_label_format_sortie' => 'Le format de sortie par d&eacute;faut',
+	'cfg_label_hauteur' => 'Hauteur par d&eacute;faut',
+	'cfg_label_largeur' => 'Largeur par d&eacute;faut',
+	'cfg_label_proportions' => 'Conserver les proportions',
+	'cfg_label_repertoire_sortie' => 'R&eacute;pertoire par d&eacute;faut',
+	'cfg_legende_formats_entree' => 'Format d\'entr&eacute;e',
+	'cfg_legende_formats_sortie' => 'Les sorties',
+);
+
+?>
