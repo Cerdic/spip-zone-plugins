a544219f0a7dafbd2e0fcfa589dc483d2f39494f
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-06-11 22:47:29 +0000

    re oups ... il est tard
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@38760 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spipmotion/action/spipmotion_ajouter_file_encodage.php
+++ spipmotion/action/spipmotion_ajouter_file_encodage.php
@@ -50,14 +50,14 @@ function action_spipmotion_ajouter_file_encodage_post($r){
  */
 function spipmotion_supprimer_versions($id_document){
 
-	$v = sql_select("id_document,id_vignette,fichier","spip_documents","id_orig=".intval($id_document));
+	$v = sql_select("id_document,id_vignette,fichier","spip_documents","id_document=".intval($id_document));
 
 	include_spip('inc/documents');
-
 	/**
 	 * Pour chaque version du document original
 	 */
 	while($version = sql_fetch($v)){
+		spip_log('suppression de ','test');spip_log($version,'test');
 		/**
 		 * On ajoute l'id_document dans la liste des documents
 		 * à supprimer de la base
@@ -76,6 +76,7 @@ function spipmotion_supprimer_versions($id_document){
 		if($version['id_vignette'] > 0){
 			$liste[] = $version['id_vignette'];
 			$fichier = sql_getfetsel('fichier','spip_documents','id_document='.$version['id_vignette']);
+			spip_log('suppression du fichier '.$fichier,'test');
 			if (@file_exists($f = get_spip_doc($fichier))) {
 				supprimer_fichier($f);
 			}
@@ -83,6 +84,7 @@ function spipmotion_supprimer_versions($id_document){
 
 	}
 	if(is_array($liste)){
+		spip_log($liste,'test');
 		$in = sql_in('id_document', $liste);
 		sql_delete("spip_documents", $in);
 		sql_delete("spip_documents_liens", $in);
