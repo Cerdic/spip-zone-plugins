1142d82acd02b2616357ea874227a8ec9e15cdb2
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-06-13 19:48:37 +0000

    un seul argument pour le pipeline insert_head, et indentation en tabulations
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@38806 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- html5/html5_pipelines.php
+++ html5/html5_pipelines.php
@@ -2,7 +2,7 @@
 
 // Charge la CSS si pas encore fait, par compatibilite avec SPIP 2.0 (pas encore
 // de #INSERT_HEAD_CSS) puis insere un hack javascript pour IE
-function html5_insert_head($flux, $lang) {
+function html5_insert_head($flux) {
 	$javascript_ie = find_in_path('javascript/html5-ie.js');
 	$flux .= html5_insert_head_css('')
 		. "<!--[if IE]>\n"
@@ -12,11 +12,11 @@ function html5_insert_head($flux, $lang) {
 }
 
 function html5_insert_head_css($flux='') {
-        static $html5_headcss_done = false;
-        if (!$html5_headcss_done) {
+	static $html5_headcss_done = false;
+	if (!$html5_headcss_done) {
 		$reset = find_in_path('reset_html5.css');
 		$flux .= "<link rel='stylesheet' type='text/css' media='all' href='$reset' />\n";
-        	$html5_headcss_done = true;
+    $html5_headcss_done = true;
 	}
 	return $flux;
 }
