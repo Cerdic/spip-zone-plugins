7f56f626be600136bb3d2a50010dc344d2051a8d
webmaster@geomaticien.com <webmaster@geomaticien.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-06-22 22:37:44 +0000

    - English lang file - Bouton de fermeture de la palette en entité HTML (pour éviter un bug constaté sous firefox 3.5 avec spip 2.1 - ce bug ne se produisait pas sous spip 2.0.10)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@38973 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ palette/lang/palette_en.php
@@ -0,0 +1,13 @@
+<?php
+// Ceci est un fichier langue de SPIP
+$GLOBALS[$GLOBALS['idx_lang']] = array( // Espace ecrire
+	'palette' => "Palette",
+	'fermer' => 'Close palette',
+	'palette_public' => "Activate Palette for public area",
+	'palette_ecrire' => "Activate Palette for private area",
+	'config_desc' => '<strong>Palette</strong> adds automatically a color picker wheel to an input field,
+		if you add to it a CSS class &laquo;palette&raquo;',
+	
+);
+
+?>
