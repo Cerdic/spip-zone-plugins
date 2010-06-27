0ad7915ea2852ba9eae786d8e0d206d8cddef6f1
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-06-27 18:48:14 +0000

    les fichiers de formulaires à l'inscriptions
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@39054 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ rubrique_a_linscription/rubrique_a_linscription.php
@@ -0,0 +1,17 @@
+<?php
+
+function rubrique_a_linscription_formulaire_traiter($flux){
+	if ($flux['args']['form']=='inscription'){
+		$mail = _request('mail_inscription');
+		$nom_inscription = str_replace('@',' (chez) ',_request('nom_inscription'));
+		include_spip('base/abstract_sql');
+		$id_auteur = sql_getfetsel('id_auteur','spip_auteurs','email='.sql_quote($mail));
+		$id_rubrique = sql_insertq("spip_rubriques", array( 'titre'=> _T('Rubrique de '.$nom_inscription), 'id_secteur'=> 0));
+		sql_update("spip_rubriques",array("id_secteur"=>$id_rubrique), "id_rubrique=".$id_rubrique);
+		sql_insertq('spip_auteurs_rubriques', array(
+		'id_auteur' => $id_auteur,
+		'id_rubrique' => $id_rubrique));
+		spip_log('Création de la rubrique pour l\'auteur '.$mail,'rubrique_a_linscription');
+	}
+	return $flux;	
+}
