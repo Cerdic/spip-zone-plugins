02b9f15cf404e1532d2c028e6b6b23d441617743
brunobergot@gmail.com <brunobergot@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-07-01 19:50:51 +0000

    et oups :p
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@39144 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- jquery_ui/jqueryui_pipelines.php
+++ jquery_ui/jqueryui_pipelines.php
@@ -4,7 +4,7 @@ function jqueryui_jquery_plugins($plugins){
 	$config = @unserialize($GLOBALS['meta']['jqueryui']);
 	
 	if (!is_array($config))
-		$config = array();
+		$config['plugins'] = array();
 	
 	foreach ($config['plugins'] as $val) {
 		$plugins[] = _DIR_JQUERYUI."ui/".$val.".js";
