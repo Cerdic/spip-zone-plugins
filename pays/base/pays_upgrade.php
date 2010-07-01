168fe12f9fca39b0967c9602e2c0047f82925afe
cyril@cym.fr <cyril@cym.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-07-01 08:32:01 +0000

    Ajout du plugin pays; inspiré de la partie pays du plugin spip_geographie; integre les codes iso 3166
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@39124 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ pays/base/pays_upgrade.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Plugin Pays pour Spip 2.0
+ * Licence GPL
+ * Auteur Organisation Internationale de Normalisation http://www.iso.org/iso/fr/country_codes/iso_3166_code_lists.htm
+ * Cedric Morin et Collectif SPIP pour version spip_geo_pays
+ * Portage sous SPIP par Cyril MARION - Ateliers CYM http://www.cym.fr
+ *
+ */
+
+include_spip('inc/meta');
+include_spip('base/create');
+include_spip('base/pays_peupler_base');
+
+function pays_upgrade($nom_meta_base_version, $version_cible){
+
+	$current_version = "0.0";
+	if (isset($GLOBALS['meta'][$nom_meta_base_version]))
+		$current_version = $GLOBALS['meta'][$nom_meta_base_version];
+	
+	if ($current_version=="0.0") {
+		creer_base();
+		peupler_base();
+		ecrire_meta($nom_meta_base_version, $current_version=$version_cible);
+	}
+}
+function pays_vider_tables($nom_meta_base_version) {
+
+	sql_drop_table("spip_pays");
+	effacer_meta($nom_meta_base_version);
+}
+
+
+?>
