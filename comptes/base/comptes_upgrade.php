4ae3cb207a3bec42d83e81ad366096ffd605fbb0
cyril@cym.fr <cyril@cym.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-07-01 15:25:30 +0000

    Ajout d'explications sur la page de gestion du plugin; ajout des listes de comptes et de contacts pour vérifs;
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@39137 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- comptes/base/comptes_upgrade.php
+++ comptes/base/comptes_upgrade.php
@@ -8,11 +8,10 @@ include_spip('inc/meta');
 include_spip('base/create');
 include_spip('base/peupler_base');
 
-
 function comptes_upgrade($nom_meta_base_version, $version_cible){
 	/**
 	 *
-	 *  11/01/2009 : ajout table spips_emails, version 1.0.1
+	 *  11/01/2009 : ajout table spip_emails, version 1.0.1
 	 *
 	 */
 	 
@@ -25,7 +24,7 @@ function comptes_upgrade($nom_meta_base_version, $version_cible){
 		peupler_base_comptes();
 		ecrire_meta($nom_meta_base_version, $current_version=$version_cible);
 	}
-	if (version_compare($current_version,"1.0.1","<")){
+	if (version_compare($current_version,"1.0.2","<")){
 		maj_tables('spip_comptes');
 		maj_tables('spip_contacts');
 		maj_tables('spip_comptes_contacts');
@@ -38,7 +37,7 @@ function comptes_upgrade($nom_meta_base_version, $version_cible){
 		maj_tables('spip_champs');
 		maj_tables('spip_champs_liens');
 		
-		ecrire_meta($nom_meta_base_version,$current_version="1.0.1");
+		ecrire_meta($nom_meta_base_version,$current_version="1.0.2");
 	}
 }
 
--- comptes/lang/comptes_fr.php
+++ comptes/lang/comptes_fr.php
@@ -3,7 +3,13 @@
 if (!defined("_ECRIRE_INC_VERSION")) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-	// C
-	'comptes' => 'Plugin Comptes &amp; Contacts'
+	// I
+	'info_cadre_navigation' => 'Ce plugin est en cours de d&eacute;veloppement. Son objectif est d\'&eacute;tendre le auteurs SPIP en ajoutant 2 tables d\'extension, Comptes (personnes morales) et Contact (personnes physiques).',
+
+	// N
+	'nom_bouton_plugin' => 'Comptes &amp; Contacts',
+	
+	// T
+	'titre_page_gestion' => 'Page de gestion du plugin Comptes &amp; contacts',
 );
 ?>
