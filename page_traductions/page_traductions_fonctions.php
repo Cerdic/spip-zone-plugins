4e3510eb1769f648a56ddc29d66a2977418ae68c
suske@brubel.net <suske@brubel.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-07-04 23:26:14 +0000

    bien declarer la fonction (ça fonctionnait depuis 2007 grâce à mes_fonctions.php :-p )
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@39178 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- page_traductions/page_traductions_fonctions.php
+++ page_traductions/page_traductions_fonctions.php
@@ -2,7 +2,7 @@
 
 // menu_lang plat sans URL sur la langue en cours
 // Voir http://www.spip-contrib.net/Formulaire-menu-lang-plat-sans-URL et FORUM
-function traductions_page_lang_plat ($langues) {
+function lang_plat ($langues) {
     include_spip('inc/charsets');
     $list_lang = '';
     $tab_langues = explode(",",$langues);
