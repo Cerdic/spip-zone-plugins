cba8002d62bddd9de79d43aba424e8b676cbc98c
cyril@cym.fr <cyril@cym.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-07-06 15:58:22 +0000

    Ajout de trads pour le tableau des pays;
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@39189 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ pays/lang/pays_en.php
@@ -0,0 +1,16 @@
+<?php
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	// C
+	'code' => 'ISO3166-1 code',
+	
+	// I
+	'id_pays' => 'ID',
+	
+	// N
+	'nom' => 'Country name',
+	
+	// T
+	'titre_page_test' => 'List of Planet Earth countries',
+);
+
+?>
--- pays/lang/pays_fr.php
+++ pays/lang/pays_fr.php
@@ -1,5 +1,14 @@
 <?php
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'code' => 'Code ISO3166-1',
+
+	// I
+	'id_pays' => 'ID',
+
+	// N
+	'nom' => 'Nom du pays',
 	
 	// T
 	'titre_page_test' => 'La liste des pays du monde',
