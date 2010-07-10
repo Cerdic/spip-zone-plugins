2ecd5e78e4e0679b873708661479f7162c545f6c
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-07-10 14:23:05 +0000

    Un bouton de menu plus conforme
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@39223 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- actijour_2/actijour_pipe.php
+++ actijour_2/actijour_pipe.php
@@ -13,19 +13,6 @@
 
 $p=explode(basename(_DIR_PLUGINS)."/",str_replace('\\','/',realpath(dirname(__FILE__))));
 define('_DIR_PLUGIN_ACTIJOUR',(_DIR_PLUGINS.end($p)));
- 
-	# bouton interface spip
-	function actijour_ajouterBoutons($boutons_admin) {
-		// si on est admin
-		if ($GLOBALS['connect_statut'] == "0minirezo" && $GLOBALS["connect_toutes_rubriques"]) {
-		  // on voit le bouton dans la barre "statistiques"
-		  $boutons_admin['statistiques_visites']->sousmenu["actijour_pg"]= new Bouton(
-			_DIR_PLUGIN_ACTIJOUR."img_pack/activ_jour.gif",  // icone
-			_T('acjr:activite_du_jour')	// titre  
-			);
-		}
-		return $boutons_admin;
-	}
 
 	# style + js
 	function actijour_header_prive($flux) {
