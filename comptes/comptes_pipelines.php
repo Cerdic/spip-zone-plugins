dc400cd37a80ad8152afd10a75697582b2910e3f
cam.lafit@azerttyu.net <cam.lafit@azerttyu.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-07-14 18:05:30 +0000

    Afficher les coordonnées des auteurs
     * Simple liste des adresses d'auteurs
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@39277 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ comptes/comptes_pipelines.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Plugin Comptes & Contacts pour Spip 2.0
+ * Licence GPL (c) 2009 - 2010 - Ateliers CYM
+ */
+
+/**
+ *
+ * Insertion dans le pipeline affiche_milieu
+ * Dans la page auteur_infos, insertion des champs spécifiques d'Inscription2
+ *
+ * @return array Le $flux modifié
+ * @param array $flux
+ */
+function comptes_affiche_milieu($flux){
+	if($flux['args']['exec'] == 'auteur_infos') {
+#		$exceptions_des_champs_auteurs_elargis = pipeline('i2_exceptions_des_champs_auteurs_elargis',array());
+#		$legender_auteur_supp = recuperer_fond('prive/inscription2_fiche',array('id_auteur'=>$flux['args']['id_auteur'],'exceptions'=>$exceptions_des_champs_auteurs_elargis));
+		$flux['data'] .= recuperer_fond('prive/listes/adresses',array('id_auteur'=>$flux['args']['id_auteur']));
+	}
+	return $flux;
+}
+
+
+
+
+
+?>
