43549d89f456389d532d130fc9f6b9791d1a3cec
cyril@cym.fr <cyril@cym.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-07-20 16:42:48 +0000

    fichier langue : correction de quelques accents;
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@39400 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- coordonnees/lang/cop_fr.php
+++ coordonnees/lang/cop_fr.php
@@ -8,19 +8,19 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'adresse_pro' => 'Professionnel',
 	'adresse_perso' => 'Domicile',
 	'ajouter_adresse' => 'Ajouter une adresse',
-	'ajouter_telephone' => 'Ajouter un téléphone',
+	'ajouter_telephone' => 'Ajouter un t&eacute;l&eacute;phone',
 	'ajouter_email' => 'Ajouter un email',
 
 	// C
 	'confirmer_suppression_adresse' => "Voulez-vous vraiment supprimer cette adresse ?",
-	'confirmer_suppression_numero' => "Voulez-vous vraiment supprimer ce numéro ?",
+	'confirmer_suppression_numero' => "Voulez-vous vraiment supprimer ce num&eacute;ro ?",
 	'confirmer_suppression_email' => "Voulez-vous vraiment supprimer ce courriel ?",
 	
 	// E
 	'emails' => 'Courriels',
-	'editer_adresse' => 'Éditer une adresse',
-	'editer_numero' => 'Éditer un numéro',
-	'editer_email' => 'Éditer un courriel',
+	'editer_adresse' => '&Eacute;diter une adresse',
+	'editer_numero' => '&Eacute;diter un num&eacute;ro',
+	'editer_email' => '&Eacute;diter un courriel',
 	'explication_type_adresse' => "Le type peut être 'perso' ou 'pro'.",
 	'explication_type_numero' => "Le type peut être 'perso' ou 'pro' ou 'fax'.",
 	'explication_type_email' => "Le type peut être 'perso' ou 'pro'.",
@@ -31,13 +31,13 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// I
 	'item_nouveau_adresse' => 'Nouvelle adresse',
-	'item_nouveau_numero' => 'Nouveau numéro',
+	'item_nouveau_numero' => 'Nouveau num&eacute;ro',
 	'item_nouveau_email' => 'Nouvelle courriel',
 	'info_adresse_utilisee_par' => "Adresse utilis&eacute;e par :",
-	'info_numero_utilise_par' => "Numéro utilis&eacute; par :",
+	'info_numero_utilise_par' => "Num&eacute;ro utilis&eacute; par :",
 	'info_email_utilise_par' => "Courriel utilis&eacute; par :",
 	'info_gauche_numero_adresse' => 'N° Adresse',
-	'info_gauche_numero_numero' => 'N° Numéro',
+	'info_gauche_numero_numero' => 'N° Num&eacute;ro',
 	'info_gauche_numero_email' => 'N° Email',
 	
 	// L
@@ -47,30 +47,30 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'label_numero' => "Numero",
 	'label_email' => "Courriel",
 	'label_type_adresse' => "Type d'adresse",
-	'label_type_numero' => "Type de numéro",
+	'label_type_numero' => "Type de num&eacute;ro",
 	'label_type_email' => "Type de courriel",
 	'label_pays' => "Pays",
 	'label_ville' => "Ville",
 	'label_voie' => "Voie",
 	
 	// N
-	'numeros' => 'Numéros',
+	'numeros' => 'Num&eacute;ros',
 	'nouvelle_adresse' => 'Nouvelle adresse',
-	'nouveau_numero' => 'Nouveau numéro',
+	'nouveau_numero' => 'Nouveau num&eacute;ro',
 	'nouvel_email' => 'Nouveau courriel',
 
 	// M
 	'modifier_adresse' => "Modifier cette adresse",
-	'modifier_numero' => "Modifier ce numéro",
+	'modifier_numero' => "Modifier ce num&eacute;ro",
 	'modifier_email' => "Modifier ce courriel",
 
 	// S
 	'supprimer_adresse' => "Supprimer cette adresse",
-	'supprimer_numero' => "Supprimer ce numéro",
+	'supprimer_numero' => "Supprimer ce num&eacute;ro",
 	'supprimer_email' => "Supprimer ce courriel",
 	
 	// T
-	'titre_coordonnees'	=> 'Coordonnées',
+	'titre_coordonnees'	=> 'Coordonn&eacute;es',
 
 );
 
