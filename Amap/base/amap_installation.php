be40d48419703ff6718451d7acdc3e8babc8a728
pierrekuhn82@gmail.com <pierrekuhn82@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-07-21 10:30:40 +0000

    Rajout de la création des mots clefs utiliser pour créer les saisons.
    Version 0.1.1 de base
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@39421 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- Amap/base/amap_installation.php
+++ Amap/base/amap_installation.php
@@ -8,19 +8,32 @@
 
 include_spip('inc/meta');
 include_spip('base/create');
+include_spip('base/amap_tables');
 
 function amap_upgrade($nom_meta_version_base, $version_cible){
 	$current_version = 0.0;
-	if (   (!isset($GLOBALS['meta'][$nom_meta_version_base]))
-			|| (($current_version = $GLOBALS['meta'][$nom_meta_version_base]) != $version_cible)
-		){
-			if (version_compare($current_version,'0.0','<=')){
-				// Creation des tables
-				include_spip('base/create');
-				include_spip('base/abstract_sql');
-				creer_base();
-				ecrire_meta($nom_meta_version_base, $current_version=$version_cible, 'non');
-				}
+		if ((!isset($GLOBALS['meta'][$nom_meta_version_base])) || (($current_version = $GLOBALS['meta'][$nom_meta_version_base]) != $version_cible)){
+		if (version_compare($current_version,'0.0','<=')){
+			// Creation des tables
+			include_spip('base/create');
+			include_spip('base/abstract_sql');
+			creer_base();
+			amap_config_site();
+			amap_config_motsclefs();
+			ecrire_meta($nom_meta_version_base, $current_version=$version_cible, 'non');
+			}
+		if (version_compare($current_version,'0.1.1','<')) {
+			ecrire_meta('config_precise_groupes', 'oui','non');
+			ecrire_meta('articles_mots', 'oui','non');
+			create_groupe("modalites_affichage_article", "L'article correspondant aux distributions d'une saison donnée sera associé au mot clé « amap_agenda »", "", 'oui', 'non', 'non', 'non', 'non', 'oui', 'non', 'oui', 'non', 'non');
+				create_mot("modalites_affichage_article", "amap_contrat", "Affecter ce mot clef à l'article concernant les contrats.", "");
+				create_mot("modalites_affichage_article", "amap_distribution", "Affecter ce mot clef à l'article concernant la distribution.", "");
+				create_mot("modalites_affichage_article", "amap_responsable", "Affecter ce mot clef à l'article concernant le responsable.", "");
+				create_mot("modalites_affichage_article", "amap_sortie", "Affecter ce mot clef à l'article concernant la sorties.", "");
+				create_mot("modalites_affichage_article", "amap_vacance", "Affecter ce mot clef à l'article concernant les vacances.", "");
+			ecrire_meta($nom_meta_base_version,$current_version='0.1.1','non');
+			spip_log("Amap rajoute des mots clef V0.1.1", "amap_installation");
+		}
 	}
 }
 function amap_vider_tables($nom_meta_version_base){
@@ -44,6 +57,12 @@ function amap_vider_tables($nom_meta_version_base){
 	sql_drop_table('spip_amap_type_contrat');
 	sql_drop_table('spip_amap_vacance');
 	sql_drop_table('spip_amap_variete');
+	supprimer_mot_groupe("modalites_affichage_article", "amap_contrat");
+	supprimer_mot_groupe("modalites_affichage_article", "amap_distribution");
+	supprimer_mot_groupe("modalites_affichage_article", "amap_responsable");
+	supprimer_mot_groupe("modalites_affichage_article", "amap_sortie");
+	supprimer_mot_groupe("modalites_affichage_article", "amap_vacance");
+	vider_groupe("modalites_affichage_article");
 	effacer_meta($nom_meta_version_base);
 }
 ?>
--- Amap/base/amap_tables.php
+++ Amap/base/amap_tables.php
@@ -3,9 +3,190 @@
 /**
 * Plugin AMAP pour Spip 2.0
 * Pierre KUHN
-*
+* Config pris sur Soyezcreateurs
 *
 */
+include_spip('inc/meta');
+
+//fonction qui permet de créer les métas de config du site
+function amap_config_site() {
+	ecrire_meta('config_precise_groupes', 'oui','non');
+	ecrire_meta('articles_mots', 'oui','non');
+	return true;
+}
+
+// GROUPE
+// fonction qui permet de trouver si un groupe de mots clés existe à partir du titre
+function find_groupe($titre) {
+	$titre = addslashes($titre);
+	spip_log("1. (find_groupe) recherche des occurences dans la table spip_groupes_mots de l'id de : $titre", "amap_installation");
+	$count = sql_countsel("spip_groupes_mots", "titre='$titre'");
+	spip_log("2. (find_groupe) resultat de la recherche : $count occurences pour $titre", "amap_installation");
+	return $count;
+}
+
+// fonction pour trouver l'id du groupe de mots clés à partir du titre du groupe
+function id_groupe($titre) {
+	$titre = addslashes($titre);
+	spip_log("1. (id_groupe) selection dans la table spip_groupes_mots de l'id de : $titre", "amap_installation");
+	$result = sql_fetsel("id_groupe", "spip_groupes_mots", "titre='$titre'");
+	$resultat = $result['id_groupe'];
+	spip_log("2. (id_groupe) selection = $resultat pour $titre", "amap_installation");
+	return $resultat;
+}
+
+//fonction qui permet de créer un groupe de mots clés
+function create_groupe($groupe, $descriptif='', $texte='', $unseul='non', $obligatoire='non', $articles='oui', $breves='non', $rubriques='non', $syndic='non', $evenements='non', $minirezo='oui', $comite='oui', $forum='non') {
+	$id_groupe = find_groupe($groupe);
+	$tables_liees = '';
+	if ($articles == 'oui') 
+		$tables_liees.='articles,';
+	if ($breves == 'oui') 
+		$tables_liees.='breves,';
+	if ($rubriques == 'oui') 
+		$tables_liees.='rubriques,';
+	if ($syndic == 'oui') 
+		$tables_liees.='syndic,';
+	if ($evenements == 'oui') 
+		$tables_liees.='evenements,';
+	spip_log("1. (create_groupe) pret a creer groupe : titre = $groupe. retour de find_groupe = $id_groupe", "amap_installation");
+	if ($id_groupe == 0) {
+		$id_insert = sql_insertq(
+			"spip_groupes_mots", array(
+				"id_groupe" => '',
+				"titre" => $groupe,
+				"descriptif" => $descriptif,
+				"texte" => $texte,
+				"unseul" => $unseul,
+				"obligatoire" => $obligatoire,
+				"tables_liees" => $tables_liees,
+				"minirezo" => $minirezo,
+				"comite" => $comite,
+				"forum" => $forum
+			)
+		);
+		spip_log("2. (create_groupe) retour de find_groupe : $id_groupe, donc insertion avec id = $id__insert et titre = $groupe", "amap_installation");
+	} 
+	else if ($id_groupe > 0) {
+		$id_insert = remplacer_groupe($groupe, $descriptif, $texte, $unseul, $obligatoire, $tables_liees, $minirezo, $comite, $forum);
+		spip_log("2. (create_groupe) retour de find_groupe : $id_groupe... passage a remplacer_groupe", "amap_installation");
+	}
+	return $id_insert;
+}
+
+// fonction qui suprime des mots clef
+function supprimer_mot_groupe($nom_groupe,$nom_mot) {
+	$id_groupe = id_groupe($nom_groupe);
+	if ($id_groupe>0) {
+		$id_mot = id_mot($nom_mot, $id_groupe);
+		if ($id_mot>0) {
+			sql_delete("spip_mots", "id_mot=$id_mot");
+			sql_delete("spip_mots_articles", "id_mot=$id_mot");
+			sql_delete("spip_mots_rubriques", "id_mot=$id_mot");
+			sql_delete("spip_mots_syndic", "id_mot=$id_mot");
+			sql_delete("spip_mots_forum", "id_mot=$id_mot");
+		}
+	}
+}
+
+function vider_groupe($nom_groupe) {
+	$id_groupe = id_groupe($nom_groupe);
+	if ($id_groupe>0) {
+		$id_mots = sql_select('id_mot',  'spip_mots',  'id_groupe='.sql_quote($id_groupe));
+		while($id_mot = sql_fetch($id_mots)){
+			sql_delete("spip_mots", "id_mot=".$id_mot['id_mot']);
+			sql_delete("spip_mots_articles", "id_mot=".$id_mot['id_mot']);
+			sql_delete("spip_mots_rubriques", "id_mot=".$id_mot['id_mot']);
+			sql_delete("spip_mots_syndic", "id_mot=".$id_mot['id_mot']);
+			sql_delete("spip_mots_forum", "id_mot=".$id_mot['id_mot']);
+		}
+		sql_delete("spip_groupes_mots", "id_groupe=$id_groupe");
+	}
+}
+
+//fonction qui mets à jour un groupe de mots clés
+function remplacer_groupe($titre, $descriptif, $texte, $unseul, $obligatoire, $tables_liees, $minirezo, $comite, $forum) {
+	$id_groupe = id_groupe($titre);
+	sql_updateq(
+		"spip_groupes_mots", array(
+			"descriptif" => $descriptif,
+			"texte" => $texte,
+			"unseul" => $unseul,
+			"obligatoire" => $obligatoire,
+			"tables_liees" => $tables_liees,
+			"minirezo" => $minirezo,
+			"comite" => $comite,
+			"forum" => $forum
+		), "id_groupe=$id_groupe"
+	);
+	return true;
+}
+
+// MOT
+// fonction qui permet de trouver si un mot clé existe à partir du titre et de l'id du groupe
+function find_mot($titre, $id_groupe) {
+	$titre = addslashes($titre);
+	$count = sql_countsel(
+		"spip_mots", 
+		"titre = '$titre' AND id_groupe = $id_groupe"
+	);
+	return $count;
+}
+
+//fonction qui permet de trouver l'id du mot clé à partir du titre et de l'id du groupe
+function id_mot($titre, $id_groupe) {
+	spip_log("1. (id_mot) debut de recherche de l'id de $titre avec $id_groupe", "amap_installation");
+	$titre = addslashes($titre);
+	$result = sql_fetsel(
+		"id_mot", 
+		"spip_mots", 
+		"titre='$titre' AND id_groupe = $id_groupe"
+	);
+	$id_mot = $result['id_mot'];
+	spip_log("2. (id_mot) retour de la fonction id_mot = $id_mot", "amap_installation");
+	return $id_mot;
+}
+
+//fonction qui permet de créer un mot clé
+function create_mot($groupe, $mot, $descriptif='', $texte='') {
+	$id_groupe = id_groupe($groupe);
+	$find_mot = find_mot($mot, $id_groupe);
+	if ($find_mot == 0) {
+		spip_log("1. (create_mot) debut create_mot. mot inexistant donc creation : $id_groupe - $mot", "amap_installation");
+		$motcle = sql_insertq(
+			"spip_mots", array(
+				"id_mot" => '',
+				"titre" => $mot,
+				"descriptif" => $descriptif,
+				"texte" => $texte,
+				"id_groupe" => $id_groupe,
+				"type" => $groupe
+			)
+		);
+		spip_log("2. (create_mot) mot cle $mot insere sous l'id $motcle dans la table avec groupe = $id_groupe", "amap_installation");
+	}
+	else if ($find_mot > 0) {
+		$id_mot = id_mot($mot, $id_groupe);
+		spip_log("1. (create_mot) mise a jour dans la table du mot cle : $mot", "amap_installation");
+		remplacer_mot($id_mot, $descriptif, $texte, $id_groupe, $groupe);
+	}
+	else {
+		spip_log("insertion impossible ! debug : groupe = $groupe --- id_groupe = $id_groupe", "amap_installation");
+	}
+}
+
+//fonction qui permet de mettre à jour un mot clé 
+function remplacer_mot($id_mot, $descriptif, $texte, $id_groupe, $groupe) {
+	sql_updateq(
+			"spip_mots", array(
+				"descriptif" => $descriptif,
+				"texte" => $texte,
+				"id_groupe" => $id_groupe,
+				"type" => $groupe
+			), "id_mot=$id_mot"
+		);
+	return true;
+}
 
 function amap_declarer_tables_interfaces($interface){
 	//-- Alias
@@ -308,5 +489,15 @@ function amap_declarer_tables_principales($tables_principales){
 		'join' => array('id_famille'=>'id_famille')
 		);
     return $tables_principales;
+}
+//fonction qui permet de créer le contenu
+function amap_config_motsclefs() {
+	//les groupes puis mots
+	create_groupe("modalites_affichage_article", "L'article correspondant aux distributions d'une saison donnée sera associé au mot clé « amap_agenda »", "", 'non', 'non', 'oui', 'non', 'non', 'non', 'oui', 'oui', 'oui', 'non');
+		create_mot("modalites_affichage_article", "amap_contrat", "Affecter ce mot clef à l'article concernant les contrats.", "");
+		create_mot("modalites_affichage_article", "amap_distribution", "Affecter ce mot clef à l'article concernant la distribution.", "");
+		create_mot("modalites_affichage_article", "amap_responsable", "Affecter ce mot clef à l'article concernant le responsable.", "");
+		create_mot("modalites_affichage_article", "amap_sortie", "Affecter ce mot clef à l'article concernant la sorties.", "");
+		create_mot("modalites_affichage_article", "amap_vacance", "Affecter ce mot clef à l'article concernant les vacances.", "");
 }
 ?>
