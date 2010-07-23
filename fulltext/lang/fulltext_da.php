174876537f614da676724c4b4b0ddf285695fcfb
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-07-23 18:17:24 +0000

    langues (fulltext)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@39498 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ fulltext/lang/fulltext_da.php
@@ -0,0 +1,80 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'accents_pas_pris' => 'Accenttegn spiller ingen rolle (id&eacute; eller ide vil give de samme resultater...)',
+	'asie' => 'asien',
+	'asterisque_terminale' => 'vil ikke give resultat: man skal afslutte med stjernen (*)',
+	'aussi' => 'ogs&aring;',
+
+	// C
+	'casse_indifferente' => 'Det er lige meget, om man skriver med store eller sm&aring; bogstaver.',
+	'convertir_myisam' => 'Konverter til MyISAM',
+	'convertir_toutes' => 'Convert all tables to MyISAM', # NEW
+	'convertir_utf8' => 'convert to UTF-8 to restore coherency', # NEW
+	'creer_tous' => 'Dan alle foresl&aring;ede FULLTEXT tabeller',
+
+	// E
+	'enfan' => 'barn',
+	'enfance' => 'barndom',
+	'enfant' => 'barn',
+	'enfanter' => 'barnef&oslash;dsel',
+	'enfantillage' => 'barnagtig',
+	'enfants' => 'barndomsven',
+	'et' => 'OG',
+	'etranger' => 'fremmed',
+	'exemples' => 'Eksempler p&aring; brug',
+
+	// F
+	'fant' => 'erim',
+	'fonctionnement_recherche' => 'Hvordan s&oslash;gefunktionen virker p&aring; denne side',
+	'fulltext_cree' => 'FULLTEXT created', # NEW
+
+	// I
+	'il_faut_myisam' => 'MyISAM kr&aelig;ves',
+	'incoherence_charset' => 'There is an inconsistency between the character set of your site and that of the database. This may lead to bad results being given for searches which contain accented letters:', # NEW
+	'index_regenere' => 'table index regenerated', # NEW
+	'index_reinitialise' => 'The documents showing an error have been reinitialised', # NEW
+	'index_supprime' => 'index deleted', # NEW
+
+	// L
+	'liste_tables_connues' => 'Here is the list of tables taken into account for searches. You can add more FULLTEXT elements -- see the documentation at', # NEW
+
+	// M
+	'mais_pas' => 'men IKKE',
+
+	// O
+	'ou_bien' => 'or else', # NEW
+
+	// P
+	'pas_index' => 'No FULLTEXT index', # NEW
+	'premier_soit' => 'ENTEN',
+
+	// R
+	'regenerer_tous' => 'Regenerate all FULLTEXT indexes', # NEW
+	'reinitialise_index_doc' => 'Reinitialise the indexation of documents showing an error', # NEW
+	'reserve_webmestres' => 'Kun webadministratorer kan bruge denne side',
+	'retourne' => 'Giver tekst som indeholder',
+
+	// S
+	'sequence_exacte' => 'den pr&aelig;cise s&aelig;tning',
+	'soit' => 'ELLER',
+	'supprimer' => 'Slet',
+
+	// T
+	'table_convertie' => 'table converted to MyISAM', # NEW
+	'table_format' => 'This table\'s format is', # NEW
+	'table_non_reconnue' => 'unrecognised table', # NEW
+	'textes_premier' => 'men viser f&oslash;rst tekst, som indeholder',
+
+	// U
+	'utiliser_operateurs_logiques' => 'S&oslash;gningen bruger de standard logiske operatorer.'
+);
+
+?>
