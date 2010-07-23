6d850d22d61531255d28208f4f9e256f7866e13e
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-07-23 18:17:28 +0000

    langues (palette)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@39500 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ palette/lang/palette_ca.php
@@ -0,0 +1,23 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'config_desc' => '<strong>Palette</strong> afegir&agrave; autom&agrave;ticament un selector de colors a un camp d\'entrada, simplement si li donem la classe CSS &laquo;palette&raquo;',
+	'config_exemple' => 'Exemple:',
+
+	// F
+	'fermer' => 'Tancar la palette',
+
+	// P
+	'palette' => 'Palette',
+	'palette_ecrire' => 'Activar Palette a l\'espai privat',
+	'palette_public' => 'Activar Palette al lloc p&uacute;blic'
+);
+
+?>
