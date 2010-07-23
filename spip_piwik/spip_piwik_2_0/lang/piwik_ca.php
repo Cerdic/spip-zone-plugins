cd9bb29d0455af079d616edb82fcc340cfb74fc7
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-07-23 18:17:32 +0000

    langues (piwik)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@39501 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ spip_piwik/spip_piwik_2_0/lang/piwik_ca.php
@@ -0,0 +1,53 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'action_creer_site' => 'Crear el lloc',
+
+	// C
+	'cfg_description_piwik' => 'Podeu introduir aqu&iacute; el vostre nom d\'usuari piwik i l\'adre&ccedil;a del servidor que gestiona les vostres estad&iacute;stiques.',
+	'cfg_erreur_recuperation_data' => 'Hi ha un error de comunicaci&oacute; amb el servidor, verifiqueu l\'adre&ccedil;a i el token',
+	'cfg_erreur_token' => 'El vostre token d\'identificaci&oacute; &eacute;s inv&agrave;lid',
+
+	// E
+	'explication_adresse_serveur' => 'Entreu l\'adre&ccedil;a sense "http://" ni "https://" ni barra final',
+	'explication_creer_site' => 'El seg&uuml;ent enlla&ccedil; us permet crear un lloc al servidor Piwik que estar&agrave; disponible a continuaci&oacute; a la llista. Verifiqueu que heu configurat l\'adre&ccedil;a correctament i el nom del vostre lloc SPIP abans de clicar. S&oacute;n aquestes informacions les que s\'utilitzaran.',
+	'explication_exclure_ips' => 'Per excloure diverses adreces, separeu-les amb punts i comes',
+	'explication_identifiant_site' => 'La llista dels llocs disponibles al servidor Piwik s\'ha recuperat autom&agrave;ticament gr&agrave;cies a les informacions presentades. Seleccioneu de la seg&uuml;ent llista la que m&eacute;s us convingui',
+	'explication_mode_insertion' => 'Hi ha dues maneres d\'inserir a les p&agrave;gines el codi necessari per un bon funcionament del connector. Mitjan&ccedil;ant el pipeline "insert_head" (m&egrave;tode autom&agrave;tic per&ograve; poc configurable), o mitjan&ccedil;ant la inserci&oacute; d\'una etiqueta (m&egrave;tode manual inserint a la part inferior de les vostres p&agrave;gines l\'etiqueta #PIWIK) que, a m&eacute;s a m&eacute;s, &eacute;s totalment configurable.',
+	'explication_restreindre_statut_prive' => 'Escolliu aqu&iacute; els estats d\'usuaris que no es comptabilitzaran a les estad&iacute;stiques en l\'espai privat',
+	'explication_restreindre_statut_public' => 'Escolliu aqu&iacute; els estats d\'usuaris que no es comptabilitzaran a les estad&iacute;stiques a la part p&uacute;blica',
+	'explication_token' => 'El token d\'identificaci&oacute; est&agrave; disponible o b&eacute; a les vostres prefer&egrave;ncies personals o a la part API de vostre servidor Piwik',
+
+	// L
+	'label_adresse_serveur' => 'Adre&ccedil;a URL del servidor (https:// o http://)',
+	'label_comptabiliser_prive' => 'Comptabilitzar les visites de l\'espai privat',
+	'label_creer_site' => 'Crear un lloc al servidor Piwik',
+	'label_exclure_ips' => 'Excloure certes adreces IP',
+	'label_identifiant_site' => 'L\'identificador del vostre lloc al servidor Piwik',
+	'label_mode_insertion' => 'Mode d\'inserci&oacute; a les p&agrave;gines p&uacute;bliques',
+	'label_restreindre_auteurs_prive' => 'Restringir determinats usuaris connectats (privat)',
+	'label_restreindre_auteurs_public' => 'Restringir determinats usuaris connectats (p&uacute;blic)',
+	'label_restreindre_statut_prive' => 'Restringir determinats estats d\'usuaris a l\'espai privat',
+	'label_restreindre_statut_public' => 'Restringir determinats estats d\'usuaris a l\'espai p&uacute;blic',
+	'label_token' => 'Token d\'identificaci&oacute; al vostre servidor',
+
+	// M
+	'mode_insertion_balise' => 'Inserci&oacute; per mitj&agrave; de l\'etiqueta #PIWIK (cal que modifiqueu els vostres esquelets)',
+	'mode_insertion_pipeline' => 'Inserci&oacute; autom&agrave;tica per mitja del pipeline "insert_head"',
+
+	// P
+	'piwik' => 'Piwik',
+
+	// T
+	'texte_votre_identifiant' => 'Nom d\'usuari',
+	'textes_url_piwik' => 'El vostre servidor piwik'
+);
+
+?>
