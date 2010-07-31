47414deca595103f2874f0e3e3528f43545093b6
marcimat@rezo.net <marcimat@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-07-31 09:59:26 +0000

    Des commentaires en plus.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@39611 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- coordonnees/cop_pipelines.php
+++ coordonnees/cop_pipelines.php
@@ -5,6 +5,11 @@
  * Licence GPL (c) 2010 Matthieu Marcillaud 
 **/
 
+
+/**
+ * Ajout des informations de coordonnées (adresses, mails, numéros)
+ * sur la page de visualisation d'un auteur
+**/
 function cop_affiche_milieu($flux) {
 	if ($flux['args']['exec'] == 'auteur_infos') {
 		if ($id_auteur = $flux['args']['id_auteur']) {
@@ -19,6 +24,10 @@ function cop_affiche_milieu($flux) {
 }
 
 
+/**
+ * Ajout de l'objet 'adresse'
+ * à la liste des objets pouvant recevoir des champs extras 
+**/
 function cop_objets_extensibles($objets){
 		return array_merge($objets, array(
 			'adresse' => _T('cop:adresses'),
