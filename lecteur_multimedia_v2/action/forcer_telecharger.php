ccfcfa497559cda5b3b46b0d6bbf7e28efe73ec1
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-08-06 17:02:24 +0000

    Faire fonctionner le wrapper de téléchargement en local et en distant
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@39685 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- lecteur_multimedia_v2/action/forcer_telecharger.php
+++ lecteur_multimedia_v2/action/forcer_telecharger.php
@@ -27,38 +27,36 @@ include_spip('inc/headers');
  * @return
  */
 function action_forcer_telecharger_dist() {
-include_spip('inc/documents');
+	include_spip('inc/documents');
 
 	// $file exige pour eviter le scan id_document par id_document
 	$f = rawurldecode(_request('file'));
 	$f = str_replace('IMG/','',$f);
 	$file = get_spip_doc($f);
 	$arg = rawurldecode(_request('arg'));
-
+	
 	if(strpos($f,_DIR_SITE)!== false){
 		$f_loc = str_replace(_DIR_SITE,'',$f);
 		$file = get_spip_doc($f_loc);
 	}
 
 	$status = $dcc = false;
-	spip_log($file,'test');
 	if (strpos($f,'../') !== false
-	OR preg_match(',^\w+://,', $f)) {
+	OR (preg_match(',^\w+://,', $f) && strpos($f,$GLOBALS['meta']['adresse_site']))) {
 		$status = 403;
 	}
-	else if (!file_exists($file)
-		OR !is_readable($file)) {
-			spip_log('plouf','test');
-		$status = 404;
-	} else {
+	else {
 		$where = "documents.fichier=".sql_quote(set_spip_doc($file))
 		. ($arg ? " AND documents.id_document=".intval($arg): '');
 
-		$doc = sql_fetsel("documents.id_document, types.mime_type, documents.extension", "spip_documents AS documents LEFT JOIN spip_types_documents AS types ON documents.extension=types.extension",$where);
+		$doc = sql_fetsel("documents.id_document,documents.distant, types.mime_type, documents.extension", "spip_documents AS documents LEFT JOIN spip_types_documents AS types ON documents.extension=types.extension",$where);
 
 		if (!$doc){
 			$status = 404;
-		} else {
+		}else if (($doc['distant'] == 'non') && (!file_exists($file)
+			OR !is_readable($file))) {
+				$status = 404;
+		} else if($doc['distant'] == 'non'){
 
 			// ETag pour gerer le status 304
 			$ETag = md5($file . ': '. filemtime($file));
@@ -97,7 +95,7 @@ include_spip('inc/documents');
 		// header("Expires: 0"); // set expiration time
 		// header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
 
-		if ($cl = filesize($file)){
+		if (($doc['distant'] == 'non') && ($cl = filesize($file))){
 			header("Content-Length: $cl;\n");
 		}
 		
