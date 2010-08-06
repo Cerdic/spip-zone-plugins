09eaa8930d44c29fe7c89d3ccfe3f352a30f0e1a
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-08-06 08:16:26 +0000

    update de la lib soundmanager
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@39676 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- lecteur_multimedia_v2/lm2_options.php
+++ lecteur_multimedia_v2/lm2_options.php
@@ -1,7 +1,7 @@
 <?php
 
 if (!defined('_DIR_LIB_SM'))
-	define('_DIR_LIB_SM', _DIR_RACINE . 'lib/soundmanagerv296a-20100520/');
+	define('_DIR_LIB_SM', _DIR_RACINE . 'lib/soundmanagerv296a-20100624/');
 
 
 ?>
