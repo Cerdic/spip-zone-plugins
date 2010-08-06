d6a2797cca583807e47c0e24c96c50ccd1d9fc2a
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-08-06 17:28:44 +0000

    Début de mise à jour du plugin Archive.
    Si je ne me suis pas trompé, ce commit ne modifie absolument rien au fonctionnement. Ce n'est que du rangement de fichiers.
    
    En revanche, on utilise le pipeline "pre_boucle" plutôt que la surcharge de la boucle ARTICLES. Du coup c'est compatible avec Accès Restreint, notamment.
    
    (Oui l'icône était moche.)
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@39686 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- archive/archive_fonctions.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-//charge les gestion du critere archive
-include_spip('public/archive_criteres');
-//appelle la surcharge de la boucle article
-include_spip('public/archive_boucles');
-
-?>
--- archive/archive_options.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Lancement des taches cron pour l'archivage
-function archive_taches_generales_cron($taches_generales){ 
-       $taches_generales['archive_cron']=1*24*3600;
-	    return $taches_generales;
- }
-?>
