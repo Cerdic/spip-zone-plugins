152406c70e53e963f5684741ff7f4b21566989c2
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-08-06 11:51:27 +0000

    Pouvoir choisir la transition pour le lecteur ainsi que la durée de la transition
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@39683 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- lecteur_multimedia_v2/lang/lm2_fr.php
+++ lecteur_multimedia_v2/lang/lm2_fr.php
@@ -16,11 +16,14 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'cfg_lien_doc' => '<a href="http://multimedia.spip-zone.info" class="spip_out">Documentation</a>',
 
 	// L
+	'label_360animtransition' => 'Transition utilis&eacute; pour passer de l\'&eacute;tat "stop" &agrave; la lecture',
+	'label_360animduration' => 'Dur&eacute;e de la transition (en ms)',
 	'label_360loadringcolor' => 'Couleur de la taille du son d&eacute;j&agrave; charg&eacute;',
 	'label_360playringcolor' => 'Couleur correspondant &agrave; la taille du son d&eacute;j&agrave; lu',
 	'label_360backgroundringcolor' => 'Couleur de base du cercle de lecture',
 	'legende_style_360player' => 'Styles du 360 player',
 	'legende_style_players' => 'Styles du ou des lecteurs',
+
 	// T
 	'titre_liste_lecture' => 'Liste de lecture'
 
