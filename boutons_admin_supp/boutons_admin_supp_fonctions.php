3444aaf86d191260b9cb9602dc19ab9eac9d1001
prigent.yohann@gmail.com <prigent.yohann@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-08-08 18:59:52 +0000

    insert_head_css ici
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@39711 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- boutons_admin_supp/boutons_admin_supp_fonctions.php
+++ boutons_admin_supp/boutons_admin_supp_fonctions.php
@@ -1,28 +1,34 @@
 <?php
 require find_in_path("boutons_supp_options.php");
 
-function boutons_admin_supp_insert_head($flux) {
-switch($GLOBALS['style_barre']){
-	case "a_droite":
-		$css = "\n
+function boutons_admin_supp_insert_head_css($flux) {
+	static $done = false;
+	if (!$done) {
+		$done = true;
+		switch($GLOBALS['style_barre']){
+			case "a_droite":
+				$flux .= "\n
 <style type=\"text/css\">
 	.spip-admin-boutons {
 	display:list-item;
 	list-style-type:none;
 	}
 </style>\n";
-		break;
-
-	case "translucide":
-		$css = "\n
+			break;
+			case "translucide":
+				$flux .= "\n
 <style type=\"text/css\">
 	.spip-admin-float, #bouton_montrer {
 	opacity:".$GLOBALS['translucidite']." !important;
 	}
 </style>\n";
-		break;
+			break;
+		}
+	}
+	return $flux;
 }
-	return $css.$flux;
+function boutons_admin_supp_insert_head($flux) {
+	$flux = boutons_admin_supp_insert_head_css($flux); // au cas ou il n'est pas implemente
+	return $flux;
 }
-
 ?>
