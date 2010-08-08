1ca867ea77a3114ccfb7386d59fb79bd92cad964
prigent.yohann@gmail.com <prigent.yohann@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-08-08 19:17:55 +0000

    insert_head_css ici aussi
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@39714 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- porte_plume_extras/enluminures_typographiques_v3/typoenluminee_pipelines.php
+++ porte_plume_extras/enluminures_typographiques_v3/typoenluminee_pipelines.php
@@ -1,16 +1,25 @@
 <?php
 
 // insert le css pour les styles supplementaires de la BTE dans le <head> du document (#INSERT_HEAD)
-function TypoEnluminee_insert_head($flux) {
-	if (isset($GLOBALS['BarreTypoEnrichie_Preserve_Header']) OR !function_exists('lire_config'))
-		global $BarreTypoEnrichie_Preserve_Header;
-	else
-		$BarreTypoEnrichie_Preserve_Header = lire_config('bte/insertcss','Oui');
-	if ($BarreTypoEnrichie_Preserve_Header == 'Oui')
-		return $flux.'<link rel="stylesheet" href="'.find_in_path('css/enluminurestypo.css').'" type="text/css" media="all" />'."\n";
+function TypoEnluminee_insert_head_css($flux) {
+	static $done = false;
+	if (!$done) {
+		$done = true;
+		if (isset($GLOBALS['BarreTypoEnrichie_Preserve_Header']) OR !function_exists('lire_config'))
+			global $BarreTypoEnrichie_Preserve_Header;
+		else
+			$BarreTypoEnrichie_Preserve_Header = lire_config('bte/insertcss','Oui');
+		if ($BarreTypoEnrichie_Preserve_Header == 'Oui')
+			$flux .= '<link rel="stylesheet" href="'.find_in_path('css/enluminurestypo.css').'" type="text/css" media="all" />'."\n";
+	}
 	return $flux;
 }
 
+// insert le css pour les styles supplementaires de la BTE dans le <head> du document (#INSERT_HEAD)
+function TypoEnluminee_insert_head($flux) {
+	$flux = TypoEnluminee_insert_head_css($flux);
+	return $flux;
+}
 function TypoEnluminee_header_prive($texte) {
 	$texte.= '<link rel="stylesheet" type="text/css" href="' . _DIR_PLUGIN_TYPOENLUMINEE . 'css/enluminurestypo.css" />' . "\n";
 	return $texte;
