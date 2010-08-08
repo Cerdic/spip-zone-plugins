f056a18810b65fd7c0ff56f3e7699789502aed19
prigent.yohann@gmail.com <prigent.yohann@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-08-08 19:48:25 +0000

    insert_head_css ici aussi
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@39716 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- thickbox2/head.php
+++ thickbox2/head.php
@@ -1,14 +1,23 @@
 <?php
 
+function ThickBox1_insert_head_css($flux){
+	static $done = false;
+	if (!$done) {
+		$done = true;
+		$flux .= ThickBox_call_css();
+	}
+	return $flux;
+}
 
 function ThickBox1_insert_head($flux){
-
+	$flux = ThickBox1_insert_head_css($flux);
+	
 	// on ajoute la class thickbox aux liens de type="image/xxx"
 
 	// TODO: ne charger thickbox.js et thickbox.css que si 
 	// jQuery("a.thickbox,a[type='image/jpeg'],...").size() > 0)
 	if(!$GLOBALS["spip_pipeline"]["insert_js"])
-		$flux = ThickBox1_header_prive($flux);
+		$flux .= ThickBox_call_js();
 
 	return $flux;
 }
@@ -16,26 +25,8 @@ function ThickBox1_insert_head($flux){
 function ThickBox1_header_prive($flux) {
 include_spip("inc/filtres");
 
-$flux .=
-
-'
-<script src=\''.url_absolue(find_in_path('javascript/thickbox.js')).'\' type=\'text/javascript\'></script>
-<link rel="stylesheet" href="'.url_absolue(find_in_path('thickbox.css')).'" type="text/css" media="all" />
-<script type="text/javascript"><!--
-// Inside the function "this" will be "document" when called by ready() 
-// and "the ajaxed element" when called because of onAjaxLoad 
-var init_f = function() {
-	if (jQuery("a.thickbox,a[type=\'image/jpeg\'],a[type=\'image/png\'],a[type=\'image/gif\']",this).addClass("thickbox").size()) {
-		TB_chemin_animation = "'.url_absolue(find_in_path('circle_animation.gif')).'";
-		TB_chemin_close = "'.url_absolue(find_in_path('close.gif')).'";
-		TB_chemin_css = "'.url_absolue(find_in_path('thickbox.css')).'";
-		TB_init(this);
-	};
-}
-//onAjaxLoad is defined in private area only
-if(typeof onAjaxLoad == "function") onAjaxLoad(init_f);
-if (window.jQuery) jQuery(document).ready(init_f);
-// --></script>';
+$flux .= ThickBox_call_css();
+$flux .= ThickBox_call_js();
 
 return $flux;
 
@@ -100,4 +91,28 @@ return $flux;
   
 }
 
+function ThickBox_call_js() {
+	$flux = '<script src=\''.url_absolue(find_in_path('javascript/thickbox.js')).'\' type=\'text/javascript\'></script>';
+	$flux .= '<script type="text/javascript"><!--
+// Inside the function "this" will be "document" when called by ready() 
+// and "the ajaxed element" when called because of onAjaxLoad 
+var init_f = function() {
+	if (jQuery("a.thickbox,a[type=\'image/jpeg\'],a[type=\'image/png\'],a[type=\'image/gif\']",this).addClass("thickbox").size()) {
+		TB_chemin_animation = "'.url_absolue(find_in_path('circle_animation.gif')).'";
+		TB_chemin_close = "'.url_absolue(find_in_path('close.gif')).'";
+		TB_chemin_css = "'.url_absolue(find_in_path('thickbox.css')).'";
+		TB_init(this);
+	};
+}
+//onAjaxLoad is defined in private area only
+if(typeof onAjaxLoad == "function") onAjaxLoad(init_f);
+if (window.jQuery) jQuery(document).ready(init_f);
+// --></script>';
+	return $flux;
+}
+
+function ThickBox_call_css() {
+	$flux = '<link rel="stylesheet" href="'.url_absolue(find_in_path('thickbox.css')).'" type="text/css" media="all" />';
+	return $flux;
+}
 ?>
