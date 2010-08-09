1c82939062ba86524a6ad1a473945642e9fe8418
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-08-09 09:22:00 +0000

    langues (listes)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@39749 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ mailman_popup/lang/listes_sk.php
@@ -0,0 +1,56 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'ajoutee' => 'The address &lt;@var_email@&gt; will be added to the list &lt;@liste@&gt; once it has been checked. Please reply to the message which has just been sent to you.', # MODIF
+
+	// C
+	'confirm' => '<NEW>&#381;iados&#357; o potvrdenie bola odoslan&aacute; na &lt;@var_email@&gt;.',
+
+	// D
+	'deja' => '<NEW>&lt;@var_email@&gt;: U&#382; ste prihl&aacute;sen&yacute; do zoznamu &lt;@liste@&gt;.',
+	'desabo' => '<NEW>Registr&aacute;cia bola zru&#353;en&aacute;.',
+
+	// F
+	'fermer' => '<NEW>Zatvori&#357;',
+
+	// I
+	'inscription' => '<NEW>Registr&aacute;cia do &lt;@liste@&gt;',
+
+	// M
+	'mail_removed' => '<NEW>
+
+Adresa &lt;@var_email@&gt; bola odstr&aacute;nen&aacute; zo zoznamu @liste@.
+
+Ak sa vyskytol probl&eacute;m alebo ak ste
+ne&#382;iadali o zru&#353;enie, pros&iacute;m, nap&iacute;&#353;te na &lt;@responsable@&gt;.
+
+Dovidenia a &#271;akujeme.
+',
+	'merci' => '<NEW>&#270;akujeme.',
+
+	// P
+	'pasabo' => '<NEW>Adresa &lt;@var_email@&gt; nie je prihl&aacute;sen&aacute; do zoznamu &lt;@liste@&gt;.',
+	'patientez' => '<NEW>Pros&iacute;m, po&#269;kajte...',
+
+	// Q
+	'quitter' => '<NEW>Zru&#353;enie',
+
+	// S
+	'subject_removed' => '<NEW>Va&#353;a adresa bola odstr&aacute;nen&aacute; zo zoznamu @liste@.',
+
+	// T
+	'titrefenetre' => '<NEW>Prihl&aacute;si&#357; sa',
+
+	// V
+	'veuillez' => '<NEW>Pros&iacute;m, dajte n&aacute;m svoju adresu.',
+	'votreemail' => '<NEW>V&aacute;&#353; e-mail:'
+);
+
+?>
