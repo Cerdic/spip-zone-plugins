7fa5b58eb8ebfbdcff087f073d8d216363574c97
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-08-09 09:39:14 +0000

    Gestion des dépendances inter plugins de jquery ui pour le formulaire de configuration
    Il faut faire la même chose pour le pipeline pour sécurité
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@39751 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- jquery_ui/formulaires/config_jqueryui_fonctions.php
+++ jquery_ui/formulaires/config_jqueryui_fonctions.php
@@ -24,7 +24,101 @@ function cfg_config_jqueryui_pre_traiter(&$cfg){
 	$plugins_pipeline = pipeline('jqueryui_forcer');
 	if(!is_array($plugins_pipeline))
 		$plugins_pipeline = array();
+	
+	$dependance_core = array(
+							'jquery.ui.mouse',
+							'jquery.ui.widget',
+							'jquery.ui.datepicker'
+	);
+
+	/**
+	 * Dépendances à widget
+	 * Si un autre plugin est dépendant d'un de ceux là, on ne les ajoute pas
+	 */
+	$dependance_widget = array(
+							'jquery.ui.mouse',
+							'jquery.ui.accordion',
+							'jquery.ui.autocomplete',
+							'jquery.ui.button',
+							'jquery.ui.dialog',
+							'jquery.ui.tabs',
+							'jquery.ui.progressbar'						
+							);
+	
+	$dependance_mouse = array(
+							'jquery.ui.draggable',
+							'jquery.ui.droppable',
+							'jquery.ui.resizable',
+							'jquery.ui.selectable',
+							'jquery.ui.sortable',
+							'jquery.ui.slider'
+						);
+	
+	$dependance_position = array(
+							'jquery.ui.autocomplete',
+							'jquery.ui.dialog',
+							);
+	
+	$dependance_draggable = array(
+							'jquery.ui.droppable'
+							);
+	
+	$dependance_effects = array(
+							'jquery.effects.blind',
+							'jquery.effects.bounce',
+							'jquery.effects.clip',
+							'jquery.effects.drop',
+							'jquery.effects.explode',
+							'jquery.effects.fold',
+							'jquery.effects.highlight',
+							'jquery.effects.pulsate',
+							'jquery.effects.scale',
+							'jquery.effects.shake',
+							'jquery.effects.slide',
+							'jquery.effects.transfer'
+						);
+	
 	$valeurs = array_unique(array_merge($plugins_pipeline,$valeurs));
+	
+	/**
+	 * Vérification des dépendances :
+	 * On commence par le bas de l'échelle :
+	 * - draggable
+	 * - position
+	 * - mouse
+	 * - widget
+	 * - core
+	 * - effects
+	 */
+	if((count($intersect = array_intersect($valeurs,$dependance_draggable)) > 0) && !in_array('jquery.ui.draggable',$valeurs)){
+		$keys = array_keys($intersect);
+		array_splice($valeurs,$keys[0], 0, "jquery.ui.draggable");
+	}
+	if((count($intersect = array_intersect($valeurs,$dependance_position)) > 0) && !in_array('jquery.ui.position',$valeurs)){
+		$keys = array_keys($intersect);
+		array_splice($valeurs,$keys[0], 0, "jquery.ui.position");
+	}
+	if((count($intersect = array_intersect($valeurs,$dependance_mouse)) > 0) && !in_array('jquery.ui.mouse',$valeurs)){
+		$keys = array_keys($intersect);
+		array_splice($valeurs,$keys[0], 0, "jquery.ui.mouse");
+	}
+	if((count($intersect = array_intersect($valeurs,$dependance_widget)) > 0) && !in_array('jquery.ui.widget',$valeurs)){
+		$keys = array_keys($intersect);
+		array_splice($valeurs,$keys[0], 0, "jquery.ui.widget");
+	}
+	if((count($intersect = array_intersect($valeurs,$dependance_core)) > 0) && !in_array('jquery.ui.core',$valeurs)){
+		$keys = array_keys($intersect);
+		array_splice($valeurs,$keys[0], 0, "jquery.ui.core");
+	}
+	if((count($intersect = array_intersect($valeurs,$dependance_effects)) > 0) && !in_array('jquery.effects.core',$valeurs)){
+		$keys = array_keys($intersect);
+		array_splice($valeurs,$keys[0], 0, "jquery.effects.core");
+	}
+	if((count($intersect = array_intersect($valeurs,$dependance_effects)) > 0) && !in_array('jquery.effects.core',$valeurs)){
+		$keys = array_keys($intersect);
+		array_splice($valeurs,$keys[0], 0, "jquery.effects.core");
+	}
+	
 	set_request('plugins',$valeurs);
 	$cfg->val['plugins'] = $valeurs;
 }
