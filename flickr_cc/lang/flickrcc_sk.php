4406f05ff7905f25ebac1cf244f4822aa520dabe
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-08-09 09:20:32 +0000

    langues (flickrcc)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@39731 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ flickr_cc/lang/flickrcc_sk.php
@@ -0,0 +1,37 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'ajouter_image_flickr' => '<NEW>Prida&#357; obr&aacute;zok z Flickra',
+
+	// C
+	'cc_commerciale' => '<NEW>CC - Povolen&eacute; pou&#382;itie na komer&#269;n&eacute; &uacute;&#269;ely',
+	'cc_modification' => '<NEW>CC - Povolen&aacute; modifik&aacute;cia',
+	'cc_pas_de_restriction' => '<NEW>&#381;iadne zn&aacute;me obmedzenia autorsk&yacute;mi pr&aacute;vami',
+	'cc_paternite' => 'Creative Commons - Attribution License', # MODIF
+
+	// F
+	'fermer' => '<NEW>Zatvori&#357;',
+	'flickr_cc_explication' => '<NEW><p>Vyh&#318;ad&aacute;vajte na Flickri obr&aacute;zky publikovan&eacute; na z&aacute;klade licenci&iacute; Creative Commons. Zadajte svoje vlastn&eacute; k&#318;&uacute;&#269;ov&eacute; slovo (-&aacute;) a uve&#271;te typ licenci&iacute;, ktor&yacute; chcete pou&#382;i&#357;.</p>
+<p>Kliknite na obr&aacute;zok, ktor&yacute; chcete vlo&#382;i&#357; do svojho &#269;l&aacute;nku: bude automaticky stiahnut&yacute; a pripojen&yacute; k v&aacute;&#353;mu dokumentu. Ak sa chcete dosta5 na p&ocirc;vodn&uacute; str&aacute;nku na Flickri, kliknite na meno autora.</p>
+<p>Tieto obr&aacute;zky na Flicri publikovali ich autori: pros&iacute;m, re&#353;pektujte ich pr&aacute;cu a autorsk&eacute; pr&aacute;va:</p>
+<ul><li>neodstra&#328;ujte meno autora a odkaz na jeho str&aacute;nku na Flickri;</li>
+<li>re&#353;pektujte podmienky licencie Creative Commons pri ka&#382;dom obr&aacute;zku (napr. nevyu&#382;&iacute;vajte obr&aacute;zok na komer&#269;n&eacute; &uacute;&#269;ely, ak to licencia nepovo&#318;uje);</li>
+<li>ak autor po&#382;iada o odstr&aacute;nenie svojho obr&aacute;zka z va&#353;ej str&aacute;nky, mus&iacute;te mu vyhovie&#357; do 24 hod&iacute;n.</li>
+</ul>
+<p>Tento n&aacute;stroj vyu&#382;&iacute;va Flickr API, ale nie je schv&aacute;len&yacute; alebo autorizovan&yacute; Flickrom.</p>',
+
+	// R
+	'rechercher_flickr' => '<NEW>Vyh&#318;ada&#357; na Flickri',
+	'resultat_par_date' => '<NEW>pod&#318;a d&aacute;tumu',
+	'resultat_par_interet' => '<NEW>pod&#318;a zauj&iacute;mavosti',
+	'resultat_par_pertinence' => '<NEW>pod&#318;a d&ocirc;le&#382;itosti'
+);
+
+?>
