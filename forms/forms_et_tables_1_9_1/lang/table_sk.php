4a3c6534924928d8212a22168798cd084bb3d62e
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-08-09 09:20:42 +0000

    langues (table)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@39734 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ forms/forms_et_tables_1_9_1/lang/table_sk.php
@@ -0,0 +1,55 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'articles_utilisant' => 'Articles using this table', # MODIF
+	'aucune_reponse' => 'No data', # MODIF
+
+	// C
+	'champs_formulaire' => 'Fields of the table', # MODIF
+
+	// F
+	'formulaire' => 'Table', # MODIF
+
+	// I
+	'icone_ajouter_donnees' => 'Add data', # MODIF
+	'icone_creer_table' => '', # MODIF
+	'importer_donnees_csv' => 'Imprt data', # MODIF
+	'info_supprimer_formulaire' => 'Do you really want to delete this table?', # MODIF
+	'info_supprimer_formulaire_reponses' => 'Existing data is associated with this table. Do you really want to delete it?', # MODIF
+
+	// L
+	'lien_retirer_donnee_liante' => 'Delete the link from this data item', # MODIF
+	'lien_retirer_donnee_liee' => 'Delete this data item', # MODIF
+
+	// N
+	'nombre_reponses' => '@nombre@ data items', # MODIF
+	'nouveau_formulaire' => 'New table', # MODIF
+
+	// S
+	'suivi_reponses' => 'See data', # MODIF
+	'supprimer_formulaire' => 'Delete this table', # MODIF
+
+	// T
+	'telecharger_reponses' => 'Download data', # MODIF
+	'texte_donnee_statut' => 'Status of this data item', # MODIF
+	'texte_statut_poubelle' => 'Data item deleted', # MODIF
+	'texte_statut_prepa' => 'Data item being edited', # MODIF
+	'texte_statut_prop' => 'Proposed data item', # MODIF
+	'texte_statut_publie' => 'Published data item', # MODIF
+	'texte_statut_refuse' => 'Refused data item', # MODIF
+	'titre_formulaire' => 'Table title', # MODIF
+	'toutes_tables' => 'All the tables', # MODIF
+	'type_des_tables' => 'Tables', # MODIF
+
+	// U
+	'une_reponse' => 'One data item', # MODI
+);
+
+?>
