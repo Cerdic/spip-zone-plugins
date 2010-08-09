46a3782c3ab593e63ca98fd7b508de223f76eb4d
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-08-09 09:21:21 +0000

    langues (rainette)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@39742 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ rainette/lang/rainette_sk.php
@@ -0,0 +1,148 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'coucher_soleil' => '<NEW>z&aacute;pad slnka',
+
+	// D
+	'demain' => '<NEW>zajtra',
+	'derniere_maj' => '<NEW>aktualizovan&eacute;',
+	'direction_E' => '<NEW>v&yacute;chod',
+	'direction_ENE' => '<NEW>v&yacute;chod severov&yacute;chod',
+	'direction_ESE' => '<NEW>v&yacute;chod juhov&yacute;chod',
+	'direction_N' => '<NEW>sever',
+	'direction_NE' => '<NEW>severov&yacute;chod',
+	'direction_NNE' => '<NEW>sever severov&yacute;chod',
+	'direction_NNW' => '<NEW>sever severoz&aacute;pad',
+	'direction_NW' => '<NEW>severoz&aacute;pad',
+	'direction_S' => '<NEW>juh',
+	'direction_SE' => '<NEW>juhov&yacute;chod',
+	'direction_SSE' => '<NEW>juh juhov&yacute;chod',
+	'direction_SSW' => '<NEW>juh juhoz&aacute;pad',
+	'direction_SW' => '<NEW>juhoz&aacute;pad',
+	'direction_W' => '<NEW>z&aacute;pad',
+	'direction_WNW' => '<NEW>z&aacute;pad severoz&aacute;pad',
+	'direction_WSW' => '<NEW>z&aacute;pad juhoz&aacute;pad',
+
+	// H
+	'humidite' => '<NEW>vlhkos&#357; vzduchu',
+
+	// J
+	'jour' => '<NEW>de&#328;',
+
+	// L
+	'latitude' => '<NEW>zemepisn&aacute; &#353;&iacute;rka',
+	'lever_soleil' => '<NEW>v&yacute;chod slnka',
+	'longitude' => '<NEW>zemepisn&aacute; d&#314;&#382;ka',
+
+	// M
+	'meteo' => '<NEW>po&#269;asie',
+	'meteo_0' => '<NEW>torn&aacute;do',
+	'meteo_1' => '<NEW>tropick&aacute; b&uacute;rka',
+	'meteo_10' => '<NEW>mrzn&uacute;ci d&aacute;&#382;&#271;',
+	'meteo_11' => '<NEW>preh&aacute;nky',
+	'meteo_12' => '<NEW>preh&aacute;nky',
+	'meteo_13' => 'snow flurries', # MODIF
+	'meteo_14' => 'light snow showers', # MODIF
+	'meteo_15' => '<NEW>snehov&aacute; metelica',
+	'meteo_16' => '<NEW>sneh',
+	'meteo_17' => '<NEW>&#318;adovec',
+	'meteo_18' => '<NEW>p&#318;u&#353;&#357;',
+	'meteo_19' => '<NEW>prach',
+	'meteo_2' => '<NEW>hurik&aacute;n',
+	'meteo_20' => '<NEW>hmla',
+	'meteo_21' => 'haze', # MODIF
+	'meteo_22' => 'smoky', # MODIF
+	'meteo_23' => '<NEW>b&uacute;rliv&eacute;',
+	'meteo_24' => '<NEW>veterno',
+	'meteo_25' => '<NEW>chladno',
+	'meteo_26' => '<NEW>zamra&#269;en&eacute;',
+	'meteo_27' => '<NEW>zv&auml;&#269;&#353;a zamra&#269;en&eacute; (noc)',
+	'meteo_28' => '<NEW>zv&auml;&#269;&#353;a zamra&#269;en&eacute; (de&#328;)',
+	'meteo_29' => '<NEW>s&#269;asti zamra&#269;en&eacute; (noc)',
+	'meteo_3' => '<NEW>prudk&eacute; b&uacute;rky',
+	'meteo_30' => '<NEW>s&#269;asti zamra&#269;en&eacute; (de&#328;)',
+	'meteo_31' => '<NEW>jasno (noc)',
+	'meteo_32' => '<NEW>slne&#269;no',
+	'meteo_33' => 'fair (night)', # MODIF
+	'meteo_34' => 'fair (day)', # MODIF
+	'meteo_35' => '<NEW>d&aacute;&#382;&#271; zmie&#353;an&yacute; s &#318;adovcom',
+	'meteo_36' => '<NEW>hor&uacute;co',
+	'meteo_37' => '<NEW>lok&aacute;lne b&uacute;rky',
+	'meteo_38' => '<NEW>miestami b&uacute;rky',
+	'meteo_39' => '<NEW>miestami b&uacute;rky',
+	'meteo_4' => '<NEW>b&uacute;rky',
+	'meteo_40' => '<NEW>miestami preh&aacute;nky',
+	'meteo_41' => '<NEW>intenz&iacute;vne sne&#382;enie',
+	'meteo_42' => '<NEW>miestami snehov&eacute; preh&aacute;nky',
+	'meteo_43' => '<NEW>intenz&iacute;vne sne&#382;enie',
+	'meteo_44' => '<NEW>s&#269;asti zamra&#269;en&eacute;',
+	'meteo_45' => '<NEW>b&uacute;rky',
+	'meteo_46' => '<NEW>snehov&eacute; preh&aacute;nky',
+	'meteo_47' => '<NEW>lok&aacute;lne b&uacute;rky',
+	'meteo_5' => '<NEW>d&aacute;&#382;&#271; so snehom',
+	'meteo_6' => '',
+	'meteo_7' => 'mixed snow and sleet', # MODIF
+	'meteo_8' => '<NEW>mrzn&uacute;ce mrholenie',
+	'meteo_9' => '<NEW>mrholenie',
+	'meteo_conditions' => '<NEW>aktu&aacute;lne podmienky',
+	'meteo_consultation' => '<NEW>Po&#269;asie v meste @ville@ ',
+	'meteo_de' => '<NEW>po&#269;asie v meste @ville@',
+	'meteo_na' => '<NEW>nezn&aacute;me',
+	'meteo_previsions' => '<NEW>predpove&#271;',
+	'meteo_previsions_aujourdhui' => '<NEW>predpove&#271; na dnes',
+	'meteo_previsions_n_jours' => '<NEW>Predpove&#271; na @nbj@ dn&iacute;',
+
+	// N
+	'nuit' => '<NEW>noc',
+
+	// P
+	'point_rosee' => 'dew point', # MODIF
+	'pression' => '<NEW>tlak',
+
+	// R
+	'risque_precipitation' => 'precipitation', # MODIF
+
+	// S
+	'station_observation' => '<NEW>stanica',
+
+	// T
+	'temperature_max' => '<NEW>maxim&aacute;lna',
+	'temperature_min' => '<NEW>minim&aacute;lna',
+	'temperature_ressentie' => '<NEW>na pocit je',
+	'tendance_symbole_falling' => '<NEW>&#8595;',
+	'tendance_symbole_rising' => '<NEW>&#8593;',
+	'tendance_symbole_steady' => '<NEW>&#8594;',
+	'tendance_texte_falling' => '<NEW>kles&aacute;',
+	'tendance_texte_rising' => '<NEW>st&uacute;pa',
+	'tendance_texte_steady' => '<NEW>ust&aacute;len&aacute;',
+
+	// U
+	'unite_angle_metrique' => '&deg;', # MODIF
+	'unite_angle_standard' => '&deg;', # MODIF
+	'unite_distance_metrique' => 'km', # MODIF
+	'unite_distance_standard' => 'm&iacute;&#318;', # MODIF
+	'unite_pourcentage_metrique' => '%', # MODIF
+	'unite_pourcentage_standard' => '%', # MODIF
+	'unite_precipitation_metrique' => 'mm', # MODIF
+	'unite_precipitation_standard' => 'palcov', # MODIF
+	'unite_pression_metrique' => 'mbar', # MODIF
+	'unite_pression_standard' => 'palcov', # MODIF
+	'unite_temperature_metrique' => '&deg;C', # MODIF
+	'unite_temperature_standard' => '&deg;F', # MODIF
+	'unite_vitesse_metrique' => 'km/h', # MODIF
+	'unite_vitesse_standard' => 'm/h', # MODIF
+
+	// V
+	'valeur_indeterminee' => 'N/A', # MODIF
+	'vent' => '<NEW>vietor',
+	'visibilite' => '<NEW>vidite&#318;nos&#357;'
+);
+
+?>
