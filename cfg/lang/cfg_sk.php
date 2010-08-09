5b07f1aeb65d21ede9eb7e36b3b638339e7c2436
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-08-09 09:20:16 +0000

    langues (cfg)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@39726 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ cfg/lang/cfg_sk.php
@@ -0,0 +1,64 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'bouton_effacer' => 'Effacer', # NEW
+	'bouton_mettre_a_jour' => 'Update', # MODIF
+	'bouton_reset' => 'Reset', # NEW
+
+	// C
+	'cfg' => 'CFG', # MODIF
+	'choisir_module_a_configurer' => 'Choose the module to configure.', # MODIF
+	'config_enregistree' => '<b>@nom@</b> has been saved', # MODIF
+	'config_supprimee' => '<b>@nom@</b> has been deleted', # MODIF
+	'configuration_modules' => 'Module configuration', # MODIF
+
+	// E
+	'erreur_copie_fichier' => 'It is impossible to copythe file @fichier@ to its final destination', # MODIF
+	'erreur_enregistrement' => 'An error occurred while saving <b>@nom@</b>', # MODIF
+	'erreur_lecture' => 'Impossible to read @nom@', # MODIF
+	'erreur_open_w_fichier' => 'Impossible to open @fichier@ for modification', # MODIF
+	'erreur_suppression' => 'An error occurred while deleting <b>@nom@</b>', # MODIF
+	'erreur_suppression_fichier' => 'The file @fichier@ cannot be deleted.', # MODIF
+	'erreur_type_id' => 'The field @champ@ must begin with a letter or an underscore', # MODIF
+	'erreur_type_idnum' => 'The field @champ@ must be numeric', # MODIF
+	'erreur_type_pwd' => 'The field @champ@ must comprise more than 5 characters', # MODIF
+
+	// I
+	'id_manquant' => 'Missing identifier', # MODIF
+	'installation_librairies' => 'Download libraries', # MODIF
+	'installation_liste_libs' => 'List of libraries', # MODIF
+	'installer_dossier_lib' => 'You must create a directory named @dir@ with write permissions at the root of your SPIP site.', # MODIF
+	'installer_lib_192' => 'To install a library, unzip the zip file manually and copy its contents to the @dir@ directory.', # MODIF
+
+	// L
+	'label_activer' => 'Activate', # MODIF
+	'label_obligatoire' => 'Required entry', # MODIF
+
+	// N
+	'nom_table_manquant' => 'Missing SQL table name', # MODIF
+	'nouveau' => 'New', # MODIF
+
+	// O
+	'ok' => 'OK', # MODIF
+
+	// P
+	'pas_de_champs_dans' => '<MODIF>No field found in @nom@', # MODIF
+	'pas_de_changement' => 'No change found in <b>@nom@</b>', # MODIF
+
+	// R
+	'refus_configuration_administrateur' => 'Only the site administrators are authorised to change these parameters.', # MODIF
+	'refus_configuration_webmestre' => 'Only the site webmasters are authorised to modify these parameters.', # MODIF
+	'reset' => 'Delete', # MODIF
+
+	// S
+	'supprimer' => '<MODIF>Odstr&aacute;ni&#357;'
+);
+
+?>
