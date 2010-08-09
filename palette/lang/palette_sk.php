808a80041ba429f0e649ac9b09dc768cfccb37e9
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-08-09 09:21:19 +0000

    langues (palette)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@39741 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ palette/lang/palette_sk.php
@@ -0,0 +1,24 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'config_desc' => '<NEW><strong>Paleta</strong> automaticky prid&aacute; koliesko s v&yacute;berom farieb k vstupn&eacute;mu po&#318;u,
+  ak ho ozna&#269;&iacute;te CSS triedou &#132;palette&#147;',
+	'config_exemple' => '<NEW>Vzorka :',
+
+	// F
+	'fermer' => '<NEW>Zatvori&#357; paletu',
+
+	// P
+	'palette' => '<NEW>Paleta',
+	'palette_ecrire' => '<NEW>Aktivova&#357; paletu pre s&uacute;kromn&uacute; z&oacute;nu',
+	'palette_public' => '<NEW>Aktivova&#357; paletu pre verejn&uacute; z&oacute;nu'
+);
+
+?>
