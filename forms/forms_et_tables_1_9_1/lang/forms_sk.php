b1b8195b0d1dab9baa4e5038cb6e516477e09685
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-08-09 09:20:40 +0000

    langues (forms)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@39733 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ forms/forms_et_tables_1_9_1/lang/forms_sk.php
@@ -0,0 +1,222 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'activer_barre_typo' => 'Activate the typographical bar', # MODIF
+	'adresse_invalide' => '<NEW>T&aacute;to adresa nie je platn&aacute;',
+	'afficher' => '<NEW>Zobrazi&#357;',
+	'aide_contextuelle' => '<NEW>Kontextov&aacute; n&aacute;poveda',
+	'ajouter_champ' => '<NEW>Prida&#357; pole',
+	'ajouter_champ_type' => '<NEW>Vytvori&#357; pole typu:',
+	'ajouter_choix' => '<NEW>Prida&#357; v&yacute;ber',
+	'apparence_formulaire' => '<NEW>Vzh&#318;ad formul&aacute;ra',
+	'article_inserer_un_formulaire' => '<NEW>Vlo&#382;i&#357; formul&aacute;r',
+	'article_inserer_un_formulaire_detail' => '<NEW>Do &#269;l&aacute;nku m&ocirc;&#382;ete vklada&#357; formul&aacute;re, aby ste umo&#382;nili n&aacute;v&#353;tevn&iacute;kom poskytn&uacute;&#357; inform&aacute;cie. Vyberte si, pros&iacute;m, formul&aacute;r zo zoznamu a do textu &#269;l&aacute;nku si skop&iacute;rujte  vkladac&iacute; k&oacute;d. ',
+	'article_recopier_raccourci' => '<NEW>Na vlo&#382;enie tohto formul&aacute;ra skop&iacute;rujte tento vkladac&iacute; k&oacute;d to textu &#269;l&aacute;nku',
+	'articles_utilisant' => '<NEW>&#268;l&aacute;nky vyu&#382;&iacute;vaj&uacute;ce tento formul&aacute;r',
+	'attention' => '<NEW>Upozornenie:',
+	'aucune_reponse' => '<NEW>&#382;iadne &uacute;daje',
+	'avis_message_confirmation' => '<NEW>Na adresu @mail@ bola odoslan&aacute; potvrdzovacia spr&aacute;va',
+
+	// B
+	'boite_info' => '<NEW>Kliknite na tieto ikony, aby ste mohli zobrazi&#357; &uacute;daje z formul&aacute;ra, upravi&#357; formul&aacute;r, kop&iacute;rova&#357; ho, exportova&#357; ho, vypr&aacute;zdni&#357; ho alebo ho odstr&aacute;ni&#357;.',
+
+	// C
+	'cfg_activer' => '<NEW>&Aacute;no',
+	'cfg_associer_donnees' => '<NEW>Priradi&#357; &uacute;daje z tabuliek',
+	'cfg_associer_donnees_articles' => '<NEW>Umo&#382;ni&#357; prira&#271;ova&#357; d&aacute;ta u &#269;l&aacute;nkov:',
+	'cfg_associer_donnees_auteurs' => '<NEW>Umo&#382;ni&#357; prira&#271;ova&#357; d&aacute;ta autorov:',
+	'cfg_associer_donnees_rubriques' => '<NEW>Umo&#382;ni&#357; prira&#271;ova&#357; d&aacute;ta u rubr&iacute;k:',
+	'cfg_bouton_type_image' => '<NEW>tla&#269;idlo s obr&aacute;zkom',
+	'cfg_bouton_type_texte' => '<NEW>textov&eacute; tla&#269;idlo (posla&#357;)',
+	'cfg_bouton_valider' => '<NEW>tla&#269;idlo potvrdi&#357;',
+	'cfg_bouton_valider_texte' => '<NEW>M&ocirc;&#382;ete ur&#269;i&#357;, ak&yacute; druh tla&#269;idiel sa pou&#382;ije vo formul&aacute;roch:',
+	'cfg_desactiver' => '<NEW>Nie',
+	'cfg_inserer_head' => '<NEW>Vlo&#382;i&#357; do &lt;hlavi&#269;ky&gt;',
+	'cfg_inserer_head_texte' => '<NEW>Do &lt;hlavi&#269;ky&gt; nasledovn&yacute;ch s&uacute;borov str&aacute;nky automaticky vklada&#357; odkazy (odpor&uacute;&#269;a sa):',
+	'champ_descendre' => '<NEW>dole',
+	'champ_email_details' => '<NEW>Pros&iacute;m, zadajte platn&uacute; e-mailov&uacute; adresu (napr. vy@poskytovatel.sk).',
+	'champ_listable_admin' => '<NEW>Toto pole zobrazi&#357; v zoznamoch s&uacute;kromnej z&oacute;ny',
+	'champ_listable_publique' => '<NEW>Toto pole zobrazi&#357; v zoznamoch verejnej str&aacute;nky',
+	'champ_monter' => '<NEW>hore',
+	'champ_necessaire' => '<NEW>Toto pole sa mus&iacute; vyplni&#357;.',
+	'champ_nom' => '<NEW>N&aacute;zov po&#318;a',
+	'champ_nom_bloc' => '<NEW>N&aacute;zov bloku',
+	'champ_nom_groupe' => '<NEW>Skupina',
+	'champ_nom_texte' => 'Text', # MODIF
+	'champ_public' => '<NEW>Toto pole sa zobraz&iacute; na verejnej str&aacute;nke',
+	'champ_saisie_desactivee' => '<NEW>Znemo&#382;ni&#357; &uacute;pravu tohto po&#318;a',
+	'champ_specifiant' => '<NEW>Toto pole vymedzuje d&aacute;ta (triedenie, filter, popis)',
+	'champ_table_jointure_type' => '<NEW>Typ tabu&#318;ky na pripojenie',
+	'champ_type_date' => '<NEW>D&aacute;tum',
+	'champ_type_email' => '<NEW>e-mailov&aacute; adresa',
+	'champ_type_fichier' => '<NEW>s&uacute;bory na stiahnutie',
+	'champ_type_joint' => '<NEW>Spoji&#357; s inou tabu&#318;kou',
+	'champ_type_ligne' => '<NEW>riadok textu',
+	'champ_type_monnaie' => '<NEW>Mena',
+	'champ_type_mot' => '<NEW>k&#318;&uacute;&#269;ov&eacute; slov&aacute;',
+	'champ_type_multiple' => '<NEW>v&yacute;ber z viacer&yacute;ch mo&#382;nost&iacute;',
+	'champ_type_numerique' => '<NEW>&#268;&iacute;seln&eacute;',
+	'champ_type_password' => '<NEW>Heslo',
+	'champ_type_select' => 'unique choice', # MODIF
+	'champ_type_separateur' => '<NEW>Nov&yacute; blok ot&aacute;zok',
+	'champ_type_texte' => 'text', # MODIF
+	'champ_type_textestatique' => '<NEW>Vysvetl&iacute;vka',
+	'champ_type_url' => '<NEW>adresa webstr&aacute;nky',
+	'champ_url_details' => '<NEW>Pros&iacute;m, zadajte platn&uacute; www adresu (napr. http://www.mojastranka.sk/...).',
+	'champs_formulaire' => '<NEW>Polia formul&aacute;ra',
+	'changer_choix_multiple' => '<NEW>Zmeni&#357; na v&yacute;ber z viac mo&#382;nost&iacute;',
+	'changer_choix_unique' => 'Change to unique choice', # MODIF
+	'choisir_email' => '<NEW>Vyberte si e-mail pod&#318;a',
+	'confirm_supprimer_champ' => '<NEW>Ur&#269;ite chcete odstr&aacute;ni&#357; pole \'@champ@\'?',
+	'confirm_supprimer_donnee' => '<NEW>Ur&#269;ite chcete odstr&aacute;ni&#357; d&aacute;ta \'@donnee@\'?',
+	'confirm_vider_table' => '<NEW>Ur&#269;ite chcete odstr&aacute;ni&#357; v&#353;etky d&aacute;ta z tabu&#318;ky \'@table@\'?',
+	'confirmer_champ_password' => 'Label for double (confirmation) field', # MODIF
+	'confirmer_password' => '<NEW>Potvrdenie',
+	'confirmer_reponse' => 'Confirm reply by mail with:', # MODIF
+	'csv_classique' => 'Classic CSV (,)', # MODIF
+	'csv_excel' => '<NEW>CSV pre Excel (;)',
+	'csv_tab' => 'CSV with tabs', # MODIF
+
+	// D
+	'date' => '<NEW>D&aacute;tum',
+	'date_invalide' => '<NEW>Neplatn&yacute; form&aacute;t d&aacute;tumu',
+	'donnees_modifiable' => '<NEW>D&aacute;ta, ktor&eacute; m&ocirc;&#382;e pou&#382;&iacute;vate&#318; upravi&#357;.',
+	'donnees_multiple' => '<NEW>V&yacute;ber z viacer&yacute;ch mo&#382;nost&iacute;.',
+	'donnees_nonmodifiable' => '<NEW>D&aacute;ta, ktor&eacute; pou&#382;&iacute;vate&#318; nem&ocirc;&#382;e upravi&#357;.',
+	'donnees_nonmultiple' => 'Single choice.', # MODIF
+	'donnees_prot' => 'Protected form. The recorded data will be available from the private area only.', # MODIF
+	'donnees_pub' => 'Public form. The recorded data will be available to the site visitors.', # MODIF
+	'dupliquer' => 'Duplicate', # MODIF
+
+	// E
+	'echec_upload' => '<NEW>Presun s&uacute;boru sa nepodaril.',
+	'edit_champ_obligatoire' => '<NEW>Toto pole je povinn&eacute;',
+	'editer' => '<NEW>Upravi&#357;',
+	'email_independant' => 'Email independent from reply', # MODIF
+	'exporter' => 'Export', # MODIF
+	'exporter_article' => 'Export in an article', # MODIF
+
+	// F
+	'fichier_trop_gros' => '<NEW>Tento s&uacute;bor je pr&iacute;li&#353; ve&#318;k&yacute;.',
+	'fichier_type_interdit' => '<NEW>Tento typ s&uacute;borov je zak&aacute;zan&yacute;.',
+	'form_erreur' => '<NEW>Chyba:',
+	'format_fichier' => '<NEW>Form&aacute;t s&uacute;boru:',
+	'format_liste' => 'drop-down', # MODIF
+	'format_liste_ou_radio' => '<NEW>Form&aacute;t zoznamu',
+	'format_radio' => 'radio buttons', # MODIF
+	'forms_obligatoires' => '<NEW>Formul&aacute;re potrebn&eacute; na &uacute;pravu tohto:',
+	'formulaire' => '<NEW>Formul&aacute;r',
+	'formulaire_aller' => '<NEW>Prejs&#357; na formul&aacute;r',
+	'formulaires_copie' => '<NEW>K&oacute;pia @nom@',
+	'formulaires_sondages' => '<NEW>Formul&aacute;re a ankety',
+
+	// H
+	'html_wrapper' => '<NEW>Uzavrie&#357; pole HTML k&oacute;de',
+
+	// I
+	'icone_ajouter_donnees' => '<NEW>Pridanie odpoved&iacute;',
+	'icone_creer_formulaire' => '<NEW>Vytvorenie nov&eacute;ho formul&aacute;ra',
+	'icone_creer_table' => 'Create a new form', # MODIF
+	'importer_form' => '<NEW>Importova&#357; formul&aacute;r',
+	'info_apparence' => '<NEW>Toto je uk&aacute;&#382;ka formul&aacute;ra, takto ho uvidia n&aacute;v&#353;tevn&iacute;ci verejnej str&aacute;nky.',
+	'info_articles_lies_donnee' => '<NEW>&#268;l&aacute;nky s odkazmi',
+	'info_champs_formulaire' => '<NEW>Tu m&ocirc;&#382;ete vytv&aacute;ra&#357; a upravova&#357; polia, ktor&eacute; bud&uacute; vyp&#314;&#328;a&#357; n&aacute;v&#353;tevn&iacute;ci.',
+	'info_obligatoire_02' => '<NEW>[Povinn&eacute;]',
+	'info_rubriques_liees_donnee' => '<NEW>Rubriky s odkazmi',
+	'info_sondage' => '<NEW>Ak je v&aacute;&#353; formul&aacute;r anketou, v&yacute;sledky v&yacute;berov&yacute;ch pol&iacute; bud&uacute; pridan&eacute; a zobrazia sa.',
+	'info_supprimer_formulaire' => '<NEW>Ur&#269;ite chcete odstr&aacute;ni&#357; tento formul&aacute;r?',
+	'info_supprimer_formulaire_reponses' => '<NEW>Na tento formul&aacute;r pri&#353;li odpovede. Ur&#269;ite ho chcete odstr&aacute;ni&#357;?',
+
+	// L
+	'lien_apercu' => '<NEW>Uk&aacute;&#382;ka',
+	'lien_champ' => '<NEW>Polia',
+	'lien_propriete' => '<NEW>Vlastnosti',
+	'lien_retirer_donnee_liante' => '<NEW>Vymaza&#357; tento odkaz',
+	'lien_retirer_donnee_liee' => '<NEW>Vymaza&#357; odkaz',
+	'lier_articles' => '<NEW>Umo&#382;ni&#357; prira&#271;ovanie d&aacute;t a &#269;l&aacute;nkov',
+	'lier_documents' => '<NEW>Umo&#382;ni&#357; k d&aacute;tam prip&aacute;ja&#357; dokumenty',
+	'lier_documents_mail' => '<NEW>Prip&aacute;ja&#357; dokumenty k e-mailom',
+	'liste_choix' => '<NEW>Zoznam dostupn&yacute;ch v&yacute;berov',
+
+	// M
+	'moderation_donnees' => '<NEW>Skontrolova&#357; d&aacute;ta pred publikovan&iacute;m:',
+	'modifiable_donnees' => '<NEW>D&aacute;ta, ktor&eacute; sa daj&uacute; upravova&#357; z verejnej str&aacute;nky:',
+	'monetaire_invalide' => '<NEW>Neplatn&eacute; pole mena',
+	'monnaie_euro' => 'Euro (&euro;)', # MODIF
+	'multiple_donnees' => '<NEW>&Uacute;prava d&aacute;t z verejnej str&aacute;nky:',
+
+	// N
+	'nb_decimales' => '<NEW>Po&#269;et desatinn&yacute;ch miest',
+	'nombre_reponses' => '<NEW>@number@ odpoved&iacute;',
+	'nouveau_champ' => '<NEW>Nov&eacute; pole',
+	'nouveau_choix' => '<NEW>Nov&yacute; v&yacute;ber',
+	'nouveau_formulaire' => '<NEW>Nov&yacute; formul&aacute;r',
+	'numerique_invalide' => '<NEW>Neplatn&eacute; &#269;&iacute;seln&eacute; pole',
+
+	// P
+	'page' => '<NEW>Str&aacute;nka',
+	'pas_mail_confirmation' => '<NEW>&#381;iadny potvrdzovac&iacute; e-mail',
+	'probleme_technique' => 'technical problem. Your reply can not be taken into account.', # MODIF
+	'probleme_technique_upload' => '<NEW>technick&yacute; probl&eacute;m. Presun s&uacute;boru sa nepodaril.',
+	'publication_donnees' => '<NEW>Publik&aacute;cia d&aacute;t',
+
+	// R
+	'rang' => '<NEW>Hodnotenie',
+	'remplir_un_champ' => '<NEW>Pros&iacute;m, vypl&#328;te aspo&#328; jedno pole.',
+	'reponse' => '<NEW>odpove&#271; @id_reponse@',
+	'reponse_depuis' => '<NEW>Zo str&aacute;nky',
+	'reponse_enregistree' => '<NEW>Va&#353;a odpove&#271; bola zaregistrovan&aacute;.',
+	'reponse_envoyee' => '<NEW>Odpove&#271; odoslan&aacute;',
+	'reponse_envoyee_a' => '<NEW>na',
+	'reponse_retrovez' => '<NEW>N&aacute;js&#357; t&uacute;to odpove&#271; v administra&#269;nom rozhran&iacute;:',
+	'reponses' => '<NEW>odpovede',
+	'resultats' => '<NEW>V&yacute;sledky: ',
+
+	// S
+	'site_introuvable' => '<NEW>T&aacute;to webstr&aacute;nka sa nena&#353;la.',
+	'sondage_deja_repondu' => '<NEW>Na t&uacute;to anketu ste u&#382; odpovedali',
+	'sondage_non' => '<NEW>Tento formul&aacute;r nie je anketa',
+	'sondage_oui' => '<NEW>Tento formul&aacute;r je anketa',
+	'suivi_formulaire' => 'Form follow-up', # MODIF
+	'suivi_formulaires' => 'Forms follow-up', # MODIF
+	'suivi_reponses' => 'Replies follow-up', # MODIF
+	'supprimer' => '<NEW>Odstr&aacute;ni&#357;',
+	'supprimer_champ' => '<NEW>Odstr&aacute;ni&#357; toto pole',
+	'supprimer_choix' => '<NEW>odstr&aacute;ni&#357; tento v&yacute;ber',
+	'supprimer_formulaire' => '<NEW>Odstr&aacute;ni&#357; tento formul&aacute;r',
+	'supprimer_reponse' => '<NEW>Odstr&aacute;ni&#357; t&uacute;to odpove&#271;',
+
+	// T
+	'tables' => '<NEW>Tabu&#318;ky',
+	'taille_max' => '<NEW>Maxim&aacute;lna ve&#318;kos&#357; (v kB)',
+	'telecharger' => '<NEW>Uploadova&#357;',
+	'telecharger_reponses' => '<NEW>Stiahnu&#357; odpovede',
+	'titre_formulaire' => '<NEW>N&aacute;zov formul&aacute;ra',
+	'total_votes' => '<NEW>Celkom hlasov',
+	'tous_formulaires' => '<NEW>V&#353;etky formul&aacute;re',
+	'tous_sondages' => '<NEW>V&#353;etky ankety',
+	'tous_sondages_proteges' => '<NEW>V&#353;etky chr&aacute;nen&eacute; ankety',
+	'tous_sondages_public' => '<NEW>V&#353;etky verejn&eacute; ankety',
+	'toutes_tables' => '<NEW>V&#353;etky formul&aacute;re',
+	'type_form' => '<NEW>Typ formul&aacute;ra',
+
+	// U
+	'une_reponse' => '<NEW>jedna polo&#382;ka d&aacute;t',
+	'unite_monetaire' => '<NEW>Menov&aacute; jednotka',
+
+	// V
+	'valider' => '<NEW>Overi&#357;',
+	'verif_web' => '<NEW>Skontrolova&#357; existenciu webstr&aacute;nky',
+	'vider' => '<NEW>Vypr&aacute;zdni&#357;',
+	'voir_article' => '<NEW>Zobrazi&#357; &#269;l&aacute;nok',
+	'voir_resultats' => '<NEW>Zobrazi&#357; v&yacute;sledky'
+);
+
+?>
