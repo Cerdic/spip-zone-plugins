9d768cc35a73f9c2663e157869ae209ec8fc6278
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-08-09 09:20:14 +0000

    langues (bando)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@39725 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ bandeau/lang/bando_sk.php
@@ -0,0 +1,37 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// I
+	'icone_accueil' => 'Launch pad', # MODIF
+	'icone_administration' => 'Maintenance', # MODIF
+	'icone_configuration' => 'Configuration', # MODIF
+	'icone_edition' => 'Site edit', # MODIF
+	'icone_langage' => 'My language', # MODIF
+	'icone_mes_infos' => 'My informations', # MODIF
+	'icone_preferences' => 'My preferences', # MODIF
+	'icone_reactions' => 'Activity', # MODIF
+	'icone_squelette' => 'Templates', # MODIF
+	'icone_suivi_edito' => 'Publication', # MODIF
+	'icone_suivi_publication' => 'To follow the publication', # MODIF
+	'icone_visiter_site' => 'Visit', # MODIF
+
+	// L
+	'label_choix_langue' => 'Manage your language ', # MODIF
+	'label_slogan_site' => 'Slogan', # MODIF
+	'label_taille_ecran' => 'Display', # MODIF
+	'label_texte_et_icones_navigation' => 'Navigation menu', # MODIF
+	'label_texte_et_icones_page' => 'Page display', # MODIF
+
+	// T
+	'titre_config_langage' => 'Configurer la langue', # MODIF
+	'titre_configurer_preferences' => 'Configure your preferences', # MODIF
+	'titre_identite_site' => 'Site\'s identity', # MODI
+);
+
+?>
