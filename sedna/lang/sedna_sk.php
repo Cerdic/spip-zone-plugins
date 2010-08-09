40bb33a12bbdebfaff5a466a84cb5e818617ad4f
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-08-09 09:21:42 +0000

    langues (sedna)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@39746 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ sedna/lang/sedna_sk.php
@@ -0,0 +1,51 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'aff_resume' => 'Show summaries', # MODIF
+	'afficher_sources' => 'Show this feed', # MODIF
+	'annee' => 'Last year', # MODIF
+	'articles_recents_court' => 'Recent posts', # MODIF
+
+	// C
+	'connexion' => 'Prihl&aacute;senie',
+
+	// D
+	'deconnexion' => 'Odhl&aacute;si&#357;',
+	'derniere_syndication' => 'Last syndication of this feed performed', # MODIF
+	'deuxjours' => 'Posledn&eacute; dva dni',
+
+	// L
+	'liens' => 'pr&iacute;spevok',
+	'liens_pluriel' => 'pr&iacute;spevkov',
+
+	// M
+	'masquer_resume' => 'Hide summaries', # MODIF
+	'masquer_sources' => 'Hide this feed', # MODIF
+	'mois' => 'Last month', # MODIF
+
+	// P
+	'pas_articles' => 'No post within this period of time!', # MODIF
+	'pas_synchro' => 'Nesynchronizuj',
+	'preferences' => 'Nastavenia',
+	'probleme_de_syndication' => 'Syndication problem', # MODIF
+
+	// S
+	'semaine' => 'Minul&yacute; t&yacute;&#382;de&#328;',
+	'sources' => 'Feeds', # MODIF
+	'synchro' => 'Synchronizuj',
+	'synchro_titre' => 'Save the list of read posts on website', # MODIF
+	'syndication_ajour' => 'Update now', # MODIF
+	'syndication_fait' => 'Syndication done', # MODIF
+
+	// T
+	'toutes' => 'V&#353;etky'
+);
+
+?>
