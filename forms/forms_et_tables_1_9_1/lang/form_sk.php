e869ac39cd556d3597ccb3450c22cd4495815d0f
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-08-09 09:20:37 +0000

    langues (form)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@39732 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ forms/forms_et_tables_1_9_1/lang/form_sk.php
@@ -0,0 +1,56 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'articles_utilisant' => '<NEW>&#268;l&aacute;nky, ktor&eacute; pou&#382;&iacute;vaj&uacute; tento formul&aacute;r',
+	'aucune_reponse' => '<NEW>&#381;iadna odpove&#271;',
+
+	// C
+	'champs_formulaire' => '<NEW>Pole formul&aacute;ra',
+
+	// F
+	'formulaire' => '<NEW>Formul&aacute;r',
+
+	// I
+	'icone_ajouter_donnees' => '<NEW>Prida&#357; odpovede',
+	'icone_creer_table' => '<NEW>Vytvori&#357; nov&yacute; formul&aacute;r',
+	'importer_donnees_csv' => '<NEW>Na&#269;&iacute;ta&#357; odpovede',
+	'info_supprimer_formulaire' => '<NEW>Naozaj chcete tento formul&aacute;r odstr&aacute;ni&#357;?',
+	'info_supprimer_formulaire_reponses' => '<NEW>K tomuto formul&aacute;ru je priraden&yacute;ch nieko&#318;ko odpoved&iacute;. 
+Naozaj ho chcete vymaza&#357;?',
+
+	// L
+	'lien_retirer_donnee_liante' => '<NEW>Odstr&aacute;ni&#357; odkaz z tejto odpovede',
+	'lien_retirer_donnee_liee' => '<NEW>Odstr&aacute;ni&#357; t&uacute;to odpove&#271;',
+
+	// N
+	'nombre_reponses' => '<NEW>@nombre@ odpoved&iacute;',
+	'nouveau_formulaire' => '<NEW>Nov&yacute; formul&aacute;r',
+
+	// S
+	'suivi_reponses' => '<NEW>Zobrazi&#357; odpovede',
+	'supprimer_formulaire' => '<NEW>Vymaza&#357; tento formul&aacute;r',
+
+	// T
+	'telecharger_reponses' => '<NEW>Stiahnu&#357; odpovede',
+	'texte_donnee_statut' => '<NEW>Status tejto odpovede',
+	'texte_statut_poubelle' => '<NEW>Odpove&#271; bola odstr&aacute;nen&aacute;',
+	'texte_statut_prepa' => '<NEW>Odpove&#271; sa upravuje',
+	'texte_statut_prop' => '<NEW>Navrhovan&aacute; odpove&#271;',
+	'texte_statut_publie' => '<NEW>Publikovan&aacute; odpove&#271;',
+	'texte_statut_refuse' => '<NEW>Zamietnut&aacute; odpove&#271;',
+	'titre_formulaire' => '<NEW>N&aacute;zov formul&aacute;ra',
+	'toutes_tables' => '<NEW>V&#353;etky formul&aacute;re',
+	'type_des_tables' => '<NEW>Formul&aacute;re',
+
+	// U
+	'une_reponse' => '<NEW>Jedna odpove&#271;'
+);
+
+?>
