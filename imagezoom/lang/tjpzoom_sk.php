e460d54661d0fec218e5da5ee71f8e86c8c3d9c8
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-08-09 09:20:46 +0000

    langues (tjpzoom)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@39736 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ imagezoom/lang/tjpzoom_sk.php
@@ -0,0 +1,30 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// D
+	'descriptif' => 'This page allows you to configure the style of the magnifier. A personal style can be configured: see the <a href="http://plugins.spip.net/?page=article&amp;id_article=48">online documentation</a>.', # MODIF
+
+	// E
+	'example' => '<NEW>Grafick&yacute; v&yacute;sledok',
+
+	// N
+	'nostyle' => '<NEW>&#381;iaden &#353;t&yacute;l',
+
+	// O
+	'other' => '<NEW>In&yacute;',
+
+	// S
+	'size_vignette' => '<NEW>Maxim&aacute;lna &#353;&iacute;rka miniat&uacute;ry',
+	'style' => '<NEW>&#352;t&yacute;l lupy',
+
+	// T
+	'titre' => '<NEW>Configur&aacute;cia lupy'
+);
+
+?>
