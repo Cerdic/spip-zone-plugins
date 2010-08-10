6cb48647dbdbe1f5a9949a3d39badf8454605019
booz@rezo.net <booz@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-08-10 14:59:47 +0000

    le plugin jqplot, pour faire des graphiques jolis et élaborés en js est disponible, voir les exemples jqplot.html pour intégration
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@39757 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ jqplot/jqplot_options.php
@@ -0,0 +1,7 @@
+<?php
+
+if (!defined('_DIR_LIB')) define('_DIR_LIB', _DIR_RACINE . 'lib/');
+
+define('_DIR_JQPLOT_JS',_DIR_LIB.'dist/');
+
+?>
---
+++ jqplot/jqplot_pipelines.php
@@ -0,0 +1,14 @@
+<?php
+function jqplot_header_prive($flux){
+ $flux .= "<script type='text/javascript' src='" . _DIR_JQPLOT_JS . "jquery.jqplot.js'></script>\n";
+ $flux .= "<link rel='stylesheet' type='text/css' media='all' href='" . _DIR_JQPLOT_JS . "jquery.jqplot.css' />\n";
+
+ return $flux;
+}
+
+function jqplot_insert_head($flux){
+ $flux = jqplot_header_prive($flux) ;
+
+ return $flux;
+}
+?>
