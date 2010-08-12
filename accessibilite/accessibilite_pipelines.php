e1f2ccf73552b9fbf88bb9541e023f4d4777cbad
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-08-12 11:03:37 +0000

    Meilleur title pour les liens vers les documents : Intitulé du document (ou à défaut : Document), type, poids et langue (voir http://permalink.gmane.org/gmane.comp.web.spip.zone/19110)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@39801 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- accessibilite/accessibilite_pipelines.php
+++ accessibilite/accessibilite_pipelines.php
@@ -1,25 +1,35 @@
 <?php
 
 function accessibilite_pre_liens($texte){
-	// uniquement dans le public
-	//if (test_espace_prive()) return $texte;
+	define(_ACCESSIBILITE_CONSERVER_BULLE, false);
 	$regs = $match = array();
 	// pour chaque lien
 	if (preg_match_all(_RACCOURCI_LIEN, $texte, $regs, PREG_SET_ORDER)) {	
 		foreach ($regs as $reg) {
-			// si le lien est de type raccourcis "doc40"
+			// Attributs du lien (texte, bulle, lang)
+			$intitule = traiter_raccourci_lien_atts($reg[1]);
+			// si le lien est de type raccourcis "doc40"			
 			$type = typer_raccourci($reg[4]);
 			if ($type[0] == 'document') {
 				// Rechercher la taille du Doc dont l'id est dans $type[2]
 				$row = sql_fetsel(
-					array('taille', 'spip_types_documents.titre'), 
+					array('spip_documents.titre as T1', 'taille', 'spip_types_documents.titre as T2'), 
 					array('spip_documents', 'spip_types_documents'), 
 					array('id_document='.$type[2], 'spip_documents.extension=spip_types_documents.extension')
 					);
-				$title = (strpos($reg[0], '|') ? ' - ':'|') 
-					. $row['titre'] 
-					. ' (' . taille_en_octets($row['taille']) . ')';
-				$lien = substr_replace($reg[0], $title, strpos($reg[0], '->'), 0);
+				$textelien = ($intitule[0]) ? $intitule[0]:$row['T1'];
+				$langue = ($intitule[2]) ? '{'.$intitule[2].'}':'';
+				// Si pas de titre pour le doc, remplacer par "Document"
+				$titredoc = ($row['T1']) ? $row['T1']:_T('info_document');
+				// Quand un title est spécifie il doit etre plus plus long que l'intitule
+				// car les lecteurs d'ecran lisent le plus long des deux
+				$title = ((($intitule[1]) && _ACCESSIBILITE_CONSERVER_BULLE) ? $titredoc. ' &ndash; ' .$intitule[1]:$titredoc) // Le texte du lien + Nom du doc
+					. ' &ndash; ' . $row['T2'] // Le type du doc
+					. ' (' . taille_en_octets($row['taille']) . ')' // sa taille
+					. (($intitule[2]) ? ' ('.traduire_nom_langue($intitule[2]).')':''); // La langue presente dans le lien (malheureusement, info non disponible dans la table spip_documents)
+				
+				// Rebatir le raccourcis typo du lien avec les informations modifiees
+				$lien = '['. $textelien . '|'. $title .$langue .'->'. $reg[4] .']';
 				$texte = str_replace($reg[0], $lien, $texte);
 			}
 		}	
