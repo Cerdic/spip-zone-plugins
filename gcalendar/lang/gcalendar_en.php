627240ebf5bc27fa53752e168d4f64e633dbe8d5
superyms@yahoo.fr <superyms@yahoo.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-08-12 12:26:24 +0000

    ajout du multilinguisme langues : frnaçaise arabe anglais plugin.xml plus clair passage en mode test mise à jour version 1.0.9
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@39804 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ gcalendar/lang/gcalendar_en.php
@@ -0,0 +1,19 @@
+<?php
+
+// This is a SPIP module file  --  Ceci est un fichier module de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+'adress_mail' =>'E-mail address associated with the calendar: ',
+'affichage_agenda' => 'Display address of the agenda',
+'chargement_agenda' => 'Loading calendar ...',
+'descriptif' => '<h4>Configuring the Plugin GCalendar</h4>',
+'gcalendar' => 'GCalendar',
+'nombre_evenements' => 'Number of events:',
+'non' => 'No',
+'oui' => 'Yes',
+
+
+);
+
+?>
---
+++ gcalendar/lang/gcalendar_fr.php
@@ -0,0 +1,19 @@
+<?php
+
+// This is a SPIP module file  --  Ceci est un fichier module de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+'adress_mail' =>"Adresse mail associée à l'agenda : ",
+'affichage_agenda' => "Affichage de l'adresse de l'agenda",
+'chargement_agenda' => "Chargement de l'agenda...",
+'descriptif' => '<h4>Configuration du Plugin GCalendar</h4>',
+'gcalendar' => 'GCalendar',
+'nombre_evenements' => "Nombre d'évènements :",
+'non' => 'Non',
+'oui' => 'Oui',
+
+
+);
+
+?>
