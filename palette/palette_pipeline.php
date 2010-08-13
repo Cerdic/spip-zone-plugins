8ce40fb19513185fca70c43f318ccd731f173d2c
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-08-13 19:40:27 +0000

    Passer par des pipelines modernes en SPIP >= 2.1
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@39830 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- palette/palette_pipeline.php
+++ palette/palette_pipeline.php
@@ -1,16 +1,20 @@
 <?php
 
 function palette_insert_head($flux) {
-  $cfg = unserialize($GLOBALS['meta']['palette']);
-  if ($cfg['palette_public'] =='on')
-	  $flux .= palette_header_common('public');
+	if(version_compare($GLOBALS['spip_version_branche'],"2.1.0","<")){
+		$cfg = unserialize($GLOBALS['meta']['palette']);
+		if ($cfg['palette_public'] =='on')
+			$flux .= palette_header_common('public');
+	}
 	return $flux;
 }
 
 function palette_header_prive($flux) {
-  $cfg = unserialize($GLOBALS['meta']['palette']);
-	if ($cfg['palette_ecrire'] == 'on')
-	  $flux .= palette_header_common('prive');
+	if(version_compare($GLOBALS['spip_version_branche'],"2.1.0","<")){
+		$cfg = unserialize($GLOBALS['meta']['palette']);
+		if ($cfg['palette_ecrire'] == 'on')
+			$flux .= palette_header_common('prive');
+	}
 	return $flux;
 }
 
@@ -21,11 +25,40 @@ function palette_header_prive($flux) {
  * @return string
  */
 function palette_header_common($type) {
-	# spip_log("type = $type");
 	$f = charger_fonction('palette_header', 'inc');
 	if (is_callable($f))
 		return $f($type);
 	else
 		return '';
+}
+
+function palette_jquery_plugins($plugins){
+	if(version_compare($GLOBALS['spip_version_branche'],"2.1.0",">=")){
+		$cfg = unserialize($GLOBALS['meta']['palette']);
+		if((test_espace_prive() && ($cfg['palette_ecrire'] == 'on')) OR !test_espace_prive()){
+			$plugins[] = _DIR_LIB_PALETTE.'farbtastic.js';
+			$plugins[] = 'javascript/palette2.js';
+		}
+	}
+	return $plugins;
+}
+
+function palette_insert_head_css($flux){
+	if(version_compare($GLOBALS['spip_version_branche'],"2.1.0",">=")){
+		static $done = false;
+		if ($done) return $flux;
+		$done = true;
+		
+		$css1 = _DIR_LIB_PALETTE.'farbtastic.css';
+		if((test_espace_prive() && ($cfg['palette_ecrire'] == 'on'))){
+			$css2 = generer_url_public('palette.css');
+			$flux .= "\n<link rel='stylesheet' href='$css1' type='text/css' media='projection, screen, tv' />\n";
+			$flux .= "\n<link rel='stylesheet' href='$css2' type='text/css' media='projection, screen, tv' />\n";	
+		}
+		else if(!test_espace_prive()){
+			$flux .= "\n<link rel='stylesheet' href='$css1' type='text/css' media='projection, screen, tv' />\n";
+		}
+	}
+	return $flux;
 }
 ?>
