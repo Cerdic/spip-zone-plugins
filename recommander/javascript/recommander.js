c512405198bcccef9a58222b77567d7d18c91ed8
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-08-22 09:50:41 +0000

    le bouton peut etre un lien
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@39981 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- recommander/javascript/recommander.js
+++ recommander/javascript/recommander.js
@@ -21,4 +21,5 @@ recommander_js();
 $("#recommander>h2, #recommander_bouton").click(function(){
 	$("div#formulaire_recommander:visible").slideUp("slow");
 	$("div#formulaire_recommander:hidden").slideDown("slow");
+	return false; // si jamais le bouton est un lien
 });
