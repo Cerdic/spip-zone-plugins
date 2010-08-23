e54941e2e5b642d7ecfb21d2d62c3a7e0c7237b1
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-08-23 16:44:27 +0000

    Essayer de trouver une frame non monochrome pour pour la vignette
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@40025 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spipmotion/inc/spipmotion_recuperer_logo.php
+++ spipmotion/inc/spipmotion_recuperer_logo.php
@@ -14,37 +14,58 @@
  * Fonction de récupération de vignette depuis un document video
  * @param int $id_document
  */
-function inc_spipmotion_recuperer_logo($id_document){
+function inc_spipmotion_recuperer_logo($id_document,$frame=100){
 	spip_log("SPIPMOTION : recuperation d un logo du document $id_document","spipmotion");
 	if(!intval($id_document) OR !class_exists('ffmpeg_movie')){
 		return;
 	}
 
 	include_spip('inc/documents');
+	include_spip('inc/filtres_images_mini');
 
-	$document = sql_fetsel("docs.id_document,docs.fichier", "spip_documents AS docs INNER JOIN spip_documents_liens AS L ON L.id_document=docs.id_document","L.id_document=".sql_quote($id_document));
+	$document = sql_fetsel("docs.id_document,docs.fichier,docs.framecount", "spip_documents AS docs INNER JOIN spip_documents_liens AS L ON L.id_document=docs.id_document","L.id_document=".sql_quote($id_document));
 	$chemin_court = $document['fichier'];
 	$chemin = get_spip_doc($chemin_court);
 	$movie = new ffmpeg_movie($chemin,0);
 	if($movie->hasVideo()){
-		$frame1 = $movie->getFrame(100);
-		if($frame1){
-			$string_temp = "$id-$type-$id_document";
-			$query = md5($string_temp);
-			$dossier_temp = _DIR_VAR;
-			$fichier_temp = "$dossier_temp$query.jpg";
-
-			$img_temp = $frame1->toGDImage();
-			imagejpeg($img_temp, $fichier_temp);
-			$img_finale = $fichier_temp;
-			$mode = 'vignette';
-
-			$ajouter_documents = charger_fonction('ajouter_documents', 'inc');
-			$x = $ajouter_documents($img_finale, $img_finale,
-					    $type, $id, $mode, $id_document, $actifs);
-
-			imagedestroy($img_temp);
-			unlink($img_finale);
+		$vignette = false;
+		
+		while(!$vignette && ($frame<$document['framecount'])){
+			$frame1 = $movie->getFrame($frame);
+			if($frame1){
+				$string_temp = "$id-$type-$id_document";
+				$query = md5($string_temp);
+				$dossier_temp = _DIR_VAR;
+				$fichier_temp = "$dossier_temp$query.jpg";
+	
+				$img_temp = $frame1->toGDImage();
+				imagejpeg($img_temp, $fichier_temp);
+				$img_finale = $fichier_temp;
+				$mode = 'vignette';
+				
+				if(_DIR_PLUGIN_FONCTIONS_IMAGES){
+					include_spip('fonctions_images_fonctions');
+					if(!filtrer('image_monochrome',$fichier_temp)){
+						imagedestroy($img_temp);
+						unlink($img_finale);
+						$frame = $frame+50;
+					}else{
+						$ajouter_documents = charger_fonction('ajouter_documents', 'inc');
+						$x = $ajouter_documents($img_finale, $img_finale,
+								    $type, $id, $mode, $id_document, $actifs);
+						imagedestroy($img_temp);
+						unlink($img_finale);
+						$vignette = true;
+					}
+				}else{
+					$ajouter_documents = charger_fonction('ajouter_documents', 'inc');
+					$x = $ajouter_documents($img_finale, $img_finale,
+							    $type, $id, $mode, $id_document, $actifs);
+					imagedestroy($img_temp);
+					unlink($img_finale);
+					$vignette = true;
+				}
+			}
 		}
 	}
 	return $x;
