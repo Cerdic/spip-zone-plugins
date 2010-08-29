8bc577d86e6eed584230ee3af3838fe510791ddf
andre@alternatives.ca <andre@alternatives.ca@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-08-29 17:22:35 +0000

    Fichier de langue : anglais
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@40393 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ webfonts/lang/webfonts_en.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	'titre_menu_webfonts' => 'WebFonts',
+	'titre_page_webfonts' => 'Web Fonts setting for this site',
+	'label_googlefonts_api' => 'Font list from Google Font Directory',
+	'explication_googlefonts_api' => 'Add each font family and style name that you wish to use in your stylesheet, as defined in "Font API parameter name" specifications (on Google Font Directory website). One font family per line.',
+
+	
+);
+?>
