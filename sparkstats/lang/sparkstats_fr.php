395e2fd4087605d14e7d0063e11e221703fc112c
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-08-29 11:54:13 +0000

    Un logo basique et une petite description dans le CFG
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@40382 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- sparkstats/lang/sparkstats_fr.php
+++ sparkstats/lang/sparkstats_fr.php
@@ -4,6 +4,9 @@
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+	// D
+	'description_sparkstats' => 'Sparkstats affiche un mini graphique des visites li&eacute;es &agrave; un article.',
+
 	// S
 	'sparkstats_cible' => 'S&eacute;lecteur jQuery cible o&ugrave; sera affich&eacute; le graphique',
 
