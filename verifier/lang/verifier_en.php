59ce89db96c361d4f3d3377dfcfaf88b8f50503a
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-08-29 21:57:19 +0000

    langues (verifier)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@40418 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ verifier/lang/verifier_en.php
@@ -0,0 +1,64 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// E
+	'erreur_date' => 'The date format is invalid.',
+	'erreur_email' => 'The email address <em>@email@</em> is not correctly formatted.',
+	'erreur_email_nondispo' => 'The email address <em>@email@</em> has already been used.',
+	'erreur_entier' => 'The value must be an integer.',
+	'erreur_entier_entre' => 'The value must be between @min@ and @max@.',
+	'erreur_entier_max' => 'The value must be less than @max@.',
+	'erreur_entier_min' => 'The value must be greater than @min@.',
+	'erreur_numerique' => 'The number format is invalid.',
+	'erreur_regex' => 'The regex string is incorrectly formatted.',
+	'erreur_siren' => 'The SIREN number is invalid.',
+	'erreur_siret' => 'The SIRET number is invalid.',
+	'erreur_taille_egal' => 'The value must have exactly @egal@ characters.',
+	'erreur_taille_entre' => 'The value must have between @min@ and @max@ characters.',
+	'erreur_taille_max' => 'The value must have no more than @max@ characters.',
+	'erreur_taille_min' => 'The value must have no less than @min@ characters.',
+	'erreur_telephone' => 'The telephone number is invalid.',
+	'erreur_url' => 'The URL address is invalid.',
+
+	// O
+	'option_email_disponible_label' => 'Available address',
+	'option_email_disponible_label_case' => 'Check that the address has not already be used by another user',
+	'option_email_mode_5322' => 'Check against the strictest standards available',
+	'option_email_mode_label' => 'Email checking mode',
+	'option_email_mode_normal' => 'Normal SPIP checking',
+	'option_email_mode_strict' => 'Less permissive checking',
+	'option_entier_max_label' => 'Maximum value',
+	'option_entier_min_label' => 'Minimum value',
+	'option_regex_modele_label' => 'The value must match the following expression',
+	'option_siren_siret_mode_label' => 'Are you sure you wish to confirm?',
+	'option_siren_siret_mode_siren' => 'SIREN number',
+	'option_siren_siret_mode_siret' => 'SIRET number',
+	'option_taille_max_label' => 'Maximum size',
+	'option_taille_min_label' => 'Minimum size',
+
+	// T
+	'type_date' => 'Date',
+	'type_date_description' => 'Check that the value is date in the DD/MM/YYYY format. The separator character can be anything (".", "/", etc).',
+	'type_email' => 'Email address',
+	'type_email_description' => 'Check that the email address is correctly formatted.',
+	'type_email_disponible' => 'Availability of an email address',
+	'type_email_disponible_description' => 'Check that the email address has not already been used by another system user.',
+	'type_entier' => 'Integer',
+	'type_entier_description' => 'Check that the value is an integer, with the option of being restricted between two range values.',
+	'type_regex' => 'Regular expression',
+	'type_regex_description' => 'Check that the value matches the defined expression. For more information on using regular expressions, please refer to <a href="http://fr2.php.net/manual/en/reference.pcre.pattern.syntax.php">the online PHP help</a>.',
+	'type_siren_siret' => 'SIREN or SIRET',
+	'type_siren_siret_description' => 'Check that the value is a valid number from the French <a href="http://fr.wikipedia.org/wiki/SIREN">Syst&egrave;me d’Identification du R&eacute;pertoire des ENtreprises</a> (Company Registry ID System).',
+	'type_taille' => 'Size',
+	'type_taille_description' => 'Check that the size of the value corresponds to the minimum and/or maximum specified.',
+	'type_telephone' => 'Telephone number',
+	'type_telephone_description' => 'Check that the telephone number matches a recognised telephone number format.'
+);
+
+?>
