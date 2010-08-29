27e02cebc08c2a7b3ae0764e425cc398e2547020
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-08-29 21:56:58 +0000

    langues (thumbsites)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@40417 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ thumbsites/lang/thumbsites_en.php
@@ -0,0 +1,49 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'bouton_effacer' => 'Delete',
+	'bouton_valider' => 'Submit',
+	'bulle_bouton_effacer' => 'Delete the entries and return to the original values',
+	'bulle_bouton_valider' => 'Confirm the entries',
+
+	// C
+	'cfg_descriptif' => 'This page is sued for configuring the Thumbsites plugin, and in particular, to select the thumbnails server. To find out more, please refer to the <a href="http://www.spip-contrib.net/?article2584">documentation on contrib</a>',
+	'cfg_inf_girafa_description' => 'The thumbnails generated will have a maximum size of 160 x 120 pixels (these values can be modified on the Girafa site)',
+	'cfg_inf_girafa_parametres' => 'Please enter your personal details:',
+	'cfg_inf_girafa_presentation' => 'To use this service you must be subscribed on the <a href="http://www.girafa.com">Girafa</a> site. There is a free version if you need less than 2000 screen captures per day.',
+	'cfg_inf_miwin_parametres' => 'Please enter your personal details:',
+	'cfg_inf_miwin_presentation' => 'To use this service, you must include a link to the <a href="http://thumbs.miwim.fr">Miwin</a> site. The continued existence of that link is periodically checked by a script.',
+	'cfg_inf_rotothumb_parametres' => 'Please enter your personal details:',
+	'cfg_inf_rotothumb_presentation' => 'To use this service, you do not have to subscribe but you do have to create a link to the <a href="http://www.robothumb.com">Robothumb</a> site. The continued existence of that link is periodically checked by their servers.',
+	'cfg_inf_thumbshots_de_description' => 'The generated thumbnails will be 120 x 90 pixels.',
+	'cfg_inf_thumbshots_de_presentation' => '<a href="http://www.thumbshots.de">Thumbshots.de</a> offers a service equivalent to Thumbshots.com. Even though the services are identical, they are actually 2 distinct sites.',
+	'cfg_inf_thumbshots_description' => 'The generated thumbnails will be 120 x 90 pixels.',
+	'cfg_inf_thumbshots_presentation' => '<a href="http://www.thumbshots.com">Thumbshots.com</a> offers a free service that does not require identification.',
+	'cfg_inf_websnapr_parametres' => 'Please enter your personal details:',
+	'cfg_inf_websnapr_presentation' => 'To use this service, you must subscribe on the <a href="http://www.websnapr.com">Websnapr</a> site. The free version enables the processing of 250,000 site screen captures per month at a maximum speed of 80 captures per hour.',
+	'cfg_itm_serveur_girafa' => 'Girafa',
+	'cfg_itm_serveur_miwin' => 'Miwin',
+	'cfg_itm_serveur_robothumb' => 'Robothumb',
+	'cfg_itm_serveur_thumbshots' => 'Thumbshots.com',
+	'cfg_itm_serveur_thumbshots_de' => 'Thumbshots.de',
+	'cfg_itm_serveur_websnapr' => 'Websnapr',
+	'cfg_itm_taille_vignette' => '@taille@ pixels',
+	'cfg_lbl_choix_serveur' => 'Select the thumbnails server',
+	'cfg_lbl_cle' => 'Your key',
+	'cfg_lbl_identifiant' => 'Your identifier',
+	'cfg_lbl_signature' => 'Your signature',
+	'cfg_lbl_taille_vignette' => 'Size of thumbnails',
+	'cfg_lgd_choix_serveur' => 'Server',
+
+	// T
+	'titre_thumbshot_site' => 'SITE THUMBNAIL'
+);
+
+?>
