eacfc0fcfc9f82a544bee901af2e59e807823f47
gaitan@gmail.com <gaitan@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-09-01 03:20:40 +0000

    spanish lang file
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@40451 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ mediabox/lang/mediabox_es.php
@@ -0,0 +1,43 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+    // B
+    'bouton_reinitialiser' => 'Reinicializar',
+
+    'titre_menu_box' => 'Caja Multimedia',
+    'titre_page_configurer_box' => 'Configuraci&iacute;on de la caja multimedia',
+
+    'explication_selecteur' => 'Indique los selectores que activar&aacute;n la caja (Expresi&oacute;n CSS o selector jQuery)',
+    'explication_selecteur_galerie' => 'Indique los selectores que activar&aacute;n la caja en forma de galer&iacute;a. (Expresi&oacute;n CSS o selector jQuery)',
+    'explication_splash_url' => 'Indique la URL de un contenido multimedia que se mostrar&aacute; en una caja la primera vez que un visitante accede al sitio p&uacute;blico',
+    'explication_traiter_toutes_images' => '&iquest;Insertar una caja en todas las im&aacute;genes?',
+
+    'label_apparence' => 'Apariencia',
+    'label_selecteur_commun' => 'Selector general',
+    'label_selecteur_galerie' => 'Selector de galer&iacute;a',
+    'label_traiter_toutes_images' => 'Im&aacute;genes',
+    'label_skin' => 'Estilo visual',
+    'label_aucun_style' => 'No usar ning&uacute;n estilo por defecto',
+    'label_transition' => 'Efecto de transici&oacute;n',
+    'label_choix_transition_elastic' => 'El&aacute;stico',
+    'label_choix_transition_fade' => 'Fondo degradado',
+    'label_choix_transition_none' => 'Ninguno',
+    'label_speed' => 'Velocidad de transici&oacute;n (ms)',
+    'label_maxwidth' => 'Ancho m&aacute;ximop (% o px)',
+    'label_maxheight' => 'Alto m&aacute;ximo (% o px)',
+    'label_slideshow_speed' => 'Tiempo de permanencia de las diapositivas en presentaci&oacute;n (ms)',
+    'label_splash' => 'Caja Splash',
+    'label_splash_url' => 'URL del contenido',
+
+    'boxstr_slideshowStart' => 'Presentaci&oacute;n de diapositivas',
+    'boxstr_slideshowStop' => 'Detener',
+    'boxstr_current' => '{current}/{total}',
+    'boxstr_previous' => 'Anterior',
+    'boxstr_next' => 'Siguiente',
+    'boxstr_close' => 'Cerrar',
+    'boxstr_zoom' => 'Zoom',
+    
+);
+?>
