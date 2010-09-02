f8ce91e4325733dfca8cd677a1c3b9c108211e71
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-09-02 21:44:01 +0000

    Un plugin à utiliser dans le cadre de la mutualisation : création automatique d'un auteur à l'activation du plugin. Il suffit de mettre le plugin dans extensions/ pour en bénéficier. Il faut aussi dans config/mes_options.php mettre : {{{ define ('_AA_STATUT', '0minirezo'); define ('_AA_webmestre', 'oui'); define ('_AA_LOGIN', 'adminmutu'); define ('_AA_PASSWORD', 'password'); define ('_AA_BIO', 'Administrateur de la mutualisation'); define ('_AA_NOM_SITE', 'Site Admin mutu'); define ('_AA_URL_SITE', 'http://www.spip.net/'); define ('_AA_NOM', 'Admin (Admin du serveur)'); define ('_AA_EMAIL', 'uneadresseemailvalide'); }}}
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@40484 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ auteur_automatique/base/auteur_automatique_install.php
@@ -0,0 +1,51 @@
+<?php
+/*
+* Configuration de SPIP pour auteur_automatique
+* Realisation : RealET : real3t@gmail.com
+* Attention, fichier en UTF-8 sans BOM
+*/
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+/*
+ * Fonction d'installation, mise a jour de la base
+ *
+ * @param unknown_type $nom_meta_base_version
+ * @param unknown_type $version_cible
+ */
+function auteur_automatique_upgrade($nom_meta_base_version,$version_cible){
+	$current_version = 0.0;
+
+	if (version_compare($current_version,'1.0','<')) {
+		if (defined("_AA_PASSWORD")) {
+			include_spip('action/editer_auteur');
+			$id_auteur = insert_auteur();
+			$c = array('statut'=>_AA_STATUT,
+					'webmestre'=>_AA_webmestre,
+					'login'=>_AA_LOGIN,
+					'pass'=>_AA_PASSWORD,
+					'bio'=>_AA_BIO,
+					'nom_site'=>_AA_NOM_SITE,
+					'url_site'=>_AA_URL_SITE,
+					'nom'=>_AA_NOM,
+					'email'=>_AA_EMAIL
+					);
+			$err = instituer_auteur($id_auteur, $c);
+			$err .= auteurs_set($id_auteur, $c);
+			if ($err) spip_log("auteur_automatique: $err");
+		}
+		include_spip('inc/meta');
+		ecrire_meta($nom_meta_base_version,$current_version='1.0','non');
+	}
+}
+
+/*
+ * Fonction de desinstallation
+ *
+ * @param unknown_type $nom_meta_base_version
+ */
+function auteur_automatique_vider_tables($nom_meta_base_version) {
+	include_spip('inc/meta');
+	effacer_meta($nom_meta_base_version);
+}
+?>
