ae0ef9a5b5b25c71d0211faa423f3ba9e887c217
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-09-03 09:05:32 +0000

    Une alerte urgence enlevement d'articles
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@40488 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ alerte_urgence/lang/alerte_fr.php
@@ -0,0 +1,10 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/_stable_/cfg/lang/
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	'message_urgent'=>'Vos articles ont tous disparu de votre site SPIP : [un correctif est disponible->http://www.spip.net/fr_article5248.html]',
+);
+
+?>
