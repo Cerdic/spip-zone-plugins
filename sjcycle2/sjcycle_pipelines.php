d235a8bbe7051cde08afff8958b821109f892ba7
yffic@lefourneau.com <yffic@lefourneau.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-09-04 14:27:36 +0000

    Correction d'un bug d'affichage des tooltips
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@40505 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- sjcycle2/sjcycle_pipelines.php
+++ sjcycle2/sjcycle_pipelines.php
@@ -3,7 +3,7 @@
 function sjcycle_insert_head($flux){
 	$conf_jcycle = lire_config('sjcycle');
 	$flux .="\n".'<script src="'.url_absolue(find_in_path('javascript/jquery.cycle.all.min.js')).'" type="text/javascript"></script>';
-	if($conf_jcycle["sjcycle_tooltip"]) {/* Modif Yffic*/
+	if($conf_jcycle["tooltip"]) {
 		$flux .="\n".'<link rel="stylesheet" href="'.url_absolue(find_in_path('javascript/jquery.tooltip.css')).'" type="text/css" media="all" />';
 		$flux .="\n".'<script src="'.url_absolue(find_in_path('javascript/jquery.tooltip.js')).'" type="text/javascript" charset="utf-8"></script>';
 	}
