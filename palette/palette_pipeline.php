2f757506c495fc96389bdb82ac4f0841fb502a7d
webmaster@geomaticien.com <webmaster@geomaticien.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-09-07 05:13:00 +0000

    Restauration du fonctionnement normal du plugin Palette. Bugs corrigés : - correction des incohérences entre palette_install et config_palette pour restaurer le fonctionnement (le formulaire cfg ne marchait pas à cause de ces incohérences) - ajout d'un avertissement dans le commentaire de palette.js.html dans l'espoir d'éviter de nouvelles confusions entre un squelette spip (".html") contenant des chaines de langue à tradaire et un simple js ... - revert + corrections sur palette_pipeline : restaure la lecture des chaines de langues incluses ainsi que le fonctionnement normal des css, qui était cassé. - correction du plugin.xml en fonction de ce revert, et versionnemment 1.3.1
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@40528 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- palette/palette_pipeline.php
+++ palette/palette_pipeline.php
@@ -1,20 +1,16 @@
 <?php
 
 function palette_insert_head($flux) {
-	if(version_compare($GLOBALS['spip_version_branche'],"2.1.0","<")){
-		$cfg = unserialize($GLOBALS['meta']['palette']);
-		if ($cfg['palette_public'] =='on')
-			$flux .= palette_header_common('public');
-	}
+  $cfg = unserialize($GLOBALS['meta']['config_palette']);
+  if ($cfg['palette_public'] == 'on')
+	  $flux .= palette_header_common('public');
 	return $flux;
 }
 
 function palette_header_prive($flux) {
-	if(version_compare($GLOBALS['spip_version_branche'],"2.1.0","<")){
-		$cfg = unserialize($GLOBALS['meta']['palette']);
-		if ($cfg['palette_ecrire'] == 'on')
-			$flux .= palette_header_common('prive');
-	}
+  $cfg = unserialize($GLOBALS['meta']['config_palette']);
+	if ($cfg['palette_ecrire'] == 'on')
+	  $flux .= palette_header_common('prive');
 	return $flux;
 }
 
@@ -25,40 +21,11 @@ function palette_header_prive($flux) {
  * @return string
  */
 function palette_header_common($type) {
+	# spip_log("type = $type");
 	$f = charger_fonction('palette_header', 'inc');
 	if (is_callable($f))
 		return $f($type);
 	else
 		return '';
-}
-
-function palette_jquery_plugins($plugins){
-	if(version_compare($GLOBALS['spip_version_branche'],"2.1.0",">=")){
-		$cfg = unserialize($GLOBALS['meta']['palette']);
-		if((test_espace_prive() && ($cfg['palette_ecrire'] == 'on')) OR !test_espace_prive()){
-			$plugins[] = _DIR_LIB_PALETTE.'farbtastic.js';
-			$plugins[] = 'javascript/palette2.js';
-		}
-	}
-	return $plugins;
-}
-
-function palette_insert_head_css($flux){
-	if(version_compare($GLOBALS['spip_version_branche'],"2.1.0",">=")){
-		static $done = false;
-		if ($done) return $flux;
-		$done = true;
-		
-		$css1 = _DIR_LIB_PALETTE.'farbtastic.css';
-		if((test_espace_prive() && ($cfg['palette_ecrire'] == 'on'))){
-			$css2 = generer_url_public('palette.css');
-			$flux .= "\n<link rel='stylesheet' href='$css1' type='text/css' media='projection, screen, tv' />\n";
-			$flux .= "\n<link rel='stylesheet' href='$css2' type='text/css' media='projection, screen, tv' />\n";	
-		}
-		else if(!test_espace_prive()){
-			$flux .= "\n<link rel='stylesheet' href='$css1' type='text/css' media='projection, screen, tv' />\n";
-		}
-	}
-	return $flux;
 }
 ?>
