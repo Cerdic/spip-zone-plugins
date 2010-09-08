43d284501b8d18c9a11365eff7d60810d670b545
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-09-08 13:53:22 +0000

    un modèle pour afficher le zip des doc d'un article
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@40558 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ zippeur/lang/zippeur_fr.php
@@ -0,0 +1,10 @@
+<?php
+/** Fichier de langue de SPIP **/
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	"ensemble_fichier"		=> "Zip de l'ensemble des fichiers"
+	
+);
+
+?>
