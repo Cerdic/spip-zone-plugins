b053960c2a0011fa3d5c6dc4e3331ce9b970e3f7
webmaster@geomaticien.com <webmaster@geomaticien.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-09-08 21:11:37 +0000

    Renommé composant Google-analytics en Google_analytics car désormais le "-" est un caractère interdit dans les noms de composants.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@40576 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ acs/models/cat/composants/google_analytics/ecrire/lang/google_analytics_ecrire_en.php
@@ -0,0 +1,11 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// ACS component specific - spécifique au composant ACS
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+'description' => 'Google_analytics script inclusion',
+'help' => '<a href="?exec=admin_vider">Empty cache</a> when activatating these component.',
+
+'UA' => 'User account (UA-xxxxxx-x)'
+);
+?>
