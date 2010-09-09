94f900c35fafc662a82a137fc7ad429d9e1be341
marcimat@rezo.net <marcimat@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-09-09 12:40:06 +0000

    Passage au grenier de C&C et de l'inutilise jquery_plugins
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@40592 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- comptes/comptes_fonctions.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Plugin Comptes & Contacts pour Spip 2.0
- * Licence GPL (c) 2009 - 2010 - Ateliers CYM
- */
-
-?>
--- comptes/comptes_options.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Plugin Comptes & Contacts pour Spip 2.0
- * Licence GPL (c) 2009 - 2010 - Ateliers CYM
- */
-
-$GLOBALS['help_server'][] = url_de_base(1)."ecrire/"._DIR_PLUGIN_COMPTES."aide/";
-
-?>
