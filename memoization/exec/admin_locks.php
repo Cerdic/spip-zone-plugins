d7e458d948bbc43aec86391b6998468127e93315
fil@rezo.net <fil@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-09-10 08:20:09 +0000

    une page pour tester les locks (a supprimer ou passer dans tests/ quand on aura avance)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@40631 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ memoization/exec/admin_locks.php
@@ -0,0 +1,59 @@
+<?php
+
+header('Content-Type: text/plain');
+
+echo time()."\n";
+
+var_dump(_MEMOIZE);
+
+echo "lock::you=".var_export(cache_get('lock::test'), 1).";\n";
+echo "test=".var_export(cache_get('test'), 1).";\n";
+
+
+$a = null;
+
+switch ($_GET['o']) {
+	case 'unknown':
+		$a = cache_get(uniqid(''));
+		break;
+
+	case 'unset':
+		$a = cache_unset('test');
+		break;
+
+	case 'unsetlock':
+		$a = cache_unset('lock::test');
+		break;
+
+	case 'lock':
+		flush();
+		$a = cache_lock('test');
+		flush();
+		echo "sleep: ".time()."\n";
+		sleep($_GET['sleep']);
+		break;
+
+	case 'inc':
+		$a = cache_inc('lock::test');
+		break;
+
+	case 'dec':
+		$a = cache_dec('lock::test');
+		break;
+
+	case 'unlock':
+		$a = cache_unlock('test');
+		break;
+
+
+}
+
+echo "\$a = ".var_export($a, true).";\n";
+
+echo "lock::test=".var_export(cache_get('lock::test'), 1).";\n";
+echo "test=".var_export(cache_get('test'), 1).";\n";
+
+
+echo time();
+
+exit;
