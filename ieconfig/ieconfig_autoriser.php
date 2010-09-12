cfb89440e21d9ad868808c3c300d847e299746ed
joseph@larmarange.net <joseph@larmarange.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-09-12 23:01:32 +0000

    Seuls les webmestres peuvent utiliser ieconfig.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@40705 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- ieconfig/ieconfig_autoriser.php
+++ ieconfig/ieconfig_autoriser.php
@@ -10,4 +10,9 @@ function autoriser_bando_ieconfig_bouton_dist($faire, $type, $id, $qui, $opt) {
 	return autoriser('configurer', $type, $id, $qui, $opt);
 }
 
+// Seuls les webmestres peuvent utiliser IEconfig
+function autoriser_ieconfig_configurer_dist($faire, $type, $id, $qui, $opt) {
+	return autoriser('webmestre', $type, $id, $qui, $opt);
+}
+
 ?>
