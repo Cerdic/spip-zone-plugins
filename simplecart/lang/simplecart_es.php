890c51d3865939bc258fe7e230f989b304841e53
gaitan@gmail.com <gaitan@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-09-12 21:34:45 +0000

    spanish language file
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@40700 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ simplecart/lang/simplecart_es.php
@@ -0,0 +1,71 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+    // A
+    'add_to_cart' => 'Agregar al carrito',
+
+    //B
+    'buy' => 'Comprar', 
+
+    //C
+    'cart_configuration' => 'Configuración del carrito',
+
+    'cart_headers' => 'Cabeceras del carrito',
+    'cart_headers_explication' => 'Puedes editar los campos que el carrito muestra. Lee <a href="http://simplecartjs.com/documentation.html">Cart formatting configuration page</a> para detalles',
+
+
+    'checkout' => 'Finalizar la compra',
+
+    'checkout_to' => 'Pagar a través de',
+
+    'empty' => 'Vaciar',
+
+    
+    
+    //D
+    'description' => 'Un carrito de compras simple en javascript',
+
+    //G
+    'google_merchant_id' => 'Identificador de Google Checkout (merchant id)',
+
+    //H
+    'header_name' => 'Nombre',
+    'header_price' => 'Precio',
+    'header_quantity' => 'Cantidad',
+    'header_total' => 'Total',
+
+    //O
+    'other_parameters' => 'Otros parámetros',
+
+    //P
+    'paypal_account' => 'Email de cuenta PayPal',
+    'paypal_account_explication' => 'PayPal es la opción por omisión para realizar el pago. Si tienes una cuenta PayPal, ingresa el email de tu cuenta',
+   
+
+    //S
+    'shipping_flat_rate' => 'Tasa de envío fija',
+    'shipping_flat_rate_explication' => 'Agrega una tasa fija a la orden completa',
+    'shipping_quantity_rate' => 'Tasa de envío por cantidad',
+    'shipping_quantity_rate_explication' => 'Agrega una tasa fija por cada item  en el carrito',
+    'shipping_total_rate' => 'Tasa de envío como porcentaje del total',
+    'shipping_total_rate_explication' => 'Agrega un costo de envío proporcional al costo total de la orden',
+
+    
+    //T
+    'title' => 'SimpleCart',
+
+    'items_in_cart' => 'Tienes @quantity@ productos en tu carrito', //<span class="simpleCart_quantity"></span>
+
+    //Y
+    'your_cart' => 'Tu carrito',
+    
+
+    'total_in_cart' => 'Total en el carrito: @total@',  //<div class="simpleCart_total"></div>
+
+    'tax_rate' => 'Tasa impositiva',
+    
+
+);
+?>
