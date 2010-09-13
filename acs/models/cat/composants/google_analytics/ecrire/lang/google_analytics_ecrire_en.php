bd71ff5525f7461a56ac2d870cc2b3b57cf706f5
webmaster@geomaticien.com <webmaster@geomaticien.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-09-13 03:05:22 +0000

    Mise à jour des fichiers de langue
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@40722 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- acs/models/cat/composants/google_analytics/ecrire/lang/google_analytics_ecrire_en.php
+++ acs/models/cat/composants/google_analytics/ecrire/lang/google_analytics_ecrire_en.php
@@ -3,7 +3,9 @@
 // ACS component specific - spécifique au composant ACS
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+'nom' => 'Google analytics',
 'description' => 'Google_analytics script inclusion',
+'info' => 'Cf. <a href="http://www.google.com/analytics/">www.google.com/analytics/</a>',
 'help' => '<a href="?exec=admin_vider">Empty cache</a> when activatating these component.',
 
 'UA' => 'User account (UA-xxxxxx-x)'
--- acs/models/cat/composants/google_analytics/ecrire/lang/google_analytics_ecrire_fr.php
+++ acs/models/cat/composants/google_analytics/ecrire/lang/google_analytics_ecrire_fr.php
@@ -3,7 +3,9 @@
 // ACS component specific - spécifique au composant ACS
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+'nom' => 'Google analytics',
 'description' => 'Inclusion du script de Google_analytics',
+'info' => 'Cf. <a href="http://www.google.com/analytics/">www.google.com/analytics/</a>',
 'help' => 'Il est conseillé de <a href="?exec=admin_vider">vider le cache</a> après activation de ce composant.',
 
 'UA' => 'Compte utilisateur (UA-xxxxxx-x)'
