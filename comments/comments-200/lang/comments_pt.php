1045a38916e62cdd2312d90aadb3b63c9bf972eb
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-09-14 06:09:51 +0000

    langues (comments)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@40752 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- comments/comments-200/lang/comments_pt.php
+++ comments/comments-200/lang/comments_pt.php
@@ -1,6 +1,8 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/comments/comments-200/lang/
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
