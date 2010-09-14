9968fa89785f0bc94ff0e3b8e5d143c809c237a2
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-09-14 06:14:14 +0000

    langues (spipbb)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@40764 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spipBB/dev/2_1_0/lang/spipbb_fr.php
+++ spipBB/dev/2_1_0/lang/spipbb_fr.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/spipBB/dev/lang/
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/spipBB/dev/2_1_0/lang/
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
