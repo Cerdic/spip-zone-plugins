928b47429034e680c38e6b9fec8452d9decd2eea
paladin@quesaco.org <paladin@quesaco.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-09-20 07:39:48 +0000

    descripteur du plugin et fichier de lang
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@40911 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ get_info_spip/lang/gins_fr.php
@@ -0,0 +1,42 @@
+<?php
+
+	// $LastChangedRevision$
+	// $LastChangedBy$
+	// $LastChangedDate$
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	'gins' => 'Info configuration SPIP',
+	'informations' => 'Informations',
+	'info_gauche_info_spip' => '<strong>Cette page est uniquement accessible
+		aux responsables du site.</strong>
+		<p> Elle donne acc&egrave;s aux diff&eacute;rentes
+		informations de configuration du site SPIP.</p>',
+	'jquery_inactif' => 'jQuery non d&#233;tect&#233;. Merci de l&#8217;activer.',
+	
+	'menu_proprietes' => 'Menu des propri&#233;t&#233;s',
+
+	'configspip' => 'Infos SPIP',
+	'configsyst' => 'Infos Syst&#232;me',
+	'phpconfig' => 'Configuration PHP',
+	
+	'constantes' => 'Constantes',
+	
+	'SPIP_version_code_revision_base' => 'SPIP version (code) [revision] /base/',
+	'SPIP_plugins_actifs' => 'Plugins actifs',
+	'SPIP_plugins_librairies' => 'Plugins librairies',
+	'SPIP_nombre_auteurs_vali/tous' => 'Nombre d&#39;auteurs valid&#233;s/tous',
+	'SPIP_nombre_articles_publ/tous' => 'Nombre d&#39;articles publi&#233;s/tous',
+	'SPIP_nombre_rubriques_publ/tous' => 'Nombre de rubriques publi&#233;es/toutes',
+	'SPIP_nombre_forums_publ/tous' => 'Nombre de forums publi&#233;s/tous',
+	'SPIP_nombre_documents_publ/tous' => 'Nombre de documents publi&#233;s/tous',
+	
+	'B' => 'octets',
+	'KB' => 'Ko.',
+	'MB' => 'Mo.',
+	'GB' => 'Go.',
+	'TB' => 'To.',
+
+);
