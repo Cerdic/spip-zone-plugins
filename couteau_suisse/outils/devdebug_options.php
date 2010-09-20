c1584f5c8918a19a2a6d02c1ac29761a7e33487e
piero.wbmstr@gmail.com <piero.wbmstr@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-09-20 15:55:28 +0000

    Nouvelle lame (les fichiers cette fois) : un utilitaire de dev qui permet de gérer le renvoi des erreurs PHP à l'écran selon l'espace de SPIP voulu (vu avec Patrice et Maïeul)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@40922 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ couteau_suisse/outils/devdebug_options.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * @name 		DevelopmentDebugger
+ * @author 		Piero Wbmstr <piero.wbmstr@gmail.com>
+ * @link 		http://www.spip-contrib.net/?article3572
+ */
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+/**
+ * On charge la config de l'outil et le valeurs d'erreurs renvoyees
+ */
+function devdebug_charger_debug(){
+	// On renvoie direct si pas defini
+	if(!defined('_DEVDEBUG_MODE')) return;
+	// Sinon, on traite
+	$prive = test_espace_prive() ? 'prive':'public';
+	if(_DEVDEBUG_MODE==1){
+		// Les liens d'erreur gŽnŽrŽs par PHP renvoient ... en local ! dans le php.ini standard
+		// On les dŽfinit du type 'http://fr.php.net/manual/en/ %s .php' (nŽcessite une connexion)
+		@ini_set('docref_root', "http://fr.php.net/manual/en/");
+		@ini_set('docref_ext', '.php');
+		// On lance le php error tracking quoiqu'il arrive
+		@ini_set('track_errors',1);
+		// On Žvite d'afficher les erreurs rŽpŽtŽes
+		@ini_set('ignore_repeated_errors',1);
+		// Compatibilite PHP (recup de 'inc_version', mais qui le dŽfinit trop tard)
+		@define('E_DEPRECATED',8192); // PHP 5.3
+		// Et let's go
+		$niveau = 'E_WARNING';
+		if(defined('_DEVDEBUG_NIVEAU')) switch(_DEVDEBUG_NIVEAU) {
+			case 'warning' : $niveau = "E_ALL ^ E_NOTICE"; break;
+			case 'error' : $niveau = "E_ALL ^ (E_NOTICE | E_WARNING)"; break;
+			case 'strict' : $niveau = "-1"; break;
+			case 'all' : $niveau = "E_ALL |ÊE_DEPRECATED"; break;
+			case 'user' : $niveau = "E_USER_NOTICE |ÊE_USER_WARNING | E_USER_ERROR"; break;
+			case 'notice' : default : $niveau = "E_ALL"; break;
+		}
+		if(defined('_DEVDEBUG_ESPACE')) switch(_DEVDEBUG_ESPACE) {
+			case 'public' :
+				if($prive =='public') {
+					ini_set('display_errors',1); 
+					eval("error_reporting($niveau);");
+				}
+				else ini_set('display_errors',0);
+				break;
+			case 'prive' :
+				if($prive =='prive') {
+					ini_set('display_errors',1); 
+					eval("error_reporting($niveau);");
+				}
+				else ini_set('display_errors',0);
+				break;
+			default :
+				ini_set('display_errors',1); 
+				eval("error_reporting($niveau);");
+		}
+	}
+	elseif(_DEVDEBUG_MODE==0) ini_set('display_errors',0);
+}
+
+/**
+ * Page de reglages accessible tout le temps par le webmestre : 'ecrire/?exec=debug'
+ * En cas de probleme
+ * => on detoure le CS : 
+ *		=> "redirect" enleve
+ *		=> ajout d'un input "exec" hidden
+ * (je sais c'est mal!)
+ */
+function exec_debug() {
+	ini_set('display_errors','1'); error_reporting(E_ALL ^ (E_NOTICE | E_WARNING));
+	include_spip('inc/minipres'); 
+	global $connect_statut;
+	if ($connect_statut != "0minirezo" || !autoriser('configurer', 'configuration')){ echo minipres(); exit;}
+	include_spip('inc/cs_outils'); 
+	$content = description_outil2('devdebug');
+	$content = str_replace(" name='redirect'", " name='abcdef'", $content);
+	$content = str_replace("?exec=devdebugger", '', $content);
+	echo minipres(' ',str_replace("<input type='hidden' name='action' value='description_outil' />", "<input type='hidden' name='action' value='description_outil' /><input type='hidden' name='exec' value='devdebugger' />", $content));
+	exit;  
+}
+
+?>
