7a5d78d9149b50bffc5ce536161a800407042fe6
cyril@cym.fr <cyril@cym.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-09-21 13:25:15 +0000

    travail (toujours pas opérationnel) sur bloc ajout mot clé;
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@40964 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- mots_auteurs/mots_auteurs_pipelines.php
+++ mots_auteurs/mots_auteurs_pipelines.php
@@ -13,14 +13,23 @@
  * sur la page de visualisation d'un auteur
 **/
 function mots_auteurs_affiche_milieu($flux) {
+
+	// si on est sur une page d'info d'un auteur
 	if ($flux['args']['exec'] == 'auteur_infos') {
+	
+		// on récupère l'auteur en cours, et si on le récupère correctement...
 		if ($id_auteur = $flux['args']['id_auteur']) {
 			$contexte = array(
 				'objet' => 'auteur',
 				'id_objet' => $id_auteur
 			);
+			
+			// ...on ajoute la boite de séletion des mots-clé dans le flux html
+			$editer_mots = charger_fonction('editer_mots', 'inc');
+			$flux['data'] .= '<h3>test (debut)</h3>'; 
+			$flux['data'] .= $editer_mots('auteur', $id_auteur, $cherche_mot, $select_groupe, $flag_editable, false, 'auteurs');
+			$flux['data'] .= '<h3>test (fin)</h3>';
 
-			$flux = $editer_mots('article', $id_article, $cherche_mot, $select_groupe, $flag_editable, false, 'auteurs'));
 		}
 	}
 	return $flux;
