a6de0e1a85ce310d7c9dced754b3e66592ce7afe
cyril@cym.fr <cyril@cym.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-09-21 11:59:51 +0000

    travail sur le bloc dépliant dans la page auteurs;
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@40959 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ mots_auteurs/lang/mots_auteurs_fr.php
@@ -0,0 +1,13 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	// L
+	'liste_mots' => 'Liste des mots cl&eacute; attribu&eacute;s &agrave; cet auteur.',
+
+	// T
+	'titre_mots' => 'Mots cl&eacute;',
+);
+
+?>
