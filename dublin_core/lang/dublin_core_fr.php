790b15ab2799198f836d63d6a6c2066832ef3206
joseph@larmarange.net <joseph@larmarange.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-09-22 10:35:18 +0000

    tri alphabétique des chaines de langue
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@40980 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- dublin_core/lang/dublin_core_fr.php
+++ dublin_core/lang/dublin_core_fr.php
@@ -4,26 +4,25 @@
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-	
-	'dublin_core' => 'Dublin Core',
 	'configurer_dublin_core' => 'Configurer les champs Dublin Core des articles',
-	'label_dc_creator' => 'Auteurs (DC.Creator)',
+	'dublin_core' => 'Dublin Core',
 	'item_auteurs' => 'Auteurs de l\'article',
-	'item_nom_site_spip' => 'Nom du site SPIP',
-	'item_surtitre' => 'Sur-titre de l\'article',
-	'item_soustitre' => 'Sous-titre de l\'article',
-	'label_issn' => 'ISSN (optionnel)',
-	'label_dc_subject' => 'Sujet ou mots-cl&eacute;s (DC.Subject)',
-	'item_motscles' => 'Tous les mots-cl&eacute;s de l\'article',
-	'item_groupe_mot' => 'Mots-cl&eacute;s d\'un groupe sp&eacute;cifique',
 	'item_chapo' => 'Chapo de l\'article',
-	'label_dc_subject_groupe_mot' => 'Si mots-cl&eacute;s d\'un groupe sp&eacute;cifique, lequel&nbsp;?',
-	'label_dc_description_abstract' => 'R&eacute;sum&eacute; (DC.Description .Abstract)',
 	'item_descriptif' => 'Descriptif de l\'article',
+	'item_groupe_mot' => 'Mots-cl&eacute;s d\'un groupe sp&eacute;cifique',
 	'item_introduction' => 'Introduction de l\'article',
+	'item_motscles' => 'Tous les mots-cl&eacute;s de l\'article',
+	'item_nom_site_spip' => 'Nom du site SPIP',
+	'item_soustitre' => 'Sous-titre de l\'article',
+	'item_surtitre' => 'Sur-titre de l\'article',
+	'label_dc_creator' => 'Auteurs (DC.Creator)',
+	'label_dc_description_abstract' => 'R&eacute;sum&eacute; (DC.Description .Abstract)',
 	'label_dc_rights_license' => 'URL Licence (DC.Rights.License) (optionnel)',
 	'label_dc_rights_license_details' => 'URL d&eacute;crivant la licence s\'appliquant &agrave; l\'article. Si vous activez le plugin Licence, la licence attribu&eacute;e &agrave; un article sera prioritaire sur la valeur saisie ici.',
 	'label_dc_rights_rightsholder' => 'Propri&eacute;taire des droits (DC.Rights.RightsHolder)',
+	'label_dc_subject' => 'Sujet ou mots-cl&eacute;s (DC.Subject)',
+	'label_dc_subject_groupe_mot' => 'Si mots-cl&eacute;s d\'un groupe sp&eacute;cifique, lequel&nbsp;?',
+	'label_issn' => 'ISSN (optionnel)',
 );
 
 ?>
