8486a895cc6925ba2ce0c02fbe0a24280c36c304
joseph@larmarange.net <joseph@larmarange.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-09-22 18:29:37 +0000

    Utilisation d'un pipeline ieconfig_metas à la place d'une globale
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@41018 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- refbase/refbase_options.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Support d'ieconfig
-
-$GLOBALS['ieconfig_metas']['refbase']['titre'] = 'Refbase';
-$GLOBALS['ieconfig_metas']['refbase']['icone'] = 'img_pack/refbase-24.png';
-$GLOBALS['ieconfig_metas']['refbase']['metas_serialize'] = 'refbase';
-
-?>
