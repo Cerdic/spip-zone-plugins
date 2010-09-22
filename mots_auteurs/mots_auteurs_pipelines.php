eed1f2dd0f1bc5520d8563e2c6ddaed5c37c34b8
marcimat@rezo.net <marcimat@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-09-22 16:57:45 +0000

    On commence à rendre les fonctions un peu plus génériques... Pour pouvoir plus tard faire ailleurs que sur les auteurs.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@41006 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- mots_auteurs/mots_auteurs_pipelines.php
+++ mots_auteurs/mots_auteurs_pipelines.php
@@ -15,24 +15,72 @@
 **/
 function mots_auteurs_affiche_milieu($flux) {
 
-	// si on est sur une page d'info d'un auteur
-	if ($flux['args']['exec'] == 'auteur_infos') {
+	static $ou = array(
+		'auteur_infos' => array(
+			'objet' => 'auteur',
+		)
+	);
 	
-		// on récupère l'auteur en cours, et si on le récupère correctement...
-		if ($id_auteur = $flux['args']['id_auteur']) {
-			$contexte = array(
-				'objet' => 'auteur',
-				'id_objet' => $id_auteur
-			);
-			
-			// ...on ajoute la boite de séletion des mots-clé dans le flux html
-			$flag_editable = autoriser('modifier', 'auteur', $id_auteur);
-			$editer_mots = charger_fonction('editer_mots', 'inc');
-			$flux['data'] .= $editer_mots('auteur', $id_auteur, $cherche_mot, $select_groupe, $flag_editable, false, 'auteurs');
+	// si on est sur une page ou il faut inserer les mots cles...
+	if (in_array($flux['args']['exec'], array_keys($ou))) {
 
+		$me = $ou[ $flux['args']['exec'] ];
+		$objet = $me['objet']; // auteur
+		$_id_objet = id_table_objet($objet); // id_auteur
+		
+		// on récupère l'identifiant de l'objet...
+		if ($id_objet = $flux['args'][ $_id_objet ]) {
+			$flux['data'] .= mots_objets_ajouter_selecteur_mots($objet, $id_objet, array(
+				'exec_url' => $flux['args']['exec']
+			));
+			
 		}
 	}
 	return $flux;
+}
+
+
+
+/**
+ * Retourne le selecteur de mots pour un objet donnee
+ *
+ * @param string $objet : nom de l'objet
+ * @param int $id_objet : identifiant de l'objet
+ * @param array $opt options
+ * 		@param string $cherche_mot	un mot cherché particulier
+ * 		@param string $select_groupe	un/des groupe particulier ?
+ * 		@param bool $editable	autorisé ou non à voir ajouter des mots
+ * 		@param bool 
+ * 		@param string $exec_url	url de exec de retour.
+ * 
+ * @return string	HTML produit.
+**/
+function mots_objets_ajouter_selecteur_mots($objet, $id_objet, $opt = array()) {
+
+	if (!isset($opt['flag_editable'])) {
+		$opt['flag_editable'] = autoriser('modifier', $objet, $id_objet);
+	}
+	// pas beau !
+	if (!isset($opt['cherche_mot'])) {
+		$opt['cherche_mot'] = _request('cherche_mot');
+	}
+	// pas beau !
+	if (!isset($opt['select_groupe'])) {
+		$opt['select_groupe'] = _request('select_groupe');
+	}
+	// pas beau !
+	if (!isset($opt['exec_url'])) {
+		$opt['exec_url'] = '';
+	}
+	
+	$editer_mots = charger_fonction('editer_mots', 'inc');
+
+	return $editer_mots(
+		$objet, $id_objet,
+		$opt['cherche_mot'], $opt['select_groupe'],
+		$opt['flag_editable'], false, $opt['exec_url']
+	);
+
 }
 
 
@@ -45,14 +93,26 @@ function mots_auteurs_libelle_association_mots($flux){
 
 // Ajout du contenu aux formulaires CVT du core.
 function mots_auteurs_editer_contenu_objet($flux){
+
+	$liste = array(
+		'auteurs' => _T('mots_auteurs:item_mots_cles_association_auteurs'),
+	);
+		
 	// Concernant le formulaire CVT 'editer_groupe_mot', on veut faire apparaitre l'objet auteurs
 	if ($flux['args']['type']=='groupe_mot') {
 		// Si le formulaire concerne les groupes de mots-cles, alors recupere le resultat
-		// de la compilation du squelette 'inc-groupe-mot-mots_auteurs.html' qui contient les lignes
+		// de la compilation du squelette 'inc-groupe-mot-mots_objets.html' qui contient les lignes
 		// a ajouter au formulaire CVT,
-		$mots_auteurs_gp_objets = recuperer_fond('formulaires/inc-groupe-mot-mots_auteurs', $flux['args']['contexte']);
+		$mots_objets_checkbox = '';
+		foreach ($liste as $table_objet=>$nom) {
+			$mots_objets_checkbox .=
+				recuperer_fond('formulaires/inc-groupe-mot-mots_objets', array_merge($flux['args']['contexte'], array(
+					'table' => $table_objet,
+					'label' => $nom,
+				)));
+		}
 		// que l'on insere ensuite a l'endroit approprie, a savoir avant le texte <!--choix_tables--> du formulaire
-		$flux['data'] = preg_replace('%(<!--choix_tables-->)%is', $mots_auteurs_gp_objets."\n".'$1', $flux['data']);
+		$flux['data'] = preg_replace('%(<!--choix_tables-->)%is', $mots_objets_checkbox."\n".'$1', $flux['data']);
 	}
 	return $flux;
 }
