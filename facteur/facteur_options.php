aa5d39b15186807e21c94badf09462b99dce828e
joseph@larmarange.net <joseph@larmarange.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-09-22 18:28:53 +0000

    Utilisation d'un pipeline ieconfig_metas à la place d'une globale
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@41016 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- facteur/facteur_options.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Support d'ieconfig
-
-$GLOBALS['ieconfig_metas']['facteur']['titre'] = _T('facteur:configuration_facteur');
-$GLOBALS['ieconfig_metas']['facteur']['icone'] = 'prive/images/facteur-24.png';
-$GLOBALS['ieconfig_metas']['facteur']['metas_brutes'] = 'facteur_adresse_envoi,facteur_adresse_envoi_nom,facteur_adresse_envoi_email,facteur_smtp,facteur_smtp_host,facteur_smtp_port,facteur_smtp_auth,facteur_smtp_username,facteur_smtp_password,facteur_smtp_secure,facteur_smtp_sender,facteur_filtre_images,facteur_filtre_css,facteur_filtre_iso_8859';
-
-?>
