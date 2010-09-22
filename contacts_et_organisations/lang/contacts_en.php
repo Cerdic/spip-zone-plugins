fb68cc341d642da6caa4b7b2efbfc1c429024b67
cyril@cym.fr <cyril@cym.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-09-22 15:16:14 +0000

    On ajoute les traductions en anglais; on améliore la page de tests;
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@40995 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ contacts_et_organisations/lang/contacts_en.php
@@ -0,0 +1,79 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	// B
+	'bouton_rechercher' => "Search",
+	'bouton_contacts' => 'Contacts',
+	'bouton_organisations' => 'Organisations',
+	
+	// C
+	'contacts' => 'Contacts',
+	'confirmer_supprimer_contact' => "Ares you sure you want to delete all information about this contact ?",
+	'confirmer_supprimer_organisation' => "Are you sure you want to delete all the information about this organisation ?",
+	'confirmer_delier_organisation' => "Are you sure you want to unlink this contact from this organisation ?",
+	'confirmer_delier_contact' => "Are you sure you want to unlink this organisation from this contact ?",
+	
+	// D
+	'definir_auteur_comme_contact' => "Set as contact",
+	'definir_auteur_comme_organisation' => "Set as organisation",
+
+	// E
+	'est_un_contact' => "This author is set as a contact.",
+	'est_une_organisation' => "This author is set as an organisation.",
+	'explication_statut_juridique' => "company, organisation, ...",
+	'explication_activite' => 'Activiity of the organisation : NGO, education, edition...',
+	'explication_contacts_ou_organisations' => "You can set this author as a contact or as an organisation.Vous pouvez définir cet auteur
+		Additional fields can be then lieeed from the author modification page.",
+	'explication_supprimer_contact' => "Deleting this contact will suppress all the additional information which has been filed on the author page.",
+	'explication_supprimer_organisation' => "Deleting this organisation will suppress all the additional information which has been filed on the author page.",
+	'explication_identification' => 'Organisation identification : VAT, etc.',
+	'explications_page_contacts' => 'Page en cours de d&eacute;veloppement. <br /><br />Actions envisag&eacute;es :<ul>
+	<li>voir tous les contacts</li><li>transformer les auteurs en contacts</li><li>importer des contacts</li><li>...</li></ul><br />Merci pour vos suggestions sur <a href="http://www.spip-contrib.net/Plugin-Contacts-Organisations#pagination_comments-list">le forum</a> ;-)',
+	'explications_page_organisations' => 'Page en cours de d&eacute;veloppement. <br /><br />Actions envisag&eacute;es :<ul>
+	<li>voir toutes les organisations</li><li>transformer des auteurs en organisations</li><li>importer des organsations</li><li>...</li></ul><br />Merci pour vos suggestions sur <a href="http://www.spip-contrib.net/Plugin-Contacts-Organisations#pagination_comments-list">le forum</a> ;-)',
+
+	// I
+	'infos_contacts_ou_organisations' => "Contacts & Organisations",
+	'info_tous_contacts' => "All the contacts",
+	'info_toutes_organisations' => "All the organisations",
+	'info_organisation_appartenance' => "Belonging Organisation",
+	'info_contacts_organisation' => "Organisation's contacts",
+	
+	// L
+	'label_activite' => "Activity",
+	'label_civilite' => "Gender",
+	'label_fonction' => "Function",
+	'label_nom' => "Name",
+	'label_prenom' => "First name",
+	'label_date_naissance' => "Date of birth",
+	'label_date_creation' => "Date of creation",
+	'label_descriptif' => "Description",
+	'label_identification' => "Identification",
+	'label_statut_juridique' => "Status",
+	'label_recherche_contacts' => "Search in contacts",
+	'label_recherche_organisations' => "Search in organisations",
+	'lier_contact' => "Attach a contact",
+	'lier_organisation' => "Attach to an organisation",
+	'lier_ce_contact' => "Attach this contact",
+	'lier_cette_organisation' => "Attach to this organisation",
+	'liste_contacts' => 'List of all contacts in the database',
+	'liste_organisations' => 'List of all organisations in the database',
+
+	// O
+	'organisations' => 'Organisations',
+		
+	// S
+	'supprimer_contact' => "Delete this contact",
+	'supprimer_organisation' => "Delete this organisation",
+
+	// T
+	'titre_contact' => "Contact details",
+	'titre_organisation' => "Organisation details",
+	'titre_page_contacts' => 'Contacts management',
+	'titre_page_organisations' => 'Organisations management',
+	
+);
+
+?>
