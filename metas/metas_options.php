7341b661818d5b6893b571b7b93ffca39e267b72
joseph@larmarange.net <joseph@larmarange.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-09-22 18:30:16 +0000

    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@41020 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- metas/metas_options.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Support d'ieconfig
-
-$GLOBALS['ieconfig_metas']['metas']['titre'] = _T('metas:configuration_metas');
-$GLOBALS['ieconfig_metas']['metas']['icone'] = 'images/metas-24.png';
-$GLOBALS['ieconfig_metas']['metas']['metas_brutes'] = 'spip_metas_title,spip_metas_description,spip_metas_keywords,spip_metas_mots_importants';
-
-?>
