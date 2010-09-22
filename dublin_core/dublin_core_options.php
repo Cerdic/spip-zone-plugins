217bfbccb26b9a502cccd1046add29c87ff261bb
joseph@larmarange.net <joseph@larmarange.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-09-22 17:46:13 +0000

    Pouvoir exporter la config avec IEconfig
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@41013 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ dublin_core/dublin_core_options.php
@@ -0,0 +1,9 @@
+<?php
+
+// Support d'ieconfig
+
+$GLOBALS['ieconfig_metas']['dublin_core']['titre'] = _T('dublin_core:dublin_core');
+$GLOBALS['ieconfig_metas']['dublin_core']['icone'] = 'images/dublin_core-24.png';
+$GLOBALS['ieconfig_metas']['dublin_core']['metas_serialize'] = 'dublin_core';
+
+?>
