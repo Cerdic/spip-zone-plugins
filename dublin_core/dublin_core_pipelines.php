630dbaf94fc0a577abdae80d520e4d861889fc6d
joseph@larmarange.net <joseph@larmarange.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-09-22 11:44:36 +0000

    Il faut faire l'insertion y compris dans un contexte non Zpip
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@40981 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- dublin_core/dublin_core_pipelines.php
+++ dublin_core/dublin_core_pipelines.php
@@ -1,9 +1,16 @@
 <?php
 function dublin_core_insert_head($flux){
-	if ($GLOBALS['page']['contexte']['type'] == 'article'){
-		$flux .= recuperer_fond('dublin_core_article', array('id_article'=>$GLOBALS['page']['contexte']['id_article']));
-		# interpreter le squelette spip en lui passant les bonnes informations
-	} # si on est dans une page article.html
+	// Si Zpip, on connait le type de page
+	 if (defined('_DIR_PLUGIN_Z')) {
+		if ($GLOBALS['page']['contexte']['type'] == 'article'){
+			$flux .= recuperer_fond('dublin_core_article', array('id_article'=>$GLOBALS['page']['contexte']['id_article']));
+		}
+	// Sinon, on regarde juste si on dispose d'un id_article dans l'environnement
+	} else {
+		if ($GLOBALS['page']['contexte']['id_article']>0){
+			$flux .= recuperer_fond('dublin_core_article', array('id_article'=>$GLOBALS['page']['contexte']['id_article']));
+		}
+	}
 	return $flux;
 }
 
