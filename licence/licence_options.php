6e76f7ed8e7a0c9ee75bfec0da27ee6e460ee8d0
joseph@larmarange.net <joseph@larmarange.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-09-22 17:51:53 +0000

    Pouvoir exporter la config avec IEconfig
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@41014 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ licence/licence_options.php
@@ -0,0 +1,9 @@
+<?php
+
+// Support d'ieconfig
+
+$GLOBALS['ieconfig_metas']['licence']['titre'] = _T('licence:cfg_titre_licence');
+$GLOBALS['ieconfig_metas']['licence']['icone'] = 'img_pack/licence_logo24.png';
+$GLOBALS['ieconfig_metas']['licence']['metas_serialize'] = 'dublin_core';
+
+?>
