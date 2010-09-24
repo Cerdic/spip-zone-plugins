4db023ee08f481aca99051bfdea865bb6f734c09
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-09-24 22:40:30 +0000

    une fonction pour migrer un utilisateur. Il faudra l'appeler lorsqu'on migrera les articles.
    En attendant il va falloir s'occuper de la migration des rubriques/category
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@41110 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- dotclear2spip/dot2_fonctions.php
+++ dotclear2spip/dot2_fonctions.php
@@ -1,4 +1,18 @@
 <?php
+function dot2_migrer_utilisateur($user_id){
+	$users = sql_select('user_super,user_url,user_name,user_firstname,user_displayname','dc_user',"`user_id`=".sql_quote($user_id));
+	
+	while($user = sql_fetch($users)){
+		// ! \\ SPIP ne fonctionne pas comme Doctclear au niveau des noms donc on va essayer de convertir
+		if ($user['user_displayname']){
+			$nom	= $user['user_displayname'];
+		}
+		else{
+			$nom	= $user['user_firstname'].' '.$user['user_name'];
+		}
+		print $nom;
+	}
+}
 
 function dot2_migrer_mots_article($id_post,$id_article,$id_groupe=''){
 	$crud = charger_fonction('crud','action');
@@ -15,7 +29,7 @@ function dot2_migrer_mots_article($id_post,$id_article,$id_groupe=''){
 	while ($tag = sql_fetch($tags_post)){
 		#si on a déja créé ce mot
 		$titre = $tag['meta_id'];
-		if ($id_mot = sql_getfetsel('id_mot','spip_mots',array('`titre`=\''.$titre.'\'','id_groupe='.$id_groupe))){
+		if ($id_mot = sql_getfetsel('id_mot','spip_mots',array('`titre`='.sql_quote($titre),'id_groupe='.$id_groupe))){
 			sql_insertq('spip_mots_articles',array('id_article'=>$id_article,'id_mot'=>$id_mot));
 			spip_log("Lier le mot $id_mot à l'article $id_article","dot2");	
 		}
@@ -34,7 +48,7 @@ function dot2_migrer_mots_article($id_post,$id_article,$id_groupe=''){
 
 function dot2_migrer_sites($blog_id,$id_rubrique){
 	$crud = charger_fonction('crud','action');
-	$dc_link = sql_select('link_title,link_href,link_position','dc_link',array("`blog_id`='".$blog_id."'"));
+	$dc_link = sql_select('link_title,link_href,link_position','dc_link',array("`blog_id`=".sql_quote($blog_id)));
 	
 	while($site = sql_fetch($dc_link)){
 		$nom_site = $site['link_position']."0. ".$site['link_title'];
