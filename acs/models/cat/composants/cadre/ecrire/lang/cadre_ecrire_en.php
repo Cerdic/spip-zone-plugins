047748896f34b72fe73825dbc5f04b1f38cf4539
webmaster@geomaticien.com <webmaster@geomaticien.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-09-24 19:59:38 +0000

    Module devient Cadre
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@41093 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ acs/models/cat/composants/cadre/ecrire/lang/cadre_ecrire_en.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP-ACS language file  --  Ceci est un fichier langue de SPIP-ACS
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+'nom' => 'Cadre',
+'description' => 'Box and container for other components.',
+'help' => 'Cadre define a box AND contain other components.',
+
+'marge' => 'Margin',
+'orientation' => 'Orientation',
+'vertical' => 'vertical',
+'horizontal' => 'horizontal',
+);
+?>
---
+++ acs/models/cat/composants/cadre/ecrire/lang/cadre_ecrire_fr.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP-ACS language file  --  Ceci est un fichier langue de SPIP-ACS
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+'nom' => 'Cadre',
+'description' => 'Cadre et conteneur pour d\'autres composants.',
+'help' => 'Cadre permet de définir des propriétés pour un cadre et d\'y glisser d\'autres composants.',
+
+'marge' => 'Marge sur le bord',
+'Orientation' => 'Orientation',
+'OrientationVertical' => 'verticale',
+'OrientationHorizontal' => 'horizontale',
+);
+?>
