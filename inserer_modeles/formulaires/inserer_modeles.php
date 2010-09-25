179f3301c8902daec16144df911b994c1c2f26dd
joseph@larmarange.net <joseph@larmarange.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-09-25 15:01:07 +0000

    Quelques amélioriations d'interfaces, corrections du formulaire et ajout d'un sélecteur de document
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@41158 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- inserer_modeles/formulaires/inserer_modeles.php
+++ inserer_modeles/formulaires/inserer_modeles.php
@@ -1,6 +1,6 @@
 <?php
 
-function formulaires_inserer_modeles_charger_dist() {
+function formulaires_inserer_modeles_charger_dist($id_article,$id_rubrique,$id_breve) {
 	include_spip('inc/inserer_modeles');
 	$contexte = array();
 	
@@ -8,6 +8,8 @@ function formulaires_inserer_modeles_charger_dist() {
 		$contexte['_liste_formulaires_modeles'] = inserer_modeles_lister_formulaires_modeles();
 	} else {
 		$infos_modele = charger_infos_formulaire_modele(_request('formulaire_modele'));
+		include_spip('inc/saisies');
+		$contexte = saisies_charger_champs($infos_modele['parametres']);
 		$contexte['formulaire_modele'] = _request('formulaire_modele');
 		$contexte['nom'] = _T_ou_typo($infos_modele['nom']);
 		$contexte['logo'] = $infos_modele['logo'];
@@ -15,6 +17,13 @@ function formulaires_inserer_modeles_charger_dist() {
 		if (_request('code_modele'))
 			$contexte['code_modele'] = _request('code_modele');
 	}
+	
+	if (is_numeric($id_article))
+		$contexte['id_article'] = $id_article;
+	if (is_numeric($id_rubrique))
+		$contexte['id_rubrique'] = $id_rubrique;
+	if (is_numeric($id_breve))
+		$contexte['id_breve'] = $id_breve;
 	
 	return $contexte;
 }
@@ -42,11 +51,13 @@ function formulaires_inserer_modeles_traiter_dist() {
 		$infos = charger_infos_formulaire_modele(_request('formulaire_modele'));
 		$champs = saisies_lister_champs($infos['parametres'],false);
 		$code = '<'._request('modele');
-		$code .= _request('id_modele') ? _request('id_modele') : '';
-		$code .= _request('classe') ? '|'._request('classe') : '';
+		if (_request('id_modele') && _request('id_modele')!='')
+			$code .= _request('id_modele');
+		if (_request('classe') && _request('classe')!='')
+			$code .= '|'._request('classe');
 		foreach ($champs as $champ)
-			if($champ != 'modele' && $champ != 'classe' && $champ != 'id_modele')
-				$code .= _request($champ) ? "|$champ="._request($champ) : '';
+			if($champ != 'modele' && $champ != 'classe' && $champ != 'id_modele' && _request($champ) && _request($champ)!='')
+				$code .= "|$champ="._request($champ);
 		$code .= '>';
 		set_request('code_modele',$code);
 		return array('message_ok' => _T('inserer_modeles:message_copier_code'));
--- inserer_modeles/lang/inserer_modeles_fr.php
+++ inserer_modeles/lang/inserer_modeles_fr.php
@@ -1,15 +1,33 @@
 <?php
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-	'nom_formulaire' => 'un formulaire',
+	
 	'titre_inserer_modeles' => 'Ins&eacute;rer un mod&egrave;le',
 	'choisir_modele' => 'Que souhaitez-vous ins&eacute;rer&nbsp;?',
 	'bouton_choisir' => 'Choisir',
 	'bouton_inserer' => 'Ins&eacute;rer',
-	'label_formulaire_formidable' => 'Quel formulaire&nbsp;?',
+	'item_center' => 'au centre',
+	'item_left' => '&agrave; gauche',
+	'item_right' => '&agrave; droite',
 	'titre_inserer' => 'Ins&eacute;rer @modele@',
 	'erreur_choix_modele' => 'Vous devez choisir un mod&egrave;le.',
+	'label_alignement' => 'Alignement&nbsp;:',
 	'message_copier_code' => 'Vous pouvez recopier le code ci-dessous dans votre texte ou modifier les param&egrave;tres de votre mod&egrave;le.',
+	
+	// A déplacer dans formidable
+	'nom_formulaire' => 'un formulaire',
+	'label_formulaire_formidable' => 'Quel formulaire&nbsp;?',
+	
+	// A déplacer dans mediatheque
+	'nom_doc' => 'la vignette d\'un document',
+	'nom_img' => 'une image',
+	'label_id_document' => 'Document num&eacute;ro&nbsp;:',
+	'label_id_image' => 'Image num&eacute;ro&nbsp;:',
+	'bouton_parcourir_docs_article' => 'Parcourir l\'article',
+	'bouton_parcourir_docs_breve' => 'Parcourir la br&egrave;ve',
+	'bouton_parcourir_docs_rubrique' => 'Parcourir la rubrique',
+	'bouton_parcourir_mediatheque' => 'Parcourir la m&eacute;diath&egrave;que',
+	
 );
 
 ?>
