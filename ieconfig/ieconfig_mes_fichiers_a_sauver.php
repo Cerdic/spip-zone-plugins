30a5ab181046f9a90080e31f3fc60de59e5282b2
joseph@larmarange.net <joseph@larmarange.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-09-25 19:58:15 +0000

    Compatibilité avec le plugin mes_fichiers
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@41188 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ ieconfig/ieconfig_mes_fichiers_a_sauver.php
@@ -0,0 +1,11 @@
+<?php
+
+function saveauto_mes_fichiers_a_sauver($flux){
+	$tmp_ieconfig = defined('_DIR_TMP') ? _DIR_TMP.'ieconfig/': _DIR_RACINE.'tmp/ieconfig/';
+	if (@is_dir($tmp_ieconfig))
+		$flux[] = $tmp_ieconfig;
+	
+	return $flux;
+}
+
+?>
