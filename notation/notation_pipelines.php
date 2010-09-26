ef797941f19a1a2e9c8da374af1f8c63a9ad2bd2
booz@rezo.net <booz@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-09-26 15:20:37 +0000

    utiliser find_in_path pour permettre les surcharges des fichiers
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@41243 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- notation/notation_pipelines.php
+++ notation/notation_pipelines.php
@@ -6,7 +6,7 @@ function notation_header_prive($flux){
 }
 
 function notation_insert_head($flux){
-	$flux .= '<link rel="stylesheet" href="'._DIR_PLUGIN_NOTATION.'css/notation.v2.css" type="text/css" media="all" />';
+	$flux .= '<link rel="stylesheet" href="'.find_in_path('css/notation.v2.css').'" type="text/css" media="all" />';
 	return $flux;
 }
 
@@ -14,9 +14,9 @@ function notation_affichage_final($flux){
     if (strpos($flux, "'notation_note notation_note_on_load'") === false)
 		return $flux;
 	$incHead = "";
-	$incHead .= "<script src='"._DIR_PLUGIN_NOTATION."javascript/jquery.MetaData.js' type='text/javascript'></script>\n";
-	$incHead .= "<script src='"._DIR_PLUGIN_NOTATION."javascript/jquery.rating.js' type='text/javascript'></script>\n";
-	$incHead .= "<script src='"._DIR_PLUGIN_NOTATION."javascript/notation.js' type='text/javascript'></script>\n";
+	$incHead .= "<script src='".find_in_path('javascript/jquery.MetaData.js')." type='text/javascript'></script>\n";
+	$incHead .= "<script src='".find_in_path('javascript/jquery.rating.js')." type='text/javascript'></script>\n";
+	$incHead .= "<script src='".find_in_path('javascript/notation.js')." type='text/javascript'></script>\n";
 	include_spip('inc/filtres');
 	if(function_exists('compacte_head')){
 		$incHead = compacte_head($incHead);
