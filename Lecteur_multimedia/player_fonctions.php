af312c554b44deeb8a3f3111e69135216b764860
prigent.yohann@gmail.com <prigent.yohann@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-09-27 22:54:23 +0000

    Supporter le RTL avec un petit hack
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@41304 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- Lecteur_multimedia/player_fonctions.php
+++ Lecteur_multimedia/player_fonctions.php
@@ -33,7 +33,7 @@ function Player_call_js() {
 }
 
 function Player_call_css() {
-	$flux = "\n".'<link rel="stylesheet" href="'.find_in_path('player.css').'" type="text/css" media="all" />';
+	$flux = "\n".'<link rel="stylesheet" href="'.direction_css(find_in_path('player.css')).'" type="text/css" media="all" />';
 	return $flux;
 }
 
