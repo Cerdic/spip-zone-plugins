8aba2a188f26b03cd3f0bb0e1627f18b34ee9125
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-09-28 13:26:10 +0000

    Si l'encodage est en cours depuis plus de 5heures... c'est qu'il y a eu un problème quelque part ...
    On le réinitialise donc
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@41309 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spipmotion/genie/spipmotion_file.php
+++ spipmotion/genie/spipmotion_file.php
@@ -28,8 +28,8 @@ function genie_spipmotion_file($time)  {
 	$nb_encodages = sql_countsel('spip_spipmotion_attentes', "encode='non'");
 	spip_log('début de tache cron','spipmotion');
 	spip_log("Il y a $nb_encodages vidéo(s) à encoder","spipmotion");
-	$en_cours = sql_getfetsel('id_spipmotion_attente','spip_spipmotion_attentes',"encode='en_cours'");
-	if(($nb_encodages>0) && ($GLOBALS['meta']['spipmotion_casse'] != 'oui') && !intval($en_cours)){
+	$en_cours = sql_fetsel('id_spipmotion_attente,maj','spip_spipmotion_attentes',"encode='en_cours'");
+	if(($nb_encodages>0) && ($GLOBALS['meta']['spipmotion_casse'] != 'oui') && !intval($en_cours['id_spipmotion_attente'])){
 		$doc_attente = sql_fetsel("*","spip_spipmotion_attentes","encode='non'","","id_spipmotion_attente ASC","1");
 		$id_document = $doc_attente['id_document'];
 		$id_doc_attente = $doc_attente['id_spipmotion_attente'];
@@ -43,11 +43,16 @@ function genie_spipmotion_file($time)  {
 			sql_delete('spip_spipmotion_attentes','id_document='.sql_quote($id_document));
 			genie_spipmotion_file($time);
 		}
-
 	}else if(lire_config('spipmotion_casse') == 'oui'){
 		spip_log('Attention, problème dans la configuration','spipmotion');
-	}else if(intval($en_cours)){
-		spip_log("L'id $en_cours de la file d'attente est en cours d'encodage",'spipmotion');
+	}else if(intval($en_cours['id_spipmotion_attente']) && ($en_cours['maj'] < date('Y-m-d H:i:s',mktime(date('H')-5)))){
+		/**
+		 * Il est peut être nécessaire de vérifier qu'un processus n'est pas en cours?
+		 */
+		spip_log("L'id". $en_cours['id_spipmotion_attente']." de la file d'attente est en cours d'encodage depuis plus de 5 h (".$en_cours['maj']."), on doit le réinitialiser",'spipmotion');
+		sql_updateq('spip_spipmotion_attentes',array('encode' => 'non'),'id_spipmotion_attente ='.intval($en_cours['id_spipmotion_attente']));
+	}else if(intval($en_cours['id_spipmotion_attente'])){
+		spip_log("L'id". $en_cours['id_spipmotion_attente']." de la file d'attente est en cours d'encodage",'spipmotion');
 		spip_log("On attend sa fin avant d'en commencer un nouveau",'spipmotion');
 	}
 	spip_log('fin de tache cron','spipmotion');
