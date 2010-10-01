6fb3cb1718bb70f9e515b095cd3c4d0ba023a432
joseph@larmarange.net <joseph@larmarange.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-10-01 16:08:29 +0000

    On annule les dernières modifs. Autre solution à trouver.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@41394 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spipdf/spipdf.php
+++ spipdf/spipdf.php
@@ -20,108 +20,144 @@ include_spip('inc/charsets') ;
 // repris dans le plugin article_pdf => a modifier
 // http://zone.spip.org/trac/spip-zone/browser/_plugins_/article_pdf
 function spipdf_first_clean($texte) {
-	//Translation des codes iso PB avec l'utilisation de <code>
-	$trans = get_html_translation_table(HTML_ENTITIES);
 
-	// supprimer les remarques HTML (du Couteau Suisse ?)
-	$texte = preg_replace(',<!-- .* -->,msU', '', $texte);
-	
-	$trans = array_flip($trans);
-	$trans["<br />\n"] = '<BR>'; // Pour éviter que le \n ne se tranforme en espace dans les <DIV class=spip_code> (TT, tag SPIP : code)
-	$trans['&#176;'] = '°';
-	$trans["&#339;"] = 'oe';
-	$trans["&#8211;"] = '-';
-	$trans["&#8216;"] = '\'';
-	$trans["&#8217;"] = '\'';		
-	$trans["&#8220;"] = '"';
-	$trans["&#8221;"] = '"';
-	$trans["&#8230;"] = '...';
-	$trans["&#8364;"] = 'Euros';
-	//$trans["&ucirc;"] = "û";
-	$trans['->'] = '-»';
-	$trans['<-'] = '«-';
-	$trans['&nbsp;'] = ' ';
-	$trans['&mdash;'] = '-';
+        //Translation des codes iso PB avec l'utilisation de <code>
+        $trans = get_html_translation_table(HTML_ENTITIES);
 
-	// certains titles font paniquer l'analyse
-	$texte = preg_replace(',title=".*",msU', 'title=""', $texte);
+        // supprimer les remarques HTML (du Couteau Suisse ?)
+        $texte = preg_replace(',<!-- .* -->,msU', '', $texte);
+        
+        $trans = array_flip($trans);
+        $trans["<br />\n"] = '<BR>'; // Pour éviter que le \n ne se tranforme en espace dans les <DIV class=spip_code> (TT, tag SPIP : code)
+        $trans['&#176;'] = '°';
+        $trans["&#339;"] = 'oe';
+        $trans["&#8211;"] = '-';
+        $trans["&#8216;"] = '\'';
+        $trans["&#8217;"] = '\'';		
+        $trans["&#8220;"] = '"';
+        $trans["&#8221;"] = '"';
+        $trans["&#8230;"] = '...';
+        $trans["&#8364;"] = 'Euros';
+        //$trans["&ucirc;"] = "û";
+        $trans['->'] = '-»';
+        $trans['<-'] = '«-';
+        $trans['&nbsp;'] = ' ';
+        $trans['&mdash;'] = '-';
 
-	$texte = unicode2charset(charset2unicode($texte), 'ISO-8859-1'); // repasser tout dans un charset acceptable par export PDF
-	//$texte = utf8_decode($texte); 
-	$texte = strtr($texte, $trans);
+        // certains titles font paniquer l'analyse
+        $texte = preg_replace(',title=".*",msU', 'title=""', $texte);
 
-	return $texte;
+        $texte = unicode2charset(charset2unicode($texte), 'ISO-8859-1'); // repasser tout dans un charset acceptable par export PDF
+        //$texte = utf8_decode($texte); 
+        $texte = strtr($texte, $trans);
+
+        return $texte;
 }
-
-// Nettoyer HTML
-function spipdf_nettoyer_html($html) {
-	// supprimer les spans autour des images et récupérer le placement
-	$patterns_float = '/<span class=\'spip_document_.*spip_documents.*float:(.*);.*>(.*)<\/span>/iUms';
-	function remplaceSpan($matches) { return str_replace('img', 'img style="padding:5px;" align="'.$matches[1].'"', $matches[2]); }
-	$html = preg_replace_callback($patterns_float, 'remplaceSpan', $html);
-
-	// supprimer les spans autour des images
-	$patterns_float = '/<span class=\'spip_document_.*spip_documents.*>(.*)<\/span>/iUms';
-	function remplaceSpanCenter($matches) { return $matches[1]; }
-	$html = preg_replace_callback($patterns_float, 'remplaceSpanCenter', $html);
-
-	// supprimer les dl autour des images et récupérer le placement
-	$patterns_float = '/<dl class=\'spip_document_.*spip_documents.*float:(.*);.*<dt>(.*)<\/dt>.*<\/dl>/iUms';
-	function remplaceDt($matches) { return str_replace('img', 'img style="padding:5px;" align="'.$matches[1].'"', $matches[2]); }
-	$html = preg_replace_callback($patterns_float, 'remplaceDt', $html);
-
-	// supprimer les dl autour des images
-	$patterns_float = '/<dl class=\'spip_document_.*spip_documents.*<dt>(.*)<\/dt>.*<\/dl>/iUms';
-	function remplaceDtCenter($matches) { return $matches[1]; }
-	$html = preg_replace_callback($patterns_float, 'remplaceDtCenter', $html);
-
-	// remplacer les captions
-	$patterns_caption = '/<table(.*)<caption>(.*)<\/caption>(.*)<\/table>/iUms';
-	function remplaceCaption($matches) { 
-		$table  = '<table style="border:none;"'.$matches[1].'<tr><td style="text-align: center;border:none;">'.$matches[2].'</td></tr>';
-		$table .= '<tr><td style="border:none;">';
-		$table .= '<table'.$matches[1].$matches[3].'</table>';
-		$table .= '</td></tr></table>';
-		return $table; 
-	}
-	$html = preg_replace_callback($patterns_caption, 'remplaceCaption', $html);
-
-	// tableaux centré
-	$html = preg_replace('/<table/iUms', '<table align="center"', $html);
-
-	// gestion de caractères spéciaux et de charset
-	$html = spipdf_first_clean($html);
-	
-	//  supprimer spipdf
-	$pattern_spipdf = '<spipdf(.*)\/>';
-	$html = preg_replace('/'.$pattern_spipdf.'/iUms', '', $html);
-
-	return $html;
-}
-
 
 // generation de pdf
-function spipdf_recuperer_fond($flux) {
-	$html = $flux['data']['texte'];
-	// l'aticle doit commencer par <spipdf />
-	// on matche les pages qui contiennent la déclaration <spipdf
-	if(strpos($html, '<spipdf')!==false) {
-		// On nettoie le html pour HTML2PDF
-		$html = spipdf_nettoyer_html($html);
-		
-		// appel de la classe HTML2pdf
-		require_once(dirname(__FILE__).'/html2pdf/html2pdf.class.php');
-		try
-		{
-			$html2pdf = new HTML2PDF('P','A4','fr', false, 'ISO-8859-15');
-			$html2pdf->setDefaultFont('Arial');
-			$content_PDF = $html2pdf->Output('', true);
-			
-			$flux['data']['texte'] = $content_PDF;
-		}
-		catch(HTML2PDF_exception $e) { echo $e; }
-	}
+function spipdf_affichage_final($flux) {
 
-	return $flux;
+    // l'aticle doit commencer par <spipdf nom="pdf_article_#ID_ARTICLE" />
+    // on matche les pages qui contiennent la déclaration <spipdf
+    if(strpos($flux, '<spipdf')!==false) {
+
+        // supprimer barre admin Non fonctionnel
+        if(defined('SPIPDF_MASQUE_FORMULAIRE_ADMIN')) {
+            $patterns = array(
+                '/<link(.*)spip_admin\.css(.*)\/>/iUsm',
+                '/<link(.*)spip_admin_perso\.css(.*)\/>/iUsm',
+                //'/<div(.*)spip-admin-bloc(.*)<\/div>/iUsm',
+                '/<div(.*)spip-admin(.*)\/div>/iUsm',
+            );
+            $replaces = array(
+                '',
+                '',
+                '',
+            );
+            $flux = preg_replace($patterns, $replaces, $flux);
+        }
+
+        // supprimer les spans autour des images et récupérer le placement
+        $patterns_float = '/<span class=\'spip_document_.*spip_documents.*float:(.*);.*>(.*)<\/span>/iUms';
+        function remplaceSpan($matches) { return str_replace('img', 'img style="padding:5px;" align="'.$matches[1].'"', $matches[2]); }
+        $flux = preg_replace_callback($patterns_float, 'remplaceSpan', $flux);
+
+        // supprimer les spans autour des images
+        $patterns_float = '/<span class=\'spip_document_.*spip_documents.*>(.*)<\/span>/iUms';
+        function remplaceSpanCenter($matches) { return $matches[1]; }
+        $flux = preg_replace_callback($patterns_float, 'remplaceSpanCenter', $flux);
+
+        // supprimer les dl autour des images et récupérer le placement
+        $patterns_float = '/<dl class=\'spip_document_.*spip_documents.*float:(.*);.*<dt>(.*)<\/dt>.*<\/dl>/iUms';
+        function remplaceDt($matches) { return str_replace('img', 'img style="padding:5px;" align="'.$matches[1].'"', $matches[2]); }
+        $flux = preg_replace_callback($patterns_float, 'remplaceDt', $flux);
+
+        // supprimer les dl autour des images
+        $patterns_float = '/<dl class=\'spip_document_.*spip_documents.*<dt>(.*)<\/dt>.*<\/dl>/iUms';
+        function remplaceDtCenter($matches) { return $matches[1]; }
+        $flux = preg_replace_callback($patterns_float, 'remplaceDtCenter', $flux);
+
+        // remplacer les captions
+        $patterns_caption = '/<table(.*)<caption>(.*)<\/caption>(.*)<\/table>/iUms';
+        function remplaceCaption($matches) { 
+            $table  = '<table style="border:none;"'.$matches[1].'<tr><td style="text-align: center;border:none;">'.$matches[2].'</td></tr>';
+            $table .= '<tr><td style="border:none;">';
+            $table .= '<table'.$matches[1].$matches[3].'</table>';
+            $table .= '</td></tr></table>';
+            return $table; 
+        }
+        $flux = preg_replace_callback($patterns_caption, 'remplaceCaption', $flux);
+
+        // tableaux centré
+        $flux = preg_replace('/<table/iUms', '<table align="center"', $flux);
+
+        // gestion de cractères soéciaux et de charset
+        $flux = spipdf_first_clean($flux);
+
+        // titre du fichier récupéré dans le squelette
+        $pattern_spipdf = '<spipdf\snom="(.*)"\s\/>';
+        preg_match('/'.$pattern_spipdf.'/iUms', $flux, $match_titre);
+
+        // on a un titre
+        if(!empty($match_titre[1])) {
+            $titre_pdf = $match_titre[1];
+        } else { // sinon, soit le titre du fichier, soit un uniqid (ancien fonctionnement)
+
+            if(!empty($GLOBALS['contexte']['id_article'])) {
+                $id_article = $GLOBALS['contexte']['id_article'];
+            } else {
+                $id_article = uniqid();
+            }
+            // pattern de remplacement
+            if(defined('SPIPDF_TITRE_FICHIER')) {
+                $titre_pdf = sprintf(SPIPDF_TITRE_FICHIER, $id_article);
+            } else {
+                $titre_pdf = 'Article-'.$id_article.'.pdf';
+            }
+        }
+
+        //  supprimer spipdf
+        $flux = preg_replace('/'.$pattern_spipdf.'/iUms', '', $flux);
+
+        // appel de la classe HTML2pdf
+        require_once(dirname(__FILE__).'/html2pdf/html2pdf.class.php');
+        try
+        {
+            $html2pdf = new HTML2PDF('P','A4','fr', false, 'ISO-8859-15');
+            $html2pdf->setDefaultFont('Arial');
+            $html2pdf->writeHTML($flux);
+            if(defined('SPIPDF_DEBUG_HTML2PDF')) {
+                $html2pdf->setModeDebug();
+            }
+            // génère et renvoi la page au browser
+            $html2pdf->Output($titre_pdf);
+            // stoppe le flux d'execution
+            return null;
+        }
+        catch(HTML2PDF_exception $e) { echo $e; }
+    }
+
+    return $flux;
+
 }
 ?>
