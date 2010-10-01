26e1ec421e0c8bff5daf5550781fba1cb3a397fb
joseph@larmarange.net <joseph@larmarange.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-10-01 14:48:11 +0000

    Passer par recuperer_fond pour utiliser le cache natif de SPIP et un peu de nettoyage
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@41392 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spipdf/spipdf_options.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// choix du motif pour le nom des fichiers => %s va correspondre au numéro de l'article
-//define('SPIPDF_TITRE_FICHIER', 'Mon-Article-%s-Site.pdf');
-
-// debug de html2pdf
-//define('SPIPDF_DEBUG_HTML2PDF', true);
-
-?>
