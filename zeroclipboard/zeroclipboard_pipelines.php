9b148ec949dde8f480c67556a6dcdbf85c0e12c9
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-10-02 13:13:38 +0000

    Avoir un <head> un peu plus clair (suite)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@41408 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- zeroclipboard/zeroclipboard_pipelines.php
+++ zeroclipboard/zeroclipboard_pipelines.php
@@ -1,8 +1,10 @@
 <?php
 
 function zeroclipboard_insert_head($flux){
-	$flux .= '<script type="text/javascript" src="'.find_in_path(_DIR_LIB_ZEROCLIPBOARD.'ZeroClipboard.js').'"></script>'."\n";
-	$flux .= '<script type="text/javascript" src="'.generer_url_public('zeroclipboard.js').'"></script>'."\n";
+	$flux .= '
+<script type="text/javascript" src="'.find_in_path(_DIR_LIB_ZEROCLIPBOARD.'ZeroClipboard.js').'"></script>
+<script type="text/javascript" src="'.generer_url_public('zeroclipboard.js').'"></script>
+';
 	return $flux;
 }
 
