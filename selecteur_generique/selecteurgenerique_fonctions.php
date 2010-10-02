6447ba5aa9d58a2eba45edfee4f521071325ff73
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-10-02 13:12:39 +0000

    Avoir un <head> un peu plus clair (début d'une petite série)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@41405 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- selecteur_generique/selecteurgenerique_fonctions.php
+++ selecteur_generique/selecteurgenerique_fonctions.php
@@ -5,8 +5,10 @@ function selecteurgenerique_verifier_js($flux){
     if(strpos($flux,'jquery.autocomplete.js')===FALSE){
 		$autocompleter = find_in_path('javascript/jquery.autocomplete.js');
 		$autocompletecss = find_in_path('iautocompleter.css');
-		$contenu .= "<script type='text/javascript' src='$autocompleter'></script>";
-		$contenu .= "<link rel='stylesheet' href='$autocompletecss' type='text/css' media='all' />";
+		$contenu .= "
+<script type='text/javascript' src='$autocompleter'></script>
+<link rel='stylesheet' href='$autocompletecss' type='text/css' media='all' />
+";
 	};
 	return $contenu;
 }
