34630c470d8f625a0a248d240196ddecd488d890
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-10-02 18:57:43 +0000

    Révision mineure :
    - On met un alt "précédent"/"suivant" sur les images de l'inclure
    - On met une class distinctive sur les liens de l'inclure aussi prev/next pourpouvoir les styler indifféremment
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@41415 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ criteres_suivant_precedent/lang/csp_en.php
@@ -0,0 +1,10 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	// A
+	'alt_lien_next' => 'Next',
+	'alt_lien_prev' => 'Previous',
+);
+?>
---
+++ criteres_suivant_precedent/lang/csp_fr.php
@@ -0,0 +1,10 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	// A
+	'alt_lien_next' => 'Suivant',
+	'alt_lien_prev' => 'Pr&eacute;c&eacute;dent',
+);
+?>
