8628eb31d9742465c273551b851e5db36f09643e
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-10-03 14:44:01 +0000

    Une version en anglais (rapidement traduite)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@41418 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ polyhierarchie/lang/polyhier_en.php
@@ -0,0 +1,38 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'articles_associes' => 'Linked articles',
+	
+	// B
+	
+	// D
+	
+	// E
+	
+	// F
+	
+	// I
+	
+	// L
+	'label_parents' => 'In the sections',
+	'label_autres_parents' => 'Also in the sections',
+	
+	// M
+	// P
+	'parent_obligatoire' => 'You should at least specify a section',
+	
+	// R
+	'rubriques_associees' => 'Linked sections',
+	
+	// T
+	
+	// U
+	
+	//
+
+);
+?>
