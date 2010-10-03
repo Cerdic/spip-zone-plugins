463493da4db1038a47587ad5f63bdac6f869b963
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-10-03 22:10:02 +0000

    Traduction rapide en anglais
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@41426 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ gravatar/lang/gravatar_en.php
@@ -0,0 +1,25 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+'gravatar_info' => 'To show your avatar here, register it first on <a href="http://www.gravatar.com/" rel="external nofollow" class="spip_out">gravatar.com</a> (free and painless).',
+'gravatar_info_forum' => 'To show your avatar with your message, register it first on <a href="http://www.gravatar.com/" rel="external nofollow" class="spip_out">gravatar.com</a> (free et painless) and don\'t forget to indicate your Email addresse here.',
+'titre_gravatar_auteur' => 'AUTHOR\'S GRAVATAR',
+
+'cfg_titre_gravatar' => 'Gravatars',
+'cfg_descr_gravatar' => 'Setup the Gravatar\'s display',
+
+'label_image_defaut' => 'Default image',
+'explication_image_defaut' => 'For those who don\'t have any avatar, use&nbsp;:',
+'label_image_defaut_404' => 'no image',
+'label_image_defaut_png' => '<tt>images/gravatar.png</tt>',
+'label_image_defaut_gravatar' => 'the Gravatar\'s logo',
+'label_image_defaut_identicon' => '<i>Identicon</i> (generated from the Email address)',
+'label_image_defaut_monsterid' => '<i>MonsterID</i> (generated from the Email address)',
+'label_image_defaut_wavatar' => '<i>Wavatar</i> (generated from the Email address)',
+'label_taille' => 'Gravatar\'s size',
+);
+
+?>
