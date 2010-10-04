7f9f21c95a1c50607d314b97079295c346f83066
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-10-04 17:58:34 +0000

    rendre ce super critère public... ne devrait pas faire dysfonctionner le privé...
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@41465 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ selecteur_generique/public/selecteur_generique_criteres.php
@@ -0,0 +1,45 @@
+<?php 
+// critere {contenu_auteur_select} , cf. sedna
+function critere_contenu_auteur_select_dist($idb, &$boucles, $crit) {
+	$boucle = &$boucles[$idb];
+
+	if (isset($crit->param[0][0])
+	AND $crit->param[0][0]->texte == 'strict')
+		$debut = '';
+	else
+		$debut = '%';
+
+	// un peu trop rapide, ca... le compilateur exige mieux (??)
+	$boucle->hash = '
+	// RECHERCHE
+	if ($r = _request("q")) {
+		$r = _q("'.$debut.'$r%");
+		$s = "(
+			auteurs.nom LIKE $r
+			OR auteurs.email LIKE $r'
+
+	// on ne cherche pas dans la bio etc
+	// si on peut trouver direct dans le nom ou l'email
+	. (!$debut
+		? ''
+		: '
+			OR auteurs.bio LIKE $r
+			OR auteurs.nom_site LIKE $r
+			OR auteurs.url_site LIKE $r
+		')
+	.'
+		)";
+	} else {
+		$s = 1;
+	}
+	';
+	$boucle->where[] = '$s';
+}
+
+// Un filtre pour afficher le bonhomme_statut
+function icone_statut_auteur($statut) {
+	include_spip('inc/presentation');
+	$text = bonhomme_statut(array('statut'=>$statut));
+	return replace($text,'\n','');
+}
+?>
