1d1f27458d98e370436b5da3d188d37f13646d2f
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-10-05 20:30:58 +0000

    langues (inscription2)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@41528 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- inscription2/inscription2_2_0/lang/inscription2_de.php
+++ inscription2/inscription2_2_0/lang/inscription2_de.php
@@ -11,16 +11,16 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'a_confirmer' => 'Zu best&amp;auml;tigen',
 	'accesrestreint' => 'Eingeschr&amp;auml;nkter Zugang',
 	'aconfirmer' => 'Zu best&amp;auml;tigen',
-	'action_adherent' => 'Aktionen', # MODIF
+	'action_adherent' => 'Aktionen',
 	'activation_compte' => 'Aktivieren Sie Ihr Konto',
-	'adherents' => 'Mitglieder', # MODIF
+	'adherents' => 'Nutzer',
 	'admin' => 'Admin',
 	'admin_modifier_auteur_supp' => 'Weiterf&uuml;hrende Informationen',
 	'adresse' => 'Adresse',
 	'adresse_pro' => 'Adresse (beruflich)',
-	'afficher_tous' => 'Afficher tous les utilisateurs', # NEW
-	'ajouter_adherent' => 'Neuen Teilnehmer anlegen', # MODIF
-	'ans' => 'ans', # NEW
+	'afficher_tous' => 'Alle Nutzer anzeigen',
+	'ajouter_adherent' => 'Neuen Nutzer anlegen',
+	'ans' => 'Jahre',
 	'aout' => 'August',
 	'aucun_resultat_recherche' => 'Nichts gefunden',
 	'auteur' => 'Autor',
@@ -40,21 +40,21 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'choisir_categories' => 'Bitte hier ausw&auml;hlen:',
 	'choisir_nouveau_password' => 'Klicken Sie auf den folgenden Link, um ein neues Passwort zu w&auml;hlen.',
 	'choisir_zones' => 'Bitte w&auml;hlen Sie die Bereiche, zu denen neue Mitglieder geh&ouml;ren sollen.',
-	'choisissez' => 'Choisissez', # NEW
-	'choix_affordance_email' => 'Email', # NEW
-	'choix_affordance_login' => 'Login (par d&eacute;faut dans SPIP)', # NEW
-	'choix_affordance_login_email' => 'Login et email', # NEW
+	'choisissez' => 'Ausw&auml;hlen',
+	'choix_affordance_email' => 'E-Mail',
+	'choix_affordance_login' => 'Login (SPIP-Standard)',
+	'choix_affordance_login_email' => 'Login und E-Mail',
 	'choix_domaine' => 'Bitte w&auml;hlen Sie ihre Domain',
 	'civilite' => 'Anrede',
 	'code_postal' => 'Postleitzahl',
 	'code_postal_pro' => 'Postleitzahl (beruflich)',
 	'commentaire' => 'Kommentar',
 	'compte_active' => 'Ihr Konto wurde aktiviert',
-	'compte_efface' => 'Ihr Zugang wurde gel&ouml;scht', # MODIF
-	'conf_plugin' => 'Konfiguration Inscription2', # MODIF
-	'conf_plugin_page' => 'Gespeicherte Konfiguration des Plugin Inscription2', # MODIF
+	'compte_efface' => 'Ihr Konto wurde gel&ouml;scht',
+	'conf_plugin' => 'Konfiguration von Inscription 2.0',
+	'conf_plugin_page' => 'Gespeicherte Konfiguration des Plugin Inscription 2.0',
 	'configs' => 'Konfigurationen',
-	'configuration' => 'Konfiguration', # MODIF
+	'configuration' => 'Konfiguration der Nutzereintr&auml;ge',
 	'confirmation' => '<p>Wollen Sie wirklich fortfahren?</p><p>Alle &Auml;nderungen sind unwiederrufbar!!!</p>',
 	'contacts_personnels' => 'Pers&ouml;nliche Kontakte',
 	'contacts_pros' => 'Berufliche Kontakte',
@@ -63,29 +63,29 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// D
 	'decembre' => 'Dezember',
-	'delete_user_select' => 'Ausgew&auml;hlte Nutzer l&ouml;schen', # MODIF
+	'delete_user_select' => 'Ausgew&auml;hlte Nutzer l&ouml;schen',
 	'demande_password' => 'Ihr Passwort:',
-	'descriptif_plugin' => 'Vous trouverez ici tous les utilisateurs inscrits sur le site. Leur statut est indiqu&eacute; par la couleur de leur icone.<br /><br />Vous pouvez configurer des champs suppl&eacute;mentaires, propos&eacute;s en option aux visiteurs au moment de l\'inscription.', # NEW
-	'description_cfg' => 'Dieses Plugin erm&ouml;glicht, Zusatzinformationen &uuml;ber die Redakteure ihrer Website zu erfassen. Bitte w&auml;hlen sie die entsprechenden Felder hier aus.', # MODIF
+	'descriptif_plugin' => 'Hier sehen sie alle bei der Website eingeschriebenen Nutzer. Ihr Status wird die Farbe ihre Icons angezeigt.<br /><br />Sie k&ouml;nnen zus&auml;tzliche Felder einstellen, die den Nutzern bei der Anmeldung zum Ausf&uuml;llen angezeigt werden.',
+	'description_cfg' => 'Zusatzfelder f&uuml;r Nutzer konfigurieren',
 	'description_page' => 'Hier k&ouml;nnen Sie Ihre gespeicherten Einstellungen &uuml;berpr&uuml;fen.',
 	'divers' => 'Verschiedenes',
 	'domaine' => 'Domaine',
 	'domaines' => 'Domain',
 
 	// E
-	'editer_adherent' => 'Mitglied bearbeiten', # MODIF
+	'editer_adherent' => 'Nutzer bearbeiten',
 	'effacement_auto_impossible' => 'Der Zugang kann nicht automatische gel&ouml;scht werden. Bitte nehmen sie mit uns Kontakt auf.',
 	'email' => 'Email',
 	'email_bonjour' => 'Hallo @nom@,',
-	'email_deja_enregistre' => 'Cette adresse email est d&eacute;j&agrave; enregistr&eacute;e. Utilisez le formulaire de connexion pour acc&eacute;der &agrave; votre compte.', # NEW
+	'email_deja_enregistre' => 'Diese E-Mail existiert bereits. Bitte verwende sie das Login-Formular, um zu ihrem Konto zu gelangen.',
 	'email_obligatoire' => 'Email ist Pflichtfeld',
 	'email_valide' => 'Bitte geben Sie eine g&uuml;ltige Mailadresse ein',
-	'erreur_reglement_obligatoire' => 'Vous devez accepter le r&egrave;glement', # NEW
+	'erreur_reglement_obligatoire' => 'Sie m&uuml;ssen die Nutzungsbedingungen anerkennen.',
 	'exp_divers' => 'Dieses Feld steht den Besuchern f&uuml;r Kommentare zur Verf&uuml;gung.',
 	'exp_publication' => 'Genehmigung der Ver&ouml;ffentlichung der pers&ouml;nlichen Daten',
 	'exp_statut' => 'W&auml;hlen Sie den Status f&uuml;r neue Mitglieder',
 	'exp_statut_rel' => 'Das Feld enth&auml;lt einen anderen Status als den von SPIP, damit eine Institution eigene Berechtigungen vergeben kann.',
-	'explication_affordance_form' => 'Champ affich&eacute; sur les formulaires d\'identification (#LOGIN_PUBLIC)', # NEW
+	'explication_affordance_form' => 'Feld wird im Anmeldeformular (#LOGIN_PUBLIC) angezeigt',
 
 	// F
 	'fax' => 'Fax',
@@ -93,13 +93,13 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'feminin' => 'Frau',
 	'fevrier' => 'Februar',
 	'fiche' => 'Eintrag',
-	'fiche_adherent' => 'Mitgliedseintrag', # MODIF
-	'fiche_expl' => ' : Das Feld wird im Mitgliedseintrag angezeigt', # MODIF
+	'fiche_adherent' => 'Nutzerseite',
+	'fiche_expl' => ' : Das Feld wird auf der Nutzerseite im Redaktionssystem angezeigt',
 	'fiche_mod' => '&Auml;nderung m&ouml;glich',
-	'fiche_mod_expl' => ' : Das Feld kann vom Mitglied im &ouml;ffentlichen Bereich ge&auml;ndert werden, wenn das Plugin CRAYONS installiert ist.', # MODIF
+	'fiche_mod_expl' => ' : Das Feld kann vom Mitglied im &ouml;ffentlichen Bereich ge&auml;ndert werden, wenn das Plugin #CRAYONS (Der Stift) installiert ist.',
 	'fonction' => 'Funktion',
 	'form' => 'Formular',
-	'form_expl' => ' : Das Feld wird im Formular INSCRIPTION2 angezeigt', # MODIF
+	'form_expl' => ' : Das Feld wird im Formular INSCRIPTION2 angezeigt',
 	'form_oblig_expl' => ' : Pflichtfeld des Formulars',
 	'format' => 'Format',
 	'formulaire_inscription' => 'Anmeldeformular',
@@ -109,33 +109,33 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'formulaire_remplir_validation' => 'Bitte &uuml;berpr&uuml;fen sie die ung&uuml;ltigen Felder.',
 
 	// G
-	'general_infos' => 'Allgemeine Informationen', # MODIF
+	'general_infos' => 'Allgemeine Informationen',
 	'geoloc' => 'Geografische Lokalisierung',
 	'geomap_obligatoire' => 'Um Daten zur Lokalisierung von Personen zu verwenden, wird das Plugin GoogleMapApi ben&ouml;tigt.',
 	'gerent' => 'Mitglied',
-	'gestion_adherent' => 'Mitgliederverwaltung', # MODIF
+	'gestion_adherent' => 'Nutzerverwaltung',
 
 	// H
 	'html' => 'HTML',
 
 	// I
-	'icone_afficher_utilisateurs' => 'Afficher les utilisateurs', # NEW
-	'icone_menu_config' => 'Inscription2', # MODIF
-	'identification' => 'Identification', # NEW
+	'icone_afficher_utilisateurs' => 'Nutzer anzeigen',
+	'icone_menu_config' => 'Erweiterte Nutzerverwaltung',
+	'identification' => 'Identifikation',
 	'info_connection' => 'Verbindungsinformationen',
-	'info_defaut' => 'Standardinformationen', # MODIF
-	'info_defaut_desc' => 'Obligatorische Standardinformationen', # MODIF
-	'info_gener' => 'Allgemeine Informationen', # MODIF
+	'info_defaut' => 'Obligatorische Angaben',
+	'info_defaut_desc' => 'Einstellungen',
+	'info_gener' => 'Allgemeine Informationen',
 	'info_gener_desc' => 'Optionen, die von den neuen Mitgliedern des web site verlangt werden',
 	'info_internes' => 'Interne Informationen',
 	'info_internes_desc' => 'Options qui seront stock&eacute;es dans la base de donn&eacute;es mais ne seront pas affich&eacute;es dans le formulaire des nouveaux adh&eacute;rents',
 	'info_perso' => 'Pers&ouml;nliche Informationen',
-	'info_perso_desc' => 'Pers&ouml;nliche Informationen, die von bei neuen Teilnehmern der Website abgefragt werden',
+	'info_perso_desc' => 'Pers&ouml;nliche Informationen, die von bei neuen Nutzern der Website abgefragt werden',
 	'info_pro' => 'Berufliche Angaben',
 	'infos_adherent' => 'Zusatzinformationen',
-	'infos_complementaires' => 'Informations compl&eacute;mentaires', # NEW
+	'infos_complementaires' => 'Zusatzinformationen',
 	'infos_personnelles' => 'Neue pers&ouml;nliche Angaben',
-	'inscription2' => 'Inscription 2.0', # NEW
+	'inscription2' => 'Inscription 2.0',
 	'inscriptions' => 'Anmeldungen',
 	'inserez_infos' => 'Tragen Sie bitte die Angaben ein',
 
@@ -145,23 +145,23 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'juin' => 'Juni',
 
 	// L
-	'label_affordance_form' => 'Param&eacute;trage des formulaires d\'identification', # NEW
-	'label_public_reglement' => 'J\'ai lu et j\'accepte le r&egrave;glement', # NEW
-	'label_public_reglement_url' => 'J\'ai lu et j\'accepte le <a href="@url@" class="spip_in reglement">r&egrave;glement</a>', # NEW
-	'label_reglement' => 'R&egrave;glement &agrave; valider', # NEW
-	'label_reglement_article' => 'Article original du site correspondant au r&egrave;glement', # NEW
-	'label_validation_numero_international' => 'Forcer les num&eacute;ros de t&eacute;l&eacute;phone &agrave; &ecirc;tre sous la forme internationale', # NEW
+	'label_affordance_form' => 'Einstellungen der Anmeldeformulare',
+	'label_public_reglement' => 'Ich habe die Nutzungsbedingungen gelesen und akzeptiert.',
+	'label_public_reglement_url' => 'Ich habe die <a href="@url@" class="spip_in reglement">Nutzungsbedingungen</a> gelesen und akzeptiert.',
+	'label_reglement' => 'Nutzungsbedingungen best&auml;tigen',
+	'label_reglement_article' => 'Artikel, der die Nutzungsbedingungen enth&auml;lt',
+	'label_validation_numero_international' => 'Internationales Format f&uuml;r Telefonnummern erzwingen',
 	'latitude' => 'Geographische Breite',
-	'legend_affordance_form' => 'Formulaire d\'identification', # NEW
-	'legend_oubli_pass' => 'Pas de mot de passe / mot de passe oubli&eacute;', # NEW
-	'legend_reglement' => 'R&egrave;glement du site', # NEW
-	'legend_validation' => 'Validations', # NEW
+	'legend_affordance_form' => 'Loginformular',
+	'legend_oubli_pass' => 'Kein Pa&szlig;wort / Pa&szlig;wort vergessen',
+	'legend_reglement' => 'Nutzungsbedingungen',
+	'legend_validation' => 'Best&auml;tigungen',
 	'legende' => 'Legende',
 	'lisez_mail' => 'An die eingetragene Adresse wurde eine Email geschickt. Sie enth&auml;lt die zur Aktivierung Ihres Kontos erforderlichen Informationen.',
-	'liste_adherents' => 'Mitglieder anzeigen', # MODIF
-	'liste_comptes_titre' => 'Mitgliederliste', # MODIF
+	'liste_adherents' => 'Nutzerliste anzeigen',
+	'liste_comptes_titre' => 'Nutzerliste',
 	'login' => 'Benutzername (login)',
-	'logo_auteur' => 'Logo', # NEW
+	'logo_auteur' => 'Logo',
 	'longitude' => 'Geographische L&auml;nge',
 
 	// M
@@ -174,12 +174,12 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'mobile' => 'Funktelefon',
 	'mobile_pro' => 'Funktelefon (beruflich)',
 	'modif_pass_titre' => 'Passwort &auml;ndern',
-	'moins_seconde' => 'moins d\'une seconde', # NEW
+	'moins_seconde' => 'unter einer Sekunde',
 	'mot_passe_reste_identique' => 'Ihr Passwort wurde ge&auml;ndert',
 
 	// N
 	'naissance' => 'Geburtstag',
-	'nb_users_supprimes' => '@nb@ Mitglieder wurden gel&ouml;scht.', # MODIF
+	'nb_users_supprimes' => '@nb@ Nutzer gel&ouml;scht.',
 	'nettoyer_tables' => 'Tabellen aufr&auml;umen',
 	'no_user_selected' => 'Sie haben kein Mitglied ausgew&auml;hlt',
 	'nom' => 'Pseudonym',
@@ -190,14 +190,14 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'non_renseignee' => 'Keine Angabe',
 	'novembre' => 'November',
 	'numero_valide' => 'Bitte geben Sie eine g&uuml;ltige Zahl ein',
-	'numero_valide_international' => 'Ce num&eacute;ro doit &ecirc;tre sous la forme internationale (ex: +32 475 123 456)', # NEW
+	'numero_valide_international' => 'Die Telefonnummer mu&szlig; im internationalen Format engegeben werden (z.B. +49 30 12345678)',
 
 	// O
 	'octobre' => 'Oktober',
 
 	// P
 	'page_confirmation' => 'Seite zur Best&auml;tigung Ihrer Anmeldung',
-	'par_defaut' => 'Standard', # MODIF
+	'par_defaut' => 'Pflichtfeld',
 	'pass' => 'Passwort',
 	'pass_egal' => 'Bitte wiederholen Sie das Passwort.',
 	'pass_indiquez_cidessous' => 'Bitte geben Sie die Mailadresse an,
@@ -207,19 +207,19 @@ sagt, wie Sie vorgehen m&uuml;ssen,
 um Ihre Zugangsdaten zu &auml;ndern.',
 	'pass_minimum' => 'Ihr Passwort muss mindestens 5 Zeichen lang sein.',
 	'pass_oubli_mot' => 'Passwort &auml;ndern',
-	'pass_rappel_email' => 'Rappel : votre adresse email est "@email@".', # NEW
-	'pass_rappel_login_email' => 'Rappel : votre login est "@login@" et votre adresse email est "@email@".', # NEW
+	'pass_rappel_email' => 'Erinnerung: Ihre E-Mail-Adresse ist "@email@".',
+	'pass_rappel_login_email' => 'Erinnerung: Ihr Login ist "@login@" und ihre E-Mail-Adresse ist "@email@".',
 	'pass_recevoir_mail' => 'Sie erhalten nun eine Mail mit Informationen zum &Auml;ndern Ihrer Zugangsdaten.',
 	'password_obligatoire' => 'Das Passwort ist erforderlich.',
 	'password_retaper' => 'Passwort best&auml;tigen',
 	'pays' => 'Land',
-	'pays_defaut' => 'Pays par d&eacute;faut', # NEW
+	'pays_defaut' => 'Standardland',
 	'pays_pro' => 'Land (beruflich)',
 	'pgp' => 'PGP Schl&uuml;ssel',
 	'prenom' => 'Vorname',
 	'probleme_email' => 'Mailproblem: Die Mail zum Aktivieren des Kontos konnte nicht verschickt werden.',
 	'profession' => 'Beruf',
-	'profil_droits_insuffisants' => 'Sie d&uuml;rfen diesen Autor nicht &auml;ndern<br/>', # MODIF
+	'profil_droits_insuffisants' => 'Sie d&uuml;rfen diesen Autor nicht bearbeiten<br/>',
 	'profil_modifie_ok' => 'Die &Auml;nderung Ihres Profils wurden gespeichert.',
 	'publication' => 'Ver&ouml;ffentlichung',
 
@@ -227,30 +227,30 @@ um Ihre Zugangsdaten zu &auml;ndern.',
 	'raccourcis' => 'Schnellzugang',
 	'rappel_login' => 'Erinnerung - Ihr Login lautet: ',
 	'rappel_password' => 'Ihr Passwort',
-	'recherche_case' => 'Suchfeld', # MODIF
-	'recherche_utilisateurs' => 'Redakteure suchen', # MODIF
-	'recherche_valeur' => 'Inhalt der Suche', # MODIF
+	'recherche_case' => 'Im Feld:',
+	'recherche_utilisateurs' => 'Redakteure suchen',
+	'recherche_valeur' => 'Suchen:',
 	'redemande_password' => 'Geben Sie Ihr Passwort erneut ein:',
 	'rien_a_faire' => 'Keien Aktion erforderlich',
 
 	// S
 	'saisir_email_valide' => 'Bitte geben sie eine g&uuml;ltige E-Mail Adresse an.',
-	'secondes' => 'secondes', # NEW
+	'secondes' => 'Sekunden',
 	'secteur' => 'Sektor',
 	'septembre' => 'September',
 	'sexe' => 'Anrede/Geschlecht',
 	'societe' => 'Firma / Verein ...',
 	'statut' => 'Status',
 	'statut_rel' => 'Interner Status',
-	'statuts_actifs' => 'Les couleurs des icones correspondent aux statuts suivants : ', # NEW
+	'statuts_actifs' => 'Die Farbend der Icons entsprechen diesem Status:',
 	'suppression_faite' => 'Ihr Daten wurden gel&ouml;scht.',
-	'supprimer_adherent' => 'Mitglied l&ouml;schen', # MODIF
+	'supprimer_adherent' => 'Nutzer l&ouml;schen',
 	'supprimer_adherent_red' => 'l&ouml;schen',
 	'surnom' => 'Spitzname',
 
 	// T
 	'table' => 'Liste',
-	'table_expl' => ' : Das Feld wird in der Mitgliedsliste angezeigt (Redaktionssystem)', # MODIF
+	'table_expl' => ': Das Feld wird in der Nutzerliste angezeigt (Redaktionssystem)',
 	'tel' => 'Tel.',
 	'telephone' => 'Telefon',
 	'telephone_pro' => 'Telefon (beruflich)',
@@ -258,39 +258,36 @@ um Ihre Zugangsdaten zu &auml;ndern.',
 	'texte_email_confirmation' => 'Ihr Konto ist aktiviert. Sie k&ouml;nnen sich jetzt mit Ihren pers&ouml;nlichen Zugangsdaten einloggen.n
 
 Ihr Login ist&nbsp;: @login@
-Ihr Passwort haben Sie soeben enigegeben.
+Ihr Passwort haben Sie soeben eingegeben.
 
 Vielen Dank f&uuml;r Ihr Vertrauen
 
-Das @nom_site@-Team', # MODIF
+Das @nom_site@-Team',
 	'texte_email_inscription' => 'Sie sind dabei, ihre Anmeldung bei der Website @nom_site@ abzuschlie&szlig;en.
 Bitte klicken Sie auf den Link weiter unten klicken und legen sie ihr Passwort fest.
 
 @link_activation@
+
 
 Vielen Dank f&uuml;r Ihr Vertrauen.
 
 Das @nom_site@-Team
 @url_site@
 
-Ihr Login ist: @login@
-@url_site@
-
 Wenn Sie sich nicht angemeldet haben, klicken Sie bitte auf den Link weiter unten, um alle erfassten Daten zu l&ouml;schen.n
-
 @link_suppresion@
 
 
-', # MODIF
+',
 	'titre_confirmation' => 'Best&auml;tigung',
 
 	// U
-	'un_an' => 'un an', # NEW
-	'un_jour' => 'un jour', # NEW
-	'un_mois' => 'un mois', # NEW
-	'une_heure' => 'une heure', # NEW
-	'une_minute' => 'une minute', # NEW
-	'une_seconde' => 'une seconde', # NEW
+	'un_an' => 'ein Jahr',
+	'un_jour' => 'ein Tag',
+	'un_mois' => 'ein Monat',
+	'une_heure' => 'eine Stunde',
+	'une_minute' => 'eine Minute',
+	'une_seconde' => 'eine Sekunde',
 	'url_site' => 'URL der Website',
 	'url_societe' => 'Website (der Firma/Verein)',
 
@@ -302,7 +299,7 @@ Wenn Sie sich nicht angemeldet haben, klicken Sie bitte auf den Link weiter unte
 	'vos_contacts_personnels' => 'Ihre pers&ouml;nlichen Kontakte',
 	'votre_adresse' => 'Ihre pers&ouml;nliche Adresse',
 	'votre_adresse_pro' => 'Ihre berufliche Adresse',
-	'votre_login_mail' => 'Votre login ou email :', # NEW
+	'votre_login_mail' => 'Ihre E-Mail oder Login:',
 	'votre_mail' => 'Ihr E-Mail:',
 	'votre_nom_complet' => 'Ihr vollst&auml;ndiger Name',
 	'votre_profil' => 'Ihr Profil',
