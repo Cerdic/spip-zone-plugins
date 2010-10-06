0089ab2291b562f8314a914339da4cefb68a14ea
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-10-06 21:00:16 +0000

    Il manquait des traductions en anglais ici
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@41591 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- notifications/lang/notifications_en.php
+++ notifications/lang/notifications_en.php
@@ -10,6 +10,25 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'article_prive_admins_restreints' => '<strong>Administrators</strong>&nbsp;: Administrators get notified when article(s) is(are) proposed in their section',
 	'article_prive_auteurs' => '<strong>Authors</strong>&nbsp;: Authors get notified when their article(s) is(are) published',
 
+	'article_propose_sujet' => '[@nom_site_spip@] Submitted : @titre@',
+	'article_propose_titre' => 'Article submitted
+	---------------',
+	'article_propose_detail' => 'The article "@titre@" is submitted for publication.',
+	'article_propose_url' => 'You are invited to review it and to give your opinion
+	in the forum linked to it. It is available at the address :',
+	
+	'article_valide_sujet' => '[@nom_site_spip@] VALIDATED : @titre@',
+	'article_valide_titre' => 'Article validated
+	--------------',
+	'article_valide_detail' => 'The article "@titre@" is validated by @connect_nom@.',
+	'article_valide_date' => 'Without change, this article will be published',
+	'article_valide_url' => 'Meanwhile, it is visible at this temporary address :',
+	
+	'article_publie_sujet' => '[@nom_site_spip@] PUBLISHED: @titre@',
+	'article_publie_titre' => 'Article published
+	--------------',
+	'article_publie_detail' => 'The article "@titre@" was validated by @connect_nom@.',
+
 	// E
 	'evenement_notification' => 'Following events may generate email notifications.',
 
@@ -17,24 +36,35 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'forum_prives_auteur' => '<strong>Authors</strong>&nbsp;: Authors get notified when comments are posted to their article(s) or comment(s) on the private area.',
 	'forum_prives_moderateur' => 'Please write here moderators\' email adress for private forums, coma separated.',
 	'forum_prives_thread' => '<strong>Forum thread</strong>&nbsp;: Posters to the same thread get notified when a new comment is posted to the (private) thread.',
+	'forums_prives' => 'Forums in private area',
 	'forums_public' => 'Public forums',
 	'forums_public_a_noter' => 'Note : if forums are awaiting validation from moderators before publication, only authors with rights to validate forums get notified when the comment is posted ; other recipients get notified only when moderators validate the comment.',
 	'forums_public_auteurs' => '<strong>Authors</strong>&nbsp;: Authors get notified when new comments are posted to their article(s) on the public area.',
 	'forums_public_moderateur' => 'Please write here moderators\' email adress for public forums, coma separated.',
 	'forums_public_thread' => '<strong>Forum thread</strong>&nbsp;: Posters to the same thread get notified when a new comment is posted to the (public) thread.',
-	'forums_prives' => 'Forums in private area',
-	
+
+	// I
+	'inscription' => 'Editors registration',
+	'inscription_admins' => 'Administrators',
+	'inscription_explication' => 'Which authors receive notifications when registering new editors?',
+	'inscription_label' => 'Status',
+	'inscription_statut_aucun' => 'None',
+	'inscription_statut_webmestres' => 'Webmaster',
+
 	// L
-	'lien_documentation' => '<a href="http://www.spip-contrib.net/Notifications" class="spip_out">Cf. documentation</a>',
+	'lien_documentation' => '<a href="http://www.spip-contrib.net/Notifications" class="spip_out">View documentation</a>',
 
 	// M
 	'messagerie_interne' => 'Private messages',
 	'messagerie_interne_signaler' => '<strong>Notify new private messages</strong>&nbsp;: activate this to get redactors notified when they haven\'t seen a Private Message had been sent to them. Redactors get notified 20 minutes after the Private Message is sent, in order to avoid spam, when the redactor is connected to the private area.',
 	'moderateur' => '<strong>Moderator</strong>',
 
+	// N
+	'notifications' => 'Notifications',
+
 	// S
 	'signature_petition' => 'Petition signatures',
-	'signature_petition_moderateur' => 'Please write here moderators\' email adress for petitions, coma separated.',
+	'signature_petition_moderateur' => 'Please write here moderator\'s email address for petitions, coma separated.',
 	'suivis_perso' => 'Personnal notifications follow-up',
 	'suivis_perso_activer_option' => 'If you activate this option, each visitor clicking this follow-up URL will be registered in the <code>spip_auteurs</code> DB table, with status <code>6visiteur</code>. He\'ll be then able to view all the messages he posted on the website, configure his own notification options, ...',
 	'suivis_perso_non' => 'No follow-up',
--- notifications/lang/notifications_fr.php
+++ notifications/lang/notifications_fr.php
@@ -4,83 +4,82 @@
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-// A
-'alt_logo_conf' => 'Logo du plugin Notifications',
-'article_prive' => 'Publication d\'articles',
-'article_prive_admins_restreints' => '<strong>Administrateurs</strong>&nbsp;: Les administrateurs restreints re&#231;oivent les notifications lors de proposition d\'articles dans leur rubrique',
-'article_prive_auteurs' => '<strong>Auteurs</strong>&nbsp;: les auteurs re&#231;oivent les notifications lors de la publication de leur(s) article(s)',
-
-'article_propose_sujet' => '[@nom_site_spip@] Propose : @titre@',
-'article_propose_titre' => 'Article propos&eacute;
----------------',
-'article_propose_detail' => 'L\'article "@titre@" est propos&eacute; &agrave; la publication
-depuis',
-'article_propose_url' => 'Vous &ecirc;tes invit&eacute; &agrave; venir le consulter et &agrave; donner votre opinion
-dans le forum qui lui est attach&eacute;. Il est disponible &agrave; l\'adresse :',
-
-'article_valide_sujet' => '[@nom_site_spip@] VALIDE : @titre@',
-'article_valide_titre' => 'Article valid&eacute;
---------------',
-'article_valide_detail' => 'L\'article "@titre@" a &eacute;t&eacute; valid&eacute; par @connect_nom@.',
-'article_valide_date' => 'Sous r&eacute;serve de changement, cet article sera publi&eacute;',
-'article_valide_url' => 'En attendant, il est visible &agrave; cette adresse temporaire&nbsp;:',
-
-'article_publie_sujet' => '[@nom_site_spip@] PUBLIE : @titre@',
-'article_publie_titre' => 'Article publi&eacute;
---------------',
-'article_publie_detail' => 'L\'article "@titre@" vient d\'&ecirc;tre publi&eacute; par @connect_nom@.',
-
-
-
-// E
-'evenement_notification' => 'Les &#233;v&#233;nements suivants peuvent d&#233;clencher une notification par email.',
-
-// F
-'forum_prives_auteur' => '<strong>Auteurs</strong>&nbsp;: les auteurs re&#231;oivent les notifications des forums post&#233;s sous leurs articles ou leurs messages dans le site priv&#233;.',
-'forum_prives_moderateur' => 'Indiquez ci-dessous l\'adresse email du mod&#233;rateur des forums priv&#233;s (ou plusieurs, s&#233;par&#233;s par des virgules).',
-'forum_prives_thread' => '<strong>Fil de discussion</strong>&nbsp;: les participants au m&#234;me fil de discussion re&#231;oivent les notifications des forums (priv&#233;s).',
-'forums_prives' => 'Forums priv&#233;s',
-'forums_public' => 'Forums publics',
-'forums_public_a_noter' => '&Agrave; noter : dans le cas des forums mod&#233;r&#233;s &#224; priori, seuls les auteurs ayant le droit de valider les forums sont notifi&#233;s lors de l\'envoi du forum ; les autres destinataires sont notifi&#233;s lors de la validation du message par le mod&#233;rateur.',
-'forums_public_auteurs' => '<strong>Auteurs</strong>&nbsp;: les auteurs re&#231;oivent les notifications des forums post&#233;s sous leurs articles dans le site public.',
-'forums_public_moderateur' => 'Indiquez ci-dessous l\'adresse email du mod&#233;rateur des forums publics (ou plusieurs, s&#233;par&#233;s par des virgules).',
-'forums_public_thread' => '<strong>Fil de discussion</strong>&nbsp;: les participants au m&#234;me fil de discussion re&#231;oivent les notifications des forums (publics).',
-
-// I
-'inscription' => 'Inscription des r&eacute;dacteurs',
-'inscription_admins' => 'Administrateurs',
-'inscription_explication' => 'Quels auteurs re&#231;oivent les notifications lors de l\'inscription de nouveaux rédacteurs ?',
-'inscription_label' => 'Statut',
-'inscription_statut_aucun' => 'Aucun',
-'inscription_statut_webmestres' => 'Webmestres',
-
-// L
-'lien_documentation' => '<a href="http://www.spip-contrib.net/Notifications" class="spip_out">Cf. documentation</a>',
-
-// M
-'messagerie_interne' => 'Messagerie interne',
-'messagerie_interne_signaler' => '<strong>Signaler les nouveaux messages priv&#233;s</strong>&nbsp;: activer cette option pour que le site envoie une notification lorsqu\'un r&#233;dacteur n\'a pas vu un nouveau message dans sa messagerie. Le syst&#232;me attend 20 minutes avant de notifier le r&#233;dacteur, de mani&#232;re &#224; ne pas spammer un r&#233;dacteur d&#233;j&#224; en ligne dans l\'espace priv&#233;.',
-'moderateur' => '<strong>Mod&#233;rateur</strong>',
-
-// N
-'notifications' => 'Notifications',
-
-// S
-'signature_petition' => 'Signatures de p&#233;tition',
-'signature_petition_moderateur' => 'Indiquez ci-dessous l\'adresse email du mod&#233;rateur des p&#233;titions (ou plusieurs, s&#233;par&#233;s par des virgules).',
-'suivis_perso' => 'Suivi personnalis&#233;',
-'suivis_perso_activer_option' => 'Si vous activez cette option, chaque visiteur qui se connecte sur cet URL de suivi sera enregistr&#233; dans la table <code>spip_auteurs</code>, avec le statut <code>6visiteur</code>. Il pourra alors voir l\'ensemble des messages qu\'il a sign&#233;s sur le forum, r&#233;gler ses options de notification, etc.',
-'suivis_perso_non' => 'Pas de suivi',
-'suivis_perso_oui' => 'Suivi activ&#233;',
-'suivis_perso_url_suivis' => '<strong>Ajouter une URL de suivi personnalis&#233;</strong> dans chacun des emails de notification. &#192; partir de cette URL, l\'utilisateur pourra configurer ses pr&#233;f&#233;rences individuelles de notification.',
-'suivis_public_article_thread' => 'TODO: case à cocher sur chaque article/thread',
-'suivis_public_changer_email' => 'TODO: changer d\'email',
-'suivis_public_description' => 'Vous pourrez (quand ce sera fonctionnel...) y retrouver tous vos messages de forum, obtenir un fil RSS des r&#233;ponses qui y seront apport&#233;es, choisir votre mode de notification, etc.',
-'suivis_public_info_email' => 'Adresse email',
-'suivis_public_notif_desactiver' => 'TODO: case à cocher pour ne plus recevoir de notifications',
-'suivis_public_vos_forums' => 'Vos forums',
-'suivis_public_vos_forums_date' => 'Vos forums, par date',
-'suivis_public_votre_page' => 'Ceci est votre page personnalis&#233;e de suivi du site', // #NOM_SITE_SPIP
+	// A
+	'alt_logo_conf' => 'Logo du plugin Notifications',
+	'article_prive' => 'Publication d\'articles',
+	'article_prive_admins_restreints' => '<strong>Administrateurs</strong>&nbsp;: Les administrateurs restreints re&#231;oivent les notifications lors de proposition d\'articles dans leur rubrique',
+	'article_prive_auteurs' => '<strong>Auteurs</strong>&nbsp;: les auteurs re&#231;oivent les notifications lors de la publication de leur(s) article(s)',
+	
+	'article_propose_sujet' => '[@nom_site_spip@] Propose : @titre@',
+	'article_propose_titre' => 'Article propos&eacute;
+	---------------',
+	'article_propose_detail' => 'L\'article "@titre@" est propos&eacute; &agrave; la publication
+	depuis',
+	'article_propose_url' => 'Vous &ecirc;tes invit&eacute; &agrave; venir le consulter et &agrave; donner votre opinion
+	dans le forum qui lui est attach&eacute;. Il est disponible &agrave; l\'adresse :',
+	
+	'article_valide_sujet' => '[@nom_site_spip@] VALIDE : @titre@',
+	'article_valide_titre' => 'Article valid&eacute;
+	--------------',
+	'article_valide_detail' => 'L\'article "@titre@" a &eacute;t&eacute; valid&eacute; par @connect_nom@.',
+	'article_valide_date' => 'Sous r&eacute;serve de changement, cet article sera publi&eacute;',
+	'article_valide_url' => 'En attendant, il est visible &agrave; cette adresse temporaire&nbsp;:',
+	
+	'article_publie_sujet' => '[@nom_site_spip@] PUBLIE : @titre@',
+	'article_publie_titre' => 'Article publi&eacute;
+	--------------',
+	'article_publie_detail' => 'L\'article "@titre@" vient d\'&ecirc;tre publi&eacute; par @connect_nom@.',
+	
+	
+	
+	// E
+	'evenement_notification' => 'Les &#233;v&#233;nements suivants peuvent d&#233;clencher une notification par email.',
+	
+	// F
+	'forum_prives_auteur' => '<strong>Auteurs</strong>&nbsp;: les auteurs re&#231;oivent les notifications des forums post&#233;s sous leurs articles ou leurs messages dans le site priv&#233;.',
+	'forum_prives_moderateur' => 'Indiquez ci-dessous l\'adresse email du mod&#233;rateur des forums priv&#233;s (ou plusieurs, s&#233;par&#233;s par des virgules).',
+	'forum_prives_thread' => '<strong>Fil de discussion</strong>&nbsp;: les participants au m&#234;me fil de discussion re&#231;oivent les notifications des forums (priv&#233;s).',
+	'forums_prives' => 'Forums priv&#233;s',
+	'forums_public' => 'Forums publics',
+	'forums_public_a_noter' => '&Agrave; noter : dans le cas des forums mod&#233;r&#233;s &#224; priori, seuls les auteurs ayant le droit de valider les forums sont notifi&#233;s lors de l\'envoi du forum ; les autres destinataires sont notifi&#233;s lors de la validation du message par le mod&#233;rateur.',
+	'forums_public_auteurs' => '<strong>Auteurs</strong>&nbsp;: les auteurs re&#231;oivent les notifications des forums post&#233;s sous leurs articles dans le site public.',
+	'forums_public_moderateur' => 'Indiquez ci-dessous l\'adresse email du mod&#233;rateur des forums publics (ou plusieurs, s&#233;par&#233;s par des virgules).',
+	'forums_public_thread' => '<strong>Fil de discussion</strong>&nbsp;: les participants au m&#234;me fil de discussion re&#231;oivent les notifications des forums (publics).',
+	
+	// I
+	'inscription' => 'Inscription des r&eacute;dacteurs',
+	'inscription_admins' => 'Administrateurs',
+	'inscription_explication' => 'Quels auteurs re&#231;oivent les notifications lors de l\'inscription de nouveaux rédacteurs ?',
+	'inscription_label' => 'Statut',
+	'inscription_statut_aucun' => 'Aucun',
+	'inscription_statut_webmestres' => 'Webmestres',
+	
+	// L
+	'lien_documentation' => '<a href="http://www.spip-contrib.net/Notifications" class="spip_out">Cf. documentation</a>',
+	
+	// M
+	'messagerie_interne' => 'Messagerie interne',
+	'messagerie_interne_signaler' => '<strong>Signaler les nouveaux messages priv&#233;s</strong>&nbsp;: activer cette option pour que le site envoie une notification lorsqu\'un r&#233;dacteur n\'a pas vu un nouveau message dans sa messagerie. Le syst&#232;me attend 20 minutes avant de notifier le r&#233;dacteur, de mani&#232;re &#224; ne pas spammer un r&#233;dacteur d&#233;j&#224; en ligne dans l\'espace priv&#233;.',
+	'moderateur' => '<strong>Mod&#233;rateur</strong>',
+	
+	// N
+	'notifications' => 'Notifications',
+	
+	// S
+	'signature_petition' => 'Signatures de p&#233;tition',
+	'signature_petition_moderateur' => 'Indiquez ci-dessous l\'adresse email du mod&#233;rateur des p&#233;titions (ou plusieurs, s&#233;par&#233;s par des virgules).',
+	'suivis_perso' => 'Suivi personnalis&#233;',
+	'suivis_perso_activer_option' => 'Si vous activez cette option, chaque visiteur qui se connecte sur cet URL de suivi sera enregistr&#233; dans la table <code>spip_auteurs</code>, avec le statut <code>6visiteur</code>. Il pourra alors voir l\'ensemble des messages qu\'il a sign&#233;s sur le forum, r&#233;gler ses options de notification, etc.',
+	'suivis_perso_non' => 'Pas de suivi',
+	'suivis_perso_oui' => 'Suivi activ&#233;',
+	'suivis_perso_url_suivis' => '<strong>Ajouter une URL de suivi personnalis&#233;</strong> dans chacun des emails de notification. &#192; partir de cette URL, l\'utilisateur pourra configurer ses pr&#233;f&#233;rences individuelles de notification.',
+	'suivis_public_article_thread' => 'TODO: case à cocher sur chaque article/thread',
+	'suivis_public_changer_email' => 'TODO: changer d\'email',
+	'suivis_public_description' => 'Vous pourrez (quand ce sera fonctionnel...) y retrouver tous vos messages de forum, obtenir un fil RSS des r&#233;ponses qui y seront apport&#233;es, choisir votre mode de notification, etc.',
+	'suivis_public_notif_desactiver' => 'TODO: case à cocher pour ne plus recevoir de notifications',
+	'suivis_public_vos_forums' => 'Vos forums',
+	'suivis_public_vos_forums_date' => 'Vos forums, par date',
+	'suivis_public_votre_page' => 'Ceci est votre page personnalis&#233;e de suivi du site', // #NOM_SITE_SPIP
 
 // Mails de suivis
 // à venir
