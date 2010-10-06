f988b54ea2b3472bcb8d8f9b102cd8d5ecb92977
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-10-06 22:41:18 +0000

    Une traduction anglophone
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@41597 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- zen-garden/lang/zengarden_fr.php
+++ zen-garden/lang/zengarden_fr.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/_stable_/acces_restreint/lang/
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/_stable_/zen-garden/lang
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
