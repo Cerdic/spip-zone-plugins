cafaf691807ec36922bcba4fbc33c5f778b7abbf
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-10-06 17:52:05 +0000

    Une version anglophone
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@41577 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ opensearch/lang/opensearch_en.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	//C
+	'cfg_boite_opensearch' => 'OpenSearch plugin configuration',
+	'cfg_descr_opensearch' => 'OpenSearch : custom search engine for your website.',
+	'cfg_inf_id_rubrique' => 'Select the section in which to search.',
+	'cfg_inf_page_recherche' => 'Specify the skeleton to use for the site search (exemple : look for the recherche.html skeleton)',
+	'cfg_lbl_id_rubrique' => 'Target section',
+	'cfg_lbl_page_recherche' => 'Search skeleton',
+	'cfg_option_tout' => 'the whole website',
+	'cfg_titre_opensearch' => 'OpenSearch'
+);
+?>
