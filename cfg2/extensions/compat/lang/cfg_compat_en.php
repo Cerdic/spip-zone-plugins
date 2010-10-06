3c3859b8b3aa0e3c3a866f65277679f0a6cf27ac
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-10-06 20:00:21 +0000

    Traduction en anglais
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@41585 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ cfg2/extensions/compat/lang/cfg_compat_en.php
@@ -0,0 +1,19 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/_stable_/cfg/lang/
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// E
+	'erreur_copie_fichier' => 'Impossible to copy the file @fichier@ to its final location',
+	'erreur_suppression_fichier' => 'Impossible to delete the file @fichier@.',
+	'erreur_type_id' => 'The @champ@ field must begin with a letter or an underscore',
+	'erreur_type_idnum' => 'The @champ@ field should be digits',
+	'erreur_type_pwd' => 'The @champ@ field  must be over 5 characters',
+
+	// N
+	'nouveau' => 'New',
+);
+
+?>
