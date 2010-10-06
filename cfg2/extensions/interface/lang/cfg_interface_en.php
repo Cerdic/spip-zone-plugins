dde51eb0a97765122cceb2d32fe70cf5a6c20431
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-10-06 20:17:33 +0000

    Une trad anglophone encore
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@41587 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ cfg2/extensions/interface/lang/cfg_interface_en.php
@@ -0,0 +1,21 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/_stable_/cfg/lang/
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'configuration_cfg' => 'Extensions configuration',
+	'choisir_module_a_configurer' => 'Choose the module to configure.',
+	'configuration_modules' => 'Modules configuration',
+	'configurer' => 'Configure',
+
+	// D
+	'description_cfg' => 'CFG facilitates the creation of configuration interface for the extensions of SPIP.',
+	
+	// E
+	'erreur_compatibilite' => 'Please install the compatibility plugin (cfg_compat) to read this old configuration format of CFG.',
+);
+
+?>
