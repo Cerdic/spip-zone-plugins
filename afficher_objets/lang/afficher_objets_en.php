45df71c4f1dee89b97a41cc481759138b27d114d
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-10-07 01:49:36 +0000

    Traduction anglophone
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@41602 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ afficher_objets/lang/afficher_objets_en.php
@@ -0,0 +1,11 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/afficher_objets
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	'info_statut'=>'Status',
+	'nb_articles' => '@nb@ articles',
+);
+
+?>
