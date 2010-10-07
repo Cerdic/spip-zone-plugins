2fb63ff96cb5dc165f846812c600f73f2d5d12e4
yffic@lefourneau.com <yffic@lefourneau.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-10-07 15:21:00 +0000

    Mise à jour pour utilisation de INSERT_HEAD_CSS
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@41616 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- menu_babbibel/tags/stable/fonctions_menu.php
+++ menu_babbibel/tags/stable/fonctions_menu.php
@@ -1,8 +1,17 @@
 <?php
 
+function babbi_headeur_css($flux){
+	static $done = false;
+	if (!$done) {
+		$done = true;
+		$flux .= '<link rel="stylesheet" href="'.find_in_path('menu_babbi.css').'" type="text/css" media="projection, screen" />';
+	}
+	return $flux;
+}
+
 function babbi_headeur($flux){
 	$flux .= "<script type='text/javascript' src='".find_in_path('js/menu_babbi.js')."'></script>\n";
-	$flux .= '<link rel="stylesheet" href="'.find_in_path('menu_babbi.css').'" type="text/css" media="projection, screen" />';
+	$flux .= babbi_headeur_css(''); // compat pour les vieux spip
 	return $flux;
 }
 ?>
