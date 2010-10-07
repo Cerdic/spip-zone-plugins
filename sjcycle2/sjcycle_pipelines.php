28dd166cb666ea596e61af580153ae41ead3116d
yffic@lefourneau.com <yffic@lefourneau.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-10-07 13:28:30 +0000

    Modification pour utilisation du insert_head_css
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@41615 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- sjcycle2/sjcycle_pipelines.php
+++ sjcycle2/sjcycle_pipelines.php
@@ -1,14 +1,15 @@
 <?php
 
-function sjcycle_insert_head($flux){
-	$conf_jcycle = lire_config('sjcycle');
-	$flux .="\n".'<script src="'.url_absolue(find_in_path('javascript/jquery.cycle.all.min.js')).'" type="text/javascript"></script>';
-	if($conf_jcycle["tooltip"]) {
-		$flux .="\n".'<link rel="stylesheet" href="'.url_absolue(find_in_path('javascript/jquery.tooltip.css')).'" type="text/css" media="all" />';
-		$flux .="\n".'<script src="'.url_absolue(find_in_path('javascript/jquery.tooltip.js')).'" type="text/javascript" charset="utf-8"></script>';
-	}
-	/* options */
-	$flux .= "\n".'<style type="text/css" media="all">
+function sjcycle_insert_head_css($flux){
+	static $done = false;
+	if (!$done) {
+		$done = true;
+		$conf_jcycle = lire_config('sjcycle');
+		if($conf_jcycle["tooltip"]) {
+			$flux .="\n".'<link rel="stylesheet" href="'.url_absolue(find_in_path('javascript/jquery.tooltip.css')).'" type="text/css" media="all" />';
+		}
+		
+		$flux .= "\n".'<style type="text/css" media="all">
 .'.$conf_jcycle["div_class"].'{
 padding:0px;
 margin:auto;
@@ -17,7 +18,19 @@ clear:both;
 }
 </style>
 '; 
-	
+	}
+	return $flux;
+}
+
+function sjcycle_insert_head($flux){
+	$conf_jcycle = lire_config('sjcycle');
+	$flux .="\n".'<script src="'.url_absolue(find_in_path('javascript/jquery.cycle.all.min.js')).'" type="text/javascript"></script>';
+	if($conf_jcycle["tooltip"]) {
+		$flux .="\n".'<script src="'.url_absolue(find_in_path('javascript/jquery.tooltip.js')).'" type="text/javascript" charset="utf-8"></script>';
+	}
+
+	$flux .= sjcycle_insert_head_css(''); // compat pour les vieux spip
+
 	return $flux;
 }
 
