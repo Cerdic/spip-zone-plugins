f222f2d3d6250eb79e81bb1c7e3f81d8f8a0a398
Alex.Gomes@ituc-csi.org <Alex.Gomes@ituc-csi.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-10-11 14:56:21 +0000

    Ajout initial de Speedsyndic2 sur la zone
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@41688 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ speedsyndic2/lang/speedsyndic_en.php
@@ -0,0 +1,13 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+'erreur' => 'Error: ',
+'frequence' => 'Refresh interval: (in seconds)',
+'frequence_num' => 'Please enter a numberical value for the refresh interval.',
+'frequence_trente' =>  'The refresh interval cannot be inferior to 30.',
+'syndiclist' => 'Sites to speedsyndicate: ',
+);
+
+?>
