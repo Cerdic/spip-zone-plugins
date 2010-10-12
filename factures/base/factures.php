27f620c86eb25f899aea988adb057258177c8b82
cyril@cym.fr <cyril@cym.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-10-12 16:18:31 +0000

    premier jet de création des tables...
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@41713 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- factures/base/factures.php
+++ factures/base/factures.php
@@ -6,5 +6,91 @@
  */
 
 
+if (!defined("_ECRIRE_INC_VERSION")) return;
 
+/***********************************************************************************/
+/* DECLARATION DES TABLES INTERFACE
+/***********************************************************************************/
+function factures_declarer_tables_interfaces($interface){
+	
+	$interface['table_des_tables']['factures'] = 'factures';
+	$interface['table_des_tables']['lignes_factures'] = 'numeros';
+
+	// -- Liaisons
+	$interface['tables_jointures']['spip_factures'][]= 'organisations';
+	$interface['tables_jointures']['spip_organisations'][]= 'factures';
+
+	// gerer le critere de date
+	$interface['table_date']['factures'] = 'date_facture';
+	$interface['table_date']['factures'] = 'date_payement';
+	$interface['table_date']['factures'] = 'fin_validite';
+
+	// titre
+	$interface['table_titre']['contacts'] = "CONCAT(nom,' ',prenom) AS titre, '' AS lang";
+	$interface['table_titre']['organisations'] = "nom AS titre, '' AS lang";
+
+	return $interface;
+}
+
+/***********************************************************************************/
+/* DECLARATION DES TABLES PRINCIPALES
+/***********************************************************************************/
+function factures_declarer_tables_principales($tables_principales){
+
+	// structure de la table factures
+	$factures = array(
+		"id_facture"			=>	"int(11) NOT NULL auto_increment",
+		"num_facture"			=>	"varchar(50) default NULL",
+		"num_devis"				=>	"varchar(50) default NULL",
+		"id_type_document"		=>	"int(11) default NULL",
+		"delais_validite"		=>	"int(11) default NULL",
+		"date_facture"			=>	"DATETIME NULL default NULL",
+		"date_payement"			=>	"DATETIME NULL NULL",
+		"reglement"				=>	"varchar(50) default NULL",
+		"fin_validite"			=>	"DATETIME NULL default NULL",
+		"id_organisation"		=>	"int(11) default NULL",
+		"id_projet"				=>	"smallint(6) default NULL",
+		"nom_client"			=>	"varchar(50) default NULL",
+		"id_type_presta"		=>	"int(11) default NULL",
+		"montant"				=>	"decimal(18,2) default NULL",
+		"charge_estimee"		=>	"float default NULL",
+		"delais"				=>	"varchar(50) default NULL",
+		"nb_heures_vendues"		=>	"decimal(18,2) default NULL",
+		"libelle_facture"		=>	"mediumtext",
+		"libelle_forfait"		=>	"mediumtext",
+		"nota_bene"				=>	"mediumtext"
+	);
+	$factures_key = array(
+		"PRIMARY KEY"			=>	"id_facture",
+		"KEY id_projet"			=>	"id_projet",
+		"KEY id_organisation"	=>	"id_organsiation",
+		"KEY date_facture"		=>	"date_facture",
+		"KEY num_facture"		=>	"num_facture"
+	);
+	$tables_principales['spip_factures'] = array(
+		'field' => &$factures,
+		'key' => &$factures_key
+	);
+	
+	// structure de la table factures
+	$lignes_factures = array(
+		"id_ligne"				=>	"int(11) NOT NULL auto_increment",
+		"id_facture"			=>	"int(11) default NULL",
+		"position"				=>	"int(11) default NULL",
+		"quantite"				=>	"float default NULL",
+		"unite"					=>	"varchar(50) default NULL",
+		"designation"			=>	"text",
+		"prix unitaires_ht"		=>	"decimal(18,2) default NULL",
+		"commentaire"			=>	"mediumtext"
+	);
+	$lignes_factures_key = array(
+		"PRIMARY KEY"			=>	"id_ligne",
+		"KEY id_facture"		=>	"id_facture"
+	);
+	$tables_principales['spip_lignes_facture'] = array(
+		'field' => &$lignes_factures,
+		'key' => &$lignes_factures_key
+	);
+
+}
 ?>
--- factures/base/factures_upgrade.php
+++ factures/base/factures_upgrade.php
@@ -5,6 +5,33 @@
  * par Cyril Marion - Camille Lafitte
  */
 
+function factures_upgrade($nom_meta_base_version, $version_cible){
+	include_spip('inc/meta');
+	
+	/**
+	 *
+	 *  11/10/2010 : creation
+	 *
+	 */
+	 
+	$current_version = "0.0";
+	if (isset($GLOBALS['meta'][$nom_meta_base_version]))
+		$current_version = $GLOBALS['meta'][$nom_meta_base_version];
+	
+	if ($current_version=="0.0") {
+		include_spip('base/create');
+		creer_base();
+		ecrire_meta($nom_meta_base_version, $current_version=$version_cible);
+	}
 
+}
+
+function factures_vider_tables($nom_meta_base_version) {
+	
+	sql_drop_table("spip_factures");
+	sql_drop_table("spip_lignes_factures");
+
+	effacer_meta($nom_meta_base_version);
+}
 
 ?>
