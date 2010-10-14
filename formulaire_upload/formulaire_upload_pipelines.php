78bb90e9692164d0b986485cfc0abf405a70f302
toutati@free.fr <toutati@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-10-14 13:07:02 +0000

    un @media print avec un all sur la css
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@41738 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- formulaire_upload/formulaire_upload_pipelines.php
+++ formulaire_upload/formulaire_upload_pipelines.php
@@ -2,7 +2,7 @@
 
 function FormulaireUpload_insert_head($flux){
 	$flux .='<script src="'._DIR_PLUGIN_FORMULAIREUPLOAD.'javascript/jquery.MultiFile.js" type="text/javascript"></script>';
-	$flux .= '<link rel="stylesheet" href="'._DIR_PLUGIN_FORMULAIREUPLOAD.'formulaire_upload.css" type="text/css" media="projection, screen, tv" />';
+	$flux .= '<link rel="stylesheet" href="'._DIR_PLUGIN_FORMULAIREUPLOAD.'formulaire_upload.css" type="text/css" media="all" />';
 
 	return $flux;
 }
