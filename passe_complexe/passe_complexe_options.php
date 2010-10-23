a9fd4dfe9651f3b49519efea8914ad4453a3f40f
brunobergot@gmail.com <brunobergot@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-10-23 15:15:24 +0000

    compat jquery
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@41881 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- passe_complexe/passe_complexe_options.php
+++ passe_complexe/passe_complexe_options.php
@@ -4,7 +4,7 @@
 function passe_complexe_header_prive($flux){
   if (_request('exec')=='auteur_infos'){
 	include_spip('inc/passe_complexe');
-	$flux .= passe_complexe_generer_javascript("input[@name=new_pass]");
+	$flux .= passe_complexe_generer_javascript("input[name=new_pass]");
   } else if (_request('exec')=='cfg'){
 	include_spip('inc/passe_complexe');
 	$flux .= passe_complexe_generer_javascript("input.type_pwd");
