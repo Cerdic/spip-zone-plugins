ce728a5bc7c1f5f55ccc19e3484c5b1bd9f27600
pierre.fiches@gmail.com <pierre.fiches@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-10-26 14:00:32 +0000

    Palette à configurer indifféremment avec cfg ou spip-bonux
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@41923 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- palette/palette_install.php
+++ palette/palette_install.php
@@ -11,6 +11,11 @@ function palette_upgrade($nom_meta_base_version,$version_cible){
 				ecrire_config('palette/palette_public','');
 			if(is_null(lire_config('palette/palette_ecrire')))
 				ecrire_config('palette/palette_ecrire','on');
+		}else{
+				$config = @unserialize($GLOBALS['meta']['palette']);
+				if (!is_array($config))
+					$config = 'a:2:{s:14:"palette_public";N;s:14:"palette_ecrire";s:2:"on";}';
+					ecrire_meta('palette', $config);		
 		}
 		ecrire_meta($nom_meta_base_version,$current_version=$version_cible, 'non');
 	}
@@ -19,6 +24,8 @@ function palette_upgrade($nom_meta_base_version,$version_cible){
 function palette_vider_tables($nom_meta_base_version) {
 	if (function_exists('effacer_config')){
 		effacer_config('palette');
+	}else{
+		effacer_meta('palette');
 	}
 	effacer_meta($nom_meta_base_version);
 }
