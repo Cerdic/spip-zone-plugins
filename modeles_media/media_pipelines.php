10522c000750d48aa55f182501069564e432ed30
joseph@larmarange.net <joseph@larmarange.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-10-26 12:10:09 +0000

    modification des tailles par défaut
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@41919 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- modeles_media/media_pipelines.php
+++ modeles_media/media_pipelines.php
@@ -12,12 +12,12 @@ function media_affiche_milieu($flux){
 function media_configurer_liste_metas($metas){
 	$metas['media_taille_icone_largeur'] = 52;
 	$metas['media_taille_icone_hauteur'] = 52;
-	$metas['media_taille_petit_largeur'] = 100;
-	$metas['media_taille_petit_hauteur'] = 100;
-	$metas['media_taille_moyen_largeur'] = 250;
-	$metas['media_taille_moyen_hauteur'] = 250;
-	$metas['media_taille_grand_largeur'] = 500;
-	$metas['media_taille_grand_hauteur'] = 500;
+	$metas['media_taille_petit_largeur'] = 120;
+	$metas['media_taille_petit_hauteur'] = 90;
+	$metas['media_taille_moyen_largeur'] = 320;
+	$metas['media_taille_moyen_hauteur'] = 240;
+	$metas['media_taille_grand_largeur'] = 640;
+	$metas['media_taille_grand_hauteur'] = 480;
 	$metas['media_largeur_min_legende'] = 120;
 	$metas['media_largeur_max_legende'] = 350;
 	return $metas;
