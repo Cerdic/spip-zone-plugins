07dbfc35b64469e66a20f8409f6b92817cfa2ba5
fredlefred@gmail.com <fredlefred@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-11-03 16:54:21 +0000

    erreur dans l'implementation du filtre |qrcode ajout de parametre optionnels : qrcode{taille} et qrcode{taille,ecc}
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@42051 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- qrcode/qrcode_fonctions.php
+++ qrcode/qrcode_fonctions.php
@@ -27,12 +27,4 @@ $().ready(function() {
 	return $flux ;
 }
 
-function qrcode($texte) {
-	( $taille = lire_config('qrcode/taille') ) || ( $taille = 1 ) ;
-	( $ecc = lire_config('qrcode/ecc') ) || ( $ecc = 'L' ) ;
-	if ($class = lire_config('qrcode/css')) { $class = ' class="'.$class.'"' ; }
-	if ($style = lire_config('qrcode/style')) { $style = ' style="'.$style.'"' ; }
-	return "<img$class$style src=\""._DIR_RACINE."?page=qrcode&data=".urlencode($texte)."&size=$taille&level=$ecc\" alt=\"qrcode:$texte\" title=\""._T('qrcode:aide')."\"/>" ;
-}
-
 ?>
