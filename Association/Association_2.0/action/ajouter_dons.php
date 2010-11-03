2bde95f45195d8333c001be3b4a46dcd5cab0587
esj@rezo.net <esj@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-11-03 15:41:25 +0000

    [source:_plugins_/Association/Association_2_0 Association]: un paramètre pas encore utile, mais l'ignorer est trompeur.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@42047 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- Association/Association_2.0/action/ajouter_dons.php
+++ Association/Association_2.0/action/ajouter_dons.php
@@ -29,7 +29,7 @@ function action_ajouter_dons() {
 	don_insert($id_adherent, $date_don, $argent, $bienfaiteur, $valeur, $journal, $contrepartie, $colis, $commentaire);
 }
 
-function don_insert($id_adherent, $date_don, $argent, $bienfaiteur='', $valeur='', $journal='', $contrepartie='', $colis='', $commentaire='', $valide=0)
+function don_insert($id_adherent, $date_don, $argent, $bienfaiteur='', $valeur='', $journal='', $contrepartie='', $colis='', $commentaire='', $vu=0)
 {
 	include_spip('base/association');		
 	$id_adherent = intval($id_adherent);
@@ -53,7 +53,7 @@ function don_insert($id_adherent, $date_don, $argent, $bienfaiteur='', $valeur='
 
 	sql_insertq('spip_asso_comptes', array(
 		    'date' => $date,
-		    'vu' => 0,
+		    'vu' => $vu,
 		    'imputation' => $GLOBALS['association_metas']['pc_dons'],
 		    'recette' => $argent,
 		    'journal' => $journal,
