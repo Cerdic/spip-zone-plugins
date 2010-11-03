aae26d8a4bf41c12f359d3862f89be90299e3719
fredlefred@gmail.com <fredlefred@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-11-03 12:32:27 +0000

    utilisation des qrcode dans spip import initial
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@42042 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ qrcode/lang/qrcode_fr.php
@@ -0,0 +1,22 @@
+<?php
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	'aide' => 'Utilisez votre mobile pour scanner ce code barre et y télécharger le document',
+	'css' => 'Classe CSS : ',
+	'css_aide' => 'Tous les qrcodes insérés auront cette classe',
+	'descriptif' => 'Les QRcodes sont des codes barres lisibles par divers appareils et en particulier par les téléphones portables. Ils
+peuvent contenir diverses informations par exemple : des URL permettant de télécharger des fichiers sans avoir
+à saisir manuellement l\'adresse de téléchargement.',
+	'ecc' => 'Correction d\'erreurs :',
+	'ecc_aide' => 'Ce pourcentage indique combien d\'erreurs peuvent être récupérées en cas d\'erreur de lecture du QRcode',
+	'instructions' => 'Vous pouvez configurer comment les QRcodes apparaîtront dans vos articles SPIP.',
+	'options' => 'Options :',
+	'style' => 'Style CSS :',
+	'style_aide' => 'Tous les qrcodes insérés auront ce style',
+	'taille' => 'Taille :',
+	'taille_aide' => 'Chaque élément du QRcode fera la taille spécifiée.',
+	'utiliser_pour_documents_joints' => 'Utiliser les QRcode pour permettre les téléchargements des documents joints'
+
+);
+
+?>
---
+++ qrcode/qrcode_fonctions.php
@@ -0,0 +1,38 @@
+<?php
+
+
+function qrcode_insert_head($flux) {
+	if (lire_config("qrcode/documents")) {
+		( $taille = lire_config('qrcode/taille') ) || ( $taille = 1 ) ;
+		( $ecc = lire_config('qrcode/ecc') ) || ( $ecc = 'L' ) ;
+		if ($class = lire_config('qrcode/css')) { $class = ' class="'.$class.'"' ; }
+		if ($style = lire_config('qrcode/style')) { $style = ' style="'.$style.'"' ; }
+		$flux .= "<script type='text/javascript'>
+var url_site_spip = '".$GLOBALS['meta']['adresse_site']."' ;
+
+$().ready(function() {
+	$('.documents_joints a').each(function(ndx,item) {
+		var re = new RegExp('^(https?|ftp)://') ;
+		var url = $(this).attr('href') ;
+		if (!re.test(url)) {
+			url = url_site_spip + url ;
+		}
+		$(this).parent().prepend('<img$class$style src=\""._DIR_RACINE."?page=qrcode&data='+encodeURIComponent(url)+'&size=$taille&level=$ecc\" alt=\"qrcode:'+url+'\" title=\""._T('qrcode:aide')."\"/>') ;
+	}) ;
+}) ;
+
+</script>
+" ;
+	}
+	return $flux ;
+}
+
+function qrcode($texte) {
+	( $taille = lire_config('qrcode/taille') ) || ( $taille = 1 ) ;
+	( $ecc = lire_config('qrcode/ecc') ) || ( $ecc = 'L' ) ;
+	if ($class = lire_config('qrcode/css')) { $class = ' class="'.$class.'"' ; }
+	if ($style = lire_config('qrcode/style')) { $style = ' style="'.$style.'"' ; }
+	return "<img$class$style src=\""._DIR_RACINE."?page=qrcode&data=".urlencode($texte)."&size=$taille&level=$ecc\" alt=\"qrcode:$texte\" title=\""._T('qrcode:aide')."\"/>" ;
+}
+
+?>
