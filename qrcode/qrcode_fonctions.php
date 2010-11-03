0e170011beab38869e7f4889afe4dec4daf372a1
fredlefred@gmail.com <fredlefred@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-11-03 20:49:06 +0000

    erreur d'implementatiopn
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@42057 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- qrcode/qrcode_fonctions.php
+++ qrcode/qrcode_fonctions.php
@@ -1,6 +1,5 @@
 <?php
 
-
 function qrcode_insert_head($flux) {
 	if (lire_config("qrcode/documents")) {
 		( $taille = lire_config('qrcode/taille') ) || ( $taille = 1 ) ;
@@ -25,6 +24,14 @@ $().ready(function() {
 " ;
 	}
 	return $flux ;
+}
+
+function qrcode($texte,$taille=false,$ecc=false) {
+	$taille || ( $taille = lire_config('qrcode/taille') ) || ( $taille = 1 ) ;
+	$ecc || ( $ecc = lire_config('qrcode/ecc') ) || ( $ecc = 'L' ) ;
+	if ($class = lire_config('qrcode/css')) { $class = ' class="'.$class.'"' ; }
+	if ($style = lire_config('qrcode/style')) { $style = ' style="'.$style.'"' ; }
+	return "<img$class$style src=\""._DIR_RACINE."?page=qrcode&data=".urlencode($texte)."&size=$taille&level=$ecc\" alt=\"qrcode:$texte\" title=\""._T('qrcode:aide')."\"/>" ;
 }
 
 ?>
