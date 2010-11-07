f8c617b6c3bf5b223ea1771d9139301b7a7dc217
da@weeno.net <da@weeno.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-11-07 07:17:40 +0000

    Traduction en anglais et espagnol
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@42121 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ date_inscription/lang/date_inscription_en.php
@@ -0,0 +1,13 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+'date_inscription' => 'Registration date',
+
+'non_renseignee' => 'not indicated'
+
+);
+
+?>
---
+++ date_inscription/lang/date_inscription_es.php
@@ -0,0 +1,13 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+'date_inscription' => 'Fecha de inscripci&oacute;n',
+
+'non_renseignee' => 'no indicada'
+
+);
+
+?>
