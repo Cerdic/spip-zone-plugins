8dc89a0b67451faa1f00e6d4c13d8cb20e593d68
da@weeno.net <da@weeno.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-11-08 02:41:34 +0000

    Ajout du plugin Bitcoin
    Premier jet du plugin Bitcoin. Actuellement le plugin fournit uniquement
    un champ "adresse_bitcoin" dans la table auteurs, mais il serait peut-être
    bien de fournir un modèle "Accepte les dons Bitcoin" ou quelque chose comme
    ça. Si quelqu'un a des idées de trucs pratiques...
    
    
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@42159 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ bitcoin/lang/bitcoin_es.php
@@ -0,0 +1,5 @@
+<?php
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	'adresse_bitcoin' => 'Direcci&oacute;n de pago de Bitcoin',
+	'adresse_bitcoin_precisions' => 'Es una cadena de car&aacute;cteres que representa la direcci&oacute;n a la cual le pueden hacer un pago v&iacute;a Bitcoin. Ejemplo: 1LA71x9eeoqJwhnWkXRSSiwL6oqTMM4wPE',
+);
