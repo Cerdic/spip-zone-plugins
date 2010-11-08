8d99bcf74266d75246db8d758b4dd6da55c26d09
da@weeno.net <da@weeno.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-11-08 02:36:58 +0000

    Ahem. On va la refaire, en moins crispée.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@42158 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- inscription2/bitcoin/lang/bitcoin_en.php
+++
@@ -1,5 +0,0 @@
-<?php
-$GLOBALS[$GLOBALS['idx_lang']] = array(
-	'adresse_bitcoin' => 'Bitcoin payment address',
-	'adresse_bitcoin_precisions' => 'This is a string that represents the address where people may send you a payment using Bitcoin. Example: 1LA71x9eeoqJwhnWkXRSSiwL6oqTMM4wPE',
-);
