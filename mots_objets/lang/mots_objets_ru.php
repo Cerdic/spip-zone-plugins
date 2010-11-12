a5851bdbaa322ddb0febdbafb6e755f34bd71404
serge.markitanenko@gmail.com <serge.markitanenko@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-11-12 16:43:07 +0000

    Russian language
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@42236 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ mots_objets/lang/mots_objets_ru.php
@@ -0,0 +1,34 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// I
+	'icone_creer_mot_cle_auteur' => 'Создать новый кейворд и добавить к этому автору',
+	'icone_creer_mot_cle_document' => 'Создать новый кейворд и добавить к этому документу',
+
+	'info_auteurs' => 'Автора',
+	'info_nombre_auteurs' => '@nb@ автора(ов)',
+
+	'info_documents' => 'Документы',
+	'info_nombre_documents' => '@nb@ документа(ов)',
+
+	'info_un_auteur' => '1 автор',
+	'info_un_document' => '1 документ',
+
+	'item_mots_cles_association_auteurs' => 'к автору',
+	'item_mots_cles_association_documents' => 'к документу',
+		
+	// L
+	'liste_mots' => 'Перень кейвордов, доступных для этого автора.',
+
+	// O
+	'objet_auteurs' => 'Документы',
+
+	// T
+	'titre_mots' => 'Кейворды;',
+	
+);
+
+?>
