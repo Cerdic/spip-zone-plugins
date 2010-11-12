482473de5a30dadb4c4f31fec1bf2e7feac7cf0e
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-11-12 17:27:00 +0000

    Halte a l'usine a gaz ! r35159 a introduit un formulaire de config et une feuille de style dynamique dispendieuse quand il suffisant de faire 3 css statiques et le choix dans le pipeline. Egalement, renommer le formulaire de configuration pour fonctionner sans cfg, et utiliser le pipeline insert_head_css
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@42240 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- mesfavoris/mesfavoris_pipelines.php
+++ mesfavoris/mesfavoris_pipelines.php
@@ -1,7 +1,14 @@
 <?php
 
-function mesfavoris_insert_head($flux){
-	$css = generer_url_public('mesfavoris.css');
+function mesfavoris_insert_head_css($flux){
+	$config = "";
+	if (isset($GLOBALS['meta']['mesfavoris']))
+		$config = unserialize($GLOBALS['meta']['mesfavoris']);
+	if ($config AND isset($config['style_formulaire']))
+		$config = $config['style_formulaire'];
+
+	if (!$config OR !$css=find_in_path("mesfavoris-$config.css"))
+		$css = find_in_path("mesfavoris-32.css");
 	$flux .= "<link rel='stylesheet' type='text/css' media='all' href='$css' />\n";
 	return $flux;
 }
