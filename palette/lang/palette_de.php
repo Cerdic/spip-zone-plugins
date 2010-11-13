668a34e8025f63a728635b5eb08500b6f36e8261
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-11-13 17:40:14 +0000

    langues (palette)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@42274 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ palette/lang/palette_de.php
@@ -0,0 +1,26 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'config_desc' => '<strong>Palette</strong> erm&ouml;glicht ein Eingabefeld
+mit einem Modul zur Farbauswahl zu erg&auml;nzen,
+indem es die CSS-Lasse &laquo;palette&raquo; erh&auml;lt.',
+	'config_exemple' => 'Beispiel:',
+	'config_lien_doc' => '<a href="http://www.spip-contrib.net/Palette" class="spip_out">Siehe Dokumentation</a>',
+
+	// F
+	'fermer' => 'Palette schlie&szlig;en',
+
+	// P
+	'palette' => 'Palette',
+	'palette_ecrire' => 'Palette im Redaktionssystem aktivieren',
+	'palette_public' => 'Palette f&uuml;r den &ouml;ffentlichen Teil der Website aktivieren'
+);
+
+?>
