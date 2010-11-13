cb74cfe5a8a31df7b44664052269985a027c572b
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-11-13 17:39:23 +0000

    langues (comments)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@42266 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ comments/comments-200/lang/comments_de.php
@@ -0,0 +1,50 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cfg_forum_longueur' => 'L&auml;nge der Nachricht',
+	'cfg_forum_longueur_maxi' => 'Maximale L&auml;nge',
+	'cfg_forum_longueur_mini' => 'Minimale L&auml;nge',
+	'comment' => 'Kommentar',
+	'comments' => 'Kommentare',
+	'comments_h' => 'Ihr Kommentar',
+
+	// D
+	'date_heure_a' => 'um',
+	'date_jour_le' => 'Am',
+
+	// F
+	'forum_qui_etes_vous' => 'Wer sind Sie?',
+
+	// L
+	'label_email' => 'E-Mail (Wird nicht ver&ouml;ffentlicht)',
+	'label_nom' => 'Name',
+	'label_url' => 'Ihre Webseite',
+
+	// M
+	'moderation_info' => 'Achtung, Ihre Nachricht wird erst nach vorheriger Pr&uuml;fung freigegeben.',
+
+	// P
+	'permalink_to' => 'Permalink zum Kommentar.',
+
+	// R
+	'reponse_comment_modere' => 'Ihr Kommentar wurde gespeichert und wird nach vorheriger Pr&uuml;fung freigegeben.',
+	'reponse_comment_ok' => 'Vielen Dank f&uuml;r Ihren Kommentar!',
+
+	// S
+	'saisie_texte_info' => 'Dieses Feld akzeptiert SPIP-Abk&uuml;rzungen <code>[-&gt;urls] {{bold}} {italics} &lt;quotes&gt; &lt;code&gt;</code> und HTML-Codes <code>&lt;q&gt; &lt;del&gt; &lt;ins&gt;</code>. Um einen Absatz zu erzeugen, lassen Sie einfach eine Zeile frei.',
+	'saisie_texte_legend' => 'Hinterlassen Sie Ihren Kommentar hier.',
+	'submit1' => 'Vorschau',
+	'submit2' => 'Abschicken',
+
+	// T
+	'titre_comments' => 'Kommentare'
+);
+
+?>
