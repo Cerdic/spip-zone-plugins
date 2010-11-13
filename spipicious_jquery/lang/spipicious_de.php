d098d66a830984259bee15936a7859ca1009a27b
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-11-13 17:40:46 +0000

    langues (spipicious)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@42279 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ spipicious_jquery/lang/spipicious_de.php
@@ -0,0 +1,63 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'add_tags' => 'Tags hinzuf&uuml;gen',
+	'aucun_tags' => 'Kein Tag zugeordnet',
+	'auteur_tag_usage' => 'Dieser Autor hat diesen Tag @nb@ mal verwendet',
+
+	// B
+	'by' => 'Von',
+
+	// C
+	'cfg_description' => 'Dieses Plugin erm&ouml;glicht einfaches Taggen von Artikeln.',
+
+	// E
+	'explication_ajout_tag' => 'Um mehrere Tags auf einmal zuzuordnen, verwenden sie bitte das Trennzeichen "@separateur@".',
+	'explication_suppression_tag' => 'W&auml;hlen sie ihre Tags in der Liste aus, und best&auml;tigen sie das L&ouml;schen.',
+
+	// I
+	'identifier' => 'Um diesem Artikel Tags zuzuordnen, m&uuml;ssen sie sich anmelden.',
+
+	// L
+	'label_tags' => 'Tags:',
+	'liste_auteurs_tag' => 'Dieser Tag wird verwendet von:',
+
+	// M
+	'message_aucun_tags' => 'Kein Tag zugeordnet',
+	'message_installation_activation' => 'Installation der Tabellen und Standardkonfiguration von spip.icio.us',
+	'message_upgrade_database' => 'Update der spip.ici.ous-Datenbank auf Version @version@<br/>',
+
+	// N
+	'no_tags_associes' => 'Sie haben kein zugeordnetes Schlagwort',
+	'nom_groupe_tag' => 'zugeordnete Schlagwortgruppe',
+
+	// S
+	'spipicious' => 'Spip.icio.us',
+
+	// T
+	'tag_ajoute' => 'Sie haben ein Tag zugeordnet:<br />@name@',
+	'tag_deja_present' => 'Sie haben dieses Tag bereits zugeordnet.',
+	'tag_supprime' => 'Sie haben das Tag gel&ouml;scht:<br />@name@',
+	'tags' => 'Tags:',
+	'tags_ajoutes' => 'Sie haben mehrere Tags zugeordnet:<br />@name@',
+	'tags_supprimes' => 'Sie haben mehrere Tags gel&ouml;scht:<br />@name@',
+	'title_tag_utilise_nb' => 'Dieses Schlagwort wurde von @nb@ Besuchern zugeordnet.',
+	'title_tag_utilise_un' => 'Dieses Schlagwort wurde von einem Besucher zugeordnet.',
+	'to' => 'mit',
+	'types_utilisateurs' => 'Wer hat Zugriff auf das Formular?',
+
+	// V
+	'vos_tags' => 'Ihre Tages (&Auml;nderung)',
+
+	// W
+	'waiting' => 'ihre Tags suchen'
+);
+
+?>
