bed8612d209c8df3f20aff83baaf9613079d9bcd
vxl@noos.fr <vxl@noos.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-11-14 07:31:57 +0000

    Fichier de langue en allemand et en italien.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@42293 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ gravatar/lang/gravatar_de.php
@@ -0,0 +1,25 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+'gravatar_info' => 'Um Ihren Avatar hier anzeigen zu lassen, registrieren Sie sich erst hier <a href="http://www.gravatar.com/" rel="external nofollow" class="spip_out">gravatar.com</a> (kostenlos und einfach).',
+'gravatar_info_forum' => 'Um Ihren Avatar hier anzeigen zu lassen, registrieren Sie sich erst hier <a href="http://www.gravatar.com/" rel="external nofollow" hreflang="en" class="spip_out">gravatar.com</a> (kostenlos und einfach). Vergessen Sie nicht, hier Ihre E-Mail-Adresse einzutragen.',
+'titre_gravatar_auteur' => 'GRAVATAR DES AUTORS',
+
+'cfg_titre_gravatar' => 'Gravatars',
+'cfg_descr_gravatar' => 'Benutzen Sie das Gravatar-Rendering',
+
+'label_image_defaut' => 'Standardbild',
+'explication_image_defaut' => 'F&uuml;r alle, die kein Gravatar haben:',
+'label_image_defaut_404' => 'Kein Bild',
+'label_image_defaut_png' => '<tt>images/gravatar.png</tt>',
+'label_image_defaut_gravatar' => 'Gravatar logo',
+'label_image_defaut_identicon' => '<i>Identicon</i> (erzeugt von der E-Mail-Adresse)',
+'label_image_defaut_monsterid' => '<i>MonsterID</i> (erzeugt von der E-Mail-Adresse)',
+'label_image_defaut_wavatar' => '<i>Wavatar</i> (erzeugt von der E-Mail-Adresse)',
+'label_taille' => 'Gr&ouml;&szlig;e des Gravatars',
+);
+
+?>
---
+++ gravatar/lang/gravatar_it.php
@@ -0,0 +1,24 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+'gravatar_info' => 'Per mostrare qui il tuo avatar, registralo prima su <a href="http://www.gravatar.com/" rel="external nofollow" class="spip_out">gravatar.com</a> (gratis e indolore).',
+'gravatar_info_forum' => 'Per mostrare qui il tuo avatar, registralo prima su <a href="http://www.gravatar.com/" rel="external nofollow" class="spip_out">gravatar.com</a> (gratis e indolore). Non dimenticare di fornire il tuo indirizzo email.',
+'titre_gravatar_auteur' => 'GRAVATAR DELL\'AUTORE',
+
+'cfg_titre_gravatar' => 'Gravatars',
+
+'label_image_defaut' => 'immagine standard',
+'explication_image_defaut' => 'Per coloro che non hanno Gravatar, usa:',
+'label_image_defaut_404' => 'Non c\'è immagine',
+'label_image_defaut_png' => '<tt>images/gravatar.png</tt>',
+'label_image_defaut_gravatar' => 'Logo di Gravatar',
+'label_image_defaut_identicon' => '<i>Identicon</i> (generato dall\'indirizzo email)',
+'label_image_defaut_monsterid' => '<i>MonsterID</i> (generato dall\'indirizzo email)',
+'label_image_defaut_wavatar' => '<i>Wavatar</i> (generato dall\'indirizzo email)',
+'label_taille' => 'Dimensioni dei Gravatar',
+);
+
+?>
