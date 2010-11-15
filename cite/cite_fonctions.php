9e6f9b15e685d2eed5f61bef357d3b57edcb7782
joseph@larmarange.net <joseph@larmarange.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-11-15 15:58:30 +0000

    Première version (toujours en dev) d'un plugin fournissant des modèles <cite> permettant d'insérer des références bibliographiques (inspiré des Wikipedia Citation Template). Pour le moment, seuls les articles scientifiques ont été codés.
    Le modèle affiche la référence mise en forme avec des liens hypertextes sur les identifieurs (ISSN, ISBN, DOI, liens...), ajoute un COinS (référence bibliograohique encapsulée dans un <span> et pouvant être détectée par des logiciels comme Zotero) et des liens permettant de télécharger la référence aux formats RIS (EndNote, Rerence Manager) et BibTeX.
    
    Exemple d'appel :
    
    <cite|journal|authors=Auteur principal, prénom ; Auteur 2, P2; Auteur 3, P3;Groupe auteurs|
      title=Titre de l'article scientifique|
      volume=3|
      issue=12|
      pages=75-97|
      year=2004|
      url=http://www.toto.com|
      issn=1932-6203|
      doi=10.1371/journal.pone.0013189>
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@42337 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ cite/cite_fonctions.php
@@ -0,0 +1,55 @@
+<?php
+
+function cite_authors_coins ($auts) {
+	$ret = '';
+	$auts = explode(';',$auts);
+	$first_aut = explode(',',$auts[0],2);
+	$ret .= '&rft.aulast='.urlencode(trim($first_aut[0]));
+	$ret .= (isset($first_aut[1])) ? '&rft.aufirst='.urlencode(trim($first_aut[1])) : '';
+	foreach ($auts as $aut) {
+		$aut = explode(',',$aut,2);
+		$ret .= '&rft.au='.urlencode(trim($aut[0]).',');
+		if ($aut[1])
+			$ret .= urlencode(' '.trim($aut[1]));
+	}
+	return $ret;
+}
+
+function cite_authors_ris ($auts) {
+	$auts = explode(';',$auts);
+	foreach ($auts as $cle => $aut) {
+		$aut = explode(',',$aut,2);
+		$auts[$cle] = 'A1  - '.trim($aut[0]).',';
+		if ($aut[1])
+			$auts[$cle] .= trim($aut[1]);
+	}
+	return implode("\n",$auts);
+}
+
+function cite_authors_bibtex ($auts) {
+	$auts = explode(';',$auts);
+	foreach ($auts as $cle => $aut) {
+		$aut = explode(',',$aut,2);
+		$auts[$cle] = trim($aut[0]).',';
+		if ($aut[1])
+			$auts[$cle] .= " ".trim($aut[1]);
+	}
+	return 'author = {'.implode(" and ",$auts).'},';
+}
+
+function cite_pages_ris ($pages) {
+	$pages = explode('-',trim($pages));
+	$ret = "SP  - ".$pages[0];
+	if (isset($pages[1]))
+		$ret .= "\nEP  - ".$pages[1];
+	return $ret;
+}
+
+function bibtex_encode($texte) {
+	include_spip('inc/transtab_unicode_bibtex');
+	foreach (transtab_unicode_bibtex() as $utf8 => $bibtex)
+		$texte = str_replace($utf8,$bibtex,$texte);
+	return $texte;
+}
+
+?>
---
+++ cite/lang/cite_fr.php
@@ -0,0 +1,26 @@
+<?php
+
+// S&eacute;curit&eacute;
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	
+	'explication_authors' => 'Entrez les auteurs sous la forme <i>Nom, Pr&eacute;nom</i> en utilisant la virgule comme s&eacute;parateur. Utilisez le point-virgule pour s&eacute;parer plusieurs auteurs. Exemple&nbsp: <i>Einstein, Albert ; Descartes, Ren&eacute ; Groupe d\'auteurs</i>.',
+	'explication_pages' => 'Utilisez le tiret (-) entre la page de d&eacute;but et la page de fin. Exemple&nbsp: <i>125-136</i>.',
+	'item_journal' => 'article scientifique',
+	'label_authors' => 'Auteurs&nbsp;:',
+	'label_doi' => 'DOI&nbsp;:',
+	'label_issn' => 'ISSN',
+	'label_issue' => 'Num&eacute;ro&nbsp;:',
+	'label_journal' => 'Nom de la revue&nbsp;:',
+	'label_pages' => 'Pages&nbsp;:',
+	'label_title' => 'Titre&nbsp;:',
+	'label_url' => 'Lien (URL)&nbsp;:',
+	'label_variante' => 'Type de document&nbsp;:',
+	'label_volume' => 'Volume&nbsp;:',
+	'label_year' => 'Ann&eacute;e&nbsp;:',
+	'reference_bibliographie' => 'une r&eacute;f&eacute;rence bibliographique',
+
+);
+
+?>
