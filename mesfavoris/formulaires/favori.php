15a3b4fc722bc81da31fd6a970057912171c15b9
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-11-16 17:29:36 +0000

    passer l'objet dans le html du form, ca a son utilite et ca penalise peu le cache
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@42369 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- mesfavoris/formulaires/favori.php
+++ mesfavoris/formulaires/favori.php
@@ -15,7 +15,8 @@
 function formulaires_favori_charger_dist($objet, $id_objet){
 	$valeur = array(
 		'editable'=>true,
-		'_deja_favori'=>false
+		'_deja_favori'=>false,
+		'_objet'=>$objet
 	);
 	if (!isset($GLOBALS['visiteur_session']['statut'])){
 		$valeur['editable'] = false;
@@ -31,7 +32,7 @@ function formulaires_favori_charger_dist($objet, $id_objet){
 }
 
 function formulaires_favori_traiter_dist($objet, $id_objet){
-	$res = array();
+	$res = array('message_ok'=>' ');
 	if ($id_auteur = intval($GLOBALS['visiteur_session']['id_auteur'])){
 		include_spip('inc/mesfavoris');
 		if (!is_null(_request('ajouter'))){
