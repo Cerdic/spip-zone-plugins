cbfd35079f9db20e414d36857c5a87774d33ff4f
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-11-16 11:48:15 +0000

    eviter de polluer *tout* le site public avec une feuille de style calculee qui ne servira que sur une page. On insere un marqueur dans _base.html, et on le repere via affichage_final pour inserer la css au besoin.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@42359 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- saisies/saisies_pipelines.php
+++ saisies/saisies_pipelines.php
@@ -6,24 +6,20 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 function saisies_header_prive($flux){
 	$js = find_in_path('javascript/saisies.js');
 	$flux .= "\n<script type='text/javascript' src='$js'></script>\n";
-	$flux .= saisies_insert_head_css('');
-	return $flux;
-}
-
-function saisies_insert_head($flux){
-	$js = find_in_path('javascript/saisies.js');
-	$flux .= "\n<script type='text/javascript' src='$js'></script>\n";
-	$flux .= saisies_insert_head_css(''); // compat pour les vieux spip
-	return $flux;
-}
-
-function saisies_insert_head_css($flux){
-	static $done = false;
-	if ($done) return $flux;
-	$done = true;
-	
 	$css = generer_url_public('saisies.css');
 	$flux .= "\n<link rel='stylesheet' href='$css' type='text/css' media='all' />\n";
+	return $flux;
+}
+
+function saisies_affichage_final($flux){
+	if (($p = strpos($flux,"<!--!inserer_saisie_editer-->"))!==false){
+		$pi = strpos($flux, "<script");
+		if (!$pi)
+			$pi = $p; // si pas de <script inserer comme un goret entre 2 <li> de saisies
+		$css = generer_url_public('saisies.css');
+		$ins = "\n<link rel='stylesheet' href='$css' type='text/css' media='all' />\n";
+		$flux = substr_replace($flux, $ins, $pi, 0);
+	}
 	return $flux;
 }
 
