ef59caba53d43304e5f3f894895a2feb4d42844d
joseph@larmarange.net <joseph@larmarange.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-11-16 14:18:54 +0000

    Disposer d'une modalbox même si mediatheque et mediabox ne sont pas installés
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@42363 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ inserer_modeles/modalbox/modalbox_pipelines.php
@@ -0,0 +1,15 @@
+<?php
+
+function inserer_modeles_modalbox_insert_head($flux){
+	if (!defined('_DIR_PLUGIN_MEDIABOX') && !defined('_DIR_PLUGIN_GESTDOC')) {
+		// Insertion des librairies js
+		$flux .='<script src="'.find_in_path('modalbox/jquery.simplemodal-1.3.3.js').'" type="text/javascript"></script>';
+		$flux .='<script src="'.find_in_path('modalbox/modalbox.js').'" type="text/javascript"></script>';
+		$css = generer_url_public('modalbox/style_prive_plugin_modalbox');
+		$flux .= "\n<link rel='stylesheet' href='$css' type='text/css' />\n";
+	}
+
+	return $flux;
+}
+
+?>
