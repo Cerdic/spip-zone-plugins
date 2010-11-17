b70213c38657e36d14817e416ce9be4abf5f76d8
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-11-17 22:32:31 +0000

    Un pipeline pas forcément défini
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@42383 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- mes_fichiers_2/mes_fichiers_options.php
+++ mes_fichiers_2/mes_fichiers_options.php
@@ -3,6 +3,10 @@
 // Declaration du pipeline permettant d'ajouter des fichiers a la sauvegarde mes_fichiers
 $GLOBALS['spip_pipeline']['mes_fichiers_a_sauver'] = '';
 
+if(!isset($GLOBALS['spip_pipeline']['post_sauvegarde'])){
+	$GLOBALS['spip_pipeline']['post_sauvegarde'] = '';
+}
+
 // Repertoire de stockage des archives creees
 define('_DIR_MES_FICHIERS', _DIR_TMP . 'mes_fichiers/');
 
