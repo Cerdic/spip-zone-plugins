87ccf8ee95f46c34d814b9391bf46cdea999b27b
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-11-22 23:46:51 +0000

    Mettre au carré les fichiers de langues
    Indentation etc
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@42460 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- authentification/openid/lang/openid_en.php
+++ authentification/openid/lang/openid_en.php
@@ -1,12 +1,14 @@
 <?php
-   /*******
-    * Fichier de langue pour le plugin OpenID
-    ******/
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/authentification/openid/lang
+if (!defined("_ECRIRE_INC_VERSION")) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+
 	// E
-  'erreur_openid' => 'OpenID authentication error: have you entered a valid OpenID?',
+	'erreur_openid' => 'OpenID authentication error: have you entered a valid OpenID?',
 	'erreur_librairies' => 'Unable to locate OpenID authentification libraries',
+
 	// F
 	'form_login_openid' => 'You can use your <a href=\'http://openid.net/\'>OpenID</a> as login',
 	'form_forum_indiquer_openid' => 'You can sign up on this site with your OpenID address',
@@ -16,12 +18,14 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'form_pet_votre_openid' => 'OpenID login',
 
 	// L
-  'login_login2' => 'Login (identifier for connection to the site) or OpenID:',
-	// O
-  'openid' => 'OpenID',
-	// U
-  'utilisateur_inconnu' => 'Unknown user',
-	// V
-  'verif_refusee' => 'Authentication request cancelled',
+	'login_login2' => 'Login (identifier for connection to the site) or OpenID:',
 	
+	// O
+	'openid' => 'OpenID',
+	
+	// U
+	'utilisateur_inconnu' => 'Unknown user',
+	
+	// V
+	'verif_refusee' => 'Authentication request cancelled',
 );
--- authentification/openid/lang/openid_fr.php
+++ authentification/openid/lang/openid_fr.php
@@ -1,7 +1,7 @@
 <?php
-   /*******
-    * Fichier de langue pour le plugin OpenID
-    ******/
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/authentification/openid/lang
+if (!defined("_ECRIRE_INC_VERSION")) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
@@ -9,6 +9,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'erreur_openid' => 'Cette adresse OpenID ne semble pas valide',
 	'erreur_librairies' => 'Impossible de trouver les librairies d\'authenfication OpenID',
 	'erreur_openid_info_manquantes' => 'Votre fournisseur OpenID ne nous a pas envoy&eacute; toutes les informations n&eacute;cessaires',
+	
 	// F
 	'form_login_openid' => 'Vous pouvez aussi utiliser un identifiant OpenID (<a href="http://www.openidfrance.fr/" target="_blank">aide</a>)',
 	'form_forum_indiquer_openid' => 'Vous pouvez vous inscrire sur ce site en indiquant un identifiant OpenID.',
@@ -18,10 +19,13 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'form_pet_votre_openid' => 'Votre adresse OpenID',
 	'form_login_openid_inconnu' => 'Cette adresse OpenID est inconnue. Corrigez, ou enregistrez-la dans votre profil.',
 	'form_login_statut_nouveau' => 'Pour votre premi&egrave;re connexion, vous devez utiliser le mot de passe qui vous a &eacute;t&eacute; envoy&eacute; par mail',
+	
 	// O
-  'openid' => 'OpenID',
+	'openid' => 'OpenID',
+	
 	// U
-  'utilisateur_inconnu' => 'Utilisateur inconnu sur ce site',
+	'utilisateur_inconnu' => 'Utilisateur inconnu sur ce site',
+	
 	// V
-  'verif_refusee' => 'V&eacute;rification refus&eacute;e'
+	'verif_refusee' => 'V&eacute;rification refus&eacute;e'
 );
