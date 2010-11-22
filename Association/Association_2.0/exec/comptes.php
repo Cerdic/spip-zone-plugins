a759207632ebbbd716647ff82741337172044d66
esj@rezo.net <esj@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-11-22 12:04:15 +0000

    [source:_plugins_/Association/Association_2_0 Association]. la navigation était cassée dans le livre de compte depuis r42220.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@42449 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- Association/Association_2.0/exec/comptes.php
+++ Association/Association_2.0/exec/comptes.php
@@ -22,69 +22,88 @@ function exec_comptes() {
 	} else {
 		$annee= intval(_request('annee'));
 		if(empty($annee)){$annee = date('Y');}
-		$valide = _request('valide');
-		if (!is_numeric($valide)) $valide = '';
+		$vu = _request('vu');
+		if (!is_numeric($vu)) $vu = '';
 
 		if ( isset ($_REQUEST['imputation'] )) { $imputation = $_REQUEST['imputation']; }
 		else { $imputation= "%"; }
-		
-		$where = "imputation like " . sql_quote($imputation)
-		  . (!is_numeric($valide) ? '' : (" AND vu=$valide"));
+		exec_comptes_args($annee, $vu, $imputation, _request('debut'));
+	}
+}
 
-		$sel = comptes_select_annee($where, $annee,'imputation='.$imputation . "&valide=$valide");
-		$where .= " AND date_format( date, '%Y' ) = $annee";
+function exec_comptes_args($annee, $vu, $imputation, $debut) 
+{
+	$where = "imputation like " . sql_quote($imputation)
+	  . (!is_numeric($vu) ? '' : (" AND vu=$vu"));
 
-		$totaux = comptes_totaux($where, $imputation, $annee);
+	$sel = comptes_select_annee($where, $annee,'imputation='.$imputation . "&vu=$vu");
+	$where .= " AND date_format( date, '%Y' ) = $annee";
 
-		$commencer_page = charger_fonction('commencer_page', 'inc');
-		echo $commencer_page(_T('asso:titre_gestion_pour_association')) ;
-		association_onglets();
+	$totaux = comptes_totaux($where, $imputation, $annee);
 
-		echo debut_gauche("",true);
-		echo debut_boite_info(true);
-		echo association_date_du_jour();	
-		echo '<p>', _T('asso:en_bleu_recettes_en_rose_depenses'), '</p>'; 
-		echo $totaux;
-		echo fin_boite_info(true);	
-		
-		$url_bilan = generer_url_ecrire('bilan', "annee=$annee");		
-		$res = association_icone(_T('Bilan') . " $annee",  $url_bilan, 'finances.jpg')
-		. association_icone(_T('asso:ajouter_une_operation'),  generer_url_ecrire('edit_compte'), 'ajout_don.png');
+	$commencer_page = charger_fonction('commencer_page', 'inc');
+	echo $commencer_page(_T('asso:titre_gestion_pour_association')) ;
+	association_onglets();
 
-		echo bloc_des_raccourcis($res);
-		
-		echo debut_droite("",true);
-		
-		debut_cadre_relief(  "", false, "",  _T('asso:informations_comptables'));
-		
-		echo "\n<table width='100%'>";
-		echo '<tr><td>', $sel,'</td>';
-		
-		echo '<td style="text-align:right;">';
-		echo '<form method="post" action="';
-		echo generer_url_ecrire('comptes');
-		echo '"><div>';
-		echo '<select name ="imputation" class="fondl" onchange="form.submit()">';
-		echo '<option value="%" ';
-		if ($imputation=="%") { echo ' selected="selected"'; }
-		echo '>Tous</option>';
-		$sql = sqL_select('code, classe, intitule', 'spip_asso_plan','', '', "classe,code");
-		while ($plan = sql_fetch($sql)) {
-			echo '<option value="'.$plan['code'].'" ';
-			if ($imputation==$plan['code']) { echo ' selected="selected"'; }
-			echo '>'.$plan['classe'].' - '.$plan['intitule'].'</option>';
-		}
-		echo '</select></div></form></td>';
-		echo '</tr></table>';
+	echo debut_gauche("",true);
+	echo debut_boite_info(true);
+	echo association_date_du_jour();	
+	echo '<p>', _T('asso:en_bleu_recettes_en_rose_depenses'), '</p>'; 
+	echo $totaux;
+	echo fin_boite_info(true);	
+	
+	$url_bilan = generer_url_ecrire('bilan', "annee=$annee");		
+	$res = association_icone(_T('Bilan') . " $annee",  $url_bilan, 'finances.jpg')
+	. association_icone(_T('asso:ajouter_une_operation'),  generer_url_ecrire('edit_compte'), 'ajout_don.png');
+
+	echo bloc_des_raccourcis($res);
+	
+	echo debut_droite("",true);
+	
+	debut_cadre_relief(  "", false, "",  _T('asso:informations_comptables'));
+	
+	echo "\n<table width='100%'>";
+	echo '<tr><td>', $sel,'</td>';
+	
+	echo '<td style="text-align:right;">';
+	echo '<form method="post" action="';
+	echo generer_url_ecrire('comptes');
+	echo '"><div>';
+	echo '<select name ="imputation" class="fondl" onchange="form.submit()">';
+	echo '<option value="%" ';
+	if ($imputation=="%") { echo ' selected="selected"'; }
+	echo '>Tous</option>';
+	$sql = sqL_select('code, classe, intitule', 'spip_asso_plan','', '', "classe,code");
+	while ($plan = sql_fetch($sql)) {
+		echo '<option value="'.$plan['code'].'" ';
+		if ($imputation==$plan['code']) { echo ' selected="selected"'; }
+		echo '>'.$plan['classe'].' - '.$plan['intitule'].'</option>';
+	}
+	echo '</select></div></form></td>';
+	echo '</tr></table>';
 
 	//TABLEAU
-		$max_par_page = 30;
-		$limit = intval(_request('debut')) . "," . $max_par_page;
-		$auteurs = comptes_while($where, $limit);
+	$max_par_page = 30;
+	$limit = intval(_request('debut')) . "," . $max_par_page;
+	$table = comptes_while($where, $limit);
 
-		if ($auteurs) {
+	if ($table) {
 
-	$table = "<table border='0' cellpadding='2' cellspacing='0' width='100%' class='arial2' style='border: 1px solid #aaaaaa;'>\n"
+		//SOUS-PAGINATION
+
+		$nombre_selection=sql_countsel('spip_asso_comptes', $where);
+		$pages=intval($nombre_selection/$max_par_page) + 1;
+		$args = 'annee='.$annee.'&imputation='.$imputation. (is_numeric($vu) ? "&vu=$vu" : ''); 
+		$nav = '';
+		if ($pages != 1) for ($i=0;$i<$pages;$i++) { 
+			$position= $i * $max_par_page;
+			if ($position == $debut)
+			  { $nav .= '<strong>'.$position.' </strong>'; }
+			else { $h = generer_url_ecrire('comptes',$args.'&debut='.$position);
+			  $nav .= "<a href='$h'>$position</a>\n"; }
+		  }
+
+		$table = "<table border='0' cellpadding='2' cellspacing='0' width='100%' class='arial2' style='border: 1px solid #aaaaaa;'>\n"
 	. "<tr style='background-color: #DBE1C5;'>\n"
 	. '<th style="text-align: right;">' . _T('asso:id'). "</th>\n"
 	. '<th style="text-align: right;">' . _T('asso:date') . "</th>\n"
@@ -94,30 +113,14 @@ function exec_comptes() {
 	. '<th>' . _T('asso:financier') . "</th>\n"
 	. '<td colspan="3" style="text-align: center;"><strong>&nbsp;</strong></td>'
 	. '</tr>'
-	. $auteurs
-	. "</table>\n";
+	. $table
+	. "</table>\n"
+	. "<table width='100%'><tr>\n<td>" . $nav . '</td><td style="text-align:right;"><input type="submit" value="' . _L('Valider') . '" class="fondo" /></td></tr></table>';
 
-	//SOUS-PAGINATION
-
-	$nombre_selection=sql_countsel('spip_asso_comptes', $where);
-	$pages=intval($nombre_selection/$max_par_page) + 1;
-	$nav = '';
-	if ($pages != 1) { 
-		for ($i=0;$i<$pages;$i++) { 
-			$position= $i * $max_par_page;
-			if ($position == $debut)
-			  { $nav .= '<strong>'.$position.' </strong>'; }
-			else { $nav .= '<a href="'.generer_url_ecrire('comptes','annee='.$annee.'&imputation='.$imputation.'&debut='.$position).'">'.$position."</a>\n"; }
-		}	
+		echo generer_form_ecrire('action_comptes', $table);
 	}
-
-	$table2 = "\n<table width='100%'><tr>\n<td>" . $nav . '</td><td style="text-align:right;"><input type="submit" value="' . _L('Valider') . '" class="fondo" /></td></tr></table>';
-
-	echo generer_form_ecrire('action_comptes', $table . $table2);
 	fin_cadre_relief();  
 	echo fin_page_association(); 
-		}
-	}
 }
 
 function comptes_while($where, $limit)
