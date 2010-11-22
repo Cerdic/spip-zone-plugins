5d3ced853a07ca36a3960840d0c8bbce623f73c6
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-11-22 02:03:18 +0000

    Un formulaire de configuration un peu plus moderne
    Les images dans le répertoire images/
    
    Un code d'insertion google analytics un peu plus moderne également
    
    Traduction du plugin.xml en anglais, le lien vers la doc est un lien, rien d'autre, on ne met également pas de liens internes dans le plugin.xml
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@42444 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- google_analytics/googleanalytics_insert_head.php
+++ google_analytics/googleanalytics_insert_head.php
@@ -2,24 +2,27 @@
 
 
 function googleanalytics_insert_head($flux){
-$id_google = lire_config('googleanalytics/idGoogle');
-if (!$id_google || $id_google == '_' || $id_google == 'UA-xxxxxx') {
+	$id_google = lire_config('googleanalytics/idGoogle');
+	if (!$id_google || $id_google == '_' || $id_google == 'UA-xxxxxx') {
 		return $flux;
 	}
 	else {
 
-$flux .= '
+	$flux .= '
 <script type="text/javascript">
-var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-document.write(unescape("%3Cscript src=\'" + gaJsHost + "google-analytics.com/ga.js\' type=\'text/javascript\'%3E%3C/script%3E"));
-</script>
-<script type="text/javascript">
-var pageTracker = _gat._getTracker("'.$id_google.'");
-pageTracker._initData();
-pageTracker._trackPageview();
-</script>';
-return $flux;
-}
+	var _gaq = _gaq || [];
+	_gaq.push(["_setAccount", "UA-823028-18"]);
+	_gaq.push(["_trackPageview"]);
+
+	(function() {
+		var ga = document.createElement("script"); ga.type = "text/javascript"; ga.async = true;
+		ga.src = ("https:" == document.location.protocol ? "https://ssl" : "http://www") + ".google-analytics.com/ga.js";
+		var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ga, s);
+	})();
+</script>'."\n";
+
+		return $flux;
+	}
 }
 
 ?>
--- google_analytics/lang/googleanalytics_fr.php
+++ google_analytics/lang/googleanalytics_fr.php
@@ -3,8 +3,14 @@
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-'textes_id_google' => 'Votre identifiant google',
-'texte_votre_identifiant' => 'Votre identifiant google analytics du type UA-12345',
-'retour_a_spip' => 'Retour &agrave; la page compl&egrave;te',
+	// C
+	'cfg_titre' => 'Google analytics',
+
+	// T
+	'textes_id_google' => 'Votre identifiant google',
+	'texte_votre_identifiant' => 'Votre identifiant google analytics du type UA-12345',
+	
+	// R
+	'retour_a_spip' => 'Retour &agrave; la page compl&egrave;te',
 );
 ?>
