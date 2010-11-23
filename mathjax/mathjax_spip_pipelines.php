1d8e860bc5dcb352e4d5e6047ae65a9d21458577
prigent.yohann@gmail.com <prigent.yohann@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-11-23 22:48:13 +0000

    MathJax for SPIP (http://www.mathjax.org/)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@42480 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ mathjax/mathjax_spip_pipelines.php
@@ -0,0 +1,26 @@
+<?php
+
+// Sécurité
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+function mathjax_spip_insert_head($flux){
+	$js = find_in_path('lib/mathjax-v1.0.1a/MathJax.js');
+	$param = <<<EOF
+		MathJax.Hub.Config({
+    	    extensions: ["tex2jax.js"],
+    	    jax: ["input/TeX", "output/HTML-CSS"],
+    	    tex2jax: {
+    	        inlineMath: [ ['$','$'], ["\\(","\\)"] ],
+    	        displayMath: [ ['$$','$$'], ["\\[","\\]"] ],
+    	    },
+    	    "HTML-CSS": { availableFonts: ["TeX"] }
+    	});
+	</script>
+EOF;
+	$flux .= "\n<script type='text/javascript' src='$js'>
+		$param
+	</script>\n";
+	return $flux;
+}
+
+?>
