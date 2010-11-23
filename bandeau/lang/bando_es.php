8379c0c225984824143ec5ea7eccd854094c923c
lspeciale@gmail.com <lspeciale@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-11-23 14:41:35 +0000

    Traducción de alguna líneas
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@42469 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- bandeau/lang/bando_es.php
+++ bandeau/lang/bando_es.php
@@ -9,29 +9,29 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// I
 	'icone_accueil' => 'Mis tareas',
-	'icone_administration' => 'Wartung',
+	'icone_administration' => 'Administraci&oacute;n',
 	'icone_configuration' => 'Configuraci&oacute;n',
 	'icone_edition' => 'Edici&oacute;n',
 	'icone_langage' => 'Mi lengua',
 	'icone_mes_infos' => 'Mis informaciones',
 	'icone_preferences' => 'Mis preferencias',
 	'icone_reactions' => 'Actividad',
-	'icone_squelette' => 'Skelette',
+	'icone_squelette' => 'Esqueletos',
 	'icone_suivi_edito' => 'Publicaci&oacute;n',
 	'icone_suivi_publication' => 'Seguir la publicaci&oacute;n',
 	'icone_visiter_site' => 'Ver el sitio p&uacute;blico',
 
 	// L
-	'label_adresses_secondaires' => 'Adresses ALIAS du site public', # NEW
+	'label_adresses_secondaires' => 'Direcciones ALIAS del sitio p&uacute;', 
 	'label_choix_langue' => 'Seleccine su lengua',
-	'label_slogan_site' => 'Slogan del sitio',
+	'label_slogan_site' => 'Eslogan del sitio',
 	'label_taille_ecran' => 'Largo de la pantalla',
 	'label_texte_et_icones_navigation' => 'Menu de navigai&oacute;n',
 	'label_texte_et_icones_page' => 'Afichar en la p&aacute;gina',
 
 	// T
-	'texte_redirection_adresses_secondaires' => 'Les Alias sont des URL alternatives qui permettent aussi d\'acc&egrave;der à votre site.
-		Ces adresses seront redirig&eacute;es automatiquement vers l\'adresse principale. Indiquez une adresse par ligne', # NEW
+	'texte_redirection_adresses_secondaires' => 'Los ALIAS son las URL alternativas que tambi&eacute;n permiten el acceso a vustro sitio.
+	Estas direcciones son dirigidas autom&aacute;ticamente hacia le direcci&oacute;n principal. Indique un ALIAS por l&iacute;nea.',
 	'titre_config_langage' => 'Configurar la lengua',
 	'titre_configurer_preferences' => 'Configurar sus preferencias',
 	'titre_identite_site' => 'Identidad del sitio'
