d437a4c90b96a86f5a26186682eaf0c0629f95a0
xdjuj@apsulis.com <xdjuj@apsulis.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-11-25 14:36:49 +0000

    Mieux gérer les id/noms des vidéos Se séparer de parse_url qui merdait => règle normalement le problème ponctuel qui insérait une mauvaise référence de vidéo
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@42507 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- videos/formulaires/insertion_video.php
+++ videos/formulaires/insertion_video.php
@@ -9,26 +9,20 @@ function formulaires_insertion_video_charger_dist($id_article){
 
 function formulaires_insertion_video_verifier_dist(){
 	$erreurs = array();
-	$url = _request('url');
+	// Retirer les trucs qui emmerdent : tous les arguments d'ancre / les espaces foireux les http:// et les www. éventuels
+	$url = preg_replace('%(#.*$|http://|www.)%', '', trim(_request('url')));
 	
 	if(preg_match('/dailymotion/',$url)){
 		set_request('type','dailym');
-		$laVideo = preg_replace('#(/video/|//www.dailymotion.com)#','',parse_url($url,PHP_URL_PATH));
+		$laVideo = preg_replace('#dailymotion\.com/video/#','',$url);
 	}
 	else if(preg_match('/vimeo/',$url)){
 		set_request('type','vimeo');
-		$laVideo = preg_replace('#(/|//www.vimeo.com|//vimeo.com/)#','',parse_url($url,PHP_URL_PATH));
+		$laVideo = preg_replace('#vimeo\.com/#','',$url);
 	}
-	else if(preg_match('/youtube/',$url)){
+	else if(preg_match('/(youtube|youtu\.be)/',$url)){
 		set_request('type','youtube');
-		$lUrl = explode('&',preg_replace('/\?/','',parse_url($url,PHP_URL_QUERY)));
-		foreach($lUrl as $clef=>$valeur){
-			if(preg_match('/v=/',$valeur)) $laVideo = preg_replace('#v=#','',$valeur);
-		}
-	}
-	else if(preg_match('/youtu.be/',$url)){
-		set_request('type','youtube');
-		$laVideo = preg_replace('#http://youtu.be/#','',$url);
+		$laVideo = preg_replace('#(youtu\.be/|youtube\.com/watch\?v=|&.*$|\?hd=1)#','',$url);
 	}
 
 	if(!$laVideo) $erreurs['pas_valide'] = _T('Adresse non valide.');
@@ -38,25 +32,28 @@ function formulaires_insertion_video_verifier_dist(){
 }
 
 function formulaires_insertion_video_traiter_dist($id_article){
-	include_spip('inc/acces');
-	include_spip('lib/Videopian'); // http://www.upian.com/upiansource/videopian/
-	/*
-		TODO Si on veut être compatible gentiment, il faut tester si on est bien en PHP5 sinon il ne faut pas utiliser la Classe Videopian et décommenter les 3 lignes suivantes
-	*/
-	$Videopian = new Videopian();
-	/*
-		TODO Peut être qu'il serait bien de catcher l'erreur éventuelle par exemple en cas de refus de connexion par le serveur distant
-	*/
-	$infosVideo = $Videopian->get(_request('url'));
-		
-	// $fichier = serialize(array(_request('type'),_request('laVideo'))); // pour tout ranger dans #FICHIER
-	// $titreArticle = sql_getfetsel('titre','spip_articles',"id_article=".$id_article);
-	// $titre = $type."-".$fichier."-".$titreArticle;
+	include_spip('inc/acces');	
 	$type = _request('type');
 	$fichier = _request('laVideo');
-	$titre = $infosVideo->title;
-	$descriptif = $infosVideo->description;
-	// $logoDocument = $infosVideo->thumbnails->0->url; // A brancher sur la copie de document
+	$url = _request('url');
+	
+	if(!preg_match('/youtu\.be/',$url)){
+		/*
+			TODO Si on veut être compatible gentiment, il faut tester si on est bien en PHP5 sinon il ne faut pas utiliser la Classe Videopian et décommenter les 3 lignes suivantes
+		*/
+		include_spip('lib/Videopian'); // http://www.upian.com/upiansource/videopian/
+		$Videopian = new Videopian();
+		/*
+			TODO Peut être qu'il serait bien de catcher l'erreur éventuelle par exemple en cas de refus de connexion par le serveur distant
+		*/
+		$infosVideo = $Videopian->get($url);		
+		$titre = $infosVideo->title;
+		$descriptif = $infosVideo->description;
+		// $logoDocument = $infosVideo->thumbnails->0->url; // A brancher sur la copie de document
+	}
+	else{
+		$titre = sql_getfetsel('titre','spip_articles',"id_article=".$id_article);
+	}
 	
 	// On va pour l'instant utiliser le champ extension pour stocker le type de source
 	$champs = array(
