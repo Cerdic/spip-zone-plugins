f5d3ce6d23e110287f92d9ed7842246412748b3d
listes@nouveauxterritoires.fr <listes@nouveauxterritoires.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-11-29 08:42:02 +0000

    nouvelle version
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@42565 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- depublication/192/depublication_options.php
+++
@@ -1,9 +0,0 @@
-<?php
-	
-include_spip('base/depublication_installer');	
-// On vérifie que la structure de la base est okpour c eplugin
-depublication_upgrade();
-
-// on regarde si des articles sont à dépublier
-depublication_articles();
-?>
