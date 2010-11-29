a1349a6081b178cbc44891a662076ec1892a7525
amaury.adon@laposte.net <amaury.adon@laposte.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-11-29 12:40:22 +0000

    on commence le déménagement pour apporter la gestion des fichiers joints aux annonces de séminaires
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@42570 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ seminaire/seminaires_pipelines.php
@@ -0,0 +1,4 @@
+$GLOBALS['gestdoc_exec_colonne_document'][] = ' evenements_edit';
+$GLOBALS['gestdoc_liste_champs'][] = 'descriptif';
+function  evenements_post_edition($flux){}
+function evenements_affiche_gauche($flux){}
