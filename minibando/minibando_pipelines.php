5e5eefb371cd093175edee70eace334fbc197b21
brunobergot@gmail.com <brunobergot@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-12-03 16:09:02 +0000

    afficher le bouton des stats avec SPIP 2.1.x (reprise du code utilisé dans le pipeline formulaire_admin de l'extension statistiques)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@42642 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- minibando/minibando_pipelines.php
+++ minibando/minibando_pipelines.php
@@ -15,6 +15,25 @@ function minibando_formulaire_admin($flux) {
 
 	$flux['data'] = preg_replace('%(<!--minibando-->)%is', $minibando.'$1', $flux['data']);
 
+	// SPIP 2.1.x => pas d'extension statistiques...
+	if (!function_exists('stats_formulaire_admin')) {
+		if (
+		 isset($flux['args']['contexte']['objet'])
+		 AND $objet = $flux['args']['contexte']['objet']
+		 AND isset($flux['args']['contexte']['id_objet'])
+		 AND $id_objet = $flux['args']['contexte']['id_objet']
+		 ) {
+			if ($l = admin_stats($objet, $id_objet, $GLOBALS['var_preview'])) {
+				$btn = recuperer_fond('prive/bouton/statistiques', array(
+					'visites' => $l[0],
+					'popularite' => $l[1],
+					'statistiques' => $l[2],
+				));
+				$flux['data'] = preg_replace('%(<!--extra-->)%is', $btn.'$1', $flux['data']);				
+			}
+		}
+	}
+
 	return $flux;
 }
 
