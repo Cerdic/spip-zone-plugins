7cb7d59f35392bc5bc78cd97f258bdb2b102df91
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-12-07 13:00:23 +0000

    début d'amélioration de la configuration de memoization.
    Internationalisation également
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@42704 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ memoization/lang/memoization_fr.php
@@ -0,0 +1,36 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	// C
+	'cfg_titre' => 'M&#233;moization',
+	'cfg_description' => 'M&#233;moization. Cette page de configuration est r&#233;serv&#233;e au webmestre du site.',
+
+	// E
+	'explication_memcache_servers' => 'indiquer un serveur par ligne, sous la forme <kbd>host:port</kbd>',
+
+	// L
+	'label_cache_pages' => 'G&#233;rer le cache des pages',
+	'label_methode' => 'Choisir le mode de m&#233;moization',
+	'label_memcache_serveurs' => 'Serveur(s) memcache&#160;:',
+	'legend_cache_methode' => 'M&#233;thode de m&#233;moization',
+	'legend_cache_pages' => 'Cache des pages',
+	'legend_memcache' => 'R&#233;glages de memcache',
+	'lien_administration_memcache' => 'Administrer memcache',
+
+	// O
+	'option_cache_defaut' => 'Par la m&#233;moization d&#233;finie ci-dessus',
+	'option_cache_file' => 'Ancienne m&#233;thode du core',
+	'option_cache_filepage' => 'Par la m&#233;moization filecache',
+	'option_methode_apc' => 'APC',
+	'option_methode_defaut' => 'Auto-d&#233;tection',
+	'option_methode_eaccelerator' => 'EAccelerator',
+	'option_methode_filecache' => 'Fichiers (filecache)',
+	'option_methode_memcache' => 'Memcache',
+	'option_methode_nocache' => 'D&#233;sactiv&#233; (nocache)',
+	'option_methode_xcache' => 'XCache',
+
+);
+
+?>
