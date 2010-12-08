377b01fff309c101de3540c28305a9bb801317e0
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-12-08 11:06:57 +0000

    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@42745 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ traduction_articles_autrement/lang/taa_es.php
@@ -0,0 +1,19 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP nommé  genere le NOW()
+// langue / language = en
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// A
+'affichage_liste_compacte' => "Lista de artículos compacto",
+'affichage_edition_seule' => "Solo mode de edición para los artículos",
+
+// O
+'options' => "Opciones",
+
+// T
+'titre_plugin' => "Traducción de articulos repensada",
+
+);
+?>
