16c397c55fd98073a48bf6a07fdae3d78caf6f15
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-12-12 17:12:44 +0000

    langues (bando)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@42838 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- bandeau/lang/bando_es.php
+++ bandeau/lang/bando_es.php
@@ -9,29 +9,29 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// I
 	'icone_accueil' => 'Mis tareas',
-	'icone_administration' => 'Administraci&oacute;n',
+	'icone_administration' => 'Wartung',
 	'icone_configuration' => 'Configuraci&oacute;n',
 	'icone_edition' => 'Edici&oacute;n',
 	'icone_langage' => 'Mi lengua',
 	'icone_mes_infos' => 'Mis informaciones',
 	'icone_preferences' => 'Mis preferencias',
 	'icone_reactions' => 'Actividad',
-	'icone_squelette' => 'Esqueletos',
+	'icone_squelette' => 'Skelette',
 	'icone_suivi_edito' => 'Publicaci&oacute;n',
 	'icone_suivi_publication' => 'Seguir la publicaci&oacute;n',
 	'icone_visiter_site' => 'Ver el sitio p&uacute;blico',
 
 	// L
-	'label_adresses_secondaires' => 'Direcciones ALIAS del sitio p&uacute;', 
+	'label_adresses_secondaires' => 'Direcciones ALIAS del sitio p&uacute;blico',
 	'label_choix_langue' => 'Seleccine su lengua',
-	'label_slogan_site' => 'Eslogan del sitio',
+	'label_slogan_site' => 'Slogan del sitio',
 	'label_taille_ecran' => 'Largo de la pantalla',
 	'label_texte_et_icones_navigation' => 'Menu de navigai&oacute;n',
 	'label_texte_et_icones_page' => 'Afichar en la p&aacute;gina',
 
 	// T
-	'texte_redirection_adresses_secondaires' => 'Los ALIAS son las URL alternativas que tambi&eacute;n permiten el acceso a vustro sitio.
-	Estas direcciones son dirigidas autom&aacute;ticamente hacia la direcci&oacute;n principal. Indique un ALIAS por l&iacute;nea.',
+	'texte_redirection_adresses_secondaires' => 'Los Alias son URLs alternativas que tambi&eacute;n permiten acceder a su sitio.
+  Estas direcciones son automaticamente redirigidas hacia la direcci&oacute;n principal. Indicar una direcci&oacute;n por linea.',
 	'titre_config_langage' => 'Configurar la lengua',
 	'titre_configurer_preferences' => 'Configurar sus preferencias',
 	'titre_identite_site' => 'Identidad del sitio'
