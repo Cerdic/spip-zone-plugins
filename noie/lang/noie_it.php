fe377f321d2bb6b9b73a664d53097a473d5a3249
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-12-12 17:13:52 +0000

    langues (noie)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@42848 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- noie/lang/noie_it.php
+++ noie/lang/noie_it.php
@@ -8,8 +8,8 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// N
-	'noie_info' => 'Pour naviguer de fa&ccedil;on plus satisfaisante sur ce site et le reste du Web, nous vous recommandons d\'<strong>actualiser votre <a href="http://www.microsoft.com/france/windows/internet-explorer/">Internet Explorer</a></strong> ou d\'essayer un autre navigateur populaire comme <a href="http://www.mozilla-europe.org/fr/firefox/">Firefox</a>, <a href="http://www.google.fr/chrome">Chrome</a>, <a href="http://www.opera.com/">Opera</a> ou <a href="http://www.apple.com/fr/safari/">Safari</a>.', # NEW
-	'noie_titre' => 'Attention, votre Internet Explorer n\'est pas &agrave; jour&nbsp;!', # NEW
+	'noie_info' => 'Per navigare meglio su questo sito su tutto il Web, ti consigliamo di <strong>aggiornare il tuo <a href="http://www.microsoft.com/italy/windows/internet-explorer/">Internet Explorer</a></string> o di provare un altro browser popolare come <a href="http://www.mozilla-europe.org/it/firefox/">Firefox</a>, <a href="http://www.google.it/chrome/">Chrome</a>, <a href="http://www.opera.com/browser/">Opera</a> o <a href="http://www.apple.com/it/safari/">Safari</a>.',
+	'noie_titre' => 'Attenzione, il tuo Internet Explorer non &egrave; aggiornato!',
 
 	// T
 	'toocool_alt' => 'Too Cool for IE',
