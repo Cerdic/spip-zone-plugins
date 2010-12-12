9cada35002b006c85bd2982fd481925da8f19e94
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-12-12 17:13:58 +0000

    langues (piwik)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@42849 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spip_piwik/spip_piwik_2_0/lang/piwik_ca.php
+++ spip_piwik/spip_piwik_2_0/lang/piwik_ca.php
@@ -9,11 +9,13 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
 	'action_creer_site' => 'Crear el lloc',
+	'action_recuperer_liste' => 'R&eacute;cup&eacute;rer la liste des sites', # NEW
 
 	// C
 	'cfg_description_piwik' => 'Podeu introduir aqu&iacute; el vostre nom d\'usuari piwik i l\'adre&ccedil;a del servidor que gestiona les vostres estad&iacute;stiques.',
 	'cfg_erreur_recuperation_data' => 'Hi ha un error de comunicaci&oacute; amb el servidor, verifiqueu l\'adre&ccedil;a i el token',
 	'cfg_erreur_token' => 'El vostre token d\'identificaci&oacute; &eacute;s inv&agrave;lid',
+	'cfg_erreur_user_token' => 'La correspondance Nom d\'utilisateur / Token n\'est pas correcte.', # NEW
 
 	// E
 	'explication_adresse_serveur' => 'Entreu l\'adre&ccedil;a sense "http://" ni "https://" ni barra final',
@@ -21,9 +23,14 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'explication_exclure_ips' => 'Per excloure diverses adreces, separeu-les amb punts i comes',
 	'explication_identifiant_site' => 'La llista dels llocs disponibles al servidor Piwik s\'ha recuperat autom&agrave;ticament gr&agrave;cies a les informacions presentades. Seleccioneu de la seg&uuml;ent llista la que m&eacute;s us convingui',
 	'explication_mode_insertion' => 'Hi ha dues maneres d\'inserir a les p&agrave;gines el codi necessari per un bon funcionament del connector. Mitjan&ccedil;ant el pipeline "insert_head" (m&egrave;tode autom&agrave;tic per&ograve; poc configurable), o mitjan&ccedil;ant la inserci&oacute; d\'una etiqueta (m&egrave;tode manual inserint a la part inferior de les vostres p&agrave;gines l\'etiqueta #PIWIK) que, a m&eacute;s a m&eacute;s, &eacute;s totalment configurable.',
+	'explication_recuperer_liste' => 'Le lien ci-dessous vous permet de r&eacute;cup&eacute;rer la liste des sites que votre compte peut administrer sur le serveur Piwik.', # NEW
 	'explication_restreindre_statut_prive' => 'Escolliu aqu&iacute; els estats d\'usuaris que no es comptabilitzaran a les estad&iacute;stiques en l\'espai privat',
 	'explication_restreindre_statut_public' => 'Escolliu aqu&iacute; els estats d\'usuaris que no es comptabilitzaran a les estad&iacute;stiques a la part p&uacute;blica',
 	'explication_token' => 'El token d\'identificaci&oacute; est&agrave; disponible o b&eacute; a les vostres prefer&egrave;ncies personals o a la part API de vostre servidor Piwik',
+
+	// I
+	'info_aucun_site_compte' => 'Aucun site n\'est associ&eacute; &agrave; votre compte Piwik.', # NEW
+	'info_aucun_site_compte_demander_admin' => 'Vous devez demander &agrave; un administrateur de votre serveur Piwik d\'ajouter un site correspondant.', # NEW
 
 	// L
 	'label_adresse_serveur' => 'Adre&ccedil;a URL del servidor (https:// o http://)',
@@ -32,6 +39,8 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'label_exclure_ips' => 'Excloure certes adreces IP',
 	'label_identifiant_site' => 'L\'identificador del vostre lloc al servidor Piwik',
 	'label_mode_insertion' => 'Mode d\'inserci&oacute; a les p&agrave;gines p&uacute;bliques',
+	'label_piwik_user' => 'Compte utilisateur Piwik', # NEW
+	'label_recuperer_liste' => 'R&eacute;cup&eacute;rer la liste des sites sur le serveur Piwik', # NEW
 	'label_restreindre_auteurs_prive' => 'Restringir determinats usuaris connectats (privat)',
 	'label_restreindre_auteurs_public' => 'Restringir determinats usuaris connectats (p&uacute;blic)',
 	'label_restreindre_statut_prive' => 'Restringir determinats estats d\'usuaris a l\'espai privat',
--- spip_piwik/spip_piwik_2_0/lang/piwik_de.php
+++ spip_piwik/spip_piwik_2_0/lang/piwik_de.php
@@ -9,11 +9,13 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
 	'action_creer_site' => 'Site anlegen',
+	'action_recuperer_liste' => 'R&eacute;cup&eacute;rer la liste des sites', # NEW
 
 	// C
 	'cfg_description_piwik' => 'Hier k&ouml;nnen sie ihre PIWIK-Anmeldedaten sowie die Adresse des Servers f&uuml;r ihre Statistiken angeben.',
 	'cfg_erreur_recuperation_data' => 'Kommunikation mit dem Server gescheitert, bitte &uuml;berpr&uuml;fen sie die Adresse und das Token.',
 	'cfg_erreur_token' => 'Ihre Token-ID ist ung&uuml;ltig.',
+	'cfg_erreur_user_token' => 'La correspondance Nom d\'utilisateur / Token n\'est pas correcte.', # NEW
 
 	// E
 	'explication_adresse_serveur' => 'Geben sie die Adresse ohne "http://" oder "https://" und ohne Slash "/" am Ende an.',
@@ -21,9 +23,14 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'explication_exclure_ips' => 'Um mehrere Adressen auszuschlie&szlig;en, trennen sie sie bitte mit einem Semikolon.',
 	'explication_identifiant_site' => 'Die Liste der auf dem PIWIK-Server verf&uuml;gbaren Websites wurde aufgrund der angegebenen Informationen &uuml;bertragen. Bitte w&auml;hlen sie in der untenstehenden Liste die gew&uuml;nschte Site aus.',
 	'explication_mode_insertion' => 'Es gibt zwei Methoden, den f&uuml;r das Funktionieren des Plugins erforderlichen Code in die Seiten einzuf&uuml;gen: Mit der Pipeline "indert_head" (vollautomatisch aber ohne erweiterte Konfigurationsm&ouml;glichkeiten) oder durch Einf&uuml;gen des Tags #PIWIK in den Fu&szlig;bereich ihrer Skelette (dann k&ouml;nnen sie alle Konfigurationsoptionen nutzen).',
+	'explication_recuperer_liste' => 'Le lien ci-dessous vous permet de r&eacute;cup&eacute;rer la liste des sites que votre compte peut administrer sur le serveur Piwik.', # NEW
 	'explication_restreindre_statut_prive' => 'W&auml;hlen sie hier den Status der Besucher, deren Zugriffe auf das Redaktionssystem nicht in der Statistik erfa&szlig;t werden',
 	'explication_restreindre_statut_public' => 'W&auml;hlen sie hier den Status der Besucher, deren Zugriffe auf den &ouml;ffentlichen Teil der Website nicht in der Statistik erfa&szlig;t werden',
 	'explication_token' => 'Das Identifikations-Token finden sie in ihren pers&ouml;nlichen Einstellungen oder im API-Bereich ihres PIWIK-Servers.',
+
+	// I
+	'info_aucun_site_compte' => 'Aucun site n\'est associ&eacute; &agrave; votre compte Piwik.', # NEW
+	'info_aucun_site_compte_demander_admin' => 'Vous devez demander &agrave; un administrateur de votre serveur Piwik d\'ajouter un site correspondant.', # NEW
 
 	// L
 	'label_adresse_serveur' => 'Adresse (URL) des Servers (https:// oder http://)',
@@ -32,6 +39,8 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'label_exclure_ips' => 'Bestimmte IP-Adressen ausschlie&szlig;en',
 	'label_identifiant_site' => 'ID ihrer Website auf dem Piwik-Server',
 	'label_mode_insertion' => 'Typ des Einf&uuml;gens in die &ouml;ffentlichen Seiten',
+	'label_piwik_user' => 'Compte utilisateur Piwik', # NEW
+	'label_recuperer_liste' => 'R&eacute;cup&eacute;rer la liste des sites sur le serveur Piwik', # NEW
 	'label_restreindre_auteurs_prive' => 'Einschr&auml;nkungen f&uuml;r manche angemeldeten Besucher (Redaktion)',
 	'label_restreindre_auteurs_public' => 'Einschr&auml;nkungen f&uuml;r manche angemeldeten Besucher (&ouml;ffentlicher Bereich)',
 	'label_restreindre_statut_prive' => 'Einschr&auml;nkungen f&uuml;r manche Mitglieder im Redaktionssystem',
