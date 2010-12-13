d12d1099d56ea49009ce1687f9e2d7f5d37c0c75
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-12-13 08:59:50 +0000

    conflict avec thélia, remplacer chemin() par find_in_path()
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@42873 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- traduction_articles_autrement/taa_pipelines.php
+++ traduction_articles_autrement/taa_pipelines.php
@@ -1,7 +1,7 @@
 <?php
 function taa_header_prive($flux){
 
-    $flux .= '<link rel="stylesheet" href="'.chemin('css/taa_styles.css').'" type="text/css" media="all" />';
+    $flux .= '<link rel="stylesheet" href="'.find_in_path('css/taa_styles.css').'" type="text/css" media="all" />';
  	return $flux;	
 
  }
