5856328c0242198bbf39948093aebddfba9f1733
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-12-13 22:03:52 +0000

    plugin.xml en anglais et amélioration du fichier de langue en anglais ... en même temps on vire tous ces tabs qui ne servent à rien sauf à se prendre le choux
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@42888 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- abomailmans/lang/abomailmans_en.php
+++ abomailmans/lang/abomailmans_en.php
@@ -4,108 +4,115 @@
 // $Id: abomailmans_fr.php 26976 2009-03-01 00:33:38Z kent1@arscenic.info $
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-// A
-	'abomailmans' 					=> 'Mailmans',
-	'abonne'						=> 'I subscribe',
-	'active'                        => 'Active',
-	'aucune_langue'					=> 'None specified',
+	// A
+	'abomailmans' => 'Mailmans',
+	'abonne' => 'I subscribe',
+	'active' => 'Active',
+	'activation' => 'Activation',
+	'aucune_langue' => 'None specified',
 
-// B
-	'bouton_listes_diffusion'		=> 'Mailing lists',
-	'btn_abonnement'                => 'Subscribe',
-	'btn_desabonnement'             => 'Unsubscribe',
+	// B
+	'bouton_listes_diffusion' => 'Mailing lists',
+	'btn_abonnement' => 'Subscribe',
+	'btn_desabonnement' => 'Unsubscribe',
 
-// C
-	'choisir_liste'					=> 'You should choose a list.',
-	'contenu_date'					=> 'Content since this date',
-	'creation_droits_insuffisants'	=> 'Your rights are insufficients',
+	// C
+	'choisir_liste' => 'You should choose a list.',
+	'contenu_date' => 'Content since this date',
+	'creation_droits_insuffisants' => 'Your rights are insufficients',
 
-// D
-	'desactive'                     => 'Deactivated',
+	// D
+	'desactive' => 'Disabled',
+	'destinataire' => 'Recipient',
 
-// E
-	'emailliste_abomailman'			=> 'The email address of the list',
-	'emailliste_abosympa'           => 'The email address of the Sympa admin',
-	'email'							=> 'Email address',
+	// E
+	'email' => 'Email address',
+	'emailliste_abomailman' => 'The email address of the list',
+	'emailliste_abosympa' => 'The email address of the Sympa admin',
 	'emailliste_subscribe' => 'Subscribe',
 	'emailliste_unsubscribe' => 'Unsubscribe',
-	'email_abonnement'				=> 'Your email address',
-	'email_envoye'					=> 'The email has been sent to the list : @liste@.',
+	'emails_a_renseigner' => 'Email addresses to provide',
+	'email_abonnement' => 'Your email address',
+	'email_envoye' => 'The email has been sent to the list : @liste@.',
+	'email_oublie' => 'You forgot your email address',
+	'envoi_apercu' => 'Previsualization',
+	'envoi_confirmer' => 'Confirm and send',
+	'envoi_liste_parametres' => 'List of parameters [Facultatif]',
+	'envoi_parametres' => 'Settings',
+	'envoi_regulier' => 'Automatics mail',
+	'envoi_regulier_info' => 'Leave empty if you do not want to send automatically',
+	'envoi_regulier_tous_les' => 'Send each',
 	'envoi_vers'=> 'send to',
-	'email_oublie'					=> 'You forgot your email address',
-	'envoi_apercu'					=> 'Previsualization',
-	'envoi_confirmer'				=> 'Confirm and send',
-	'envoi_regulier'				=> 'Automatics mail',
-	'envoi_regulier_info'			=> 'Keep empty if you do not want to send automatically',
-	'envoi_regulier_tous_les'			=> 'Send each',
-	'envoi_liste_parametres'	=>'List of parameters [Facultatif]',
-	'periodicite'			=> ' days.',
-	'envoyer_courier'				=> 'Send an email',
-	'envoyer_courier_liste'			=> 'Send this content to this list :',
-	'envoyer_mailmans'				=> 'Select the template and his content',
-	'erreur_email_liste_oublie'		=> 'The email address of the list is mandatory',
-	'explication_email_sympa'		=> 'If this field is filled in, the list is considered as a "Sympa" list, if not, as a "Mailman" list.',
-	'explication_email_subscribe'		=> 'Email for subscribe, something like <code>suffix+subscribe@exemple.org</code>',
-	'explication_email_unsubscribe'		=> 'Email for unsubscribe',
+	'envoyer_courier' => 'Send an email',
+	'envoyer_courier_liste'	=> 'Send this content to this list :',
+	'envoyer_mailmans' => 'Select the template and his content',
+	'erreur_email_liste_oublie' => 'The email address of the list is mandatory',
+	'explication_email_sympa' => 'If this field is filled in, the list is considered as a "Sympa" list, if not, as a "Mailman" list.',
+	'explication_email_subscribe' => 'Email for subscribe, something like <code>suffix+subscribe@exemple.org</code>',
+	'explication_email_unsubscribe' => 'Email for unsubscribe',
 
-// I
-	'icone_ajouter_liste'			=> 'Add a new list',
-	'icone_envoyer_mail_liste'		=> 'Send an e-mail to the lists from the content of this site',
-	'info_sisympa'                  => '[Necessary for a Sympa list]',
-	'insciption_listes_legende'		=> 'Subscription to the mailing lists',
-	'inscription_lettres_legende'	=> 'Subscription to the newsletters<br />and mailing lists',
+	// I
+	'icone_ajouter_liste' => 'Add a new list',
+	'icone_envoyer_mail_liste' => 'Send an e-mail to the lists from the content of this site',
+	'info_sisympa' => '[Mandatory for a Sympa list]',
+	'insciption_listes_legende'	=> 'Subscription to the mailing lists',
+	'inscription_lettres_legende' => 'Subscription to the newsletters<br />and mailing lists',
 
-// J
-	'je_m_abonne'					=> 'Check to confirm the subscription or unsubscription.',
+	// J
+	'je_m_abonne' => 'Check to confirm the subscription or unsubscription.',
 
-// L
-	'label_etat_liste'				=> 'Status of the list',
-	'langue_liste'					=> 'Language of the list',
-	'les_listes_mailmans'			=> 'The Mailman\'s or Sympa\'s lists filled',
-	'lire_article' 					=> 'Read the article',
-	'liste_creee'					=> 'The list number @id@ (@titre@) has been created.',
-	'liste_non_existante'			=> 'The asked list doesn\'t exist or has been deleted',
-	'liste_oublie'                  => 'You forgot to check a list.',
-	'liste_supprimee'				=> 'The list number @id@ (@titre@) has been deleted.',
-	'liste_updatee'					=> 'The list number @id@ (@titre@) has been updated.',
+	// L
+	'label_etat_liste' => 'Status of the list',
+	'langue_liste' => 'Language of the list',
+	'les_listes_mailmans' => 'The Mailman\'s or Sympa\'s lists filled',
+	'lire_article' => 'Read the article',
+	'liste_creee' => 'The list number @id@ (@titre@) has been created.',
+	'liste_non_existante' => 'The asked list doesn\'t exist or has been deleted',
+	'liste_oublie' => 'You forgot to check a list.',
+	'liste_supprimee' => 'The list number @id@ (@titre@) has been deleted.',
+	'liste_updatee' => 'The list number @id@ (@titre@) has been updated.',
 
-// M
-	'message'						=> 'Introduction of your email, before the site\'s content',
-	'message_confirmation_a'      	=> 'A subscription request to the following lists has been sent :',
+	// M
+	'message' => 'Introduction of your email, before the site\'s content',
+	'message_confirmation_a' => 'A subscription request to the following lists has been sent :',
 	'message_confirmation_unique_a' => 'A subscription request to the following list has been sent :',
-	'message_confirmation_d'        => 'An unsubscribing request from the lists below has been sent. ',
-	'message_confirmation_unique_d'	=> 'An unsubscribing request from the list below has been sent. ',
-	'message_confirm_suite'         => 'To validate your request, please reply to the confirmation email that you will receive.',
-	'mot'							=> 'And list the articles linked to the keyword',
+	'message_confirmation_d' => 'An unsubscribing request from the lists below has been sent. ',
+	'message_confirmation_unique_d' => 'An unsubscribing request from the list below has been sent. ',
+	'message_confirm_suite' => 'To validate your request, please reply to the confirmation email that you will receive.',
+	'mot' => 'And list the articles linked to the keyword',
 
-// N
-	'nom'							=> 'Name and first name (optional)',
+	// N
+	'nom' => 'Name and first name (optional)',
 
-// P
-	'prenom'						=> 'First name',
+	// P
+	'periodicite' => ' days.',
+	'prenom' => 'First name',
 
-// R
-	'rubrique'						=> 'And list the articles of the section',
+	// R
+	'rubrique' => 'And list the articles of the section',
 
-// S
-	'souhaite_rester'				=> 'I wish to keep informed',
-	'sujet'							=> 'Subject of the mail',
-	'sujet_obligatoire'				=> 'The subject is necessary.',
-	'supprimer'						=> 'Delete',
-	'sympa_message_confirmation'	=> 'A validation email has been sent to the address : ',
+	// S
+	'souhaite_rester' => 'I wish to keep informed',
+	'sujet' => 'Subject of the mail',
+	'sujet_obligatoire' => 'The subject is mandatory.',
+	'supprimer' => 'Delete',
+	'suppression_definitive' => 'Permanent removal !',
+	'sympa_message_confirmation' => 'A validation email has been sent to the address : ',
 
-// T
-	'template'						=> 'Choose a template and his content',
-	'template_defaut'			=> 'Default template',
-	'titre_abomailman'				=> 'Title of the list',
-	'titre_liste_obligatoire'		=> 'The title of the list is mandatory',
-	'toute_liste'					=> 'All mailing lists',
+	// T
+	'template' => 'Choose a template and his content',
+	'template_defaut' => 'Default template',
+	'texte_descriptif' =>'Description',
+	'titre_abomailman' => 'Title of the list',
+	'titre_liste_obligatoire' => 'The title of the list is mandatory',
+	'toute_liste' => 'All mailing lists',
 
-// V
-	'veut_s_abonner'=>'want to subscribe',
-	'veut_se_desabonner'=>'want to unsubscribe',
-	'verifier_formulaire'			=> 'Please verify the filling of the form.',
-	'votre_email'					=> 'Your email',
+	// V
+	'veut_s_abonner' => 'want to subscribe',
+	'veut_se_desabonner' => 'want to unsubscribe',
+	'verifier_formulaire' => 'Please verify the filling of the form.',
+	'voir_modele_depuis' =>'See an example of the template with',
+	'votre_email' => 'Your email',
 
 );
 
--- abomailmans/lang/abomailmans_fr.php
+++ abomailmans/lang/abomailmans_fr.php
@@ -4,114 +4,114 @@
 // $Id$
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-//A
-	'abomailmans' 					=> 'Mailmans',
-	'abonne'					=> 'Je m\'abonne',
-	'activation'					=>'Activation',#NEW
-	'active'                       			=> 'Active',
-	'aucune_langue'					=> 'Aucune sp&eacute;cifique',
+	// A
+	'abomailmans' => 'Mailmans',
+	'abonne' => 'Je m\'abonne',
+	'activation' => 'Activation',
+	'active' => 'Active',
+	'aucune_langue' => 'Aucune sp&eacute;cifique',
 
-//B
-	'bouton_listes_diffusion'		=> 'Les listes de diffusion',
-	'btn_abonnement'                => 'S\'abonner',
-	'btn_desabonnement'             => 'Se d&eacute;sabonner',
-	'emails_a_renseigner' =>'Emails &agrave; renseigner',
+	// B
+	'bouton_listes_diffusion' => 'Les listes de diffusion',
+	'btn_abonnement' => 'S\'abonner',
+	'btn_desabonnement' => 'Se d&eacute;sabonner',
 
-//C
-	'choisir_liste'					=> 'Vous devez choisir une liste.',
-	'contenu_date'					=> 'Contenu a partir de cette date',
-	'creation_droits_insuffisants'	=> 'Vos droits sont insuffisants...',
+	//C
+	'choisir_liste' => 'Vous devez choisir une liste.',
+	'contenu_date' => 'Contenu a partir de cette date',
+	'creation_droits_insuffisants' => 'Vos droits sont insuffisants...',
 
-//D
-	'desactive'                     => 'D&eacute;sactiv&eacute;e',
-	'destinataire'                     => 'Destinataire',#NEW
+	// D
+	'desactive' => 'D&eacute;sactiv&eacute;e',
+	'destinataire' => 'Destinataire',
 
-//E
-	'emailliste_abomailman'			=> 'L\'adresse e-mail de la liste',
-	'emailliste_abosympa'           => 'L\'adresse e-mail de l\'administrateur Sympa',
-	'email'							=> 'E-mail',
+	// E
+	'email' => 'E-mail',
+	'emailliste_abomailman' => 'L\'adresse e-mail de la liste',
+	'emailliste_abosympa' => 'L\'adresse e-mail de l\'administrateur Sympa',
 	'emailliste_subscribe' => 'Abonnement',
-	'emailliste_unsubscribe' => 'Desabonnement',                                          
-	'email_abonnement'				=> 'Votre adresse e-mail',
-	'email_envoye'					=> 'L\'e-mail a &eacute;t&eacute; envoy&eacute; &agrave; la liste de diffusion : @liste@.',
+	'emailliste_unsubscribe' => 'Desabonnement',
+	'emails_a_renseigner' => 'Emails &agrave; renseigner',                                          
+	'email_abonnement' => 'Votre adresse e-mail',
+	'email_envoye' => 'L\'e-mail a &eacute;t&eacute; envoy&eacute; &agrave; la liste de diffusion : @liste@.',
+	'email_oublie' => 'Vous avez oubli&eacute; votre adresse e-mail',
+	'envoi_apercu' => 'Aper&ccedil;u',
+	'envoi_confirmer' => 'Confirmer et envoyer',
+	'envoi_liste_parametres' =>'Liste des param&egrave;tres [Facultatif]',
+	'envoi_parametres' => 'Param&eacute;tres',
+	'envoi_regulier' => 'Envois automatiques',
+	'envoi_regulier_info' => 'Laisser vide pour ne pas avoir d\'envois automatiques',
+	'envoi_regulier_tous_les' => 'Envoi tous les',
 	'envoi_vers'=> 'envoi vers',
-	'email_oublie'					=> 'Vous avez oubli&eacute; votre adresse e-mail',
-	'envoi_apercu'					=> 'Aper&ccedil;u',
-	'envoi_confirmer'				=> 'Confirmer et envoyer',
-	'envoi_regulier'				=> 'Envois automatiques',#NEW
-	'envoi_regulier_info'				=> 'Laisser vide pour ne pas avoir d\'envois automatiques',#NEW
-	'envoi_regulier_tous_les'			=> 'Envoi tous les',#NEW
-	'envoi_parametres' 				=> 'Param&eacute;tres',#NEW
-	'envoi_liste_parametres'			=>'Liste des param&egrave;tres [Facultatif]',#NEW
-	'periodicite'					=> ' jours.',#NEW
-	'envoyer_courier'				=> 'Envoyer un courrier',
-	'envoyer_courier_liste'			=> 'Envoyer ce courrier &agrave; cette liste de diffusion :',
-	'envoyer_mailmans'				=> 'S&eacute;l&eacute;ctionner le mod&egrave;le et son contenu',
-	'erreur_email_liste_oublie'		=> 'L\'adresse email de la liste est obligatoire',
-	'explication_email_sympa'		=> 'Si ce champ est renseign&eacute;, la liste est consid&eacute;r&eacute;e comme une liste de serveur "Sympa", dans le cas contraire comme une liste "Mailman".',
-	'explication_email_subscribe'		=> 'Email d\'abonnement, de style <code>suffixe+subscribe@exemple.org</code>',
-	'explication_email_unsubscribe'		=> 'Email de desabonnement',
+	'envoyer_courier' => 'Envoyer un courrier',
+	'envoyer_courier_liste' => 'Envoyer ce courrier &agrave; cette liste de diffusion :',
+	'envoyer_mailmans' => 'S&eacute;l&eacute;ctionner le mod&egrave;le et son contenu',
+	'erreur_email_liste_oublie' => 'L\'adresse email de la liste est obligatoire',
+	'explication_email_sympa' => 'Si ce champ est renseign&eacute;, la liste est consid&eacute;r&eacute;e comme une liste de serveur "Sympa", dans le cas contraire comme une liste "Mailman".',
+	'explication_email_subscribe' => 'Email d\'abonnement, de style <code>suffixe+subscribe@exemple.org</code>',
+	'explication_email_unsubscribe' => 'Email de d&eacute;sabonnement',
 	
-//I
-	'icone_ajouter_liste'			=> 'Ajouter une nouvelle liste',
-	'icone_envoyer_mail_liste'		=> 'Envoyer un e-mail aux lettres &agrave; partir du contenu de ce site',
-	'info_sisympa'                  => '[Obligatoire si liste Sympa]',
-	'insciption_listes_legende'		=> 'Abonnement au listes de diffusion',
-	'inscription_lettres_legende'	=> 'Abonnement aux listes de <br />diffusion et de discussions',
+	// I
+	'icone_ajouter_liste' => 'Ajouter une nouvelle liste',
+	'icone_envoyer_mail_liste' => 'Envoyer un e-mail aux lettres &agrave; partir du contenu de ce site',
+	'info_sisympa' => '[Obligatoire si liste Sympa]',
+	'insciption_listes_legende' => 'Abonnement au listes de diffusion',
+	'inscription_lettres_legende' => 'Abonnement aux listes de <br />diffusion et de discussions',
 
-//J
-	'je_m_abonne'					=> 'Cochez pour valider l\'abonnement ou le d&eacute;sabonnement.',
+	// J
+	'je_m_abonne' => 'Cochez pour valider l\'abonnement ou le d&eacute;sabonnement.',
 
-//L
-	'label_etat_liste'				=> '&Eacute;tat de la liste',
-	'langue_liste'					=> 'Langue de la liste',
-	'les_listes_mailmans'			=> 'Les listes mailmans ou sympas renseign&eacute;es',
-	'lire_article' 					=> 'Lire l\'article',
-	'liste_creee'					=> 'La liste num&eacute;ro @id@ (@titre@) a &eacute;t&eacute; cr&eacute;&eacute;e.',
-	'liste_non_existante'			=> 'La liste demand&eacute;e n\'existe pas ou a &eacute;t&eacute; supprim&eacute;e',
-	'liste_oublie'                  => 'Vous avez oubli&eacute; de cocher une liste !',
-	'liste_supprimee'				=> 'La liste num&eacute;ro @id@ (@titre@) a &eacute;t&eacute; supprim&eacute;e.',
-	'liste_updatee'					=> 'La liste num&eacute;ro @id@ (@titre@) a &eacute;t&eacute; mise &agrave; jour.',
+	// L
+	'label_etat_liste' => '&Eacute;tat de la liste',
+	'langue_liste' => 'Langue de la liste',
+	'les_listes_mailmans' => 'Les listes mailmans ou sympas renseign&eacute;es',
+	'lire_article' => 'Lire l\'article',
+	'liste_creee' => 'La liste num&eacute;ro @id@ (@titre@) a &eacute;t&eacute; cr&eacute;&eacute;e.',
+	'liste_non_existante' => 'La liste demand&eacute;e n\'existe pas ou a &eacute;t&eacute; supprim&eacute;e',
+	'liste_oublie' => 'Vous avez oubli&eacute; de cocher une liste !',
+	'liste_supprimee' => 'La liste num&eacute;ro @id@ (@titre@) a &eacute;t&eacute; supprim&eacute;e.',
+	'liste_updatee' => 'La liste num&eacute;ro @id@ (@titre@) a &eacute;t&eacute; mise &agrave; jour.',
 
-//M
-	'message'						=> 'Introduction &agrave; votre courrier, avant le contenu issu du site',
-	'message_confirmation_a'      	=> 'Une demande d\'abonnement aux listes suivantes vient d\'&ecirc;tre envoy&eacute;e :',
+	// M
+	'message' => 'Introduction &agrave; votre courrier, avant le contenu issu du site',
+	'message_confirmation_a' => 'Une demande d\'abonnement aux listes suivantes vient d\'&ecirc;tre envoy&eacute;e :',
 	'message_confirmation_unique_a' => 'Une demande d\'abonnement &agrave; la liste suivante vient d\'&ecirc;tre envoy&eacute;e :',
-	'message_confirmation_d'        => 'Une demande de d&eacute;sabonnement aux listes suivantes vient d\'&ecirc;tre envoy&eacute;e. ',
-	'message_confirmation_unique_d'	=> 'Une demande de d&eacute;sabonnement &agrave; la liste suivante vient d\'&ecirc;tre envoy&eacute;e. ',
-	'message_confirm_suite'         => 'Pour valider votre demande, r&eacute;pondez &agrave; la demande de confirmation que vous allez recevoir par mail.',
-	'mot'							=> 'Et lister les articles du mot cl&eacute;',
+	'message_confirmation_d' => 'Une demande de d&eacute;sabonnement aux listes suivantes vient d\'&ecirc;tre envoy&eacute;e. ',
+	'message_confirmation_unique_d' => 'Une demande de d&eacute;sabonnement &agrave; la liste suivante vient d\'&ecirc;tre envoy&eacute;e. ',
+	'message_confirm_suite' => 'Pour valider votre demande, r&eacute;pondez &agrave; la demande de confirmation que vous allez recevoir par mail.',
+	'mot' => 'Et lister les articles du mot cl&eacute;',
 
-//N
-	'nom'							=> 'Nom et pr&eacute;nom (facultatif)',
+	// N
+	'nom' => 'Nom et pr&eacute;nom (facultatif)',
 
-//P
-	'prenom'						=> 'Pr&eacute;nom',
+	// P
+	'periodicite' => ' jours.',
+	'prenom' => 'Pr&eacute;nom',
 
-//R
-	'rubrique'						=> 'Et lister les articles de la rubrique',
+	// R
+	'rubrique' => 'Et lister les articles de la rubrique',
 
-//S
-	'souhaite_rester'				=> 'Je souhaite rester inform&eacute;-e',
-	'sujet'							=> 'Sujet du courrier',
-	'sujet_obligatoire'				=> 'Le sujet est obligatoire.',
-	'supprimer'						=> 'Supprimer',
-	'suppression_definitive' 	=>'Suppression d&eacute;finitive !',#NEW
-	'sympa_message_confirmation'	=> 'Un email de validation a &eacute;t&eacute; envoy&eacute; &agrave l\'adresse : ',
+	// S
+	'souhaite_rester' => 'Je souhaite rester inform&eacute;-e',
+	'sujet' => 'Sujet du courrier',
+	'sujet_obligatoire' => 'Le sujet est obligatoire.',
+	'supprimer' => 'Supprimer',
+	'suppression_definitive' => 'Suppression d&eacute;finitive !',
+	'sympa_message_confirmation' => 'Un email de validation a &eacute;t&eacute; envoy&eacute; &agrave l\'adresse : ',
 
-//T
-	'template'				=> 'Choisissez le mod&egrave;le et son contenu',
-	'template_defaut'			=> 'Mod&egrave;le par defaut',#NEW
-	'texte_descriptif'			=>'Descriptif',#NEW
-	'titre_abomailman'			=> 'Titre de la liste',
-	'titre_liste_obligatoire'		=> 'Le titre de la liste est obligatoire',
-	'toute_liste'					=> 'Toutes les listes de diffusions',
+	// T
+	'template' => 'Choisissez le mod&egrave;le et son contenu',
+	'template_defaut' => 'Mod&egrave;le par defaut',
+	'texte_descriptif' =>'Descriptif',
+	'titre_abomailman' => 'Titre de la liste',
+	'titre_liste_obligatoire' => 'Le titre de la liste est obligatoire',
+	'toute_liste' => 'Toutes les listes de diffusions',
 
-//V
-	'veut_s_abonner'=>'veut s\'abonner',
-	'veut_se_desabonner'=>'veut se desabonner',
-	'verifier_formulaire'			=>	'V&eacute;rifiez le remplissage du formulaire.',
-	'voir_modele_depuis'			=>'Voir un exemple du mod&eacute;le avec', #NEW
-	'votre_email'					=> 'Votre email',
+	// V
+	'veut_s_abonner' => 'veut s\'abonner',
+	'veut_se_desabonner' => 'veut se desabonner',
+	'verifier_formulaire' => 'V&eacute;rifiez le remplissage du formulaire.',
+	'voir_modele_depuis' =>'Voir un exemple du mod&eacute;le avec',
+	'votre_email' => 'Votre email',
 );
 ?>
