0482092ce41fb5f56d177b4df387aa700404d783
marcimat@rezo.net <marcimat@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-12-15 18:39:43 +0000

    Suivre les évolutions de SPIP
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@42935 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- porte_plume_extras/codes/pp_codes_fonctions.php
+++ porte_plume_extras/codes/pp_codes_fonctions.php
@@ -1,6 +1,10 @@
 <?php
 
-@define('_URL_BROWSER_TRAC', 'http://trac.rezo.net/trac/spip/browser/spip/');
+// trac SVN
+# @define('_URL_BROWSER_TRAC', 'http://trac.rezo.net/trac/spip/browser/spip/');
+
+// trac GIT
+@define('_URL_BROWSER_TRAC', 'http://core.spip.org/trac/spip/browser/@file@?rev=spip-2.1');
 
 /*
  * Un raccourci pour des chemins vers de trac
@@ -10,6 +14,12 @@
  */
 if (!function_exists('glossaire_trac')) {
 	function glossaire_trac($texte, $id=0) {
+		// si @file@ present dans le define, on remplace par le texte
+		if (false !== strpos(_URL_BROWSER_TRAC, '@file@')) {
+			return str_replace('@file@', $texte, _URL_BROWSER_TRAC) . ($id ? '#L'.$id : '');
+		}
+		
+		// sinon, on met bout a bout comme avant...
 		return _URL_BROWSER_TRAC . $texte . ($id ? '#L'.$id : '');
 	} 
 }
