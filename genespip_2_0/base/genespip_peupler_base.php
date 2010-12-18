b2278217bf776e15bbef2702ac81af1c4e0ac7c5
pierrekuhn82@gmail.com <pierrekuhn82@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-12-18 22:09:06 +0000

    Plus français.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@43003 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- genespip_2_0/base/genespip_peupler_base.php
+++ genespip_2_0/base/genespip_peupler_base.php
@@ -10,7 +10,7 @@ function peupler_base_genespip() {
 
 	sql_insertq_multi('spip_genespip_type_evenements', array(
 		array('id_type_evenement'=>'1','type_evenement'=>'naissance','clair_evenement'=>'<multi>[fr]Naissance<multi>'),
-		array('id_type_evenement'=>'2','type_evenement'=>'dece','clair_evenement'=>'<multi>[fr]D&eacute;c&eacute;d&eacute;</multi>'),
+		array('id_type_evenement'=>'2','type_evenement'=>'dece','clair_evenement'=>'<multi>[fr]D&eacute;c&eacute;s</multi>'),
 		array('id_type_evenement'=>'3','type_evenement'=>'mariage','clair_evenement'=>'<multi>[fr]Mariage</multi>'),
 		)
 	);
