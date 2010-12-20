2d7c26b5a00e2b764914c06249d05307a4194518
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-12-20 14:53:19 +0000

    Traduction rapide en anglais
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@43037 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ porte_plume_extras/partout/lang/ppp_en.php
@@ -0,0 +1,17 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	// C
+	'cfg_attention' => 'Warning',
+	'cfg_description' => 'according to your templates, be sure to only activate the Quill on fields where the use will not create xhtml error.',
+	'cfg_titre' => 'Quill everywhere',
+	
+	// L
+	'label_hauteur_champ' => 'Half size of the screen',
+	'legend_barre_typo' => 'Enable the editing toolbar on :',
+	'legend_hauteur_champ' => 'Height of the text field on articles',
+);
+
+?>
---
+++ porte_plume_extras/partout/lang/ppp_fr.php
@@ -0,0 +1,17 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	// C
+	'cfg_attention' => 'Attention',
+	'cfg_description' => 'en fonction de vos squelettes, veillez &agrave; n\'activer le Porte Plume que sur des champs pour lesquels l\'utilisation des raccourcis n\'engendrera pas d\'erreur xhtml.',
+	'cfg_titre' => 'Porte Plume Partout',
+
+	// L
+	'label_hauteur_champ' => 'Demi-hauteur de l\'&eacute;cran',
+	'legend_barre_typo' => 'Activer la barre typographique sur :',
+	'legend_hauteur_champ' => 'Hauteur du champ texte pour les articles',
+);
+
+?>
