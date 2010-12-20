68c53e40d5b09119a57b6f78a24b6e4a5d517baa
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-12-20 21:42:32 +0000

    un oubli de chaine de langue
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@43048 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- apropos/lang/apropos_fr.php
+++ apropos/lang/apropos_fr.php
@@ -4,6 +4,7 @@
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+	
 
 	// P
 	'les_plugins' => 'Il utilise les plugins suivants :',
@@ -11,6 +12,10 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// S
 	'site_utilise' => 'Ce site utilise un <a href="http://fr.wikipedia.org/wiki/Systeme_de_gestion_de_contenu" title="D&eacute;finition de CMS sur Wikipedia">CMS</a>. Il est &eacute;labor&eacute; et tourne avec <a href="http://www.spip.net/fr" title="Aller sur le site officiel de SPIP">SPIP</a> version ',
 	'spip_contrib' => 'Pour plus d\'informations sur ces plugins, rendez vous sur le site <a href="http://www.spip-contrib.net/" title="L&rsquo;espace des contributions externes, qui recense l&rsquo;ensemble des fonctionnalit&eacute;s suppl&eacute;mentaires mises &agrave; disposition &agrave; la communaut&eacute; par les utilisateurs de SPIP.">http://www.spip-contrib.net</a>.',
+	
+	// T
+	
+	'titre'			=> 'À propos'
 
 );
 
