d12c0fc125124f0d69b5b7fc4d290c2aaa54a5bc
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-12-24 09:44:35 +0000

    indication de la rubrique de référence dans l'onglet correspondant
    ajout du fichier de langue en fr
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@43118 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ traduction_rubriques_autrement/lang/tra_fr.php
@@ -0,0 +1,14 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP nommé  genere le NOW()
+// langue / language = fr
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// A
+'ajouter_traduction' => "Ajouter une traduction",
+
+// R
+'rubrique_reference' => "Rubrique de r&eacute;f&eacute;rence",
+);
+?>
