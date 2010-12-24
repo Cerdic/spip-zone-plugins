4ecfab1af24bcb14a6df002e0a482daeec369c3e
olivier.durif@aliceadsl.fr <olivier.durif@aliceadsl.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-12-24 10:54:25 +0000

    inscription2_abomailmans.log
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@43123 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ inscription2/lang/i2_abomailmans_fr.php
@@ -0,0 +1,36 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://trac.rezo.net/spip-zone/_plugins_/_test_/inscription2/inscription2_2_0/lang
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'choisir_listes' => 'Choisissez ci-dessous les listes de diffusion &#224; proposer aux nouveaux adh&eacute;rents&nbsp;: ',
+	
+	// E
+	'envoi_vers'=> 'envoi vers',
+	'exp_liste' => 'Souhaitez-vous vous abonner à notre liste de diffusion&nbsp;?',
+	'exp_listes' => 'Choisissez ci-dessous les listes de diffusion auxquelles vous souhaitez vous abonner',
+	
+	// D
+	'deselect_listes' => '> tout d&#233;s&eacute;lectionner',
+			
+	// N
+	'liste' => 'Listes de diffusion abomailmans',
+	'liste_label' => 'Abonnement aux listes de diffusion',
+	
+	// O
+	'optout' => 'Cases &agrave; cocher non pr&#233;coch&#233;es (optout)',
+	
+	// S
+	'select_listes' => '(vous pouvez s&#233;lectionner plusieurs listes avec la touche shift)',
+	
+	// V
+	'veut_s_abonner' => 'veut s\'abonner',
+	
+	// Z
+	'z' => 'zzz'
+);
+
+?>
