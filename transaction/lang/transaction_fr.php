2b664c9d55fb99738448392f62874b18a65eb477
apachot@openstudio.fr <apachot@openstudio.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-12-24 09:59:27 +0000

    Développement d'une extension pour le plugin Formidable, ajoutant dans les formulaires deux nouveaux types de champs : "sélection d'un montant parmi une liste de montants proposés" et "saisie d'un montant libre", pour l'interfacage avec les API bancaires.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@43119 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ transaction/lang/transaction_fr.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/transaction/lang/
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	'saisie_montant_titre' => 'Saisie montant',
+	'saisie_montant_selection_titre' => 'S&eacute;lection montant',
+	'saisie_radio_defaut_choix1' => '15 &euro;',
+	'saisie_radio_defaut_choix2' => '50 &euro;',
+	'saisie_radio_defaut_choix3' => '100 &euro;'
+	
+);
+
+?>
