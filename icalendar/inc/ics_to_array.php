b32aa63a9ef70f755106cda77c13bfafd5121e1f
fil@rezo.net <fil@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-12-25 22:03:30 +0000

    pour simplifier l'usage de la boucle icalendar, noter les dates cles dans un format plus facile a recuperer : {{{ #VALEUR{dtstart/str} }}}
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@43142 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- icalendar/inc/ics_to_array.php
+++ icalendar/inc/ics_to_array.php
@@ -7,12 +7,31 @@ function inc_ics_to_array($u) {
 	$tmp = _DIR_TMP . 'ics-'.md5($u);
 	ecrire_fichier($tmp, $u);
 
-	$v = new vcalendar();
-	$v->setConfig( 'filename', $tmp );
-	$v->parse();
+	$cal = new vcalendar();
+	$cal->setConfig( 'filename', $tmp );
+	$cal->parse();
 
 	supprimer_fichier($tmp);
 
-	return($v->components);
+	# noter les dates cles dans un format plus facile a recuperer
+	foreach($cal->components as $k => &$v) {
+		foreach(array('dtstart', 'dtend', 'dtstamp', 'lastmodified', 'created')
+		as $champ) {
+			if (isset($v->$champ)) {
+				$w = &$v->$champ;
+				$date = table_valeur($w, "/value");
+				$w['str'] = sprintf('%04d-%02d-%02d %02d:%02d:%02d',
+					$date['year'],
+					$date['month'],
+					$date['day'],
+					$date['hour'],
+					$date['min'],
+					$date['sec']
+				);
+			}
+		}
+	}
+
+	return($cal->components);
 }
 
