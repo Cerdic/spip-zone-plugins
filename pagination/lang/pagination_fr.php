9d9c2b6b1c2aad38ac778e4afcdbb2346d30c617
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-12-29 14:35:13 +0000

    Un modele complet de pagination, et plein de variantes de style adaptees de http://mis-algoritmos.com/2007/03/16/some-styles-for-your-pagination/
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@43228 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ pagination/lang/pagination_fr.php
@@ -0,0 +1,14 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+'pages' => 'Pages',
+'prev' => '&laquo; Pr&eacute;c&eacute;dent',
+'next' => 'Suivant &raquo;',
+
+);
+
+
+?>
