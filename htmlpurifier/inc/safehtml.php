4abd7b2a6ccd505a0cfe065afd31721c3392643f
fil@rezo.net <fil@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2010-12-29 14:17:44 +0000

    htmlpurifier en version 4.2.0
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@43225 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- htmlpurifier/inc/safehtml.php
+++ htmlpurifier/inc/safehtml.php
@@ -13,25 +13,32 @@
 
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
-function inc_safehtml_dist($t) {
+function inc_safehtml($t) {
 	static $purifier;
 
-	include_spip('library/HTMLPurifier.auto');
+	include_spip('inc/memoization');
+	if (function_exists('cache_get')
+	AND $a = cache_get($cle = 'safehtml:'.md5($t)))
+		return $a;
+
+	include_spip('lib/HTMLPurifier.standalone');
 	if (!isset($purifier))
 		$purifier = new HTMLPurifier();
 		
-	// C'est un pis aller. D'apres la doc, il faudrait indiquer le chemin absolu
-	// d'un dossier avec droit d'écriture pour apache
 	$config = HTMLPurifier_Config::createDefault();
-	$config->set('Core', 'DefinitionCache', null);
-	// En remarque parce que je ne suis pas sur de la syntaxe
-	#$config->set('Cache', 'SerializerPath', dirname(__FILE__).'/'._DIR_TMP);
+	$config->set('Cache.SerializerPath', preg_replace(',/$,', '', realpath(_DIR_TMP)));
 
 	// HTML Purifier prefere l'utf-8
 	if ($GLOBALS['meta']['charset'] == 'utf-8')
-		return $purifier->purify($t);
+		$t = $purifier->purify($t);
 	else
-		return unicode_to_charset($purifier->purify(charset2unicode($t)));
+		$t = unicode_to_charset($purifier->purify(charset2unicode($t)));
+
+	if (function_exists('cache_get'))
+		cache_set($cle, $t);
+
+	return $t;
+
 }
 
 ?>
