62631c4caaf051ee88cdb3f4817847c0cb017bee
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-01-07 10:39:21 +0000

    Interface de job_queue : traduction, et affichage du temps calcule avant prochain job, ce qui permet de detecter un bug de ce cote. Initialiser $_SERVER['REQUEST_TIME'] si necessaire pour les utilisateurs n'ayant ni un PHP ni un SPIP a jour, c'est mieux que planter toute la queue
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@43370 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ job_queue/lang/queue_fr.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/_stable_/acces_restreint/lang/
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	'executer_maintenant' => 'Executer maintenant',
+	'purger_queue' => 'Purger la liste de travaux',
+	'nb_jobs_in_queue' => '@nb@ travaux en attente',
+	'one_job_in_queue' => '1 travail en attente',
+	'next_job_in_nb_sec' => 'Prochain travail dans @nb@ s',
+
+);
+
+?>
