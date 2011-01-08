86b29d4e07792411b24477b931e0b5ff33c8910b
brunobergot@gmail.com <brunobergot@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-01-08 19:19:13 +0000

    un parametre pour la balise noizetier_afficher_noisette qui permet de désactiver l'ajax des inclures réalisés (passer noajax=oui à l'inclure noizetier-generer-bloc)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@43427 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- noiZetier/balise/noizetier_afficher_noisette.php
+++ noiZetier/balise/noizetier_afficher_noisette.php
@@ -10,6 +10,10 @@ function balise_NOIZETIER_AFFICHER_NOISETTE_dist($p) {
 	$id_noisette = champ_sql('id_noisette', $p);
 	$noisette = champ_sql('noisette', $p);
 	$parametres = champ_sql('parametres', $p);
+	$_ajax = 'true';
+	
+	if (($v = interprete_argument_balise(1,$p))!==NULL)
+		$_ajax = 'false';
 
 	// si pas de contexte attribuer, on passe tout le contexte que l'on recoit
 	// sinon, on regarde si 'aucun' ou 'env' est indique :
@@ -20,7 +24,7 @@ function balise_NOIZETIER_AFFICHER_NOISETTE_dist($p) {
 	$p->code =  "(!$id_noisette) ? _T('zbug_champ_hors_motif', array('champ'=>'ID_NOISETTE', 'motif'=>'NOISETTES')) : recuperer_fond(
 		'noisettes/'.$noisette,
 		array_merge(unserialize($parametres), noizetier_choisir_contexte($noisette, $environnement)),
-		array('ajax'=>true)
+		array('ajax'=>$_ajax)
 	)";
 		
 	return $p;
