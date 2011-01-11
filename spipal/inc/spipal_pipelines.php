dd53e84f59ddc6dc40903ff83380a1399773dd2d
esj@rezo.net <esj@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-01-11 17:43:55 +0000

    [source:_plugins_/spipal spipal]. Petite erreur dans la restriction aux rubriques.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@43513 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spipal/inc/spipal_pipelines.php
+++ spipal/inc/spipal_pipelines.php
@@ -23,7 +23,7 @@ function spipal_ajouter_boutons($flux){
 function spipal_affiche_milieu($flux) {
 	if ($flux['args']['exec'] !== 'articles') return $flux;
 	$id_article = $flux['args']['id_article'];
-	$id = sql_fetsel("id_rubrique", "spip_articles", "id_article=$id_article");
+	$id = sql_getfetsel("id_rubrique", "spip_articles", "id_article=$id_article");
 	if (empty($GLOBALS['spipal_metas']['rubriques'])
 	OR preg_match("/\b$id\b/", $GLOBALS['spipal_metas']['rubriques'])) {
 		$f = charger_fonction('spipal_article', 'inc');
