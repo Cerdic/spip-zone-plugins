c3cb59e5cf5bdf4a44b9ad78f6b4e4e74928d6a2
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-01-13 18:55:28 +0000

    le pipeline recuperer_fond est appele a chaque hit : deplacer son traitement dans le fichier options deja inclus a chaque hit + ne pas l'appeler en spip > 2 puisqu'il ne sert plus dans ce cas
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@43553 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- authentification/openid/openid_options.php
+++ authentification/openid/openid_options.php
@@ -1,25 +1,31 @@
 <?php
 /**
  * Plugin OpenID
- * Licence GPL (c) 2007-2010 Edouard Lafargue, Mathieu Marcillaud, Cedric Morin, Fil
+ * Licence GPL (c) 2007-2009 Edouard Lafargue, Mathieu Marcillaud, Cedric Morin, Fil
  *
  */
 
 $GLOBALS['liste_des_authentifications']['openid'] = 'openid';
 
 /**
- * Pipeline permettant de modifier le tableau des informations passée à l'action
- * finale d'authentification après récupération des informations du provider
- * 
- * cf : inc/openid.php
+ * Afficher l'openid sur le formulaire de login
+ *	->Utilise uniquement pour spip 2.0.x
+ * et sur le form inscription
+ *
+ * @param <type> $flux
+ * @return <type>
  */
-$GLOBALS['spip_pipeline']['openid_recuperer_identite'] = '';
+function openid_recuperer_fond($flux) {
+	if ($flux['args']['fond']=='formulaires/login' AND version_compare($GLOBALS['spip_version_branche'],"2.1.0 dev","<")){
+		include_spip('inc/openid');
+		$flux['data']['texte'] = openid_login_form($flux['data']['texte'],$flux['data']['contexte']);
+	}
+	if ($flux['args']['fond']=='formulaires/inscription'){
 
-/**
- * Pipeline permettant de modifier l'url de redirection de l'action
- * finale d'identification pour y ajouter en paramètre les champs demandés
- * 
- * cf : action/inscrire_openid.php
- */
-$GLOBALS['spip_pipeline']['openid_inscrire_redirect'] = '';
+		$insc = recuperer_fond('formulaires/inc-inscription-openid',$flux['data']['contexte']);
+		$flux['data']['texte'] = str_replace('<ul',$insc . '<ul',$flux['data']['texte']);
+
+	}
+	return $flux;
+}
 ?>
--- authentification/openid/openid_pipelines.php
+++ authentification/openid/openid_pipelines.php
@@ -140,26 +140,5 @@ function openid_afficher_contenu_objet($flux){
 	return $flux;
 }
 
-/**
- * Afficher l'openid sur le formulaire de login
- *	->Utilise uniquement pour spip 2.0.x
- * et sur le form inscription
- *
- * @param <type> $flux
- * @return <type>
- */
-function openid_recuperer_fond($flux) {
-	if ($flux['args']['fond']=='formulaires/login' AND version_compare($GLOBALS['spip_version_branche'],"2.1.0 dev","<")){
-		include_spip('inc/openid');
-		$flux['data']['texte'] = openid_login_form($flux['data']['texte'],$flux['data']['contexte']);
-	}
-	if ($flux['args']['fond']=='formulaires/inscription'){
-
-		$insc = recuperer_fond('formulaires/inc-inscription-openid',$flux['data']['contexte']);
-		$flux['data']['texte'] = str_replace('<ul',$insc . '<ul',$flux['data']['texte']);
-
-	}
-	return $flux;
-}
 
 ?>
