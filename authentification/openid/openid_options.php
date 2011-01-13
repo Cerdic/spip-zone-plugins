8189cf4dcba52775a51321dc882014f9a61ba775
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-01-13 19:06:52 +0000

    oups r43553 a par megarde ecrase une partie de r42459 que l'on retablit donc
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@43554 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- authentification/openid/openid_options.php
+++ authentification/openid/openid_options.php
@@ -1,11 +1,28 @@
 <?php
 /**
  * Plugin OpenID
- * Licence GPL (c) 2007-2009 Edouard Lafargue, Mathieu Marcillaud, Cedric Morin, Fil
+ * Licence GPL (c) 2007-2010 Edouard Lafargue, Mathieu Marcillaud, Cedric Morin, Fil
  *
  */
 
 $GLOBALS['liste_des_authentifications']['openid'] = 'openid';
+
+/**
+ * Pipeline permettant de modifier le tableau des informations passée à l'action
+ * finale d'authentification après récupération des informations du provider
+ *
+ * cf : inc/openid.php
+ */
+$GLOBALS['spip_pipeline']['openid_recuperer_identite'] = '';
+
+/**
+ * Pipeline permettant de modifier l'url de redirection de l'action
+ * finale d'identification pour y ajouter en paramètre les champs demandés
+ *
+ * cf : action/inscrire_openid.php
+ */
+$GLOBALS['spip_pipeline']['openid_inscrire_redirect'] = '';
+
 
 /**
  * Afficher l'openid sur le formulaire de login
@@ -28,4 +45,5 @@ function openid_recuperer_fond($flux) {
 	}
 	return $flux;
 }
+
 ?>
