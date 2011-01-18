60291417909a855edf6c3525228bf3bc098feeae
toutati@free.fr <toutati@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-01-18 11:51:46 +0000

    Corrections scories, je pige que couic au systeme d'autorisation ...mais ça à l'air de marcher
    (+ Waou, pour les utilisateurs mac scplugin marche sur snow leopard si on fait un premier commit en ligne de commande)
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@43652 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- ma-lettre/malettre_autoriser.php
+++ ma-lettre/malettre_autoriser.php
@@ -7,12 +7,6 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 function malettre_autoriser(){}
 
 // declarations d'autorisations
-// voir lettre ? = tous les admins
-/*function autoriser_malettre_voir_dist($faire, $type, $id, $qui, $opt) {
-    return ($qui['statut'] == '0minirezo');
-}*/
-
-// declarations d'autorisations
 	function autoriser_malettre_bouton_dist($faire, $type, $id, $qui, $opt) {
 	        return autoriser('voir', 'malettre', $id, $qui, $opt);
 	}
