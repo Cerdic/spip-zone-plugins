a73dda30c09f7a3b37b83098675686172734b0b1
marcimat@rezo.net <marcimat@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-01-18 13:06:31 +0000

    Le catalogue ne s'affiche que sur les rubriques demandées (necessite bonux, saisies, cfg)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@43653 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- catalogue/catalogue_pipelines.php
+++ catalogue/catalogue_pipelines.php
@@ -12,18 +12,16 @@ function catalogue_affiche_milieu($flux) {
 	$exec =  $flux['args']['exec'];
 	
 	// si on est sur la page ?exec=articles
-	if ($exec=='articles'){
+	if ($exec=='articles' AND $id_article = $flux['args']['id_article']) {
 	
-		// on récupère l'id_article
 		$id_article = $flux['args']['id_article'];
-		$afficher = true;
+		if (!$id_rubrique = $flux['args']['id_rubrique']) {
+			$id_rubrique = sql_getfetsel('id_rubrique', 'spip_articles', 'id_article='. $id_article);
+		}
 
-		if ($afficher) {
-			$contexte = array();
-			foreach($_GET as $key=>$val)
-				$contexte[$key] = $val;
-				
-			$catalogue = recuperer_fond('prive/contenu/catalogue_article',$contexte);
+		if (afficher_catalogue_dans_rubrique($id_rubrique)) {
+			// a corriger $_GET... trop permissif
+			$catalogue = recuperer_fond('prive/contenu/catalogue_article', $_GET); // hum hum...
 			$flux['data'] .= $catalogue;
 		}
 	}
@@ -31,4 +29,42 @@ function catalogue_affiche_milieu($flux) {
 	return $flux;
 }
 
+
+/**
+ * Détection des rubriques ayant un catalogue 
+ *
+ * @param int $id_rubrique : rubrique demandee (cette rubrique a t-elle un catalogue ?)
+ * @return bool
+**/
+function afficher_catalogue_dans_rubrique($id_rubrique) {
+	static $rubs = array();
+	static $rubs_catalogue = false;
+
+	// deja trouve
+	if (isset($rubs[$id_rubrique])) {
+		return $rubs[$id_rubrique];
+	}
+
+	// init
+	if ($rubs_catalogue === false) {
+		$r = lire_config('catalogue/branches');
+		include_spip('spip_bonux_fonctions');
+		$rubs_catalogue = picker_selected($r, 'rubrique'); // fn de spip bonux
+	}
+
+	// trouve
+	if (in_array($id_rubrique, $rubs_catalogue)) {
+		$rubs[$id_rubrique] = true;
+		return true;
+	}
+
+	// rechercher dans la parente
+	if ($id_parent = sql_getfetsel('id_parent', 'spip_rubriques', 'id_rubrique='.$id_rubrique)) {
+		 $rubs[$id_rubrique] = afficher_catalogue_dans_rubrique($id_parent);
+		 return $rubs[$id_rubrique];
+	}
+
+	// perdu :)
+	return false;
+}
 ?>
--- catalogue/lang/catalogue_fr.php
+++ catalogue/lang/catalogue_fr.php
@@ -5,33 +5,24 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
 	'activites' => 'Activit&eacute;s',
-	'activites_explications' => 'Souhaitez vous participer aux activit&eacute;s et animations propos&eacute;es dans la semaine ?',
-	'activites_type' => 'Si oui, quel type d\'activit&eacute;s ?',
-	'adresse_suite' => '(adresse suite)',
-	'ajouter_panier' => 'S\'inscrire avec cette option',
 	'ajouter_variante' => 'AJOUTER UNE VARIANTE :',
 	'ajouter_variante_question' => '&Ecirc;tes-vous sur de vouloir ajouter une variante &agrave; cet article ?',
-	'aller_fiche' => 'S\'inscrire en ligne pour cette formule',
 	
 	// B
 	'bouton_ajouter_variante' => 'Ajouter une variante',
 	
 	// C
 	'catalogue' => 'Plugin Catalogue',
-	'code_ville' => 'Code postal + Ville',
-	'contre_indication_explications' => 'Avez-vous une contre-indication quelconque (r&eacute;gime alimentaire, allergie...)',
-	'contre_indication_nature' => 'Si oui, nature de la contre-indication',
+	'configurer_le_catalogue' => "Configurer le catalogue",
 	'creer_variante' => 'Cr&eacute;er une nouvelle variante de prix pour cet article',
 	
 	// E
 	'edition_variante' => '&Eacute;dition variante',
-	'etape_suivante' => '&Eacute;tape suivante',
-	
+
 	// G
 	'go' => 'Go !',
 	
 	// I
-	'info_prix' => 'Prix d&eacute;taill&eacute;s de cette formule',
 	'info_variante_edit' => 'Cette page est en cours de d&eacute;veloppement, veuillez nous en excuser.<br /><br />Cependant vous pouvez tout &agrave; fait &eacute;diter la variante qui s\'affiche sur cette page, et ceci gr&acirc;ce aux "crayons".<br /><br />Double-cliquez sur le champ &agrave; &eacute;diter, ou cliquez sur la petite image qui apparait au survol d\'un champ modifiable.',
 	
 	// L
@@ -49,13 +40,9 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'ok' => 'OK !',
 	'options_dispo' => 'Options disponibles',
 	
-	// P
-	'partage' => 'Chambre',
-	'partage_explications' => 'Souhaitez-vous partager votre chambre double avec une personne en particulier ? (si vous n\'avez pas choisi l\'option "single") ',
-	'partage_nom' => 'Si oui, le nom de cette personne',
-	
 	// R
 	'retour_article' => 'Retour &agrave; l\'article ',
+	'rubriques_catalogue' => "Rubriques d'application du catalogue",
 	
 	// S
 	'supprimer' => 'Supprimer',
@@ -63,13 +50,6 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// T
 	'titre_bloc_catalogue' => 'CATALOGUE : prix et options li&eacute;s &agrave; l\'article',
 	'titre_page_gestion' => 'Page de gestion du catalogue',
-		
-	// V
-	'votre_nom' => 'Votre nom',
-	'votre_prenom' => 'Votre pr&eacute;nom',
-	'votre_adresse' => 'Votre adresse',
-	'votre_niveau' => 'Votre niveau salsa',
-	'votre_style' => 'Votre style',
 	
 );
 ?>
