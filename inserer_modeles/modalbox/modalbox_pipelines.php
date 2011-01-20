5dceaa65d29bd6657ff75b2501b3a2dcfdea68d3
joseph@larmarange.net <joseph@larmarange.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-01-20 18:34:09 +0000

    Tenir compte compte du changement de préfixe de la médiathèque
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@43727 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- inserer_modeles/modalbox/modalbox_pipelines.php
+++ inserer_modeles/modalbox/modalbox_pipelines.php
@@ -1,11 +1,11 @@
 <?php
 
 function inserer_modeles_modalbox_insert_head($flux){
-	if (!defined('_DIR_PLUGIN_MEDIABOX') && !defined('_DIR_PLUGIN_GESTDOC')) {
+	if (!defined('_DIR_PLUGIN_MEDIABOX') && !defined('_DIR_PLUGIN_GESTDOC') && !defined('_DIR_PLUGIN_MEDIAS')) {
 		// Insertion des librairies js
 		$flux .='<script src="'.find_in_path('modalbox/jquery.simplemodal-1.3.3.js').'" type="text/javascript"></script>';
 		$flux .='<script src="'.find_in_path('modalbox/modalbox.js').'" type="text/javascript"></script>';
-		$css = generer_url_public('modalbox/style_prive_plugin_modalbox','',true);
+		$css = generer_url_public('modalbox/style_prive_plugin_modalbox');
 		$flux .= "\n<link rel='stylesheet' href='$css' type='text/css' />\n";
 	}
 
