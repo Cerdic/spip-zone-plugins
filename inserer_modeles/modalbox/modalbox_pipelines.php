bb32db8b1515f8f58000cd1f1301a20ebd9530db
joseph@larmarange.net <joseph@larmarange.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-01-20 16:18:41 +0000

    Eviter que le / ne soit transformé en entité html
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@43717 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- inserer_modeles/modalbox/modalbox_pipelines.php
+++ inserer_modeles/modalbox/modalbox_pipelines.php
@@ -5,7 +5,7 @@ function inserer_modeles_modalbox_insert_head($flux){
 		// Insertion des librairies js
 		$flux .='<script src="'.find_in_path('modalbox/jquery.simplemodal-1.3.3.js').'" type="text/javascript"></script>';
 		$flux .='<script src="'.find_in_path('modalbox/modalbox.js').'" type="text/javascript"></script>';
-		$css = generer_url_public('modalbox/style_prive_plugin_modalbox');
+		$css = generer_url_public('modalbox/style_prive_plugin_modalbox','',true);
 		$flux .= "\n<link rel='stylesheet' href='$css' type='text/css' />\n";
 	}
 
