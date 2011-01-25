66e8d3307f2ef5a4fc5bdbc2a7d4286be9d197a5
patfr@ifrance.com <patfr@ifrance.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-01-25 14:45:46 +0000

    Ecran de sécu : erreur de syntaxe concernant le bug de SPIP sur les 'exec'
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@43865 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- couteau_suisse/config_outils.php
+++ couteau_suisse/config_outils.php
@@ -350,7 +350,7 @@ add_variables( array(
 	// code d'appel en realpath() pour config/mes_options.php (SPIP < 2.1)
 	'code:%s' => "// bug SPIP 1.92, 2.0 et 2.1
 if(isset(\$_REQUEST['exec']) && strncmp('admin_couteau_suisse',\$_REQUEST['exec'],20)==0) \$_REQUEST['exec']='admin_couteau_suisse';
-".(!defined('_SPIP20100'))?"if(!defined('_ECRAN_SECURITE') && @file_exists(\$f=\"".str_replace('\\','/',realpath(dirname(__FILE__)))."/lib/ecran_securite/distant_ecran_securite.php\")) include \$f;":'',
+".((!defined('_SPIP20100'))?"if(!defined('_ECRAN_SECURITE') && @file_exists(\$f=\"".str_replace('\\','/',realpath(dirname(__FILE__)))."/lib/ecran_securite/distant_ecran_securite.php\")) include \$f;":''),
 ), array(
 	'nom' => 'ecran_load',
 	'format' => _format_NOMBRE,
