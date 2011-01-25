c08e4dd5b3156b33f83a52e33db155098fd9949e
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-01-25 17:44:45 +0000

    Ne pas boucler infiniment si on n'a pas la bonne frame
    On teste la frame 50 par défaut plutot que 100
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@43873 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spipmotion/inc/spipmotion_recuperer_logo.php
+++ spipmotion/inc/spipmotion_recuperer_logo.php
@@ -12,9 +12,10 @@
 
 /**
  * Fonction de récupération de vignette depuis un document video
- * @param int $id_document
+ * @param int $id_document L'id numérique du document
+ * @param int $frame la frame à capturer
  */
-function inc_spipmotion_recuperer_logo($id_document,$frame=100){
+function inc_spipmotion_recuperer_logo($id_document,$frame=50){
 	spip_log("SPIPMOTION : recuperation d un logo du document $id_document","spipmotion");
 	if(!intval($id_document) OR !class_exists('ffmpeg_movie')){
 		return;
@@ -22,17 +23,17 @@ function inc_spipmotion_recuperer_logo($id_document,$frame=100){
 
 	include_spip('inc/documents');
 	include_spip('inc/filtres_images_mini');
-
+	$retour = 0;
 	$document = sql_fetsel("docs.id_document,docs.fichier,docs.framecount", "spip_documents AS docs INNER JOIN spip_documents_liens AS L ON L.id_document=docs.id_document","L.id_document=".sql_quote($id_document));
 	$chemin_court = $document['fichier'];
 	$chemin = get_spip_doc($chemin_court);
 	$movie = new ffmpeg_movie($chemin,0);
 	if($movie->hasVideo()){
 		$vignette = false;
-		
-		while(!$vignette && ($frame<$document['framecount'])){
+		while(!$vignette && ($frame < $document['framecount'])){
 			$frame1 = $movie->getFrame($frame);
 			if($frame1){
+				spip_log($frame1,'spipmotion');
 				$string_temp = "$id-$type-$id_document";
 				$query = md5($string_temp);
 				$dossier_temp = _DIR_VAR;
@@ -43,12 +44,13 @@ function inc_spipmotion_recuperer_logo($id_document,$frame=100){
 				$img_finale = $fichier_temp;
 				$mode = 'vignette';
 				
-				if(_DIR_PLUGIN_FONCTIONS_IMAGES){
+				if(defined('_DIR_PLUGIN_FONCTIONS_IMAGES')){
 					include_spip('fonctions_images_fonctions');
 					if(!filtrer('image_monochrome',$fichier_temp)){
 						imagedestroy($img_temp);
 						unlink($img_finale);
 						$frame = $frame+50;
+						$retour++;
 					}else{
 						$ajouter_documents = charger_fonction('ajouter_documents', 'inc');
 						$x = $ajouter_documents($img_finale, $img_finale,
@@ -65,6 +67,9 @@ function inc_spipmotion_recuperer_logo($id_document,$frame=100){
 					unlink($img_finale);
 					$vignette = true;
 				}
+			}
+			else{
+				return false;
 			}
 		}
 	}
