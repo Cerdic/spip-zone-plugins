84523566e96cbf47c1f9b9c2c954612998b71a70
joseph@larmarange.net <joseph@larmarange.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-01-29 14:52:09 +0000

    Extension pour le porte-plume ajoutant un bouton pour changer la langue dans le texte
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@43976 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ porte_plume_extras/changement_langue/lang/pp_chgt_lg_fr.php
@@ -0,0 +1,11 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/contact/lang/
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	'outil_changement_langue' => 'Changement de langue dans le texte.',
+	'code_langue' => 'Indiquez le code de la nouvelle langue (par exemple&nbsp;: fr pour le français, en pour l\'anglais).',
+);
+
+?>
---
+++ porte_plume_extras/changement_langue/pp_chgt_lg_pipelines.php
@@ -0,0 +1,35 @@
+<?php
+
+// Insertion dans le porte-plume
+
+function pp_chgt_lg_porte_plume_barre_pre_charger($barres) {
+	$barre = &$barres['edition'];
+	$barre->ajouterApres('grpCaracteres', array(
+				"id" => "sepChgtLang",
+				"separator" => "---------------",
+				"display"   => true,
+	));
+	
+	
+	
+	$barre->ajouterApres('sepChgtLang', array(
+		"id"          => 'changement_langue',
+		"name"        => _T('pp_chgt_lg:outil_changement_langue'),
+		"className"   => 'outil_changement_langue',
+		"openWith"    => "<multi>[[!["._T('pp_chgt_lg:code_langue')."]!]]",
+		"closeWith"   => "</multi>",
+		"display"     => true
+	 ));
+	
+	return $barres;
+}
+
+// Icônes pour le porte-plume
+
+function pp_chgt_lg_porte_plume_lien_classe_vers_icone($flux) {
+	$icones = array();
+	$icones['outil_changement_langue'] = 'changement_langue.png';
+	return array_merge($flux, $icones);
+}
+
+?>
