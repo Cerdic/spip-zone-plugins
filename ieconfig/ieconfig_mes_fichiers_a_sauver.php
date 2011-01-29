18d5f5f348873ffe187d8dbd08a9ea390561d195
joseph@larmarange.net <joseph@larmarange.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-01-29 03:04:57 +0000

    une erreur de préfixe
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@43964 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- ieconfig/ieconfig_mes_fichiers_a_sauver.php
+++ ieconfig/ieconfig_mes_fichiers_a_sauver.php
@@ -1,6 +1,6 @@
 <?php
 
-function saveauto_mes_fichiers_a_sauver($flux){
+function ieconfig_mes_fichiers_a_sauver($flux){
 	$tmp_ieconfig = defined('_DIR_TMP') ? _DIR_TMP.'ieconfig/': _DIR_RACINE.'tmp/ieconfig/';
 	if (@is_dir($tmp_ieconfig))
 		$flux[] = $tmp_ieconfig;
