325948f740ac2ea875d28ad548eaa3794d5b0e42
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-01-31 17:10:44 +0000

    Rester compatible avec les vieux sites qui n'utilisent peut être pas que jquery
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@44024 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- nyroceros/js/nyromodal.js
+++ nyroceros/js/nyromodal.js
@@ -4,31 +4,31 @@ var nyro_init = function() {
 	// On cache les embed et object qui n'ont pas de wmode=transparent pour eviter qu'ils
 	// passent par dessus la modale
 	if(!navigator.platform.match('Mac')){
-		$.fn.nyroModal.settings.processHandler = function(){
+		jQuery.fn.nyroModal.settings.processHandler = function(){
 			jQuery('embed[wmode!=transparent]:visible').addClass('nyro_cache').css('visibility','hidden')
 				.parents('object').addClass('nyro_cache').css('visibility','hidden');
 		}
-		$.fn.nyroModal.settings.endRemove = function(){
+		jQuery.fn.nyroModal.settings.endRemove = function(){
 			jQuery('.nyro_cache').removeClass('.nyro_cache').css('visibility','visible');
 		}
 	}
 	if (nyro_traiter_toutes_images) {
 		// selectionner tous les liens vers des images
-		$("a[type=\'image/jpeg\'],a[type=\'image/png\'],a[type=\'image/gif\']",this)
+		jQuery("a[type=\'image/jpeg\'],a[type=\'image/png\'],a[type=\'image/gif\']",this)
 		.addClass("nyroceros") // noter qu\'on l\'a vue
 		.attr("onclick","") // se debarrasser du onclick de SPIP
 		.nyroModal({bgColor: nyro_bgcolor}); // activer le nyro
 	}
 	// passer le portfolio en mode galerie de nyro
-	$(nyro_selecteur_galerie, this)
+	jQuery(nyro_selecteur_galerie, this)
 	.attr("rel","galerie-portfolio");
 
 	// charger nyro sur autre chose
-	$(nyro_selecteur_commun).nyroModal({bgColor: nyro_bgcolor});
+	jQuery(nyro_selecteur_commun).nyroModal({bgColor: nyro_bgcolor});
 
   // preload images
 	if (nyro_preload) {
-	  $.fn.preload = function() {
+		jQuery.fn.preload = function() {
 	    var url;
 	    return this.each(function() {
 	      if ((url = $(this).attr("href")) && url.match(/\.(jpg|jpeg|png|gif)$/ )) {
@@ -37,9 +37,9 @@ var nyro_init = function() {
 	      }
 	    });
 	  }
-	  $.fn.nyroModal.settings.endShowContent = function(elts,settings) {
-	    $(".nyroModalNext").preload();
+	jQuery.fn.nyroModal.settings.endShowContent = function(elts,settings) {
+		jQuery(".nyroModalNext").preload();
 	  }
-	  $(".nyroceros[rel]:eq(0)").preload();
+	jQuery(".nyroceros[rel]:eq(0)").preload();
 	}
 };
