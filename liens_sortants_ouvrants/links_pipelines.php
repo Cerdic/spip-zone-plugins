89ffcfb07d4847fe2045c705ac0906654416536f
romy@rezo.net <romy@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-01-31 21:55:49 +0000

    renomages
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@44041 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- liens_sortants_ouvrants/links_pipelines.php
+++ liens_sortants_ouvrants/links_pipelines.php
@@ -1,9 +1,9 @@
 <?php
 
-function liens_sortants_ouvrants_insert_head($flux) {
+function links_insert_head($flux) {
 	$flux .= '<script type="text/javascript">var liens_sortants_site = \''.$GLOBALS['meta']['adresse_site'].'\';</script>';
-	$flux .= '<script  src="'.find_in_path('liens_sortants_ouvrants.js').'" type="text/javascript"></script>';
-	$flux .= '<link rel="stylesheet" href="'.find_in_path('liens_sortants_ouvrants.css').'" type="text/css" media="all" />';
+	$flux .= '<script  src="'.find_in_path('links.js').'" type="text/javascript"></script>';
+	$flux .= '<link rel="stylesheet" href="'.find_in_path('links.css').'" type="text/css" media="all" />';
 	return $flux;
 }
 
