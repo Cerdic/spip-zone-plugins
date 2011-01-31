8a60750be65b9eb6032630f3ef1ad94458d10637
romy@rezo.net <romy@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-01-31 21:47:11 +0000

    Renommage
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@44037 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ liens_sortants_ouvrants/links_fr.php
@@ -0,0 +1,12 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+'lien_externe' => 'Lien externe',
+'lien_ouvrant' => 'Lien ouvrant une nouvelle fen&ecirc;tre',
+
+);
+
+?>
