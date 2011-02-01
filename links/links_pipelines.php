eb7c32f4d7f91037bc359b3742634f53042fd1de
romy@rezo.net <romy@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-02-01 23:50:15 +0000

    C'est mieux avec la feuille de style :-)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@44071 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- links/links_pipelines.php
+++ links/links_pipelines.php
@@ -3,7 +3,7 @@
 function links_insert_head($flux) {
 	$flux .= '<script type="text/javascript">var liens_sortants_site = \''.$GLOBALS['meta']['adresse_site'].'\';</script>';
 	$flux .= '<script  src="'.find_in_path('links.js').'" type="text/javascript"></script>';
-	$flux .= '<link rel="stylesheet" href="'.find_in_path('links.css').'" type="text/css" media="all" />';
+	$flux .= '<link rel="stylesheet" href="'.find_in_path('css/links.css').'" type="text/css" media="all" />';
 	return $flux;
 }
 
