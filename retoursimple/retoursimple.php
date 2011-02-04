af2cd8e379bd87e2134adbc7674afddf203fe6a8
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-02-04 22:43:56 +0000

    post_autobr plutôt qu'un truc pourris
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@44155 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- retoursimple/retoursimple.php
+++ retoursimple/retoursimple.php
@@ -1,10 +1,5 @@
 <?php
 
 function retoursimple_pre_propre($texte){
-	$texte = trim($texte); 
-	$texte = preg_replace("/\n([\w\d])/", "\n<br />\\1", $texte); 
-	$texte = preg_replace("@^ ?<br />@", "", $texte); 
-	$texte = str_replace("<br />_ ","<br />",$texte);
-	$texte = "\n".$texte;
-	return $texte;	
+	return post_autobr($texte);	
 }
