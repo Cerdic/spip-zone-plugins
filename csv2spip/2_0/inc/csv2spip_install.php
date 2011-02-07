7d2e4a7da99cc3991f68ccc08e775406286293cf
cy_altern@yahoo.fr <cy_altern@yahoo.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-02-07 12:02:45 +0000

    compatibilite 2.1 du processus d'installation du plugin
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@44264 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- csv2spip/2_0/inc/csv2spip_install.php
+++ csv2spip/2_0/inc/csv2spip_install.php
@@ -1,40 +1,42 @@
 <?php
 
-  if (!defined("_ECRIRE_INC_VERSION")) return;
+if (!defined("_ECRIRE_INC_VERSION")) return;
 
-	function csv2spip_installation($num_version){
-  // creer la table spip_tmp_csv2spip à l'install du plugin plutôt qu'à chaque utilisation du plugin
-		include_spip('inc/csv2spip_tables');
-    include_spip('base/create');
-    include_spip('base/abstract_sql');
-    creer_base();
-     
-  // stocker le num de version dans spip_meta
-    ecrire_meta('csv2spip_version',$num_version);
-    
-    if (count($Terreurs) != 0) echo implode('<br>',$Terreurs);
-    
-	}
+function csv2spip_installation($num_version){
+	// creer la table spip_tmp_csv2spip à l'install du plugin plutôt qu'à chaque utilisation du plugin
+	include_spip('inc/csv2spip_tables');
+	include_spip('base/create');
+	include_spip('base/abstract_sql');
+	creer_base();
+	 
+	// stocker le num de version dans spip_meta
+	ecrire_meta('csv2spip_version',$num_version);
+
+	if (count($Terreurs) != 0)
+		echo implode('<br>',$Terreurs);
+
+}
 	
-	function csv2spip_desinstallation() {
-		effacer_meta('csv2spip_version');
-		ecrire_metas();
-	}
+function csv2spip_desinstallation() {
+	effacer_meta('csv2spip_version');
+	ecrire_metas();
+}
 
-	function csv2spip_install($action){
-    // récupérer le numéro de version
-      $Tplugins_actifs = liste_plugin_actifs();
-      $version_script = $Tplugins_actifs['CSV2SPIP']['version'];
+function csv2spip_upgrade($action){
+// récupérer le numéro de version
+  $Tplugins_actifs = liste_plugin_actifs();
+  $version_script = $Tplugins_actifs['CSV2SPIP']['version'];
 
-    // install/désinstall ? 
-		  switch ($action){
-			case 'install':
-				if (lire_meta('csv2spip_version') != $version_script) csv2spip_installation($version_script);
-				break;
-			case 'uninstall':
-				csv2spip_desinstallation();
-				break;
-		}
+// install/désinstall ? 
+	  switch ($action){
+		case 'install':
+			if (lire_meta('csv2spip_version') != $version_script)
+				csv2spip_installation($version_script);
+			break;
+		case 'uninstall':
+			csv2spip_desinstallation();
+			break;
 	}
+}
 
 ?>
