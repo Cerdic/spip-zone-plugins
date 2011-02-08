aaee5ed62163f96ac70b343db40ddbd743a18156
bmarne@gmail.com <bmarne@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-02-08 00:01:04 +0000

    Nouveau plugin: surcharge du porte plume avec de nouveaux icônes à la place de gras et italique+modif du comportement par défaut de SPIP sur les arccourcis de simples et doubles accolades en em/strong.
    L'objectif est d'avoir un porte plume avec des raccourcis sémantiques plutôt que typographiques.
    
    Peu de temps dispo donc, icônes moches et pas encore de doc...
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@44300 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ icones_semantiques/icones_semantiques_options.php
@@ -0,0 +1,11 @@
+<?php
+// Icones sémantiques pour le Porte Plume
+// par Bertrand Marne
+//
+//remplacer les raccourcis gras/italiques par emphase (em) et emphase forte (strong)
+//
+$debut_italique = '<em>' ;
+$fin_italique = '</em>'  ;
+$debut_gras = '<strong>' ;
+$fin_gras = '</strong>' ;
+?>
