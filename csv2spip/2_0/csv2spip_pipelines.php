12afff9e99f1e33901eda27b6f12d4b92285cad5
cy_altern@yahoo.fr <cy_altern@yahoo.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-02-09 00:18:06 +0000

    tout le monde n'utilisant pas le plugin Bandeau, gérer le bandeau de SPIP "old style" sans erreur...
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@44350 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- csv2spip/2_0/csv2spip_pipelines.php
+++ csv2spip/2_0/csv2spip_pipelines.php
@@ -1,26 +1,14 @@
 <?php
-/* csv2spip est un plugin pour créer/modifier les visiteurs, rédacteurs et administrateurs restreints d'un SPIP à partir de fichiers CSV
-*	 					VERSION : 3.1 => plugin pour spip 2.*
-*
-* Auteur : cy_altern
-*  
-* Ce programme est un logiciel libre distribue sous licence GNU/GPL.
-*  
-**/
-
 	function csv2spip_ajouter_boutons($boutons_admin) {
-		// si on est admin
-		global $spip_version_code;
-		if ($spip_version_code < 14864) {
-			if ($GLOBALS['connect_statut'] == "0minirezo") {
-			  // on voit le bouton comme  sous-menu de "auteurs"
+		if ($GLOBALS['connect_statut'] == "0minirezo") {
+			include_spip('inc/plugin');
+			$Tplugins = liste_plugin_actifs();
+			if (!array_key_exists('BANDO', $Tplugins)) {
+			  // on voit le bouton comme  sous-menu de "auteurs", si le plugin bando est actif c'est un bouton du menu Maintenance  (cf plugin.xml)
 				$boutons_admin['auteurs']->sousmenu['csv2spip']= new Bouton("../"._DIR_PLUGIN_CSV2SPIP."/img_pack/csv2spip-24.png", _T('csvspip:module_titre') );
 			}
 		}
-
 		return $boutons_admin;
-
 	}
-
 
 ?>
