df56bc04530011201c56e7f9b8b3821a98087029
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-02-09 02:02:11 +0000

    Un peu plus smart au niveau du formulaire de configuration...
    Traduction anglophone
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@44351 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ flattr/lang/flattr_en.php
@@ -0,0 +1,22 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	// C
+	'cfg_explication_uid' => 'You can find this information on <a href="https://flattr.com/dashboard" target="flattr">your Flattr account page</a>.',
+	'cfg_label_category' => 'Default category',
+	'cfg_label_showthings' => 'Show &#8216;things&#8217; on flattr.com',
+	'cfg_label_uid' => 'User ID',
+	'cfg_legend_reglages' => 'Misc settings',
+	'cfg_legend_vous' => 'You',
+	'cfg_value_audio' => 'Audio',
+	'cfg_value_hide' => 'Hide',
+	'cfg_value_images' => 'Images',
+	'cfg_value_rest' => 'Rest',
+	'cfg_value_show' => 'Yes, show',
+	'cfg_value_software' => 'Sofware',
+	'cfg_value_text' => 'Text',
+	'cfg_value_video' => 'Video',
+);
+?>
---
+++ flattr/lang/flattr_fr.php
@@ -0,0 +1,23 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	// C
+	'cfg_explication_uid' => 'Vous trouverez cette information sur <a href="https://flattr.com/dashboard" target="flattr">la page de votre compte Flattr</a>.',
+	'cfg_label_category' => 'Cat&eacute;gorie par d&eacute;faut',
+	'cfg_label_showthings' => 'Afficher les &#8216;things&#8217; sur flattr.com',
+	'cfg_label_uid' => 'Nom d\'utilisateur',
+	'cfg_legend_reglages' => 'R&#233;glages divers',
+	'cfg_legend_vous' => 'Vous',
+	'cfg_value_audio' => 'Audio',
+	'cfg_value_hide' => 'Masquer',
+	'cfg_value_images' => 'Images',
+	'cfg_value_rest' => 'Divers',
+	'cfg_value_show' => 'Oui, afficher',
+	'cfg_value_software' => 'Logiciel',
+	'cfg_value_text' => 'Texte',
+	'cfg_value_video' => 'Vid&eacute;o',
+	
+);
+?>
