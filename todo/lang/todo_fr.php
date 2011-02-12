a3b47db90906a2dc8b2240845ad6f56b47bec15f
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-02-12 01:22:13 +0000

    Un début de plugin utilisant textwheel pour l'instant de manière très très simple.
    Le but du plugin est d'écrire des todo-lists dans les contenus SPIP avec un formalisme rapide.
    
    Pour l'instant ce que j'ai réussi à faire c'est :
    +++ (ça démarre la todo pour ne pas interférer)
    + à faire
    - terminé
    o en cours
    +++
    
    Ce qui est intéressant c'est que ça ne fait PAS un replace avec du code HTML. Mais ça parse la liste, extrait un tableau, et ça l'envoit à un squelette SPIP !
    
    Ce squelette est en AJAX, et permet alors d'afficher la liste comme on veut, mais surtout de pouvoir la trier ! Notamment par statut !
    
    Pour l'instant c'est tout brut et il n'y a aucun style. Il faut essentiellement faire un beau HTML, des styles CSS, et des chaines de langues. Mais sinon ça marche !
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@44466 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ todo/lang/todo_fr.php
@@ -0,0 +1,11 @@
+<?php
+
+// Sécurité
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+'configurer_bando' => 'Choses à faire',
+'configurer_titre' => 'Configurer les listes de choses à faire'
+);
+
+?>
