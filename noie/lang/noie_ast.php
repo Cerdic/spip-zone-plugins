47ef97903ec9f661fd7a542f81b542e37e76b8d0
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-02-12 09:56:42 +0000

    langues (noie)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@44494 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- noie/lang/noie_ast.php
+++ noie/lang/noie_ast.php
@@ -3,13 +3,13 @@
 // extrait automatiquement de http://www.spip.net/trad-lang/
 // ** ne pas modifier le fichier **
 
-if (!defined("_ECRIRE_INC_VERSION")) return;
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// N
-	'noie_info' => 'Pour naviguer de fa&ccedil;on plus satisfaisante sur ce site et le reste du Web, nous vous recommandons d\'<strong>actualiser votre <a href="http://www.microsoft.com/france/windows/internet-explorer/">Internet Explorer</a></strong> ou d\'essayer un autre navigateur populaire comme <a href="http://www.mozilla-europe.org/fr/firefox/">Firefox</a>, <a href="http://www.google.fr/chrome">Chrome</a>, <a href="http://www.opera.com/">Opera</a> ou <a href="http://www.apple.com/fr/safari/">Safari</a>.', # NEW
-	'noie_titre' => 'Attention, votre Internet Explorer n\'est pas &agrave; jour&nbsp;!', # NEW
+	'noie_info' => 'Pa navegar de forma m&aacute;s afayadiza per esti sitiu y pol restu de la Web, encamentamos <strong>anovar el to <a href="http://www.microsoft.com/france/windows/internet-explorer/">Internet Explorer</a></strong> o probar otru navegador popular como <a href="http://www.mozilla.com/en-US/firefox/all.html">Firefox</a>, <a href="http://www.google.com/chrome">Chrome</a>, <a href="http://www.opera.com/">Opera</a> o <a href="http://www.apple.com/safari/">Safari</a>.',
+	'noie_titre' => 'Atenci&oacute;n, &iexcl;el to Internet Explorer nun ta anov&aacute;u!',
 
 	// T
 	'toocool_alt' => 'Gayaspero enforma pa IE',
