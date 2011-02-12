b14edd2c3cde0afe246c16e699ad3024d6686640
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-02-12 14:18:19 +0000

    ereg deprecated dans les versions récentes de php
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@44559 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- Lecteur_multimedia/player_fonctions.php
+++ Lecteur_multimedia/player_fonctions.php
@@ -100,13 +100,13 @@ function Player_post_propre($texte) {
 }
 
 function joli_titre($titre){
-$titre=basename($titre);
-$titre=ereg_replace('.mp3','',$titre);
-$titre=ereg_replace('^ ','',$titre);
-$titre = eregi_replace("_"," ", $titre );
-$titre = eregi_replace("'"," ",$titre );
+	$titre=basename($titre);
+	$titre=preg_replace('/.mp3/','',$titre);
+	$titre=preg_replace('/^ /','',$titre);
+	$titre = preg_replace("/_/i"," ", $titre );
+	$titre = preg_replace("/'/i"," ",$titre );
 
-return $titre ;
+	return $titre ;
 }
 
 
