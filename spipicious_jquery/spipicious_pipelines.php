847e4bbb0a2c169eb3fc4b68c9f814d30a41dfe7
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-02-13 19:19:59 +0000

    On s'insère dans l'optimisation des tables pour enlever les liens mot/objet/id_objet sur les objet/id_objet disparus
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@44622 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spipicious_jquery/spipicious_pipelines.php
+++ spipicious_jquery/spipicious_pipelines.php
@@ -104,4 +104,32 @@ EOS;
 	return $flux;
 }
 
+/**
+ * Insertion dans le pipeline optimiser_base_disparus (SPIP)
+ * 
+ * Supprimer les liens spipicious/objet sur les éléments disparus
+ * @param array $flux le contexte du pipeline
+ */
+function spipicious_optimiser_base_disparus($flux){
+	/**
+	 * On fonctionne comme les documents dans genie/optimiser
+	 */
+	$r = sql_select("DISTINCT objet","spip_spipicious");
+	while ($t = sql_fetch($r)){
+		$type = $t['objet'];
+		$spip_table_objet = table_objet_sql($type);
+		spip_log($spip_table_objet,'test');
+		$id_table_objet = id_table_objet($type);
+		$res = sql_select("L.id_mot AS id,L.id_objet AS id_objet",
+			      "spip_spipicious AS L
+			        LEFT JOIN $spip_table_objet AS O
+			          ON O.$id_table_objet=L.id_objet AND L.objet=".sql_quote($type),
+				"O.$id_table_objet IS NULL");
+		while ($row = sql_fetch($res)){
+			sql_delete("spip_spipicious", array("id_mot=".$row['id'],"id_objet=".$row['id_objet'],"objet=".sql_quote($type)));
+			spip_log("Entree ".$row['id']."/".$row['id_objet']."/$type supprimee dans la table spip_spipicious",'test');
+		}
+	}
+	return $flux;
+}
 ?>
