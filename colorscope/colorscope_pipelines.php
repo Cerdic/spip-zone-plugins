3bdb723b9ae65de2ed0b5b48005d4138f494ac32
cyril@cym.fr <cyril@cym.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-02-14 17:03:33 +0000

    la couleur est appelée en mode décimal, pour ne pas interférer avec le code hexadecimal (qui était interprété 2 fois, sinon); on en profite pour ajouter une shortdesc dans le fichier de descriptions et pour passer le plugin en version 0.4;
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@44675 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- colorscope/colorscope_pipelines.php
+++ colorscope/colorscope_pipelines.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Spipmine pour Spip 2.0
- * Licence GPL (c) 2009 - 2010
+ * Licence GPL (c) 2009 - 2011
  * Auteur Cyril MARION - Ateliers CYM
  * Merci a Arno* pour les filtres couleur :-)
  *
@@ -24,7 +24,7 @@ function bloc_colorscope($couleurs) {
 	}
 	// $fond = '#'.couleur_inverser(couleur_extreme($coul));
 
-	$bloc = '<span style="display:inline-block;text-align:center;width:90px;height:20px;background-color:'.$coul.';color:'.$fond.'">'.$coul.'</span>';
+	$bloc = '<span style="display:inline-block;text-align:center;width:90px;height:20px;background-color:'.format_decimal($coul).';color:'.$fond.'">'.$coul.'</span>';
 	return $bloc;
 }
 
@@ -32,6 +32,16 @@ function luminance($couleur) {
 	$couleurs = couleur_hex_to_dec($couleur);
 	$valeur = 0.2126*$couleurs["red"] + 0.7152*$couleurs["green"] + 0.0722*$couleurs["blue"];
 	return $valeur;
+}
+
+function format_decimal($couleur) {
+	$couleurs = couleur_hex_to_dec($couleur);
+	$rouge = $couleurs['red'];
+	$vert = $couleurs['green'];	
+	$bleu = $couleurs['blue'];
+
+	$couleur_dec = "rgb(".$rouge.",".$vert.",".$bleu.")";
+	return $couleur_dec;
 }
 
 ?>
