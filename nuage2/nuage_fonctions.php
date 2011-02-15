083c315bb67e7a6a06be36ce34c89224a1ffba3f
cam.lafit@azerttyu.net <cam.lafit@azerttyu.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-02-15 13:12:47 +0000

    Bonux rend plus blanc que blanc
     * #FREQUENCE n'est pas utile dans ce cas #COMPTEUR{objet} répond au meme besoin
     * Une variable #GET{expose} permet de calculer les mots clefs concernés par la page consultée
      * pallie à la balise #EXPOSE qui ne peut etre exploitée, car les modeles sont hors contexte de page
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@44707 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- nuage2/nuage_fonctions.php
+++ nuage2/nuage_fonctions.php
@@ -1,26 +1,4 @@
 <?php
-function critere_frequence($idb, &$boucles, $crit){
-	$op='';
-	$boucle = &$boucles[$idb];
-	$params = $crit->param;
-	$type = array_shift($params);
-	$type = $type[0]->texte;
-	if(preg_match(',^(\w+)([<>=])([0-9]+)$,',$type,$r)){
-		$type=$r[1];
-		$op=$r[2];
-		$op_val=$r[3];
-	}
-	$type_id = 'freq.id_'.$type;
-	$type_requete = $boucle->type_requete;
-	$id_table = $boucle->id_table . '.' . $boucle->primary;
-	$boucle->select[]= 'COUNT('.$type_id.') AS frequence';
-	$boucle->from['freq']="spip_'.$type_requete.'_".$type."s";
-	$boucle->where[]= array("'='", "'".$id_table."'", "'freq.".$boucle->primary."'");
-	$boucle->group[]=$id_table;
-	if ($op)
-		$boucle->having[]= array("'".$op."'", "'frequence'",$op_val);
-	
-}
 function critere_popularite($idb, &$boucles, $crit){
 	$op='';
 	$boucle = &$boucles[$idb];
@@ -121,11 +99,6 @@ function nuage_note($score,$scoremax=1,$max=10) {
 	return ceil($max*$score);
 }
 
-function balise_FREQUENCE_dist($p) {
-	$p->code = '$Pile[$SP][\'frequence\']';
-	$p->interdire_scripts = false;
-	return $p;
-}
 function balise_POPULARITE_RELATIVE_dist($p) {
 	$p->code = '$Pile[$SP][\'popularite_relative\']';
 	$p->interdire_scripts = false;
