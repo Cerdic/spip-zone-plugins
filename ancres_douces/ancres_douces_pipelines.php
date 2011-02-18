72dfb40d7f45e23472ef0d2d35557207f4c68d78
jluc@no-log.org <jluc@no-log.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-02-18 17:09:04 +0000

    Pour par exemple limiter l'action de ancres_douces au div #contenu ou aux divs de classe .ancres_douces, insérez dans votre config/mes_options.php les lignes : define ('ANCRES_DOUCES_CONTEXTE','#contenu'); ou define ('ANCRES_DOUCES_CONTEXTE','.ancres_douces');
    Cela peut éviter des interférences avec d'autres sliders ou galeries jquery
    
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@44814 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- ancres_douces/ancres_douces_pipelines.php
+++ ancres_douces/ancres_douces_pipelines.php
@@ -1,5 +1,16 @@
 <?php
+
+// Pour par exemple limiter l'action de ancres_douces au div #contenu ou aux divs de classe .ancres_douces,
+// insérez dans votre config/mes_options.php les lignes :
+// define ('ANCRES_DOUCES_CONTEXTE','#contenu'); 
+// ou 
+// define ('ANCRES_DOUCES_CONTEXTE','.ancres_douces'); 
+
 function ancres_douces_insert_head($flux){
+
+if (!defined('CONTEXTE_ANCRES_DOUCES'))
+	$appel_ancres_douces='jQuery';
+else $appel_ancres_douces='$(\''.CONTEXTE_ANCRES_DOUCES.'\')';
 
 // Inclusion des scripts jquery
 $flux .= '<script src="'.url_absolue(find_in_path("js/jquery.scrollto.js")).'" type="text/javascript"></script>';
@@ -7,7 +18,7 @@ $flux .= '<script src="'.url_absolue(find_in_path("js/jquery.localscroll.js")).'
 
 // Code d'init
 $flux .= '<script type="text/javascript">/* <![CDATA[ */
-function ancre_douce_init() {if(typeof jQuery.localScroll=="function")jQuery.localScroll({hash:true});}
+function ancre_douce_init() {if(typeof jQuery.localScroll=="function")'.$appel_ancres_douces.'.localScroll({hash:true});}
 if(window.jQuery)jQuery(document).ready(ancre_douce_init);
 onAjaxLoad(ancre_douce_init);
 /* ]]> */</script>';
