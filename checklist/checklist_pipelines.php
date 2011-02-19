ae812bbb0dec128c6831ca408bfbc53e6025ccad
cyril@cym.fr <cyril@cym.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-02-19 19:55:03 +0000

    Attention : en cours, pas terminé !
    L'objectif serait de ne pas créer d'objet nouveau mais de se servir de l'objet ticket existant; le plugin mots_objets permettrait de catégoriser les tickets. 
    
    Pour l'instant je ne parviens pas à passer correctement le tableau à la fonction declaration_liaison_mots ?
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@44839 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ checklist/checklist_pipelines.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Plugin checklist
+ * Licence GPL (c) 2011 - Ateliers CYM
+ */
+
+function checklist_declarer_liaison_mots() {
+
+	// passer le tableau à la fonction declaration_liaison_mots :-)
+	
+	/*
+	declaration_liaison_mots('tickets', array(
+		'exec_formulaire_liaison' => "tickets_infos",
+		
+		'singulier' => "checklist:info_un_ticket",
+		'pluriel'   => "checklist:info_des_tickets",
+		'libelle_objet' => "checklist:objet_ticket",
+		'libelle_liaisons_objets' => "checklist:item_mots_cles_association_tickets"		
+	));
+	*/
+	
+	// spip_log('pipeline declarer liaison mot lu...','checklist');
+	
+	return;
+
+}
+
+?>
---
+++ checklist/lang/checklist_fr.php
@@ -0,0 +1,17 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// I
+	'info_un_ticket' => 'ticket',
+	'info_nombre_tickets' => '@nb@ tickets',
+	'item_mots_cles_association_tickets' => 'aux tickets',
+	
+	// O
+	'objet_ticket' => 'Ticket',
+
+);
+
+?>
