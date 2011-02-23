c92d36b3705cfa6725a50391f0f2d8cd75da06eb
cyril@cym.fr <cyril@cym.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-02-23 18:52:20 +0000

    transformaion des forms en forms avec saisies;
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@44907 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- reperes/lang/reperes_fr.php
+++ reperes/lang/reperes_fr.php
@@ -17,6 +17,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'cfg_lbl_insertion' 		=> 'Point d\'insertion',
 	'cfg_inf_insertion' 		=> 'Ins&#233;rer le bloc de <strong>rep&egrave;res</strong> :',
 	'cfg_help_insertion' 		=> 'Choisissez l\'endroit du squelette o&ugrave; sera ins&eacute;r&eacute; le bloc contenant les rep&egrave;res. Vous pouvez indiquer tout &eacute;l&eacute;ment html qui compose votre page en sp&eacute;cifiant bien s\'il s\'agit d\'une ID ou d\'une CLASS.',
+	'cfg_lbl_bloc'				=> 'le bloc ',
 	'cfg_lbl_avant' 			=> 'avant',
 	'cfg_lbl_dans' 				=> 'dans',
 	'cfg_lbl_apres' 			=> 'apr&egrave;s',	
@@ -24,6 +25,8 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'cfg_help_ajouter' 			=> 'Vous pouvez ajouter un rep&egrave;re et sp&eacute;cifiant ses caract&eacute;riqtiques. L\'identifiant est ajout&eacute; automatiquement.<br /><br />Vous devez s&eacute;lectionner une <em>orientation</em> (verticale ou horizontale) puis indiquer une <em>position</em> et une <em>couleur</em> en h&eacute;xad&eacute;cimal (sous la forme &quot;#3a0f44&quot;) ou selon son nom web (yellow, cyan, purple...).<br /><br />La position s\'indique en pixels (ou pourcentage) &agrave; partir du bord gauche du contenant si l\'orientation est verticale, ou du bord haut du contenant si l\'orientation est horizontale.',
 	'cfg_lbl_existant' 			=> 'Rep&egrave;res existants',
 	'cfg_help_existant'			=> 'Voici la liste des rep&egrave;res d&eacute;j&agrave; param&eacute;tr&eacute;s. Utilisez le bouton "supprimer" pour supprimer un rep&egrave;re &eacute;xistant.',
+	
+	'configurer_insertion' => 'Point d\'insertion',
 
 	// D
 	'deja_modifie' => 'D&eacute;j&agrave; modifi&eacute;',
