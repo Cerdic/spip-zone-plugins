4f68e62daee3315448671cd88c0b8e238290e79c
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-02-25 12:58:01 +0000

    Oups : pour le JS c'est à la fin du head qu'il faut le mettre, après jQuery.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@44930 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- saisies/saisies_pipelines.php
+++ saisies/saisies_pipelines.php
@@ -13,14 +13,19 @@ function saisies_header_prive($flux){
 
 function saisies_affichage_final($flux){
 	if (($p = strpos($flux,"<!--!inserer_saisie_editer-->"))!==false){
+		// On insère la CSS devant le premier <script> trouvé
 		$pi = strpos($flux, "<script");
 		if (!$pi)
 			$pi = $p; // si pas de <script inserer comme un goret entre 2 <li> de saisies
 		$css = generer_url_public('saisies.css');
+		$ins_css = "\n<link rel='stylesheet' href='$css' type='text/css' media='all' />\n";
+		$flux = substr_replace($flux, $ins_css, $pi, 0);
+		
+		// On insère le JS à la fin du <head>
+		$pos_head = strpos($flux, '</head');
 		$js = find_in_path('javascript/saisies.js');
-		$ins = "\n<link rel='stylesheet' href='$css' type='text/css' media='all' />\n";
-		$ins .= "\n<script type='text/javascript' src='$js'></script>\n";
-		$flux = substr_replace($flux, $ins, $pi, 0);
+		$ins_js = "\n<script type='text/javascript' src='$js'></script>\n";
+		$flux = substr_replace($flux, $ins_js, $pos_head, 0);
 	}
 	return $flux;
 }
