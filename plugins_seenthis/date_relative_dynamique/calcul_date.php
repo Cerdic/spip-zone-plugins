310bd443eebdc9875c00d471232f1fba2d12e335
arno@rezo.net <arno@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-02-26 12:15:06 +0000

    Deux plugins développés pour Seenthis.net: (a) lien_court affiche un lien raccourci, les elements n'etant masqués que par CSS, (b) date_relative_dynamique affiche les dates sous la forme «il y a...», en javascript.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@44968 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ plugins_seenthis/date_relative_dynamique/calcul_date.php
@@ -0,0 +1,33 @@
+<?php
+
+function date_u($date) {
+	return date("U", strtotime($date));
+}
+
+function calcul_date_insert_head($flux) {
+	$lang = $GLOBALS["spip_lang"];
+	
+	
+	$ret = "<script src='spip.php?page=js.calcul_date&lang=$lang' type=\"text/javascript\"></script>";
+
+	
+	
+	return $flux.$ret;
+	
+}
+
+
+// Ca, on ne l'utilise plus: on fait un POST et on recupere la date du serveur de cette facon...
+/*
+function calcul_date_affichage_final($flux) {
+	
+	$ret .= "
+	<script type='text/javascript'><!--
+	var date_now = \"".date('U')."\";
+	--></script>
+	";
+	
+	return str_replace("</head>", $ret."</head>", $flux);
+}
+*/
+?>
