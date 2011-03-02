a280f6bac3f209a6989cbdac664192558c24e10b
cyril@cym.fr <cyril@cym.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-03-02 15:19:19 +0000

    on ajoute l'attribut type dans la balise link pour supprimer les avertissements du validateur;
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@45082 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- todo/todo_pipelines.php
+++ todo/todo_pipelines.php
@@ -4,12 +4,12 @@
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 function todo_insert_head_css($flux){
-	$flux.= '<link rel="stylesheet" href="'.find_in_path('css/todo.css').'" />';
+	$flux.= '<link rel="stylesheet" type="text/css" href="'.find_in_path('css/todo.css').'" />';
 	return $flux;
 }
 
 function todo_header_prive($flux){
-	$flux.= '<link rel="stylesheet" href="'.find_in_path('css/todo.css').'" />';
+	$flux.= '<link rel="stylesheet" type="text/css" href="'.find_in_path('css/todo.css').'" />';
 	return $flux;
 }
 
