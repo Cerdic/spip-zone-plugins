0e2e2651fc2c5e40519ee6c6de99f0cf3a6f7194
thomas.kolli@googlemail.com <thomas.kolli@googlemail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-03-02 19:51:38 +0000

    Ajout du plugin Medias Meta glue. Plugin faisant 'glue' entre la mediathque et le plugin metadonnees photos
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@45095 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ metadonnees_mediatheque/lang/medias_metas_fr.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+
+* saveauto : plugin de sauvegarde automatique de la base de données de SPIP
+
+*  
+
+* Ce programme est un logiciel libre distribue sous licence GNU/GPL.
+
+*  
+
+**/
+
+	$GLOBALS['i18n_medias_metas_fr'] = array(
+
+			    // cfg
+
+						 'medias_metas' => 'Métas Médias glue',
+             'config' => 'Plugin Métas Médias glue: configuration',
+             'help_titre' => 'Cette page vous permet de configurer les options du plugin Métas Médias glue.',
+             'oui' => 'oui',
+             'non' => 'non',
+             'choix_meta' => 'Utiliser l\'affichage modifiées',
+             'help_choix_meta' => '(Sinon, utilisation de l\'affichage officiel du plugin métadonnées photos)'
+             
+  );
+
+?>
