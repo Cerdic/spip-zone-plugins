77a5234e64b583ab32a889ec8f62bed19c8ab28b
ngombe@gmail.com <ngombe@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-03-04 21:05:14 +0000

    fichier de langues
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@45175 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ nivoslider/lang/nivoslider_en.php
@@ -0,0 +1,52 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/cfg/lang/
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	// A
+	'animspeed' => 'Slide transition speed',
+	// B
+	'bouton_effacer' => 'Delete',
+	// C
+	'caption_opacity' => 'Universal caption opacity',
+	'controlnav' => 'Show bullets',
+	'cfg_title_nivoslider' => 'NivoSlider configration',
+	'cfg_boite_nivoslider' => 'Nivo Slider plugin configuration.<br /><br />',
+	// D
+	'directionnav' => 'Show Next &amp; Prev arrows ',
+	'directionnavhide' => 'Only show on hover',
+	// E
+	'effect' => 'Transition effect',
+	// F
+	// G
+	// H
+	'height' => 'Height',
+	// I 
+	// J
+	// K
+	'keyboardnav' => 'Use left & right arrows',
+	// L
+	// M
+	// N
+	'non' => 'no',
+	// O
+	'oui' => 'yes',
+	// P
+	'pausetime' => 'Pause between images',
+	'pauseonhover' => 'Stop animation while hovering',
+	// Q
+	// R
+	// S
+	'slices' => 'Cut images in slices',
+	// T
+	// U
+	// V
+	// W
+	'width' => 'Width',
+	// X
+	// Y
+	// Z
+);
+
+?>
---
+++ nivoslider/lang/nivoslider_fr.php
@@ -0,0 +1,52 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/cfg/lang/
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	// A
+	'animspeed' => 'Vitesse de l\'animation',
+	// B
+	'bouton_effacer' => 'Effacer',
+	// C
+	'caption_opacity' => 'Transparence du message',
+	'controlnav' => 'Afficher les puces',
+	'cfg_title_nivoslider' => 'Configuration de NivoSlider',
+	'cfg_boite_nivoslider' => 'Configuration du plugin Nivo Slider.<br /><br />',
+	// D
+	'directionnav' => 'Utiliser les flèches',
+	'directionnavhide' => 'Cacher les fl&egrave;ches (afficher au survol)',
+	// E
+	'effect' => 'Effet de transition',
+	// F
+	// G
+	// H
+	'height' => 'Hauteur',
+	// I 
+	// J
+	// K
+	'keyboardnav' => 'Utiliser les fl&eacute;ches droite &amp; gauche du clavier',
+	// L
+	// M
+	// N
+	'non' => 'non',
+	// O
+	'oui' => 'oui',
+	// P
+	'pausetime' => 'Pause entre les images',
+	'pauseonhover' => 'Pause au survol',
+	// Q
+	// R
+	// S
+	'slices' => 'Nombre de tranches',
+	// T
+	// U
+	// V
+	// W
+	'width' => 'Largeur',
+	// X
+	// Y
+	// Z
+);
+
+?>
