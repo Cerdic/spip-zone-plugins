a9aa76c6025aed1d27008dddaf381d10113d0a80
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-03-06 21:38:16 +0000

    langues (ajaxform)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@45205 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ ajaxforms/lang/ajaxform_fr_tu.php
@@ -0,0 +1,32 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'aucun_fichier_recu' => 'Tu dois s&eacute;lectionner un fichier.',
+
+	// C
+	'changer_document' => 'Changer le document',
+	'confirmer_suppression' => 'La suppression a &eacute;t&eacute; effectu&eacute;e.',
+
+	// D
+	'document_ajoute' => 'Le document a &eacute;t&eacute; ajout&eacute;.',
+
+	// E
+	'erreur_ajout_document' => 'Une erreur est survenue lors de l\'ajout du document.',
+
+	// L
+	'logo' => 'Logo',
+	'logo_maj' => 'Le logo a &eacute;t&eacute; mis &agrave; jour.',
+	'logo_survol' => 'Logo survol&eacute;',
+
+	// M
+	'mauvaise_reception' => 'Oups ! Erreur dans la r&eacute;ception du fichier. R&eacute;essaye...'
+);
+
+?>
