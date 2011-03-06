058c231f77e70d36aa5b044dbfe46f40261c1520
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-03-06 21:39:09 +0000

    langues (tra)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@45221 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ traduction_rubriques_autrement/lang/tra_fr_tu.php
@@ -0,0 +1,17 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'ajouter_traduction' => 'Ajouter une traduction',
+
+	// R
+	'rubrique_reference' => '(rubrique de r&eacute;f&eacute;rence)'
+);
+
+?>
