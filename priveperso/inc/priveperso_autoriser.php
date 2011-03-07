f4fc57a90e770b9578f4144d770e64f024b1d54e
sebastien.zamith@irsamc.ups-tlse.fr <sebastien.zamith@irsamc.ups-tlse.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-03-07 23:27:04 +0000

    pour pouvoir activer/desactiver les articles par rubrique, je passe par le pipeline autoriser, d'ou le nouveau fichier.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@45248 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ priveperso/inc/priveperso_autoriser.php
@@ -0,0 +1,26 @@
+<?php
+
+function priveperso_autoriser(){}
+
+function autoriser_rubrique_creerarticledans($faire, $type, $id, $qui, $opt){
+
+	include_spip('inc/inscrire_priveperso');
+
+	$id_rubrique = priveperso_recupere_id_rubrique();
+	if ($id_rubrique){
+
+// On vérifie si la rubrique en cours ou une des rubriques parentes est personnalisée
+		if (!priveperso_rubrique_deja_perso($id_rubrique)){
+			$id_rub = priveperso_trouver_rubrique_parent_perso($id_rubrique);
+			if (($id_rub!==NULL) && ($id_rub!=='0')) $id_rubrique = $id_rub;
+		}
+
+		if (priveperso_rubrique_deja_perso($id_rubrique)){
+			$priveperso = priveperso_recuperer_valeurs($id_rubrique);
+			if ($priveperso['autoriser_articles']=='non') return false;
+		}
+		else {return true;}
+	}
+}
+
+?>
