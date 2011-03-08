75210092d6d6d471f0adac08b2e7e7c9287c9cec
ben.spip@gmail.com <ben.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-03-08 18:20:55 +0000

    Premier upload d'un plugin mailchimp. Utilise la version 1.3 de l'API mailchimp. Utilise le plugin mes_abonne pour stocker les inscrits dans le SPIP. Lorsque le formulaire d inscription est utilise, stocke en local et chez mailchimp. Pas de zip pour l'instant, pas de doc pour l'instant mais il est fonctionnel
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@45268 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ mailchimp/lang/mailchimp_fr.php
@@ -0,0 +1,22 @@
+<?php
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	//A
+	'api_errorcode' => 'Erreur retournée par l\'API mailchimp ',
+
+	//C
+	'config_erreur' => 'Erreur Les paramêtres d\'accès à Mailchimp ne sont pas renseignés, l\'administrateur du site peut le faire à cette adresse : <a href="ecrire/?exec=configurer_mailchimp">configuration MailChimp</a>',
+
+	//A
+	'demande_inscription_envoyee' => 'Un mail vous a été envoyé a l\'adresse "@email@" . Pour valider votre abonnement vous devez cliquer sur le lien que vous avez reçu. Vérifiez éventuellement dans votre dossier SPAM ou dans votre corbeille si jamais vous ne trouvez pas cet email de confirmation.',
+	'demande_desincription_ok' => 'Votre adresse "@email@" a également été retirée de Mailchimp. Au revoir.',
+
+
+	//fin parceque bon penser a mettre une virgule ou pas c'est penible
+	'zzzz'=>'zzzz'
+
+);
+
+?>
