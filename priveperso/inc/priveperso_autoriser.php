b6b98d338074f8e1496d4fa877da585b7d450b37
sebastien.zamith@irsamc.ups-tlse.fr <sebastien.zamith@irsamc.ups-tlse.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-03-08 22:37:44 +0000

    Correction de bug sur autoriser_articles. Ajout d'une nouvelle autorisation pour gérer l'utilisation ou non des sous-rubriques.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@45272 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- priveperso/inc/priveperso_autoriser.php
+++ priveperso/inc/priveperso_autoriser.php
@@ -18,9 +18,34 @@ function autoriser_rubrique_creerarticledans($faire, $type, $id, $qui, $opt){
 		if (priveperso_rubrique_deja_perso($id_rubrique)){
 			$priveperso = priveperso_recuperer_valeurs($id_rubrique);
 			if ($priveperso['autoriser_articles']=='non') return false;
+			else return true;
 		}
 		else {return true;}
 	}
+}
+
+function autoriser_rubrique_creerrubriquedans($faire, $type, $id, $qui, $opt){
+	
+	include_spip('inc/inscrire_priveperso');
+
+	$id_rubrique = priveperso_recupere_id_rubrique();
+	if ($id_rubrique){
+
+// On vérifie si la rubrique en cours ou une des rubriques parentes est personnalisée
+		if (!priveperso_rubrique_deja_perso($id_rubrique)){
+			$id_rub = priveperso_trouver_rubrique_parent_perso($id_rubrique);
+			if (($id_rub!==NULL) && ($id_rub!=='0')) $id_rubrique = $id_rub;
+		}
+
+		if (priveperso_rubrique_deja_perso($id_rubrique)){
+			$priveperso = priveperso_recuperer_valeurs($id_rubrique);
+			if ($priveperso['autoriser_sous_rubriques']=='non') return false;
+			else return true;
+		}
+		else {return true;}
+	}	
+	
+	
 }
 
 ?>
