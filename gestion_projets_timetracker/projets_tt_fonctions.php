5f0892a1fc41e2a7d449f2b7629028b5546923c1
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-03-08 11:30:49 +0000

    la fonction difference au core
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@45259 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- gestion_projets_timetracker/projets_tt_fonctions.php
+++ gestion_projets_timetracker/projets_tt_fonctions.php
@@ -1,31 +1,6 @@
 <?php
-     function difference($date_debut,$date_fin,$secondes=3600){
-     
-     
-     $date_debut_jours = date('d-m-Y',strtotime($date_debut));
-     $date_debut_heures = date('G:i:s',strtotime($date_debut));
-     
-     $split_jours_debut = explode("-", $date_debut_jours);
-     $split_heures_debut = explode(":", $date_debut_heures);
-     
-     $date_fin_jours = date('d-m-Y',strtotime($date_fin));
-     $date_fin_heures = date('G:i:s',strtotime($date_fin));
-     
-     $split_jours_fin = explode("-", $date_fin_jours);
-     $split_heures_fin = explode(":", $date_fin_heures);
-     
-     $date_f = mktime($split_heures_fin[0], $split_heures_fin[1], $split_heures_fin[2], $split_jours_fin[1], $split_jours_fin[0], $split_jours_fin[2]) ;
-     $date_debut = mktime($split_heures_debut[0], $split_heures_debut[1], $split_heures_debut[2], $split_jours_debut[1], $split_jours_debut[0], $split_jours_debut[2]) ;   	
-     $date_difference = ($date_f-$date_debut)/60;
-     
-     return number_format($date_difference,20);
-     
-     
-     
-     }
-     
-     
      function duree($id_tache,$difference=''){
+     include_spip('gestion_projet_fonctions');
      
      $sql=sql_select('duree','spip_projets_timetracker','id_tache='.sql_quote($id_tache));
      $duree=0;	
