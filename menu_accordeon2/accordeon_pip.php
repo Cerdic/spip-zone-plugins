52f6fb2dbd8bc0d59b312cb583ead8a1ad114294
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-03-09 20:26:18 +0000

    menu accordéon pour SPIP 2 : necessite jqueryui + le plugin menu => il faut créer un menu avec la classe .accordeon
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@45296 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ menu_accordeon2/accordeon_pip.php
@@ -0,0 +1,12 @@
+<?php
+
+function accordeon_jqueryui_forcer($array){
+	$array[] ='jquery.ui.accordion';
+	return $array;	
+}
+
+function accordeon_jquery_plugins($array){
+	$array[]='accordeon.js';
+	return $array;
+}
+?>
