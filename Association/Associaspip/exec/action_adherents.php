0edf82049752183f7ceece5953ca59de7b6b833b
esj@rezo.net <esj@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-03-11 15:14:47 +0000

    [source:_plugins_/Association/Associaspip Associaspip]. Quand on retire un membre, indiquer son numéro dans la page de confirmation (dans le cas des homonymes, c'est indispensable).
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@45334 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- Association/Associaspip/exec/action_adherents.php
+++ Association/Associaspip/exec/action_adherents.php
@@ -48,9 +48,9 @@ function supprimer_adherents($delete_tab)
 	$res ='<table>';
 	foreach ($delete_tab as $id) {
 		$id = intval($id);
-		$query = sql_select("*",_ASSOCIATION_AUTEURS_ELARGIS, "id_auteur=$id");
+		$query = sql_select("id_auteur, prenom, nom_famille",_ASSOCIATION_AUTEURS_ELARGIS, "id_auteur=$id");
 		while($data = sql_fetch($query)) {
-			$res .="\n<tr><td><strong>".$data['nom_famille'].' '.$data['prenom'].'</strong></td><td><input type="checkbox" name="drop[]" value="'.$id.'" checked="checked" /></td></tr>';
+			$res .="\n<tr><td>" . $data['id_auteur'] . " <strong>".$data['nom_famille'].' '.$data['prenom'].'</strong></td><td><input type="checkbox" name="drop[]" value="'.$id.'" checked="checked" /></td></tr>';
 		}
 	}
 	$res .='<tr>';
