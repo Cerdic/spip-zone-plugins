baa831c26f39931c34b503540745889890e0886e
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-03-13 14:58:38 +0000

    Ajout du type de parseur pour préparer la migration vers la nouvelle DTD
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@45374 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- svp/svp_options.php
+++ svp/svp_options.php
@@ -16,6 +16,11 @@ define('_SVP_LOG_PAQUETS', true);
 // Définition de la boite du logo de depot pour utilisation de iconifier()
 $GLOBALS['logo_libelles']['id_depot'] = _T('svp:titre_boite_logo_depot');
 
+// Type parseur XML a appliquer pour recuperer les infos du plugin
+// - plugin, pour utiliser plugin.xml
+// - paquet, pour paquet.xml
+define('_SVP_DTD_PLUGIN', 'plugin');
+
 // Infos *temporaires* du plugin de prefixe 'theme'
 define('_SVP_PREFIXE_PLUGIN_THEME', 'THEME');
 define('_SVP_NOM_PLUGIN_THEME', 'Thèmes SPIP');
