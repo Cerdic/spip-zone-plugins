5bc42076705140757c519103476d47f271047994
erational@erational.org <erational@erational.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-03-15 05:51:57 +0000

    echappe les <- pour permettre les raccourcis  [xxx<-]
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@45424 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spip2spip/spiptospip_fonctions.php
+++ spip2spip/spiptospip_fonctions.php
@@ -133,6 +133,9 @@ function analyser_backend_spip2spip($rss){
 	$rss = str_replace("&lt;multi&gt;",  "@@@MULTI@@@", $rss);
 	$rss = str_replace("&lt;/multi&gt;",  "@@@MULTJ@@@", $rss);
 	
+	// lien interne  <- (pas echappe)
+	$rss = str_replace("&lt;-",  "@@@LIEN_INV@@@", $rss);
+	
 	// version du flux
 	$version_flux = 0;
 	if (preg_match_all(',<spip2spip version="(.*?)">,Uims',$rss,$r, PREG_SET_ORDER)) 
@@ -300,7 +303,8 @@ function analyser_backend_spip2spip($rss){
 				$data[$var] = str_replace("@@@SPIP_CDATA$n@@@",$e, $data[$var]);
       if (!defined("_SPIP2SPIP_IMPORT_HTML")) $data[$var] = trim(textebrut($data[$var]));  // protection import HTML 
 			$data[$var] = str_replace("@@@MULTI@@@", "<multi>", $data[$var]);
-	    $data[$var] = str_replace("@@@MULTJ@@@", "</multi>", $data[$var]);	
+	    $data[$var] = str_replace("@@@MULTJ@@@", "</multi>", $data[$var]);
+      $data[$var] = str_replace("@@@LIEN_INV@@@", "<-", $data[$var]);	
 		}
 
 		//$data['item'] = $item;  //utile pour spip2spip ?		
