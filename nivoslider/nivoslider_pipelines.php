fc3a9937dbe6c7b1862e9e6eb124058c79ea06fe
ngombe@gmail.com <ngombe@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-03-16 01:15:24 +0000

    bugfix
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@45451 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- nivoslider/nivoslider_pipelines.php
+++ nivoslider/nivoslider_pipelines.php
@@ -1,17 +1,12 @@
 <?php
 
-function nivoslider_insert_head_css($flux){
+function nivoslider_insert_head($flux){ 
 	static $done = false;
 	if (!$done) {
 		$done = true;
 		$flux .= '<link rel="stylesheet" href="'.url_absolue(generer_url_public('css_nivoslider')).'" type="text/css" media="all" />';
+		$flux .= '<script src="'.url_absolue(find_in_path('js/jquery.nivo.slider.pack.js')).'" type="text/javascript"></script>';
 	}
-	return $flux;
-}
-
-function nivoslider_insert_head($flux){
-	$flux .='<script src="'.url_absolue(find_in_path('js/jquery.nivo.slider.pack.js')).'" type="text/javascript"></script>';
-	$flux .= nivoslider_insert_head_css($flux); // compat pour les vieux spip
 	return $flux;
 }
 
