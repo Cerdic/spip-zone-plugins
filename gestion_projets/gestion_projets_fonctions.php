df581d8f9a9d9c545f79656b9d40f58acf8a79db
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-03-17 16:31:53 +0000

    reprendre le paramtyre secondes dans le calcul
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@45504 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- gestion_projets/gestion_projets_fonctions.php
+++ gestion_projets/gestion_projets_fonctions.php
@@ -1,7 +1,5 @@
 <?php
      function difference($date_debut,$date_fin,$secondes=3600){
-     
-     
      $date_debut_jours = date('d-m-Y',strtotime($date_debut));
      $date_debut_heures = date('G:i:s',strtotime($date_debut));
      
@@ -16,9 +14,9 @@
      
      $date_f = mktime($split_heures_fin[0], $split_heures_fin[1], $split_heures_fin[2], $split_jours_fin[1], $split_jours_fin[0], $split_jours_fin[2]) ;
      $date_debut = mktime($split_heures_debut[0], $split_heures_debut[1], $split_heures_debut[2], $split_jours_debut[1], $split_jours_debut[0], $split_jours_debut[2]) ;   	
-     $date_difference = ($date_f-$date_debut)/60;
-     
-     return number_format($date_difference,20); 
+     $date_difference = ($date_f-$date_debut)/$secondes;
+
+     return number_format($date_difference,2); 
      }
 
 ?>
