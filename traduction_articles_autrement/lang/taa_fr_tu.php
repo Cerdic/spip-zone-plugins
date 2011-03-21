6d4c64ba07f5b0232abb9b4603a6c696ebd279d8
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-03-21 21:08:39 +0000

    langues (taa)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@45652 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ traduction_articles_autrement/lang/taa_fr_tu.php
@@ -0,0 +1,40 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'affichage_edition_seule' => 'Affichage des articles seulement en mode &eacute;dition',
+	'affichage_liste_compacte' => 'Affichage compacte des liste des articles',
+
+	// C
+	'changer_langue' => 'Changer la langue de l\'article',
+
+	// F
+	'fermer' => 'Fermer',
+
+	// L
+	'lier_traduction' => 'Attacher une traduction',
+
+	// M
+	'modifier_options_langue' => 'options',
+
+	// N
+	'necesite_afficher_objet' => 'Pour pouvoir utiliser cette fonction tu dois installer le plugin',
+
+	// O
+	'options' => 'Modifier les options de langues',
+
+	// T
+	'titre_plugin' => 'Traduction d\'articles autrement',
+	'traductions' => 'Traductions',
+
+	// V
+	'voir_traductions' => 'Voir le d&eacute;tail des traductions'
+);
+
+?>
