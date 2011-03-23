794cfe5144f396d50e3320649fee14187df9e175
esj@rezo.net <esj@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-03-23 08:21:37 +0000

    [source:_plugins_/Association/Associaspip Associaspip] Ne pas déclencher de faux messages d'erreurs quand on n'a pas encore défini le plan comptable. Et respecter les règles de typographie française.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@45744 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- Association/Associaspip/formulaires/configurer_association.php
+++ Association/Associaspip/formulaires/configurer_association.php
@@ -16,23 +16,22 @@ function formulaires_configurer_association_verifier_dist() {
 	$ref_attribuee = array();
 	// on verifie qu'il n'a pas deux fois la meme reference comptable en incluant celle des cotisations
 	$ref_attribuee[_request('pc_cotisations')]=0;
-	if (_request('dons') == 'on') {
-		$ref_dons = _request('pc_dons');
+	if ((_request('dons') == 'on') AND $ref_dons = _request('pc_dons')) {
 		if (!array_key_exists($ref_dons,$ref_attribuee)) {
 			$ref_attribuee[$ref_dons]=0;
 		}
 		else $erreur = true;
 	}
 
-	if (_request('ventes') == 'on') {
-		$ref_ventes = _request('pc_ventes');
+	if ((_request('ventes') == 'on' AND $ref_ventes = _request('pc_ventes'))) {
+	  
 		$ref_frais_envoi = _request('pc_frais_envoi');
 		if (!array_key_exists($ref_ventes,$ref_attribuee)) {
 			$ref_attribuee[$ref_ventes]=0;
 		}
 		else $erreur = true;
 		if ($ref_ventes != $ref_frais_envoi) {
-			/* vente et frais_envoi peuvent etre associés a la meme reference comptable meme si c'est deconseille d'un point de vue comptable */
+			/* vente et frais_envoi peuvent etre associes a la meme reference comptable meme si c'est deconseille d'un point de vue comptable */
 			if (!array_key_exists($ref_frais_envoi,$ref_attribuee)) {
 				$ref_attribuee[$ref_frais_envoi]=0;
 			}
@@ -40,23 +39,20 @@ function formulaires_configurer_association_verifier_dist() {
 		}
 	}
 
-	if (_request('prets') == 'on') {
-		$ref_prets = _request('pc_prets');
+	if ((_request('prets') == 'on') AND $ref_prets = _request('pc_prets')){
 		if (!array_key_exists($ref_prets,$ref_attribuee)) {
 			$ref_attribuee[$ref_prets]=0;
 		}
 		else $erreur = true;
 	}	 
 
-	if (_request('activites') == 'on') {
-		$ref_activites = _request('pc_activites');
+	if ((_request('activites') == 'on') AND $ref_activites = _request('pc_activites')) {
 		if (!array_key_exists($ref_activites,$ref_attribuee)) {
 			$ref_attribuee[$ref_activites]=0;
 		}
 		else $erreur = true;
 	}	 
-
-	if ($erreur) $erreurs['message_erreur'] = _T('asso:erreur_configurer_association_titre').'<br/>'._T('asso:erreur_configurer_association_reference_multiple');
-	return $erreurs;
+	return !$erreur ? array() 
+	  : array('message_erreur' => _T('asso:erreur_configurer_association_titre').'<br/>'._T('asso:erreur_configurer_association_reference_multiple')) ;
 }
 ?>
