d50d50b27170bf97809002228b014b8fff90de47
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-03-23 23:28:20 +0000

    langues (thumbsites)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@45800 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- thumbsites/lang/thumbsites_gl.php
+++ thumbsites/lang/thumbsites_gl.php
@@ -11,23 +11,23 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'bouton_effacer' => 'Desfacer',
 	'bouton_valider' => 'Validar',
 	'bulle_bouton_effacer' => 'Desfacer os resultados de volver aos valores de partida',
-	'bulle_bouton_valider' => 'Validar as sa&iacute;das',
+	'bulle_bouton_valider' => 'Validar as saídas',
 
 	// C
 	'cfg_descriptif' => 'Cette page vous permez de configurer le plugin Thumbsites, et, en particulier, de choisir le serveur de vignettes. Pour en savoir plus, consultez la <a href="http://www.spip-contrib.net/?article2584">documentation sur contrib</a>', # NEW
-	'cfg_inf_girafa_description' => 'Les vignettes g&eacute;n&eacute;r&eacute;es seront au maximum de 160 x 120 pixels (cette valeur peut &ecirc;tre modifi&eacute;e sur le site de Girafa)', # NEW
-	'cfg_inf_girafa_parametres' => 'Veuillez saisir vos donn&eacute;es personnelles:', # NEW
-	'cfg_inf_girafa_presentation' => 'Pour utiliser ce service vous devez &ecirc;tre inscrit sur le site <a href="http://www.girafa.com">Girafa</a>. Il existe une version gratuite si vous souhaitez moins de 2000 captures par jour.', # NEW
-	'cfg_inf_miwin_parametres' => 'Veuillez saisir vos donn&eacute;es personnelles:', # NEW
-	'cfg_inf_miwin_presentation' => 'Pour utiliser ce service vous devez mettre un lien vers le site <a href="http://thumbs.miwim.fr">Miwin</a>. La pr&eacute;sence de ce lien est v&eacute;rifi&eacute; p&eacute;riodiquement par un script.', # NEW
-	'cfg_inf_rotothumb_parametres' => 'Veuillez saisir vos donn&eacute;es personnelles:', # NEW
-	'cfg_inf_rotothumb_presentation' => 'Pour utiliser ce service vous ne devez pas vous enregistrer mais vous devez mettre un lien vers le site <a href="http://www.robothumb.com">Robothumb</a>. La pr&eacute;sence de ce lien est v&eacute;rifi&eacute; p&eacute;riodiquement par leur soin.', # NEW
-	'cfg_inf_thumbshots_de_description' => 'Les vignettes g&eacute;n&eacute;r&eacute;es seront de 120 x 90 pixels.', # NEW
-	'cfg_inf_thumbshots_de_presentation' => '<a href="http://www.thumbshots.de">Thumbshots.de</a> propose un service &eacute;quivalent &agrave; Thumbshots.com. Bien que les services soient identiques, ce sont bien 2 sites distincts.', # NEW
-	'cfg_inf_thumbshots_description' => 'Les vignettes g&eacute;n&eacute;r&eacute;es seront de 120 x 90 pixels.', # NEW
-	'cfg_inf_thumbshots_presentation' => '<a href="http://www.thumbshots.com">Thumbshots.com</a> propose un service gratuit qui ne n&eacute;cessite pas d\'identification.', # NEW
-	'cfg_inf_websnapr_parametres' => 'Veuillez saisir vos donn&eacute;es personnelles:', # NEW
-	'cfg_inf_websnapr_presentation' => 'Pour utiliser ce service vous devez &ecirc;tre inscrit sur le site <a href="http://www.websnapr.com">Websnapr</a>. La version gratuite permet de traiter de 250.000 captures de site par mois, &agrave; un rythme de 80 captures par heure.', # NEW
+	'cfg_inf_girafa_description' => 'Les vignettes générées seront au maximum de 160 x 120 pixels (cette valeur peut être modifiée sur le site de Girafa)', # NEW
+	'cfg_inf_girafa_parametres' => 'Veuillez saisir vos données personnelles:', # NEW
+	'cfg_inf_girafa_presentation' => 'Pour utiliser ce service vous devez être inscrit sur le site <a href="http://www.girafa.com">Girafa</a>. Il existe une version gratuite si vous souhaitez moins de 2000 captures par jour.', # NEW
+	'cfg_inf_miwin_parametres' => 'Veuillez saisir vos données personnelles:', # NEW
+	'cfg_inf_miwin_presentation' => 'Pour utiliser ce service vous devez mettre un lien vers le site <a href="http://thumbs.miwim.fr">Miwin</a>. La présence de ce lien est vérifié périodiquement par un script.', # NEW
+	'cfg_inf_rotothumb_parametres' => 'Veuillez saisir vos données personnelles:', # NEW
+	'cfg_inf_rotothumb_presentation' => 'Pour utiliser ce service vous ne devez pas vous enregistrer mais vous devez mettre un lien vers le site <a href="http://www.robothumb.com">Robothumb</a>. La présence de ce lien est vérifié périodiquement par leur soin.', # NEW
+	'cfg_inf_thumbshots_de_description' => 'Les vignettes générées seront de 120 x 90 pixels.', # NEW
+	'cfg_inf_thumbshots_de_presentation' => '<a href="http://www.thumbshots.de">Thumbshots.de</a> propose un service équivalent à Thumbshots.com. Bien que les services soient identiques, ce sont bien 2 sites distincts.', # NEW
+	'cfg_inf_thumbshots_description' => 'Les vignettes générées seront de 120 x 90 pixels.', # NEW
+	'cfg_inf_thumbshots_presentation' => '<a href="http://www.thumbshots.com">Thumbshots.com</a> propose un service gratuit qui ne nécessite pas d\'identification.', # NEW
+	'cfg_inf_websnapr_parametres' => 'Veuillez saisir vos données personnelles:', # NEW
+	'cfg_inf_websnapr_presentation' => 'Pour utiliser ce service vous devez être inscrit sur le site <a href="http://www.websnapr.com">Websnapr</a>. La version gratuite permet de traiter de 250.000 captures de site par mois, à un rythme de 80 captures par heure.', # NEW
 	'cfg_itm_serveur_girafa' => 'Girafa', # NEW
 	'cfg_itm_serveur_miwin' => 'Miwin', # NEW
 	'cfg_itm_serveur_robothumb' => 'Robothumb', # NEW
@@ -36,14 +36,14 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'cfg_itm_serveur_websnapr' => 'Websnapr', # NEW
 	'cfg_itm_taille_vignette' => '@taille@ pixels', # NEW
 	'cfg_lbl_choix_serveur' => 'Choisissez le serveur de vignettes', # NEW
-	'cfg_lbl_cle' => 'Votre cl&eacute;', # NEW
+	'cfg_lbl_cle' => 'Votre clé', # NEW
 	'cfg_lbl_identifiant' => 'Votre identifiant', # NEW
 	'cfg_lbl_signature' => 'Votre signature', # NEW
 	'cfg_lbl_taille_vignette' => 'Taille des vignettes', # NEW
 	'cfg_lgd_choix_serveur' => 'Serveur', # NEW
 
 	// T
-	'titre_thumbshot_site' => 'VI&Ntilde;ETA DO SITIO'
+	'titre_thumbshot_site' => 'VIÑETA DO SITIO'
 );
 
 ?>
