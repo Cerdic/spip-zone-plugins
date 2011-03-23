ce11076adff55066f0381b12140831e58a1bc165
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-03-23 16:10:18 +0000

    de même
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@45764 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- traduction_articles_autrement/lang/taa_fr_tu.php
+++ traduction_articles_autrement/lang/taa_fr_tu.php
@@ -8,7 +8,6 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
-	'affichage_edition_seule' => 'Affichage des articles seulement en mode &eacute;dition',
 	'affichage_liste_compacte' => 'Affichage compacte des liste des articles',
 
 	// C
