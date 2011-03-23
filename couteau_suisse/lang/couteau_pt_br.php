7a62ac0998ee897d6aad06bf1db8974a6de6ea58
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-03-23 23:27:03 +0000

    langues (couteau)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@45775 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- couteau_suisse/lang/couteau_pt_br.php
+++ couteau_suisse/lang/couteau_pt_br.php
@@ -11,7 +11,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'2pts' => '@objet@:',
 
 	// A
-	'attente_confirmation' => 'Aguardando confirma&ccedil;&atilde;o',
+	'attente_confirmation' => 'Aguardando confirmação',
 
 	// B
 	'base_donnees' => 'Base de dados',
@@ -21,34 +21,34 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// C
 	'colorer_fond' => 'Colorir o fundo',
 	'colorer_texte' => 'Colorir o texto',
-	'configurer' => 'Configurar o Canivete Sui&ccedil;o',
-	'connections' => 'Conex&otilde;es',
+	'configurer' => 'Configurar o Canivete Suiço',
+	'connections' => 'Conexões',
 
 	// D
 	'date_court' => '@jour@/@mois@/@annee@',
-	'derniers_connectes' => '&Uacute;ltimos conectados:',
+	'derniers_connectes' => 'Últimos conectados:',
 
 	// E
 	'email_forum' => 'Obrigado por informar o seu e-mail!',
-	'explique_spam' => 'Aten&ccedil;&atilde;o: voc&ecirc; usou tags, caracteres ou palavras proibidas.<br /><br />Retorne!',
+	'explique_spam' => 'Atenção: você usou tags, caracteres ou palavras proibidas.<br /><br />Retorne!',
 
 	// L
 	'lancer_test' => 'Iniciar o teste!',
 	'lutte_spam' => 'Lute contra o SPAM',
 
 	// M
-	'maj_actu' => 'For&ccedil;ar a atualiza&ccedil;&atilde;o remota',
+	'maj_actu' => 'Forçar a atualização remota',
 	'maj_liste' => 'Lista dos plugins detectados:',
 	'maj_maj' => 'Atualizar o plugin selecionado',
 	'maj_ok' => 'Este [plugin->@zip@] parece estar [em dia->@url@].',
-	'maj_rev' => 'Rev.&nbsp;@revision@',
-	'maj_rev_ko' => 'A [revis&atilde;o remota->@url@] n&atilde;o foi encontrada.',
-	'maj_rev_ok' => 'A revis&atilde;o [@revision@->@url@] est&aacute; [dispon&iacute;vel->@zip@].',
-	'maj_spip' => 'Ap&oacute;s ter atualizado todos os seus plugins com precau&ccedil;&atilde;o, &eacute; fortemente aconselh&aacute;vel atualizar o pr&oacute;prio SPIP, clicando no link abaixo e seguindo as instru&ccedil;&otilde;es apresentadas.',
+	'maj_rev' => 'Rev. @revision@',
+	'maj_rev_ko' => 'A [revisão remota->@url@] não foi encontrada.',
+	'maj_rev_ok' => 'A revisão [@revision@->@url@] está [disponível->@zip@].',
+	'maj_spip' => 'Após ter atualizado todos os seus plugins com precaução, é fortemente aconselhável atualizar o próprio SPIP, clicando no link abaixo e seguindo as instruções apresentadas.',
 	'maj_svn' => 'Este plugin SVN parece estar [em dia->@url@].',
-	'maj_verif' => 'Verifique previamente o arquivo que lhe conv&eacute;m:',
-	'maj_verif2' => 'Aten&ccedil;&atilde;o: ap&oacute;s ter clicado no bot&atilde;o abaixo, certifique-se que o arquivo transferido corresponde ao plugin que deseja atualizar.',
-	'maj_zip_ko' => 'O arquivo .zip remoto n&atilde;o pode ser identificado.',
+	'maj_verif' => 'Verifique previamente o arquivo que lhe convém:',
+	'maj_verif2' => 'Atenção: após ter clicado no botão abaixo, certifique-se que o arquivo transferido corresponde ao plugin que deseja atualizar.',
+	'maj_zip_ko' => 'O arquivo .zip remoto não pode ser identificado.',
 	'msg_publies' => 'Unicamente as mensagens publicadas',
 
 	// N
@@ -57,84 +57,84 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'non_confirmes' => 'Nomes confirmados:',
 
 	// O
-	'objet_article' => 'Mat&eacute;ria',
-	'objet_articles' => 'Mat&eacute;rias',
+	'objet_article' => 'Matéria',
+	'objet_articles' => 'Matérias',
 	'objet_auteur' => 'Autor',
 	'objet_auteurs' => 'Autores',
 	'objet_breve' => 'Nota',
 	'objet_breves' => 'Notas',
 	'objet_mot' => 'Palavra',
 	'objet_mots' => 'Palavras',
-	'objet_petition' => 'Peti&ccedil;&atilde;o',
-	'objet_petitions' => 'Peti&ccedil;&otilde;es',
-	'objet_rubrique' => 'Se&ccedil;&atilde;o',
-	'objet_rubriques' => 'Se&ccedil;&otilde;es',
+	'objet_petition' => 'Petição',
+	'objet_petitions' => 'Petições',
+	'objet_rubrique' => 'Seção',
+	'objet_rubriques' => 'Seções',
 	'objet_syndic' => 'Site',
 	'objet_syndics' => 'Sites',
 
 	// P
-	'page_debut' => 'Primeira p&aacute;gina',
-	'page_fin' => '&Uacute;ltima p&aacute;gina',
-	'page_lien' => 'P&aacute;gina @page@: @title@',
-	'page_precedente' => 'P&aacute;gina anterior',
-	'page_suivante' => 'Pr&oacute;xima p&aacute;gina',
+	'page_debut' => 'Primeira página',
+	'page_fin' => 'Última página',
+	'page_lien' => 'Página @page@: @title@',
+	'page_precedente' => 'Página anterior',
+	'page_suivante' => 'Próxima página',
 	'plug_actifs' => 'Plugins ativos',
 	'plug_inactifs' => 'Plugins inativos',
-	'plugin_xml' => 'Re&uacute;ne num &uacute;nico plugin uma lista de pequenas e &uacute;teis funcionalidades, que melhoram o gerenciamento do seu site SPIP.
+	'plugin_xml' => 'Reúne num único plugin uma lista de pequenas e úteis funcionalidades, que melhoram o gerenciamento do seu site SPIP.
 
-Cada uma destas ferramentas pode ser ativada ou desativada pelo usu&aacute;rio na [p&aacute;gina de administra&ccedil;&atilde;o do plugin->./?exec=admin_couteau_suisse] e gerar um certo n&uacute;mero de vari&aacute;veis: clique em {{Configuration}}, e depois escolha a guia {{Le Couteau Suisse}}.
+Cada uma destas ferramentas pode ser ativada ou desativada pelo usuário na [página de administração do plugin->./?exec=admin_couteau_suisse] e gerar um certo número de variáveis: clique em {{Configuration}}, e depois escolha a guia {{Le Couteau Suisse}}.
 
-As categorias dispon&iacute;veis s&atilde;o: Administra&ccedil;&atilde;o, Melhoramentos tipogr&aacute;ficos, Atalhos tipogr&aacute;ficos, Exibi&ccedil;&atilde;o p&uacute;blica, Tags, filtros, crit&eacute;rios.
+As categorias disponíveis são: Administração, Melhoramentos tipográficos, Atalhos tipográficos, Exibição pública, Tags, filtros, critérios.
 
-Descobra neste plugin as suas ferramentas favoritas: {Suprime o n&uacute;mero}, {Formato dos URLs}, {Sobrescritos}, {Aspas tipogr&aacute;ficas}, {Bullets melhorados}, {Luta contra o SPAM}, {MailCrypt}, {URLs melhorados}, {SPIP e links... externos}, {Smileys}, {Um &iacute;ndice para os seus artigos}, {Dividir em p&aacute;ginas e guias}, etc., etc.
+Descobra neste plugin as suas ferramentas favoritas: {Suprime o número}, {Formato dos URLs}, {Sobrescritos}, {Aspas tipográficas}, {Bullets melhorados}, {Luta contra o SPAM}, {MailCrypt}, {URLs melhorados}, {SPIP e links... externos}, {Smileys}, {Um índice para os seus artigos}, {Dividir em páginas e guias}, etc., etc.
 
-N&atilde;o deixe de consultar os artigos de documenta&ccedil;&atilde;o do plugin, publicados em: [spip-contrib.net->http://www.spip-contrib.net/Le-Couteau-Suisse].
+Não deixe de consultar os artigos de documentação do plugin, publicados em: [spip-contrib.net->http://www.spip-contrib.net/Le-Couteau-Suisse].
 
 Compatibilidade: SPIP v1.9x e v2.0', # MODIF
-	'pp_autobr' => 'Ins&eacute;rer un bloc o&ugrave; les retours &aacute; la ligne sont automatiques', # NEW
+	'pp_autobr' => 'Insérer un bloc où les retours á la ligne sont automatiques', # NEW
 	'pp_blocs_bloc' => 'Inserir um bloco comprimido',
 	'pp_blocs_visible' => 'Inserir um bloco expandido',
-	'pp_chatons_inserer' => 'Ins&eacute;rer le chaton &laquo;&nbsp;@chaton@&nbsp;&raquo;', # NEW
-	'pp_chatons_inserer_drop' => 'Ins&eacute;rer un chaton dans votre texte', # NEW
+	'pp_chatons_inserer' => 'Insérer le chaton « @chaton@ »', # NEW
+	'pp_chatons_inserer_drop' => 'Insérer un chaton dans votre texte', # NEW
 	'pp_couleur_fond' => 'Fundo @couleur@',
 	'pp_couleur_icone_fond' => 'F',
 	'pp_couleur_icone_texte' => 'T',
 	'pp_couleur_texte' => 'Texto @couleur@',
-	'pp_couteau_suisse_drop' => 'Usar uma ferramenta do Canivete Sui&ccedil;o',
-	'pp_decoration_inserer' => 'Decora&ccedil;&atilde;o &laquo;&nbsp;@balise@.@racc@&nbsp;&raquo;',
-	'pp_decoration_inserer_drop' => 'Aplicar ao texto uma &laquo;&nbsp;d&eacute;coration&nbsp;&raquo; personalizada',
+	'pp_couteau_suisse_drop' => 'Usar uma ferramenta do Canivete Suiço',
+	'pp_decoration_inserer' => 'Decoração « @balise@.@racc@ »',
+	'pp_decoration_inserer_drop' => 'Aplicar ao texto uma « décoration » personalizada',
 	'pp_decoupe_onglets' => 'Inserir um sistema de guias',
-	'pp_decoupe_separateur' => 'Inserir um separador de p&aacute;gina ou de guia',
-	'pp_filets_inserer' => 'Inserir um fio do tipo &laquo;&nbsp;@filet@&nbsp;&raquo;',
-	'pp_filets_inserer_drop' => 'Inserir um fio de separa&ccedil;&atilde;o',
+	'pp_decoupe_separateur' => 'Inserir um separador de página ou de guia',
+	'pp_filets_inserer' => 'Inserir um fio do tipo « @filet@ »',
+	'pp_filets_inserer_drop' => 'Inserir um fio de separação',
 	'pp_smileys_inserer' => 'Inserir um emoticon @smiley@',
-	'pp_un_titre' => 'Um t&iacute;tulo',
+	'pp_un_titre' => 'Um título',
 	'pp_votre_texte' => 'Coloque o seu texto aqui',
-	'pp_votre_titre' => 'T&iacute;tulo @nb@',
+	'pp_votre_titre' => 'Título @nb@',
 
 	// S
-	'smileys_dispos' => 'Caricaturas dispon&iacute;veis:',
-	'sommaire_page' => ',&nbsp;p@page@',
-	'sommaire_page_long' => ',&nbsp;p&aacute;gina&nbsp;@page@',
-	'sommaire_titre' => '&Iacute;ndice',
+	'smileys_dispos' => 'Caricaturas disponíveis:',
+	'sommaire_page' => ', p@page@',
+	'sommaire_page_long' => ', página @page@',
+	'sommaire_titre' => 'Índice',
 	'stats_auteur' => '@icon@ @nom@, @date@',
-	'stats_date' => '@jour@/@mois@/@annee@&nbsp;&agrave;s&nbsp;@h@h@m@',
+	'stats_date' => '@jour@/@mois@/@annee@ às @h@h@m@',
 
 	// T
 	'test_base' => 'Teste da base de dados',
 	'test_manuel' => 'Teste manual',
 	'texte_formatspip' => 'TEXTO ORIGINAL EM FORMATO SPIP',
 	'textes_formatspip' => 'TEXTOS ORIGINAIS EM FORMATO SPIP',
-	'titre' => 'Canivete Su&iacute;&ccedil;o',
+	'titre' => 'Canivete Suíço',
 	'tri_auteurs' => 'ORDEM DOS AUTORES',
 	'tri_descendre' => 'Baixar este autor',
 	'tri_monter' => 'Subir este autor',
 
 	// U
-	'urls_propres_erreur' => 'Este formato n&atilde;o leva em conta os URLs limpos listados acima.',
-	'urls_propres_format' => 'O formato atual dos URLs &eacute;: {{&laquo;&nbsp;@format@&nbsp;&raquo;}}. [<span>[Configuration->@url@]</span>]',
-	'urls_propres_lien' => 'Link p&uacute;blico de acesso',
-	'urls_propres_objet' => 'Aqui s&atilde;o listados os URLs limpos gravados na base, pelos quais os visitantes poder&atilde;o navegar no seu site.',
+	'urls_propres_erreur' => 'Este formato não leva em conta os URLs limpos listados acima.',
+	'urls_propres_format' => 'O formato atual dos URLs é: {{« @format@ »}}. [<span>[Configuration->@url@]</span>]',
+	'urls_propres_lien' => 'Link público de acesso',
+	'urls_propres_objet' => 'Aqui são listados os URLs limpos gravados na base, pelos quais os visitantes poderão navegar no seu site.',
 	'urls_propres_titre' => 'URLS LIMPOS',
 
 	// V
