24be122e00200e3b2bbef135ab7ebfa5580e630b
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-03-24 20:45:44 +0000

    Installation des tables et fonctions pour obtenir le prix HT et le prix TTC du panier, sur l'API Prix.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@45843 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- paniers/base/paniers_installation.php
+++ paniers/base/paniers_installation.php
@@ -1,11 +1,56 @@
 <?php
-/**
- * Plugin Paniers pour Spip 2.0
- * Licence GPL 
- * Auteur Cyril Marion - Ateliers CYM
- * 
- */
 
+// Sécurité
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
+include_spip('inc/meta');
+
+// Installation et mise à jour
+function produits_upgrade($nom_meta_version_base, $version_cible){
+
+	$version_actuelle = '0.0';
+	if (
+		(!isset($GLOBALS['meta'][$nom_meta_version_base]))
+		|| (($version_actuelle = $GLOBALS['meta'][$nom_meta_version_base]) != $version_cible)
+	){
+		
+		if (version_compare($version_actuelle,'0.0','=')){
+			// Création des tables
+			include_spip('base/create');
+			include_spip('base/abstract_sql');
+			creer_base();
+			
+			ecrire_meta($nom_meta_version_base, $version_actuelle=$version_cible, 'non');
+		}
+		
+		/*if (version_compare($version_actuelle,'0.5','<')){
+			include_spip('base/create');
+			include_spip('base/abstract_sql');
+			
+			// Modification de produits
+			sql_alter('');
+						
+			// On change la version
+			echo "Mise à jour du plugin produits en version 0.5<br/>";
+			ecrire_meta($nom_meta_version_base, $version_actuelle=$version_cible, 'non');
+		}*/
+	
+	}
+
+}
+
+// Désinstallation
+function produits_vider_tables($nom_meta_version_base){
+
+	include_spip('base/abstract_sql');
+	
+	// On efface les tables du plugin
+	sql_drop_table('spip_produits');
+	sql_drop_table('spip_produits_liens');
+		
+	// On efface la version entregistrée
+	effacer_meta($nom_meta_version_base);
+
+}
 
 ?>
