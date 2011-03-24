2b103cd0b9ad54877a365eb2490d5fbb3a852c33
piero.wbmstr@gmail.com <piero.wbmstr@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-03-24 17:25:00 +0000

    POPUP : si le premier paramètre passé à la balise est une URL complète (supposée distante donc), on la laisse telle quelle ...
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@45838 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- popup/spipopup/balise/popup.php
+++ popup/spipopup/balise/popup.php
@@ -7,10 +7,9 @@ function balise_POPUP_dist($p) {
 
 function balise_POPUP_dyn($param='', $page=false, $width=false, $height=false, $title='') {
 	spipopup_config();
-	if(!$page) $page = POPUP_SKEL;
-	$page = str_replace('.html','',$page);
 	if(!$width) $width = POPUP_WIDTH;
 	if(!$height) $height = POPUP_HEIGHT;
+	// Cas des objets SPIP
 	if (preg_match(_RACCOURCI_URL, $param, $match)) {
 		if(in_array($match[1], array(
 			'art', 'article', 'breve', 'auteur', 'syndic', 'mot', 'document', 'doc', 'site'
@@ -22,9 +21,21 @@ function balise_POPUP_dyn($param='', $page=false, $width=false, $height=false, $
 			);
 			$param = "id_$type=".$match[2];
 		}
+		if(!$page) $page = POPUP_SKEL;
+		$page = str_replace('.html','',$page);
+		$url = generer_url_public($page, $param);
+		$url_nopopup = generer_url_public($type,$param);
+	// Cas d'une URL complete
+	} elseif(
+		substr($param, 0, strlen('http://'))=='http://' ||
+		substr($param, 0, strlen('https://'))=='https://'
+	) {
+		$url = $url_nopopup = $param;
+	// Sinon, on considère que c'est une erreur, on renvoie le param sans traitement
+	} else {
+		$url = generer_url_public($page, $param);
+		$url_nopopup = generer_url_public($type,$param);
 	}
-	$url = generer_url_public($page, $param);
-	$url_nopopup = generer_url_public($type,$param);
 	$_title = (strlen($title) ? $title." " : '')._T('spipopup:nouvelle_fenetre');
 	echo "$url_nopopup\" onclick=\"_popup_set('$url',$width,$height);return false;\" title=\"".$_title;
 }
