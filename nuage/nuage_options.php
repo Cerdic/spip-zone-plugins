2da4d969de299684fba5c3a79d4c840bc8d3f06b
prigent.yohann@gmail.com <prigent.yohann@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-03-24 23:48:07 +0000

    insert_head_css pour le nuage. L'ancien fichier date de 4 ans, je pense que j'ai bien fais de passer par un vrai insert_head donc...
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@45857 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- nuage/nuage_options.php
+++ nuage/nuage_options.php
@@ -1,10 +1,15 @@
 <?php
-
-function nuage_insert_head($flux) {
-	$css .= "<link rel='stylesheet' href='spip.php?page=nuage_style.css' type='text/css' media='all' />\n";
-	if (strpos($flux,'<head')!==FALSE)
-		return preg_replace('/(<head[^>]*>)/i', "\n\$1".$css, $flux, 1);
-	else 
-		return $flux.$css;
+function nuage_insert_head_css($flux) {
+	$css = "<link rel='stylesheet' href='spip.php?page=nuage_style.css' type='text/css' media='all' />\n";
+	static $done = false;
+	if (!$done) {
+		$done = true;
+		$flux .= "\n".$css;
+	}
+	return $flux;
+}
+function nuage_insert_head($flux){
+	$flux = nuage_insert_head_css($flux); // au cas ou il n'est pas implemente
+	return $flux;
 }
 ?>
