5bdf9f8d925608db96d24f965d684fd8eab6b711
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-03-24 16:42:57 +0000

    Une erreur bidon : oublié d'aller chercher la valeur actuelle.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@45834 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- produits/formulaires/configurer_produits.php
+++ produits/formulaires/configurer_produits.php
@@ -4,13 +4,14 @@
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 function formulaires_configurer_produits_saisies_dist(){
+	include_spip('inc/config');
 	return array(
 		array(
 			'saisie' => 'input',
 			'options' => array(
 				'nom' => 'taxe',
 				'label' => _T('produits:configurer_taxe_defaut_label'),
-				'defaut' => 0,
+				'defaut' => lire_config('produits/taxe', 0),
 			),
 			'verifier' => array(
 				'type' => 'decimal'
