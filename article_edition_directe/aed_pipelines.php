65c4dacfaa38ffd2a72d1977cedeb5f27be0b2f2
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-03-24 06:13:46 +0000

    - enlevé chargement de fonction de taa - enlévé condition config taa
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@45811 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- article_edition_directe/aed_pipelines.php
+++ article_edition_directe/aed_pipelines.php
@@ -12,8 +12,6 @@ function aed_header_prive($flux){
    if ($type=='article'){
 	$id_article= _request('id_article');
 	$row = sql_fetsel("*", "spip_articles", "id_article=$id_article");
-	$barre=charger_fonction('barre_langues','inc');
-	$barre_langue=$barre($id_article);
 	
 	$contexte = array(
 		'icone_retour'=>icone_inline(_T('icone_retour'), $oups, "article-24.gif", "rien.gif",$GLOBALS['spip_lang_left']),
@@ -22,7 +20,6 @@ function aed_header_prive($flux){
 		'new'=>$new?$new:$id_article,
 		'id_rubrique'=>$row['id_rubrique'],
 		'id_secteur'=>$row['id_secteur'],
-		'lier_trad'=>$lier_trad,
 		'config_fonc'=>'articles_edit_config',
 		// passer row si c'est le retablissement d'une version anterieure
 		'row'=> $id_version
@@ -30,7 +27,7 @@ function aed_header_prive($flux){
 		: null
 		);
 	
-	$formulaire=$retour=recuperer_fond('prive/editer/article_mod',$contexte);
+	$formulaire=recuperer_fond('prive/editer/article_mod',$contexte);
 	
 	$flux['data'] =preg_replace('/<div id=\'props\' class=\'tabs-container\'>/',$formulaire.'<div id="props" class="tabs-container">',$flux['data']);
 	
@@ -45,7 +42,7 @@ function aed_affiche_gauche($flux){
 	
 	if(test_plugin_actif('medias') or test_plugin_actif('gest_doc')) $mediatheque='ok';
 	
-	if($exec=='articles' AND $mediatheque AND lire_config('taa/edition_seule') AND autoriser('joindredocument','article',$id)){
+	if($exec=='articles' AND $mediatheque AND autoriser('joindredocument','article',$id)){
 		$flux['data'] .= recuperer_fond('prive/editer/colonne_documents_aed',array('objet'=>'article','id_objet'=>$id));
 		}
 
