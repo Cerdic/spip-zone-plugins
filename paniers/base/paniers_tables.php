7d1e52f83894c02669ffc67d492803564506d264
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-03-24 09:04:19 +0000

    On commence à ranger et remplir cette coquille vide pour en faire un plugin générique de Paniers : un panier est une collection d'objets quelconques ayant un prix.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@45817 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- paniers/base/paniers_tables.php
+++ paniers/base/paniers_tables.php
@@ -1,11 +1,66 @@
 <?php
-/**
- * Plugin Paniers pour Spip 2.0
- * Licence GPL 
- * Auteur Cyril Marion - Ateliers CYM
- * 
- */
 
+// Sécurité
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
+function paniers_declarer_tables_interfaces($interface){
+	// 'spip_' dans l'index de $tables_principales
+	$interface['table_des_tables']['paniers'] = 'paniers';
+	
+	//-- Jointures ----------------------------------------------------
+	$interface['tables_jointures']['spip_auteurs'][]= 'paniers';
+		
+	$interface['table_date']['paniers'] = 'date';
+	
+	return $interface;
+}
+
+function paniers_declarer_tables_principales($tables_principales){
+	//-- Table paniers -----------------------------------------------------------
+	$paniers = array(
+		'id_panier' => 'bigint(21) not null',
+		'id_auteur' => 'bigint(21) not null defaut 0',
+		'cookie' => 'varchar(255) not null default ""',
+		'statut' => 'varchar(10) not null defaut "attente"',
+		'date' => 'datetime not null default "0000-00-00 00:00:00"',
+		'maj' => 'timestamp not null',
+	);
+	
+	$paniers_cles = array(
+		'PRIMARY KEY' => 'id_panier'
+	);
+	
+	$tables_principales['spip_paniers'] = array(
+		'field' => &$paniers,
+		'key' => &$paniers_cles,
+		'join'=> array(
+			'id_panier' => 'id_panier'
+		)
+	);
+
+	return $tables_principales;
+}
+
+function paniers_declarer_tables_auxiliaires($tables_auxiliaires){
+	
+	//-- Table de relations des paniers avec les objets
+	$spip_paniers_liens = array(
+		'id_panier' => 'bigint(21) not null default 0',
+		'id_objet' => 'bigint(21) not null default 0'
+		'objet' => 'varchar(25) not null default ""',
+	);
+	
+	$spip_paniers_liens_cles = array(
+		'PRIMARY KEY' => 'id_panier, id_objet, objet',
+		'KEY id_panier' => 'id_panier'
+	);
+	
+	$tables_auxiliaires['spip_paniers_liens'] = array(
+		'field' => &$spip_paniers_liens,
+		'key' => &$spip_paniers_liens_cles
+	);
+	
+	return $tables_auxiliaires;
+}
 
 ?>
