2405e8ca293433d514dd389718f155633a9dca30
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-03-25 23:06:41 +0000

    langues (ajaxform)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@45939 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ ajaxforms/lang/ajaxform_pt_br.php
@@ -0,0 +1,32 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'aucun_fichier_recu' => 'Você deve selecionar um arquivo.',
+
+	// C
+	'changer_document' => 'Alterar o documento',
+	'confirmer_suppression' => 'A exclusão foi efetuada.',
+
+	// D
+	'document_ajoute' => 'O documento foi incluído.',
+
+	// E
+	'erreur_ajout_document' => 'Ocorreu um erro na inclusão do documento.',
+
+	// L
+	'logo' => 'Ícone',
+	'logo_maj' => 'O ícone foi atualizado.',
+	'logo_survol' => 'Ícone de mouseOver',
+
+	// M
+	'mauvaise_reception' => 'Oops! Erro no recebimento do arquivo. Tente de novo...'
+);
+
+?>
