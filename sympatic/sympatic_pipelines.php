44229055164c56d32f6a6eb430f66078052b7565
brunobergot@gmail.com <brunobergot@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-03-28 11:45:28 +0000

    ne pas lancer les traitements de sympatic à chaque modification d'un auteur mais bien uniquement lors du changement de mail
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@46044 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- sympatic/sympatic_pipelines.php
+++ sympatic/sympatic_pipelines.php
@@ -5,7 +5,8 @@ function sympatic_pre_edition($flux){
 	if (($flux['args']['table']=='spip_auteurs') AND ($flux['args']['action']=='modifier')){
 		$id_auteur = $flux['args']['id_objet'];
 		// s'il y a changement de mail pour un auteur
-		if (($email_nouveau=$flux['data']['email'])!=($email_ancien=sql_getfetsel('email', 'spip_auteurs', 'id_auteur='.intval($id_auteur)))){
+		if (($email_nouveau=$flux['data']['email'])
+			AND $email_nouveau!=($email_ancien=sql_getfetsel('email', 'spip_auteurs', 'id_auteur='.intval($id_auteur)))){
 			spip_log("changement email $email_ancien vers $email_nouveau auteur $id_auteur","sympatic");
 			include_spip('inc/sympatic');
 			$result = sql_select('id_liste','spip_sympatic_abonnes','id_auteur='.intval($id_auteur));
