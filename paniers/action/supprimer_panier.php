e9d82c28e3efe916839a8f2d750f29f330a9ff11
marcimat@rezo.net <marcimat@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-04-01 17:47:38 +0000

    Une action pour supprimer un panier (non utilisée dans le plugin actuellement).
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@46251 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ paniers/action/supprimer_panier.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * Plugin Panier
+ * Distribue sous licence GPL
+ *
+ */
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+function action_supprimer_panier_dist() {
+
+	$securiser_action = charger_fonction('securiser_action', 'inc');
+	$id_panier = $securiser_action();
+
+	// suppression
+	if ($id_panier = intval($id_panier)) {
+		sql_delete('spip_paniers', 'id_panier=' . $id_panier);
+		sql_delete('spip_paniers_liens', 'id_panier=' . $id_panier);
+	}
+
+	// retour
+	include_spip('inc/headers');
+	redirige_par_entete(_request('redirect'));
+
+}
+
+?>
