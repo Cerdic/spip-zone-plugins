313865d126ca74202299d375c7a4035acce8f336
webmaster@geomaticien.com <webmaster@geomaticien.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-04-01 15:15:42 +0000

    Correction du zoom à l'ouverture des cartes sur une rubrique ou un article. Correction mineures dans d'autres composants : cf. log.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@46246 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- acs/models/cat/composants/carte/ecrire/lang/carte_ecrire_fr.php
+++ acs/models/cat/composants/carte/ecrire/lang/carte_ecrire_fr.php
@@ -5,11 +5,12 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 'nom' => 'Carte',
 'description' => 'Carte des rubriques',
-'help' => 'Nécessite le plugin GIS et ses dépendances.<br />
-Affiche sur une carte les dernières rubriques géoréférencées mises à jour. Si celles-ci ont des mots-clés du groupe marker_icon, 
-elles sont affichées avec leurs logos de mot-clé comme pictogrammes personnalisées.
+'help' => 'Nécessite le plugin GIS et ses d&eacute;pendances.<br />
+Affiche sur une carte les dernières rubriques g&eacute;or&eacute;f&eacute;renc&eacute;es mises à jour. Si celles-ci ont des mots-cl&eacute;s du groupe marker_icon, 
+elles sont affich&eacute;es avec leurs logos de mot-cl&eacute; comme pictogrammes personnalis&eacute;es.
 <br /><br />
-Le centrage et le zoom sont ceux pass&eacute;s en param&egrave;tres, ou ceux de l\'aricle ou de la rubrique, ou les valeurs par defaut du compossant carte,',
+Le centrage et le zoom sont ceux pass&eacute;s en param&egrave;tres, ou ceux de l\'article ou de la rubrique, ou les valeurs par defaut du compossant carte.<br /><br />
+Pictogrammes punaises: si oui, les pictogrammes ne sont pas centr&eacute;s mais positionn&eacute;s comme une punaise au dessus du point de r&eacute;f&eacute;rence.',
 
 'ShowLayerSwitcher' => 'Afficher le s&eacute;lecteur de cartes',
 'tooltip' => 'Info-Bulle',
