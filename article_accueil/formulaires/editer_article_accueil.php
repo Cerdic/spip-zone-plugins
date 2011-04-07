f1be95c48e0625b74b6ebe6741ba5da1dbdd25bb
joseph@larmarange.net <joseph@larmarange.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-04-07 14:04:34 +0000

    Être certain que les fonctions SQL sont bien chargées.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@46439 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- article_accueil/formulaires/editer_article_accueil.php
+++ article_accueil/formulaires/editer_article_accueil.php
@@ -32,6 +32,7 @@ function formulaires_editer_article_accueil_charger($id_rubrique){
 function formulaires_editer_article_accueil_traiter($id_rubrique){
 	$update = array();
 	if (!is_null($id_accueil=_request('id_article_accueil'))){
+		include_spip('base/abstract_sql');
 		$update['id_article_accueil'] = $id_accueil;
 		sql_updateq('spip_rubriques',$update,'id_rubrique='.intval($id_rubrique));
 	}
