d982b06a2b30f8ee4ae6611247a155aeb6c9c97c
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-04-08 22:56:41 +0000

    Un tout début de plugin Clients. Le but est de permettre un formulaire d'inscription facile et configurable pour un client, en l'inscrivant à la fois comme utilisateur du site (spip_auteurs) et comme contact (spip_contacts) du plugin Contacts & Organisations.
    Pour ce premier commit ça ne fait rien de bien méchant (mais ça marche) : ça inscrit l'utilisateur en utilisateur SPIP, mais sans donner de pseudo, uniquement avec un champ prénom et nom. Tout est (et sera) en Saisies.
    
    #FORMULAIRE_INSCRIPTION_CLIENT pour l'utiliser.
    
    Il reste donc à enregistrer aussi le contact, à ajouter les autres champs actuels, et à les rendre configurables (actif ou pas, obligatoire, etc).
    D'autres champs sont prévus avec le plugin Coordonnées aussi.
    
    Enfin, un deuxième formulaire permettra au client d'éditer son profil.
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@46509 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ clients/formulaires/inscription_client.php
@@ -0,0 +1,71 @@
+<?php
+
+// Sécurité
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+include_spip('inc/filtres');
+
+function formulaires_inscription_client_saisies_dist(){
+	$mode = tester_config(0);
+	return array(
+		array(
+			'saisie' => 'input',
+			'options' => array(
+				'nom' => 'co__prenom',
+				'label' => _T('contacts:label_prenom'),
+				'obligatoire' => 'oui'
+			)
+		),
+		array(
+			'saisie' => 'input',
+			'options' => array(
+				'nom' => 'co__nom',
+				'label' => _T('contacts:label_nom'),
+				'obligatoire' => 'oui'
+			)
+		),
+		array(
+			'saisie' => 'input',
+			'options' => array(
+				'nom' => 'mail_inscription',
+				'label' => _T('contacts:label_email'),
+				'obligatoire' => 'oui'
+			)
+		)
+	);
+}
+
+function formulaires_inscription_client_charger_dist(){
+	// On récupère le formulaire classique d'inscription
+	$mode = tester_config(0);
+	$inscription_dist = charger_fonction('charger', 'formulaires/inscription');
+	$contexte = $inscription_dist($mode,'');
+	
+	return $contexte;
+}
+
+function formulaires_inscription_client_verifier_dist(){
+	// On crée un faux positif pour le nom car on le construira nous-même plus tard
+	set_request('nom_inscription', 'glop');
+	
+	// On récupère les erreurs du formulaire d'inscription classique
+	$mode = tester_config(0);
+	$inscription_dist = charger_fonction('verifier', 'formulaires/inscription');
+	$erreurs = $inscription_dist($mode,'');
+	
+	return $erreurs;
+}
+
+function formulaires_inscription_client_traiter_dist(){
+	// Le pseudo SPIP est construit
+	set_request('nom_inscription', _request('co__prenom').' '._request('co__nom'));
+	
+	// On récupère les erreurs du formulaire d'inscription classique
+	$mode = tester_config(0);
+	$inscription_dist = charger_fonction('traiter', 'formulaires/inscription');
+	$retours = $inscription_dist($mode,'');
+	
+	return $retours;
+}
+
+?>
