2b87bc6bb5b0a289be9f432f482daa755f023bda
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-04-09 08:52:48 +0000

    Précision sur un item de langue
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@46518 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- plugonet/lang/plugonet_fr.php
+++ plugonet/lang/plugonet_fr.php
@@ -12,7 +12,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 // I
 	'info_generer' => 'Cette option vous permet de g&eacute;n&eacute;rer le nouveau fichier paquet.xml de description d\'un plugin &agrave; partir du fichier plugin.xml existant. Ce formulaire propose la liste des fichiers plugin.xml pr&eacute;sents dans le dossier <code>plugins/</code> de ce site.',
-	'info_choix_pluginxml' => 'Choisissez les fichiers que vous souhaitez convertir.',
+	'info_choix_pluginxml' => 'Choisissez les fichiers que vous souhaitez convertir dans la nouvelle DTD.',
 
 // L
 	'label_choix_pluginxml' => 'Fichiers plugin.xml &agrave; convertir',
