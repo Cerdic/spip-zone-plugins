143b3cb8d9d22859337babb281cf989cbe2dc8e8
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-04-09 21:52:56 +0000

    Le plugin Clients crée maintenant un contact en plus de l'auteur, en remplissant tous les champs qu'il trouve dans le formulaire et qui correspondent à des champs de la table spip_contacts (préfixés de "co__").
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@46529 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- clients/formulaires/inscription_client.php
+++ clients/formulaires/inscription_client.php
@@ -60,10 +60,29 @@ function formulaires_inscription_client_traiter_dist(){
 	// Le pseudo SPIP est construit
 	set_request('nom_inscription', _request('co__prenom').' '._request('co__nom'));
 	
-	// On récupère les erreurs du formulaire d'inscription classique
+	// On active le traitement du formulaire d'inscription classique, donc on crée un nouvel utilisateur
 	$mode = tester_config(0);
 	$inscription_dist = charger_fonction('traiter', 'formulaires/inscription');
 	$retours = $inscription_dist($mode,'');
+	
+	// On récupère l'auteur qu'on vient de créer avec l'email du form
+	if ($id_auteur = sql_getfetsel('id_auteur', 'spip_auteurs', 'email = '.sql_quote(_request('mail_inscription')))){
+		// On crée un contact pour cet utilisateur
+		$definir_contact = charger_fonction('definir_contact', 'action/');
+		$definir_contact("contact/$id_auteur");
+		
+		// On récupère tous les champs d'un contact
+		$champs = sql_fetsel('*', "spip_contacts_liens LEFT JOIN spip_contacts USING(id_contact)", 'id_objet='.$id_auteur." AND objet = 'auteur'");
+		$id_contact = $champs['id_contact'];
+		
+		// Pour chaque champ, on regarde si on l'a modifié dans ce formulaire-là
+		foreach ($champs as $cle=>$null){
+			if (isset($_REQUEST['co__'.$cle])) {
+				$c[$cle] = _request('co__' . $cle);
+			}
+		}
+		modifier_contenu('contact', $id_contact, array('invalideur' => "id='id_contact/$id_contact'"), $c);
+	}
 	
 	return $retours;
 }
