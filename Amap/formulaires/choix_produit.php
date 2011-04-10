affa8886e3d409a15e79f4cc609f9779fcf50dcd
pierrekuhn82@gmail.com <pierrekuhn82@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-04-10 19:21:26 +0000

    Amélioration des formulaire choix afin que si on choisi une saison ou un produit, ils reste afficher
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@46548 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ Amap/formulaires/choix_produit.php
@@ -0,0 +1,8 @@
+<?php
+function formulaires_choix_produit_charger_dist($id_saison) {
+	$valeurs = array(
+		"id_produit" => "$id_produit",
+	);
+	return $valeurs;
+}
+?>
---
+++ Amap/formulaires/choix_saison.php
@@ -0,0 +1,8 @@
+<?php
+function formulaires_choix_saison_charger_dist($id_saison) {
+	$valeurs = array(
+		"id_saison" => "$id_saison",
+	);
+	return $valeurs;
+}
+?>
