14a4e109bf9b6b2be4da8b0e5168b10dead3f2d6
romy@rezo.net <romy@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-04-11 13:13:20 +0000

    rangement
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@46576 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- menu_deroulant/menuder_fonctions.php
+++ menu_deroulant/menuder_fonctions.php
@@ -2,7 +2,7 @@
 
 function menuder_insert_head($flux){
 	$flux .= "<script type='text/javascript' src='".find_in_path('menuder.js')."'></script>\n";
-	$flux .= '<link rel="stylesheet" href="'.find_in_path('menuder.css').'" type="text/css" media="projection, screen" />';
+	$flux .= '<link rel="stylesheet" href="'.find_in_path('css/menuder.css').'" type="text/css" media="projection, screen" />';
 	return $flux;
 }
 ?>
