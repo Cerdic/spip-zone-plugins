11817242dc07f4bed8ba714e2e790a85f52e7e7b
nicolas@hoizey.com <nicolas@hoizey.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-04-12 16:44:39 +0000

    amélioration du vocabulaire
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@46630 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- liens_contenus/trunk/lang/lienscontenus_fr.php
+++ liens_contenus/trunk/lang/lienscontenus_fr.php
@@ -11,20 +11,20 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'aucun_objets_avec_lien_vers_courant' => 'Aucun autre contenu ne contient de lien vers celui-ci.',
 
 	// C
-	'confirmation_depublication' => 'Attention, un contenu publié pointe vers celui-ci, et sera impacté si vous le dépubliez !\\n\\nVoulez-vous vraiment changer le statut ?',
-	'confirmation_publication' => 'Attention, un contenu vers lequel pointe celui-ci n\'est pas publié !\\n\\nVoulez-vous vraiment changer le statut ?',
-	'confirmation_suppression' => 'Attention, un contenu publié pointe vers celui-ci, et sera impacté si vous le supprimez !\\n\\nVoulez-vous vraiment le supprimer ?',
+	'confirmation_depublication' => 'Attention, des liens internes pointent vers ce contenu !\\n\\nSon changement de statut aurait des répercussions sur le fonctionnement du site.\\n\\nVoulez-vous vraiment changer le statut ?',
+	'confirmation_publication' => 'Attention, ce contenu contient un lien pointant vers un contenu qui n\'est pas publié !\\n\\nSon changement de statut aurait des répercussions sur le fonctionnement du site.\\n\\nVoulez-vous vraiment changer le statut ?',
+	'confirmation_suppression' => 'Attention, des liens internes pointent vers ce contenu !\\n\\nSa suppression aurait des répercussions sur le fonctionnement du site.\\n\\nVoulez-vous vraiment le supprimer ?',
 
 	// I
-	'information_element_contenu' => 'Attention, un autre contenu pointe vers celui-ci !',
+	'information_element_contenu' => 'Attention, des liens internes pointent vers ce contenu !',
 
 	// L
 	'legende_liens_faux_objets' => 'Les liens en rouge et barrés indiquent des contenus liés qui n\'existent pas.',
 	'liens_entre_contenus' => 'Liens entre contenus',
 
 	// O
-	'objets_avec_liens_depuis_courant' => 'Ce contenu contient des liens vers ceux-ci :',
-	'objets_avec_liens_vers_courant' => 'Ces contenus contiennent des liens vers celui-ci :',
+	'objets_avec_liens_depuis_courant' => 'Ce contenu contient des liens vers ces autres :',
+	'objets_avec_liens_vers_courant' => 'Ces autres contenus contiennent des liens vers celui-ci :',
 
 	// S
 	'statut_poubelle' => 'A la poubelle',
