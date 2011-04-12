7c97d12a02badafd5661a7b131f8220f368933f8
romy@rezo.net <romy@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-04-12 22:11:18 +0000

    Changement de prefix
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@46641 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- petronille/basecss_pipelines.php
+++ petronille/basecss_pipelines.php
@@ -1,22 +1,16 @@
 <?php
-/*
- * Plugin xxx
- * (c) 2009 xxx
- * Distribue sous licence GPL
- *
- */
 
 /**
- * Inserer le head de petronille avant la premiere css du head
+ * Inserer le head avant la premiere css du head
  * @param string $head
  * @return string
  */
-function petronille_pied_de_biche($head){
+function basecss_pied_de_biche($head){
 	$search = "<link";
 	if (preg_match(",<link\s[^>]*stylesheet,Uims", $head, $match))
 		$search = $match[0];
 	$p = stripos($head, $search);
-	$h = recuperer_fond('inclure/petronille-head',array());
+	$h = recuperer_fond('inclure/basecss-head',array());
 	$h = "\n".trim($h)."\n";
 
 	$head = substr_replace($head, $h, $p, 0);
@@ -29,8 +23,8 @@ function petronille_pied_de_biche($head){
  * @param string $flux
  * @return string
  */
-function petronille_insert_head($flux){
-	$flux .= "<"."?php header(\"X-Spip-Filtre: petronille_pied_de_biche\"); ?".">";
+function basecss_insert_head($flux){
+	$flux .= "<"."?php header(\"X-Spip-Filtre: basecss_pied_de_biche\"); ?".">";
 	return $flux;
 }
 ?>
