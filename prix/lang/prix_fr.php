2ddff3d39c75949978450f0131409550946d66cd
marcimat@rezo.net <marcimat@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-04-14 16:50:26 +0000

    Des labels en langue
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@46762 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- prix/lang/prix_fr.php
+++ prix/lang/prix_fr.php
@@ -4,8 +4,16 @@
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+
+'label_prix_ht' => 'Prix HT',
+'label_prix_ttc' => 'Prix TTC',
+
+'label_taxes' => 'Taxes',
+'label_total_ht' => 'Total HT',
+'label_total_ttc' => 'Total TTC',
+
 'prix_ht' => '@prix@ HT',
-'prix_ttc' => '@prix@ TTC'
+'prix_ttc' => '@prix@ TTC',
 );
 
 ?>
