7f77a2d4178dfc3b2f652dd68738b1a961ef9249
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-04-14 16:34:25 +0000

    langues (zeroclipboard)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@46761 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- zeroclipboard/lang/zeroclipboard_en.php
+++ zeroclipboard/lang/zeroclipboard_en.php
@@ -1,11 +1,17 @@
 <?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.mediaspip.net/spip.php?page=tradlang
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+
 	// L
-	'link_text_copy' => 'Copy',
 	'link_text_copied' => 'Copied',
-	'link_title_copy' => 'Copy to clipboard',
+	'link_text_copy' => 'Copy',
 	'link_title_copied' => 'The content has been copied to clipboard',
+	'link_title_copy' => 'Copy to clipboard'
 );
 
 ?>
--- zeroclipboard/lang/zeroclipboard_fr.php
+++ zeroclipboard/lang/zeroclipboard_fr.php
@@ -1,11 +1,15 @@
 <?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/zeroclipboard/lang
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+
 	// L
+	'link_text_copied' => 'Copié',
 	'link_text_copy' => 'Copier',
-	'link_text_copied' => 'Copi&eacute;',
-	'link_title_copy' => 'Copier dans le presse-papier',
-	'link_title_copied' => 'Le contenu est copi&eacute; dans le presse-papier',
+	'link_title_copied' => 'Le contenu est copié dans le presse-papier',
+	'link_title_copy' => 'Copier dans le presse-papier'
 );
 
 ?>
