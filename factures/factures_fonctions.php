d429c107a2bdefbdd04bf3f3aa52ac39a9abe99a
cyril@cym.fr <cyril@cym.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-04-15 11:39:20 +0000

    on supprime les options, inutiles pour l'instant; on peaufine les 2 boutons pour la gestion du plugin, et la gestion des factures & devis; on ajoute les fonctions manquantes d'après SPIPMINE (tva, ttc, alerte...);
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@46794 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- factures/factures_fonctions.php
+++ factures/factures_fonctions.php
@@ -1,10 +1,54 @@
 <?php
 /**
  * Plugin factures - Facturer avec Spip 2.0
- * Licence GPL (c) 2010
+ * (c) 2009-2011
+ * Licence GPL 
  * par Cyril Marion - Camille Lafitte
  */
 
+/******************************************************************/
+/*   FONCTIONS D'AFFICHAGE (filtres simples)
+/******************************************************************/
+function horaire($horaire) {
+	// affiche un nombre d'heures correctement formatté
+	$horaire = ($horaire && $horaire !=='')? $horaire." h":"";
+	return $horaire;
+}
+function monetaire($montant) {
+	// affiche un montant en euro correctement formatté
+	setlocale(LC_MONETARY, 'fr_FR');
+	$montant = money_format('%i', $montant);
+	$montant = preg_replace("/EUR/", "&euro;", $montant);
+	$montant = preg_replace("/ /", "&nbsp;", $montant);
+	return $montant;
+}
+
+@define('tvaNormale', 1.196);
+@define('tvaReduite', 1.055);
+function tva($montant) {
+	$tva = $montant * (tvaNormale -1);
+	return $tva;
+}
+function ttc($montant) {
+	$ttc = $montant * tvaNormale;
+	return $ttc;
+}
+function prix_ttc($ht, $taux='tvaNormale') {
+	// calcule un montant TTC à partir d'un montant HT
+	setlocale(LC_MONETARY, 'fr_FR');
+	$taux = constant($taux);
+	$ttc = $ht * $taux;
+	$ttc = money_format('%i', $ttc);
+	$ht  = money_format('%i', $ht);
+	$tva = $ht . " HT" . " (" . $ttc . " TTC)";
+	$tva = preg_replace("/EUR/", "&euro;", $tva);
+	return $tva;
+}
+function alerte($nombre) {
+	// affiche un nombre avec la CSS alerte s'il est inférieur ou égal à zéro
+	if ($nombre < 0) $nombre = "<span class='alerte'>$nombre</span>";
+	return $nombre;
+}
 
 
 ?>
--- factures/lang/factures_fr.php
+++ factures/lang/factures_fr.php
@@ -8,10 +8,26 @@
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+	
+	// B
+	'bouton_factures' => 'Factures &amp; Devis',
+	'bouton_configurer_factures' => 'Configurer Factures &amp; Devis',
+	
+	// E
+	'editer_facturation_titre' => 'Editer une facture',
+	'explication_liste_factures' => 'Objectif : un tableau, avec pagination, moteur de recherche, possibilit&eacute; de tri (clic sur colonne) et de filtre (s&eacute;lection d\'un mot cl&eacute;, p.ex. le type de document : facture ou devis)',
+	'explications_page_factures' => 'Sur cette page :<br />
+- liste des factures &amp; devis<br />
+- bouton cr&eacute;er facture',
+	'explications_page_config_factures' => 'essentiellement choix du groupe de mots cl&eacute; d&eacute;finissant les types de factures (facture, devis, proforma, avoir) et les organisations &eacute;mettrices de factures (au moins une, celle de celui qui facture); si pas de groupe pr&eacute;-d&eacute;fini on en cr&eacute;e un; si pas d\'Organisation pr&eacute;-d&eacute;finie, on en cr&eacute;e une.',
 
- // N
-'nom_defaut' => 'Fiche facture par défaut',
+	// N
+	'nom_defaut' => 'Fiche facture par d&eacute;faut',
 
+	// T
+	'titre_page_factures' => 'G&eacute;rer les Factures &amp; Devis',
+	'titre_page_config_factures' => 'Configuration du plugin Factures &amp; Devis',
+	'titre_liste_factures' => 'Liste des factures existantes',
 
 );
 ?>
