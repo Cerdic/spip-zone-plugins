1cbdf95bc79d76feeaaa4833c4c56d46a082ba89
xdjuj@apsulis.com <xdjuj@apsulis.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-04-19 06:50:07 +0000

    Si BANDEAU est activé, il ne faut pas afficher le ajouterBouton à "naviguer" (sinon on se retrouve avec un bouton Import CSV dans le premier niveau de menu de navigation)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@46867 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- csv_import/csvimport_boutons.php
+++ csv_import/csvimport_boutons.php
@@ -21,7 +21,8 @@ function csvimport_ajouterBoutons($boutons_admin) {
 	 * On ajoute le bouton uniquement pour les admins
 	 */
 	if ($GLOBALS['connect_statut'] == "0minirezo" && $GLOBALS["connect_toutes_rubriques"] AND 
-	(!isset($GLOBALS['meta']["activer_csvimport"]) OR $GLOBALS['meta']["activer_csvimport"]!="non")) {
+	(!isset($GLOBALS['meta']["activer_csvimport"]) OR $GLOBALS['meta']["activer_csvimport"]!="non") AND
+	(!test_plugin_actif('bando'))) {
 
 		// on voit le bouton dans la barre "naviguer"
 		$boutons_admin['naviguer']->sousmenu["csvimport_tous"]= new Bouton(
