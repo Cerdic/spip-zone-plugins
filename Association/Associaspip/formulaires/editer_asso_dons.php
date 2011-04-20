4a735ae74b43a6d48392ddba80f045b959d25e37
esj@rezo.net <esj@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-04-20 09:11:23 +0000

    [source:_plugins_/Association/Associaspip Associaspip] Ajout de 2011 dans le copyright (aurait dû être fait avec le précédent). Dépot obtenu par: {{{ for i in $(grep -l 'Copyr.*Sa.*V' */*p) do sed "s/2010\(.*\)     (V2/2010-2011\1 (V2/" $i > x mv x $i done }}}
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@46893 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- Association/Associaspip/formulaires/editer_asso_dons.php
+++ Association/Associaspip/formulaires/editer_asso_dons.php
@@ -8,7 +8,7 @@ include_spip('inc/editer');
  *  Associaspip, extension de SPIP pour gestion d'associations             *
  *                                                                         *
  *  Copyright (c) 2007 Bernard Blazin & Francois de Montlivault (V1)       *
- *  Copyright (c) 2010 Emmanuel Saint-James & Jeannot Lapin     (V2)       *
+ *  Copyright (c) 2010-2011 Emmanuel Saint-James & Jeannot Lapin (V2)       *
  *                                                                         *
  *  Ce programme est un logiciel libre distribue sous licence GNU/GPL.     *
  *  Pour plus de details voir le fichier COPYING.txt ou l'aide en ligne.   *
