a5a82942eb3437c8849b0fdf11dda721d8ff59c5
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-04-21 19:04:35 +0000

    Pas besoin de ce exec c'est salvatore qui fait tout le boulot d'import / export
    Des autorisations qui marchent
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@46956 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- trad-lang/trad-lang_spip2/tradlang_autoriser.php
+++ trad-lang/trad-lang_spip2/tradlang_autoriser.php
@@ -135,7 +135,7 @@ function autoriser_tradlang_configurer_dist($faire, $type, $id, $qui, $opt){
 function autoriser_tradlang_modifier_dist($faire, $type, $id, $qui, $opt){
 	$autorise = false;
 	$utiliser_defaut = true;
-
+	spip_log($opt,'test');
 	if(intval($id) > 0){		
 		$infos_chaine = sql_fetsel('*','spip_tradlang','id_tradlang='.$id);
 		$lang_mere = sql_getfetsel('lang_mere','spip_tradlang_modules','module='.sql_quote($infos_chaine['module']));
@@ -267,13 +267,20 @@ function autoriser_tradlang_voir_dist($faire, $type, $id, $qui, $opt){
  * @param unknown_type $qui
  * @param unknown_type $opt
  */
-function autoriser_tradlang_module_creer_dist($faire, $type, $id, $qui, $opt){
-	spip_log('on passe là?','tradlang');
+function autoriser_tradlangmodule_creer_dist($faire, $type, $id, $qui, $opt){
 	return false;
 }
 
+/**
+ * Autorisation à créer une chaine de langue
+ * Renvoit false
+ * @param unknown_type $faire
+ * @param unknown_type $type
+ * @param unknown_type $id
+ * @param unknown_type $qui
+ * @param unknown_type $opt
+ */
 function autoriser_tradlang_creer_dist($faire, $type, $id, $qui, $opt){
-	spip_log('on passe là?','tradlang');
 	return false;
 }
 
