09b212a5c2844f3527fa7f650c11a8c9b76b6785
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-04-21 14:17:52 +0000

    une fonction de révision spécifique pour le crontroleur du crayon pour permettre de modifier le champ "statut"
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@46938 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- trad-lang/trad-lang_spip2/tradlang_options.php
+++ trad-lang/trad-lang_spip2/tradlang_options.php
@@ -17,4 +17,21 @@ function revision_tradlang($id_tradlang,$c=false){
 		),
 		$c);
 }
+
+function str_statut_revision($id_tradlang,$c=false){
+	if(isset($c['statut'])){
+		include_spip('action/editer_tradlang');
+		$statut['statut'] = $c['statut'];
+	}
+	
+	if(is_array($statut)){
+		instituer_tradlang($id_tradlang, $statut);
+	}
+	return modifier_contenu('tradlang', $id_tradlang,
+		array(
+			'invalideur' => $invalideur,
+			'date_modif' => 'ts'
+		),
+		$c);
+}
 ?>
