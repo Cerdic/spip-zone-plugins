e9e653c32f28021b4584ae4cbbc897c9054c6786
romy@rezo.net <romy@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-04-22 22:45:50 +0000

    Inutile d'imprimer les liens d'évitement
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@47011 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ skiplink/skiplink_pipelines.php
@@ -0,0 +1,8 @@
+<?php
+
+function skiplink_insert_head_css($flux) {
+	$flux .= '<link rel="stylesheet" type="text/css" href="'.find_in_path('css/skiplink.css').'" media="all" />'."\n";
+	return $flux;
+}
+
+?>
