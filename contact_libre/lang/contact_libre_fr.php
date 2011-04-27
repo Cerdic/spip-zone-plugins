a9b1c75f06282a9e5cda1c0e431fadab1e4dca41
brunobergot@gmail.com <brunobergot@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-04-27 19:21:19 +0000

    on se branche sur le plugin insérer modèles
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@47138 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ contact_libre/lang/contact_libre_fr.php
@@ -0,0 +1,13 @@
+<?php
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	'label_email' => 'Adresse email',
+	
+	'modele_nom_formulaire' => 'Formulaire de contact libre',
+	
+);
+
+?>
