53650807655a16b98a480671ab8b4c99527d92a7
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-04-27 18:34:22 +0000

    On améliore la découpe de l'aide en ligne
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@47134 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- plugonet/plugonet_options.php
+++ plugonet/plugonet_options.php
@@ -1,4 +1,24 @@
 <?php
 // Le serveur d'aide en ligne pour ce plugin
 $GLOBALS['help_server'][] ='http://blog.smellup.net/aide/';
+$GLOBALS['index_aide_plugonet'] = array(
+	'paqxmlgen',
+	'paqxmlpaquet',
+	'paqxmlnom',
+	'paqxmldesc',
+	'paqxmlaut',
+	'paqxmlcred',
+	'paqxmlcopy',
+	'paqxmllic',
+	'paqxmltrad',
+	'paqxmlbout',
+	'paqxmlnec',
+	'paqxmlpipe',
+	'paqxmllib',
+	'paqxmlproc',
+	'paqxmlpath',
+	'paqxmlspip',
+	'paqxmlfoi'
+);
+
 ?>
