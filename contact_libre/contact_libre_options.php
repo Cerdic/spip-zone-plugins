ff6e8b5be65269682a8a9028c778ecdee745ade1
brunobergot@gmail.com <brunobergot@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-04-27 13:02:14 +0000

    et hop un petit formulaire de contact libre auquel on passe directement une adresse mail
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@47115 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ contact_libre/contact_libre_options.php
@@ -0,0 +1,6 @@
+<?php
+
+// brancher le plugin sur nospam
+$GLOBALS['formulaires_no_spam'][] = 'contact_libre';
+
+?>
---
+++ contact_libre/formulaires/contact_libre.php
@@ -0,0 +1,82 @@
+<?php
+
+function formulaires_contact_libre_charger_dist($adresse, $url=''){
+	include_spip('inc/texte');
+	$valeurs = array('sujet_message'=>'','texte_message'=>'','email_message'=>'');
+	
+	// id du formulaire (pour en avoir plusieurs sur une meme page)
+	$valeurs['id'] = rand(1, 100);
+	
+	return $valeurs;
+}
+
+function formulaires_contact_libre_verifier_dist($adresse, $url=''){
+	$erreurs = array();
+	include_spip('inc/filtres');
+	include_spip('inc/texte');
+	
+	if (!$adres = _request('email_message'))
+		$erreurs['email_message'] = _T("info_obligatoire");
+	elseif(!email_valide($adres))
+		$erreurs['email_message'] = _T('form_prop_indiquer_email');
+
+	if (!$sujet=_request('sujet_message'))
+		$erreurs['sujet_message'] = _T("info_obligatoire");
+	elseif(!(strlen($sujet)>3))
+		$erreurs['sujet_message'] = _T('forum_attention_trois_caracteres');
+
+	if (!$texte=_request('texte_message'))
+		$erreurs['texte_message'] = _T("info_obligatoire");
+	
+	if(_request("nobot"))
+		$erreurs['nobot'] = true;
+	
+	// on traite les spams
+	if (include_spip('inc/nospam')) {
+		$caracteres = compter_caracteres_utiles($texte);
+		// moins de 10 caracteres sans les liens = spam !
+		if ($caracteres < 10){
+			$erreurs['texte_message'] = _T('forum_attention_dix_caracteres');
+		}
+		// on analyse le sujet
+		$infos_sujet = analyser_spams($sujet);
+		// si un lien dans le sujet = spam !
+		if ($infos_sujet['nombre_liens'] > 0)
+			$erreurs['sujet_message'] = _T('nospam:erreur_spam');
+
+		// on analyse le texte
+		$infos_texte = analyser_spams($texte);
+		if ($infos_texte['nombre_liens'] > 0) {
+			// si un lien a un titre de moins de 3 caracteres = spam !
+			if ($infos_texte['caracteres_texte_lien_min'] < 3) {
+				$erreurs['texte_message'] = _T('nospam:erreur_spam');
+			}
+			// si le texte contient plus de trois lien = spam !
+			if ($infos_texte['nombre_liens'] >= 3)
+				$erreurs['texte_message'] = _T('nospam:erreur_spam');
+		}
+	}
+
+	if (!_request('confirmer') AND !count($erreurs))
+		$erreurs['previsu']=' ';
+	return $erreurs;
+}
+
+function formulaires_contact_libre_traiter_dist($adresse, $url=''){
+	
+	$adres = _request('email_message');
+	$sujet=_request('sujet_message');
+	$texte=_request('texte_message');
+	
+	$texte .= "\n\n-- "._T('envoi_via_le_site')." ".supprimer_tags(extraire_multi($GLOBALS['meta']['nom_site']))." (".$GLOBALS['meta']['adresse_site']."/) --\n";
+	if($url)
+		$texte .= "\n\n-- Depuis la page : ".supprimer_tags($url)." --\n";
+	$envoyer_mail = charger_fonction('envoyer_mail','inc');
+	$envoyer_mail($adresse, $sujet, $texte, $adres,
+				"X-Originating-IP: ".$GLOBALS['ip']);
+	$message = _T("form_prop_message_envoye");
+
+	return array('message_ok'=>$message);
+}
+
+?>
