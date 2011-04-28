48038a0e7f6125e0014d4a5a6884dd4fa54f1f56
squonk@anarchic-teapot.net <squonk@anarchic-teapot.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-04-28 21:10:06 +0000

    langues (accesrestreint)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@47186 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- acces_restreint/lang/accesrestreint_en.php
+++ acces_restreint/lang/accesrestreint_en.php
@@ -8,7 +8,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
-	'ajouter_droits_auteur' => 'Give me instant acces to this area',
+	'ajouter_droits_auteur' => 'Give me access to this area',
 	'aucune_zone' => 'No zones',
 	'auteurs' => 'authors',
 
@@ -17,46 +17,46 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// C
 	'colonne_id' => '#',
-	'confirmer_ajouter_auteurs' => 'Are you sure you want to add this author to the zone?',
-	'confirmer_retirer_auteur_zone' => 'Are you sure you wish to remove this author from this zone?',
-	'confirmer_retirer_auteurs' => 'Are you sure you wish to remove all authors from this zone?',
-	'confirmer_supprimer_zone' => 'Are you sure you want to delete this zone?',
+	'confirmer_ajouter_auteurs' => 'Are you sure you want to add this author to the area?',
+	'confirmer_retirer_auteur_zone' => 'Are you sure you wish to remove this author from this area?',
+	'confirmer_retirer_auteurs' => 'Are you sure you wish to remove all authors from this area?',
+	'confirmer_supprimer_zone' => 'Are you sure you want to delete this area?',
 	'creer_zone' => 'Create a new area',
 
 	// D
 	'descriptif' => 'Description',
 
 	// I
-	'icone_menu_config' => 'restricted access',
+	'icone_menu_config' => 'Restricted access',
 	'icone_supprimer_zone' => 'Delete this area',
-	'info_acces_restreint' => 'Access to this page is restricted. Please identify yourself to continue',
+	'info_acces_restreint' => 'Access to this page is restricted. Please log in to continue',
 	'info_ajouter_auteur' => 'Add this author',
 	'info_ajouter_auteurs' => 'Add all authors',
 	'info_ajouter_zones' => 'Add all areas',
-	'info_aucun_acces' => 'No access authorised',
-	'info_aucun_auteur' => 'No author in the zone',
-	'info_aucune_zone' => 'No zone',
+	'info_aucun_acces' => 'No access allowed',
+	'info_aucun_auteur' => 'No authors in the area',
+	'info_aucune_zone' => 'No areas',
 	'info_auteurs_lies_zone' => 'Authors with access to this area',
-	'info_lien_action_proteger' => 'Prot&eacute;ger l\'acc&egrave;s &agrave; cette rubrique.', # NEW
+	'info_lien_action_proteger' => 'Restrict access to this section',
 	'info_page' => 'This page allows you to create restricted areas in your site',
 	'info_retirer_auteurs' => 'Remove all authors',
-	'info_retirer_zone' => 'Move out of area',
-	'info_retirer_zones' => 'Move out of all areas',
-	'info_rubrique_dans_zone' => 'Cette rubrique fait partie de la zone :', # NEW
-	'info_rubrique_dans_zones' => 'Cette rubrique fait partie des zones :', # NEW
+	'info_retirer_zone' => 'Remove from area',
+	'info_retirer_zones' => 'Remove from all areas',
+	'info_rubrique_dans_zone' => 'This section is in area:',
+	'info_rubrique_dans_zones' => 'This section is in areas:',
 
 	// M
-	'modifier_zone' => 'Modify the zone',
+	'modifier_zone' => 'Edit the area',
 
 	// P
-	'page_zones_acces' => 'restricted access',
+	'page_zones_acces' => 'Restricted Access',
 	'par_titre' => 'By title',
 	'privee' => 'Private',
 	'publique' => 'Public',
 
 	// R
 	'rubriques' => 'sections',
-	'rubriques_zones_acces' => 'Restricted area\\\'s sections',
+	'rubriques_zones_acces' => 'Sections in this area',
 
 	// S
 	'selectionner_une_zone' => 'Select a restricted area',
@@ -64,10 +64,10 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// T
 	'titre' => 'Title',
 	'titre_ajouter_zone' => 'Add to a restricted area',
-	'titre_boite_protegee_non' => 'Acc&egrave;s non prot&eacute;g&eacute;', # NEW
-	'titre_boite_protegee_oui' => 'Acc&egrave;s prot&eacute;g&eacute;', # NEW
-	'titre_cadre_modifier_zone' => 'Modify a zone',
-	'titre_page_config' => 'Configure the access rights',
+	'titre_boite_protegee_non' => 'Unrestricted access',
+	'titre_boite_protegee_oui' => 'Restricted access',
+	'titre_cadre_modifier_zone' => 'Edit an area',
+	'titre_page_config' => 'Configure access rights',
 	'titre_table' => 'All restricted areas',
 	'titre_zones_acces' => 'Restricted areas',
 	'toutes' => 'All',
@@ -76,7 +76,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'voir_toutes' => 'Show all areas',
 
 	// Z
-	'zone_numero' => 'ZONE NUMBER:',
+	'zone_numero' => 'AREA NUMBER:',
 	'zone_restreinte_espace_prive' => 'Restrict access to this area in the private area',
 	'zone_restreinte_publique' => 'Restrict access to this area in the public site'
 );
