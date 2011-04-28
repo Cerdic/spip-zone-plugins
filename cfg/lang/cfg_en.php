b41f9a48a2759f59bcb2986e7e6a5c920ecf742b
squonk@anarchic-teapot.net <squonk@anarchic-teapot.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-04-28 21:10:07 +0000

    langues (cfg)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@47187 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- cfg/lang/cfg_en.php
+++ cfg/lang/cfg_en.php
@@ -20,22 +20,22 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'configuration_modules' => 'Module configuration',
 
 	// E
-	'erreur_copie_fichier' => 'It is impossible to copy the file @fichier@ to its final destination',
+	'erreur_copie_fichier' => 'It is impossible to copy the file @fichier@ to the target directory',
 	'erreur_enregistrement' => 'An error occurred while saving <b>@nom@</b>',
-	'erreur_lecture' => 'Impossible to read @nom@',
-	'erreur_open_w_fichier' => 'Impossible to open @fichier@ for modification',
+	'erreur_lecture' => 'Cannot read @nom@',
+	'erreur_open_w_fichier' => 'Impossible to write to @fichier@',
 	'erreur_suppression' => 'An error occurred while deleting <b>@nom@</b>',
 	'erreur_suppression_fichier' => 'The file @fichier@ cannot be deleted.',
 	'erreur_type_id' => 'The field @champ@ must begin with a letter or an underscore',
 	'erreur_type_idnum' => 'The field @champ@ must be numeric',
-	'erreur_type_pwd' => 'The field @champ@ must comprise more than 5 characters',
+	'erreur_type_pwd' => 'The field @champ@ must contain more than 5 characters',
 
 	// I
 	'id_manquant' => 'Missing identifier',
 	'installation_librairies' => 'Download libraries',
 	'installation_liste_libs' => 'List of libraries',
 	'installer_dossier_lib' => 'You must create a directory named @dir@ with write permissions at the root of your SPIP site.',
-	'installer_lib_192' => 'To install a library, unzip the zip file manually and copy its contents to the @dir@ directory.',
+	'installer_lib_192' => 'To install a library, unzip the archive manually and copy its contents to the @dir@ directory.',
 
 	// L
 	'label_activer' => 'Activate',
