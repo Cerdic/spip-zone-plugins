83ef0448923a1fd5b166768411e12cbb252d3787
pierrekuhn82@gmail.com <pierrekuhn82@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-05-01 17:11:50 +0000

    Création de rubrique et sous rubrique en vu d'avoir des article exemple. Correction sur la page panier pour avoir le bon formulaire. Rajout d'un champ extra sur la table auteur: type panier.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@47300 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- Amap/base/amap_installation.php
+++ Amap/base/amap_installation.php
@@ -11,7 +11,7 @@ include_spip('base/create');
 include_spip('base/amap_tables');
 
 function amap_upgrade($nom_meta_version_base, $version_cible){
-	$current_version = 0.3;
+	$current_version = 0.0;
 		if ((!isset($GLOBALS['meta'][$nom_meta_version_base])) || (($current_version = $GLOBALS['meta'][$nom_meta_version_base]) != $version_cible)){
 		if (version_compare($current_version,'0.3','<=')){
 			// Creation des tables
@@ -24,7 +24,7 @@ function amap_upgrade($nom_meta_version_base, $version_cible){
 			// Creation des champs extras
 			amap_declarer_champs_extras();
 			sql_alter("TABLE spip_auteurs ADD adhesion text NULL");
-			spip_log("Amap s'installe V0.0", "amap_installation");
+			spip_log("Amap s'installe V0.3", "amap_installation");
 			ecrire_meta($nom_meta_version_base, $current_version=$version_cible, 'non');
 			}
 		if (version_compare($current_version,'0.4','<')){
@@ -32,6 +32,16 @@ function amap_upgrade($nom_meta_version_base, $version_cible){
 			sql_drop_table("spip_amap_banques");
 			sql_drop_table("spip_amap_paniers");
 			spip_log("Suppression des table banques et paniers V0.4", "amap_installation");
+			}
+		if (version_compare($current_version,'0.5','<')) {
+			create_rubrique("000. Agenda de la saison", "0");
+			$id_rubrique = id_rubrique("000. Agenda de la saison");
+			if ($id_rubrique >0) {
+				create_rubrique("001. Distribution", $id_rubrique);
+				create_rubrique("002. Événements", $id_rubrique);
+			}
+			sql_alter("TABLE spip_auteurs ADD type_panier text NULL");
+			spip_log("Création de rubrique et champ extra type_panier V0.5", "amap_installation");                
 			ecrire_meta($nom_meta_version_base, $current_version=$version_cible, 'non');
 			}
 		}
@@ -57,6 +67,8 @@ function amap_vider_tables($nom_meta_version_base){
 	sql_drop_table('spip_paniers');
 	//suppression des champs supplementaire
 	$champs = amap_declarer_champs_extras();
+	//supprimer les mots clef
+	vider_groupe("_Amap_config");
 	effacer_meta($nom_meta_version_base);
 }
 ?>
--- Amap/base/amap_tables.php
+++ Amap/base/amap_tables.php
@@ -184,6 +184,78 @@ function remplacer_mot($id_mot, $descriptif, $texte, $id_groupe, $groupe) {
 }
 
 
+// fonction qui permet de trouver si une rubrique existe à partir du titre
+function find_rubrique($titre) {
+        $titre = addslashes($titre);
+        $count = sql_countsel(
+                "spip_rubriques", 
+                "titre = '$titre'"
+        );
+        return $count;
+}
+
+//fonction qui permet de trouver l'id d'une rubrique à partir du titre
+function id_rubrique($titre) {
+        $result = sql_fetsel(
+                "id_rubrique", 
+                "spip_rubriques", 
+                "titre='$titre'"
+        );
+        $resultat = $result['id_rubrique'];
+        spip_log("1. (id_rubrique) recherche de l'id_rubrique de $titre = $resultat", "amap_installation");
+        return $resultat;
+}
+
+// fonction qui permet de renommer une rubrique à partir du titre
+function rename_rubrique($titre, $nouveau_titre) {
+        $id_rubrique = id_rubrique($titre);
+        if ($id_rubrique) {
+                sql_updateq(
+                        "spip_rubriques", array(
+                                "titre" => $nouveau_titre
+                        ), "id_rubrique=$id_rubrique"
+                );
+                spip_log("rename_rubrique) renommage de $titre en $nouveau_titre", "amap_installation");
+        }
+        return true;
+}
+       
+//fonction qui permet de créer une rubrique
+function create_rubrique($titre, $id_parent='0', $descriptif='') {
+        $id_rubrique = find_rubrique($titre);
+        if ($id_rubrique == 0) {
+                $id_rubrique = sql_insertq(
+                        "spip_rubriques", array(
+                                "titre" => $titre,
+                                "id_parent" => $id_parent,
+                                "descriptif" => $descriptif
+                        )
+                );
+                sql_updateq(
+                        "spip_rubriques", array(
+                                "id_secteur" => $id_rubrique
+                        ), "id_rubrique=$id_rubrique"
+                );
+                spip_log("1. (create_rubrique) rubrique cree : id = $id_rubrique, titre = $titre", "amap_installation");
+        }
+        else if ($id_rubrique > 0) {
+                $id_rubrique = id_rubrique($titre);
+                remplacer_rubrique($id_rubrique, $id_parent, $descriptif);
+        }
+        return $id_rubrique;
+}
+
+//fonction qui mets à jour une rubrique
+function remplacer_rubrique($id_rubrique, $id_parent, $descriptif) {
+	sql_updateq(
+		"spip_rubriques", array(
+			"id_parent" => $id_parent,
+			"descriptif" => $descriptif
+		), "id_rubrique=$id_rubrique"
+	);
+	return true;
+}
+
 //tables du plugins amap
 function amap_declarer_tables_interfaces($interface){
 	//-- Alias
@@ -475,6 +547,17 @@ function amap_declarer_champs_extras($champs = array()){
                 'type' => 'input', // type de saisie
                 'sql' => "bigint NULL", // declaration sql
         ));
+		$champs[] = new ChampExtra(array(
+				'table' => 'auteurs', // sur quelle table ?
+				'champ' => 'type_panier', // nom sql
+				'label' => 'amap:type_panier', // chaine de langue 'prefix:cle'
+				'type' => 'menu-radio', // type de saisie
+				'enum' => array(
+					"petit" => "Petit",
+					"grand" => "Grand",
+				),
+				'sql' => "text NOT NULL DEFAULT ''", // declaration sql
+		));
         return $champs;
 }
 ?>
