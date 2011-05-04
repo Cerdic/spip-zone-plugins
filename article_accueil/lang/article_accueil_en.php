1f2738695e1b2d7f029d505e76944ad376fdb037
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-05-04 12:24:59 +0000

    Version anglaise rapide
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@47402 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ article_accueil/lang/article_accueil_en.php
@@ -0,0 +1,16 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	// A
+	'article_accueil' => 'Home article',
+	'aucun_article_accueil' => 'No article',
+	
+	// L
+	'label_id_article_accueil' => 'Home article',
+	
+	// R
+	'rubrique_article_en_accueil' => 'Home article&nbsp;:',
+);
+?>
