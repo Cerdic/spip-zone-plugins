06bfe8aedaedac6f8bafdc279196f357c1a62b55
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-05-05 15:15:03 +0000

    Nouvelle version de menu langue liens uniquement basé sur #MENU_LANG... à tester...
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@47470 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ menu_langues_liens/lang/mll_en.php
@@ -0,0 +1,19 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/licence/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cfg_titre_mll' => 'Language menu as links',
+	
+	// E
+	'explication_langues_invisibles' => 'Allows to remove some languages ​​from the menu',
+
+	// L
+	'label_langues_invisibles' => 'Invisible languages in the​​ menu',
+
+);
+
+?>
---
+++ menu_langues_liens/lang/mll_fr.php
@@ -0,0 +1,19 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/licence/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cfg_titre_mll' => 'Menu de langues sous forme de liens',
+	
+	// E
+	'explication_langues_invisibles' => 'Permet d\'enlever certaines langues du menu',
+
+	// L
+	'label_langues_invisibles' => 'Langues invisibles du menu',
+
+);
+
+?>
