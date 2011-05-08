7d5a21e1ca7994d4f6e8fbf1a3fd408f7cbc5119
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-05-08 14:55:37 +0000

    Ampliation du plugin aed, inclure d'autres objets spip. Sélectionable depuis cfg.
    - Etat actuel, sélectionable l'objet article
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@47552 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ edition_directe/edition_directe_pipelines.php
@@ -0,0 +1,52 @@
+<?php
+
+function edition_directe_header_prive($flux){
+    	$flux .= '<link rel="stylesheet" href="'.generer_url_public("edition_directe_styles").'" type="text/css" media="all" />';
+	return $flux;	
+ }
+
+/*Modifie l'affichage de l'article dans l'espace interne*/
+ function edition_directe_afficher_fiche_objet($flux){
+    $type = $flux['args']['type'];
+
+   if ($type=='article'AND lire_config('edition_directe/article')){
+	$id_article= _request('id_article');
+	$row = sql_fetsel("*", "spip_articles", "id_article=$id_article");
+	
+	$contexte = array(
+		'icone_retour'=>icone_inline(_T('icone_retour'), $oups, "article-24.gif", "rien.gif",$GLOBALS['spip_lang_left']),
+		'redirect'=>generer_url_ecrire("articles"),
+		'titre'=>$row['titre'],
+		'new'=>$new?$new:$id_article,
+		'id_rubrique'=>$row['id_rubrique'],
+		'id_secteur'=>$row['id_secteur'],
+		'config_fonc'=>'articles_edit_config',
+		// passer row si c'est le retablissement d'une version anterieure
+		'row'=> $id_version
+		? $row
+		: null
+		);
+	
+	$formulaire=recuperer_fond('prive/editer/article_mod',$contexte);
+	
+	$flux['data'] =preg_replace('/<div id=\'props\' class=\'tabs-container\'>/',$formulaire.'<div id="props" class="tabs-container">',$flux['data']);
+	
+	}
+return $flux;
+}
+
+// affichage du formulaire de téléchargement des docs
+function edition_directes_affiche_gauche($flux){
+	$exec= $flux['args']['exec'];
+	$id = $flux['args']['id_article'];
+	
+	if(test_plugin_actif('medias') or test_plugin_actif('gest_doc')) $mediatheque='ok';
+	
+	if($exec=='articles' AND $mediatheque AND autoriser('joindredocument','article',$id) AND lire_config('edition_directe/article')){
+		$flux['data'] .= recuperer_fond('prive/editer/colonne_documents_aed',array('objet'=>'article','id_objet'=>$id));
+		}
+
+return $flux;
+}
+
+?>
---
+++ edition_directe/lang/edir_fr.php
@@ -0,0 +1,34 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/traduction_articles_autrement/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'activer_edition_directe'=>'Activer l\'édition directe pour :',
+	'article'=>'Articles',
+	'auteur'=>'Auteurs',	
+		
+	// B
+	'breve'=>'Brèves',	
+			
+	// G
+	'groupe_mots'=>'Groupes Mots',
+					
+	// M
+	'mot'=>'Mots',					
+		
+	// R
+	'rubrique'=>'Rubriques',
+	
+	// S
+	'site'=>'Sites',			
+			
+	// T
+	'titre_plugin' => 'Edition directe',
+	
+
+);
+
+?>
