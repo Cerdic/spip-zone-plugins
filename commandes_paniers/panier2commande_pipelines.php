45fe9fb7c941081f58ac2bdec9122b6a72e13bfa
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-05-08 00:53:43 +0000

    Un plugin qui, lorsqu'il est activé, remplit automatiquement le contenu d'une commande "en cours" lors de sa création, avec le contenu du panier du même visiteur.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@47547 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ commandes_paniers/panier2commande_pipelines.php
@@ -0,0 +1,51 @@
+<?php
+
+// Sécurité
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+function panier2commande_post_insertion($flux){
+	// Après insertion d'une commande "encours" et s'il y a un panier en cours
+	if (
+		$flux['args']['table'] == 'spip_commandes'
+		and ($id_commande = intval($flux['args']['id_objet'])) > 0
+		and $flux['data']['statut'] == 'encours'
+		and include_spip('inc/paniers')
+		and $id_panier = paniers_id_panier_encours()
+	){
+		// On récupère le contenu du panier
+		$panier = sql_allfetsel(
+			'*',
+			'spip_paniers_liens',
+			'id_panier = '.$id_panier
+		);
+		
+		// Pour chaque élément du panier, on va remplir la commande
+		if ($panier and is_array($panier)){
+			include_spip('spip_bonux_fonctions');
+			$fonction_prix = charger_fonction('prix', 'inc/');
+			$fonction_prix_ht = charger_fonction('ht', 'inc/prix');
+			foreach($panier as $emplette){
+				$prix_ht = $fonction_prix_ht($emplette['objet'], $emplette['id_objet']);
+				$prix = $fonction_prix($emplette['objet'], $emplette['id_objet']);
+				$taxe = round(($prix - $prix_ht) / $prix_ht, 3);
+				sql_insertq(
+					'spip_commandes_details',
+					array(
+						'id_commande' => $id_commande,
+						'statut' => 'encours',
+						'objet' => $emplette['objet'],
+						'id_objet' => $emplette['id_objet'],
+						'descriptif' => generer_info_entite($emplette['id_objet'], $emplette['objet'], 'titre', '*'),
+						'quantite' => $emplette['quantite'],
+						'prix_unitaire_ht' => $prix_ht,
+						'taxe' => $taxe
+					)
+				);
+			}
+		}
+	}
+	
+	return $flux;
+}
+
+?>
