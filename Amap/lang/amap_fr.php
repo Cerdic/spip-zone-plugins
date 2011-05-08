c85e97056f02629d2dee6e352fd6801791dbc0e7
pierrekuhn82@gmail.com <pierrekuhn82@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-05-08 14:38:38 +0000

    Fichiers inutiliser à ce jour et mise à jour du fichier de langue fr
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@47551 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- Amap/lang/amap_fr.php
+++ Amap/lang/amap_fr.php
@@ -8,8 +8,6 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'action_modifier' => 'Modifier',
 	'action_supprimer' => 'Supprimer',
 	'adhesion' => 'Adhésion (ex:2008)',
-	'adresse_email' => 'Adresse email',
-	'agenda' => 'Agenda',
 	'annuaire'  => 'Annuaire',
 	'annuaire_enregistre'  => 'La personne a bien été enregistrée',
 	'annuaire_enregistres'  => 'Liste des personnes enregistrées',
@@ -17,48 +15,16 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'attention_modifications' => 'Les modifications effectuées dans ces pages influent notablement sur le fonctionnement du module AMAP.',
     	
 	// B
-	'banques' => 'Banques',
-	'banques_enregistrement' => 'Enregistrement d\'une banque supplémentaire',
-	'banques_enregistres' => 'Liste des banques enregistrées',
 
 	// C
-	'choisir_produit' => 'Choisir un produit',
-	'choisir_saison' => 'Choisir une saison',
-	'choisir' => 'Choisir',
-	'choix' => 'Choix',
-	'choix_produit' => 'Choix du produit',
-	'choix_saison' => 'Choix de la saison',
-	'cle_primaire_de'  => 'est la clé primaire de',
 	'configuration'  => 'Configuration',
 	'configuration_annuaire'  => 'Configuration de l\'annuaire',
-	'configuration_distri_evenement' => 'Configuration des événements',
-	'configuration_distri_produit' => 'Configuration des produits',
-	'configuration_distri_vacance' => 'Configuration des vacances',
-	'configuration_lieu'  => 'Configuration des lieux',
-	'configuration_produit'  => 'Configuration des produits',
-	'configuration_saison'  => 'Configuration des saisons',
-	'configuration_type_contrat'  => 'Configuration des types de contrat',
-	'contrats'  => 'Contrats',
-	'contrat_liste' => 'Liste des contrats',
-	'contrat_liste_produit_saison' => 'Liste des contrats pour le produit @label_produit@ de la saison @id_saison@',
-	'contrat_participation' => 'Participation aux sorties',
-	'contrat_reglement' => 'Règlement',
-	'contrat_sortie' => 'Sorties à la ferme',
-	'contrat_tarifs' => 'Tarif des contrats',
 
 	// D
 	'date_distribution' => 'Date de la distribution',
-	'date_evenement' => 'Date de l\'évènement',
 	'distributions'  => 'Distributions',
-	'distri_evenement'  => 'Événements',
-	'distri_produit'  => 'Produits',
-	'distri_vacance'  => 'Vacances',
 
 	// E
-	'enregistrement_contrat_produit_saison' => 'Enregistrement d\'un nouveau contrat pour le produit @label_produit@ de la saison @id_saison@',
-	'enregistrement_evenement_distribution' => 'Enregistrement d\'un nouvel évènement pour la saison @id_saison@',
-	'enregistrement_vacance' => 'Enregistrement d\'une nouvelle vacance pour la saison',
-	'evenements_enregistre' => 'L\'évènement a été enregistré',
 
 	// F
 
@@ -67,75 +33,37 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'grand' => 'Grand',
 
 	// I
-	'id_banque' => 'ID banque',
-	'id_lieu' => 'ID lieu',
-	'id_panier' => 'ID panier',
-	'id_produit' => 'ID produit',
-	'id_saison' => 'ID saison',
-	'id_type_contrat' => 'ID type contrat',
 
 	// L
-	'label_produit' => 'Type de produit',
-	'label_type_contrat' => 'Durée du contrat',
-	'lieux' => 'Lieux',
-	'lieux_nom' => 'Nom',
-	'lieux_rue' => 'Rue',
-	'lieux_cp' => 'Code postal',
-	'lieux_ville' => 'Ville',
-	'lieux_telephone' => 'Téléphone',
-	'lieux_enregistre' => 'Le lieu a été enregistré',
-	'lieux_enregistrement' => 'Enregistrement d\'un nouveau lieu de distribution',
-	'lieux_enregistres' => 'Liste des lieux possibles',
-	'liste_distribution_enregistres' => 'Liste des produits présents aux distributions de la saison',
-	'liste_distribution_enregistrement' => 'Enregistrement d\'un produit supplémentaire pour la saison',
-	'liste_evenement_distribution' => 'Liste des différents événements de distributions de la saison @id_saison@',
-	'liste_vacance' => 'Liste des vacances de la saison',
 
 	// M
+	'mettre_disposition' => 'Mettre à disposition',
 
 	// N
 	'nom'  => 'NOM',
 
 	// P
+	'panier' => 'Panier',
 	'panier_enregistrement' => 'Enregistrement d\'un panier',
 	'panier_liste' => 'Liste des paniers',
 	'panier_livraison' => 'Contenu d\'un panier',
 	'paniers'  => 'Paniers',
 	'pas_connecte_ou_reconnu' => 'Vous n\'êtes pas connectés ou on ne vous a pas reconnu.',
-	'paysan' => 'Paysan',
 	'petit' => 'Petit',
 	'prenom' => 'Prénom',
-	'produits' => 'Produits',
-	'produits_enregistrement' => 'Enregistrement d\'un produit supplémentaire',
-	'produits_enregistre' => 'Le produit a été enregistré',
-	'produits_enregistres' => 'Liste des produits enregistrés',
 
 	// R
-	'responsable' => 'Responsable',
-	'responsables' => 'Responsables',
 
 	// S
-	'saisons' => 'Saisons',
-	'saisons_enregistrement' => 'Enregistrement d\'une saison supplémentaire',
-	'saisons_enregistre' => 'La saison a été enregistrée',
-	'saisons_enregistres' => 'Liste des saisons enregistrées',
-	'sorties' => 'Sorties',
 
 	// T
 	'table_vide_aucun_enregistrement' => 'Cette table est actuellement vide:
 					<br/>Elle ne contient aucun enregistrement.',
-	'tarifs_enregistrement_saison_produit' => 'Enregistrement d\'un tarif supplémentaire pour la saison @id_saison2@ du produit @label_produit2@',
-	'tarifs_enregistres_saison_produit' => 'Liste des tarifs possibles pour la saison @id_saison1@ du produit @label_produit1@',
 	'tel_fixe' => 'Téléphone fixe',
 	'tel_portable' => 'Téléphone portable',
 	'type_panier' => 'Type de panier',
-	'types_contrats_enregistre' => 'Le type de contrat a été enregistré',
-	'types_contrats_enregistres' => 'Liste des types de contrats possibles',
-	'types_contrats_enregistrement' => 'Enregistrement d\'un type de contrat supplémentaire',
-	'types_de_contrat' => 'Types de contrat',
 
 	// V
-	'vacances' => 'Vacances',
 	'vos_paniers' => 'Vos paniers',
 
 	// Z
