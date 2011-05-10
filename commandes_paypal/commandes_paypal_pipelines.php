8260e94062752f4ac7c10a74153936b3ccbba019
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-05-10 20:37:52 +0000

    C'est maintenant le traitement qui gère les différents types de retours de Paypal.
    Pour l'instant on sait gérer "Completed" et "Pending" (en attente), les autres cas sont considérés comme "erreur" à gérer à la main ensuite.
    
    Il reste essentiellement à notifier par mail et ça me parait pas mal.
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@47654 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- commandes_paypal/commandes_paypal_pipelines.php
+++ commandes_paypal/commandes_paypal_pipelines.php
@@ -8,26 +8,51 @@ function commandes_paypal_traitement_paypal($flux){
 	if (
 		$flux['args']['paypal']['custom'] == 'payer_commande'
 		and $reference = $flux['args']['paypal']['invoice']
-		and $id_commande = sql_getfetsel('id_commande', 'spip_commandes', array('reference = '.sql_quote($reference), 'statut = '.sql_quote('encours')))
+		and $commande = sql_fetsel('id_commande, statut, id_auteur', 'spip_commandes', 'reference = '.sql_quote($reference))
 	){
+		$id_commande = $commande['id_commande'];
+		$statut_commande = $commande['statut'];
+		$statut_paypal = $flux['args']['paypal']['payment_status'];
+		$prix_paypal = $flux['args']['paypal']['mc_gross'];
+		
+		// Si le statut Paypal est "Pending" on passe juste la commande en attente et on verra plus tard pour le reste
+		if ($statut_paypal == 'Pending'){
+			$statut_nouveau = 'attente';
+		}
+		// Si Paypal est "Completed" on vérifie que le montant correspond au prix de cette commande
+		elseif ($statut_paypal == 'Completed'){
+			$fonction_prix = charger_fonction('prix', 'inc/');
+			$prix_commande = $fonction_prix('commande', $id_commande);
+			 
+			 // Si on a pas assez payé
+			 if ($prix_paypal < $prix_commande){
+			 	$statut_nouveau = 'partiel';
+			 }
+			 // Sinon c'est bon
+			 else{
+			 	$statut_nouveau = 'paye';
+			 }
+		}
+		// Sinon on dit que c'est en erreur
+		else{
+			$statut_nouveau = 'erreur';
+		}
+		
 		// On change le statut de la commande
 		$ok = sql_updateq(
 			'spip_commandes',
 			array(
-				'statut' => 'paye'
+				'statut' => $statut_nouveau
 			),
 			'id_commande = '.$id_commande
 		);
 		
-		// On change le statut de tous les détails
-		if ($ok)
-			$ok = sql_updateq(
-				'spip_commandes_details',
-				array(
-					'statut' => 'paye'
-				),
-				'id_commande = '.$id_commande
-			);
+		// Si c'est bon, on appelle une notification
+		if ($ok){
+			$id_auteur = $commande['id_auteur'];
+			$fonction_notif = charger_fonction('notifications', 'inc/');
+			$fonction_notif('commande_instituer', $id_commande, array('ancien_statut' => $statut_commande, 'nouveau_statut' => $statut_nouveau));
+		}
 	}
 	
 	return $flux;
