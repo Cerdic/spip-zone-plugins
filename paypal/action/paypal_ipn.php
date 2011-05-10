ddaeb4d88125d95f5f820f9242892fe146384a7a
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-05-10 09:56:29 +0000

    Il ne faut pas vérifier le statut du paiement avant d'envoyer aux traitements ! En effet il ya plein de statuts possibles qui ne sont pas des erreurs non plus. Notamment "Pending" (en attente) qui ensuite peut être validé par le marchand sur son compte.
    Ce sont donc aux plugins utilisant IPN (comme "commandes_paypal") de gérer les différents statuts et faire telle ou telle action en conséquence.
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@47625 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- paypal/action/paypal_ipn.php
+++ paypal/action/paypal_ipn.php
@@ -25,29 +25,21 @@ function action_paypal_ipn_dist() {
 
 	// retour peut etre "INVALID" ou "VERIFIED".
 	spip_log($retour,'paypal');
-
-	if ($retour == "VERIFIED") {
-
-		// verifications :
-		// 1) paiement ok
-		// 2) email compte paypal ok
-		// 3) identifiant unique de transaction
-		if (($datas['payment_status'] == 'Completed')
-		and ($datas['receiver_email'] == $conf['api'][$envr]['account'])) {
-			// 3 ... on suppose que c'est OK
-			// on pourrait imaginer creer une table speciale pour les transactions
-			// mais bon...
-			
-			spip_log('Retour de Paypal vérifié, on peut passer aux traitements','paypal');
-			// c'est tout bon, on envoie ca au pipeline pour traitements
-			pipeline('traitement_paypal', array(
-				'args'=>array(
-					'paypal' => $datas,
-					'test' => (($envr == 'test') or ($datas['test_ipn'] == 1)),
-				),
-				'data'=>'')
-			);
-		}
+	
+	// Si c'est bon, on vérifie aussi que l'email soit celui configuré dans le compte
+	if (
+		$retour == "VERIFIED"
+		and $datas['receiver_email'] == $conf['api'][$envr]['account']
+	) {
+		spip_log('Retour de Paypal vérifié, on peut passer aux traitements','paypal');
+		// c'est tout bon, on envoie ca au pipeline pour traitements
+		pipeline('traitement_paypal', array(
+			'args'=>array(
+				'paypal' => $datas,
+				'test' => (($envr == 'test') or ($datas['test_ipn'] == 1)),
+			),
+			'data'=>'')
+		);
 	}
 
 	// retourner un statut code 200 pour paypal
