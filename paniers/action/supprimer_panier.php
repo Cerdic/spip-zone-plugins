0408be85c794530af1f7203fa1e3c1cfb4a23bf3
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-05-11 10:53:56 +0000

    On peut appeler cette action explicitement sans la sécurité.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@47674 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- paniers/action/supprimer_panier.php
+++ paniers/action/supprimer_panier.php
@@ -8,17 +8,17 @@
 
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
-function action_supprimer_panier_dist() {
-
-	$securiser_action = charger_fonction('securiser_action', 'inc');
-	$id_panier = $securiser_action();
-
+function action_supprimer_panier_dist($arg=null) {
+	if (is_null($arg)){
+		$securiser_action = charger_fonction('securiser_action', 'inc');
+		$arg = $securiser_action();
+	}
+	
 	// suppression
-	if ($id_panier = intval($id_panier)) {
+	if ($id_panier = intval($arg)) {
 		sql_delete('spip_paniers', 'id_panier=' . $id_panier);
 		sql_delete('spip_paniers_liens', 'id_panier=' . $id_panier);
 	}
-
 }
 
 ?>
