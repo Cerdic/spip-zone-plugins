7992db1f92efb372124c3d98968426294cd29822
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-05-11 11:54:41 +0000

    Lors de la création d'une nouvelle commande "encours", il ne faut supprimer celle indiquée dans la session que si elle est toujours "encours" (entre temps elle peut avoir été payée).
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@47677 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- commandes/inc/commandes.php
+++ commandes/inc/commandes.php
@@ -13,19 +13,25 @@ function creer_commande_encours(){
 	
 	// S'il y a une commande en cours dans la session, on la supprime
 	if (($id_commande = intval(session_get('id_commande'))) > 0){
-		// On supprime son contenu
-		sql_delete('spip_commandes_details', 'id_commande = '.$id_commande);
+		// Si la commande est toujours "encours" on la supprime de la base
+		if ($statut = sql_getfetsel('statut', 'spip_commandes', 'id_commande = '.$id_commande) and $statut == 'encours'){
+			// On supprime son contenu
+			sql_delete('spip_commandes_details', 'id_commande = '.$id_commande);
 		
-		// S'il y a des adresses attachées à la commande, on les supprime
-		if ($adresses_commande = sql_allfetsel('id_adresse', 'spip_adresses_liens', array('objet = '.sql_quote('commande'), 'id_objet = '.$id_commande))){
-			$adresses_commande = array_map('reset', $adresses_commande);
-			$in = sql_in('id_adresse', $adresses_commande);
-			sql_delete('spip_adresses_liens', $in);
-			sql_delete('spip_adresses', $in);
+			// S'il y a des adresses attachées à la commande, on les supprime
+			if ($adresses_commande = sql_allfetsel('id_adresse', 'spip_adresses_liens', array('objet = '.sql_quote('commande'), 'id_objet = '.$id_commande))){
+				$adresses_commande = array_map('reset', $adresses_commande);
+				$in = sql_in('id_adresse', $adresses_commande);
+				sql_delete('spip_adresses_liens', $in);
+				sql_delete('spip_adresses', $in);
+			}
+		
+			// On supprime la commande
+			sql_delete('spip_commandes', 'id_commande = '.$id_commande);
 		}
 		
-		// On supprime la commande
-		sql_delete('spip_commandes', 'id_commande = '.$id_commande);
+		// Dans tous les cas on supprime la valeur de session
+		session_set('id_commande');
 	}
 	
 	// Le visiteur en cours
