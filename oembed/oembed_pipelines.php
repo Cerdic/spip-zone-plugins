3b53fd3a28a50e5360f94d363c492aead1db9c86
fil@rezo.net <fil@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-05-12 12:30:30 +0000

    seuls les autoliens bneficient de la detection oembed (mettre a jour textwheel pour avoir la class=auto) + revert de malencontreux envois en r47711
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@47715 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- oembed/oembed_pipelines.php
+++ oembed/oembed_pipelines.php
@@ -84,7 +84,10 @@ function oembed_post_edition($flux) {
 function oembed_pre_propre($texte) {
 	include_spip('inc/oembed');
 	foreach (extraire_balises($texte, 'a') as $lien) {
-		if ($url = extraire_attribut($lien, 'href') AND oembed_verifier_provider($url)) {
+		if ($url = extraire_attribut($lien, 'href')
+		# seuls les autoliens beneficient de la detection oembed
+		AND preg_match(',\bauto\b,', extraire_attribut($lien, 'class'))
+		AND oembed_verifier_provider($url)) {
 			$fond = recuperer_fond('modeles/oembed',array('url'=>$url));
 			if ($fond = trim($fond))
 				$texte = str_replace($lien, $fond, $texte);
