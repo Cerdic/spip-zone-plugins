9f252951edaed82abc475594f9a377c00ee920eb
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-05-13 06:57:48 +0000

    compatibilite SPIP 3.0
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@47734 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- couteau-kiss/exec/configurer_ck.php
+++ couteau-kiss/exec/configurer_ck.php
@@ -13,6 +13,7 @@ function exec_couteau_dist() {
 		echo minipres();
 	} else {
 
+		include_spip('inc/presentation');
 		$commencer_page = charger_fonction('commencer_page', 'inc');
 		echo $commencer_page(_T('ck:titre_page_couteau'), "configuration", "ck");
 
@@ -20,16 +21,11 @@ function exec_couteau_dist() {
 		echo barre_onglets("configuration", "ck");
 
 		echo debut_gauche('plugin',true);
-		echo debut_boite_info(true);
-		$s .= propre(_T('ck:texte_boite_info'));
-		echo $s;
-		echo fin_boite_info(true);
-
 		echo debut_droite('plugin', true);
 		echo pipeline('affiche_milieu',
 			array(
 			'args'=>array('exec'=>'ck'),
-			'data'=>recuperer_fond('prive/ck',$_GET)
+			'data'=>recuperer_fond('prive/squelettes/contenu/configurer_ck',$_GET)
 			)
 		);
 
