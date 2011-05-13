702a2bd0b6fc507551fd5468ae8c1ad97b19cad8
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-05-13 11:01:05 +0000

    portage provisoire a minima pour SPIP 3.0 : - le formulaire de configuration est refait en html régulier, mais on a perdu le drag&ndrop des champs - le formulaire de contact marche, avec 2 chaines de langue manquantes fournies ici en depannage - les messages sont bien enregistrés, mais l'interface de consultation qui repose sur les fonctions du privée est cassée
    A finir plus tard, mais l'essentiel fonctionne
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@47745 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ contact/lang/public_fr.php
@@ -0,0 +1,13 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/contact/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	'info_texte_message' => 'Texte de votre message',
+	'form_pet_votre_email' => 'Votre email',
+
+);
+
+?>
