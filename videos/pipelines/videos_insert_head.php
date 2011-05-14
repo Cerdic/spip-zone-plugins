c8641748210d7b21cf70e366c25850f26bda8294
xdjuj@apsulis.com <xdjuj@apsulis.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-05-14 15:35:35 +0000

    - Se rapprocher de SPIP 3 : --- Nécessiter la médiathèque --- Se brancher sur le bando - Pouvoir choisir le wmode pour les fallback et les iframes - Corriger quelques bugs FF (couleurs de background du fallback et marge inventées) - Corriger le bug de modèle, si un id_document est présent il doit être prioritaire sur #ID (sinon ça plante les appels via le squelette et #MODELE) - Corriger la boucle des "autres formats présents" pour la balise <video> qui était très imprécise et remontait beaucoup trop de résultats - Utiliser le figcaption lié à la balise <video> plutôt que la définition des docs spip pour les formats utilisés par la balise (c'est plus propre en HTML5) -
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@47794 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ videos/pipelines/videos_insert_head.php
@@ -0,0 +1,11 @@
+<?php
+function videos_insert_head($flux){
+	$variables = generer_url_public('videos_variables.js');
+	$css = find_in_path('theme/css/videos.css');
+	$flux .="
+<!-- Variables de configuration pour le plugin Vidéo(s) -->
+<script type='text/javascript' src='$variables'></script>\n".
+'<link rel="stylesheet" href="'.direction_css($css).'" type="text/css" media="all" />'.
+"<!-- // Vidéo(s) -->"."\n";
+	return $flux;
+}
