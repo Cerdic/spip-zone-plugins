642999f3dfff43ac8242bb4a3458b41e7ccc4a9e
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-05-16 19:16:15 +0000

    compatibilite SPIP 3.0, et une classe submit sur le submit. il faudrait reecrire le skel qui fait appel a debut_cadre, mais ça oblige a forker. On laisse tel quel pour le moment
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@47855 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- coordonnees/coordonnees_pipelines.php
+++ coordonnees/coordonnees_pipelines.php
@@ -11,14 +11,19 @@
  * sur la page de visualisation d'un auteur
 **/
 function coordonnees_affiche_milieu($flux) {
-	if ($flux['args']['exec'] == 'auteur_infos') {
-		if ($id_auteur = $flux['args']['id_auteur']) {
+	if (
+		(($flux['args']['exec'] == 'auteur_infos') AND $id_auteur = $flux['args']['id_auteur'])
+		OR ($exec = trouver_objet_exec(_request('exec'))
+		    AND $exec['type']=='auteur'
+				AND !$exec['edition']
+		    AND $id_auteur = $flux['args']['id_auteur'])
+		){
+			include_spip('inc/presentation');
 			$contexte = array(
 				'objet' => 'auteur',
 				'id_objet' => $id_auteur
 			);
 			$flux['data'] .= recuperer_fond('prive/boite/coordonnees', $contexte, array('ajax'=>true));
-		}
 	}
 	return $flux;
 }
