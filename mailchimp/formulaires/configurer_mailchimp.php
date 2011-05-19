7ffc8a2cfd052b645ac9ab776d70ab7d78645396
ben.spip@gmail.com <ben.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-05-19 18:40:42 +0000

    utilisation de ecrire_config
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@47945 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- mailchimp/formulaires/configurer_mailchimp.php
+++ mailchimp/formulaires/configurer_mailchimp.php
@@ -4,13 +4,16 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 # API mailchimp
 include_spip('inc/1.3/MCAPI.class');
 
+#pour ecrire_config 
+include_spip('inc/config');
+
  
 /**
  * Formulaire de configuration du plugin Mailchimp
  * On vérifie juste que l'on peut se connecter à l'API mailchimp   
  * ( et on stocke les 5 premiers abonnés de la liste pour passer à traiter) 
  */
-function formulaires_configurer_mailchimp_verifier_dist($chaine='')
+function formulaires_configurer_mailchimp_verifier_dist()
 {
 	$res = array();
 
@@ -19,7 +22,7 @@ function formulaires_configurer_mailchimp_verifier_dist($chaine='')
 	$apiKey = _request("apiKey");
 	$listId = _request("listId");
 
-	spip_log(__FILE__." ".__LINE__. " $apiKey - $listId" ) ;
+	spip_log("Plugin mailchimp form vérifier : $apiKey - $listId" ) ;
 
 	// initialisation d'un objet mailchimp
 	$api = new MCAPI($apiKey);
@@ -60,10 +63,11 @@ function formulaires_configurer_mailchimp_verifier_dist($chaine='')
 function formulaires_configurer_mailchimp_traiter_dist($chaine)
 {
 	$res = array();
-
 	#Ecriture des parametres dans META 
-	ecrire_meta("mailchimp/apiKey", _request("apiKey") );
-	ecrire_meta("mailchimp/listId", _request("listId") );
+	ecrire_config("mailchimp/apiKey", _request("apiKey") );
+	ecrire_config("mailchimp/listId", _request("listId") );
+
+	spip_log ("Plugin mailchimp/ sauvegarde de la meta apiKey:". _request("apiKey") ." ListId:". _request("listId") );
 
 	#Retour succes 
 	$res = array('message_ok' => _T('mailchimp:retour_test_api')._MAILCHIMP_CONF_LISTE_ABONNES  );
