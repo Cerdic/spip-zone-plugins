b6fe8bedc16c99f39a5a8c9337c7301b1ef4ea7e
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-05-19 09:45:45 +0000

    oups, il faut inserer le js dans insert_head, pas dans insert_head_css deplacement/renomage/resizing de l'icone version
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@47922 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- faq/faq_pipelines.php
+++ faq/faq_pipelines.php
@@ -1,22 +1,22 @@
 <?php
 
-function faq_head(){
+function faq_css(){
 	$css ="";
-	$css .= '<script src="'.find_in_path('faq.js').'" type="text/javascript"></script>';
 	$css .= '<link rel="stylesheet" href="'.find_in_path('faq.css').'" type="text/css" media="all" />';
 	return $css;
 }
 
 function faq_insert_head($flux) {
 	if (intval($GLOBALS['spip_version_branche'])<3){
-		$flux .= faq_head();
+		$flux .= faq_css();
 	}
+	$flux .= '<script src="'.find_in_path('faq.js').'" type="text/javascript"></script>';
 	return $flux;
 }
 
 function faq_insert_head_css($flux) {
 	if (intval($GLOBALS['spip_version_branche'])>=3){
-		$flux .= faq_head();
+		$flux .= faq_css();
 	}
 	return $flux;
 }
