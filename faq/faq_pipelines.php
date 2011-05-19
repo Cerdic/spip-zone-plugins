d885d4eda73820ceaaac4e684a8f7eeebb2dbe30
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-05-19 09:13:27 +0000

    compat SPIP 3 : inserer dans insert_head_css pour passer avant les styles du site.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@47919 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- faq/faq_pipelines.php
+++ faq/faq_pipelines.php
@@ -1,8 +1,23 @@
 <?php
 
+function faq_head(){
+	$css ="";
+	$css .= '<script src="'.find_in_path('faq.js').'" type="text/javascript"></script>';
+	$css .= '<link rel="stylesheet" href="'.find_in_path('faq.css').'" type="text/css" media="all" />';
+	return $css;
+}
+
 function faq_insert_head($flux) {
-	$flux .= '<script src="'.find_in_path('faq.js').'" type="text/javascript"></script>';
-	$flux .= '<link rel="stylesheet" href="'.find_in_path('faq.css').'" type="text/css" media="all" />';
+	if (intval($GLOBALS['spip_version_branche'])<3){
+		$flux .= faq_head();
+	}
+	return $flux;
+}
+
+function faq_insert_head_css($flux) {
+	if (intval($GLOBALS['spip_version_branche'])>=3){
+		$flux .= faq_head();
+	}
 	return $flux;
 }
 
