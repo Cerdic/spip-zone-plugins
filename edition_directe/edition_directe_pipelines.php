fce1b16fe7872b317b7bf22949c29e64a6eb276a
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-05-19 09:54:24 +0000

    Eviter les erreurs si l'id n'est pas présent dans le contexte
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@47924 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- edition_directe/edition_directe_pipelines.php
+++ edition_directe/edition_directe_pipelines.php
@@ -13,26 +13,27 @@ function edition_directe_header_prive($flux){
 	// objet article
    if ($type=='article'AND lire_config('edition_directe/article')){
 	$id_article= _request('id_article');
-	$row = sql_fetsel("*", "spip_articles", "id_article=$id_article");
-	
-	$contexte = array(
-		'icone_retour'=>icone_inline(_T('icone_retour'), $oups, "article-24.gif", "rien.gif",$GLOBALS['spip_lang_left']),
-		'redirect'=>generer_url_ecrire("articles"),
-		'titre'=>$row['titre'],
-		'new'=>$new?$new:$id_article,
-		'id_rubrique'=>$row['id_rubrique'],
-		'id_secteur'=>$row['id_secteur'],
-		'config_fonc'=>'articles_edit_config',
-		// passer row si c'est le retablissement d'une version anterieure
-		'row'=> $id_version
-		? $row
-		: null
-		);
-	
-	$formulaire=recuperer_fond('prive/editer/article_mod',$contexte);
-	
-	$flux['data'] =preg_replace('/<div id=\'props\' class=\'tabs-container\'>/',$formulaire.'<div id="props" class="tabs-container">',$flux['data']);
-	
+	if($id_article){
+		$row = sql_fetsel("*", "spip_articles", "id_article=$id_article");
+		
+		$contexte = array(
+			'icone_retour'=>icone_inline(_T('icone_retour'), $oups, "article-24.gif", "rien.gif",$GLOBALS['spip_lang_left']),
+			'redirect'=>generer_url_ecrire("articles"),
+			'titre'=>$row['titre'],
+			'new'=>$new?$new:$id_article,
+			'id_rubrique'=>$row['id_rubrique'],
+			'id_secteur'=>$row['id_secteur'],
+			'config_fonc'=>'articles_edit_config',
+			// passer row si c'est le retablissement d'une version anterieure
+			'row'=> $id_version
+			? $row
+			: null
+			);
+		
+		$formulaire=recuperer_fond('prive/editer/article_mod',$contexte);
+		
+		$flux['data'] =preg_replace('/<div id=\'props\' class=\'tabs-container\'>/',$formulaire.'<div id="props" class="tabs-container">',$flux['data']);
+		}
 	}
 
 	
@@ -44,33 +45,37 @@ function edition_directe_afficher_contenu_objet($flux){
     $type = $flux['args']['type'];
 	// objet rubrique
 	if ($type=='rubrique'AND lire_config('edition_directe/rubrique')){
+	
 	$id_rubrique= _request('id_rubrique');
-	$row = sql_fetsel("*", "spip_rubriques", "id_rubrique=$id_rubrique");
-
-	$contexte = array(
-		'new'=>$id_rubrique,
-		'titre'=>$row['titre'],
-		'id_rubrique'=>$row['id_parent'], // pour permettre la specialisation par la rubrique appelante
-		'config_fonc'=>'rubriques_edit_config',
-		);
-
-	 
-	 $flux['data']=recuperer_fond("prive/editer/rubrique_mod", $contexte);
+	if($id_rubrique){
+		$row = sql_fetsel("*", "spip_rubriques", "id_rubrique=$id_rubrique");
+	
+		$contexte = array(
+			'new'=>$id_rubrique,
+			'titre'=>$row['titre'],
+			'id_rubrique'=>$row['id_parent'], // pour permettre la specialisation par la rubrique appelante
+			'config_fonc'=>'rubriques_edit_config',
+			);
+	
+		 
+		 $flux['data']=recuperer_fond("prive/editer/rubrique_mod", $contexte);
+		 }
 	
 	}
 	if ($type=='breve'AND lire_config('edition_directe/breve')){
 	$id_breve= _request('id_breve');
-
-	$contexte = array(
-	'redirect'=>generer_url_ecrire("breves_voir"),
-	'new'=>$id_breve,
-	'id_rubrique'=>$id_rubrique,
-	'config_fonc'=>'breves_edit_config'
-	);
-
-
-	 
-	 $flux['data']=recuperer_fond("prive/editer/breve", $contexte);
+		if($id_breve){
+			$contexte = array(
+			'redirect'=>generer_url_ecrire("breves_voir"),
+			'new'=>$id_breve,
+			'id_rubrique'=>$id_rubrique,
+			'config_fonc'=>'breves_edit_config'
+			);
+		
+		
+			 
+			 $flux['data']=recuperer_fond("prive/editer/breve", $contexte);
+			 }
 	
 	}	
 	return $flux;
