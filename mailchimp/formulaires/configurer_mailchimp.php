eb8363311e8bc0e762b188c3a051f66c09e77ef6
ben.spip@gmail.com <ben.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-05-19 18:27:06 +0000

    compatible spip3 ( et nécessite spip3.0.0dev du coup)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@47944 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- mailchimp/formulaires/configurer_mailchimp.php
+++ mailchimp/formulaires/configurer_mailchimp.php
@@ -1,44 +1,74 @@
 <?php
 
 if (!defined("_ECRIRE_INC_VERSION")) return;
-include_spip('inc/actions');
-include_spip('inc/editer');
 # API mailchimp
 include_spip('inc/1.3/MCAPI.class');
-// necessaire pour utiliser lire_config
-include_spip('inc/config');
 
-
-function formulaires_configurer_mailchimp_verifier_dist()
+ 
+/**
+ * Formulaire de configuration du plugin Mailchimp
+ * On vérifie juste que l'on peut se connecter à l'API mailchimp   
+ * ( et on stocke les 5 premiers abonnés de la liste pour passer à traiter) 
+ */
+function formulaires_configurer_mailchimp_verifier_dist($chaine='')
 {
 	$res = array();
 
-
+	$chaine="<br/>";
 	#recuperation de la config
 	$apiKey = _request("apiKey");
 	$listId = _request("listId");
 
-	spip_log(__FILE__."  ".__LINE__);
-	spip_log($apiKey);
-	spip_log($listId);
+	spip_log(__FILE__." ".__LINE__. " $apiKey - $listId" ) ;
 
 	// initialisation d'un objet mailchimp
 	$api = new MCAPI($apiKey);
+	
+	// appel de la méthode Suscribed qui renvoie les 5 premiers inscrits 
 	$retval = $api->listMembers($listId, 'subscribed', null, 0, 5);
 
-	if ($api->errorCode){
-		spip_log(__FILE__."  ".__LINE__);
+	// L'api a retourné une erreur 
+	if ($api->errorCode)
+	{
 		$res = array('message_erreur' => _T('mailchimp:configurer_erreur_api')."<br/>"._T('mailchimp:api_errorcode')."<br/><b>".$api->errorCode."</b><br/><b>".$api->errorMessage ."</b>");
-	} else {
-		spip_log(__FILE__."  ".__LINE__);
-		$chaine=_T('mailchimp:retour_test_api')."<br/><br/>";
-		foreach ($retval['data'] as $member){
-			spip_log(__FILE__."  ".__LINE__);
-			$chaine .= $member['email']." - ".$member['timestamp']."<br/>";
-		}
-		$res = array('message_ok' => $chaine);
 	}
-	spip_log(__FILE__."  ".__LINE__);
+	else 
+	{
+		// On récupère les 5 derniers 
+		foreach ($retval['data'] as $member)
+		{
+			$chaine .= $member['email']." - ".$member['timestamp']."<br/> ";
+		}
+	}
+	//C'est pas beau mais bon, pas de possibilité de passer une variable de vérifier à traiter 	
+	define('_MAILCHIMP_CONF_LISTE_ABONNES',$chaine);
+
+
 	return $res;
 }
+
+
+
+/**
+ * Formulaire de configuration du plugin Mailchimp
+ * On traite l'information : sauvegarde dans une meta et 
+ * affichage du succes dans une belle boite .     
+ * 
+ */
+
+
+function formulaires_configurer_mailchimp_traiter_dist($chaine)
+{
+	$res = array();
+
+	#Ecriture des parametres dans META 
+	ecrire_meta("mailchimp/apiKey", _request("apiKey") );
+	ecrire_meta("mailchimp/listId", _request("listId") );
+
+	#Retour succes 
+	$res = array('message_ok' => _T('mailchimp:retour_test_api')._MAILCHIMP_CONF_LISTE_ABONNES  );
+
+	return $res;
+}
+
 ?>
