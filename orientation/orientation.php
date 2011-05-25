d9cc3592a9964024cb2c4197b240df0d34c7661e
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-05-25 21:14:02 +0000

    portage SPIP 3 sans casser la compat 2.x + une icone
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@48117 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- orientation/orientation.php
+++ orientation/orientation.php
@@ -3,30 +3,30 @@
 /* Le plugin orientation ajoute le critere {portrait} , {carre} ou {paysage} pour le classement des photos */
 
 // {portrait}
-function critere_portrait($idb, &$boucles, $crit) {
+function critere_portrait_dist($idb, &$boucles, $crit) {
 	$boucle = &$boucles[$idb];
 	$table = $boucle->id_table;
 	$boucle->where[] = $crit->not
-		?"'$table.hauteur <= $table.largeur'"
-		:"'$table.hauteur > $table.largeur'";
+		?"'($table.largeur>0 AND $table.hauteur <= $table.largeur)'"
+		:"'($table.largeur>0 AND $table.hauteur > $table.largeur)'";
 }
 
 // {paysage}
-function critere_paysage($idb, &$boucles, $crit) {
+function critere_paysage_dist($idb, &$boucles, $crit) {
 	$boucle = &$boucles[$idb];
 	$table = $boucle->id_table;
 	$boucle->where[] = $crit->not
-		?"'$table.largeur <= $table.hauteur'"
-		:"'$table.largeur > $table.hauteur'";
+		?"'($table.largeur>0 AND $table.largeur <= $table.hauteur)'"
+		:"'($table.largeur>0 AND $table.largeur > $table.hauteur)'";
 }
 
 // {carre}
-function critere_carre($idb, &$boucles, $crit) {
+function critere_carre_dist($idb, &$boucles, $crit) {
 	$boucle = &$boucles[$idb];
 	$table = $boucle->id_table;
-	$boucle->where[] = $crit->not
-	?"'$table.largeur != $table.hauteur'"
-	:"'$table.largeur = $table.hauteur'";
+	$boucle->where[] = $crit->not?
+		 "'($table.largeur != $table.hauteur)'"
+		:"'($table.largeur>0 AND $table.largeur = $table.hauteur)'";
 }
 
 ?>
