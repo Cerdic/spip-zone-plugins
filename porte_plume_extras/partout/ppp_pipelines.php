8f91925a24e6e53d89af7608ad2ff0c369e47dc9
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-05-26 15:33:54 +0000

    propager le var_mode=recalcul
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@48150 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- porte_plume_extras/partout/ppp_pipelines.php
+++ porte_plume_extras/partout/ppp_pipelines.php
@@ -3,6 +3,8 @@
 //Appel du pipeline qui ajoute des barres de porte-plume partout dans Descriptif, chapo, PS, Bio et Descriptif du site
 function ppp_insert_head_prive($flux){
 	$js = generer_url_public('barre_generalisee.js');
+	if (_VAR_MODE=="recalcul")
+		$js = parametre_url($js, 'var_mode', 'recalcul');
 	$flux .= "<script type='text/javascript' src='$js'></script>\n";
 	return $flux;
 }
