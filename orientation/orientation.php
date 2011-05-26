55cd7fef3fc6458e0d399fd4878a8eb5c7c2e86d
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-05-26 06:54:16 +0000

    coherence avec r48122. A noter que le plugin n'a d'interet en SPIP 3 que si on a besoin des criteres sur une autre boucle que la boucle DOCUMENTS
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@48123 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- orientation/orientation.php
+++ orientation/orientation.php
@@ -1,32 +1,58 @@
 <?php
 
 /* Le plugin orientation ajoute le critere {portrait} , {carre} ou {paysage} pour le classement des photos */
-
-// {portrait}
+/**
+ * critere {portrait} qui selectionne
+ * - les documents dont les dimensions sont connues
+ * - les documents dont la hauteur est superieure a la largeur
+ *
+ * {!portrait} exclus ces documents
+ *
+ * @param string $idb
+ * @param object $boucles
+ * @param object $crit
+ */
 function critere_portrait_dist($idb, &$boucles, $crit) {
 	$boucle = &$boucles[$idb];
 	$table = $boucle->id_table;
-	$boucle->where[] = $crit->not
-		?"'($table.largeur>0 AND $table.hauteur <= $table.largeur)'"
-		:"'($table.largeur>0 AND $table.hauteur > $table.largeur)'";
+	$not = ($crit->not?"NOT ":"");
+	$boucle->where[] = "'$not($table.largeur>0 AND $table.hauteur > $table.largeur)'";
 }
 
-// {paysage}
+/**
+ * critere {paysage} qui selectionne
+ * - les documents dont les dimensions sont connues
+ * - les documents dont la hauteur est inferieure a la largeur
+ *
+ * {!paysage} exclus ces documents
+ *
+ * @param string $idb
+ * @param object $boucles
+ * @param object $crit
+ */
 function critere_paysage_dist($idb, &$boucles, $crit) {
 	$boucle = &$boucles[$idb];
 	$table = $boucle->id_table;
-	$boucle->where[] = $crit->not
-		?"'($table.largeur>0 AND $table.largeur <= $table.hauteur)'"
-		:"'($table.largeur>0 AND $table.largeur > $table.hauteur)'";
+	$not = ($crit->not?"NOT ":"");
+	$boucle->where[] = "'$not($table.largeur>0 AND $table.largeur > $table.hauteur)'";
 }
 
-// {carre}
+/**
+ * critere {carre} qui selectionne
+ * - les documents dont les dimensions sont connues
+ * - les documents dont la hauteur est egale a la largeur
+ *
+ * {!carre} exclus ces documents
+ *
+ * @param string $idb
+ * @param object $boucles
+ * @param object $crit
+ */
 function critere_carre_dist($idb, &$boucles, $crit) {
 	$boucle = &$boucles[$idb];
 	$table = $boucle->id_table;
-	$boucle->where[] = $crit->not?
-		 "'($table.largeur != $table.hauteur)'"
-		:"'($table.largeur>0 AND $table.largeur = $table.hauteur)'";
+	$not = ($crit->not?"NOT ":"");
+	$boucle->where[] = "'$not($table.largeur>0 AND $table.largeur = $table.hauteur)'";
 }
 
 ?>
