1f3f3b904a6236daef83d5a68f1279d19a0b7fed
denisb@laposte.net <denisb@laposte.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-05-28 10:46:46 +0000

    svp : on renomme le critere pour ne pas avoir d'homonymie avec un champ existant et on ameliore les conditions (derogatoires !) d'utilisation...
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@48236 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- svp/inc/svp_lister_compatibles.php
+++ svp/inc/svp_lister_compatibles.php
@@ -2,11 +2,18 @@
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 /**
- * Fonction qui calcule le tableau des plugins ou paquets compatibles avec une version de spip
- * Cette fonction est appelee par le critere compatibilite_spip
+ * Fonction qui calcule le tableau des plugins ou paquets
+ * compatibles avec une version de spip.
+ * Cette fonction est appelee par le critere {compatible_spip}
  * @return 
  */
 function inc_svp_lister_compatibles($version, $table, $pkey) {
+
+	// version explicite dans l'appel du critere
+	// mais inexistante (#GET{vers} non declare par exemple)
+	if (!strlen($version)) {
+		$version = $GLOBALS['spip_version_branche'];
+	}
 
 	include_spip("inc/plugin");
 
