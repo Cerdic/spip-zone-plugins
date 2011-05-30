eaecc6a167aa193314d33a7afeeaaf63017a7eb3
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-05-30 08:00:04 +0000

    branches pour la v2 qui ne s'arretera a SPIP 2.x
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@48269 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ en_travaux/branches/v2/en_travaux_options.php
@@ -0,0 +1,101 @@
+<?php
+/*
+ * Plugin En Travaux
+ * (c) 2006-2009 Arnaud Ventre, Cedric Morin
+ * Distribue sous licence GPL
+ *
+ */
+
+#var_dump($GLOBALS['meta']['entravaux_id_auteur']);
+
+function autoriser_travaux_dist(){
+	return autoriser('webmestre');
+}
+
+if (isset($GLOBALS['meta']['entravaux_id_auteur']) AND $GLOBALS['meta']['entravaux_id_auteur']){
+	// desactiver le cache
+	define('_NO_CACHE',1);
+
+	// au cas ou, placer tout nouveau calcul dans le cache
+	$GLOBALS['marqueur'].= ":en_travaux";
+
+	if (test_espace_prive() AND (_request('exec')!='admin_plugin'
+	//		OR _request('action')!='activer_plugins'
+	)){
+		include_spip('inc/autoriser');
+		if (!autoriser('travaux')){
+			$travaux = recuperer_fond("en_travaux",array());
+			// fallback : le fond renvoie parfois du vide ...
+			if (!strlen($travaux)){
+				@define('_SPIP_SCRIPT','spip.php');
+				echo "Acces interdit (en travaux) <a href='"
+				.generer_url_action('logout',"logout=public",false,true)
+				."'>Deconnexion</a>";
+			}
+			die();
+		}
+	}
+
+}
+else {
+		include_spip('inc/autoriser');
+		if (!autoriser('travaux')){
+			// se desactiver tout seul car on ne sert a rien
+			// sauf a ralentir le site !
+			include_spip('inc/plugin');
+			ecrire_plugin_actifs(array('ENTRAVAUX'=>trim(substr(_DIR_PLUGIN_ENTRAVAUX,strlen(_DIR_PLUGINS)),'/')),false,'enleve');
+			// avec un message d'erreur smart
+			ecrire_meta('plugin_erreur_activation',_T('entravaux:erreur_droit'));
+		}
+}
+
+/**
+ * Pipeline styliser pour rerouter tous les fonds vers en_travaux
+ * sauf si l'auteur connecte est celui qui a active le plugin
+ *
+ * @param array $flux
+ * @return array
+ */
+function entravaux_styliser($flux){
+	if (isset($GLOBALS['meta']['entravaux_id_auteur']) AND $GLOBALS['meta']['entravaux_id_auteur']){
+		include_spip('inc/autoriser');
+		if (!autoriser('travaux')
+			AND !in_array($flux['args']['fond'],array('login_sos','formulaires/login','formulaires/menu_lang','formulaires/inc-logo_auteur','formulaires/administration'))){
+			$ext = $flux['args']['ext'];
+			$fond = find_in_path('en_travaux.html');
+			$flux['data'] = substr($fond, 0, - strlen(".$ext"));
+		}
+	}
+	return $flux;
+}
+
+
+/**
+ * Afficher une icone de travaux sur tout le site pour que le webmestre n'oublie pas
+ * de retablir le site
+ * 
+ * @param <type> $flux 
+ */
+function entravaux_affichage_final($flux){
+	$image = find_in_path("en_travaux.png");
+	if ($GLOBALS['html']
+	AND !_AJAX){
+		include_spip('inc/minipres'); // pour http_img_pack
+		$x = '<div id="icone_travaux" style="
+		padding-right: 5px;
+		padding-top: 2px;
+		padding-bottom: 5px;
+		position: absolute;
+		left: 26px;
+		top: 26px;
+		">'
+		. http_img_pack(find_in_path('en_travaux-64.png'), _T('entravaux:en_travaux'), '')
+		. '</div>';
+		if (!$pos = strpos($flux, '</body>'))
+			$pos = strlen($flux);
+		$flux = substr_replace($flux, $x, $pos, 0);
+	}
+	return $flux;
+}
+
+?>
---
+++ en_travaux/branches/v2/lang/entravaux_it.php
@@ -0,0 +1,14 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+'activer_message' => 'Attivare una sessione di manutenzione',
+'en_travaux' => 'In manutenzione',
+'info_travaux_texte' => 'Questo sito &egrave; stato in manutenzione.<br /> Torna pi&ugrave; tardi...',
+'parametrage_page_travaux' => 'Configurazione della pagina di manutenzione',
+'info_message' => 'Questa pagina consente di visualizzare un messaggio temporaneo su tutte le pagine del sito durante una sessione di manutenzione.',
+'message_temporaire' => 'Il vostro messaggio di manutenzione:',
+
+);
+?>
