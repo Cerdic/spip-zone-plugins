75db5c6bbebf4a86569e355a5987a36b73482746
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-06-01 20:27:22 +0000

    Copie de la 2.0.0 (fichier par fichier car je sais pas faire autrement avec SvnX)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@48335 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ actijour_2/2_1_0/exec/actijour_conf.php
@@ -0,0 +1,72 @@
+<?php
+/*
++--------------------------------------------+
+| ACTIVITE DU JOUR v. 2.0 - 06/2009 - SPIP 2.x
++--------------------------------------------+
+| H. AROUX . Scoty . koakidi.com
+| D. Chiche . pour la maj 2.0
+| Script certifie KOAK2.0 strict, mais si !
++--------------------------------------------+
+| Stats globales : pages, articles, visites.
+| Divers liens, avertissements ...
++--------------------------------------------+
+*/
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+include_spip('inc/presentation');
+
+function exec_actijour_conf() {
+
+# elements spip
+global 	$connect_statut,
+		$connect_toutes_rubriques,
+		$connect_id_auteur,
+		$couleur_claire, $couleur_foncee;
+
+#
+# function requises ...
+#
+include_spip("inc/actijour_init");
+include_spip('inc/form_config');
+
+#
+# affichage
+#
+$commencer_page = charger_fonction('commencer_page', 'inc');
+echo $commencer_page(_T('acjr:titre_actijour'), "suivi", "actijour_pg");
+echo "<a name='haut_page'></a>";
+
+
+# Vérifier si Admin principal du site
+if ($connect_statut != '0minirezo' OR !$connect_toutes_rubriques) {
+	echo _T('avis_non_acces_page');
+	echo fin_gauche(), fin_page();
+	exit;
+}
+
+echo debut_gauche("", true);
+	echo entete_page();
+	echo signature_plugin();
+
+
+echo debut_droite("", true);
+
+/*---------------------------------------------------------------------------*\
+Onglets pages sup.
+\*---------------------------------------------------------------------------*/
+	echo onglets_actijour(_request('exec'));
+
+/*---------------------------------------------------------------------------*\
+tableau de conf
+\*---------------------------------------------------------------------------*/
+	echo formulaire_configuration_acjr();
+
+
+# retour haut de page
+echo bouton_retour_haut();
+
+echo fin_gauche(), fin_page();
+
+} // fin exec
+?>
