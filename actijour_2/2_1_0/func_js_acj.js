fb974cd4013d813c2a9c119cc7ca676bd92ff2b1
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-06-01 20:27:43 +0000

    Copie de la 2.0.0 (fichier par fichier car je sais pas faire autrement avec SvnX)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@48336 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ actijour_2/2_1_0/func_js_acj.js
@@ -0,0 +1,17 @@
+/* 
++--------------------------------------------+
+| ACTIVITE DU JOUR v. 2.0 - 06/2009 - SPIP 2.x
++--------------------------------------------+
+| H. AROUX . Scoty . koakidi.com
+| D. Chiche . pour la maj 2.0
+| Script certifie KOAK2.0 strict, mais si !
++--------------------------------------------+
+| javascript ... popup pour bargraph actijour
++--------------------------------------------+
+*/
+function actijourpop(id)
+	{
+	document.getElementById(id).target = 'graph_article';
+	window.open('', 'graph_article','width=530,height=450,menubar=no,scrollbars=yes,resizable=yes');
+	if(neo.window.focus){neo.window.focus();}
+	}
