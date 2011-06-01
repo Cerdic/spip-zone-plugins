b397da5a549c08c430e9cb9426ccfff8530477e3
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-06-01 22:17:31 +0000

    branche 2.1.0 du plugin actijour
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@48361 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ actijour/branches/v210/exec/actijour_top.php
@@ -0,0 +1,101 @@
+<?php
+/*
++--------------------------------------------+
+| ACTIVITE DU JOUR v. 2.0 - 06/2009 - SPIP 2.x
++--------------------------------------------+
+| H. AROUX . Scoty . koakidi.com
+| D. Chiche . pour la maj 2.0
+| Script certifie KOAK2.0 strict, mais si !
++--------------------------------------------+
+| topTen articles : 8j, 30j, general
++--------------------------------------------+
+*/
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+include_spip('inc/presentation');
+include_spip('inc/statistiques');
+
+
+function exec_actijour_top() {
+
+// elements spip
+global 	$connect_statut,
+		$connect_toutes_rubriques,
+		$connect_id_auteur,
+		$couleur_claire, $couleur_foncee;
+
+//
+// function requises ...
+include_spip("inc/actijour_init");
+include_spip('inc/affiche_blocs');
+
+
+#
+# affichage
+#
+$commencer_page = charger_fonction('commencer_page', 'inc');
+echo $commencer_page(_T('acjr:titre_actijour'), "suivi", "actijour_pg");
+echo "<a name='haut_page'></a>";
+
+
+
+# Vérifier si Admin principal du site
+if ($connect_statut != '0minirezo' OR !$connect_toutes_rubriques) {
+	echo _T('avis_non_acces_page');
+	echo fin_gauche(), fin_page();
+	exit;
+}
+
+
+echo debut_gauche("", true);
+	echo entete_page();
+
+/*---------------------------------------------------------------------------*\
+info colonnes tableaux
+\*---------------------------------------------------------------------------*/
+	echo debut_boite_info(true);
+		echo _T('acjr:info_colonnes_topten')."\n";
+	echo fin_boite_info(true);
+
+/*---------------------------------------------------------------------------*\
+scoty signe son mefait
+\*---------------------------------------------------------------------------*/
+	echo signature_plugin();
+
+
+
+echo debut_droite("", true);
+
+/*---------------------------------------------------------------------------*\
+Onglets pages sup.
+\*---------------------------------------------------------------------------*/
+	echo onglets_actijour(_request('exec'));
+
+
+/*---------------------------------------------------------------------------*\
+classement des 10 articles les + visites sur 8 jours
+\*---------------------------------------------------------------------------*/
+	echo topten_articles_periode('8');
+
+
+/*---------------------------------------------------------------------------*\
+classement des 10 articles les + visites sur 30 jours
+\*---------------------------------------------------------------------------*/
+	echo topten_articles_periode('30');
+
+
+/*---------------------------------------------------------------------------*\
+classement des 10 articles les + visites
+\*---------------------------------------------------------------------------*/
+	echo topten_articles_global();
+
+
+
+# retour haut de page
+echo bouton_retour_haut();
+
+echo fin_gauche(), fin_page();
+
+} // fin fonction
+?>
---
+++ actijour/branches/v210/inc/form_config.php
@@ -0,0 +1,85 @@
+<?php
+/*
++--------------------------------------------+
+| ACTIVITE DU JOUR v. 2.0 - 06/2009 - SPIP 2.x
++--------------------------------------------+
+| H. AROUX . Scoty . koakidi.com
+| D. Chiche . pour la maj 2.0
+| Script certifie KOAK2.0 strict, mais si !
++--------------------------------------------+
+| formulaire de configuration
++--------------------------------------------+
+*/
+
+function formulaire_configuration_acjr() {
+
+	# elements spip
+	global 	$connect_statut,
+			$connect_toutes_rubriques,
+			$connect_id_auteur;
+	
+	# auteur connecte
+	$q=sql_select("nom FROM spip_auteurs WHERE id_auteur="._q($connect_id_auteur));
+	$r=sql_fetch($q);
+	$nom = typo($r['nom']);	
+
+	$aff='';
+	$aff.= "<p class='space_20'></p>";
+	
+	$aff.= "<form action='".generer_url_action('acjr_config')."' method='post'>"
+		. "<input type='hidden' name='redirect' value='".generer_url_ecrire("actijour_conf")."' />\n"
+		. "<input type='hidden' name='hash' value='".calculer_action_auteur("acjr_config-rien")."' />\n"
+		. "<input type='hidden' name='id_auteur' value='".$connect_id_auteur."' />\n"
+		. "<input type='hidden' name='arg' value='rien' />\n"
+		. "<input type='hidden' name='version_plug' value='".$GLOBALS['actijour_plug_version']."' />\n";
+		
+		
+	$aff.= debut_cadre_trait_couleur('',true,'',_T('acjr:configuration_commune'))
+		. "<table width='100%' cellpadding='2' cellspacing='0' border='0' class='table_conf'>"
+		. "<tr><td class='col_info'>"._T('acjr:conf_nbl_art')."</td>"
+		. "<td class='col_val'>"
+		. "<input type='text' name='nbl_art' value='".$GLOBALS['actijour']['nbl_art']."' size='4' class='fondl' />"
+		. "</td></tr>"
+		. "<tr><td class='col_info'>"._T('acjr:conf_nbl_aut')."</td>"
+		. "<td class='col_val'>"
+		. "<input type='text' name='nbl_aut' value='".$GLOBALS['actijour']['nbl_aut']."' size='4' class='fondl' />"
+		. "</td></tr>"
+		. "<tr><td class='col_info'>"._T('acjr:conf_nbl_mensuel')."</td>"
+		. "<td class='col_val'>"
+		. "<input type='text' name='nbl_mensuel' value='".$GLOBALS['actijour']['nbl_mensuel']."' size='4' class='fondl' />"
+		. "</td></tr>"
+		. "<tr><td class='col_info'>"._T('acjr:conf_nbl_topsem')."</td>"
+		. "<td class='col_val'>"
+		. "<input type='text' name='nbl_topsem' value='".$GLOBALS['actijour']['nbl_topsem']."' size='4' class='fondl' />"
+		. "</td></tr>"
+		. "<tr><td class='col_info'>"._T('acjr:conf_nbl_topmois')."</td>"
+		. "<td class='col_val'>"
+		. "<input type='text' name='nbl_topmois' value='".$GLOBALS['actijour']['nbl_topmois']."' size='4' class='fondl' />"
+		. "</td></tr>"
+		. "<tr><td class='col_info'>"._T('acjr:conf_nbl_topgen')."</td>"
+		. "<td class='col_val'>"
+		. "<input type='text' name='nbl_topgen' value='".$GLOBALS['actijour']['nbl_topgen']."' size='4' class='fondl' />"
+		. "</td></tr>"
+		. "</table>"
+		. fin_cadre_trait_couleur(true);
+	
+	$_ordon_pg_m = $GLOBALS['actijour']['admin-'.$connect_id_auteur]['ordon_pg_m'];
+	$ordon_pg_m = ($_ordon_pg_m)?$_ordon_pg_m:'1,2,3,4';
+
+	$aff.= debut_cadre_trait_couleur('',true,'',_T('acjr:configuration_perso', array('nom'=>$nom)))
+		. "<table width='100%' cellpadding='2' cellspacing='0' border='0' class='table_conf'>"
+		. "<tr><td class='col_info'>"._T('acjr:conf_ordon_milieu')."</td>"
+		. "<td class='col_val'>"
+		. "<input type='hidden' name='id_admin' value='$connect_id_auteur' />"
+		. "<input type='text' name='ordon_pg_m' value='".$ordon_pg_m."' size='8' class='fondl' />"
+		. "</td></tr>"
+		. "</table>"
+		. fin_cadre_trait_couleur(true);
+
+	$aff.= "<div align='right'><input type='submit' value='"._T('valider')."' class='fondo' /></div>\n"
+		. "</form>";
+	
+	return $aff;
+}
+
+?>
