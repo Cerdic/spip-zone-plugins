f6fea784344d7a47bdac4a0a507d53b3fcd36d6a
dwojylac@free.fr <dwojylac@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-06-02 16:45:18 +0000

    oups petite erreur de procédure... on supprime les js
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@48417 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- wdiaporama/wdiaporama.php
+++
@@ -1,7 +0,0 @@
-<?php
-function wdiaporama_inclure_headers($flux) {
-	$flux .= '<!-- plugin wdiaporama -->'."\n";
-	$flux .= '<script type="text/javascript" src="'.find_in_path('js/jquery.innerfade.js').'"></script>'."\n";
-	return $flux;
-}
-?>
