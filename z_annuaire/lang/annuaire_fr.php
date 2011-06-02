5ff4fa0d8bce06fafacc2999aef50e4139cfade6
pierrekuhn82@gmail.com <pierrekuhn82@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-06-02 18:23:12 +0000

    Mise ajour des fichiers de langue.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@48427 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- z_annuaire/lang/annuaire_fr.php
+++ z_annuaire/lang/annuaire_fr.php
@@ -1,40 +1,39 @@
 <?php
-// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_squelettes_/annuaire/lang/
+
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-	// A
-	'accueil_intro' => 'Bienvenue sur la page de pr&eacute;sentation du plugin "annuaire".',
+// A
+	'accueil_intro' => 'Bienvenue sur la page de présentation du plugin "annuaire".',
 	'accueil_option' => 'Certain plugins sont optionnel :',
-	'accueil_option_contact' => 'Le plugin "contact" permets ou non d\'affich&eacute; la page de contact et son r&eacute;glage dans le plugin.',
-	'accueil_option_couteau_suisse' => 'Le plugin "couteau suisse" vous fournis d&eacute;sorm&eacute; une lame pour cacher les adresses mails se trouvant dans les balises #LESAUTEURS et #DESCRIPTIF sur la page site.',
-	'accueil_option_pub' => 'Si vous souhaitez int&eacute;gr&eacute; dans votre site des pub google, il vous faudra cr&eacute;&eacute; les mots clefs "pub-entete" et "pub-nav" dans le groupe "Affichage" et plac&eacute; la br&egrave;ve dans une rubrique nomm&eacute; "squelette".',
+	'accueil_option_contact' => 'Le plugin "contact" permets ou non d\'affiché la page de contact et son réglage dans le plugin.',
+	'accueil_option_couteau_suisse' => 'Le plugin "couteau suisse" vous fournis désormé une lame pour cacher les adresses mails se trouvant dans les balises #LESAUTEURS et #DESCRIPTIF sur la page site.',
+	'accueil_option_pub' => 'Si vous souhaitez intégré dans votre site des pub google, il vous faudra créé les mots clefs "pub-entete" et "pub-nav" dans le groupe "Affichage" et placé la brève dans une rubrique nommé "squelette".',
 	'accueil_option_slogan' => 'Le plugin "slogan" permet de rajouter un slogan a votre site.',
-	'accueil_option_spiplistes' => 'Le plugin "spip liste" ou "clevermail" permetent ou non d\'affich&eacute;s la page abonnement et son r&eacute;glage dans le plugin.',
-	'accueil_option_visiteurs' => 'Le plugin "visiteur connect&eacute;s" ou la lame du couteau suisse permets d\'afficher dans la colonne de navigation (droite) le nombre de visiteur connect&eacute;s.',
-	'accueil_presentation' => 'Pr&eacute;sentation',
+	'accueil_option_spiplistes' => 'Le plugin "spip liste" ou "clevermail" permetent ou non d\'affichés la page abonnement et son réglage dans le plugin.',
+	'accueil_option_visiteurs' => 'Le plugin "visiteur connectés" ou la lame du couteau suisse permets d\'afficher dans la colonne de navigation (droite) le nombre de visiteur connectés.',
+	'accueil_presentation' => 'Présentation',
 	'accueil_requis' => 'Les requis pour un bon fonctionnement du site',
-	'accueil_requis_1' => 'Pour un bon r&eacute;f&eacute;rencement de votre site, ils vous faut cr&eacute;er :
+	'accueil_requis_1' => 'Pour un bon référencement de votre site, ils vous faut créer :
 					<br />un groupe de mot clef "_META"
-					<br />les mots clefs keywords, description et remplir le champ texte pour que votre site ce r&eacute;f&eacute;rence
-					<br />et &agrave; l\'inverse cr&eacute;&eacute; robots et le texte noindex, nofollow pour ne pas le r&eacute;f&eacute;renc&eacute; ',
-	'accueil_requis_2' => 'Pour afficher des partenaires sur la page "Partenaire", il vous faut cr&eacute;&eacute; le groupe "Affichage" avec le mot clef "partenaire" et attibuer le &agrave; chacun des sites voulus.',
+					<br />les mots clefs keywords, description et remplir le champ texte pour que votre site ce référence
+					<br />et à l\'inverse créé robots et le texte noindex, nofollow pour ne pas le référencé ',
+	'accueil_requis_2' => 'Pour afficher des partenaires sur la page "Partenaire", il vous faut créé le groupe "Affichage" avec le mot clef "partenaire" et attibuer le à chacun des sites voulus.',
 	'actuellement' => 'Il y a actuellement ',
 	'aff_date' => 'Afficher la date',
 	'aff_date_expli' => 'Souhaitez-vous afficher la date du jour sur le site ?',
 	'affichage' => 'L\'affichage',
 	'afficher_colonne_nav' => 'Vous avez choisi de l\'afficher dans la navigation.',
 	'ajouter_commentaire' => 'Ajouter un commentaire',
-	'ajouter_google' => 'ajouter &agrave; google',
+	'ajouter_google' => 'ajouter à google',
 	'annuaire' => 'annuaire',
 	'annuaire_abonnement' => 'Page abonnement',
 	'annuaire_accueil' => 'Accueil',
 	'annuaire_bandeau' => 'Bandeau',
 	'annuaire_colonne' => 'Colonne et pied',
 	'annuaire_contact' => 'Page contact',
-	'annuaire_general' => 'G&eacute;n&eacute;ralit&eacute;s',
+	'annuaire_general' => 'Généralités',
 	'annuaire_gratuit' => 'Annuaire gratuit de liens en dur',
 	'annuaire_herbier' => 'Page herbier',
 	'annuaire_menu' => 'Menu du site',
@@ -44,32 +43,28 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'annuaire_rubrique' => 'Page rubrique',
 	'annuaire_site' => 'Page site',
 	'annuaire_sommaire' => 'Page sommaire',
-	'article_syndic' => '1 article syndiqu&eacute;',
 	'articles' => '@nb@ articles',
-	'articles_syndic' => '@nb@ articles syndiqu&eacute;s',
-
-	// B
-	'boucles' => 'R&eacute;glages des boucles',
+// B
+	'boucles' => 'Réglages des boucles',
 	'bouton_configurer' => 'Configurer l\'annuaire',
-
-	// C
-	'cache_titre' => 'Cach&eacute; le titre',
-	'cache_titre_news' => 'de la boucle "nouveaut&eacute;"',
-	'cache_titre_plus_votes_defaut' => 'de la boucle "plus vot&eacute;s"',
-	'cache_titre_plus_votes_vertical' => 'de la boucle "plus vot&eacute;s" (cadres verticaux)',
+// C
+	'cache_titre' => 'Caché le titre',
+	'cache_titre_news' => 'de la boucle "nouveauté"',
+	'cache_titre_plus_votes_defaut' => 'de la boucle "plus votés"',
+	'cache_titre_plus_votes_vertical' => 'de la boucle "plus votés" (cadres verticaux)',
 	'cache_titre_zard' => 'de la boucle "hasard"',
 	'conception' => 'conception de l\'annuaire',
-	'config_boucle_vertical' => 'Affichage des r&eacute;sultats en vertical (tableau)',
+	'config_boucle_vertical' => 'Affichage des résultats en vertical (tableau)',
 	'config_boucle_zard' => 'Affichage de la boucle hasard',
 	'config_boucle_zard_expli' => 'Vous pouvez l\'afficher soit sur la page sommaire soit dans la navigation et dans ce cas on affiche pas les noisettes.',
 	'config_cache_boucle' => 'Ne pas afficher la boucle',
 	'config_defaut_boucle' => 'Affichage par defaut de la boucle.',
-	'config_defaut_inclure' => 'Affichage par defaut des noisettes.',
+	'config_defaut_inclure' => 'Afficher les inclures',
 	'config_defaut_menu_lien' => 'Afficher le lien.',
 	'config_non_menu_lien' => 'Ne pas afficher le lien.',
 	'configuration_bandeau' => 'configuration du "bandeau"',
 	'configuration_colonne_pied' => 'configuration de la "colonne et du pied"',
-	'configuration_general' => 'configuration des g&eacute;n&eacute;ralit&eacute;s du site',
+	'configuration_general' => 'configuration des généralités du site',
 	'configuration_herbier' => 'configuration de la page "herbier"',
 	'configuration_menu' => 'configuration des liens du menu',
 	'configuration_mot' => 'configuration de la page "mot"',
@@ -79,60 +74,56 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'configuration_site' => 'configuration de la page "site"',
 	'configuration_sommaire' => 'configuration de la page "sommaire"',
 
-	// D
-	'descriptif_accueil' => 'Pr&eacute;sentation du plugins "annuaire" et de ses fonctions.',
-	'descriptif_bandeau' => '<h4>R&eacute;glage du bandeau</h4>
-				<br />R&eacute;gl&eacute; vos styles sur le bandeau.',
-	'descriptif_colonne' => '<h4>R&eacute;glage du style</h4>
+// D
+	'descriptif_accueil' => 'Présentation du plugins "annuaire" et de ses fonctions.',
+	'descriptif_bandeau' => '<h4>Réglage du bandeau</h4>
+				<br />Réglé vos styles sur le bandeau.',
+	'descriptif_colonne' => '<h4>Réglage du style</h4>
 				<br />Choisissez vos couleurs pour les colonnes et pied de page.',
-	'descriptif_general' => '<h4>R&eacute;glages g&eacute;n&eacute;raux</h4>
-				<br />Une page qui regroupe tous les r&eacute;glages g&eacute;n&eacute;raux du site.',
+	'descriptif_general' => '<h4>Réglages généraux</h4>
+				<br />Une page qui regroupe tous les réglages généraux du site.',
 	'descriptif_herbier' => '<h4>R&eacute;glage de l\'herbier</h4>
 				<br />R&eacute;gl&eacute; vos boucles et styles sur l\'herbier.
 				<p />C\'est la page où l\'on affiche l\'actualiter des sites syndiqu&eacute;',
-	'descriptif_menu' => '<h4>R&eacute;glage du menu</h4>
-				<br />Afficher ou non les liens vers les pages du site, cette configuration agit sur le menu de l\'ent&ecirc;tete et le pied de page.',
-	'descriptif_mot' => '<h4>R&eacute;glage de la page mot</h4>
-				<br />R&eacute;gl&eacute; vos boucles et styles sur la page mot.',
-	'descriptif_partenaire' => '<h4>R&eacute;glage de la page partenaire</h4>
-				<br />R&eacute;gl&eacute; votre boucle et vos style sur la page partenaire.',
-	'descriptif_recherche' => '<h4>R&eacute;glage de la page recherche</h4>
-				<br />R&eacute;gl&eacute; vos boucles et styles sur la page recherche.',
-	'descriptif_rubrique' => '<h4>R&eacute;glage de la page rubrique</h4>
-				<br />R&eacute;gl&eacute; vos boucles et styles sur la page rubrique.',
-	'descriptif_site' => '<h4>R&eacute;glage de la page site</h4>
-				<br />R&eacute;gl&eacute; vos boucles et styles sur la page site.',
-	'descriptif_sommaire' => '<h4>R&eacute;glage sur le sommaire</h4>
-				<br />R&eacute;gl&eacute; vos boucles et styles du sommaire.',
+	'descriptif_menu' => '<h4>Réglage du menu</h4>
+				<br />Afficher ou non les liens vers les pages du site, cette configuration agit sur le menu de l\'entêtete et le pied de page.',
+	'descriptif_mot' => '<h4>Réglage de la page mot</h4>
+				<br />Réglé vos boucles et styles sur la page mot.',
+	'descriptif_partenaire' => '<h4>Réglage de la page partenaire</h4>
+				<br />Réglé votre boucle et vos style sur la page partenaire.',
+	'descriptif_recherche' => '<h4>Réglage de la page recherche</h4>
+				<br />Réglé vos boucles et styles sur la page recherche.',
+	'descriptif_rubrique' => '<h4>Réglage de la page rubrique</h4>
+				<br />Réglé vos boucles et styles sur la page rubrique.',
+	'descriptif_site' => '<h4>Réglage de la page site</h4>
+				<br />Réglé vos boucles et styles sur la page site.',
+	'descriptif_sommaire' => '<h4>Réglage sur le sommaire</h4>
+				<br />Réglé vos boucles et styles du sommaire.',
 
-	// E
-	'editer_site' => '&Eacute;diter le site',
-	'email_lesauteurs:description' => 'Sur les balises #LESAUTEURS et #DESCRIPTIF de la page "site" nous coupons les adresses mail si elles existent afin d&rsquo;&eacute;vit&eacute; que le webmaster nous dise qu&rsquo;on le spam.',
-	'email_lesauteurs:nom' => 'Email dans les flux rss',
-	'epingle_site' => '&eacute;pingl&eacute;s sur',
+// E
+	'editer_site' => 'Éditer le site',
+	'epingle_site' => 'épinglés sur',
 	'espace_membre_gestion_sites' => 'Espace Membres Gestions des sites',
-	'est_rattache' => 'est rattach&eacute; &agrave;',
+	'est_rattache' => 'est rattaché à',
+// F
 
-	// G
+// G
 	'groupe_mots' => 'Groupes de mots',
-	'groupe_mots_expli' => 'Remplir avec le titre des groupes de mots clef, s&eacute;par&eacute; par des virgules, que vous ne souhaitez pas afficher dans la colonne de droite.',
-
-	// H
+	'groupe_mots_expli' => 'Remplir avec le titre des groupes de mots clef, séparé par des virgules, que vous ne souhaitez pas afficher dans la colonne de droite.',
+// H
 	'hasard' => 'Sites au hasard',
-	'hebergement' => 'Indiquer la soci&eacute;t&eacute;e ou la personne qui vous h&eacute;berge : (afficher colonne de droite en bas uniquement si remplis), possibilit&eacute;e de mettre du code spip ou html.',
-	'hebergeur' => 'H&eacute;bergeur du site :',
+	'hebergement' => 'Indiquer la sociétée ou la personne qui vous héberge : (afficher colonne de droite en bas uniquement si remplis), possibilitée de mettre du code spip ou html.',
+	'hebergeur' => 'Hébergeur du site :',
 
-	// I
-	'infos' => 'Afficher une infos sur toutes les rubriques (si vous la souhaitez diff&eacute;rentes utiliser la description.)',
-
-	// L
-	'laissez_commentaire' => 'Laisser un commentaire',
+// I
+	'infos' => 'Afficher une infos sur toutes les rubriques (si vous la souhaitez différentes utiliser la description.)',
+// L
+	'laissez_commentaire' => 'Laissez un commentaire',
 	'le_mot' => 'Le mot',
 	'le_site' => 'Le site compte',
 	'lire_suite' => 'Lire la suite',
 
-	// M
-
+// M
 	'menu_lien_abonnement' => 'Lien vers la page abonnement',
 	'menu_lien_afficher' => 'Lien vers les pages du menu',
 	'menu_lien_contact' => 'Lien vers la page contact',
@@ -141,50 +132,41 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'menu_lien_plan_complet' => 'Lien vers toutes les rubriques',
 	'menu_lien_sommaire' => 'Lien vers l\'accueil (sommaire)',
 	'miniatures_par' => 'Miniatures par',
-
-	// N
+// N
 	'nbr_articles' => 'Nombre d\'articles :',
 	'nbr_sites' => 'Nombre de sites :',
 	'non' => 'Non',
 	'nous_avons' => 'Nous avons',
 	'nouveau_site' => 'Nouveau site',
-	'nouveaute' => 'Site nouveaut&eacute;',
-
-	// O
+	'nouveaute' => 'Site nouveauté',
+// O
 	'oui' => 'Oui',
-
-	// P
+// P
 	'partenaire' => 'partenaire',
-	'partenaires' => '@nb@ partenaires',
 	'pas_commentaire' => 'Pas encore de commentaire',
-	'pas_descriptif' => 'Nous n\'avons pas de descriptif pour ce site, si vous &ecirc;tes le webmaster vous pouvez nous contacter afin de proposer un descriptif au site.',
-	'pas_le_droit_ecrire' => 'Vous n\'avez pas d\'autorisation en &eacute;crire',
-	'pas_pagination' => 'Nombre d\'&eacute;l&eacute;ments par boucle',
-	'pas_partenaire' => 'Nous n\'avons pas de partenaire(s) &agrave; ce jour, si vous le souhaitez, rejoingnez-nous...',
+	'pas_descriptif' => 'Nous n\'avons pas de descriptif pour ce site, si vous êtes le webmaster vous pouvez nous contacter afin de proposer un descriptif au site.',
+	'pas_le_droit_ecrire' => 'Vous n\'avez pas d\'autorisation en écrire',
+	'pas_pagination' => 'Nombre d\'éléments par boucle',
+	'pas_partenaire' => 'Nous n\'avons pas de partenaire(s) à ce jour, si vous le souhaitez, rejoingnez-nous...',
 	'pas_votes' => 'Pas encore de votes',
 	'pas_zard' => 'Pas de site(s) au hasard, enregistrez-en ...',
-	'plus_votes' => 'Sites les plus vot&eacute;s',
-	'popu_site' => 'La popularit&eacute; du site est de ',
+	'plus_votes' => 'Sites les plus votés',
+	'popu_site' => 'La popularité du site est de ',
+// R
+	'resultats' => 'Résultats dans les boucles',
+	'rss' => 'Nombres d\'articles syndiqué par page.',
 
-	// R
-	'resultats' => 'R&eacute;sultats dans les boucles',
-	'rss' => 'Nombres d\'articles syndiqu&eacute; par page.',
-
-	// S
-	'site' => '1 site',
-	'site1' => 'de "site"',
-	'sites' => '@nb@ sites',
+// S
 	'style' => 'Style :',
-	'styliste' => 'Indiquer la personne responsable la charte graphique : (afficher colonne de droite en bas uniquement si remplis), possibilit&eacute;e de mettre du code spip ou html.',
-	'synthese_flux' => 'Synth&egrave;se des flux',
-
-	// T
+	'styliste' => 'Indiquer la personne responsable la charte graphique : (afficher colonne de droite en bas uniquement si remplis), possibilitée de mettre du code spip ou html.',
+	'synthese_flux' => 'Synthèse des flux',
+// T
 	'toutes_rubriques' => 'Toutes les rubriques',
-
-	// V
+// U
+// V
 	'vote' => '1 vote',
 	'votes' => '@nb@ votes',
 
-);
 
+);
 ?>
