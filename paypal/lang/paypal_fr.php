196415cda360feec783fb8da310215e727a2c758
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-06-02 08:01:26 +0000

    langues (paypal)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@48406 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- paypal/lang/paypal_fr.php
+++ paypal/lang/paypal_fr.php
@@ -1,43 +1,48 @@
 <?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/paypal/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-// Configuration
-	'configuration_paypal' => "Paypal",
-	'description_paypal' => "Configurer le systeme de paiement en ligne Paypal",
 
-	'api_test' => 'API de test',
+	// A
 	'api_prod' => 'API de production',
-	
-	'label_soumission' => 'Soumission du formulaire',
-	'explication_soumission' => 'Sur quelle page de paypal est soumis le formulaire ?',
-	
-	'label_identifiant_vendeur' => 'Identifiant du vendeur',
-	'explication_identifiant_vendeur' => 'Correspond &agrave; l\'adresse email du compte du vendeur',
+	'api_test' => 'API de test',
 
-	'label_environnement' => 'Environnement',
-	'explication_environnement' => "Vous pouvez indiquer dans ce formulaire des donn&eacute;es
+	// C
+	'configuration_paypal' => 'Paypal',
+
+	// D
+	'description_paypal' => 'Configurer le systeme de paiement en ligne Paypal',
+
+	// E
+	'explication_account' => 'Correspond &agrave; l\'adresse email du compte du vendeur',
+	'explication_environnement' => 'Vous pouvez indiquer dans ce formulaire des donn&eacute;es
 		qui concernent le site de test de Paypal (Sandbox, pour le d&eacute;veloppement)
 		ainsi que celles valables uniquement pour de vraies transactions.
-		Quel environnement souhaitez-vous utiliser ?",
-	'label_environnement_test' => 'Environnement de test',
-	'label_environnement_prod' => 'Environnement de production',
+		Quel environnement souhaitez-vous utiliser ?',
+	'explication_identifiant_vendeur' => 'Correspond &agrave; l\'adresse email du compte du vendeur',
+	'explication_soumission' => 'Sur quelle page de paypal est soumis le formulaire ?',
+	'explication_tax' => '&Eacute;ventuellement la valeur de la taxe qui doit s\'appliquer (exemple pour TVA : 19.60)',
 
+	// I
+	'info_bouton_paypal' => 'Effectuez vos paiements via PayPal :
+		une solution rapide, gratuite et s&eacute;curis&eacute;e',
+
+	// L
+	'label_account' => 'Account (email)',
 	'label_currency_code' => 'Devise',
 	'label_currency_code_eur' => 'Euro (&euro;)',
 	'label_currency_code_usd' => 'Dollar ($)',
-
-	'label_tax' => 'Taxes',	
-	'explication_tax' => '&Eacute;ventuellement la valeur de la taxe qui doit s\'appliquer (exemple pour TVA : 19.60)',
-	
-	'label_account' => 'Account (email)',
-	'explication_account' => 'Correspond &agrave; l\'adresse email du compte du vendeur',
-	
-	'label_username' => 'API Username (idenfiant)',
+	'label_environnement' => 'Environnement',
+	'label_environnement_prod' => 'Environnement de production',
+	'label_environnement_test' => 'Environnement de test',
+	'label_identifiant_vendeur' => 'Identifiant du vendeur',
 	'label_password' => 'API Password (password)',
 	'label_signature' => 'Signature ',
-
-	'info_bouton_paypal' => "Effectuez vos paiements via PayPal :
-		une solution rapide, gratuite et s&eacute;curis&eacute;e",
-	
-
+	'label_soumission' => 'Soumission du formulaire',
+	'label_tax' => 'Taxes',
+	'label_username' => 'API Username (idenfiant)'
 );
+
 ?>
