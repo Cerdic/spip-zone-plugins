c1effc8d5482829a9e2a849baa0898df1a022420
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-06-02 08:01:25 +0000

    langues (paniers)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@48405 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- paniers/lang/paniers_fr.php
+++ paniers/lang/paniers_fr.php
@@ -1,26 +1,32 @@
 <?php
-
-// Sécurité
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/paniers/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
 	'action_ajouter' => 'Ajouter au panier',
+
+	// M
 	'mon_panier' => 'Mon panier',
 	'mon_panier_modifier' => 'Modifier mon panier',
 	'mon_panier_voir' => 'Voir mon panier',
+
+	// P
 	'panier_description' => 'Description',
 	'panier_erreur_quantites' => 'Les quantités doivent être des nombres entiers.',
 	'panier_montant' => 'Montant',
+	'panier_prix_unitaire' => 'Prix unitaire',
 	'panier_quantite' => 'Quantité',
 	'panier_quantite_ok' => 'Les quantités ont bien été modifiées.',
-	'panier_prix_unitaire' => 'Prix unitaire',
 	'panier_recalculer' => 'Recalculer',
 	'panier_taxes' => 'Dont taxes',
 	'panier_total_ht' => 'Total hors taxe',
 	'panier_total_ttc' => 'Total',
 	'panier_valider' => 'Valider mon panier',
 	'panier_vide' => 'Le panier est vide.',
-	'paniers' => 'Paniers',
+	'paniers' => 'Paniers'
 );
 
 ?>
