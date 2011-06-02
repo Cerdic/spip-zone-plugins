8d1eed3ad489c8649cb08c8657983fca0d62b085
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-06-02 08:01:22 +0000

    langues (coordonnees)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@48404 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- coordonnees/lang/coordonnees_en.php
+++ coordonnees/lang/coordonnees_en.php
@@ -1,77 +1,81 @@
 <?php
-
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+
 	// A
-	'adresses' => 'Addresses',
-	'adresse_pro' => 'Professional',
 	'adresse_perso' => 'Home',
+	'adresse_pro' => 'Professional',
+	'adresses' => 'Addresses',
 	'ajouter_adresse' => 'Add an address',
-	'ajouter_telephone' => 'Add a phone number',
 	'ajouter_email' => 'Add an email',
+	'ajouter_telephone' => 'Add a phone number',
 
 	// C
-	'confirmer_suppression_adresse' => "Are you sure you want to detele this address ?",
-	'confirmer_suppression_numero' => "Are you sure you want to detele this phone number ?",
-	'confirmer_suppression_email' => "Are you sure you want to detele this email ?",
-	
+	'confirmer_suppression_adresse' => 'Are you sure you want to detele this address ?',
+	'confirmer_suppression_email' => 'Are you sure you want to detele this email ?',
+	'confirmer_suppression_numero' => 'Are you sure you want to detele this phone number ?',
+
 	// E
-	'emails' => 'Emails',
 	'editer_adresse' => 'Edit an address',
-	'editer_numero' => 'Edit a phone number',
 	'editer_email' => 'Edit an email',
-	'explication_type_adresse' => "Type can be private or work",
-	'explication_type_numero' => "Type can be private, work or fax",
-	'explication_type_email' => "Type can be private or profesionnal",
-	
+	'editer_numero' => 'Edit a phone number',
+	'emails' => 'Emails',
+	'explication_type_adresse' => 'Type can be private or work',
+	'explication_type_email' => 'Type can be private or profesionnal',
+	'explication_type_numero' => 'Type can be private, work or fax',
+
 	// F
 	'fax' => 'Fax.',
-	'mobile' => 'Mobile',
 
 	// I
-	'item_nouvelle_adresse' => 'New address',
+	'info_adresse_utilisee_par' => 'This address used by :',
+	'info_email_utilise_par' => 'This email is used by :',
+	'info_gauche_numero_adresse' => 'Address id',
+	'info_gauche_numero_email' => 'Email id',
+	'info_gauche_numero_numero' => 'Number id',
+	'info_numero_utilise_par' => 'This phone number is used by :',
 	'item_nouveau_numero' => 'New phone number',
 	'item_nouvel_email' => 'New email',
-	'info_adresse_utilisee_par' => "This address used by :",
-	'info_numero_utilise_par' => "This phone number is used by :",
-	'info_email_utilise_par' => "This email is used by :",
-	'info_gauche_numero_adresse' => 'Address id',
-	'info_gauche_numero_numero' => 'Number id',
-	'info_gauche_numero_email' => 'Email id',
-	
+	'item_nouvelle_adresse' => 'New address',
+
 	// L
-	'label_boite_postale' => "Postal box",
-	'label_code_postal' => "Postal code",
-	'label_complement' => "Additional information",
-	'label_numero' => "Phone number",
-	'label_email' => "Email",
-	'label_type_adresse' => "Address type",
-	'label_type_numero' => "Type of phone number",
-	'label_type_email' => "Type of email",
-	'label_pays' => "Country",
-	'label_ville' => "City",
-	'label_voie' => "Address",
-	
-	// N
-	'numeros' => 'Phone numbers',
-	'nouvelle_adresse' => 'New address',
-	'nouveau_numero' => 'New phone number',
-	'nouvel_email' => 'New email',
+	'label_boite_postale' => 'Postal box',
+	'label_code_postal' => 'Postal code',
+	'label_complement' => 'Additional information',
+	'label_email' => 'Email',
+	'label_numero' => 'Phone number',
+	'label_pays' => 'Country',
+	'label_titre' => 'Titre', # NEW
+	'label_type_adresse' => 'Address type',
+	'label_type_email' => 'Type of email',
+	'label_type_numero' => 'Type of phone number',
+	'label_ville' => 'City',
+	'label_voie' => 'Address',
 
 	// M
-	'modifier_adresse' => "Edit this address",
-	'modifier_numero' => "Edit this phone number",
-	'modifier_email' => "Edit this email",
+	'mobile' => 'Mobile',
+	'modifier_adresse' => 'Edit this address',
+	'modifier_email' => 'Edit this email',
+	'modifier_numero' => 'Edit this phone number',
+
+	// N
+	'nouveau_numero' => 'New phone number',
+	'nouvel_email' => 'New email',
+	'nouvelle_adresse' => 'New address',
+	'numeros' => 'Phone numbers',
 
 	// S
-	'supprimer_adresse' => "Delette this address",
-	'supprimer_numero' => "Delete this phone number",
-	'supprimer_email' => "Delete this email",
-	
-	// T
-	'titre_coordonnees'	=> 'Address book',
+	'supprimer_adresse' => 'Delette this address',
+	'supprimer_email' => 'Delete this email',
+	'supprimer_numero' => 'Delete this phone number',
 
+	// T
+	'titre_coordonnees' => 'Address book'
 );
 
 ?>
--- coordonnees/lang/coordonnees_fr.php
+++ coordonnees/lang/coordonnees_fr.php
@@ -1,78 +1,79 @@
 <?php
-
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/coordonnees/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+
 	// A
-	'adresses' => 'Adresses',
-	'adresse_pro' => 'Professionnel',
 	'adresse_perso' => 'Domicile',
+	'adresse_pro' => 'Professionnel',
+	'adresses' => 'Adresses',
 	'ajouter_adresse' => 'Ajouter une adresse',
-	'ajouter_telephone' => 'Ajouter un t&eacute;l&eacute;phone',
 	'ajouter_email' => 'Ajouter un email',
+	'ajouter_telephone' => 'Ajouter un t&eacute;l&eacute;phone',
 
 	// C
-	'confirmer_suppression_adresse' => "Voulez-vous vraiment supprimer cette adresse ?",
-	'confirmer_suppression_numero' => "Voulez-vous vraiment supprimer ce num&eacute;ro ?",
-	'confirmer_suppression_email' => "Voulez-vous vraiment supprimer ce courriel ?",
-	
+	'confirmer_suppression_adresse' => 'Voulez-vous vraiment supprimer cette adresse ?',
+	'confirmer_suppression_email' => 'Voulez-vous vraiment supprimer ce courriel ?',
+	'confirmer_suppression_numero' => 'Voulez-vous vraiment supprimer ce num&eacute;ro ?',
+
 	// E
-	'emails' => 'Courriels',
 	'editer_adresse' => '&Eacute;diter une adresse',
-	'editer_numero' => '&Eacute;diter un num&eacute;ro',
 	'editer_email' => '&Eacute;diter un courriel',
-	'explication_type_adresse' => "Le type peut être 'perso' ou 'pro'.",
-	'explication_type_numero' => "Le type peut être 'perso' ou 'pro' ou 'fax'.",
-	'explication_type_email' => "Le type peut être 'perso' ou 'pro'.",
-	
+	'editer_numero' => '&Eacute;diter un num&eacute;ro',
+	'emails' => 'Courriels',
+	'explication_type_adresse' => 'Le type peut être \'perso\' ou \'pro\'.',
+	'explication_type_email' => 'Le type peut être \'perso\' ou \'pro\'.',
+	'explication_type_numero' => 'Le type peut être \'perso\' ou \'pro\' ou \'fax\'.',
+
 	// F
 	'fax' => 'Fax.',
-	'mobile' => 'Mobile',
 
 	// I
-	'item_nouvelle_adresse' => 'Nouvelle adresse',
+	'info_adresse_utilisee_par' => 'Adresse utilis&eacute;e par :',
+	'info_email_utilise_par' => 'Courriel utilis&eacute; par :',
+	'info_gauche_numero_adresse' => 'N° Adresse',
+	'info_gauche_numero_email' => 'N° Email',
+	'info_gauche_numero_numero' => 'N° Num&eacute;ro',
+	'info_numero_utilise_par' => 'Num&eacute;ro utilis&eacute; par :',
 	'item_nouveau_numero' => 'Nouveau num&eacute;ro',
 	'item_nouvel_email' => 'Nouveau courriel',
-	'info_adresse_utilisee_par' => "Adresse utilis&eacute;e par :",
-	'info_numero_utilise_par' => "Num&eacute;ro utilis&eacute; par :",
-	'info_email_utilise_par' => "Courriel utilis&eacute; par :",
-	'info_gauche_numero_adresse' => 'N° Adresse',
-	'info_gauche_numero_numero' => 'N° Num&eacute;ro',
-	'info_gauche_numero_email' => 'N° Email',
-	
+	'item_nouvelle_adresse' => 'Nouvelle adresse',
+
 	// L
-	'label_complement' => "Compl&eacute;ment",
-	'label_boite_postale' => "Boîte Postale",
-	'label_code_postal' => "Code Postal",
-	'label_numero' => "Num&eacute;ro",
-	'label_email' => "Courriel",
+	'label_boite_postale' => 'Boîte Postale',
+	'label_code_postal' => 'Code Postal',
+	'label_complement' => 'Compl&eacute;ment',
+	'label_email' => 'Courriel',
+	'label_numero' => 'Num&eacute;ro',
+	'label_pays' => 'Pays',
 	'label_titre' => 'Titre',
-	'label_type_adresse' => "Type d'adresse",
-	'label_type_numero' => "Type de num&eacute;ro",
-	'label_type_email' => "Type de courriel",
-	'label_pays' => "Pays",
-	'label_ville' => "Ville",
-	'label_voie' => "Adresse",
-	
-	// N
-	'numeros' => 'Num&eacute;ros',
-	'nouvelle_adresse' => 'Nouvelle adresse',
-	'nouveau_numero' => 'Nouveau num&eacute;ro',
-	'nouvel_email' => 'Nouveau courriel',
+	'label_type_adresse' => 'Type d\'adresse',
+	'label_type_email' => 'Type de courriel',
+	'label_type_numero' => 'Type de num&eacute;ro',
+	'label_ville' => 'Ville',
+	'label_voie' => 'Adresse',
 
 	// M
-	'modifier_adresse' => "Modifier cette adresse",
-	'modifier_numero' => "Modifier ce num&eacute;ro",
-	'modifier_email' => "Modifier ce courriel",
+	'mobile' => 'Mobile',
+	'modifier_adresse' => 'Modifier cette adresse',
+	'modifier_email' => 'Modifier ce courriel',
+	'modifier_numero' => 'Modifier ce num&eacute;ro',
+
+	// N
+	'nouveau_numero' => 'Nouveau num&eacute;ro',
+	'nouvel_email' => 'Nouveau courriel',
+	'nouvelle_adresse' => 'Nouvelle adresse',
+	'numeros' => 'Num&eacute;ros',
 
 	// S
-	'supprimer_adresse' => "Supprimer cette adresse",
-	'supprimer_numero' => "Supprimer ce num&eacute;ro",
-	'supprimer_email' => "Supprimer ce courriel",
-	
-	// T
-	'titre_coordonnees'	=> 'Coordonn&eacute;es',
+	'supprimer_adresse' => 'Supprimer cette adresse',
+	'supprimer_email' => 'Supprimer ce courriel',
+	'supprimer_numero' => 'Supprimer ce num&eacute;ro',
 
+	// T
+	'titre_coordonnees' => 'Coordonn&eacute;es'
 );
 
 ?>
