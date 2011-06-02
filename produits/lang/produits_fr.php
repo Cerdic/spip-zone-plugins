ee8e2361be92a8341340b0d9db46295612490847
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-06-02 08:01:32 +0000

    langues (produits)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@48408 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- produits/lang/produits_fr.php
+++ produits/lang/produits_fr.php
@@ -1,30 +1,36 @@
 <?php
-
-// Sécurité
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/produits/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-'configurer_taxe_defaut_label' => 'Taxe par d&eacute;faut',
-'configurer_titre' => 'Configuration des produits',
-'info_1_produit' => 'Un produit',
-'info_aucun_produit' => 'Aucun produit',
-'info_nb_produits' => '@nb@ produits',
-'produit_bouton_ajouter' => 'Ajouter un produit',
-'produit_champ_descriptif_label' => 'Court descriptif',
-'produit_champ_prix_ht_label' => 'Prix HT',
-'produit_champ_reference_label' => 'R&eacute;f&eacute;rence',
-'produit_champ_rubrique_label' => 'Rubrique',
-'produit_champ_texte_label' => 'Texte',
-'produit_champ_titre_label' => 'Titre',
-'produit_champ_taxe_label' => 'Taxe',
-'produit_champ_taxe_explication' => 'Valeur par défaut sur le site : @taxe@. Ce champ peut être laissé vide.',
-'produit_editer' => '&Eacute;diter le produit',
-'produit_modifier' => 'Modifier le produit :',
-'produit_nouveau' => 'Nouveau produit',
-'produit_numero' => 'Produit n°',
-'produit_reference' => 'R&eacute;f&eacute;rence "@reference@"',
-'produit_statut' => 'Modifier le statut :',
-'produits_titre' => 'Produits',
+
+	// C
+	'configurer_taxe_defaut_label' => 'Taxe par d&eacute;faut',
+	'configurer_titre' => 'Configuration des produits',
+
+	// I
+	'info_1_produit' => 'Un produit',
+	'info_aucun_produit' => 'Aucun produit',
+	'info_nb_produits' => '@nb@ produits',
+
+	// P
+	'produit_bouton_ajouter' => 'Ajouter un produit',
+	'produit_champ_descriptif_label' => 'Court descriptif',
+	'produit_champ_prix_ht_label' => 'Prix HT',
+	'produit_champ_reference_label' => 'R&eacute;f&eacute;rence',
+	'produit_champ_rubrique_label' => 'Rubrique',
+	'produit_champ_taxe_explication' => 'Valeur par défaut sur le site : @taxe@. Ce champ peut être laissé vide.',
+	'produit_champ_taxe_label' => 'Taxe',
+	'produit_champ_texte_label' => 'Texte',
+	'produit_champ_titre_label' => 'Titre',
+	'produit_editer' => '&Eacute;diter le produit',
+	'produit_modifier' => 'Modifier le produit :',
+	'produit_nouveau' => 'Nouveau produit',
+	'produit_numero' => 'Produit n°',
+	'produit_reference' => 'R&eacute;f&eacute;rence "@reference@"',
+	'produit_statut' => 'Modifier le statut :',
+	'produits_titre' => 'Produits'
 );
 
 ?>
