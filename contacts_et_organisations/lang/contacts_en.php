71a42051d4da88099a38ffc4b77b049c3cf54d61
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-06-02 08:01:21 +0000

    langues (contacts)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@48403 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- contacts_et_organisations/lang/contacts_en.php
+++ contacts_et_organisations/lang/contacts_en.php
@@ -1,86 +1,139 @@
 <?php
-
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'aucun_contact' => 'Il n\'y a aucun contact !', # NEW
+	'aucune_organisation' => 'Il n\'y a aucune organisation !', # NEW
+
 	// B
-	'bouton_rechercher' => "Search",
 	'bouton_contacts' => 'Contacts',
+	'bouton_contacts_organisations' => 'Contacts & Organisations', # NEW
 	'bouton_organisations' => 'Organisations',
-	
+	'bouton_rechercher' => 'Search',
+	'bouton_repertoire' => 'Répertoire', # NEW
+
 	// C
+	'changer' => 'Changer', # NEW
+	'chercher_contact' => 'Chercher', # NEW
+	'chercher_organisation' => 'Chercher', # NEW
+	'chercher_statut' => 'Statut', # NEW
+	'confirmer_delier_contact' => 'Are you sure you want to unlink this organisation from this contact ?',
+	'confirmer_delier_organisation' => 'Are you sure you want to unlink this contact from this organisation ?',
+	'confirmer_supprimer_contact' => 'Ares you sure you want to delete all information about this contact ?',
+	'confirmer_supprimer_organisation' => 'Are you sure you want to delete all the information about this organisation ?',
+	'contact' => 'Un contact', # NEW
+	'contact_associe_a_auteur_numero' => 'Associée à auteur numéro', # NEW
+	'contact_associer_a_auteur' => 'Associer à un auteur', # NEW
+	'contact_creer' => 'Créer un contact', # NEW
+	'contact_editer' => 'Éditer ce contact', # NEW
+	'contact_modifier' => 'Modifier un contact', # NEW
+	'contact_nouveau' => 'Nouveau contact', # NEW
+	'contact_numero' => 'Contact numéro', # NEW
+	'contact_voir' => 'Voir', # NEW
 	'contacts' => 'Contacts',
-	'confirmer_supprimer_contact' => "Ares you sure you want to delete all information about this contact ?",
-	'confirmer_supprimer_organisation' => "Are you sure you want to delete all the information about this organisation ?",
-	'confirmer_delier_organisation' => "Are you sure you want to unlink this contact from this organisation ?",
-	'confirmer_delier_contact' => "Are you sure you want to unlink this organisation from this contact ?",
-	
+	'contacts_nb' => '@nb@ contacts', # NEW
+	'creer_auteur_contact' => 'Cr&eacute;er un nouvel auteur et le lier &agrave; ce contact', # NEW
+	'creer_auteur_organisation' => 'Cr&eacute;er un nouvel auteur et le lier &agrave; cette organisation', # NEW
+
 	// D
-	'definir_auteur_comme_contact' => "Set as contact",
-	'definir_auteur_comme_organisation' => "Set as organisation",
+	'definir_auteur_comme_contact' => 'Set as contact',
+	'definir_auteur_comme_organisation' => 'Set as organisation',
+	'delier_cet_auteur' => 'Désassocier', # NEW
 
 	// E
-	'est_un_contact' => "This author is set as a contact.",
-	'est_une_organisation' => "This author is set as an organisation.",
-	'explication_statut_juridique' => "company, organisation, ...",
+	'est_un_contact' => 'This author is set as a contact.',
+	'est_une_organisation' => 'This author is set as an organisation.',
 	'explication_activite' => 'Activiity of the organisation : NGO, education, edition...',
-	'explication_contacts_ou_organisations' => "You can set this author as a contact or as an organisation.Vous pouvez définir cet auteur
-		Additional fields can be then lieeed from the author modification page.",
-	'explication_supprimer_contact' => "Deleting this contact will suppress all the additional information which has been filed on the author page.",
-	'explication_supprimer_organisation' => "Deleting this organisation will suppress all the additional information which has been filed on the author page.",
+	'explication_contacts_ou_organisations' => 'You can set this author as a contact or as an organisation.Vous pouvez définir cet auteur
+		Additional fields can be then lieeed from the author modification page.',
 	'explication_identification' => 'Organisation identification : VAT, etc.',
+	'explication_statut_juridique' => 'company, organisation, ...',
+	'explication_supprimer_contact' => 'Deleting this contact will suppress all the additional information which has been filed on the author page.',
+	'explication_supprimer_organisation' => 'Deleting this organisation will suppress all the additional information which has been filed on the author page.',
 	'explications_page_contacts' => 'Page en cours de d&eacute;veloppement. <br /><br />Actions envisag&eacute;es :<ul>
 	<li>voir tous les contacts</li><li>transformer les auteurs en contacts</li><li>importer des contacts</li><li>...</li></ul><br />Merci pour vos suggestions sur <a href="http://www.spip-contrib.net/Plugin-Contacts-Organisations#pagination_comments-list">le forum</a> ;-)',
 	'explications_page_organisations' => 'Page en cours de d&eacute;veloppement. <br /><br />Actions envisag&eacute;es :<ul>
 	<li>voir toutes les organisations</li><li>transformer des auteurs en organisations</li><li>importer des organsations</li><li>...</li></ul><br />Merci pour vos suggestions sur <a href="http://www.spip-contrib.net/Plugin-Contacts-Organisations#pagination_comments-list">le forum</a> ;-)',
 
 	// I
-	'infos_contacts_ou_organisations' => "Contacts & Organisations",
-	'info_tous_contacts' => "All the contacts",
-	'info_nb_contacts' => 'Linked contacts',	
-	'info_toutes_organisations' => "All the organisations",
-	'info_organisation_appartenance' => "Belonging Organisation",
-	'info_contacts_organisation' => "Organisation's contacts",
-	
+	'info_contacts_organisation' => 'Organisation\'s contacts',
+	'info_nb_contacts' => 'Linked contacts',
+	'info_organisation_appartenance' => 'Belonging Organisation',
+	'info_organisations_appartenance' => 'Organisations d\'appartenance', # NEW
+	'info_organisations_filles' => 'Organisations filles', # NEW
+	'info_organisations_meres' => 'Organisations mères', # NEW
+	'info_tous_contacts' => 'All the contacts',
+	'info_toutes_organisations' => 'All the organisations',
+	'infos_contacts_ou_organisations' => 'Contacts & Organisations',
+
 	// L
-	'label_activite' => "Activity",
-	'label_civilite' => "Gender",
-	'label_date_creation' => "Date of creation",
-	'label_date_naissance' => "Date of birth",
-	'label_descriptif' => "Description",
+	'label_activite' => 'Activity',
+	'label_civilite' => 'Gender',
+	'label_date_creation' => 'Date of creation',
+	'label_date_naissance' => 'Date of birth',
+	'label_descriptif' => 'Description',
 	'label_email' => 'Email',
-	'label_fonction' => "Function",
-	'label_identification' => "Identification",
-	'label_nom' => "Name",
-	'label_nom_organisation' => "Organisation",
+	'label_fonction' => 'Function',
+	'label_identification' => 'Identification',
+	'label_nom' => 'Name',
+	'label_nom_organisation' => 'Organisation',
 	'label_organisation' => 'Linked organisation',
-	'label_prenom' => "First name",
-	'label_prenom_nom' => "First + last name",
+	'label_organisation_parente' => 'Organisation parente', # NEW
+	'label_prenom' => 'First name',
+	'label_prenom_nom' => 'First + last name',
 	'label_pseudo' => 'Pseudo',
-	'label_recherche_contacts' => "Search in contacts",
-	'label_recherche_organisations' => "Search in organisations",
-	'label_statut_juridique' => "Status",
+	'label_recherche_auteurs' => 'Chercher dans les auteurs', # NEW
+	'label_recherche_contacts' => 'Search in contacts',
+	'label_recherche_organisations' => 'Search in organisations',
+	'label_statut_juridique' => 'Status',
 	'label_telephone' => 'Phone',
-	'lier_ce_contact' => "Attach this contact",
-	'lier_cette_organisation' => "Attach to this organisation",
-	'lier_contact' => "Attach a contact",
-	'lier_organisation' => "Attach to an organisation",
+	'label_type_liaison' => 'Liaison', # NEW
+	'lier_ce_contact' => 'Attach this contact',
+	'lier_cet_auteur' => 'Lier', # NEW
+	'lier_cette_organisation' => 'Attach to this organisation',
+	'lier_contact' => 'Attach a contact',
+	'lier_organisation' => 'Attach to an organisation',
 	'liste_contacts' => 'List of all contacts in the database',
 	'liste_organisations' => 'List of all organisations in the database',
 
+	// N
+	'nb_contact' => '1 contact', # NEW
+	'nb_contacts' => '@nb@ contacts', # NEW
+
 	// O
+	'organisation_associe_a_auteur_numero' => 'Associée à auteur numéro', # NEW
+	'organisation_associer_a_auteur' => 'Associer à un auteur', # NEW
+	'organisation_creer' => 'Créer une organisation', # NEW
+	'organisation_creer_fille' => 'Créer une organisation fille', # NEW
+	'organisation_editer' => 'Éditer cette organisation', # NEW
+	'organisation_modifier' => 'Modifier une organisation', # NEW
+	'organisation_nouveau' => 'Nouvelle organisation', # NEW
+	'organisation_numero' => 'Organisation numéro', # NEW
+	'organisation_un' => 'Une organisation', # NEW
+	'organisation_voir' => 'Voir', # NEW
 	'organisations' => 'Organisations',
-		
+	'organisations_nb' => '@nb@ organisations', # NEW
+
+	// R
+	'recherche_de' => 'Recherche de «@recherche@»', # NEW
+	'rechercher' => 'Rechercher', # NEW
+
 	// S
-	'supprimer_contact' => "Delete this contact",
-	'supprimer_organisation' => "Delete this organisation",
+	'supprimer_contact' => 'Delete this contact',
+	'supprimer_organisation' => 'Delete this organisation',
 
 	// T
-	'titre_contact' => "Contact details",
-	'titre_organisation' => "Organisation details",
+	'titre_contact' => 'Contact details',
+	'titre_organisation' => 'Organisation details',
 	'titre_page_contacts' => 'Contacts management',
 	'titre_page_organisations' => 'Organisations management',
-	
+	'titre_page_repertoire' => 'Répertoire' # NEW
 );
 
 ?>
