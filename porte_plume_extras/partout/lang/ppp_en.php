0728e0c931cf6ac3bd2ecf2f0ab49e1f2a4a6f17
guy.cesaro@gmail.com <guy.cesaro@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-06-02 18:00:13 +0000

    compatibilite SPIP3 sans cfg, langue en utf8. Il y a un probleme de cache sur la config.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@48424 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- porte_plume_extras/partout/lang/ppp_en.php
+++ porte_plume_extras/partout/lang/ppp_en.php
@@ -1,17 +1,27 @@
 <?php
-// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-if (!defined("_ECRIRE_INC_VERSION")) return;
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+// Produit automatiquement par le plugin LangOnet a partir de la langue source en
+// Module: ppp
+// Langue: en
+// Date: 02-06-2011 19:52:12
+// Items: 8
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-	// C
+
+// B
+	'bouton_mettre_a_jour' => 'Update',
+// C
 	'cfg_attention' => 'Warning',
 	'cfg_description' => 'according to your templates, be sure to only activate the Quill on fields where the use will not create xhtml error.',
 	'cfg_titre' => 'Quill everywhere',
-	
-	// L
+// L
 	'label_hauteur_champ' => 'Half size of the screen',
 	'legend_barre_typo' => 'Enable the editing toolbar on :',
 	'legend_hauteur_champ' => 'Height of the text field on articles',
+// S
+	'supprimer' => 'Reset to the default values',
 );
-
 ?>
--- porte_plume_extras/partout/lang/ppp_fr.php
+++ porte_plume_extras/partout/lang/ppp_fr.php
@@ -1,17 +1,27 @@
 <?php
-// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-if (!defined("_ECRIRE_INC_VERSION")) return;
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+// Produit automatiquement par le plugin LangOnet a partir de la langue source fr
+// Module: ppp
+// Langue: fr
+// Date: 02-06-2011 19:51:58
+// Items: 8
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-	// C
-	'cfg_attention' => 'Attention',
-	'cfg_description' => 'en fonction de vos squelettes, veillez &agrave; n\'activer le Porte Plume que sur des champs pour lesquels l\'utilisation des raccourcis n\'engendrera pas d\'erreur xhtml.',
-	'cfg_titre' => 'Porte Plume Partout',
 
-	// L
-	'label_hauteur_champ' => 'Demi-hauteur de l\'&eacute;cran',
+// B
+	'bouton_mettre_a_jour' => 'Mettre à jour',
+// C
+	'cfg_attention' => 'Attention',
+	'cfg_description' => 'en fonction de vos squelettes, veillez à n\'activer le Porte Plume que sur des champs pour lesquels l\'utilisation des raccourcis n\'engendrera pas d\'erreur xhtml.',
+	'cfg_titre' => 'Porte Plume Partout',
+// L
+	'label_hauteur_champ' => 'Demi-hauteur de l\'écran',
 	'legend_barre_typo' => 'Activer la barre typographique sur :',
 	'legend_hauteur_champ' => 'Hauteur du champ texte pour les articles',
+// S
+	'supprimer' => 'Remettre les valeurs par défaut',
 );
-
 ?>
