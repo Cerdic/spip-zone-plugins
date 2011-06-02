73ad3fa6e12a40b0908b350cf819c774231151cf
dwojylac@free.fr <dwojylac@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-06-02 16:59:36 +0000

    on sépare fancybox qui fera l'objet d'un autre plugin de la partie proprement spip
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@48420 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- wgalerie/wgalerie.php
+++ wgalerie/wgalerie.php
@@ -1,11 +1,8 @@
 <?php
-function wgalerie_inclurejs_css($flux) {
+function wgalerie_inclure_js($flux) {
 	$flux .= '<!-- plugin wgalerie -->'."\n";
-	$flux .= '<link href="'.find_in_path('fancybox/jquery.fancybox-1.3.1.css').'" rel="stylesheet" type="text/css" media="screen" />'."\n";
-	$flux .= '<script type="text/javascript" src="'.find_in_path('fancybox/jquery.mousewheel-3.0.2.pack.js').'"></script>'."\n";
-	$flux .= '<script type="text/javascript" src="'.find_in_path('fancybox/jquery.fancybox-1.3.1.pack.js').'"></script>'."\n";
-	$flux .= recuperer_fond('lien_css_fancybox_ie')."\n";
 	$flux .= '<script type="text/javascript" src="'.find_in_path('js/wgalerie.js').'"></script>'."\n";
 	return $flux;
 }
+
 ?>
