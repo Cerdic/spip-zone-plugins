f65e6b4583053607869a61fd9e7c53c098b1711b
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-06-02 08:01:28 +0000

    langues (prix)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@48407 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- prix/lang/prix_fr.php
+++ prix/lang/prix_fr.php
@@ -1,19 +1,20 @@
 <?php
-
-// Sécurité
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/prix/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-'label_prix_ht' => 'Prix HT',
-'label_prix_ttc' => 'Prix TTC',
+	// L
+	'label_prix_ht' => 'Prix HT',
+	'label_prix_ttc' => 'Prix TTC',
+	'label_taxes' => 'Taxes',
+	'label_total_ht' => 'Total HT',
+	'label_total_ttc' => 'Total TTC',
 
-'label_taxes' => 'Taxes',
-'label_total_ht' => 'Total HT',
-'label_total_ttc' => 'Total TTC',
-
-'prix_ht' => '@prix@ HT',
-'prix_ttc' => '@prix@ TTC',
+	// P
+	'prix_ht' => '@prix@ HT',
+	'prix_ttc' => '@prix@ TTC'
 );
 
 ?>
