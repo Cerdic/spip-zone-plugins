57cefb5c605ed186e7230665e304681085fc680a
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-06-02 08:01:19 +0000

    langues (commandes)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@48402 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- commandes/lang/commandes_fr.php
+++ commandes/lang/commandes_fr.php
@@ -1,7 +1,10 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-if (!defined("_ECRIRE_INC_VERSION")) return;
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/commandes/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+
 	// A
 	'action_facture' => 'Facture',
 	'action_supprimer' => 'Supprimer',
@@ -20,26 +23,26 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// D
 	'designation' => 'Désignation',
-	
+
 	// E
 	'etat' => 'État',
 
 	// F
-	'facture_num' => 'Facture n° <span>@num@</span>',
 	'facture_date' => 'Date : <span>@date@</span>',
-	
+	'facture_num' => 'Facture n° <span>@num@</span>',
+
 	// I
 	'info_toutes_commandes' => 'Toutes les commandes',
 
 	// M
 	'montant' => 'Montant',
-	
+
 	// N
 	'nom_bouton_plugin' => 'Commandes',
 
 	// R
 	'reference' => 'Référence',
-	
+
 	// S
 	'statut_attente' => 'En attente de validation',
 	'statut_encours' => 'En cours',
@@ -47,7 +50,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'statut_partiel' => 'Partiellement payée',
 	'statut_paye' => 'Payée',
 	'statut_retour' => 'Retournée',
-	'statut_retour_partiel' => 'Retour partiel',
+	'statut_retour_partiel' => 'Retour partiel'
 );
 
 ?>
