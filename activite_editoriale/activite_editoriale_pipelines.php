417a7519ac1a6aee4d307e5f62510d323b2a7c94
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-06-04 19:54:59 +0000

    compatibilité SPIP 3 … du moins pour la fonction initiale de ce plugin qui était l'alerte de mail. Je ne suis pas responsable de la partie statistiques, qui est utilise de tas de veilles fonctions. Je serai d'avis de revenir à la fonction initiale du plugin. En tout cas je m'engage pas sur ce truc.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@48484 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- activite_editoriale/activite_editoriale_pipelines.php
+++ activite_editoriale/activite_editoriale_pipelines.php
@@ -6,6 +6,7 @@ function activite_editoriale_taches_generales_cron($taches_generales) {
 }
 
 function activite_editoriale_boite_infos($flux){
+	include_spip('inc/presentation');
 	$type = $flux['args']['type'];
 	if (function_exists('lire_config')){
         $config_stats_rubriques = lire_config('activite_editoriale/stats_rubriques','0');
@@ -17,12 +18,12 @@ function activite_editoriale_boite_infos($flux){
 		{
 			if($config_stats_rubriques) {
 				$flux['data'] .= recuperer_fond('fonds/stat_rubrique',array('id_rubrique' => $flux['args']['id']));
-				$flux['data'] .= icone_horizontale(_T('activite_editoriale:icone_statistics'), generer_url_ecrire('activite_editoriale_statistics',"id_objet=$id&type=$type"), "", _DIR_PLUGIN_ACTIVITE_EDITORIALE."/images/statistics.png", false);
+				$flux['data'] .= icone_horizontale(_T('activite_editoriale:icone_statistics'), generer_url_ecrire('activite_editoriale_statistics',"id_objet=$id&type=$type"), "", find_in_path("/images/statistics.png"), false);
 			}
 			if($config_mots_cles)
 			{
 				$flux['data'] .= recuperer_fond('fonds/mots_cles',array('id_rubrique' => $flux['args']['id']));
-				$flux['data'] .= icone_horizontale(_T('activite_editoriale:icone_mots_cles'), generer_url_ecrire('activite_editoriale_mots_cles',"id_objet=$id&type=$type"), "", _DIR_PLUGIN_ACTIVITE_EDITORIALE."/images/mot-cle.gif", false);
+				$flux['data'] .= icone_horizontale(_T('activite_editoriale:icone_mots_cles'), generer_url_ecrire('activite_editoriale_mots_cles',"id_objet=$id&type=$type"), "", find_in_path("/images/mot-cle.gif"), false);
 			}
 		}
 	}
