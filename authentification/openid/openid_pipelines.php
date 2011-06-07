a80cd64a969e59e9063f5a9407084dec80511035
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-06-07 12:41:35 +0000

    robustifier la regexp (spip3)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@48538 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- authentification/openid/openid_pipelines.php
+++ authentification/openid/openid_pipelines.php
@@ -14,7 +14,7 @@
 function openid_editer_contenu_objet($flux){
 	if ($flux['args']['type']=='auteur') {
 		$openid = recuperer_fond('formulaires/inc-openid', $flux['args']['contexte']);
-		$flux['data'] = preg_replace('%(<li class="editer_email(.*?)</li>)%is', '$1'."\n".$openid, $flux['data']);
+		$flux['data'] = preg_replace('%(<li class=["\'][^"\']*editer_email(.*?)</li>)%is', '$1'."\n".$openid, $flux['data']);
 	}
 	return $flux;
 }
