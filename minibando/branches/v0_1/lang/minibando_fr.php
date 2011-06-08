8a7bd79e579a8624bab2e0cbdec9c0eefa32897c
brunobergot@gmail.com <brunobergot@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-06-08 13:50:47 +0000

    création de la branche 0.1 qu'on va pouvoir nettoyer par la suite
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@48587 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ minibando/branches/v0_1/lang/minibando_fr.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	'mode_debug' => 'Mode debug',
+	'mode_profile' => 'Mode profile',
+	'mode_inclure' => 'Mode inclure',
+	
+	'titre_debug' => 'Debug',
+	'titre_outils_rapides' => 'Cr&eacute;ation',
+
+);
+
+?>
