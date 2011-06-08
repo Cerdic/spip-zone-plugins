2a9481c21506e5fe918ee46e207fb25605cf9850
fil@rezo.net <fil@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-06-08 12:59:48 +0000

    plugin telepathe pour faire de la transmission d'idees d'un site a un autre (aka definir une API via des squelettes)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@48580 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ telepathe/trunk/telepathe_fonctions.php
@@ -0,0 +1,75 @@
+<?php
+
+function telepathe($contenu=null, $type=null) {
+	static $t;
+
+	// id = initialisation
+	if ($type == 'id')
+		$t = array();
+
+	// autre, remplir
+	if (!is_null($type)
+	AND is_array($t)) {
+
+		# un pipeline qui permet de traiter tout le contenu a envoyer,
+		# par exemple pour recoder les URLs des liens
+		$contenu = pipeline('telepathe', $contenu);
+
+		// si type finit par *, c'est un array
+		// si type finit par +, c'est une concatenation
+		preg_match(',^(.*?)([\*\+])?$,S', $type, $r);
+		switch($r[2]) {
+			case '':
+				$t[$r[1]] = $contenu;
+				break;
+			case '*':
+				$t[$r[1]][] = $contenu;
+				break;
+			case '+':
+				$t[$r[1]] .= $contenu;
+				break;
+		}
+		return "<dt>$type</dt>\n<dd>".safehtml(filtre_print($contenu))."</dd>";
+	}
+
+	// rien, renvoyer le tableau
+	if (is_null($type))
+		return $t;
+}
+
+
+function telepathe_formater($html) {
+
+	$v = telepathe();
+	$r = array("a".$v['id'] => $v);
+
+	switch (_request('format')) {
+		case 'json':
+			return pretty_json_encode($r);
+		case 'yaml':
+			if (include_spip('inc/yaml'))
+				return yaml_encode($r);
+		#case 'xml':
+		#	return xml($r);
+		default:
+			return $html;
+	}
+
+
+}
+
+## essai (ratŽ) de faire du joli json affichable dans le nav ; mais
+## avec yaml c'est plus propre
+function pretty_json_encode($x) {
+	return json_encode($x);
+
+	if (is_array($x)) {
+		$a = array();
+		foreach ($x as $k=>$v)
+			$a[] = " ".json_encode($k).":\n\t".pretty_json_encode($v);
+		return "{\n".join(",\n", $a)."\n}\n";
+	}
+
+	return json_encode($x);
+
+}
