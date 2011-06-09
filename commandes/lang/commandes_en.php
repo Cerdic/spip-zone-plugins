04125ebf30933d7132d5caab0a84b5e6eb5b863e
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-06-09 21:57:59 +0000

    langues (commandes)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@48654 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ commandes/lang/commandes_en.php
@@ -0,0 +1,58 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'action_facture' => 'Invoice',
+	'action_supprimer' => 'Delete',
+	'adresse_facturation' => 'Billing address',
+	'adresse_livraison' => 'Shipping address',
+
+	// C
+	'commande_client' => 'Customer',
+	'commande_date' => 'Order date',
+	'commande_date_paiement' => 'Payment date',
+	'commande_montant' => 'Amount',
+	'commande_reference' => 'Reference',
+	'commande_statut' => 'Status',
+	'confirmer_supprimer_commande' => 'Can you confirm the deletion of this order?',
+	'contenu' => 'Content',
+
+	// D
+	'designation' => 'Designation',
+
+	// E
+	'etat' => 'Status',
+
+	// F
+	'facture_date' => 'Date: <span>@date@</span>',
+	'facture_num' => 'Invoice n° <span>@num@</span>',
+
+	// I
+	'info_toutes_commandes' => 'All orders',
+
+	// M
+	'montant' => 'Amount',
+
+	// N
+	'nom_bouton_plugin' => 'Orders',
+
+	// R
+	'reference' => 'Reference',
+
+	// S
+	'statut_attente' => 'Pending',
+	'statut_encours' => 'Ongoing',
+	'statut_envoye' => 'Sent',
+	'statut_partiel' => 'Partially paid',
+	'statut_paye' => 'Paid',
+	'statut_retour' => 'Return',
+	'statut_retour_partiel' => 'Partially return'
+);
+
+?>
