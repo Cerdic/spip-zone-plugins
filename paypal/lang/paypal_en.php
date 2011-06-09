8d4db6542887e87c3df11377ecaa9679ce3ca696
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-06-09 21:58:30 +0000

    langues (paypal)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@48663 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ paypal/lang/paypal_en.php
@@ -0,0 +1,50 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'api_prod' => 'Production API',
+	'api_test' => 'Test API',
+
+	// C
+	'configuration_paypal' => 'Paypal',
+
+	// D
+	'description_paypal' => 'Configure the online payment system Paypal',
+
+	// E
+	'explication_account' => 'Corresponds to email the seller\'s account',
+	'explication_environnement' => '<NEW>Vous pouvez indiquer dans ce formulaire des données
+  qui concernent le site de test de Paypal (Sandbox, pour le développement)
+  ainsi que celles valables uniquement pour de vraies transactions.
+  Quel environnement souhaitez-vous utiliser ?',
+	'explication_identifiant_vendeur' => 'Corresponds to email the seller\'s account',
+	'explication_soumission' => 'On what page of Paypal is submitted the form?',
+	'explication_tax' => 'Eventually the value of the tax to apply (for example, French VAT: 19.60)',
+
+	// I
+	'info_bouton_paypal' => 'Make payments with PayPal: it\'s fast, free and secure!',
+
+	// L
+	'label_account' => 'Account (email)',
+	'label_currency_code' => 'Currency',
+	'label_currency_code_eur' => 'Euro (€)',
+	'label_currency_code_usd' => 'Dollar (
+)',
+	'label_environnement' => 'Environment',
+	'label_environnement_prod' => 'Production environment',
+	'label_environnement_test' => 'Test environment',
+	'label_identifiant_vendeur' => 'Seller ID',
+	'label_password' => 'API Password (password)',
+	'label_signature' => 'Signature ',
+	'label_soumission' => 'Submission Form',
+	'label_tax' => 'Taxes',
+	'label_username' => 'API Username'
+);
+
+?>
