573ac2b49784438e16ac4d55ae6df8e65768e06d
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-06-09 21:58:32 +0000

    langues (produits)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@48665 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ produits/lang/produits_en.php
@@ -0,0 +1,38 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'configurer_taxe_defaut_label' => 'Default tax',
+	'configurer_titre' => 'Configuration of products',
+
+	// I
+	'info_1_produit' => 'One product',
+	'info_aucun_produit' => 'No product',
+	'info_nb_produits' => '@nb@ products',
+
+	// P
+	'produit_bouton_ajouter' => 'Add a product',
+	'produit_champ_descriptif_label' => 'Short description',
+	'produit_champ_prix_ht_label' => 'Duty-free price',
+	'produit_champ_reference_label' => 'Reference',
+	'produit_champ_rubrique_label' => 'Section',
+	'produit_champ_taxe_explication' => 'Default value on this site: @taxe@. You can leave this field empty.',
+	'produit_champ_taxe_label' => 'Tax',
+	'produit_champ_texte_label' => 'Text',
+	'produit_champ_titre_label' => 'Title',
+	'produit_editer' => 'Edit this product',
+	'produit_modifier' => 'Modify the product :',
+	'produit_nouveau' => 'New product',
+	'produit_numero' => 'Product n°',
+	'produit_reference' => 'Reference "@reference@"',
+	'produit_statut' => 'Modify the status :',
+	'produits_titre' => 'Products'
+);
+
+?>
