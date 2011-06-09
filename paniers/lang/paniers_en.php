cd5d72cd3323c2916a8806072f32a91749c3f895
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-06-09 21:58:29 +0000

    langues (paniers)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@48662 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ paniers/lang/paniers_en.php
@@ -0,0 +1,34 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'action_ajouter' => 'Add to cart',
+
+	// M
+	'mon_panier' => 'My cart',
+	'mon_panier_modifier' => 'Modify my cart',
+	'mon_panier_voir' => 'See my cart',
+
+	// P
+	'panier_description' => 'Description',
+	'panier_erreur_quantites' => 'Quantities must be integers.',
+	'panier_montant' => 'Amount',
+	'panier_prix_unitaire' => 'Unit price',
+	'panier_quantite' => 'Quantity',
+	'panier_quantite_ok' => 'The quantities have been changed.',
+	'panier_recalculer' => 'Recalculate',
+	'panier_taxes' => 'With taxes',
+	'panier_total_ht' => 'Total tax-free',
+	'panier_total_ttc' => 'Total',
+	'panier_valider' => 'Validate my cart',
+	'panier_vide' => 'Cart is empty.',
+	'paniers' => 'Shopping carts'
+);
+
+?>
