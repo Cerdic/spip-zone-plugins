b5ce0a26c8c152edc5ddd25fa64a473176daf213
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-06-09 21:42:17 +0000

    langues (texteslegaux)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@48651 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spip_proprietaire/lang/texteslegaux_en.php
+++ spip_proprietaire/lang/texteslegaux_en.php
@@ -1,39 +1,31 @@
 <?php
-// RAPPELS
-// Les textes de cette page peuvent etre rediges avec les raccourcis typo de SPIP
-// !! - Les accents doivent etre code en HTML : é => &eacute;
-// !! - Les apostrophes doivents etre echappees : ' => \'
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// B //
+	// B
 	'business_cards' => '{{{Business Cards}}}',
 
-
-// C //
-	'copyright_info' => '&copy; Copyright @date@ @nom_site@ | All rights reserved.',
-	'mention_cnil' => 'As such, {{[@nom_site@->@url_site@]}} was the subject of a statement from the {{<abbr title="National French Commission for informatical freedom - Commission Nationale de l&#039;informatique et des Libert&#233;s - www.cnil.fr">CNIL</abbr>}} on @date_cnil@, registered under reference {{@numero_cnil@.}}',
+	// C
 	'carte_visite_info' => 'You can view or download our {{cards}} by clicking the following links:
 - {{[site card->@classique@]}}
 - {{[full web card->@complete@]}}
 - {{[responsible card->@responsable@]}}
 - {{[administrator card->@administrateur@]}}
 - {{[webmaster card->@webmaster@]}}',
-	'createur_mentions_legales' => '[createur<-]{{{Designer / Creator}}}
-
-Whole site {{[@nom_site@->@url_site@]}}, design, graphics and applications, has been created and developed@createur_administrateur_texte@ by @createur_forme@ {{@createur_nom@}}. @createur_web@ @createur_mail_texte@
-',
-	'createur_mentions_legales_idem' => '[createur<-]{{{Designer / Creator}}}
-
-Website {{[@nom_site@->@url_site@]}} has been created by its owner.
-',
-	'conditions_utilisation_intro' => '{{{De facto acceptance}}}
-
-{{By using {{[@nom_site@->@url_site@]}}, the user acknowledges and accepts the conditions listed here.}}
-',
 	'conditions_utilisation_abus' => '{{{Abuse}}}
 
 Pursuant to [French law n° 2004-575 of 21 june 2004 on confidence in the digital economy|See the text (legifrance.gouv.fr) [fr]->http://www.legifrance.gouv.fr/affichTexte.do?cidTexte=JORFTEXT000000801164], to report a content issue or if you are a victim of fraudulent use of the site {{[@nom_site@->@url_site@]}}, please contact the administrator site by e-mail: [@proprietaire_mail_administratif@->mailto:@proprietaire_mail_administratif@].
+',
+	'conditions_utilisation_cookies' => '{{{Cookies}}}
+
+Some personal information and some witnesses markers may be recorded by {{[@nom_site@->@url_site@]}} on the personal computer of the user without his permission ({"cookies" or "Java applets"}). Unless explicitly stated otherwise &#091;*&#093;, these technical procedures are not essential for the proper functioning of {{[@nom_site@->@url_site@]}} and their use can of course be disabled in the browser used by the Internet user without its navigation may be affected. {{None of this information is retained by the site after disconnecting from the Internet.}}
+ 
+<small>&#091;*&#093; It is worth to precise here that access to different private areas of the site may require acceptance by the user of the deposit of a cookie on his personal computer to a security issue.</small>
 ',
 	'conditions_utilisation_infos_personnelles' => '{{{Collection and processing of personal data}}}
 
@@ -43,17 +35,9 @@ Pursuant to [French law n° 2004-575 of 21 june 2004 on confidence in the digita
 Pursuant to [French law n° 78-17 of 6 january 1978 relating to data, files and freedoms|See the text (legifrance.gouv.fr) [fr]->http://www.legifrance.gouv.fr/affichTexte.do?cidTexte=JORFTEXT000000886460], {{the user has a right to access, correct or delete personal information stored on the {{[@nom_site@->@url_site@]}}, exercisable at any time with the administrator of {{[@nom_site@->@url_site@]}}.}} For such requests, please send a letter to @proprietaire_forme@ @proprietaire_nom@ or contact email address: [@proprietaire_mail_administratif@->mailto:@proprietaire_mail_administratif@].
 @cnil_texte@
 ',
-	'conditions_utilisation_cookies' => '{{{Cookies}}}
+	'conditions_utilisation_intro' => '{{{De facto acceptance}}}
 
-Some personal information and some witnesses markers may be recorded by {{[@nom_site@->@url_site@]}} on the personal computer of the user without his permission ({"cookies" or "Java applets"}). Unless explicitly stated otherwise &#091;*&#093;, these technical procedures are not essential for the proper functioning of {{[@nom_site@->@url_site@]}} and their use can of course be disabled in the browser used by the Internet user without its navigation may be affected. {{None of this information is retained by the site after disconnecting from the Internet.}}
- 
-<small>&#091;*&#093; It is worth to precise here that access to different private areas of the site may require acceptance by the user of the deposit of a cookie on his personal computer to a security issue.</small>
-',
-	'conditions_utilisation_responsabilite' => '{{{Disclaimer}}}
-
-{{[@nom_site@->@url_site@]}} includes information provided by external companies or hypertext links to other sites or other external sources that have not been developed by {{[@nom_site@->@url_site@]}}. Behaviors of target sites, possibly malicious, cannot be attached to the responsibility of {{[@nom_site@->@url_site@]}}. More generally, the content available on this site is for informational purposes. It is up to the user to use this information wisely. {{[@nom_site@->@url_site@]}} is not responsible for informations, opinions and recommendations of third parties.
-
-Because {{[@nom_site@->@url_site@]}} can not control these sites or external sources, {{[@nom_site@->@url_site@]}} cannot be held responsible for the availability of these sites or external sources and cannot accept any responsibility for the content, advertising, products, services or other materials available from such sites or resources. In addition, {{[@nom_site@->@url_site@]}} cannot be held responsible for any damage or loss or alleged consequential in connection with the use or the fact of having trusted the content, goods or services available from such sites or sources external.
+{{By using {{[@nom_site@->@url_site@]}}, the user acknowledges and accepts the conditions listed here.}}
 ',
 	'conditions_utilisation_liens' => '{{{Links to @nom_site@}}}
 
@@ -68,12 +52,27 @@ The establishment of {{[@nom_site@->@url_site@]}} links does not require authori
 <a href="@url_site@" title="@nom_site@ - @descriptif_site@"><img src="@logo_site@" border="0" /></a>
 </code>|
 ',
+	'conditions_utilisation_responsabilite' => '{{{Disclaimer}}}
 
-// E //
-	'enregistre_pres' => 'registered with',
+{{[@nom_site@->@url_site@]}} includes information provided by external companies or hypertext links to other sites or other external sources that have not been developed by {{[@nom_site@->@url_site@]}}. Behaviors of target sites, possibly malicious, cannot be attached to the responsibility of {{[@nom_site@->@url_site@]}}. More generally, the content available on this site is for informational purposes. It is up to the user to use this information wisely. {{[@nom_site@->@url_site@]}} is not responsible for informations, opinions and recommendations of third parties.
+
+Because {{[@nom_site@->@url_site@]}} can not control these sites or external sources, {{[@nom_site@->@url_site@]}} cannot be held responsible for the availability of these sites or external sources and cannot accept any responsibility for the content, advertising, products, services or other materials available from such sites or resources. In addition, {{[@nom_site@->@url_site@]}} cannot be held responsible for any damage or loss or alleged consequential in connection with the use or the fact of having trusted the content, goods or services available from such sites or sources external.
+',
+	'copyright_info' => '&copy; Copyright @date@ @nom_site@ | All rights reserved.',
+	'createur_mentions_legales' => '[createur<-]{{{Designer / Creator}}}
+
+Whole site {{[@nom_site@->@url_site@]}}, design, graphics and applications, has been created and developed@createur_administrateur_texte@ by @createur_forme@ {{@createur_nom@}}. @createur_web@ @createur_mail_texte@
+',
+	'createur_mentions_legales_idem' => '[createur<-]{{{Designer / Creator}}}
+
+Website {{[@nom_site@->@url_site@]}} has been created by its owner.
+',
+
+	// E
 	'egalement_administrateur' => ' and is administrated',
+	'enregistre_pres' => 'registered with',
 
-// H //
+	// H
 	'hebergeur_mentions_legales' => '[hebergeur<-]{{{Hosting}}}
 
 {{[@nom_site@->@url_site@]}} is hosted by @hebergeur_forme@ {{@hebergeur_nom@}}@type_serveur_texte@@os_serveur_texte@. @hebergeur_web@ @hebergeur_mail_texte@
@@ -85,7 +84,8 @@ The establishment of {{[@nom_site@->@url_site@]}} links does not require authori
 Website {{[@nom_site@->@url_site@]}} is hosted by its owner.
 ',
 
-// M //
+	// M
+	'mention_cnil' => 'As such, {{[@nom_site@->@url_site@]}} was the subject of a statement from the {{<abbr title="National French Commission for informatical freedom - Commission Nationale de l&#039;informatique et des Libert&#233;s - www.cnil.fr">CNIL</abbr>}} on @date_cnil@, registered under reference {{@numero_cnil@.}}',
 	'mentions_legales_copyright' => '{{{Contents and copyright}}}
 
 Pursuant to [articles L. 111-1 et L. 123-1 of the French Intellectual Property Code|See the text (legifrance.gouv.fr) [fr]->http://www.legifrance.gouv.fr/affichCode.do?cidTexte=LEGITEXT000006069414], the entire contents of this site ({texts, images, videos and any media in general}), unless explicitly stated otherwise, is {{protected by copyright}}. Reproduction, even partial, of the contents of pages on this site without prior agreement of @proprietaire_forme@ [{{@proprietaire_nom@}}->#proprietaire] is strictly prohibited.
@@ -100,32 +100,35 @@ The pages and content on this site are generated by {{SPIP}} free software, dist
 </small>
 ',
 
-// O //
+	// O
 	'os_du_serveur' => ' running on a system {{@os_serveur@}}',
 
-// P //
+	// P
+	'pas_config' => 'Il n\'y a pas assez d\'information pour g&eacute;n&eacute;rer cette page ... Veuillez renseigner ces informations sur [l\'espace de configuration du plugin "Mentions L&eacute;gales"->@url_config@].', # NEW
+	'pour_les_contacter' => 'To contact them, use the email address : [@mail@->mailto:@mail@].',
 	'proprietaire_mentions_legales' => '[editeur<-]{{{Owner / Publisher}}}
 
 {{[@nom_site@->@url_site@]}} is a website edited by @proprietaire_forme@ {{@proprietaire_nom@}} and published by {{@proprietaire_nom_responsable@}}{@proprietaire_fonction_responsable_texte@}. Whole site {{[@nom_site@->@url_site@]}}, pages and contents, is the property of @proprietaire_forme@ {{@proprietaire_nom@}}. @proprietaire_web@
 <br />{{[@nom_site@->@url_site@]}} administrative contact site : [@proprietaire_mail_administratif@->mailto:@proprietaire_mail_administratif@].
 ',
-	'pour_les_contacter' => 'To contact them, use the email address : [@mail@->mailto:@mail@].',
 
-// R //
+	// R
 	'reportez_vous_au_site' => 'For more information, visit the website : [->@site@].',
 
-// S //
+	// S
 	'sous_le_numero' => 'under reference',
 	'sur_un_serveur' => ', on a {{@serveur@}} server',
 
-// V //
-	// Texte v_cards
+	// T
+	'test_fichier_langue' => 'TEST', # NEW
+
+	// V
 	'vcard_info' => 'If you use a mail manager, mobile phone, an address book or organizer, you can download our contact details at {{\'vCard\' format}} ({Virtual Card}) below.
 
 You can, depending on your system:
 - directly use the following URL to download this information automatically: {{@vcard_url@}}
 - view the card in your browser: {{[->@vcard_url@]}}
-- save it to your computer ({put the file extension {{\'.VCF\'}} if it\'s not the case}): {{[->@vcard_url_download@]}}',
-
+- save it to your computer ({put the file extension {{\'.VCF\'}} if it\'s not the case}): {{[->@vcard_url_download@]}}'
 );
+
 ?>
--- spip_proprietaire/lang/texteslegaux_fr.php
+++ spip_proprietaire/lang/texteslegaux_fr.php
@@ -1,39 +1,29 @@
 <?php
-// RAPPELS
-// Les textes de cette page peuvent etre rediges avec les raccourcis typo de SPIP
-// !! - Les accents doivent etre code en HTML : é => &eacute;
-// !! - Les apostrophes doivents etre echappees : ' => \'
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/spip_proprietaire/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// B //
+	// B
 	'business_cards' => '{{{Business Cards}}}',
 
-
-// C //
-	'copyright_info' => '&copy; Copyright @date@ @nom_site@ | Tous droits r&eacute;serv&eacute;s.',
-	'mention_cnil' => '&Agrave; ce titre, {{[@nom_site@->@url_site@]}} a fait l\'objet d\'une d&eacute;claration aupr&egrave;s de la {{<abbr title="Commission Nationale de l&#039;Informatique et des Libert&#233;s - www.cnil.fr">CNIL</abbr>}} le @date_cnil@, enregistr&eacute;e sous la r&eacute;f&eacute;rence {{@numero_cnil@.}}',
+	// C
 	'carte_visite_info' => 'Vous pouvez visualiser ou t&eacute;l&eacute;charger nos {{cartes de visite}} en cliquant sur les liens suivants :
 - {{[carte de visite du site->@classique@]}}
 - {{[carte de visite web compl&egrave;te->@complete@]}}
 - {{[carte de visite du responsable->@responsable@]}}
 - {{[carte de visite de l’administrateur->@administrateur@]}}
 - {{[carte de visite du webmestre du site->@webmaster@]}}',
-	'createur_mentions_legales' => '[createur<-]{{{Concepteur / Cr&eacute;ateur}}}
-
-L’ensemble du site {{[@nom_site@->@url_site@]}}, conception, charte graphique et applications, a &eacute;t&eacute; cr&eacute;&eacute; et d&eacute;velopp&eacute;@createur_administrateur_texte@ par @createur_forme@ {{@createur_nom@}}. @createur_web@ @createur_mail_texte@
-',
-	'createur_mentions_legales_idem' => '[createur<-]{{{Concepteur / Cr&eacute;ateur}}}
-
-Le site {{[@nom_site@->@url_site@]}} a &eacute;t&eacute; cr&eacute;&eacute; par l’&eacute;diteur.
-',
-	'conditions_utilisation_intro' => '{{{Acceptation de fait}}}
-
-{{En utilisant {{[@nom_site@->@url_site@]}}, l\'internaute prend note et accepte les conditions ici &eacute;num&eacute;r&eacute;es.}}
-',
 	'conditions_utilisation_abus' => '{{{Abus}}}
 
 En application de la [loi n° 2004-575 du 21 juin 2004 pour la confiance dans l’&eacute;conomie num&eacute;rique|Voir le texte (legifrance.gouv.fr)->http://www.legifrance.gouv.fr/affichTexte.do?cidTexte=JORFTEXT000000801164], pour signaler un contenu litigieux ou si vous &ecirc;tes victime d’une utilisation frauduleuse du site {{[@nom_site@->@url_site@]}}, merci de contacter l’administrateur du site &agrave; l’adresse courriel : [@proprietaire_mail_administratif@->mailto:@proprietaire_mail_administratif@].
+',
+	'conditions_utilisation_cookies' => '{{{Cookies}}}
+
+Certaines informations nominatives et certains marqueurs t&eacute;moins peuvent &ecirc;tre enregistr&eacute;es par {{[@nom_site@->@url_site@]}} sur l’ordinateur personnel de l’internaute sans expression de la volonté de ce dernier ({"cookies" ou "applets JAVA"}). Sauf indication contraire explicite &#091;*&#093;, ces proc&eacute;d&eacute;s techniques ne sont pas indispensables au bon fonctionnement de {{[@nom_site@->@url_site@]}} et leur utilisation peut bien entendu &ecirc;tre d&eacute;sactiv&eacute;e dans le navigateur utilis&eacute; par l’internaute sans que sa navigation en soit affect&eacute;e. {{Aucune de ces informations n’est conservée par le site apr&egrave;s d&eacute;connexion de l’internaute.}}
+
+<small>&#091;*&#093; Il est utile de pr&eacute;ciser ici que les acc&egrave;s aux diff&eacute;rents espaces priv&eacute;s du site peuvent n&eacute;cessiter l’acceptation par l’internaute du d&eacute;p&ocirc;t d’un cookie sur son ordinateur personnel pour une question de s&eacute;curit&eacute;.</small>
 ',
 	'conditions_utilisation_infos_personnelles' => '{{{Collecte et traitement de donn&eacute;es personnelles}}}
 
@@ -43,17 +33,9 @@ En application de la [loi n° 2004-575 du 21 juin 2004 pour la confiance dans l�
 En application de la [loi n° 78-17 du 6 janvier 1978 relative &agrave; l’informatique, aux fichiers et aux libert&eacute;s|Voir le texte (legifrance.gouv.fr)->http://www.legifrance.gouv.fr/affichTexte.do?cidTexte=JORFTEXT000000886460], {{l’internaute dispose d’un droit d’acc&egrave;s, de rectification et de suppression des informations personnelles le concernant stock&eacute;es par {{[@nom_site@->@url_site@]}}, qu’il peut exercer &agrave; tout moment aupr&egrave;s de l’administrateur de {{[@nom_site@->@url_site@]}}.}} Pour une demande de ce type, veuillez adresser un courrier &agrave; @proprietaire_forme@ @proprietaire_nom@ ou contacter l’adresse courriel : [@proprietaire_mail_administratif@->mailto:@proprietaire_mail_administratif@].
 @cnil_texte@
 ',
-	'conditions_utilisation_cookies' => '{{{Cookies}}}
+	'conditions_utilisation_intro' => '{{{Acceptation de fait}}}
 
-Certaines informations nominatives et certains marqueurs t&eacute;moins peuvent &ecirc;tre enregistr&eacute;es par {{[@nom_site@->@url_site@]}} sur l’ordinateur personnel de l’internaute sans expression de la volonté de ce dernier ({"cookies" ou "applets JAVA"}). Sauf indication contraire explicite &#091;*&#093;, ces proc&eacute;d&eacute;s techniques ne sont pas indispensables au bon fonctionnement de {{[@nom_site@->@url_site@]}} et leur utilisation peut bien entendu &ecirc;tre d&eacute;sactiv&eacute;e dans le navigateur utilis&eacute; par l’internaute sans que sa navigation en soit affect&eacute;e. {{Aucune de ces informations n’est conservée par le site apr&egrave;s d&eacute;connexion de l’internaute.}}
-
-<small>&#091;*&#093; Il est utile de pr&eacute;ciser ici que les acc&egrave;s aux diff&eacute;rents espaces priv&eacute;s du site peuvent n&eacute;cessiter l’acceptation par l’internaute du d&eacute;p&ocirc;t d’un cookie sur son ordinateur personnel pour une question de s&eacute;curit&eacute;.</small>
-',
-	'conditions_utilisation_responsabilite' => '{{{Limitation de responsabilit&eacute;}}}
-
-{{[@nom_site@->@url_site@]}} comporte des informations mises &agrave; disposition par des soci&eacute;t&eacute;s externes ou des liens hypertextes vers d\'autres sites ou d’autres sources externes qui n\'ont pas &eacute;t&eacute; d&eacute;velopp&eacute;s par {{[@nom_site@->@url_site@]}}. Les comportements des sites cibles, &eacute;ventuellement malveillants, ne sauraient &ecirc;tre rattach&eacute;s &agrave; la responsabilit&eacute; de {{[@nom_site@->@url_site@]}}. Plus g&eacute;n&eacute;ralement, le contenu mis &agrave; disposition sur ce site est fourni &agrave; titre informatif. Il appartient &agrave; l\'internaute d\'utiliser ces informations avec discernement. La responsabilit&eacute; de {{[@nom_site@->@url_site@]}} ne saurait &ecirc;tre engag&eacute;e de fait quant aux informations, opinions et recommandations formul&eacute;es par des tiers.
-
-Dans la mesure o&ugrave; {{[@nom_site@->@url_site@]}} ne peut contr&ocirc;ler ces sites ou sources externes, {{[@nom_site@->@url_site@]}} ne peut &ecirc;tre tenu pour responsable de la mise &agrave; disposition de ces sites ou sources externes et ne peut supporter aucune responsabilit&eacute; quant aux contenus, publicit&eacute;s, produits, services ou tout autre mat&eacute;riel disponible sur ou &agrave; partir de ces sites ou sources externes. De plus, {{[@nom_site@->@url_site@]}} ne pourra &ecirc;tre tenu responsable de tous dommages ou pertes av&eacute;r&eacute;s ou all&eacute;gu&eacute;s cons&eacute;cutifs ou en relation avec l’utilisation ou le fait d’avoir fait confiance au contenu, &agrave; des biens ou des services disponibles sur ces sites ou sources externes. 
+{{En utilisant {{[@nom_site@->@url_site@]}}, l\'internaute prend note et accepte les conditions ici &eacute;num&eacute;r&eacute;es.}}
 ',
 	'conditions_utilisation_liens' => '{{{Liens vers @nom_site@}}}
 
@@ -68,12 +50,27 @@ L’&eacute;tablissement de lien vers {{[@nom_site@->@url_site@]}} ne requiert p
 <a href="@url_site@" title="@nom_site@ - @descriptif_site@"><img src="@logo_site@" border="0" /></a>
 </code>|
 ',
+	'conditions_utilisation_responsabilite' => '{{{Limitation de responsabilit&eacute;}}}
 
-// E //
-	'enregistre_pres' => 'enregistr&eacute; aupr&egrave;s de',
+{{[@nom_site@->@url_site@]}} comporte des informations mises &agrave; disposition par des soci&eacute;t&eacute;s externes ou des liens hypertextes vers d\'autres sites ou d’autres sources externes qui n\'ont pas &eacute;t&eacute; d&eacute;velopp&eacute;s par {{[@nom_site@->@url_site@]}}. Les comportements des sites cibles, &eacute;ventuellement malveillants, ne sauraient &ecirc;tre rattach&eacute;s &agrave; la responsabilit&eacute; de {{[@nom_site@->@url_site@]}}. Plus g&eacute;n&eacute;ralement, le contenu mis &agrave; disposition sur ce site est fourni &agrave; titre informatif. Il appartient &agrave; l\'internaute d\'utiliser ces informations avec discernement. La responsabilit&eacute; de {{[@nom_site@->@url_site@]}} ne saurait &ecirc;tre engag&eacute;e de fait quant aux informations, opinions et recommandations formul&eacute;es par des tiers.
+
+Dans la mesure o&ugrave; {{[@nom_site@->@url_site@]}} ne peut contr&ocirc;ler ces sites ou sources externes, {{[@nom_site@->@url_site@]}} ne peut &ecirc;tre tenu pour responsable de la mise &agrave; disposition de ces sites ou sources externes et ne peut supporter aucune responsabilit&eacute; quant aux contenus, publicit&eacute;s, produits, services ou tout autre mat&eacute;riel disponible sur ou &agrave; partir de ces sites ou sources externes. De plus, {{[@nom_site@->@url_site@]}} ne pourra &ecirc;tre tenu responsable de tous dommages ou pertes av&eacute;r&eacute;s ou all&eacute;gu&eacute;s cons&eacute;cutifs ou en relation avec l’utilisation ou le fait d’avoir fait confiance au contenu, &agrave; des biens ou des services disponibles sur ces sites ou sources externes. 
+',
+	'copyright_info' => '&copy; Copyright @date@ @nom_site@ | Tous droits r&eacute;serv&eacute;s.',
+	'createur_mentions_legales' => '[createur<-]{{{Concepteur / Cr&eacute;ateur}}}
+
+L’ensemble du site {{[@nom_site@->@url_site@]}}, conception, charte graphique et applications, a &eacute;t&eacute; cr&eacute;&eacute; et d&eacute;velopp&eacute;@createur_administrateur_texte@ par @createur_forme@ {{@createur_nom@}}. @createur_web@ @createur_mail_texte@
+',
+	'createur_mentions_legales_idem' => '[createur<-]{{{Concepteur / Cr&eacute;ateur}}}
+
+Le site {{[@nom_site@->@url_site@]}} a &eacute;t&eacute; cr&eacute;&eacute; par l’&eacute;diteur.
+',
+
+	// E
 	'egalement_administrateur' => ' et est administr&eacute;',
+	'enregistre_pres' => 'enregistr&eacute; aupr&egrave;s de',
 
-// H //
+	// H
 	'hebergeur_mentions_legales' => '[hebergeur<-]{{{H&eacute;bergeur}}}
 
 {{[@nom_site@->@url_site@]}} est h&eacute;berg&eacute; par @hebergeur_forme@ {{@hebergeur_nom@}}@type_serveur_texte@@os_serveur_texte@. @hebergeur_web@ @hebergeur_mail_texte@
@@ -85,7 +82,8 @@ L’&eacute;tablissement de lien vers {{[@nom_site@->@url_site@]}} ne requiert p
 Le site {{[@nom_site@->@url_site@]}} est h&eacute;berg&eacute; par l’&eacute;diteur.
 ',
 
-// M //
+	// M
+	'mention_cnil' => '&Agrave; ce titre, {{[@nom_site@->@url_site@]}} a fait l\'objet d\'une d&eacute;claration aupr&egrave;s de la {{<abbr title="Commission Nationale de l&#039;Informatique et des Libert&#233;s - www.cnil.fr">CNIL</abbr>}} le @date_cnil@, enregistr&eacute;e sous la r&eacute;f&eacute;rence {{@numero_cnil@.}}',
 	'mentions_legales_copyright' => '{{{Contenus et droits de reproduction}}}
 
 En application des [articles L. 111-1 et L. 123-1 du Code de la Propri&eacute;t&eacute; Intellectuelle|Voir le texte (legifrance.gouv.fr)->http://www.legifrance.gouv.fr/affichCode.do?cidTexte=LEGITEXT000006069414], l’ensemble des contenus de ce site ({textes, images, vid&eacute;os et tout m&eacute;dia en g&eacute;n&eacute;ral}), sauf mention contraire explicite, est {{prot&eacute;g&eacute; par le droit d’auteur}}. La reproduction, m&ecirc;me partielle, des contenus des pages de ce site sans accord pr&eacute;alable de @proprietaire_forme@ [{{@proprietaire_nom@}}->#proprietaire] est strictement interdite.
@@ -100,37 +98,35 @@ Les pages et le contenu de ce site sont g&eacute;n&eacute;r&eacute;s par le logi
 </small>
 ',
 
-// O //
+	// O
 	'os_du_serveur' => ' fonctionnant sur un syst&egrave;me {{@os_serveur@}}',
 
-// P //
+	// P
 	'pas_config' => 'Il n\'y a pas assez d\'information pour g&eacute;n&eacute;rer cette page ... Veuillez renseigner ces informations sur [l\'espace de configuration du plugin "Mentions L&eacute;gales"->@url_config@].',
+	'pour_les_contacter' => 'Pour les contacter, utilisez l’adresse courriel : [@mail@->mailto:@mail@].',
 	'proprietaire_mentions_legales' => '[editeur<-]{{{Propri&eacute;taire / &Eacute;diteur}}}
 
 {{[@nom_site@->@url_site@]}} est un site web &eacute;dit&eacute; par @proprietaire_forme@ {{@proprietaire_nom@}} et publi&eacute; sous la direction de {{@proprietaire_nom_responsable@}}{@proprietaire_fonction_responsable_texte@}. L’ensemble du site {{[@nom_site@->@url_site@]}}, pages et contenus, est la propri&eacute;t&eacute; de @proprietaire_forme@ {{@proprietaire_nom@}}. @proprietaire_web@
 <br />Contact administratif du site {{[@nom_site@->@url_site@]}} : [@proprietaire_mail_administratif@->mailto:@proprietaire_mail_administratif@].
 ',
-	'pour_les_contacter' => 'Pour les contacter, utilisez l’adresse courriel : [@mail@->mailto:@mail@].',
 
-// R //
+	// R
 	'reportez_vous_au_site' => 'Pour plus d’informations, reportez-vous au site : [->@site@].',
 
-// S //
+	// S
 	'sous_le_numero' => 'sous le num&eacute;ro',
 	'sur_un_serveur' => ', sur un serveur {{@serveur@}}',
 
-// T //
-	// a conserver !!
+	// T
 	'test_fichier_langue' => 'TEST',
 
-// V //
-	// Texte v_cards
+	// V
 	'vcard_info' => 'Si vous utilisez un gestionnaire de mails, un t&eacute;l&eacute;phone portable, un carnet d’adresse ou un organiseur &eacute;lectronique, vous pouvez t&eacute;l&eacute;charger nos coordonn&eacute;es compl&egrave;tes au {{format \'vCard\'}} ({Virtual Card}) ci-dessous.
 
 Vous pouvez, selon votre syst&egrave;me :
 - utiliser directement l’adresse URL ci-dessous pour charger automatiquement ces informations : {{@vcard_url@}}
 - visualiser la carte dans votre navigateur : {{[->@vcard_url@]}}
-- l’enregistrer sur votre ordinateur ({indiquer obligatoirement une extension de fichier {{\'.VCF\'}} si ce n’est pas le cas}) : {{[->@vcard_url_download@]}}',
-
+- l’enregistrer sur votre ordinateur ({indiquer obligatoirement une extension de fichier {{\'.VCF\'}} si ce n’est pas le cas}) : {{[->@vcard_url_download@]}}'
 );
+
 ?>
