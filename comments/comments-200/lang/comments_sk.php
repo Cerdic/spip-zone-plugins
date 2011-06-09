9cab2b3b5e343d0d9495d234d00a34df0d90b957
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-06-09 21:58:16 +0000

    langues (comments)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@48655 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ comments/comments-200/lang/comments_sk.php
@@ -0,0 +1,50 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cfg_forum_longueur' => 'Dĺžka správ',
+	'cfg_forum_longueur_maxi' => 'Maximálna dĺžka',
+	'cfg_forum_longueur_mini' => 'Minimálna dĺžka',
+	'comment' => 'komentár',
+	'comments' => 'komentárov',
+	'comments_h' => 'Vaše komentáre',
+
+	// D
+	'date_heure_a' => 'o',
+	'date_jour_le' => ' ',
+
+	// F
+	'forum_qui_etes_vous' => 'Kto ste?',
+
+	// L
+	'label_email' => 'E-mail (nezverejňuje sa)',
+	'label_nom' => 'Meno',
+	'label_url' => 'Vaša webstránka',
+
+	// M
+	'moderation_info' => 'Upozornenie: Vaša správa sa zobrazí, až keď bude skontrolovaná a schválená.',
+
+	// P
+	'permalink_to' => 'Trvalý odkaz na komentár',
+
+	// R
+	'reponse_comment_modere' => 'Váš komentár bol uložený a čaká na kontrolu predtým, ako bude publikovaný.',
+	'reponse_comment_ok' => 'Ďakujeme vám za komentár!',
+
+	// S
+	'saisie_texte_info' => 'Tento formulár akceptuje skratky SPIPU <code>[-&gt;urls] {{tučné}} {kurzíva} &lt;úvodzovky&gt; &lt;kód&gt;</code> a HTML kód <code>&lt;q&gt; &lt;del&gt; &lt;ins&gt;</code>. Ak chcete urobiť odsek, jednoducho vynechajte prázdne riadky.',
+	'saisie_texte_legend' => 'Sem napíšte svoj komentár',
+	'submit1' => 'Ukážka',
+	'submit2' => 'Poslať',
+
+	// T
+	'titre_comments' => 'Komentáre'
+);
+
+?>
