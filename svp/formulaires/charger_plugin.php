44d51bf07281e2509ad6c16180767f7b1e8774e4
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-06-15 12:52:39 +0000

    Premier jet de la nouvelle version de la page ajouter plugins
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@48809 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- svp/formulaires/charger_plugin.php
+++ svp/formulaires/charger_plugin.php
@@ -16,35 +16,51 @@ function formulaires_charger_plugin_verifier_dist(){
 }
 
 function formulaires_charger_plugin_traiter_dist(){
-	include_spip('inc/svp_rechercher');
 
 	$retour = array();
-	$phrase = _request('phrase');
-	$categorie = _request('categorie');
-	$etat = _request('etat');
-	$depot = _request('depot');
-	$doublon = (_request('doublon') == 'oui') ? true : false;
-	$tri = ($phrase) ? 'score' : 'nom';
-	$version_spip = $GLOBALS['spip_version_branche'].".".$GLOBALS['spip_version_code'];
-	$afficher_exclusions = false;
+	
+	if (_request('rechercher')) {
+		include_spip('inc/svp_rechercher');
 
-	// On recupere la liste des paquets:
-	// - sans doublons, ie on ne garde que la version la plus recente 
-	// - correspondant a ces criteres
-	// - compatible avec la version SPIP installee sur le site
-	// - et n'etant pas deja installes (ces paquets peuvent toutefois etre affiches)
-	// tries par nom ou score
-	$plugins = svp_rechercher_plugins_spip($phrase, $categorie, $etat, $depot, $version_spip,
-											svp_lister_plugins_installes(), $afficher_exclusions, $doublon, $tri);
-
-	// Determination des messages de retour
-	if (!$plugins)
-		$retour['message_erreur'] = _T('svp:message_ok_aucun_plugin_trouve');
+		// On a demande un recherche (bouton rechercher)
+		$phrase = _request('phrase');
+		$categorie = _request('categorie');
+		$etat = _request('etat');
+		$depot = _request('depot');
+		$doublon = (_request('doublon') == 'oui') ? true : false;
+		$tri = ($phrase) ? 'score' : 'nom';
+		$version_spip = $GLOBALS['spip_version_branche'].".".$GLOBALS['spip_version_code'];
+		$afficher_exclusions = false;
+	
+		// On recupere la liste des paquets:
+		// - sans doublons, ie on ne garde que la version la plus recente 
+		// - correspondant a ces criteres
+		// - compatible avec la version SPIP installee sur le site
+		// - et n'etant pas deja installes (ces paquets peuvent toutefois etre affiches)
+		// tries par nom ou score
+		$plugins = svp_rechercher_plugins_spip($phrase, $categorie, $etat, $depot, $version_spip,
+												svp_lister_plugins_installes(), $afficher_exclusions, $doublon, $tri);
+	
+		// Determination des messages de retour
+		if (!$plugins)
+			$retour['message_erreur'] = _T('svp:message_ok_aucun_plugin_trouve');
+		else {
+			$retour['message_ok']['resume'] = _T('svp:message_ok_plugins_trouves', 
+												array('nb_plugins' => count($plugins),
+													'tri' => _T('svp:info_tri_' . $tri)));
+			$retour['message_ok']['plugins'] = $plugins;
+		}
+	}
 	else {
-		$retour['message_ok']['resume'] = _T('svp:message_ok_plugins_trouves', 
-											array('nb_plugins' => count($plugins),
-												'tri' => _T('svp:info_tri_' . $tri)));
-		$retour['message_ok']['plugins'] = $plugins;
+		// On a demande une installation (bouton installer)
+		$plugins = _request('a_installer');
+		if (!$plugins) {
+			$retour['message_erreur'] = _T('svp:message_nok_aucun_plugin_selectionne');
+		}
+		else {
+			$charger = charger_fonction('charger_plugins','action');
+			$charger();
+		}
 	}
 	$retour['editable'] = true;
 
