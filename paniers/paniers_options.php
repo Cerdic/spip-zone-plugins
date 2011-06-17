45e3bbb94ae93f505dd0f49a7938caa81b8cef8d
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-06-17 15:25:42 +0000

    Bon ça c'est le minimum quand même... les bots n'ont pas de sessions. Mais ça ne règle pas le bug de base qui à priori est dans SPIP.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@48895 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- paniers/paniers_options.php
+++ paniers/paniers_options.php
@@ -7,13 +7,18 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 if (_DIR_RESTREINT) {
 	include_spip('inc/paniers');
 	include_spip('inc/session');
-	// Si on a bien un panier
-	if (_request('action') != 'cron' and $id_panier = paniers_id_panier_encours()){
-		// On met le panier dans la session
-		session_set('id_panier', $id_panier);
-	}
-	else{
-		session_set('id_panier');
+	
+	// Si c'est pas un bot :
+	if (!_IS_BOT){
+		// Si on a bien un panier
+		if (_request('action') != 'cron' and $id_panier = paniers_id_panier_encours()){
+			// On met le panier dans la session
+			session_set('id_panier', $id_panier);
+		}
+		// Sinon on s'assure que la session ne contient pas un ancien numéro
+		else{
+			session_set('id_panier');
+		}
 	}
 }
 
