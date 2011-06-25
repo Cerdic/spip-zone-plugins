858aedc53b050efce659a5e7e9bc816a680151c2
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-06-25 20:03:28 +0000

    Pourquoi faire ces inclusions si c'est un bot ?...
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@49050 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- paniers/paniers_options.php
+++ paniers/paniers_options.php
@@ -5,11 +5,11 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 
 // À chaque hit en partie publique, on va chercher le panier du visiteur actuel, et sinon le créer
 if (_DIR_RESTREINT) {
-	include_spip('inc/paniers');
-	include_spip('inc/session');
-	
 	// Si c'est pas un bot :
 	if (!_IS_BOT){
+		include_spip('inc/paniers');
+		include_spip('inc/session');
+	
 		// Si on a bien un panier
 		if (_request('action') != 'cron' and $id_panier = paniers_id_panier_encours()){
 			// On met le panier dans la session
