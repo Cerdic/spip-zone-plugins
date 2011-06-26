631536b8b3cbeb66acf3e190d3a2324d0bf972b6
patfr@ifrance.com <patfr@ifrance.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-06-26 23:21:42 +0000

    chtite précaution
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@49073 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- jeux/jeux_boutons.php
+++ jeux/jeux_boutons.php
@@ -1,19 +1,19 @@
 <?php
 
 function jeux_ajouterBoutons($boutons_admin) {
-    //test si le plugin bando est activé
+	//test si le plugin bando est activé
 	$liste_plugin = unserialize($GLOBALS['meta']['plugin']);
+	
+	if (is_array($liste_plugin) && array_key_exists('BANDO', $liste_plugin))
+		return $boutons_admin;
 
-	if (array_key_exists('BANDO',$liste_plugin)==true){
-	   return $boutons_admin;
-	}	
-		  // on voit le bouton dans la barre "naviguer"
-		  $boutons_admin['naviguer']->sousmenu["jeux_tous"]= new Bouton(
+	// on voit le bouton dans la barre "naviguer"
+	$boutons_admin['naviguer']->sousmenu["jeux_tous"]= new Bouton(
 			find_in_path("img/jeu-24.png"),  // icone
 			_T('jeux:jeux')	// titre
-			);
-		
-		return $boutons_admin;
+		);
+	
+	return $boutons_admin;
 }
 
 
