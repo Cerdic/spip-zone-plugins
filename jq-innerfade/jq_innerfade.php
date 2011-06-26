6e733d03ae47875656d3bd02b644267b1aac12e5
dwojylac@free.fr <dwojylac@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-06-26 12:43:34 +0000

    on repars à zéro
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@49054 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- jq-innerfade/jq_innerfade.php
+++
@@ -1,7 +0,0 @@
-<?php
-function jq_innerfade_inclure_headers($flux) {
-	$flux .= '<!-- plugin innerfade -->'."\n";
-	$flux .= '<script type="text/javascript" src="'.find_in_path('js/jquery.innerfade.js').'"></script>'."\n";
-	return $flux;
-}
-?>
