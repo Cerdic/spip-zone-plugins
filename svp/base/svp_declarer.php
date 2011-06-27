57d5a244672c7f32f1ece84e251d28c58ee84f85
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-06-27 17:07:32 +0000

    Gros ménage dans SVP maintenant que les squelettes publics sont transférés dans un plugin à part. Il restait aussi des scories de fichiers inutiles et un pipeline de trop suite à la squelettisation de l'espace privé.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@49092 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- svp/base/svp_declarer.php
+++ svp/base/svp_declarer.php
@@ -74,7 +74,7 @@ function svp_declarer_tables_objets_sql($tables) {
 			"branches_spip"	=> "varchar(255) DEFAULT '' NOT NULL"), // union des branches spip supportees par les paquets du plugin
 		'key'					=> array(
 			"PRIMARY KEY"	=> "id_plugin",
-			"KEY"	=> "prefixe"),
+			"KEY"			=> "prefixe"),
 		'tables_jointures'		=> array('id_depot' => 'depots_plugins'),
 		'principale'			=> 'oui',
 
--- svp/svp_pipelines.php
+++ svp/svp_pipelines.php
@@ -1,25 +1,4 @@
 <?php
-/**
- * Insertion dans le pipeline affiche_enfants
- * Affiche un bloc avec la liste des paquets associes au depot affiche
- *
- * @param object $flux
- * @return $flux
- */
-function svp_affiche_enfants($flux){
-	if ($flux['args']['exec'] == 'depots') {
-		$id_depot = $flux['args']['id_depot'];
-		$flux['data'] .= recuperer_fond('prive/squelettes/liste/paquets_plugin_depot', 
-			array('id_depot'=>$id_depot, 
-				'titre' => _T('svp:titre_boite_paquets_plugin_depot'), 
-				'bloc' => '_paquets_plugin'));
-		$flux['data'] .= recuperer_fond('prive/squelettes/liste/paquets_autre_depot', 
-			array('id_depot'=>$id_depot, 
-				'titre' => _T('svp:titre_boite_paquets_autre_depot'), 
-				'bloc' => '_paquets_autre'));
-	}
-	return $flux;
-}
 
 /**
  * Insertion dans le pipeline header_prive_css
@@ -29,22 +8,8 @@ function svp_affiche_enfants($flux){
  * @return $flux
  */
 function svp_header_prive_css($flux){
-	$css = find_in_path('css/svp_prive.css');
+	$css = find_in_path('prive/themes/svp_prive.css');
 	$flux .= "\n<link rel='stylesheet' href='$css' type='text/css' />\n";
-	return $flux;
-}
-
-/**
- * Insertion dans le pipeline insert_head_css
- * Permet d'inserer les css necessaires aux page publiques de SVP
- *
- * @param object $flux
- * @return $flux
- */
-function svp_insert_head_css($flux) {
-	if ($f = find_in_path('css/svp_habillage.css')) {
-		$flux .= '<link rel="stylesheet" href="'.direction_css($f).'" type="text/css" media="all" />';
-	}
 	return $flux;
 }
 
