b3ce7e3a2bc42cfc4e9519e8130841ac16517b8a
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-06-28 13:48:54 +0000

    traduction en slovaque (jaro)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@49134 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ mediatheque/lang/gestdoc_sk.php
@@ -0,0 +1,156 @@
+﻿<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'aucun_document' => 'Žiaden dokument',
+	'aucun_file'=>'Žiaden súbor',
+	'aucun_image'=>'Žiaden obrázok',
+	'aucun_audio'=>'No sound',
+	'aucun_video'=>'No video',
+	'aucune_utilisation'=>'Tento dokument sa nepoužíva.',
+	'aucune_vignette' => 'Žiadna miniatúra',
+
+	
+	// B
+	'bouton_copier_local'=>'Skopírovať na stránku',
+	'bouton_download_depuis'=>'z(o):',
+	'bouton_download_local' => 'môjho počítača',
+	'bouton_download_par_mediatheque' => 'knižnice multimédií',
+	'bouton_download_par_ftp' => 'servera',
+	'bouton_download_sur_le_web' => 'internetu',
+	'bouton_attacher_document' => 'Pripojiť',
+	'bouton_enlever_document' => 'Zrušiť prepojenie',
+	'bouton_enlever_supprimer_document' => 'Odstrániť',
+	'bouton_modifier_document' => 'Upraviť',
+	'bouton_parcourir' => 'Prehľadávať...',
+	'bouton_refresh_document' => 'Obnoviť tento dokument',
+	'bouton_remplacer_image' => 'Nahradiť tento obrázok',
+	'bouton_remplacer_document' => 'Nahradiť tento dokument',
+	'bouton_remplacer_image_document' => 'Nahradiť dokumentom alebo obrázkom',
+	'bouton_supprimer' => 'Odstrániť',
+	'bouton_upload' => 'Nahrať',
+
+	
+	// D
+	'documents'=> 'Knižnica multimédií',
+	'documents_distants'=> 'Vzdialené',
+	'documents_locaux'=> 'Lokálne',
+	'documents_non_publies'=> 'Nepublikované',
+	'documents_publies'=> 'Publikované',
+	'documents_tous'=> 'Všetky',
+	'documents_brises' => 'Pokazené',
+	'documents_orphelins' => 'Nepoužívané',
+	'descriptif'=>'Popis',
+	'des_documents'=>'Súborov: @nb@',
+	'des_files'=>'Dokumentov: @nb@',
+	'des_images'=>'Obrázkov: @nb@',
+	'des_audios'=>'Zvukov: @nb@',
+	'des_videos'=>'Videí: @nb@',
+	'des_utilisations'=>'Použití: @nb@',
+//There are two forms for plural of nouns, so I have translated it like this.
+	'document_installe_succes' => 'Súbor sa úspešne nahral',
+	'document_copie_locale_succes' => 'Súbor bol úspešne skopírovaný na stránku',
+	'nb_documents_installe_succes' => '@nb@ súborov sa úspešne nahralo',
+	'document_vu' => 'Vložiť do textu',
+	'document_attache_succes' => 'Dokument bol úspešne pridaný',
+	
+	// E
+	'erreur_indiquez_un_fichier'=>'Zadajte súbor!',
+	'erreur_aucun_document'=>'Tento dokument sa v knižnici multimédií nenachádza',
+	'erreur_aucun_fichier'=>'Žiaden súbor sa nenašiel',
+	'erreur_upload_type_interdit'=> 'Sťahovanie súborov @nom@ nie je povolené',
+	'erreur_chemin_ftp'=>'Zadaný súbor sa na serveri nenašiel',
+	'erreur_chemin_distant' => 'Vzdialený súbor @nom@ sa nepodarilo nájsť',
+	'erreur_copie_fichier' => 'Súbor @nom@ sa nedá skopírovať',
+	'erreur_format_fichier_image' => 'Formát @nom@ nie je vhodný pre obrázok',
+	'erreur_upload_vignette' => 'Chyba pri nahrávaní miniatúry @nom@',
+	
+	// F
+	'format_date_incorrect' => 'Nesprávny dátum alebo čas',
+	'fichier_modifie' => 'Súbor bol zmenený. Kliknite na tlačidlo Uložiť.',
+	
+	// I
+	'icone_creer_document' => 'Pridať dokument',
+	'id' => 'ID',
+	'infos'=>'Technické údaje',
+	'info_modifier_document'=>'Zmeniť dokument',
+	'info_heure' => 'Čas',
+	'info_largeur' => 'Šírka',
+	'info_hauteur' => 'Výška',
+	'info_gauche_numero_document'=>'DOKUMENT ČÍSLO',
+	'info_statut_document'=> 'Tento dokument je:',
+	'info_illustrations' => 'Ilustrácie',
+	'info_doc_max_poids' => 'Dokumenty musia mať menej ako @maxi@ (tento súbor má @actuel@).',
+	'info_image_max_poids' => 'Obrázky musia mať menej ako @maxi@ (tento súbor má @actuel@).',
+
+	// L
+	'logo' => 'Logo',
+	'label_refdoc_joindre' => 'Dokument číslo',
+	'label_parents' => 'Tento dokument je prepojený s&nbsp;prvkom',
+	'label_fichier' => 'Súbor',
+	'label_fichier_vignette' => 'Miniatúra',
+	'label_caracteristiques' => 'Vlastnosti',
+	'label_apercu' => 'Ukážka',
+	'label_credits' => 'Autori',
+	'lien_tout_enlever' => 'Odstrániť všetko',
+	
+	// M
+	'media_audio' => 'Zvuky',
+	'media_video' => 'Videá',
+	'media_image' => 'Obrázky',
+	'media_file' => 'Ostatné',
+	
+	// P
+	'par_id' => 'ID',
+	'par_date' => 'Dátum',
+	'par_taille' => 'Veľkosť',
+	'par_largeur' => 'Šírka',
+	'par_hauteur' => 'Výška',
+	'par_titre' => 'Názov',
+	
+	// T
+	'tous_les_medias'=>'Všetky multimédia',
+	
+	// U
+	'un_document' => '1 súbor',
+	'un_file'=>'1 dokument',
+	'un_image'=>'1 obrázok',
+	'un_audio'=>'1 zvuk',
+	'un_video'=>'1 video',
+	'une_utilisation'=>'1 použitie',
+	'upload_info_mode_document' => 'Poslať do portfólia',
+	'upload_info_mode_image' => 'Odstrániť z portfólia',
+	
+	//
+	'verifier_documents_brises' => 'Skontrolovať chýbajúce súbory',
+	'vignette_supprimee' => 'Miniatúra bola odstránená',
+
+	
+/*	'affichage' => 'Affichage :',
+	'attr_alt' => 'Pas de balise alt ?? ',
+	'conteneur' => 'Conteneur :',
+	'fichier_introuvable' => 'Fichier introuvable',
+	'filtrer' => 'Filtrer :',
+	'info_breve' => 'Breve ',
+	'info_breves' => 'Breves',
+	'info_doc' => 'Cette page r&eacute;capitule la liste de tous vos documents. Pour modifier les informations de chaque document, suivez le lien vers la page de sa rubrique.',
+	'info_reparer' => 'Cette page reconstitue les liens entre documents et articles dans lequel ils sont ins&eacute;r&eacute;s par un tag <code><imgxx> <docxx> ou <embxx></code>',
+	'info_rubrique' => 'Rubrique ',
+	'lien_ajoute' => 'lien ajout&eacute;',
+	'mis_jour_liens' => 'Liens mis a jour',
+	'mis_jour_tailles' => 'Mettre les tailles a jour',
+	'par' => 'Par @numero@',
+	'portfolio' => 'Portfolio',
+	'reparer_liens' => 'Reparer les liens',
+	'info_syndication' => 'Syndication',
+	'sans_titre_descriptif' => 'Sans titre ni descriptif',
+	'taille_erronee' => 'Taille erron&eacute;e',
+	'type' => 'Type :',
+	'tous' => 'Tous',
+	'tous_doc'  => 'Tous vos documents',
+	'tous_docs'  => 'Tous les Documents'*/
+);
+?>
