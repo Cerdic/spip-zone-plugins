fcb856e5c4e982a2abcad01697796ac86fdf751b
romy@rezo.net <romy@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-06-28 23:22:06 +0000

    Sans oublier les traductions
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@49163 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ galerie_carree/lang/local_fr.php
@@ -0,0 +1,14 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+'images' => 'images',
+'images_tout' => 'Voir toutes les images',
+'images_der' => 'Derni&egrave;res images',
+'cliquer_pour_agrandir' => 'Cliquez sur les vignettes pour agrandir.'
+
+);
+
+?>
