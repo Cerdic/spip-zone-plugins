a8580c15491b60dc4abce5dec44aca2ce9c30bea
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-07-03 12:36:54 +0000

    langues (noie)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@49270 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- noie/lang/noie_sk.php
+++ noie/lang/noie_sk.php
@@ -9,13 +9,13 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// N
 	'noie_info' => 'Pour naviguer de façon plus satisfaisante sur ce site et le reste du Web, nous vous recommandons d\'<strong>actualiser votre <a href="http://www.microsoft.com/france/windows/internet-explorer/">Internet Explorer</a></strong> ou d\'essayer un autre navigateur populaire comme <a href="http://www.mozilla-europe.org/fr/firefox/">Firefox</a>, <a href="http://www.google.fr/chrome">Chrome</a>, <a href="http://www.opera.com/">Opera</a> ou <a href="http://www.apple.com/fr/safari/">Safari</a>.', # NEW
-	'noie_titre' => 'Attention, votre Internet Explorer n\'est pas à jour !', # NEW
+	'noie_titre' => 'Pozor, váš Internet Explorer nie je aktuálny!',
 
 	// T
-	'toocool_alt' => '<NEW>Príliš kúlové pre IE',
-	'toocool_info' => '<NEW>Stránka, ktorú sa snažíte zobraziť, bola označená ake Príliš kúlová pre Internet Explorer.
+	'toocool_alt' => 'Príliš kúl pre IE',
+	'toocool_info' => 'Stránka, ktorú sa snažíte zobraziť, bola označená ako Príliš kúl pre Internet Explorer.
 <br />Prosím, doprajte si chvíľu na vyskúšanie jedného z týchto skvelých prehliadačov: <a href="http://www.w3junkies.com/toocool/index.php?language=en">http://www.w3junkies.com/toocool/</a>',
-	'toocool_titre' => '<NEW>Príliš kúlové pre Internet Explorer'
+	'toocool_titre' => 'Príliš kúl pre Internet Explorer'
 );
 
 ?>
