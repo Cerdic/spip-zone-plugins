add46e13f00fd0e901cc69f22d7e2b63192b5429
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-07-04 19:00:44 +0000

    debut d'ajout d'une fonction latex_propre
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@49323 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ latexwheel/latexwheel_fonctions.php
@@ -0,0 +1,30 @@
+<?php
+
+function propre_latex($t) {
+	static $wheel, $notes;
+
+
+	if (!isset($wheel)) {
+		$ruleset = SPIPTextWheelRuleset::loader(
+			$GLOBALS['spip_wheels']['raccourcis'],'personnaliser_raccourcis'
+		);
+		$wheel = new TextWheel($ruleset);
+
+		if (_request('var_mode') == 'wheel'
+		AND autoriser('debug')) {
+			$f = $wheel->compile();
+			echo "<pre>\n".htmlspecialchars($f)."</pre>\n";
+			exit;
+		}
+	}
+
+
+
+	$t = $wheel->text($t);
+
+
+
+	return $t;
+}
+
+?>
