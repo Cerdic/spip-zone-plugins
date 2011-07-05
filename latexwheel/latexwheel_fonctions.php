84a7230251aa76973230dbcb5b795598bded0d3a
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-07-05 17:06:23 +0000

    on pique à spip2latex le système pour traiter les modeles
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@49399 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- latexwheel/latexwheel_fonctions.php
+++ latexwheel/latexwheel_fonctions.php
@@ -3,8 +3,11 @@
 function propre_latex($t) {
 	
 	$t = echappe_html(latex_echappe_coloration($t)); 
+	
 	$t = appliquer_regles_wheel($t,array('latex/latex.yaml'));
+	$t = latex_traiter_modeles($t);
 	$t = echappe_retour($t);
+	
 	return $t;
 }
 
@@ -26,5 +29,50 @@ function latex_recuperer_php($t){
 	return str_replace('&lt;?','<?',$t);
 	
 }	
+
+function latex_traiter_modeles($texte) {
+	/* Je reprend le code des spip2latex_traiter_modeles du plugin spip2latex/*
+	include_spip('inc/lien');
+
+	/*
+	 * code, cadre/frame et math sont deja traites et sont base64-encodes
+	 * On ne devrait pas les voir ici.
+	 */
+	$modeles_builtin = array('<sc>', '<sup>', '<sub>', '<del>', '<quote>',
+				 '<cadre>', '<frame>', '<poesie>', '<poetry>',
+				 '<code>', '<math>');
+
+	$modele_regex = sprintf("@%s@is", _RACCOURCI_MODELE);
+	if (preg_match_all($modele_regex, $texte, $regs, PREG_SET_ORDER)) {
+		foreach ($regs as $reg) {
+			
+			/*
+			 * Seront traites plus tard.
+			 */
+			if (in_array(trim($reg[0]), $modeles_builtin))
+				continue;
+
+			/*	
+			 * Supprimer les echappements dans l'appel du
+			 * modele.
+			 * XXX seulement _ ?
+			 */
+			$modele = sprintf("<latex_%s", 
+					  substr($reg[0], 1));
+			$s = array("@\\_");
+			$r = array("_");
+			$modele = str_replace($s, $r, $modele);
+			
+			$search[] = $reg[0];
+			$replace[] = $modele;
+		}
+	
+		$texte = str_replace($search, $replace, $texte);
+		$texte = traiter_modeles($texte);
+	}
+
+	return $texte;
+}
+
 
 ?>
