c8248115ec5e9d4651ed1a3b0d57455223879511
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-07-05 15:50:58 +0000

    retour partiel sur [[49387]], on garde juste la fonction pour php
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@49396 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- latexwheel/latexwheel_fonctions.php
+++ latexwheel/latexwheel_fonctions.php
@@ -2,14 +2,9 @@
 
 function propre_latex($t) {
 	
-	$t = latex_echappe_coloration($t);
-
-	$t = echappe_html($t);
+	$t = echappe_html(latex_echappe_coloration($t)); 
 	$t = appliquer_regles_wheel($t,array('latex/latex.yaml'));
 	$t = echappe_retour($t);
-	
-	$t = latex_retour_coloration($t);
-
 	return $t;
 }
 
@@ -26,14 +21,6 @@ function appliquer_regles_wheel($texte,$regles){
 	return  $wheel->text($texte);
 }
 
-function latex_retour_coloration($t){
-	$t = preg_replace_callback(",<latex>(.*)</latex>,u",latex_retour_coloration_decode,$t);
-	return $t;	
-}
-
-function latex_retour_coloration_decode($t){
-	return base64_decode($t[1]);	
-}
 function latex_recuperer_php($t){
 	
 	return str_replace('&lt;?','<?',$t);
--- latexwheel/wheels/latex/latex-code.php
+++ latexwheel/wheels/latex/latex-code.php
@@ -1,17 +1,16 @@
 <?php
 
 function tw_code_latex($code){
-	$texte = str_replace($code[1],"\begin{minted}{".strtolower($code[3])."}",$code[0]);
-	$texte = str_replace('</code>','\end{minted}',$texte);
-	$texte = '<latex>'.base64_encode($texte).'</latex>';
-	return $texte;
+	if (count($code)>1)
+		return "<html>\begin{minted}{".strtolower($code[2])."}";
+	else
+		return 	"\end{minted}</html>";
 }
 
 function tw_cadre_latex($code){
-	$texte = str_replace($code[1],"\begin{minted}[linenos]{".strtolower($code[3])."}",$code[0]);
-	$texte = str_replace('</cadre>','\end{minted}',$texte);
-	$texte = '<latex>'.base64_encode($texte).'</latex>';
-
-	return $texte;
+	if (count($code)>1)
+		return "<html>\begin{minted}[linenos]{".strtolower($code[2])."}";
+	else
+		return 	"\end{minted}</html>";
 }
 ?>
