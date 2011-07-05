6778abbc5eb3b5f2ee165e6d1f7847a23c0236a2
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-07-05 15:08:18 +0000

    bon, pour les codes on met en base 64. Il faut aussi un filtre latex_retour_php pour éviter les &lt:php
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@49387 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- latexwheel/latexwheel_fonctions.php
+++ latexwheel/latexwheel_fonctions.php
@@ -1,12 +1,14 @@
 <?php
 
 function propre_latex($t) {
-
-	$t = echappe_html(latex_echappe_coloration($t));
 	
+	$t = latex_echappe_coloration($t);
+
+	$t = echappe_html($t);
 	$t = appliquer_regles_wheel($t,array('latex/latex.yaml'));
 	$t = echappe_retour($t);
-
+	
+	$t = latex_retour_coloration($t);
 
 	return $t;
 }
@@ -23,4 +25,19 @@ function appliquer_regles_wheel($texte,$regles){
 	$wheel = new TextWheel($ruleset);
 	return  $wheel->text($texte);
 }
+
+function latex_retour_coloration($t){
+	$t = preg_replace_callback(",<latex>(.*)</latex>,u",latex_retour_coloration_decode,$t);
+	return $t;	
+}
+
+function latex_retour_coloration_decode($t){
+	return base64_decode($t[1]);	
+}
+function latex_recuperer_php($t){
+	
+	return str_replace('&lt;?','<?',$t);
+	
+}	
+
 ?>
--- latexwheel/wheels/latex/latex-code.php
+++ latexwheel/wheels/latex/latex-code.php
@@ -1,16 +1,17 @@
 <?php
 
 function tw_code_latex($code){
-	if (count($code)>1)
-		return "<html>\begin{minted}{".strtolower($code[2])."}";
-	else
-		return 	"\end{minted}</html>";
+	$texte = str_replace($code[1],"\begin{minted}{".strtolower($code[3])."}",$code[0]);
+	$texte = str_replace('</code>','\end{minted}',$texte);
+	$texte = '<latex>'.base64_encode($texte).'</latex>';
+	return $texte;
 }
 
 function tw_cadre_latex($code){
-	if (count($code)>1)
-		return "<html>\begin{minted}[linenos]{".strtolower($code[2])."}";
-	else
-		return 	"\end{minted}</html>";
+	$texte = str_replace($code[1],"\begin{minted}[lineos]{".strtolower($code[3])."}",$code[0]);
+	$texte = str_replace('</cadre>','\end{minted}',$texte);
+	$texte = '<latex>'.base64_encode($texte).'</latex>';
+
+	return $texte;
 }
 ?>
