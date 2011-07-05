1377e2c659e5692d5b6baacaebcacee3a0bb7af7
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-07-05 21:23:21 +0000

    encore des problèmes de <code> : on n'analyse d'abord ceux qui ont un retour à la ligne. On suppose que l'auteur en met systématiquement avant un cadre ou un code coloré. Mais c'est pas terrible... faudrait que je revois mes expressions ref
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@49421 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- latexwheel/wheels/latex/latex-code.php
+++ latexwheel/wheels/latex/latex-code.php
@@ -2,14 +2,14 @@
 
 function tw_code_latex($code){
 	if (count($code)>1)
-		return "<html>\begin{minted}{".strtolower($code[2])."}";
+		return "<html>\begin{minted}{".strtolower($code[2])."}\n";
 	else
 		return 	"\end{minted}</html>";
 }
 
 function tw_cadre_latex($code){
 	if (count($code)>1)
-		return "<html>\begin{minted}[linenos]{".strtolower($code[2])."}";
+		return "<html>\begin{minted}[linenos]{".strtolower($code[2])."}\n";
 	else
 		return 	"\end{minted}</html>";
 }
