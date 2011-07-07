db0c8f89c085e08b3a65d39fab6d795dc797c787
gaitan@gmail.com <gaitan@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-07-07 08:35:03 +0000

    lang es
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@49473 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ disqus/lang/disqus_es.php
@@ -0,0 +1,20 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	'title' => 'Disqus',
+	'description' => 'Plataforma de comentarios Disqus para SPIP',
+	'configuration' => 'Variables de configuraci&oacute;n', 
+	'disqus_shortname' => 'Identificaci&oacute;n del sitio (Disqus shortname)',
+	'disqus_shortname_explication' => 'Para instalar Disqus, necesitar&aacute;s saber el identificador (<em>shortname</em>) de tu foro, 
+									   como fue registrado en <a href="http://disqus.com">Disqus</a>',
+
+
+);
+
+?>
