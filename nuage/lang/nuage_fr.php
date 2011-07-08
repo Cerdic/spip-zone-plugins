d3a5e5a4b4ee39235ddf161e278da326ff1b9595
yffic@lefourneau.com <yffic@lefourneau.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-07-08 15:55:48 +0000

    Une noisette pour Nuage
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@49517 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ nuage/lang/nuage_fr.php
@@ -0,0 +1,17 @@
+<?php
+
+// This is a SPIP-forum module file  --  Ceci est un fichier module de SPIP-forum
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// N
+'noisette_titre_defaut' => 'Tags',
+'noisette_nom_noisette' => 'Nuage',
+'noisette_description' => 'Affiche un nuage des mots cl&eacute;s',
+'noisette_label_afficher_titre' => 'Afficher le titre&nbsp;:',
+'noisette_titre_noisette' => 'Titre&nbsp;:',
+'noisette_label_niveau_titre' => 'Choisir le niveau du titre',
+
+);
+
+?>
