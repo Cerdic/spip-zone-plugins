93b297a316846c5805330b79b4b8721ef604c8ed
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-07-08 11:44:03 +0000

    Permet d'avoir le php coloré même sans <?php nécéssite la version 1.7 de minted.sty. À télécharger : http://minted.googlecode.com/hg/minted.sty . Mettre le fichier .sty à côté du fichier latex principal, à compiler. En attendant que l'auteur ait le temps de sortir la release
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@49510 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- latexwheel/wheels/latex/latex-code.php
+++ latexwheel/wheels/latex/latex-code.php
@@ -4,15 +4,28 @@ function tw_echappe_code_latex($code){
 }
 
 function tw_code_latex($code){
+	$lang = strtolower($code[2]);
+		if ($lang == 'php'){
+		$options =	'[startinline]';	// Permet d'avoir le php coloré même sans <?php nécéssite la version 1.7 de minted.sty. À télécharger : http://minted.googlecode.com/hg/minted.sty . Mettre le fichier .sty à côté du fichier latex principal, à compiler. En attendant que l'auteur ait le temps de sortir la release
+	}
+	else 
+		$options = '';
+	
 	if (count($code)>1)
-		return echappe_html("<html>\begin{minted}{".strtolower($code[2])."}\n",'latex');
+		return echappe_html("<html>\begin{minted}$options{".$lang."}\n",'latex');
 	else
 		return 	echappe_html("\n\end{minted}</html>",'latex');
 }
 
 function tw_cadre_latex($code){
+	$lang = strtolower($code[2]);
+	if ($lang == 'php'){
+		$options =	'[linenos,startinline]';
+	}
+	else 
+		$options = '[linenos]';
 	if (count($code)>1)
-		return echappe_html("<html>\begin{minted}[linenos]{".strtolower($code[2])."}\n",'latex');
+		return echappe_html("<html>\begin{minted}$options{$lang}\n",'latex');
 	else
 		return echappe_html("\n\end{minted}</html>",'latex');
 }
