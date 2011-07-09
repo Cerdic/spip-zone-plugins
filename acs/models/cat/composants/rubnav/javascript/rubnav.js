3025e57efdd6fbf87f62d6819ac54d74acf715bf
webmaster@geomaticien.com <webmaster@geomaticien.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-07-09 23:59:33 +0000

    Petite amélioration du javascript d'affichage des menus déroulants.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@49533 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- acs/models/cat/composants/rubnav/javascript/rubnav.js
+++ acs/models/cat/composants/rubnav/javascript/rubnav.js
@@ -3,7 +3,7 @@ jQuery(document).ready(
 	function() {
 		function _setHover() {
 			jQuery(".cRubnav li.menu-item:not(.on)").hover(function(){
-			  jQuery("ul.hidden:first", this).css("display", "none").show("fast");
+			  jQuery("ul.hidden", this).hide().filter(":first").show("fast");
 			},function(){
 				jQuery("ul.hidden", this).hide();
 			});
