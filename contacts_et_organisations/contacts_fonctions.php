e31a02e4ad3eb068daca259ab923bc9cbae645ab
ailynn@free.fr <ailynn@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-07-12 14:53:51 +0000

    Ajout : Espace privé, fiche organisation. Déplacement du contenu de la boîte d'information sans prive/infos pour permettre aux autres plugins d'utiliser le pipeline boite_infos
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@49564 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- contacts_et_organisations/contacts_fonctions.php
+++ contacts_et_organisations/contacts_fonctions.php
@@ -51,4 +51,29 @@ function balise_ORGANISATIONS_dist($p) {
 	return $p;
 }
 
+// Voir en ligne, ou apercu, ou rien (renvoie tout le bloc)
+function voir_contact_en_ligne_contact_organisation($type, $id){
+
+	$en_ligne = $message = '';
+
+	$image='racine-24.gif';
+	$en_ligne = 'calcul';
+	$af = 0;
+	$inline=0;
+
+	if ($en_ligne == 'calcul')
+		$message = _T('icone_voir_en_ligne');
+	else if ($en_ligne == 'preview'
+	AND autoriser('previsualiser'))
+		$message = _T('previsualiser');
+	else
+		return '';
+
+	$h = generer_url_public($type, "id_$type=$id&var_mode=$en_ligne");
+
+	return $inline  
+	  ? icone_inline($message, $h, $image, "rien.gif", $GLOBALS['spip_lang_left'])
+	: icone_horizontale($message, $h, $image, "rien.gif",$af);
+}
+
 ?>
