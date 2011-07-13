7f38cc1c90fc1859f4173a87a882860d311910cb
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-07-13 08:23:03 +0000

    is_numeric plutot
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@49571 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- gis/trunk/gis_pipelines.php
+++ gis/trunk/gis_pipelines.php
@@ -113,35 +113,35 @@ function gis_post_edition($flux){
 				spip_log("GIS EXIFS : Récuperation des coordonnees du fichier $fichier","gis");
 				
 				$LatDeg = explode("/",$exifs["GPSLatitude"][0]);
-				if(intval($LatDeg[1]) > 0)
+				if(is_numeric($LatDeg[1]) > 0)
 					$intLatDeg = $LatDeg[0]/$LatDeg[1];
 
 				$LatMin = explode("/",$exifs["GPSLatitude"][1]);
-				if(intval($LatMin[1]) > 0)
+				if(is_numeric($LatMin[1]) > 0)
 					$intLatMin = $LatMin[0]/$LatMin[1];
 
 				$LatSec = explode("/",$exifs["GPSLatitude"][2]);
-				if(intval($LatSec[1]) > 0)
+				if(is_numeric($LatSec[1]) > 0)
 					$intLatSec = $LatSec[0]/$LatSec[1];
 
 				$LongDeg = explode("/",$exifs["GPSLongitude"][0]);
-				if(intval($LongDeg[1]) > 0)
+				if(is_numeric($LongDeg[1]) > 0)
 					$intLongDeg = $LongDeg[0]/$LongDeg[1];
 
 				$LongMin = explode("/",$exifs["GPSLongitude"][1]);
-				if(intval($LongMin[1]) > 0)
+				if(is_numeric($LongMin[1]) > 0)
 					$intLongMin = $LongMin[0]/$LongMin[1];
 
 				$LongSec = explode("/",$exifs["GPSLongitude"][2]);
-				if(intval($LongSec[1]) > 0)
+				if(is_numeric($LongSec[1]) > 0)
 					$intLongSec = $LongSec[0]/$LongSec[1];
 
 				// round to 5 = approximately 1 meter accuracy
-				if(intval($intLatDeg) && intval($intLatMin) && intval($intLatSec))
+				if(is_numeric($intLatDeg) && is_numeric($intLatMin) && is_numeric($intLatSec))
 					$latitude = round(dms_to_dec($exifs["GPSLatitudeRef"],
 						$intLatDeg,$intLatMin,$intLatSec),5);
 
-				if(intval($intLongDeg) && intval($intLongMin) && intval($intLongSec))
+				if(is_numeric($intLongDeg) && is_numeric($intLongMin) && is_numeric($intLongSec))
 					$longitude =  round(dms_to_dec($exifs["GPSLongitudeRef"],
 						$intLongDeg,$intLongMin,$intLongSec), 5);
 				if($config['adresse'] == 'on'){
