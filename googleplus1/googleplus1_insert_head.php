038441dd66f032bf2e6a7ce86b15515d71d7df75
yaquoi@yaquoi.com <yaquoi@yaquoi.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-07-14 10:55:06 +0000

    introduction d'une methode javascript d'insertion dans le squelette (plus besoin de la balise)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@49598 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- googleplus1/googleplus1_insert_head.php
+++ googleplus1/googleplus1_insert_head.php
@@ -1,12 +1,38 @@
 <?php
 
+function googleplus1_css(){
+	return '<link rel="stylesheet" type="text/css" href="'.find_in_path('googleplus1.css').'" media="all" />'."\n";
+}
+
+/**
+ * ajout feuille de style dans le HEAD_CSS
+ * pris en charge correctement a partir de SPIP 3
+ * @param string $flux
+ * @return string
+ */
+function googleplus1_insert_head_css($flux){
+	if (intval($GLOBALS['spip_version_branche'])>=3)
+		$flux .= googleplus1_css();
+	return $flux;
+}
+
 function googleplus1_insert_head($flux){
+	if (intval($GLOBALS['spip_version_branche'])<3)
+	$flux .= googleplus1_css();
 
 	$googleplus1_lang = lire_config('langue_site');
 	$flux .= '
 		<script type="text/javascript" src="https://apis.google.com/js/plusone.js">
 		{lang: \''.$googleplus1_lang.'\'}
 		</script>';
+	include_spip('inc/filtres');
+	if (function_exists('produire_fond_statique'))
+		$jsFile = produire_fond_statique('googleplus1.js');
+	else
+		$jsFile = generer_url_public('googleplus1.js');
+
+	$flux .= "<script src='$jsFile' type='text/javascript'></script>\n";
+	return $flux;
 	return $flux;
 }
 ?>
--- googleplus1/lang/googleplus1_fr.php
+++ googleplus1/lang/googleplus1_fr.php
@@ -3,9 +3,23 @@
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-'taillepetit' => 'Petit (15 px)',
-'taillemoyen' => 'Moyen (20 px)',
-'taillestandard' => 'Standard (24 px)',
-'taillegrand' => 'Grand (60 px)'
+	
+	//C	
+	'cfg_en_bas' => 'En bas de chaque page',
+	'cfg_exemples' => 'Exemples (sur les squelettes dist) :',
+	'cfg_explication_boite_info' => 'Ici vous pouvez choisir la taille et la position du bouton google+1 dans vos pages.',
+	'cfg_inserer' => 'Ins&#233;rer le bloc google+1 en-dessous du bloc suivant :',
+	'cfg_insertion' => 'Insertion dans les pages :',
+	'cfg_selector' => 'S&#233;lecteur :',
+	'cfg_sous_bloc' => 'Sous le bloc de titre des pages article',
+	'cfg_sous_div' => 'Sous la div nomm&#233;e id=google+1',
+	'cfg_titre_boite_info' => 'Configuration du plugin Google+1',
+	'cfg_titre_google+1' => 'Google+1',
+	'cfg_taille' => 'Choisir la taille du bouton :',
+	'cfg_taillepetit' => 'Petit (15 px)',
+	'cfg_taillemoyen' => 'Moyen (20 px)',
+	'cfg_taillestandard' => 'Standard (24 px)',
+	'cfg_taillegrand' => 'Grand (60 px)',
+	'cfg_titre_googleplus1' => 'Google+1',
 );
 ?>
