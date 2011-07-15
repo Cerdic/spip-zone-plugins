4d7ba166ee6e25b57af5754bd9d10e7010562056
paladin@quesaco.org <paladin@quesaco.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-07-15 16:39:29 +0000

    Documentation pour phpdoc.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@49661 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- auteurs_supprimer/exec/auteurs_supprimer.php
+++ auteurs_supprimer/exec/auteurs_supprimer.php
@@ -1,5 +1,22 @@
 <?php
 
+/**
+ * Permet de supprimer plusieurs auteurs par un seul formulaire.
+ *
+ * Si SPIP-Listes actif, supprime également les abonnements
+ * aux listes et le format de réception de l'auteur.
+ *
+ * Ne permet pas de supprimer les auteurs
+ * qui ont un article.
+ *
+ * Squelette à appeler via :
+ * 	http://<votredomain>/?page=auteurs_supprimer
+ * ou via le bouton dans sous-menu des auteurs
+ *
+ * @author Christian Paulus
+ * @license GPLv3
+ * @version 20110714
+ */
 
 // $LastChangedRevision$
 // $LastChangedBy$
