9dd0960ca96fbe1f64052a0823b2dbeee59e8169
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-07-15 15:10:08 +0000

    langues (genespip)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@49640 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ genespip_2_0/lang/genespip_sk.php
@@ -0,0 +1,225 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'a_ete_mise_a_la_poubelle' => 'bol hodený do koša',
+	'acces_non' => 'Prístupové práva » obmedzené',
+	'acces_oui' => 'Droits d´accès » détails des fiches', # NEW
+	'acces_restreint' => 'Prístup na túto stránku je obmedzený',
+	'administrateur' => 'Administrátor',
+	'adresse' => 'Adresa',
+	'ajout_evenement' => 'Pridať novú udalosť',
+	'ajout_lieu' => 'Pridať miesto',
+	'ajout_media' => 'Pridať multimédium',
+	'ajouter_nouveau_lien_article_spip_existant' => 'Pridať nový odkaz na existujúci článok v SPIPE',
+	'annuler' => 'Zrušiť',
+	'arborescence_des' => 'Stromová štruktúra',
+	'arbre_asc' => 'Spodná časť stromu',
+	'article_spip_lie' => 'Odkaz na článok v SPIPE číslo',
+	'aucune_fiche_possede_numero' => 'Takéto číslo nemá žiaden záznam ',
+	'avec' => 'avec', # NEW
+
+	// B
+	'base_contient' => 'Databáza obsahuje',
+	'base_genespip' => 'Base GeneSpip', # NEW
+
+	// C
+	'cellules_creees' => 'Cellules créées', # NEW
+	'cellules_occupees' => 'Cellules occupées', # NEW
+	'centans' => 'Interdire l‘affichage des dates de moins de 100 ans', # NEW
+	'charger' => 'Charger', # NEW
+	'choisir' => 'Choisir', # NEW
+	'cliquer_ici_pour_lier_article_avec_fiche' => 'Cliquer ici pour lier l´article à la fiche', # NEW
+	'confirmer' => 'Confirmer', # NEW
+	'contact' => 'Contact', # NEW
+	'continuer' => 'Continuer', # NEW
+	'creation_annulee' => 'Création annulée', # NEW
+	'creation_fiche' => 'Création de la fiche', # NEW
+	'creer' => 'Créer', # NEW
+	'creer_document' => '›› Créer un document ››', # NEW
+
+	// D
+	'date' => 'Date', # NEW
+	'debut_gedcom' => 'Début GedCom', # NEW
+	'deces' => 'Décès', # NEW
+	'dep' => 'Dep.', # NEW
+	'departement' => 'Département', # NEW
+	'derniere_modif_le' => 'Derniére moodif le', # NEW
+	'derniere_modification' => 'Dernière mise à jour de la fiche', # NEW
+	'descriptif_cfg' => '<p>Cette zone permet de configurer les variables de base du plugin.</p><p> Pour fonctionner correctement ce plugin doit être installé en parallèle avec le plugin cfg</p>', # NEW
+	'detail_fiche' => 'Détail fiche', # NEW
+	'document' => 'Documents', # NEW
+
+	// E
+	'enfant' => 'enfant', # NEW
+	'enfants' => 'Enfants', # NEW
+	'enfants_couple' => 'Enfants du couple', # NEW
+	'epouse' => 'épouse', # NEW
+	'epoux' => 'époux', # NEW
+	'est_present_dans_base' => 'est prèsent dans la base', # NEW
+	'evenement' => 'Évènement', # NEW
+	'evenements' => 'Évènements', # NEW
+	'evenements_lies' => 'Evènements liés', # NEW
+	'export_gedcom' => 'Export GedCom', # NEW
+	'export_termine' => 'Export terminé', # NEW
+	'exporter' => 'Exporter', # NEW
+
+	// F
+	'famille' => 'Famille', # NEW
+	'fermer' => 'Fermer', # NEW
+	'fiche_de' => 'Fiche de', # NEW
+	'fiche_detail' => 'Fiche détail', # NEW
+	'fiche_document' => 'Fiche document', # NEW
+	'fiche_enfant' => 'Fiche enfant', # NEW
+	'fiche_evt_par_lieu' => 'Fiche évènements par lieu', # NEW
+	'fiche_lieux' => 'Fiche lieux', # NEW
+	'fiche_no' => 'Fiche nº', # NEW
+	'fiche_num' => 'Fiche N°', # NEW
+	'fiche_parents' => 'Fiche parents', # NEW
+	'fiche_union' => 'Fiche union', # NEW
+	'fiches' => 'Fiches', # NEW
+	'fichier' => 'Fichier', # NEW
+	'fichier_gedcom' => 'Fichier GedCom', # NEW
+
+	// G
+	'galerie' => 'Galerie', # NEW
+	'gedcom' => 'GedCom', # NEW
+	'genealogie' => 'Généalogie', # NEW
+	'genespip' => 'GeneSPIP', # NEW
+
+	// I
+	'indication_format_photo' => '<br />Poid max. 100 ko', # NEW
+	'individu' => 'individu', # NEW
+	'info_centans' => '<p>Interdire systématiquement l‘affichage des dates ayant moins de 100 ans.<br />Cette option ne s‘applique que pour les visiteurs<br />Si cette option est désactivée, il est possible de limiter l‘affichage fiche par fiche.</p>', # NEW
+	'info_deces' => 'Info décés', # NEW
+	'info_doc' => '<p>Plugin de généalogie.</p> <p>Publier votre généalogie sur internet à partir d´un site SPIP.</p>', # NEW
+	'info_gedcom_etape1' => 'Votre arbre est vide, importer un fichier GedCom (1 Mo Max) ou créer une nouvelle fiche.<br />', # NEW
+	'info_gedcom_etape2' => 'Afin d´importer votre fichier GedCom, GeneSPIP a besoin de connaître votre méthode de traitement de l´entrée &quot;PLAC&quot;<br />GeneSPIP a sélectionner les 5 premières entrées &quot;PLAC&quot;, Choisissez en une qui servira de référence pour la suite', # NEW
+	'info_gedcom_etape3' => 'A partir de l´entrée choisie, sélectionnez pour chaque information la manière dont GeneSPIP doit la traduire', # NEW
+	'info_mariage' => 'Info mariage', # NEW
+	'info_multilingue' => '<p>Afficher la bannière multilingue sur le site.<br />Allez dans le menu multilingue de SPIP pour désactiver les langues inutiles</p>', # NEW
+	'info_naissance' => 'Info naissance', # NEW
+	'info_pub' => '<p>Les revenus publicitaires sont déstinés au créateur de GeneSPIP, vous pouvez placer votre propre banniere publicitaire en modifiant le contenu du fichier pub/200x200genespip.html, abonnez-vous au programme google adsense si vous souhaitez créer votre banniere</p>', # NEW
+	'info_restriction_acces' => '<p>Limiter les accès aux détails des fiches sur le site public</p>', # NEW
+	'info_restriction_acces0' => '<b>0</b> : Limitation aux administrateurs', # NEW
+	'info_restriction_acces1' => '<b>1</b> : Limitation aux administrateurs et rédacteurs', # NEW
+	'info_restriction_acces2' => '<b>2</b> : Limitation aux administrateurs, rédacteurs et visiteurs(privée)', # NEW
+	'info_restriction_acces3' => '<b>3</b> : Aucune limitation', # NEW
+	'informations_sur_la_base' => 'Informations sur la base', # NEW
+	'inscription' => 'Inscription', # NEW
+	'invalide' => 'Invalide', # NEW
+
+	// J
+	'journal' => 'Journal d‘évènements', # NEW
+
+	// L
+	'le' => 'le', # NEW
+	'les_3_dernieres_publications_dans' => 'Les 3 dernières publications dans', # NEW
+	'lieu' => 'lieu', # NEW
+	'lieux' => 'Lieux', # NEW
+	'limitation' => 'Rendre invisible les dates sur le site publique', # NEW
+	'limitation_non' => '(» Dates visibles)', # NEW
+	'limitation_oui' => '(» Dates invisibles)', # NEW
+	'liste_des_articles' => 'Liste des articles SPIP', # NEW
+	'liste_des_enfants' => 'Liste des enfants', # NEW
+	'liste_des_lieux' => 'Liste des lieux', # NEW
+	'liste_des_personnes_nees' => 'Listes des personnes nées', # NEW
+	'liste_des_unions' => 'Liste des unions', # NEW
+	'liste_noms' => 'Liste des noms', # NEW
+	'liste_patronyme' => 'Liste des patronymes', # NEW
+
+	// M
+	'mariage' => 'mariage', # NEW
+	'mariage_le' => 'Mariage le', # NEW
+	'media' => 'Média', # NEW
+	'mere' => 'Mère', # NEW
+	'mere_inconnu' => 'Mère inconnu', # NEW
+	'metier' => 'Métier', # NEW
+	'mettre_jour_base_fichier_gedcom' => 'Mettre à jour la base avec un fichier GedCom (1 Mo Max)', # NEW
+	'mise_a_jour_liste_eclair' => 'Mise à jour de la liste éclair', # NEW
+	'modifier' => 'Modifier', # NEW
+	'modifier_fiche' => 'Modifier la fiche', # NEW
+	'mon_pagerank' => 'Mon PageRank', # NEW
+	'multilingue' => 'Site multilingue', # NEW
+
+	// N
+	'naissance' => 'Naissance', # NEW
+	'nettoyage_champ_nom' => 'Nettoyage champ NOM', # NEW
+	'nettoyage_table_lieux' => 'Nettoyage table LIEUX', # NEW
+	'nom' => 'Nom', # NEW
+	'nombre_de_fiches' => 'Nombre de fiches', # NEW
+	'nombre_de_patronymes' => 'Nombre de patronymes', # NEW
+	'note' => 'Note', # NEW
+	'note_individu' => 'Note individu', # NEW
+	'nouvelle_fiche' => 'Nouvelle fiche', # NEW
+	'nouvelle_liaison_document_realise' => 'Nouvelle liaison ”document“ réalisée', # NEW
+	'nouvelle_union' => 'Nouvelle union', # NEW
+	'num_departement' => 'Nº Dép.', # NEW
+
+	// P
+	'pagerank_actuel' => 'PageRank actuel', # NEW
+	'parente' => 'Parenté', # NEW
+	'parents' => 'Parents', # NEW
+	'patronymes' => 'Patronymes', # NEW
+	'pays' => 'Pays', # NEW
+	'pere' => 'Père', # NEW
+	'pere_inconnu' => 'Père inconnu', # NEW
+	'photo' => 'Photo', # NEW
+	'photos' => 'Photos', # NEW
+	'portrait' => 'Portrait', # NEW
+	'poubelle' => 'Poubelle', # NEW
+	'prenom' => 'Prénom', # NEW
+	'pub' => 'pub', # NEW
+	'publication' => 'Publication', # NEW
+
+	// R
+	'redacteur' => 'Rédacteur', # NEW
+	'region' => 'Région', # NEW
+	'requete' => 'Requête', # NEW
+	'requete_invalide' => 'Requête invalide', # NEW
+	'restaure' => 'Restaure', # NEW
+	'restreint' => 'Restreint', # NEW
+	'restriction_acces' => 'Restriction d´accès', # NEW
+	'retour_fiche' => 'Retour fiche', # NEW
+	'retour_sur_fiche' => 'Retour sur la fiche', # NEW
+	'retour_sur_fiche_sans_enregistrer' => 'Retour sur la fiche sans enregistrer', # NEW
+
+	// S
+	'sexe' => 'Sexe', # NEW
+	'signature' => 'Signature', # NEW
+	'signatures' => 'Signatures', # NEW
+	'site_genealogie_genespip' => 'Site de généalogie par GENESPIP', # NEW
+	'source' => 'Source', # NEW
+	'statistiques' => 'Statistiques', # NEW
+	'suppression_table_temporaire' => 'Suppresion de la table temporaire', # NEW
+	'supprimer' => 'Supprimer', # NEW
+
+	// T
+	'table_lieux_cree' => 'Table des lieux créé', # NEW
+	'tableau_ocupe_a' => 'Tableau occupé à', # NEW
+	'telecharger' => 'Télécharger', # NEW
+	'telecharger_succes' => 'Télécharger avec succés', # NEW
+	'theme' => 'Thème', # NEW
+	'themes' => 'Thèmes', # NEW
+	'titre_menu_genespip' => 'GeneSPIP', # NEW
+
+	// U
+	'union' => 'Unions, mariages', # NEW
+	'union_fiche_no' => 'Union fiche n&ordm', # NEW
+
+	// V
+	'valider' => 'Valider', # NEW
+	'version_base_genespip' => 'Version Base GeneSPIP', # NEW
+	'version_plugin' => 'Version Plugin', # NEW
+	'version_squelette' => 'Version Squelette', # NEW
+	'ville' => 'Ville', # NEW
+	'visiteur' => 'Visiteur' # NEW
+);
+
+?>
