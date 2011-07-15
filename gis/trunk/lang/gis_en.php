9e0edd2f6445099261cc83e166be18293d30b60c
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-07-15 15:10:07 +0000

    langues (gis)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@49639 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- gis/trunk/lang/gis_en.php
+++ gis/trunk/lang/gis_en.php
@@ -1,72 +1,78 @@
 <?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-	//A
+
+	// A
 	'aucun_gis' => 'No point',
 	'aucun_objet' => 'No object',
 
 	// B
 	'bouton_lier' => 'Link this point',
-	'bouton_supprimer_lien' => 'Remove this link',
 	'bouton_supprimer_gis' => 'Delete this point permanently',
+	'bouton_supprimer_lien' => 'Remove this link',
 
 	// C
-	"cfg_descr_gis" => "Geographic Information System.<br />Link to the documentation.",
-	"cfg_inf_adresse" => "Displays additional address fields (country, city, state, address ...)",
-	"cfg_inf_geolocaliser_user_html5" => "If the user's browser allows it, its approximate geographic location is retrieved to give the default position when creating a new point.",
-	"cfg_inf_cloudmade" => "This API needs a key you can create on <a href='@url@'>the CloudMade website</a>.",
-	"cfg_inf_geocoder" => "Enable search function in form.",
-	"cfg_inf_google" => "This API needs a key you can create on <a href='@url@'>the GoogleMaps website</a>.",
-	'cfg_lbl_adresse' => "Show address fields",
-	"cfg_lbl_api" => "Geolocation API",
-	"cfg_lbl_api_cloudmade" => "CloudMade",
-	"cfg_lbl_api_google" => "Google Maps v2",
-	"cfg_lbl_api_googlev3" => "Google Maps v3",
-	"cfg_lbl_api_key_cloudmade" => "CloudMade API key",
-	"cfg_lbl_api_key_google" => "GoogleMaps API key",
-	"cfg_lbl_api_mapquest" => "MapQuest",
-	"cfg_lbl_api_microsoft" => "Microsoft Bing",
-	"cfg_lbl_api_multimap" => "MultiMap",
-	"cfg_lbl_api_openlayers" => "OpenLayers",
-	"cfg_lbl_api_openspace" => "OpenSpace",
-	"cfg_lbl_api_yahoo" => "Yahoo",
-	"cfg_lbl_geocoder" => "Geocoder",
-	"cfg_lbl_geolocaliser_user_html5" => "At the creation step, center the map on the location of the user",
-	"cfg_titre_gis" => "GIS",
-	
+	'cfg_descr_gis' => 'Geographic Information System.<br />Link to the documentation.',
+	'cfg_inf_adresse' => 'Displays additional address fields (country, city, state, address ...)',
+	'cfg_inf_cloudmade' => 'This API needs a key you can create on <a href=\'@url@\'>the CloudMade website</a>.',
+	'cfg_inf_geocoder' => 'Enable search function in form.',
+	'cfg_inf_geolocaliser_user_html5' => 'If the user\'s browser allows it, its approximate geographic location is retrieved to give the default position when creating a new point.',
+	'cfg_inf_google' => 'This API needs a key you can create on <a href=\'@url@\'>the GoogleMaps website</a>.',
+	'cfg_lbl_adresse' => 'Show address fields',
+	'cfg_lbl_api' => 'Geolocation API',
+	'cfg_lbl_api_cloudmade' => 'CloudMade',
+	'cfg_lbl_api_google' => 'Google Maps v2',
+	'cfg_lbl_api_googlev3' => 'Google Maps v3',
+	'cfg_lbl_api_key_cloudmade' => 'CloudMade API key',
+	'cfg_lbl_api_key_google' => 'GoogleMaps API key',
+	'cfg_lbl_api_mapquest' => 'MapQuest',
+	'cfg_lbl_api_microsoft' => 'Microsoft Bing',
+	'cfg_lbl_api_multimap' => 'MultiMap',
+	'cfg_lbl_api_openlayers' => 'OpenLayers',
+	'cfg_lbl_api_openspace' => 'OpenSpace',
+	'cfg_lbl_api_yahoo' => 'Yahoo',
+	'cfg_lbl_geocoder' => 'Geocoder',
+	'cfg_lbl_geolocaliser_user_html5' => 'At the creation step, center the map on the location of the user',
+	'cfg_titre_gis' => 'GIS',
+
 	// E
-	"editer_gis_editer" => "Edit this point",
-	"editer_gis_explication" => "This page lists the whole geolocated points of the website.",
-	"editer_gis_nouveau" => "Create a new point",
-	"editer_gis_titre" => "The geolocated points",
-	"erreur_recherche_pas_resultats" => "No point corresponds to the searched text.",
+	'editer_gis_editer' => 'Edit this point',
+	'editer_gis_explication' => 'This page lists the whole geolocated points of the website.',
+	'editer_gis_nouveau' => 'Create a new point',
+	'editer_gis_titre' => 'The geolocated points',
+	'erreur_recherche_pas_resultats' => 'No point corresponds to the searched text.',
 
 	// F
-	"formulaire_creer_gis" => "Create a new geolocated point :",
-	"formulaire_modifier_gis" => "Modify the geolocated point :",
+	'formulaire_creer_gis' => 'Create a new geolocated point :',
+	'formulaire_modifier_gis' => 'Modify the geolocated point :',
 
 	// I
-	"icone_gis_tous" => "Geolocated points",
-	"info_numero_gis" => "Point number",
-	"info_id_objet" => "N&deg;",
-	"info_liste_gis_objet" => "Linked points to this object",
-	"info_liste_objets_gis" => "Objects attached to this point",
-	"info_objet" => "Object",
-	"info_supprimer_lien" => "Detach",
-	"info_voir_fiche_objet" => "Go to page",
-	
+	'icone_gis_tous' => 'Geolocated points',
+	'info_id_objet' => 'N&deg;',
+	'info_liste_gis_objet' => 'Linked points to this object',
+	'info_liste_objets_gis' => 'Objects attached to this point',
+	'info_numero_gis' => 'Point number',
+	'info_objet' => 'Object',
+	'info_supprimer_lien' => 'Detach',
+	'info_voir_fiche_objet' => 'Go to page',
+
 	// L
-	"label_adress" => "Addresse",
-	"label_code_postal" => "Postal code",
-	"label_pays" => "Country",
-	"label_rechercher_address" => "Search for an address",
-	"label_rechercher_point" => "Search for a point",
-	"label_region" => "Region",
-	"label_ville" => "Town",
-	"lat" => "Latitude",
-	"libelle_logo_gis" => "POINT\'S LOGO",
-	"lon" => "Longitude",
-	
+	'label_adress' => 'Addresse',
+	'label_code_postal' => 'Postal code',
+	'label_pays' => 'Country',
+	'label_rechercher_address' => 'Search for an address',
+	'label_rechercher_point' => 'Search for a point',
+	'label_region' => 'Region',
+	'label_ville' => 'Town',
+	'lat' => 'Latitude',
+	'libelle_logo_gis' => 'POINT\\\'S LOGO',
+	'lon' => 'Longitude',
+
 	// T
 	'titre_bloc_creer_point' => 'Link a new point',
 	'titre_bloc_points_lies' => 'Linked points',
@@ -75,8 +81,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'titre_nombre_utilisations' => '@nb@ uses',
 
 	// Z
-	"zoom" => "Zoom"
-		
+	'zoom' => 'Zoom'
 );
 
 ?>
--- gis/trunk/lang/gis_fr.php
+++ gis/trunk/lang/gis_fr.php
@@ -1,82 +1,85 @@
 <?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/gis/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-	//A
+
+	// A
 	'aucun_gis' => 'Aucun point',
 	'aucun_objet' => 'Aucun objet',
-	
+
 	// B
 	'bouton_lier' => 'Lier ce point',
-	'bouton_supprimer_lien' => 'Supprimer ce lien',
 	'bouton_supprimer_gis' => 'Supprimer d&eacute;finitivement ce point',
+	'bouton_supprimer_lien' => 'Supprimer ce lien',
 
 	// C
-	"cfg_descr_gis" => "Syst&egrave;me d'Information G&eacute;ographique.<br />Lien vers la documentation.",
-	"cfg_inf_adresse" => "Affiche des champs suppl&eacute;mentaires d'adresse (pays, ville, r&eacute;gion, adresse...)",
-	"cfg_inf_cloudmade" => "Cette API n&eacute;cessite une cl&eacute; &agrave; cr&eacute;er sur <a href='@url@'>le site de CloudMade</a>.",
-	"cfg_inf_geocoder" => "Activer la fonction de recherche &agrave; partir d'une adresse dans le formulaire.",
-	"cfg_inf_geolocaliser_user_html5" => "Si le navigateur de l'utilisateur le permet, son emplacement g&eacute;ographique approximatif est r&eacute;cup&eacute;r&eacute; pour donner la position par d&eacute;faut lors de la cr&eacute;ation d'un point.",
-	"cfg_inf_google" => "Cette API n&eacute;cessite une cl&eacute; &agrave; cr&eacute;er sur <a href='@url@'>le site de GoogleMaps</a>.",
-	"cfg_lbl_adresse" => "Afficher les champs d'adresse",
-	"cfg_lbl_api" => "API de cartographie",
-	"cfg_lbl_api_cloudmade" => "CloudMade",
-	"cfg_lbl_api_google" => "Google Maps v2",
-	"cfg_lbl_api_googlev3" => "Google Maps v3",
-	"cfg_lbl_api_key_cloudmade" => "Cl&eacute; CloudMade",
-	"cfg_lbl_api_key_google" => "Cl&eacute; GoogleMaps",
-	"cfg_lbl_api_mapquest" => "MapQuest",
-	"cfg_lbl_api_microsoft" => "Microsoft Bing",
-	"cfg_lbl_api_multimap" => "MultiMap",
-	"cfg_lbl_api_openlayers" => "OpenLayers",
-	"cfg_lbl_api_openspace" => "OpenSpace",
-	"cfg_lbl_api_yahoo" => "Yahoo",
-	"cfg_lbl_geocoder" => "Geocoder",
-	"cfg_lbl_geolocaliser_user_html5" => "Centrer la carte sur l'emplacement de l'utilisateur &agrave; la cr&eacute;ation",
-	"cfg_titre_gis" => "GIS",
-	
+	'cfg_descr_gis' => 'Syst&egrave;me d\'Information G&eacute;ographique.<br />Lien vers la documentation.',
+	'cfg_inf_adresse' => 'Affiche des champs suppl&eacute;mentaires d\'adresse (pays, ville, r&eacute;gion, adresse...)',
+	'cfg_inf_cloudmade' => 'Cette API n&eacute;cessite une cl&eacute; &agrave; cr&eacute;er sur <a href=\'@url@\'>le site de CloudMade</a>.',
+	'cfg_inf_geocoder' => 'Activer la fonction de recherche &agrave; partir d\'une adresse dans le formulaire.',
+	'cfg_inf_geolocaliser_user_html5' => 'Si le navigateur de l\'utilisateur le permet, son emplacement g&eacute;ographique approximatif est r&eacute;cup&eacute;r&eacute; pour donner la position par d&eacute;faut lors de la cr&eacute;ation d\'un point.',
+	'cfg_inf_google' => 'Cette API n&eacute;cessite une cl&eacute; &agrave; cr&eacute;er sur <a href=\'@url@\'>le site de GoogleMaps</a>.',
+	'cfg_lbl_adresse' => 'Afficher les champs d\'adresse',
+	'cfg_lbl_api' => 'API de cartographie',
+	'cfg_lbl_api_cloudmade' => 'CloudMade',
+	'cfg_lbl_api_google' => 'Google Maps v2',
+	'cfg_lbl_api_googlev3' => 'Google Maps v3',
+	'cfg_lbl_api_key_cloudmade' => 'Cl&eacute; CloudMade',
+	'cfg_lbl_api_key_google' => 'Cl&eacute; GoogleMaps',
+	'cfg_lbl_api_mapquest' => 'MapQuest',
+	'cfg_lbl_api_microsoft' => 'Microsoft Bing',
+	'cfg_lbl_api_multimap' => 'MultiMap',
+	'cfg_lbl_api_openlayers' => 'OpenLayers',
+	'cfg_lbl_api_openspace' => 'OpenSpace',
+	'cfg_lbl_api_yahoo' => 'Yahoo',
+	'cfg_lbl_geocoder' => 'Geocoder',
+	'cfg_lbl_geolocaliser_user_html5' => 'Centrer la carte sur l\'emplacement de l\'utilisateur &agrave; la cr&eacute;ation',
+	'cfg_titre_gis' => 'GIS',
+
 	// E
-	"editer_gis_editer" => "Modifier ce point",
-	"editer_gis_explication" => "Cette page liste l'ensemble des points g&eacute;olocalis&eacute;s du site.",
-	"editer_gis_nouveau" => "Cr&eacute;er un nouveau point",
-	"editer_gis_titre" => "Les points g&eacute;olocalis&eacute;s",
-	"erreur_recherche_pas_resultats" => "Aucun point ne correspond &agrave; la recherche.",
+	'editer_gis_editer' => 'Modifier ce point',
+	'editer_gis_explication' => 'Cette page liste l\'ensemble des points g&eacute;olocalis&eacute;s du site.',
+	'editer_gis_nouveau' => 'Cr&eacute;er un nouveau point',
+	'editer_gis_titre' => 'Les points g&eacute;olocalis&eacute;s',
+	'erreur_recherche_pas_resultats' => 'Aucun point ne correspond &agrave; la recherche.',
 
 	// F
-	"formulaire_creer_gis" => "Cr&eacute;er un point g&eacute;olocalis&eacute; :",
-	"formulaire_modifier_gis" => "Modifier le point g&eacute;olocalis&eacute; :",
+	'formulaire_creer_gis' => 'Cr&eacute;er un point g&eacute;olocalis&eacute; :',
+	'formulaire_modifier_gis' => 'Modifier le point g&eacute;olocalis&eacute; :',
 
 	// I
-	"icone_gis_tous" => "Points g&eacute;olocalis&eacute;s",
-	"info_numero_gis" => "Point num&eacute;ro",
-	"info_id_objet" => "N&deg;",
-	"info_liste_gis_objet" => "Points li&eacute;s &agrave; cet objet",
-	"info_liste_objets_gis" => "Objets li&eacute;s &agrave; ce point",
-	"info_objet" => "Objet",
-	"info_supprimer_lien" => "D&eacute;tacher",
-	"info_voir_fiche_objet" => "Voir la fiche",
+	'icone_gis_tous' => 'Points g&eacute;olocalis&eacute;s',
+	'info_id_objet' => 'N&deg;',
+	'info_liste_gis_objet' => 'Points li&eacute;s &agrave; cet objet',
+	'info_liste_objets_gis' => 'Objets li&eacute;s &agrave; ce point',
+	'info_numero_gis' => 'Point num&eacute;ro',
+	'info_objet' => 'Objet',
+	'info_supprimer_lien' => 'D&eacute;tacher',
+	'info_voir_fiche_objet' => 'Voir la fiche',
 
 	// L
-	"label_adress" => "Adresse",
-	"label_code_postal" => "Code postal",
-	"label_pays" => "Pays",
-	"label_rechercher_address" => "Rechercher une adresse",
-	"label_rechercher_point" => "Rechercher un point",
-	"label_region" => "R&eacute;gion",
-	"label_ville" => "Ville",
-	"lat" => "Latitude",
-	"libelle_logo_gis" => "LOGO DU POINT",
-	"lon" => "Longitude",
-	
+	'label_adress' => 'Adresse',
+	'label_code_postal' => 'Code postal',
+	'label_pays' => 'Pays',
+	'label_rechercher_address' => 'Rechercher une adresse',
+	'label_rechercher_point' => 'Rechercher un point',
+	'label_region' => 'R&eacute;gion',
+	'label_ville' => 'Ville',
+	'lat' => 'Latitude',
+	'libelle_logo_gis' => 'LOGO DU POINT',
+	'lon' => 'Longitude',
+
 	// T
 	'titre_bloc_creer_point' => 'Lier un nouveau point',
 	'titre_bloc_points_lies' => 'Points liés',
 	'titre_bloc_rechercher_point' => 'Rechercher un point',
 	'titre_nombre_utilisation' => 'Une utilisation',
 	'titre_nombre_utilisations' => '@nb@ utilisations',
-	
+
 	// Z
-	"zoom" => "Zoom"
-		
+	'zoom' => 'Zoom'
 );
 
 ?>
