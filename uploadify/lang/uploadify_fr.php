8350780a054b4039aec5a0db1540fac47c70885a
yaquoi@yaquoi.com <yaquoi@yaquoi.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-07-16 00:18:00 +0000

    debugage d'une veille version... cela semble fonctionner... à voir
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@49668 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- uploadify/lang/uploadify_fr.php
+++ uploadify/lang/uploadify_fr.php
@@ -3,16 +3,25 @@
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-'titre_uploadify' => 'Uploadify - T&eacute;l&eacute;chargement de fichiers par lot',
-'texte_uploadify' => 'Vous pouvez t&eacute;l&eacute;charger un lot de fichiers en une seule fois dans les articles en tant que documents joints.',
-'texte_uploadify_article' => 'Vous pouvez t&eacute;l&eacute;charger un lot de fichiers dans votre article.<br />Actualisez la page pour visualiser les fichiers ajout&eacute;s.',
-'file_size_limit' => '<strong>Taille limite des fichiers</strong><br/>Ce param&egrave;tre doit &ecirc;tre exprim&eacute; en kb',
-'file_types' => '<strong>Types de fichiers t&eacute;l&eacute;chargeables</strong><br/>format : *.ext separ&eacute;s par un ";" , *.* pour tout type',
-'file_upload_limit' => '<strong>Nombre des fichiers t&eacute;l&eacute;chargeables en un lot</strong><br/>La valeur 0 indique "pas de limites".<br/>',
-'debug' => '<strong>Active la fonction debug</strong>',
-'texte_uploadqueue' => 'File d\'attente',
-'texte_filesupload' => 'fichier(s) t&eacute;l&eacute;charg&eacute;(s)',
-'texte_boutonupload' => 'T&eacute;l&eacute;charger',
-'texte_cancelupload' => 'Annuler le t&eacute;l&eacute;chargement',
+	//C
+	'cfg_explication_boite_info' => 'Ici vous pouvez indiquer diff&eacute;rentes valeurs pour configurer le plugin.',
+	'cfg_file_size_limit' => '<strong>Taille limite des fichiers</strong><br/>Ce param&egrave;tre doit &ecirc;tre exprim&eacute; en kb',
+	'cfg_file_types' => '<strong>Types de fichiers t&eacute;l&eacute;chargeables</strong><br/>format : *.ext separ&eacute;s par un ";" , *.* pour tout type',
+	'cfg_file_upload_limit' => '<strong>Nombre des fichiers t&eacute;l&eacute;chargeables en un lot</strong><br/>La valeur 0 indique "pas de limites".<br/>',
+	'cfg_titre_boite_info' => 'Configuration du plugin Uploadify',
+	'cfg_titre_uploadify' => 'Uploadify',
+
+	//D	
+	'debug' => '<strong>Active la fonction debug</strong>',
+	
+	//T	
+	'texte_boutonupload' => 'T&eacute;l&eacute;charger',
+	'texte_cancelupload' => 'Annuler le t&eacute;l&eacute;chargement',
+	'texte_filesupload' => 'fichier(s) t&eacute;l&eacute;charg&eacute;(s)',
+	'texte_parcourir' => 'Parcourir',
+	'texte_uploadify_article' => '<ol><li>Cliquez sur <strong>"Parcourir"</strong> pour s&eacute;lectionner un ou plusieurs fichiers &agrave; t&eacute;l&eacute;charger.</li><li>Cliquer sur <strong>"T&eacute;l&eacute;charger".</strong></li><li>&Agrave; la fin du t&eacute;l&eacute;chargement, actualisez la page.</li></ol>',
+	'texte_uploadify' => 'Vous pouvez t&eacute;l&eacute;charger un lot de fichiers en une seule fois dans les articles en tant que documents joints.',
+	'texte_uploadqueue' => 'File d\'attente',
+	'titre_uploadify' => 'Uploadify - T&eacute;l&eacute;chargement de fichiers par lot',
 );
 ?>
--- uploadify/uploadify_pipelines.php
+++ uploadify/uploadify_pipelines.php
@@ -1,31 +1,36 @@
 <?php
-/*
-function uploadify_ajouterBoutons($boutons_admin) {
-		// si on est admin ou admin restreint
-		if ($GLOBALS['connect_statut'] == "0minirezo" || $GLOBALS["connect_toutes_rubriques"]) {
-		//AND $GLOBALS["options"]=="avancees") {
-		  // on voit le bouton dans la barre "naviguer"
-			$boutons_admin['naviguer']->sousmenu['uploadify_admin']= new Bouton(
-			"../"._DIR_PLUGIN_UPLOADIFY."/uploadify-24.png",  // icone
-			_T('uploadify:uploadify')	// titre
-			);
-		}
-		return $boutons_admin;
-}
-*/
 
 function uploadify_header_prive($flux) {
-global $connect_statut,$connect_toutes_rubriques;
+	
+	global $connect_statut,$connect_toutes_rubriques;
 
-$dir_plugin = _DIR_PLUGIN_UPLOADIFY;
-$js = find_in_path('jquery.uploadify.v2.1.0.min.js');
-$css = $dir_plugin.'uploadify.css';
-$swfobject = $dir_plugin.'swfobject.js';
-$url_upload = '../?page=uploadify';
-//'scriptData':{'exec':'uploadify_upload','id_article':$id_article},
-$type = 'article';
-$id_article=intval(_request('id_article'));
-$exec = _request('exec');
+	$id_article=intval(_request('id_article'));
+
+	if (_request('exec') == 'articles' AND autoriser('modifier','article',$id_article)) {
+
+		$flux .= '<link rel="stylesheet" type="text/css" href="'.find_in_path('uploadify.css').'" media="all" />'."\n";
+		$flux .= "<script type='text/javascript' src='".find_in_path('jquery.uploadify.v2.1.4.min.js')."'></script>\n";
+		$flux .= "<script type='text/javascript' src='".find_in_path('swfobject.js')."'></script>\n";
+
+
+		$jsFile = generer_url_public('uploadify.js','id_article='.$id_article);
+
+		$flux .= "<script type='text/javascript' src='$jsFile'></script>\n";
+	}
+	return $flux;
+/*
+if ((_request('exec') == 'articles')  AND ($GLOBALS['connect_statut'] == "0minirezo" || $GLOBALS["connect_toutes_rubriques"])) {
+	$session = session_id();
+	$dir_plugin = _DIR_PLUGIN_UPLOADIFY;
+	$js = find_in_path('jquery.uploadify.v2.1.4.min.js');
+	$css = $dir_plugin.'uploadify.css';
+	$swfobject = $dir_plugin.'swfobject.js';
+	$url_upload = '../?page=uploadify';
+	//'scriptData':{'exec':'uploadify_upload','id_article':$id_article},
+	//$type = "article";
+	$id_article=intval(_request('id_article'));
+	$exec = _request('exec');
+
 
 // si cfg dispo, on charge les valeurs
 if (function_exists(lire_config))  {   
@@ -41,11 +46,11 @@ else $simUploadLimit_txt = "'simUploadLimit':'$simUploadLimit'," ;
 if (!$debug || $debug == '_' || $debug == '') $debug = "false" ;
 
 //,if ($GLOBALS['connect_statut'] == "0minirezo" || $GLOBALS["connect_toutes_rubriques"]) {
-if ($exec=='articles') {
-if  ($GLOBALS['meta']["documents_$type"]=='non'
-	OR !autoriser('joindredocument', $type, $id_article))
-		$flux = '';
-	else {
+//if ($exec=='articles') {
+//if  ($GLOBALS['meta']["documents_$type"]=='non') {
+//	OR !autoriser('joindredocument', $type, $id_article))
+//		$flux = '';}
+//	else {
 	$flux .= "<!-- uploadify -->
 	<link href='$css' rel='stylesheet' type='text/css' />
 	<script type='text/javascript' src='$js'></script>\n";
@@ -68,12 +73,16 @@ if  ($GLOBALS['meta']["documents_$type"]=='non'
 		'onQueueFull': function (evt, queueID, fileObj, response, data) {
          alert(response);
 	  		}
+
 	});
 	});
 	</script>";
 	}
-	}
+
+//	}
+
 	return $flux;
+*/
 }
 
 function uploadify_affiche_milieu($flux){
@@ -82,29 +91,34 @@ global $connect_statut,$connect_toutes_rubriques;
  $args = $flux['args'];
  $out = "";
  $id_article = $args['id_article'];
- $type = "article";
 
 //if ($GLOBALS['connect_statut'] == "0minirezo" || $GLOBALS["connect_toutes_rubriques"]) {
-if ($args['exec']=='articles') {
-if  ($GLOBALS['meta']["documents_$type"]=='non'
-	OR !autoriser('joindredocument', $type, $id_article))
-		$out = '';
+if (_request('exec') == 'articles' AND autoriser('modifier','article',$id_article)) {
+//if ($GLOBALS['connect_statut'] == "0minirezo" || $GLOBALS["connect_toutes_rubriques"]) {
+//if ($args['exec']=='articles') {
+	if  ($GLOBALS['meta']["documents_$type"]=='non'
+		OR !autoriser('joindredocument', $type, $id_article))
+			$out = '';
 	else {
 	$bouton = _T('uploadify:titre_uploadify');
-	$out .= debut_cadre_enfonce(_DIR_PLUGIN_UPLOADIFY.'uploadify-24.png', true, "", 
+	if(function_exists('bouton_block_depliable')) 
+		$out .= debut_cadre_enfonce(_DIR_PLUGIN_UPLOADIFY.'images/uploadify-24.png', true, "", 
 			bouton_block_depliable($bouton,$flag === 'ajax','uploadify'))
 			. debut_block_depliable($flag === 'ajax','uploadify');
+  	else 
+  	  	$out .= debut_cadre_enfonce(_DIR_PLUGIN_UPLOADIFY.'images/uploadify-24.png', true, "", "");
+	
 	$out .= '<div>'._T('uploadify:texte_uploadify_article').'</div>';
 	$out .= '<div>
-	<div style="float:left;width:100px;"><input type="file" id="fileInput" name="fileInput" /></div>
-  	<div style="float:right;padding:10px;"><a href="javascript:$(\'#fileInput\').uploadifyUpload();">'._T('uploadify:texte_boutonupload').'</a> | <a href="javascript:$(\'#fileInput\').uploadifyClearQueue();">'._T('uploadify:texte_cancelupload').'</a></div>
-</div>';
-/*$out .= '
-<input id="fileInput" name="fileInput" width="110" height="30" type="file"><a href="javascript:$(\'#fileInput\').uploadifyUpload();">Upload Files</a> | <a href="javascript:$(\'#fileInput\').uploadifyClearQueue();">Clear Queue</a></div>';
-*/
+	<div style="float:left;width:150px;"><input type="file" id="fileInput" name="fileInput" /></div>
+  	<div style="float:left;padding:5px 8px 0px 8px;height:25px;border:1px #CCCCCC solid;font-size:120%;"><a href="javascript:$(\'#fileInput\').uploadifyUpload();" class="icone36"><span>'._T('uploadify:texte_boutonupload').'</span></a></div>
+	<div style="float:left;padding:5px 8px 0px 8px;height:25px;border:1px #CCCCCC solid;font-size:120%;margin-left:5px"><a href="javascript:$(\'#fileInput\').uploadifyClearQueue();" class="icone36"><span>'._T('uploadify:texte_cancelupload').'</span></a></div>
+	<div style="clear:both;border:1px #CCCCCC solid;font-size:120%;padding:5px 8px 0px 8px;height:25px;width:120px"><a href="" class="icone36"><span>Actualiser la page</span></a></div>
+	</div>';
+
 	$out .= fin_block()
 			. fin_cadre_enfonce(true);
-	}
+		}	
 	$flux['data'].= $out;
 	}
 	return $flux;	
