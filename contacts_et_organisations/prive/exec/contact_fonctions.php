31fe30bf3e293ab6428155c75c4086babf83b5d2
cyril@cym.fr <cyril@cym.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-07-19 18:55:03 +0000

    on joute un fil d'ariane dans les pages de gestion;
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@49766 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- contacts_et_organisations/prive/exec/contact_fonctions.php
+++ contacts_et_organisations/prive/exec/contact_fonctions.php
@@ -1,3 +1,12 @@
 <?php
-include_spip('inc/presentation');
+$id_contact = _request('id_contact');
+$id_auteur = sql_getfetsel('id_objet', 'spip_contacts_liens', 'id_contact=' . intval($id_contact) . ' AND objet=\'auteur\'');
+
+if ($id_auteur) {
+	include_spip('inc/headers');
+	$redirect = generer_url_ecrire('auteur_infos','id_auteur='.$id_auteur, true);
+	redirige_par_entete($redirect);
+}
+else
+	include_spip('inc/presentation');
 ?>
--- contacts_et_organisations/prive/exec/organisation_fonctions.php
+++ contacts_et_organisations/prive/exec/organisation_fonctions.php
@@ -1,3 +1,12 @@
 <?php
-include_spip('inc/presentation');
+$id_organisation = _request('id_organisation');
+$id_auteur = sql_getfetsel('id_auteur', 'spip_organisations', 'id_organisation='. intval($id_organisation) );
+
+if ($id_auteur) {
+	include_spip('inc/headers');
+	$redirect = generer_url_ecrire('auteur_infos','id_auteur='.$id_auteur, true);
+	redirige_par_entete($redirect);
+}
+else
+	include_spip('inc/presentation');
 ?>
