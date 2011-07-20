3dd2a04062803ee34d69657611d7e2a067cc6687
toutati@free.fr <toutati@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-07-20 20:53:56 +0000

    grrr, reparer encore, avec une version plus avancee qui integre les dates des rubriques
    automatiquement l'abonnement accorde l'acces si les dates correspondent
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@49813 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ abonnement/trunk/zabonnement/trunk/action/supprimer_contactabonnement.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Plugin Coordonnées 
+ * Licence GPL (c) 2010 Matthieu Marcillaud 
+**/
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+// suppression d'un objet abonnement pour un contact
+/* [(#URL_ACTION_AUTEUR{supprimer_contactabonnement,
+ contactabonnement-#ENV{id_auteur}-#ID_OBJET-#OBJET,[(#ENV{retour,#SELF})]})],
+*/
+	
+function action_supprimer_contactabonnement_dist($arg=null) {
+	if (is_null($arg)){
+		$securiser_action = charger_fonction('securiser_action', 'inc');
+		$arg = $securiser_action();
+	}
+	$arg = explode('-', $arg);
+
+	// todo verifier suppression dans la commande !!
+	if ($arg[0] == 'contactabonnement' and intval($arg[1]) and intval($arg[2])) {
+		action_supprimer_contactabonnement_post($arg[1],$arg[2],$arg[3]);
+	}	
+
+}
+
+function fermer_zone($id_auteur,$ids_zone)
+{
+	$array_ids = explode(",", $ids_zone);
+	foreach($array_ids as $id_zone)
+	{
+	spip_log("fermer_zone $id_zone pour $id_auteur",'abonnement');
+		sql_delete("spip_zones_auteurs", array(
+			"id_zone"=>$id_zone,
+			"id_auteur"=>$id_auteur
+		));
+	}
+}
+
+function action_supprimer_contactabonnement_post($id_auteur,$id_abonnement,$objet) {
+
+	if ($id_auteur and $id_abonnement) {
+		//l' abonnement
+		$abonnement = sql_fetsel('*', 'spip_abonnements', 'id_abonnement = '. $id_abonnement);
+		$ids_zone=$abonnement['ids_zone'];
+			if($ids_zone!='')
+			fermer_zone($id_auteur,$ids_zone);
+		
+		// on supprime l'abonnement de l'auteur
+		sql_delete("spip_contacts_abonnements", array(
+			"id_auteur=" . sql_quote($id_auteur),
+			"id_objet=" . sql_quote($id_abonnement),
+			"objet='$objet'"
+		));
+
+	}
+	
+
+	include_spip('inc/invalideur');
+	//suivre_invalideur("id='id_contactabonnement/$id_contactabonnement'");
+}
+?>
