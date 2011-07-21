c5f8958aa65efb1859816ae13a29282e5eb4abfd
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-07-21 11:46:17 +0000

    un document publie une rubrique (marcimat et al) + coquille de nom de variable
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@49819 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- mediatheque/action/editer_document.php
+++ mediatheque/action/editer_document.php
@@ -161,7 +161,9 @@ function instituer_document($id_document,$champs=array()){
 			$table = table_objet_sql($row['objet']);
 			$desc = $trouver_table($table);
 			// si pas de champ statut, c'est un objet publie, donc c'est bon
-			if (!isset($desc['field']['statut'])){
+			// si c'est une rubrique, c'est bon aussi, car un document publie une rubrique
+			if (!isset($desc['field']['statut'])
+			  OR $row['objet']=='rubrique'){
 				$statut = 'publie';
 				$date_publication=0;
 				continue;
@@ -181,7 +183,7 @@ function instituer_document($id_document,$champs=array()){
 			}
 		}
 		$date_publication = date('Y-m-d H:i:s',$date_publication);
-		if ($statut=='publie' AND $statut_ancien=='publie' AND $date_publie==$date_publication_ancienne)
+		if ($statut=='publie' AND $statut_ancien=='publie' AND $date_publication==$date_publication_ancienne)
 			return false;
 		if ($statut!='publie' AND $statut_ancien!='publie' AND $statut_ancien!='0')
 			return false;
