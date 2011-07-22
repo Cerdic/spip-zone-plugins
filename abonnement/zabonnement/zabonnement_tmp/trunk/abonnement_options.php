7087fd6457fed1569b5ca4518a5fee552a6aa6d7
toutati@free.fr <toutati@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-07-22 12:30:48 +0000

    comic de repetition etc
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@49840 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- abonnement/zabonnement/zabonnement_tmp/trunk/abonnement_options.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-//AJout de more dans les blocs 
-$GLOBALS['z_blocs']=array('contenu','navigation','extra','head','more');
-// activer le chargement parallele sur les blocs contenu et more
-#define('_Z_AJAX_PARALLEL_LOAD','contenu,more');
-
-?>
