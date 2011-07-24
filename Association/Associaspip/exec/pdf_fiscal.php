e627f27f8b516be2149e74c9b1ada865d0ade66c
marcel@bolla.fr <marcel@bolla.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-07-24 04:33:32 +0000

    La variable $mail est pas initialisee
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@49876 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- Association/Associaspip/exec/pdf_fiscal.php
+++ Association/Associaspip/exec/pdf_fiscal.php
@@ -2,7 +2,7 @@
 /***************************************************************************\
  *  Associaspip, extension de SPIP pour gestion d'associations             *
  *                                                                         *
- *  Copyright (c) 2007 Bernard Blazin & François de Montlivault (V1)       *
+ *  Copyright (c) 2007 Bernard Blazin & Franï¿½ois de Montlivault (V1)       *
  *  Copyright (c) 2010-2011 Emmanuel Saint-James & Jeannot Lapin (V2)       *
  *                                                                         *
  *  Ce programme est un logiciel libre distribue sous licence GNU/GPL.     *
@@ -53,8 +53,10 @@ function exec_pdf_fiscal()
 				      "spip_asso_dons AS D LEFT JOIN spip_asso_comptes AS C ON C.id_journal=D.id_don",
 				      $critere . "C.vu AND date_format( D.date_don, '%Y' ) = $annee AND id_adherent=$id_auteur");
 
+           $mail = sql_getfetsel('email', 'spip_auteurs',"id_auteur=$id_auteur");
+
 		if (!$montants)
-		  {echo "Versement en $annee pour l'adherent de mail $mail: $montants";}
+		  {echo "Pas de versement(s) \"valid&eacute;(s)\" en $annee pour l'adh&eacute;rent de mail {".$mail."}.";}
 		else {
 		  $nom=$data['prenom'].' '.$data['nom_famille']; 
 		  $adresse=$data['adresse'];
