b3fc713b989aa96722048e9cda8e02c0bcbf8872
toutati@free.fr <toutati@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-07-24 11:56:01 +0000

    grrrr, c'est quand meme bien de retourner le flux, des fois que d'autres s'en servent... faire la meme chose sur docs_logos_evenements
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@49888 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- docs_logos_abonnement/imgabo_pipelines.php
+++ docs_logos_abonnement/imgabo_pipelines.php
@@ -1,7 +1,7 @@
 <?php
 $GLOBALS['gestdoc_exec_colonne_document'][] = 'abonnement_edit';
 $GLOBALS['gestdoc_liste_champs'][] = 'descriptif';
-function imgabo_post_edition($flux){}
+function imgabo_post_edition($flux){return $flux;}
 function imgabo_affiche_gauche($flux){
     if (($flux['args']['exec'] == 'abonnement_edit')
 		AND $table = preg_replace(",_edit$,","",$flux['args']['exec'])
@@ -10,7 +10,7 @@ function imgabo_affiche_gauche($flux){
 		AND $id_abonnement = intval($flux['args'][$id_table_objet])){
 		$GLOBALS['logo_libelles']['id_abonnement'] = _T('imgabo:logo_abonnement');
 		$iconifier = charger_fonction('iconifier', 'inc');
-		$flag_editable = autoriser('modifier', 'abonnement', $id_abonnement, null, array('id_article' => $id_article));
+		$flag_editable = autoriser('modifier', 'abonnement', $id_abonnement);
 		$out .= $iconifier('id_abonnement', $id_abonnement, 'abonnement_edit', $flag_editable);
 		$flux['data'] .= $out;
 	}
