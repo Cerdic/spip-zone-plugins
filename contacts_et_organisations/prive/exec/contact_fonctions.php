f967d00efb432ad1458cad6a7b473b026689d504
marcimat@rezo.net <marcimat@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-07-25 19:17:47 +0000

    Pas de redirection automatique sur des contacts ou organisation vers la page des auteurs s'ils sont auteurs. Mettre _CO_REDIRIGER_SUR_AUTEURS = true pour rétablir ce fonctionnement.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@49971 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- contacts_et_organisations/prive/exec/contact_fonctions.php
+++ contacts_et_organisations/prive/exec/contact_fonctions.php
@@ -1,12 +1,19 @@
 <?php
-$id_contact = _request('id_contact');
-$id_auteur = sql_getfetsel('id_objet', 'spip_contacts_liens', 'id_contact=' . intval($id_contact) . ' AND objet=\'auteur\'');
+// Rediriger automatiquement un contact défini comme auteur
+// sur la page des auteurs.
+if (defined('_CO_REDIRIGER_SUR_AUTEURS') and _CO_REDIRIGER_SUR_AUTEURS) {
 
-if ($id_auteur) {
-	include_spip('inc/headers');
-	$redirect = generer_url_ecrire('auteur_infos','id_auteur='.$id_auteur, true);
-	redirige_par_entete($redirect);
+	$id_contact = _request('id_contact');
+	$id_auteur = sql_getfetsel('id_objet', 'spip_contacts_liens', 'id_contact=' . intval($id_contact) . ' AND objet=\'auteur\'');
+
+	if ($id_auteur) {
+		include_spip('inc/headers');
+		$redirect = generer_url_ecrire('auteur_infos','id_auteur='.$id_auteur, true);
+		redirige_par_entete($redirect);
+	}
+
 }
-else
-	include_spip('inc/presentation');
+
+include_spip('inc/presentation');
+
 ?>
--- contacts_et_organisations/prive/exec/organisation_fonctions.php
+++ contacts_et_organisations/prive/exec/organisation_fonctions.php
@@ -1,12 +1,19 @@
 <?php
-$id_organisation = _request('id_organisation');
-$id_auteur = sql_getfetsel('id_auteur', 'spip_organisations', 'id_organisation='. intval($id_organisation) );
 
-if ($id_auteur) {
-	include_spip('inc/headers');
-	$redirect = generer_url_ecrire('auteur_infos','id_auteur='.$id_auteur, true);
-	redirige_par_entete($redirect);
+// Rediriger automatiquement une organisation définie comme auteur
+// sur la page des auteurs.
+if (defined('_CO_REDIRIGER_SUR_AUTEURS') and _CO_REDIRIGER_SUR_AUTEURS) {
+
+	$id_organisation = _request('id_organisation');
+	$id_auteur = sql_getfetsel('id_auteur', 'spip_organisations', 'id_organisation='. intval($id_organisation) );
+
+	if ($id_auteur) {
+		include_spip('inc/headers');
+		$redirect = generer_url_ecrire('auteur_infos','id_auteur='.$id_auteur, true);
+		redirige_par_entete($redirect);
+	}
 }
-else
-	include_spip('inc/presentation');
+
+include_spip('inc/presentation');
+
 ?>
