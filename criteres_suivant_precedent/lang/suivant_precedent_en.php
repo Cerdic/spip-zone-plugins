d9e810120bca07e0e4dbe14171a0a8f9a379dd4a
yffic@lefourneau.com <yffic@lefourneau.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-07-29 17:46:54 +0000

    Ajout d'une noisette de navigation générique "article précédent suivant" (Eric)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@50080 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- criteres_suivant_precedent/lang/suivant_precedent_en.php
+++ criteres_suivant_precedent/lang/suivant_precedent_en.php
@@ -1,4 +1,4 @@
-<?php
+﻿<?php
 
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
 
@@ -6,5 +6,10 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// A
 	'alt_lien_next' => 'Next',
 	'alt_lien_prev' => 'Previous',
+
+	// N
+	'noisette_description' => 'Displays the navigation links to the previous article and the next article',
+	'noisette_label_meme_rubrique' => 'View the links only to articles in the same section',
+	'noisette_nom' => 'Links Previous / Next',
 );
 ?>
--- criteres_suivant_precedent/lang/suivant_precedent_fr.php
+++ criteres_suivant_precedent/lang/suivant_precedent_fr.php
@@ -1,10 +1,15 @@
-<?php
+﻿<?php
 
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// A
 	'alt_lien_next' => 'Suivant',
-	'alt_lien_prev' => 'Pr&eacute;c&eacute;dent',
+	'alt_lien_prev' => 'Précédent',
+
+	// N
+	'noisette_description' => 'Affiche les liens de navigation vers l\'article précédent et l\'article suivant',
+	'noisette_label_meme_rubrique' => 'Afficher les liens uniquement vers les articles de la même rubrique',
+	'noisette_nom' => 'Liens Précédent / Suivant',
 );
 ?>
