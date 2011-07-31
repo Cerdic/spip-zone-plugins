8ebcdd410f6c2d06c5f8d6fa9935e38fb9717f73
yffic@lefourneau.com <yffic@lefourneau.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-07-31 21:38:48 +0000

    Utilisation de la saisie Secteur pour sélectionner le ou les secteurs
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@50137 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- forum_sectorise/base/forumsectorise_install.php
+++ forum_sectorise/base/forumsectorise_install.php
@@ -16,15 +16,25 @@ function forumsectorise_upgrade($nom_meta_base_version,$version_cible){
 	$current_version = 0.0;
 	if (   (!isset($GLOBALS['meta'][$nom_meta_base_version]) )
 			|| (($current_version = $GLOBALS['meta'][$nom_meta_base_version])!=$version_cible)){
-		$config = lire_config('forumsectorise');
-		if (!is_array($config)) {
-			$config = array();
+		if (version_compare($current_version,'0.0','=')){
+			$config = array(
+								'id_secteur' => '',
+								'type' => 'non',
+								'option' => 'tous'
+							);
+			ecrire_meta($nom_meta_base_version, $current_version="0.0", 'non');
 		}
-		$config = array_merge(array(
-				'id_secteur' => '',
-				'type' => 'non',
-				'option' => 'tous'
-		), $config);
+
+		if (version_compare($current_version,'0.2','<=')){	
+			if (!isset($config)) {
+				$config = lire_config('forumsectorise');
+			}
+			// On peut maintenant selectionner plusieurs secteurs avec la saisie secteur
+			$config['id_secteur'] = array( 0 => $config['id_secteur'] );
+			ecrire_meta($nom_meta_base_version, $current_version="0.2", 'non');
+			echo _T('forumsectorise:msg_maj_version', array('version'=>$current_version))."<br/>";
+		}
+
 		ecrire_meta('forumsectorise', serialize($config));
 		ecrire_meta($nom_meta_base_version,$current_version=$version_cible,'non');
 	}
--- forum_sectorise/lang/forumsectorise_fr.php
+++ forum_sectorise/lang/forumsectorise_fr.php
@@ -1,23 +1,26 @@
-<?php
+﻿<?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// C
-	'cfg_boite_forumsectorise' => 'Configuration du plugin Forum sectoris&eacute;',
-	'cfg_boite_forumsectorise' => 'Ce plugin permet d\'activer les forums publics uniquement sur un secteur du site. Tous les nouveaux articles cr&eacute;&eacute;s dans ce secteur se verront attribuer le type de forum d&eacute;fini dans la configuration du plugin. Ce r&eacute;glage surcharge le mode de fonctionnement des forums publics d&eacute;fini dans la <a href="?exec=config_contenu" title="page de configuration du site">page de configuration du site</a>',
-	'cfg_titre_forumsectorise' => 'Forum sectoris&eacute;',
+	'cfg_boite_forumsectorise' => 'Configuration du plugin Forum sectorisé',
+	'cfg_boite_forumsectorise' => 'Ce plugin permet d\'activer les forums publics sur un ou plusieurs secteurs du site. Tous les nouveaux articles créés dans ce secteur se verront attribuer le type de forum défini dans la configuration du plugin. Ce réglage surcharge le mode de fonctionnement des forums publics défini dans la <a href="?exec=config_contenu" title="page de configuration du site">page de configuration du site</a>',
+	'cfg_titre_forumsectorise' => 'Forum sectorisé',
 
 	// E
-	'explication_id_secteur' => 'Indiquer ici le num&eacute;ro de secteur &agrave; traiter (saisir 0 pour appliquer l\'action &agrave; tous les articles du site)',
-	'explication_type' => 'Choisir le type de forum &agrave; activer pour ce secteur',
+	'explication_id_secteur' => 'Indiquer ici le numéro de secteur à traiter (saisir 0 pour appliquer l\'action à tous les articles du site)',
+	'explication_type' => 'Choisir le type de forum à activer pour ce secteur',
 	'explication_option' => 'Choisir une des options',
 
 	// L
 	'label_id_secteur' => 'Secteur',
 	'label_type' => 'Type de forum',
 	'label_option' => 'Options',
+
+	// M
+	'msg_maj_version' => 'Mise à jour des tables du plugin Forum sectorisé (version bdd @version@)',
 
 );
 ?>
