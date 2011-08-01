94ae86e96a758fcedd96fb4a33f15a495f441215
joseph@larmarange.net <joseph@larmarange.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-08-01 17:37:09 +0000

    Détails de mise en forme.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@50161 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- anythingslider/trunk/lang/anythingslider_fr.php
+++ anythingslider/trunk/lang/anythingslider_fr.php
@@ -4,14 +4,11 @@
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-
-	
 	'anythingslider' => 'AnythingSlider',
-	'explication_configurer_anythingslider' => 'Choisissez les éléments à insérer dans l\'en-tête des pages publiques (voir la <a href="https://github.com/ProLoser/AnythingSlider/wiki">documentation du script</a>).',
 	'configurer_anythingslider' => 'Configurer AnythingSlider',
-	'label_themes' => 'Thèmes',
+	'explication_configurer_anythingslider' => 'Choisissez les éléments à insérer dans l\'en-tête des pages publiques (voir la <a href="https://github.com/ProLoser/AnythingSlider/wiki">documentation du script</a>).',
 	'label_scripts' => 'Scripts optionnels',
-	
+	'label_themes' => 'Thèmes',
 );
 
 ?>
