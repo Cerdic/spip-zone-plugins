87f14355cd916ccca4d03af6f0ab3cd3b39c2da2
xdjuj@apsulis.com <xdjuj@apsulis.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-08-02 15:31:13 +0000

    - Mise à jour vers la nouvelle version de la lib html5media - Nécessiter la lib génère une erreur à la demande de téléchargement dans l'actualisation des plugins, l'archive pèse 20Mo le problème provient sûrement de là (car elle contient des médias de test = lourds)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@50175 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- videos/pipelines/videos_jquery_plugins.php
+++ videos/pipelines/videos_jquery_plugins.php
@@ -4,6 +4,6 @@
 	TODO Tester si insertion est activée dans CONFIG, pas la peine d'insérer des scripts sur les pages qui contiennent pas de vidéos
 */
 function videos_jquery_plugins($scripts){
-	$scripts[] = "javascript/html5media/html5media.min.js";
+	$scripts[] = "lib/html5media-1.1.4/api/html5media.min.js";
     return $scripts;
 }
