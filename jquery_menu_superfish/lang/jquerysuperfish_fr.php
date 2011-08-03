df3f0cdf07715b4fb432133f3bfea212e9e99dd7
yffic@lefourneau.com <yffic@lefourneau.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-08-03 12:36:07 +0000

    Fichiers de langue en UTF-8 sans Bom
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@50202 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- jquery_menu_superfish/lang/jquerysuperfish_fr.php
+++ jquery_menu_superfish/lang/jquerysuperfish_fr.php
@@ -8,16 +8,16 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'cfg_titre_jquerysuperfish' => 'Menu jQuery Superfish',
 	
 	// E
-	'explication_animation'=>'Un objet equivalent au premier param&egrave;tre de la méthode jQuery .animate() permettant d\'animer les sous-menus. Par exemple <strong>opacity:\'show\',height:\'show\'</strong>, équivaut &agrave; une animation fade-in et slide-down',
-	'explication_classe'=>'Classe de l\'&eacute;l&eacute;ment ul sur lequel portera le menu',
-	'explication_delai'=>'Le d&eacute;lai en millisecondes entre la sortie du pointeur du sous-menu et la fermeture de celui-ci',
-	'explication_menu'=>'Cliquez sur oui pour activer et param&eacute;trer un menu @type@ sur une arborescence ul/li',
+	'explication_animation'=>'Un objet equivalent au premier paramètre de la méthode jQuery .animate() permettant d\'animer les sous-menus. Par exemple <strong>opacity:\'show\',height:\'show\'</strong>, équivaut à une animation fade-in et slide-down',
+	'explication_classe'=>'Classe de l\'élément ul sur lequel portera le menu',
+	'explication_delai'=>'Le délai en millisecondes entre la sortie du pointeur du sous-menu et la fermeture de celui-ci',
+	'explication_menu'=>'Cliquez sur oui pour activer et paramétrer un menu @type@ sur une arborescence ul/li',
 
 	// L
 	'label_animation' => 'Animation',
 	'label_classe' => 'Classe',
-	'label_delai' => 'D&eacute;lai',
-	'label_menu' => 'G&eacute;rer un menu @type@',
+	'label_delai' => 'Délai',
+	'label_menu' => 'Gérer un menu @type@',
 	'legend_menu' => 'Menu @type@' ,
 
 );
