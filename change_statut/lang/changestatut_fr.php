fef6ff948281ff5564ed55501eddbfd7f6a5579d
yffic@lefourneau.com <yffic@lefourneau.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-08-03 12:34:20 +0000

    Fichiers de langue en UTF-8 sans Bom
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@50198 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- change_statut/lang/changestatut_fr.php
+++ change_statut/lang/changestatut_fr.php
@@ -5,8 +5,8 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// V
-	'versadmin' => 'Changer le statut &agrave; administrateur',
-	'versredacteur' => 'Changer le statut &agrave; r&eacute;dacteur',
-	'verswebmestre' => 'Changer le statut &agrave; webmestre'
+	'versadmin' => 'Changer le statut à administrateur',
+	'versredacteur' => 'Changer le statut à rédacteur',
+	'verswebmestre' => 'Changer le statut à webmestre'
 );
 ?>
