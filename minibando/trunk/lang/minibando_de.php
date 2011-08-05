7ba64e366358ecf01f6f000dee8eeadf9d6441e3
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-08-05 14:18:58 +0000

    langues (minibando)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@50291 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ minibando/trunk/lang/minibando_de.php
@@ -0,0 +1,20 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// M
+	'mode_debug' => 'Režim ladenia',
+	'mode_inclure' => 'Mode inclure', # NEW
+	'mode_profile' => 'Mode profile', # NEW
+
+	// T
+	'titre_debug' => 'Debug', # NEW
+	'titre_outils_rapides' => 'Cr&eacute;ation' # NEW
+);
+
+?>
---
+++ minibando/trunk/lang/minibando_sk.php
@@ -0,0 +1,20 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// M
+	'mode_debug' => 'Režim ladenia',
+	'mode_inclure' => 'Režim vkladania',
+	'mode_profile' => 'Režim vizitky',
+
+	// T
+	'titre_debug' => 'Ladiť',
+	'titre_outils_rapides' => 'Vytváranie'
+);
+
+?>
