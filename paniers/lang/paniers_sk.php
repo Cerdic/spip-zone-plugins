20cb68d61b178100bf56767fe0c8edb79996d16e
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-08-05 14:19:19 +0000

    langues (paniers)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@50296 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ paniers/lang/paniers_sk.php
@@ -0,0 +1,34 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'action_ajouter' => 'Pridať do vozíka',
+
+	// M
+	'mon_panier' => 'Mon panier', # NEW
+	'mon_panier_modifier' => 'Modifier mon panier', # NEW
+	'mon_panier_voir' => 'Voir mon panier', # NEW
+
+	// P
+	'panier_description' => 'Description', # NEW
+	'panier_erreur_quantites' => 'Les quantités doivent être des nombres entiers.', # NEW
+	'panier_montant' => 'Montant', # NEW
+	'panier_prix_unitaire' => 'Prix unitaire', # NEW
+	'panier_quantite' => 'Quantité', # NEW
+	'panier_quantite_ok' => 'Les quantités ont bien été modifiées.', # NEW
+	'panier_recalculer' => 'Recalculer', # NEW
+	'panier_taxes' => 'Dont taxes', # NEW
+	'panier_total_ht' => 'Total hors taxe', # NEW
+	'panier_total_ttc' => 'Total', # NEW
+	'panier_valider' => 'Valider mon panier', # NEW
+	'panier_vide' => 'Le panier est vide.', # NEW
+	'paniers' => 'Paniers' # NEW
+);
+
+?>
