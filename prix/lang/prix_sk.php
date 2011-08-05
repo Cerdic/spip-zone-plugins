b1534c2156759801b8a100ab5b914bfd98cfcfe4
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-08-05 14:19:20 +0000

    langues (prix)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@50297 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ prix/lang/prix_sk.php
@@ -0,0 +1,22 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// L
+	'label_prix_ht' => 'Cena bez DPH',
+	'label_prix_ttc' => 'Cena s DPH',
+	'label_taxes' => 'DPH',
+	'label_total_ht' => 'Celkom bez DPH',
+	'label_total_ttc' => 'Celkom s DPH',
+
+	// P
+	'prix_ht' => '@prix@ bez DPH',
+	'prix_ttc' => '@prix@ s DPH'
+);
+
+?>
