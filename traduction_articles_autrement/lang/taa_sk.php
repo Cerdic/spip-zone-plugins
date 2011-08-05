30d2da4c3c2faffdf474d3a9253f53739c284508
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-08-05 14:19:25 +0000

    langues (taa)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@50300 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ traduction_articles_autrement/lang/taa_sk.php
@@ -0,0 +1,43 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'affichage_edition_seule' => 'Zobrazovať články len v režime úprav',
+	'affichage_liste_compacte' => 'Kompaktné zobrazenie zoznamu článkov',
+
+	// C
+	'changer_langue' => 'Zmeniť jazyk článku',
+
+	// F
+	'fermer' => 'Zatvoriť',
+
+	// L
+	'lier_traduction' => 'Pripojiť preklad',
+
+	// M
+	'modifier_options_langue' => 'možnosti',
+
+	// N
+	'necesite_afficher_objet' => 'Na využívanie tejto funkcie si musíte nainštalovať zásuvný modul',
+
+	// O
+	'options' => 'Upraviť možnosti jazykov',
+
+	// R
+	'rang' => 'Hodnotenie',
+
+	// T
+	'titre_plugin' => 'Iný preklad článku',
+	'traductions' => 'Preklady',
+
+	// V
+	'voir_traductions' => 'Zobraziť podrobnosti o prekladoch'
+);
+
+?>
