b470a0e3fb214224bfdc7ae337f2ca1ef657a987
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-08-08 12:31:32 +0000

    Il manquait clairement un test là...
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@50373 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- commandes_paypal/commandes_paypal_pipelines.php
+++ commandes_paypal/commandes_paypal_pipelines.php
@@ -44,7 +44,9 @@ function commandes_paypal_traitement_paypal($flux){
 		);
 		
 		// Si le statut est "paye" alors on ajoute la date de paiement
-		$changements['date_paiement'] = date('Y-m-d H:i:s');
+		if ($statut_nouveau == 'paye'){
+			$changements['date_paiement'] = date('Y-m-d H:i:s');
+		}
 		
 		// On fait la mise à jour
 		$ok = sql_updateq(
