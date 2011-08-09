e31b3d3490cfff56d03a953b21dae549b309a4ab
yffic@lefourneau.com <yffic@lefourneau.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-08-09 20:49:12 +0000

    Style du champ extra dans l'espace privé pour le faire ressortir un peu
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@50411 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- champs_extras/extensions/type_articles/lang/type_articles_fr.php
+++ champs_extras/extensions/type_articles/lang/type_articles_fr.php
@@ -2,7 +2,7 @@
 // Fichier ayant vocation à être surchargé depuis un autre plugin
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 	//T
-	'titre' => "Type de l'article",
+	'titre' => "Type de l'article : ",
 	'type_defaut'=>"Article",
 	'type_1'=>"Nom du type 1",
 	'type_2'=>"Nom du type 2"
