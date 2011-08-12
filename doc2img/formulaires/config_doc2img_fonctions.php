63ed9649784bc7088ab05043db6a552a0022c4c1
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-08-12 14:45:45 +0000

    Amélioration de la gestion des erreurs
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@50435 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- doc2img/formulaires/config_doc2img_fonctions.php
+++ doc2img/formulaires/config_doc2img_fonctions.php
@@ -7,7 +7,6 @@
  * Fonction de vérification du formulaire de configuration CFG
  */
 function cfg_config_doc2img_verifier(&$cfg){
-	$modifs = $cfg->log_modif;
 	if (class_exists('Imagick')) {
 		if(!is_array($formats = lire_config('doc2img_imagick_extensions'))){
 			include_spip('inc/metas');
@@ -17,14 +16,23 @@ function cfg_config_doc2img_verifier(&$cfg){
 		}
 		$valeurs = $cfg->val;
 		if($valeurs['format_document']){
-			$formats_choisis = explode(',',$valeurs['format_document']);
-			$diff = array_diff(array_map('strtolower',$formats_choisis),array_map('strtolower',$formats));
+			$formats_choisis = explode(',',trim($valeurs['format_document']));
+			$diff = array_diff(array_map('trim',array_map('strtolower',$formats_choisis)),array_map('trim',array_map('strtolower',$formats)));
 		}
 		if(count($diff) > 1){
 			$cfg->messages['erreurs']['format_document'] = _T('doc2img:erreur_formats_documents',array('types'=>implode(',',$diff)));
 		}else if(count($diff) == 1){
 			$cfg->messages['erreurs']['format_document'] = _T('doc2img:erreur_format_document',array('type'=>implode(',',$diff)));
 		}
+	}
+}
+
+function cfg_config_doc2img_pre_traiter(&$cfg){
+	$valeurs = $cfg->val;
+	if($valeurs['format_document']){
+		$formats_choisis = explode(',',trim($valeurs['format_document']));
+		$formats = array_map('trim',array_map('strtolower',$formats_choisis));
+		$valeurs['format_document'] = implode(',',$valeurs['format_document']);
 	}
 }
 ?>
