85e32f6ee2741936f4cf5f6f01be284daa23a93c
yffic@lefourneau.com <yffic@lefourneau.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-08-13 11:53:37 +0000

    - Ménage chaînes de langue - exec en squelette
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@50451 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ gestion_ml/gestionml_options.php
@@ -0,0 +1,5 @@
+<?php
+if ($_SERVER["HTTP_HOST"] == "127.0.0.1") {
+	define('_HTTP_HOST_LOCAL','');
+}
+?>
--- gestion_ml/lang/gestionml_fr.php
+++ gestion_ml/lang/gestionml_fr.php
@@ -1,43 +1,55 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-	if (!defined("_ECRIRE_INC_VERSION")) return;
+if (!defined("_ECRIRE_INC_VERSION")) return;
 
-	$GLOBALS[$GLOBALS['idx_lang']] = array(
-		'titre' => 'Gestion ML',
-		'description' => 'Gestion de Mailing Lists',
-		'titre_cadre' => 'Gestion de Mailing Lists',
-		'titre_boite_cfg' => 'Configuration du plugin Gestion ML',
-		'boite_info' => 'Cette page vous permet d\'accéder à la gestion des utilisateurs des mailing-lists pour lesquelles le webmestre de ce site vous en a donné l\'autorisation.',
-		
-		// Exec
-		'infos' => 'Information sur la liste',
-		'abonnes' => 'Gestion des abonnés',
-		'envoyer_mail' => 'Recevoir la liste des abonnés par mail',
-		'supprimer_abonne' => 'Supprimer l\'abonné',
-		'ajouter_mail' => 'Ajouter un email',
-		'btn_ajouter' => 'Ajouter',
-		'titre_abonnes' => 'Abonnés de la liste @liste@',
-		'aucune_listes_dispo' => 'Aucune liste disponible',
-		'titre_listes_dispo' => 'Listes disponibles',
-		'titre_parametres' => 'Paramètres de la liste @liste@',
-		'confirmer_suppression' => 'Etes-vous sûr de vouloir supprimer cet email ?',
-		
-		// Config
-		'afficher_hebergeurs' => 'Choisir votre hébergeur de listes' ,
-		'config_local_legend' => 'Paramétrage du serveur local' ,
-		'serveur_local' => 'Serveur local' ,
-		'explication_serveur_local' => 'Le nom de votre serveur local si vous avez besoin de tester en local (souvent localhost)' ,
-		'config_ovh_legend' => 'Paramétrage OVH' ,
-		'serveur_distant' => 'Serveur Soap' ,
-		'explication_serveur_distant' => 'Url du fichier wsdl fournie par OVH' ,
-		'domaine' => 'Votre domaine' ,
-		'explication_domaine' => 'Saisissez votre domaine sans les www' ,
-		'identifiant' => 'Identifiant' ,
-		'explication_identifiant' => 'Votre identifiant de connexion au serveur SOAP (nic-handle)' ,
-		'mot_de_passe' => 'Mot de passe' ,
-		'explication_mot_de_passe' => 'Votre mot de passe de connexion au serveur SOAP (mot de passe d\'accès au manager OVH)' ,
-		'auteurs_listes_legend' => 'Configuration des listes gérables par auteur' ,
-		'liste_de' => '@nom@' ,
-		'explication_liste_de' => 'Choisissez la ou les listes que pourra gérer l\'auteur @nom@' ,
-	);
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	// A
+	'aucune_liste_dispo' => 'Aucune liste disponible',
+
+	// B
+	'boite_info' => 'Cette page vous permet d\'accéder à la gestion des utilisateurs des mailing-lists pour lesquelles le webmestre de ce site vous en a donné l\'autorisation.',
+	'btn_ajouter' => 'Ajouter',
+
+	// C
+	'config_local_legend' => 'Paramétrage du serveur local' ,
+	'config_ovh_legend' => 'Paramétrage OVH' ,
+	'confirmer_suppression' => 'Etes-vous sûr de vouloir supprimer cet email ?',
+
+	// D
+	'description' => 'Gestion de Mailing Lists',
+
+	// E
+	'explication_domaine' => 'Saisissez votre domaine sans les www' ,
+	'explication_identifiant' => 'Votre identifiant de connexion au serveur SOAP (nic-handle)' ,
+	'explication_liste_de' => 'Choisissez la ou les listes que pourra gérer l\'auteur @nom@' ,
+	'explication_mot_de_passe' => 'Votre mot de passe de connexion au serveur SOAP (mot de passe d\'accès au manager OVH)' ,
+	'explication_serveur_distant' => 'Url du fichier wsdl fournie par OVH' ,
+	'explication_serveur_local' => 'Le nom de votre serveur local si vous avez besoin de tester en local (souvent localhost)' ,
+	
+	// I
+	'icone_abonnes' => 'Gestion des abonnés',
+	'icone_envoyer_mail' => 'Recevoir la liste des abonnés par mail',
+	'icone_infos' => 'Information sur la liste',
+
+	// L
+	'label_afficher_hebergeurs' => 'Choisir votre hébergeur de listes' ,
+	'label_ajouter_mail' => 'Ajouter un email',
+	'label_auteurs_listes' => 'Configuration des listes gérables par auteur' ,
+	'label_domaine' => 'Votre domaine' ,
+	'label_identifiant' => 'Identifiant' ,
+	'label_liste_de' => '@nom@' ,
+	'label_mot_de_passe' => 'Mot de passe' ,
+	'label_serveur_distant' => 'Serveur Soap' ,
+	'label_serveur_local' => 'Serveur local' ,
+	
+	// T
+	'titre' => 'Gestion ML',
+	'titre_abonnes' => 'Abonnés de la liste @liste@',
+	'titre_boite_cfg' => 'Configuration du plugin Gestion ML',
+	'titre_listes_dispo' => 'Listes disponibles',
+	'titre_parametres' => 'Paramètres de la liste @liste@',
+
+	// S
+	'supprimer_abonne' => 'Supprimer l\'abonné',
+);
 ?>
