0de6e85f40c26ceda6180896598f02507358df42
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-08-15 14:50:51 +0000

    Le groupe de mots - Tags - peut être également utilisé dans l'admin du site
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@50479 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spipicious_jquery/base/spipicious_upgrade.php
+++ spipicious_jquery/base/spipicious_upgrade.php
@@ -12,19 +12,17 @@
  *
  */
 
-$GLOBALS['spipicious_base_version'] = 0.6;
 
-function spipicious_upgrade(){
-	$version_base = $GLOBALS['spipicious_base_version'];
+function spipicious_upgrade($nom_meta_base_version,$version_cible){
 	$current_version = 0.0;
-	if ((!isset($GLOBALS['meta']['spipicious_base_version']) )
-			|| (($current_version = $GLOBALS['meta']['spipicious_base_version'])!=$version_base)){
+	if ((!isset($GLOBALS['meta'][$nom_meta_base_version]) )
+			|| (($current_version = $GLOBALS['meta'][$nom_meta_base_version])!=$version_cible)){
 		include_spip('base/spipicious');
-		if ($current_version==0.0){
+		if (version_compare($current_version,'0.0','<=')){
 			include_spip('base/create');
 			include_spip('base/abstract_sql');
 			creer_base();
-			ecrire_meta('spipicious_base_version',$current_version=$version_base,'non');
+			ecrire_meta($nom_meta_base_version,$current_version=$version_base,'non');
 
 			/**
 			 * On crée un groupe de mots dédié qui servira à la configuration
@@ -34,7 +32,7 @@ function spipicious_upgrade(){
 			$titre_groupe = '- Tags -';
 			$id_groupe = sql_getfetsel('id_groupe','spip_groupes_mots','titre='.sql_quote($titre_groupe));
 			if(!$id_groupe){
-				$id_groupe = sql_insertq('spip_groupes_mots',array('titre' => $titre_groupe));
+				$id_groupe = sql_insertq('spip_groupes_mots',array('titre' => $titre_groupe,'tables_liees' => 'articles','minirezo'=>'oui','comite'=>'oui','forum'=>'oui'));
 			}
 			if($GLOBALS['meta']['config_precise_groupes'] == 'non'){
 				ecrire_meta('config_precise_groupes','oui','oui');
@@ -46,20 +44,20 @@ function spipicious_upgrade(){
 			ecrire_meta('spipicious',serialize($config_spipicious),'oui');
 			echo _T('spipicious:message_installation_activation');
 		}
-		if($current_version<0.2){
+		if(version_compare($current_version,'0.2','<')){
 			sql_alter("TABLE `spip_spipicious` ADD PRIMARY KEY (`id_mot`) ");
 			sql_alter("TABLE `spip_spipicious` ADD KEY (`id_auteur`) ");
 			sql_alter("TABLE `spip_spipicious` ADD maj timestamp AFTER position ");
 			echo _T('spipicious:message_upgrade_database',array('version'=>0.2));
-			ecrire_meta('spipicious_base_version',$current_version=0.2,'non');
+			ecrire_meta($nom_meta_base_version,$current_version=0.2,'non');
 		}
-		if($current_version<0.3){
+		if(version_compare($current_version,'0.3','<')){
 			sql_alter("TABLE `spip_spipicious` ADD id_rubrique bigint(21) NOT NULL AFTER`id_article` ");
 			sql_alter("TABLE `spip_spipicious` ADD id_document bigint(21) NOT NULL AFTER`id_rubrique` ");
 			echo _T('spipicious:message_upgrade_database',array('version'=>0.3));
-			ecrire_meta('spipicious_base_version',$current_version=0.3,'non');
+			ecrire_meta($nom_meta_base_version,$current_version=0.3,'non');
 		}
-		if($current_version<0.4){
+		if(version_compare($current_version,'0.4','<')){
 			$desc = sql_showtable("spip_spipicious", true);
 			if (isset($desc['PRIMARY KEY']['id_mot'])){
 				sql_alter("TABLE `spip_spipicious` DROP PRIMARY KEY (`id_mot`) ");
@@ -73,15 +71,15 @@ function spipicious_upgrade(){
 				echo "Creation de la table spip_mots_documents<br/>";
 			}
 			echo _T('spipicious:message_upgrade_database',array('version'=>0.4));
-			ecrire_meta('spipicious_base_version',$current_version=0.4,'non');
+			ecrire_meta($nom_meta_base_version,$current_version=0.4,'non');
 		}
-		if($current_version<0.5){
+		if(version_compare($current_version,'0.5','<')){
 			sql_alter("TABLE `spip_spipicious` ADD id_syndic bigint(21) NOT NULL AFTER`id_document` ");
 			sql_alter("TABLE `spip_spipicious` ADD id_evenement bigint(21) NOT NULL AFTER`id_syndic` ");
 			echo _T('spipicious:message_upgrade_database',array('version'=>0.5));
-			ecrire_meta('spipicious_base_version',$current_version=0.5,'non');
+			ecrire_meta($nom_meta_base_version,$current_version=0.5,'non');
 		}
-		if($current_version<0.6){
+		if(version_compare($current_version,'0.6','<')){
 			sql_alter("TABLE `spip_spipicious` ADD id_objet bigint(21) NOT NULL AFTER `id_auteur` ");
 			sql_alter("TABLE `spip_spipicious` ADD objet VARCHAR (25) DEFAULT '' NOT NULL AFTER `id_objet` ");
 			spipicious_id_objet_objet_upgrade();
@@ -93,34 +91,26 @@ function spipicious_upgrade(){
 			sql_alter("TABLE `spip_spipicious` DROP COLUMN `id_syndic`");
 			sql_alter("TABLE `spip_spipicious` DROP COLUMN `id_evenement`");
 			echo _T('spipicious:message_upgrade_database',array('version'=>0.6));
-			ecrire_meta('spipicious_base_version',$current_version=0.6,'non');
+			ecrire_meta($nom_meta_base_version,$current_version=0.6,'non');
 		}
-
+		if(version_compare($current_version,'0.6.1','<')){
+			$id_groupe = lire_config('spipicious/groupe_mot');
+			if($id_groupe){
+				sql_updateq('spip_groupes_mots',array('tables_liees' => 'articles','minirezo'=>'oui','comite'=>'oui','forum'=>'oui'),'id_groupe='.$id_groupe);
+			}
+			ecrire_meta($nom_meta_base_version,$current_version='0.6.1','non');
+		}
 		ecrire_metas();
 	}
 }
 
-function spipicious_vider_tables() {
+function spipicious_vider_tables($nom_meta_version_base) {
 	sql_drop_table("spip_spipicious");
-	effacer_meta('spipicious_base_version');
+	effacer_meta($nom_meta_version_base);
 	effacer_meta('spipicious');
 	ecrire_metas();
 }
 
-function spipicious_install($action){
-	$version_base = $GLOBALS['spipicious_base_version'];
-	switch ($action){
-		case 'test':
-			return (isset($GLOBALS['meta']['spipicious_base_version']) AND ($GLOBALS['meta']['spipicious_base_version']>=$version_base));
-			break;
-		case 'install':
-			spipicious_upgrade();
-			break;
-		case 'uninstall':
-			spipicious_vider_tables();
-			break;
-	}
-}
 
 /**
  * Reunir en un seul champs id_objet/objet
