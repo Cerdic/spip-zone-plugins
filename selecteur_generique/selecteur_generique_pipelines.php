dd022d4d26a29fcfa418c3f77553c5ed6a2b3001
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-08-15 11:57:43 +0000

    Le sélecteur générique utilise désormais l'autocompleteur de jquery ui à partir de SPIP 2.1.10
    On garde le même format de réponse qu'auparavant ...
    
    Il va falloir modifier les fonctions des plugins en conséquence (cf les prochains commits sur spip.icio.us)
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@50471 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ selecteur_generique/selecteur_generique_pipelines.php
@@ -0,0 +1,257 @@
+<?php
+
+function SelecteurGenerique_jqueryui_forcer($plugins){
+	if (defined('DESACTIVER_SELECTEUR_GENERIQUE')
+	AND DESACTIVER_SELECTEUR_GENERIQUE)
+		return $plugins;
+	
+	$plugins[] = 'jquery.ui.autocomplete';
+	return $plugins;
+}
+function SelecteurGenerique_inserer_auteur() {
+
+	$ac = generer_url_ecrire('selecteur_generique');
+	$id_article = _request('id_article');
+	$statut = _request('statut');
+	$input = 'input[name=cherche_auteur][autocomplete!=off]';
+
+	return <<<EOS
+
+(function($) {
+	var appliquer_selecteur_cherche_auteur = function() {
+		// chercher l'input de saisie
+		var me = jQuery('$input');
+		me.autocomplete(
+			{
+				source: function( request, response ) {
+					$.ajax({
+						url: "$ac",
+						data:{quoi:'auteur',id_article:'$id_article',statut:'$statut',q:request.term},
+						success: function(data) {
+							datas = selecteur_format(data);
+							response( $.map( datas, function( item ) {
+								return item;
+							}));
+						}
+					});
+				},
+				delay: 300,
+				select: function( event, ui ) {
+					if (ui.item.result > 0) {
+						me.attr('name', 'old_value')
+							.parents('form')
+							.append(
+								jQuery("<input type='hidden' name='nouv_auteur' value='"+ui.item.result+"' />" // signature SPIP 1.9
+								+"<input type='hidden' name='auteur_article_${id_article}_new' value='"+ui.item.result+"' />"  // signature SPIP 2.0
+								)
+							)
+							.find("input[type=submit]")
+								.click()
+							.end();
+					}
+					return false;
+				}
+			}
+		);
+	};
+	$(function(){
+		appliquer_selecteur_cherche_auteur();
+		onAjaxLoad(appliquer_selecteur_cherche_auteur);
+	});
+})(jQuery);
+EOS;
+
+}
+
+function SelecteurGenerique_inserer_mot() {
+	if ($id = _request('id_article')) {
+		$type = 'id_article';
+	} elseif ($id = _request('id_rubrique')) {
+		$type = 'id_rubrique';
+	} elseif ($id = _request('id_syndic')) {
+		$type = 'id_syndic';
+	} else {
+		$id = _request('id_breve');
+		$type = 'id_breve';
+	}
+
+	$ac = generer_url_ecrire('selecteur_generique');
+	return <<<EOS
+
+(function($) {
+	var appliquer_selecteur_cherche_mot = function() {
+		// chercher l'input de saisie
+		var groupes = new Array;
+		var groupes_titre = new Array;
+		var hide=false;
+		$('input[name=cherche_mot][autocomplete!=off]')
+		.each(function(){
+			var id_groupe = $(this).parents('form').find('input[name=select_groupe]').val();
+			groupes.push(id_groupe);
+			groupes_titre.push($(this).val());
+			if (hide)
+				$(this).parent().hide();
+			hide=true;
+		});
+		hide=false;
+		$('input[name=cherche_mot][autocomplete!=off]:first')
+		.each(function(){
+			var inp = this;
+			$(this)
+			.val(groupes_titre.join(', '))
+			.attr('title',groupes_titre.join(', '));
+			$(this)
+			.parents('form')
+			.bind('submit', function(){
+				$('.ac_results').remove();
+			})
+			.find('input[name=select_groupe]')
+			.val(groupes.join(','));
+			$(inp).autocomplete(
+				{
+					source: function( request, response ) {
+						$.ajax({
+							url: "$ac",
+							data:{quoi:'mot',$type:'$id',groupes: groupes.join(','),q:request.term},
+							success: function(data) {
+								datas = selecteur_format(data);
+								response( $.map( datas, function( item ) {
+									return item;
+								}));
+							}
+						});
+					},
+					select:function( event, ui ) {
+						if (ui.item.result > 0) {
+							$(inp)
+								.attr('name', 'old_value')
+								.parents('form')
+								.append(
+									$("<input type='hidden' name='nouv_mot' value='"+ui.item.result[2]+"' />")
+								).find('input[type=submit]')
+								.click()
+								.end();
+						}else{
+							return ui.item.entry;
+						}
+						return false;
+					}
+				}
+			);
+		});
+	};
+	$(function(){
+		appliquer_selecteur_cherche_mot();
+		onAjaxLoad(appliquer_selecteur_cherche_mot);
+	});
+})(jQuery);
+EOS;
+
+}
+
+function SelecteurGenerique_inserer_rubrique() {
+
+	$ac = parametre_url(generer_url_ecrire('selecteur_generique',
+		'quoi=rubrique'),
+		'id_article', _request('id_article'), '\\x26');
+	$ac = generer_url_ecrire('selecteur_generique');
+
+	return <<<EOS
+(function($) {
+	var appliquer_selecteur_cherche_rubrique = function() {
+
+		// chercher l'input de saisie
+		var me = jQuery('input[id=titreparent]');
+		me.each(function(){
+			var inp = this;
+			var id_groupe = jQuery(this).parents('form').find('input[name=select_groupe]').val();
+
+			jQuery(inp).focus(function(){
+				$(this).val('');
+			});
+			jQuery(inp).attr('disabled','').autocomplete(
+				{
+					source: function( request, response ) {
+						$.ajax({
+							url: "$ac",
+							data:{quoi:'rubrique',q:request.term},
+							success: function(data) {
+								datas = selecteur_format(data);
+								response(
+									datas
+								);
+							}
+						});
+					},
+					delay: 300,
+					select:function( event, ui ) {
+						if (ui.item.result > 0) {
+							jQuery(inp)
+								.val(ui.item.entry)
+								.parents('form').find('input[id=id_parent]')
+								.val(ui.item.result)
+								.end();
+						}else{
+							return ui.item.entry;
+						}
+						return false;
+					}
+				}
+			);
+		});
+	};
+	$(function(){
+		appliquer_selecteur_cherche_rubrique();
+		onAjaxLoad(appliquer_selecteur_cherche_rubrique);
+	});
+})(jQuery);
+EOS;
+}
+
+// Calcule et insere le javascript necessaire pour la page
+function SelecteurGenerique_inserer_javascript($flux) {
+
+	if (defined('DESACTIVER_SELECTEUR_GENERIQUE')
+	AND DESACTIVER_SELECTEUR_GENERIQUE)
+		return $flux;
+
+	$js = '';
+	$js_final = '';
+
+	if (_request('exec') == 'articles'
+	OR _request('exec') == 'acces_restreint_edit') {
+		$js .= SelecteurGenerique_inserer_auteur();
+	}
+
+	if (_request('exec') == 'articles'
+	OR _request('exec') == 'naviguer'
+	OR _request('exec') == 'breves_voir'
+	OR _request('exec') == 'sites') {
+		$js .= SelecteurGenerique_inserer_mot();
+	}
+	if (_request('exec') == 'articles_edit'
+	OR _request('exec') == 'rubriques_edit') {
+		$js .= SelecteurGenerique_inserer_rubrique();
+	}
+	if ($js){
+		/**
+		 * On insère les fonctions de bases supplémentaires
+		 */
+		if(strpos($flux,'selecteur_generique_functions')===FALSE){
+			$functions = find_in_path('javascript/selecteur_generique_functions.js');
+			$js_final .= "
+<script type='text/javascript' src='$functions'></script>
+";
+		};
+		$js_final .= '<script type="text/javascript"><!--'
+		. "\n"
+		. $js
+		. "\n"
+		. '// --></script>'
+		. "\n";
+	}
+	return $flux.$js_final;
+
+}
+
+?>
--- selecteur_generique/selecteurgenerique_fonctions.php
+++ selecteur_generique/selecteurgenerique_fonctions.php
@@ -1,15 +1,110 @@
 <?php
 
+/**
+ * Vérifier la présence des scripts nécessaires au sélecteur générique dans une page
+ * @param string $flux Le contenu de la page
+ */
 function selecteurgenerique_verifier_js($flux){
+	global $spip_version_branche;
 	$contenu = "";
-    if(strpos($flux,'jquery.autocomplete.js')===FALSE){
-		$autocompleter = find_in_path('javascript/jquery.autocomplete.js');
-		$autocompletecss = find_in_path('iautocompleter.css');
-		$contenu .= "
+	/**
+	 * Si on est dans une version récente, on utilise jquery ui
+	 */
+	if(defined('_DIR_PLUGIN_JQUERYUI') && ($spip_version_branche >= '2.1.10')){
+		/**
+		 * On a besoin de jquery.ui.autocomplete.js et de ses dépendances
+		 */
+		if(strpos($flux,'jquery.ui.autocomplete.js')===FALSE){
+			/**
+			 * ui.core.js
+			 */
+			if(strpos($flux,'jquery.ui.core.js')===FALSE){
+				$ui = find_in_path(_DIR_JQUERYUI_JS.'jquery.ui.core.js');
+				$contenu .= "
+<script type='text/javascript' src='$ui'></script>
+";
+			}
+			/**
+			 * ui.widget.js
+			 */
+			if(strpos($flux,'jquery.ui.core.js')===FALSE){
+				$widget = find_in_path(_DIR_JQUERYUI_JS.'jquery.ui.widget.js');
+				$contenu .= "
+<script type='text/javascript' src='$widget'></script>
+";
+			}
+			/**
+			 * ui.position.js
+			 */
+			if(strpos($flux,'jquery.ui.position.js')===FALSE){
+				$position = find_in_path(_DIR_JQUERYUI_JS.'jquery.ui.position.js');
+				$contenu .= "
+<script type='text/javascript' src='$position'></script>
+";
+			}
+			/**
+			 * Finalement on insère l'autocompleteur
+			 */
+			$autocompleter = find_in_path('javascript/jquery.autocomplete.js');
+			
+			$contenu .= "
+<script type='text/javascript' src='$autocompleter'></script>
+";
+		};
+		
+		/**
+		 * On insère également les fonctions de bases supplémentaires
+		 */
+		if(strpos($flux,'selecteur_generique_functions')===FALSE){
+			$functions = find_in_path('javascript/selecteur_generique_functions.js');
+			$contenu .= "
+<script type='text/javascript' src='$functions'></script>
+";
+		};
+		
+		/**
+		 * On intègre la CSS qui va bien également et ses dépendances
+		 */
+		if(strpos($flux,'jquery.ui.autocomplete.css')===FALSE){
+			/**
+			 * ui.core.css
+			 */
+			if(strpos($flux,'jquery.ui.core.css')===FALSE){
+				$ui_css = find_in_path(_DIR_JQUERYUI_CSS.'ui.core.css');
+				$contenu .= "
+<link rel='stylesheet' href='$ui_css' type='text/css' media='all' />
+";
+			}
+			/**
+			 * ui.autocomplete.css
+			 */
+			if(strpos($flux,'jquery.ui.autocomplete.css')===FALSE){
+				$autocomplete_css = find_in_path(_DIR_JQUERYUI_CSS.'jquery.ui.autocomplete.css');
+				$contenu .= "
+<link rel='stylesheet' href='$autocomplete_css' type='text/css' media='all' />
+";
+			}
+
+			/**
+			 * ui.theme.css
+			 */
+			if(strpos($flux,'jquery.ui.theme.css')===FALSE){
+				$theme_css = find_in_path(_DIR_JQUERYUI_CSS.'jquery.ui.theme.css');
+				$contenu .= "
+<link rel='stylesheet' href='$theme_css' type='text/css' media='all' />
+";
+			}
+		}
+	}else if($spip_version_branche < '2.1.10'){
+	    if(strpos($flux,'jquery.autocomplete.js')===FALSE){
+			$autocompleter = find_in_path('javascript/jquery.autocomplete.js');
+			$autocompletecss = find_in_path('iautocompleter.css');
+			$contenu .= "
 <script type='text/javascript' src='$autocompleter'></script>
 <link rel='stylesheet' href='$autocompletecss' type='text/css' media='all' />
-";
-	};
+	";
+		};
+	}
 	return $contenu;
 }
 ?>
