7393e3c75c657733d5b6c4817addaae1ba72f33a
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-08-15 12:03:22 +0000

    On suit le nouveau sélecteur générique à base de jquery.ui
    On reste compatible avec l'ancien mais on nécessite la dernière version quand même
    
    Incrément de version en 0.7.0
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@50472 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spipicious_jquery/spipicious_pipelines.php
+++ spipicious_jquery/spipicious_pipelines.php
@@ -20,7 +20,7 @@
  * @return
  */
 function spipicious_insert_head($flux){
-	global $visiteur_session;
+	global $visiteur_session,$spip_version_branche;
 	include_spip('inc/autoriser');
 	if(autoriser('tagger_spipicious','article',$id_objet,$visiteur_session,$opt)){
 
@@ -30,8 +30,98 @@ function spipicious_insert_head($flux){
 	$selecteur = generer_url_public('selecteurs_tags');
     $tags_link = generer_url_public('inc-tags');
 
-	$flux .= 
+    if(defined('_DIR_PLUGIN_JQUERYUI') && ($spip_version_branche >= '2.1.10')){
+		$flux .= 
 <<<EOS
+<script type="text/javascript"><!--
+	(function($) {
+		var spipicious_call = 0;
+		var appliquer_selecteur_spipicious = function() {
+
+			// chercher l'input de saisie
+			var spipicious = $('input[name=spipicious_tags][autocomplete!=off]');
+
+			var id_objet = $("input#spipicious_id").val();
+			var type = $("input#spipicious_type").val();
+			if((spipicious.size()>0) && ($('.tags_'+type+'_'+id_objet).size()>0) && (spipicious_call > 1)){
+				$.ajax({
+					type: "GET",
+					url:'$tags_link',
+					data: {
+						id_objet : id_objet,
+						objet : type
+					},
+					success:function(data,status){
+						var newdata = jQuery(data+' #tags').html();
+						$('.tags_'+type+'_'+id_objet).addClass('loading').html(newdata).removeClass('loading');
+						$('body').trigger('spipicious_change');
+					}
+				});
+			}
+			
+			
+			//.addClass('loading').load('$tags_link&id_'+type+'='+id_objet).removeClass('loading');
+			spipicious
+				.bind( "keydown", function( event ) {
+					if ( event.keyCode === $.ui.keyCode.TAB &&
+						$( this ).data( "autocomplete" ).menu.active ) {
+						event.preventDefault();
+					}
+				})
+				.autocomplete(
+					{
+						source: function( request, response ) {
+							$.ajax({
+								url: "$selecteur",
+								data:{
+									id_objet:id_objet,
+									type:type,
+									q:extractLast( request.term )
+								},
+								success: function(data) {
+									datas = selecteur_format(data);
+									response( $.map( datas, function( item ) {
+										return item;
+									}));
+								}
+							});
+						},
+						focus: function() {
+							// prevent value inserted on focus
+							return false;
+						},
+						delay: 200,
+						select: function( event, ui ) {
+							var terms = split_multiple( this.value );
+							// remove the current input
+							terms.pop();
+							// add the selected item
+							if (ui.item.result > 0) {
+								terms.push( ui.item.value );
+							}else{
+								terms.push( ui.item.entry );
+							}
+							// add placeholder to get the comma-and-space at the end
+							terms.push( "" );
+							this.value = terms.join( ";" );
+							return false;
+						}
+					}
+				);
+			spipicious_call++;
+		};
+
+		$(function(){
+			appliquer_selecteur_spipicious();
+			onAjaxLoad(appliquer_selecteur_spipicious);
+		});
+	})(jQuery);
+// --></script>
+EOS;
+}
+	else{
+		$flux .=
+		<<<EOS
 <script type="text/javascript"><!--
 	(function($) {
 		var spipicious_call = 0;
@@ -100,7 +190,9 @@ function spipicious_insert_head($flux){
 	})(jQuery);
 // --></script>
 EOS;
-}
+	}
+	}
+	
 	return $flux;
 }
 
