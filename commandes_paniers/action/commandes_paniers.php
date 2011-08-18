03ab4764b18ad74548fc4c6669c0a8b7beac5546
cam.lafit@azerttyu.net <cam.lafit@azerttyu.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-08-18 21:40:18 +0000

    Proposer une action de conversion de paniers en commande
     * Pour le moment on traite un besoin simple la conversion panier -> commande
     * La redirection se fait sur la commande alors créée
    
    Dans les ameliorations on peut considerer :
    
     * des autorisations
     * des definitions des contacts affectés à la commande
     * gerer proprement la redirection
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@50550 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ commandes_paniers/action/commandes_paniers.php
@@ -0,0 +1,36 @@
+<?php
+    if (!defined("_ECRIRE_INC_VERSION")) return;
+
+    include_spip('inc/base');
+
+    function action_commandes_paniers_dist(){
+        $securiser_action = charger_fonction('securiser_action', 'inc');
+        $arg = $securiser_action();
+
+        $champs = array(
+            'statut' => 'encours'
+        );
+
+        $id_objet = sql_insertq(
+            'spip_commandes',
+            $champs
+        );
+
+        pipeline('post_insertion',
+            array(
+                'args' => array(
+                    'table' => 'spip_commandes',
+                    'id_objet' => $id_objet
+                ),
+                'data' => $champs
+            )
+        );
+
+        $supprimer_panier = charger_fonction('supprimer_panier_encours', 'action/');
+        $supprimer_panier();
+
+        include_spip('inc/headers');
+        redirige_par_entete(generer_url_public('commande','id_commande='.$id_objet,true));
+
+    }
+?>
