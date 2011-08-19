e0f65c5acd567a4805e6f48918fb2f8475458be4
cam.lafit@azerttyu.net <cam.lafit@azerttyu.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-08-19 11:07:52 +0000

    Une action pour lier tout objet à une commande : #URL_ACTION_AUTEUR{id_commande,id_objet,objet}
     * Le besoin de base : pouvoir associer un auteur à une commande donnée
     * L'action est elargie pour charger des fonctions de lien pour autre objet par le chargement de le fonction inc_lier_commande_objet($id_commande,$id_objet)
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@50561 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ commandes/action/lier_commande.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Plugin Contacts & Organisations 
+ * Licence GPL (c) 2010-2011 Matthieu Marcillaud, Cyril Marion
+**/
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+function action_lier_commande_dist($arg=null) {
+	if (is_null($arg)){
+		$securiser_action = charger_fonction('securiser_action', 'inc');
+		$arg = $securiser_action();
+	}
+    //id_commande/id_objet/objet
+	$arg = explode('/', $arg);
+
+    $id_commande =intval($arg[0]);
+    $id_objet =intval($arg[1]);
+    $objet = $arg[2];
+
+    if (is_null($objet))
+        $objet = "auteur";
+
+    if ($f=charger_fonction('lier_commande_'.$objet, 'inc')) {
+        $f($id_commande,$id_objet);
+    } else {
+		spip_log("action_lier_commande_".$objet."_dist $arg pas compris");
+    }
+
+}
+
+?>
