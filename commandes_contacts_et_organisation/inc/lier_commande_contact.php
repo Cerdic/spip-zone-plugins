be13aa09468d8a14efd95072084fb854a5bf7dbb
cam.lafit@azerttyu.net <cam.lafit@azerttyu.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-08-19 16:16:25 +0000

    Erreur conceptuelle : il faut tester l'existance avant d'agir
     * Pour le moment on considere qu'une commande ne peut être affectée qu'une seule fois à un contact ou une organisation
     * Ce prerequis implique le controle d'une affectation préalable
      * si existance alors on met à jour
      * sinon on lie les objets
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@50568 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- commandes_contacts_et_organisation/inc/lier_commande_contact.php
+++ commandes_contacts_et_organisation/inc/lier_commande_contact.php
@@ -6,18 +6,25 @@ include_spip('base/abstract_sql');
     function inc_lier_commande_contact_dist($id_commande,$id_contact) {
 
         if (intval($id_commande) && intval($id_contact)) {
-
-            $res = sql_updateq(
+            //La commande est elle deja effectée ?
+            $res = sql_getfetsel(
+                'id_contact',
                 'spip_contacts_liens',
-                array(
-                    'id_contact' => $id_contact,
-                    'objet' => 'commande',
-                    'id_objet' => $id_commande
-                ),
                 'id_objet = '.$id_commande.' AND objet = "commande"'
             );
-
-            if (!$res) {
+            //Si oui on met à jour l'affection
+            if ($res) {
+                $res = sql_updateq(
+                    'spip_contacts_liens',
+                    array(
+                        'id_contact' => $id_contact,
+                        'objet' => 'commande',
+                        'id_objet' => $id_commande
+                    ),
+                    'id_objet = '.$id_commande.' AND objet = "commande"'
+                );
+            //Si non on insert
+            } else {
                 $res = sql_insertq(
                     'spip_contacts_liens',
                     array(
--- commandes_contacts_et_organisation/inc/lier_commande_organisation.php
+++ commandes_contacts_et_organisation/inc/lier_commande_organisation.php
@@ -6,18 +6,24 @@ include_spip('base/abstract_sql');
     function inc_lier_commande_organisation_dist($id_commande,$id_organisation) {
 
         if (intval($id_commande) && intval($id_organisation)) {
-
-            $res = sql_updateq(
+            //La commande est elle deja effectée ?
+            $res = sql_getfetsel(
+                'id_organisation',
                 'spip_organisations_liens',
-                array(
-                    'id_organisation' => $id_organisation,
-                    'objet' => 'commande',
-                    'id_objet' => $id_commande
-                ),
                 'id_objet = '.$id_commande.' AND objet = "commande"'
             );
 
-            if (!$res) {
+            if ($res) {
+                $res = sql_updateq(
+                    'spip_organisations_liens',
+                    array(
+                        'id_organisation' => $id_organisation,
+                        'objet' => 'commande',
+                        'id_objet' => $id_commande
+                    ),
+                    'id_objet = '.$id_commande.' AND objet = "commande"'
+                );
+            } else {
                 $res = sql_insertq(
                     'spip_organisations_liens',
                     array(
