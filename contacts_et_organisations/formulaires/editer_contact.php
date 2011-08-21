f9dfc8905b7cd83bddd4be8dd93947db03ed808e
cyril@cym.fr <cyril@cym.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-08-21 08:53:52 +0000

    suppression des redirections;
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@50617 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- contacts_et_organisations/formulaires/editer_contact.php
+++ contacts_et_organisations/formulaires/editer_contact.php
@@ -19,9 +19,6 @@ function formulaires_editer_contact_traiter_dist($id_contact='new', $id_organisa
 	if ($redirect) refuser_traiter_formulaire_ajax();
 	$res = formulaires_editer_objet_traiter('contact', $id_contact, $id_organisation, 0, $redirect);
 	if ($redirect) {
-		if (!parametre_url($redirect, 'id_contact')) {
-			$redirect = parametre_url($redirect, 'id_contact', $res['id_contact']);
-		}
 		$res['redirect'] = $redirect;
 	}
 	return $res;
