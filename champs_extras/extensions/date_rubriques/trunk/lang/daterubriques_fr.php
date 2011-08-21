d88482f256369d7305ddfc1e57f352177ee399b0
toutati@free.fr <toutati@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-08-21 07:50:15 +0000

    orthographe et iso plutot que utf8
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@50616 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- champs_extras/extensions/date_rubriques/trunk/lang/daterubriques_fr.php
+++ champs_extras/extensions/date_rubriques/trunk/lang/daterubriques_fr.php
@@ -10,31 +10,31 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	//B
 	'bando_date_rubriques'=>'Dates des rubriques',
 	'bouton_attribuer'=>'Attribuer',
-	'bouton_previsualiser'=>'Previsualiser',
+	'bouton_previsualiser'=>'Pr&eacute;visualiser',
 
 	//D
 	'date_label' => 'Date : ',
-	'date_premier_article'=>'Attribuer à chaque rubrique d\'un secteur la date de son article le plus ancien',
+	'date_premier_article'=>'Attribuer &agrave; chaque rubrique d\'un secteur la date de son article le plus ancien',
 
 	//E
-	'entrer_id_secteur'=>'Entrer un numéro de secteur',
-	'entrer_liste_secteurs'=>'Entrez les numéros des secteurs séparés par une virgule',
-	'explication_secteurs' => 'Choisir le ou les secteurs pour lesquel rajouter ce champ supplémentaire. Ne rien sélectionner pour que le champ soit rajouté à toutes les rubriques du site.',
+	'entrer_id_secteur'=>'Entrer un num&eacute;ro de secteur',
+	'entrer_liste_secteurs'=>'Entrez les num&eacute;ros des secteurs s&eacute;par&eacute;s par une virgule',
+	'explication_secteurs' => 'Choisir le ou les secteurs pour lesquels rajouter ce champ suppl&eacute;mentaire. Ne rien s&eacute;lectionner pour que le champ soit rajout&eacute; &agrave; toutes les rubriques du site.',
 
 	//I
 	'id_secteur'=>'id_secteur',
 	'info_rubriques_secteur'=>'Rubriques du secteur @numero@',
 	
 	//L
-	'label_secteurs' => 'Secteurs concernés',
+	'label_secteurs' => 'Secteurs concern&eacute;s',
 
 	//M
-	'mise_a_jour_rubriques'=>'Les dates des rubriques ont été mises a jour',
+	'mise_a_jour_rubriques'=>'Les dates des rubriques ont &eacute;t&eacute; mises a jour',
 
 	//R
 	'restreindre_secteurs'=>'Restreindre aux secteurs suivants',
 	'rubriques_liste'=>'Dater les rubriques par lot',
 
 	//V
-	'valeur_incorrecte'=>'Ce n\'est pas vraiment un numéro!',
+	'valeur_incorrecte'=>'Ce n\'est pas vraiment un num&eacute;ro!',
 );
