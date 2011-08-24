d86569af26a7cbb44bde7ff8670830ad7bcea5cb
ben.spip@gmail.com <ben.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-08-24 15:02:26 +0000

    array_replace_recursive Compatibilite PHP<5.3.0 source : http://www.php.net/manual/en/function.array-replace-recursive.php#92574
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@50699 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- saisies/formulaires/construire_formulaire.php
+++ saisies/formulaires/construire_formulaire.php
@@ -222,6 +222,49 @@ function formulaires_construire_formulaire_traiter($identifiant, $formulaire_ini
 		// S'il y a des valeurs par défaut pour ce type de saisie, on les ajoute
 		if (($defaut = $saisies_disponibles[$ajouter_saisie]['defaut']) and is_array($defaut)){
 			$defaut = _T_ou_typo($defaut, 'multi');
+
+			//Compatibilite PHP<5.3.0 
+			//source : http://www.php.net/manual/en/function.array-replace-recursive.php#92574
+			if (!function_exists('array_replace_recursive'))
+			{
+				function array_replace_recursive($array, $array1)
+				{
+					function recurse($array, $array1)
+					{
+						foreach ($array1 as $key => $value)
+						{
+							// create new key in $array, if it is empty or not an array
+							if (!isset($array[$key]) || (isset($array[$key]) && !is_array($array[$key])))
+							{
+								$array[$key] = array();
+							}
+							// overwrite the value in the base array
+							if (is_array($value))
+							{
+								$value = recurse($array[$key], $value);
+							}
+							$array[$key] = $value;
+						}
+						return $array;
+					}
+ 
+					// handle the arguments, merge one by one
+					$args = func_get_args();
+					$array = $args[0];
+					if (!is_array($array))
+					{
+						return $array;
+					}
+					for ($i = 1; $i < count($args); $i++)
+					{
+						if (is_array($args[$i]))
+						{
+							$array = recurse($array, $args[$i]);
+						}
+					}
+    				return $array;
+				}
+			}
 			$saisie = array_replace_recursive($saisie, $defaut);
 		}
 		$formulaire_actuel = saisies_inserer($formulaire_actuel, $saisie);
