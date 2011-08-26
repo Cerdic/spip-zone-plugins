3ecb474799c2778ba9f245d798f2740c921a335e
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-08-26 15:30:58 +0000

    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@50748 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ traduction_article_autrement/branches/0.3/lang/taa_en.php
@@ -0,0 +1,43 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'affichage_edition_seule' => 'Only display articles in editing mode',
+	'affichage_liste_compacte' => 'Compact display of the article lists',
+
+	// C
+	'changer_langue' => 'Modify the article\'s language',
+
+	// F
+	'fermer' => 'Close',
+
+	// L
+	'lier_traduction' => 'Link a translation',
+
+	// M
+	'modifier_options_langue' => 'options',
+
+	// N
+	'necesite_afficher_objet' => 'This function requires the installation of the plugin',
+
+	// O
+	'options' => 'Modify the language options',
+
+	// R
+	'rang' => 'row',
+
+	// T
+	'titre_plugin' => 'Translate articles differently',
+	'traductions' => 'Translations',
+
+	// V
+	'voir_traductions' => 'View the details of the translations'
+);
+
+?>
