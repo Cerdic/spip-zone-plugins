b6e3dfd3ea1fb984294ab41defe31db02d1fc8a2
cam.lafit@azerttyu.net <cam.lafit@azerttyu.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-08-26 17:28:38 +0000

    Fournir un champ quantité pour les produits
     * Un début simple dans le CVT produit, un nouveau champ apparait avec la quantité associé au produit
     * La vérification et traitement sont à venir
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@50757 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ stocks/stocks_pipelines.php
@@ -0,0 +1,29 @@
+<?php
+
+// Sécurité
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+function stocks_formulaire_charger($flux) {
+
+    $form = $flux['args']['form'];
+
+    if ($form == "editer_produit")  {
+        include_spip('inc/stocks');
+        $id_produit = intval($flux['args']['args'][0]);
+        $quantite = get_quantite("produit",$id_produit);
+
+        // La quantité produit
+        $flux['data']['_saisies'][] = array(
+            'saisie' => 'input',
+            'options' => array(
+                'nom' => 'quantite_produit',
+                'label' => '<:stocks:quantite_produit:>',
+                'defaut' => $quantite
+            )
+        );
+    }
+    
+    return $flux;
+
+}
+?>
