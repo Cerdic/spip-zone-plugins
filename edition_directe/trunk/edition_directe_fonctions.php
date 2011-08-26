c7de219cd24e1542d6c492405bd76c3605a6e912
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-08-26 19:06:19 +0000

    delete cette tentative
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@50762 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- edition_directe/trunk/edition_directe_fonctions.php
+++
@@ -1,9 +0,0 @@
-<?php
-// teste si l'objet est en mode edition directe ou non
-function objet_edition_directe($objet){
-	if(!$config=lire_config('edition_directe'))
-	$config=array('article'=>'on');
-	
-	return $config[$objet];
-}
-?>
