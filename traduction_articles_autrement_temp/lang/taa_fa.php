d2e4c99ed5daa743ad7cbfa789bda2d0e2ce3e47
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-08-26 15:15:49 +0000

    préparation trunk
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@50745 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ traduction_articles_autrement_temp/lang/taa_fa.php
@@ -0,0 +1,43 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'affichage_edition_seule' => 'فقط نمايش مقالات در حالت ويرايش ',
+	'affichage_liste_compacte' => 'نمايش فشرده‌ي فهرست مقالات',
+
+	// C
+	'changer_langue' => 'تغيير زبان مقاله',
+
+	// F
+	'fermer' => 'بستن',
+
+	// L
+	'lier_traduction' => 'پيوست يك ترجمه',
+
+	// M
+	'modifier_options_langue' => 'گزينه‌ها',
+
+	// N
+	'necesite_afficher_objet' => 'اين كاركرد مستلزم نصب پلاگين است',
+
+	// O
+	'options' => 'اصلاح گزينه‌هاي زبان ',
+
+	// R
+	'rang' => 'Rang', # NEW
+
+	// T
+	'titre_plugin' => 'ترجمه‌ي متفاوت متقالات',
+	'traductions' => 'ترجمه‌ها',
+
+	// V
+	'voir_traductions' => 'بنگريد به جزئيات ترجمه‌ها'
+);
+
+?>
---
+++ traduction_articles_autrement_temp/taa_fonctions.php
@@ -0,0 +1,16 @@
+<?php
+function rubrique_traduction($lang,$id_rubrique){
+
+	$id_trad_parent=sql_getfetsel('id_trad','spip_rubriques','id_rubrique=' . sql_quote($id_rubrique));
+	$trad = '';
+	
+	if ($id_trad_parent){
+		$trad = sql_getfetsel('id_rubrique','spip_rubriques','id_trad='. sql_quote($id_trad_parent) . ' AND lang='. sql_quote($lang));
+		}
+ 	else{
+ 	 	$trad = sql_getfetsel('id_secteur','spip_rubriques','id_parent=0 AND lang='. sql_quote($lang));	
+ 		}
+
+	return $trad;
+}
+?>
