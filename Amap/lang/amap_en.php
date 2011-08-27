13349c1effb7aabfaffac516a11682eff610e90d
benlarcher@gmail.com <benlarcher@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-08-27 13:09:39 +0000

    AJout fichier lang en
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@50778 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ Amap/lang/amap_en.php
@@ -0,0 +1,104 @@
+<?php
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'action' => 'Action ?',
+	'action_modifier' => 'Modify',
+	'action_supprimer' => 'Delete',
+	'adherent_sans_type_panier' => 'Your members do not have any basket type assigned.', 
+	'adhesion' => 'Membership (ie:2008)',
+	'adhesion_auteur' => 'Membership :',
+	'administrateur' => 'Administrator',
+	'attention' => 'WARNING !',
+	'attention_modifications' => '<p>You have just activated the CSA (Community Supported Agriculture) plugin. It created a new section "Season agenda" which includes two sub-sections "Delivery" and "Events". It also created the section "Archives".</p>
+									1. Before continuing, please fill up all the dates of the season via the plugin agenda.<br />
+									2. This has to be done before each new season begins.<br />
+
+									3. At the end of each season, you have to update the CSA members list and archive into the appropriate sections, the dates of last season, this is needed for a proper functioning of this plugin.<br />
+
+									4. Update the basket type and the subscriptions on each author page, or no basket  delivery will be attributed.<br />
+									<p><strong>NON RESPECT OF THOSE RULES WILL RESULT IN THE PLUGIN DYSFUNCTION</strong></p>', 
+	'aucun_panier_pour_nom' => 'There are no basket for @nom@.',
+	'avant_le' => 'before',
+
+	// C
+	'contenu_panier' => 'Basket content',
+
+	// D
+	'date_distribution' => 'Delivery date',
+	'distribution_paniers' => 'Basket delivery of @nb@',
+
+	// E
+	'enregistrement' => 'Registration',
+	'enregistrement_panier' => 'Basket registration',
+	'envoyer' => 'Send',
+
+	// G
+	'gestion_amap' => 'CSA administration',
+	'grand' => 'Big',
+
+	// I
+	'information_amap' => 'CSA information',
+	'impression' => 'Print',
+	'impression_donnees' => 'Print data',
+	'impression_donnees_fonction_date' => 'Print baskets from a specific date selected :',
+	'impression_explication' => 'Only the dates with at least 1 basket are clickable and open a new window.',
+
+	// L
+	'les_paniers' => 'The basket of',
+	'liste_amapiens' => 'CSA members list',
+	'liste_amapiens_enregistres'  => 'registered CSA members list',
+	'liste_paniers' => 'Basket list',
+	'liste_paniers_distribuer_le' => 'Basket list to deliver on',
+	'livraison' => 'Delivery',
+
+	// M
+	'mettre_disposition' => 'Place at disposal',
+	'mini_doc' => 'Short documentation',
+
+	// N
+	'nom'  => 'NAME',
+
+	// P
+	'panier' => 'Basket',
+	'panier_dispo' => 'Basket available',
+	'panier_dispo_auteur' => 'Hello,
+		<br />The basket of @date_distribution@ is at disposal
+		<br />@nom@',
+	'panier_distribuer' => 'basket to deliver',
+	'paniers_distribuer' => 'baskets to deliver',
+	'panier_liste' => 'Basket list',
+	'panier_livraison' => 'Basket content',
+	'pas_connecte_ou_reconnu' => 'You are not logged in or not identified.',
+	'pas_date_distributions' => 'No distributuion date are filled in.',
+	'pas_paniers' => 'You do not have any basket for this season, no contract has been done concerning you',
+	'petit' => 'Small',
+
+	// R
+	'reste_panier_distribuer' => 'We still have 1 basket to deliver',
+	'reste_panier_recuperer' => 'You still have 1 basket to get',
+	'reste_paniers_distribuer' => 'We still have @nb@ baskets to deliver',
+	'reste_paniers_recuperer' => 'You still have @nb@ baskets to get',
+	'retour_auteur' => 'Back to the author page',
+
+	//S
+	'signature' => 'Signature',
+
+	// T
+	'table_vide_aucun_enregistrement' => 'This table is empty :
+					<br />It does not contain any registration.',
+	'type_panier' => 'Basket type',
+	'type_panier_auteur' => 'Basket type :',
+
+	//U
+	'utiliser_entete_colone_tri' => 'Use column header to sort CSA Members (in black active sorting method and in green available ones).',
+
+	// V
+	'visiteur' => 'Visitor',
+	'vos_paniers' => 'Your baskets',
+);
+
+?>
