57c83401fa0300954e3dd4027fd74d5a6892872f
james@rezo.net <james@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-08-28 22:26:52 +0000

    ménage plugin.xml
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@50822 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- nuage/trunk/nuage_fonctions.php
+++ nuage/trunk/nuage_fonctions.php
@@ -206,4 +206,19 @@ function nuage_extrait($nuage,$nombre){
 	if (!is_array($nuage)) $nuage = unserialize($nuage);
 	return array_splice($nuage,$nombre);   
 }
+
+function nuage_insert_head_css($flux) {
+	$css = "<link rel='stylesheet' href='".find_in_path("css/nuage.css")."' type='text/css' />\n";
+	static $done = false;
+	if (!$done) {
+		$done = true;
+		$flux .= "\n".$css;
+	}
+	return $flux;
+}
+
+function nuage_insert_head($flux){
+	$flux = nuage_insert_head_css($flux); // au cas ou il n'est pas implemente
+	return $flux;
+}
 ?>
