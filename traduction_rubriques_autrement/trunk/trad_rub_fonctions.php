ebed4074491aa71eef5ef4086a42b6befcf01eef
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-08-29 15:40:44 +0000

    - Permettre d'avoir un autre set de rubriques traduites à la racine - enlever ce qui n'est pas nécessaire
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@50841 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- traduction_rubriques_autrement/trunk/trad_rub_fonctions.php
+++ traduction_rubriques_autrement/trunk/trad_rub_fonctions.php
@@ -1,20 +1,20 @@
 <?php
+
+// Détermine l'id_parent de la nouvell rubrique traduite
 function destination_traduction($lang,$id_trad){
-	$id_trad_parent=sql_fetsel('id_trad,id_parent','spip_rubriques','id_rubrique='.sql_quote($id_trad));
-	if($id_trad_parent['id_trad']){
-		$trads = sql_getfetsel('id_parent','spip_rubriques','id_trad='.sql_quote($id_trad_parent['id_trad']).' AND lang='.sql_quote($lang));	
-		if($id_trad=sql_getfetsel('id_trad','spip_rubriques','id_trad='.sql_quote($id_trad_parent['id_parent']).' AND lang='.sql_quote($lang)))
-			$trads = sql_getfetsel('id_rubrique','spip_rubriques','id_trad='.sql_quote($id_trad).' AND lang='.sql_quote($lang));	
+
+	// on établit l'id_parent
+	$id_trad_parent=sql_getfetsel('id_parent','spip_rubriques','id_rubrique='.$id_trad);
+	
+	//puis sa traduction
+	$id_parent_trad=sql_getfetsel('id_trad','spip_rubriques','id_rubrique='.$id_trad_parent);
+	
+	// S'il il existe une traduction parente dans la langue demandé on retourne son id
+	if($id_parent_trad) $trads = sql_getfetsel('id_rubrique','spip_rubriques','id_trad='.$id_parent_trad.' AND lang='.sql_quote($lang));
+	//Sinon on cherche à la racine	 et on retourne l'id d'une rubrique non traduite
+ 	else{
+ 		$trads = sql_getfetsel('id_rubrique','spip_rubriques','id_parent=0 AND id_trad=0 AND lang='.sql_quote($lang));	
 		}
-	elseif($id_trad_parent['id_parent']){
-		$id_trad_parent2=sql_fetsel('id_trad,id_parent','spip_rubriques','id_rubrique='.sql_quote($id_trad_parent['id_parent']));
-		if($id_trad_parent2['id_trad']){
-				$trads = sql_getfetsel('id_rubrique','spip_rubriques','id_trad='.sql_quote($id_trad_parent2['id_trad']).' AND lang='.sql_quote($lang));
-			}
-		}
- 	if(!$trads){
- 	 	$trads = sql_getfetsel('id_rubrique','spip_rubriques','id_parent=0 AND lang='.sql_quote($lang));		
- 		}
 return $trads;
 }	 
 ?>
