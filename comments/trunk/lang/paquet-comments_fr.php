898027dfaa14cb3c74109d76242f49d70492613e
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-08-30 17:46:30 +0000

    comments mets le paquet pour SPIP 3 !
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@50886 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ comments/trunk/lang/paquet-comments_fr.php
@@ -0,0 +1,13 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/comments/comments-200/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	'comments_slogan' => 'Des commentaires tout simplement',
+	'comments_description' => 'Affichage des messages en liste, façon commentaires de blog, avec formulaire simplifié. Commentaires microformatés, nomenclature homogéne.',
+
+);
+
+?>
