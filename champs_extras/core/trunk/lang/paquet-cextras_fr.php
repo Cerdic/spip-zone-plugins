fd8996a38dae7ed2c518bdcb9f563b526104fe2d
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-08-30 22:14:37 +0000

    paquet
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@50899 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ champs_extras/core/trunk/lang/paquet-cextras_fr.php
@@ -0,0 +1,8 @@
+<?php
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	'cextras_titre' => 'Champs Extras',
+	'cextras_slogan' => 'Créer de nouveaux champs aux objets d\'édition de SPIP',
+	'cextras_description' => ' ',
+);
+?>
