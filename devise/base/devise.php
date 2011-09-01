eb4573686885767c05d517ac346a08425fb82e46
da@weeno.net <da@weeno.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-09-01 03:05:39 +0000

    On utilise maintenant la saisie dans la définition du champ
    La définition du champ extra utilise maintenant la saisie "externe" (c'est
    un grand mot, vu qu'on la fournit nous...) pour définir le champ devise
    des auteurs.
    
    La présentation formatée (XXX - Nom) reste présente dans le select lors
    de la modification de la fiche auteur, mais dans la vue de la fiche on n'a
    plus que le code ISO, ce qui n'est pas très grave mais un peu dommage.
    Si quelqu'un sait comment faire...
    
    
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@50925 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- devise/base/devise.php
+++ devise/base/devise.php
@@ -1,11 +1,6 @@
 <?php
 if (!defined("_ECRIRE_INC_VERSION")) return;
 function devise_declarer_champs_extras($champs = array()){
-	include_spip('base/devise_options');
-	$lines = Array();
-	foreach (devises_codes() as $code) {
-		$lines[$code] = "$code - "._T("devise:$code");
-	}
 	$champs[] = new ChampExtra(array(
 		'table' => 'auteurs', // sur quelle table ?
 		'champ' => 'devise', // nom sql
@@ -13,9 +8,10 @@ function devise_declarer_champs_extras($champs = array()){
 		// 'precisions' => '', // precisions sur le champ
 		'obligatoire' => false, // 'oui' ou '' (ou false)
 		'rechercher' => false, // false, ou true ou directement la valeur de ponderation (de 1 à 8 generalement)
-		'type' => 'menu-enum', // type de saisie
-		'enum' => $lines, // valeurs possibles
+		'type' => 'devise', // type de saisie
 		'sql' => "text NOT NULL DEFAULT ''", // declaration sql
+		// experimental
+		'saisie_externe' => true, // saisies
 	));
 	return $champs;
 }
