6fb5d4daa905eafb71bd29a8fa40d1f4110722e5
da@weeno.net <da@weeno.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-09-01 02:54:20 +0000

    Oups, fichier commité trop vite (ça part dans le wagon suivant)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@50923 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- devise/base/devise.php
+++ devise/base/devise.php
@@ -1,6 +1,11 @@
 <?php
 if (!defined("_ECRIRE_INC_VERSION")) return;
 function devise_declarer_champs_extras($champs = array()){
+	include_spip('base/devise_options');
+	$lines = Array();
+	foreach (devises_codes() as $code) {
+		$lines[$code] = "$code - "._T("devise:$code");
+	}
 	$champs[] = new ChampExtra(array(
 		'table' => 'auteurs', // sur quelle table ?
 		'champ' => 'devise', // nom sql
@@ -8,10 +13,9 @@ function devise_declarer_champs_extras($champs = array()){
 		// 'precisions' => '', // precisions sur le champ
 		'obligatoire' => false, // 'oui' ou '' (ou false)
 		'rechercher' => false, // false, ou true ou directement la valeur de ponderation (de 1 à 8 generalement)
-		'type' => 'devise', // type de saisie
+		'type' => 'menu-enum', // type de saisie
+		'enum' => $lines, // valeurs possibles
 		'sql' => "text NOT NULL DEFAULT ''", // declaration sql
-		// experimental
-		'saisie_externe' => true, // saisies
 	));
 	return $champs;
 }
