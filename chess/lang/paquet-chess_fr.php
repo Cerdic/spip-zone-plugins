d6a1da8e3ce6e87d5397fcb0fde6612a933f40a8
jack@lmpe.fr <jack@lmpe.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-09-02 11:13:29 +0000

    - utilise cfg et pas nécessite - paquet.xml avec compatibilité spip 3.0
    en spip 3.0 ce serait bien de se dispenser de cfg... mais je n'ai pas (encore ?) réussi à créer un formulaire  configurer_chess.html qui fonctionne.
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@50957 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ chess/lang/paquet-chess_fr.php
@@ -0,0 +1,10 @@
+<?php
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	'chess_titre' => '',
+	'chess_slogan' => 'Pour voir des parties d\'&#233;checs avec les fichiers pgn',
+	'chess_description' => 'Ce plugin est destin&#233; &#224; lire en ligne des parties d\'&#233;checs grace &#224; des fichiers PGN.  
+  Il suffit d\'ins&#233;rer dans un article la balise &#60;chessXX&#62; o&#249; XX est le num&#233;ro de l&#39;article et de t&#233;l&#233;charger les documents pgn.',
+
+ );
+?>
