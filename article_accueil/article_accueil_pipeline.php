1f42b6ee6bc86fad4ddf99c2160736083d4bb048
da@weeno.net <da@weeno.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-09-06 21:49:40 +0000

    chemin() disparaît, find_in_path() reste
    En SPIP 3, il semble que la fonction chemin() a disparu... Comme c'était
    un synonyme de find_in_path(), on utilise donc plutôt ce dernier.
    
    
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@51081 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- article_accueil/article_accueil_pipeline.php
+++ article_accueil/article_accueil_pipeline.php
@@ -20,7 +20,7 @@ function article_accueil_affiche_milieu($flux){
 			if (autoriser('modifier','rubrique',$id)) {
 				$ids = 'formulaire_editer_article_accueil-' . $id;
 				$bouton = bouton_block_depliable(strtoupper(_T('article_accueil:article_accueil')), false, $ids);
-				$flux['data'] .= debut_cadre('e', chemin('article_accueil-24.png','images/'),'',$bouton, '', '', true);
+				$flux['data'] .= debut_cadre('e', find_in_path('article_accueil-24.png','images/'),'',$bouton, '', '', true);
 				$flux['data'] .= recuperer_fond('prive/editer/article_accueil', array_merge($_GET, array('id'=>$id)));
 				$flux['data'] .= fin_cadre();
 			}
