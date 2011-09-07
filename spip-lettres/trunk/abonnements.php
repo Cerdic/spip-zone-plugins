02538afb63233055ea6a1f244bd034822c98d93c
jluc@no-log.org <jluc@no-log.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-09-07 14:18:00 +0000

    UTF8 sans BOM pour les accents (?) et correction pour une boucle sur une table dont la déclaration fait visiblement défaut... en attendant correction des déclarations (faudrait aussi déclarer les diverses jointures).
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@51109 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spip-lettres/trunk/abonnements.php
+++ spip-lettres/trunk/abonnements.php
@@ -1,13 +1,12 @@
 [(#REM)
-	Exemple de boucles présentant les abonnements de l'internaute connecté
+	Exemple de boucles prÃ¨sentant les abonnements de l'internaute connectÃ©
 ]
 <BOUCLE_auteur(ABONNES){0,10}{objet=auteurs}{id_objet=#SESSION{id_auteur}}>
-	Votre n° d'abonné : #ID_ABONNE<br>
-	
+	Votre nÂ° d'abonnÃ© : #ID_ABONNE<br>
 	<B_abonnements>
-	Voici les thèmes des abonnements auxquels vous avez souscrit :
+	Voici les thÃ¨mes des abonnements auxquels vous avez souscrit :
 	<ul>
-		<BOUCLE_abonnements(ABONNES_RUBRIQUES){id_abonne}>
+		<BOUCLE_abonnements(spip_abonnes_rubriques){id_abonne}>
 			<BOUCLE_theme(THEMES){id_rubrique}>
 				<li>#TITRE</li>
 			</BOUCLE_theme>
@@ -17,6 +16,6 @@
 	Vous n'avez souscrit aucun abonnement sur ce site.
 	<//B_abonnements>
 </BOUCLE_auteur>
-	Cette page permet aux inscrits de connaître l'état de leurs abonnements aux lettres de ce site.
+	Cette page permet aux inscrits de connaÃªtre l'Ã©tat de leurs abonnements aux lettres de ce site.
 	#LOGIN
 <//B_auteur>
