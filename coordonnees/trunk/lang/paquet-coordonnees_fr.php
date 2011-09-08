8f17d39baa5af8a45ec734b7cd805ada964e98d9
marcimat@rezo.net <marcimat@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-09-08 16:17:10 +0000

    Langues manquantes.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@51154 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ coordonnees/trunk/lang/paquet-coordonnees_fr.php
@@ -0,0 +1,11 @@
+<?php
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	'coordonnees_nom' => "Coordonn&eacute;es",
+	'coordonnees_slogan' => "Des coordonn&eacute;es postales, t&eacute;lephoniques ... pour vos auteurs",
+	'coordonnees_description' => "Ajoute la possibilit&eacute; de donner des coordonn&eacute;es postales (adresses, mails, t&eacute;l&eacute;phones)
+	aux objets de SPIP, tout particuli&egrave;rement aux auteurs. Plusieurs adresses et num&eacute;ros de t&eacute;l&eacute;phone peuvent ainsi &eacirc;tre donn&eacute;s &agrave; chaque auteur.",
+);
+
+?>
