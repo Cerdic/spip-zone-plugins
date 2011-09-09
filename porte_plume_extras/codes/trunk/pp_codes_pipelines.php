1dafc0ded78515828a292b85d3653ab6ecd46f69
marcimat@rezo.net <marcimat@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-09-09 14:38:20 +0000

    Suivre http://zone.spip.org/trac/spip-zone/changeset/51202 .
    On utilise la nouvelle API ->ajouterPlusieursApres pour ajouter plusieurs boutons d'un coup Après le bouton <cadre> du porte plume.
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@51203 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- porte_plume_extras/codes/trunk/pp_codes_pipelines.php
+++ porte_plume_extras/codes/trunk/pp_codes_pipelines.php
@@ -5,86 +5,82 @@ function pp_codes_porte_plume_barre_pre_charger($barres){
 	// on ajoute les boutons dans les 2 barres de SPIP
 	foreach (array('edition' => true,'forum' => false) as $nom=>$visible) {
 		$barre = &$barres[$nom];
-		$barre->ajouterApres('grpCaracteres', array(
-					"id" => "sepCode",
-					"separator" => "---------------",
-					"display"   => $visible,
+
+		$barre->ajouterPlusieursApres('cadre', array(
+			// bouton <cadre spip>
+			array(
+				"id"          => 'cadre_spip',
+				"name"        => _T('pp_codes:outil_inserer_cadre_spip'),
+				"className"   => 'outil_cadre_spip', 
+				"openWith" => "<cadre class='spip'>\n",
+				"closeWith" => "\n</cadre>",
+				"display"     => $visible,
+			), 
+			// bouton <cadre php>
+			array(
+				"id"          => 'cadre_php',
+				"name"        => _T('pp_codes:outil_inserer_cadre_php'),
+				"className"   => 'outil_cadre_php', 
+				"openWith" => "<cadre class='php'>\n",
+				"closeWith" => "\n</cadre>",
+				"display"     => $visible,
+			),
+			// bouton <cadre xml>
+			 array(
+				"id"          => 'cadre_xml',
+				"name"        => _T('pp_codes:outil_inserer_cadre_xml'),
+				"className"   => 'outil_cadre_xml', 
+				"openWith" => "<cadre class='xml'>\n",
+				"closeWith" => "\n</cadre>",
+				"display"     => $visible,
+			),
+			// bouton <pre>
+			array(
+				"id"          => 'pre',
+				"name"        => _T('pp_codes:outil_inserer_pre'),
+				"className"   => 'outil_pre', 
+				"openWith" => "<pre>",
+				"closeWith" => "</pre>",
+				"display"     => $visible,
+			),
+			// bouton <var>
+			array(
+				"id"          => 'var',
+				"name"        => _T('pp_codes:outil_inserer_var'),
+				"className"   => 'outil_var', 
+				"openWith" => "<var>",
+				"closeWith" => "</var>",
+				"display"     => $visible,
+			),
+			// bouton <samp>
+			array(
+				"id"          => 'samp',
+				"name"        => _T('pp_codes:outil_inserer_samp'),
+				"className"   => 'outil_samp',
+				"openWith" => "<samp>",
+				"closeWith" => "</samp>",
+				"display"     => $visible,
+			),
+			// bouton <kbd>
+			array(
+				"id"          => 'kbd',
+				"name"        => _T('pp_codes:outil_inserer_kbd'),
+				"className"   => 'outil_kbd',
+				"openWith" => "<kbd>",
+				"closeWith" => "</kbd>",
+				"display"     => $visible,
+			),
+			// Lien vers Trac 
+			// trop specifique a SPIP pour etre affiche par defaut...
+			array(
+				"id"          => 'lienTrac',
+				"name"        => _T('pp_codes:outil_inserer_lien_trac'),
+				"className"   => 'outil_lien_trac',
+				"openWith" => "[?",
+				"closeWith" => "#trac]",
+				"display"     => false,
+			),
 		));
-		$barre->ajouterApres('sepCode', array(
-					// groupe code et bouton <code>
-					"id"          => 'grpCode',
-					"name"        => _T('pp_codes:outil_inserer_code'),
-					"className"   => 'outil_code',
-					"openWith" => "<code>",
-					"closeWith" => "</code>",		
-					"display"     => $visible,
-					"dropMenu"    => array(
-						// bouton <cadre>
-						array(
-							"id"          => 'cadre_spip',
-							"name"        => _T('pp_codes:outil_inserer_cadre_spip'),
-							"className"   => 'outil_cadre_spip', 
-							"openWith" => "<cadre class='spip'>\n",
-							"closeWith" => "\n</cadre>",
-							"display"     => $visible,
-						),
-						// bouton <cadre>
-						array(
-							"id"          => 'cadre_php',
-							"name"        => _T('pp_codes:outil_inserer_cadre_php'),
-							"className"   => 'outil_cadre_php', 
-							"openWith" => "<cadre class='php'>\n",
-							"closeWith" => "\n</cadre>",
-							"display"     => $visible,
-						),
-						// bouton <pre>
-						array(
-							"id"          => 'pre',
-							"name"        => _T('pp_codes:outil_inserer_pre'),
-							"className"   => 'outil_pre', 
-							"openWith" => "<pre>",
-							"closeWith" => "</pre>",
-							"display"     => $visible,
-						),
-						// bouton <var>
-						array(
-							"id"          => 'var',
-							"name"        => _T('pp_codes:outil_inserer_var'),
-							"className"   => 'outil_var', 
-							"openWith" => "<var>",
-							"closeWith" => "</var>",
-							"display"     => $visible,
-						),
-						// bouton <samp>
-						array(
-							"id"          => 'samp',
-							"name"        => _T('pp_codes:outil_inserer_samp'),
-							"className"   => 'outil_samp',
-							"openWith" => "<samp>",
-							"closeWith" => "</samp>",
-							"display"     => $visible,
-						),
-						// bouton <kbd>
-						array(
-							"id"          => 'kbd',
-							"name"        => _T('pp_codes:outil_inserer_kbd'),
-							"className"   => 'outil_kbd',
-							"openWith" => "<kbd>",
-							"closeWith" => "</kbd>",
-							"display"     => $visible,
-						),
-						// Lien vers Trac 
-						// trop specifique a SPIP pour etre affiche par defaut...
-						array(
-							"id"          => 'lienTrac',
-							"name"        => _T('pp_codes:outil_inserer_lien_trac'),
-							"className"   => 'outil_lien_trac',
-							"openWith" => "[?",
-							"closeWith" => "#trac]",
-							"display"     => false,
-						),
-					),
-				));
 	}
 	return $barres;
 }
@@ -112,8 +108,6 @@ function pp_codes_porte_plume_barre_charger($barres){
 
 function pp_codes_porte_plume_lien_classe_vers_icone($flux){
 	return array_merge($flux, array(
-		'outil_code'=>'tag.png',
-		'outil_cadre'=>'page_white_code.png',
 		'outil_cadre_spip'=>'cadre_spip.png',
 		'outil_cadre_php'=>'page_white_php.png',
 		'outil_pre'=>'page_white_code_red.png',
