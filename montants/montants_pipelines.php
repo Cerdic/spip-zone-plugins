ff9682c92fd02e4ec5d5b21d9aa4df6abc66aa29
toutati@free.fr <toutati@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-09-09 10:25:10 +0000

    Simplifier l'ecriture
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@51189 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- montants/montants_pipelines.php
+++ montants/montants_pipelines.php
@@ -1,42 +1,32 @@
 <?php
 /**
- * todo faire descriptif ou 'commentaire' champ supp pour montants
  * Insertion dans le pipeline prix_ht
- * si l'objet n'a pas de prix, 
- * on cherche son prix et si il existe on lui applique
+ * si l'objet n'a pas de prix propre 
+ * on cherche son prix dans la table montants
  *
  * @return
  * @param object $flux
  */
- 
+
+//ssi montant existe afficher le prix des objets sur leurs pages
 function montants_affiche_milieu($flux){
-	if($flux['args']['exec'] == 'naviguer') {
-		$affiche_prixdefaut = recuperer_fond('prive/prixdefaut_rubrique',
-			array('id_objet'=>$flux['args']['id_rubrique'],	
-			'objet'=>'rubrique'
-			));
-		$flux['data'] .= $affiche_prixdefaut;
+	
+	$pages= array('naviguer'=>'rubrique','articles'=>'article','mots_edit'=>'mot');
+	foreach($pages AS $page => $objet){
+		if($flux['args']['exec'] == $page) {
+			$montant=sql_getfetsel("id_montant","spip_montants","objet='$objet'");
+			if($montant)
+			$flux['data'] .= recuperer_fond("prive/prixdefaut_$objet",
+				array('id_objet'=>$flux['args']['id_'.$objet],	
+				'objet'=>$objet
+				));
+		}
 	}
 	
-	if($flux['args']['exec'] == 'articles') {
-		$affiche_prixdefaut = recuperer_fond('prive/prixdefaut_article',
-			array('id_objet'=>$flux['args']['id_article'],	
-			'objet'=>'article'
-			));
-		$flux['data'] .= $affiche_prixdefaut;
-	}
-
-	if($flux['args']['exec'] == 'mots_edit') {
-		$affiche_prixdefaut = recuperer_fond('prive/prixdefaut_mot',
-			array('id_objet'=>$flux['args']['id_mot'],	
-			'objet'=>'mot'
-			));
-		$flux['data'] .= $affiche_prixdefaut;
-	}
-	return $flux;
+return $flux;
 }
 
-//passer tout ça en squelettes?
+		
 function montants_prix_ht($flux){
 	
 	$prixdefaut=$flux['data'];
@@ -71,7 +61,7 @@ function montants_prix_ht($flux){
 					}
 					break;
 				case 'mot':
-					//groupe du mot
+					//groupe du mot !obligatoire
 					$parents[] =sql_getfetsel("id_groupe","spip_mots","id_mot=".$id_objet);
 					break;
 				default: 
@@ -79,17 +69,21 @@ function montants_prix_ht($flux){
 					break;
 			}
 		
+	
+	
+		if(!$prix_ht){
 		// l'objet doit appartenir a un parent spécifique
 		$prix_ht=sql_getfetsel('prix_ht', 'spip_montants',"objet='".$type_objet."' AND le_parent IN (".join(',',$parents).")");
-		spip_log("$type_objet $id_objet prix $prix_ht parents=".join(',',$parents),'montants');
-	}
+		//spip_log("log 0 $type_objet $id_objet prix $prix_ht parents=".join(',',$parents),'montants');
+		}
+	
 	
 	//sinon le prix est-il pour id_objet dans la liste specifique
 	foreach($ids_objet as $ids){
 					$array_ids = explode(",", $ids['ids_objet']);
 					if(count($array_ids)<1) $id_montant=$ids['id_montant'];
 						if(in_array($id_objet,$array_ids)){
-						spip_log("HT SPECIAL $type_objet $id_objet coute ".$ids['prix_ht'],'montants');
+						//spip_log("log 1 $type_objet $id_objet coute ".$ids['prix_ht'],'montants');
 						$prix_ht=$ids['prix_ht'];
 						}
 						
@@ -97,18 +91,16 @@ function montants_prix_ht($flux){
 	
 	if(!$prix_ht){
 	// sinon son prix est declare par defaut dans spip_montants
-	$prixdefaut = sql_getfetsel('prix_ht', 'spip_montants','objet='."'$type_objet' AND 'id_montant' = '$id_montant'");
-	$prix_ht=$prixdefaut['prix_ht'];
-	
-	spip_log("HT defaut pour $type_objet ($id_objet) est $prixdefaut",'montants');
+	$prix_ht = sql_getfetsel('prix_ht', 'spip_montants','objet='."'$type_objet' AND 'id_montant' = '$id_montant'");	
+	//spip_log("log 2 pour montant($id_montant) = $type_objet ($id_objet) est $prixdefaut",'montants');
 	}
 	
-	if ($prixdefaut==0 && $prix_ht) {
-		$flux['data']=$prix_ht;
+	if ($prix_ht) $flux['data']=$prix_ht;
+	//si aucun prix generer une erreur manifeste = trop chere pour etre vrai a revoir?
+	else $flux['data']=999.99;
+	
 	}
 	
-	//erreur manifeste = trop chere pour etre vrai
-	if(!$flux['data']) $flux['data']=999.99;
 	return $flux;
 }
 
