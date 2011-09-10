95ffc297245ac7ebab7a166c8a6b5e93b50aca77
cam.lafit@azerttyu.net <cam.lafit@azerttyu.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-09-10 10:58:50 +0000

    Rendre fonctionnel l'ajout des articles liés en SPIP3
     * Les inclusions doivent être explicites
     * Ne pas jouer les attila, en pensant que c'est une faute de frappe
      * rajout de l'exec article, plus pratique pour lier un article
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@51224 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- a2a/a2a_pipelines.php
+++ a2a/a2a_pipelines.php
@@ -2,10 +2,12 @@
 
 function a2a_affiche_milieu($flux){
 	include_spip('public/assembler');
+        include_spip('inc/layer');
+        include_spip('inc/presentation');
 	
 	$contexte = array();
 	
-	if ($flux['args']['exec'] == "articles"){
+	if (($flux['args']['exec'] == "articles") || ($flux['args']['exec'] == "article")){
 		$contexte['id_article_orig'] = $flux["args"]["id_article"];
 		$flux['data'] .= "<div id='pave_a2a'>";
 		$bouton = bouton_block_depliable(_T('a2a:articles_lies'), "replie", "pave_a2a_depliable");
