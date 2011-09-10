82b7588293f67cd0d5a520d278e588a6f5b6a349
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-09-10 14:41:52 +0000

    Notifications met le paquet.xml pour SPIP 3
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@51229 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ notifications/lang/paquet-notifications_en.php
@@ -0,0 +1,13 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/_dev_/notifications/lang
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	// A
+	'notifications_slogan' => 'Advanced notifications',
+	'notifications_description' => 'Sends mail notifications for certain actions : forums posts and answers notified to users, private messages notified to redactors or articles publication notified to their authors.',
+
+);
+
+?>
---
+++ notifications/lang/paquet-notifications_fr.php
@@ -0,0 +1,13 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/_dev_/notifications/lang
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	// A
+	'notifications_slogan' => 'Notifications avancées',
+	'notifications_description' => 'Active les notifications par email de certaines actions, notamment des forums et de la messagerie de l\'espace priv&#233;, permet &#233;galement l\'envoi d\'un mail aux auteurs lors de la publication de leurs articles.',
+
+);
+
+?>
