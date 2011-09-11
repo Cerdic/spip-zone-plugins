e04c0c4bec9420492d9ce538a20308ca74a3bbe3
yffic@lefourneau.com <yffic@lefourneau.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-09-11 19:32:07 +0000

    - Ajout d'un exemple pour les options de configuration permettant d'ouvrir le menu sur l'élément courant - Chaînes de langues - icônes et correction de balises dans la page de config
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@51262 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ menu_accordeon2/lang/accordeon_fr.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	// C
+	'cfg_boite_accordeon' => 'Configuration avancée du plugin Menu Accordéon',
+	'cfg_titre_accordeon' => 'Menu jQuery Accordéon',
+	
+	// E
+	'explication_options'=>'Vous pouvez mettre ici les options à passer au script d\'effet accordéon. <a href="http://jqueryui.com/demos/accordion/">Voir la documentation d\'accordéon</a>. Ne pas mettre les accolades. Saisir par exemple <strong>collapsible: true, active: \'.accordeon li.on a\'</strong>, pour ouvrir le menu de classe accordeon sur l\'élément courant',
+
+	// L
+	'label_options' => 'Animation',
+);
+?>
