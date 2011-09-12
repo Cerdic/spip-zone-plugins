9bde7c249fd1fcc79dd47388ce298be9b0b07e32
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-09-12 08:42:09 +0000

    Report pour enlever complètement Yahoo et maquer Bing.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@51277 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- gis/trunk/gis_pipelines.php
+++ gis/trunk/gis_pipelines.php
@@ -31,8 +31,6 @@ function gis_inserer_javascript($flux){
 		$flux .="\n".'<script type="text/javascript" src="http://api.maps.ovi.com/jsl.js"></script>'."\n";
 	if ($config['api'] == 'yandex')
 		$flux .="\n".'<script type="text/javascript" src="http://api-maps.yandex.ru/1.1/index.xml?key='.$config['api_key_yandex'].'"></script>'."\n";
-	if ($config['api'] == 'yahoo')
-		$flux .="\n".'<script type="text/javascript" src="http://api.maps.yahoo.com/ajaxymap?v=3.8&appid=MapstractionDemo"></script>'."\n";
 	
 	// insertion de la lib mapstraction
 	if(in_array($config['api'],array('cartociudad','google','googlev3','yandex'))){
