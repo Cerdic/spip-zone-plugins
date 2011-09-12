d1b39385f513969424ba1a1053e40c763889e5b2
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-09-12 07:08:56 +0000

    Allez faut bien commencer : voilà donc le paquet.xml + des fichiers images mieux placés + les fonctions d'installation/upgrade.
    Le plugin s'installe et fonctionne, mise à part le bug sur l'installation des librairies en SPIP 3.
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@51271 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ gis/trunk/formulaires/configurer_gis.php
@@ -0,0 +1,46 @@
+<?php 
+
+/**
+ * Fonction de verification du formulaire de configuration
+ * - On vérifie si dans les cas de cloudmade ou de google (v2), une clé a 
+ * été fournie
+ */
+function formulaires_configurer_gis_verifier_dist(){
+	$erreurs = array();
+	
+	if(in_array(_request('api'), array('cloudmade','google'))){
+		$obligatoire = "api_key_"._request('api');
+		if(!_request($obligatoire)){
+			$erreurs[$obligatoire] = _T('info_obligatoire');
+		}
+	}
+	
+	// S'il n'y a pas d'erreur on va chercher l'ancienne valeur de l'API pour voir si elle a changé
+	if (empty($erreurs)){
+		include_spip('inc/config');
+		$ancienne_api = lire_config('gis/api');
+		// On la garde en mémoire dans le hit pour une utilisation plus loin
+		set_request('ancienne_api', $ancienne_api);
+	}
+	
+	return $erreurs;
+}
+
+function gis_configurer_gis_post_traitement($flux){
+	// Si on est dans le formulaire de config de GIS et qu'on change d'API
+	if (
+		$flux['args']['form'] == 'configurer_gis'
+		and _request('api') != _request('ancienne_api')
+	){
+		// On invalide le cache du site
+		include_spip('inc/invalideur');
+		suivre_invalideur('1');
+		
+		// Et on recharge entièrement la page
+		$flux['data']['redirect'] = self();
+	}
+	
+	return $flux;
+}
+
+?>
---
+++ gis/trunk/lang/paquet-gis_fr.php
@@ -0,0 +1,11 @@
+<?php
+
+// Sécurité
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+'gis_slogan' => 'Système d\'information géographique',
+'gis_description' => 'Fournit la structure et les interfaces pour créer, gérer et afficher des points géolocalisées sur des cartes. Ces dernières peuvent être récupérées depuis plusieurs fournisseurs grâce à la librairie Mapstraction.',
+);
+
+?>
