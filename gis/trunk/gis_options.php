395682698b8b8231e419de7d72dfda79dc6c2883
brunobergot@gmail.com <brunobergot@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-09-14 16:03:23 +0000

    On se branche sur notre propre version de la lib Mapstraction pour ne pas avoir à attendre leur release pour chaque modif mineure. La lib est générée sur le serveur de mediaspip à partir de la branche suivante : https://github.com/brunob/mxn/tree/gis
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@51408 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- gis/trunk/gis_options.php
+++ gis/trunk/gis_options.php
@@ -1,6 +1,6 @@
 <?php
 
-define('_DIR_LIB_GIS',_DIR_RACINE.'lib/mxn-2.0.17/');
+define('_DIR_LIB_GIS',_DIR_RACINE.'lib/mxn-gis-2.0.9/');
 
 $table_des_traitements['VILLE'][]= 'typo(extraire_multi(%s))';
 $table_des_traitements['PAYS'][]= 'typo(extraire_multi(%s))';
