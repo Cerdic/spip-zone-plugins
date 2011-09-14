6f59d9d8f70ac688044dbb365a3d635c441dab3a
toutati@free.fr <toutati@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-09-14 19:06:07 +0000

    On ajoute les fonctions de mortimer avec le coefficient pour ne pas faire de gros mots
    Et comme flash ne prend pas les variables on les rajoute dans le fichier xml
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@51417 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- cumulus/cumulus_fonctions.php
+++ cumulus/cumulus_fonctions.php
@@ -2,7 +2,7 @@
 
 //juste pour E dans l'O
 function oeuf($str){
-	$pattern = "(Î|Ï|&#338;)";
+	$pattern = "(Å’|Å“|&#338;)";
 
 if($str!="") {
 $str= preg_replace( "$pattern" , "Oe" , $str);
@@ -16,4 +16,47 @@ function no_diese($txt) {
         return str_replace('#', '', $txt);
 }
 
+
+/*
+ *   +----------------------------------+
+ *    Nom des Filtres :    noop, filtre_max, coef et repeat
+ *   +----------------------------------+
+ *    Date : 23 Mars 2005
+ *    Auteur :  Pierre Andrews (mortimer.pa@free.fr)
+ *   +-------------------------------------+
+ *    Fonctions de ces filtres : ces filtres permettent
+ *   de faire un affichage variant en fonction de l'importance
+ *    de l'objet.  Vois la contrib pour plus d'informations.
+ *   +-------------------------------------+
+ *  
+ * Pour toute suggestion, remarque, proposition d'ajout
+ * reportez-vous au forum de l'article :
+ * http://www.spip_contrib.net/article.php3?id_article=879
+*/
+
+function noop($texte) {
+  return '';
+}
+
+function filtre_max($texte, $id='tout') {
+  static $max = array();
+  if($max[$id] < $texte) {
+    $max[$id] = $texte;
+  }
+  return $max[$id];
+}
+
+function coef($max,$nbr,$nbrMax=6) {
+  return 1+($nbr/$max*$nbrMax);
+}
+
+function repeat($nombre,$texte,$avant,$apres,$min = 0) {
+  if($nombre > $min) {
+    for($i=0;$i < $nombre;$i++) {
+      $texte = $avant.$texte.$apres;
+    }
+    return $texte;
+  } else
+    return '';
+}
 ?>
