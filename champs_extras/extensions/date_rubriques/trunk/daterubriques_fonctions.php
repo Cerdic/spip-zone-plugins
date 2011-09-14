c48baf0f58bb541ceae7c2ffd3d33602ae2a5681
yffic@lefourneau.com <yffic@lefourneau.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-09-14 14:06:15 +0000

    Pas trouvé pourquoi mais ça plante (fonction lire_config non existante) lors d'une demande de prévisualisation même avec cfg et Bonux actifs. Donc on vérifie que la fonction existe bien avant de l'appeler
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@51405 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- champs_extras/extensions/date_rubriques/trunk/daterubriques_fonctions.php
+++ champs_extras/extensions/date_rubriques/trunk/daterubriques_fonctions.php
@@ -1,5 +1,10 @@
 <?php
 	include_spip('inc/cextras_autoriser');
 	include_spip('inc/config');
-	restreindre_extras('rubrique', 'date_utile', lire_config('daterubriques/secteurs',0), 'secteur');
+	/* Pas trouvé pourquoi mais ca plante (fonction lire_config non existante) lors d'une demande de 
+		previsualisation meme avec cfg et Bonux actifs.
+		Donc on verifie que la fonction existe bien */
+	if (function_exists('lire_config')) {
+		restreindre_extras('rubrique', 'date_utile', lire_config('daterubriques/secteurs',0), 'secteur');
+	}
 ?>
