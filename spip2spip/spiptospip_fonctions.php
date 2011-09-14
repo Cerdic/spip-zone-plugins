5c870e93aaa7f75eb85c28c8580af9e1194f8922
erational@erational.org <erational@erational.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-09-14 06:19:33 +0000

    utilison <bouton> et en route pour SPIP3
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@51393 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spip2spip/spiptospip_fonctions.php
+++ spip2spip/spiptospip_fonctions.php
@@ -4,20 +4,6 @@
 // Fonctions
 //---------------------------------------
 
-// ajout bouton 
-function spip2spip_ajouterBoutons($boutons_admin) {
-		// si on est admin
-		if ($GLOBALS['connect_statut'] == "0minirezo" && $GLOBALS["connect_toutes_rubriques"]) {
-		  // on voit le bouton dans la barre "naviguer"
-		  $boutons_admin['naviguer']->sousmenu['spip2spip']= 
-      new Bouton(_DIR_PLUGIN_SPIP2SPIP."/img_pack/icon.png", _T("spiptospip:titre"));
-		}
-		return $boutons_admin;
-}
-// FIXME migrer ds la nlle ecriture: 
-//http://programmer.spip.org/ajouter_boutons 
-
-
 //
 // verifie s'il s'agit du bon format de backend
 // a terme peut etre utile pour recuperer le numero de version
