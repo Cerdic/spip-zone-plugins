d620aa49335069673ce06eb876cd2909726a89ff
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-09-15 19:43:51 +0000

    langues (contacts)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@51458 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ contacts_et_organisations/trunk/lang/contacts_sk.php
@@ -0,0 +1,140 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'aucun_contact' => 'Žiaden kontakt tu nie je!',
+	'aucune_organisation' => 'Žiadna organizácia tu nie je!',
+
+	// B
+	'bouton_contacts' => 'Kontakty',
+	'bouton_contacts_organisations' => 'Osobné kontakty a organizácie',
+	'bouton_organisations' => 'Organizácie',
+	'bouton_rechercher' => 'Vyhľadať',
+	'bouton_repertoire' => 'Adresár',
+
+	// C
+	'changer' => 'Zmeniť',
+	'chercher_contact' => 'Vyhľadať',
+	'chercher_organisation' => 'Vyhľadať',
+	'chercher_statut' => 'Stav',
+	'confirmer_delier_contact' => 'Určite chcete zrušiť prepojenie tejto organizácie s týmto kontaktom?',
+	'confirmer_delier_organisation' => 'Určite chcete zrušiť prepojenie tohto kontaktu s touto organizáciou?',
+	'confirmer_delier_organisation_rubrique' => 'Určite chcete zrušiť prepojenie tejto organizácie s touto rubrikou?',
+	'confirmer_supprimer_contact' => 'Určite chcete zmazať všetky údaje o tomto kontakte?',
+	'confirmer_supprimer_organisation' => 'Určite chcete zmazať všetky údaje o tejto organizácii?',
+	'contact' => 'Jeden kontakt',
+	'contact_associe_a_auteur_numero' => 'Prepojiť s autorom číslo',
+	'contact_associer_a_auteur' => 'Prepojiť s autorom',
+	'contact_creer' => 'Vytvoriť kontakt',
+	'contact_editer' => 'Upraviť tento kontakt',
+	'contact_modifier' => 'Upraviť kontakt',
+	'contact_nouveau' => 'Nový kontakt',
+	'contact_numero' => 'Kontakt číslo',
+	'contact_voir' => 'Zobraziť',
+	'contacts' => 'Kontakty',
+	'contacts_nb' => '@nb@ kontaktov',
+	'contacts_un' => 'Kontakt',
+	'creer_auteur_contact' => 'Vytvoriť nového autora a prepojiť ho s týmto kontaktom',
+	'creer_auteur_organisation' => 'Vytvoriť nového autora a prepojiť ho s touto organizáciou',
+
+	// D
+	'definir_auteur_comme_contact' => 'Nastaviť ako kontakt',
+	'definir_auteur_comme_organisation' => 'Definovať ako organizáciu',
+	'delier_cet_auteur' => 'Zrušiť prepojenie',
+
+	// E
+	'est_un_contact' => 'Tento autor je nastavený ako kontakt',
+	'est_une_organisation' => 'Tento autor je nastavený ako organizácia',
+	'explication_activite' => 'Činnosť organizácie: mimovládna organizácia, vzdelávanie, publikovanie a pod.',
+	'explication_contacts_ou_organisations' => 'Môžete tohto autora nastaviť ako kontakt alebo ako organizáciu.  Ďalšie polia potom môžete vyplniť na stránke Upraviť autora.',
+	'explication_identification' => 'Identifikácia organizácie: napr. DIČ.',
+	'explication_statut_juridique' => 'spoločnosť, organizácia, združenie a pod.',
+	'explication_supprimer_contact' => 'Vymazaním tohto kontaktu vymažete všetky ďalšie údaje, ktoré boli vyplnené na stránke autora.',
+	'explication_supprimer_organisation' => 'Odstránením tejto organizácie odstránite všetky ďalšie údaje, ktoré boli vyplnené na autorskej stránke.',
+	'explications_page_contacts' => 'Stránka sa rekonštruuje.<br /><br />Plánované akcie:<ul>
+<li>zobraziť všetky kontakty,</li><li>zmeniť autorov v kontaktoch,</li><li>nahrať kontakty,</li><li>li>...</li></ul><br />Ďakujeme vám za podnety na  <a href="http://www.spip-contrib.net/Plugin-Contacts-Organisations#pagination_comments-list">diskuskom fóre;-)</ a>',
+	'explications_page_organisations' => 'Stránka sa rekonštruuje. <br /> <br /> Plánované akcie: 
+ <ul>  <li> zobraziť všetky organizácie, </ li> upraviť autorov v organizáciách, </ li> nahrať organizácie, </ li> ...</ li> </ ul> <br / > Ďakujeme vám za podnety na <a href="http://www.spip-contrib.net/Plugin-Contacts-Organisations#pagination_comments-list"> diskusnom fóre;-) </ a>',
+
+	// I
+	'info_contacts_organisation' => 'Kontakty organizácie',
+	'info_nb_contacts' => 'Prepojené kontakty',
+	'info_organisation_appartenance' => 'Organizácia',
+	'info_organisations_appartenance' => 'Členstvo v organizáciách',
+	'info_organisations_filles' => 'Podradené organizácie',
+	'info_organisations_meres' => 'Nadradené organizácie',
+	'info_tous_contacts' => 'Všetky kontakty',
+	'info_toutes_organisations' => 'Všetky organizácie',
+	'infos_contacts_ou_organisations' => 'Kontakty a organizácie',
+
+	// L
+	'label_activite' => 'Aktivita',
+	'label_civilite' => 'Pohlavie',
+	'label_date_creation' => 'Dátum vytvorenia',
+	'label_date_naissance' => 'Dátum narodenia',
+	'label_descriptif' => 'Popis',
+	'label_email' => 'E-mail',
+	'label_fonction' => 'Funkcia',
+	'label_identification' => 'Identifikácia',
+	'label_nom' => 'Meno',
+	'label_nom_organisation' => 'Organizácia',
+	'label_organisation' => 'Prepojená organizácia',
+	'label_organisation_parente' => 'Nadradená organizácia',
+	'label_prenom' => 'Krstné meno',
+	'label_prenom_nom' => 'Meno a priezvisko',
+	'label_pseudo' => 'Prezývka',
+	'label_recherche_auteurs' => 'Vyhľadať v autoroch',
+	'label_recherche_contacts' => 'Vyhľadať v kontaktoch',
+	'label_recherche_organisations' => 'Vyhľadať v organizáciách',
+	'label_statut_juridique' => 'Právne postavenie',
+	'label_telephone' => 'Telefón',
+	'label_type_liaison' => 'Odkaz',
+	'lier_ce_contact' => 'Pripojiť tento kontakt',
+	'lier_cet_auteur' => 'Odkaz',
+	'lier_cette_organisation' => 'Pripojiť k tejto organizácii',
+	'lier_contact' => 'Pripojiť kontakt',
+	'lier_organisation' => 'Pripojiť k organizácii',
+	'liste_contacts' => 'Zoznam všetkých kontaktov v databáze',
+	'liste_organisations' => 'Zoznam všetkých organizácií v databáze',
+
+	// N
+	'nb_contact' => '1 kontakt',
+	'nb_contacts' => '@nb@ kontaktov',
+
+	// O
+	'organisation_associe_a_auteur_numero' => 'Prepojené s autorom číslo',
+	'organisation_associer_a_auteur' => 'Prepojiť s autorom',
+	'organisation_creer' => 'Zapísať organizáciu',
+	'organisation_creer_fille' => 'Zapísať podradenú organizáciu',
+	'organisation_editer' => 'Upraviť túto organizáciu',
+	'organisation_modifier' => 'Upraviť organizáciu',
+	'organisation_nouveau' => 'Nová organizácia',
+	'organisation_numero' => 'Organizácia číslo',
+	'organisation_un' => 'Jedna organizácia',
+	'organisation_voir' => 'Zobraziť',
+	'organisations' => 'Organizácie',
+	'organisations_nb' => '@nb@ organizácií',
+
+	// R
+	'recherche_de' => 'Vyhľadať "@recherche@"',
+	'rechercher' => 'Vyhľadať',
+
+	// S
+	'supprimer_contact' => 'Odstrániť tento kontakt',
+	'supprimer_organisation' => 'Odstrániť túto organizáciu',
+
+	// T
+	'titre_contact' => 'Kontaktné údaje',
+	'titre_organisation' => 'Informácie o organizácii',
+	'titre_page_contacts' => 'Riadenie kontaktov',
+	'titre_page_organisations' => 'Riadenie organizácií',
+	'titre_page_repertoire' => 'Adresár'
+);
+
+?>
