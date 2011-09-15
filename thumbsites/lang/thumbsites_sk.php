eb1a138f41f0661975906fc64b0211d855057af0
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-09-15 19:45:06 +0000

    langues (thumbsites)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@51499 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ thumbsites/lang/thumbsites_sk.php
@@ -0,0 +1,55 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'bouton_effacer' => 'Odstrániť',
+	'bouton_rafraichir' => 'Obnoviť miniatúru',
+	'bouton_valider' => 'Potvrdiť',
+	'bulle_bouton_effacer' => 'Odstrániť vstupy a vrátiť sa k pôvodným hodnotám',
+	'bulle_bouton_valider' => 'Potvrdiť vstupy',
+
+	// C
+	'cfg_descriptif' => 'This page is sued for configuring the Thumbsites plugin, and in particular, to select the thumbnails server. To find out more, please refer to the <a href="http://www.spip-contrib.net/?article2584">documentation on contrib</a>', # NEW
+	'cfg_inf_apercite_parametres' => 'Prosím, zadajte svoje osobné údaje:',
+	'cfg_inf_apercite_presentation' => '<a href="http://www.apercite.fr">apercite.fr</ a> ponúka bezplatnú službu, ktorá si nevyžaduje identifikáciu.',
+	'cfg_inf_girafa_description' => 'The thumbnails generated will have a maximum size of 160 x 120 pixels (these values can be modified on the Girafa site)', # NEW
+	'cfg_inf_girafa_parametres' => 'Prosím, zadajte svoje osobné údaje:',
+	'cfg_inf_girafa_presentation' => 'To use this service you must be subscribed on the <a href="http://www.girafa.com">Girafa</a> site. There is a free version if you need less than 2000 screen captures per day.', # NEW
+	'cfg_inf_miwin_parametres' => 'Prosím, zadajte svoje osobné údaje:',
+	'cfg_inf_miwin_presentation' => 'To use this service, you must include a link to the <a href="http://thumbs.miwim.fr">Miwin</a> site. The continued existence of that link is periodically checked by a script.', # NEW
+	'cfg_inf_rotothumb_parametres' => 'Prosím, zadajte svoje osobné údaje:',
+	'cfg_inf_rotothumb_presentation' => 'To use this service, you do not have to subscribe but you do have to create a link to the <a href="http://www.robothumb.com">Robothumb</a> site. The continued existence of that link is periodically checked by their servers.', # NEW
+	'cfg_inf_thumbshots_de_description' => 'Vygenerované miniatúry budú mať 120 x 90 pixelov.',
+	'cfg_inf_thumbshots_de_presentation' => '<a href="http://www.thumbshots.de">Thumbshots.de</a> offers a service equivalent to Thumbshots.com. Even though the services are identical, they are actually 2 distinct sites.', # NEW
+	'cfg_inf_thumbshots_description' => 'Vygenerované miniatúry budú mať 120 x 90 pixelov.',
+	'cfg_inf_thumbshots_presentation' => '<a href="http://www.thumbshots.com">Thumbshots.com</a> ponúka bezplatnú službu, ktorá si nevyžaduje registráciu.',
+	'cfg_inf_websnapr_parametres' => 'Prosím, zadajte svoje osobné údaje:',
+	'cfg_inf_websnapr_presentation' => 'To use this service, you must subscribe on the <a href="http://www.websnapr.com">Websnapr</a> site. The free version enables the processing of 250,000 site screen captures per month at a maximum speed of 80 captures per hour.', # NEW
+	'cfg_itm_serveur_apercite' => 'Apercite.fr',
+	'cfg_itm_serveur_girafa' => 'Girafa',
+	'cfg_itm_serveur_miwin' => 'Miwin',
+	'cfg_itm_serveur_robothumb' => 'Robothumb',
+	'cfg_itm_serveur_thumbshots' => 'Thumbshots.com',
+	'cfg_itm_serveur_thumbshots_de' => 'Thumbshots.de',
+	'cfg_itm_serveur_websnapr' => 'Websnapr',
+	'cfg_itm_taille_vignette' => '@taille@ pixelov',
+	'cfg_lbl_choix_serveur' => 'Vyberte server miniatúr',
+	'cfg_lbl_cle' => 'Váš kľúč',
+	'cfg_lbl_duree_cache' => 'Trvanie cache (v dňoch)',
+	'cfg_lbl_identifiant' => 'Váš identifikátor',
+	'cfg_lbl_signature' => 'Váš podpis',
+	'cfg_lbl_taille_vignette' => 'Veľkosť miniatúr',
+	'cfg_lgd_cache' => 'Cache',
+	'cfg_lgd_choix_serveur' => 'Server',
+
+	// T
+	'titre_thumbshot_site' => 'MINIATÚRA STRÁNKY'
+);
+
+?>
