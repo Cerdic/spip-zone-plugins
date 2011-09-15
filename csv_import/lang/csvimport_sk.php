e989ef934e83d9cf381ce1c06531f7eaea915c69
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-09-15 19:43:57 +0000

    langues (csvimport)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@51463 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ csv_import/lang/csvimport_sk.php
@@ -0,0 +1,84 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'administrer_tables' => 'Riadiť tabuľky',
+	'ajouter_donnees' => 'Pridať údaje',
+	'ajouter_table' => 'Pridať do tabuľky',
+	'aucune_donnee' => 'V súbore nie sú žiadne údaje.',
+	'aucune_table_declaree' => 'Na nahrávanie do CSV nie je deklarovaná žiadna tabuľka',
+	'avertissement_ajout' => 'Údaje v súbore CSV budú pridané do tabuľky "@table@", ako je to opísané nižšie.',
+	'avertissement_remplacement' => 'Súčasťou tejto operácie bude odstránenie všetkých existujúcich údajov v tabuľke. Údaje zo súboru budú vložené, ako je to uvedené nižšie:',
+
+	// C
+	'caracteres_separation' => 'Znak oddeľovača',
+	'champs_csv' => 'Polia v súbore CSV',
+	'champs_table' => 'Polia v tabuľke',
+	'confirmation_ajout_base' => 'Le CSV a été correctement ajouté à la base !', # NEW
+	'correspondance_incomplete' => 'Súvzťažnosti v tabuľkách CSV sú neúplné.',
+	'correspondance_indefinie' => 'Súvzťažnosti v tabuľkách CSV nie sú definované.',
+	'csvimport' => 'Nahrávanie CSV',
+
+	// D
+	'delimiteur_indefini' => 'Oddeľovač nie je definovaný',
+	'description_table_introuvable' => 'Popis tabuľky sa nedá nájsť',
+
+	// E
+	'erreurs_ajout_base' => 'Pri vkladaní do databázy vzniklo @nb@ chýb.',
+	'etape' => '(&Eacute;tape @step@ sur 3)', # NEW
+	'export_classique' => 'štadardný CSV (,)',
+	'export_excel' => 'CSV Excelu (;)',
+	'export_format' => 'Formát súboru:',
+	'export_table' => 'Export tabuľky: @table@',
+	'export_tabulation' => 'CSV oddelený tabulátorom',
+	'exporter' => 'Exportovať',
+	'extrait_CSV_importe' => 'Extrait du fichier CSV importé : ', # NEW
+	'extrait_table' => 'Extrait de la table "@nom_table@" : ', # NEW
+
+	// F
+	'fichier_absent' => 'Súbor chýba',
+	'fichier_choisir' => 'Súbor CSV, ktorý sa má nahrať',
+	'fichier_vide' => 'Prázdny súbor',
+
+	// I
+	'import_csv' => 'Nahrať CSV: @table@',
+	'import_export_tables' => 'Nahrávanie/export tabuliek',
+
+	// L
+	'ligne_entete' => '1. riadok ako hlavičku',
+	'lignes_table' => 'V tabuľke "@table@" je @nb_resultats@.',
+	'lignes_totales' => '@nb@ riadkov spolu.',
+	'lignes_totales_csv' => '@nb@ riadkov v súbore CSV spolu.',
+
+	// N
+	'nb_enregistrements' => '@nb@ uložených záznamov',
+	'noms_colonnes_CSV' => 'Noms des colonnes (attendus) du fichier CSV à importer : ', # NEW
+
+	// P
+	'pas_importer' => 'Nenahrávať',
+	'premieres_lignes' => '@nb@ prvých riadkov v súbore.',
+	'previsualisation_CSV_integre' => 'Prévisualisation des @nb@ premières lignes du fichier CSV intégrées à la table : ', # NEW
+	'probleme_chargement_fichier' => 'Problém pri spúšťaní súboru',
+	'probleme_chargement_fichier_erreur' => 'Problém pri spúšťaní súboru (chyba @erreur@).',
+	'probleme_inextricable' => 'Nerozoznaná chyba.',
+
+	// R
+	'remplacer_toute_table' => 'Nahradiť celú tabuľku',
+
+	// T
+	'table_vide' => 'Tabuľka "@table@" je prázdna.',
+	'tables_declarees' => 'Tabuľky deklarované',
+	'tables_presentes' => 'Tabuľky existujúce v databáze',
+	'tout_remplacer' => 'Nahradiť všetko',
+
+	// Z
+	'z' => 'zzz'
+);
+
+?>
