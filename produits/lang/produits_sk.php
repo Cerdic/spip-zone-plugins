90aee382b85127b9aa3ad90001e4cb7e37487ba4
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-09-15 19:44:41 +0000

    langues (produits)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@51488 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ produits/lang/produits_sk.php
@@ -0,0 +1,38 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'configurer_taxe_defaut_label' => 'Predvolená daň',
+	'configurer_titre' => 'Nastavenia výrobkov',
+
+	// I
+	'info_1_produit' => 'Jeden výrobok',
+	'info_aucun_produit' => 'Žiaden výrobok',
+	'info_nb_produits' => '@nb@ výrobkov',
+
+	// P
+	'produit_bouton_ajouter' => 'Pridať výrobok',
+	'produit_champ_descriptif_label' => 'Krátky popis',
+	'produit_champ_prix_ht_label' => 'Cena bez dane',
+	'produit_champ_reference_label' => 'Referencie',
+	'produit_champ_rubrique_label' => 'Rubrika',
+	'produit_champ_taxe_explication' => 'Predvolená hodnota na tejto stránke: @taxe@. Toto pole môžete nechať prázdne.',
+	'produit_champ_taxe_label' => 'Daň',
+	'produit_champ_texte_label' => 'Text',
+	'produit_champ_titre_label' => 'Nadpis',
+	'produit_editer' => 'Upraviť tento výrobok',
+	'produit_modifier' => 'Upraviť výrobok:',
+	'produit_nouveau' => 'Nový výrobok',
+	'produit_numero' => 'Výrobok č.',
+	'produit_reference' => 'Referencia "@reference@"',
+	'produit_statut' => 'Upraviť stav:',
+	'produits_titre' => 'Výrobky'
+);
+
+?>
