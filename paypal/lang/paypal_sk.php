ae860eb150924201af1dfa27aba02dc688f70111
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-09-15 19:44:35 +0000

    langues (paypal)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@51484 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ paypal/lang/paypal_sk.php
@@ -0,0 +1,50 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'api_prod' => 'Výroba API',
+	'api_test' => 'Test API',
+
+	// C
+	'configuration_paypal' => 'Paypal',
+
+	// D
+	'description_paypal' => 'Nastaviť internetový platobný systém Paypal',
+
+	// E
+	'explication_account' => 'Zhoduje sa s e-mailom predávajúceho',
+	'explication_environnement' => 'Do tohto formulára
+ môžete zadať údaje, ktoré sa týkajú testovacej stránky Paypalu (Pieskovisko, Vývoj),
+  ako aj údaje, ktoré platia len pre skutočné transakcie.
+  Ktoré prostredie chcete použiť?',
+	'explication_identifiant_vendeur' => 'Zhoduje sa s e-mailom predávajúceho',
+	'explication_soumission' => 'Na akú stránku Paypalu sa posiela formulár?',
+	'explication_tax' => 'Prípadne hodnotu dane, ktorá sa má použiť (napríklad francúzska DPH: 19,60)',
+
+	// I
+	'info_bouton_paypal' => 'Plaťte cez PayPal: je rýchly, bezplatný a bezpečný!',
+
+	// L
+	'label_account' => 'Účet (e-mail)',
+	'label_currency_code' => 'Mena',
+	'label_currency_code_eur' => 'Euro (€)',
+	'label_currency_code_usd' => 'Americký dolár (
+)',
+	'label_environnement' => 'Prostredie',
+	'label_environnement_prod' => 'Výrobné prostredie',
+	'label_environnement_test' => 'Otestovať prostredie',
+	'label_identifiant_vendeur' => 'ID predávajúceho',
+	'label_password' => 'Heslo API (heslo)',
+	'label_signature' => 'Podpis',
+	'label_soumission' => 'Prihlasovací formulár',
+	'label_tax' => 'Dane',
+	'label_username' => 'Používateľské meno API'
+);
+
+?>
