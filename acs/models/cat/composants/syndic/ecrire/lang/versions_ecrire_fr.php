3b4c94ed31b9e6164c49be14db5b5b2c0b504bcc
webmaster@geomaticien.com <webmaster@geomaticien.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-09-16 17:06:12 +0000

    Suppression du dossier "models"
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@51533 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- acs/models/cat/composants/syndic/ecrire/lang/versions_ecrire_fr.php
+++
@@ -1,9 +0,0 @@
-<?php // This is a SPIP-ACS language file  --  Ceci est un fichier langue de SPIP-ACS
-
-$GLOBALS[$GLOBALS['idx_lang']] = array(
-
-'nom' => 'Versions',
-'description' => 'Versions d\'un article.',
-'help' => 'Affiche les r&eacute;visions successives d\'un article.'
-);
-?>
--- acs/models/cat/composants/tags/tags_balises.php
+++
@@ -1,7 +0,0 @@
-<?php
-if ($GLOBALS['meta']['acsTagsAnime'] == 'oui') {
-  function tags_jslib() {
-  	return array('composants/tags/javascript/tags_anim.js');
-  }
-}
-?>
