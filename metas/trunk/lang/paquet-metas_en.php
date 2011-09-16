85a6b8085f4d10b078502b98a4b7f9824a5e8b0e
ben.spip@gmail.com <ben.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-09-16 16:26:37 +0000

    plugin.xml en paquet.xml
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@51531 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ metas/trunk/lang/paquet-metas_en.php
@@ -0,0 +1,12 @@
+<?php
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// M
+    'metas_titre' => 'Meta',
+    'metas_description' => 'This plugin makes possible to create and edit the META data for articles and rubriques of SPIP site.',
+	'metas_slogan' => 'Edit META title,keywords and description'
+);
+?>
