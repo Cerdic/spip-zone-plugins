29738a21e9cb9f159fb7455e383ea32a7c0185a9
ben.spip@gmail.com <ben.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-09-16 16:33:26 +0000

    un ptit formulaire pour éditer les metas, pas encore fonctionnel, mais sauvegardons, on ne sait jamais
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@51532 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ metas/trunk/formulaires/editer_metas.php
@@ -0,0 +1,45 @@
+<?php
+
+/***************************************************************************\
+ *  SPIP, Systeme de publication pour l'internet                           *
+ *                                                                         *
+ *  Copyright (c) 2001-2011                                                *
+ *  Arnaud Martin, Antoine Pitrou, Philippe Riviere, Emmanuel Saint-James  *
+ *                                                                         *
+ *  Ce programme est un logiciel libre distribue sous licence GNU/GPL.     *
+ *  Pour plus de details voir le fichier COPYING.txt ou l'aide en ligne.   *
+\***************************************************************************/
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+include_spip('inc/actions');
+include_spip('inc/editer');
+
+/**
+ * @param string $id_meta
+ * @return
+ */
+function formulaires_editer_metas_charger_dist($id_meta='new'){
+	$valeurs ;
+	return $valeurs;
+}
+
+/**
+ * @return
+ */
+
+function formulaires_editer_metas_verifier_dist(){
+
+	$erreurs ;
+	return $erreurs;
+}
+/**
+ * @return array
+ */
+function formulaires_editer_metas_traiter_dist(){
+	$res = array();
+	return $res;
+}
+
+
+?>
