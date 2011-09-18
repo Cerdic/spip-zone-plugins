cb1f61c753b709da3f7203dbfc138fd756d9b8a7
marcimat@rezo.net <marcimat@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-09-18 13:13:46 +0000

    prepa trunk/branches
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@51581 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- seminaire/lang/paquet_seminaire.php
+++
@@ -1,6 +0,0 @@
-<?php
-$GLOBALS[$GLOBALS['idx_lang']] = array(	
-	'seminaire_slogan'			=>		'Gérer vos séminaires',
-	'seminaire_description'	=>		'Surcouche au plugin Agenda2  pour  gérer les séminaires et autres événements d\'un laboratoire de recherche.',
-);
-?>
