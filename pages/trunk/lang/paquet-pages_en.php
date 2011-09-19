7341b5ba42dcfaf3b05d75422f748e41740eece9
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-09-19 12:13:18 +0000

    un peu de refraichissement pour SPIP3 : paquet.xml etc..
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@51615 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ pages/trunk/lang/paquet-pages_en.php
@@ -0,0 +1,13 @@
+<?php
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+'pages_slogan' => 'Unlinked pages',
+'pages_description' => 'This plugin allows you to create pages of articles that are not linked to any particular hierarchy.
+However they may be associated with a name template.
+This allows the creation of pages of legal information, about, contact, etc..
+',
+
+);
+
+?>
---
+++ pages/trunk/lang/paquet-pages_fr.php
@@ -0,0 +1,12 @@
+<?php
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+'pages_slogan' => 'Des pages sans rubrique',
+'pages_description' => 'Ce plugin permet de créer des pages d\'articles qui ne sont reliées à aucune hiérarchie particulière.
+En revanche elles peuvent être associées à un nom de squelette.
+Cela permet notamment de créer des pages de notice légale, d\'à-propos, de contact, etc.',
+
+);
+
+?>
