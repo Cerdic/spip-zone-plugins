0a1684bb88de2607eeca827d9b7844e7920219a5
cyril@cym.fr <cyril@cym.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-09-21 18:07:01 +0000

    on remet les balises prenom_auteur et civilite_auteur qui avaient sauté durant la mise à jour (mais qui étaient restées dans le trunk);
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@51677 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- contacts_et_organisations/branches/v1/contacts_fonctions.php
+++ contacts_et_organisations/branches/v1/contacts_fonctions.php
@@ -51,6 +51,44 @@ function balise_ORGANISATIONS_dist($p) {
 	return $p;
 }
 
+// Balise #PRENOM_AUTEUR
+// a modifier pour les appeler "dist"
+function trouve_prenom($id_auteur) {
+	
+	$prenom = sql_getfetsel("prenom","spip_contacts LEFT JOIN spip_contacts_liens ON (spip_contacts.id_contact=spip_contacts_liens.id_contact AND objet='auteur')", "id_objet=" . intval($id_auteur));
+	
+	if (!empty($prenom))
+		return $prenom;
+
+	return '';
+}
+function balise_PRENOM_AUTEUR($p) {
+	$id_auteur = champ_sql('id_auteur', $p);
+	$p->code = "trouve_prenom(".$id_auteur.")";
+	$p->statut = 'php';
+	return $p;
+}
+
+// Balise #CIVILITE_AUTEUR
+// a modifier pour les appeler "dist"
+function trouve_civilite($id_auteur) {
+	
+	$civilite = sql_getfetsel("civilite","spip_contacts LEFT JOIN spip_contacts_liens ON (spip_contacts.id_contact=spip_contacts_liens.id_contact AND objet='auteur')", "id_objet=" . intval($id_auteur));
+	
+	if (!empty($civilite))
+		return $civilite;
+
+	return '';
+}
+function balise_CIVILITE_AUTEUR($p) {
+	$id_auteur = champ_sql('id_auteur', $p);
+	$p->code = "trouve_civilite(".$id_auteur.")";
+	$p->statut = 'php';
+	return $p;
+}
+
+
+
 // Voir en ligne, ou apercu, ou rien (renvoie tout le bloc)
 function voir_contact_en_ligne_contact_organisation($type, $id){
 
