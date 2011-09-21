cec462f1e75978eb5bc7e58777dbd4b08a319443
ben.spip@gmail.com <ben.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-09-21 14:09:53 +0000

    Voilà ça prends forme ... les metas sont sauvegardées pour les articles et les rubriques
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@51655 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- metas/trunk/metas_pipelines.php
+++ metas/trunk/metas_pipelines.php
@@ -9,27 +9,14 @@
 
 function metas_affiche_milieu($flux)
 {
-    spip_log(__LINE__ . "metas_affiche_milieu", "ben." . _LOG_ERREUR);
     $en_cours = trouver_objet_exec($flux['args']['exec']);
 
-    //debug
-    ob_start();
-    var_export($flux['args']);
-    var_export($en_cours);
-    $en_cours_debug = ob_get_contents();
-    ob_end_clean();
-    spip_log(__LINE__ . " dollar flux + dollar en_vours:" . $en_cours_debug, "ben." . _LOG_ERREUR);
-
-    spip_log(__LINE__ . " edition:" . $en_cours['edition'], "ben." . _LOG_ERREUR);
-    spip_log(__LINE__ . " type:" . $en_cours['type'], "ben." . _LOG_ERREUR);
-    spip_log(__LINE__ . " id_table_objet:" . $en_cours['id_table_objet'], "ben." . _LOG_ERREUR);
-    spip_log(__LINE__ . " id_article:" . $flux['args']['id_article'], "ben." . _LOG_ERREUR);
+    // Mode edition, affichage du formulaire.
     if ($en_cours['edition'] == true // page visu
         AND $type = $en_cours['type']
             AND $id_table_objet = $en_cours['id_table_objet']
                 AND ($id = intval($flux['args'][$id_table_objet]))
     ) {
-        spip_log(__LINE__, "ben." . _LOG_ERREUR);
         $texte = recuperer_fond(
             'prive/squelettes/inclure/editer_metas',
             array(
@@ -38,15 +25,14 @@ function metas_affiche_milieu($flux)
                  'id_objet' => $id
             )
         );
-
-        spip_log(__LINE__." texte:". $texte, "ben." . _LOG_ERREUR);
-
-
-        $flux['data'] .= $texte;
-
-
-        spip_log(__LINE__ , "ben." . _LOG_ERREUR);
+        // on affiche le texte des metas au niveau du commentaire affiche_milieu (et pas en fin de page)
+        if ($p = strpos($flux['data'], "<!--affiche_milieu-->"))
+            $flux['data'] = substr_replace($flux['data'], $texte, $p, 0);
+        else
+            $flux['data'] .= $texte;
     }
+
+
     return $flux;
 }
 
