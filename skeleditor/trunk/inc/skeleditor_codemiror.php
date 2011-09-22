62cb215d4184549cc0dd15e55b6ddf0a9cd1c537
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-09-22 16:38:12 +0000

    externaliser le js d'init dans un fichier
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@51704 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- skeleditor/trunk/inc/skeleditor_codemiror.php
+++ skeleditor/trunk/inc/skeleditor_codemiror.php
@@ -59,16 +59,16 @@ function skeleditor_inline_inc($extension){
 	$dir = _DIR_PLUGIN_SKELEDITOR;
 	$css .= "<link rel='stylesheet' href='".$dir."codemirror/lib/codemirror.css' type='text/css' />\n"
 	  . "<link rel='stylesheet' href='".$dir."codemirror/theme/default.css' type='text/css' />\n"
-	  . "<link rel='stylesheet' href='".$dir."css/skeleditor.css' type='text/css' />";
+	  . "<link rel='stylesheet' href='".$dir."css/skeleditor.css' type='text/css' />\n";
 
-	$js .= "<script src='".$dir."codemirror/lib/codemirror.js' type='text/javascript'></script>";
+	$js .= "<script src='".$dir."codemirror/lib/codemirror.js' type='text/javascript'></script>\n";
 
 	foreach($mode as $m) {
 		$test = $dir."codemirror/mode/$m/$m";
 		if (file_exists($f=$test.".css"))
-			$css .= "<link rel='stylesheet' href='$f' type='text/css' />";
+			$css .= "<link rel='stylesheet' href='$f' type='text/css' />\n";
 		if (file_exists($f=$test.".js"))
-			$js .= "<script src='$f' type='text/javascript'></script>";
+			$js .= "<script src='$f' type='text/javascript'></script>\n";
 	}
 
 	return array($css,$js);
@@ -80,20 +80,20 @@ function skeleditor_inline_inc($extension){
  * @param string $extension
  * @return string
  */
-function determine_mime_type($extension) {
+function skeleditor_codemirror_determine_mode($extension) {
 	$mode = "";
-  $mime_types = array(
+  $modes = array(
 		'txt' => 'text/plain',
 		'htm' => 'text/html',
 		'html' => 'text/html',
 		'php' => 'application/x-httpd-php',
 		'css' => 'text/css',
-		//'js' => 'application/javascript', codemirror2 ne doit pas avoir de mode définit pour les js
+		'js' => 'javascript', //codemirror2 ne doit pas avoir de mode définit pour les js
 		'json' => 'application/json',
 		'xml' => 'application/xml',
 	);
-	if (array_key_exists($extension, $mime_types)) {
-		$mode = 'mode:"'.$mime_types[$extension].'",';
+	if (array_key_exists($extension, $modes)) {
+		$mode = $modes[$extension];
 	}
 	return $mode;
 }
@@ -113,53 +113,12 @@ function skeleditor_codemirror($filename,$editable=true){
 	list($css,$js) = skeleditor_inline_inc($infos['extension']);
 
 	// readOnly: jQuery("#code").attr("readonly"),
-	$mode = determine_mime_type($infos['extension']);
+	$mode = skeleditor_codemirror_determine_mode($infos['extension']);
+	$script =
+    '<script type="text/javascript">var cm_mode="'.$mode.'";</script>'
+		. $css
+		. $js
+		. "<script src='".find_in_path("javascript/codemirror_init.js")."' type='text/javascript'></script>\n";
 
-$script =
-	$css
-	. $js
-	. '<script type="text/javascript">
-var editor = CodeMirror.fromTextArea(document.getElementById(\'code\'), {
-        lineNumbers: true,
-        matchBrackets: true,
-        indentUnit: 6,
-        indentWithTabs: true,
-        enterMode: "keep",
-				'.$mode.'
-        tabMode: "shift",
-		});
-var lastPos = null, lastQuery = null, marked = [];
-
-function unmark() {
-  for (var i = 0; i < marked.length; ++i) marked[i]();
-  marked.length = 0;
-}
-
-function search() {
-  unmark();
-  var text = document.getElementById("query").value;
-  if (!text) return;
-  for (var cursor = editor.getSearchCursor(text); cursor.findNext();)
-    marked.push(editor.markText(cursor.from(), cursor.to(), "searched"));
-
-  if (lastQuery != text) lastPos = null;
-  var cursor = editor.getSearchCursor(text, lastPos || editor.getCursor());
-  if (!cursor.findNext()) {
-    cursor = editor.getSearchCursor(text);
-    if (!cursor.findNext()) return;
-  }
-  editor.setSelection(cursor.from(), cursor.to());
-  lastQuery = text; lastPos = cursor.to();
-}
-
-function replace() {
-  unmark();
-  var text = document.getElementById("query").value,
-      replace = document.getElementById("replace").value;
-  if (!text) return;
-  for (var cursor = editor.getSearchCursor(text); cursor.findNext();)
-    editor.replaceRange(replace, cursor.from(), cursor.to());
-}
-</script>';
 	return $script;
 }
