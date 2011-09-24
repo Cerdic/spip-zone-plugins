a4728819619b76c9cd37a38feff4dbb005af8653
gildas.cotomale@gmail.com <gildas.cotomale@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-09-24 17:50:10 +0000

    Extension de la table des membres par ChampsExtras : il manquait le fichier d'interface
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@51772 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ Association/Associaspip/inc/champs_extras.php
@@ -0,0 +1,23 @@
+<?php
+/***************************************************************************
+ *  Associaspip, extension de SPIP pour gestion d'associations             *
+ *                                                                         *
+ *  Copyright (c) 2007 Bernard Blazin & Francois de Montlivault (V1)       *
+ *  Copyright (c) 2010-2011 Emmanuel Saint-James & Jeannot Lapin (V2)       *
+ *                                                                         *
+ *  Ce programme est un logiciel libre distribue sous licence GNU/GPL.     *
+ *  Pour plus de details voir le fichier COPYING.txt ou l'aide en ligne.   *
+\***************************************************************************/
+
+/**
+ * Interface avec le plugin "Champs Extras 2" : ajout des objets 
+ * membres
+ * a la liste des objets pouvant recevoir des champs extras... 
+**/
+function association_objets_extensibles($objets){
+	return array_merge($objets, array(
+		'membres' => _T('asso:membres'),
+		
+	));
+}
+?>
