a8027782067e1234cd7343262addc7959d5db12a
pierrekuhn82@gmail.com <pierrekuhn82@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-09-28 18:45:55 +0000

    Evitons des logs intenpestif.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@51993 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- Amap/amap_autoriser.php
+++ Amap/amap_autoriser.php
@@ -1,6 +1,9 @@
 <?php
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
+// fonction pour le pipeline, n'a rien a effectuer
+function amap_autoriser(){}
+
 function autoriser_amap_bouton_dist($faire, $type, $id, $qui, $opt) {
 	return ($qui['statut'] == '0minirezo');
 }
