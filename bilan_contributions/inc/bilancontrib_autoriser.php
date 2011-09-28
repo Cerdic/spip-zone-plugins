b68ba697dae5fa7c7fd448f574aa16319b183bf1
yffic@lefourneau.com <yffic@lefourneau.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-09-28 10:18:10 +0000

    Gérer les autorisations
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@51968 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- bilan_contributions/inc/bilancontrib_autoriser.php
+++ bilan_contributions/inc/bilancontrib_autoriser.php
@@ -4,11 +4,17 @@
 function bilancontrib_autoriser(){}
 
 /**
- * Autorisation a voir les evenements prives
+ * Autorisation a afficher le bilan
  */
 function autoriser_bilancontrib_afficher_dist($faire,$quoi,$id,$qui,$opts){
-	return ($qui['statut'] == '0minirezo')
+	return ($qui['webmestre'] == 'oui') ;
 }
 
+function autoriser_bilancontrib21_bouton_dist($faire,$quoi,$id,$qui,$options) {
+	return autoriser('afficher','bilancontrib',$id,$qui,$options);
+}
+function autoriser_bilancontrib_bouton_dist($faire,$quoi,$id,$qui,$options) {
+	return autoriser('afficher','bilancontrib',$id,$qui,$options);
+}
 
 ?>
