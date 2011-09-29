504593707f7f6c7ffde66e09db1f3b63e82bd5ec
cyril@cym.fr <cyril@cym.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-09-29 15:10:30 +0000

    Correction sur la déclaration d'un serveur d'aide spécifique; la déclaration précédente générait une erreur : Fatal error: [] operator not supported for strings in sjcycle2/sjcycle_options.php on line 3;
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@52041 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- sjcycle2/sjcycle_options.php
+++ sjcycle2/sjcycle_options.php
@@ -1,3 +1,4 @@
 <?php
-$GLOBALS['help_server'][] = url_de_base(1) . str_replace("../", "", _DIR_PLUGIN_SJCYCLE) . "aide/";
+// URL supplémentaire où trouver de l'aide en ligne
+$GLOBALS['help_server'] = url_de_base(1) . str_replace("../", "", _DIR_PLUGIN_SJCYCLE) . "aide/";
 ?>
