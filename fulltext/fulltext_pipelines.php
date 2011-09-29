15ce5275697b7187f661c827492d83d3f27e7971
gilles.vincent@gmail.com <gilles.vincent@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-09-29 19:15:29 +0000

    Avec cette ecriture, ['fulltext_index_document'] vallait true et pas 600
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@52053 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- fulltext/fulltext_pipelines.php
+++ fulltext/fulltext_pipelines.php
@@ -6,7 +6,11 @@
 function fulltext_taches_generales_cron($taches_generales) {
 	$fulltext = sql_fetsel('valeur', 'spip_meta', 'nom = "fulltext"');
 	$fulltext = unserialize($fulltext['valeur']);
-	$taches_generales['fulltext_index_document'] = $fulltext['intervalle_cron'] ? $fulltext['intervalle_cron'] : @define('_FULLTEXT_INTERVALLE_CRON',600); // toutes les 10 minutes
+	if ($fulltext['intervalle_cron']) {$taches_generales['fulltext_index_document'] = $fulltext['intervalle_cron'];
+	} else {
+		@define('_FULLTEXT_INTERVALLE_CRON',600); // toutes les 10 minutes
+		$taches_generales['fulltext_index_document'] = _FULLTEXT_INTERVALLE_CRON;
+	}
 	return $taches_generales;
 }
 
