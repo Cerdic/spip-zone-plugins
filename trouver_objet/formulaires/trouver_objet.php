424e7cba46c62cc5aa9311295cf348bc45636b21
toutati@free.fr <toutati@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-10-03 12:40:25 +0000

    Avant de passer en stable , le plugin est mis en test, mais tout fonctionne bien,
    On peut donc chercher un objet via un zoli selecteur et en + maintenant donner la page de retour que l'on veut, 
    sinon le formulaire redirige sur la page de l'objet trouvé
    
    cf demo /?page=trouver_objet
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@52368 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- trouver_objet/formulaires/trouver_objet.php
+++ trouver_objet/formulaires/trouver_objet.php
@@ -8,7 +8,7 @@
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 // chargement des valeurs par defaut des champs du formulaire
-function formulaires_trouver_objet_charger($objet, $source, $id_source, $identifiant,$paramselecteur=''){
+function formulaires_trouver_objet_charger($objet, $source, $id_source, $identifiant,$paramselecteur='',$retour){
  // on considere objet au singulier + s
 	return 
 		array(
@@ -23,31 +23,36 @@ function formulaires_trouver_objet_charger($objet, $source, $id_source, $identif
 		);
 }
 
-function formulaires_trouver_objet_verifier($objet, $source, $id_source, $identifiant,$paramselecteur=''){
+function formulaires_trouver_objet_verifier($objet, $source, $id_source, $identifiant,$paramselecteur='',$retour){
 	// si pas d'id, le selecteur generique n'a pas fonctionne
 	// on fait comment alors ??
 	$id_koi ="id_".$objet;
 
 	if (!_request('pid_objet')) {
 		return array(
-		'message_erreur' => _T('grappes:pas_de_identifiant'),
+		'message_erreur' => _T('trouvobjet:pas_de_identifiant'),
 			);
 	}
 }
 
-function formulaires_trouver_objet_traiter($objet, $source, $id_source, $identifiant,$paramselecteur=''){
+function formulaires_trouver_objet_traiter($objet, $source, $id_source, $identifiant,$paramselecteur='',$retour){
 	
   // Empecher le traitement en AJAX car on sait que le formulaire va rediriger autre part
      refuser_traiter_formulaire_ajax();
      
      $id_objet = _request('pid_objet');
      $id_koi ="id_".$objet;
-   
+     if($retour) {
+     	     //$redirect=generer_url_public($retour,"$objet=$id_objet");
+                 include_spip('inc/headers');
+         	$redirect = redirige_par_entete(str_replace('&amp;','&', $retour."&$id_koi=$id_objet"));
+     }
+     else $redirect = generer_url_public($objet,"$id_koi=$id_objet");
     // Valeurs de retours
     return array(
     	'ajax' => ' ',
     	//'editable' => false,
-        'redirect'=> generer_url_public($objet,"$id_koi=$id_objet")
+        'redirect'=> $redirect
 
 	    );
 
---
+++ trouver_objet/lang/trouvobjet_fr.php
@@ -0,0 +1,19 @@
+<?php
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// I
+	'trouver_articles_avec_mot' => 'Rechercher des articles avec un mot-clef',
+	'bouton_chercher' => 'Chercher',
+	'bouton_trouver'=>'Trouver',
+	'choisir_groupe'=>'Choisissez un groupe de mots',
+	'choisir_rubrique'=>'Choisissez une rubrique',
+	'trouver_mot_in'=>'Rechercher un mot dans <strong>@titre@</strong>',
+	'trouver_article_in'=>'Rechercher un article dans <strong>@titre@</strong>',
+	'pas_de_identifiant'=>'Nom inconnu',
+
+
+);
+
+?>
