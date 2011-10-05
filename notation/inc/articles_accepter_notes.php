7693a08495f1727c18a3e72cfae7e4fca19d7e27
jluc@no-log.org <jluc@no-log.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-10-05 16:26:28 +0000

    Moins d'abondance de logs, mais plus de parlance
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@52525 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- notation/inc/articles_accepter_notes.php
+++ notation/inc/articles_accepter_notes.php
@@ -12,8 +12,8 @@ function inc_articles_accepter_notes_dist($id_objet) {
 	$art_accepter_note = sql_getfetsel('accepter_note', 'spip_articles', array(
 		"id_article = ". intval($id_objet)
 	));
-	spip_log($art_accepter_note,'notation');
-	if (in_array($art_accepter_note,array('oui','non'))) {
+	if ($art_accepter_note and in_array($art_accepter_note,array('oui','non'))) {
+		spip_log("l'article $id_objet accepte les notes : $art_accepter_note",'notation');
 		$accepter_note = $art_accepter_note;
 	}
 	return substr($accepter_note, 0, 3);
