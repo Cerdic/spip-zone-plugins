d7f395b6a1cf3cb1a71dc3a64a696611e8932d98
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-10-05 08:12:30 +0000

    Des reports de kent1 que je n'avais point fait.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@52466 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- gis/trunk/gis_pipelines.php
+++ gis/trunk/gis_pipelines.php
@@ -31,7 +31,7 @@ function gis_inserer_javascript($flux){
 		$flux .="\n".'<script type="text/javascript" src="http://api.maps.ovi.com/jsl.js"></script>'."\n";
 	if ($config['api'] == 'yandex')
 		$flux .="\n".'<script type="text/javascript" src="http://api-maps.yandex.ru/1.1/index.xml?key='.$config['api_key_yandex'].'"></script>'."\n";
-	
+		
 	// insertion de la lib mapstraction
 	if(in_array($config['api'],array('cartociudad','google','googlev3','yandex'))){
 		$geocoder = ($config['geocoder']) ? ',[geocoder]' : '';
