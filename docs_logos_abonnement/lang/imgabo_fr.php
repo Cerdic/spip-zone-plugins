9c22cab43ba5211ec677452a08133ab15cfc75cc
toutati@free.fr <toutati@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-10-05 18:16:44 +0000

    spip<3 et lang apparemment!
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@52562 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- docs_logos_abonnement/lang/imgabo_fr.php
+++ docs_logos_abonnement/lang/imgabo_fr.php
@@ -4,8 +4,8 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-  'logo_abonnement' => "Logo de cet abonnement",
-  'abonnement' => "Abonnement",
+  'logo_abonnement' => 'Logo de cette offre d\'abonnement',
+  'abonnement' => 'Abonnement',
 
 ); //
 ?>
