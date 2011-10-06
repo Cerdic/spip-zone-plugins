a951571c3fb3feaea4ff40a4fe0fc2e75cb49de0
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-10-06 16:50:14 +0000

    un paquet.xml pour gerer le nom traduit
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@52714 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ couteau-kiss/lang/paquet-ck_gl.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/_stable_/acces_restreint/lang/
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	'ck_nom' => 'A navalla KISS',
+	'ck_slogan' => 'Simplifier certains réglages SPIP',
+	'ck_description' => 'Simplicité, efficacité, légèreté.
+_ Un couteau qui tient vraiment dans la poche en
+une unique page de configuration pour les réglages cachés de SPIP.',
+
+);
+
+?>
