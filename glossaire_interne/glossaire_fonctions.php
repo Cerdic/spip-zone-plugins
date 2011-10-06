3303ed45333fa9a7f372ebaaec96f32c4a81400f
homere1974@yahoo.fr <homere1974@yahoo.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-10-06 08:43:08 +0000

    Diffs... de 2008.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@52649 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- glossaire_interne/glossaire_fonctions.php
+++ glossaire_interne/glossaire_fonctions.php
@@ -1,8 +1,8 @@
 <?php
 
 function glossaire_insert_head($flux){
-	$flux .='<link rel="stylesheet" href="plugins/glossaire_interne/glossaire.css" media="screen" type="text/css" />';
-	$flux .='<script type="text/javascript" src="plugins/glossaire_interne/glossaire.js"></script>';
+	$flux .="<link rel='stylesheet' href='".find_in_path('glossaire.css')."' type='text/css' media='all' />\n";
+	$flux .="<script type='text/javascript' src='".find_in_path('glossaire.js')."'></script>";
 	return $flux;
 }
 
@@ -22,11 +22,21 @@ function first_replace($c,$r,$t)
 function lier_au_glossaire($texte)
 {
 $r = spip_query("SELECT id_mot, titre FROM spip_mots WHERE type='Glossaire'");
-while($o = spip_fetch_array($r))
-		{
-		$texte = first_replace("$o[titre]","<a href=\"spip.php?mot".$o[id_mot]."\" class=\"affgloss\" title=\"Glossaire\" onclick=\"popupGloss('spip.php?page=mot_glossaire&id_mot=$o[id_mot]'); return false;\">$o[titre]</a>",$texte);
-		}
-	return $texte;
+
+	/*contrib de philippe auriol pour zapper les liens */
+	    $search= '@<a[^>]*?>.*?</a>@msi'; 	    preg_match_all ($search, $texte, $tagMatches);	    $replace = "#MaChaine#";
+ 	    $texte = preg_replace($search, $replace, $texte);
+
+		while($o = spip_fetch_array($r))
+			{
+			$texte = first_replace("$o[titre]","<a href=\"spip.php?mot".$o[id_mot]."\" class=\"affgloss\" title=\"Glossaire\" onclick=\"popupGloss('spip.php?page=mot_glossaire&amp;id_mot=$o[id_mot]'); return false;\">$o[titre]</a>",$texte);
+			}
+
+ 	    	for($i=0;$i<sizeof($tagMatches[0]);$i++)
+	            {
+ 	            $texte= first_replace ($replace,$tagMatches[0][$i], $texte);	            }
+
+		return $texte;
 }
 
 ?>
