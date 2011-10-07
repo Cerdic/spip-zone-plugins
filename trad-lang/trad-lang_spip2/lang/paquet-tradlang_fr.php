35cfb6e9ac0f45a136c1eb7a44f034d8445d68f7
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-10-07 15:30:04 +0000

    Améliorations qui trainaient ... on les commite même si on n'est pas foutu de les lister
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@52902 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ trad-lang/trad-lang_spip2/lang/paquet-tradlang_fr.php
@@ -0,0 +1,19 @@
+<?php
+
+/// @file
+/// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+/// 
+///  Fichier produit par PlugOnet
+/// Module: paquet-tradlang
+/// Langue: fr
+/// Date: 07-10-2011 16:03:20
+/// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+// T
+	'tradlang_description' => 'Un plugin pour gérer directement les fichiers langues depuis l\'interface privée.',
+	'tradlang_slogan' => 'Un plugin pour gérer directement les fichiers langues depuis l\'interface privée',
+);
+?>
