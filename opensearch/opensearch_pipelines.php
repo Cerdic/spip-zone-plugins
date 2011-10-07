bc86f3855d7f4b5039b460434ae6f94a59260cf0
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-10-07 09:07:55 +0000

    if (!defined("_ECRIRE_INC_VERSION")) return; sur tout fichier PHP pour sécurité future principalement
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@52796 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- opensearch/opensearch_pipelines.php
+++ opensearch/opensearch_pipelines.php
@@ -1,5 +1,7 @@
 <?php
 
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
 function opensearch_insert_head($flux){
 	$nom_site = textebrut(typo($GLOBALS['meta']['nom_site']));
 	$url_opensearch = url_absolue(generer_url_public('opensearch.xml'));
