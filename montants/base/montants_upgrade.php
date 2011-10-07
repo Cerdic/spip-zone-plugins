373aef10ea9bafad2a30a9f397c014282a6b3d25
toutati@free.fr <toutati@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-10-07 20:06:01 +0000

    Bug à l'install, pas encore trop compris ... ça devrait marcher là
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@52943 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- montants/base/montants_upgrade.php
+++ montants/base/montants_upgrade.php
@@ -3,9 +3,7 @@
 * Plugin montant
 *
 * Copyright (c) 2011
-* Anne-lise Martenot elastick.net / BoOz booz@rezo.net 
-* Ce programme est un logiciel libre distribue sous licence GNU/GPL.
-* Pour plus de details voir le fichier COPYING.txt.
+* Anne-lise Martenot elastick.net
 *  
 **/
 
@@ -16,25 +14,25 @@ include_spip('base/create');
 
 function montants_upgrade($nom_meta_base_version, $version_cible){
 
-	$current_version = 0.0;
+	$current_version = "0.0";
 	
 	if (isset($GLOBALS['meta'][$nom_meta_base_version]))
 		$current_version = $GLOBALS['meta'][$nom_meta_base_version];
-	
-	if (version_compare($current_version,"0.1","<=")){
-		sql_alter("TABLE spip_montants ADD taxe decimal(4,3) default null");
-		sql_alter("TABLE spip_montants ADD descriptif text NOT NULL");
-		ecrire_meta($nom_meta_base_version,$current_version=$version_cible);
-	}
-	
-	if (version_compare($current_version,"0.2","<=")){
-		sql_alter("TABLE spip_montants CHANGE le_parent le_parent tinytext NOT NULL");
-		ecrire_meta($nom_meta_base_version,$current_version=$version_cible);
-	}
-	
+
 	if ($current_version=="0.0") {
 		creer_base();
 		ecrire_meta($nom_meta_base_version, $current_version=$version_cible);
+	}
+
+	if (version_compare($current_version,"0.1","<")){
+		sql_alter("TABLE spip_montants ADD taxe decimal(4,3) default null");
+		sql_alter("TABLE spip_montants ADD descriptif text NOT NULL");
+		ecrire_meta($nom_meta_base_version,$current_version=0.1);
+	}
+	
+	if (version_compare($current_version,"0.2","<")){
+		sql_alter("TABLE spip_montants CHANGE le_parent le_parent tinytext NOT NULL");
+		ecrire_meta($nom_meta_base_version,$current_version=0.2);
 	}
 
 }
