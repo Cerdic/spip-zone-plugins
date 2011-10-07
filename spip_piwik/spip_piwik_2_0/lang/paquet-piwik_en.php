2621b218a93f4174ffe4a01bb4e745b9e61d4d71
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-10-07 14:44:25 +0000

    Devrait être fonctionnel sous SPIP 3.0 donc paquet.xml et tout le tintouin
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@52900 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ spip_piwik/spip_piwik_2_0/lang/paquet-piwik_en.php
@@ -0,0 +1,18 @@
+<?php
+
+/// @file
+/// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+/// 
+///  Fichier produit par PlugOnet
+/// Module: paquet-piwik
+/// Langue: en
+/// Date: 07-10-2011 16:03:20
+/// Items: 1
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+// P
+	'piwik_description' => 'Adds the Piwik statistics script (web traffic analyzer) on the pages of the site.',
+);
+?>
---
+++ spip_piwik/spip_piwik_2_0/lang/paquet-piwik_fr.php
@@ -0,0 +1,19 @@
+<?php
+
+/// @file
+/// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+/// 
+///  Fichier produit par PlugOnet
+/// Module: paquet-piwik
+/// Langue: fr
+/// Date: 07-10-2011 16:03:20
+/// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+// P
+	'piwik_description' => 'Ajoute le script de statistiques Piwik (analyseur de trafic web) sur les pages du site.',
+	'piwik_slogan' => 'Interfacer Spip et Piwik',
+);
+?>
