9c4e631ea3e53a60f869dd004f809fec042906b1
yffic@lefourneau.com <yffic@lefourneau.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-10-09 21:25:46 +0000

    Nom, slogan, description Pb de version, il est inférieur à celle pour spip 2.1
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@53134 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- tradrub/trunk/lang/paquet-tradrub_fr.php
+++ tradrub/trunk/lang/paquet-tradrub_fr.php
@@ -2,9 +2,9 @@
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-	#'tradrub_nom' => "TradRub",
-	'tradrub_slogan' => "Traduction entre rubriques",
-	'tradrub_description' => "Gestion de liens de traduction sur les rubriques.",
+	'tradrub_nom' => "Traduction entre rubriques",
+	'tradrub_slogan' => "Gestion de liens de traduction entre les rubriques",
+	'tradrub_description' => "Ce plugin rajoute un système inspiré par celui des traductions des articles : il ajoute un champ id_trad à la table rubriques et lie ainsi les traductions des rubriques.",
 );
 
 ?>
