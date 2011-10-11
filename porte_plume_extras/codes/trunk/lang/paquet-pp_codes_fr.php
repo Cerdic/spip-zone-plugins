81374bf398ca1bdf03fb6a3e552bd8aae77c7f39
yffic@lefourneau.com <yffic@lefourneau.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-10-11 20:02:26 +0000

    Pétouille dans la description
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@53333 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- porte_plume_extras/codes/trunk/lang/paquet-pp_codes_fr.php
+++ porte_plume_extras/codes/trunk/lang/paquet-pp_codes_fr.php
@@ -3,6 +3,6 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'pp_codes_nom' => "Codes Informatiques pour Porte Plume",
 	'pp_codes_slogan' => "Ajoute des boutons à la barre d'outils du Porte Plume pour gérer des codes informatiques",
 	'pp_codes_description' => "Gestion de boutons pour ins&#233;rer du code informatique via la barre d'&#233;dition Porte Plume.
-Ajoute aussi un raccourci &#91;&#63;ecrire/inc_versions.php#trac].",
+Ajoute aussi un raccourci <code>[->ecrire/inc_versions.php#trac]</code>.",
 );
 ?>
