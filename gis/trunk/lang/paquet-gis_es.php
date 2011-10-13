0011d7ffec430c9e14bd21598731c60f488db1d7
lesage.sylvain@laposte.net <lesage.sylvain@laposte.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-10-13 15:30:29 +0000

    Traduction de paquet-gis en espagnol
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@53394 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ gis/trunk/lang/paquet-gis_es.php
@@ -0,0 +1,11 @@
+<?php
+
+// Sécurité
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+'gis_slogan' => 'Sistema de información geográfica',
+'gis_description' => 'Provee la estructura y las interfaces para crear, manejar y visualizar puntos geolocalizados sobre mapas. Estás mapas puedes ser recuperadas desde varios proveedores gracias a la libreria Mapstraction.',
+);
+
+?>
