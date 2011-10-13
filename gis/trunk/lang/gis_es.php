c310db245c105a5af271f2615bcc34275f1234c4
lesage.sylvain@laposte.net <lesage.sylvain@laposte.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-10-13 15:48:54 +0000

    Traduction du fichier de langue en espagnol
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@53395 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ gis/trunk/lang/gis_es.php
@@ -0,0 +1,88 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/gis/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'aucun_gis' => 'Ning&uacute;n punto',
+	'aucun_objet' => 'Ning&uacute;n objeto',
+
+	// B
+	'bouton_lier' => 'Asociar este punto',
+	'bouton_supprimer_gis' => 'Suprimir este punto de manera definitiva',
+	'bouton_supprimer_lien' => 'Suprimir est&aacute; asociaci&oacute;n',
+
+	// C
+	'cfg_descr_gis' => 'Sistema de Informaci&oacute;n Geogr&aacute;fica.<br /><a href="http://www.spip-contrib.net/3887">Ir a la documentaci&oacute;n</a>.',
+	'cfg_inf_adresse' => 'Mostrar campos adicionales de direcci&oacute;n (pa&iacute;s, ciudad, regi&oacute;n, direcci&oacute;n...)',
+	'cfg_inf_cloudmade' => 'Est&aacute; API necesita una clave a generar en <a href=\'@url@\'>el sitio de CloudMade</a>.',
+	'cfg_inf_geocoder' => 'Activar las funciones del geocoder (b&uacute;squeda desde una direcci&oacute;n, recuperaci&oacute;n de la direcci&oacute;n a partir de las coordenadas).',
+	'cfg_inf_geolocaliser_user_html5' => 'Si el navegador del usuario lo permite, su ubicaci&oacute;n geogr&aacute;fica aproximada esta recuperada y ser&aacute; la posici&oacute;n por omisi&oacute;n cuando se crear&oacute; un nuevo punto.',
+	'cfg_inf_google' => 'Esta API necesita una clave a generar en <a href=\'@url@\'>el sitio de GoogleMaps</a>.',
+	'cfg_inf_yandex' => 'Esta API necesita una clave a generar en <a href=\'@url@\'>el sitio de Yandex</a>.',
+	'cfg_lbl_adresse' => 'Mostrar los campos de direcci&oacute;n',
+	'cfg_lbl_api' => 'API de cartogr&aacute;fia',
+	'cfg_lbl_api_cloudmade' => 'CloudMade',
+	'cfg_lbl_api_google' => 'Google Maps v2',
+	'cfg_lbl_api_googlev3' => 'Google Maps v3',
+	'cfg_lbl_api_key_cloudmade' => 'Clave CloudMade',
+	'cfg_lbl_api_key_google' => 'Clave GoogleMaps',
+	'cfg_lbl_api_key_yandex' => 'Clave Yandex',
+	'cfg_lbl_api_mapquest' => 'MapQuest',
+	'cfg_lbl_api_microsoft' => 'Microsoft Bing',
+	'cfg_lbl_api_openlayers' => 'OpenLayers',
+	'cfg_lbl_api_ovi' => 'Ovi Nokia',
+	'cfg_lbl_api_yandex' => 'Yandex',
+	'cfg_lbl_geocoder' => 'Geocoder',
+	'cfg_lbl_geolocaliser_user_html5' => 'Centrar el mapa sobre la ubicaci&oacute;n del usuario a la creaci&oacute;n',
+	'cfg_titre_gis' => 'GIS',
+
+	// E
+	'editer_gis_editer' => 'Modificar este punto',
+	'editer_gis_explication' => 'Est&aacute; p&aacute;gina lista todos los puntos geolocalizados del sitio.',
+	'editer_gis_nouveau' => 'Crear un nuevo punto',
+	'editer_gis_titre' => 'Los puntos geolocalizados',
+	'erreur_recherche_pas_resultats' => 'Ning&uacute;n punto corresponde a la b&uacute;squeda.',
+	'explication_api_forcee' => 'La API esta impuesta por un otro plugin o esqueleto.',
+
+	// F
+	'formulaire_creer_gis' => 'Crear un punto geolocalizado:',
+	'formulaire_modifier_gis' => 'Modificar el punto geolocalizado:',
+
+	// I
+	'icone_gis_tous' => 'Puntos geolocalizados',
+	'info_id_objet' => 'N&deg;',
+	'info_liste_gis' => 'Puntos geolocalizados',
+	'info_liste_gis_objet' => 'Puntos asociados a este objeto',
+	'info_liste_objets_gis' => 'Objetos asociados a este punto',
+	'info_numero_gis' => 'Punto n&uacute;mero',
+	'info_objet' => 'Objeto',
+	'info_supprimer_lien' => 'Suprimir la asociaci&oacute;n',
+	'info_voir_fiche_objet' => 'Ver la ficha',
+
+	// L
+	'label_adress' => 'Direcci&oacute;n',
+	'label_code_postal' => 'C&oacute;digo postal',
+	'label_pays' => 'Pa&iacute;s',
+	'label_rechercher_address' => 'Buscar una direcci&oacute;n',
+	'label_rechercher_point' => 'Buscar un punto',
+	'label_region' => 'Regi&oacute;n',
+	'label_ville' => 'Ciudad',
+	'lat' => 'Latitud',
+	'libelle_logo_gis' => 'LOGOTIPO DEL PUNTO',
+	'lon' => 'Longitud',
+
+	// T
+	'titre_bloc_creer_point' => 'Asociar un nuevo punto',
+	'titre_bloc_points_lies' => 'Puntos asociados',
+	'titre_bloc_rechercher_point' => 'Buscar un punto',
+	'titre_nombre_utilisation' => 'Una utilizaci&oacute;n',
+	'titre_nombre_utilisations' => '@nb@ utilizaciones',
+
+	// Z
+	'zoom' => 'Zoom'
+);
+
+?>
