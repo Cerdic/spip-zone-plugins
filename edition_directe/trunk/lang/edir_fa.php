235ba3e926374f283cb9c0f296ede364c579757a
davoodhossein@yahoo.com <davoodhossein@yahoo.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-10-15 21:26:21 +0000

    langues (edir)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@53508 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ edition_directe/trunk/lang/edir_fa.php
@@ -0,0 +1,37 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'activer_edition_directe' => 'فعال سازي ويرايش مستقيم براي: ',
+	'article' => 'مقاله‌ها',
+	'auteur' => 'نويسندگان',
+
+	// B
+	'breve' => 'خبرها',
+
+	// G
+	'groupe_mots' => 'گروه‌واژه‌ها',
+
+	// M
+	'mot' => 'واژه‌ها',
+
+	// R
+	'rubrique' => 'بخش‌ها',
+
+	// S
+	'site' => 'سايت‌ها',
+
+	// T
+	'titre_plugin' => 'ويرايش مستقيم',
+
+	// V
+	'vider_cache' => 'لطفاً حافظه‌ي دم‌دستي را خالي كنيد'
+);
+
+?>
