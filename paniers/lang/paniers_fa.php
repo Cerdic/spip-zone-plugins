4aec35192c0f983bfa9e38ba028fd43ba9ecad27
davoodhossein@yahoo.com <davoodhossein@yahoo.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-10-15 21:27:00 +0000

    langues (paniers)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@53516 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ paniers/lang/paniers_fa.php
@@ -0,0 +1,34 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'action_ajouter' => 'افزودن به سبد',
+
+	// M
+	'mon_panier' => 'سبد من',
+	'mon_panier_modifier' => 'تعديل سبد من',
+	'mon_panier_voir' => 'سبدم را ببين',
+
+	// P
+	'panier_description' => 'شرح',
+	'panier_erreur_quantites' => 'مقدارها بايد عددهاي صحيح باشد.',
+	'panier_montant' => 'مقدار',
+	'panier_prix_unitaire' => 'في واحد',
+	'panier_quantite' => 'كميت',
+	'panier_quantite_ok' => 'كميت‌ها تغيير كرده',
+	'panier_recalculer' => 'تجديد محاسبه',
+	'panier_taxes' => 'با ماليات‌ها',
+	'panier_total_ht' => 'كل تخفيف مالياتي',
+	'panier_total_ttc' => 'كل',
+	'panier_valider' => 'تأييد سبدم',
+	'panier_vide' => 'سبد خالي است.',
+	'paniers' => 'سبدهاي خريد'
+);
+
+?>
