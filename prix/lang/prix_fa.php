c9672a51e166580dcf343a9ea364821dae070ac9
davoodhossein@yahoo.com <davoodhossein@yahoo.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-10-15 21:27:21 +0000

    langues (prix)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@53518 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ prix/lang/prix_fa.php
@@ -0,0 +1,22 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// L
+	'label_prix_ht' => 'Prix HT',
+	'label_prix_ttc' => 'Prix TTC',
+	'label_taxes' => 'ماليات‌ها',
+	'label_total_ht' => 'كلHT',
+	'label_total_ttc' => 'كل ماليات گرفته شده (TTC)',
+
+	// P
+	'prix_ht' => '@prix@ HT',
+	'prix_ttc' => '@prix@ TTC'
+);
+
+?>
