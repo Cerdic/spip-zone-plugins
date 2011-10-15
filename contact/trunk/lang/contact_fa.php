9f096cdd224a2d3cebaa4ae946f5662847cb3a97
davoodhossein@yahoo.com <davoodhossein@yahoo.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-10-15 21:26:17 +0000

    langues (contact)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@53504 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ contact/trunk/lang/contact_fa.php
@@ -0,0 +1,134 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'alerte_effacement' => 'تأييد حذف داده‌هاي پيكربندي تماس',
+	'aucun_message' => 'پيامي براي نمايش موجود نيست',
+
+	// B
+	'bouton_effacer' => 'پاك‌ كردن',
+
+	// C
+	'champ_adresse' => 'نشاني',
+	'champ_civi_explication' => 'تنظيمات پيشرفته براي ميدان عنوان ',
+	'champ_civi_mademoiselle_ok' => 'گنجاندن گزينه‌ي «دوشيزه» در ميدان عنوان ',
+	'champ_civilite' => 'نزاكت ',
+	'champ_code_postal' => 'زيپ كد',
+	'champ_etat' => 'كشور',
+	'champ_impose' => 'ميدان وضع شده',
+	'champ_infolettre' => 'خبرنامه',
+	'champ_mail' => 'نامه(ميل)',
+	'champ_nom' => 'اسم',
+	'champ_obligatoire' => 'الزامي؟',
+	'champ_organisation' => 'سازمان',
+	'champ_pays' => 'كشور',
+	'champ_portable' => 'شماره همراه',
+	'champ_prenom' => 'اسم كوچك',
+	'champ_sujet' => 'موضوع',
+	'champ_telephone' => 'شماره تلفن',
+	'champ_texte' => 'متن',
+	'champ_ville' => 'شهر',
+	'configuration_barre_forum' => 'نمايش ميله‌ي حروف‌ نگاري در سخنگاه‌ها',
+	'configuration_barre_impossible' => 'پلاگين ديگري استفاده از پلاگين قلم را مسدود كرده است تنظيم‌هاي زير هيچ تأثيري نخواهد داشت. ',
+	'configuration_barre_oui' => 'نمايش ميله‌ي كامل حروف‌ نگاري.',
+	'configuration_champs_message' => 'تنظيم‌هاي «قلم‌‌دان» (porte-plume)',
+	'configuration_champs_message_explication' => 'تنظيم ميله‌ي حروف‌ نگاري، مربوط به متن پيام.',
+	'configuration_description' => 'پيكربندي فرم تماس',
+	'configuration_destinataires' => 'دريافت‌ كنندگان',
+	'configuration_destinataires_explication' => 'انتخاب يك يا چند نويسنده‌ كه بازديدكنندگان ممكن است بخواهند با آنان تماس بگيرند.اگر انتخاب نشود، از نخستين نويسنده (كسي كه وب‌ سايت را نصب كرده)استفاده خواهد شد.',
+	'configuration_destinataires_plusieurs' => 'مجوز به بازديدكنندگان براي انتخاب <b>چند </b> گيرنده از ميان نويسندگان منتخب زير ',
+	'configuration_destinataires_plusieurs_et' => 'دادن اجازه‌ي انتخاب از ميان چند دريافت‌ كننده از ميان فهرست منتخب بالا و افزودن كساني از اين گروه نويسندگان كه به فرم اضافه شده‌اند.  ',
+	'configuration_destinataires_plusieurs_ou' => 'دادن اجازه‌ي انتخاب چند دريافت كننده از ميان فهرست منتخب بالا يا از ميان نويسندگاني كه به فرم اضافه شده اند. ',
+	'configuration_destinataires_tous' => 'تمام</b> نويسندگان ايميل‌ها را دريافت كنند', # b
+	'configuration_destinataires_tous_et' => '<b>تمام</b> نويسندگان ايميل‌ها را دريافت مي‌كنند و به فرم اضافه مي‌شوند. ',
+	'configuration_destinataires_tous_ou' => ' <b>تمام</b> نويسندگان ايميل‌ها را دريافت كنند يا فقط كساني كه به اين فرم اضافه شده‌اند دريافت كنند. ',
+	'configuration_destinataires_un' => 'دادن اجازه به بازديدكنندگان براي انتخاب <b> يك نويسنده</b> از ميان فهرست زير به عنوان دريافت كننده ',
+	'configuration_destinataires_un_et' => '... زياد شده توسط نويسندگان انتقالي از فرم‌ها.',
+	'configuration_destinataires_un_ou' => '...يا در ميان گروه نويسندگان انتقالي از فرم‌ها',
+	'configuration_intro' => 'مقدمه',
+	'configuration_intro_explication' => 'متني را بنويسيد كه فرم تماس شما را معرفي كند. ',
+	'configuration_no_barre' => 'نشان ندادن ميل‌ابزار حروفنگاري.',
+	'configuration_no_previsualisation' => 'نمايش ميل‌ابزار حروفنگاري بدون پيش‌ نمايش.',
+	'configuration_pj' => 'پيوست‌ها',
+	'configuration_pj_autoriser' => 'مجوز افزودن پيوست',
+	'configuration_pj_facteur_absent' => 'براي استفاده از اين كاركرد، بايد پلاگين فاكتور (Facteur) را نصب كنيد.',
+	'configuration_pj_nb_max' => 'بيشترين تعداد پيوست‌ها',
+	'configuration_regle_liste_explication' => 'انتخاب يك قاعده‌ي محاسبه براي فهرست دريافت‌كنندگان بالقوه‌ي و انتخاب روش پيشنهادي به بازديدكنندگان.',
+	'configuration_texteinfolettre' => 'متن اطلاع‌نامه (infolettre)',
+	'configuration_texteinfolettre_explication' => 'اصلاح متن اطلاع‌نامه چك‌ باكس.',
+	'consulter_memoire' => 'اين پيام روي سايت شما ثبت مي‌شود: ',
+	'courriel_de' => 'از',
+	'courriel_pour' => 'براي',
+
+	// D
+	'deplacement_possible' => 'جا به جايي عمودي با بكش و بيانداز',
+	'description_menu_contact' => 'مدخل منيو به صفحه‌ي فرم تماس',
+	'description_page-contact' => 'دسترسي از طريق<i>spip.php?page=contact</i>,اين صفحه‌ي دلبخواهي به شما اجازه مي‌دهد يك صفحه‌ي فرم تماس براي سايت خود ايجاد كنيد (يادتان نرود كه يك پيوند به منيو‌هاي خود اضافه كنيد).  ',
+
+	// F
+	'form_destinataire' => 'گزينش يك گيرنده',
+	'form_destinataires' => 'گزينش گيرندگان',
+	'form_pj_ajouter_pluriel' => 'افزودن پيوند‌ها',
+	'form_pj_ajouter_singulier' => 'افزودن يك پيوست',
+	'form_pj_fichier_ajoute' => 'فايل افزوده شده : ',
+	'form_pj_importer' => 'واردسازي يك پرونده',
+	'form_pj_previsu_pluriel' => 'پيوست‌ها',
+	'form_pj_previsu_singulier' => 'پيوست',
+	'form_pj_supprimer' => 'حذف ',
+	'form_pj_titre' => 'تيتر پرونده',
+	'forum_attention_nbre_caracteres' => 'توجه!</b> پيام شما بايد دست كم @nbre_caract@  كاراكتر داشته باشد', # b
+
+	// H
+	'horodatage' => 'فرم به اينجا پست شد : @horodatage@.',
+
+	// I
+	'inforigine' => 'اطلاعات فرم:‌ ',
+	'informez_moi' => 'با دريافت اطلاعات با ايميل از سازمان شما موافقم. ',
+	'infos_collecte' => 'اطلاعات گردآمده',
+	'infos_supplementaires' => 'اطلاعات اضافه',
+
+	// M
+	'madame' => 'خانم',
+	'mademoiselle' => 'دوشيزه',
+	'modele_label_auteur' => 'پيش‌ گزينش يك نويسنده (دلبخواهي)؟',
+	'modele_nom_formulaire' => 'فرم تماس پيشرفته',
+	'monsieur' => 'آقا',
+	'msg_accueil' => 'پيام‌هاي دريافت شده از تماس.',
+	'msg_contact' => 'پيام تماس',
+	'msg_expediteur' => 'فرستنده:‌',
+	'msg_lus' => 'پيام خوانده شده',
+	'msg_messagerie' => 'پيك تماس',
+	'msg_nouveaux' => 'پيام‌هاي جديد',
+	'msg_pas_nouveaux' => 'پيام جديد نداريد.',
+	'msg_revenir_accueil' => 'بازگشت به ليست پيام‌هاي تماس.',
+	'msg_supprimer_message' => 'حذف اين پيام',
+
+	// N
+	'noisette_label_afficher_titre_noisette' => 'نمايش تيتر؟',
+	'noisette_label_niveau_titre' => 'انتخاب تيتر جديد',
+	'noisette_label_titre_noisette' => 'تيتر: ',
+	'noisette_label_titre_noisette_perso' => 'اگر تيتر شخصي‌ شده: ',
+	'noisette_titre_perso' => 'تيتر شخصي',
+
+	// P
+	'par' => 'به وسيله‌ي:',
+	'preselection' => 'انداختن تمام اقلام از ليست دريافت‌ كنندگان ',
+
+	// S
+	'sauv_message' => 'ثبت پيام‌هاي تماس',
+	'sauv_message_explication' => 'اجازه‌ي ثبت پيام‌هاي تماس‌ها و نمايش آن‌ها در ميانجي تعيين شده.',
+	'sauv_message_ok' => 'ثبت پيام‌هاي تماس‌ها',
+	'succes' => 'ممنون، پيام شما ارسال شد. <br /> در اسرع وقت پاسخ مي‌دهيم.<br /> گروه@equipe_site@ ',
+
+	// T
+	'titre' => 'فرم تماس',
+	'titre_court' => 'تماس'
+);
+
+?>
