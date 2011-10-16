01e6b02ace76ad289c2e36cebe3e2ab256a3496b
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-10-16 11:59:22 +0000

    version spip 2 et spip 2.1 only
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@53536 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ edition_directe/branches/2.0/lang/edir_de.php
@@ -0,0 +1,37 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'activer_edition_directe' => 'Direkt Edition für :',
+	'article' => 'Artikel',
+	'auteur' => 'Autoren',
+
+	// B
+	'breve' => 'Meldungen',
+
+	// G
+	'groupe_mots' => 'Schlagwortgruppe',
+
+	// M
+	'mot' => 'Schlagwort',
+
+	// R
+	'rubrique' => 'Rubriken',
+
+	// S
+	'site' => 'Websites',
+
+	// T
+	'titre_plugin' => 'Direkte Edition',
+
+	// V
+	'vider_cache' => 'Leeren Sie bitte die Sitecache'
+);
+
+?>
---
+++ edition_directe/branches/2.0/lang/edir_en.php
@@ -0,0 +1,37 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'activer_edition_directe' => 'Activate direct edition for :',
+	'article' => 'Articles',
+	'auteur' => 'Authors',
+
+	// B
+	'breve' => 'News item',
+
+	// G
+	'groupe_mots' => 'Keyword Groups',
+
+	// M
+	'mot' => 'Key word',
+
+	// R
+	'rubrique' => 'Sections',
+
+	// S
+	'site' => 'Sites',
+
+	// T
+	'titre_plugin' => 'Direct Edition',
+
+	// V
+	'vider_cache' => 'Please empty the cache'
+);
+
+?>
