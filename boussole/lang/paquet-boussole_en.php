a69de9c9ca42d92326a4b8b452983d821525e06c
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-10-18 13:18:35 +0000

    le paquet pour la boussole
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@53599 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ boussole/lang/paquet-boussole_en.php
@@ -0,0 +1,21 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+// Fichier produit par PlugOnet
+// Module: paquet-boussole
+// Langue: en
+// Date: 18-10-2011 15:14:24
+// Items: 3
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// B
+	'boussole_description' => 'Install this plugin on your site and you will have access, in your skeletons, to all the sites of the SPIP Galaxy using 
+models, noisettes or the Z page provided.
+_ For DIYers, you can also use this plugin to display your own list of sites and design your own displays.',
+	'boussole_nom' => 'Compass',
+	'boussole_slogan' => 'The favorite places of the SPIP galaxy or out there!',
+);
+?>
---
+++ boussole/lang/paquet-boussole_fr.php
@@ -0,0 +1,21 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+// Fichier produit par PlugOnet
+// Module: paquet-boussole
+// Langue: fr
+// Date: 18-10-2011 15:14:24
+// Items: 3
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// B
+	'boussole_description' => 'Installez ce plugin sur votre site et vous aurez accès, dans vos squelettes, à l\'ensemble des sites de la Galaxie SPIP en utilisant
+les modèles, les noisettes ou la page Z proposés.
+_ Pour les bricoleurs, vous pouvez aussi utiliser ce plugin pour afficher votre propre liste de sites et concevoir vos propres affichages.',
+	'boussole_nom' => 'Boussole',
+	'boussole_slogan' => 'Les bonnes adresses de la galaxie SPIP ou d\'ailleurs !',
+);
+?>
