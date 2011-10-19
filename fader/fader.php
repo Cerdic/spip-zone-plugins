d158c296ebcc2ae882c3f7c8c2fd8014066f4b8e
cvallois@yahoo.fr <cvallois@yahoo.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-10-19 18:19:44 +0000

    correction consécutive au copier/coller de la création du plugin
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@53641 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- fader/fader.php
+++ fader/fader.php
@@ -1,8 +1,8 @@
 <?php
-function igooglelike_insert_head($flux)
+function fader_insert_head($flux)
 {
 	$flux .= '<link type="text/css" rel="stylesheet" href="'.url_absolue(find_in_path('css/fader.css')).'" />
-			<script type="text/javascript" src="'.url_absolue(find_in_path('lib/fade-plugin.js')).'"></script>'	
+			<script type="text/javascript" src="'.url_absolue(find_in_path('lib/fader-plugin.js')).'"></script>'	
 			;
 
 return $flux;
