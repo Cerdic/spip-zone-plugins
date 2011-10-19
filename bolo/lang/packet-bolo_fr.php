a326f0c9a1052782c0cb8a018fc466a97c9ac411
cyril@cym.fr <cyril@cym.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-10-19 10:12:04 +0000

    tests de compatibilité SPIP 3.0 ok; ajout du fichier paquet.xml; 1er pas vers la génération de texte dans d'autres langues que le latin; déport de la traduction des champs langue de paguet vers le dossier lang.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@53629 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ bolo/lang/packet-bolo_fr.php
@@ -0,0 +1,18 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/_stable_/acces_restreint/lang/
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	'bolo_nom' => 'Bolo',
+	'bolo_slogan' => 'Outils pour cr&#233;er du faux texte',
+	'bolo_description' => 'Ce plugin permet d'ins&#233;rer tr&#232;s facilement du faux texte dans les articles ou dans les squelettes, en sp&#233;cifiant le nombre de caract&#232;res souhait&#233;s.
+		
+		On l'emploie soit directement dans les squelettes en utilisant le code <code>[(#BOLO|couper{800})]</code>, soit dans les champs #TEXTE des articles gr&acirc;ce au mod&#232;le <code>&lt;bolo800&gt;</code>.
+		
+		La version actuelle permet seulement d'ins&#233;rer du texte latin; une future version pourrait permettre d'ins&#233;rer du texte latin ou fran&ccedil;ais accentu&#233;.',
+
+);
+
+?>
