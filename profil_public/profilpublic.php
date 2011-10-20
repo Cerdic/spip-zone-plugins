495f7adc30778825150aed03e0548e750d3aa771
cvallois@yahoo.fr <cvallois@yahoo.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-10-20 15:17:07 +0000

    mise en conformité du nommage: suppression des dernières traces d"igoogle-like"
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@53683 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- profil_public/profilpublic.php
+++ profil_public/profilpublic.php
@@ -1,7 +1,7 @@
 <?php
 function profilpublic_insert_head($flux)
 {
-	$flux .= '<link type="text/css" rel="stylesheet" href="'.url_absolue(find_in_path('css/igoogle-like.css')).'" />
+	$flux .= '<link type="text/css" rel="stylesheet" href="'.url_absolue(find_in_path('css/profilpublic.css')).'" />
 				<script type="text/javascript" src="'.url_absolue(find_in_path('lib/jquery.cookie.js')).'"></script>
 				<script type="text/javascript" src="'.url_absolue(find_in_path('lib/igoogle-like.js')).'"></script>
 				<script type="text/javascript" src="'.url_absolue(find_in_path('lib/fade-plugin.js')).'"></script>'	
