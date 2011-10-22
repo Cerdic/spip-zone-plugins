d4d79c8a1a4033774cab465c5a454f31c848bb2f
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-10-22 19:40:23 +0000

    Améliorer la traduction et le nommage du plugin.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@53716 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- chess/lang/paquet-chess_en.php
+++ chess/lang/paquet-chess_en.php
@@ -1,8 +1,8 @@
 <?php
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-	'chess_titre' => '',
-	'chess_slogan' => 'Allow to Show Chess Games with pgn Files',
+	'chess_titre' => 'Chess',
+	'chess_slogan' => 'Allow to display chess games with pgn files',
 	'chess_description' => 'This plugin allows to read chess games on line with their pgn file.
   To insert the widget you have just to write in your article the tag   &#60;chessXX&#62; where XX is the number of the article, then upload the pgn files.',
 
