e84f7c69781a7ebbd2898cdfa673603fb092e91b
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-10-22 09:53:16 +0000

    Une en-tête de fichier erronée
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@53710 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- plugonet/inc/plugonet_traiter.php
+++ plugonet/inc/plugonet_traiter.php
@@ -1,14 +1,5 @@
 <?php
 
-/***************************************************************************\
- *  SPIP, Systeme de publication pour l'internet                           *
- *                                                                         *
- *  Copyright (c) 2001-2011                                                *
- *                                                                         *
- *  Ce programme est un logiciel libre distribue sous licence GNU/GPL.     *
- *  Pour plus de details voir le fichier COPYING.txt ou l'aide en ligne.   *
-\***************************************************************************/
-
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 include_spip('inc/plugin');
