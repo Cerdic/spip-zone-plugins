986f1dc45daa2adb116a38ab1b1ef1747c5d22bc
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-10-28 09:11:19 +0000

    fichier de langue en utf icone fatcow version 3.0 et non 2.0 car il existe deja une version 2.1 pour spip 2
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@53784 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- tradrub/trunk/lang/paquet-tradrub_fr.php
+++ tradrub/trunk/lang/paquet-tradrub_fr.php
@@ -4,7 +4,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'tradrub_nom' => "Traduction entre rubriques",
 	'tradrub_slogan' => "Gestion de liens de traduction entre les rubriques",
-	'tradrub_description' => "Ce plugin rajoute un système inspiré par celui des traductions des articles : il ajoute un champ id_trad à la table rubriques et lie ainsi les traductions des rubriques.",
+	'tradrub_description' => "Ce plugin rajoute un systÃ¨me inspirÃ© par celui des traductions des articles : il ajoute un champ id_trad Ã  la table rubriques et lie ainsi les traductions des rubriques.",
 );
 
 ?>
