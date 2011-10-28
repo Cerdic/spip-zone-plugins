36716e3ad5ad475b7028e7df0d192a1233e06aae
guy.cesaro@gmail.com <guy.cesaro@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-10-28 23:22:21 +0000

    Rester compatible avec agenda 2 >= 53773, qui declare generer_url_ecrire_evenement(merci Totof). Ca n'avait rien a faire dans ce plugin de toute facon, il aurait fallu le commit dans agenda2 et pas ailleurs, idiot que je suis. http://www.spip-contrib.net/Documents-et-logo-sur-les-evenements#forum451321
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@53823 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- docs_logos_agenda2/agenda_fonctions.php
+++ docs_logos_agenda2/agenda_fonctions.php
@@ -1,12 +1,13 @@
 <?php
 
-
-function generer_url_ecrire_evenement($id, $args='', $ancre='', $statut='', $connect='') {
-	$a = "id_evenement=" . intval($id);
-	$h = ($connect)
-	?  generer_url_entite_absolue($id, 'evenement', $args, $ancre, $connect)
-	: (generer_url_ecrire('evenements_edit', $a . ($args ? "&$args" : ''))
-		. ($ancre ? "#$ancre" : ''));
-	return $h;
+if (!function_exists('generer_url_ecrire_evenement')) {
+	function generer_url_ecrire_evenement($id, $args='', $ancre='', $statut='', $connect='') {
+		$a = "id_evenement=" . intval($id);
+		$h = ($connect)
+		?  generer_url_entite_absolue($id, 'evenement', $args, $ancre, $connect)
+		: (generer_url_ecrire('evenements_edit', $a . ($args ? "&$args" : ''))
+			. ($ancre ? "#$ancre" : ''));
+		return $h;
+	}
 }
 ?>
