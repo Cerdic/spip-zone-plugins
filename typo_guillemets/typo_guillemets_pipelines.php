79c2d1df49169962c4ac663f7226673191cf7c3b
romy@rezo.net <romy@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-10-30 00:48:58 +0000

    rangement
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@53846 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- typo_guillemets/typo_guillemets_pipelines.php
+++ typo_guillemets/typo_guillemets_pipelines.php
@@ -2,7 +2,7 @@
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 function typo_guillemets_insert_head_css($flux) {
-	$flux .= '<link rel="stylesheet" type="text/css" href="'.find_in_path('typo_guillemets.css').'" media="all" />'."\n";
+	$flux .= '<link rel="stylesheet" type="text/css" href="'.find_in_path('css/typo_guillemets.css').'" media="all" />'."\n";
 	return $flux;
 }
 
