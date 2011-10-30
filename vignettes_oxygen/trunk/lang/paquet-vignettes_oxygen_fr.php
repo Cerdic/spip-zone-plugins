664e7abb11a687f5b17c9dec377f80170a1f7f2e
joseph@larmarange.net <joseph@larmarange.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-10-30 17:39:02 +0000

    Lien incorrect
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@53873 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- vignettes_oxygen/trunk/lang/paquet-vignettes_oxygen_fr.php
+++ vignettes_oxygen/trunk/lang/paquet-vignettes_oxygen_fr.php
@@ -12,7 +12,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 // V
-	'vignettes_oxygen_description' => 'Ce plugin fournit un ensemble d\'icônes de documents en 128x128 pixels venant remplacer les icônes par défaut de SPIP. Cette collection est basées sur les icônes du projet [Oxygen->http://www.gnu.org/licenses/lgpl.html].',
+	'vignettes_oxygen_description' => 'Ce plugin fournit un ensemble d\'icônes de documents en 128x128 pixels venant remplacer les icônes par défaut de SPIP. Cette collection est basées sur les icônes du projet [Oxygen->http://www.softicons.com/free-icons/system-icons/oxygen-icons-by-oxygen].',
 	'vignettes_oxygen_slogan' => 'Collection d\'icônes pour les documents',
 );
 ?>
