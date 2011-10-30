b2b459bf40a8de3d8e23b741365f7dd0c06e4272
zonepro@free.fr <zonepro@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-10-30 19:46:32 +0000

    commit lang 2
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@53886 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ sel/lang/paquet-sel_en.php
@@ -0,0 +1,10 @@
+<?php
+     if (!defined('_ECRIRE_INC_VERSION')) return;
+     $GLOBALS[$GLOBALS['idx_lang']] = array(
+		'sel_nom' => "Lets (Local Exchange Trading Systems)",
+		'sel_slogan' => "Simply, manage your Lets",
+		'sel_description' => "		
+			English description
+		",
+     );
+?>
