b5a1ca4703603fe34d830b0ed83e6e713b4a846c
joseph@larmarange.net <joseph@larmarange.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-10-30 16:38:50 +0000

    Dépôt des vignettes oxygen
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@53862 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ vignettes_oxygen/README.txt
@@ -0,0 +1,5 @@
+Vignettes pour SPIP basées sur les icônes Oxygen (Sources : http://www.softicons.com/free-icons/system-icons/oxygen-icons-by-oxygen), sous licence GNU LGPL (http://www.gnu.org/licenses/lgpl.html)
+
+Utilisation de la police Liberation pour les extensions (http://en.wikipedia.org/wiki/Liberation_fonts).
+
+Les sources SVG sont disponibles sur SPIP-Zone : http://zone.spip.org/trac/spip-zone/browser/_graphismes_/vignettes/oxygen
---
+++ vignettes_oxygen/trunk/lang/paquet-vignettes_oxygen_fr.php
@@ -0,0 +1,18 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+// Fichier produit par PlugOnet
+// Module: paquet-vignettes_oxygen
+// Langue: fr
+// Date: 30-10-2011 16:22:44
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// V
+	'vignettes_oxygen_description' => 'Ce plugin fournit un ensemble d\'icônes de documents en 128x128 pixels venant remplacer les icônes par défaut de SPIP. Cette collection est basées sur les icônes du projet [Oxygen->http://www.gnu.org/licenses/lgpl.html].',
+	'vignettes_oxygen_slogan' => 'Collection d\'icônes pour les documents',
+);
+?>
