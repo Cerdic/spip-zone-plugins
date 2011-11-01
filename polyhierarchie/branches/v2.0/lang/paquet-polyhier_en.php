02a0075cafe9b35d1c00cf73d4f53b2c7e2acd15
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-11-01 19:51:17 +0000

    On ajoute la traduction du nom du plugin dans les fichiers de langue destinés au paquet.xml
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@53950 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- polyhierarchie/branches/v2.0/lang/paquet-polyhier_en.php
+++ polyhierarchie/branches/v2.0/lang/paquet-polyhier_en.php
@@ -5,6 +5,7 @@
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
+	'polyhier_nom' => 'PolyHierarchy',
 	'polyhier_slogan' => 'Attach an article or a section on several parent sections.',
 	'polyhier_description' => 'Plugin for creating cross-links by referencing sections
 	and articles to other sections that their direct parent.',
--- polyhierarchie/branches/v2.0/lang/paquet-polyhier_fr.php
+++ polyhierarchie/branches/v2.0/lang/paquet-polyhier_fr.php
@@ -5,6 +5,7 @@
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
+	'polyhier_nom' => 'PolyHiérarchie',
 	'polyhier_slogan' => 'Rattacher un article ou une rubrique à plusieurs rubriques parentes',
 	'polyhier_description' => 'Plugin permettant de créer des liens transversaux en rattachant des rubriques
 	et articles à d\'autres rubriques que leur parent direct.',
