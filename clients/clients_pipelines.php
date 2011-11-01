3c50faf81f7132b475f1089a99895570fd58fca2
pierre.fiches@gmail.com <pierre.fiches@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-11-01 11:13:39 +0000

    Fichier manquant dans le précédent commit
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@53925 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- clients/clients_pipelines.php
+++ clients/clients_pipelines.php
@@ -1,5 +1,7 @@
 <?php
 
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
 function clients_header_prive($flux)
 {
 	$flux .= '<script type="text/javascript">/* <![CDATA[ */';
@@ -8,19 +10,43 @@ function clients_header_prive($flux)
 	$flux .= 'if (!$(this).is(":checked")){';
 	$flux .= '	$("#champ_elm_2").hide();';
 	$flux .= '	$("#champ_elm_2").siblings().hide();';
+	$flux .= '	$(".editer_type_civ").hide();';
 	$flux .= '	$(".editer_elm_civ").hide();';
 	$flux .= '}});';
 	$flux .= '$("li.editer_elm #champ_elm_1").click(function(){';
 	$flux .= 'if ($(this).is(":checked")){';
 	$flux .= '	$("#champ_elm_2").slideDown();';
 	$flux .= '	$("#champ_elm_2").siblings().slideDown();';
-	$flux .= '	$(".editer_elm_civ").slideDown();';
+	$flux .= '	$(".editer_type_civ").slideDown();';
 	$flux .= '}else{';
 	$flux .= '	$("#champ_elm_2").slideUp();';
 	$flux .= '	$("#champ_elm_2").siblings().slideUp();';
+	$flux .= '	$(".editer_type_civ").slideUp();';
+	$flux .= '}';
+	$flux .= '});';
+		
+	$flux .= '$("li.editer_type_civ #champ_type_civ_2").each(function(){';
+	$flux .= 'if (!$(this).is(":checked")){';
+	$flux .= '	$(".editer_elm_civ").hide();';
+	$flux .= '}});';
+	$flux .= '$("li.editer_type_civ #champ_type_civ_2, li.editer_elm #champ_elm_1").click(function(){';	
+	$flux .= 'if ($(this).is(":checked")){';
+	$flux .= '	$(".editer_elm_civ").slideDown();';
+	$flux .= '}else{';
 	$flux .= '	$(".editer_elm_civ").slideUp();';
 	$flux .= '}';
 	$flux .= '});';
+	
+	$flux .= '$("li.editer_type_civ #champ_type_civ_1").each(function(){';
+	$flux .= 'if ($(this).is(":checked")){';
+	$flux .= '	$(".editer_elm_civ").hide();';
+	$flux .= '}});';
+	$flux .= '$("li.editer_type_civ #champ_type_civ_1").click(function(){';
+	$flux .= 'if ($(this).is(":checked")){';
+	$flux .= '	$(".editer_elm_civ").slideUp();';
+	$flux .= '}else{';
+	$flux .= '	$(".editer_elm_civ").slideDown();';
+	$flux .= '}});';
 	
 	$flux .= '$("li.editer_elm #champ_elm_3").each(function(){';
 	$flux .= 'if (!$(this).is(":checked")){';
