45a84c0c1c1b4847380e571ec14a6b112a45afdc
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-11-01 20:06:53 +0000

    Correction des fichiers de langue du paquet.xml
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@53951 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- bolo/lang/paquet-bolo_fr.php
+++ bolo/lang/paquet-bolo_fr.php
@@ -6,12 +6,11 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	'bolo_nom' => 'Bolo',
-	'bolo_slogan' => 'Outils pour cr&#233;er du faux texte',
-	'bolo_description' => 'Ce plugin permet d'ins&#233;rer tr&#232;s facilement du faux texte dans les articles ou dans les squelettes, en sp&#233;cifiant le nombre de caract&#232;res souhait&#233;s.
-		
-		On l'emploie soit directement dans les squelettes en utilisant le code <code>[(#BOLO|couper{800})]</code>, soit dans les champs #TEXTE des articles gr&acirc;ce au mod&#232;le <code>&lt;bolo800&gt;</code>.
-		
-		La version actuelle permet seulement d'ins&#233;rer du texte latin; une future version pourrait permettre d'ins&#233;rer du texte latin ou fran&ccedil;ais accentu&#233;.',
+	'bolo_slogan' => 'Outils pour créer du faux texte',
+	'bolo_description' => 'Ce plugin permet d\'insérer très facilement du faux texte dans les articles ou dans les squelettes, en spécifiant le nombre de caractères souhaités.
+On l\'emploie soit directement dans les squelettes en utilisant le code <code>[(#BOLO|couper{800})]</code>, soit dans les champs #TEXTE des articles grâce au modèle <code><bolo800></code>.
+
+La version actuelle permet seulement d\'insérer du texte latin; une future version pourrait permettre d\'insérer du texte latin ou français accentué.',
 
 );
 
