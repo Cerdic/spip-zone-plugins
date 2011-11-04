573528e7a234e15a3977628fb61ae619882ed3e9
yffic@lefourneau.com <yffic@lefourneau.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-11-04 21:32:41 +0000

    Fichier de FAQ pour le Manuel de Rédaction
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@54038 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ sjcycle2/lang/faq-sjcycle_fr.php
@@ -0,0 +1,10 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	'diapo_q' => 'Comment insérer un diaporama dans un article ?',
+	'diapo' => 'Après avoir joint vos images à un article, vous pouvez insérer un diaporama dans votre texte en utilisant <code><sjcycleXX></code> (XX étant le numéro de l’article). Des d’options sont disponibles, consultez-les en cliquant sur le bouton d’aide affiché dans l’encadré de la colonne gauche (quand vous êtes sur la page d’édition de l’article)',
+);
+?>
