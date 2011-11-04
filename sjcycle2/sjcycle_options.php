807501244b32e4fbf82f209932544e9427c17342
yffic@lefourneau.com <yffic@lefourneau.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-11-04 21:32:02 +0000

    Revert de 52041 qui cassait la génération de l'aide globale de spip
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@54037 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- sjcycle2/sjcycle_options.php
+++ sjcycle2/sjcycle_options.php
@@ -3,5 +3,5 @@
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 // URL supplémentaire où trouver de l'aide en ligne
-$GLOBALS['help_server'] = url_de_base(1) . str_replace("../", "", _DIR_PLUGIN_SJCYCLE) . "aide/";
+$GLOBALS['help_server'][] = url_de_base(1) . str_replace("../", "", _DIR_PLUGIN_SJCYCLE) . "aide/";
 ?>
