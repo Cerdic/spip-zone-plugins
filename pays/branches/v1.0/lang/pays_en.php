71bb87930869b32758c06848bbc6d733797a8eb8
guillaume@outofspace.fr <guillaume@outofspace.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-11-05 11:07:36 +0000

    prépa trunk/base
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@54046 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ pays/branches/v1.0/lang/pays_en.php
@@ -0,0 +1,20 @@
+<?php
+if (!defined("_ECRIRE_INC_VERSION")) return;
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	// C
+	'code' => 'ISO3166-1 code',
+	
+	// I
+	'id_pays' => 'ID',
+	
+	// N
+	'nom' => 'Country name',
+
+	// P
+	'pays' => 'Country',
+
+	// T
+	'titre_page_test' => 'List of Planet Earth countries',
+);
+
+?>
---
+++ pays/branches/v1.0/lang/pays_fr.php
@@ -0,0 +1,21 @@
+<?php
+if (!defined("_ECRIRE_INC_VERSION")) return;
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'code' => 'Code ISO3166-1',
+
+	// I
+	'id_pays' => 'ID',
+
+	// N
+	'nom' => 'Nom du pays',
+
+	// P
+	'pays' => 'Pays',
+
+	// T
+	'titre_page_test' => 'La liste des pays du monde',
+);
+
+?>
