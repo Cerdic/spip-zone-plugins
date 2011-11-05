9e51fe2b9b381bd1752353031294da11bbda4f51
guillaume@outofspace.fr <guillaume@outofspace.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-11-05 11:30:22 +0000

    version 1.1 passage en spip3
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@54047 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- pays/trunk/lang/pays_en.php
+++ pays/trunk/lang/pays_en.php
@@ -1,12 +1,20 @@
 <?php
-if (!defined("_ECRIRE_INC_VERSION")) return;
+if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-	// C
-	'code' => 'ISO3166-1 code',
-	
+
 	// I
-	'id_pays' => 'ID',
-	
+	'icone_creer_pays' => 'Add a country',
+	'icone_modifier_pays' => 'Modify this country',
+	'info_aucun_pays' => 'No country',
+	'info_1_pays' => 'A country',
+	'info_nb_pays' => '@nb@ countries',
+
+	// L
+	'label_code' => 'ISO3166-1 code',
+	'label_id_pays' => 'ID',
+	'label_nom' => 'Country name',
+	'label_infos' => 'Informations',
+
 	// N
 	'nom' => 'Country name',
 
@@ -14,7 +22,10 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'pays' => 'Country',
 
 	// T
+	'texte_logo_pays' => 'Flag of country',
 	'titre_page_test' => 'List of Planet Earth countries',
+	'titre_pays' => 'Countries',
+	
 );
 
 ?>
