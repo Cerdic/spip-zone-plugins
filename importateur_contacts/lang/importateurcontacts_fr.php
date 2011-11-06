4e718ddce33c773bdb81eedac458d2549b347877
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-11-06 14:42:57 +0000

    langues (importateurcontacts)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@54103 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- importateur_contacts/lang/importateurcontacts_fr.php
+++ importateur_contacts/lang/importateurcontacts_fr.php
@@ -1,19 +1,25 @@
 <?php
-
-// Sécurité
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/importateur_contacts/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-'configurer_fournisseur_active_avec_moteur' => 'Activé avec @moteur@',
-'configurer_fournisseur_desactive' => 'Désactivé',
-'configurer_titre' => 'Configurer les services d\'importation de contacts',
-'erreur_aucun_fournisseur' => 'Aucun service pouvant fournir des contacts n\'a été trouvé. Veuillez installer au moins une librairie d\'importation de contacts.',
-'importer_bouton_recuperer_contacts' => 'Récupérer mes contacts',
-'importer_fournisseur_label' => 'Choisissez le service chez qui récupérer vos contacts',
-'info_titre' => 'Importateur de contacts',
-'info_aucun_contact' => 'Aucun contact n\'a été trouvé pour ce compte.',
-'info_nb_contacts' => '@nb@ contacts ont été récupérés.',
-'info_nb_contacts_erreur' => '@nb@ contacts ont été récupérés mais n\'ont pu être utilisés.',
+
+	// C
+	'configurer_fournisseur_active_avec_moteur' => 'Activé avec @moteur@',
+	'configurer_fournisseur_desactive' => 'Désactivé',
+	'configurer_titre' => 'Configurer les services d\'importation de contacts',
+
+	// E
+	'erreur_aucun_fournisseur' => 'Aucun service pouvant fournir des contacts n\'a été trouvé. Veuillez installer au moins une librairie d\'importation de contacts.',
+
+	// I
+	'importer_bouton_recuperer_contacts' => 'Récupérer mes contacts',
+	'importer_fournisseur_label' => 'Choisissez le service chez qui récupérer vos contacts',
+	'info_aucun_contact' => 'Aucun contact n\'a été trouvé pour ce compte.',
+	'info_nb_contacts' => '@nb@ contacts ont été récupérés.',
+	'info_nb_contacts_erreur' => '@nb@ contacts ont été récupérés mais n\'ont pu être utilisés.',
+	'info_titre' => 'Importateur de contacts'
 );
 
 ?>
