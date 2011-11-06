8eec2d5679a9a9a37631b76fe9fc3ca80e742876
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-11-06 14:43:00 +0000

    langues (openinviter)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@54106 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- openinviter/lang/openinviter_fr.php
+++ openinviter/lang/openinviter_fr.php
@@ -1,14 +1,18 @@
 <?php
-
-// Sécurité
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/openinviter/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-'champ_email_label' => 'Votre adresse email @fournisseur@',
-'champ_password_label' => 'Mot de passe',
-'champ_password_explication' => 'Il ne sera pas gardé en mémoire.',
-'erreur_authentification' => 'L\'authentification a échoué. Vérifiez l\'adresse et le mot de passe et réessayez.',
-'erreur_generale' => 'Une erreur interne empêche la récupération de vos contacts.',
+
+	// C
+	'champ_email_label' => 'Votre adresse email @fournisseur@',
+	'champ_password_explication' => 'Il ne sera pas gardé en mémoire.',
+	'champ_password_label' => 'Mot de passe',
+
+	// E
+	'erreur_authentification' => 'L\'authentification a échoué. Vérifiez l\'adresse et le mot de passe et réessayez.',
+	'erreur_generale' => 'Une erreur interne empêche la récupération de vos contacts.'
 );
 
 ?>
