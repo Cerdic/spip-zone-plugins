f67d1f585abb913be64d7203ae1fe84f5492fa18
fabrice.albert@laposte.net <fabrice.albert@laposte.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-11-06 09:46:56 +0000

    Modifier le nom de la librairie pour inclure le md5 que SPIP ajoute au téléchargement
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@54075 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- gmapmxn/trunk/gmapmxn_options.php
+++ gmapmxn/trunk/gmapmxn_options.php
@@ -2,6 +2,6 @@
 
 // Path de la librairie mxn
 if (!defined('_DIR_LIB')) define('_DIR_LIB', _DIR_RACINE . 'lib/');
-define('_DIR_LIB_MXN', _DIR_LIB.'mxn-2.0.17/');
+define('_DIR_LIB_MXN', _DIR_LIB.'h12833bc5-mxn-2.0.17/');
 
 ?>
