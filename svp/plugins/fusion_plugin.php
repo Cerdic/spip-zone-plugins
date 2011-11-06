213dc4e9e35d5ff375b05b032f6431cd52365312
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-11-06 17:08:59 +0000

    Mise au point finale de la fusion des balises techniques d'un plugin.xml contenant deux balises <plugin>. Incrément de version car il faut maintenant modifier l'affichage des squelettes utilisant SVP
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@54127 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- svp/plugins/fusion_plugin.php
+++ svp/plugins/fusion_plugin.php
@@ -7,6 +7,7 @@ include_spip('inc/svp_outiller');
 // Fusion des informations de chaque balise plugin en considerant la compatibilite SPIP
 // Pour les balise plugin unique cette fonction permet de generer une structure
 // identique pour les balises dites techniques
+// On limite le traitement a deux balises plugins maximum, ce qui est le cas de tous les plugin.xml actuellement
 function plugins_fusion_plugin($plugins) {
 	global $balises_techniques;
 
@@ -50,22 +51,81 @@ function plugins_fusion_plugin($plugins) {
 		// -- categorie, logo : si la valeur du bloc selectionne est vide on essaye d'en trouver une non vide dans les autres blocs
 		// -- compatible : on constuit l'intervalle global de compatibilite SPIP
 		// -- necessite, utilise, lib : on construit le tableau par intervalle de compatibilite SPIP
-		foreach ($plugins as $_cle => $_plugin) {
-			if ($_cle <> $cle_min_max) {
-				// categorie et icon
-				if (!$fusion['categorie'] AND $_plugin['categorie'])
-					$fusion['categorie'] = $_plugin['categorie'];
-				if (!$fusion['logo'] AND $_plugin['logo'])
-					$fusion['logo'] = $_plugin['logo'];
-				// compatible
-				$fusion['compatibilite'] = fusionner_intervalles($fusion['compatibilite'], $_plugin['compatibilite']);
-				// necessite, utilise, lib : on indexe chaque liste de dependances par l'intervalle de compatibilite
-				// sans regrouper les doublons pour l'instant
+		$cle_min_min = ($cle_min_max==0) ? 1 : 0;
+		if (!$fusion['categorie'] AND $plugins[$cle_min_min]['categorie'])
+			$fusion['categorie'] = $plugins[$cle_min_min]['categorie'];
+		if (!$fusion['logo'] AND $plugins[$cle_min_min]['logo'])
+			$fusion['logo'] = $plugins[$cle_min_min]['logo'];
+		$fusion['compatibilite'] = fusionner_intervalles($fusion['compatibilite'], $plugins[$cle_min_min]['compatibilite']);
+
+		// necessite, utilise, lib : on indexe chaque liste de dependances par l'intervalle de compatibilite
+		// sans regrouper les doublons pour l'instant
+		foreach ($balises_techniques as $_btech) {
+			if (!isset($fusion[$_btech]) OR !$fusion[$_btech]) {
+				if ($plugins[$cle_min_min][$_btech]) {
+					// La balise technique est vide dans le tableau de fusion mais non vide dans la deuxieme balise plugin
+					// On range cette balise dans le tableau fusion de sa compatibilite et on cree la cle commune vide
+					$fusion[$_btech][$plugins[$cle_min_min]['compatibilite']] = $plugins[$cle_min_min][$_btech];
+					$fusion[$_btech][0] = array();
+				}
+			}
+			else if (!isset($plugins[$cle_min_min][$_btech]) OR !$plugins[$cle_min_min][$_btech]) {
+				// La balise technique est non vide dans le tableau de fusion mais vide dans la deuxieme balise plugin
+				// On deplace cette balise dans le tableau fusion de sa compatibilite et on cree la cle commune vide
+				$balise = $fusion[$_btech];
+				unset($fusion[$_btech]);
+				$fusion[$_btech][$plugins[$cle_min_max]['compatibilite']] = $balise;
+				$fusion[$_btech][0] = array();
+			}
+			else {
+				// Les deux tableaux contiennent une balise technique non vide : il faut fusionner cette balise technique !
+				// On parcourt le premier tableau (fusion) en verifiant une egalite avec le deuxieme tableau
+				foreach ($fusion[$_btech] as $_cle0 => $_balise0) {
+					$balise_commune = false;
+					foreach ($plugins[$cle_min_min][$_btech] as $_cle1 => $_balise1) {
+						if (balise_compare_egalite($_balise0, $_balise1)) {
+							// On classe cette balise dans le bloc commun (index 0) et on la supprime dans les
+							// 2 tableaux en cours de comparaison
+							unset($fusion[$_btech][$_cle0]);
+							$fusion[$_btech][0][] = $_balise1;
+							unset($plugins[$cle_min_min][$_btech][$_cle1]);
+							$balise_commune = true;
+							break;
+						}
+					}
+					if (!$balise_commune) {
+						$fusion[$_btech][$plugins[$cle_min_max]['compatibilite']][] = $_balise0;
+						unset($fusion[$_btech][$_cle0]);
+					}
+					if (!isset($fusion[$_btech][0]))
+						$fusion[$_btech][0] = array();
+				}
+
+				// On traite maintenant les balises restantes du deuxieme tableau
+				if ($plugins[$cle_min_min][$_btech])
+					foreach ($plugins[$cle_min_min][$_btech] as $_balise2) {
+						$fusion[$_btech][$plugins[$cle_min_min]['compatibilite']][] = $_balise2;
+					}
 			}
 		}
+
 	}
 	
 	return $fusion;
+}
+
+
+function balise_compare_egalite($balise1, $balise2) {
+
+	foreach ($balise1 as $_attribut1 => $_valeur1){
+		if (!array_key_exists($_attribut1, $balise2))
+			return false;
+		else
+			if ($_valeur1 != $balise2[$_attribut1])
+				return false;
+	}
+
+	return true;
 }
 
 ?>
