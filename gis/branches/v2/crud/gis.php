b7807854083625a19a41f70b958c51e18bf8020c
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-11-08 13:43:54 +0000

    Du coup gis peut porter son crudifix
    Et on incrémente le numéro de version
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@54196 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ gis/branches/v2/crud/gis.php
@@ -0,0 +1,55 @@
+<?php
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+include_spip('action/editer_gis');
+
+/**
+ * Interface C(r)UD pour GIS
+ */
+
+/**
+ * Create :
+ * Crée un point géolocalisé
+ * 
+ * @param $dummy
+ * @param array $set : Le contenu des champs à mettre en base
+ * @return array : un array avec (bool) success, (string) message et (array) result indiquant l'id créé 
+ */
+function crud_gis_create_dist($dummy,$set=null){
+	if ($id = insert_gis()){
+		list($e,$ok) = revision_gis($id,$set);
+	}
+	else{
+		$e = _L('create error');
+	}
+	return array('success'=>$e?false:true,'message'=>$e?$e:$ok,'result'=>array('id'=>$id));
+}
+
+/**
+ * Update :
+ * Met à jour un point géolocalisé
+ * 
+ * @param $dummy
+ * @param array $set : Le contenu des champs à mettre en base
+ * @return array : un array avec (bool) success, (string) message et (array) result indiquant l'id créé 
+ */
+function crud_gis_update_dist($id,$set=null){
+	$id_gis = sql_getfetsel('id_gis','spip_gis','id_gis='.intval($id));
+	if(!$id_gis){
+		$e = _T('gis:erreur_gis_inconnu',array('id'=>$id));
+	}else if(autoriser('modifier','gis',$id)){
+		list($e,$ok) = revisions_gis($id,$set);
+	}else{
+		$e = _L('update error');
+	}
+	return array('success'=>$e?false:true,'message'=>$e?$e:$ok,'result'=>array('id'=>$id));
+}
+function crud_gis_delete_dist($id){
+	if(autoriser('supprimer','gis',$id)){
+		list($e,$ok) = supprimer_gis($id);
+	}
+	return array('success'=>$e?false:true,'message'=>$e?$e:$ok,'result'=>array('id'=>$id));
+}
+
+?>
