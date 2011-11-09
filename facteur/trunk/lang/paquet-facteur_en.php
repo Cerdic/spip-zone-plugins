45e19f36da7352c0af205f1f4ad12ea2c136bf7a
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-11-09 11:19:25 +0000

    Portage vers SPIP 3
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@54241 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ facteur/trunk/lang/paquet-facteur_en.php
@@ -0,0 +1,19 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+// 
+///  Fichier produit par PlugOnet
+// Module: paquet-facteur
+// Langue: en
+// Date: 09-11-2011 12:03:33
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// F
+	'facteur_description' => 'Postman delivers emails in HTML, text or mixed format; via SMTP or not',
+	'facteur_nom' => 'Postman',
+);
+?>
---
+++ facteur/trunk/lang/paquet-facteur_fr.php
@@ -0,0 +1,20 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+// 
+///  Fichier produit par PlugOnet
+// Module: paquet-facteur
+// Langue: fr
+// Date: 09-11-2011 12:03:33
+// Items: 3
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// F
+	'facteur_description' => 'Facteur s\'occupe de la distribution des courriels au format HTML, texte ou mixte ; via SMTP ou non',
+	'facteur_nom' => 'Facteur',
+	'facteur_slogan' => 'Il distribue votre courrier',
+);
+?>
