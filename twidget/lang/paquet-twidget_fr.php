f1eab07a9ff45d9ffde4ad32e6091b1bda86f4bc
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-11-10 10:11:07 +0000

    Compatibilite SPIP3 et paquet.xml
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@54283 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ twidget/lang/paquet-twidget_fr.php
@@ -0,0 +1,25 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+// 
+///  Fichier produit par PlugOnet
+// Module: paquet-twidget
+// Langue: fr
+// Date: 10-11-2011 11:09:47
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// T
+	'twidget_description' => 'Un widget Twitter facile a installer
+_ Ajoutez une inclusion dans vos squelettes pour afficher un widget twitter :
+_ <code>#INCLURE{fond=inclure/twidget_profile,user=GusLeLapin}</code>
+ ou <code>#INCLURE{fond=inclure/twidget_search,search=#SPIP,title=\'Suivez twitter\',subject=\'SPIP\'}</code>
+
+ Le plugin fait office de proxy afin qu\'aucune requête ne soit faite par vos visiteurs vers twitter
+ et éviter tout traçage possible de leur activité (préservation de leur vie personnelle).',
+	'twidget_slogan' => 'Un widget Twitter facile a installer',
+);
+?>
