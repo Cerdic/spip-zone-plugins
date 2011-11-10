fe6b35500bd03c5f27e78a97c21ffda466f11562
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-11-10 09:54:02 +0000

    Compat SPIP 3 avec un paquet.xml
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@54281 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ recommander/lang/paquet-recommander_en.php
@@ -0,0 +1,26 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+// 
+///  Fichier produit par PlugOnet
+// Module: paquet-recommander
+// Langue: en
+// Date: 10-11-2011 10:52:39
+// Items: 3
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// R
+	'recommander_description' => '"Recommend an article to a friend" functionnality.
+			
+We add it in a skeleton like this :
+<code>#RECOMMANDER{title of the page,URL of the page,introduction of the page,subject}</code>
+_ for example
+<code>#RECOMMANDER{#TITRE,#URL_ARTICLE,#INTRODUCTION}</code>
+_ Url, introduction and subject are optionals.',
+	'recommander_nom' => 'Recommend to a friend',
+	'recommander_slogan' => '"Recommend an article to a friend" functionnality',
+);
+?>
---
+++ recommander/lang/paquet-recommander_fr.php
@@ -0,0 +1,28 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+// 
+///  Fichier produit par PlugOnet
+// Module: paquet-recommander
+// Langue: fr
+// Date: 10-11-2011 10:52:39
+// Items: 3
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// R
+	'recommander_description' => 'Fonction recommander un article à un ami.
+
+On l\'ajoute dans n\'importe quel squelette sous la forme :
+<code>#RECOMMANDER{titre de la page,url de la page,intro de la page,sujet}</code>
+_ par exemple
+<code>#RECOMMANDER{#TITRE,#URL_ARTICLE,#INTRODUCTION}</code>
+_ l\'url, l\'intro et le sujet sont facultatifs
+
+L\'interface est en ahah.',
+	'recommander_nom' => 'Recommander à un ami',
+	'recommander_slogan' => 'Fonction recommander un article à un ami',
+);
+?>
