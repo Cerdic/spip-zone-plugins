a4e006ea9493c828fc497018ad603dfb4cce1d4b
klaus@spip.de <klaus@spip.de@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-11-10 10:17:04 +0000

    première traduction des paniers en allemand - by klaus++
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@54284 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ paniers/lang/paniers_de.php
@@ -0,0 +1,32 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/paniers/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'action_ajouter' => 'In den Warenkorb legen',
+
+	// M
+	'mon_panier' => 'Mein Warenkorb',
+	'mon_panier_modifier' => 'Meinen Warenkorb ändern',
+	'mon_panier_voir' => 'Meinen Warenkorb ansehen',
+
+	// P
+	'panier_description' => 'Beschreibung',
+	'panier_erreur_quantites' => 'Mengenangaben müssen in Ganzzahlen sein.',
+	'panier_montant' => 'Betrag',
+	'panier_prix_unitaire' => 'Einzelpreis',
+	'panier_quantite' => 'Menge',
+	'panier_quantite_ok' => 'Die Mengenangaben wurden geäündert.',
+	'panier_recalculer' => 'Neu berechnen',
+	'panier_taxes' => 'Steueranteil',
+	'panier_total_ht' => 'Bruttosumme',
+	'panier_total_ttc' => 'Summe',
+	'panier_valider' => 'Warenkorb bestätigen',
+	'panier_vide' => 'Der Warenkorb ist leer.',
+	'paniers' => 'Warenkörbe'
+);
+
+?>
