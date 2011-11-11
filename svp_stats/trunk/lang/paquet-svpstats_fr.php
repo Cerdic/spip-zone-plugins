3c3a777c6d4046693a5c8d466c8c068f56405c71
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-11-11 11:55:46 +0000

    Passage en paquet.xml et incrément de version au passage
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@54317 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ svp_stats/trunk/lang/paquet-svpstats_fr.php
@@ -0,0 +1,17 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+// Module: paquet-svpstats
+// Langue: fr
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// S
+	'svpstats_description' => 'Ce plugin est un module optionnel de SVP. 
+_ Il permet d\'acquérir, de mettre à jour et de restituer les statistiques d\'utilisation des plugins SPIP sur le net. 
+Ces statistiques sont issues du site http://stats/spip.org.',
+	'svpstats_slogan' => 'Module SVP de gestion des statistiques d\'utilisation des plugins',
+);
+?>
