c2f2fab7b60cc8de5d5f3ab0643d3a34870af7fc
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-11-11 11:36:42 +0000

    On passe SVP en paquet.xml et incrément de version pour la peine.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@54310 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ svp/lang/paquet-svp_fr.php
@@ -0,0 +1,16 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+// Module: paquet-svp
+// Langue: fr
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// S
+	'svp_description' => 'Ce plugin fournit une API permettant de collecter, d\'effectuer des recherches multi-critères et de présenter les informations 
+	sur les plugins SPIP - modules fonctionnels, thèmes et squelettes. Cette API sert aussi de base au plugin STP.',
+	'svp_slogan' => 'SerVeur d\'information sur les Plugins',
+);
+?>
