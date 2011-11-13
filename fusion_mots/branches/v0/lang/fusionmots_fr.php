2ccfa6221b0770190ef89a1d229cf3ad15b702e5
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-11-13 14:57:24 +0000

    branchement phase 4
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@54380 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ fusion_mots/branches/v0/lang/fusionmots_fr.php
@@ -0,0 +1,18 @@
+<?php
+/** Fichier de langue de SPIP **/
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	
+	'erreur_par'		=>'Choisissez un mot de remplacement',
+	'erreur_remplacer'	=>'Choisissez au moins un mot à remplacer',
+	'explication'		=>'Sur cette page vous pouvez fusionner des mots-clefs. Choisissez à gauche les mots-clefs à remplacer par celui choisi à droite.',
+	'fusionmots'		=>"Fusionner des mots-clef",
+	'par'				=>'Par',
+	'remplacer'			=>'Remplacer le(s) mot(s)-clef',
+	
+);
+
+?>
+
+
