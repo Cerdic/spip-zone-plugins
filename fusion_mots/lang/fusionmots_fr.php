74923726cb3562036ce2d5158f626b78e828cd13
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-11-13 12:35:32 +0000

    gestion des erreurs
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@54372 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- fusion_mots/lang/fusionmots_fr.php
+++ fusion_mots/lang/fusionmots_fr.php
@@ -3,10 +3,13 @@
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-    'explication' =>'Sur cette page vous pouvez fusionner des mots-clefs. Choisissez à gauche les mots-clefs à remplacer par celui choisi à droite.',
-	'fusionmots'  =>"Fusionner des mots-clef",
-	'par'         =>'Par',
-	'remplacer'   =>'Remplacer le(s) mot(s)-clef',
+	
+	'erreur_par'		=>'Choisissez un mot de remplacement',
+	'erreur_remplacer'	=>'Choisissez au moins un mot à remplacer',
+	'explication'		=>'Sur cette page vous pouvez fusionner des mots-clefs. Choisissez à gauche les mots-clefs à remplacer par celui choisi à droite.',
+	'fusionmots'		=>"Fusionner des mots-clef",
+	'par'				=>'Par',
+	'remplacer'			=>'Remplacer le(s) mot(s)-clef',
 	
 );
 
