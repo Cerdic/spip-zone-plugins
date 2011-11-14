b043c2f4de852472460b64885073dbf2218e2464
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-11-14 10:21:28 +0000

    factoriser image_histogramme
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@54428 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- metadonnees_photo/trunk/exec/pave_histogramme.php
+++ metadonnees_photo/trunk/exec/pave_histogramme.php
@@ -4,7 +4,7 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 
 function exec_pave_histogramme() {
 	include_spip('inc/actions');
-	ajax_retour(recuperer_fond("prive/squelettes/inclure/image_histogramme_small", array('id_document' => _request('id_document'))));
+	ajax_retour(recuperer_fond("prive/squelettes/inclure/image_histogramme", array('max'=>138,'id_document' => _request('id_document'))));
 }
 
 
