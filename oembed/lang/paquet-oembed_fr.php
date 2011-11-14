c76945f718ce19df99a5b50b3399f638e882455a
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-11-14 14:34:38 +0000

    un paquet.xml une icone qui s'apparente un peu a ce qui ressemble le plus a une icone officielle de oEmbed, et qui parle un peu
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@54441 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ oembed/lang/paquet-oembed_fr.php
@@ -0,0 +1,19 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-oembed
+// Langue: fr
+// Date: 14-11-2011 15:22:10
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// O
+	'oembed_description' => 'oEmbed client/provider pour SPIP.',
+	'oembed_slogan' => 'oEmbed client/provider pour SPIP',
+);
+?>
