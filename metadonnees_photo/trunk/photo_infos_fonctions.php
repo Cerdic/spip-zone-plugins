20263aa868b0b213e34b091f77aad9f15e3db6d4
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-11-14 10:47:45 +0000

    passer quoi au squelette pour differencier les cas d'utilisation indenter le html simplifier le js pour se passer des id avec md5
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@54430 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- metadonnees_photo/trunk/photo_infos_fonctions.php
+++ metadonnees_photo/trunk/photo_infos_fonctions.php
@@ -9,7 +9,7 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
  */
 function photo_infos_afficher_metas_document($flux){
 	if ($id_document = $flux['args']['id_document']){
-		$flux["data"] .= recuperer_fond("prive/squelettes/inclure/image_pave_exif",array('id_document' => $id_document));
+		$flux["data"] .= recuperer_fond("prive/squelettes/inclure/image_pave_exif",array('id_document' => $id_document,'quoi'=>$flux['args']['quoi']));
 	}
 	return $flux;
 }
