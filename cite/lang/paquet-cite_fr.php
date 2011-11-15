4a65798e185466c338e4adb9f3f7a26d982ee02c
joseph@larmarange.net <joseph@larmarange.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-11-15 16:50:45 +0000

    On génère un paquet.xml pour SPIP 3 et utilisation d'icônes fatcow pour harmonisation graphique.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@54491 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ cite/lang/paquet-cite_fr.php
@@ -0,0 +1,18 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+// Fichier produit par PlugOnet
+// Module: paquet-cite
+// Langue: fr
+// Date: 15-11-2011 16:29:31
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// C
+	'cite_description' => 'Le plugin Cite fournit des modèles permettant de citer proprement des références bibliographiques dans vos articles tout en offrant un export dans des formats bibliographiques courants.',
+	'cite_slogan' => 'Insérer des références bibliographiques',
+);
+?>
