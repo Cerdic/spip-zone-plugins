482aeafb402fd79c21a3f08309998af36d5e1844
joseph@larmarange.net <joseph@larmarange.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-11-15 17:06:26 +0000

    On recopie la v1
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@54494 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ refbase/branches/v_1/refbase_ieconfig_metas.php
@@ -0,0 +1,10 @@
+<?php
+
+function refbase_ieconfig_metas($table){
+	$table['refbase']['titre'] = 'Refbase';
+	$table['refbase']['icone'] = 'img_pack/refbase-24.png';
+	$table['refbase']['metas_serialize'] = 'refbase';
+	return $table;
+}
+
+?>
