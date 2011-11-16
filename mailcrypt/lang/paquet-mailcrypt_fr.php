1353ae11f7480a881e44ee629b9061169fb585e5
joseph@larmarange.net <joseph@larmarange.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-11-16 17:08:27 +0000

    Ajout du paquet.xml
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@54523 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ mailcrypt/lang/paquet-mailcrypt_fr.php
@@ -0,0 +1,18 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+// Fichier produit par PlugOnet
+// Module: paquet-mailcrypt
+// Langue: fr
+// Date: 16-11-2011 17:05:36
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// M
+	'mailcrypt_description' => 'MailCrypt remplace les liens de courriels dans vos textes par un lien javascript afin d\'éviter que des robots spammeurs ne collectent les emails affichés sur votre site. La modification est transparente sur un navigateur disposant de javascript. Lorsque javascript n\'est pas installé, le mail reste compréhensible par un être humain.',
+	'mailcrypt_slogan' => 'Protéger les liens de courriels',
+);
+?>
