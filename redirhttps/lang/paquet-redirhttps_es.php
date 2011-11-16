102c8c7a4f235204d095e5287c82d6b08b65f942
lesage.sylvain@laposte.net <lesage.sylvain@laposte.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-11-16 21:14:49 +0000

    creation de paquet.xml, avec plugOnet.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@54530 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ redirhttps/lang/paquet-redirhttps_es.php
@@ -0,0 +1,19 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-redirhttps
+// Langue: es
+// Date: 16-11-2011 16:57:06
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// R
+	'redirhttps_description' => 'Redirección hacía HTTPS cuando el contendio le necesita (espacio privado, login, páginas públicas con el usuario conectado)',
+	'redirhttps_nom' => 'Redireciones HTTPS',
+);
+?>
---
+++ redirhttps/lang/paquet-redirhttps_fr.php
@@ -0,0 +1,20 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-redirhttps
+// Langue: fr
+// Date: 16-11-2011 16:57:06
+// Items: 3
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// R
+	'redirhttps_description' => 'Redirection vers HTTPS quand le contenu le nécessite (espace privé, login, pages publiques avec utilisateur connecté)',
+	'redirhttps_nom' => 'Redirections HTTPS',
+	'redirhttps_slogan' => 'Sécuriser l\'accès à certaines pages du site',
+);
+?>
