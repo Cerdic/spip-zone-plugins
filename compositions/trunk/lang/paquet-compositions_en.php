ff6c9f2da64e635c4558f2955b92cdac56a05eb9
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-11-17 15:33:26 +0000

    un paquet pour compositions 3.0 pour SPIP 3
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@54556 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ compositions/trunk/lang/paquet-compositions_en.php
@@ -0,0 +1,18 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-compositions
+// Langue: en
+// Date: 17-11-2011 16:23:17
+// Items: 1
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// C
+	'compositions_description' => 'This plugin allows you to define several variants of compositions for each kind of SPIP objects (article, sections, etc..) and to easily apply them, case by case, from the private area.',
+);
+?>
---
+++ compositions/trunk/lang/paquet-compositions_fr.php
@@ -0,0 +1,19 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-compositions
+// Langue: fr
+// Date: 17-11-2011 16:23:17
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// C
+	'compositions_description' => 'Ce plugin vous permet de définir plusieurs variantes de compositions pour chaque type objet SPIP (article, rubrique, etc.) et de les appliquer simplement, cas par cas, depuis l\'espace privé.',
+	'compositions_slogan' => 'Varier les mises en page selon les rubriques, les articles...',
+);
+?>
