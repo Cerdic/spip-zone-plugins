ed6959236ef8acdafc6d66df7c7bed169990b93d
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-11-17 16:02:38 +0000

    Un fichier en langue anglaise ... correct
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@54557 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- gis/branches/v2/lang/gis_en.php
+++ gis/branches/v2/lang/gis_en.php
@@ -2,8 +2,7 @@
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
 // extrait automatiquement de http://www.spip.net/trad-lang/
 // ** ne pas modifier le fichier **
-
-if (!defined('_ECRIRE_INC_VERSION')) return;
+if (!defined("_ECRIRE_INC_VERSION")) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
@@ -20,9 +19,10 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'cfg_descr_gis' => 'Geographic Information System.<br /><a href="http://www.spip-contrib.net/3887" class="spip_out">Link to the documentation.</a>',
 	'cfg_inf_adresse' => 'Displays additional address fields (country, city, state, address ...)',
 	'cfg_inf_cloudmade' => 'This API needs a key you can create on <a href=\'@url@\'>the CloudMade website</a>.',
-	'cfg_inf_geocoder' => 'Enable search function in form.', # MODIF
+	'cfg_inf_geocoder' => 'Enable geocoder functions in forms (search from an address, recovery of the address from the coordinates).',
 	'cfg_inf_geolocaliser_user_html5' => 'If the user\'s browser allows it, its approximate geographic location is retrieved to give the default position when creating a new point.',
 	'cfg_inf_google' => 'This API needs a key you can create on <a href=\'@url@\'>the GoogleMaps website</a>.',
+	'cfg_inf_yandex' => 'This API needs a key you can create on <a href=\'@url@\' class=\'spip_out\'>the yandex website</a>.',
 	'cfg_lbl_adresse' => 'Show address fields',
 	'cfg_lbl_api' => 'Geolocation API',
 	'cfg_lbl_api_cloudmade' => 'CloudMade',
@@ -30,36 +30,43 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'cfg_lbl_api_googlev3' => 'Google Maps v3',
 	'cfg_lbl_api_key_cloudmade' => 'CloudMade API key',
 	'cfg_lbl_api_key_google' => 'GoogleMaps API key',
+	'cfg_lbl_api_key_yandex' => 'Yandex API key',
 	'cfg_lbl_api_mapquest' => 'MapQuest',
 	'cfg_lbl_api_microsoft' => 'Microsoft Bing',
-	'cfg_lbl_api_multimap' => 'MultiMap',
 	'cfg_lbl_api_openlayers' => 'OpenLayers',
-	'cfg_lbl_api_openspace' => 'OpenSpace',
-	'cfg_lbl_api_yahoo' => 'Yahoo',
+	'cfg_lbl_api_ovi' => 'Ovi Nokia',
+	'cfg_lbl_api_yandex' => 'Yandex',
 	'cfg_lbl_geocoder' => 'Geocoder',
 	'cfg_lbl_geolocaliser_user_html5' => 'At the creation step, center the map on the location of the user',
+	'cfg_lbl_maptype' => 'Base map',
+	'cfg_lbl_maptype_carte' => 'Map',
+	'cfg_lbl_maptype_hybride' => 'Hybrid',
+	'cfg_lbl_maptype_relief' => 'Relief',
+	'cfg_lbl_maptype_satellite' => 'Satellite',
 	'cfg_titre_gis' => 'GIS',
 
 	// E
 	'editer_gis_editer' => 'Edit this point',
-	'editer_gis_explication' => 'This page lists the whole geolocated points of the website.',
+	'editer_gis_explication' => 'This page lists the whole location-based points of the website.',
 	'editer_gis_nouveau' => 'Create a new point',
-	'editer_gis_titre' => 'The geolocated points',
+	'editer_gis_titre' => 'The location-based points',
 	'erreur_recherche_pas_resultats' => 'No point corresponds to the searched text.',
+	'explication_api_forcee' => 'The is imposed by another plugin or skeleton.',
+	'explication_maptype_force' => 'The base map is imposed by another plugin or skeleton.',
 
 	// F
-	'formulaire_creer_gis' => 'Create a new geolocated point :',
-	'formulaire_modifier_gis' => 'Modify the geolocated point :',
+	'formulaire_creer_gis' => 'Create a new location-based point :',
+	'formulaire_modifier_gis' => 'Modify the location-based point :',
 
 	// I
-	'icone_gis_tous' => 'Geolocated points',
-	'info_id_objet' => 'N&deg;',
-	'info_liste_gis' => 'Points géolocalisés', # NEW
-	'info_liste_gis_objet' => 'Linked points to this object',
-	'info_liste_objets_gis' => 'Objects attached to this point',
+	'icone_gis_tous' => 'Location-based points',
+	'info_id_objet' => 'N°',
+	'info_liste_gis' => 'Location-based points',
+	'info_liste_gis_objet' => 'Location-based points linked to this object',
+	'info_liste_objets_gis' => 'Objects linked to this object',
 	'info_numero_gis' => 'Point number',
 	'info_objet' => 'Object',
-	'info_supprimer_lien' => 'Detach',
+	'info_supprimer_lien' => 'Unlink',
 	'info_voir_fiche_objet' => 'Go to page',
 
 	// L
