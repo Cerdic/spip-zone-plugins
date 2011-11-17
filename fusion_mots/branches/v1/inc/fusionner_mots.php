971ff47608873370c8d256855dad0c0f6c1f81f4
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-11-17 21:06:53 +0000

    nouvelles requetes sql pour spip 3
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@54571 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- fusion_mots/branches/v1/inc/fusionner_mots.php
+++ fusion_mots/branches/v1/inc/fusionner_mots.php
@@ -4,45 +4,40 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 function fusionner_mots($source,$cible){
 	// $source 	=> 	array des id_mot
 	// $cible	=>	id_mot	
+
+	$objets = sql_allfetsel('DISTINCT objet', 'spip_mots_liens');
+
 	
-	$objets = array('article','breve','document','forum','rubrique','syndic');
+	foreach($objets as $objet){
+		$objet = sql_quote($objet['objet']);
+		
 	
-	foreach ($objets as $objet){
-		$table = 'spip_mots_'.table_objet($objet);
-		if ($objet == 'forum') {// une petite exception
-			$table = 'spip_mots_forum';
-		}	
-		
-		
 		foreach ($source as $id_mot){
 			if ($id_mot !=$cible){
 				
 				// pour éviter les entrées double, vérifier les liens déjà existant
-				$liens_existants 	= sql_allfetsel('id_'.$objet,$table,'id_mot='.intval($cible));
+				$liens_existants 	= sql_allfetsel('id_objet','spip_mots_liens','id_mot='.intval($cible).' and objet='.$objet);
+
 		
 				$liens_existants_formates = array();
 				foreach ($liens_existants as $lien){
-					$liens_existants_formates[] = $lien['id_'.$objet];
+					$liens_existants_formates[] = $lien['id_objet'];
 				}
 				$liens_existants_formates = implode($liens_existants_formates,',');
 				
-				// On met à jour, sauf quand le liens est déjà existant
-				$where = 'id_mot='.intval($id_mot);
+				// On met à jour, sauf quand le lien est déjà existant
+				$where = 'id_mot='.intval($id_mot).' and objet='.$objet;
 				if ($liens_existants_formates != ''){
-					$where.=  ' and '. sql_in('id_'.$objet,$liens_existants_formates,'NOT')	;	
-					
-				}
-				sql_update($table, array("id_mot"=>sql_quote($cible)),$where);	
+					$where.=  ' and '. sql_in('id_objet',$liens_existants_formates,'NOT')	;
+				}	
+				sql_update('spip_mots_liens', array("id_mot"=>sql_quote($cible)),$where);	
 				// on supprime les anciens liens qui existent encore, ceux qu'on n'a pas modifié pour cause de duplicata
-				sql_delete($table,'id_mot='.$id_mot);
-				
-				// On supprime le mot
-				sql_delete ('spip_mots','id_mot='.$id_mot);
+				sql_delete('spip_mots_liens','id_mot='.$id_mot.' and objet='.$objet);
+			
 			}
 		}
 	}
-	
-
-	
+	// suppression des mots
+	sql_delete('spip_mots','id_mot!='.$cible.' and '.sql_in('id_mot',$source)); 
 }
 ?>
