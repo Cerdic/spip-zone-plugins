83679c03cdfc1e8c424e81661ade5d597f5c778b
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-11-18 22:19:19 +0000

    lang en et form de demo
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@54612 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ oeproxy/lang/oeproxy_en.php
@@ -0,0 +1,19 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+'api_call_title' => 'API Call',
+'api_result_html' => 'HTML Result',
+'api_result_xml' => 'XML Result',
+'api_result_json' => 'JSON Result',
+
+'liste_urls_exemples' => 'Try one of these',
+'label_url'=>'Entry your URL',
+'bouton_tester'=>'oEmbed!',
+
+);
+
+
+?>
--- oeproxy/lang/oeproxy_fr.php
+++ oeproxy/lang/oeproxy_fr.php
@@ -4,6 +4,12 @@
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
+'api_call_title' => 'Appel de l\'API',
+'api_result_html' => 'Résultat HTML',
+'api_result_xml' => 'Résultat XML',
+'api_result_json' => 'Résultat JSON',
+
+'liste_urls_exemples' => 'Quelques exemples',
 'label_url'=>'URL de la page',
 'bouton_tester'=>'oEmbed!',
 
