b8cfaa27614cd66a771f91b0302284910537461e
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-11-18 15:30:38 +0000

    formulaire et page de demo !
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@54594 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ oeproxy/formulaires/demo_oembed.php
@@ -0,0 +1,38 @@
+<?php
+/*
+ * Plugin oEmebed The Web
+ * (c) 2011 Cedric Morin
+ * Distribue sous licence GPL
+ *
+ * http://oembed.com/
+ *
+ */
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+function formulaires_demo_oembed_charger_dist(){
+	return array(
+		'url'=>''
+	);
+}
+
+function demo_link($url){
+	if (!$url) return "";
+	return url_absolue(parametre_url("oeproxy.api/",'url',$url));
+}
+
+function demo_oembed($url,$format='json'){
+	$url = demo_link($url);
+	if (!$url) return "";
+	$url = parametre_url($url,'format',$format,'&');
+
+	include_spip('inc/distant');
+	return recuperer_page($url);
+}
+
+function formulaires_demo_oembed_traiter_dist(){
+
+
+	return array('editable' => true);
+
+}
---
+++ oeproxy/lang/oeproxy_fr.php
@@ -0,0 +1,13 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+'label_url'=>'URL de la page',
+'bouton_tester'=>'oEmbed!',
+
+);
+
+
+?>
