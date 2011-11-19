6f450a2e9670fe1810fe7839723c6c20a07f9cc9
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-11-19 16:02:36 +0000

    des fournisseurs oembed
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@54636 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ oembed/oembed/input/posttraite_rdio_rich.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin oEmbed
+ * Licence GPL3
+ *
+ */
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+function oembed_input_posttraite_rdio_rich_dist($data){
+
+	$data['media'] = 'sound';
+
+	return $data;
+}
