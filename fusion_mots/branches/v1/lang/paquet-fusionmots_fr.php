0a034d58c6318a16069adbd567caddc2909df869
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-11-19 10:34:40 +0000

    Correction des plugin.xml et paquet.xml et surtout ajout du fichier de langue de la description et du slogan qui est indispensable pour le paquet.xml. Je renouvèle donc ma requête d'utiliser systématiquement plugonet pour créer un paquet à partir d'un plugin.xml afin qu'il génère l'ensemble des fichiers nécessaires !
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@54616 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ fusion_mots/branches/v1/lang/paquet-fusionmots_fr.php
@@ -0,0 +1,15 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+// Module: paquet-fusionmots
+// Langue: fr
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// F
+	'fusionmots_description' => 'Permet aux administrateurs de remplacer en série des mots-clés par d\'autres, pour en diminuer le nombre et éliminer les redondances.',
+	'fusionmots_slogan' => 'Vous avez trop de mots ? Fusionnez les !',
+);
+?>
