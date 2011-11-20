356c7c2c0e8273a66d7a1e546397d5265024ab00
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-11-20 19:14:12 +0000

    mise en forme de imdb
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@54719 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ oembed/oembed/input/posttraite_oeproxy_imdb_rich.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin oEmbed
+ * Licence GPL3
+ *
+ */
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+function oembed_input_posttraite_oeproxy_imdb_rich_dist($data, $url){
+
+	include_spip('inc/filtres');
+	$data['html'] = filtrer('image_reduire',$data['html'],75,100);
+
+	return $data;
+}
