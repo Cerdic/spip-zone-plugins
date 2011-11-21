4642d5ae8fac8d08a70cac62fea7ddd2f886343f
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-11-21 14:02:32 +0000

    Un paquet.xml, et maj du necessite bonux + pipeline insert_head pas necessaire
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@54738 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ nuage/trunk/lang/paquet-nuage_fr.php
@@ -0,0 +1,24 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-nuage
+// Langue: fr
+// Date: 21-11-2011 14:58:26
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// N
+	'nuage_description' => 'Ce plugin permet d’afficher les mots clés en vrac, en faisant varier la taille de la police en fonction du nombre d\'articles associés au mot-clé. En d\'autres termes, spip permet de créer des « tags » !
+
+	Plusieurs modèles disponibles :
+-* article_nuage : affiche les mots clés de l\'article la taille variant en fonction de la fréquence des mots dans les articles du site
+-* rubrique_nuage : affiche les mots clés des articles de la branche, la taille variant en fonction de la fréquence des mots dans les articles de cette branche
+-* nuage_popularite : affiche les mots clés du site (ou d\'un groupe), la taille variant en fonction de sa popularité relative (somme des popularité des articles ayant ce mot)',
+	'nuage_slogan' => 'Afficher les mots-clés en faisant varier la taille de la police',
+);
+?>
