96bfde91fa6e1cf30bc92d216d62921ba0029779
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-11-21 16:18:50 +0000

    la branche v3 reservee a spip 2.1.x mais il y a visiblement des trucs la dedans qui ne marchent pas du tout le trunk reserve a spip 3.0, et on vire les criteres et modeles completement non fonctionnels si ils avaient un usage, on les recodera fonctionnels (mais on se demande comment ils pouvaient marcher)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@54744 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- nuage/trunk/nuage_fonctions.php
+++ nuage/trunk/nuage_fonctions.php
@@ -2,100 +2,6 @@
 
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
-function critere_popularite($idb, &$boucles, $crit){
-	$op='';
-	$boucle = &$boucles[$idb];
-	$params = $crit->param;
-	$type = array_shift($params);
-	$type = $type[0]->texte;
-	if(preg_match(',^(\w+)([<>=])([0-9]+)$,',$type,$r)){
-		$type=$r[1];
-		$op=$r[2];
-		$op_val=$r[3];
-	}
-	$type_id = 'pop.id_'.$type;
-	$pop = 'lf.popularite';
-	$type_requete = $boucle->type_requete;
-	$id_table = $boucle->id_table . '.' . $boucle->primary;
-	$pop_max=max(1 , 0 + $GLOBALS['meta']['popularite_max']);
-	$boucle->select[]= 'CEIL(SUM('.$pop.'*100/'.$pop_max.')) AS popularite_relative';
-	$boucle->from['pop']="spip_'.$type_requete.'_".$type."s";
-	$boucle->where[]= array("'='", "'".$id_table."'", "'pop.".$boucle->primary."'");
-	$boucle->from['lf']="spip_".$type."s";
-	$boucle->where[]= array("'='", "'".$type_id."'", "'lf.id_".$type."'");
-	$boucle->group[]=$id_table;
-	if ($op)
-		$boucle->having[]= array("'".$op."'", "'popularite_relative'",$op_val);
-	
-}
-function critere_frequence_branche($idb, &$boucles, $crit){
-	$op='';
-	$boucle = &$boucles[$idb];
-	$params = $crit->param;
-	$type = array_shift($params);
-	$type = $type[0]->texte;
-	if(preg_match(',^(\w+)([<>=])([0-9]+)$,',$type,$r)){
-		$type=$r[1];
-		$op=$r[2];
-		$op_val=$r[3];
-	}
-	$type_id = 'freqb.id_'.$type;
-	$type_requete = $boucle->type_requete;
-	$id_table = $boucle->id_table . '.' . $boucle->primary;
-	$boucle->select[]= 'COUNT('.$type_id.') AS frequence';
-	$boucle->from['freqb']="spip_'.$type_requete.'_".$type."s";
-	$boucle->where[]= array("'='", "'".$id_table."'", "'freqb.".$boucle->primary."'");
-	$boucle->from['lf']="spip_".$type."s";
-	$boucle->where[]= array("'='", "'".$type_id."'", "'lf.id_".$type."'");
-	$boucle->group[]=$id_table;
-	if ($op)
-		$boucle->having[]= array("'".$op."'", "'frequence'",$op_val);
-	
-	$not = $crit->not;
-	$boucle = &$boucles[$idb];
-	$arg = calculer_argument_precedent($idb, 'id_rubrique', $boucles);
-	$c = "calcul_mysql_in('lf.id_rubrique', calcul_branche($arg), '')";
-	$c = "($arg ? $c : 1)";			
-	if ($not)
-		$boucle->where[]= array("'NOT'", $c);
-	else
-		$boucle->where[]= $c;
-}
-function critere_popularite_branche($idb, &$boucles, $crit){
-	$op='';
-	$boucle = &$boucles[$idb];
-	$params = $crit->param;
-	$type = array_shift($params);
-	$type = $type[0]->texte;
-	if(preg_match(',^(\w+)([<>=])([0-9]+)$,',$type,$r)){
-		$type=$r[1];
-		$op=$r[2];
-		$op_val=$r[3];
-	}
-	$type_id = 'pop.id_'.$type;
-	$pop = 'lf.popularite';
-	$type_requete = $boucle->type_requete;
-	$id_table = $boucle->id_table . '.' . $boucle->primary;
-	$pop_max=max(1 , 0 + $GLOBALS['meta']['popularite_max']);
-	$boucle->select[]= 'CEIL(SUM('.$pop.'*100/'.$pop_max.')) AS popularite_relative';
-	$boucle->from['pop']="spip_'.$type_requete.'_".$type."s";
-	$boucle->where[]= array("'='", "'".$id_table."'", "'pop.".$boucle->primary."'");
-	$boucle->from['lf']="spip_".$type."s";
-	$boucle->where[]= array("'='", "'".$type_id."'", "'lf.id_".$type."'");
-	$boucle->group[]=$id_table;
-	if ($op)
-		$boucle->having[]= array("'".$op."'", "'popularite_relative'",$op_val);
-	
-	$not = $crit->not;
-	$boucle = &$boucles[$idb];
-	$arg = calculer_argument_precedent($idb, 'id_rubrique', $boucles);
-	$c = "calcul_mysql_in('lf.id_rubrique', calcul_branche($arg), '')";
-	$c = "($arg ? $c : 1)";
-	if ($not)
-		$boucle->where[]= array("'NOT'", $c);
-	else
-		$boucle->where[]= $c;
-}
 
 function nuage_note($score,$scoremax=1,$max=10) {
 	if ($scoremax == 0) $scoremax = 1;
@@ -212,19 +118,7 @@ function nuage_extrait($nuage,$nombre){
 
 function nuage_insert_head_css($flux) {
 	$css = "<link rel='stylesheet' href='".find_in_path("css/nuage.css")."' type='text/css' />\n";
-	static $done = false;
-	if (intval($GLOBALS['spip_version_branche'])>2
-		OR !$done) {
-		$done = true;
-		$flux .= "\n".$css;
-	}
-	return $flux;
-}
-
-function nuage_insert_head($flux){
-	// au cas ou il n'est pas implemente, mais plus necessaire en spip>=3
-	if (intval($GLOBALS['spip_version_branche'])<3)
-		$flux = nuage_insert_head_css($flux);
+	$flux .= "\n".$css;
 	return $flux;
 }
 ?>
