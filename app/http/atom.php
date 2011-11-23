117a737bf381c2e48b7d335d0cf785928585384b
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-11-23 09:08:08 +0000

    On appelle un squelette "http/atom/<collection>-ressource" quand on appelle une URL d'une ressource.
    Dans le contexte, on met le GET comme pour le squelette de la collection, mais on ajoute aussi :
    - ressource => truc
    - id_collection => truc
    
    Avec id_collection généré proprement par id_table_objet().
    
    Donc si on a une URL "atom/patates/1234", on aurra 
    ressource => 1234
    id_patate => 1234
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@54803 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- app/http/atom.php
+++ app/http/atom.php
@@ -36,6 +36,32 @@ function http_atom_get_collection_dist($collection){
 	}
 }
 
-
+/*
+ * GET sur une ressource
+ * http://site/rest.api/atom/patates
+ */
+function http_atom_get_ressource_dist($collection, $ressource){
+	// Pour l'instant on va simplement chercher un squelette du nom de la ressource
+	// Le squelette prend en contexte les paramètres du GET + l'identifiant de la ressource en essayant de faire au mieux
+	include_spip('base/objets');
+	$cle = id_table_objet($collection);
+	$contexte = array(
+		$cle => $ressource,
+		'ressource' => $ressource,
+	);
+	$contexte = array_merge($_GET, $contexte);
+	
+	if ($flux = recuperer_fond("http/atom/$collection-ressource", $contexte)){
+		header('Status: 200 OK');
+		header("Content-type: application/atom+xml; charset=utf-8");
+		echo $flux;
+		exit;
+	}
+	// Si on ne trouve rien c'est que ça n'existe pas
+	else{
+		header('Status: 404 Not Found');
+		exit;
+	}
+}
 
 ?>
