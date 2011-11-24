d54f0517ebcc7ab9f25e2404e81441e1a90b4e4b
yffic@lefourneau.com <yffic@lefourneau.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-11-24 14:04:59 +0000

    Marchait pas bien, ca
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@54847 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- manuel_site/lang/faq-manuelsite_en.php
+++ manuel_site/lang/faq-manuelsite_en.php
@@ -11,7 +11,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 'img_q' => 'What size should my photo be?',
 'img' => 'There is no "right" size to display an image in an article. It all depends on its content: if it\'s a portrait, a height of 200px should be sufficient, and if it is a beautiful landscape, you can go up to @largeur_max@ pixels width. In any case, no need to send an image of 3000 pixels width, any screen can\'t display it in its entirety! Unless the document is intended for printing.
-_ {Please, note that the maximum weight not to exceed is '. Poids_max_image $.' otherwise the download will be denied}.',
+_ {Please, note that the maximum weight not to exceed is '. $poids_max_image .' otherwise the download will be denied}.',
 
 'img_nombre_q' => 'How easily fill a portfolio?',
 'img_nombre' => 'It is possible to send a lot of photos in one click in an article:
