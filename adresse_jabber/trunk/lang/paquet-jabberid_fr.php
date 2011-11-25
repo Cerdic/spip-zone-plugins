386d30f3e13cb7602c63b6248a0ae753b24e603f
da@weeno.net <da@weeno.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-11-25 06:26:36 +0000

    Corrections syntaxiques de paquet.xml
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@54871 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ adresse_jabber/trunk/lang/paquet-jabberid_fr.php
@@ -0,0 +1,16 @@
+<?php
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	'slogan' => 'Une adresse Jabber pour les auteurs',
+	'description' => 'Permet de disposer d\'un champ "JID" (adresse Jabber) sur les auteurs de SPIP.
+
+Jabber est un système de messagerie instantanée (à la manière de MSN) et décentralisé (comme les mails).
+Il est basé sur le protocole ouvert XMPP.
+
+Si vous ne connaissez pas encore ce réseau de messagerie, vous pourrez trouver des informations sur le
+site francophone JabberFR.
+
+Ce plugin fournit un champ supplémentaire dans le formulaire d\'édition des informations d\'un auteur. La
+balise correspondante est #JID (appellation standard de l\'identifiant Jabber).',
+);
+
+?>
