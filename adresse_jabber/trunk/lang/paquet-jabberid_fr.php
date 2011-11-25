c2a82bb8cf3bb4bf44883f6c2f9e3f27f5ef526d
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-11-25 19:29:25 +0000

    Tout faux le passage à paquet.xml ! Alors en mode vieux con : il est préférable d'utiliser LangOnet pour générer le paquet.xml et les autres fichiers (langue, et migration) afin de ne rien oublier. Ensuite, on adapte le paquet.xml à son besoin.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@54885 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- adresse_jabber/trunk/lang/paquet-jabberid_fr.php
+++ adresse_jabber/trunk/lang/paquet-jabberid_fr.php
@@ -1,16 +1,24 @@
 <?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+// Module: paquet-jabberid
+// Langue: fr
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-	'slogan' => 'Une adresse Jabber pour les auteurs',
-	'description' => 'Permet de disposer d\'un champ "JID" (adresse Jabber) sur les auteurs de SPIP.
+
+// J
+	'jabberid_description' => 'Permet de disposer d\'un champ "JID" (adresse Jabber) sur les auteurs de SPIP.
 
 Jabber est un système de messagerie instantanée (à la manière de MSN) et décentralisé (comme les mails).
 Il est basé sur le protocole ouvert XMPP.
 
 Si vous ne connaissez pas encore ce réseau de messagerie, vous pourrez trouver des informations sur le
-site francophone JabberFR.
+site francophone [JabberFR->http://jabberfr.org/].
 
 Ce plugin fournit un champ supplémentaire dans le formulaire d\'édition des informations d\'un auteur. La
-balise correspondante est #JID (appellation standard de l\'identifiant Jabber).',
+balise correspondante est <code>#JID</code> (appellation standard de l\'identifiant Jabber).',
+	'jabberid_slogan' => 'Une adresse Jabber pour les auteurs',
 );
-
 ?>
