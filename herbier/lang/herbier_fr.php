259ef9f4a8a75abd11b12d830cbfcc08ae178b89
romy@rezo.net <romy@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-11-27 17:49:52 +0000

    Montée de version : - meilleur recadrage des captures - styles moins intrusifs - logo de plugin, etc.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@54934 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ herbier/lang/herbier_fr.php
@@ -0,0 +1,13 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/crayons/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+'herbier_dans' => 'Dans l\'Herbier de&nbsp;<acronym title="Syst&egrave;me de Publication pour Internet">SPIP</acronym>',
+'herbier' => 'L\'Herbier de&nbsp;<acronym title="Syst&egrave;me de Publication pour Internet">SPIP</acronym>'
+
+);
+
+?>
