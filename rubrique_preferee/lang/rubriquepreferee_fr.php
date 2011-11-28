914c8580e65bf6886c0e9c9b95f38f7531d1ff5d
yffic@lefourneau.com <yffic@lefourneau.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-11-28 18:07:56 +0000

    Ce plugin permet de définir une rubrique préférée pour chaque auteur... Dès qu'il lancera la création d'un article, la rubrique par défaut proposée sera cette rubrique préférée (s'il ne se trouve pas déjà dans une rubrique), sinon, la première rubrique dans laquelle il est autorisé à publier, sinon, fonctionnement par défaut de Spip
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@54961 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ rubrique_preferee/lang/rubriquepreferee_fr.php
@@ -0,0 +1,16 @@
+<?php
+if (!defined("_ECRIRE_INC_VERSION")) return;
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	
+	// E
+	'explication' => 'Choisissez ici la rubrique qui sera proposée par défaut lors de la création d\'un nouvel article depuis la page d\'accueil',
+
+	// M
+	'msg_erreur_installation' => 'Erreur à l\'installation du champ extra rubrique préférée',
+
+	// T
+	'titre' => 'Rubrique préférée ',
+
+);
+
+?>
