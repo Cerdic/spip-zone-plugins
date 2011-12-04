2b877ef70ffde38eb02b7aafd229d996b4bfc628
romy@rezo.net <romy@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-12-04 14:54:17 +0000

    Rangement en sous-repertoires
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@55100 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- varicelle/varicelle_pipelines.php
+++ varicelle/varicelle_pipelines.php
@@ -1,7 +1,7 @@
 <?php
 
 function varicelle_insert_head_css($flux) {
-	$flux .= '<link rel="stylesheet" type="text/css" href="'.find_in_path('varicelle.css').'" media="all" />'."\n";
+	$flux .= '<link rel="stylesheet" type="text/css" href="'.find_in_path('css/varicelle.css').'" media="all" />'."\n";
 	return $flux;
 }
 
