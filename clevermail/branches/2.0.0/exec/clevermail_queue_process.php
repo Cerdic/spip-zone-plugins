5cef6ac91e553f43146d0adbf9041b1e7c146b03
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-12-06 16:49:50 +0000

    Pour avoir le verbose, il faut que ce soit le 2e paramètre.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@55169 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- clevermail/branches/2.0.0/exec/clevermail_queue_process.php
+++ clevermail/branches/2.0.0/exec/clevermail_queue_process.php
@@ -2,6 +2,6 @@
 function exec_clevermail_queue_process() {
   include_spip('genie/clevermail_queue_process');
   // On force l'envoi en affichant une trace
-  genie_clevermail_queue_process_dist('yes');
+  genie_clevermail_queue_process_dist('', 'yes');
 }
 ?>
