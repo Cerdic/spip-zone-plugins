1ec4067a4c121f91200b154da6a7dc7ec199fffd
marcimat@rezo.net <marcimat@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-12-06 10:56:50 +0000

    Passage de plugonet pour créer le paquet.xml de SPIP 3
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@55153 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ spip-pmb/trunk/lang/paquet-pmb_fr.php
@@ -0,0 +1,21 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-pmb
+// Langue: fr
+// Date: 06-12-2011 11:50:22
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// P
+	'pmb_description' => 'Ce portail web pour [PMB->http://www.pmbservices.fr] contient un squelette prêt à l’emploi compatible avec les thèmes ZPIP. L’interface avec le catalogue pmb s’effectue par les webservices pmb disponibles à partir de la version 3.3.
+
+PMB est un Sigb (Système Intégré de Gestion de Bibliothèque) libre.',
+	'pmb_slogan' => 'Utiliser SPIP comme portail web du logiciel PMB',
+);
+?>
