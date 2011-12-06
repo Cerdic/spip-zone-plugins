780b42cdbf89962b508b773c6470489977f93aec
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-12-06 11:29:29 +0000

    Portage SPIP 3 : pas de necessite sur bonux, et retablie le bon nom de casier si on utilise CFG + page d'admin en squelette
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@55155 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ menu_langues_liens/lang/paquet-mll_en.php
@@ -0,0 +1,20 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-mll
+// Langue: en
+// Date: 06-12-2011 12:21:22
+// Items: 3
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// M
+	'mll_description' => 'Replace the original language menu by a clickable menu. This menu is displayed only if at least two languages are ​​actually used on the site.',
+	'mll_nom' => 'Language menu as links',
+	'mll_slogan' => 'View languages ​​as clickable links',
+);
+?>
---
+++ menu_langues_liens/lang/paquet-mll_fr.php
@@ -0,0 +1,20 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-mll
+// Langue: fr
+// Date: 06-12-2011 12:21:22
+// Items: 3
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// M
+	'mll_description' => 'Remplace le menu déroulant de langues par un menu cliquable. Ce menu n\'est affiché que s\'il y a au moins 2 langues vraiment utilisées dans le site.',
+	'mll_nom' => 'Menu de langues avec liens',
+	'mll_slogan' => 'Afficher les langues sous forme de liens cliquables',
+);
+?>
