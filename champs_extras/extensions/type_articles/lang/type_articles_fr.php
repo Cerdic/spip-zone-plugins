f06d4f37a8365bff78199c43a7f93df8e6e9882d
yffic@lefourneau.com <yffic@lefourneau.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-12-07 14:42:58 +0000

    Une noisette pour lister les articles d'un type particulier. Utilise Aveline
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@55190 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- champs_extras/extensions/type_articles/lang/type_articles_fr.php
+++ champs_extras/extensions/type_articles/lang/type_articles_fr.php
@@ -2,6 +2,13 @@
 if (!defined("_ECRIRE_INC_VERSION")) return;
 // Fichier ayant vocation à être surchargé depuis un autre plugin
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+	// L
+	'label_choix_type' => 'Type des articles :',
+
+	// N
+	'noisette_description' => 'Liste l\'ensemble des articles d\'un type spécifique du site, dans la même rubrique, dans une rubrique donnée...',
+	'noisette_titre' => 'Liste des articles d\'un type spécifique',
+
 	//T
 	'titre' => "Type de l'article : ",
 	'type_defaut'=>"Article",
