62f32af950aa7192722fd559e53109e7dfdbd48f
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-12-07 14:55:53 +0000

    langues (contacts)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@55218 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ contacts_et_organisations/trunk/lang/contacts_fa.php
@@ -0,0 +1,140 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'aucun_contact' => 'ارتباطي برقرار نيست!',
+	'aucune_organisation' => 'سازماني موجود نيست!',
+
+	// B
+	'bouton_contacts' => 'تماس‌ها',
+	'bouton_contacts_organisations' => 'تماس‌ها & سازمان‌ها',
+	'bouton_organisations' => 'سازمان‌ها',
+	'bouton_rechercher' => 'جستجو',
+	'bouton_repertoire' => 'دايركتوري',
+
+	// C
+	'changer' => 'تغيير',
+	'chercher_contact' => 'جستجو',
+	'chercher_organisation' => 'جستجو',
+	'chercher_statut' => 'رتبه',
+	'confirmer_delier_contact' => 'مطمئن هستيد كه مي‌خواهيد پيوند اين سازمان و اين ارتباط را قطع كنيد؟',
+	'confirmer_delier_organisation' => 'مطمئن هستيد كه مي‌‌خواهيد پيوند با اين سازمان را قطع كنيد؟',
+	'confirmer_delier_organisation_rubrique' => 'مطمئن هستيد كه مي‌‌خواهيد ارتباط اين سازمان با اين بخش را قطع كنيد؟‌',
+	'confirmer_supprimer_contact' => 'مطمئن هستيد كه مي‌خواهيد تمام اطلاعات مربوط يه اين ارتباط را حذف كنيد؟‌',
+	'confirmer_supprimer_organisation' => 'مطمئن هستيد كه مي‌خواهيد تمام اطلاعات مربوط به اين سازمان را حذف كنيد؟‌',
+	'contact' => 'يك تماس',
+	'contact_associe_a_auteur_numero' => 'پيوند به نويسنده‌ي شماره‌ي ',
+	'contact_associer_a_auteur' => 'پيوند به يك نويسنده',
+	'contact_creer' => 'ايجاد يك تماس',
+	'contact_editer' => 'ويرايش اين تماس',
+	'contact_modifier' => 'اصلاح يك تماس',
+	'contact_nouveau' => 'تماس جديد',
+	'contact_numero' => 'تماس شماره‌ي',
+	'contact_voir' => 'بنگريد',
+	'contacts' => 'تماس‌ها',
+	'contacts_nb' => '@nb@ تماس ',
+	'contacts_un' => 'يك تماس ',
+	'creer_auteur_contact' => 'ايجاد يك نويسنده و پيوند او با اين تماس ',
+	'creer_auteur_organisation' => 'ايجاد يك نويسنده و پيوند او به اين سازمان',
+
+	// D
+	'definir_auteur_comme_contact' => 'تعريف به عنوان تماس ',
+	'definir_auteur_comme_organisation' => 'تعريف به عنوان سازمان',
+	'delier_cet_auteur' => 'قطع',
+
+	// E
+	'est_un_contact' => 'اين نويسنده به عنوان يك تماس تعريف مي‌شود.',
+	'est_une_organisation' => 'اين نويسنده به عنوان يك سازمان تعريف مي‌شود.',
+	'explication_activite' => 'فعاليت سازمان:‌ بشردوستانه، فناوري اطلاعات، چاپ . . . ',
+	'explication_contacts_ou_organisations' => 'مي‌توانيد اين نويسنده را به عنوان يك تماس يا يك سازمان تعريف كنيد.ميدان‌هاي اضافي مي‌توانند بعداً از صفحه‌ي تكميلي اطلاعات نويسنده پر شوند.',
+	'explication_identification' => 'هويت سازمان:‌ ',
+	'explication_statut_juridique' => 'سازمان، انجمن، شركت . . . ',
+	'explication_supprimer_contact' => 'حذف اين تماس تمام اطلاعات اضافي را كه در صفحه‌ي نويسنده پر شده حذف مي‌كند. ',
+	'explication_supprimer_organisation' => 'حذف اين سازمان تمام اطلاعات اضافي را كه در صفحه‌ي نويسنده پر شده حذف خواهد كرد. ',
+	'explications_page_contacts' => 'Page en cours de d&eacute;veloppement. <br /><br />Actions envisag&eacute;es :<ul>
+	<li>voir tous les contacts</li><li>transformer les auteurs en contacts</li><li>importer des contacts</li><li>...</li></ul><br />Merci pour vos suggestions sur <a href="http://www.spip-contrib.net/Plugin-Contacts-Organisations#pagination_comments-list">le forum</a> ;-)', # NEW
+	'explications_page_organisations' => 'Page en cours de d&eacute;veloppement. <br /><br />Actions envisag&eacute;es :<ul>
+	<li>voir toutes les organisations</li><li>transformer des auteurs en organisations</li><li>importer des organsations</li><li>...</li></ul><br />Merci pour vos suggestions sur <a href="http://www.spip-contrib.net/Plugin-Contacts-Organisations#pagination_comments-list">le forum</a> ;-)', # NEW
+
+	// I
+	'info_contacts_organisation' => 'تماس‌هاي سازمان',
+	'info_nb_contacts' => 'تماس‌هاي پيوند شده',
+	'info_organisation_appartenance' => 'تعلق سازمان',
+	'info_organisations_appartenance' => 'تعلق سازمان‌ها',
+	'info_organisations_filles' => 'پرونده‌هاي سازمان‌ها',
+	'info_organisations_meres' => 'سازمان‌هاي مادر',
+	'info_tous_contacts' => 'تمام تماس‌ها',
+	'info_toutes_organisations' => 'تمام سازمان‌ها',
+	'infos_contacts_ou_organisations' => 'تماس‌ها & سازمان‌‌ها',
+
+	// L
+	'label_activite' => 'فعاليت',
+	'label_civilite' => 'ادب',
+	'label_date_creation' => 'تاريخ ايجاد',
+	'label_date_naissance' => 'تاريخ تولد',
+	'label_descriptif' => 'شرح',
+	'label_email' => 'ايميل',
+	'label_fonction' => 'كاركرد',
+	'label_identification' => 'هويت',
+	'label_nom' => 'نام',
+	'label_nom_organisation' => 'سازمان',
+	'label_organisation' => 'سازمان وابسته',
+	'label_organisation_parente' => 'سازمان مادر',
+	'label_prenom' => 'اسم كوچك',
+	'label_prenom_nom' => 'اسم كوچك + اسم فاميل',
+	'label_pseudo' => 'اسم مستعار',
+	'label_recherche_auteurs' => 'جستجو در ميان نويسندگان',
+	'label_recherche_contacts' => 'جستجو در ميان تماس‌ها',
+	'label_recherche_organisations' => 'جستجو در ميان سازمان‌ها',
+	'label_statut_juridique' => 'وضعيت',
+	'label_telephone' => 'تلفن.',
+	'label_type_liaison' => 'ارتباط',
+	'lier_ce_contact' => 'الحاق اين تماس',
+	'lier_cet_auteur' => 'پيوند',
+	'lier_cette_organisation' => 'الحاق به اين سازمان',
+	'lier_contact' => 'الحاق به اين تماس ',
+	'lier_organisation' => 'الحاق به يك سازمان',
+	'liste_contacts' => 'ليست تمام تماس‌ها',
+	'liste_organisations' => 'ليست تمام سازمان‌ها',
+
+	// N
+	'nb_contact' => 'تماس 1',
+	'nb_contacts' => '@nb@ تماس ',
+
+	// O
+	'organisation_associe_a_auteur_numero' => 'ارتباط با شماره‌ي نويسنده',
+	'organisation_associer_a_auteur' => 'ارتباط با يك نويسنده',
+	'organisation_creer' => 'ايجاد يك سازمان',
+	'organisation_creer_fille' => 'ايجاد يك پرونده‌ي سازمان',
+	'organisation_editer' => 'ويرايش اين سازمان',
+	'organisation_modifier' => 'تعديل يك سازمان',
+	'organisation_nouveau' => 'سازمان جديد',
+	'organisation_numero' => 'شماره‌ي سازمان',
+	'organisation_un' => 'يك سازمان',
+	'organisation_voir' => 'بنگريد',
+	'organisations' => 'سازمان‌ها',
+	'organisations_nb' => '@nb@ سازمان',
+
+	// R
+	'recherche_de' => ' جستجوي «@recherche@»',
+	'rechercher' => 'جستجو',
+
+	// S
+	'supprimer_contact' => 'حذف اين تماس ',
+	'supprimer_organisation' => 'حذف اين سازمان',
+
+	// T
+	'titre_contact' => 'حزئيات تماس',
+	'titre_organisation' => 'جزئيات سازمان',
+	'titre_page_contacts' => 'مديريت تماس‌ها',
+	'titre_page_organisations' => 'مديريت سازمان‌ها',
+	'titre_page_repertoire' => 'ديركتور'
+);
+
+?>
