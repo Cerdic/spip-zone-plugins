af8df9a58081a0c5b9e99047c38ee22e65890d7f
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-12-07 14:55:56 +0000

    langues (openinviter)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@55220 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ openinviter/lang/openinviter_sk.php
@@ -0,0 +1,20 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'champ_email_label' => 'Vaša e-mailová adresa @&ndash; poskytovateľ@',
+	'champ_password_explication' => 'Nebudeme si ho pamätať.',
+	'champ_password_label' => 'Heslo',
+
+	// E
+	'erreur_authentification' => 'Nepodarilo sa overiť vašu totožnosť. Skontrolujte adresu a heslo a skúste to znova.',
+	'erreur_generale' => 'Obnoveniu vašich kontaktov bráni vnútorná chyba.'
+);
+
+?>
