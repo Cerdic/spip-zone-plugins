fe9734abf4a432a67c98cf71153639571ee98aba
michel.possoz@skynet.be <michel.possoz@skynet.be@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-12-07 14:54:10 +0000

    langues (crayons)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@55200 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- crayons/lang/crayons_nl.php
+++ crayons/lang/crayons_nl.php
@@ -8,11 +8,11 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
-	'activation_barre_impossible' => 'Un autre plugin interdit l\'usage du porte-plume dans l\'espace public.', # NEW
+	'activation_barre_impossible' => 'Een andere plugin verbiedt het gebruik van pen in de openbare ruimte.',
 	'annuler' => 'Annuleren',
 
 	// B
-	'bouton_supprimer' => 'Supprimer', # NEW
+	'bouton_supprimer' => 'Verwijderen',
 
 	// D
 	'deja_modifie' => 'Reeds gewijzigd',
@@ -23,37 +23,37 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'editer_@type@_@id@' => '@type@ @id@ in geheel uitgeven',
 	'editer_tout' => 'Alles uitgeven',
 	'effets' => 'Gevolgen',
-	'explication_exec_autorise' => 'Pages autorisées à la modification
-		(?exec=...) : (, pour séparer les exec - * pour toutes).', # NEW
-	'explication_introduction_article' => 'L\'introduction d\'un article affiche le descriptif de l\'article, sinon affiche ses 600 premiers caractères (chapeau puis texte).', # NEW
-	'explication_reduire_logo' => 'Réduire le logo s\'il dépasse
-		la taille maximale indiquée en pixels. Exemple : 100. (0 = jamais)', # NEW
+	'explication_exec_autorise' => 'Toegestaan ​​om pagina\'s te wijzigen
+  (?exec=...) : (, te scheiden exec - * voor allen).',
+	'explication_introduction_article' => 'De introductie van een functie geeft de beschrijving van het item, anders geeft de eerste 600 karakters (hoed en tekst).',
+	'explication_reduire_logo' => 'Verminder het logo als het hoger is dan 
+   de maximale grootte in pixels. Voorbeeld: 100. (0 = nooit)',
 
 	// L
-	'label_activer_barre_typo' => 'Activer la barre typographique dans les crayons ?', # NEW
-	'label_activer_crayons_prive' => 'Activer crayons pour la partie privée.', # NEW
-	'label_barre_typo' => 'Barre typographique', # NEW
-	'label_clickhide' => 'Un clic en-dehors masque les crayons', # NEW
-	'label_effets' => 'Choix des effets', # NEW
-	'label_espaceprive' => 'Crayons dans le privé ?', # NEW
-	'label_exec_autorise' => 'Pages autorisées', # NEW
-	'label_filet' => 'Filet', # NEW
-	'label_message' => 'Obtenir un message', # NEW
-	'label_msg_abandon' => 'Sur abandon', # NEW
-	'label_msg_no_change' => 'Si pas de changement', # NEW
-	'label_reduire_logo' => 'Réduire les logos', # NEW
-	'label_yellow_fade' => 'Fade', # NEW
-	'legend_controleur_hyperlien' => 'Hyperlien', # NEW
-	'legend_controleur_logo' => 'Choisir un logo :', # NEW
-	'legend_controleur_logo_remplacer' => 'Remplacer ce logo', # NEW
-	'legend_controleur_qui' => 'Qui ?', # NEW
-	'legend_controleur_vignette' => 'Choisir une vignette :', # NEW
-	'legend_controleur_vignette_remplacer' => 'Remplacer cette vignette', # NEW
-	'legend_editer_prive' => 'Éditer l\'espace privé (expérimental)', # NEW
-	'legend_introduction_article' => 'Texte introductif de l\'article', # NEW
+	'label_activer_barre_typo' => 'Draai het typografie roer in de potloden ?',
+	'label_activer_crayons_prive' => 'Schakel potloden voor de private partij.',
+	'label_barre_typo' => 'Typografische bar',
+	'label_clickhide' => 'Te klikken buiten het masker potloden',
+	'label_effets' => 'Selectie-effecten',
+	'label_espaceprive' => 'Potloden in de prive-sector ?',
+	'label_exec_autorise' => 'pagina\'s geautoriseerde',
+	'label_filet' => 'Net',
+	'label_message' => 'Het krijgen van een bericht',
+	'label_msg_abandon' => 'Abandon zuid',
+	'label_msg_no_change' => 'Als er geen verandering',
+	'label_reduire_logo' => 'Verminderen logos',
+	'label_yellow_fade' => 'Fade',
+	'legend_controleur_hyperlien' => 'Hyperlien',
+	'legend_controleur_logo' => 'Kies een logo :',
+	'legend_controleur_logo_remplacer' => 'Vervang deze logo',
+	'legend_controleur_qui' => 'Dat ?',
+	'legend_controleur_vignette' => 'Selecteer een miniatuur :',
+	'legend_controleur_vignette_remplacer' => 'Vervang deze tag',
+	'legend_editer_prive' => 'Bewerk de private deel (experimentele)',
+	'legend_introduction_article' => 'Aanhef van artikel',
 
 	// M
-	'message_image_obligatoire' => 'Il faut choisir une image', # NEW
+	'message_image_obligatoire' => 'U moet kiezen voor een foto',
 	'messages' => 'Berichten',
 	'modifie_par_ailleurs' => 'Voorts gewijzigd',
 
@@ -70,8 +70,8 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'svp_copier_coller' => 'Désolé, copiez/collez vos changements et recommencez',
 
 	// T
-	'titre_config_crayons' => 'Configurer les crayons', # NEW
-	'titre_crayons' => 'Crayons', # NEW
+	'titre_config_crayons' => 'Configureren potloden',
+	'titre_crayons' => 'Podloden',
 
 	// U
 	'update_impossible' => 'De update is niet geslaagd.',
