2b31839c584fc3e0ee2561bc622ee6e8480dbecf
marcimat@rezo.net <marcimat@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-12-08 11:37:59 +0000

    On utilise l'API de champs extras pour installer et désinstaller
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@55267 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- motus/trunk/motus_administrations.php
+++ motus/trunk/motus_administrations.php
@@ -6,19 +6,14 @@ include_spip('base/motus');
 
 function motus_upgrade($nom_meta_base_version,$version_cible){
 	$maj = array();
-	
-	$table_champs = motus_declarer_champs_extras();
-	foreach($table_champs as $table=>$champs)
-		$maj['create'][] = array('champs_extras_creer',$table, $champs);
-	
+	cextras_api_upgrade(motus_declarer_champs_extras(), $maj['create']);
+
 	include_spip('base/upgrade');
 	maj_plugin($nom_meta_base_version, $version_cible, $maj);
 }
 
 function motus_vider_tables($nom_meta_base_version) {
-	$table_champs = titrecourt_declarer_champs_extras();
-	foreach($table_champs as $table=>$champs)
-		champs_extras_supprimer($table, $champs);
+	cextras_api_vider_table(motus_declarer_champs_extras());
 	effacer_meta($nom_meta_base_version);
 }
 
