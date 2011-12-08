2735bc1477c7379846ac49b4a7426725696a1b8d
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-12-08 12:33:08 +0000

    Compatible SPIP 3.0 ... un thème un peu plus passe-partout qui ne casse pas les styles par défaut du site
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@55274 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ tablesorter/lang/paquet-tablesorter_fr.php
@@ -0,0 +1,19 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-tablesorter
+// Langue: fr
+// Date: 08-12-2011 13:11:26
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// T
+	'tablesorter_description' => 'Ce plugin permet de trier les tableaux portant la class CSS "spip" en cliquant simplement sur l\'entête d\'une colonne (le tableau HTML doit donc posséder les tags thead et tbody). Voir en ligne toutes les options de tri, sur le site de [->http://tablesorter.com].',
+	'tablesorter_slogan' => 'Avec tableSorter, le tri sera top !',
+);
+?>
