a50e60cd5b8060da0aed27b066b47ae2efd96aa0
cam.lafit@azerttyu.net <cam.lafit@azerttyu.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-12-08 15:14:41 +0000

    Ne pas forcer des saisies si dans charger on demande le contraire
     * On peut interdire l'affichage d'un formulaire CVT via un retrun false dans C
     * Dans un tel cas, saisies ne genere rien et passe la main aussi
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@55279 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- saisies/saisies_pipelines.php
+++ saisies/saisies_pipelines.php
@@ -38,6 +38,9 @@ function saisies_formulaire_saisies($flux) { return $flux; }
 
 // Déclarer automatiquement les champs d'un CVT si on les trouve dans un tableau de saisies et s'ils ne sont pas déjà déclarés
 function saisies_formulaire_charger($flux){
+    // Si le flux data est inexistant, on quitte : Le CVT d'origine a décidé de ne pas continuer
+    if (!is_array($flux['data']))
+        return $flux;
 	// Il faut que la fonction existe et qu'elle retourne bien un tableau
 	if (include_spip('inc/saisies')
 		and $saisies = saisies_chercher_formulaire($flux['args']['form'], $flux['args']['args'])
