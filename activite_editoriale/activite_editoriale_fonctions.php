4294c0cb1efe4b5e579b716699780fc4c21e6d69
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-12-08 00:00:56 +0000

    encore un peu de code de ce commit
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@55251 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- activite_editoriale/activite_editoriale_fonctions.php
+++ activite_editoriale/activite_editoriale_fonctions.php
@@ -8,10 +8,4 @@ function age_rubrique($date_str) {
 	return intval((time() - strtotime($date_str)) / (60 * 60 * 24));
 }
 
-function mysqldate($date_str)
-{
-	$date=split('/',$date_str);
-	
-	return $date[2].'-'.$date[1].'-'.$date[0];
-}
 ?>
