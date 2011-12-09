3e8244b9ed0daf44323dbded9f84f271082ab90e
drabdou@wakzine.com <drabdou@wakzine.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-12-09 13:45:18 +0000

    ajout de tweet'n'spip à la zone
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@55297 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ tweetnspip/lang/tweetnspip_fr.php
@@ -0,0 +1,37 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+  // main form
+	'cfg_boite_tweetnspip' => 'Configuration du plugin Tweet\'N\'SPIP',
+	'cfg_descr_tweetnspip' => 'Tweet\'N\'SPIP : Plugin jQuery Twitter pour SPIP',
+	'cfg_titre_tweetnspip' => 'Tweet\'N\'SPIP',
+	
+	// parameters
+	'cfg_inf_username' => 'Nom du user twitter &agrave; suivre.',
+	'cfg_lbl_username' => 'Compte Twitter',
+	
+	'cfg_inf_numtweets' => 'Nombre de tweets &agrave; afficher.',
+	'cfg_lbl_numtweets' => 'Nombre de tweets',
+	
+	'cfg_inf_loadertext' => 'Texte affich&eacute; pendant le chargement des tweets.',
+	'cfg_lbl_loadertext' => 'Message de chargement',
+	'cfg_def_loadertext' => 'Chargement des tweets...',
+	
+	'cfg_inf_slidein' => 'Activer ou non l&rsquo;effet de glissement vertical &agrave; la fin du charmement des tweets.',
+	'cfg_lbl_slidein' => 'Effet d&rsquo;apparition',
+	
+	'cfg_inf_showheading' => 'Afficher ou pas l&rsquo;ent&ecirc;te.',
+	'cfg_lbl_showheading' => 'Afficher l&rsquo;ent&ecirc;te',
+
+	'cfg_inf_headingtext' => 'Texte de l&rsquo;ent&ecirc;te.',
+	'cfg_lbl_headingtext' => 'Texte de l&rsquo;ent&ecirc;te',
+	'cfg_def_headingtext' => 'Derniers Tweets',
+
+	'cfg_inf_showprofilelink' => 'Affiche ou nom le lien vers le profile twitter.',
+	'cfg_lbl_showprofilelink' => 'Afficher le lien vers le profil',
+	
+);
+?>
---
+++ tweetnspip/tweetnspip_pipelines.php
@@ -0,0 +1,42 @@
+<?php
+
+function tweetnspip_insert_head($flux){
+	include_spip("inc/filtres");
+	// Initialisation des valeurs de config
+	$config = @unserialize($GLOBALS['meta']['tweetnspip']);
+	if (!is_array($config))
+		$config = array();
+	$config = array_merge(array(
+		'username' => 'MrWaz',
+    'numtweets' => '5',    
+    'loadertext' => 'Charge les tweets...',
+    'slidein' => 'true',
+    'showheading' => 'true',
+    'headingtext' => 'Derniers tweets',
+    'showprofilelink' => 'true'
+	), $config);
+	// Insertion des librairies js
+	$flux .='<script src="'.url_absolue(find_in_path('javascript/jquery.twitter.js')).'" type="text/javascript"></script>';
+	
+	//
+	$flux .='<script type="text/javascript">
+        <!--//--><![CDATA[//><!--
+        $(document).ready(function() {
+                $("#twitter").getTwitter({
+                        userName: "'.$config['username'].'",
+                        numTweets: '.$config['numtweets'].',
+                        loaderText: "'.$config['loadertext'].'",
+                        slideIn: '.$config['slidein'].',
+                        showHeading: '.$config['showheading'].',
+                        headingText: "'.$config['headingtext'].'",
+                        showProfileLink: '.$config['showprofilelink'].'
+                        });
+                });
+        //--><!]]>
+	</script>';
+	// Inclusion des styles propres a tweetnspip
+	$flux .='<link rel="stylesheet" href="'.url_absolue(find_in_path('styles/jquery.twitter.css')).'" media="all" type="text/css" />';
+	return $flux;
+}
+
+?>
