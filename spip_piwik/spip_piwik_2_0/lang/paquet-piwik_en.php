c41345a5e42489d4876eb1ea34a93d7132b39e46
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-12-09 08:29:41 +0000

    A la création du site, il nous le récupère correctement et automatiquement dans la conf... Le slogan en anglais... spip-bonux pas nécessaire en SPIP 3.0 car boucle POUR dans les itérateurs du core
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@55290 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spip_piwik/spip_piwik_2_0/lang/paquet-piwik_en.php
+++ spip_piwik/spip_piwik_2_0/lang/paquet-piwik_en.php
@@ -14,5 +14,6 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 // P
 	'piwik_description' => 'Adds the Piwik statistics script (web traffic analyzer) on the pages of the site.',
+	'piwik_slogan' => 'Link [SPIP->http://www.spip.net] and [Piwik->http://www.piwik.org]',
 );
 ?>
