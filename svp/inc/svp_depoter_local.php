8bf783fd7049be3adf5c26030e8a194729da9c65
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-12-10 10:36:24 +0000

    Début du merge de stp et svp : déclaration de la base et fonctions de dépotage des plugins locaux
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@55318 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ svp/inc/svp_depoter_local.php
@@ -0,0 +1,129 @@
+<?php
+
+
+function stp_actualiser_paquets_locaux() {
+
+	spip_timer('paquets_locaux');
+	$paquets = stp_descriptions_paquets_locaux();
+	$hash = md5(serialize($paquets));
+	include_spip('inc/config');
+	if (lire_config('stp/hash_local') != $hash) {
+		stp_base_supprimer_paquets_locaux();
+		stp_base_inserer_paquets_locaux($paquets);
+		ecrire_config('stp/hash_local', $hash);
+	}
+	$temps = spip_timer('paquets_locaux');
+
+	return "Éxécuté en : " . $temps . "<br />";
+	
+}
+
+
+
+function stp_descriptions_paquets_locaux() {
+	include_spip('inc/plugin');
+	liste_plugin_files(_DIR_PLUGINS);
+	liste_plugin_files(_DIR_EXTENSIONS);
+	$get_infos = charger_fonction('get_infos', 'plugins');
+	$res = array(
+		'_DIR_PLUGINS'    => $get_infos(array(), false, _DIR_PLUGINS),
+		'_DIR_EXTENSIONS' => $get_infos(array(), false, _DIR_EXTENSIONS),
+	);
+	if (defined('_DIR_PLUGINS_SUPP') and _DIR_PLUGINS_SUPP) {
+		liste_plugin_files(_DIR_PLUGINS_SUPP);
+		$res['_DIR_PLUGINS_SUPP'] = $get_infos(array(), false, _DIR_PLUGINS_SUPP);
+	}
+	return $res;
+}
+
+
+// supprime les paquets et plugins locaux.
+function stp_base_supprimer_paquets_locaux() {
+	sql_delete('spip_paquets', 'id_depot = ' . 0); //_paquets locaux en 0
+	sql_delete('spip_plugins', sql_in('id_plugin', sql_get_select('DISTINCT(id_plugin)', 'spip_paquets'), 'NOT'));
+}
+
+
+function stp_base_inserer_paquets_locaux($paquets_locaux) {
+	include_spip('inc/svp_depoter');
+	
+	// On initialise les informations specifiques au paquet :
+	// l'id du depot et les infos de l'archive
+	$paquet_base = array(
+		'id_depot' => 0,
+		'nom_archive' => '',
+		'nbo_archive' => '',
+		'maj_archive' => '',
+		'src_archive' => '',
+		'date_modif' => '',
+	);
+
+	$preparer_sql_paquet = charger_fonction('preparer_sql_paquet', 'plugins');
+
+	// pour chaque decouverte, on insere les paquets en base.
+	// on evite des requetes individuelles, tres couteuses en sqlite...
+	$cle_plugins    = array(); // prefixe => id
+	$insert_plugins = array(); // insertion prefixe...
+	$insert_paquets = array(); // insertion de paquet...
+	
+	foreach($paquets_locaux as $const_dir => $paquets) {
+		foreach ($paquets as $paquet) {
+			$le_paquet = $paquet_base;
+
+			#$le_paquet['traductions'] = serialize($paquet['traductions']);
+
+			if ($champs = $preparer_sql_paquet($paquet)) {
+
+				// Eclater les champs recuperes en deux sous tableaux, un par table (plugin, paquet)
+				$champs = eclater_plugin_paquet($champs);
+				$paquet_plugin = true;
+				
+				// On complete les informations du paquet et du plugin
+				$le_paquet = array_merge($le_paquet, $champs['paquet']);
+				$le_plugin = $champs['plugin'];
+
+				// On loge l'absence de categorie ou une categorie erronee et on positionne la categorie par defaut "aucune"
+				if (!$le_plugin['categorie']) {
+					$le_plugin['categorie'] = 'aucune';
+				} else {
+					if (!in_array($le_plugin['categorie'], $GLOBALS['categories_plugin'])) {
+						$le_plugin['categorie'] = 'aucune';
+					}
+				}
+
+				// creation du plugin...
+				$prefixe = $le_plugin['prefixe'];
+				if (!isset($cle_plugins[$prefixe])) {
+					if (!$id_plugin = sql_getfetsel('id_plugin', 'spip_plugins', 'prefixe = '.sql_quote($prefixe))) {
+						$insert_plugins[$prefixe] = $le_plugin;
+					} else {
+						$cle_plugins[$prefixe] = $id_plugin;
+					}
+				}
+
+				// ajout du prefixe dans le paquet, supprime avant insertion...
+				$le_paquet['prefixe'] = $prefixe;				
+				$insert_paquets[] = $le_paquet;
+			}
+		}
+	}
+
+	if ($insert_plugins) {
+		sql_insertq_multi('spip_plugins', $insert_plugins);
+		$pls = sql_allfetsel(array('id_plugin', 'prefixe'), 'spip_plugins', sql_in('prefixe', array_keys($insert_plugins)));
+		foreach ($pls as $p) {
+			$cle_plugins[$p['prefixe']] = $p['id_plugin'];
+		}
+	}
+	
+	if ($insert_paquets) {
+
+		foreach ($insert_paquets as $c => $p) {
+			$insert_paquets[$c]['id_plugin'] = $cle_plugins[$p['prefixe']];
+			unset($insert_paquets[$c]['prefixe']);
+		}
+		sql_insertq_multi('spip_paquets', $insert_paquets);
+	}
+}
+
+?>
