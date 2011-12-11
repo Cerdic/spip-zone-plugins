c0e857336729b24ce44b6c289cf5f2839879cffb
marcimat@rezo.net <marcimat@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-12-11 15:01:48 +0000

    Installer les plugins en passant sur la page d'admin plugins.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@55393 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ svp/prive/squelettes/contenu/admin_plugin_fonctions.php
@@ -0,0 +1,6 @@
+<?php
+	// on installe les plugins maintenant,
+	// cela permet aux scripts d'install de faire des affichages (moches...)
+	include_spip('inc/plugin');
+	plugin_installes_meta();
+?>
