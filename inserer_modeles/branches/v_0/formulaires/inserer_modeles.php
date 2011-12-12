385c0a2fbb3ad51bdbca3206d706e5f0dccb180c
joseph@larmarange.net <joseph@larmarange.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-12-12 23:33:15 +0000

    Copie de la branche en cours vers branche/v_0
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@55462 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ inserer_modeles/branches/v_0/formulaires/inserer_modeles.php
@@ -0,0 +1,112 @@
+<?php
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+function formulaires_inserer_modeles_charger_dist($id_article,$id_rubrique,$id_breve,$formulaire_modele='',$modalbox='') {
+	include_spip('inc/inserer_modeles');
+	$contexte = array();
+	
+	if ((!_request('formulaire_modele') && $formulaire_modele=='') || _request('annuler')) {
+		$contexte['_liste_formulaires_modeles'] = inserer_modeles_lister_formulaires_modeles();
+	} else {
+		if ($formulaire_modele!='')
+			$contexte['ne_pas_afficher_bouton_annuler'] = 'on';
+		if ($formulaire_modele=='')
+			$formulaire_modele = _request('formulaire_modele');
+		$infos_modele = charger_infos_formulaire_modele($formulaire_modele);
+		include_spip('inc/saisies');
+		$champs_saisies = saisies_charger_champs($infos_modele['parametres']);
+		// On charge les valeurs éventuellement passées par l'url
+		foreach($champs_saisies as $champ => $val) {
+			if (_request($champ))
+				$champs_saisies[$champ] = _request($champ);
+		}
+		$contexte = array_merge($contexte,$champs_saisies);
+		
+		$contexte['formulaire_modele'] = $formulaire_modele;
+		$contexte['_nom'] = _T_ou_typo($infos_modele['nom']);
+		$contexte['logo'] = $infos_modele['logo'];
+		$contexte['_saisies'] = $infos_modele['parametres'];
+		if (_request('_code_modele'))
+			$contexte['_code_modele'] = _request('_code_modele');
+	}
+	
+	if (is_numeric($id_article))
+		$contexte['id_article'] = $id_article;
+	if (is_numeric($id_rubrique))
+		$contexte['id_rubrique'] = $id_rubrique;
+	if (is_numeric($id_breve))
+		$contexte['id_breve'] = $id_breve;
+	if ($modalbox!='') {
+		$contexte['modalbox'] = 'oui';
+		$_modalbox_retour = url_absolue(generer_url_ecrire('inserer_modeles','',true));
+		if (substr($formulaire_modele,-5)=='.yaml')
+				$formulaire_modele = substr($formulaire_modele,0,-5);
+		$_modalbox_retour = parametre_url($_modalbox_retour,'formulaire_modele',$formulaire_modele,'&');
+		if (is_numeric($id_article))
+			$_modalbox_retour = parametre_url($_modalbox_retour,'id_article',$id_article,'&');
+		if (is_numeric($id_rubrique))
+			$_modalbox_retour = parametre_url($_modalbox_retour,'id_rubrique',$id_rubrique,'&');
+		if (is_numeric($id_breve))
+			$_modalbox_retour = parametre_url($_modalbox_retour,'id_breve',$id_breve,'&');
+		$contexte['_modalbox_retour'] = $_modalbox_retour;
+	}
+	
+	return $contexte;
+}
+
+function formulaires_inserer_modeles_verifier_dist() {
+	$erreurs = array();
+	
+	if (_request('choisir') && !_request('formulaire_modele'))
+		$erreurs['message_erreur'] = _T('inserer_modeles:erreur_choix_modele');
+	
+	if (_request('inserer')) {
+		include_spip('inc/saisies');
+		include_spip('inc/inserer_modeles');
+		$infos = charger_infos_formulaire_modele(_request('formulaire_modele'));
+		$erreurs = saisies_verifier($infos['parametres']);
+	}
+	spip_log("formulaires_inserer_modeles_verifier_dist "._request('id_modele'),"bug");
+	return $erreurs;
+}
+
+function formulaires_inserer_modeles_traiter_dist() {
+	if (_request('inserer')) {
+		include_spip('inc/saisies');
+		include_spip('inc/inserer_modeles');
+		$infos = charger_infos_formulaire_modele(_request('formulaire_modele'));
+		$champs = saisies_lister_champs($infos['parametres'],false);
+		if(isset($infos['traiter']))
+			$f = charger_fonction($infos['traiter'],'formulaires',true);
+		else $f=false;
+		if ($f)
+			$code = $f($champs);
+		else {
+			$code = '<'._request('modele');
+			if (_request('id_modele') && _request('id_modele')!='')
+				$code .= _request('id_modele');
+			if (_request('variante') && _request('variante')!='')
+				$code .= '|'._request('variante');
+			if (_request('classe') && _request('classe')!='')
+				$code .= '|'._request('classe');
+			if (_request('align') && _request('align')!='')
+				$code .= '|'._request('align');
+			foreach ($champs as $champ) {
+				if($champ != 'modele' && $champ != 'variante' && $champ != 'classe' && $champ != 'id_modele' && $champ != 'align' && _request($champ) && _request($champ)!='') {
+					if($champ == _request($champ))
+						$code .= "|$champ";
+					// On transforme les tableaux en une liste
+					elseif (is_array(_request($champ)))
+						$code .= "|$champ=".implode(',',_request($champ));
+					else
+						$code .= "|$champ="._request($champ);
+				}
+			}
+			$code .= '>';
+		}
+		set_request('_code_modele',$code);
+		return array('message_ok' => _T('inserer_modeles:message_copier_code'));
+	}
+}
+
+?>
