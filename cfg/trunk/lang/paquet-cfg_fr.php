52bb4bec248482206d6927579035402411d86071
marcimat@rezo.net <marcimat@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-12-13 16:45:19 +0000

    Portage de CFG pour SPIP 3...
    On supprime TOUT :)
    IL faut migrer (fonds/ et formulaires/x.html anciens) vers de nouveaux horizons en SPIP 3. Voir le fichier documentation.txt pour des renseignements dessus.
    
    On ne garde que la methode de stockage "php" qui me semble la seule qui peut être encore utile.
    De toutes façons, les stockage sont extensibles, donc, ceux qui voudront refaire "table" et "tablepack" le pourront.
    
    Il reste que le stockage php est fait dans local/cfg et ce n'est pas top.
    Il faudrait les migrer dans config/cfg/ par exemple pour éviter des écrasements non souhaités.
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@55493 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ cfg/trunk/lang/paquet-cfg_fr.php
@@ -0,0 +1,12 @@
+<?php
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// C
+	'cfg_description' => 'Augmente les possibilités de gestion de configuration pour d\'autres plugins.',
+	'cfg_slogan' => 'Gestion de configurations.',
+	'cfg_titre' => 'CFG',
+);
+?>
