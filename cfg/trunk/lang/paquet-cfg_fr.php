ced8cd918bb001d86d3f5462e6858c52321160eb
marcimat@rezo.net <marcimat@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-12-13 17:48:32 +0000

    Commentaire
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@55494 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- cfg/trunk/lang/paquet-cfg_fr.php
+++ cfg/trunk/lang/paquet-cfg_fr.php
@@ -5,7 +5,14 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 // C
-	'cfg_description' => 'Augmente les possibilités de gestion de configuration pour d\'autres plugins.',
+	'cfg_description' => '
+		Augmente les possibilités de gestion de configuration pour d\'autres plugins
+		en fournissant un mode de stockage PHP.
+
+		Attention : une parte du fonctionnement de CFG pour SPIP 2 a été intégré dans SPIP 3 de façon
+		légèrement différente. Il est nécessaire de migrer les plugins qui utilisaient CFG pour SPIP 2.
+		La plupart n\'auront plus besoin de ce plugin pour gérer leur configurations.',
+			
 	'cfg_slogan' => 'Gestion de configurations.',
 	'cfg_titre' => 'CFG',
 );
