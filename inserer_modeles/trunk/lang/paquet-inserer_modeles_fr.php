8b6e924f5c10b3713ec84c41898d2752eace6487
joseph@larmarange.net <joseph@larmarange.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-12-13 00:17:15 +0000

    Début du passage à SPIP 3 : paquet.xml, necessite mediabox, icônes fatcow
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@55466 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ inserer_modeles/trunk/lang/paquet-inserer_modeles_fr.php
@@ -0,0 +1,18 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+// Fichier produit par PlugOnet
+// Module: paquet-inserer_modeles
+// Langue: fr
+// Date: 12-12-2011 23:52:09
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// I
+	'inserer_modeles_description' => 'Ce plugin fournit une aide à l\'insertion des modèles dans vos textes à travers des formulaires de saisies. Ces formulaires sont accessibles dans la colonne de droite ou via un bouton dans le porte-plume.',
+	'inserer_modeles_slogan' => 'Une aide à l\'insertion des modèles dans vos textes',
+);
+?>
