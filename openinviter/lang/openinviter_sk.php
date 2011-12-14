61d67f2238323d34028a64957587d6efdd7f12a4
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-12-14 11:30:05 +0000

    langues (openinviter)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@55533 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- openinviter/lang/openinviter_sk.php
+++ openinviter/lang/openinviter_sk.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// extrait automatiquement de http://www.spip.net/trad-lang/
+// extrait automatiquement de http://trad.spip.org
 // ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
@@ -8,7 +8,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// C
-	'champ_email_label' => 'Vaša e-mailová adresa @&ndash; poskytovateľ@',
+	'champ_email_label' => 'Vaša e-mailová adresa @– poskytovateľ@',
 	'champ_password_explication' => 'Nebudeme si ho pamätať.',
 	'champ_password_label' => 'Heslo',
 
