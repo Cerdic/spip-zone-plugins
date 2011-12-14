cd6d8e7e6e83c000dad97b7fe63358933859a306
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-12-14 11:30:09 +0000

    langues (parrainage)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@55537 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- parrainage/trunk/lang/parrainage_fr.php
+++ parrainage/trunk/lang/parrainage_fr.php
@@ -6,16 +6,16 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
-	'ajouter_filleul_confirmation' => 'Le contact a bien &eacute;t&eacute; ajout&eacute;, vous pourrez ensuite inviter cette personne.',
+	'ajouter_filleul_confirmation' => 'Le contact a bien été ajouté, vous pourrez ensuite inviter cette personne.',
 
 	// C
-	'configuration_delai_sans_nouvelles_label' => 'D&eacute;lai pour le statut &quot;Sans nouvelles&quot;',
+	'configuration_delai_sans_nouvelles_label' => 'Délai pour le statut &quot;Sans nouvelles&quot;',
 	'configuration_invitation_obligatoire_label' => 'Invitation obligatoire',
 	'configuration_titre' => 'Configuration du parrainage',
 
 	// E
-	'erreur_aucun_filleul' => 'Vous n\'avez s&eacute;lectionn&eacute; aucun filleul.',
-	'erreur_invitation_invalide' => 'Votre invitation ne correspond pas &agrave; l\'adresse de courriel indiqu&eacute;e.',
+	'erreur_aucun_filleul' => 'Vous n\'avez sélectionné aucun filleul.',
+	'erreur_invitation_invalide' => 'Votre invitation ne correspond pas à l\'adresse de courriel indiquée.',
 	'erreur_invitation_obligatoire' => 'Vous devez avoir une invitation pour vous inscrire sur le site.',
 
 	// F
@@ -27,38 +27,38 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// I
 	'inscription_code_invitation_label' => 'Code d\'invitation',
-	'invitation_message' => '@nom_parrain@ vous invite &agrave; rejoindre le site @site@.',
+	'invitation_message' => '@nom_parrain@ vous invite à rejoindre le site @site@.',
 	'invitation_sujet' => '@nom@ vous invite à rejoindre le site @site@',
-	'invitation_url' => 'Rendez-vous &agrave; l\'adresse suivante pour vous inscrire :',
+	'invitation_url' => 'Rendez-vous à l\'adresse suivante pour vous inscrire :',
 
 	// P
 	'parrainage_inviter' => 'Envoyer l\'invitation',
-	'parrainage_message_aucun' => 'Aucune de ces personnes n\'a besoin d\'&ecirc;tre invit&eacute;e.',
-	'parrainage_message_aucun_1' => 'Cette personne est déjà invit&eacute;e ou inscrite.',
-	'parrainage_message_erreur' => 'Au moins une invitation n\'a pas &eacute;t&eacute; envoy&eacute;e correctement.',
+	'parrainage_message_aucun' => 'Aucune de ces personnes n\'a besoin d\'être invitée.',
+	'parrainage_message_aucun_1' => 'Cette personne est déjà invitée ou inscrite.',
+	'parrainage_message_erreur' => 'Au moins une invitation n\'a pas été envoyée correctement.',
 	'parrainage_message_label' => 'Votre message',
-	'parrainage_message_ok_pluriel' => '@nombre@ invitations ont bien &eacute;t&eacute; programm&eacute;es, elles sont maintenant en cours d\'envoi.',
-	'parrainage_message_ok_singulier' => 'Une invitation a bien &eacute;t&eacute; programm&eacute;e, elle est maintenant en cours d\'envoi.',
+	'parrainage_message_ok_pluriel' => '@nombre@ invitations ont bien été programmées, elles sont maintenant en cours d\'envoi.',
+	'parrainage_message_ok_singulier' => 'Une invitation a bien été programmée, elle est maintenant en cours d\'envoi.',
 	'parrainage_supprimer_filleul' => 'Supprimer ce filleul',
-	'parrainage_supprimer_filleul_confirmation' => '&Ecirc;tes-vous s&ucirc;r de vouloir supprimer @nom@ de vos contacts ?',
+	'parrainage_supprimer_filleul_confirmation' => 'Êtes-vous sûr de vouloir supprimer @nom@ de vos contacts ?',
 	'plugin_nom' => 'Parrainage',
 
 	// S
 	'statut_contact' => 'Contact',
-	'statut_contact_explication' => 'Cette personne est dans votre carnet d\'adresse. Vous pouvez l\'inviter &agrave; rejoindre le site.',
-	'statut_deja_inscrit' => 'D&eacute;j&agrave; inscrit',
-	'statut_deja_inscrit_explication' => 'Cette personne est d&eacute;j&agrave; inscrite sur le site, mais n\'est pas votre filleul !',
+	'statut_contact_explication' => 'Cette personne est dans votre carnet d\'adresse. Vous pouvez l\'inviter à rejoindre le site.',
+	'statut_deja_inscrit' => 'Déjà inscrit',
+	'statut_deja_inscrit_explication' => 'Cette personne est déjà inscrite sur le site, mais n\'est pas votre filleul !',
 	'statut_en_cours' => 'Invitation en cours d\'envoi',
-	'statut_en_cours_explication' => 'Votre message d\'invitation est en cours d\'exp&eacute;dition.',
+	'statut_en_cours_explication' => 'Votre message d\'invitation est en cours d\'expédition.',
 	'statut_filleul' => 'Filleul',
 	'statut_filleul_depuis' => 'Filleul inscrit depuis @date@',
-	'statut_filleul_explication' => 'Gr&acirc;ce &agrave; vous, cette personne s\'est inscrite sur le site : vous &ecirc;tes son parrain.',
-	'statut_invite' => 'Invit&eacute; @duree@',
-	'statut_invite_explication' => 'Vous avez d&eacute;j&agrave; invit&eacute; cette personne il y a peu de temps.',
+	'statut_filleul_explication' => 'Grâce à vous, cette personne s\'est inscrite sur le site : vous êtes son parrain.',
+	'statut_invite' => 'Invité @duree@',
+	'statut_invite_explication' => 'Vous avez déjà invité cette personne il y a peu de temps.',
 	'statut_sans_nouvelles' => 'Pas de nouvelles depuis @date@',
-	'statut_sans_nouvelles_explication' => 'Vous avez invit&eacute; cette personne il y a un certain temps, mais elle n\'est jamais venue.',
-	'statut_visite' => 'A visit&eacute; le site',
-	'statut_visite_explication' => 'Cette personne a d&eacute;j&agrave; suivi votre invitation &agrave; venir sur le site, mais ne s\'est pas encore inscrite.'
+	'statut_sans_nouvelles_explication' => 'Vous avez invité cette personne il y a un certain temps, mais elle n\'est jamais venue.',
+	'statut_visite' => 'A visité le site',
+	'statut_visite_explication' => 'Cette personne a déjà suivi votre invitation à venir sur le site, mais ne s\'est pas encore inscrite.'
 );
 
 ?>
