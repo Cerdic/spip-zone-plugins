9098603252cba9e511acc4e5189da545c021be84
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-12-16 18:35:10 +0000

    Changement de slogan et description. SVP devient SerVeur de Plugins
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@55633 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- svp/lang/paquet-svp_fr.php
+++ svp/lang/paquet-svp_fr.php
@@ -9,8 +9,8 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 // S
-	'svp_description' => 'Ce plugin fournit une API permettant de collecter, d\'effectuer des recherches multi-critères et de présenter les informations 
-	sur les plugins SPIP - modules fonctionnels, thèmes et squelettes. Cette API sert aussi de base au plugin STP.',
-	'svp_slogan' => 'SerVeur d\'information sur les Plugins',
+	'svp_description' => 'Ce plugin fournit, d\'une part,  une API permettant de collecter, d\'effectuer des recherches multi-critères et de présenter les informations
+	sur les plugins SPIP - modules fonctionnels, thèmes et squelettes, et d\'autre part, une nouvelle interface d\'administration des plugins gérant les dépendances entre plugins.',
+	'svp_slogan' => 'SerVeur d\'information et de téléchargement des Plugins',
 );
 ?>
