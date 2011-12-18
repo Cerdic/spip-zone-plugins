7a3c9e252833966a5cd9481393663e68f4c045ca
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-12-18 15:21:16 +0000

    Corriger les accents dans la description.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@55705 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- acces_restreint/trunk/lang/paquet-accesrestreint_fr.php
+++ acces_restreint/trunk/lang/paquet-accesrestreint_fr.php
@@ -1,11 +1,8 @@
 <?php
 
 // Ceci est un fichier langue de SPIP -- This is a SPIP language file
-// Fichier produit par PlugOnet
 // Module: paquet-accesrestreint
 // Langue: fr
-// Date: 18-10-2011 17:20:21
-// Items: 3
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
@@ -13,8 +10,10 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 // A
 	'accesrestreint_description' => '_ Chaque zone contient des rubriques.
-_ Les auteurs peuvent etre associes a des zones pour avoir le droit d\'y acceder.
-_ Toutes les boucles natives de SPIP sont surchargees pour en filtrer les resultats en fonction des droits du visiteur.',
+
+_ Les auteurs peuvent etre associés à des zones pour avoir le droit d\'y accéder.
+
+_ Toutes les boucles natives de SPIP sont surchargées pour en filtrer les résultats en fonction des droits du visiteur.',
 	'accesrestreint_nom' => 'Acces Restreint',
 	'accesrestreint_slogan' => 'Gestion de zones d\'acces restreint',
 );
