a467d30b78e0eabc2c56beecf630c3f72220c764
brunobergot@gmail.com <brunobergot@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-12-19 23:34:21 +0000

    bourrinner les entites html < &#128;
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@55774 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- delicious2seenthis/del2seen_pipelines.php
+++ delicious2seenthis/del2seen_pipelines.php
@@ -6,7 +6,8 @@ function del2seen_post_syndication($flux){
 	$config = @unserialize($GLOBALS['meta']['del2seen']);
 	if ($flux[1] == $config['id_site_delicious'] AND intval($flux[3])) {
 		// préparer les données du seen
-		$titre = $flux[2]['titre'];
+		// unicode2charset ne traite pas les entites < &#128;
+		$titre = html_entity_decode($flux[2]['titre'],ENT_QUOTES,$GLOBALS['meta']['charset']);
 		$link = $flux[2]['url'];
 		$comment = $flux[2]['descriptif'];
 		foreach($flux[2]['tags'] as $tag) {
