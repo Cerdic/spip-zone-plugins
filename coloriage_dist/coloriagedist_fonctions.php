c2a0c1ef33132543bebf71c5a4a7fd6913aa325b
erational@erational.org <erational@erational.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-12-19 09:16:33 +0000

    CSS dynamique du coloriage intégrable au compresseur CSS (merci marcimat)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@55726 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- coloriage_dist/coloriagedist_fonctions.php
+++ coloriage_dist/coloriagedist_fonctions.php
@@ -1,11 +1,12 @@
 <?php
-
+if (!defined("_ECRIRE_INC_VERSION")) return;
 //
 // functions
 //
 
 function coloriagedist_insert_head($flux){
-  $flux .= "<link rel='stylesheet' type='text/css' href='?page=css_coloriage' />\n";   
+  $url_css = generer_url_public("css_coloriage");
+  $flux .= "<link rel='stylesheet' type='text/css' media='all' href='$url_css' />\n";    
   return $flux;
 }
 
