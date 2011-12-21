6765f25b93bac3c1a6ca57d1edc726f92c23041f
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-12-21 22:55:49 +0000

    la branche 1.2, recherché dans les archives
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@55841 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ palette/branches/1_2/lang/palette_fr.php
@@ -0,0 +1,14 @@
+<?php
+// Ceci est un fichier langue de SPIP
+$GLOBALS[$GLOBALS['idx_lang']] = array( // Espace ecrire
+	'palette' => "Palette",
+	'fermer' => 'Fermer la palette',
+	'palette_public' => "Active Palette sur le site public",
+	'palette_ecrire' => "Active Palette pour l'espace ecrire",
+	'config_titre' => '<h4>Configuration du plugin Palette</h4>',
+	'config_desc' => '<p>Palette permet d\'ajouter automatiquement un s&eacute;lecteur de couleurs &agrave; un champ input, simplement en lui donnant la classe <strong>palette</strong>:</p>',
+	'config_exemple' => 'Exemple : ',
+	
+);
+
+?>
