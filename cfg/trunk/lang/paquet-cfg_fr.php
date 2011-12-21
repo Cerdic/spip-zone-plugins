95d750ea96ee7bbf90f5bbde74941cd2e58e08e9
brunobergot@gmail.com <brunobergot@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-12-21 17:43:19 +0000

    orthographe
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@55813 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- cfg/trunk/lang/paquet-cfg_fr.php
+++ cfg/trunk/lang/paquet-cfg_fr.php
@@ -9,7 +9,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 		Augmente les possibilités de gestion de configuration pour d\'autres plugins
 		en fournissant un mode de stockage PHP.
 
-		Attention : une parte du fonctionnement de CFG pour SPIP 2 a été intégré dans SPIP 3 de façon
+		Attention : une partie du fonctionnement de CFG pour SPIP 2 a été intégrée dans SPIP 3 de façon
 		légèrement différente. Il est nécessaire de migrer les plugins qui utilisaient CFG pour SPIP 2.
 		La plupart n\'auront plus besoin de ce plugin pour gérer leur configurations.',
 			
