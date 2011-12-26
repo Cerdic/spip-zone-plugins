0e8cd713f1fb92f47e8329952c94ac77bdc578b5
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-12-26 19:58:12 +0000

    début migration vers SPIP 3 : étape 1 : passer en paquet.xml ..…la suite après les vacances
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@56031 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ palette/trunks/lang/paquet-palette_en.php
@@ -0,0 +1,19 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-palette
+// Langue: en
+// Date: 26-12-2011 20:52:48
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// P
+	'palette_description' => 'Set class palette to an input to add a color picker: <code><input class="palette" name="couleur" value="#dfdfdf" /></code>',
+	'palette_slogan' => 'Color picker',
+);
+?>
