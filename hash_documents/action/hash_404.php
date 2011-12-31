37088c2e92e81a9ff342e89f52a3f252b2e9201c
yffic@lefourneau.com <yffic@lefourneau.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-12-31 11:55:13 +0000

    - Le contenu du fichier action/action_404.php doit se trouver dans la fonction action_hash_404_dist(). Mais comment ça pouvait fonctionner avant ?! - L'appel à action_404.php doit se faire depuis la racine du site et non depuis le dossier ecrire => modif de l'exemple du .htaccess et des chemins de test d'existence des fichiers
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@56141 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- hash_documents/action/hash_404.php
+++ hash_documents/action/hash_404.php
@@ -7,46 +7,47 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 
 RewriteEngine On
 RewriteCond %{REQUEST_FILENAME} !-f
-# Si spip est range a la racine du domaine ou pour un mutualise
-RewriteRule .* /ecrire/?action=hash_404 [L]
-# Si spip est range dans un sous dossier spip
-#RewriteRule .* /spip/ecrire/?action=hash_404 [L]
+# Si spip est range a la racine du domaine
+RewriteRule .* ../index.php?action=hash_404 [L] 
+# Pour un mutualise, vaut mieux travailler en url absolue
+#RewriteRule .* /index.php?action=hash_404 [L]
 
 */
+function action_hash_404_dist(){
 
-include_spip("hash_fonctions");
-$doc = preg_replace(',^.*?IMG/,', '', $_SERVER['REQUEST_URI']);
-
-if (($dest = hasher_adresser_document($doc)
-AND file_exists('../'.$GLOBALS['meta']['dir_img'].$dest))
-OR ($dest = hasher_adresser_document($doc, true)
-AND file_exists('../'.$GLOBALS['meta']['dir_img'].$dest))
-) {
-	$url = 'http://'.$_SERVER['HTTP_HOST'].preg_replace(',^(.*?IMG/).*,', '\1', $_SERVER['REQUEST_URI']).$dest;
-	header('HTTP/1.1 301 Moved Permanently');
-	header('Location: '.$url);
-	echo '<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
-<html><head>
-<title>301 Moved Permanently</title>
-</head><body>
-<h1>Moved Permanently</h1>
-<p>The requested URL '.htmlspecialchars($_SERVER['REQUEST_URI']).' has moved to <a href="'.$url.'">'.$url.'</a>.</p>
-<hr>
-'.$_SERVER['SERVER_SIGNATURE'].'
-</body></html>
-';
+	include_spip("hash_fonctions");
+	$doc = preg_replace(',^.*?IMG/,', '', $_SERVER['REQUEST_URI']);
+	
+	if (($dest = hasher_adresser_document($doc)
+	AND file_exists('./'.$GLOBALS['meta']['dir_img'].$dest))
+	OR ($dest = hasher_adresser_document($doc, true)
+	AND file_exists('./'.$GLOBALS['meta']['dir_img'].$dest))
+	) {
+		$url = 'http://'.$_SERVER['HTTP_HOST'].preg_replace(',^(.*?IMG/).*,', '\1', $_SERVER['REQUEST_URI']).$dest;
+		header('HTTP/1.1 301 Moved Permanently');
+		header('Location: '.$url);
+		echo '<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+	<html><head>
+	<title>301 Moved Permanently</title>
+	</head><body>
+	<h1>Moved Permanently</h1>
+	<p>The requested URL '.htmlspecialchars($_SERVER['REQUEST_URI']).' has moved to <a href="'.$url.'">'.$url.'</a>.</p>
+	<hr>
+	'.$_SERVER['SERVER_SIGNATURE'].'
+	</body></html>
+	';
+	}
+	else {
+		echo '<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+	<html><head>
+	<title>404 Not Found</title>
+	</head><body>
+	<h1>Not Found</h1>
+	<p>The requested URL '.htmlspecialchars($_SERVER['REQUEST_URI']).' was not found on this server.</p>
+	<hr>
+	'.$_SERVER['SERVER_SIGNATURE'].'
+	</body></html>
+	';
+	}
 }
-else {
-	echo '<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
-<html><head>
-<title>404 Not Found</title>
-</head><body>
-<h1>Not Found</h1>
-<p>The requested URL '.htmlspecialchars($_SERVER['REQUEST_URI']).' was not found on this server.</p>
-<hr>
-'.$_SERVER['SERVER_SIGNATURE'].'
-</body></html>
-';
-}
-
 ?>
