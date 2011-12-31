360627b7cc2c6f2eea7c68c78c684391949b8428
marcel@bolla.fr <marcel@bolla.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2011-12-31 15:08:05 +0000

    Affichage conditionnel des boutons ...
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@56149 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- Association/Associaspip/balise/bouton_type_operation.php
+++ Association/Associaspip/balise/bouton_type_operation.php
@@ -36,38 +36,51 @@ function balise_BOUTON_TYPE_OPERATION_dyn($id_compte, $type_operation) {
 
 	$res .= "\n<div class='choix'>";
 
+	$pc_ok = FALSE; // controle l'existence de references comptables
+
 	$num_classe = $GLOBALS['association_metas']['classe_charges'];
-	$res .= "\n<input type='radio'";
-	$res .= (($id_compte && ($type_operation !== $num_classe)) ? " disabled='disabled' " : " ");
-	$res .= "class='radio' name='type_operation' value='" . $num_classe . "' id='type_operation_depense'";
-	$res .= (($type_operation == $num_classe) ? " checked='checked'" : "");
-	$res .= " onclick=\"remplirSelectImputation(" . $num_classe . ");
+	if(sql_countsel('spip_asso_plan', "classe='$num_classe'")) {
+		$pc_ok = TRUE;
+		$res .= "\n<input type='radio'";
+		$res .= (($id_compte && ($type_operation !== $num_classe)) ? " disabled='disabled' " : " ");
+		$res .= "class='radio' name='type_operation' value='" . $num_classe . "' id='type_operation_depense'";
+		$res .= (($type_operation == $num_classe) ? " checked='checked'" : "");
+		$res .= " onclick=\"remplirSelectImputation(" . $num_classe . ");
 		afficheDiv('label_imputation','label_depense','depense','label_journal_depense','mode_paiement','justification','destination');
-		cacheDiv('label_destination','label_depense_evaluee','label_journal_recette','recette'); \" />";
-	$res .= "\n<label for='type_operation_depense'>D&eacute;pense</label>";
+			cacheDiv('label_destination','label_depense_evaluee','label_journal_recette','recette'); \" />";
+		$res .= "\n<label for='type_operation_depense'>D&eacute;pense</label>";
+	}
 
 	$num_classe = $GLOBALS['association_metas']['classe_produits'];
-	$res .= "\n<input type='radio'";
-	$res .= (($id_compte && ($type_operation !== $num_classe)) ? " disabled='disabled' " : " ");
-	$res .= "class='radio' name='type_operation' value='" . $num_classe . "' id='type_operation_recette'";
-	$res .= (($type_operation == $num_classe) ? " checked='checked'" : "");
-	$res .= " onclick=\"remplirSelectImputation(" . $num_classe . ");
+	if(sql_countsel('spip_asso_plan', "classe='$num_classe'")) {
+		$pc_ok = TRUE;
+		$res .= "\n<input type='radio'";
+		$res .= (($id_compte && ($type_operation !== $num_classe)) ? " disabled='disabled' " : " ");
+		$res .= "class='radio' name='type_operation' value='" . $num_classe . "' id='type_operation_recette'";
+		$res .= (($type_operation == $num_classe) ? " checked='checked'" : "");
+		$res .= " onclick=\"remplirSelectImputation(" . $num_classe . ");
 		afficheDiv('label_imputation','label_recette','recette','label_journal_recette','mode_paiement','justification','destination');
-		cacheDiv('label_destination','label_recette_evaluee','label_journal_depense','depense'); \" />";
-	$res .= "\n<label for='type_operation_recette'>Recette</label>";
+			cacheDiv('label_destination','label_recette_evaluee','label_journal_depense','depense'); \" />";
+		$res .= "\n<label for='type_operation_recette'>Recette</label>";
+	}
 
+	// TODO :  Si pas 2 classes financières différentes alors pas de virement interne
 	$num_classe = $GLOBALS['association_metas']['classe_banques'];
-	$res .= "\n<input type='radio'";
-	$res .= (($id_compte && ($type_operation !== $num_classe)) ? " disabled='disabled' " : " ");
-	$res .= "class='radio' name='type_operation' value='" . $num_classe . "' id='type_operation_virement'";
-	$res .= (($type_operation == $num_classe) ? " checked='checked'" : "");
-	$res .= " onclick=\"remplirSelectImputation(" . $num_classe . ");
+	if(sql_countsel('spip_asso_plan', "classe='$num_classe'")) {
+		$pc_ok = TRUE;
+		$res .= "\n<input type='radio'";
+		$res .= (($id_compte && ($type_operation !== $num_classe)) ? " disabled='disabled' " : " ");
+		$res .= "class='radio' name='type_operation' value='" . $num_classe . "' id='type_operation_virement'";
+		$res .= (($type_operation == $num_classe) ? " checked='checked'" : "");
+		$res .= " onclick=\"remplirSelectImputation(" . $num_classe . ");
 		afficheDiv('label_destination','label_depense','depense','label_journal_depense','mode_paiement','justification');
-		cacheDiv('label_imputation','label_depense_evaluee','label_journal_recette','recette','destination'); \" />";
-	$res .= "\n<label for='type_operation_virement'>Virement</label>";
+			cacheDiv('label_imputation','label_depense_evaluee','label_journal_recette','recette','destination'); \" />";
+		$res .= "\n<label for='type_operation_virement'>Virement</label>";
+	}
 
 	$num_classe = $GLOBALS['association_metas']['classe_contributions_volontaires'];
 	if(sql_countsel('spip_asso_plan', "classe='$num_classe'")) {
+		$pc_ok = TRUE;
 		$res .= "\n<input type='radio'";
 		$res .= (($id_compte && ($type_operation !== $num_classe)) ? " disabled='disabled' " : " ");
 		$res .= "class='radio' name='type_operation' value='" . $num_classe . "' id='type_operation_contribution_volontaire'";
@@ -77,6 +90,11 @@ function balise_BOUTON_TYPE_OPERATION_dyn($id_compte, $type_operation) {
 			cacheDiv('label_destination','label_depense','label_recette','mode_paiement'); \" />";
 		$res .= "\n<label for='type_operation_contribution_volontaire'>Contrib. volontaire</label>";
 	}
+
+	if(!$pc_ok) {
+		$res .= "<strong>" . _T('asso:erreur_creer_un_plan_pour_activer_ce_module')."</strong>";
+	}
+
 	$res .= "\n</div>";
 	$res .= "\n</li>";
 	
