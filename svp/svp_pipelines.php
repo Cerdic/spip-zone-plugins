dc6dddf34c7fd6fce6efbbbfaba922cafea608c2
marcimat@rezo.net <marcimat@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-01 14:58:03 +0000

    Testons pour Éric un filtrage des boucles (PAQUETS) et (PLUGINS) automatique : elles n'affichent pas par défaut les paquets et plugins locaux. Utiliser les critères {id_depot} ou {tout} pour annuler.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@56155 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- svp/svp_pipelines.php
+++ svp/svp_pipelines.php
@@ -40,4 +40,56 @@ function svp_ajouter_onglets($flux){
     return $flux;
 }
 
+
+/**
+ * On affiche dans les boucles (PLUGINS) (DEPOTS) et (PAQUETS)
+ * que les distants par defaut
+ * Utiliser id_depot= ou tout pour tout avoir.
+ *
+ * @param 
+ * @return 
+**/
+function svp_pre_boucle($boucle) {
+
+	// DEPOTS, PAQUETS
+	// Pour DEPOTS, on n'a jamais id_depot=0 dedans... donc... pas la peine.
+	if (
+		$boucle->type_requete == 'paquets'
+		# OR $boucle->type_requete == 'depots'
+	) {
+		$id_table = $boucle->id_table;
+		$m_id_depot = $id_table .'.id_depot';
+		// Restreindre aux depots distants
+		if (!isset($boucle->modificateur['criteres']['id_depot']) && 
+			!isset($boucle->modificateur['tout'])) {
+				$boucle->where[] = array("'>'", "'$m_id_depot'", "'\"0\"'");				
+		}
+	}
+	// PLUGINS
+	elseif ($boucle->type_requete == 'plugins') {
+		$id_table = $boucle->id_table;
+		// les modificateurs ne se creent que sur les champs de la table principale
+		// pas sur une jointure, il faut donc analyser les criteres passes pour
+		// savoir si l'un deux est un 'id_depot'...
+		// pff...
+		$id_depot = false;
+		foreach($boucle->criteres as $c){
+			if ($c->param[0][0]->texte == 'id_depot') {
+				$id_depot = true;
+				break;
+			}
+		}
+		if (!$id_depot && 
+			!isset($boucle->modificateur['tout'])) {
+				// Restreindre aux mots cles non techniques
+				$boucle->from["depots_plugins"] =  "spip_depots_plugins";
+				$boucle->where[] = array("'='", "'depots_plugins.id_plugin'", "'$id_table.id_plugin'");
+				$boucle->where[] = array("'>'", "'depots_plugins.id_depot'", "'\"0\"'");
+		}
+	}
+
+	
+	return $boucle;
+
+}
 ?>
