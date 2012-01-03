0812fde940182efd595e1241a9d5546c211d4fb7
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-03 19:45:33 +0000

    langues (paquet-tradlang)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@56383 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ trad-lang/trad-lang_spip2/lang/paquet-tradlang_sk.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// T
+	'tradlang_description' => 'Zásuvný modul na správu jazykových súborov priamo zo SPIPu.',
+	'tradlang_slogan' => 'Správa jazykových súborov'
+);
+
+?>
