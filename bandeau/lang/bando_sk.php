552a83636327058348d18f9861f63ed4afcfd275
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-03 11:04:13 +0000

    langues (bando)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@56218 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- bandeau/lang/bando_sk.php
+++ bandeau/lang/bando_sk.php
@@ -8,36 +8,36 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// I
-	'icone_accueil' => 'Launch pad', # MODIF
-	'icone_administration' => 'Maintenance', # MODIF
-	'icone_configuration' => 'Configuration', # MODIF
-	'icone_edition' => 'Site edit',
-	'icone_langage' => 'My language', # MODIF
-	'icone_mes_infos' => 'My informations', # MODIF
+	'icone_accueil' => 'Úvodná stránka',
+	'icone_administration' => 'Údržba',
+	'icone_configuration' => 'Konfigurácia',
+	'icone_edition' => 'Úpravy',
+	'icone_langage' => 'Môj jazyk',
+	'icone_mes_infos' => 'Moje údaje',
 	'icone_preferences' => 'My preferences',
-	'icone_reactions' => 'Activity',
-	'icone_squelette' => 'Templates', # MODIF
-	'icone_suivi_edito' => 'Publication', # MODIF
-	'icone_suivi_publication' => 'To follow the publication', # MODIF
-	'icone_visiter_site' => 'Visit', # MODIF
+	'icone_reactions' => 'Aktivita',
+	'icone_squelette' => 'Šablóny',
+	'icone_suivi_edito' => 'Publikovanie',
+	'icone_suivi_publication' => 'Sledovať publikovanie',
+	'icone_visiter_site' => 'Zobraziť verejne prístupnú stránku',
 
 	// L
-	'label_adresses_secondaires' => 'Adresy ALIASOV na verejnú stránku',
-	'label_bando_outils' => 'Panel s nástrojmi',
+	'label_adresses_secondaires' => 'Sekundárne adresy verejne prístupnej stránky',
+	'label_bando_outils' => 'Panel s nástrojmi',
 	'label_bando_outils_afficher' => 'Zobraziť nástroje',
 	'label_bando_outils_masquer' => 'Schovať nástroje',
-	'label_choix_langue' => 'Manage your language ', # MODIF
-	'label_slogan_site' => 'Slogan', # MODIF
-	'label_taille_ecran' => 'Display', # MODIF
-	'label_texte_et_icones_navigation' => 'Navigation menu', # MODIF
-	'label_texte_et_icones_page' => 'Page display', # MODIF
+	'label_choix_langue' => 'Vyberte si jazyk',
+	'label_slogan_site' => 'Slogan stránky',
+	'label_taille_ecran' => 'Šírka obrazovky',
+	'label_texte_et_icones_navigation' => 'Menu navigácie',
+	'label_texte_et_icones_page' => 'Zobrazenie stránky',
 
 	// T
-	'texte_redirection_adresses_secondaires' => 'Adresy ALIASOV sú alternatívne adresy, ktoré môžete použiť na prístup na svoju stránku.
+	'texte_redirection_adresses_secondaires' => 'Sekundárne adresy sú alternatívne adresy, ktoré môžete použiť pri prístupe na svoju stránku.
   Tieto adresy budú automaticky presmerované na vašu hlavnú adresu. Na každý riadok zadajte jednu adresu ',
-	'titre_config_langage' => 'Configurer la langue', # MODIF
-	'titre_configurer_preferences' => 'Configure your preferences',
-	'titre_identite_site' => 'Site\'s identity',
+	'titre_config_langage' => 'Nastaviť jazyk',
+	'titre_configurer_preferences' => 'Nastavte si svoje predvoľby',
+	'titre_identite_site' => 'Identita stránky',
 	'titre_infos_perso' => 'Moje osobné údaje'
 );
 
