6a5eea78aaccf7d80deeb5d631ff11f60b490bbb
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-03 14:07:15 +0000

    langues (openid)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@56321 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- authentification/openid/lang/openid_en.php
+++ authentification/openid/lang/openid_en.php
@@ -1,31 +1,35 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/authentification/openid/lang
-if (!defined("_ECRIRE_INC_VERSION")) return;
+// extrait automatiquement de http://trad.spip.org
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// E
-	'erreur_openid' => 'OpenID authentication error: have you entered a valid OpenID?',
 	'erreur_librairies' => 'Unable to locate OpenID authentification libraries',
+	'erreur_openid' => 'OpenID authentication error: have you entered a valid OpenID?',
+	'erreur_openid_info_manquantes' => 'Votre fournisseur OpenID ne nous a pas envoyé toutes les informations nécessaires', # NEW
 
 	// F
-	'form_login_openid' => 'You can use your <a href=\'http://en.wikipedia.org/wiki/OpenID\'>OpenID</a> as login',
 	'form_forum_indiquer_openid' => 'You can sign up on this site with your OpenID address',
+	'form_forum_openid' => 'Sign up with OpenID',
+	'form_login_openid' => 'You can use your <a href=\'http://en.wikipedia.org/wiki/OpenID\'>OpenID</a> as login',
+	'form_login_openid_inconnu' => 'Cette adresse OpenID est inconnue. Corrigez, ou enregistrez-la dans votre profil.', # NEW
 	'form_login_openid_ok' => 'This login uses OpenID.',
 	'form_login_openid_pass' => 'Connect with password.',
-	'form_forum_openid' => 'Sign up with OpenID',
+	'form_login_statut_nouveau' => 'Pour votre première connexion, vous devez utiliser le mot de passe qui vous a été envoyé par mail', # NEW
 	'form_pet_votre_openid' => 'OpenID login',
 
-	// L
-	'login_login2' => 'Login (identifier for connection to the site) or OpenID:',
-	
 	// O
 	'openid' => 'OpenID',
-	
+
 	// U
 	'utilisateur_inconnu' => 'Unknown user',
-	
+
 	// V
-	'verif_refusee' => 'Authentication request cancelled',
+	'verif_refusee' => 'Authentication request cancelled'
 );
+
+?>
--- authentification/openid/lang/openid_fr.php
+++ authentification/openid/lang/openid_fr.php
@@ -1,31 +1,33 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/authentification/openid/lang
-if (!defined("_ECRIRE_INC_VERSION")) return;
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/authentification/openid/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// E
-	'erreur_openid' => 'Cette adresse OpenID ne semble pas valide',
 	'erreur_librairies' => 'Impossible de trouver les librairies d\'authenfication OpenID',
-	'erreur_openid_info_manquantes' => 'Votre fournisseur OpenID ne nous a pas envoy&eacute; toutes les informations n&eacute;cessaires',
-	
+	'erreur_openid' => 'Cette adresse OpenID ne semble pas valide',
+	'erreur_openid_info_manquantes' => 'Votre fournisseur OpenID ne nous a pas envoyé toutes les informations nécessaires',
+
 	// F
-	'form_login_openid' => 'Vous pouvez aussi utiliser un identifiant OpenID (<a href="http://fr.wikipedia.org/wiki/OpenID" target="_blank">aide</a>)',
 	'form_forum_indiquer_openid' => 'Vous pouvez vous inscrire sur ce site en indiquant un identifiant OpenID.',
+	'form_forum_openid' => 'Inscription par OpenID',
+	'form_login_openid' => 'Vous pouvez aussi utiliser un identifiant OpenID (<a href="http://fr.wikipedia.org/wiki/OpenID" target="_blank">aide</a>)',
+	'form_login_openid_inconnu' => 'Cette adresse OpenID est inconnue. Corrigez, ou enregistrez-la dans votre profil.',
 	'form_login_openid_ok' => 'Cet identifiant utilise OpenID.',
 	'form_login_openid_pass' => 'Utiliser un mot de passe',
-	'form_forum_openid' => 'Inscription par OpenID',
+	'form_login_statut_nouveau' => 'Pour votre première connexion, vous devez utiliser le mot de passe qui vous a été envoyé par mail',
 	'form_pet_votre_openid' => 'Votre adresse OpenID',
-	'form_login_openid_inconnu' => 'Cette adresse OpenID est inconnue. Corrigez, ou enregistrez-la dans votre profil.',
-	'form_login_statut_nouveau' => 'Pour votre premi&egrave;re connexion, vous devez utiliser le mot de passe qui vous a &eacute;t&eacute; envoy&eacute; par mail',
-	
+
 	// O
 	'openid' => 'OpenID',
-	
+
 	// U
 	'utilisateur_inconnu' => 'Utilisateur inconnu sur ce site',
-	
+
 	// V
-	'verif_refusee' => 'V&eacute;rification refus&eacute;e'
+	'verif_refusee' => 'Vérification refusée'
 );
+
+?>
