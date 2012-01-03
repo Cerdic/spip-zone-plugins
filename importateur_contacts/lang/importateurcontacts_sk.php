8167af89ca0900aa3c16918e731e93eb1c198164
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-03 11:04:34 +0000

    langues (importateurcontacts)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@56239 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ importateur_contacts/lang/importateurcontacts_sk.php
@@ -0,0 +1,27 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'configurer_fournisseur_active_avec_moteur' => 'Aktivovať cez @moteur@',
+	'configurer_fournisseur_desactive' => 'Deaktivovať',
+	'configurer_titre' => 'Nastaviť služby na nahrávanie kontaktov',
+
+	// E
+	'erreur_aucun_fournisseur' => 'Žiadna služba vám teraz nemôže zabezpečiť, aby ste našli svoje kontakty. Nainštalujte si, prosím, aspoň jednu knižnicu na nahrávanie kontaktov.',
+
+	// I
+	'importer_bouton_recuperer_contacts' => 'Získať kontakty',
+	'importer_fournisseur_label' => 'Vyberte si službu, z ktorej chcete získať svoje kontakty',
+	'info_aucun_contact' => 'Pre tento účet sa nenašiel žiaden kontakt.',
+	'info_nb_contacts' => 'Získali ste @nb@ kontaktov.',
+	'info_nb_contacts_erreur' => 'Získali ste @nb@ kontaktov, ale nedali sa použiť',
+	'info_titre' => 'Nahrávač kontaktov'
+);
+
+?>
