b2417fa2ba8b959e552c30e73ca704124da32bc7
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-03 11:04:31 +0000

    langues (gis)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@56236 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- gis/trunk/lang/gis_sk.php
+++ gis/trunk/lang/gis_sk.php
@@ -13,72 +13,72 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// B
 	'bouton_lier' => 'Prepojiť tento bod',
-	'bouton_supprimer_gis' => 'Natrvalo odstrániť tento bod', # MODIF
+	'bouton_supprimer_gis' => 'Natrvalo odstrániť tento bod',
 	'bouton_supprimer_lien' => 'Odstrániť tento odkaz',
 
 	// C
-	'cfg_descr_gis' => 'Geografický informačný systém.<br />Odkaz na dokumentáciu.', # MODIF
-	'cfg_inf_adresse' => 'Zobrazí ďalšie polia adresy (krajina, mesto, štát, adresa a pod.)', # MODIF
-	'cfg_inf_cloudmade' => 'Táto API potrebuje kľúč, ktorý si môžete vytvoriť na <a href=\'@url@\'>internetovej stránke v Cloude.</a>', # MODIF
-	'cfg_inf_geocoder' => 'Aktivovať funkciu vyhľadávania vo formulári.', # MODIF
-	'cfg_inf_geolocaliser_user_html5' => 'Ak to povoľuje prehliadač používateľa, na určenie predvolenej polohy pri vytváraní nového bodu sa zistí približná geografická poloha používateľa.', # MODIF
-	'cfg_inf_google' => 'Táto API potrebuje kľúč, ktorý si môžete vytvoriť na <a href=\'@url@\'>stránke GoogleMaps.</a>', # MODIF
-	'cfg_inf_yandex' => 'Cette API nécessite une clé à créer sur <a href=\'@url@\'>le site de Yandex</a>.', # MODIF
-	'cfg_lbl_activer_objets' => 'Activer la géolocalisation sur les contenus :', # NEW
+	'cfg_descr_gis' => 'Geografický informačný systém.<br />Pozrieť si dokumentáciu.</a>',
+	'cfg_inf_adresse' => 'Zobrazí ďalšie polia adresy (krajinu, mesto, štát, adresu a pod.)',
+	'cfg_inf_cloudmade' => 'Táto aplikácia potrebuje kľúč na vytvorenie <a href=\'@url@\'>stránky v Cloude.</a>',
+	'cfg_inf_geocoder' => 'Aktivovať funkciu geokódera (vyhľadávanie z jednej adresy, zistenie adresy zo súradníc).',
+	'cfg_inf_geolocaliser_user_html5' => 'Ak to povoľuje prehliadač používateľa, na určenie predvolenej polohy pri vytváraní nového bodu sa ukladá približná geografická poloha používateľa.',
+	'cfg_inf_google' => 'Táto aplikácia potrebuje kľúč, ktorý si treba vytvoriť na <a href=\'@url@\'>stránke GoogleMaps.</a>',
+	'cfg_inf_yandex' => 'Táto aplikácia potrebuje kľúč na vytvorenie <a href=\'@url@\'>stránky v Yandexe.</a>',
+	'cfg_lbl_activer_objets' => 'Aktivovať geolokalizáciu obsahu:',
 	'cfg_lbl_adresse' => 'Zobraziť polia adresy',
 	'cfg_lbl_api' => 'Geolokačná API',
 	'cfg_lbl_api_cloudmade' => 'CloudMade',
 	'cfg_lbl_api_google' => 'Google Maps v2',
 	'cfg_lbl_api_googlev3' => 'Google Maps v3',
-	'cfg_lbl_api_key_cloudmade' => 'kľúč CloudMade', # MODIF
-	'cfg_lbl_api_key_google' => 'Kľúč GoogleMaps', # MODIF
-	'cfg_lbl_api_key_yandex' => 'Clé Yandex', # MODIF
+	'cfg_lbl_api_key_cloudmade' => 'Kľúč CloudMade',
+	'cfg_lbl_api_key_google' => 'Kľúč GoogleMaps',
+	'cfg_lbl_api_key_yandex' => 'Kľúč Yandex',
 	'cfg_lbl_api_mapquest' => 'MapQuest',
 	'cfg_lbl_api_microsoft' => 'Microsoft Bing',
 	'cfg_lbl_api_openlayers' => 'OpenLayers',
-	'cfg_lbl_api_ovi' => 'Ovi Nokia', # NEW
-	'cfg_lbl_api_yandex' => 'Yandex', # NEW
+	'cfg_lbl_api_ovi' => 'Ovi Nokia',
+	'cfg_lbl_api_yandex' => 'Yandex',
 	'cfg_lbl_geocoder' => 'Geocoder',
-	'cfg_lbl_geolocaliser_user_html5' => 'Pri vytváraní vycentrujte mapu na polohe používateľa', # MODIF
-	'cfg_lbl_maptype' => 'Fond cartographique', # NEW
-	'cfg_lbl_maptype_carte' => 'Carte', # NEW
-	'cfg_lbl_maptype_hybride' => 'Hybride', # NEW
-	'cfg_lbl_maptype_relief' => 'Relief', # NEW
-	'cfg_lbl_maptype_satellite' => 'Satellite', # NEW
+	'cfg_lbl_geolocaliser_user_html5' => 'Pri vytváraní vycentrujte mapu na polohe používateľa',
+	'cfg_lbl_maptype' => 'Základná mapa',
+	'cfg_lbl_maptype_carte' => 'Mapa',
+	'cfg_lbl_maptype_hybride' => 'Zmiešaná',
+	'cfg_lbl_maptype_relief' => 'Reliéf',
+	'cfg_lbl_maptype_satellite' => 'Satelitná',
 	'cfg_titre_gis' => 'GIS',
 
 	// E
 	'editer_gis_editer' => 'Upraviť tento bod',
-	'editer_gis_explication' => 'Na tejto stránke sú uvedené všetky geolokalizované body webu.', # MODIF
-	'editer_gis_nouveau' => 'Vytvoriť nový bod', # MODIF
-	'editer_gis_titre' => 'Geolokalizované body', # MODIF
-	'erreur_recherche_pas_resultats' => 'Vyhľadávaného textu sa netýka žiaden bod.', # MODIF
-	'explication_api_forcee' => 'L\'API est imposée par un autre plugin ou squelette.', # NEW
-	'explication_maptype_force' => 'Le fond cartographique est imposé par un autre plugin ou squelette.', # NEW
+	'editer_gis_explication' => 'Táto stránka uvádza všetky geolokalizované body na webe.',
+	'editer_gis_nouveau' => 'Vytvoriť nový bod',
+	'editer_gis_titre' => 'Geolokalizované body',
+	'erreur_recherche_pas_resultats' => 'Vyhľadávania sa netýka žiaden bod.',
+	'explication_api_forcee' => 'Túto aplikáciu používa iný zásuvný modul alebo iná šablóna.',
+	'explication_maptype_force' => 'Základnú mapu si vyžaduje iný zásuvný modul alebo šablóna.',
 
 	// F
-	'formulaire_creer_gis' => 'Vytvoriť nový geolokalizovaný bod:', # MODIF
-	'formulaire_modifier_gis' => 'Upraviť geolokalizované body:', # MODIF
+	'formulaire_creer_gis' => 'Vytvoriť geolokalizovaný bod:',
+	'formulaire_modifier_gis' => 'Upraviť geolokalizovaný bod:',
 
 	// G
-	'gis_pluriel' => 'Points géolocalisés', # NEW
-	'gis_singulier' => 'Point géolocalisé', # NEW
+	'gis_pluriel' => 'Geolokalizované body',
+	'gis_singulier' => 'Geolokalizovaný bod',
 
 	// I
-	'icone_gis_tous' => 'Geolokalizované body', # MODIF
-	'info_1_gis' => 'Un point géolocalisé', # NEW
-	'info_1_objet_gis' => '1 objet lié à ce point', # NEW
-	'info_aucun_gis' => 'Aucun point géolocalisé', # NEW
-	'info_aucun_objet_gis' => 'Aucun objet lié à ce point', # NEW
-	'info_id_objet' => 'Č.', # MODIF
-	'info_liste_gis' => 'Points géolocalisés', # NEW
-	'info_nb_gis' => '@nb@ points géolocalisés', # NEW
-	'info_nb_objets_gis' => '@nb@ objets liés à ce point', # NEW
-	'info_numero_gis' => 'Bod číslo', # MODIF
+	'icone_gis_tous' => 'Geolokalizované body',
+	'info_1_gis' => 'Jeden geolokalizovaný bod',
+	'info_1_objet_gis' => '1 objekt prepojený s týmto bodom',
+	'info_aucun_gis' => 'Žiaden geolokalizovaný bod',
+	'info_aucun_objet_gis' => 'Žiaden objekt prepojený s týmto bodom',
+	'info_id_objet' => 'Č.',
+	'info_liste_gis' => 'Geolokalizované body',
+	'info_nb_gis' => '@nb@ geolokalizovaných bodov',
+	'info_nb_objets_gis' => '@nb@ objektov prepojených s týmto bodom',
+	'info_numero_gis' => 'Bod číslo',
 	'info_objet' => 'Objekt',
-	'info_recherche_gis_zero' => 'Aucun résultat pour « @cherche_gis@ ».', # NEW
-	'info_supprimer_lien' => 'Odpojiť', # MODIF
-	'info_supprimer_liens' => 'Détacher tous les points', # MODIF
+	'info_recherche_gis_zero' => 'Žiadne výsledky pre "@cherche_gis@".',
+	'info_supprimer_lien' => 'Zrušiť prepojenie',
+	'info_supprimer_liens' => 'Zrušiť všetky body',
 	'info_voir_fiche_objet' => 'Prejsť na stránku',
 
 	// L
@@ -87,26 +87,26 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'label_pays' => 'Krajina',
 	'label_rechercher_address' => 'Vyhľadať adresu',
 	'label_rechercher_point' => 'Vyhľadať bod',
-	'label_region' => 'Región (kraj)', # MODIF
+	'label_region' => 'Región (kraj)',
 	'label_ville' => 'Mesto',
 	'lat' => 'Zemepisná šírka',
 	'libelle_logo_gis' => 'LOGO BODU',
-	'lien_ajouter_gis' => 'Ajouter ce point', # NEW
+	'lien_ajouter_gis' => 'Pridať tento bod',
 	'lon' => 'Zemepisná dĺžka',
 
 	// T
-	'texte_ajouter_gis' => 'Ajouter un point géolocalisé', # NEW
-	'texte_creer_associer_gis' => 'Créer et associer un point géolocalisé', # NEW
-	'texte_creer_gis' => 'Créer un point géolocalisé', # NEW
-	'texte_modifier_gis' => 'Modifier le point géolocalisé', # NEW
-	'texte_voir_gis' => 'Voir le point géolocalisé', # NEW
+	'texte_ajouter_gis' => 'Pridať geolokalizovaný bod',
+	'texte_creer_associer_gis' => 'Vytvoriť a prepojiť geolokalizovaný bod',
+	'texte_creer_gis' => 'Vytvoriť geolokalizovaný bod',
+	'texte_modifier_gis' => 'Upraviť geolokalizovaný bod',
+	'texte_voir_gis' => 'Zobraziť geolokalizovaný bod',
 	'titre_bloc_creer_point' => 'Prepojiť nový bod',
 	'titre_bloc_points_lies' => 'Prepojené body',
 	'titre_bloc_rechercher_point' => 'Vyhľadať bod',
 	'titre_nombre_utilisation' => 'Jedno použitie',
 	'titre_nombre_utilisations' => '@nb@ použití',
-	'titre_nouveau_point' => 'Nouveau point', # NEW
-	'titre_objet' => 'Titre', # NEW
+	'titre_nouveau_point' => 'Nový bod',
+	'titre_objet' => 'Názov',
 
 	// Z
 	'zoom' => 'Lupa'
