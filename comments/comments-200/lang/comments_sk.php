982f0a5dac76e5f8ab0051a96d68ccf3da107672
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-03 11:04:17 +0000

    langues (comments)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@56222 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- comments/comments-200/lang/comments_sk.php
+++ comments/comments-200/lang/comments_sk.php
@@ -11,17 +11,17 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'cfg_forum_longueur' => 'Dĺžka správ',
 	'cfg_forum_longueur_maxi' => 'Maximálna dĺžka',
 	'cfg_forum_longueur_mini' => 'Minimálna dĺžka',
-	'cfg_option_aucun' => 'Aucun', # NEW
-	'cfg_option_compteur' => 'Compteur (1.)', # NEW
-	'cfg_option_diese' => 'Dièse (#)', # NEW
-	'cfg_option_liste' => 'Liste de commentaires (à plat)', # NEW
-	'cfg_option_picto' => 'Picto', # NEW
-	'cfg_option_thread' => 'Enfilade de commentaires (en thread)', # NEW
-	'cfg_option_thread_un' => 'Enfilade de commentaires (en thread) à un niveau', # NEW
-	'cfg_permalien_label' => 'Apparence du lien permanent vers le commentaire', # NEW
-	'cfg_permalien_legende' => 'Permalien', # NEW
-	'cfg_type_label' => 'Présentation du fil de discussion', # NEW
-	'cfg_type_legende' => 'Liste ou thread ?', # NEW
+	'cfg_option_aucun' => 'Žiadny',
+	'cfg_option_compteur' => 'Počítadlo (1.)',
+	'cfg_option_diese' => 'Krížik (#)',
+	'cfg_option_liste' => 'Zoznam komentárov (jednoduchý)',
+	'cfg_option_picto' => 'Pikto',
+	'cfg_option_thread' => 'Rad komentárov (vo vlákne)',
+	'cfg_option_thread_un' => 'Rad komentárov (vo vlákne) po úroveň',
+	'cfg_permalien_label' => 'Vzhľad trvalého odkazu v komentári',
+	'cfg_permalien_legende' => 'Trvalý odkaz',
+	'cfg_type_label' => 'Spôsob zobrazenia priebehu diskusie',
+	'cfg_type_legende' => 'Zoznam alebo vlákno?',
 	'comment' => 'komentár',
 	'comments' => 'komentárov',
 	'comments_h' => 'Vaše komentáre',
@@ -31,13 +31,13 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'date_jour_le' => ' ',
 
 	// F
-	'forum_attention_peu_caracteres' => '<b>Attention ! </b>votre message est trop court (@compte@ caractères) : pour pouvoir être enregistré, il ne doit pas être inférieur à @min@ caractères.', # NEW
+	'forum_attention_peu_caracteres' => '<b>Pozor!</b> Váš príspevok je príliš krátky na to, aby mohol byť uložený (@compte@ znakov); musí mať minimálne @min@ znakov.',
 	'forum_qui_etes_vous' => 'Kto ste?',
 
 	// L
 	'label_email' => 'E-mail (nezverejňuje sa)',
 	'label_nom' => 'Meno',
-	'label_url' => 'Vaša webstránka',
+	'label_url' => 'Vaša webová stránka',
 
 	// M
 	'moderation_info' => 'Upozornenie: Vaša správa sa zobrazí, až keď bude skontrolovaná a schválená.',
