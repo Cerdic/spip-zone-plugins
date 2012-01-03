f4393c40e097a58ea630c68e7f7b1ebb5961f9e1
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-03 11:04:11 +0000

    langues (accesrestreint)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@56216 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- acces_restreint/trunk/lang/accesrestreint_sk.php
+++ acces_restreint/trunk/lang/accesrestreint_sk.php
@@ -8,57 +8,57 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
-	'ajouter_droits_auteur' => 'Dať mi prístup do tejto zóny',
-	'aucune_zone' => 'Žiadne zóny',
+	'ajouter_droits_auteur' => 'Udeliť mi prístup do tejto oblasti',
+	'aucune_zone' => 'Žiadne oblasti',
 	'auteurs' => 'autori',
 
 	// B
-	'bouton_configurer_acces' => 'Configuration des accès .htaccess', # NEW
-	'bouton_creer_la_zone' => 'Vytvoriť novú zónu',
+	'bouton_configurer_acces' => 'Nastavenie prístupu cez .htaccess',
+	'bouton_creer_la_zone' => 'Vytvoriť novú oblasť',
 
 	// C
 	'colonne_id' => 'Č.',
-	'confirmer_ajouter_auteurs' => 'Určite chcete pridať tohto autora do tejto zóny?',
-	'confirmer_retirer_auteur_zone' => 'Určite chcete odstrániť tohto autora z tejto zóny?',
-	'confirmer_retirer_auteurs' => 'Určite chcete odstrániť všetkých autorov z tejto zóny?',
-	'confirmer_retirer_rubrique_zone' => 'Êtes vous sûr de vouloir retirer cette rubrique de cette zone ?', # NEW
-	'confirmer_supprimer_zone' => 'Určite chcete odstrániť túto zónu?',
-	'creer_zone' => 'Vytvoriť novú zónu',
+	'confirmer_ajouter_auteurs' => 'Určite chcete pridať tohto autora do tejto oblasti?',
+	'confirmer_retirer_auteur_zone' => 'Určite chcete odstrániť tohto autora z tejto oblasti?',
+	'confirmer_retirer_auteurs' => 'Určite chcete odstrániť všetkých autorov z tejto oblasti?',
+	'confirmer_retirer_rubrique_zone' => 'Určite chcete odstrániť túto rubriku z tejto oblasti?',
+	'confirmer_supprimer_zone' => 'Určite chcete odstrániť túto oblasť?',
+	'creer_zone' => 'Vytvoriť novú oblasť',
 
 	// D
 	'descriptif' => 'Popis',
 
 	// E
-	'explication_creer_htaccess' => 'Cette option interdit la lecture des documents joints si le texte auquel ils se rattachent n\'est pas publi&eacute', # NEW
+	'explication_creer_htaccess' => 'Táto možnosť bráni čítaniu pripojeného súboru, ak text ku ktorému je pripojený, nie je publikovaný',
 
 	// I
 	'icone_menu_config' => 'Obmedzený prístup',
-	'icone_supprimer_zone' => 'Odstrániť túto zónu',
-	'info_1_zone' => '1 zone', # NEW
+	'icone_supprimer_zone' => 'Odstrániť túto oblasť',
+	'info_1_zone' => '1 oblasť',
 	'info_acces_restreint' => 'Prístup na túto stránku je obmedzený. Prosím, prihláste sa, aby ste mohli pokračovať',
 	'info_ajouter_auteur' => 'Pridať tohto autora',
 	'info_ajouter_auteurs' => 'Pridať všetkých autorov',
-	'info_ajouter_zones' => 'Pridať všetky zóny',
+	'info_ajouter_zones' => 'Pridať všetky oblasti',
 	'info_aucun_acces' => 'Nepovolený prístup',
-	'info_aucun_auteur' => 'V tejto zóne nie sú žiadni autori',
-	'info_aucune_zone' => 'Žiadne zóny',
-	'info_auteurs_lies_zone' => 'Autori s prístupom do tejto zóny',
-	'info_lien_action_proteger' => 'Obmedziť prístup do tejto zóny',
-	'info_nb_zones' => '@nb@ zones', # NEW
-	'info_page' => 'Táto stránka vám umožňuje vytvoriť na vašej stránke zóny s obmedzeným prístupom',
+	'info_aucun_auteur' => 'V tejto oblasti nie sú žiadni autori',
+	'info_aucune_zone' => 'Žiadne oblasti',
+	'info_auteurs_lies_zone' => 'Autori s prístupom do tejto oblasti',
+	'info_lien_action_proteger' => 'Obmedziť prístup do tejto oblasti',
+	'info_nb_zones' => '@nb@ oblastí',
+	'info_page' => 'Táto stránka vám umožňuje vytvoriť na vašej stránke oblasti s obmedzeným prístupom',
 	'info_retirer_auteurs' => 'Odstrániť všetkých autorov',
-	'info_retirer_zone' => 'Odstrániť zo zóny',
-	'info_retirer_zones' => 'Odstrániť zo všetkých zón',
-	'info_rubrique_dans_zone' => 'Táto rubrika je v zóne:',
-	'info_rubrique_dans_zones' => 'Táto rubrika je v zónach:',
+	'info_retirer_zone' => 'Odstrániť z oblasti',
+	'info_retirer_zones' => 'Odstrániť zo všetkých oblastí',
+	'info_rubrique_dans_zone' => 'Táto rubrika sa nachádza v oblasti:',
+	'info_rubrique_dans_zones' => 'Táto rubrika sa nachádza v oblastiach:',
 
 	// L
-	'label_creer_htaccess' => 'Accès aux document joints par leur URL', # NEW
-	'label_creer_htaccess_non' => 'autoriser la lecture', # NEW
-	'label_creer_htaccess_oui' => 'interdire la lecture', # NEW
+	'label_creer_htaccess' => 'Prístup k pripojeným súborom podľa ich adresy',
+	'label_creer_htaccess_non' => 'Povoliť čítanie',
+	'label_creer_htaccess_oui' => 'Zakázať čítanie',
 
 	// M
-	'modifier_zone' => 'Upraviť zónu',
+	'modifier_zone' => 'Upraviť oblasť',
 
 	// P
 	'page_zones_acces' => 'Obmedzený prístup',
@@ -68,32 +68,32 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// R
 	'rubriques' => 'rubriky',
-	'rubriques_zones_acces' => 'Rubriky v tejto zóne',
+	'rubriques_zones_acces' => 'Rubriky v tejto oblasti',
 
 	// S
-	'selectionner_une_zone' => 'Vyberte zónu s obmedzeným prístupom',
+	'selectionner_une_zone' => 'Vyberte oblasť s obmedzeným prístupom',
 
 	// T
-	'texte_ajouter_zone' => 'Ajouter une zone', # NEW
-	'texte_creer_associer_zone' => 'Créer et associer une zone', # NEW
+	'texte_ajouter_zone' => 'Pridať oblasť',
+	'texte_creer_associer_zone' => 'Vytvoriť a prepojiť oblasť',
 	'titre' => 'Názov',
-	'titre_ajouter_zone' => 'Pridať zónu s obmedzeným prístupom',
+	'titre_ajouter_zone' => 'Pridať oblasť s obmedzeným prístupom',
 	'titre_boite_protegee_non' => 'Neobmedzený prístup',
 	'titre_boite_protegee_oui' => 'Obmedzený prístup',
-	'titre_cadre_modifier_zone' => 'Upraviť zónu',
+	'titre_cadre_modifier_zone' => 'Upraviť oblasť',
 	'titre_page_config' => 'Nastaviť prístupové práva',
-	'titre_table' => 'Všetky zóny s obmedzeným prístupom',
-	'titre_zone_acces' => 'Zone d\'accès restreint', # NEW
-	'titre_zones_acces' => 'Zóny s obmedzeným prístupom',
+	'titre_table' => 'Všetky oblasti s obmedzeným prístupom',
+	'titre_zone_acces' => 'Oblasť s obmedzeným prístupom',
+	'titre_zones_acces' => 'Oblasti s obmedzeným prístupom',
 	'toutes' => 'Všetky',
 
 	// V
-	'voir_toutes' => 'Zobraziť všetky zóny',
+	'voir_toutes' => 'Zobraziť všetky oblasti',
 
 	// Z
-	'zone_numero' => 'ZÓNA ČÍSLO:',
-	'zone_restreinte_espace_prive' => 'Obmedzte prístup do tejto zóny v súkromnej zóne',
-	'zone_restreinte_publique' => 'Obmedzte prístup do tejto zóny na verejnej stránke'
+	'zone_numero' => 'OBLASŤ ČÍSLO:',
+	'zone_restreinte_espace_prive' => 'Obmedzte prístup do tejto oblasti v súkromnej zóne',
+	'zone_restreinte_publique' => 'Obmedzte prístup do tejto oblasti na verejne prístupnej stránke'
 );
 
 ?>
