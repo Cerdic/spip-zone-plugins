6e39b4649bfc84620bb2a525f3393c1afa98352a
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-03 11:04:37 +0000

    langues (lienscontenus)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@56242 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- liens_contenus/trunk/lang/lienscontenus_sk.php
+++ liens_contenus/trunk/lang/lienscontenus_sk.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// extrait automatiquement de http://www.spip.net/trad-lang/
+// extrait automatiquement de http://trad.spip.org
 // ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
@@ -9,7 +9,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
 	'alerte_publie_contenant' => 'Pozor, tento text je publikovaný, ale sú v ňom odkazy na iný text, ktorý nie je!',
-	'alerte_publie_contenant_ko' => 'Attention, ce contenu est publié, mais contient des liens vers des contenus qui n\'existent pas !', # NEW
+	'alerte_publie_contenant_ko' => 'Pozor, tento text je publikovaný, ale sú v ňom odkazy na taký, ktorý neexistuje!',
 	'aucun_objets_avec_lien_depuis_courant' => 'V tomto texte nie sú odkazy na iný text.',
 	'aucun_objets_avec_lien_vers_courant' => 'Žiaden iný text neodkazuje na tento.',
 
@@ -19,41 +19,41 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'confirmation_suppression' => 'Pozor, v publikovanom texte je aspoň jeden odkaz na tento text, bude tým ovplyvnený, ak aktuálny text odstránite!nnStále ho chcete odstrániť?',
 
 	// I
-	'inexistant' => 'inexistant (@id_objet@)', # NEW
+	'inexistant' => 'neexistuje (@id_objet@)',
 	'information_element_contenu' => 'Pozor, ďalšie textové odkazy na tento text!',
 
 	// L
 	'legende_liens_faux_objets' => 'Červené prečiarknuté odkazy sú prepojenia na texty, ktoré neexistujú.',
-	'liens_entre_contenus' => 'Liens entre contenus', # NEW
+	'liens_entre_contenus' => 'Odkazy v obsahu',
 
 	// O
 	'objets_avec_liens_depuis_courant' => 'V tomto texte sú odkazy na tieto texty:',
 	'objets_avec_liens_vers_courant' => 'V týchto textoch sú odkazy na tento:',
 
 	// S
-	'statut_poubelle' => 'V koši',
+	'statut_poubelle' => 'Do koša',
 	'statut_prepa' => 'Upravuje sa',
 	'statut_prop' => 'Odoslaný na schválenie',
 	'statut_publie' => 'Publikovaný online',
 	'statut_refuse' => 'Zamietnutý',
 
 	// T
-	'type_article' => '@titre@ (@id_objet@)', # NEW
-	'type_article_inexistant' => 'Article inexistant (@id_objet@)', # NEW
-	'type_auteur' => '@titre@', # NEW
-	'type_auteur_inexistant' => 'Auteur inexistant (@id_objet@)', # NEW
-	'type_breve' => '@titre@ (@id_objet@)', # NEW
-	'type_breve_inexistant' => 'Brève inexistante (@id_objet@)', # NEW
-	'type_document' => '@titre@ (@id_objet@)', # NEW
-	'type_document_inexistant' => 'Document inexistant (@id_objet@)', # NEW
-	'type_forum' => '@titre@ (@id_objet@)', # NEW
-	'type_forum_inexistant' => 'Message inexistant (@id_objet@)', # NEW
-	'type_modele' => 'Modèle "@id_objet@"', # NEW
-	'type_modele_inexistant' => 'Modèle inexistant (@id_objet@)', # NEW
-	'type_rubrique' => '@titre@ (@id_objet@)', # NEW
-	'type_rubrique_inexistant' => 'Rubrique inexistante (@id_objet@)', # NEW
-	'type_syndic' => '@titre@ (@id_objet@)', # NEW
-	'type_syndic_inexistant' => 'Site inexistant (@id_objet@)' # NEW
+	'type_article' => '@titre@ (@id_objet@)',
+	'type_article_inexistant' => 'Článok neexistuje (@id_objet@)',
+	'type_auteur' => '@titre@',
+	'type_auteur_inexistant' => 'Autor neexistuje (@id_objet@)',
+	'type_breve' => '@titre@ (@id_objet@)',
+	'type_breve_inexistant' => 'Novinka neexistuje (@id_objet@)',
+	'type_document' => '@titre@ (@id_objet@)',
+	'type_document_inexistant' => 'Súbor neexistuje (@id_objet@)',
+	'type_forum' => '@titre@ (@id_objet@)',
+	'type_forum_inexistant' => 'Správa neexistuje (@id_objet@)',
+	'type_modele' => 'Model "@id_objet@"',
+	'type_modele_inexistant' => 'Model neexistuje (@id_objet@)',
+	'type_rubrique' => '@titre@ (@id_objet@)',
+	'type_rubrique_inexistant' => 'Rubrika neexistuje (@id_objet@)',
+	'type_syndic' => '@titre@ (@id_objet@)',
+	'type_syndic_inexistant' => 'Stránka neexistuje (@id_objet@)'
 );
 
 ?>
