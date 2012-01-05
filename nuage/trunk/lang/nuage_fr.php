c5061291b4239e946911b4a2bd09a115545a4fdd
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-05 11:26:33 +0000

    langues (nuage)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@56621 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- nuage/trunk/lang/nuage_fr.php
+++ nuage/trunk/lang/nuage_fr.php
@@ -1,18 +1,17 @@
 <?php
-
-// This is a SPIP-forum module file  --  Ceci est un fichier module de SPIP-forum
-if (!defined("_ECRIRE_INC_VERSION")) return;
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/nuage/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// N
-'noisette_titre_defaut' => 'Tags',
-'noisette_nom_noisette' => 'Nuage',
-'noisette_description' => 'Affiche un nuage des mots cl&eacute;s',
-'noisette_label_afficher_titre' => 'Afficher le titre&nbsp;:',
-'noisette_titre_noisette' => 'Titre&nbsp;:',
-'noisette_label_niveau_titre' => 'Choisir le niveau du titre',
-
+	// N
+	'noisette_description' => 'Affiche un nuage des mots clés',
+	'noisette_label_afficher_titre' => 'Afficher le titre :',
+	'noisette_label_niveau_titre' => 'Choisir le niveau du titre',
+	'noisette_nom_noisette' => 'Nuage',
+	'noisette_titre_defaut' => 'Tags',
+	'noisette_titre_noisette' => 'Titre :'
 );
 
 ?>
