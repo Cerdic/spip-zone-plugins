b0019d9f446868575c75ea9b676849541fc15249
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-05 09:48:20 +0000

    langues (paquet-spip_bonux)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@56583 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spip-bonux-3/lang/paquet-spip_bonux_fr.php
+++ spip-bonux-3/lang/paquet-spip_bonux_fr.php
@@ -1,20 +1,14 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-// 
-///  Fichier produit par PlugOnet
-// Module: paquet-spip_bonux
-// Langue: fr
-// Date: 22-10-2011 17:21:20
-// Items: 2
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/spip-bonux-3/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// S
+	// S
 	'spip_bonux_description' => 'Le plugin qui lave plus SPIP que SPIP, avec des chouettes cadeaux dedans !
 	Tableaux, compteur, etc.',
-	'spip_bonux_slogan' => 'Le plugin qui lave plus SPIP que SPIP.',
+	'spip_bonux_slogan' => 'Le plugin qui lave plus SPIP que SPIP.'
 );
+
 ?>
