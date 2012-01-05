73c9277ce874861dc67ff2dfb69bc106959bd7fd
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-05 11:26:40 +0000

    langues (ppp)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@56628 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- porte_plume_extras/partout/lang/ppp_en.php
+++ porte_plume_extras/partout/lang/ppp_en.php
@@ -1,27 +1,27 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-// Produit automatiquement par le plugin LangOnet a partir de la langue source en
-// Module: ppp
-// Langue: en
-// Date: 02-06-2011 19:52:12
-// Items: 8
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org
+// ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// B
+	// B
 	'bouton_mettre_a_jour' => 'Update',
-// C
+
+	// C
 	'cfg_attention' => 'Warning',
 	'cfg_description' => 'according to your templates, be sure to only activate the Quill on fields where the use will not create xhtml error.',
 	'cfg_titre' => 'Quill everywhere',
-// L
+
+	// L
 	'label_hauteur_champ' => 'Half size of the screen',
 	'legend_barre_typo' => 'Enable the editing toolbar on :',
 	'legend_hauteur_champ' => 'Height of the text field on articles',
-// S
-	'supprimer' => 'Reset to the default values',
+
+	// S
+	'supprimer' => 'Reset to the default values'
 );
+
 ?>
--- porte_plume_extras/partout/lang/ppp_fr.php
+++ porte_plume_extras/partout/lang/ppp_fr.php
@@ -1,27 +1,25 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-// Produit automatiquement par le plugin LangOnet a partir de la langue source fr
-// Module: ppp
-// Langue: fr
-// Date: 02-06-2011 19:51:58
-// Items: 8
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/porte_plume_extras/partout/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// B
+	// B
 	'bouton_mettre_a_jour' => 'Mettre à jour',
-// C
+
+	// C
 	'cfg_attention' => 'Attention',
 	'cfg_description' => 'en fonction de vos squelettes, veillez à n\'activer le Porte Plume que sur des champs pour lesquels l\'utilisation des raccourcis n\'engendrera pas d\'erreur xhtml.',
 	'cfg_titre' => 'Porte Plume Partout',
-// L
+
+	// L
 	'label_hauteur_champ' => 'Demi-hauteur de l\'écran',
 	'legend_barre_typo' => 'Activer la barre typographique sur :',
 	'legend_hauteur_champ' => 'Hauteur du champ texte pour les articles',
-// S
-	'supprimer' => 'Remettre les valeurs par défaut',
+
+	// S
+	'supprimer' => 'Remettre les valeurs par défaut'
 );
+
 ?>
