2985519d754a31069a9b08fc07901cbfd03fc72f
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-05 09:48:23 +0000

    langues (paquet-tablesorter)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@56586 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- tablesorter/lang/paquet-tablesorter_fr.php
+++ tablesorter/lang/paquet-tablesorter_fr.php
@@ -1,19 +1,13 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-tablesorter
-// Langue: fr
-// Date: 08-12-2011 13:11:26
-// Items: 2
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/tablesorter/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// T
+	// T
 	'tablesorter_description' => 'Ce plugin permet de trier les tableaux portant la class CSS "spip" en cliquant simplement sur l\'entête d\'une colonne (le tableau HTML doit donc posséder les tags thead et tbody). Voir en ligne toutes les options de tri, sur le site de [->http://tablesorter.com].',
-	'tablesorter_slogan' => 'Avec tableSorter, le tri sera top !',
+	'tablesorter_slogan' => 'Avec tableSorter, le tri sera top !'
 );
+
 ?>
