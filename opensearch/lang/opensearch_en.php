e03f66da926dcca3e27d54c78b617de1d8a42c10
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-05 11:26:35 +0000

    langues (opensearch)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@56623 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- opensearch/lang/opensearch_en.php
+++ opensearch/lang/opensearch_en.php
@@ -1,18 +1,20 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/opensearch/lang/
+// extrait automatiquement de http://trad.spip.org
+// ** ne pas modifier le fichier **
+
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// C
-	'cfg_boite_opensearch' => 'OpenSearch plugin configuration',
-	'cfg_descr_opensearch' => 'OpenSearch : custom search engine for your website.',
-	'cfg_inf_id_rubrique' => 'Select the section in which to search.',
-	'cfg_inf_page_recherche' => 'Specify the skeleton to use for the site search (exemple : look for the recherche.html skeleton)',
-	'cfg_lbl_id_rubrique' => 'Target section',
-	'cfg_lbl_page_recherche' => 'Search skeleton',
-	'cfg_option_tout' => 'the whole website',
+	'cfg_boite_opensearch' => 'OpenSearch plugin configuration', # MODIF
+	'cfg_descr_opensearch' => 'OpenSearch : custom search engine for your website.', # MODIF
+	'cfg_inf_id_rubrique' => 'Select the section in which to search.', # MODIF
+	'cfg_inf_page_recherche' => 'Specify the skeleton to use for the site search (exemple : look for the recherche.html skeleton)', # MODIF
+	'cfg_lbl_id_rubrique' => 'Target section', # MODIF
+	'cfg_lbl_page_recherche' => 'Search skeleton', # MODIF
+	'cfg_option_tout' => 'the whole website', # MODIF
 	'cfg_titre_opensearch' => 'OpenSearch'
 );
 
