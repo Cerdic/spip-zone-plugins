4750825162271aa2367e924054b2d63eca6fb829
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-05 11:26:37 +0000

    langues (paquet-pages)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@56625 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- pages/trunk/lang/paquet-pages_en.php
+++ pages/trunk/lang/paquet-pages_en.php
@@ -1,13 +1,18 @@
 <?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-'pages_slogan' => 'Unlinked pages',
-'pages_description' => 'This plugin allows you to create pages of articles that are not linked to any particular hierarchy.
+	// P
+	'pages_description' => 'This plugin allows you to create pages of articles that are not linked to any particular hierarchy.
 However they may be associated with a name template.
 This allows the creation of pages of legal information, about, contact, etc..
 ',
-
+	'pages_slogan' => 'Unlinked pages'
 );
 
 ?>
--- pages/trunk/lang/paquet-pages_fr.php
+++ pages/trunk/lang/paquet-pages_fr.php
@@ -1,12 +1,15 @@
 <?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/pages/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-'pages_slogan' => 'Des pages sans rubrique',
-'pages_description' => 'Ce plugin permet de créer des pages d\'articles qui ne sont reliées à aucune hiérarchie particulière.
+	// P
+	'pages_description' => 'Ce plugin permet de créer des pages d\'articles qui ne sont reliées à aucune hiérarchie particulière.
 En revanche elles peuvent être associées à un nom de squelette.
 Cela permet notamment de créer des pages de notice légale, d\'à-propos, de contact, etc.',
-
+	'pages_slogan' => 'Des pages sans rubrique'
 );
 
 ?>
