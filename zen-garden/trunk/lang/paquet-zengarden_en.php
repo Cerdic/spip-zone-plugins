0106beacc8146743e2176a6802b67295f304704c
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-05 09:48:25 +0000

    langues (paquet-zengarden)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@56588 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- zen-garden/trunk/lang/paquet-zengarden_en.php
+++ zen-garden/trunk/lang/paquet-zengarden_en.php
@@ -1,13 +1,15 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/_stable_/zen-garden/lang
-if (!defined("_ECRIRE_INC_VERSION")) return;
+// extrait automatiquement de http://trad.spip.org
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-	
-	'zengarden_slogan' => 'A Zen garden for Zpip',
+
+	// Z
 	'zengarden_description' => ' ',
-	
+	'zengarden_slogan' => 'A Zen garden for Zpip'
 );
 
 ?>
--- zen-garden/trunk/lang/paquet-zengarden_fr.php
+++ zen-garden/trunk/lang/paquet-zengarden_fr.php
@@ -1,13 +1,13 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/_stable_/zen-garden/lang
-if (!defined("_ECRIRE_INC_VERSION")) return;
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/zen-garden/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-	
-	'zengarden_slogan' => 'Un jardin Zen pour Zpip',
+
+	// Z
 	'zengarden_description' => ' ',
-	
+	'zengarden_slogan' => 'Un jardin Zen pour Zpip'
 );
 
 ?>
