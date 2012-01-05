0b9d4aaa1b00fc72201cf485bfa5ee5781011cfe
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-05 11:26:08 +0000

    langues (afficher_objets)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@56596 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- afficher_objets/lang/afficher_objets_en.php
+++ afficher_objets/lang/afficher_objets_en.php
@@ -1,11 +1,20 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/afficher_objets
-if (!defined("_ECRIRE_INC_VERSION")) return;
+// extrait automatiquement de http://trad.spip.org
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-	'info_statut'=>'Status',
+
+	// I
+	'info_statut' => 'Status',
+
+	// N
 	'nb_articles' => '@nb@ articles',
+
+	// R
+	'rang' => 'Rang' # NEW
 );
 
 ?>
--- afficher_objets/lang/afficher_objets_fr.php
+++ afficher_objets/lang/afficher_objets_fr.php
@@ -1,12 +1,18 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/_stable_/cfg/lang/
-if (!defined("_ECRIRE_INC_VERSION")) return;
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/afficher_objets/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-	'info_statut'=>'Statut',
+
+	// I
+	'info_statut' => 'Statut',
+
+	// N
 	'nb_articles' => '@nb@ articles',
-	'rang' => 'Rang',
+
+	// R
+	'rang' => 'Rang'
 );
 
 ?>
