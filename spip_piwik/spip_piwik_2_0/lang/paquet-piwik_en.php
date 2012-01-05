aa20fc35fd36b79f2b6b401a1de9473080c241b0
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-05 09:48:15 +0000

    langues (paquet-piwik)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@56577 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spip_piwik/spip_piwik_2_0/lang/paquet-piwik_en.php
+++ spip_piwik/spip_piwik_2_0/lang/paquet-piwik_en.php
@@ -1,19 +1,15 @@
 <?php
-
-/// @file
-/// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-/// 
-///  Fichier produit par PlugOnet
-/// Module: paquet-piwik
-/// Langue: en
-/// Date: 07-10-2011 16:03:20
-/// Items: 1
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org
+// ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-// P
+
+	// P
 	'piwik_description' => 'Adds the Piwik statistics script (web traffic analyzer) on the pages of the site.',
-	'piwik_slogan' => 'Link [SPIP->http://www.spip.net] and [Piwik->http://www.piwik.org]',
+	'piwik_slogan' => 'Link [SPIP->http://www.spip.net] and [Piwik->http://www.piwik.org]'
 );
+
 ?>
--- spip_piwik/spip_piwik_2_0/lang/paquet-piwik_fr.php
+++ spip_piwik/spip_piwik_2_0/lang/paquet-piwik_fr.php
@@ -1,19 +1,13 @@
 <?php
-
-/// @file
-/// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-/// 
-///  Fichier produit par PlugOnet
-/// Module: paquet-piwik
-/// Langue: fr
-/// Date: 07-10-2011 16:03:20
-/// Items: 2
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/spip_piwik/spip_piwik_2_0/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-// P
+
+	// P
 	'piwik_description' => 'Ajoute le script de statistiques Piwik (analyseur de trafic web) sur les pages du site.',
-	'piwik_slogan' => 'Interfacer Spip et Piwik',
+	'piwik_slogan' => 'Interfacer Spip et Piwik'
 );
+
 ?>
