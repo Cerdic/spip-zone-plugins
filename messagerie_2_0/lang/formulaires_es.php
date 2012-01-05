f81f49c9053a3f37a251f6b258c2c3e9e86d5aa9
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-05 11:26:21 +0000

    langues (formulaires)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@56610 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- messagerie_2_0/lang/formulaires_es.php
+++ messagerie_2_0/lang/formulaires_es.php
@@ -1,14 +1,23 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-'info_obligatoire' => '***', 
-'info_obligatoire_legende' => 'campos obligatorios', 
-'info_obligatoire_rappel' => 'Esta informaci&oacute;n es obligatoria',
-'email_invalide' => 'Esta direcci&oaccute;n de email no es v&aacute;lida',
-'valeur_incorrecte' => 'Este dato no es correcto',
-'erreur_enregistrement' => 'Ha ocurrido un error al guardarlo',
+	// E
+	'email_invalide' => 'Esta direcci&oaccute;n de email no es válida',
+	'erreur_enregistrement' => 'Ha ocurrido un error al guardarlo',
 
+	// I
+	'info_obligatoire' => '***',
+	'info_obligatoire_legende' => 'campos obligatorios',
+	'info_obligatoire_rappel' => 'Esta información es obligatoria',
+
+	// V
+	'valeur_incorrecte' => 'Este dato no es correcto'
 );
+
 ?>
