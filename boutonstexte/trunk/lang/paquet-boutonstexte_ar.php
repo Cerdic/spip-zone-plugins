b31dc06a77b6ca0525ae22d7cb5a374bc7c32e49
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-05 11:26:11 +0000

    langues (paquet-boutonstexte)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@56600 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- boutonstexte/trunk/lang/paquet-boutonstexte_ar.php
+++ boutonstexte/trunk/lang/paquet-boutonstexte_ar.php
@@ -1,21 +1,18 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-// 
-///  Fichier produit par PlugOnet
-// Module: paquet-boutonstexte
-// Langue: ar
-// Date: 09-11-2011 14:40:21
-// Items: 2
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org
+// ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// B
+	// B
 	'boutonstexte_description' => 'يضيف ازار تكبير وتصغير الخط في نص المقال. الازرار تظهر قبل كل علامة div من صنف texte في النص اذا لم تكن موجودة سابقاً (صور من صنف textsizeup وtextsizedown وtextonly للنص فقط)
 
 يمكن تنفيذ الاعدادالاختياري بتثبيت الملحق cfg وبتحميل الصفحة. يمكن فرض بعض العناصر بواسطة جافاسكريبت',
 	'boutonstexte_nom' => 'تكبير وتصغير',
+	'boutonstexte_slogan' => 'Des boutons pour gérer la taille des caractères' # NEW
 );
+
 ?>
