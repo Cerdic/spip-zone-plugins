cdfd4e7d8a7430dd3a714d344b1decefa8a4ccfc
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-05 11:26:10 +0000

    langues (boutonstexte)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@56599 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- boutonstexte/trunk/lang/boutonstexte_fr.php
+++ boutonstexte/trunk/lang/boutonstexte_fr.php
@@ -1,26 +1,38 @@
 <?php
-
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-if (!defined("_ECRIRE_INC_VERSION")) return;
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/boutonstexte/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-'boutonstexte_nom' => 'Boutons dans le Texte',
-'textes_icones' => 'Textes des bulles d\'icones',
-'texte_seulement' => 'Texte seulement',
-'retour_a_spip' => 'Retour &agrave; la page compl&egrave;te',
-'augmenter_police' => 'Augmenter police',
-'diminuer_police' => 'Diminuer police',
-'selecteur' => 'S&eacute;lecteur du texte &agrave; traiter',
 
-'label_txtOnly' => 'Texte seulement *',
-'label_txtBackSpip' => 'Texte retour spip',
-'label_txtSizeUp' => 'Texte augmenter *',
-'label_txtSizeDown' => 'Texte diminuer *',
-'label_selector' => 'Expression CSS ou etendue jQuery',
-'label_skin' => 'Apparence des boutons',
-'legend_apparence' => 'Habillage',
+	// A
+	'augmenter_police' => 'Augmenter police',
 
+	// B
+	'boutonstexte_nom' => 'Boutons dans le Texte',
 
-'donnees_techniques' => 'Donn&eacute;es techniques &agrave; ne pas changer'
+	// D
+	'diminuer_police' => 'Diminuer police',
+	'donnees_techniques' => 'Données techniques à ne pas changer',
+
+	// L
+	'label_selector' => 'Expression CSS ou etendue jQuery',
+	'label_skin' => 'Apparence des boutons',
+	'label_txtBackSpip' => 'Texte retour spip',
+	'label_txtOnly' => 'Texte seulement *',
+	'label_txtSizeDown' => 'Texte diminuer *',
+	'label_txtSizeUp' => 'Texte augmenter *',
+	'legend_apparence' => 'Habillage',
+
+	// R
+	'retour_a_spip' => 'Retour à la page complète',
+
+	// S
+	'selecteur' => 'Sélecteur du texte à traiter',
+
+	// T
+	'texte_seulement' => 'Texte seulement',
+	'textes_icones' => 'Textes des bulles d\'icones'
 );
+
 ?>
