785f3144e187eaa436926af8cd81fd010bbdf2cb
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-05 11:26:32 +0000

    langues (paquet-notifications)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@56620 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- notifications/lang/paquet-notifications_en.php
+++ notifications/lang/paquet-notifications_en.php
@@ -1,13 +1,15 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/_dev_/notifications/lang
-if (!defined("_ECRIRE_INC_VERSION")) return;
+// extrait automatiquement de http://trad.spip.org
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-	// A
-	'notifications_slogan' => 'Advanced notifications',
-	'notifications_description' => 'Sends mail notifications for certain actions : forums posts and answers notified to users, private messages notified to redactors or articles publication notified to their authors.',
 
+	// N
+	'notifications_description' => 'Sends mail notifications for certain actions : forums posts and answers notified to users, private messages notified to redactors or articles publication notified to their authors.',
+	'notifications_slogan' => 'Advanced notifications'
 );
 
 ?>
