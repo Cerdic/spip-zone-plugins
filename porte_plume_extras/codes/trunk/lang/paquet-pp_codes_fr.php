bfcf340e995e4280c060579d61a06e77b8d9081f
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-05 11:26:39 +0000

    langues (paquet-pp_codes)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@56627 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- porte_plume_extras/codes/trunk/lang/paquet-pp_codes_fr.php
+++ porte_plume_extras/codes/trunk/lang/paquet-pp_codes_fr.php
@@ -1,8 +1,15 @@
 <?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/porte_plume_extras/codes/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-	'pp_codes_nom' => "Codes Informatiques pour Porte Plume",
-	'pp_codes_slogan' => "Ajoute des boutons à la barre d'outils du Porte Plume pour gérer des codes informatiques",
-	'pp_codes_description' => "Gestion de boutons pour ins&#233;rer du code informatique via la barre d'&#233;dition Porte Plume.
-Ajoute aussi un raccourci <code>[->ecrire/inc_versions.php#trac]</code>.",
+
+	// P
+	'pp_codes_description' => 'Gestion de boutons pour insérer du code informatique via la barre d\'édition Porte Plume.
+Ajoute aussi un raccourci <code>[->ecrire/inc_versions.php#trac]</code>.',
+	'pp_codes_nom' => 'Codes Informatiques pour Porte Plume',
+	'pp_codes_slogan' => 'Ajoute des boutons à la barre d\'outils du Porte Plume pour gérer des codes informatiques'
 );
+
 ?>
