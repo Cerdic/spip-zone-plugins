cb37d562bda3c4a72ac3a49b5ada0823259e515d
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-05 11:26:14 +0000

    langues (paquet-ck)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@56603 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- couteau-kiss/lang/paquet-ck_pt_br.php
+++ couteau-kiss/lang/paquet-ck_pt_br.php
@@ -1,16 +1,18 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/_stable_/acces_restreint/lang/
-if (!defined("_ECRIRE_INC_VERSION")) return;
+// extrait automatiquement de http://trad.spip.org
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-	'ck_nom' => 'Canivete KISS',
-	'ck_slogan' => 'Simplifier certains réglages SPIP',
+	// C
 	'ck_description' => 'Simplicité, efficacité, légèreté.
 _ Un couteau qui tient vraiment dans la poche en
 une unique page de configuration pour les réglages cachés de SPIP.',
-
+	'ck_nom' => 'Canivete KISS',
+	'ck_slogan' => 'Simplifier certains réglages SPIP'
 );
 
 ?>
