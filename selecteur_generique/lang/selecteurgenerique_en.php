5a83c594ab3d3088964da1706a1b9e7cd2711c1d
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-05 11:26:41 +0000

    langues (selecteurgenerique)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@56629 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- selecteur_generique/lang/selecteurgenerique_en.php
+++ selecteur_generique/lang/selecteurgenerique_en.php
@@ -1,13 +1,14 @@
 <?php
-
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-if (!defined("_ECRIRE_INC_VERSION")) return;
+// extrait automatiquement de http://trad.spip.org
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-	'pas_suggestion' => 'No suggestions',
-
+	// P
+	'pas_suggestion' => 'No suggestions'
 );
-
 
 ?>
--- selecteur_generique/lang/selecteurgenerique_fr.php
+++ selecteur_generique/lang/selecteurgenerique_fr.php
@@ -1,13 +1,12 @@
 <?php
-
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-if (!defined("_ECRIRE_INC_VERSION")) return;
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/selecteur_generique/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-	'pas_suggestion' => 'Pas de suggestion',
-
+	// P
+	'pas_suggestion' => 'Pas de suggestion'
 );
-
 
 ?>
