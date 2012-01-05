a93b4f658f6721d92ec08471dc52a980055448dc
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-05 11:26:26 +0000

    langues (motstechniques)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@56614 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- mots_techniques/trunk/lang/motstechniques_fr.php
+++ mots_techniques/trunk/lang/motstechniques_fr.php
@@ -1,11 +1,15 @@
 <?php
-
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/mots_techniques/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-'info_mots_cles_techniques'=>'Groupe de mots-cl&eacute;s techniques&nbsp;: ',
-'bouton_mots_cles_techniques'=>'Les mots-cl&eacute;s de ce groupe seront cach&eacute;s par d&eacute;faut dans l\'espace public ?',
+	// B
+	'bouton_mots_cles_techniques' => 'Les mots-clés de ce groupe seront cachés par défaut dans l\'espace public ?',
 
+	// I
+	'info_mots_cles_techniques' => 'Groupe de mots-clés techniques : '
 );
+
 ?>
