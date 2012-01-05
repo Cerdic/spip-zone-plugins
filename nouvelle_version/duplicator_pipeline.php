366871c61b77af6532078e0d2485b8a42c4a6249
frederic.bouteille.mae@gmail.com <frederic.bouteille.mae@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-05 14:04:14 +0000

    cache le bouton publie en ligne au profit de publier cette verison quand on est sur une nouvelle version de l article
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@56650 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- nouvelle_version/duplicator_pipeline.php
+++ nouvelle_version/duplicator_pipeline.php
@@ -20,6 +20,10 @@ function duplicator_boite_infos($flux){
 			//// on verifie les autorisations de publications sur l'article d'origine
 			if(autoriser('modifier','article',$flux['args']['row']['version_of'])){
 			$flux['data'] .= icone_horizontale(_T('duplicator:icone_remplacer_article'), generer_url_ecrire('remplacer_article',"id_objet=$id&id_orig=$idorig&type=$type"), "",_DIR_PLUGIN_DUPLICATOR."images/article_new_version-24.png", false);
+			$flux['data'] .= "<script>
+					$('.publie').hide();
+					</script>
+";	
 			}
 		}
 	//}
