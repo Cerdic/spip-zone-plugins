cf63f7d3b9c9f596a948cef6f470a0581475b4fe
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-05 09:48:19 +0000

    langues (spip_bonux)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@56582 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spip-bonux-3/lang/spip_bonux_en.php
+++ spip-bonux-3/lang/spip_bonux_en.php
@@ -1,29 +1,36 @@
 <?php
-
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org
+// ** ne pas modifier le fichier **
+
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-'afficher_calendrier' => 'Show the calendar',
-'annee_precedente' => 'Previous year',
-'annee_suivante' => 'Next year',
-'annuler_recherche' => 'Cancel the search',
+	// A
+	'afficher_calendrier' => 'Show the calendar',
+	'annee_precedente' => 'Previous year',
+	'annee_suivante' => 'Next year',
+	'annuler_recherche' => 'Cancel the search',
 
-'bouton_fermer' => "Close",
+	// B
+	'bouton_fermer' => 'Close',
 
-'erreur_date' => 'This date is incorrect',
-'erreur_date_corrigee' => 'The date has been corrected',
-'erreur_heure' => 'This hour is incorrect',
-'erreur_heure_corrigee' => 'The hour has been corrected',
+	// E
+	'erreur_date' => 'This date is incorrect',
+	'erreur_date_corrigee' => 'The date has been corrected',
+	'erreur_heure' => 'This hour is incorrect',
+	'erreur_heure_corrigee' => 'The hour has been corrected',
 
-'mois_precedent' => 'Previous month',
-'mois_suivant' => 'Next month',
-'id_rapide' => 'Ajout rapide',
-'pages' => 'Pages',
+	// I
+	'id_rapide' => 'Ajout rapide',
 
+	// M
+	'mois_precedent' => 'Previous month',
+	'mois_suivant' => 'Next month',
 
+	// P
+	'pages' => 'Pages'
 );
-
 
 ?>
--- spip-bonux-3/lang/spip_bonux_fr.php
+++ spip-bonux-3/lang/spip_bonux_fr.php
@@ -1,29 +1,34 @@
 <?php
-
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/spip-bonux-3/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-'afficher_calendrier' => 'Afficher le calendrier',
-'annee_precedente' => 'An&eacute;e pr&eacute;c&eacute;dente',
-'annee_suivante' => 'Ann&eacute;e suivante',
-'annuler_recherche' => 'Annuler la recherche',
+	// A
+	'afficher_calendrier' => 'Afficher le calendrier',
+	'annee_precedente' => 'Anée précédente',
+	'annee_suivante' => 'Année suivante',
+	'annuler_recherche' => 'Annuler la recherche',
 
-'bouton_fermer' => "Fermer",
+	// B
+	'bouton_fermer' => 'Fermer',
 
-'erreur_date' => 'Cette date est incorrecte',
-'erreur_date_corrigee' => 'La date a &eacute;t&eacute; corrig&eacute;e',
-'erreur_heure' => 'Cette heure est incorrecte',
-'erreur_heure_corrigee' => 'L\'heure a &eacute;t&eacute; corrig&eacute;e',
+	// E
+	'erreur_date' => 'Cette date est incorrecte',
+	'erreur_date_corrigee' => 'La date a été corrigée',
+	'erreur_heure' => 'Cette heure est incorrecte',
+	'erreur_heure_corrigee' => 'L\'heure a été corrigée',
 
-'mois_precedent' => 'Mois pr&eacute;c&eacute;dent',
-'mois_suivant' => 'Mois suivant',
-'id_rapide' => 'Ajout rapide',
-'pages' => 'Pages',
+	// I
+	'id_rapide' => 'Ajout rapide',
 
+	// M
+	'mois_precedent' => 'Mois précédent',
+	'mois_suivant' => 'Mois suivant',
 
+	// P
+	'pages' => 'Pages'
 );
-
 
 ?>
