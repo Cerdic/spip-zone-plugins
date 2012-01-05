e56b9e84e934e89ee19975b6e3ab9d47b7bfad1a
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-05 11:26:38 +0000

    langues (pp_codes)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@56626 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- porte_plume_extras/codes/trunk/lang/pp_codes_en.php
+++ porte_plume_extras/codes/trunk/lang/pp_codes_en.php
@@ -1,23 +1,34 @@
 <?php
-$GLOBALS[$GLOBALS['idx_lang']] = array(
-	//L
-	'label_activer_barre_edition' => "Site edit toolbar",
-	'label_activer_barre_forum' => "Forum toolbar",
-	
-	//O
-	'outil_inserer_cadre_spip' => "Insert tags for a SPIP class frame segment (cadre)",
-	'outil_inserer_cadre_php' => "Insert tags for a PHP class frame segment (cadre)",
-	'outil_inserer_pre' => "Insert tags for preformatted text (pre)",
-	'outil_inserer_var' => "Insert tags for a variable (var)",
-	'outil_inserer_samp' => "Insert tags for an escaped sample (samp)",
-	'outil_inserer_kbd' => "Insert tags for keyboard sequence (kdb)",
-	'outil_inserer_lien_trac' => "Insert a link to SPIP's trac repository",
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org
+// ** ne pas modifier le fichier **
 
-	//P
-	'pp_codes' => "Computer codes for Porte Plume",
-	
-	//T
-	'titre_configurer_pp_codes' => "Configurer the Porte Plume extension for special display codes",
-	'titre_activer_extension_sur' => "Activate on which toolbars?",
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// L
+	'label_activer_barre_edition' => 'Site edit toolbar',
+	'label_activer_barre_forum' => 'Forum toolbar',
+	'label_outils_actifs' => 'Outils actifs', # NEW
+
+	// O
+	'outil_inserer_cadre_css' => 'Insérer un code preformaté CSS (cadre)', # NEW
+	'outil_inserer_cadre_php' => 'Insert tags for a PHP class frame segment (cadre)',
+	'outil_inserer_cadre_spip' => 'Insert tags for a SPIP class frame segment (cadre)',
+	'outil_inserer_cadre_xml' => 'Insérer un code preformaté XML (cadre)', # NEW
+	'outil_inserer_kbd' => 'Insert tags for keyboard sequence (kdb)',
+	'outil_inserer_lien_trac' => 'Insert a link to SPIP\'s trac repository',
+	'outil_inserer_pre' => 'Insert tags for preformatted text (pre)',
+	'outil_inserer_samp' => 'Insert tags for an escaped sample (samp)',
+	'outil_inserer_var' => 'Insert tags for a variable (var)',
+
+	// P
+	'pp_codes' => 'Computer codes for Porte Plume',
+
+	// T
+	'titre_activer_extension_sur' => 'Activate on which toolbars?',
+	'titre_configurer_pp_codes' => 'Configurer the Porte Plume extension for special display codes'
 );
+
 ?>
--- porte_plume_extras/codes/trunk/lang/pp_codes_fr.php
+++ porte_plume_extras/codes/trunk/lang/pp_codes_fr.php
@@ -1,26 +1,32 @@
 <?php
-$GLOBALS[$GLOBALS['idx_lang']] = array(
-	//L
-	'label_activer_barre_edition' => "Barre d'&eacute;dition",
-	'label_activer_barre_forum' => "Barre de forum",
-	'label_outils_actifs' => "Outils actifs",
-	
-	//O
-	'outil_inserer_cadre_spip' => "Ins&eacute;rer un code preformat&eacute; SPIP (cadre)",
-	'outil_inserer_cadre_php' => "Ins&eacute;rer un code preformat&eacute; PHP (cadre)",
-	'outil_inserer_cadre_xml' => "Ins&eacute;rer un code preformat&eacute; XML (cadre)",
-	'outil_inserer_cadre_css' => "Ins&eacute;rer un code preformat&eacute; CSS (cadre)",
-	'outil_inserer_pre' => "Ins&eacute;rer un code preformat&eacute; (pre)",
-	'outil_inserer_var' => "Ins&eacute;rer une variable (var)",
-	'outil_inserer_samp' => "Ins&eacute;rer une sortie de code (samp)",
-	'outil_inserer_kbd' => "Ins&eacute;rer une entr&eacute;e clavier (kdb)",
-	'outil_inserer_lien_trac' => "Ins&eacute;rer un lien vers le trac de SPIP",
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/porte_plume_extras/codes/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
-	//P
-	'pp_codes' => "Codes informatiques pour Porte Plume",
-	
-	//T
-	'titre_configurer_pp_codes' => "Configurer l'extension codes informatiques pour Porte Plume",
-	'titre_activer_extension_sur' => "Activer sur quelles barres d'outils ?",
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// L
+	'label_activer_barre_edition' => 'Barre d\'édition',
+	'label_activer_barre_forum' => 'Barre de forum',
+	'label_outils_actifs' => 'Outils actifs',
+
+	// O
+	'outil_inserer_cadre_css' => 'Insérer un code preformaté CSS (cadre)',
+	'outil_inserer_cadre_php' => 'Insérer un code preformaté PHP (cadre)',
+	'outil_inserer_cadre_spip' => 'Insérer un code preformaté SPIP (cadre)',
+	'outil_inserer_cadre_xml' => 'Insérer un code preformaté XML (cadre)',
+	'outil_inserer_kbd' => 'Insérer une entrée clavier (kdb)',
+	'outil_inserer_lien_trac' => 'Insérer un lien vers le trac de SPIP',
+	'outil_inserer_pre' => 'Insérer un code preformaté (pre)',
+	'outil_inserer_samp' => 'Insérer une sortie de code (samp)',
+	'outil_inserer_var' => 'Insérer une variable (var)',
+
+	// P
+	'pp_codes' => 'Codes informatiques pour Porte Plume',
+
+	// T
+	'titre_activer_extension_sur' => 'Activer sur quelles barres d\'outils ?',
+	'titre_configurer_pp_codes' => 'Configurer l\'extension codes informatiques pour Porte Plume'
 );
+
 ?>
