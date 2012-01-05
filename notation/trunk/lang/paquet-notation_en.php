b49af7fea375a5ce39eb34f093c39ef46e4571b1
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-05 11:26:30 +0000

    langues (paquet-notation)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@56618 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- notation/trunk/lang/paquet-notation_en.php
+++ notation/trunk/lang/paquet-notation_en.php
@@ -1,22 +1,18 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-// 
-///  Fichier produit par PlugOnet
-// Module: paquet-notation
-// Langue: en
-// Date: 03-11-2011 23:09:03
-// Items: 3
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org
+// ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// N
+	// N
 	'notation_description' => 'Defines the <code>#FORMULAIRE_NOTATION</code> tag which allows the capture and the display of the rating of a loop element.
 _ Defines the models <code><autxx|notation|id_article=yy></code> which gives the author\'s rating <i>xx</i> for the article <i>yy</i>
 and <code><notation|topten></code> which gives the 10 best articles.',
 	'notation_nom' => 'Rating',
-	'notation_slogan' => 'Assign a rating to SPIP elements',
+	'notation_slogan' => 'Assign a rating to SPIP elements'
 );
+
 ?>
--- notation/trunk/lang/paquet-notation_fr.php
+++ notation/trunk/lang/paquet-notation_fr.php
@@ -1,22 +1,16 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-// 
-///  Fichier produit par PlugOnet
-// Module: paquet-notation
-// Langue: fr
-// Date: 03-11-2011 23:09:03
-// Items: 3
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/notation/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// N
+	// N
 	'notation_description' => 'Définition de la balise <code>#FORMULAIRE_NOTATION</code> permettant la saisie et l\'affichage de la note d\'un élément de boucle.
 _ Définition des modèles <code><autxx|notation|id_article=yy></code> qui donne la note de l\'auteur <i>xx</i> pour l\'article <i>yy</i>
 et <code><notation|topten></code> qui donne les 10 meilleurs articles.',
 	'notation_nom' => 'Notation',
-	'notation_slogan' => 'Attribuer une note aux éléments de SPIP',
+	'notation_slogan' => 'Attribuer une note aux éléments de SPIP'
 );
+
 ?>
