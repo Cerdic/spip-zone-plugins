97a1dd41cbd33af5442d09cd0ea4e8b5fed83c95
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-05 09:48:17 +0000

    langues (paquet-saisies)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@56580 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- saisies/lang/paquet-saisies_en.php
+++ saisies/lang/paquet-saisies_en.php
@@ -1,17 +1,19 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/menus/lang/
+// extrait automatiquement de http://trad.spip.org
+// ** ne pas modifier le fichier **
+
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
+	// S
 	'saisies_description' => 'This plugin makes it easier to write form fields by providing a #SAISIE tag. 
 		The generated HTML is compatible with the classification of forms
 		proposed by SPIP > 2.0 and with the configuration plugin CFG.',
 	'saisies_nom' => 'Entries for forms',
-	'saisies_titre' => 'Entries for forms',
 	'saisies_slogan' => 'Create easily forms fields.',
-
+	'saisies_titre' => 'Entries for forms'
 );
 
 ?>
