bd5ad17c4bd3128bcef095e4e8e2a98e2fda4b90
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-05 11:26:23 +0000

    langues (recommander)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@56612 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- messagerie_2_0/lang/recommander_es.php
+++ messagerie_2_0/lang/recommander_es.php
@@ -1,12 +1,22 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-'label_destinataire' => 'Nombre',
-'label_texte' => 'A&ntilde;adir mensaje',
-'label_envoyer' => 'Recomendar',
-'x_vous_recommande_le_vin' => '@nom_lien@ te recomiendo @titre_vin@',
-'recommander_a_un_ami' => 'Recomendar a un amigo o amiga',
 
+	// L
+	'label_destinataire' => 'Nombre',
+	'label_envoyer' => 'Recomendar',
+	'label_texte' => 'Añadir mensaje',
+
+	// R
+	'recommander_a_un_ami' => 'Recomendar a un amigo o amiga',
+
+	// X
+	'x_vous_recommande_le_vin' => '@nom_lien@ te recomiendo @titre_vin@'
 );
+
 ?>
