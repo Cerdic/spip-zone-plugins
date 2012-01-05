05fd8570dd49b9d6a45b79ec1f97d4117ab02e6e
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-05 09:48:05 +0000

    langues (paquet-ancresdouces)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@56568 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- ancres_douces/lang/paquet-ancresdouces_en.php
+++ ancres_douces/lang/paquet-ancresdouces_en.php
@@ -1,13 +1,15 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/comments/comments-200/lang/
+// extrait automatiquement de http://trad.spip.org
+// ** ne pas modifier le fichier **
+
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-	'ancresdouces_slogan' => 'Smooth anchors',
+	// A
 	'ancresdouces_description' => 'This plugin allows a continuous scrolling of the page when the visitor clicks on a link to an anchor of the same page.',
-
+	'ancresdouces_slogan' => 'Smooth anchors'
 );
 
 ?>
--- ancres_douces/lang/paquet-ancresdouces_fr.php
+++ ancres_douces/lang/paquet-ancresdouces_fr.php
@@ -1,13 +1,13 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/comments/comments-200/lang/
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/ancres_douces/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-	'ancresdouces_slogan' => 'Des ancres en douceur',
+	// A
 	'ancresdouces_description' => 'Ce plugin permet un défilement continu de la page lorsque le visiteur clique sur un lien pointant vers une ancre de l\'article en cours de lecture.',
-
+	'ancresdouces_slogan' => 'Des ancres en douceur'
 );
 
 ?>
