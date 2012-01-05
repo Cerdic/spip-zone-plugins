858b9705fc051cfe15c92a13ada3dbdae0d483fc
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-05 11:26:22 +0000

    langues (messagerie)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@56611 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- messagerie_2_0/lang/messagerie_es.php
+++ messagerie_2_0/lang/messagerie_es.php
@@ -1,24 +1,34 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-"aucun_message_recu" => 'Por ahora no se han recibido mensajes',
-"aucun_message_envoye" => 'No has enviado ning&uacute;n mensaje',
+	// A
+	'aucun_message_envoye' => 'No has enviado ningún mensaje',
+	'aucun_message_recu' => 'Por ahora no se han recibido mensajes',
 
-'label_selection' => 'Seleccionar',
+	// L
+	'label_selection' => 'Seleccionar',
 
-'titre_message_general'=>'Aviso general',
+	// M
+	'mail_sujet_envoyermessage' => '[@nom_site@] Tienes un nuevo mensaje',
+	'mail_texte_envoyermessage' => '@nom@ has enviado un nuevo mensaje (@url_site@) !',
 
-'texte_signature_email' => "\n----\nCorreo enviado por @nom_site@ (@url_site@)",
+	// R
+	'replier' => 'Reenviar este mensaje',
+	'repondre_message' => 'Contestar este mensaje',
 
-'texte_un_nouveau_message' => "Tienes <b>un</b> nuevo mensaje",
-'texte_des_nouveaux_messages' => "Tienes <b>@nb@</b> nuevos mensajes",
-
-'mail_sujet_envoyermessage'=>'[@nom_site@] Tienes un nuevo mensaje',
-'mail_texte_envoyermessage'=>'@nom@ has enviado un nuevo mensaje (@url_site@) !',
-
-'replier' => 'Reenviar este mensaje',
-'repondre_message' => 'Contestar este mensaje',
+	// T
+	'texte_des_nouveaux_messages' => 'Tienes <b>@nb@</b> nuevos mensajes',
+	'texte_signature_email' => '
+----
+Correo enviado por @nom_site@ (@url_site@)',
+	'texte_un_nouveau_message' => 'Tienes <b>un</b> nuevo mensaje',
+	'titre_message_general' => 'Aviso general'
 );
+
 ?>
--- messagerie_2_0/lang/messagerie_fr.php
+++ messagerie_2_0/lang/messagerie_fr.php
@@ -1,24 +1,32 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/messagerie_2_0/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-"aucun_message_recu" => 'Vous n\'avez re&ccedil;u aucun message.',
-"aucun_message_envoye" => 'Vous n\'avez envoy&eacute; aucun message.',
+	// A
+	'aucun_message_envoye' => 'Vous n\'avez envoyé aucun message.',
+	'aucun_message_recu' => 'Vous n\'avez reçu aucun message.',
 
-'label_selection' => 'Selectionner',
+	// L
+	'label_selection' => 'Selectionner',
 
-'titre_message_general'=>'Annonce g&eacute;n&eacute;rale',
+	// M
+	'mail_sujet_envoyermessage' => '[@nom_site@] Vous avez un nouveau message',
+	'mail_texte_envoyermessage' => '@nom@ vous a envoyé un nouveau message (@url_site@) !',
 
-'texte_signature_email' => "\n----\nCourriel envoy&eacute; par @nom_site@ (@url_site@)",
+	// R
+	'replier' => 'Replier ce message',
+	'repondre_message' => 'Répondre à ce message',
 
-'texte_un_nouveau_message' => "Vous avez <b>un</b> nouveau message",
-'texte_des_nouveaux_messages' => "Vous avez <b>@nb@</b> nouveaux messages",
-
-'mail_sujet_envoyermessage'=>'[@nom_site@] Vous avez un nouveau message',
-'mail_texte_envoyermessage'=>'@nom@ vous a envoy&eacute; un nouveau message (@url_site@) !',
-
-'replier' => 'Replier ce message',
-'repondre_message' => 'R&eacute;pondre &agrave; ce message',
+	// T
+	'texte_des_nouveaux_messages' => 'Vous avez <b>@nb@</b> nouveaux messages',
+	'texte_signature_email' => '
+----
+Courriel envoyé par @nom_site@ (@url_site@)',
+	'texte_un_nouveau_message' => 'Vous avez <b>un</b> nouveau message',
+	'titre_message_general' => 'Annonce générale'
 );
+
 ?>
