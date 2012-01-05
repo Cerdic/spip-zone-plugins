89dd8743ac3c4be3dc17ea6d2402be0a01e0e709
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-05 11:26:44 +0000

    langues (paquet-twidget)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@56632 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- twidget/lang/paquet-twidget_fr.php
+++ twidget/lang/paquet-twidget_fr.php
@@ -1,18 +1,11 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-// 
-///  Fichier produit par PlugOnet
-// Module: paquet-twidget
-// Langue: fr
-// Date: 10-11-2011 11:09:47
-// Items: 2
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/twidget/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// T
+	// T
 	'twidget_description' => 'Un widget Twitter facile a installer
 _ Ajoutez une inclusion dans vos squelettes pour afficher un widget twitter :
 _ <code>#INCLURE{fond=inclure/twidget_profile,user=GusLeLapin}</code>
@@ -20,6 +13,7 @@ _ <code>#INCLURE{fond=inclure/twidget_profile,user=GusLeLapin}</code>
 
  Le plugin fait office de proxy afin qu\'aucune requête ne soit faite par vos visiteurs vers twitter
  et éviter tout traçage possible de leur activité (préservation de leur vie personnelle).',
-	'twidget_slogan' => 'Un widget Twitter facile a installer',
+	'twidget_slogan' => 'Un widget Twitter facile a installer'
 );
+
 ?>
