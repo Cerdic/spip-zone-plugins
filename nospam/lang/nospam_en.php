b142138b8b0f9172550ff6487afaf9c1480057fb
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-05 09:48:12 +0000

    langues (nospam)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@56575 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- nospam/lang/nospam_en.php
+++ nospam/lang/nospam_en.php
@@ -1,14 +1,17 @@
-﻿<?php
+<?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-if (!defined("_ECRIRE_INC_VERSION")) return;
+// extrait automatiquement de http://trad.spip.org
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
+	// E
 	'erreur_jeton' => 'Unable to take account of your message. Thank you to resubmit!',
 	'erreur_spam' => 'Unable to take account of your message!',
 	'erreur_spam_doublon' => 'An identical message already exists!',
-	'erreur_spam_ip' => 'Too many comments detrimental to the quality!',
-
+	'erreur_spam_ip' => 'Too many comments detrimental to the quality!'
 );
 
 ?>
--- nospam/lang/nospam_es.php
+++ nospam/lang/nospam_es.php
@@ -1,14 +1,17 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-if (!defined("_ECRIRE_INC_VERSION")) return;
+// extrait automatiquement de http://trad.spip.org
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-	'erreur_jeton' => 'No se puede tener en cuenta su mensaje. &#161;Gracias a presentar de nuevo!',
+	// E
+	'erreur_jeton' => 'No se puede tener en cuenta su mensaje. ¡Gracias a presentar de nuevo!',
 	'erreur_spam' => 'No se puede tener en cuenta su mensaje!',
-	'erreur_spam_doublon' => 'Un mensaje id&#233;ntico ya existe!',
-	'erreur_spam_ip' => 'Tambi&#233;n muchos comentarios en detrimento de la calidad!',
-
+	'erreur_spam_doublon' => 'Un mensaje idéntico ya existe!',
+	'erreur_spam_ip' => 'También muchos comentarios en detrimento de la calidad!'
 );
 
 ?>
