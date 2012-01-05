9bf0746996ded5ee19642759dc6009e37632fcc0
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-05 11:26:19 +0000

    langues (paquet-player)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@56608 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- player/trunk/lang/paquet-player_fr.php
+++ player/trunk/lang/paquet-player_fr.php
@@ -1,24 +1,18 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-player
-// Langue: fr
-// Date: 22-11-2011 09:40:13
-// Items: 2
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/player/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// P
+	// P
 	'player_description' => 'Ce plugin permet la lecture et l\'affichage de sons ou de vidéos.
 Il ajoute des lecteurs flash adaptés aux formats .mp3.
 Il agit sur tous les <code><docXX|player></code> insérés dans les textes, aussi bien que dans les squelettes.
 
 -* Dans un texte <code><docXX|player></code> affiche un lecteur flash audio (plusieurs lecteurs au choix);
 -* Dans un squelette <code>#MODELE{playliste}</code> permet d\'afficher une playliste des derniers mp3',
-	'player_slogan' => 'Lire des sons et des vidéos',
+	'player_slogan' => 'Lire des sons et des vidéos'
 );
+
 ?>
