c639e47e13dfae09318092a15aaefa28d084547c
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-05 11:26:29 +0000

    langues (notation)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@56617 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- notation/trunk/lang/notation_en.php
+++ notation/trunk/lang/notation_en.php
@@ -1,85 +1,89 @@
 <?php
-/**
-* Plugin Notation v.0.5
-* par JEM (jean-marc.viglino@ign.fr) / b_b
-*
-* Copyright (c) 2008-2010
-* Logiciel libre distribue sous licence GNU/GPL.
-*
-* Contextualisation des messages
-*
-**/
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org
+// ** ne pas modifier le fichier **
 
-if (!defined("_ECRIRE_INC_VERSION")) return;
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-	'notation' => 'Ratings',
-
-	// Installation
-	'creation_des_tables_mysql' => 'Creating tables',
-	'creer_tables' => 'Create the tables',
-	'effacer_tables' => 'Erase the tables',
-	'afficher_tables' => 'Show ratings',
-
-	// Creation / destruction
-	'creation' => 'Creating tables',
-	'cree' => 'Tables created',
-	'destruction' => 'Destruction of tables',
-	'detruit' => 'Tables destroyed...',
-	'detruire' => '<strong style="color:red">Warning, this command will destroy the tables of the plugin !</strong><br />You should use it only if you want to uninstall the plugin...',
-
-	// Affichage
-	'vos_notes' => 'Your top 5 scores',
-	'topten' => 'The top 10 scores',
-	'toptenp' => 'The 10 highest scores (weighted)',
-	'topnb' => 'The 10 items which are the most rated',
-	'objets' => 'Items',
-	'articles' => 'Articles',
-	'notes' => 'Ratings',
-	'moyenne' => 'Average',
-	'votre_note' => 'Your rating',
-	'notesp' => 'Weighted scores',
-	'moyennep' => 'Weighted average',
-	'nbvotes' => 'Ratings&nbsp;nb',
-	'nbvotes_moyen' => 'Number of average votes by item : ',
-	'nbvotes_total' => 'Total number of votes on the site : ',
-	'nbobjets_note' => 'Number of rated items : ',
-
-  // Parametrage
-	'configuration_notation' => 'Configure ratings',
-	'param' => 'Setting',
-	'ponderation' => 'Weighting of the rating',
-	'info_ponderation' => 'The weighting factor lends more value to items that have received enough votes.<br />Enter below the number of votes beyond which you think the score is reliable.',
-	'valeur_ponderation' => 'Weighting factor',
+	// A
 	'acces' => 'Accessibility',
-	'info_acces' => 'Open voting : ',
+	'afficher_tables' => 'Show ratings',
+	'aide' => 'Help',
+	'articles' => 'Articles',
+	'auteur' => 'Author',
+
+	// B
+	'bouton_radio_fermee' => 'Fermée', # NEW
+	'bouton_radio_ouvert' => 'Ouverte', # NEW
+	'bouton_voter' => 'Vote',
+
+	// C
+	'change_note_label' => 'Allow voters to change their rating',
+	'configuration_notation' => 'Configure ratings',
+	'creation' => 'Creating tables',
+	'creation_des_tables_mysql' => 'Creating tables',
+	'cree' => 'Tables created',
+	'creer_tables' => 'Create the tables',
+
+	// D
+	'date' => 'date', # NEW
+	'derniers_votes' => 'Derniers votes', # NEW
+	'destruction' => 'Destruction of tables',
+	'detruire' => '<strong style="color:red">Warning, this command will destroy the tables of the plugin !</strong><br />You should use it only if you want to uninstall the plugin...',
+	'detruit' => 'Tables destroyed...',
+
+	// E
+	'effacer_tables' => 'Erase the tables',
+	'err_balise' => '[ NOTATION_ERR : tag outside an article loop ]',
+	'err_db_notation' => '[ RATING ERROR : only one rating by article ]',
 	'exemple' => 'Score distribution (score = 5, weighting factor = @ponderation@) : ',
-	'item_adm' => 'to the administrators',
-	'item_aut' => 'to the editors',
-	'item_ide' => 'to registered people',
-	'item_all' => 'to all',
-	'titre_ip' => 'Mode:',
+	'explication_accepter_note' => 'Si "fermée", la notation sera activable au cas par cas sur les objets ayant cette fonctionnalité.', # NEW
+
+	// I
+	'info_acces' => 'Open voting : ',
+	'info_etoiles' => 'This setting allows you to change the maximum value of the note (the number of stars between 1 and 10, and 5 by default).<br />
+                    <strong style="color:red">/!\\ Warning</strong> : you should not modify this setting after the scoring to be initiated because the ratings will not be calculated again and this can cause inconsistencies in the scoring ...<br />
+                    This parameter must be fixed once and for all when creating ratings.',
+	'info_fonctionnement_note' => 'Fonctionnement de la notation', # NEW
 	'info_ip' => 'To be the easiest possible to use, the rating is attached to the IP address of the voter, which avoids two successive votes in the database, with a few drawbacks ...  especially if you manage author\'s votes.<br />
 				In this case, we set the note on the user identifier (when he is registered, of course).<br />
                 If you want to guarantee the uniqueness of the note, limit the voting <b>only</b> to registered users (above).',
-	'item_ip' => 'one vote by IP',
+	'info_modifications' => 'Changing of the ratings',
+	'info_ponderation' => 'The weighting factor lends more value to items that have received enough votes.<br />Enter below the number of votes beyond which you think the score is reliable.',
+	'ip' => 'IP',
+	'item_adm' => 'to the administrators',
+	'item_all' => 'to all',
+	'item_aut' => 'to the editors',
 	'item_id' => 'One vote by user',
+	'item_ide' => 'to registered people',
+	'item_ip' => 'one vote by IP',
+
+	// J
+	'jaidonnemonavis' => 'I gave my opinion !',
+	'jaime' => 'I like',
+	'jaimepas' => 'I don\'t like',
+	'jaimeplus' => 'I no longer like',
+	'jechangedavis' => 'I withdraw my opinion',
+
+	// L
+	'label_accepter_note' => 'Statut de la notation sur tous les objets', # NEW
+
+	// M
+	'moyenne' => 'Average',
+	'moyennep' => 'Weighted average',
+
+	// N
 	'nb_etoiles' => 'Ratings value',
-	'info_etoiles' => 'This setting allows you to change the maximum value of the note (the number of stars between 1 and 10, and 5 by default).<br />
-                    <strong style="color:red">/!\ Warning</strong> : you should not modify this setting after the scoring to be initiated because the ratings will not be calculated again and this can cause inconsistencies in the scoring ...<br />
-                    This parameter must be fixed once and for all when creating ratings.',
-	'valeur_nb_etoiles' => 'Rating from 1 to ',
-
-	// Les erreurs
-	'err_balise' => '[ NOTATION_ERR : tag outside an article loop ]',
-	'err_db_notation' => '[ RATING ERROR : only one rating by article ]',
-	// Aide
-	'aide' => 'Help',
-
-  // Affichage des notes
+	'nbobjets_note' => 'Number of rated items : ',
+	'nbvotes' => 'Ratings nb',
+	'nbvotes_moyen' => 'Number of average votes by item : ',
+	'nbvotes_total' => 'Total number of votes on the site : ',
+	'notation' => 'Ratings',
+	'note' => 'Rating : ',
 	'note_1' => 'Rating : 1',
+	'note_10' => 'Rating : 10',
 	'note_2' => 'Rating : 2',
 	'note_3' => 'Rating : 3',
 	'note_4' => 'Rating : 4',
@@ -88,26 +92,30 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'note_7' => 'Rating : 7',
 	'note_8' => 'Rating : 8',
 	'note_9' => 'Rating : 9',
-	'note_10' => 'Rating : 10',
+	'note_pond' => 'Notes ponderées', # NEW
+	'notes' => 'Ratings',
 
-	// Formulaires
-	'voter' => 'Vote : ',
-	'note' => 'Rating : ',
-	'votes' => 'Votes',
+	// O
+	'objets' => 'Items',
+
+	// P
+	'param' => 'Setting',
+	'ponderation' => 'Weighting of the rating',
+
+	// T
+	'titre_ip' => 'Mode:',
+	'topnb' => 'The 10 items which are the most rated',
+	'topten' => 'The top 10 scores',
+	'toptenp' => 'The 10 highest scores (weighted)',
+
+	// V
+	'valeur_nb_etoiles' => 'Rating from 1 to ',
+	'valeur_ponderation' => 'Weighting factor',
+	'vos_notes' => 'Your top 5 scores',
 	'vote' => 'vote',
-	'bouton_voter' => 'Vote',
-	'auteur' => 'Author',
-	'ip' => 'IP',
-
-	'change_note_label' => "Allow voters to change their rating",
-	'info_modifications' => "Changing of the ratings",
-
-	'jaime' => 'I like',
-	'jaimepas' => 'I don\'t like',
-	'jaimeplus' => 'I no longer like',
-	'jechangedavis' => 'I withdraw my opinion',
-	'jaidonnemonavis' => 'I gave my opinion !',
-	
+	'voter' => 'Vote : ',
+	'votes' => 'Votes',
+	'votre_note' => 'Your rating'
 );
 
 ?>
