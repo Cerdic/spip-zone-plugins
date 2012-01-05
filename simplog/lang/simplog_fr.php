a21da008861c0de5686d662cbfc7b94325830bb6
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-05 09:48:18 +0000

    langues (simplog)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@56581 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- simplog/lang/simplog_fr.php
+++ simplog/lang/simplog_fr.php
@@ -1,72 +1,38 @@
 <?php
-
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/simplog/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// A
-'actualiser' => 'Actualiser',
-'autoriser_non' => 'Vous n\'&ecirc;tes pas autoris&eacute;.',
+	// A
+	'actualiser' => 'Actualiser',
+	'autoriser_non' => 'Vous n\'êtes pas autorisé.',
 
-// B
+	// C
+	'clic_rafraichir' => 'Cliquer pour rafraîchir',
+	'clic_senstri' => 'Cliquer pour inverser l\'ordre d\'affichage des données',
+	'consultation_logs' => 'Consultation des fichiers de traces',
 
-// C
-'clic_rafraichir' => 'Cliquer pour rafra&icirc;chir',
-'clic_senstri' => 'Cliquer pour inverser l\'ordre d\'affichage des donn&eacute;es',
-'consultation_logs' => 'Consultation des fichiers de traces',
+	// D
+	'description_simplog' => 'Ce plugin permet de visualiser simplement :) les fichiers de traces contenus dans le répertoire <i>tmp</i> de Spip.',
 
-// D
-'description_simplog' => 'Ce plugin permet de visualiser simplement :) les fichiers de traces contenus dans le répertoire <i>tmp</i> de Spip.',
+	// F
+	'fichier' => 'Fichier',
+	'fichier_inconnu' => 'Fichier inconnu',
 
+	// L
+	'liste_fic' => 'Liste des fichiers',
+	'logs' => 'Consultation des journaux',
 
-// E
+	// P
+	'plugin_simplog' => 'Plugin Simplog',
 
-// F
-'fichier' => 'Fichier',
-'fichier_inconnu' => 'Fichier inconnu',
+	// R
+	'rep_tmp' => 'Votre Répertoire',
 
-// G
-
-// H
-
-
-// I
-
-// J
-
-// L
-'liste_fic' => 'Liste des fichiers',
-'logs' => 'Consultation des journaux',
-
-
-// M
-
-
-// N
-
-// O
-
-
-// P
-'plugin_simplog' => 'Plugin Simplog',
-
-
-//R
-'rep_tmp' => 'Votre R&eacute;pertoire',
-
-// S
-
-
-// T
-
-
-// V
-
-
-
-// Z
-'z' => 'z'
-
+	// Z
+	'z' => 'z'
 );
 
 ?>
