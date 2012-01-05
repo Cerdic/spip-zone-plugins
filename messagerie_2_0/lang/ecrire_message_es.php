b2cd2db28230c12fc387b7fe6174215c9b6d88ca
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-05 11:26:20 +0000

    langues (ecrire_message)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@56609 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- messagerie_2_0/lang/ecrire_message_es.php
+++ messagerie_2_0/lang/ecrire_message_es.php
@@ -1,27 +1,38 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-'a_un_ami' => 'Escribir un mensaje',
-'label_destinataire' => 'Para',
-'label_objet' => 'T&iacute;tulo',
-'label_texte_message' => 'Mensaje',
-'label_envoyer' => 'Enviar',
-'envoyer_un_message' => 'enviar un mensaje',
 
-'selection_selectionner' => 'seleccionar',
-'selection_aucun' => 'ninguno',
-'selection_lus' => 'le&iacute;dos',
-'selection_non_lus' => 'no le&iacute;dos',
-'selection_tous' => 'todos',
-'label_effacer' => 'eliminar',
-'label_marquer_lus' => 'marcar como le&iacute;do',
-'label_marquer_non_lus' => 'marcar como no le&iacute;do',
+	// A
+	'a_un_ami' => 'Escribir un mensaje',
 
-'envoye_a' => 'A ',
+	// E
+	'envoye_a' => 'A ',
+	'envoyer_un_message' => 'enviar un mensaje',
 
-'message_envoye_erreur' => 'Ha ocurrido un error, el mensaje no se ha enviado',
-'message_envoye' => 'Mensaje enviado',
+	// L
+	'label_destinataire' => 'Para',
+	'label_effacer' => 'eliminar',
+	'label_envoyer' => 'Enviar',
+	'label_marquer_lus' => 'marcar como leído',
+	'label_marquer_non_lus' => 'marcar como no leído',
+	'label_objet' => 'Título',
+	'label_texte_message' => 'Mensaje',
 
+	// M
+	'message_envoye' => 'Mensaje enviado',
+	'message_envoye_erreur' => 'Ha ocurrido un error, el mensaje no se ha enviado',
+
+	// S
+	'selection_aucun' => 'ninguno',
+	'selection_lus' => 'leídos',
+	'selection_non_lus' => 'no leídos',
+	'selection_selectionner' => 'seleccionar',
+	'selection_tous' => 'todos'
 );
+
 ?>
--- messagerie_2_0/lang/ecrire_message_fr.php
+++ messagerie_2_0/lang/ecrire_message_fr.php
@@ -1,27 +1,36 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/messagerie_2_0/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-'a_un_ami' => 'Ecrire un message',
-'label_destinataire' => 'Destinataire',
-'label_objet' => 'Sujet',
-'label_texte_message' => 'Message',
-'label_envoyer' => 'Envoyer',
-'envoyer_un_message' => 'envoyer un message',
 
-'selection_selectionner' => 's&eacute;lectionner',
-'selection_aucun' => 'aucun',
-'selection_lus' => 'lus',
-'selection_non_lus' => 'non lus',
-'selection_tous' => 'tous',
-'label_effacer' => 'effacer',
-'label_marquer_lus' => 'marquer comme lu',
-'label_marquer_non_lus' => 'marquer comme non lu',
+	// A
+	'a_un_ami' => 'Ecrire un message',
 
-'envoye_a' => 'A ',
+	// E
+	'envoye_a' => 'A ',
+	'envoyer_un_message' => 'envoyer un message',
 
-'message_envoye_erreur' => 'Une erreur s\'est produite, votre message n\'a pas &eacute;t&eacute; envoy&eacute;',
-'message_envoye' => 'Votre message a bien &eacute;t&eacute; envoy&eacute;',
+	// L
+	'label_destinataire' => 'Destinataire',
+	'label_effacer' => 'effacer',
+	'label_envoyer' => 'Envoyer',
+	'label_marquer_lus' => 'marquer comme lu',
+	'label_marquer_non_lus' => 'marquer comme non lu',
+	'label_objet' => 'Sujet',
+	'label_texte_message' => 'Message',
 
+	// M
+	'message_envoye' => 'Votre message a bien été envoyé',
+	'message_envoye_erreur' => 'Une erreur s\'est produite, votre message n\'a pas été envoyé',
+
+	// S
+	'selection_aucun' => 'aucun',
+	'selection_lus' => 'lus',
+	'selection_non_lus' => 'non lus',
+	'selection_selectionner' => 'sélectionner',
+	'selection_tous' => 'tous'
 );
+
 ?>
