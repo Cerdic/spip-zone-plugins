7fe804957a930251165ce35735cb87605098519e
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-05 11:26:47 +0000

    langues (tradsync)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@56635 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- tradsync/lang/tradsync_en.php
+++ tradsync/lang/tradsync_en.php
@@ -1,102 +1,118 @@
 <?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org
+// ** ne pas modifier le fichier **
 
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-	//'articles_non_traduits' => "Articles non traduits (ou traductions non d&eacute;finies)",
-	'articles_non_traduits' => "Articles",
-	'articles_originaux_modifies' => "Articles",
-	'articles_mots_differents' => "Articles",
-	
-	'comparer_arborescence' => "Compare hierarchies",
-	'configurer_tradsync' => "Help with translations",
-	'configurer_tradsync_description' => "Configure the various elements
-		to fine tune the assistanec provided.",
-	
-	'espace_traducteurs' => "Translation zone",		
-	'explication_langue' => "Select the language to be used for checking
-		the status of translations.",
+	// A
+	'articles_mots_differents' => 'Articles',
+	'articles_non_traduits' => 'Articles',
+	'articles_originaux_modifies' => 'Articles',
 
-	'explication_comparer_arborescence' => "
+	// C
+	'comparer_arborescence' => 'Compare hierarchies',
+	'configurer_tradsync' => 'Help with translations',
+	'configurer_tradsync_description' => 'Configure the various elements
+		to fine tune the assistanec provided.',
+
+	// E
+	'editer_ce_mot' => 'Edit this keyword',
+	'editer_cet_article' => 'Edit this article',
+	'editer_cette_rubrique' => 'Edit this section',
+	'espace_traducteurs' => 'Translation zone',
+	'explication_comparer_arborescence' => '
 		Two sector navigators, one beside the other. These are used to visually
 		compare the hierarchies of two separate branches.
-	",
-	'explication_espace_traducteurs' => "
+	',
+	'explication_espace_traducteurs' => '
 		The translation zone aims to facilitate the task of seeking out
 		translations that need to be made or updated.
-	",
-	'explication_traductions_a_realiser' => "
-		This area lists all of the articles, sections and keywords
-		that do not appear to have been translated yet.
-	",
-	'explication_traductions_a_verifier' => "
-		This area lists all of the articles and sections including
-		the original article to be modified after the 
-		translation.
-	",
-	'explication_traductions_de_mots' => "
-		List the keywords which do not seem to have been translated yet.
-	",
-	'explication_traductions_mots_differents' => "
-		List the translated objects which do not have exactly
-		the same keywords assigned as for the original object.
-	",
-	'explication_traductions_mots_synchroniser' => "
-		The &laquo;synchronise&raquo; action copies all of the keywords from the original
-		element to the translated element. Note, however, that this does not delete any
-		additional keywords that have already been linked to the translated object.
-	",
-	'explication_groupe_mots_multi' => "
+	',
+	'explication_groupe_mots_multi' => '
 		Select the keywords group that will be used to collect translated words by using
 		the &lt;multi&gt; tags in titles. In this way, it will be possible to verify that
 		a translation is made for keywords in that group.
-	",
-	'editer_ce_mot' => "Edit this keyword",
-	'editer_cet_article' => "Edit this article",
-	'editer_cette_rubrique' => "Edit this section",
-	
+	',
+	'explication_langue' => 'Select the language to be used for checking
+		the status of translations.',
+	'explication_traductions_a_realiser' => '
+		This area lists all of the articles, sections and keywords
+		that do not appear to have been translated yet.
+	',
+	'explication_traductions_a_verifier' => '
+		This area lists all of the articles and sections including
+		the original article to be modified after the 
+		translation.
+	',
+	'explication_traductions_de_mots' => '
+		List the keywords which do not seem to have been translated yet.
+	',
+	'explication_traductions_mots_differents' => '
+		List the translated objects which do not have exactly
+		the same keywords assigned as for the original object.
+	',
+	'explication_traductions_mots_synchroniser' => '
+		The «synchronise» action copies all of the keywords from the original
+		element to the translated element. Note, however, that this does not delete any
+		additional keywords that have already been linked to the translated object.
+	',
+
 	// I
-	'info_actions' => "Actions",
-	'info_revisions' => "Revisions",
-	'info_source' => "Source",
-	'info_statut' => "Status",
-	'info_statut_court' => "S",
-	'info_titre' => "Title",
-	'info_type' => "Group",
-	
-	'label_langue' => "Target language",
-	'label_groupe_mots_multi' => "Multi keyword groups",
-	
-	'langue' => "Language",
-	'langue_court' => "L",
-	'langue_en_cours' => "The currently selected language is : <strong>@langue@</strong>.",
-	'liste_des_lieux' => "Areas to be explored",
+	'info_actions' => 'Actions',
+	'info_revisions' => 'Revisions',
+	'info_source' => 'Source',
+	'info_statut' => 'Status',
+	'info_statut_court' => 'S',
+	'info_titre' => 'Title',
+	'info_type' => 'Group',
 
-	'navigateurs' => "Navigators",
-	'non_traduit' => "Untranslated or translations not declared",
-	'mots_non_traduits' => "Untranslated keywords",
-	'mots_cles_non_traduits' => "Keywords",
-	'mots_cles_differents' => "Keywords different from the original",
-	
-	'origine_modifiee' => "Original modified",
-	//'rubriques_non_traduites' => "Rubriques non traduites (ou traductions non d&eacute;finies)",
-	'rubriques_non_traduites' => "Sections",
-	'rubriques_originales_modifiees' => "Sections",
-	'rubriques_mots_differents' => "Sections",
-	
-	'selection_de_la_langue_cible' => "Target language selection",
-	'second_navigateur' => "Second navigator",
-	'synchroniser' => "Synchronise",
-	'synchroniser_tout' => "Synchronise all",
-	
-	'traductions_a_realiser' => "Translations to be made",
-	'traductions_a_verifier' => "Translations to be checked",
-	'traductions_de_mots' => "Translations of keywords",
-	'traductions_mots_differents' => "Synchronise the keywords",
-	
-	
-	'zbug_critere_necessite_parametre' => "The {@critere@} criteria requires a parameter",
+	// L
+	'label_groupe_mots_multi' => 'Multi keyword groups',
+	'label_langue' => 'Target language',
+	'langue' => 'Language',
+	'langue_court' => 'L',
+	'langue_en_cours' => 'The currently selected language is : <strong>@langue@</strong>.',
+	'liste_des_lieux' => 'Areas to be explored',
 
+	// M
+	'mots_cles_differents' => 'Keywords different from the original',
+	'mots_cles_non_traduits' => 'Keywords',
+	'mots_non_traduits' => 'Untranslated keywords',
+
+	// N
+	'navigateurs' => 'Navigators',
+	'non_traduit' => 'Untranslated or translations not declared',
+
+	// O
+	'origine_modifiee' => 'Original modified',
+
+	// R
+	'rubriques_mots_differents' => 'Sections',
+	'rubriques_non_traduites' => 'Sections',
+	'rubriques_originales_modifiees' => 'Sections',
+
+	// S
+	'second_navigateur' => 'Second navigator',
+	'selection_de_la_langue_cible' => 'Target language selection',
+	'synchroniser' => 'Synchronise',
+	'synchroniser_tout' => 'Synchronise all',
+
+	// T
+	'traductions_a_realiser' => 'Translations to be made',
+	'traductions_a_verifier' => 'Translations to be checked',
+	'traductions_de_mots' => 'Translations of keywords',
+	'traductions_mots_differents' => 'Synchronise the keywords',
+
+	// V
+	'voir_article_source' => 'Voir l\'article source', # NEW
+	'voir_les_revisions_de_la_source' => 'Voir les révisions de la source', # NEW
+	'voir_rubrique_source' => 'Voir la rubrique source', # NEW
+
+	// Z
+	'zbug_critere_necessite_parametre' => 'The {@critere@} criteria requires a parameter'
 );
 
 ?>
--- tradsync/lang/tradsync_fr.php
+++ tradsync/lang/tradsync_fr.php
@@ -1,108 +1,116 @@
 <?php
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/tradsync/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-	//'articles_non_traduits' => "Articles non traduits (ou traductions non d&eacute;finies)",
-	'articles_non_traduits' => "Articles",
-	'articles_originaux_modifies' => "Articles",
-	'articles_mots_differents' => "Articles",
-	
-	'comparer_arborescence' => "Comparer l'arborescence",
-	'configurer_tradsync' => "Aide &agrave; la traduction",
-	'configurer_tradsync_description' => "Configurer les diff&eacute;rentes &eacute;l&eacute;ments
-		pour affiner l'aide donn&eacute;e.",
-	
-	'espace_traducteurs' => "Espace des traducteurs",		
-	'explication_langue' => "Choisir la langue dont il faut verifier
-		l'&eacute;tat des traductions.",
+	// A
+	'articles_mots_differents' => 'Articles',
+	'articles_non_traduits' => 'Articles',
+	'articles_originaux_modifies' => 'Articles',
 
-	'explication_comparer_arborescence' => "
-		Deux navigateurs de rubriques l'un &agrave; cot&eacute; de l'autre. Cela permet de comparer
+	// C
+	'comparer_arborescence' => 'Comparer l\'arborescence',
+	'configurer_tradsync' => 'Aide à la traduction',
+	'configurer_tradsync_description' => 'Configurer les différentes éléments
+		pour affiner l\'aide donnée.',
+
+	// E
+	'editer_ce_mot' => 'Éditer ce mot-clé',
+	'editer_cet_article' => 'Éditer cet article',
+	'editer_cette_rubrique' => 'Éditer cette rubrique',
+	'espace_traducteurs' => 'Espace des traducteurs',
+	'explication_comparer_arborescence' => '
+		Deux navigateurs de rubriques l\'un à coté de l\'autre. Cela permet de comparer
 		visuellement la hierarchie de deux branches.
-	",
-	'explication_espace_traducteurs' => "
-		L'espace des traducteurs a pour but de faciliter la t&acirc;che de recherche
-		de traductions &agrave; r&eacute;aliser ou &agrave; v&eacute;rifier.
-	",
-	'explication_traductions_a_realiser' => "
-		Cet espace liste l'ensemble des articles, rubriques et mots
-		qui ne semblent pas avoir &eacute;t&eacute; traduits.
-	",
-	'explication_traductions_a_verifier' => "
-		Cet espace liste l'ensemble des articles et rubriques dont
-		l'article d'origine a &eacute;t&eacute; modifi&eacute;
-		ult&eacute;rieurement &agrave; la traduction.
-	",
-	'explication_traductions_de_mots' => "
-		Lister les mots qui ne semblent pas &ecirc;tre traduits.
-	",
-	'explication_traductions_mots_differents' => "
-		Lister les objets traduits qui n'ont pas exactement
-		les m&ecirc;mes mots cl&eacute;s attribu&eacute;s par rapport &agrave; l'objet d'origine.
-	",
-	'explication_traductions_mots_synchroniser' => "
-		L'action &laquo;synchroniser&raquo; copie l'ensemble des mots de l'&eacute;l&eacute;ment
-		d'origine dans l'&eacute;l&eacute;ment traduit. Cependant, il ne supprime pas les
-		mots-cl&eacute;s qui seraient pr&eacute;sents en plus dans la traduction.
-	",
-	'explication_groupe_mots_multi' => "
+	',
+	'explication_espace_traducteurs' => '
+		L\'espace des traducteurs a pour but de faciliter la tâche de recherche
+		de traductions à réaliser ou à vérifier.
+	',
+	'explication_groupe_mots_multi' => '
 		Choisir les groupes de mots qui reçoivent des mots traduits en utilisant
 		des balises &lt;multi&gt; sur les titres. De la sorte, il sera possible de
-		verifier qu'une traduction est r&eacute;alis&eacute;e pour les mots de ces groupes.
-	",
-	'editer_ce_mot' => "&Eacute;diter ce mot-cl&eacute;",
-	'editer_cet_article' => "&Eacute;diter cet article",
-	'editer_cette_rubrique' => "&Eacute;diter cette rubrique",
+		verifier qu\'une traduction est réalisée pour les mots de ces groupes.
+	',
+	'explication_langue' => 'Choisir la langue dont il faut verifier
+		l\'état des traductions.',
+	'explication_traductions_a_realiser' => '
+		Cet espace liste l\'ensemble des articles, rubriques et mots
+		qui ne semblent pas avoir été traduits.
+	',
+	'explication_traductions_a_verifier' => '
+		Cet espace liste l\'ensemble des articles et rubriques dont
+		l\'article d\'origine a été modifié
+		ultérieurement à la traduction.
+	',
+	'explication_traductions_de_mots' => '
+		Lister les mots qui ne semblent pas être traduits.
+	',
+	'explication_traductions_mots_differents' => '
+		Lister les objets traduits qui n\'ont pas exactement
+		les mêmes mots clés attribués par rapport à l\'objet d\'origine.
+	',
+	'explication_traductions_mots_synchroniser' => '
+		L\'action «synchroniser» copie l\'ensemble des mots de l\'élément
+		d\'origine dans l\'élément traduit. Cependant, il ne supprime pas les
+		mots-clés qui seraient présents en plus dans la traduction.
+	',
 
 	// I
-	'info_actions' => "Actions",
-	'info_revisions' => "R&eacute;visions",
-	'info_source' => "Source",
-	'info_statut' => "Statut",
-	'info_statut_court' => "S",
-	'info_titre' => "Titre",
-	'info_type' => "Groupe",
-	
-	'label_langue' => "Langue cible",
-	'label_groupe_mots_multi' => "Groupes de mots multis",
-	
-	'langue' => "Langue",
-	'langue_court' => "L",
-	'langue_en_cours' => "La langue s&eacute;lectionn&eacute;e actuellement est : <strong>@langue@</strong>.",
-	'liste_des_lieux' => "Lieux &agrave; explorer",
+	'info_actions' => 'Actions',
+	'info_revisions' => 'Révisions',
+	'info_source' => 'Source',
+	'info_statut' => 'Statut',
+	'info_statut_court' => 'S',
+	'info_titre' => 'Titre',
+	'info_type' => 'Groupe',
 
-	'navigateurs' => "Navigateurs",
-	'non_traduit' => "Non traduits ou traductions non d&eacute;clar&eacute;es",
-	'mots_non_traduits' => "Mots-cl&eacute;s non traduits",
-	'mots_cles_non_traduits' => "Mots-cl&eacute;s",
-	'mots_cles_differents' => "Mots-cl&eacute;s diff&eacute;rents de l'original",
-	
-	'origine_modifiee' => "Original modifi&eacute;",
-	//'rubriques_non_traduites' => "Rubriques non traduites (ou traductions non d&eacute;finies)",
-	'rubriques_non_traduites' => "Rubriques",
-	'rubriques_originales_modifiees' => "Rubriques",
-	'rubriques_mots_differents' => "Rubriques",
+	// L
+	'label_groupe_mots_multi' => 'Groupes de mots multis',
+	'label_langue' => 'Langue cible',
+	'langue' => 'Langue',
+	'langue_court' => 'L',
+	'langue_en_cours' => 'La langue sélectionnée actuellement est : <strong>@langue@</strong>.',
+	'liste_des_lieux' => 'Lieux à explorer',
+
+	// M
+	'mots_cles_differents' => 'Mots-clés différents de l\'original',
+	'mots_cles_non_traduits' => 'Mots-clés',
+	'mots_non_traduits' => 'Mots-clés non traduits',
+
+	// N
+	'navigateurs' => 'Navigateurs',
+	'non_traduit' => 'Non traduits ou traductions non déclarées',
+
+	// O
+	'origine_modifiee' => 'Original modifié',
+
+	// R
+	'rubriques_mots_differents' => 'Rubriques',
+	'rubriques_non_traduites' => 'Rubriques',
+	'rubriques_originales_modifiees' => 'Rubriques',
 
 	// S
-	'selection_de_la_langue_cible' => "S&eacute;lection de la langue cible",
-	'second_navigateur' => "Second navigateur",
-	'synchroniser' => "Synchroniser",
-	'synchroniser_tout' => "Synchroniser tout",
+	'second_navigateur' => 'Second navigateur',
+	'selection_de_la_langue_cible' => 'Sélection de la langue cible',
+	'synchroniser' => 'Synchroniser',
+	'synchroniser_tout' => 'Synchroniser tout',
 
 	// T
-	'traductions_a_realiser' => "Traductions &agrave; r&eacute;aliser",
-	'traductions_a_verifier' => "Traductions &agrave; v&eacute;rifier",
-	'traductions_de_mots' => "Traductions de mots-cl&eacute;s",
-	'traductions_mots_differents' => "Synchroniser les mots cl&eacute;s",
+	'traductions_a_realiser' => 'Traductions à réaliser',
+	'traductions_a_verifier' => 'Traductions à vérifier',
+	'traductions_de_mots' => 'Traductions de mots-clés',
+	'traductions_mots_differents' => 'Synchroniser les mots clés',
 
 	// V
-	'voir_article_source' => "Voir l'article source",
-	'voir_rubrique_source' => "Voir la rubrique source",
-	'voir_les_revisions_de_la_source' => "Voir les r&eacute;visions de la source",
-		
-	'zbug_critere_necessite_parametre' => "Le critere {@critere@} necessite un parametre",
+	'voir_article_source' => 'Voir l\'article source',
+	'voir_les_revisions_de_la_source' => 'Voir les révisions de la source',
+	'voir_rubrique_source' => 'Voir la rubrique source',
 
+	// Z
+	'zbug_critere_necessite_parametre' => 'Le critere {@critere@} necessite un parametre'
 );
 
 ?>
