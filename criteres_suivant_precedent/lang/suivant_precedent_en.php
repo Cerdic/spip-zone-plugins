c7fd9daf508478ad48fa6fdc648d376bdebeece2
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-05 11:26:15 +0000

    langues (suivant_precedent)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@56604 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- criteres_suivant_precedent/lang/suivant_precedent_en.php
+++ criteres_suivant_precedent/lang/suivant_precedent_en.php
@@ -1,9 +1,12 @@
 <?php
-
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-if (!defined("_ECRIRE_INC_VERSION")) return;
+// extrait automatiquement de http://trad.spip.org
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+
 	// A
 	'alt_lien_next' => 'Next',
 	'alt_lien_prev' => 'Previous',
@@ -11,6 +14,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// N
 	'noisette_description' => 'Displays the navigation links to the previous article and the next article',
 	'noisette_label_meme_rubrique' => 'View the links only to articles in the same section',
-	'noisette_nom' => 'Links Previous / Next',
+	'noisette_nom' => 'Links Previous / Next'
 );
+
 ?>
--- criteres_suivant_precedent/lang/suivant_precedent_fr.php
+++ criteres_suivant_precedent/lang/suivant_precedent_fr.php
@@ -1,9 +1,10 @@
 <?php
-
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-if (!defined("_ECRIRE_INC_VERSION")) return;
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/criteres_suivant_precedent/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+
 	// A
 	'alt_lien_next' => 'Suivant',
 	'alt_lien_prev' => 'Précédent',
@@ -11,6 +12,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// N
 	'noisette_description' => 'Affiche les liens de navigation vers l\'article précédent et l\'article suivant',
 	'noisette_label_meme_rubrique' => 'Afficher les liens uniquement vers les articles de la même rubrique',
-	'noisette_nom' => 'Liens Précédent / Suivant',
+	'noisette_nom' => 'Liens Précédent / Suivant'
 );
+
 ?>
