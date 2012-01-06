eb10fae11b2cbe528572ac233d333fe71c6726e5
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-06 12:30:28 +0000

    langues (pp_codes)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@56716 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- porte_plume_extras/codes/trunk/lang/pp_codes_en.php
+++ porte_plume_extras/codes/trunk/lang/pp_codes_en.php
@@ -10,13 +10,13 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// L
 	'label_activer_barre_edition' => 'Site edit toolbar',
 	'label_activer_barre_forum' => 'Forum toolbar',
-	'label_outils_actifs' => 'Outils actifs', # NEW
+	'label_outils_actifs' => 'Active tools',
 
 	// O
-	'outil_inserer_cadre_css' => 'Insérer un code preformaté CSS (cadre)', # NEW
+	'outil_inserer_cadre_css' => 'Insert a CSS preformatted code (cadre)',
 	'outil_inserer_cadre_php' => 'Insert tags for a PHP class frame segment (cadre)',
 	'outil_inserer_cadre_spip' => 'Insert tags for a SPIP class frame segment (cadre)',
-	'outil_inserer_cadre_xml' => 'Insérer un code preformaté XML (cadre)', # NEW
+	'outil_inserer_cadre_xml' => 'Insert a preformatted XML code (cadre)',
 	'outil_inserer_kbd' => 'Insert tags for keyboard sequence (kdb)',
 	'outil_inserer_lien_trac' => 'Insert a link to SPIP\'s trac repository',
 	'outil_inserer_pre' => 'Insert tags for preformatted text (pre)',
