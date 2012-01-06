0129f8ab6298078c06b7fd1f0f299bec24e3cf57
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-06 12:30:25 +0000

    langues (nuage)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@56713 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ nuage/trunk/lang/nuage_en.php
@@ -0,0 +1,19 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// N
+	'noisette_description' => 'Display a tags cloud',
+	'noisette_label_afficher_titre' => 'Display the title:',
+	'noisette_label_niveau_titre' => 'Choose the heading level',
+	'noisette_nom_noisette' => 'Cloud',
+	'noisette_titre_defaut' => 'Tags',
+	'noisette_titre_noisette' => 'Title:'
+);
+
+?>
