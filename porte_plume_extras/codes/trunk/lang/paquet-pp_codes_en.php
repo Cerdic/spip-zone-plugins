4f3482b07b7e2efbaa4da7f2e6e2b02374bcbd17
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-06 12:30:29 +0000

    langues (paquet-pp_codes)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@56717 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ porte_plume_extras/codes/trunk/lang/paquet-pp_codes_en.php
@@ -0,0 +1,17 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// P
+	'pp_codes_description' => 'Buttons management to insert computer code with the Quill toolbar.
+Adds a shortcut <code>[->ecrire/inc_versions.php#trac]</code>.',
+	'pp_codes_nom' => 'Computer codes for Quill',
+	'pp_codes_slogan' => 'Adds buttons to the Quill toolbar to handle computer code'
+);
+
+?>
