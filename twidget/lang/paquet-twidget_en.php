476845e8a8fb8a2d1b48554f924b7aaa28aa6720
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-06 12:30:36 +0000

    langues (paquet-twidget)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@56724 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ twidget/lang/paquet-twidget_en.php
@@ -0,0 +1,20 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// T
+	'twidget_description' => 'A Twitter widget easy to install
+_ Add an include to your templates to display a Twitter widget:
+_ <code>#INCLURE{fond=inclure/twidget_profile,user=GusLeLapin}</code>
+ or <code>#INCLURE{fond=inclure/twidget_search,search=#SPIP,title=\'Follow Twitter\',subject=\'SPIP\'}</code>
+
+The plugin acts as a proxy so that no request is made by your visitors to Twitter and avoid the possibility to trace their activities (preservation of personal life).',
+	'twidget_slogan' => 'A Twitter widget easy to install'
+);
+
+?>
