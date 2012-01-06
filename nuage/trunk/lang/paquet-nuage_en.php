94b7653957668bf2803ceee455f8cf05729e3919
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-06 12:30:26 +0000

    langues (paquet-nuage)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@56714 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ nuage/trunk/lang/paquet-nuage_en.php
@@ -0,0 +1,20 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// N
+	'nuage_description' => 'This plugin displays the keywords in bulk by varying the font size on the number of items associated with the keyword. In other words, spip can create "tags"!
+
+Several models available:
+-* article_nuage : displays keywords of the article, the size varying according to the frequency of words in the articles of the Web site;
+-* rubrique_nuage : displays keywords of the articles in the branch, the size varying according to the frequency of words in the articles of this branch;
+-* nuage_popularite : displays the keywords of the Web site (or group), the size varying according to its relative popularity (sum of popularity of articles having that word);',
+	'nuage_slogan' => 'Display the keywords changing their font size'
+);
+
+?>
