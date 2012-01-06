d576b70c3c0abd759398996a3ac6e4a31ac039c0
bmarne@gmail.com <bmarne@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-06 21:48:15 +0000

    Toujours pas d'interface pour associer un mot à un mot, mais des modèles et une page de demo (/spip.php?page=demomo) pour voir un peu comment ça fonctionne...
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@56738 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ mots_sur_mots/lang/momo_fr.php
@@ -0,0 +1,35 @@
+<?php
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+//A
+//B
+//C
+//D
+//E
+	'enfants_du_mot_ou_des_mots' => 'Enfants de tous les mots-cl&eacute;s (qui en ont !)',
+//F
+//G
+//H
+//I
+//J
+//K
+//L
+//M
+//N
+//O
+//P
+	'parents_du_mot_ou_des_mots' => 'Parents de tous les mots-clés (qui en ont !)',
+	'plus_d_info_sur' => 'Plus d\'informations sur &laquo;&nbsp;@titre@&nbsp;&raquo;',
+//Q
+//R
+//S
+//T
+//U
+//V
+//W
+//X
+//Y
+//Z
+
+);
+?>
