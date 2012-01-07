4f3f9a35d7884b53b71ff3855afb8edeb91de817
marcel@bolla.fr <marcel@bolla.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-07 18:10:25 +0000

    Erreur dans le nom du champ "active"
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@56753 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- Association/Associaspip/inc/association_comptabilite.php
+++ Association/Associaspip/inc/association_comptabilite.php
@@ -293,7 +293,7 @@ function association_valider_plan_comptable()
 function association_liste_plan_comptable($val,$actives='') {
 	$res = array();
 	/* recupere le code et l'intitule de tous les comptes de classe $val */
-	$query = sql_select("code, intitule", "spip_asso_plan", "classe='$val'".($actives?" AND acti=$actives":""), "", "code");
+	$query = sql_select("code, intitule", "spip_asso_plan", "classe='$val'".($actives?" AND active=$actives":""), "", "code");
 	while ($data = sql_fetch($query)) {
 		$code = $data['code'];
 		$intitule = $data['intitule'];
