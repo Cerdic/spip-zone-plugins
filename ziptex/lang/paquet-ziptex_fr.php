848a9d57fdfcf618464ca201b8631fd0cb531736
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-08 18:34:53 +0000

    passage en paquet.xml
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@56797 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ ziptex/lang/paquet-ziptex_fr.php
@@ -0,0 +1,19 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-ziptex
+// Langue: fr
+// Date: 08-01-2012 19:32:07
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// Z
+	'ziptex_description' => 'Il s\'agit d\'une API permettant de regrouper en ZIP le résultat d\'une série de squelettes visant à produire à des fichiers .tex.',
+	'ziptex_slogan' => 'Empaqueter vos .tex !',
+);
+?>
