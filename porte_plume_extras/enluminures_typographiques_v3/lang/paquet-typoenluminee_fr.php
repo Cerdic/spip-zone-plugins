2b39c9655481d13ccc4ff89337421549807f97cd
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-08 17:11:10 +0000

    Un paquet.xml pour SPIP 3 généré avec PlugOnet Reste à passer la CFG à la mode SPIP 3
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@56786 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ porte_plume_extras/enluminures_typographiques_v3/lang/paquet-typoenluminee_fr.php
@@ -0,0 +1,32 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-typoenluminee
+// Langue: fr
+// Date: 08-01-2012 17:02:08
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// T
+	'typoenluminee_description' => 'Ce plugin ajoute des raccourcis typographiques et des boutons au porte plume. Par exemple :
+-* Paragraphes :
+-** {{Sous-titres}} <code>{{{**</code>Texte du titre<code>}}}</code>, le nombre d\'étoiles (*) variant de 2 à 5, la barre de raccourcis ne proposant que 2 et 3
+-** {{Centrer}} <code>[|</code>Paragraphe centré<code>|]</code>
+-** {{Aligner à droite}} <code>[/</code>Paragraphe aligné à droite<code>/]</code>
+-** {{Encadrer}} <code>[(</code>Paragraphe à encadrer<code>)]</code>
+-* Caractères :
+-** {{Mise en évidence}} <code>[*</code>texte en évidence<code>*]</code>
+-** {{Mise en évidence variante}} <code>[**</code>texte en évidence (autre couleur)<code>*]</code>
+-** {{Mise en exposant}} {&lt;sup&gt;}texte en exposant{&lt;/sup&gt;}
+-** {{Petites capitales}} {&lt;sc&gt;}texte en petite capitales{&lt;/sc&gt;}
+-** {{Biffé}} {&lt;del&gt;}texte biffé{&lt;/del&gt;}
+
+',
+	'typoenluminee_slogan' => 'Plein de raccourcis pour la mise en forme !',
+);
+?>
