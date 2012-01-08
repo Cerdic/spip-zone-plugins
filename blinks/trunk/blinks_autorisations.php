172565b906517b9e45622bcdf373575dc0a09970
l.oiseau2nuit@gmail.com <l.oiseau2nuit@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-08 15:45:51 +0000

    menues corrections : mes blinks s'inserent ENFIN en base de donnees...
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@56775 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- blinks/trunk/blinks_autorisations.php
+++ blinks/trunk/blinks_autorisations.php
@@ -3,17 +3,16 @@
 	// fonction pour le pipeline, n'a rien a effectuer
 	function blinks_autoriser(){}
 	// declarations d'autorisations
-	function autoriser_lien_bouton_dist($faire, $type, $id, $qui, $opt) {
-		return autoriser('voir', 'lien', $id, $qui, $opt);
+	function autoriser_blinks_bouton_dist($faire, $type, $id, $qui, $opt) {
+		return autoriser('voir', 'blinks', $id, $qui, $opt);
 	}
-	function autoriser_lien_voir_dist($faire, $type, $id, $qui, $opt) {
+	function autoriser_blinks_voir_dist($faire, $type, $id, $qui, $opt) {
 		return true;
 	}
-	function autoriser_lien_voir_dist($faire, $type, $id, $qui, $opt) {
-		return autoriser('modifier', 'lien', $id, $qui, $opt);
+	function autoriser_blink_voir_dist($faire, $type, $id, $qui, $opt) {
+		return autoriser('modifier', 'blink', $id, $qui, $opt);
 	}
-	function autoriser_lien_modifier_dist($faire, $type, $id, $qui, $opt) {
+	function autoriser_blink_modifier_dist($faire, $type, $id, $qui, $opt) {
 		return in_array($qui['statut'], array('0minirezo', '1comite'));
 	}	
-	
 ?>
