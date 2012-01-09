aa9a98f0de7a28dfce62e7b2231fc25d2c59b897
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-09 07:55:34 +0000

    langues (paquet-mots_techniques)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@56818 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ mots_techniques/trunk/lang/paquet-mots_techniques_sk.php
@@ -0,0 +1,19 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// M
+	'mots_techniques_description' => 'Par défaut, les boucles MOTS et GROUPES_MOTS filtreront les mots-clés 
+				techniques. Cependant, le critère <code>{tout}</code> permettra de renvoyer tous 
+				les mots clés et le critère <code>{technique=oui}</code> 
+				permet de sélectionner par type technique.', # NEW
+	'mots_techniques_slogan' => 'Skupine kľúčových slov pridá spresnenie "technické"',
+	'mots_techniques_titre' => 'Technické kľúčové slová'
+);
+
+?>
