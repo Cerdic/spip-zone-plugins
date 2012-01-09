fb88d22ef11e9135e22c75468500c8393a201e95
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-09 07:55:38 +0000

    langues (slogan)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@56820 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ slogan/lang/slogan_sk.php
@@ -0,0 +1,20 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'config_info_enregistree' => 'Nastavenia a ich uloženie',
+
+	// I
+	'info_slogan_site' => 'Slogan vašej stránky',
+
+	// T
+	'titre_identite_site' => 'Identita stránky'
+);
+
+?>
