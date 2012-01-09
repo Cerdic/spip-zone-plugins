bae71e75e2797653622f9ed285f4ebc817561e16
marcimat@rezo.net <marcimat@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-09 16:25:05 +0000

    On utilise {ajaxload} et le plugin inclure-ajaxload pour charger en ajax si l'on veut. L'utilisation d'APL de ZPIP ne correspondait en fait pas à ce qu'il fallait.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@56845 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spip-pmb/trunk/pmb_options.php
+++ spip-pmb/trunk/pmb_options.php
@@ -10,29 +10,4 @@ $GLOBALS['pmb_statut_nouvel_auteur'] = '6forum';
 $GLOBALS['exception_des_connect'][] = 'pmb';
 
 
-/**
- *  
- * Indiquer les inclusions en ajax parallele
- *  
-**/
-/*
-// indiquer qu'on peut mettre des inclusions a chargement ajax
-define('_Z_AJAX_PARALLEL_LOAD_OK', true);
-
-// il n'y a pas de solution en SPIP 3 encore geniale pour faire un {ajaxload} sur
-// une inclusion, mais on peut dire que tout ce qui se trouve dans le
-// repertoire 'ajaxload' sera charge en parallele.
-define('_Z_AJAX_PARALLEL_LOAD', 'ajaxload'); // separes par des virgules
-
-// pour que la definition de _Z_AJAX_PARALLEL_LOAD fonctionne
-// il faut que le bloc inclu soit un bloc Z
-if (!isset($GLOBALS['z_blocs'])) {
-	$GLOBALS['z_blocs'] = array('contenu');
-}
-
-if (!in_array('ajaxload', $GLOBALS['z_blocs'])) {
-	$GLOBALS['z_blocs'][] = 'ajaxload';
-}
-
-*/
 ?>
