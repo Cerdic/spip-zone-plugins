ef327d59c6cbf764e76fe1beb27300618deaf3f1
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-09 07:55:36 +0000

    langues (parrainage)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@56819 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ parrainage/trunk/lang/parrainage_sk.php
@@ -0,0 +1,66 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'ajouter_filleul_confirmation' => 'Kontakt bol pridaný, potom môžete tohto človeka pozvať.',
+
+	// C
+	'configuration_delai_sans_nouvelles_label' => 'Časový limit pre stav "Žiadne nové"',
+	'configuration_invitation_obligatoire_label' => 'Vyžaduje sa pozvánka',
+	'configuration_titre' => 'Nastavenia sponzoringu',
+
+	// E
+	'erreur_aucun_filleul' => 'Nevybrali ste žiaden subjekt, ktorý sponzorujete.',
+	'erreur_invitation_invalide' => 'Vaša pozvánka sa nezhoduje so zadanou e-mailovou adresou',
+	'erreur_invitation_obligatoire' => 'Na to, aby ste sa mohli zaregistrovať na tejto stránke, musíte mať pozvánku.',
+
+	// F
+	'filleul_actions' => 'Akcie',
+	'filleul_ajouter' => 'Pridať',
+	'filleul_email' => 'E-mailová adresa',
+	'filleul_nom' => 'Meno (názov)',
+	'filleul_statut' => 'Stav',
+
+	// I
+	'inscription_code_invitation_label' => 'Kód pozvánky',
+	'invitation_message' => '@nom_parrain@ vás pozýva, aby ste sa pripojili k stránke @site@.',
+	'invitation_sujet' => '@nom@ vás pozýva, aby ste sa pripojili k stránke @site@',
+	'invitation_url' => 'Na prihlásenie navštívte túto adresu:',
+
+	// P
+	'parrainage_inviter' => 'Poslať pozvánku',
+	'parrainage_message_aucun' => 'Nikoho z týchto ľudí netreba pozvať.',
+	'parrainage_message_aucun_1' => 'Tento človek je už pozvaný alebo zaregistrovaný.',
+	'parrainage_message_erreur' => 'Au moins une invitation n\'a pas été envoyée correctement.', # NEW
+	'parrainage_message_label' => 'Vaša správa',
+	'parrainage_message_ok_pluriel' => '@nombre@ invitations ont bien été programmées, elles sont maintenant en cours d\'envoi.', # NEW
+	'parrainage_message_ok_singulier' => 'Une invitation a bien été programmée, elle est maintenant en cours d\'envoi.', # NEW
+	'parrainage_supprimer_filleul' => 'Odstrániť tento sponzorovaný subjekt',
+	'parrainage_supprimer_filleul_confirmation' => 'Určite chcete odstrániť @nom@ zo svojich kontaktov?',
+	'plugin_nom' => 'Sponzoring',
+
+	// S
+	'statut_contact' => 'Kontakt',
+	'statut_contact_explication' => 'Cette personne est dans votre carnet d\'adresse. Vous pouvez l\'inviter à rejoindre le site.', # NEW
+	'statut_deja_inscrit' => 'Už zaregistrovaný',
+	'statut_deja_inscrit_explication' => 'Cette personne est déjà inscrite sur le site, mais n\'est pas votre filleul !', # NEW
+	'statut_en_cours' => 'Pozvánka sa posiela',
+	'statut_en_cours_explication' => 'Vaša pozvánka sa posiela.',
+	'statut_filleul' => 'Sponzorovaný subjekt',
+	'statut_filleul_depuis' => 'Filleul inscrit depuis @date@', # NEW
+	'statut_filleul_explication' => 'Grâce à vous, cette personne s\'est inscrite sur le site : vous êtes son parrain.', # NEW
+	'statut_invite' => 'Invité @duree@', # NEW
+	'statut_invite_explication' => 'Vous avez déjà invité cette personne il y a peu de temps.', # NEW
+	'statut_sans_nouvelles' => 'Žiadni noví od @date@',
+	'statut_sans_nouvelles_explication' => 'Vous avez invité cette personne il y a un certain temps, mais elle n\'est jamais venue.', # NEW
+	'statut_visite' => 'Navštívil(a) stránku',
+	'statut_visite_explication' => 'Tento človek už dostal vašu pozvánku, aby prišiel na stránku, ale nezaregistroval sa.'
+);
+
+?>
