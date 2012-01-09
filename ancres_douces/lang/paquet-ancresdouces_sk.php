68ac82383ff6069d079477935278a2895fa5c27a
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-09 07:55:26 +0000

    langues (paquet-ancresdouces)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@56812 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ ancres_douces/lang/paquet-ancresdouces_sk.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'ancresdouces_description' => 'Tento zásuvný modul umožňuje nepretržité posúvanie stránky, keď návštevník klikne na odkaz na kotvu na rovnakej stránke.',
+	'ancresdouces_slogan' => 'Kotvy s ľahkosťou'
+);
+
+?>
