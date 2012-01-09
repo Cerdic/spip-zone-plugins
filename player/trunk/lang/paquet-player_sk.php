735bb353b87d4aca93e75e387124bde0548b8bd4
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-09 07:55:33 +0000

    langues (paquet-player)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@56816 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ player/trunk/lang/paquet-player_sk.php
@@ -0,0 +1,20 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// P
+	'player_description' => 'Ce plugin permet la lecture et l\'affichage de sons ou de vidéos.
+Il ajoute des lecteurs flash adaptés aux formats .mp3.
+Il agit sur tous les <code><docXX|player></code> insérés dans les textes, aussi bien que dans les squelettes.
+
+-* Dans un texte <code><docXX|player></code> affiche un lecteur flash audio (plusieurs lecteurs au choix);
+-* Dans un squelette <code>#MODELE{playliste}</code> permet d\'afficher une playliste des derniers mp3', # NEW
+	'player_slogan' => 'Prehrávanie zvukov a videí'
+);
+
+?>
