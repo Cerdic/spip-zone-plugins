9dba608d8f39d7b593d9b83ce9a90b3c2169761c
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-09 07:55:28 +0000

    langues (paquet-boutonstexte)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@56813 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ boutonstexte/trunk/lang/paquet-boutonstexte_sk.php
@@ -0,0 +1,18 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'boutonstexte_description' => 'Rajoute des boutons pour l\'affichage des textes comme augmenter/diminuer police. Les boutons sont rajoutés avant chaque div de classe texte du contenu si ils ne sont pas déjà présents dans la page (images de classe textsizeup, textsizedown ou textonly)
+
+La configuration optionnelle peut se faire en installant le plugin cfg. Certains éléments peuvent être forcés par javascript.', # NEW
+	'boutonstexte_nom' => 'Tlačidlá v texte',
+	'boutonstexte_slogan' => 'Tlačidlá na zmenu veľkosti písma'
+);
+
+?>
