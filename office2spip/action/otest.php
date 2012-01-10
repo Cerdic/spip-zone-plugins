6bdc2dca1d60f94bbbde9f659cf3dd371e607e9c
fil@rezo.net <fil@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-10 23:01:23 +0000

    affiner un peu l'api
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@56896 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- office2spip/action/otest.php
+++ office2spip/action/otest.php
@@ -1,20 +1,22 @@
 <?php
 
+//
+// Ouvrir n'importe quel fichier avec LibreOffice et le renvoyer au format SPIP
+//
+// usage de ce webservice :
+// curl -F "file=@toto.odt" "http://SERVEUR/?action=otest&secret=xxxx&fmt=spip"
+// renvoie le fichier au format SPIP, dans un JSON
+//
+// - format de sortie dans &fmt=(spip|html|text)
+// - images (en base64) avec &attachments=1
+//
+// TODO :
+// - utiliser job_queue pour passer en asynchrone
+// - codes de retours propres (ex: 404 : inconnu, 503 = pas encore converti)
+// - etc.
+
 function action_otest() {
-	include_spip('inc/securiser_action');
-	$secret = substr(md5('otest+'.secret_du_site()), 0,6);
-
-
-	if (_request('secret') != $secret) {
-		if (!autoriser('ecrire'))
-			echo "Connectez-vous pour connaitre le secret.";
-		else
-			echo "Le secret : ".$secret;
-
-		exit;
-	}
-
-	include_spip('inc/fonctionsale');
+	office_controler_secret();
 
 	$rep = sous_repertoire(_DIR_TMP, 'upload_office');
 
@@ -24,11 +26,30 @@ function action_otest() {
 			if ($dst = office_load($file, $rep)) {
 				unset($file['tmp_name']);
 				$file['md5'] = $dst;
-				$file['text'] = sale(@file_get_contents($rep.$dst.'.html'));
-				if ($g = glob($rep.$dst.'_*')) {
-					$file['attachments'] = array();
-					foreach($g as $j) {
-						$file['attachments'][basename($j)] = base64_encode(file_get_contents($j));
+
+				$html = @file_get_contents($rep.$dst.'.html');
+				switch(_request('fmt')) {
+					case 'html':
+						$file['html'] = $html;
+						break;
+					case 'spip':
+						include_spip('inc/fonctionsale');
+						$file['spip'] = sale($html);
+					default:
+						include_spip('inc/filtres');
+						include_spip('inc/charset');
+						$file['text'] = unicode_to_utf_8(html2unicode(
+							trim(supprimer_tags($html))
+						));
+						break;
+				}
+
+				if (_request('attachments')) {
+					if ($g = glob($rep.$dst.'_*')) {
+						$file['attachments'] = array();
+						foreach($g as $j) {
+							$file['attachments'][basename($j)] = base64_encode(file_get_contents($j));
+						}
 					}
 				}
 				$res[] = $file;
@@ -41,7 +62,7 @@ function action_otest() {
 
 
 function office_load($file, $rep) {
-	if (!preg_match(',\.(docx|doc|rtf|xls|xlsx)$,', $file['name'], $r))
+	if (!preg_match(',\.(docx|doc|rtf|xls|xlsx|odt)$,', $file['name'], $r))
 		return;
 
 	$ext = $r[1];
@@ -59,4 +80,19 @@ function office_load($file, $rep) {
 
 function office_convertir($doc) {
 	exec("unoconv --format=html $doc");
+}
+
+// securiser en demandant le secret
+function office_controler_secret() {
+	include_spip('inc/securiser_action');
+	$secret = substr(md5('otest+'.secret_du_site()), 0,6);
+
+	if (_request('secret') != $secret) {
+		if (!autoriser('ecrire'))
+			echo "Connectez-vous pour connaitre le secret.";
+		else
+			echo "Le secret : ".$secret;
+
+		exit;
+	}
 }
