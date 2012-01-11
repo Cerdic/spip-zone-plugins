8a6ddb533b39f08a8b386583eab44b0dc88a87cb
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-11 22:24:17 +0000

    langues (paquet-boussole)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@56935 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ boussole/lang/paquet-boussole_sk.php
@@ -0,0 +1,18 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'boussole_description' => 'Installez ce plugin sur votre site et vous aurez accès, dans vos squelettes, à l\'ensemble des sites de la Galaxie SPIP en utilisant
+les modèles, les noisettes ou la page Z proposés.
+_ Pour les bricoleurs, vous pouvez aussi utiliser ce plugin pour afficher votre propre liste de sites et concevoir vos propres affichages.', # NEW
+	'boussole_nom' => 'Buzola',
+	'boussole_slogan' => 'Obľúbené adresy v galaxii SPIPu alebo inde!'
+);
+
+?>
