580fee10155493ca75e99b27450a79b73cc9f398
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-13 08:24:17 +0000

    langues (paquet-yaml)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@57037 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ yaml/lang/paquet-yaml_sk.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// Y
+	'yaml_description' => 'Ce plugin fournit les fonctions de lecture/écriture du format YAML:
+	<code>yaml_decode()</code> et <code>yaml_encode()</code>. Il fournit aussi le format yaml pour la boucle (DATA).', # NEW
+	'yaml_slogan' => 'Jednoduchý formát súborov na upravovanie zoznamov dát'
+);
+
+?>
