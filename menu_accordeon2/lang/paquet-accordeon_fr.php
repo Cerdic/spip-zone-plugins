66fdb0e28921ef0cf547a2446d6056cb7cc835d0
yffic@lefourneau.com <yffic@lefourneau.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-15 14:38:28 +0000

    Etait noté compatible Spip3 par erreur. Mise en compat Spip3
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@57127 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ menu_accordeon2/lang/paquet-accordeon_fr.php
@@ -0,0 +1,19 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-accordeon
+// Langue: fr
+// Date: 15-01-2012 13:24:52
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// A
+	'accordeon_description' => 'Un menu accordèon avec le plugin menu',
+	'accordeon_slogan' => 'Animer le menu de SPIP avec un effet d\'ouverture de tiroir',
+);
+?>
