32b46e2a06cb050875c19c9695264c17ca787adf
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-16 13:51:42 +0000

    création de la table spip_gre ves
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@57206 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ greve/base/greves.php
@@ -0,0 +1,25 @@
+<?php
+function greves_declarer_tables_interfaces($interface){
+	$interface['table_des_tables']['greves'] = 'greves';	
+	$interface['table_des_traitements']['TEXTE']['greve'] = _TRAITEMENT_TYPO;
+	return $interface;
+}
+function greves_declarer_tables_principales($tables_principales){
+	//-- Table GREVES ------------------------------------------
+	$greves = array(
+			"id_greve"	=> "bigint(21) NOT NULL",
+			"texte"	=> "text DEFAULT '' NOT NULL",
+			"titre"	=> "tinytext DEFAULT '' NOT NULL",
+			"debut"	=> "datetime",
+			"fin"	=> "datetime"
+			);
+	
+	$greves_key = array(
+			"PRIMARY KEY"	=> "id_greve",
+			);
+	
+	$tables_principales['spip_greves'] =
+		array('field' => &$greves, 'key' => &$greves_key);
+	return $tables_principales;
+}
+?>
---
+++ greve/base/greves_upgrade.php
@@ -0,0 +1,17 @@
+<?php
+include_spip('inc/meta');
+include_spip('base/create');
+function greves_upgrade($nom_meta_base_version, $version_cible){
+	$current_version = "0.0";
+	if (isset($GLOBALS['meta'][$nom_meta_base_version]))
+		$current_version = $GLOBALS['meta'][$nom_meta_base_version];
+	if ($current_version=="0.0") {
+		creer_base();
+		ecrire_meta($nom_meta_base_version, $current_version=$version_cible);
+	}
+}
+function greves_vider_tables($nom_meta_base_version) {
+	sql_drop_table("spip_greves");
+	effacer_meta($nom_meta_base_version);
+}
+?>
