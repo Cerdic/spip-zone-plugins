37b4ed817ec7e8b1fe368f188854687f9d920373
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-16 13:51:55 +0000

    exception pour la page qui permet de se connecter à l'EP
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@57219 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- greve/greves_pipelines.php
+++ greve/greves_pipelines.php
@@ -1,9 +1,8 @@
 <?php
 if (!defined("_ECRIRE_INC_VERSION")) return;
 function greves_styliser($flux){
-	
-	if($id_greve=greve_active()){
-		if(substr_count($flux['args']['fond'],'/')==0 and $flux['args']['fond']!='inc-head' and $flux['args']['fond']!='inc-entete'){
+	if($id_greve=greve_active() and !test_espace_prive()){
+		if(substr_count($flux['args']['fond'],'/')==0 and $flux['args']['fond']!='inc-head' and $flux['args']['fond']!='inc-entete' and $flux['args']['contexte']['page']!='login'){
 
 			$flux['data'] 				= 	str_replace('.html','',find_in_path('greve.html'));
 			$flux['args']['fond']		= 	'greve';
