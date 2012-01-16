a6e8cb1d30dc6a7de3b4bb68e3105f5a09ce09c6
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-16 13:51:45 +0000

    liste des grèves
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@57209 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- greve/lang/greves_fr.php
+++ greve/lang/greves_fr.php
@@ -3,13 +3,17 @@
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+	'explications'			=>'Entre les dates comprises dans une grève, on ne peut plus consulter le site. À la place s\'affiche le texte de la grève',
 	'greves'				=> 'Gr&egrave;ves',
 	'info_modifier_greve'	=>'Modifier une gr&egrave;ve',
 	'label_debut'			=>'D&eacute;but [Obligatoire]',
+	'label_id'				=> 'Id',
 	'label_fin'				=>'Fin [Obligatoire]',
 	'label_titre'			=>'Titre',
 	'label_texte'			=>'Texte',
+	'liste_des_greves'		=>'Liste des grèves',
 	'texte_nouvelle_greve'	=>'Nouvelle gr&egrave;ve'
+
 );
 
 ?>
