20537af8d6969f3928d1a95dc8d379d875eb3cf2
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-16 14:37:29 +0000

    paquet.xml pour spip 3
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@57229 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ greves/lang/paquet-greves_fr.php
@@ -0,0 +1,21 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-greves
+// Langue: fr
+// Date: 16-01-2012 15:35:37
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// G
+	'greves_description' => 'Permet de mettre temporairement un site hors service pour protester. On peut éditer une grève, qui reçoit un titre, un texte explicatif, une date de début et une de fin.
+    
+    Entre la date de début et la date de fin, le site n\'affiche plus que le titre de la grève et son explication.',
+	'greves_slogan' => 'Ce n\'est qu\'un début, continuons le combat !',
+);
+?>
