73a5c9e4532e616ca9841cf63e92823bb8783b73
denisb@laposte.net <denisb@laposte.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-17 07:11:30 +0000

    nospam : surcharge  du fichier de langue de l'extension forum.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@57252 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- nospam/lang/nospam_fr.php
+++ nospam/lang/nospam_fr.php
@@ -9,7 +9,11 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'erreur_jeton' => 'Impossible de prendre en compte votre message. Merci de le soumettre à nouveau !',
 	'erreur_spam' => 'Impossible de prendre en compte votre message !',
 	'erreur_spam_doublon' => 'Un message identique existe déjà !',
-	'erreur_spam_ip' => 'Trop de commentaires nuit à la qualité !'
+	'erreur_spam_ip' => 'Trop de commentaires nuit à la qualité !',
+
+	// F
+	'forum_saisie_texte_info' => 'Pour la mise en forme de votre message, ce formulaire n\'accepte que les raccourcis SPIP <code>[-&gt;url] {{gras}} {italique} &lt;quote&gt; &lt;code&gt;</code> &lt;cadre&gt;</cadre> et le code HTML <code>&lt;q&gt; &lt;del&gt; &lt;ins&gt;</code>. Pour créer des paragraphes, laissez simplement des lignes vides.'
+
 );
 
 ?>
