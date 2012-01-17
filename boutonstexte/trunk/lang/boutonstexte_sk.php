1dffc45e88cce760263276210a2727c44d184646
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-17 07:28:44 +0000

    langues (boutonstexte)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@57256 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ boutonstexte/trunk/lang/boutonstexte_sk.php
@@ -0,0 +1,40 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'augmenter_police' => 'Podmienky zväčšenia',
+
+	// B
+	'boutonstexte_nom' => 'Tlačidlá v texte',
+
+	// D
+	'diminuer_police' => 'Podmienky zmenšenia',
+	'donnees_techniques' => 'Technické dáta sa nemenia',
+
+	// L
+	'label_selector' => 'Výraz CSS alebo rozšírený výraz jQuery',
+	'label_skin' => 'Vzhľad tlačidiel',
+	'label_txtBackSpip' => 'Text pre návrat',
+	'label_txtOnly' => 'Iba text *',
+	'label_txtSizeDown' => 'Zmenšiť text *',
+	'label_txtSizeUp' => 'Zväčšiť text *',
+	'legend_apparence' => 'Vzhľad',
+
+	// R
+	'retour_a_spip' => 'Vrátiť sa na úplnú stránku',
+
+	// S
+	'selecteur' => 'Vyberte text, ktorý sa má upraviť',
+
+	// T
+	'texte_seulement' => 'Iba text',
+	'textes_icones' => 'Text bublinkovej nápovede'
+);
+
+?>
