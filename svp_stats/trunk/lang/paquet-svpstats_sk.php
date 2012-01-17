f846684d1e115241d51a10c6fd500c0160bfb77c
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-17 07:28:52 +0000

    langues (paquet-svpstats)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@57264 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ svp_stats/trunk/lang/paquet-svpstats_sk.php
@@ -0,0 +1,17 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// S
+	'svpstats_description' => 'Ce plugin est un module optionnel de SVP. 
+_ Il permet d\'acquérir, de mettre à jour et de restituer les statistiques d\'utilisation des plugins SPIP sur le net. 
+Ces statistiques sont issues du site <a href="http://stats.spip.org">stats.spip.org</a>.', # NEW
+	'svpstats_slogan' => 'Modul SVP na správu štatistík využívania zásuvných modulov'
+);
+
+?>
