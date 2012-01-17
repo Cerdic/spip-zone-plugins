586832d3d15a050c4ad4af585fa5017f79d00d51
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-17 07:28:55 +0000

    langues (paquet-twidget)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@57267 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ twidget/lang/paquet-twidget_sk.php
@@ -0,0 +1,21 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// T
+	'twidget_description' => 'Un widget Twitter facile a installer
+_ Ajoutez une inclusion dans vos squelettes pour afficher un widget twitter :
+_ <code>#INCLURE{fond=inclure/twidget_profile,user=GusLeLapin}</code>
+ ou <code>#INCLURE{fond=inclure/twidget_search,search=#SPIP,title=\'Suivez twitter\',subject=\'SPIP\'}</code>
+
+ Le plugin fait office de proxy afin qu\'aucune requête ne soit faite par vos visiteurs vers twitter
+ et éviter tout traçage possible de leur activité (préservation de leur vie personnelle).', # NEW
+	'twidget_slogan' => 'Jednoduchý widget pre Twitter na nainštalovanie'
+);
+
+?>
