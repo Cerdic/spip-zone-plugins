c2adad974503e2e7618f8181d34c3044814f43aa
yffic@lefourneau.com <yffic@lefourneau.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-18 19:53:26 +0000

    Modif d'une chaîne de langue, la taxe n'étant pas définie dans Paypal
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@57312 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- produits/lang/produits_fr.php
+++ produits/lang/produits_fr.php
@@ -6,7 +6,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// C
-	'configurer_taxe_defaut_explication' => 'Saisir la taxe par défaut applicable aux produits sous la forme 0.196 pour une TVA à 19,6% par exemple (cette valeur peut être surchargée ensuite pour chaque produit). Attention cependant à ne pas configurer à nouveau cette taxe dans un autre plugin (Paypal par exemple), sinon laissez 0.',
+	'configurer_taxe_defaut_explication' => 'Saisir la taxe par défaut applicable aux produits sous la forme 0.196 pour une TVA à 19,6% par exemple (cette valeur peut être surchargée ensuite pour chaque produit).',
 	'configurer_taxe_defaut_label' => 'Taxe par défaut',
 	'configurer_titre' => 'Configuration des produits',
 
