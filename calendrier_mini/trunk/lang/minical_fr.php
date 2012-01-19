e6a40c8220169441313a34f017d6cadbce33e63c
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-19 18:10:00 +0000

    une version trunk
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@57349 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ calendrier_mini/trunk/lang/minical_fr.php
@@ -0,0 +1,18 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/calendrier_mini/spip-1.9/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// D
+	'date_jour_abbr_1' => 'dim.',
+	'date_jour_abbr_2' => 'lun.',
+	'date_jour_abbr_3' => 'mar.',
+	'date_jour_abbr_4' => 'mer.',
+	'date_jour_abbr_5' => 'jeu.',
+	'date_jour_abbr_6' => 'ven.',
+	'date_jour_abbr_7' => 'sam.'
+);
+
+?>
