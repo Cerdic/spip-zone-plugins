2f4240083caf4b503ddb7f0738a84903bbeff433
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-19 09:29:49 +0000

    Une fonction d'importation des acronymes depuis F&T lors de l'installation de notre plugin Dictionnaires. Commit à l'aveugle car je n'ai pas de base de test sous la main là. Il faut donc tester en activant ce plugin pour la première fois sur un site qui contient des acronymes dans F&T.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@57324 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- dictionnaires/trunk/dictionnaires_administrations.php
+++ dictionnaires/trunk/dictionnaires_administrations.php
@@ -25,6 +25,9 @@ function dictionnaires_upgrade($nom_meta_version_base, $version_cible){
 			ecrire_config('dictionnaires/remplacer_premier_defaut', 'on');
 			ecrire_config('dictionnaires/remplacer_premier_abbr', 'on');
 			
+			// Migration depuis F&T si présent
+			dictionnaires_migrer_acronymes();
+			
 			echo "Installation du plugin dictionnaires<br/>";
 			ecrire_meta($nom_meta_version_base, $version_actuelle=$version_cible, 'non');
 		}
@@ -43,6 +46,52 @@ function dictionnaires_upgrade($nom_meta_version_base, $version_cible){
 	
 	}
 
+}
+
+function dictionnaires_migrer_acronymes(){
+	// Si F&T est actif là tout de suite et qu'il y a une table d'acronymes
+	if (
+		_DIR_PLUGIN_FORMS
+		and include_spip('base/forms_base_api')
+		and count($liste=Forms_liste_tables('acronymes_sigles'))
+	){
+		include_spip('forms_fonctions');
+		$id_form = intval(reset($liste));
+		spip_query("SELECT id_donnee FROM spip_forms_donnees WHERE id_form="._q($id_form)." AND statut='publie'");
+		$acronymes = sql_allfetsel('id_donnee, statut, date', 'spip_forms_donnees', 'id_form = '.$id_form);
+		if ($acronymes and is_array($acronymes)){
+			// On commence par créer un dictionnaire pour l'importation
+			include_spip('action/editer_dictionnaire');
+			if ($id_dictionnaire = insert_dictionnaire()){
+				// On lui met des champs par défaut
+				dictionnaire_set($id_dictionnaire, array(
+					'titre' => _T('dictionnaire:importer_acronymes_titre'),
+					'actif' => 0,
+					'descriptif' => _T('dictionnaire:importer_acronymes_descriptif'),
+				));
+				
+				// On parcourt ensuite les acronymes à importer pour récupérer leurs infos
+				foreach ($acronymes as $acronyme){
+					if ($titre = trim(str_replace("." , "", forms_calcule_les_valeurs('forms_donnees_champs', $acronyme['id_donnee'], 'ligne_1', $id_form,' ', true)))){
+						$definition = array(
+							'id_dictionnaire' => $id_dictionnaire,
+							'titre' => $titre,
+							'texte' => forms_calcule_les_valeurs('forms_donnees_champs', $acronyme['id_donnee'], 'texte_1', $id_form,' ', true),
+							'date' => $acronyme['date'],
+							'statut' => ($acronyme['statut'] == 'publie') ? 'publie' : 'prop',
+							'lang' => forms_calcule_les_valeurs('forms_donnees_champs', $acronyme['id_donnee'], 'select_2', $id_form,' ', true)
+						);
+						
+						// On crée la définition dans la base
+						include_spip('action/editer_definition');
+						if ($id_definition = insert_definition()){
+							definition_set($id_definition, $definition);
+						}
+					}
+				}
+			}
+		}
+	}
 }
 
 // Désinstallation
