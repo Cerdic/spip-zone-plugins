4464a26db1f715aa01142c2b6a17774bbe908534
joseph@larmarange.net <joseph@larmarange.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-20 13:54:40 +0000

    image en 16x16 pour ieconfig
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@57389 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- refbase/trunk/refbase_ieconfig_metas.php
+++ refbase/trunk/refbase_ieconfig_metas.php
@@ -4,7 +4,7 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 
 function refbase_ieconfig_metas($table){
 	$table['refbase']['titre'] = 'RefBase';
-	$table['refbase']['icone'] = 'prive/themes/spip/images/refbase-24.png';
+	$table['refbase']['icone'] = 'prive/themes/spip/images/refbase-16.png';
 	$table['refbase']['metas_serialize'] = 'refbase';
 	return $table;
 }
