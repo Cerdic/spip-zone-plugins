92fa997fbf16c87a366e0064fd9ec7b868ed3f90
yffic@lefourneau.com <yffic@lefourneau.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-20 09:46:02 +0000

    Pétouilles d'indentation
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@57366 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- commandes_paniers/action/commandes_paniers.php
+++ commandes_paniers/action/commandes_paniers.php
@@ -1,32 +1,31 @@
 <?php
-    if (!defined("_ECRIRE_INC_VERSION")) return;
+if (!defined("_ECRIRE_INC_VERSION")) return;
 
-    include_spip('inc/base');
-	include_spip('inc/session');
-    include_spip('inc/commandes');
+include_spip('inc/base');
+include_spip('inc/session');
+include_spip('inc/commandes');
 
-    function action_commandes_paniers_dist(){
+function action_commandes_paniers_dist(){
 
-    	// On commence par chercher le panier du visiteur actuel s'il n'est pas donné
-    	if (!$id_panier) $id_panier = session_get('id_panier');
-
-        //Si aucun panier ne pas agir
-        if (is_null($id_panier)) 
-            return;        
-
-        $securiser_action = charger_fonction('securiser_action', 'inc');
-        $arg = $securiser_action();
-        
-        $id_objet = creer_commande_encours();
-
-        $supprimer_panier = charger_fonction('supprimer_panier_encours', 'action/');
-        $supprimer_panier();
-
-        if (is_null(_request('redirect'))) {   
-            $redirect = generer_url_public('commande','id_commande='.$id_objet,true);
-            include_spip('inc/headers');
-            redirige_par_entete($redirect);
-        }
-
-    }
+	// On commence par chercher le panier du visiteur actuel s'il n'est pas donné
+	if (!$id_panier) $id_panier = session_get('id_panier');
+	
+	//Si aucun panier ne pas agir
+	if (is_null($id_panier)) 
+		return;        
+	
+	$securiser_action = charger_fonction('securiser_action', 'inc');
+	$arg = $securiser_action();
+	
+	$id_objet = creer_commande_encours();
+	
+	$supprimer_panier = charger_fonction('supprimer_panier_encours', 'action/');
+	$supprimer_panier();
+	
+	if (is_null(_request('redirect'))) {   
+		$redirect = generer_url_public('commande','id_commande='.$id_objet,true);
+		include_spip('inc/headers');
+		redirige_par_entete($redirect);
+	}
+}
 ?>
