81690d144dc02072c345d7d4b94d5f2fbe650804
klaus@spip.de <klaus@spip.de@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-20 12:02:16 +0000

    Bon, enfin, la traduction allemande ...
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@57374 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ dublin_core/lang/dublin_core_de.php
@@ -0,0 +1,28 @@
+﻿<?php
+
+// S&eacute;curit&eacute;
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	'configurer_dublin_core' => 'Dublin Core Felder für Artikel konfigurieren',
+	'dublin_core' => 'Dublin Core',
+	'item_auteurs' => 'Autor/en des Artikels',
+	'item_chapo' => 'Kopftext des Artikels',
+	'item_descriptif' => 'Beschreibung des Artikels',
+	'item_groupe_mot' => 'Schlagworte einer spezifischen Gruppe',
+	'item_introduction' => 'Einleitung des Artikels',
+	'item_motscles' => 'Alle Schlagworte des Artikels',
+	'item_nom_site_spip' => 'Name der Webbsite',
+	'item_soustitre' => 'Untertitel des Artikels',
+	'item_surtitre' => 'Über-Titel des Artikels',
+	'label_dc_creator' => 'Autoren (DC.Creator)',
+	'label_dc_description_abstract' => 'Zusammenfassung (DC.Description .Abstract)',
+	'label_dc_rights_license' => 'Lizenz-URL (DC.Rights.License) (optional)',
+	'label_dc_rights_license_details' => 'Lizenz-URL des Artikels. Falls sie das Lizenz-Plugin aktiviert haben, hat der dort eingetragene URL Vorrang vor dem hier eingestellten.',
+	'label_dc_rights_rightsholder' => 'Rechteinhaber (DC.Rights.RightsHolder)',
+	'label_dc_subject' => 'Themen oder Schlagworte (DC.Subject)',
+	'label_dc_subject_groupe_mot' => 'Welche Gruppe auswählen, falls nur Schlagworte einer Gruppe verwendet werden?',
+	'label_issn' => 'ISSN (optional)',
+);
+
+?>
