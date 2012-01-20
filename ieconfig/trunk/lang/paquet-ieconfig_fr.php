9cd8555b867c14defce1ec53119d6ccae72206aa
joseph@larmarange.net <joseph@larmarange.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-20 14:15:31 +0000

    Oublis lors du commit précédent, ajout d'un message d'avertissement et passage du fichier de langue en UTF8
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@57395 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ ieconfig/trunk/lang/paquet-ieconfig_fr.php
@@ -0,0 +1,18 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+// Fichier produit par PlugOnet
+// Module: paquet-ieconfig
+// Langue: fr
+// Date: 20-01-2012 12:38:10
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// I
+	'ieconfig_description' => 'Ce plugin permet d\'exporter la configuration de SPIP et des plugins compatibles dans un seul et unique fichier au format YAML. Ces fichiers de configuration peuvent ensuite être importés.',
+	'ieconfig_slogan' => 'Importer, exporter des configurations de plugins',
+);
+?>
