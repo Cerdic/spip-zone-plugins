590ce610419efe6b5cd3bb9d455db63487035d87
marcimat@rezo.net <marcimat@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-21 12:29:24 +0000

    - correction de l'explication des sélecteurs qui n'étaient pas chargées - éviter une erreur sur |picker_selected en SPIP 3
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@57440 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- saisies/saisies_fonctions.php
+++ saisies/saisies_fonctions.php
@@ -4,6 +4,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 
 include_spip('inc/saisies');
 include_spip('balise/saisie');
-
+// picker_selected (spip 3)
+include_spip('formulaires/selecteur/generique_fonctions');
 
 ?>
