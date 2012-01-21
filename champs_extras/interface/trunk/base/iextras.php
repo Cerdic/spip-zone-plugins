1d2fa4dc621854db2fa7bc8934874313fb12000a
marcimat@rezo.net <marcimat@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-21 19:58:59 +0000

    Coquille de test sur le nom le la fonction.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@57481 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- champs_extras/interface/trunk/base/iextras.php
+++ champs_extras/interface/trunk/base/iextras.php
@@ -7,7 +7,7 @@ function iextras_declarer_champs_extras($saisies_tables=array()) {
 	// lors du renouvellement de l'alea, au demarrage de SPIP
 	// les chemins de plugins ne sont pas encore connus.
 	// il faut se mefier et charger tout de meme la fonction, sinon page blanche.
-	if (!function_exists('iextras_get_extras')) {
+	if (!function_exists('iextras_champs_extras_definis')) {
 		spip_log("ERREUR : inclusion inc/iextras non executee", 'iextras');
 		include_once(dirname(__file__).'/../inc/iextras.php');
 	}
