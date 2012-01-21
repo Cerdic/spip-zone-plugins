45489cec20d26121764f85766d6ad6a258b516a4
marcimat@rezo.net <marcimat@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-21 17:36:28 +0000

    On tente une option de normalisation sur verifier(), plutôt que d'avoir une fonction normaliser() séparer, puisque Rastapopoulos préfère comme ça.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@57473 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- verifier/inc/verifier.php
+++ verifier/inc/verifier.php
@@ -8,9 +8,12 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
  * @param mixed $valeur La valeur a verifier.
  * @param string $type Le type de verification a appliquer.
  * @param array $options Un eventuel tableau d'options suivant le type.
+ * @param array $valeur_normalisee
+ * 		Si des options de verification modifient la valeur entrante (normalisent),
+ * 		alors la valeur modifie sera stockee dans cette variable.
  * @return string Retourne une chaine vide si c'est valide, sinon une chaine expliquant l'erreur.
  */
-function inc_verifier_dist($valeur, $type, $options=null){
+function inc_verifier_dist($valeur, $type, $options=null, &$valeur_normalisee=null){
 
 	// On vérifie que les options sont bien un tableau
 	if (!is_array($options))
@@ -21,7 +24,7 @@ function inc_verifier_dist($valeur, $type, $options=null){
 
 	// On cherche si une fonction correspondant au type existe
 	if ($verifier = charger_fonction($type, 'verifier',true)){
-		$erreur = $verifier($valeur, $options);
+		$erreur = $verifier($valeur, $options, $valeur_normalisee);
 	}
 
 	// On passe le tout dans le pipeline du meme nom
@@ -31,7 +34,7 @@ function inc_verifier_dist($valeur, $type, $options=null){
 			'args' => array(
 				'valeur' => $valeur,
 				'type' => $type,
-				'options' => $options
+				'options' => $options,
 			),
 			'data' => $erreur
 		)
@@ -42,44 +45,53 @@ function inc_verifier_dist($valeur, $type, $options=null){
 
 /**
  * Liste toutes les vérifications possibles
- *
+ * 
+ * @param string $repertoire
+ * 		Dans quel repertoire chercher les yaml.
+ * 
  * @return Retourne un tableau listant les vérifications et leurs options
  */
-function verifier_lister_disponibles(){
-	static $verifications = null;
-	
-	if (is_null($verifications)){
-		$verifications = array();
-		$liste = find_all_in_path('verifier/', '.+[.]yaml$');
+function verifier_lister_disponibles($repertoire='verifier'){
+	static $verifications = array();
+
+	if (is_null($verifications[$repertoire])){
+		$verifications[$repertoire] = array();
+		$liste = find_all_in_path("$repertoire/", '.+[.]yaml$');
 		
 		if (count($liste)){
 			foreach ($liste as $fichier=>$chemin){
-				$type_verif = preg_replace(',[.]yaml$,i', '', $fichier);
+				$type = preg_replace(',[.]yaml$,i', '', $fichier);
 				$dossier = str_replace($fichier, '', $chemin);
 				// On ne garde que les vérifications qui ont bien la fonction !
-				if (charger_fonction($type_verif, 'verifier', true)
+				if (charger_fonction($type, $repertoire, true)
 					and (
-						is_array($verif = verifier_charger_infos($type_verif))
+						is_array($verif = verifier_charger_infos($type, $repertoire))
 					)
 				){
-					$verifications[$type_verif] = $verif;
+					$verifications[$repertoire][$type] = $verif;
 				}
 			}
 		}
 	}
 	
-	return $verifications;
+	return $verifications[$repertoire];
 }
+
 
 /**
  * Charger les informations contenues dans le yaml d'une vérification
  *
- * @param string $type_verif Le type de la vérification
+ * @param string $type_verif
+ * 		Le type de la vérification
+ * 
+ * @param string $repertoire
+ * 		Dans quel repertoire chercher les yaml.
+ * 
  * @return array Un tableau contenant le YAML décodé
  */
-function verifier_charger_infos($type_verif){
+function verifier_charger_infos($type_verif, $repertoire='verifier'){
 	include_spip('inc/yaml');
-	$fichier = find_in_path("verifier/$type_verif.yaml");
+	$fichier = find_in_path("$repertoire/$type_verif.yaml");
 	$verif = yaml_decode_file($fichier);
 	if (is_array($verif)){
 		$verif['titre'] = $verif['titre'] ? _T_ou_typo($verif['titre']) : $type_verif;
