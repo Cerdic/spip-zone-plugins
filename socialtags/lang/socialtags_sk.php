4aefac5a40233b4d7ce6a920a59b7c12ef8f2a10
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-22 17:51:08 +0000

    langues (socialtags)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@57587 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ socialtags/lang/socialtags_sk.php
@@ -0,0 +1,64 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cfg_1' => 'Nastavenie zásuvného modulu Social tags',
+	'cfg_2' => 'Tu si môžete vybrať, s ktorými stránkami budete zdieľať svoje články.',
+	'cfg_3' => 'Dokumentácia',
+	'cfg_activer_javascript' => 'SVP, prosím aktivovať Javascript.',
+	'cfg_badge_fb' => 'Odznak/Widget pre Facebook',
+	'cfg_badge_fb_existe' => 'Si vous avez un Badge Facebook (<a href="http://www.facebook.com/facebook-widgets/" class="spip_out">aide sur le site FB</a>), indiquez ci-dessous son contenu, soit en JS, soit en HTML.', # NEW
+	'cfg_badge_html' => 'Odznak HTML',
+	'cfg_badge_html_ajouter_1' => '(pridať do svojej šablóny',
+	'cfg_badge_html_ajouter_2' => ', alebo do článku',
+	'cfg_badge_js' => 'Odznak JS',
+	'cfg_badge_js_1' => ' (automaticky zaradené do <code>@code@</code>)',
+	'cfg_en_bas' => 'V spodnej časti každej stránky',
+	'cfg_exemples' => 'Exemples (sur les squelettes dist) :', # NEW
+	'cfg_explication_boite_info' => 'Ici vous pouvez choisir les sites de partages de vos articles.', # NEW
+	'cfg_inserer' => 'Insérer le bloc social-tags en-dessous du bloc suivant :', # NEW
+	'cfg_insertion' => 'Vloženie medzi stránky:',
+	'cfg_liste_site' => 'Zoznam sociálnych stránok',
+	'cfg_plusone' => 'Google plus one',
+	'cfg_plusone_existe' => 'Pour activer Google+1, installer le plugin (<a href="http://www.spip-contrib.net/Google-1" class="spip_out">aide sur spip-contrib</a>).', # NEW
+	'cfg_referer' => 'N\'afficher le badge et les boutons que pour les visiteurs originaires des sites concernés.', # NEW
+	'cfg_selector' => 'Sélecteur :', # NEW
+	'cfg_sous_bloc' => 'Pod blokom názvu stránok "článok"',
+	'cfg_sous_div' => 'S divom s názvom id=socialtags',
+	'cfg_titre_boite_info' => 'Nastavenia zásuvného modulu Social tags',
+	'cfg_titre_socialtags' => 'Social tags',
+	'cfg_tout_cocher' => 'Označiť všetko',
+	'cfg_tout_decocher' => 'Odznačiť všetko',
+
+	// D
+	'description_noisette_badge_facebook' => 'Affiche le badge Facebook défini dans la page de configuration', # NEW
+	'description_noisette_facebook_like' => 'Zobrazí tlačidlo <b>Páči sa mi to</b> alebo <b>Odporučiť,</b> prípadne <b>Poslať</b>',
+
+	// N
+	'noisette_action_like' => 'Páči sa mi to',
+	'noisette_action_recommand' => 'Odporučiť',
+	'noisette_label_action' => 'Akcia',
+	'noisette_label_afficher_send' => 'Tlačidlo Poslať?',
+	'noisette_label_afficher_titre_noisette' => 'Zobraziť názov?',
+	'noisette_label_niveau_titre' => 'Vybrať úroveň nadpisu',
+	'noisette_label_show_faces' => 'Zobraziť pod tlačidlo fotografiu z profilu?',
+	'noisette_label_titre_noisette' => 'Názov:',
+	'noisette_label_titre_noisette_perso' => 'Ak je názov vlastný:',
+	'noisette_label_width' => 'Šírka',
+	'noisette_titre_perso' => 'Vlastný názov',
+	'nom_noisette_badge_facebook' => 'Odznak pre Facebook',
+	'nom_noisette_facebook_like' => 'Tlačidlo Páči sami pre Facebook',
+
+	// T
+	'titre_mon_badge_facebook' => 'Môj odznak pre facebook',
+	'titre_perso' => 'Vlastný názov',
+	'titre_sur_facebook' => 'Na Facebook'
+);
+
+?>
