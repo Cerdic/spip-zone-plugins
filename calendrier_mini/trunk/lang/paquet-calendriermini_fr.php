e761205324c65ca2204202f093170d60f9385ae2
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-22 17:50:29 +0000

    langues (paquet-calendriermini)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@57550 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- calendrier_mini/trunk/lang/paquet-calendriermini_fr.php
+++ calendrier_mini/trunk/lang/paquet-calendriermini_fr.php
@@ -1,22 +1,16 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-calendriermini
-// Langue: fr
-// Date: 20-01-2012 12:15:05
-// Items: 2
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/calendrier_mini/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// C
+	// C
 	'calendriermini_description' => 'Permet l\'utilisation d\'une balise #CALENDRIER_MINI.
 _ Cette balise affiche un calendrier au design issu de dotclear et donc compatible avec les styles issus de ce système de blog.
 _ Lui sont adjoints d\'autre éléments, tels que balises, critères, modèles...
 _ Icone de [Tempest->http://tempest.deviantart.com/] sous licence CC BY-NC-ND.',
-	'calendriermini_slogan' => 'Permet l\'utilisation d\'une balise #CALENDRIER_MINI',
+	'calendriermini_slogan' => 'Permet l\'utilisation d\'une balise #CALENDRIER_MINI'
 );
+
 ?>
