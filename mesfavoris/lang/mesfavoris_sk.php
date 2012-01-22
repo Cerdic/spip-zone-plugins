2d43595f937dcd4abc8a1905d7270f8949c2821a
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-22 17:50:46 +0000

    langues (mesfavoris)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@57566 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ mesfavoris/lang/mesfavoris_sk.php
@@ -0,0 +1,37 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'add' => 'Pridať',
+	'add_to' => 'Pridať do môjho výberu',
+	'ajoute_le' => 'Pridané',
+
+	// C
+	'configurer' => 'Nastaviť',
+
+	// L
+	'label_style_formulaire16' => 'Malý (16 px)',
+	'label_style_formulaire24' => 'Stredný (24 px)',
+	'label_style_formulaire32' => 'Veľký (32 px)',
+	'legend_formulaire_public' => 'Verejný formulár',
+	'legend_style_formulaire' => 'Štýl formulára',
+	'login_first' => 'Prepáčte, akcia si vyžaduje uloženie, najprv sa prihláste',
+
+	// M
+	'mes_favoris' => 'Moje obľúbené položky',
+
+	// R
+	'remove' => 'Odstrániť',
+	'remove_from' => 'Odstrániť z môjho výberu',
+
+	// S
+	'ses_favoris' => 'Jeho/jej obľúbené položky'
+);
+
+?>
