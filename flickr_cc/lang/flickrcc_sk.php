00b2de8c7334f7a0671d81ef5873322a09b3f774
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-22 17:50:36 +0000

    langues (flickrcc)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@57557 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- flickr_cc/lang/flickrcc_sk.php
+++ flickr_cc/lang/flickrcc_sk.php
@@ -8,30 +8,30 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
-	'ajouter_image_flickr' => '<NEW>Pridať obrázok z Flickra',
+	'ajouter_image_flickr' => 'Pridať obrázok z Flickra',
 
 	// C
-	'cc_commerciale' => '<NEW>CC - Povolené použitie na komerčné účely',
-	'cc_modification' => '<NEW>CC - Povolená modifikácia',
-	'cc_pas_de_restriction' => '<NEW>Žiadne známe obmedzenia autorskými právami',
-	'cc_paternite' => 'Creative Commons - Attribution License',
+	'cc_commerciale' => 'CC – možné používanie na komerčné účely',
+	'cc_modification' => 'CC – sú možné zmeny',
+	'cc_pas_de_restriction' => 'Žiadne známe obmedzenia',
+	'cc_paternite' => 'Creative Commons – Attribution License',
 
 	// F
-	'fermer' => '<NEW>Zatvoriť',
-	'flickr_cc_explication' => '<NEW><p>Vyhľadávajte na Flickri obrázky publikované na základe licencií Creative Commons. Zadajte svoje vlastné kľúčové slovo (-á) a uveďte typ licencií, ktorý chcete použiť.</p>
-<p>Kliknite na obrázok, ktorý chcete vložiť do svojho článku: bude automaticky stiahnutý a pripojený k vášmu dokumentu. Ak sa chcete dosta5 na pôvodnú stránku na Flickri, kliknite na meno autora.</p>
-<p>Tieto obrázky na Flicri publikovali ich autori: prosím, rešpektujte ich prácu a autorské práva:</p>
-<ul><li>neodstraňujte meno autora a odkaz na jeho stránku na Flickri;</li>
-<li>rešpektujte podmienky licencie Creative Commons pri každom obrázku (napr. nevyužívajte obrázok na komerčné účely, ak to licencia nepovoľuje);</li>
-<li>ak autor požiada o odstránenie svojho obrázka z vašej stránky, musíte mu vyhovieť do 24 hodín.</li>
+	'fermer' => 'Zatvoriť',
+	'flickr_cc_explication' => '<p>Vyhľadávajte na Flickri obrázky publikované na základe licencií Creative Commons. Zadajte svoje vlastné kľúčové slovo (-á) a uveďte typ licencií, ktorý chcete použiť.</p>
+<p>Kliknite na obrázok, ktorý chcete vložiť do svojho článku: bude automaticky stiahnutý a pripojený k vášmu dokumentu. Ak sa chcete dostať na pôvodnú stránku na Flickri, kliknite na meno autora.</p>
+<p>Tieto obrázky na Flicri publikovali ich autori: prosím, rešpektujte ich prácu a autorské práva:</p>
+<ul><li>neodstraňujte meno autora a odkaz na jeho stránku na Flickri,</li>
+<li>rešpektujte podmienky licencie Creative Commons pri každom obrázku (napr. nevyužívajte obrázok na komerčné účely, ak to licencia nepovoľuje),</li>
+<li>ak autor požiada o odstránenie svojho obrázka z vašej stránky, musíte mu vyhovieť do 24 hodín.</li>
 </ul>
-<p>Tento nástroj využíva Flickr API, ale nie je schválený alebo autorizovaný Flickrom.</p>',
+<p>Tento nástroj využíva Flickr API, ale Flickr ho neschválil ani nepovolil.</p>',
 
 	// R
-	'rechercher_flickr' => '<NEW>Vyhľadať na Flickri',
-	'resultat_par_date' => '<NEW>podľa dátumu',
-	'resultat_par_interet' => '<NEW>podľa zaujímavosti',
-	'resultat_par_pertinence' => '<NEW>podľa dôležitosti'
+	'rechercher_flickr' => 'Vyhľadať na Flickri',
+	'resultat_par_date' => 'podľa dátumu',
+	'resultat_par_interet' => 'podľa záujmu',
+	'resultat_par_pertinence' => 'podľa dôležitosti'
 );
 
 ?>
