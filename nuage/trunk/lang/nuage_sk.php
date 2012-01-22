56737ff2434d4bd7f5cd3e2f46ac0931be68e87c
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-22 17:50:57 +0000

    langues (nuage)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@57577 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ nuage/trunk/lang/nuage_sk.php
@@ -0,0 +1,19 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// N
+	'noisette_description' => 'Pridá mrak kľúčových slov',
+	'noisette_label_afficher_titre' => 'Pridať názov:',
+	'noisette_label_niveau_titre' => 'Vybrať úroveň nadpisu',
+	'noisette_nom_noisette' => 'Nuage (mrak)',
+	'noisette_titre_defaut' => 'Tagy',
+	'noisette_titre_noisette' => 'Nadpis:'
+);
+
+?>
