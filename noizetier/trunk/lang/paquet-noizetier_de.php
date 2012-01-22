746f2e9af0d56ae7a5882d4a8772dfe26e73fe70
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-22 17:50:53 +0000

    langues (paquet-noizetier)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@57573 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- noizetier/trunk/lang/paquet-noizetier_de.php
+++ noizetier/trunk/lang/paquet-noizetier_de.php
@@ -1,19 +1,16 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-noizetier
-// Langue: de
-// Date: 19-01-2012 11:51:59
-// Items: 2
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org
+// ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// N
+	// N
+	'noizetier_description' => 'Le noiZetier fournit une interface dans l’espace privé permettant de choisir, pour chaque page, des noisettes à ajouter aux blocs contenu, navigation et extra de chaque page. Les noisettes viennent ainsi compléter les contenus par défaut prévus par le squelette.', # NEW
 	'noizetier_nom' => 'Nussbaum',
-	'noizetier_slogan' => 'Verwaltung der Code-Nïsse, als Ergänzung der Z-Squelette oder als eigenständiges Modu.',
+	'noizetier_slogan' => 'Verwaltung der Code-Nïsse, als Ergänzung der Z-Squelette oder als eigenständiges Modu.'
 );
+
 ?>
