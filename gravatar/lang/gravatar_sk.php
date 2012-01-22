ee3ae180c2ebccd060441b04f784b03dedec0c04
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-22 17:50:40 +0000

    langues (gravatar)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@57561 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ gravatar/lang/gravatar_sk.php
@@ -0,0 +1,35 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cfg_descr_gravatar' => 'Nastavte vzhľad gravatarov',
+	'cfg_titre_gravatar' => 'Gravatary',
+
+	// E
+	'explication_image_defaut' => 'Pre tých, ktorí nemajú žiaden avatar, používať:',
+
+	// G
+	'gravatar_info' => 'Ak chcete, aby sa fotka vašej tváre zobrazila tu, najprv ju uložte na <a href="http://www.gravatar.com/" rel="external nofollow" class="spip_out">gravatar.com</a> (bezplatné a bezbolestné).',
+	'gravatar_info_forum' => 'Ak chcete, aby sa fotka vašej tváre zobrazila vo vašej správe, najprv ju uložte na <a href="http://www.gravatar.com/" rel="external nofollow" class="spip_out">gravatar.com</a> (bezplatné a bezbolestné) a nezabudnite tu pridať svoju e-mailovú adresu.',
+
+	// L
+	'label_image_defaut' => 'Predvolený obrázok',
+	'label_image_defaut_404' => 'žiaden obrázok',
+	'label_image_defaut_gravatar' => 'logo Gravatar',
+	'label_image_defaut_identicon' => '<i>Identikona</i> (vytvára sa cez e-mailovú adresu)',
+	'label_image_defaut_monsterid' => '<i>MonsterID</i> (vytvára sa cez e-mailovú adresu)',
+	'label_image_defaut_png' => '<tt>images/gravatar.png</tt>',
+	'label_image_defaut_wavatar' => '<i>Wavatar</i> (vytvára sa cez e-mailovú adresu)',
+	'label_taille' => 'Veľkosť gravatarov',
+
+	// T
+	'titre_gravatar_auteur' => 'Gravatar'
+);
+
+?>
