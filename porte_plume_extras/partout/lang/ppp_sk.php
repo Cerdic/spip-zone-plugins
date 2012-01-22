e1ce7472147d5255ef736a731c1267c5cf650f87
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-22 17:51:04 +0000

    langues (ppp)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@57584 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ porte_plume_extras/partout/lang/ppp_sk.php
@@ -0,0 +1,27 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'bouton_mettre_a_jour' => 'Aktualizovať',
+
+	// C
+	'cfg_attention' => 'Pozor',
+	'cfg_description' => 'uistite sa, že v šablónach máte povolené používanie Porte Plume iba pri poliach, kde  použitie skratiek nevyvolá chybu xhtml.',
+	'cfg_titre' => 'Všade Porte Plume',
+
+	// L
+	'label_hauteur_champ' => 'Polovičná výška obrazovky',
+	'legend_barre_typo' => 'Typografický panel aktivovať na:',
+	'legend_hauteur_champ' => 'Výška textového poľa pre články',
+
+	// S
+	'supprimer' => 'Obnoviť pôvodné hodnoty'
+);
+
+?>
