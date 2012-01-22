a487bf0196fd1c4579d31a96a3e75f1dc982d3ea
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-22 17:50:59 +0000

    langues (opensearch)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@57579 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ opensearch/lang/opensearch_sk.php
@@ -0,0 +1,21 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cfg_boite_opensearch' => 'Nastavenie zásuvného modulu OpenSearch',
+	'cfg_descr_opensearch' => 'OpenSearch: vyhľadávač prispôsobený pre vašu stránku.',
+	'cfg_inf_id_rubrique' => 'Vyberte rubriku, v ktorej sa má vyhľadávať.',
+	'cfg_inf_page_recherche' => 'Uveďte šablónu, ktorá sa použije na vyhľadávanie na stránke (príklad: vyhľadávanie šablóny recherche.html)',
+	'cfg_lbl_id_rubrique' => 'Cieľová rubrika',
+	'cfg_lbl_page_recherche' => 'Šablóna vyhľadávania',
+	'cfg_option_tout' => 'Celá stránka',
+	'cfg_titre_opensearch' => 'OpenSearch'
+);
+
+?>
