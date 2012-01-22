f91b019b59e90579d41c793950bcd1966f305f84
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-22 17:51:03 +0000

    langues (paquet-pp_codes)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@57583 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ porte_plume_extras/codes/trunk/lang/paquet-pp_codes_sk.php
@@ -0,0 +1,17 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// P
+	'pp_codes_description' => 'Ovládanie tlačidiel na vkladanie počítačového kódu prostredníctvom panelu úprav Porte Plume.
+Pridáva aj skratku <code>[->ecrire/inc_versions.php#trac].</code>',
+	'pp_codes_nom' => 'Počítačové kódy pre Porte Plume',
+	'pp_codes_slogan' => 'Pridá tlačidlá na panel nástrojov Porte Plume na správu počítačových kódov'
+);
+
+?>
