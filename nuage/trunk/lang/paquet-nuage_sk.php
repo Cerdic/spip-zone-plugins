a5fab8709a35a636a0e1706f1f8fb8d29c2bc69a
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-22 17:50:58 +0000

    langues (paquet-nuage)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@57578 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ nuage/trunk/lang/paquet-nuage_sk.php
@@ -0,0 +1,20 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// N
+	'nuage_description' => 'Ce plugin permet d’afficher les mots-clés en vrac, en faisant varier la taille de la police en fonction du nombre d\'articles associés au mot-clé. En d\'autres termes, spip permet de créer des « tags » !
+
+	Plusieurs modèles disponibles :
+-* article_nuage : affiche les mots-clés de l\'article la taille variant en fonction de la fréquence des mots dans les articles du site
+-* rubrique_nuage : affiche les mots-clés des articles de la branche, la taille variant en fonction de la fréquence des mots dans les articles de cette branche
+-* nuage_popularite : affiche les mots-clés du site (ou d\'un groupe), la taille variant en fonction de sa popularité relative (somme des popularité des articles ayant ce mot)', # NEW
+	'nuage_slogan' => 'Zobrazí kľúčové slová s rôznou veľkosťou písma'
+);
+
+?>
