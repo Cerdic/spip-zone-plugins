dd6ba38c0f5fcf85f4affe777bc8c20ff762f859
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-22 17:51:01 +0000

    langues (pages)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@57581 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ pages/trunk/lang/pages_sk.php
@@ -0,0 +1,32 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'aucune_page' => 'Momentálne žiadne stránky neexistujú.',
+
+	// C
+	'convertir_article' => 'Zmeniť na článok',
+	'convertir_page' => 'Zmeniť na stránku',
+	'creer_page' => 'Vytvoriť novú stránku',
+
+	// I
+	'input_page' => 'une_page',
+
+	// M
+	'modifier_page' => 'Upraviť stránku:',
+
+	// P
+	'pages_uniques' => 'Jedinečné stránky',
+
+	// T
+	'titre_page' => 'Stránka',
+	'toutes_les_pages' => 'Všetky stránky'
+);
+
+?>
