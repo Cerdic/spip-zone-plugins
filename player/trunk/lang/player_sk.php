4e17ad4b53c0d02b167dcf6cfa7fcdd17869574e
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-22 17:50:45 +0000

    langues (player)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@57565 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ player/trunk/lang/player_sk.php
@@ -0,0 +1,116 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// _
+	'_blank' => 'Nové okno',
+	'_self' => 'Aktuálne okno',
+
+	// A
+	'always' => 'Vždy',
+	'auto' => 'Automaticky skrývať',
+	'autohide' => 'Automaticky skrývať',
+
+	// B
+	'blogo_vous_ecoutez' => 'Teraz počúvate:',
+
+	// C
+	'configuration_player' => 'Nastavenia prehrávača multimédií',
+
+	// L
+	'label_autoload' => 'Začať nahrávať a zobraziť prvý obrázok',
+	'label_autonext' => 'Automaticky prehrať ďalšie video',
+	'label_autoplay' => 'Prehrať automaticky',
+	'label_bgcolor' => 'Farba pozadia:',
+	'label_bgcolor1' => 'Prvá farba farebného prechodu: ',
+	'label_bgcolor2' => 'Druhá farba farebného prechodu:',
+	'label_buffer' => 'Počet sekúnd na nahratie do vyrovnávacej pamäte: ',
+	'label_buffermessage' => 'Správa pri nahrávaní do vyrovnávacej pamäte: ',
+	'label_buttoncolor' => 'Farby tlačidiel: ',
+	'label_buttonovercolor' => 'Couleur des boutons au survol : ', # NEW
+	'label_config' => 'URL textového súboru s nastaveniami: ',
+	'label_configxml' => 'URL súboru XML s nastaveniami: ',
+	'label_currentflvcolor' => 'Farba vybraného videa: ',
+	'label_flv' => 'Adresy súborov videí FLV, ktoré treba nahrať:',
+	'label_height' => 'Nastaviť výšku prehrávača: ',
+	'label_iconplaybgalpha' => 'Priehľadnosť pozadia ikony PREHRAŤ: ',
+	'label_iconplaybgcolor' => 'Pozadie ikony PREHRAŤ: ',
+	'label_iconplaycolor' => 'Farba ikony PREHRAŤ: ',
+	'label_loadingcolor' => 'Farba panelu nahrávania: ',
+	'label_loop' => 'Boucler', # NEW
+	'label_margin' => 'Okraj videa: ',
+	'label_onclick' => 'Action click sur la vidéo (\'none\', \'playpause\' ou URL de la destination) : ', # NEW
+	'label_onclicktarget' => 'Cible de l\'URL au click : ', # NEW
+	'label_ondoubleclick' => 'Action double-click sur la vidéo (\'none\', \'fullscreen\', \'playpause\', ou URL à ouvrir) : ', # NEW
+	'label_ondoubleclicktarget' => 'Cible de l\'URL au double click sur la vidéo : ', # NEW
+	'label_player_mp3' => 'Choisissez le lecteur qui sera utilisé pour les fichiers MP3', # NEW
+	'label_player_mp3_dewplayer' => 'Dew player', # NEW
+	'label_player_mp3_eraplayer' => 'Erational player', # NEW
+	'label_player_mp3_neoplayer' => 'Prehrávač Neolao',
+	'label_player_mp3_pixplayer' => 'One pixel out player', # NEW
+	'label_playeralpha' => 'Priehľadnosť farby prehrávača: ',
+	'label_playercolor' => 'Farba prehrávača: ',
+	'label_playertimeout' => 'Délai en millisecondes avant que le lecteur se cache : ', # NEW
+	'label_playlisttextcolor' => 'Farba textu v zozname: ',
+	'label_playonload' => 'Prehrávať video pri nahrávaní',
+	'label_scrollbarcolor' => 'Couleur de la barre de défilement : ', # NEW
+	'label_scrollbarovercolor' => 'Couleur de la barre de défilement au survol : ', # NEW
+	'label_scrollbarsize' => 'Taille de la barre de défilement : ', # NEW
+	'label_shortcut' => 'Aktivovať klávesové skratky',
+	'label_showfullscreen' => 'Zobraziť tlačidlo na celú obrazovku',
+	'label_showiconplay' => 'Ikonu PREHRAŤ zobraziť v strede videa',
+	'label_showmouse' => 'Zobrazenie myšky: ',
+	'label_shownext' => 'Afficher le bouton NEXT', # NEW
+	'label_showopen' => 'Afficher le bouton OPEN (2 pour la liste) : ', # NEW
+	'label_showplayer' => 'Zobrazenie panelu s tlačidlami: ',
+	'label_showprevious' => 'Afficher le bouton PREVIOUS', # NEW
+	'label_showstop' => 'Zobraziť tlačidlo ZASTAVIŤ',
+	'label_showswitchsubtitles' => 'Zobraziť tlačidlo zobraziť/skryť titulky',
+	'label_showtime' => 'Zobraziť tlačidlo ČAS: ',
+	'label_showtime_0' => 'Nezobraziť',
+	'label_showtime_1' => 'Zobraziť tlačidlo',
+	'label_showtime_2' => 'Zobraziť zostávajúci čas',
+	'label_showtitleandstartimage' => 'Afficher le titre et l\'image de départ en même temps', # NEW
+	'label_showtitlebackground' => 'Zobrazenie pozadia názvu: ',
+	'label_showvolume' => 'Zobraziť tlačidlo HLASITOSŤ',
+	'label_shuffle' => 'Náhodné prehrávanie',
+	'label_skin' => 'URL súboru JPEG (neprogresívneho), ktorý treba nahrať: ',
+	'label_slidercolor1' => 'Prvá farba farebného prechodu panela: ',
+	'label_slidercolor2' => 'Druhá farba farebného prechodu panela: ',
+	'label_sliderovercolor' => 'Couleur de la barre au survol : ', # NEW
+	'label_srt' => 'Používať titulky SRT',
+	'label_srtbgcolor' => 'Farba pozadia titulkov: ',
+	'label_srtcolor' => 'Farba titulkov: ',
+	'label_srtsize' => 'Veľkosť titulkov: ',
+	'label_startimage' => 'Le ou les images de titre :', # NEW
+	'label_title' => 'Le ou les titres : ', # NEW
+	'label_titlecolor' => 'Farba názvu: ',
+	'label_titlesize' => 'Veľkosť písma názvu: ',
+	'label_top1' => 'Image par dessus la vidéo : ', # NEW
+	'label_videobgcolor' => 'Farba pozadia bez videa: ',
+	'label_videodelay' => 'Trvanie zobrazenia názvu pri zmene videa:',
+	'label_volume' => 'Predvolené nastavenie hlasitosti: ',
+	'label_width' => 'Nastaviť šírku prehrávača:',
+
+	// M
+	'maxi' => 'Maxi', # NEW
+	'mini' => 'Mini', # NEW
+	'multi' => 'Multi', # NEW
+
+	// N
+	'never' => 'Nikdy',
+	'normal' => 'Normal', # NEW
+
+	// P
+	'pas_de_lecteur_video' => 'Žiaden prehrávač videí nie je dostupný.',
+	'player_audio' => 'Player audio', # NEW
+	'player_nom' => 'Prehrávač multimédií',
+	'player_video' => 'Player vidéo' # NEW
+);
+
+?>
