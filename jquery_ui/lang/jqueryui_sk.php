64a7948ea5331b2605690de8a929d9af8035cf6c
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-24 16:22:09 +0000

    langues (jqueryui)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@57665 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ jquery_ui/lang/jqueryui_sk.php
@@ -0,0 +1,68 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'afficher_calendrier' => 'Zobraziť kalendár',
+
+	// C
+	'cfg_boite_jqueryui' => 'Nastavenia jQuery UI',
+	'cfg_explication_plugins' => 'Vyberte si zásuvné moduly, ktoré sa majú vložiť do hlavičky verejne prístupných stránok.',
+	'cfg_explication_themes' => 'Vyberte si farebné motívy pre jQuery-UI.',
+	'cfg_lbl_plugins' => 'Zásuvné moduly',
+	'cfg_lbl_themes' => 'Farebný motív',
+	'cfg_no_css' => 'nenahrávať CSS pre jQuery-UI',
+	'cfg_titre_jqueryui' => 'jQuery UI',
+	'cfg_val_complete' => 'Kompletné jQuery UI',
+	'cfg_val_effects_blind' => 'Effet blind', # NEW
+	'cfg_val_effects_bounce' => 'Efekt skok',
+	'cfg_val_effects_clip' => 'Effet clip', # NEW
+	'cfg_val_effects_core' => 'Efekty jadra',
+	'cfg_val_effects_drop' => 'Effet drop', # NEW
+	'cfg_val_effects_explode' => 'Efekt výbuch',
+	'cfg_val_effects_fold' => 'Efekt poskladať',
+	'cfg_val_effects_highlight' => 'Efekt zvýraznenie',
+	'cfg_val_effects_pulsate' => 'Effet pulsate', # NEW
+	'cfg_val_effects_scale' => 'Effet scale', # NEW
+	'cfg_val_effects_shake' => 'Effet shake', # NEW
+	'cfg_val_effects_slide' => 'Effet slide', # NEW
+	'cfg_val_effects_transfer' => 'Efekt presun',
+	'cfg_val_ui_accordion' => 'UI accordion', # NEW
+	'cfg_val_ui_autocomplete' => 'UI automatické dopĺňanie',
+	'cfg_val_ui_button' => 'UI tlačidlo',
+	'cfg_val_ui_core' => 'Core UI', # NEW
+	'cfg_val_ui_datepicker' => 'UI datepicker', # NEW
+	'cfg_val_ui_dialog' => 'UI dialog', # NEW
+	'cfg_val_ui_draggable' => 'UI draggable', # NEW
+	'cfg_val_ui_droppable' => 'UI droppable', # NEW
+	'cfg_val_ui_mouse' => 'UI myška',
+	'cfg_val_ui_position' => 'UI position', # NEW
+	'cfg_val_ui_progressbar' => 'UI progressbar', # NEW
+	'cfg_val_ui_resizable' => 'UI resizable', # NEW
+	'cfg_val_ui_selectable' => 'UI selectable', # NEW
+	'cfg_val_ui_slider' => 'UI slider', # NEW
+	'cfg_val_ui_sortable' => 'UI sortable', # NEW
+	'cfg_val_ui_tabs' => 'UI tabs', # NEW
+	'cfg_val_ui_widget' => 'UI widget',
+
+	// D
+	'date_mois_10_abbr' => 'okt.',
+	'date_mois_11_abbr' => 'nov.',
+	'date_mois_12_abbr' => 'dec.',
+	'date_mois_1_abbr' => 'jan.',
+	'date_mois_2_abbr' => 'feb.',
+	'date_mois_3_abbr' => 'mar.',
+	'date_mois_4_abbr' => 'apr.',
+	'date_mois_5_abbr' => 'máj',
+	'date_mois_6_abbr' => 'jún',
+	'date_mois_7_abbr' => 'júl',
+	'date_mois_8_abbr' => 'aug.',
+	'date_mois_9_abbr' => 'sept.'
+);
+
+?>
