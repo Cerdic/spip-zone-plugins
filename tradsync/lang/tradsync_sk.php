09a165355bee7d5d57224450eea205a0bf0134a8
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-24 16:22:16 +0000

    langues (tradsync)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@57669 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ tradsync/lang/tradsync_sk.php
@@ -0,0 +1,118 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'articles_mots_differents' => 'Články',
+	'articles_non_traduits' => 'Články',
+	'articles_originaux_modifies' => 'Články',
+
+	// C
+	'comparer_arborescence' => 'Comparer l\'arborescence', # NEW
+	'configurer_tradsync' => 'Pomoc k prekladu',
+	'configurer_tradsync_description' => 'Configurer les différentes éléments
+		pour affiner l\'aide donnée.', # NEW
+
+	// E
+	'editer_ce_mot' => 'Upraviť toto kľúčové slovo',
+	'editer_cet_article' => 'Upraviť tento článok',
+	'editer_cette_rubrique' => 'Upraviť túto rubriku',
+	'espace_traducteurs' => 'Prekladateľské rozhranie',
+	'explication_comparer_arborescence' => '
+		Deux navigateurs de rubriques l\'un à coté de l\'autre. Cela permet de comparer
+		visuellement la hierarchie de deux branches.
+	', # NEW
+	'explication_espace_traducteurs' => '
+		L\'espace des traducteurs a pour but de faciliter la tâche de recherche
+		de traductions à réaliser ou à vérifier.
+	', # NEW
+	'explication_groupe_mots_multi' => '
+		Choisir les groupes de mots qui reçoivent des mots traduits en utilisant
+		des balises &lt;multi&gt; sur les titres. De la sorte, il sera possible de
+		verifier qu\'une traduction est réalisée pour les mots de ces groupes.
+	', # NEW
+	'explication_langue' => 'Choisir la langue dont il faut verifier
+		l\'état des traductions.', # NEW
+	'explication_traductions_a_realiser' => '
+		Cet espace liste l\'ensemble des articles, rubriques et mots
+		qui ne semblent pas avoir été traduits.
+	', # NEW
+	'explication_traductions_a_verifier' => '
+		Cet espace liste l\'ensemble des articles et rubriques dont
+		l\'article d\'origine a été modifié
+		ultérieurement à la traduction.
+	', # NEW
+	'explication_traductions_de_mots' => '
+		Lister les mots qui ne semblent pas être traduits.
+	', # NEW
+	'explication_traductions_mots_differents' => '
+		Lister les objets traduits qui n\'ont pas exactement
+		les mêmes mots clés attribués par rapport à l\'objet d\'origine.
+	', # NEW
+	'explication_traductions_mots_synchroniser' => '
+		L\'action «synchroniser» copie l\'ensemble des mots de l\'élément
+		d\'origine dans l\'élément traduit. Cependant, il ne supprime pas les
+		mots-clés qui seraient présents en plus dans la traduction.
+	', # NEW
+
+	// I
+	'info_actions' => 'Actions', # NEW
+	'info_revisions' => 'Révisions', # NEW
+	'info_source' => 'Zdroj',
+	'info_statut' => 'Stav',
+	'info_statut_court' => 'S', # NEW
+	'info_titre' => 'Názov',
+	'info_type' => 'Skupina',
+
+	// L
+	'label_groupe_mots_multi' => 'Groupes de mots multis', # NEW
+	'label_langue' => 'Cieľový jazyk',
+	'langue' => 'Jazyk',
+	'langue_court' => 'L', # NEW
+	'langue_en_cours' => 'Vybraný jazyk je teraz: <strong>@langue@.</strong>',
+	'liste_des_lieux' => 'Lieux à explorer', # NEW
+
+	// M
+	'mots_cles_differents' => 'Mots-clés différents de l\'original', # NEW
+	'mots_cles_non_traduits' => 'Kľúčové slová',
+	'mots_non_traduits' => 'Nepreložené kľúčové slová',
+
+	// N
+	'navigateurs' => 'Prehliadače',
+	'non_traduit' => 'Non traduits ou traductions non déclarées', # NEW
+
+	// O
+	'origine_modifiee' => 'Original modifié', # NEW
+
+	// R
+	'rubriques_mots_differents' => 'Rubriky',
+	'rubriques_non_traduites' => 'Rubriky',
+	'rubriques_originales_modifiees' => 'Rubriky',
+
+	// S
+	'second_navigateur' => 'Second navigateur', # NEW
+	'selection_de_la_langue_cible' => 'Výber cieľového jazyka',
+	'synchroniser' => 'Synchronizovať',
+	'synchroniser_tout' => 'Synchronizovať všetky',
+
+	// T
+	'traductions_a_realiser' => 'Preklady, ktoré treba vykonať',
+	'traductions_a_verifier' => 'Preklady, ktoré treba potvrdiť',
+	'traductions_de_mots' => 'Preklady kľúčových slov',
+	'traductions_mots_differents' => 'Synchronizovať kľúčové slová',
+
+	// V
+	'voir_article_source' => 'Voir l\'article source', # NEW
+	'voir_les_revisions_de_la_source' => 'Voir les révisions de la source', # NEW
+	'voir_rubrique_source' => 'Voir la rubrique source', # NEW
+
+	// Z
+	'zbug_critere_necessite_parametre' => 'Le critere {@critere@} necessite un parametre' # NEW
+);
+
+?>
