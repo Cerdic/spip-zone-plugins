cc89759893ac6e0e8eb50f9bb0d44a1eb1391e99
yffic@lefourneau.com <yffic@lefourneau.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-24 18:23:35 +0000

    Un paquet
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@57677 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ minibando/trunk/lang/paquet-minibando_fr.php
@@ -0,0 +1,19 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-minibando
+// Langue: fr
+// Date: 24-01-2012 19:21:48
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// M
+	'minibando_description' => 'Le plugin minibando remplace les boutons d’administration de SPIP par une barre d\'outils qui reprend les éléments disponibles dans la navigation haute de l’espace privé de SPIP.',
+	'minibando_slogan' => 'Un minibando pour un maximum de fonctionnalités !',
+);
+?>
