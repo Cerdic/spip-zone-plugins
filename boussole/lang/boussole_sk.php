e13b9cd1b78b44635e0c9c83c37fde519fb4c462
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-24 16:22:03 +0000

    langues (boussole)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@57660 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ boussole/lang/boussole_sk.php
@@ -0,0 +1,203 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'bouton_boussole' => 'Buzola',
+	'bouton_configurer' => 'Nastaviť',
+	'bouton_supprimer' => 'Odstrániť',
+	'bulle_afficher' => 'Zobraziť v šablónach',
+	'bulle_aller_boussole' => 'Prejsť na stránku buzoly',
+	'bulle_aller_site' => 'Prejsť na odkazovanú stránku',
+	'bulle_cacher' => 'Nezobrazovať v šablónach',
+	'bulle_configurer_boussole' => 'Nastaviť zobrazenie buzoly',
+	'bulle_deplacer_bas' => 'Posunúť nadol',
+	'bulle_deplacer_haut' => 'Posunúť nahor',
+	'bulle_supprimer_boussole' => 'Odstrániť buzolu z databázy',
+
+	// D
+	'descriptif_boussole_spip' => 'La boussole SPIP regroupe l\'ensemble des sites « officiels » de la galaxie SPIP. Elle définit pour chaque site, son logo, son nom, son slogan et sa description. N\'hésitez donc pas à l\'utiliser dans vos propres sites pour aiguiller vos visiteurs dans la galaxie SPIP.', # NEW
+	'descriptif_site_spip_blog' => 'Par ce que SPIP est un projet collaboratif, SPIP-BLOG.net  rassemble billets techniques, auto-dérision, humeurs, trolls, annonces diverses,... En cela il est bien le reflet de la communauté SPIP : d\'abord et avant tout, énormément de tendresse.', # NEW
+	'descriptif_site_spip_contrib' => 'Site collaboratif, SPIP-CONTRIB.net met à disposition l\'ensemble des contributions externes : plugins, scripts, filtres, squelettes, documentations, trucs et astuces,... versées à la communauté (liens de téléchargement) par les utilisateurs de SPIP. Ses forums assurent le lien entre développeurs et utilisateurs.', # NEW
+	'descriptif_site_spip_core' => 'Le site CORE.SPIP.org rassemble l\'historique des modifications du code de SPIP, une gestion de tickets permettant de signaler des bugs et de proposer des suggestions d\'amélioration et un espace de téléchargement des versions de SPIP.', # NEW
+	'descriptif_site_spip_demo' => 'Site de test réinitialisé chaque nuit, DEMO.SPIP.org permet à tout un chacun de tester SPIP dans sa dernière version stable (avec, au choix, le statut de rédacteur ou d\'administrateur), en un clic et sans avoir à l\'installer.', # NEW
+	'descriptif_site_spip_doc' => 'DOC.SPIP.org est le site référenciel où est détaillée, analysée et commentée (nom, emplacement, arguments, rôle et utilisation) chacune des fonctions du code source de SPIP.', # NEW
+	'descriptif_site_spip_edgard' => 'Edgard est le compagnon fidèle et infatigable de SPIP IRC où il intervient toujours à bon escient, avec tendresse et humour. C\'est depuis sa maison de EDGARD.SPIP.org qu\'il amène sur IRC conseils, réponses et bonne humeur. Accessoirement, Edgard est un robot (mais il ne donne pas l\'impression de le savoir...)', # NEW
+	'descriptif_site_spip_forum' => 'FORUM.SPIP.org est le site d\'échange et d\'entraide entre les utilisateurs de SPIP. Le site, qui existe dans une dizaine de langues, s\'organise autour de quatre grandes rubriques : installation & mise-à-jour, utilisation de l’espace privé, administration, gestion, configuration du site, création de squelettes.', # NEW
+	'descriptif_site_spip_herbier' => 'L\'herbier de SPIP n\'est pas un annuaire de liens comme les autres. Non. C\'est un <strong>album souvenir</strong> de sites réalisés avec SPIP, saisis à un moment donné de leur existence sur la toile. Il raconte une histoire du Web, celle de SPIP, depuis 2001. Cet herbier regroupe les sites que nous avons eu envie d\'épingler, parce qu\'intéressants pour tel contenu agréablement mis en valeur, telle aventure humaine, telle fonctionnalité habilement mise en œuvre, ou seulement visuellement séduisants. Des sites dont on se souvient, auprès desquels on est revenu, qui ont fait référence en marquant leur époque d\'une manière ou d\'une autre.', # NEW
+	'descriptif_site_spip_info' => 'En quelques pages, SPIP-INFO.net répond aux questions que se posent celles et ceux qui s\'interrogent sur la capacité de SPIP à servir de base à leur projet de développement d\'un site web. Particuliers, associations, entreprises trouveront ici toutes les informations pour étayer leur choix.', # NEW
+	'descriptif_site_spip_irc' => 'La communauté SPIP, qui ne dort jamais, s\'est dotée d\'un canal IRC (discussion instantanée par internet) ouvert à tous : http://webchat.freenode.net/?channels=#spip', # NEW
+	'descriptif_site_spip_mag' => 'Popis stránky SPIP Mag\'',
+	'descriptif_site_spip_net' => 'À l\'usage des utilisateurs et des webmestres, SPIP.net est le site officiel recommandé à toutes celles et ceux qui veulent installer un site sous SPIP, en comprendre le langage des boucles, balises et filtres, écrire et utiliser les squelettes. Il présente glossaire, tutoriels, conseils, historique des versions et espace de téléchargement. SPIP.net est traduit dans plus de vingt langues.', # NEW
+	'descriptif_site_spip_party' => 'Passer son temps entre un clavier et un écran pour coder du SPIP : c\'est bien. Se rencontrer entre développeurs et utilisateurs dans la vraie vie, en chair et en os, autour d\'un gateau et d\'un jus de fruit (ou autre boisson...) : c\'est beaucoup mieux. SPIP-PARTY.net est le site qui recense toutes ces rencontres : passées (compte-rendus, photos) et à venir (calendrier, agenda).', # NEW
+	'descriptif_site_spip_plugin' => 'PLUGINS.SPIP.net se veut l\'annuaire complet des modules complémentaires pour SPIP (plugins, squelettes, thèmes). Sont présentés pour chaque module : descriptif, auteur, licence, niveau de compatibilité par version de SPIP, dernières modifications apportées, état des traductions, statistique d\'utilisation, liens de documentation et de téléchargement.', # NEW
+	'descriptif_site_spip_programmer' => 'Plutôt destiné à un public de développeurs ou webmestres ayant déjà des connaissances en PHP, SQL, HTML, CSS et JavaScript, PROGRAMMER.SPIP.org présente la plupart des fonctionnalités (APIs, surcharges, pipelines,...) de SPIP à travers de nombreux exemples de code. Le site offre en téléchargement l\'intégralité de son contenu au format pdf sous licence libre cc-by-sa. PROGRAMMER.SPIP.org est consultable en français, anglais et espagnol.', # NEW
+	'descriptif_site_spip_sedna' => 'SEDNA.SPIP.org est l\'agrégateur RSS pour toute l\'actualité de la galaxie SPIP. Les dernières informations de plus de 70 sites gravitant autour de SPIP sont ainsi aisément consultables.', # NEW
+	'descriptif_site_spip_test' => 'Sur GRML.EU, installez facilement votre propre site en SPIP dans sa toute dernière version de développement. Grâce aux capacités de mutualisation de SPIP : une seule installation du «core» pour de multiples sites, vous êtes «chez vous», dans «votre» site, avec «votre» configuration. Convient particulièrement pour des stages de formation à l\'utilisation de SPIP.', # NEW
+	'descriptif_site_spip_twit' => 'SPIP.ORG permet aux utilisateurs de publier de courts messages sur le Web (140 caractères maximum) à destination du public ou de leurs amis. La publication peut se faire via le site, des flux RSS ou encore par messagerie instantanée.', # NEW
+	'descriptif_site_spip_user' => 'SPIP@REZO.net est la liste d\'entraide entre utilisateurs de SPIP. Les archives de cette liste sont consultables sur http://archives.rezo.net/archives/spip.mbox/ ou encore, sous forme de blog sur http://blog.gmane.org/gmane.comp.web.spip.user.', # NEW
+	'descriptif_site_spip_video' => 'Popis stránky Videá',
+	'descriptif_site_spip_zine' => 'Popis stránky SPIPZine',
+	'descriptif_site_spip_zone' => 'Espace de développement régi par une charte, ZONE.SPIP.org donne accès au versioning de toutes les contributions complémentaires à SPIP (plugins, squelettes, thèmes, modèles, outils,...).', # NEW
+	'description_noisette_boussole' => 'Affichage standard d\'une boussole. Vous pouvez choisir le modèle d\'affichage (liens textuels, logos...) ainsi que sa configuration précise', # NEW
+	'description_noisette_boussole_actualite' => 'Affichage des articles syndiqués des sites d\'une boussole selon le modèle d\'affichage <code>boussole_liste_actualite</code>.', # NEW
+	'description_noisette_boussole_contenu_z' => 'Affichage de toutes les informations d\'une boussole comme contenu principal d\'une page Z et selon le modèle d\'affichage <code>boussole_contenu_z</code>.', # NEW
+	'description_noisette_boussole_fil_ariane' => 'Affichage du fil d\'ariane d\'une boussole.', # NEW
+	'description_page_boussole' => 'Stránka s podrobnými údajmi o buzole',
+
+	// I
+	'info_ajouter_boussole' => 'En ajoutant des boussoles à votre base de données, vous aurez la possiblité d\'utiliser les modèles fournis pour les afficher dans vos pages publiques.<br />Une boussole est décrite par un fichier XML contenant les informations sur la boussole et ses sites.<br /><br />Si la boussole existe déjà ce formulaire permettra de la mettre à jour en conservant la configuration d\'affichage.', # NEW
+	'info_aucun_logo_boussole' => 'Buzola nemá žiadne logo',
+	'info_boite_boussoles_gerer' => '<strong>Cette page est uniquement accessible aux responsables du site.</strong><p>Elle permet l’ajout, la mise à jour et la suppression des boussoles. Il est aussi possible de se rendre sur la page de configuration de l\'affichage de chaque boussole.</p>', # NEW
+	'info_configurer_boussole' => 'Ce formulaire vous permez de configurer l\'affichage de la boussole en choisissant les sites à afficher ou pas et l\'ordre d\'affichage dans un groupe. Les sites non affichés sont repérés par un fond hachuré et une police grise.', # NEW
+	'info_fichier_boussole' => 'Zadajte adresu súboru s popisom svojej buzoly',
+	'info_liste_aucune_boussole' => 'Do vašej databázy ešte nebola pridaná žiadna buzola. Na jej pridanie použite formulár nižšie.',
+	'info_liste_boussoles' => 'Na tejto stránke nájdete zoznam buzol pridaných do vašej databázy.',
+	'info_site_boussole' => 'Táto stránka je súčasťou buzoly "@boussole@"',
+
+	// L
+	'label_1_site' => '@nb@ stránka',
+	'label_a_class' => 'Trieda so záložkou (kotvou) loga',
+	'label_actualise_le' => 'Aktualizovaný',
+	'label_affiche' => 'Affiché ?', # NEW
+	'label_afficher_descriptif' => 'Zobraziť popisy stránok?',
+	'label_afficher_lien_accueil' => 'Zobraziť odkaz na úvodnú stránku?',
+	'label_afficher_slogan' => 'Zobraziť slogany stránok?',
+	'label_ariane_separateur' => 'Oddeľovač:',
+	'label_boussole' => 'Buzola, ktorá sa má zobraziť',
+	'label_cartouche_boussole' => 'Zobraziť rám buzoly?',
+	'label_demo' => 'Nájde demo stránku tejto buzoly na adrese',
+	'label_descriptif' => 'Popis',
+	'label_div_class' => 'Classe du div englobant', # NEW
+	'label_div_id' => 'Id du div englobant', # NEW
+	'label_fichier_xml' => 'Súbor XML',
+	'label_li_class' => 'Trieda každého tagu li v zozname',
+	'label_logo' => 'Logo',
+	'label_max_articles' => 'Maximálny počet zobrazených článkov na stránku',
+	'label_max_sites' => 'Maximálny počet stránok',
+	'label_mode' => 'Vyberte si buzolu',
+	'label_mode_standard' => '"@boussole@" – oficiálna buzola stránok SPIPu',
+	'label_mode_url_perso' => 'Vlastná buzola',
+	'label_modele' => 'Spôsob zobrazenia',
+	'label_n_sites' => '@nb@ stránok',
+	'label_nom' => 'Názov',
+	'label_p_class' => 'Classe du paragraphe englobant le descriptif', # NEW
+	'label_sepia' => 'Kód sépiovej farby (sans #)',
+	'label_slogan' => 'Slogan',
+	'label_taille_logo' => 'Maximálna veľkosť loga (v pixeloch)',
+	'label_taille_logo_boussole' => 'Maximálna veľkosť loga buzoly (v pixeloch)',
+	'label_taille_titre' => 'Maximálna veľkosť názvu buzoly',
+	'label_titre_actualite' => 'Zobraziť názov bloku s novinkami?',
+	'label_titre_boussole' => 'Zobraziť nadpis buzoly?',
+	'label_titre_groupe' => 'Zobraziť názov skupiny?',
+	'label_titre_site' => 'Zobraziť názvy stránok?',
+	'label_type_bulle' => 'Informácia zobrazená v bublinkovej nápovedi každého odkazu',
+	'label_type_description' => 'Popis zobrazený vedľa loga',
+	'label_ul_class' => 'Trieda tagu ul v zozname',
+	'label_url' => 'URL',
+	'label_version' => 'Verzia',
+
+	// M
+	'message_nok_boussole_inconnue' => 'Žiadna buzola nemá prezývku "@alias@"',
+	'message_nok_champ_obligatoire' => 'Toto pole je povinné',
+	'message_nok_ecriture_bdd' => 'Chyba pri zápise do databázy',
+	'message_nok_xml_introuvable' => 'Súbor "@fichier@" sa nenašiel',
+	'message_nok_xml_invalide' => 'Súbor XML "@fichier@" s popisom buzoly nevyhovuje štandardu DTD',
+	'message_ok_boussole_actualisee' => 'Buzola "@fichier@" bola aktualizovaná.',
+	'message_ok_boussole_ajoutee' => 'Buzola "@fichier@" bola pridaná.',
+	'modele_boussole_liste_avec_logo' => 'Zoznam odkazov s názvami, logami a popisom',
+	'modele_boussole_liste_par_groupe' => 'Zoznam textových odkazov podľa skupiny',
+	'modele_boussole_liste_simple' => 'Jednoduchý zoznam textových odkazov',
+	'modele_boussole_panorama' => 'Galéria log',
+	'modele_boussole_panorama_sepia' => 'Galéria log s efektom sépia',
+
+	// N
+	'nom_boussole_spip' => 'Galaxia SPIP',
+	'nom_groupe_spip_actualite' => 'Novinky SPIPu',
+	'nom_groupe_spip_aide' => 'Aide et échanges autour de SPIP', # NEW
+	'nom_groupe_spip_decouverte' => 'Objavovanie SPIPu',
+	'nom_groupe_spip_extension' => 'Rožírenia a príspevky k SPIPu',
+	'nom_groupe_spip_reference' => 'Références SPIP', # NEW
+	'nom_noisette_boussole' => 'Buzola',
+	'nom_noisette_boussole_actualite' => 'Buzola – novinky',
+	'nom_noisette_boussole_contenu_z' => 'Buzola – hlavný obsah stránky',
+	'nom_noisette_boussole_fil_ariane' => 'Boussole - Fil d\'ariane de la page', # NEW
+	'nom_page_boussole' => 'Buzola',
+	'nom_site_spip_blog' => 'SPIP Blog',
+	'nom_site_spip_contrib' => 'SPIP-Contrib',
+	'nom_site_spip_core' => 'Jadro SPIPu',
+	'nom_site_spip_demo' => 'Demo SPIPu',
+	'nom_site_spip_doc' => 'SPIP Doc',
+	'nom_site_spip_edgard' => 'Edgard',
+	'nom_site_spip_forum' => 'Diskusné fóra SPIPu',
+	'nom_site_spip_herbier' => 'L\'Herbier de SPIP', # NEW
+	'nom_site_spip_info' => 'SPIP info',
+	'nom_site_spip_irc' => 'SPIP IRC',
+	'nom_site_spip_mag' => 'SPIP Mag\'',
+	'nom_site_spip_net' => 'SPIP.net',
+	'nom_site_spip_party' => 'SPIP Party',
+	'nom_site_spip_plugin' => 'Zásuvné moduly SPIPu',
+	'nom_site_spip_programmer' => 'Programmer SPIP', # NEW
+	'nom_site_spip_sedna' => 'Sedna',
+	'nom_site_spip_test' => 'SPIP Test', # NEW
+	'nom_site_spip_twit' => 'SPIP.org',
+	'nom_site_spip_user' => 'SPIP User', # NEW
+	'nom_site_spip_video' => 'Multimédiá SPIPu',
+	'nom_site_spip_zine' => 'SPIPZine',
+	'nom_site_spip_zone' => 'SPIP Zone',
+
+	// O
+	'onglet_boussoles' => 'Riadiť buzoly',
+	'option_aucune_description' => 'Žiaden popis',
+	'option_descriptif_site' => 'Popis stránky',
+	'option_nom_site' => 'Názov stránky',
+	'option_nom_slogan_site' => 'Názov a slogan stránky',
+	'option_slogan_site' => 'Slogan stránky',
+
+	// R
+	'resume_table_boussoles' => 'Zoznam pridaných buzol',
+
+	// S
+	'slogan_boussole_spip' => 'Obľúbené adresy galaxie SPIP!',
+	'slogan_site_spip_blog' => 'Du logiciel libre et de la tendresse', # NEW
+	'slogan_site_spip_contrib' => 'L\'espace des contributions à SPIP', # NEW
+	'slogan_site_spip_core' => 'L\'espace de développement des versions de SPIP', # NEW
+	'slogan_site_spip_demo' => 'Vyskúšať najnovšiu stabilnú verziu SPIPu',
+	'slogan_site_spip_doc' => 'La référence technique du code de SPIP', # NEW
+	'slogan_site_spip_edgard' => 'Un coup de bot et ça repart !', # NEW
+	'slogan_site_spip_forum' => 'Diskusné fórum používateľov SPIPu',
+	'slogan_site_spip_herbier' => 'Výber významných stránok v SPIPe',
+	'slogan_site_spip_info' => 'Na objavenie SPIPu a komunity okolo neho',
+	'slogan_site_spip_irc' => 'Príďte a porozprávajte sa na chate SPIPu',
+	'slogan_site_spip_mag' => 'Noviny SPIPu',
+	'slogan_site_spip_net' => 'Oficiálna dokumentácia a stiahnutie SPIPu',
+	'slogan_site_spip_party' => 'Kalendár stretnutí a iných akcií okolo SPIPu',
+	'slogan_site_spip_plugin' => 'Priečinok so zásuvnými modulmi SPIPu',
+	'slogan_site_spip_programmer' => 'Dokumentácia vývojárov SPIPu',
+	'slogan_site_spip_sedna' => 'Kanál RSS galaxie SPIP',
+	'slogan_site_spip_test' => 'Vyskúšať inštaláciu a zavedenie SPIPu na stránku',
+	'slogan_site_spip_twit' => 'Mikroblog komunity SPIPu',
+	'slogan_site_spip_user' => 'La liste d\'entraide des utilisateurs de SPIP', # NEW
+	'slogan_site_spip_video' => 'Multimediálna knižnica SPIPu',
+	'slogan_site_spip_zine' => 'Webzine o SPIPe a o všetkom okolo neho',
+	'slogan_site_spip_zone' => 'L\'espace de développement des contributions à SPIP', # NEW
+
+	// T
+	'titre_actualite_spip' => 'Novinky z Galaxie SPIP',
+	'titre_bloc_boussoles' => 'BUZOLY',
+	'titre_boite_autres_boussoles' => 'Ďalšie buzoly',
+	'titre_boite_infos_boussole' => 'BOUSSOLE D\'ALIAS', # NEW
+	'titre_boite_logo_boussole' => 'LOGO BUZOLY',
+	'titre_form_ajouter_boussole' => 'Pridať alebo aktualizovať buzolu',
+	'titre_formulaire_configurer' => 'Nastavenia zobrazenia buzoly',
+	'titre_liste_boussoles' => 'Zoznam dostupných buzol',
+	'titre_page_boussole' => 'Buzola',
+	'titre_page_edition_boussole' => 'Upraviť buzolu'
+);
+
+?>
