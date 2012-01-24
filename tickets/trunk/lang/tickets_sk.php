d9ff497524ba52a8e67a8f8f00a5f4f6e26fe405
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-24 16:22:15 +0000

    langues (tickets)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@57668 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ tickets/trunk/lang/tickets_sk.php
@@ -0,0 +1,218 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'afficher_tickets' => 'Zobraziť lístky',
+	'assignation_attribuee_a' => 'Lístok bol priradený k @nom@.',
+	'assignation_mail_titre' => 'Zmena priradenia lístka',
+	'assignation_modifiee' => 'Priradenie bolo aktualizované',
+	'assignation_non_modifiee' => 'Priradenie nebolo aktualizované',
+	'assignation_supprimee' => 'Priradenie tohto lístka bolo odstránené.',
+	'assigne_a' => 'Priradené k:',
+	'assignes_a' => 'Priradené k',
+	'autres_tickets_ouverts' => 'Ďalšie otvorené lístky',
+
+	// C
+	'cfg_bouton_radio_desactiver_forum_public' => 'Deaktivovať používanie verejných diskusných fór.',
+	'cfg_descr_tickets' => 'Nastavenia zásuvného modulu lístky pre SPIP',
+	'cfg_explication_cacher_moteurs' => 'Cache les tickets des moteurs de recherche en leur demandant de ne pas indexer leur contenus lorsqu\'ils sont affichés dans l\'espace public.', # NEW
+	'cfg_explication_composants' => 'Súčasti oddeľte znakom ":".',
+	'cfg_explication_jalons' => 'Séparez les jalons par le caractère ":".', # NEW
+	'cfg_explication_projets' => 'Projekty oddeľte znakom ":".',
+	'cfg_explication_readonly' => 'Cette partie de la configuration est déjà définie autre part.', # NEW
+	'cfg_explication_versions' => 'Verzie oddeľte znakom ":".',
+	'cfg_form_tickets_autorisations' => 'Nastavenie povolení',
+	'cfg_form_tickets_general' => 'Všeobecné nastavenia',
+	'cfg_inf_type_autorisation' => 'Si vous choisissez par statut ou par auteur, il vous sera demandé ci-dessous votre sélection de statuts ou d\'auteurs.', # NEW
+	'cfg_lbl_autorisation_auteurs' => 'Povoliť podľa zoznamu autorov',
+	'cfg_lbl_autorisation_statuts' => 'Povoliť podľa funkcie autora',
+	'cfg_lbl_autorisation_webmestre' => 'Povoliť iba webmasterom',
+	'cfg_lbl_cacher_moteurs' => 'Schovať vyhľadávače',
+	'cfg_lbl_case_joindre_fichiers' => 'Autoriser à joindre un ou plusieurs fichiers aux tickets', # NEW
+	'cfg_lbl_case_notification_publique' => 'Être notifié dans l\'espace public plutôt que dans l\'espace privé (Nécessite Zpip)', # NEW
+	'cfg_lbl_case_selecteur_navigateur' => 'Disposer d\'un sélecteur de navigateur dans le formulaire d\'édition de ticket', # NEW
+	'cfg_lbl_composants' => 'Súčasti',
+	'cfg_lbl_desactiver_public' => 'Deaktivovať verejný prístup',
+	'cfg_lbl_forums_publics' => 'Komentáre na lístky',
+	'cfg_lbl_jalons' => 'Jalons', # NEW
+	'cfg_lbl_joindre_fichiers' => 'Pripojiť jeden súbor alebo viac',
+	'cfg_lbl_liste_auteurs' => 'Autori stránky',
+	'cfg_lbl_notification_publique' => 'Verejný oznam',
+	'cfg_lbl_projets' => 'Projekty',
+	'cfg_lbl_selecteur_navigateur' => 'Vybrať prehliadač',
+	'cfg_lbl_statuts_auteurs' => 'Možné funkcie',
+	'cfg_lbl_type_autorisation' => 'Méthode d\'autorisation', # NEW
+	'cfg_lbl_versions' => 'Verzie',
+	'cfg_lgd_autorisation_assigner' => 'Priradiť lístky',
+	'cfg_lgd_autorisation_commenter' => 'Okomentovať lístky',
+	'cfg_lgd_autorisation_ecrire' => 'Napísať lístok',
+	'cfg_lgd_autorisation_modifier' => 'Upraviť lístky',
+	'cfg_lgd_champs_optionnels' => 'Nepovinné polia',
+	'cfg_lgd_champs_options_autres' => 'Ďalšie možnosti',
+	'cfg_lgd_notifs_forums' => 'Diskusné fóra a oznamy',
+	'cfg_titre_tickets' => 'Lístky',
+	'champ_assigner' => 'Priradiť k:',
+	'champ_composant' => 'Súčasť:',
+	'champ_createur' => 'Vytvoril(a):',
+	'champ_date' => 'Dátum:',
+	'champ_date_debut' => 'Od:',
+	'champ_date_fin' => 'Až do:',
+	'champ_date_modif' => 'Zmenené',
+	'champ_description' => 'Popis lístka',
+	'champ_exemple' => 'Príklad:',
+	'champ_fichier' => 'Pripojiť súbor',
+	'champ_id' => 'Číslo',
+	'champ_id_assigne' => 'Priradený k:',
+	'champ_id_auteur' => 'Autor:',
+	'champ_importance' => 'Importance :', # NEW
+	'champ_jalon' => 'Jalon ', # NEW
+	'champ_maj' => 'MAJ :', # NEW
+	'champ_maj_long' => 'Dátum aktualizácie',
+	'champ_navigateur' => 'Prehliadač:',
+	'champ_nouveau_commentaire' => 'Nový komentár',
+	'champ_projet' => 'Projekt:',
+	'champ_recherche' => 'Vyhľadávanie:',
+	'champ_severite' => 'Závažnosť:',
+	'champ_statut' => 'Stav:',
+	'champ_sticked' => 'Prilepený:',
+	'champ_texte' => 'Text',
+	'champ_titre' => 'Zhrnutie',
+	'champ_titre_ticket' => 'Názov lístka',
+	'champ_type' => 'Typ:',
+	'champ_url_exemple' => 'URL d’exemple', # NEW
+	'champ_version' => 'Verzia:',
+	'changement_statut_mail' => 'Le statut de ce ticket a été modifié de "@ancien@" à "@nouveau@".', # NEW
+	'classement_assigne' => 'Lístky podľa priradenia',
+	'classement_asuivre' => 'Vaše lístky na sledovanie',
+	'classement_jalon' => 'Tickets par jalon', # NEW
+	'classement_termine' => 'Tickets terminés', # NEW
+	'classement_type' => 'Lístky podľa typu',
+	'commentaire' => 'komentár',
+	'commentaire_aucun' => 'Žiaden komentár',
+	'commentaires' => 'komentárov',
+	'commenter_ticket' => 'Okomentovať tento lístok',
+	'creer_ticket' => 'Vytvoriť lístok',
+
+	// D
+	'date_creation_auteur' => 'Ticket créé le <strong>@date@</strong> par <strong>@nom@</strong>', # NEW
+
+	// E
+	'erreur_date_saisie' => 'Tento dátum je neplatný',
+	'erreur_date_saisie_superieure' => 'La date maximale doit être supérieure à la date minimale', # NEW
+	'erreur_texte_longueur_mini' => 'La longueur minimale du texte est de @nb@ caractères.', # NEW
+	'erreur_verifier_formulaire' => 'Skontrolujte si svoj formulár',
+	'explication_champ_sticked' => 'Les tickets épinglés sont toujours affichés en premier, quelque soit leur statut.', # NEW
+	'explication_description_ticket' => 'Décrivez aussi précisément que possible le besoin ou le problème rencontré.
+	Indiquez en particulier s’il se produit systématiquement ou occasionnellement.
+	S’il s’agit d’un problème d\'affichage, précisez avec quel navigateur vous le rencontrez.', # NEW
+	'explication_description_ticket_ss_nav' => 'Décrivez aussi précisément que possible le besoin ou le problème rencontré.
+	Indiquez en particulier s’il se produit systématiquement ou occasionnellement.', # NEW
+	'explication_fichier' => 'Pridajte súbor k svojmu lístku.',
+	'explication_redaction' => 'Quand vous avez terminé la rédaction de votre ticket, sélectionnez le statut «ouvert et discuté».', # NEW
+	'explication_url_exemple' => 'Indiquez ici l’URL d’une page concernée par ce ticket.', # NEW
+
+	// I
+	'icone_modifier_ticket' => 'Upraviť tento lístok',
+	'icone_retour_ticket' => 'Vrátiť sa k lístku',
+	'info_commentaire' => 'Komentár #@id@:',
+	'info_document_ajoute' => 'Pridaný:',
+	'info_liste_tickets' => 'Lístok',
+	'info_numero_ticket' => 'LÍSTOK ČÍSLO:',
+	'info_ticket_1' => '1 lístok',
+	'info_ticket_aucun' => 'Žiaden lístok',
+	'info_ticket_nb' => 'lístkov: @nb@',
+	'info_tickets' => 'Lístky',
+	'info_tickets_ouvert' => 'otvorené a diskutované',
+	'info_tickets_redac' => 'upravuje sa',
+
+	// L
+	'label_paginer_par' => 'Paginer par :', # NEW
+	'lien_filtrer' => 'Rozriediť lístky',
+	'lien_supprimer_filtres' => 'Enlever tous les filtres', # NEW
+
+	// M
+	'mail_texte_message_auto' => 'Ceci est un message automatique : n\'y repondez pas.', # NEW
+	'message_aucun_ticket_recherche' => 'Aucun ticket ne correspond à votre recherche', # NEW
+	'message_automatique' => 'Ceci est un message automatique : n\'y repondez pas.', # NEW
+
+	// N
+	'no_assignation' => 'Hocikto',
+	'non_assignes' => 'Nepriradené',
+	'nouveau_commentaire_mail' => 'Nový komentár na lístok',
+	'nouveau_ticket' => 'Nový lístok',
+
+	// O
+	'option_navigateur_autre' => 'Iný',
+	'option_navigateur_tous' => 'Všetky prehliadače',
+
+	// P
+	'page_titre' => 'Lístky, systém na sledovanie chýb',
+
+	// R
+	'revenir_gestion' => 'Revenir à la gestion des tickets', # NEW
+
+	// S
+	'severite_bloquant' => 'Bloquant', # NEW
+	'severite_important' => 'Important', # NEW
+	'severite_normal' => 'Normal', # NEW
+	'severite_peu_important' => 'Peu important', # NEW
+	'sinscrire' => 'S\'inscrire', # NEW
+	'statut_ferme' => 'Uzavretý',
+	'statut_ferme_long' => 'Všetky uzavreté lístky',
+	'statut_inchange' => 'Stav sa nezmenil.',
+	'statut_mis_a_jour' => 'Stav bol aktualizovaný',
+	'statut_ouvert' => 'Otvorený a diskutovaný',
+	'statut_poubelle' => 'Do koša',
+	'statut_redac' => 'Upravuje sa',
+	'statut_resolu' => 'Vyriešený',
+	'statut_resolu_long' => 'Všetky vyriešené lístky',
+	'suivre_tickets_assignes_a' => 'Lístky pripojené k @nom@',
+	'suivre_tickets_comments' => 'Suivi des commentaires de tickets', # NEW
+	'suivre_tickets_comments_rss' => 'Suivre ces commentaires par RSS', # NEW
+	'suivre_tickets_de' => 'Les tickets de @nom@', # NEW
+	'suivre_tickets_id' => 'Sledovať lístok #@id@ : @titre@',
+	'suivre_tickets_rss' => 'Suivre ces tickets par RSS', # NEW
+	'suivre_tickets_rss_unique' => 'Suivre ce ticket par RSS', # NEW
+	'suivre_tickets_statut' => 'Les tickets ayant le ou les statuts :', # NEW
+	'suivre_tickets_tous' => 'Všetky lístky',
+	'syndiquer_ticket' => 'Syndikovať lístok:',
+	'syndiquer_tickets' => 'Syndiquer les tickets du site', # NEW
+
+	// T
+	'texte_ticket_statut' => 'Stav lístka:',
+	'ticket' => 'Lístok',
+	'ticket_enregistre' => 'Uložený lístok',
+	'tickets' => 'Lístky',
+	'tickets_autorisations' => 'Povolenia',
+	'tickets_derniers_commentaires' => 'Najnovšie komentáre',
+	'tickets_en_cours_auteur' => 'Lístky @nom@, ktoré sa spracúvajú',
+	'tickets_general' => 'Všeobecné',
+	'tickets_sticked' => 'Prilepené lístky',
+	'tickets_sur_inscription' => 'L\'écriture des tickets ou commentaires n\'est possible qu\'aux personnes identifiées.', # NEW
+	'tickets_sur_inscription_droits' => 'Les droits dont vous disposez sont insuffisants.', # NEW
+	'tickets_traites' => 'Všetky spracované lístky',
+	'tickets_tries' => 'Lístky, ktoré vyhovujú vašim kriktériám',
+	'titre' => 'Lístky, sledovanie chýb',
+	'titre_identification' => 'Identification', # NEW
+	'titre_liste' => 'Zoznam lístkov',
+	'tous_tickets_ouverts' => 'Všetky otvorené lístky',
+	'type_amelioration' => 'Amélioration', # NEW
+	'type_amelioration_long' => 'Les tickets demandant une amélioration', # NEW
+	'type_probleme' => 'Problème', # NEW
+	'type_probleme_long' => 'Les problèmes à résoudre', # NEW
+	'type_tache' => 'Úloha',
+	'type_tache_long' => 'Úlohy, ktoré treba vykonať',
+
+	// V
+	'vos_tickets_assignes' => 'Lístky, ktoré boli priradené vám',
+	'vos_tickets_assignes_auteur' => 'Les tickets de @nom@ qui vous sont assignés', # NEW
+	'vos_tickets_en_cours' => 'Vaše lístky, ktoré sa upravujú'
+);
+
+?>
