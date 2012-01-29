4c759214b682966548a55dc9a198f8a947b4b6e9
f.mata@bedream.fr <f.mata@bedream.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-29 23:27:24 +0000

    Ajout du plugin Mailjet dans _plugins_/mj_customsmtp et modification de archivelist.txt. Le plugin Mailjet permet à Spip d'envoyer tous ses emails en utilisant l'API Mailjet (www.mailjet.com).
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@57817 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ mj_customsmtp/lang/mj_customsmtp_fr.php
@@ -0,0 +1,22 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+'mj_customsmtp_title' => 'Mailjet - Configuration',
+'mj_customsmtp_form_error' => 'Merci de remplir tous les champs obligatoires.',
+'mj_customsmtp_enabled' => 'Activé :',
+'mj_customsmtp_test' => 'Envoyer un email de test maintenant :',
+'mj_customsmtp_test_address' => 'Destinataire de l\'email de test :',
+'mj_customsmtp_username' => 'Clé API :',
+'mj_customsmtp_password' => 'Clé secrète :',
+'mj_settings' => 'Paramètres Mailjet',
+'mj_global_settings' => 'Paramètres généraux',
+'mj_test_mail_subject' => 'Votre email de test depuis Mailjet',
+'mj_test_mail_message' => 'Votre configuration de Mailjet fonctionne !',
+'mj_error_autoconfig' => 'Merci de contacter Mailjet pour corriger cette erreur.<br /><br />Erreur %d - %s',
+
+);
+
+
+?>
