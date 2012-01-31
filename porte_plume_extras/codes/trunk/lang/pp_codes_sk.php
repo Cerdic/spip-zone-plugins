5b293c56aa6b796efb3c38eda24141f18238b8d6
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-31 09:09:10 +0000

    langues (pp_codes)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@57876 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ porte_plume_extras/codes/trunk/lang/pp_codes_sk.php
@@ -0,0 +1,34 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// L
+	'label_activer_barre_edition' => 'Panel úprav',
+	'label_activer_barre_forum' => 'Panel diskusného fóra',
+	'label_outils_actifs' => 'Aktívne nástroje',
+
+	// O
+	'outil_inserer_cadre_css' => 'Vložiť predformátovaný kód CSS (rám)',
+	'outil_inserer_cadre_php' => 'Vložiť predformátovaný kód PHP (rám)',
+	'outil_inserer_cadre_spip' => 'Vložiť predformátovaný kód SPIPu (rám)',
+	'outil_inserer_cadre_xml' => 'Vložiť predformátovaný kód XML (rám)',
+	'outil_inserer_kbd' => 'Vložiť vstup z klávesnice (kdb)',
+	'outil_inserer_lien_trac' => 'Vložiť odkaz na trac SPIPu',
+	'outil_inserer_pre' => 'Vložiť predformátovaný kód (pre)',
+	'outil_inserer_samp' => 'Vložiť výstup kódu (samp)',
+	'outil_inserer_var' => 'Vložiť premennú (var)',
+
+	// P
+	'pp_codes' => 'Počítačové kódy pre Porte Plume',
+
+	// T
+	'titre_activer_extension_sur' => 'Na čo aktivovať panely s nástrojmi?',
+	'titre_configurer_pp_codes' => 'Nastaviť rozšírenie počítačových kódov pre Porte Plume'
+);
+
+?>
