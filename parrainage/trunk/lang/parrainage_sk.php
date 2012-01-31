54a86ec523250c24862ef9ff02f6a22fa7b634c9
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-31 09:09:09 +0000

    langues (parrainage)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@57875 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- parrainage/trunk/lang/parrainage_sk.php
+++ parrainage/trunk/lang/parrainage_sk.php
@@ -8,15 +8,15 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
-	'ajouter_filleul_confirmation' => 'Kontakt bol pridaný, potom môžete tohto človeka pozvať.',
+	'ajouter_filleul_confirmation' => 'Ak si tohto človeka pridáte do kontaktov, potom ho môžete pozvať.',
 
 	// C
 	'configuration_delai_sans_nouvelles_label' => 'Časový limit pre stav "Žiadne nové"',
 	'configuration_invitation_obligatoire_label' => 'Vyžaduje sa pozvánka',
-	'configuration_titre' => 'Nastavenia sponzoringu',
+	'configuration_titre' => 'Nastavenia patronátu',
 
 	// E
-	'erreur_aucun_filleul' => 'Nevybrali ste žiaden subjekt, ktorý sponzorujete.',
+	'erreur_aucun_filleul' => 'Nevybrali ste žiadneho priateľa.',
 	'erreur_invitation_invalide' => 'Vaša pozvánka sa nezhoduje so zadanou e-mailovou adresou',
 	'erreur_invitation_obligatoire' => 'Na to, aby ste sa mohli zaregistrovať na tejto stránke, musíte mať pozvánku.',
 
@@ -24,7 +24,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'filleul_actions' => 'Akcie',
 	'filleul_ajouter' => 'Pridať',
 	'filleul_email' => 'E-mailová adresa',
-	'filleul_nom' => 'Meno (názov)',
+	'filleul_nom' => 'Meno',
 	'filleul_statut' => 'Stav',
 
 	// I
@@ -37,25 +37,25 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'parrainage_inviter' => 'Poslať pozvánku',
 	'parrainage_message_aucun' => 'Nikoho z týchto ľudí netreba pozvať.',
 	'parrainage_message_aucun_1' => 'Tento človek je už pozvaný alebo zaregistrovaný.',
-	'parrainage_message_erreur' => 'Au moins une invitation n\'a pas été envoyée correctement.', # NEW
+	'parrainage_message_erreur' => 'Minimálne jedna pozvánka nebola odoslaná úspešne.',
 	'parrainage_message_label' => 'Vaša správa',
-	'parrainage_message_ok_pluriel' => '@nombre@ invitations ont bien été programmées, elles sont maintenant en cours d\'envoi.', # NEW
+	'parrainage_message_ok_pluriel' => '@nombre@ pozvánok bolo naprogramovaných, teraz sa posielajú.',
 	'parrainage_message_ok_singulier' => 'Pozvánka bola naprogramovaná, teraz sa posiela.',
-	'parrainage_supprimer_filleul' => 'Odstrániť tento sponzorovaný subjekt',
+	'parrainage_supprimer_filleul' => 'Odstrániť tohto priateľa',
 	'parrainage_supprimer_filleul_confirmation' => 'Určite chcete odstrániť @nom@ zo svojich kontaktov?',
-	'plugin_nom' => 'Sponzoring',
+	'plugin_nom' => 'Patronát',
 
 	// S
 	'statut_contact' => 'Kontakt',
-	'statut_contact_explication' => 'Cette personne est dans votre carnet d\'adresse. Vous pouvez l\'inviter à rejoindre le site.', # NEW
+	'statut_contact_explication' => 'Tento človek je vo vašom adresári. Môžete ho pozvať, aby sa pridal na stránku.',
 	'statut_deja_inscrit' => 'Už zaregistrovaný',
-	'statut_deja_inscrit_explication' => 'Cette personne est déjà inscrite sur le site, mais n\'est pas votre filleul !', # NEW
+	'statut_deja_inscrit_explication' => 'Tento človek je už zaregistrovaný na stránke, ale nie je váš priateľ!',
 	'statut_en_cours' => 'Pozvánka sa posiela',
 	'statut_en_cours_explication' => 'Vaša pozvánka sa posiela.',
-	'statut_filleul' => 'Sponzorovaný subjekt',
-	'statut_filleul_depuis' => 'Sponzorovaný subjekt registrovaný od @date@',
-	'statut_filleul_explication' => 'Grâce à vous, cette personne s\'est inscrite sur le site : vous êtes son parrain.', # NEW
-	'statut_invite' => 'Hosť @duree@',
+	'statut_filleul' => 'Priateľ',
+	'statut_filleul_depuis' => 'Priateľ od @date@',
+	'statut_filleul_explication' => 'Vďaka vám sa tento človek zaregistroval na stránke: vy ste jeho patrón.',
+	'statut_invite' => 'Pozvaný @duree@',
 	'statut_invite_explication' => 'Nedávno ste tohto človeka požiadali.',
 	'statut_sans_nouvelles' => 'Žiadni noví od @date@',
 	'statut_sans_nouvelles_explication' => 'Tohto človeka ste pred nejakým časom pozvali, ale nikdy neprišiel.',
