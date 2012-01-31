3498cc4b7df073260d233c433702d68f4850ac3c
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-01-31 09:09:16 +0000

    langues (tradsync)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@57881 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- tradsync/lang/tradsync_sk.php
+++ tradsync/lang/tradsync_sk.php
@@ -13,43 +13,35 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'articles_originaux_modifies' => 'Články',
 
 	// C
-	'comparer_arborescence' => 'Comparer l\'arborescence', # NEW
+	'comparer_arborescence' => 'Porovnať stromovú štruktúru',
 	'configurer_tradsync' => 'Pomoc k prekladu',
-	'configurer_tradsync_description' => 'Configurer les différentes éléments
-		pour affiner l\'aide donnée.', # NEW
+	'configurer_tradsync_description' => 'Nastaviť rôzne prvky na spresnenie poskytovanej pomoci',
 
 	// E
 	'editer_ce_mot' => 'Upraviť toto kľúčové slovo',
 	'editer_cet_article' => 'Upraviť tento článok',
 	'editer_cette_rubrique' => 'Upraviť túto rubriku',
 	'espace_traducteurs' => 'Prekladateľské rozhranie',
-	'explication_comparer_arborescence' => '
-		Deux navigateurs de rubriques l\'un à coté de l\'autre. Cela permet de comparer
-		visuellement la hierarchie de deux branches.
-	', # NEW
-	'explication_espace_traducteurs' => '
-		L\'espace des traducteurs a pour but de faciliter la tâche de recherche
-		de traductions à réaliser ou à vérifier.
-	', # NEW
+	'explication_comparer_arborescence' => '		Dva prehliadače rubrík jeden vedľa druhého. Umožňuje vizuálne
+		porovnať hierarchiu dvoch vetiev.
+	',
+	'explication_espace_traducteurs' => '		Prekladateľské rozhranie slúži na pomoc s vykonávaním prekladov, ich schvaľovaním a vyhľadávaním.
+	',
 	'explication_groupe_mots_multi' => '
 		Choisir les groupes de mots qui reçoivent des mots traduits en utilisant
 		des balises &lt;multi&gt; sur les titres. De la sorte, il sera possible de
 		verifier qu\'une traduction est réalisée pour les mots de ces groupes.
 	', # NEW
-	'explication_langue' => 'Choisir la langue dont il faut verifier
-		l\'état des traductions.', # NEW
-	'explication_traductions_a_realiser' => '
-		Cet espace liste l\'ensemble des articles, rubriques et mots
-		qui ne semblent pas avoir été traduits.
-	', # NEW
+	'explication_langue' => 'Vyberte jazyk, pri ktorom sa musí overiť
+		stav prekladov.',
+	'explication_traductions_a_realiser' => '		V tomto rozhraní sa nachádza zoznam článkov, rubrík a kľúčových slov, ktoré sa nezdajú byť preložené.',
 	'explication_traductions_a_verifier' => '
 		Cet espace liste l\'ensemble des articles et rubriques dont
 		l\'article d\'origine a été modifié
 		ultérieurement à la traduction.
 	', # NEW
-	'explication_traductions_de_mots' => '
-		Lister les mots qui ne semblent pas être traduits.
-	', # NEW
+	'explication_traductions_de_mots' => '		Urobiť zoznam kľúčových slov, ktoré sa nezdajú byť preložené.
+	',
 	'explication_traductions_mots_differents' => '
 		Lister les objets traduits qui n\'ont pas exactement
 		les mêmes mots clés attribués par rapport à l\'objet d\'origine.
@@ -61,11 +53,11 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	', # NEW
 
 	// I
-	'info_actions' => 'Actions', # NEW
-	'info_revisions' => 'Révisions', # NEW
+	'info_actions' => 'Akcie',
+	'info_revisions' => 'Opravy',
 	'info_source' => 'Zdroj',
 	'info_statut' => 'Stav',
-	'info_statut_court' => 'S', # NEW
+	'info_statut_court' => 'S',
 	'info_titre' => 'Názov',
 	'info_type' => 'Skupina',
 
@@ -73,21 +65,21 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'label_groupe_mots_multi' => 'Groupes de mots multis', # NEW
 	'label_langue' => 'Cieľový jazyk',
 	'langue' => 'Jazyk',
-	'langue_court' => 'L', # NEW
+	'langue_court' => 'J',
 	'langue_en_cours' => 'Vybraný jazyk je teraz: <strong>@langue@.</strong>',
-	'liste_des_lieux' => 'Lieux à explorer', # NEW
+	'liste_des_lieux' => 'Miesta na prehľadávanie',
 
 	// M
-	'mots_cles_differents' => 'Mots-clés différents de l\'original', # NEW
+	'mots_cles_differents' => 'Kľúčové slová odlišné od originálu',
 	'mots_cles_non_traduits' => 'Kľúčové slová',
 	'mots_non_traduits' => 'Nepreložené kľúčové slová',
 
 	// N
 	'navigateurs' => 'Prehliadače',
-	'non_traduit' => 'Non traduits ou traductions non déclarées', # NEW
+	'non_traduit' => 'Nepreložené alebo nedeklarované preklady',
 
 	// O
-	'origine_modifiee' => 'Original modifié', # NEW
+	'origine_modifiee' => 'Originál bol zmenený',
 
 	// R
 	'rubriques_mots_differents' => 'Rubriky',
@@ -95,7 +87,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'rubriques_originales_modifiees' => 'Rubriky',
 
 	// S
-	'second_navigateur' => 'Second navigateur', # NEW
+	'second_navigateur' => 'Druhý prehliadač',
 	'selection_de_la_langue_cible' => 'Výber cieľového jazyka',
 	'synchroniser' => 'Synchronizovať',
 	'synchroniser_tout' => 'Synchronizovať všetky',
@@ -107,12 +99,12 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'traductions_mots_differents' => 'Synchronizovať kľúčové slová',
 
 	// V
-	'voir_article_source' => 'Voir l\'article source', # NEW
-	'voir_les_revisions_de_la_source' => 'Voir les révisions de la source', # NEW
-	'voir_rubrique_source' => 'Voir la rubrique source', # NEW
+	'voir_article_source' => 'Zobraziť zdrojový článok',
+	'voir_les_revisions_de_la_source' => 'Zobraziť opravy zdroja',
+	'voir_rubrique_source' => 'Zobraziť zdrojovú rubriku',
 
 	// Z
-	'zbug_critere_necessite_parametre' => 'Le critere {@critere@} necessite un parametre' # NEW
+	'zbug_critere_necessite_parametre' => 'Kritérium {@critere@} si vyžaduje parameter'
 );
 
 ?>
