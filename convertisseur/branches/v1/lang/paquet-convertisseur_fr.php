cc59432610602582c1decb2cd07f21ad801e148d
yffic@lefourneau.com <yffic@lefourneau.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-02-04 17:36:37 +0000

    Paquets de langue manquants
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@58033 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ convertisseur/branches/v1/lang/paquet-convertisseur_fr.php
@@ -0,0 +1,20 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-convertisseur
+// Langue: fr
+// Date: 04-02-2012 18:31:53
+// Items: 3
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// C
+	'convertisseur_description' => 'Convertit les formats de type wiki, phpBB, quark xpress tags en format SPIP et vice versa ; intègre au besoin les textes convertis dans des articles ; on peut uploader plusieurs textes en un seul coup en les zippant au préalable',
+	'convertisseur_nom' => 'Convertisseur de formats',
+	'convertisseur_slogan' => 'Convertir les textes wiki, phpBB et quark xpress en SPIP',
+);
+?>
