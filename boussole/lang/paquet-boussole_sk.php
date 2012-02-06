d8164bd8f5214754eab3cb69eb3c0c8e7e45cba1
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-02-06 11:34:45 +0000

    langues (paquet-boussole)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@58072 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- boussole/lang/paquet-boussole_sk.php
+++ boussole/lang/paquet-boussole_sk.php
@@ -8,9 +8,8 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// B
-	'boussole_description' => 'Installez ce plugin sur votre site et vous aurez accès, dans vos squelettes, à l\'ensemble des sites de la Galaxie SPIP en utilisant
-les modèles, les noisettes ou la page Z proposés.
-_ Pour les bricoleurs, vous pouvez aussi utiliser ce plugin pour afficher votre propre liste de sites et concevoir vos propres affichages.', # NEW
+	'boussole_description' => 'Nainštalujte si tento zásuvný modul na svoju stránku a vo svojich šablónach budete mať prístup na všetky stránky Galaxie SPIPu pomocou  modelov šablón, orieškov alebo navrhovanej stránky Z.
+_ Sami môžete tento zásuvný modul používať na zobrazenie svojho vlastného zoznamu stránok a vytvoriť svoje vlastné zobrazenia.',
 	'boussole_nom' => 'Buzola',
 	'boussole_slogan' => 'Obľúbené adresy v galaxii SPIPu alebo inde!'
 );
