df1c57982e2b189addde096aa5b5ae8ca7170c35
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-02-08 09:30:20 +0000

    langues (ieconfig)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@58121 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- ieconfig/trunk/lang/ieconfig_sk.php
+++ ieconfig/trunk/lang/ieconfig_sk.php
@@ -9,15 +9,15 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// E
 	'explication_ieconfig_import_fichier' => 'Súbor s nastaveniami vo formáte YAML.',
-	'explication_ieconfig_import_local' => 'Liste des configurations détectées dans un sous-répertoire <i>ieconfig/</i> ou dans le répertoire <i>tmp/ieconfig/</i>.', # NEW
+	'explication_ieconfig_import_local' => 'Zoznam nastavení, ktorý sa našiel v podpriečinku répertoire <i>ieconfig/</i> v priečinku <i>tmp/ieconfig/.</i>',
 
 	// I
 	'item_sauvegarder' => 'Zálohovať súbor',
 	'item_telecharger' => 'Stiahnuť súbor',
 
 	// L
-	'label_configurations_a_exporter' => 'Configurations à exporter', # NEW
-	'label_exporter' => 'Exportovať?', # MODIF
+	'label_configurations_a_exporter' => 'Nastavenia na exportovanie',
+	'label_exporter' => 'Exportovať nastavenia?',
 	'label_ieconfig_export' => 'Všeobecné možnosti exportu',
 	'label_ieconfig_export_choix' => 'Čo chcete?',
 	'label_ieconfig_export_description' => 'Popis:',
@@ -28,7 +28,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'label_importer' => 'Importovať?',
 
 	// M
-	'message_erreur_export' => 'Une erreur s\'est produite lors de l\'enregistrement du fichier <i>@filename@</i> dans le répertoire <i>tmp/ieconfig/</i>.', # NEW
+	'message_erreur_export' => 'Pri ukladaní súboru <i>@filename@</i> do priečinka <i>tmp/ieconfig/</i> nastala chyba.',
 	'message_erreur_fichier_import_manquant' => 'Musíte zadať súbor s nastaveniami, ktorý sa má nahrať.',
 	'message_ok_export' => 'Súbor <i>@filename@</i> bol zálohovaný v priečinku <i>tmp/ieconfig/.</i>',
 	'message_ok_import' => 'Nastavenia boli úspešne nahraté.',
@@ -36,10 +36,10 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// T
 	'texte_configuration_identique' => 'Nastavenia v tomto súbore sú rovnaké ako vaše aktuálne nastavenia.',
 	'texte_description' => 'Popis:',
-	'texte_ieconfig_export_explication' => 'Vous pouvez sauvegarder localement votre export au format YAML dans le répertoire <i>tmp/ieconfig/</i> ou bien le télécharger.', # NEW
-	'texte_importer_configuration' => 'Cette option vous permet de restaurer une sauvegarde précédemment effectuée votre configuration ou bien importer une configuration fournie par un plugin. Soyez prudent avec cette fonctionnalité : <strong>les modifications, pertes éventuelles, sont irréversibles</strong>.', # NEW
+	'texte_ieconfig_export_explication' => 'Váš export môžete uložiť lokálne vo formáte YAML do priečinka <i>tmp/ieconfig/</i> alebo si ho stiahnuť.',
+	'texte_importer_configuration' => 'Táto možnosť vám umožňuje obnoviť databázu vytvorenú pred vykonaním zmien v nastaveniach alebo nahrať nastavenia, ktoré poskytuje zásuvný modul. Túto funkciu používajte opatrne: <strong>zmeny, potenciálne straty sa nedajú vrátiť späť.</strong>',
 	'texte_nom' => 'Názov:',
-	'texte_plugins_manquants' => 'Ce fichier contient des configurations pour les plugins suivants qui ne sont pas activés sur votre site : <i>@plugins@</i>. Ces configurations ne seront donc pas importées.', # NEW
+	'texte_plugins_manquants' => 'V tomto súbore sú nastavenia pre tieto zásuvné moduly, ktoré neboli  aktivované na vašej stránke: <i>@plugins@.</i> Tieto nastavenia sa nenahrajú.',
 	'titre_export' => 'Exportovať nastavenia',
 	'titre_ieconfig' => 'Nahrať/Exportovať nastavenia',
 	'titre_import' => 'Nahrať nastavenia'
