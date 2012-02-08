c5e88e47eff3d1415b45d45feabb48ecd27ac171
brunobergot@gmail.com <brunobergot@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-02-08 12:26:27 +0000

    premier dépot du plugin badge_don
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@58141 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ badge_don/badge_don_pipelines.php
@@ -0,0 +1,22 @@
+<?php
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+function badge_don_insert_head($flux){
+	$flux .= '<link rel="stylesheet" href="'.find_in_path('badge_don.css').'" type="text/css" media="projection, screen, tv" />';
+	return $flux;
+}
+
+function badge_don_affichage_final($page) {
+
+	if (!$GLOBALS['html']) return $page;
+	
+	$badge = recuperer_fond('modeles/badge_don', array());
+	// Insertion du badge avant la fermeture du body
+	if (!strpos($page, 'id="badge_don"'))
+		$page = preg_replace(',</body>,i', "$badge\n".'\0', $page, 1);
+
+	return $page;
+}
+
+?>
---
+++ badge_don/lang/badge_don_fr.php
@@ -0,0 +1,13 @@
+<?php
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	'label_url_don' => 'Adresse de la page de dons',
+	'titre_badge_don' => 'Badge pour dons',
+	'titre_lien' => 'Soutenir par un don'
+	
+);
+
+?>
