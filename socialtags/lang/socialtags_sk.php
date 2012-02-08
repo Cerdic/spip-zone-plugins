2f9ab88f1ba5ea39aff6dea684fd99c53ec49335
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-02-08 09:30:26 +0000

    langues (socialtags)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@58127 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- socialtags/lang/socialtags_sk.php
+++ socialtags/lang/socialtags_sk.php
@@ -13,14 +13,14 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'cfg_3' => 'Dokumentácia',
 	'cfg_activer_javascript' => 'SVP, prosím aktivovať Javascript.',
 	'cfg_badge_fb' => 'Odznak/Widget pre Facebook',
-	'cfg_badge_fb_existe' => 'Si vous avez un Badge Facebook (<a href="http://www.facebook.com/facebook-widgets/" class="spip_out">aide sur le site FB</a>), indiquez ci-dessous son contenu, soit en JS, soit en HTML.', # NEW
+	'cfg_badge_fb_existe' => 'Ak máte odznak Facebooku (<a href="http://www.facebook.com/facebook-widgets/" class="spip_out">pomoc na stránke FB</a>), uveďte jeho obsah v JS alebo v HTML.',
 	'cfg_badge_html' => 'Odznak HTML',
 	'cfg_badge_html_ajouter_1' => '(pridať do svojej šablóny',
 	'cfg_badge_html_ajouter_2' => ', alebo do článku',
 	'cfg_badge_js' => 'Odznak JS',
 	'cfg_badge_js_1' => ' (automaticky zaradené do <code>@code@</code>)',
 	'cfg_en_bas' => 'V spodnej časti každej stránky',
-	'cfg_exemples' => 'Exemples (sur les squelettes dist) :', # NEW
+	'cfg_exemples' => 'Príklady (šablón dist) :',
 	'cfg_explication_boite_info' => 'Tu si môžete vybrať, s ktorými stránkami sa podelíte o svoje články.',
 	'cfg_inserer' => 'Vložiť blok tagov sociálnych sietí pod nasledujúci blok:',
 	'cfg_insertion' => 'Vloženie medzi stránky:',
@@ -28,7 +28,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'cfg_plusone' => 'Google plus one',
 	'cfg_plusone_existe' => 'Na aktivovanie Google+1 inštalovať zásuvný modul (<a href="http://www.spip-contrib.net/Google-1" class="spip_out">pomoc pre spip-contrib.</a>)',
 	'cfg_referer' => 'Zobraziť len odznak a tlačidlá pre návštevníkov týchto stránok.',
-	'cfg_selector' => 'Sélecteur :', # NEW
+	'cfg_selector' => 'Oddeľovač:',
 	'cfg_sous_bloc' => 'Pod blokom názvu stránok "článok"',
 	'cfg_sous_div' => 'S divom s názvom id=socialtags',
 	'cfg_titre_boite_info' => 'Nastavenia zásuvného modulu Social tags',
