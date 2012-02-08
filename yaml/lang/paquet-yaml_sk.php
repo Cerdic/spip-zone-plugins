62a9dd68916288f4eeab982a53c076836bd62ded
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-02-08 09:30:33 +0000

    langues (paquet-yaml)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@58134 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- yaml/lang/paquet-yaml_sk.php
+++ yaml/lang/paquet-yaml_sk.php
@@ -8,8 +8,8 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// Y
-	'yaml_description' => 'Ce plugin fournit les fonctions de lecture/écriture du format YAML:
-	<code>yaml_decode()</code> et <code>yaml_encode()</code>. Il fournit aussi le format yaml pour la boucle (DATA).', # NEW
+	'yaml_description' => 'Tento zásuvný modul ponúka funkcie čítanie/zápisu formátu YAML:
+	<code>yaml_decode()</code> a <code>yaml_encode()</code>. Poskytuje formát yaml aj pre cyklus (DATA).',
 	'yaml_slogan' => 'Jednoduchý formát súborov na upravovanie zoznamov dát'
 );
 
