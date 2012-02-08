8d8cdfc48a1704e76dd5c376bc9421424a38b6f9
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-02-08 09:30:24 +0000

    langues (paniers)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@58125 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- paniers/lang/paniers_sk.php
+++ paniers/lang/paniers_sk.php
@@ -14,8 +14,8 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'configurer_titre' => 'Nastaviť zásuvný modul Paniers',
 
 	// D
-	'description_noisette' => 'Affiche le mini-panier (Nombre d\'articles et prix total)', # NEW
-	'dont_total_taxe' => 'Dont total des taxes', # NEW
+	'description_noisette' => 'Zobrazí minivozík (počet položiek a celkovú cenu)',
+	'dont_total_taxe' => 'S DPH',
 
 	// L
 	'limite_enregistres_label' => 'Zaregistrovaní návštevníci',
@@ -29,14 +29,14 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'mon_panier_voir' => 'Zobraziť môj nákupný vozík',
 
 	// N
-	'noisette_label_afficher_lien_page_panier' => 'Afficher un lien vers la page spip.php?page=panier ?', # NEW
-	'noisette_label_afficher_titre_noisette' => 'Afficher un titre ?', # NEW
-	'noisette_label_niveau_titre' => 'Choisir le niveau du titre', # NEW
-	'noisette_label_titre_noisette' => 'Titre :', # NEW
-	'noisette_label_titre_noisette_perso' => 'Si titre personnalisé :', # NEW
-	'noisette_titre_perso' => 'Titre personnalisé', # NEW
+	'noisette_label_afficher_lien_page_panier' => 'Zobraziť odkaz na stránku spip.php?page=panier?',
+	'noisette_label_afficher_titre_noisette' => 'Zobraziť nadpis?',
+	'noisette_label_niveau_titre' => 'Vybrať úroveň nadpisu',
+	'noisette_label_titre_noisette' => 'Nadpis:',
+	'noisette_label_titre_noisette_perso' => 'Ak je nadpis vlastný:',
+	'noisette_titre_perso' => 'Vlastný nadpis',
 	'nom_bouton_plugin' => 'Paniers',
-	'nom_noisette' => 'Mini panier', # NEW
+	'nom_noisette' => 'Minivozík',
 
 	// P
 	'panier_description' => 'Popis',
@@ -52,16 +52,16 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'panier_valider' => 'Potvrdiť nákupný vozík',
 	'panier_vide' => 'Nákupný vozík je prázdny.',
 	'paniers' => 'Nákupné vozíky',
-	'produit_un' => '1 produit', # NEW
-	'produits_plusieurs' => '@nb@ produits', # NEW
+	'produit_un' => '1 výrobok',
+	'produits_plusieurs' => 'počet výrobkov: @nb@',
 
 	// T
-	'titre_mon_panier' => 'Mon panier', # NEW
-	'titre_panier' => 'Panier', # NEW
+	'titre_mon_panier' => 'Môj nákupný vozík',
+	'titre_panier' => 'Nákupný vozík',
 
 	// V
-	'vider_le_panier' => 'Vider le panier', # NEW
-	'voir_mon_panier' => 'Voir mon panier' # NEW
+	'vider_le_panier' => 'Vyprázdniť vozík',
+	'voir_mon_panier' => 'Zobraziť vozík'
 );
 
 ?>
