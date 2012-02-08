8ca75fbaf5003fe87c28e885c0d60e3e8ae89779
guillaume@outofspace.fr <guillaume@outofspace.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-02-08 10:06:40 +0000

    - Remplacement des accents présent dans le fichier de traduction francais par leur equivalent HTML - Coquille dans la fonction liste_objets_coordonnees, les exec ont changé dans SPIP3
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@58139 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- coordonnees/trunk/coordonnees_pipelines.php
+++ coordonnees/trunk/coordonnees_pipelines.php
@@ -13,9 +13,9 @@
 **/
 function liste_objets_coordonnees($quoi = '') {
 	$liste = array(
-		'auteur'       => array('titre'=>_T('coordonnees:auteurs'),   'exec'=>'auteur_infos'),
-		'article'      => array('titre'=>_T('coordonnees:articles'),  'exec'=>'articles'),
-		'rubrique'     => array('titre'=>_T('coordonnees:rubriques'), 'exec'=>'naviguer'),
+		'auteur'       => array('titre'=>_T('coordonnees:auteurs'),   'exec'=>'auteur'),
+		'article'      => array('titre'=>_T('coordonnees:articles'),  'exec'=>'article'),
+		'rubrique'     => array('titre'=>_T('coordonnees:rubriques'), 'exec'=>'rubrique'),
 		'contact'      => array('titre'=>_T('contacts:contacts'),     'exec'=>'contact'),
 		'organisation' => array('titre'=>_T('contacts:organisations'),'exec'=>'organisation'),
 	);
--- coordonnees/trunk/lang/coordonnees_fr.php
+++ coordonnees/trunk/lang/coordonnees_fr.php
@@ -11,7 +11,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'adresses' => 'Adresses',
 	'ajouter_adresse' => 'Ajouter une adresse',
 	'ajouter_email' => 'Ajouter un email',
-	'ajouter_telephone' => 'Ajouter un numéro',
+	'ajouter_telephone' => 'Ajouter un num&eacute;ro',
 	'articles' => 'Articles',
 	'auteurs' => 'Auteurs',
 
@@ -19,12 +19,12 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'configuration_coordonnees' => 'Configuration des coordonnées',
 	'confirmer_suppression_adresse' => 'Voulez-vous vraiment supprimer cette adresse ?',
 	'confirmer_suppression_email' => 'Voulez-vous vraiment supprimer ce courriel ?',
-	'confirmer_suppression_numero' => 'Voulez-vous vraiment supprimer ce numéro ?',
+	'confirmer_suppression_numero' => 'Voulez-vous vraiment supprimer ce num&eacute;ro ?',
 
 	// E
-	'editer_adresse' => 'Éditer une adresse',
-	'editer_email' => 'Éditer un courriel',
-	'editer_numero' => 'Éditer un numéro',
+	'editer_adresse' => '&Eacute;diter une adresse',
+	'editer_email' => '&Eacute;diter un courriel',
+	'editer_numero' => '&Eacute;diter un num&eacute;ro',
 	'emails' => 'Courriels',
 	'explication_objets_actifs' => 'Sur quels objets éditoriaux proposer les coordonnées ?',
 	'explication_type_adresse' => 'Le type peut être \'perso\' ou \'pro\'.',
@@ -35,29 +35,29 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'fax' => 'Fax.',
 
 	// I
-	'info_adresse_utilisee_par' => 'Adresse utilisée par :',
-	'info_email_utilise_par' => 'Courriel utilisé par :',
+	'info_adresse_utilisee_par' => 'Adresse utilis&eacute;e par :',
+	'info_email_utilise_par' => 'Courriel utilis&eacute; par :',
 	'info_gauche_numero_adresse' => 'N° Adresse',
 	'info_gauche_numero_email' => 'N° Email',
-	'info_gauche_numero_numero' => 'N° Numéro',
-	'info_numero_utilise_par' => 'Numéro utilisé par :',
-	'item_nouveau_numero' => 'Nouveau numéro',
+	'info_gauche_numero_numero' => 'N° Num&eacute;ro',
+	'info_numero_utilise_par' => 'Num&eacute;ro utilis&eacute; par :',
+	'item_nouveau_numero' => 'Nouveau num&eacute;ro',
 	'item_nouvel_email' => 'Nouveau courriel',
 	'item_nouvelle_adresse' => 'Nouvelle adresse',
 
 	// L
 	'label_boite_postale' => 'Boîte Postale',
 	'label_code_postal' => 'Code Postal',
-	'label_complement' => 'Complément',
+	'label_complement' => 'Compl&eacute;ment',
 	'label_email' => 'Courriel',
-	'label_numero' => 'Numéro',
+	'label_numero' => 'Num&eacute;ro',
 	'label_objets_actifs' => 'Objets',
 	'label_pays' => 'Pays',
-	'label_telephone' => 'Téléphone',
+	'label_telephone' => 'T&eacute;l&eacute;phone',
 	'label_titre' => 'Titre',
 	'label_type_adresse' => 'Type d\'adresse',
 	'label_type_email' => 'Type de courriel',
-	'label_type_numero' => 'Type de numéro',
+	'label_type_numero' => 'Type de num&eacute;ro',
 	'label_ville' => 'Ville',
 	'label_voie' => 'Adresse',
 
@@ -65,13 +65,13 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'mobile' => 'Mobile',
 	'modifier_adresse' => 'Modifier cette adresse',
 	'modifier_email' => 'Modifier ce courriel',
-	'modifier_numero' => 'Modifier ce numéro',
+	'modifier_numero' => 'Modifier ce num&eacute;ro',
 
 	// N
-	'nouveau_numero' => 'Nouveau numéro',
+	'nouveau_numero' => 'Nouveau num&eacute;ro',
 	'nouvel_email' => 'Nouveau courriel',
 	'nouvelle_adresse' => 'Nouvelle adresse',
-	'numeros' => 'Numéros',
+	'numeros' => 'Num&eacute;ros',
 
 	// R
 	'rubriques' => 'Rubriques',
@@ -79,10 +79,10 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// S
 	'supprimer_adresse' => 'Supprimer cette adresse',
 	'supprimer_email' => 'Supprimer ce courriel',
-	'supprimer_numero' => 'Supprimer ce numéro',
+	'supprimer_numero' => 'Supprimer ce num&eacute;ro',
 
 	// T
-	'titre_coordonnees' => 'Coordonnées'
+	'titre_coordonnees' => 'Coordonn&eacute;es'
 );
 
 ?>
