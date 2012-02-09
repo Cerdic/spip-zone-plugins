0d08224504d770b762838ffbb6a766a8247486d5
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-02-09 13:54:48 +0000

    un paquet.xml pour voir
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@58163 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ dictionnaires/trunk/lang/paquet-dictionnaires_fr.php
@@ -0,0 +1,19 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-dictionnaires
+// Langue: fr
+// Date: 09-02-2012 14:32:07
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// D
+	'dictionnaires_description' => 'Permet la création de dictionnaires regroupant des définitions. Les termes définis sont automatiquement détectés dans les textes du site et il est alors possible de les insérer dans le HTML du texte de manière accessible.',
+	'dictionnaires_slogan' => 'Créer des dictionnaires',
+);
+?>
