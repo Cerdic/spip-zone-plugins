edcd1037d519c91f33ca38e4957e5cdf822d0676
marcimat@rezo.net <marcimat@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-02-14 14:00:17 +0000

    Des simplifications. Je ne suis pas certain que ces balises servent à quelque chose...
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@58283 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- contacts_et_organisations/trunk/contacts_fonctions.php
+++ contacts_et_organisations/trunk/contacts_fonctions.php
@@ -5,48 +5,39 @@
  */
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
-function balise_PSEUDO_dist($p) {
-	/***
-	 * Cette balise s'emploie dans une boucle (CONTACTS).
-	 * Elle retourne le champ #NOM du spip_auteur d'un contact.
-	 *
-	 */
-	$p->code = "recuperer_fond('modeles/pseudo',
+/***
+ * Cette balise s'emploie dans une boucle (CONTACTS).
+ * Elle retourne le champ #NOM du spip_auteur d'un contact.
+ *
+ */
+function balise_NOM_AUTEUR_dist($p) {
+	$connect = !$p->id_boucle ? '' : $p->boucles[$p->id_boucle]->sql_serveur;
+	
+	$p->code = "recuperer_fond('modeles/nom_auteur',
 		array('id_contact' => ".champ_sql('id_contact', $p)
 		."), array('trim'=>true), "
 		. _q($connect)
 		.")";
 	$p->interdire_scripts = false; // securite apposee par recuperer_fond()
 	return $p;
-	}
+}
 
+
+/***
+ * Cette balise s'emploie dans une boucle (CONTACTS).
+ * Elle retourne les champs #NOM des spip_organisations liés au contact.
+ *
+ */
 function balise_ORGANISATIONS_dist($p) {
-	/***
-	 * Cette balise s'emploie dans une boucle (CONTACTS).
-	 * Elle retourne les champs #NOM des spip_organisations liés au contact.
-	 *
-	 */
-	$_lesorganisations = champ_sql('lesorganisations', $p); 
 
-	// si le champ 'lesorganisations' n'existe pas	
-	 
-	if ($_lesorganisations
-	AND $_lesorganisations != '@$Pile[0][\'lesorganisations\']') {
-		// on applique le modele lesorganisations.html en passant id_contact dans le contexte;
-		$p->code = "safehtml($_lesorganisations)";
-		$p->interdire_scripts = false;
-	} else {
-		// sinon on prend le champ 'lesorganisations' 
-		$connect = !$p->id_boucle ? '' 
-		  : $p->boucles[$p->id_boucle]->sql_serveur;
+	$connect = !$p->id_boucle ? '' : $p->boucles[$p->id_boucle]->sql_serveur;
 
-		$p->code = "recuperer_fond('modeles/lesorganisations',
-			array('id_contact' => ".champ_sql('id_contact', $p)
-			."), array('trim'=>true), "
-			. _q($connect)
-			.")";
-		$p->interdire_scripts = false; // securite apposee par recuperer_fond()
-	}
+	$p->code = "recuperer_fond('modeles/lesorganisations',
+		array('id_contact' => ".champ_sql('id_contact', $p)
+		."), array('trim'=>true), "
+		. _q($connect)
+		.")";
+	$p->interdire_scripts = false; // securite apposee par recuperer_fond()
 
 	return $p;
 }
