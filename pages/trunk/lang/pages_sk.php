7c6b840be34374f4caa235d914617fad3244e1ea
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-02-17 19:22:04 +0000

    langues (pages)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@58586 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- pages/trunk/lang/pages_sk.php
+++ pages/trunk/lang/pages_sk.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// extrait automatiquement de http://trad.spip.org
+// extrait automatiquement de http://trad.spip.org/tradlang_module/pages?lang_cible=sk
 // ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
