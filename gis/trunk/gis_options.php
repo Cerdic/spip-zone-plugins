d21573d07de674bda576a068e0147a07ed86ea3d
xdjuj@apsulis.com <xdjuj@apsulis.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-02-22 17:03:37 +0000

    Report de la rev 58348 de b_b
    Log:
    version 2.2.2 : maj de la lib Mapstraction + ajout d'un paramètre autofocus_marker au modèle de la carte pour permettre de limiter l'affichage à une seule infobulle
    
    
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@58780 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- gis/trunk/gis_options.php
+++ gis/trunk/gis_options.php
@@ -1,9 +1,12 @@
 <?php
 
-define('_DIR_LIB_GIS',_DIR_RACINE.'lib/mxn-gis-2.0.9/');
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+define('_DIR_LIB_GIS','lib/mxn-gis-2.2.2/');
 
 $table_des_traitements['VILLE'][]= 'typo(extraire_multi(%s))';
 $table_des_traitements['PAYS'][]= 'typo(extraire_multi(%s))';
 
 $GLOBALS['logo_libelles']['id_gis'] = _T('gis:libelle_logo_gis');
+
 ?>
--- gis/trunk/javascript/gis.js
+++ gis/trunk/javascript/gis.js
@@ -6,15 +6,30 @@ var gis_get_navigator_location = function(map,zoom){
 	}
 }
 
-function gis_focus_marker(id, map) {
+function gis_close_infowindows(map) {
 	var map = eval('map'+ map);
 	for (i in map.markers) {
-		if (map.markers[i].attributes.marker == id) {
-			map.setCenter(map.markers[i].location);
-			map.markers[i].openBubble();
-		} else {
-			map.markers[i].closeBubble();
+		map.markers[i].closeBubble();
+	}
+}
+
+function gis_focus_marker(id, map) {
+	var mxn = eval('map'+ map);
+	gis_close_infowindows(map);
+	for (i in mxn.markers) {
+		if (mxn.markers[i].attributes.marker == id) {
+			mxn.setCenter(mxn.markers[i].location);
+			mxn.markers[i].openBubble();
 		}
+	}
+}
+
+function gis_autofocus_marker(map) {
+	var mxn = eval('map'+ map);
+	for (i in mxn.markers) {
+		mxn.markers[i].openInfoBubble.addHandler(function(name, source, args) {
+			gis_close_infowindows(map);
+		});
 	}
 }
 
