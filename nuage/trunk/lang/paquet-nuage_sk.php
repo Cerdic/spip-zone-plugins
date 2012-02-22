313d3c5c24acb656d1281dab27222d871f9fb06f
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-02-22 15:10:05 +0000

    langues (paquet-nuage)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@58769 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- nuage/trunk/lang/paquet-nuage_sk.php
+++ nuage/trunk/lang/paquet-nuage_sk.php
@@ -8,12 +8,12 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// N
-	'nuage_description' => 'Ce plugin permet d’afficher les mots-clés en vrac, en faisant varier la taille de la police en fonction du nombre d\'articles associés au mot-clé. En d\'autres termes, spip permet de créer des « tags » !
+	'nuage_description' => 'Tento zásuvný modul umožňuje zobraziť kľúčové slová neusporiadane rôznou  veľkosťou písma v závislosti od počtu položiek priradených ku kľúčovému slovu. Inými slovami spip umožňuje vytvárať "značky"!
 
-	Plusieurs modèles disponibles :
--* article_nuage : affiche les mots-clés de l\'article la taille variant en fonction de la fréquence des mots dans les articles du site
--* rubrique_nuage : affiche les mots-clés des articles de la branche, la taille variant en fonction de la fréquence des mots dans les articles de cette branche
--* nuage_popularite : affiche les mots-clés du site (ou d\'un groupe), la taille variant en fonction de sa popularité relative (somme des popularité des articles ayant ce mot)', # NEW
+	K dispozícii je niekoľko modelov:
+-* article_nuage: zobrazí kľúčové slová článku, ktorých veľkosť písma sa odlišuje podľa frekvencie kľúčových slov v článku na stránke,
+-* rubrique_nuage: zobrazí kľúčové slová článkov vetvy, ktorých veľkosť písma sa bude odlišovať podľa frekvencie kľúčových slov v článkoch danej vetvy,
+-* nuage_popularite: zobrazí kľúčové slová stránky (alebo skupiny), ktorých veľkosť písma sa bude odlišovať podľa ich relatívnej popularity (súčet popularity článkov s daným kľúčovým slovom)',
 	'nuage_slogan' => 'Zobrazí kľúčové slová s rôznou veľkosťou písma'
 );
 
