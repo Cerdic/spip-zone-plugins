924a97142e499d8fe18275784b3209ac86127ce6
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-02-23 14:35:22 +0000

    langues (paquet-agenda)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@58793 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- agenda/trunk/lang/paquet-agenda_fr.php
+++ agenda/trunk/lang/paquet-agenda_fr.php
@@ -1,19 +1,13 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-// 
-///  Fichier produit par PlugOnet
-// Module: paquet-agenda
-// Langue: fr
-// Date: 02-11-2011 16:35:56
-// Items: 2
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/agenda/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// A
+	// A
 	'agenda_description' => 'Agenda Evénementiel',
-	'agenda_slogan' => 'Agenda Evénementiel',
+	'agenda_slogan' => 'Agenda Evénementiel'
 );
+
 ?>
