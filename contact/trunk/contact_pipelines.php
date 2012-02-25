b829f5062da4853129f1e5b4eeb1614c2e0a0e4a
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-02-25 12:54:46 +0000

    Sécurité sur contact_administrations
    Le pipeline jqueryui_plugins est dans le core, plus besoin du plugin adéquat
    
    Un titre sur la page de configuration
    
    On replace la page de configuration au bon endroit
    
    Un lien dans le menu pour la configuration
    
    On supprime les répertoire vides
    
    
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@58824 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- contact/trunk/contact_pipelines.php
+++ contact/trunk/contact_pipelines.php
@@ -3,12 +3,12 @@
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 /**
- * Pipeline jqueryui_forcer pour demander au plugin l'insertion des scripts pour .sortable()
+ * Pipeline jqueryui_plugins (SPIP) pour demander au plugin l'insertion des scripts pour .sortable()
  *
  * @param array $plugins
  * @return array
  */
-function contact_jqueryui_forcer($plugins){
+function contact_jqueryui_plugins($plugins){
 	$plugins[] = "jquery.ui.core";
 	$plugins[] = "jquery.ui.widget";
 	$plugins[] = "jquery.ui.mouse";
