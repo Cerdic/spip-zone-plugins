5b2dbf284622480e1d003ec3a1a6f897322d2f06
fil@rezo.net <fil@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-02-26 14:45:11 +0000

    Sauts de ligne automatiques, adressables en CSS :   .autobr:before{content:"\0B6";color:orange;} effacables en CSS :   .autobr br {content:""} pour retrouver le fonctionnement SPIP ancien (ignorer les sauts de ligne) :   define('_AUTOBR', '');
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@58836 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- textwheel/wheels/spip/spip.php
+++ textwheel/wheels/spip/spip.php
@@ -1,5 +1,19 @@
 <?php
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
 include_spip('inc/texte');
+
+/*
+
+ Sauts de ligne automatiques, adressables en CSS :
+   .autobr:before{content:"\0B6";color:orange;}
+ effacables en CSS :
+   .autobr br {content:""}
+ pour retrouver le fonctionnement SPIP ancien (ignorer les sauts de ligne) :
+   define('_AUTOBR', '');
+
+*/
+
 
 /**
  * callback pour la puce qui est definissable/surchargeable
@@ -16,6 +30,8 @@ function replace_puce(){
  * on refait le preg, a la main
  */
 function fermer_para_mano(&$t) {
+	defined('_AUTOBR')||define('_AUTOBR', "<span class='autobr'><br /></span>");
+
 	# match: ",<p (.*)<(/?)(STOP P|div|pre|ul|ol|li|blockquote|h[1-6r]|t(able|[rdh]|body|foot|extarea)|form|object|center|marquee|address|d[ltd]|script|noscript|map|button|fieldset|style)\b,UimsS"
 	# replace: "\n<p "+trim($1)+"</p>\n<$2$3"
 
@@ -28,12 +44,14 @@ function fermer_para_mano(&$t) {
 			",</?(?:stop p|div|pre|ul|ol|li|blockquote|h[1-6r]|t(able|[rdh]|body|foot|extarea)|form|object|center|marquee|address|d[ltd]|script|noscript|map|button|fieldset|style)\b,S",
 			$pi, $r)) {
 				$pos = strpos($pi, $r[0]);
-				$t .= "\n<p ".rtrim(substr($p,0,$pos))."</p>\n".substr($p,$pos);
+				$t .= "\n<p ".str_replace("\n", _AUTOBR."\n", rtrim(substr($p,0,$pos)))."</p>\n".substr($p,$pos);
 			} else {
 				$t .= '<p '.$p;
 			}
 		}
 	}
+
+	$t = str_replace(_AUTOBR."\n<br />", '<br />', $t);
 
 	return $t;
 }
