8bd34d6cffefc24438e16caa7155047abb47d0f5
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-02-27 07:45:01 +0000

    langues (paquet-multilang)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@58860 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- multilang/lang/paquet-multilang_fr.php
+++ multilang/lang/paquet-multilang_fr.php
@@ -1,19 +1,13 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-multilang
-// Langue: fr
-// Date: 23-02-2012 22:31:20
-// Items: 2
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/multilang/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// M
-	'multilang_description' => 'Ce plugin rajoute un menu de langues du type <code>[fr] [en] [it]</code> au-dessus de chaque formulaire, en fonction des langues activ&#233;es dans la configuration du site.',
-	'multilang_slogan' => 'Gérer l\'affichage du contenu des tags multi par un menu',
+	// M
+	'multilang_description' => 'Ce plugin rajoute un menu de langues du type <code>[fr] [en] [it]</code> au-dessus de chaque formulaire, en fonction des langues activées dans la configuration du site.',
+	'multilang_slogan' => 'Gérer l\'affichage du contenu des tags multi par un menu'
 );
+
 ?>
--- multilang/lang/paquet-multilang_it.php
+++ multilang/lang/paquet-multilang_it.php
@@ -1,18 +1,15 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-multilang
-// Langue: it
-// Date: 23-02-2012 22:31:20
-// Items: 1
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org/tradlang_module/paquet-multilang?lang_cible=it
+// ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// M
+	// M
 	'multilang_description' => 'Questo plugin aggiunge un menu di linguaggi di tipo <code>[fr] [it] [it]</code> nella parte superiore di ogni modulo, a seconda della lingua selezionata nella configurazione del sito.',
+	'multilang_slogan' => 'Gérer l\'affichage du contenu des tags multi par un menu' # NEW
 );
+
 ?>
