a80ed213f1dc10674002325d68740bee27594d80
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-02-27 11:56:06 +0000

    langues (paquet-courtcircuit)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@58879 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- court-circuit/trunk/lang/paquet-courtcircuit_fr.php
+++ court-circuit/trunk/lang/paquet-courtcircuit_fr.php
@@ -1,19 +1,13 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-courtcircuit
-// Langue: fr
-// Date: 26-02-2012 14:49:10
-// Items: 2
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/court-circuit/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// C
+	// C
 	'courtcircuit_description' => 'Permet de rediriger certaines rubriques vers un article particulier. Vous pouvez configurer, dans l\'espace privé, les régles de redirection à appliquer.',
-	'courtcircuit_slogan' => 'Accês direct aux articles',
+	'courtcircuit_slogan' => 'Accês direct aux articles'
 );
+
 ?>
