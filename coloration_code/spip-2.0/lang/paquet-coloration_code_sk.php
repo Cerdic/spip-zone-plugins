734ae03561b8896a33001277516b855f936cda51
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-02-27 07:44:51 +0000

    langues (paquet-coloration_code)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@58852 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- coloration_code/spip-2.0/lang/paquet-coloration_code_sk.php
+++ coloration_code/spip-2.0/lang/paquet-coloration_code_sk.php
@@ -8,19 +8,19 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// C
-	'coloration_code_description' => 'Il suffit de mettre le code entre
+	'coloration_code_description' => 'Jednoducho dajte kód do
 _ {{&lt;code class="langage"&gt;...&lt;/code&gt;}}
-_ ou avec un cadre
+_ alebo do rámu
 _ {{&lt;cadre class="langage"&gt;...&lt;/cadre&gt;}}.
 
-Les langages supportés sont ceux fournis par [->http://sourceforge.net/projects/geshi/] avec une classe supplementaire: "spip".
+Podporované jazyky sú tie, ktoré ponúka [->http://sourceforge.net/projects/geshi/] s doplnkovou triedou: "spip".
 
 
-Par défaut, si le code mis en surbrillance fait plus d\'une ligne, il est mis en cache sous forme textuelle et proposé au téléchargement. Ce fonctionnement est controlé globalement par une constante PLUGIN_COLORATION_CODE_TELECHARGE défaut true. Il peut être forcé localement en rajoutant la classe "sans_telechargement" dans un sens ou "chargement" dans l\'autre comme
+Ak je podľa predvolených nastavení kód zvýraznený viac ako jednou čiarou, je kešovaný vo forme textu a je k dispozícii na stiahnutie.  Celú túto operáciu globálne ovláda  konštanta PLUGIN_COLORATION_CODE_TELECHARGE v predvolených nastaveniach nastavená na hodnotu "true". Zvýraznenie môže byť lokálne vynútené pridaním triedy "sans_telechargement" (= bez stiahnutia) na jednej strane alebo pre "nahrávanie" na strane druhej
 _ {{&lt;code class="php sans_telechargement"&gt;}}
 
-Vous pouvez aussi utiliser le filtre {coloration_code_color} dans un squelette comme
-_ <code>#TEXTE**|coloration_code_color{spip,code}</code> : colore #TEXTE avec le language spip en format code (sans cadre), voir exemple lecode.html. L\'url depuis l\'article serait <code>#URL_SITE_SPIP/spip.php?page=lecode&id_article=#ENV{id_article}</code>', # NEW
+Môžete tiež využiť filter {coloration_code_color} v šablóne, ako
+_ <code>#TEXTE**|coloration_code_color{spip,code}</code>:  colore #TEXTE s jazykom spip vo formáte kódu (bez rámu), pozrite si príklad lecode.html. Internetová adresa pre článok by bola <code>#URL_SITE_SPIP/spip.php?page=lecode&id_article=#ENV{id_article}</code>',
 	'coloration_code_nom' => 'Kód zvýraznenia',
 	'coloration_code_slogan' => 'Zvýraznenie syntaxe zdrojového kódu v článkoch'
 );
