f57283295cf23a8d3fcccbcaf7a9ee976d818d3e
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-02-27 07:44:48 +0000

    langues (paquet-boutonstexte)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@58850 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- boutonstexte/trunk/lang/paquet-boutonstexte_sk.php
+++ boutonstexte/trunk/lang/paquet-boutonstexte_sk.php
@@ -8,9 +8,9 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// B
-	'boutonstexte_description' => 'Rajoute des boutons pour l\'affichage des textes comme augmenter/diminuer police. Les boutons sont rajoutés avant chaque div de classe texte du contenu si ils ne sont pas déjà présents dans la page (images de classe textsizeup, textsizedown ou textonly)
+	'boutonstexte_description' => 'Pridá tlačidlá na zväčšenie/zmenšenie veľkosti písma. Tlačidlá budú pridané pred každý text označený ako trieda div za predpokladu, že sa ešte nenachádzajú na stránke (obrázky triedy textsizeup, textsizedown alebo textonly)
 
-La configuration optionnelle peut se faire en installant le plugin cfg. Certains éléments peuvent être forcés par javascript.', # NEW
+Voliteľné nastavenia môžete pridať, ak nainštalujete zásuvný modul cfg. Niektoré prvky môžu na svoje fungovanie potrebovať javascript.',
 	'boutonstexte_nom' => 'Tlačidlá v texte',
 	'boutonstexte_slogan' => 'Tlačidlá na zmenu veľkosti písma'
 );
