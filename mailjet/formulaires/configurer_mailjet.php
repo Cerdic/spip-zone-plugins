0b6af655e5ea6fb1f03deedba9a96a903b62bebf
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-02-29 19:25:56 +0000

    Formulaire de configuration moderne, et page de configuration dediee accessible depuis le panneau plugin
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@58941 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ mailjet/formulaires/configurer_mailjet.php
@@ -0,0 +1,82 @@
+<?php
+/*
+ * Plugin Mailjet
+ *
+ */
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+function formulaires_configurer_mailjet_charger_dist(){
+
+	include_spip('inc/autoriser');
+	if (!autoriser('configurer'))
+		return _T('mailjet:configuration_acces_interdit');
+
+	$valeurs = array(
+		'mailjet_enabled' => isset($GLOBALS['meta']['mailjet_enabled'])?$GLOBALS['meta']['mailjet_enabled']:true,
+		'mailjet_username' => isset($GLOBALS['meta']['mailjet_username'])?$GLOBALS['meta']['mailjet_username']:'',
+		'mailjet_password' => isset($GLOBALS['meta']['mailjet_password'])?$GLOBALS['meta']['mailjet_password']:'',
+	);
+
+	return $valeurs;
+}
+
+function formulaires_configurer_mailjet_verifier_dist(){
+	$erreurs = array();
+
+	if (_request('mailjet_enabled')){
+		foreach (array('mailjet_username','mailjet_password') as $obli){
+			if (!_request($obli))
+				$erreurs[$obli] = _T('info_obligatoire');
+		}
+	}
+	// sinon 0 dans mailjet_enabled
+	else
+		set_request('mailjet_enabled',0);
+
+	return $erreurs;
+}
+
+function formulaires_configurer_mailjet_traiter_dist(){
+
+	$res = array('message_ok'=>_T('config_info_enregistree'),'editable'=>true);
+	$enabled = _request('mailjet_enabled');
+	ecrire_meta('mailjet_enabled',$enabled);
+	if ($enabled){
+		foreach (array('mailjet_username','mailjet_password') as $config){
+			ecrire_meta($config,_request('config'));
+		}
+		// detecter le port
+		$configs = array (
+			array ('ssl://', 465),
+			array ('tls://', 587),
+			array ('', 587),
+			array ('', 588),
+			array ('tls://', 25),
+			array ('', 25)
+		);
+
+		$host = 'in.mailjet.com';
+		$connected = FALSE;
+
+		for ($i = 0; $i < count ($configs); ++$i){
+			$soc = @fSockOpen ($configs[$i][0].$host, $configs[$i][1], $errno, $errstr, 5);
+			if ($soc){
+				fClose($soc);
+				$connected = TRUE;
+				break;
+			}
+		}
+		if ($connected) {
+			ecrire_meta('mailjet_host', $configs[$i][0].$host);
+			ecrire_meta('mailjet_port', $configs[$i][1]);
+			$res['message_ok'] .= "<br />"._T('mailjet:mj_autoconfig_host_port',array('host'=>"<tt>".$GLOBALS['meta']['mailjet_host']."</tt>",'port'=>$GLOBALS['meta']['mailjet_port']));
+		}
+		else {
+			unset($res['message_ok']);
+			$res['message_erreur'] = _T('mailjet:mj_error_autoconfig',array('no'=>$errno,'str'=>$errstr));
+		}
+	}
+
+	return $res;
+}
--- mailjet/lang/mailjet_fr.php
+++ mailjet/lang/mailjet_fr.php
@@ -5,17 +5,19 @@
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 'mailjet_title' => 'Mailjet - Configuration',
 'mailjet_form_error' => 'Merci de remplir tous les champs obligatoires.',
-'mailjet_enabled' => 'Activé :',
+'label_mailjet_enabled_1' => 'Activer la prise en charge des mails par Mailjet',
 'mailjet_test' => 'Envoyer un email de test maintenant :',
 'mailjet_test_address' => 'Destinataire de l\'email de test :',
-'mailjet_username' => 'Clé API :',
-'mailjet_password' => 'Clé secrète :',
+'label_mailjet_username' => 'Clé API :',
+'label_mailjet_password' => 'Clé secrète :',
+'explications_api_mailjet' => 'Les informations pour le paramétrage de Mailjet sont disponibles dans votre compte Mailjet <a href="https://fr.mailjet.com/account/api_keys">https://fr.mailjet.com/account/api_keys</a>',
+'configuration_acces_interdit' => 'Vous n\'avez pas le droit d\'accèder à cette configuration',
 'mj_settings' => 'Paramètres Mailjet',
 'mj_global_settings' => 'Paramètres généraux',
 'mj_test_mail_subject' => 'Votre email de test depuis Mailjet',
 'mj_test_mail_message' => 'Votre configuration de Mailjet fonctionne !',
-'mj_error_autoconfig' => 'Merci de contacter Mailjet pour corriger cette erreur.<br /><br />Erreur %d - %s',
-
+'mj_error_autoconfig' => 'Impossible de determiner la configuration Host/Port<br />Erreur @no@ - @str@',
+'mj_autoconfig_host_port' => 'La detection automatique a déterminé le Host @host@ et port @port@',
 );
 
 
