ead6d8f04db624cd0a46d68394f83496d5197965
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-03-01 19:46:25 +0000

    langues (courtcircuit)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@58982 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ court-circuit/trunk/lang/courtcircuit_sk.php
@@ -0,0 +1,44 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org/tradlang_module/courtcircuit?lang_cible=sk
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'aide_en_ligne' => 'Online pomocník',
+
+	// C
+	'configurer_courtcircuit' => 'Nastaviť pravidlá krátkeho prepojenia rubrík',
+	'courtcircuit' => 'Krátke prepojenie',
+
+	// E
+	'explication_liens_rubriques' => 'Zmeniť internetové adresy presmerovaných rubrík priamo v šablónach?',
+	'explication_regles' => 'Rôzne pravidlá budú testované v tomto poradí. Ak presmerovanie neupravuje žiadne pravidlo, potom bude rubrika zobrazená normálne.',
+	'explication_sousrubrique' => 'Prehľadať prvú podrubriku (zotriedenú podľa čísla, nadpisu a dátumu)? Pravidlá na presmerovanie budú znova testované v tejto podrubrike.',
+	'explication_variantes_squelettes' => 'Príklad: šablóny v podobe rubrique-2.html alebo rubrique=3.html.',
+
+	// I
+	'item_appliquer_redirections' => 'Použiť pravidlá na presmerovanie',
+	'item_jamais_rediriger' => 'Nikdy nepresmerovať',
+	'item_ne_pas_rediriger' => 'Nepresmerovať',
+	'item_rediriger_sur_article' => 'Presmerovať na tento článok',
+
+	// L
+	'label_article_accueil' => 'Úvodný článok rubriky',
+	'label_composition_rubrique' => 'Rubrika s kompozíciou',
+	'label_exceptions' => 'Výnimky',
+	'label_liens' => 'URL rubrík',
+	'label_liens_rubriques' => 'Konať podľa tagu #URL_RUBRIQUE?',
+	'label_plus_recent' => 'Najnovší článok rubriky',
+	'label_plus_recent_branche' => 'Článok najnovšej vetvy',
+	'label_rang_un' => 'Prvý článok (číslované články)',
+	'label_regles' => 'Pravidlá presmerovania rubrík',
+	'label_sousrubrique' => 'Podrubriky',
+	'label_un_article' => 'Jediný článok rubriky',
+	'label_variantes_squelettes' => 'Rubrika s variantom šablón'
+);
+
+?>
