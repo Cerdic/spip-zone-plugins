8782d37ec268867fbe094232a3c9eae13a0d83c6
marcimat@rezo.net <marcimat@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-03-04 19:40:18 +0000

    On tente en espérant ne rien compromettre, de réaliser des saisies avec des noms de tableaux (pour SPIP 3 uniquement) :
    #SAISIE{input, contenant/casier, … } qui créera un name="contenant[casier]"  . La valeur sera correctement trouvée car #ENV{contenant/casier} est calculé en SPIP 3.
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@59077 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- saisies/saisies_fonctions.php
+++ saisies/saisies_fonctions.php
@@ -7,4 +7,32 @@ include_spip('balise/saisie');
 // picker_selected (spip 3)
 include_spip('formulaires/selecteur/generique_fonctions');
 
+
+/**
+ * Passer un nom en une valeur compatible avec une classe css
+ * toto => toto,
+ * toto/truc => toto_truc,
+ * toto[truc] => toto_truc,
+**/
+function saisie_nom2classe($nom) {
+	return str_replace(array('/', '[', ']'), '_', $nom);
+}
+
+/**
+ * Passer un nom en une valeur compatible avec un name de formulaire
+ * toto => toto,
+ * toto/truc => toto[truc],
+ * toto[truc] => toto[truc],
+**/
+function saisie_nom2name($nom) {
+	if (false === strpos($nom, '/')) {
+		return $nom;
+	}
+	$nom = explode('/', $nom);
+	$premier = array_shift($nom);
+	$nom = implode('][', $nom);
+	return $premier . '[' . $nom . ']';
+}
+
+
 ?>
