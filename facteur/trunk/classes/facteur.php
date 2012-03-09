a060e7ddd9a12fbf9be36f8d6bc9db675c9d2ba1
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-03-09 15:45:25 +0000

    coquille
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@59167 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- facteur/trunk/classes/facteur.php
+++ facteur/trunk/classes/facteur.php
@@ -48,7 +48,7 @@ class Facteur extends PHPMailer {
 		}
 		else
 			if (!$this->AddAddress($email))
-				spip_log("Erreur AddAddress $adresseMail : ".print_r($this->ErrorInfo,true),'facteur');
+				spip_log("Erreur AddAddress $email : ".print_r($this->ErrorInfo,true),'facteur');
 
 		if (!empty($GLOBALS['meta']['facteur_smtp_sender'])) {
 			$this->Sender = $GLOBALS['meta']['facteur_smtp_sender'];
