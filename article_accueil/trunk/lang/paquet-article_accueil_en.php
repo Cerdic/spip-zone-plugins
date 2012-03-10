40bcdfe2a2ef7bddff683a9d59efee38d4bbc946
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-03-10 21:58:43 +0000

    langues (paquet-article_accueil)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@59214 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- article_accueil/trunk/lang/paquet-article_accueil_en.php
+++ article_accueil/trunk/lang/paquet-article_accueil_en.php
@@ -1,20 +1,16 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-article_accueil
-// Langue: en
-// Date: 10-03-2012 19:39:38
-// Items: 3
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org/tradlang_module/paquet-article_accueil?lang_cible=en
+// ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// A
+	// A
 	'article_accueil_description' => 'This plugin allows you to assign an article as home of a section. Field "id_article_accueil" can then be used by the skeletons.',
 	'article_accueil_nom' => 'Home article',
-	'article_accueil_slogan' => 'Assign a home article to a section',
+	'article_accueil_slogan' => 'Assign a home article to a section'
 );
+
 ?>
--- article_accueil/trunk/lang/paquet-article_accueil_fr.php
+++ article_accueil/trunk/lang/paquet-article_accueil_fr.php
@@ -1,20 +1,14 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-article_accueil
-// Langue: fr
-// Date: 10-03-2012 19:39:38
-// Items: 3
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/article_accueil/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// A
+	// A
 	'article_accueil_description' => 'Ce plugin vous permet de d\'attribuer un article d\'accueil aux rubriques. Le champ id_article_accueil peut ensuite être utilisé par les squelettes.',
 	'article_accueil_nom' => 'Articles d\'accueil',
-	'article_accueil_slogan' => 'Attribuer un article d\'accueil aux rubriques',
+	'article_accueil_slogan' => 'Attribuer un article d\'accueil aux rubriques'
 );
+
 ?>
