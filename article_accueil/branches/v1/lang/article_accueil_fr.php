9ee4eb90b587cedf46512da7d4c6921a66f1cc07
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-03-10 18:34:39 +0000

    La branche de la version 1
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@59206 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ article_accueil/branches/v1/lang/article_accueil_fr.php
@@ -0,0 +1,17 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	// A
+	'article_accueil' => 'Article d\'accueil',
+	'aucun_article_accueil' => 'Aucun article',
+	
+	// L
+	'label_id_article_accueil' => 'Article d\'accueil',
+	
+	// R
+	'rubrique_article_en_accueil' => 'Article d\'accueil&nbsp;:',
+);
+?>
