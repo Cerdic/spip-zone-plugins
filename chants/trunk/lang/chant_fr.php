9d443e6dbabe26bdec484e005458b0ed9d73a653
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-03-10 09:03:04 +0000

    uniformisation du nom du fichier xml par rapport au zip  généré du répertoire. - remplissage du squelette par défaut si Z n'est pas activé - mise à jour de la todolist Il faudra peut-être revoir le zip, Apache n'aime pas les apostrophes dans les noms de fichier en local/, faudra trouver une parade pour être à 100% dans la lignée d'OpenSong
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@59180 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- chants/trunk/lang/chant_fr.php
+++ chants/trunk/lang/chant_fr.php
@@ -1,52 +1,55 @@
 <?php
 if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-    // C
-    'chant' => 'Chant :',
-    'chants' => 'Chants :',
-    // D
-    'date_annee' => 'Année de sortie',
-    
-    // E
-    'exporter' => 'Exporter le chant',
-    'exporter_opensong' => 'Exporter le chant au format OpenSong',
-    
-    // I
-    'icone_creer_chant' => "Ajouter un chant",
-    'icone_modifier_chant' => "Modifier ce chant",
-    'icone_retour_chant' => 'Retour au chant',
-    'info_aucun_chant' => "Aucun chant",
-    'info_1_chant' => "Un chant",
-    'info_nb_chants' => "@nb@ chants",
-    'info_nombre_hymne' => 'Numéro',
-    'info_numero_chant' => 'CHANT NUMÉRO :',
-    'infos_complementaires' => 'Informations complémentaires',
-    
-    // L
-    'label_titre' => 'Titre :',
-    'label_copyright' => 'Copyright :',
-    'label_date_annee' => 'Année de sortie :',
-    'label_numero' => 'Numéro :',
-    'label_presentation' => 'Ordre des sections :',
-    'label_ccli' => 'N° CCLI :',
-    'label_capo' => 'Capo (de 1 à 5) :',
-    'label_tempo' => 'Tempo :',
-    'label_signature' => 'Mesure :',
-    'label_paroles' => 'Paroles du chant :',
-    'label_tonalite' => 'Tonalité',
-    'logo_chant' => 'Logo du chant',
-    
-    // T
-    'text_chant_propose_publication' => 'Chant proposé pour la publication.',
-    'texte_changer_statut' => 'Ce chant est :',
-    'texte_copyright' => 'Copyright',
-    'texte_date_annee' => 'Année de sortie',
-    'texte_travail_chant' => '@nom_auteur_modif@ a travaillé sur ce chant il y a @date_diff@ minutes',
-    'titre_chant' => 'Chant',
-    'titre_chants' => 'Chants',
-    'titre_chants_rubrique' => 'Chants de la rubrique',
-    'titre_langue_chant' => 'Langue du chant',
-    'titre_logo_chant' => ' Logo du chant',
+	// A
+	'aucun_chant' => 'Il n\'y a pas de chant à cette adresse',
+
+	// C
+	'chant' => 'Chant :',
+	'chants' => 'Chants :',
+	// D
+	'date_annee' => 'Année de sortie',
+	
+	// E
+	'exporter' => 'Exporter le chant',
+	'exporter_opensong' => 'Exporter le chant au format OpenSong',
+	
+	// I
+	'icone_creer_chant' => "Ajouter un chant",
+	'icone_modifier_chant' => "Modifier ce chant",
+	'icone_retour_chant' => 'Retour au chant',
+	'info_aucun_chant' => "Aucun chant",
+	'info_1_chant' => "Un chant",
+	'info_nb_chants' => "@nb@ chants",
+	'info_nombre_hymne' => 'Numéro',
+	'info_numero_chant' => 'CHANT NUMÉRO :',
+	'infos_complementaires' => 'Informations complémentaires',
+	
+	// L
+	'label_titre' => 'Titre :',
+	'label_copyright' => 'Copyright :',
+	'label_date_annee' => 'Année de sortie :',
+	'label_numero' => 'Numéro :',
+	'label_presentation' => 'Ordre des sections :',
+	'label_ccli' => 'N° CCLI :',
+	'label_capo' => 'Capo (de 1 à 5) :',
+	'label_tempo' => 'Tempo :',
+	'label_signature' => 'Mesure :',
+	'label_paroles' => 'Paroles du chant :',
+	'label_tonalite' => 'Tonalité',
+	'logo_chant' => 'Logo du chant',
+	
+	// T
+	'text_chant_propose_publication' => 'Chant proposé pour la publication.',
+	'texte_changer_statut' => 'Ce chant est :',
+	'texte_copyright' => 'Copyright',
+	'texte_date_annee' => 'Année de sortie',
+	'texte_travail_chant' => '@nom_auteur_modif@ a travaillé sur ce chant il y a @date_diff@ minutes',
+	'titre_chant' => 'Chant',
+	'titre_chants' => 'Chants',
+	'titre_chants_rubrique' => 'Chants de la rubrique',
+	'titre_langue_chant' => 'Langue du chant',
+	'titre_logo_chant' => ' Logo du chant',
 )
 
 ?>
