b34c4fdc216417e45e887d30c5d39dbed5beb507
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-03-10 21:58:41 +0000

    langues (article_accueil)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@59213 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- article_accueil/trunk/lang/article_accueil_es.php
+++ article_accueil/trunk/lang/article_accueil_es.php
@@ -1,17 +1,21 @@
 <?php
-
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-if (!defined("_ECRIRE_INC_VERSION")) return;
+// extrait automatiquement de http://trad.spip.org/tradlang_module/article_accueil?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+
 	// A
 	'article_accueil' => 'Artículo destacado',
 	'aucun_article_accueil' => 'Ningún artículo',
-	
+
 	// L
 	'label_id_article_accueil' => 'Artículo destacado',
-	
+
 	// R
-	'rubrique_article_en_accueil' => 'Artículo destacado actual:',
+	'rubrique_article_en_accueil' => 'Artículo destacado actual:'
 );
+
 ?>
--- article_accueil/trunk/lang/article_accueil_fr.php
+++ article_accueil/trunk/lang/article_accueil_fr.php
@@ -1,17 +1,19 @@
 <?php
-
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-if (!defined("_ECRIRE_INC_VERSION")) return;
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/article_accueil/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+
 	// A
 	'article_accueil' => 'Article d\'accueil',
 	'aucun_article_accueil' => 'Aucun article',
-	
+
 	// L
 	'label_id_article_accueil' => 'Article d\'accueil',
-	
+
 	// R
-	'rubrique_article_en_accueil' => 'Article d\'accueil&nbsp;:',
+	'rubrique_article_en_accueil' => 'Article d\'accueil :'
 );
+
 ?>
