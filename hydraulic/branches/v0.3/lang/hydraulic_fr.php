f8f5c90ac2671662d9ccd2b7a75326106a9b1891
m.dulondel@laposte.net <m.dulondel@laposte.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-03-12 08:54:59 +0000

    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@59231 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- hydraulic/branches/v0.3/lang/hydraulic_fr.php
+++ hydraulic/branches/v0.3/lang/hydraulic_fr.php
@@ -4,6 +4,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
     // A
     'abscisse'        => "Abscisse",
     'arret_calcul'    => "Arrêt du calcul : Hauteur critique atteinte à l'abscisse",
+    'acier_lamine'    => "Acier laminé - Béton lisse",
     // C
     'calcul_caract'   => "Le calcul a les caractéristiques suivantes :",
     'calcul_remous'   => "Calcul d'une courbe de remous",
@@ -13,13 +14,24 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
     'condition_limite'=> "Conditions aux limites",
     'calcul_fluvial'  => "Condition limite aval >= Hauteur critique : calcul de la partie fluviale à partir de l'aval",
     'calcul_torrentiel'=> "Condition limite amont <= Hauteur critique : calcul de la partie torrentielle à partir de l'amont",
-    'champ_obligatoire'=> 'Ce champ est obligatoire',
+    'champ_obligatoire'=> "Ce champ est obligatoire",
+    'choix_materiau'  => "Choix du matériau",
+    'caract_lechapt_calmon' => "Caractéristiques",
+    'calculer_lechapt_calmon' => "Calcul de Lechapt et Calmon",
+    'calcul_val'      => "Calculer valeur",
+    'choix_section'   => "Choix de la section",
     // D
     'debit_amont'     => "Débit amont (m3/s)",
     'def_section_trap'=> "Définition de la section trapézoïdale",
     // F
     'fond'            => "Fond",
     'fruit'           => "Fruit (m/m)",
+    'fixer_val'       => "Fixer valeur",
+    'fonte_beton_corrosive' =>"Fonte ou acier non revêtus - Béton grossier (eau corrosive)",
+    'fonte_beton_ncorrosive' =>"Fonte ou acier non revêtus - Béton grossier (eau peu corrosive)",
+    'fonte_revt_ciment' => "Fonte ou acier revêtement ciment",
+    'fonte_revt_bitume' => "Fonte ou acier revêtement bitume - Béton centrifugé",
+    'fonte_revt_centrifuge' => "Fonte ou acier revêtement centrifugé",
     // H
     'h_amont_imposee' => "Hauteur d'eau imposée à l'amont (m)",
     'h_aval_imposee'  => "Hauteur d'eau aval imposée (m)",
@@ -38,14 +50,34 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
     'pas_nombre'      => "Nombre de pas d'espace",
     'pente_fond'      => "Pente du fond (m/m)",
     'precision_calc'  => "Précision de calcul et d'affichage des cotes (m)",
-    'pas_calcul_depuis_aval' => 'Condition limite aval < Hauteur critique : pas de calcul possible depuis l\'aval',
-    'pas_calcul_depuis_amont' => 'Condition limite amont > Hauteur critique : pas de calcul possible depuis l\'amont',
+    'pas_calcul_depuis_aval' => "Condition limite aval < Hauteur critique : pas de calcul possible depuis l'aval",
+    'pas_calcul_depuis_amont' => "Condition limite amont > Hauteur critique : pas de calcul possible depuis l'amont",
+    'pas_var'         => "Pas de variation de",
+    'pvc'             => "PVC - Polyéthylène",
+    'param_Q'         => "Q: Débit (m³/s)",
+    'param_D'         => "D: Diamètre du tuyau (m)",
+    'param_J'         => "J: Perte de charge (m)",
+    'param_Lg'        => "Lg: Longueur du tuyau (m)",
+    'param_calc_lechapt_calmon' => "Paramètres de calcul",
+    'precision_lechapt_calmon'  => "Précision de calcul(m)",
     //S
-    'saisie_erreur'   => 'Votre saisie contient des erreurs !',
+    'saisie_erreur'   => "Votre saisie contient des erreurs !",
+    'section_trap'    => "Section Trapézoïdale",
+    'section_rect'    => "Section Rectangulaire",
+    'section_circ'    => "Section Circulaire",
+    'section_puiss'   => "Section Puissance",
+
     //T
-    'tirant_eau'      => 'Tirant d\'eau (m)',
+    'tirant_eau'      => "Tirant d'eau (m)",
+    'type_materiau'   => "Type du matériau",
+    'tuyau_lisse1'    => "Tuyau hydrauliquement lisse - 0.05 ≤ D ≤ 0.2",
+    'tuyau_lisse2'    => "Tuyau hydrauliquement lisse - 0.25 ≤ D ≤ 1",
+    'type_section'    => "Type de section",
     //V
-    'valeur_positive' => 'Cette valeur doit être strictement positive'
+    'valeur_positive' => "Cette valeur doit être strictement positive",
+    'val_min'         => "Valeur minimum de",
+    'val_max'         => "Valeur maximum de",
+    'varier_val'      => "Varier valeur"
 );
 
 ?>
