e960b89d4a2653526e1138f585298428e16e21e3
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-03-13 14:54:01 +0000

    la branche
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@59261 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ squirrels_love_chimps_lists_plus/branches/0.1/lang/sclp_fr.php
@@ -0,0 +1,68 @@
+
+
+<?php
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	
+	//A
+	'abonnement_inscrit' => 'Inscrits aux listes d\'envois',
+	'activer_liste' => 'Activer cette liste',
+	'aucun_abonnement' => 'Pas d\'abonnments',
+	
+	//C
+	'choix_mailinglists' => 'Sélectionnez les liste MailChimp à synchroniser avec les listes Spip',
+	'confirmer_delete' => 'Vous êtes en train d\'éliminer définitivement cette liste, voulez-vous vraiment continuer ?',
+	'creer_liste' => 'Créer la liste',
+	'creer_listes' => 'Création de listes ',
+	'creer_liste_depuis_mc' => 'Créer une liste à partir d\'une liste MailChimp',
+	'creer_nouvelle_liste' => 'Créer une nouvelle liste Spip',
+	
+	//D
+	'desactiver_liste' => 'Mettre cette liste à la poubelle',
+	
+	//E
+	'eliminer_liste' => 'Éliminer définitivement cette liste',
+	
+	//F
+	'fermer' => 'Fermer',
+	
+	//G
+	'gerer_abonnements_listes' => 'Gerer les listes d\'envois',
+	'gestion_listes' => 'Gestion listes',
+	
+	//L	
+	'lang' => 'Langue',
+	'liste' => 'Liste',
+	'liste_cree' => 'La liste à été créée',
+	'liste_modifie' => 'La liste à été modifiée',
+	'liste_non_syncroniser' => 'Ne pas synchroniser',
+	'listes_activees' => 'Les listes actives',
+	'listes_prive' => 'Les listes privées',
+	'listes_public' => 'Les listes publiques',
+	
+	
+	//M
+	'modifier_abonnments' => 'Modifier les abonnements',
+	
+	//N
+	'newsletters_dispos' => 'Les abonnements disponibles',
+	
+	//P
+	'poubelle' => 'Poubelle',
+	
+	//R
+	'retour' => 'Retour',
+	
+	//S
+	'syncroniser_liste' => 'Syncroniser avec la liste',
+	'statut_poubelle' => 'Poubelle',	
+	'statut_prive' => 'Privé',	
+	'statut_public' => 'Public',	
+	
+	//T
+	'titre' => 'Titre',
+
+	);
+
+?>
