e6fd758dc776285e9275754474dd626e1a9371ea
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-03-13 15:34:54 +0000

    Adaptations pour sclp
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@59263 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- squirrels_love_chimps/trunk/squirrel_chimp_options.php
+++ squirrels_love_chimps/trunk/squirrel_chimp_options.php
@@ -2,6 +2,6 @@
 	// Declaration de la pipeline pour l'api de configuration
      $GLOBALS['spip_pipeline']['squirrel_chimp_definitions'];
 
-	// Declaration de la pipeline pour declarer d'avantages de pages pour l'insertion de du menu raccourcis modeles
+	// Declaration de la pipeline pour declarer d'avantages de pages pour l'insertion  du menu raccourcis modeles
      $GLOBALS['spip_pipeline']['squirrel_chimp_pages_raccourcis_models'];
 ?>
