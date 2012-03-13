e91d1a0476b723adc06bb12c4949c57a5ac45e5f
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-03-13 16:05:35 +0000

    salvatore
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@59265 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- traduction_articles_autrement/branches/1.0/lang/taa_fa.php
+++ traduction_articles_autrement/branches/1.0/lang/taa_fa.php
@@ -1,11 +1,14 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// extrait automatiquement de http://www.spip.net/trad-lang/
+// extrait automatiquement de http://trad.spip.org/tradlang_module/taa?lang_cible=fa
 // ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'affichage_liste_compacte_desactive' => 'غيرفعال‌سازي نمايش فشرده‌ي فهرست مقاله‌ها را ',
 
 	// C
 	'changer_langue' => 'تغيير زبان مقاله',
