6f4456afc7ede01f9f9485c0d3c71dd13ac53874
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-03-13 16:08:13 +0000

    salvatore
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@59266 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- traduction_rubriques_autrement/branches/0.1/lang/tra_ast.php
+++ traduction_rubriques_autrement/branches/0.1/lang/tra_ast.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// extrait automatiquement de http://www.spip.net/trad-lang/
+// extrait automatiquement de http://trad.spip.org/tradlang_module/tra?lang_cible=ast
 // ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
