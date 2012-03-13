8f8c8cf096e0e97490a777d73a570edde40d0621
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-03-13 14:44:18 +0000

    Evite un le message d'erreur quand aucune liste n'est choisie Installer proprement Adaptation de la version
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@59259 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- squirrels_love_chimps_lists_plus/trunk/base/installation.php
+++ squirrels_love_chimps_lists_plus/trunk/base/installation.php
@@ -1,65 +1,28 @@
 <?php
 
 if (!defined("_ECRIRE_INC_VERSION")) return;
-include_spip('base/create');
-/*
-function sclp_upgrade($nom_meta_base_version,$version_cible){
-        $current_version = 0.0;
-        if ((!isset($GLOBALS['meta'][$nom_meta_base_version]))
-        || (($current_version = $GLOBALS['meta'][$nom_meta_base_version])!=$version_cible)){
-			
-                include_spip('base/sclp');
-                // cas d'une installation
-                if ($version_cible > $GLOBALS['meta'][$nom_meta_base_version]){
-                	if($GLOBALS['meta'][$nom_meta_base_version]==''){
-						
-				include_spip('base/create');
-				creer_base();
-				maj_tables('spip_listes');	
-				maj_tables('spip_auteurs_listes');									
-				maj_tables('spip_auteurs');	 
-				maj_tables('spip_listes_syncro');	 
-				sql_updateq('spip_listes',array('statut'=>'active'),"statut=''");	
-				sql_updateq('spip_listes',array('statut'=>'desactive'),"statut='inact'");	
-										
-				}
-			else{
-				include_spip('base/create');
-				creer_base();
-				maj_tables('spip_listes');	
-				maj_tables('spip_auteurs_listes');									
-				maj_tables('spip_auteurs');	
-				maj_tables('spip_listes_syncro');	 				
-				}
-               }
-            ecrire_meta($nom_meta_base_version, $current_version=$version_cible);    
-        }
-	    */
+
+
 	    
 function sclp_upgrade($nom_meta_base_version, $version_cible){
-	
-        $current_version = 0.0;
-        if ((!isset($GLOBALS['meta'][$nom_meta_base_version]))
-        || (($current_version = $GLOBALS['meta'][$nom_meta_base_version])!=$version_cible)){
-                creer_base();
-                maj_tables('spip_auteurs');	 
-                maj_tables('spip_listes_syncro');	 
-                maj_tables('spip_listes');	 
-                maj_tables('spip_auteurs_listes');	              
-                sql_updateq('spip_listes',array('statut'=>'prive'),"statut=''");	
-				sql_updateq('spip_listes',array('statut'=>'poubelle'),"statut='inact'");
-			
-				ecrire_meta($nom_meta_base_version, $current_version=$version_cible);	
-        }
-        else{
-                // ajout du champ "robe" et "infos"
-               //creer_base();
-                maj_tables('spip_auteurs');	 
-                maj_tables('spip_listes_syncro');	 
-                maj_tables('spip_listes');	 
-                maj_tables('spip_auteurs_listes');	 
-                
-                ecrire_meta($nom_meta_base_version,$current_version=$version_cible);
-        }
+	$current_version = 0.0;
+	if ((!isset($GLOBALS['meta'][$nom_meta_base_version]))
+	|| (($current_version = $GLOBALS['meta'][$nom_meta_base_version])!=$version_cible)){
+		if ($current_version==0.0){
+            include_spip('base/create');
+            creer_base();
+            maj_tables('spip_auteurs');	 
+            maj_tables('spip_listes_syncro');	 
+            maj_tables('spip_listes');	 
+            maj_tables('spip_auteurs_listes');	              
+            sql_updateq('spip_listes',array('statut'=>'prive'),"statut=''");	
+			sql_updateq('spip_listes',array('statut'=>'poubelle'),"statut='inact'");
+			ecrire_meta($nom_meta_base_version, $current_version=$version_cible);	
+            }
+		
+		
+
+	}
+
 }	    
 ?>
