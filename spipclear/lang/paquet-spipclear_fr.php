717eb3b43b1cc6876688d3cf16f5b5b9e7e84b5d
ben.spip@gmail.com <ben.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-03-14 12:07:36 +0000

    SPIPClear est donc compatible SPIP3, si on l'installe sur un spip2 c'est le plugin.xml qui est pris en compte, si on l'installe sur spip3 c'est le paquet.xml qui compte
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@59314 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ spipclear/lang/paquet-spipclear_fr.php
@@ -0,0 +1,19 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-spipclear
+// Langue: fr
+// Date: 14-03-2012 13:01:24
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// S
+	'spipclear_description' => 'SpipClear est conçu pour transformer un secteur en blog ou carnet web. Pour cela, il s\'inspire de DotClear, un système de gestion de blog développé en php par Neokraft.net. Il est nécessaire de créer, a minima, les pages sommaire, article-x et rubrique-x pour activer le blog sur le secteur choisi.',
+	'spipclear_slogan' => 'Un site à la dotclear en Spip',
+);
+?>
