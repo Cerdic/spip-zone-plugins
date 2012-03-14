893ba2e643681e2851d468e2ffca1aa3fe253eb0
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-03-14 14:21:03 +0000

    langues (paquet-a2a)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@59324 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- a2a/trunk/lang/paquet-a2a_en.php
+++ a2a/trunk/lang/paquet-a2a_en.php
@@ -1,18 +1,15 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-a2a
-// Langue: en
-// Date: 14-03-2012 13:41:38
-// Items: 1
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org/tradlang_module/paquet-a2a?lang_cible=en
+// ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// A
+	// A
 	'a2a_description' => 'This plugin allows you to link items in a timely manner without having to use keywords of SPIP. It can be used to set up links such as "See also".',
+	'a2a_slogan' => 'Pour lier des articles' # NEW
 );
+
 ?>
--- a2a/trunk/lang/paquet-a2a_fr.php
+++ a2a/trunk/lang/paquet-a2a_fr.php
@@ -1,19 +1,13 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-a2a
-// Langue: fr
-// Date: 14-03-2012 13:41:38
-// Items: 2
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/a2a/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// A
+	// A
 	'a2a_description' => 'Ce plugin permet de lier des articles de façon ponctuelle sans avoir à utiliser les mots-clés de SPIP. Il peut être utilisé pour mettre en place des liens de type « Lire aussi ».',
-	'a2a_slogan' => 'Pour lier des articles',
+	'a2a_slogan' => 'Pour lier des articles'
 );
+
 ?>
