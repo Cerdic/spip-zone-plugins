07af8313817056a77f914c36e485b339189b87ee
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-03-14 14:21:05 +0000

    langues (paquet-abomailmans)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@59326 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- abomailmans/trunk/lang/paquet-abomailmans_de.php
+++ abomailmans/trunk/lang/paquet-abomailmans_de.php
@@ -1,20 +1,17 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-abomailmans
-// Langue: de
-// Date: 19-02-2012 14:39:06
-// Items: 2
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org/tradlang_module/paquet-abomailmans?lang_cible=de
+// ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// A
+	// A
 	'abomailmans_description' => 'Dieses Plugin ermöglich mit dem TAG #FORMULAIRE_ABOMAILMAN das Abnonnieren von Mailinglisten.
 			Dem Adminstrator steht ein Interface zur Verwaltung dieser Listen zur Verfügung.',
 	'abomailmans_nom' => 'Abonnieren Sie Mailing-Listen',
+	'abomailmans_slogan' => 'Interfacer vos listes de diffusions mailman, ezmlm ou sympa avec votre SPIP' # NEW
 );
+
 ?>
--- abomailmans/trunk/lang/paquet-abomailmans_en.php
+++ abomailmans/trunk/lang/paquet-abomailmans_en.php
@@ -1,19 +1,16 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-abomailmans
-// Langue: en
-// Date: 19-02-2012 14:39:06
-// Items: 2
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org/tradlang_module/paquet-abomailmans?lang_cible=en
+// ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// A
+	// A
 	'abomailmans_description' => 'This plugin adds a form of registration to mailing lists services as [Mailman->http://www.gnu.org/software/mailman/], [Ezmlm->http://www.ezmlm.org/] or [Sympa->http://www.sympa.org] the administrator can manage from the private space via a dedicated interface.',
 	'abomailmans_nom' => 'Mailing lists subscription',
+	'abomailmans_slogan' => 'Interfacer vos listes de diffusions mailman, ezmlm ou sympa avec votre SPIP' # NEW
 );
+
 ?>
