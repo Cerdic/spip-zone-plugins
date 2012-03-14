5978505a8432fa4283f0cc918930675ded7be1ca
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-03-14 14:21:02 +0000

    langues (a2a)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@59323 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- a2a/trunk/lang/a2a_en.php
+++ a2a/trunk/lang/a2a_en.php
@@ -1,39 +1,44 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-if (!defined("_ECRIRE_INC_VERSION")) return;
+// extrait automatiquement de http://trad.spip.org/tradlang_module/a2a?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
+	'appliquer_type_liaison' => 'Type de liaison :', # NEW
+	'appliquer_type_liaison_explication' => 'Décrivez le type de lien à appliquer lors de la liaison. Utilisez de préférence un code ou un mot court.', # NEW
 	'articles_lies' => 'Link to an article',
-	
+
 	// E
 	'explication_navigateur' => 'Click on [Add] to navigate through the site and select the article(s) to link.',
-	
+
 	// L
 	'lier_cet_article' => 'Link',
 	'lier_cet_article_deux_cotes' => 'Link both sides',
-	
+
 	// P
 	'pas_articles_lies' => 'No linked articles',
 	'pas_de_resultat' => 'No article found',
-	
+
 	// R
 	'rang_moins' => 'Up',
 	'rang_plus' => 'Down',
 	'recherche_arbo' => 'Tree navigation',
 	'recherche_texte' => 'Textual search',
 	'rechercher' => 'Search',
-	'rechercher_un_article_a_lier' => 'Search for an article:',
 	'rechercher_seulement_titres' => 'Search only in titles',
 	'rechercher_tip_articles_ids' => 'To find article number 123 directly, type \'art123\'.',
-	
+	'rechercher_un_article_a_lier' => 'Search for an article:',
+
 	// S
 	'supprimer_le_lien' => 'Unlink',
 	'supprimer_le_lien_deux_cotes' => 'Unlink both sides',
-	
+
 	// V
-	'voir' => 'See',
+	'voir' => 'See'
 );
 
 ?>
--- a2a/trunk/lang/a2a_fr.php
+++ a2a/trunk/lang/a2a_fr.php
@@ -1,41 +1,42 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-if (!defined("_ECRIRE_INC_VERSION")) return;
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/a2a/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
-	'appliquer_type_liaison' => 'Type de liaison&nbsp;:',
+	'appliquer_type_liaison' => 'Type de liaison :',
 	'appliquer_type_liaison_explication' => 'Décrivez le type de lien à appliquer lors de la liaison. Utilisez de préférence un code ou un mot court.',
-	'articles_lies' => 'ARTICLES LI&Eacute;S',
-	
+	'articles_lies' => 'ARTICLES LIÉS',
+
 	// E
-	'explication_navigateur' => 'Cliquez sur [Ajouter] pour naviguer dans l\'arborescence du site afin de s&eacute;lectionner les articles &agrave; lier.',
-	
+	'explication_navigateur' => 'Cliquez sur [Ajouter] pour naviguer dans l\'arborescence du site afin de sélectionner les articles à lier.',
+
 	// L
 	'lier_cet_article' => 'Lier',
-	'lier_cet_article_deux_cotes' => 'Lier des deux cot&eacute;s',
-	
+	'lier_cet_article_deux_cotes' => 'Lier des deux cotés',
+
 	// P
-	'pas_articles_lies' => 'Pas d\'articles li&eacute;s',
-	'pas_de_resultat' => 'Pas de r&eacute;sultats',
-	
+	'pas_articles_lies' => 'Pas d\'articles liés',
+	'pas_de_resultat' => 'Pas de résultats',
+
 	// R
 	'rang_moins' => 'Monter',
 	'rang_plus' => 'Descendre',
 	'recherche_arbo' => 'Recherche arborescente',
 	'recherche_texte' => 'Recherche textuelle',
 	'rechercher' => 'Rechercher',
-	'rechercher_un_article_a_lier' => 'Rechercher un article&nbsp;:',
 	'rechercher_seulement_titres' => 'Rechercher seulement dans les titres',
-	'rechercher_tip_articles_ids' => 'Pour afficher directement l\'article num&eacute;ro 123, entrer \'art123\'.',
-	
+	'rechercher_tip_articles_ids' => 'Pour afficher directement l\'article numéro 123, entrer \'art123\'.',
+	'rechercher_un_article_a_lier' => 'Rechercher un article :',
+
 	// S
-	'supprimer_le_lien' => 'D&eacute;lier',
-	'supprimer_le_lien_deux_cotes' => 'D&eacute;lier des deux cot&eacute;s',
-	
+	'supprimer_le_lien' => 'Délier',
+	'supprimer_le_lien_deux_cotes' => 'Délier des deux cotés',
+
 	// V
-	'voir' => 'Voir',
+	'voir' => 'Voir'
 );
 
 ?>
