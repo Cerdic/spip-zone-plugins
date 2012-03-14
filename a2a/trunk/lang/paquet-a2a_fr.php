2d7cbb1bd3adea8949659508c828eee8de5b5adb
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-03-14 13:26:23 +0000

    Une version qui semble fonctionnelle en SPIP 3.0
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@59320 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ a2a/trunk/lang/paquet-a2a_fr.php
@@ -0,0 +1,19 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-a2a
+// Langue: fr
+// Date: 14-03-2012 13:41:38
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// A
+	'a2a_description' => 'Ce plugin permet de lier des articles de façon ponctuelle sans avoir à utiliser les mots-clés de SPIP. Il peut être utilisé pour mettre en place des liens de type « Lire aussi ».',
+	'a2a_slogan' => 'Pour lier des articles',
+);
+?>
