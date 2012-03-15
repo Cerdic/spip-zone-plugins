04cfaba78c9ae8036e3bfde9b8fc09eafae25669
alesk@spip.org <alesk@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-03-15 18:23:52 +0000

    Intégration SPIP de de l'implémentation PHP, SASS.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@59352 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ sassphp/generer.php
@@ -0,0 +1,34 @@
+﻿<?php
+
+require 'SassParser.php';
+
+// créé un tableau $styles à partir de la recherche
+// de tous les fichiers *.sass & *.scss dans le "path" spip
+foreach (find_all_in_path("", ".[.]s[ac]ss") as $styles) 
+  {
+    // chaque fichier sass produit un fichier css de la forme :
+    // "cheminVersFichierSass/nomFichier.css"
+	$styles = str_replace(".scss", "", $styles);
+	$styles = str_replace(".sass", "", $styles);
+    $styles_css = dirname($styles).'/'.basename($styles).'.css';
+
+    // le compilateur sass compile chaque fichiers less et produit
+    // la feuille de style correspondante dans le même répertoire.
+	$options = array(
+		'style' => 'nested',
+		'cache' => FALSE,
+		'syntax' => $syntax,
+		'debug' => FALSE,
+		'callbacks' => array(
+		'warn' => 'cb_warn',
+		'debug' => 'cb_debug',
+		),
+	);
+  
+	$parser = new SassParser($options);
+	$contenu = $parser->toCss($styles);
+	ecrire_fichier($styles_css, $contenu);
+    
+  }
+
+?>
