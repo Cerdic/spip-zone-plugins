cd96e94a01f835f4c6ac863aeefa34b518539c4c
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-03-15 11:33:22 +0000

    langues (paquet-licence)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@59337 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- licence/lang/paquet-licence_en.php
+++ licence/lang/paquet-licence_en.php
@@ -1,20 +1,16 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-licence
-// Langue: en
-// Date: 15-03-2012 10:18:38
-// Items: 3
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org/tradlang_module/paquet-licence?lang_cible=en
+// ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// L
+	// L
 	'licence_description' => 'Link a license on articles and documents',
 	'licence_nom' => 'License',
-	'licence_slogan' => 'A license for articles and documents',
+	'licence_slogan' => 'A license for articles and documents'
 );
+
 ?>
--- licence/lang/paquet-licence_fr.php
+++ licence/lang/paquet-licence_fr.php
@@ -1,20 +1,14 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-licence
-// Langue: fr
-// Date: 15-03-2012 10:18:38
-// Items: 3
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/licence/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// L
+	// L
 	'licence_description' => 'Permet de relier une licence d\'utilisation à un article ou un document',
 	'licence_nom' => 'Licence',
-	'licence_slogan' => 'Une Licence pour articles et documents',
+	'licence_slogan' => 'Une Licence pour articles et documents'
 );
+
 ?>
