8b0f61cea16791a5839f455b8a54dd541f565c1c
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-03-16 15:18:03 +0000

    langues (paquet-iextras)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@59446 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- champs_extras/interface/trunk/lang/paquet-iextras_en.php
+++ champs_extras/interface/trunk/lang/paquet-iextras_en.php
@@ -1,8 +1,16 @@
 <?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org/tradlang_module/paquet-iextras?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-	'iextras_titre' => 'Extra fields (Interface)',
-	'iextras_slogan' => 'Interface to manage the extra fields on SPIP objects',
+	// I
 	'iextras_description' => '',
+	'iextras_slogan' => 'Interface to manage the extra fields on SPIP objects',
+	'iextras_titre' => 'Extra fields (Interface)'
 );
+
 ?>
--- champs_extras/interface/trunk/lang/paquet-iextras_fr.php
+++ champs_extras/interface/trunk/lang/paquet-iextras_fr.php
@@ -1,9 +1,15 @@
 <?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/champs_extras/interface/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-	'iextras_titre' => 'Champs Extras (Interface)',
-	'iextras_slogan' => 'Offre une interface graphique pour gérer des champs extras',
+	// I
 	'iextras_description' => 'Ajoute dans l\'espace privé une interface complète de gestion de champs supplémentaires
 							dans les objets éditoriaux.',
+	'iextras_slogan' => 'Offre une interface graphique pour gérer des champs extras',
+	'iextras_titre' => 'Champs Extras (Interface)'
 );
+
 ?>
