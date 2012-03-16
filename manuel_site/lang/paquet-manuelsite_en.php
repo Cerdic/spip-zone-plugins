f188449a94c808766b44e15137527fcc17a42138
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-03-16 15:18:08 +0000

    langues (paquet-manuelsite)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@59449 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- manuel_site/lang/paquet-manuelsite_en.php
+++ manuel_site/lang/paquet-manuelsite_en.php
@@ -1,20 +1,16 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-manuelsite
-// Langue: en
-// Date: 15-03-2012 12:35:03
-// Items: 3
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org/tradlang_module/paquet-manuelsite?lang_cible=en
+// ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// M
+	// M
 	'manuelsite_description' => 'This plugin displays an help icon on every page of the private space showing the Website Editor Manual. This manual is an article of the website. The plugin also provides a set of Frequently Asked Questions that can be easily inserted in your manual.',
 	'manuelsite_nom' => 'Website Editor Manual',
-	'manuelsite_slogan' => 'A manual specific to your site for your editors',
+	'manuelsite_slogan' => 'A manual specific to your site for your editors'
 );
+
 ?>
