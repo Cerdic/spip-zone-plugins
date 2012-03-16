b1f2649841fa673fc7e7946d3c97d88118bc98a3
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-03-16 10:33:49 +0000

    Petites corrections en langue anglaise
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@59386 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- manuel_site/lang/manuelsite_en.php
+++ manuel_site/lang/manuelsite_en.php
@@ -4,22 +4,22 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// C
-	'configurer_explication' => 'This plugin installs an help icon making it possible to post since any page of private space the Website Editor Manual. This manual is @texte@ The purpose of it is to explain to the editors the architecture of the site, in which heading to arrange what, how code and install a video… So, all that you want and which is specific to your website.',
-	'configurer_explication_l_article' => '<a href="?exec=articles&amp;id_article=@idart@" title="Manuel de redaction">l’article @idart@</a> de votre site.',
+	'configurer_explication' => 'This plugin displays an help icon on every page of the private space showing the Website Editor Manual. This manual is @texte@ Its purpose is to explain the architecture of the site to the editors, in which section to store what, how to encode and install a video… So, all that you want and which is specific to your website.',
+	'configurer_explication_l_article' => '<a href="?exec=articles&amp;id_article=@idart@" title="Website Editor Manual">the article #@idart@</a> of your website.',
 	'configurer_explication_un_article' => 'an article of the website.',
 	'configurer_titre' => 'Configure the Website Editor Manual',
 
 	// E
 	'erreur_article' => 'The article of the manuel defined in the plugin\'s configuration is untraceable : @idart@',
 	'erreur_pas_darticle' => 'The article of the manuel is not defined in the plugin\'s configuration',
-	'explication_afficher_bord_gauche' => 'Display the manual\'s icon in top on the left (if not the manual will be displayes in column)',
+	'explication_afficher_bord_gauche' => 'Display the manual\'s icon in the top left corner (if not, the manual will be displayed in a column)',
 	'explication_background_color' => 'Type in the background color of the manual display area',
 	'explication_cacher_public' => 'Hide this article in the public space, even in rss flow',
 	'explication_email' => 'Contact email for editors',
 	'explication_faq' => 'Listed below are the generic blocks used to write your manual (the text is displayed, not formatted, when then mouse is over the code). Just copy / paste the code in the textarea of your article.<br />To hide the question, add <i>|q=no</i>.<br />To add parameters, add <i>|params=p1:v1;p2:v2</i>.',
-	'explication_id_article' => 'Type in the number of the article wich contain the manual',
+	'explication_id_article' => 'Type in the number of the article which contains the manual',
 	'explication_intro' => 'Introduction text of the manual (will be placed before the introduction of the article)',
-	'explication_largeur' => 'Type in the manual display area width',
+	'explication_largeur' => 'Type in the manual display area width (in px)',
 
 	// F
 	'fermer_le_manuel' => 'Close the manual',
--- manuel_site/lang/paquet-manuelsite_en.php
+++ manuel_site/lang/paquet-manuelsite_en.php
@@ -13,7 +13,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 // M
-	'manuelsite_description' => 'This plugin installs an help icon making it possible to post since any page of private space the Website Editor Manual. This manual is an article of the website. The plugin also provides a set of Frequently Asked Questions that can be easily inserted in your manual.',
+	'manuelsite_description' => 'This plugin displays an help icon on every page of the private space showing the Website Editor Manual. This manual is an article of the website. The plugin also provides a set of Frequently Asked Questions that can be easily inserted in your manual.',
 	'manuelsite_nom' => 'Website Editor Manual',
 	'manuelsite_slogan' => 'A manual specific to your site for your editors',
 );
