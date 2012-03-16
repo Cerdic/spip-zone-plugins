5825eb3c1b470386de48430c6503c79f259f341d
davoodhossein@yahoo.com <davoodhossein@yahoo.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-03-16 15:17:56 +0000

    langues (paquet-ancresdouces)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@59439 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ ancres_douces/lang/paquet-ancresdouces_fa.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org/tradlang_module/paquet-ancresdouces?lang_cible=fa
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'ancresdouces_description' => 'اين پلاگين اجازه‌ي تداوم صفحه به صورت طوماري را وقتي يك بازديد‌كننده پيوند به يك مقاله‌ي خوانده شده را كلك كند مي‌دهد',
+	'ancresdouces_slogan' => 'لنگر‌‌هاي نرم'
+);
+
+?>
