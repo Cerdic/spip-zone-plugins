cb28731cf8dd39399977e1550bb0c5f94c8fa0e9
davoodhossein@yahoo.com <davoodhossein@yahoo.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-03-16 15:17:55 +0000

    langues (afficher_objets)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@59438 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ afficher_objets/lang/afficher_objets_fa.php
@@ -0,0 +1,20 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org/tradlang_module/afficher_objets?lang_cible=fa
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// I
+	'info_statut' => 'وضعيت',
+
+	// N
+	'nb_articles' => '@nb@ مقاله',
+
+	// R
+	'rang' => 'رتبه'
+);
+
+?>
