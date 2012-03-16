abfd923b2b6063f8b8e4274b84b9f3164a9fc6b6
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-03-16 15:18:06 +0000

    langues (paquet-licence)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@59447 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ licence/lang/paquet-licence_sk.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org/tradlang_module/paquet-licence?lang_cible=sk
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// L
+	'licence_description' => 'Umožňuje prepojiť licenciu na používanie s článkom alebo dokumentom',
+	'licence_nom' => 'Licencia',
+	'licence_slogan' => 'Licencia pre články a dokumenty'
+);
+
+?>
