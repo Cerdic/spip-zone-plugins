daa2088394a2cd6f8f41b6e3a0057f2fb1622053
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-03-16 15:18:13 +0000

    langues (saisies)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@59453 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- saisies/lang/saisies_ru.php
+++ saisies/lang/saisies_ru.php
@@ -8,10 +8,10 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// B
-	'bouton_parcourir_docs_article' => 'Parcourir l\'article', # NEW
-	'bouton_parcourir_docs_breve' => 'Parcourir la brève', # NEW
-	'bouton_parcourir_docs_rubrique' => 'Parcourir la rubrique', # NEW
-	'bouton_parcourir_mediatheque' => 'Parcourir la médiathèque', # NEW
+	'bouton_parcourir_docs_article' => 'Посмотреть статью',
+	'bouton_parcourir_docs_breve' => 'Посмотреть новость',
+	'bouton_parcourir_docs_rubrique' => 'Посмотреть раздел',
+	'bouton_parcourir_mediatheque' => 'Изменить библиотеку мультимедиа',
 
 	// C
 	'construire_action_annuler' => 'Отменить',
@@ -22,25 +22,25 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'construire_action_supprimer' => 'Удалить',
 	'construire_ajouter_champ' => 'Добавить поле',
 	'construire_attention_enregistrer' => 'Не забудьте сохранить внесенные изменения!',
-	'construire_attention_modifie' => 'Le formulaire ci-dessous est différent du formulaire initial. Vous avez la possibilité de le réinitialiser à son état avant vos modifications.', # NEW
-	'construire_attention_supprime' => 'Vos modifications comportent des suppressions de champs. Veuillez confirmer l\'enregistrement de cette nouvelle version du formulaire.', # NEW
+	'construire_attention_modifie' => 'Вы перешли на другую форму. у вас есть возможность вернуться назад нажав на кнопку ниже.',
+	'construire_attention_supprime' => 'Изменения включают удаление полей. Пожалуйста, подтвердите регистрацию новой формы.',
 	'construire_aucun_champs' => 'На данный момент поля в форме отсутствуют',
-	'construire_confirmer_supprimer_champ' => 'Вы действительно хотите удалить это поле?',
-	'construire_info_nb_champs_masques' => '@nb@ champ(s) masqué(s) le temps de configurer le groupe.', # NEW
-	'construire_position_explication' => 'Сначала укажите поле которое будет заменено этим.',
+	'construire_confirmer_supprimer_champ' => 'Вы действительно хотите удалить это поле??',
+	'construire_info_nb_champs_masques' => '@nb@ скрытых полей',
+	'construire_position_explication' => 'На какое поле заменить?',
 	'construire_position_fin_formulaire' => 'В конце формы',
-	'construire_position_fin_groupe' => 'À la fin du groupe @groupe@', # NEW
-	'construire_position_label' => 'Место положения поля',
-	'construire_reinitialiser' => 'Сбросить все данный в форме',
-	'construire_reinitialiser_confirmer' => 'Вы потеряете все изменения, которые внесли ранее! Вы действительно хотите возвратиться к начальной форме?',
-	'construire_verifications_aucune' => 'Aucune', # NEW
-	'construire_verifications_label' => 'Type de vérification à effectuer', # NEW
+	'construire_position_fin_groupe' => 'В конце группы @groupe@',
+	'construire_position_label' => 'Замена поля',
+	'construire_reinitialiser' => 'Вернуться назад',
+	'construire_reinitialiser_confirmer' => 'Вы действительно хотите вернуться назад?',
+	'construire_verifications_aucune' => 'Не проверять',
+	'construire_verifications_label' => 'Тип проверки',
 
 	// E
 	'erreur_option_nom_unique' => 'Такое название поля уже используется.',
 
 	// I
-	'info_configurer_saisies' => 'Page de test des Saisies', # NEW
+	'info_configurer_saisies' => 'Тестовая страница формы',
 
 	// L
 	'label_annee' => 'Год',
@@ -48,125 +48,125 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'label_mois' => 'Месяц',
 
 	// O
-	'option_aff_art_interface_explication' => 'Afficher uniquement les articles de la langue de l\'utilisateur', # NEW
-	'option_aff_art_interface_label' => 'Affichage multilingue', # NEW
+	'option_aff_art_interface_explication' => 'Отображать только статьи в настройках языка пользователя',
+	'option_aff_art_interface_label' => 'Многоязычное отображение',
 	'option_aff_langue_explication' => 'Показать выбранный язык статьи или раздела перед названием',
 	'option_aff_langue_label' => 'Показать текущий язык',
-	'option_aff_rub_interface_explication' => 'Afficher uniquement les rubriques de la langue de l\'utilisateur', # NEW
-	'option_aff_rub_interface_label' => 'Affichage multilingue', # NEW
-	'option_attention_explication' => 'Сообщение является более важным нежели объяснение.',
+	'option_aff_rub_interface_explication' => 'Отображать только разделы в языковых настройках пользователя.',
+	'option_aff_rub_interface_label' => 'Многоязычное отображение',
+	'option_attention_explication' => 'Сообщение, которое является более важным нежели комментарий.',
 	'option_attention_label' => 'Примечание!',
 	'option_autocomplete_defaut' => 'Оставить по умолчанию',
-	'option_autocomplete_explication' => 'Au chargement de la page, votre navigateur peut pré-remplir le champ en fonction de son historique', # NEW
+	'option_autocomplete_explication' => 'При загрузке страницы браузер может предварительно заполнить поля на основании истории',
 	'option_autocomplete_label' => 'Предварительное заполнение поля',
 	'option_autocomplete_off' => 'Отключить',
 	'option_autocomplete_on' => 'Включить',
 	'option_cacher_option_intro_label' => 'Не отображать первый пустой вариант',
-	'option_choix_destinataires_explication' => 'Un ou plusieurs auteurs parmis lesquels l\'utilisateur pourra faire son choix. Si rien n\'est sélectionné, c\'est l\'auteur qui a installé le site qui sera choisi.', # NEW
+	'option_choix_destinataires_explication' => 'Один или несколько авторов, среди которых пользователь может сделать свой ​​выбор. Если ничего не выбрано, то то будет выбран автор, который сейчас на сайте.',
 	'option_choix_destinataires_label' => 'Возможные получатели',
 	'option_class_label' => 'Дополнительные CSS классы',
-	'option_cols_explication' => 'Длина поля в символах. Этот вариант не всегда используется, потому что стили CSS могут переопределить его.',
+	'option_cols_explication' => 'Длина поля в символах.',
 	'option_cols_label' => 'Ширина',
-	'option_datas_explication' => 'Vous devez indiquez un choix par ligne sous la forme "cle|Label du choix"', # NEW
+	'option_datas_explication' => 'Вы должны указать выбор для каждой строки в виде "ключ | ярлык выбора"',
 	'option_datas_label' => 'Список возможных вариантов',
-	'option_defaut_label' => 'Значение по умолчанию.',
-	'option_disable_avec_post_explication' => 'Identique à l\'option précédente mais poste quand même la valeur dans un champ caché.', # NEW
-	'option_disable_avec_post_label' => 'Désactiver mais poster', # NEW
-	'option_disable_explication' => 'Le champ ne peut plus obtenir le focus.', # NEW
+	'option_defaut_label' => 'Выводить по умолчанию',
+	'option_disable_avec_post_explication' => 'Такой же вариант как и предыдущий, но вносит значение в скрытое поле.',
+	'option_disable_avec_post_label' => 'Отключено, но опубликовано.',
+	'option_disable_explication' => 'Поле не может получить фокус',
 	'option_disable_label' => 'Сделать поле неактивным',
 	'option_explication_explication' => 'Краткое описание (по необходимости)',
-	'option_explication_label' => 'Обьяснение',
+	'option_explication_label' => 'Комментарий',
 	'option_groupe_affichage' => 'Показать',
 	'option_groupe_description' => 'Описание',
 	'option_groupe_utilisation' => 'Использование',
 	'option_groupe_validation' => 'Валидация',
-	'option_info_obligatoire_explication' => 'Vous pouvez modifier l\'indication d\'obligation par défaut : <i>[Obligatoire]</i>.', # NEW
-	'option_info_obligatoire_label' => 'Indication d\'obligation', # NEW
-	'option_inserer_barre_choix_edition' => 'barre d\'édition complète', # NEW
-	'option_inserer_barre_choix_forum' => 'Панель инструментов форума',
-	'option_inserer_barre_explication' => 'Insère une barre d\'outils du porte-plume si ce dernier est activé.', # NEW
-	'option_inserer_barre_label' => 'Вставить панель управления',
-	'option_label_case_label' => 'Label placé à côté de la case', # NEW
+	'option_info_obligatoire_explication' => 'Вы можете изменить стандартные настройки обязательного заполнения полей.. ',
+	'option_info_obligatoire_label' => 'Обязательное заполнение полей',
+	'option_inserer_barre_choix_edition' => 'Да',
+	'option_inserer_barre_choix_forum' => 'Только форум',
+	'option_inserer_barre_explication' => 'Вставить porte-plume?',
+	'option_inserer_barre_label' => 'Панель управления',
+	'option_label_case_label' => 'Позиция чекбокса',
 	'option_label_explication' => 'Название, которое будет отображаться',
-	'option_label_label' => 'Label', # NEW
-	'option_maxlength_explication' => 'Пользователь не может ввести больше знаков чем это число.',
-	'option_maxlength_label' => 'Максимально возможное количество символов',
+	'option_label_label' => 'Название',
+	'option_maxlength_explication' => 'Пользователь не может ввести больше знаков чем:',
+	'option_maxlength_label' => 'Максимальное к-во символов',
 	'option_multiple_explication' => 'Пользователю предоставляется возможность выбрать несколько вариантов',
 	'option_multiple_label' => 'Множественный выбор',
-	'option_nom_explication' => 'Un nom informatique qui identifiera le champ. Il ne doit contenir que des caractères alpha-numériques minuscules ou le caractère "_".', # NEW
+	'option_nom_explication' => 'ID компьютера , который идентифицирует поле. Он может содержать только строчные буквы и цифры и символ подчеркивания "_".',
 	'option_nom_label' => 'Название поля',
 	'option_obligatoire_label' => 'Поле обязательное для заполнения',
-	'option_option_intro_label' => 'Label du premier choix vide', # NEW
+	'option_option_intro_label' => 'Обозначение для пустого выбора',
 	'option_option_statut_label' => 'Показать статус',
 	'option_pliable_label' => 'Расширяемая',
 	'option_pliable_label_case' => 'Группы полей могут быть расширены или сжаты.',
 	'option_plie_label' => 'Сжато.',
-	'option_plie_label_case' => 'Si le groupe de champs est pliable, il sera déjà plié à l\'affichage du formulaire.', # NEW
-	'option_previsualisation_explication' => 'Si le porte-plume est activé, ajoute un onglet pour prévisualiser le rendu du texte saisi.', # NEW
-	'option_previsualisation_label' => 'Включить просмотры.',
+	'option_plie_label_case' => 'Если группу полей можно расширить или сжать, тогда эта опция их сожмет с отображением полей.',
+	'option_previsualisation_explication' => 'Создать вкладку для просмотра введенного текста?',
+	'option_previsualisation_label' => 'Просмотр',
 	'option_readonly_explication' => 'Поле можно просмотреть, выбрать, но изменить нельзя.',
 	'option_readonly_label' => 'Только чтение',
-	'option_rows_explication' => 'Высота поля в строках. Этот вариант не всегда используется так как стили CSS могут переопределять его.',
-	'option_rows_label' => 'Количество строк',
-	'option_size_explication' => 'Длина поля в символах. Этот вариант не всегда используется, так как CSS стили могут переопределять его.',
+	'option_rows_explication' => 'Высота поля в строках.',
+	'option_rows_label' => 'Высота',
+	'option_size_explication' => 'Длина поля в символах.',
 	'option_size_label' => 'Размер поля',
 	'option_type_choix_plusieurs' => 'Позволяет выбрать <strong>несколько</strong> получателей.',
-	'option_type_choix_tous' => 'Mettre <strong>tous</strong> ces auteurs en destinataires. L\'utilisateur n\'aura aucun choix.', # NEW
-	'option_type_choix_un' => 'Permettre à l\'utilisateur de choisir <strong>un seul</strong> destinataire.', # NEW
-	'option_type_explication' => 'En mode "masqué", le contenu du champ ne sera pas visible.', # NEW
+	'option_type_choix_tous' => 'Отметить <strong>всех</strong> авторов как получателей. Пользователю выбор не предоставляется.',
+	'option_type_choix_un' => 'Сделать возможным выбор только одного получателя.',
+	'option_type_explication' => 'В "скрытом" режиме содержимое поля будет отображаться в виде звездочек.',
 	'option_type_label' => 'Тип поля',
-	'option_type_password' => 'Masqué', # NEW
-	'option_type_text' => 'Normal', # NEW
+	'option_type_password' => 'Скрытый',
+	'option_type_text' => 'Нормальный',
 
 	// S
 	'saisie_auteurs_explication' => 'Позволяет выбрать одного или несколько авторов.',
 	'saisie_auteurs_titre' => 'Автора',
-	'saisie_case_explication' => 'Permet d\'activer ou de désactiver quelque chose.', # NEW
-	'saisie_case_titre' => 'Case unique', # NEW
-	'saisie_checkbox_explication' => 'Permet de choisir plusieurs options avec des cases.', # NEW
-	'saisie_checkbox_titre' => 'Cases à cocher', # NEW
-	'saisie_date_explication' => 'Permet de saisir une date ? l\'aide d\'un calendrier', # NEW
+	'saisie_case_explication' => 'Используется для включения/отключения определенной опции.',
+	'saisie_case_titre' => 'Единичный выбор ',
+	'saisie_checkbox_explication' => 'Используется для выбора нескольких вариантов при помощи check-box.',
+	'saisie_checkbox_titre' => 'Чекбокс',
+	'saisie_date_explication' => 'Используется для ввода даты при помощи календаря.',
 	'saisie_date_titre' => 'Дата',
-	'saisie_destinataires_explication' => 'Permet de choisir un ou plusieurs destinataires parmis des auteurs pré-sélectionné.', # NEW
+	'saisie_destinataires_explication' => 'Используется для выбора одного или нескольких получателей из числа предварительно выбранных авторов.',
 	'saisie_destinataires_titre' => 'Получатели',
-	'saisie_explication_explication' => 'Un texte explicatif général.', # NEW
-	'saisie_explication_titre' => 'Пояснение',
-	'saisie_fieldset_explication' => 'Un cadre qui pourra englober plusieurs champs.', # NEW
-	'saisie_fieldset_titre' => 'Groupe de champs', # NEW
+	'saisie_explication_explication' => 'Описание.',
+	'saisie_explication_titre' => 'Информация',
+	'saisie_fieldset_explication' => 'Форма, которая содержит несколько полей.',
+	'saisie_fieldset_titre' => 'Филдсет',
 	'saisie_file_explication' => 'Отправить файл',
 	'saisie_file_titre' => 'Файл',
-	'saisie_hidden_explication' => 'Un champ pré-rempli que l\'utilisateur ne pourra pas voir.', # NEW
+	'saisie_hidden_explication' => 'Предварительно заполненные поля, которые недоступны пользователю.',
 	'saisie_hidden_titre' => 'Скрытое поле',
-	'saisie_input_explication' => 'Une simple ligne de texte, pouvant être visible ou masquée (mot de passe).', # NEW
+	'saisie_input_explication' => 'Строчка текста, которая может быть как скрытой так и видимой (пароль).',
 	'saisie_input_titre' => 'Текстовое поле',
-	'saisie_oui_non_explication' => 'Oui ou non, c\'est clair ? :)', # NEW
+	'saisie_oui_non_explication' => 'Или ДА или НЕТ ',
 	'saisie_oui_non_titre' => 'Да или нет',
 	'saisie_radio_defaut_choix1' => 'Один',
 	'saisie_radio_defaut_choix2' => 'Два',
 	'saisie_radio_defaut_choix3' => 'Три',
-	'saisie_radio_explication' => 'Permet de choisir une option parmis plusieurs disponibles.', # NEW
-	'saisie_radio_titre' => 'Boutons radios', # NEW
-	'saisie_selecteur_article' => 'Affiche un navigateur de sélection d\'article', # NEW
-	'saisie_selecteur_article_titre' => 'Sélecteur d\'article', # NEW
-	'saisie_selecteur_rubrique' => 'Affiche un navigateur de sélection de rubrique', # NEW
-	'saisie_selecteur_rubrique_article' => 'Affiche un navigateur de sélection d\'article ou de rubrique', # NEW
-	'saisie_selecteur_rubrique_article_titre' => 'Sélecteur d\'article ou rubrique', # NEW
-	'saisie_selecteur_rubrique_titre' => 'Sélecteur de rubrique', # NEW
-	'saisie_selection_explication' => 'Choisir une option dans une liste déroulante.', # NEW
-	'saisie_selection_multiple_explication' => 'Permet de choisir plusieurs options avec une liste.', # NEW
+	'saisie_radio_explication' => 'Используется для выбора одной опции из нескольких.',
+	'saisie_radio_titre' => 'Радио кнопка',
+	'saisie_selecteur_article' => 'Отобразить браузер выбора статьи',
+	'saisie_selecteur_article_titre' => 'Селектор статьи',
+	'saisie_selecteur_rubrique' => 'Отобразить браузер выбора раздела',
+	'saisie_selecteur_rubrique_article' => 'Отобразить браузер выбора статьи или раздела',
+	'saisie_selecteur_rubrique_article_titre' => 'Селектор статьи или раздела.',
+	'saisie_selecteur_rubrique_titre' => 'Селектор раздела.',
+	'saisie_selection_explication' => 'Выбрать пункт из выпадающего списка.',
+	'saisie_selection_multiple_explication' => 'Используется для выбора нескольких вариантов из списка.',
 	'saisie_selection_multiple_titre' => 'Множественный выбор',
-	'saisie_selection_titre' => 'Liste déroulante', # NEW
+	'saisie_selection_titre' => 'Выпадающий список',
 	'saisie_textarea_explication' => 'Многострочное текстовое поле',
-	'saisie_textarea_titre' => 'Bloc de texte', # NEW
+	'saisie_textarea_titre' => 'Текстовое поле',
 
 	// T
 	'tous_visiteurs' => 'Все посетители (в том числе не зарегистрированы)',
 
 	// V
-	'vue_sans_reponse' => '<i>Sans réponse</i>', # NEW
+	'vue_sans_reponse' => '<i>Без ответа</i>',
 
 	// Z
-	'z' => 'zzz' # NEW
+	'z' => 'zzz'
 );
 
 ?>
