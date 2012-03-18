7e3980f13752f3aa221dcc16d6c181404ad2523b
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-03-18 19:58:54 +0000

    langues (openinviter)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@59524 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ openinviter/lang/openinviter_en.php
@@ -0,0 +1,20 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org/tradlang_module/openinviter?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'champ_email_label' => 'Your email @fournisseur@',
+	'champ_password_explication' => 'It won\'t be stored.',
+	'champ_password_label' => 'Password',
+
+	// E
+	'erreur_authentification' => 'Authentication failed. Check the address and the password and try again.',
+	'erreur_generale' => 'An internal error prevents the recovery of your contacts.'
+);
+
+?>
