26922b4c95b3dab3ab0fc3d4137bf743c79cc47e
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-03-18 19:58:47 +0000

    langues (paquet-manuelsite)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@59517 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ manuel_site/lang/paquet-manuelsite_sk.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org/tradlang_module/paquet-manuelsite?lang_cible=sk
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// M
+	'manuelsite_description' => 'Ce plugin installe une icône d\'aide permettant d\'afficher depuis n\'importe quelle page de l\'espace privé le manuel de rédaction du site. Ce manuel est un article du site. Le plugin fournit également un jeu d\'item de Foire Aux Questions courants pouvant être insérés facilement dans votre manuel.', # NEW
+	'manuelsite_nom' => 'Manuál na publikovanie stránky',
+	'manuelsite_slogan' => 'Špeciálny manuál na vašu stránku pre vašich redaktorov'
+);
+
+?>
