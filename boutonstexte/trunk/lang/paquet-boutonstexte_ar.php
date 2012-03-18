b5220b3fd2f95afa8d6a93f7ca16129096523f4a
george@middleeastwatch.net <george@middleeastwatch.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-03-18 19:58:35 +0000

    langues (paquet-boutonstexte)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@59505 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- boutonstexte/trunk/lang/paquet-boutonstexte_ar.php
+++ boutonstexte/trunk/lang/paquet-boutonstexte_ar.php
@@ -8,11 +8,11 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// B
-	'boutonstexte_description' => 'يضيف ازار تكبير وتصغير الخط في نص المقال. الازرار تظهر قبل كل علامة div من صنف texte في النص اذا لم تكن موجودة سابقاً (صور من صنف textsizeup وtextsizedown وtextonly للنص فقط)
+	'boutonstexte_description' => 'يضيف أزرار تكبير وتصغير الخط في نص المقال. تظهر الأزرار قبل كل علامة div من صنف texte في النص اذا لم تكن موجودة سابقاً (صور من صنف textsizeup وtextsizedown وtextonly للنص فقط)
 
-يمكن تنفيذ الاعدادالاختياري بتثبيت الملحق cfg وبتحميل الصفحة. يمكن فرض بعض العناصر بواسطة جافاسكريبت',
+يمكن تنفيذ الإعداد الاختياري بتثبيت الملحق cfg. يمكن فرض بعض العناصر بواسطة جافاسكريبت.',
 	'boutonstexte_nom' => 'تكبير وتصغير',
-	'boutonstexte_slogan' => 'Des boutons pour gérer la taille des caractères' # NEW
+	'boutonstexte_slogan' => 'أزرار للتحكم بحجم الحروف'
 );
 
 ?>
