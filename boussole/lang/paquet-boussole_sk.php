0c51696706049066c2bc473639784a4846616383
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-03-18 19:58:33 +0000

    langues (paquet-boussole)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@59503 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- boussole/lang/paquet-boussole_sk.php
+++ boussole/lang/paquet-boussole_sk.php
@@ -10,7 +10,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// B
 	'boussole_description' => 'Nainštalujte si tento zásuvný modul na svoju stránku a vo svojich šablónach budete mať prístup na všetky stránky Galaxie SPIPu pomocou  modelov šablón, orieškov alebo navrhovanej stránky Z.
 _ Sami môžete tento zásuvný modul používať na zobrazenie svojho vlastného zoznamu stránok a vytvoriť svoje vlastné zobrazenia.',
-	'boussole_nom' => 'Buzola',
+	'boussole_nom' => 'Kompas',
 	'boussole_slogan' => 'Obľúbené adresy v galaxii SPIPu alebo inde!'
 );
 
