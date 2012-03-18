391715678d1c8bd301eec4b9502d3ac16114d13a
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-03-18 19:58:30 +0000

    langues (paquet-abomailmans)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@59500 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- abomailmans/trunk/lang/paquet-abomailmans_en.php
+++ abomailmans/trunk/lang/paquet-abomailmans_en.php
@@ -8,9 +8,11 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
-	'abomailmans_description' => 'This plugin adds a form of registration to mailing lists services as [Mailman->http://www.gnu.org/software/mailman/], [Ezmlm->http://www.ezmlm.org/] or [Sympa->http://www.sympa.org] the administrator can manage from the private space via a dedicated interface.',
-	'abomailmans_nom' => 'Mailing lists subscription',
-	'abomailmans_slogan' => 'Interfacer vos listes de diffusions mailman, ezmlm ou sympa avec votre SPIP' # NEW
+	'abomailmans_description' => 'This plugin adds a registration form to mailing lists and newsletters services as [Mailman->http://www.gnu.org/software/mailman/], [Ezmlm->http://www.ezmlm.org/] or [Sympa->http://www.sympa.org].
+
+The administrator can manage the lists from the private space via a dedicated interface.',
+	'abomailmans_nom' => 'Mailing lists and newsletters subscription',
+	'abomailmans_slogan' => 'Interface your Mailman, Ezmlm or Sympa mailing lists and newsletters with SPIP'
 );
 
 ?>
