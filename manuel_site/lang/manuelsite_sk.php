bb516cd8a2e153545c9c6722c3131bd2f2c12214
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-03-18 19:58:46 +0000

    langues (manuelsite)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@59516 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ manuel_site/lang/manuelsite_sk.php
@@ -0,0 +1,54 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org/tradlang_module/manuelsite?lang_cible=sk
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'configurer_explication' => 'Ce plugin installe une icône d’aide permettant d’afficher depuis n’importe quelle page de l’espace privé le manuel de rédaction du site. Ce manuel est @texte@ Il a pour but d’expliquer aux rédacteurs l’architecture du site, dans quelle rubrique ranger quoi, comment encoder et installer une vidéo... Bref tout ce que vous voulez et qui est spécifique à votre site.', # NEW
+	'configurer_explication_l_article' => '<a href="?exec=articles&id_article=@idart@" title="Manuál na publikovanie">článok @idart@</a> z vašej stránky.',
+	'configurer_explication_un_article' => 'článok stránky.',
+	'configurer_titre' => 'Nastaviť manuál na publikovanie stránky',
+
+	// E
+	'erreur_article' => 'Článok manuálu definovaný v nastaveniach zásuvného modulu sa nenašiel: @idart@',
+	'erreur_pas_darticle' => 'Článok manuálu nie je definovaný v nastaveniach zásuvného modulu',
+	'explication_afficher_bord_gauche' => 'Ikonu manuálu zobraziť vľavo hore (ak sa manuál zobrazí v stĺpci)',
+	'explication_background_color' => 'Uveďte farbu pozadia oblasti, v ktorej sa zobrazí manuál',
+	'explication_cacher_public' => 'Schovať tento článok na verejne prístupnej stránke, vrátane kanála rss',
+	'explication_email' => 'Kontaktný e-mail pre redaktorov',
+	'explication_faq' => 'Vous trouverez ci-dessous les blocs génériques utilisables pour rédiger votre manuel (le texte s’affiche sans mise en forme au survol du code). Il suffit de copier/coller le code correspondant dans la zone de texte de votre article.<br />Pour ne pas afficher la question, rajouter <i>|q=non</i>.<br />Pour ajouter des paramètres, rajouter <i>|params=p1:v1;p2:v2</i>.', # NEW
+	'explication_id_article' => 'Zadajte číslo článku, ktorý je súčasťou manuálu',
+	'explication_intro' => 'Úvodný text manuálu (bude umiestnený pred úvod, resp. perex)',
+	'explication_largeur' => 'Zadajte šírku oblasti, v ktorej sa zobrazí manuál',
+
+	// F
+	'fermer_le_manuel' => 'Zatvoriť manuál',
+
+	// H
+	'help' => 'Pomoc: ',
+
+	// I
+	'intro' => 'Ce document a pour but d’aider les rédacteurs à l’utilisation du site. Il vient en complément du document intitulé « [Cours SPIP 2.0 pour rédacteurs->http://www.spip-contrib.net/Mode-d-emploi-SPIP-2-pour] » qui est une aide globale à l’utilisation de SPIP. Vous y trouverez une description de l’architecture du site, de l’aide technique sur des points particuliers...', # NEW
+
+	// L
+	'label_afficher_bord_gauche' => 'Zobrazenie',
+	'label_background_color' => 'Farba pozadia',
+	'label_cacher_public' => 'Schovať',
+	'label_email' => 'E-mail',
+	'label_id_article' => 'Č. článku',
+	'label_intro' => 'Úvod',
+	'label_largeur' => 'Šírka',
+	'legende_apparence' => 'Vzhľad',
+	'legende_contenu' => 'Obsah',
+
+	// T
+	'titre_faq' => 'Časté otázky o Manuále na publikovanie stránky',
+	'titre_manuel' => 'Manuál na publikovanie stránky',
+	'titre_menu' => 'Manuál na publikovanie stránky'
+);
+
+?>
