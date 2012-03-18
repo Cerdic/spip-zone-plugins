314d97782cafd4eecdf9e6bbf0da77bbd20c739c
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-03-18 19:58:42 +0000

    langues (paquet-courtcircuit)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@59512 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ court-circuit/trunk/lang/paquet-courtcircuit_en.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org/tradlang_module/paquet-courtcircuit?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'courtcircuit_description' => 'Allows to redirect certain sections to a specific article. You can configure in the private area, the redirection rules to apply.',
+	'courtcircuit_slogan' => 'Direct access to articles'
+);
+
+?>
