79f0a2632f71611324125db8b3888ae1187c6887
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-03-20 12:49:24 +0000

    typo
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@59571 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- manuel_site/lang/manuelsite_fr.php
+++ manuel_site/lang/manuelsite_fr.php
@@ -6,18 +6,18 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// C
 	'configurer_explication' => 'Ce plugin installe une icône d’aide permettant d’afficher depuis n’importe quelle page de l’espace privé le manuel de rédaction du site. Ce manuel est @texte@ Il a pour but d’expliquer aux rédacteurs l’architecture du site, dans quelle rubrique ranger quoi, comment encoder et installer une vidéo... Bref tout ce que vous voulez et qui est spécifique à votre site.',
-	'configurer_explication_l_article' => '<a href="@url@" title="Manuel de redaction">l’article @idart@</a> de votre site.',
+	'configurer_explication_l_article' => '<a href="@url@" title="Manuel de redaction">l’article #@idart@</a> de votre site.',
 	'configurer_explication_un_article' => 'un article du site.',
 	'configurer_titre' => 'Configurer le manuel de rédaction du site',
 
 	// E
-	'erreur_article' => 'L’article du manuel défini dans la configuration du plugin est introuvable : @idart@',
+	'erreur_article' => 'L’article du manuel défini dans la configuration du plugin est introuvable : #@idart@',
 	'erreur_pas_darticle' => 'L’article du manuel n’est pas défini dans la configuration du plugin',
 	'explication_afficher_bord_gauche' => 'Afficher l’icone du manuel en haut à gauche (sinon le manuel sera affiché en colonne)',
 	'explication_background_color' => 'Entrez la couleur de fond de la zone d’affichage du manuel',
 	'explication_cacher_public' => 'Cacher cet article dans l’espace public, flux rss compris',
 	'explication_email' => 'Email de contact pour les rédacteurs',
-	'explication_faq' => 'Vous trouverez ci-dessous les codes des blocs génériques utilisables pour rédiger votre manuel. Le texte correpondant à chaque code s’affiche (sans mise en forme) au survol de celui-ci. Il suffit de copier/coller le code désiré dans la zone de texte de votre article.<br />Pour ne pas afficher la question, rajouter <i>|q=non</i>.<br />Pour ajouter des paramètres, rajouter <i>|params=p1:v1;p2:v2</i>.',
+	'explication_faq' => 'Vous trouverez ci-dessous les codes des blocs génériques utilisables pour rédiger votre manuel. Le texte correspondant à chaque code s’affiche (sans mise en forme) au survol de celui-ci. Il suffit de copier/coller le code désiré dans la zone de texte de votre article.<br />Pour ne pas afficher la question, rajouter <i>|q=non</i>.<br />Pour ajouter des paramètres, rajouter <i>|params=p1:v1;p2:v2</i>.',
 	'explication_id_article' => 'Entrez le numéro de l’article qui contient le manuel',
 	'explication_intro' => 'Texte d’introduction au manuel (sera placé avant le chapo)',
 	'explication_largeur' => 'Entrez la largeur de la zone d’affichage du manuel',
