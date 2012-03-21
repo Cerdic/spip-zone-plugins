c3f9d0cd0ecb41bdcbd27f5cd1ed42c76e23376b
daniel@anic.fr <daniel@anic.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-03-21 21:55:23 +0000

    Des commentaires pour plus tard
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@59613 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- wp2spip/wp2spip_options.php
+++ wp2spip/wp2spip_options.php
@@ -2,12 +2,9 @@
 	function balise_WP2SPIP_DUMP ($p) {
 		spip_log('wp2spip dump Start');
 		$wp2spip=recuperer_fond ('wp2spip');
-
 		$ou='wp2spip.xml';
-
 		ecrire_fichier(_DIR_DUMP . $ou, $wp2spip);
 		spip_log('wp2spip dump Done');
-
 		$p->code = "'Base Wordpress convertie au format xml SPIP 2.1.12  !<br/><br/>Rendez-vous &agrave; la page <a href=\'?exec=admin_tech\' title=\'\'>Maintenance</a> du site et importer la base wp2spip.xml<br/><br/>Attention cette restauration remplacera votre base SPIP actuelle!!'";
 		return $p;
 	}
