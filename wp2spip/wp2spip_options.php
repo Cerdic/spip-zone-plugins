0ec4f799e45168f057ac56d5ad0421a2dc56bc1b
l.oiseau2nuit@gmail.com <l.oiseau2nuit@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-03-21 20:34:05 +0000

    Plugin WP2SPIP : Importer un dump Wordpress dans SPIP (par Daniel ANIC)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@59609 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ wp2spip/wp2spip_options.php
@@ -0,0 +1,17 @@
+ <?php
+
+function balise_WP2SPIP_DUMP ($p) {
+spip_log('wp2spip dump start');
+$wp2spip=recuperer_fond ('wp2spip');
+
+$ou='wp2spip.xml';
+if (ecrire_fichier(_DIR_DUMP . $ou, $wp2spip)) {
+ecrire_fichier(_DIR_DUMP . $ou, $wp2spip);
+spip_log('wp2spip dump Done');
+
+}
+  $p->code = "'Base Wordpress convertie au format xml SPIP 2.1.12  !<br/><br/>Rendez-vous &agrave; la page <a href=\'?exec=admin_tech\' title=\'\'>Maintenance</a> du site et importer la base wp2spip.xml<br/><br/>Attention cette restauration remplacera votre base SPIP actuelle!!'";
+  return $p;
+}
+
+?> 
