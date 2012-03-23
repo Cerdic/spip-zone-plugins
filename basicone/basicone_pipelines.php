e7af3764249e5653b7fb33adce9dd6185a43dc5d
chankalan@free.fr <chankalan@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-03-23 12:46:50 +0000

    Basicone : un jeu d'icones pour l'espace privé de SPIP3
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@59655 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ basicone/basicone_pipelines.php
@@ -0,0 +1,8 @@
+<?php
+
+function basicone_header_prive($flux){
+    $flux .= '<link rel="stylesheet" href="' . _DIR_PLUGIN_BASICONE  .'css/basicone.css" type="text/css" media="all" />';
+    return $flux;
+}
+
+?>
