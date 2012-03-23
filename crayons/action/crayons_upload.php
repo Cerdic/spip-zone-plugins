083d4741bc651846db5cc4dd54c364fc67a66448
guy.cesaro@gmail.com <guy.cesaro@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-03-23 10:54:34 +0000

    tentative pour que l'ajout de documents via drag/drop fonctionne avec SPIP3
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@59653 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- crayons/action/crayons_upload.php
+++ crayons/action/crayons_upload.php
@@ -25,16 +25,31 @@ function action_crayons_upload() {
 	// on n'accepte qu'un seul document à la fois, dans la variable 'upss'
 	if ($file = $_FILES['upss']
 	AND $file['error'] == 0) {
-		include_spip('inc/ajouter_documents');
-
+	
 		$source = $file['tmp_name'];  # /tmp/php/phpxxx
-		$nom_envoye = $file['name'];  # mon-image.jpg
-
-		$id = ajouter_un_document($source, $nom_envoye, $type, $id, 'document', $id_document=0, &$documents_actifs, $titrer=true);
+		$nom_envoye = $file['name'];  # mon-image.jpg	
+		
+		include_spip('plugins/installer'); // spip_version_compare dans SPIP 3.x 
+		include_spip('inc/plugin'); // spip_version_compare dans SPIP 2.x 
+		if (function_exists(spip_version_compare)) { // gerer son absence dans les branche precedente a SPIP 2.x
+			if (spip_version_compare($GLOBALS['spip_version_branche'], '3.0.0alpha', '>=')) 
+					define('_SPIP3', true); 		
+		} 
+		if (defined('_SPIP3')) {
+			include_spip('action/ajouter_documents');
+			
+			$ajouter_un_document = charger_fonction('ajouter_un_document','action');
+			$id = $ajouter_un_document("new", $file, $type, $id, 'document');
+			
+		} else {
+			include_spip('inc/ajouter_documents');
+			
+			$id = ajouter_un_document($source, $nom_envoye, $type, $id, 'document', $id_document=0, &$documents_actifs, $titrer=true);
+		}
 	}
 
 	if (!$id) {
-		$erreur = "errur !";
+		$erreur = "erreur !";
 	}
 
 	$a = recuperer_fond('modeles/uploader_item',array('id_document' => $id, 'erreur' => $erreur));
