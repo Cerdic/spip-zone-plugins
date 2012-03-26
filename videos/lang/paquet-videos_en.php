d588f7012c2125c9d5ad7034ab10577bec953526
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-03-26 08:45:51 +0000

    langues (paquet-videos)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@59740 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- videos/lang/paquet-videos_en.php
+++ videos/lang/paquet-videos_en.php
@@ -1,20 +1,16 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-videos
-// Langue: en
-// Date: 21-03-2012 09:29:03
-// Items: 3
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org/tradlang_module/paquet-videos?lang_cible=en
+// ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// V
+	// V
 	'videos_description' => 'This plugin allows the integration, using a simple copy/paste of URL, of videos hosted on major platforms (Dailymotion, Vimeo, YouTube, Culturebox) and manage them as spip documents. It also allows the HTML5 display of video in MP4, H.264, Ogg, WebM anf Mkv formats, even in mobile browsers.',
 	'videos_nom' => 'Video(s)',
-	'videos_slogan' => 'Management interface and integration models of videos',
+	'videos_slogan' => 'Management interface and integration models of videos'
 );
+
 ?>
--- videos/lang/paquet-videos_fr.php
+++ videos/lang/paquet-videos_fr.php
@@ -1,18 +1,11 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-videos
-// Langue: fr
-// Date: 21-03-2012 09:29:03
-// Items: 3
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/videos/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// V
+	// V
 	'videos_description' => 'Ce plugin permet d’intégrer, à partir d’un
 			simple copier/coller d’URL, des vidéos hébergées
 			sur les principales plateformes (Dailymotion, Vimeo, Youtube, CultureBox)
@@ -20,6 +13,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 			l’affichage HTML5 des vidéos aux formats MP4, H264, Ogg,
 			WebM et Mkv même dans les navigateurs mobiles.',
 	'videos_nom' => 'Vidéo(s)',
-	'videos_slogan' => 'Interface de gestion et modèles d\'insertion de vidéos',
+	'videos_slogan' => 'Interface de gestion et modèles d\'insertion de vidéos'
 );
+
 ?>
