e33d779aa954e071f3e8876539f4f5553b1450c4
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-03-26 08:45:50 +0000

    langues (videos)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@59739 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- videos/lang/videos_fr.php
+++ videos/lang/videos_fr.php
@@ -1,40 +1,44 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-if (!defined("_ECRIRE_INC_VERSION")) return;
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/videos/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+
 	// C
 	'cfg_description_doc' => 'Documentation',
-	'cfg_description_titre' => 'Configuration du plugin Vid&eacute;o(s)',
-	'cfg_explication_hauteur' => "Choisissez la hauteur par d&eacute;faut pour vos vid&eacute;os. Cette option reste surchargeable dans l&#x27;appel au mod&egrave;le.",
-	'cfg_explication_largeur' => "Choisissez la largeur par d&eacute;faut pour vos vid&eacute;os. Cette option reste surchargeable dans l&#x27;appel au mod&egrave;le.",
-	'cfg_explication_liste_definition' => "Encapsuler la vid&eacute;o dans un dl/dt/dd comme peut le faire SPIP pour sa gestion de document.",
-	'cfg_explication_titre_descriptif' => "Afficher le titre et la description des vid&eacute;os. Cette option reste surchargeable dans l&#x27;appel au mod&egrave;le.",
-	'cfg_explication_wmode' => "Choisissez le mode &agrave; appliquer pour les objets Flash (fallback Flowplayer, iFrames Dailymotion, Youtube, Vimeo).",
-	'cfg_label_hauteur' => "Hauteur par d&eacute;faut",	
-	'cfg_label_largeur' => "Largeur par d&eacute;faut",	
-	'cfg_label_liste_definition' => "Liste de d&eacute;finition HTML",	
-	'cfg_label_titre_descriptif' => "Titre et descriptif",	
-	'cfg_label_wmode' => "Wmode par d&eacute;faut",	
+	'cfg_description_titre' => 'Configuration du plugin Vidéo(s)',
+	'cfg_explication_hauteur' => 'Choisissez la hauteur par défaut pour vos vidéos. Cette option reste surchargeable dans l\'appel au modèle.',
+	'cfg_explication_largeur' => 'Choisissez la largeur par défaut pour vos vidéos. Cette option reste surchargeable dans l\'appel au modèle.',
+	'cfg_explication_liste_definition' => 'Encapsuler la vidéo dans un dl/dt/dd comme peut le faire SPIP pour sa gestion de document.',
+	'cfg_explication_titre_descriptif' => 'Afficher le titre et la description des vidéos. Cette option reste surchargeable dans l\'appel au modèle.',
+	'cfg_explication_wmode' => 'Choisissez le mode à appliquer pour les objets Flash (fallback Flowplayer, iFrames Dailymotion, Youtube, Vimeo).',
+	'cfg_label_hauteur' => 'Hauteur par défaut',
+	'cfg_label_largeur' => 'Largeur par défaut',
+	'cfg_label_liste_definition' => 'Liste de définition HTML',
+	'cfg_label_titre_descriptif' => 'Titre et descriptif',
+	'cfg_label_wmode' => 'Wmode par défaut',
 	'cfg_titre_configurations_communes' => 'Configurations communes',
 	'cfg_titre_configurations_dailymotion' => 'Configurations Dailymotion',
 	'cfg_titre_configurations_html5' => 'Configuration HTML5 / Alternative',
-	'cfg_titre_configurations_vimeo' => 'Configurations Vim&eacute;o',
+	'cfg_titre_configurations_vimeo' => 'Configurations Viméo',
 	'cfg_titre_configurations_youtube' => 'Configurations Youtube',
-	'confirmation_ajout' => 'La vid&eacute;o "@type@:@titre@" a bien &eacute;t&eacute; ajout&eacute;e.',
-	
+	'confirmation_ajout' => 'La vidéo "@type@:@titre@" a bien été ajoutée.',
+
 	// E
-	'erreur_adresse_invalide' => "Cette adresse n'est pas valide. Si vous tentez d'importer un fichier vid&eacute;o (MP4 ou autre), merci d'utiliser l'ajout de document classique de SPIP.",
-	'explication_ajouter_video' => '(vid&eacute;o distante YouTube, Vimeo, DailyMotion)',
-	
+	'erreur_adresse_invalide' => 'Cette adresse n\'est pas valide. Si vous tentez d\'importer un fichier vidéo (MP4 ou autre), merci d\'utiliser l\'ajout de document classique de SPIP.',
+	'explication_ajouter_video' => '(vidéo distante YouTube, Vimeo, DailyMotion)',
+
 	// L
-	'label_ajouter_video' => "Ajouter une vid&eacute;o",
-	'label_ajouter_autre_video' => "Ajouter une autre vid&eacute;o",
-	'label_url' => "Adresse de la vid&eacute;o",
-	
+	'label_ajouter_autre_video' => 'Ajouter une autre vidéo',
+	'label_ajouter_video' => 'Ajouter une vidéo',
+	'label_url' => 'Adresse de la vidéo',
+
 	// T
-	'titre_admin' => 'Plugin Vid&eacute;o(s)',
-	
+	'titre_admin' => 'Plugin Vidéo(s)',
+
 	// V
-	'valider' => "Ajouter la vid&eacute;o"
+	'valider' => 'Ajouter la vidéo'
 );
+
+?>
