08587e876c06e2b0ade0ffdd0464be9c02886370
marcimat@rezo.net <marcimat@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-03-28 10:51:54 +0000

    Évolution du plugin inclure-ajaxload en encapsulant le javascript dans une fonction, que l'on rappelle elle-même lors d'un chargement ajax : de la sorte si une inclusion {ajaxload} est chargée dans un squelette lui-même ajouté en ajax (par exemple avec l'ajax parallèle des blocs de ZPIP), l'inclusion sera réellement faite.
    Pour éviter une récursion, les classes .includeajax et .includestatic sont renommées lorsque le js les a traitées. On les suffixe de _load pendant le chargement, et de _loaded une fois le chargement réalisé.
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@59867 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- inclure-ajaxload/javascript/inclure-ajaxload.js
+++ inclure-ajaxload/javascript/inclure-ajaxload.js
@@ -1,29 +1,33 @@
 jQuery(document).ready(function($) {
-	$('.includeajax').each(function() {
-		var me = $(this);
-		var env = $('a', this).attr('rel');
-		if (env) {
-			$('a', this).attr('href','#');
-			$.ajax({
-				url: "spip.php",
-				type: "GET",
-				cache: true,
-				data: { var_ajax: 'recuperer', var_ajax_env: env },
-				success: function(c) { me.html(c); }
-			});
-		}
-	});
-	$('.includestatic').each(function() {
-		var me = $(this);
-		var env = $('a', this).attr('rel');
-		if (env) {
-			$('a', this).attr('href','#');
-			$.ajax({
-				url: env,
-				type: "GET",
-				cache: true,
-				success: function(c) { me.html(c); }
-			});
-		}
-	});
+	function charger_inclure_ajaxload() {
+		$('.includeajax').each(function() {
+			var me = $(this);
+			var env = $('a', this).attr('rel');
+			if (env) {
+				$('a', this).attr('href','#');
+				$.ajax({
+					url: "spip.php",
+					type: "GET",
+					cache: true,
+					data: { var_ajax: 'recuperer', var_ajax_env: env },
+					success: function(c) { me.html(c).removeClass('includeajax_load').addClass('includeajax_loaded'); }
+				});
+			}
+		}).removeClass('includeajax').addClass('includeajax_load');
+		$('.includestatic').each(function() {
+			var me = $(this);
+			var env = $('a', this).attr('rel');
+			if (env) {
+				$('a', this).attr('href','#');
+				$.ajax({
+					url: env,
+					type: "GET",
+					cache: true,
+					success: function(c) {  me.html(c).removeClass('includestatic_load').addClass('includestatic_loaded'); }
+				});
+			}
+		}).removeClass('includestatic').addClass('includestatic_load');
+	}
+	charger_inclure_ajaxload();
+	onAjaxLoad(charger_inclure_ajaxload);
 });
