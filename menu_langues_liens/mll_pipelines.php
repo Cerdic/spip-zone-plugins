1ece391c56bcc435c4fced59c54e74981f2411cc
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-03-30 21:04:39 +0000

    Il n'y a rien de calculé dans cette feuille de style donc elle reste statique
    On incrémente la version
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@59945 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- menu_langues_liens/mll_pipelines.php
+++ menu_langues_liens/mll_pipelines.php
@@ -3,7 +3,7 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 
 function mll_insert_head_css($flux){
 	// Insertion de la feuille de styles du menu de langues
-	$css_mll = generer_url_public('mll_styles.css');
+	$css_mll = find_in_path('mll_styles.css');
 	$flux .='<link rel="stylesheet" type="text/css" media="screen" href="'.$css_mll.'" />';
 
 	return $flux;
