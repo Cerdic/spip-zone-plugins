609574343e197a79fc7ae9976e13b0bcad589511
pierrekuhn82@gmail.com <pierrekuhn82@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-04-01 20:04:23 +0000

    ON améliore un peu la gestion de la récupération, reste encore l'envoie de mail à voir.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@59987 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- Amap/formulaires/editer_amap_panier_recupere.php
+++ Amap/formulaires/editer_amap_panier_recupere.php
@@ -5,12 +5,12 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 include_spip('inc/actions');
 include_spip('inc/editer');
 
-function formulaires_editer_amap_panier_dispo_charger_dist($id_amap_panier,$id_auteur,$id_producteur,$date_distribution) {
+function formulaires_editer_amap_panier_recupere_charger_dist($id_amap_panier,$id_auteur,$id_producteur,$date_distribution) {
 	$valeurs = array('id_amap_panier'=>$id_amap_panier, 'id_auteur'=>$id_auteur, 'id_producteur'=>$id_producteur, 'date_distribution'=>$date_distribution);
 	return $valeurs;
 }
 
-function formulaires_editer_amap_panier_dispo_verifier_dist($id_amap_panier,$id_auteur,$id_producteur,$date_distribution){
+function formulaires_editer_amap_panier_recupere_verifier_dist($id_amap_panier,$id_auteur,$id_producteur,$date_distribution){
 	$erreurs = array();
 	// verifier que les champs obligatoires sont bien la :
 	foreach(array('id_auteur','id_producteur','date_distribution') as $obligatoire)
@@ -21,7 +21,7 @@ function formulaires_editer_amap_panier_dispo_verifier_dist($id_amap_panier,$id_
 	return $erreurs;
 }
 
-function formulaires_editer_amap_panier_dispo_traiter_dist($id_amap_panier,$id_auteur,$id_producteur,$date_distribution) {
+function formulaires_editer_amap_panier_recupere_traiter_dist($id_amap_panier,$id_auteur,$id_producteur,$date_distribution) {
 	// Le n° de panier
 	$id_amap_panier = _request('id_amap_panier');
 	// L'ahérent du panier
@@ -36,7 +36,7 @@ function formulaires_editer_amap_panier_dispo_traiter_dist($id_amap_panier,$id_a
 	spip_log("Le $id_amap_panier a bien été récupéré par l'adhérent $id_auteur, panier produit par $id_producteur pour la livraison du $date_distribution", "amap_installation");
 
 	// Valeurs de retours
-	$message['message_ok'] = _T('Le $id_amap_panier a bien été récupéré par $id_auteur');
+	$message['message_ok'] = _T('Le panier vous a bien été attribuer');
 	return $message;
 }
 ?>
