465802365d1374b75226e64d8f76316fa20b6167
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-04-01 13:03:09 +0000

    langues (nospam)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@59977 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- nospam/lang/nospam_en.php
+++ nospam/lang/nospam_en.php
@@ -14,7 +14,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'erreur_spam_ip' => 'Too many comments detrimental to the quality!',
 
 	// F
-	'forum_saisie_texte_info' => 'Pour la mise en forme de votre message, ce formulaire n\'accepte que les raccourcis SPIP <code>[-&gt;url] {{gras}} {italique} &lt;quote&gt; &lt;code&gt;</code> &lt;cadre&gt;</cadre> et le code HTML <code>&lt;q&gt; &lt;del&gt; &lt;ins&gt;</code>. Pour créer des paragraphes, laissez simplement des lignes vides.' # NEW
+	'forum_saisie_texte_info' => 'For the formatting of your message, this form only accepts SPIP shortcuts <code>[-&gt;url] {{bold}} {italic} &lt;quote&gt; &lt;code&gt;</code> &lt;cadre&gt;</cadre> et le code HTML <code>&lt;q&gt; &lt;del&gt; &lt;ins&gt;</code>. To create paragraphs, simply leave blank lines.'
 );
 
 ?>
