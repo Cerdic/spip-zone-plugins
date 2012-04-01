8e9fd0d63bf90c9af1f7e514255608cad806cc76
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-04-01 13:03:01 +0000

    langues (paquet-fulltext)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@59971 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- fulltext/lang/paquet-fulltext_en.php
+++ fulltext/lang/paquet-fulltext_en.php
@@ -8,29 +8,9 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// F
-	'fulltext_description' => 'This plugin allows on one hand to use the FULLTEXT search mode  of MySQL and thereby improve greatly the search engine compared to the native search engine of SPIP, and on the other hand to index the contents of some documents.
--* exploit the FULLTEXT SEARCH IN BOOLEAN MODE of MySQL, cf. [->http://dev.mysql.com/doc/refman/5.0/en/fulltext-boolean.html]
-
-For an optimum use, you need to install additional programs and setting up  their use in <code> mes_options.php</code> or via the plugin control panel:
--** All types of documents must have a defined constant as <code>_FULLTEXT_EXT_EXE</code> (where EXT is the document extension) or equivalent in the plugin control panel.
--** Document indexing is disabled by default (no constant defined).
--* For {{PDF}} documents:
--** Install [Xpdf->http://www.foolabs.com/xpdf/]
--*** On [Ubuntu->http://packages.ubuntu.com/fr/hardy/xpdf-utils],
--*** On Mac OS X via [MacPorts->http://xpdf.darwinports.com/] or with this [compiled version->http://users.phg-online.de/tk/MOSXS/xpdf-tools-3.dmg],
--*** On [other OS->http://www.foolabs.com/xpdf/download.html]
--** Define these constants (or use the control panel):
--*** <code>_FULLTEXT_PDF_EXE</code> (for example <code>/usr/bin/pdftotext</code>) : path to the binary <code>pdftotext</code> of [Xdpf->http://www.foolabs.com/xpdf/] to transform PDF files in raw text
--*** <code>_FULLTEXT_PDF_CMD_OPTIONS</code> (par exemple <code>-enc UTF-8</code>) : Options d\'appel de l\'exécutable
--*<code>_FULLTEXT_TAILLE</code> : Taille maximum conservée pour la version texte des fichiers (50000 par défaut)
--* For the {{DOC, PPT, XLS}} documents:
--** Install [Catdoc->http://www.wagner.pp.ru/~vitus/software/catdoc/]
--*** on [Ubuntu/Linux->http://www.wagner.pp.ru/~vitus/software/catdoc/],
--*** on [Windows->http://blog.brush.co.nz/2009/09/catdoc-windows/]
--** Define the corresponding constants (or use the control panel) the same way as for the PDF.
--* For {{ODT, DOCX, PPTX, XLSX}} documents:
--** Use PHP functions and classes (requires PHP 5.2 and the -enable-zip option).
-  ', # NEW
+	'fulltext_description' => 'This plugin allows on one hand to use the FULLTEXT search mode  of MySQL and thereby greatly improve the search engine compared to the native search engine of SPIP, and on the other hand to index the contents of some documents.
+_ For an optimum use, you need to install additional programs on the server and setting up  their use in <code>mes_options.php</code> or via the plugin control panel.
+_ See the plugin documentation for more details.',
 	'fulltext_slogan' => 'FULLTEXT indexation to speed up searches'
 );
 
