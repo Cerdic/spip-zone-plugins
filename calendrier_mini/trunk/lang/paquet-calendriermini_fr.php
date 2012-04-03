190e197d33a1028a4bbe1feb756cd1953a264be2
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-04-03 19:12:22 +0000

    credits pour l'icone dans la balise credits, ne pas dupliquer le slogan dans la description
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@60063 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- calendrier_mini/trunk/lang/paquet-calendriermini_fr.php
+++ calendrier_mini/trunk/lang/paquet-calendriermini_fr.php
@@ -6,10 +6,8 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// C
-	'calendriermini_description' => 'Permet l\'utilisation d\'une balise #CALENDRIER_MINI.
-_ Cette balise affiche un calendrier au design issu de dotclear et donc compatible avec les styles issus de ce système de blog.
-_ Lui sont adjoints d\'autre éléments, tels que balises, critères, modèles...
-_ Icone de [Tempest->http://tempest.deviantart.com/] sous licence CC BY-NC-ND.',
+	'calendriermini_description' => '#CALENDRIER_MINI affiche un calendrier au design issu de dotclear et donc compatible avec les styles issus de ce système de blog.
+_ Lui sont adjoints d\'autre éléments, tels que balises, critères, modèles...',
 	'calendriermini_slogan' => 'Permet l\'utilisation d\'une balise #CALENDRIER_MINI'
 );
 
