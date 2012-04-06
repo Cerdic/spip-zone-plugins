04dc90c366d0d29de79c5c43215ccf2e8774d1c5
ben.spip@gmail.com <ben.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-04-06 14:07:51 +0000

    un joli ptit paquet
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@60157 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ criteres_suivant_precedent/lang/paquet-suivant_precedent_en.php
@@ -0,0 +1,20 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-suivant_precedent
+// Langue: en
+// Date: 06-04-2012 16:03:45
+// Items: 3
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// S
+	'suivant_precedent_description' => 'This plugin is an experiment allowing to find in a given loop, the next or previous item. This usage is primarily useful when creating navigation by category or by topic (keywords).',
+	'suivant_precedent_nom' => 'Next / Previous criteria',
+	'suivant_precedent_slogan' => 'Adds loops criteria <code>{suivant}</code> and <code>{precedent}</code>',
+);
+?>
---
+++ criteres_suivant_precedent/lang/paquet-suivant_precedent_fr.php
@@ -0,0 +1,20 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-suivant_precedent
+// Langue: fr
+// Date: 06-04-2012 16:03:45
+// Items: 3
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// S
+	'suivant_precedent_description' => 'Ce plugin est une expérimentation permettant de trouver, dans une boucle donnée, l’élément suivant ou l’élément précédent. Cet usage est principalement utile pour créer des navigations par rubrique ou par thème (mots-clés).',
+	'suivant_precedent_nom' => 'Critères Suivant / Précédent',
+	'suivant_precedent_slogan' => 'Ajoute des critères de boucles <code>{suivant}</code> et <code>{precedent}</code>',
+);
+?>
