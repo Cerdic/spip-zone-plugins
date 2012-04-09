2158f0271746fb34f5a3aea7b21a0a69d3aa20c6
george@middleeastwatch.net <george@middleeastwatch.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-04-09 22:31:05 +0000

    langues (nospam)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@60243 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- nospam/lang/nospam_ar.php
+++ nospam/lang/nospam_ar.php
@@ -14,7 +14,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'erreur_spam_ip' => 'تعليقات كثيرة جدا هي على حساب الجودة !',
 
 	// F
-	'forum_saisie_texte_info' => 'Pour la mise en forme de votre message, ce formulaire n\'accepte que les raccourcis SPIP <code>[-&gt;url] {{gras}} {italique} &lt;quote&gt; &lt;code&gt;</code> &lt;cadre&gt;</cadre> et le code HTML <code>&lt;q&gt; &lt;del&gt; &lt;ins&gt;</code>. Pour créer des paragraphes, laissez simplement des lignes vides.' # NEW
+	'forum_saisie_texte_info' => 'لتنسيق رسالتك، لا تدعم هذه الاستمارة الا اختصارات SPIP مثل <code>[-&gt;url] {{أسود}} {مائل}&lt;quote&gt; &lt;رموز برمجية&gt;</code> &lt;إطار&gt;</cadre> وعلامات HTML كـ<code>&lt;q&gt; &lt;del&gt; &lt;ins&gt;</code>. لإنشاء فقرات، يكفي ترك أسطر فارغة.'
 );
 
 ?>
