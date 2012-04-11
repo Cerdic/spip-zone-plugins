40af1e68580a61be859a649ea42c4376097f38d6
l.oiseau2nuit@gmail.com <l.oiseau2nuit@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-04-11 12:41:12 +0000

    On branche/ et on trunk/ (A votre sante)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@60308 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ zpip_html5/trunk/javascript/readme.txt
@@ -0,0 +1,3 @@
+VERSIONS COURANTES :
+jquery-1.7.1.min
+modernizr-2.5.3.min
---
+++ zpip_html5/trunk/z5_options.php
@@ -0,0 +1,4 @@
+<?php
+$GLOBALS['z_blocs'] = array('content','extra1','extra2','head','head_js','header','footer');
+
+?>
