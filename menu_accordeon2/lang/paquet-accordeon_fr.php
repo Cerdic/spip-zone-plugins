59b6c7135affb7ece1b53f53ec26ae7f5250ff18
yffic@lefourneau.com <yffic@lefourneau.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-04-15 13:25:07 +0000

    Rangement des logos dans le dossier prive/themes Slogan + description d'après doc contrib Ajout de l'accès à la page de config dans le menu "Squelettes" Pipeline pour ieconfig Autorisation de configuration pour les webmestres par défaut (comme avec cfg)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@60408 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- menu_accordeon2/lang/paquet-accordeon_fr.php
+++ menu_accordeon2/lang/paquet-accordeon_fr.php
@@ -12,8 +12,8 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// A
-	'accordeon_description' => 'Un menu accordèon avec le plugin menu',
-	'accordeon_slogan' => 'Animer le menu de SPIP avec un effet d\'ouverture de tiroir',
+	// A
+	'accordeon_description' => 'Ce plugin permet d\'appliquer à une structure ul/li un effet accordéon (présentation « en tiroir »). Il utilise le script <a href="http://jqueryui.com/demos/accordion/">jQuery UI Accordeon</a>. Le travail de génération de la structure ul/li peut-être délégué au plugin Menus ou à une noisette Aveline par exemple ou même géré indépendamment en squelette. Il suffit de donner au ul principal la classe accordeon.',
+	'accordeon_slogan' => 'Animer un menu avec effet d\'accordéon',
 );
 ?>
