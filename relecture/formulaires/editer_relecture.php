5eb8334129acd3191ef824e505cf56eb254c12c3
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-04-15 07:31:47 +0000

    Broutilles sur le formulaire d'édition d'une relecture
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@60389 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- relecture/formulaires/editer_relecture.php
+++ relecture/formulaires/editer_relecture.php
@@ -4,18 +4,25 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 
 include_spip('inc/editer');
 
-function formulaires_editer_relecture_charger_dist($id_relecture, $redirect){
-	$valeurs = formulaires_editer_objet_charger('relecture', $id_relecture, 0, 0, $redirect, 'relectures_edit_config');
+function formulaires_editer_relecture_charger_dist($id_relecture='new', $redirect='', $row=array(), $hidden='') {
+	$valeurs = formulaires_editer_objet_charger('relecture', $id_relecture, 0, 0, $redirect, 'relectures_edit_config', $row, $hidden);
 	return $valeurs;
 }
 
-function formulaires_editer_relecture_verifier_dist($id_relecture, $redirect){
+/**
+ * Identifier le formulaire en faisant abstraction des parametres qui ne representent pas l'objet edite
+ */
+function formulaires_editer_relecture_identifier_dist($id_relecture='new', $redirect='', $row=array(), $hidden=''){
+	return serialize(array(intval($id_relecture)));
+}
+
+function formulaires_editer_relecture_verifier_dist($id_relecture='new', $redirect='', $row=array(), $hidden='') {
 	$erreurs = formulaires_editer_objet_verifier('relecture', $id_relecture, array('description'));
 	return $erreurs;
 }
 
 // http://doc.spip.org/@inc_editer_article_dist
-function formulaires_editer_relecture_traiter_dist($id_relecture, $redirect){
+function formulaires_editer_relecture_traiter_dist($id_relecture='new', $redirect='', $row=array(), $hidden='') {
 	return formulaires_editer_objet_traiter('relecture', $id_relecture, 0, 0, $redirect);
 }
 
