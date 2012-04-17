dd613f1c54a449e6812022e2d5ca1b58fe25460e
benlarcher@gmail.com <benlarcher@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-04-17 09:20:56 +0000

    Mise à jour
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@60434 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- Amap/lang/amap_en.php
+++ Amap/lang/amap_en.php
@@ -8,6 +8,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'action' => 'Action ?',
 	'action_modifier' => 'Modify',
 	'action_supprimer' => 'Delete',
+	'adherent' => 'Member', 
 	'adherent_sans_type_panier' => 'Your members do not have any basket type assigned.', 
 	'adhesion' => 'Membership (ie:2008)',
 	'adhesion_auteur' => 'Membership :',
@@ -21,14 +22,20 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 									4. Update the basket type and the subscriptions on each author page, or no basket  delivery will be attributed.<br />
 									<p><strong>NON RESPECT OF THOSE RULES WILL RESULT IN THE PLUGIN DYSFUNCTION</strong></p>', 
-	'aucun_panier_pour_nom' => 'There are no basket for @nom@.',
+	'aucun_panier_pour_nom' => 'There is no basket for @nom@.',
+	'aucun_panier_produit_par_nom' => 'There is no basket made by @nom@.',
 	'avant_le' => 'before',
 
 	// C
+	'confirmation_envoi' => 'The availability of your basket for @date_distribution@ is confirmed, thanks.',
 	'contenu_panier' => 'Basket content',
+	'contenu_panier_explication' => 'You can edit content the same way as in SPIP.',
+	'creer_paniers_pour_nom' => 'Create baskets for @nom@.',
 
 	// D
-	'date_distribution' => 'Delivery date',
+	'date_distribution' => 'Giving date',
+	'date_livraison' => 'Delivery date',
+	'depuis_le' => 'since',
 	'distribution_paniers' => 'Basket delivery of @nb@',
 
 	// E
@@ -48,14 +55,19 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'impression_explication' => 'Only the dates with at least 1 basket are clickable and open a new window.',
 
 	// L
-	'les_paniers' => 'The basket of',
+	'les_livraisons' => 'Deliveries',
+	'les_livraisons_effectuees' => 'Past deliveries',
+	'les_paniers' => 'The basket of @nom@',
 	'liste_amapiens' => 'CSA members list',
 	'liste_amapiens_enregistres'  => 'registered CSA members list',
-	'liste_paniers' => 'Basket list',
+	'liste_livraisons' => 'Deliveries list',
+	'liste_paniers' => 'Baskets list',
 	'liste_paniers_distribuer_le' => 'Basket list to deliver on',
+	'liste_paniers_vendu_par' => 'Baskets list produced by @nom@',
 	'livraison' => 'Delivery',
 
 	// M
+	'manque_fpdf_imprimer' => 'The plugin "fpdf" is missing to print your baskets lists.',
 	'mettre_disposition' => 'Place at disposal',
 	'mini_doc' => 'Short documentation',
 
@@ -64,25 +76,42 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// P
 	'panier' => 'Basket',
-	'panier_dispo' => 'Basket available',
+	'panier_deja_vendu' => 'You have sold already 1 basket',
+	'panier_dispo' => 'Basket available the @date_distribution@',
 	'panier_dispo_auteur' => 'Hello,
 		<br />The basket of @date_distribution@ is at disposal
-		<br />@nom@',
+		<br />@nom_adherent@',
+	'panier_dispo_auteur_mail' => 'Hello,
+The basket of @date_distribution@ is at disposal, please follow this link @lien@ to get it
+@panier_dispo_plus@
+@nom_adherent@',
+	'panier_dispo_plus' => 'Extra infos to give (It will be added in the mail sent before your name)',
 	'panier_distribuer' => 'basket to deliver',
-	'paniers_distribuer' => 'baskets to deliver',
-	'panier_liste' => 'Basket list',
+	'panier_liste' => 'Baskets list',
 	'panier_livraison' => 'Basket content',
+	'panier_recupere' => 'Transaction of the @date_distribution@ basket done',
+	'panier_recupere_auteur_mail' => 'Hello,
+I will get the basket of the @date_distribution@ produced by @nom_producteur@
+@nom_adherent@',
+	'paniers_deja_vendu' => 'You have sold @nb@ basket(s)',
+	'paniers_distribuer' => 'baskets to deliver',
 	'pas_connecte_ou_reconnu' => 'You are not logged in or not identified.',
-	'pas_date_distributions' => 'No distributuion date are filled in.',
+	'pas_date_distributions' => 'No distribution date are filled in.',
 	'pas_paniers' => 'You do not have any basket for this season, no contract has been done concerning you',
+	'pas_producteur_amap' => 'ou do not have any producer in your CSA.',
 	'petit' => 'Small',
+	'producteur' => 'Producer',
+	'producteurs' => 'Producers',
+
+	// Q
+	'qui_recupere_panier_disponible' => 'Who wants to get the available basket ?',
 
 	// R
 	'reste_panier_distribuer' => 'We still have 1 basket to deliver',
 	'reste_panier_recuperer' => 'You still have 1 basket to get',
 	'reste_paniers_distribuer' => 'We still have @nb@ baskets to deliver',
 	'reste_paniers_recuperer' => 'You still have @nb@ baskets to get',
-	'retour_auteur' => 'Back to the author page',
+	'retour_auteur' => 'Back to the author page @nom@ ',
 
 	//S
 	'signature' => 'Signature',
@@ -92,6 +121,8 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 					<br />It does not contain any registration.',
 	'type_panier' => 'Basket type',
 	'type_panier_auteur' => 'Basket type :',
+	'type_adherent' => 'Member type',
+	'type_adherent_auteur' => 'Member type :',
 
 	//U
 	'utiliser_entete_colone_tri' => 'Use column header to sort CSA Members (in black active sorting method and in green available ones).',
