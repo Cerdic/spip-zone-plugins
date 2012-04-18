27208ce2ba418535b3b1fa23f43e2cf448459e4c
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-04-18 11:10:49 +0000

    langues (manuelsite)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@60463 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- manuel_site/lang/manuelsite_sk.php
+++ manuel_site/lang/manuelsite_sk.php
@@ -8,7 +8,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// C
-	'configurer_explication' => 'Ce plugin installe une icône d’aide permettant d’afficher depuis n’importe quelle page de l’espace privé le manuel de rédaction du site. Ce manuel est @texte@ Il a pour but d’expliquer aux rédacteurs l’architecture du site, dans quelle rubrique ranger quoi, comment encoder et installer une vidéo... Bref tout ce que vous voulez et qui est spécifique à votre site.', # NEW
+	'configurer_explication' => 'Tento zásuvný modul nainštaluje ikonu pomocníka, ktorá umožní zobraziť zo súkromnej zóny akúkoľvek stranu z manuálu o publikovaní na stránke. Tento manuál je @texte@ Musí redaktorom priblížiť štruktúru stránky, čo je v ktorej rubrike, ako zakódovať a nainštalovať video, atď. Skrátka všetko, čo chcete a čo je špecifické pre vašu stránku.',
 	'configurer_explication_l_article' => '<a href="@url@" title="Manuál na publikovanie">článok #@idart@</a> z vašej stránky.',
 	'configurer_explication_un_article' => 'článok stránky.',
 	'configurer_titre' => 'Nastaviť manuál na publikovanie stránky',
@@ -20,7 +20,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'explication_background_color' => 'Uveďte farbu pozadia oblasti, v ktorej sa zobrazí manuál',
 	'explication_cacher_public' => 'Schovať tento článok na verejne prístupnej stránke, vrátane kanála rss',
 	'explication_email' => 'Kontaktný e-mail pre redaktorov',
-	'explication_faq' => 'Vous trouverez ci-dessous les blocs génériques utilisables pour rédiger votre manuel (le texte s’affiche sans mise en forme au survol du code). Il suffit de copier/coller le code correspondant dans la zone de texte de votre article.<br />Pour ne pas afficher la question, rajouter <i>|q=non</i>.<br />Pour ajouter des paramètres, rajouter <i>|params=p1:v1;p2:v2</i>.', # NEW
+	'explication_faq' => 'Nižšie nájdete všeobecné bloky, ktoré môžete použiť pri písaní svojho manuálu (text sa zobrazí bez formátovania kódu). Stačí skopírovať a prilepiť príslušný kód do poľa pre text článku.<br />Ak nechcete zobraziť otázku, zadajte <i>|q=non</i>.<br />Ak chcete pridať parametre, zadajte <i>|params=p1:v1;p2:v2.</i>',
 	'explication_id_article' => 'Zadajte číslo článku, ktorý je súčasťou manuálu',
 	'explication_intro' => 'Úvodný text manuálu (bude umiestnený pred úvod, resp. perex)',
 	'explication_largeur' => 'Zadajte šírku oblasti, v ktorej sa zobrazí manuál',
@@ -32,7 +32,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'help' => 'Pomoc: ',
 
 	// I
-	'intro' => 'Ce document a pour but d’aider les rédacteurs à l’utilisation du site. Il vient en complément du document intitulé « [Cours SPIP pour rédacteurs->@url@] » qui est une aide globale à l’utilisation de SPIP. Vous y trouverez une description de l’architecture du site, de l’aide technique sur des points particuliers...', # NEW
+	'intro' => 'Cieľom tohto dokumentu je pomôcť redaktorom s používaním stránky. Dopĺňa dokument s názvom "[Kurz SPIPu pre redaktorov->@url@]", ktorý poskytuje základnú pomoc s používaním SPIPu. Nájdete tu opis štruktúry stránky, technickú pomoc zameranú na konkrétne veci, atď.',
 
 	// L
 	'label_afficher_bord_gauche' => 'Zobrazenie',
