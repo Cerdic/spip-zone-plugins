c346913c776a0f6a115edb48490e2d58f1278604
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-04-21 18:53:52 +0000

    Changement de champ pour la date. Modification du formulaire d'édition.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@60531 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- relecture/formulaires/editer_relecture.php
+++ relecture/formulaires/editer_relecture.php
@@ -4,31 +4,12 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 
 include_spip('inc/editer');
 
-function formulaires_editer_relecture_charger_dist($id_relecture='oui', $redirect='', $row=array(), $hidden='') {
+function formulaires_editer_relecture_charger_dist($id_relecture='oui', $redirect='') {
 	// Traitement standard de chargement
 	$valeurs = formulaires_editer_objet_charger('relecture', $id_relecture, 0, 0, $redirect, 'relectures_edit_config', $row, $hidden);
-
-	// Traitement particulier de l'objet relecture si l'appel correspond a une ouverture :
-	// - recuperation des informations de l'article concerne (id, chapo, texte, descriptif, ps et la revision courante)
-	// - mise a jour de la date d'ouverture
-	if ($id_relecture == 'oui') {
-		if ($id_article = intval(_request('id_article'))) {
-			$select = array('chapo AS article_chapo', 'descriptif AS article_descr', 'texte AS article_texte', 'ps AS article_ps');
-			$from = 'spip_articles';
-			$where = array("id_article=$id_article");
-			$article = sql_fetsel($select, $from, $where);
-
-			$from = 'spip_versions';
-			$where = array("objet=" . sql_quote('article'), "id_objet=$id_article");
-			$revision = sql_getfetsel('max(id_version) AS revision_ouverture', $from, $where);
-
-			$valeurs = array_merge($valeurs, $article);
-			$valeurs['id_article'] = $id_article;
-			$valeurs['revision_ouverture'] = $revision;
-			$valeurs['date_ouverture'] = time();
-			$valeurs['statut'] = 'ouverte';
-			$valeurs['titre'] = 'Nouvelle relecture';
-		}
+	if ($id_article = intval(_request('id_article'))) {
+		// On supprime l'index 'id_article' du tableau desvaleurs afin que id_article soit transmis dans le GET
+		unset($valeurs['id_article']);
 	}
 
 	return $valeurs;
@@ -37,17 +18,44 @@ function formulaires_editer_relecture_charger_dist($id_relecture='oui', $redirec
 /**
  * Identifier le formulaire en faisant abstraction des parametres qui ne representent pas l'objet edite
  */
-function formulaires_editer_relecture_identifier_dist($id_relecture='new', $redirect='', $row=array(), $hidden=''){
+function formulaires_editer_relecture_identifier_dist($id_relecture='oui'){
 	return serialize(array(intval($id_relecture)));
 }
 
-function formulaires_editer_relecture_verifier_dist($id_relecture='new', $redirect='', $row=array(), $hidden='') {
+function formulaires_editer_relecture_verifier_dist($id_relecture='oui', $redirect='') {
 	$erreurs = formulaires_editer_objet_verifier('relecture', $id_relecture, array('description'));
 	return $erreurs;
 }
 
 // http://doc.spip.org/@inc_editer_article_dist
-function formulaires_editer_relecture_traiter_dist($id_relecture='new', $redirect='', $row=array(), $hidden='') {
+function formulaires_editer_relecture_traiter_dist($id_relecture='oui', $redirect='') {
+
+	// Traitement particulier de l'objet relecture si l'appel correspond a une ouverture :
+	// - recuperation des informations de l'article concerne (id, chapo, texte, descriptif, ps et la revision courante)
+	// - mise a jour de la date d'ouverture
+	if ($id_relecture == 'oui') {
+		if ($id_article = intval(_request('id_article'))) {
+			$select = array('id_article, chapo AS article_chapo', 'descriptif AS article_descr', 'texte AS article_texte', 'ps AS article_ps');
+			$from = 'spip_articles';
+			$where = array("id_article=$id_article");
+			$article = sql_fetsel($select, $from, $where);
+
+			$from = 'spip_versions';
+			$where = array("objet=" . sql_quote('article'), "id_objet=$id_article");
+			$revision = sql_getfetsel('max(id_version) AS revision_ouverture', $from, $where);
+
+			foreach ($article as $_cle => $_valeur) {
+				set_request($_cle, $_valeur);
+			}
+			set_request('revision_ouverture', $revision);
+			set_request('statut', 'ouverte');
+
+			// Pour eviter que le traitement standard ne cree un enregistrement dans la table spip_auteurs_liens
+			// il faut supprimer la reference a l'auteur connecte
+			set_request('id_auteur','');
+		}
+	}
+
 	return formulaires_editer_objet_traiter('relecture', $id_relecture, 0, 0, $redirect);
 }
 
