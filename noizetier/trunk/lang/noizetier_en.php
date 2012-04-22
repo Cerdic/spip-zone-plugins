ab66f1f77a208ce985640d772de848e704a34483
gilles.vincent@gmail.com <gilles.vincent@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-04-22 08:31:53 +0000

    langues (noizetier)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@60545 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- noizetier/trunk/lang/noizetier_en.php
+++ noizetier/trunk/lang/noizetier_en.php
@@ -9,7 +9,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// C
 	'choisir_noisette' => 'Choose the nut you want to add:',
-	'compositions_non_installe' => '<b>Plugin compositions :</b> ce plugin n\'est pas installés sur votre site. Il n\'est pas nécessaire au fonctionnement du noizetier. Cependant, s\'il est activé, vous pourrez déclarer des compositions directement dans le noizetier.', # NEW
+	'compositions_non_installe' => '<b>Plugin Compositions:</b> this plugin isn\'t installed on your site. It\'s not necessary to the correct working of the nuts. However, when it\'s activated, you can create compositions directly inside your Nuts Manager.',
 
 	// D
 	'description_bloc_contenu' => 'Main content of each page.',
@@ -20,25 +20,25 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// E
 	'editer_composition' => 'Edit this composition',
 	'editer_composition_heritages' => 'Define heritages',
-	'editer_compositions' => 'Gérer les compositions du noiZetier', # NEW
+	'editer_compositions' => 'Manage the compositions of the Nuts Manager',
 	'editer_configurer_page' => 'Configure the nuts of this page',
 	'editer_exporter_configuration' => 'Export the configuration',
 	'editer_importer_configuration' => 'Import a configuration.',
-	'editer_noizetier_compositions_explication' => 'Vous pouvez créer ici des compositions qui ne différeront que par les noisettes qui leurs seront ajoutés.', # NEW
-	'editer_noizetier_compositions_titre' => 'Compositions du noiZetier', # NEW
-	'editer_noizetier_explication' => 'Configure here the nuts to ad to your site\'s pages.',
+	'editer_noizetier_compositions_explication' => 'You can create here some compositions that only differ by the added nuts.',
+	'editer_noizetier_compositions_titre' => 'Compositions of the Nuts Manager',
+	'editer_noizetier_explication' => 'Configure here the nuts to add to your pages.',
 	'editer_noizetier_titre' => 'Manage the nuts',
 	'editer_nouvelle_composition' => 'Create a new composition',
 	'erreur_doit_choisir_noisette' => 'You have to choose a nut.',
 	'erreur_mise_a_jour' => 'An error occurred while updating the database.',
-	'explication_heritages_composition' => 'Vous pouvez définir ici les compositions qui seront héritées par les objets de la branche.', # NEW
-	'explication_noizetier_css' => 'Vous pouvez ajouter à la noisette d\'éventuelles classes CSS supplémentaires.', # NEW
+	'explication_heritages_composition' => 'Here you can set the compositions that will be inherited by the objects of the branch.',
+	'explication_noizetier_css' => 'You can add some additional CSS classes to the nut.',
 	'explication_raccourcis_typo' => 'You can use the SPIP typographical shortcuts.',
 
 	// F
 	'formulaire_ajouter_noisette' => 'Add a nut',
 	'formulaire_composition' => 'Identifier of composition',
-	'formulaire_composition_explication' => 'Indiquez un mot-clé unique (minuscules, sans espace, sans tiret (-) et sans accent) permettant d\'identifier cette composition.<br />Par exemple : <i>macompo</i>.', # NEW
+	'formulaire_composition_explication' => 'Specify a unique keyword (lowercase, no spaces, no dashes (-) and without accents) to identify the composition. <br />For example: <i>mycompo</i>.',
 	'formulaire_composition_mise_a_jour' => 'Composition updated',
 	'formulaire_configurer_bloc' => 'Configure the block:',
 	'formulaire_configurer_page' => 'Configure the page:',
@@ -48,12 +48,12 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'formulaire_description_explication' => 'You can use SPIP usual shortcuts, especially the &lt;multi&gt; tag.',
 	'formulaire_erreur_format_identifiant' => 'The identifier can only contain lowercase letters without accents, numbers and the "_" (underscore) character.',
 	'formulaire_icon' => 'Icon',
-	'formulaire_icon_explication' => 'Vous pouvez saisir le chemin relatif vers une icône (par exemple : <i>images/objet-liste-contenus.png</i>). Pour voir une liste d\'images installèes dans les répertoires les plus courants, vous pouvez <a href="../spip.php?page=icones_preview">consulter cette page</a>.', # NEW
+	'formulaire_icon_explication' => 'You can enter the relative path to an icon (eg <i>images/list-item-contenus.png</i>). To see a list of images installed in the most common directories, you can visit this <a href="../spip.php?page=icones_preview">page</a>.',
 	'formulaire_identifiant_deja_pris' => 'This identifier already exists!',
-	'formulaire_import_compos' => 'Importer les compositions du noizetier', # NEW
+	'formulaire_import_compos' => 'Import the compositions of the Nuts Manager',
 	'formulaire_import_fusion' => 'Merge with the current configuration',
 	'formulaire_import_remplacer' => 'Replace the current configuration',
-	'formulaire_liste_compos_config' => 'Ce fichier de configuration définis les compositions du noizetier suivantes :', # NEW
+	'formulaire_liste_compos_config' => 'This configuration file defines the following Nuts Manager compositions:',
 	'formulaire_liste_pages_config' => 'This configuration file defines
 nuts on the following pages:',
 	'formulaire_modifier_composition' => 'Edit this composition:',
@@ -64,23 +64,23 @@ nuts on the following pages:',
 	'formulaire_nom_explication' => 'You can use the &lt;multi&gt; tag.',
 	'formulaire_nouvelle_composition' => 'New composition',
 	'formulaire_obligatoire' => 'Required field',
-	'formulaire_supprimer_composition' => 'Delete thiscomposition',
+	'formulaire_supprimer_composition' => 'Delete this composition',
 	'formulaire_supprimer_noisette' => 'Delete this nuts',
 	'formulaire_supprimer_noisettes_page' => 'Delete the nuts of this page',
 	'formulaire_type' => 'Composition type',
-	'formulaire_type_explication' => 'Indiquez sur quel objet / quelle page porte cette composition.', # NEW
+	'formulaire_type_explication' => 'Indicate on which object/page does this composition apply.',
 	'formulaire_type_import' => 'Import type',
-	'formulaire_type_import_explication' => 'Vous pouvez fusionner le fichier de configuration avec votre configuration actuelle (les noisettes de chaque page seront ajoutées à vos noisettes déjà définies) ou bien remplacer votre configuration par celle-ci.', # NEW
+	'formulaire_type_import_explication' => 'You can merge the configuration file with your actual configuration (the nuts of each page will be added to your already defined nuts) or you can replace your configuration by this one.',
 
 	// I
 	'ieconfig_ne_pas_importer' => 'Do not import',
-	'ieconfig_noizetier_export_explication' => 'Exportera la configuration des noisettes et les compositions du noiZetier.', # NEW
+	'ieconfig_noizetier_export_explication' => 'Will export the configuration of the nuts and compositions of the Nuts Manager.',
 	'ieconfig_noizetier_export_option' => 'Included in the export?',
-	'ieconfig_non_installe' => '<b>Plugin Importeur/Exporteur de configurations :</b> ce plugin n\'est pas installés sur votre site. Il n\'est pas nécessaire au fonctionnement du noizetier. Cependant, s\'il est activé, vous pourrez exportez et importers des configurations de noisettes dans le noizetier.', # NEW
-	'ieconfig_probleme_import_config' => 'Un problème a été rencontré lors de l\'importation de la configuration du noiZetier.', # NEW
+	'ieconfig_non_installe' => '<b>Configuration Import/Export Plugin:</b> this plugin isn\'t installed on your site. It is not necessarry to the correct working of the Nuts Manager. However, when it\'s activated, you can  export and import some nuts configurations into the Nuts Manager.',
+	'ieconfig_probleme_import_config' => 'A problem occured while importing the Nuts Manager configuration.',
 	'info_composition' => 'COMPOSITION:',
-	'info_page' => 'PAGE :',
-	'installation_tables' => 'Tables du plugin noiZetier installées.<br />', # NEW
+	'info_page' => 'PAGE:',
+	'installation_tables' => 'Installed tables of the Nuts Manager Plugin.<br />',
 	'item_titre_perso' => 'custom title',
 
 	// L
@@ -95,16 +95,16 @@ nuts on the following pages:',
 
 	// N
 	'ne_pas_definir_d_heritage' => 'Do not define heritage',
-	'noisettes_composition' => 'Noisettes spécifiques à la composition <i>@composition@</i> :', # NEW
-	'noisettes_page' => 'Noisettes spécifiques à la page <i>@type@</i> :', # NEW
+	'noisettes_composition' => 'Specific nuts to the composition <i>@composition@</i>:',
+	'noisettes_page' => 'Specific nuts to the page <i>@type@</i>:',
 	'noisettes_toutes_pages' => 'Common nuts to all pages:',
-	'noizetier' => 'noiZetier', # NEW
+	'noizetier' => 'Nuts Manager',
 	'nom_bloc_contenu' => 'Content',
 	'nom_bloc_extra' => 'Extra',
 	'nom_bloc_navigation' => 'Navigation',
 	'nom_bloctexte' => 'Block of free text',
 	'non' => 'No',
-	'notice_enregistrer_rang' => 'Cliquez sur Enregistrer pour sauvegarder l\'ordre des noisettes.', # NEW
+	'notice_enregistrer_rang' => 'Clic on Save to store the nuts order.',
 
 	// O
 	'oui' => 'Yes',
@@ -114,7 +114,7 @@ nuts on the following pages:',
 
 	// W
 	'warning_noisette_plus_disponible' => 'WARNING: this nut is no longer available.',
-	'warning_noisette_plus_disponible_details' => 'Le squelette de cette noisette (<i>@squelette@</i>) n\'est plus accessible. Il se peut qu\'il s\'agisse d\'une noisette nécessitant un plugin que vous avez désactivé ou désinstallé.' # NEW
+	'warning_noisette_plus_disponible_details' => 'The template of the nut (<i>@squelette@</i>) is no longer available. It may be a nut requiring a plugin that you have disabled or uninstalled.'
 );
 
 ?>
