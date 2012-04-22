a146c8cf1955cb05643ed0e5cc161d30c04f719d
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-04-22 08:31:46 +0000

    langues (commandes)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@60540 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- commandes/trunk/lang/commandes_en.php
+++ commandes/trunk/lang/commandes_en.php
@@ -9,39 +9,39 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
 	'action_facture' => 'Invoice',
-	'action_modifier' => 'Modify', # MODIF
+	'action_modifier' => 'Modify',
 	'action_supprimer' => 'Delete',
 	'adresse_facturation' => 'Billing address',
 	'adresse_livraison' => 'Shipping address',
 
 	// B
-	'bonjour' => 'Hello', # MODIF
+	'bonjour' => 'Hello',
 
 	// C
 	'commande_client' => 'Customer',
 	'commande_date' => 'Order date', # MODIF
 	'commande_date_paiement' => 'Payment date', # MODIF
-	'commande_editer' => 'Edit the order', # MODIF
-	'commande_modifier' => 'Modify the order:', # MODIF
+	'commande_editer' => 'Edit the order',
+	'commande_modifier' => 'Modify the order:',
 	'commande_montant' => 'Amount',
-	'commande_nouvelle' => 'New order', # MODIF
-	'commande_numero' => 'Order No. ', # MODIF
+	'commande_nouvelle' => 'New order',
+	'commande_numero' => 'Order No. ',
 	'commande_reference' => 'Reference',
-	'commande_reference_numero' => 'Reference No. ', # MODIF
+	'commande_reference_numero' => 'Reference No. ',
 	'commande_statut' => 'Status',
-	'commandes_titre' => 'Orders', # MODIF
-	'configurer_titre' => 'Configure the plugin "Commande"', # MODIF
+	'commandes_titre' => 'Orders',
+	'configurer_titre' => 'Configure the plugin "Commande"',
 	'confirmer_supprimer_commande' => 'Can you confirm the deletion of this order?',
-	'contact_label' => 'Contact:', # MODIF
+	'contact_label' => 'Contact:',
 	'contenu' => 'Content',
 
 	// D
-	'date_commande_label' => 'Date of creation:', # MODIF
-	'date_envoi_label' => 'Date of dispatch:', # MODIF
-	'date_maj_label' => 'Date of update:', # MODIF
-	'date_paiement_label' => 'Date of payment:', # MODIF
+	'date_commande_label' => 'Date of creation:',
+	'date_envoi_label' => 'Date of dispatch:',
+	'date_maj_label' => 'Date of update:',
+	'date_paiement_label' => 'Date of payment:',
 	'designation' => 'Designation',
-	'details_commande' => 'Details of the order:', # MODIF
+	'details_commande' => 'Details of the order:',
 
 	// E
 	'etat' => 'Status',
@@ -49,81 +49,81 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// F
 	'facture_date' => 'Date: <span>@date@</span>',
 	'facture_num' => 'Invoice n° <span>@num@</span>',
-	'facture_titre' => 'Invoice', # MODIF
+	'facture_titre' => 'Invoice',
 
 	// I
-	'info_numero_commande' => 'ORDER NUMBER:', # MODIF
+	'info_numero_commande' => 'ORDER NUMBER:',
 	'info_toutes_commandes' => 'All orders',
 
 	// L
-	'label_recherche' => 'Search', # MODIF
+	'label_recherche' => 'Search',
 
 	// M
-	'merci_de_votre_commande' => 'We have registered your order and we appreciate your trust.', # MODIF
-	'modifier_commande_statut' => 'This order is:', # MODIF
+	'merci_de_votre_commande' => 'We have registered your order and we appreciate your trust.',
+	'modifier_commande_statut' => 'This order is:',
 	'montant' => 'Amount',
 
 	// N
 	'nom_bouton_plugin' => 'Orders',
-	'notifications_activer_explication' => 'Send email notifications of order ?', # MODIF
-	'notifications_activer_label' => 'Activate', # MODIF
-	'notifications_cfg_titre' => 'Notifications', # MODIF
-	'notifications_client_explication' => 'Send the notifications to the customer ?', # MODIF
-	'notifications_client_label' => 'Client', # MODIF
-	'notifications_expediteur_administrateur_label' => 'Select an administrator:', # MODIF
-	'notifications_expediteur_choix_administrateur' => 'an administrator', # MODIF
-	'notifications_expediteur_choix_email' => 'an e-mail', # MODIF
-	'notifications_expediteur_choix_facteur' => 'idem plugin Facteur', # MODIF
-	'notifications_expediteur_choix_webmaster' => 'a webmaster', # MODIF
+	'notifications_activer_explication' => 'Send email notifications of order ?',
+	'notifications_activer_label' => 'Activate',
+	'notifications_cfg_titre' => 'Notifications',
+	'notifications_client_explication' => 'Send the notifications to the customer?',
+	'notifications_client_label' => 'Client',
+	'notifications_expediteur_administrateur_label' => 'Select an administrator:',
+	'notifications_expediteur_choix_administrateur' => 'an administrator',
+	'notifications_expediteur_choix_email' => 'an e-mail',
+	'notifications_expediteur_choix_facteur' => 'idem plugin Facteur',
+	'notifications_expediteur_choix_webmaster' => 'a webmaster',
 	'notifications_expediteur_email_label' => 'Enter an e-mail:', # MODIF
-	'notifications_expediteur_explication' => 'Choose the sender notifications for the seller and buyer', # MODIF
-	'notifications_expediteur_label' => 'Sender', # MODIF
-	'notifications_expediteur_webmaster_label' => 'Select a webmaster:', # MODIF
-	'notifications_explication' => 'Notifications are used to send emails after changes in order status: Pending, ongoing, sent, partially paid, paid, returned, partially return', # MODIF
-	'notifications_parametres' => 'Parameters of notifications', # MODIF
-	'notifications_quand_explication' => 'What(s) status change(s) triggers(s) sending a notification?', # MODIF
-	'notifications_quand_label' => 'Outbreak', # MODIF
-	'notifications_vendeur_administrateur_label' => 'Select one or more administrators:', # MODIF
-	'notifications_vendeur_choix_administrateur' => 'one or more administrators', # MODIF
-	'notifications_vendeur_choix_email' => 'one or more emails', # MODIF
-	'notifications_vendeur_choix_webmaster' => 'one or more webmasters', # MODIF
-	'notifications_vendeur_email_explication' => 'Enter one or more email separated by commas:', # MODIF
-	'notifications_vendeur_email_label' => 'Email(s) :', # MODIF
-	'notifications_vendeur_explication' => 'Choose (the) recipient(s) of the notifications for shipments to the seller', # MODIF
-	'notifications_vendeur_label' => 'Seller', # MODIF
-	'notifications_vendeur_webmaster_label' => 'Select one or more webmasters:', # MODIF
+	'notifications_expediteur_explication' => 'Choose the sender notifications for the seller and buyer',
+	'notifications_expediteur_label' => 'Sender',
+	'notifications_expediteur_webmaster_label' => 'Select a webmaster:',
+	'notifications_explication' => 'Notifications are used to send emails after changes in order status: Pending, ongoing, sent, partially paid, paid, returned, partially return',
+	'notifications_parametres' => 'Parameters of notifications',
+	'notifications_quand_explication' => 'Which are changes of status that will send a notification?',
+	'notifications_quand_label' => 'Outbreak',
+	'notifications_vendeur_administrateur_label' => 'Select one or more administrators:',
+	'notifications_vendeur_choix_administrateur' => 'one or more administrators',
+	'notifications_vendeur_choix_email' => 'one or more emails',
+	'notifications_vendeur_choix_webmaster' => 'one or more webmasters',
+	'notifications_vendeur_email_explication' => 'Enter one or more email separated by commas:',
+	'notifications_vendeur_email_label' => 'Email(s) :',
+	'notifications_vendeur_explication' => 'Choose (the) recipient(s) of the notifications for shipments to the seller',
+	'notifications_vendeur_label' => 'Seller',
+	'notifications_vendeur_webmaster_label' => 'Select one or more webmasters:',
 
 	// P
-	'parametres_cfg_titre' => 'Parameters', # MODIF
-	'parametres_duree_vie_explication' => 'Enter the lifetime of a command with the current status (in seconds)', # MODIF
-	'parametres_duree_vie_label' => 'Lifetime', # MODIF
-	'passer_la_commande' => 'Checkout', # MODIF
+	'parametres_cfg_titre' => 'Parameters',
+	'parametres_duree_vie_explication' => 'Enter the lifetime of a command with the current status (in seconds)',
+	'parametres_duree_vie_label' => 'Lifetime',
+	'passer_la_commande' => 'Checkout',
 
 	// R
-	'recapitulatif' => 'Order summary:', # MODIF
+	'recapitulatif' => 'Order summary:',
 	'reference' => 'Reference',
-	'reference_label' => 'Reference:', # MODIF
-	'reference_ref' => 'Reference @ref@', # MODIF
+	'reference_label' => 'Reference:',
+	'reference_ref' => 'Reference @ref@',
 
 	// S
-	'simuler' => 'Simulate the status change', # MODIF
+	'simuler' => 'Simulate the status change',
 	'statut_attente' => 'Pending',
 	'statut_encours' => 'Ongoing',
 	'statut_envoye' => 'Sent',
-	'statut_erreur' => 'Error', # MODIF
-	'statut_label' => 'Status:', # MODIF
+	'statut_erreur' => 'Error',
+	'statut_label' => 'Status:',
 	'statut_partiel' => 'Partially paid',
 	'statut_paye' => 'Paid',
 	'statut_retour' => 'Returned',
 	'statut_retour_partiel' => 'Partially return',
-	'supprimer' => 'Delete', # MODIF
+	'supprimer' => 'Delete',
 
 	// U
-	'une_commande_de' => 'An order of: ', # MODIF
-	'une_commande_sur' => 'An order on @nom@', # MODIF
+	'une_commande_de' => 'An order of: ',
+	'une_commande_sur' => 'An order on @nom@',
 
 	// V
-	'votre_commande_sur' => 'Your order on @nom@' # MODIF
+	'votre_commande_sur' => 'Your order on @nom@'
 );
 
 ?>
