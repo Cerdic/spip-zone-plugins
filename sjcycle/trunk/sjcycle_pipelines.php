cf697e9d358eec31f0bbc9391ff41e7935bf30a6
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-04-23 17:05:31 +0000

    Pas besoin d'urls absolues sur les css et scripts...
    On permet un env par pour le tri sur le modèle...
    
    On permet un env limite également sur le modèle
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@60577 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- sjcycle/trunk/sjcycle_pipelines.php
+++ sjcycle/trunk/sjcycle_pipelines.php
@@ -10,23 +10,21 @@ function sjcycle_insert_head_css($flux){
 		$done = true;
 		$conf_jcycle = lire_config('sjcycle');
 		if($conf_jcycle["tooltip"]) {
-			$flux .="\n".'<link rel="stylesheet" href="'.url_absolue(find_in_path('javascript/jquery.tooltip.css')).'" type="text/css" media="all" />';
+			$flux .="\n".'<link rel="stylesheet" href="'.find_in_path('javascript/jquery.tooltip.css').'" type="text/css" media="all" />';
 		}
-		$flux .= '<link rel="stylesheet" href="'.url_absolue(generer_url_public('sjcycle.css')).'" type="text/css" media="all" />';
+		$flux .= '<link rel="stylesheet" href="'.generer_url_public('sjcycle.css').'" type="text/css" media="all" />';
 	}
-
 	return $flux;
 }
 
 function sjcycle_insert_head($flux){
 	$conf_jcycle = lire_config('sjcycle');
-	$flux .="\n".'<script src="'.url_absolue(find_in_path('javascript/jquery.cycle.all.js')).'" type="text/javascript"></script>';
-	if($conf_jcycle["tooltip"]) {
-		$flux .="\n".'<script src="'.url_absolue(find_in_path('javascript/jquery.tooltip.js')).'" type="text/javascript" charset="utf-8"></script>';
+	$flux .="\n".'<script src="'.find_in_path('javascript/jquery.cycle.all.js').'" type="text/javascript"></script>';
+	if($conf_jcycle['tooltip']) {
+		$flux .="\n".'<script src="'.find_in_path('javascript/jquery.tooltip.js').'" type="text/javascript" charset="utf-8"></script>';
 	}
-
+	
 	$flux .= sjcycle_insert_head_css(''); // compat pour les vieux spip
-
 	return $flux;
 }
 
