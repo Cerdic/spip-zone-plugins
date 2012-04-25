f99facb83fa5d958529deaa738e84f8e90fdde0c
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-04-25 08:20:39 +0000

    langues (seo)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@60635 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- seo/lang/seo_en.php
+++ seo/lang/seo_en.php
@@ -1,26 +1,24 @@
 <?php
-/**
-* BouncingOrange SPIP SEO plugin
-*
-* @category   SEO
-* @package    SPIP_SEO
-* @author     Pierre ROUSSET (p.rousset@gmail.com)
-* @copyright  Copyright (c) 2009 BouncingOrange (http://www.bouncingorange.com)
-* @license    http://opensource.org/licenses/gpl-2.0.php  General Public License (GPL 2.0)
-*/
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org/tradlang_module/seo?lang_cible=en
+// ** ne pas modifier le fichier **
 
-if (!defined("_ECRIRE_INC_VERSION")) return;
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// S
+	'S.E.O' => 'SEO',
+
 	// A
 	'alexa' => 'Alexa',
 	'alexa_activate' => 'Activate Alexa',
 	'alexa_id' => 'Website Alexa\'s ID',
-	
+
 	// C
 	'canonical_url' => 'Canonical URL',
 	'canonical_url_activate' => 'Activate Canonical URL',
-	
+
 	// G
 	'google_analytics' => 'Google Analytics',
 	'google_analytics_activate' => 'Activate Analytics',
@@ -28,35 +26,33 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'google_webmaster_tools' => 'Google Webmaster Tools',
 	'google_webmaster_tools_activate' => 'Activate Webmaster Tools',
 	'google_webmaster_tools_id' => 'Meta Code verification',
-	
+
 	// I
-	'insert_head'=>"Automatic insertion in #INSERT_HEAD",
-	'insert_head_descriptif'=>"Automatic insertion of SEO configuration in &lt;header&gt;",
-	'insert_head_activate'=>"Activate automatic insertion",
-	
+	'insert_head' => 'Automatic insertion in #INSERT_HEAD',
+	'insert_head_activate' => 'Activate automatic insertion',
+	'insert_head_descriptif' => 'Automatic insertion of SEO configuration in &lt;header&gt;',
+
 	// M
 	'meta_author' => 'Author:',
 	'meta_copyright' => 'Copyright:',
 	'meta_description' => 'Description:',
 	'meta_keywords' => 'Keywords:',
-	'meta_page_title_sommaire_value' => 'Page Title Value + Home Page Meta Value',
-	'meta_page_title_value' => 'Page Title Value',
 	'meta_page_description_sommaire_value' => 'Page Description Value + Home Page Meta Value',
 	'meta_page_description_value' => 'Page Description Value',
+	'meta_page_title_sommaire_value' => 'Page Title Value + Home Page Meta Value',
+	'meta_page_title_value' => 'Page Title Value',
 	'meta_robots' => 'Robots:',
 	'meta_sommaire_value' => 'Home Page Meta Value',
-	'meta_tags' => 'Meta Tags',	
+	'meta_tags' => 'Meta Tags',
 	'meta_tags_activate' => 'Activate meta tags',
 	'meta_tags_default' => 'Defaults Meta Tag (for Articles and Sections)',
 	'meta_tags_edit_activate' => 'Activate meta tags editing in sections and articles',
 	'meta_tags_editing' => 'Meta Tags Editing',
 	'meta_tags_sommaire' => 'Home Page Meta Tags',
 	'meta_title' => 'Title:',
-	
+
 	// S
-	'S.E.O' => 'SEO',
-	'seo' => 'Search Engine Optimisation',
-	
+	'seo' => 'Search Engine Optimisation'
 );
 
 ?>
--- seo/lang/seo_fr.php
+++ seo/lang/seo_fr.php
@@ -1,39 +1,35 @@
 <?php
-/**
-* BouncingOrange SPIP SEO plugin
-*
-* @category   SEO
-* @package    SPIP_SEO
-* @author     Pierre ROUSSET (p.rousset@gmail.com)
-* @copyright  Copyright (c) 2009 BouncingOrange (http://www.bouncingorange.com)
-* @license    http://opensource.org/licenses/gpl-2.0.php  General Public License (GPL 2.0)
-*/
-
-if (!defined("_ECRIRE_INC_VERSION")) return;
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/seo/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// S
+	'S.E.O' => 'SEO',
+
 	// A
 	'alexa' => 'Alexa',
 	'alexa_activate' => 'Activer Alexa',
 	'alexa_id' => 'Identifiant du site pour Alexa',
-	
+
 	// C
 	'canonical_url' => 'URL Canoniques',
 	'canonical_url_activate' => 'Activer les URL Canoniques',
-	
+
 	// G
 	'google_analytics' => 'Google Analytics',
 	'google_analytics_activate' => 'Activer Analytics',
 	'google_analytics_id' => 'Google Analytics web property ID',
 	'google_webmaster_tools' => 'Google Webmaster Tools',
 	'google_webmaster_tools_activate' => 'Activer Webmaster Tools',
-	'google_webmaster_tools_id' => 'Meta code de v&eacute;rification',
-	
+	'google_webmaster_tools_id' => 'Meta code de vérification',
+
 	// I
-	'insert_head'=>"Insertion automatique dans #INSERT_HEAD",
-	'insert_head_descriptif'=>"Insertion automatique de la configuration SEO dans le &lt;header&gt;",
-	'insert_head_activate'=>"Activer l'insertion automatique",
-	
+	'insert_head' => 'Insertion automatique dans #INSERT_HEAD',
+	'insert_head_activate' => 'Activer l\'insertion automatique',
+	'insert_head_descriptif' => 'Insertion automatique de la configuration SEO dans le &lt;header&gt;',
+
 	// M
 	'meta_author' => 'Auteur :',
 	'meta_copyright' => 'Copyright :',
@@ -41,22 +37,20 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'meta_keywords' => 'Mots Clefs :',
 	'meta_page_description_sommaire_value' => 'Valeur de la Description de la Page + Valeur Meta du Sommaine',
 	'meta_page_description_value' => 'Valeur de la Description de la Page',
-	'meta_page_title_value' => 'Valeur du Titre de la Page',
 	'meta_page_title_sommaire_value' => 'Valeur du Titre de la Page + Valeur Meta du Sommaire',
+	'meta_page_title_value' => 'Valeur du Titre de la Page',
 	'meta_robots' => 'Robots :',
 	'meta_sommaire_value' => 'Valeur de la Meta du Sommaire',
-	'meta_tags' => 'Meta Tags',	
+	'meta_tags' => 'Meta Tags',
 	'meta_tags_activate' => 'Activer les meta tags',
-	'meta_tags_default' => 'Valeur des Meta Tags par D&eacute;faut (pour les Articles et les Rubriques)',
-	'meta_tags_edit_activate' => 'Activer l\'&eacute;dition des meta tags dans les rubriques et les articles',
-	'meta_tags_editing' => 'Edition des Meta Tags',	
+	'meta_tags_default' => 'Valeur des Meta Tags par Défaut (pour les Articles et les Rubriques)',
+	'meta_tags_edit_activate' => 'Activer l\'édition des meta tags dans les rubriques et les articles',
+	'meta_tags_editing' => 'Edition des Meta Tags',
 	'meta_tags_sommaire' => 'Valeur des Meta Tags du Sommaire (page d\'accueil)',
 	'meta_title' => 'Titre :',
-	
-	// S
-	'S.E.O' => 'SEO',
-	'seo' => 'Search Engine Optimisation',
 
+	// S
+	'seo' => 'Search Engine Optimisation'
 );
 
 ?>
