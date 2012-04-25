858c9877d6a3fb00a5ba66157f18f79f1bf5d16d
davoodhossein@yahoo.com <davoodhossein@yahoo.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-04-25 06:37:30 +0000

    langues (minical)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@60623 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ calendrier_mini/trunk/lang/minical_fa.php
@@ -0,0 +1,27 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org/tradlang_module/minical?lang_cible=fa
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'aucune_date' => 'اين ماه برنامه‌اي نيست',
+
+	// D
+	'date_jour_abbr_1' => 'يك‌شنبه.',
+	'date_jour_abbr_2' => 'دوشنبه.',
+	'date_jour_abbr_3' => 'سه‌شنبه.',
+	'date_jour_abbr_4' => 'چهارشنبه.',
+	'date_jour_abbr_5' => 'پنجشنبه.',
+	'date_jour_abbr_6' => 'جمعه.',
+	'date_jour_abbr_7' => 'شنبه.',
+
+	// M
+	'mois_precedent' => 'ماه قبل',
+	'mois_suivant' => 'ماه بعد'
+);
+
+?>
