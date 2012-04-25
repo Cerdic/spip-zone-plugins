95aace4787dbee7c80d4e3effac6f0f486dbfd94
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-04-25 10:30:29 +0000

    Un paquet.xml et un état en dev, vu que rien ne marche pour l'instant.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@60650 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ numerotation/trunk/lang/paquet-numero_fr.php
@@ -0,0 +1,19 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-numero
+// Langue: fr
+// Date: 25-04-2012 12:25:32
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// N
+	'numero_description' => 'Ce plugin permet d\'un clic de numéroter/re-numéroter/dé-numéroter tous les articles ou sous-rubriques d\'une rubrique.',
+	'numero_slogan' => 'Gérer facilement la numérotation des articles et rubriques',
+);
+?>
