ef1dc448512094e22b524343728b6be644a5f03a
amaury.adon@laposte.net <amaury.adon@laposte.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-04-27 17:18:44 +0000

    Ajout des groupes de mots clés et des mots clés à l'installation. Ca y est ! Toute la procédure d'install est correcte !
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@60718 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- seminaire/trunk/seminaire_administrations.php
+++ seminaire/trunk/seminaire_administrations.php
@@ -19,11 +19,44 @@ function seminaire_upgrade($nom_meta_base_version, $version_cible) {
 
 	cextras_api_upgrade(seminaire_declarer_champs_extras(), $maj['create']);
 	/**activer les mots clés et leur configuration avancée s'ils ne le sont pas déjà**/
-	 if ($GLOBALS['meta']['articles_mots']!=oui){
+	if ($GLOBALS['meta']['articles_mots']!=oui){
 	 	ecrire_meta("articles_mots", "oui");
 	 	ecrire_meta("config_precise_groupes", "oui");
 	 	ecrire_meta("documents_objets", "spip_evenements");	
 	 	}
+ 	/**Creer le groupe de mots clés Type pour les types d'événements**/
+	if (sql_countsel('spip_mots', "titre IN ('seminaire','groupe de travail','evenement important')") == 0) 
+   	{
+			$id_groupe = sql_insertq('spip_groupes_mots', 
+			array('titre'=>'Type', 'descriptif'=>_T('seminaire:mots_cles_techniques_kitcnrs'),'tables_liees'=>'evenements', 'minirezo'=>'oui','comite'=>'oui')
+                 );
+	if (sql_error() != '') die((_T('seminaire:erreur_install_groupe_technique ')).sql_error());
+       
+        $Tstatuts = array('séminaire','groupe de travail','événement important');
+	foreach ($Tstatuts as $st) 
+		{
+		sql_insertq('spip_mots', 
+			array('titre'=>$st, 'descriptif'=>$st, 'id_groupe'=>$id_groupe, 'type'=>'Type')
+				);
+		if (sql_error() != '') $Terreur[] = (_T('erreur_creation_mot_cle')).$st.': '.sql_error();
+    	};
+	};
+	/** création du groupe de mots clés Catégorie et de ses mots cles pours les équipes **/
+    if (sql_countsel('spip_mots', "titre IN ('Algèbre, Dynamique et Topologie','Analyse Appliquée', 'Analyse et Géométrie', 'FRUMAM', 'Géométrie et Singularités', 'Guide d’ondes et milieux stratiﬁés', 'Probabilités et statistiques', 'Séminaire des doctorants', 'Théorie des nombres')") == 0) 
+    {
+        $id_groupe = sql_insertq('spip_groupes_mots',array('titre'=>'Catégorie', 'descriptif'=> _T('seminaire:mots_cles_categories'), 'tables_liees'=>'articles', 'minirezo'=>'oui','comite'=>'oui')
+                  );
+        if (sql_error() != '') die((_T('seminaire:erreur_install_groupe_coordonnees')).sql_error());
+        
+        $Tstatuts = array('Algèbre, Dynamique et Topologie','Analyse Appliquée', 'Analyse et Géométrie', 'FRUMAM', 'Géométrie et Singularités', 'Guide d’ondes et milieux stratiﬁés', 'Probabilités et statistiques', 'Séminaire des doctorants', 'Théorie des nombres');
+        foreach ($Tstatuts as $st) {
+          sql_insertq('spip_mots', 
+              array('titre'=>$st, 'id_groupe'=>$id_groupe, 'type'=>'Catégorie')
+                  );
+          if (sql_error() != '') $Terreurs[] = (_T('erreur_creation_mot_cle')).$st.': '.sql_error();
+        }
+    }
+
 	include_spip('base/upgrade');
 	maj_plugin($nom_meta_base_version, $version_cible, $maj);
 }
