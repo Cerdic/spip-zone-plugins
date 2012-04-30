17decf75e6250058d11adae312c6dfee0ba6138d
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-04-30 08:28:25 +0000

    langues (paquet-calendriermini)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@60790 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- calendrier_mini/trunk/lang/paquet-calendriermini_en.php
+++ calendrier_mini/trunk/lang/paquet-calendriermini_en.php
@@ -8,10 +8,8 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// C
-	'calendriermini_description' => 'Allow to use a #CALENDRIER_MINI tag.
-_ This tag displays a calendar designed as dotclear\'s one and therefore compatible with the styles from this blog system.
-_ Other tools are added, such as tags, criteria, models...
-_ Icon from [Tempest->http://tempest.deviantart.com/] under CC BY-NC-ND license.', # MODIF
+	'calendriermini_description' => '#CALENDRIER_MINI displays a calendar designed as dotclear\'s one and therefore compatible with the styles from this blog system.
+_ Other tools are added, such as tags, criteria, models...',
 	'calendriermini_slogan' => 'Allow to use a #CALENDRIER_MINI tag'
 );
 
