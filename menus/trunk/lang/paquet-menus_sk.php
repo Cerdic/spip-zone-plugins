ed9f4aad3816d2393a44f77eb25bfab7d5be5d45
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-04-30 08:28:35 +0000

    langues (paquet-menus)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@60798 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- menus/trunk/lang/paquet-menus_sk.php
+++ menus/trunk/lang/paquet-menus_sk.php
@@ -8,13 +8,13 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// M
-	'menus_description' => 'Lorsqu\'on ne se sert pas du plugin {{Menus}}, on est obligé de définir tous ses menus dans les squelettes,
-	ce qui fait que les administrateurs du site n\'ont pas directement la main dessus et que lorsqu\'ils veulent y changer quelque chose, ils sont obligés
-	d\'en faire la demande à la personne en charge des squelettes. De plus, lorsqu\'on veut des liens statiques (un lien vers un article précis,
-	ou vers une page précise, ou vers un site externe) il faut les écrire en dur dans le squelette du menu.
+	'menus_description' => 'Keď sa nepoužíva zásuvný modul{{Menus,}} všetky šablóny musia byť definované v ich menu,
+	čiže administrátori stránky to nemajú úplne pod kontrolou a ak chcú niečo zmeniť, musia sa obrátiť 
+	na človeka, ktorý je zodpovedný za šablóny. Okrem toho ak chcete pridať statické odkazy (odkaz na konkrétny článok
+	alebo na konkrétnu stránku, alebo na externú stránku), tie musia byť zapísané do šablóny menu.
 
-	Le but du plugin {{Menus}} est donc de permettre de facilement élaborer des menus au moyen d\'une interface conviviale, directement dans la partie privée.
-	{{Attention !}} Ce plugin ne s\'occupe pas de la manière dont seront affichés les menus. Il permet de les créer facilement et d\'en générer le code HTML.', # NEW
+	Cieľom zásuvného modulu {{Menus}} je umožniť vám ľahko vytvárať menu pomocou intuitívneho rozhrania priamo do súkromnej zóny.
+	{{Pozor!}} Tento zásuvný modul sa nezaoberá tým, ako sa položky menu zobrazia. Umožňuje  ľahké vytváranie menu a vygenerovanie kódu HTML.',
 	'menus_slogan' => 'Vytvorte svoje vlastné ponuky menu.',
 	'menus_titre' => 'Menu'
 );
