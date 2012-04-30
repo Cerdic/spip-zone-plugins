da32d85920a4d77213ebc5981de3afab388aa4a6
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-04-30 08:28:34 +0000

    langues (faq-manuelsite)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@60797 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- manuel_site/lang/faq-manuelsite_sk.php
+++ manuel_site/lang/faq-manuelsite_sk.php
@@ -8,17 +8,17 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// F
-	'forum' => 'Les forums sont activés par défaut sur vos articles @complement@ ; ils sont désactivables au cas par cas... Les visiteurs peuvent donc réagir à vos articles... Vous serez prévenu par mail à chaque fois qu’un message est posté sur un de vos articles. Petit revers de médaille : les spams qui ne sont pas toujours évident à repérer et que vous devrez parfois gérer manuellement. Pour traiter un message de forum (le supprimer s’il ne vous plait pas ou le signaler comme spam si c’en est un) :
--* Dans le site public, sur la page de l’article, si vous êtes identifiés, il y a 2 boutons "Supprimer ce Message" ou "SPAM"
--* Dans l’espace privé, via le menu Activité / Suivre les Forums', # NEW
+	'forum' => 'Diskusné fóra sú podľa predvolených nastavení aktivované v článkoch  @complement@; dajú sa deaktivovať jedno po druhom. Návštevníci môžu reagovať na vaše články. Ak niekto pošle príspevok k vášmu článku, dostanete o tom oznam cez e-mail.  Malé negatívum: spamy, ktoré nie je vždy ľahké nájsť a ktoré budete musieť usmerniť manuálne. Ak chcete spracovať príspevok v diskusnom fóre (vymazať ho, ak sa vám nepáči, alebo ho označiť ako spam, ak to je spam):
+-* Ak ste prihlásený (-á), na verejne prístupnej stránke na stránke s článkom sú dve tlačidlá "Odstrániť príspevok" alebo "SPAM"
+-* V súkromnej zóne cez menu Aktivita / Riadiť diskusné fóra',
 	'forum_q' => 'Ako riadiť diskusné fóra?',
 
 	// I
-	'img' => 'Il n’y a pas de « bonne » taille pour afficher une image dans un article. En tout cas, inutile d’envoyer une image de 3000 pixels de large, aucun écran ne pourra l’afficher dans son intégralité ! Sauf si le document est destiné à l’impression.
--* Si l’image doit être intégrée au texte d’un article, tout dépend de son contenu : si c’est un portrait, une hauteur de 200px devrait suffire sinon gare aux rides ; si c’est un beau paysage, on peut aller jusqu’à {{@largeur_max@}} pixels max de large.
--* Si l’image est prévue pour le porte-folio d’un article, ne pas dépasser 1000 pixels de large ou 600 pixels de haut.
+	'img' => 'Neexistuje žiadna "správna" veľkosť na zobrazenie obrázka v článku. V každom prípade netreba posielať obrázok široký 3 000 pixelov, lebo ho žiadna obrazovka nezobrazí naraz! Výnimkou je prípad, keď je dokument určený na tlač.
+-* Ak bude obrázok zaradený do textu článku, všetko závisí od jeho vlastností: ak je orientovaný na výšku, výška 200 px by mala stačiť, v opačnom prípade treba dať pozor na nedokonalosti v zobrazení; ak je orientovaný na šírku, môže mať maximálnu šírku až {{@largeur_max@}} pixelov.
+-* Ak bude obrázok súčasťou portfólia k článku, neodporúča sa prekročiť šírku 1 000 pixelov a výšku 600 pixelov.
 
-{Attention, le poids max à ne pas dépasser est de 150M sans quoi le téléchargement sera refusé}.', # NEW
+{Pozor, maximálna veľkosť nesmie prekročiť 150 MB, inak sa obrázok nebude dať stiahnuť.}',
 	'img_nombre' => 'Do článku sa dá do článku poslať viac fotiek jedným klikom:
 -* Vybrané fotky skopírujte do priečinka na svojom pevnom disku
 -* Upravte ich veľkosť
@@ -30,15 +30,15 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'img_q' => 'Akú veľkosť by mala mať moja fotka?',
 
 	// S
-	'son' => 'Préparer votre son au format mp3 en mono avec une fréquence de 11 ou 22 kHz et un bitrate (taux de compression) de 64kbps (ou plus si vous désirez une qualité supérieure).
+	'son' => 'Uložte si zvukový záznam vo formáte mp3 v režime mono s frekvenciou 11 alebo 22 kHz a bitovou rýchlosťou (rýchlosť pri kompresii) 64 kb/s (alebo viac, ak chcete vyššiu kvalitu).
 	
-Associer le fichier mp3 à votre article comme pour une image et lui donner un titre et éventuellement une description et un crédit. Enfin placer dans le corps de votre article à l’endroit souhaité <code><docXX|center|player></code>. Un lecteur flash apparaîtra alors dans votre site public pour permettre au visiteur de lancer le son. 
-_ {Attention, la taille max d’un fichier est de 150M, soit environ une durée de 225 minutes}', # NEW
-	'son_audacity' => 'Pour travailler un fichier audio, vous pouvez utiliser le logiciel Audacity (Mac, Windows, Linux) téléchargeable par ici [->http://audacity.sourceforge.net/]. Quelques astuces :
--* Après installation du logiciel, vous aurez besoin aussi de la librairie lame pour l’encodage mp3 [->http://audacity.sourceforge.net/help/faq?s=install&item=lame-mp3].
--* Pour passer le fichier en mono : Menu {Pistes/Piste stéréo vers mono}
--* Pour créer le fichier mp3 : Menu {Fichier/Exporter}
--* Pour régler le bitrate : Menu {Fichier/Exporter/Options/Qualité}', # NEW
+Súbor mp3 zaraďte do článku ako obrázok a dajte mu nadpis, prípadne pridajte popis a poďakovanie (spolu)autorom. Nakoniec ho vložte do článku na miesto, na ktoré chcete  <code><docXX|center|player></code>. Na verejne prístupnej stránke sa zobrazí ikona USB kľúča, aby návštevníci mohli zvukový záznam spustiť.
+_ {Pozor, maximálna veľkosť súboru je 150 MB alebo časový úsek približne 225 minút}',
+	'son_audacity' => 'Na prácu s audio súborom môžete využiť program Audacity (Mac, Windows, Linux), ktorý sa dá stiahnuť odtiaľto [->http://audacity.sourceforge.net/]. Niekoľko tipov:
+-* Po inštalácii programu budete potrebovať aj knižnicu na kódovanie lame mp3 [->http://audacity.sourceforge.net/help/faq?s=install&item=lame-mp3].
+-* Ak chcete súbor previesť do režimu mono: Menu {Stopy/Stereo stopy do mono}
+-* Ak chcete vytvoriť súbor mp3: Menu {Súbor/Exportovať}
+-* Ak chcete nastaviť bitovú rýchlosť: Menu {Súbor/Exportovať/Možnosti/Kvalita}',
 	'son_audacity_q' => 'Ako pripraviť súbor so zvukom?',
 	'son_q' => 'Ako pridať k článku súbor so zvukom?',
 
@@ -49,10 +49,10 @@ _ {Attention, la taille max d’un fichier est de 150M, soit environ une durée 
 	'trier_q' => 'Ako nastaviť správne poradie zobrazenia článkov, rubrík alebo pripojených súborov?',
 
 	// V
-	'video_320x240' => 'Préparer votre vidéo au format flv (streaming flash) en 320x240 pixels avec un bitrate (taux de compression) de 400kbps et un son en mono/64kbps. Pour convertir un fichier vidéo, vous pouvez utiliser le logiciel avidemux (Mac, Windows, Linux) téléchargeable par ici [->http://www.avidemux.org/]. 
+	'video_320x240' => 'Uložte svoje video vo formáte flv (streaming flash) s rozmermi 320 x 240 pixelov s bitovou rýchlosťou (rýchlosť pri kompresii) 400 kb/s a v režime mono 64 kb/s. Ak chcete súbor videa kovertovať,  môžete využiť program avidemux (Mac, Windows, Linux), ktorý si môžete stiahnuť zo stránky [www.avidemux.org/.->http://www.avidemux.org/] 
 
-Associer le fichier créé à votre article comme un document joint, lui donner un titre, éventuellement une description et un crédit, et une taille (largeur 320, hauteur 240). Enfin placer dans le corps de votre article à l’endroit souhaité <code><docXX|center|video></code>. Un lecteur flash apparaîtra alors dans votre site public pour permettre au visiteur de lancer la vidéo.
-_ {Attention, la taille max d’un fichier est de 150M, soit environ une durée de 37.5 minutes}', # NEW
+Vytvorený súbor zaraďte do článku ako pripojený dokument, dajte mu nadpis, prípadne pridajte podpis a poďakovanie autorom aj veľkosť (šírka 320, výška 240). Nakoniec ho pridajte do článku na miesto, na ktoré chcete <code><docXX|center|video></code>. Na verejne prístupnej stránke sa zobrazí ikona USB kľúča, aby si návštevníci mohli video prehrať. 
+_ {Pozor, maximálna veľkosť súboru je 150 MB alebo časový úsek približne 37,5 minúty}',
 	'video_320x240_q' => 'Ako pridať video k článku?',
 	'video_dist' => 'Ak sa vaše video nachádza na stránke DailyMotion, YouTube alebo Viméo  a máte ho na novej karte vášho prehliadača, prejdite na stránku, kde sa video nachádza a skopírujte internetovú adresu videa. Na stránke úprav svojho článku kliknite na tlačidlo "Pridať video" a prilepte jeho internetovú adresu. Potom vložte do textu článku <code><videoXX|center></code>',
 	'video_dist_q' => 'Ako k článku pridať video z dailymotion (youtube, atď.)?'
