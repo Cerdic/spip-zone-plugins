68b1a20a2540e8077355c891abf2dc042091c803
piero.wbmstr@gmail.com <piero.wbmstr@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-05-04 14:55:21 +0000

    Première version (copie) pour SPIP3
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@60911 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ spip_proprietaire/spip_3_copy/balise/textes_legaux.php
@@ -0,0 +1,80 @@
+<?php
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+function balise_TEXTES_LEGAUX($p) {
+	spip_proprio_charger_toutes_les_langues();
+	return calculer_balise_dynamique($p,TEXTES_LEGAUX,array());
+}
+
+function balise_TEXTES_LEGAUX_dyn($chaine='', $who='', $separator='<br />') {
+	include_spip('inc/presentation');
+	include_spip('spip_proprio_fonctions');
+	$conf = spip_proprio_recuperer_config();
+	static $spip_proprio_no_config = false;
+	if (is_null($conf)) {
+		include_spip('inc/autoriser');
+		if ($spip_proprio_no_config===false && autoriser('ecrire')) {
+			$div = propre( _T('proprietaire:pas_config', array(
+				'url_config' => generer_url_ecrire('spip_proprio')
+			)) );
+			echo $div;
+			$spip_proprio_no_config = true;
+		}
+		return;
+	}
+
+	$conf['nom_site'] = $GLOBALS['meta']['nom_site'];
+	$conf['url_site'] = $GLOBALS['meta']['adresse_site'];
+	$conf['descriptif_site'] = textebrut($GLOBALS['meta']['descriptif_site']);
+	$chercher_logo = charger_fonction('chercher_logo', 'inc');
+	$logo_site = $chercher_logo(0, 'id_syndic', 'on');
+	$conf['logo_site'] = $logo_site[0];
+
+	$entries = array('proprietaire', 'hebergeur', 'createur');
+	foreach($entries as $entry){
+		if( $conf[$entry.'_legal_forme'] AND strlen($conf[$entry.'_legal_forme']) ){
+			$article = ($conf[$entry.'_legal_genre'] == 'fem') ? _T('spipproprio:la') : _T('spipproprio:le');
+			$conf[$entry.'_forme']	= 
+				($conf[$entry.'_legal_abbrev'] AND strlen($conf[$entry.'_legal_abbrev'])) ? 
+					apostrophe($conf[$entry.'_legal_abbrev'], $article) : 
+						apostrophe($conf[$entry.'_legal_forme'], $article);
+		}
+		else $conf[$entry.'_forme']	= '';
+
+		$conf[$entry.'_web'] = 
+			( $conf[$entry.'_site_web'] AND strlen($conf[$entry.'_site_web'])>7 ) ? 
+				_T('texteslegaux:reportez_vous_au_site', array('site'=>$conf[$entry.'_site_web'])) : '';
+
+		$conf[$entry.'_mail_texte'] = 
+			( $conf[$entry.'_mail'] AND strlen($conf[$entry.'_mail']) ) ? 
+				$separator._T('texteslegaux:pour_les_contacter', array('mail'=>$conf[$entry.'_mail'])) : '';
+
+		$conf[$entry.'_fonction_responsable_texte'] = 
+			( $conf[$entry.'_fonction_responsable'] AND strlen($conf[$entry.'_fonction_responsable']) ) ? 
+				' ('.$conf[$entry.'_fonction_responsable'].')' : '';
+	}
+
+	$conf['type_serveur_texte'] = 
+		( $conf['type_serveur'] AND strlen($conf['type_serveur']) ) ? 
+			_T('texteslegaux:sur_un_serveur', array('serveur'=>$conf['type_serveur'])) : '';
+
+	$conf['os_serveur_texte'] = 
+		( $conf['os_serveur'] AND strlen($conf['os_serveur']) ) ? 
+			_T('texteslegaux:os_du_serveur', array(
+				'os_serveur'=> ($conf['os_serveur_web'] AND strlen($conf['os_serveur_web'])) ?
+					'<a href="'.$conf['os_serveur_web'].'" class="spip_out">'.$conf['os_serveur'].'</a>' : $conf['os_serveur']
+			)) : '';
+
+	$conf['cnil_texte'] = 
+		( $conf['numero_cnil'] AND strlen($conf['numero_cnil']) ) ? 
+			"<br />"._T('texteslegaux:mention_cnil', $conf) : '';
+
+	$conf['createur_administrateur_texte'] = 
+		( $conf['createur_administrateur'] AND $conf['createur_administrateur'] == 'oui' ) ? 
+			_T('texteslegaux:egalement_administrateur') : '';
+
+//	$div = propre( _T('texteslegaux:'.$chaine, $conf) );
+	$div = propre( _T('proprietaire:'.$chaine, $conf) );
+	echo $div;
+}
+?>
