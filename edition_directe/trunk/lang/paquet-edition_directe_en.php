effc9fb99838b9b320fff6666b3cfc444cd009fc
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-05-05 13:35:25 +0000

    langues (paquet-edition_directe)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@60969 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- edition_directe/trunk/lang/paquet-edition_directe_en.php
+++ edition_directe/trunk/lang/paquet-edition_directe_en.php
@@ -1,18 +1,15 @@
 <?php
-	 
-	 // Ceci est un fichier langue de SPIP -- This is a SPIP language file
-	 // Fichier produit par PlugOnet
-	 // Module: paquet-edition_directe
-	 // Langue: es
-	 // Date: 30-07-2011 15:01:37
-	 // Items: 2
-	 
-	 if (!defined('_ECRIRE_INC_VERSION')) return;
-	 
-	 $GLOBALS[$GLOBALS['idx_lang']] = array(
-	 
-	 // T
-	 'edition_directe_description' => 'Permits direct editing of the spip objects from their page. By default only articles are in direct mode, you can ad other objects in the plugins configuration panel',	
-	'edition_directe_slogan' => 'Permits direct editing of the spip objects from their page',
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org/tradlang_module/paquet-edition_directe?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// E
+	'edition_directe_description' => 'Permits direct editing of the spip objects from their page. By default only articles are in direct mode, you can ad other objects in the plugins configuration panel',
+	'edition_directe_slogan' => 'Permits direct editing of the spip objects from their page'
 );
+
 ?>
--- edition_directe/trunk/lang/paquet-edition_directe_fr.php
+++ edition_directe/trunk/lang/paquet-edition_directe_fr.php
@@ -1,18 +1,13 @@
 <?php
-	 
-	 // Ceci est un fichier langue de SPIP -- This is a SPIP language file
-	 // Fichier produit par PlugOnet
-	 // Module: paquet-edition_directe
-	 // Langue: fr
-	 // Date: 30-07-2011 15:01:37
-	 // Items: 2
-	 
-	 if (!defined('_ECRIRE_INC_VERSION')) return;
-	 
-	 $GLOBALS[$GLOBALS['idx_lang']] = array(
-	 
-	 // T
-	 'edition_directe_description' => 'Editer directement les objets spip depuis leur page dans l&#8217;espace priv&#233;. Les objets &#224; &#233;diter directement doivent &#234;tre activ&#233;s dans Configuration',	
-	'edition_directe_slogan' => 'Editer directement les objets spip depuis leur page',
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/edition_directe/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// E
+	'edition_directe_description' => 'Editer directement les objets spip depuis leur page dans l’espace privé. Les objets à éditer directement doivent être activés dans Configuration',
+	'edition_directe_slogan' => 'Editer directement les objets spip depuis leur page'
 );
+
 ?>
