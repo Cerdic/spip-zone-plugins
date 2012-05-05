39895ff1e3492ec1e3b309bdb16ad6454564a573
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-05-05 13:35:48 +0000

    langues (sclp)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@60988 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- squirrels_love_chimps_lists_plus/trunk/lang/sclp_fr.php
+++ squirrels_love_chimps_lists_plus/trunk/lang/sclp_fr.php
@@ -1,43 +1,43 @@
-
-
 <?php
-if (!defined("_ECRIRE_INC_VERSION")) return;
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/squirrels_love_chimps_lists_plus/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-	
-	//A
-	'abonnes' => 'Abonnés',
+
+	// A
 	'abonnement_inscrit' => 'Inscrits aux listes d\'envois',
+	'abonnes' => 'Abonnés',
 	'activer_liste' => 'Activer cette liste',
 	'actualisation_spip_ok_probleme_actualisation_mailchimp' => 'Actualisation spip ok, mais il y a un problème avec l\'actualisation vers Mailchimp. si vous utilisez plusieurs tables pour la syncro, cet auteur ne figure probablement pas dans toutes les tables',
 	'aucun_abonnement' => 'Pas d\'abonnments',
-	
-	//C
+
+	// C
 	'choix_mailinglists' => 'Sélectionner les listes créées sur SPIP à synchroniser avec les listes MailChimp',
 	'confirmer_delete' => 'Vous êtes en train d\'éliminer définitivement cette liste, voulez-vous vraiment continuer ?',
 	'creer_liste' => 'Créer la liste',
-	'creer_listes' => 'Créer une liste',
 	'creer_liste_depuis_mc' => 'Créer une liste à partir d\'une liste MailChimp',
+	'creer_listes' => 'Créer une liste',
 	'creer_nouvelle_liste' => 'Créer une nouvelle liste Spip',
-	
-	//D
-	'desactiver_liste' => 'Mettre cette liste à la poubelle',
+
+	// D
 	'derniere_syncro' => 'Dernière synchronisation',
-	
-	//E
+	'desactiver_liste' => 'Mettre cette liste à la poubelle',
+
+	// E
 	'editer' => 'Editer',
 	'editer_champs_accordance' => 'Accorder les champs',
 	'eliminer' => 'Éliminer',
 	'eliminer_liste' => 'Éliminer définitivement cette liste',
-	
-	//F
+
+	// F
 	'fermer' => 'Fermer',
-	
-	//G
+
+	// G
 	'gerer_abonnements_listes' => 'Gérer les listes d\'envois',
 	'gestion_listes' => 'Gestion des listes',
-	
-	//L	
+
+	// L
 	'lang' => 'Langue',
 	'liste' => 'Liste',
 	'liste_cree' => 'La liste à été créée',
@@ -46,31 +46,29 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'listes_activees' => 'Les listes actives',
 	'listes_prive' => 'Les listes privées',
 	'listes_public' => 'Les listes publiques',
-	
-	
-	//M
-	'modifier_abonnments' => 'Modifier les abonnements',
-	
-	//N
-	'newsletters_dispos' => 'Les abonnements disponibles',
-	
-	//P
-	'poubelle' => 'Poubelle',
-	
-	//R
-	'retour' => 'Retour',
-	
-	//S
-	'syncroniser_maintenant' => 'Synchroniser maintenant',
-	'syncroniser_liste' => 'Synchroniser avec la liste',
-	'statut' => 'Statut',	
-	'statut_poubelle' => 'Poubelle',	
-	'statut_prive' => 'Privé',	
-	'statut_public' => 'Public',	
-	
-	//T
-	'titre' => 'Titre',
 
-	);
+	// M
+	'modifier_abonnments' => 'Modifier les abonnements',
+
+	// N
+	'newsletters_dispos' => 'Les abonnements disponibles',
+
+	// P
+	'poubelle' => 'Poubelle',
+
+	// R
+	'retour' => 'Retour',
+
+	// S
+	'statut' => 'Statut',
+	'statut_poubelle' => 'Poubelle',
+	'statut_prive' => 'Privé',
+	'statut_public' => 'Public',
+	'syncroniser_liste' => 'Synchroniser avec la liste',
+	'syncroniser_maintenant' => 'Synchroniser maintenant',
+
+	// T
+	'titre' => 'Titre'
+);
 
 ?>
