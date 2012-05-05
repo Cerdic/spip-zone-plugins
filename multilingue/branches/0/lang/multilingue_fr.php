70cbee060510fb0c11db82d86ac839b9f138e64e
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-05-05 10:56:20 +0000

    enlever ce qui n'est pas nécessaire
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@60956 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- multilingue/branches/0/lang/multilingue_fr.php
+++ multilingue/branches/0/lang/multilingue_fr.php
@@ -8,13 +8,6 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// M
 	'multilingue_titre' => 'Site multilingue facile',
 
-	// C
-	'cfg_exemple' => 'Exemple',
-	'cfg_exemple_explication' => 'Explication de cet exemple',
-	'cfg_titre_parametrages' => 'Paramétrages',
-
-	// T
-	'titre_page_configurer_multilingue' => 'multlinguisme',
 );
 
 ?>
