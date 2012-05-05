c714b0e808dc5b12c651360507f01a3724bf25bd
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-05-05 13:35:54 +0000

    langues (tra)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@60994 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- traduction_rubriques_autrement/trunk/lang/tra_de.php
+++ traduction_rubriques_autrement/trunk/lang/tra_de.php
@@ -9,13 +9,13 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
 	'ajouter_traduction' => 'Übersetzung hinzufügen',
-	'avis_rubrique_source' => 'Vous devez d\'abord traduire toutes les rubrique parentes', # NEW
+	'avis_rubrique_source' => 'Sie müssen zuerst die darunterliegenden Rubriken übersetzen', # MODIF
 
 	// R
 	'rubrique_reference' => '(Referenzrubrik)',
 
 	// T
-	'trad_delier_rubrique' => 'Ne plus lier cette rubrique à ces traductions' # NEW
+	'trad_delier_rubrique' => 'Ne plus lier cette rubrique à ces traductions' # Diese Rubrik von den aktuellen Übersetzungen lösen
 );
 
 ?>
