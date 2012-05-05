da5223b1b5f9ae316428ce724215df862eb2f81f
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-05-05 13:35:41 +0000

    langues (paquet-spip_400)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@60981 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spip_400/spip_3/lang/paquet-spip_400_fr.php
+++ spip_400/spip_3/lang/paquet-spip_400_fr.php
@@ -1,18 +1,11 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-spip_400
-// Langue: fr
-// Date: 01-05-2012 11:53:47
-// Items: 2
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/spip_400/spip_3/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// S
+	// S
 	'spip_400_description' => 'Ce plugin tente de compléter la distribution de SPIP en proposant des modèles de pages d\'erreur HTTP ({codes 401 et 404}) avec un texte explicatif et la possibilité pour l\'internaute de transmettre un "ticket de bug" au webmestre du site.
 
 Il propose notamment : 
@@ -21,6 +14,7 @@ Il propose notamment :
 - l\'écriture de messages de LOG dans un fichier spécifique ...
 
 Une page de configuration est proposée en option si vous utilisez le plugin [CFG : moteur de configuration->http://www.spip-contrib.net/?rubrique575].',
-	'spip_400_slogan' => 'Gestion poussée des erreurs HTTP (401, 404) pour SPIP',
+	'spip_400_slogan' => 'Gestion poussée des erreurs HTTP (401, 404) pour SPIP'
 );
+
 ?>
