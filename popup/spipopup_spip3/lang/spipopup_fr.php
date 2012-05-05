e66412678751fe5c08c0cb5dd04c6ea16c527f93
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-05-05 13:35:34 +0000

    langues (spipopup)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@60975 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- popup/spipopup_spip3/lang/spipopup_fr.php
+++ popup/spipopup_spip3/lang/spipopup_fr.php
@@ -1,37 +1,53 @@
 <?php
-/**
- * @name 		JavascriptPopup_lang_FR
- * @author 		Piero Wbmstr <piero.wbmstr@gmail.com>
- */
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/popup/spipopup_spip3/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// Description
-	'nom' => 'Balise &#035;POPUP',
-
-// CFG
-	'titre_descr_cfg' => 'Configuration de l\'outil "Popup"',
-	'descr_cfg' => 'Documentation du plugin pour plus d\'infos : [spip-contrib.net/?article3573->http://www.spip-contrib.net/?article3573]',
-	'legend_cfg_balise' => 'Concernant la fen&ecirc;tre externe',
-	'skel_defaut' => 'Squelette par d&eacute;faut utilis&eacute; pour afficher le contenu de la fen&ecirc;tre',
-	'skel_defaut_comment' => 'Il s\'agit initialement du squelette "popup_defaut.html" pr&eacute;sent &agrave; la racine du plugin.',
-	'width_and_height' => 'Taille de la fen&ecirc;te',
-	'width' => 'Largeur (en pixels)',
-	'height' => 'Hauteur (en pixels)',
-	'titre_popup' => 'Nom JavaScript de la fen&ecirc;tre',
-	'titre_popup_comment' => 'Vous pourrez par la suite utiliser ce nom pour la d&eacute;signer dans vos scripts ("window.popup").',
-	'options_popup' => 'Options JavaScript pass&eacute;es &agrave; la nouvelle fen&ecirc;tre',
-	'options_popup_comment' => 'Indiquez ici un tableau d\'options qui seront pass&eacute;es &agrave; la nouvelle fen&ecirc;tre sous la forme d\'un tableau du genre : "variable1: valeur1, variable2: valeur2, ..." ({ex.: "location: 0, scrollbars: 1"}).',
-
-// Popup
-	'popup_titre' => 'Bo&#238;te de dialogue',
-	'btn_imprimer' => 'Imprimer',
-	'btn_imprimer_ttl' => 'Imprimer cette page',
+	// B
 	'btn_fermer_fenetre' => 'Fermer',
-	'btn_fermer_fenetre_ttl' => 'Fermer cette fen&#234;tre',
+	'btn_fermer_fenetre_ttl' => 'Fermer cette fenêtre',
 	'btn_haut_page' => 'Haut de page',
 	'btn_haut_page_ttl' => 'Retour en haut de page',
-	'nouvelle_fenetre' => '[Nouvelle fen&#234;tre]',
-	'retour_fenetre' => '[Retour en fen&#234;tre principale]',
+	'btn_imprimer' => 'Imprimer',
+	'btn_imprimer_ttl' => 'Imprimer cette page',
+
+	// D
+	'descr_cfg' => 'Documentation du plugin pour plus d\'infos : [spip-contrib.net/?article3573->http://www.spip-contrib.net/?article3573]',
+
+	// H
+	'height' => 'Hauteur (en pixels)',
+
+	// L
+	'legend_cfg_balise' => 'Concernant la fenêtre externe',
+
+	// N
+	'nom' => 'Balise #POPUP',
+	'nouvelle_fenetre' => '[Nouvelle fenêtre]',
+
+	// O
+	'options_popup' => 'Options JavaScript passées à la nouvelle fenêtre',
+	'options_popup_comment' => 'Indiquez ici un tableau d\'options qui seront passées à la nouvelle fenêtre sous la forme d\'un tableau du genre : "variable1: valeur1, variable2: valeur2, ..." ({ex.: "location: 0, scrollbars: 1"}).',
+
+	// P
+	'popup_titre' => 'Boîte de dialogue',
+
+	// R
+	'retour_fenetre' => '[Retour en fenêtre principale]',
+
+	// S
+	'skel_defaut' => 'Squelette par défaut utilisé pour afficher le contenu de la fenêtre',
+	'skel_defaut_comment' => 'Il s\'agit initialement du squelette "popup_defaut.html" présent à la racine du plugin.',
+
+	// T
+	'titre_descr_cfg' => 'Configuration de l\'outil "Popup"',
+	'titre_popup' => 'Nom JavaScript de la fenêtre',
+	'titre_popup_comment' => 'Vous pourrez par la suite utiliser ce nom pour la désigner dans vos scripts ("window.popup").',
+
+	// W
+	'width' => 'Largeur (en pixels)',
+	'width_and_height' => 'Taille de la fenête'
 );
+
 ?>
