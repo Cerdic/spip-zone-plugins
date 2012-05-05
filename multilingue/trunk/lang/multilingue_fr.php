bc7a6cb93b29b3d2cb87588c3b275e9629d2739f
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-05-05 13:35:31 +0000

    langues (multilingue)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@60973 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- multilingue/trunk/lang/multilingue_fr.php
+++ multilingue/trunk/lang/multilingue_fr.php
@@ -1,13 +1,12 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/multilingue/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
- 
+
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// M
-	'multilingue_titre' => 'Site multilingue facile',
-
+	'multilingue_titre' => 'Site multilingue facile'
 );
 
 ?>
