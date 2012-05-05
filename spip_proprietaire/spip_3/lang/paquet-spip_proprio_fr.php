0c3bc3c0c9d96224bbd71c97722e56110496aa37
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-05-05 13:35:44 +0000

    langues (paquet-spip_proprio)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@60984 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spip_proprietaire/spip_3/lang/paquet-spip_proprio_fr.php
+++ spip_proprietaire/spip_3/lang/paquet-spip_proprio_fr.php
@@ -1,18 +1,11 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-spip_proprio
-// Langue: fr
-// Date: 01-05-2012 11:34:46
-// Items: 2
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/spip_proprietaire/spip_3/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// S
+	// S
 	'spip_proprio_description' => 'Ce plugin vous propose la génération automatique de {{Mentions Légales}}, {{Conditions d\'Utilisation}} et informations de {{Copyright}} propres à votre site SPIP.
 
 -* La page de configuration du plugin, accessible depuis l\'onglet "Configuration" de l\'espace privé de SPIP, vous permet de saisir différentes informations sur l\'éditeur du site, son hébergeur et son créateur.
@@ -24,6 +17,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 Certaines icônes du plugin, dans les modèles publics, proviennent du set "{{[bwpx.icns->http://paularmstrongdesigns.com/portfolio/bwpx-icns/]}}" de {{Paul Armstrong}}, distribué sous licence {Creative Commons Attribution-Share Alike 3.0} (US).
 
 Les icônes de l\'espace privé proviennent du projet {{[GNOME->http://www.gnome.org/]}}.',
-	'spip_proprio_slogan' => 'Mentions Légales, Conditions d\'Utilisation et Copyright automatiques',
+	'spip_proprio_slogan' => 'Mentions Légales, Conditions d\'Utilisation et Copyright automatiques'
 );
+
 ?>
