31bc0a6c44450e8e02b687334995df2cf240de23
davoodhossein@yahoo.com <davoodhossein@yahoo.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-05-05 13:35:29 +0000

    langues (gravatar)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@60971 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ gravatar/lang/gravatar_fa.php
@@ -0,0 +1,38 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org/tradlang_module/gravatar?lang_cible=fa
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cfg_descr_gravatar' => 'پيكربندي نمايش گراواتار',
+	'cfg_titre_gravatar' => 'گرواواتارها',
+
+	// E
+	'explication_image_defaut' => 'براي كساني كه آواتار ندارند، استفاده كنند: ',
+
+	// G
+	'gravatar_info' => 'براي نمايش آواتار خود در اينجا، اول در اينجا ثبت نام‌كند
+<a href="http://www.gravatar.com/" rel="external 
+nofollow" class="spip_out">gravatar.com</a> (مفت و راحت).',
+	'gravatar_info_forum' => 'براي نمايش آواتار همراه با پيام خود، اول اينجا ثبتشان كنيد
+ <a href="http://www.gravatar.com/" rel="external nofollow" class="spip_out">gravatar.com</a> (مفت و رايگان) و يادتان نرو كه نشاني ايميل خودتان را هم اينجا قيد كنيد.',
+
+	// L
+	'label_image_defaut' => 'تصوير پيش گزيده',
+	'label_image_defaut_404' => 'تصويري نيست',
+	'label_image_defaut_gravatar' => 'لوگو گواتار',
+	'label_image_defaut_identicon' => '<i>Identicon</i> (ايجاد شده از نشاني ايميل )',
+	'label_image_defaut_monsterid' => '<i>MonsterID</i> (ايجاد شده از نشاني ايميل)',
+	'label_image_defaut_png' => '<tt>تاصوير/گراواتار.png</tt>',
+	'label_image_defaut_wavatar' => '<i>Wavatar</i> (ايجاد شده از نشاني ايميل)',
+	'label_taille' => 'اندازه‌ي گراواتارها',
+
+	// T
+	'titre_gravatar_auteur' => 'گراواتار'
+);
+
+?>
