a9fd82b59773f48d105713ff0075e13dd38a37a0
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-05-05 13:35:51 +0000

    langues (paquet-tipafriend)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@60991 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- tipafriend/spip_3/lang/paquet-tipafriend_fr.php
+++ tipafriend/spip_3/lang/paquet-tipafriend_fr.php
@@ -1,18 +1,11 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-tipafriend
-// Langue: fr
-// Date: 01-05-2012 15:28:26
-// Items: 3
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/tipafriend/spip_3/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// T
+	// T
 	'tipafriend_description' => 'Ce plugin propose un formulaire complet pour envoyer une page d\'un site SPIP ({n\'importe laquelle}) à une liste d\'adresses e-mail.
 
 Il s\'utilise en plaçant la balise {{#TIPAFRIEND}} dans vos squelettes ; le formulaire apparaît en fenêtre popup par défaut, mais peut être intégré en div type {{thickbox}} ({plugin SPIP}) via le panneau de configuration accessible depuis le plugin {{[CFG : moteur de configuration->http://www.spip-contrib.net/?rubrique575]}} ({optionnel}).
@@ -21,6 +14,7 @@ Une documentation interne est disponible lorsque le plugin est actif sur la page
 
 Logo et certaines images du plugin : [->http://pixel-mixer.com/]',
 	'tipafriend_nom' => 'Tip A Friend',
-	'tipafriend_slogan' => 'Envoyer une page d\'un site SPIP (n\'importe laquelle) à une liste d\'adresses e-mail',
+	'tipafriend_slogan' => 'Envoyer une page d\'un site SPIP (n\'importe laquelle) à une liste d\'adresses e-mail'
 );
+
 ?>
