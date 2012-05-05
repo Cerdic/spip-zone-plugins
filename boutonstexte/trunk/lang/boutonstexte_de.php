f0efd4c7f492c44065a6e94186a14009360828af
klaus@spip.de <klaus@spip.de@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-05-05 13:35:20 +0000

    langues (boutonstexte)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@60966 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- boutonstexte/trunk/lang/boutonstexte_de.php
+++ boutonstexte/trunk/lang/boutonstexte_de.php
@@ -1,14 +1,40 @@
 <?php
-
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-if (!defined("_ECRIRE_INC_VERSION")) return;
+// extrait automatiquement de http://trad.spip.org/tradlang_module/boutonstexte?lang_cible=de
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-'textes_icones' => 'Hilfe zu Ikone',
-'texte_seulement' => 'Nur Text',
-'retour_a_spip' => 'Zur&uuml;ck zu voller Seite',
-'augmenter_police' => 'Schrift vergr&ouml;&szlig;ern',
-'diminuer_police' => 'Schrift verkleinern',
-'donnees_techniques' => 'Technishe Daten nicht zu &auml;ndern'
+
+	// A
+	'augmenter_police' => 'Schrift vergrößern',
+
+	// B
+	'boutonstexte_nom' => 'Boutons dans le Texte', # NEW
+
+	// D
+	'diminuer_police' => 'Schrift verkleinern',
+	'donnees_techniques' => 'Unveränderliche technische Einstellungen',
+
+	// L
+	'label_selector' => 'Erweiterter CSS oder jQuery Ausdruck',
+	'label_skin' => 'Aussehen der Knöpfe',
+	'label_txtBackSpip' => 'Texte retour spip', # NEW
+	'label_txtOnly' => 'Nur Text *',
+	'label_txtSizeDown' => 'Text verkleinern *',
+	'label_txtSizeUp' => 'Text vergrößern *',
+	'legend_apparence' => 'Gestaltung', # Habillage im Frz. meint i.d.R. Umgebung, Basisgestaltung oder thème.
+
+	// R
+	'retour_a_spip' => 'Zurück zu voller Seite',
+
+	// S
+	'selecteur' => 'ID oder Klasse des Texts',
+
+	// T
+	'texte_seulement' => 'Nur Text',
+	'textes_icones' => 'Hilfe zu Ikone'
 );
+
 ?>
