8fe5194c3754f446d6bb8a2c13e11f08285f275a
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-05-06 18:13:29 +0000

    langues (paquet-tipafriend)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@61046 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ tipafriend/spip_3/lang/paquet-tipafriend_sk.php
@@ -0,0 +1,22 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org/tradlang_module/paquet-tipafriend?lang_cible=sk
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// T
+	'tipafriend_description' => 'Ce plugin propose un formulaire complet pour envoyer une page d\'un site SPIP ({n\'importe laquelle}) à une liste d\'adresses e-mail.
+
+Il s\'utilise en plaçant la balise {{#TIPAFRIEND}} dans vos squelettes ; le formulaire apparaît en fenêtre popup par défaut, mais peut être intégré en div type {{thickbox}} ({plugin SPIP}) via le panneau de configuration accessible depuis le plugin {{[CFG : moteur de configuration->http://www.spip-contrib.net/?rubrique575]}} ({optionnel}).
+
+Une documentation interne est disponible lorsque le plugin est actif sur la page publique [tipafriend_documentation->../?page=tipafriend_documentation].
+
+Logo et certaines images du plugin : [->http://pixel-mixer.com/]', # NEW
+	'tipafriend_nom' => 'Odporučiť priateľom',
+	'tipafriend_slogan' => 'Pošlite stránku v SPIPe (akúkoľvek) priateľom v zozname e-mailových adries'
+);
+
+?>
