1dc5e27701a5e534342ecee6420488f3bc411ee7
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-05-06 12:47:04 +0000

    salvatore
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@61024 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ squirrels_love_chimps/branches/1/lang/tra_en.php
@@ -0,0 +1,21 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org/tradlang_module/tra?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'ajouter_traduction' => 'Add a translation',
+	'avis_rubrique_source' => 'Vous devez d\'abord traduire toutes les rubrique parentes', # NEW
+
+	// R
+	'rubrique_reference' => '(reference section)',
+
+	// T
+	'trad_delier_rubrique' => 'Ne plus lier cette rubrique à ces traductions' # NEW
+);
+
+?>
---
+++ squirrels_love_chimps/branches/1/lang/tra_fa.php
@@ -0,0 +1,21 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org/tradlang_module/tra?lang_cible=fa
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'ajouter_traduction' => 'يك ترجمه اضافه كنيد',
+	'avis_rubrique_source' => 'Vous devez d\'abord traduire toutes les rubrique parentes', # NEW
+
+	// R
+	'rubrique_reference' => 'بخش براي ارجاع',
+
+	// T
+	'trad_delier_rubrique' => 'Ne plus lier cette rubrique à ces traductions' # NEW
+);
+
+?>
