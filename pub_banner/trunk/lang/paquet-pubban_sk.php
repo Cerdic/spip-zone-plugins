baeea9f8c8f861e0f3f336d32c6ce52ebc993b59
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-05-06 18:13:22 +0000

    langues (paquet-pubban)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@61039 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ pub_banner/trunk/lang/paquet-pubban_sk.php
@@ -0,0 +1,31 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org/tradlang_module/paquet-pubban?lang_cible=sk
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// P
+	'pubban_description' => 'Gestion et statistiques de bannières publicitaires pour intégrées aux squelettes SPIP ...
+
+{{UTILISATION}}
+
+-* Ce plugin ajoute la balise {{ #PUBBAN{nom de la bannière} }} pour les squelettes, qui est remplacée par une frame englobant la bannière.
+-* Une page publique vous permet de présenter les statistiques des encarts et de les exporter au format \'.xls\'.
+
+{{VALEURS PAR DEFAUT}}
+
+-* Des bannières standards sont chargées par défaut à l\'installation ; il s\'agit des quatre types les plus répandus sur le web. Vous pouvez les effacer et en créer de nouvelles.
+-* Des exemples d\'encarts ({écureuil SPIP}) sont également chargés, pour démonstration.
+
+{{AIDE / DOC}}
+
+-* Une explication ainsi qu\'un exemple complet sont disponibles dans la documentation, qui vous permettra également de visualiser l\'intégration des bannières dans vos squelettes. Cette documentation est disponible, si le plugin est actif sur votre site, sur la page publique "pubban_documentation".
+
+-* La page de statistiques du plugin est une adaptation libre du script \'{{[VRG pub->http://vrgpub.frankdevelopper.com/]}}\' développé par {{Vincent Roseberry}} ({sous licence GPL à l\'époque}).', # NEW
+	'pubban_slogan' => 'Ovládanie reklamných pútačov'
+);
+
+?>
