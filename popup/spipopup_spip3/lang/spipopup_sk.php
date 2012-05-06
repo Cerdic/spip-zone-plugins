66070420eaf08763c9b0925435a527e3dc312656
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-05-06 18:13:20 +0000

    langues (spipopup)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@61037 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ popup/spipopup_spip3/lang/spipopup_sk.php
@@ -0,0 +1,55 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org/tradlang_module/spipopup?lang_cible=sk
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'btn_fermer_fenetre' => 'Zatvoriť',
+	'btn_fermer_fenetre_ttl' => 'Zatvoriť toto okno',
+	'btn_haut_page' => 'Vrchná časť stránky',
+	'btn_haut_page_ttl' => 'Návrat na vrchnú časť stránky',
+	'btn_imprimer' => 'Vytlačiť',
+	'btn_imprimer_ttl' => 'Vytlačiť túto stránku',
+
+	// D
+	'descr_cfg' => 'Dokumentácia zásuvného modulu pre viac informácií: [spip-contrib.net/?article3573->http://www.spip-contrib.net/?article3573]',
+
+	// H
+	'height' => 'Výška (v pixeloch)',
+
+	// L
+	'legend_cfg_balise' => 'O externom okne',
+
+	// N
+	'nom' => 'Tag #POPUP',
+	'nouvelle_fenetre' => '[Nové okno]',
+
+	// O
+	'options_popup' => 'Nastavenia JavaScriptu, ktoré prešli na nové okno',
+	'options_popup_comment' => 'Tu zadajte pole s nastaveniami, ktoré prejdú na nové okno vo forme tabuľky v podobe: "premenná1: hodnota1, premenná2: hodnota2, ..." ({príkl.: "poloha: 0, rolovacie pruhy: 1"}).',
+
+	// P
+	'popup_titre' => 'Dialógové okno',
+
+	// R
+	'retour_fenetre' => '[Späť na hlavné okno]',
+
+	// S
+	'skel_defaut' => 'Predvolená šablóna, ktorá sa použije na zobrazenie obsahu okna',
+	'skel_defaut_comment' => 'Toto je začiatok šablóny "popup_defaut.html", ktorá sa nachádza v koreňovom adresári zásuvného modulu.',
+
+	// T
+	'titre_descr_cfg' => 'Nastavenia nástroja "Vyskakovacie okno"',
+	'titre_popup' => 'Názov JavaScriptu okna',
+	'titre_popup_comment' => 'Na označenie v svojich skriptoch ("window.popup") môžete použiť tento názov.',
+
+	// W
+	'width' => 'Šírka (v pixeloch)',
+	'width_and_height' => 'Veľkosť okna'
+);
+
+?>
