f8864cf00fab37dd4dcb0b93d2ee634e21f25cd5
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-05-07 05:56:03 +0000

    salvatore
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@61054 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ multilingue/branches/0/lang/multilingue_sk.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org/tradlang_module/multilingue?lang_cible=sk
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// M
+	'multilingue_titre' => 'Jednoduchá viacjazyčná stránka'
+);
+
+?>
---
+++ multilingue/branches/0/lang/paquet-multilingue_sk.php
@@ -0,0 +1,34 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org/tradlang_module/paquet-multilingue?lang_cible=sk
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// M
+	'multilingue_description' => 'Met en place un site multilingue avec une langue par secteur.
+	
+	Le plugin force l\'installation de, plusieurs plugin
+	
+	- [Traduction entre rubriques->http://plugins.spip.net/tradrub.html]
+	- [Langue préférée par l’internaute ->http://www.spip-contrib.net/Plugin-Langue-preferee-par-l-internaute]
+	- [Traductions de rubriques autrement->http://www.spip-contrib.net/Traductions-de-rubriques-autrement-3]
+	- [Traductions d’articles autremen->http://www.spip-contrib.net/Traductions-d-articles-autrement-3]
+	
+	Sous Spip 2 ou 2.1 il es recommendable d\'installer auparavant le plugin [Step->http://www.spip-contrib.net/STEP-Systeme-de-Telechargements-de-Plugins], puis installer ce plugin ce qui forcera également l\'installation de tous les plugin nécessaires.
+	
+	
+	Le plugin modifie également quelques squelettes (rubriques et articles) et quelques noisettes de bases.
+	
+	Une fois installé les plugins nécessaires, il vous suffira de déterminer les langues du site dans "Configuration"->"Mulilinguisme/Gestion des langues
+
+	et voilà votre site est multilingue
+	
+	', # NEW
+	'multilingue_nom' => 'Jednoduchá viacjazyčná stránka',
+	'multilingue_slogan' => 'Vytvorte jednoduchú viacjazyčnú stránku'
+);
+
+?>
