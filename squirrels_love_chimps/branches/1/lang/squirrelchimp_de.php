782ef29b383452ee501dbff9e6247d4318146842
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-05-07 05:58:15 +0000

    salvatore
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@61055 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- squirrels_love_chimps/branches/1/lang/squirrelchimp_de.php
+++ squirrels_love_chimps/branches/1/lang/squirrelchimp_de.php
@@ -1,27 +1,32 @@
 <?php
-if (!defined("_ECRIRE_INC_VERSION")) return;
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org/tradlang_module/squirrelchimp?lang_cible=de
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-	//A
+	// A
 	'api_errorcode' => 'Zurückmeldung MailChimp. Code:',
 
-	//C
+	// C
 	'config_erreur' => 'Irrtum : die MailCimp Zugansdaten sind nicht gespeichert, Der Site Administrator kann dies  : <a href="ecrire/?exec=configurer_mailchimp">hier korrigieren</a>',
 	'config_squirrel_chimp' => 'Generell',
-	'configurer_erreur_api' => 'Irrtum, Ihre Daten wurden nicht gespeichert',
-	'configurer_legend' => 'MailChimp Identifizierungs Parameter',
 	'configurer_apikey' => 'Einmaliges Kennzeichen des MailChimp Kontos',
 	'configurer_apikey_explication' => 'API Key den man im Verwaltungspanel : <a href="http://admin.mailchimp.com/account/api">MailChimp API</a> findet',
+	'configurer_erreur_api' => 'Irrtum, Ihre Daten wurden nicht gespeichert',
+	'configurer_legend' => 'MailChimp Identifizierungs Parameter',
+	'configurer_listid' => 'Identifiant unique de la liste', # NEW
+	'configurer_listid_explication' => 'Chaque liste de personnes possède un identifiant unique. Visualisez la liste qui vous intéresse sur cette page : <a href ="https://admin.mailchimp.com/lists/">Mailchimp list</a>. Cliquez sur "Settings/list Settings and unique id" puis en bas de la page vous trouverez l\'identifiant: "unique id for list"', # NEW
 
-	//R
-
-	'titre_cadre_raccourcis' => 'Abkürzung',
+	// R
 	'retour_accueil' => 'Zurück zur Hauptseite',
 	'retour_test_api' => 'Verbindung mit MailChimp wurde erstellt, die Daten wurden gespeichert',
-	
-	//T
-	'titre_plugin' => 'Squirrels Love Chimps',
+
+	// T
+	'titre_cadre_raccourcis' => 'Abkürzung',
+	'titre_plugin' => 'Squirrels Love Chimps'
 );
 
 ?>
