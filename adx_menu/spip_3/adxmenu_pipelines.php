03c1fdb5f989115a1874a14ba84d1b9c1d573ac7
piero.wbmstr@gmail.com <piero.wbmstr@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-05-08 23:23:16 +0000

    On passé par "insert_head_css" (SPIP3) et une petite harmonisation des styles avec la nouvelle dist
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@61135 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- adx_menu/spip_3/adxmenu_pipelines.php
+++ adx_menu/spip_3/adxmenu_pipelines.php
@@ -14,15 +14,29 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 function adxmenu_insert_head($flux){
 	$conf = function_exists('lire_config') ? lire_config('adxmenu') : false;
 	$adx = (!$conf OR !strlen($conf['adx'])) ? ADXMENU_OUVERTURE_DEFAUT : $conf['adx'];
+	include_spip('plugins/installer'); // spip_version_compare dans SPIP 3.x
+	include_spip('inc/plugin'); // spip_version_compare dans SPIP 2.x
+	if (spip_version_compare($GLOBALS['spip_version_branche'], '3.0.0alpha', '>=')) {
+    $css_styles = generer_url_public("adxmenu_css_styles_spip3.css",'type_menu='.$adx);
+	} else {
+    $css_styles = generer_url_public("adxmenu_css_styles.css",'type_menu='.$adx);
+	}
 	$flux .= "<!-- ADX Menu insert head -->"
 		."\n<link rel='stylesheet' href='".generer_url_public("adxmenu_css.css",'type_menu='.$adx)."' type='text/css' media='projection, screen, tv' />"
 		."\n<!--[if lte IE 6]>"
 		."\n<link rel='stylesheet' href='".generer_url_public('css/adxmenu_css_ie.css','type_menu='.$adx)."' type='text/css' media='projection, screen, tv' />"
 		."\n<script src='".url_absolue(find_in_path('javascript/ADxMenu.js'))."' type='text/javascript'></script>"
 		."\n<![endif]-->"
-		."\n<link rel='stylesheet' href='".generer_url_public("adxmenu_css_styles.css",'type_menu='.$adx)."' type='text/css' media='projection, screen, tv' />"
+		."\n<link rel='stylesheet' href='".$css_styles."' type='text/css' media='projection, screen, tv' />"
 		."<!-- END ADX Menu insert head -->\n";
 	return $flux;
+}
+
+/** 
+ * Insertion des CSS par INSERT_HEAD_CSS (pour SPIP3)
+ */
+function adxmenu_insert_head_css($flux){
+	return adxmenu_insert_head($flux);
 }
 
 ?>
