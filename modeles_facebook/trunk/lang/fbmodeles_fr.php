e3642aa6238f319f0f0a69d5112af0b3f1c5bab7
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-05-09 13:33:51 +0000

    langues (fbmodeles)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@61151 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- modeles_facebook/trunk/lang/fbmodeles_fr.php
+++ modeles_facebook/trunk/lang/fbmodeles_fr.php
@@ -1,90 +1,96 @@
 <?php
-// RAPPELS
-// Les textes de cette page peuvent etre rediges avec les raccourcis typo de SPIP
-// !! - Les accents doivent etre code en HTML : é => &eacute;
-// !! - Les apostrophes doivents etre echappees : ' => \'
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/modeles_facebook/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-	'oui' => 'Oui',
-	'non' => 'Non',
 
-// C //
-	// Documentation
+	// C
 	'cf_navigation' => 'Cf. [colonne de navigation->@url@]',
-	// Configuration (CFG)
-	'cfg_descr_titre' => 'Mod&egrave;les Facebook',
-	'cfg_label_titre' => 'Configuration des mod&egrave;les Facebook',
-	'cfg_descr' => 'Vous devez ici d&eacute;finir les diff&eacute;rents identifiants fournis par le syst&egrave;me Facebook.<br /><br />Plus d\'infos : [->http://www.facebook.com/insights/].
+	'cfg_comment_appid' => 'Identifiant d\'une application propre à vote site ; cela nécessite d\'avoir créé l\'application.',
+	'cfg_comment_border_color' => 'Indiquez une couleur en code héxadécimal AVEC le dièse initial.',
+	'cfg_comment_colorscheme' => 'Sélectionnez ici le profil prédéterminé par les modules qui sera utilisé pour l\'affichage.',
+	'cfg_comment_font' => 'Sélectionnez ici la police de caractère qui sera utilisée pour l\'affichage des modules.',
+	'cfg_comment_identifiants' => '{{Utilisez les champs ci-dessous pour préciser les différents identifiants que vous souhaitez utiliser.}} Ils ne sont pas obligatoires, mais peuvent permettre notamment de suivre des statistiques précises proposées par Facebook.',
+	'cfg_comment_pageid' => 'Identifiant d\'une page ; cela nécessite d\'avoir créé la page.',
+	'cfg_comment_reglages' => '{{Vous pouvez ici choisir certains réglages concernant les outils javascript de Facebook.}} Par défaut, les modèles utilisent le langage XFBML ({SDK javascript Facebook}) mais vous pouvez désactiver cette fonctionnalité, les outils seront alors chargés en frames.',
+	'cfg_comment_url_page' => 'Adresse URL complète de votre page ou profil Facebook ; elle sera utilisée par défaut par les modèles (URL du type "<code>http://www.facebook.com/...</code>").',
+	'cfg_comment_userid' => 'Identifiant(s) utilisateur(s) des administrateurs des plugins. Vous pouvez en indiquer plusieurs en les séparant par une virgule.',
+	'cfg_comment_xfbml' => 'Utilisation de la bibliothèque javascript du SDK Facebook et du langage associé. Si vous choisissez "non", les modules seront présentés en iframe.',
+	'cfg_descr' => 'Vous devez ici définir les différents identifiants fournis par le système Facebook.<br /><br />Plus d\'infos : [->http://www.facebook.com/insights/].
 
-Pour inclure les balises "Open Graph" en en-t&ecirc;te de vos pages publiques, vous devez ins&eacute;rer le mod&egrave;le "insert_head_og" en lui passant l\'environnement : {{&#035;MODELE&#123;insert_head_og&#125;&#123;env&#125;}}.
+Pour inclure les balises "Open Graph" en en-tête de vos pages publiques, vous devez insérer le modèle "insert_head_og" en lui passant l\'environnement : {{#MODELE{insert_head_og}{env}}}.
 <br /><br />Plus d\'infos : [->http://developers.facebook.com/docs/opengraph/].',
+	'cfg_descr_titre' => 'Modèles Facebook',
 	'cfg_identifiants' => 'Identifiants Facebook',
-	'cfg_comment_identifiants' => '{{Utilisez les champs ci-dessous pour pr&eacute;ciser les diff&eacute;rents identifiants que vous souhaitez utiliser.}} Ils ne sont pas obligatoires, mais peuvent permettre notamment de suivre des statistiques pr&eacute;cises propos&eacute;es par Facebook.',
-	'cfg_reglages' => 'R&eacute;glages par d&eacute;faut',
-	'cfg_comment_reglages' => '{{Vous pouvez ici choisir certains r&eacute;glages concernant les outils javascript de Facebook.}} Par d&eacute;faut, les mod&egrave;les utilisent le langage XFBML ({SDK javascript Facebook}) mais vous pouvez d&eacute;sactiver cette fonctionnalit&eacute;, les outils seront alors charg&eacute;s en frames.',
 	'cfg_label_appid' => 'Identifiant application "App ID"',
-	'cfg_comment_appid' => 'Identifiant d\'une application propre &agrave; vote site ; cela n&eacute;cessite d\'avoir cr&eacute;&eacute; l\'application.',
-	'cfg_label_pageid' => 'Identifiant page "Page ID"',
-	'cfg_comment_pageid' => 'Identifiant d\'une page ; cela n&eacute;cessite d\'avoir cr&eacute;&eacute; la page.',
-	'cfg_label_userid' => 'Identifiant utilisateur "User ID"',
-	'cfg_comment_userid' => 'Identifiant(s) utilisateur(s) des administrateurs des plugins. Vous pouvez en indiquer plusieurs en les s&eacute;parant par une virgule.',
-	'cfg_label_url_page' => 'URL de page ou profil',
-	'cfg_comment_url_page' => 'Adresse URL compl&egrave;te de votre page ou profil Facebook ; elle sera utilis&eacute;e par d&eacute;faut par les mod&egrave;les (URL du type "<code>http://www.facebook.com/...</code>").',
-	'cfg_label_xfbml' => 'Utilisation du XFBML',
-	'cfg_comment_xfbml' => 'Utilisation de la biblioth&egrave;que javascript du SDK Facebook et du langage associ&eacute;. Si vous choisissez "non", les modules seront pr&eacute;sent&eacute;s en iframe.',
-	'cfg_label_border_color' => 'Couleur de bordure par d&eacute;faut',
-	'cfg_comment_border_color' => 'Indiquez une couleur en code h&eacute;xad&eacute;cimal AVEC le di&egrave;se initial.',
-	'cfg_label_font' => 'Police utilis&eacute;e par d&eacute;faut',
-	'cfg_comment_font' => 'S&eacute;lectionnez ici la police de caract&egrave;re qui sera utilis&eacute;e pour l\'affichage des modules.',
+	'cfg_label_border_color' => 'Couleur de bordure par défaut',
 	'cfg_label_colorscheme' => 'Profil de couleurs',
-	'cfg_comment_colorscheme' => 'S&eacute;lectionnez ici le profil pr&eacute;d&eacute;termin&eacute; par les modules qui sera utilis&eacute; pour l\'affichage.',
+	'cfg_label_font' => 'Police utilisée par défaut',
+	'cfg_label_pageid' => 'Identifiant page "Page ID"',
+	'cfg_label_titre' => 'Configuration des modèles Facebook',
+	'cfg_label_url_page' => 'URL de page ou profil',
+	'cfg_label_userid' => 'Identifiant utilisateur "User ID"',
+	'cfg_label_xfbml' => 'Utilisation du XFBML',
+	'cfg_reglages' => 'Réglages par défaut',
 
-// D //
-	'defaut'=>'Défaut',
-	// Documentation
-	'doc_titre_page' => 'Page de documentation du plugin Mod&egrave;les Facebook',	
-	'doc_titre_court' => 'Documentation Mod&egrave;les Facebook',	
+	// D
+	'defaut' => 'Défaut',
+	'doc_chapo' => 'Le plugin Modèles Facebook pour SPIP 2.0 ({et plus}) propose un ensemble de modèles, ou noisettes, permettant d\'utiliser simplement et rapidement les plugins sociaux proposés par Facebook.',
 	'doc_en_ligne' => 'Documentation',
-	'doc_chapo' => 'Le plugin Mod&egrave;les Facebook pour SPIP 2.0 ({et plus}) propose un ensemble de mod&egrave;les, ou noisettes, permettant d\'utiliser simplement et rapidement les plugins sociaux propos&eacute;s par Facebook.',
+	'doc_titre_court' => 'Documentation Modèles Facebook',
+	'doc_titre_page' => 'Page de documentation du plugin Modèles Facebook',
 	'documentation' => '{{{Utilisation du plugin}}}
 
-Comme montr&eacute; ci-dessus, les mod&egrave;les s\'incluent directement en leur passant les options souhait&eacute;es.
+Comme montré ci-dessus, les modèles s\'incluent directement en leur passant les options souhaitées.
 
-Chaque mod&egrave;le peut recevoir une liste d\'options, dont certaines sont n&eacute;cessaires &agrave; son affichage. Pour une liste compl&egrave;te, reportez-vous aux informations des en-t&ecirc;te de fichiers de mod&egrave;les, dans le r&eacute;pertoire "<code>modeles/</code>" du plugin.
+Chaque modèle peut recevoir une liste d\'options, dont certaines sont nécessaires à son affichage. Pour une liste complète, reportez-vous aux informations des en-tête de fichiers de modèles, dans le répertoire "<code>modeles/</code>" du plugin.
 
-Le plugin propose &eacute;galement un mod&egrave;le g&eacute;n&eacute;rant des informations {{Open Graph}}, les metas informations utilis&eacute;es par Facebook, propres &agrave; chaque objet SPIP. Pour l\'utiliser, vous devez ajouter manuellement en en-t&ecirc;te de vos squelettes le mod&egrave;le "{{insert_head_og}}".
+Le plugin propose également un modèle générant des informations {{Open Graph}}, les metas informations utilisées par Facebook, propres à chaque objet SPIP. Pour l\'utiliser, vous devez ajouter manuellement en en-tête de vos squelettes le modèle "{{insert_head_og}}".
 
-{{Attention - }}Ce mod&egrave;le n&eacute;cessite de recevoir l\'environnement courant, vous devez donc l\'inclure dans chacun des squelettes de pages ({"article.html", "rubrique.html" ...}) et non dans l\'inclusion globale en en-t&ecirc;te ({"inc_head.html"}) en indiquant : 
+{{Attention - }}Ce modèle nécessite de recevoir l\'environnement courant, vous devez donc l\'inclure dans chacun des squelettes de pages ({"article.html", "rubrique.html" ...}) et non dans l\'inclusion globale en en-tête ({"inc_head.html"}) en indiquant : 
 <cadre class=\'spip\'>
-{{&#035;MODELE&#123;insert_head_og&#125;&#123;env&#125;}}
+{{#MODELE{insert_head_og}{env}}}
 </cadre>
 ',
 
-// E //
-	// Documentation
+	// E
 	'exemple' => '{{{Exemple}}}
 
-Les diff&eacute;rents blocs ci-dessous vous pr&eacute;sentent un exemple de chaque mod&egrave;le avec des valeurs fictives. Reportez-vous au mod&egrave;le correspondant pour les options.',
+Les différents blocs ci-dessous vous présentent un exemple de chaque modèle avec des valeurs fictives. Reportez-vous au modèle correspondant pour les options.',
 
-// F //
-	'fb_modeles' => 'Mod&egrave;les Facebook',
+	// F
+	'fb_modeles' => 'Modèles Facebook',
 
-// J //
-	'javascript_inactif' => 'Le javascript est inactif sur votre navigateur. Certaines fonctionnalit&eacute;s de cet outil seront inactives ...',
-	
-// L //
-	'licence' => 'Plugin pour SPIP 2.0+ : {{"Facebook Models" - copyright &#169; 2009 [Piero Wbmstr->http://www.spip-contrib.net/PieroWbmstr] sous licence [GPL->http://www.opensource.org/licenses/gpl-3.0.html] }}.',
+	// I
+	'info_doc' => 'Si vous rencontrez des problèmes pour afficher cette page, [cliquez-ici->@link@].',
+	'info_doc_titre' => 'Note concernant l\'affichage de cette page',
+	'info_skel_contrib' => 'Page de documentation complète en ligne sur spip-contrib : [->http://www.spip-contrib.fr/?article3567].',
+	'info_skel_doc' => 'Cette page de documentation est conçue sous forme de squelette SPIP fonctionnant avec la distribution standard ({fichiers du répertoire "squelettes-dist/"}). Si vous ne parvenez pas à visualiser la page, ou que votre site utilise ses propres squelettes, les liens ci-dessous vous permettent de gérer son affichage :
 
-// N //
-	'new_window' => 'Nouvelle fen&ecirc;tre',
+-* [Mode "texte simple"->@mode_brut@] ({html simple + balise INSERT_HEAD})
+-* [Mode "squelette Zpip"->@mode_zpip@] ({squelette Z compatible})
+-* [Mode "squelette SPIP"->@mode_spip@] ({compatible distribution})',
 
-// P //
+	// J
+	'javascript_inactif' => 'Le javascript est inactif sur votre navigateur. Certaines fonctionnalités de cet outil seront inactives ...',
+
+	// L
+	'licence' => 'Plugin pour SPIP 2.0+ : {{"Facebook Models" - copyright © 2009 [Piero Wbmstr->http://www.spip-contrib.net/PieroWbmstr] sous licence [GPL->http://www.opensource.org/licenses/gpl-3.0.html] }}.',
+
+	// N
+	'new_window' => 'Nouvelle fenêtre',
+	'non' => 'Non',
+
+	// O
+	'oui' => 'Oui',
+
+	// P
 	'page_test' => 'Page de test (locale)',
-	'page_test_in_new_window' => 'Page de test en nouvelle fen&#234;tre',
-	// Documentation
+	'page_test_in_new_window' => 'Page de test en nouvelle fenêtre',
 	'personnalisation' => '{{{Personnalisation}}}
 
-Chaque mod&egrave;le pr&eacute;sente son contenu dans un bloc de type <code>div</code> portant des classes CSS du type <code>fb_modeles fb_XXX</code> o&ugrave; {{XXX}} est le nom du modèle. Cela permet une personnalisation des styles pour l\'ensemble des mod&egrave;les et pour chacun d\'eux.
+Chaque modèle présente son contenu dans un bloc de type <code>div</code> portant des classes CSS du type <code>fb_modeles fb_XXX</code> où {{XXX}} est le nom du modèle. Cela permet une personnalisation des styles pour l\'ensemble des modèles et pour chacun d\'eux.
 
 
 Par exemple pour le module Facebook "Send" :
@@ -94,19 +100,11 @@ Par exemple pour le module Facebook "Send" :
 </div>
 </cadre>',
 
-// T //
-	'titre_original' => 'Facebook Models, plugin pour SPIP 2.0+',
-
-// Infos squelette de doc
+	// S
 	'sep' => '----',
-	'info_doc' => 'Si vous rencontrez des probl&#232;mes pour afficher cette page, [cliquez-ici->@link@].',
-	'info_doc_titre' => 'Note concernant l&#039;affichage de cette page',
-	'info_skel_doc' => 'Cette page de documentation est con&#231;ue sous forme de squelette SPIP fonctionnant avec la distribution standard ({fichiers du r&#233;pertoire &#034;squelettes-dist/&#034;}). Si vous ne parvenez pas &#224; visualiser la page, ou que votre site utilise ses propres squelettes, les liens ci-dessous vous permettent de g&#233;rer son affichage :
 
--* [Mode &#034;texte simple&#034;->@mode_brut@] ({html simple + balise INSERT_HEAD})
--* [Mode &#034;squelette Zpip&#034;->@mode_zpip@] ({squelette Z compatible})
--* [Mode &#034;squelette SPIP&#034;->@mode_spip@] ({compatible distribution})',
-	'info_skel_contrib' => 'Page de documentation compl&egrave;te en ligne sur spip-contrib : [->http://www.spip-contrib.fr/?article3567].',
-
+	// T
+	'titre_original' => 'Facebook Models, plugin pour SPIP 2.0+'
 );
+
 ?>
