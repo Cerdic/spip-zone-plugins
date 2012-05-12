98d9001fd965805620eb2021c608e2b7fa11bcae
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-05-12 01:36:20 +0000

    langues (paquet-fb_modeles)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@61213 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ modeles_facebook/trunk/lang/paquet-fb_modeles_sk.php
@@ -0,0 +1,23 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org/tradlang_module/paquet-fb_modeles?lang_cible=sk
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// F
+	'fb_modeles_description' => 'Ce plugin propose un ensemble de modèles SPIP pour intégrer simplement et rapidement les modules sociaux du site Facebook :
+
+-*  {{send}} : le bouton « Envoyer » classique (avec commentaire)
+-*  {{like}} : le bouton « J\'aime » classique (avec commentaire)
+-*  {{like box}} : liste des commentaires des utilisateurs qui ont « aimé »
+-*  {{activity}} : liste des activités récentes d\'une page ou d\'un profil
+-*  {{comments}} : liste des commentaires et bouton permettant d\'en écrire un
+-*  {{live stream}} (expérimental) : boîte de discussion type « chat »', # NEW
+	'fb_modeles_nom' => 'Šablóny Facebooku',
+	'fb_modeles_slogan' => 'Šablóny na vloženie modulov sociálnej siete Facebook'
+);
+
+?>
