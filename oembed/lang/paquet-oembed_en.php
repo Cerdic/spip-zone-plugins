ab3c67cc9bb568a63229b962ec25aec75f5e9dcc
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-05-13 01:36:54 +0000

    langues (paquet-oembed)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@61243 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ oembed/lang/paquet-oembed_en.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org/tradlang_module/paquet-oembed?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// O
+	'oembed_description' => 'oEmbed client/provider for SPIP.',
+	'oembed_slogan' => 'oEmbed client/provider for SPIP'
+);
+
+?>
