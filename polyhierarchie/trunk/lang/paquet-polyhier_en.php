21e9c4cb1ea958dc1667bf60e2c70ba07e04463c
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-05-13 01:36:57 +0000

    langues (paquet-polyhier)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@61245 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- polyhierarchie/trunk/lang/paquet-polyhier_en.php
+++ polyhierarchie/trunk/lang/paquet-polyhier_en.php
@@ -1,14 +1,16 @@
 <?php
-
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org/tradlang_module/paquet-polyhier?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-	// A
-	'polyhier_slogan' => 'Attach an article or a section on several parent sections.',
+	// P
 	'polyhier_description' => 'Plugin for creating cross-links by referencing sections
 	and articles to other sections that their direct parent.',
-
-
+	'polyhier_slogan' => 'Attach an article or a section on several parent sections.'
 );
+
 ?>
--- polyhierarchie/trunk/lang/paquet-polyhier_fr.php
+++ polyhierarchie/trunk/lang/paquet-polyhier_fr.php
@@ -1,14 +1,14 @@
 <?php
-
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/polyhierarchie/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-	// A
-	'polyhier_slogan' => 'Rattacher un article ou une rubrique à plusieurs rubriques parentes',
+	// P
 	'polyhier_description' => 'Plugin permettant de créer des liens transversaux en rattachant des rubriques
 	et articles à d\'autres rubriques que leur parent direct.',
-
-
+	'polyhier_slogan' => 'Rattacher un article ou une rubrique à plusieurs rubriques parentes'
 );
+
 ?>
