3804aa0ee2e7cffa79bdcf83ee5a428f860ba2d0
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-05-13 01:36:35 +0000

    langues (compositions)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@61235 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- compositions/trunk/lang/compositions_en.php
+++ compositions/trunk/lang/compositions_en.php
@@ -1,43 +1,48 @@
 <?php
-
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-if (!defined("_ECRIRE_INC_VERSION")) return;
+// extrait automatiquement de http://trad.spip.org/tradlang_module/compositions?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-	// A
-	'article_en_accueil' => 'This article was selected as home of the section',
-	'aucun_article_accueil' => 'No article',
 
 	// C
-	'choisir_article_accueil' => 'Choose as home article',
 	'composition' => 'Composition',
-	'compositions' => 'Compositions',
+	'composition_defaut' => 'composition par défaut', # NEW
+	'composition_heritee' => 'héritée', # NEW
+	'composition_utilisee' => 'Used :',
 	'composition_verrouillee' => 'This composition is locked by the webmaster',
-	'composition_utilisee' => 'Used&nbsp;:',
+	'compositions' => 'Compositions',
 
 	// D
 	'des_utilisations' => '@nb@ uses',
 
+	// H
+	'heritages' => 'Cette composition définit des compositions par défaut pour les objets suivants :', # NEW
+
 	// L
+	'label_activer_composition_objets' => 'Utiliser les compositions sur les objets', # NEW
+	'label_branche_verrouillee' => 'Les compositions de cette branche sont verrouillées.', # NEW
 	'label_chemin_compositions' => 'Compositions folder',
 	'label_chemin_compositions_details' => 'Specify the path where the skeletons will be found for composition.',
 	'label_composition' => 'Type of composition',
+	'label_composition_branche_lock' => 'Verrouiller la composition de tous les objets de la branche', # NEW
 	'label_composition_explication' => 'You are Webmaster, you can',
 	'label_composition_lock' => 'Lock the composition',
 	'label_composition_rubrique' => 'Sections composition',
-	'label_id_article_accueil' => 'Home article',
+	'label_information' => 'Information', # NEW
+	'label_masquer_formulaire' => 'Masquer le formulaire', # NEW
+	'label_masquer_formulaire_composition' => 'Masquer le formulaire de choix d\'une composition lorsque l\'utilisateur n\'a pas les droits de la modifier.', # NEW
 	'label_pas_de_composition' => 'No composition',
 	'label_styliser' => 'Selection of skeletons',
 	'label_styliser_auto' => 'Do not use automatic selection. The selection is supported by my skeletons.',
-	'label_utiliser_article_accueil' => 'Do not use home articles of sections',
-
-	// R
-	'rubrique_article_en_accueil' => 'Home article&nbsp;:',
+	'label_tout_verrouiller' => 'Tout verrouiller', # NEW
+	'label_toutes_verrouilles' => 'Toutes les compositions sont verrouillées.', # NEW
+	'label_verrouiller_toutes_compositions' => 'Verrouiller toutes les compositions (seuls les webmestres pourront les modifier).', # NEW
 
 	// U
-	'une_utilisation' => '1 use',
-
-	// V
-	'voir_article_accueil' => 'View article',
+	'une_utilisation' => '1 use'
 );
+
 ?>
--- compositions/trunk/lang/compositions_es.php
+++ compositions/trunk/lang/compositions_es.php
@@ -1,39 +1,48 @@
 <?php
-
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-if (!defined("_ECRIRE_INC_VERSION")) return;
+// extrait automatiquement de http://trad.spip.org/tradlang_module/compositions?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-	// A
-	'article_en_accueil' => 'Este articulo esta seleccionado en inicio de la secci&oacute;n',
-	'aucun_article_accueil' => 'Ningun articulo',
 
 	// C
-	'choisir_article_accueil' => 'Elegir como articulo de inicio',
-	'composition' => 'Composici&oacute;n',
+	'composition' => 'Composición',
+	'composition_defaut' => 'composition par défaut', # NEW
+	'composition_heritee' => 'héritée', # NEW
+	'composition_utilisee' => 'Composition :', # NEW
+	'composition_verrouillee' => 'Esta composición esta cerrada por el webmaster',
 	'compositions' => 'Composiciones',
-	'composition_verrouillee' => 'Esta composici&oacute;n esta cerrada por el webmaster',
 
 	// D
 	'des_utilisations' => '@nb@ utilizaciones',
 
+	// H
+	'heritages' => 'Cette composition définit des compositions par défaut pour les objets suivants :', # NEW
+
 	// L
+	'label_activer_composition_objets' => 'Utiliser les compositions sur les objets', # NEW
+	'label_branche_verrouillee' => 'Les compositions de cette branche sont verrouillées.', # NEW
 	'label_chemin_compositions' => 'Carpeta de las composiciones',
-	'label_chemin_compositions_details' => 'Indicar el camino en cual buscar los esqueletos de composici&oacute;n.',
-	'label_composition' => 'Tipo de composici&oacute;n',
+	'label_chemin_compositions_details' => 'Indicar el camino en cual buscar los esqueletos de composición.',
+	'label_composition' => 'Tipo de composición',
+	'label_composition_branche_lock' => 'Verrouiller la composition de tous les objets de la branche', # NEW
 	'label_composition_explication' => 'Estas Webmaster, puedes',
-	'label_composition_lock' => 'Cerrar la composici&oacute;n',
-	'label_composition_rubrique' => 'Composici&oacute;n de las secciones',
-	'label_id_article_accueil' => 'Articulo de inicio',
-	'label_pas_de_composition' => ' Ninguna composici&oacute;n',
-	'label_styliser' => 'Selecci&oacute;n de los esqueletos',
-	'label_styliser_auto' => 'No utilizar la selecci&oacute;n automatica. La selecci&oacute;n esta manejada por mis esqueletos.',
-	'label_utiliser_article_accueil' => 'No utilizar los articulos de inicio de secci&oacute;n',
+	'label_composition_lock' => 'Cerrar la composición',
+	'label_composition_rubrique' => 'Composición de las secciones',
+	'label_information' => 'Information', # NEW
+	'label_masquer_formulaire' => 'Masquer le formulaire', # NEW
+	'label_masquer_formulaire_composition' => 'Masquer le formulaire de choix d\'une composition lorsque l\'utilisateur n\'a pas les droits de la modifier.', # NEW
+	'label_pas_de_composition' => ' Ninguna composición',
+	'label_styliser' => 'Selección de los esqueletos',
+	'label_styliser_auto' => 'No utilizar la selección automatica. La selección esta manejada por mis esqueletos.',
+	'label_tout_verrouiller' => 'Tout verrouiller', # NEW
+	'label_toutes_verrouilles' => 'Toutes les compositions sont verrouillées.', # NEW
+	'label_verrouiller_toutes_compositions' => 'Verrouiller toutes les compositions (seuls les webmestres pourront les modifier).', # NEW
 
 	// U
-	'une_utilisation' => '1 utilizaci&oacute;n',
-
-	// V
-	'voir_article_accueil' => 'Ver el articulo',
+	'une_utilisation' => '1 utilización'
 );
+
 ?>
