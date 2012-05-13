c7d89415fc240939d1757aa05eab04f9ff639975
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-05-13 01:36:55 +0000

    langues (polyhier)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@61244 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ polyhierarchie/trunk/lang/polyhier_sk.php
@@ -0,0 +1,28 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org/tradlang_module/polyhier?lang_cible=sk
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'articles_associes' => 'Súvisiace články',
+
+	// L
+	'label_autre_parent' => 'Aj v rubrike',
+	'label_autres_parents' => 'Aj v rubrikách',
+	'label_parents' => 'V rubrikách',
+
+	// P
+	'parent_obligatoire' => 'Musíte zadať aspoň jednu rubriku',
+
+	// R
+	'rubriques_associees' => 'Súvisiace rubriky',
+
+	// T
+	'titre_polyhierarchie' => 'Polyhierarchia'
+);
+
+?>
