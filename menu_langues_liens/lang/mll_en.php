377cfd559d8323adb2d65379d2fc30417aa01a85
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-05-13 01:36:43 +0000

    langues (mll)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@61240 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- menu_langues_liens/lang/mll_en.php
+++ menu_langues_liens/lang/mll_en.php
@@ -1,24 +1,26 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/licence/lang/
+// extrait automatiquement de http://trad.spip.org/tradlang_module/mll?lang_cible=en
+// ** ne pas modifier le fichier **
+
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// C
 	'cfg_titre_mll' => 'Language menu as links',
-	
+
 	// E
 	'explication_langues_invisibles' => 'Allows to remove some languages ​​from the menu',
 
 	// L
-	'label_langues_invisibles' => 'Invisible languages in the​​ menu',
-	'label_format' => 'Languages display format',	
-	'label_format_complet' => 'Complete (for instance, Fran&#231;ais)',
-	'label_format_abrege' => 'Shortened (for instance, Fr)',	
+	'label_format' => 'Languages display format',
+	'label_format_abrege' => 'Shortened (for instance, Fr)',
+	'label_format_complet' => 'Complete (for instance, Français)',
 	'label_format_liste' => 'Languages list display format',
 	'label_format_liste_bloc' => 'A block divided by spaces',
 	'label_format_liste_liste' => 'A bulleted list',
+	'label_langues_invisibles' => 'Invisible languages in the​​ menu'
 );
 
 ?>
---
+++ menu_langues_liens/lang/mll_sk.php
@@ -0,0 +1,26 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org/tradlang_module/mll?lang_cible=sk
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cfg_titre_mll' => 'Jazykové menu vo forme odkazov',
+
+	// E
+	'explication_langues_invisibles' => 'Umožňuje odstrániť z menu niektoré jazyky',
+
+	// L
+	'label_format' => 'Formát zobrazenia jazykov',
+	'label_format_abrege' => 'Skrátený (napríklad Sk)',
+	'label_format_complet' => 'Úplný (napríklad Slovenčina)',
+	'label_format_liste' => 'Formát zoznamu jazykov',
+	'label_format_liste_bloc' => 'Blok oddelený medzerami',
+	'label_format_liste_liste' => 'Zoznam s odrážkami',
+	'label_langues_invisibles' => 'Jazyky, ktoré nebudú zobrazené v menu'
+);
+
+?>
