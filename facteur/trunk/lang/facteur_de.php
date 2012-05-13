cc25ac2d27ca91aebfee38e863fd97e0228da788
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-05-13 01:36:40 +0000

    langues (facteur)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@61237 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- facteur/trunk/lang/facteur_de.php
+++ facteur/trunk/lang/facteur_de.php
@@ -1,8 +1,14 @@
 <?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org/tradlang_module/facteur?lang_cible=de
+// ** ne pas modifier le fichier **
 
-if (!defined("_ECRIRE_INC_VERSION")) return;
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// Z
+	'Z' => 'ZZzZZzzz',
 
 	// C
 	'config_info_enregistree' => 'Die Konfiguration des Briedträgers wurde gespeichert',
@@ -11,28 +17,33 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'configuration_mailer' => 'Konfiguration des Mailers',
 	'configuration_smtp' => 'Auswahl der Versandmethode',
 	'configuration_smtp_descriptif' => 'Im Zweifel hier die mail() Funktion von PHP eintragen.',
-	'corps_email_de_test' => 'Das ist ein Versandtest mit Sondärzeichen: Bär Größe Maß accentué',  // Laisser tel quel, car
-	
+	'corps_email_de_test' => 'Das ist ein Versandtest mit Sondärzeichen: Bär Größe Maß accentué',
+
 	// E
 	'email_test_envoye' => 'Die Testmail wurde fehlerfrei versand. Falls sie nicht richtig ankommt,
                       bearbeiten sie ihre Serverkonfiguration oder kontaktieren sie den Administrator.',
 	'erreur' => 'Fehler',
+	'erreur_dans_log' => ' : consultez le fichier log pour plus de détails', # NEW
 	'erreur_generale' => 'Mehrere Konfigurationsfehler. Bitte Inhalt des Formulars korrigieren.',
 	'erreur_invalid_host' => 'Servername inkorrekt',
 	'erreur_invalid_port' => 'Portnummer inkorrekt',
-	
+
 	// F
 	'facteur_adresse_envoi_email' => 'E-Mail :',
 	'facteur_adresse_envoi_nom' => 'Name:',
+	'facteur_bcc' => 'Copie Cachée (BCC) :', # NEW
+	'facteur_cc' => 'Copie (CC) :', # NEW
+	'facteur_copies' => 'Copies :', # NEW
+	'facteur_copies_descriptif' => 'Un email sera envoyé en copie aux adresses définies. Une seule adresse en copie et/ou une seule adresse en copie cachée.', # NEW
 	'facteur_filtre_accents' => 'Sonderzeichen in HTML-Entitäten umwandeln (z.B. für Hotmail).',
 	'facteur_filtre_css' => 'Stile zwischen <head> und </head> zu "inline" Stilen umwandeln, sinnvoll für Webmail die interne Stile externen vorzieht.',
-	'facteur_filtre_iso_8859' => 'Nach ISO-8859-1 umwandeln',
 	'facteur_filtre_images' => 'Verlinkte Bilder in E-Mail einbetten',
+	'facteur_filtre_iso_8859' => 'Nach ISO-8859-1 umwandeln',
 	'facteur_filtres' => 'Filter',
 	'facteur_filtres_descriptif' => 'Beim Versand können mehrere Filter eingesetzt werden.',
 	'facteur_smtp_auth' => 'Autorisierung erforderlich:',
-	'facteur_smtp_auth_oui' => 'ja',
 	'facteur_smtp_auth_non' => 'nein',
+	'facteur_smtp_auth_oui' => 'ja',
 	'facteur_smtp_host' => 'Server:',
 	'facteur_smtp_password' => 'Passwort:',
 	'facteur_smtp_port' => 'Port:',
@@ -43,29 +54,26 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'facteur_smtp_sender' => 'Fehlercodes (optional)',
 	'facteur_smtp_sender_descriptif' => 'Legt im Kopf der Mail die Empfängeradresse für Fehlermeldungen fest (bzw. den Return-Path), bestimmt ebenfalls die Absenderadresse bei Versand per SMTP.',
 	'facteur_smtp_username' => 'Benutzername:',
-	
+
 	// N
 	'note_test_configuration' => 'Eine Mail wird an die Absendeadresse geschickt (oder an den Webmaster).',
-	
+
 	// P
 	'personnaliser' => 'Individuelle Einstellungen',
-	
+
 	// T
 	'tester' => 'Testen',
 	'tester_la_configuration' => 'Konfiguration testen',
-	
+
 	// U
 	'utiliser_mail' => 'Funktion mail() von PHP verwenden',
 	'utiliser_reglages_site' => 'Einstellungen von SPIP verwenden: als Name wird die Bezeichnung der SPIP-Website verwendet und als Adresse die des Webmasters.',
 	'utiliser_smtp' => 'SMTP verwenden',
-	
+
 	// V
 	'valider' => ' OK ',
 	'version_html' => 'HTML-Version.',
-	'version_texte' => 'Textversion.',
-	
-	'Z' => 'ZZzZZzzz'
-
+	'version_texte' => 'Textversion.'
 );
 
 ?>
--- facteur/trunk/lang/facteur_en.php
+++ facteur/trunk/lang/facteur_en.php
@@ -1,58 +1,78 @@
 <?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org/tradlang_module/facteur?lang_cible=en
+// ** ne pas modifier le fichier **
 
-if (!defined("_ECRIRE_INC_VERSION")) return;
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-'configuration_facteur' => 'Postman',
-'configuration_adresse_envoi' => "Sender's address",
-'utiliser_reglages_site' => "Use the site's settings : the email address is the webmaster's one and the name of the website is the name of the sender",
-'personnaliser' => "Customize",
-'facteur_adresse_envoi_nom' => "Name :",
-'facteur_adresse_envoi_email' => "Email :",
-'facteur_bcc' => "Blind Carbon Copy (BCC) :",
-'facteur_cc' => "Carbon Copy (CC) :",
-'facteur_copies' => "Copies :",
-'facteur_copies_descriptif' => "An email will be sent to specified adresses. One Carbon Copy and/or one Blind Carbon Copy.",
-'configuration_mailer' => 'Mailer\'s configuration',
-'configuration_smtp' => 'Choose your mailer',
-'configuration_smtp_descriptif' => 'If you\'re not sure about the settings, leave them set to "PHP mail".',
-'config_info_enregistree' => 'Postman\'s configuration is now saved',
-'utiliser_mail' => 'Use mail function from PHP',
-'utiliser_smtp' => 'Use SMTP',
-'erreur_invalid_host' => 'This host name is not valid',
-'erreur_invalid_port' => 'This port number is not valid',
-'facteur_smtp_host' => 'Host :',
-'facteur_smtp_port' => 'Port :',
-'facteur_smtp_auth' => 'Requires authentication :',
-'facteur_smtp_auth_oui' => 'yes',
-'facteur_smtp_auth_non' => 'no',
-'facteur_smtp_username' => 'Username :',
-'facteur_smtp_password' => 'Paswword :',
-'facteur_smtp_secure' => 'Secure :',
-'facteur_smtp_secure_non' => 'no',
-'facteur_smtp_secure_ssl' => 'SSL',
-'facteur_smtp_secure_tls' => 'TLS',
-'facteur_smtp_sender' => 'Return-Path (optional)',
-'facteur_smtp_sender_descriptif' => 'Define the Return-Path in the mail header, useful for error feedback, also in SMTP mode it defines the sender\'s email.',
-'facteur_filtres' => "Filters",
-'facteur_filtres_descriptif' => "Some filters can be applied before sending an email.",
-'facteur_filtre_images' => "Embed images referenced in emails",
-'facteur_filtre_css' => "Transform styles present between &lt;head&gt; and &lt;/head&gt; into inline styles, useful for webmails because inline styles overwrite external styles.",
-'facteur_filtre_accents' => "Transform accents into their html entities (useful for Hotmail).",
-'facteur_filtre_iso_8859' => "Convert to ISO-8859-1",
-'valider' => "Submit",
-'tester_la_configuration' => "Test the config",
-'note_test_configuration' => "A test email will be sent to the \"sender\".",
-'corps_email_de_test' => "This is a test email",
-'version_html' => "HTML version.",
-'version_texte' => "Text version.",
-'tester' => "Test",
-'erreur' => "Error",
-'email_test_envoye' => "The test email has been sent without error!",
+	// Z
+	'Z' => 'ZZzZZzzz',
 
-'Z' => 'ZZzZZzzz'
+	// C
+	'config_info_enregistree' => 'Postman\'s configuration is now saved',
+	'configuration_adresse_envoi' => 'Sender\'s address',
+	'configuration_facteur' => 'Postman',
+	'configuration_mailer' => 'Mailer\'s configuration',
+	'configuration_smtp' => 'Choose your mailer',
+	'configuration_smtp_descriptif' => 'If you\'re not sure about the settings, leave them set to "PHP mail".',
+	'corps_email_de_test' => 'This is a test email',
 
+	// E
+	'email_test_envoye' => 'The test email has been sent without error!',
+	'erreur' => 'Error',
+	'erreur_dans_log' => ' : consultez le fichier log pour plus de détails', # NEW
+	'erreur_generale' => 'Il y a une ou plusieurs erreurs de configuration. Veuillez vérifier le contenu du formulaire.', # NEW
+	'erreur_invalid_host' => 'This host name is not valid',
+	'erreur_invalid_port' => 'This port number is not valid',
+
+	// F
+	'facteur_adresse_envoi_email' => 'Email :',
+	'facteur_adresse_envoi_nom' => 'Name :',
+	'facteur_bcc' => 'Blind Carbon Copy (BCC) :',
+	'facteur_cc' => 'Carbon Copy (CC) :',
+	'facteur_copies' => 'Copies :',
+	'facteur_copies_descriptif' => 'An email will be sent to specified adresses. One Carbon Copy and/or one Blind Carbon Copy.',
+	'facteur_filtre_accents' => 'Transform accents into their html entities (useful for Hotmail).',
+	'facteur_filtre_css' => 'Transform styles present between &lt;head&gt; and &lt;/head&gt; into inline styles, useful for webmails because inline styles overwrite external styles.',
+	'facteur_filtre_images' => 'Embed images referenced in emails',
+	'facteur_filtre_iso_8859' => 'Convert to ISO-8859-1',
+	'facteur_filtres' => 'Filters',
+	'facteur_filtres_descriptif' => 'Some filters can be applied before sending an email.',
+	'facteur_smtp_auth' => 'Requires authentication :',
+	'facteur_smtp_auth_non' => 'no',
+	'facteur_smtp_auth_oui' => 'yes',
+	'facteur_smtp_host' => 'Host :',
+	'facteur_smtp_password' => 'Paswword :',
+	'facteur_smtp_port' => 'Port :',
+	'facteur_smtp_secure' => 'Secure :',
+	'facteur_smtp_secure_non' => 'no',
+	'facteur_smtp_secure_ssl' => 'SSL',
+	'facteur_smtp_secure_tls' => 'TLS',
+	'facteur_smtp_sender' => 'Return-Path (optional)',
+	'facteur_smtp_sender_descriptif' => 'Define the Return-Path in the mail header, useful for error feedback, also in SMTP mode it defines the sender\'s email.',
+	'facteur_smtp_username' => 'Username :',
+
+	// N
+	'note_test_configuration' => 'A test email will be sent to the "sender".',
+
+	// P
+	'personnaliser' => 'Customize',
+
+	// T
+	'tester' => 'Test',
+	'tester_la_configuration' => 'Test the config',
+
+	// U
+	'utiliser_mail' => 'Use mail function from PHP',
+	'utiliser_reglages_site' => 'Use the site\'s settings : the email address is the webmaster\'s one and the name of the website is the name of the sender',
+	'utiliser_smtp' => 'Use SMTP',
+
+	// V
+	'valider' => 'Submit',
+	'version_html' => 'HTML version.',
+	'version_texte' => 'Text version.'
 );
 
 ?>
