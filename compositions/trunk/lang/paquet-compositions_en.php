e4be73bd91f362fb84cc0adcedbfe0b3c6d3d4ba
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-05-13 01:36:37 +0000

    langues (paquet-compositions)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@61236 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- compositions/trunk/lang/paquet-compositions_en.php
+++ compositions/trunk/lang/paquet-compositions_en.php
@@ -1,18 +1,15 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-compositions
-// Langue: en
-// Date: 17-11-2011 16:23:17
-// Items: 1
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org/tradlang_module/paquet-compositions?lang_cible=en
+// ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// C
+	// C
 	'compositions_description' => 'This plugin allows you to define several variants of compositions for each kind of SPIP objects (article, sections, etc..) and to easily apply them, case by case, from the private area.',
+	'compositions_slogan' => 'Differ page layouts according to sections, articles...'
 );
+
 ?>
--- compositions/trunk/lang/paquet-compositions_fr.php
+++ compositions/trunk/lang/paquet-compositions_fr.php
@@ -1,19 +1,13 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-compositions
-// Langue: fr
-// Date: 17-11-2011 16:23:17
-// Items: 2
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/compositions/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// C
+	// C
 	'compositions_description' => 'Ce plugin vous permet de définir plusieurs variantes de compositions pour chaque type objet SPIP (article, rubrique, etc.) et de les appliquer simplement, cas par cas, depuis l\'espace privé.',
-	'compositions_slogan' => 'Varier les mises en page selon les rubriques, les articles...',
+	'compositions_slogan' => 'Varier les mises en page selon les rubriques, les articles...'
 );
+
 ?>
