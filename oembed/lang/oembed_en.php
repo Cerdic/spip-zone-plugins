25aac3209a3a850a31ae51d958354da4d6fd7209
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-05-13 01:36:53 +0000

    langues (oembed)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@61242 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ oembed/lang/oembed_en.php
@@ -0,0 +1,45 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org/tradlang_module/oembed?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'aucun_provider' => 'No provider',
+
+	// E
+	'erreur_ajout_provider' => 'Error when adding the provider.',
+	'erreur_scheme_doublon' => 'This scheme is already in the list.',
+	'explication_scheme' => 'use * as wildcard',
+	'explication_upload_url' => 'Simply specify the URL of the page that contains the document for sites @hosts@',
+
+	// I
+	'info_1_provider' => '1 provider',
+	'info_nb_providers' => '@nb@ providers',
+
+	// L
+	'label_detecter_lien' => 'Automatically detect providers (ignore the white list)?',
+	'label_embed_auto' => 'Automatically convert URLs inserted in the text?',
+	'label_endpoint' => 'Endpoint',
+	'label_maxheight' => 'Maximum height by default (px)',
+	'label_maxwidth' => 'Maximum width by default (px)',
+	'label_scheme' => 'Scheme',
+
+	// O
+	'ok_ajout_provider' => 'Provider added',
+
+	// S
+	'supprimer_provider' => 'Delete',
+
+	// T
+	'titre_ajouter_provider' => 'Add a provider',
+	'titre_configuration_avancee_providers' => 'Access the advanced configuration of oEmbed providers',
+	'titre_configurer_oembed' => 'Configure oEmbed',
+	'titre_configurer_providers' => 'oEmbed providers',
+	'titre_oembed' => 'oEmbed'
+);
+
+?>
--- oembed/lang/oembed_fr.php
+++ oembed/lang/oembed_fr.php
@@ -1,37 +1,43 @@
 <?php
-
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/oembed/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-'aucun_provider'=>'Aucun provider',
+	// A
+	'aucun_provider' => 'Aucun provider',
 
-'erreur_ajout_provider'=>'Erreur lors de l\'ajout du provider.',
-'erreur_scheme_doublon'=>'Ce scheme est déjà présent dans la liste.',
-'explication_scheme'=>'utilisez * comme wildcard',
-'explication_upload_url' => 'Indiquez simplement l\'URL de la page qui contient le document pour les sites @hosts@',
+	// E
+	'erreur_ajout_provider' => 'Erreur lors de l\'ajout du provider.',
+	'erreur_scheme_doublon' => 'Ce scheme est déjà présent dans la liste.',
+	'explication_scheme' => 'utilisez * comme wildcard',
+	'explication_upload_url' => 'Indiquez simplement l\'URL de la page qui contient le document pour les sites @hosts@',
 
-'info_1_provider'=>'1 provider',
-'info_nb_providers'=>'@nb@ providers',
+	// I
+	'info_1_provider' => '1 provider',
+	'info_nb_providers' => '@nb@ providers',
 
-'ok_ajout_provider'=>'Provider ajouté',
+	// L
+	'label_detecter_lien' => 'Détecter automatiquement les providers (ignorer la liste blanche) ?',
+	'label_embed_auto' => 'Transformer automatiquement les URLs insérées dans le texte ?',
+	'label_endpoint' => 'Endpoint',
+	'label_maxheight' => 'Hauteur maxi. par défaut (px)',
+	'label_maxwidth' => 'Largeur maxi. par défaut (px)',
+	'label_scheme' => 'Scheme',
 
-'label_detecter_lien'=>'Détecter automatiquement les providers (ignorer la liste blanche) ?',
-'label_embed_auto'=>'Transformer automatiquement les URLs insérées dans le texte ?',
-'label_endpoint'=>'Endpoint',
-'label_scheme'=>'Scheme',
-'label_maxwidth' => 'Largeur maxi. par défaut (px)',
-'label_maxheight' => 'Hauteur maxi. par défaut (px)',
+	// O
+	'ok_ajout_provider' => 'Provider ajouté',
 
-'supprimer_provider'=>'Supprimer',
+	// S
+	'supprimer_provider' => 'Supprimer',
 
-'titre_oembed'=>'oEmbed',
-'titre_configurer_oembed'=>'Configurer oEmbed',
-'titre_configurer_providers'=>'Providers oEmbed',
-'titre_ajouter_provider'=>'Ajouter un provider',
-'titre_configuration_avancee_providers' => 'Accéder à la configuration avancée des fournisseurs oEmbed',
-
+	// T
+	'titre_ajouter_provider' => 'Ajouter un provider',
+	'titre_configuration_avancee_providers' => 'Accéder à la configuration avancée des fournisseurs oEmbed',
+	'titre_configurer_oembed' => 'Configurer oEmbed',
+	'titre_configurer_providers' => 'Providers oEmbed',
+	'titre_oembed' => 'oEmbed'
 );
-
 
 ?>
