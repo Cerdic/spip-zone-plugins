3376cca11cd37693de72a6c80c5ff26ce74ad640
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-05-13 01:36:49 +0000

    langues (paquet-mll)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@61241 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- menu_langues_liens/lang/paquet-mll_en.php
+++ menu_langues_liens/lang/paquet-mll_en.php
@@ -1,20 +1,16 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-mll
-// Langue: en
-// Date: 06-12-2011 12:21:22
-// Items: 3
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org/tradlang_module/paquet-mll?lang_cible=en
+// ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// M
+	// M
 	'mll_description' => 'Replace the original language menu by a clickable menu. This menu is displayed only if at least two languages are ​​actually used on the site.',
 	'mll_nom' => 'Language menu as links',
-	'mll_slogan' => 'View languages ​​as clickable links',
+	'mll_slogan' => 'View languages ​​as clickable links'
 );
+
 ?>
