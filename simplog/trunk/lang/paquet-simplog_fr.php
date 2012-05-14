b52bb96e7b80e7cdda2921e0775e4fad05c268bc
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-05-14 07:49:55 +0000

    paquet.xml + son fichier de langue => on ne reste compat que SPIP 3.0
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@61264 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ simplog/trunk/lang/paquet-simplog_fr.php
@@ -0,0 +1,20 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-simplog
+// Langue: fr
+// Date: 14-05-2012 09:42:35
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// S
+	'simplog_description' => 'Une fois le plugin activé, les administrateurs ont la possibilité de consulter tous les journaux disponibles sur le site. Une entrée du menu Maintenance permet de se rendre sur la page de consultation (simplog).',
+	'simplog_nom' => 'Simples Logs',
+	'simplog_slogan' => 'Visualisation des logs SPIP par les administrateurs',
+);
+?>
