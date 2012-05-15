ad2c197299838b03aa05fbb9e5e8398b1bf3aef1
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-05-15 01:36:28 +0000

    langues (paquet-oembed)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@61286 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ oembed/lang/paquet-oembed_sk.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org/tradlang_module/paquet-oembed?lang_cible=sk
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// O
+	'oembed_description' => 'Klient/poskytovateľ oEmbedu pre SPIP.',
+	'oembed_slogan' => 'Klient/poskytovateľ oEmbedu pre SPIP'
+);
+
+?>
