bf9c1ef3f90f17d889898afa0a2ce68936783d66
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-05-15 10:47:50 +0000

    On garde les dates dans SPIP => pas besoin de les traduire ici à nouveau (minimisons les efforts des traducteurs)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@61300 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- calendrier_mini/trunk/lang/minical_fr.php
+++ calendrier_mini/trunk/lang/minical_fr.php
@@ -8,15 +8,6 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// A
 	'aucune_date' => 'Rien pour ce mois',
 
-	// D
-	'date_jour_abbr_1' => 'dim.',
-	'date_jour_abbr_2' => 'lun.',
-	'date_jour_abbr_3' => 'mar.',
-	'date_jour_abbr_4' => 'mer.',
-	'date_jour_abbr_5' => 'jeu.',
-	'date_jour_abbr_6' => 'ven.',
-	'date_jour_abbr_7' => 'sam.',
-
 	// M
 	'mois_precedent' => 'Mois précédent',
 	'mois_suivant' => 'Mois suivant'
