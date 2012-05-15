7b42939da2f2fe50f0607d145769b0bd89e3abd8
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-05-15 11:06:44 +0000

    Le nom du plugin dans le fichier de langue du paquet.xml
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@61305 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- champs_extras/extensions/titre_court_rubriques/trunk/lang/paquet-titrecourt_fr.php
+++ champs_extras/extensions/titre_court_rubriques/trunk/lang/paquet-titrecourt_fr.php
@@ -1,9 +1,9 @@
 <?php
 if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-	
-	'titrecourt_slogan' => 'Pour apprendre &agrave; ajouter un champs extra',
+	'titre_court_nom' => 'Titre Court pour Rubriques',
 	'titrecourt_description' => 'Ajoute un champ "titre_court" sur les rubriques de SPIP.',
+	'titrecourt_slogan' => 'Pour apprendre &agrave; ajouter un champs extra',
 	
 );
 ?>
