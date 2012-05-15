edb90c69be1075be6a6af45a91a3dae49961ff8c
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-05-15 01:36:27 +0000

    langues (oembed)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@61285 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ oembed/lang/oembed_sk.php
@@ -0,0 +1,45 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org/tradlang_module/oembed?lang_cible=sk
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'aucun_provider' => 'Žiaden poskytovateľ',
+
+	// E
+	'erreur_ajout_provider' => 'Chyba pri pridávaní poskytovateľa.',
+	'erreur_scheme_doublon' => 'Táto schéma sa už v zozname nachádza.',
+	'explication_scheme' => 'ako zástupný znak použite *',
+	'explication_upload_url' => 'Jednoducho zadajte internetovú adresu stránky, na ktorej je dokument pre stránky @hosts@',
+
+	// I
+	'info_1_provider' => '1 poskytovateľ',
+	'info_nb_providers' => '@nb@ poskytovateľov',
+
+	// L
+	'label_detecter_lien' => 'Zistiť poskytovateľov automaticky (ignorovať biely zoznam)?',
+	'label_embed_auto' => 'Meniť automaticky internetové adresy vložené do textu?',
+	'label_endpoint' => 'Koncový bod',
+	'label_maxheight' => 'Predvolená max. výška (px)',
+	'label_maxwidth' => 'Predvolená max. šírka (px)',
+	'label_scheme' => 'Schéma',
+
+	// O
+	'ok_ajout_provider' => 'Poskytovateľ pridaný',
+
+	// S
+	'supprimer_provider' => 'Odstrániť',
+
+	// T
+	'titre_ajouter_provider' => 'Pridať poskytovateľa',
+	'titre_configuration_avancee_providers' => 'Dostať sa k podrobným nastaveniam poskytovateľov v oEmbede',
+	'titre_configurer_oembed' => 'Nastaviť oEmbed',
+	'titre_configurer_providers' => 'oEmbedu – Poskytovatelia',
+	'titre_oembed' => 'oEmbed'
+);
+
+?>
