60dcd3805ffe3af5d397c6dfbd99d944c90c1415
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-05-15 11:05:29 +0000

    Le nom se traduit par module_nom et pas module_titre (on laisse module_titre pour l'instant pour reporter les valeurs dans tradlang), on le supprimera par la suite
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@61304 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- champs_extras/interface/trunk/lang/paquet-iextras_fr.php
+++ champs_extras/interface/trunk/lang/paquet-iextras_fr.php
@@ -8,6 +8,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// I
 	'iextras_description' => 'Ajoute dans l\'espace privé une interface complète de gestion de champs supplémentaires
 							dans les objets éditoriaux.',
+	'iextras_nom' => 'Champs Extras (Interface)',
 	'iextras_slogan' => 'Offre une interface graphique pour gérer des champs extras',
 	'iextras_titre' => 'Champs Extras (Interface)'
 );
