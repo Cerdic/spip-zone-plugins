becb4e8b88bca97e47aee4a14f5b1d8e1a6c4958
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-05-15 01:36:20 +0000

    langues (facteur)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@61283 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- facteur/trunk/lang/facteur_en.php
+++ facteur/trunk/lang/facteur_en.php
@@ -12,7 +12,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// C
 	'config_info_enregistree' => 'Postman\'s configuration is now saved',
-	'configuration_adresse_envoi' => 'Sender\'s address',
+	'configuration_adresse_envoi' => 'Sender\'s address configuration',
 	'configuration_facteur' => 'Postman',
 	'configuration_mailer' => 'Mailer\'s configuration',
 	'configuration_smtp' => 'Choose your mailer',
@@ -20,19 +20,19 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'corps_email_de_test' => 'This is a test email',
 
 	// E
-	'email_test_envoye' => 'The test email has been sent without error!',
+	'email_test_envoye' => 'The test email was successfully sent. If you do not receive it correctly, check the configuration of your server or contact a server administrator.',
 	'erreur' => 'Error',
-	'erreur_dans_log' => ' : consultez le fichier log pour plus de détails', # NEW
-	'erreur_generale' => 'Il y a une ou plusieurs erreurs de configuration. Veuillez vérifier le contenu du formulaire.', # NEW
+	'erreur_dans_log' => ': check the log file for more details',
+	'erreur_generale' => 'There are one or more configuration errors. Please check the contents of the form.',
 	'erreur_invalid_host' => 'This host name is not valid',
 	'erreur_invalid_port' => 'This port number is not valid',
 
 	// F
-	'facteur_adresse_envoi_email' => 'Email :',
-	'facteur_adresse_envoi_nom' => 'Name :',
-	'facteur_bcc' => 'Blind Carbon Copy (BCC) :',
-	'facteur_cc' => 'Carbon Copy (CC) :',
-	'facteur_copies' => 'Copies :',
+	'facteur_adresse_envoi_email' => 'Email:',
+	'facteur_adresse_envoi_nom' => 'Name:',
+	'facteur_bcc' => 'Blind Carbon Copy (BCC):',
+	'facteur_cc' => 'Carbon Copy (CC):',
+	'facteur_copies' => 'Copies:',
 	'facteur_copies_descriptif' => 'An email will be sent to specified adresses. One Carbon Copy and/or one Blind Carbon Copy.',
 	'facteur_filtre_accents' => 'Transform accents into their html entities (useful for Hotmail).',
 	'facteur_filtre_css' => 'Transform styles present between &lt;head&gt; and &lt;/head&gt; into inline styles, useful for webmails because inline styles overwrite external styles.',
@@ -40,19 +40,19 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'facteur_filtre_iso_8859' => 'Convert to ISO-8859-1',
 	'facteur_filtres' => 'Filters',
 	'facteur_filtres_descriptif' => 'Some filters can be applied before sending an email.',
-	'facteur_smtp_auth' => 'Requires authentication :',
+	'facteur_smtp_auth' => 'Requires authentication:',
 	'facteur_smtp_auth_non' => 'no',
 	'facteur_smtp_auth_oui' => 'yes',
-	'facteur_smtp_host' => 'Host :',
-	'facteur_smtp_password' => 'Paswword :',
-	'facteur_smtp_port' => 'Port :',
-	'facteur_smtp_secure' => 'Secure :',
+	'facteur_smtp_host' => 'Host:',
+	'facteur_smtp_password' => 'Password:',
+	'facteur_smtp_port' => 'Port:',
+	'facteur_smtp_secure' => 'Secure:',
 	'facteur_smtp_secure_non' => 'no',
 	'facteur_smtp_secure_ssl' => 'SSL',
 	'facteur_smtp_secure_tls' => 'TLS',
 	'facteur_smtp_sender' => 'Return-Path (optional)',
 	'facteur_smtp_sender_descriptif' => 'Define the Return-Path in the mail header, useful for error feedback, also in SMTP mode it defines the sender\'s email.',
-	'facteur_smtp_username' => 'Username :',
+	'facteur_smtp_username' => 'Username:',
 
 	// N
 	'note_test_configuration' => 'A test email will be sent to the "sender".',
@@ -66,7 +66,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// U
 	'utiliser_mail' => 'Use mail function from PHP',
-	'utiliser_reglages_site' => 'Use the site\'s settings : the email address is the webmaster\'s one and the name of the website is the name of the sender',
+	'utiliser_reglages_site' => 'Use the site\'s settings: the email address is the webmaster\'s one and the name of the website is the name of the sender',
 	'utiliser_smtp' => 'Use SMTP',
 
 	// V
