591ee6787a02d03e18692447a95b95758b57a5df
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-05-15 11:04:51 +0000

    Le nom se traduit par module_nom et pas module_titre (on laisse module_titre pour l'instant pour reporter les valeurs dans tradlang), on le supprimera par la suite
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@61303 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- champs_extras/core/trunk/lang/paquet-cextras_fr.php
+++ champs_extras/core/trunk/lang/paquet-cextras_fr.php
@@ -9,6 +9,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'cextras_description' => 'Offre une API simple permettant de créer de nouveaux champs dans les objets éditoriaux.
 					Il est donc le socle pour d\'autres plugins notamment pour «Champs Extras Interface» qui donne
 					une interface graphique de gestion de ces nouveaux champs.',
+	'cextras_nom' => 'Champs Extras',
 	'cextras_slogan' => 'API de gestion de nouveaux champs dans les objets éditoriaux.',
 	'cextras_titre' => 'Champs Extras'
 );
