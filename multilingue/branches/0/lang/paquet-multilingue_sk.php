d666d40ea804a89edcd2a9069cf715f95b70e980
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-05-15 15:41:34 +0000

    Mettre des boucles autours des noisettes zspip
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@61319 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- multilingue/branches/0/lang/paquet-multilingue_sk.php
+++ multilingue/branches/0/lang/paquet-multilingue_sk.php
@@ -8,25 +8,22 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// M
-	'multilingue_description' => 'Met en place un site multilingue avec une langue par secteur.
+	'multilingue_description' => 'Nastaví viacjazyčnú stránku s jazykom pre daný sektor.
 	
-	Le plugin force l\'installation de, plusieurs plugin
+	Zásuvný modul si vynúti inštaláciu viacerých zásuvných modulov na
 	
-	- [Traduction entre rubriques->http://plugins.spip.net/tradrub.html]
-	- [Langue préférée par l’internaute ->http://www.spip-contrib.net/Plugin-Langue-preferee-par-l-internaute]
-	- [Traductions de rubriques autrement->http://www.spip-contrib.net/Traductions-de-rubriques-autrement-3]
-	- [Traductions d’articles autremen->http://www.spip-contrib.net/Traductions-d-articles-autrement-3]
+	- [preklad rubrík,->http://plugins.spip.net/tradrub.html]
+	- [jazyk, ktorý preferuje používateľ,->http://www.spip-contrib.net/Plugin-Langue-preferee-par-l-internaute]
+	- [preklady iných rubrík,->http://www.spip-contrib.net/Traductions-de-rubriques-autrement-3]
+	- [preklady iných článkov.->http://www.spip-contrib.net/Traductions-d-articles-autrement-3]
 	
-	Sous Spip 2 ou 2.1 il es recommendable d\'installer auparavant le plugin [Step->http://www.spip-contrib.net/STEP-Systeme-de-Telechargements-de-Plugins], puis installer ce plugin ce qui forcera également l\'installation de tous les plugin nécessaires.
+	V Spipe 2 alebo 2.1 sa odporúča nainštalovať ich pred inštaláciou zásuvného modulu [Step->http://www.spip-contrib.net/STEP-Systeme-de-Telechargements-de-Plugins], potom nainštalujte tento zásuvný modul, ktorý si vynúti aj inštaláciu všetkých potrebných zásuvných modulov.
 	
+	Zásuvný modul mení aj niektoré šablóny (rubriky a články) a niekoľko základných orieškov.
 	
-	Le plugin modifie également quelques squelettes (rubriques et articles) et quelques noisettes de bases.
-	
-	Une fois installé les plugins nécessaires, il vous suffira de déterminer les langues du site dans "Configuration"->"Mulilinguisme/Gestion des langues
+	Keď nainštalujete potrebné zásuvné moduly, jazyky stránky vyberte v menu  "Konfigurácia"->"Viacjazyčnosť/Riadenie jazykov"
 
-	et voilà votre site est multilingue
-	
-	', # NEW
+	a potom bude vaša stránka viacjazyčná',
 	'multilingue_nom' => 'Jednoduchá viacjazyčná stránka',
 	'multilingue_slogan' => 'Vytvorte jednoduchú viacjazyčnú stránku'
 );
