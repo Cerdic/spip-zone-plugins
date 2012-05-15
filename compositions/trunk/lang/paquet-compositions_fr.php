f3a5bbac7a9060f66f5b5d59a3af372b94804bb9
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-05-15 11:13:05 +0000

    Le nom du plugin dans le fichier de langue du paquet.xml
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@61308 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- compositions/trunk/lang/paquet-compositions_fr.php
+++ compositions/trunk/lang/paquet-compositions_fr.php
@@ -7,6 +7,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// C
 	'compositions_description' => 'Ce plugin vous permet de définir plusieurs variantes de compositions pour chaque type objet SPIP (article, rubrique, etc.) et de les appliquer simplement, cas par cas, depuis l\'espace privé.',
+	'compositions_nom' => 'Compositions',
 	'compositions_slogan' => 'Varier les mises en page selon les rubriques, les articles...'
 );
 
