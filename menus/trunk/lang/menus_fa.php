046ff988c3e2cc95c4e5ac96fa1b438279107e7e
davoodhossein@yahoo.com <davoodhossein@yahoo.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-05-16 01:39:50 +0000

    langues (menus)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@61345 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ menus/trunk/lang/menus_fa.php
@@ -0,0 +1,179 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org/tradlang_module/menus?lang_cible=fa
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'confirmer_supprimer_entree' => 'واقعاً مي‌خواهيد اين مدخل را حذف كنيد؟',
+
+	// D
+	'description_menu_accueil' => 'پيوند به صفحه‌ي پذيرش سايت.',
+	'description_menu_articles_rubrique' => 'نمايش فهرست مقالات يك بخش ',
+	'description_menu_deconnecter' => 'اگر بازديد كننده وصل شد، يك مدخل اضافه كنيد تا قطع شدن را به او ارايه دهد.',
+	'description_menu_espace_prive' => 'پيوندي كه ارتباط با سايت را فعال مي‌كند در صورتي كه قبلاً به سايت وصل نشده باشيد، و بعد وارد قسمت شخصي سايت شويد البته اگر مجاز باشيد.  ',
+	'description_menu_groupes_mots' => 'به طور خودكار گروه‌واژه‌ها و مقالات مرتبط با آن‌ها را فهرست مي‌كند. اين فهرست به صورت پيش‌ گزيده گروه‌‌واژه‌ها و كليدواژه‌هاي تحت آن‌ها را نشان مي‌دهد. اگر اسكلت يك groupes_mots.html وجود داشته باشد، پيوند به آن گروه‌واژه فعال مي‌شود. ',
+	'description_menu_lien' => 'افزودن يك پيوند فرداً تعيين شده، هم دروني (يوآرال نسبي) و هم بروني (http://...)',
+	'description_menu_mapage' => 'اگر بازديد‌كننده متصل است، يك پيوند به صفحه‌ي نويسنده‌‌‌اش اضافه كن.',
+	'description_menu_mots' => 'خودكار يك منوي فهرست تمام مقالات مرتبط با كليدواژه را نشان بده.',
+	'description_menu_objet' => 'ايجاد پيوند به چيزهاي اسپيپي: مقاله، بخش يا چيزهاي ديگر. پيش‌گزيده، هر مدخل تيتر آن چيز را داراست.',
+	'description_menu_page_speciale' => 'افزودن پيوند به صفحه‌ي اسكلت با استفاده از يوآرال اين فرم <code>spip.php?page=nom&param1=xx&param2=yyy...</code>. چنين صفحه‌هايي اغلب توسط پلاگين‌ها استفاده مي‌شوند.  ',
+	'description_menu_page_speciale_zajax' => 'افزودن يك پيوند به يك بلوك در صفحه‌اي قابل دسترس به وسيله‌ي يك يوآرال از نوع <code>spip.php?page=nom&param1=xx&param2=yyy...</code>. اين مستلزم يك استكلت نوع z و يك پلاگين <a href="http://www.spip-contrib.net/MediaBox">médiabox</a> خواهد بود.  ',
+	'description_menu_rubriques' => 'نمايش فهرستي از بخش‌ها و، در صورت تمايل، زيربخش‌هاي تا چندين سطح. پيش‌ گزيده، تمام بخش‌ها‌ي ريشه‌ي سايت ديده مي‌شوند كه به ترتيب عنوان (شماره‌اي و بعد الفبائي) مرتب شده‌اند.',
+	'description_menu_rubriques_articles' => 'نمايش يك فهرست از بخش‌ها، و اگر مي‌خواهيد، زيربخش‌ها و مقاله‌ها در چندين سطح. 
+به صورت پيش‌گزيده، نمايش تمام بخش‌ها ريشه، مرتب شده بر اساس تيتر(شماره‌اي و بعد الفبايي). مقاله‌ها به طور نظام‌مند بعد از بخش‌ها قرار مي‌گيرند. 
+',
+	'description_menu_secteurlangue' => 'Cette entrée est spécifique aux sites utilisant un secteur par langue. Elle affiche automatiquement un menu listant les rubriques du secteur correspondant à la langue de la page et, si on veut, les sous-rubriques sur plusieurs niveaux. Par défaut, affiche toutes les rubriques depuis la racine, triées par titre (numériquement puis alphabétiquement).', # NEW
+	'description_menu_texte_libre' => 'فقط متني كه دوست داريد، يا يك كد زباني اسپيپ  (<:...:>)',
+
+	// E
+	'editer_menus_editer' => 'اصلاح اين منيو',
+	'editer_menus_explication' => 'منيوهاي سايتتان را اينجا ايجاد و پيكربندي كنيد.',
+	'editer_menus_exporter' => 'صادر سازي اين منيو',
+	'editer_menus_nouveau' => 'ايجاد يك منيوي جديد',
+	'editer_menus_titre' => 'منيوهاي سايت',
+	'entree_afficher_articles' => 'مقاله‌ها در منيو‌ گنجانده شود؟ (براي اين بگذاريد «بله»)',
+	'entree_afficher_item_suite' => 'گنجاندن مقاله‌ها در منيو؟ (براي اين بگذاريد «بله»)',
+	'entree_articles_max' => 'اگر بله، نمايش مقاله‌ها فقط اگر بخش شامل حداكثر ايكس ايكس مقاله است؟ (عدد مربوط به حداكثر مقاله را بگذاريد، براي نمايش تمام مقاله‌ها، خالي بگذاريد)
+',
+	'entree_articles_max_affiches' => 'Si oui, limiter le nombre d\'articles listés à xx maximum (suivis d\'un item "... Tous les articles" comportant un lien vers la rubrique parente) ? (indiquer le nombre maximum d\'articles, laissez vide pour afficher tous les articles)', # NEW
+	'entree_aucun' => 'هيچي',
+	'entree_bloc' => 'بلاك ازپيپ (Bloc Zpip)',
+	'entree_choisir' => 'نوع مدخلي را كه مي‌خواهيد اضافه كنيد انتخاب نماييد.',
+	'entree_classe_parent' => 'Classe des liens des éléments parents. Cette classe sera rajoutée aux li>a ayant une suite ul/li. Par exemple, si vous saisissez "daddy", cela vous permet d\'utiliser le plugin menu deroulant 2 pour la mise en forme du menu.', # NEW
+	'entree_connexion_objet' => 'براي ديدن اوبژه وصل بودن («session»وارد كنيد) يا قطع بودن («nosession»وارد كنيد) لازم است',
+	'entree_contenu' => 'Contenu', # NEW
+	'entree_css' => 'كلاش‌هاي سي.اس.اس (حامل) مدخل',
+	'entree_css_lien' => 'كلاس‌هاي سي.اس.اس پيوند',
+	'entree_id_groupe' => 'تعداد گروه واژه‌ها',
+	'entree_id_mot' => 'تعداد كليدواژه‌ها',
+	'entree_id_objet' => 'تعداد',
+	'entree_id_rubrique' => 'تعداد بخش‌هاي والد',
+	'entree_id_rubrique_ou_courante' => 'Numéro de la rubrique parente ou "courante" si la rubrique parente est la rubrique courante du contexte', # NEW
+	'entree_id_rubriques_exclues' => 'Numéros des rubriques à exclure, séparés par des virgules', # NEW
+	'entree_id_secteur_exclus' => 'Numéros des secteurs à exclure, séparés par des virgules', # NEW
+	'entree_infini' => 'تا بي‌نهايت',
+	'entree_mapage' => 'صفحه‌ي شخصي من',
+	'entree_masquer_articles_uniques' => 'Si oui et si une rubrique contient un seul article, le masquer ? (mettre "oui" pour cela)', # NEW
+	'entree_niveau' => 'سطح زيربخش‌ها',
+	'entree_nombre_articles' => 'تعداد حداكثر مقاله‌ها (پيش گزيده صفر)',
+	'entree_page' => 'نام صفحه',
+	'entree_parametres' => 'فهرست پارامترها',
+	'entree_rubriques_max_affichees' => 'Si oui, limiter le nombre de rubriques listés à xx maximum (suivis d\'un item "... Toutes les rubriques" comportant un lien vers la rubrique parente) ? (indiquer le nombre maximum de rubriques, laissez vide pour afficher toutes les rubriques)', # NEW
+	'entree_sousrub_cond' => 'N\'afficher que les sous-rubriques de la rubrique en cours (mettre "oui", sinon laisser vide)', # NEW
+	'entree_suivant_connexion' => 'Restreindre cette entrée suivant la connexion (mettre "connecte" pour afficher seulement si le visiteur est connecté, "deconnecte" pour le cas contraire, ou laisser vide pour toujours afficher)', # NEW
+	'entree_suivant_connexion_connecte' => 'seulement si connecté', # NEW
+	'entree_suivant_connexion_deconnecte' => 'seulement si déconnecté', # NEW
+	'entree_sur_n_articles' => '@n@ مقاله‌ي منتشره',
+	'entree_sur_n_mots' => '@n@ كلمه‌هاي منتشره ',
+	'entree_sur_n_niveaux' => 'تحت @n@ سطح',
+	'entree_titre' => 'Titre', # NEW
+	'entree_titre_connecter' => 'تيتر براي دستيابي به فرم شناسايي',
+	'entree_titre_prive' => 'تيتر براي دسترسي به قسمت شخصي',
+	'entree_traduction_articles_rubriques' => 'Dans la mesure du possible, afficher les articles de la rubrique dans la langue du contexte (mettre "trad" pour cela)', # NEW
+	'entree_traduction_objet' => 'در مور مقاله، ترجمه را در ارتباط با آن متن انتخاب كنيد (براي آن trad را بگذاريد)',
+	'entree_tri_alpha' => ' معيار مرتب كردن بخش‌ها (الفبايي). اگر «تاريخ»"date" را تايپ كنيد، معيار اضافه شده (طبق تاريخ)‌ {par date}خواهد بدو بخش‌ها طبق ترايخ مرتب خواهند شد
+',
+	'entree_tri_alpha_articles' => 'Critère de tri des articles (alphabétique). Si vous saisissez "date", le critère ajouté sera {par date} et les articles seront triés par date', # NEW
+	'entree_tri_alpha_articles_inverse' => 'Inverser le critère de tri alphabétique ? (mettre "oui" pour cela)', # NEW
+	'entree_tri_alpha_inverse' => 'Inverser le critère de tri alphabétique ? (mettre "oui" pour cela)', # NEW
+	'entree_tri_num' => 'معيار مرتب كردن بخش‌ها (رقم). اگر «تيتر» را تايپ كنيد، معيار افزوده شده (طبق تيتر) {par num titre}خواهد بود و بخش‌ها بر اساس شماره تيتر‌ها مرتب خواهند شد
+',
+	'entree_tri_num_articles' => 'Critère de tri des articles (numérique). Si vous saisissez "titre", le critère ajouté sera {par num titre} et les articles seront triés par numéro de titre', # NEW
+	'entree_tri_num_articles_inverse' => 'Inverser le critère de tri numérique ? (mettre "oui" pour cela)', # NEW
+	'entree_tri_num_inverse' => 'Inverser le critère de tri numérique ? (mettre "oui" pour cela)', # NEW
+	'entree_type_objet' => 'نوع اوبژه',
+	'entree_url' => 'Adresse', # NEW
+	'entree_url_public' => 'نشاني بازگشت بعد از اتصال ',
+	'erreur_aucun_type' => 'هيچ نوع مدخلي يافت نشد.',
+	'erreur_autorisation' => 'شما مجاز به اصلاح منيوي سايت نيستيد.',
+	'erreur_identifiant_deja' => 'از اين شناسه يك منيوي ديگر استفاده مي‌كند',
+	'erreur_identifiant_forme' => 'شناسه بايد فقط از حروف، رقم، و خط‌ زير تركيب شده باشد.',
+	'erreur_menu_inexistant' => 'منيوي درخواستي شماره @id@ موجود نيست.',
+	'erreur_mise_a_jour' => 'هنگام به هنگام سازي پايگاه داده‌ها خطايي رخ داده است.',
+	'erreur_parametres' => 'در پارامتر‌هاي صفحه خطايي وجود دارد',
+	'erreur_type_menu' => 'بايد نوعي از منيو را انتخاب كنيد',
+	'erreur_type_menu_inexistant' => 'Ce type de menu n\'est pas/plus disponible', # NEW
+
+	// F
+	'formulaire_ajouter_entree' => 'يك مدخل اضافه كنيد',
+	'formulaire_ajouter_sous_menu' => 'ايجاد يك زيرمنيو',
+	'formulaire_css' => 'سي.اس.اس طبقات',
+	'formulaire_css_explication' => 'مي‌توانيد به منيوي خود طبقات سي.اس.اس تكميلي اضافه كنيد.',
+	'formulaire_deplacer_bas' => 'حركت به پائين',
+	'formulaire_deplacer_haut' => 'حركت به بالا',
+	'formulaire_facultatif' => 'دلبخواهي ',
+	'formulaire_identifiant' => 'شناسه',
+	'formulaire_identifiant_explication' => 'يك كليدواژه‌ي منحصر به فرد بدهيد تا بتوانيد منيوي خود را به راحتي فرابخوانيد.',
+	'formulaire_ieconfig_choisir_menus_a_importer' => 'منيو‌هايي را كه مي‌خواهيد صادر كنيد انخاب كنيد',
+	'formulaire_ieconfig_importer' => 'واردات',
+	'formulaire_ieconfig_menu_meme_identifiant' => 'توجه: يك منيو با همين شناسه در سايت شما موجود است!',
+	'formulaire_ieconfig_menus_a_exporter' => 'منيو‌هاي صادراتي:',
+	'formulaire_ieconfig_ne_pas_importer' => 'وارد نكردن',
+	'formulaire_ieconfig_remplacer' => 'جايگزين سازي منيوي موجود با منوي وارداتي',
+	'formulaire_ieconfig_renommer' => 'تجديد نام اين منيو پيش از واردات',
+	'formulaire_importer' => 'واردسازي يك منيو',
+	'formulaire_importer_explication' => 'Si vous avez exporté un menu dans un fichier, vous pouvez l\'importer maintenant.', # NEW
+	'formulaire_modifier_entree' => 'Modifier cette entrée', # NEW
+	'formulaire_modifier_menu' => 'Modifier le menu :', # NEW
+	'formulaire_nouveau' => 'Nouveau menu', # NEW
+	'formulaire_partie_construction' => 'Construction du menu', # NEW
+	'formulaire_partie_identification' => 'Identification du menu', # NEW
+	'formulaire_supprimer_entree' => 'Supprimer cette entrée', # NEW
+	'formulaire_supprimer_menu' => 'Supprimer le menu', # NEW
+	'formulaire_supprimer_sous_menu' => 'Supprimer ce sous-menu', # NEW
+	'formulaire_titre' => 'Titre', # NEW
+
+	// I
+	'info_afficher_articles' => 'Les articles seront inclus dans le menu.', # NEW
+	'info_articles_max' => 'Seulement si la rubrique contient au plus @max@ articles', # NEW
+	'info_articles_max_affiches' => 'Affichage limité à @max@ articles', # NEW
+	'info_classe_parent' => 'Classe des éléments parents : ', # NEW
+	'info_connexion_obligatoire' => 'Connexion obligatoire', # NEW
+	'info_deconnexion_obligatoire' => 'Uniquement déconnecté', # NEW
+	'info_masquer_articles_uniques' => 'Articles uniques masqués', # NEW
+	'info_numero_menu' => 'MENU NUMÉRO :', # NEW
+	'info_page_speciale' => 'Lien vers la page « @page@ »', # NEW
+	'info_page_speciale_zajax' => 'Modalbox de la page « @page@ » pour le bloc « @bloc@ &#187', # NEW
+	'info_rubrique_courante' => 'Rubrique courante', # NEW
+	'info_rubriques_exclues' => ' / sauf rubrique(s) @id_rubriques@', # NEW
+	'info_rubriques_max_affichees' => 'Affichage limité à @max@ rubriques', # NEW
+	'info_secteur_exclus' => ' / sauf secteur(s) @id_secteur@', # NEW
+	'info_sousrub_cond' => 'Seules les sous-rubriques de la rubriques en cours sont affichées.', # NEW
+	'info_tous_groupes_mots' => 'Tous les groupes de mots', # NEW
+	'info_traduction_recuperee' => 'Le contexte décidera de la traduction choisie', # NEW
+	'info_tri' => 'ترتيب بخش‌ها:‌',
+	'info_tri_alpha' => '(الفبايي)',
+	'info_tri_articles' => 'Tri des articles :', # NEW
+	'info_tri_num' => '(شماره‌اي)',
+
+	// N
+	'noisette_description' => 'گنجاندن منيوي تعريف شده با پلاگين منو‌ها.',
+	'noisette_label_afficher_titre_menu' => 'نمايش تيتر منيو؟',
+	'noisette_label_identifiant' => 'منيو براي نمايش: ',
+	'noisette_nom_noisette' => 'منيو',
+	'nom_menu_accueil' => 'Accueil', # NEW
+	'nom_menu_articles_rubrique' => 'مقاله‌هاي يك بخش',
+	'nom_menu_deconnecter' => 'Se déconnecter', # NEW
+	'nom_menu_espace_prive' => 'اتصال/ پيوند به قسمت شخصي',
+	'nom_menu_groupes_mots' => 'كليدواژه‌ها و مقاله‌هاي يك گروه‌ واژه',
+	'nom_menu_lien' => 'پيوند دلبخواهي',
+	'nom_menu_mapage' => 'صفحه‌ من',
+	'nom_menu_mots' => 'مقاله‌هاي يك كليد واژه',
+	'nom_menu_objet' => 'مقاله، بخش يا ساير چيزهاي اسپيپي ',
+	'nom_menu_page_speciale' => 'پيوند به يك صفحه‌ي اسكلت',
+	'nom_menu_page_speciale_zajax' => 'يك بلوك از صفحه‌ي ازپيپ',
+	'nom_menu_rubriques' => 'Liste ou arborescence de rubriques', # MODIF
+	'nom_menu_rubriques_evenements' => 'برنامه‌هاي مربوط به بخش‌ها',
+	'nom_menu_secteurlangue' => 'بخش‌هاي زباني ',
+	'nom_menu_texte_libre' => 'متن آزاد',
+
+	// T
+	'tous_les_articles' => '... Tous les articles', # NEW
+	'toutes_les_rubriques' => '... Toutes les rubriques' # NEW
+);
+
+?>
