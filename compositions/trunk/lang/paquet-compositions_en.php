6642d6d9fe6be7c80e9b19bbca94ec64226c8dc1
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-05-16 01:39:41 +0000

    langues (paquet-compositions)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@61342 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- compositions/trunk/lang/paquet-compositions_en.php
+++ compositions/trunk/lang/paquet-compositions_en.php
@@ -9,6 +9,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// C
 	'compositions_description' => 'This plugin allows you to define several variants of compositions for each kind of SPIP objects (article, sections, etc..) and to easily apply them, case by case, from the private area.',
+	'compositions_nom' => 'Compositions',
 	'compositions_slogan' => 'Differ page layouts according to sections, articles...'
 );
 
