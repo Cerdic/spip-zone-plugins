f1907e83964edd7d2a61a20f500b23cd02211d53
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-05-17 15:51:18 +0000

    On inclu inc/config des fois que
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@61371 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- tickets/trunk/inc/ticket_accepter_forums_publics.php
+++ tickets/trunk/inc/ticket_accepter_forums_publics.php
@@ -8,6 +8,7 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
  * @return string  : "non", "pos"(teriori), "pri"(ori), "abo"(nnement)
  */
 function inc_ticket_accepter_forums_publics_dist($id_ticket){
+	include_spip('inc/config');
 	$accepter_forum = lire_config("tickets/general/forums_publics", "posteriori");
 	return substr($accepter_forum, 0, 3);
 }
