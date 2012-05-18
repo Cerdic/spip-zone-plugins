291ddc2663c8dcba839c91e821c21a6196d64a2a
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-05-18 10:39:16 +0000

    Pouvoir utiliser le porte-plume vraiment partout avec la possibilité de mettre un sélecteur CSS/jQuery pour l'activer sur de nouveaux champs (même sur des input text)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@61388 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- porte_plume_extras/partout/lang/ppp_fr.php
+++ porte_plume_extras/partout/lang/ppp_fr.php
@@ -13,10 +13,15 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'cfg_description' => 'en fonction de vos squelettes, veillez à n\'activer le Porte Plume que sur des champs pour lesquels l\'utilisation des raccourcis n\'engendrera pas d\'erreur xhtml.',
 	'cfg_titre' => 'Porte Plume Partout',
 
+	// E
+	'explication_personnalisation' => 'Indiquez la cible des éléments qui utiliseront la barre typographique (Expression CSS ou étendue jQuery).',
+	
 	// L
 	'label_hauteur_champ' => 'Demi-hauteur de l\'écran',
+	'label_personnalisation' => 'Sélecteur personnalisé',
 	'legend_barre_typo' => 'Activer la barre typographique sur :',
 	'legend_hauteur_champ' => 'Hauteur du champ texte pour les articles',
+	'legend_personnalisation' => 'Personnalisation',
 
 	// S
 	'supprimer' => 'Remettre les valeurs par défaut'
--- porte_plume_extras/partout/ppp_pipelines.php
+++ porte_plume_extras/partout/ppp_pipelines.php
@@ -1,7 +1,13 @@
 <?php
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
-//Appel du pipeline qui ajoute des barres de porte-plume partout dans Descriptif, chapo, PS, Bio et Descriptif du site
+/**
+ * Insertion dans le pipeline insert_head_prive (SPIP)
+ * Ajoute des barres de porte-plume sur les champs configurés
+ * 
+ * @param string $flux : le contexte du pipeline
+ * @return string $flux : le contexte modifié
+ */
 function ppp_insert_head_prive($flux){
 	$js = generer_url_public('barre_generalisee.js');
 	if (defined('_VAR_MODE') && _VAR_MODE=='recalcul')
