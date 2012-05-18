f75f393ec206a984a932f4d9e67f36aff275775d
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-05-18 10:12:47 +0000

    Un paquet.xml + ses fichiers de langue
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@61386 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ porte_plume_extras/partout/lang/paquet-ppp_en.php
@@ -0,0 +1,20 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-ppp
+// Langue: en
+// Date: 18-05-2012 12:06:03
+// Items: 3
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// P
+	'ppp_description' => 'Quill everywhere enables the editing toolbar on other text fields (standfirst, description, ps)',
+	'ppp_nom' => 'Quill everywhere',
+	'ppp_slogan' => 'Display Quill on most input fields',
+);
+?>
---
+++ porte_plume_extras/partout/lang/paquet-ppp_fr.php
@@ -0,0 +1,20 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-ppp
+// Langue: fr
+// Date: 18-05-2012 12:06:03
+// Items: 3
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// P
+	'ppp_description' => 'Porte Plume Partout sert à ajouter la barre d\'édition aux champs DESCRIPTIF, CHAPO et PS',
+	'ppp_nom' => 'Porte Plume Partout',
+	'ppp_slogan' => 'Afficher le Porte Plume sur la plupart des champs de saisie',
+);
+?>
