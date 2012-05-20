22a6cf47bbec67dc2db9e0759d2d09d92f9686d1
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-05-20 07:36:14 +0000

    salvatore
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@61444 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- traduction_rubriques_autrement/branches/1/lang/paquet-trad_rub_sk.php
+++ traduction_rubriques_autrement/branches/1/lang/paquet-trad_rub_sk.php
@@ -9,7 +9,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// T
 	'taa_slogan' => 'Prechádzanie medzi preloženými rubrikami.',
-	'trad_rub_description' => 'Ce plugin, déstiné pour les sites mutlilingues avec [un secteur par langue->http://www.spip-contrib.net/Un-exemple-de-site-multilingue], vous offre une approche similaire au niveau de la navigation et de la gestion de traductions des rubriques à celle du plugin [Traduction articles autrement->http://files.spip.org/spip-zone/traduction_articles_autrement.zip] dont l\'installation est recommandé. Comme spip ne propose pas nativement la traduction des rubriques l\'installation du plugin [Traduction entre rubrique->http://files.spip.org/spip-zone/tradrub.zip] est nécessaire.' # NEW
+	'trad_rub_description' => 'Tento zásuvný modul, ktorý bol vytvorený pre viacjazyčné stránky [so sektormi podľa jazyka->http://www.spip-contrib.net/Un-exemple-de-site-multilingue], vám pokiaľ ide o navigáciu a riadenie prekladov rubrík ponúka podobný prístup ako zásuvný modul [Preklad článkov inak,->http://files.spip.org/spip-zone/traduction_articles_autrement.zip] ktorého inštalácia sa odporúča. Keďže spip v predvolených nastaveniach neponúka preklad rubrík, treba si nainštalovať zásuvný modul [Preklad medzi rubrikami.->http://files.spip.org/spip-zone/tradrub.zip]'
 );
 
 ?>
