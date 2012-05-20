bac3fe767367625ef9426d0f176369e24c14715c
johan.pustoch@crdp.ac-versailles.fr <johan.pustoch@crdp.ac-versailles.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-05-20 16:36:20 +0000

    Plugin proposant un modele de document permettant d'integrer des images actives dans SPIP
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@61461 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ imagesactives/lang/paquet-imagesactives_fr.php
@@ -0,0 +1,12 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+'scolaspip_slogan' => 'Dans votre site SPIP : fini le temps des images statiques, place aux Images actives !',
+'scolaspip_description' => 'Ce plugin fournit un modèle de document doc_image_active pour afficher une Image Active dans un contenu SPIP',
+
+);
+
+?>
