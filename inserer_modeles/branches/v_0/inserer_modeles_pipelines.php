1b42aafccc37386f1059d8af299a0eace8023760
joseph@larmarange.net <joseph@larmarange.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-05-20 13:54:13 +0000

    On passe par un squelette publique plutôt que par un exec.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@61451 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- inserer_modeles/branches/v_0/inserer_modeles_pipelines.php
+++ inserer_modeles/branches/v_0/inserer_modeles_pipelines.php
@@ -1,19 +1,16 @@
 <?php
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
-// On passe pour le moment via affiche_droite
-// Mais envisager dans le futur une intégration au porte plume
 function inserer_modeles_affiche_droite($flux){
 	if (in_array($flux['args']['exec'],array('articles_edit','breves_edit','rubriques_edit','mots_edit'))) {
 		include_spip('inc/inserer_modeles');
 		if (count(inserer_modeles_lister_formulaires_modeles())>0)
-			$flux['data'] .= recuperer_fond('prive/inserer_modeles',$flux['args']);
+			$flux['data'] .= recuperer_fond('inclure/inserer_modeles',$flux['args']);
 	}
 	return $flux;
 }
 
 // Insertion dans le porte-plume
-
 function inserer_modeles_porte_plume_barre_pre_charger($barres) {
 	include_spip('inc/inserer_modeles');
 	$formulaires_modeles = inserer_modeles_lister_formulaires_modeles();
@@ -34,9 +31,9 @@ function inserer_modeles_porte_plume_barre_pre_charger($barres) {
 				"id" => 'inserer_modele_'.$nom,
 				"name" => _T_ou_typo($formulaire['nom']),
 				"className" => 'outil_inserer_modele_'.$nom,
-				"beforeInsert" => "function() {jQuery.modalboxload('".generer_url_ecrire(
-					'inserer_modeles',
-					"formulaire_modele=$nom&id_article='+$(\"[name='id_article']\").val()+'&id_rubrique='+$(\"[name='id_rubrique']\").val()+'&id_breve='+$(\"[name='id_breve']\").val()"
+				"beforeInsert" => "function() {jQuery.modalboxload('".generer_url_public(
+					'inclure/inserer_modeles',
+					"modalbox=oui&formulaire_modele=$nom&id_article='+$(\"[name='id_article']\").val()+'&id_rubrique='+$(\"[name='id_rubrique']\").val()+'&id_breve='+$(\"[name='id_breve']\").val()"
 				).",{minHeight: '90%'});}",
 				"display" => true
 			);
@@ -48,9 +45,9 @@ function inserer_modeles_porte_plume_barre_pre_charger($barres) {
 			"name"        => _T('inserer_modeles:outil_inserer_modeles'),
 			"key"         => "M",
 			"className"   => 'outil_inserer_modeles',
-			"beforeInsert" => "function() {jQuery.modalboxload('".generer_url_ecrire(
-				'inserer_modeles',
-				"id_article='+$(\"[name='id_article']\").val()+'&id_rubrique='+$(\"[name='id_rubrique']\").val()+'&id_breve='+$(\"[name='id_breve']\").val()"
+			"beforeInsert" => "function() {jQuery.modalboxload('".generer_url_public(
+				'inclure/inserer_modeles',
+				"modalbox=oui&id_article='+$(\"[name='id_article']\").val()+'&id_rubrique='+$(\"[name='id_rubrique']\").val()+'&id_breve='+$(\"[name='id_breve']\").val()"
 			).",{minHeight: '90%'});}",
 			"display"     => true,
 			"dropMenu"    => $sous_menu
