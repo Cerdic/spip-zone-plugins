ecf812df940c5975d9b48560f89206622247c680
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-05-21 13:58:39 +0000

    Create branch activite_editoriale1
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@61496 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ activite_editoriale/branches/activite_editoriale1/activite_editoriale_pipelines.php
@@ -0,0 +1,6 @@
+<?php
+
+function activite_editoriale_taches_generales_cron($taches_generales) {
+	$taches_generales['activite_editoriale_alerte'] = 24*3600; // tous les jours
+  	return $taches_generales;
+}
---
+++ activite_editoriale/branches/activite_editoriale1/lang/activite_editoriale_fr.php
@@ -0,0 +1,76 @@
+<?php
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+    //A
+    'activite_editoriale'   			=>'Activité éditoriale',
+	'activite_editoriale_statistiques'	=> 'Activité éditoriales: statistiques de publication',
+	'au'								=> ' au ',
+	'apartirde'							=> 'À partir du ',
+	'activer_stats'						=> 'Activer les stats des rubriques',
+	'activer_mots_cles'					=> 'Liste des articles sans mots-clés',
+	'activer_stat_oui'					=> 'Oui',
+	'activer_stat_non'					=> 'Non',
+	
+	//D
+	'delai' 							=> 'D&eacute;lai',
+	'delai_precisions' 					=> '(en nombre de jours)',
+	'derniere_maj'     					=> 'Dernière MàJ',
+	'du'								=> 'Du ',
+	'detail_mots_cles' 					=> 'Articles sans mot-clé',
+	'date'								=> 'Date',
+	
+	//E
+	'editer'							=> 'Éditer l\'article',
+	
+	//G
+	'gestionnaire'     					=> 'Vous êtes identifié comme un gestionnaire de la rubrique',
+
+	//I
+	'identifiants' 						=> 'Identifiants ou email  des auteurs &agrave; alerter',
+	'identifiants_precisions' 			=> '(s&eacute;par&eacute;s par des ",")',
+	'icone_statistics'					=> 'Voir les statistiques de la rubrique',
+	'icone_mots_cles'					=> 'Voir le détail',
+	'id_article'						=> 'ID de l\'article',
+	
+	//J
+	'jours'        						=> 'jour(s)',
+	'jusqua'							=> 'Jusqu\'au',
+	
+	//N
+	'nb_art_secteur' 					=> 'Nb d\'articles publiés dans le secteur',
+	'nb_art_branche' 						=> 'Nb d\'articles publiés dans la branche',
+	'nb_art_total' 						=> 'Nb d\'articles publiés total',
+	'nb_total_notag'					=> 'Nb d\'articles publiés sans mot-clé',
+	'nb_articles'						=> 'Nombre d\'articles',
+	'nb_rubriques'						=> 'Nombre de rubriques',
+	'nb_total_articles'					=> 'Nombre total d\'article',
+	'nb_total_rubriques'				=> 'Nombre total de rubriques',
+	
+	//P
+	'periode'							=> 'Période',
+	
+	//R
+	'responsables'             			=> 'Responsables',
+	'rubriques_a_suivre'       			=> 'Rubriques à suivre',
+	'rubrique_doit_maj'        			=> 'Une rubrique doit être mise a jour',
+	'rubrique_pas_maj'         			=> 'Attention, la rubrique “ @titre@ ” n\'a pas été mise à jour depuis @jours@ jours',
+	'rubriques_pas_a_jour'     			=> 'Rubriques n\'ayant pas été mises à jour à temps',
+	'redacteurs'						=> 'Rédacteurs',
+	'rubrique'							=> 'RUBRIQUE: ',
+	
+	//S
+	'secteur'							=> 'Secteur:',
+	
+	//T
+	'top_auteurs'						=> 'Top des auteurs',
+	'toutelaperiode'					=> 'Toute la période',
+	'titre_article'						=> 'Titre de l\'article',
+	
+	//CFG
+	'cfg'                      			=> 'Configurer le plugin Activité éditoriale',
+	'cfg_champ'                			=> 'Quel est le champ analysé pour savoir la MAJ ?',
+	'cfg_maj_rubrique'         			=> 'Le champ MAJ de la rubrique',
+	'cfg_date_modif_branche'   			=> 'Le champ DATE_MODIF des articles (breves) de la branche',
+	'cfg_date_modif_rubrique' 			=> 'Le champ DATE_MODIF des articles (breves) de la rubrique'
+	
+
+);
