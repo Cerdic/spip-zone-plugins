4c2e3bac7e4b21b631867e6f4aa8334c36c416cd
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-05-21 14:41:49 +0000

    passage en champs extra 3
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@61500 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- activite_editoriale/trunk/activite_editoriale_administrations.php
+++ activite_editoriale/trunk/activite_editoriale_administrations.php
@@ -1,16 +1,17 @@
 <?php
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
-include_spip('inc/cextras_gerer');
+include_spip('inc/cextras');
 include_spip('base/activite_editoriale');
 
-function activite_editoriale_upgrade($nom_meta_base_version,$version_cible){
-	$champs = activite_editoriale_declarer_champs_extras();
-	installer_champs_extras($champs, $nom_meta_base_version, $version_cible);
+function activite_editoriale_upgrade($nom_meta_base_version,$version_cible) {
+  $maj = array();
+  cextras_api_upgrade(activite_editoriale_declarer_champs_extras(), $maj['create']);	
+  include_spip('base/upgrade');
+  maj_plugin($nom_meta_base_version, $version_cible, $maj);
 }
-
 function activite_editoriale_vider_tables($nom_meta_base_version) {
-	$champs = activite_editoriale_declarer_champs_extras();
-	desinstaller_champs_extras($champs, $nom_meta_base_version);
+  cextras_api_vider_tables(activite_editoriale_declarer_champs_extras());
+  effacer_meta($nom_meta_base_version);
 }
 ?>
--- activite_editoriale/trunk/base/activite_editoriale.php
+++ activite_editoriale/trunk/base/activite_editoriale.php
@@ -2,26 +2,27 @@
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 function activite_editoriale_declarer_champs_extras($champs = array()){
-	$champs[] = new ChampExtra(array(
-		'table' => 'rubriques', // sur quelle table ?
-		'champ' => 'extras_delai', // nom sql
+	$champs['spip_rubriques']['extras_delai']=array(
+		'saisie'=>'input',
+		'options'=>array(
+			'nom'=>'extras_delai',
+			'sql' => "tinytext NOT NULL DEFAULT ''", // declaration sql
+			'rechercher'=>false
+			),
 		'label' => 'activite_editoriale:delai', // chaine de langue 'mon_plug:mon_label'
-		'precisions' => 'activite_editoriale:delai_precisions', // precisions sur le champ
+		'explication' => 'activite_editoriale:delai_precisions', // precisions sur le champ
 		'obligatoire' => false, // 'true', 'false' ou ''
-		'rechercher' => false, // 'false', 'true' ou directement la valeur de ponderation (de 1 a 8 generalement)
-		'type' => 'ligne', // type de saisie
-		'sql' => "tinytext NOT NULL DEFAULT ''", // declaration sql
-	));
-	$champs[] = new ChampExtra(array(
-		'table' => 'rubriques', // sur quelle table ?
-		'champ' => 'extras_identifiants', // nom sql
+			);
+	$champs['spip_rubriques']['extras_identifiants'] = array(
+	'saisie'=>'input',
+		'options'=>array(
+			'nom'=>'extras_identifiants',
+			'rechercher'=>false,
+			'sql' => "tinytext NOT NULL DEFAULT ''"), // declaration sql
 		'label' => 'activite_editoriale:identifiants', // chaine de langue 'mon_plug:mon_label'
-		'precisions' => 'activite_editoriale:identifiants_precisions', // precisions sur le champ
+		'explication' => 'activite_editoriale:identifiants_precisions', // precisions sur le champ
 		'obligatoire' => false, // 'true', 'false' ou ''
-		'rechercher' => false, // 'false', 'true' ou directement la valeur de ponderation (de 1 a 8 generalement)
-		'type' => 'ligne', // type de saisie
-		'sql' => "tinytext NOT NULL DEFAULT ''", // declaration sql
-	));
+	);
 	return $champs;
 }
 
