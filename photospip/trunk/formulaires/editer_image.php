bcf5c05e2f3204702178e04206fa37297cfa6e8b
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-05-22 11:09:50 +0000

    Améliorations diverses du formulaire d'édition et on fait marcher correctement la sélection de zone qui plantait au refresh ajax
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@61543 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- photospip/trunk/formulaires/editer_image.php
+++ photospip/trunk/formulaires/editer_image.php
@@ -7,9 +7,9 @@ include_spip('inc/autoriser');
 include_spip('inc/documents');
 include_spip('photospip_fonctions');
 
-function formulaires_editer_image_charger_dist($id_document='new', $retour='', $config_fonc='image_edit_config'){
+function formulaires_editer_image_charger_dist($id_document='new', $retour=''){
 	$valeurs = array();
-	$id_document = sql_getfetsel('id_document','spip_documents','id_document='.intval($id_documents));
+	$id_document = sql_getfetsel('id_document','spip_documents','id_document='.intval($id_document));
 	$valeurs['id_document'] = $id_document;
 	
 	if(!$id_document){
@@ -19,17 +19,24 @@ function formulaires_editer_image_charger_dist($id_document='new', $retour='', $
 	}
 	
 	$limite = lire_config('photospip/limite_version',1000000);
-	$nb_versions = sql_count('*','spip_documents_inters','id_document='.intval($id_document));
+	$nb_versions = sql_countsel('spip_documents_inters','id_document='.intval($id_document));
 	if($nb_versions >= $limite){
 		$valeurs['modifiable'] = false;
 		$valeurs['message_erreur'] = _T('phpotospip:erreur_nb_versions_atteint',array('nb'=>$limite));
+		return $valeurs;
 	}
 		
 	/**
 	 * Restaurer les inputs en cas de test
 	 */
 	foreach(array('filtre',
-		'ratio','recadre_width','recadre_height','recadre_x1','recadre_y1',
+		'ratio',
+		'recadre_width',
+		'recadre_height',
+		'recadre_x1',
+		'recadre_x2',
+		'recadre_y1',
+		'recadre_y2',
 		'params_image_sepia',
 		'params_image_gamma',
 		'params_image_flou',
@@ -41,15 +48,8 @@ function formulaires_editer_image_charger_dist($id_document='new', $retour='', $
 	}
 	
 	$valeurs['largeur_previsu'] = test_espace_prive()? 548 : lire_config('photospip/largeur_previsu','450');
-	if(!is_numeric($id_document)){
-		$valeurs['message_erreur'] = _T('photospip:erreur_doc_numero');
-		$valeurs['editable'] = false;
-	}
-	else if(!$id_document = sql_getfetsel('id_document','spip_documents','id_document='.intval($id_document))){
-		$valeurs['message_erreur'] = _T('photospip:erreur_doc_numero');
-		$valeurs['editable'] = false;
-	}
-	else if(!autoriser('modifier','document',$id_document)){
+	
+	if(!autoriser('modifier','document',$id_document)){
 		$valeurs['message_erreur'] = _T('photospip:erreur_auth_modifier');
 		$valeurs['editable'] = false;
 	}
@@ -60,20 +60,7 @@ function formulaires_editer_image_charger_dist($id_document='new', $retour='', $
 	return $valeurs;
 }
 
-// Choix par defaut des options de presentation
-function image_edit_config($row)
-{
-	global $spip_ecran, $spip_lang, $spip_display;
-
-	$config = $GLOBALS['meta'];
-	$config['lignes'] = ($spip_ecran == "large")? 8 : 5;
-	$config['langue'] = $spip_lang;
-
-	$config['restreint'] = ($row['statut'] == 'publie');
-	return $config;
-}
-
-function formulaires_editer_image_verifier_dist($id_document='new', $id_parent='', $retour='', $lier_trad=0, $config_fonc='documents_edit_config', $row=array(), $hidden=''){
+function formulaires_editer_image_verifier_dist($id_document='new', $retour=''){
 	if(!$var_filtre = _request('filtre')){
 		$erreurs['message_erreur'] = _T('photospip:erreur_form_filtre');
 	}
@@ -98,6 +85,78 @@ function formulaires_editer_image_verifier_dist($id_document='new', $id_parent='
 		}
 	}
 	return $erreurs;
+}
+
+// http://doc.spip.org/@inc_editer_article_dist
+function formulaires_editer_image_traiter_dist($id_document='new', $retour=''){
+	$res = array();
+	
+	$var_filtre = _request('filtre');
+	$params = photospip_recuperer_params_form($var_filtre);
+	$row = sql_fetsel('*','spip_documents','id_document='.intval($id_document)); 
+	$src = get_spip_doc($row['fichier']);
+	
+	if (preg_match(',^(.*)-photospip(\d+).([^.]+)$,', $src, $match)) {
+		$version = $match[2];
+		$orig_src = $match[1].'.'.$match[3];
+		$newversion = ++$version;
+		spip_log("La nouvelle version sera $newversion","photospip");
+	}
+	if($version){
+		// $dest = preg_replace(',\.[^.]+$,', '-r'.$var_rot.'$0', $src); //original
+		$dest = preg_replace(",\.[^.]+$,", "-photospip".($newversion)."$0", $orig_src);
+		spip_log("la destination sera $dest","photospip");
+		spip_log("application du filtre $var_filtre $src : $dest","photospip");
+	}
+	else{
+		$dest = preg_replace(',\.[^.]+$,', '-photospip1.png', $src);
+		// on transforme l'image en png non destructif
+		include_spip('inc/filtres_images');
+		spip_log("On transforme l'image source en PNG non destructif","photospip");
+		$src = extraire_attribut(image_alpha($src,0),'src');
+		spip_log("application du filtre $var_filtre $src : $dest","photospip");
+	}
+	
+	if($var_filtre == "tourner"){
+		include_spip('inc/filtres');
+		include_spip('public/parametrer'); // charger les fichiers fonctions #bugfix spip 2.1.0
+		$dst_img = filtrer('image_rotation',$src,$params[3]);
+		$dst_img = filtrer('image_format',$dst_img,$row['extension']);
+		$dst_img = extraire_attribut($dst_img,'src');
+		include_spip('inc/getdocument');
+		deplacer_fichier_upload($dst_img,$dest);
+	}
+	
+	else{
+		$sortie = photospipfiltre($src, $dest, $var_filtre,$params);
+		if(!$sortie){
+			$res['message_erreur'] = 'photospip n a pas pu appliquer le filtre '.$var_filtre;
+			return $res;
+		}
+	}
+
+	$size_image = getimagesize($dest);
+	spip_log("taille de l'image $size_image[0] x $size_image[1]","photospip");
+	$largeur = $size_image[0];
+	$hauteur = $size_image[1];
+	$ext = substr(basename($dest), strpos(basename($dest), ".")+1, strlen(basename($dest)));
+	$poids = filesize($dest);
+	
+	// succes !
+	if ($largeur>0 AND $hauteur>0) {
+		if(is_array($params))
+			$params = serialize($params);
+		sql_insertq("spip_documents_inters",array("id_document" => $row['id_document'],"id_auteur" => $id_auteur,"extension" => $row['extension'], "fichier" => $row['fichier'], "taille" => $row['taille'],"hauteur" => $row['hauteur'], "largeur" => $row['largeur'],"mode" => $row['mode'], "version" => ($version? $version:1), "filtre" => $var_filtre, "param" => $params));
+		sql_updateq('spip_documents', array('fichier' => set_spip_doc($dest), 'taille' => $poids, 'largeur'=>$largeur, 'hauteur'=>$hauteur, 'extension' => $ext), "id_document=".intval($row['id_document']));
+		spip_log("Update de l'image dans la base poid= $poids, extension = $ext, hauteur= $hauteur, largeur = $largeur, fichier = $dest","photospip");
+	}
+
+	if (!isset($res['redirect']))
+		$res['editable'] = true;
+	if (!isset($res['message_erreur']))
+		$res['message_ok'] = _L('Votre modification a &eacute;t&eacute; enregistr&eacute;e');
+
+	return $res;
 }
 
 function photospip_recuperer_params_form($var_filtre){
@@ -134,77 +193,4 @@ function photospip_recuperer_params_form($var_filtre){
 	}
 	return array($param1,$param2,$param3,$params);
 }
-// http://doc.spip.org/@inc_editer_article_dist
-function formulaires_editer_image_traiter_dist($id_document='new', $id_parent='', $retour='', $lier_trad=0, $config_fonc='documents_edit_config', $row=array(), $hidden=''){
-	$res = array();
-	$var_filtre = _request('filtre');
-	$params = photospip_recuperer_params_form($var_filtre);
-	spip_log($params,'photospip');
-	$row = sql_fetsel('*','spip_documents','id_document='.intval($id_document));
-	include_spip('inc/documents'); 
-	$src = get_spip_doc($row['fichier']);
-	if (preg_match(',^(.*)-photospip(\d+).([^.]+)$,', $src, $match)) {
-		$version = $match[2];
-		$orig_src = $match[1].'.'.$match[3];
-		spip_log("nouvel src $src","photospip");
-		spip_log("version = $version","photospip");
-		$newversion = ++$version;
-		spip_log("La nouvelle version sera $newversion","photospip");
-	}
-	if($version){
-		// $dest = preg_replace(',\.[^.]+$,', '-r'.$var_rot.'$0', $src); //original
-		$dest = preg_replace(",\.[^.]+$,", "-photospip".($newversion)."$0", $orig_src);
-		spip_log("la destination sera $dest","photospip");
-		spip_log("application du filtre $var_filtre $src : $dest","photospip");
-	}
-	else{
-		$dest = preg_replace(',\.[^.]+$,', '-photospip1.png', $src);
-		// on transforme l'image en png non destructif
-		include_spip('inc/filtres_images');
-		spip_log("On transforme l'image source en PNG non destructif","photospip");
-		$src = extraire_attribut(image_alpha($src,0),'src');
-		spip_log("application du filtre $var_filtre $src : $dest","photospip");
-	}
-	
-	if($var_filtre == "tourner"){
-		include_spip('inc/filtres');
-		include_spip('public/parametrer'); // charger les fichiers fonctions #bugfix spip 2.1.0
-		$dst_img = filtrer('image_rotation',$src,$params[3]);
-		$dst_img = filtrer('image_format',$dst_img,$row['extension']);
-		$dst_img = extraire_attribut($dst_img,'src');
-		include_spip('inc/getdocument');
-		deplacer_fichier_upload($dst_img,$dest);
-	}
-	else{
-		$sortie = photospipfiltre($src, $dest, $var_filtre,$params);
-		if(!$sortie){
-			$res['message_erreur'] = 'photospip n a pas pu appliquer le filtre '.$var_filtre;
-			return $res;
-		}
-	}
-
-	$size_image = getimagesize($dest);
-	spip_log("taille de l'image $size_image[0] x $size_image[1]","photospip");
-	$largeur = $size_image[0];
-	$hauteur = $size_image[1];
-	$ext = substr(basename($dest), strpos(basename($dest), ".")+1, strlen(basename($dest)));
-	$poids = filesize($dest);
-	
-	// succes !
-	if ($largeur>0 AND $hauteur>0) {
-		if(is_array($params))
-			$params = serialize($params);
-		sql_insertq("spip_documents_inters",array("id_document" => $row['id_document'],"id_auteur" => $id_auteur,"extension" => $row['extension'], "fichier" => $row['fichier'], "taille" => $row['taille'],"hauteur" => $row['hauteur'], "largeur" => $row['largeur'],"mode" => $row['mode'], "version" => ($version? $version:1), "filtre" => $var_filtre, "param" => $params));
-		sql_updateq('spip_documents', array('fichier' => set_spip_doc($dest), 'taille' => $poids, 'largeur'=>$largeur, 'hauteur'=>$hauteur, 'extension' => $ext), "id_document=".intval($row['id_document']));
-		spip_log("Update de l'image dans la base poid= $poids, extension = $ext, hauteur= $hauteur, largeur = $largeur, fichier = $dest","photospip");
-	}
-
-	if (!isset($res['redirect']))
-		$res['editable'] = true;
-	if (!isset($res['message_erreur']))
-		$res['message_ok'] = _L('Votre modification a &eacute;t&eacute; enregistr&eacute;e');
-
-	return $res;
-}
-
 ?>
