f95ea2336aa3dfc0c0984288dd63cdf004855639
webmaster@geomaticien.com <webmaster@geomaticien.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-05-24 20:54:06 +0000

    Ménage après création trunk et branches/v2 (lors de la création d'une branche subclipse copie une révision ou le HEAD alors qu'ici on l'a déplacé)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@61647 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- acs/lang/acs_en.php
+++
@@ -1,5 +0,0 @@
-<?php // This is a SPIP language file. Specific to ACS plugin : DO NOT MODIFY
-
-require_once _DIR_ACS.'inc/composant/composants_ajouter_langue.php';
-acs_langue('en');
-?>
--- acs/lang/acs_fr.php
+++
@@ -1,5 +0,0 @@
-<?php // Ceci est un fichier langue de SPIP specifique au plugin ACS : NE PAS MODIFIER
-
-require_once _DIR_ACS.'inc/composant/composants_ajouter_langue.php';
-acs_langue('fr');
-?>
