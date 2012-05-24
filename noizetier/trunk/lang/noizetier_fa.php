bd69f503a300dbdd36f494cb887aa8d98f22a3b6
davoodhossein@yahoo.com <davoodhossein@yahoo.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-05-24 01:40:14 +0000

    langues (noizetier)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@61626 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ noizetier/trunk/lang/noizetier_fa.php
@@ -0,0 +1,124 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org/tradlang_module/noizetier?lang_cible=fa
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'choisir_noisette' => 'انتخاب فندقي كه مي‌خواهيد اضافه كنيد:',
+	'compositions_non_installe' => '<b>پلاگين تركيب‌ها: </b> اين پلاگين روي سايت شما نصب نشده است. براي كاركرد درست فندق‌ها لازم نيست. با اين همه، وقتي فعال شود، مي‌توانيد به صورت مستقيم در درون «مديريت فندق‌ها»ي خود تركيب‌هاي ايجاد كنيد. 
+',
+
+	// D
+	'description_bloc_contenu' => 'محتواي اصلي هر صفحه.',
+	'description_bloc_extra' => 'اطلاعات وراي متني براي هر صفحه.',
+	'description_bloc_navigation' => 'اطلاعات ناوبري مناسب براي هر صفحه.',
+	'description_bloctexte' => 'تيتر دلبخواهي است. براي متن، مي‌توانيد از ميان‌برهاي حروف‌نگاري اسپيپ استفاده كنيد.',
+
+	// E
+	'editer_composition' => 'اصلاح اين تركيب ',
+	'editer_composition_heritages' => 'تعريف ميراث‌ها',
+	'editer_compositions' => 'مديريت تركيب‌هاي مديرفندق‌ها (noiZetier)',
+	'editer_configurer_page' => 'پيكربندي فندق‌هاي اين صفحه',
+	'editer_exporter_configuration' => 'صادرسازي پيكربندي ',
+	'editer_importer_configuration' => 'واردسازي يك پيكربندي.',
+	'editer_noizetier_compositions_explication' => 'اينجا مي‌توانيد تركيب‌هايي را ايجاد كنيد كه فقط در فندق‌هاي اضافه شده فرق داشته باشند.
+',
+	'editer_noizetier_compositions_titre' => 'تركيب‌هاي فندقي (noiZetier)',
+	'editer_noizetier_explication' => 'پيكربندي فندق‌ها براي افزودن به صفحه‌هاي سايت شما.',
+	'editer_noizetier_titre' => 'مديريت فندق‌ها',
+	'editer_nouvelle_composition' => 'ايجاد يك تركيب جديد',
+	'erreur_doit_choisir_noisette' => 'مي‌توانيد يك فندق انتخاب كنيد.',
+	'erreur_mise_a_jour' => 'هنگام به هنگام سازي پايگاه داده‌ها خطايي رخ داده است.',
+	'explication_heritages_composition' => 'اينجا مي‌توانيد تركيب‌هايي را تنظيم كنيد كه اشياي اين شاخه به ارث خواهند برد.
+',
+	'explication_noizetier_css' => 'مي‌تواند بعضي از طبقات اضافي سي.اس.اس‌ را به اين فندق اضافه كنيد.
+',
+	'explication_raccourcis_typo' => 'مي‌توانيد از ميان‌ برهاي حروف‌نگاري اسپيپ استفاه كنيد.',
+
+	// F
+	'formulaire_ajouter_noisette' => 'افزودن يك فندق',
+	'formulaire_composition' => 'شناسه تركيب ',
+	'formulaire_composition_explication' => 'يك كليد واژه‌ي منحصر به فرد (بدون حروف كوچك، بدون فاصله، بدون خط تيره (-)، بدون آكسون) مشخص كنيد كه تا اين تركيب را شناسايي كند.  <br />به عنوان نمونه: <i>macompo</i>. ',
+	'formulaire_composition_mise_a_jour' => 'تركيب به هنگام سازي',
+	'formulaire_configurer_bloc' => 'پيكربندي بلوك: ',
+	'formulaire_configurer_page' => 'پيكربندي صفحه: ',
+	'formulaire_deplacer_bas' => 'حركت به پائين',
+	'formulaire_deplacer_haut' => 'حركت به بالا',
+	'formulaire_description' => 'توصيف ',
+	'formulaire_description_explication' => 'مي‌توانيد از ميان‌برهاي معمولي اسپيپ، به ويژه برچسب &lt;multi&gt استفاده كنيد',
+	'formulaire_erreur_format_identifiant' => 'شناسه نمي‌تواند شامل چيزي جز حروف كوچ بدون آكسان و كارآكتر _ (زيزخط) باشد.',
+	'formulaire_icon' => 'صورتك',
+	'formulaire_icon_explication' => 'مي‌توانيد مسير نسبي به سوي يك صورتك را وارد كنيد (براي نمونه : <i>images/objet-liste-contenus.png</i>)
+براي ديدن فهرست تصاوير اخيراً نصب شده مي‌توانيد  به اين صفحه مرجعه كنيد: <a href="../spip.php?page=icones_preview">',
+	'formulaire_identifiant_deja_pris' => 'از اين شناسه اكنون استفاده مي‌شود!',
+	'formulaire_import_compos' => 'واردسازي تركيب‌هاي فندق‌ها',
+	'formulaire_import_fusion' => 'ادغام با پيكربندي فعلي',
+	'formulaire_import_remplacer' => 'جايگزين سازي پيكربندي فعلي',
+	'formulaire_liste_compos_config' => 'اين پرونده‌ي پيكربندي تركيب‌هاي فندق‌هاي بعدي را تعريف مي‌كند: ',
+	'formulaire_liste_pages_config' => 'اين پرونده‌ي پيكربندي فندق‌هاي روي اين صفحه‌ها را تعريف مي‌كند:‌',
+	'formulaire_modifier_composition' => 'اصلاح اين تركيب: ',
+	'formulaire_modifier_composition_heritages' => 'اصلاح ميراث اين تركيب: ',
+	'formulaire_modifier_noisette' => 'اصلاح اين فندق',
+	'formulaire_noisette_sans_parametre' => 'اين فندوق پارامتري را پيشنهاد نمي‌كند.',
+	'formulaire_nom' => 'تيتر',
+	'formulaire_nom_explication' => 'مي‌توانيد از برچسب &lt;multi&gt استفاده كنيد؛',
+	'formulaire_nouvelle_composition' => 'تركيب جديد',
+	'formulaire_obligatoire' => 'ميدان الزامي ',
+	'formulaire_supprimer_composition' => 'حذف اين فندق',
+	'formulaire_supprimer_noisette' => 'حذف اين فندق',
+	'formulaire_supprimer_noisettes_page' => 'حذف فندق‌هاي اين صفحه ',
+	'formulaire_type' => 'Type de composition', # NEW
+	'formulaire_type_explication' => 'Indiquez sur quel objet / quelle page porte cette composition.', # NEW
+	'formulaire_type_import' => 'Type d\'importation', # NEW
+	'formulaire_type_import_explication' => 'Vous pouvez fusionner le fichier de configuration avec votre configuration actuelle (les noisettes de chaque page seront ajoutées à vos noisettes déjà définies) ou bien remplacer votre configuration par celle-ci.', # NEW
+
+	// I
+	'ieconfig_ne_pas_importer' => 'Ne pas importer', # NEW
+	'ieconfig_noizetier_export_explication' => 'Exportera la configuration des noisettes et les compositions du noiZetier.', # NEW
+	'ieconfig_noizetier_export_option' => 'Inclure dans l\'export ?', # NEW
+	'ieconfig_non_installe' => '<b>Plugin Importeur/Exporteur de configurations :</b> ce plugin n\'est pas installé sur votre site. Il n\'est pas nécessaire au fonctionnement du noizetier. Cependant, s\'il est activé, vous pourrez exporter et importer des configurations de noisettes dans le noizetier.', # NEW
+	'ieconfig_probleme_import_config' => 'Un problème a été rencontré lors de l\'importation de la configuration du noiZetier.', # NEW
+	'info_composition' => 'COMPOSITION :', # NEW
+	'info_page' => 'PAGE :', # NEW
+	'installation_tables' => 'Tables du plugin noiZetier installées.<br />', # NEW
+	'item_titre_perso' => 'titre personnalisé', # NEW
+
+	// L
+	'label_afficher_titre_noisette' => 'Afficher un titre de noisettes ?', # NEW
+	'label_niveau_titre' => 'Niveau du titre :', # NEW
+	'label_noizetier_css' => 'Classes CSS :', # NEW
+	'label_texte' => 'Texte :', # NEW
+	'label_titre' => 'Titre :', # NEW
+	'label_titre_noisette' => 'Titre de la noisette :', # NEW
+	'label_titre_noisette_perso' => 'Titre personnalisé:', # NEW
+	'liste_icones' => 'Liste d\'icônes', # NEW
+
+	// N
+	'ne_pas_definir_d_heritage' => 'Ne pas définir d\'héritage', # NEW
+	'noisettes_composition' => 'Noisettes spécifiques à la composition <i>@composition@</i> :', # NEW
+	'noisettes_page' => 'Noisettes spécifiques à la page <i>@type@</i> :', # NEW
+	'noisettes_toutes_pages' => 'Noisettes communes à toutes les pages :', # NEW
+	'noizetier' => 'noiZetier', # NEW
+	'nom_bloc_contenu' => 'Contenu', # NEW
+	'nom_bloc_extra' => 'Extra', # NEW
+	'nom_bloc_navigation' => 'Navigation', # NEW
+	'nom_bloctexte' => 'Bloc de texte libre', # NEW
+	'non' => 'Non', # NEW
+	'notice_enregistrer_rang' => 'Cliquez sur Enregistrer pour sauvegarder l\'ordre des noisettes.', # NEW
+
+	// O
+	'oui' => 'Oui', # NEW
+
+	// P
+	'page' => 'Page', # NEW
+
+	// W
+	'warning_noisette_plus_disponible' => 'ATTENTION : cette noisette n\'est plus disponible.', # NEW
+	'warning_noisette_plus_disponible_details' => 'Le squelette de cette noisette (<i>@squelette@</i>) n\'est plus accessible. Il se peut qu\'il s\'agisse d\'une noisette nécessitant un plugin que vous avez désactivé ou désinstallé.' # NEW
+);
+
+?>
