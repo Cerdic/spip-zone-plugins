6f4615056bd253ebc4c5fb564f345cc03b54d429
serge.markitanenko@gmail.com <serge.markitanenko@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-05-24 01:40:06 +0000

    langues (paquet-compositions)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@61624 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ compositions/trunk/lang/paquet-compositions_ru.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org/tradlang_module/paquet-compositions?lang_cible=ru
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'compositions_description' => 'Этот плагин позволяет вам задавать определнный шаблон вывода для конкретного раздела, статьи и т.д. ',
+	'compositions_nom' => 'Шаблоны страниц',
+	'compositions_slogan' => 'Выбор шаблона для вывода статьи, раздела и т.д.'
+);
+
+?>
