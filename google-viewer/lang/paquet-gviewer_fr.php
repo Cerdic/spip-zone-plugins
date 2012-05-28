a348b71e69ffd098e02e1343d0a745fa7908cf31
joseph@larmarange.net <joseph@larmarange.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-05-28 16:16:24 +0000

    Plugin compatible SPIP 3 AU passage, on passe en stable
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@61779 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ google-viewer/lang/paquet-gviewer_fr.php
@@ -0,0 +1,19 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-gviewer
+// Langue: fr
+// Date: 28-05-2012 16:13:34
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// G
+	'gviewer_description' => 'Modèle pour utiliser le [lecteur de documents en ligne de Google->http://docs.google.com/viewer]. Ce plugin étend le modève <code><mediaXX|embed></code> pour les documents de type doc, docx, xls, xlsx, ppt, pptx, pdf, ai, psd, eps, ps, et ttf. En l\'absence des modèles media, il est possible d\'utiliser <code><embXX|google></code>.',
+	'gviewer_slogan' => 'Modèles dinsertion de documents utilisant le lecteur de documents de Google',
+);
+?>
