5c6fbf9ea1f7fe55820a811e2a3d7c23632b514e
davoodhossein@yahoo.com <davoodhossein@yahoo.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-05-29 01:43:07 +0000

    langues (notifications)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@61791 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ notifications/trunk/lang/notifications_fa.php
@@ -0,0 +1,93 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org/tradlang_module/notifications?lang_cible=fa
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'alt_logo_conf' => 'لوگوي پلاگين اطلاعيه‌ها',
+	'article_prive' => 'نشر مقاله‌ها',
+	'article_prive_admins_restreints' => '<strong>ادمين‌ها </strong> : ادمين‌ها وقتي مقاله‌ها داخل بخش‌ آن‌ها پيشنهاد شود با خبر خواهند شد',
+	'article_prive_auteurs' => '<strong>نويسندگان</strong> :نويسندگان هنگام نشر مقاله‌ (ها) با خبر مي‌شوند',
+	'article_propose_detail' => 'مقاله "@titre@" براي نشر پيشنهاد شده',
+	'article_propose_sujet' => '[@nom_site_spip@] پيشنهاد شده: @titre@',
+	'article_propose_titre' => 'مقاله پيشنهادي
+	---------------',
+	'article_propose_url' => 'دعوت مي‌شويد كه در فرم پيوست مشورت و نظر خود را اعلام كنيد. اين فرم در اين شناني در دسترس است: ',
+	'article_publie_detail' => 'مقاله‌ي "@titre@" به وسيله‌ي @connect_nom@ منتشر شده.',
+	'article_publie_sujet' => '[@nom_site_spip@] منتشر شده: @titre@',
+	'article_publie_titre' => 'مقاله‌ي منتشر شده
+	--------------',
+	'article_valide_date' => 'بدون تغيير اين مقاله نشر خواهد شد',
+	'article_valide_detail' => 'مقاله‌ي "@titre@" از سوي  @connect_nom@ معتبر شده.',
+	'article_valide_sujet' => '[@nom_site_spip@] معتبر : @titre@',
+	'article_valide_titre' => 'مقاله‌ي تأييدي 
+	--------------',
+	'article_valide_url' => 'در ضمن، در اين نشاني موقت ديدني است:',
+
+	// B
+	'breve_propose_detail' => 'خبر "@titre@" براي نشر پيشنهاد شده',
+	'breve_propose_sujet' => '[@nom_site_spip@]  پيشنهاد شده: @titre@',
+	'breve_propose_titre' => 'خبر پيشنهادي
+	---------------',
+	'breve_propose_url' => 'دعوت مي‌شويد مشورت و نظر خود را در فرم پيوست ارايه دهيد. در اين نشاني در دسترس اشت:',
+	'breve_publie_detail' => 'خبر "@titre@" به وسيله‌ي  @connect_nom@ منتشر شده.',
+	'breve_publie_sujet' => '[@nom_site_spip@] منتشر شده: @titre@',
+	'breve_publie_titre' => 'خبر منتشر شده
+	--------------',
+
+	// E
+	'evenement_notification' => 'رويدادهاي آتي ممكن است سبب اطلاعيه‌هاي ايميلي شوند.',
+
+	// F
+	'forum_prives_auteur' => '<strong>نويسندگان: </strong>: نويسنده‌ها اطلاعيه‌هايي از سخنگاه‌هاي پست شده روي مقالاتشان يا پيام‌هايشان در قسمت شخصي سايت دريافت مي‌كنند. ',
+	'forum_prives_moderateur' => 'در زير نشاني ايميل گرداننده‌ي سخنگاه‌هاي قسمت شخصي را قيد كنيد (اگرچندتاست، با ويرگول جدا كنيد).',
+	'forum_prives_thread' => '<strong>خط گفتگو</strong> : شركت كنندگان در همان خط گفتگو اطلايه‌هاي سخنگاه‌ها را دريافت خواهند كرد (خصوصي)',
+	'forums_prives' => 'سخنگاه‌هاي خصوصي',
+	'forums_public' => 'سخنگاه‌هاي همگاني',
+	'forums_public_a_noter' => 'À noter : dans le cas des forums modérés à priori, seuls les auteurs ayant le droit de valider les forums sont notifiés lors de l\'envoi du forum ; les autres destinataires sont notifiés lors de la validation du message par le modérateur.', # NEW
+	'forums_public_auteurs' => '<strong>نويسندگان</strong>: نويسندگان اطلاعيه‌هايي در سخنگاه‌هاي پست شده روي مقاله‌هايشان در سايت همگاني دريافت مي‌كنند.',
+	'forums_public_moderateur' => 'در زير نشاني ايميل گرداننده‌ (يا گردانندگان - جدا شده با ويرگول) سخنگاههاي عمومي  را قيد كنند.',
+	'forums_public_thread' => '<strong>خط گفتگو</strong>: شركت‌كنندگان در همان خط گفتگو اطلاعيه‌هايي از سخنگاه‌ها (همگاني) دريافت مي‌كنند.',
+
+	// I
+	'inscription' => 'ثبت سردبيران',
+	'inscription_admins' => 'مديران',
+	'inscription_explication' => 'كدام نويسندگان اطلاعيه ثبت سردبيران جديد را دريافت مي‌كنند؟',
+	'inscription_label' => 'وضعيت ',
+	'inscription_statut_aucun' => 'هيچ',
+	'inscription_statut_webmestres' => 'وب‌مسترها',
+
+	// L
+	'lien_documentation' => '<a href="http://www.spip-contrib.net/Notifications" class="spip_out">به اسناد بنگريد</a>',
+
+	// M
+	'message_voir_configuration' => 'به پيكربندي اطلاعيه‌ها بنگريد',
+	'messagerie_interne' => 'پيك داخلي',
+	'messagerie_interne_signaler' => '<strong>Signaler les nouveaux messages privés</strong> : activer cette option pour que le site envoie une notification lorsqu\'un rédacteur n\'a pas vu un nouveau message dans sa messagerie. Le système attend 20 minutes avant de notifier le rédacteur, de manière à ne pas spammer un rédacteur déjà en ligne dans l\'espace privé.', # NEW
+	'moderateur' => '<strong>Modérateur</strong>', # NEW
+
+	// N
+	'notifications' => 'Notifications', # NEW
+
+	// S
+	'signature_petition' => 'Signatures de pétition', # NEW
+	'signature_petition_moderateur' => 'Indiquez ci-dessous l\'adresse email du modérateur des pétitions (ou plusieurs, séparés par des virgules).', # NEW
+	'suivis_perso' => 'Suivi personnalisé', # NEW
+	'suivis_perso_activer_option' => 'Si vous activez cette option, chaque visiteur qui se connecte sur cet URL de suivi sera enregistré dans la table <code>spip_auteurs</code>, avec le statut <code>6visiteur</code>. Il pourra alors voir l\'ensemble des messages qu\'il a signés sur le forum, régler ses options de notification, etc.', # NEW
+	'suivis_perso_non' => 'Pas de suivi', # NEW
+	'suivis_perso_oui' => 'Suivi activé', # NEW
+	'suivis_perso_url_suivis' => '<strong>Ajouter une URL de suivi personnalisé</strong> dans chacun des emails de notification. À partir de cette URL, l\'utilisateur pourra configurer ses préférences individuelles de notification.', # NEW
+	'suivis_public_article_thread' => 'TODO: case à cocher sur chaque article/thread', # NEW
+	'suivis_public_changer_email' => 'TODO: changer d\'email', # NEW
+	'suivis_public_description' => 'Vous pourrez (quand ce sera fonctionnel...) y retrouver tous vos messages de forum, obtenir un fil RSS des réponses qui y seront apportées, choisir votre mode de notification, etc.', # NEW
+	'suivis_public_notif_desactiver' => 'TODO: case à cocher pour ne plus recevoir de notifications', # NEW
+	'suivis_public_vos_forums' => 'Vos forums', # NEW
+	'suivis_public_vos_forums_date' => 'Vos forums, par date', # NEW
+	'suivis_public_votre_page' => 'Ceci est votre page personnalisée de suivi du site' # NEW
+);
+
+?>
