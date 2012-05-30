fd329ad2ba72e49f4938c36cacec270ded74074a
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-05-30 12:49:08 +0000

    indentation
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@61853 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- gis/trunk/base/gis.php
+++ gis/trunk/base/gis.php
@@ -13,31 +13,31 @@ function gis_declarer_tables_interfaces($interface){
 	$interface['tables_jointures']['spip_mots'][] = 'gis_liens';
 	$interface['tables_jointures']['spip_rubriques'][] = 'gis_liens';
 	$interface['tables_jointures']['spip_syndic'][] = 'gis_liens';
-	
-	$interface['table_des_tables']['gis']='gis';
-	$interface['table_des_tables']['gis_liens']='gis_liens';
+
+	$interface['table_des_tables']['gis'] = 'gis';
+	$interface['table_des_tables']['gis_liens'] = 'gis_liens';
 
 	// Traitements typo et raccourcis
-	$interface['table_des_traitements']['DESCRIPTIF_GIS'][]= _TRAITEMENT_RACCOURCIS;
-	$interface['table_des_traitements']['VILLE'][]= 'typo(extraire_multi(%s))';
-	$interface['table_des_traitements']['PAYS'][]= 'typo(extraire_multi(%s))';
-	
+	$interface['table_des_traitements']['DESCRIPTIF_GIS'][] = _TRAITEMENT_RACCOURCIS;
+	$interface['table_des_traitements']['VILLE'][] = 'typo(extraire_multi(%s))';
+	$interface['table_des_traitements']['PAYS'][] = 'typo(extraire_multi(%s))';
+
 	return $interface;
 }
 
 function gis_declarer_tables_objets_sql($tables){
-    /* Declaration de la table de points gis */
-    $tables['spip_gis'] = array(
+	/* Declaration de la table de points gis */
+	$tables['spip_gis'] = array(
 		/* Declarations principales */
-        'table_objet' => 'gis',
-        'table_objet_surnoms' => array('gis'),
-        'type' => 'gis',
-        'type_surnoms' => array('gi'),
+		'table_objet' => 'gis',
+		'table_objet_surnoms' => array('gis'),
+		'type' => 'gis',
+		'type_surnoms' => array('gi'),
 
 		/* La table */
-        'field'=> array(
+		'field' => array(
 			"id_gis" => "bigint(21) NOT NULL",
-			"titre" 	=> "varchar(255) NOT NULL DEFAULT ''",
+			"titre" => "varchar(255) NOT NULL DEFAULT ''",
 			"descriptif" => "text NOT NULL DEFAULT ''",
 			"lat" => "double NULL NULL",
 			"lon" => "double NULL NULL",
@@ -48,76 +48,76 @@ function gis_declarer_tables_objets_sql($tables){
 			"region" => "text NOT NULL DEFAULT ''",
 			"ville" => "text NOT NULL DEFAULT ''",
 			"code_postal" => "varchar(255) NOT NULL DEFAULT ''"
-        ),
-        'key' => array(
-            "PRIMARY KEY"   => "id_gis",
-        ),
-        'principale' => 'oui',
-		'modeles' => array('carte_gis','carte_gis_preview'),
+		),
+		'key' => array(
+			"PRIMARY KEY" => "id_gis",
+		),
+		'principale' => 'oui',
+		'modeles' => array('carte_gis', 'carte_gis_preview'),
 
 		/* Le titre, la date et la gestion du statut */
-        'titre' => "titre, '' AS lang",
+		'titre' => "titre, '' AS lang",
 
 		/* L'édition, l'affichage et la recherche */
-        'page' => 'gis',
-        'url_voir' => 'gis',
-        'url_edit' => 'gis_edit',
-        'editable' => 'oui',
+		'page' => 'gis',
+		'url_voir' => 'gis',
+		'url_edit' => 'gis_edit',
+		'editable' => 'oui',
 		/*'champs_editables' => array(), */
 		'icone_objet' => 'gis',
-        'rechercher_champs' => array(
-            'titre' => 8,
-            'descriptif' => 5,
-            'pays' => 3,
-            'region' => 3,
-            'ville' => 3,
-            'code_postal' => 3,
-        ),
+		'rechercher_champs' => array(
+			'titre' => 8,
+			'descriptif' => 5,
+			'pays' => 3,
+			'region' => 3,
+			'ville' => 3,
+			'code_postal' => 3,
+		),
 
-        /* Les textes standard */
-        'texte_ajouter' => 'gis:texte_ajouter_gis',
-        'texte_retour' => 'icone_retour',
-        'texte_modifier' => 'gis:texte_modifier_gis',
-        'texte_creer' => 'gis:texte_creer_gis',
-        'texte_creer_associer' => 'gis:texte_creer_associer_gis',
-        'texte_objet' => 'gis:gis_singulier',
-        'texte_objets' => 'gis:gis_pluriel',
-        'info_aucun_objet'=> 'gis:info_aucun_gis',
-        'info_1_objet' => 'gis:info_1_gis',
-        'info_nb_objets' => 'gis:info_nb_gis',
-        'texte_logo_objet' => 'gis:libelle_logo_gis',
-    );
+		/* Les textes standard */
+		'texte_ajouter' => 'gis:texte_ajouter_gis',
+		'texte_retour' => 'icone_retour',
+		'texte_modifier' => 'gis:texte_modifier_gis',
+		'texte_creer' => 'gis:texte_creer_gis',
+		'texte_creer_associer' => 'gis:texte_creer_associer_gis',
+		'texte_objet' => 'gis:gis_singulier',
+		'texte_objets' => 'gis:gis_pluriel',
+		'info_aucun_objet' => 'gis:info_aucun_gis',
+		'info_1_objet' => 'gis:info_1_gis',
+		'info_nb_objets' => 'gis:info_nb_gis',
+		'texte_logo_objet' => 'gis:libelle_logo_gis',
+	);
 
 	$spip_gis_liens = array(
-		"id_gis" 	=> "bigint(21) NOT NULL",
-		"objet" 	=> "VARCHAR (25) DEFAULT '' NOT NULL",
-		"id_objet" 	=> "bigint(21) NOT NULL");
-	
+		"id_gis" => "bigint(21) NOT NULL",
+		"objet" => "VARCHAR (25) DEFAULT '' NOT NULL",
+		"id_objet" => "bigint(21) NOT NULL");
+
 	$spip_gis_liens_key = array(
-		"PRIMARY KEY" 	=> "id_gis,id_objet,objet",
+		"PRIMARY KEY" => "id_gis,id_objet,objet",
 		"KEY id_objet" => "id_gis");
-	
+
 	$tables_auxiliaires['spip_gis_liens'] = array(
 		'field' => &$spip_gis_liens,
 		'key' => &$spip_gis_liens_key);
 
-    return $tables;
+	return $tables;
 }
 
 function gis_declarer_tables_auxiliaires($tables_auxiliaires){
 	$spip_gis_liens = array(
-		"id_gis" 	=> "bigint(21) NOT NULL",
-		"objet" 	=> "VARCHAR (25) DEFAULT '' NOT NULL",
-		"id_objet" 	=> "bigint(21) NOT NULL");
-	
+		"id_gis" => "bigint(21) NOT NULL",
+		"objet" => "VARCHAR (25) DEFAULT '' NOT NULL",
+		"id_objet" => "bigint(21) NOT NULL");
+
 	$spip_gis_liens_key = array(
-		"PRIMARY KEY" 	=> "id_gis,id_objet,objet",
+		"PRIMARY KEY" => "id_gis,id_objet,objet",
 		"KEY id_objet" => "id_gis");
-	
+
 	$tables_auxiliaires['spip_gis_liens'] = array(
 		'field' => &$spip_gis_liens,
 		'key' => &$spip_gis_liens_key);
-	
+
 	return $tables_auxiliaires;
 }
 
