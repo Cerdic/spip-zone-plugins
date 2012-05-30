487f3193c05d6237c93842c0ebf515f82e94ab70
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-05-30 01:39:28 +0000

    langues (paquet-mesfavoris)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@61836 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- mesfavoris/lang/paquet-mesfavoris_fr.php
+++ mesfavoris/lang/paquet-mesfavoris_fr.php
@@ -1,19 +1,13 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-mesfavoris
-// Langue: fr
-// Date: 29-05-2012 10:39:30
-// Items: 2
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/mesfavoris/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// M
+	// M
 	'mesfavoris_description' => '(Dé)Sélectionne un objet pour gérer des objets (articles, rubriques ...) favoris pour un visiteur authentifié',
-	'mesfavoris_slogan' => 'Sélection de favoris',
+	'mesfavoris_slogan' => 'Sélection de favoris'
 );
+
 ?>
