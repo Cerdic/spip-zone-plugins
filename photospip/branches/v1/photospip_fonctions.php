f713863fcdaac387a05ed52a357ef39f72ccb112
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-06-01 15:23:36 +0000

    La première version clean qui marche
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@61976 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ photospip/branches/v1/photospip_fonctions.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * PhotoSPIP
+ * Modification d'images dans SPIP
+ *
+ * Auteurs :
+ * kent1 (kent1@arscenic.info -  http://www.kent1.info)
+ *
+ * © 2008-2012 - Distribue sous licence GNU/GPL
+ * Pour plus de details voir le fichier COPYING.txt
+ *
+ */
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+/*
+
+@argument balise : balise sur lequel est appliquer le filtre appliquer_filtre
+@argument filtre : nom du filtre à appliquer
+@param : paramètres pour le filtre 
+
+@return : retourn la balise traité par filtre si le filtre existe autrement la balise appelante non traitée
+
+*/
+
+function photospip_appliquer_filtre($balise, $filtre,$param1=NULL,$param2=NULL,$param3=NULL) {
+	$filtre = chercher_filtre($filtre);
+	spip_log("On a trouvé $filtre",'photospip');
+	if (function_exists($filtre)){
+		spip_log("$filtre($balise,$param1,$param2,$param3);","photospip");
+		if ($param1){
+			if($filtre == 'image_sepia')
+				$param1 = str_replace('#','',$param1);
+			return $filtre($balise,$param1,$param2,$param3);
+			spip_log("$filtre($balise,$param1,$param2,$param3);","photospip");
+		}
+		else{
+			return $filtre($balise);
+			spip_log("$filtre($balise);","photospip");
+		}
+	} else {
+		return $balise;
+	}
+}
+?>
