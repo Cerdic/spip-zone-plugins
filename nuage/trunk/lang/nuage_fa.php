9d7da3e6c22bfcaba56f6e169265d7d5a8269b66
davoodhossein@yahoo.com <davoodhossein@yahoo.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-06-01 01:39:12 +0000

    langues (nuage)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@61933 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ nuage/trunk/lang/nuage_fa.php
@@ -0,0 +1,19 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org/tradlang_module/nuage?lang_cible=fa
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// N
+	'noisette_description' => 'نمايش يك ابر از كليد‌واژه‌ها',
+	'noisette_label_afficher_titre' => 'نمايش تيتر: ',
+	'noisette_label_niveau_titre' => 'گزينش سطح تيتر',
+	'noisette_nom_noisette' => 'ابر',
+	'noisette_titre_defaut' => 'برچست‌ها',
+	'noisette_titre_noisette' => 'تيتر:'
+);
+
+?>
