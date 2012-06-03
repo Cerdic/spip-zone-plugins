47fd23ac7306f50701a3640fbe8e664713c5c0dc
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-06-03 14:01:43 +0000

    compat SPIP 3.0
    paquet.xml
    doc2img_administrations
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@62050 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ doc2img/trunk/lang/paquet-doc2img_fr.php
@@ -0,0 +1,20 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-doc2img
+// Langue: fr
+// Date: 03-06-2012 15:51:34
+// Items: 3
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// D
+	'doc2img_description' => 'Ce plugin permet de convertir à la volée des pdf, tiff et autres types de document en image ou série d\'images. Ces images sont ensuite exploitables via une boucle spécifique.',
+	'doc2img_nom' => 'Documents en Images',
+	'doc2img_slogan' => 'Convertir des documents textuels à la volée',
+);
+?>
