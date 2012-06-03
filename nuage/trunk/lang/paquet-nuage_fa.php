5b042f9f91453b818c83f383a6c7b86b7f984825
davoodhossein@yahoo.com <davoodhossein@yahoo.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-06-03 01:35:33 +0000

    langues (paquet-nuage)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@62033 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ nuage/trunk/lang/paquet-nuage_fa.php
@@ -0,0 +1,21 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org/tradlang_module/paquet-nuage?lang_cible=fa
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// N
+	'nuage_description' => 'اين پلاگين كليد‌واژه‌ها را به صورت فله با تغيير اندازه‌ قلم تعدادي از موضوع‌هاي مربوط به كليد واژه نشان مي‌دهد. به عبارت ديگر، اسپيپ مي‌توان «تگ» خلق كند!
+چند مدل در دسترس است:
+-* article_nuage: كليدواژه‌هاي مقاله را،‌ با اندازه‌هاي متفاوت طبق وفور كلمات  در مقالات وب سايت نشان مي‌دهد
+-* rubrique_nuage: كليدواژه‌هاي مقاله‌ها را در شاخه‌ها نشان مي‌دهد، انداز طبق وفور كلمات در مقاله‌ي همان شاخه تغيير خواهد كرد. 
+-* nuage_popularite: كليدواژه‌هاي وب سايت (يا گروه) را با اندازه‌ي متغيير طبق محبوبيت نسبي (كل محبوبيت مقاله‌هاي داراي آن واژه) نشان مي‌دهد. 
+
+',
+	'nuage_slogan' => 'نمايش كليدواژه‌ه با تغيير اندازه‌ي قلم'
+);
+
+?>
