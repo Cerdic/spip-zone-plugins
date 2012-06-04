dd7d97cf43b928627fa4b51906badaca6f13035a
davoodhossein@yahoo.com <davoodhossein@yahoo.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-06-04 01:38:49 +0000

    langues (opensearch)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@62069 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ opensearch/lang/opensearch_fa.php
@@ -0,0 +1,21 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org/tradlang_module/opensearch?lang_cible=fa
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cfg_boite_opensearch' => 'پيكربندي پلاگين اوپن سرچ (OpenSearch)',
+	'cfg_descr_opensearch' => 'اوپن سرچ: موتور جستجوگر سفارشي براي سايت شما.',
+	'cfg_inf_id_rubrique' => 'بخش مورد نظر براي جستچو را برگزينيد',
+	'cfg_inf_page_recherche' => 'اسكلت براي حستجو در سايت را مشخص كنيد (مثلن: جستجو روي اسلكت recherche.html )',
+	'cfg_lbl_id_rubrique' => 'بخش هدف',
+	'cfg_lbl_page_recherche' => 'اسكلت جستجو',
+	'cfg_option_tout' => 'تمام سايت',
+	'cfg_titre_opensearch' => 'اوپن سرچ'
+);
+
+?>
