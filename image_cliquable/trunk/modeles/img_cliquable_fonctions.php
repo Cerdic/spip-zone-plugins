5010ad53520623bcdf467769b0ec6d7d805a887a
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-06-04 16:19:06 +0000

    Validité W3C en cas de & dans l'URL de destination
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@62123 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- image_cliquable/trunk/modeles/img_cliquable_fonctions.php
+++ image_cliquable/trunk/modeles/img_cliquable_fonctions.php
@@ -2,6 +2,6 @@
 if (!defined('_ECRIRE_INC_VERSION')) return; 
 
 function accessibilite_enlien($texte) {
-	return extraire_attribut(propre('[->'.$texte.']'),'href');
+	return str_replace('&','&amp;',str_replace('&amp;','&',extraire_attribut(propre('[->'.$texte.']'),'href')));
 }
 ?>
