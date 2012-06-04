01b85e017e7f21d1de7c915169f6aee574500b57
davoodhossein@yahoo.com <davoodhossein@yahoo.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-06-04 01:38:51 +0000

    langues (pages)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@62070 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ pages/trunk/lang/pages_fa.php
@@ -0,0 +1,32 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org/tradlang_module/pages?lang_cible=fa
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'aucune_page' => ' براي الأن هيچ صفحه‌ي نيست',
+
+	// C
+	'convertir_article' => 'تبديل به يك مقاله',
+	'convertir_page' => 'تبديل به يك صفحه',
+	'creer_page' => 'آفرينش يك صفحه‌ي نو',
+
+	// I
+	'input_page' => 'une_page',
+
+	// M
+	'modifier_page' => 'اصلاح صفحه: ',
+
+	// P
+	'pages_uniques' => 'صفحه‌هاي تك',
+
+	// T
+	'titre_page' => 'صفحه',
+	'toutes_les_pages' => 'تمام صفه‌ها'
+);
+
+?>
