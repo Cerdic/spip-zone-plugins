45901ffe4cd6fb708f536ff29841b30a3d77ea8d
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-06-05 01:39:43 +0000

    langues (paquet-facteur)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@62138 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ facteur/trunk/lang/paquet-facteur_sk.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org/tradlang_module/paquet-facteur?lang_cible=sk
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// F
+	'facteur_description' => 'Facteur s\'occupe de la distribution des courriels au format HTML, texte ou mixte ; via SMTP ou non', # NEW
+	'facteur_nom' => 'Poštár',
+	'facteur_slogan' => 'Posiela vašu poštu'
+);
+
+?>
