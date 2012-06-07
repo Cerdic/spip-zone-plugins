6274033296be3c8aca2effa301d245fce2bc21a4
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-06-07 12:28:01 +0000

    photospip.css.html et photospip.js.html ne sont plus d'actualité
    La version pour SPIP 2.1 passe en stable
    
    Le trunk passe spécifiquement pour la version pour SPIP 3.0
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@62224 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ photospip/trunk/lang/paquet-photospip_fr.php
@@ -0,0 +1,19 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-photospip
+// Langue: fr
+// Date: 07-06-2012 14:21:46
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// P
+	'photospip_description' => 'Modifier les images insérées dans SPIP en leur appliquant différents filtres',
+	'photospip_slogan' => 'Modification d\'images dans SPIP',
+);
+?>
