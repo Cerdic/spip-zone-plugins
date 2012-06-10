9d5b3a61d69ef5482761d9c96fa3502ba9a2f267
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-06-10 11:20:14 +0000

    Traiter correctement le texte d'un log sur plusieurs lignes.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@62367 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- simplog/trunk/prive/squelettes/contenu/simplog_fonctions.php
+++ simplog/trunk/prive/squelettes/contenu/simplog_fonctions.php
@@ -21,13 +21,19 @@ function simplog_phraser_ligne($contenu) {
 	foreach ($contenu as $_ligne) {
 		if ($l = trim($_ligne) AND $l != '[-- rotate --]') {
 			preg_match('#^(.*:\d\d)\s(.*)\s\(pid\s(.*)\)\s:([bipru]*):([^:]*):\s(.*)$#i', $_ligne, $matches);
-			$ligne['date'] = date('Y-m-d H:i:s', strtotime($matches[1]));
-			$ligne['ip'] = trim($matches[2]);
-			$ligne['pid'] = trim($matches[3]);
-			$ligne['hit'] = _T('simplog:info_hit_'. strtolower(trim($matches[4])));
-			$ligne['gravite'] = strtolower(trim($matches[5]));
-			$ligne['texte'] = trim($matches[6]);
-			$tableau[] = $ligne;
+			if (!$matches[1]) {
+				// Ce n'est pas une nouvelle ligne mais la suite du texte de la ligne en cours
+				$tableau[count($tableau)-1]['texte'] .= "\n" . trim($_ligne);
+			}
+			else {
+				$ligne['date'] = date('Y-m-d H:i:s', strtotime($matches[1]));
+				$ligne['ip'] = trim($matches[2]);
+				$ligne['pid'] = trim($matches[3]);
+				$ligne['hit'] = _T('simplog:info_hit_'. strtolower(trim($matches[4])));
+				$ligne['gravite'] = strtolower(trim($matches[5]));
+				$ligne['texte'] = trim($matches[6]);
+				$tableau[] = $ligne;
+			}
 		}
 	}
 
