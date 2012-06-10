a0a00bbbaf09699531256ac707b3f4258288913b
renato@rezo.net <renato@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-06-10 08:21:08 +0000

    langue italienne
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@62364 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ mediatheque/lang/medias_it.php
@@ -0,0 +1,166 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'aucun_document' => 'Nessun documento',
+	'aucun_file'=>'Nessun documento',
+	'aucun_image'=>'nessuna immagine',
+	'aucun_audio'=>'Nessun suono',
+	'aucun_video'=>'Nessun video',
+	'aucune_utilisation'=>'Questo documento non &egrave; utilizzato',
+	'aucune_vignette' => 'Nessuna miniatura',
+
+	
+	// B
+	'bouton_copier_local'=>'Copia nel sito',
+	'bouton_download_depuis'=>'da:',
+	'bouton_download_local' => 'mio computer',
+	'bouton_download_par_mediatheque' => 'la mediateca',
+	'bouton_download_par_ftp' => 'il server',
+	'bouton_download_sur_le_web' => 'internet',
+	'bouton_attacher_document' => 'Collega',
+	'bouton_enlever_document' => 'Scollega',
+	'bouton_enlever_supprimer_document' => 'Elimina',
+	'bouton_enlever_supprimer_document_confirmation' => 'Sei sicuro di voler eliminare il documento?',
+	'bouton_modifier_document' => 'Modifica',
+	'bouton_parcourir' => 'Sfoglia...',
+	'bouton_refresh_document' => 'Aggiorna il documento',
+	'bouton_remplacer_image' => 'Sostituisci questa immagine',
+	'bouton_remplacer_document' => 'Sostituisci questo documento',
+	'bouton_remplacer_image_document' => 'Sostituisci con una immagine o documento',
+	'bouton_supprimer' => 'Elimina',
+	'bouton_supprimer_tous_orphelins' => 'Elimina tutti i documento non collegati',
+	'bouton_supprimer_tous_orphelins_confirmation' => 'Sei sicuro di voler eliminare @nb@ documenti non collegati?',
+	'bouton_upload' => 'Upload',
+
+	
+	// D
+	'documents'=> 'Mediateca',
+	'documents_distants'=> 'Remoti',
+	'documents_locaux'=> 'Locali',
+	'documents_non_publies'=> 'Non pubblicati',
+	'documents_publies'=> 'Pubblicati',
+	'documents_tous'=> 'Tutti',
+	'documents_brises' => 'Non trovati',
+	'documents_orphelins' => 'Inutilizzati',
+	'descriptif'=>'Descrizione',
+	'des_documents'=>'@nb@ documenti',
+	'des_files'=>'@nb@ documenti',
+	'des_images'=>'@nb@ immagini',
+	'des_audios'=>'@nb@ suoni',
+	'des_videos'=>'@nb@ video',
+	'des_utilisations'=>'@nb@ utilizzazioni',
+	'document_installe_succes' => 'Il file &egrave; stato caricato correttamente',
+	'document_copie_locale_succes' => 'Il file &egrave; stato copiato nel sito',
+	'nb_documents_installe_succes' => '@nb@ file caricati correttamente',
+	'document_vu' => 'Inserito nel testo',
+	'document_attache_succes' => 'Il documento &egrave; stato correttamente aggiunto',
+	
+	// E
+	'erreurs' => '@nb@ errori',
+	'erreurs_voir' => 'Vedi i @nb@ errori',
+	'erreur_indiquez_un_fichier'=>'Specifica un file!',
+	'erreur_aucun_document'=>'Questo documento non esiste nella mediateca',
+	'erreur_aucun_fichier'=>'nessun file trovato',
+	'erreur_upload_type_interdit'=> "Il caricamento dei file di tipo @nom@ non &egrave; autorizzato",
+	'erreur_chemin_ftp'=>'Il file specificato non &egrave; stato trovato sul server',
+	'erreur_chemin_distant' => 'Il file remoto @nom@ non &egrave; stato trovato',
+	'erreur_copie_fichier' => 'Impossibile copiare il file @nom@',
+	'erreur_format_fichier_image' => 'Il formato di @nom@ non corrisponde ad una immagine',
+	'erreur_upload_vignette' => 'Errore durante il caricamento della miniatura @nom@',
+	
+	// F
+	'format_date_incorrect' => 'La data o l\'ora non sono valide',
+	'fichier_modifie' => 'Il file &egrave; stato modificato. Clicca su salva.',
+	
+	// I
+	'icone_creer_document' => 'Aggiungi un documento',
+	'id' => 'ID',
+	'infos'=>'Informazioni tecniche',
+	'info_modifier_document'=>'Modifica il documento',
+	'info_heure' => 'Ora',
+	'info_largeur' => 'Larghezza',
+	'info_hauteur' => 'Altezza',
+	'info_gauche_numero_document'=>'DOCUMENTO NUMERO',
+	'info_statut_document'=> 'Questo documento &egrave;:',
+	'info_illustrations' => 'Illustrazioni',
+	'info_doc_max_poids' => 'I documenti devono obbligatoriamente essere pi&ugrave; piccoli di @maxi@ (questo file &egrave; di @actuel@).',
+	'info_image_max_poids' => 'Le immagini devono obbligatoriamente essere pi&ugrave; piccole di @maxi@ (questo file &egrave; di @actuel@).',
+
+	// L
+	'logo' => 'Logo',
+	'label_refdoc_joindre' => 'Documento numero',
+	'label_parents' => 'Questo documento &egrave; collegato a',
+	'label_fichier' => 'File',
+	'label_fichier_vignette' => 'Miniatura',
+	'label_caracteristiques' => 'caratteristiche',
+	'label_apercu' => 'Anteprima',
+	'label_credits' => 'Credtis',
+	'lien_tout_enlever' => 'Elimina tutto',
+	
+	// M
+	'media_audio' => 'Suoni',
+	'media_video' => 'Video',
+	'media_image' => 'Immagini',
+	'media_file' => 'Altro',
+
+	// O
+	'objet_documents'=>'Documenti',
+	
+	// P
+	'par_id' => 'ID',
+	'par_date' => 'Data',
+	'par_taille' => 'Peso',
+	'par_largeur' => 'Larghezza',
+	'par_hauteur' => 'Altezza',
+	'par_titre' => 'Titolo',
+	
+	// T
+	'tous_les_medias'=>'Tutti i file multimediali',
+	'titre_page_documents_edit' => 'Modifica il documento: @titre@',
+	
+	// U
+	'un_document' => '1 documento',
+	'un_file'=>'1 documento',
+	'un_image'=>'1 immagine',
+	'un_audio'=>'1 suono',
+	'un_video'=>'1 video',
+	'une_utilisation'=>'1 utilizzazione',
+	'upload_info_mode_document' => 'Aggiungi nel portfolio',
+	'upload_info_mode_image' => 'Elimina dal portfolio',
+	'upload_zip_mode_document' => 'Aggiungi tutte le immagini nel portfolio',
+	
+	//
+	'verifier_documents_brises' => 'Verifica i file mancanti',
+	'vignette_supprimee' => 'La miniatura &egrave; stata eliminata',
+	'voir_descriptif' => 'Vedi la descrizione',
+
+	
+/*	'affichage' => 'Affichage :',
+	'attr_alt' => 'Pas de balise alt ?? ',
+	'conteneur' => 'Conteneur :',
+	'fichier_introuvable' => 'Fichier introuvable',
+	'filtrer' => 'Filtrer :',
+	'info_breve' => 'Breve ',
+	'info_breves' => 'Breves',
+	'info_doc' => 'Cette page r&eacute;capitule la liste de tous vos documents. Pour modifier les informations de chaque document, suivez le lien vers la page de sa rubrique.',
+	'info_reparer' => 'Cette page reconstitue les liens entre documents et articles dans lequel ils sont ins&eacute;r&eacute;s par un tag <code><imgxx> <docxx> ou <embxx></code>',
+	'info_rubrique' => 'Rubrique ',
+	'lien_ajoute' => 'lien ajout&eacute;',
+	'mis_jour_liens' => 'Liens mis a jour',
+	'mis_jour_tailles' => 'Mettre les tailles a jour',
+	'par' => 'Par @numero@',
+	'portfolio' => 'Portfolio',
+	'reparer_liens' => 'Reparer les liens',
+	'info_syndication' => 'Syndication',
+	'sans_titre_descriptif' => 'Sans titre ni descriptif',
+	'taille_erronee' => 'Taille erron&eacute;e',
+	'type' => 'Type :',
+	'tous' => 'Tous',
+	'tous_doc'  => 'Tous vos documents',
+	'tous_docs'  => 'Tous les Documents'*/
+);
+?>
