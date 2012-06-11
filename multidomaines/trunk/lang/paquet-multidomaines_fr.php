93b7a010b7a4d12dbe730969977cc34bf0d2de33
pierrekuhn82@gmail.com <pierrekuhn82@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-06-11 20:38:42 +0000

    Première modification, rajout du paquet et de son fichier langue.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@62396 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ multidomaines/trunk/lang/paquet-multidomaines_fr.php
@@ -0,0 +1,19 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-multidomaines
+// Langue: fr
+// Date: 11-06-2012 22:30:07
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// M
+	'multidomaines_description' => 'Le plug-in Multidomaines permet d\'associer un nom de domaine ou un sous-domaine à un secteur spip. Les balises rajoutées permettent de pointer un lien absolu aussi facilement qu\'avec les balises originales. Il offre la possibilité de faire plusieurs sites avec les mêmes données et/ou un site internet sur plusieurs domaines différents.',
+	'multidomaines_slogan' => 'Associer des secteurs à un domaine',
+);
+?>
