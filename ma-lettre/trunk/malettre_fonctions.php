82810a9d3ae9dc285c0f4d323bc5018b914b6ee3
erational@erational.org <erational@erational.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-06-11 08:10:08 +0000

    la solution pour recuperer_propre avec des adresses publiques on passe par $GLOBALS['lien_implicite_cible_public'] = true;
    grand merci à denisb & rastopopoulos pour cette trouvaille !
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@62381 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- ma-lettre/trunk/malettre_fonctions.php
+++ ma-lettre/trunk/malettre_fonctions.php
@@ -6,6 +6,12 @@
 function malettre_get_contents($patron,$id_edito=0,$selection,$selection_eve,$lang) {
 
  $date = date('Y-m-d');
+ 
+ // on passe la globale ien_implicite_cible_public en true 
+ // pour avoir les liens internes en public (en non prive d'apres le contexte)
+ // credit de l'astuce: denisb & rastapopoulos
+ $GLOBALS['lien_implicite_cible_public'] = true;
+
  $texte_patron =  recuperer_fond(
 		"public/$patron",
 		array(
@@ -17,12 +23,9 @@ function malettre_get_contents($patron,$id_edito=0,$selection,$selection_eve,$la
           )
 	);
   
-  // hack pourri en attendant mieux pour gerer les urls internes
-  // piste denisb: explorer le 3er argument de recuperer_fond $options['compil'][0]
-  $texte_patron = str_replace("ecrire/?exec=article&", "?page=article&",$texte_patron);
-  $texte_patron = str_replace("ecrire/?exec=rubrique&", "?page=rubrique&",$texte_patron);
-  $texte_patron = str_replace("ecrire/?exec=auteur&", "?page=auteur&",$texte_patron);
-	
+  // on revient a la config initiale
+  unset($GLOBALS['lien_implicite_cible_public']);
+
   return $texte_patron;	
   			          	
 }
