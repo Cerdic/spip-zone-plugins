49013d1c39a164b113e0f9d899b13e3c3b71fcf4
pierrekuhn82@gmail.com <pierrekuhn82@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-06-11 20:47:49 +0000

    Mise à jour du paquet.xml
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@62398 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- multidomaines/trunk/lang/paquet-multidomaines_fr.php
+++ multidomaines/trunk/lang/paquet-multidomaines_fr.php
@@ -2,12 +2,6 @@
 
 // Ceci est un fichier langue de SPIP -- This is a SPIP language file
 
-///  Fichier produit par PlugOnet
-// Module: paquet-multidomaines
-// Langue: fr
-// Date: 11-06-2012 22:30:07
-// Items: 2
-
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
