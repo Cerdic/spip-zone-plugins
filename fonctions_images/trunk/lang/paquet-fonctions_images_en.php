ec2e6c3f6246759bbd90227a57575e8b6a76e1b4
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-06-12 21:40:12 +0000

    Début de passage à SPIP 3.0 : -* paquet.xml + ses fichiers de langue -* déplacement de la démo dans un répertoire démo -* on repasse en test pour l'instant
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@62436 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ fonctions_images/trunk/lang/paquet-fonctions_images_en.php
@@ -0,0 +1,39 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-fonctions_images
+// Langue: en
+// Date: 12-06-2012 23:37:32
+// Items: 3
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// F
+	'fonctions_images_description' => 'Provides image processing filters, mainly
+extracted from the website [Paris-Beyrouth->http://www.paris-beyrouth.org/].
+
+{{Proposed filters :}}
+-* en_image (usefull for SPIP < 1.9.3)
+		
+-* couleur_chroma
+-* couleur_saturer
+-* couleur_teinter
+-* couleur_inverserluminosite
+-* couleur_foncerluminosite
+-* couleur_eclaircirluminosite
+-* image_contour_alpha
+-* image_courbe
+-* image_dispersion
+-* image_estampage_alpha
+-* image_float
+-* image_niveaux_gris_auto
+-* image_podpod
+-* image_saturer',
+	'fonctions_images_nom' => 'Images functions',
+	'fonctions_images_slogan' => 'Filters for images functions',
+);
+?>
---
+++ fonctions_images/trunk/lang/paquet-fonctions_images_fr.php
@@ -0,0 +1,37 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-fonctions_images
+// Langue: fr
+// Date: 12-06-2012 23:37:32
+// Items: 3
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// F
+	'fonctions_images_description' => 'Fournit des filtres pour le traitement des images, essentiellement extraits du site [Paris-Beyrouth->http://www.paris-beyrouth.org/].
+
+{{Filtres proposés :}}
+-* en_image (utile pour SPIP < 1.9.3)
+-* couleur_chroma
+-* couleur_saturer
+-* couleur_teinter
+-* couleur_inverserluminosite
+-* couleur_foncerluminosite
+-* couleur_eclaircirluminosite
+-* image_contour_alpha
+-* image_courbe
+-* image_dispersion
+-* image_estampage_alpha
+-* image_float
+-* image_niveaux_gris_auto
+-* image_podpod
+-* image_saturer',
+	'fonctions_images_nom' => 'Fonctions d\'images',
+	'fonctions_images_slogan' => 'Filtres pour le traitement des images',
+);
+?>
