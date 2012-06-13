aec1042a034aace5ec060703774a6e5f9c4d94dd
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-06-13 13:03:42 +0000

    Des bidouilles pour être mieux en SPIP 3.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@62468 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ cvt-upload/lang/paquet-cvtupload_fr.php
@@ -0,0 +1,19 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-cvtupload
+// Langue: fr
+// Date: 13-06-2012 14:56:15
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// C
+	'cvtupload_description' => 'Fournit des outils pour aider à charger des fichiers proprement depuis un formulaire CVT.',
+	'cvtupload_slogan' => 'Télécharger proprement des fichiers depuis un formulaire CVT',
+);
+?>
