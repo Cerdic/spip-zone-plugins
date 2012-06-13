fb2546ca0aa29d86bb2031c0939dd655c0491a45
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-06-13 19:47:14 +0000

    La première branche
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@62502 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ spipicious_jquery/branches/v1/lang/spipicious_fr.php
@@ -0,0 +1,61 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/spipicious_jquery/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'add_tags' => 'Ajouter des tags',
+	'aucun_tags' => 'Aucun tag associé.',
+	'auteur_tag_usage' => 'Cet auteur utilise @nb@ fois ce tag',
+
+	// B
+	'by' => 'Par',
+
+	// C
+	'cfg_description' => 'Ce plugin permet d\'ajouter aisément des tags aux objets.',
+
+	// E
+	'explication_ajout_tag' => 'Pour ajouter plusieurs tags à la fois, utilisez le séparateur : "@separateur@".',
+	'explication_suppression_tag' => 'Sélectionnez vos tags dans la liste ci-dessous et validez pour les supprimer.',
+
+	// I
+	'identifier' => 'Pour ajouter des tags sur cet article, vous devez vous identifier',
+
+	// L
+	'label_tags' => 'Tags :',
+	'liste_auteurs_tag' => 'Ce tag est utilisé par :',
+
+	// M
+	'message_aucun_tags' => 'Aucun tag n\'est associé',
+	'message_installation_activation' => 'Installation des tables et configuration par défaut de spip.icio.us',
+	'message_upgrade_database' => 'Mise à de la base de donné de spip.ici.ous en version @version@<br/>',
+
+	// N
+	'no_tags_associes' => 'Vous n\'avez pas de mot clé associé',
+	'nom_groupe_tag' => 'Groupe de mots clé associé',
+
+	// S
+	'spipicious' => 'Spip.icio.us',
+
+	// T
+	'tag_ajoute' => 'Vous avez ajouté un tag :<br />@name@',
+	'tag_deja_present' => 'Vous aviez déjà ajouté ce tag.',
+	'tag_supprime' => 'Vous avez supprimé le tag :<br />@name@',
+	'tags' => 'Tags :',
+	'tags_ajoutes' => 'Vous avez ajouté @nb@ tags :<br />@name@',
+	'tags_supprimes' => 'Vous avez supprimé @nb@ tags :<br />@name@',
+	'title_tag_utilise_nb' => 'Ce mot clé est associé par @nb@ personnes.',
+	'title_tag_utilise_un' => 'Ce mot clé est associé par une personne.',
+	'to' => 'avec',
+	'types_utilisateurs' => 'Qui a accès au formulaire d\'ajout de tags ?',
+
+	// V
+	'vos_tags' => 'Vos tags (modification)',
+
+	// W
+	'waiting' => 'recherche de vos tags'
+);
+
+?>
