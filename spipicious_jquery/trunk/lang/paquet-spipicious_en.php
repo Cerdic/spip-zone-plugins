15b937f46eaa32984551ad9ec220c5566d2a43a9
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-06-14 01:40:44 +0000

    langues (paquet-spipicious)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@62538 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spipicious_jquery/trunk/lang/paquet-spipicious_en.php
+++ spipicious_jquery/trunk/lang/paquet-spipicious_en.php
@@ -1,21 +1,17 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-spipicious
-// Langue: en
-// Date: 13-06-2012 23:51:28
-// Items: 2
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org/tradlang_module/paquet-spipicious?lang_cible=en
+// ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// S
+	// S
 	'spipicious_description' => 'Help authenticated users to add keywords/tags to objects from the public area.
 _ Keywords are added to a group we can configure (\'{{- tags -}}\' by default)
 _ Icon from [Pawel Kadysz->http://oneseventyseven.com/]',
-	'spipicious_slogan' => 'Tag each objects',
+	'spipicious_slogan' => 'Tag each objects'
 );
+
 ?>
--- spipicious_jquery/trunk/lang/paquet-spipicious_fr.php
+++ spipicious_jquery/trunk/lang/paquet-spipicious_fr.php
@@ -1,21 +1,15 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-spipicious
-// Langue: fr
-// Date: 13-06-2012 23:51:28
-// Items: 2
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/spipicious_jquery/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// S
+	// S
 	'spipicious_description' => 'Permet aux visiteurs authentifiés d\'ajouter des tags (mots-clés) aux différents objets.
 _ Les mots-clés sont ajoutés dans un groupe configurable (par défaut \'{{- tags -}}\')
 _ Icone de [Pawel Kadysz->http://oneseventyseven.com/]',
-	'spipicious_slogan' => 'Tagger tous les objets',
+	'spipicious_slogan' => 'Tagger tous les objets'
 );
+
 ?>
