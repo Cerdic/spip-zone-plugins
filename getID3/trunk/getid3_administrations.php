bba4dc80841ded89b8d80262714f6494d9473da2
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-06-14 09:43:16 +0000

    Un processus d'installation SPIP 3
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@62541 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- getID3/trunk/getid3_administrations.php
+++ getID3/trunk/getid3_administrations.php
@@ -13,45 +13,34 @@
 
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
-include_spip('inc/meta');
-
 function getid3_upgrade($nom_meta_base_version,$version_cible){
-	$current_version = 0.0;
-	if (   (!isset($GLOBALS['meta'][$nom_meta_base_version]) )
-			|| (($current_version = $GLOBALS['meta'][$nom_meta_base_version])!=$version_cible)){
-		if (version_compare($current_version,'0.0','<=')){
-			include_spip('base/create');
-			maj_tables('spip_documents');
-			$getid3_binaires = charger_fonction('getid3_verifier_binaires','inc');
-			$getid3_binaires(true);
-			
-			ecrire_meta($nom_meta_base_version,$current_version=$version_cible,'non');
-			echo '<p>'._T('getid3:install_ajout_champs_documents').'</p>';
-		}
-		if (version_compare($current_version,'0.1','<')){
-			include_spip('base/create');
-			maj_tables('spip_documents');
-			ecrire_meta($nom_meta_base_version,$current_version=0.1);
-			echo _T('getid3:install_mise_a_jour_base',array('version'=>'0.1'));
-		}
-		if (version_compare($current_version,'0.2','<')){
-			include_spip('base/create');
-			maj_tables('spip_documents');
-			ecrire_meta($nom_meta_base_version,$current_version=0.2);
-			echo _T('getid3:install_mise_a_jour_base',array('version'=>'0.2'));
-		}
-		if (version_compare($current_version,'0.3.1','<')){
-			/**
-			 * Vérifier si les logiciels sont présents
-			 */
-			$getid3_binaires = charger_fonction('getid3_verifier_binaires','inc');
-			$getid3_binaires(true);
-			ecrire_meta($nom_meta_base_version,$current_version='0.3.1');
-		}
-	}
+	$maj = array();
+	
+	$maj['create'] = array(
+		array('maj_tables',array('spip_documents')),
+		array('getid3_verifier_binaires',array())
+	);
+	$maj['0.1'] = array(
+		array('maj_tables',array('spip_documents')),
+	);
+	$maj['0.2'] = array(
+		array('maj_tables',array('spip_documents')),
+	);
+	$maj['0.3.1'] = array(
+		array('getid3_verifier_binaires',array())
+	);
+	
+	
+	include_spip('base/upgrade');
+	maj_plugin($nom_meta_base_version, $version_cible, $maj);
 }
 
 function getid3_vider_tables($nom_meta_base_version) {
 	effacer_meta($nom_meta_base_version);
+}
+
+function getid3_verifier_binaires(){
+	$getid3_binaires = charger_fonction('getid3_verifier_binaires','inc');
+	$getid3_binaires(true);
 }
 ?>
