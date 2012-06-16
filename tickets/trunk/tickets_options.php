5900ab13e12211135d226bb02229a49d58368dab
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-06-16 13:24:17 +0000

    Spip-3-isation plus poussée de la configuration des tickets
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@62591 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- tickets/trunk/tickets_options.php
+++ tickets/trunk/tickets_options.php
@@ -6,5 +6,7 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
  * Activer le plugin no_spam sur les tickets
  */
 $GLOBALS['formulaires_no_spam'][] = 'editer_ticket';
+// Liste des pages de configuration dans l'ordre de presentation
+define('_TICKETS_PAGES_CONFIG', 'general:autorisations');
 
 ?>
