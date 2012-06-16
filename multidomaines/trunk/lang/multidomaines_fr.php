5acff0174e3714ac9b565b961f3352160dab8884
pierrekuhn82@gmail.com <pierrekuhn82@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-06-16 08:59:20 +0000

    Amélioration des fichier de langues.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@62584 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- multidomaines/trunk/lang/multidomaines_fr.php
+++ multidomaines/trunk/lang/multidomaines_fr.php
@@ -6,17 +6,25 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-//T
-	'titre_multidomaines' => 'Multidomaines',
+//C
+	'configurations' => 'Ce formulaire va vous permettre de configurer le site de façon général. ',
 
 //L
 	'label_editer_url' => 'Url par defaut',
 	'label_squelette' => 'Dossier des squelettes',
 	'label_url' => 'Url du secteur',
 
+// M
+	'multidomaines_description' => 'Le plug-in Multidomaines permet d\'associer un nom de domaine ou un sous-domaine à un secteur spip. Les balises rajoutées permettent de pointer un lien absolu aussi facilement qu\'avec les balises originales. Il offre la possibilité de faire plusieurs sites avec les mêmes données et/ou un site internet sur plusieurs domaines différents.',
+	'multidomaines_slogan' => 'Associer des secteurs à un domaine',
+
 //P
 	'precisions_editer_url' => 'l\'url doit être valide, complète et se terminer par un slash<br /> ex: https://sous-dmomaine.domaine.com/',
 	'precisions_squelette' => 'ex : squelettes',
 	'precisions_url' => 'l\'url doit être valide,ne contenir qu\'un domaine et se terminer par un slash. ex: https://sous-dmomaine.domaine.com/',
+
+//T
+	'titre_multidomaines' => 'Multidomaines',
+
 );
 ?>
