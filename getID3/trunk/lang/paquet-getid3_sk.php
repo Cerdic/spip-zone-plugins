c9afb474e1541ec1810f3274b68bcdee2446fbb2
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-06-16 01:38:45 +0000

    langues (paquet-getid3)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@62576 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ getID3/trunk/lang/paquet-getid3_sk.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org/tradlang_module/paquet-getid3?lang_cible=sk
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// G
+	'getid3_description' => 'Čítanie a zápis audio značiek do zvukových súborov
+_ [->http://getid3.sourceforge.net/]',
+	'getid3_slogan' => 'Čítanie a zápis audio značiek'
+);
+
+?>
