056a1cd10ff1cc6b9dff9bcf85ef7f824465e426
pierrekuhn82@gmail.com <pierrekuhn82@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-06-16 09:41:04 +0000

    Correction de la lecture des meta.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@62587 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- multidomaines/trunk/balise/url_auteur_absolu.php
+++ multidomaines/trunk/balise/url_auteur_absolu.php
@@ -4,7 +4,7 @@ function balise_URL_AUTEUR_ABSOLU_dist($p) {
 	
 	include_spip('balise/url_');
 	
-	$p->code = '$GLOBALS["meta"]["multidomaines_url"].'.generer_generer_url('auteur', $p);
+	$p->code = '$GLOBALS["meta"]["multidomaines/editer_url"].'.generer_generer_url('auteur', $p);
 
 	
 	$p->interdire_scripts = false;
