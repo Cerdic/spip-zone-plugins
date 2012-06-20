e9aad1961180f88177c3ca5b51e6cd33c24235bd
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-06-20 15:00:01 +0000

    optimisation et robustesse du js
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@62774 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- mailcrypt/mailcrypt.js
+++ mailcrypt/mailcrypt.js
@@ -1,11 +1,5 @@
-function mc_lancerlien(a,b){
-	x='ma'+'ilto'+':'+a+'@'+b;
-	return x;
-}
-$(document).ready(function(){
-	$('.spancrypt').empty();
-	$('.spancrypt').append('@');
-	$('a.spip_mail').attr('title',function(i, val) {
-		return val.replace(/\.\..t\.\./,'@');
-	});
+function mc_lancerlien(a,b){	x='ma'+'ilto'+':'+a+'@'+b;	return x;}
+jQuery(function(){
+	jQuery('.spancrypt').empty().append('@');
+	jQuery('a.spip_mail').attr('title',function(i, val) {	return val.replace(/\.\..t\.\./,'@');	});
 });
