2f628437669e66f4ce9674febfb938c55ae4810c
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-06-20 08:52:38 +0000

    action pour prendre en charge la modération par lien dans les emails de notification
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@62741 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ notifications/trunk/action/instituer_forum_paremail.php
@@ -0,0 +1,67 @@
+<?php
+
+/***************************************************************************\
+ *  SPIP, Systeme de publication pour l'internet                           *
+ *                                                                         *
+ *  Copyright (c) 2001-2011                                                *
+ *  Arnaud Martin, Antoine Pitrou, Philippe Riviere, Emmanuel Saint-James  *
+ *                                                                         *
+ *  Ce programme est un logiciel libre distribue sous licence GNU/GPL.     *
+ *  Pour plus de details voir le fichier COPYING.txt ou l'aide en ligne.   *
+\***************************************************************************/
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+// http://doc.spip.org/@action_instituer_forum_dist
+function action_instituer_forum_paremail_dist() {
+
+	// verification manuelle de la signature : cas particulier de cette action signee par email
+	$arg = _request('arg');
+	$hash = _request('hash');
+
+	include_spip("inc/securiser_action");
+	$action = 'instituer_forum_paremail';
+	$pass = secret_du_site();
+
+	$verif = _action_auteur("$action-$arg", '', $pass, 'alea_ephemere');
+
+	$auth = false;
+	if ($verif==$hash)
+		$auth = true;
+
+	// si hash est ok, verifier si l'email correspond a un auteur qui a le droit de faire cette action
+	if ($auth){
+		$arg = explode("-",$arg);
+		$id_forum = array_shift($arg);
+		$statut = array_shift($arg);
+		// l'email est ce qui reste
+		$email = implode("-",$arg);
+		// reconstituer l'arg pour l'action standard
+		$arg = "$id_forum-$statut";
+
+		if ($message = sql_fetsel("id_objet","objet","spip_forum","id_forum=".intval($id_forum))){
+			// trouver le(s) auteur(s) et verifier leur autorisation
+			$res = sql_select("*","spip_auteurs","email=".sql_quote($email,'','text'));
+			while ($auteur = sql_fetch($res)){
+				if (autoriser("modererforum",$message['objet'],$message['id_objet'],$auteur)){
+					$auth = true;
+					// on ajoute l'exception car on est pas identifie avec cet id_auteur
+					autoriser_exception("modererforum",$message['objet'],$message['id_objet']);
+					break;
+				}
+			}
+		}
+	}
+
+	if ($auth){
+		spip_log("Moderation message $id_forum $statut par $email","moderationparemail"._LOG_INFO_IMPORTANTE);
+		$instituer_forum = charger_fonction("instituer_forum","action");
+		$instituer_forum($arg);
+	}
+
+	// Dans tous les cas on finit sur un minipres qui dit si ok ou echec
+	$titre = ($auth ? "Le message #$id_forum a bien été passé en $statut" : "Vous n'avez pas le droit de moderer ce message");
+	include_spip('inc/minipres');
+	echo minipres($titre);
+
+}
