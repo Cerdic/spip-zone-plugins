1bdeaf72c4568affbb6296b853328c3b50043623
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-06-21 07:26:45 +0000

    Ce ne sera une version que pour SPIP > 3.0
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@62803 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ auteurs_syndic/trunk/lang/paquet-auteurs_syndic_fr.php
@@ -0,0 +1,20 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-auteurs_syndic
+// Langue: fr
+// Date: 21-06-2012 09:22:48
+// Items: 3
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// A
+	'auteurs_syndic_description' => 'Ajoute la possibilité d\'ajouter des auteurs aux sites syndiqués',
+	'auteurs_syndic_nom' => 'Auteurs pour les sites syndiqués',
+	'auteurs_syndic_slogan' => 'Ajoute la possibilité d\'ajouter des auteurs aux sites syndiqués',
+);
+?>
