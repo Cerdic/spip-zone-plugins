b654cf4446e647994c5b2f77966efcb039693884
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-06-22 01:40:35 +0000

    langues (paquet-auteurs_syndic)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@62851 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- auteurs_syndic/trunk/lang/paquet-auteurs_syndic_en.php
+++ auteurs_syndic/trunk/lang/paquet-auteurs_syndic_en.php
@@ -1,20 +1,16 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-auteurs_syndic
-// Langue: en
-// Date: 21-06-2012 09:22:48
-// Items: 3
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org/tradlang_module/paquet-auteurs_syndic?lang_cible=en
+// ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// A
+	// A
 	'auteurs_syndic_description' => 'Adds the ability to add authors to syndicated sites',
 	'auteurs_syndic_nom' => 'Authors for Web site',
-	'auteurs_syndic_slogan' => 'Adds the ability to add authors to syndicated sites',
+	'auteurs_syndic_slogan' => 'Adds the ability to add authors to syndicated sites'
 );
+
 ?>
--- auteurs_syndic/trunk/lang/paquet-auteurs_syndic_fr.php
+++ auteurs_syndic/trunk/lang/paquet-auteurs_syndic_fr.php
@@ -1,20 +1,14 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-auteurs_syndic
-// Langue: fr
-// Date: 21-06-2012 09:22:48
-// Items: 3
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/auteurs_syndic/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// A
+	// A
 	'auteurs_syndic_description' => 'Ajoute la possibilité d\'ajouter des auteurs aux sites syndiqués',
 	'auteurs_syndic_nom' => 'Auteurs pour les sites syndiqués',
-	'auteurs_syndic_slogan' => 'Ajoute la possibilité d\'ajouter des auteurs aux sites syndiqués',
+	'auteurs_syndic_slogan' => 'Ajoute la possibilité d\'ajouter des auteurs aux sites syndiqués'
 );
+
 ?>
