b204896d71918b847ed56914c3bd5934f51e8360
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-06-23 16:36:05 +0000

    On inclu action/editer_article pour SPIP 3.0 et revision_article
    Des classes editer sur les li
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@62894 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- licence/action/licence_ajouter.php
+++ licence/action/licence_ajouter.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Plugin Licence
- * (c) 2007-2009 fanouch
+ * (c) 2007-2012 fanouch
  * Distribue sous licence GPL
  *
  */
@@ -11,6 +11,7 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 function action_licence_ajouter_dist(){
 	$securiser_action = charger_fonction('securiser_action','inc');
 	if ($id_article = intval($securiser_action())){
+		include_spip('action/editer_article');
 		include_spip('inc/modifier');
 		$c = array('id_licence'=>_request('id_licence'));
 		revision_article($id_article, $c);
