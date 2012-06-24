e0da8d661b00040e4a7b34230708b8ff2db9ce0b
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-06-24 01:42:32 +0000

    langues (paquet-spip_proprio)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@62918 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spip_proprietaire/spip_3/lang/paquet-spip_proprio_sk.php
+++ spip_proprietaire/spip_3/lang/paquet-spip_proprio_sk.php
@@ -8,7 +8,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// S
-	'spip_proprio_description' => 'Ce plugin vous propose la génération automatique de {{Mentions Légales}}, {{Conditions d\'Utilisation}} et informations de {{Copyright}} propres à votre site SPIP.
+	'spip_proprio_description' => 'Tento zásuvný modul vám umožňuje automatické vytváranie {{informácií právneho charakteru,}} {{podmienok používania stránky}} a informácií o {{autorských právach}} k vašej stránke v SPIPe.
 
 -* La page de configuration du plugin, accessible depuis l\'onglet "Configuration" de l\'espace privé de SPIP, vous permet de saisir différentes informations sur l\'éditeur du site, son hébergeur et son créateur.
 -* <u>Les textes légaux se basent par défaut sur la législation en vigueur en France ({au 1er janvier 2010})</u> et représentent en grande partie le {{minimum légal pour tout site web}}. Ils sont modifiables dans un fichier de langue spécifique pour vous permettre de les personnaliser.
@@ -18,7 +18,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 Certaines icônes du plugin, dans les modèles publics, proviennent du set "{{[bwpx.icns->http://paularmstrongdesigns.com/portfolio/bwpx-icns/]}}" de {{Paul Armstrong}}, distribué sous licence {Creative Commons Attribution-Share Alike 3.0} (US).
 
-Les icônes de l\'espace privé proviennent du projet {{[GNOME->http://www.gnome.org/]}}.', # NEW
+Les icônes de l\'espace privé proviennent du projet {{[GNOME->http://www.gnome.org/]}}.', # MODIF
 	'spip_proprio_slogan' => 'Automatické vytvorenie informácií právneho charakteru, podmienok používania stránky a poznámky o autorských právach'
 );
 
