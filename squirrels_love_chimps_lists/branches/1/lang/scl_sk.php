c339da54478916f12907c0684399b0073d842dc6
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-06-25 06:03:50 +0000

    langues
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@62963 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ squirrels_love_chimps_lists/branches/1/lang/scl_sk.php
@@ -0,0 +1,95 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org/tradlang_module/scl?lang_cible=sk
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'abonnement_mailinglist' => 'Abonnement à la mailingliste', # NEW
+	'abonnement_mailinglists' => 'Prihlásenie sa na odoberanie príspevkov e-mailovej konferencie',
+	'actualiser_mailinglists_actualise' => 'Keď sa profil autora aktualizuje (synchronizovať diskusnú skupinu)',
+	'actualiser_mailinglists_ajout' => 'Keď bude pridaný autor',
+	'actualiser_mailinglists_enlever' => 'Keď je autor odstránený – presunutý do koša (vylúčený z e-mailovej konferencie)',
+
+	// B
+	'bypass_ml_opt_in' => 'Outrepasser le double opt-in de MailChimp. Les auteurs nouvellement enregistrés ne reçoivent pas de mail de confirmation de MailChimp (MailChimp <a target="_blank" href="http://www.mailchimp.com/kb/article/how-does-confirmed-optin-or-double-optin-work">recommande de ne pas en abuser</a>, donc prudence!)', # NEW
+
+	// C
+	'champ_spip' => 'Polia SPIPu',
+	'champs_extras' => 'Deklarujte polia, ktoré budete používať',
+	'champs_extras_explication' => 'uveďte polia pre každú tabuľku, ktorú budete používať',
+	'champs_supp' => 'Quels informations additionnelles de SPIP voulez-vous utiliser pour la synchronisation avec MailChimp (l\'email est toujours synchronisé)', # NEW
+	'champs_supp_mapping' => 'Utilisez le tableau suivant pour accorder les champs SPIP aux champs MailChimp', # NEW
+	'champs_utilises' => 'Použité polia',
+	'choisir_liste' => 'Vous devez d\'abord <a href="@url@">choisir au moins une liste</a>', # NEW
+	'choix_mailinglists' => 'Ktorá e-mailová konferencia sa má aktualizovať automaticky',
+	'choix_mailinglists_abonnement' => 'Choisissez la/les listes', # NEW
+	'config_squirrel_chimp_lists' => 'Listes', # NEW
+
+	// D
+	'declarer_champs_extras' => 'Deklarujte doplnkové polia SPIPu',
+	'demande_desincription_ok' => 'Votre adresse "@email@" a également été retirée de Mailchimp. Au revoir.', # NEW
+	'demande_inscription_envoyee1' => 'Un mail vous a été envoyé à l\'adresse "@email@".', # NEW
+	'demande_inscription_envoyee2' => 'Pour valider votre abonnement, vous devez cliquer sur le lien dans l\'email que vous avez reçu.', # NEW
+	'demande_inscription_envoyee3' => 'Vérifiez éventuellement dans votre dossier SPAM ou dans votre corbeille si jamais vous ne trouvez pas cet email.', # NEW
+	'demande_inscription_envoyee_ok' => 'Vaše prihlásenie na odoberanie bolo zaregistrované.',
+	'donnees_personnelles' => 'Vaše osobné údaje',
+
+	// E
+	'email' => 'E-mail',
+	'email2_identique' => 'Tento e-mail musí byť rovnaký',
+	'email_confirmer' => 'Potvrďte svoju e-mailovú adresu',
+	'enregistrement_ok' => 'Zmeny boli uložené',
+	'exemple' => 'Príklad',
+
+	// G
+	'gestion_listes' => 'Gestion des listes', # NEW
+
+	// I
+	'id_liste' => 'id liste', # NEW
+	'id_separe_virgules' => 'ID1,ID2,..', # NEW
+	'inscrits_list' => 'Nombre d\'inscrits', # NEW
+
+	// L
+	'listes_mailchimp' => 'Les listes MailChimp gérées par SPIP', # NEW
+
+	// M
+	'mailinglists' => 'E-mailové konferencie',
+	'mailinglists_dispos' => 'Dostupné diskusné skupiny:',
+	'modele' => 'Šablóna:',
+	'modeles' => 'Šablóny:',
+	'modeles_form_abonnement' => 'Formulaire Abonnement', # NEW
+
+	// N
+	'nom' => 'Priezvisko a meno',
+	'nom_liste' => 'Názov',
+
+	// P
+	'pas_accorder' => 'Nezadané',
+
+	// Q
+	'quand_actualiser_mailinglists' => 'Quand voulez-vous actualiser les listes choisies ?', # NEW
+
+	// S
+	'sync_actualise' => 'aktualizované:',
+	'sync_ajoute' => 'pridané:',
+	'sync_auteurs' => 'Synchroniser les auteurs vers MailChimp', # NEW
+	'sync_echec' => 'zlyhanie',
+	'sync_echec_batch' => 'Échec de la souscription en masse', # NEW
+	'sync_erreurs' => 'Chyby:',
+	'sync_message' => 'Správa:',
+	'sync_ok' => 'Synchronizácia bola úspešná:',
+
+	// T
+	'tables_extras' => 'Deklarovať tabuľky, ktoré sa budú používať',
+	'tables_extras_explication' => 'Indiquez le(s)table(s) séparée(s) par virgule, chaque table declarée doit obligatoirement contenir un champ "id_auteur"', # NEW
+	'tout' => 'Toutes les listes', # NEW
+
+	// U
+	'une_liste' => 'Une seule liste' # NEW
+);
+
+?>
