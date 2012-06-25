8aa66e2be4b4041d35ab13a244a684ed736044e2
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-06-25 01:40:53 +0000

    langues (abomailmans)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@62943 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ abomailmans/trunk/lang/abomailmans_sk.php
@@ -0,0 +1,140 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org/tradlang_module/abomailmans?lang_cible=sk
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'abomailman' => 'E-mailová alebo diskusná skupina',
+	'abomailmans' => 'E-mailové alebo diskusné skupiny',
+	'abonne' => 'Prihlasujem sa na odoberanie',
+	'activation' => 'Aktivácia',
+	'active' => 'Aktivovaný',
+	'aucune_langue' => 'Žiaden konkrétny',
+
+	// B
+	'bouton_listes_diffusion' => 'E-mailové skupiny',
+	'btn_abonnement' => 'Prihlásiť sa na odoberanie',
+	'btn_desabonnement' => 'Odhlásiť sa z odoberania',
+
+	// C
+	'choisir_liste' => 'Musíte si vybrať skupinu.',
+	'contenu_date' => 'Novinky z tohto dňa',
+	'creation_droits_insuffisants' => 'Vaše práva sú nedostatočné.',
+
+	// D
+	'desactive' => 'Deaktivovaný',
+	'destinataire' => 'Príjemca',
+
+	// E
+	'email' => 'E-mail',
+	'email_abonnement' => 'Vaša e-mailová adresa',
+	'email_envoye' => 'E-mail bol odoslaný do skupiny: @liste@.',
+	'email_oublie' => 'Zabudli ste svoju e-mailovú adresu',
+	'emailliste_abomailman' => 'E-mailová adresa skupiny',
+	'emailliste_abosympa' => 'L\'adresse e-mail de l\'administrateur Sympa', # NEW
+	'emailliste_subscribe' => 'Abonnement', # NEW
+	'emailliste_unsubscribe' => 'Désabonnement', # NEW
+	'emails_a_renseigner' => 'E-maily pre informácie',
+	'envoi_apercu' => 'Anketa',
+	'envoi_confirmer' => 'Potvrdiť a poslať',
+	'envoi_liste_parametres' => 'Zoznam parametrov [Nepovinné]',
+	'envoi_parametres' => 'Parametre',
+	'envoi_regulier' => 'Automatické odosielanie',
+	'envoi_regulier_info' => 'Nevypĺňajte, ak nechcete využívať automatické odosielanie',
+	'envoi_regulier_tous_les' => 'Poslať všetky',
+	'envoi_vers' => 'príjemca',
+	'envoyer_courier' => 'Poslať e-mail',
+	'envoyer_courier_liste' => 'Tento e-mail poslať do e-mailovej skupiny:',
+	'envoyer_mailmans' => 'Vyberte si šablónu a jej obsah',
+	'erreur_email_liste_oublie' => 'E-mailová adresa skupiny je povinný údaj',
+	'erreur_nobot' => 'Registrácia sa z technických príčin neuskutočnila',
+	'explication_email_subscribe' => 'E-mail na prihlásenie sa na odoberanie v podobe<code>suffixe+subscribe@exemple.org</code>',
+	'explication_email_sympa' => 'Si ce champ est renseigné, la liste est considérée comme une liste de serveur "Sympa", dans le cas contraire comme une liste "Mailman" ou "ezmlm".', # NEW
+	'explication_email_unsubscribe' => 'Email de désabonnement', # NEW
+
+	// I
+	'icone_ajouter_liste' => 'Pridať novú skupinu',
+	'icone_envoyer_mail_liste' => 'Poslať e-mail s textom tejto stránky',
+	'icone_modifier_abomailman' => 'Upraviť skupinu',
+	'icone_retour_abomailman' => 'Vrátiť sa na skupinu',
+	'info_abomailman_aucun' => 'Žiadna skupina',
+	'info_abomailmans_1' => 'Jedna skupina',
+	'info_abomailmans_nb' => '@nb@ skupín',
+	'info_sisympa' => '[Obligatoire si liste Sympa]', # NEW
+	'insciption_listes_legende' => 'Abonnement aux listes de diffusion', # NEW
+	'inscription_lettres_legende' => 'Abonnement aux listes de diffusion et de discussions', # NEW
+
+	// J
+	'je_m_abonne' => 'Cochez pour valider l\'abonnement ou le désabonnement.', # NEW
+
+	// L
+	'label_etat_liste' => 'Stav skupiny',
+	'label_type_abo' => 'Typ',
+	'label_type_ml' => 'Diskusná skupina',
+	'label_type_news' => 'E-mailová skupina',
+	'langue_liste' => 'Jazyk skupiny',
+	'legende_inscription_ml' => 'Inscription à la liste de discussion', # NEW
+	'legende_inscription_news' => 'Inscription à la liste de diffusion', # NEW
+	'legende_inscriptions_ml' => 'Inscription aux listes de discussion', # NEW
+	'legende_inscriptions_news' => 'Inscription aux listes de diffusion', # NEW
+	'les_listes_mailmans' => 'Les listes mailmans, sympa ou ezmlm renseignées', # NEW
+	'lire_article' => 'Čítať článok',
+	'liste_creee' => 'Skupina číslo @id@ (@titre@) bola vytvorená.',
+	'liste_non_existante' => 'Požadovaná skupina neexistuje alebo bola vymazaná',
+	'liste_oublie' => 'Zabudli ste skontrolovať skupinu!',
+	'liste_supprimee' => 'Skupina číslo @id@ (@titre@) bola vymazaná.',
+	'liste_updatee' => 'Skupina číslo @id@ (@titre@) bola aktualizovaná.',
+
+	// M
+	'message' => 'Introduction à votre courrier, avant le contenu issu du site', # NEW
+	'message_confirm_suite' => 'Pour valider votre demande, répondez à la demande de confirmation que vous allez recevoir par mail.', # NEW
+	'message_confirmation_a' => 'Une demande d\'abonnement aux listes suivantes vient d\'être envoyée :', # NEW
+	'message_confirmation_d' => 'Une demande de désabonnement aux listes suivantes vient d\'être envoyée. ', # NEW
+	'message_confirmation_unique_a' => 'Une demande d\'abonnement à la liste suivante vient d\'être envoyée :', # NEW
+	'message_confirmation_unique_d' => 'Une demande de désabonnement à la liste suivante vient d\'être envoyée. ', # NEW
+	'mot' => 'A vypísať články s kľúčovým slovom ',
+
+	// N
+	'nom' => 'Priezvisko a meno (nepovinné)',
+	'nouveau_abomailman' => 'Nová e-mailová skupina',
+
+	// P
+	'pas_template_txt' => 'K tejto šablóne neexistuje textová verzie',
+	'periodicite' => ' dní/dni.',
+	'prenom' => 'Krstné meno',
+	'previsu_html' => 'html',
+	'previsu_txt' => 'nenaformátovaný text',
+
+	// R
+	'rubrique' => 'A vypísať články z rubriky',
+
+	// S
+	'souhaite_rester' => 'Chcem mať prehľad',
+	'sujet' => 'Predmet e-mailu',
+	'sujet_obligatoire' => 'Predmet je povinný.',
+	'suppression_definitive' => 'Definitívne odstránenie!',
+	'supprimer' => 'Odstrániť',
+	'sympa_message_confirmation' => 'Potvrdzovací e-mail bol odoslaný na túto adresu: ',
+
+	// T
+	'template' => 'Vyberte si šablónu a jej obsah',
+	'template_defaut' => 'Predvolená šablóna',
+	'template_defaut_info' => 'Si le fichier modele_choisi.txt.html existe, la newsletter sera envoyée en mode html + texte. Sinon seule la version html sera expédiée.', # NEW
+	'texte_descriptif' => 'Popis',
+	'titre_abomailman' => 'Názov skupiny',
+	'titre_liste_obligatoire' => 'Názov skupiny je povinný údaj',
+	'toute_liste' => 'Všetky e-mailové skupiny',
+
+	// V
+	'verifier_formulaire' => 'Skontrolujte, ako ste vyplnili formulár.',
+	'veut_s_abonner' => 'veut s\'abonner', # NEW
+	'veut_se_desabonner' => 'veut se désabonner', # NEW
+	'voir_modele_depuis' => 'Voir un exemple du modèle avec', # NEW
+	'votre_email' => 'Váš e-mail'
+);
+
+?>
