80bab114e093095e3a7b730ef21b7c3a3dcf6305
joseph@larmarange.net <joseph@larmarange.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-06-26 17:05:02 +0000

    Ajout d'un plugin pour afficher les commentaires HTML dans l'espace privé et ajouter un bouton au porte-plume
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@63017 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ porte_plume_extras/afficher_commentaires_html/lang/commhtml_fr.php
@@ -0,0 +1,10 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_squelettes_/aveline/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	'outil_commentaire_html' => '&lt;!-- Insérer un commentaire HTML --&gt;',
+);
+
+?>
