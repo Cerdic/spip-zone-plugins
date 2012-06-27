25183ec7ade8d7fbec07332053bcae76422509d2
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-06-27 01:38:29 +0000

    langues (paquet-auteurs_syndic)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@63026 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ auteurs_syndic/trunk/lang/paquet-auteurs_syndic_sk.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org/tradlang_module/paquet-auteurs_syndic?lang_cible=sk
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'auteurs_syndic_description' => 'Umožňuje k syndikovaným stránkam pridávať autorov',
+	'auteurs_syndic_nom' => 'Autori na syndikované stránky',
+	'auteurs_syndic_slogan' => 'Umožňuje k syndikovaným stránkam pridávať autorov'
+);
+
+?>
