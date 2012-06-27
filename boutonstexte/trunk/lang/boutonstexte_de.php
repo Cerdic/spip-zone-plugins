b57e5daabed851681145440b89114b99712f4317
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-06-27 01:38:32 +0000

    langues (boutonstexte)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@63027 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- boutonstexte/trunk/lang/boutonstexte_de.php
+++ boutonstexte/trunk/lang/boutonstexte_de.php
@@ -11,30 +11,30 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'augmenter_police' => 'Schrift vergrößern',
 
 	// B
-	'boutonstexte_nom' => 'Boutons dans le Texte', # NEW
+	'boutonstexte_nom' => 'Schaltflächen für Texte',
 
 	// D
 	'diminuer_police' => 'Schrift verkleinern',
-	'donnees_techniques' => 'Unveränderliche technische Einstellungen',
+	'donnees_techniques' => 'Fest eingestellte Parameter',
 
 	// L
 	'label_selector' => 'Erweiterter CSS oder jQuery Ausdruck',
-	'label_skin' => 'Aussehen der Knöpfe',
-	'label_txtBackSpip' => 'Texte retour spip', # NEW
+	'label_skin' => 'Gestaltung der Schaltflächen',
+	'label_txtBackSpip' => 'Zurück zu SPIP',
 	'label_txtOnly' => 'Nur Text *',
 	'label_txtSizeDown' => 'Text verkleinern *',
 	'label_txtSizeUp' => 'Text vergrößern *',
 	'legend_apparence' => 'Gestaltung', # Habillage im Frz. meint i.d.R. Umgebung, Basisgestaltung oder thème.
 
 	// R
-	'retour_a_spip' => 'Zurück zu voller Seite',
+	'retour_a_spip' => 'Zurück zur vollständigen Seite',
 
 	// S
-	'selecteur' => 'ID oder Klasse des Texts',
+	'selecteur' => 'CSS-Klasse oder ID des Texts',
 
 	// T
 	'texte_seulement' => 'Nur Text',
-	'textes_icones' => 'Hilfe zu Ikone'
+	'textes_icones' => 'Tooltips des Icons'
 );
 
 ?>
