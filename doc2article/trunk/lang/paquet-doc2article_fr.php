fa83870b2928d8d7321806eb676930592634dbb0
brunobergot@gmail.com <brunobergot@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-06-28 11:40:01 +0000

    utiliser la balise credit pour citer la source de l'icône
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@63060 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- doc2article/trunk/lang/paquet-doc2article_fr.php
+++ doc2article/trunk/lang/paquet-doc2article_fr.php
@@ -13,8 +13,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 // D
-	'doc2article_description' => 'Importer l\'ensemble des fichiers du répertoire tmp/upload et créer un article par fichier automatiquement.
-	Icone en CC-BY de http://www.fatcow.com/',
+	'doc2article_description' => 'Importer l\'ensemble des fichiers du répertoire tmp/upload et créer un article par fichier automatiquement.',
 	'doc2article_slogan' => 'Importer l\'ensemble des fichiers du répertoire tmp/upload et créer un article par fichier automatiquement',
 );
 ?>
