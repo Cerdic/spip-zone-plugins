0ed3690ae19ba0334c5282656b0602fb738d7aee
brunobergot@gmail.com <brunobergot@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-06-28 11:47:29 +0000

    un paquet.xml pour crud (plugonet powa)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@63061 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ crud/lang/paquet-crud_fr.php
@@ -0,0 +1,19 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-crud
+// Langue: fr
+// Date: 28-06-2012 13:45:37
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// C
+	'crud_description' => 'Une interface C(r)UD pour SPIP qui peut s\'utiliser par l\'intermédiaire d\'une action définie par son url ou par appel direct.',
+	'crud_slogan' => 'Interface de création, mise à jour et suppression d\'un objet',
+);
+?>
