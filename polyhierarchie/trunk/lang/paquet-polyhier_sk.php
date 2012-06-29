a04b149c7b632c2cc8623930ee3546c5d5282375
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-06-29 01:40:33 +0000

    langues (paquet-polyhier)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@63085 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ polyhierarchie/trunk/lang/paquet-polyhier_sk.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org/tradlang_module/paquet-polyhier?lang_cible=sk
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// P
+	'polyhier_description' => 'Zásuvný modul umožňujúci vytvárať krížové odkazy na rubriky a články v nadväzujúcich rubrikách
+	aj v iných rubrikách ako v priamej nadradenej rubrike.',
+	'polyhier_slogan' => 'Vytvorte odkaz na článok alebo rubriku vo viacerých nadradených rubrikách'
+);
+
+?>
