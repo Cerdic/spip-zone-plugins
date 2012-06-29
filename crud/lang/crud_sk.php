f8e819c1bc7d96784086d81cac6052c5715f42b5
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-06-29 01:40:25 +0000

    langues (crud)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@63082 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ crud/lang/crud_sk.php
@@ -0,0 +1,24 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org/tradlang_module/crud?lang_cible=sk
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// E
+	'erreur_action_erronee' => 'CRUD: chyba akcie @action@',
+	'erreur_action_inconnue_table' => 'CRUD: tabuľka @table@ nepozná akciu @action@',
+	'erreur_article_inconnue' => 'CRUD: článok @id@ neexistuje',
+	'erreur_creation' => 'CRUD: objekt typu "@objet@" sa nedá vytvoriť (Skontrolujte si svoje práva)',
+	'erreur_info_obligatoire' => 'CRUD: Pole @info@ je povinné',
+	'erreur_objet_inexistant' => 'CRUD: objekt @objet@ #@id_objet@ neexistuje',
+	'erreur_rubrique_inconnue' => 'CRUD: rubrika @id@ neexistuje',
+	'erreur_suppression' => 'CRUD: chyba pri vymazávaní objektu "@objet@" #@id_objet@ (Skontrolujte si svoje práva)',
+	'erreur_table_erronee' => 'CRUD: chyba tabuľky @table@',
+	'erreur_table_inconnue' => 'CRUD: systém nepozná tabuľku @table@',
+	'erreur_update' => 'CRUD: chyba pri aktualizácii objektu "@objet@" #@id@ (Skontrolujte si svoje práva)'
+);
+
+?>
