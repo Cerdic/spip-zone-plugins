671d245e127a54b358f1ea05ac1ff846f9395f07
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-06-29 01:40:26 +0000

    langues (paquet-crud)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@63083 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- crud/lang/paquet-crud_fr.php
+++ crud/lang/paquet-crud_fr.php
@@ -1,19 +1,13 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-crud
-// Langue: fr
-// Date: 28-06-2012 13:45:37
-// Items: 2
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/crud/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// C
+	// C
 	'crud_description' => 'Une interface C(r)UD pour SPIP qui peut s\'utiliser par l\'intermédiaire d\'une action définie par son url ou par appel direct.',
-	'crud_slogan' => 'Interface de création, mise à jour et suppression d\'un objet',
+	'crud_slogan' => 'Interface de création, mise à jour et suppression d\'un objet'
 );
+
 ?>
---
+++ crud/lang/paquet-crud_sk.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org/tradlang_module/paquet-crud?lang_cible=sk
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'crud_description' => 'Rozhranie C(r)UD pre SPIP, ktoré môžete využívať prostredníctvom akcie, ktorá je definovaná cez svoju url alebo priamo prostredníctvom dialógového okna.',
+	'crud_slogan' => 'Rozhranie na vytváranie, aktualizovanie a vymazávanie objektu'
+);
+
+?>
