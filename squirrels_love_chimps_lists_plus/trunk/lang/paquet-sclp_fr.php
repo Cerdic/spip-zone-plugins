65bdc6a8550768662599240bd2f2aaaa78f9722d
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-06-30 16:34:13 +0000

    Compatibilité spip3
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@63131 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ squirrels_love_chimps_lists_plus/trunk/lang/paquet-sclp_fr.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// M
+	'sclp_description' => 'Gestion avancés de mailing listes de [Mailchimp->http://mailchimp.com/] 
+		
+		Synchronistation entre listes Spip et liste mailchimps',
+	'sclp_nom' => 'Squirrels Love Chimps Plus',
+	'sclp_slogan' => 'Gestion avancé de mailing listes de Spip et de Mailchimp',
+);
+
+?>
