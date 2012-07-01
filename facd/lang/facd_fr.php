aec1ac923e8bf2c1ba8fa6a749eb1e6f7522beb5
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-07-01 01:38:18 +0000

    langues (facd)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@63135 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- facd/lang/facd_fr.php
+++ facd/lang/facd_fr.php
@@ -1,50 +1,56 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/facd/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
+	// B
 	'bouton_relancer_tout' => 'Reconvertir tous les documents',
-	'bouton_relancer_tout_message' => 'Êtes vous sûr de vouloir relancer l\'encodage de tous les fichiers',
 	'bouton_relancer_tout_format' => 'Reconvertir tous les documents au format @format@',
 	'bouton_relancer_tout_format_message' => 'Êtes vous sûr de vouloir relancer l\'encodage de tous les fichiers au format @format@.',
-	
+	'bouton_relancer_tout_message' => 'Êtes vous sûr de vouloir relancer l\'encodage de tous les fichiers',
+
+	// E
+	'erreur_document_plus_disponible' => 'Ce document n\'est plus disponible sur le site',
+	'explication_file_attente' => 'Cette page liste les documents présents dans la file d\'attente. Ceux-ci seront convertis automatiquement par CRON.',
+
+	// I
 	'info_document_conversion' => 'Ce document est en cours de conversion',
 	'info_document_conversion_erreur' => 'La conversion de ce document a échoué',
 	'info_document_dans_file_attente' => 'En attente de conversion',
-	
-	'erreur_document_plus_disponible' => 'Ce document n\'est plus disponible sur le site',
-	'explication_file_attente' => 'Cette page liste les documents présents dans la file d\'attente. Ceux-ci seront convertis automatiquement par CRON.',
-	
 	'info_relancer_erreurs' => 'Relancer toutes les conversions en erreur',
 	'info_statut_conversion_en_cours' => 'En cours',
 	'info_statut_conversion_erreur' => 'En erreur',
 	'info_statut_conversion_non' => 'En attente',
 	'info_statut_conversion_oui' => 'Converti',
 	'info_tous_docs_facd' => 'Documents de la file d\'attente',
-	
+
+	// L
 	'lien_convertir_document' => 'Convertir ce document',
 	'lien_recharger' => 'Recharger',
 	'liste_attente_aucun' => 'Aucun document dans la file d\'attente.',
 	'liste_attente_tous' => 'Documents présents dans la file d\'attente',
 	'liste_convert_aucun' => 'Aucun document converti',
 	'liste_convert_tous' => 'Documents convertis',
-	
+
+	// T
 	'thead_date' => 'Date',
 	'thead_duree' => 'Durée',
 	'thead_duree_conversion' => 'Durée de conversion',
 	'thead_extension' => 'Format',
 	'thead_fonction' => 'Fonction utilisée',
-	'thead_id_auteur' => 'Auteur',
 	'thead_id' => 'ID',
+	'thead_id_auteur' => 'Auteur',
 	'thead_id_document' => 'Document original',
 	'thead_nombre' => 'Nombre',
 	'thead_statut' => 'Statut',
 	'titre_page_file' => 'File d\'attente du module de conversion des medias',
-	'titre_page_file_menu' => 'File d\'attente de conversion',
 	'titre_page_file_convertis_jour' => 'Conversions par date',
-	
-	'version_encodee_de' => 'Ce document est une conversion du document @id_orig@'
+	'titre_page_file_menu' => 'File d\'attente de conversion',
 
+	// V
+	'version_encodee_de' => 'Ce document est une conversion du document @id_orig@'
 );
+
 ?>
---
+++ facd/lang/facd_sk.php
@@ -0,0 +1,58 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org/tradlang_module/facd?lang_cible=sk
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'bouton_relancer_tout' => 'Prekonvertovať všetky dokumenty',
+	'bouton_relancer_tout_format' => 'Prekonvertovať všetky dokumenty vo formáte @format@',
+	'bouton_relancer_tout_format_message' => 'Určite chcete znova spustiť zakódovanie všetkých súborov vo formáte @format@?',
+	'bouton_relancer_tout_message' => 'Určite chcete znova spustiť zakódovanie všetkých súborov',
+
+	// E
+	'erreur_document_plus_disponible' => 'Tento dokument už viac na stránke nie je dostupný',
+	'explication_file_attente' => 'Na tejto stránke je zoznam dokumentov, ktoré sa nachádzajú v rade. CRON ich automaticky skonvertuje.',
+
+	// I
+	'info_document_conversion' => 'Tento dokument sa konvertuje',
+	'info_document_conversion_erreur' => 'Konverzia tohto dokumentu sa nepodarila',
+	'info_document_dans_file_attente' => 'Čaká na konverziu',
+	'info_relancer_erreurs' => 'Znova spustiť všetky nevydarené konverzie',
+	'info_statut_conversion_en_cours' => 'Spracúva sa',
+	'info_statut_conversion_erreur' => 'Chybný',
+	'info_statut_conversion_non' => 'Čaká',
+	'info_statut_conversion_oui' => 'Konvertovaný',
+	'info_tous_docs_facd' => 'Dokumenty v rade',
+
+	// L
+	'lien_convertir_document' => 'Konvertovať tento dokument',
+	'lien_recharger' => 'Obnoviť',
+	'liste_attente_aucun' => 'Žiaden dokument v rade.',
+	'liste_attente_tous' => 'Dokumenty v rade',
+	'liste_convert_aucun' => 'Žiaden konvertovaný dokument',
+	'liste_convert_tous' => 'Skonvertované dokumenty',
+
+	// T
+	'thead_date' => 'Dátum',
+	'thead_duree' => 'Trvanie',
+	'thead_duree_conversion' => 'Trvanie konverzie',
+	'thead_extension' => 'Formát',
+	'thead_fonction' => 'Použitá funkcia',
+	'thead_id' => 'P. č.',
+	'thead_id_auteur' => 'Autor',
+	'thead_id_document' => 'Pôvodný dokument',
+	'thead_nombre' => 'Počet',
+	'thead_statut' => 'Stav',
+	'titre_page_file' => 'Rad modulu na konverziu médií',
+	'titre_page_file_convertis_jour' => 'Konverzie podľa dátumu',
+	'titre_page_file_menu' => 'Rad na konverziu',
+
+	// V
+	'version_encodee_de' => 'Tento súbor je konverziou dokumentu @id_orig@'
+);
+
+?>
