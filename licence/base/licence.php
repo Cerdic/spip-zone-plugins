f90902a482c2d79393b7d4ff47539ea0bd175722
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-07-03 13:16:06 +0000

    Les champs id_licence ajoutés aux tables sont dans les champs_editables
    Version 0.4.1
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@63226 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- licence/base/licence.php
+++ licence/base/licence.php
@@ -1,12 +1,12 @@
 <?php
 /*
  * Plugin Licence
- * (c) 2007-2010 fanouch
+ * (c) 2007-2012 fanouch
  * Distribue sous licence GPL
  *
  */
 
-if (!defined("_ECRIRE_INC_VERSION")) return;
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 function licence_declarer_tables_principales($tables_principales){
 	$tables_principales['spip_documents']['field']['id_licence'] = "bigint(21) NOT NULL";
@@ -14,4 +14,14 @@ function licence_declarer_tables_principales($tables_principales){
 	return $tables_principales;
 }
 
+/**
+ * Insertion dans le pipeline declarer_tables_objets_sql (SPIP)
+ * On ajoute nos champs dans les champs editables de la table spip_documents
+ */
+function licence_declarer_tables_objets_sql($tables){
+	$tables['spip_articles']['champs_editables'][] = 'id_licence';
+	$tables['spip_documents']['champs_editables'][] = 'id_licence';
+	
+	return $tables;
+}
 ?>
