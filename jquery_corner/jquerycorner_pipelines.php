7ea2c5823dfbabe7c99e8ee86fa1426941648804
yffic@cousumain.info <yffic@cousumain.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-07-04 17:08:08 +0000

    Relancer le script sur AjaxReload
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@63269 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- jquery_corner/jquerycorner_pipelines.php
+++ jquery_corner/jquerycorner_pipelines.php
@@ -18,9 +18,15 @@ function jquerycorner_insert_head($flux){
 	// S'il y a au moins un element
 	if($conf_jquerycorner["nombre"]>0) {
 		$flux .="\n".'<script src="'.url_absolue(find_in_path('javascript/jquery.corner.js')).'" type="text/javascript"></script>';
-		$flux .= '
+		$flux .= "\n".'
 <script type="text/javascript">/* <![CDATA[ */
-	jQuery(document).ready(function(){'.$les_effets.'});
+	jQuery(document).ready(function(){
+		function jquerycorner_init(){
+			'.$les_effets.'
+		}
+		jquerycorner_init();
+		if(typeof onAjaxLoad == "function") onAjaxLoad(jquerycorner_init);
+	});
 /* ]]> */</script>
 ';
 	}
