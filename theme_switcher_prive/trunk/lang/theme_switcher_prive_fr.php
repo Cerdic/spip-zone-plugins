6e77f25915a947e864cc8eb83b8bc25e9693d4cc
arnaud.berard@mister-graphx.com <arnaud.berard@mister-graphx.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-07-04 13:50:41 +0000

    - ajout d'un mode elastic layout dans les préférences utilisateurs en plus de ecran_large et petit
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@63263 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- theme_switcher_prive/trunk/lang/theme_switcher_prive_fr.php
+++ theme_switcher_prive/trunk/lang/theme_switcher_prive_fr.php
@@ -9,7 +9,8 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
         'explication_vector_icons'=>'Des icones simples gérant le changement de couleur de l\'espace privé et [provenant de DesignerFolio ->http://www.dezinerfolio.com/freebie/30-free-vector-icons]',
         'explication_spip2'=>'Les icones du bandeau utilisé dans Spip 2',
         'explication_vector_icons_2'=>'Le même jeux mais moins austère',
-        
+        // I
+        'info_elastic'=>'Elastic (largeur adaptable a l&apos;&eacute;cran)',
         // L
         'label_theme'=>'Choix du th&egrave;me de l&apos;espace priv&eacute;',
         'lien_theme_spip_dist'=> 'Theme Priv&eacute; spip3',
