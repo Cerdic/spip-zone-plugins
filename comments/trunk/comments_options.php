27fc2da5bc604efdd70f6b4e26512db8d1af1107
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-07-05 15:49:14 +0000

    Permettre de configurer la longeur mini et maxi des commentaires (la configuration de longueur mini n'aura un effet qu'a partir de la version 1.8.15 du plugin forum de SPIP)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@63298 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- comments/trunk/comments_options.php
+++ comments/trunk/comments_options.php
@@ -1,7 +1,17 @@
 <?php
 
-// Limiter la longueur des messages
-define('_FORUM_LONGUEUR_MAXI', 1500);
+// pas la peine d'init les constantes dans un autre contexte qu'en post
+// ca economise
+if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+	include_spip("inc/config");
+
+	// Limiter la longueur des messages
+	if ($max = lire_config("comments/forum_longueur_maxi"))
+		define('_FORUM_LONGUEUR_MAXI', $max);
+	if ($min = lire_config("comments/forum_longueur_mini"))
+		define('_FORUM_LONGUEUR_MINI', $min);
+}
+
 // pour la version thread
 define('_FORUM_AUTORISER_POST_ID_FORUM',true);
 ?>
