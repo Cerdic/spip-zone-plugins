c73846bd6fecf1fb2d9eaa8fe6ac05e084eddd52
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-07-06 01:38:04 +0000

    langues (paquet-facd)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@63313 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ facd/lang/paquet-facd_en.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org/tradlang_module/paquet-facd?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// F
+	'facd_description' => 'Conversion queue management for documents.',
+	'facd_slogan' => 'Conversion queue management for documents'
+);
+
+?>
