3c3a9faa88691356b8d845aa29fb29263dd2bb06
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-07-06 01:38:03 +0000

    langues (facd)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@63312 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ facd/lang/facd_en.php
@@ -0,0 +1,58 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org/tradlang_module/facd?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'bouton_relancer_tout' => 'Reconvert all documents',
+	'bouton_relancer_tout_format' => 'Reconvert all documents in @format@',
+	'bouton_relancer_tout_format_message' => 'Are you sure you want to restart the conversion of all files in the @format@ format.',
+	'bouton_relancer_tout_message' => 'Are you sure you want to restart the conversion of all files',
+
+	// E
+	'erreur_document_plus_disponible' => 'This document is not available on the website anymore',
+	'explication_file_attente' => 'This page lists the documents in the queue. These will be converted automatically by cron.',
+
+	// I
+	'info_document_conversion' => 'The conversion of this document is in progress',
+	'info_document_conversion_erreur' => 'The conversion of that document failed',
+	'info_document_dans_file_attente' => 'Awaiting conversion',
+	'info_relancer_erreurs' => 'Restart all conversions in error',
+	'info_statut_conversion_en_cours' => 'In progress',
+	'info_statut_conversion_erreur' => 'Error',
+	'info_statut_conversion_non' => 'Awaiting',
+	'info_statut_conversion_oui' => 'Converted',
+	'info_tous_docs_facd' => 'Documents in the queue',
+
+	// L
+	'lien_convertir_document' => 'Convert this document',
+	'lien_recharger' => 'Reload',
+	'liste_attente_aucun' => 'No document in the queue.',
+	'liste_attente_tous' => 'Documents in the queue',
+	'liste_convert_aucun' => 'No converted document',
+	'liste_convert_tous' => 'Conberted documents',
+
+	// T
+	'thead_date' => 'Date',
+	'thead_duree' => 'Duration',
+	'thead_duree_conversion' => 'Conversion duration',
+	'thead_extension' => 'Format',
+	'thead_fonction' => 'Used function',
+	'thead_id' => 'ID',
+	'thead_id_auteur' => 'Author',
+	'thead_id_document' => 'Original document',
+	'thead_nombre' => 'Number',
+	'thead_statut' => 'Status',
+	'titre_page_file' => 'Queue of the document conversion plugin',
+	'titre_page_file_convertis_jour' => 'Conversions by date',
+	'titre_page_file_menu' => 'Conversion queue',
+
+	// V
+	'version_encodee_de' => 'This document is a conversion of the document #@id_orig@'
+);
+
+?>
