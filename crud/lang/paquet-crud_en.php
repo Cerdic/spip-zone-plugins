a1f0b395b0cfb3fa0afb8c79b6f464ea30adc175
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-07-06 01:38:02 +0000

    langues (paquet-crud)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@63311 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ crud/lang/paquet-crud_en.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org/tradlang_module/paquet-crud?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'crud_description' => 'C(r)UD Interface for SPIP that can be used via an action defined by its URL or calling it directly.',
+	'crud_slogan' => 'Interface to create, update and delete an object'
+);
+
+?>
