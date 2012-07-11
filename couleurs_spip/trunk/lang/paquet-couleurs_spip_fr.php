bc73c02851162d2478821f5ca8ff0d32cee02941
chankalan@free.fr <chankalan@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-07-11 07:08:00 +0000

    correction nom de fichier css
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@63388 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- couleurs_spip/trunk/lang/paquet-couleurs_spip_fr.php
+++ couleurs_spip/trunk/lang/paquet-couleurs_spip_fr.php
@@ -8,7 +8,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// C
 	'couleurs_spip_description' => 'Ce plugin permet de modifier l\'apparence de parties de texte. Pour mettre certains mots en couleur ou modifier leur taille ou toute autre caractéristique typographique, il suffit d\'entourer ceux-ci de balises de type <<code>cs_rouge</code>><cs_rouge>mon texte rouge</cs><<code>/cs</code>>. 
-	En ajoutant des styles dans le fichier css/couleur_spip.css - le recopier dans ce cas dans le dossier squelettes utilisé -, on peut ajouter autant de balise qu\'on le désire.',
+	En ajoutant des styles dans le fichier css/couleurs_spip.css - le recopier dans ce cas dans le dossier squelettes utilisé -, on peut ajouter autant de balise qu\'on le désire.',
 	'couleurs_spip_slogan' => 'Des textes en couleur',
 );
 ?>
