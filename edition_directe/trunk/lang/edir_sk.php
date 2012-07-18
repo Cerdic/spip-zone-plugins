9e865247bb97148130e33335e5d35a3028256c6d
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-07-18 01:37:19 +0000

    langues (edir)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@63525 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- edition_directe/trunk/lang/edir_sk.php
+++ edition_directe/trunk/lang/edir_sk.php
@@ -9,13 +9,13 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
 	'activer_edition_directe' => 'Aktivovať priame úpravy pre:',
-	'activer_edition_directe_objet' => 'Activer l\'édition directe pour l\'objet ', # NEW
+	'activer_edition_directe_objet' => 'Aktivovať priame úpravy objektu',
 
 	// D
-	'desactiver_edition_directe_objet' => 'Désactiver l\'édition directe pour l\'objet ', # NEW
+	'desactiver_edition_directe_objet' => 'Deaktivovať priame úpravy objektu',
 
 	// T
-	'titre_plugin' => 'Priama editácia',
+	'titre_plugin' => 'Priame úpravy',
 
 	// V
 	'vider_cache' => 'Prosím, vyprázdnite cache'
