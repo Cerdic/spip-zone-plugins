da3c4c3cf11436354c5df2674545dd4acebe6627
yaquoi@yaquoi.com <yaquoi@yaquoi.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-07-18 23:32:16 +0000

    j y arrive...
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@63550 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ cooliris/V2/lang/paquet-cooliris_fr.php
@@ -0,0 +1,13 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// C
+	'cooliris_description' => 'Ce plugin permet d\'afficher les images d\'un article avec Cooliris, un mur 3D en flash',
+	'cooliris_slogan' => 'Cooliris, un mur d\'images 3D '
+);
+?>
