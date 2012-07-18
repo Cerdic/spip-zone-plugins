8cca844e06cd252513b99db417835688b6ac74a9
yffic@cousumain.info <yffic@cousumain.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-07-18 09:12:45 +0000

    Correction de chaînes de langue BrowserID => Persona + lien wikipedia
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@63532 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- persona/trunk/lang/persona_fr.php
+++ persona/trunk/lang/persona_fr.php
@@ -9,11 +9,11 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	
 	// C
 	'cacher_login_classique' => 'Masquer ?',
-	'configurer_titre' => 'Configurer Browser ID',
+	'configurer_titre' => 'Configurer Persona',
 	
 	// E
-	'explication_cacher_login_classique' => 'En cochant cette case, le formulaire classique de login sera masqué par défaut sur la page de connexion. Seul le bouton "persona" sera donc visible. Sous ce bouton, un lien permet de démasquer le formulaire.',
-	'explication_login' => 'Vous pouvez vous connecter grâce à votre Browser ID (<a href="http://fr.wikipedia.org/wiki/persona" target="_blank">aide</a>)' ,
+	'explication_cacher_login_classique' => 'En cochant cette case, le formulaire classique de login sera masqué par défaut sur la page de connexion. Seul le bouton "Persona" sera donc visible. Sous ce bouton, un lien permet de démasquer le formulaire.',
+	'explication_login' => 'Vous pouvez vous connecter grâce à votre identifiant Mozilla Persona (<a href="http://fr.wikipedia.org/wiki/Mozilla_Persona" target="_blank">aide</a>)' ,
 	
 	// F
 	'form_afficher' => 'Afficher le formulaire de login classique',
