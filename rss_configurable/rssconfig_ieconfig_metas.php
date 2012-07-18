86fc7583df313992c2dc03611899fe23654994cd
yffic@cousumain.info <yffic@cousumain.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-07-18 10:38:08 +0000

    Compatibilité Spip 3
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@63533 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- rss_configurable/rssconfig_ieconfig_metas.php
+++ rss_configurable/rssconfig_ieconfig_metas.php
@@ -2,7 +2,7 @@
 
 function rssconfig_ieconfig_metas($table){
 	$table['rssconfig']['titre'] = _T('rssconfig:rssconfig');
-	$table['rssconfig']['icone'] = 'images/rssconfig-24.png';
+	$table['rssconfig']['icone'] = 'prive/themes/spip/images/rssconfig-24.png';
 	$table['rssconfig']['metas_serialize'] = 'rssconfig';
 	return $table;
 }
