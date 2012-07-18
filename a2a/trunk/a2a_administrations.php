cc05ccf1fc5a9c971e7668d782ccb5bc3cacabd7
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-07-18 20:49:56 +0000

    structure correct des paramètres passés à la fonction maj_plugin
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@63544 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- a2a/trunk/a2a_administrations.php
+++ a2a/trunk/a2a_administrations.php
@@ -9,9 +9,9 @@ function a2a_upgrade($nom_meta_base_version,$version_cible){
 		array('creer_base'),
 	);
 	
-	$maj['0.2.0'] = array('maj_tables',array('spip_articles_lies'));
-	$maj['0.3.0'] = array('sql_alter',array('TABLE spip_articles_lies CHANGE rang rang bigint(21) NOT NULL DEFAULT "0"'));
-	$maj['0.4.0'] = array('maj_tables',array('spip_articles_lies'));
+	$maj['0.2.0'] = array(array('maj_tables',array('spip_articles_lies')));
+	$maj['0.3.0'] = array(array('sql_alter',array('TABLE spip_articles_lies CHANGE rang rang bigint(21) NOT NULL DEFAULT "0"')));
+	$maj['0.4.0'] = array(array('maj_tables',array('spip_articles_lies')));
 	
 	include_spip('base/upgrade');
 	maj_plugin($nom_meta_base_version, $version_cible, $maj);
