4b25f749330a67a131bd0bc584974bd1f39598dc
cyril@cym.fr <cyril@cym.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-07-19 20:33:03 +0000

    on récupère l'objet et son id;
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@63580 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- fil_ariane/trunk/fil_ariane_fonctions.php
+++ fil_ariane/trunk/fil_ariane_fonctions.php
@@ -3,6 +3,9 @@
 /***
  * Plugin Fil d'Ariane pour SPIP
  * Auteur : Cyril Marion, Ateliers CYM
+ *
+ * Tres forte inspiration du site http://programmer.spip.org/
+ * Notamment la rubrique Recuperer-objet-et-id_objet
  */
 
 /***
@@ -11,15 +14,26 @@
  * et construit un fil d'Ariane.
  */
 function balise_FIL_ARIANE_dist($p){
-    $p->code = "construire_FIL_ARIANE()";;
+
+    // L'id de l'objet
+    $_id_objet = $p->boucles[$p->id_boucle]->primary;
+
+    // Le champ (?)
+    $id_objet = champ_sql($_id_objet, $p);
+
+    // L'objet issu du nom de la table
+    $objet = $p->boucles[$p->id_boucle]->id_table;
+
+    $p->code = "construire_FIL_ARIANE('$objet', $id_objet)";
     return $p;
 }
 /***
- * Construit le fil d'ariane
+ * Construit le fil d'Ariane
  */
-function construire_FIL_ARIANE(){
+function construire_FIL_ARIANE($objet, $id_objet){
+    $objet = objet_type($objet);
     $fil_ariane = '<div class="fil_ariane">';
-    $fil_ariane.= '<a href="#">Racine du site</a> &gt; <a href="#">Rubrique</a> &gt; <strong>Hello world !</strong></a>';
+    $fil_ariane.= '<a href="#">Racine du site</a> &gt; <a href="#">Rubrique</a> &gt; <strong>' . $objet . ' ' . $id_objet . '</strong></a>';
     $fil_ariane.= '</div>';
     return $fil_ariane;
 }
