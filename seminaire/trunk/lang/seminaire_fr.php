35ad3d7c816ff6deef59b33c21f33f9186e35dc5
amaury.adon@laposte.net <amaury.adon@laposte.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-07-21 17:14:59 +0000

    oubli des fichiers de langue dans le précédent commit, je pense que ça ne va pas être le seul oubli tel que je suis parti...
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@63596 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- seminaire/trunk/lang/seminaire_fr.php
+++ seminaire/trunk/lang/seminaire_fr.php
@@ -6,7 +6,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// S
-	'seminaire_titre' => 'Séminaire LATP',
+	'seminaire_titre' => 'Séminaire',
 
 	// C
 	'cfg_exemple' => 'Exemple',
