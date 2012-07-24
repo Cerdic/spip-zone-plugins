8942b8c21dd7ef374edd6748708bfe8e40898fa7
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-07-24 16:13:34 +0000

    chaine de langue manquante
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@63684 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- jeux/trunk/lang/jeu_fr.php
+++ jeux/trunk/lang/jeu_fr.php
@@ -7,6 +7,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
     'icone_creer_jeu'          =>'Créer un jeu',
     'icone_modifier_jeu'       =>'Modifier le jeu',
     'info_aucun_jeu'            =>'Aucune jeu',
+    'info_nb_jeux'              =>'@nb@ jeux',
     'label_texte'               =>'Texte',
     'label_titre'               =>'Titre',
     'texte_changer_statut_jeu'  =>'Le statut de ce jeu est :',
