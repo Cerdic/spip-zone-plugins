d9dc516d923b5df58aca72305c1d013f6e837c9b
jp.GUIHARD@ahbretagne.com <jp.GUIHARD@ahbretagne.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-07-24 10:22:27 +0000

    Prise en compte d'un retour qui ne serait pas un array lorsque les stats sont vides.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@63667 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- apropos_stats/Trunk/apropos_stats_fonctions.php
+++ apropos_stats/Trunk/apropos_stats_fonctions.php
@@ -2,6 +2,7 @@
 /**
  * Plugin À propos statistiques
  * (c) 2012 Jean-Philippe Guihard
+ * Tout ceci vient du plugin SVP Statistiques d'Eric Lupinacci
  */
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
@@ -26,8 +27,13 @@ function calcul_info_apropos_stats($params){
 
 	//je vais lire la table spip_plugins, la colone nbr_sites de la ligne prefixe=ce que j'ai passé en paramètre
 	
-	$nombreDeSites = implode(sql_fetsel("nbr_sites", "spip_plugins", "prefixe=".sql_quote($params)));
-	return $nombreDeSites ;
-
+	$nombreDeSites = sql_fetsel("nbr_sites", "spip_plugins", "prefixe=".sql_quote($params));
+	
+	// je vérifie que je recupere bien un array car il peut y avoir un retour vide si le plugin n'est pas présent dans la base de la zone
+	
+	if ( is_array( $nombreDeSites ) ) {
+	$result = implode('',$nombreDeSites) ;
+	return $result ;
+	}
 }
 ?>
