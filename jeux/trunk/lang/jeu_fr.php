f81ae84cf774e30377ca0a2233dd49a0e992ef6f
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-07-24 13:22:56 +0000

    chaine de langues pour les pages construites automatiquement par spip
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@63670 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ jeux/trunk/lang/jeu_fr.php
@@ -0,0 +1,14 @@
+<?php
+/** Fichier de langue de SPIP **/
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+    'info_1_jeu'               =>'Liste des jeux',
+    'icone_creer_jeu'          =>'Créer un jeu',
+    'icone_modifier_jeu'       =>'Modifier le jeu',
+    'info_aucun_jeu'          =>'Aucune jeu',
+    'titre_jeux'               =>'Jeux',
+    'titre_logo_jeu'           =>'Logo du jeu'
+);
+
+?>
