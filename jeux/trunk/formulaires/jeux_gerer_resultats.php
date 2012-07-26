32e961df58f8b140fd55733795b92702c162d62d
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-07-26 17:29:54 +0000

    début formulaire de suppreession resultats / auteurs
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@64045 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ jeux/trunk/formulaires/jeux_gerer_resultats.php
@@ -0,0 +1,9 @@
+<?php
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+function formulaires_jeux_gerer_resultats_charger($param=array()){
+   
+    return $param;
+}
+?>
---
+++ jeux/trunk/lang/jeux_gerer_resultats_fr.php
@@ -0,0 +1,15 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+'compacter'     =>'Compacter les résultats',
+'pour_auteur'   =>'Pour cet auteur :',
+'pour_jeu'      =>'Pour ce jeu :',
+'pour_tous'     =>'Pour tout les résultats :',
+'supprimer'     =>'Supprimer les résultats'
+);
+
+
+?>
