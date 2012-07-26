9eb80fbe2807f4a1cf416bfc30d90ee8e133ec21
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-07-26 10:57:00 +0000

    langues (simplecart)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@63951 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- simplecart/lang/simplecart_fr.php
+++ simplecart/lang/simplecart_fr.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// extrait automatiquement de http://trad.spip.org/tradlang_module/simplecart?lang_cible=fr
+// extrait automatiquement de http://trad.spip.net/tradlang_module/simplecart?lang_cible=fr
 // ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
@@ -23,7 +23,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'cart_headers_explication' => 'Editez les champs du panier. Voir <a href="http://simplecartjs.com/documentation.html">Cart formatting configuration page</a> pour détails',
 	'checkout' => 'Finaliser l\'achat',
 	'checkout2email' => 'Envoyer une commande par email',
-	'checkout2email_explication' => 'La commande est envoyée à l\' adresse suivante. On n\'utilise pas de moyen de paiement.', # MODIF
+	'checkout2email_explication' => 'La commande est envoyée à l\'adresse suivante. On n\'utilise pas de moyen de paiement.',
 	'checkout_methods' => 'Moyens de paiement',
 	'checkout_to' => 'Payé par',
 
@@ -31,38 +31,38 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'description' => 'Un panier d\'achat simple en javascript',
 	'devise' => 'Devise',
 	'devise_choix' => 'Choix de la devise',
-	'devise_explication' => 'La devise qui sera utilisée pour le paiement (Vérifiez avant la compatibilité avec le moyen de paiement)', # MODIF
+	'devise_explication' => 'La devise qui sera utilisée pour le paiement (Vérifiez avant la compatibilité avec le moyen de paiement)',
 	'dineromail_country' => 'Pays',
 	'dineromail_country_argentine' => 'Argentine',
 	'dineromail_country_brazil' => 'Bresil',
 	'dineromail_country_chile' => 'Chili',
-	'dineromail_country_explication' => 'Choisir le pays ou à été enregistré le compte DineroMail.', # MODIF
+	'dineromail_country_explication' => 'Choisir le pays où a été enregistré le compte DineroMail.',
 	'dineromail_country_mexico' => 'Mexique',
 	'dineromail_currency' => 'Monnaie de la transaction',
-	'dineromail_currency_explication' => 'Type de la monnaie de la transaction. Il n\'y a pas de conversion de ce type. Les prix doivent être indiqués dans la monnaie sélectionnée', # MODIF
+	'dineromail_currency_explication' => 'Type de monnaie de la transaction. Il n\'y a pas de conversion de ce type. Les prix doivent être indiqués dans la monnaie sélectionnée',
 	'dineromail_currency_local' => 'Monnaie locale',
 	'dineromail_currency_usd' => 'Dollars américains',
 	'dineromail_header_image' => 'Image de l\'en-tête',
-	'dineromail_header_image_explication' => 'URL absolue du logo à montrer dans l\'en-tête de DineroMail (jpg o gif, 150px x 50px)', # MODIF
+	'dineromail_header_image_explication' => 'URL absolue du logo à montrer dans l\'en-tête de DineroMail (jpg ou gif, 150px x 50px)',
 	'dineromail_merchant_id' => 'Numéro de compte',
-	'dineromail_merchant_id_explication' => 'Numéro d\'identification de votre compte DineroMail sans l\'indicateur digital ', # MODIF
+	'dineromail_merchant_id_explication' => 'Numéro d\'identification de votre compte DineroMail sans l\'indicateur digital ',
 	'dineromail_payments_methods' => 'Moyen de paiement DineroMail',
-	'dineromail_payments_methods_explication' => 'Chaine de texte qui définit les moyens de paiement autorisés. Laisser en blanc pour toutes les disponibilités dans le pays.', # MODIF
+	'dineromail_payments_methods_explication' => 'Chaine de texte qui définit les moyens de paiement autorisés. Laisser vide pour permettre tous ceux disponibles dans le pays.',
 	'dineromail_see' => 'Voir',
 	'dineromail_seller_name' => 'Nom du vendeur',
-	'dineromail_seller_name_explication' => 'Légende que le vendeur vut montrer dans l\'en-tête', # MODIF
+	'dineromail_seller_name_explication' => 'Légende que le vendeur veut afficher dans l\'en-tête. Laisser vide pour afficher l\'adresse email associé au compte.',
 
 	// E
 	'empty' => 'Vider',
 	'error_url' => 'URL achat erronné',
-	'error_url_explication' => 'URL où se redirige l\'acheteur en cas de transaction erronnée', # MODIF
+	'error_url_explication' => 'URL où se redirige l\'acheteur en cas de transaction erronnée',
 
 	// F
 	'final_total' => 'Total',
 
 	// G
 	'google_merchant_id' => 'Id de l\'identification du marchand',
-	'google_merchant_id_explication' => 'Numéro d\'identification de votre compte marchand Google', # MODIF
+	'google_merchant_id_explication' => 'Numéro d\'identification de votre compte marchand Google',
 
 	// H
 	'header_name' => 'Nom',
@@ -71,9 +71,9 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'header_total' => 'Total',
 
 	// O
-	'ok_url' => 'URL achat réussi', # MODIF
-	'ok_url_explication' => 'URL où sera redirigé l\'acheteur en cas de transaction réussie.', # MODIF
-	'other_parameters' => 'Autres paramètres', # MODIF
+	'ok_url' => 'URL lors d\'un achat réussi',
+	'ok_url_explication' => 'URL où sera redirigé l\'acheteur en cas de transaction réussie.',
+	'other_parameters' => 'Autres paramètres',
 
 	// P
 	'paypal_account' => 'Compte PayPal',
@@ -84,11 +84,11 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// S
 	'shipping_cost' => 'Coût d\'envoi',
 	'shipping_flat_rate' => 'Taxe fixe de l\'envoi',
-	'shipping_flat_rate_explication' => 'Ajouter une taxe fixe à l\'ordre complet', # MODIF
-	'shipping_quantity_rate' => 'Coût de l\'envoi par quantité', # MODIF
+	'shipping_flat_rate_explication' => 'Ajouter une taxe fixe à l\'ordre complet',
+	'shipping_quantity_rate' => 'Coût de l\'envoi par quantité',
 	'shipping_quantity_rate_explication' => 'Ajouter un montant fixe pour chaque item',
-	'shipping_total_rate' => 'Coût d\'envoi en pourcentage de la somme totale', # MODIF
-	'shipping_total_rate_explication' => 'Ajouter un coût d\'envoi proportionnel au coût de la commande', # MODIF
+	'shipping_total_rate' => 'Frais d\'envoi en pourcentage de la somme totale',
+	'shipping_total_rate_explication' => 'Ajouter un coût d\'envoi proportionnel au coût de la commande',
 	'subtotal' => 'Sous-total',
 
 	// T
