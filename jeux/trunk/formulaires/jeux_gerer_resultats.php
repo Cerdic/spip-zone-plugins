ff384676dbf0ac150f672f17ac0f8193a6ca4154
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-07-26 17:29:58 +0000

    pour les saisies, les faires en PHP, pour profiter du branchement avec CVT + styles + ne pas demander de confirmer tant que choix pas fait
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@64048 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- jeux/trunk/formulaires/jeux_gerer_resultats.php
+++ jeux/trunk/formulaires/jeux_gerer_resultats.php
@@ -2,17 +2,44 @@
 
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
+function formulaires_jeux_gerer_resultats_saisies($param=array()){
+    if($param['id_auteur']){
+        $label_faire = _T('jeux_gerer_resultats:pour_auteur');   
+    }
+    elseif($param['id_jeu']){
+        $label_faire = _T('jeux_gerer_resultats:pour_jeu');   
+    }
+    else{
+        $label_faire = _T('jeux_gerer_resultats:pour_tous');   
+    }
+    $saisies = array(
+        array('saisie'=>'radio',
+                'options'=>array(
+                    'nom'=>'faire',
+                    'obligatoire'=>'oui',
+                    'datas'=>array(
+                        'compacter'=>_T('jeux_gerer_resultats:compacter'),
+                        'supprimer'=>_T('jeux_gerer_resultats:supprimer')
+                    ),
+                    'label'=>$label_faire
+                )
+            ));
+    return $saisies;
+}
+
 function formulaires_jeux_gerer_resultats_charger($param=array()){
+    $param['saisies'] = formulaires_jeux_gerer_resultats_saisies($param);
     return $param;
 }
 function formulaires_jeux_gerer_resultats_verifier($param=array()){
     $erreurs = array();
-    if (!_request('confirmer')){
+    if (!_request('confirmer') and _request('faire')){
         $erreurs['non_confirme']=true;
     }
     return $erreurs;
 }
 function formulaires_jeux_gerer_resultats_traiter($param=array()){
+    
     return $param;
 }
 ?>
