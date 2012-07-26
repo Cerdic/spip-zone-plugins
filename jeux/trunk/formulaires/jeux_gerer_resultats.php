e5eafdaa8879c13425354079fed8dd6d36482f57
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-07-26 17:29:57 +0000

    demander à confirmer le choix
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@64047 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- jeux/trunk/formulaires/jeux_gerer_resultats.php
+++ jeux/trunk/formulaires/jeux_gerer_resultats.php
@@ -3,7 +3,16 @@
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 function formulaires_jeux_gerer_resultats_charger($param=array()){
-   
+    return $param;
+}
+function formulaires_jeux_gerer_resultats_verifier($param=array()){
+    $erreurs = array();
+    if (!_request('confirmer')){
+        $erreurs['non_confirme']=true;
+    }
+    return $erreurs;
+}
+function formulaires_jeux_gerer_resultats_traiter($param=array()){
     return $param;
 }
 ?>
--- jeux/trunk/lang/jeux_gerer_resultats_fr.php
+++ jeux/trunk/lang/jeux_gerer_resultats_fr.php
@@ -3,13 +3,11 @@
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-
 'compacter'     =>'Compacter les résultats',
+'confirmer_choix'=>'Il vous faut confirmer votre choix',
 'pour_auteur'   =>'Pour cet auteur :',
 'pour_jeu'      =>'Pour ce jeu :',
 'pour_tous'     =>'Pour tout les résultats :',
 'supprimer'     =>'Supprimer les résultats'
 );
-
-
 ?>
