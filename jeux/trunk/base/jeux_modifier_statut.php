bfea492a93d7e754084762c660d64e901731705b
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-07-27 11:49:30 +0000

    code mort
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@64103 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- jeux/trunk/base/jeux_modifier_statut.php
+++
@@ -1,7 +0,0 @@
-<?php
-include_spip('inc/utils');
-function jeu_modifier_statut($id,$statut){
-    sql_updateq("spip_jeux", array('statut'=>$statut), "id_jeu=$id");
-	spip_log("modifier statut jeu$id, $statut",'jeux');
-}
-?>
