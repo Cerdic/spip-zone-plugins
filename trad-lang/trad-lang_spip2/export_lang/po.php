12212ea5b5c78249b4eb4e26089fbb2bdfb26651
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-07-27 14:56:20 +0000

    Première tentative pour jouer avec les .po, on exporte nos fichiers de langue SPIP dans ce format...
    Il faut maintenant les tester et savoir les réimporter
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@64117 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ trad-lang/trad-lang_spip2/export_lang/po.php
@@ -0,0 +1,72 @@
+<?php
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+function export_lang_po_dist($module,$langue,$dir_lang){
+	/**
+	 * Le fichier final
+	 * local/cache-lang/module_lang.php
+	 */
+	$fic_exp = $dir_lang."/".$module."_".$langue.".po";
+
+	$tab = "\t";
+
+	$res=sql_select("id,str,comm,statut","spip_tradlangs","module=".sql_quote($module)." AND lang=".sql_quote($langue),"id");
+	$x=array();
+	$prev="";
+	$tous = $lorigine; // on part de l'origine comme ca on a tout meme si c'est pas dans la base de donnees (import de salvatore/lecteur.php)
+	while ($row=sql_fetch($res)) {
+		$tous[$row['id']] = $row;
+	}
+	ksort($tous);
+	foreach ($tous as $row) {
+		if ($prev!=strtoupper($row['id'][0])) $x[] = "\n# ".strtoupper($row['id'][0]);
+		$prev=strtoupper($row['id'][0]);
+		if (strlen($row['statut']) && ($row['statut'] != 'OK'))
+			$row['comm'] .= ' '.$row['statut'];
+		if (trim($row['comm'])) $row['comm']=" # ".trim($row['comm']); // on rajoute les commentaires ?
+
+		$str = $row['str'];
+
+		$oldmd5 = md5($str);
+		//$str = unicode_to_utf_8(html_entity_decode($str, ENT_NOQUOTES, 'utf-8'));
+		$str = unicode_to_utf_8(
+			html_entity_decode(
+				preg_replace('/&([lg]t;)/S', '&amp;\1', $str),
+				ENT_NOQUOTES, 'utf-8')
+		);
+		$newmd5 = md5($str);
+		if ($oldmd5 !== $newmd5) sql_updateq("spip_tradlangs",array('md5'=>$newmd5), "md5=".sql_quote($oldmd5)." AND module=".sql_quote($module));
+
+		$x[]=($row['comm'] ? "#".$row['comm']."\n" : "").
+"msgid \"".$row['id']."\"
+msgstr \"".str_replace('"','\"',$str)."\"\n";
+	}
+
+	// historiquement les fichiers de lang de spip_loader ne peuvent pas etre securises
+	$secure = ($module == 'tradloader')
+		? ''
+		: "if (!defined('_ECRIRE_INC_VERSION')) return;\n\n";
+
+	$fd = fopen($fic_exp, 'w');
+
+	# supprimer la virgule du dernier item
+	$x[count($x)-1] = preg_replace('/,([^,]*)$/', '\1', $x[count($x)-1]);
+
+	$contenu = join("\n",$x);
+	
+	# ecrire le fichier
+	fwrite($fd,
+	'msgid ""
+msgstr ""
+
+# This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+# extrait automatiquement de '.$url_trad.'
+'
+. str_replace("\r\n", "\n", $contenu)
+	);
+	fclose($fd);
+	@chmod($fic_exp, 0666);
+	return $fic_exp;
+}
+?>
--- trad-lang/trad-lang_spip2/lang/tradlang_fr.php
+++ trad-lang/trad-lang_spip2/lang/tradlang_fr.php
@@ -11,6 +11,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// B
 	'bouton_activer_lang' => 'Activer la langue "@lang@" pour ce module',
 	'bouton_exporter_fichier' => 'Exporter le fichier',
+	'bouton_exporter_fichier_po' => 'Exporter le fichier en .po',
 	'bouton_exporter_fichier_langue' => 'Exporter le fichier de langue en "@lang@"',
 	'bouton_exporter_fichier_langue_original' => 'Exporter le fichier de langue original ("@lang_mere@")',
 	'bouton_exporter_fichier_zip' => 'Exporter les fichiers en zip',
