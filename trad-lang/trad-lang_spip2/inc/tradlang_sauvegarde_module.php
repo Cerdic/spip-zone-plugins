87f95870dbfb897f20aecc248dfbdb4df4141be2
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-07-27 14:09:52 +0000

    On change un peu de méthode pour préparer la possibilité d'avoir plusieurs types de fichiers à exporter...
    On met les types d'export dans export_lang/type.php appelé par charger_fonction en fonction des cas
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@64115 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- trad-lang/trad-lang_spip2/inc/tradlang_sauvegarde_module.php
+++ trad-lang/trad-lang_spip2/inc/tradlang_sauvegarde_module.php
@@ -17,7 +17,7 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
  * @param string $dir_lang le répertoire de stockage des fichiers de langue
  * @return string $fic_exp le chemin complet du fichier de langue
  */
-function inc_tradlang_sauvegarde_module_dist($module,$langue,$dir_lang=false){
+function inc_tradlang_sauvegarde_module_dist($module,$langue,$dir_lang=false,$type=false){
 	include_spip('inc/flock');
 	include_spip('inc/filtres'); # Pour url_absolue
 
@@ -34,81 +34,21 @@ function inc_tradlang_sauvegarde_module_dist($module,$langue,$dir_lang=false){
 		}
 	}
 	
-	$id_tradlang_module = sql_getfetsel('id_tradlang_module','spip_tradlang_modules','module='.sql_quote($module));
+	$tradlang_module = sql_fetsel('id_tradlang_module,type_export','spip_tradlang_modules','module='.sql_quote($module));
 	
 	/**
 	 * L'URL du site de traduction
 	 */
-	$url_trad = parametre_url(url_absolue(generer_url_entite($id_tradlang_module,'tradlang_module')),'lang_cible',$langue);
+	$url_trad = parametre_url(url_absolue(generer_url_entite($tradlang_module['id_tradlang_module'],'tradlang_module')),'lang_cible',$langue);
 	
-	/**
-	 * Le fichier final
-	 * local/cache-lang/module_lang.php
-	 */
-	$fic_exp = $dir_lang."/".$module."_".$langue.".php";
-
-	$tab = "\t";
-
-	$res=sql_select("id,str,comm,statut","spip_tradlangs","module=".sql_quote($module)." AND lang=".sql_quote($langue),"id");
-	$x=array();
-	$prev="";
-	$tous = $lorigine; // on part de l'origine comme ca on a tout meme si c'est pas dans la base de donnees (import de salvatore/lecteur.php)
-	while ($row=sql_fetch($res)) {
-		$tous[$row['id']] = $row;
-	}
-	ksort($tous);
-	foreach ($tous as $row) {
-		if ($prev!=strtoupper($row['id'][0])) $x[] = "\n$tab// ".strtoupper($row['id'][0]);
-		$prev=strtoupper($row['id'][0]);
-		if (strlen($row['statut']) && ($row['statut'] != 'OK'))
-			$row['comm'] .= ' '.$row['statut'];
-		if (trim($row['comm'])) $row['comm']=" # ".trim($row['comm']); // on rajoute les commentaires ?
-
-		$str = $row['str'];
-
-		$oldmd5 = md5($str);
-		//$str = unicode_to_utf_8(html_entity_decode($str, ENT_NOQUOTES, 'utf-8'));
-		$str = unicode_to_utf_8(
-			html_entity_decode(
-				preg_replace('/&([lg]t;)/S', '&amp;\1', $str),
-				ENT_NOQUOTES, 'utf-8')
-		);
-		$newmd5 = md5($str);
-		if ($oldmd5 !== $newmd5) sql_updateq("spip_tradlangs",array('md5'=>$newmd5), "md5=".sql_quote($oldmd5)." AND module=".sql_quote($module));
-
-		$x[]="$tab".var_export($row['id'],1).' => ' .var_export($str,1).','.$row['comm'] ;
-	}
-
-	// historiquement les fichiers de lang de spip_loader ne peuvent pas etre securises
-	$secure = ($module == 'tradloader')
-		? ''
-		: "if (!defined('_ECRIRE_INC_VERSION')) return;\n\n";
-
-	$fd = fopen($fic_exp, 'w');
-
-	# supprimer la virgule du dernier item
-	$x[count($x)-1] = preg_replace('/,([^,]*)$/', '\1', $x[count($x)-1]);
-
-	$contenu = join("\n",$x);
+	if(!$type)
+		$type = $tradlang_module['type_export'];
 	
-	# ecrire le fichier
-	fwrite($fd,
-	'<'.'?php
-// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// extrait automatiquement de '.$url_trad.'
-// ** ne pas modifier le fichier **
-'
-."\n".$secure.'$GLOBALS[$GLOBALS[\'idx_lang\']] = array(
-'
-. str_replace("\r\n", "\n", $contenu)
-.'
-);
-
-?'.'>'
-	);
-	fclose($fd);
-	@chmod($fic_exp, 0666);
+	if(!$f = charger_fonction($type,'export_lang'))
+		$f = charger_fonction('spip','export_lang');
+	
+	$fichier = $f($module,$langue,$dir_lang);
   
-	return $fic_exp;
+	return $fichier;
 }
 ?>
