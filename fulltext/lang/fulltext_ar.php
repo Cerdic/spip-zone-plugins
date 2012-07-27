abf488b387c2ddb083d7a6e9507703e98e451272
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-07-27 01:39:06 +0000

    langues (fulltext)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@64076 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- fulltext/lang/fulltext_ar.php
+++ fulltext/lang/fulltext_ar.php
@@ -9,7 +9,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
 	'accents_pas_pris' => 'المحرك يتجاهل العلامات النطقية (فإدخال «déjà» او «deja» يعطي «déjà» و«dejà» و«déja»...)',
-	'activer_indexation' => 'تفعيل فهرسة', # MODIF
+	'activer_indexation' => 'تفعيل فهرسة ملفات @ext@',
 	'asie' => 'آسيا',
 	'asterisque_terminale' => 'لا يعطي شيئاً: فالنجمة يجب ان تكون في الآخر',
 	'aussi' => 'أيضاً',
@@ -17,7 +17,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// C
 	'casse_indifferente' => 'لا يتأثر المحرك بحالة الأحرف ان كانت كبيرة او صغيرة.',
 	'configuration_indexation_document' => 'إعداد فهرسة المستندات',
-	'configurer_egalement_doc' => 'يمكن أيضاً إعداد فهرسة المستندات:', # MODIF
+	'configurer_egalement_doc' => 'يمكن أيضاً إعداد فهرسة المستندات:',
 	'convertir_myisam' => 'تحويل الى MyISAM',
 	'convertir_toutes' => 'تحويل كل الجداول الى  MyISAM',
 	'convertir_utf8' => 'تحويل الى UTF-8 لاسترجاع الاتساق',
@@ -26,7 +26,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// D
 	'des_utilisations' => '@nb@ استخدام',
 	'descriptif' => 'الوصف',
-	'documents_proteges' => 'مستندات محمية', # MODIF
+	'documents_proteges' => 'مستندات محمية',
 
 	// E
 	'enfan' => 'ولد',
@@ -36,14 +36,14 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'enfantillage' => 'ولدن',
 	'enfants' => 'ولد',
 	'erreur_binaire_indisponible' => 'Ce logiciel n\'est pas disponible sur le serveur.', # NEW
-	'erreur_doc_bin' => 'يجب تحديد التطبيق المستخدم لاسترجاع ملفات doc.', # MODIF
-	'erreur_intervalle_cron' => 'يجب تحديد مدة تفوق الثانية', # MODIF
-	'erreur_nb_docs' => 'يجب تحديد عدد مستندات للمعالجة يفوق المستند الواحد.', # MODIF
-	'erreur_pdf_bin' => 'يجب تحديد التطبيق المستخدم لاسترجاع ملفات pdf.', # MODIF
-	'erreur_ppt_bin' => 'يجب تحديد التطبيق المستخدم لاسترجاع ملفات ppt.', # MODIF
-	'erreur_taille_index' => 'يجب فهرسة حرف واحد على الأقل.', # MODIF
+	'erreur_doc_bin' => 'يجب تحديد التطبيق المستخدم لاسترجاع ملفات doc.',
+	'erreur_intervalle_cron' => 'يجب تحديد مدة تفوق الثانية',
+	'erreur_nb_docs' => 'يجب تحديد عدد مستندات للمعالجة في كل تمريرة.',
+	'erreur_pdf_bin' => 'يجب تحديد التطبيق المستخدم لاسترجاع ملفات pdf.',
+	'erreur_ppt_bin' => 'يجب تحديد التطبيق المستخدم لاسترجاع ملفات ppt.',
+	'erreur_taille_index' => 'يجب فهرسة حرف واحد على الأقل.',
 	'erreur_verifier_configuration' => 'Il y a des erreurs de configuration.', # NEW
-	'erreur_xls_bin' => 'يجب تحديد التطبيق المستخدم لاسترجاع ملفات xls.', # MODIF
+	'erreur_xls_bin' => 'يجب تحديد التطبيق المستخدم لاسترجاع ملفات xls.',
 	'et' => '<strong>و</strong>',
 	'etranger' => 'غريب',
 	'exemples' => 'أمثلة على الاستخدام',
@@ -74,37 +74,37 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'indiquer_options_bin' => 'تحديد خيارات فهرسة',
 	'infos' => 'المعلومات',
 	'infos_documents_proteges' => 'ستجد هنا قائمة بالمستندات المحمية والتي لم تتم فهرستها',
-	'infos_fulltext_document' => 'يمكن من هنا اختيار اي نوع من المستندات يخضع لفهرسة الملحق وإعداد التطبيقات المستخدمة وخياراتها.', # MODIF
+	'infos_fulltext_document' => 'يمكن من هنا اختيار اي نوع من المستندات يخضع لفهرسة الملحق وإعداد التطبيقات المستخدمة وخياراتها.',
 	'intervalle_cron' => 'Intervalle de temps entre deux passages du CRON (en secondes).', # NEW
 
 	// L
 	'liste_tables_connues' => 'هذه هي قائمة الجداول التي تعرّف عليها محرك البحث. يمكنك إضافة عناصر FULLTEXT، انظر التوثيق على العنوان',
-	'logo' => 'Logo', # NEW
+	'logo' => 'الشعار',
 
 	// M
 	'mais_pas' => 'ولكن <strong>ليس</strong>',
-	'message_ok_configuration' => 'تم تسجيل تفضيلاتك', # MODIF
-	'message_ok_update_configuration' => 'تم تحديث تفضيلاتك', # MODIF
+	'message_ok_configuration' => 'تم تسجيل تفضيلاتك',
+	'message_ok_update_configuration' => 'تم تحديث تفضيلاتك',
 
 	// N
 	'nb_err' => 'En erreur d\'indexation', # NEW
-	'nb_index' => 'Indexés', # MODIF
-	'nb_non_index' => 'Non-indexés', # MODIF
-	'nb_ptg' => 'Protégés (non-indexés)', # MODIF
-	'necessite_version_php' => '(يتطلب PHP 5.2 الأقل وخيار -enable-zip)', # MODIF
-	'nombre_caracteres' => 'عدد الحروف المفهرسة (من بداية المستند).', # MODIF
+	'nb_index' => 'مفهرسة',
+	'nb_non_index' => 'غير مفهرسة',
+	'nb_ptg' => 'محمية (غير مفهرشة)',
+	'necessite_version_php' => '(يتطلب PHP 5.2 الأقل وخيار -enable-zip)',
+	'nombre_caracteres' => 'عدد الحروف المفهرسة (من بداية المستند).',
 	'nombre_documents' => 'Nombre de documents à traiter par itération du CRON', # MODIF
 
 	// O
 	'ou_bien' => 'أو',
 
 	// P
-	'pas_document_ptg' => 'Il n\'y a pas de document protégé.', # NEW
+	'pas_document_ptg' => 'لا توجد مستندات محمية.',
 	'pas_index' => 'لا يوجد فهارس FULLTEXT',
 	'premier_soit' => '<strong>إما</strong>',
 
 	// Q
-	'que_des_exemples' => 'ملاحظة: عناوين التطبيقات المقترحة هنا هي أمثلة.', # MODIF
+	'que_des_exemples' => 'ملاحظة: عناوين التطبيقات والخيارات المقترحة هنا هي أمثلة.',
 
 	// R
 	'regenerer_tous' => 'إعادة إنتاج كل فهارس FULLTEXT',
@@ -112,7 +112,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'reinitialise_index_ptg' => 'Réinitialiser l\'indexation des documents protégés', # NEW
 	'reinitialise_totalement_doc' => 'إعادة تأصيل فهرسة كل المستندات',
 	'reserve_webmestres' => 'صفحة محصورة بمسؤول الموقع',
-	'retour_configuration_fulltext' => 'العودة الى إعداد الملحق', # MODIF
+	'retour_configuration_fulltext' => 'العودة الى إعداد الملحق',
 	'retourne' => 'يسترجع النصوص التي تحتوي ',
 
 	// S
@@ -133,7 +133,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'utiliser_operateurs_logiques' => 'يستخدم البحث العوامل المنطقية الشائعة.',
 
 	// V
-	'voir_doc_ptg' => 'Voir les documents protegés' # NEW
+	'voir_doc_ptg' => 'عرض المستنرات المحمية'
 );
 
 ?>
