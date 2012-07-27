0540ce2d51509f533d0c5b179a213f4f7baebd9d
marcimat@rezo.net <marcimat@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-07-27 18:00:45 +0000

    On sort chosen dans un plugin externe...
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@64130 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- roles/trunk/roles_pipelines.php
+++ roles/trunk/roles_pipelines.php
@@ -10,7 +10,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 
 
 /**
- * Ajoute Chosen & bootstrap (minimal) aux plugins chargés
+ * Ajoute Bootstrap (minimal) aux plugins chargés
  * 
  * @param array $flux
  *     Liste des js chargés
@@ -18,23 +18,18 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
  *     Liste complétée des js chargés
 **/
 function roles_jquery_plugins($flux) {
-	$flux[] = 'lib/chosen/chosen.jquery.js';
 	$flux[] = 'javascript/bootstrap-dropdown.js';
-	$flux[] = 'javascript/roles.js';
 	return $flux;
 }
 
 
 /**
- * Ajoute Chosen & bootstrap (minimal) aux css chargées
+ * Ajoute Bootstrap (minimal) aux css chargées
  * 
  * @param string $texte Contenu du head HTML concernant les CSS
  * @return string       Contenu du head HTML concernant les CSS
 **/
 function roles_header_prive_css($texte) {
-
-	$css = find_in_path('lib/chosen/chosen.css');
-	$texte .= "<link rel='stylesheet' type='text/css' media='all' href='$css' />\n";
 
 	$css = find_in_path('css/bootstrap-button-dropdown.css');
 	$texte .= "<link rel='stylesheet' type='text/css' media='all' href='$css' />\n";
