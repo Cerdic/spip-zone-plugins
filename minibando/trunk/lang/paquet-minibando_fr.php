09e7868ae54bbbbbf8234e5c9bc5a6b6394f8009
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-07-28 01:41:06 +0000

    langues (paquet-minibando)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@64164 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- minibando/trunk/lang/paquet-minibando_fr.php
+++ minibando/trunk/lang/paquet-minibando_fr.php
@@ -1,19 +1,13 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-minibando
-// Langue: fr
-// Date: 24-01-2012 19:21:48
-// Items: 2
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/minibando/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// M
+	// M
 	'minibando_description' => 'Le plugin minibando remplace les boutons d’administration de SPIP par une barre d\'outils qui reprend les éléments disponibles dans la navigation haute de l’espace privé de SPIP.',
-	'minibando_slogan' => 'Un minibando pour un maximum de fonctionnalités !',
+	'minibando_slogan' => 'Un minibando pour un maximum de fonctionnalités !'
 );
+
 ?>
