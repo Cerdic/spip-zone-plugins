e21823f2e400d45d0615605776af98c91b704683
marcimat@rezo.net <marcimat@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-07-30 17:37:49 +0000

    Traîtrise de taille sur les tableaux qui ont des Chosen. Lorsque la taille des colonne d'un tableau est variable (c'est à dire… par défaut), la largeur de la colonne fluctue en fonction du nombre de sélecteurs Chosen présents dedans, et comme le JS Chosen leur attribue une taille fixe en fonction de la taille dont ils disposent, bien les Chosen avaient rarement la même taille puisque la colonne bougeait avec.
    La correction précédente n'était pas suffisante car elle avait tendance à attribuer par défaut trop de largeur à Chosen au détriment des autres colonnes. On change de stratégie en utilisant par défaut pour les chosen d'une colonne la taille du premier chosen de la colonne. Ça devrait être déjà bien mieux.
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@64223 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- chosen/javascript/spip_chosen.js
+++ chosen/javascript/spip_chosen.js
@@ -1,29 +1,46 @@
 ;(function ($) {
 jQuery(document).ready(function(){
 
-	/* Remet la bonne taille aux select chosen présents sur la page */
-	/* Remet le title sur le chosen s'il en existait un sur le select */
-	spip_chosen_width_and_title = function() {
-		$('.chzn-container').each(function (index) {
-			/*
-			 * Remettre la bonne taille des éléments qui change avec la
-			 * présence de chosen du fait du changement de la largeur de colonne
-			 * lorsqu'on est dans un tableau.
-			 * Il faut le repasser visible pour avoir une taille correcte !
-			 */
-			select = $(this).prev().show(); 
-			width = select.width();
-			select.hide();
-			$(this).css('width', width).find("> .chzn-drop").css('width', width - 2);
+	/* Remet le title sur les chosen s'il en existait un sur le select */
+	spip_chosen_title = function() {
+		$('.chzn-container').each(function () {
 			/* Si un title était sur le sélect d'origine, le remettre sur le html de Chosen */
-			if (title = select.attr('title')) { $(this).attr('title', title); }
+			if (title = $(this).prev().attr('title')) {
+				$(this).attr('title', title);
+			}
 		});
+	}
+
+	/* Homogénéise la taille de select chosen présents à l'intérieur d'un tableau.
+	 *
+	 * Lorsqu'on place des sélecteurs dans une colonne de tableau, la largeur
+	 * de la colonne fluctue en fonction du contenu qu'elle possède et
+	 * celle-ci s'agrandit au fur et à mesure que les Chosen se chargent.
+	 * Chaque Chosen n'a du coup plus forcément la même taille que son voisin
+	 * (sa taille étant définie - en pixels - avec la taille du conteneur)
+	 *
+	 * Du coup, on regarde le premier Chosen de chaque colonne d'un tableau
+	 * on applique sa taille aux autres chosen de cette colonne.
+	 */
+	spip_chosen_table_width = function() {
+		$('table').has('.chzn-container').each(function() {
+			$taille = []; // la taille des premiers chosen pour chaque colonne
+			$(this).find('.chzn-container').each(function () {
+				index = $(this).parent('td').index();
+				if (!$taille[index]) {
+					$taille[index] = $(this).width() + 10; // un tout petit rien de plus en général
+				}
+				$(this).css('width', $taille[index]).find("> .chzn-drop").css('width', $taille[index] - 2);
+			});
+		});
+		
 	}
 
 	/* lance Chosen sur les .chosen */
 	spip_chosen = function() {
 		$("select.chosen").chosen();
-		spip_chosen_width_and_title();
+		spip_chosen_title();
+		spip_chosen_table_width();
 	}
 
 	spip_chosen();
