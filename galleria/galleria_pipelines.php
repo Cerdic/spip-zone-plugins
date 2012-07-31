4ad213e5c0616b1e16381af12c875f0b92b92a45
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-07-31 08:46:23 +0000

    Renommage du fichier head.php au nom trop generique intervalle de compatibilite reel Renommage des fichiers js en galleria.js sans numero de version pour permettre l'upgrade en avec historique des modifs (le numero de version est dans le script) Dans le modele, encapsuler dans jQuery() pour eviter de demarrer avant document.ready ce qui provoque une erreur
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@64240 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- galleria/galleria_pipelines.php
+++ galleria/galleria_pipelines.php
@@ -1,20 +1,25 @@
 <?php
+/**
+ * Plugin Galleria
+ *
+ */
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
 
 function galleria_insert_head($flux){
-	$flux .= '<!-- insert_head -->';
 	$flux .= insert_js();
 	return $flux;
 }
 
 function galleria_header_prive($flux){
 	include_spip("inc/filtres");
-	$flux .= '<!-- header_prive -->';
 	$flux .= insert_js();
 	return $flux;
 }
 
 function insert_js(){
-	return '<script type="text/javascript" src="'.url_absolue(find_in_path('galleria/galleria-1.2.5.min.js')).'"></script>';
+	return '<script type="text/javascript" src="'.url_absolue(find_in_path('galleria/galleria.min.js')).'"></script>';
 }
 
 ?>
