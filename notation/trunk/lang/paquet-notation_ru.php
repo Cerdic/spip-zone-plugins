4db1bac82dea850239ec754791c1d1571b0cadda
n.komar@aloha.org.ua <n.komar@aloha.org.ua@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-08-01 01:40:01 +0000

    langues (paquet-notation)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@64296 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ notation/trunk/lang/paquet-notation_ru.php
@@ -0,0 +1,18 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-notation?lang_cible=ru
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// N
+	'notation_description' => 'Тег <code>#FORMULAIRE_NOTATION</code> предназначен для вывода звезд голосования.
+_ Модель <code><autxx|notation|id_article=yy></code> используется для вывода статьи.
+ <code><notation|topten></code> выводит 10 оценок..',
+	'notation_nom' => 'Голосование (Notation)',
+	'notation_slogan' => 'Устанавливает рейтинг элементов'
+);
+
+?>
