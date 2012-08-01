f977bbc9e65b75808432e217fa9cd50297d91594
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-08-01 11:11:32 +0000

    passage en paquet.xml et changeemnt de numéro pour garder un certaine cohérence (non comptatible avec l'ancienne version, puisque ne s'appuyant pas sur la même version de spip)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@64316 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ critere_mots/trunk/lang/paquet-critere_mots_fr.php
@@ -0,0 +1,19 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-critere_mots
+// Langue: fr
+// Date: 01-08-2012 13:09:46
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// C
+	'critere_mots_description' => 'Permet d\'afficher les éléments qui ont plusieurs mots clés en communs, via la variable $mots[] passée dans l\'environnement',
+	'critere_mots_slogan' => 'Sélectionner des objets ayant des mots clés en communs',
+);
+?>
