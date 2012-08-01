e95e84e2e2736ee23446a01ea5106baa3a86ecbf
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-08-01 01:39:55 +0000

    langues (paquet-chosen)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@64294 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- chosen/lang/paquet-chosen_fr.php
+++ chosen/lang/paquet-chosen_fr.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/chosen/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
@@ -10,7 +10,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 La classe CSS <code>chosen</code> sur un <code><select></code> chargera automatiquement Chosen dessus.',
 	'chosen_nom' => 'Chosen',
-	'chosen_slogan' => 'Intégrer la librairie Chosen dans SPIP',
+	'chosen_slogan' => 'Intégrer la librairie Chosen dans SPIP'
 );
 
 ?>
