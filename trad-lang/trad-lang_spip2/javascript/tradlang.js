b41e0b44db67985f8ed9ff2c2cde438c4c98c86f
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-08-02 14:58:19 +0000

    sur le onAjaxload on recharge ces fonctions
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@64345 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- trad-lang/trad-lang_spip2/javascript/tradlang.js
+++ trad-lang/trad-lang_spip2/javascript/tradlang.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+var tradlang_switchers = function(){
 	$('#formulaire_switcher_langue .fieldset ul,#formulaire_switcher_langue .boutons,.formulaire_choisir_module .fieldset ul,.formulaire_choisir_module .boutons').hide();
 	$('#formulaire_switcher_langue .fieldset h3,.formulaire_choisir_module .fieldset h3').css({cursor:'pointer'}).click(function(){
 		if($(this).next('ul').is(':hidden')){
@@ -7,4 +7,9 @@ $(document).ready(function(){
 			$(this).parents('.formulaire_spip').find('.fieldset ul,.boutons').slideUp();
 		}
 	});
+}
+
+$(document).ready(function(){
+	tradlang_switchers();
+	onAjaxLoad(tradlang_switchers)
 });
