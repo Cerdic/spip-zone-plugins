7bb847497e97ac84a3b3993a910986dfec6fbfd7
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-08-03 17:05:36 +0000

    Oups du commit précédent
    On utilise les répertoire css et javascript qui sont en général utilisés dans d'autres plugins
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@64381 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- tablesorter/tablesorter_pipelines.php
+++ tablesorter/tablesorter_pipelines.php
@@ -4,7 +4,7 @@ function tablesorter_insert_head_css($flux){
 	static $done = false;
 	if (!$done) {
 		$done = true;
-		$flux .= '<link rel="stylesheet" href="'.url_absolue(find_in_path('styles/tablesorter.css')).'" type="text/css" />';
+		$flux .= '<link rel="stylesheet" href="'.url_absolue(find_in_path('css/tablesorter.css')).'" type="text/css" />';
 	}
 
 	return $flux;
@@ -13,7 +13,7 @@ function tablesorter_insert_head_css($flux){
 
 function tablesorter_insert_head($flux){
 	// Insertion des librairies js
-	$flux .='<script src="'.url_absolue(find_in_path('scripts/jquery.tablesorter.js')).'" type="text/javascript"></script>';
+	$flux .='<script src="'.url_absolue(find_in_path('javascript/jquery.tablesorter.js')).'" type="text/javascript"></script>';
 	// Init de tablesorter
 	$flux .='
 	<script type="text/javascript">/* <![CDATA[ */
@@ -32,9 +32,9 @@ function tablesorter_insert_head($flux){
 
 function tablesorter_header_prive($flux){
 	// Insertion des librairies js
-	$flux .='<script src="'.url_absolue(find_in_path('scripts/jquery.tablesorter.js')).'" type="text/javascript"></script>';
+	$flux .='<script src="'.url_absolue(find_in_path('javascript/jquery.tablesorter.js')).'" type="text/javascript"></script>';
 	// Inclusion des styles du plugin
-	$flux .='<link rel="stylesheet" href="'.url_absolue(find_in_path('styles/tablesorter.css')).'" type="text/css" />';
+	$flux .='<link rel="stylesheet" href="'.url_absolue(find_in_path('css/tablesorter.css')).'" type="text/css" />';
 	// Init de tablesorter
 	$flux .='
 	<script type="text/javascript">/* <![CDATA[ */
