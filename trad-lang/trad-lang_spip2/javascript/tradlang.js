190a80eaacfbbccf206550a64ca9b3bc25a78e13
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-08-04 21:29:21 +0000

    Un début de page de statistiques
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@64393 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- trad-lang/trad-lang_spip2/javascript/tradlang.js
+++ trad-lang/trad-lang_spip2/javascript/tradlang.js
@@ -1,13 +1,13 @@
 var tradlang_switchers = function(){
-	$('#formulaire_switcher_langue .fieldset ul,#formulaire_switcher_langue .boutons,.formulaire_choisir_module .fieldset ul,.formulaire_choisir_module .boutons').hide();
-	$('#formulaire_switcher_langue .fieldset h3,.formulaire_choisir_module .fieldset h3').each(function(){
+	$('#formulaire_switcher_langue .fieldset ul,#formulaire_switcher_langue .boutons,.formulaire_choisir_module .fieldset ul,.formulaire_choisir_module .boutons,table.info.trads tbody').hide();
+	$('#formulaire_switcher_langue .fieldset h3,.formulaire_choisir_module .fieldset h3,table.info.trads caption').each(function(){
 		if($(this).find('a.legend_link').size() == 0){
 			$(this).wrapInner('<a href="#" class="legend_link"></a>');
 			$(this).find('a.legend_link').unbind('click').click(function(){
-				if($(this).parent('h3').next('ul').is(':hidden')){
-					$(this).parents('.formulaire_spip').find('.fieldset ul:hidden,.boutons:hidden').slideDown();
+				if($(this).parent('h3').next('ul').is(':hidden') || $(this).parent('caption').next('tbody').is(':hidden')){
+					$(this).parents('.formulaire_spip,table').find('.fieldset ul:hidden,.boutons:hidden,tbody:hidden').slideDown();
 				}else{
-					$(this).parents('.formulaire_spip').find('.fieldset ul,.boutons').slideUp();
+					$(this).parents('.formulaire_spip,table').find('.fieldset ul,.boutons,tbody').slideUp();
 				}
 				return false;
 			});
