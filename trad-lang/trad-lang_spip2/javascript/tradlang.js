4b69e3ae895efb0cf2aff909cc5a505b13883148
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-08-04 10:12:33 +0000

    Petite amélioration du js, on transforme le h3.legend en h3.legend a
    Trois nouvelles colonnes qui ne doivent pas permettre de tri
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@64388 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- trad-lang/trad-lang_spip2/javascript/tradlang.js
+++ trad-lang/trad-lang_spip2/javascript/tradlang.js
@@ -1,12 +1,19 @@
 var tradlang_switchers = function(){
 	$('#formulaire_switcher_langue .fieldset ul,#formulaire_switcher_langue .boutons,.formulaire_choisir_module .fieldset ul,.formulaire_choisir_module .boutons').hide();
-	$('#formulaire_switcher_langue .fieldset h3,.formulaire_choisir_module .fieldset h3').css({cursor:'pointer'}).click(function(){
-		if($(this).next('ul').is(':hidden')){
-			$(this).parents('.formulaire_spip').find('.fieldset ul:hidden,.boutons:hidden').slideDown();
-		}else{
-			$(this).parents('.formulaire_spip').find('.fieldset ul,.boutons').slideUp();
+	$('#formulaire_switcher_langue .fieldset h3,.formulaire_choisir_module .fieldset h3').each(function(){
+		if($(this).find('a.legend_link').size() == 0){
+			$(this).wrapInner('<a href="#" class="legend_link"></a>');
+			$(this).find('a.legend_link').unbind('click').click(function(){
+				if($(this).parent('h3').next('ul').is(':hidden')){
+					$(this).parents('.formulaire_spip').find('.fieldset ul:hidden,.boutons:hidden').slideDown();
+				}else{
+					$(this).parents('.formulaire_spip').find('.fieldset ul,.boutons').slideUp();
+				}
+				return false;
+			});
 		}
 	});
+	
 }
 
 $(document).ready(function(){
