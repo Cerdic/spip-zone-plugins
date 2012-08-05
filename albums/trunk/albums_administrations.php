8fcb475ce845bf7d20e84936706a635ae1cb3361
tcharlss@hotmail.fr <tcharlss@hotmail.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-08-05 20:38:45 +0000

    Révision des fonctions d'installation/désinstallation :  prise en compte d'une m.a.j depuis la version 1.1. Correction de la déclaration des metas.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@64423 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- albums/trunk/albums_administrations.php
+++ albums/trunk/albums_administrations.php
@@ -1,55 +1,70 @@
 <?php
-if (!defined("_ECRIRE_INC_VERSION")) return;
+/**
+ * Plugin Albums
+ * Licence GNU/GPL
+ */
 
-// INSTALLATION
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+
+/**
+ * Fonction d'installation du plugin et de mise à jour.
+**/
 function albums_upgrade($nom_meta_base_version, $version_cible){
 	$maj = array();
+
+	include_spip('inc/config');
+
+	# Premiere installation  creation des tables
 	$maj['create'] = array(
-		array('maj_tables', array('spip_albums')),
-		array('maj_tables', array('spip_albums_liens')),
+		array('maj_tables', array('spip_albums', 'spip_albums_liens')),
 	);
 
-	// config : valeurs par defaut
-	include_spip('inc/config');
-	ecrire_config('albums/objets', array('spip_articles'));
-	ecrire_config('albums/afficher_champ_categorie', ''); #desactive par defaut
-	ecrire_config('albums/afficher_champ_descriptif', 'on');
-	
-	// si besoin, ajoute les albums a la liste des objets pour les documents-joints
-	if ($documents_objets = lire_config('documents_objets')
-		AND strpos($documents_objets, 'spip_albums,') === false 
-	) {
-		$documents_objets_avec_albums = $documents_objets . 'spip_albums,';
-		ecrire_config('documents_objets', $documents_objets_avec_albums);
-	}
+	# Version 2.0.2 : meta + suppression colonne categorie
+	$maj['2.0.2'] = array(
+		# On supprime la colonne 'categorie'
+		array('sql_alert','TABLE spip_albums DROP COLUMN categorie'),
+		# Configuration : valeurs par defaut
+		array(ecrire_config('albums/afficher_champ_descriptif', 'on'),
+			ecrire_config('albums/objets', array('spip_articles')),
+			ecrire_config('albums/afficher_champ_descriptif', 'on'),
+			ecrire_config('albums/vue_icones', array('titre')),
+			ecrire_config('albums/vue_liste', array('icone', 'mimetype', 'poids', 'dimensions'))),
+	);
+
+	# On active l'ajout de documents aux albums
+	if (!in_array('spip_albums', explode(',',$GLOBALS['meta']['documents_objets'])))
+		ecrire_meta('documents_objets',$GLOBALS['meta']['documents_objets'] . 'spip_albums,');
 
 	include_spip('base/upgrade');
 	maj_plugin($nom_meta_base_version, $version_cible, $maj);
 }
 
-// DESINSTALLATION
+
+/**
+ * Fonction de désinstallation du plugin.
+**/
 function albums_vider_tables($nom_meta_base_version) {
 
-	// supression des tables
+	# supression des tables
 	sql_drop_table("spip_albums");
 	sql_drop_table("spip_albums_liens");
-	
-	// suppression meta & config
+
+	# suppression meta & config
 	effacer_meta($nom_meta_base_version);
 	effacer_config('albums');
-	
-	// si besoin, retire les albums de la liste des objets pour les documents-joints
-	if ($documents_objets = lire_config('documents_objets')
-		AND strpos($documents_objets, 'spip_albums,') === true 
-	) {
-		$documents_objets_sans_albums = str_replace('spip_albums,', '', $documents_objets);
-		ecrire_config('documents_objets', $documents_objets_sans_albums);
-	}
-	
-	// suppression des liens des documents lies aux albums
+
+	# a faire : retirer les albums de la liste des objets pour les documents
+
+	# Suppression des liens des documents lies aux albums
+	# -> utiliser optimiser_base_disparus a la place ?
 	if (sql_countsel("spip_documents_liens", "objet='album'")){
-		sql_delete("spip_documents_liens", "objet='album'"); # variables sql_delete : $from $where ...
+		sql_delete("spip_documents_liens", "objet='album'");
 	}
+
+	# Nettoyer les versionnages et forums
+	sql_delete("spip_versions",		sql_in("objet", array('album')));
+	sql_delete("spip_versions_fragments",	sql_in("objet", array('album')));
 }
 
 ?>
