f17f027e95bbd45219bdddb257f2be95d1b89109
tcharlss@hotmail.fr <tcharlss@hotmail.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-08-05 20:50:25 +0000

    Configuration : ajout de quelques options 'expérimentales' pour gérer les éléments affichés par les modèles Formulaire editer_album : on simplifie un peu
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@64430 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- albums/trunk/formulaires/editer_album.php
+++ albums/trunk/formulaires/editer_album.php
@@ -1,35 +1,65 @@
 <?php
 /**
- * Formulaire de creation et d'edition d'une album
+ * Plugin Albums
+ * Licence GNU/GPL
  */
- 
-if (!defined("_ECRIRE_INC_VERSION")) return;
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 include_spip('inc/actions');
 include_spip('inc/editer');
 
-// CHARGER
-function formulaires_editer_album_charger_dist($id_album='new',$id_rubrique=0,$retour='',$lier_trad=0,$config_fonc='',$row=array(),$hidden=''){
-	$valeurs = formulaires_editer_objet_charger('album',$id_album,$id_rubrique,$lier_trad,$retour,$config_fonc,$row,$hidden);
-	return $valeurs;
-}
-
-// IDENTIFIER
 /**
  * Identifier le formulaire en faisant abstraction des parametres qui ne representent pas l'objet edite
  */
-function formulaires_editer_album_identifier_dist($id_album='new',$id_rubrique=0,$retour='',$lier_trad=0,$config_fonc='',$row=array(),$hidden=''){
-	return serialize(array(intval($id_album)));
+function formulaires_editer_album_identifier_dist($id_album='new', $retour='', $associer_objet='', $lier_trad=0, $config_fonc='', $row=array(), $hidden=''){
+	return serialize(array(intval($id_album), $associer_objet));
 }
 
-// VERIFIER
-function formulaires_editer_album_verifier_dist($id_album='new',$id_rubrique=0,$retour='',$lier_trad=0,$config_fonc='',$row=array(),$hidden=''){
+/**
+ * Declarer les champs postes et y integrer les valeurs par defaut
+ */
+function formulaires_editer_album_charger_dist($id_album='new', $retour='', $associer_objet='', $lier_trad=0, $config_fonc='', $row=array(), $hidden=''){
+	$valeurs = formulaires_editer_objet_charger('album',$id_album,'',$lier_trad,$retour,$config_fonc,$row,$hidden);
+
+	// On recupere certaines valeurs dans l'environnement pour le squelette du formulaire
+	list($objet, $id_objet) = explode('|', $associer_objet);
+	$valeurs['associer_objet'] = $associer_objet; // pour statut 
+	$valeurs['objet'] = $objet; // pour titre par défaut
+	$valeurs['id_objet'] = $id_objet; // idem
+
+	return $valeurs;
+}
+
+/**
+ * Verifier les champs postes et signaler d'eventuelles erreurs
+ */
+function formulaires_editer_album_verifier_dist($id_album='new', $retour='', $associer_objet='', $lier_trad=0, $config_fonc='', $row=array(), $hidden=''){
 	return formulaires_editer_objet_verifier('album',$id_album);
 }
 
-// TRAITER
-function formulaires_editer_album_traiter_dist($id_album='new',$id_rubrique=0,$retour='',$lier_trad=0,$config_fonc='',$row=array(),$hidden=''){
-	return formulaires_editer_objet_traiter('album',$id_album,$id_rubrique,$lier_trad,$retour,$config_fonc,$row,$hidden);
+/**
+ * Traiter les champs postes
+ */
+function formulaires_editer_album_traiter_dist($id_album='new', $retour='', $associer_objet='', $lier_trad=0, $config_fonc='', $row=array(), $hidden=''){
+	$res = formulaires_editer_objet_traiter('album',$id_album,'',$lier_trad,$retour,$config_fonc,$row,$hidden);
+
+	// Un lien a prendre en compte ?
+	if ($associer_objet AND $id_album = $res['id_album']) {
+		list($objet, $id_objet) = explode('|', $associer_objet);
+
+		if ($objet AND $id_objet AND autoriser('modifier', $objet, $id_objet)) {
+			include_spip('action/editer_liens');
+			objet_associer(array('album' => $id_album), array($objet => $id_objet));
+			if (isset($res['redirect'])) {
+				$res['redirect'] = parametre_url ($res['redirect'], 'id_album', '', '&');
+				$res['redirect'] = parametre_url ($res['redirect'], 'id_album_ajoute', $id_album, '&');
+			}
+		}
+
+	}
+	return $res;
+
 }
 
 
