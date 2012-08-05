4bf8d33e15664f81b14f5fdece00b37256136cb9
tcharlss@hotmail.fr <tcharlss@hotmail.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-08-05 20:55:51 +0000

    Modèles : - m.a.j des 2 modèles (vue en liste et en 'aperçu') pour prendre en compte les trucs optionnels. On change également le message affiché dans l'espace privé - Ajout d'un modèle statut_album qui fait comme le filtre |puce_statut - Quelques modifs sur le modèle qui affiche le logo dans l'espace privé
    Css partie publique : 
    Quelques modifs pour que le titre ne dépasse pas la largeur de l'image
    
    Fichier de langue : nettoyage
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@64431 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- albums/trunk/lang/album_fr.php
+++ albums/trunk/lang/album_fr.php
@@ -1,93 +1,114 @@
 <?php
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-	
+
+	// A
+
 	// C
-	'creer_et_associer_un_album' => "Créer et associer un album",
-	
+	'cfg_titre_albums'		=> "Albums",
+	'c_albums_info'			=> "Gestion des albums",
+	'c_albums_info_texte'		=> "<div class='albums'>
+	Les onglets & filtres permettent de sélectionner les albums selon plusieurs critères :
+	<ul>
+	<li><strong>Onglets 1er rang:</strong> objets auxquels les albums sont liés.</li>
+	<li><strong>Filtres 2ème rang:</strong>
+		<ul>
+		<li>statuts des albums</li>
+		<li>vus = albums insérés dans un texte.</li>
+		<li>inutilisés = albums sans lien avec un objet éditorial.</li>
+		</ul>
+	</li>
+	<li><strong>Filtres 3ème rang:</strong> types de médias contenus (plusieurs choix simultanés possibles).</li>
+	</ul>
+	<p>Enfin, les icones à droite de la liste permettent de changer le statut d'un album.</p>
+	<p><a href='http://contrib.spip.net/Albums-3971'>Documentation en ligne</a></p>
+	</div>",
+
+	// E
+	'explication_affichage_modele'		=> "La pérennité de ces options n'est pas garantie !",
+
 	// F
-	'filtre_tous' => "Tous",
-	'filtre_vus' => "Vus",
-	'filtre_non_vus' => "Non vus",
-	'filtre_albums_vides' => "Albums vides",
-	
+	'filtre_tous'				=> "Tous",
+	'filtre_vus'				=> "Vus",
+	'filtre_non_vus'			=> "Non vus",
+	'filtre_albums_vides'			=> "Albums vides",
+	'filtre_albums_non_vides'		=> "Albums non vides",
+
 	// H
-	'heading_logo' => "Logo",
-	'heading_id' => "ID",
-	'heading_infos' => "Infos",
-	'heading_contenu' => "Contenu",
-	'heading_utilisation' => "Utilisation",
-	'heading_vu' => "Utilisation comme modèle",
-	'heading_descriptif' => "Description",
-	'heading_categorie' => "Categorie",
-	'heading_parents' => "Parent(s)",
-	
-	
+	'heading_logo'				=> "Logo",
+	'heading_id'				=> "ID",
+	'heading_infos'				=> "Infos",
+	'heading_titre'				=> "Titre",
+	'heading_contenu'			=> "Contenu",
+	'heading_liaison'			=> "Liaison",
+	'heading_descriptif'			=> "Description",
+
 	// I
-	'info_aucun_album' => "Aucun album",
-	'info_aucun_document' => "Cet album est vide",
-	'info_1_album' => "1 album",
-	'info_nb_albums' => "@nb@ albums",
-	'info_1_document' => "1 document",
-	'info_nb_documents' => "@nb@ documents",
-	'icone_modifier_album' => "Modifier cet album",
-	'icone_creer_album' => "Créer un album",
-	'info_titre' => "Titre",
-	'info_aucun_objet_associe' => "Cet album n'est associé à aucun objet",
-	'info_aucun_album_supplementaire' => "Aucun album supplémentaire",
-	'info_diaporama_espace_prive' => "Diaporama",
-	'info_album_vide' => "Album vide",
-	'info_album_vu' => "Album présent dans le texte",
-	'info_retirer_vu' => "Impossible de retirer : ",
-	'info_album_associe' => "Cet album est associé à un objet",
-	'info_album_insere_dans_texte' => "Inséré dans le texte",
-	'info_statut_modele' => "Album non publié",
-	'info_documents_joints_albums' => "L'ajout de documents-joints aux albums n'est pas activé",
-	'info_aucun_album_criteres' => "Aucun album ne correspond à ces critères",
-	'info_aucun_album_vide' => "Aucun album vide",
-	
+	'icone_ajouter_album'			=> "Ajouter un album",
+	'icone_creer_album'			=> "Créer un nouvel album",
+	'icone_associer_album'			=> "Associer un album existant",
+	'icone_modifier_album'			=> "Modifier cet album",
+	'icone_delier_album'			=> "Enlever cet album",
+	'icone_fermer'				=> "Fermer",
+	'info_aucun_album'			=> "Aucun album",
+	'info_aucun_album_supplementaire'	=> "Aucun album supplémentaire",
+	'info_aucun_album_criteres'		=> "Aucun album ne correspond à ces critères",
+	'info_1_album'				=> "1 album",
+	'info_nb_albums'			=> "@nb@ albums",
+	'info_album_vide'			=> "Album vide",
+	'info_statut_modele'			=> "Album non publié",
+	'info_documents_joints_albums'		=> "Veillez à activer l'ajout de documents-joints aux albums",
+	'info_aucun_album_vide'			=> "Aucun album vide",
+	'info_dans_objet'			=> "dans des",
+	'info_association'			=> "Associé à :",
+	'info_recherche_album_zero'		=> "Aucun résultat pour « @cherche_album@ »",
+	'info_resultat_recherche_aucun'		=> 'Aucun résultat',
+	'info_resultat_recherche_un'		=> '1 résultat',
+	'info_resultat_recherche_plus'		=> '@nb@ résultats',
+
 	// L
-	'label_fieldset_objets_albums' => "Objets sur lesquels apparaîtront les albums",
-	'label_activer_album_objets' => "Activer les albums pour les contenus :",
-	'label_fieldset_afficher_champs' => "Champs affichés dans le formulaire d'édition",
-	'label_titre' => "Titre",
-	'label_categorie' => "Catégorie",
-	'label_descriptif' => "Descriptif",
-	'label_contenu' => "Contenu",
-	'label_contenus' => "Contenus",
-	'label_conteneur' => "Conteneur",
-	'label_conteneurs' => "Conteneurs",
-	'label_parents' => "Parents",
-	'lien_ajouter_album' => "Ajouter cet album",
-	'lien_associer_album' => "Associer cet album",
-	'lien_retirer_album' => "Retirer cet album",
-	'lien_retirer_tous_albums' => "Retirer tous les albums",
-	'label_case_utiliser_titre_defaut' => "Le titre par défaut est celui de l'objet parent",
-	'label_utiliser_titre_defaut' => "Titre par défaut",
-	
+	'label_fieldset_objets_albums'		=> "Objets auxquels les albums peuvent être associés",
+	'label_activer_album_objets'		=> "Activer les albums pour les contenus :",
+	'label_fieldset_champs'			=> "Champs du formulaire d'édition",
+	'label_afficher_descriptif'		=> "Descriptif",
+	'label_case_utiliser_titre_defaut'	=> "Par défaut, assigner le titre de l'objet parent",
+	'label_utiliser_titre_defaut'		=> "Titre",
+	'label_fieldset_affichage_modele'	=> "Modèles : informations optionnelles",
+	'label_vue_icones'			=> "Vue Aperçu",
+	'label_vue_liste'			=> "Vue Liste",
+	'label_vue_icones_titre'		=> "Titre",
+	'label_vue_liste_icone'			=> "Icône",
+	'label_vue_liste_mimetype'		=> "Mimetype",
+	'label_vue_liste_poids'			=> "Poids",
+	'label_vue_liste_dimensions'		=> "Dimensions <small>(images)</small>",
+	'label_titre'				=> "Titre",
+	'label_categorie'			=> "Catégorie",
+	'label_descriptif'			=> "Descriptif",
+
 	// O
-	'onglet_tout' => "Tout",
-	'onglet_tous' => "Tous",
-	'onglet_tous_contenus' => "Tous les contenus",
-	
+	'onglet_tous'				=> "Tous",
+	'onglet_creer_album'			=> "Créer",
+	'onglet_associer_album'			=> "Associer",
+
 	// T
-	'titre_album' => "Album",
-	'titre_albums' => "Albums",
-	'titre_page_albums' => "Albums",
-	'titre_page_configurer_albums' => "Configurer les Albums",
-	'texte_inserer_modele' => "un album",
-	'texte_creer_album' => "Créer un nouvel album",
-	'texte_ajouter_album' => "Ajouter un album",
-	'texte_associer_album' => "Associer un album existant",
-	'titre_logo_album' => "Logo",
-	'titre_info_association' => "Utilisé dans",
-	'texte_album_vide' => "Album vide",
-	'texte_aucun_document' => "Cet album est vide",
-	'texte_sans_titre' => "Sans titre",
-	'texte_modifier' => "Modifier",
-	'titre_albums_joints' => "Albums",
-	'texte_changer_statut' => "Modifier le statut"
-	
+	'titre_album'				=> "Album",
+	'titre_albums'				=> "Albums",
+	'titre_page_configurer_albums'		=> "Configurer les Albums",
+	'titre_logo_album'			=> "Logo",
+	'texte_statut_publie'			=> "publié",
+	'texte_statut_refuse'			=> "non publié",
+	'texte_creer_album'			=> "Créer un nouvel album",
+	'texte_ajouter_album'			=> "Ajouter un album",
+	'texte_associer_album'			=> "Associer un album existant",
+	'texte_modifier'			=> "Modifier",
+	'texte_changer_statut'			=> "Modifier le statut",
+
+	// U
+	'un_image'				=> "image",
+	'un_audio'				=> "bande-son",
+	'un_video'				=> "vidéo",
+	'un_file'				=> "document"
+
 );
 
 ?>
