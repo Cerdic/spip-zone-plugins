4fe048eff7275e35e150660b5b96f0a4dd6a03a8
brunobergot@gmail.com <brunobergot@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-08-06 14:47:07 +0000

    version 4.1.0 : on se branche sur Leaflet 0.4.2 (on peut encore nettoyer pas mal de code sur les news, etc) + maj des plugins de la  lib
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@64471 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- gis/trunk/gis_options.php
+++ gis/trunk/gis_options.php
@@ -2,7 +2,7 @@
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
-define('_DIR_LIB_GIS','lib/leaflet-gis-4.0.7/');
+define('_DIR_LIB_GIS','lib/leaflet-gis-4.1.0/');
 
 $GLOBALS['logo_libelles']['id_gis'] = _T('gis:libelle_logo_gis');
 
