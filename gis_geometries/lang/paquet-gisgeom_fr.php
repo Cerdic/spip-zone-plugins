19fb44bd014519533067ba1d67b9d4b16826ff3a
brunobergot@gmail.com <brunobergot@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-08-06 16:24:22 +0000

    Version 1.2.0 : necessite GIS 4.1.0 + compat Leaflet 0.4 + passage en paquet.xml
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@64476 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ gis_geometries/lang/paquet-gisgeom_fr.php
@@ -0,0 +1,19 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-gisgeom
+// Langue: fr
+// Date: 06-08-2012 18:20:02
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// G
+	'gisgeom_description' => 'Ce plugin ajoute la prise en charge des formes géométriques à GIS. Il utilise les fonctions spatiales de MySQL disponibles à partir de la version 4.1.',
+	'gisgeom_slogan' => 'Prise en charge formes géométriques dans GIS',
+);
+?>
