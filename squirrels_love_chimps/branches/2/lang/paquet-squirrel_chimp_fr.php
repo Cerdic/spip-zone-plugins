1d9463adf68905e9967e819f199fee4c3ddd323d
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-08-06 08:08:42 +0000

    langues
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@64456 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- squirrels_love_chimps/branches/2/lang/paquet-squirrel_chimp_fr.php
+++ squirrels_love_chimps/branches/2/lang/paquet-squirrel_chimp_fr.php
@@ -1,11 +1,11 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/squirrels_love_chimps/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-	// M
+	// S
 	'squirrel_chimp_description' => 'Ce plugin constitue le coeur d\'un set de plugins qui on pour but de d\'intégerer connecter SPIP  avec [MailChimp->http://mailchimp.com/]
 	
 		Il met à disposition l\'api de Mailchimp ainsi qu\'un framework permettant d\'ajouter facilement d\'autre plugins
@@ -15,7 +15,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 		-* [Squirrels Love Chimps Lists us->http://files.spip.org/spip-zone/squirrels_love_chimps_lists_plus.zip](squirrel_chimp_lists_plus)	
 		-* [Squirrels Love Chimps Campagnes->http://files.spip.org/spip-zone/squirrels_love_chimps_campaigns.zip](squirrel_chimp_campaigns)',
 	'squirrel_chimp_nom' => 'Squirrels Love Chimps',
-	'squirrel_chimp_slogan' => 'Connecter SPIP avec Mailchimp',
+	'squirrel_chimp_slogan' => 'Connecter SPIP avec Mailchimp'
 );
 
 ?>
