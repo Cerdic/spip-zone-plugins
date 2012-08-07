609d7d0c7f9466bd355107fe7167d592d62cf2ac
tcharlss@hotmail.fr <tcharlss@hotmail.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-08-07 18:05:01 +0000

    On prépare le recensement dans archivelist.txt + lien vers doc provisoire pour les courageux testeurs
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@64571 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- albums/trunk/lang/paquet-albums_fr.php
+++ albums/trunk/lang/paquet-albums_fr.php
@@ -6,7 +6,8 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// A
 	'albums_nom' => "Albums",
 	'albums_slogan' => "Grouper vos documents sous forme d'albums",
-	'albums_description' => "<ins>Ceci est une version de développement</ins>. 
+	'albums_description' => "<ins>ATTENTION, VERSION EN DEVELOPPEMENT !</ins>. 
+	Veillez à consulter <a href='http://contrib.spip.net/Albums-v2-DEV'>la documentation provisoire</a> avant installation ou mise à jour.
 
 	Ce plugin ajoute un nouvel objet 'album' que l'on peut lier à n'importe quel autre objet éditorial.
 	Les albums sont des conteneurs pour documents, ils vous permettent par exemple de séparer
