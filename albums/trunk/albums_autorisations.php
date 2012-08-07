46bd7180a58b83f865073e3562cc8db98e688c0d
tcharlss@hotmail.fr <tcharlss@hotmail.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-08-07 11:41:52 +0000

    Ajout d'une autorisation pour supprimer définitivement un album, 4 conditions : etre admin, l'album doit etre vide, inutilise et a la poubelle + révisions diverses
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@64548 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- albums/trunk/albums_autorisations.php
+++ albums/trunk/albums_autorisations.php
@@ -26,23 +26,23 @@ function albums_autoriser(){}
  * Autorisation a afficher l'icone de creation rapide
  * = autorisation creer
  */ 
-function autoriser_albumcreer_menu_dist($faire, $type='', $id=0, $qui=NULL, $opt=NULL){
-	return autoriser('creer','album',$id,$qui,$opt);
+function autoriser_albumcreer_menu_dist($faire, $type='', $id=0, $qui=NULL, $opts=NULL){
+	return autoriser('creer','album',$id,$qui,$opts);
 }
 
 /**
  * Autorisation a afficher les albums dans le menu
  * = autorisation administrer
  */ 
-function autoriser_albums_menu_dist($faire, $type='', $id=0, $qui=NULL, $opt=NULL){
-	return autoriser('administrer','album',$id,$qui,$opt);
+function autoriser_albums_menu_dist($faire, $type='', $id=0, $qui=NULL, $opts=NULL){
+	return autoriser('administrer','album',$id,$qui,$opts);
 }
 
 /**
  * Autorisation a administrer les albums
  * Admins
  */ 
-function autoriser_albums_administrer_dist($faire,$quoi,$id,$qui,$options) {
+function autoriser_albums_administrer_dist($faire, $type, $id, $qui, $opts) {
 	return $qui['statut'] == '0minirezo';
 }
 
@@ -50,8 +50,9 @@ function autoriser_albums_administrer_dist($faire,$quoi,$id,$qui,$options) {
  * Autorisation a ajouter (creer ou associer) un album a un objet
  * Il faut avoir le droit de modifier l'objet
  */
-function autoriser_ajouteralbumdans_dist($faire, $objet, $id_objet, $qui, $options) {
-	if (!$id_objet) return false; // pas d'album sur un article vide !
+function autoriser_album_ajouter_dist($faire, $type, $associer_objet, $qui, $opts) {
+	list($objet, $id_objet) = explode('|',$associer_objet);
+	if (!intval($id_objet) OR !$objet) return false;
 	return autoriser('modifier', $objet, $id_objet, $qui);
 }
 
@@ -59,50 +60,65 @@ function autoriser_ajouteralbumdans_dist($faire, $objet, $id_objet, $qui, $optio
  * Autorisation a creer un album
  * Admins et redacteurs
  */ 
-function autoriser_album_creer_dist($faire, $type, $id, $qui, $opt) {
+function autoriser_album_creer_dist($faire, $type, $id, $qui, $opts) {
 	return in_array($qui['statut'], array('0minirezo', '1comite'));
-}
-
-/**
- * Autorisation a voir les fiches completes
- * Tout le monde
- */ 
-function autoriser_album_voir_dist($faire, $type, $id, $qui, $opt) {
-	return true;
-}
-
-/**
- * Autorisation a modifier un album
- * il faut etre admin ou auteur de l'album
- */ 
-function autoriser_album_modifier_dist($faire, $type, $id, $qui, $opt) {
-
-	if (!function_exists('auteurs_album'))
-		include_spip('inc/auth'); // pour auteurs_album si espace public
-
-	return
-		auteurs_album($id, "id_auteur=".$qui['id_auteur']) 
-		OR in_array($qui['statut'], array('0minirezo'));
-
-}
-
-/**
- * Autorisation a mettre un album a la poubelle
- * Il faut etre admin ou auteur de l'album 
- */ 
-function autoriser_album_supprimer_dist($faire, $type, $id, $qui, $opt) {
-	return $qui['statut'] == '0minirezo' AND !$qui['restreint'];
 }
 
 /**
  * Autorisation a associer/dissocier un album a un objet
  * Il faut etre admin ou pouvoir modifier l'objet
  */
-function autoriser_album_associer_dist($faire, $type, $associer_objet, $qui, $options) {
+function autoriser_album_associer_dist($faire, $type, $associer_objet, $qui, $opts) {
 	list($objet, $id_objet) = explode('|',$associer_objet); 
-	if (!$id_objet) return false;
+	if (!intval($id_objet) OR !$objet) return false;
 	return autoriser('modifier', $objet, $id_objet, $qui);
-	#return in_array($qui['statut'], array('0minirezo', '1comite'));
+}
+
+/**
+ * Autorisation a voir les fiches completes
+ * Tout le monde
+ */ 
+function autoriser_album_voir_dist($faire, $type, $id, $qui, $opts) {
+	return true;
+}
+
+/**
+ * Autorisation a modifier le logo d'un album
+ * = autoriser modifier album
+ */
+function autoriser_album_iconifier_dist($faire, $quoi){
+	return autoriser('modifier', 'album', $id, $qui, $opts);
+}
+
+/**
+ * Autorisation a modifier un album
+ * il faut etre admin ou auteur de l'album
+ */ 
+function autoriser_album_modifier_dist($faire, $type, $id, $qui, $opts) {
+	$id = sql_quote($id);
+	if (!function_exists('auteurs_objet'))
+		include_spip('inc/auth'); // pour auteurs_objet si espace public
+
+	return
+		auteurs_objet($type, $id, "id_auteur=".$qui['id_auteur'])
+		OR in_array($qui['statut'], array('0minirezo'));
+
+}
+
+/**
+ * Autorisation a supprimer definitivement un album
+ * Il faut etre admin, que l'album soit vide, a la poubelle et inutilise
+ */
+function autoriser_album_supprimer_dist($faire, $type, $id, $qui, $opts) {
+	$id = sql_quote($id);
+	$statut = sql_fetsel("statut", "spip_albums", "id_album='$id'");
+	$docs = sql_fetsel("id_objet", "spip_documents_liens", "objet='$type' AND id_objet='$id'");
+	$liaison = sql_fetsel("id_album", "spip_albums_liens", "id_album='$id'");
+	return
+		!in_array($liaison['id_album'], array($id)) # inutilise (sans lien vers d'autres objets)
+		AND !in_array($docs['id_objet'], array($id)) # vide (sans document ayant un lien vers l'album)
+		AND in_array($statut['statut'], array('poubelle')) # a la poubelle
+		AND ( $qui['statut'] == '0minirezo' AND !$qui['restreint'] ); # statut: admin
 }
 
 /**
@@ -124,9 +140,8 @@ function autoriser_autoassocieralbum_dist($faire, $type, $id, $qui, $opts) {
  *     - array : liste des id_auteur trouvés
  *     - false : serveur SQL indisponible
  */
-function auteurs_album($id_album, $cond='')
-{
-	return sql_allfetsel("id_auteur", "spip_auteurs_liens", "objet='album' AND id_objet=$id_album". ($cond ? " AND $cond" : ''));
+function auteurs_objet($objet='album', $id_objet, $cond=''){
+	return sql_allfetsel("id_auteur", "spip_auteurs_liens", "objet='$objet' AND id_objet=".intval($id_objet). ($cond ? " AND $cond" : ''));
 }
 
 ?>
