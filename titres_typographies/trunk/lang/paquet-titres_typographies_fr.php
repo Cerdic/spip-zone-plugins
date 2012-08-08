d1f9df532a0cbf3c51fb899e676182a1ec52e23c
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-08-08 20:21:00 +0000

    paquet.xml et langues minimum
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@64620 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ titres_typographies/trunk/lang/paquet-titres_typographies_fr.php
@@ -0,0 +1,11 @@
+<?php
+/** Fichier de langue de SPIP **/
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	'titres_typographies_slogan'=>'Typographiez vos titres !',
+	'titres_typographies_description'=>'Permet d\'utiliser les raccourcis typographiques de SPIP dans les champs TITRE et apparentés.'
+	
+);
+
+?>
