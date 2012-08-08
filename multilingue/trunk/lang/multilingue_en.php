e5cae3a70ad87d987a879270799e98ebc0032f03
websolutions@mychacra.net <websolutions@mychacra.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-08-08 01:41:32 +0000

    langues (multilingue)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@64581 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ multilingue/trunk/lang/multilingue_en.php
@@ -0,0 +1,24 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/multilingue?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'afficher_menu_select' => 'Show a dropdown menu (select)',
+
+	// M
+	'menu_langue' => 'Language selection menu',
+	'multilingue_titre' => 'Multilingual Web-site Easy',
+
+	// P
+	'precision_menu_select' => 'Will only work if you use the original includes',
+
+	// T
+	'titre_page_configurer_multilingue' => 'Configuration of Multilingual Web-site Easy'
+);
+
+?>
