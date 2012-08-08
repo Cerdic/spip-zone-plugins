49f044b69c3cbb0ed28f4d3ca076f6fb364ba33e
chankalan@free.fr <chankalan@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-08-08 15:14:41 +0000

    sans retour de bug = changement d'état + ajout de fichier de langue + légère incrémentation juste pour dire...
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@64606 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ basicone/lang/paquet-basicone_fr.php
@@ -0,0 +1,12 @@
+<?php
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+if (!defined('_ECRIRE_INC_VERSION')) return;
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'basicone_slogan' => 'Des icônes pour l\'interface privé de SPIP',
+	'basicone_description' => 'Un jeu d\'icônes sobres en noir et transparence pour s\'intégrer à toutes les couleurs de l\'interface privée, voir plus encore...',
+	'basicone_nom' => 'Basicône',
+	
+);
+?>
