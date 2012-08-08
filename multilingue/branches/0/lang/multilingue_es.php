a2b6dd747dc36a2cfa4007491d97a9944071b99b
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-08-08 05:47:59 +0000

    langues
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@64592 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ multilingue/branches/0/lang/multilingue_es.php
@@ -0,0 +1,24 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/multilingue?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'afficher_menu_select' => 'Muestra el menú desplegable (select)',
+
+	// M
+	'menu_langue' => 'Menú de idioma',
+	'multilingue_titre' => 'Sitio web Multilingüe Fácil',
+
+	// P
+	'precision_menu_select' => 'Funcionará solo si se usan las inclusiones (INCLURE) originales de este plugin',
+
+	// T
+	'titre_page_configurer_multilingue' => 'Configuración de Sitio web Multilingüe Fácil'
+);
+
+?>
