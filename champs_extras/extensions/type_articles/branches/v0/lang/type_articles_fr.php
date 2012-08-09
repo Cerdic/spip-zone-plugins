4abb0bd9e1594cac668457727d85ffafa6bf032e
yffic@cousumain.info <yffic@cousumain.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-08-09 12:18:05 +0000

    Branchage étape 5
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@64656 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ champs_extras/extensions/type_articles/branches/v0/lang/type_articles_fr.php
@@ -0,0 +1,18 @@
+<?php
+if (!defined("_ECRIRE_INC_VERSION")) return;
+// Fichier ayant vocation à être surchargé depuis un autre plugin
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	// L
+	'label_choix_type' => 'Type des articles :',
+
+	// N
+	'noisette_description' => 'Liste l\'ensemble des articles d\'un type spécifique du site, dans la même rubrique, dans une rubrique donnée...',
+	'noisette_titre' => 'Liste des articles d\'un type spécifique',
+
+	//T
+	'titre' => "Type de l'article : ",
+	'type_defaut'=>"Article",
+	'type_1'=>"Nom du type 1",
+	'type_2'=>"Nom du type 2"
+);
+?>
