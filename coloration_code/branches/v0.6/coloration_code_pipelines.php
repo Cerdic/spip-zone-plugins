131c19850af99898d52b735c968f0b5cfedab98b
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-08-12 18:42:52 +0000

    un / qui n'avait rien à faire
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@64745 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- coloration_code/branches/v0.6/coloration_code_pipelines.php
+++ coloration_code/branches/v0.6/coloration_code_pipelines.php
@@ -2,6 +2,6 @@
 
 function coloration_code_header_prive_css($css){
 	$css2=find_in_path('prive/themes/spip/coloration_code.css');
-	return $css.="\n<link rel='stylesheet' type='text/css' href='$css2' id='csscoloration_code'> /\n";
+	return $css.="\n<link rel='stylesheet' type='text/css' href='$css2' id='csscoloration_code'> \n";
 }
 ?>
