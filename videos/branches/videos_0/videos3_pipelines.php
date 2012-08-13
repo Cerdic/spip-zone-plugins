f1df55ef333b0d4882154acc57cc84b5ed4da9cb
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-08-13 18:52:13 +0000

    une branche 0, qui sera dédiée à SPIP 2.x
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@64765 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ videos/branches/videos_0/videos3_pipelines.php
@@ -0,0 +1,37 @@
+<?php
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+function videos_insert_head($flux){
+	include_spip('inc/config');
+	$css = find_in_path('theme/css/videos.css');
+	$flux .="
+<!-- Variables de configuration pour le plugin Vidéo(s) -->
+<script type='text/javascript'>var CONFIG_WMODE = '".lire_config('videos/wmode','opaque')."';</script>\n".
+'<link rel="stylesheet" href="'.direction_css($css).'" type="text/css" media="all" />'.
+"<!-- // Vidéo(s) -->"."\n";
+	return $flux;
+}
+
+function videos_jquery_plugins($scripts){
+	$scripts[] = "lib/html5media-1.1.5/api/html5media.min.js";
+	return $scripts;
+}
+
+function videos_affiche_gauche($flux) {
+
+	if ($en_cours = trouver_objet_exec($flux['args']['exec'])
+		AND $en_cours['edition']!==false // page edition uniquement
+		AND $type = $en_cours['type']
+		AND $id_table_objet = $en_cours['id_table_objet']
+		// id non defini sur les formulaires de nouveaux objets
+		AND (isset($flux['args'][$id_table_objet]) and $id = intval($flux['args'][$id_table_objet])
+			// et justement dans ce cas, on met un identifiant negatif
+		    OR $id = 0-$GLOBALS['visiteur_session']['id_auteur'])
+	  AND autoriser('joindredocument',$type,$id)){
+		$flux['data'] .= recuperer_fond('prive/contenu/videos_affiche_boite',array('objet'=>$type,'id_objet'=>$id));
+	}
+
+	return $flux;
+}
+
+?>
