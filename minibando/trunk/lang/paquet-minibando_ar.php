8cfcaa383151311a241f50774713be8090dd8410
george@middleeastwatch.net <george@middleeastwatch.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-08-13 01:40:03 +0000

    langues (paquet-minibando)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@64753 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ minibando/trunk/lang/paquet-minibando_ar.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-minibando?lang_cible=ar
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// M
+	'minibando_description' => 'Le plugin minibando remplace les boutons d’administration de SPIP par une barre d\'outils qui reprend les éléments disponibles dans la navigation haute de l’espace privé de SPIP.', # NEW
+	'minibando_slogan' => 'شريط أدوات صغير للكثير من الوظائف!'
+);
+
+?>
