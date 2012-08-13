4c753c76fa7415aaf1ffff12b7a9531349d641e3
george@middleeastwatch.net <george@middleeastwatch.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-08-13 01:39:53 +0000

    langues (paquet-menus)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@64751 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ menus/trunk/lang/paquet-menus_ar.php
@@ -0,0 +1,23 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-menus?lang_cible=ar
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// M
+	'menus_description' => 'Lorsqu\'on ne se sert pas du plugin {{Menus}}, on est obligé de définir tous ses menus dans les squelettes,
+	ce qui fait que les administrateurs du site n\'ont pas directement la main dessus et que lorsqu\'ils veulent y changer quelque chose, ils sont obligés
+	d\'en faire la demande à la personne en charge des squelettes. De plus, lorsqu\'on veut des liens statiques (un lien vers un article précis,
+	ou vers une page précise, ou vers un site externe) il faut les écrire en dur dans le squelette du menu.
+
+	Le but du plugin {{Menus}} est donc de permettre de facilement élaborer des menus au moyen d\'une interface conviviale, directement dans la partie privée.
+	{{Attention !}} Ce plugin ne s\'occupe pas de la manière dont seront affichés les menus. Il permet de les créer facilement et d\'en générer le code HTML.', # NEW
+	'menus_nom' => 'القوائم',
+	'menus_slogan' => 'إنشاء قوائم شخصية',
+	'menus_titre' => 'القوائم'
+);
+
+?>
