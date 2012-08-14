a5d04d82fa1eda9f8e76dec5339e24a9d098ec6a
toutati@free.fr <toutati@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-08-14 10:11:18 +0000

    Permettre d'entrer un identifiant ou une class sans avoir à toucher au squelettes, et par defaut on reste sur .accordeon pour ne pas perdre la compatibilité.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@64792 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- menu_accordeon/branches/accordeon2/lang/accordeon_fr.php
+++ menu_accordeon/branches/accordeon2/lang/accordeon_fr.php
@@ -8,9 +8,11 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'cfg_titre_accordeon' => 'Menu jQuery Accordéon',
 	
 	// E
+	'explication_identifiant'=>'.class ou #identifiant de votre menu, par défaut .accordeon',
 	'explication_options' => 'Vous pouvez mettre ici les options à passer au script d\'effet accordéon. <a href="http://jqueryui.com/demos/accordion/">Voir la documentation d\'accordéon</a>. Ne pas mettre les accolades. Saisir par exemple <br /><strong>collapsible: true, active: $(".accordeon > li.on > a,.accordeon > li:first-child > a").last()</strong><br /> pour ouvrir le menu de classe accordeon sur l\'élément courant ou sinon sur le premier du DOM',
 
 	// L
+	'label_identifiant'=>'Identifiant du menu',
 	'label_options' => 'Animation',
 	
 	// T
