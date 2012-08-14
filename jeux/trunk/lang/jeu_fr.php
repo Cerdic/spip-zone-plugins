33d1b4834925f0edf4e4500199a5254b6f51e464
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-08-14 01:40:08 +0000

    langues (jeu)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@64780 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- jeux/trunk/lang/jeu_fr.php
+++ jeux/trunk/lang/jeu_fr.php
@@ -1,28 +1,35 @@
 <?php
-/** Fichier de langue de SPIP **/
-if (!defined("_ECRIRE_INC_VERSION")) return;
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/jeux/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-    'info_1_jeu'               =>'Un jeu',
-    'info_1_resultat'           =>'Un résultat',
-    'icone_creer_jeu'          =>'Créer un jeu',
-    'icone_modifier_jeu'       =>'Modifier le jeu',
-    'info_aucun_jeu'            =>'Aucun jeu',
-    'info_aucun_resultat'       =>'Aucun résultat',
-    'info_nb_jeux'              =>'@nb@ jeux',
-    'info_nb_resultats'         =>'@nb@ résultats',
-    'info_texte'                =>'Texte',
-    'info_type_resultat'        =>'Type de résultat',
-    'label_date'                =>'Date',
-    'label_nom'                 =>'Auteur',
-    'label_resultat'            =>'Résultat',
-    'label_texte'               =>'Texte',
-    'label_titre_prive'         =>'Titre privé',
-    'label_type_jeu'            =>'Type de jeu',
-    'label_type_resultat'       =>'Stockage des résultats',
-    'texte_changer_statut_jeu'  =>'Le statut de ce jeu est :',
-    'titre_jeux'               =>'Jeux',
-    'titre_logo_jeu'           =>'Logo du jeu'
+
+	// I
+	'icone_creer_jeu' => 'Créer un jeu',
+	'icone_modifier_jeu' => 'Modifier le jeu',
+	'info_1_jeu' => 'Un jeu',
+	'info_1_resultat' => 'Un résultat',
+	'info_aucun_jeu' => 'Aucun jeu',
+	'info_aucun_resultat' => 'Aucun résultat',
+	'info_nb_jeux' => '@nb@ jeux',
+	'info_nb_resultats' => '@nb@ résultats',
+	'info_texte' => 'Texte',
+	'info_type_resultat' => 'Type de résultat',
+
+	// L
+	'label_date' => 'Date',
+	'label_nom' => 'Auteur',
+	'label_resultat' => 'Résultat',
+	'label_texte' => 'Texte',
+	'label_titre_prive' => 'Titre privé',
+	'label_type_jeu' => 'Type de jeu',
+	'label_type_resultat' => 'Stockage des résultats',
+
+	// T
+	'texte_changer_statut_jeu' => 'Le statut de ce jeu est :',
+	'titre_jeux' => 'Jeux',
+	'titre_logo_jeu' => 'Logo du jeu'
 );
 
 ?>
