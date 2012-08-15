2b198299d4e46b9478c72519269ec83a8dadb2a1
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-08-15 12:20:35 +0000

    une constante pour indiquer le maximum de zip effacable d'un coup
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@64837 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- zippeur/trunk/genie/zippeur_effacer_zip.php
+++ zippeur/trunk/genie/zippeur_effacer_zip.php
@@ -3,7 +3,7 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 function genie_zippeur_effacer_zip_dist($t){
 	// recherche dans la bdd 
 	
-	$info = sql_select("id_zip,nom",'spip_zippeur','date_modif +INTERVAL delai_suppression SECOND < NOW() AND delai_suppression > 0','0,1'); 
+	$info = sql_select("id_zip,nom",'spip_zippeur','date_modif +INTERVAL delai_suppression SECOND < NOW() AND delai_suppression > 0','0,'._ZIPPEUR_MAX_EFFACER_ZIP); 
 	while ($ligne = sql_fetch($info) ){
 		defined('_DIR_SITE') ? $chemin = _DIR_SITE._NOM_TEMPORAIRES_ACCESSIBLES.'cache-zip/'.$ligne['nom'].".zip" : $chemin = _DIR_RACINE._NOM_TEMPORAIRES_ACCESSIBLES.'cache-zip/'.$ligne['nom'].".zip";
 		var_dump($chemin);
