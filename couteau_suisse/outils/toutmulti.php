6fd7a0edcd12c27b028397ce7965c81c68417e9a
patfr@ifrance.com <patfr@ifrance.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-08-15 13:55:14 +0000

    Blocs multilingues : utilisation de la constante MODULES_IDIOMES de SPIP 3.0
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@64844 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- couteau_suisse/outils/toutmulti.php
+++ couteau_suisse/outils/toutmulti.php
@@ -7,15 +7,15 @@
   <:chaine{argument1=un texte, argument2=un autre texte}:>
 */
 
+
 function ToutMulti_rempl($texte) {
-	if(!defined('CS_MODULES_CORE') || !defined('CS_BALISE_IDIOMES')) {
-		// expression tiree du code de SPIP 2.0 et 3.0 : ecrire/public/phraser_html.php
-		// les filtres ont ete retires ; on pourrait ignorer les modules et laisser SPIP se debrouiller...
-		@define('CS_BALISE_IDIOMES',',<:(([a-z0-9_]+):)?([a-z0-9_:]+)({([^=>]*=[^>]*)})?:>,iS');
-		// modification du separateur des listes de modules a consulter sous SPIP 3
-		// on pourrait mettre cette constante a '' pour laisser SPIP se debrouiller...
-		@define('CS_MODULES_CORE', defined('_SPIP30000')?'spip|ecrire|public:':'spip/ecrire/public:');
-	}
+	// expression tiree du code de SPIP 2.0 et 3.0 : ecrire/public/phraser_html.php
+	// les filtres ont ete retires
+	if (!defined('CS_BALISE_IDIOMES'))
+		define('CS_BALISE_IDIOMES',',<:(([a-z0-9_]+):)?([a-z0-9_:]+)({([^=>]*=[^>]*)})?:>,iS');
+	// separateur des listes de modules a consulter (MODULES_IDIOMES est une constante definie sous SPIP 3.0)
+	if (!defined('MODULES_IDIOMES'))
+			define('MODULES_IDIOMES', defined('_SPIP30000')?'public|spip|ecrire':'spip/ecrire/public');
 	if (preg_match_all(CS_BALISE_IDIOMES, $texte, $matches, PREG_SET_ORDER)) {
 		foreach ($matches as $m) {
 			// Stocker les arguments de la balise de traduction
@@ -24,7 +24,7 @@ function ToutMulti_rempl($texte) {
 				$arg = explode('=', $val);
 				if (strlen($key = trim($arg[0]))) $args[$key] = trim($arg[1]);	
 			}
-			$texte = str_replace($m[0], _T((strlen($m[1])?$m[1]:CS_MODULES_CORE).$m[3], $args), $texte);
+			$texte = str_replace($m[0], _T((strlen($m[1])?$m[1]:(MODULES_IDIOMES.':')).$m[3], $args), $texte);
 		}
 	}
 	return $texte;
