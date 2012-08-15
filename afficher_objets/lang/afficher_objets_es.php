9cdb18c34c4ceb09c6610149842d420e97bff23b
dani@belvil.net <dani@belvil.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-08-15 01:37:57 +0000

    langues (afficher_objets)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@64821 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ afficher_objets/lang/afficher_objets_es.php
@@ -0,0 +1,20 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/afficher_objets?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// I
+	'info_statut' => 'Estatuto',
+
+	// N
+	'nb_articles' => '@nb@ artículos',
+
+	// R
+	'rang' => 'Rango'
+);
+
+?>
