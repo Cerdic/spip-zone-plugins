be78ba14e75cc74126da02e5d46a9821ab4c4178
dani@belvil.net <dani@belvil.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-08-15 01:37:58 +0000

    langues (paquet-auteurs_syndic)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@64822 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ auteurs_syndic/trunk/lang/paquet-auteurs_syndic_es.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-auteurs_syndic?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'auteurs_syndic_description' => 'Añade la posibilidad de agregar autores a los sitios sindicados',
+	'auteurs_syndic_nom' => 'Autores para los sitios sindicados',
+	'auteurs_syndic_slogan' => 'Añade la posibilidad de agregar autores a los sitios sindicados'
+);
+
+?>
