8a5025e7e8bb298e0f35148680830d91069f26ef
serge.markitanenko@gmail.com <serge.markitanenko@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-08-16 01:40:48 +0000

    langues (crayons)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@64868 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- crayons/lang/crayons_ru.php
+++ crayons/lang/crayons_ru.php
@@ -1,77 +1,87 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// extrait automatiquement de http://www.spip.net/trad-lang/
+// extrait automatiquement de http://trad.spip.net/tradlang_module/crayons?lang_cible=ru
 // ** ne pas modifier le fichier **
 
-if (!defined("_ECRIRE_INC_VERSION")) return;
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
-	'activation_barre_impossible' => 'Un autre plugin interdit l\'usage du porte-plume dans l\'espace public.', # NEW
-	'annuler' => '&#1054;&#1090;&#1084;&#1077;&#1085;&#1072;',
+	'activation_barre_impossible' => 'Другой плагин не дайет использовать porte-plume на сайте.',
+	'annuler' => 'Отмена',
 
 	// B
-	'bouton_supprimer' => 'Supprimer', # NEW
+	'bouton_supprimer' => 'Удалить',
 
 	// D
-	'deja_modifie' => '&#1059;&#1078;&#1077; &#1080;&#1079;&#1084;&#1077;&#1085;&#1077;&#1085;',
-	'donnees_mal_formatees' => '&#1053;&#1077;&#1087;&#1088;&#1072;&#1074;&#1080;&#1083;&#1100;&#1085;&#1099;&#1081; &#1092;&#1086;&#1088;&#1084;&#1072;&#1090; &#1079;&#1072;&#1087;&#1088;&#1086;&#1089;&#1072;',
+	'deja_modifie' => 'Уже изменен',
+	'donnees_mal_formatees' => 'Неправильный формат запроса',
 
 	// E
-	'editer' => '&#1056;&#1077;&#1076;&#1072;&#1082;&#1090;&#1080;&#1088;&#1086;&#1074;&#1072;&#1090;&#1100;',
-	'editer_@type@_@id@' => '&#1056;&#1077;&#1076;&#1072;&#1082;&#1090;&#1080;&#1088;&#1086;&#1074;&#1072;&#1090;&#1100; @type@ @id@ &#1087;&#1086;&#1083;&#1085;&#1086;&#1089;&#1090;&#1100;&#1102;',
-	'editer_tout' => '&#1056;&#1077;&#1076;&#1072;&#1082;&#1090;&#1080;&#1088;&#1086;&#1074;&#1072;&#1090;&#1100; &#1087;&#1086;&#1083;&#1085;&#1086;&#1089;&#1090;&#1100;&#1102;',
-	'effets' => 'Effets', # NEW
-	'explication_exec_autorise' => 'Pages autoris&eacute;es &agrave; la modification
-		(?exec=...) : (, pour s&eacute;parer les exec).', # NEW
-	'explication_reduire_logo' => 'R&#233;duire le logo s\'il d&#233;passe
-		la taille maximale indiquee en pixels. Exemple : 100. (0 = jamais)', # NEW
+	'editer' => 'Редактировать',
+	'editer_@type@_@id@' => 'Редактировать @type@ @id@ полностью',
+	'editer_tout' => 'Редактировать полностью',
+	'effets' => 'Эффекты',
+	'explication_exec_autorise' => 'Страницы, на которых можно редактировать информацию   (?exec=...) : (, разделитель между страницами. * для всех страниц).',
+	'explication_introduction_article' => 'Вводная статьи выводит информацию из поля "введение". Если оно пустое, товыводится первые 600 символов текста статьи.',
+	'explication_reduire_logo' => 'Уменьшать логотипы если их размер превышает 
+  максимальный размер (в пикселях) Например: 100. (0 = никогда)',
+
+	// I
+	'info_experimental' => 'Экспериментальная опция',
 
 	// L
-	'label_activer_barre_typo' => 'Activer la barre typographique dans les crayons ?', # NEW
-	'label_activer_crayons_prive' => 'Activer crayons pour la partie priv&eacute;e.', # NEW
-	'label_barre_typo' => 'Barre typographique', # NEW
-	'label_clickhide' => 'Un clic en-dehors masque les crayons', # NEW
-	'label_effets' => 'Choix des effets', # NEW
-	'label_espaceprive' => 'Crayons dans le priv&eacute; ?', # NEW
-	'label_exec_autorise' => 'Pages autoris&eacute;es', # NEW
-	'label_filet' => 'Filet', # NEW
-	'label_message' => 'Obtenir un message', # NEW
-	'label_msg_abandon' => 'Sur abandon', # NEW
-	'label_msg_no_change' => 'Si pas de changement', # NEW
-	'label_reduire_logo' => 'R&eacute;duire les logos', # NEW
-	'label_yellow_fade' => 'Fade', # NEW
-	'legend_controleur_logo' => 'Choisir un logo&nbsp;:', # NEW
-	'legend_controleur_vignette' => 'Choisir une vignette&nbsp;:', # NEW
-	'legend_editer_prive' => '&Eacute;diter l\'espace priv&eacute; (exp&eacute;rimental)', # NEW
+	'label_activer_barre_typo' => 'Включить панель инструментов для плагина?',
+	'label_activer_crayons_prive' => 'Включить "Карандаш" в админке сайта',
+	'label_barre_typo' => 'Панель инструментов',
+	'label_clickhide' => 'Нажмите вне области редактирования чтобы закрыть экран',
+	'label_effets' => 'Выбрать эффект',
+	'label_espaceprive' => 'Использовать "Карандаш" в админке сайта?',
+	'label_exec_autorise' => 'Использовать на страницах',
+	'label_filet' => 'Линия',
+	'label_message' => 'Показать сообщение',
+	'label_msg_abandon' => 'При отмене',
+	'label_msg_no_change' => 'Если не были сделаны изменения',
+	'label_reduire_logo' => 'Уменьшать логотипы',
+	'label_upload_document' => 'Разрешить добавлять документы перетаскиванием их поле "текст" статьи (article.texte)',
+	'label_yellow_fade' => 'Тень',
+	'legend_controleur_hyperlien' => 'Ссылка',
+	'legend_controleur_logo' => 'Выбрать логотип:',
+	'legend_controleur_logo_remplacer' => 'Заменить логотип',
+	'legend_controleur_qui' => 'Кто?',
+	'legend_controleur_vignette' => 'Выбрать миниатюру:',
+	'legend_controleur_vignette_remplacer' => 'Заменить миниатюру',
+	'legend_documents' => 'Загрузить документ',
+	'legend_editer_prive' => 'Использовать в админке сайта',
+	'legend_introduction_article' => 'Вводный текст для статьи',
 
 	// M
-	'message_image_obligatoire' => 'Il faut choisir une image', # NEW
-	'messages' => 'Messages', # NEW
-	'modifie_par_ailleurs' => '&#1041;&#1099;&#1083; &#1080;&#1079;&#1084;&#1077;&#1085;&#1077;&#1085; &#1074; &#1076;&#1088;&#1091;&#1075;&#1086;&#1084; &#1084;&#1077;&#1089;&#1090;&#1077; &#1074; &#1090;&#1086; &#1078;&#1077; &#1074;&#1088;&#1077;&#1084;&#1103;',
+	'message_image_obligatoire' => 'Вам необходимо выбрать сообщение',
+	'messages' => 'Сообщения',
+	'modifie_par_ailleurs' => 'Был изменен в другом месте в то же время',
 
 	// N
-	'non_autorise' => '&#1053;&#1077;&#1090; &#1076;&#1086;&#1089;&#1090;&#1091;&#1087;&#1072;',
-	'non_implemente' => '&#1053;&#1077; &#1074;&#1099;&#1087;&#1086;&#1083;&#1085;&#1077;&#1085;&#1086;',
+	'non_autorise' => 'Нет доступа',
+	'non_implemente' => 'Не выполнено',
 
 	// P
-	'pas_de_modification' => '&#1041;&#1077;&#1079; &#1080;&#1079;&#1084;&#1077;&#1085;&#1077;&#1085;&#1080;&#1081;',
-	'pas_de_valeur' => '&#1053;&#1077; &#1091;&#1082;&#1072;&#1079;&#1072;&#1085;&#1086; &#1079;&#1085;&#1072;&#1095;&#1077;&#1085;&#1080;&#1077; &#1076;&#1083;&#1103; &#1101;&#1090;&#1086;&#1075;&#1086; id',
+	'pas_de_modification' => 'Без изменений',
+	'pas_de_valeur' => 'Не указано значение для этого id',
 
 	// S
-	'sauvegarder' => '&#1057;&#1086;&#1093;&#1088;&#1072;&#1085;&#1080;&#1090;&#1100; &#1080;&#1079;&#1084;&#1077;&#1085;&#1077;&#1085;&#1080;&#1103;?',
-	'svp_copier_coller' => '&#1057;&#1086;&#1093;&#1088;&#1072;&#1085;&#1080;&#1090;&#1077; &#1089;&#1076;&#1077;&#1083;&#1072;&#1085;&#1085;&#1099;&#1077; &#1080;&#1079;&#1084;&#1077;&#1085;&#1077;&#1085;&#1080;&#1103; &#1074; &#1076;&#1088;&#1091;&#1075;&#1086;&#1084; &#1084;&#1077;&#1089;&#1090;&#1077; &#1080; &#1087;&#1086;&#1087;&#1088;&#1086;&#1073;&#1091;&#1081;&#1090;&#1077; &#1079;&#1072;&#1085;&#1086;&#1074;&#1086;',
+	'sauvegarder' => 'Сохранить изменения?',
+	'svp_copier_coller' => 'Сохраните сделанные изменения в другом месте и попробуйте заново',
 
 	// T
-	'titre_config_crayons' => 'Configurer les crayons', # NEW
-	'titre_crayons' => 'Crayons', # NEW
+	'titre_config_crayons' => 'Настроить "Карандаш"',
+	'titre_crayons' => 'Карандаш',
 
 	// U
-	'update_impossible' => 'La mise &agrave; jour a &eacute;chou&eacute;.', # NEW
+	'update_impossible' => 'Не удалось обновить.',
 
 	// V
-	'veuillez_patienter' => '&#1048;&#1076;&#1077;&#1090; &#1087;&#1086;&#1080;&#1089;&#1082; ...'
+	'veuillez_patienter' => 'Идет поиск ...'
 );
 
 ?>
