9d6b86aa03c6416d139a1cb52025691d287c13dd
suske@brubel.net <suske@brubel.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-08-17 01:39:57 +0000

    langues (paquet-ancresdouces)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@64907 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ ancres_douces/lang/paquet-ancresdouces_nl.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-ancresdouces?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'ancresdouces_description' => 'Deze plugin maakt een zachte en continu scrol in de  pagina als de bezoeker op een link naar een anker van het artikel klikt.',
+	'ancresdouces_nom' => 'Zachte ankers',
+	'ancresdouces_slogan' => 'Zachte scrol naar de ankers'
+);
+
+?>
