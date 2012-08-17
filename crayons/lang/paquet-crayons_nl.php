da69b5cd673eb6da844d24ddfc85e6623dd9c9fe
suske@brubel.net <suske@brubel.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-08-17 01:40:05 +0000

    langues (paquet-crayons)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@64910 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ crayons/lang/paquet-crayons_nl.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-crayons?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'crayons_description' => 'Snel bewerken van de publieke pagina\'s van de site.',
+	'crayons_nom' => 'Potloden',
+	'crayons_slogan' => 'Snel bewerken van de publieke pagina\'s van de site.'
+);
+
+?>
