768d81ba7a35e3a539447afc9443cc7547362ba8
serge.markitanenko@gmail.com <serge.markitanenko@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-08-20 01:39:39 +0000

    langues (paquet-mots_techniques)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@65007 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ mots_techniques/trunk/lang/paquet-mots_techniques_ru.php
@@ -0,0 +1,17 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-mots_techniques?lang_cible=ru
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// M
+	'mots_techniques_description' => 'Плагин позволяет выделять служебные группы ключей - это ключи, которые задают функционал сайта и не несут какой-либо информационной нагрузки. По умолчанию циклы MOTS и GROUPES_MOTS не будут выводить служебные ключи. В тоже время критерий  <code>{tout}</code> будет выводить информацию о всех ключах, а критерий <code>{technique=oui}</code> 
+				будет выводить только служебные ключи.',
+	'mots_techniques_slogan' => 'Позволяет выделять группу ключей для служебных целей',
+	'mots_techniques_titre' => 'Служебные ключи (Mots techniques)'
+);
+
+?>
