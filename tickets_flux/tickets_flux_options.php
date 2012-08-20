64a896a065a6cb258f5a0127a9ed2de162bb33f4
cyril@cym.fr <cyril@cym.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-08-20 14:54:45 +0000

    Pour générer un flux des tickets en cours sur un site, et pour centraliser ces flux provenant de multiples sites.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@65045 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ tickets_flux/tickets_flux_options.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Tickets Flux
+ *
+ */
+
+    // comment forcer un réglage SPIP dans mes_options ?
+    // quelle différence entre globals et define ? pareil oubien ?
+//    define('_SYNDICATION_INTEGRALE', 'oui');
+//    $GLOBALS['spip_syndication_integrale'] = "oui";
+
+$syndication_integrale = "oui";
+
+?>
