18134fc1c32a6f2086ecd313f58fda84cff5f2d2
cyril@cym.fr <cyril@cym.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-08-20 17:27:11 +0000

    icone pour modifier le site suivi;
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@65058 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- tickets_flux/tickets_flux_options.php
+++ tickets_flux/tickets_flux_options.php
@@ -4,11 +4,6 @@
  *
  */
 
-    // comment forcer un réglage SPIP dans mes_options ?
-    // quelle différence entre globals et define ? pareil oubien ?
-//    define('_SYNDICATION_INTEGRALE', 'oui');
-//    $GLOBALS['spip_syndication_integrale'] = "oui";
-
 $syndication_integrale = "oui";
 
 ?>
