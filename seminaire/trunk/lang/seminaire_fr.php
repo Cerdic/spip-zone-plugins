378d97b35bed51293e8d7d49c127e8dc5c9491be
amaury.adon@laposte.net <amaury.adon@laposte.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-08-22 16:16:56 +0000

    diverses corrections de pétouilles tant esthétiques que javascript
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@65125 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- seminaire/trunk/lang/seminaire_fr.php
+++ seminaire/trunk/lang/seminaire_fr.php
@@ -5,52 +5,63 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
  
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-	// S
-	'seminaire_titre' => 'Séminaire',
+	// A
+	'attention_mot_cle'			 =>		'Il faut IMPERATIVEMENT mettre un mot cl&eacute; sur l&rsquo;&eacute;v&eacute;nement',
+	'abstract'					 => 	'R&eacute;sum&eacute;',
+	'apreciser'					 =>		'A pr&eacute;ciser',
+	'aujour'					 =>		'Les &eacute;v&eacute;nements de la journ&eacute;e',
+	'a_venir'					 =>		'Les prochains s&eacute;minaires',
+	'attention_type'			 =>		'Le choix du type d&rsquo;&eacute;v&eacute;nement d&eacute;termine le tri qui lui sera appliqu&eacute;.',
+	'abonnement'				 =>		'S&rsquo;abonner au calendrier',
 
 	// C
 	'cfg_exemple' => 'Exemple',
 	'cfg_exemple_explication' => 'Explication de cet exemple',
 	'cfg_titre_parametrages' => 'Paramétrages',
+	'choix_mot'					 =>		'Type d&rsquo;&eacute;v&eacute;nement',
+	'creer_evenement'			 =>		'Ajouter un &eacute;v&eacute;nement', 
 
-	// T
-	'titre_page_configurer_seminaire' => 'seminaire',
-	'titre_evenement'			 =>		'Titre à préciser',
-	
-		//pour le formulaire de s&eacute;minaires
+	// E
+	'erreur_install_groupe_categories' => 'erreur &agrave; la cr&eacute;ation du groupe',
 	'evenement_titre'			 =>		'Titre de l&rsquo;expos&eacute;',
-	'name'						 => 	'Nom de l&rsquo;intervenant',
-	'attention_mot_cle'			 =>		'Il faut IMPERATIVEMENT mettre un mot cl&eacute; sur l&rsquo;&eacute;v&eacute;nement',
-	'origin'					 => 	'Institut d&rsquo;origine de l&rsquo;intervenant',
-	'abstract'					 => 	'R&eacute;sum&eacute;',
-	'notes'						 =>		'Notes de dernières minutes',
+	'evenements_depuis_debut'	 =>		'Archives',	
+	'exemple'					 =>		'Par exemple : CMI - Salle R164',
+
+	// L
+	'lien_tout_replier'			 =>		'Sans r&eacute;sum&eacute;',
+	'lien_tout_deplier'			 =>		'Avec r&eacute;sum&eacute;',
 	'lieu'						 =>		'Lieu',
+
+	// M
+	'mots_categorie_kitcnrs'	 => 	'Les mots cl&eacute;s à affecter aux articles pour que les &eacute;v&eacute;nements soient encore mieux tri&eacute;s',
+	'mots_cles_categories'		 =>		'Les diff&eacute;rentes cat&eacute;gories de s&eacute;minaires, groupes de travail et &eacute;v&eacute;nements exceptionnels',
+	'mots_cles_techniques_kitcnrs'=>	'Les mots cl&eacute;s pour rep&eacute;rer plus finement les diff&eacute;rents types d&rsquo;&eacute;v&eacute;nements',
+
+	// N
+	'name'						 => 	'Nom de l&rsquo;intervenant',
+	'nom_de_l_intervenant'		 =>		'Intervenant &agrave; pr&eacute;ciser',
+	'notes'						 =>		'Notes de dernières minutes',
+
+	// O
+	'origin'					 => 	'Institut d&rsquo;origine de l&rsquo;intervenant',
+	'oubli_titre'				 =>		'Ne pas oublier de mettre le titre de l&rsquo;&eacute;v&eacute;nement',
+
+
+	// P
+	'plus'						 =>		'En savoir plus',
 	'precisions_name'			 =>		'le nom sera affich&eacute; dans le calendrier et dans le mini calendrier',
 	'precisions_origin'			 =>		'N&rsquo;apparaît que dans les d&eacute;tails de l&rsquo;&eacute;v&eacute;nement',
 	'precisions_abstract'		 =>		'pour mettre du code LATEX, ajoutez les balises <math> </math> en d&eacute;but et fin de texte (ex :  <math>La fraction $\frac{1}{2}$</math>)',
 	'precisions_notes'			 =>		'N&rsquo;apparaît que dans les d&eacute;tails de l&rsquo;&eacute;v&eacute;nement',
-	'apreciser'					 =>		'A pr&eacute;ciser',
-	'plus'						 =>		'En savoir plus',
-	'a_venir'					 =>		'Les prochains s&eacute;minaires',
-	'choix_mot'					 =>		'Type d&rsquo;&eacute;v&eacute;nement',
-	'attention_type'			 =>		'Le choix du type d&rsquo;&eacute;v&eacute;nement d&eacute;termine le tri qui lui sera appliqu&eacute;.',
-	'oubli_titre'				 =>		'Ne pas oublier de mettre le titre de l&rsquo;&eacute;v&eacute;nement',
-	'exemple'					 =>		'Par exemple : CMI - Salle R164',
-	'aujour'					 =>		'Les &eacute;v&eacute;nements de la journ&eacute;e',
-	'lien_tout_replier'			 =>		'Sans r&eacute;sum&eacute;',
-	'lien_tout_deplier'			 =>		'Avec r&eacute;sum&eacute;',
-	'titre_agenda'				 =>		'Agenda Scientifique',
-	'nom_de_l_intervenant'		 =>		'Intervenant &agrave; pr&eacute;ciser',
-	//pour les groupes de mots cl&eacute;s
-	'mots_categorie_kitcnrs'	 => 	'Les mots cl&eacute;s à affecter aux articles pour que les &eacute;v&eacute;nements soient encore mieux tri&eacute;s',
-	'mots_cles_techniques_kitcnrs'=>	'Les mots cl&eacute;s pour rep&eacute;rer plus finement les diff&eacute;rents types d&rsquo;&eacute;v&eacute;nements',
-	'type'						 =>		'Type d&rsquo;&eacute;v&eacute;nement',
-	'evenements_depuis_debut'	 =>		'Archives',	
-	'creer_evenement'			 =>		'Ajouter un &eacute;v&eacute;nement', 
-	'erreur_install_groupe_categories' => 'erreur &agrave; la cr&eacute;ation du groupe',
-	'mots_cles_categories'		 =>		'Les diff&eacute;rentes cat&eacute;gories de s&eacute;minaires, groupes de travail et &eacute;v&eacute;nements exceptionnels',
-	'abonnement'				 =>		'S&rsquo;abonner au calendrier',
 
+	// S
+	'seminaire_titre' => 'Séminaire',
+
+	// T
+	'titre_agenda'				 =>		'Agenda Scientifique',
+	'titre_evenement'			 =>		'Titre à préciser',
+	'titre_page_configurer_seminaire' => 'seminaire',
+	'type'						 =>		'Type d&rsquo;&eacute;v&eacute;nement',
 );
 
 ?>
