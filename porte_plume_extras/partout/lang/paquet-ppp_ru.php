cd76972f5f1d5d8bf195ba84da52ada6999c1f01
serge.markitanenko@gmail.com <serge.markitanenko@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-08-24 21:13:22 +0000

    langues (paquet-ppp)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@65183 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ porte_plume_extras/partout/lang/paquet-ppp_ru.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-ppp?lang_cible=ru
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// P
+	'ppp_description' => 'Добавление панели инструментов для форматирования текста на все основные поля сайта (chapo, post script, descriptif)',
+	'ppp_nom' => 'Панель инструментов для всех полей сайта (Porte Plume Partout)',
+	'ppp_slogan' => 'Добавление панели инструментов для форматирования текста на все основные поля сайта.'
+);
+
+?>
