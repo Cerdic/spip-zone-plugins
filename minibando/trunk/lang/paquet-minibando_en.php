8c366894754101d15d264b931c6a6ee088b9f0d8
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-08-24 21:13:19 +0000

    langues (paquet-minibando)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@65181 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ minibando/trunk/lang/paquet-minibando_en.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-minibando?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// M
+	'minibando_description' => 'The minibando plugin replaces the administration buttons of SPIP by a toolbar that shows the items available in the top navigation of the SPIP private area.',
+	'minibando_slogan' => 'A minibando for a maximum of functionalities!'
+);
+
+?>
