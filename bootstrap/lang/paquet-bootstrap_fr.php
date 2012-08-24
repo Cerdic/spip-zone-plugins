7440a0082253c0dc479a237928f48951fc5f6fe1
pierre.basson@gmail.com <pierre.basson@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-08-24 11:38:55 +0000

    plugin twitter bootstrap pour spip
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@65158 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ bootstrap/lang/paquet-bootstrap_fr.php
@@ -0,0 +1,13 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	'bootstrap_description' => '',
+	'bootstrap_nom' => 'Twitter Bootstrap',
+	'bootstrap_slogan' => 'Version 2.1.0',
+);
+
+?>
