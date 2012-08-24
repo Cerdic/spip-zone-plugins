08eda3f4b21f2230964279a94d0b0f786416fde3
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-08-24 21:13:18 +0000

    langues (recommander)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@65180 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ recommander/lang/recommander_sk.php
@@ -0,0 +1,20 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/recommander?lang_cible=sk
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// D
+	'destinataire' => 'Príjemca',
+
+	// R
+	'recommander' => 'Odporučiť túto stránku',
+	'recommander_lecture' => 'Dobrý deň, @from@ Vám odporúča prečítať si túto stránku: ',
+	'recommander_message' => 'Odporučiť',
+	'recommander_titre' => 'Prečítajte si na @nom_site@ --'
+);
+
+?>
