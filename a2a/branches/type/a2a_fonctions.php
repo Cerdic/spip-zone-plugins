67f98591d2e23206738d95716ab7f417484e0b35
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-08-24 22:25:05 +0000

    #ARTICLES_LIES prend en 2ème argument le type de liaison
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@65194 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- a2a/branches/type/a2a_fonctions.php
+++ a2a/branches/type/a2a_fonctions.php
@@ -5,16 +5,20 @@ function a2a_traduire_type_liaisons($type){
 	$types_liaisons = lire_config('a2a/types_liaisons');
 	return _T($types_liaisons[$type]);	
 }
-function lister_articles_lies($id_article, $ordre){
-	return sql_allfetsel('id_article_lie','spip_articles_lies','id_article=' . sql_quote($id_article),'',"rang $ordre");
+function lister_articles_lies($id_article, $ordren,$type_liaison=null){
+	if ($type_liaison==null)
+		return sql_allfetsel('id_article_lie','spip_articles_lies','id_article=' . sql_quote($id_article),'',"rang $ordre");
+	else
+		return sql_allfetsel('id_article_lie','spip_articles_lies','id_article=' . sql_quote($id_article) . ' AND type_liaison=' . sql_quote($type_liaison),'',"rang $ordre");
 }
 
-function balise_ARTICLES_LIES($p) {
+function balise_ARTICLES_LIES($p) { 
 	$id_article = champ_sql('id_article', $p);
 	$ordre = 'ASC';
+	$type_liaison=interprete_argument_balise(2,$p);
 	if($inverse = interprete_argument_balise(1,$p))
 		$ordre = 'DESC';
-	$p->code = "lister_articles_lies($id_article, $ordre)";
+	$type_liaison ? $p->code = "lister_articles_lies($id_article, $ordre,$type_liaison)" : $p->code = "lister_articles_lies($id_article, $ordre)";
 	$p->type = 'php';  
 	return $p;
 }
