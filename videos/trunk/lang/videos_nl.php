08733a87f27146f5b44e14ffed5475831d8b3b8e
suske@brubel.net <suske@brubel.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-08-25 01:40:22 +0000

    langues (videos)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@65202 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ videos/trunk/lang/videos_nl.php
@@ -0,0 +1,46 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/videos?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cfg_description_doc' => 'Documentatie',
+	'cfg_description_titre' => 'Configuratie va, de Video(s) plugin',
+	'cfg_explication_hauteur' => 'Choisissez la hauteur par défaut pour vos vidéos. Cette option reste surchargeable dans l\'appel au modèle.', # NEW
+	'cfg_explication_largeur' => 'Choisissez la largeur par défaut pour vos vidéos. Cette option reste surchargeable dans l\'appel au modèle.', # NEW
+	'cfg_explication_liste_definition' => 'Encapsuler la vidéo dans un dl/dt/dd comme peut le faire SPIP pour sa gestion de document.', # NEW
+	'cfg_explication_titre_descriptif' => 'Afficher le titre et la description des vidéos. Cette option reste surchargeable dans l\'appel au modèle.', # NEW
+	'cfg_explication_wmode' => 'Kies een mode te gebruiken voor de Flash objecten (fallback Flowplayer, iFrames Dailymotion, Youtube, Vimeo).',
+	'cfg_label_hauteur' => 'Standaard hoogte',
+	'cfg_label_largeur' => 'Standaard breedte',
+	'cfg_label_liste_definition' => 'HTML definitie lijst ',
+	'cfg_label_titre_descriptif' => 'Titel en beschrijving ',
+	'cfg_label_wmode' => 'Standaard Wmode',
+	'cfg_titre_configurations_communes' => 'Algemene configuratie',
+	'cfg_titre_configurations_dailymotion' => 'Dailymotion configuratie ',
+	'cfg_titre_configurations_html5' => 'HTML5   configuratie / Alternative',
+	'cfg_titre_configurations_vimeo' => 'Viméo configuratie',
+	'cfg_titre_configurations_youtube' => 'Youtube configuratie ',
+	'confirmation_ajout' => 'La vidéo "@type@:@titre@" a bien été ajoutée.', # NEW
+
+	// E
+	'erreur_adresse_invalide' => 'Cette adresse n\'est pas valide. Si vous tentez d\'importer un fichier vidéo (MP4 ou autre), merci d\'utiliser l\'ajout de document classique de SPIP.', # NEW
+	'explication_ajouter_video' => '(vidéo distante YouTube, Vimeo, DailyMotion)', # NEW
+
+	// L
+	'label_ajouter_autre_video' => 'Ajouter une autre vidéo', # NEW
+	'label_ajouter_video' => 'Een video bijvoegen',
+	'label_url' => 'Adresse de la vidéo', # NEW
+
+	// T
+	'titre_admin' => 'Plugin Vidéo(s)', # NEW
+
+	// V
+	'valider' => 'De video bijvoegen'
+);
+
+?>
