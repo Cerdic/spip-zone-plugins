67b7bdc7be742eea1d1825a7618e4b8128a347d4
gouz@root-me.org <gouz@root-me.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-08-25 10:31:11 +0000

    Ajout de paquet.xml
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@65222 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ messagerie/trunk/lang/paquet-messagerie_fr.php
@@ -0,0 +1,19 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-messagerie
+// Langue: fr
+// Date: 25-08-2012 12:18:29
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// M
+	'messagerie_description' => 'Gestion de la messagerie interne entre auteurs dans les squelettes publics.',
+	'messagerie_slogan' => 'une messagerie interne',
+);
+?>
