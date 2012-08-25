3a970923c524039091fd9cedb3be9d7a7b6379e9
piero.wbmstr@gmail.com <piero.wbmstr@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-08-25 17:07:28 +0000

    Ajout de " />" au lieu de " >" en fin des metas du plugin (conformité XHMLT) - merci JS pour le retour
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@65276 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spip_proprietaire/spip_3/spip_proprio_pipelines.php
+++ spip_proprietaire/spip_3/spip_proprio_pipelines.php
@@ -9,11 +9,11 @@ function spip_proprio_insert_head($flux){
 	$proprietaire = spip_proprio_recuperer_config();
 	$flux .= "<!-- SpipProprio insert head -->";
 	if (!empty($proprietaire['proprietaire_nom']))
-		$flux .= "\n<meta name=\"copyright\" content=\"".textebrut($proprietaire['proprietaire_nom'])."\">";
+		$flux .= "\n<meta name=\"copyright\" content=\"".textebrut($proprietaire['proprietaire_nom'])."\" />";
 	if (!empty($proprietaire['createur_nom']) || !empty($proprietaire['proprietaire_nom']))
-		$flux .= "\n<meta name=\"author\" content=\"".textebrut(!empty($proprietaire['createur_nom']) ? $proprietaire['createur_nom'] : $proprietaire['proprietaire_nom'])."\">";
+		$flux .= "\n<meta name=\"author\" content=\"".textebrut(!empty($proprietaire['createur_nom']) ? $proprietaire['createur_nom'] : $proprietaire['proprietaire_nom'])."\" />";
 	if (!empty($proprietaire['proprietaire_mail_administratif']))
-		$flux .= "\n<meta name=\"reply-to\" content=\"".$proprietaire['proprietaire_mail_administratif']."\">";
+		$flux .= "\n<meta name=\"reply-to\" content=\"".$proprietaire['proprietaire_mail_administratif']."\" />";
 	$flux .= "<!-- END ADX Menu insert head -->\n";
 	return $flux;
 }
