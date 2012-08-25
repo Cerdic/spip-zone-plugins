4ff51186b5a821e1d5384aea0441ca56507c4f23
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-08-25 09:22:13 +0000

    Devrait régler : "Ticket #9 : Accès à une traduction d’un module" cf : http://trad.spip.net/ticket/acces-a-une-traduction-d-un-module
    On ne referme pas le sélecteur de module après changement d'id_module
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@65210 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- trad-lang/trad-lang_spip2/javascript/tradlang.js
+++ trad-lang/trad-lang_spip2/javascript/tradlang.js
@@ -1,5 +1,5 @@
 var tradlang_switchers = function(){
-	$('#formulaire_switcher_langue .fieldset ul,#formulaire_switcher_langue .boutons,.formulaire_choisir_module .fieldset ul,.formulaire_choisir_module .boutons,table.info.trads tbody').hide();
+	$('#formulaire_switcher_langue .fieldset ul,#formulaire_switcher_langue .boutons,div.tradlang_choisir_module:not(".module_changed") .formulaire_choisir_module .fieldset ul,div.tradlang_choisir_module:not(".module_changed") .formulaire_choisir_module .boutons,table.info.trads tbody').hide();
 	$('#formulaire_switcher_langue .fieldset h3,.formulaire_choisir_module .fieldset h3,table.info.trads caption').each(function(){
 		if($(this).find('a.legend_link').size() == 0){
 			$(this).wrapInner('<a href="#" class="legend_link"></a>');
