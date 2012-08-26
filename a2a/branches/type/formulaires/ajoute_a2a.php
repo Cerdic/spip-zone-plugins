2413db3c0ab19e5f2e1505bb4f9da461187ab281
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-08-26 17:56:09 +0000

    structure du formulaire d'ajout typé de relation
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@65312 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ a2a/branches/type/formulaires/ajoute_a2a.php
@@ -0,0 +1,38 @@
+<?php
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+// chargement des valeurs par defaut des champs du formulaire
+function formulaires_recherche_a2a_charger($id_article){
+	$recherche = _request('recherche');
+	$recherche_titre = _request('recherche_titre');
+	$type_liaison = _request('type_liaison');
+	$id_article_orig = $id_article;
+
+	return 
+		array(
+			'recherche' => $recherche,
+			'recherche_titre' => $recherche_titre,
+			'id_article_orig' => $id_article_orig,
+			'type_liaison' => $type_liaison,
+		);
+}
+
+function formulaires_recherche_a2a_verifier($id_article){
+	$nv_type_liaison=_request('type_liaison');
+	$types_liaions	= 	array_keys(lister_types_liaisons());
+	if ($nv_type_liaison){
+		if (!in_array($nv_type_liaison,$types_liaions)){
+			return array('message_erreur'=>_T('a2a:type_inexistant'));
+		}
+	}
+	elseif(lire_config('a2a/type_obligatoire')){
+		return array('message_erreur'=>_T('a2a:type_inexistant'));
+	}
+}
+
+function formulaires_recherche_a2a_traiter($id_article){
+	return true; // permettre d'editer encore le formulaire
+}
+
+?>
