575201968dd54d4d862e7409df7d54b83fc230df
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-08-26 17:56:15 +0000

    appel du formulaire de liaison entre article après le formulaire de recherche d'article
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@65316 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- a2a/branches/type/formulaires/recherche_a2a.php
+++ a2a/branches/type/formulaires/recherche_a2a.php
@@ -6,7 +6,6 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 function formulaires_recherche_a2a_charger($id_article){
 	$recherche = _request('recherche');
 	$recherche_titre = _request('recherche_titre');
-	$type_liaison = _request('type_liaison');
 	$id_article_orig = $id_article;
 
 	return 
@@ -14,21 +13,10 @@ function formulaires_recherche_a2a_charger($id_article){
 			'recherche' => $recherche,
 			'recherche_titre' => $recherche_titre,
 			'id_article_orig' => $id_article_orig,
-			'type_liaison' => $type_liaison,
 		);
 }
 
 function formulaires_recherche_a2a_verifier($id_article){
-	$nv_type_liaison=_request('type_liaison');
-	$types_liaions	= 	array_keys(lister_types_liaisons());
-	if ($nv_type_liaison){
-		if (!in_array($nv_type_liaison,$types_liaions)){
-			return array('message_erreur'=>_T('a2a:type_inexistant'));
-		}
-	}
-	elseif(lire_config('a2a/type_obligatoire')){
-		return array('message_erreur'=>_T('a2a:type_inexistant'));
-	}
 }
 
 function formulaires_recherche_a2a_traiter($id_article){
