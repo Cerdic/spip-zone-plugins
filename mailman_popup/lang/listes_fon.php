af15df8a8f9ee3d6837b1c3e9f68066a6397512d
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-08-26 01:41:09 +0000

    langues (listes)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@65293 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ mailman_popup/lang/listes_fon.php
@@ -0,0 +1,48 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/listes?lang_cible=fon
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'ajoutee' => 'nyikɔ &lt;@var_email@&gt ; na gɔna ɖiɖèmɛ ɔ &lt;@liste@&gt, nu mi vɔ lɛ kpɔn fo ɔ. Mi nu mi na yi gbè nu wɛn é sɛ ɖo mi é ',
+
+	// C
+	'confirm' => 'Yé ɖalɔwemamɛ bo  sɛ  ɖo &lt;@var_email@&gt;.',
+
+	// D
+	'deja' => '&lt;@var_email@&gt;: Mi ko na nyikɔ mi tɔn &lt;@liste@&gt;.',
+	'desabo' => 'ɖiɖetɔn sin agun ɔ mɛ é ko sɔ gbè',
+
+	// F
+	'fermer' => 'Su',
+
+	// I
+	'inscription' => 'nyikɔ ni na ɖo &lt;@liste@&gt;',
+
+	// M
+	'mail_removed' => 'nyikɔ <@var_email@> sunsun sin agun ɔ mɛ @liste@.Nu nɖé jɔ ɔ , kabi nu mi ma byɔ ɖiɖetɔn si mɛ a ,Mi kɛnklin bo wlan <@gân@>.O ɖabɔ, mi kuɖéwu.',
+	'merci' => 'mi kudewu.',
+
+	// P
+	'pasabo' => 'nyikɔ &lt;@var_emailu@&gt; é lo agun ɔ mɛ a  &lt;@liste@&gt;.',
+	'patientez' => 'Mi kɛnklin bo kuhun ',
+
+	// Q
+	'quitter' => 'ɖiɖetɔnsimɛ ',
+
+	// S
+	'subject_removed' => 'Yé sunsun nyikɔ mitɔn sin agun ɔ mɛ @liste@.',
+
+	// T
+	'titrefenetre' => 'nyikɔ ni na ',
+
+	// V
+	'veuillez' => 'Mi kɛnklɛn mi lɛ vɔ tɛɖɛ nyikɔ mi tɔn ji ',
+	'votreemail' => 'e-mailu mi tɔn '
+);
+
+?>
