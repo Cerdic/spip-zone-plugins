96cdef33e1edbb3714a42679fb9ba7c8d5d25826
patfr@ifrance.com <patfr@ifrance.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-08-27 02:39:19 +0000

    Plugin "Chess", tel que décrit dans la doc. + Broutilles typo
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@65339 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- chess/lang/paquet-chess_en.php
+++ chess/lang/paquet-chess_en.php
@@ -2,9 +2,9 @@
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	'chess_titre' => 'Chess',
-	'chess_slogan' => 'Allow to display chess games with pgn files',
-	'chess_description' => 'This plugin allows to read chess games on line with their pgn file.
-  To insert the widget you have just to write in your article the tag   &#60;chessXX&#62; where XX is the number of the article, then upload the pgn files.',
+	'chess_slogan' => 'Allow to display chess games with PGN files',
+	'chess_description' => 'This plugin allows to read chess games on line with their PGN file.
+  To insert the widget you have just to write in your article the tag &#60;chessXX&#62; where XX is the number of the article, then upload the PGN files.',
 
  );
 ?>
--- chess/lang/paquet-chess_fr.php
+++ chess/lang/paquet-chess_fr.php
@@ -1,7 +1,7 @@
 <?php
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-	'chess_titre' => 'Echecs',
+	'chess_titre' => 'Chess (Echecs)',
 	'chess_slogan' => 'Pour afficher des parties d\'&#233;checs au format PGN',
 	'chess_description' => 'Ce plugin est destin&#233; &#224; lire en ligne des parties d\'&#233;checs gr&#226;ce &#224; des fichiers PGN.  
   Il suffit d\'ins&#233;rer dans un article la balise &#60;chessXX&#62; o&#249; XX est le num&#233;ro de l&#39;article et de t&#233;l&#233;charger les documents PGN.',
