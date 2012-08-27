5b7d4257beb3574b82297cd1d9643710570eb36b
amaury.adon@laposte.net <amaury.adon@laposte.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-08-27 18:12:08 +0000

    Passer le css par insert_head
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@65345 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ seminaire/branches/v1.5/seminaire_pipelines.php
@@ -0,0 +1,4 @@
+function seminaire_insert_head($texte){
+    $texte .= '<link rel="stylesheet" type="text/css" href="'.find_in_path('inc-css-seminaire.css.html').'" media="all" />'."\n";
+    return $texte;
+}
