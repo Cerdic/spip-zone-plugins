2884424cedd7e0c143ee3e195049be52dd4ac16b
george@middleeastwatch.net <george@middleeastwatch.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-08-27 01:40:20 +0000

    langues (suivant_precedent)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@65335 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ criteres_suivant_precedent/lang/suivant_precedent_ar.php
@@ -0,0 +1,20 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/suivant_precedent?lang_cible=ar
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'alt_lien_next' => 'التالي',
+	'alt_lien_prev' => 'السابق',
+
+	// N
+	'noisette_description' => 'يعرض روابط التصفح الى المقال السابق والمقال التالي',
+	'noisette_label_meme_rubrique' => 'عرض الروابط الى مقالات القسم نفسه فقط',
+	'noisette_nom' => 'روابط السابق / التالي'
+);
+
+?>
