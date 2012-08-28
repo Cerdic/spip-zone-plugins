b4cd47428901e192e88d8f82e24a05df5fdb4437
brunobergot@gmail.com <brunobergot@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-08-28 17:02:21 +0000

    version 1.3.2 : pas besoin de deux css calculées, la css préfixée style_prive_plugin_ étant insérée automatiquement par SPIP + pétouilles de styles
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@65358 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- a2a/branches/type/a2a_pipelines.php
+++ a2a/branches/type/a2a_pipelines.php
@@ -20,8 +20,4 @@ function a2a_affiche_milieu($flux){
 	
 }
 
-function a2a_header_prive_css($css){
-	$css.= '<link rel="stylesheet" type="text/css" href="'.generer_url_public('a2a_prive.css').'" id="csspriveea2a" />';
-	return $css;
-}
 ?>
