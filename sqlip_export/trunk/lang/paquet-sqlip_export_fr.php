c6eccaf19b23c7082820bc9beb431da6728af532
suske@brubel.net <suske@brubel.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-08-31 14:45:39 +0000

    Traiter les varchar sans longueur, les default sur les text, un paquet et sa langue + todo, règles et de l'ordre
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@65427 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ sqlip_export/trunk/lang/paquet-sqlip_export_fr.php
@@ -0,0 +1,9 @@
+<?php
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+        'prefix_description' => 'Ce plugin utilise un squelette et les it&#233;rateurs pour produire un fichier texte contenant les d&#233;finitions de tables et les contenus d\'un site SPIP. A ce stade, il ne fonctionne que dans le sens sqlite -> MySQL. Pour produire la page, le webmestre doit appeler [->../spip.php?page=sqlite-mysql]',
+        'prefix_slogan' => 'Des dump SQL pour diff&#233;rents moteurs de base de donn&#233;es'
+);
+?>
---
+++ sqlip_export/trunk/sqlite-mysql_fonctions.php
@@ -0,0 +1,30 @@
+<?php
+
+// la cle primaire est declaree avec le reste du sql => recuperer sa seule declaration
+function cle_prim($val) {
+if ($position_cle=strpos($val,"PRIMARY")) {
+$cle_primaire=substr($val,$position_cle);
+$cle_primaire=str_replace("))",")",$cle_primaire); 
+}
+return $cle_primaire;
+}
+
+//sqlite fournit des noms de cle de la forme nom_table_cle => supprimer nom_table
+function keyname($nomlong,$nomtable) {
+$nomtable=$nomtable."_";
+if ($nomlong!=="") {
+$nom_cle=str_replace($nomtable,"",$nomlong);
+return $nom_cle;
+}
+else return false;
+}
+
+// nettoyer pour MySQL => enlever les entites num, les échappements préexistants + addslashes
+function mysql_prep($value) {
+	$value = str_replace("&#39;","'",$value);
+    $value = str_replace("\'","'",$value);
+    $text_mysql = addslashes($value);
+    return $text_mysql;
+}
+
+?>
