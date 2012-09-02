575af01539bd7405aaa0fc259a0ef1534778dc9b
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-09-02 12:22:14 +0000

    nettoyer un peu le HTML5 avant de passer à spip_query_code
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@65450 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spip-bible/trunk/traduction/na28.php
+++ spip-bible/trunk/traduction/na28.php
@@ -23,7 +23,22 @@ function generer_url_passage($livre,$chapitre_debut,$verset_debut,$chapitre_fin,
 	return ($base."/$id/$debut/$fin");
 }
 function recuperer_passage($livre,$chapitre_debut,$verset_debut,$chapitre_fin,$verset_fin,$lang){
+	
 
-	return generer_url_passage($livre,$chapitre_debut,$verset_debut,$chapitre_fin,$verset_fin,$lang);	
+	$url = generer_url_passage($livre,$chapitre_debut,$verset_debut,$chapitre_fin,$verset_fin,$lang);	
+	return extraire_passage($url,$verset_debut,$verset_fin);
+}
+
+function extraire_passage($url,$verset_debut,$verset_fin){
+	include_spip('inc/querypath');
+	include_spip("inc/distant");
+	include_spip("inc/charsets");
+	$code = importer_charset(recuperer_page($url),'utf-8');
+	$tab = explode('<body>',$code);
+	$code = $tab[2];
+	$tab = explode('</body>',$code);
+	$code = $tab[0];
+	$qp = spip_query_path($code,'body #main',array('omit_xml_declaration'=>true,'encoding'=>'UTF-8','use_parser'=>'xml'));
+	
 }
 ?>
