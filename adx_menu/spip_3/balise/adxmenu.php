e37cea629b9428056113e00b461d7d4f9a3284a2
piero.wbmstr@gmail.com <piero.wbmstr@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-09-02 09:25:56 +0000

    Evidemment !!!
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@65449 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- adx_menu/spip_3/balise/adxmenu.php
+++ adx_menu/spip_3/balise/adxmenu.php
@@ -1,6 +1,8 @@
 <?php
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
+include_spip('base/abstract_sql');
+
 function balise_ADXMENU_dist($p) {
     return calculer_balise_dynamique($p, ADXMENU, array('id_article', 'id_rubrique'));
 }
