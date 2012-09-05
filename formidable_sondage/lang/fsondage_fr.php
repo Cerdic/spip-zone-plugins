bf916ffe90d9ec7c33ba3c49da6e6fc6a787ed32
marcimat@rezo.net <marcimat@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-09-05 15:50:48 +0000

    Une configuration pour indiquer éventuellement une classe CSS sur la barre de progression.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@65574 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- formidable_sondage/lang/fsondage_fr.php
+++ formidable_sondage/lang/fsondage_fr.php
@@ -4,6 +4,13 @@
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+	// 
+
+	// C
+	'cfg_classe_label' => 'Classe CSS de la barre de progression',
+	'cfg_classe_explication' => 'Vous pouvez indiquer des classes CSS qui seront ajoutées
+		sur le conteneur de chaque graphique, tel que <code>blue</code>, <code>orange</code>, <code>green</code> ou tout ce qui vous plairait !',
+	'cfg_titre_parametrages' => 'Paramétrages',
 
 	// F
 	'fsondage_titre' => 'Formidable Sondage',
@@ -14,12 +21,11 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'sondage_nb_reponses_total' => '@nb@ réponses à ce sondage',
 
 	// T
+	'titre_page_configurer_fsondage' => 'Configurer Formidable Sondage',
 	'traiter_enregistrement_option_sondage_label' => 'Ce formulaire est-il un sondage ?',
 	'traiter_enregistrement_option_sondage_explication' => 'Dès qu\'une personne a rempli
 		le formulaire, un graphique des réponses apparaît. Le formulaire ne peut être
 		répondu qu\'une fois par personne.',
-
-	
 );
 
 ?>
