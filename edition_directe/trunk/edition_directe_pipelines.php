815a1d494f83d15d7c93992b435a8caaaf5bb54a
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-09-05 07:39:11 +0000

    Le plugin ne s'applique uniquement pour l'espace privé
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@65545 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- edition_directe/trunk/edition_directe_pipelines.php
+++ edition_directe/trunk/edition_directe_pipelines.php
@@ -35,48 +35,52 @@ function edition_directe_recuperer_fond($flux){
 	$texte=$flux['data']['texte'];
 	$contexte['objet']=$objet;
 	$id='id_'.$objet;
+	$exec=_request('exec');
 	if($contexte['exec']=='site')$id='id_syndic';
 	$contexte['id_objet']=$contexte[$id];
 	
-	// On cherche les objets actifs pour l'édition directe
-	$objets=objets_edition_directe();
+	// Seulement dans l'espace priv&eacute;
+	if($exec){
+		// On cherche les objets actifs pour l'édition directe
+		$objets=objets_edition_directe();
+		
+		// Les objets éditables déclarés
+		$objets_dispos=lister_objets(array());
 	
-	// Les objets éditables déclarés
-	$objets_dispos=lister_objets(array());
-
-	// Insertion du formulaire d'édition	
-	if(in_array($objet,$objets)){		
-		  if ($fond == 'prive/squelettes/contenu/'.$objet){
-				
-				$edition=recuperer_fond('prive/echafaudage/contenu/objet_edit_directe',$contexte,array('ajax'=>true));
-				$icone='
-				<span class="icone_edition_directe icone active">
-					<a href="'.generer_action_auteur('edition_directe_auteur','inactive-'.$objet,generer_url_ecrire($objet,$id.'='.$contexte['id_objet'],false)).'" title="'._T('edir:desactiver_edition_directe_objet').$objet.'">
-						<img src="'.find_in_path('prive/themes/spip/images/edir-24.png').'"/>
-						<b>'._T('edir:titre_plugin').'</b>
-					</a>
-				</span>';
-				$patterns = array('/class=\'icone/','/<!--\/hd-->/','/<h1>/');
-				$replacements = array('class="icone invisible',$edition.'<!--/hd-->',$icone.'<h1>');						
-				$flux['data']['texte'] = preg_replace($patterns,$replacements,$texte,1);
-		    }
-		//Suppression de la prévisualisation	
-		 if ($fond == 'prive/objets/contenu/'.$objet){	
-				$flux['data']['texte'] = '';
-		    }	
-		    	    
-		}
-	elseif ($fond == 'prive/squelettes/contenu/'.$objet AND in_array($objet,$objets_dispos)){
-		$icone='
-				<span class="icone_edition_directe icone inactive">
-					<a href="'.generer_action_auteur('edition_directe_auteur','active-'.$objet,generer_url_ecrire($objet,$id.'='.$contexte['id_objet'],false)).'" title="'._T('edir:activer_edition_directe_objet').$objet.'">
-						<img src="'.find_in_path('prive/themes/spip/images/edir-24.png').'"/>
-						<b>'._T('edir:titre_plugin').'</b>
-					</a>
-				</span>';		
-		$patterns = array('/<h1>/');
-		$replacements = array($icone.'<h1>');
-		$flux['data']['texte'] = preg_replace($patterns,$replacements,$texte,1);					
+		// Insertion du formulaire d'édition	
+		if(in_array($objet,$objets)){		
+			  if ($fond == 'prive/squelettes/contenu/'.$objet){
+					
+					$edition=recuperer_fond('prive/echafaudage/contenu/objet_edit_directe',$contexte,array('ajax'=>true));
+					$icone='
+					<span class="icone_edition_directe icone active">
+						<a href="'.generer_action_auteur('edition_directe_auteur','inactive-'.$objet,generer_url_ecrire($objet,$id.'='.$contexte['id_objet'],false)).'" title="'._T('edir:desactiver_edition_directe_objet').$objet.'">
+							<img src="'.find_in_path('prive/themes/spip/images/edir-24.png').'"/>
+							<b>'._T('edir:titre_plugin').'</b>
+						</a>
+					</span>';
+					$patterns = array('/class=\'icone/','/<!--\/hd-->/','/<h1>/');
+					$replacements = array('class="icone invisible',$edition.'<!--/hd-->',$icone.'<h1>');						
+					$flux['data']['texte'] = preg_replace($patterns,$replacements,$texte,1);
+			    }
+			//Suppression de la prévisualisation	
+			 if ($fond == 'prive/objets/contenu/'.$objet){	
+					$flux['data']['texte'] = '';
+			    }	
+			    	    
+			}
+		elseif ($fond == 'prive/squelettes/contenu/'.$objet AND in_array($objet,$objets_dispos)){
+			$icone='
+					<span class="icone_edition_directe icone inactive">
+						<a href="'.generer_action_auteur('edition_directe_auteur','active-'.$objet,generer_url_ecrire($objet,$id.'='.$contexte['id_objet'],false)).'" title="'._T('edir:activer_edition_directe_objet').$objet.'">
+							<img src="'.find_in_path('prive/themes/spip/images/edir-24.png').'"/>
+							<b>'._T('edir:titre_plugin').'</b>
+						</a>
+					</span>';		
+			$patterns = array('/<h1>/');
+			$replacements = array($icone.'<h1>');
+			$flux['data']['texte'] = preg_replace($patterns,$replacements,$texte,1);					
+			}
 		}
 
  return $flux;   
