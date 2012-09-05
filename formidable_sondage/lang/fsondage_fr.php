2d70ce359e9915e714b5da75a7754f2d85cef92e
marcimat@rezo.net <marcimat@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-09-05 15:35:14 +0000

    Un plugin ajoutant une option de sondage au traitement enregistrant en base de données pour Formidable.
    L'option activée, un graphique est affiché à la place du formulaire dès que quelqu'un a déjà voté ou que quelqu'un vient juste de voter.
    
    Pas certain que ça marche pour tout, mais radio et checkbox ça a l'air ok.
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@65572 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ formidable_sondage/lang/fsondage_fr.php
@@ -0,0 +1,25 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// F
+	'fsondage_titre' => 'Formidable Sondage',
+
+	// S
+	'sondage_zero_reponse_total' => 'Aucune réponse à ce sondage',
+	'sondage_une_reponse_total' => 'Une réponse à ce sondage',
+	'sondage_nb_reponses_total' => '@nb@ réponses à ce sondage',
+
+	// T
+	'traiter_enregistrement_option_sondage_label' => 'Ce formulaire est-il un sondage ?',
+	'traiter_enregistrement_option_sondage_explication' => 'Dès qu\'une personne a rempli
+		le formulaire, un graphique des réponses apparaît. Le formulaire ne peut être
+		répondu qu\'une fois par personne.',
+
+	
+);
+
+?>
