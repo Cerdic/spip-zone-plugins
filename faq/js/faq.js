a6ccf0ac4bde1710e7361dc53e615db371f9dd7b
chankalan@free.fr <chankalan@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-09-06 13:41:14 +0000

    pour éviter un petit bug lors d'insertion de document dans la faq qui ont aussi la structure dl>dt/dd
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@65611 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- faq/js/faq.js
+++ faq/js/faq.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	$('dl.faq').find('dt').addClass("close").click(function(){
+	$('dl.faq > dt').addClass("close").click(function(){
 		$(this).toggleClass("close").next().toggle('fast');
 		return false;
 	}).next().hide();
