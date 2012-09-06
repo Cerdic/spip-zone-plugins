098bfa8c628cc1fc136aa9b6ab3462530a61882f
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-09-06 14:16:10 +0000

    le wrapper html des mails peut recevoir soit du html brut a encapsuler : il est injecté tel quel, soit du texte brut, qui est alors mis en forme comme avant. C'est le html qui a la priorité.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@65612 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- facteur/trunk/inc/envoyer_mail.php
+++ facteur/trunk/inc/envoyer_mail.php
@@ -72,7 +72,7 @@ function inc_envoyer_mail($destinataire, $sujet, $corps, $from = "", $headers = 
 	// si le mail est en texte brut, on l'encapsule dans un modele surchargeable
 	// pour garder le texte brut, il suffit de faire un modele qui renvoie uniquement #ENV*{texte}
 	if ($message_texte AND ! $message_html){
-		$message_html = recuperer_fond("emails/texte",array('texte'=>$message_texte,'html'=>$message_texte,'sujet'=>$sujet));
+		$message_html = recuperer_fond("emails/texte",array('texte'=>$message_texte,'sujet'=>$sujet));
 	}
 
 	// mode TEST : forcer l'email
