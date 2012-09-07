61afdd61c7c58482961e31974d4857ce3cc4a958
chankalan@free.fr <chankalan@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-09-07 09:51:42 +0000

    ajout de config uniquement sur secteurs
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@65668 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- couleur_rubrique/trunk/pb_couleur_rubrique_pipelines.php
+++ couleur_rubrique/trunk/pb_couleur_rubrique_pipelines.php
@@ -8,15 +8,25 @@ function pb_couleur_rubrique_header_prive($flux){
 function pb_couleur_rubrique_affiche_droite($flux) {
 	
         $exec = $flux["args"]["exec"];
-       
         if ($exec == "rubrique") {
-            $id_rubrique = $flux["args"]["id_rubrique"];
-            $contexte = array('id_rubrique'=>$id_rubrique);
-            $flux["data"] .= recuperer_fond("inclure/couleur_rubrique", $contexte);
-        }
-     
+        $id_rubrique = $flux["args"]["id_rubrique"];
+        // si la config est sur "oui, que les secteurs"
+        if (lire_config('pb_couleur_rubrique/secteurs') == 'oui') {
+		// calcul du secteur
+		$id_secteur = sql_getfetsel('id_secteur', 'spip_rubriques', 'id_rubrique=' . intval($id_rubrique));
+		// on affiche que dans le secteur
+		if ($id_secteur == $id_rubrique) {
+			$contexte = array('id_rubrique'=>$id_rubrique);
+			$flux["data"] .= recuperer_fond("inclure/couleur_rubrique", $contexte);
+			}
+		} else {
+		$contexte = array('id_rubrique'=>$id_rubrique);
+		$flux["data"] .= recuperer_fond("inclure/couleur_rubrique", $contexte);
+		}
+	}
+
         return $flux;
-    }
+}
 
 
 ?>
