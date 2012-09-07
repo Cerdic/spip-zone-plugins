ad09a3f6e418964af2b00289178027e1adae9041
toutati@free.fr <toutati@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-09-07 20:13:11 +0000

    Ce plugin ne nécessite pas cextras, donc on evite qu'il crie partout qu'il ne sait pas qui c'est !  oups mis le xml dans le precedent commit !
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@65681 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- montants/montants_fonctions.php
+++ montants/montants_fonctions.php
@@ -4,7 +4,7 @@
 	include_spip('inc/cextras_autoriser');
 	include_spip('inc/config');
 	/* On verifie que la fonction existe bien */
-	if (function_exists('lire_config')) {
+	if (function_exists('lire_config') && function_exists('restreindre_extras')) {
 		restreindre_extras('rubrique', 'prix', lire_config('montants/secteurs',0), 'secteur');
 		restreindre_extras('article', 'prix', lire_config('montants/secteurs',0), 'secteur');
 	}
