4af1c25762a186e3ae624fd3dd1b4d21b5f28159
marcimat@rezo.net <marcimat@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-09-07 09:20:08 +0000

    Puces statuts rapides sur les dictionnaires et définitions
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@65665 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ dictionnaires/trunk/exec/puce_statut_dictionnaires.php
@@ -0,0 +1,61 @@
+<?php
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+/**
+ * Gestion des puces d'action rapide des dictionnaires
+ *
+ * @package SPIP\Dictionnaire\Puce_statut
+**/
+
+include_spip('inc/presentation');
+
+/**
+ * Gestion de l'affichage ajax des puces d'action rapide des dictionnaires
+ *
+ * Récupère l'identifiant id et le type d'objet dans les données postées
+ * et appelle la fonction de traitement de cet exec.
+ * 
+ * @see exec_puce_statut_formulaires_args()
+ * @return string Code HTML
+**/
+function exec_puce_statut_dictionnaires_dist()
+{
+	exec_puce_statut_dictionnaires_args(_request('id'),  _request('type'));
+}
+
+/**
+ * Traitement de l'affichage ajax des puces d'action rapide
+ *
+ * Appelle la fonction de traitement des puces statuts
+ * après avoir retrouvé le statut en cours de l'objet
+ * et son parent
+ * 
+ * @param int $id
+ *     Identifiant de l'objet
+ * @param string $type
+ *     Type d'objet
+ * @return string Code HTML
+**/
+function exec_puce_statut_dictionnaires_args($id, $type)
+{
+	if ($table_objet_sql = table_objet_sql($type)
+		AND $d = lister_tables_objets_sql($table_objet_sql)
+		AND isset($d['statut_textes_instituer'])
+		AND $d['statut_textes_instituer'])
+	{
+		$prim = id_table_objet($type);
+		$id = intval($id);
+		$r = sql_fetsel("statut", $table_objet_sql, "$prim=$id");
+		$statut = $r['statut'];
+		$id_parent = 0;
+	}
+	else {
+		$id_parent = intval($id);
+		$statut = 'prop'; // arbitraire
+	}
+	$puce_statut = charger_fonction('puce_statut', 'inc');
+	ajax_retour($puce_statut($id,$statut,$id_parent,$type, true));
+}
+
+?>
