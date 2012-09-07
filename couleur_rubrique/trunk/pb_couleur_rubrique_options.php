5b0683121061c91bd1a1ba7bfd7ed9c46a788f9c
chankalan@free.fr <chankalan@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-09-07 13:50:30 +0000

    un filtre sur #ID_RUBRIQUE pour retourner la couleur du secteur
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@65676 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- couleur_rubrique/trunk/pb_couleur_rubrique_options.php
+++ couleur_rubrique/trunk/pb_couleur_rubrique_options.php
@@ -11,4 +11,10 @@ function couleur_rubrique($id_rubrique) {
 	return pb_couleur_rubrique($id_rubrique);
 }
 
+function couleur_secteur($id_rubrique){
+	$id_secteur = sql_getfetsel('id_secteur', 'spip_rubriques', 'id_rubrique=' . intval($id_rubrique));
+	$couleur_secteur = lire_meta("pb_couleur_rubrique$id_secteur");
+	return $couleur_secteur;
+}
+
 ?>
