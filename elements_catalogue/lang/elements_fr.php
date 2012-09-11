74a86b1a0103977e6356384205afee64214a371f
marcimat@rezo.net <marcimat@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-09-11 13:03:15 +0000

    Ajout d'un plugin «elements_catalogue» qui fournit des éléments en exemple ou utilisables directement.
    C'est dans ce plugin que des gens peuvent partager leurs éléments s'il le veulent.
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@65750 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ elements_catalogue/lang/elements_fr.php
@@ -0,0 +1,21 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+ 
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// E
+	'element_jeu' => 'Jeu',
+	'element_jeu_description' => 'Ajout d\'un jeu',
+
+	'element_sondage' => 'Sondage',
+	'element_sondage_description' => 'Ajout d\'un sondage',
+
+	// L
+	'label_id_jeu' => 'Choix du jeu',
+	'label_id_sondage' => 'Choix du sondage',
+
+);
+
+?>
---
+++ elements_catalogue/lang/paquet-celements_fr.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// E
+	'celements_description' => 'Propose des éléments qui peuvent être utilisés ou dont vous
+		pouvez vous inspirer !',
+	'celements_nom' => 'Eléments, le catalogue',
+	'celements_slogan' => 'Fournit une liste d\'éléments',
+);
+
+?>
