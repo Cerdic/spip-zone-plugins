d1ef0d67d6defdbfddedf90aa1e1546869cf5bd8
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-09-15 15:49:23 +0000

    Mise à jour du paquet.xml avec la version et l'etat. Déplacement de la vignette dans un pipeline plus conforme en utilisant la charte SPIP 3
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@65895 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- thumbsites/trunk/thumbsites_pipelines.php
+++ thumbsites/trunk/thumbsites_pipelines.php
@@ -1,23 +1,35 @@
 <?php
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
-function thumbsites_affiche_gauche($flux){
-	if ($id_syndic = intval(_request('id_syndic'))
-	AND $row = sql_fetsel('url_site', 'spip_syndic', 'id_syndic='.$id_syndic)
-	AND $url = $row['url_site']) {
+
+/**
+ * Boite de configuration des objets
+ *
+ * @param array $flux
+ * @return array
+ */
+function thumbsites_afficher_config_objet($flux){
+	$type = $flux['args']['type'];
+	if (($type == 'site')
+	AND ($id = intval($flux['args']['id']))
+	AND ($url = sql_getfetsel('url_site', 'spip_syndic', 'id_syndic=' . sql_quote($id)))) {
 		include_spip('inc/thumbsites_filtres');
 		if ($thumbshot_cache = thumbshot($url)) {
-			// On affiche un bloc identique a celui du logo du site
-			if ($taille = @getimagesize($thumbshot_cache))
+			if ($taille = @getimagesize($thumbshot_cache)) {
 				$taille = _T('info_largeur_vignette', array('largeur_vignette' => $taille[0], 'hauteur_vignette' => $taille[1]));
-				$flux["data"] .= recuperer_fond('prive/squelettes/contenu/thumbsites_affiche_boite',array(
-				'thumbshot_cache' => $thumbshot_cache,
-				'taille' => $taille,
-				'id_syndic' => $id_syndic,
-				'url' => $url
-				));
+				$flux["data"] .= recuperer_fond('prive/squelettes/navigation/thumbshot',
+									array(
+										'id_objet' => $id,
+										'objet'=> objet_type(table_objet($type)),
+										'thumbshot_cache' => $thumbshot_cache,
+										'largeur' => $taille[0],
+										'hauteur' => $taille[1],
+										'url' => $url
+									));
+			}
 		}
 	}
+
 	return $flux;
 }
 
