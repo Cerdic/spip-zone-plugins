19acef146479fc72d259b4190b94167718b8893a
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-09-16 01:40:36 +0000

    langues (nospam)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@65906 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- nospam/lang/nospam_en.php
+++ nospam/lang/nospam_en.php
@@ -8,12 +8,12 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// E
-	'erreur_attributs_html_interdits' => 'Il n\'est pas permis d\'utiliser les attributs html <tt>class</tt> ou <tt>style</tt>', # NEW
+	'erreur_attributs_html_interdits' => 'It\'s not allowed to use the html attributes <tt>class</tt> or <tt>style</tt>',
 	'erreur_jeton' => 'Unable to take account of your message. Thank you to resubmit!',
 	'erreur_spam' => 'Unable to take account of your message!',
 	'erreur_spam_doublon' => 'An identical message already exists!',
 	'erreur_spam_ip' => 'Too many comments detrimental to the quality!',
-	'erreur_url_deja_spammee' => 'Ce message contient des liens suspects qui ressemblent à du SPAM. Merci de les retirer.', # NEW
+	'erreur_url_deja_spammee' => 'This message contains suspicious links that look like SPAM. Thanks to delete them.',
 
 	// F
 	'forum_saisie_texte_info' => 'For the formatting of your message, this form only accepts SPIP shortcuts <code>[-&gt;url] {{bold}} {italic} &lt;quote&gt; &lt;code&gt;</code> &lt;cadre&gt;</cadre> et le code HTML <code>&lt;q&gt; &lt;del&gt; &lt;ins&gt;</code>. To create paragraphs, simply leave blank lines.'
