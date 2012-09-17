fbd8377d811427ef9122cfe03f5c8878a414bbd6
cyril@cym.fr <cyril@cym.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-09-17 14:10:22 +0000

    Merci Matthieu ;-) on ajoute une peu de style et surtout on fait que ça fonctionne, dans un premier temps avec un tableau dans la balise;
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@65952 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- fil_ariane/trunk/fil_ariane_fonctions.php
+++ fil_ariane/trunk/fil_ariane_fonctions.php
@@ -15,25 +15,66 @@
  */
 function balise_FIL_ARIANE_dist($p){
 
-    // L'id de l'objet
-    $_id_objet = $p->boucles[$p->id_boucle]->primary;
+    // on attend un tableau avec le fil d'ariane
+    // le 1er paramètre passé avec la balise "fil_ariane"
+    $fil = interprete_argument_balise(1,$p);
 
-    // Le champ (?)
-    $id_objet = champ_sql($_id_objet, $p);
+    if (!$fil) {
+        // On appele la fonction qui construit le fil en prenant en compte seulement l'objet
+        // L'id de l'objet
+        $_id_objet = $p->boucles[$p->id_boucle]->primary;
 
-    // L'objet issu du nom de la table
-    $objet = $p->boucles[$p->id_boucle]->id_table;
+        // Code php mis en cache, et qui sera exécuté et qui est sensé ramener la valeur du champ
+        $id_objet = champ_sql($_id_objet, $p);
 
-    $p->code = "construire_FIL_ARIANE('$objet', $id_objet)";
+        // L'objet issu du nom de la table
+        $objet = $p->boucles[$p->id_boucle]->id_table;
+
+        $p->code = "calcule_hierarchie_objet('$objet', $id_objet)";
+    }
+
+    else {
+        // On décortique le tableau $fil et on appelle la fonction qui construit le fil
+        // avec les valeurs du tableau
+        $p->code = "construire_FIL_ARIANE($fil)";
+
+    }
+
     return $p;
+}
+
+/***
+ * @param $objet
+ * @param $id_objet
+ * Calcule la hierarchie d'un objet et la retourne sous forme d'un tableau
+ */
+function calcule_hierarchie_objet($objet, $id_objet) {
+    return '';
 }
 /***
  * Construit le fil d'Ariane
  */
-function construire_FIL_ARIANE($objet, $id_objet){
-    $objet = objet_type($objet);
+function construire_FIL_ARIANE($fil){
+
     $fil_ariane = '<div class="fil_ariane">';
-    $fil_ariane.= '<a href="#">Racine du site</a> &gt; <a href="#">Rubrique</a> &gt; <strong>' . $objet . ' ' . $id_objet . '</strong></a>';
+
+    if (!is_array($fil)) {
+        return '';
+    }
+
+    $nb= count($fil);
+    $passe=0;
+    $fil_ariane.=" nb elets : $nb | ";
+
+    foreach($fil as $titre => $lien) {
+        if($passe>0) $fil_ariane.=" &gt; ";
+        $fil_ariane.= "<a". (($passe == $nb-1)?' class="on"':'') ." href='$lien'>$titre</a>";
+        $passe++;
+    }
+
+
+    // dernier : en gras, sans lien et sans '>'
+
     $fil_ariane.= '</div>';
     return $fil_ariane;
 }
