3f89422534e5034724bceb5cda96b5ff5c6d831e
sblanc@cc-pays-de-gex.fr <sblanc@cc-pays-de-gex.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-09-17 16:40:30 +0000

    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@65981 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ forms/forms_et_tables_2_5/forms_fonctions.php
@@ -0,0 +1,9 @@
+<?php
+// pour les fonctions, variables et constantes nécessaires à l’espace public :
+include_spip('forms_fonctions-v1');
+include_spip('forms_options-v1.php');
+include_spip('public/forms_boucles');
+include_spip('public/forms_balises'); //-- ATTENTION la boucle #VALEUR existe dans SPIP3, celle de forms_ est renommé en #LAVALEUR pour éviter le conflit.
+
+
+?>
