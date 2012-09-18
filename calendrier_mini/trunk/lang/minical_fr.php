beaa50be34401c89152131d7fba15c432258a705
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-09-18 14:09:09 +0000

    Introduction d'une configuration du calendrier mini qui permet de personaliser : - le jour de debut de semaine - le format des jours affichés - l'affichage ou non des jours des mois précédents/suivants - le changement rapide de mois/année
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@66022 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- calendrier_mini/trunk/lang/minical_fr.php
+++ calendrier_mini/trunk/lang/minical_fr.php
@@ -10,7 +10,19 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// M
 	'mois_precedent' => 'Mois précédent',
-	'mois_suivant' => 'Mois suivant'
+	'mois_suivant' => 'Mois suivant',
+
+	'config_titre_calendriermini' => 'Mini-Calendrier',
+	'label_jour1' => 'Premier jour de la semaine',
+	'label_affichage_hors_mois' => 'Jours affichés',
+	'label_affichage_hors_mois_1' => 'Afficher les jours des mois précédent et suivant',
+	'label_affichage_hors_mois_0' => 'Masquer les jours des mois précédent et suivant',
+	'label_format_jour' => 'Format des jours',
+	'label_format_jour_initiale' => 'Initiale',
+	'label_format_jour_abbr' => 'Court',
+	'label_changement_rapide' => 'Navigation',
+	'label_changement_rapide_1' => 'Activer la selection rapide du mois ou de l\'année',
+	'label_changement_rapide_0' => 'Desactiver la selection rapide du mois ou de l\'année',
 );
 
 ?>
