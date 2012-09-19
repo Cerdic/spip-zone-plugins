e0b6736128a4a48b87da55117074dfb5dcd0f630
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-09-19 01:41:57 +0000

    langues (minical)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@66049 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- calendrier_mini/trunk/lang/minical_fr.php
+++ calendrier_mini/trunk/lang/minical_fr.php
@@ -8,21 +8,24 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// A
 	'aucune_date' => 'Rien pour ce mois',
 
+	// C
+	'config_titre_calendriermini' => 'Mini-Calendrier',
+
+	// L
+	'label_affichage_hors_mois' => 'Jours affichés',
+	'label_affichage_hors_mois_0' => 'Masquer les jours des mois précédent et suivant',
+	'label_affichage_hors_mois_1' => 'Afficher les jours des mois précédent et suivant',
+	'label_changement_rapide' => 'Navigation',
+	'label_changement_rapide_0' => 'Desactiver la selection rapide du mois ou de l\'année',
+	'label_changement_rapide_1' => 'Activer la selection rapide du mois ou de l\'année',
+	'label_format_jour' => 'Format des jours',
+	'label_format_jour_abbr' => 'Court',
+	'label_format_jour_initiale' => 'Initiale',
+	'label_jour1' => 'Premier jour de la semaine',
+
 	// M
 	'mois_precedent' => 'Mois précédent',
-	'mois_suivant' => 'Mois suivant',
-
-	'config_titre_calendriermini' => 'Mini-Calendrier',
-	'label_jour1' => 'Premier jour de la semaine',
-	'label_affichage_hors_mois' => 'Jours affichés',
-	'label_affichage_hors_mois_1' => 'Afficher les jours des mois précédent et suivant',
-	'label_affichage_hors_mois_0' => 'Masquer les jours des mois précédent et suivant',
-	'label_format_jour' => 'Format des jours',
-	'label_format_jour_initiale' => 'Initiale',
-	'label_format_jour_abbr' => 'Court',
-	'label_changement_rapide' => 'Navigation',
-	'label_changement_rapide_1' => 'Activer la selection rapide du mois ou de l\'année',
-	'label_changement_rapide_0' => 'Desactiver la selection rapide du mois ou de l\'année',
+	'mois_suivant' => 'Mois suivant'
 );
 
 ?>
