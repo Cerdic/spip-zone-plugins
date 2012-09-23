749bda5003d9edbaba610024288fd24744b5011e
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-09-23 01:40:36 +0000

    langues (thumbsites)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@66166 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- thumbsites/trunk/lang/thumbsites_sk.php
+++ thumbsites/trunk/lang/thumbsites_sk.php
@@ -19,8 +19,8 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'cfg_descriptif' => 'Táto stránka vám umožňuje nastaviť zásuvný modul Thumbsites a najmä vybrať si server pre miniatúry. Ak chcete zistiť viac informácií, pozrite si <a href="http://www.spip-contrib.net/?article2584">documentation sur contrib</a>dokumentáciu na contribe</a>',
 	'cfg_inf_apercite_parametres' => 'Prosím, zadajte svoje osobné údaje:',
 	'cfg_inf_apercite_presentation' => '<a href="http://www.apercite.fr">apercite.fr</ a> ponúka bezplatnú službu, ktorá si nevyžaduje identifikáciu.',
-	'cfg_inf_choix_serveur' => 'Choisissez le serveur qui fournira les vignettes de vos sites référencés et compléter éventuellement son paramétrage.', # NEW
-	'cfg_inf_miwim_presentation' => 'Pour utiliser ce service vous devez mettre un lien vers le site <a href="http://thumbs.miwim.fr">Miwim</a>. La présence de ce lien est vérifié périodiquement par un script.', # NEW
+	'cfg_inf_choix_serveur' => 'Vyberte si server, čo poskytuje miniatúry stránok, na ktoré odkazujete, a prípadne vyplňte jeho parametre ',
+	'cfg_inf_miwim_presentation' => 'Na to, aby ste mohli využívať túto službu, musíte pridať odkaz na stránku <a href="http://thumbs.miwim.fr">Miwim.</a> Existenciu tohto odkazu pravidelne kontroluje skript.',
 	'cfg_inf_rotothumb_parametres' => 'Prosím, zadajte svoje osobné údaje:',
 	'cfg_inf_rotothumb_presentation' => 'Ak chcete využívať túto službu, nemusíte sa zaregistrovať, ale musíte vytvoriť odkaz na stránku <a href="http://www.robothumb.com">Robothumb.</a> Pokračujúca existencia tohto odkazu sa pravidelne overuje skriptami.',
 	'cfg_inf_thumbshots_de_description' => 'Vygenerované miniatúry budú mať 120 x 90 pixelov.',
@@ -31,7 +31,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'cfg_inf_websnapr_presentation' => 'Ak chcete využívať túto službu, musíte sa zaregistrovať na stránke <a href="http://www.websnapr.com">Websnapr.</a> Bezplatná verzia umožňuje spracovanie 250 000 snímok obrazovky zo stránky za mesiac maximálnou rýchlosťou 80 snímok za hodinu.',
 	'cfg_itm_serveur_apercite' => 'Apercite.fr',
 	'cfg_itm_serveur_girafa' => 'Girafa',
-	'cfg_itm_serveur_miwim' => 'Miwim', # NEW
+	'cfg_itm_serveur_miwim' => 'Miwim',
 	'cfg_itm_serveur_robothumb' => 'Robothumb',
 	'cfg_itm_serveur_thumbshots' => 'Thumbshots.com',
 	'cfg_itm_serveur_thumbshots_de' => 'Thumbshots.de',
@@ -40,11 +40,11 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'cfg_lbl_cle' => 'Váš kľúč',
 	'cfg_lbl_duree_cache' => 'Trvanie cache (v dňoch)',
 	'cfg_lbl_identifiant' => 'Váš identifikátor',
-	'cfg_lbl_serveur' => 'Serveur', # NEW
+	'cfg_lbl_serveur' => 'Server',
 	'cfg_lbl_signature' => 'Váš podpis',
 	'cfg_lbl_taille_vignette' => 'Veľkosť miniatúr',
 	'cfg_lgd_cache' => 'Cache',
-	'cfg_lgd_choix_serveur' => 'Server', # MODIF
+	'cfg_lgd_choix_serveur' => 'Služba',
 	'cfg_titre_thumbsites' => 'Nastaviť Thumbsites',
 	'configuration_thumbsites' => 'Thumbsites',
 
