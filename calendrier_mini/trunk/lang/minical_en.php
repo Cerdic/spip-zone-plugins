9a109fba1c380ae8a9ddf8dcb4f82ba7913fad68
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-09-24 01:44:45 +0000

    langues (minical)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@66186 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- calendrier_mini/trunk/lang/minical_en.php
+++ calendrier_mini/trunk/lang/minical_en.php
@@ -10,6 +10,21 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// A
 	'aucune_date' => 'Nothing for this month',
 
+	// C
+	'config_titre_calendriermini' => 'Mini-Calendrier', # NEW
+
+	// L
+	'label_affichage_hors_mois' => 'Days displayed',
+	'label_affichage_hors_mois_0' => 'Masquer les jours des mois précédent et suivant', # NEW
+	'label_affichage_hors_mois_1' => 'Afficher les jours des mois précédent et suivant', # NEW
+	'label_changement_rapide' => 'Navigation', # NEW
+	'label_changement_rapide_0' => 'Desactiver la selection rapide du mois ou de l\'année', # NEW
+	'label_changement_rapide_1' => 'Activer la selection rapide du mois ou de l\'année', # NEW
+	'label_format_jour' => 'Days format',
+	'label_format_jour_abbr' => 'Short',
+	'label_format_jour_initiale' => 'Initiale', # NEW
+	'label_jour1' => 'First day of the week',
+
 	// M
 	'mois_precedent' => 'Previous month',
 	'mois_suivant' => 'Next month'
