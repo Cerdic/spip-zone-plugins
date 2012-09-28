d0e62ee1b74c484019c4c092368fc59f1767d1fc
toutati@free.fr <toutati@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-09-28 00:50:35 +0000

    une noisette paramétrable pour twidget
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@66308 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ twidget/lang/twidget_fr.php
@@ -0,0 +1,22 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/twidget/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	
+	'nom_noisette_twidget'=>'Noisette twidget',
+	'description_noisette_twidget'=>'Paramétrez la noisette twidget pour Twitter',
+	'noisette_label_width_twidget'=>'Largeur',
+	'noisette_label_shell_background_twidget'=>'Couleur de fond',
+	'noisette_label_shell_color_twidget'=>'Couleur du texte',
+	'noisette_label_demande'=>'Choisissez le type de compte',
+	'noisette_demande_twidget_profile'=>'Compte avec un nom d\'utilisateur',
+	'noisette_demande_twidget_search'=>'Compte avec un mot recherché',
+	'noisette_label_user_twidget_profile'=>'Un nom d\'utilisateur',
+	'noisette_label_search_twidget_search'=>'La recherche comme #SPIP',
+	'noisette_label_title_twidget_search'=>'Le titre de votre recherche',
+	'noisette_label_subject_twidget_search'=>'Le sujet de votre recherche comme SPIP',
+);
+
+?>
