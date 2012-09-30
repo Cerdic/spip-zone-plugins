877926a98218d9db3bbd9ffaa157df8be60e9e4b
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-09-30 16:55:40 +0000

    Changer la version de cette branche expérimentale et améliorer sa description pour mieux prévenir les utilisateurs.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@66371 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- a2a/branches/type/lang/paquet-a2a_fr.php
+++ a2a/branches/type/lang/paquet-a2a_fr.php
@@ -6,7 +6,9 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
-	'a2a_description' => 'Ce plugin permet de lier des articles de façon ponctuelle sans avoir à utiliser les mots-clés de SPIP. Il peut être utilisé pour mettre en place des liens de type « Lire aussi ».',
+	'a2a_description' => 'Ce plugin permet de lier des articles de façon ponctuelle sans avoir à utiliser les mots-clés de SPIP. Il peut être utilisé pour mettre en place des liens de type « Lire aussi ».
+	
+	Cette nouvelle version permet de typer les relations entre articles. Cette fonction de typage est en expérimentation, veuillez donc encore utiliser la version stable pour vos sites en production.',
 	'a2a_slogan' => 'Pour lier des articles'
 );
 
