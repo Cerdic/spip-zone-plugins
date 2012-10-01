acfe3d6e6a5c36be644997d39ef4b8a91b68c537
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-10-01 01:52:34 +0000

    langues (zpip)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@66379 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- z-core/lang/zpip_en.php
+++ z-core/lang/zpip_en.php
@@ -20,8 +20,8 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'date_forum' => '@date@ at @heure@',
 
 	// I
-	'info_1_commentaire' => '1 commentaire', # NEW
-	'info_nb_commentaires' => '@nb@ commentaires', # NEW
+	'info_1_commentaire' => '1 comment',
+	'info_nb_commentaires' => '@nb@ comments',
 
 	// L
 	'lire_la_suite' => 'Read more',
