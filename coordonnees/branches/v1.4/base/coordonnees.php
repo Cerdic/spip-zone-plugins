325f1fc6440afcc89dc7b9f800b113f9980aadca
gildas.cotomale@gmail.com <gildas.cotomale@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-10-01 21:44:36 +0000

    correction d'une coquille suite à l'introduction du champ region pour adresses
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@66418 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- coordonnees/branches/v1.4/base/coordonnees.php
+++ coordonnees/branches/v1.4/base/coordonnees.php
@@ -36,7 +36,7 @@ function coordonnees_declarer_tables_principales($tables_principales){
 		"boite_postale" => "VARCHAR(40) DEFAULT '' NOT NULL",
 		"code_postal" => "VARCHAR(40) DEFAULT '' NOT NULL",
 		"ville" => "TINYTEXT DEFAULT '' NOT NULL",
-		"region" => "VARCHAR(40) DEFAULT '' NOT NULL"
+		"region" => "VARCHAR(40) DEFAULT '' NOT NULL",
 		"pays" => "VARCHAR(3) DEFAULT '' NOT NULL", // peut etre sur 2 caracteres (codes : ISO alpha ou numerique, ONU, etc.) ou sur 3 caracterse (codes : ISO alpha, CIO, etc.)
 		"maj" => "TIMESTAMP"
 		);
