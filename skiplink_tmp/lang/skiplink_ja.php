cdaf272e4c8a173ddd11d38baa8b3edcc84817fe
suske@brubel.net <suske@brubel.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-10-03 14:47:36 +0000

    On recommence pour l'historique
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@66471 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ skiplink_tmp/lang/skiplink_ja.php
@@ -0,0 +1,12 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+'top' => '&#38914;&#19978;',
+'go_top' => '&#12488;&#12483;&#12503;&#12395;&#25147;&#12427;',
+
+);
+
+?>
