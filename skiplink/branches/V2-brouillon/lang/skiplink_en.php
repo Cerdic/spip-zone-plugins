384fe2aee7f3c93d2b58f71d159ea569560a3353
suske@brubel.net <suske@brubel.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-10-03 14:27:04 +0000

    préparer le retour de la v1
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@66469 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ skiplink/branches/V2-brouillon/lang/skiplink_en.php
@@ -0,0 +1,34 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+/* Skip Links */
+'go_content' => 'Skip to content',
+'go_nav' => 'Skip to the menu',
+'go_search' => 'Skip to search',
+
+/* go_top */
+'a_top' => 'top',
+'top' => 'Top',
+'go_top' => 'Back to top',
+
+/* go_bottom */
+'a_bottom' => 'bottom',
+'bottom' => 'Bottom',
+'go_bottom' => 'Bottom',
+
+/* yoyo */
+
+/* go_back */
+'back' => 'Back',
+'go_back' => 'Back',
+
+/* go_home */
+'home' => 'Home',
+'go_home' => 'Home'
+
+);
+
+?>
---
+++ skiplink/branches/V2-brouillon/lang/skiplink_es.php
@@ -0,0 +1,13 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+'go_content' => 'Ir al contenido',
+'go_nav' => 'Ir al men&uacute;',
+'go_search' => 'Ir a Buscar',
+
+);
+
+?>
