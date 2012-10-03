4b53520a82ca1cc5e8e081374e05c3b7b49050e5
suske@brubel.net <suske@brubel.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-10-03 15:21:12 +0000

    nettoyage
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@66476 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- skiplink/skiplink_tmp/skiplink_pipelines.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function skiplink_insert_head_css($flux) {
-	$flux .= '<link rel="stylesheet" type="text/css" href="'.find_in_path('css/skiplink.css').'" media="all" />'."\n";
-	$flux .= '<link rel="stylesheet" type="text/css" href="'.find_in_path('css/focus.css').'" media="all" />'."\n";
-	return $flux;
-}
-
-?>
