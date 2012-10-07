8828474fd4c6fb18f554c8ce5425a140bb80bab5
dani@belvil.net <dani@belvil.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-10-07 01:41:41 +0000

    langues (paquet-ancresdouces)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@66533 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ ancres_douces/lang/paquet-ancresdouces_es.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-ancresdouces?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'ancresdouces_description' => 'Este plugin permite un despliegue continuo de la página cuando el visitante selecciona un enlace que apunta a un ancla en el artículo en curso de lectura.',
+	'ancresdouces_nom' => 'Anclas suaves',
+	'ancresdouces_slogan' => 'Anclas de enlace con suavidad|'
+);
+
+?>
