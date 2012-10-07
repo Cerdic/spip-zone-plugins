f03d993448f44e36dbb03c0a052f2275bb4be526
dani@belvil.net <dani@belvil.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-10-07 01:41:46 +0000

    langues (paquet-article_accueil)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@66534 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- article_accueil/trunk/lang/paquet-article_accueil_es.php
+++ article_accueil/trunk/lang/paquet-article_accueil_es.php
@@ -9,7 +9,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
 	'article_accueil_description' => 'Este plugin te permite atribuir un artículo de inicio a las secciones. De ahí, se puede usar el campo id_article_accueil en los esqueletos.',
-	'article_accueil_nom' => 'Articles d\'accueil', # NEW
+	'article_accueil_nom' => 'Artículos de portada',
 	'article_accueil_slogan' => 'Atribuir un artículo de inicio a las secciones'
 );
 
