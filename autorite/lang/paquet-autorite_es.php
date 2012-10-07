ef8e7c8556259c266893006fb3a18d2ddc7087cb
dani@belvil.net <dani@belvil.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-10-07 01:41:50 +0000

    langues (paquet-autorite)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@66535 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ autorite/lang/paquet-autorite_es.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-autorite?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'autorite_description' => 'Este plugin permite configurar autorizaciones diferentes de las que vienen por omisión.',
+	'autorite_nom' => 'Autoridad',
+	'autorite_slogan' => 'Algunos parámetros de autorización (¡agrega los tuyos!)'
+);
+
+?>
