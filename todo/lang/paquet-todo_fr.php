6692c381798ee6df3b7e90376d06e9221af03bbf
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-10-08 22:10:47 +0000

    Broutilles pour SPIP 3, passage en v1 test vu que le base fonctionne (mise à part le tri sur titre bizarre).
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@66592 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ todo/lang/paquet-todo_fr.php
@@ -0,0 +1,19 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-todo
+// Langue: fr
+// Date: 08-10-2012 23:06:55
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// T
+	'todo_description' => 'Ajoute de nouveaux raccourcis typographiques permettant de décrire de manière simple des listes de choses à faire dans un contenu SPIP.',
+	'todo_slogan' => 'Lister rapidement des choses à faire',
+);
+?>
