1ce95844532dcd5591d542ea6da956d711d28984
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-10-10 15:17:27 +0000

    plutot que surcharger tout nav.less on redefinit simplement le filtre lien_ou_expose pour lui faire renvoyer un <a class='active' href='#'> quand le lien est exposé (dans le site public uniquement, ne pas casser l'espace prive)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@66668 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ bootstrap/bootstrap_fonctions.php
@@ -0,0 +1,20 @@
+<?php
+
+if (!test_espace_prive()){
+function filtre_lien_ou_expose_dist($url,$libelle=NULL,$on=false,$class="",$title="",$rel="", $evt=''){
+	if ($on) {
+		$bal = 'a';
+		$att = 'href="#" class="on active'.($class?' '.attribut_html($class):'').'"';
+	} else {
+		$bal = 'a';
+		$att = "href='$url'"
+	  	.($title?" title='".attribut_html($title)."'":'')
+	  	.($class?" class='".attribut_html($class)."'":'')
+	  	.($rel?" rel='".attribut_html($rel)."'":'')
+		.$evt;
+	}
+	if ($libelle === NULL)
+		$libelle = $url;
+	return "<$bal $att>$libelle</$bal>";
+}
+}
