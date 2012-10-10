b4899dad25d203467ec3c82e688ee0c8fe4fa898
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-10-10 01:46:46 +0000

    langues (paquet-todo)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@66637 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- todo/lang/paquet-todo_fr.php
+++ todo/lang/paquet-todo_fr.php
@@ -1,19 +1,13 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-todo
-// Langue: fr
-// Date: 08-10-2012 23:06:55
-// Items: 2
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/todo/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// T
+	// T
 	'todo_description' => 'Ajoute de nouveaux raccourcis typographiques permettant de décrire de manière simple des listes de choses à faire dans un contenu SPIP.',
-	'todo_slogan' => 'Lister rapidement des choses à faire',
+	'todo_slogan' => 'Lister rapidement des choses à faire'
 );
+
 ?>
