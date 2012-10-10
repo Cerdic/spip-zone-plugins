5a6d7096a5e422a0ac55439350d2790f3ad5878a
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-10-10 01:46:39 +0000

    langues (todo)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@66636 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- todo/lang/todo_fr.php
+++ todo/lang/todo_fr.php
@@ -1,19 +1,27 @@
 <?php
-
-// Sécurité
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/todo/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-'configurer_bando' => 'Choses à faire',
-'configurer_titre' => 'Configurer les listes de choses à faire',
-'label_statut' => 'Statut',
-'label_titre' => 'Titre',
-'statut_afaire' => '&Agrave; faire',
-'statut_encours' => 'En cours',
-'statut_termine' => 'Termin&eacute;',
-'tri_cle' => 'Remettre dans l\'ordre',
-'tri_statut' => 'Trier par statut',
-'tri_titre' => 'Trier par titre',
+
+	// C
+	'configurer_bando' => 'Choses à faire',
+	'configurer_titre' => 'Configurer les listes de choses à faire',
+
+	// L
+	'label_statut' => 'Statut',
+	'label_titre' => 'Titre',
+
+	// S
+	'statut_afaire' => 'À faire',
+	'statut_encours' => 'En cours',
+	'statut_termine' => 'Terminé',
+
+	// T
+	'tri_cle' => 'Remettre dans l\'ordre',
+	'tri_statut' => 'Trier par statut',
+	'tri_titre' => 'Trier par titre'
 );
 
 ?>
