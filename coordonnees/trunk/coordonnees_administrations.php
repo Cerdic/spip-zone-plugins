45e668118d39353d0b91a96286275962a6304159
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-10-11 20:57:50 +0000

    il manquait des virgules + dans l'API de maj les paramètres sont directement après le nom de la fonction, ya pas de tableau qui encapsule
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@66719 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- coordonnees/trunk/coordonnees_administrations.php
+++ coordonnees/trunk/coordonnees_administrations.php
@@ -15,42 +15,44 @@ function coordonnees_upgrade($nom_meta_base_version, $version_cible){
 		array('maj_tables', array('spip_numeros_liens')),
 		array('maj_tables', array('spip_emails')),
 		array('maj_tables', array('spip_emails_liens')),
-		array('ecrire_meta',array('coordonnees', serialize(array('objets'=>array('auteur')))))
-		);
+		array('ecrire_meta', 'coordonnees', serialize(array('objets'=>array('auteur'))))
+	);
 	$maj['1.1'] = array(
-			array('sql_update', array(array("voie" => "CONCAT(numero, ' ', voie)"),array("numero IS NOT NULL", "numero <> ''"))),
-			array('sql_alter', "TABLE spip_adresses DROP COLUMN numero"));
+		array('sql_update', array(array("voie" => "CONCAT(numero, ' ', voie)"),array("numero IS NOT NULL", "numero <> ''"))),
+		array('sql_alter', "TABLE spip_adresses DROP COLUMN numero")
+	);
 
 	$maj['1.2'] = array(
-			array('sql_alter', array('TABLE spip_adresses CHANGE type_adresse titre varchar(255) not null default ""')),
-			array('sql_alter', array('TABLE spip_numeros CHANGE type_numero titre varchar(255) not null default ""')),
-			array('sql_alter', array('TABLE spip_emails CHANGE type_email titre varchar(255) not null default ""')));
+		array('sql_alter', 'TABLE spip_adresses CHANGE type_adresse titre varchar(255) not null default ""'),
+		array('sql_alter', 'TABLE spip_numeros CHANGE type_numero titre varchar(255) not null default ""'),
+		array('sql_alter', 'TABLE spip_emails CHANGE type_email titre varchar(255) not null default ""')
+	);
 
 	// On passe les pays en code ISO, beaucoup plus génériques que les ids SQL
 	$maj['1.3'] = array(
-			array('sql_alter', array('TABLE spip_adresses ADD pays_code varchar(2) not null default ""')),
-			array('coordonnees_upgrade_1_3'),
-			array('sql_alter', array('TABLE spip_adresses DROP pays')),
-			array('sql_alter', array('TABLE spip_adresses CHANGE pays_code pays varchar(2) not null default ""')),
-
-			);
+		array('sql_alter', 'TABLE spip_adresses ADD pays_code varchar(2) not null default ""'),
+		array('coordonnees_upgrade_1_3'),
+		array('sql_alter', 'TABLE spip_adresses DROP pays'),
+		array('sql_alter', 'TABLE spip_adresses CHANGE pays_code pays varchar(2) not null default ""'),
+	);
 
 	// On avait supprimer les types, mais ils reviennent en force mais dans les LIENS
 	$maj['1.4'] = array(
-			array('sql_alter', array('TABLE spip_adresses_liens ADD type varchar(25) not null default ""')),
-			array('sql_alter', array('TABLE spip_adresses_liens DROP PRIMARY KEY')),
-			array('sql_alter', array('TABLE spip_adresses_liens ADD PRIMARY KEY (id_adresse, id_objet, objet, type)')),
-			array('sql_alter', array('TABLE spip_numeros_liens ADD type varchar(25) not null default ""')),
-			array('sql_alter', array('TABLE spip_numeros_liens DROP PRIMARY KEY')),
-			array('sql_alter', array('TABLE spip_numeros_liens ADD PRIMARY KEY (id_numero, id_objet, objet, type)')),
-			array('sql_alter', array('TABLE spip_emails_liens ADD type varchar(25) not null default ""')),
-			array('sql_alter', array('TABLE spip_emails_liens DROP PRIMARY KEY')),
-			array('sql_alter', array('TABLE spip_emails_liens ADD PRIMARY KEY (id_email, id_objet, objet, type)')),
+			array('sql_alter', 'TABLE spip_adresses_liens ADD type varchar(25) not null default ""'),
+			array('sql_alter', 'TABLE spip_adresses_liens DROP PRIMARY KEY'),
+			array('sql_alter', 'TABLE spip_adresses_liens ADD PRIMARY KEY (id_adresse, id_objet, objet, type)'),
+			array('sql_alter', 'TABLE spip_numeros_liens ADD type varchar(25) not null default ""'),
+			array('sql_alter', 'TABLE spip_numeros_liens DROP PRIMARY KEY'),
+			array('sql_alter', 'TABLE spip_numeros_liens ADD PRIMARY KEY (id_numero, id_objet, objet, type)'),
+			array('sql_alter', 'TABLE spip_emails_liens ADD type varchar(25) not null default ""'),
+			array('sql_alter', 'TABLE spip_emails_liens DROP PRIMARY KEY'),
+			array('sql_alter', 'TABLE spip_emails_liens ADD PRIMARY KEY (id_email, id_objet, objet, type)'),
 
 			);
 	// mettre les auteurs par defaut comme objet «coordonnable»
 	$maj['1.5'] = array(
-			array('ecrire_meta',array('coordonnees', serialize(array('objets'=>array('auteur'))))));
+		array('ecrire_meta','coordonnees', serialize(array('objets'=>array('auteur'))))
+	);
 
 	// ajout du champs region a la table adresses
 	$maj['1.6'] = array(
@@ -60,13 +62,13 @@ function coordonnees_upgrade($nom_meta_base_version, $version_cible){
 	// migration de certaines valeurs pour pouvoir faire fonctionner les selecteurs pendant l'edition
 	//!\ comme on n'est pas certain de tous les migrer il y a donc rupture de compatibilite ? :-S
 	$maj['1.7'] = array(
-		array('sql_updateq' array("spip_adresses_liens", array('type'=>'work'), "LOWER(type) LIKE 'pro%'") ),
-		array('sql_updateq' array("spip_numeros_liens", array('type'=>'work'), "LOWER(type) LIKE 'pro%'") ),
-		array('sql_updateq' array("spip_adresses_liens", array('type'=>'home'), "LOWER(type) LIKE 'perso%'") ),
-		array('sql_updateq' array("spip_adresses_liens", array('type'=>'home'), "LOWER(type) LIKE 'dom%'") ),
-		array('sql_updateq' array("spip_numeros_liens", array('type'=>'home'), "LOWER(type) LIKE 'perso%'") ),
-		array('sql_updateq' array("spip_numeros_liens", array('type'=>'cell'), "LOWER(type) LIKE 'cel%'") ),
-		array('sql_updateq' array("spip_numeros_liens", array('type'=>'cell'), "LOWER(type) LIKE 'mob%'") ),
+		array('sql_updateq', "spip_adresses_liens", array('type'=>'work'), "LOWER(type) LIKE 'pro%'"),
+		array('sql_updateq', "spip_numeros_liens", array('type'=>'work'), "LOWER(type) LIKE 'pro%'"),
+		array('sql_updateq', "spip_adresses_liens", array('type'=>'home'), "LOWER(type) LIKE 'perso%'"),
+		array('sql_updateq', "spip_adresses_liens", array('type'=>'home'), "LOWER(type) LIKE 'dom%'"),
+		array('sql_updateq', "spip_numeros_liens", array('type'=>'home'), "LOWER(type) LIKE 'perso%'"),
+		array('sql_updateq', "spip_numeros_liens", array('type'=>'cell'), "LOWER(type) LIKE 'cel%'"),
+		array('sql_updateq', "spip_numeros_liens", array('type'=>'cell'), "LOWER(type) LIKE 'mob%'"),
 	);
 
 	include_spip('base/upgrade');
