4f7be6ec0f5918c46fc878011958129449b68b20
prigent.yohann@gmail.com <prigent.yohann@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-10-12 16:18:49 +0000

    version 3.0
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@66758 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ spip_suggest/formulaires/configurer_spip_suggest.php
@@ -0,0 +1,26 @@
+<?php
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+function formulaires_configurer_spip_suggest_charger_dist(){
+	foreach(array(
+		"db_name", "db_keys", "suggest_width", "suggest_selecteur", "suggest_classement"
+		) as $m)
+		$valeurs[$m] = $GLOBALS['meta'][$m];
+
+	return $valeurs;
+}
+
+
+function formulaires_configurer_spip_suggest_traiter_dist(){
+	$res = array('editable'=>true);
+	foreach(array(
+		"db_name", "db_keys", "suggest_width", "suggest_selecteur", "suggest_classement"
+		) as $m)
+			ecrire_meta($m, _request($m));
+
+	$res['message_ok'] = _T('config_info_enregistree');
+	return $res;
+}
+
+?>
