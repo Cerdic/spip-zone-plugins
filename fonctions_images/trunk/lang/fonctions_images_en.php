dd52459d4eaf37a9246a2986e4792d9883a3abdb
tcharlss@hotmail.fr <tcharlss@hotmail.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-10-14 14:35:04 +0000

    Ajout de fichiers de langues pour la page démo
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@66787 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ fonctions_images/trunk/lang/fonctions_images_en.php
@@ -0,0 +1,16 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// O
+	'onglet_filtres_couleurs' => 'Color filters',
+	'onglet_filtres_images' => 'Image filters',
+
+	// T
+	'titre_page_demo' => 'Images functions demo page'
+);
+?>
---
+++ fonctions_images/trunk/lang/fonctions_images_fr.php
@@ -0,0 +1,16 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// O
+	'onglet_filtres_couleurs' => 'Les filtres de couleur',
+	'onglet_filtres_images' => 'Les fitres de traitement d\'image',
+
+	// T
+	'titre_page_demo' => 'Démo du plugin fonction d\'images'
+);
+?>
