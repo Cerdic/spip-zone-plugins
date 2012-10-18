c514d246bb5334f408542c9ea3a784d2c51fc41c
cam.lafit@azerttyu.net <cam.lafit@azerttyu.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-10-18 13:08:44 +0000

    Finissons la mise en place de la configuration FORCER_SQUELETTE
     * Des chaines de langues pour comprendre ce qu'on fait c'est parfois utile
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@66902 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- seo/lang/seo_fr.php
+++ seo/lang/seo_fr.php
@@ -17,6 +17,10 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'canonical_url' => 'URL Canoniques',
 	'canonical_url_activate' => 'Activer les URL Canoniques',
 
+    // F
+    'forcer_squelette_descriptif' => 'Les metas SEO sont prioritaires aux meta générées par les squelettes',
+    'forcer_squelette_label' => 'Charger les metas pour tous squelettes',
+
 	// G
 	'google_analytics' => 'Google Analytics',
 	'google_analytics_activate' => 'Activer Analytics',
