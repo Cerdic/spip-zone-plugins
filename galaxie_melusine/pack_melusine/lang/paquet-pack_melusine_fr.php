e56936fdfc071c5a1b9f415f13d5c0e53eba4dfc
bmarne@gmail.com <bmarne@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-10-18 11:38:07 +0000

    Mélusine pack est un plugin "vide" qui necessite tous les plugins de la galaxie Mélusine et permet donc de tout installer en une opération grâce à SVP.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@66895 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ galaxie_melusine/pack_melusine/lang/paquet-pack_melusine_fr.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// P
+	'pack_melusine_description' => 'Un squelette modulable et facilement configurable.
+
+Avec en prime: des pages pour les mobiles, et un module de personnalisais des couleurs des articles.',
+	'pack_melusine_nom' => 'Pack Mélusine',
+	'pack_melusine_slogan' => 'L\'ensemble des plugins de la galaxie Mélusine',
+);
+
+?>
