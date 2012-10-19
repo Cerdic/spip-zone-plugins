cffb51589cfec499b3f4368063efeb765aa31270
joseph@larmarange.net <joseph@larmarange.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-10-19 21:47:40 +0000

    Fichiers manquants
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@66936 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ dublin_core/trunk/lang/paquet-dublin_core_fr.php
@@ -0,0 +1,23 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-dublin_core
+// Langue: fr
+// Date: 19-10-2012 13:56:43
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// D
+	'dublin_core_description' => 'Un petit squelette pour l\'insertion de méta-données Dublin Core dans la partie head des articles.
+	
+	« Le Dublin Core est un schéma de métadonnées générique qui permet de décrire des ressources numériques ou physiques et d\'établir des relations avec d\'autres ressources. » ([Wikipédia->http://fr.wikipedia.org/wiki/Dublin_Core]).
+	
+	[->http://www.dublincore.org/]',
+	'dublin_core_slogan' => 'Des Dublins Core pour vos articles',
+);
+?>
