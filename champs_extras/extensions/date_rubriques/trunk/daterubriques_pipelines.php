fd3beca60a4763c728ffe63d255cd8b1cb1f0b36
toutati@free.fr <toutati@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-10-20 21:38:01 +0000

    après moults  tests, bug réparé… le champ date_utile était updaté par la date du jour quand une modif de champs (titre) avait lieu avec crayons - retour welcome -
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@66976 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- champs_extras/extensions/date_rubriques/trunk/daterubriques_pipelines.php
+++ champs_extras/extensions/date_rubriques/trunk/daterubriques_pipelines.php
@@ -5,20 +5,13 @@ function daterubriques_pre_edition($flux){
 
 	// recuperer les champs crees par les plugins
 	if ($extras = cextras_get_extras_match($flux['args']['table'])) {
-		// recherchons un eventuel prefixe utilise pour poster les champs
-		$type = objet_type(table_objet($flux['args']['table']));
-		$prefixe = _request('prefixe_champs_extras_' . $type);
-		if (!$prefixe) {
-			$prefixe = '';
-		}
+
 		foreach ($extras as $c) {
 			if ( $c->champ == "date_utile" ) {
 				//On met la date saisie au format MySql AAAA-MM-JJ
-				if ($date = recup_date($flux['data']['date_utile'])) {				
-					$flux['data']['date_utile'] = date("Y-m-d",mktime($date[3],$date[4],0,$date[1],$date[2],$date[0]));
-				} else {
-					$flux['data']['date_utile'] = date("Y-m-d");
-				}
+				if ($date = recup_date($flux['data']['date_utile'])) {	
+					$flux['data']['date_utile'] = date("Y-m-d 00:00:00",mktime($date[3],$date[4],0,$date[1],$date[2],$date[0]));
+				} 
 			}
 		}
 	}
