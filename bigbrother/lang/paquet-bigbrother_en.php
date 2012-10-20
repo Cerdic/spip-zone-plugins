7efc5bba323679b6cc46528b1889a7b4d34a64c9
brunobergot@gmail.com <brunobergot@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-10-20 14:32:21 +0000

    début de compat SPIP 3 : refonte de la page de config + paquet.xml etc
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@66957 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ bigbrother/lang/paquet-bigbrother_en.php
@@ -0,0 +1,20 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-bigbrother
+// Langue: en
+// Date: 20-10-2012 15:28:26
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// B
+	'bigbrother_description' => 'This plugin allows you to store in a database the achievement of predefined actions 
+		(eg: visits of persons, length of visits, creation and modification of objects ...)',
+	'bigbrother_slogan' => 'This plugin allows you to store in a database the achievement of predefined actions',
+);
+?>
---
+++ bigbrother/lang/paquet-bigbrother_fr.php
@@ -0,0 +1,20 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-bigbrother
+// Langue: fr
+// Date: 20-10-2012 15:28:26
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// B
+	'bigbrother_description' => 'Ce plugin permet de stocker en base de donnée la réalisation d\'actions prédéfinies 
+		(ex: visites de personnes, duré des visites, création et modifications d\'objets ...)',
+	'bigbrother_slogan' => 'Ce plugin permet de stocker en base de donnée la réalisation d\'actions prédéfinies',
+);
+?>
