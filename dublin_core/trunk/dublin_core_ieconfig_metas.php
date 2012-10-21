995b286602adb6d44b90793efc353b21a071b5af
joseph@larmarange.net <joseph@larmarange.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-10-21 10:54:07 +0000

    Correction icone ieconfig
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@66986 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- dublin_core/trunk/dublin_core_ieconfig_metas.php
+++ dublin_core/trunk/dublin_core_ieconfig_metas.php
@@ -2,7 +2,7 @@
 
 function dublin_core_ieconfig_metas($table){
 	$table['dublin_core']['titre'] = _T('dublin_core:dublin_core');
-	$table['dublin_core']['icone'] = 'images/dublin_core-24.png';
+	$table['dublin_core']['icone'] = 'dublin_core-16.png';
 	$table['dublin_core']['metas_serialize'] = 'dublin_core';
 	return $table;
 }
