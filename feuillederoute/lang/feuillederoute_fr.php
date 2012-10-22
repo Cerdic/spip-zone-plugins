174e72547028413ec6d16979af2a0ecf23db54ce
chankalan@free.fr <chankalan@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-10-22 14:38:47 +0000

    correction
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@67006 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- feuillederoute/lang/feuillederoute_fr.php
+++ feuillederoute/lang/feuillederoute_fr.php
@@ -1,6 +1,5 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/manuel_site/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
