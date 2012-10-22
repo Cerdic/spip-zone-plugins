e6520a94ca72b285ad6e79dd43d998e09cec9d57
chankalan@free.fr <chankalan@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-10-22 14:38:07 +0000

    correction d'orthographe
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@67005 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- feuillederoute/lang/feuillederoute_fr.php
+++ feuillederoute/lang/feuillederoute_fr.php
@@ -9,7 +9,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'choisir_une_feuille' => 'Renseignez le numéro de la feuille dédiée et validez.',
 	'configurer_feuillederoute' => 'Configuration de la feuille de route.',
 	// Q
-	'quelle_feuille' => 'Quel feuille utiliser pour rédiger le texte de la feuille de route ?',
+	'quelle_feuille' => 'Quelle feuille utiliser pour rédiger le texte de la feuille de route ?',
 	
 	// T
 	'titre_menu' => 'Feuille de Route'
