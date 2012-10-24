7f13b0b60015b1e77156b460405743bf4a05b866
klaus@spip.de <klaus@spip.de@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-10-24 01:41:00 +0000

    langues (paquet-entravaux)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@67041 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ en_travaux/trunk/lang/paquet-entravaux_de.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-entravaux?lang_cible=de
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// E
+	'entravaux_description' => 'Dieses Plugin kann zu Wartungsarbeiten aktiviert werden, und zeigt dann eine Meldung {{anstelle}} des öffentlichen Bereichs der Website an. Achtung - im Wartungsmodus haben nur Webmaster Zugang zu Redaktionssystem und öffentlicher Website.',
+	'entravaux_nom' => 'Wartung',
+	'entravaux_slogan' => 'Ausgabe einer Wartungsmeldung'
+);
+
+?>
