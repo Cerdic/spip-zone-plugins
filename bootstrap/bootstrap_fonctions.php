587dad3f3cf308a812de704a7f3c405ec05010a7
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-10-25 16:09:35 +0000

    surcharger bouton_action pour placer les classes sur le button et non sur le form. On repere le cas particulier .ajax que l'on recopie sur le form pour que le js matche comme il faut. Cela risque de casser des choses, a tester...
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@67137 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- bootstrap/bootstrap_fonctions.php
+++ bootstrap/bootstrap_fonctions.php
@@ -44,3 +44,38 @@ function bootstrap_affichage_final($flux){
 	}
 	return $flux;
 }
+
+
+/**
+ * Generer un bouton_action
+ * utilise par #BOUTON_ACTION
+ *
+ * @param string $libelle
+ * @param string $url
+ * @param string $class
+ * @param string $confirm
+ *   message de confirmation oui/non avant l'action
+ * @param string $title
+ * @param string $callback
+ *   callback js a appeler lors de l'evenement action (apres confirmation eventuelle si $confirm est non vide)
+ *   et avant execution de l'action. Si la callback renvoie false, elle annule le declenchement de l'action
+ * @return string
+ */
+function filtre_bouton_action_dist($libelle, $url, $class="", $confirm="", $title="", $callback=""){
+	if ($confirm) {
+		$confirm = "confirm(\"" . attribut_html($confirm) . "\")";
+	  if ($callback)
+		  $callback = "$confirm?($callback):false";
+	  else
+		  $callback = $confirm;
+	}
+	$ajax = explode(" ",$class);
+	if (in_array("ajax",$ajax))
+		$ajax = " ajax";
+	else
+		$ajax = "";
+	$onclick = $callback?" onclick='return ".addcslashes($callback,"'")."'":"";
+	$title = $title ? " title='$title'" : "";
+	return "<form class='bouton_action_post$ajax' method='post' action='$url'><div>".form_hidden($url)
+		."<button type='submit' class='submit btn $class'$title$onclick>$libelle</button></div></form>";
+}
