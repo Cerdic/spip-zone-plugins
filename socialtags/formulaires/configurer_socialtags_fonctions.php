d4d53d7e382ec6f9d57eca340b88ffb18021410c
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-10-26 10:50:38 +0000

    utiliser <category> pour classer les services et placer dans un groupe de tête les quelques couramment usités. Facilite la configuration.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@67157 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- socialtags/formulaires/configurer_socialtags_fonctions.php
+++ socialtags/formulaires/configurer_socialtags_fonctions.php
@@ -7,18 +7,19 @@ function socialtags_choix(){
 	global $couleur_fonce;
 	$cfg = is_array($cfg = lire_config('socialtags/tags')) ? $cfg : array();
 
-	$retour = '';
+	$retour = array();
 	foreach (socialtags_liste() as $service) {
 		$t = $service['titre'];
 		$u = $service['url'];
 		$a = $service['lesauteurs'];
 		$d = isset($service['descriptif']) ? $service['descriptif'] : '';
 
+		$category = (count($service['tags'])?textebrut(reset($service['tags'])):'99');
 		$image = 'data:image/png;base64,'.base64_encode(file_get_contents(find_in_path('images/'.$a.'.png')));
 		//$image = find_in_path('images/'.$a.'.png');
 		$checked = in_array($a, $cfg) ? ' checked="checked"' : '';
 
-		$retour .= "<div class='choix'>
+		$retour[$category] .= "<div class='choix'>
 				<input type='checkbox' id='choix_{$a}' name='tags[]' value='{$a}'{$checked} />
 				<label for='choix_{$a}'>
 					<img src=\"{$image}\" title=\"".texte_script($t)."\" alt=\"\" />
@@ -27,6 +28,7 @@ function socialtags_choix(){
 				</label>
 			</div>";
 	}
-	return $retour;
+	ksort($retour);
+	return implode("<hr />",$retour);
 }
 ?>
