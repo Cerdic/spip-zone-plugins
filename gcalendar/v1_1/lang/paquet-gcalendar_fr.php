8ba6b900e658856fd4c14d5a193afd1b47e43a23
gilles@quiniou.net <gilles@quiniou.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-10-26 12:07:01 +0000

    Version spip 3 (sans cfg)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@67164 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ gcalendar/v1_1/lang/paquet-gcalendar_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// G
+	'gcalendar_description' => 'affiche un calendrier google',
+	'gcalendar_nom' => 'Gcalendar',
+	'gcalendar_slogan' => '',
+);
+
+?>
