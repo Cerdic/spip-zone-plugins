dd19ad7e0f5f8a86fee848749f7045bc9950b23f
klaus@spip.de <klaus@spip.de@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-10-27 11:57:08 +0000

    traduction allemande
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@67202 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ editer_liens_simples/lang/paquet-editer_liens_simples_de.php
@@ -0,0 +1,10 @@
+<?php
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+// E
+	'editer_liens_simples_nom' => 'Spalten einfach verknüpfen',
+	'editer_liens_simples_description' => 'Stellt eine API und ein Interface bereit, mit denen Tabellenspalten im Format spip_xxx_yyy (im Unterschied zu spip_xxx_verbindung) verbunden werden können.',
+	'editer_liens_simples_slogan' => 'API für Links zwischen Spalten im Format spip_xxx_yyy',
+);
+?>
