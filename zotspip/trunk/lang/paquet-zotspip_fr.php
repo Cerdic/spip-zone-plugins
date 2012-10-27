5666ed186f571edb8b7b13efda59c790fae00fb9
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-10-27 01:39:47 +0000

    langues (paquet-zotspip)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@67195 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- zotspip/trunk/lang/paquet-zotspip_fr.php
+++ zotspip/trunk/lang/paquet-zotspip_fr.php
@@ -1,19 +1,13 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-zotspip
-// Langue: fr
-// Date: 20-10-2012 16:12:51
-// Items: 2
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/zotspip/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// Z
+	// Z
 	'zotspip_description' => 'Synchronise Spip avec une bibliothèque (personnelle ou collective) de références bibliographiques Zotero. Utilisez Zotero pour gérer / importer / rédiger vos références bibliographiques, puis incorporez vos références bibliographiques dans votre Spip avec ZotSpip au travers de modèles dédiés.',
-	'zotspip_slogan' => 'La puissance de Zotero dans votre Spip',
+	'zotspip_slogan' => 'La puissance de Zotero dans votre Spip'
 );
+
 ?>
