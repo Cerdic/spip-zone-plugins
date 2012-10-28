978a9a0d19ecfe6b5944d272234a17dd23a5b15b
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-10-28 19:10:47 +0000

    Filtrer les archives non lisibles, corrompues ou vides. A voir si le nettoyage ne devait pas les supprimer aussi.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@67230 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- mes_fichiers/trunk/inc/mes_fichiers_utils.php
+++ mes_fichiers/trunk/inc/mes_fichiers_utils.php
@@ -80,6 +80,15 @@ function mes_fichiers_a_sauver() {
 function mes_fichiers_a_telecharger() {
 	$prefixe = lire_config('mes_fichiers/prefixe','mf2');
 	$liste = preg_files(_DIR_MES_FICHIERS . $prefixe.'_*.zip', 20);
+
+	// On filtre les fichiers vides ou corrompues qui sont des résultats d'erreur lors de l'archivage
+	foreach ($liste as $_cle => $_archive) {
+		if (!is_file($_archive)
+		OR !is_readable($_archive)
+		OR (filesize($_archive) == 0))
+			unset($liste[$_cle]);
+	}
+
 	return array_reverse($liste);
 }
 
