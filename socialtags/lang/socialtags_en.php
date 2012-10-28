ae2c7b5b53e968ed15116264688f1cf4950318f5
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-10-28 02:42:05 +0000

    langues (socialtags)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@67213 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- socialtags/lang/socialtags_en.php
+++ socialtags/lang/socialtags_en.php
@@ -32,8 +32,8 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'cfg_titre_socialtags' => 'Social tags',
 	'cfg_tout_cocher' => 'Check all',
 	'cfg_tout_decocher' => 'Uncheck all',
-	'cfg_wopen' => 'Mode pop-up', # NEW
-	'cfg_wopen_exp' => 'Ouvrir le lien vers le service dans une nouvelle fenêtre', # NEW
+	'cfg_wopen' => 'Popup mode',
+	'cfg_wopen_exp' => 'Open the service link in a new window',
 
 	// D
 	'description_noisette_badge_facebook' => 'Display the Facebook badge defined in the configuration page',
