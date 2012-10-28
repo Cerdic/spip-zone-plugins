ce0b1ad182265ecc3e4ade7910cfd1961b14c9ea
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-10-28 17:50:17 +0000

    On surcharge un define de la librairie afin de permettre la création des fichiers temporaires dans le répertoires des archives dont on sait qu'il est toujours accessible en écriture (spipfactory pour la remontée du problème en mutu)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@67228 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- mes_fichiers/trunk/mes_fichiers_options.php
+++ mes_fichiers/trunk/mes_fichiers_options.php
@@ -7,5 +7,6 @@ if(!isset($GLOBALS['spip_pipeline']['post_sauvegarde'])){
 
 // Repertoire de stockage des archives creees
 if (!defined('_DIR_MES_FICHIERS')) define('_DIR_MES_FICHIERS', _DIR_TMP . 'mes_fichiers/');
+define('PCLZIP_TEMPORARY_DIR', _DIR_MES_FICHIERS);
 
 ?>
