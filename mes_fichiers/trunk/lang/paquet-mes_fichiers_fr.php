6d114a3346680971b2093a50161975f9db2e6a36
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-10-29 02:40:25 +0000

    langues (paquet-mes_fichiers)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@67232 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- mes_fichiers/trunk/lang/paquet-mes_fichiers_fr.php
+++ mes_fichiers/trunk/lang/paquet-mes_fichiers_fr.php
@@ -1,21 +1,17 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-// Fichier produit par PlugOnet
-// Module: paquet-mes_fichiers
-// Langue: fr
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/mes_fichiers/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// M
+	// M
 	'mes_fichiers_description' => 'Ce plugin fabrique un fichier d\'archives contenant les données de personnalisation du site comme le dernier dump de sauvegarde de la base, les dossiers des squelettes personnalisés, le dossier des images...
 Le fichier d\'archives est créé dans <code>tmp/mes_fichiers/</code> et contient dans son nom la date de création.
 
 Il est aussi possible de lancer une sauvegarde régulière automatique de tous les fichiers de personnalisation combinée à un nettoyage des archives obsolètes et de notifier toutes les actions.',
 	'mes_fichiers_nom' => 'Mes fichiers',
-	'mes_fichiers_slogan' => 'Archiver ses fichiers et dossiers de personnalisation',
+	'mes_fichiers_slogan' => 'Archiver ses fichiers et dossiers de personnalisation'
 );
+
 ?>
