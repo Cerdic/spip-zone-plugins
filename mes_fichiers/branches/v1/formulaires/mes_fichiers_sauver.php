c0418b5b3ff8d9d714dc93141a10ff8670b1cf08
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-10-29 18:27:52 +0000

    Oups, un copier-coller malencontreux (merci kent1)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@67255 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- mes_fichiers/branches/v1/formulaires/mes_fichiers_sauver.php
+++ mes_fichiers/branches/v1/formulaires/mes_fichiers_sauver.php
@@ -24,7 +24,7 @@ function formulaires_mes_fichiers_sauver_traiter_dist(){
 	$sauver = charger_fonction('mes_fichiers_sauver','action');
 	$erreur = $sauver();
 	if($erreur){
-		$retour['message_erreur'] =	_T('mes_fichiers:message_sauvegarde_nok') .	" $erreur";
+		$res['message_erreur'] =	_T('mes_fichiers:message_sauvegarde_nok') .	" $erreur";
 	}else{
 		$res['message_ok'] = _T('mes_fichiers:message_sauvegarde_ok');
 	}
