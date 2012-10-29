0ff28bb719f9ecaa119c1abec2aaed06717744f2
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-10-29 15:37:33 +0000

    Pétouilles dans le paquet.xml
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@67251 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- saveauto/trunk/lang/paquet-saveauto_fr.php
+++ saveauto/trunk/lang/paquet-saveauto_fr.php
@@ -14,14 +14,21 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 // S
 	'saveauto_description' => 'Permet de réaliser une sauvegarde de toute la base de données utilisée par SPIP.
+
 			Le fichier .zip (ou .sql) obtenu est stocké dans un répertoire (par défaut /tmp/dump, configurable)
+
 			et peut être envoyé par mail.
+
 			
+
 			Les sauvegardes stockées considérées comme obsolètes (en fonction du paramètre de config correspondant)
+
 			sont automatiquement détruites.
+
 			
+
 			Une interface permet de déclencher manuellement les sauvegardes et de gérer les fichiers générés',
-	'saveauto_nom' => 'sauvegarde automatique',
+	'saveauto_nom' => 'Sauvegarde automatique',
 	'saveauto_slogan' => 'Sauvegarde automatique de la base de données de SPIP',
 );
 ?>
