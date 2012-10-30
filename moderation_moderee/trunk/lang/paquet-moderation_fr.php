8b0c1e4acb67f6a761a14c4d1552e8a4b7a7d36b
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-10-30 13:53:09 +0000

    passage en paquet.xml
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@67289 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ moderation_moderee/trunk/lang/paquet-moderation_fr.php
@@ -0,0 +1,21 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-moderation
+// Langue: fr
+// Date: 30-10-2012 14:47:05
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// M
+	'moderation_description' => 'Ce plugin permet aux utilisateurs des forums publics configurés {{à priori}}, qui sont enregistrés et logués en tant que visiteur rédacteur ou admin, de ne pas subir ce type de modération. Leur commentaire est automatiquement validé sans qu\'il y ait une action de la part d\'un modérateur des forums depuis l\'espace privée.
+_ Ceci ne fonctionne que pour les forums publics {{à priori}}.
+_ On peut choisir quels types de commentateurs auront droit à la modération modérée (visiteur / rédacteur / admin) mais ils doivent tous être logués.',
+	'moderation_slogan' => 'Amélioration de la modération',
+);
+?>
