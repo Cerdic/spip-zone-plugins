2f5acda48dc19a5434fd0cf4ecc3df32a1e58efc
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-10-30 14:44:48 +0000

    installation correcte
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@67292 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ moderation_moderee/trunk/moderation_administrations.php
@@ -0,0 +1,22 @@
+<?php
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+include_spip('inc/config');
+include_spip('base/upgrade');
+function moderation_upgrade($nom_meta_version_base, $version_cible){
+    $maj = array();
+    var_dump($maj);
+    $maj['create'] = array(array("moderation_create"));
+    maj_plugin($nom_meta_version_base, $version_cible, $maj);
+}
+function moderation_create(){
+    ecrire_config('moderation/visit', 'oui');
+    ecrire_config('moderation/redac', 'oui');
+    ecrire_config('moderation/admin', 'oui');
+    return true;
+}
+function moderation_vider_tables($nom_meta_version_base){
+      effacer_config("moderation");
+      effacer_meta($nom_meta_version_base);  
+}
+?>
