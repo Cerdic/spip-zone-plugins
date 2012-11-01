dc2775f0e351319d6de966bf89b7aac7b851d169
klaus@spip.de <klaus@spip.de@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-11-01 02:40:49 +0000

    langues (paquet-ancresdouces)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@67378 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ ancres_douces/lang/paquet-ancresdouces_de.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-ancresdouces?lang_cible=de
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'ancresdouces_description' => 'Dieses Plugin ermöglicht ein sanftes Gleiten zu einem Ankerpunkt, wenn der Benutzer einen Link zu ihm anklickt.',
+	'ancresdouces_nom' => 'Soft-Anker',
+	'ancresdouces_slogan' => 'Anker werden gleitend angesteuert.'
+);
+
+?>
