28a8033a69f6cc02e3f7c244a7d5505421b7b3eb
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-11-06 14:10:10 +0000

    jQuery et pas $
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@67508 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- bootstrap/bootstrap2spip/js/hashgrid.js
+++ bootstrap/bootstrap2spip/js/hashgrid.js
@@ -426,7 +426,7 @@ var hashgrid = function(set) {
  * You can call hashgrid from your own code, but it's loaded here as
  * an example for your convenience.
  */
-$(document).ready(function() {
+jQuery(document).ready(function() {
 
 	var grid = new hashgrid({
 		id: 'bsgrid',
