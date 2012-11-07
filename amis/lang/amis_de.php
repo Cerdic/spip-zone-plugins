9202b9367924116a7b8467cb0c37728be82bd173
klaus@spip.de <klaus@spip.de@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-11-07 18:07:19 +0000

    traduction allemande v001
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@67569 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ amis/lang/amis_de.php
@@ -0,0 +1,44 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+'ajouter_a_mes_amis' => 'zu meinen Freunden hinzufügen',
+
+'des_invitations_amis' => '@nb@ Einladungen von Freunden',
+
+'envoyer_un_message' => 'Nachricht senden',
+
+'invitations' => 'Meine Einladungen',
+'inviter_ami' => 'Einen Freund einladen',
+
+'label_email' => 'Email',
+'label_message' => 'Persönliche Nachricht hinzufügen',
+'label_inviter' => 'Einladen',
+'label_recherche_ami' => 'Suchen',
+'label_rechercher' => 'Suchen',
+
+'mail_ajouterami_texte' => 'Hallo @nom@ ! @ami@ hat Deine Einladung auf @nom_site@ angenommen.',
+'mail_ajouterami_titre' => 'Einladung auf @nom_site@ angenommen.',
+'mail_inviterami_texte' => 'Hallo @nom@ ! @ami@ möchte Dich zu seinen Freunden auf @nom_site@ hinzufügen.',
+'mail_inviterami_titre' => 'Einaldung auf die Website @nom_site@.',
+'mail_inviterami_explication' => 'Deine Freundesliste verwalten:',
+'mail_rejoignez_sujet' => '@nom@ läd Dich auf @nom_site@ ein',
+'mail_rejoignez_message' => 'Werde Mitglied auf @nom_site@ (@url_site@) ! @nom@ macht schon mit.',
+
+'nb_amis_un' => 'ein Freund',
+'nb_amis' => '@nb@ Freunde',
+
+'rechercher_ami' => 'Freund suchen',
+
+'sollicitations' => 'Anfragen',
+
+'titre_amis' => 'Freunde',
+'titre_amis_auteur' => 'Freunde von @nom@',
+
+'une_invitation_amis' => '1 Einladung von einem Freund',
+
+'votre_ami_a_ete_invite' => 'Deine Einladung wurde an @email@ gesendet.'
+
+);
+?>
