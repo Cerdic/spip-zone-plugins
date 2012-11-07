a5071ebc29eea1cc3588abd5887a924f83023795
pierrekuhn82@gmail.com <pierrekuhn82@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-11-07 16:48:58 +0000

    On remonte le trunkdepuis la v0.1 pour ce soir
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@67559 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ analyclick/trunck/lang/anaclic_fr.php
@@ -0,0 +1,20 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+
+'statistiques_documents'		=> 'T&eacute;l&eacute;chargements',
+'configurer'					=> 'Configuration des t&eacute;l&eacute;chargements',
+'configurer_info'				=> 'Choississez un d&eacute;lais entre deux t&eacute;l&eacute;chargements. Les clics pendant ce d&eacute;lais ne seront pas comptabilis&eacute;s.<br>Choississez un d&eacute;lais n&eacute;gatif pour un d&eacute;lais infini (un seul t&eacute;l&eacute;chargement par IP).',
+'delais'						=> 'D&eacute;lais entre deux t&eacute;l&eacute;chargements',
+'info_afficher'					=> 'Afficher les statistiques de :', 
+'info_telechargement_30'		=> 'Afficher les statistiques de t&eacute;l&eacute;chargement <b>sur les 30 derniers jours</b> :',
+'info_telechargement'			=> 'Afficher les statistiques de t&eacute;l&eacute;chargement <b>depuis le d&eacute;but</b> :',
+'no_statistique'				=> 'Pas de statistique disponible'
+
+);
+
+
+?>
