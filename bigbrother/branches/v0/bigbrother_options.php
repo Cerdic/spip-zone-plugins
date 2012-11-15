5c25d683b2bdd669d7b41dfcf93ef20fd3524762
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-11-15 12:03:01 +0000

    define('_CACHE_CONTEXTES_AJAX',true); est trop expérimental pour l'utiliser sur tous les sites installant le plugin, autre solution à trouver
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@67750 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- bigbrother/branches/v0/bigbrother_options.php
+++ bigbrother/branches/v0/bigbrother_options.php
@@ -18,5 +18,4 @@ if(!defined('_DIR_LIB_FLOT')){
 	define('_DIR_LIB_FLOT','lib/flot');
 }
 
-define('_CACHE_CONTEXTES_AJAX',true);
 ?>
--- bigbrother/trunk/bigbrother_options.php
+++ bigbrother/trunk/bigbrother_options.php
@@ -13,5 +13,4 @@ if(!defined('_DIR_LIB_FLOT')){
 	define('_DIR_LIB_FLOT','lib/flot');
 }
 
-define('_CACHE_CONTEXTES_AJAX',true);
 ?>
