c49bc59b9d6253b5ff1fff9d8428e1ee701b7aa2
brunobergot@gmail.com <brunobergot@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-11-17 17:29:29 +0000

    version 4.1.13 : ajout d'une option overview=oui pour afficher une mini carte dans le coin en bas à droite de la carte
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@67776 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- gis/trunk/gis_options.php
+++ gis/trunk/gis_options.php
@@ -2,7 +2,7 @@
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
-define('_DIR_LIB_GIS','lib/leaflet-gis-4.1.11/');
+define('_DIR_LIB_GIS','lib/leaflet-gis-4.1.13/');
 
 $GLOBALS['logo_libelles']['id_gis'] = _T('gis:libelle_logo_gis');
 
