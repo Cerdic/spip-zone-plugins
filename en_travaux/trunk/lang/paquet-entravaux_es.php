5cb30c6e20483f0a918b658c6f3af419dbec7c79
dani@belvil.net <dani@belvil.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-11-19 02:40:56 +0000

    langues (paquet-entravaux)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@67796 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ en_travaux/trunk/lang/paquet-entravaux_es.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-entravaux?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// E
+	'entravaux_description' => 'Para ser activado antes de una fase de mantenimiento, este ce plugin muestra entonces una página de advertencia, {{en vez}} del sitio público. Cuidado: sólo el webmaster aún accede al espacio privado y al sitio público.',
+	'entravaux_nom' => 'En obras',
+	'entravaux_slogan' => 'Indicar una fase de mantenimiento'
+);
+
+?>
