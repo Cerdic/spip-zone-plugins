e7550c198dd1adaec580a55982cd605b2cf20489
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-11-19 02:41:01 +0000

    langues (mes_fichiers)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@67798 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- mes_fichiers/trunk/lang/mes_fichiers_en.php
+++ mes_fichiers/trunk/lang/mes_fichiers_en.php
@@ -43,19 +43,19 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'label_cfg_prefixe' => 'Préfix',
 	'label_cfg_sauvegarde_reguliere' => 'Enable automatic backups',
 	'label_cfg_taille_max_rep' => 'Maximum folder\'s size',
-	'legende_cfg_generale' => 'Paramètres d\'archivage généraux', # NEW
+	'legende_cfg_generale' => 'Common backup parameters',
 	'legende_cfg_notification' => 'Notifications',
 	'legende_cfg_sauvegarde_reguliere' => 'Automatic actions',
 
 	// M
-	'message_cleaner_sujet' => 'Deleting backups', # MODIF
-	'message_notif_cleaner_intro' => 'Automatic deletion of obsolete backup files (creation date former than @duree@ days) performed successfully. The following files have been deleted : ', # MODIF
-	'message_notif_sauver_intro' => 'New backup is available. It was created by @auteur@.', # MODIF
+	'message_cleaner_sujet' => 'Deleting backups',
+	'message_notif_cleaner_intro' => 'Automatic deletion of obsolete backups (creation date former than @duree@ days) was performed successfully. The following files have been deleted : ',
+	'message_notif_sauver_intro' => 'New backup is available. It was created by @auteur@.',
 	'message_rien_a_sauver' => 'Neither file nor folder to backup.',
 	'message_rien_a_telecharger' => 'No backup available to download.',
 	'message_sauvegarde_nok' => 'Backup error. The archive file has not been created.',
 	'message_sauvegarde_ok' => 'The archive file has been created.',
-	'message_sauver_sujet' => 'Files backup', # MODIF
+	'message_sauver_sujet' => 'Backup',
 	'message_telechargement_nok' => 'Downloading error.',
 	'message_zip_auteur_indetermine' => 'Undetermined',
 	'message_zip_propriete_nok' => 'No property available on this archive.',
