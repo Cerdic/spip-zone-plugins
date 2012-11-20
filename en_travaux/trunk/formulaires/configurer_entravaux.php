b6f1024fb606fe8bb008fccfa3636fc09b673347
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-11-20 14:24:54 +0000

    Faire marcher le formulaire, pour cause de meta mal nommée à la lecture (oups)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@67825 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- en_travaux/trunk/formulaires/configurer_entravaux.php
+++ en_travaux/trunk/formulaires/configurer_entravaux.php
@@ -16,7 +16,7 @@ function formulaires_configurer_entravaux_charger_dist(){
 	$valeurs = array(
 		'accesferme' => is_entravaux()?'1':'',
 		'message' => $GLOBALS['meta']['entravaux_message'],
-		'disallow_robots' => $GLOBALS['meta']['disallow_robots'],
+		'disallow_robots' => $GLOBALS['meta']['entravaux_disallow_robots'],
 	);
 
 	return $valeurs;
@@ -36,7 +36,7 @@ function formulaires_configurer_entravaux_traiter_dist(){
 
 
 	foreach(array('message','disallow_robots') as $k)
-		ecrire_meta('entravaux_'.$k,_request($k),'non');
+		ecrire_meta('entravaux_'.$k,_request($k)?_request($k):'','non');
 
 	return array('message_ok' => _T('config_info_enregistree'));
 }
