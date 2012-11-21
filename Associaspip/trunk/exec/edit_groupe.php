de65a54d7bced10c0c09983e31570c9656613b41
esj@rezo.net <esj@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-11-21 07:22:57 +0000

    [source:_plugins_/Associaspip Associaspip] bug: on ne pouvait pas créer un groupe.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@67834 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- Associaspip/trunk/exec/edit_groupe.php
+++ Associaspip/trunk/exec/edit_groupe.php
@@ -12,21 +12,31 @@ if (!defined('_ECRIRE_INC_VERSION'))
 	return;
 
 function exec_edit_groupe() {
-	$r = association_controle_id('groupe', 'asso_groupes', 'editer_groupes');
-	if ($r) {
-		include_spip ('inc/navigation_modules');
-		list($id_groupe, $groupe) = $r;
-		onglets_association('gestion_groupes', 'adherents');
-		// INFO
-		$infos['entete_utilise'] = _T('asso:nombre_fois', array('nombre'=>sql_countsel('spip_asso_groupes_liaisons',"id_groupe=$id_groupe")) );
-		echo association_totauxinfos_intro($groupe['nom'], 'groupe', $id_groupe, $infos );
-		// datation et raccourcis
-		raccourcis_association('groupes');
-		debut_cadre_association('annonce.gif', ($id_groupe)?'titre_editer_groupe':'titre_creer_groupe');
-		echo recuperer_fond('prive/editer/editer_asso_groupes', array (
-			'id' => $id_groupe
-		));
-		fin_page_association();
+	if (!autoriser('editer_groupes', 'association')) {
+		include_spip('inc/minipres');
+		echo minipres();
+	} else {
+		$id_groupe = association_passeparam_id('groupe');
+		if (!$id_groupe)
+			$r = array(0, array());
+		else  $r = association_controle_id('groupe', 'asso_groupes');
+		if ($r) {
+			include_spip ('inc/navigation_modules');
+			list($id_groupe, $groupe) = $r;
+			onglets_association('gestion_groupes', 'adherents');
+			if ($groupe) {
+				$infos = sql_countsel('spip_asso_groupes_liaisons',"id_groupe=$id_groupe");
+				$infos = array('entete_utilise' => _T('asso:nombre_fois', array('nombre'=> $infos)));
+				echo association_totauxinfos_intro($groupe['nom'], 'groupe', $id_groupe, $infos );
+				$titre = 'titre_editer_groupe';
+			} else  $titre = 'titre_creer_groupe';
+			// datation et raccourcis
+			raccourcis_association('groupes');
+			debut_cadre_association('annonce.gif', $titre);
+			echo recuperer_fond('prive/editer/editer_asso_groupes',
+					array ('id' => $id_groupe));
+			fin_page_association();
+		}
 	}
 }
 
