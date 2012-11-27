ff24b184e20d56afe8dc03cb36d85dbd1240c584
olivier.gautier@ac-rouen.fr <olivier.gautier@ac-rouen.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-11-27 16:16:29 +0000

    Oups, le flux gauche était cassé dans les articles et rubriques, on code ça plus proprement avec le pipeline affiche_gauche.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@67959 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- compteur_graphique_3/compteurgraphique_pipeline.php
+++ compteur_graphique_3/compteurgraphique_pipeline.php
@@ -6,4 +6,19 @@ function compteurgraphique_declarer_tables_interfaces($interface){
 		$interface['table_des_tables']['compteurgraphique']='compteurgraphique';
 	return $interface;
 }
+function compteurgraphique_affiche_gauche($flux){
+	$exec = $flux['args']['exec'];
+	$test_configuration = sql_select("id_compteur",spip_compteurgraphique,"statut = 9");
+	$tab_configuration = sql_fetch($test_configuration);
+	$res_configuration = $tab_configuration['id_compteur'];
+	if (!isset($res_configuration) OR ($GLOBALS['connect_statut'] == "0minirezo")) {
+		if (($exec == 'article_edit') OR ($exec == 'article')) {
+			$flux['data'] .= recuperer_fond('prive/squelettes/navigation/article_compteurgraphique',array('id_article' => $id_article));
+		}
+		if (($exec == 'rubrique_edit') OR ($exec == 'rubrique')) {
+			$flux['data'] .= recuperer_fond('prive/squelettes/navigation/rubrique_compteurgraphique',array('id_rubrique' => $id_rubrique));
+		}
+	}
+	return $flux;
+}
 ?>
