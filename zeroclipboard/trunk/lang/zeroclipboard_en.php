a0335481109b30b1a3356bddfe2ae15f12b38835
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-11-29 02:40:15 +0000

    langues (zeroclipboard)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@68001 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- zeroclipboard/trunk/lang/zeroclipboard_en.php
+++ zeroclipboard/trunk/lang/zeroclipboard_en.php
@@ -1,5 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/zeroclipboard?lang_cible=en
 // ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
