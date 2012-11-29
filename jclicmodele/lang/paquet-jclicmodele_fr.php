f7d2357a787dea7091c3f797fd6d96fce1246fd4
olivier.gautier@ac-rouen.fr <olivier.gautier@ac-rouen.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-11-29 11:08:59 +0000

    Mise à jour du plugin JclicModele pour SPIP 3
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@68006 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ jclicmodele/lang/paquet-jclicmodele_fr.php
@@ -0,0 +1,8 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+'jclicmodele_slogan' => 'Mod&#232;le permettant d&#8217;ins&#233;rer des activit&#233;s Jclic',
+'jclicmodele_description' => "Ce plugin propose un mod&#232;le permettant d&#8217;ins&#233;rer des activit&#233;s Jclic dans votre site.",
+);
+?>
