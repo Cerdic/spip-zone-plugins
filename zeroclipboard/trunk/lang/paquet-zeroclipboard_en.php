ec278fadb63565affe3538ff348497ca60783e18
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-11-29 02:40:16 +0000

    langues (paquet-zeroclipboard)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@68002 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- zeroclipboard/trunk/lang/paquet-zeroclipboard_en.php
+++ zeroclipboard/trunk/lang/paquet-zeroclipboard_en.php
@@ -1,6 +1,8 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/zeroclipboard/lang
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-zeroclipboard?lang_cible=en
+// ** ne pas modifier le fichier **
+
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
--- zeroclipboard/trunk/lang/paquet-zeroclipboard_fr.php
+++ zeroclipboard/trunk/lang/paquet-zeroclipboard_fr.php
@@ -1,12 +1,12 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/zeroclipboard/lang
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/zeroclipboard/trunk/lang
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// Z
-	'zeroclipboard_description' => 'Ajoute un lien "Copier" avant chaque &#233;l&#233;ment de class <code>.copypaste</code> permettant de mettre son contenu dans le presse-papier',
+	'zeroclipboard_description' => 'Ajoute un lien "Copier" avant chaque élément de class <code>.copypaste</code> permettant de mettre son contenu dans le presse-papier',
 	'zeroclipboard_nom' => 'Zeroclipboard',
 	'zeroclipboard_slogan' => 'Copier coller facile'
 );
