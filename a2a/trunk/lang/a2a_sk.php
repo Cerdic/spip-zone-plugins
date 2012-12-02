6445d895745a46964df564c0f17634747d84a147
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-12-02 02:40:37 +0000

    langues (a2a)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@68064 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- a2a/trunk/lang/a2a_sk.php
+++ a2a/trunk/lang/a2a_sk.php
@@ -11,14 +11,14 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'articles_lies' => 'PREPOJENÉ ČLÁNKY',
 
 	// C
-	'cfg_a2a' => 'Relations entre articles', # NEW
-	'changer_type_liaison' => 'Changer', # NEW
-	'configurer_a2a' => 'Configurer les relations entre articles', # NEW
+	'cfg_a2a' => 'Vzťahy medzi článkami',
+	'changer_type_liaison' => 'Zmeniť',
+	'configurer_a2a' => 'Nastaviť vzťahy medzi článkami',
 
 	// E
 	'explication_navigateur' => 'Ak sa chcete orientovať v stromovej štruktúre stránky, aby ste mohli vybrať, články, na ktoré chcete vytvoriť odkaz, kliknite na tlačidlo [Pridať].',
-	'explication_type_obligatoire' => 'Vous pouvez imposer de typer les liaisons', # NEW
-	'explication_types_differents' => 'Cochez cette case pour permettre de lier plusieurs fois les deux mêmes articles, en changeant le type à chaque fois', # NEW
+	'explication_type_obligatoire' => 'Môžete zadať typ odkazov',
+	'explication_types_differents' => 'Zaškrtnite toto políčko, ak chcete niekoľko krát vytvoriť prepojenie na dva rovnaké články a hocikedy mať možnosť zmeniť jeho typ',
 	'explication_types_liaisons' => 'Vous pouvez définir des types de relations : indiquez-en une par ligne, suivie d\'une virgule et d\'une description. Une ligne vide pour la valeur par défaut. La description peut être une chaîne de langue.', # NEW
 
 	// I
@@ -28,7 +28,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// L
 	'lien_lier_article' => 'Čítať jeden článok alebo viac článkov',
 	'lier_cet_article' => 'Vytvoriť odkaz',
-	'lier_cet_article_deux_cotes' => 'Vytvoriť dva odkazy', # MODIF
+	'lier_cet_article_deux_cotes' => 'Prepojiť obe strany',
 
 	// O
 	'ob_pb' => 'Il n\'est pas possible de rendre obligatoire le type de liaison : en effet les articles ci-dessous possèdent des liaisons non typées. Typez les d\'abords, puis rendez-obligatoire le typage.', # NEW
@@ -51,20 +51,20 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// S
 	'sup_pb' => 'Impossible de supprimer les types de liaisons suivantes : des liaisons les utilisent. Il vous faut donc modifier au préalable ces liaions.', # NEW
 	'supprimer_le_lien' => 'Zrušiť odkazy',
-	'supprimer_le_lien_deux_cotes' => 'Zrušiť oba odkazy', # MODIF
+	'supprimer_le_lien_deux_cotes' => 'Zrušiť prepojenie',
 
 	// T
 	'td_pb' => 'Vous demandez l\'interdiction de lier plusieurs fois deux mêmes articles. Or certains articles possèdent plusieurs liaisons avec d\'autres articles. Corrigez cela avant.', # NEW
 	'type' => 'Typ',
-	'type_inexistant' => 'Type de liaison inexistant', # NEW
+	'type_inexistant' => 'Typ prepojenia neexistuje',
 	'type_liaison' => 'Typ prepojenia',
-	'type_obligatoire' => 'Typage obligatoire', # NEW
-	'types_differents' => 'Liaisons multiples', # NEW
-	'types_liaisons' => 'Types de liaison', # NEW
-	'types_liaisons_non_modifiables' => 'Les types de liaisons suivants ne sont pas modifiables via la configuration', # NEW
+	'type_obligatoire' => 'Povinné rozlíšenie typu',
+	'types_differents' => 'Viacnásobné prepojenia',
+	'types_liaisons' => 'Typy prepojenia',
+	'types_liaisons_non_modifiables' => 'Tieto typy prepojení sa v nastaveniach nedajú zmeniť ',
 
 	// U
-	'utilise_par_articles' => ', utilisé par les liaisons des articles :', # NEW
+	'utilise_par_articles' => ', používa sa v prepojeniach medzi článkami:',
 
 	// V
 	'voir' => 'Zobraziť'
