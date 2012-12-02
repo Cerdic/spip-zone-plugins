c5060de430c9b5dc7d36fcc8d9fb6315747ea155
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-12-02 02:40:44 +0000

    langues (paquet-opensearch)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@68066 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- opensearch/lang/paquet-opensearch_en.php
+++ opensearch/lang/paquet-opensearch_en.php
@@ -1,12 +1,15 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-opensearch?lang_cible=en
+// ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// O
+	// O
 	'opensearch_description' => 'Plugin which enables to offer the users to add a custom search engine related to your website on their browser.',
+	'opensearch_slogan' => 'Votre site comme moteur de recherche' # NEW
 );
+
 ?>
--- opensearch/lang/paquet-opensearch_fr.php
+++ opensearch/lang/paquet-opensearch_fr.php
@@ -1,19 +1,13 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-opensearch
-// Langue: fr
-// Date: 02-12-2012 02:18:15
-// Items: 2
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/opensearch/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// O
+	// O
 	'opensearch_description' => 'Plugin qui permet de proposer aux visiteurs de votre site d\'ajouter un moteur de recherche personnalisée à leur navigateur.',
-	'opensearch_slogan' => 'Votre site comme moteur de recherche',
+	'opensearch_slogan' => 'Votre site comme moteur de recherche'
 );
+
 ?>
