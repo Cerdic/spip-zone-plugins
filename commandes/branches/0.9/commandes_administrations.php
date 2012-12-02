5ffb2e845db18f16916fc16b2765d7944ed725ff
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-12-02 14:00:07 +0000

    branche 0.9
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@68080 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ commandes/branches/0.9/commandes_administrations.php
@@ -0,0 +1,107 @@
+<?php
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+
+/*function commandes_upgrade($nom_meta_base_version, $version_cible) {
+    include_spip('commandes_fonctions');
+    include_spip('inc/config');
+	$maj = array();
+	
+	$config = lire_config('commandes');
+    if (!is_array($config)) {
+            $config = array();
+        }
+    $id_webmestre = commandes_id_premier_webmestre();
+    $config = array_merge(array(
+                'duree_vie' => '1',
+                'activer' => '',
+                'quand' => array_keys(commandes_lister_statuts()),
+                'expediteur' => 'webmaster',
+                'expediteur_webmaster' => $id_webmestre,
+                'expediteur_administrateur' => '',
+                'expediteur_email' => '',
+                'vendeur' => 'webmaster',
+                'vendeur_webmaster' => $id_webmestre,
+                'vendeur_administrateur' => '',
+                'vendeur_email' => '',
+                'client' => 'on'
+        ), $config);
+
+	$maj['create'] = array(
+	   array(
+	       'maj_tables', array('spip_commandes,spip_commandes_details'),
+           'ecrire_config','commandes',$config,
+           )
+       );
+    $maj['0.2'] = array(
+       array('maj_tables', array('spip_commandes_details'))
+       );
+    $maj['0.3'] = array(
+        array(
+           'ecrire_config','commandes',array('duree_vie'=>3600)
+           )
+
+       );
+	include_spip('base/upgrade');
+	maj_plugin($nom_meta_base_version, $version_cible, $maj);
+}*/
+
+function commandes_upgrade($nom_meta_base_version, $version_cible) {
+    include_spip('commandes_fonctions');
+    include_spip('inc/config');
+    $maj = array();
+    
+    $config = lire_config('commandes');
+    if (!is_array($config)) {
+            $config = array();
+        }
+    $id_webmestre = commandes_id_premier_webmestre();
+    $config = array_merge(array(
+                'duree_vie' => '1',
+                'activer' => '',
+                'quand' => array_keys(commandes_lister_statuts()),
+                'expediteur' => 'webmaster',
+                'expediteur_webmaster' => $id_webmestre,
+                'expediteur_administrateur' => '',
+                'expediteur_email' => '',
+                'vendeur' => 'webmaster',
+                'vendeur_webmaster' => $id_webmestre,
+                'vendeur_administrateur' => '',
+                'vendeur_email' => '',
+                'client' => 'on'
+        ), $config);
+
+    $maj['create'] = array(
+            array(
+                   'maj_tables', array('spip_commandes','spip_commandes_details'),
+                   ),
+             array(
+                   'ecrire_config','commandes',$config
+                   ),                  
+            );
+    $maj['0.2']  = array(  
+            array('maj_tables', array('spip_commandes_details'))
+            );
+    $maj['0.3']  = array(  
+           'ecrire_config','commandes',array('duree_vie'=>3600)
+        );  
+                           
+    include_spip('base/upgrade');
+    maj_plugin($nom_meta_base_version, $version_cible, $maj);
+}
+
+
+function commandes_vider_tables($nom_meta_base_version) {
+
+
+	sql_drop_table("spip_commandes,spip_commandes_details");
+
+	# Nettoyer les versionnages et forums
+	sql_delete("spip_versions",              sql_in("objet", array('commande')));
+	sql_delete("spip_versions_fragments",    sql_in("objet", array('commande')));
+
+	effacer_meta($nom_meta_base_version);
+}
+
+?>
---
+++ commandes/branches/0.9/formulaires/configurer_commandes.php
@@ -0,0 +1,195 @@
+<?php
+
+// Sécurité
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+function formulaires_configurer_commandes_saisies_dist(){
+	include_spip('inc/config');
+	include_spip('inc/plugin');
+	include_spip('commandes_fonctions');
+	$config = lire_config('commandes');
+
+	$choix_expediteurs = array(
+			'webmaster' => _T('commandes:notifications_expediteur_choix_webmaster'),
+			'administrateur' => _T('commandes:notifications_expediteur_choix_administrateur'),
+			'email' => _T('commandes:notifications_expediteur_choix_email')
+	);
+	
+	if (defined('_DIR_PLUGIN_FACTEUR')){
+		$choix_expediteurs['facteur'] = _T('commandes:notifications_expediteur_choix_facteur');
+	}
+
+	return array(
+		array(
+			'saisie' => 'fieldset',
+			'options' => array(
+				'nom' => 'fieldset_parametres',
+				'label' => _T('commandes:parametres_cfg_titre')
+			),
+			'saisies' => array(
+				array(
+					'saisie' => 'input',
+					'options' => array(
+						'nom' => 'duree_vie',
+						'label' => _T('commandes:parametres_duree_vie_label'),
+						'explication' => _T('commandes:parametres_duree_vie_explication'),
+						'defaut' => $config['duree_vie']
+					)
+				)
+			)
+		),
+		array(
+			'saisie' => 'fieldset',
+			'options' => array(
+				'nom' => 'fieldset_notifications',
+				'label' => _T('commandes:notifications_cfg_titre')
+			),
+			'saisies' => array(
+				array(
+					'saisie' => 'explication',
+					'options' => array(
+						'nom' => 'exp1',
+						'texte' => _T('commandes:notifications_explication')
+					)
+				),
+				array(
+					'saisie' => 'oui_non',
+					'options' => array(
+						'nom' => 'activer',
+						'label' => _T('commandes:notifications_activer_label'),
+						'explication' => _T('commandes:notifications_activer_explication'),
+						'defaut' => $config['activer']
+					)
+				)
+			)
+		),	
+		array(
+			'saisie' => 'fieldset',
+			'options' => array(
+				'nom' => 'fieldset_notifications_parametres',
+				'label' => _T('commandes:notifications_parametres'),
+				'afficher_si' => '@activer@ == "on"',
+			),
+			'saisies' => array(
+				array(
+					'saisie' => 'selection_multiple',
+					'options' => array(
+						'nom' => 'quand',
+						'label' => _T('commandes:notifications_quand_label'),
+						'explication' => _T('commandes:notifications_quand_explication'),
+						'cacher_option_intro' => 'on',
+						'datas' => commandes_lister_statuts(),
+						'defaut' => $config['quand']
+					)
+				),
+				
+				array(
+					'saisie' => 'selection',
+					'options' => array(
+						'nom' => 'expediteur',
+						'label' => _T('commandes:notifications_expediteur_label'),
+						'explication' => _T('commandes:notifications_expediteur_explication'),
+						'cacher_option_intro' => 'on',
+						'defaut' => $config['expediteur'],
+						'datas' => $choix_expediteurs
+					)
+				),
+				
+				array(
+					'saisie' => 'auteurs',
+					'options' => array(
+						'nom' => 'expediteur_webmaster',
+						'label' => _T('commandes:notifications_expediteur_webmaster_label'),
+						'statut' => '0minirezo',
+						'cacher_option_intro' => "on",
+						'webmestre' => 'oui',
+						'defaut' => $config['expediteur_webmaster'],
+						'afficher_si' => '@expediteur@ == "webmaster"',
+					)
+				),
+				array(
+					'saisie' => 'auteurs',
+					'options' => array(
+						'nom' => 'expediteur_administrateur',
+						'label' => _T('commandes:notifications_expediteur_administrateur_label'),
+						'statut' => '0minirezo',
+						'cacher_option_intro' => "on",
+						'defaut' => $config['expediteur_administrateur'],
+						'afficher_si' => '@expediteur@ == "administrateur"',
+					)
+				),
+				array(
+					'saisie' => 'input',
+					'options' => array(
+						'nom' => 'expediteur_email',
+						'label' => _T('commandes:notifications_expediteur_email_label'),
+						'defaut' => $config['expediteur_email'],
+						'afficher_si' => '@expediteur@ == "email"',
+					)
+				),
+				array(
+					'saisie' => 'selection',
+					'options' => array(
+						'nom' => 'vendeur',
+						'label' => _T('commandes:notifications_vendeur_label'),
+						'explication' => _T('commandes:notifications_vendeur_explication'),
+						'cacher_option_intro' => 'on',
+						'defaut' => $config['vendeur'],
+						'datas' => array(
+							'webmaster' => _T('commandes:notifications_vendeur_choix_webmaster'),
+							'administrateur' => _T('commandes:notifications_vendeur_choix_administrateur'),
+							'email' => _T('commandes:notifications_vendeur_choix_email')
+						)
+					)
+				),
+				array(
+					'saisie' => 'auteurs',
+					'options' => array(
+						'nom' => 'vendeur_webmaster',
+						'label' => _T('commandes:notifications_vendeur_webmaster_label'),
+						'statut' => '0minirezo',
+						'cacher_option_intro' => "on",
+						'webmestre' => 'oui',
+						'multiple' => 'oui',
+						'defaut' => $config['vendeur_webmaster'],
+						'afficher_si' => '@vendeur@ == "webmaster"',
+					)
+				),
+				array(
+					'saisie' => 'auteurs',
+					'options' => array(
+						'nom' => 'vendeur_administrateur',
+						'label' => _T('commandes:notifications_vendeur_administrateur_label'),
+						'statut' => '0minirezo',
+						'multiple' => 'oui',
+						'cacher_option_intro' => "on",
+						'defaut' => $config['vendeur_administrateur'],
+						'afficher_si' => '@vendeur@ == "administrateur"',
+					)
+				),
+				
+				array(
+					'saisie' => 'input',
+					'options' => array(
+						'nom' => 'vendeur_email',
+						'label' => _T('commandes:notifications_vendeur_email_label'),
+						'explication' => _T('commandes:notifications_vendeur_email_explication'),
+						'defaut' => $config['vendeur_email'],
+						'afficher_si' => '@vendeur@ == "email"',
+					)
+				),
+				array(
+					'saisie' => 'oui_non',
+					'options' => array(
+						'nom' => 'client',
+						'label' => _T('commandes:notifications_client_label'),
+						'explication' => _T('commandes:notifications_client_explication'),
+						'defaut' => $config['client'],
+					)
+				)
+			)
+		)
+	);
+}
+
+?>
