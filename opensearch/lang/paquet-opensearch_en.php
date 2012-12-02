7f4cc64f9412e9fd87345e16927d98441ba97996
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-12-02 01:33:43 +0000

    Réel passage en SPIP 3.0
    Passage en paquet xml
    
    Compatibilité de la configuration
    
    etc...
    
    
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@68061 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ opensearch/lang/paquet-opensearch_en.php
@@ -0,0 +1,12 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// O
+	'opensearch_description' => 'Plugin which enables to offer the users to add a custom search engine related to your website on their browser.',
+);
+?>
---
+++ opensearch/lang/paquet-opensearch_fr.php
@@ -0,0 +1,19 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-opensearch
+// Langue: fr
+// Date: 02-12-2012 02:18:15
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// O
+	'opensearch_description' => 'Plugin qui permet de proposer aux visiteurs de votre site d\'ajouter un moteur de recherche personnalisée à leur navigateur.',
+	'opensearch_slogan' => 'Votre site comme moteur de recherche',
+);
+?>
