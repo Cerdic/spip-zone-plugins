47734eba67799232df26bd671f8537b57b9abf67
cyril@cym.fr <cyril@cym.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-12-05 15:53:12 +0000

    adaptations minimes du slogan;
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@68174 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- fil_ariane/trunk/lang/paquet-fil_ariane_fr.php
+++ fil_ariane/trunk/lang/paquet-fil_ariane_fr.php
@@ -5,8 +5,8 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-	'fil_ariane_slogan'         => "Une balise #FIL_ARIANE",
-	'fil_ariane_description'    => "Ce fil d'Ariane ultra-simple cherche tout seul l'arborescence des objets éditoriaux SPIP.",
+	'fil_ariane_slogan'         => "Une balise pour faire des fils d'Ariane",
+	'fil_ariane_description'    => "Ce fil d'Ariane ultra-simple cherche tout seul l'arborescence des objets éditoriaux de SPIP.",
 );
 
 ?>
