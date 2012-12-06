c9db4a104dc16be5dfc3807bab4e7efe83b47d84
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-12-06 17:44:57 +0000

    methode newsletter/content qui fournit le contenu (sujet, html, texte) d'une newsletter pour envoi. L'id passe en argument est purement interne a l'implementation, et provient de celui qu'on a fournit a l'appel de send() (unitaire ou methode a venir pour l'envoi en nombre). Ceci permet de ne pas promener partout le contenu complet de la lettre mais uniquement son id.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@68202 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ newsletters/newsletter/content.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Plugin Newsletters
+ * (c) 2012 Cedric Morin
+ * Licence GNU/GPL
+ */
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+
+function newsletter_content_dist($id){
+	// recuperer les messages
+	$corps = sql_fetsel('titre as sujet,html_email as html,texte_email as texte','spip_newsletters','id_newsletter='.intval($id));
+	if (!$corps)
+		return false;
+	if (!$corps['texte'] AND !$corps['html'])
+		return false;
+
+	// si pas de version texte : la generer a partir de la version HTML
+	if (!$corps['texte']){
+		include_spip("inc/newsletters");
+		$corps['texte'] = newsletters_html2text($corps['html']);
+	}
+
+	return $corps;
+}
