c1a6388d73560572494d5adc56ff015bacad2ecb
pierrekuhn82@gmail.com <pierrekuhn82@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-12-07 07:47:57 +0000

    On rajoute le bouton dans le menu "Edition". Déplacement des immages dans un répertoire du prive. Ajout du fichier pipelines pour géré le js et la css du plugins.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@68213 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ spipimmo/spipimmo_pipelines.php
@@ -0,0 +1,28 @@
+<?php
+/**
+* Plugin SPIP-Immo
+*
+* @author: CALV V3
+* @author: Pierre KUHN V3
+*
+* Copyright (c) 2007-12
+* Logiciel distribue sous licence GPL.
+*
+**/
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+/**
+ * Insertion des css du plugin dans les pages publiques
+ *
+ * @param $flux
+ * @return mixed
+ */
+
+function spipimmo_header_prive($flux){
+		$flux .= '<link rel="stylesheet" href="' ._DIR_PLUGIN_SPIPIMMO_IMG_PACK . 'spipimmo.css" type="text/css" />' . "\n";
+		$flux .= '<script src="' ._DIR_PLUGIN_SPIPIMMO_IMG_PACK . 'spipimmo.js" type="text/javascript"></script>' . "\n";
+		return $flux;
+}
+
+?>
