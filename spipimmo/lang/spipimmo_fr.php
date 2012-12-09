5f9d54a9a71736d4ffba9b698fd887bddbbbbb26
pierrekuhn82@gmail.com <pierrekuhn82@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-12-09 08:43:54 +0000

    Simplification du formulaire pour les annonce, on va passer par un formulaire propriétaire pour les autres informations.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@68261 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spipimmo/lang/spipimmo_fr.php
+++ spipimmo/lang/spipimmo_fr.php
@@ -1,42 +1,78 @@
-<?php
+﻿<?php
 
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	//A
+	'acces_handicape' => 'Accès handicapé',
 	'adresse' => 'Adresse',
-	'annee_construction' => 'Année de construction',
+	'annonce' => 'Annonce',
+	'ascenseur' => 'Ascenseur',
 	//C
 	'charge' => 'Charge',
 	'code_postal' => 'Code postal',
-	'code_postal_internet' => 'Code postal (internet)',
 	'creer_annonce' => 'Créer une annonce',
+	'creer_proprietaire' => 'Créer un propriétaire',
 	//D
 	'dpe' => 'DPE',
-	'date_modif' => 'Date de disponibilité',
+	'date_dispo' => 'Date de disponibilité',
+	'date_modif' => 'Date de modification',
+	'depot_garantie' => 'Dépot de garantie',
+	//E
+	'etage' => 'Etage',
+	'etage_code' => 'Etage code',
 	//H
-	'honoraire' => 'Honoraire',
+	'honoraires' => 'Honoraires',
 	//L
 	'liste_annonces' => 'Liste des annonces',
 	//N
+	'nature_chauffage' => 'Nature de chauffage',
+	'nbr_balcon' => 'Nombre de balcon',
+	'nbr_cave' => 'Nombre de cave',
+	'nbr_chambre' => 'Nombre de chambre',
+	'nbr_etage' => 'Nombre d\'étage',
+	'nbr_garage' => 'Nombre de garage',
+	'nbr_mur_mitoyen' => 'Nombre de mur mitoyen',
+	'nbr_park_ext' => 'Nombre de parking extérieur',
+	'nbr_park_int' => 'Nombre de parking intérieur',
+	'nbr_piece' => 'Nombre de pièce',
+	'nbr_sdb' => 'Nombre de salle de bain',
+	'nbr_sde' => 'Nombre de salle d\'eau',
+	'nbr_terrasse' => 'Nombre de terrasse',
+	'nbr_wc' => 'Nombre de WC',
 	'negociateur' => 'Négociateur',
 	'numero_mandat' => 'Numéro de mandat',
 	//P
+	'piscine' => 'Piscine',
+	'prestige' => 'Prestige',
 	'prix_loyer' => 'Prix loyer',
+	'proximite' => 'Proximité',
 	'publier' => 'Publier',
+	'quartier' => 'Quartier',
+	//R
+	'residence' => 'Résidence',
 	//S
+	'secteur' => 'Secteur',
 	'spipimmo' => 'SPIP-Immo',
+	'surface_facade' => 'Surface de facade',
+	'surface_habitable' => 'Surface habitable',
+	'surface_sejour' => 'Surface séjour',
+	'surface_terrain' => 'Surface terrain',
+	'surface_total' => 'Surface total',
 	//T
 	'taxe_fonciere' => 'Taxe foncière',
 	'taxe_habitation' => 'Taxe d\'habitation',
+	'transport' => 'Transport',
 	'travaux' => 'Travaux',
+	'type_chauffage' => 'Type de chauffage',
+	'type_cuisine' => 'Type de cuisine',
 	'type_mandat' => 'Type de Mandat',
-	'type_offre' => 'Type d\'offre',
+	'type_offre' => 'Type de sous sol',
+	'type_sous_sol' => 'Nombre de mur mitoyen',
 	//V
 	'vente_location' => 'Vente/Location',
 	'ville' => 'Ville',
-	'ville_internet' => 'Ville (internet)',
 
 );
 
