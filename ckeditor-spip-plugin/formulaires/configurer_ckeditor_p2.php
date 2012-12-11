fff5d7bf12941f7e6c91c5e0765944b48f91c862
fredlefred@gmail.com <fredlefred@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-12-11 12:32:58 +0000

    utilisation de saisies pour la configuration des barres d'outils saisie 'case_image'
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@68313 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- ckeditor-spip-plugin/formulaires/configurer_ckeditor_p2.php
+++ ckeditor-spip-plugin/formulaires/configurer_ckeditor_p2.php
@@ -3,7 +3,15 @@
 include_spip('inc/ckeditor_constantes') ;
 include_spip('inc/ckeditor_tools') ;
 include_spip("inc/toolbars") ;
+include_spip('inc/ckeditor_lire_config') ;
 
+function cke_case($val) {
+	if (($val==='on') || ($val==='off')) {
+		return $val ;
+	} else {
+		return ($val?'on':'off') ;
+	}
+}
 
 function formulaires_configurer_ckeditor_p2_charger_dist() {
         ($cfg = lire_config("ckeditor")) || ($cfg = array()) ;
@@ -11,9 +19,44 @@ function formulaires_configurer_ckeditor_p2_charger_dist() {
 	foreach($GLOBALS['toolbars'] as $toolbar) {
 		foreach($toolbar as $tool => $item) {
 			if (!ckeditor_tweaks_actifs('smileys') && ($tool == 'Smiley')) continue ;
-			$valeurs["tool_$tool"] = array_key_exists("tool_$tool", $cfg)?$cfg["tool_$tool"]:($item[1]) ;
+			$valeurs["tool_$tool"] = cke_case(array_key_exists("tool_$tool", $cfg)?$cfg["tool_$tool"]:($item[1])) ;
 		}
 	}
+
+	$saisies = array() ;
+
+	$html2spip = ckeditor_lire_config('html2spip_limite', _CKE_HTML2SPIP_LIMITE_DEF) ;
+	foreach($GLOBALS['toolbars'] as $index => $toolbar) {
+		$fieldset = array(
+			'saisie' => 'fieldset',
+			'options' => array(
+				'nom' => 'toolbar_'.$index
+			),
+			'saisies' => array()
+		) ;
+
+		$cp=0;
+		foreach($toolbar as $tool => $item) {
+			if (!ckeditor_tweaks_actifs('smileys') && ($tool == 'Smiley')) continue ;
+			$saisie =  array('options'=>array('nom'=>'tool_'.$tool)) ;
+			if ($html2spip && !$item[2]) {
+				$saisie['options']['disabled']='oui' ;
+			}
+			$saisie['options']['label_case'] = _T('ckeditor:tool_'.$tool) ;
+
+			if (isset($item[3]) && ($icon = find_in_path(sprintf("prive/themes/spip/images/icons/icon-%02d.png",$item[3])))) {
+				$saisie['saisie'] = 'case_image' ;
+				$saisie['options']['image_case'] = $icon ;
+			} else {
+				$saisie['saisie'] = 'case' ;
+			}
+			$fieldset['saisies'][] = $saisie ;
+		}
+		$saisies[] = $fieldset ;
+	}
+
+	$valeurs['cke_saisies_p2'] = $saisies ;
+
 	return $valeurs ;
 }
 
@@ -35,7 +78,7 @@ function formulaires_configurer_ckeditor_p2_traiter_dist() {
 		foreach($GLOBALS['toolbars'] as $toolbar) {
 			foreach($toolbar as $tool => $size) {
 				if (!ckeditor_tweaks_actifs('smileys') && ($tool == 'Smiley')) continue ;
-				if ($action == 'ok' ) ecrire_config("ckeditor/tool_$tool", _request("tool_$tool")?1:0) ;
+				if ($action == 'ok' ) ecrire_config("ckeditor/tool_$tool", _request("tool_$tool")==='on'?1:0) ;
 				if ($action == 'reset') effacer_config("ckeditor/tool_$tool") ;
 			}
 		}
