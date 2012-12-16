53bd7991af64232436c464238dfeb5b791d32429
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-12-16 11:06:12 +0000

    Voilà une version SPIP 3 finalisée du plugin. On rajoute une petite CSS dans le privée afin que le bloc contenant la vignette ait un look correct en attendant que le logo du site devienne une vraie boite au sens SPIP 3. Mise à jour de la page de démo. Passage de l'état en test.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@68405 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- thumbsites/trunk/lang/paquet-thumbsites_fr.php
+++ thumbsites/trunk/lang/paquet-thumbsites_fr.php
@@ -2,11 +2,8 @@
 
 // Ceci est un fichier langue de SPIP -- This is a SPIP language file
 
-///  Fichier produit par PlugOnet
 // Module: paquet-thumbsites
 // Langue: fr
-// Date: 08-04-2012 19:09:00
-// Items: 2
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
@@ -16,6 +13,6 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	
 // T
 	'thumbsites_description' => 'Thumbsites fournit un système de capture d\'écran de site générée par des serveurs dédiés. Le plugin propose des balises, des filtres et des modèles pour afficher la vignette d\'un site donné. Ce plugin gère aussi un cache qui permet de pallier les indisponibilités fréquentes des serveurs et d\'accélérer les affichages. Une configuration est disponible dans l\'espace privé.',
-	'thumbsites_slogan' => 'Une vignette pour les objets sites',
+	'thumbsites_slogan' => 'Une vignette pour vos sites référencés',
 );
 ?>
--- thumbsites/trunk/thumbsites_pipelines.php
+++ thumbsites/trunk/thumbsites_pipelines.php
@@ -16,7 +16,6 @@ function thumbsites_afficher_config_objet($flux){
 		include_spip('inc/thumbsites_filtres');
 		if ($thumbshot_cache = thumbshot($url)) {
 			if ($taille = @getimagesize($thumbshot_cache)) {
-				$taille = _T('info_largeur_vignette', array('largeur_vignette' => $taille[0], 'hauteur_vignette' => $taille[1]));
 				$flux["data"] .= recuperer_fond('prive/squelettes/navigation/thumbshot',
 									array(
 										'id_objet' => $id,
