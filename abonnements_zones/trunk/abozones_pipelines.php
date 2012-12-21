c85b80cdffc3564c9e5ab10c3ff496db54d55d66
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2012-12-21 21:00:04 +0000

    Un premier pipeline qui ajoute/retire des zones à un auteur lorsqu'il est abonné ou désabonné à une offre liée à des zones. It works !
    Maintenant il faut le faire aussi pour les abonnements actifs lorsqu'on ajoute ou retire une zone *à l'offre*.
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@68549 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- abonnements_zones/trunk/abozones_pipelines.php
+++ abonnements_zones/trunk/abozones_pipelines.php
@@ -18,4 +18,50 @@ function abozones_afficher_complement_objet($flux){
 	return $flux;
 }
 
+/*
+ * Ajouter ou retirer un utilisateur des zones liées à une offre suivant divers événements
+ */
+function abozones_post_edition($flux){
+	// Lorsqu'un abonnement change de statut... et que l'offre est liée à des zones !
+	if (
+		$flux['args']['table'] == 'spip_abonnements'
+		and $flux['args']['action'] == 'instituer'
+		and $id_abonnement = intval($flux['args']['id_objet'])
+		and $abonnement = sql_fetsel('id_abonnements_offre,id_auteur', 'spip_abonnements', 'id_abonnement = '.$id_abonnement)
+		and $id_auteur = $abonnement['id_auteur']
+		and $id_abonnements_offre = $abonnement['id_abonnements_offre']
+		and include_spip('action/editer_liens')
+		and $liens = objet_trouver_liens(array('zones'=>'*'), array('abonnements_offre'=>$id_abonnements_offre))
+	) {
+		include_spip('inc/autoriser');
+		include_spip('action/editer_zone');
+		
+		// On ne récupère que les ids
+		$zones = array();
+		foreach ($liens as $lien) {
+			$zones[] = $lien['id_zone'];
+		}
+		
+		// Si c'est une activation on ajoute les zones trouvées à l'utilisateur de l'abonnement SANS autorisation
+		if ($flux['data']['statut'] == 'actif') {
+			autoriser_exception('affecterzones', 'auteur', $id_auteur);
+			zone_lier($zones, 'auteur', $id_auteur);
+			autoriser_exception('affecterzones', 'auteur', $id_auteur, false);
+		}
+		// Si c'est une désactivation (ancien statut actif, et nouveau différent)
+		// on supprime les zones
+		elseif (
+			$flux['args']['statut_ancien'] == 'actif'
+			and isset($flux['data']['statut'])
+			and $flux['data']['statut'] != 'actif'
+		) {
+			autoriser_exception('retirerzones', 'auteur', $id_auteur);
+			zone_lier($zones, 'auteur', $id_auteur, 'del');
+			autoriser_exception('retirerzones', 'auteur', $id_auteur, false);
+		}
+	}
+	
+	return $flux;
+}
+
 ?>
