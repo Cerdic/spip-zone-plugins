b20a0fd0f4a420e7c30ca4922ee37a1e230db2e9
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-01-04 02:40:04 +0000

    langues (suivant_precedent)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@68686 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ criteres_suivant_precedent/lang/suivant_precedent_es.php
@@ -0,0 +1,20 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/suivant_precedent?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'alt_lien_next' => 'Siguiente',
+	'alt_lien_prev' => 'Anterior',
+
+	// N
+	'noisette_description' => 'Muestra los enlances al artículo anterior y al artículo siguiente',
+	'noisette_label_meme_rubrique' => 'Muestra únicamente los enlaces a los artículos de la misma sección',
+	'noisette_nom' => 'Enlaces Anterior / Siguiente'
+);
+
+?>
