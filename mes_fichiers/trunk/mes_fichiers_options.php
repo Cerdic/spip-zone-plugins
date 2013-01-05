0745dd232193de2f5a1c9e9489b43ac69a7e504b
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-01-05 16:27:19 +0000

    Définition plus robuste du pipeline post_sauvegarde dont l'utilité m'échappe toujours...
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@68726 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- mes_fichiers/trunk/mes_fichiers_options.php
+++ mes_fichiers/trunk/mes_fichiers_options.php
@@ -1,10 +1,6 @@
 <?php
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
-if(!isset($GLOBALS['spip_pipeline']['post_sauvegarde'])){
-	$GLOBALS['spip_pipeline']['post_sauvegarde'] = '';
-}
-
 // Repertoire de stockage des archives creees
 if (!defined('_DIR_MES_FICHIERS')) define('_DIR_MES_FICHIERS', _DIR_TMP . 'mes_fichiers/');
 define('PCLZIP_TEMPORARY_DIR', _DIR_TMP);
