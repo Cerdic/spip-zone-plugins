6d21a2c030d13ce79cf7b1ad0a63370f5d8d2492
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-01-06 02:41:04 +0000

    langues (multilang)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@68744 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ multilang/lang/multilang_es.php
@@ -0,0 +1,47 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/multilang?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cfg_activer' => 'Activar Multilang en la página de edición:',
+	'cfg_boite_multilang' => 'Configuración del plugin Multilang',
+	'cfg_descr_multilang' => 'Multilang: Plugin de gestión de las etiquetas "multi"',
+	'cfg_effacer' => 'Reiniciar',
+	'cfg_explication_crayons' => 'Se requiere activar Multilang en el espacio público del sitio.',
+	'cfg_langues' => 'Idiomas utilizados',
+	'cfg_lbl_article' => 'artículos',
+	'cfg_lbl_auteur' => 'autores',
+	'cfg_lbl_breve' => 'breves',
+	'cfg_lbl_crayons' => 'Utilizar Multilang en los lápices',
+	'cfg_lbl_document' => 'documentos',
+	'cfg_lbl_espace_public' => 'Utilizar Multilang en el espacio público del sitio',
+	'cfg_lbl_evenement' => 'Eventos',
+	'cfg_lbl_formstables' => 'forms&tables',
+	'cfg_lbl_gis' => 'puntos geolocalizados',
+	'cfg_lbl_motcle' => 'palabaras clave',
+	'cfg_lbl_rubrique' => 'secciones',
+	'cfg_lbl_site' => 'sitios',
+	'cfg_lbl_siteconfig' => 'de la configuración del sitio',
+	'cfg_titre_multilang' => 'Multilang',
+	'champ_numero' => 'Número',
+	'configuration_multilang' => 'Multilang',
+
+	// E
+	'explication_langues_utilisees' => 'Seleccione los idiomas a utilizar para el menú de Multilang. No seleccione ninguna para utilizarlas todas.',
+
+	// L
+	'label_langues_utilisees' => 'Idiomas utilizados por Multilang',
+	'lien_desactiver' => 'Todo',
+	'lien_multi_title' => 'Editar los campos multilingües en «@lang@»',
+	'lien_multi_title_sans' => 'La versión «@lang@» está incompleta. Haga clic para editar.',
+
+	// M
+	'message_champs_readonly' => 'Los campos multilingües son sólo de lectura, elija un idioma para modificarlos'
+);
+
+?>
