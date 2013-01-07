498201515dd15cbb78311a60a183280b00ded1b1
jonathan@beespip.org <jonathan@beespip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-01-07 09:34:47 +0000

    Insertion des communes et pays en multi
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@68795 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- geographie/imports/geographie.php
+++ geographie/imports/geographie.php
@@ -3,10 +3,8 @@ function imports_geographie_dist(){
 	// importer les pays
 	include_spip('imports/pays');
 	include_spip('inc/charsets');
-  
-  
-	foreach($GLOBALS['liste_pays'] as $k=>$p)
-		sql_insertq('spip_geo_pays',array('id_pays'=>$k,'nom'=>unicode2charset(html2unicode($p))));
+
+	sql_insertq_multi('spip_geo_pays',$GLOBALS['liste_pays']);
 
 	// importer les regions/dept/communes francaise
 	$id_pays = 70;
@@ -16,17 +14,19 @@ function imports_geographie_dist(){
 		lire_fichier($fichier,$table);
 		if ($table = unserialize($table)) {
 			foreach($table as $region=>$departements){
-				$id_region = sql_insertq('spip_geo_regions',array('nom'=>$region,'id_pays'=>$id_pays));
+				$id_region = sql_insertq('spip_geo_regions',array('nom'=>unicode2charset(html2unicode($region)),'id_pays'=>$id_pays));
 				foreach($departements as $departement=>$communes) {
 					$abbr = reset($communes);
 					$abbr = substr($abbr['insee'],0,3);
 					if (substr($abbr,0,1)=='0')
 						$abbr = substr($abbr,1);
-					$id_departement = sql_insertq('spip_geo_departements',array('nom'=>$departement,'abbr'=>$abbr,'id_region'=>$id_region));
+					$id_departement = sql_insertq('spip_geo_departements',array('nom'=>unicode2charset(html2unicode($departement)),'abbr'=>$abbr,'id_region'=>$id_region));
+					$tabcom=array();
 					foreach($communes as $commune) {
 						if ($commune['insee']) // par securite pour eviter une insertion vide
-							sql_insertq('spip_geo_communes',array('nom'=>unicode2charset($commune['nom']),'insee'=>$commune['insee'],'code_postal'=>$commune['cp'],'latitude'=>$commune['lat'],'longitude'=>$commune['long'],'id_departement'=>$id_departement,'id_pays'=>$id_pays));
+							$tabcom[]=array('nom'=>unicode2charset($commune['nom']),'insee'=>$commune['insee'],'code_postal'=>$commune['cp'],'latitude'=>$commune['lat'],'longitude'=>$commune['long'],'id_departement'=>$id_departement,'id_pays'=>$id_pays);
 					}
+					sql_insertq_multi('spip_geo_communes',$tabcom);
 				}
 			}
 		}
