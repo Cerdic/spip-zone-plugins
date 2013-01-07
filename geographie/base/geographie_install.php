22acb3301fac9887b66004fc4d8beba387d89229
jonathan@beespip.org <jonathan@beespip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-01-07 09:34:10 +0000

    Arrondissement sur nouvelle installation, et maj pays
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@68794 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- geographie/base/geographie_install.php
+++ geographie/base/geographie_install.php
@@ -11,6 +11,11 @@
 					echo "Geographie Install<br/>";
 					$importer_geographie = charger_fonction('geographie','imports');
 					$importer_geographie();
+					if (include_spip('base/arrondissements')){
+						echo "Création des arrondissements : <br/>", _DIR_PLUGIN_GEOGRAPHIE.'base/arrondissements.txt<br/>';
+						$importer_arrondissements = charger_fonction('arrondissements','imports');
+						$importer_arrondissements();
+					}					
 					ecrire_meta($nom_meta_base_version,$current_version=$version_cible,'non');
 				}
 				else return;
@@ -35,6 +40,16 @@
 				}
 				else return;
 			}
+			else if ($current_version<0.4){
+				if (include_spip('base/geographie')){
+					sql_drop_table("spip_geo_pays");
+					creer_base();
+					echo "Mise &agrave; des pays<br/>";
+					reimport_pays();
+					ecrire_meta($nom_meta_base_version,$current_version=0.4,'non');
+				}
+				else return;
+			}
 			ecrire_metas();
 		}
 	}
@@ -50,9 +65,8 @@
 	}
 
 	function reimport_pays(){
-		include_spip('imports/pays');
-		include_spip('inc/charset');
-		foreach($GLOBALS['liste_pays'] as $k=>$p)
-			sql_insertq('spip_geo_pays',array('id_pays'=>$k,'nom'=>unicode2charset(html2unicode($p))));
+	include_spip('imports/pays');
+		
+	sql_insertq_multi('spip_geo_pays',$GLOBALS['liste_pays']);
 	}
 ?>
