06bd3bd34cf9fcd6df89e1b2a2b3e1c54d132e51
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-01-07 02:36:49 +0000

    langues (paquet-multilang)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@68787 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ multilang/lang/paquet-multilang_es.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-multilang?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// M
+	'multilang_description' => 'Este plugin añade un menú de idiomas del tipo <code>[fr] [en] [it]</code> encima de cada formulario, en función de los idiomas activados en la configuración del sitio.',
+	'multilang_slogan' => 'Administrar la muestra del contenido de las etiquetas multi con un menu'
+);
+
+?>
