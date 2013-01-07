999de820ff7aa457a8c15c7376e72447edf049bf
jonathan@beespip.org <jonathan@beespip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-01-07 09:32:43 +0000

    insertion en multi des arrondissements
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@68792 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- geographie/base/arrondissements.php
+++ geographie/base/arrondissements.php
@@ -2,7 +2,7 @@
 
 function imports_arrondissements_dist(){
 	include_spip('inc/charset');
-
+	$tab_arrond=array();
 	$fichier = preg_files(_DIR_PLUGIN_GEOGRAPHIE . "/base/",'arrondissements[.]txt$');
 	//echo count( $fichier),' trouvé, ',$fichier[0];
 	lire_fichier( $fichier[0], $arronds);	//echo $arronds;
@@ -12,21 +12,22 @@ function imports_arrondissements_dist(){
 		  foreach( $laDptArronds as $liIndex => $laDptArrond) {
 				if ($liIdDepartement = sql_getfetsel( 'id_departement', 'spip_geo_departements', '(abbr='. "'$lsAbbr')")) {//sql_quote( $lsAbbr).')')) {
 					if ($liIdCommune = sql_getfetsel( 'id_commune', 'spip_geo_communes', "(id_departement=$liIdDepartement) AND (nom=".sql_quote( $laDptArrond['commune']).')')) {
-						if (sql_insertq('spip_geo_arrondissements', array(
-								'id_departement'=>$liIdDepartement,
-								'nom'=>$laDptArrond['nom'],
-								'id_commune'=>$liIdCommune,
-								'population'=>$laDptArrond['population'],
-								'superficie'=>$laDptArrond['superficie'],
-              	'densite'   =>$laDptArrond['densite'],
-              	'nb_communes'=>$laDptArrond['nb_communes'],
-							))) {
-							$liCpt++;
-						}	else { echo 'Echec à la création de ', $laDptArrond['abbr'],':', $laDptArrond['commune'],'<br>';	};
+						$tab_arrond[]=array(
+											'id_departement'=>$liIdDepartement,
+											'nom'=>$laDptArrond['nom'],
+											'id_commune'=>$liIdCommune,
+											'population'=>$laDptArrond['population'],
+											'superficie'=>$laDptArrond['superficie'],
+											'densite'   =>$laDptArrond['densite'],
+											'nb_communes'=>$laDptArrond['nb_communes'],
+											);
+						$liCpt++;
 					} else { echo 'Commune ', $laDptArrond['abbr'],': ', $laDptArrond['commune'],' non trouvée !<br>'; }
 				} else { echo "Département $lsAbbr non trouvé !<br>";	}
 			}
 		}
+		if (!empty($tab_arrond))
+			sql_insertq_multi('spip_geo_arrondissements',$tab_arrond);
 	}
 	echo "$liCpt arrondissements installés.<br/>";
 }
