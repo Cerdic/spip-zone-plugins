e53d1bece2fc9f1a18fcc5e64ea81a29cbe71738
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-01-07 02:36:59 +0000

    langues (opensearch)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@68790 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ opensearch/lang/opensearch_es.php
@@ -0,0 +1,21 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/opensearch?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cfg_boite_opensearch' => 'Configuración del plugin OpenSearch',
+	'cfg_descr_opensearch' => 'OpenSearch: motor de búsqueda personalizada para su sitio.',
+	'cfg_inf_id_rubrique' => 'Seleccione la sección en la cual efectear la búsqueda.',
+	'cfg_inf_page_recherche' => 'Indique el esqueleto a utilizar para la búsqueda en el sitio (ejemplo: recherche para el esqueleto recherche.html)',
+	'cfg_lbl_id_rubrique' => 'Sección de destino',
+	'cfg_lbl_page_recherche' => 'Esqueleto de búsqueda',
+	'cfg_option_tout' => 'Todo el sitio',
+	'cfg_titre_opensearch' => 'OpenSearch'
+);
+
+?>
