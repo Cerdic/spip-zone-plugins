2e80ec1b8c179826f00947291dda045c8858f244
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-01-08 02:41:05 +0000

    langues (nuage)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@68830 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ nuage/trunk/lang/nuage_es.php
@@ -0,0 +1,19 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/nuage?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// N
+	'noisette_description' => 'Muestra una nube de palabras clave',
+	'noisette_label_afficher_titre' => 'Mostra el título:',
+	'noisette_label_niveau_titre' => 'Elegir el nivel de título',
+	'noisette_nom_noisette' => 'Nube',
+	'noisette_titre_defaut' => 'Etiquetas',
+	'noisette_titre_noisette' => 'Título:'
+);
+
+?>
