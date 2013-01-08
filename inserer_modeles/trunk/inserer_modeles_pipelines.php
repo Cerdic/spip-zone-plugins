f8a2c0ed1e4bc77d2a1ff62810cc22c455e93e82
tcharlss@hotmail.fr <tcharlss@hotmail.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-01-08 14:12:45 +0000

    - Tentative pour corriger un bug  : le chargement du formulaire depuis le bouton du porte-plume ne marchait pas. On utilise le filtre 'url_absolue' pour s'assurer la modalbox trouve l' adresse. - squelette du formulaire : légère modification de stylage + pas besoin de mettre le js du double-clic avec la modalbox.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@68842 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- inserer_modeles/trunk/inserer_modeles_pipelines.php
+++ inserer_modeles/trunk/inserer_modeles_pipelines.php
@@ -32,10 +32,10 @@ function inserer_modeles_porte_plume_barre_pre_charger($barres) {
 				"id" => 'inserer_modele_'.$nom,
 				"name" => _T_ou_typo($formulaire['nom']),
 				"className" => 'outil_inserer_modele_'.$nom,
-				"beforeInsert" => "function() {jQuery.modalboxload('".generer_url_public(
+				"beforeInsert" => "function() {jQuery.modalboxload('".url_absolue(generer_url_public(
 					'inserer_modeles',
 					"modalbox=oui&formulaire_modele=$nom&id_article='+$(\"[name='id_article']\").val()+'&id_rubrique='+$(\"[name='id_rubrique']\").val()+'&id_breve='+$(\"[name='id_breve']\").val()"
-				).",{minHeight: '90%'});}",
+				)).",{minHeight: '90%'});}",
 				"display" => true
 			);
 		}
@@ -46,10 +46,10 @@ function inserer_modeles_porte_plume_barre_pre_charger($barres) {
 			"name"        => _T('inserer_modeles:outil_inserer_modeles'),
 			"key"         => "M",
 			"className"   => 'outil_inserer_modeles',
-			"beforeInsert" => "function() {jQuery.modalboxload('".generer_url_public(
+			"beforeInsert" => "function() {jQuery.modalboxload('".url_absolue(generer_url_public(
 				'inserer_modeles',
 				"modalbox=oui&id_article='+$(\"[name='id_article']\").val()+'&id_rubrique='+$(\"[name='id_rubrique']\").val()+'&id_breve='+$(\"[name='id_breve']\").val()"
-			).",{minHeight: '90%'});}",
+			)).",{minHeight: '90%'});}",
 			"display"     => true,
 			"dropMenu"    => $sous_menu
 		 ));
