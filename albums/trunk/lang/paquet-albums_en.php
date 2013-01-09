8dfec0538ff637c96615f41487587570b365d333
tcharlss@hotmail.fr <tcharlss@hotmail.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-01-09 02:45:13 +0000

    langues (paquet-albums)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@68864 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ albums/trunk/lang/paquet-albums_en.php
@@ -0,0 +1,19 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-albums?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'albums_description' => 'This plugin adds a new \'album\' object which you can link to any content.
+Albums act as containers for your documents : they allow you to group them to your liking.
+Managing series of documents with the albums is made more convenient.',
+	'albums_nom' => 'Albums',
+	'albums_slogan' => 'Group your documents as albums',
+	'albums_titre' => 'Albums'
+);
+
+?>
