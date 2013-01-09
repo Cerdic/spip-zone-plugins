01870790bf0d1b20458e9819fa516ee75c4a29fc
tcharlss@hotmail.fr <tcharlss@hotmail.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-01-09 16:54:27 +0000

    premier jet du plugin 'Icônes Griseus', un jeu d'icônes pour l'espace privé
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@68899 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ griseusico/lang/paquet-griseusico_fr.php
@@ -0,0 +1,13 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// G
+	'griseus_nom' => 'Ic&ocirc;nes Griseus',
+	'griseus_slogan' => 'Un jeu d\'ic&ocirc;nes pour l\'espace priv&eacute;',
+);
+
+?>
