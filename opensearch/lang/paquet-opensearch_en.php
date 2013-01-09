1ba37c2cd78107885bd9b1479799d5642832d5ee
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-01-09 02:45:53 +0000

    langues (paquet-opensearch)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@68875 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- opensearch/lang/paquet-opensearch_en.php
+++ opensearch/lang/paquet-opensearch_en.php
@@ -9,7 +9,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// O
 	'opensearch_description' => 'Plugin which enables to offer the users to add a custom search engine related to your website on their browser.',
-	'opensearch_slogan' => 'Votre site comme moteur de recherche' # NEW
+	'opensearch_slogan' => 'Your website as search engine'
 );
 
 ?>
