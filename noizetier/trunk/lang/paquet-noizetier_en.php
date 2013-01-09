f563e5c0c7019dc7295b58738e1fa230afb584ea
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-01-09 02:45:49 +0000

    langues (paquet-noizetier)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@68873 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ noizetier/trunk/lang/paquet-noizetier_en.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-noizetier?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// N
+	'noizetier_description' => 'The nuts manager provides an interface in the private area to choose nuts  to add to the content, navigation and extra blocks of each page. Nuts thus complement the default contents provided by the skeleton.',
+	'noizetier_nom' => 'Nuts Manager (NoiZetier)',
+	'noizetier_slogan' => 'Nuts management, natively functional with Z skeletons'
+);
+
+?>
