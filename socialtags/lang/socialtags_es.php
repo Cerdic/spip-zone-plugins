58bb8dd9c68c3a0f999f2bef5b6865353efcd064
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-01-09 02:46:03 +0000

    langues (socialtags)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@68879 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ socialtags/lang/socialtags_es.php
@@ -0,0 +1,77 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/socialtags?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cfg_activer_javascript' => 'Por favor, active Javascript.',
+	'cfg_badge_fb' => 'Insignia/Widget Facebook',
+	'cfg_badge_fb_existe' => 'Si tiene una Insignia Facebook (<a href="http://www.facebook.com/facebook-widgets/" class="spip_out">aide sur le site FB</a>), indique aquí su contenido, bien en JS, bien en HTML.',
+	'cfg_badge_html' => 'Insignia HTML',
+	'cfg_badge_html_ajouter_1' => '(adjuntar en vuestro esqueleto',
+	'cfg_badge_html_ajouter_2' => ', o en un artículo',
+	'cfg_badge_js' => 'Insignia JS',
+	'cfg_badge_js_1' => ' (automáticamente incluido en <code>@code@</code>)',
+	'cfg_en_bas' => 'Abajo de cada página',
+	'cfg_exemples' => 'Ejemplos (en los esqueletos dist):',
+	'cfg_explication_boite_info' => 'Aquí puede elegir los sitios donde compartir sus artículos.',
+	'cfg_inserer' => 'Insertar el bloque de etiquetas sociales debajo del bloque siguiente:',
+	'cfg_insertion' => 'Inserción en las páginas:',
+	'cfg_liste_site' => 'Lista de sitios sociales',
+	'cfg_plusone' => 'Google plus one',
+	'cfg_plusone_existe' => 'Para activar Google+1, instalar el plugin (<a href="http://www.spip-contrib.net/Google-1" class="spip_out">ayuda en spip-contrib</a>).',
+	'cfg_referer' => 'No mostrar la insignia ni los botones más que a los visitantes originales de los respectivos sitios ',
+	'cfg_selector' => 'Selector:',
+	'cfg_sous_bloc' => 'Bajo el bloque de título de páginas artículo',
+	'cfg_sous_div' => 'Bajo la div llamada id=socialtags',
+	'cfg_titre_boite_info' => 'Configuración del plugin Etiquetas sociales',
+	'cfg_titre_socialtags' => 'Etiquetas sociales',
+	'cfg_tout_cocher' => 'Marcar todo',
+	'cfg_tout_decocher' => 'Desmarcar todo',
+	'cfg_wopen' => 'Modo pop-up',
+	'cfg_wopen_exp' => 'Abrir el enlace al sitio en una nueva ventana',
+
+	// D
+	'description_noisette_badge_facebook' => 'Muestra la insignia Facebook definida en la página de configuración ',
+	'description_noisette_facebook_like' => 'Muestra el botón Facebook <b>Me gusta</b> o <b>Compartir</b> y eventualmente <b>Enviar</b>',
+	'description_noisette_facebook_like_box' => 'Muestra el cuadro de Facebook <b>Me gusta</b>. Muestra particularmente el número de fans a los que le gusta la página de Facebook referenciada',
+
+	// N
+	'noisette_action_like' => 'Me gusta',
+	'noisette_action_recommand' => 'Recomendar',
+	'noisette_colorscheme_dark' => 'Fondo oscuro',
+	'noisette_colorscheme_light' => 'Fondo claro',
+	'noisette_label_action' => 'Acción',
+	'noisette_label_afficher_send' => '¿Botón Enviar?',
+	'noisette_label_afficher_titre_noisette' => '¿Mostrar título?',
+	'noisette_label_border_color' => 'Introducir el código color del borde del cuadro (en formato #123ABC)',
+	'noisette_label_colorscheme' => 'Apariencia',
+	'noisette_label_header' => '¿Mostrar el encabezado del cuadro «Encontrarnos en Facebook»?',
+	'noisette_label_height' => 'Altura',
+	'noisette_label_href' => 'Url de la página Facebook',
+	'noisette_label_layout' => 'Texto',
+	'noisette_label_niveau_titre' => 'Elegir nivel del título',
+	'noisette_label_show_faces' => '¿Mostrar fotos del perfil bajo el botón?',
+	'noisette_label_stream' => '¿Mostrar flujo de artículos de la página de Facebook?',
+	'noisette_label_titre_noisette' => 'Título:',
+	'noisette_label_titre_noisette_perso' => 'Si título personalizado:',
+	'noisette_label_width' => 'Anchura',
+	'noisette_layout_box_count' => 'Total sólo por encima',
+	'noisette_layout_button_count' => 'Total sólo a la derecha',
+	'noisette_layout_standard' => 'Texto completo a la derecha',
+	'noisette_titre_perso' => 'Título personalizado',
+	'nom_noisette_badge_facebook' => 'Insignia Facebook',
+	'nom_noisette_facebook_like' => 'Botón Facebook-like',
+	'nom_noisette_facebook_like_box' => 'Cuadro Facebook-like',
+
+	// T
+	'titre_menu' => 'Etiquetas sociales',
+	'titre_mon_badge_facebook' => 'Mi insignia Facebook',
+	'titre_sur_facebook' => 'En Facebook'
+);
+
+?>
