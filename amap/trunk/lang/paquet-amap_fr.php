66557fd71389710886fede0483370d0e0bcf3104
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-01-10 02:43:38 +0000

    langues (paquet-amap)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@68915 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- amap/trunk/lang/paquet-amap_fr.php
+++ amap/trunk/lang/paquet-amap_fr.php
@@ -1,21 +1,15 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-amap
-// Langue: fr
-// Date: 09-01-2013 23:14:04
-// Items: 2
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/amap/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// A
+	// A
 	'amap_description' => 'Le plugin est développé pour géré les paniers et adhérents (dit amapiens). 
 		Les paniers sont créés et attribués à un amapien, ce dernier pourra mettre à disposition un panier si il n\'est pas présent 
 		pour le récupérer le jour prévu.',
-	'amap_slogan' => 'Gestion des paniers et adhérents pour les AMAP',
+	'amap_slogan' => 'Gestion des paniers et adhérents pour les AMAP'
 );
+
 ?>
