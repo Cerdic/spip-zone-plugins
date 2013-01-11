8bda6127311dc3ea03900218f3e4b205b1821c17
tcharlss@hotmail.fr <tcharlss@hotmail.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-01-11 22:32:35 +0000

    description du plugin dans la langue du paquet.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@68952 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- griseusico/lang/paquet-griseusico_fr.php
+++ griseusico/lang/paquet-griseusico_fr.php
@@ -6,8 +6,12 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// G
-	'griseus_nom' => 'Ic&ocirc;nes Griseus',
-	'griseus_slogan' => 'Un jeu d\'ic&ocirc;nes pour l\'espace priv&eacute;',
+	'griseusico_nom' => 'Ic&ocirc;nes Griseus',
+	'griseusico_slogan' => 'Un jeu d\'ic&ocirc;nes pour l\'espace priv&eacute;',
+	'griseusico_description' => 'Griseus, un jeu d\'ic&ocirc;nes qui se veut sobre et tente de faire en sorte de ne pas d&eacute;tourner l\'attention du contenu. Quelques principes de base :
+		- pour la barre d\'action rapide et pour les objets &eacute;ditoriaux en taille 16px, utiliser des ic&ocirc;nes symboliques.
+		- pour les menus de configuration, des ic&ocirc;nes monochromes simples.
+		- pour tous les autres cas de figure, &eacute;viter les fioritures et se limiter &agrave; la couleur violet de SPIP dans la mesure du possible.'
 );
 
 ?>
