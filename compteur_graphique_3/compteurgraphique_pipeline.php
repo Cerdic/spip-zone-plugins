80d7669cb7d1b3589929ad5f4b4669b6a33eeb5d
olivier.gautier@ac-rouen.fr <olivier.gautier@ac-rouen.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-01-15 14:21:08 +0000

    Attention aux nouveaux articles et nouvelles rubriques : pas de messages d'erreur dans l'interface privée.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@69010 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- compteur_graphique_3/compteurgraphique_pipeline.php
+++ compteur_graphique_3/compteurgraphique_pipeline.php
@@ -12,10 +12,10 @@ function compteurgraphique_affiche_gauche($flux){
 	$tab_configuration = sql_fetch($test_configuration);
 	$res_configuration = $tab_configuration['id_compteur'];
 	if (!isset($res_configuration) OR ($GLOBALS['connect_statut'] == "0minirezo")) {
-		if (($exec == 'article_edit') OR ($exec == 'article')) {
+		if ((($exec == 'article_edit') OR ($exec == 'article')) AND (_request('new')!='oui')) {
 			$flux['data'] .= recuperer_fond('prive/squelettes/navigation/article_compteurgraphique',array('id_article' => $id_article));
 		}
-		if (($exec == 'rubrique_edit') OR ($exec == 'rubrique')) {
+		if ((($exec == 'rubrique_edit') OR ($exec == 'rubrique')) AND (_request('new')!='oui')) {
 			$flux['data'] .= recuperer_fond('prive/squelettes/navigation/rubrique_compteurgraphique',array('id_rubrique' => $id_rubrique));
 		}
 	}
