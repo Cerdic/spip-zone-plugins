489521579d4dccb265d856eecb54aa1b96c0336b
benlarcher@gmail.com <benlarcher@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-01-15 02:41:18 +0000

    langues (boussole)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@68998 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ boussole/trunk/lang/boussole_en.php
@@ -0,0 +1,198 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/boussole?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'bouton_boussole' => 'Compass',
+	'bouton_supprimer' => 'Delete',
+	'bulle_afficher' => 'Display in the models',
+	'bulle_aller_site' => 'Go to the page of the referenced website',
+	'bulle_cacher' => 'Do not display in the models',
+	'bulle_deplacer_bas' => 'Move down',
+	'bulle_deplacer_haut' => 'Move up',
+
+	// C
+	'colonne_alias' => 'Alias',
+	'colonne_nbr_sites' => 'Contains',
+	'colonne_titre' => 'Title',
+	'colonne_version' => 'Version',
+
+	// D
+	'descriptif_boussole_spip' => 'La boussole SPIP regroupe l\'ensemble des sites « officiels » de la galaxie SPIP. Elle définit pour chaque site, son logo, son nom, son slogan et sa description. N\'hésitez donc pas à l\'utiliser dans vos propres sites pour aiguiller vos visiteurs dans la galaxie SPIP.', # NEW
+	'descriptif_site_spip_blog' => 'Par ce que SPIP est un projet collaboratif, SPIP-BLOG.net  rassemble billets techniques, auto-dérision, humeurs, trolls, annonces diverses,... En cela il est bien le reflet de la communauté SPIP : d\'abord et avant tout, énormément de tendresse.', # NEW
+	'descriptif_site_spip_contrib' => 'Site collaboratif, SPIP-CONTRIB.net met à disposition l\'ensemble des contributions externes : plugins, scripts, filtres, squelettes, documentations, trucs et astuces,... versées à la communauté (liens de téléchargement) par les utilisateurs de SPIP. Ses forums assurent le lien entre développeurs et utilisateurs.', # NEW
+	'descriptif_site_spip_core' => 'Le site CORE.SPIP.org rassemble l\'historique des modifications du code de SPIP, une gestion de tickets permettant de signaler des bugs et de proposer des suggestions d\'amélioration et un espace de téléchargement des versions de SPIP.', # NEW
+	'descriptif_site_spip_demo' => 'Site de test réinitialisé chaque nuit, DEMO.SPIP.org permet à tout un chacun de tester SPIP dans sa dernière version stable (avec, au choix, le statut de rédacteur ou d\'administrateur), en un clic et sans avoir à l\'installer.', # NEW
+	'descriptif_site_spip_doc' => 'DOC.SPIP.org est le site référenciel où est détaillée, analysée et commentée (nom, emplacement, arguments, rôle et utilisation) chacune des fonctions du code source de SPIP.', # NEW
+	'descriptif_site_spip_edgard' => 'Edgard est le compagnon fidèle et infatigable de SPIP IRC où il intervient toujours à bon escient, avec tendresse et humour. C\'est depuis sa maison de EDGARD.SPIP.org qu\'il amène sur IRC conseils, réponses et bonne humeur. Accessoirement, Edgard est un robot (mais il ne donne pas l\'impression de le savoir...)', # NEW
+	'descriptif_site_spip_forum' => 'FORUM.SPIP.org est le site d\'échange et d\'entraide entre les utilisateurs de SPIP. Le site, qui existe dans une dizaine de langues, s\'organise autour de quatre grandes rubriques : installation & mise-à-jour, utilisation de l’espace privé, administration, gestion, configuration du site, création de squelettes.', # NEW
+	'descriptif_site_spip_herbier' => 'L\'herbier de SPIP n\'est pas un annuaire de liens comme les autres. Non. C\'est un <strong>album souvenir</strong> de sites réalisés avec SPIP, saisis à un moment donné de leur existence sur la toile. Il raconte une histoire du Web, celle de SPIP, depuis 2001. Cet herbier regroupe les sites que nous avons eu envie d\'épingler, parce qu\'intéressants pour tel contenu agréablement mis en valeur, telle aventure humaine, telle fonctionnalité habilement mise en œuvre, ou seulement visuellement séduisants. Des sites dont on se souvient, auprès desquels on est revenu, qui ont fait référence en marquant leur époque d\'une manière ou d\'une autre.', # NEW
+	'descriptif_site_spip_info' => 'En quelques pages, SPIP-INFO.net répond aux questions que se posent celles et ceux qui s\'interrogent sur la capacité de SPIP à servir de base à leur projet de développement d\'un site web. Particuliers, associations, entreprises trouveront ici toutes les informations pour étayer leur choix.', # NEW
+	'descriptif_site_spip_irc' => 'La communauté SPIP, qui ne dort jamais, s\'est dotée d\'un canal IRC (discussion instantanée par internet) ouvert à tous : http://webchat.freenode.net/?channels=#spip', # NEW
+	'descriptif_site_spip_mag' => 'Description du site SPIP Mag\'', # NEW
+	'descriptif_site_spip_net' => 'À l\'usage des utilisateurs et des webmestres, SPIP.net est le site officiel recommandé à toutes celles et ceux qui veulent installer un site sous SPIP, en comprendre le langage des boucles, balises et filtres, écrire et utiliser les squelettes. Il présente glossaire, tutoriels, conseils, historique des versions et espace de téléchargement. SPIP.net est traduit dans plus de vingt langues.', # NEW
+	'descriptif_site_spip_party' => 'Passer son temps entre un clavier et un écran pour coder du SPIP : c\'est bien. Se rencontrer entre développeurs et utilisateurs dans la vraie vie, en chair et en os, autour d\'un gateau et d\'un jus de fruit (ou autre boisson...) : c\'est beaucoup mieux. SPIP-PARTY.net est le site qui recense toutes ces rencontres : passées (compte-rendus, photos) et à venir (calendrier, agenda).', # NEW
+	'descriptif_site_spip_plugin' => 'PLUGINS.SPIP.net se veut l\'annuaire complet des modules complémentaires pour SPIP (plugins, squelettes, thèmes). Sont présentés pour chaque module : descriptif, auteur, licence, niveau de compatibilité par version de SPIP, dernières modifications apportées, état des traductions, statistique d\'utilisation, liens de documentation et de téléchargement.', # NEW
+	'descriptif_site_spip_programmer' => 'Plutôt destiné à un public de développeurs ou webmestres ayant déjà des connaissances en PHP, SQL, HTML, CSS et JavaScript, PROGRAMMER.SPIP.org présente la plupart des fonctionnalités (APIs, surcharges, pipelines,...) de SPIP à travers de nombreux exemples de code. Le site offre en téléchargement l\'intégralité de son contenu au format pdf sous licence libre cc-by-sa. PROGRAMMER.SPIP.org est consultable en français, anglais et espagnol.', # NEW
+	'descriptif_site_spip_sedna' => 'SEDNA.SPIP.org est l\'agrégateur RSS pour toute l\'actualité de la galaxie SPIP. Les dernières informations de plus de 70 sites gravitant autour de SPIP sont ainsi aisément consultables.', # NEW
+	'descriptif_site_spip_test' => 'Sur GRML.EU, installez facilement votre propre site en SPIP dans sa toute dernière version de développement. Grâce aux capacités de mutualisation de SPIP : une seule installation du «core» pour de multiples sites, vous êtes «chez vous», dans «votre» site, avec «votre» configuration. Convient particulièrement pour des stages de formation à l\'utilisation de SPIP.', # NEW
+	'descriptif_site_spip_twit' => 'SPIP.ORG permet aux utilisateurs de publier de courts messages sur le Web (140 caractères maximum) à destination du public ou de leurs amis. La publication peut se faire via le site, des flux RSS ou encore par messagerie instantanée.', # NEW
+	'descriptif_site_spip_user' => 'SPIP@REZO.net est la liste d\'entraide entre utilisateurs de SPIP. Les archives de cette liste sont consultables sur http://archives.rezo.net/archives/spip.mbox/ ou encore, sous forme de blog sur http://blog.gmane.org/gmane.comp.web.spip.user.', # NEW
+	'descriptif_site_spip_video' => 'Description du site Vidéos', # NEW
+	'descriptif_site_spip_zine' => 'Description du site SPIPZine', # NEW
+	'descriptif_site_spip_zone' => 'Espace de développement régi par une charte, ZONE.SPIP.org donne accès au versioning de toutes les contributions complémentaires à SPIP (plugins, squelettes, thèmes, modèles, outils,...).', # NEW
+	'description_noisette_boussole' => 'Standard compass display. You can choose the displayed model (text links, logos...) and its configuration',
+	'description_noisette_boussole_actualite' => 'Display syndicated website articles of a compass depending on the display model <code>boussole_liste_actualite</code>.',
+	'description_noisette_boussole_contenu_z' => 'Affichage de toutes les informations d\'une boussole comme contenu principal d\'une page Z et selon le modèle d\'affichage <code>boussole_contenu_z</code>.', # NEW
+	'description_noisette_boussole_fil_ariane' => 'Affichage du fil d\'ariane d\'une boussole.', # NEW
+	'description_page_boussole' => 'Page des informations détaillées d\'une boussole', # NEW
+
+	// I
+	'info_ajouter_boussole' => 'En ajoutant des boussoles à votre base de données, vous aurez la possiblité d\'utiliser les modèles fournis pour les afficher dans vos pages publiques.<br />Une boussole est décrite par un fichier XML contenant les informations sur la boussole et ses sites.<br /><br />Si la boussole existe déjà ce formulaire permettra de la mettre à jour en conservant la configuration d\'affichage.', # NEW
+	'info_boite_boussoles_gerer' => '<strong>Cette page est uniquement accessible aux responsables du site.</strong><p>Elle permet l’ajout, la mise à jour et la suppression des boussoles. Il est aussi possible de se rendre sur la page de configuration de l\'affichage de chaque boussole.</p>', # NEW
+	'info_configurer_boussole' => 'Ce formulaire vous permez de configurer l\'affichage de la boussole en choisissant les sites à afficher ou pas et l\'ordre d\'affichage dans un groupe. Les sites non affichés sont repérés par un fond hachuré et une police grise.', # NEW
+	'info_fichier_boussole' => 'Saisissez l\'url du fichier de description de votre boussole', # NEW
+	'info_liste_aucune_boussole' => 'Aucune boussole n\'a encore été chargée dans votre base. Utilisez le formulaire ci-dessous pour en ajouter.', # NEW
+	'info_site_boussole' => 'Ce site fait partie de la boussole :', # NEW
+	'info_site_boussoles' => 'This website belongs to the compasses :',
+
+	// L
+	'label_1_site' => '@nb@ site', # NEW
+	'label_a_class' => 'Classe de l\'ancre englobant le logo', # NEW
+	'label_actualise_le' => 'Actualisée le', # NEW
+	'label_affiche' => 'Affiché ?', # NEW
+	'label_afficher_descriptif' => 'Afficher le descriptif des sites ?', # NEW
+	'label_afficher_lien_accueil' => 'Afficher le lien vers la page d\'accueil ?', # NEW
+	'label_afficher_slogan' => 'Afficher le slogan des sites ?', # NEW
+	'label_ariane_separateur' => 'Séparateur :', # NEW
+	'label_boussole' => 'Boussole à afficher', # NEW
+	'label_cartouche_boussole' => 'Afficher le cartouche de la boussole ?', # NEW
+	'label_demo' => 'Retrouvez la page de démo de cette boussole à l\'adresse', # NEW
+	'label_descriptif' => 'Descriptif', # NEW
+	'label_div_class' => 'Classe du div englobant', # NEW
+	'label_div_id' => 'Id du div englobant', # NEW
+	'label_fichier_xml' => 'Fichier XML', # NEW
+	'label_li_class' => 'Classe de chaque balise li de la liste', # NEW
+	'label_logo' => 'Logo', # NEW
+	'label_max_articles' => 'Nombre max d\'articles affichés par site', # NEW
+	'label_max_sites' => 'Nombre max de sites', # NEW
+	'label_mode' => 'Choisissez une boussole', # NEW
+	'label_mode_standard' => '« @boussole@ », boussole officielle des sites SPIP', # NEW
+	'label_mode_url_perso' => 'Boussole personnelle', # NEW
+	'label_modele' => 'Modèle d\'affichage', # NEW
+	'label_n_sites' => '@nb@ sites', # NEW
+	'label_nom' => 'Nom', # NEW
+	'label_p_class' => 'Classe du paragraphe englobant le descriptif', # NEW
+	'label_sepia' => 'Sepia color code (without #)',
+	'label_slogan' => 'Slogan',
+	'label_taille_logo' => 'Maximum size of the logo (in pixels)',
+	'label_taille_logo_boussole' => 'Maximum size of the compass logo (in pixels)',
+	'label_taille_titre' => 'Maximum size of the compass title',
+	'label_titre_actualite' => 'Display the title of the news block ?',
+	'label_titre_boussole' => 'Display the title of the compass ?',
+	'label_titre_groupe' => 'Display the title of each group ?',
+	'label_titre_site' => 'Display the title of each website ?',
+	'label_type_bulle' => 'Displayed information in the bubble on each link',
+	'label_type_description' => 'Displayed description on the side of the logo',
+	'label_ul_class' => 'ul marker class of the list',
+	'label_url' => 'URL',
+	'label_version' => 'Version',
+
+	// M
+	'message_nok_boussole_inconnue' => 'No compass correspond to the alias « @alias@ »',
+	'message_nok_champ_obligatoire' => 'Mandatory field',
+	'message_nok_ecriture_bdd' => 'Database writing error',
+	'message_nok_xml_introuvable' => 'The file « @fichier@ » cannot be found',
+	'message_nok_xml_invalide' => 'The XML description file « @fichier@ » of the compass is not conform to the DTD',
+	'message_ok_boussole_actualisee' => 'The compass « @fichier@ » has been updated.',
+	'message_ok_boussole_ajoutee' => 'The compass « @fichier@ » has been added.',
+	'modele_boussole_liste_avec_logo' => 'List of links with names, logos and descritpion',
+	'modele_boussole_liste_par_groupe' => 'List of text links per group',
+	'modele_boussole_liste_simple' => 'List with text links',
+	'modele_boussole_panorama' => 'Logos galery',
+	'modele_boussole_panorama_sepia' => 'Logos galery with sepia effect ',
+
+	// N
+	'nom_boussole_spip' => 'SPIP Galaxy',
+	'nom_groupe_spip_actualite' => 'SPIP News',
+	'nom_groupe_spip_aide' => 'SPIP help and exchange',
+	'nom_groupe_spip_decouverte' => 'SPIP discovery',
+	'nom_groupe_spip_extension' => 'SPIP add-ons and contributions',
+	'nom_groupe_spip_reference' => 'SPIP References',
+	'nom_noisette_boussole' => 'Compass',
+	'nom_noisette_boussole_actualite' => 'Compass - News',
+	'nom_noisette_boussole_contenu_z' => 'Compass - Main content of the page',
+	'nom_noisette_boussole_fil_ariane' => 'Compass - page breadcrumb trail',
+	'nom_page_boussole' => 'Compass',
+	'nom_site_spip_blog' => 'SPIP Blog',
+	'nom_site_spip_contrib' => 'SPIP-Contrib',
+	'nom_site_spip_core' => 'SPIP Core',
+	'nom_site_spip_demo' => 'SPIP Demo',
+	'nom_site_spip_doc' => 'SPIP Doc',
+	'nom_site_spip_edgard' => 'Edgard',
+	'nom_site_spip_forum' => 'SPIP Forums',
+	'nom_site_spip_herbier' => 'SPIP Herbarium',
+	'nom_site_spip_info' => 'SPIP info',
+	'nom_site_spip_irc' => 'SPIP IRC',
+	'nom_site_spip_mag' => 'SPIP Mag\'',
+	'nom_site_spip_net' => 'SPIP.net',
+	'nom_site_spip_party' => 'SPIP Party',
+	'nom_site_spip_plugin' => 'SPIP Plugins',
+	'nom_site_spip_programmer' => 'Programming with SPIP',
+	'nom_site_spip_sedna' => 'Sedna',
+	'nom_site_spip_test' => 'SPIP Test',
+	'nom_site_spip_twit' => 'SPIP.org',
+	'nom_site_spip_user' => 'SPIP User',
+	'nom_site_spip_video' => 'SPIP Media',
+	'nom_site_spip_zine' => 'SPIPZine',
+	'nom_site_spip_zone' => 'SPIP Zone',
+
+	// O
+	'option_aucune_description' => 'No description',
+	'option_descriptif_site' => 'Website description',
+	'option_nom_site' => 'Website name',
+	'option_nom_slogan_site' => 'Name and slogan of the website',
+	'option_slogan_site' => 'Website slogan',
+
+	// S
+	'slogan_boussole_spip' => 'The best links of the SPIP Galaxy !',
+	'slogan_site_spip_blog' => 'Free software and tenderness',
+	'slogan_site_spip_contrib' => 'SPIP contribution area',
+	'slogan_site_spip_core' => 'SPIP versions development area ',
+	'slogan_site_spip_demo' => 'Test the last SPIP stable version',
+	'slogan_site_spip_doc' => 'Technical coding reference of SPIP',
+	'slogan_site_spip_edgard' => 'Un coup de bot et ça repart !', # NEW
+	'slogan_site_spip_forum' => 'SPIP users forum',
+	'slogan_site_spip_herbier' => 'A selection of amazing SPIP websites',
+	'slogan_site_spip_info' => 'Discover SPIP and its community',
+	'slogan_site_spip_irc' => 'Welcome on the SPIP chat room',
+	'slogan_site_spip_mag' => 'SPIP news sheet',
+	'slogan_site_spip_net' => 'Official SPIP documentation and download ',
+	'slogan_site_spip_party' => 'Agenda of the SPIP meetings',
+	'slogan_site_spip_plugin' => 'SPIP Plugins Directory',
+	'slogan_site_spip_programmer' => 'SPIP developers documentation',
+	'slogan_site_spip_sedna' => 'SPIP Galaxy RSS feeds',
+	'slogan_site_spip_test' => 'Test the set up and the implementation of a SPIP site',
+	'slogan_site_spip_twit' => 'SPIP Community blog',
+	'slogan_site_spip_user' => 'Mutual aid list of the SPIP users',
+	'slogan_site_spip_video' => 'SPIP media library',
+	'slogan_site_spip_zine' => 'Webzine about SPIP',
+	'slogan_site_spip_zone' => 'Area dedicated to SPIP contribution developement',
+
+	// T
+	'titre_actualite_spip' => 'SPIP Galaxy news',
+	'titre_boite_autres_boussoles' => 'Other compasses',
+	'titre_boite_infos_boussole' => 'ALIAS COMPASS',
+	'titre_boite_logo_boussole' => 'COMPASS LOGO',
+	'titre_form_ajouter_boussole' => 'Add or update a compass',
+	'titre_formulaire_configurer' => 'Compass display configuration',
+	'titre_liste_boussoles' => 'List of available compasses',
+	'titre_page_boussole' => 'Compass management'
+);
+
+?>
