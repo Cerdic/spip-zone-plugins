7b95bdaed1f5343da15d68ec7459004fe5de620a
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-01-16 02:40:52 +0000

    langues (manuelsite)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@69021 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- manuel_site/lang/manuelsite_en.php
+++ manuel_site/lang/manuelsite_en.php
@@ -8,26 +8,26 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// C
-	'configurer_explication' => 'This plugin displays an help icon on every page of the private space showing the Website Editor Manual. This manual is @texte@ Its purpose is to explain the architecture of the site to the editors, in which section to store what, how to encode and install a video… So, all that you want and which is specific to your website.',
-	'configurer_explication_l_article' => '<a href="@url@" title="Website Editor Manual">the article #@idart@</a> of your website.',
+	'configurer_explication' => 'This plugin displays an help icon on every page of the private space showing the Website Editor Handbook. This handbook is @texte@ Its purpose is to explain the architecture of the site to the editors, in which section to store what, how to encode and install a video… So, all that you want and which is specific to your website.',
+	'configurer_explication_l_article' => '<a href="@url@" title="Website Editor Handbook">the article #@idart@</a> of your website.',
 	'configurer_explication_un_article' => 'an article of the website.',
-	'configurer_titre' => 'Configure the Website Editor Manual',
+	'configurer_titre' => 'Configure the Website Editor Handbook',
 
 	// E
-	'erreur_article' => 'The article of the manual defined in the plugin\'s configuration is not found: #@idart@',
-	'erreur_article_publie' => 'L’article du manuel défini dans la configuration du plugin n\'est pas publié en ligne : <a href="@url@">#@idart@</a>', # NEW
-	'erreur_pas_darticle' => 'The article of the manuel is not defined in the plugin\'s configuration',
-	'explication_afficher_bord_gauche' => 'Display the manual\'s icon in the top left corner (if not, the manual will be displayed in a column)',
-	'explication_background_color' => 'Type in the background color of the manual display area',
+	'erreur_article' => 'The article of the handbook defined in the plugin\'s configuration is not found: #@idart@',
+	'erreur_article_publie' => 'The article defined in the configuration of the handbook plugin is not published online: <a href="@url@">#@idart@</a>',
+	'erreur_pas_darticle' => 'The article of the handbook is not defined in the plugin\'s configuration',
+	'explication_afficher_bord_gauche' => 'Display the handbook\'s icon in the top left corner (if not, the handbook will be displayed in a column)',
+	'explication_background_color' => 'Type in the background color of the handbook display area',
 	'explication_cacher_public' => 'Hide this article in the public space, even in rss flow',
 	'explication_email' => 'Contact email for editors',
-	'explication_faq' => 'Listed below are generic blocks codes which can be used to write your manual. The text for each code is displayed (without formatting) when rolling over it. Just copy / paste the code in the textarea of your article.<br />To hide the question, add the parameter <i>|q=no</i>.<br />To add other parameters, add <i>|params=p1:v1;p2:v2</i>.',
-	'explication_id_article' => 'Type in the number of the article which contains the manual',
-	'explication_intro' => 'Introduction text of the manual (will be placed before the introduction of the article)',
-	'explication_largeur' => 'Type in the manual display area width (in px)',
+	'explication_faq' => 'Listed below are generic blocks codes which can be used to write your handbook. The text for each code is displayed (without formatting) when rolling over it. Just copy / paste the code in the textarea of your article.<br />To hide the question, add the parameter <i>|q=no</i>.<br />To add other parameters, add <i>|params=p1:v1;p2:v2</i>.',
+	'explication_id_article' => 'Type in the number of the article which contains the handbook',
+	'explication_intro' => 'Introduction text of the handbook (will be placed before the introduction of the article)',
+	'explication_largeur' => 'Type in the handbook display area width (in px)',
 
 	// F
-	'fermer_le_manuel' => 'Close the manual',
+	'fermer_le_manuel' => 'Close the handbook',
 
 	// H
 	'help' => 'Help : ',
@@ -47,9 +47,9 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'legende_contenu' => 'Contents',
 
 	// T
-	'titre_faq' => 'FAQ of the Website Editor Manual',
-	'titre_manuel' => 'Website Editor Manual',
-	'titre_menu' => 'Website Editor Manual'
+	'titre_faq' => 'FAQ of the Website Editor Handbook',
+	'titre_manuel' => 'Website Editor Handbook',
+	'titre_menu' => 'Website Editor Handbook'
 );
 
 ?>
