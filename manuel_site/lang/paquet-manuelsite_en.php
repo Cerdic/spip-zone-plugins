f0ed5c72757dc3ba337b50eb495bfabbf0872bd5
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-01-16 02:40:55 +0000

    langues (paquet-manuelsite)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@69022 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- manuel_site/lang/paquet-manuelsite_en.php
+++ manuel_site/lang/paquet-manuelsite_en.php
@@ -8,9 +8,9 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// M
-	'manuelsite_description' => 'This plugin displays an help icon on every page of the private space showing the Website Editor Manual. This manual is an article of the website. The plugin also provides a set of Frequently Asked Questions that can be easily inserted in your manual.',
-	'manuelsite_nom' => 'Website Editor Manual',
-	'manuelsite_slogan' => 'A manual specific to your site for your editors'
+	'manuelsite_description' => 'This plugin displays an help icon on every page of the private space showing the Website Editor Handbook. This handbook is an article of the website. The plugin also provides a set of Frequently Asked Questions that can be easily inserted in your handbook.',
+	'manuelsite_nom' => 'Website Editor Handbook',
+	'manuelsite_slogan' => 'A handbook specific to your site for your editors'
 );
 
 ?>
