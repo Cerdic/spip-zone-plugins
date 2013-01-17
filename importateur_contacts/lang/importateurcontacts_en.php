1236e4a875716478fe1b29cc3442e6698cccfe5f
benlarcher@gmail.com <benlarcher@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-01-17 02:41:33 +0000

    langues (importateurcontacts)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@69039 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ importateur_contacts/lang/importateurcontacts_en.php
@@ -0,0 +1,27 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/importateurcontacts?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'configurer_fournisseur_active_avec_moteur' => 'Activate with @moteur@',
+	'configurer_fournisseur_desactive' => 'Desactivate',
+	'configurer_titre' => 'Configure contacts importation service',
+
+	// E
+	'erreur_aucun_fournisseur' => 'No service providing contacts has been found. Please, install at least one contact importation library.',
+
+	// I
+	'importer_bouton_recuperer_contacts' => 'Recover my contacts',
+	'importer_fournisseur_label' => 'Choose the service where you recover your contacts',
+	'info_aucun_contact' => 'No contact have been found on this account.',
+	'info_nb_contacts' => '@nb@ contacts have been recovered.',
+	'info_nb_contacts_erreur' => '@nb@ contacts have been recovered but can not be used.',
+	'info_titre' => 'Contacts Importer'
+);
+
+?>
