afb4a90b9b4fc0131c043124c55b8d0d6fce1aea
joseph@larmarange.net <joseph@larmarange.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-01-17 15:35:54 +0000

    Transformation de Biblio AERES en Biblio Check, plus générique et ne gérant pas les spécificités du classement AERES.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@69050 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ bibliocheck/trunk/lang/paquet-bibliocheck_fr.php
@@ -0,0 +1,19 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-bibliocheck
+// Langue: fr
+// Date: 30-11-2012 13:50:03
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// B
+	'bibliocheck_description' => 'Ce plugin fournit une interface pour indiquer en ligne des corrections à effectuer ou des références à ajouter à une base de données bibliographiques gérée avec ZotSpip. Biblio Check se configure sur la page de configuration de ZotSpip.',
+	'bibliocheck_slogan' => 'Vérifiez votre base bibliographique Zotero',
+);
+?>
