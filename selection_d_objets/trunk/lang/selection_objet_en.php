8c9feaee04f512fec1f64266febdb5f6d4f22df0
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-01-19 02:41:29 +0000

    langues (selection_objet)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@69086 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- selection_d_objets/trunk/lang/selection_objet_en.php
+++ selection_d_objets/trunk/lang/selection_objet_en.php
@@ -16,6 +16,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// E
 	'explication_selection_objets_cible' => 'Les objets cibles ou pourront être affichés les objets sélectionnés', # NEW
+	'explication_selection_type_liens' => 'Vous pouvez définir des types de relations : indiquez-en une par ligne, suivie d\'une virgule et d\'une description. Une ligne vide pour la valeur par défaut. La description peut être une chaîne de langue.', # NEW
 
 	// I
 	'icone_creer_selection_objet' => 'Select an object',
@@ -36,7 +37,8 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// O
 	'objet_destination_choisis' => 'Selected destination(s):',
-	'objets_cible_dispos' => 'Les Objets Cible disponibles', # NEW
+	'objets_cible' => 'Les Objets Cible', # NEW
+	'objets_selection' => 'Les Objets Sélection', # NEW
 
 	// R
 	'racine' => 'Root',
@@ -50,6 +52,9 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'selection_objet_cible_titre' => 'Selection d\'objets cibles', # NEW
 	'selection_objets_pour' => 'Object selection for',
 	'selection_rubrique_objet' => 'Objects that can be selected',
+	'selection_type_lien' => 'Type lien', # NEW
+	'selection_type_liens' => 'Définition de type de liens', # NEW
+	'selectione' => 'Sélectionné :', # NEW
 
 	// T
 	'texte_ajouter_objet' => 'Select an object',
@@ -58,7 +63,9 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'titre_langue_objet' => 'Language of this Selected Object',
 	'titre_logo_selection_objet' => 'Logo of this Selected Object',
 	'titre_selection_objets' => 'Selected Object',
-	'titre_selection_objets_rubrique' => 'Selected Objects of the section'
+	'titre_selection_objets_rubrique' => 'Selected Objects of the section',
+	'type_liens_defaut' => 'utilisable pour tous les objets cibles. Pour chaque objet peuvent être définies des types de lien spécifique', # NEW
+	'type_liens_surcharge' => 'surcharge les types de lien par défaut' # NEW
 );
 
 ?>
--- selection_d_objets/trunk/lang/selection_objet_fr.php
+++ selection_d_objets/trunk/lang/selection_objet_fr.php
@@ -14,7 +14,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// E
 	'explication_selection_objets_cible' => 'Les objets cibles ou pourront être affichés les objets sélectionnés',
-	'explication_selection_type_liens' => 'Vous pouvez définir des types de relations : indiquez-en une par ligne, suivie d\'une virgule et d\'une description. Une ligne vide pour la valeur par défaut. La description peut être une chaîne de langue.',	
+	'explication_selection_type_liens' => 'Vous pouvez définir des types de relations : indiquez-en une par ligne, suivie d\'une virgule et d\'une description. Une ligne vide pour la valeur par défaut. La description peut être une chaîne de langue.',
 
 	// I
 	'icone_creer_selection_objet' => 'Sélectionner un objet',
@@ -36,7 +36,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// O
 	'objet_destination_choisis' => 'Destination(s) choisie(s) :',
 	'objets_cible' => 'Les Objets Cible',
-	'objets_selection' => 'Les Objets Sélection',	
+	'objets_selection' => 'Les Objets Sélection',
 
 	// R
 	'racine' => 'Racine',
@@ -49,10 +49,10 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'selection_objet' => 'Sélection',
 	'selection_objet_cible_titre' => 'Selection d\'objets cibles',
 	'selection_objets_pour' => 'Sélection objets pour',
-	'selection_type_lien' => 'Type lien',
-	'selection_type_liens' => 'Définition de type de liens',		
-	'selectione' => 'Sélectionné :',	
 	'selection_rubrique_objet' => 'Objets qui peuvent être sélectionnés',
+	'selection_type_lien' => 'Type lien',
+	'selection_type_liens' => 'Définition de type de liens',
+	'selectione' => 'Sélectionné :',
 
 	// T
 	'texte_ajouter_objet' => 'Sélectionner un objet',
@@ -62,8 +62,8 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'titre_logo_selection_objet' => 'Logo de cet objet sélectionné',
 	'titre_selection_objets' => 'Objet sélectionné',
 	'titre_selection_objets_rubrique' => 'Objets sélectionnés de la rubrique',
-    'type_liens_defaut' => 'utilisable pour tous les objets cibles. Pour chaque objet peuvent être définies des types de lien spécifique', 	
-    'type_liens_surcharge' => 'surcharge les types de lien par défaut',        	
+	'type_liens_defaut' => 'utilisable pour tous les objets cibles. Pour chaque objet peuvent être définies des types de lien spécifique',
+	'type_liens_surcharge' => 'surcharge les types de lien par défaut'
 );
 
 ?>
