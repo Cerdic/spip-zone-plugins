898191e3bb04eb4706e691b1b532027b0d838892
benlarcher@gmail.com <benlarcher@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-01-20 02:42:01 +0000

    langues (thumbsites)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@69106 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- thumbsites/trunk/lang/thumbsites_en.php
+++ thumbsites/trunk/lang/thumbsites_en.php
@@ -8,34 +8,34 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// B
-	'bouton_copier_comme_logo' => 'Définir comme logo', # NEW
-	'bouton_rafraichir' => 'Rafraichir la vignette', # NEW
+	'bouton_copier_comme_logo' => 'Define as logo',
+	'bouton_rafraichir' => 'Refresh the thumbnail',
 
 	// C
 	'cfg_descriptif' => 'This page is sued for configuring the Thumbsites plugin, and in particular, to select the thumbnails server. To find out more, please refer to the <a href="http://www.spip-contrib.net/?article2584">documentation on contrib</a>',
-	'cfg_inf_apercite_presentation' => '<a href="http://www.apercite.fr">apercite.fr</a> propose un service gratuit qui ne nécessite pas d\'identification.', # NEW
-	'cfg_inf_choix_serveur' => 'Choisissez le serveur qui fournira les vignettes de vos sites référencés et compléter éventuellement son paramétrage.', # NEW
-	'cfg_inf_miwim_presentation' => 'Pour utiliser ce service vous devez mettre un lien vers le site <a href="http://thumbs.miwim.fr">Miwim</a>. La présence de ce lien est vérifié périodiquement par un script.', # NEW
+	'cfg_inf_apercite_presentation' => '<a href="http://www.apercite.fr">apercite.fr</a> propose a free service without identification.',
+	'cfg_inf_choix_serveur' => 'Choose the server which will provide thumbnails of your favorite referenced websites and eventually complete the configuration.',
+	'cfg_inf_miwim_presentation' => 'To use this service, you need to implement a link to <a href="http://thumbs.miwim.fr">Miwim</a>. The presence of this link is verified periodically by a script.',
 	'cfg_inf_rotothumb_presentation' => 'To use this service, you do not have to subscribe but you do have to create a link to the <a href="http://www.robothumb.com">Robothumb</a> site. The continued existence of that link is periodically checked by their servers.',
 	'cfg_inf_thumbshots_de_presentation' => '<a href="http://www.thumbshots.de">Thumbshots.de</a> offers a service equivalent to Thumbshots.com. Even though the services are identical, they are actually 2 distinct sites.',
 	'cfg_inf_thumbshots_presentation' => '<a href="http://www.thumbshots.com">Thumbshots.com</a> offers a free service that does not require identification.',
 	'cfg_inf_websnapr_presentation' => 'To use this service, you must subscribe on the <a href="http://www.websnapr.com">Websnapr</a> site. The free version enables the processing of 250,000 site screen captures per month at a maximum speed of 80 captures per hour.',
-	'cfg_itm_serveur_apercite' => 'Apercite.fr', # NEW
-	'cfg_itm_serveur_miwim' => 'Miwim', # NEW
+	'cfg_itm_serveur_apercite' => 'Apercite.fr',
+	'cfg_itm_serveur_miwim' => 'Miwim',
 	'cfg_itm_serveur_robothumb' => 'Robothumb',
 	'cfg_itm_serveur_thumbshots' => 'Thumbshots.com',
 	'cfg_itm_serveur_thumbshots_de' => 'Thumbshots.de',
 	'cfg_itm_serveur_websnapr' => 'Websnapr',
 	'cfg_itm_taille_vignette' => '@taille@ pixels',
 	'cfg_lbl_cle' => 'Your key',
-	'cfg_lbl_duree_cache' => 'Durée du cache (en jours)', # NEW
-	'cfg_lbl_serveur' => 'Serveur', # NEW
+	'cfg_lbl_duree_cache' => 'Cache duration (in days)',
+	'cfg_lbl_serveur' => 'Server',
 	'cfg_lbl_taille_vignette' => 'Size of thumbnails',
-	'cfg_lbl_usage' => 'Conditions d\'utilisation', # NEW
-	'cfg_lgd_cache' => 'Cache', # NEW
-	'cfg_lgd_choix_serveur' => 'Server', # MODIF
-	'cfg_titre_form' => 'Configurer Thumbsites', # NEW
-	'cfg_titre_page' => 'Thumbsites', # NEW
+	'cfg_lbl_usage' => 'Conditions of use',
+	'cfg_lgd_cache' => 'Cache',
+	'cfg_lgd_choix_serveur' => 'Service',
+	'cfg_titre_form' => 'Configure Thumbsites',
+	'cfg_titre_page' => 'Thumbsites',
 
 	// T
 	'titre_thumbshot_site' => 'SITE THUMBNAIL'
