2e179ceb47cc08c1bbef709c453faedacab5b201
benlarcher@gmail.com <benlarcher@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-01-20 02:42:07 +0000

    langues (zotspip)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@69107 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- zotspip/trunk/lang/zotspip_en.php
+++ zotspip/trunk/lang/zotspip_en.php
@@ -28,7 +28,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'createurs' => 'Author(s)',
 
 	// D
-	'description_page-biblio' => 'Recherche et affichage des références bibliographiques de la librairie Zotero synchronisée avec ZotSpip.', # NEW
+	'description_page-biblio' => 'Search and display of the bibliographical references from the library Zotero synchronised with ZotSpip.',
 	'deselectionner_tout' => 'Unselect all',
 	'droits_insuffisants' => 'You don\'t have the permission to do this modification.',
 
@@ -127,7 +127,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'modifier_en_ligne' => 'Modify online on zotero.org',
 
 	// N
-	'nom_page-biblio' => 'Biblio', # NEW
+	'nom_page-biblio' => 'Bibliography',
 	'nom_prenom' => 'Last name, first name',
 
 	// O
