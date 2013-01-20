d07e324605779498fb5108114d205f6dc31201a1
benlarcher@gmail.com <benlarcher@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-01-20 02:41:32 +0000

    langues (paquet-amap)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@69097 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ amap/trunk/lang/paquet-amap_en.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-amap?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'amap_description' => 'The plugin is developped to manage baskets and subscibers. 
+The baskets are created and given to someone subscribing. He can make its basket available if he cannot be there to get it on the distribution day.',
+	'amap_slogan' => 'Baskets and subscribers management for CSA'
+);
+
+?>
