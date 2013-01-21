c7fa5812de712a68fc33252333c3ad2c2c562735
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-01-21 19:40:16 +0000

    Les css n'étant utilisés que dans l'espace privé utilisons plutôt la technique du fichier style_prive_plugin_boussole.html.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@69162 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- boussole/trunk/boussole_pipelines.php
+++ boussole/trunk/boussole_pipelines.php
@@ -4,20 +4,6 @@
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 /**
- * Insertion dans le pipeline header_prive
- * Inclure les css prive de la boussole (formulaire de configuration)
- *
- * @param object $flux
- * @return $flux
- */
-function boussole_header_prive($flux){
-	$css = find_in_path('css/boussole_prive.css');
-	$flux .= "\n<link rel='stylesheet' href='$css' type='text/css' />\n";
-	return $flux;
-}
-
-
-/**
  * Insertion dans le pipeline affiche_milieu
  * Affiche un bloc l'identification de la boussole a laquelle appartient le site edite
  *
