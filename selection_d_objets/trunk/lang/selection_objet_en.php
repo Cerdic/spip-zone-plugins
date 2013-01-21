ffead5f6498fc2a1a61ab317589c7c497ae3a25e
benlarcher@gmail.com <benlarcher@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-01-21 02:41:14 +0000

    langues (selection_objet)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@69126 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- selection_d_objets/trunk/lang/selection_objet_en.php
+++ selection_d_objets/trunk/lang/selection_objet_en.php
@@ -15,8 +15,8 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'configurations' => 'Configurations',
 
 	// E
-	'explication_selection_objets_cible' => 'Les objets cibles ou pourront être affichés les objets sélectionnés', # NEW
-	'explication_selection_type_liens' => 'Vous pouvez définir des types de relations : indiquez-en une par ligne, suivie d\'une virgule et d\'une description. Une ligne vide pour la valeur par défaut. La description peut être une chaîne de langue.', # NEW
+	'explication_selection_objets_cible' => 'The target objects where can be displayed the selected objects',
+	'explication_selection_type_liens' => 'You can define types of relationships: please indicate one per line, followed by a comma and a description. A blank line for the default value. The description can be a chain of language.',
 
 	// I
 	'icone_creer_selection_objet' => 'Select an object',
@@ -37,7 +37,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// O
 	'objet_destination_choisis' => 'Selected destination(s):',
-	'objets_cible' => 'Les Objets Cible', # NEW
+	'objets_cible' => 'The target objects',
 	'objets_selection' => 'Les Objets Sélection', # NEW
 
 	// R
@@ -47,14 +47,14 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'rubriques_choisis' => 'Selected sections:',
 
 	// S
-	'selection_cible' => 'Seléction des cibles pour', # NEW
+	'selection_cible' => 'Target selection for',
 	'selection_objet' => 'Selection',
-	'selection_objet_cible_titre' => 'Selection d\'objets cibles', # NEW
+	'selection_objet_cible_titre' => 'Selection of target objects',
 	'selection_objets_pour' => 'Object selection for',
 	'selection_rubrique_objet' => 'Objects that can be selected',
-	'selection_type_lien' => 'Type lien', # NEW
-	'selection_type_liens' => 'Définition de type de liens', # NEW
-	'selectione' => 'Sélectionné :', # NEW
+	'selection_type_lien' => 'Link type',
+	'selection_type_liens' => 'Links type definition',
+	'selectione' => 'Selected :',
 
 	// T
 	'texte_ajouter_objet' => 'Select an object',
@@ -64,8 +64,8 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'titre_logo_selection_objet' => 'Logo of this Selected Object',
 	'titre_selection_objets' => 'Selected Object',
 	'titre_selection_objets_rubrique' => 'Selected Objects of the section',
-	'type_liens_defaut' => 'utilisable pour tous les objets cibles. Pour chaque objet peuvent être définies des types de lien spécifique', # NEW
-	'type_liens_surcharge' => 'surcharge les types de lien par défaut' # NEW
+	'type_liens_defaut' => 'can be used for all target objects. For each object can be defined specific link types',
+	'type_liens_surcharge' => 'overload the default link types'
 );
 
 ?>
