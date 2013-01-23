3fb2c5e080122b03e4aba2d3e3b966f311890921
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-01-23 21:15:20 +0000

    Installation SPIP 3, et simplification vu que c'était un peu bizarre.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@69245 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- bigbrother/trunk/bigbrother_administrations.php
+++ bigbrother/trunk/bigbrother_administrations.php
@@ -5,43 +5,24 @@
 #  Licence : GPL                                    #
 #------------------------------------------------- -#
 
-if (!defined("_ECRIRE_INC_VERSION")) return;
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 // La fonction de base appelée par le gestionnaire de plugins
 function bigbrother_upgrade($nom_meta_base_version,$version_cible){
-	include_spip('inc/meta');
-	include_spip('base/abstract_sql');
-	$current_version = 0.0;
-	if ((!isset($GLOBALS['meta'][$nom_meta_base_version]))
-		|| (($current_version = $GLOBALS['meta'][$nom_meta_base_version])!=$version_cible)){
-
-		if (version_compare($current_version,'0.0','<=')){
-			include_spip('base/create');
-			// A la première installation on crée les tables
-			creer_base();
-			ecrire_meta($nom_meta_base_version,$current_version=$version_cible,'non');
-		}
-		if (version_compare($current_version,'0.1','<')){
-			include_spip('base/create');
-			// A la première installation on crée les tables
-			creer_base();
-			ecrire_meta($nom_meta_base_version,$current_version=0.1,'non');
-		}
-		if (version_compare($current_version,'0.2','<')){
-			include_spip('base/create');
-			// A la première installation on crée les tables
-			creer_base();
-			ecrire_meta($nom_meta_base_version,$current_version=0.2,'non');
-		}
-	}
+	$maj = array();
+	$maj['create'] = array(
+		array('maj_tables',array('spip_journal', 'spip_visites_auteurs', 'spip_visites_articles_auteurs')),
+	);
+	
+	include_spip('base/upgrade');
+	maj_plugin($nom_meta_base_version, $version_cible, $maj);
 }
 
 // Supprimer les tables du plugin
 function bigbrother_vider_tables($nom_meta_base_version) {
-	include_spip('base/abstract_sql');
-	sql_query("DROP TABLE spip_visites_auteurs");
-	sql_query("DROP_TABLE spip_visites_articles_auteurs");
-	sql_query("DROP TABLE spip_journal");
+	sql_drop_table('spip_visites_auteurs');
+	sql_drop_table('spip_visites_articles_auteurs');
+	sql_drop_table('spip_journal');
 	effacer_meta($nom_meta_base_version);
 }
 
