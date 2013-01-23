0284f6022db3f16a05a265a86b398d0d00151c3f
benlarcher@gmail.com <benlarcher@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-01-23 02:40:48 +0000

    langues (paquet-multilingue)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@69216 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ multilingue/trunk/lang/paquet-multilingue_en.php
@@ -0,0 +1,32 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-multilingue?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// M
+	'multilingue_description' => 'Implement a multilingual site with a language per area.
+
+The plugin forces the installation of several plugin
+
+- [Translation between sections->http://plugins.spip.net/tradrub.html]
+- [Preferred language by the user->http://www.spip-contrib.net/Plugin-Langue-preferee-par-l-internaute]
+- [Translations of other sections->http://www.spip-contrib.net/Traductions-de-rubriques-autrement-3]
+- [Another way to translate articles-> http://www.spip-contrib.net/Traductions-d-articles-autrement-3]
+
+Under Spip 2 or 2.1 it is advisable to install before the plugin [Step->http://www.spip-contrib.net/STEP-Systeme-de-Telechargements-de-Plugins], then install this plugin which will force also the install of all needed plugins.
+
+
+The plugin modifies some skeletons (topics and articles) and some nuts bases.
+
+Once installed the necessary plugins, simply determine the language of the site in "Settings" -> "Mulilinguisme / Language management
+
+and that your site is multilingual',
+	'multilingue_nom' => 'Easy multilingual site',
+	'multilingue_slogan' => 'Create easily a multilingual site'
+);
+
+?>
