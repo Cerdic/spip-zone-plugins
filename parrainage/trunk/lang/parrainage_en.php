430acf7ae54ca1fb4365d728e17afda4693ea40d
benlarcher@gmail.com <benlarcher@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-01-23 02:40:51 +0000

    langues (parrainage)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@69217 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ parrainage/trunk/lang/parrainage_en.php
@@ -0,0 +1,66 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/parrainage?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'ajouter_filleul_confirmation' => 'The contact has been added, you can invite that person.',
+
+	// C
+	'configuration_delai_sans_nouvelles_label' => 'Period for the status "No news"',
+	'configuration_invitation_obligatoire_label' => 'Mandatory invitation',
+	'configuration_titre' => 'Sponsorship configuration',
+
+	// E
+	'erreur_aucun_filleul' => 'You have not selected any sponsored person.',
+	'erreur_invitation_invalide' => 'Your invitation does not match the email address provided.',
+	'erreur_invitation_obligatoire' => 'You need an invitation to register on the site.',
+
+	// F
+	'filleul_actions' => 'Actions',
+	'filleul_ajouter' => 'Add',
+	'filleul_email' => 'E-mail',
+	'filleul_nom' => 'Name',
+	'filleul_statut' => 'Status',
+
+	// I
+	'inscription_code_invitation_label' => 'Invitation code',
+	'invitation_message' => '@nom_parrain@ invites you to join the site @site@.',
+	'invitation_sujet' => '@nom@ invits you to join the website @site@',
+	'invitation_url' => 'Go to the following address to register :',
+
+	// P
+	'parrainage_inviter' => 'Send invitation',
+	'parrainage_message_aucun' => 'None of these people needs to be invited.',
+	'parrainage_message_aucun_1' => 'This person is already invited or registered.',
+	'parrainage_message_erreur' => 'At least one invitation was not sent correctly.',
+	'parrainage_message_label' => 'Your message',
+	'parrainage_message_ok_pluriel' => '@nombre@ invitations have been programmed, they are now being send.',
+	'parrainage_message_ok_singulier' => 'An invitation has been programmed, it is now being send.',
+	'parrainage_supprimer_filleul' => 'Delete this sponsored person',
+	'parrainage_supprimer_filleul_confirmation' => 'Are you sure you want to delete @nom@ from your contacts ?',
+	'plugin_nom' => 'Sponsorship',
+
+	// S
+	'statut_contact' => 'Contact',
+	'statut_contact_explication' => 'This person is in your address book. You can send an invitation to join the site.',
+	'statut_deja_inscrit' => 'Already registered',
+	'statut_deja_inscrit_explication' => 'This person is already registered on the site, but is not sponsored by you !',
+	'statut_en_cours' => 'Invitation being send',
+	'statut_en_cours_explication' => 'Your invitation message is being send.',
+	'statut_filleul' => 'Sponsored person',
+	'statut_filleul_depuis' => 'Sponsored person registered since @date@',
+	'statut_filleul_explication' => 'Thanks to you, this person registered on the site : you are his sponsor.',
+	'statut_invite' => 'Invited @duree@',
+	'statut_invite_explication' => 'You have already invited this person some time ago.',
+	'statut_sans_nouvelles' => 'No news since @date@',
+	'statut_sans_nouvelles_explication' => 'You have invited this person some time ago, but he never came.',
+	'statut_visite' => 'Visited the site',
+	'statut_visite_explication' => 'This person has already received your invitation to come to the site, but is not yet registered.'
+);
+
+?>
