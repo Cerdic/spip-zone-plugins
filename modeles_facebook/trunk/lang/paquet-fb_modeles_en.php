244d15c7976342aa7bd8722063feece44cde8c4d
benlarcher@gmail.com <benlarcher@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-01-23 02:40:47 +0000

    langues (paquet-fb_modeles)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@69215 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ modeles_facebook/trunk/lang/paquet-fb_modeles_en.php
@@ -0,0 +1,23 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-fb_modeles?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// F
+	'fb_modeles_description' => 'This plugin provides a set of models for SPIP, to integrate quickly and easily the social modules of Facebook:
+
+-*  {{send}} : the "Send" classic button (with comment)
+-*  {{like}} : classic button "I like" (with comment)
+-*  {{like box}} : list of comments from users who have "liked"
+-*  {{activity}} : list of recent activities of a page or profile
+-*  {{comments}} : list of comments and button to write one
+-*  {{live stream}} (experimental) chatbox',
+	'fb_modeles_nom' => 'Facebook Models',
+	'fb_modeles_slogan' => 'Models to include Facebook social modules'
+);
+
+?>
