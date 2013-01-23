abe1ee1da907404a6e0301d4f28bdb40c5181ed3
benlarcher@gmail.com <benlarcher@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-01-23 02:40:56 +0000

    langues (paquet-pubban)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@69219 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ pub_banner/trunk/lang/paquet-pubban_en.php
@@ -0,0 +1,31 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-pubban?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// P
+	'pubban_description' => 'Management and statistics for ads banners integrated SPIP skeletons ...
+
+{{USE}}
+
+- * This plugin adds the {{ # PUBBAN {banner name} }} to the skeleton, which is replaced by a frame encompassing the banner.
+- * A public page allows you to present the statistics of inserts and export them on \'.xls\' format.
+
+{{DEFAULT VALUES}}
+
+- * Standard banners are loaded by default during installation, there are the four most common types on the web. You can delete them and create new ones.
+- * Examples of inserts ({SPIP squirrel}) are also loaded for demonstration.
+
+{{HELP / DOC}}
+
+- * An explanation and a complete example is available in the documentation, you will also see the integration of banners in your skeletons. This documentation is available if the plugin is active on your site, on the public page "pubban_documentation."
+
+- * The statistics page of the plugin is a free adaptation of the script \'{{[VRG pub->http://vrgpub.frankdevelopper.com/]}}\', developed by {{Vincent Roseberry}} ({GPL licensed at the time}).',
+	'pubban_slogan' => ' Ads banners manager'
+);
+
+?>
