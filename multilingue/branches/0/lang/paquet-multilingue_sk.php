6e4578e4c8c0e078dc29b67bcd1b703e800ffeca
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-01-23 06:53:20 +0000

    langues
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@69221 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- multilingue/branches/0/lang/paquet-multilingue_sk.php
+++ multilingue/branches/0/lang/paquet-multilingue_sk.php
@@ -10,7 +10,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// M
 	'multilingue_description' => 'Nastaví viacjazyčnú stránku s jazykom pre daný sektor.
 	
-	Zásuvný modul si vynúti inštaláciu viacerých zásuvných modulov na
+	Zásuvný modul si vynúti inštaláciu viacerých zásuvných modulov
 	
 	- [preklad rubrík,->http://plugins.spip.net/tradrub.html]
 	- [jazyk, ktorý preferuje používateľ,->http://www.spip-contrib.net/Plugin-Langue-preferee-par-l-internaute]
@@ -23,7 +23,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	
 	Keď nainštalujete potrebné zásuvné moduly, jazyky stránky vyberte v menu  "Konfigurácia"->"Viacjazyčnosť/Riadenie jazykov"
 
-	a potom bude vaša stránka viacjazyčná', # MODIF
+	a potom bude vaša stránka viacjazyčná',
 	'multilingue_nom' => 'Jednoduchá viacjazyčná stránka',
 	'multilingue_slogan' => 'Vytvorte jednoduchú viacjazyčnú stránku'
 );
