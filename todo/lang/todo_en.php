259245ffd3dec1382c6179692482cc451166654f
benlarcher@gmail.com <benlarcher@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-01-24 02:41:52 +0000

    langues (todo)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@69268 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ todo/lang/todo_en.php
@@ -0,0 +1,29 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/todo?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'configurer_bando' => 'Things to do',
+	'configurer_titre' => 'Configure the list of things to do',
+
+	// L
+	'label_statut' => 'Status',
+	'label_titre' => 'Title',
+
+	// S
+	'statut_afaire' => 'To do',
+	'statut_encours' => 'Ongoing',
+	'statut_termine' => 'Done',
+
+	// T
+	'tri_cle' => 'Put back in order',
+	'tri_statut' => 'Sort by status',
+	'tri_titre' => 'Sort by title'
+);
+
+?>
