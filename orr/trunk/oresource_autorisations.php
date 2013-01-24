6cfcede7d46a0da39dcec72f61c342f362b06112
tofulm@gmail.com <tofulm@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-01-24 12:10:26 +0000

    creation des autorisations pour les statuts et mise en place de ces autorisations dans le squelette
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@69295 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- orr/trunk/oresource_autorisations.php
+++ orr/trunk/oresource_autorisations.php
@@ -7,6 +7,33 @@
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
+function recherche_autorisation($idressource,$statut_connecte,$autorisation){
+$result= false;
+$res = sql_select(
+	array(
+		"auto.id_orr_autorisation AS idauto",
+		"auto.orr_type_objet AS type",
+		"auto.orr_statut AS statut",
+		"auto.orr_autorisation_valeur AS valeur"),
+	array(
+		"spip_orr_autorisations AS auto",
+		"spip_orr_autorisations_liens AS lien"),
+	array(
+		"auto.id_orr_autorisation = lien.id_orr_autorisation",
+		"lien.objet='orr_ressource'",
+		"lien.id_objet=$idressource"));
+
+	while ($r=sql_fetch($res)) {
+		
+		if (  ($r['type'] == "statut") AND ($r['statut'] == "$statut_connecte") ) {
+			if (strpos($r['valeur'], $autorisation) !== false) {
+				$result = true;
+			}
+		}
+	}
+	return $result;
+}
+
 // declaration vide pour ce pipeline.
 function oresource_autoriser(){}
 
@@ -50,22 +77,34 @@ function autoriser_orrressource_supprimer_dist($faire, $type, $id, $qui, $opt) {
 
 // creer
 function autoriser_orrreservation_creer_dist($faire, $type, $id, $qui, $opt) {
-	return in_array($qui['statut'], array('0minirezo'));
+	$statut=$qui['statut'];
+	$autorisation="C";
+	$resultat=recherche_autorisation($id,$statut,$autorisation);
+	return $resultat;
 }
 
 // voir les fiches completes
 function autoriser_orrreservation_voir_dist($faire, $type, $id, $qui, $opt) {
-	return true;
+	$statut=$qui['statut'];
+	$autorisation="V";
+	$resultat=recherche_autorisation($id,$statut,$autorisation);
+	return $resultat;
 }
 
 // modifier
 function autoriser_orrreservation_modifier_dist($faire, $type, $id, $qui, $opt) {
-	return in_array($qui['statut'], array('0minirezo'));
+	$statut=$qui['statut'];
+	$autorisation="M";
+	$resultat=recherche_autorisation($id,$statut,$autorisation);
+	return $resultat;
 }
 
 // supprimer
 function autoriser_orrreservation_supprimer_dist($faire, $type, $id, $qui, $opt) {
-	return $qui['statut'] == '0minirezo' AND !$qui['restreint'];
+	$statut=$qui['statut'];
+	$autorisation="S";
+	$resultat=recherche_autorisation($id,$statut,$autorisation);
+	return $resultat;
 }
 
 
