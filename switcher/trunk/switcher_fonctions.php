34ab91b2640113d5f0ad22934e3587718c888ee9
cam.lafit@azerttyu.net <cam.lafit@azerttyu.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-01-24 16:42:06 +0000

    La configuration n'est pas toujours présente au début
    * Si aucune liste d'auteurs autorisés n'est fourni, il faut considérer un tableau vide par défaut.
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@69306 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- switcher/trunk/switcher_fonctions.php
+++ switcher/trunk/switcher_fonctions.php
@@ -15,7 +15,7 @@ function switcher_affichage_final($texte = ""){
 
 	if ($html) {
 
-	    $auteur_autorise = in_array($GLOBALS['visiteur_session']['id_auteur'],lire_config('switcher/auteurs_autorises')) ? true : false;
+	    $auteur_autorise = in_array($GLOBALS['visiteur_session']['id_auteur'],lire_config('switcher/auteurs_autorises',array())) ? true : false;
 #	    var_dump(lire_config('switcher/auteurs_autorises'),$auteur_autorise,$GLOBALS['visiteur_session']['id_auteur']);
 	
 		if (SWITCHER_AFFICHER || $auteur_autorise) {
