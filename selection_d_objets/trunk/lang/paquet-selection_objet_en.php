afcc73f7e5887f270e213d7d81b8cb18c625e6b2
benlarcher@gmail.com <benlarcher@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-01-24 02:41:26 +0000

    langues (paquet-selection_objet)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@69255 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ selection_d_objets/trunk/lang/paquet-selection_objet_en.php
@@ -0,0 +1,17 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-selection_objet?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// S
+	'selection_objet_description' => 'Plugin inspired by the plugin  article selection.
+Selects spip objects, store them and customize their display depending on the selected section',
+	'selection_objet_nom' => 'Objects selection',
+	'selection_objet_slogan' => 'List your spip objects'
+);
+
+?>
