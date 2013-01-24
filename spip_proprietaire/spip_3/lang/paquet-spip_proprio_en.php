8c70065dc25129f8c463743fbd351218d871b3a1
benlarcher@gmail.com <benlarcher@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-01-24 02:41:31 +0000

    langues (paquet-spip_proprio)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@69258 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ spip_proprietaire/spip_3/lang/paquet-spip_proprio_en.php
@@ -0,0 +1,25 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-spip_proprio?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// S
+	'spip_proprio_description' => 'This plugin offers automatic generation of {{Legal statements}}, {{Terms of Use}} and {{information Copyright}} characteristic to your SPIP site.
+
+- * The plugin configuration page, accessible from the "Configuration" tab of the SPIP private area, allow you to enter different information about the publisher of the site, its creator and host.
+- * <u>Legal texts are based by default on the legislation in France ({on the 1 January 2010}) </ u> and are largely {{legal minimum for any website}}. They can be changed in a specific language file to customize.
+- * A set of models are available:
+- Tools to enhance your SPIP site: vCard, cards, blocks of information ...
+- Tools to organize your SPIP site: two new models {header} and {pied}, based on the distribution of SPIP skeletons (\'inc-header.html\' and \'inc-pied.html\'), with copyright information and access to legal statements page.
+
+Some icons plugin in the public template, comes from the set "{{[bwpx.icns->http://paularmstrongdesigns.com/portfolio/bwpx-icns/]}}" from {{Paul Armstrong}}, distributed under {license Creative Commons Attribution-Share Alike 3.0} (U.S.).
+
+Icons from the private area of the project {{[GNOME-> http://www.gnome.org/]}}.',
+	'spip_proprio_slogan' => 'Legal statements, Terms of Use and automatic Copyright'
+);
+
+?>
