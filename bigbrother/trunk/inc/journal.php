c75ee41e1a7786bc6da8c94e91e89f0d81f19cba
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-01-24 09:48:03 +0000

    PHPDoc + formatage du code + on renvoit soit un booléen soit l'id_journal créé en base + on ne teste pas la configuration dans cette fonction là car dans le formuloaire de config (et donc dans la meta) il est impossible de prévoir toutes les actions et donc si on en met une pas prévue ça n'aurait pas enregistré en base + la grosse modif : on garde en mémoire dans un même hit PHP ce qui a été journalisé (mêmes action+objet+id_objet) et si dans le même hit ya la même suite, alors on ne fait que modifier la ligne déjà insérée en fusionnant les infos supplémentaires
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@69287 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- bigbrother/trunk/inc/journal.php
+++ bigbrother/trunk/inc/journal.php
@@ -5,62 +5,109 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 
 /*
  * Consigner une phrase dans le journal de bord du site
- * Cette API travaille a minima, mais un plugin pourra stocker
- * ces journaux en base et fournir des outils d'affichage, de selection etc
  *
- * @param string $journal
- * @param array $opt
+ * Cette fonction surcharge celle de SPIP, afin d'enregistrer les informations en base
+ * Il est toutefois possible de surcharger ce comportement par défaut pour une action précise
+ * en créant une fonction journal_FAIRE_dist($opt).
+ *
+ * @param string $phrase Une phrase humaine décrivant ce qu'on journalise (pour l'écriture en log)
+ * @param array $opt Un tableau listant les informations plus détaillées, qui seront enregistrées en base
+ * @return bool|int Retourne l'identifiant de journal inséré en base, sinon false
  */
 function inc_journal_dist($phrase, $opt = array()) {
-	if (_IS_BOT)
-		return;
-
-	if (!strlen($phrase))
-		return;
+	// Dans un hit PHP, on va garder en mémoire ce qui a déjà été fait
+	static $action = array();
+	
+	// Si c'est un robot ou s'il n'y a aucune phrase, on ne fait jamais rien
+	if (_IS_BOT or !strlen($phrase)){
+		return false;
+	}
+	
+	// On ajoute les précisions à la fin de la phrase de log
 	if ($opt)
 		$phrase .= " :: ".str_replace("\n", ' ', join(', ',$opt));
-
-	/**
-	 * Journalise t on en base ou pas
-	 * @var unknown_type
-	 */
-	$config = unserialize($GLOBALS['meta']['bigbrother']);
-	if(isset($config[$opt['faire']]) && ($config[$opt['faire']] == 'oui')){
-		if((lire_config('bigbrother/enregistrer_ip') == 'oui') && !$opt['infos']['ip']){
-			$opt['infos']['ip'] = $GLOBALS['ip'];
-		}
-		if($f = charger_fonction($opt['faire'],'journal',true)){
-			$f($opt);
-		}else{
-			$champs = array(
-					'id_auteur' => $opt['qui'],
-					'action' => $opt['faire'],
-					'id_objet' => $opt['id'],
-					'objet' => $opt['quoi'],
-					'infos' => $opt['infos'],
-					'date' => $opt['date'] ? $opt['date'] : date('Y-m-d H:i:s', time())
-				);
-			// Envoyer aux plugins
-			$champs = pipeline('pre_edition',
-				array(
-					'args' => array(
-						'table' => 'spip_journal',
-						'action'=>'inserer'
-					),
-					'data' => $champs
-				)
-			);
+	
+	// Garde-t-on aussi l'IP en mémoire ?
+	if((lire_config('bigbrother/enregistrer_ip') == 'oui') && !$opt['infos']['ip']){
+		$opt['infos']['ip'] = $GLOBALS['ip'];
+	}
+	
+	// On peut surcharger la fonction pour une action précise
+	if($f = charger_fonction($opt['faire'],'journal',true)){
+		$ok = $f($opt);
+	}
+	// Sinon par défaut on enregistre en base de données
+	else{
+		$champs = array(
+			'id_auteur' => $opt['qui'],
+			'action' => $opt['faire'],
+			'id_objet' => $opt['id'],
+			'objet' => $opt['quoi'],
+			'infos' => $opt['infos'],
+			'date' => $opt['date'] ? $opt['date'] : date('Y-m-d H:i:s', time()),
+		);
+		// Envoyer aux plugins
+		$champs = pipeline('pre_edition',
+			array(
+				'args' => array(
+					'table' => 'spip_journal',
+					'action'=>'inserer'
+				),
+				'data' => $champs
+			)
+		);
+		
+		// Maintenant que l'on sait exactement ce qui va être fait,
+		// on enregistre en base que si ça n'a pas déjà été fait pour les mêmes arguments
+		// et si ça a déjà été fait, on met plutôt à jour la ligne d'avant en fusionnant
+		// le tableau des informations supplémentaires
+		
+		// Si on a pas déjà journalisé cette action dans CE hit PHP
+		// ou si ça s'est mal passé (pas d'id_journal correct)
+		// alors on réinsère une ligne de journal
+		if (
+			!isset($action[$champ['action']][$champ['objet']][$champ['id']]['id_journal'])
+			or !intval($action[$champ['action']][$champ['objet']][$champ['id']]['id_journal'])
+		){
+			$infos = $champs['infos'];
+			// On sérialise les infos supplémentaires pour la base
 			if(is_array($champs['infos'])){
 				$champs['infos'] = serialize($champs['infos']);
 			}
-			sql_insertq(
+			// On insère
+			$ok = sql_insertq(
 				'spip_journal',
 				$champs
 			);
+			// On garde en mémoire ce qui vient d'être journalisé dans ce hit PHP
+			$action[$champ['action']][$champ['objet']][$champ['id']] = array('id_journal'=>$ok,'infos'=>$infos);
+		}
+		// Sinon on met à jour, et on fusionne les infos
+		else{
+			$id_journal = $action[$champ['action']][$champ['objet']][$champ['id']]['id_journal'];
+			$infos = $action[$champ['action']][$champ['objet']][$champ['id']]['infos'];
+			if (is_array($champs['infos']) and is_array($infos)){
+				// On fusionne les infos supplémentaires
+				$infos = array_merge_recursive($infos, $champs['infos']);
+				// On sérialise pour la base
+				$champs['infos'] = serialize($infos);
+				// On met à jour la ligne en base
+				sql_updateq(
+					'spip_journal',
+					$champs,
+					'id_journal = '.intval($id_journal)
+				);
+				// On met à jour la variable statique
+				$action[$champ['action']][$champ['objet']][$champ['id']] = array('id_journal'=>$$id_journal,'infos'=>$infos);
+			}
 		}
 	}
+	
+	// Dans tous les cas, on log dans un fichier
 	include_spip('inc/filtres');
 	spip_log(filtrer_entites($phrase), 'journal');
+	
+	return $ok;
 }
 
 ?>
