17751359fd3e14f7ccfd0f64f4898c270fa5adee
tofulm@gmail.com <tofulm@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-01-24 11:35:13 +0000

    ajout des champs de langue pour les autorisations
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@69292 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ orr/trunk/lang/orr_autorisation_fr.php
@@ -0,0 +1,30 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+    'ajouter_lien_orr_autorisation' => 'Ajouter le lien de cette autorisation',
+
+    //I
+    'info_aucun_orr_autorisation' => 'Aucune autorisation',
+    'info_1_orr_autorisation' => '1 autorisation',
+    'info_nb_orr_autorisations' => '@nb@ autorisations',
+
+    //L
+    'label_orr_autorisation_nom'  => 'Nom de l\'autorisation',
+
+    //R
+    'retirer_lien_orr_autorisation' => 'Supprimer le lien de cette autorisation',
+    'retirer_tous_liens_orr_autorisations' => 'Supprimer tous les liens des autorisations',
+
+    //T
+    'texte_ajouter_orr_autorisation' => 'Ajouter une autorisation',
+
+    //V
+    'valeur_autorisation' => 'Valeur',
+
+);
