fb72fe60cc24f6833975c73677e84a7f40a85ade
benlarcher@gmail.com <benlarcher@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-01-24 02:41:22 +0000

    langues (paquet-redirhttps)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@69253 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ redirhttps/lang/paquet-redirhttps_en.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-redirhttps?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// R
+	'redirhttps_description' => 'Redirect to HTTPS when the content requires it (private area, login, public pages with user logged in)',
+	'redirhttps_nom' => 'HTTPS redirections',
+	'redirhttps_slogan' => 'Secure access to certain pages of the site'
+);
+
+?>
