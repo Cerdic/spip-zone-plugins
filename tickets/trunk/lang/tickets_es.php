517dde07b88eb59d24c8bc6284244d83b4da0b9e
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-01-24 02:41:46 +0000

    langues (tickets)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@69265 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ tickets/trunk/lang/tickets_es.php
@@ -0,0 +1,229 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/tickets?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'afficher_tickets' => 'Mostrar los tickets',
+	'assignation_attribuee_a' => 'El ticket se ha asignado a @nom@.',
+	'assignation_mail_titre' => 'Cambio de asignación de ticket',
+	'assignation_modifiee' => 'Asignación actualizada',
+	'assignation_non_modifiee' => 'Asignación no actualizada',
+	'assignation_supprimee' => 'La asignación de este ticket ha sido eliminada.',
+	'assigne_a' => 'Asignado a:',
+	'assignes_a' => 'Asignados a',
+	'autres_tickets_ouverts' => 'Otros tickets abiertos',
+
+	// C
+	'cfg_bouton_radio_desactiver_forum_public' => 'Desactivar el uso de foros públicos.',
+	'cfg_bouton_tickets' => 'Tickets',
+	'cfg_descr_autorisations' => 'Esta página de configuración permite configurar las autorizaciones para escribir, modificar, asignar y comentar los tickets.',
+	'cfg_descr_general' => 'Esta página de configuración permite elegir los valores de los campos opcionales, moderar los comentarios así como otras configuraciones.',
+	'cfg_explication_cacher_moteurs' => 'Esconde los tickets de los motores de búsqueda pidiéndoles que no indexen los contenidos cuando se muestren en el espacio público.',
+	'cfg_explication_composants' => 'Separe los componentes por el carácter ":".',
+	'cfg_explication_formats_documents_ticket' => 'Separe los formatos por una coma',
+	'cfg_explication_jalons' => 'Separe los hitos por el carácter ":".',
+	'cfg_explication_projets' => 'Separe los proyectos por el carácter ":".',
+	'cfg_explication_readonly' => 'Esta parte de la configuración ya está definida en otra parte. ',
+	'cfg_explication_versions' => 'Separe las versiones por el carácter ":".',
+	'cfg_form_tickets_autorisations' => 'Autorizaciones',
+	'cfg_form_tickets_general' => 'Configuraciones generales',
+	'cfg_inf_type_autorisation' => 'Si discrimina por estatus o por autor, se le pedirá seleccionar estatus o autor. ',
+	'cfg_lbl_autorisation_auteurs' => 'Autorizar por lista de autores',
+	'cfg_lbl_autorisation_statuts' => 'Autorizar por estatus de autor',
+	'cfg_lbl_autorisation_webmestre' => 'Autorizar solamente a los webmasters',
+	'cfg_lbl_cacher_moteurs' => 'Esconder los motores de búsqueda',
+	'cfg_lbl_case_joindre_fichiers' => 'Autorizar a adjuntar uno o más archivos en los tickets',
+	'cfg_lbl_case_notification_publique' => 'Ser notificado en el espacio público antes que en el espacio privado',
+	'cfg_lbl_case_selecteur_navigateur' => 'Disponer de un selector de navegador en los formularios de edición de ticket',
+	'cfg_lbl_composants' => 'Componentes',
+	'cfg_lbl_desactiver_public' => 'Desactivar el acceso público',
+	'cfg_lbl_formats_documents_ticket' => 'Formatos de documentos aceptados',
+	'cfg_lbl_forums_publics' => 'Comentarios en los tickets',
+	'cfg_lbl_jalons' => 'Hitos',
+	'cfg_lbl_joindre_fichiers' => 'Adjuntar uno o varios archivos',
+	'cfg_lbl_liste_auteurs' => 'Autores del sitio',
+	'cfg_lbl_notification_publique' => 'Notificación pública',
+	'cfg_lbl_projets' => 'Proyectos',
+	'cfg_lbl_selecteur_navigateur' => 'Selector del navegador',
+	'cfg_lbl_statuts_auteurs' => 'Posibles estatus',
+	'cfg_lbl_type_autorisation' => 'Método de autorización',
+	'cfg_lbl_versions' => 'Versiones',
+	'cfg_lgd_autorisation_assigner' => 'Asignar los tickets',
+	'cfg_lgd_autorisation_commenter' => 'Comentar los tickets',
+	'cfg_lgd_autorisation_ecrire' => 'Escribir los tickets',
+	'cfg_lgd_autorisation_modifier' => 'Modificar los tickets',
+	'cfg_lgd_champs_optionnels' => 'Campos opcionales',
+	'cfg_lgd_champs_options_autres' => 'Otras opciones',
+	'cfg_lgd_notifs_forums' => 'Foros y notificaciones',
+	'cfg_titre_tickets' => 'Tickets - Configuración del plugin',
+	'champ_assigner' => 'Asignar a:',
+	'champ_composant' => 'Componente:',
+	'champ_createur' => 'Creado por:',
+	'champ_date' => 'Fecha:',
+	'champ_date_debut' => 'A partir de:',
+	'champ_date_fin' => 'Hasta:',
+	'champ_date_modif' => 'Modificado el',
+	'champ_description' => 'Descripción del ticket',
+	'champ_exemple' => 'Ejemplo:',
+	'champ_fichier' => 'Adjuntar un archivo',
+	'champ_id' => 'Número',
+	'champ_id_assigne' => 'Asignado a:',
+	'champ_id_auteur' => 'Autor:',
+	'champ_importance' => 'Importancia:',
+	'champ_jalon' => 'Hito:',
+	'champ_maj' => 'Actualizado:',
+	'champ_maj_long' => 'Fecha de actualización',
+	'champ_navigateur' => 'Navegador:',
+	'champ_nouveau_commentaire' => 'Nuevo comentario',
+	'champ_projet' => 'Proyecto:',
+	'champ_recherche' => 'Búsqueda:',
+	'champ_severite' => 'Severidad:',
+	'champ_statut' => 'Estatus:',
+	'champ_sticked' => 'Anclar:',
+	'champ_texte' => 'Texto',
+	'champ_titre' => 'Resumen',
+	'champ_titre_ticket' => 'Título del ticket',
+	'champ_type' => 'Tipo:',
+	'champ_url_exemple' => 'URL del ejemplo',
+	'champ_version' => 'Versión:',
+	'changement_statut_mail' => 'El estado de este ticket ha sido modificado de "@ancien@" a "@nouveau@".',
+	'classement_assigne' => 'Tickets por asignación',
+	'classement_asuivre' => 'Tickets a seguir',
+	'classement_composant' => 'Tickets por componente',
+	'classement_jalon' => 'Tickets por hito',
+	'classement_projet' => 'Tickets por proyecto',
+	'classement_termine' => 'Tickets terminados',
+	'classement_type' => 'Tickets por tipo',
+	'classement_version' => 'Tickets por versión',
+	'commentaire' => 'comentario',
+	'commentaire_aucun' => 'Ningún comentario',
+	'commentaires' => 'comentarios',
+	'commenter_ticket' => 'Comentar este ticket',
+	'creer_ticket' => 'Crear un ticket',
+
+	// D
+	'date_creation_auteur' => 'Ticket creado el <strong>@date@</strong> por <strong>@nom@</strong>',
+
+	// E
+	'erreur_date_saisie' => 'Esta fecha no es válida',
+	'erreur_date_saisie_superieure' => 'La fecha máxima ha de ser superior a la fecha mínima',
+	'erreur_texte_longueur_mini' => 'La longitud mínima del texto es de @nb@ caracteres.',
+	'erreur_verifier_formulaire' => 'Confirme su formulario',
+	'explication_champ_sticked' => 'Los tickets anclados se muestran siempre primero, cualquiera que sea su estatus.',
+	'explication_description_ticket' => 'Describa lo más concretamente posible la necesidad o el problema.
+Indique en particular si se ha producido sistemáticamente u ocasionalmente.
+Si se trata de un problema de visualización, precise con qué navegador lo experimenta. ',
+	'explication_description_ticket_ss_nav' => 'Describa lo más concretamente posible la necesidad o el problema. 
+Indique en particular si se ha producido sistemáticamente u ocasionalmente.',
+	'explication_fichier' => 'Adjuntar un archivo a su ticket.',
+	'explication_redaction' => 'Cuando haya finalizado la redacción de su ticket, seleccione el estatus «abierto y discutido».',
+	'explication_url_exemple' => 'Indiquez ici l’URL d’une page concernée par ce ticket.', # NEW
+
+	// I
+	'icone_modifier_ticket' => 'Modificar este ticket',
+	'icone_retour_ticket' => 'Volver al ticket',
+	'info_commentaire' => 'Comentario #@id@:',
+	'info_document_ajoute' => 'Adjuntado:',
+	'info_liste_tickets' => 'Tickets',
+	'info_numero_ticket' => 'TICKET NÚMERO:',
+	'info_ticket_1' => '1 ticket',
+	'info_ticket_aucun' => 'Ningún ticket',
+	'info_ticket_nb' => '@nb@ tickets',
+	'info_tickets' => 'Tickets',
+	'info_tickets_ouvert' => 'abiertos y discutidos',
+	'info_tickets_redac' => 'redacción en curso',
+
+	// L
+	'label_paginer_par' => 'Paginar por:',
+	'lien_filtrer' => 'Filtrar los tickets',
+	'lien_supprimer_filtres' => 'Quitar todos los filtros',
+
+	// M
+	'mail_texte_message_auto' => 'Éste es un mensaje automático: no lo responda. ',
+	'message_aucun_ticket_recherche' => 'Ningún ticket corresponde a su búsqueda',
+	'message_automatique' => 'Éste es un mensaje automático: no lo responda. ',
+	'message_page_publique_indisponible' => 'Esta página no está disponible. Confirme que ZPIP está activado y que su configuración del plugin Tickets autoriza el acceso público.',
+	'message_zpip_inactif' => 'Esta opción está desactivada porque necesita el plugin ZPIP.',
+
+	// N
+	'no_assignation' => 'Nadie',
+	'non_assignes' => 'No asignados',
+	'nouveau_commentaire_mail' => 'Nuevo comentario en el ticket',
+	'nouveau_ticket' => 'Nuevo ticket',
+
+	// O
+	'option_navigateur_autre' => 'Otro',
+	'option_navigateur_tous' => 'Todos los navegadores',
+
+	// P
+	'page_titre' => 'Tickets, sistema de seguimiento de bugs',
+
+	// R
+	'revenir_gestion' => 'Volver a la gestión de los tickets',
+
+	// S
+	'severite_bloquant' => 'Bloqueo',
+	'severite_important' => 'Importante',
+	'severite_normal' => 'Normal',
+	'severite_peu_important' => 'Poco importante',
+	'sinscrire' => 'Inscribirse',
+	'statut_ferme' => 'Cerrado',
+	'statut_ferme_long' => 'Todos los tickets cerrados',
+	'statut_inchange' => 'El estatus no ha sido modificado.',
+	'statut_mis_a_jour' => 'Estatus actualizado',
+	'statut_ouvert' => 'Abierto y discutido',
+	'statut_poubelle' => 'A la basura',
+	'statut_redac' => 'Redacción en curso',
+	'statut_resolu' => 'Resuelto',
+	'statut_resolu_long' => 'Todos los tickets resueltos',
+	'suivre_tickets_assignes_a' => 'Tickets asignados a @nom@',
+	'suivre_tickets_comments' => 'Seguimiento de los comentarios de los tickets',
+	'suivre_tickets_comments_rss' => 'Seguir estos comentarios por RSS',
+	'suivre_tickets_de' => 'Tickets de @nom@',
+	'suivre_tickets_id' => 'Seguimiento del ticket #@id@: @titre@',
+	'suivre_tickets_rss' => 'Seguir estos tickets por RSS',
+	'suivre_tickets_rss_unique' => 'Seguir este ticket por RSS',
+	'suivre_tickets_statut' => 'Tickets que tengan el estatus o los estatus:',
+	'suivre_tickets_tous' => 'Todos los tickets',
+	'syndiquer_ticket' => 'Syndiquer le ticket :', # NEW
+	'syndiquer_tickets' => 'Syndiquer les tickets du site', # NEW
+
+	// T
+	'texte_ticket_statut' => 'Estatus del ticket:',
+	'ticket' => 'Ticket',
+	'ticket_enregistre' => 'Ticket registrado',
+	'tickets' => 'Tickets',
+	'tickets_autorisations' => 'Autorizaciones',
+	'tickets_derniers_commentaires' => 'Últimos comentarios',
+	'tickets_en_cours_auteur' => 'Tickets de @nom@ en proceso',
+	'tickets_general' => 'Configuraciones generales',
+	'tickets_sticked' => 'Tickets anclados',
+	'tickets_sur_inscription' => 'La redacción de tickets o de comentarios sólo es posible para las personas identificadas.',
+	'tickets_sur_inscription_droits' => 'Les droits dont vous disposez sont insuffisants.', # NEW
+	'tickets_traites' => 'Todos los tickets procesados',
+	'tickets_tries' => 'Tickets correspondientes a sus criterios',
+	'titre' => 'Tickets, seguimiento de bugs',
+	'titre_identification' => 'Identificación',
+	'titre_liste' => 'Lista de los tickets',
+	'tous_tickets_en_redaction' => 'Todos los tickets en redacción',
+	'tous_tickets_ouverts' => 'Todos los tickets abiertos',
+	'tous_vos_tickets' => 'Todos sus tickets',
+	'type_amelioration' => 'Mejora',
+	'type_amelioration_long' => 'Tickets que piden una mejora',
+	'type_probleme' => 'Problema',
+	'type_probleme_long' => 'Problemas a resolver',
+	'type_tache' => 'Tâche', # NEW
+	'type_tache_long' => 'Les tâches à accomplir', # NEW
+
+	// V
+	'vos_tickets_assignes' => 'Tickets que le han sido asignados',
+	'vos_tickets_assignes_auteur' => 'Les tickets de @nom@ qui vous sont assignés', # NEW
+	'vos_tickets_en_cours' => 'Sus tickets en curso de redacción'
+);
+
+?>
