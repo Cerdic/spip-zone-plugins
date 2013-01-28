c91365fc1a8d28dbeae04c7048a75b7ea6198cb4
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-01-28 02:41:47 +0000

    langues (paquet-mesfavoris)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@69466 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ mesfavoris/lang/paquet-mesfavoris_es.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-mesfavoris?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// M
+	'mesfavoris_description' => '(De)Selecciona un objeto para gestionar los objetos (artículos, secciones...) favoritos para un visitante autentificado',
+	'mesfavoris_slogan' => 'Selección de favoritos'
+);
+
+?>
