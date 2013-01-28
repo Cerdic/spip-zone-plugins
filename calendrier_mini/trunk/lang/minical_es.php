80780c6f1f79c87f3c152d400fc644b270390bb6
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-01-28 02:41:25 +0000

    langues (minical)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@69462 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- calendrier_mini/trunk/lang/minical_es.php
+++ calendrier_mini/trunk/lang/minical_es.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// extrait automatiquement de http://trad.spip.org/tradlang_module/minical?lang_cible=es
+// extrait automatiquement de http://trad.spip.net/tradlang_module/minical?lang_cible=es
 // ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
@@ -8,20 +8,26 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
-	'aucune_date' => 'Rien pour ce mois', # NEW
+	'aucune_date' => 'Nada para este mes',
 
-	// D
-	'date_jour_abbr_1' => 'dom',
-	'date_jour_abbr_2' => 'lun',
-	'date_jour_abbr_3' => 'mar',
-	'date_jour_abbr_4' => 'mie',
-	'date_jour_abbr_5' => 'jue',
-	'date_jour_abbr_6' => 'vie',
-	'date_jour_abbr_7' => 'sab',
+	// C
+	'config_titre_calendriermini' => 'Mini-Calendario',
+
+	// L
+	'label_affichage_hors_mois' => 'Días mostrados',
+	'label_affichage_hors_mois_0' => 'Ocultar los días del mes precedente y siguiente',
+	'label_affichage_hors_mois_1' => 'Mostrar los días del mes precedente y siguiente',
+	'label_changement_rapide' => 'Navegación',
+	'label_changement_rapide_0' => 'Desactivar la selección rápida del mes o del año',
+	'label_changement_rapide_1' => 'Activar la selección rápida del mes o del año',
+	'label_format_jour' => 'Formato de los días',
+	'label_format_jour_abbr' => 'Corto',
+	'label_format_jour_initiale' => 'Inicial',
+	'label_jour1' => 'Primer día de la semana',
 
 	// M
-	'mois_precedent' => 'Mois précédent', # NEW
-	'mois_suivant' => 'Mois suivant' # NEW
+	'mois_precedent' => 'Mes precedente',
+	'mois_suivant' => 'Mes siguiente'
 );
 
 ?>
