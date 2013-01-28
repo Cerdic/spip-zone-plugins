dd0af12f8388faade18841fb8156effb87cc58bb
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-01-28 02:41:57 +0000

    langues (paquet-notation)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@69468 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ notation/trunk/lang/paquet-notation_es.php
@@ -0,0 +1,18 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-notation?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// N
+	'notation_description' => 'Definición de la etiqueta <code>#FORMULAIRE_NOTATION</code> permitiendo introducir y mostrar la nota de un elemento del bucle.
+_ Definición de los modelos <code><autxx|notation|id_article=yy></code> que da la nota del autor <i>xx</i> para el artículo <i>yy</i>
+et <code><notation|topten></code> que da los 10 mejores artículos.',
+	'notation_nom' => 'Notación',
+	'notation_slogan' => 'Atribuir una nota a los elementos de SPIP'
+);
+
+?>
