899b8a7d2b8296a711139124a13a4f53c8cbd070
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-01-28 02:42:01 +0000

    langues (paquet-nuage)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@69469 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ nuage/trunk/lang/paquet-nuage_es.php
@@ -0,0 +1,20 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-nuage?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// N
+	'nuage_description' => 'Este plugin permite mostrar las palabras clave sin orden, variando el tamaño de fuente en función del número de artículos asociados a la palabra clave. En otros términos, ¡SPIP permite crear «tags»!
+
+Varios modelos disponibles:
+-* artículo_nube: muestra a las palabras clave del artículo variando el tamaño en función de la frecuencia de las palabras en los artículos del sitio
+-* sección_nube: muestra las palabras clave de los artículos de la rama, variando el tamaño en función de la frecuencia de las palabras en los artículos de esta rama
+-* nube_popularidad: muestra las palabras clave del sitio (o de un grupo), variando el tamaño en función de su popularidad relativa (suma de la popularidad de los artículos disponiendo de esta palabra)',
+	'nuage_slogan' => 'Mostrar las palabras clave variando el tamaño de la fuente'
+);
+
+?>
