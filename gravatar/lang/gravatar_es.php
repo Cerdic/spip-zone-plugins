7be07154b4cb25a7acd9357457b342111f923c4f
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-01-28 02:41:38 +0000

    langues (gravatar)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@69465 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ gravatar/lang/gravatar_es.php
@@ -0,0 +1,35 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/gravatar?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cfg_descr_gravatar' => 'Configurar el informe de los gravatars',
+	'cfg_titre_gravatar' => 'Gravatars',
+
+	// E
+	'explication_image_defaut' => 'Para quienes no tengan avatar, utilizar:',
+
+	// G
+	'gravatar_info' => 'Para mostrar su avatar aquí, guárdelo en <a href="http://www.gravatar.com/" rel="external nofollow" class="spip_out">gravatar.com</a> (gratis y fácil).',
+	'gravatar_info_forum' => 'Para mostrar su avatar con su mensaje, guárdelo en <a href="http://www.gravatar.com/" rel="external nofollow" class="spip_out">gravatar.com</a> (gratuit et indolore) y no olvide indicar su dirección de correo electrónico aquí.',
+
+	// L
+	'label_image_defaut' => 'Imagen por defecto',
+	'label_image_defaut_404' => 'ninguna imagen',
+	'label_image_defaut_gravatar' => 'logo Gravatar',
+	'label_image_defaut_identicon' => '<i>Identicon</i> (generado tras la dirección de correo electrónico)',
+	'label_image_defaut_monsterid' => '<i>MonsterID</i> (generado tras la dirección de correo electrónico)',
+	'label_image_defaut_png' => '<tt>images/gravatar.png</tt>',
+	'label_image_defaut_wavatar' => '<i>Wavatar</i> (generado tras la dirección de correo electrónico)',
+	'label_taille' => 'Tamaño de los gravatars',
+
+	// T
+	'titre_gravatar_auteur' => 'Gravatar'
+);
+
+?>
