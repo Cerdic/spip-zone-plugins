c7f8ca229819aecc607da29a32534a7d4d2185ce
erational@erational.org <erational@erational.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-01-30 04:32:29 +0000

    flickr en version SPIP 3
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@69516 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ flickr_hasard/trunk/lang/flickr_rand_fr.php
@@ -0,0 +1,21 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+  // E
+  'explication_plage' => '  Indiquer un chiffre entre 1 et 4000 (valeur conseillée: 200, maximum autorisé par Flickr: 4000). 
+                             Plus le chiffre est grand, plus vos images seront variées mais moins elles seront récentes',
+
+  // L
+  'label_plage' => 'Plage de recherche de vos images aléatoires',
+  
+
+  
+);
+
+?>
