ee8c1fe0c72ee042d704dc478f8edcd090d83d13
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-02-01 22:46:30 +0000

    Embed code : Générateur de code d'embed de document (Notamment compatible avec mediaspip_player)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@69606 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ embed_code/trunk/lang/embed_code_fr.php
@@ -0,0 +1,20 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans http://svn.aires-de-confluxence.info/svn/plugins_spip/embed_code
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	// I
+	'info_embed_code' => 'Intégrer ce média',
+	
+	// L
+	'label_code_embed' => 'Code à copier',
+	'label_hauteur' => 'Hauteur',
+	'label_largeur' => 'Largeur',
+	'lien_preview_embed' => 'Prévisualiser le rendu',
+	
+	// M
+	'message_document_inexistant' => 'Ce document n\'est pas disponible sur le site <a href="@url_site@">@nom_site@</a>.'
+);
+
+?>
---
+++ embed_code/trunk/lang/paquet-embed_code_fr.php
@@ -0,0 +1,14 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// E
+	'embed_code_description' => 'Génèrer des code d\'inclusion distant de documents pour utilisation sur d\'autres sites.',
+	'embed_code_nom' => 'Embed code',
+	'embed_code_slogan' => 'Génération de code embed pour documents',
+);
+?>
