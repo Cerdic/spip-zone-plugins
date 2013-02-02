54f9d2c6e1cd05e3aba0b8c13602ac45eb70f575
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-02-02 02:41:32 +0000

    langues (paquet-diogene_mots)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@69620 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- diogene/diogene_complements/diogene_mots/trunk/lang/paquet-diogene_mots_fr.php
+++ diogene/diogene_complements/diogene_mots/trunk/lang/paquet-diogene_mots_fr.php
@@ -1,20 +1,14 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-diogene_mots
-// Langue: fr
-// Date: 15-06-2012 13:20:08
-// Items: 3
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/diogene/diogene_complements/diogene_mots/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// D
+	// D
 	'diogene_mots_description' => 'Complément mots-clés pour "Diogene"',
 	'diogene_mots_nom' => 'Diogene - Mots',
-	'diogene_mots_slogan' => 'Complément mots-clés pour "Diogene"',
+	'diogene_mots_slogan' => 'Complément mots-clés pour "Diogene"'
 );
+
 ?>
