32d6c80212e0d78df5c0910f5bf5fb54c933b400
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-02-02 02:41:30 +0000

    langues (paquet-diogene_licence)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@69618 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- diogene/diogene_complements/diogene_licence/trunk/lang/paquet-diogene_licence_en.php
+++ diogene/diogene_complements/diogene_licence/trunk/lang/paquet-diogene_licence_en.php
@@ -1,20 +1,16 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-diogene_licence
-// Langue: en
-// Date: 14-06-2012 17:25:36
-// Items: 3
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-diogene_licence?lang_cible=en
+// ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// D
+	// D
 	'diogene_licence_description' => '"License" add-on for "Diogene" (use the Licences plugin)',
 	'diogene_licence_nom' => 'Diogene - License',
-	'diogene_licence_slogan' => '"License" add-on for "Diogene" (use the Licences plugin)',
+	'diogene_licence_slogan' => '"License" add-on for "Diogene" (use the Licences plugin)'
 );
+
 ?>
--- diogene/diogene_complements/diogene_licence/trunk/lang/paquet-diogene_licence_fr.php
+++ diogene/diogene_complements/diogene_licence/trunk/lang/paquet-diogene_licence_fr.php
@@ -1,20 +1,14 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-diogene_licence
-// Langue: fr
-// Date: 14-06-2012 17:25:36
-// Items: 3
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/diogene/diogene_complements/diogene_licence/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// D
+	// D
 	'diogene_licence_description' => 'Complément "licence" pour "Diogene" (utilise le plugin Licences)',
 	'diogene_licence_nom' => 'Diogene - Licence',
-	'diogene_licence_slogan' => 'Complément "licence" pour "Diogene" (utilise le plugin Licences)',
+	'diogene_licence_slogan' => 'Complément "licence" pour "Diogene" (utilise le plugin Licences)'
 );
+
 ?>
