7ab8b2f94172f7c00d58d749cb58fb8b3bf4838a
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-02-02 16:36:09 +0000

    Transfert de tous les composants de la boussole spip dans le plugin
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@69644 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ boussole_spip/trunk/lang/paquet-bouspip_fr.php
@@ -0,0 +1,13 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'bouspip_description' => 'Ce plugin fournit l\'ensemble des informations sur les sites de la Galaxie SPIP. Combiné au plugin Boussole il permet de créer un serveur des sites de la Galaxie accessibles par tous les sites utilisateur.',
+	'bouspip_nom' => 'Boussole SPIP',
+	'bouspip_slogan' => 'Les bonnes adresses de la galaxie SPIP !'
+);
+
+?>
