e9cf6740abc66936e70504681e1209e7d2b7e1af
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-02-02 02:41:48 +0000

    langues (paquet-jfu)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@69629 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- jquery_file_upload/trunk/lang/paquet-jfu_fr.php
+++ jquery_file_upload/trunk/lang/paquet-jfu_fr.php
@@ -1,20 +1,14 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-jfu
-// Langue: fr
-// Date: 20-06-2012 21:07:58
-// Items: 2
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/jquery_file_upload/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// J
+	// J
 	'jfu_description' => 'jQuery-File-Upload
 _ Ce plugin  fournit la librairie [jQuery-File-Upload->http://aquantum-demo.appspot.com/file-upload].',
-	'jfu_slogan' => 'jQuery-File-Upload',
+	'jfu_slogan' => 'jQuery-File-Upload'
 );
+
 ?>
