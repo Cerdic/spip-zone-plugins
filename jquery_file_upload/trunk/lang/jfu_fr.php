9409bf0299d50984dea0440e3d816ae11bf38fc2
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-02-02 02:41:46 +0000

    langues (jfu)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@69628 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- jquery_file_upload/trunk/lang/jfu_fr.php
+++ jquery_file_upload/trunk/lang/jfu_fr.php
@@ -1,18 +1,19 @@
 <?php
-
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-if (!defined("_ECRIRE_INC_VERSION")) return;
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/jquery_file_upload/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-	// 
+
+	// A
 	'analyze_document' => 'Analyse de votre document',
-	
+
 	// B
+	'bouton_cancel_upload' => 'Annuler la mise en ligne',
 	'bouton_delete' => 'Supprimer',
 	'bouton_start' => 'Envoyer',
-	'bouton_cancel_upload' => 'Annuler la mise en ligne',
 	'bouton_start_upload' => 'Lancer la mise en ligne',
-	
+
 	// E
 	'erreur_emptyresult' => 'Le retour est vide',
 	'erreur_fichier_trop_gros' => 'Le fichier est trop gros.',
@@ -21,20 +22,19 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'erreur_max_number_files' => 'Vous avez dépassé le nombre maximal de fichiers',
 	'erreur_message_erreur' => 'Erreur :',
 	'erreur_uploadedbytes' => 'Le nombre de bytes envoyés dépassent la taille maximale',
-	
+
 	// L
 	'legend_mise_en_ligne_multiple' => 'Choisissez les fichiers...',
 	'legend_mise_en_ligne_unique' => 'Choisissez le fichier...',
-	
+
 	// M
 	'message_file_size_limit' => 'La taille maximale d\'un fichier est de @taille@ MB',
 	'message_queue_limit_max' => 'La limite maximale du nombre de fichier dans la file d\'attente est @nb@.',
 	'message_types_fichiers_autorises' => 'L\'ensemble des extensions de fichier autorisées sont : @types@',
-	
+
 	// T
 	'temps_passe' => 'passé',
-	'temps_restant' => 'restant',
-	
+	'temps_restant' => 'restant'
 );
 
 ?>
