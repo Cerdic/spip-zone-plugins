f3b6e8b06e82e5c7cfd1f61ceecb78aa24563254
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-02-02 02:41:24 +0000

    langues (diogene_geo)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@69613 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- diogene/diogene_complements/diogene_geo/trunk/lang/diogene_geo_en.php
+++ diogene/diogene_complements/diogene_geo/trunk/lang/diogene_geo_en.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// extrait automatiquement de http://trad.mediaspip.net/spip.php?page=tradlang
+// extrait automatiquement de http://trad.spip.net/tradlang_module/diogene_geo?lang_cible=en
 // ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
@@ -28,6 +28,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// M
 	'message_article_geolocaliser' => 'Geolocate this page',
+	'message_postion_auto' => 'Your media has been automatically positionned according to its metadata.',
 
 	// N
 	'non_localisable' => 'Not localizable',
