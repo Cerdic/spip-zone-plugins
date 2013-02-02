981c91228fc976265c4d32bb95d3818d2a439f93
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-02-02 02:41:26 +0000

    langues (paquet-diogene_geo)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@69614 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- diogene/diogene_complements/diogene_geo/trunk/lang/paquet-diogene_geo_en.php
+++ diogene/diogene_complements/diogene_geo/trunk/lang/paquet-diogene_geo_en.php
@@ -1,20 +1,16 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-diogene_geo
-// Langue: en
-// Date: 14-06-2012 12:52:18
-// Items: 3
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-diogene_geo?lang_cible=en
+// ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// D
+	// D
 	'diogene_geo_description' => 'Geolocation add-on for "Diogene".',
 	'diogene_geo_nom' => 'Diogene - Geo',
-	'diogene_geo_slogan' => 'Geolocation add-on for "Diogene"',
+	'diogene_geo_slogan' => 'Geolocation add-on for "Diogene"'
 );
+
 ?>
