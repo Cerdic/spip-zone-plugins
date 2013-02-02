8afe92c6670d1694608feab6973496c6320b1298
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-02-02 02:42:00 +0000

    langues (paquet-piecon)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@69633 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- piecon/trunk/lang/paquet-piecon_fr.php
+++ piecon/trunk/lang/paquet-piecon_fr.php
@@ -1,6 +1,6 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/piecon/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
@@ -8,6 +8,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// P
 	'piecon_description' => 'Ce plugin ne fait qu\'insérer et configurer la librairie Javascript [Piecon->http://lipka.github.com/piecon/] dans SPIP',
 	'piecon_nom' => 'Piecon',
-	'piecon_slogan' => 'Insertion de la librairie Piecon dans SPIP',
+	'piecon_slogan' => 'Insertion de la librairie Piecon dans SPIP'
 );
+
 ?>
