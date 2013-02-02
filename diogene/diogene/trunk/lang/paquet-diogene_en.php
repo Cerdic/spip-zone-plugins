7d213dfb8363f08169928ab3b1c6956decc166fc
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-02-02 02:41:22 +0000

    langues (paquet-diogene)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@69612 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- diogene/diogene/trunk/lang/paquet-diogene_en.php
+++ diogene/diogene/trunk/lang/paquet-diogene_en.php
@@ -1,24 +1,20 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-diogene
-// Langue: en
-// Date: 11-06-2012 13:56:51
-// Items: 2
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-diogene?lang_cible=en
+// ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// D
+	// D
 	'diogene_description' => 'The plugin Diogene permits to create sector-specific forms masks for the three specific object types:
 -* articles;
 -* sections;
 -* websites;
 
 It allows to define for a particular sector a form mask per object, adding or removing fields to customize the edit form to conform to what we really want to publish in these areas but also bringing within the same edit form all the information to publish quickly.',
-	'diogene_slogan' => 'Forms templates creation for the objects',
+	'diogene_slogan' => 'Forms templates creation for the objects'
 );
+
 ?>
--- diogene/diogene/trunk/lang/paquet-diogene_fr.php
+++ diogene/diogene/trunk/lang/paquet-diogene_fr.php
@@ -1,24 +1,18 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-diogene
-// Langue: fr
-// Date: 11-06-2012 13:56:51
-// Items: 2
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/diogene/diogene/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// D
+	// D
 	'diogene_description' => 'Le plugin Diogène permet de créer des masques de formulaires spécifiques par secteur sur les trois objets spécifiques que sont :
 -* les articles;
 -* les rubriques;
 -* les sites
 
 Il permet ainsi de définir en fonction d’un secteur particulier, un masque de formulaire par objet, ajoutant ou enlevant ainsi des champs afin de rendre le formulaire d’édition conforme à ce que l’on souhaite réellement publier dans ces secteurs mais également rassemblant au sein d’un même formulaire d’édition l’ensemble des informations permettant de publier rapidement.',
-	'diogene_slogan' => 'Création de templates de formulaires pour les objets',
+	'diogene_slogan' => 'Création de templates de formulaires pour les objets'
 );
+
 ?>
