01faea3daa777e1a2c153109efad03e6465f4d5d
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-02-02 02:41:52 +0000

    langues (paquet-mediaspip_player)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@69631 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- mediaspip_player/trunk/lang/paquet-mediaspip_player_en.php
+++ mediaspip_player/trunk/lang/paquet-mediaspip_player_en.php
@@ -1,20 +1,16 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-mediaspip_player
-// Langue: en
-// Date: 15-06-2012 15:18:40
-// Items: 3
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-mediaspip_player?lang_cible=en
+// ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// M
+	// M
 	'mediaspip_player_description' => 'HTML5 Media player based on the audio and video tag with a flash fallback for old browsers (via [Flowplayer->http://flowplayer.org])',
 	'mediaspip_player_nom' => 'MediaSPIP media player',
-	'mediaspip_player_slogan' => 'HTML5 Media player made for MediaSPIP',
+	'mediaspip_player_slogan' => 'HTML5 Media player made for MediaSPIP'
 );
+
 ?>
--- mediaspip_player/trunk/lang/paquet-mediaspip_player_fr.php
+++ mediaspip_player/trunk/lang/paquet-mediaspip_player_fr.php
@@ -1,20 +1,14 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-mediaspip_player
-// Langue: fr
-// Date: 15-06-2012 15:18:40
-// Items: 3
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/mediaspip_player/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// M
+	// M
 	'mediaspip_player_description' => 'Lecteur multimédia HTML5 s\'appuyant sur les balises audio et video et disposant d\'une solution de repli en flash (via [Flowplayer->http://flowplayer.org]) pour les navigateurs anciens',
 	'mediaspip_player_nom' => 'Lecteur de médias de MediaSPIP',
-	'mediaspip_player_slogan' => 'Lecteur multimédia HTML5 pour MediaSPIP',
+	'mediaspip_player_slogan' => 'Lecteur multimédia HTML5 pour MediaSPIP'
 );
+
 ?>
