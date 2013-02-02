822d025029a4e6c36003475ca0db4b7e9b87be38
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-02-02 02:41:58 +0000

    langues (piecon)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@69632 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- piecon/trunk/lang/piecon_en.php
+++ piecon/trunk/lang/piecon_en.php
@@ -1,24 +1,26 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/piecon/trunk/lang/
+// extrait automatiquement de http://trad.spip.net/tradlang_module/piecon?lang_cible=en
+// ** ne pas modifier le fichier **
+
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// C
-	'cfg_titre_piecon' => 'Configuration de Piecon',
+	'cfg_titre_piecon' => 'Configuration de Piecon', # NEW
 
 	// E
 	'explication_fallback' => 'Le fallback est la méthode utilisée pour changer soit le title, soit la favicon de la page. Il peut avoir 3 valeurs :<br />
 "false", la valeur par défaut, ne changera que la favicon lorsque c\'est possible<br />
 "true", seul le title sera modifié en indiquant un pourcentage même si le navigateur accepte le changement de favicon<br />
-"force", changera la favicon lorsque c\'est possible ainsi que le title de la page;',
+"force", changera la favicon lorsque c\'est possible ainsi que le title de la page;', # NEW
 
 	// L
-	'label_background' => 'Couleur de fond',
-	'label_color' => 'Couleur',
-	'label_fallback' => 'Méthode par défaut',
-	'label_shadow' => 'Couleur d\'ombre',
+	'label_background' => 'Background color',
+	'label_color' => 'Color',
+	'label_fallback' => 'Default method',
+	'label_shadow' => 'Shadow color',
 
 	// O
 	'option_fallback_false' => 'false',
