09f78b9583995c787894ddcebebe7c84ffa14592
suske@brubel.net <suske@brubel.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-02-03 17:06:30 +0000

    SlideShow fullscreen jquery - import initial
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@69656 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ supersized/trunk/lang/paquet-supersized_fr.php
@@ -0,0 +1,16 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+// Module: paquet-accesrestreint
+// Langue: fr
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// A
+	'supersized_description' => 'Intégration dans SPIP du plugin jQuery SperSized Slideshow',
+	'supersized_nom' => 'Supersized slideshow',
+	'supersized_slogan' => 'Un slideshow en plein écran pour vos images',
+);
+?>
