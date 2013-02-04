3d32354e9a940b7bc603acaa1986745715ecb440
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-02-04 16:53:52 +0000

    langonet indique que ces chaines ne sont pas utilisées. On les enlève
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@69702 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spipicious_jquery/trunk/lang/spipicious_fr.php
+++ spipicious_jquery/trunk/lang/spipicious_fr.php
@@ -7,11 +7,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
 	'add_tags' => 'Ajouter des tags',
-	'aucun_tags' => 'Aucun tag associé.',
 	'auteur_tag_usage' => 'Cet auteur utilise @nb@ fois ce tag',
-
-	// B
-	'by' => 'Par',
 
 	// C
 	'cfg_description' => 'Ce plugin permet d\'ajouter aisément des tags aux objets.',
@@ -20,24 +16,16 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'explication_ajout_tag' => 'Pour ajouter plusieurs tags à la fois, utilisez le séparateur : "@separateur@".',
 	'explication_suppression_tag' => 'Sélectionnez vos tags dans la liste ci-dessous et validez pour les supprimer.',
 
-	// I
-	'identifier' => 'Pour ajouter des tags sur cet article, vous devez vous identifier',
-
 	// L
 	'label_tags' => 'Tags :',
 	'liste_auteurs_tag' => 'Ce tag est utilisé par :',
 
 	// M
 	'message_aucun_tags' => 'Aucun tag n\'est associé',
-	'message_installation_activation' => 'Installation des tables et configuration par défaut de spip.icio.us',
-	'message_upgrade_database' => 'Mise à de la base de donné de spip.ici.ous en version @version@<br/>',
 
 	// N
 	'no_tags_associes' => 'Vous n\'avez pas de mot clé associé',
 	'nom_groupe_tag' => 'Groupe de mots clé associé',
-
-	// S
-	'spipicious' => 'Spip.icio.us',
 
 	// T
 	'tag_ajoute' => 'Vous avez ajouté un tag :<br />@name@',
@@ -48,14 +36,10 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'tags_supprimes' => 'Vous avez supprimé @nb@ tags :<br />@name@',
 	'title_tag_utilise_nb' => 'Ce mot clé est associé par @nb@ personnes.',
 	'title_tag_utilise_un' => 'Ce mot clé est associé par une personne.',
-	'to' => 'avec',
 	'types_utilisateurs' => 'Qui a accès au formulaire d\'ajout de tags ?',
 
 	// V
 	'vos_tags' => 'Vos tags (modification)',
-
-	// W
-	'waiting' => 'recherche de vos tags'
 );
 
 ?>
