1fc30f91f8a9837aad3aad80f3c7968b9573329a
suske@brubel.net <suske@brubel.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-02-04 20:05:54 +0000

    bricoles cosmético-orthographiques et un logo moche mais réel
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@69707 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- supersized/trunk/lang/paquet-supersized_fr.php
+++ supersized/trunk/lang/paquet-supersized_fr.php
@@ -9,7 +9,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 // A
-	'supersized_description' => 'Intégration dans SPIP du plugin jQuery SperSized Slideshow',
+	'supersized_description' => 'Intégration dans SPIP du plugin jQuery Supersized Slideshow',
 	'supersized_nom' => 'Supersized slideshow',
 	'supersized_slogan' => 'Un slideshow en plein écran pour vos images',
 );
