a02a9970bf0083c76e7bc0651bba61b1f151d7e0
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-02-04 02:41:59 +0000

    langues (multilingue)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@69662 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- multilingue/trunk/lang/multilingue_en.php
+++ multilingue/trunk/lang/multilingue_en.php
@@ -10,6 +10,15 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// A
 	'afficher_menu_select' => 'Show a dropdown menu (select)',
 
+	// D
+	'desactiver_traduction_logo_objet' => 'Désactiver la possibilité que la balise #LOGO_OBJET inclut le logo de l\'objet d\'origine s\'elle ne trouve pas de logo pour l\'objet en question', # NEW
+
+	// E
+	'explication_desactiver_traduction_logo_objet' => 'Si l\'objet est traduit (présence d\'un id_trad comme pour les articles et rubriques) ce plugin fait en sorte que la balise #LOGO_ARTICLE p.ex cherche le logo de la traduction de l\'article (article de référence) si celui-ci ne dipose pas de logo.', # NEW
+
+	// L
+	'logo_objet' => 'Logo Objet', # NEW
+
 	// M
 	'menu_langue' => 'Language selection menu',
 	'multilingue_titre' => 'Multilingual Web-site Easy',
