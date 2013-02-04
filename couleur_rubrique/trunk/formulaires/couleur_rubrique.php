9f9c7959933eb578165687cbeffaf75e58def4ce
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-02-04 16:33:01 +0000

    indentation, defined de protection, et petouilles de mise en forme sur les checkbox des formulaires + renommage de script.js plus explicite + suppression du fichier _administration qui ne sert a rien (pas de schema)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@69701 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- couleur_rubrique/trunk/formulaires/couleur_rubrique.php
+++ couleur_rubrique/trunk/formulaires/couleur_rubrique.php
@@ -1,16 +1,17 @@
 <?php
+if (!defined("_ECRIRE_INC_VERSION")) return;
 
 function formulaires_couleur_rubrique_charger_dist($id_rubrique){
 	// chargement des valeurs du formulaire
-	$valeurs = array('pb_couleur_rubrique'=>'','supprimer'=>'');
+	$valeurs = array('pb_couleur_rubrique' => '', 'supprimer' => '');
 	// autorisation : #ENV{editable} est evite car on veut toujours voir le formulaire meme apres validation
 	return $valeurs;
 }
 
 function formulaires_couleur_rubrique_verifier_dist($id_rubrique){
-        // rien de particulier a verifier
-        $erreurs = array();
-	if (!_request('pb_couleur_rubrique',$_POST) && !_request('supprimer',$_POST))
+	// rien de particulier a verifier
+	$erreurs = array();
+	if (!_request('pb_couleur_rubrique') && !_request('supprimer'))
 		$erreurs['message_erreur'] = _T('pb_couleur_rubrique:info_obligatoire');
 	return $erreurs;
 }
@@ -20,12 +21,12 @@ function formulaires_couleur_rubrique_traiter_dist($id_rubrique){
 	$cr = _request('pb_couleur_rubrique');
 	$couleur = str_replace("#", "", $cr);
 	// enregistrer/supprimer les valeurs
-	ecrire_meta("pb_couleur_rubrique$id_rubrique",$couleur);
-	if (_request('supprimer', $_POST))
-		ecrire_meta("pb_couleur_rubrique$id_rubrique","");
-		
+	ecrire_meta("pb_couleur_rubrique$id_rubrique", $couleur);
+	if (_request('supprimer'))
+		ecrire_meta("pb_couleur_rubrique$id_rubrique", "");
+
 	ecrire_metas();
-	
+
 }
 
 ?>
