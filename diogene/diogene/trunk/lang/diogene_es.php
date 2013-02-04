422d74de812f81758fe559b576b6fa69ec0a814e
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-02-04 02:41:47 +0000

    langues (diogene)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@69660 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ diogene/diogene/trunk/lang/diogene_es.php
@@ -0,0 +1,167 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/diogene?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'bouton_editer_diogene' => 'Plantillas de formularios',
+	'bouton_supprimer_diogene' => 'Eliminar esta plantilla',
+
+	// C
+	'champ_date_publication' => 'Fecha de publicación',
+	'champ_date_publication_anterieure' => 'Fecha de publicación anterior',
+	'champ_forum' => 'Activar / desactivar los foros',
+	'choix_langue' => 'Idioma',
+
+	// D
+	'diogene' => 'Plantilla de formulario',
+	'diogene_statuts' => 'Diógenes (Estatus)',
+	'diogenes' => 'Plantillas de formularios',
+
+	// E
+	'editer_diogene_explication' => 'Diógenes permite modificar los fumularios de edición clásicos quitando o añadiendo ciertos campos.',
+	'editer_diogenes_titre' => 'Las plantillas de formularios',
+	'erreur' => 'Error',
+	'erreur_autorisation_login_publier' => 'Debe estar identificado para publicar aquí.',
+	'erreur_autorisation_modifier_site' => 'Su estatus no le autoriza para modificar este sitio',
+	'erreur_autorisation_statut_publier' => 'Su estatus no le autoriza para publicar aquí. ',
+	'erreur_autorisation_statut_publier_limite' => 'Vous avez atteint la limite d\'objet pouvant être <a href="@url@">en attente de publication</a> (@nb@)', # NEW
+	'erreur_diogene_multiple_page' => 'No puede tener más que una sola plantilla "página" en este sitio',
+	'erreur_droits_objet_publier' => 'Sus derechos actuales no le autorizan para publicar este tipo de objeto. ',
+	'erreur_forums' => 'Error en la elección de la configuración de los foros',
+	'erreur_identifiant_existant' => 'Este identificador ya existe',
+	'erreur_objet_diogene_max' => 'Le nombre de masques maximum (@max@) pour ce type d\'objet (@objet@) est déjà atteint.', # NEW
+	'erreur_objet_non_diogene' => 'Ce type d\'objet n\'est pas pris en compte (@objet@).', # NEW
+	'erreur_objet_publier' => 'El tipo de objeto que desea publicar no existe.',
+	'erreur_secteur_diogene_inexistant' => 'Atención, la sección que concierne a esta plantilla de formulario no existe.',
+	'erreur_valeur_float' => 'El valor "@champ@" debe ser un número',
+	'erreur_valeur_int' => 'El valor "@champ@" debe ser un número fijo. ',
+	'explication_article_deja_traduit' => 'Este artículo ya está traducido en: ',
+	'explication_contenu_attente' => 'Ces objets d\'autres auteurs sont en attente de publication définitive. Leur publication a été demandée.', # NEW
+	'explication_info_type' => 'Cet identifiant doit être unique et ne comporter aucun caractère spécifiques', # NEW
+	'explication_nombre_attente' => 'Nombre maximal d\'objets en attente de publication (n\'affecte pas les administrateurs, laisser 0 pour illimité).', # NEW
+	'explication_rubrique_statut' => 'Une rubrique est considérée comme visible (publiée) lorsqu\'elle contient au moins un autre objet publié.', # NEW
+	'explication_statut_auteur' => 'Ce champ défini le statut minimal pour pouvoir créer un objet correspondant à ce template.', # NEW
+	'explication_statut_auteur_publier' => 'Ce champ défini le statut minimal pour pouvoir publier définitiment un objet correspondant à ce template.', # NEW
+
+	// F
+	'formulaire_modifier_diogene' => 'Modificar la plantilla (Diógenes)',
+	'formulaire_nouveau' => 'Nueva plantilla de formulario',
+
+	// I
+	'icone_editer_diogene_nouveau' => 'Nueva plantilla (@type@)',
+	'icone_modifier_diogene' => 'Modificar esta plantilla',
+	'icone_nouveau_diogene' => 'Crear una nueva plantilla',
+	'info_1_diogene' => '1 plantilla',
+	'info_aucun_diogene' => 'Ninguna plantilla',
+	'info_aucune_sous_rubrique' => 'Il n\'existe pas encore de sous-rubrique correspondante.', # NEW
+	'info_diogenes_rien_publie' => 'Aún no ha publicado nada.',
+	'info_menu_diogene' => 'Élément de menu pour le plugin Diogène', # NEW
+	'info_nb_diogenes' => '@nb@ templates', # NEW
+	'info_numero_diogene' => 'Template numéro', # NEW
+	'info_publier_rubrique' => 'Publicar en esta sección:',
+	'info_referencement_automatise' => 'Les informations de référencement automatisé', # NEW
+	'info_rubrique_new' => 'Nueva sección vacía',
+	'info_rubrique_publie' => 'Rubrique visible', # NEW
+	'info_rubrique_vide' => 'Sección vacía',
+	'info_statut' => 'Estatus: ',
+	'info_syndication' => 'Sindicación',
+	'info_traduction_article' => 'Cet article est une traduction de «<a href="@url@">@titre@</a>».', # NEW
+	'info_type' => 'Identificador',
+
+	// L
+	'label_cextras_enleves' => 'Campos extras para no mostrar',
+	'label_champs_ajoutes' => 'Campos a añadir',
+	'label_champs_caches' => 'Campos a ocultar',
+	'label_change_statut_normal' => 'Modificar el estatus de su estatus',
+	'label_change_statut_normal_site' => 'Modificar el estatus de este sitio',
+	'label_description' => 'Descripción',
+	'label_limiter_rubriques' => 'Limitar a las secciones',
+	'label_limiter_secteur' => 'Limitar a la red',
+	'label_logo_site' => 'Logo del sitio',
+	'label_menu' => 'Figurer dans le menu public', # NEW
+	'label_nombre_attente' => 'Nombre maximal en attente', # NEW
+	'label_polyhier' => 'Polyhiérarchie', # NEW
+	'label_polyhier_desactiver' => 'Désactiver la polyhiérarchie', # NEW
+	'label_statut_auteur' => 'Statut minimal des auteurs', # NEW
+	'label_statut_auteur_publier' => 'Statut minimal des auteurs pouvant publier définitivement', # NEW
+	'legende_champs_diogene' => 'Campos del formulario',
+	'legende_selecteur_statut' => 'Estatus',
+	'legende_statuts_diogene' => 'Estatus',
+	'libelle_logo_diogene' => 'Logo de la plantilla',
+	'lien_creer' => 'Crear: ',
+	'lien_creer_version' => 'Crear una versión en: @lang@',
+	'lien_editer_publication' => 'Editar esta publicación',
+	'lien_editer_rubrique' => 'Editar esta sección',
+	'lien_editer_site' => 'Editar este sitio',
+	'lien_modifier_version' => 'Modificar la versión en: @lang@',
+	'lien_modifier_version_originale' => 'Cambiar la versión original a: @lang@',
+	'lien_publier_nouvelle_rubrique' => 'Publicar una nueva sección (@type@)',
+	'lien_version' => 'Versión: @lang@',
+
+	// M
+	'message_article_mis_a_jour' => 'El artículo ha sido actualizado',
+	'message_article_traduit_en' => 'Este artículo ya existe en:',
+	'message_confirm_depublier' => '¿Está seguro de querer publicarlo?',
+	'message_confirm_poubelle' => '¿Está seguro de querer eliminarlo?',
+	'message_confirm_sup' => '¿Está seguro de continuar con la eliminación?',
+	'message_diogene_update' => 'La plantilla de formulario ha sido actualizada',
+	'message_erreur_general' => 'El formulario contiene errores, verifique por favor el contenido.',
+	'message_id_parent_unique' => 'Este objeto no puede ser creado más que en la sección «@rubrique@»',
+	'message_pas_auteur' => 'Atención. No está identificado como autor de este objeto.',
+	'message_rubrique_creee' => 'La sección se ha creado correctamente. No será visible hasta que no se publique un elemento en ella.',
+	'message_rubrique_mis_a_jour' => 'La sección ha sido actualizada.',
+	'message_site_mis_a_jour' => 'El sitio ha sido actualizado',
+	'message_valider_action' => '¿Está seguro?',
+	'message_valider_suppression' => '¿Está seguro de querer eliminar esta plantilla?',
+	'modifier_rubriques' => 'Modificar las secciones',
+	'modifier_vos_objets' => 'Modificar sus publicaciones',
+
+	// O
+	'option_statut_changer' => 'Cambiar el estatus a: @statut@',
+	'option_statut_laisser' => 'Dejar el estatus en: @statut@',
+
+	// P
+	'publier_titre' => 'Publicar: ',
+	'publier_titre_hierarchie' => 'Publicar',
+
+	// S
+	'statuts' => 'Estatus',
+
+	// T
+	'texte_choix_rubrique' => 'Publicar en la sección',
+	'texte_statut_en_cours_poubelle_normal' => 'Dejar su artículo en la papelera',
+	'texte_statut_en_cours_poubelle_site' => 'Dejar este sitio en la papelera',
+	'texte_statut_en_cours_prop_normal' => 'Dejar su artículo propuesto para publicación',
+	'texte_statut_en_cours_prop_site' => 'Dejar este sitio propuesto para publicación',
+	'texte_statut_en_cours_publie_normal' => 'Dejar su artículo publicado',
+	'texte_statut_en_cours_publie_site' => 'Dejar este sitio publicado',
+	'texte_statut_en_cours_redaction' => 'Dejar su artículo en curso de redacción',
+	'texte_statut_poubelle_normal' => 'Tirar su artículo a la papelera',
+	'texte_statut_poubelle_normal_pas_auteur' => 'Tirar este artículo a la papelera',
+	'texte_statut_poubelle_site' => 'Tirar este sitio a la papelera',
+	'texte_statut_prop_normal' => 'Proponer su artículo para publicación',
+	'texte_statut_prop_site' => 'Proponer este sitio para publicación',
+	'texte_statut_publie_normal' => 'Publicar definitivamente su artículo',
+	'texte_statut_publie_site' => 'Publicar definitivamente este sitio',
+	'texte_statut_redaction' => 'Poner su artículo en curso de redacción',
+	'texte_statut_redaction_sans_statut' => 'Poner su artículo en curso de redacción',
+	'titre_lien_publier' => 'Publicar',
+	'titre_modification_article' => 'Modificación del artículo: @titre@',
+	'titre_modification_rubrique' => 'Modificación de la sección: @titre@',
+	'titre_modification_site' => 'Modificación del sitio: @titre@',
+	'titre_modifier_article' => 'Modificar esta publicación',
+	'titre_modifier_publication' => 'Modificar esta publicación',
+	'titre_modifier_rubrique' => 'Modificar esta sección',
+	'titre_modifier_site' => 'Modificar este sitio',
+	'titre_publier_nouveau_page' => 'Un nuevo objeto',
+	'titre_publier_proposes' => 'Un objeto propuesto (@nb@)',
+	'titre_publier_proposes_page' => 'Un objeto propuesto',
+	'titre_publier_proposes_page_vous' => 'Sus objetos propuestos'
+);
+
+?>
