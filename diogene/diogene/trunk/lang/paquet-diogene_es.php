a7b31ac87d642e1e8136aa2722805994f8c65db8
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-02-05 02:41:13 +0000

    langues (paquet-diogene)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@69715 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ diogene/diogene/trunk/lang/paquet-diogene_es.php
@@ -0,0 +1,20 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-diogene?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// D
+	'diogene_description' => 'El plugin Diógenes permite crear plantillas de formularios específicos por sectores en tres objetos concretos que son:
+-* artículos;
+-* secciones;
+-* sitios
+
+Permite así definir, en base a un sector particular, una plantilla de formulario por objeto añadiendo o quitando de este modo campos para adecuar el formulario de edición a aquello que se desea realmente publicar en estos sectores, pero también recogiendo en un mismo formulario de edición toda la información necesaria para publicar rápidamente.',
+	'diogene_slogan' => 'Creación de plantillas de formularios para los objetos'
+);
+
+?>
