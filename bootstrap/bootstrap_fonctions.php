be5b69f7d29f95305a7feb6aca4fae72145c6abc
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-02-05 13:37:56 +0000

    filtre |navbar_responsive qui permet de poser le markup necessaire pour collapser la navbar en dessous de la largeur @navbarCollapse qui passe à 767px par defaut si le markup est deja là le filtre ne fait rien + une classe facultative .navbar-responsive à poser à côté de .navbar (utilisable independamment du filtre ci-dessus) qui passe la navbar en simple bouton à cliquer en position absolute (le menu apparait en depliage au clic sur le bouton)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@69754 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- bootstrap/bootstrap_fonctions.php
+++ bootstrap/bootstrap_fonctions.php
@@ -1,5 +1,42 @@
 <?php
 
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+
+/**
+ * Ajouter le markup html pour une navbar responsive
+ * [<div class="navbar navbar-inverse navbar-responsive" id="nav">
+ * (#INCLURE{fond=inclure/nav,env}|navbar_responsive)
+ * </div>]
+ *
+ * @param string $nav
+ * @return string
+ */
+function navbar_responsive($nav){
+	if (strpos($nav,"nav-collapse")!==false) return $nav;
+
+	$uls = extraire_balises($nav,"ul");
+	$n = 1;
+	while ($ul = array_shift($uls)
+	  AND strpos(extraire_attribut($ul,"class"),"nav")===false){
+		$n++;
+	}
+	if ($ul){
+		$respnav = $nav;
+		$p = strpos($respnav,$ul);
+		$respnav = substr_replace($respnav,'<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse"><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></a>'."\n".'<div class="nav-collapse collapse">',$p,0);
+		$l=strlen($respnav);$p=$l-1;
+		while ($n--){
+			$p = strrpos($respnav,"</ul>",$p-$l);
+		}
+		if ($p)
+			$respnav = substr_replace($respnav,'</div>',$p+5,0);
+		else
+			$respnav = $nav;
+	}
+	return $respnav;
+}
+
 function filtre_lien_ou_expose_dist($url,$libelle=NULL,$on=false,$class="",$title="",$rel="", $evt=''){
 	if ($on) {
 		$bal = 'strong';
