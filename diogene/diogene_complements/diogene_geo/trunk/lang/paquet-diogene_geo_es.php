3f32b5b840c48f5545974e3c724aba155b30d6ce
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-02-05 02:41:17 +0000

    langues (paquet-diogene_geo)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@69717 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ diogene/diogene_complements/diogene_geo/trunk/lang/paquet-diogene_geo_es.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-diogene_geo?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// D
+	'diogene_geo_description' => 'Complemento geográfico para "Diógenes"',
+	'diogene_geo_nom' => 'Diógenes - Geo',
+	'diogene_geo_slogan' => 'Complemento geográfico para "Diógenes"'
+);
+
+?>
