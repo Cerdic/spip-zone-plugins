6008948e52e672488eaff4de533766f14458818e
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-02-05 02:41:15 +0000

    langues (diogene_geo)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@69716 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ diogene/diogene_complements/diogene_geo/trunk/lang/diogene_geo_es.php
@@ -0,0 +1,44 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/diogene_geo?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'adresse' => 'Dirección',
+
+	// F
+	'form_legend' => 'Informaciones geográficas',
+
+	// G
+	'gis_info_descriptif' => 'Descriptivo del punto',
+	'gis_info_titre' => 'Título del punto',
+
+	// L
+	'label_geo_afficher' => 'Mostrar las informaciones de geolocalización',
+	'label_geo_cacher' => 'Ocultar el mapa por defecto',
+	'label_geo_cacher_defaut' => 'Esconde el mapa y los campos de geolocalización por defecto',
+	'label_recherche' => 'Mostrar un campo de búsqueda debajo del mapa.',
+	'label_supprimer_gis' => 'Eliminar informaciones geográficas vinculadas',
+	'latitude' => 'Latitud',
+	'longitude' => 'Longitud',
+
+	// M
+	'message_article_geolocaliser' => 'Geolocalizar esta página',
+	'message_postion_auto' => 'Su medio se ha configurado automáticamente de acuerdo a sus metadatos.',
+
+	// N
+	'non_localisable' => 'No localizable',
+	'non_localisable_case' => 'Este medio no es localizable',
+
+	// R
+	'recherche' => 'Búsqueda',
+
+	// Z
+	'zoom' => 'Zoom'
+);
+
+?>
