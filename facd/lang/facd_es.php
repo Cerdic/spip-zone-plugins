f2037745b313b32692ca5748b4b24f884db75fec
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-02-05 02:41:34 +0000

    langues (facd)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@69727 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ facd/lang/facd_es.php
@@ -0,0 +1,61 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/facd?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'bouton_relancer_tout' => 'Reconvertir todos los documentos',
+	'bouton_relancer_tout_format' => 'Reconvertir todos los documentos al formato @format@',
+	'bouton_relancer_tout_format_message' => '¿Está seguro de querer reiniciar la codificación de todos los archivos en el formato @format@?',
+	'bouton_relancer_tout_message' => '¿Está seguro de querer reiniciar la codificación de todos los archivos?',
+
+	// E
+	'erreur_document_plus_disponible' => 'Este documento ya no está disponible en el sitio',
+	'explication_file_attente' => 'Esta página lista los documentos en cola. Éstos serán convertidos automáticamente por CRON.',
+
+	// I
+	'info_document_conversion' => 'Este documento está en curso de conversión',
+	'info_document_conversion_erreur' => 'La conversión de este documento a fracasado',
+	'info_document_dans_file_attente' => 'A la espera de conversión',
+	'info_relancer_erreurs' => 'Reiniciar todas las conversiones erróneas',
+	'info_statut_conversion_en_cours' => 'En curso',
+	'info_statut_conversion_erreur' => 'En error',
+	'info_statut_conversion_non' => 'A la espera',
+	'info_statut_conversion_oui' => 'Convertido',
+	'info_tous_docs_facd' => 'Documentos en cola',
+	'info_voir_log_erreur' => 'Consultar el registro de error',
+
+	// L
+	'label_relancer_conversion' => 'Reiniciar la conversión',
+	'lien_convertir_document' => 'Convertir este documento',
+	'lien_recharger' => 'Recargar',
+	'liste_attente_aucun' => 'Ningún documento en la cola.',
+	'liste_attente_tous' => 'Documentos en cola',
+	'liste_convert_aucun' => 'Ningún documento convertido',
+	'liste_convert_tous' => 'Documentos convertidos',
+
+	// T
+	'thead_date' => 'Fecha',
+	'thead_duree' => 'Duración',
+	'thead_duree_conversion' => 'Duración de conversión',
+	'thead_extension' => 'Formato',
+	'thead_fonction' => 'Función utilizada',
+	'thead_id' => 'ID',
+	'thead_id_auteur' => 'Autor',
+	'thead_id_document' => 'Original',
+	'thead_nombre' => 'Número',
+	'thead_statut' => 'Estatus',
+	'titre_log_conversion' => 'Contenido del registro de conversión #@id@',
+	'titre_page_file' => 'Cola de espera del módulo de conversión de medios',
+	'titre_page_file_convertis_jour' => 'Conversiones por fecha',
+	'titre_page_file_menu' => 'Cola de espera de conversión',
+
+	// V
+	'version_encodee_de' => 'Este documento es una conversión del documento @id_orig@'
+);
+
+?>
