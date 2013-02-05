c02563a14fb8ee5051ae7ec4143750a9fde247f9
suske@brubel.net <suske@brubel.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-02-05 21:21:39 +0000

    des images en 1200px pour le slide. Max 6 art/rub comme base sans paramètre art/rub.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@69766 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- supersized/trunk/lang/supersized_fr.php
+++ supersized/trunk/lang/supersized_fr.php
@@ -6,7 +6,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// V
-	'voir_slideshow' => 'Voir le slideshow !'
+	'voir_slideshow' => 'Voir le slideshow'
 
 );
 
