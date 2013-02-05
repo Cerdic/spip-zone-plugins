fbca246c1159fd6be7ecc334e37202ca489aa1ae
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-02-05 02:41:32 +0000

    langues (paquet-diogene_spipicious)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@69726 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ diogene/diogene_complements/diogene_spipicious/trunk/lang/paquet-diogene_spipicious_es.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-diogene_spipicious?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// D
+	'diogene_spipicious_description' => 'Complemento "etiquetas personales" para "Diógenes" (utiliza SpipIcious)',
+	'diogene_spipicious_slogan' => 'Complemento "etiquetas personales"'
+);
+
+?>
