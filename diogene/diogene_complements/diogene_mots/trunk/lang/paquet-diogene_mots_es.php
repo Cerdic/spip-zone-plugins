0290d2bd7f67f38028e05ebd01a5e1cc69056eda
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-02-05 02:41:27 +0000

    langues (paquet-diogene_mots)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@69723 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ diogene/diogene_complements/diogene_mots/trunk/lang/paquet-diogene_mots_es.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-diogene_mots?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// D
+	'diogene_mots_description' => 'Complemento palabras clave para "Diógenes"',
+	'diogene_mots_nom' => 'Diógenes - Palabras',
+	'diogene_mots_slogan' => 'Complemento palabras clave para "Diógenes"'
+);
+
+?>
