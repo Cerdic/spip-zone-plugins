9c96c11ac9aa26d275fa374facd7a92da1a66d4d
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-02-05 02:41:22 +0000

    langues (diogene_licence)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@69720 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ diogene/diogene_complements/diogene_licence/trunk/lang/diogene_licence_es.php
@@ -0,0 +1,17 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/diogene_licence?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// F
+	'form_legend' => 'Licencia',
+
+	// L
+	'label_licence_defaut' => 'Licencia por defecto'
+);
+
+?>
