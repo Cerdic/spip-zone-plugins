7d4bb409b3f10fd9a206805c8071980586a5395b
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-02-05 02:41:28 +0000

    langues (paquet-diogene_notation)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@69724 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ diogene/diogene_complements/diogene_notation/trunk/lang/paquet-diogene_notation_es.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-diogene_notation?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// D
+	'diogene_notation_description' => 'Complemento notación para "Diógenes"',
+	'diogene_notation_nom' => 'Diógenes - Notaciones',
+	'diogene_notation_slogan' => 'Complemento notación para "Diógenes"'
+);
+
+?>
