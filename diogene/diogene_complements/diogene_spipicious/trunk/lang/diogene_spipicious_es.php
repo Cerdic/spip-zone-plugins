33596e386fb12d556a88325ab6765ed0e2fe5239
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-02-05 02:41:30 +0000

    langues (diogene_spipicious)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@69725 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ diogene/diogene_complements/diogene_spipicious/trunk/lang/diogene_spipicious_es.php
@@ -0,0 +1,24 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/diogene_spipicious?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// E
+	'explication_tags' => 'Esta opción utilizará el plugin SPIPicious para proponer a los autores agregar etiquetas en sus publicaciones',
+
+	// F
+	'form_legend' => 'Etiquetas',
+
+	// L
+	'label_remove_tags' => 'Eliminación de etiquetas',
+	'label_tags' => 'Añada una o más etiquetas',
+
+	// T
+	'tags_spipicious' => 'Etiquetas'
+);
+
+?>
