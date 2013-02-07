31d4c9d2b496bcfcaeb72b754a3224210ca15591
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-02-07 02:41:49 +0000

    langues (paquet-embed_code)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@69790 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ embed_code/trunk/lang/paquet-embed_code_es.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-embed_code?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// E
+	'embed_code_description' => 'Generar códigos de inclusión distantes de documentos para uso en otros sitios web.', # MODIF
+	'embed_code_nom' => 'Código incrustado',
+	'embed_code_slogan' => 'Generación de código incrustado para documentos' # MODIF
+);
+
+?>
