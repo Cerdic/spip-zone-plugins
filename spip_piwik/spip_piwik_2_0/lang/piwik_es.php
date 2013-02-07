5595d15789dd8bdbc5460db26002303ae8cf5141
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-02-07 02:42:04 +0000

    langues (piwik)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@69796 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spip_piwik/spip_piwik_2_0/lang/piwik_es.php
+++ spip_piwik/spip_piwik_2_0/lang/piwik_es.php
@@ -9,7 +9,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
 	'action_creer_site' => 'Crear la lista',
-	'action_recuperer_liste' => 'Recuperar la lista de los sitios',
+	'action_recuperer_liste' => 'Recuperar la lista de sitios webs',
 
 	// C
 	'cfg_description_piwik' => 'Aquí puede indicar su identificador piwik, así como la dirección del servidor que administra sus estadísticas.',
@@ -22,41 +22,41 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'explication_creer_site' => 'El siguiente enlace le permite crear un sitio web en el servidor Piwik que estará disponible después en la lista. Verifique que ha configurado bien la dirección y el nombre de su sitio web SPIP antes de hacer click, ésta será la información utilizada.',
 	'explication_exclure_ips' => 'Para excluir varias direcciones, sepárelas por punto y coma',
 	'explication_identifiant_site' => 'La lista de los sitios webs disponibles en el servidor Piwik se ha recuperado automáticamente grácias a las informaciones presentadas. Seleccione en la siguiente lista el que le convenga',
-	'explication_mode_insertion' => 'Il existe deux modes d\'insertion dans les pages du code nécessaire au bon fonctionnement du plugin. Par le pipeline "insert_head" (méthode automatique mais peu configurable), ou par l\'insertion d\'une balise (méthode manuelle en insérant dans le pied de vos pages la balise #PIWIK) qui, quant à elle est pleinement configurable.', # NEW
-	'explication_recuperer_liste' => 'Le lien ci-dessous vous permet de récupérer la liste des sites que votre compte peut administrer sur le serveur Piwik.', # NEW
-	'explication_restreindre_statut_prive' => 'Choisissez ici les statuts d\'utilisateurs qui ne seront pas comptabilisés dans les statistiques dans l\'espace privé', # NEW
-	'explication_restreindre_statut_public' => 'Choisissez ici les statuts d\'utilisateurs qui ne seront pas comptabilisés dans les statistiques dans la partie publique', # NEW
-	'explication_token' => 'Le token d\'identification est disponible dans vos préférences personnelles ou dans la partie API de votre serveur Piwik', # NEW
+	'explication_mode_insertion' => 'Hay dos modos de inserción en las páginas del código necesario para el buen funcionamiento del plugin. Mediante el pipeline "insert_head" (método automático pero poco configurable), o mediante la inserción de una etiqueta (método manual cuando se inserta al pie de sus páginas la etiqueta #PIWIK), la cual es plenamente configurable. ',
+	'explication_recuperer_liste' => 'El siguiente enlace le permite recuperar la lista de sitios webs que su cuenta puede administrar en el servidor Piwik.',
+	'explication_restreindre_statut_prive' => 'Elija aquí los estatus de usuarios que no serán contabilizados en las estadísticas en el espacio privado',
+	'explication_restreindre_statut_public' => 'Elija aquí los estatus de usuarios que no serán contabilizados en las estadísticas en la parte público',
+	'explication_token' => 'El token de identificación está disponible en sus preferencias personales o en la parte API de su servidor Piwik',
 
 	// I
 	'info_aucun_site_compte' => 'Ningún sitio web está asociado a su cuenta Piwik.',
 	'info_aucun_site_compte_demander_admin' => 'Ha de solicitar a un administrador de su servidor Piwik el añadir un sitio correspondiente',
 
 	// L
-	'label_adresse_serveur' => 'Adresse URL du serveur (https:// ou http://)', # NEW
+	'label_adresse_serveur' => 'Dirección URL del servidor (https:// o http://)',
 	'label_comptabiliser_prive' => 'Contabilizar las visitas al espacio privado',
 	'label_creer_site' => 'Crear un sitio en el servidor Piwik',
 	'label_exclure_ips' => 'Excluir ciertas direcciones IP',
-	'label_identifiant_site' => 'L\'identifiant de votre site sur le serveur Piwik', # NEW
+	'label_identifiant_site' => 'El identificador de su sitio web en el servidor Piwik',
 	'label_mode_insertion' => 'Modo de inserción en las páginas públicas',
-	'label_piwik_user' => 'Compte utilisateur Piwik', # NEW
-	'label_recuperer_liste' => 'Récupérer la liste des sites sur le serveur Piwik', # NEW
+	'label_piwik_user' => 'Cuenta de usuario Piwik',
+	'label_recuperer_liste' => 'Recuperar la lista de sitios en el servidor Piwik',
 	'label_restreindre_auteurs_prive' => 'Restringir a ciertos usuarios conectados (privados)',
 	'label_restreindre_auteurs_public' => 'Restringir a ciertos usuarios conectados (público)',
-	'label_restreindre_statut_prive' => 'Restreindre certains statuts d\'utilisateurs dans l\'espace privé', # NEW
-	'label_restreindre_statut_public' => 'Restreindre certains statuts d\'utilisateurs dans la partie publique', # NEW
+	'label_restreindre_statut_prive' => 'Restringir a ciertos estatus de usuarios en el espacio privado',
+	'label_restreindre_statut_public' => 'Restringir a ciertos estatus de usuarios en la parte pública',
 	'label_token' => 'Token de identificación en el servidor',
 
 	// M
-	'mode_insertion_balise' => 'Insertion par la balise #PIWIK (modification nécessaire de vos squelettes)', # NEW
-	'mode_insertion_pipeline' => 'Insertion automatique par le pipeline "insert_head"', # NEW
+	'mode_insertion_balise' => 'Inserción por la etiqueta #PIWIK (modificación necesaria de sus essqueletos)',
+	'mode_insertion_pipeline' => 'Inserción automática por el pipeline "insert_head"',
 
 	// P
 	'piwik' => 'Piwik',
 
 	// T
-	'texte_votre_identifiant' => 'Votre identifiant', # NEW
-	'textes_url_piwik' => 'Votre serveur piwik' # NEW
+	'texte_votre_identifiant' => 'Su identificador',
+	'textes_url_piwik' => 'Su servidor Piwik'
 );
 
 ?>
