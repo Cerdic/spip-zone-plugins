c76aed40cc004600ba39d7864e97bcbeb960d651
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-02-07 02:41:46 +0000

    langues (embed_code)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@69789 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ embed_code/trunk/lang/embed_code_en.php
@@ -0,0 +1,23 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/embed_code?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// I
+	'info_embed_code' => 'Embed this media',
+
+	// L
+	'label_code_embed' => 'Code to paste',
+	'label_hauteur' => 'Height',
+	'label_largeur' => 'Width',
+	'lien_preview_embed' => 'Preview rendering',
+
+	// M
+	'message_document_inexistant' => 'This document is not available on the site <a href="@url_site@">@nom_site@</a>.'
+);
+
+?>
---
+++ embed_code/trunk/lang/embed_code_es.php
@@ -0,0 +1,23 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/embed_code?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// I
+	'info_embed_code' => 'Integrar este medio',
+
+	// L
+	'label_code_embed' => 'Código a copiar',
+	'label_hauteur' => 'Altura',
+	'label_largeur' => 'Anchura',
+	'lien_preview_embed' => 'Previsualizar el resultado',
+
+	// M
+	'message_document_inexistant' => 'Este documento no está disponible en el sitio <a href="@url_site@">@nom_site@</a>.'
+);
+
+?>
