bd87a7db2f5356aff668a10c94957cefa6ba5345
chankalan@free.fr <chankalan@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-02-07 08:32:16 +0000

    description - icônes un peu mieux - version - todo.txt
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@69805 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- feuillederoute/lang/paquet-feuillederoute_fr.php
+++ feuillederoute/lang/paquet-feuillederoute_fr.php
@@ -6,7 +6,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// T
 	'feuillederoute_description' => 'Ce plugin ajoute un bouton d\'administration sur le site public pour afficher/masquer un texte simple, hors tout contexte éditorial. Il est sensé être un moyen de communication entre administrateur et rédacteur.
-	 En complément, il peut être pratique d\'utiliser le plugin "Todo" pour la rédaction de tâches à faire, en cours ou exécutées.',
+	 En complément, il peut être pratique d\'utiliser le plugin "Todo" pour la rédaction de tâches à faire, en cours ou exécutées. Il est compatible avec le plugin Minibando.',
 	'feuillederoute_nom' => 'Feuille de Route',
 	'feuillederoute_slogan' => 'Un bouton d\'admin pour partager des informations hors publication.'
 );
