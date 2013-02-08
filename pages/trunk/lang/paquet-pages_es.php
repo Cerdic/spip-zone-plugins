582b8310c8bf6131429e33a3cde7b6c726ba1411
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-02-08 02:41:11 +0000

    langues (paquet-pages)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@69811 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ pages/trunk/lang/paquet-pages_es.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-pages?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// P
+	'pages_description' => 'Este plugin permite crear páginas de artículos que no están asociadas a ninguna jerarquía particular.
+Por contra, pueden asociarse a un nombre de esqueleto. Ello permite crear páginas de aviso legal, acerca de, contacto, etc.',
+	'pages_slogan' => 'Páginas sin sección'
+);
+
+?>
