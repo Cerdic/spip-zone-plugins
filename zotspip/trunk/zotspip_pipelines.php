57b294ba5eb7477971ae171c3b8980e492def6eb
joseph@larmarange.net <joseph@larmarange.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-02-08 20:47:53 +0000

    sécurité
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@69822 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- zotspip/trunk/zotspip_pipelines.php
+++ zotspip/trunk/zotspip_pipelines.php
@@ -1,4 +1,8 @@
 <?php
+
+// Sécurité
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
 function zotspip_insert_head($flux){
 	$flux .= '<link rel="stylesheet" href="'.find_in_path('css/zotspip.css').'" type="text/css" />';
 	$flux .= '<link rel="unapi-server" type="application/xml" title="unAPI" href="'.url_absolue(generer_url_public('zotspip_unapi','source=zotspip')).'" />';
