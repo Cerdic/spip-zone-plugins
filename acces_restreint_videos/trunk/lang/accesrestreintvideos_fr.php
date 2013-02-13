78e96962f646183c95330983e5f651f1146959e8
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-02-13 11:15:45 +0000

    Un début de plugin s'appuyant sur Accès Restreint + oEmbed et qui permet d'insérer des médias restreints à l'intérieur de contenus publics.
    Le but est de pouvoir insérer des vidéos dans des articles publiques, mais qui ne seront visibles que pour les abonnés (enfin ceux qui ont les droits sur une zone).
    
    Avec Dailymotion ou autre, on peut uploader des vidéos mais les configurer pour que celles-ci ne puissent être visionnées QUE sur un site/domaine précis. C'est ce genre de vidéo semi-bloquée que l'on peut alors insérer en tant que vidéo restreinte.
    
    Pour cela le plugin propose un simple modèle <video_restreinte> qui prend les mêmes paramètres que le modèle <oembed>. Essentiellement "url=..."
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@69874 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ acces_restreint_videos/trunk/lang/accesrestreintvideos_fr.php
@@ -0,0 +1,23 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+ 
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'accesrestreintvideos_titre' => 'Accès restreint aux vidéos',
+	
+	// C
+	'configurer_id_zone_defaut_explication' => 'La zone qui sera utilisée par défaut pour restreindre l’accès.',
+	'configurer_id_zone_defaut_label' => 'Zone par défaut',
+	'configurer_texte_restreint_explication' => 'Ce qui sera affiché au visiteur s’il n’a pas accès au contenu.',
+	'configurer_texte_restreint_label' => 'Texte de la restriction',
+	
+	// T
+	'texte_restreint' => 'Ce contenu est restreint.',
+	'texte_restreint_titre' => 'Contenu restreint',
+	'titre_page_configurer_accesrestreintvideos' => 'Configurer l\'accès restreint aux vidéos',
+);
+
+?>
---
+++ acces_restreint_videos/trunk/lang/paquet-accesrestreintvideos_fr.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'accesrestreintvideos_description' => 'Ce plugin fournit un modèle qui permet de restreindre l\'affichage de médias suivant les droits des visiteurs à des zones du plugin \"Accès restreint\".
+
+Il est ainsi possible d\'insérer une vidéo restreinte à l\'intérieur d\'un article qui lui est public.',
+	'accesrestreintvideos_nom' => 'Accès restreint aux vidéos',
+	'accesrestreintvideos_slogan' => 'Restreindre le visionnage de médias',
+);
+
+?>
