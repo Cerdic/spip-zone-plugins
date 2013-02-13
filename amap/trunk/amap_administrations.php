81437a4ba4e03e9b28b9938369a5a46e846fe05f
pierrekuhn82@gmail.com <pierrekuhn82@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-02-13 22:55:47 +0000

    Création des rubriques nécessaire au plugins.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@69885 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- amap/trunk/amap_administrations.php
+++ amap/trunk/amap_administrations.php
@@ -12,21 +12,33 @@
 **/
 
 include_spip('base/amap_tables');
+include_spip('amap_fonctions');
 include_spip('inc/meta');
 include_spip('inc/cextras');
 
-function amap_upgrade($nom_meta_base_version, $version_cible){
+function amap_upgrade($nom_meta_version_base, $version_cible){
 
 	$maj = array();
 	$maj['create'] = array(
 			array('maj_tables', array('spip_amap_disponibles','spip_amap_livraisons','spip_amap_paniers','spip_amap_responsables')),
+			array('amap_rubriques'),
 	);
 	cextras_api_upgrade(amap_declarer_champs_extras(), $maj['create']);
 	include_spip('base/upgrade');
-	maj_plugin($nom_meta_base_version, $version_cible, $maj);
+	maj_plugin($nom_meta_version_base, $version_cible, $maj);
 }
 
-function amap_vider_tables($nom_meta_base_version){
+function amap_rubriques(){
+	create_rubrique("000. Agenda de la saison", "0");
+	$id_rubrique = id_rubrique("000. Agenda de la saison");
+	if ($id_rubrique >0) {
+		create_rubrique("001. Distribution", $id_rubrique);
+		create_rubrique("002. Événements", $id_rubrique);
+	}
+	create_rubrique("001. Archives", "0");
+}
+
+function amap_vider_tables($nom_meta_version_base){
 	//supprimer toutes les tables
 	sql_drop_table('spip_amap_disponibles');
 	sql_drop_table('spip_amap_livraisons');
@@ -34,6 +46,6 @@ function amap_vider_tables($nom_meta_base_version){
 	sql_drop_table('spip_amap_responsables');
 	//suppression des champs supplementaire
 	cextras_api_vider_tables(amap_declarer_champs_extras());
-	effacer_meta($nom_meta_base_version);
+	effacer_meta($nom_meta_version_base);
 }
 ?>
