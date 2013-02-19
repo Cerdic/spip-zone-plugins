3f7976b03e97d925723711630319fff3af390bb0
chankalan@free.fr <chankalan@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-02-19 14:54:33 +0000

    ajout du réglage des autorisations calqué sur Tickets - nécessite SAISIES - version 1.0.0
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@69982 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- feuillederoute/trunk/lang/feuillederoute_fr.php
+++ feuillederoute/trunk/lang/feuillederoute_fr.php
@@ -4,6 +4,19 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 	
+	// C 
+	'cfg_titre_feuillederoute' => 'Configurer la Feuille de Route',
+	'cfg_form_feuillederoute_autorisations' => 'Autorisations',
+	'cfg_lbl_type_autorisation' => 'Méthode d\'autorisation',
+	'cfg_inf_type_autorisation' => 'Choisissez un type d\'autorisations. Pour les choix par Statuts et par Liste d\'auteurs, vous pourrez en choisir plusieurs (ctrl + clic)',
+	'cfg_lbl_autorisation_auteurs' => 'autoriser par liste d\'auteurs',
+	'cfg_lbl_autorisation_statuts' => 'autoriser par statut d\'auteurs',
+	'cfg_lbl_autorisation_webmestre' => 'autoriser uniquement les webmestres',
+	'cfg_lbl_liste_auteurs' => 'Auteurs du site',
+	'cfg_lbl_statuts_auteurs' => 'Statuts possibles',
+	'cfg_lgd_autorisation_lire' => 'Lire la Feuille de route',
+	'cfg_lgd_autorisation_modifier' => 'Modifier la Feuille de route',
+	
 	// F
 	'feuillederoute' => 'Feuille de Route',
 	
