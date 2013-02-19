eb941d3155c41944e9fb410af479533a8bba56c9
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-02-19 21:16:18 +0000

    Une fonction d'upgrade version 3.0 et surtout qui fonctionne pour la version 0.94
    Incrément de version du plugin
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@69994 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- doc2img/trunk/doc2img_administrations.php
+++ doc2img/trunk/doc2img_administrations.php
@@ -16,80 +16,62 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
  * @param $nom_meta_base_version Le nom de la meta d'installation
  * @param $version_cible La version actuelle du plugin
  */
-function doc2img_upgrade($nom_meta_base_version,$version_cible){
-
-	include_spip('inc/meta');
+function doc2img_upgrade($nom_meta_base_version, $version_cible){
 	include_spip('base/abstract_sql');
 	include_spip('base/create');
-	include_spip('inc/config');
 	$current_version = 0.0;
 
-	if ((!isset($GLOBALS['meta'][$nom_meta_base_version]))
-		|| (($current_version = $GLOBALS['meta'][$nom_meta_base_version])!=$version_cible)){
+	$maj = array();
+	
+	// Première installation
+	$maj['create'] = array(
+		array('maj_tables', array('spip_documents')),
+		array('doc2img_creer_config')
+	);
+	
+	$maj['0.3'] = array(
+		array('doc2img_creer_config')
+	);
+	
+	$maj['0.93'] = array(
+		array('doc2img_creer_config')
+	);
+	
+	$maj['0.94s'] = array(
+		array('maj_tables', array('spip_documents')),
+		array('doc2img_creer_config'),
+		array('doc2img_update_to_docs')
+	);
+	
+	include_spip('base/upgrade');
+	maj_plugin($nom_meta_base_version, $version_cible, $maj);
+}
 
-		if (version_compare($current_version,'0.0','<=')){
-			
-			// A la première installation on crée les tables
-			maj_tables('spip_documents');
-
-            // Insertion d'une première configuration
-            if(!is_array(lire_config('doc2img'))){
-	            $cfg = array(
-	                "format_document" => "pdf,bmp,tiff",
-	            	"resolution" => "150",
-	                "repertoire_cible" => "doc2img",
-	                "format_cible" => "png",
-	                "proportion" => "on"
-	            );
-				ecrire_meta('doc2img',serialize($cfg));
-            }
-            
-			if (class_exists('Imagick')) {
-				if(!is_array($formats = lire_config('doc2img_imagick_extensions'))){
-					$imagick = new Imagick();
-					$formats = $imagick->queryFormats();
-					ecrire_meta('doc2img_imagick_extensions',serialize($formats));
-				}
-			}
-
-			ecrire_meta($nom_meta_base_version,$current_version=$version_cible,'non');
-		}
-		if (version_compare($current_version,'0.3','<')){
-            //définition des paramètres de base
-			if(!is_array(lire_config('doc2img'))){
-	            $cfg = array(
-	                "format_document" => "pdf,bmp,tiff",
-	            	"resolution" => "150",
-	                "repertoire_cible" => "doc2img",
-	                "format_cible" => "png",
-	                "proportion" => "on"
-	            );
-				ecrire_meta('doc2img',serialize($cfg));
-			}
-			ecrire_meta($nom_meta_base_version,$current_version='0.3','non');
-		}
-		if (version_compare($current_version,'0.93','<')){
-			if (class_exists('Imagick')) {
-				if(!is_array($formats = lire_config('doc2img_imagick_extensions'))){
-					$imagick = new Imagick();
-					$formats = $imagick->queryFormats();
-					ecrire_meta('doc2img_imagick_extensions',serialize($formats));
-				}
-			}
-			ecrire_meta($nom_meta_base_version,$current_version='0.93','non');
-		}
-		if (version_compare($current_version,'0.94','<')){
-			/**
-			 * Transformation des anciens doc2img en documents normaux
-			 */
-			maj_tables('spip_documents');
-			doc2img_update_to_docs();
-			ecrire_meta($nom_meta_base_version,$current_version='0.94','non');
+function doc2img_creer_config(){
+	include_spip('inc/config');
+	// Insertion d'une première configuration
+    if(!is_array(lire_config('doc2img'))){
+        $cfg = array(
+            "format_document" => "pdf,bmp,tiff",
+        	"resolution" => "150",
+            "repertoire_cible" => "doc2img",
+            "format_cible" => "png",
+            "proportion" => "on"
+        );
+		ecrire_meta('doc2img',serialize($cfg));
+    }
+    
+	if (class_exists('Imagick')) {
+		if(!is_array($formats = lire_config('doc2img_imagick_extensions'))){
+			$imagick = new Imagick();
+			$formats = $imagick->queryFormats();
+			ecrire_meta('doc2img_imagick_extensions',serialize($formats));
 		}
 	}
 }
 
 function doc2img_update_to_docs(){
+	spip_log('on update les docs de doc2img','test');
 	include_spip('inc/documents');
 	include_spip('action/editer_document');
 	$doc2imgs = sql_select('*','spip_doc2img');
@@ -101,19 +83,22 @@ function doc2img_update_to_docs(){
 		$id_document = $doc2img['id_document'];
 		$files = array(array('tmp_name'=>get_spip_doc($doc2img['fichier']),'name'=>basename(get_spip_doc($doc2img['fichier']))));
 		$x = $ajouter_documents('new', $files,'document', $id_document, 'doc2img');
-		if(intval($x)){
+		if(intval(reset($x))){
 			/**
 			 * Si on a un document :
 			 * - on ajoute le numéro de page dans spip_documents 
-			 * - on supprime le document en base
+			 * - on supprime le doc2img en base
 			 * - on supprime le fichier physique
 			 */
-			document_modifier($x,array("page" => $doc2img['page']));
+			document_modifier(reset($x),array("page" => $doc2img['page']));
 			sql_delete('spip_doc2img','id_document='.intval($id_document).' AND fichier='.sql_quote($doc2img['fichier']));
 			spip_unlink(get_spip_doc($doc2img['fichier']));
 		}
-		if (time() >= _TIME_OUT)
+		if (time() >= _TIME_OUT){
+			spip_log('on free et on retourne','test');
+			sql_free($doc2imgs);
 			return;
+		}
 	}
 	sql_drop_table('spip_doc2img');
 }
