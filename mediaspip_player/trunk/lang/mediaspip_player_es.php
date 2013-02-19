bf3d494631774f665908c8d3d562f006eb2ec8b6
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-02-19 02:40:57 +0000

    langues (mediaspip_player)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@69976 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ mediaspip_player/trunk/lang/mediaspip_player_es.php
@@ -0,0 +1,82 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/mediaspip_player?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'alt_cover_fichier' => 'Logo del documento',
+
+	// B
+	'bouton_ajouter_conversion' => 'Añadir una conversión',
+	'bouton_fullscreen' => 'Pasar a modo pantalla completa',
+	'bouton_fullscreen_full' => 'Volver a pantalla normal',
+	'bouton_lire' => 'Leer',
+	'bouton_loading' => 'Cargando',
+	'bouton_loop' => 'Poner en bucle',
+	'bouton_loop_looped' => 'Desactivar el bucle',
+	'bouton_mute' => 'Poner en sordina',
+	'bouton_pause' => 'Pausa',
+	'bouton_seeking' => 'Seeking',
+	'bouton_unmute' => 'Activar el sonido',
+	'bouton_volume' => 'Volumen',
+	'bouton_volume_muted' => 'Volumen (sordina)',
+
+	// C
+	'cfg_titre_html5_player' => 'Lector HTML5',
+
+	// E
+	'erreur_valeur_int' => 'El valor de este número debe ser un número.',
+	'erreur_valeur_int_inf' => 'El valor de este campo debe ser inferior a @nb@.',
+	'erreur_valeur_int_sup' => 'El valor de este campo debe ser superior a @nb@.',
+	'explication_cookie_volume' => 'Guardar el estado del volumen en una cookie con el fin de recuperarlo para cada lector incluso cambiando de página.',
+	'explication_infos_embed' => 'Mostrar un banner indicando el título de la url del sitio en los documentos a insertar.',
+	'explication_slider_volume_orientation' => 'Permite modificar la orientación del deslizador de volumen (depende del tema utilizado en general).',
+	'explication_video_hauteur_embed' => 'Definido por la altura de los videos por defecto utilizada por el código de exportación. ',
+	'explication_video_largeur_embed' => 'Definido por la anchura de los videos por defecto utilizada por el código de exportación.',
+
+	// I
+	'info_ecoule' => 'Tiempo transcurrido',
+	'info_erreur' => 'Error',
+	'info_nb_conversion' => '@nb@ conversiones asociadas al documento',
+	'info_restant' => 'Tiempo restante',
+	'info_streaming' => 'Streaming',
+	'info_total' => 'Duración',
+	'info_une_conversion' => 'Una conversación asociada al documento',
+
+	// L
+	'label_cookie_volume' => 'Registrar el volumen en una cookie',
+	'label_hauteur' => 'Altura',
+	'label_infos_embed' => 'Mostrar el banner en las inserciones',
+	'label_largeur' => 'Anchura',
+	'label_slider_volume_orientation' => 'Orientación del deslizador de volumen',
+	'label_video_hauteur_embed' => 'Altura de los videos para la exportación',
+	'label_video_largeur_embed' => 'Anchura de los videos para la exportación',
+
+	// M
+	'message_erreur_flash' => 'Para ver este media debe instalar o poner en nivel flash player (versión mínima: 10)<br />Descargar la <a href="@url@">última versión aquí</a>.',
+
+	// S
+	'statut_error' => 'Su navegador no parece poder leer este documento. ',
+	'statut_error_broken' => 'El video parece ilegible.',
+	'statut_error_network' => 'Problema de red, inténtelo de nuevo más tarde.',
+	'statut_error_stopped' => 'La carga se ha detenido. ',
+	'statut_loop' => 'En bucle',
+	'statut_mute' => 'Sordina',
+	'statut_pause' => 'Pausa',
+	'statut_play' => 'Lectura',
+	'statut_seek_back' => 'Volver a:',
+	'statut_seek_to' => 'Avanzar a:',
+	'statut_unloop' => 'Bucle desactivado',
+	'statut_unmute' => 'Sonido reactivado',
+	'statut_volume' => 'Volumen',
+
+	// V
+	'valeur_horizontal' => 'Horizontal',
+	'valeur_vertical' => 'Vertical'
+);
+
+?>
