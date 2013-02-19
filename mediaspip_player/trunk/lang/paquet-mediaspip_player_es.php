ce38c261fac373a1243a8c673dde234eb026439b
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-02-19 02:41:01 +0000

    langues (paquet-mediaspip_player)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@69977 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ mediaspip_player/trunk/lang/paquet-mediaspip_player_es.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-mediaspip_player?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// M
+	'mediaspip_player_description' => 'Lector multimedia HTML5 se apoya en las etiquetas audio y video y disponiendo de una solución de repliegue en flash (vía [Flowplayer->http://flowplayer.org]) para navegadores antiguos',
+	'mediaspip_player_nom' => 'Lector de medios de MediaSPIP',
+	'mediaspip_player_slogan' => 'Lector multimedia HTML5 para MediaSPIP'
+);
+
+?>
