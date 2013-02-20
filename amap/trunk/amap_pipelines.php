43d74c3e19b74bf7f1eccad3a28feaadb220ef07
pierrekuhn82@gmail.com <pierrekuhn82@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-02-20 21:45:00 +0000

    Correction dans le prive du plugins pour que les lien et pipelines s'affiche.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@70044 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- amap/trunk/amap_pipelines.php
+++ amap/trunk/amap_pipelines.php
@@ -13,7 +13,7 @@
 
 function amap_affiche_gauche($flux){
 	include_spip('inc/presentation');
-	if ($flux['args']['exec'] == 'auteur_infos'){
+	if ($flux['args']['exec'] == 'infos_perso'){
 		$flux['data'] .= recuperer_fond('prive/inclure/affiche_boite', array('id_auteur'=>$flux['args']['id_auteur']));
 	}
 return $flux;
