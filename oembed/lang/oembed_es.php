770bfc2dfcf4bf5f9fd17e2b0ec29ddc6f4f545d
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-02-20 02:43:17 +0000

    langues (oembed)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@70013 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ oembed/lang/oembed_es.php
@@ -0,0 +1,45 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/oembed?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'aucun_provider' => 'Ningún proveedor',
+
+	// E
+	'erreur_ajout_provider' => 'Error al añadir el proveedor.',
+	'erreur_scheme_doublon' => 'Este esquema ya existe en la lista.',
+	'explication_scheme' => 'utilice * como comodín',
+	'explication_upload_url' => 'Indique simplemente la URL de la página que contiene el documento para los sitios @hosts@',
+
+	// I
+	'info_1_provider' => '1 proveedor',
+	'info_nb_providers' => '@nb@ proveedores',
+
+	// L
+	'label_detecter_lien' => '¿Detectar automáticamente los proveedores (ignorar la lista blanca)?',
+	'label_embed_auto' => '¿Transformar automáticamente las URL insertadas en el texto?',
+	'label_endpoint' => 'Endpoint',
+	'label_maxheight' => 'Altura máxima por defecto (px)',
+	'label_maxwidth' => 'Anchura máxima por defecto (px)',
+	'label_scheme' => 'Esquema',
+
+	// O
+	'ok_ajout_provider' => 'Proveedor añadido',
+
+	// S
+	'supprimer_provider' => 'Eliminar',
+
+	// T
+	'titre_ajouter_provider' => 'Añadir un proveedor',
+	'titre_configuration_avancee_providers' => 'Acceder a la configuración avanzada de los proveedores oEmbed',
+	'titre_configurer_oembed' => 'Configurar oEmbed',
+	'titre_configurer_providers' => 'Proveedores oEmbed',
+	'titre_oembed' => 'oEmbed'
+);
+
+?>
