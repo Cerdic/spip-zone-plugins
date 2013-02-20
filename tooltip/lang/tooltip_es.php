68222f3296cd4cd5139eecf885551c169034a6ca
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-02-20 02:43:37 +0000

    langues (tooltip)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@70019 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ tooltip/lang/tooltip_es.php
@@ -0,0 +1,21 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/tooltip?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// E
+	'explication_selecteur' => 'Indique el objetivo de los elementos que desencadenarán los tooltips (Expresión CSS o extensión jQuery)',
+
+	// L
+	'label_selecteur' => 'Selector',
+
+	// T
+	'titre_menu_tooltips' => 'Tooltips',
+	'titre_page_configurer_tooltips' => 'Configuración de los tooltips'
+);
+
+?>
