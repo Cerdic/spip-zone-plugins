62de97ac077e47d13d5d9dd9ca2d1ddd1714b58c
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-02-20 02:43:09 +0000

    langues (paquet-embed_code)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@70010 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- embed_code/trunk/lang/paquet-embed_code_en.php
+++ embed_code/trunk/lang/paquet-embed_code_en.php
@@ -8,9 +8,9 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// E
-	'embed_code_description' => 'Generate embed code for documents to use on other sites', # MODIF
+	'embed_code_description' => 'Generate embed code for documents to use on other sites',
 	'embed_code_nom' => 'Embed code',
-	'embed_code_slogan' => 'Embed code generation for documents' # MODIF
+	'embed_code_slogan' => 'Embed code generation for documents'
 );
 
 ?>
