0a27306950848eb5e4dce23f356a6236b918c401
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-02-20 02:43:06 +0000

    langues (paquet-doc2img)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@70009 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- doc2img/trunk/lang/paquet-doc2img_en.php
+++ doc2img/trunk/lang/paquet-doc2img_en.php
@@ -1,20 +1,16 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-doc2img
-// Langue: en
-// Date: 03-06-2012 15:51:34
-// Items: 3
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-doc2img?lang_cible=en
+// ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// D
+	// D
 	'doc2img_description' => 'This plugin will convert on the fly pdf, tiff and other kind of documents in image or serie of images. These images can then be used with a specific loop.',
 	'doc2img_nom' => 'Documents to images',
-	'doc2img_slogan' => 'Convert text documents on the fly',
+	'doc2img_slogan' => 'Convert text documents on the fly'
 );
+
 ?>
--- doc2img/trunk/lang/paquet-doc2img_fr.php
+++ doc2img/trunk/lang/paquet-doc2img_fr.php
@@ -1,20 +1,14 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-doc2img
-// Langue: fr
-// Date: 03-06-2012 15:51:34
-// Items: 3
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/doc2img/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// D
+	// D
 	'doc2img_description' => 'Ce plugin permet de convertir à la volée des pdf, tiff et autres types de document en image ou série d\'images. Ces images sont ensuite exploitables via une boucle spécifique.',
 	'doc2img_nom' => 'Documents en Images',
-	'doc2img_slogan' => 'Convertir des documents textuels à la volée',
+	'doc2img_slogan' => 'Convertir des documents textuels à la volée'
 );
+
 ?>
