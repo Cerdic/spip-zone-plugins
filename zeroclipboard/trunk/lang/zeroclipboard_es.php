72561ce73fe4b9d4bb7c0306fd3402cf2ef3ef48
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-02-20 02:43:43 +0000

    langues (zeroclipboard)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@70021 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ zeroclipboard/trunk/lang/zeroclipboard_es.php
@@ -0,0 +1,17 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/zeroclipboard?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// L
+	'link_text_copied' => 'Copiado',
+	'link_text_copy' => 'Copiar',
+	'link_title_copied' => 'El contenido se ha copiado en el portapapeles',
+	'link_title_copy' => 'Copiar en el portapapeles'
+);
+
+?>
