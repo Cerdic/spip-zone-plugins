7ad35f85a429da479656cca0e6ef37b30eb48685
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-02-20 02:43:21 +0000

    langues (polyhier)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@70015 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ polyhierarchie/trunk/lang/polyhier_es.php
@@ -0,0 +1,28 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/polyhier?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'articles_associes' => 'Artículos relacionados',
+
+	// L
+	'label_autre_parent' => 'También en la sección',
+	'label_autres_parents' => 'También en las secciones',
+	'label_parents' => 'En las secciones',
+
+	// P
+	'parent_obligatoire' => 'Debe indicar al menos una sección',
+
+	// R
+	'rubriques_associees' => 'Secciones relacionadas',
+
+	// T
+	'titre_polyhierarchie' => 'Polijerarquía'
+);
+
+?>
