725bf9ca0c3df07f76caaa683a0b58ef548f7dc7
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-02-21 02:41:50 +0000

    langues (paquet-opensearch)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@70056 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ opensearch/lang/paquet-opensearch_es.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-opensearch?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// O
+	'opensearch_description' => 'Plugin que permite proponer a los visitantes de su sitio añadir un motor de búsqueda personalizado para su navegador. ',
+	'opensearch_slogan' => 'Su sitio como motor de búsqueda'
+);
+
+?>
