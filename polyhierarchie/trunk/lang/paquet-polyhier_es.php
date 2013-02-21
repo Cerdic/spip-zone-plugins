599a39596e6038fa7dada42865c30263f70f6fe7
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-02-21 02:41:53 +0000

    langues (paquet-polyhier)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@70057 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ polyhierarchie/trunk/lang/paquet-polyhier_es.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-polyhier?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// P
+	'polyhier_description' => 'Plugin que permite crear enlaces vinculando las secciones y artículos a más secciones que las de su origen directo.',
+	'polyhier_slogan' => 'Vincular un artículo o una sección a varias secciones de la misma familia'
+);
+
+?>
