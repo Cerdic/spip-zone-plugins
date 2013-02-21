607410cd226549459bc5ebfa17a47236b439e26c
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-02-21 02:41:45 +0000

    langues (paquet-licence)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@70054 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ licence/lang/paquet-licence_es.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-licence?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// L
+	'licence_description' => 'Permite unir una licencia de uso a un artículo o a un documento',
+	'licence_nom' => 'Licencia',
+	'licence_slogan' => 'Una Licencia para artículos y documentos'
+);
+
+?>
