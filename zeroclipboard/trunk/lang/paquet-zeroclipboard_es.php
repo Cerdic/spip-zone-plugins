4b13d7d94680b5aeab216c7d678f408ee9247b11
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-02-21 02:42:05 +0000

    langues (paquet-zeroclipboard)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@70062 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ zeroclipboard/trunk/lang/paquet-zeroclipboard_es.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-zeroclipboard?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// Z
+	'zeroclipboard_description' => 'Añade un enlace "Copiar" delante de cada elemento de clase <code>.copypaste</code> permitiendo meter su contenido en el portapapeles',
+	'zeroclipboard_nom' => 'Zeroclipboard',
+	'zeroclipboard_slogan' => 'Copiar pegar fácil'
+);
+
+?>
