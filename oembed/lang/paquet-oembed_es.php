487b61b64d726dd5c8cccda862611d567abe4e31
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-02-21 02:41:49 +0000

    langues (paquet-oembed)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@70055 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ oembed/lang/paquet-oembed_es.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-oembed?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// O
+	'oembed_description' => 'oEmbed cliente/proveedor para SPIP.',
+	'oembed_slogan' => 'oEmbed cliente/proveedor para SPIP'
+);
+
+?>
