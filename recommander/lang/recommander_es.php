24f5787bd415098b04a652e5c7ce7a92c820b7b4
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-02-22 02:43:59 +0000

    langues (recommander)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@70093 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ recommander/lang/recommander_es.php
@@ -0,0 +1,20 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/recommander?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// D
+	'destinataire' => 'Destinatario',
+
+	// R
+	'recommander' => 'Recomendar esta página',
+	'recommander_lecture' => 'Buenos días, @from@ les recomienda la lectura de esta página: ',
+	'recommander_message' => 'Recomendar',
+	'recommander_titre' => 'Leer en @nom_site@ --'
+);
+
+?>
