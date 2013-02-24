f80d154acb4f6b38963116fb693ab81f9294632d
jean-marc.viglino@ign.fr <jean-marc.viglino@ign.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-02-24 10:31:23 +0000

    {MAJ] Ajout d'un formulaire : GEOPORTAIL_FORMULAIRE pour modifier la position d'un objet (article, rubrique ou document) de puis l'espace public.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@70141 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spip_geoportail/balise/geoportail_formulaire.php
+++ spip_geoportail/balise/geoportail_formulaire.php
@@ -15,17 +15,21 @@ if (!defined("_ECRIRE_INC_VERSION")) return;	#securite
 
 function balise_GEOPORTAIL_FORMULAIRE_dist ($p) 
 {
-	return calculer_balise_dynamique($p,'GEOPORTAIL_FORMULAIRE', array($objet,$id));
+	return calculer_balise_dynamique($p,'GEOPORTAIL_FORMULAIRE', array('id_document','id_article','id_rubrique'));
 }
 
 function balise_GEOPORTAIL_FORMULAIRE_stat($args, $filtres) 
 {
-	if (!$args[0]) return "GEOPORTAIL_FORMULAIRE en dehors d'une boucle objet.";
+	if ($args[0]) $args = array_merge(array('document',$args[0]), array_slice($args,3));
+	elseif ($args[1]) $args = array_merge(array('article',$args[1]), array_slice($args,3));
+	elseif ($args[2]) $args = array_merge(array('rubrique',$args[2]), array_slice($args,3));
+	else return "GEOPORTAIL_FORMULAIRE en dehors d'une boucle objet.";
 	return $args;
 }
 
 function balise_GEOPORTAIL_FORMULAIRE_dyn($objet='', $id_objet='', $titre='', $deplier=false) 
-{	return array(
+{	
+	return array(
        'formulaires/geoportail_formulaire',
        0,
        array(
