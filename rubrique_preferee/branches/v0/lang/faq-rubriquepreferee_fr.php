39f2bcd683571acc44b990cbabfca11ac54af46f
brunobergot@gmail.com <brunobergot@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-02-24 15:21:21 +0000

    et hop création de la branche v0 (pour SPIP 2)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@70148 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ rubrique_preferee/branches/v0/lang/faq-rubriquepreferee_fr.php
@@ -0,0 +1,12 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	'choisir_q' => 'Comment définir ma rubrique préférée ?',
+	'choisir' => 'Pour définir votre rubrique préférée (qui sera la rubrique par défaut lors de la création d\'un nouvel article depuis la page d\'accueil), choisissez-la depuis votre page auteur.',
+
+
+);
+?>
