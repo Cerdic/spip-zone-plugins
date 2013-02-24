fd44ae16e5d11ce296d2c3a516160be9f62567aa
toutati@free.fr <toutati@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-02-24 22:26:29 +0000

    On change de nom, le docker pour importer des docs distants suffit bien!
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@70173 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ docker/lang/docker_fr.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/docker/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+'docker_description'=>'Importe et copie localement les documents distants qui sont déjà dans votre SPIP.',
+'entrer_id_rubrique'=>'Laisser vide ou entrer un numéro de rubrique pour traiter ses documents',
+'aucun_document_distant'=>'Il n\'existe aucun document distant à importer!',
+'id_rubrique'=>'Numéro de la rubrique (option)',
+'docker_lancer_importation'=>'Lancer l\'importation',
+);
+
+?>
