2f79514fc4fb900b622566d9b29aa147c7818154
suske@brubel.net <suske@brubel.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-02-27 10:02:50 +0000

    ] sur le lien de la description
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@70228 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- jquery_menu_superfish/lang/paquet-jquerysuperfish_fr.php
+++ jquery_menu_superfish/lang/paquet-jquerysuperfish_fr.php
@@ -13,7 +13,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 // J
-	'jquerysuperfish_description' => 'Une adaptation pour Spip du plugin [jQuery Superfish->http://users.tpg.com.au/j_birch/plugins/superfish/ à partir d\'une structure ul/li générée par exemple par le plugin [Menus->http://www.spip-contrib.net/Menus,3139]',
+	'jquerysuperfish_description' => 'Une adaptation pour Spip du plugin [jQuery Superfish->http://users.tpg.com.au/j_birch/plugins/superfish/] à partir d\'une structure ul/li générée par exemple par le plugin [Menus->http://www.spip-contrib.net/Menus,3139]',
 	'jquerysuperfish_slogan' => 'Affiche un menu déroulant',
 );
 ?>
