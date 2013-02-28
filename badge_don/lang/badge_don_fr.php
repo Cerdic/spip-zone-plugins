622a5784a1d20d12c74a7c598a2d8f9f838c982b
brunobergot@gmail.com <brunobergot@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-02-28 10:43:51 +0000

    Version 0.2.0 : permettre d'utiliser des raccourcis de type SPIP (art23 ou rub32) pour l'url de la page de don
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@70270 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- badge_don/lang/badge_don_fr.php
+++ badge_don/lang/badge_don_fr.php
@@ -4,6 +4,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
+	'explication_url_don' => 'URL ou raccourci de type SPIP (art23 ou rub32 par exemple).',
 	'label_url_don' => 'Adresse de la page de dons',
 	'titre_badge_don' => 'Badge pour dons',
 	'titre_lien' => 'Soutenir par un don'
