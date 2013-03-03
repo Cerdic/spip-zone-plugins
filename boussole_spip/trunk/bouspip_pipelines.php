93c0afb2376d2df403f1ffaa3c927fc4abc04728
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-03-03 09:25:17 +0000

    Correction d'items de langue (supprimer le &) Changement de format du tableau des boussoles disponibles
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@70345 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- boussole_spip/trunk/bouspip_pipelines.php
+++ boussole_spip/trunk/bouspip_pipelines.php
@@ -10,7 +10,7 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
  * @return $flux
  */
 function bouspip_declarer_boussoles($flux) {
-	$flux['spip'] = "bouspip";
+	$flux['spip'] = array('serveur' => 'spip', 'prefixe' => 'bouspip');
 	return $flux;
 }
 
