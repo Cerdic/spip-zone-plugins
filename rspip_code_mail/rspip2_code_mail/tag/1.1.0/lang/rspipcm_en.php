6ccf41a20c70a6a99844f2b03bdb75cb494380f9
robert.sebille@cassiopea.org <robert.sebille@cassiopea.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-03-03 12:52:59 +0000

    Stabilisation 1.1.0 (Support des forums)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@70352 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ rspip_code_mail/rspip2_code_mail/tag/1.1.0/lang/rspipcm_en.php
@@ -0,0 +1,25 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+   // PAS DE CHAINE VIDE. Laisser au moins un espace.
+   
+   //D
+   "dans_le_domaine" => "in the domain",
+
+   //E
+   // Usage sample: "ecrire_a_post" => " and after it,", "ecrire_a_pre" => "Before ", will be written 
+   // Before <a name> and after it, [...]
+   "ecrire_a_post" => " ",
+   "ecrire_a_pre" => "Write to",
+   // Pour javascript: pas d'entites html.
+   "entrez_resultat_addition" => "Antispam. How much is",
+   // Pour javascript: pas d'entites html.
+   "erreur_entrez_resultat_addition" => "Mistake! How much is",
+   
+   // final
+   "pour_les_distraits" => "Pas de virgule a la fin du tableau"
+
+   );
+     
+?>
---
+++ rspip_code_mail/rspip3_code_mail/tag/1.1.0/lang/rspipcm_fr.php
@@ -0,0 +1,25 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+   // PAS DE CHAINE VIDE. Laisser au moins un espace.
+
+   //D
+   "dans_le_domaine" => "dans le domaine",
+
+   //E
+   // Exemple d'usage: "ecrire_a_post" => " et apres lui,", "ecrire_a_pre" => "Avant ", sera ecrit 
+   // Avant <un nom> et apres lui, [...]
+   "ecrire_a_post" => " ",
+   "ecrire_a_pre" => "Ecrire &agrave;",
+   // Pour javascript: pas d'entites html.
+   "entrez_resultat_addition" => "Antispam. Combien vaut",
+   // Pour javascript: pas d'entites html.
+   "erreur_entrez_resultat_addition" => "Erreur! Combien vaut",
+   
+   // final
+   "pour_les_distraits" => "Pas de virgule a la fin du tableau"
+
+   );
+     
+?>
