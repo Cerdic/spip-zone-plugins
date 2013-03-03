1f1cfe0facc40d526bfb5c41fabba5f91e584153
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-03-03 10:23:00 +0000

    Amélioration de la fonction de conversion xml en tableau
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@70347 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- boussole/trunk/inc/simplexml_to_array.php
+++ boussole/trunk/inc/simplexml_to_array.php
@@ -17,14 +17,14 @@ function inc_simplexml_to_array_dist($obj, $utiliser_namespace='false') {
 
 	// Cette fonction getDocNamespaces() est longue sur de gros xml. On permet donc
 	// de l'activer ou pas suivant le contenu supposé du XML
-	if ($obj) {
+	if ($obj AND is_object($obj)) {
 		if ($utiliser_namespace)
 			$namespace = $obj->getDocNamespaces(true);
 		$namespace[NULL] = NULL;
 
 		$name = strtolower((string)$obj->getName());
 		$text = trim((string)$obj);
-		if( strlen($text) <= 0 ) {
+		if (strlen($text) <= 0) {
 			$text = NULL;
 		}
 
@@ -32,28 +32,26 @@ function inc_simplexml_to_array_dist($obj, $utiliser_namespace='false') {
 		$attributes = array();
 
 		// get info for all namespaces
-		if (is_object($obj)) {
-			foreach( $namespace as $ns=>$nsUrl ) {
-				// atributes
-				$objAttributes = $obj->attributes($ns, true);
-				foreach( $objAttributes as $attributeName => $attributeValue ) {
-					$attribName = strtolower(trim((string)$attributeName));
-					$attribVal = trim((string)$attributeValue);
-					if (!empty($ns)) {
-						$attribName = $ns . ':' . $attribName;
-					}
-					$attributes[$attribName] = $attribVal;
+		foreach( $namespace as $ns=>$nsUrl ) {
+			// attributes
+			$objAttributes = $obj->attributes($ns, true);
+			foreach( $objAttributes as $attributeName => $attributeValue ) {
+				$attribName = strtolower(trim((string)$attributeName));
+				$attribVal = trim((string)$attributeValue);
+				if (!empty($ns)) {
+					$attribName = $ns . ':' . $attribName;
 				}
+				$attributes[$attribName] = $attribVal;
+			}
 
-				// children
-				$objChildren = $obj->children($ns, true);
-				foreach( $objChildren as $childName=>$child ) {
-					$childName = strtolower((string)$childName);
-					if( !empty($ns) ) {
-						$childName = $ns.':'.$childName;
-					}
-					$children[$childName][] = inc_simplexml_to_array_dist($child);
+			// children
+			$objChildren = $obj->children($ns, true);
+			foreach( $objChildren as $childName=>$child ) {
+				$childName = strtolower((string)$childName);
+				if( !empty($ns) ) {
+					$childName = $ns.':'.$childName;
 				}
+				$children[$childName][] = inc_simplexml_to_array_dist($child);
 			}
 		}
 
