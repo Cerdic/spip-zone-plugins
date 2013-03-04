3912c49ba45d28c5e2e3dbc7bce1e23167a4a6f8
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-03-04 02:43:31 +0000

    langues (paquet-doc2img)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@70370 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ doc2img/trunk/lang/paquet-doc2img_es.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-doc2img?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// D
+	'doc2img_description' => 'Este plugin permite convertir sobre la marcha los pdf, tiff y otros tipos de documento en imagen o serie de imágenes. Estas imágenes pueden explotarse a través de un bucle específico.',
+	'doc2img_nom' => 'Documentos en Imágenes',
+	'doc2img_slogan' => 'Convertir los documentos de texto sobre la marcha'
+);
+
+?>
