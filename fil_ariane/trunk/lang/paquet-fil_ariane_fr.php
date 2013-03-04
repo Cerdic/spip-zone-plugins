c979b3777e19852f9ea2db82526fbc65c68dff8f
cyril@cym.fr <cyril@cym.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-03-04 17:26:49 +0000

    on supprime toute référence à une base de données (fil_ariane n'a pas de base);
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@70391 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- fil_ariane/trunk/lang/paquet-fil_ariane_fr.php
+++ fil_ariane/trunk/lang/paquet-fil_ariane_fr.php
@@ -5,7 +5,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-	'fil_ariane_slogan'         => "Une balise pour faire des fils d'Ariane",
+	'fil_ariane_slogan'         => "Une balise pour faire de jolis fils d'Ariane",
 	'fil_ariane_description'    => "Ce fil d'Ariane ultra-simple cherche tout seul l'arborescence des objets éditoriaux de SPIP.",
 );
 
