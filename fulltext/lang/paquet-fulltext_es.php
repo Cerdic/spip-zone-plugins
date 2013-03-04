e1c97bd275a453d2e703fed209afd13c53d3baa9
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-03-04 02:43:34 +0000

    langues (paquet-fulltext)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@70371 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ fulltext/lang/paquet-fulltext_es.php
@@ -0,0 +1,17 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-fulltext?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// F
+	'fulltext_description' => 'Este plugin permite por un lado explotar el modo de búsqueda FULLTEXT de MySQL y mejorar así en gran medida las búsquedas por informe en el funcionamiento original de SPIP, y por otro lado indexar el contenido de ciertos documentos.
+_ Para un funcionamiento óptimo, ciertos programas complementarios son necesarios en el servidor y deben estar parametrados en <code>mes_options.php</code> o a través del panel de configuración.
+_ Ver la documentación del plugin para más detalles. ',
+	'fulltext_slogan' => 'Indexación FULLTEXT para agilizar las búsquedas'
+);
+
+?>
