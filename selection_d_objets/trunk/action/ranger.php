0dce155247ecef5531b76f11c0b2eff4361a83e8
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-03-06 12:12:00 +0000

    - nettoyer les rangements- faire fonctionner quanf pas d'objet présent; utiliser d'avantage l'id_selection_objet - le script du drag and drop mis dans selection_interface pour eviter un bubbling
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@70467 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- selection_d_objets/trunk/action/ranger.php
+++ selection_d_objets/trunk/action/ranger.php
@@ -11,38 +11,14 @@ function action_ranger_dist($arg=null){
 	include_spip("inc/autoriser");
 	include_spip("inc/config");
 
-	list($action,$lang,$id_selection_objet,$objet_dest,$id_objet_dest,$load,$nouvel_ordre)=explode('-',$arg);
+	list($action,$lang,$id_selection_objet,$ordre,$objet_dest,$id_objet_dest,$load)=explode('-',$arg);
+    $load=_request('load');
 
     switch($action){
         case 'supprimer_ordre' :
 
             include_spip('formulaires/bouton_article');
-    
-            if($objet=='rubrique'){
-            
-                $langues=explode(",",lire_config("langues_utilisees"));
-            
-                foreach ($langues as $key => $langue){
-                
-                    $where=array(
-                        'id_selection_objet='.$id_selection_objet,                                             
-                        );
-                                
-                    sql_delete("spip_selection_objets",$where);
-                        
-                    // on vérifie l'ordre des objets déjà enregistrés et on corrige si beselection_objetin
-                    
-                    $where = array(
-                    'id_objet_dest='.$id_objet_dest,
-                    'objet_dest='.sql_quote($objet_dest),
-                    'lang='.sql_quote($lang)
-                    );
-                    
-                    $ordre=$verifier_ordre($where); 
-                    }
-                }
-            else{
-            
+
             spip_log('eliminer 1','selection');
                 $where=array(
                     'id_selection_objet='.$id_selection_objet,
@@ -59,104 +35,70 @@ function action_ranger_dist($arg=null){
                     );
                     
                 $ordre=$verifier_ordre($where); 
-                }
+                
             break;
+            
         case 'remonter_ordre':
             $where = array(             
             'lang="'.$lang.'"',
             'objet_dest="'.$objet_dest.'"',
-            'id_objet_dest="'.$id_objet_dest.'"',                   
+            'id_objet_dest="'.$id_objet_dest.'"',  
+            'ordre<="'.$ordre.'"',                            
                 );
         
-            $result = sql_select("*", "spip_selection_objets", $where, "ordre");
+            $result = sql_select("*", "spip_selection_objets", $where,'', "ordre DESC",2);
             
             while ($row = sql_fetch($result)) {
-                $id_selection_objet_row = $row["id_selection_objet"];        
-                $ordre_row = $row["ordre"];
-                $lang_row = $row["lang"];       
-                if ($id_selection_objet  == $id_selection_objet_row) break;
-                $ordre_new = $ordre_row;
-                $id_selection_objet_prec = $id_selection_objet_row;       
-            
-            }
-
-            $where = array(             
-                    "id_selection_objet='$id_selection_objet'",      
-                    );
-                    
-    
-            spip_log('action '.$action.serialize($where).$ordre_new,'selecion_objet');
-                
-            sql_updateq("spip_selection_objets", array("ordre" => $ordre_new), $where);
-            
-            $where = array(             
-                    "id_selection_objet='$id_selection_objet_prec'",       
-                    );      
-                    
-            spip_log('action '.$action.serialize($where).$ordre_row,'selecion_objet');  
-            
-            sql_updateq("spip_selection_objets", array("ordre" => $ordre_row), $where);
-            break;
-                case 'descendre_ordre': 
-                $where = array(             
-                'lang="'.$lang.'"',
-                'objet_dest="'.$objet_dest.'"',
-                'id_objet_dest="'.$id_objet_dest.'"',
-                "id_objet='$id_objet'", 
-                "objet='$objet'",                       
-                );
-        
-    
-            $result = sql_select("*", "spip_selection_objets",$where, "ordre");
-            
-            if ($row = sql_fetch($result)) {
-    
-                $ordre = $row["ordre"];
-                
-                $where = array(             
-                    'lang="'.$lang.'"',
-                    'objet_dest="'.$objet_dest.'"',
-                    'id_objet_dest="'.$id_objet_dest.'"',
-                    'ordre>"'.$ordre.'"',                   
-                    );
-                
-                $result2 = sql_select("*", "spip_selection_objets",$where, "ordre LIMIT 0,1");
-                
-                    if ($row2 = sql_fetch($result2)) {
-                        $ordre_suiv = $row2["ordre"];
-                        $id_objet_suiv = $row2["id_objet"];
-                        $objet_suiv = $row2["objet"];                   
-                        
-                        $where = array(             
-                            "lang='$lang'",
-                            "objet_dest='$objet_dest'",
-                            "id_objet_dest='$id_objet_dest'",
-                            "id_objet='$id_objet'", 
-                            "objet='$objet'",       
-                            );
-                        
-    
-                        sql_updateq("spip_selection_objets", array("ordre" => $ordre_suiv),$where);
-    
-                        
-                        $where = array(             
-                            "lang='$lang'",
-                            "objet_dest='$objet_dest'",
-                            "id_objet_dest='$id_objet_dest'",
-                            "id_objet='$id_objet_suiv'",    
-                            "objet='$objet_suiv'",      
-                            );
-                        
-                        sql_updateq("spip_selection_objets", array("ordre" => $ordre),$where);
-                        }
+                if($id_selection_objet==$row["id_selection_objet"]){
+                    $o =$ordre-1;
                 }
+                else{
+                    $o =$ordre;
+                }
+            sql_updateq("spip_selection_objets", array("ordre" =>$o),'id_selection_objet='.$row['id_selection_objet']); 
+            }
+                 $where = array(
+                    'id_objet_dest='.$id_objet_dest,
+                    'objet_dest='.sql_quote($objet_dest),
+                    'lang='.sql_quote($lang)    
+                    );            
+        $ordre=$verifier_ordre($where);     
             break;
+            
+        case 'descendre_ordre': 
+            $where = array(             
+            'lang="'.$lang.'"',
+            'objet_dest="'.$objet_dest.'"',
+            'id_objet_dest="'.$id_objet_dest.'"',  
+            'ordre>="'.$ordre.'"',                            
+                );
+
+            $result = sql_select("*", "spip_selection_objets", $where,'', "ordre",2);
+            
+            
+            while ($row = sql_fetch($result)) {
+                if($id_selection_objet==$row["id_selection_objet"]){
+                    $o =$ordre+1;
+                }
+                else{
+                    $o =$ordre;
+                }
+            sql_updateq("spip_selection_objets", array("ordre" =>$o),'id_selection_objet='.$row['id_selection_objet']); 
+            }
+                 $where = array(
+                    'id_objet_dest='.$id_objet_dest,
+                    'objet_dest='.sql_quote($objet_dest),
+                    'lang='.sql_quote($lang)    
+                    );            
+        $ordre=$verifier_ordre($where);  
+            break;
+        //drag&drop
         case 'nouvel_ordre':
             $nouvel_ordre=explode(',',_request('nouvel_ordre'));
             $ordre=0;
-            foreach($nouvel_ordre AS $id_objet){
+            foreach($nouvel_ordre AS $id_selection_objet){
                 $ordre++;
-                sql_updateq("spip_selection_objets", array("ordre" => $ordre),'id_objet='.$id_objet);
+                sql_updateq("spip_selection_objets", array("ordre" => $ordre),'id_selection_objet='.$id_selection_objet);
                 include_spip('inc/invalideur');
                 suivre_invalideur("id='selection_objet/$id_selection_objet'");    
             }
@@ -164,8 +106,8 @@ function action_ranger_dist($arg=null){
         }
 
     if($load){
-       $contexte = array('id_objet_dest'=>$id_objet_dest,'objet_dest'=>$objet_dest,'l'=>$lang);
-        echo recuperer_fond('prive/objets/liste/inc-selection_interface',$contexte);
+        $contexte = array('id_objet_dest'=>$id_objet_dest,'objet_dest'=>$objet_dest,'l'=>$lang);
+        echo recuperer_fond($load,$contexte);
     } 
 return $return;
 }
