a2ab33d7af4e914d0697b53214e70e8af872f388
serge.markitanenko@gmail.com <serge.markitanenko@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-03-08 02:42:29 +0000

    langues (paquet-zeroclipboard)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@70512 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ zeroclipboard/trunk/lang/paquet-zeroclipboard_ru.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-zeroclipboard?lang_cible=ru
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// Z
+	'zeroclipboard_description' => 'Добавляет ссылку  "Копировать" перед каждым элементом страницы с классом <code>.copypaste</code> и при нажатии на нее добавляет содержимое в буфер обмена компьютера (Ctrl-C)',
+	'zeroclipboard_nom' => 'Zeroclipboard',
+	'zeroclipboard_slogan' => 'Позволяет создать ссылку "Скопировать" на сайте'
+);
+
+?>
