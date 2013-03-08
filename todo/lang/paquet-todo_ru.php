d3c58f946ec73b7b1f391ce595771c27c08ee670
serge.markitanenko@gmail.com <serge.markitanenko@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-03-08 02:42:25 +0000

    langues (paquet-todo)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@70511 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ todo/lang/paquet-todo_ru.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-todo?lang_cible=ru
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// T
+	'todo_description' => 'Добавляет новые символы для форматирования текста, которыми удобно составлять списки дел и задачю',
+	'todo_slogan' => 'Новые элементы форматирования текста в редакторе'
+);
+
+?>
