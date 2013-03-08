caa0848a815237e2eee6f1bf66945e1b6e08efcd
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-03-08 15:39:37 +0000

    Un miniplugin qui ajoute un fichier Sitemap dédié aux actualités, afin de ne pas avoir à modifier salement nos supers URLs pour être accepté par Google Actualités.
    Ça liste donc les articles moins vieux que 2 jours, et pas plus de 1000 dans la liste. Et ça surcharge le robots.txt par défaut pour déclarer le Sitemap supplémentaire en plus du premier déjà fourni par SPIP.
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@70522 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ sitemap_news/trunk/lang/paquet-sitemap_news_fr.php
@@ -0,0 +1,10 @@
+<?php
+
+// Sécurité
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	'sitemap_news_description' => 'Pour ne pas avoir à changer ses URLs dans un format précis, et pour être indexé plus facilement dans Google News, il est préférable de fournir à ce moteur de recherche un fichier Sitemap dédié aux actualités, dont la documentation est ici : http://support.google.com/news/publisher/bin/answer.py?hl=fr&answer=74288',
+	'sitemap_news_nom' => 'Sitemap d’actualités',
+	'sitemap_news_slogan' => 'Fourni un fichier Sitemap dédié aux actualités du site',
+);
