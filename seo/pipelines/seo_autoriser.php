f6d214985946a9082263a5242200ef99dc9d0a8a
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-03-09 17:09:11 +0000

    simplifications
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@70549 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- seo/pipelines/seo_autoriser.php
+++ seo/pipelines/seo_autoriser.php
@@ -8,9 +8,6 @@ function autoriser_seo_bouton_dist($faire, $type, $id,  $qui, $opt){
 	global $connect_statut, $connect_toutes_rubriques;
 
 	// seul les administrateurs globaux ont acces au bouton de configuration
-	if($connect_statut 	&& $connect_toutes_rubriques){
-		return true;
-	}	
-	return false;
+	return $connect_statut 	&& $connect_toutes_rubriques;
 }
 ?>
