08632a995e55b605a944452ea56f0843e297c13f
ydikoi@me.com <ydikoi@me.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-03-09 09:40:25 +0000

    Modification de paquet.xml (crédit vignette) et ajout de slogan & description dans le module de langue
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@70539 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ timeline_blog/lang/paquet-timeline_blog_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/noizetier/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// N
+	'timeline_blog_description' => 'Le plugin timelime fournit un squelette de blog pour zpip, organisé autour de dates. En un clic, on peut afficher les commentaires et formulaire de réponse, qui sont par défaut cachés.',
+	'timeline_blog_nom' => 'Timeline_blog',
+	'Timeline_blog_slogan' => 'Squelette de blog pour Zpip'
+);
+
+?>
