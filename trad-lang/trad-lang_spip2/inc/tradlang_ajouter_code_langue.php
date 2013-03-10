e2b1904d93c1593379484d4758af32b8f00dde40
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-03-10 13:43:23 +0000

    Un peu de phpdoc
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@70558 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- trad-lang/trad-lang_spip2/inc/tradlang_ajouter_code_langue.php
+++ trad-lang/trad-lang_spip2/inc/tradlang_ajouter_code_langue.php
@@ -2,12 +2,34 @@
 
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
+/**
+ * Fonction d'ajout de création d'une langue pour un module
+ * 
+ * Par exemple, l'activation de la langue italienne (it) pour le module "ecrire"
+ * dupliquera les chaînes du module "ecrire" dans sa langue originale ("fr" par exemple)
+ * tout en mettant le statut "NEW"
+ * 
+ * @param array $module
+ * 		Les informations du module en base (on a besoin des champs "module","lang_mere")
+ * @param string $lang
+ * 		La langue dans laquelle on souhaite créer la nouvelle version 
+ */
 function inc_tradlang_ajouter_code_langue($module,$lang){
 	/**
-	 * Insertion des chaines de la langue mère avec le statut NEW
+	 * Sélection des chaînes de la langue mère du module
 	 */
 	$chaines_mere = sql_select('*','spip_tradlangs',"module=".sql_quote($module['module'])." AND lang=".sql_quote($module['lang_mere']));
 	while($chaine = sql_fetch($chaines_mere)){
+		/**
+		 * Insertion en base :
+		 * - On crée un titre qui doit être unique
+		 * - On change la langue avec le $lang passé en paramètre
+		 * - On vide les traducteurs
+		 * - On recrée le md5
+		 * - On met la date_modif à tout de suite
+		 * - On met langue_choisie à "oui"
+		 * - On vire "maj" et "id_tradlang" qui sont des champs automatiquement incrémentés 
+		 */
 		$chaine['titre'] = $chaine['id'].' : '.$chaine['module'].' - '.$lang;
 		$chaine['lang'] = $lang;
 		$chaine['statut'] = 'NEW';
@@ -28,12 +50,10 @@ function inc_tradlang_ajouter_code_langue($module,$lang){
 	suivre_invalideur('1');
 	
 	/**
-	 * On génère le fichier correspondant
+	 * On génère le fichier correspondant si la configuration de tradlang le demande
 	 */
-	$config = @unserialize($GLOBALS['meta']['tradlang']);
-	if (!is_array($config))
-		return;
-	if(($config['sauvegarde_locale'] == 'on') && ($config['sauvegarde_post_edition'] == 'on')){
+	include_spip('inc/config');
+	if((lire_config('tradlang/sauvegarde_locale') == 'on') && (lire_config('tradlang/sauvegarde_post_edition') == 'on')){
 		include_spip('tradlang_fonctions');
 		if($dir_lang = tradlang_dir_lang()){
 			$sauvegarder_module = charger_fonction('tradlang_sauvegarde_module','inc');
