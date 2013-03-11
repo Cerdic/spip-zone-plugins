3d1aa8e6b9013d87494eda3fea35aefe94f15bee
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-03-11 16:43:34 +0000

    Pétouille de commentaires
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@70605 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- boussole/trunk/boussole_options.php
+++ boussole/trunk/boussole_options.php
@@ -6,11 +6,12 @@ if (!defined('_BOUSSOLE_ALIAS_SERVEUR'))
 	define('_BOUSSOLE_ALIAS_SERVEUR', '');
 
 // Liste des serveurs disponibles pour le client du plugin Boussole
-// -- Par défaut, le serveur de la Galaxie SPIP est disponible
+// -- Par défaut, le serveur de la Galaxie SPIP est toujours disponible
 $GLOBALS['client_serveurs_disponibles']['spip'] = array(
 	'api' => 'http://boussole.spip.net/spip.php?action=[action][arguments]');
 
 // Liste des boussoles "manuelles" (non fournies sous forme de plugin) disponibles sur le serveur
+// -- par défaut le serveur n'en contient aucune
 isset($GLOBALS['serveur_boussoles_disponibles']) AND $GLOBALS['serveur_boussoles_disponibles']
 	? $GLOBALS['serveur_boussoles_disponibles']
 	: array();
