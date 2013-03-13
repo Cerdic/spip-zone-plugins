f7d01154a7381576585bc449b4ad49653b3b7168
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-03-13 11:00:18 +0000

    langues (paquet-bouspip)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@70650 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- boussole_spip/trunk/lang/paquet-bouspip_fr.php
+++ boussole_spip/trunk/lang/paquet-bouspip_fr.php
@@ -1,5 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/boussole_spip/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
