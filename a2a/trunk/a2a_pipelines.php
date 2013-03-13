80d9422a8f22f4b2b029a780aac363d8615ff450
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-03-13 14:54:24 +0000

    Ne pas afficher le bloc sur ?exec=articles si pas de id_article passé en paramètre
    Un peu de phpdoc
    
    version 2.0.3
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@70678 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- a2a/trunk/a2a_pipelines.php
+++ a2a/trunk/a2a_pipelines.php
@@ -1,14 +1,32 @@
 <?php
-
+/**
+ * Plugin A2A
+ * 
+ * Fichier des utilisations de pipelines du plugin
+ * 
+ * @package SPIP\A2A\Pipelines
+ */
+ 
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
+/**
+ * Insertion dans le pipeline affiche_milieu (SPIP)
+ * 
+ * Affiche le bloc d'association d'articles entre eux sur la page de visualisation d'article
+ * dans l'espache privé
+ * 
+ * Ajoute la liste des articles liés et le formulaire de liaison dans $flux['data']
+ * 
+ * @param array $flux
+ * 		Le contexte du pipeline
+ * @param array $flux
+ * 		Le contexte du pipeline modifié si besoin
+ */
 function a2a_affiche_milieu($flux){	
-	$contexte = array();
-
-	if (($flux['args']['exec'] == "articles") || ($flux['args']['exec'] == "article")){
-	
+	if (($flux['args']['exec'] == "articles" && isset($flux['args']['id_article']) && $flux['args']['id_article'] > 0) || ($flux['args']['exec'] == "article")){
+		$contexte = array();
 		$contexte['id_article_orig'] = $flux["args"]["id_article"];		
-        $contexte['formulaire']=_request('formulaire');
+        $contexte['formulaire'] = _request('formulaire');
 		$texte = recuperer_fond('prive/contenu/a2a_article', $contexte, array('ajax'=>true));
 		
 		if (($p = strpos($flux['data'],'<!--affiche_milieu-->'))!==false)
@@ -17,7 +35,6 @@ function a2a_affiche_milieu($flux){
 			$flux['data'] .= $texte;
 	}
 	return $flux;
-	
 }
 
 ?>
