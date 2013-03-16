c281b105cbfb093ac5aa270741da97cfd7142aba
marcimat@rezo.net <marcimat@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-03-16 02:42:33 +0000

    langues (paquet-boussole)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@70790 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ boussole/trunk/lang/paquet-boussole_eo.php
@@ -0,0 +1,17 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-boussole?lang_cible=eo
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'boussole_description' => 'Instalu tiun kromprogramon sur vian retejon kaj vi aliros, en viaj skeletoj, la tuton de la retejoj de la SPIP-Galakcio uzante la ŝablonojn, la nuksetojn kaj la Z-paĝo proponita.
+_ Memfarantoj, vi povas uzi ankaŭ tiun kromprogramon por afiŝi vian propran retejliston kaj koncepti viajn proprajn afiŝadojn.',
+	'boussole_nom' => 'Kompaso',
+	'boussole_slogan' => 'La taŭgaj adresoj de la SPIP-galakcio aŭ aliloke !'
+);
+
+?>
