90ebdcdc46903680a02324caac8c9e0a50a286d6
passe2048-vague@yahoo.fr <passe2048-vague@yahoo.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-03-16 15:00:56 +0000

    fausse manip, mille excuses
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@70826 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- adminer/boutons_admin_supp/lang/basupp_ar.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
-$GLOBALS[$GLOBALS['idx_lang']] = array(
-    'editer' => '&#1578;&#1581;&#1585;&#1610;&#1585;',
-    );
-?>
--- adminer/boutons_admin_supp/lang/basupp_it.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
-$GLOBALS[$GLOBALS['idx_lang']] = array(
-	'editer' => 'Modifica',
-   );
-?>
