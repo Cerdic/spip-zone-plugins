249e4b8fcd15ec049ff0e7881e3d3fd8dedf4557
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-03-17 02:42:42 +0000

    langues (langonet)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@70849 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- langonet/branches/v0/lang/langonet_fr.php
+++ langonet/branches/v0/lang/langonet_fr.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/langonet/lang/
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/langonet/branches/v0/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
