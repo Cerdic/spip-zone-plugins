3b387c6d8c35ce1a82c3155ca3504eb0998adb7f
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-03-18 02:48:15 +0000

    langues (paquet-getid3)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@70861 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ getID3/trunk/lang/paquet-getid3_es.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-getid3?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// G
+	'getid3_description' => 'Leer y escribir las etiquetas audio en los documentos de tipo audio
+_ [->http://getid3.sourceforge.net/]',
+	'getid3_slogan' => 'Leer y escribir las etiquetas audio'
+);
+
+?>
