68656312219a164c4c99a4199732671d19269977
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-03-19 02:42:40 +0000

    langues (inscription3)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@70888 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ inscription/trunk/lang/inscription3_es.php
@@ -0,0 +1,281 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/inscription3?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'a_confirmer' => 'Para confirmar',
+	'activation_compte' => 'Active su cuenta',
+	'admin' => 'Admin',
+	'afficher_tous' => 'Mostrar todos los usuarios',
+	'ajouter_adherent' => 'Crear un nuevo usuario',
+	'aucun' => 'Ninguno',
+	'aucun_resultat_recherche' => 'No hay ningún resultado para su búsqueda.',
+	'autre' => 'Otro',
+
+	// B
+	'bouton_suppression_compte' => 'Eliminar su cuenta',
+
+	// C
+	'cfg_description' => 'Configurar los campos adicionales para los usuarios y otras funcionalidades.',
+	'cfg_titre' => 'Inscripción 3',
+	'choix_affordance_email' => 'Correo electrónico',
+	'choix_affordance_libre' => 'Opción libre (a continuación)',
+	'choix_affordance_login' => 'Nombre de usuario (por defecto en SPIP)',
+	'choix_affordance_login_email' => 'Nombre de usuario y correo electrónico',
+	'choix_feminin' => 'Señora',
+	'choix_inscription_texte_aucun' => 'Ninguno',
+	'choix_inscription_texte_libre' => 'Opción libre (a continuación)',
+	'choix_inscription_texte_origine' => 'El original (por defecto de SPIP)',
+	'choix_masculin' => 'Señor',
+	'compte_active' => 'Su cuenta en @nom_site@',
+	'configuration' => 'Configuración de los campos de usuarios',
+	'contacts_personnels' => 'Contactos personales',
+
+	// D
+	'delete_user_select' => 'Eliminar el (los) usuario(s) seleccionado(s)',
+	'descriptif_page_inscription' => 'Registro en el sitio @site@',
+	'descriptif_plugin' => 'Aquí encontrará todos los usuarios inscritos en el sitio. Su estatus se indica por el color de su icono.<br /><br />Puede configurar los campos adicionales, propuestos como opción a los visitantes en el momento del registro. ',
+	'divers' => 'Diverso',
+
+	// E
+	'email_bonjour' => 'Buenos días @nom@,',
+	'erreur_chaine_valide' => 'Inserte por favor una cadena de caracteres', # MODIF
+	'erreur_chainelettre' => '(compuesto sólo por letras)',
+	'erreur_chainenombre' => '(compuesta de letras y/o cifras)',
+	'erreur_champ_obligatoire' => 'Este campo es obligatorio',
+	'erreur_compte_attente' => 'Su cuenta está a la espera de confirmación',
+	'erreur_compte_attente_mail' => 'Esta dirección está asociada a una cuenta no confirmada',
+	'erreur_effacement_auto_impossible' => 'La cuenta no puede ser borrada automáticamente, póngase en contacto con nosotros.',
+	'erreur_info_statut' => 'El usuario @nom@ tiene por estatus "@statut@".',
+	'erreur_inscription_desactivee' => 'El registro en este sitio está desactivado',
+	'erreur_login_deja_utilise' => 'Este nombre de usuario ya existe. Por favor, elija otro. ',
+	'erreur_naissance_futur' => '¿Ha nacido realmente en el futuro?',
+	'erreur_naissance_moins_cinq' => '¿Tiene realmente menos de 5 años?',
+	'erreur_naissance_plus_110' => '¿Tiene realmente más de 110 años?',
+	'erreur_numero_valide' => 'Inserte por favor un número válido',
+	'erreur_numero_valide_international' => 'Este número debe figurar según la forma internacional (ejemplo: +32 475 123 456)',
+	'erreur_reglement_obligatoire' => 'Debe aceptar las condiciones',
+	'erreur_signature_deja_utilise' => 'Este valor ya está siendo utilizado por otro usuario.',
+	'erreur_suppression_compte_connecte' => 'Debe estar conectado al sitio para eliminar su cuenta. ',
+	'erreur_suppression_compte_non_auteur' => 'No dispone de los derechos suficientes para eliminar esta cuenta.',
+	'erreur_suppression_compte_webmestre' => 'La cuenta a eliminar es la de un webmaster, usted no puede eliminarla. ',
+	'erreur_suppression_comptes_impossible' => 'La eliminación de su cuenta ha fallado',
+	'exp_statut_rel' => 'Campo diferente del estatus de SPIP, aquí sirve para el control interno de una institución',
+	'explication_affordance_form' => 'Campo mostrado en los formularios de identificación (#LOGIN_PUBLIC)',
+	'explication_auto_login' => 'Si la contraseña es introducida en el formulario, el usuario será automáticamente conectado en la confirmación del formulario de creación de cuenta.',
+	'explication_creation' => 'Registra en la base de datos la fecha de creación de la cuenta.',
+	'explication_info_internes' => 'Opciones que serán almacenadas en la base de datos pero no serán mostradas en el formulario de los nuevos usuarios',
+	'explication_inscription_texte' => 'Texto de introducción visible al inicio del formulario de inscripción',
+	'explication_modifier_logo_auteur' => 'Para modificar el logo (a la izquierda), haga doble click encima. ',
+	'explication_password_complexite' => 'Añadir una verificación javascript de la contraseña cuando los usuarios sean invitados a elegirla o a modificarla. ',
+	'explication_reglement_article' => 'El artículo "<a href="@url@" class="spip_in">@titre@</a>" se utiliza como articulo de condiciones.',
+	'explication_statut' => 'Elija el estatus que desea atribuir a los nuevos usuarios',
+	'explication_suppression_compte' => 'Confirmar la eliminación de su cuenta (@nom@ - @email@)',
+	'explication_valider_compte' => 'Las cuentas han de confirmarse por un administrador antes de poder ser utilizadas.',
+
+	// F
+	'fiche_adherent' => 'Ficha usuario',
+	'fiche_expl' => 'El campo será visible en la ficha del usuario (página autor del espacio público)',
+	'fiche_mod_expl' => 'El campo será modificable desde la interfaz pública por el usuario si se utiliza un formulario de edición de perfil (#FORMULAIRE_EDITER_AUTEUR) o por el plugin "lápices"',
+	'form_expl' => 'El campo se mostrará en el formulario #FORMULAIRE_INSCRIPTION',
+	'form_oblig_expl' => 'Indicar la entrada como obligatoria en los formularios de inscripción y de modificación',
+	'form_retour_aconfirmer' => 'Su cuenta se ha creado correctamente. Está a la espera de confirmación por parte de un administrador.',
+	'form_retour_inscription_pass' => 'Su cuenta se ha creado correctamente. Puede hacer uso de ella inmediatamente para conectarse al sitio usando su correo electrónico de registro como identificador.',
+	'form_retour_inscription_pass_logue' => 'Su cuenta se ha creado correctamente. Usted está actualmente identificado correctamente.',
+	'formulaire_inscription' => 'Formulario de registro',
+	'formulaire_inscription_ok' => 'Su registro ha sido tenido en cuenta. Recibirá por correo electrónico sus identificadores de conexión. ',
+	'formulaire_remplir_obligatoires' => 'Rellene por favor los campos obligatorios',
+	'formulaire_remplir_validation' => 'Puede verificar los campos que no serán confirmados.',
+
+	// I
+	'icone_afficher_utilisateurs' => 'Mostrar los usuarios',
+	'info_aconfirmer' => 'para confirmar',
+	'info_cextras_desc' => 'Campos adicionales ya existentes.',
+	'info_connection' => 'Información de conexión',
+	'info_defaut_desc' => 'Posibilidades de configuración',
+	'info_pass_faible' => 'Débil',
+	'info_pass_fort' => 'Seguro',
+	'info_pass_moyen' => 'Medio',
+	'info_pass_tres_faible' => 'Muy débil',
+	'info_pass_tres_fort' => 'Muy seguro',
+	'info_perso_desc' => 'Información personal que será solicitada a los nuevos usuarios del sitio',
+	'infos_personnelles' => 'Información personal',
+
+	// L
+	'label_adresse' => 'Dirección',
+	'label_affordance_form' => 'Configuración de los formularios de identificación',
+	'label_affordance_form_libre' => 'Texto en caso de opción libre',
+	'label_auto_login' => 'Identificación automática',
+	'label_bio' => 'Biografía',
+	'label_civilite' => 'Tratamiento',
+	'label_code_postal' => 'Código postal',
+	'label_commentaire' => 'Comentario',
+	'label_creation' => 'Fecha de creación de la ficha',
+	'label_email' => 'Correo electrónico',
+	'label_fax' => 'Fax',
+	'label_fonction' => 'Función',
+	'label_inscription_depuis' => 'Miembro desde el @date@.',
+	'label_inscription_texte' => 'Introducción del formulario',
+	'label_inscription_texte_libre' => 'Texto en caso de opción libre',
+	'label_login' => 'Nombre de usuario (login)',
+	'label_logo_auteur' => 'Logo',
+	'label_mobile' => 'Móvil',
+	'label_naissance' => 'Fecha de nacimiento',
+	'label_nom' => 'Firma',
+	'label_nom_famille' => 'Apellido(s)',
+	'label_nom_site' => 'Nombre del sitio',
+	'label_pass' => 'Contraseña',
+	'label_password_complexite' => 'Verificar la complejidad de la contraseña',
+	'label_password_retaper' => 'Confirme la contraseña',
+	'label_pays' => 'País',
+	'label_pays_defaut' => 'País por defecto',
+	'label_pgp' => 'Clave PGP',
+	'label_prenom' => 'Nombre',
+	'label_profession' => 'Profesión',
+	'label_public_reglement' => 'He leído y acepto las condiciones',
+	'label_public_reglement_url' => 'He leído y acepto las <a href="@url@" class="spip_in reglement">condiciones</a>',
+	'label_public_reglement_url_mediabox' => 'He leído y acepto las <a href="@url@" @js@ class="spip_in reglement">condiciones</a>',
+	'label_reglement' => 'Confirmar condiciones',
+	'label_reglement_article' => 'Artículo original del sitio correspondiente a las condiciones',
+	'label_reglement_explication' => 'Mostrar una casilla de condiciones y forzar su confirmación',
+	'label_secteur' => 'Sector',
+	'label_sexe' => 'Tratamiento',
+	'label_societe' => 'Sociedad / Asociación',
+	'label_statut' => 'Estatus',
+	'label_surnom' => 'Apodo',
+	'label_telephone' => 'Teléfono',
+	'label_travail' => 'profesional',
+	'label_url_site' => 'Url del sitio',
+	'label_url_societe' => 'Sitio web sociedad',
+	'label_validation_numero_international' => 'Mostrar obligatoriamente los números de teléfono según la forma internacional',
+	'label_valider_comptes' => 'Confirmar las cuentas',
+	'label_ville' => 'Ciudad',
+	'label_website' => 'Sitio Internet',
+	'legend_oubli_pass' => 'Sin contraseña / contraseña olvidada',
+	'legende' => 'Leyenda',
+	'legende_affordance_form' => 'Formulario de identificación',
+	'legende_cextras' => 'Campos adicionales',
+	'legende_formulaire_inscription' => 'Formulario de inscripción',
+	'legende_info_defaut' => 'Información obligatoria',
+	'legende_info_internes' => 'Información interna',
+	'legende_info_perso' => 'Información personal',
+	'legende_password' => 'Contraseña',
+	'legende_reglement' => 'Condiciones del sitio',
+	'legende_validation' => 'Confirmaciones',
+	'lisez_mail' => 'Se acaba de enviar un correo electrónico a la dirección proporcionada. Para activar su cuenta, siga por favor las instrucciones. ',
+	'liste_adherents' => 'Ver la lista de usuarios',
+	'liste_comptes_titre' => 'Lista de usuarios',
+
+	// M
+	'menu_info_inscription3' => 'Enlace a una página de registro del sitio',
+	'menu_nom_inscription3' => 'Enlace al registro',
+	'menu_titre_lien_inscription' => 'Registro',
+	'message_auteur_inscription_confirmer_contenu_admin' => '@nom@ ha solicitado una cuenta en el sitio. Usted puede confirmar o rechazar esta solicitud.',
+	'message_auteur_inscription_confirmer_contenu_user' => 'su cuenta está actualmente a la espera de confirmación por parte de un administrador del sitio.',
+	'message_auteur_inscription_confirmer_titre_admin' => '[@nom_site_spip@] Solicitud de confirmación de la cuenta de @nom@',
+	'message_auteur_inscription_confirmer_titre_user' => '[@nom_site_spip@] Su cuenta está a la espera de confirmación',
+	'message_auteur_inscription_pass' => 'su cuenta se ha creado correctamente. Ha elegido usted mismo su contraseña.',
+	'message_auteur_inscription_pass_rappel_login' => 'Recordatorio: su nombre de usuario es "@login@".',
+	'message_auteur_inscription_pass_titre_user' => '[@nom_site_spip@] Su cuenta ha sido creada',
+	'message_auteur_inscription_verifier_contenu_plusieurs' => 'Varias cuentas están a la espera:',
+	'message_auteur_inscription_verifier_contenu_un' => 'Una cuenta está a la espera:',
+	'message_auteur_inscription_verifier_titre_plusieurs' => '[@nom_site_spip@] Varias cuentas usuarias para confirmar',
+	'message_auteur_inscription_verifier_titre_un' => '[@nom_site_spip@] Una cuenta de usuario para confirmar',
+	'message_auteur_invalide_contenu_admin' => '@admin@ ha rechazado la cuenta de @nom@.',
+	'message_auteur_invalide_contenu_user' => 'un administrador ha rechazado la confirmación de su cuenta.',
+	'message_auteur_invalide_titre_admin' => '[@nom_site_spip@] Cuenta de @nom@ rechazada',
+	'message_auteur_invalide_titre_user' => '[@nom_site_spip@] Su cuenta ha sido rechazada',
+	'message_auteur_valide_contenu_admin' => '@admin@ ha confirmado la cuenta de @nom@.',
+	'message_auteur_valide_titre_admin' => '[@nom_site_spip@] Cuenta de @nom@ confirmada',
+	'message_auto' => '(éste es un mensaje automático)',
+	'message_compte_efface' => 'Su cuenta ha sido borrado.',
+	'message_modif_email_ok' => 'Su dirección de correo electrónico se ha modificado correctamente.',
+	'message_users_supprimes_nb' => '@nb@ usuario(s) eliminado(s).',
+	'message_users_supprimes_un' => 'Un usuario ha sido eliminado.',
+	'modif_pass_titre' => 'Modificar su contraseña',
+	'mot_passe_reste_identique' => 'Su contraseña no ha sido modificada.',
+
+	// N
+	'no_user_selected' => 'No ha seleccionado ningún usuario. ',
+	'nom_explication' => 'su nombre o su pseudónimo',
+	'non_renseigne' => 'no informado.',
+	'non_renseignee' => 'no informada.',
+
+	// O
+	'option_choisissez' => 'Elija',
+
+	// P
+	'par_defaut' => 'Este campo es obligatorio',
+	'pass_indiquez_cidessous' => 'Indique a continuación la dirección de correo electrónico con la cual usted se ha registrado previamente. Recibirá un correo electrónico indicándole los pasos a seguir para modificar su acceso. ',
+	'pass_oubli_mot' => 'Cambio de su contraseña',
+	'pass_rappel_email' => 'Recordatorio: su dirección de correo electrónico es "@email@".',
+	'pass_rappel_login_email' => 'Recordatorio: su nombre de usuario es "@login@" y su dirección de correo electrónico es "@email@".',
+	'pass_recevoir_mail' => 'Usted va a recibir un correo electrónico indicándole cómo modificar su acceso al sitio.',
+	'password_obligatoire' => 'La contraseña es obligatoria.',
+	'probleme_email' => 'Problema de correo electrónico: el correo electrónico de activación no puede enviarse.',
+	'profil_droits_insuffisants' => 'Disculpe, no tiene derecho a modificar este autor<br/>',
+	'profil_modifie_ok' => 'Su perfil se ha modificado correctamente. ',
+
+	// R
+	'raccourcis' => 'Accesos directos',
+	'recherche_case' => 'En el campo:',
+	'recherche_utilisateurs' => 'Buscar un usuario',
+	'recherche_valeur' => 'Buscar:',
+
+	// S
+	'statut_rel' => 'Estatus interno',
+	'statuts_actifs' => 'Los colores de los iconos corresponden a los estatus siguientes: ',
+	'supprimer_adherent' => 'Eliminar usuarios',
+
+	// T
+	'table_expl' => 'El campo se mostrará en la lista de usuarios (espacio privado)',
+	'texte_email_confirmation' => 'Su cuenta está activa, desde ahora puede conectarse al sitio usando sus identificadores personales.
+
+Su nombre de usuario es: @login@
+y acaba de elegir su contraseña. 
+
+Gracias por su confianza. 
+
+El equipo de @nom_site@
+@url_site@',
+	'texte_email_inscription' => 'usted está en el momento de confirmar su registro en el sitio @nom_site@.
+
+Haga click en el siguiente enlace para activar su cuenta y elegir su contraseña. 
+
+@link_activation@
+
+
+Gracias por su confianza.
+El equipo de @nom_site@.
+@url_site@
+
+Si no ha solicitado este registro o si no quiere formar parte de nuestro sitio, haga click en el siguiente enlace.
+@link_suppresion@
+
+
+',
+	'thead_fiche' => 'Ficha',
+	'thead_fiche_mod' => 'Modificable',
+	'thead_form' => 'Formulario',
+	'thead_obligatoire' => 'Obligatorio',
+	'thead_table' => 'Tabla',
+	'titre_modifier_auteur' => 'Modificar el perfil de este usuario',
+	'titre_modifier_auteur_nom' => 'Modificar el perfil de @nom@',
+	'titre_modifier_profil' => 'Modificar su perfil',
+	'titre_supprimer_compte' => 'Eliminar su cuenta',
+
+	// V
+	'vos_articles_auteur' => 'Sus artículos',
+	'vos_contacts_personnels' => 'Sus contactos personales',
+	'votre_adresse' => 'Su dirección personal',
+	'votre_login_mail' => 'Su nombre de usuario o dirección de correo electrónico:',
+	'votre_mail' => 'Su dirección de correo electrónico:',
+	'votre_nom_complet' => 'Su nombre completo'
+);
+
+?>
