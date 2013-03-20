82f3cabdd35ce6591389c947bfecfff0d4272772
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-03-20 02:43:27 +0000

    langues (paquet-sedna)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@70943 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ sedna/trunk/lang/paquet-sedna_es.php
@@ -0,0 +1,17 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-sedna?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// S
+	'sedna_description' => 'Sedna es un agregador de feeds RSS (y ATOM etc) basado en los sitios sindicados de SPIP. Se inspira vagamente en «Safari RSS» para la presentación.
+
+Su objetivo es ser y seguir siendo sencillo en su código, y rápido de cargar. También, un tanto estético.',
+	'sedna_slogan' => 'Agregador RSS'
+);
+
+?>
