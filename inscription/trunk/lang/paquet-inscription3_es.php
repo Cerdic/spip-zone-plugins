2f94f1b8576cd45724a0a4fd26f34426afcfe5bb
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-03-20 02:42:57 +0000

    langues (paquet-inscription3)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@70935 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ inscription/trunk/lang/paquet-inscription3_es.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-inscription3?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// I
+	'inscription3_description' => 'Plugin que permite tener una gestión más fina de las cuentas de los usuarios, al registrarse y editar su perfil.',
+	'inscription3_nom' => 'Inscripción 3',
+	'inscription3_slogan' => 'Ampliar la información sobre los autores'
+);
+
+?>
