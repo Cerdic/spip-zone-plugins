68901a2ebefea79b7c07b11c2e77386d897e878b
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-03-20 02:43:13 +0000

    langues (paquet-piecon)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@70938 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ piecon/trunk/lang/paquet-piecon_es.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-piecon?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// P
+	'piecon_description' => 'Este plugin simplemente inserta y configura la biblioteca Javascript [Piecon->http://lipka.github.com/piecon/] en SPIP',
+	'piecon_nom' => 'Piecon',
+	'piecon_slogan' => 'Inserción de la biblioteca Piecon en SPIP'
+);
+
+?>
