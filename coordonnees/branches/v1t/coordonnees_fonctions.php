636a093de73212b27dc94d4025878b46d50e36ec
gildas.cotomale@gmail.com <gildas.cotomale@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-03-20 07:16:50 +0000

    commentaires PHPdoc des filtres
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@70944 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- coordonnees/branches/v1t/coordonnees_fonctions.php
+++ coordonnees/branches/v1t/coordonnees_fonctions.php
@@ -4,6 +4,27 @@
  * Licence GPL (c) 2010 Matthieu Marcillaud
 **/
 
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+/**
+ * Fonction privee mutualisee utilisee par les filtres logo_type_xx
+ *
+ * @param string $id
+ *  Suffixe du xx du filtre logo_type_xx appelant ;
+ *  Infixe du logo "images/type_xx_yy.???" a associer ;
+ *  Correspond normalement a la classe vCard : adr, tel, email
+ * @param string $val
+ *  Valeur associee transmise par le filtre logo_type_xx ;
+ *  Suffixe du logo "images/type_xx_yy.???" a associer ;
+ *  Correspond au "type" de liaison de la la coordonnee (home, work, etc.)
+ * @return string
+ *  Balise <IMG> (s'il existe un logo "images/type_$id_$val") ou <ABBR> (sinon),
+ * avec classes semantiques micro-format et traduction des valeurs cles RFC2426
+ * @note
+ *  http://www.alsacreations.com/tuto/lire/1222-microformats-design-patterns.html
+ *  http://www.alsacreations.com/tuto/lire/1223-microformats-composes.html
+ *
+**/
 function logo_type_($id='', $val='') {
 	global $formats_logos;
 	$type = strtolower($val);
@@ -21,27 +42,80 @@ function logo_type_($id='', $val='') {
 		return '';
 }
 
-// RFC2426/CCITT.X520 : dom home intl parcel postal pref work
+/**
+ * Filtre d'affichage du type d'une adresse
+ *
+ * @param string $type_adresse
+ *  Valeur du type de liaison (cf. logo_type_).
+ *  Les valeurs nativement prises en compte sont les codes normalisees
+ * CCITT.X520/RFC2426 (section 3.2.1) : dom home intl parcel postal pref work
+ * @return string
+ *  Balise HTML micro-format (cf. logo_type_)
+**/
 function logo_type_adr($type_adresse) {
 	return logo_type_('adr', $type_adresse);
 }
 
-// RFC2426/CCITT.X500 : bbs car cell fax home isdn modem msg pager pcs pref video voice work
-// RFC6350/CCITT.X520.1988 : cell fax pager text textphone video voice x-... (iana-token)
-// + : dsl
+/**
+ * Filtre d'affichage du type d'un numero
+ *
+ * @param string $type_numero
+ *  Valeur du type de liaison (cf. logo_type_).
+ *  Les valeurs nativement prises en compte sont les codes normalisees
+ * CCITT.X500/RFC2426 (section 3.3.1) : bbs car cell fax home isdn modem msg pager pcs pref video voice work
+ * CCITT.X520.1988/RFC6350 (section 6.4.1) : cell fax pager text textphone video voice x-... (iana-token)
+ * ainsi que : dsl <http://fr.wikipedia.org/wiki/Digital_Subscriber_Line#Familles>
+ * @return string
+ *  Balise HTML micro-format (cf. logo_type_)
+**/
 function logo_type_tel($type_numero) {
 	return logo_type_('tel', $type_numero);
 }
 
-// RFC2426/IANA : internet pref x400
-// RFC6350/CCITT.X520+RFC5322 : home intl work
+/**
+ * Filtre d'affichage du type d'un courriel
+ *
+ * @param string $type_email
+ *  Valeur du type de liaison (cf. logo_type_).
+ *  Les valeurs nativement prises en compte sont les codes normalisees
+ * IANA/RFC2426 (section 3.3.2) : internet pref x400
+ * CCITT.X520+RFC5322/RFC6350 (section 6.4.2) : home intl work
+ * @return string
+ *  Balise HTML micro-format (cf. logo_type_)
+**/
 function logo_type_email($type_email) {
 	return logo_type_('email', $type_email);
 }
 
-// RFC6350/CCITT.X520+RFC5322 readapte : perso pro
+/**
+ * Filtre d'affichage du type d'un mel (2nd)
+ *
+ * @param string $type_adresse
+ *  Valeur du type de liaison (cf. logo_type_).
+ *  Cette adaptation reconnait les codes RFC6350 traduits "perso" et "pro"
+ * (respectivement pour "home" et "work")
+ * @return string
+ *  Balise HTML micro-format (cf. logo_type_)
+ * @note
+ *  On fait un nouveau filtre pour ne pas melanger ce qui releve de la norme et
+ * les bidouilles temporaires.
+**/
 function logo_type_mel($type_email) {
 	return logo_type_('mel', $type_email);
+}
+
+/**
+ * filtre d'affichage du type d'une messagerie de presence
+ *
+ * @param string $type_messagerie
+ *  Valeur du type de liaison (cf. logo_type_).
+ *  Les valeurs nativement prises en compte sont les codes normalisees
+ * CCITT.X520+RFC5322/RFC6350 (section 6.4.3) : pref
+ * @return string
+ *  Balise HTML micro-format (cf. logo_type_)
+**/
+function logo_type_impp($type_messagerie) {
+	return logo_type_('impp', $type_messagerie);
 }
 
 ?>
