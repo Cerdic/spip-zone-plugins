9134c762fdb8a4c0bc4ce2101bd320a38f019046
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-03-20 02:42:59 +0000

    langues (paquet-jfu)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@70936 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ jquery_file_upload/trunk/lang/paquet-jfu_es.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-jfu?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// J
+	'jfu_description' => 'jQuery-File-Upload
+_ Este plugin ofrece la libreria [jQuery-File-Upload->http://aquantum-demo.appspot.com/file-upload].',
+	'jfu_slogan' => 'jQuery-File-Upload'
+);
+
+?>
