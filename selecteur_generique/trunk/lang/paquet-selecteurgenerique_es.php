3f6a43865b0c5bb0ddb8b43855c005dd2470f4b2
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-03-20 02:43:18 +0000

    langues (paquet-selecteurgenerique)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@70940 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ selecteur_generique/trunk/lang/paquet-selecteurgenerique_es.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-selecteurgenerique?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// S
+	'selecteurgenerique_description' => 'Reemplaza los selectores (autores, palabras, secciones) por algo más ligero y rápido, capaz de tratar tantos datos como sea posible. ',
+	'selecteurgenerique_nom' => 'Selector genérico',
+	'selecteurgenerique_slogan' => 'Mejora de los selectores del espacio privado'
+);
+
+?>
