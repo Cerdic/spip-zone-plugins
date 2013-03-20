96eba9e623a181872343f880846cd99a53dba3b6
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-03-20 02:43:16 +0000

    langues (ppp)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@70939 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ porte_plume_extras/partout/lang/ppp_es.php
@@ -0,0 +1,32 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/ppp?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'bouton_mettre_a_jour' => 'Actualizar',
+
+	// C
+	'cfg_attention' => 'Atención',
+	'cfg_description' => 'en función de sus esqueletos, tenga cuidado en no activar el Porta Plumas más que en los campos para los que el uso de accesos directos no genere errores xhtml.',
+	'cfg_titre' => 'Porta Plumas Por todas partes',
+
+	// E
+	'explication_personnalisation' => 'Indique el destino de los elementos que utilizarán la barra tipográfica (Expresión CSS o extensión jQuery).',
+
+	// L
+	'label_hauteur_champ' => 'Media altura de la pantalla',
+	'label_personnalisation' => 'Selector personalizado',
+	'legend_barre_typo' => 'Activar la barra tipográfica en:',
+	'legend_hauteur_champ' => 'Altura del campo texto para los artículos',
+	'legend_personnalisation' => 'Personalización',
+
+	// S
+	'supprimer' => 'Reponer los valores por defecto'
+);
+
+?>
