383a74aeb79888d3e1f68ec32be921f31312c86b
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-03-21 02:43:18 +0000

    langues (a2a)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@70965 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- a2a/trunk/lang/a2a_es.php
+++ a2a/trunk/lang/a2a_es.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// extrait automatiquement de http://trad.spip.org/tradlang_module/a2a?lang_cible=es
+// extrait automatiquement de http://trad.spip.net/tradlang_module/a2a?lang_cible=es
 // ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
@@ -8,34 +8,63 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
-	'appliquer_type_liaison' => 'Type de liaison :', # NEW
-	'appliquer_type_liaison_explication' => 'Décrivez le type de lien à appliquer lors de la liaison. Utilisez de préférence un code ou un mot court.', # NEW
-	'articles_lies' => 'Connectar un artículo',
+	'articles_lies' => 'ARTÍCULOS RELACIONADOS',
+
+	// C
+	'cfg_a2a' => 'Relaciones entre artículos',
+	'changer_type_liaison' => 'Cambiar',
+	'configurer_a2a' => 'Configurar las relaciones entre artículos',
 
 	// E
-	'explication_navigateur' => 'Cliquez sur [Ajouter] pour naviguer dans l\'arborescence du site afin de sélectionner les articles à lier.', # NEW
+	'explication_navigateur' => 'Haga click en [Añadir] para navegar en la arborescencia del sitio a fin de seleccionar los artículos a relacionar. ',
+	'explication_type_obligatoire' => 'Vous pouvez imposer de typer les liaisons', # NEW
+	'explication_types_differents' => 'Marque esta casilla para permitir relacionar varias veces dos mismos artículos, cambiando el tipo cada vez',
+	'explication_types_liaisons' => 'Vous pouvez définir des types de relations : indiquez-en une par ligne, suivie d\'une virgule et d\'une description. Une ligne vide pour la valeur par défaut. La description peut être une chaîne de langue.', # NEW
+
+	// I
+	'info_nombre_articles_lies' => '@nb@ artículos relacionados',
+	'info_un_article_lie' => '1 artículo relacionado',
 
 	// L
-	'lier_cet_article' => 'Connectar',
-	'lier_cet_article_deux_cotes' => 'Lier des deux cotés', # NEW
+	'lien_lier_article' => 'Relacionar con uno o varios artículos',
+	'lier_cet_article' => 'Conectar',
+	'lier_cet_article_deux_cotes' => 'Vincular los dos lados',
+
+	// O
+	'ob_pb' => 'Il n\'est pas possible de rendre obligatoire le type de liaison : en effet les articles ci-dessous possèdent des liaisons non typées. Typez les d\'abords, puis rendez-obligatoire le typage.', # NEW
 
 	// P
-	'pas_articles_lies' => 'No artículo relacionado',
-	'pas_de_resultat' => 'No resultados',
+	'pas_articles_lies' => 'Ningún artículo relacionado',
+	'pas_de_resultat' => 'Ningún resultado',
 
 	// R
-	'rang_moins' => 'Arriba',
+	'rang' => 'Rango',
+	'rang_moins' => 'Subir',
 	'rang_plus' => 'Abajo',
-	'recherche_arbo' => 'Recherche arborescente', # NEW
-	'recherche_texte' => 'Recherche textuelle', # NEW
+	'recherche_arbo' => 'Búsqueda arborescente',
+	'recherche_texte' => 'Búsqueda textual',
 	'rechercher' => 'Buscar',
-	'rechercher_seulement_titres' => 'Rechercher seulement dans les titres', # NEW
-	'rechercher_tip_articles_ids' => 'Pour afficher directement l\'article numéro 123, entrer \'art123\'.', # NEW
+	'rechercher_seulement_titres' => 'Buscar solamente en los títulos',
+	'rechercher_tip_articles_ids' => 'Para mostrar directamente el artículo 123, introducir "art123".',
 	'rechercher_un_article_a_lier' => 'Buscar un artículo:',
 
 	// S
+	'sup_pb' => 'Imposible eliminar las siguientes conexiones: se están utilizando. Hay que modificar las conexiones previamente. ',
 	'supprimer_le_lien' => 'Desconectar',
-	'supprimer_le_lien_deux_cotes' => 'Délier des deux cotés', # NEW
+	'supprimer_le_lien_deux_cotes' => 'Desvincular los dos lados',
+
+	// T
+	'td_pb' => 'Vous demandez l\'interdiction de lier plusieurs fois deux mêmes articles. Or certains articles possèdent plusieurs liaisons avec d\'autres articles. Corrigez cela avant.', # NEW
+	'type' => 'Tipo',
+	'type_inexistant' => 'Tipo de conexión inexistente',
+	'type_liaison' => 'Tipo de conexión',
+	'type_obligatoire' => 'Typage obligatoire', # NEW
+	'types_differents' => 'Conexiones múltiples',
+	'types_liaisons' => 'Tipos de conexión',
+	'types_liaisons_non_modifiables' => 'Los siguientes tipos de conexiones no pueden modificarse con la configuración',
+
+	// U
+	'utilise_par_articles' => ', utilizado por las conexiones de los artículos:',
 
 	// V
 	'voir' => 'Ver'
