032dcd82c7c576398a66eade275897cbc5250734
klaus@spip.de <klaus@spip.de@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-03-22 02:43:57 +0000

    langues (paquet-simplog)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@70989 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ simplog/trunk/lang/paquet-simplog_de.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-simplog?lang_cible=de
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// S
+	'simplog_description' => 'Nach Aktivierung dieses Plugins können Admins alle Logdateien der Site einsehen. Ein Unterpunkt des Menüs "Wartung" öffnet die Liste der verfügbaren Logs.',
+	'simplog_nom' => 'Log-Anzeige',
+	'simplog_slogan' => 'Anzeige der SPIP-Logdateien für Admins'
+);
+
+?>
