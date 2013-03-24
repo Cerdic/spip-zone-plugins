ac05762195f7e0bf30bd0ea45332be96e0ab1edb
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-03-24 04:14:02 +0000

    langues (paquet-bouspip)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71034 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ boussole_spip/trunk/lang/paquet-bouspip_en.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-bouspip?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'bouspip_description' => 'This plugin provides the name, url and description of all sites of the SPIP Galaxy. It must be used with the plugin Compass in server mode to provide, via a web service, its information to all the sites wishing to display the compass SPIP.',
+	'bouspip_nom' => 'SPIP Compass',
+	'bouspip_slogan' => 'The good places of Spip Galaxy !'
+);
+
+?>
