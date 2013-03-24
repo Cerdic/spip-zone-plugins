95f5dcf0f2433267a81e7b851510492de697f44a
benlarcher@gmail.com <benlarcher@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-03-24 04:15:30 +0000

    langues (jfu)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71038 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ jquery_file_upload/trunk/lang/jfu_en.php
@@ -0,0 +1,42 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/jfu?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'analyze_document' => 'Analysis of your document',
+
+	// B
+	'bouton_cancel_upload' => 'Cancel the online display',
+	'bouton_delete' => 'Delete',
+	'bouton_start' => 'Send',
+	'bouton_start_upload' => 'Send the online display',
+
+	// E
+	'erreur_emptyresult' => 'The return is empty',
+	'erreur_fichier_trop_gros' => 'The file is too large.',
+	'erreur_fichier_trop_petit' => 'The fil is too small.',
+	'erreur_invalid_file_type' => 'Invalid file type.',
+	'erreur_max_number_files' => 'You have exceeded the maximum number of files',
+	'erreur_message_erreur' => 'Error :',
+	'erreur_uploadedbytes' => 'The number of bytes sent exceeds the maximum size',
+
+	// L
+	'legend_mise_en_ligne_multiple' => 'Choose the files...',
+	'legend_mise_en_ligne_unique' => 'Choose the file...',
+
+	// M
+	'message_file_size_limit' => 'The maximum size of a file is @taille@ MB',
+	'message_queue_limit_max' => 'The maximum number of files in the queue is @nb@.',
+	'message_types_fichiers_autorises' => 'All file extensions are: @types@',
+
+	// T
+	'temps_passe' => 'passed',
+	'temps_restant' => 'left'
+);
+
+?>
