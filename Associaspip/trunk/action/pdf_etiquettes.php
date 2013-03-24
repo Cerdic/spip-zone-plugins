dd2f54c8929830e4af772e67be06e97905c5117e
gildas.cotomale@gmail.com <gildas.cotomale@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-03-24 19:54:05 +0000

    quickfix (à parfaire plus tard) in r71027
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71061 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- Associaspip/trunk/action/pdf_etiquettes.php
+++ Associaspip/trunk/action/pdf_etiquettes.php
@@ -65,25 +65,26 @@ function action_pdf_etiquettes() {
 	$pdf->titre = _T('asso:adherent_titre_liste_'.$statut);
 	$pdf->Open();
 	$pdf->AddPage();
-	$pdf->SetAutoPageBreak(0 ,0);
+	$pdf->SetAutoPageBreak(0,0);
 	$pdf->AliasNbPages();
 	$pdf->SetFontSize(8);
 
 	$affiche_civilite = $GLOBALS['association_metas']['etiquette_avec_civilite'];
 
-	$where = "l.objet='auteur' AND ( (code_postal<>'' AND ville<>'') OR (boite_postale<>'') ) AND ". sql_in('l.id_objet', $liste_id_auteurs);
-	$res = sql_select('*','spip_asso_adresses_liens l INNER JOIN spip_adresses a ON l.id_adresse=a.id_adresse', $where,'','nom_famille,prenom');
+	$where = "l.objet='auteur' AND ( (a.code_postal<>'' AND a.ville<>'') OR (a.boite_postale<>'') ) AND ". sql_in('l.id_objet', $liste_id_auteurs);
+	$res = sql_select('l.id_objet, a.*','spip_adresses_liens l INNER JOIN spip_adresses a ON l.id_adresse=a.id_adresse', $where,'' );
 	$indice = 0;
 	include_spip('filtres','inc'); // http://doc.spip.org/@extraire_multi
 	while($val = sql_fetch($res)) {
 		$etiquette = array( // cf. : http://fr.wikipedia.org/wiki/Adresse_postale#Exemples
-			'ligne1'=>$val['id_auteur'],
-			'ligne2'=>association_formater_nom( ($GLOBALS['association_metas']['etiquette_avec_civilite']?$val['sexe']:''), $val['prenom'], $val['nom_famille'], ''),
-			'ligne4'=>$val['voie'],
-			'ligne3'=>$val['complement'],
-			'ligne5'=>trim($val['boite_postale']),
-			'ligne6'=>trim($val['code_postal']).' '.$val['ville'],
-			'ligne7'=>($val['pays']==$GLOBALS['association_metas']['pays'] ? '' : extraire_multi(sql_getfetsel('nom','spip_pays', (is_numeric($val['pays'])?'id_pays':'code').'='.sql_quote($val['pays']) ,'','')) ), // pas terrible de devoir faire une requete separee pour une adresse, mais ceci ne devrait pas se produire souvent (en general)
+			'ligne1' => $val['id_objet'],
+			'ligne2' => association_formater_idnom($val['id_objet'], array(), '', ''), // pas top : il faut refaire une requete... il faudra ameliorer la requete principale en faisant une jointure qui donne tout ca directement !!!
+//			'ligne2' => association_formater_nom( ($GLOBALS['association_metas']['etiquette_avec_civilite']?$val['sexe']:''), $val['prenom'], $val['nom_famille'], ''),
+			'ligne4' => $val['voie'],
+			'ligne3' => $val['complement'],
+			'ligne5' => trim($val['boite_postale']),
+			'ligne6' => trim($val['code_postal']).' '.$val['ville'], // formatage a la francaise ; dans certains pays l'ordre est inverse (et en GB le code postal est carrement ecrit sur la ligne suivante !)
+			'ligne7' => ($val['pays']==$GLOBALS['association_metas']['pays'] ? '' : extraire_multi(sql_getfetsel('nom','spip_pays', (is_numeric($val['pays'])?'id_pays':'code').'='.sql_quote($val['pays']) ,'','')) ), // pas terrible de devoir faire une requete separee pour une adresse, mais ceci ne devrait pas se produire souvent (en general)
 		);
 		if ( (fmod($indice,$GLOBALS['association_metas']['etiquette_nb_colonne']*$GLOBALS['association_metas']['etiquette_nb_ligne'])==0)and ($indice>0) ) {
 			$pdf->AddPage();
@@ -96,7 +97,7 @@ function action_pdf_etiquettes() {
 		$imp_droite = ($posx+$pas_horizontal-$GLOBALS['association_metas']['etiquette_marge_droite_etiquette']-$GLOBALS['association_metas']['etiquette_espace_etiquettesl']-$GLOBALS['association_metas']['etiquette_marge_gauche_etiquette']);
 		$pdf->SetrightMargin($imp_droite);
 		$pdf->SetLeftMargin($posx);
-		//$pdf->setX($posx);
+		$pdf->setX($posx);
 		$pdf->setY($posy);
 		$pdf->SetFontSize(7);
 #		$pdf->Cell(0,5,($indice+1).' -'.$etiquette['ligne1'],0,2); // active l'affichage des  id_adresse-id_auteur au dessus de l'etiquette d'adresse : utile en deboguage
@@ -104,35 +105,35 @@ function action_pdf_etiquettes() {
 		$pdf->AdaptFont(9,'B');
 		$pdf->Cell(0,5,utf8_decode($etiquette['ligne2']),0,2);
 		$pdf->SetFontSize(8);
-		if ($etiquette['ligne3'] >'') {
+		if ($etiquette['ligne3']) {
 			$pdf->Cell(0,5,utf8_decode($etiquette['ligne3']),0,2);
 		}
-		if ($etiquette['ligne4'] >'') {
+		if ($etiquette['ligne4']) {
 			$pdf->Cell(0,5,utf8_decode($etiquette['ligne4']),0,2);
 		}
 		$pdf->AdaptFont(9,'B');
-		if ($etiquette['ligne5'] >'') {
+		if ($etiquette['ligne5']) {
 			$pdf->cell(0,5,utf8_decode($etiquette['ligne5']),0,2);
 		}
-		if ($etiquette['ligne6'] >'') {
+		if ($etiquette['ligne6']) {
 			$pdf->cell(0,5,utf8_decode($etiquette['ligne6']),0,2);
 		}
 		$pdf->SetFontSize(8);
-		if ($etiquette['ligne7'] >'') {
+		if ($etiquette['ligne7']) {
 			$pdf->cell(0,5,utf8_decode($etiquette['ligne7']),0,2);
 		}
 #		$pdf->Rect($indice_colonne*$pas_horizontal, $indice_ligne*$pas_vertical, $pas_horizontal, $pas_vertical );
 		$indice++;
 	}
-	if ($indice==0) {
-		$message .= _T('asso:etiquette_aucune_impression');
-	} else {
+//	if ($indice==0) {
+//		$message .= _T('asso:etiquette_aucune_impression');
+//	} else {
 		$nom_fic = 'etiquettes_'. _request('suffixe') .'_'. ($filtre_email?'avec':'sans'). 'email.pdf';
 		$pdf->Output($nom_fic, 'D');
-		$message .= _T('asso:etiquette_fichier_telecharger', array('fichier'=>$nom_fic) );
-	}
+//		$message .= _T('asso:etiquette_fichier_telecharger', array('fichier'=>$nom_fic) );
+//	}
 
-	return array('editable' => FALSE, 'message_ok'=> $message );
+//	return array('editable' => FALSE, 'message_ok'=> $message );
 }
 
 ?>
