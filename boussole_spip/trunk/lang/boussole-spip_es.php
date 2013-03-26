03722469c6e11b76a8aee4d0139ebe0a6f37036b
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-03-26 02:41:53 +0000

    langues (boussole-spip)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71089 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ boussole_spip/trunk/lang/boussole-spip_es.php
@@ -0,0 +1,94 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/boussole-spip?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// D
+	'descriptif_boussole_spip' => 'La brújula SPIP reagrupa el conjunto de sitios «oficiales» de la galaxia SPIP. Define para cada sitio su logotipo, su nombre, su eslogan y su descripción. No dude pues en utilizar sus propios sitios para dirigir a sus visitantes a la galaxia SPIP.',
+	'descriptif_site_spip_blog' => 'Porque SPIP es un proyecto colaborativo, SPIP-BLOG.net concentra apuntes técnicos, risas de uno mismo, humores, trolls, anuncios diversos... He aquí el reflejo de la comunidad SPIP: ante todo, mucha ternura. ',
+	'descriptif_site_spip_contrib' => 'Sitio colaborativo, SPIP-CONTRIB.net pone a disposición el conjunto de las contribuciones externas: plugins, scripts, filtros, esqueletos, documentaciones, consejos y trucos... servidos a la comunidad (enlaces de descarga) por los usuarios de SPIP. Sus foros garantizan el vínculo entre desarrolladores y usuarios. ',
+	'descriptif_site_spip_core' => 'El sitio CORE.SPIP.org reúne el historial de modificaciones del código de SPIP, una gestión de tickets que permite reportar errores de software (bugs) y proponer sugerencias de mejora, así como un espacio de descarga de las versiones de SPIP.',
+	'descriptif_site_spip_demo' => 'Sitio de testeo reiniciado cada noche, DEMO.SPIP.org permite a todo el mundo probar SPIP en su última versión estable (con, según la elección, el estatus de redactor o administrador), en un click y sin tener que instalarla.',
+	'descriptif_site_spip_doc' => 'DOC.SPIP.org es el sitio de referencia donde se detalla, analiza y comenta (nombre, emplazamiento, argumentos, rol y uso) cada una de las funciones del código fuente de SPIP.',
+	'descriptif_site_spip_edgard' => 'Edgard es el compañero fiel e infatigable de SPIP IRC donde siempre interviene en el momento oportuno, con ternura y humor. Es en su casa EDGARD.SPIP.org desde donde aporta a través de IRC consejos, respuestas y buen humor. Por otra parte, Edgard es un robot (sin embargo, él no da en absoluto la impresión de saberlo...)',
+	'descriptif_site_spip_forum' => 'FORUM.SPIP.org es el sitio de intercambio y ayuda entre los usuarios de SPIP. El sitio, que está disponible en una docena de idiomas, se organiza en torno a cuatro grandes apartados: instalación y actualización; uso del espacio privado; administración, gestión, configuración del sitio; creación de esqueletos.',
+	'descriptif_site_spip_herbier' => 'El herbario de SPIP no es un anuario de enlaces como el resto. No. Est un <strong>álbum de recuerdos</strong> de sitios realizados con SPIP, pintados en un momento dado de su existencia sobre el lienzo. Cuenta una historia de la Web, la de SPIP, desde 2001. Este herbario reúne los sitios que hemos tenido ganas de pescar, por ser interesantes debido a tal contenido agradablemente destacado, tal aventura humana, tal funcionalidad hábilmente implementada, o simplemente por resultar visualmente atractivos. Sitios que se recuerdan, a los que se regresan, que son referencia habiendo marcado su época de una forma u otra. ',
+	'descriptif_site_spip_info' => 'En algunas páginas, SPIP-INFO.net responde a las cuestiones que se plantean quienes se preguntan por la capacidad de SPIP de servir como base para sus proyectos de desarrollo de un sitio web. Particulares, asociaciones, empresas, ellos encontrarán aquí toda la información para sostener y apoyar su elección. ',
+	'descriptif_site_spip_irc' => 'La comunidad SPIP, que no duerme nunca, está dotada de un canal IRC (discusión instantánea por Internet) abierta a todos: http://webchat.freenode.net/?channels=#spip',
+	'descriptif_site_spip_mag' => 'Descripción del sitio SPIP Mag\'',
+	'descriptif_site_spip_net' => 'Al servicio de usuarios y webmasters, SPIP.net es el sitio oficial recomendado a quienes desean instalar un sitio bajo SPIP, entendiendo el lenguaje de bucles, etiquetas y filtros, escritura y uso de esqueletos. Ofrece glosario, tutoriales, consejos, historial de las versiones y espacio de descarga. SPIP.net está traducido a más de veinte idiomas.',
+	'descriptif_site_spip_party' => 'Pasar su tiempo entre un teclado y una pantalla para codificar SPIP: eso está bien. Conocer desarrolladores y usuarios en la vida real, en carne y hueso, alrededor de un pastel y un zumo de frutas (u otra bebida...): eso está aún mejor. SPIP-PARTY.net es el sitio que amontona todas estas reuniones: pasadas (actas, fotos) y futuras (calendario, agenda).',
+	'descriptif_site_spip_plugin' => 'PLUGINS.SPIP.net quiere ser el anuario completo de los módulos complementarios para SPIP (plugins, esqueletos, temas). Para cada módulo, se presenta: descripción, autor, licencia, nivel de compatibilidad por versión de SPIP, últimas modificaciones aportadas, estado de las traducciones, estadística de uso, enlaces de documentación y de descarga. ',
+	'descriptif_site_spip_programmer' => 'Especialmente destinado a un público de desarrolladores o webmasters teniendo ya conocimientos de PHP, SQL, HTML, CSS y JavaScript, PROGRAMMER.SPIP.org presenta la mayor parte de las funcionalidades (APIs, sobrecargas, segmentaciones...) de SPIP mediante numerosos ejemplos de código. El sitio ofrece como descarga la integridad de su contenido en formato pdf bajo licencia libre cc-by-sa. PROGRAMMER.SPIP.org puede consultarse en francés, inglés y español. ',
+	'descriptif_site_spip_sedna' => 'SEDNA.SPIP.org es el agregador RSS para toda la actualidad de la galaxia SPIP. Las últimas informaciones de más de 70 sitios girando en torno a SPIP pueden consultarse asimismo.',
+	'descriptif_site_spip_test' => 'Con GRML.EU, instale fácilmente su propio sitio en SPIP bajo su última versión de desarrollo. Gracias a las capacidades de puesta en común de SPIP: una sola instalación del «core» para múltiples sitios, usted está en «su casa», en «su» sitio, con «su» configuración. Conviene particularmente para las prácticas de formación en el uso de SPIP.',
+	'descriptif_site_spip_trad' => 'El espacio de los traductores acoge a todos aquéllos que desean ayudar a la comunidad de usuarios de SPIP participando en el trabajo de traducción de SPIP en sí mismo y de sus diversas contribuciones.',
+	'descriptif_site_spip_twit' => 'SPIP.ORG permite a los usuarios publicar mensajes cortos en la Web (140 caracteres como máximo) destinados al público o a sus amigos. La publicación puede hacerse a través del sitio, de los flujos RSS o por mensajería instantánea.',
+	'descriptif_site_spip_user' => 'SPIP@REZO.net es la lista de ayuda entre usuarios SPIP. Los archivos de esta lista pueden consultarse en http://archives.rezo.net/archives/spip.mbox/ o, también, bajo la forma de blog en http://blog.gmane.org/gmane.comp.web.spip.user.',
+	'descriptif_site_spip_video' => 'Descripción del sitio Vídeos',
+	'descriptif_site_spip_zine' => 'Descripción del sitio SPIPZine',
+	'descriptif_site_spip_zone' => 'Espacio de desarrollo regido por un reglamento, ZONE.SPIP.org da acceso al versionado de todas las contribuciones complementarias a SPIP(plugins, esqueletos, temas, modelos, herramientas...).',
+
+	// N
+	'nom_boussole_spip' => 'Galaxia SPIP',
+	'nom_groupe_spip_actualite' => 'Actualidad de SPIP',
+	'nom_groupe_spip_aide' => 'Ayuda e intercambios en torno a SPIP',
+	'nom_groupe_spip_decouverte' => 'Al descubrimiento de SPIP',
+	'nom_groupe_spip_extension' => 'Extensiones y contribuciones a SPIP',
+	'nom_groupe_spip_reference' => 'Referencias SPIP',
+	'nom_site_spip_blog' => 'SPIP Blog',
+	'nom_site_spip_contrib' => 'SPIP-Contrib',
+	'nom_site_spip_core' => 'SPIP Core',
+	'nom_site_spip_demo' => 'SPIP Demo',
+	'nom_site_spip_doc' => 'SPIP Doc',
+	'nom_site_spip_edgard' => 'Edgard',
+	'nom_site_spip_forum' => 'SPIP Foros',
+	'nom_site_spip_herbier' => 'El Herbario de SPIP',
+	'nom_site_spip_info' => 'SPIP info',
+	'nom_site_spip_irc' => 'SPIP IRC',
+	'nom_site_spip_mag' => 'SPIP Mag\'',
+	'nom_site_spip_net' => 'SPIP.net',
+	'nom_site_spip_party' => 'SPIP Party',
+	'nom_site_spip_plugin' => 'Plugins SPIP',
+	'nom_site_spip_programmer' => 'Programar SPIP',
+	'nom_site_spip_sedna' => 'Sedna',
+	'nom_site_spip_test' => 'SPIP Test',
+	'nom_site_spip_trad' => 'Traducir SPIP',
+	'nom_site_spip_twit' => 'SPIP.org',
+	'nom_site_spip_user' => 'SPIP Usuario',
+	'nom_site_spip_video' => 'Media SPIP',
+	'nom_site_spip_zine' => 'SPIPZine',
+	'nom_site_spip_zone' => 'SPIP Zona',
+
+	// S
+	'slogan_boussole_spip' => '¿Perdido en la Galaxia SPIP?',
+	'slogan_site_spip_blog' => 'Del software libre y de la ternura',
+	'slogan_site_spip_contrib' => 'Espacio de las contribuciones a SPIP',
+	'slogan_site_spip_core' => 'Espacio de desarrollo de las versiones SPIP',
+	'slogan_site_spip_demo' => 'Probar la última versión estable de SPIP',
+	'slogan_site_spip_doc' => 'La referencia técnica del código de SPIP',
+	'slogan_site_spip_edgard' => '¡Un golpe de robot y esto se pone en marcha!',
+	'slogan_site_spip_forum' => 'El foro de los usuarios de SPIP',
+	'slogan_site_spip_herbier' => 'Una selección de los sitios notables en SPIP',
+	'slogan_site_spip_info' => 'Para descubrir SPIP y su comunidad',
+	'slogan_site_spip_irc' => 'Venga a charlar al chat de SPIP',
+	'slogan_site_spip_mag' => 'La gaceta de SPIP',
+	'slogan_site_spip_net' => 'La documentación oficial y descarga de SPIP',
+	'slogan_site_spip_party' => 'Agenda de los aperitivos y otras reuniones SPIP',
+	'slogan_site_spip_plugin' => 'El anuario de los plugins SPIP',
+	'slogan_site_spip_programmer' => 'Documentación de los desarrolladores SPIP',
+	'slogan_site_spip_sedna' => 'El canal RSS de la galaxia SPIP',
+	'slogan_site_spip_test' => 'Probar la instalación e implementación de un sitio SPIP',
+	'slogan_site_spip_trad' => 'Espacio de los traductores de SPIP y de sus contribuciones',
+	'slogan_site_spip_twit' => 'El microblog de la comunidad SPIP',
+	'slogan_site_spip_user' => 'La lista de ayuda mutua de los usuarios de SPIP',
+	'slogan_site_spip_video' => 'La mediateca de SPIP',
+	'slogan_site_spip_zine' => 'La revista electrónica (webzine) en y alrededor de SPIP',
+	'slogan_site_spip_zone' => 'Espacio de desarrollo de las contribuciones a SPIP'
+);
+
+?>
