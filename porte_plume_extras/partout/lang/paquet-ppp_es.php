a764c8f7cedf4a2b5b85791473869f9e653bae0d
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-03-26 02:42:04 +0000

    langues (paquet-ppp)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71091 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ porte_plume_extras/partout/lang/paquet-ppp_es.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-ppp?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// P
+	'ppp_description' => 'Porta Plumas Por todas partes sirve para añadir la barra de edición a los campos DESCRIPTIF, CHAPO y PS',
+	'ppp_nom' => 'Porta Plumas Por todas partes',
+	'ppp_slogan' => 'Mostrar el Porta Plumas sobre la mayor parte de los campos de entrada'
+);
+
+?>
