bd683cadd7d310d0b6a9489d3c1ac0d353e32703
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-03-26 02:41:55 +0000

    langues (paquet-bouspip)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71090 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ boussole_spip/trunk/lang/paquet-bouspip_es.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-bouspip?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'bouspip_description' => 'Este plugin proporciona el nombre, la url y la descripción de todos los sitios de la Galaxia SPIP. Debe ser utilizado con el plugin Brújula en modo servidor a fin de proporcionar, a través de un servicio web, sus informaciones a todos los sitios deseando mostrar la brújula SPIP. ',
+	'bouspip_nom' => 'Brújula SPIP',
+	'bouspip_slogan' => '¡Las buenas direcciones de la galaxia SPIP!'
+);
+
+?>
