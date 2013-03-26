7af135f431e616cc5a76c442c82613b32a261941
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-03-26 02:42:06 +0000

    langues (seo)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71092 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ seo/trunk/lang/seo_es.php
@@ -0,0 +1,67 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/seo?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// S
+	'S.E.O' => 'SEO',
+
+	// A
+	'alexa' => 'Alexa',
+	'alexa_activate' => 'Activar Alexa',
+	'alexa_id' => 'Identificador del sitio para Alexa',
+
+	// B
+	'bing_webmaster' => 'Herramientas para administradores web de Bing (Bing Webmaster Tools)',
+	'bing_webmaster_activate' => 'Activar Herramientas para administradores web de Bing (Bing Webmaster Tools)',
+	'bing_webmaster_id' => 'Meta code de vérification', # NEW
+
+	// C
+	'canonical_url' => 'URL Canoniques', # NEW
+	'canonical_url_activate' => 'Activer la meta d\'URL Canoniques', # NEW
+
+	// F
+	'forcer_squelette_descriptif' => 'Les metas SEO sont prioritaires aux meta générées par les squelettes', # NEW
+	'forcer_squelette_label' => 'Charger les metas pour tous squelettes', # NEW
+
+	// G
+	'google_analytics' => 'Google Analytics',
+	'google_analytics_activate' => 'Activar Google Analytics',
+	'google_analytics_id' => 'Google Analytics web property ID', # NEW
+	'google_webmaster_tools' => 'Herramientas de administradores web de Google (Google Webmaster Tools)',
+	'google_webmaster_tools_activate' => 'Activar Herramientas de administradores de Google (Google Webmaster Tools)',
+	'google_webmaster_tools_id' => 'Meta code de vérification', # NEW
+
+	// I
+	'insert_head' => 'Insertion automatique dans #INSERT_HEAD', # NEW
+	'insert_head_activate' => 'Activar la inserción automática',
+	'insert_head_descriptif' => 'Insertion automatique de la configuration SEO dans le &lt;head&gt;', # NEW
+
+	// M
+	'meta_author' => 'Autor:',
+	'meta_copyright' => 'Copyright:',
+	'meta_description' => 'Descripción:',
+	'meta_keywords' => 'Mots Clefs :', # NEW
+	'meta_page_description_sommaire_value' => 'Valeur de la Description de la Page + Valeur Meta du Sommaine', # NEW
+	'meta_page_description_value' => 'Valeur de la Description de la Page', # NEW
+	'meta_page_title_sommaire_value' => 'Valeur du Titre de la Page + Valeur Meta du Sommaire', # NEW
+	'meta_page_title_value' => 'Valor del Título de la Página',
+	'meta_robots' => 'Robots:',
+	'meta_sommaire_value' => 'Valeur de la Meta du Sommaire', # NEW
+	'meta_tags' => 'Meta Tags', # NEW
+	'meta_tags_activate' => 'Activer les meta tags éditoriales', # NEW
+	'meta_tags_default' => 'Valeur des Meta Tags par Défaut (pour les Articles et les Rubriques)', # NEW
+	'meta_tags_edit_activate' => 'Activer l\'édition des meta tags dans les rubriques et les articles', # NEW
+	'meta_tags_editing' => 'Edición de los Meta Tags',
+	'meta_tags_sommaire' => 'Valor de los Meta Tags del Sumario (página de inicio)',
+	'meta_title' => 'Título:',
+
+	// S
+	'seo' => 'Search Engine Optimisation (Optimización de motor de búsqueda)'
+);
+
+?>
