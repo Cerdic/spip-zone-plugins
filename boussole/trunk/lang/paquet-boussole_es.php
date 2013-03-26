b02c9a07cc1e82b80a4a4954d7ade286f16e9e4a
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-03-26 02:41:51 +0000

    langues (paquet-boussole)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71088 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ boussole/trunk/lang/paquet-boussole_es.php
@@ -0,0 +1,17 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-boussole?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'boussole_description' => 'Instale este plugin en su sitio y tendrá acceso, dentro de sus esqueletos, al conjunto de sitios de la galaxia SPIP usando los modelos, avellanas o la página Z propuestos.
+_ Para los amantes del bricolage, puede utilizar también este plugin para mostrar su propia lista de sitios y diseñar sus propias visualizaciones. ',
+	'boussole_nom' => 'Brújula',
+	'boussole_slogan' => '¡Las buenas direcciones de la galaxia SPIP u otros lugares!'
+);
+
+?>
