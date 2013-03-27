cb85a7c15f0631f766feaca7c32ee9ae971f06ee
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-03-27 23:25:40 +0000

    Un peu de phpdoc
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71144 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- coloration_code/trunk/coloration_code_fonctions.php
+++ coloration_code/trunk/coloration_code_fonctions.php
@@ -1,23 +1,12 @@
 <?php
-
+/**
+ * Plugin coloration code
+ * Fonctions spécifiques au plugin
+ * 
+ * @package SPIP\Coloration_code\Fonctions
+ */
+ 
 if (!defined("_ECRIRE_INC_VERSION")) return;
-
-//    Fichier créé pour SPIP avec un bout de code emprunté à celui ci.
-//    Distribué sans garantie sous licence GPL./
-//    Copyright (C) 2006  Pierre ANDREWS
-//
-//    This program is free software; you can redistribute it and/or modify
-//    it under the terms of the GNU General Public License as published by
-//    the Free Software Foundation; either version 2 of the License, or any later version.
-//
-//    This program is distributed in the hope that it will be useful,
-//    but WITHOUT ANY WARRANTY; without even the implied warranty of
-//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//    GNU General Public License for more details.
-//
-//    You should have received a copy of the GNU General Public License
-//    along with this program; if not, write to the Free Software
-//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 // pour interdire globalement et optionnellement le téléchargement associé
 if (!defined('PLUGIN_COLORATION_CODE_TELECHARGE')) {
