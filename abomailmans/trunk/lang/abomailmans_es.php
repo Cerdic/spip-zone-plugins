d6ab4873c3d9ad5e0f7ddaeec5500dbe664746b3
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-03-28 02:37:43 +0000

    langues (abomailmans)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71152 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ abomailmans/trunk/lang/abomailmans_es.php
@@ -0,0 +1,140 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/abomailmans?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'abomailman' => 'Lista de difusión o discusiones',
+	'abomailmans' => 'Listas de difusión o discusiones',
+	'abonne' => 'Me suscribo',
+	'activation' => 'Activación',
+	'active' => 'Activo',
+	'aucune_langue' => 'Ninguno específico',
+
+	// B
+	'bouton_listes_diffusion' => 'Listas de difusión',
+	'btn_abonnement' => 'Suscribirse',
+	'btn_desabonnement' => 'Darse de baja',
+
+	// C
+	'choisir_liste' => 'Debe elegir una lista.',
+	'contenu_date' => 'Contenido a partir de esta fecha',
+	'creation_droits_insuffisants' => 'Sus derechos son insuficientes...',
+
+	// D
+	'desactive' => 'Desactivado',
+	'destinataire' => 'Destinatario',
+
+	// E
+	'email' => 'Correo electrónico',
+	'email_abonnement' => 'Su dirección de correo electrónico',
+	'email_envoye' => 'El correo electrónico ha sido enviado a la lista de difusión: @liste@.',
+	'email_oublie' => 'Ha olvidado su dirección de correo electrónico',
+	'emailliste_abomailman' => 'La dirección de correo electrónico de la lista',
+	'emailliste_abosympa' => 'La dirección de correo electrónico del administrador Sympa',
+	'emailliste_subscribe' => 'Suscripción',
+	'emailliste_unsubscribe' => 'Baja como suscriptor',
+	'emails_a_renseigner' => 'Correos electrónicos a reportar',
+	'envoi_apercu' => 'Vista previa',
+	'envoi_confirmer' => 'Confirmar y enviar',
+	'envoi_liste_parametres' => 'Liste des paramètres [Facultatif]', # NEW
+	'envoi_parametres' => 'Configuración',
+	'envoi_regulier' => 'Envíos automáticos',
+	'envoi_regulier_info' => 'Dejar en blanco para no recibir envíos automáticos',
+	'envoi_regulier_tous_les' => 'Envoi tous les', # NEW
+	'envoi_vers' => 'envoi vers', # NEW
+	'envoyer_courier' => 'Envíar un correo electrónico',
+	'envoyer_courier_liste' => 'Enviar este correo electrónico a esta lista de difusión:',
+	'envoyer_mailmans' => 'Seleccionar el modelo y su contenido',
+	'erreur_email_liste_oublie' => 'L\'adresse email de la liste est obligatoire', # NEW
+	'erreur_nobot' => 'Votre inscription n\' a pu être effectué à cause d\'un problème technique', # NEW
+	'explication_email_subscribe' => 'Email d\'abonnement, de style <code>suffixe+subscribe@exemple.org</code>', # NEW
+	'explication_email_sympa' => 'Si ce champ est renseigné, la liste est considérée comme une liste de serveur "Sympa", dans le cas contraire comme une liste "Mailman" ou "ezmlm".', # NEW
+	'explication_email_unsubscribe' => 'Correo electrónico de baja de suscripción',
+
+	// I
+	'icone_ajouter_liste' => 'Añadir una nueva lista',
+	'icone_envoyer_mail_liste' => 'Envoyer un e-mail aux lettres à partir du contenu de ce site', # NEW
+	'icone_modifier_abomailman' => 'Modificar la lista',
+	'icone_retour_abomailman' => 'Volver a la lista',
+	'info_abomailman_aucun' => 'Ninguna lista',
+	'info_abomailmans_1' => 'Una lista',
+	'info_abomailmans_nb' => '@nb@ listas',
+	'info_sisympa' => '[Obligatoire si liste Sympa]', # NEW
+	'insciption_listes_legende' => 'Suscripción a las listas de suscripción',
+	'inscription_lettres_legende' => 'Suscripción a las listas de difusión y de discusiones',
+
+	// J
+	'je_m_abonne' => 'Cochez pour valider l\'abonnement ou le désabonnement.', # NEW
+
+	// L
+	'label_etat_liste' => 'Estado de la lista',
+	'label_type_abo' => 'Tipo',
+	'label_type_ml' => 'Lista de discusión',
+	'label_type_news' => 'Lista de difusión',
+	'langue_liste' => 'Idioma de la lista',
+	'legende_inscription_ml' => 'Inscripción a la lista de discusión',
+	'legende_inscription_news' => 'Inscripción en la lista de difusión',
+	'legende_inscriptions_ml' => 'Inscripción en la listas de discusión',
+	'legende_inscriptions_news' => 'Inscripción a las listas de difusión',
+	'les_listes_mailmans' => 'Les listes mailmans, sympa ou ezmlm renseignées', # NEW
+	'lire_article' => 'Leer artículo',
+	'liste_creee' => 'La lista número @id@ (@titre@) ha sido creada.',
+	'liste_non_existante' => 'La lista solicitada no existe o ha sido eliminada',
+	'liste_oublie' => 'Vous avez oublié de cocher une liste !', # NEW
+	'liste_supprimee' => 'La lista número @id@ (@titre@) ha sido eliminada.',
+	'liste_updatee' => 'La lista número @id@ (@titre@) ha sido actualizada.',
+
+	// M
+	'message' => 'Introduction à votre courrier, avant le contenu issu du site', # NEW
+	'message_confirm_suite' => 'Para confirmar su solicitud, responda a la solicitud de confirmación que ha recibido por correo electrónico.',
+	'message_confirmation_a' => 'Una solicitud de suscripción a las siguientes listas acaba de ser enviada:',
+	'message_confirmation_d' => 'Une demande de désabonnement aux listes suivantes vient d\'être envoyée. ', # NEW
+	'message_confirmation_unique_a' => 'Una solicitud de suscripción a la siguiente lista acaba de ser enviada:',
+	'message_confirmation_unique_d' => 'Una solicitud de baja de la suscripción a la siguiente lista acaba de ser enviada. ',
+	'mot' => 'Y listar los artículos de la palabra clave',
+
+	// N
+	'nom' => 'Apellidos y nombre (opcional)',
+	'nouveau_abomailman' => 'Nueva lista de difusión',
+
+	// P
+	'pas_template_txt' => 'No hay una versión texto para este modelo',
+	'periodicite' => ' días.',
+	'prenom' => 'Nombre',
+	'previsu_html' => 'html',
+	'previsu_txt' => 'texto',
+
+	// R
+	'rubrique' => 'Y listar los artículos de la sección',
+
+	// S
+	'souhaite_rester' => 'Quiero estar informado/a',
+	'sujet' => 'Asunto del correo electrónico',
+	'sujet_obligatoire' => 'El asunto es obligatorio.',
+	'suppression_definitive' => '¡Eliminación definitiva!',
+	'supprimer' => 'Eliminar',
+	'sympa_message_confirmation' => 'Un correo electrónico de confirmación ha sido enviado a la dirección: ',
+
+	// T
+	'template' => 'Elija el modelo y su contenido',
+	'template_defaut' => 'Modelo por defecto',
+	'template_defaut_info' => 'Si le fichier modele_choisi.txt.html existe, la newsletter sera envoyée en mode html + texte. Sinon seule la version html sera expédiée.', # NEW
+	'texte_descriptif' => 'Descripción',
+	'titre_abomailman' => 'Título de la lista',
+	'titre_liste_obligatoire' => 'El título de la lista es obligatorio',
+	'toute_liste' => 'Todas las listas de difusión',
+
+	// V
+	'verifier_formulaire' => 'Verifique el contenido del formulario.',
+	'veut_s_abonner' => 'quiere suscribirse',
+	'veut_se_desabonner' => 'quiere darse de baja',
+	'voir_modele_depuis' => 'Ver un ejemplo del modelo con',
+	'votre_email' => 'Su correo electrónico'
+);
+
+?>
