0dac74460e62e17e702b8f604bc01d831e049031
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-03-29 23:39:11 +0000

    Nettoyage des répertoires de langue 2
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71284 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- emballe_medias/emballe_medias_doc2img/branches/v1/lang/em_doc2img_fr.php
+++ emballe_medias/emballe_medias_doc2img/branches/v1/lang/em_doc2img_fr.php
@@ -1,6 +1,5 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans http://svn.aires-de-confluxence.info/svn/plugins_spip/import_video/emballe_medias_doc2img/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
--- emballe_medias/emballe_medias_doc2img/trunk/lang/paquet-em_doc2img_en.php
+++ emballe_medias/emballe_medias_doc2img/trunk/lang/paquet-em_doc2img_en.php
@@ -2,12 +2,6 @@
 
 // Ceci est un fichier langue de SPIP -- This is a SPIP language file
 
-///  Fichier produit par PlugOnet
-// Module: paquet-em_doc2img
-// Langue: en
-// Date: 12-06-2012 13:15:45
-// Items: 3
-
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
