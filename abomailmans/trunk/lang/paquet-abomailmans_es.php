a53f19b507233e9350bb7de06349c7a3d4652dde
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-03-29 02:37:43 +0000

    langues (paquet-abomailmans)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71252 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ abomailmans/trunk/lang/paquet-abomailmans_es.php
@@ -0,0 +1,19 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-abomailmans?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'abomailmans_description' => 'Este plugin permite a través de una etiqueta #FORMULAIRE_ABOMAILMAN mostrar un formulario de suscripción al conjunto de las listas [Mailman->http://www.gnu.org/software/mailman/], [Ezmlm->http://www.ezmlm.org/] o [Sympa->http://www.sympa.org] renseignées.
+_ Esta inscripción se hace a través de la interfaz privada de SPIP por el menú "Edición Abomailmans listas de difusiones mailman".
+_ A continuación puede seleccionar artículos de su SPIP, por criterios de fecha, de sección y/o de palabra clave y enviar su contenido a estas listas, usando esqueletos de formateo de contenido.
+_ El enlace con las listas Mailman, Ezmlm y Sympa se hace por envíos de correo electrónico. ',
+	'abomailmans_nom' => 'Suscripción a las listas de difusión',
+	'abomailmans_slogan' => 'Vincular sus listas de difusiones mailman, ezmlm o sympa con su SPIP'
+);
+
+?>
