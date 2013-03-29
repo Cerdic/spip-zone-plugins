104899cad44deb3dffd631f1af09e42a7037da5f
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-03-29 02:37:47 +0000

    langues (compositions)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71253 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- compositions/trunk/lang/compositions_es.php
+++ compositions/trunk/lang/compositions_es.php
@@ -9,37 +9,37 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// C
 	'composition' => 'Composición',
-	'composition_defaut' => 'composition par défaut', # NEW
-	'composition_heritee' => 'héritée', # NEW
-	'composition_utilisee' => 'Composition :', # NEW
-	'composition_verrouillee' => 'Esta composición esta cerrada por el webmaster',
+	'composition_defaut' => 'composición por defecto',
+	'composition_heritee' => 'heredada',
+	'composition_utilisee' => 'Composición:',
+	'composition_verrouillee' => 'Esta composición esta cerrada por el administrador.',
 	'compositions' => 'Composiciones',
 
 	// D
 	'des_utilisations' => '@nb@ utilizaciones',
 
 	// H
-	'heritages' => 'Cette composition définit des compositions par défaut pour les objets suivants :', # NEW
+	'heritages' => 'Esta composición define las composiciones por defecto para los siguientes objetos:',
 
 	// L
-	'label_activer_composition_objets' => 'Utiliser les compositions sur les objets', # NEW
-	'label_branche_verrouillee' => 'Les compositions de cette branche sont verrouillées.', # NEW
+	'label_activer_composition_objets' => 'Utilizar las composiciones en los objetos',
+	'label_branche_verrouillee' => 'Las composiciones de esta rama están bloqueadas.',
 	'label_chemin_compositions' => 'Carpeta de las composiciones',
-	'label_chemin_compositions_details' => 'Indicar el camino en cual buscar los esqueletos de composición.',
-	'label_composition' => 'Tipo de composición',
-	'label_composition_branche_lock' => 'Verrouiller la composition de tous les objets de la branche', # NEW
-	'label_composition_explication' => 'Estas Webmaster, puedes',
-	'label_composition_lock' => 'Cerrar la composición',
+	'label_chemin_compositions_details' => 'Indique el camino en el que buscar los esqueletos de composición.',
+	'label_composition' => 'Composición',
+	'label_composition_branche_lock' => 'Bloquear la composición de todos los objetos de la rama',
+	'label_composition_explication' => 'Usted es administrador, puede',
+	'label_composition_lock' => 'Bloquear la composición',
 	'label_composition_rubrique' => 'Composición de las secciones',
-	'label_information' => 'Information', # NEW
-	'label_masquer_formulaire' => 'Masquer le formulaire', # NEW
-	'label_masquer_formulaire_composition' => 'Masquer le formulaire de choix d\'une composition lorsque l\'utilisateur n\'a pas les droits de la modifier.', # NEW
+	'label_information' => 'Información',
+	'label_masquer_formulaire' => 'Ocultar el formulario',
+	'label_masquer_formulaire_composition' => 'Ocultar el formulario de elección de una composición cuando el usuario no tenga los derechos para modificarla. ',
 	'label_pas_de_composition' => ' Ninguna composición',
 	'label_styliser' => 'Selección de los esqueletos',
-	'label_styliser_auto' => 'No utilizar la selección automatica. La selección esta manejada por mis esqueletos.',
-	'label_tout_verrouiller' => 'Tout verrouiller', # NEW
-	'label_toutes_verrouilles' => 'Toutes les compositions sont verrouillées.', # NEW
-	'label_verrouiller_toutes_compositions' => 'Verrouiller toutes les compositions (seuls les webmestres pourront les modifier).', # NEW
+	'label_styliser_auto' => 'No utilizar la selección automatica. La selección está implementada por mis esqueletos.',
+	'label_tout_verrouiller' => 'Bloquear todo',
+	'label_toutes_verrouilles' => 'Todas las composiciones están bloqueadas.',
+	'label_verrouiller_toutes_compositions' => 'Bloquear todas las composiciones (sólo los administradores podrán modificarlas).',
 
 	// U
 	'une_utilisation' => '1 utilización'
