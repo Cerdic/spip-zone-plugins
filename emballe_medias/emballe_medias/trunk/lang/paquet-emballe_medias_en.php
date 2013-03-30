5ad78de56b2cb6458b94ccbbd9d2d92284e26ade
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-03-30 02:37:23 +0000

    langues (paquet-emballe_medias)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71295 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- emballe_medias/emballe_medias/trunk/lang/paquet-emballe_medias_en.php
+++ emballe_medias/emballe_medias/trunk/lang/paquet-emballe_medias_en.php
@@ -1,14 +1,16 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-emballe_medias?lang_cible=en
+// ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// E
+	// E
 	'emballe_medias_description' => 'Configurable form to wrap medias from the public space...',
 	'emballe_medias_nom' => 'Wrap Medias',
-	'emballe_medias_slogan' => 'Pack the documents properly',
+	'emballe_medias_slogan' => 'Pack the documents properly'
 );
+
 ?>
--- emballe_medias/emballe_medias/trunk/lang/paquet-emballe_medias_fr.php
+++ emballe_medias/emballe_medias/trunk/lang/paquet-emballe_medias_fr.php
@@ -1,14 +1,14 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/emballe_medias/emballe_medias/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// E
+	// E
 	'emballe_medias_description' => 'Formulaire configurable qui emballe les medias depuis l\'espace public...',
 	'emballe_medias_nom' => 'Emballe Medias',
-	'emballe_medias_slogan' => 'Emballer ses documents proprement',
+	'emballe_medias_slogan' => 'Emballer ses documents proprement'
 );
+
 ?>
