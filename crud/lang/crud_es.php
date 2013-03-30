be542d441791689fce8abe60574a8c405ce9573e
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-03-30 02:37:16 +0000

    langues (crud)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71292 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ crud/lang/crud_es.php
@@ -0,0 +1,24 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/crud?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// E
+	'erreur_action_erronee' => 'CRUD: acción @action@ errónea',
+	'erreur_action_inconnue_table' => 'CRUD: acción @action@ desconocida para tabla @table@',
+	'erreur_article_inconnue' => 'CRUD: el artículo @id@ no existe',
+	'erreur_creation' => 'CRUD: imposible crear el objeto de tipo "@objet@" (Verifique sus derechos)',
+	'erreur_info_obligatoire' => 'CRUD: El campo @info@ es obligatorio',
+	'erreur_objet_inexistant' => 'CRUD: el objeto @objet@ #@id_objet@ no existe',
+	'erreur_rubrique_inconnue' => 'CRUD: la sección @id@ no existe',
+	'erreur_suppression' => 'CRUD: error de eliminación del objeto "@objet@" #@id_objet@ (Verifique sus derechos)',
+	'erreur_table_erronee' => 'CRUD: tabla @table@ errónea',
+	'erreur_table_inconnue' => 'CRUD: tabla @table@ desconocida',
+	'erreur_update' => 'CRUD: error de actualización del objeto "@objet@" #@id@ (Verifique sus derechos)'
+);
+
+?>
