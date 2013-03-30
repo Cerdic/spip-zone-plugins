94faa1e21c5b0ca26e9b9781180cbd5cec828089
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-03-30 18:19:57 +0000

    Correction des listes qui inséraient une classe superflue dans le ul et qui rentrait en conflit parfois avec les css du privé. Nettoyage des items de langue fr dont beaucoup ne servaient plus à rien. Il faudra le faire sur les autres langues.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71324 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- corbeille/trunk/lang/corbeille_fr.php
+++ corbeille/trunk/lang/corbeille_fr.php
@@ -1,73 +1,50 @@
 <?php
 
-// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-if (!defined("_ECRIRE_INC_VERSION")) return;
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+// Module: corbeille
+// Langue: fr
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-  
-  'articles_corbeille_tous' => "@nb@ articles dans la corbeille",
-  'articles_corbeille_un' => "1 article dans la corbeille",
 
- 	'aucun_element_supprime' => 'Rien n\'a &eacute;t&eacute; enlev&eacute; de la corbeille',
+// A
+	'articles_corbeille_tous' => '@nb@ articles dans la corbeille',
+	'articles_corbeille_un' => '1 article dans la corbeille',
+	'aucun_element_supprime' => 'Rien n\'a été enlevé de la corbeille',
+	'auteurs_corbeille_tous' => '@nb@ auteurs dans la corbeille',
+	'auteurs_corbeille_un' => '1 auteur dans la corbeille',
 
- 	'aucun' => "Aucun",
-  'auteurs_corbeille_tous' => "@nb@ auteurs dans la corbeille",
-  'auteurs_corbeille_un' => "1 auteur dans la corbeille",
+// B
+	'breves_corbeille_tous' => '@nb@ brèves dans la corbeille',
+	'breves_corbeille_un' => '1 brève dans la corbeille',
 
-	// B
-  'breves_corbeille_tous' => "@nb@ br&egrave;ves dans la corbeille",
-  'breves_corbeille_un' => "1 br&egrave;ve dans la corbeille",
-	
-	// C
-  'corbeille'=>'Corbeille',
-  'choix_doc'=>'Choisissez le type de documents &agrave; afficher', 
-  
-  // D
-  'doc_effaces' => 'Documents effac&eacute;s :',
+// C
+	'corbeille' => 'Corbeille',
 
-  // E
- 	'ecrit' => " a &eacute;crit :",
-	'effacer' => "Effacer la s&eacute;lection",
+// E
+	'effacer' => 'Effacer la sélection',
 	'effacer_tout' => 'Tout effacer',
- 	'erreur' => " : erreur !",
- 	'elements_supprimes' => '@ids@ ont &eacute;t&eacute; supprim&eacute;s de la corbeille',
- 	'element_supprime' => '@ids@ a &eacute;t&eacute; supprim&eacute; de la corbeille',
+	'element_supprime' => '@ids@ a été supprimé de la corbeille',
+	'elements_supprimes' => '@ids@ ont été supprimés de la corbeille',
 
- 	// F
-  'forums_publics_corbeille_tous' => "@nb@ messages de forums publics dans la corbeille",
-  'forums_publics_corbeille_un' => "1 messages de forums publics dans la corbeille",
-  'forums_prives_corbeille_tous' => "@nb@ messages de forums publics dans la corbeille",
-  'forums_prives_corbeille_un' => "1 messages de forums publics dans la corbeille",
+// F
+	'forums_prives_corbeille_tous' => '@nb@ messages de forums privés dans la corbeille',
+	'forums_prives_corbeille_un' => '1 messages de forums privés dans la corbeille',
+	'forums_publics_corbeille_tous' => '@nb@ messages de forums publics dans la corbeille',
+	'forums_publics_corbeille_un' => '1 messages de forums publics dans la corbeille',
 
-  // I
-	'icone_syndic' => "Sites syndiqu&eacute;s",
-	
-  //M
-	'messages_tous_pri' => "Tous les messages du forum des administrateurs dans la corbeille :", 
- 	'messages_tous_pub' => "Tous les messages du forum dans la corbeille :",
+// R
+	'readme' => 'Cette page permet de {{supprimer définitivement}} tout objet SPIP que vous avez mis à la poubelle. Vous pouvez laisser un objet SPIP dans la corbeille tant que vous le souhaitez. C\'est {{à vous}} de faire le ménage.',
 
-  //P
- 	'parution' => "Parution",
- 	'petition' => "la p&eacute;tition :",
-	'petitions_toutes' => "Toutes les p&eacute;titions dans la corbeille :",
-  
-  // readme
-  'readme' => 'Cette page permet de {{supprimer d&eacute;finitivement}} tout objet SPIP que vous avez mis &agrave; la poubelle. Vous pouvez laisser un objet SPIP dans la corbeille tant que vous le souhaitez. C\'est {{&agrave; vous}} de faire le m&eacute;nage.',
+// S
+	'selectionner' => 'sélectionner',
+	'signatures_corbeille_tous' => '@nb@ signatures dans la corbeille',
+	'signatures_corbeille_un' => '1 signature dans la corbeille',
+	'sites_corbeille_tous' => '@nb@ sites dans la corbeille',
+	'sites_corbeille_un' => '1 site dans la corbeille',
 
-  // S
-  'signe' => " a sign&eacute; via :",
-  'souci' => "souci grave !",
-  'sites_corbeille_tous' => "@nb@ sites dans la corbeille",
-  'sites_corbeille_un' => "1 site dans la corbeille",
-  'signatures_corbeille_tous' => "@nb@ signatures dans la corbeille",
-  'signatures_corbeille_un' => "1 signature dans la corbeille",
-  
-  'titre' => "Titre",
-  
-  // V
-  'voir_detail' => 'voir les d&eacute;tails',
- 
+// V
+	'voir_detail' => 'voir les détails',
 );
-
-
 ?>
