d47579f80fb18d06cccdf0acce4afca0e69301b2
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-03-30 02:37:34 +0000

    langues (paquet-manuelsite)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71301 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ manuel_site/lang/paquet-manuelsite_es.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-manuelsite?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// M
+	'manuelsite_description' => 'Este plugin instala un icono de ayuda permitiendo mostrar desde no importa qué página del espacio privado el manual de redacción del sitio. Este manual es un artículo del sitio. El plugin proporciona asimismo un ítem de Preguntas Frecuentes (FAQ) comunes, pudiendo insertarse fácilmente en su manual. ',
+	'manuelsite_nom' => 'Manual de redacción del sitio',
+	'manuelsite_slogan' => 'Un manual característico de su sitio para sus redactores'
+);
+
+?>
