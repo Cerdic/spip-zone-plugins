a2f9d1a83e949c9a0c89aab337f9bf6c62452c1b
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-03-30 02:37:12 +0000

    langues (paquet-courtcircuit)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71290 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ court-circuit/trunk/lang/paquet-courtcircuit_es.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-courtcircuit?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'courtcircuit_description' => 'Permite redirigir algunas secciones hacia un artículo particular. Puede configurar, en el espacio privado, las reglas de redirección a aplicar. ',
+	'courtcircuit_nom' => 'Cortocircuito',
+	'courtcircuit_slogan' => 'Acceso directo a los artículos'
+);
+
+?>
