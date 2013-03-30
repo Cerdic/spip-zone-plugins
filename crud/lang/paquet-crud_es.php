ad0f4ddbc44586992a95c8cdd07df9d0ac31b3b5
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-03-30 02:37:18 +0000

    langues (paquet-crud)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71293 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ crud/lang/paquet-crud_es.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-crud?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'crud_description' => 'Una interfaz C(r)UD para SPIP que se puede utilizar mediante una acción definida por su url o una llamada directa',
+	'crud_slogan' => 'Interfaz de creación, actualización y eliminación de un objeto'
+);
+
+?>
