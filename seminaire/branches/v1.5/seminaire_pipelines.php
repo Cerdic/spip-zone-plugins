84b13024e285a9b6a4f0ebe0fc60da13957aea32
amaury.adon@laposte.net <amaury.adon@laposte.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-03-30 13:41:26 +0000

    Oubli des balises ouvrantes et fermantes dans le script
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71317 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- seminaire/branches/v1.5/seminaire_pipelines.php
+++ seminaire/branches/v1.5/seminaire_pipelines.php
@@ -1,4 +1,6 @@
+<?php
 function seminaire_insert_head($texte){
     $texte .= '<link rel="stylesheet" type="text/css" href="'.find_in_path('inc-css-seminaire.css.html').'" media="all" />'."\n";
     return $texte;
 }
+?>
