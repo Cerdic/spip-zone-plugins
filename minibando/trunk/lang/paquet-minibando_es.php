10e63d2857b0c6714e2da9546a35aead7bd5731c
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-03-30 02:37:45 +0000

    langues (paquet-minibando)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71305 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ minibando/trunk/lang/paquet-minibando_es.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-minibando?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// M
+	'minibando_description' => 'El plugin minibando remplaza los botones de administración de SPIP por una barra de herramientas que incorpora elementos disponibles en la barra de navegación superior del espacio privado de SPIP. ',
+	'minibando_slogan' => '¡Un minibando para el máximo de funcionalidades!'
+);
+
+?>
