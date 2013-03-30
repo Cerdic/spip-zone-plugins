1d29329ca20a48f18f22118f8bfc6eb9da3da269
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-03-30 02:37:43 +0000

    langues (minibando)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71304 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ minibando/trunk/lang/minibando_es.php
@@ -0,0 +1,20 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/minibando?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// M
+	'mode_debug' => 'Modo debug',
+	'mode_inclure' => 'Modo inclure',
+	'mode_profile' => 'Modo profile',
+
+	// T
+	'titre_debug' => 'Debug (Depuración de programas)',
+	'titre_outils_rapides' => 'Creación'
+);
+
+?>
