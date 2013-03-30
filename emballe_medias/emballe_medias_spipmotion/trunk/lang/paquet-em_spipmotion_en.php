4065eabf36f4fbafae8f109de6195843bc55fb6c
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-03-30 02:37:30 +0000

    langues (paquet-em_spipmotion)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71299 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- emballe_medias/emballe_medias_spipmotion/trunk/lang/paquet-em_spipmotion_en.php
+++ emballe_medias/emballe_medias_spipmotion/trunk/lang/paquet-em_spipmotion_en.php
@@ -1,15 +1,16 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-em_spipmotion?lang_cible=en
+// ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// E
+	// E
 	'em_spipmotion_description' => 'Video add-on for "Wrap media" (use SPIPmotion)',
 	'em_spipmotion_nom' => 'Wrap medias - SPIPmotion',
-	'em_spipmotion_slogan' => 'Video add-on for "Wrap media" (use SPIPmotion)',
+	'em_spipmotion_slogan' => 'Video add-on for "Wrap media" (use SPIPmotion)'
 );
+
 ?>
--- emballe_medias/emballe_medias_spipmotion/trunk/lang/paquet-em_spipmotion_fr.php
+++ emballe_medias/emballe_medias_spipmotion/trunk/lang/paquet-em_spipmotion_fr.php
@@ -1,15 +1,14 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/emballe_medias/emballe_medias_spipmotion/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// E
+	// E
 	'em_spipmotion_description' => 'Complément vidéo pour "Emballe Medias" (utilise SPIPmotion)',
 	'em_spipmotion_nom' => 'Emballe Medias - SPIPmotion',
-	'em_spipmotion_slogan' => 'Complément vidéo pour "Emballe Medias" (utilise SPIPmotion)',
+	'em_spipmotion_slogan' => 'Complément vidéo pour "Emballe Medias" (utilise SPIPmotion)'
 );
+
 ?>
