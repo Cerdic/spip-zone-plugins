2c2c9ccbe05c77acd5ee00a8a8abf37f8a394091
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-03-30 02:37:10 +0000

    langues (courtcircuit)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71289 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- court-circuit/trunk/lang/courtcircuit_es.php
+++ court-circuit/trunk/lang/courtcircuit_es.php
@@ -18,8 +18,8 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'explication_liens_rubriques' => '¿Modificar la URL de las secciones redirigidas directamente en los esqueletos?',
 	'explication_regles' => 'Las diferentes reglas a continuación son probadas en este orden. Si ninguna regla define una redirección, entonces la sección será mostrada normalmente. ',
 	'explication_restreindre_langue' => 'Si esta opción está activada, sólo los artículos del idioma activo serán considerados para el cálculo de la redirección. Esta opción no es útil salvo si sus secciones contienen artículos de diferentes idiomas. No utilizar si su sitio está organizado en sectores de idioma o si utiliza los campos multi.',
-	'explication_sousrubrique' => 'Parcourir la première sous-rubrique (tri par numéro du titre et date) ? Les règles de redirection seront testées à nouveau dans cette sous-rubrique.', # NEW
-	'explication_variantes_squelettes' => 'Exemple : squelettes de la forme rubrique-2.html ou rubrique=3.html.', # NEW
+	'explication_sousrubrique' => '¿Navegar por la primera subsección (clasificación por número del título y fecha)? Las reglas de redirección se pondrán a prueba de nuevo en esta subsección. ',
+	'explication_variantes_squelettes' => 'Ejemplo: esqueletos de la forma rubrique-2.html o rubrique=3.html.',
 
 	// I
 	'item_appliquer_redirections' => 'Aplicar las reglas de redirección',
@@ -32,15 +32,15 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'label_composition_rubrique' => 'Sección con composición',
 	'label_exceptions' => 'Excepciones',
 	'label_liens' => 'URL de las secciones',
-	'label_liens_rubriques' => 'Agir sur la balise #URL_RUBRIQUE ?', # NEW
+	'label_liens_rubriques' => 'Actuar bajo la etiqueta #URL_RUBRIQUE ?',
 	'label_plus_recent' => 'Artículo más reciente de la sección',
 	'label_plus_recent_branche' => 'Artículo de la rama más reciente',
 	'label_rang_un' => 'Primer artículo (artículos numerados)',
 	'label_regles' => 'Reglas de redirección de las secciones',
-	'label_restreindre_langue' => 'Ne prendre en compte que les articles de la langue ?', # NEW
-	'label_sousrubrique' => 'Sous-rubriques', # NEW
-	'label_un_article' => 'Seul article de la rubrique', # NEW
-	'label_variantes_squelettes' => 'Rubrique avec variante de squelettes' # NEW
+	'label_restreindre_langue' => '¿No tomar en cuenta más que los artículos del idioma?',
+	'label_sousrubrique' => 'Subsecciones',
+	'label_un_article' => 'Único artículo de la sección',
+	'label_variantes_squelettes' => 'Sección con variante de esqueletos '
 );
 
 ?>
