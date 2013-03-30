8fec407b24a60a3f4dda82e544593b839e4621e8
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-03-30 02:37:26 +0000

    langues (paquet-em_doc2img)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71297 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- emballe_medias/emballe_medias_doc2img/trunk/lang/paquet-em_doc2img_en.php
+++ emballe_medias/emballe_medias_doc2img/trunk/lang/paquet-em_doc2img_en.php
@@ -1,14 +1,16 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-em_doc2img?lang_cible=en
+// ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// E
+	// E
 	'em_doc2img_description' => 'Doc2Img add-on for "Wrap media" (use doc2img)',
 	'em_doc2img_nom' => 'Wrap medias - Doc2Img',
-	'em_doc2img_slogan' => 'Doc2Img add-on for "Wrap media" (use doc2img)',
+	'em_doc2img_slogan' => 'Doc2Img add-on for "Wrap media" (use doc2img)'
 );
+
 ?>
--- emballe_medias/emballe_medias_doc2img/trunk/lang/paquet-em_doc2img_fr.php
+++ emballe_medias/emballe_medias_doc2img/trunk/lang/paquet-em_doc2img_fr.php
@@ -1,15 +1,14 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/emballe_medias/emballe_medias_doc2img/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// E
+	// E
 	'em_doc2img_description' => 'ComplÉment Doc2Img pour "Emballe Medias" (utilise doc2img)',
 	'em_doc2img_nom' => 'Emballe Medias - Doc2Img',
-	'em_doc2img_slogan' => 'ComplÉment Doc2Img pour "Emballe Medias" (utilise doc2img)',
+	'em_doc2img_slogan' => 'ComplÉment Doc2Img pour "Emballe Medias" (utilise doc2img)'
 );
+
 ?>
