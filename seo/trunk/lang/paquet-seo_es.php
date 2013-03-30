7b21fe3737bfae86263a00b6495c5532e9451d8c
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-03-30 02:37:53 +0000

    langues (paquet-seo)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71307 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ seo/trunk/lang/paquet-seo_es.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-seo?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// S
+	'seo_description' => 'SEO es un plugin que añade la posibilidad de insertar en la parte superior de su sitio web: meta tags, URL canónica, Google webmaster tools Meta Code y Google Analytics javascript. Puede configurarse desde la página de configuración de SPIP así como desde cada sección y artículo para las les meta tags.',
+	'seo_nom' => 'SEO',
+	'seo_slogan' => 'Search Engine Optimisation'
+);
+
+?>
