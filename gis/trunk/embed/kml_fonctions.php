ee994443143fbc87b37d3c7219db493521e033b4
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-03-30 12:46:13 +0000

    Un skelette pour gérer un embed externe de kml.
    Utilisé par exemple par le plugin embed_code => http://plugins.spip.net/embed_code.html
    
    Si on a un peu plus d'imagination, on peut également l'utiliser via oembed comme ici => http://oembed.nursit.com/?url=https%3A%2F%2Fwww.mediaspip.net%2Fmedias%2Fexamples%2Fautres-documents%2Farticle%2Fgranite-de-l-aber-ildut 
    
    La source de l'oembed est un MediaSPIP avec un squelette d'oembed des articles un peu modifié (https://www.mediaspip.net/medias/examples/autres-documents/article/granite-de-l-aber-ildut)
    
    
    Le kml d'exemple a été piqué à géodiversité (www.geodiversite.net) il me semble
    
    On incrémente le numéro de version : 4.7.2
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71312 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ gis/trunk/embed/kml_fonctions.php
@@ -0,0 +1,12 @@
+<?php
+// Sécurité
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+/**
+ * On inclue le fichier de pipelines pour avoir les fonctions:
+ * gis_insert_head_css
+ * gis_insert_head
+ */
+include_spip('gis_pipelines');
+
+?>
