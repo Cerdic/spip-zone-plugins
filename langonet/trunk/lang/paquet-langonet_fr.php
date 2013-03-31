5b5cd5c5233f4c297df23c9098496e769c727a7a
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-03-31 13:10:09 +0000

    Premier gros refactoring de l'interface privée et passage en paquet.xml.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71331 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ langonet/trunk/lang/paquet-langonet_fr.php
@@ -0,0 +1,19 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-langonet
+// Langue: fr
+// Date: 31-03-2013 11:59:02
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// L
+	'langonet_description' => 'Ce plugin permet d\'exécuter des actions de vérification et de nettoyage des fichiers de langue de SPIP, des plugins ou des squelettes. Il est aussi possible de consulter l\'ensemble des items contenus dans les fichiers de langue disponibles sur le site et d\'effectuer des recherches d\'items dans les fichiers de langue de SPIP. Sous certaines conditions, il produit automatiquement des items de langue à partir des textes libres repérés.',
+	'langonet_slogan' => 'Vérifier, générer, rechercher ou afficher les items de langue',
+);
+?>
--- langonet/trunk/langonet_fonctions.php
+++ langonet/trunk/langonet_fonctions.php
@@ -3,6 +3,88 @@
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 /**
+ * Creation de la liste des fichiers de log de verification
+ *
+ * @param string $verification
+ * @param string $mode
+ * @return array
+ */
+
+// $verification  => type de verification demandee ('definition' / 'utilisation' / 'fonction_l')
+// $mode  => mode de calul de la liste:
+// 				- 'recent' pour la liste des derniers logs de chaque langue
+// 				- 'tous' pour la liste de tous les logs
+function langonet_lister_fichiers_log($verification, $mode='recent') {
+
+	// On cherche le dernier fichier de log de chaque fichier de langue
+	$liste_tous = array();
+	$logs = preg_files(_DIR_TMP . "langonet/verification/$verification", '[^/]*[_%]' . $verification[0] .'_[^/]*.log$');
+	foreach ($logs as $_fichier) {
+		preg_match(',([^/]*)[_%]' . $verification[0] .'_[^/]*.log$,i', $_fichier, $matches);
+		if ($matches[1]) {
+			if ($verification != 'fonction_l') {
+				$cle = $matches[1];
+			}
+			else {
+				preg_match(",[^%]+$,i", $matches[1], $retour);
+				$cle = $retour[0] . "/";
+			}
+			$liste_tous[$cle][] = $_fichier;
+		}
+	}
+
+	// On veut la liste complete des fichiers de verification
+	if ($mode == 'recent') {
+		// On ne veut que le log le plus recent pour chaque fichier de langue
+		$liste_recents = array();
+		foreach ($liste_tous as $_cle => $_fichiers) {
+			$liste_recents[$_cle] = end($_fichiers);
+		}
+		return $liste_recents;
+	}
+	else
+		return $liste_tous;
+
+}
+
+/**
+ * Creation de la liste des fichiers de langue generes
+ *
+ * @return array
+ */
+
+function langonet_lister_fichiers_lang() {
+
+	// On cherche le dernier fichier de log de chaque fichier de langue
+	$liste_tous = array();
+	$langues = preg_files(_DIR_TMP . "langonet/generation", '[^/]*_[\w{2,3}]*.php$');
+	foreach ($langues as $_fichier) {
+		$liste_tous[basename($_fichier, '.php')] = $_fichier;
+	}
+
+	return $liste_tous;
+}
+
+
+/**
+ * Bulle d'information des liens de telechargement
+ *
+ * @param string $fichier
+ * @param string $type
+ * @return array
+ */
+
+function langonet_creer_bulle_fichier($fichier, $type='log', $action='telecharger') {
+
+	// Date du fichier formatee
+	$date = affdate_heure(date('Y-m-d H:i:s', filemtime($fichier)), 'Y-m-d H:i:s');
+	// Bulle d'information suivant le type de fichier (log ou langue)
+	$bulle = _T('langonet:bulle_' . $action . '_fichier_' . $type, array('date' => $date));
+
+	return $bulle;
+}
+
+/**
  * Creation du select des fichiers de langue
  *
  * @param string $sel_l
