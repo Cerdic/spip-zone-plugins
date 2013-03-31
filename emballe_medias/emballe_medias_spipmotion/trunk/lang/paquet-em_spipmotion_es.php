3214ab156cc01236519a90a86c629e39d1be75e3
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-03-31 01:37:43 +0000

    langues (paquet-em_spipmotion)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71329 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ emballe_medias/emballe_medias_spipmotion/trunk/lang/paquet-em_spipmotion_es.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-em_spipmotion?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// E
+	'em_spipmotion_description' => 'Complemento vídeo para "Empaqueta Medias" (utiliza SPIPmotion)',
+	'em_spipmotion_nom' => 'Empaqueta Medias - SPIPmotion',
+	'em_spipmotion_slogan' => 'Complemento vídeo para "Empaqueta Medias" (utiliza SPIPmotion)'
+);
+
+?>
