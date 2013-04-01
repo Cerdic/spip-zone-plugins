15afd35b61609587b7de65d23d5490b852334334
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-01 13:11:57 +0000

    Un paquet.xml pour openid
    On déplace l'installation dans openid_administrations pour être conforme
    
    version 1.1.14
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71368 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ openid/lang/paquet-openid_en.php
@@ -0,0 +1,19 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-openid
+// Langue: en
+// Date: 01-04-2013 14:28:02
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// O
+	'openid_description' => 'A user wishing to authenticate with her OpenID should enter it as her web site address in her profile. Then, instead of entering her login or email address in the authentication page, she should enter her OpenID.',
+	'openid_slogan' => 'Authenticates users with their OpenID',
+);
+?>
---
+++ openid/lang/paquet-openid_fr.php
@@ -0,0 +1,19 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-openid
+// Langue: fr
+// Date: 01-04-2013 14:28:02
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// O
+	'openid_description' => 'Un utilisateur qui souhaite utiliser son OpenID pour accéder au site doit l\'entrer comme adresse de son site web dans son profil. Ensuite, au lieu de mettre son login ou adresse email, il suffit d\'entrer son OpenID pour se logguer.',
+	'openid_slogan' => 'Authentification des auteurs et visiteurs à l\'aide de leur OpenID',
+);
+?>
