8a79ac548a702b719fd8bddbb28acd7b26e77ae7
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-01 13:37:39 +0000

    Création du paquet.xml de table des matières
    Nécessite de renommer table_matieres.php en tablematieres_fonctions.php
    
    Version 2.0.3
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71375 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ table_matieres/trunk/lang/paquet-tablematieres_en.php
@@ -0,0 +1,21 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-tablematieres
+// Langue: en
+// Date: 01-04-2013 15:30:49
+// Items: 3
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// T
+	'tablematieres_description' => 'Introduce an anchor for each heading of a #TEXTE field of the articles, and an 
+	associated optional tag #TABLE_MATIERES to put in the ARTICLES loops.',
+	'tablematieres_nom' => 'Table of contents',
+	'tablematieres_slogan' => 'Create the summary of an article',
+);
+?>
---
+++ table_matieres/trunk/lang/paquet-tablematieres_fr.php
@@ -0,0 +1,21 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-tablematieres
+// Langue: fr
+// Date: 01-04-2013 15:30:49
+// Items: 3
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// T
+	'tablematieres_description' => 'Introduit une ancre translitterée pour chaque intertitre du champ #TEXTE des ARTICLES,
+	ainsi qu\'une balise #TABLE_MATIERES (facultative) associée à placer, bien sûr, dans une boucle ARTICLES.',
+	'tablematieres_nom' => 'Table des Matières',
+	'tablematieres_slogan' => 'Créer le sommaire d\'un article',
+);
+?>
