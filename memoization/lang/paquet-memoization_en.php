35af87095b5f4e41f2f5609cb6fb94bbd5b11d1b
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-01 13:21:05 +0000

    On ajoute le paquet.xml de Memoization
    On déplace le fichier d'options en memoization_options.php
    
    changement de la date de copyright
    
    version 1.4.1
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71370 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ memoization/lang/paquet-memoization_en.php
@@ -0,0 +1,19 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-memoization
+// Langue: en
+// Date: 01-04-2013 15:14:36
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// M
+	'memoization_description' => 'Memoization is a computer science term, which Wikipedia defines as "a technique of reducing the execution time of a function by storing the results once on the other." It is also the name of a new function library for PHP, which follows my experiments XCache.',
+	'memoization_slogan' => 'Various methods to process SPIP\'s cache.',
+);
+?>
---
+++ memoization/lang/paquet-memoization_fr.php
@@ -0,0 +1,19 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-memoization
+// Langue: fr
+// Date: 01-04-2013 15:14:36
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// M
+	'memoization_description' => 'Mémoïzation est un terme d’informatique, dont Wikipédia donne la définition suivante : « une technique consistant à réduire le temps d’exécution d’une fonction en mémorisant ses résultats d’une fois sur l’autre ». C’est aussi le nom d’une nouvelle librairie de fonctions pour PHP, qui fait suite à mes expérimentations sur XCache.',
+	'memoization_slogan' => 'Gestion du cache de SPIP par diverses méthodes.',
+);
+?>
