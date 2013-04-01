b2687961cd77ad7fad3879321652e362808e4d03
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-01 19:08:32 +0000

    Mise au point du formulaire de recherche avec un choix par défaut et un choix spécifique.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71409 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- langonet/trunk/formulaires/langonet_rechercher.php
+++ langonet/trunk/formulaires/langonet_rechercher.php
@@ -6,23 +6,34 @@ function formulaires_langonet_rechercher_charger($type) {
 	$legende = _T('langonet:titre_form_rechercher_'.$type);
 	$explication = _T('langonet:info_rechercher_'.$type);
 	$info_pattern = _T('langonet:info_pattern_'.$type.'_cherche');
+	$info_modules = _T('langonet:info_modules_recherche_'.$type);
+	$label_defaut = _T('langonet:label_defaut_modules_'.$type);
 
 	$modules_fr = langonet_lister_modules('fr');
-	$modules_choisis = array();
-	if (_request('modules')) {
+
+	$defaut_modules = _request('defaut_modules');
+	$modules = _request('modules');
+	if (($defaut_modules == 'oui')
+	OR (!$defaut_modules AND !$modules)) {
+		$modules_choisis = ($type == 'texte') ? array('ecrire', 'spip', 'public') : array_keys($modules_fr);
+		$defaut_modules = 'oui';
+	}
+	else {
+		$modules_choisis = array();
 		foreach (_request('modules') as $_valeurs) {
 			$modules_choisis[] = reset(explode(':', $_valeurs));
 		}
 	}
-	else {
-		$modules_choisis = ($type == 'texte') ? array('ecrire', 'spip', 'public') : array_keys($modules_fr);
-	}
+
 	return array('type' => $type,
 				'_legende' => $legende,
 				'_explication' => $explication,
 				'_info_pattern' => $info_pattern,
+				'_info_modules' => $info_modules,
+				'_label_defaut' => $label_defaut,
 				'_modules' => $modules_fr,
 				'_modules_choisis' => $modules_choisis,
+				'defaut_modules' => $defaut_modules,
 				'pattern' => _request('pattern'),
 				'correspondance' => _request('correspondance'));
 }
@@ -43,7 +54,24 @@ function formulaires_langonet_rechercher_traiter($type) {
 	// Recuperation des champs du formulaire
 	$pattern = _request('pattern');
 	$correspondance = _request('correspondance');
-	$modules = _request('modules');
+
+	if (_request('defaut_modules') == 'oui') {
+		$modules_fr = langonet_lister_modules('fr');
+		$modules = array();
+		if ($type == 'texte') {
+			$modules = array(
+				'ecrire:' . $modules_fr['ecrire'],
+				'public:' . $modules_fr['public'],
+				'spip:' . $modules_fr['spip']);
+		}
+		else {
+			foreach ($modules_fr as $_module => $_fichier) {
+				$modules[] = "${_module}:${_fichier}";
+			}
+		}
+	}
+	else
+		$modules = _request('modules');
 	$langonet_rechercher = charger_fonction('langonet_rechercher_'.$type,'inc');
 
 	// Verification et formatage des resultats de la recherche
