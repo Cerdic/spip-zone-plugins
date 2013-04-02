cd2830bc574b45d895777c89c2acd7089facd7c4
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-02 14:46:31 +0000

    typo (Raquel S. Bujaldon)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71460 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- emballe_medias/emballe_medias_doc2img/trunk/lang/paquet-em_doc2img_fr.php
+++ emballe_medias/emballe_medias_doc2img/trunk/lang/paquet-em_doc2img_fr.php
@@ -6,9 +6,9 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// E
-	'em_doc2img_description' => 'ComplÉment Doc2Img pour "Emballe Medias" (utilise doc2img)',
+	'em_doc2img_description' => 'Complément Doc2Img pour "Emballe Medias" (utilise doc2img)',
 	'em_doc2img_nom' => 'Emballe Medias - Doc2Img',
-	'em_doc2img_slogan' => 'ComplÉment Doc2Img pour "Emballe Medias" (utilise doc2img)'
+	'em_doc2img_slogan' => 'Complément Doc2Img pour "Emballe Medias" (utilise doc2img)'
 );
 
 ?>
