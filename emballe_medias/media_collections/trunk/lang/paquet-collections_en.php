4f78d51fd93031fd0ec24ac873cc743929a61435
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-02 01:37:50 +0000

    langues (paquet-collections)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71426 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- emballe_medias/media_collections/trunk/lang/paquet-collections_en.php
+++ emballe_medias/media_collections/trunk/lang/paquet-collections_en.php
@@ -1,5 +1,7 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-collections?lang_cible=en
+// ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
@@ -8,7 +10,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// C
 	'collections_description' => 'Create medias collections, albums...',
 	'collections_nom' => 'Medias collections',
-	'collections_slogan' => 'Create medias collections',
+	'collections_slogan' => 'Create medias collections'
 );
 
 ?>
--- emballe_medias/media_collections/trunk/lang/paquet-collections_fr.php
+++ emballe_medias/media_collections/trunk/lang/paquet-collections_fr.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/emballe_medias/media_collections/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
@@ -8,7 +8,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// C
 	'collections_description' => 'Créer des collections de médias, des albums...',
 	'collections_nom' => 'Collections de médias',
-	'collections_slogan' => 'Créer des collections de médias',
+	'collections_slogan' => 'Créer des collections de médias'
 );
 
 ?>
