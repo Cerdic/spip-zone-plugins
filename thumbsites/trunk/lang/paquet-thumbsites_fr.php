fcdb984b04a77f787ae9900eabf2f791d4bd498a
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-02 01:38:16 +0000

    langues (paquet-thumbsites)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71436 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- thumbsites/trunk/lang/paquet-thumbsites_fr.php
+++ thumbsites/trunk/lang/paquet-thumbsites_fr.php
@@ -1,18 +1,13 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-// Module: paquet-thumbsites
-// Langue: fr
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/thumbsites/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// C
-	
-// T
+	// T
 	'thumbsites_description' => 'Thumbsites fournit un système de capture d\'écran de site générée par des serveurs dédiés. Le plugin propose des balises, des filtres et des modèles pour afficher la vignette d\'un site donné. Ce plugin gère aussi un cache qui permet de pallier les indisponibilités fréquentes des serveurs et d\'accélérer les affichages. Une configuration est disponible dans l\'espace privé.',
-	'thumbsites_slogan' => 'Une vignette pour vos sites référencés',
+	'thumbsites_slogan' => 'Une vignette pour vos sites référencés'
 );
+
 ?>
