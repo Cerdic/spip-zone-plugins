8a1d761551f32d8fe286042a1ac352a3cdcc10f2
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-02 01:37:43 +0000

    langues (paquet-suivant_precedent)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71423 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- criteres_suivant_precedent/lang/paquet-suivant_precedent_en.php
+++ criteres_suivant_precedent/lang/paquet-suivant_precedent_en.php
@@ -1,20 +1,16 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-suivant_precedent
-// Langue: en
-// Date: 06-04-2012 16:03:45
-// Items: 3
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-suivant_precedent?lang_cible=en
+// ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// S
+	// S
 	'suivant_precedent_description' => 'This plugin is an experiment allowing to find in a given loop, the next or previous item. This usage is primarily useful when creating navigation by category or by topic (keywords).',
 	'suivant_precedent_nom' => 'Next / Previous criteria',
-	'suivant_precedent_slogan' => 'Adds loops criteria <code>{suivant}</code> and <code>{precedent}</code>',
+	'suivant_precedent_slogan' => 'Adds loops criteria <code>{suivant}</code> and <code>{precedent}</code>'
 );
+
 ?>
--- criteres_suivant_precedent/lang/paquet-suivant_precedent_fr.php
+++ criteres_suivant_precedent/lang/paquet-suivant_precedent_fr.php
@@ -1,20 +1,14 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-suivant_precedent
-// Langue: fr
-// Date: 06-04-2012 16:03:45
-// Items: 3
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/criteres_suivant_precedent/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// S
+	// S
 	'suivant_precedent_description' => 'Ce plugin est une expérimentation permettant de trouver, dans une boucle donnée, l’élément suivant ou l’élément précédent. Cet usage est principalement utile pour créer des navigations par rubrique ou par thème (mots-clés).',
 	'suivant_precedent_nom' => 'Critères Suivant / Précédent',
-	'suivant_precedent_slogan' => 'Ajoute des critères de boucles <code>{suivant}</code> et <code>{precedent}</code>',
+	'suivant_precedent_slogan' => 'Ajoute des critères de boucles <code>{suivant}</code> et <code>{precedent}</code>'
 );
+
 ?>
