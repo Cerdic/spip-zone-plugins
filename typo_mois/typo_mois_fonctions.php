45b4ff2efdcd2506b3baca94f8d1a0d3c908152c
fil@rezo.net <fil@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-02 10:21:27 +0000

    ajouter \b dans la regexp pour eviter d'ajouter un insecable sur '5 maisons' (cerdic, report de http://zone.spip.org/trac/spip-zone/changeset/71442)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71449 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- typo_mois/typo_mois_fonctions.php
+++ typo_mois/typo_mois_fonctions.php
@@ -9,7 +9,7 @@ function typo_mois_mois($texte){
 		$pre1 = _T('date_jnum1');
 		$pre2 = _T('date_jnum2');
 		$pre3 = _T('date_jnum3');
-		$typo = ",([1-3]?[0-9]|$pre1|$pre2|$pre3) (".join('|', $typo).'),UimsS';
+		$typo = ",([1-3]?[0-9]|$pre1|$pre2|$pre3) (".join('|', $typo).')\b,UimsS';
 		include_spip('inc/charsets');
 		$typo = unicode2charset(html2unicode($typo));
 	}
