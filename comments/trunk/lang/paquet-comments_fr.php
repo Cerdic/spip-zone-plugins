cf16cc796db0516ce3677cc3a939850bbcc8452b
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-02 01:37:35 +0000

    langues (paquet-comments)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71421 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- comments/trunk/lang/paquet-comments_fr.php
+++ comments/trunk/lang/paquet-comments_fr.php
@@ -1,13 +1,13 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/comments/comments-200/lang/
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/comments/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-	'comments_slogan' => 'Des commentaires, tout simplement',
-	'comments_description' => 'ATTENTION, VERSION EN DEVELOPPEMENT, POUR SPIP&nbsp;3&nbsp;!<br />Affichage des messages en liste, façon commentaires de blog, avec formulaire simplifié. Commentaires microformatés, nomenclature homogène.',
-
+	// C
+	'comments_description' => 'ATTENTION, VERSION EN DEVELOPPEMENT, POUR SPIP 3 !<br />Affichage des messages en liste, façon commentaires de blog, avec formulaire simplifié. Commentaires microformatés, nomenclature homogène.',
+	'comments_slogan' => 'Des commentaires, tout simplement'
 );
 
 ?>
