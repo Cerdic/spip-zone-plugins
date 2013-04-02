5656f7682d239ae006924b7c993b0615deec8790
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-02 01:38:11 +0000

    langues (paquet-socialtags)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71434 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- socialtags/lang/paquet-socialtags_en.php
+++ socialtags/lang/paquet-socialtags_en.php
@@ -1,18 +1,16 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-socialtags
-// Langue: en
-// Date: 16-04-2012 21:31:34
-// Items: 1
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-socialtags?lang_cible=en
+// ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// S
-	'socialtags_slogan' => 'Activate the social link sharing network buttons.',
+	// S
+	'socialtags_description' => 'Le plugin Social Tags permet d\'ajouter des icônes de partage de liens vers les sites tels que Digg, Facebook, Delicious...
+En outre, si le plugin Google+1 est actif, il est possible de rajouter le bouton correspondant dans la liste des liens de Social tags. Il propose également des noisettes paramétrables permettant d\'afficher les badges et boites de type "J\'aime" de Facebook.', # NEW
+	'socialtags_slogan' => 'Activate the social link sharing network buttons.'
 );
+
 ?>
--- socialtags/lang/paquet-socialtags_fr.php
+++ socialtags/lang/paquet-socialtags_fr.php
@@ -1,20 +1,14 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-socialtags
-// Langue: fr
-// Date: 16-04-2012 21:31:34
-// Items: 2
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/socialtags/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// S
+	// S
 	'socialtags_description' => 'Le plugin Social Tags permet d\'ajouter des icônes de partage de liens vers les sites tels que Digg, Facebook, Delicious...
 En outre, si le plugin Google+1 est actif, il est possible de rajouter le bouton correspondant dans la liste des liens de Social tags. Il propose également des noisettes paramétrables permettant d\'afficher les badges et boites de type "J\'aime" de Facebook.',
-	'socialtags_slogan' => 'Activer des boutons de partage de liens sur les réseaux sociaux.',
+	'socialtags_slogan' => 'Activer des boutons de partage de liens sur les réseaux sociaux.'
 );
+
 ?>
