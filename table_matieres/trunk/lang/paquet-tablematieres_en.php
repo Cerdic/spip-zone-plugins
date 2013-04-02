c1e1b8a71081f67e2c13f977a56b50616d50d054
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-02 01:38:14 +0000

    langues (paquet-tablematieres)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71435 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- table_matieres/trunk/lang/paquet-tablematieres_en.php
+++ table_matieres/trunk/lang/paquet-tablematieres_en.php
@@ -1,21 +1,17 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-tablematieres
-// Langue: en
-// Date: 01-04-2013 15:30:49
-// Items: 3
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-tablematieres?lang_cible=en
+// ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// T
+	// T
 	'tablematieres_description' => 'Introduce an anchor for each heading of a #TEXTE field of the articles, and an 
 	associated optional tag #TABLE_MATIERES to put in the ARTICLES loops.',
 	'tablematieres_nom' => 'Table of contents',
-	'tablematieres_slogan' => 'Create the summary of an article',
+	'tablematieres_slogan' => 'Create the summary of an article'
 );
+
 ?>
--- table_matieres/trunk/lang/paquet-tablematieres_fr.php
+++ table_matieres/trunk/lang/paquet-tablematieres_fr.php
@@ -1,21 +1,15 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-tablematieres
-// Langue: fr
-// Date: 01-04-2013 15:30:49
-// Items: 3
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/table_matieres/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// T
+	// T
 	'tablematieres_description' => 'Introduit une ancre translitterée pour chaque intertitre du champ #TEXTE des ARTICLES,
 	ainsi qu\'une balise #TABLE_MATIERES (facultative) associée à placer, bien sûr, dans une boucle ARTICLES.',
 	'tablematieres_nom' => 'Table des Matières',
-	'tablematieres_slogan' => 'Créer le sommaire d\'un article',
+	'tablematieres_slogan' => 'Créer le sommaire d\'un article'
 );
+
 ?>
