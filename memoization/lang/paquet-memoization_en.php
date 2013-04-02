43889db7e53d59b608089d01f1f3c980aa3ddea2
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-02 01:37:56 +0000

    langues (paquet-memoization)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71428 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- memoization/lang/paquet-memoization_en.php
+++ memoization/lang/paquet-memoization_en.php
@@ -1,19 +1,15 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-memoization
-// Langue: en
-// Date: 01-04-2013 15:14:36
-// Items: 2
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-memoization?lang_cible=en
+// ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// M
+	// M
 	'memoization_description' => 'Memoization is a computer science term, which Wikipedia defines as "a technique of reducing the execution time of a function by storing the results once on the other." It is also the name of a new function library for PHP, which follows my experiments XCache.',
-	'memoization_slogan' => 'Various methods to process SPIP\'s cache.',
+	'memoization_slogan' => 'Various methods to process SPIP\'s cache.'
 );
+
 ?>
--- memoization/lang/paquet-memoization_fr.php
+++ memoization/lang/paquet-memoization_fr.php
@@ -1,19 +1,13 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-memoization
-// Langue: fr
-// Date: 01-04-2013 15:14:36
-// Items: 2
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/memoization/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// M
+	// M
 	'memoization_description' => 'Mémoïzation est un terme d’informatique, dont Wikipédia donne la définition suivante : « une technique consistant à réduire le temps d’exécution d’une fonction en mémorisant ses résultats d’une fois sur l’autre ». C’est aussi le nom d’une nouvelle librairie de fonctions pour PHP, qui fait suite à mes expérimentations sur XCache.',
-	'memoization_slogan' => 'Gestion du cache de SPIP par diverses méthodes.',
+	'memoization_slogan' => 'Gestion du cache de SPIP par diverses méthodes.'
 );
+
 ?>
