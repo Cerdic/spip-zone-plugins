8d5e62136aad2097af97a17b5f56e40d70bf655c
cyril@cym.fr <cyril@cym.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-03 13:55:36 +0000

    ajout d'une saisie pour les collaborateurs et d'un formulaire de configuration;
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71520 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- dayfill/trunk/lang/dayfill_fr.php
+++ dayfill/trunk/lang/dayfill_fr.php
@@ -2,7 +2,7 @@
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
- 
+
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// D
@@ -12,6 +12,13 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'cfg_exemple' => 'Exemple',
 	'cfg_exemple_explication' => 'Explication de cet exemple',
 	'cfg_titre_parametrages' => 'Paramétrages',
+
+	// E
+	'explication_id_auteur' => "Sélectionnez des collaborateurs potentiels parmi tous les auteurs du site.",
+
+	//
+	'label_auteurs_projet' => "Déjà attribués au projet",
+	'label_auteurs_potentiels' => "Collaborateurs potentiels",
 
 	// T
 	'titre_page_configurer_dayfill' => 'Configuer le gestionnaire d\'activités',
