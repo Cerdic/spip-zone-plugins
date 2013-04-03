2c7c583672845fd0315ce5add07d2e1abfde9b7c
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-03 01:36:49 +0000

    langues (collection)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71497 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- emballe_medias/media_collections/trunk/lang/collection_en.php
+++ emballe_medias/media_collections/trunk/lang/collection_en.php
@@ -13,12 +13,12 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// B
 	'bouton_diogene_statut_poubelle_normal' => 'Put your collection in the dustbin',
 	'bouton_diogene_statut_poubelle_normal_pas_auteur' => 'Put this collection in the dustbin',
-	'bouton_dissocier' => 'Dissocier', # NEW
-	'bouton_fermer_collection' => 'Rendre cette collection personnelle', # NEW
+	'bouton_dissocier' => 'Unlink',
+	'bouton_fermer_collection' => 'Make this collection personal',
 	'bouton_media_associer' => 'Link to a collection',
-	'bouton_ouvrir_collection' => 'Rendre cette collection collaborative', # NEW
-	'bouton_quitter_collection' => 'Ne plus collaborer à cette collection', # NEW
-	'bouton_rejoindre_collection' => 'Collaborer à cette collection', # NEW
+	'bouton_ouvrir_collection' => 'Make this collection collaborative',
+	'bouton_quitter_collection' => 'No longer collaborate to this collection',
+	'bouton_rejoindre_collection' => 'Collaborate to this collection',
 
 	// C
 	'collections_titre' => 'Collections',
@@ -29,17 +29,17 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// E
 	'erreur_association_collection' => 'The ombination with the collection failed.',
-	'erreur_autorisation_statut_modifier' => 'Vous n\'êtes pas autorisé à modifier cette collection.', # NEW
+	'erreur_autorisation_statut_modifier' => 'You are not allowed to edit this collection.',
 	'erreur_collection_non_existante' => 'This collection doesn\'t exist.',
 	'erreur_media_document' => 'Warning. The type of this document does not match the type of the collection..',
-	'erreur_selectionner_un_media' => 'Veuillez sélectionner au moins un média.', # NEW
+	'erreur_selectionner_un_media' => 'Select at least one media',
 	'explication_genre' => 'Which kind of medias can be linked to that collection ?',
 	'explication_genre_defaut' => 'Default value for the types of media that can be attached (useful when fast creation).',
-	'explication_medias_ajouter' => 'Les médias ci-dessous ne sont pas associés à la collection et peuvent correspondre à son genre. Sélectionnez ceux que vous souhaitez ajouter.', # NEW
-	'explication_medias_modifier' => 'Les médias ci-dessous sont associés à la collection. Pour en dissocier certains, sélectionnez les et appuyez sur le bouton de suppression.', # NEW
-	'explication_medias_modifier_drag' => 'Vous pouvez les réordonner via glisser / déposer.', # NEW
-	'explication_participer' => 'Cliquez sur le bouton ci-dessous pour participer à cette collection.', # NEW
-	'explication_participer_non' => 'Cliquez sur le bouton ci-dessous pour ne plus participer à cette collection.', # NEW
+	'explication_medias_ajouter' => 'Media below are not linked to the collection and may correspond to its kind. Select the ones you want to add.',
+	'explication_medias_modifier' => 'Media below are associated with the collection. To dissociate some, select them and press the delete button.',
+	'explication_medias_modifier_drag' => 'You can reorder them via drag and drop.',
+	'explication_participer' => 'Click the button below to participate to this collection.',
+	'explication_participer_non' => 'Click the button below to no longer participate to this collection.',
 	'explication_type' => 'Which is the use of the collection ?',
 	'explication_type_defaut' => 'Default use of the collection (useful when fast creation).',
 
@@ -54,9 +54,9 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'icone_modifier_collection' => 'Manage this collection',
 	'info_1_collection' => 'A collection',
 	'info_aucun_collection' => 'No collection',
-	'info_collaborateurs' => 'Les collaborateurs :', # NEW
-	'info_collection_creation' => 'Collection créée le <abbr title="@date_abbr@">@date@</abbr> <span class="auteurs">par @auteurs@</span>.', # NEW
-	'info_collection_maj' => 'Mise à jour : ', # NEW
+	'info_collaborateurs' => 'Contributors:',
+	'info_collection_creation' => 'Collection created on <abbr title="@date_abbr@">@date@</abbr> <span class="auteurs">by @auteurs@</span>.',
+	'info_collection_maj' => 'Updated: ',
 	'info_collections_auteur' => 'This author\'s  collections',
 	'info_media_dans_collection' => 'In the collection: ',
 	'info_media_dans_collections' => 'In the @nb@ collections: ',
@@ -64,14 +64,14 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'info_medias_aucun' => 'No media',
 	'info_medias_nb' => '@nb@ medias',
 	'info_nb_collections' => '@nb@ collections',
-	'info_non_autorise_creer_collection' => 'L\'auteur "@id_auteur@" n\'est pas autorisé à créer une collection.', # NEW
-	'info_non_autorise_modifier_collection' => 'L\'auteur "@id_auteur@" n\'est pas autorisé à modifier la collection @id@.', # NEW
-	'info_rang_collection' => 'Rang dans la collection', # NEW
+	'info_non_autorise_creer_collection' => 'The author "@id_auteur@" is not allowed to create a collection.',
+	'info_non_autorise_modifier_collection' => 'The author "@id_auteur@" is not allowed to edit the collection @id@.',
+	'info_rang_collection' => 'Rank in the collection',
 
 	// L
 	'label_choix_collection' => 'Link to the collection',
 	'label_collections_ajouter' => 'Add to the collection',
-	'label_collections_retirer' => 'Retirer de la collection', # NEW
+	'label_collections_retirer' => 'Remove from collection',
 	'label_date' => 'Date',
 	'label_date_point' => 'Date:',
 	'label_descriptif' => 'Description',
@@ -91,17 +91,17 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'label_type_point' => 'Use:',
 	'legende_collection' => 'Collections',
 	'legende_medias_collection_ajouter' => 'Add medias',
-	'legende_medias_collection_modifier' => 'Modifier la liste des médias', # NEW
+	'legende_medias_collection_modifier' => 'Edit the list of media',
 	'lien_editer_collection' => 'Edit this collection',
 	'lien_toutes_collections' => 'All collections',
 
 	// M
-	'message_aucune_collection_critere' => 'Aucune collection ne correspond à vos critères.', # Utile pour des squelettes NEW
-	'message_aucune_collection_publiee' => 'Aucune collection n\'est actuellement publiée.', # Utile pour des squelettes NEW
+	'message_aucune_collection_critere' => 'No collection matches your criteria.', # Utile pour des squelettes
+	'message_aucune_collection_publiee' => 'No collection is currently published.', # Utile pour des squelettes
 	'message_collection_aucun_media_publie' => 'No media which you are the author is available',
-	'message_collection_media_utiliser_autres_auteurs' => 'Vous pouvez lier les médias d\'autres auteurs en utilisant le formulaire d\'ajout sur chaque page de média.', # NEW
-	'message_collection_media_utiliser_interface' => 'Publiez un ou plusieurs médias via l\'interface dédiée afin de pouvoir les ajouter à cette collection.', # NEW
-	'message_collection_reorganisee' => 'La collection a été réorganisée.', # NEW
+	'message_collection_media_utiliser_autres_auteurs' => 'You can link media of other authors using form for adding on each page of media.',
+	'message_collection_media_utiliser_interface' => 'Publish one or more media via the dedicated interface in order to add them to this collection.',
+	'message_collection_reorganisee' => 'The collection has been reorganized.',
 	'message_collection_vide' => 'This collection is empty.',
 	'message_nombre_ajoute' => '1 linked media.',
 	'message_nombre_ajoutes' => '@nb@ linked medias.',
@@ -114,8 +114,8 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'option_erreur_genre' => 'The type does not match the media: @genre@',
 
 	// P
-	'publier_une_collection' => 'Une collection', # NEW
-	'publier_une_collection_desc' => 'Créer une collection de médias.', # NEW
+	'publier_une_collection' => 'A collection',
+	'publier_une_collection_desc' => 'Create a media collection.',
 
 	// R
 	'retirer_lien_collection' => 'Unlink this collection',
@@ -131,7 +131,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'title_voir_media' => 'Display the media: @titre@',
 	'titre_collection' => 'Collection',
 	'titre_collections' => 'Collections',
-	'titre_collections_rubrique' => 'Rubriques de la rubrique', # NEW
+	'titre_collections_rubrique' => 'Sections of the section',
 	'titre_creer_collection_rapide' => 'Create a collection',
 	'titre_langue_collection' => 'Language of this collection',
 	'titre_logo_collection' => 'Logo de this collection',
@@ -140,7 +140,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'titre_modifier_collection_diogene' => 'Manage this collection',
 	'titre_page_collections' => 'Collections',
 	'titre_page_configurer_collections' => 'Configure the collections',
-	'titre_participer' => 'Participer', # NEW
+	'titre_participer' => 'Participate',
 	'type_coop' => 'Cooperative',
 	'type_perso' => 'Personnal'
 );
