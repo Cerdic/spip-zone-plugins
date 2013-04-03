bf3e7488007818db2e4371a0fba476e959b9ff1e
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-03 01:36:55 +0000

    langues (paquet-em_doc2img)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71499 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ emballe_medias/emballe_medias_doc2img/trunk/lang/paquet-em_doc2img_es.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-em_doc2img?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// E
+	'em_doc2img_description' => 'Complemento Doc2Img para "Envuelve Medias" (utiliza doc2img)', # MODIF
+	'em_doc2img_nom' => 'Envuelve Medias - Doc2Img',
+	'em_doc2img_slogan' => 'Complemento Doc2Img para "Envuelve Medias" (utiliza doc2img)' # MODIF
+);
+
+?>
