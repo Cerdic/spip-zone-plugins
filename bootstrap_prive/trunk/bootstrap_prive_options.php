82c5e1584ba4e3de58ed8cdbf47fcb095ac705e8
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-03 10:15:21 +0000

    Comme promis et discuté sur IRC avec Cédric, on déplace Bootstrap Privé de _squelettes_ à _plugins_ qui lui correspond mieux.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71507 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ bootstrap_prive/trunk/bootstrap_prive_options.php
@@ -0,0 +1,9 @@
+<?php
+// define('_LOG_FILELINE', true);
+// define('_LOG_FILTRE_GRAVITE', 8);
+if (test_espace_prive()) {
+    _chemin(_DIR_PLUGIN_BOOTSTRAP . 'bootstrap');
+    _chemin(_DIR_PLUGIN_BOOTSTRAP . 'bootstrap2spip');
+}
+
+?>
