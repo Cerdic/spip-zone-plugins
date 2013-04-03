90bd06b5cbfdffe3d6d14e5f8e45b6d742fcfb5b
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-03 01:36:58 +0000

    langues (em_spipmotion)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71500 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ emballe_medias/emballe_medias_spipmotion/trunk/lang/em_spipmotion_es.php
@@ -0,0 +1,31 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/em_spipmotion?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'autre_version_format' => 'Este documento ha sido automáticamente codificado en @format@.',
+	'autres_versions' => 'Otras versiones:',
+	'autres_versions_formats' => 'Este documento se ha codificado en los formatos:',
+
+	// I
+	'info_encodage_pas_prevu' => 'Este documento no será codificado.',
+	'info_previsu' => 'Previsualización',
+
+	// L
+	'lien_recharger_voir_player' => 'Recargar el lector',
+
+	// M
+	'message_document_attente_encodage' => 'Este documento está en la lista de espera de codificación.',
+	'message_document_encours_encodage' => 'Este documento está en curso de codificación.',
+
+	// T
+	'title_infos_cacher' => 'Ocultar la información adicional',
+	'title_infos_voir' => 'Ver la información adicional'
+);
+
+?>
