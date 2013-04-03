0123c1d0054bebf0c079feabf93978a1ce22298f
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-03 01:36:53 +0000

    langues (em_doc2img)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71498 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ emballe_medias/emballe_medias_doc2img/trunk/lang/em_doc2img_es.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/em_doc2img?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// I
+	'info_nb_doc2img' => 'Este documento ha sido automáticamente convertido en una serie de @nb@ imágenes.',
+	'info_nb_doc2img_un' => 'Este documento ha sido automáticamente convertido en una imagen.'
+);
+
+?>
