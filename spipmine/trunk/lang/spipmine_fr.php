c4fc27fcfb5c257d417f4ce0051c602ab723a3d1
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-05 14:07:55 +0000

    On commit les premiers fichiers mais ils sont à travailler en relation des nouvelles versions de Projets, Factures, Contact et Organisations. Donc, en standby pour quelques petits jours encore.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71625 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ spipmine/trunk/lang/spipmine_fr.php
@@ -0,0 +1,45 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+    if (!defined('_ECRIRE_INC_VERSION')) return;
+
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+    //A
+    'aller_projet' => 'Aller au projet',
+    'aller_projet_num' => 'Aller au projet N°',
+    'attribuer_maquette_objet' => 'Attribuer une maquette à l\'objet',
+    //C
+    'choisir_benchmark_projet' => 'Choisir un benchmark pour le projet',
+    'choisir_cdc_objet' => 'Choisir un cahier des charges pour l\'objet',
+    'choisir_cdev_objet' => 'Choisir un cahier de développement pour l\'objet',
+    'commentaires' => "Commentaires",
+
+
+    //E
+    'enjeux_client' => "Enjeux pour le client",
+
+    //L
+    'label_factures_liees' => "Factures liées",
+
+    //M
+    'methode' => "Méthodologie proposée",
+
+    //O
+    'objectifs' => 'Objectifs à atteindre',
+
+    //P
+    'projet_precedent' => 'Projet précédent',
+    'projet_suivant' => 'Projet suivant',
+
+    // V
+    'voir_benchmark_projet' => 'Voir le benchmark sites du projet',
+    'voir_cdev_num' => 'Voir le cahier de développement N°',
+    'voir_cdc_num' => 'Voir le cahier des charges N°',
+    'voir_clients' => 'Voir tous les clients',
+    'voir_en_ligne' => 'Voir en ligne...',
+    'voir_maquette_num' => 'Voir la maquette N°',
+    'voir_projet_num' => 'Voir le projet N°',
+
+);
+?>
