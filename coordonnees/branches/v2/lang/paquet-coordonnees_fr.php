5f83ac09a27966c6f68b541a2c83f8cf28187bcf
toutati@free.fr <toutati@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-05 17:56:01 +0000

    Autant que ça marche avec SPIP3
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71656 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- coordonnees/branches/v2/lang/paquet-coordonnees_fr.php
+++ coordonnees/branches/v2/lang/paquet-coordonnees_fr.php
@@ -3,7 +3,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'coordonnees_nom' => "Coordonnées",
-	'coordonnees_slogan' => "Des coordonné;es postales, téléphoniques... pour vos auteurs",
+	'coordonnees_slogan' => "Des coordonnées postales, téléphoniques... pour vos auteurs",
 	'coordonnees_description' => "Ajoute la possibilité de donner des coordonnées postales (adresses, mails, téléphones) aux objets de SPIP, et tout particulièrement aux auteurs. Plusieurs adresses et numéros de téléphone peuvent ainsi être donnés à chaque auteur.",
 );
 
