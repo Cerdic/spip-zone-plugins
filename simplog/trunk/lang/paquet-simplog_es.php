eee4eb39acff27b0d2eb598c081363b31718bc79
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-05 01:37:01 +0000

    langues (paquet-simplog)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71584 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ simplog/trunk/lang/paquet-simplog_es.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-simplog?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// S
+	'simplog_description' => 'Una vez el plugin está activado, los administradores tienen la posibilidad de consultar todos los periódicos disponibles en el sitio. Una entrada del menú Mantenimiento permite ir a la página de consulta.',
+	'simplog_nom' => 'Simples Logs',
+	'simplog_slogan' => 'Visualización de los logs SPIP por los administradores'
+);
+
+?>
