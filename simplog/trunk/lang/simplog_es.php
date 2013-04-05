00df018df27cd9cb535db970c96692855ca47462
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-05 01:36:59 +0000

    langues (simplog)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71583 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ simplog/trunk/lang/simplog_es.php
@@ -0,0 +1,45 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/simplog?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'actualiser' => 'Actualizar la lista',
+
+	// C
+	'clic_rafraichir' => 'Recargar el archivo',
+	'colonne_date' => 'Fecha',
+	'colonne_gravite' => 'Gravedad',
+	'colonne_hit' => 'Hit',
+	'colonne_ip' => 'IP',
+	'colonne_pid' => 'PID',
+	'colonne_texte' => 'Texto',
+
+	// D
+	'description_simplog' => 'Esta página permite visualizar simplemente los periódicos contenidos en los repertorios de SPIP.',
+
+	// F
+	'fichier' => 'Archivo «@fichier@»',
+
+	// I
+	'info_hit_pri' => 'Privado',
+	'info_hit_pub' => 'Público',
+
+	// L
+	'liste_fic' => 'Lista de periódicos',
+	'logs' => 'Consulta de periódicos',
+
+	// M
+	'message_fichier_choisir' => 'Elija un archivo a analizar.',
+	'message_fichier_inconnu' => 'El archivo "@fichier@" no está disponible.',
+
+	// T
+	'tri_croissant' => 'Orden descendente',
+	'tri_decroissant' => 'Orden descendiente'
+);
+
+?>
