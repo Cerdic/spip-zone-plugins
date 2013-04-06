f5f28372c41a475769c11d73d5796443c7db5ad3
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-06 01:38:16 +0000

    langues (paquet-socialtags)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71688 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ socialtags/lang/paquet-socialtags_es.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-socialtags?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// S
+	'socialtags_description' => 'El plugin Social Tags permite añadir iconos de compartir enlaces a los sitios como Digg, Facebook, Delicious...
+Por otro lado, si el plugin Google+1 está activo, se puede agregar el botón correspondiente en la lista de los enlaces de liens de Social tags. Propone asimismo nueces configurables que permiten mostrar botones y cajas del tipo "Me gusta" de Facebook.',
+	'socialtags_slogan' => 'Activar botones de compartir de enlaces en las redes sociales.'
+);
+
+?>
