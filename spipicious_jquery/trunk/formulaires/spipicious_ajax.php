750e2ba97fe6bdb5985a55cd45eabdfc34a03f48
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-06 13:12:01 +0000

    PHPdoc et quelques bidouilles
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71712 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spipicious_jquery/trunk/formulaires/spipicious_ajax.php
+++ spipicious_jquery/trunk/formulaires/spipicious_ajax.php
@@ -1,31 +1,59 @@
 <?php
-
 /**
- * spip.icio.us
+ * SPIP.icio.us
  * Gestion de tags lies aux auteurs
  *
  * Auteurs :
  * kent1 (http://www.kent1.info - kent1@arscenic.info)
- * Erational
+ * Erational (http://www.erational.org)
  *
- * © 2007-2012 - Distribue sous licence GNU/GPL
- *
+ * © 2007-2013 - Distribue sous licence GNU/GPL
+ * 
+ * Formulaire d'ajout de tags
+ * 
+ * @package SPIP\SPIPicious\Formulaires
  */
 
-if (!defined("_ECRIRE_INC_VERSION")) return;	#securite
+if (!defined("_ECRIRE_INC_VERSION")) return;
 
+/**
+ * Chargement du formulaire d'ajout/suppression de tags
+ * 
+ * @param int id_objet
+ * 		L'identifiant numérique de l'objet à tagger
+ * @param string $type
+ * 		Le type d'objet à tagger
+ * @param string $retour
+ * 		Une URL de retour
+ * @return array $valeurs
+ * 		Les valeurs chargées dans le formulaire
+ */
 function formulaires_spipicious_ajax_charger($id_objet,$type='article',$retour='') {
 	include_spip('inc/autoriser');
-	if(!autoriser('tagger_spipicious',$type,$id_objet)){
+	if(!autoriser('tagger_spipicious',$type,$id_objet))
 		return array('editable'=> false);
-	}
 
 	$id_type = id_table_objet($type);
+	
+	if(!function_exists('lire_config'))
+		include_spip('inc/config');
 	$id_groupe = lire_config('spipicious/groupe_mot');
 	$valeurs = array('type'=>$type,'type'=>$type,'id_objet'=>$id_objet,'spipicious_groupe'=>$id_groupe);
 	return $valeurs;
 }
 
+/**
+ * Traitement du formulaire d'ajout/suppression de tags
+ * 
+ * @param int id_objet
+ * 		L'identifiant numérique de l'objet à tagger
+ * @param string $type
+ * 		Le type d'objet à tagger
+ * @param string $retour
+ * 		Une URL de retour
+ * @return array
+ * 		Le tableau de tous les CVT avec editable et message
+ */
 function formulaires_spipicious_ajax_traiter($id_objet,$type,$retour='') {
 	$add_tags = _request('add_tags');
 	$remove_tag = _request('remove_tags');
@@ -42,7 +70,7 @@ function formulaires_spipicious_ajax_traiter($id_objet,$type,$retour='') {
 	}
 
 	if($invalider){
-		include_spip ("inc/invalideur");
+		include_spip("inc/invalideur");
 		suivre_invalideur("1");
 	}
 
