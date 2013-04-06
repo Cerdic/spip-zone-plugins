ac6d479263abd448eaf1f1ae02840ae1ae7cbccd
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-06 12:49:55 +0000

    PHPdoc
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71709 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spipicious_jquery/trunk/spipicious_autoriser.php
+++ spipicious_jquery/trunk/spipicious_autoriser.php
@@ -1,15 +1,17 @@
 <?php
 /**
- * spip.icio.us
+ * SPIP.icio.us
  * Gestion de tags lies aux auteurs
  *
  * Auteurs :
  * kent1 (http://www.kent1.info - kent1@arscenic.info)
- * Erational
+ * Erational (http://www.erational.org)
  *
- * © 2007-2012 - Distribue sous licence GNU/GPL
+ * © 2007-2013 - Distribue sous licence GNU/GPL
  * 
  * Fichier des autorisations du plugin
+ * 
+ * @package SPIP\SPIPicious\Autorisations
  */
 
 if (!defined("_ECRIRE_INC_VERSION")) return;
@@ -17,8 +19,6 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 /**
  * Déclarer l'utilisation du pipeline
  * Cela évite de recalculer les pipeline tout le temps
- *
- * @return
  */
 function spipicious_autoriser(){}
 
@@ -26,12 +26,18 @@ function spipicious_autoriser(){}
  * Fonction définissant qui est autorisé à tagger
  * Se base sur la configuration de spip.icio.us
  *
- * @param string $faire l'action d'autoriser
- * @param string $type peut être article,rubrique,breve,forum...
- * @param int $id l'id du type sur lequel on souhaite ajouter un tag
- * @param array $qui en général $visiteur_session
- * @param object $opt
- * @return quelquechose (si oui) / rien (si non)
+ * @param string $faire 
+ * 		L'action d'autoriser
+ * @param string $type 
+ * 		L'objet sur lequel on souhaite tagger, peut être article,rubrique,breve,forum...
+ * @param int $id 
+ * 		L'id de l'objet type sur lequel on souhaite ajouter un tag
+ * @param array $qui 
+ * 		La session visiteur
+ * @param array $opt
+ * 		Les options mais pas utilisées
+ * @return bool 
+ * 		true si ok, false sinon
  */
 function autoriser_tagger_spipicious_dist($faire, $type, $id, $qui, $opt){
 	return  (in_array($qui['statut'],lire_config('spipicious/people',array('0minirezo'))));
