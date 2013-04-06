8d099387f285e667700f943df377f3d6fd5a594b
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-06 01:38:05 +0000

    langues (paquet-pp_codes)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71685 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ porte_plume_extras/codes/trunk/lang/paquet-pp_codes_es.php
@@ -0,0 +1,17 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-pp_codes?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// P
+	'pp_codes_description' => 'Gestión de botones para insertar el código informático a través de la barra de edición Portaplumas.
+Añade también un acceso directo <code>[->ecrire/inc_versions.php#trac]</code>.',
+	'pp_codes_nom' => 'Códigos Informáticos para Portaplumas',
+	'pp_codes_slogan' => 'Añade botones a la barra de herramientas del Portaplumas para generar códigos informáticos'
+);
+
+?>
