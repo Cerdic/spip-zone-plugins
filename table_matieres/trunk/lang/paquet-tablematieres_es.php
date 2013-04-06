0319544da136f37ca751189ab05c22b77ffb13b0
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-06 01:38:29 +0000

    langues (paquet-tablematieres)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71693 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ table_matieres/trunk/lang/paquet-tablematieres_es.php
@@ -0,0 +1,17 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-tablematieres?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// T
+	'tablematieres_description' => 'Introduce un ancla transcrita para cada encabezado del campo #TEXTE de los ARTÍCULOS,
+	así como una etiqueta #TABLE_MATIERES (opcional) asociada a placer, seguro, en un bucle ARTICLES.',
+	'tablematieres_nom' => 'Tabla de Materias',
+	'tablematieres_slogan' => 'Crear el resumen de un artículo'
+);
+
+?>
