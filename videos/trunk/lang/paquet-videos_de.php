bff38cb9295f133984c9dab11136386b2ce69b94
willmann@netzsachen.de <willmann@netzsachen.de@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-06 01:38:36 +0000

    langues (paquet-videos)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71696 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ videos/trunk/lang/paquet-videos_de.php
@@ -0,0 +1,20 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-videos?lang_cible=de
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// V
+	'videos_description' => 'Dieses Plugin ermöglicht das Hinzufügen von Videos, die auf Plattformen eingestellt sind 
+ (Dailymotion, Vimeo, Youtube, CultureBox), mittels einfachem Copy&Paste der URL.
+Es ermöglicht auch die HTML5- Anzeige von Videos
+in den Formaten MP4, H264, Ogg,
+WebM und Mkv sogra auf mobilen Endgeräten.',
+	'videos_nom' => 'Video(s)',
+	'videos_slogan' => 'Verwaltungsoberfläche und Modell zum Einfügen von Videos'
+);
+
+?>
