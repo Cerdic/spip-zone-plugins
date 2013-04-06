f8ce2f6643c57c5e2da88e958541b95d0bb88331
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-06 01:37:20 +0000

    langues (agenda)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71670 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- agenda/trunk/lang/agenda_es.php
+++ agenda/trunk/lang/agenda_es.php
@@ -8,141 +8,141 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
-	'absence_prise_en_compte' => 'Tu ausencia ha sido registrada',
+	'absence_prise_en_compte' => 'Su ausencia se ha registrado',
 	'activite_editoriale' => 'Actividad Editorial',
 	'afficher_calendrier' => 'Mostrar el calendario',
 	'agenda' => 'Agenda',
 	'ajouter_repetition' => 'Añadir repeticiones',
 	'ajouter_un_evenement' => 'Añadir un evento a este artículo',
-	'annee_precedente' => 'Año anterior', # MODIF
+	'annee_precedente' => 'Año anterior',
 	'annee_suivante' => 'Año siguiente',
 	'aucun_evenement' => 'Ningún evento',
-	'aucun_inscrit' => 'Aucun inscrit', # NEW
-	'aucune_rubrique_mode_agenda' => 'Por defecto, se puede crear eventos sobre cualquier sección. Si activas el modo agenda sobre una o más secciones, la gestión la creación de eventos se limitará a esa/s ramas.',
+	'aucun_inscrit' => 'Ningún inscrito',
+	'aucune_rubrique_mode_agenda' => 'Por defecto, todas las secciones permiten utilizar los eventos. Si activa el modo agenda sobre una o más secciones, la gestión de enventos se limitará a su rama.',
 
 	// B
 	'bouton_annuler' => 'Cancelar',
 	'bouton_supprimer' => 'Eliminar',
 
 	// C
-	'confirm_suppression_inscription' => 'Voulez-vous vraiment supprimer cette inscription ?', # NEW
-	'confirm_suppression_inscription_toutes' => 'Voulez-vous vraiment supprimer toutes les inscriptions ?', # NEW
-	'connexion_necessaire_pour_inscription' => 'Merci de <a href=\'#LOGIN_PUBLIC\'>vous connecter</a> pour pouvoir vous inscrire à cet événement.', # NEW
+	'confirm_suppression_inscription' => '¿Desea realmente eliminar esta inscripción?',
+	'confirm_suppression_inscription_toutes' => '¿Desea realmente eliminar todas las inscripciones?',
+	'connexion_necessaire_pour_inscription' => 'Gracias por <a href=\'#LOGIN_PUBLIC\'>conectarse</a> para poder inscribirse a este evento.',
 	'creer_evenement' => 'Crear un evento',
 
 	// E
-	'erreur_article_interdit' => 'No tiene permiso para asociar este evento al artículo',
-	'erreur_article_manquant' => 'Debes indicar un artículo',
-	'erreur_date' => 'Cette date est incorrecte', # NEW
-	'erreur_date_avant_apres' => 'Indiquez une date de fin après la date de début !', # NEW
-	'erreur_date_corrigee' => 'La date a été corrigée', # NEW
-	'erreur_heure' => 'Cette heure est incorrecte', # NEW
-	'erreur_heure_corrigee' => 'L\'heure a été corrigée', # NEW
+	'erreur_article_interdit' => 'No tiene permiso para asociar este evento a este artículo',
+	'erreur_article_manquant' => 'Debe indicar un artículo',
+	'erreur_date' => 'Esta fecha es incorrecta',
+	'erreur_date_avant_apres' => '¡Indique una fecha de finalización posterior a la fecha de inicio!',
+	'erreur_date_corrigee' => 'La fecha ha sido corregida',
+	'erreur_heure' => 'Esta hora es incorrecta',
+	'erreur_heure_corrigee' => 'La hora ha sido corregida',
 	'evenement_adresse' => 'Dirección',
-	'evenement_article' => 'Asoaciar al artículo',
-	'evenement_autres_occurences' => 'Otras ocurrencias :',
+	'evenement_article' => 'Asociado al artículo',
+	'evenement_autres_occurences' => 'Otras ocasiones:',
 	'evenement_date' => 'Fecha',
 	'evenement_date_a' => 'a las ',
 	'evenement_date_a_immediat' => 'a las ',
-	'evenement_date_au' => 'Hasta ',
+	'evenement_date_au' => 'Al',
 	'evenement_date_de' => 'De ',
-	'evenement_date_debut' => 'Fecha de comienzo',
+	'evenement_date_debut' => 'Fecha de inicio',
 	'evenement_date_du' => 'Del ',
-	'evenement_date_fin' => 'Fecha de fin',
-	'evenement_date_inscription' => 'Date d\'inscription', # NEW
+	'evenement_date_fin' => 'Fecha de finalización',
+	'evenement_date_inscription' => 'Fecha de inscripción',
 	'evenement_descriptif' => 'Descripción',
 	'evenement_horaire' => 'Todo el día',
-	'evenement_lieu' => 'Ubicación',
+	'evenement_lieu' => 'Lugar',
 	'evenement_repetitions' => 'Repeticiones',
 	'evenement_titre' => 'Título',
 	'evenements' => 'Eventos',
-	'evenements_a_venir' => 'próximos',
-	'evenements_depuis_debut' => 'Todos',
+	'evenements_a_venir' => 'Próximos',
+	'evenements_depuis_debut' => 'Desde el inicio',
 
 	// F
 	'fermer' => 'cerrar',
 
 	// I
 	'icone_creer_evenement' => 'Crear un nuevo evento',
-	'icone_modifier_evenement' => 'Modifier l\'événement', # NEW
-	'indiquez_votre_choix' => 'Indiquez votre choix', # NEW
-	'info_1_mois' => '1 mois', # NEW
-	'info_1_place' => '1 place', # NEW
+	'icone_modifier_evenement' => 'Modificar el evento',
+	'indiquez_votre_choix' => 'Indique su elección',
+	'info_1_mois' => '1 mes',
+	'info_1_place' => '1 plaza',
 	'info_aucun_evenement' => 'Ningún evento',
-	'info_evenement' => 'Événement', # NEW
-	'info_evenement_poubelle' => 'Événement supprimé', # NEW
-	'info_evenement_propose' => 'Événement proposé', # NEW
-	'info_evenement_publie' => 'Événement publié', # NEW
+	'info_evenement' => 'Evento',
+	'info_evenement_poubelle' => 'Evento eliminado',
+	'info_evenement_propose' => 'Evento propuesto',
+	'info_evenement_publie' => 'Evento publicado',
 	'info_evenements' => 'Eventos',
-	'info_inscription' => 'Inscription en ligne :', # NEW
-	'info_lieu' => 'Lieu :', # NEW
-	'info_nb_inscrits' => '@nb@ inscrits', # NEW
-	'info_nb_mois' => '@nb@ mois', # NEW
-	'info_nb_places' => '@nb@ places', # NEW
+	'info_inscription' => 'Inscripción en línea:',
+	'info_lieu' => 'Lugar:',
+	'info_nb_inscrits' => '@nb@ inscritos',
+	'info_nb_mois' => '@nb@ meses',
+	'info_nb_places' => '@nb@ plazas',
 	'info_nombre_evenements' => '@nb@ eventos',
-	'info_nouvel_evenement' => 'Nouvel événement', # NEW
-	'info_reponse_inscription_non' => 'non', # NEW
-	'info_reponse_inscription_nsp' => '?', # NEW
-	'info_reponse_inscription_oui' => 'oui', # NEW
-	'info_reponse_inscriptions' => 'Réponse', # NEW
-	'info_reponses_inscriptions' => 'Réponses :', # NEW
+	'info_nouvel_evenement' => 'Nuevo evento',
+	'info_reponse_inscription_non' => 'no',
+	'info_reponse_inscription_nsp' => '¿?',
+	'info_reponse_inscription_oui' => 'sí',
+	'info_reponse_inscriptions' => 'Respuesta',
+	'info_reponses_inscriptions' => 'Respuestas:',
 	'info_un_evenement' => '1 evento',
-	'info_un_inscrit' => 'Un inscrit', # NEW
-	'inscrits' => 'Inscriptions', # NEW
+	'info_un_inscrit' => 'Un inscrito',
+	'inscrits' => 'Inscripciones',
 
 	// L
-	'label_annee' => 'Année', # NEW
+	'label_annee' => 'Año',
 	'label_inscription' => 'Inscripción en línea ',
-	'label_periode_saison' => 'Saison', # NEW
+	'label_periode_saison' => 'Estación',
 	'label_places' => 'Limitar el número de lugares',
 	'label_reponse_jyparticipe' => 'Asistiré',
 	'label_reponse_jyparticipe_pas' => 'No asistiré',
 	'label_reponse_jyparticipe_peutetre' => 'Tal vez asista',
-	'label_vous_inscrire' => 'Tu participación',
-	'lien_desinscrire' => 'Supprimer', # NEW
-	'lien_desinscrire_tous' => 'Supprimer toutes les inscriptions', # NEW
+	'label_vous_inscrire' => 'Su participación',
+	'lien_desinscrire' => 'Eliminar',
+	'lien_desinscrire_tous' => 'Eliminar todas las inscripciones',
 	'lien_retirer_evenement' => 'Eliminar',
-	'liste_inscrits' => 'Liste des inscriptions', # NEW
+	'liste_inscrits' => 'Lista de inscripciones',
 
 	// M
 	'mois_precedent' => 'mes anterior',
 	'mois_suivant' => 'mes siguiente',
 
 	// N
-	'nb_repetitions' => '@nb@  repeticiones',
+	'nb_repetitions' => '@nb@ repeticiones',
 
 	// P
-	'participation_incertaine_prise_en_compte' => 'Tu posible participación ha sido registrada',
-	'participation_prise_en_compte' => 'Tu participación ha sido registrada',
-	'probleme_technique' => 'Ha habido un problema técnico. Por favor, intenta más tarde.',
+	'participation_incertaine_prise_en_compte' => 'Su eventual participación ha sido registrada',
+	'participation_prise_en_compte' => 'Su participación ha sido registrada',
+	'probleme_technique' => 'Ha habido un problema técnico. Por favor, inténtelo más tarde.',
 
 	// R
-	'repetition' => 'Répétition', # NEW
+	'repetition' => 'Repetición',
 	'repetition_de' => 'Repetición de',
-	'retour_evenement' => 'Retour à l\'événement', # NEW
-	'rubrique_activer_agenda' => 'Activar el modo agenda para esa sección',
-	'rubrique_dans_une_rubrique_mode_agenda' => 'Esta sección permite utilizar eventos',
-	'rubrique_desactiver_agenda' => 'Desactivar el modo agenda para esta sección',
-	'rubrique_liste_evenements_de' => 'Événements de la rubrique', # NEW
-	'rubrique_mode_agenda' => 'El modo agenda está activado para esta sección y sus ramas.',
-	'rubrique_sans_gestion_evenement' => 'No está activado el modo agenda para esta sección',
+	'retour_evenement' => 'Volver al evento',
+	'rubrique_activer_agenda' => 'Activar el modo agenda',
+	'rubrique_dans_une_rubrique_mode_agenda' => 'Esta sección permite utilizar eventos porque está en una sección en la que el modo agenda ha sido activado',
+	'rubrique_desactiver_agenda' => 'Desactivar el modo agenda',
+	'rubrique_liste_evenements_de' => 'Eventos de la sección',
+	'rubrique_mode_agenda' => 'El modo agenda está activado para esta sección y su rama',
+	'rubrique_sans_gestion_evenement' => 'El modo agenda no está activado para esta sección',
 	'rubriques' => 'Secciones Agenda',
 
 	// S
 	'sans_titre' => '(sin título)',
 
 	// T
-	'telecharger' => 'Télécharger', # NEW
-	'telecharger_oui' => 'Seulement les réponses positives', # NEW
-	'telecharger_toutes' => 'Toutes les réponses', # NEW
-	'telecharger_toutes_tous_evenements' => 'Toutes les réponses aux inscriptions', # NEW
-	'texte_agenda' => 'AGENDA', # MODIF
-	'texte_evenement_statut' => 'Cet événement est :', # NEW
-	'texte_logo_objet' => 'LOGO DE L\'EVENEMENT', # NEW
+	'telecharger' => 'Descargar',
+	'telecharger_oui' => 'Solamente respuestas positivas',
+	'telecharger_toutes' => 'Todas las respuestas',
+	'telecharger_toutes_tous_evenements' => 'Todas las respuestas a las inscripciones',
+	'texte_agenda' => 'Agenda',
+	'texte_evenement_statut' => 'Este evento es:',
+	'texte_logo_objet' => 'Logo del evento',
 	'titre_cadre_ajouter_evenement' => 'Añadir un evento',
 	'titre_cadre_modifier_evenement' => 'Modificar un evento',
 	'titre_sur_l_agenda' => 'En la agenda',
-	'titre_sur_l_agenda_aussi' => 'Et aussi...', # NEW
+	'titre_sur_l_agenda_aussi' => 'Y también...',
 	'toutes_rubriques' => 'Todas',
 
 	// U
