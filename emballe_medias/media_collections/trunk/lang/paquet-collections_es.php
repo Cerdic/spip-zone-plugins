2bfe4f6926b3f9988ccbc96b689bda19ada40647
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-06 01:37:34 +0000

    langues (paquet-collections)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71674 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ emballe_medias/media_collections/trunk/lang/paquet-collections_es.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-collections?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'collections_description' => 'Crear colecciones de medios, de álbumes...',
+	'collections_nom' => 'Colecciones de medios',
+	'collections_slogan' => 'Crear colecciones de medios'
+);
+
+?>
