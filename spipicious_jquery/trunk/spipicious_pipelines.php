d0743352ce6b06b05559cc9a5db080a2d5d5ab16
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-06 12:49:36 +0000

    PHPdoc
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71708 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spipicious_jquery/trunk/spipicious_pipelines.php
+++ spipicious_jquery/trunk/spipicious_pipelines.php
@@ -1,24 +1,31 @@
 <?php
 /**
- * spip.icio.us
+ * SPIP.icio.us
  * Gestion de tags lies aux auteurs
  *
  * Auteurs :
  * kent1 (http://www.kent1.info - kent1@arscenic.info)
- * Erational
+ * Erational (http://www.erational.org)
  *
- * © 2007-2012 - Distribue sous licence GNU/GPL
+ * © 2007-2013 - Distribue sous licence GNU/GPL
  * 
  * Fichiers des pipelines du plugin
+ * 
+ * @package SPIP\SPIPicious\Pipelines
  */
 
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 /**
+ * Insertion dans le pipeline insert_head (SPIP)
+ * 
  * Insertion du code javascript nécessaire dans le head
- *
+ * Pour ce faire, on utilise la fonction dédiée dans le plugin sélecteur générique
+ * 
  * @param string $flux
- * @return
+ * 		Le contenu de la balise #INSERT_HEAD
+ * @return string $flux
+ * 		Le contenu de la balise #INSERT_HEAD modifié
  */
 function spipicious_insert_head($flux){
 	include_spip('inc/autoriser');
@@ -26,7 +33,6 @@ function spipicious_insert_head($flux){
 		include_spip('selecteurgenerique_fonctions');
 		$flux .= selecteurgenerique_verifier_js($flux);
 	}
-	
 	return $flux;
 }
 
@@ -34,7 +40,11 @@ function spipicious_insert_head($flux){
  * Insertion dans le pipeline optimiser_base_disparus (SPIP)
  * 
  * Supprimer les liens spipicious/objet sur les éléments disparus
- * @param array $flux le contexte du pipeline
+ * 
+ * @param array $flux 
+ * 		Le contexte du pipeline
+ * @return array $flux
+ * 		Le contexte du flux non modifié
  */
 function spipicious_optimiser_base_disparus($flux){
 	/**
@@ -58,34 +68,46 @@ function spipicious_optimiser_base_disparus($flux){
 }
 
 /**
- * Insertion dans le pipeline pre_boucle de SPIP
- * Si on ne passe pas le critère tout ni statut aux boucles spipicious, on n'affiche pas ceux pas publiés 
+ * Insertion dans le pipeline pre_boucle (SPIP)
+ * 
+ * Dans une boucle SPIPICIOUS, si on ne passe pas le critère tout ou statut, 
+ * on n'affiche pas ceux non publiés
+ * 
+ * @param object $boucle
+ * 		Les paramètres de la boucle
+ * @return object $boucle
+ * 		Les paramètres de la boucle modifiés
  */
 function spipicious_pre_boucle($boucle){
 	if ($boucle->type_requete == 'spipicious') {
 		$id_table = $boucle->id_table;
 		$mstatut = $id_table .'.statut';
 		if (!isset($boucle->modificateur['criteres']['tout'])
-			&& !isset($boucle->modificateur['criteres']['statut'])) {
+			&& !isset($boucle->modificateur['criteres']['statut']))
 			$boucle->where[]= array("'='", "'$mstatut'", "'\"publie\"'");
-		}
 	}
 	return $boucle;
 }
 
 /**
- * Insertion dans le pipeline de post-edition
- * A l'institution d'un objet (changement de statut), si l'objet n'a pas le statut publié et qu'il a des liens spipicious, on passe ses liens en prop
+ * Insertion dans le pipeline de post-edition (SPIP)
+ * 
+ * A l'institution d'un objet (changement de statut), 
+ * si l'objet n'a pas le statut publié et qu'il a des tags spipicious, on passe ses tags en prop
  * sinon, on les publie
+ * 
+ * @param array $flux
+ * 		Le contexte du pipeline
+ * @return $flux
+ * 		Le contexte du pipeline sans modification
  */
 function spipicious_post_edition($flux){
 	if($flux['args']['action'] == 'instituer'){
 		$objet = objet_type($flux['args']['table']);
-		if(isset($flux['data']['statut']) && ($flux['data']['statut'] != 'publie')){
+		if(isset($flux['data']['statut']) && ($flux['data']['statut'] != 'publie'))
 			sql_updateq('spip_spipicious',array('statut'=>'prop'),'id_objet='.intval($flux['args']['id_objet']).' AND objet='.sql_quote($objet).' AND statut="publie"');
-		}else{
+		else
 			sql_updateq('spip_spipicious',array('statut'=>'publie'),'id_objet='.intval($flux['args']['id_objet']).' AND objet='.sql_quote($objet).' AND statut="prop"');
-		}
 	}
 	return $flux;
 }
