d618b03cda412563ef4696ccf0ac4901aa5b255d
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-06 01:37:59 +0000

    langues (paquet-palette)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71683 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ palette/trunk/lang/paquet-palette_es.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-palette?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// P
+	'palette_description' => 'Dé la clase palette a una entrada para añadirle un selector de colores: <code><input class="palette" name="couleur" value="#dfdfdf" /></code>',
+	'palette_slogan' => 'Selector de colores'
+);
+
+?>
