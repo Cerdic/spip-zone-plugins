86e3655cefd41aaa7004e95134019cbb42ceff7b
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-06 01:38:08 +0000

    langues (paquet-recommander)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71686 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ recommander/lang/paquet-recommander_es.php
@@ -0,0 +1,22 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-recommander?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// R
+	'recommander_description' => 'Función recomendar una página a un amigo.
+
+Se añade en no importa qué esqueleto bajo la forma:
+<code>#RECOMMANDER{título de la página,url de la página,intro de la página,asunto}</code>
+_ por ejemplo
+<code>#RECOMMANDER{#TITRE,#URL_ARTICLE,#INTRODUCTION}</code>
+_ la url, la intro y el asunto son opcionales',
+	'recommander_nom' => 'Recomendar a un amigo',
+	'recommander_slogan' => 'Función recomendar una página a un amigo'
+);
+
+?>
