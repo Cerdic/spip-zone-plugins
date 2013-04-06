ecf906411d597ebafeefe2504696f32872b9551f
willmann@netzsachen.de <willmann@netzsachen.de@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-06 01:38:34 +0000

    langues (videos)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71695 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ videos/trunk/lang/videos_de.php
@@ -0,0 +1,48 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/videos?lang_cible=de
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cfg_description_doc' => 'Dokumentation',
+	'cfg_description_titre' => 'Konfiguration des Video(s) Plugins',
+	'cfg_explication_hauteur' => 'Standardhöhe für Videos. Diese Option kann im Aufruf des Modells überschrieben werden.',
+	'cfg_explication_largeur' => 'Standardbreite für Videos. Diese Option kann im Aufruf des Modells überschrieben werden.',
+	'cfg_explication_liste_definition' => 'Videos in dl/dt/dd einbetten, wie es  SPIP beim doc Modell macht.',
+	'cfg_explication_responsive' => 'Höhe und Breite des Videos variabel machen: Lässt sich besser in eine veränderbare Website einbetten.',
+	'cfg_explication_titre_descriptif' => 'Titel und Beschreibung des Videos anzeigen. Diese Option kann im Aufruf des Modells überschrieben werden.',
+	'cfg_explication_wmode' => 'Modus für Flash-Objekte festelgen (fallback Flowplayer, iFrames Dailymotion, Youtube, Vimeo).',
+	'cfg_label_hauteur' => 'Standardhöhe',
+	'cfg_label_largeur' => 'Standardbreite',
+	'cfg_label_liste_definition' => 'Definitionsliste  (HTML)',
+	'cfg_label_responsive' => 'Skalierbare Größe',
+	'cfg_label_titre_descriptif' => 'Titel und Beschreibung',
+	'cfg_label_wmode' => 'Standard Wmode',
+	'cfg_titre_configurations_communes' => 'Allgemeine Einstellungen',
+	'cfg_titre_configurations_dailymotion' => 'Einstellungen Dailymotion',
+	'cfg_titre_configurations_html5' => 'Einstellungen HTML5 / Alternativen',
+	'cfg_titre_configurations_vimeo' => 'Viméo Einstellungen',
+	'cfg_titre_configurations_youtube' => 'Youtube Einstellungen',
+	'confirmation_ajout' => 'Das Video "@type@:@titre@" ist hinzugefügt worden &lt;video@id_document@&gt;',
+
+	// E
+	'erreur_adresse_invalide' => 'Ungültige URL. Wenn Sie ein MP4-Video importieren wollen müssen sie das Dokument anfügen Formular von SPIP nutzen',
+	'explication_ajouter_video' => '(Remote Video YouTube, Vimeo, DailyMotion)',
+
+	// L
+	'label_ajouter_autre_video' => 'Noch ein Video hinzufügen',
+	'label_ajouter_video' => 'Video hinzufügen',
+	'label_url' => 'Video URL',
+
+	// T
+	'titre_admin' => 'Plugin Video(s)',
+
+	// V
+	'valider' => 'Video hinzufügen'
+);
+
+?>
