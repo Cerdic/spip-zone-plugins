a0bd9c72a8384128ee41d57db09b031745e7b5d3
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-06 01:38:01 +0000

    langues (paquet-photospip)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71684 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ photospip/trunk/lang/paquet-photospip_es.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-photospip?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// P
+	'photospip_description' => 'Modificar las imágenes insertadas en SPIP aplicándoles diferentes filtros',
+	'photospip_slogan' => 'Modificación de imágenes en SPIP'
+);
+
+?>
