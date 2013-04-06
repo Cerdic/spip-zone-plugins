f57ab8ba889003c71cd7af50422418ee60828c73
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-06 13:24:14 +0000

    PHPdoc et broutilles
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71714 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spipicious_jquery/trunk/spipicious_administrations.php
+++ spipicious_jquery/trunk/spipicious_administrations.php
@@ -1,17 +1,26 @@
 <?php
-
 /**
- * spip.icio.us
+ * SPIP.icio.us
  * Gestion de tags lies aux auteurs
  *
  * Auteurs :
  * kent1 (http://www.kent1.info - kent1@arscenic.info)
- * Erational
+ * Erational (http://www.erational.org)
  *
- * © 2007-2012 - Distribue sous licence GNU/GPL
+ * © 2007-2013 - Distribue sous licence GNU/GPL
  *
+ * @package SPIP\SPIPicious\Installation
  */
 
+/**
+ * Installation/maj de la table spip_spipicious
+ *
+ * @param string $nom_meta_base_version
+ *     Nom de la meta informant de la version du schéma de données du plugin installé dans SPIP
+ * @param string $version_cible
+ *     Version du schéma de données dans ce plugin (déclaré dans paquet.xml)
+ * @return void
+ */
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 function spipicious_upgrade($nom_meta_base_version,$version_cible){
@@ -56,7 +65,6 @@ function spipicious_upgrade($nom_meta_base_version,$version_cible){
 		array('spipicious_update_statuts_mots',array()),
 	);
 
-	
 	$maj['0.6.3'] = array(
 		array('sql_alter',"TABLE spip_spipicious ADD INDEX id_mot (id_mot)"),
 		array('sql_alter',"TABLE spip_spipicious ADD INDEX id_objet (id_objet)"),
@@ -67,36 +75,60 @@ function spipicious_upgrade($nom_meta_base_version,$version_cible){
 	maj_plugin($nom_meta_base_version, $version_cible, $maj);
 }
 
+/**
+ * Désinstallation/suppression de la table spip_spipicious
+ *
+ * @param string $nom_meta_base_version
+ *     Nom de la meta informant de la version du schéma de données du plugin installé dans SPIP
+ * @return void
+ */
 function spipicious_vider_tables($nom_meta_version_base) {
 	sql_drop_table("spip_spipicious");
 	effacer_meta($nom_meta_version_base);
 	effacer_meta('spipicious');
-	ecrire_metas();
 }
 
+/**
+ * Fonction de création de configuration automatique
+ * 
+ * Crée un groupe de mots intitulé "- Tags -"
+ * Active la configuration précise des groupes de mots si elle ne l'est pas
+ * Active les mots sur les articles s'ils ne le sont pas
+ * Crée la configuration de base avec le groupe de mot et les administrateurs pouvant ajouter des mots
+ * 
+ * @return void
+ */
 function spipicious_creer_config(){
-	$titre_groupe = '- Tags -';
-	$id_groupe = sql_getfetsel('id_groupe','spip_groupes_mots','titre='.sql_quote($titre_groupe));
-	if(!$id_groupe){
-		$id_groupe = sql_insertq('spip_groupes_mots',array('titre' => $titre_groupe,'tables_liees' => 'articles','minirezo'=>'oui','comite'=>'oui','forum'=>'oui'));
-	}
-	if($GLOBALS['meta']['config_precise_groupes'] == 'non'){
+	if(!($id_groupe = sql_getfetsel('id_groupe','spip_groupes_mots','titre='.sql_quote('- Tags -'))))
+		$id_groupe = sql_insertq('spip_groupes_mots',array('titre' => '- Tags -','tables_liees' => 'articles','minirezo'=>'oui','comite'=>'oui','forum'=>'oui'));
+
+	if($GLOBALS['meta']['config_precise_groupes'] == 'non')
 		ecrire_meta('config_precise_groupes','oui','oui');
-	}
-	if($GLOBALS['meta']['articles_mots'] == 'non'){
+
+	if($GLOBALS['meta']['articles_mots'] == 'non')
 		ecrire_meta('articles_mots','oui','oui');
-	}
-	$config_spipicious = array('people' => array('0minirezo'),'groupe_mot' => $id_groupe);
-	ecrire_meta('spipicious',serialize($config_spipicious),'oui');
+
+	ecrire_meta('spipicious',serialize(array('people' => array('0minirezo'),'groupe_mot' => $id_groupe)),'oui');
 }
 
+/**
+ * Change la configuration du groupe de mot si déjà configuré
+ * 
+ * @return void
+ */
 function spipicious_update_conf_mot(){
+	if(!function_exists('lire_config'))
+		include_spip('inc/config');
 	$id_groupe = lire_config('spipicious/groupe_mot');
-	if($id_groupe){
+	if(intval($id_groupe) > 0)
 		sql_updateq('spip_groupes_mots',array('tables_liees' => 'articles','minirezo'=>'oui','comite'=>'oui','forum'=>'oui'),'id_groupe='.$id_groupe);
-	}
 }
 
+/**
+ * Fonction d'upgrade : vérifier les status des spipicious
+ * 
+ * @return void
+ */
 function spipicious_update_statuts_mots(){
 	$spipicious = sql_select('*','spip_spipicious');
 	while($iter = sql_fetch($spipicious)){
@@ -112,7 +144,9 @@ function spipicious_update_statuts_mots(){
 }
 
 /**
- * Reunir en un seul champs id_objet/objet
+ * Fonction d'upgrade : réunir en un seul champs id_objet/objet
+ * 
+ * @return void
  */
 function spipicious_id_objet_objet_upgrade () {
 	// Recopier les donnees avec le coupe id_objet / objet
