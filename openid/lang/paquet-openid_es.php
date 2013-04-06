202514bf644a493b2652070671e9f7a5daa012b4
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-06 01:37:57 +0000

    langues (paquet-openid)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71682 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ openid/lang/paquet-openid_es.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-openid?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// O
+	'openid_description' => 'Un usuario que desee utilizar su OpenID para acceder al sitio debe introducirlo como dirección de su sitio web en su perfil. A continuación, en lugar de meter su identificador de usuario o dirección de correo electrónico, basta con introducir su OpenID para conectarse.',
+	'openid_slogan' => 'Autentificación de los autores y visitantes en ayuda de su OpenID'
+);
+
+?>
