261d5621479167de655233d956ecbbe31397dbec
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-07 01:38:04 +0000

    langues (paquet-em_doc2img)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71734 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- emballe_medias/emballe_medias_doc2img/trunk/lang/paquet-em_doc2img_en.php
+++ emballe_medias/emballe_medias_doc2img/trunk/lang/paquet-em_doc2img_en.php
@@ -8,9 +8,9 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// E
-	'em_doc2img_description' => 'Doc2Img add-on for "Wrap media" (use doc2img)', # MODIF
+	'em_doc2img_description' => 'Doc2Img add-on for "Wrap media" (use doc2img)',
 	'em_doc2img_nom' => 'Wrap medias - Doc2Img',
-	'em_doc2img_slogan' => 'Doc2Img add-on for "Wrap media" (use doc2img)' # MODIF
+	'em_doc2img_slogan' => 'Doc2Img add-on for "Wrap media" (use doc2img)'
 );
 
 ?>
