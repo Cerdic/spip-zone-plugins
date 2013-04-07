3a4262751c354d66c1de0ac3788d8911b4b62b5b
denisb@a-working-class-hero-is-something-to.be <denisb@a-working-class-hero-is-something-to.be@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-07 23:07:00 +0000

    langOnet : un peu de cosmétique css ; un peu de correction orthographique des items de langue ; faire afficher les traductions d'item telles qu'elles sont écrites dans les fichiers de langue (tags html compris).
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71754 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- langonet/trunk/lang/paquet-langonet_fr.php
+++ langonet/trunk/lang/paquet-langonet_fr.php
@@ -13,7 +13,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 // L
-	'langonet_description' => 'Ce plugin permet d\'exécuter des actions de vérification et de nettoyage des fichiers de langue de SPIP, des plugins ou des squelettes. Il est aussi possible de consulter l\'ensemble des items contenus dans les fichiers de langue disponibles sur le site et d\'effectuer des recherches d\'items dans les fichiers de langue de SPIP. Sous certaines conditions, il produit automatiquement des items de langue à partir des textes libres repérés.',
+	'langonet_description' => 'Ce plugin permet d\'exécuter des actions de vérification et de nettoyage des fichiers de langue de SPIP, des plugins ou des squelettes. Il offre aussi la possibilité de consulter l\'ensemble des items contenus dans les fichiers de langue disponibles sur le site et d\'effectuer des recherches d\'items dans les fichiers de langue de SPIP. Sous certaines conditions, il produit automatiquement des items de langue à partir des textes libres repérés.',
 	'langonet_slogan' => 'Vérifier, générer, rechercher ou afficher les items de langue',
 );
 ?>
