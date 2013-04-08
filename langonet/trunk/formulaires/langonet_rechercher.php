7aed378547cb167500d9bd6291be0e416f4cae14
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-08 18:35:28 +0000

    La fonction de formatage a été remplacée par un fichier HTML
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71803 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- langonet/trunk/formulaires/langonet_rechercher.php
+++ langonet/trunk/formulaires/langonet_rechercher.php
@@ -93,47 +93,4 @@ function formulaires_langonet_rechercher_traiter($type) {
 	return $retour;
 }
 
-function formater_recherche($resultats) {
-	include_spip('inc/layer');
-	
-	$texte = '';
-	$total = 0;
-	foreach ($resultats['item_trouve'] as $_pertinence => $_trouves) {
-		if ($_trouves) {
-			$total += count($_trouves);
-			// On démarre un groupe d'items trouves avec un message
-			$suffixe = (count($_trouves) == 1 ? '_1' : '_n');
-			$texte .= '<div style="margin-bottom: 20px">' . "\n" .
-			          '<div class="success">' . "\n" .
-			          _T('langonet:message_ok_item_trouve_' . $_pertinence .
-			          $suffixe, array('sous_total'=>count($_trouves))) . "\n" .
-			          '</div>' . "\n";
-			foreach ($_trouves as $_item => $_infos) {
-				$texte .= bouton_block_depliable($_item . ' (' .
-				          count($_infos['fichier']) . ')', false) .
-				          debut_block_depliable(false) .
-				          "<p style=\"padding-left:2em;\">  " .
-				          _T('langonet:texte_item_defini_ou')."\n<br />";
-				foreach ($_infos['fichier'] as $_index => $_fichier_def) {
-					$texte .= "\t" . '<span style="font-weight:bold;padding-left:2em;">' .
-					          $_fichier_def . "</span><br />\n" .
-					          "\t" . '<span style="padding-left:3em;padding-right:0.5em;"><em>' .
-							  '<span style="color: #aaa">&#10078;</span> ' . $_infos['traduction'][$_index] . ' <span style="color: #aaa">&#10077;</span>' .
-							  "</em></span><br />\n";
-				}
-				$texte .= "</p>\n" .
-				          fin_block();
-			}
-			$texte .= '</div>' . "\n";
-		}
-	}
-	
-	// Tout s'est bien passe on renvoie le message ok et les resultats de la verification
-	$retour['message_ok']['resume'] = _T('langonet:message_ok_item_trouve', array('pattern' => $resultats['pattern']));
-	$retour['message_ok']['total'] = $total;
-	$retour['message_ok']['trouves'] = $texte;
-
-	return $retour;
-}
-
 ?>
