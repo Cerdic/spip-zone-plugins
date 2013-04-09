a00c151c3b013dc4e9cf9b1086cf0670caebcc28
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-09 01:39:37 +0000

    langues (paquet-suivant_precedent)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71819 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ criteres_suivant_precedent/lang/paquet-suivant_precedent_es.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-suivant_precedent?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// S
+	'suivant_precedent_description' => 'Este plugin es un experimento que permite encontrar, en un bucle dado, el elemento siguiente o el elemento precedente. Este uso es principalmente útil para crear navegaciones por sección o por tema (palabras clave).',
+	'suivant_precedent_nom' => 'Criterios Anterior / Siguiente',
+	'suivant_precedent_slogan' => 'Añade criterios de bucles <code>{siguiente}</code> y <code>{anterior}</code>'
+);
+
+?>
