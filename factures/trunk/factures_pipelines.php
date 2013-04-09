38b431c958ff6c4acc7ee35bacf301920f61c0ee
cyril@cym.fr <cyril@cym.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-09 20:59:42 +0000

    On ajoute les factures liées sur la fiche d'une organisation;  reste à faire pour demain :  - date facture = date du jour par défaut  - proposer un numéro de facture calculé selon un algo particulier  - la date de fin de validité ne doit pas être obligatoire (sauf si le document est un devis)  - récupérer l'id de l'organisation et le mettre en organisation facturée  - récupérer le id_organisation de la conf et le mettre en organisation emettrice
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71863 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- factures/trunk/factures_pipelines.php
+++ factures/trunk/factures_pipelines.php
@@ -10,7 +10,33 @@
  */
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
-	
 
+/**
+ * Afficher les factures d'une organisation
+ *
+ * Il peut y en avoir beaucoup, on le met après le contenu d'une organisation donc.
+ **/
+function factures_afficher_complement_objet($flux) {
 
+	$type = $flux['args']['type'];
+
+	// projets sur les organisations
+	if ($type == 'organisation') {
+
+		$id_organisation = $flux['args']['id'];
+		$lister_objets = charger_fonction('lister_objets', 'inc');
+
+		$bouton = '';
+		if (autoriser('creerfacturedans', 'organisation', $id_organisation)) {
+			include_spip('inc/presentation');
+			$bouton .= icone_verticale(_T("facture:icone_creer_facture"), generer_url_ecrire("facture_edit", "id_organisation=$id_organisation"), "facture-24.png", "new", "right")
+				. "<br class='nettoyeur' />";
+		}
+
+		$flux['data'] .= $lister_objets('factures', array('id_organisation'=>$id_organisation, 'par'=>'date_facture'));
+		$flux['data'] .= $bouton;
+	}
+
+	return $flux;
+}
 ?>
