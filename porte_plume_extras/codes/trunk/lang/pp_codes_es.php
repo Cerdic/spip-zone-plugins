ef70047bc2f52b34454f7424e116027169106f06
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-09 01:40:10 +0000

    langues (pp_codes)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71821 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ porte_plume_extras/codes/trunk/lang/pp_codes_es.php
@@ -0,0 +1,35 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/pp_codes?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// L
+	'label_activer_barre_edition' => 'Barra de edición',
+	'label_activer_barre_forum' => 'Barra de foro',
+	'label_outils_actifs' => 'Herramientas activas',
+
+	// O
+	'outil_inserer_cadre_css' => 'Insertar un código preformateado CSS (marco)',
+	'outil_inserer_cadre_html' => 'Insertar un código preformateado HTML (cuadro)',
+	'outil_inserer_cadre_php' => 'Insertar un código preformateado PHP (marco)',
+	'outil_inserer_cadre_spip' => 'Insertar un código preformateado SPIP (marco)',
+	'outil_inserer_cadre_xml' => 'Insertar un código preformateado XML (marco)',
+	'outil_inserer_kbd' => 'Insertar una entrada teclado (kdb)',
+	'outil_inserer_lien_trac' => 'Insertar un enlace al trac de SPIP',
+	'outil_inserer_pre' => 'Insertar un código preformateado (pre)',
+	'outil_inserer_samp' => 'Insertar una salida de código (samp)',
+	'outil_inserer_var' => 'Insertar una variable (var)',
+
+	// P
+	'pp_codes' => 'Códigos informáticos para Portaplumas',
+
+	// T
+	'titre_activer_extension_sur' => '¿Activar en qué barras de herramientas?',
+	'titre_configurer_pp_codes' => 'Configurar la extensión códigos informáticos para Portaplumas'
+);
+
+?>
