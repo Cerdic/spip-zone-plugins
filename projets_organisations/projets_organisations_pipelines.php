1d24193b3f9dd5987751700d1cd2722f6c720755
marcimat@rezo.net <marcimat@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-09 19:27:45 +0000

    Mieux vaut utiliser un autre pipeline pour passer SOUS le contenu de l'organisation.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71856 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- projets_organisations/projets_organisations_pipelines.php
+++ projets_organisations/projets_organisations_pipelines.php
@@ -1,33 +1,30 @@
 <?php
 
-function projets_organisations_affiche_milieu($flux) {
+function projets_organisations_afficher_complement_objet($flux) {
 
 	$texte = "";
-	$e = trouver_objet_exec($flux['args']['exec']);
+	$type = $flux['args']['type'];
 
-// projets sur les organisations
-	if (!$e['edition'] AND $e['type'] == 'organisation') {
+	// projets sur les organisations
+	if (!$e['edition'] AND $type == 'organisation') {
 		$texte .= recuperer_fond('prive/objets/editer/liens', array(
 			'table_source' => 'projets',
-			'objet' => $e['type'],
-			'id_objet' => $flux['args'][$e['id_table_objet']]
+			'objet' => $type,
+			'id_objet' => $flux['args']['id']
 		));
 	}
 
-// organisations sur les projets
-	if (!$e['edition'] AND $e['type'] == 'projet') {
+	// organisations sur les projets
+	if (!$e['edition'] AND $type == 'projet') {
 		$texte .= recuperer_fond('prive/objets/editer/liens', array(
 			'table_source' => 'projet',
-			'objet' => $flux['args'][$e['id_table_objet']],
+			'objet' => $flux['args']['id'],
 			'id_objet' => 'organisation'
 		));
 	}
 
 	if ($texte) {
-		if ($p=strpos($flux['data'],"<!--affiche_milieu-->"))
-			$flux['data'] = substr_replace($flux['data'],$texte,$p,0);
-		else
-			$flux['data'] .= $texte;
+		$flux['data'] .= $texte;
 	}
 
 	return $flux;
@@ -35,5 +32,3 @@ function projets_organisations_affiche_milieu($flux) {
 
 
 
-
-?>
