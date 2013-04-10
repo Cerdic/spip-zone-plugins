c0a195a44889bdfe2c31f6d291b88914d2c5f312
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-10 18:39:37 +0000

    - On passe de .row à .row-fluid - Création de la balise #SPIP_ECRAN qui retourne la globale du même nom. Ce qui permet de choisir entre .container et .container-fluid (avec cette dernière classe, la page occuper toute la largeur de l'écran en fluide) Inconvénient… Il faut faire une recalcul de la page pour avoir la nouvelle classe.
    TODO : on mettra les class^=span à width = 100% si on a la classe pleine_largeur de #LARGEUR_ECRAN
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71917 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- bootstrap_prive/trunk/bootstrap_prive_fonctions.php
+++ bootstrap_prive/trunk/bootstrap_prive_fonctions.php
@@ -1,1 +1,13 @@
 <?php
+
+/**
+ * Retourner la valeur de $GLOBALS['spip_ecran'] défini dans les préférences de l'auteur
+ * #SPIP_ECRAN
+ * 
+ * @param  $p
+ * @return
+ */
+function balise_SPIP_ECRAN_dist($p){
+	$p->code = isset($_COOKIE['spip_ecran']) ? $_COOKIE['spip_ecran'] : "etroit";
+	return $p;
+}
