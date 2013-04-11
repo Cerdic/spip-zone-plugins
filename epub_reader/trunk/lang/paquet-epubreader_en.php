412ceb54f9624920090e2b72e631c9a2c1ea1ae0
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-11 03:37:35 +0000

    langues (paquet-epubreader)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71929 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- epub_reader/trunk/lang/paquet-epubreader_en.php
+++ epub_reader/trunk/lang/paquet-epubreader_en.php
@@ -1,20 +1,16 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-epubreader
-// Langue: en
-// Date: 04-12-2012 20:40:01
-// Items: 3
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-epubreader?lang_cible=en
+// ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// E
+	// E
 	'epubreader_description' => 'Read [ePUB->http://en.wikipedia.org/wiki/EPUB] files directly in the website',
 	'epubreader_nom' => 'ePUB reader',
-	'epubreader_slogan' => 'Read ePUB files',
+	'epubreader_slogan' => 'Read ePUB files'
 );
+
 ?>
--- epub_reader/trunk/lang/paquet-epubreader_fr.php
+++ epub_reader/trunk/lang/paquet-epubreader_fr.php
@@ -1,20 +1,14 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-epubreader
-// Langue: fr
-// Date: 04-12-2012 20:40:01
-// Items: 3
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/epub_reader/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// E
+	// E
 	'epubreader_description' => 'Lire les fichiers [ePUB->http://fr.wikipedia.org/wiki/EPUB_%28format%29] directement dans le site',
 	'epubreader_nom' => 'Lecteur ePUB',
-	'epubreader_slogan' => 'Lire les fichiers ePUB',
+	'epubreader_slogan' => 'Lire les fichiers ePUB'
 );
+
 ?>
