0f35db161f0ecc7bf558e07074a20711638e3ed5
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-11 03:37:54 +0000

    langues (paquet-signalement)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71934 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- signalement/trunk/lang/paquet-signalement_fr.php
+++ signalement/trunk/lang/paquet-signalement_fr.php
@@ -1,14 +1,14 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/signalement/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// S
-	'signalement_nom' => 'Signalement',
 	'signalement_description' => 'Le plugin Signalement permet de signaler les contenus illicites sur un site.',
-	'signalement_slogan' => 'Signalement de contenus illicites',
+	'signalement_nom' => 'Signalement',
+	'signalement_slogan' => 'Signalement de contenus illicites'
 );
+
 ?>
