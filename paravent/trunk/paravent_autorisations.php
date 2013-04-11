6e151cc33e19794057b0e5519740c9b0a931b5f9
pierre.fiches@gmail.com <pierre.fiches@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-11 21:24:31 +0000

    Nettoyage
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71973 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- paravent/trunk/paravent_autorisations.php
+++ paravent/trunk/paravent_autorisations.php
@@ -11,11 +11,10 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 function paravent_autoriser(){}
 
 /**
- * Autoriser a voir le site en travaux : par defaut tous les webmestre
- * @return mixed
+ * Autoriser a voir le site en construction : par defaut tous les auteurs authentifies
+ * @return bool
  */
-function autoriser_travaux($faire, $type, $id, $qui, $opt){ 
-	//return autoriser('webmestre');
+function autoriser_travaux($faire, $type, $id, $qui, $opt){
 	return in_array($qui['statut'], array('0minirezo', '1comite'));
 	}
 ?>
