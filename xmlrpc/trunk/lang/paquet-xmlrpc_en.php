6f5df9004a7c3da01135b2d2a9b51c3716befd12
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-11 03:38:00 +0000

    langues (paquet-xmlrpc)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71936 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- xmlrpc/trunk/lang/paquet-xmlrpc_en.php
+++ xmlrpc/trunk/lang/paquet-xmlrpc_en.php
@@ -1,18 +1,15 @@
 <?php
-
-/// @file
-/// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-/// 
-///  Fichier produit par PlugOnet
-/// Module: paquet-xmlrpc
-/// Langue: en
-/// Date: 07-10-2011 16:03:20
-/// Items: 1
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-xmlrpc?lang_cible=en
+// ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-// X
-	'xmlrpc_slogan' => 'Server and client xml-rpc',
+
+	// X
+	'xmlrpc_description' => 'API xml-rpc cliente et serveur', # NEW
+	'xmlrpc_slogan' => 'Server and client xml-rpc'
 );
+
 ?>
--- xmlrpc/trunk/lang/paquet-xmlrpc_fr.php
+++ xmlrpc/trunk/lang/paquet-xmlrpc_fr.php
@@ -1,19 +1,13 @@
 <?php
-
-/// @file
-/// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-/// 
-///  Fichier produit par PlugOnet
-/// Module: paquet-xmlrpc
-/// Langue: fr
-/// Date: 07-10-2011 16:03:20
-/// Items: 2
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/xmlrpc/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+
 	// X
 	'xmlrpc_description' => 'API xml-rpc cliente et serveur',
-	'xmlrpc_slogan' => 'Serveur et client xml-rpc',
+	'xmlrpc_slogan' => 'Serveur et client xml-rpc'
 );
+
 ?>
