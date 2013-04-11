b4d139a5569a28b217be42abe7d4b3a01aa80b62
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-11 03:37:48 +0000

    langues (paquet-podcast)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71931 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- podcast/trunk/lang/paquet-podcast_en.php
+++ podcast/trunk/lang/paquet-podcast_en.php
@@ -1,13 +1,16 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans http://svn.aires-de-confluxence.info/svn/plugins_spip/podcast/lang
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-podcast?lang_cible=en
+// ** ne pas modifier le fichier **
+
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+
 	// P
 	'podcast_description' => 'A plugin to provide one or several podcasts feeds on the site',
 	'podcast_nom' => 'Podcasts',
-	'podcast_slogan' => 'One or several podcasts feeds on the site',
+	'podcast_slogan' => 'One or several podcasts feeds on the site'
 );
 
 ?>
--- podcast/trunk/lang/paquet-podcast_fr.php
+++ podcast/trunk/lang/paquet-podcast_fr.php
@@ -1,13 +1,14 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans http://svn.aires-de-confluxence.info/svn/plugins_spip/podcast/lang
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/podcast/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+
 	// P
 	'podcast_description' => 'Un plugin pour mettre à disposition un ou plusieurs flux de podcast sur son site',
 	'podcast_nom' => 'Podcasts',
-	'podcast_slogan' => 'Un ou plusieurs flux de podcast sur son site',
+	'podcast_slogan' => 'Un ou plusieurs flux de podcast sur son site'
 );
 
 ?>
