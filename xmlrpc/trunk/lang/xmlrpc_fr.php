2898a32d946853c4c94e1d37377e92d9e03523d1
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-11 03:37:59 +0000

    langues (xmlrpc)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71935 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- xmlrpc/trunk/lang/xmlrpc_fr.php
+++ xmlrpc/trunk/lang/xmlrpc_fr.php
@@ -1,24 +1,27 @@
 <?php
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/xmlrpc/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+
 	// C
 	'cfg_description' => 'Configuration du serveur xml-rpc',
-	
+
 	// E
 	'erreur_arguments_obligatoires' => 'Erreur : les arguments suivants sont obligatoires "@arguments@"',
-	'erreur_lecture' => 'Erreur de lecture de l\'objet (@objet@ #@id_objet@)',
 	'erreur_identifiant' => 'Vous devez fournir l\'identifiant numérique de l\'objet (@objet@)',
 	'erreur_impossible_lire_objet' => 'Erreur : il est impossible de lire l\'objet "@objet@" #@id_objet@',
-	'erreur_objet_inexistant' => 'L\'objet demandé n\'existe pas (@objet@ #@id_objet@)',
+	'erreur_lecture' => 'Erreur de lecture de l\'objet (@objet@ #@id_objet@)',
 	'erreur_mauvaise_identification' => 'Mauvaise identification (login/mot de passe)',
+	'erreur_objet_inexistant' => 'L\'objet demandé n\'existe pas (@objet@ #@id_objet@)',
 	'erreur_xmlrpc_desactive' => 'Le serveur xml-rpc est désactivé',
-	
+
 	// L
 	'label_api_preferee' => 'API d\'édition préférée',
 	'label_desactiver_rsd' => 'Désactiver le RSD',
 	'label_desactiver_rsd_long' => 'Désactive l\'utilisation du fichier RSD dans l\'entête des pages',
-	'label_ferme' => 'Désactiver le serveur',
+	'label_ferme' => 'Désactiver le serveur'
 );
+
 ?>
