ae86216a3c86925c77f0db85575a4cc8133cade7
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-11 03:37:47 +0000

    langues (podcast)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71930 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- podcast/trunk/lang/podcast_fr.php
+++ podcast/trunk/lang/podcast_fr.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans http://svn.aires-de-confluxence.info/svn/plugins_spip/podcast/lang
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/podcast/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
