62d9be5c6e03036ed8e7a3351a90e07dc527ba09
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-11 16:45:55 +0000

    On en profite pour ajouter une pagination alphabétique (Guytarr)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71960 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ trad-lang/trad-lang_spip2/inclure/liste_traducteurs_fonctions.php
@@ -0,0 +1,55 @@
+<?php
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+/**
+ * Calculer l'initiale d'un nom
+ *
+ * @param <type> $nom
+ * @return <type>
+ */
+function initiale($nom){
+	return spip_substr(trim(strtoupper($nom)),0,1);
+}
+
+/**
+ * Afficher l'initiale pour la navigation par lettres
+ *
+ * @staticvar string $memo
+ * @param <type> $url
+ * @param <type> $initiale
+ * @param <type> $compteur
+ * @param <type> $debut
+ * @param <type> $pas
+ * @return <type>
+ */
+function afficher_initiale($url,$initiale,$compteur,$debut,$pas){
+	static $memo = null;
+	static $res = array();
+	$out = "";
+	if (!$memo
+		OR (!$initiale AND !$url)
+		OR ($initiale!==$memo['initiale'])
+		){
+		$newcompt = intval(floor(($compteur-1)/$pas)*$pas);
+		// si fin de la pagination et une seule entree, ne pas l'afficher, ca ne sert a rien
+		if (!$initiale AND !$url AND !$memo['compteur']) $memo=null;
+		if ($memo){
+			$on = (($memo['compteur']<=$debut)
+				AND (
+						$newcompt>$debut OR ($newcompt==$debut AND $newcompt==$memo['compteur'])
+						));
+			$res[] = lien_ou_expose($memo['url'],$memo['initiale'],$on,'lien_pagination');
+		}
+		if ($initiale)
+			$memo = array('entree'=>isset($memo['entree'])?$memo['entree']+1:0,'initiale'=>$initiale,'url'=>parametre_url($url,'i',$initiale),'compteur'=>$newcompt);
+	}
+	if (!$initiale AND !$url) {
+		if (count($res)>1)
+			$out = implode(' ',$res);
+		$memo=$res=null;
+	}
+	return $out;
+}
+
+?>
