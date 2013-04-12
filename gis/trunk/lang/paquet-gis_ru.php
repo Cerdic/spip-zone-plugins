3d0506c5dc7ccf1e66668748da29fbad1689c8ae
serge.markitanenko@gmail.com <serge.markitanenko@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-12 03:37:24 +0000

    langues (paquet-gis)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@71979 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ gis/trunk/lang/paquet-gis_ru.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-gis?lang_cible=ru
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// G
+	'gis_description' => 'Плагин GIS позволяет создавать элементы на карте и привязвать их к существующим объектам SPIP. Вы можете использовать созданные карты на страницах своего сайта. Могут использовать разные карты, доступ к которым обеспечивается при помощи библиотеки Leaflet.',
+	'gis_slogan' => 'Географическая  Информационная Система (GIS)'
+);
+
+?>
