ab3b7747ebda35d82182aeb6881eedccf47fdcdc
gildas.cotomale@gmail.com <gildas.cotomale@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-16 08:40:59 +0000

    On profite du filtre sinon_interdire_acces() introduit en r72056 (d'ailleur les autres pages seront refactorisées ainsi) ; on fait suivre le calcul des cotisations sur le dernier exercice (ou l'année c'est selon la configuration choisie mais on est cohérent avec les autres pages) ; on rajoute des paramètres d'url qui n'étaient pas pris en compte (pas évident à la simple lecture car collecte déportée loin dans une fonction loin au lieu d'être parallèlement avec les autres initialisations)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@72068 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- Associaspip/trunk/exec/adherents.php
+++ Associaspip/trunk/exec/adherents.php
@@ -12,127 +12,129 @@ if (!defined('_ECRIRE_INC_VERSION'))
 	return;
 
 function exec_adherents() {
-	if (!autoriser('voir_membres', 'association', 0)) {  // on s'assure qu'il n'y ai pas d'id associe a la demande d'autorisation ici car on les consulte tous
-		include_spip('inc/minipres');
-		echo minipres();
-	} else {
-		include_spip('association_modules');
+	sinon_interdire_acces(autoriser('voir_membres', 'association', 0)); // on s'assure qu'il n'y ai pas d'id associe a la demande d'autorisation ici car on les consulte tous
+	include_spip('association_modules');
 /// INITIALISATIONS
+	$id = association_passeparam_id('auteur');
+	$args_url = array();
+	if (!$id) { // Pas de ID : les autres filtres sont actifs
+		$id = _T('asso:adherent_libelle_id_auteur');
+		$args_sql = array();
 		list($statut_interne, $critere) = association_passeparam_statut('interne', 'defaut');
+		if ($statut_interne != 'defaut') {
+			$args_url[] = "statut=$statut_interne";
+			$args_sql[] = "m.statut_interne=' $statute_interne'"; // = $critere; // :-S
+		}
+		$id_categorie = association_recuperer_entier('categorie');
+		if ($id_categorie) {
+			$args_url[] = "categorie=$id_categorie";
+			$args_sql[] = "m.id_categorie=$id_categorie";
+		}
+		$id_groupe = association_recuperer_entier('groupe');
+		if ($id_groupe) {
+			$args_url[] = "groupe=$id_groupe";
+			$args_sql[] = "g.id_groupe=$id_groupe ";
+			$jointure_groupe = ' LEFT JOIN spip_asso_fonctions AS g ON m.id_auteur=g.id_auteur ';
+		} else {
+			$jointure_groupe = '';
+		}
 		$lettre = _request('lettre');
-		$id = association_passeparam_id('auteur');
-		if (!$id) { //Filtres ID et groupe :
-			$id = _T('asso:adherent_libelle_id_auteur');
-			$id_groupe = association_recuperer_entier('groupe');
-			$id_categorie = association_recuperer_entier('categorie');
-			if ($id_categorie)
-				$critere = "m.id_categorie=$id_categorie";
-			$suffixe_pdf = "membres_$statut_interne".'_';
-			$suffixe_pdf .= $lettre?str_replace(array('/', '\\', '?', '%', '*', ':', '|', '"', '<', '>', '.', ' ', '(', ')', ';', '&', '#', '[', ']', ), '~', utf8_decode($lettre) ):'tous'; // caracteres problematiques : http://en.wikipedia.org/wiki/Filename#Reserved_characters_and_words
-			$suffixe_pdf .= "_$id_categorie"."_$id_groupe";
-		} else { // si le filtre ID est actif, on ignore les autres filtres
-			$critere = "m.id_auteur=$id";
-			$id_groupe = 0;
-			$id_categorie = 0;
-			$suffixe_pdf = "membre$id";
+		if ($lettre) {
+			$args_url[] = "lettre=$lettre";
+			$args_sql[] = "UPPER(m.nom_famille) LIKE UPPER('$lettre%') "; // le 1er UPPER (plutot que LOWER puisque les lettres sont mises et passees en majuscule) sur le champ est requis car LIKE est sensible a la casse... le 2nd UPPER est pour contrer les requetes entrees manuellement... (remarque, avec MySQL 5 et SQL Server, on aurait pu avoir simplement "nom_famille LIKE '$lettre%' COLLATE UTF_GENERAL_CI" ou mieux ailleurs : "nom_famille ILIKE '$lettre%'" mais c'est pas forcement portable)
 		}
-/// AFFICHAGES_LATERAUX (connexes)
-		echo association_navigation_onglets('titre_onglet_membres', 'adherents');
-/// AFFICHAGES_LATERAUX : TOTAUX : effectifs par statuts
-		$membres = $GLOBALS['association_liste_des_statuts'];
-		array_shift($membres); // on sort les anciens membres
-		$liste_decomptes = array();
-		foreach ($membres as $statut) {
-			$classe_css = $GLOBALS['association_styles_des_statuts'][$statut];
-			$liste_decomptes[$classe_css] = array( 'adherent_liste_nombre_'.$statut, sql_countsel('spip_asso_membres', "statut_interne='$statut'"), );
-		}
-		echo association_tablinfos_effectifs('adherents', $liste_decomptes);
-/// AFFICHAGES_LATERAUX : TOTAUX : montants des cotisations durant l'annee en cours
-		$annee = date('Y'); // dans la requete SQL est : DATE_FORMAT(NOW(), '%Y') ou YEAR(NOW())
-		$data = sql_fetsel('SUM(recette) AS somme_recettes, SUM(depense) AS somme_depenses', 'spip_asso_comptes', "DATE_FORMAT('date', '%Y')=$annee AND imputation=".sql_quote($GLOBALS['association_metas']['pc_cotisations']) );
-		echo association_tablinfos_montants('cotisations_'.$annee, $data['somme_recettes'], $data['somme_depenses']);
-/// AFFICHAGES_LATERAUX : RACCOURCIS
-		echo association_navigation_raccourcis(array(
-			array('gerer_les_groupes', 'annonce.gif', array('groupes'), array('voir_groupes', 'association', 100) ), // l'id groupe passe en parametre est a 100 car ce sont les groupes utilisateurs et non les autorisations qu'on liste
-			array('menu2_titre_mailing', 'mail-24.png', array('mailing', ((intval($id_groupe)>99)?"&filtre_id_groupe=$id_groupe":'').($statut_interne?"&filtre_statut_interne=$statut_interne":'')), array('relancer_membres', 'association') ),
-			array('synchronise_asso_membre_lien', 'reload-32.png', array('synchroniser_asso_membres'), array('gerer_membres', 'association') ),
-		), 2);
-/// AFFICHAGES_LATERAUX : Forms-PDF
-		list($where_adherents, $jointure_adherents, $code_liste_membres) = adherents_liste($lettre, $critere, $statut_interne, $id_groupe); // on appelle ici la fonction qui calcule le code du tableau de membres pour pouvoir recuperer la liste des parametres a transmettre pour les pdf
-		if ( autoriser('exporter_membres', 'association') ) { // etiquettes
-			echo association_form_etiquettes($where_adherents, $jointure_adherents, $suffixe_pdf);
-		}
-		if ( autoriser('exporter_membres', 'association')
-		) { // tableau des membres
-			$champsExclus = array();
-			if ( !$GLOBALS['association_metas']['civilite'] )
-				$champsExclus[] = 'sexe';
-			if ( !$GLOBALS['association_metas']['prenom'] )
-				$champsExclus[] = 'prenom';
-			if ( !$GLOBALS['association_metas']['id_asso'] )
-				$champsExclus[] = 'id_asso';
-			echo association_form_listepdf('membre', array('where_adherents'=>$where_adherents, 'jointure_adherents'=>$jointure_adherents, 'statut_interne'=>$statut_interne, 'suffixe'=>$suffixe_pdf), 'adherent_libelle_', $champsExclus, TRUE);
-		}
-/// AFFICHAGES_CENTRAUX (corps)
-		debut_cadre_association('annonce.gif', 'adherent_titre_liste_actifs');
-/// AFFICHAGES_CENTRAUX : FILTRES
-		$filtre_categorie = '<select name="categorie" onchange="form.submit()">';
-		$filtre_categorie .= '<option value="" ';
-		$filtre_categorie .= (($id_categorie=='%' || $id_categorie='')?' selected="selected"':'');
-		$filtre_categorie .= '>'. _T('asso:entete_tous') .'</option>';
-		$sql = sql_select(
-			'id_categorie, valeur, libelle',
-			'spip_asso_categories',
-			sql_in_select('id_categorie', 'id_categorie', 'spip_asso_membres', '', 'id_categorie'), // uniquement les categories utilisees
-			'', 'valeur');
-		while ($categorie = sql_fetch($sql)) {
-			$filtre_categorie .= '<option value="'.$categorie['id_categorie'].'"';
-			$filtre_categorie .= ($id_categorie==$categorie['id_categorie']?' selected="selected"':'');
-//			$filtre_categorie .= '>'.$categorie['valeur'].' - '.$categorie['libelle'].'</option>'; // long ; comme pour les comptes (ref - intitule)
-			$filtre_categorie .= '>'.$categorie['valeur'].'</option>'; // court (ou pas) : comme pour les groupes
-		}
-		echo association_form_filtres(array(
-			'lettre' => array($lettre, 'asso_membres', 'nom_famille', generer_url_ecrire('adherents', (!$id?($id_groupe?"&groupe=$id_groupe":'').($statut_interne?"&statut=$statut_interne":'').($id_categorie?"&categorie=$id_categorie":''):"&id=$id") ), ),
-			'id' => $id,
-			'groupe' => $id_groupe, // ne pas proposer que si on affiche les groupes : on peut vouloir filtrer par groupe sans pour autant les afficher
-			'statut'=> $statut_interne,
-		), 'adherents', array(
-			'categorie' => $filtre_categorie,
-		));
-/// AFFICHAGES_CENTRAUX : TABLEAU
-		echo $code_liste_membres;
-		fin_page_association();
+		$critere = implode(' AND ', $args_sql);
+		$suffixe_pdf = "membres_$statut_interne".'_';
+		$suffixe_pdf .= $lettre?str_replace(array('/', '\\', '?', '%', '*', ':', '|', '"', '<', '>', '.', ' ', '(', ')', ';', '&', '#', '[', ']', ), '~', utf8_decode($lettre) ):'tous'; // caracteres problematiques : http://en.wikipedia.org/wiki/Filename#Reserved_characters_and_words
+		$suffixe_pdf .= "_$id_categorie"."_$id_groupe";
+	} else { // si le filtre ID est actif, on ignore les autres filtres
+		$critere = "m.id_auteur=$id";
+		$id_groupe = 0;
+		$id_categorie = 0;
+		$suffixe_pdf = "membre$id";
+		$jointure_groupe = '';
 	}
+/// AFFICHAGES_LATERAUX (connexes)
+	echo association_navigation_onglets('titre_onglet_membres', 'adherents');
+/// AFFICHAGES_LATERAUX : TOTAUX : effectifs par statuts
+	$membres = $GLOBALS['association_liste_des_statuts'];
+	array_shift($membres); // on sort les anciens membres
+	$liste_decomptes = array();
+	foreach ($membres as $statut) {
+		$classe_css = $GLOBALS['association_styles_des_statuts'][$statut];
+		$liste_decomptes[$classe_css] = array( 'adherent_liste_nombre_'.$statut, sql_countsel('spip_asso_membres', "statut_interne='$statut'"), );
+	}
+	echo association_tablinfos_effectifs('adherents', $liste_decomptes);
+/// AFFICHAGES_LATERAUX : TOTAUX : montants des cotisations durant l'annee en cours
+	list($id_periode, $critere_periode) = association_passeparam_periode('operation', 'asso_comptes', 0); // annee/exercice actuelle/courant
+	$data = sql_fetsel('SUM(recette) AS somme_recettes, SUM(depense) AS somme_depenses', 'spip_asso_comptes', "$critere_periode AND imputation=".sql_quote($GLOBALS['association_metas']['pc_cotisations']) );
+	echo association_tablinfos_montants('cotisations_'.$id_periode, $data['somme_recettes'], $data['somme_depenses']);
+/// AFFICHAGES_LATERAUX : RACCOURCIS
+	echo association_navigation_raccourcis(array(
+		array('gerer_les_groupes', 'annonce.gif', array('groupes'), array('voir_groupes', 'association', 100) ), // l'id groupe passe en parametre est a 100 car ce sont les groupes utilisateurs et non les autorisations qu'on liste
+		array('menu2_titre_mailing', 'mail-24.png', array('mailing', ((intval($id_groupe)>99)?"&filtre_id_groupe=$id_groupe":'').($statut_interne?"&filtre_statut_interne=$statut_interne":'')), array('relancer_membres', 'association') ),
+		array('synchronise_asso_membre_lien', 'reload-32.png', array('synchroniser_asso_membres'), array('gerer_membres', 'association') ),
+	), 2);
+/// AFFICHAGES_LATERAUX : Forms-PDF
+	if ( autoriser('exporter_membres', 'association') ) { // etiquettes
+		echo association_form_etiquettes($critere, $jointure_adherents, $suffixe_pdf);
+	}
+	if ( autoriser('exporter_membres', 'association') ) { // tableau des membres
+		$champsExclus = array();
+		if ( !$GLOBALS['association_metas']['civilite'] )
+			$champsExclus[] = 'sexe';
+		if ( !$GLOBALS['association_metas']['prenom'] )
+			$champsExclus[] = 'prenom';
+		if ( !$GLOBALS['association_metas']['id_asso'] )
+			$champsExclus[] = 'id_asso';
+		echo association_form_listepdf('membre', array('where_adherents'=>$critere, 'jointure_adherents'=>$jointure_adherents, 'statut_interne'=>$statut_interne, 'suffixe'=>$suffixe_pdf), 'adherent_libelle_', $champsExclus, TRUE);
+	}
+/// AFFICHAGES_CENTRAUX (corps)
+	debut_cadre_association('annonce.gif', 'adherent_titre_liste_actifs');
+/// AFFICHAGES_CENTRAUX : FILTRES
+	$filtre_categorie = '<select name="categorie" onchange="form.submit()">';
+	$filtre_categorie .= '<option value="" ';
+	$filtre_categorie .= (($id_categorie=='%' || $id_categorie='')?' selected="selected"':'');
+	$filtre_categorie .= '>'. _T('asso:entete_tous') .'</option>';
+	$sql = sql_select(
+		'id_categorie, valeur, libelle',
+		'spip_asso_categories',
+		sql_in_select('id_categorie', 'id_categorie', 'spip_asso_membres', '', 'id_categorie'), // uniquement les categories utilisees
+		'', 'valeur');
+	while ($categorie = sql_fetch($sql)) {
+		$filtre_categorie .= '<option value="'.$categorie['id_categorie'].'"';
+		$filtre_categorie .= ($id_categorie==$categorie['id_categorie']?' selected="selected"':'');
+//		$filtre_categorie .= '>'.$categorie['valeur'].' - '.$categorie['libelle'].'</option>'; // long ; comme pour les comptes (ref - intitule)
+		$filtre_categorie .= '>'.$categorie['valeur'].'</option>'; // court (ou pas) : comme pour les groupes
+	}
+	echo association_form_filtres(array(
+		'lettre' => array($lettre, 'asso_membres', 'nom_famille', generer_url_ecrire('adherents', (!$id?($id_groupe?"&groupe=$id_groupe":'').($statut_interne?"&statut=$statut_interne":'').($id_categorie?"&categorie=$id_categorie":''):"&id=$id") ), ),
+		'id' => $id,
+		'groupe' => $id_groupe, // ne pas proposer que si on affiche les groupes : on peut vouloir filtrer par groupe sans pour autant les afficher
+		'statut'=> $statut_interne,
+	), 'adherents', array(
+		'categorie' => $filtre_categorie,
+	));
+/// AFFICHAGES_CENTRAUX : TABLEAU
+	echo adherents_liste($critere, $statut_interne, $args_url);
+	fin_page_association();
 }
+
 
 /**
  * liste des adherents
  *
- * @param string $lettre
- *   Filtre lettre
  * @param string $critere
- *   SQL de restriction selon statut
+ *   SQL de restriction selon les filtres
  * @param string $statut_interne
  *   Filtre statut interne
- * @param int $id_groupe
- *   Filtre groupe
- * @return array
- *   Liste du :
- *   - critere de requete SQL en fonction des filtres actifs,
- *   - possible jonction SQL sur la table des groupes,
- *   - code HTML du tableau affichant la liste des membres en fonction des
- *     filtres actifs et de la configuration (champs affiches ou pas)
+ * @param array $args_url
+ *   Complement (parametres) d'URL pour la pagination
+ * @return string
+ *   Code HTML du tableau affichant la liste des membres en fonction des
+ * filtres actifs et de la configuration (champs affiches ou pas)
  */
-function adherents_liste($lettre, $critere, $statut_interne, $id_groupe) {
-/// INITIALISATIONS
-	if ($lettre)
-		$critere .= " AND UPPER(m.nom_famille) LIKE UPPER('$lettre%') "; // le 1er UPPER (plutot que LOWER puisque les lettres sont mises et passees en majuscule) sur le champ est requis car LIKE est sensible a la casse... le 2nd UPPER est pour contrer les requetes entrees manuellement... (remarque, avec MySQL 5 et SQL Server, on aurait pu avoir simplement "nom_famille LIKE '$lettre%' COLLATE UTF_GENERAL_CI" ou mieux ailleurs : "nom_famille ILIKE '$lettre%'" mais c'est pas forcement portable)
-	if ($id_groupe) {
-		$critere .= " AND g.id_groupe=$id_groupe ";
-		$jointure_groupe = ' LEFT JOIN spip_asso_fonctions AS g ON m.id_auteur=g.id_auteur ';
-	} else {
-		$jointure_groupe = '';
-	}
+function adherents_liste($critere, $statut_interne, $args_url) {
 	$chercher_logo = charger_fonction('chercher_logo', 'inc');
 	include_spip('inc/filtres_images_mini');
 	$limit = intval(_request('debut')) . "," . _ASSOCIASPIP_LIMITE_SOUSPAGE;
@@ -253,11 +255,6 @@ function adherents_liste($lettre, $critere, $statut_interne, $id_groupe) {
 	. $auteurs
 	. "\n</table>\n";
 /// AFFICHAGES_CENTRAUX : PAGINATION
-	$arg = array();
-	if ($lettre)
-		$arg[]= "lettre=$lettre";
-	if ($statut_interne != 'defaut')
-		$arg[]= "statut_interne=$statut_interne";
 	$res .= "<table width='100%' class='asso_tablo_filtres'><tr>\n";
 	if (autoriser('editer_membres', 'association', 100)) {
 		$nav .= "<td align='right' class='formulaire'>\n";
@@ -275,9 +272,9 @@ function adherents_liste($lettre, $critere, $statut_interne, $id_groupe) {
 		$nav .= '<input type="hidden" name="statut_courant" value="'.$statut_interne.'" />'
 		.  '</td>';
 	}
-	$res .= association_form_souspage(array('spip_asso_membres', $critere), 'adherents', $arg, $nav);
+	$res .= association_form_souspage(array('spip_asso_membres', $critere), 'adherents', $args_url, $nav);
 // FIN
-	return 	array($critere, $jointure_groupe, generer_form_ecrire('action_adherents', $res));
+	return 	generer_form_ecrire('action_adherents', $res);
 }
 
 ?>
