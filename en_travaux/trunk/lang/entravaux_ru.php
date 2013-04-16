f651d8b7b6f69d0c64f5b3e1d88e74c77723cf83
serge.markitanenko@gmail.com <serge.markitanenko@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-16 03:36:41 +0000

    langues (entravaux)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@72065 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ en_travaux/trunk/lang/entravaux_ru.php
@@ -0,0 +1,37 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/entravaux-193?lang_cible=ru
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'activer_message' => 'Отключить сайт',
+
+	// E
+	'en_travaux' => 'Обслуживание сайта',
+	'erreur_droit' => 'Плагин "Отключение сайта" может использовать только вебмастер сайта !',
+
+	// I
+	'info_disallow_robot' => 'Плагин обеспечивает недоступность информации на сайте для поисковых систем. Когда активирован, конечно.',
+	'info_maintenance_en_cours' => 'Сайт отключен! Только вебмастера могут войти в систему управления сайтом.',
+	'info_message' => 'Введите текст, который увидят посетители сайта, пока он будет отключен.',
+	'info_travaux_texte' => 'Сайт находится в разработке.
+_ Попробуйте зайти через несколько дней.',
+
+	// L
+	'label_disallow_robots' => 'Убрать возможность индексации сайта поисковыми роботами',
+
+	// M
+	'message_temporaire' => 'Сообщение для посетителей:',
+
+	// P
+	'parametrage_page_travaux' => 'Настройки режима',
+
+	// T
+	'texte_lien_publier' => 'Включить сайт'
+);
+
+?>
