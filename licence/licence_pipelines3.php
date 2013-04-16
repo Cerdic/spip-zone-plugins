f349688182f63607527641335adb4ddd3c6c7e57
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-16 09:02:17 +0000

    En SPIP 3.0 plus besoin du pipeline pre_edition
    Il suffit d'utiliser le plugin medias (afin qu'il passe devant pour sa déclaration d'objet) et on utilise donc l'ajout dans la déclaration de l'objet document ou article
    
    Version 0.4.7
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@72073 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- licence/licence_pipelines3.php
+++ licence/licence_pipelines3.php
@@ -1,21 +1,26 @@
 <?php
 /**
  * Plugin Licence
- * (c) 2007-2012 fanouch
+ * (c) 2007-2013 fanouch
  * Distribue sous licence GPL
- *
+ * 
+ * @package SPIP\Licences\Pipelines
  */
 
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 /**
+ * Insertion dans le pipeline affiche_milieu (SPIP)
+ * 
  * Insertion au centre des pages d'articles dans le privé
- * Affiche un formulaire d'édition de la licence de l'article
+ * d'un formulaire d'édition de la licence de l'article
  *
- * @param array $flux Le contexte du pipeline
+ * @param array $flux 
+ * 		Le contexte du pipeline
+ * @return array $flux
+ * 		Le contexte du pipeline modifié
  */
 function licence_affiche_milieu($flux) {
-
 	if ($flux['args']['exec'] == 'article'){
 		$contexte['id_article'] = $flux["args"]["id_article"];
 		
@@ -30,14 +35,18 @@ function licence_affiche_milieu($flux) {
 }
 
 /**
+ * Insertion dans le pipeline pre_insertion (SPIP)
+ * 
  * Si création d'un nouvel article, on lui attribue la licence par défaut si
  * on utilise correctement les fonctions internes de SPIP pour créer des articles
  * cf : http://trac.rezo.net/trac/spip/browser/branches/spip-2.1/ecrire/action/editer_article.php#L214
  *
- * @param array $flux Le contexte du pipeline
+ * @param array $flux 
+ * 		Le contexte du pipeline
+ * @return array $flux
+ * 		Le contexte du pipeline auquel on a ajouté la licence
  */
 function licence_pre_insertion($flux){
-	// si creation d'un nouvel article lui attribuer la licence par defaut de la config
 	if ($flux['args']['table']=='spip_articles') {
 		include_spip('inc/config');
 		$licence_defaut = lire_config('licence/licence_defaut');
@@ -48,34 +57,22 @@ function licence_pre_insertion($flux){
 
 
 /**
- * Insertion dans le pipeline editer_contenu_objet
+ * Insertion dans le pipeline editer_contenu_objet (SPIP)
+ * 
+ * Ajout dans le formulaire d'édition de document du sélecteur de licence
  *
- * @param array $flux Le contexte du pipeline
- * @return $flux Le contexte du pipeline complété
+ * @param array $flux 
+ * 		Le contexte du pipeline
+ * @return array  $flux 
+ * 		Le contexte du pipeline complété
  */
 function licence_editer_contenu_objet($flux){
-	$type_form = $flux['args']['type'];
-	$id_document = $flux['args']['id'];
-	if(in_array($type_form,array('document'))){
+	if(in_array($flux['args']['type'],array('document'))){
 		if(preg_match(",<li [^>]*class=[\"']editer editer_credits.*>(.*)<\/li>,Uims",$flux['data'],$regs)){
 			include_spip('inc/licence');
-			$ajouts = recuperer_fond('prive/licence_document_saisies',array('id_document'=>$id_document,'licences' => $GLOBALS['licence_licences']));
+			$ajouts = recuperer_fond('prive/licence_document_saisies',array('id_document'=>$flux['args']['id'],'licences' => $GLOBALS['licence_licences']));
 			$flux['data'] = str_replace($regs[0],$ajouts.$regs[0],$flux['data']);
 		}
-	}
-	return $flux;
-}
-
-/**
- * Insertion dans le pipeline pre_edition
- * Récupération de l'id_licence lors de la validation du formulaire de documents
- *
- * @param array $flux Le contexte du pipeline
- * @return $flux Le contexte du pipeline complété
- */
-function licence_pre_edition($flux){
-	if(($flux['args']['type'] == 'document') && ($flux['args']['action'] == 'modifier') && _request('id_licence')){
-		$flux['data']['id_licence'] = _request('id_licence');
 	}
 	return $flux;
 }
@@ -84,10 +81,12 @@ function licence_pre_edition($flux){
  * Insertion dans le pipeline xmlrpc_methodes (xmlrpc)
  * Ajout de méthodes xml-rpc spécifiques à Licence
  * 
- * @param array $flux : un array des methodes déjà présentes, fonctionnant sous la forme :
- * -* clé = nom de la méthode;
- * -* valeur = le nom de la fonction à appeler;
- * @return array $flux : l'array complété avec nos nouvelles méthodes 
+ * @param array $flux
+ * 		Un array des methodes déjà présentes, fonctionnant sous la forme :
+ * 			-* clé = nom de la méthode;
+ * 			-* valeur = le nom de la fonction à appeler;
+ * @return array $flux
+ * 		L'array complété avec nos nouvelles méthodes 
  */
 function licence_xmlrpc_methodes($flux){
 	$flux['spip.liste_licences'] = 'licence_liste_licences';
@@ -96,7 +95,12 @@ function licence_xmlrpc_methodes($flux){
 
 /**
  * Insertion dans le pipeline xmlrpc_server_class (xmlrpc)
- * Ajout de fonctions spécifiques utilisées par le serveur xml-rpc 
+ * 
+ * Ajout de fonctions spécifiques utilisées par le serveur xml-rpc
+ * On inclu le fichier contenant les classes spécifiques
+ * 
+ * @param $flux
+ * @return $flux
  */
 function licence_xmlrpc_server_class($flux){
 	include_spip('inc/licence_xmlrpc');
