9a7d020c65c5523c3938d1d7077f83e78089b27b
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-16 08:58:53 +0000

    On peut effectivement garder les deux rel (Cedric)
    Sécurité
    
    PHPdoc
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@72070 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- licence/modeles/licence_fonctions.php
+++ licence/modeles/licence_fonctions.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * Plugin Licence
+ * (c) 2007-2013 fanouch
+ * Distribue sous licence GPL
+ * 
+ * Inclusion des licences utiles pour le squelettes modeles/licence.html 
+ * 
+ */
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 include_spip('inc/licence');
 
