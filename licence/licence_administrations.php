2fe90538b6fbba65dad7f5a45c8b92c58d3e1b74
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-16 09:00:17 +0000

    PHPdoc et on efface la meta de configuration
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@72072 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- licence/licence_administrations.php
+++ licence/licence_administrations.php
@@ -2,15 +2,27 @@
 /**
  * Plugin Licence
  *
- * (c) 2007-2012 fanouch
+ * (c) 2007-2013 fanouch
  * Distribue sous licence GPL
  *
  * Modification des tables
- *
- **/
+ * 
+ * @package SPIP\Licence\Administration
+ */
 
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
+/**
+ * Installation ou mise à jour du plugin
+ * 
+ * Ajoute un champ id_licence sur les tables spip_articles et spip_documents
+ * 
+ * @param string $nom_meta_base_version
+ * 		Le nom de la meta d'installation
+ * @param float $version_cible
+ * 		La version vers laquelle installer
+ * @return void
+ */
 function licence_upgrade($nom_meta_base_version,$version_cible){
 	$maj = array();
 	
@@ -25,8 +37,22 @@ function licence_upgrade($nom_meta_base_version,$version_cible){
 	maj_plugin($nom_meta_base_version, $version_cible, $maj);
 }
 
-
+/**
+ * Désinstallation du plugin
+ * 
+ * On supprime : 
+ * -* La meta de configuration
+ * -* La meta d'installation
+ * 
+ * On laisse :
+ * -* Les nouveaux champs sur les tables spip_documents et spip_articles
+ * 
+ * @param string $nom_meta_base_version
+ * 		Le nom de la meta d'installation
+ * @return void
+ */
 function licence_vider_tables($nom_meta_base_version) {
+	effacer_meta('licence');
 	effacer_meta($nom_meta_base_version);
 }
 
