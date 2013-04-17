50ce78baf676b345b799f3f921b21adb4dc660ab
serge.markitanenko@gmail.com <serge.markitanenko@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-17 03:36:33 +0000

    langues (paquet-entravaux)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@72112 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ en_travaux/trunk/lang/paquet-entravaux_ru.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-entravaux?lang_cible=ru
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// E
+	'entravaux_description' => 'Позволяет отключить доступ к сайту для посетителей на время проведения обслуживания или доработок сайта.',
+	'entravaux_nom' => 'Отключить сайт (En travaux)',
+	'entravaux_slogan' => 'Позволяет отключать доступ к сайту для посетителей'
+);
+
+?>
