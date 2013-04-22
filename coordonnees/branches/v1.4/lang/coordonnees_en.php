145d8218df2e48b807380eee81e77f9a7597afb2
cyril@cym.fr <cyril@cym.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-22 17:28:39 +0000

    corrections sur les chaines de langue (Department n'existe pas en anglais, c'est "State") et autres corrections cosmétiques; à reporter dans le trounque;
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@72236 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- coordonnees/branches/v1.4/lang/coordonnees_en.php
+++ coordonnees/branches/v1.4/lang/coordonnees_en.php
@@ -8,83 +8,83 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
-	'adresse_perso' => 'Home',
-	'adresse_pro' => 'Professional',
-	'adresses' => 'Addresses',
-	'ajouter_adresse' => 'Add an address',
-	'ajouter_email' => 'Add an email',
-	'ajouter_telephone' => 'Add a phone number',
-	'articles' => 'Articles', # NEW
-	'auteurs' => 'Auteurs', # NEW
+	'adresse_perso'                 => 'Home',
+	'adresse_pro'                   => 'Professional',
+	'adresses'                      => 'Addresses',
+	'ajouter_adresse'               => 'Add an address',
+	'ajouter_email'                 => 'Add an email',
+	'ajouter_telephone'             => 'Add a phone number',
+	'articles'                      => 'Articles', # NEW
+	'auteurs'                       => 'Auteurs', # NEW
 
 	// C
-	'configuration_coordonnees' => 'Configuration des coordonnées', # NEW
+	'configuration_coordonnees'     => 'Configuration des coordonnées', # NEW
 	'confirmer_suppression_adresse' => 'Are you sure you want to detele this address ?',
-	'confirmer_suppression_email' => 'Are you sure you want to detele this email ?',
-	'confirmer_suppression_numero' => 'Are you sure you want to detele this phone number ?',
+	'confirmer_suppression_email'   => 'Are you sure you want to detele this email ?',
+	'confirmer_suppression_numero'  => 'Are you sure you want to detele this phone number ?',
 
 	// E
-	'editer_adresse' => 'Edit an address',
-	'editer_email' => 'Edit an email',
-	'editer_numero' => 'Edit a phone number',
-	'emails' => 'Emails',
-	'explication_objets_actifs' => 'Sur quels objets éditoriaux proposer les coordonnées ?', # NEW
-	'explication_type_adresse' => 'Type can be private or work',
-	'explication_type_email' => 'Type can be private or profesionnal',
-	'explication_type_numero' => 'Type can be private, work or fax',
+	'editer_adresse'                => 'Edit an address',
+	'editer_email'                  => 'Edit an email',
+	'editer_numero'                 => 'Edit a phone number',
+	'emails'                        => 'Emails',
+	'explication_objets_actifs'     => 'Sur quels objets éditoriaux proposer les coordonnées ?', # NEW
+	'explication_type_adresse'      => 'Type can be private or work',
+	'explication_type_email'        => 'Type can be private or profesionnal',
+	'explication_type_numero'       => 'Type can be private, work or fax',
 
 	// F
-	'fax' => 'Fax.',
+	'fax'                           => 'Fax.',
 
 	// I
-	'info_adresse_utilisee_par' => 'This address used by :',
-	'info_email_utilise_par' => 'This email is used by :',
-	'info_gauche_numero_adresse' => 'Address id',
-	'info_gauche_numero_email' => 'Email id',
-	'info_gauche_numero_numero' => 'Number id',
-	'info_numero_utilise_par' => 'This phone number is used by :',
-	'item_nouveau_numero' => 'New phone number',
-	'item_nouvel_email' => 'New email',
-	'item_nouvelle_adresse' => 'New address',
+	'info_adresse_utilisee_par'     => 'This address used by :',
+	'info_email_utilise_par'        => 'This email is used by :',
+	'info_gauche_numero_adresse'    => 'Address id',
+	'info_gauche_numero_email'      => 'Email id',
+	'info_gauche_numero_numero'     => 'Number id',
+	'info_numero_utilise_par'       => 'This phone number is used by :',
+	'item_nouveau_numero'           => 'New phone number',
+	'item_nouvel_email'             => 'New email',
+	'item_nouvelle_adresse'         => 'New address',
 
 	// L
-	'label_boite_postale' => 'Postal box',
-	'label_code_postal' => 'Postal code',
-	'label_complement' => 'Additional information',
-	'label_email' => 'Email',
-	'label_numero' => 'Phone number',
-	'label_objets_actifs' => 'Objets', # NEW
-	'label_pays' => 'Country',
-	'label_region' => 'Department',
-	'label_titre' => 'Titre', # NEW
-	'label_type_adresse' => 'Address type',
-	'label_type_email' => 'Type of email',
-	'label_type_numero' => 'Type of phone number',
-	'label_ville' => 'City',
-	'label_voie' => 'Address',
+	'label_boite_postale'           => 'Postal box',
+	'label_code_postal'             => 'Postal code',
+	'label_complement'              => 'Additional information',
+	'label_email'                   => 'Email',
+	'label_numero'                  => 'Phone number',
+	'label_objets_actifs'           => 'Objets', # NEW
+	'label_pays'                    => 'Country',
+	'label_region'                  => 'State',
+	'label_titre'                   => 'Titre', # NEW
+	'label_type_adresse'            => 'Address type',
+	'label_type_email'              => 'Type of email',
+	'label_type_numero'             => 'Type of phone number',
+	'label_ville'                   => 'City',
+	'label_voie'                    => 'Address',
 
 	// M
-	'mobile' => 'Mobile',
-	'modifier_adresse' => 'Edit this address',
-	'modifier_email' => 'Edit this email',
-	'modifier_numero' => 'Edit this phone number',
+	'mobile'                        => 'Mobile',
+	'modifier_adresse'              => 'Edit this address',
+	'modifier_email'                => 'Edit this email',
+	'modifier_numero'               => 'Edit this phone number',
 
 	// N
-	'nouveau_numero' => 'New phone number',
-	'nouvel_email' => 'New email',
-	'nouvelle_adresse' => 'New address',
-	'numeros' => 'Phone numbers',
+	'nouveau_numero'                => 'New phone number',
+	'nouvel_email'                  => 'New email',
+	'nouvelle_adresse'              => 'New address',
+	'numeros'                       => 'Phone numbers',
 
 	// R
-	'rubriques' => 'Rubriques', # NEW
+	'rubriques'                     => 'Rubriques', # NEW
 
 	// S
-	'supprimer_adresse' => 'Delette this address',
-	'supprimer_email' => 'Delete this email',
-	'supprimer_numero' => 'Delete this phone number',
+	'supprimer_adresse'             => 'Delette this address',
+	'supprimer_email'               => 'Delete this email',
+	'supprimer_numero'              => 'Delete this phone number',
 
 	// T
-	'titre_coordonnees' => 'Address book'
+	'titre_coordonnees'             => 'Address book'
 );
 
 ?>
--- coordonnees/branches/v1.4/lang/coordonnees_fr.php
+++ coordonnees/branches/v1.4/lang/coordonnees_fr.php
@@ -6,104 +6,104 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
-	'adresses' => 'Adresses',
-	'ajouter_adresse' => 'Ajouter une adresse',
-	'ajouter_email' => 'Ajouter un email',
-	'ajouter_telephone' => 'Ajouter un numéro',
+	'adresses'                      => 'Adresses',
+	'ajouter_adresse'               => 'Ajouter une adresse',
+	'ajouter_email'                 => 'Ajouter un email',
+	'ajouter_telephone'             => 'Ajouter un numéro',
 
 	// C
-	'configuration_coordonnees' => 'Configuration des coordonnées',
+	'configuration_coordonnees'     => 'Configuration des coordonnées',
 	'confirmer_suppression_adresse' => 'Voulez-vous vraiment supprimer cette adresse ?',
-	'confirmer_suppression_email' => 'Voulez-vous vraiment supprimer ce courriel ?',
-	'confirmer_suppression_numero' => 'Voulez-vous vraiment supprimer ce numéro ?',
+	'confirmer_suppression_email'   => 'Voulez-vous vraiment supprimer ce courriel ?',
+	'confirmer_suppression_numero'  => 'Voulez-vous vraiment supprimer ce numéro ?',
 
 	// E
-	'editer_adresse' => 'Éditer une adresse',
-	'editer_email' => 'Éditer un courriel',
-	'editer_numero' => 'Éditer un numéro',
-	'emails' => 'Courriels',
-	'explication_objets_actifs' => 'Sur quels objets éditoriaux proposer les coordonnées ?',
+	'editer_adresse'                => 'Éditer une adresse',
+	'editer_email'                  => 'Éditer un courriel',
+	'editer_numero'                 => 'Éditer un numéro',
+	'emails'                        => 'Courriels',
+	'explication_objets_actifs'     => 'Sur quels objets éditoriaux proposer les coordonnées ?',
 
 	// I
-	'info_adresse_utilisee_par' => 'Adresse utilisée par :',
-	'info_email_utilise_par' => 'Courriel utilisé par :',
-	'info_gauche_numero_adresse' => 'N° Adresse',
-	'info_gauche_numero_email' => 'N° Email',
-	'info_gauche_numero_numero' => 'N° Numéro',
-	'info_numero_utilise_par' => 'Numéro utilisé par :',
-	'item_nouveau_numero' => 'Nouveau numéro',
-	'item_nouvel_email' => 'Nouveau courriel',
-	'item_nouvelle_adresse' => 'Nouvelle adresse',
+	'info_adresse_utilisee_par'     => 'Adresse utilisée par :',
+	'info_email_utilise_par'        => 'Courriel utilisé par :',
+	'info_gauche_numero_adresse'    => 'N° Adresse',
+	'info_gauche_numero_email'      => 'N° Email',
+	'info_gauche_numero_numero'     => 'N° Numéro',
+	'info_numero_utilise_par'       => 'Numéro utilisé par :',
+	'item_nouveau_numero'           => 'Nouveau numéro',
+	'item_nouvel_email'             => 'Nouveau courriel',
+	'item_nouvelle_adresse'         => 'Nouvelle adresse',
 
 	// L
-	'label_boite_postale' => 'Boîte Postale',
-	'label_code_postal' => 'Code Postal',
-	'label_complement' => 'Complément d\'adresse',
-	'label_email' => 'Courriel',
-	'label_numero' => 'Numéro',
-	'label_objets_actifs' => 'Objets',
-	'label_pays' => 'Pays',
-	'label_region' => 'Région',
-	'label_telephone' => 'Téléphone',
-	'label_titre' => 'Titre',
-	'label_type_adresse' => 'Type d\'adresse',
-	'label_type_email' => 'Type de courriel',
-	'label_type_numero' => 'Type de numéro',
-	'label_ville' => 'Commune',
-	'label_voie' => 'Adresse',
+	'label_boite_postale'           => 'Boîte Postale',
+	'label_code_postal'             => 'Code Postal',
+	'label_complement'              => 'Complément d\'adresse',
+	'label_email'                   => 'Courriel',
+	'label_numero'                  => 'Numéro',
+	'label_objets_actifs'           => 'Objets',
+	'label_pays'                    => 'Pays',
+	'label_region'                  => 'Région',
+	'label_telephone'               => 'Téléphone',
+	'label_titre'                   => 'Titre',
+	'label_type_adresse'            => 'Type d\'adresse',
+	'label_type_email'              => 'Type de courriel',
+	'label_type_numero'             => 'Type de numéro',
+	'label_ville'                   => 'Ville',
+	'label_voie'                    => 'Adresse',
 
 	// M
-	'modifier_adresse' => 'Modifier cette adresse',
-	'modifier_email' => 'Modifier ce courriel',
-	'modifier_numero' => 'Modifier ce numéro',
+	'modifier_adresse'              => 'Modifier cette adresse',
+	'modifier_email'                => 'Modifier ce courriel',
+	'modifier_numero'               => 'Modifier ce numéro',
 
 	// N
-	'nouveau_numero' => 'Nouveau numéro',
-	'nouvel_email' => 'Nouveau courriel',
-	'nouvelle_adresse' => 'Nouvelle adresse',
-	'numeros' => 'Numéros',
+	'nouveau_numero'                => 'Nouveau numéro',
+	'nouvel_email'                  => 'Nouveau courriel',
+	'nouvelle_adresse'              => 'Nouvelle adresse',
+	'numeros'                       => 'Numéros',
 
 	// S
-	'supprimer_adresse' => 'Supprimer cette adresse',
-	'supprimer_email' => 'Supprimer ce courriel',
-	'supprimer_numero' => 'Supprimer ce numéro',
+	'supprimer_adresse'             => 'Supprimer cette adresse',
+	'supprimer_email'               => 'Supprimer ce courriel',
+	'supprimer_numero'              => 'Supprimer ce numéro',
 
 	// T
 	// types (RFC2426) http://microformats.org/wiki/hcard-fr#adr_tel_email_types
 	// traductions  http://www.alsacreations.com/tuto/lire/1223-microformats-composes.html#hcard
-	'titre_coordonnees' => 'Coordonnées',
-	'type_adr_dom' => 'R&eacute;sidentielle',
-	'type_adr_home' => 'Personnelle',
-	'type_adr_intl' => '&Eacute;trang&egrave;re',
-	'type_adr_parcel' => 'Parcelle',
-	'type_adr_postal' => 'Postale (en poste restante)',
-	'type_adr_pref' => 'Principale',
-	'type_adr_principale'=>'Principale',
-	'type_adr_work' =>  'Professionnelle',
-	'type_email_internet' => 'Internet', // user@domain
-	'type_email_pref' => 'Préféré',
-	'type_email_x400' => 'X.400',
-	'type_mel_intl' => 'International', // ?
-	'type_mel_home' => 'Personnel',
-	'type_mel_work' => 'Professionnel',
-	'type_tel_bbs' => 'Service de messagerie',
-	'type_tel_car' => 'Voiture',
-	'type_tel_cell' => 'Portable', // a cote de "cell" on trouve "mobile" qui est une terminologie tres courante en Amerique du nord
-	'type_tel_dsl' => 'box DSL', // bien que pas dans la norme, c'est de plus en plus en courant et j'arrive jamais a decider s'il faut utiliser "isdn" ou "modem" a la place
-	'type_tel_fax' => 'Télécopie',
-	'type_tel_home' => 'Résidence',
-	'type_tel_isdn' => 'RNIS', // Réseau numérique à intégration de services
-	'type_tel_modem' => 'MoDem informatique',
-	'type_tel_msg' => 'Boîte vocale (répondeur)', // http://fr.wikipedia.org/wiki/R%C3%A9pondeur http://fr.wikipedia.org/wiki/Syst%C3%A8me_de_messagerie_vocale
-	'type_tel_pager' => 'Téléavertisseur (bipeur)',
-	'type_tel_pcs' => 'Service de communication personnel', // http://en.wikipedia.org/wiki/Personal_Communications_Service
-	'type_tel_pref' => 'Favori',
-	'type_tel_principale' => 'Principal',
-	'type_tel_text' => 'Texto', // SMS sur cellulaires et messages sur bipeurs modernes
-	'type_tel_textphone' => 'Retranscripteur texte', // pour non-entendants surtout : http://www.ricability.org.uk/consumer_reports/at_home/buying_a_textphone/what_are_textphones/ http://en.wikipedia.org/wiki/Telecommunications_device_for_the_deaf http://www.nokiaaccessibility.com/textphones.html
-	'type_tel_video' => 'Visiophone (visioconférence)', // http://en.wikipedia.org/wiki/Videotelephony http://fr.wikipedia.org/wiki/Visiophonie http://fr.wikipedia.org/wiki/Visioconf%C3%A9rence
-	'type_tel_voice' => 'Vocal',
-	'type_tel_work' => 'Professionnel',
+	'titre_coordonnees'             => 'Coordonnées',
+	'type_adr_dom'                  => 'R&eacute;sidentielle',
+	'type_adr_home'                 => 'Personnelle',
+	'type_adr_intl'                 => '&Eacute;trang&egrave;re',
+	'type_adr_parcel'               => 'Parcelle',
+	'type_adr_postal'               => 'Postale (en poste restante)',
+	'type_adr_pref'                 => 'Principale',
+	'type_adr_principale'           => 'Principale',
+	'type_adr_work'                 => 'Professionnelle',
+	'type_email_internet'           => 'Internet', // user@domain
+	'type_email_pref'               => 'Préféré',
+	'type_email_x400'               => 'X.400',
+	'type_mel_intl'                 => 'International', // ?
+	'type_mel_home'                 => 'Personnel',
+	'type_mel_work'                 => 'Professionnel',
+	'type_tel_bbs'                  => 'Service de messagerie',
+	'type_tel_car'                  => 'Voiture',
+	'type_tel_cell'                 => 'Portable', // a cote de "cell" on trouve "mobile" qui est une terminologie tres courante en Amerique du nord
+	'type_tel_dsl'                  => 'box DSL', // bien que pas dans la norme, c'est de plus en plus en courant et j'arrive jamais a decider s'il faut utiliser "isdn" ou "modem" a la place
+	'type_tel_fax'                  => 'Télécopie',
+	'type_tel_home'                 => 'Résidence',
+	'type_tel_isdn'                 => 'RNIS', // Réseau numérique à intégration de services
+	'type_tel_modem'                => 'MoDem informatique',
+	'type_tel_msg'                  => 'Boîte vocale (répondeur)', // http://fr.wikipedia.org/wiki/R%C3%A9pondeur http://fr.wikipedia.org/wiki/Syst%C3%A8me_de_messagerie_vocale
+	'type_tel_pager'                => 'Téléavertisseur (bipeur)',
+	'type_tel_pcs'                  => 'Service de communication personnel', // http://en.wikipedia.org/wiki/Personal_Communications_Service
+	'type_tel_pref'                 => 'Favori',
+	'type_tel_principale'           => 'Principal',
+	'type_tel_text'                 => 'Texto', // SMS sur cellulaires et messages sur bipeurs modernes
+	'type_tel_textphone'            => 'Retranscripteur texte', // pour non-entendants surtout : http://www.ricability.org.uk/consumer_reports/at_home/buying_a_textphone/what_are_textphones/ http://en.wikipedia.org/wiki/Telecommunications_device_for_the_deaf http://www.nokiaaccessibility.com/textphones.html
+	'type_tel_video'                => 'Visiophone (visioconférence)', // http://en.wikipedia.org/wiki/Videotelephony http://fr.wikipedia.org/wiki/Visiophonie http://fr.wikipedia.org/wiki/Visioconf%C3%A9rence
+	'type_tel_voice'                => 'Vocal',
+	'type_tel_work'                 => 'Professionnel',
 
 );
 
