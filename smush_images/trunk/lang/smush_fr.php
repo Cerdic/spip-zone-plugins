d3c127b1d1f9b6187ef9f4393bc9d8a3f7295f23
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-25 08:39:24 +0000

    Le fichier de langue manquant
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@72323 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ smush_images/trunk/lang/smush_fr.php
@@ -0,0 +1,24 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// I
+	'info_impossible_utilisation_locale' => 'Les logiciels nécessaires à une utilisation locale ne sont pas tous présents.',
+	'info_logiciel_nok' => 'Le logiciel "@logiciel@" n\'est pas disponible',
+	'info_logiciel_nok_tous' => 'L\'ensemble des logiciels nécessaires ne sont pas utilisables. Vous ne pouvez pas utiliser smush en local.',
+	'info_logiciel_ok' => 'Le logiciel "@logiciel@" semble utilisable',
+	'info_logiciel_ok_tous' => 'L\'ensemble des logiciels nécessaires sont utilisables. Vous pouvez utiliser smush en local.',
+	
+	// L
+	'label_eviter_traitement_auto' => 'Éviter le traitement automatique de toutes les images générées par SPIP',
+	
+	// S
+	'smush_conf_descriptif' => 'Le plugin Smush permet de réduire la taille des images d\'un site au minimum en utilisant des logiciels spécifiques sur le serveur. Cette page permet de le configurer.',
+	
+	// T
+	'titre_test_logiciels' => 'Test de présence des logiciels',
+);
+
+?>
