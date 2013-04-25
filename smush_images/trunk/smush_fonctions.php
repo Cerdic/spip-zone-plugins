1ce0cbe8a30c8779f2318cd3c379167464a30be5
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-25 08:39:32 +0000

    du phpdoc
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@72324 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- smush_images/trunk/smush_fonctions.php
+++ smush_images/trunk/smush_fonctions.php
@@ -1,8 +1,14 @@
 <?php
-
+/**
+ * Plugin smushit
+ * 
+ * Auteur :
+ * kent1 (http://www.kent1.info - kent1@arscenic.info)
+ * 
+ * @package SPIP\Smushit\Fonctions
+ */
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 $GLOBALS['spip_matrice']['image_smush'] = 'smush_pipelines.php';
-
 
 ?>
