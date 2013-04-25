e485286703c79e16fbf315e206134bce27d1e921
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-25 08:38:57 +0000

    Un formulaire de conf permettant d'éviter le passage automatique dans image_smush
    Du PHPdoc
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@72321 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ smush_images/trunk/formulaires/configurer_smush_fonctions.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin smushit
+ * 
+ * Auteur :
+ * kent1 (http://www.kent1.info - kent1@arscenic.info)
+ * 
+ * Fichier de fonctions associé au squelette prive/squelettes/contenu/smush_infos
+ * Ne fait qu'inclure inc/smush_verifier_binaires.php
+ */
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+include_spip('inc/smush_verifier_binaires');
+
+?>
--- smush_images/trunk/smush_pipelines.php
+++ smush_images/trunk/smush_pipelines.php
@@ -10,7 +10,11 @@
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 /**
- * Insertion dans le pipeline post_image_filtrer
+ * Insertion dans le pipeline post_image_filtrer (SPIP)
+ * 
+ * On passe toutes les images dans le filter image_smush sauf si :
+ * -* une constante _SMUSH_INTERDIRE_AUTO est définier
+ * -* la case eviter_traitement_auto de la config est cochée
  * 
  * @param string $flux
  * 		Le tag image (<img src...>) à réduire
@@ -18,7 +22,14 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
  * 		Le nouveau tag image
  */
 function smush_post_image_filtrer($flux) {
-	$flux = filtrer('image_smush',$flux);
+	if(!function_exists('lire_config'))
+		include_spip('inc/config');
+	if((!isset($GLOBALS['meta']['smush_casse']) || $GLOBALS['meta']['smush_casse'] != 'oui') && !defined('_SMUSH_INTERDIRE_AUTO') && (lire_config('smush/eviter_traitement_auto','off') != 'on')){
+		$flux = filtrer('image_smush',$flux);
+		spip_log('smush','test.'._LOG_ERREUR);
+	}else {
+		spip_log('On ne passe pas par smush','test.'._LOG_ERREUR);
+	}
 	return $flux;
 }
 
@@ -68,14 +79,27 @@ function image_smush($im) {
 		else if ($format == 'JPEG') {
 			$fsize = filesize($im);
 			$dest = $tmp.'.jpg';
-			if ($fsize < 10*1024) {
+			if ($fsize < 10*1024)
 				exec('jpegtran -copy none -optimize '.$im.' > '.$dest);
-			}
-			else {
+			else
 				exec('jpegtran -copy none -progressive '.$im.' > '.$dest);
-			}
 		}
 	}
 	return _image_ecrire_tag($image,array('src'=>$dest));
+}
+
+/**
+ * Pipeline taches_generales_cron de Smush (SPIP)
+ *
+ * Vérifie la présence à intervalle régulier des logiciels présents
+ * 
+ * @param array $taches_generales 
+ * 		Un array des tâches du cron de SPIP
+ * @return array $taches_generales
+ * 		L'array des taches complété
+ */
+function smush_taches_generales_cron($taches_generales){
+	$taches_generales['smush_taches_generales'] = 24*60*60;
+	return $taches_generales;
 }
 ?>
