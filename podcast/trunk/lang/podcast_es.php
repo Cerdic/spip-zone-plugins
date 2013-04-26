b3d09dbe059843aed843699153cd7c8c7e4b0406
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-26 03:38:15 +0000

    langues (podcast)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@72342 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ podcast/trunk/lang/podcast_es.php
@@ -0,0 +1,126 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/podcast?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cfg_titre_podcast' => 'Podcasts',
+
+	// D
+	'descriptif_flux_defaut' => '@site@ - Medios recientes',
+
+	// E
+	'explication_copyright' => 'Una frase explicitando los derechos de flujo de podcast',
+	'explication_description' => 'Descripción del flujo de podcast. Si este campo no se halla rellenado, se utilizará la descripción del sitio. En su defecto, se empleará una frase genérica.',
+	'explication_email_auteur' => 'Correo electrónico genérico del autor / propietario del podcast. Si se deja vacío este campo, será utilizado el correo electrónico del administrador del sitio.',
+	'explication_explicit' => 'Valor por defecto para el valor "explícito" en el flujo de podcast para cada elemento.',
+	'explication_itunes_categories_principales' => 'Estas categorías permiten clasificar su flujo en Apple Store. Adornan las etiquetas &gt;itunes:category&lt; de los flujos. Algunas de ellas pueden contener subcategorías.',
+	'explication_keywords' => 'Palabras claves asociadas al flujo. Limite a 12 y sepárelas por comas.',
+	'explication_nom_auteur' => 'Nombre genérico del autor y propietario del podcast. Útil para su referencia.',
+	'explication_podcast_auto' => 'Les documents ajoutés au site sont automatiquement intégrés au flux de podcast (Il reste possible de les y supprimer ou ajouter par la suite).', # NEW
+	'explication_podcast_types' => 'Quels types de flux sont mis à disposition des visiteurs.', # NEW
+	'explication_resume' => 'Court résumé du contenu du flux de podcast. Si ce champ n\'est pas rempli, le slogan du site sera utilisé. Si ce dernier n\'est pas disponible, une phrase par défaut prendra leur place.', # NEW
+	'explication_titre' => 'Si este campo se deja vacío, se utilizará el nombre del sitio.',
+
+	// I
+	'itunes_cat_alternative_health' => 'Medicina alternativa',
+	'itunes_cat_amateur' => 'Deportes amateur',
+	'itunes_cat_arts' => 'Artes',
+	'itunes_cat_automotive' => 'Automobile', # NEW
+	'itunes_cat_aviation' => 'Aviación',
+	'itunes_cat_buddhism' => 'Bouddhisme', # NEW
+	'itunes_cat_business' => 'Economía y empresa',
+	'itunes_cat_business_news' => 'Actualidad comercial',
+	'itunes_cat_business_shopping' => 'Shopping',
+	'itunes_cat_careers' => 'Empleo',
+	'itunes_cat_christianity' => 'Cristianismo',
+	'itunes_cat_college_high_school' => 'Deportes escolares y universitarios',
+	'itunes_cat_comedy' => 'Comedia',
+	'itunes_cat_design' => 'Diseño',
+	'itunes_cat_education' => 'Enseñanza',
+	'itunes_cat_education_technology' => 'Formato en línea',
+	'itunes_cat_fashion_beauty' => 'Moda y belleza',
+	'itunes_cat_fitness_nutrition' => 'Fitness y nutrición',
+	'itunes_cat_food' => 'Gastronomía',
+	'itunes_cat_gadgets' => 'Gadgets', # NEW
+	'itunes_cat_games_hobbies' => 'Juegos y aficiones',
+	'itunes_cat_government_organizations' => 'Gobierno y ONG',
+	'itunes_cat_health' => 'Salud',
+	'itunes_cat_higher_education' => 'Enseñanza superior',
+	'itunes_cat_hinduism' => 'Hinduismo',
+	'itunes_cat_history' => 'Historia',
+	'itunes_cat_hobbies' => 'Aficiones',
+	'itunes_cat_investing' => 'Investissements', # NEW
+	'itunes_cat_islam' => 'Islam',
+	'itunes_cat_judaism' => 'Judaísmo',
+	'itunes_cat_k_12' => 'Primaria y secundaria',
+	'itunes_cat_kids_family' => 'Hijos y familia',
+	'itunes_cat_language_courses' => 'Curso de idioma',
+	'itunes_cat_literature' => 'Literatura',
+	'itunes_cat_local' => 'Locales',
+	'itunes_cat_management_marketting' => 'Gestión y marketing',
+	'itunes_cat_medicine' => 'Medicina',
+	'itunes_cat_music' => 'Música',
+	'itunes_cat_national' => 'Nacionales',
+	'itunes_cat_natural_sciences' => 'Ciencias naturales',
+	'itunes_cat_news_politics' => 'Actualidad y política',
+	'itunes_cat_non_profit' => 'À but non lucratif', # NEW
+	'itunes_cat_other' => 'Otros',
+	'itunes_cat_other_games' => 'Otros juegos',
+	'itunes_cat_outdoor' => 'Plein air', # NEW
+	'itunes_cat_performing_arts' => 'Artes y espectáculo',
+	'itunes_cat_personnal_journals' => 'Diarios personales',
+	'itunes_cat_philosophy' => 'Filosofía',
+	'itunes_cat_places_travel' => 'Destinos y viajes',
+	'itunes_cat_podcasting' => 'Podcasts',
+	'itunes_cat_professional' => 'Deportes profesionales',
+	'itunes_cat_regional' => 'Regionales',
+	'itunes_cat_religion_spirituality' => 'Religión y espiritualidad',
+	'itunes_cat_science_medicine' => 'Ciencia y medicina',
+	'itunes_cat_self_help' => 'Autothérapie', # NEW
+	'itunes_cat_sexuality' => 'Sexualidad',
+	'itunes_cat_social_sciences' => 'Ciencias sociales',
+	'itunes_cat_society_culture' => 'Culture et société', # NEW
+	'itunes_cat_software_how_to' => 'Didacticiels', # NEW
+	'itunes_cat_spirituality' => 'Spiritualité', # NEW
+	'itunes_cat_sports_recreation' => 'Sport', # NEW
+	'itunes_cat_tech_news' => 'L\'actualité techno', # NEW
+	'itunes_cat_technology' => 'Technologie', # NEW
+	'itunes_cat_training' => 'Formation', # NEW
+	'itunes_cat_tv_film' => 'Séries TV et films', # NEW
+	'itunes_cat_video_games' => 'Jeux vidéos', # NEW
+	'itunes_cat_visual_arts' => 'Arts visuels', # NEW
+
+	// L
+	'label_contenu_explicit' => 'Contenu explicite', # NEW
+	'label_copyright' => 'Copyright du flux', # NEW
+	'label_dans_podcast' => 'Dans le flux de podcast', # NEW
+	'label_description' => 'Description du podcast', # NEW
+	'label_email_auteur' => 'Email de l\'auteur / propriétaire', # NEW
+	'label_explicit' => 'Contenu explicite', # NEW
+	'label_itunes_categories_principales' => 'Catégories principales iTunes', # NEW
+	'label_itunes_sous_categories' => 'Sous catégories de "@cat@"', # NEW
+	'label_keywords' => 'Mots clés', # NEW
+	'label_nom_auteur' => 'Nom de l\'auteur / propriétaire', # NEW
+	'label_podcast_auto' => 'Podcast automatique', # NEW
+	'label_podcast_types' => 'Types de flux mis à disposition', # NEW
+	'label_resume' => 'Résumé du flux de podcast', # NEW
+	'label_titre' => 'Titre du flux de podcast', # NEW
+	'legend_itunes' => 'Configuration spécifique pour iTunes', # NEW
+	'legend_mrss' => 'Configuration spécifique pour Media RSS', # NEW
+
+	// V
+	'valeur_channels' => 'Channels.com', # NEW
+	'valeur_clean' => 'clean', # NEW
+	'valeur_itunes' => 'iTunes', # NEW
+	'valeur_miro' => 'Miro', # NEW
+	'valeur_mrss' => 'Media RSS', # NEW
+	'valeur_no' => 'Non', # NEW
+	'valeur_yes' => 'Oui' # NEW
+);
+
+?>
