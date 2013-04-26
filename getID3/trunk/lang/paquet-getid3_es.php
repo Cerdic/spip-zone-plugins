1ed131a867d80677b37ad6d4442d9e83cf2ca384
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-26 03:38:05 +0000

    langues (paquet-getid3)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@72341 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- getID3/trunk/lang/paquet-getid3_es.php
+++ getID3/trunk/lang/paquet-getid3_es.php
@@ -8,9 +8,10 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// G
-	'getid3_description' => 'Leer y escribir las etiquetas audio en los documentos de tipo audio
-_ [->http://getid3.sourceforge.net/]', # MODIF
-	'getid3_slogan' => 'Leer y escribir las etiquetas audio' # MODIF
+	'getid3_description' => 'Este plugin permite recuperar los metadatos de archivos audio o vídeo y almacenarlos en la base de datos. Recupera asimismo las imágenes contenidas en los archivos audio.
+_ Puede también escribir algunas etiquetas en los metadatos de archivos sonoros.
+_ Para ello, utiliza la biblioteca [GetID3->http://getid3.sourceforge.net/] bajo licencia [GNU/GPL v2->http://www.getid3.org/source/license.txt] ',
+	'getid3_slogan' => 'Lectura de los metadatos audio y vídeos de documentos y escritura de las etiquetas audio'
 );
 
 ?>
