3de1ee345073ca54c5bbf8c1b5e2aceb4f2b6301
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-27 03:36:56 +0000

    langues (smush)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@72379 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- smush_images/trunk/lang/smush_fr.php
+++ smush_images/trunk/lang/smush_fr.php
@@ -1,5 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/smush_images/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
@@ -7,7 +8,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// E
 	'erreur_copie_locale' => 'Le fichier n\'a pu être copier localement (@src@).',
 	'erreur_pas_image' => 'Le fichier ne semble pas être une image de type png, jpg ou gif (@src@).',
-	
+
 	// I
 	'info_gain' => 'Le gain est de @gain@ octets soit @gain_octets@ (@percent@%).',
 	'info_gain_simple' => 'Le gain est de @gain@ octets (@percent@%).',
@@ -17,19 +18,19 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'info_logiciel_ok' => 'Le logiciel "@logiciel@" semble utilisable',
 	'info_logiciel_ok_tous' => 'L\'ensemble des logiciels nécessaires sont utilisables. Vous pouvez utiliser smush en local.',
 	'info_taille' => 'Taille : @taille@ (@octets@).',
-	
+
 	// L
 	'label_eviter_traitement_auto' => 'Éviter le traitement automatique de toutes les images générées par SPIP',
 	'label_url_test' => 'Url de votre image à tester',
-	
+
 	// S
 	'smush_conf_descriptif' => 'Le plugin Smush permet de réduire la taille des images d\'un site au minimum en utilisant des logiciels spécifiques sur le serveur. Cette page permet de le configurer.',
-	
+
 	// T
 	'titre_test_logiciels' => 'Test de présence des logiciels',
 	'titre_testeur_smush' => 'Tester sur une image',
 	'titre_version_originale' => 'Version originale',
-	'titre_version_smushed' => 'Version optimisée',
+	'titre_version_smushed' => 'Version optimisée'
 );
 
 ?>
