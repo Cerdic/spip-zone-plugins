c8ac163a6c5c144b2cc566ffc8e47bf03a214055
marcimat@rezo.net <marcimat@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-27 11:35:39 +0000

    Commit oublié : Éviter une double définition de la fonction todate() dans des cas extrêmement rares.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@72384 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- agenda/trunk/squelettes/calendrier_mini_event.json_fonctions.php
+++ agenda/trunk/squelettes/calendrier_mini_event.json_fonctions.php
@@ -1,16 +1,12 @@
 <?php
 /**
- * Plugin Agenda 4 pour Spip 3.0
- * Licence GPL 3
+ * Fichier de fonction du json du calendrier mini
  *
- * 2006-2011
- * Auteurs : cf paquet.xml
- */
+ * @package SPIP\Agenda\Fonctions
+**/
 
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
-include_spip('inc/json');
-
-function todate($t){return date('Y-m-d H:i:s',$t);}
+include_spip('calendrier_mini.json_fonctions');
 
 ?>
