c0db21170efe27037e2c9009eaa632932831487f
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-28 17:33:10 +0000

    version plus compacte et compat chargement async de jQuery par jQl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@72414 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- recommander/javascript/recommander.js
+++ recommander/javascript/recommander.js
@@ -1,10 +1,10 @@
-(function ($) {
-	$(document).ready(function(){
-		$("div#formulaire_recommander").hide().css("height","");
-		$("#recommander>h2, #recommander_bouton").click(function(){
-			$("div#formulaire_recommander:visible").slideUp("slow");
-			$("div#formulaire_recommander:hidden").slideDown("slow");
-			return false; // si jamais le bouton est un lien
-		});
+jQuery(function(){
+	(function ($) {
+	$("div#formulaire_recommander").hide().css("height","");
+	$("#recommander>h2, #recommander_bouton").click(function(){
+		$("div#formulaire_recommander:visible").slideUp("slow");
+		$("div#formulaire_recommander:hidden").slideDown("slow");
+		return false; // si jamais le bouton est un lien
 	});
-}(jQuery));
+	}(jQuery));
+});
