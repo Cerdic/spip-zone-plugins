b5d566f2cfd57d2eac25e2348a9c29b133a69809
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-30 03:36:29 +0000

    langues (paquet-signalement)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@72454 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ signalement/trunk/lang/paquet-signalement_es.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-signalement?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// S
+	'signalement_description' => 'El plugin Reseña permite señalar los contenidos ilícitos en un sitio.',
+	'signalement_nom' => 'Reseña',
+	'signalement_slogan' => 'Reseña de contenidos ilícitos'
+);
+
+?>
