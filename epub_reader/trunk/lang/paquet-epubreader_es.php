b78bfd5e31c1df4ccd2d91b06f78d5d7e9147ac1
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-04-30 03:36:19 +0000

    langues (paquet-epubreader)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@72451 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ epub_reader/trunk/lang/paquet-epubreader_es.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-epubreader?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// E
+	'epubreader_description' => 'Leer los archivos [ePUB->http://fr.wikipedia.org/wiki/EPUB_%28format%29] directamente en el sitio',
+	'epubreader_nom' => 'Lector ePUB',
+	'epubreader_slogan' => 'Leer los archivos ePUB'
+);
+
+?>
