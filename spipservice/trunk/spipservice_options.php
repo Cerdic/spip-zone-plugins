d61634a2559f3c9665185bfdf6ddc8df6fd0988d
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-05-01 13:09:57 +0000

    Suppression des define devenus inutiles. Il reste encore du code inutile comme spipservice_fonctions et pipelines mais je laisse car le plugin est en developpement.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@72499 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spipservice/trunk/spipservice_options.php
+++ spipservice/trunk/spipservice_options.php
@@ -20,9 +20,6 @@
  | <http://www.gnu.org/licenses/>                                                 |
  ________________________________________________________________________________*/
 
-define("_DIR_SPIPSERVICE_IMG_PACK", _DIR_PLUGIN_SPIPSERVICE."img_pack/");
-define("_DIR_SPIPSERVICE_PRIVE", _DIR_PLUGIN_SPIPSERVICE."prive/");
-
 define ('_DEBUG_SPIPSERVICE', false);
 
 ?>
