4a5b431dcecc85df05541c9a91a2efeec331b59b
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-05-02 03:36:42 +0000

    langues (paquet-smush)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@72514 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ smush_images/trunk/lang/paquet-smush_es.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-smush?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// S
+	'smush_description' => 'El plugin Smush permite reducir el tamaño de las imágenes de un sitio al mínimo utilizando softwares específicos en el servidor.',
+	'smush_slogan' => 'Reducir sus imágenes al mínimo.'
+);
+
+?>
