054aa9c90ef24e5bc99c613193e97769593128ba
tofulm@gmail.com <tofulm@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-05-03 12:45:12 +0000

    modification de la description du plugin
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@72556 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- orr/trunk/lang/paquet-orr_fr.php
+++ orr/trunk/lang/paquet-orr_fr.php
@@ -6,8 +6,13 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// O
-	'orr_description' => 'Adaptation du célèbre GRR pour spip',
+	'orr_description' => 'Adaptation de GRR pour spip  il est compatible dist ou zpip. Pour appeler la page : page=orr .
+    Si vous utilisez zpip, il faut renommer le fichier orr.html à la basse du plugin  en orr.html_old.
+    Attention, il est indispensable que pour chaque ressource d\'attribuer au moins :  
+    ->  1 mot clé  
+    ->  une autorisation',
 	'orr_nom' => 'ORR',
+	'orr_titre' => 'ORR',
 	'orr_slogan' => 'Organisation de réservations de ressources',
 );
 
