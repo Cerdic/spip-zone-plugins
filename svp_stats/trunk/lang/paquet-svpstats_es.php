bf5d47e245feff86c6842d3eb93886aec9f262d8
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-05-03 03:36:46 +0000

    langues (paquet-svpstats)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@72534 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ svp_stats/trunk/lang/paquet-svpstats_es.php
@@ -0,0 +1,17 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-svpstats?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// S
+	'svpstats_description' => 'Este plugin es un módulo opcional de SVP. 
+_ Permite adquirir, actualizar y restituir las estadísticas de uso de los plugin SPIP en la red. 
+Estas estadísticas son resultado del sitio <a href="http://stats.spip.org">stats.spip.org</a>.',
+	'svpstats_slogan' => 'Módulo SVP de gestión de estadísticas de uso de los plugins'
+);
+
+?>
