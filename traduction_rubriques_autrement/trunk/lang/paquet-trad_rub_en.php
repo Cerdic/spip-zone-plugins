cdbd1d85cbd666a9dc40efa582e0ab66fe657329
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-05-07 03:38:59 +0000

    langues (paquet-trad_rub)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@72605 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- traduction_rubriques_autrement/trunk/lang/paquet-trad_rub_en.php
+++ traduction_rubriques_autrement/trunk/lang/paquet-trad_rub_en.php
@@ -9,6 +9,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// T
 	'trad_rub_description' => 'This plugin for multilingual websites with [an area by language->http://contrib.spip.net/An-Example-of-a-Multilingual-Site] offers a similar approach in terms of navigation and management translations topics that plugin [Articles translations otherwise-> http://files.spip.org/spip-zone/traduction_articles_autrement.zip] whose installation is recommended. As spip does not natively translate sections, installing the plugin [Translation between sections->http://files.spip.org/spip-zone/tradrub.zip] is necessary.',
+	'trad_rub_nom' => 'Traductions de rubriques autrement', # NEW
 	'trad_rub_slogan' => 'Navigate between translated sections.'
 );
 
