161a614a645356e6348ae7c8110a5c2de14a8fd9
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-05-08 03:36:51 +0000

    langues (selection_objet)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@72631 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- selection_d_objets/trunk/lang/selection_objet_es.php
+++ selection_d_objets/trunk/lang/selection_objet_es.php
@@ -18,60 +18,60 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// E
 	'erreur_deja_selectionne' => 'Este objeto ya está seleccionado para esta lista',
 	'explication_choix_multi' => 'Puedes seleccionar a varios utilizando la tecla Mayus o ctrl (manzana o mayus para mac)',
-	'explication_selection_cible' => 'Si no se ha seleccionada a ningún objetos, todos serán disponibles',
+	'explication_selection_cible' => 'Si no se ha seleccionada a ningún objeto, todos serán disponibles',
 	'explication_selection_objets_cible' => 'Los Objetos Diana que pueden ser vinculados con "Objetos de Selección"',
-	'explication_selection_type_liens' => 'Puedes definir tipos de relación: pon uno por línea, seguido de una coma et de una descripción. La descripción puede ser una cadena de idioma', # MODIF
+	'explication_selection_type_liens' => 'Puedes definir tipos de relación: pon uno por línea, seguido de una coma et de una descripción. La descripción puede ser una cadena de idioma',
 
 	// I
 	'icone_creer_selection_objet' => 'Seleccionar un objet',
-	'icone_modifier_selection_objet' => 'Modificar este objeto seleccionado', # MODIF
-	'info_1_selection_objet' => 'Un objeto seleccionado', # MODIF
-	'info_aucun_selection_objet' => 'Ningún objeto seleccionado', # MODIF
-	'info_nb_selection_objets' => '@nb@ objetos seleccionados', # MODIF
+	'icone_modifier_selection_objet' => 'Modificar este Objeto Seleccionado',
+	'info_1_selection_objet' => 'Un Objeto Seleccionado',
+	'info_aucun_selection_objet' => 'Ningún Objeto Seleccionado',
+	'info_nb_selection_objets' => '@nb@ Objetos Seleccionados',
 
 	// L
 	'label_descriptif' => 'Descriptivo',
 	'label_id_objet' => 'Id objeto',
-	'label_id_objet_dest' => 'Id objeto destino',
+	'label_id_objet_dest' => 'Id Objeto Diana',
 	'label_objet' => 'Objeto',
-	'label_objet_dest' => 'Objeto destino', # MODIF
+	'label_objet_dest' => 'Objeto diana',
 	'label_ordre' => 'Orden',
 	'label_titre' => 'Título',
 	'label_url' => 'Url',
 
 	// O
 	'objet_destination_choisis' => 'Destinación/ones seleccionada(s):',
-	'objet_lie_a' => 'Lié à', # NEW
+	'objet_lie_a' => 'Vinculado a',
 	'objets_cible' => 'Los Objetos Diana',
 	'objets_selection' => 'Los Objetos Selección',
 
 	// R
 	'racine' => 'Raíz',
-	'retirer_lien_objet' => 'Retirar este objeto seleccionado',
-	'retirer_tous_liens_selection_objets' => 'Retirar todas las selecciones de objetos',
+	'retirer_lien_objet' => 'Eliminar este objeto seleccionado',
+	'retirer_tous_liens_selection_objets' => 'Eliminar todas las selecciones de objetos',
 	'rubriques_choisis' => 'Secciones seleccionadas:',
 
 	// S
 	'selection_cible' => 'Selección de objetos diana para',
 	'selection_objet' => 'Selección',
-	'selection_objet_cible_titre' => 'Selección de Objetos Diana', # MODIF
+	'selection_objet_cible_titre' => 'Selección de Objetos Diana',
 	'selection_objets_pour' => 'Selección objetos para:',
 	'selection_rubrique_objet' => 'Objetos que pueden ser seleccionados',
-	'selection_type_lien' => 'Tipo de vínculo',
-	'selection_type_liens' => 'Definición de tipos de enlace', # MODIF
+	'selection_type_lien' => 'Tipo de enlace',
+	'selection_type_liens' => 'Definición de tipos de enlace',
 	'selectione' => 'Seleccionado:',
 
 	// T
 	'texte_ajouter_objet' => 'Seleccionar un objeto',
-	'texte_changer_statut_objet' => 'Este objeto seleccionado está:', # MODIF
+	'texte_changer_statut_objet' => 'Este Objeto Seleccionado está:',
 	'texte_creer_asselection_objetcier_objet' => 'Crear y vincular un objet',
-	'titre_langue_objet' => 'Idioma de este objeto seccionado', # MODIF
-	'titre_logo_selection_objet' => 'Logo de este objeto seleccionado', # MODIF
-	'titre_selection_objet' => 'Objet Sélectionné', # NEW
-	'titre_selection_objets' => 'Objeto seleccionado', # MODIF
-	'titre_selection_objets_rubrique' => 'Objetos seleccionados de la sección', # MODIF
+	'titre_langue_objet' => 'Idioma de este Objeto Seleccionado',
+	'titre_logo_selection_objet' => 'Logo de este Objeto Seleccionado',
+	'titre_selection_objet' => 'Objeto Seleccionado',
+	'titre_selection_objets' => 'Objetos Seleccionados',
+	'titre_selection_objets_rubrique' => 'Objetos Seleccionados de la sección',
 	'type_liens_defaut' => 'tipos de enlace por defecto, pueden ser sobrescritos más abajo',
-	'type_liens_surcharge' => 'sobrescribir los tipos de vínculos por defecto' # MODIF
+	'type_liens_surcharge' => 'sobrescribir los tipos de enlace por defecto'
 );
 
 ?>
