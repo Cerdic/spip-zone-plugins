008a4806c25865d802c9dffc8fdfed79aa680c4d
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-05-09 09:41:06 +0000

    Entête de fichier et PHPdoc (pas grand chose donc)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@72661 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- notation/trunk/notation_fonctions.php
+++ notation/trunk/notation_fonctions.php
@@ -1,32 +1,39 @@
 <?php
 /**
-* Plugin Notation
-* par JEM (jean-marc.viglino@ign.fr) / b_b
-* 
-* Copyright (c) 2008
-* Logiciel libre distribue sous licence GNU/GPL.
-*  
-*  
-**/
+ * Plugin Notation
+ * par JEM (jean-marc.viglino@ign.fr) / b_b
+ * 
+ * Copyright (c) 2008
+ * Logiciel libre distribue sous licence GNU/GPL.
+ *  
+ *  
+ */
 
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 include_spip('inc/notation_autorisations');
 include_spip('inc/notation_balises');
 
-/** Filtre pour les tableaux :
-* transforme une liste (separee par de ,) en un tableau exploitable avec IN
-*/
-function notation_tab ($tab)
-{ $tab = explode(',',$tab);
-  return $tab;
+/** 
+ * Filtre pour les tableaux :
+ * transforme une liste separee par des virgules en un tableau exploitable avec IN
+ * 
+ * @param string $tab
+ * 		Liste séparée par des virgules
+ * @return array $tab
+ * 		Le tableau des éléments de la liste
+ */
+function notation_tab ($tab){
+	$tab = explode(',',$tab);
+	return $tab;
 }
 
 
-/* Gestion des notations dans les forums
-*  Supprime [notation]
-*  Transforme les [+] et [-] en images
-*/
+/** 
+ * Gestion des notations dans les forums
+ * Supprime [notation]
+ * Transforme les [+] et [-] en images
+ */
 
 function notation_critique($p){
 	$p = preg_replace('/\[notation\]/', '', $p);
@@ -34,7 +41,5 @@ function notation_critique($p){
 	$p = preg_replace('/\[-\]/', '<img src="'.find_in_path('img_pack/notation-moins.gif').'"> ', $p);
 	return $p;
 }
-
-
 
 ?>
