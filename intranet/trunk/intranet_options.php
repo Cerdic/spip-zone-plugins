5b5d11d94710df6dcd936f88e6f889825e965d90
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-05-09 22:31:36 +0000

    Ajout d'un pipeline intranet_pages_ok permettant de déclarer des fond qui ne passent pas par identification obligatoire
    On évite de prendre en compte les squelettes finissant par .js ou .css
    
    La bonne icone
    
    Version 0.1.1
    
    On passe en test
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@72702 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- intranet/trunk/intranet_options.php
+++ intranet/trunk/intranet_options.php
@@ -1,23 +1,19 @@
 <?php
 /*
- * Plugin En Travaux
- * (c) 2006-2009 Arnaud Ventre, Cedric Morin
+ * Plugin Intranet
+ * (c) 2013 kent1
  * Distribue sous licence GPL
  *
  */
 
-#var_dump($GLOBALS['meta']['entravaux_id_auteur']);
-
 /**
- * Autoriser a voir le site en travaux : par defaut tous les webmestre
+ * Autoriser a voir le site en Intranet : par defaut toute personne identifiée
  * @return mixed
  */
 function autoriser_intranet_dist(){ return (isset($GLOBALS['visiteur_session']['id_auteur']) && $GLOBALS['visiteur_session']['id_auteur'] > 0); }
 
 // dans le site public
 // si auteur pas autorise : placer sur un cache dedie
-// si auteur autorise, desactiver le cache :
-// il voit le site, mais pas de cache car il travaille dessus !
 if (!test_espace_prive()){
 	include_spip('inc/autoriser');
 	if (!autoriser('intranet'))
@@ -25,8 +21,7 @@ if (!test_espace_prive()){
 }
 
 /**
- * Pipeline styliser pour rerouter tous les fonds vers en_travaux
- * sauf si l'auteur connecte est celui qui a active le plugin
+ * Pipeline styliser pour rerouter tous les fonds vers intranet
  *
  * @param array $flux
  * @return array
@@ -35,13 +30,15 @@ function intranet_styliser($flux){
 	include_spip('inc/autoriser');
 	// les pages exceptions
 	$pages_ok = array('robots.txt','spip_pass','favicon.ico','informer_auteur');
+	$pages_ok = pipeline('intranet_pages_ok',$pages_ok);
 	//spip_log($flux['args'],'test.'._LOG_ERREUR);
 	if (!autoriser('intranet')
 		AND !in_array($flux['args']['fond'],$pages_ok)
 		AND !in_array($flux['args']['contexte'][_SPIP_PAGE],$pages_ok)
 		// et on laisse passer modeles et formulaires,
 		// qui ne peuvent etre inclus ou appeles que legitimement
-		AND strpos($flux['args']['fond'],'/')===false){
+		AND strpos($flux['args']['fond'],'/')===false
+		AND !in_array(substr($flux['args']['fond'],-3),array('.js','.css'))){
 		$fond = trouver_fond('inclure/intranet','',true);
 		$flux['data'] = $fond['fond'];
 	}
