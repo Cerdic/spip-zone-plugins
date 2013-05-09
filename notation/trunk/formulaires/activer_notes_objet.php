73a5e39836133f0bbec86dcdb85f0d7a7012cccb
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-05-09 09:33:59 +0000

    Entêtes de fichiers (pas grand chose donc)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@72657 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- notation/trunk/formulaires/activer_notes_objet.php
+++ notation/trunk/formulaires/activer_notes_objet.php
@@ -1,5 +1,12 @@
 <?php
-
+/**
+ * Plugin Notation
+ * par JEM (jean-marc.viglino@ign.fr) / b_b / Matthieu Marcillaud
+ *
+ * Copyright (c) 2008
+ * Logiciel libre distribue sous licence GNU/GPL.
+ *
+ */
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 // Recuperer le reglage des forums publics de l'article x
--- notation/trunk/formulaires/notation.php
+++ notation/trunk/formulaires/notation.php
@@ -1,12 +1,12 @@
 <?php
 /**
-* Plugin Notation
-* par JEM (jean-marc.viglino@ign.fr) / b_b / Matthieu Marcillaud
-*
-* Copyright (c) 2008
-* Logiciel libre distribue sous licence GNU/GPL.
-*
-**/
+ * Plugin Notation
+ * par JEM (jean-marc.viglino@ign.fr) / b_b / Matthieu Marcillaud
+ *
+ * Copyright (c) 2008
+ * Logiciel libre distribue sous licence GNU/GPL.
+ *
+ */
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 include_spip('inc/notation');
