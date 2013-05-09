beff11e7182dbfee4e93e7d1470fc4b6a8e7b26c
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-05-09 09:42:43 +0000

    Entête de fichier et PHPdoc (pas grand chose donc)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@72662 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- notation/trunk/inc/articles_accepter_notes.php
+++ notation/trunk/inc/articles_accepter_notes.php
@@ -1,11 +1,21 @@
 <?php
+/**
+ * Plugin Notation
+ * par JEM (jean-marc.viglino@ign.fr) / b_b / Matthieu Marcillaud
+ *
+ * Copyright (c) 2008
+ * Logiciel libre distribue sous licence GNU/GPL.
+ *
+ */
 
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 /**
- * Moderation des notes specifique a un article
- * @param int $id_objet identifiant de l'article
- * @return string  : "non", "oui"
+ * Modération des notes spécifique a un article
+ * @param int $id_objet 
+ * 		identifiant de l'article
+ * @return string
+ * 		"non", "oui"
  */
 function inc_articles_accepter_notes_dist($id_objet) {
 	$accepter_note = $GLOBALS['meta']["notations_publics"];
@@ -13,7 +23,6 @@ function inc_articles_accepter_notes_dist($id_objet) {
 		"id_article = ". intval($id_objet)
 	));
 	if ($art_accepter_note and in_array($art_accepter_note,array('oui','non'))) {
-		spip_log("l'article $id_objet accepte les notes : $art_accepter_note",'notation');
 		$accepter_note = $art_accepter_note;
 	}
 	return substr($accepter_note, 0, 3);
--- notation/trunk/inc/notation.php
+++ notation/trunk/inc/notation.php
@@ -1,18 +1,19 @@
 <?php
 /**
-* Plugin Notation 
-* par JEM (jean-marc.viglino@ign.fr) / b_b / Matthieu Marcillaud
-* 
-* Copyright (c) 2008
-* Logiciel libre distribue sous licence GNU/GPL.
-*  
-**/
+ * Plugin Notation
+ * par JEM (jean-marc.viglino@ign.fr) / b_b / Matthieu Marcillaud
+ *
+ * Copyright (c) 2008
+ * Logiciel libre distribue sous licence GNU/GPL.
+ *
+ */
 if (!defined("_ECRIRE_INC_VERSION")) return;
-
 
 /**
  * Retourne la configuration de la ponderation (defaut : 30)
- * @return int : valeur de ponderation
+ * 
+ * @return int $ponderation
+ * 		Valeur de ponderation
  */
 function notation_get_ponderation(){
 	static $ponderation="";
@@ -30,7 +31,8 @@ function notation_get_ponderation(){
  * Nombre d'etoile a afficher en fonction de la configuration
  * du plugin. Varie de 1 a 10. Defaut 5.
  * 
- * @return int : nombre d'etoiles a afficher
+ * @return int $nb 
+ * 		Nombre d'etoiles a afficher
  */ 
 function notation_get_nb_notes(){
 	static $nb = "";
@@ -48,9 +50,12 @@ function notation_get_nb_notes(){
  * utilise uniquement pour l'affichage dans la page de configuration
  * (vrai calcul en SQL dans action/editer_notation)
  * 
- * @param float $note : note moyenne obtenue
- * @param int $nb : nombre de votes 
- * @return int : note ponderee en fonction de la configuration du plugin
+ * @param float $note
+ * 		Note moyenne obtenue
+ * @param int $nb 
+ * 		Nombre de votes 
+ * @return int $note_ponderee 
+ * 		Note ponderee en fonction de la configuration du plugin
  */
 function notation_ponderee($note, $nb){
    $note_ponderee = round($note*(1-exp(-5*$nb/notation_get_ponderation())),2);
