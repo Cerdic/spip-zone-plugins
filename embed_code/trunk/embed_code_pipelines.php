11273b9e31dc6a3c33e85b00383a79608bfccf4d
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-05-09 22:43:31 +0000

    Version 0.3.0
    Utilisation du pipeline intranet_pages_ok pour permettre la visualisation des codes d'embed même si le site est en mode intranet (Plugin éponyme)
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@72703 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ embed_code/trunk/embed_code_pipelines.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Utilisation de pipelines
+ * 
+ * @package SPIP\Embed_code\Pipelines
+ */
+
+// Sécurité
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+
+/**
+ * Insertion dans le pipeline intranet_pages_ok (Plugin Intranet)
+ * 
+ * @pipeline intranet_pages_ok
+ * @param array $pages_ok
+ *     Tableau des pages accessibles même en intranet
+ * @return array
+ *     Tableau des pages accessibles même en intranet complété
+ */
+function embed_code_intranet_pages_ok($pages_ok){
+	$pages_ok[] = 'embed_code';
+	return $pages_ok; 
+}
+?>	
