242072de2ccb877bd2b0ba6602204763fce56eb5
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-05-12 17:32:00 +0000

    Ajout d'items obsolètes et de commentaires
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@72764 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- langonet/tests/lang/verification_fr.php
+++ langonet/tests/lang/verification_fr.php
@@ -7,19 +7,25 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// Pour la vérification "definition"
+	// -- fichiers xml
 	'defini_xml_1' => 'item défini utilisé dans xml #1',
-
 	'defini_paquet_1' => 'item défini utilisé dans paquet.xml #1',
-
 	'defini_plugin_1' => 'item défini utilisé dans plugin.xml #1',
-
+	// -- fichiers yaml
 	'defini_yaml_1' => 'item défini utilisé dans yaml #1',
-
+	// -- fichiers html
 	'defini_html_1' => 'item défini utilisé dans html #1',
 	'defini_html_2' => 'item défini utilisé dans html #2',
 	'defini_html_partiel_3' => 'item défini utilisé dans html dans un contexte partiel #3',
 	'defini_html_partiel_4' => 'item défini utilisé dans html dans un contexte partiel #4',
 	'defini_html_singulier' => '@nb@ item défini utilisé dans html pour le singulier',
 	'defini_html_pluriel' => '@nb@ items définis utilisé dans html pour le pluriel',
+	// -- fichiers php
+
+	// Pour la vérification "utilisation"
+	'obsolete_1' => 'item défini mais non utilisé #1',
+	'obsolete_2' => 'item défini mais non utilisé #2',
+	'obsolete_3' => 'item défini mais non utilisé #3',
+
 );
 ?>
