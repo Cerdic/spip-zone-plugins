08b5800c5420db6718ee1b5af3fd1dc0be24b4dc
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-05-12 16:08:30 +0000

    Séparation des items de langue en deux modules : verification_fr pour les opérations de vérification et recherche_fr pour les opérations de recherche. Correction des fichiers xml et yawl en conséquence.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@72756 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- langonet/tests/lang/recherche_fr.php
+++ langonet/tests/lang/recherche_fr.php
@@ -18,23 +18,11 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'bouton_generer_contient2' => 'alors G&Eacute;n&eacute;rer moi ça rapide !',
 	'bouton_generer_contient3' => 'ben va falloir génÉrer ça rapide !',
 
-	// Pour la vérification "definition"
-	'defini_xml_1' => 'item défini dans xml #1',
-	'defini_paquet_1' => 'item défini dans paquet.xml #1',
-	'defini_plugin_1' => 'item défini dans plugin.xml #1',
-	'defini_yaml_1' => 'item défini dans yaml #1',
-
 	'test' => 'TEST : Cet item de langue sert pour la recherche de raccourci et est égal à test.',
 	'test_item_1_variable' => 'TEST : Cet item de langue est bien défini dans le fichier de langue, mais est utilisé sous forme "complexe" dans les fichiers du répertoire scanné.',
 	'test_item_2_variable' => 'TEST : Cet item de langue est bien défini dans le fichier de langue, mais est utilisé sous forme "complexe" dans les fichiers du répertoire scanné.',
 	'test_item_non_utilise_1' => 'TEST : Cet item de langue est bien défini dans le fichier de langue (), mais n\'est pas utilisé dans les fichiers du répertoire scanné ().',
 	'test_item_non_utilise_2' => 'TEST : Cet item de langue est bien défini dans le fichier de langue (), mais n\'est pas utilisé dans les fichiers du répertoire scanné ().',
-
-	'titre_charte' => 'titre de la charte', # correspond à une utilisation dans paquet.xml ou plugin.xml
-	'titre_icone' => 'icone du titre', # correspond à un item obsolète (le bon étant titre_icones)
-
-	'utilisation_html' => 'Item utilisé correctement dans du HTML', # utilisation <:module:item:> simple
-	'utilisation_val_1' => 'Item utilisé dans une balise VAL simple', # utilisation #VAL{item}|_T
 
 	// Z
 	'z_test' => 'TEST : Cet item de langue sert pour la recherche de raccourci et contient test.',
---
+++ langonet/tests/lang/verification_fr.php
@@ -0,0 +1,15 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// Pour la vérification "definition"
+	'defini_xml_1' => 'item défini dans xml #1',
+	'defini_paquet_1' => 'item défini dans paquet.xml #1',
+	'defini_plugin_1' => 'item défini dans plugin.xml #1',
+	'defini_yaml_1' => 'item défini dans yaml #1',
+);
+?>
