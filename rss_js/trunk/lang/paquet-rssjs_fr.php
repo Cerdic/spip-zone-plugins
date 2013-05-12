c13bc820ed6e9790ec9135d6948e5eb04db91fd3
dwojylac@free.fr <dwojylac@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-05-12 17:16:29 +0000

    On passe rss-js en version 2 pour spip 3
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@72761 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ rss_js/trunk/lang/paquet-rssjs_fr.php
@@ -0,0 +1,13 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// R
+	'rssjs_description' => 'Affichage dans l\'espace public de flux Rss grâce à l\'API Google Feed, à partir de sites syndiqués ou de l\'url du flux',
+	'rssjs_slogan' => 'Affichage de flux Rss grâce à l\'API Google Feed',
+);
+?>
