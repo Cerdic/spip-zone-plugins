d80271bbdd7e63cbc7fc83dd520be06017cfa855
dwojylac@free.fr <dwojylac@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-05-12 17:32:11 +0000

    on passe le plugin en version 1.0 (pour spip 2) on ajoute un nouveau modèle
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@72765 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- rss_js/branches/spip_20_21/js/rssjs.js
+++ rss_js/branches/spip_20_21/js/rssjs.js
@@ -26,3 +26,39 @@ function initialize_rssjs(flux, entrees, recept, mode) {
 		}
 	});
 }
+
+function initialize_rssjs_complet(flux, entrees, recept, mode) {
+	/* spip remplace & par &amp; et google feed api n'aime pas */
+	var flux = flux.replace(/&amp;/g, '&');
+	var feed = new google.feeds.Feed(flux);
+	feed.setNumEntries(entrees);
+	feed.load(function(result) {
+		if (!result.error) {
+			var container = $('#'+recept);
+			
+			var element = '<h3 class="spip"><a rel="external" href="'+result.feed.link+'">'+result.feed.title+'</a></h3>';
+			var element = element+'<p>'+result.feed.description+'</p>';
+			container.append(element);
+			
+			var container =$('#dl_'+recept);
+			for (var i = 0; i < result.feed.entries.length; i++) {
+				var entry = result.feed.entries[i];
+				/* dt par defaut */
+				var element = '<dt><a rel="external nofollow" class="spip_out" href="'+entry.link+'">'+entry.title+'</a></dt>';
+
+				container.append(element);
+				/* dd */
+				if (mode != 'no_content'){
+					if (mode == 'snippet')
+						var descr = entry.contentSnippet;
+					else
+						var descr = entry.content;
+						
+					var element = '<dd>'+descr+'</dd>'
+					var element = element + '<dd>'+entry.publishedDate+'</dd>';
+					container.append(element);
+				}
+			}
+		}
+	});
+}
