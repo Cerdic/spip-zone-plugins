9bc4a50558e7df34a31f45b7e25f387baa83a24d
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-05-12 16:57:29 +0000

    Mise au point des tests pour les HTML.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@72760 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- langonet/tests/lang/verification_fr.php
+++ langonet/tests/lang/verification_fr.php
@@ -7,9 +7,19 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// Pour la vérification "definition"
-	'defini_xml_1' => 'item défini dans xml #1',
-	'defini_paquet_1' => 'item défini dans paquet.xml #1',
-	'defini_plugin_1' => 'item défini dans plugin.xml #1',
-	'defini_yaml_1' => 'item défini dans yaml #1',
+	'defini_xml_1' => 'item défini utilisé dans xml #1',
+
+	'defini_paquet_1' => 'item défini utilisé dans paquet.xml #1',
+
+	'defini_plugin_1' => 'item défini utilisé dans plugin.xml #1',
+
+	'defini_yaml_1' => 'item défini utilisé dans yaml #1',
+
+	'defini_html_1' => 'item défini utilisé dans html #1',
+	'defini_html_2' => 'item défini utilisé dans html #2',
+	'defini_html_partiel_3' => 'item défini utilisé dans html dans un contexte partiel #3',
+	'defini_html_partiel_4' => 'item défini utilisé dans html dans un contexte partiel #4',
+	'defini_html_singulier' => '@nb@ item défini utilisé dans html pour le singulier',
+	'defini_html_pluriel' => '@nb@ items définis utilisé dans html pour le pluriel',
 );
 ?>
