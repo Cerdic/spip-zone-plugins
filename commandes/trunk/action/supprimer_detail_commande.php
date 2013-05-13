5991b3113dc25c7ed25b8c31a25a29be45b94ae4
tcharlss@hotmail.fr <tcharlss@hotmail.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-05-13 17:25:16 +0000

    Ajout d'une action pour supprimer un détail (un article) d'une commande. On rajoute l'autorisation qui va avec : il faut être admin ou l'auteur de la commande. Tant qu'on y est, un peu de php doc.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@72799 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ commandes/trunk/action/supprimer_detail_commande.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Action de suppression d'un détail d'une commande
+ *
+ * @plugin     Commandes
+ * @copyright  2013
+ * @author     Ateliers CYM, Matthieu Marcillaud, Les Développements Durables
+ * @licence    GPL 3
+ * @package    SPIP\Commandes\Action
+ */
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+/**
+ * Supprime un détail (un article) d'une commande, dans la table spip_commandes_details
+ * si on est autorisé (cf. commandes_autorisations.php)
+ * ex: #URL_ACTION_AUTEUR{supprimer_detail_commande,#ID_COMMANDE-#ID_COMMANDES_DETAIL,#SELF}
+ * 
+ * @param $arg string arguments séparés par un charactère non alphanumérique
+ *                    id_commande:          identifiant de la commande
+ *                    id_commandes_detail:  identifiant du détail
+ * @return unknown_type
+**/
+function action_supprimer_detail_commande($arg=null) {
+
+	if ($arg==null) {
+		$securiser_action = charger_fonction('securiser_action', 'inc');
+		$arg = $securiser_action();
+	}
+	list($id_commande, $id_detail) = preg_split('/[\W]/', $arg);
+
+	if (
+		$id_commande = intval($id_commande)
+		and $id_detail = intval($id_detail)
+		and include_spip('inc/autoriser')
+		and autoriser('supprimerdetail','commande',$id_commande)
+	) {
+		sql_delete("spip_commandes_details", "id_commande=".$id_commande . " AND id_commandes_detail=".$id_detail);
+	}
+
+}
+
+
+?>
--- commandes/trunk/commandes_autorisations.php
+++ commandes/trunk/commandes_autorisations.php
@@ -1,13 +1,34 @@
 <?php
+/**
+ * Définit les autorisations du plugin Commandes
+ *
+ * @plugin     Commandes
+ * @copyright  2013
+ * @author     Ateliers CYM, Matthieu Marcillaud, Les Développements Durables
+ * @licence    GPL 3
+ * @package    SPIP\Commandes\Autorisations
+ */
 
 // Sécurité
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
-// Pour le pipeline
+/**
+ * Fonction d'appel pour le pipeline
+ * @pipeline autoriser */
 function commandes_autoriser($flux){ return $flux; }
 
-// Qui peut passer une commande : un client (auteur+contact)
-function autoriser_commander_dist($faire, $quoi, $id, $qui, $options){
+/**
+ * Autorisation à passer une commande
+ * - un client (auteur+contact)
+ *
+ * @param  string $faire Action demandée
+ * @param  string $type  Type d'objet sur lequel appliquer l'action
+ * @param  int    $id    Identifiant de la commande
+ * @param  array  $qui   Description de l'auteur demandant l'autorisation
+ * @param  array  $opts  Options de cette autorisation
+ * @return bool          true s'il a le droit, false sinon
+**/ 
+function autoriser_commander_dist($faire, $quoi, $id, $qui, $ops){
 	if (
 		$id_auteur = $qui['id_auteur'] > 0
 		and $contact = sql_getfetsel('id_contact', 'spip_contacts_liens', 'objet = '.sql_quote('auteur').' and id_objet = '.sql_quote($id_auteur))
@@ -17,14 +38,41 @@ function autoriser_commander_dist($faire, $quoi, $id, $qui, $options){
 		return false;
 }
 
-// on ne laisse pas les redacteurs voir les commandes
-// ni voir les commandes dans la recherche spip
-// Sauf pour celles dont il est l'auteur
-function autoriser_commande_dist($faire, $quoi, $id, $qui, $options) {
+/**
+ * Autorisation à voir(?) une commande
+ * - l'auteur de la commande
+ * - admin (mais pas restreint)
+ *
+ * @param  string $faire Action demandée
+ * @param  string $type  Type d'objet sur lequel appliquer l'action
+ * @param  int    $id    Identifiant de la commande
+ * @param  array  $qui   Description de l'auteur demandant l'autorisation
+ * @param  array  $opts  Options de cette autorisation
+ * @return bool          true s'il a le droit, false sinon
+**/ 
+function autoriser_commande_dist($faire, $quoi, $id, $qui, $opts) {
 	return
 		$qui['id_auteur'] == sql_getfetsel('id_auteur', 'spip_commandes', 'id_commande = '.sql_quote($id)) OR 
 			( $qui['statut'] == '0minirezo'
-			  AND !$qui['restreint'] );
+			 AND !$qui['restreint'] );
+}
+
+/**
+ * Autorisation à supprimer un détail d'une commande
+ * - l'auteur de la commande 
+ * - admin (mais pas restreint)
+ *
+ * @param  string $faire Action demandée
+ * @param  string $type  Type d'objet sur lequel appliquer l'action
+ * @param  int    $id    Identifiant de la commande
+ * @param  array  $qui   Description de l'auteur demandant l'autorisation
+ * @param  array  $opts  Options de cette autorisation
+ * @return bool          true s'il a le droit, false sinon
+**/ 
+function autoriser_commande_supprimerdetail_dist($faire, $type, $id, $qui, $opts) {
+	return
+		$qui['id_auteur'] == sql_getfetsel('id_auteur', table_objet_sql('commande'), "id_commande=".intval($id))
+		OR ( $qui['statut'] == '0minirezo' AND !$qui['restreint'] );
 }
 
 ?>
