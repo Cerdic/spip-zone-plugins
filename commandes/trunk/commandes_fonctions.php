49148368b40606389e2afc9ec7aad41b9adb69cb
tcharlss@hotmail.fr <tcharlss@hotmail.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-05-13 17:50:52 +0000

    du php doc
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@72801 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- commandes/trunk/commandes_fonctions.php
+++ commandes/trunk/commandes_fonctions.php
@@ -1,9 +1,27 @@
 <?php
+/**
+ * Fonctions du plugin Commandes
+ *
+ * @plugin     Commandes
+ * @copyright  2013
+ * @author     Ateliers CYM, Matthieu Marcillaud, Les Développements Durables
+ * @licence    GPL 3
+ * @package    SPIP\Commandes\Fonctions
+ */
 
 // Sécurité
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
-// Une fonction qui retourne les différents statuts possibles pour une commande ou le nom d'un statut précis
+/**
+ * Une fonction qui retourne les différents statuts possibles pour une commande ou le nom d'un statut précis
+ *
+ * @param bool|string $statut
+ *		vide ou false pour retourner un tableau
+ *		nom d'un statut précis pour retourner sa chaîne de langue
+ * @return array|string
+ *		array: tableau associatif des statuts possibles et leurs chaînes de langue
+ *		string: chaîne de langue d'un statut
+**/
 function commandes_lister_statuts($statut=false){
 	$statuts =  array(
 		'encours' => _T('commandes:statut_encours'),
@@ -23,7 +41,12 @@ function commandes_lister_statuts($statut=false){
 		return $statuts;
 }
 
-// Qui est le premier webmester ?
+/** 
+ * Fonction qui retourne l'identifiant du premier webmester
+ *
+ * @return int $id_auteur
+ *		identifiant du premier webmaster
+**/
 function commandes_id_premier_webmestre()
 {
 	include_spip('base/abstract_sql');
