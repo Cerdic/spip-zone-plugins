3587bb58e79aec171916996a889566bd515f121a
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-05-15 20:45:55 +0000

    Les deux derniers verbes, et tant qu'à faire pour ListRecords on inclut GetRecord pour chaque élément.
    En format Dublin, minimal. Faudrait quand même avoir un élément qui donne le lien vers la ressource complète... (l'article quoi).
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@72835 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- oai/trunk/lang/oai_fr.php
+++ oai/trunk/lang/oai_fr.php
@@ -14,6 +14,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'erreur_badVerb_absent' => 'La requête doit comporter l’argument «verb».',
 	'erreur_badVerb_inconnu' => 'Le verbe «@verbe@» ne fait pas parti des valeurs autorisées.',
 	'erreur_cannotDisseminateFormat' => 'Le format de méta-données «@format@» n’existe pas.',
+	'erreur_idDoesNotExist' => 'L’identifiant demandé ne correspond à aucun enregistrement.',
 	'erreur_noMetadataFormats' => 'Aucun format de méta-données n’a été trouvé pour ce dépot.',
 	'erreur_noRecordsMatch' => 'Aucun enregistrement ne correspond à ces critères.',
 	'erreur_noSetHierarchy' => 'Ce dépôt n’a aucune catégorie.',
