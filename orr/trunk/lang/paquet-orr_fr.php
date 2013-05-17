7756ca0161e19a38058e9cf38f889af77044b53f
tofulm@gmail.com <tofulm@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-05-17 07:03:36 +0000

    corrections orthographiques et numerotation de la documentation
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@72852 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- orr/trunk/lang/paquet-orr_fr.php
+++ orr/trunk/lang/paquet-orr_fr.php
@@ -7,8 +7,8 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// O
 	'orr_description' => 'Adaptation de GRR pour spip  il est compatible dist ou zpip. Pour appeler la page : page=orr .
-    Si vous utilisez zpip, il faut renommer le fichier orr.html à la basse du plugin  en orr.html_old.
-    Attention, il est indispensable que pour chaque ressource d\'attribuer au moins :  
+    Si vous utilisez zpip, il faut renommer le fichier orr.html à la base du plugin en orr.html_old.
+    Attention, il est indispensable d\'attribuer pour chaque ressource au moins :  
     ->  1 mot clé  
     ->  une autorisation',
 	'orr_nom' => 'ORR',
