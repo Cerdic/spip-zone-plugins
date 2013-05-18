1099466beb0fbe74156503d4a1745b5e73007f9c
tcharlss@hotmail.fr <tcharlss@hotmail.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-05-18 15:13:00 +0000

    Notification sur changement de statut :
    L'envoi de notification ne fonctionnait qu'en faisant appel à l'action "instituer_commande". 
    Du coup changer de statut dans l'espace privé ne provoquait pas l'envoi de notification, et d'une façon générale, la fonction "instituer_objet" n'était pas prise en compte.
    On met donc ça en place avec la pipeline post_edition. 
    
    + broutille : la pipeline affiche_gauche était chargée 2 fois !...
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@72862 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- commandes/trunk/commandes_pipelines.php
+++ commandes/trunk/commandes_pipelines.php
@@ -78,15 +78,15 @@ function commandes_accueil_encours($flux) {
 }
 
 
-/*
+/**
  * Mettre à jour les dates de paiement ou d'envoi
- * lors de la modification du statut d'une commande
+ * en amont de la modification du statut d'une commande
  *
  * @param array $flux
  *	$flux['args'][x] = arguments (action, table, id_objet etc.)
  *	$flux['data'][x] = champs modifiés (statut, date etc.)
  * @return array $flux
- */
+**/
 function commandes_pre_edition($flux){
 
 	if (
@@ -105,6 +105,43 @@ function commandes_pre_edition($flux){
 				$flux['data']['date_envoi'] = $date;
 				break;
 		}
+	}
+
+	return($flux);
+}
+
+
+/**
+ * Envoyer les notifications 
+ * en aval de la modification du statut d'une commande
+ *
+ * @param array $flux
+ *	$flux['args'][x] = arguments (table, id_objet, action, statut_ancien, date_ancienne, id_parent_ancien)
+ *	$flux['data'][x] = champs modifiés (statut, date_paiement etc.)
+ * @return array $flux
+**/
+function commandes_post_edition($flux){
+
+	if (
+		($action=$flux['args']['action']) == 'instituer' // action instituer
+		and ($table=$flux['args']['table']) == table_objet_sql('commande') // on institue une commande
+		and ($statut_ancien=$flux['args']['statut_ancien']) != ($statut=$flux['data']['statut']) // le statut a été modifié
+		and $id_commande = $flux['args']['id_objet'] // on a bien un identifiant pour la commande
+		and include_spip('inc/config')
+		and $config = lire_config('commandes')
+		and ($config['activer']) // les notifications sont activées
+		and (in_array($statut, $config['quand'])) // le nouveau statut est valide pour envoyer une notification
+		and ($notifications = charger_fonction('notifications', 'inc', true)) // le plugin notifications est actif
+	) {
+		// Déterminer l'expediteur
+		$options = array();
+		if( $config['expediteur'] != "facteur" )
+			$options['expediteur'] = $config['expediteur_'.$config['expediteur']];
+
+		// Envoyer au vendeur et au client
+		$notifications('commande_vendeur', $id_commande, $options);
+		if($config['client'])
+			$notifications('commande_client', $id_commande, $options);
 	}
 
 	return($flux);
