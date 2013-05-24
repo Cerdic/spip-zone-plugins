91311adf721ce9bd1d05f31ab1cba6a6f8450ed7
marcimat@rezo.net <marcimat@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-05-24 12:10:43 +0000

    Il y avait quelques coquilles pour faire afficher le calendrier des activités, et en plus on simplifie la quête puisque fullcalendar n'a pas besoin d'avoir 1 entrée par jour si on lui fournit les bornes de dates.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@72981 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- dayfill/trunk/action/quete_calendrier_dayfill.php
+++ dayfill/trunk/action/quete_calendrier_dayfill.php
@@ -31,16 +31,18 @@ function action_quete_calendrier_dayfill_dist(){
 	$securiser_action();
 
 	$start = _request('start');
-	$end = _request('end');
+	$end   = _request('end');
+
 	include_spip('action/quete_calendrier_prive');
 	include_spip('inc/quete_calendrier');
 
 	// recuperer la liste des evenements au format ics
 	$start = date('Y-m-d H:i:s',$start);
-	$end = date('Y-m-d H:i:s',$end);
+	$end   = date('Y-m-d H:i:s',$end);
 	$limites = array(sql_quote($start),sql_quote($end));
 
-	$duree = quete_calendrier_interval_projets_activites($limites);
+	$evt = array();
+	$duree = quete_calendrier_interval_projets_activites($limites, $evt);
 	$evt = convert_fullcalendar_quete_calendrier_interval_rv($duree);
 
 	// format json
@@ -59,53 +61,35 @@ function action_quete_calendrier_dayfill_dist(){
  * @param array $evenements
  *     Tableau des événements déjà présents qui sera complété par la fonction.
  *     Format : `$evenements[$amj][] = Tableau de description ICS`
+ * @return array
+ *     De la forme : `$evenements[date][id] = Tableau des données ICS`
 **/
 function quete_calendrier_interval_projets_activites($limites, &$evenements) {
 	list($avant, $apres) = $limites;
 
-	$result=sql_select(
+	$result = sql_select(
 		'id_projets_activite, id_projet, descriptif, date_debut, date_fin',
 		'spip_projets_activites',
 		"date_fin >= $avant AND date_debut <= $apres", '', "date_debut");
 
-	while($row=sql_fetch($result)){
+	while ($row=sql_fetch($result)) {
 		$amj = date_anneemoisjour($row['date_debut']);
 		$id = $row['id_projets_activite'];
 		$date_debut = $row["date_debut"];
 		$date_fin   = $row["date_fin"];
 
-		if (autoriser('voir','projetsactivite', $id)) {
-
-			// cf quete_calendrier_interval_rv()
-			$jour_avant  = substr($avant, 9,2);
-			$mois_avant  = substr($avant, 6,2);
-			$annee_avant = substr($avant, 1,4);
-			$jour_apres  = substr($apres, 9,2);
-			$mois_apres  = substr($apres, 6,2);
-			$annee_apres = substr($apres, 1,4);
-			$ical_apres = date_anneemoisjour("$annee_apres-$mois_apres-".sprintf("%02d",$jour_apres));
-
-			// Calcul pour les semaines a cheval sur deux mois
-			$j = 0;
-			$amj = date_anneemoisjour("$annee_avant-$mois_avant-".sprintf("%02d", $j+($jour_avant)));
-
-			while ($amj <= $ical_apres) {
-				// Ne pas prendre la fin a minuit sur jour precedent
-				if (!($amj == date_anneemoisjour($date_fin) AND preg_match(",00:00:00,", $date_fin))) {
-					$evenements[$amj][$id] = array(
-						'CATEGORIES' => 'calendrier-couleur' . (($row['id_projet']%14)+1),
-						'SUMMARY' => $row['titre'],
-						'URL' => generer_url_ecrire_objet('projets_activite',$id),
-						'DTSTART' => date_ical($date_debut),
-						'DTEND'   => date_ical($date_fin),
-					);
-
-					$j ++;
-					$ladate = date("Y-m-d",mktime (1,1,1,$mois_avant, ($j + $jour_avant), $annee_avant));
-
-					$amj = date_anneemoisjour($ladate);
-				}
-			}
+		if (autoriser('voir','projets_activite', $id)) {
+			// fullcalendar se débrouillera très bien de ça...
+			$evenements[$amj][$id] = array(
+				'CATEGORIES' => 'calendrier-couleur' . (($row['id_projet']%14)+1),
+				'SUMMARY' => $row['descriptif'],
+				'URL' => generer_url_ecrire('projets_activite', "id_projets_activite=$id"),
+				'DTSTART' => date_ical($date_debut),
+				'DTEND'   => date_ical($date_fin),
+				'DESCRIPTION' => '',
+			);
 		}
 	}
+
+	return $evenements;
 }
