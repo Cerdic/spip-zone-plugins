ca7a9261a222e3a60ceb08f3d9b08b28e4f6aadd
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-05-27 11:57:39 +0000

    Passage en paquet.xml + plugin.xml
    Le fichier de langue du paquet.xml
    
    Compatible SPIP 3.0 et version 1.1.0 du coup
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@73023 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ openinviter/lang/paquet-openinviter_fr.php
@@ -0,0 +1,19 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-openinviter
+// Langue: fr
+// Date: 25-05-2013 17:15:27
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// O
+	'openinviter_description' => 'Intégration de la librairie OpenInviter avec l\'API du plugin Importateur de contacts.',
+	'openinviter_slogan' => 'Intégration de la librairie OpenInviter.',
+);
+?>
