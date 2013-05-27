c13843363e618d8eff7327abaff1709411f50063
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-05-27 11:55:10 +0000

    On passe ne paquet.xml + plugin.xml + fichier de langue du paquet
    Déplacement de base/parrainage_installation.php en parrainage_administrations.php
    
    
    Compatible à priori complètement en SPIP 3.0
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@73021 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ parrainage/trunk/lang/paquet-parrainage_fr.php
@@ -0,0 +1,19 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-parrainage
+// Langue: fr
+// Date: 25-05-2013 20:31:01
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// P
+	'parrainage_description' => 'Permet d\'importer des contacts puis d\'envoyer en masse des invitations à rejoindre le site. Si des personnes s\'inscrivent, alors un lien est conservé avec celui qui les a invité (le parrain).',
+	'parrainage_slogan' => 'Proposer aux utilisateurs un outil de parrainage.',
+);
+?>
