54bddaaa7c18f761763655f3fbdc529015ab4ace
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-05-28 21:43:55 +0000

    PHPdoc
    Réparer l'insertion de la saisie du code d'invitation dans le formulaire d'inscription
    
    Eviter que l'invitation soit considéré comme obligatoire (dans la vérification du formulaire d'inscription)
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@73043 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- parrainage/trunk/parrainage_pipelines.php
+++ parrainage/trunk/parrainage_pipelines.php
@@ -8,6 +8,16 @@ function parrainage_taches_generales_cron($taches){
 	return $taches;
 }
 
+/**
+ * Insertion dans le pipeline formulaire_charger (SPIP)
+ * 
+ * On charge le code d'activation si présent dans l'URL
+ * 
+ * @param array $flux
+ * 		Le contexte du pipeline
+ * @return array $flux
+ * 		Le contexte du pipeline modifié
+ */
 function parrainage_formulaire_charger($flux){
 	if ($flux['args']['form'] == 'inscription'){
 		if ($code_invitation = _request('invitation'))
@@ -19,6 +29,16 @@ function parrainage_formulaire_charger($flux){
 	return $flux;
 }
 
+/**
+ * Insertion dans le pipeline recuperer_fond (SPIP)
+ * 
+ * Sur le formulaire d'inscription, on ajoute la saisie du code d'invitation
+ * 
+ * @param array $flux
+ * 		Le contexte du pipeline
+ * @return array $flux
+ * 		Le contexte du pipeline modifié
+ */
 function parrainage_recuperer_fond($flux){
 	if ($flux['args']['fond'] == 'formulaires/inscription'){
 		include_spip('inc/config');
@@ -34,31 +54,54 @@ function parrainage_recuperer_fond($flux){
 				'obligatoire' => $obligatoire
 			)
 		);
-		$flux['data']['texte'] = preg_replace("%<li class='saisie_mail_inscription.*?</li>%is", '$0'.$saisie, $flux['data']['texte']);
+		$flux['data']['texte'] = preg_replace("%<li class='.*?saisie_mail_inscription.*?</li>%is", '$0'.$saisie, $flux['data']['texte']);
 	}
 	
 	return $flux;
 }
 
+/**
+ * Insertion dans le pipeline formulaire_verifier
+ * 
+ * Vérifications sur le code d'inscription :
+ * -* Si le code d'invitation est obligatoire, refuser l'inscription si pas de code d'invitation
+ * ou code d'invitation invalide;
+ * -* Si code d'invitation fourni, refuser l'inscription si l'email ne correspond pas 
+ * 
+ * @param array $flux
+ * 		Le contexte du pipeline
+ * @return array $flux
+ * 		Le contexte du pipeline modifié
+ */
 function parrainage_formulaire_verifier($flux){
 	if ($flux['args']['form'] == 'inscription'){
 		include_spip('inc/config');
 		$code_invitation = _request('code_invitation');
+
 		// Si l'invitation est obligatoire
-		if (lire_config('parrainage/invitation_obligatoire','') and !$code_invitation){
+		if ((lire_config('parrainage/invitation_obligatoire','') == 'oui') and !$code_invitation){
 			$flux['data']['code_invitation'] = _T('parrainage:erreur_invitation_obligatoire');
 		}
-		// Si le code ne correspond pas à l'email donné
-		elseif (!$email = sql_getfetsel('email', 'spip_filleuls', 'code_invitation = '.sql_quote($code_invitation))
-			or $email != _request('mail_inscription')
+		// Si le code d'invitation est dans l'URL mais ne correspond pas à l'email donné
+		elseif ($code_invitation && (!$email = sql_getfetsel('email', 'spip_filleuls', 'code_invitation = '.sql_quote($code_invitation))
+			or $email != _request('mail_inscription'))
 		){
 			$flux['data']['code_invitation'] = _T('parrainage:erreur_invitation_invalide');
 		}
 	}
-	
 	return $flux;
 }
 
+/**
+ * Insertion dans le pipeline formulaire_traiter (SPIP)
+ * 
+ * Au traitement du formulaire d'inscription, on update le filleul lié au code d'inscription
+ * 
+ * @param array $flux
+ * 		Le contexte du pipeline
+ * @return array $flux
+ * 		Le contexte du pipeline modifié
+ */
 function parrainage_formulaire_traiter($flux){
 	if ($flux['args']['form'] == 'inscription' and $code_invitation = _request('code_invitation')){
 		// On doit d'abord chercher l'id_auteur qui vient d'être créé
