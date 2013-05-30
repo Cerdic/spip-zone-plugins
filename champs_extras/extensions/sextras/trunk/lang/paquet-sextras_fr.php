407c5afa9460c9b0319d4b8b055d102f88bd1744
bruno.caillard@free.fr <bruno.caillard@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-05-30 21:16:27 +0000

    Création
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@73082 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ champs_extras/extensions/sextras/trunk/lang/paquet-sextras_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// S
+	'sextras_description' => 'Ce plugin permet de mettre à jour la définition des champs extra à partir des définitions prises dans un site de référence choisi.',
+	'sextras_nom' => 'Champs Extras (Synchronisation)',
+	'sextras_slogan' => 'Synchroniser les champs extras à partir d\'un site de référence',
+);
+
+?>
