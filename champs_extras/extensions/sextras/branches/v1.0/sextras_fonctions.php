d337a0e7208196beffb48b00540d31ea3af76ea8
bruno.caillard@free.fr <bruno.caillard@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-05-30 21:18:38 +0000

    Création v1.0.0
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@73095 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ champs_extras/extensions/sextras/branches/v1.0/sextras_fonctions.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Fonctions utiles au plugin Champs Extras (Synchronisation)
+ *
+ * @plugin     Champs Extras (Synchronisation)
+ * @copyright  2013
+ * @author     Bruno Caillard
+ * @licence    GNU/GPL
+ * @package    SPIP\Sextras\Fonctions
+ */
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+
+/*
+ * Un fichier de fonctions permet de définir des éléments
+ * systématiquement chargés lors du calcul des squelettes.
+ *
+ * Il peut par exemple définir des filtres, critères, balises, …
+ * 
+ */
+
+?>
