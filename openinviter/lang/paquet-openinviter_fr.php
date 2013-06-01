9e245f6a16d594868057edd4b5778b3dce708c7a
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-06-01 03:37:47 +0000

    langues (paquet-openinviter)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@73148 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- openinviter/lang/paquet-openinviter_fr.php
+++ openinviter/lang/paquet-openinviter_fr.php
@@ -1,19 +1,13 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-openinviter
-// Langue: fr
-// Date: 25-05-2013 17:15:27
-// Items: 2
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/openinviter/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// O
+	// O
 	'openinviter_description' => 'Intégration de la librairie OpenInviter avec l\'API du plugin Importateur de contacts.',
-	'openinviter_slogan' => 'Intégration de la librairie OpenInviter.',
+	'openinviter_slogan' => 'Intégration de la librairie OpenInviter.'
 );
+
 ?>
