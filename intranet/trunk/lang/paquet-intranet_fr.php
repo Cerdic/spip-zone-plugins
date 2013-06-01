caf708e590b1a7399ff6be0b465bc59469b17c91
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-06-01 03:37:38 +0000

    langues (paquet-intranet)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@73146 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- intranet/trunk/lang/paquet-intranet_fr.php
+++ intranet/trunk/lang/paquet-intranet_fr.php
@@ -1,14 +1,16 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/en_travaux/trunk/lang/
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/intranet/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// E
 	'entravaux_description' => 'N\'autoriser que les utilisateurs loggués à visiter le site.',
-	'intranet_nom' => 'Intranet/Extranet',
-	'entravaux_slogan' => 'N\'autoriser que les utilisateurs loggués à visiter le site'
+	'entravaux_slogan' => 'N\'autoriser que les utilisateurs loggués à visiter le site',
+
+	// I
+	'intranet_nom' => 'Intranet/Extranet'
 );
 
 ?>
