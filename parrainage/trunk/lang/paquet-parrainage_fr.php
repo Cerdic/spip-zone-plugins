9226dc0a110e5e94093abcd3f4dbf682ca23f6da
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-06-01 03:37:55 +0000

    langues (paquet-parrainage)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@73149 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- parrainage/trunk/lang/paquet-parrainage_fr.php
+++ parrainage/trunk/lang/paquet-parrainage_fr.php
@@ -1,19 +1,13 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-parrainage
-// Langue: fr
-// Date: 25-05-2013 20:31:01
-// Items: 2
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/parrainage/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// P
+	// P
 	'parrainage_description' => 'Permet d\'importer des contacts puis d\'envoyer en masse des invitations à rejoindre le site. Si des personnes s\'inscrivent, alors un lien est conservé avec celui qui les a invité (le parrain).',
-	'parrainage_slogan' => 'Proposer aux utilisateurs un outil de parrainage.',
+	'parrainage_slogan' => 'Proposer aux utilisateurs un outil de parrainage.'
 );
+
 ?>
