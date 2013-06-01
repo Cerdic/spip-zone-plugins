d4752a5eb0db730d99fa849db47b8925c379a84e
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-06-01 03:37:37 +0000

    langues (intranet)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@73145 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- intranet/trunk/lang/intranet_fr.php
+++ intranet/trunk/lang/intranet_fr.php
@@ -1,25 +1,24 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/en_travaux/trunk/lang/
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/intranet/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-	
 	// C
 	'cfg_titre_intranet' => 'Intranet/Extranet',
-	
+
 	// E
 	'explications_message' => 'Message personnalisé à afficher au dessus du formulaire d\'identification.',
 	'explications_pages_intranet' => 'Ajouter certaines pages SPIP spécifiques accessibles sans être connectés (à séparer par des virgules ",").',
-	
+
 	// I
 	'info_intranet' => 'Identification obligatoire',
 	'info_intranet_texte' => 'Ce site n\'est accessible qu\'aux personnes identifiées.',
-	
+
 	// L
 	'label_message' => 'Personnalisation du message',
-	'label_pages_intranet' => 'Pages accessibles',
+	'label_pages_intranet' => 'Pages accessibles'
 );
 
 ?>
