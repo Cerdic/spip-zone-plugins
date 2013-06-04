864a8d2c7397351a0bb1eacdf8c38c1455189918
elaine.scott89@gmail.com <elaine.scott89@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-06-04 03:38:02 +0000

    langues (parrainage)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@73232 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- parrainage/trunk/lang/parrainage_en.php
+++ parrainage/trunk/lang/parrainage_en.php
@@ -16,9 +16,9 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'configuration_titre' => 'Sponsorship configuration',
 
 	// E
-	'erreur_aucun_contact' => 'Vous n\'avez importé aucun contact.', # NEW
+	'erreur_aucun_contact' => 'You have not imported any contacts',
 	'erreur_aucun_filleul' => 'You have not selected any sponsored person.',
-	'erreur_inscription_desactivee' => 'L\'inscription au site est désactivée. Vous ne pouvez donc parrainer personne.', # NEW
+	'erreur_inscription_desactivee' => 'Registration to the site has been disactivated. You cannot sponsor anyone.',
 	'erreur_invitation_invalide' => 'Your invitation does not match the email address provided.',
 	'erreur_invitation_obligatoire' => 'You need an invitation to register on the site.',
 
@@ -36,8 +36,8 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'invitation_url' => 'Go to the following address to register :',
 
 	// M
-	'message_insciption_fermee' => 'Attention, l\'inscription au site est fermée. Vous devez l\'activer pour utiliser ce plugin', # NEW
-	'message_ouvrir_visiteur' => 'Si vous souhaitez ouvrir l\'inscription au site à de nouveaux visiteurs, vous pouvez activer cette option ci-dessous (la configuration du plugin "Parrainage" oblige le nouvel inscrit à bénéficier d\'un code d\'invitation) : ', # NEW
+	'message_insciption_fermee' => 'Warning, registration to the site is closed. You must activate it in order to use this plugin.',
+	'message_ouvrir_visiteur' => 'If you would like to open registration to new visitors, you can activate this option below (configuring the plugin "Sponsoship"sends new subscribers an invitation code) :',
 
 	// P
 	'parrainage_inviter' => 'Send invitation',
@@ -47,17 +47,17 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'parrainage_message_label' => 'Your message',
 	'parrainage_message_ok_pluriel' => '@nombre@ invitations have been programmed, they are now being send.',
 	'parrainage_message_ok_singulier' => 'An invitation has been programmed, it is now being send.',
-	'parrainage_supprime_nb' => '@nb@ contacts supprimés', # NEW
-	'parrainage_supprime_un' => '@nb@ contact supprimé', # NEW
-	'parrainage_supprimer_filleul' => 'Delete this sponsored person', # MODIF
+	'parrainage_supprime_nb' => '@nb@ deleted contacts',
+	'parrainage_supprime_un' => '@nb@ deleted contact',
+	'parrainage_supprimer_filleul' => 'Delete this contact',
 	'parrainage_supprimer_filleul_confirmation' => 'Are you sure you want to delete @nom@ from your contacts ?',
-	'parrainage_supprimer_filleuls' => 'Supprimer les contacts sélectionnés', # NEW
-	'parrainage_supprimer_filleuls_confirmation' => 'Êtes-vous sûr de vouloir supprimer les contacts sélectionnés de vos contacts ?', # NEW
+	'parrainage_supprimer_filleuls' => 'Delete the selected contacts',
+	'parrainage_supprimer_filleuls_confirmation' => 'Are you sure you want to delete the selected contacts?',
 	'plugin_nom' => 'Sponsorship',
 
 	// S
-	'selectionner_rien' => 'Tout désélectionner', # NEW
-	'selectionner_tout' => 'Tout sélectionner', # NEW
+	'selectionner_rien' => 'Deselect all',
+	'selectionner_tout' => 'Select all',
 	'statut_contact' => 'Contact',
 	'statut_contact_explication' => 'This person is in your address book. You can send an invitation to join the site.',
 	'statut_deja_inscrit' => 'Already registered',
