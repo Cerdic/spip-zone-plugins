7d6318ba98ff789a5772877f9ee5047c24c7263f
tcharlss@hotmail.fr <tcharlss@hotmail.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-06-05 11:16:23 +0000

    paquet.xml : incrémentation de z + donner un numéro de schema, sinon SVP n'est pas content (message "installation réussie" à chaque passage sur la page de gestion des plugins)
    administrations.php : phpdoc
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@73268 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- exclure_secteur/trunk/exclure_sect_administrations.php
+++ exclure_secteur/trunk/exclure_sect_administrations.php
@@ -1,29 +1,57 @@
 <?php
+/**
+ * Fichier gérant l'installation et désinstallation du plugin Exclure secteur
+ *
+ * @plugin     Exclure secteur
+ * @copyright  2013
+ * @author     Maïeul Rouquette
+ * @licence    GPL 3
+ * @package    SPIP\exclure_sect\Installation
+ */
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
-function exclure_sect_upgrade($nom_meta_version_base, $version_cible){
-   include_spip('base/upgrade');
-   $maj=array();
-   $maj['create'] = array(array('exclure_sect_conf'));
+/**
+ * Fonction d'installation et de mise à jour du plugin
+ *
+ * @param string $nom_meta_base_version
+ *     Nom de la meta informant de la version du schéma de données du plugin installé dans SPIP
+ * @param string $version_cible
+ *     Version du schéma de données dans ce plugin (déclaré dans paquet.xml)
+ * @return void
+**/
+function exclure_sect_upgrade($nom_meta_base_version, $version_cible){
+	$maj = array();
+	$maj['create'] = array(
+		array('exclure_sect_conf')
+	);
 
+	include_spip('base/upgrade');
+	maj_plugin($nom_meta_base_version, $version_cible, $maj);
+}
 
-    maj_plugin($nom_meta_version_base, $version_cible, $maj);
-  }
-
+/**
+ * Fonction d'installation : écriture config
+**/
 function exclure_sect_conf(){
 	include_spip('inc/config');
 	if (!lire_config('secteur/exclure_sect')){
 		ecrire_config('secteur/exclure_sect',array());
 	}
 }
-function exclure_sect_vider_tables($nom_meta_version_base){
+
+/**
+ * Fonction de désinstallation du plugin
+ *
+ * @param string $nom_meta_base_version
+ *     Nom de la meta informant de la version du schéma de données du plugin installé dans SPIP
+ * @return void
+**/
+function exclure_sect_vider_tables($nom_meta_base_version){
 	if (lire_config('secteur')){
 		effacer_config('secteur');
-    }
-    effacer_meta($nom_meta_version_base);
-	return true;
+	}
+	effacer_meta($nom_meta_base_version);
 }
-
 
 ?>
