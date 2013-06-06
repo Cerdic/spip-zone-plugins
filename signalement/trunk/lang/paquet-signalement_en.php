f732cf6201f3f9379fd568c1557dd03934405683
benlarcher@gmail.com <benlarcher@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-06-06 03:37:15 +0000

    langues (paquet-signalement)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@73301 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ signalement/trunk/lang/paquet-signalement_en.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-signalement?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// S
+	'signalement_description' => 'The plugin Notification can report illegal content on the site.',
+	'signalement_nom' => 'Notification',
+	'signalement_slogan' => 'Notify illegal contents'
+);
+
+?>
