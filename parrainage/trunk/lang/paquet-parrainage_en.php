a50b11eca2925fd4e21840b09403114bc18ef96f
benlarcher@gmail.com <benlarcher@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-06-06 03:37:10 +0000

    langues (paquet-parrainage)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@73297 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ parrainage/trunk/lang/paquet-parrainage_en.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-parrainage?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// P
+	'parrainage_description' => 'Allows you to import contacts and send mass invitations to join the site. If people sign up, then a link is maintained with the one who invited them (the sponsor).',
+	'parrainage_slogan' => 'Provide users with a tool for sponsorship.'
+);
+
+?>
