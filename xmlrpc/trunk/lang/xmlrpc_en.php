42fb8e37a496988d29f958d2d0552bf1b3d97971
benlarcher@gmail.com <benlarcher@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-06-06 03:37:25 +0000

    langues (xmlrpc)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@73311 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ xmlrpc/trunk/lang/xmlrpc_en.php
@@ -0,0 +1,29 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/xmlrpc?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cfg_description' => 'Configuration of the xml-rpc server',
+
+	// E
+	'erreur_arguments_obligatoires' => 'Error: The following arguments are required "@arguments@"',
+	'erreur_identifiant' => 'You must provide the digital object identifier (@objet@)',
+	'erreur_impossible_lire_objet' => 'Error: can not read the object "@objet@" #@id_objet@',
+	'erreur_lecture' => 'Error reading the object (@objet@ #@id_objet@)',
+	'erreur_mauvaise_identification' => 'Wrong identification (login/password)',
+	'erreur_objet_inexistant' => 'The requested object does not exist (@objet@ #@id_objet@)',
+	'erreur_xmlrpc_desactive' => 'xml-rpc server is deactivated',
+
+	// L
+	'label_api_preferee' => 'favorite edition API',
+	'label_desactiver_rsd' => 'Deactivate RSD',
+	'label_desactiver_rsd_long' => 'Disables the use of the RSD file in the pages header',
+	'label_ferme' => 'Deactivate the server'
+);
+
+?>
