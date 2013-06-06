3be33c7cd6614237b4a0920f829cc76bf5098c45
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-06-06 14:45:15 +0000

    eviter de publier les forums de l'espace privé
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@73325 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- moderation_moderee/trunk/moderation_pipelines.php
+++ moderation_moderee/trunk/moderation_pipelines.php
@@ -3,7 +3,7 @@ include_spip('inc/config');
 include_spip('inc/session');
 function moderation_pre_edition($flux){
 	if ($flux['args']['table']=='spip_forum'
-		AND $flux['args']['action']=='instituer'){
+		AND $flux['args']['action']=='instituer' AND $flux["data"]["statut"]!="prive"){
 			// Pour publier direct les auteurs configurés par modération modérée
 				global $visiteur_session;
 				if ($visiteur_session){
