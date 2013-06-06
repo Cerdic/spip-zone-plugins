20e261853f6552a1af40dff6fdf0ee5f6a87ee50
benlarcher@gmail.com <benlarcher@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-06-06 03:37:17 +0000

    langues (paquet-smush)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@73303 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ smush_images/trunk/lang/paquet-smush_en.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-smush?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// S
+	'smush_description' => 'The plugin Smush can reduce the image size to the minimum using specific softwares on the server.',
+	'smush_slogan' => 'Reducing images to nearly nothing.'
+);
+
+?>
