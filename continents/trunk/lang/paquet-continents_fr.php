ca06372a07827a1e7a0b5d67b48bec24364fd5e3
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-06-07 03:37:03 +0000

    langues (paquet-continents)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@73336 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- continents/trunk/lang/paquet-continents_fr.php
+++ continents/trunk/lang/paquet-continents_fr.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/continents/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
@@ -8,7 +8,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// C
 	'continents_description' => 'Lie les pays du plugin Pays ISO 3166-1 (pays) à ces continents correspondants.',
 	'continents_nom' => 'Continents',
-	'continents_slogan' => 'A chaque pays son continent !',
+	'continents_slogan' => 'A chaque pays son continent !'
 );
 
 ?>
