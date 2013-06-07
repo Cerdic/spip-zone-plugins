e13f3ac90c4c060322a9dbc4461061fe96c7fd98
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-06-07 03:36:51 +0000

    langues (auteur_evenement)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@73333 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- auteur_evenement/trunk/lang/auteur_evenement_fr.php
+++ auteur_evenement/trunk/lang/auteur_evenement_fr.php
@@ -1,16 +1,15 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/auteur_evenement/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
- 
+
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
 	'auteur_evenement_titre' => 'Auteurs Événement',
-	
-    // I
-    'info_evenements_auteur' => 'Les événements de cet auteur',
 
+	// I
+	'info_evenements_auteur' => 'Les événements de cet auteur'
 );
 
 ?>
