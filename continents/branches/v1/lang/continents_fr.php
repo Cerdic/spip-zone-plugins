dd3dec59507357efd99e200bbfe406596e3ac2c5
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-06-07 05:36:08 +0000

    salvatore est état à stable
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@73340 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- continents/branches/v1/lang/continents_fr.php
+++ continents/branches/v1/lang/continents_fr.php
@@ -1,13 +1,13 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/continents/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// C
 	'continent_titre' => 'Continent',
-	'continents_titre' => 'Continents',	
+	'continents_titre' => 'Continents'
 );
 
 ?>
