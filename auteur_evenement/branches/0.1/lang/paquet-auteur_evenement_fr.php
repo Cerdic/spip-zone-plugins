cc6aeb98e4a875a563c830210736e313893cc388
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-06-07 05:38:54 +0000

    salvatore est état à dev
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@73341 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- auteur_evenement/branches/0.1/lang/paquet-auteur_evenement_fr.php
+++ auteur_evenement/branches/0.1/lang/paquet-auteur_evenement_fr.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/auteur_evenement/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
@@ -8,7 +8,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// A
 	'auteur_evenement_description' => 'Permet de lier des auteurs à un évenement',
 	'auteur_evenement_nom' => 'Auteurs Événement',
-	'auteur_evenement_slogan' => 'Lier des auteurs à un événement',
+	'auteur_evenement_slogan' => 'Lier des auteurs à un événement'
 );
 
 ?>
