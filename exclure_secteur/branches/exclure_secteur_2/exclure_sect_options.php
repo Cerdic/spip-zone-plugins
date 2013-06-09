dcae795708ca91b94dafb3e7844fc00305d717dc
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-06-09 20:38:47 +0000

    inutile d'avoir une branche qui soit en réalité un calque du trunk. Cf les commits de tcharlss qui n'ont servi à rien pour les non utilisateurs de SVN
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@73428 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- exclure_secteur/branches/exclure_secteur_2/exclure_sect_options.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if (!function_exists('critere_tout_voir_dist')){
-        function critere_tout_voir_dist($idb, &$boucles, $crit) {   
-            $boucle = &$boucles[$idb];
-            $boucle->modificateur['tout_voir'] = true;
-    }
-}
-?>
