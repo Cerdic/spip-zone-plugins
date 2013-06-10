4b875c9c95988e83c090e5f3d6a244a5625bbd00
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-06-10 07:49:44 +0000

    langues, changement du nom de la branche
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@73441 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ auteur_evenement/branches/v1/lang/auteur_evenement_de.php
@@ -0,0 +1,17 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/auteur_evenement?lang_cible=de
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'auteur_evenement_titre' => 'Event Autoren',
+
+	// I
+	'info_evenements_auteur' => 'Alle Events dieses Autors'
+);
+
+?>
---
+++ auteur_evenement/branches/v1/lang/paquet-auteur_evenement_en.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-auteur_evenement?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'auteur_evenement_description' => 'Allows to associate authors with events',
+	'auteur_evenement_nom' => 'Event Authors',
+	'auteur_evenement_slogan' => 'Associate an event with an author'
+);
+
+?>
