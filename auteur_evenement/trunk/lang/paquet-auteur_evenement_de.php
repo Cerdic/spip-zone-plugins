de4f76b64b08846714ebca8459040bf668d46810
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-06-10 03:37:17 +0000

    langues (paquet-auteur_evenement)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@73437 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ auteur_evenement/trunk/lang/paquet-auteur_evenement_de.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-auteur_evenement?lang_cible=de
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'auteur_evenement_description' => 'Ermöglicht es, Autoren mit Events zu verknüpen.',
+	'auteur_evenement_nom' => 'Event Autoren',
+	'auteur_evenement_slogan' => 'Autoren mit Events verknüpfen'
+);
+
+?>
---
+++ auteur_evenement/trunk/lang/paquet-auteur_evenement_es.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-auteur_evenement?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'auteur_evenement_description' => 'Permite asociar un autor a un evento',
+	'auteur_evenement_nom' => 'Autores para eventos',
+	'auteur_evenement_slogan' => 'Asociar autores a eventos'
+);
+
+?>
