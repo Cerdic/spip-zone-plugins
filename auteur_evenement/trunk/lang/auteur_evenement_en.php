d7eef070bcb92b27e945fc29ed4f2c866f1c46be
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-06-10 03:37:15 +0000

    langues (auteur_evenement)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@73436 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ auteur_evenement/trunk/lang/auteur_evenement_en.php
@@ -0,0 +1,17 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/auteur_evenement?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'auteur_evenement_titre' => 'Event Authors',
+
+	// I
+	'info_evenements_auteur' => 'Events by this author'
+);
+
+?>
