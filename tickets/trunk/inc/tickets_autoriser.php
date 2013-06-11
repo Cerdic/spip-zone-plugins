4f0f6144590f8df0930c9d81ee4d6d80294dfa81
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-06-11 12:01:03 +0000

    Premier fix pour http://contrib.spip.net/Tickets-suivi-de-bugs#forum468346
    Définir les autorisations de boutons pour que :
    - tout utilisateur dans le privé puisse accéder à la liste des tickets
    - les utilisateurs qui peuvent créer un ticket aient accès au bouton de création rapide de ticket
    
    Version 2.6.2
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@73497 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- tickets/trunk/inc/tickets_autoriser.php
+++ tickets/trunk/inc/tickets_autoriser.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Tickets
- * Licence GPL (c) 2008-2012
+ * Licence GPL (c) 2008-2013
  *
  */
  
@@ -76,6 +76,9 @@ function definir_autorisations_tickets($action,$utiliser_defaut=true){
  * @return boolean true/false : true si autorisé, false sinon
  */
 function autoriser_ticket_ecrire_dist($faire, $type, $id, $qui, $opt){
+	if(($qui['webmestre'] == 'oui') && $qui['statut'] == '0minirezo')
+		return true;
+	
 	$autorise = false;
 	$utiliser_defaut = true;
 
@@ -139,6 +142,7 @@ function autoriser_ticket_creer_dist($faire, $type, $id, $qui, $opt){
  * Autorisation d'assignation des tickets
  * (défini qui peu assigner les tickets)
  * 
+ * - Les webmestres
  * @param string $faire : l'action à faire
  * @param string $type : le type d'objet sur lequel porte l'action
  * @param int $id : l'identifiant numérique de l'objet
@@ -147,6 +151,9 @@ function autoriser_ticket_creer_dist($faire, $type, $id, $qui, $opt){
  * @return boolean true/false : true si autorisé, false sinon
  */
 function autoriser_ticket_assigner_dist($faire, $type, $id, $qui, $opt){
+	if(($qui['webmestre'] == 'oui') && $qui['statut'] == '0minirezo')
+		return true;
+	
 	$autorise = false;
 	$utiliser_defaut = true;
 
@@ -191,64 +198,9 @@ function autoriser_ticket_assigner_dist($faire, $type, $id, $qui, $opt){
 }
 
 /**
- * Autorisation de notification des tickets
- * (défini qui doit être notifié)
- * 
- * @param string $faire : l'action à faire
- * @param string $type : le type d'objet sur lequel porte l'action
- * @param int $id : l'identifiant numérique de l'objet
- * @param array $qui : les éléments de session de l'utilisateur en cours
- * @param array $opt : les options
- * @return boolean true/false : true si autorisé, false sinon
- */
-function autoriser_ticket_commenter_dist($faire, $type, $id, $qui, $opt){
-	$autorise = false;
-	$utiliser_defaut = true;
-
-	if(autoriser_ticket_modifier_dist($faire, $type, $id, $qui, $opt))
-		return autoriser_ticket_modifier_dist($faire, $type, $id, $qui, $opt);
-	
-	if(!function_exists('lire_config'))
-		include_spip('inc/config');
-	
-	$type = lire_config('tickets/autorisations/commenter_type', 'par_statut');
-	if($type){
-		switch($type) {
-			case 'webmestre':
-				// Webmestres uniquement
-				$autorise = ($qui['webmestre'] == 'oui');
-				break;
-			case 'par_statut':
-				// Traitement spécifique pour la valeur 'tous'
-				if(in_array('tous',lire_config('tickets/autorisations/commenter_statuts',array()))){
-					return true;
-				}
-				// Autorisation par statut
-				$autorise = in_array($qui['statut'], lire_config('tickets/autorisations/commenter_statuts',array('0minirezo','1comite')));
-				break;
-			case 'par_auteur':
-				// Autorisation par id d'auteurs
-				$autorise = in_array($qui['id_auteur'], lire_config('tickets/autorisations/commenter_auteurs',array()));
-				break;
-		}
-		if($autorise == true){
-			return $autorise;
-		}
-		$utiliser_defaut = false;
-	}
-
-	$liste = definir_autorisations_tickets('commenter',$utiliser_defaut);
-	if ($liste['statut'])
-		$autorise = in_array($qui['statut'], $liste['statut']);
-	else if ($liste['auteur'])
-		$autorise = in_array($qui['id_auteur'], $liste['auteur']);
-
-	return $autorise;
-}
-
-/**
  * Autorisation de modification des tickets
  * Défini qui peut modifier les tickets :
+ * - Les webmestres
  * - L'auteur du ticket
  * - Les personnes assignées
  * - Les personnes correspondant à la configuration
@@ -261,6 +213,9 @@ function autoriser_ticket_commenter_dist($faire, $type, $id, $qui, $opt){
  * @return boolean true/false : true si autorisé, false sinon
  */ 
 function autoriser_ticket_modifier_dist($faire, $type, $id, $qui, $opt){
+	if(($qui['webmestre'] == 'oui') && $qui['statut'] == '0minirezo')
+		return true;
+	
 	$autorise = false;
 	$utiliser_defaut = true;
 
@@ -317,6 +272,137 @@ function autoriser_ticket_modifier_dist($faire, $type, $id, $qui, $opt){
 }
 
 /**
+ * Autorisation à instituer des tickets
+ * Défini qui peut changer le statut des tickets :
+ * 
+ * - les webmestres
+ * 
+ * @param string $faire : l'action à faire
+ * @param string $type : le type d'objet sur lequel porte l'action
+ * @param int $id : l'identifiant numérique de l'objet
+ * @param array $qui : les éléments de session de l'utilisateur en cours
+ * @param array $opt : les options
+ * @return boolean true/false : true si autorisé, false sinon
+ */ 
+function autoriser_ticket_instituer_dist($faire, $type, $id, $qui, $opt){
+	if(($qui['webmestre'] == 'oui') && $qui['statut'] == '0minirezo')
+		return true;
+	
+	$autorise = false;
+	$utiliser_defaut = true;
+
+	if(is_numeric($id)){
+		// Si l'auteur en question est l'auteur du ticket ou l'auteur assigné au ticket,
+		// il peut modifier le ticket
+		$id_assigne_auteur = sql_fetsel('id_assigne,id_auteur','spip_tickets','id_ticket='.intval($id));
+		if(($id_assigne_auteur['id_auteur'] && ($id_assigne_auteur['id_auteur'] == $qui['id_auteur'])) || ($id_assigne_auteur['id_assigne'] && ($id_assigne_auteur['id_assigne'] == $qui['id_auteur'])))
+			return true;
+		
+		if(!function_exists('lire_config'))
+			include_spip('inc/config');
+
+		$type = lire_config('tickets/autorisations/modifier_type', 'par_statut');
+		if($type){
+			switch($type) {
+				case 'webmestre':
+					// Webmestres uniquement
+					$autorise = ($qui['webmestre'] == 'oui');
+					break;
+				case 'par_statut':
+					// Traitement spécifique pour la valeur 'tous'
+					if(in_array('tous',lire_config('tickets/autorisations/modifier_statuts',array()))){
+						return true;
+					}
+					// Autorisation par statut
+					$autorise = in_array($qui['statut'], lire_config('tickets/autorisations/modifier_statuts',array('0minirezo')));
+					break;
+				case 'par_auteur':
+					// Autorisation par id d'auteurs
+					$autorise = in_array($qui['id_auteur'], lire_config('tickets/autorisations/modifier_auteurs',array()));
+					break;
+			}
+			if($autorise == true){
+				return $autorise;
+			}
+			$utiliser_defaut = false;
+		}
+	
+		// Si $utiliser_defaut = true, on utilisera les valeurs par défaut
+		// Sinon on ajoute la possibilité de régler par define
+		$liste = definir_autorisations_tickets('modifier',$utiliser_defaut);
+		if ($liste['statut'])
+			$autorise = in_array($qui['statut'], $liste['statut']);
+		else if ($liste['auteur'])
+			$autorise = in_array($qui['id_auteur'], $liste['auteur']);
+		if(!$autorise){
+			$id_auteur = sql_getfetsel('id_auteur','spip_tickets','id_ticket='.intval($id));
+			if($id_auteur && ($id_auteur== $qui['id_auteur']))
+				$autorise = true;
+		}
+	}
+	return $autorise;
+}
+
+/**
+ * Autorisation de notification des tickets
+ * (défini qui doit être notifié)
+ * 
+ * @param string $faire : l'action à faire
+ * @param string $type : le type d'objet sur lequel porte l'action
+ * @param int $id : l'identifiant numérique de l'objet
+ * @param array $qui : les éléments de session de l'utilisateur en cours
+ * @param array $opt : les options
+ * @return boolean true/false : true si autorisé, false sinon
+ */
+function autoriser_ticket_commenter_dist($faire, $type, $id, $qui, $opt){
+	if(($qui['webmestre'] == 'oui') && $qui['statut'] == '0minirezo')
+		return true;
+	
+	$autorise = false;
+	$utiliser_defaut = true;
+
+	if(autoriser_ticket_modifier_dist($faire, $type, $id, $qui, $opt))
+		return autoriser_ticket_modifier_dist($faire, $type, $id, $qui, $opt);
+	
+	if(!function_exists('lire_config'))
+		include_spip('inc/config');
+	
+	$type = lire_config('tickets/autorisations/commenter_type', 'par_statut');
+	if($type){
+		switch($type) {
+			case 'webmestre':
+				// Webmestres uniquement
+				$autorise = ($qui['webmestre'] == 'oui');
+				break;
+			case 'par_statut':
+				// Traitement spécifique pour la valeur 'tous'
+				if(in_array('tous',lire_config('tickets/autorisations/commenter_statuts',array()))){
+					return true;
+				}
+				// Autorisation par statut
+				$autorise = in_array($qui['statut'], lire_config('tickets/autorisations/commenter_statuts',array('0minirezo','1comite')));
+				break;
+			case 'par_auteur':
+				// Autorisation par id d'auteurs
+				$autorise = in_array($qui['id_auteur'], lire_config('tickets/autorisations/commenter_auteurs',array()));
+				break;
+		}
+		if($autorise == true){
+			return $autorise;
+		}
+		$utiliser_defaut = false;
+	}
+
+	$liste = definir_autorisations_tickets('commenter',$utiliser_defaut);
+	if ($liste['statut'])
+		$autorise = in_array($qui['statut'], $liste['statut']);
+	else if ($liste['auteur'])
+		$autorise = in_array($qui['id_auteur'], $liste['auteur']);
+
+	return $autorise;
+}
+
+/**
  * Autorisation d'épingler des tickets
  * Défini qui peut épingler les tickets
  * Par défaut seulement les admins
@@ -329,6 +415,9 @@ function autoriser_ticket_modifier_dist($faire, $type, $id, $qui, $opt){
  * @return boolean true/false : true si autorisé, false sinon
  */ 
 function autoriser_ticket_epingler_dist($faire, $type, $id, $qui, $opt){
+	if(($qui['webmestre'] == 'oui') && $qui['statut'] == '0minirezo')
+		return true;
+	
 	$autorise = false;
 	$utiliser_defaut = true;
 
@@ -368,4 +457,45 @@ function autoriser_ticket_epingler_dist($faire, $type, $id, $qui, $opt){
 		$autorise = in_array($qui['id_auteur'], $liste['auteur']);
 	return $autorise;
 }
+
+/**
+ * La liste des tickets est accessible à tout le monde
+ * 
+ * @param string $faire
+ * 		L'action à faire
+ * @param string $type
+ * 		Le type d'objet sur lequel porte l'action
+ * @param int $id
+ *		L'identifiant numérique de l'objet
+ * @param array $qui
+ * 		Les éléments de session de l'utilisateur en cours
+ * @param array $opt
+ * 		Les options
+ * @return boolean true/false
+ * 		true si autorisé, false sinon
+ */
+function autoriser_tickets_menu_dist($faire, $type, $id, $qui, $opt) {
+	return true;
+}
+
+/**
+ * Le bouton de création rapide de ticket est visible pour ceux pouvant créer un ticket
+ * 
+ * @param string $faire
+ * 		L'action à faire
+ * @param string $type
+ * 		Le type d'objet sur lequel porte l'action
+ * @param int $id
+ *		L'identifiant numérique de l'objet
+ * @param array $qui
+ * 		Les éléments de session de l'utilisateur en cours
+ * @param array $opt
+ * 		Les options
+ * @return boolean true/false
+ * 		true si autorisé, false sinon
+ */
+function autoriser_ticketedit_menu_dist($faire, $type, $id, $qui, $opt) {
+	return autoriser('ecrire','ticket');
+}
+
 ?>
