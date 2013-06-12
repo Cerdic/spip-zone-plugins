bacb07dbed19afcad1730f46a768fbf50c74d7c5
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-06-12 16:17:22 +0000

    Sauvons nos widgets Twitter : il n'est plus possible d'interroger l'API v1 qui a été fermée. Du coup on necessite le plugin Microblog pour utiliser l'API v1.1 en oAuth. C'est un peu lourd mais transitoire : le plan est de fusionner la partie twitter de microblog (dont le reste ne sert guere plus) avec ce plugin dans un plugin Twitter.
    Mais en l'etat le widget fonctionne à nouveau.
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@73541 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- twidget/action/twidget.php
+++ twidget/action/twidget.php
@@ -10,12 +10,10 @@ function action_twidget_dist(){
 
 	$url = $_SERVER['QUERY_STRING'];
 	$url = preg_replace(",^action=twidget&w=/?,","",$url);
-	$sd = "api.";
-	if (strncmp($url,'search',6)==0)
-		$sd = "search.";
-	$url = "http://{$sd}twitter.com/".$url;
 
-	header("Content-type: text/javascript; charset=utf-8");
+	if (!_request('debug'))
+		header("Content-type: text/javascript; charset=utf-8");
+
 	$res = twidget_get_cached_url($url);
 	// header Expires pour eviter overflow par le meme client
 	header("Pragma: public");
@@ -35,11 +33,12 @@ function action_twidget_dist(){
 }
 
 function twidget_get_cached_url($url,$force=false) {
-	@define('_TWIDGET_CACHE',60);
-	$expires = _TWIDGET_CACHE;
-
 	// enlever le param anti cache
 	$url = preg_replace(",&\d+=cachebust,","",$url);
+
+	@define('_TWIDGET_CACHE',180);
+	$expires = _TWIDGET_CACHE;
+
 	if (_request('debug')){
 		$force = true;
 	}
@@ -69,25 +68,69 @@ function twidget_get_cached_url($url,$force=false) {
 		return $c['content'];
 	}
 
-
 	spip_log("requete api twitter $url","twitget");
 	// mettre a jour le cache
 
-	// recuperer le json
-	if (!defined('_INC_DISTANT_USER_AGENT'))
-		define('_INC_DISTANT_USER_AGENT',  $_SERVER['HTTP_USER_AGENT']);
 
-	include_spip('inc/distant');
-	$contenu = recuperer_page($url);
+	$surl = explode("?",$url);
+	$command = array_shift($surl);
+	$surl = implode("?",$surl);
+	parse_str($surl,$query);
+
+	// reaffecter API v1 sur API v1.1
+	$command_v11 = $command;
+	$callback = "";
+	switch($command){
+		case "search.json":
+			$command_v11 = "search/tweets";
+			if (isset($query['rpp'])){
+				$query['count'] = $query['rpp'];
+				unset($query['rpp']);
+			}
+			break;
+		default:
+			$command_v11 = preg_replace(",^1/|\.json$,","",$command);
+			break;
+	}
+	unset($query['clientsource']);
+	if (isset($query['callback'])){
+		$callback = $query['callback'];
+		unset($query['callback']);
+	}
+
+	include_spip("microblog_fonctions");
+
+	$res = microblog_twitter_api($command_v11,"get",$query);
 
 	if (_request('debug')){
 		var_dump($url);
-		var_dump($contenu);
-		if (!$contenu)
-			var_dump(recuperer_page($url, false, true));
+		var_dump(array($command_v11,$query));
+		var_dump($res);
 	}
 
-	$contenu = trim($contenu);
+	// redresser la sortie API v1.1 => API v1
+	switch($command){
+		case "search.json":
+			include_spip("inc/json");
+			foreach(array('completed_in','max_id','max_id_str','query') as $k)
+			$res[$k] = $res['search_metadata'][$k];
+			$res['results'] = $res['statuses'];
+			foreach($res['results'] as $k=>$r){
+				$res['results'][$k]['from_user'] = $r['user']['screen_name'];
+				$res['results'][$k]['profile_image_url'] = $r['user']['profile_image_url'];
+			}
+			unset($res['statuses']);
+			$res = json_encode($res);
+			break;
+		default:
+			$res = json_encode($res);
+			break;
+	}
+
+	if ($callback)
+		$res = $callback."(".$res.");";
+
+	$contenu = trim($res);
 
 	// ne pas cacher une requete qui echoue (twitter fail)
 	if ($contenu
@@ -96,6 +139,9 @@ function twidget_get_cached_url($url,$force=false) {
    	// intercepter, cacher et relocaliser les avatars
 		preg_match_all(',"profile_image_url":"([^"]*)",Uims',$contenu,$regs,PREG_SET_ORDER);
 		foreach($regs as $reg){
+			if (_request('debug')){
+				var_dump($reg);
+			}
 			$new = twidget_get_cached_avatar($reg[1]);
 			$contenu = str_replace($reg[1],$new,$contenu);
 		}
@@ -131,11 +177,16 @@ function twidget_get_cached_avatar($img_url){
 	$dir = sous_repertoire(_DIR_VAR,"twidget");
 	$dir = sous_repertoire($dir,substr($hash,0,2));
 	$f = $dir.$hash.$ext;
+	$f_rac = $f;
+	if (test_espace_prive())
+		$f_rac = substr($f,strlen(_DIR_RACINE));
 
 	if (file_exists($f) AND time()-filemtime($f)<_TWIDGET_CACHE_AVATAR)
-		return $f;
+		return $f_rac;
 
+	if (!function_exists("recuperer_page"))
+		include_spip("inc/distant");
 	recuperer_page($img_url,$f);
-	return $f;
+	return $f_rac;
 }
 ?>
