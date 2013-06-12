48ee4820482cfbd7c4f964c27cedeed8f976ca01
francecool@hotmail.com <francecool@hotmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-06-12 07:59:51 +0000

    Mise a jour lien documentation
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@73531 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- avatar_voki/trunk/avatar_voki_options.php
+++ avatar_voki/trunk/avatar_voki_options.php
@@ -23,6 +23,6 @@ define('AVATAR_VOKI_DOC_SPIP2', 'adxmenu_documentation_spip2'); // compat
 /**
  * URL de la page de documentation sur spip-contrib (documentation officielle)
  */
-define('AVATAR_VOKI_DOC_CONTRIB', 'http://contrib.spip.net/Un-avatar-Voki-dans-vos-articles');
+define('AVATAR_VOKI_DOC_CONTRIB', 'http://contrib.spip.net/L-Avatar-VOKI-dans-vos-articles-et-squelettes-3-0');
 
 ?>
