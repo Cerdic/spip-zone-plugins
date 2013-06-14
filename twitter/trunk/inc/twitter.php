b5f6603d6d8cf2c3235f0d6da8bdc566e3cf84a0
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-06-14 07:33:28 +0000

    L'API twitter limite le nombre de requetes par unité de temps et par application et par user. Lorsque memoization est présent, la fonction twitter_api_call microcache toutes les requetes en get. Le délai par défaut de 180s est personalisable par define, et l'option 'force' permet de forcer la requete hors cache.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@73568 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- twitter/trunk/inc/twitter.php
+++ twitter/trunk/inc/twitter.php
@@ -10,6 +10,8 @@
 
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
+if (!defined("_TWITTER_API_CALL_MICROCACHE_DELAY")) define("_TWITTER_API_CALL_MICROCACHE_DELAY",180);
+
 /**
  * Envoyer un message sur Twitter
  * @param $status
@@ -90,38 +92,7 @@ function twitter_connect($tokens=null){
 
 	$t = md5(serialize($tokens));
 	if (!isset($connection[$t])){
-		$cfg = @unserialize($GLOBALS['meta']['microblog']);
-
-		if(!is_array($tokens))
-			$tokens = array();
-
-		$tokens = array_intersect_key($tokens,
-			array(
-				'twitter_consumer_key'=>'',
-				'twitter_consumer_secret'=>'',
-				'twitter_account'=>'',
-				'twitter_token'=>'',
-				'twitter_token_secret'=>'',
-			));
-
-
-		if (!isset($tokens['twitter_consumer_key']) OR !isset($tokens['twitter_consumer_secret'])){
-			$tokens['twitter_consumer_key'] = $cfg['twitter_consumer_key'];
-			$tokens['twitter_consumer_secret'] = $cfg['twitter_consumer_secret'];
-		}
-		if (!isset($tokens['twitter_token']) OR !isset($tokens['twitter_token_secret'])){
-			$account = $cfg['default_account'];
-			if (isset($tokens['twitter_account']) AND isset($cfg['twitter_accounts'][$tokens['twitter_account']]))
-				$account = $tokens['twitter_account'];
-
-			if (!isset($cfg['twitter_accounts'][$account]))
-				$account = reset(array_keys($cfg['twitter_accounts']));
-
-			if (isset($cfg['twitter_accounts'][$account])){
-				$tokens['twitter_token'] = $cfg['twitter_accounts'][$account]['token'];
-				$tokens['twitter_token_secret'] = $cfg['twitter_accounts'][$account]['token_secret'];
-			}
-		}
+		$tokens = twitter_tokens($tokens);
 
 		if(
 			isset($tokens['twitter_consumer_key'])
@@ -150,6 +121,48 @@ function twitter_connect($tokens=null){
 	return $connection[$t];
 }
 
+/**
+ * Determiner les tokens de connexion en fonction de ceux passes
+ * et de la configuration par defaut
+ *
+ * @param array $tokens
+ * @return array
+ */
+function twitter_tokens($tokens){
+	$cfg = @unserialize($GLOBALS['meta']['microblog']);
+
+	if(!is_array($tokens))
+		$tokens = array();
+
+	$t = array_intersect_key($tokens,
+		array(
+			'twitter_consumer_key'=>'',
+			'twitter_consumer_secret'=>'',
+			'twitter_account'=>'',
+			'twitter_token'=>'',
+			'twitter_token_secret'=>'',
+		));
+
+	if (!isset($t['twitter_consumer_key']) OR !isset($t['twitter_consumer_secret'])){
+		$t['twitter_consumer_key'] = $cfg['twitter_consumer_key'];
+		$t['twitter_consumer_secret'] = $cfg['twitter_consumer_secret'];
+	}
+
+	if (!isset($t['twitter_token']) OR !isset($t['twitter_token_secret'])){
+		$account = $cfg['default_account'];
+		if (isset($t['twitter_account']) AND isset($cfg['twitter_accounts'][$t['twitter_account']]))
+			$account = $t['twitter_account'];
+
+		if (!isset($cfg['twitter_accounts'][$account]))
+			$account = reset(array_keys($cfg['twitter_accounts']));
+
+		if (isset($cfg['twitter_accounts'][$account])){
+			$t['twitter_token'] = $cfg['twitter_accounts'][$account]['token'];
+			$t['twitter_token_secret'] = $cfg['twitter_accounts'][$account]['token_secret'];
+		}
+	}
+	return $t;
+}
 
 /**
  * Fonction d'utilisation simple de l'API twitter oAuth
@@ -158,6 +171,7 @@ function twitter_connect($tokens=null){
  * @param $type string : le type de commande (get/post/delete)
  * @param $params array : les paramètres dans un array de la commande
  * @param array $options
+ *   bool force : true pour forcer la requete hors cache
  *   string return_type : le retour souhaité par défaut cela renverra la chaine ou l'array retourné par la commande.
  *                        Sinon on peut utiliser les valeurs http_code,http_info,url
  *
@@ -172,43 +186,65 @@ function twitter_connect($tokens=null){
  */
 function twitter_api_call($command,$type='get',$params=array(),$options=null){
 	include_spip('inc/microblog');
-	if (!$connection = twitter_connect($options))
-		return false;
 
-	switch($type){
-		case 'get':
-			$content = $connection->get($command,$params);
-			break;
-		case 'post':
-			$content = $connection->post($command,$params);
-			break;
-		case 'delete':
-			$content = $connection->delete($command,$params);
-			break;
-		default:
-			$content = $connection->get($command,$params);
+	// api_call en cache ?
+	$cache_key = null;
+	if ($type !== 'get'
+		OR (isset($options['force']) AND $options['force'])
+		OR !include_spip("inc/memoization")
+	  OR !function_exists("cache_get")
+	  OR !$t = twitter_tokens($options)
+	  OR !$cache_key = "twitter_api_call-".md5(serialize(array($command,$params,$t)))
+	  OR !$res = cache_get($cache_key)
+	  OR $res['time']+_TWITTER_API_CALL_MICROCACHE_DELAY<$_SERVER['REQUEST_TIME']){
+
+		if ($connection = twitter_connect($options)){
+
+			$res = array();
+			switch($type){
+				case 'post':
+					$res['content'] = $connection->post($command,$params);
+					break;
+				case 'delete':
+					$res['content'] = $connection->delete($command,$params);
+					break;
+				case 'get':
+				default:
+					$res['content'] = $connection->get($command,$params);
+					break;
+			}
+			$res['http_code'] = $connection->http_code;
+			$res['http_info'] = $connection->http_info;
+			$res['url'] = $connection->url;
+			$res['time'] = $_SERVER['REQUEST_TIME'];
+
+			if ($cache_key)
+				cache_set($cache_key,$res,_TWITTER_API_CALL_MICROCACHE_DELAY*2);
+		}
+		else {
+			if (!$res)
+				return false;
+			spip_log("twitter_api_call:$command echec connexion, on utilise le cache perime","twitter".LOG_INFO_IMPORTANTE);
+		}
 	}
 
-	$retour = isset($options['return_type'])?$options['return_type']:'';
-	switch($retour){
-		case 'http_code':
-			return $connection->http_code;
-		case 'http_info':
-			return $connection->http_info;
-		case 'url':
-			return $connection->url;
-		default:
-			if (!is_string($content) AND is_array($content)) {
-				// recopie ?
-				$contents = array();
-				foreach($content as $key => $val){
-					$contents[$key] = $val;
-				}
-				return $contents;
-			}
-			else{
-				return $content;
-			}
+	$retour = isset($options['return_type'])?$options['return_type']:'content';
+	if (!isset($res[$retour]))
+		$retour = 'content';
 
+	switch($retour){
+		default:
+			return $res[$retour];
+		case 'content':
+			if (!is_string($res['content']) AND is_array($res['content'])) {
+				// recopie ?
+				$copy = array();
+				foreach($res['content'] as $key => $val){
+					$copy[$key] = $val;
+				}
+				return $copy;
+			}
+			return $res['content'];
+			break;
 	}
 }
