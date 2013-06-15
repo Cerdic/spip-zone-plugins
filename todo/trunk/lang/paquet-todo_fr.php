5d00f174acd2cb3e05d32625122cf39fbd2c7664
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-06-15 12:02:22 +0000

    Amélioration de la description du plugin.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@73599 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- todo/trunk/lang/paquet-todo_fr.php
+++ todo/trunk/lang/paquet-todo_fr.php
@@ -6,7 +6,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// T
-	'todo_description' => 'Ajoute de nouveaux raccourcis typographiques permettant de décrire de manière simple des listes de choses à faire dans un contenu SPIP.',
+	'todo_description' => 'Ajoute de nouveaux raccourcis typographiques permettant de décrire de manière simple des listes de choses à faire dans un contenu SPIP. Cette version 2 étend la liste des statuts disponibles et permet la qualification des tâches par une priorité, des étiquettes et des informations typées comme les dates de début et de fin ou les numéros de commit et de version.',
 	'todo_slogan' => 'Lister rapidement des choses à faire'
 );
 
