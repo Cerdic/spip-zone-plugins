0b193dde87e4153af8be78f8d37351bb54257c58
nicolas.dorigny@gmail.com <nicolas.dorigny@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-06-15 15:59:50 +0000

    Messages d'info plus explicite
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@73610 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- fusion_spip/trunk/lang/fusion_spip_fr.php
+++ fusion_spip/trunk/lang/fusion_spip_fr.php
@@ -6,14 +6,11 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// B
-	'base' => 'Base de données',
-	'base_desc' => '<p>La base doit être déclarée comme base externe (-> Maintenance / Maintenance technique).</p>
-		<p>Elle doit être au même format (même shéma) que la base locale (plugins identiques).<br/>
-		Si la base source est une base mise à jour d\'une version 1.9 ou 2.x, il peut être nécessaire d\'ajouter :
-		<pre>mysql_query("SET NAMES \'utf8\'");</pre>
-		à la fin du fichier de connection.</p>',
-	'bouton_importer' => 'Importer cette base',
-	'bouton_supprimer' => 'Supprimer cette base',
+	'base' => 'Site source',
+	'base_desc' => '<p>La base de données du site source doit être <a href="'.generer_url_ecrire('admin_tech').'">déclarée comme base externe</a>.</p>
+		<p>Le site source doit être dans la même version que le site hôte.</p>',
+	'bouton_importer' => 'Démarrer la fusion',
+	'bouton_supprimer' => 'Supprimer la fusion',
 
 	// C
 	'confirme_warning' => 'Confirmer la fusion des bases ?',
@@ -27,8 +24,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// E
 	'explications' => '
 		<p><strong>Plugin expérimental, en cours de développement !</strong></p>
-		<p>Importer une base de données d\'un autre site SPIP</p>
-		<p>Cette manipulation peut consommer beaucoup de ressources CPU et être longue si la base source est volumineuse, à déconseiller sur un hébergement mutualisé. </p>',
+		<p>La fusion de bases de données peut consommer beaucoup de ressources CPU et être longue si la base source est volumineuse, à déconseiller sur un hébergement mutualisé. </p>',
 	'explications_suppression' => 'Fonctionnalité à terminer',
 	'erreur_versions' => 'Le site hôte et le site source ne sont pas dans la même version :
 		<br/>- hôte est en v @vhote@
@@ -37,7 +33,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// I
 	'img_dir' => 'Chemin physique des documents',
-	'img_dir_desc' => 'Pour copier les documents localement, indiquez (par exemple) :<pre>/var/www/site_spip/IMG</pre>Si le champ est vide, copiez manuellemnent les documents',
+	'img_dir_desc' => 'Pour copier les documents du site source dans le site hôte, indiquez leur chemin physique, par exemple :<pre>/var/www/site_spip/IMG</pre>Si le champ est vide, aucun document ne sera importé, vous devrez les copier manuellemnent.',
 
 	// M
 	'maj_base' => 'Mise à jour de la base de données',
@@ -55,7 +51,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// S
 	'secteur' => 'Secteur',
-	'secteur_desc' => 'Pour importer la base dans un secteur, sinon import à la racine',
+	'secteur_desc' => 'Pour importer le site source dans un secteur, sinon il sera importé à la racine',
 	'stats' => 'Traiter les tables de statistiques',
 
 	// T
