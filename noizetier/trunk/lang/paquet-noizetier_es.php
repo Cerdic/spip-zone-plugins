096bfce201d381206d885ce133285e92739ea3e1
alfdich@yahoo.fr <alfdich@yahoo.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-06-16 03:37:36 +0000

    langues (paquet-noizetier)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@73619 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ noizetier/trunk/lang/paquet-noizetier_es.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-noizetier?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// N
+	'noizetier_description' => 'El  noiZetier proporciona una interfaz en el espacio privado, que permite  de elegir para cada página, las noisettes a añadir a los bloques de contenido, navegación y extra  de cada página. Las noisettes completan de este modo los contenidos por defecto previstos por el esqueleto.',
+	'noizetier_nom' => 'NoiZetier',
+	'noizetier_slogan' => 'Gestionario de noisettes, originariamente funcional con los esqueletos Z'
+);
+
+?>
