60b6b9d025bae086f3c62b44c14d3e2b7fe3f2cc
bystrano@gmx.ch <bystrano@gmx.ch@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-06-17 05:56:45 +0000

    l'option pour ne pas insérer la feuille de style focus.css est prise en compte
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@73653 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- skiplink/trunk/skiplink_pipelines.php
+++ skiplink/trunk/skiplink_pipelines.php
@@ -2,7 +2,8 @@
 
 function skiplink_insert_head_css($flux) {
 	$flux .= '<link rel="stylesheet" type="text/css" href="'.find_in_path('css/skiplink.css').'" media="all" />'."\n";
-	$flux .= '<link rel="stylesheet" type="text/css" href="'.find_in_path('css/focus.css').'" media="all" />'."\n";
+  if (lire_config('skiplink/inserer_focus') !== 'non')
+    $flux .= '<link rel="stylesheet" type="text/css" href="'.find_in_path('css/focus.css').'" media="all" />'."\n";
 	return $flux;
 }
 
