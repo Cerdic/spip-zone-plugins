03f70e3a4ee96f5ba6ac40cba0fcdf23f45ca0f1
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-06-17 12:38:09 +0000

    SPIP 3.0 sait renommer les répertoire, on en profite pour éviter un truc bizare :
    -* Le zip dézippé gardait le répertoire normal du zip
    -* à cause des espaces, SPIP ne le détectait pas
    
    Du coup on force le dézip dans un répertoire sans espaces 
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@73660 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- emballe_medias/swfupload/trunk/swfupload_options.php
+++ emballe_medias/swfupload/trunk/swfupload_options.php
@@ -2,6 +2,6 @@
 
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
-define('_DIR_LIB_SWFUPLOAD',_DIR_RACINE.'lib/SWFUpload v2.2.0.1 Core/');
+define('_DIR_LIB_SWFUPLOAD',_DIR_RACINE.'lib/SWFUpload_v2.2.0.1_Core/');
 
 ?>
