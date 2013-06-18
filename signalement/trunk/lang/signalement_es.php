52f53c3479ddb66111499c94f63e9f7f9cdd8ebf
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-06-18 03:37:21 +0000

    langues (signalement)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@73681 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ signalement/trunk/lang/signalement_es.php
@@ -0,0 +1,84 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/signalement?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'bouton_alerter' => 'Alertar',
+	'bouton_enlever_alerte' => 'Quitar su alerta',
+
+	// E
+	'explication_depublier_seuil' => 'A partir de cuántas alertas un elemento es automáticamente extraído de la publicación',
+	'explication_notif_publication' => 'Seleccione los administradores que serán notificados cuando haya una nueva alerta. Si ninguno es seleccionado, todos los administradores serán notificados.',
+	'explication_select_motifs' => 'Los motivos siguientes serán mostrados en un selector para precisar la naturaleza de la alerta.',
+
+	// I
+	'icone_invalider_signalement' => 'Invalidar esta alerta',
+	'icone_invalider_signalements' => 'Invalidar estas alertas',
+	'icone_suivi_signalements' => 'Seguir estas alertas',
+	'icone_valider_signalement' => 'Validar esta alerta',
+	'icone_valider_signalements' => 'Validar estas alertas',
+	'info_1_signalement' => '1 alerta',
+	'info_aucun_signalement' => 'Ninguna alerta',
+	'info_gauche_suivi_signalement' => 'Esta página le permite moderar las alertas realizadas en el sitio',
+	'info_moderation_confirmee_refuse' => 'La alerta #@id_signalement@ se ha invalidado correctamente',
+	'info_moderation_deja_faite' => 'La alerta #@id_signalement@ ya ha sido moderada en "@statut@".',
+	'info_moderation_email_droit_insuffisant' => 'Ningún autor con el correo electrónico @email@ tiene derechos suficientes.',
+	'info_moderation_lien_titre' => 'Moderar esta alerta desde el espacio privado',
+	'info_moderation_signalement_introuvable' => 'La alerta @id@ no se encuentra.',
+	'info_nb_signalements' => '@nb@ alertas',
+	'info_sans_motif' => 'Sin motivo',
+	'info_selectionner_signalement' => 'Seleccionar las alertas:',
+
+	// L
+	'label_depublier' => 'No publicación',
+	'label_depublier_long' => 'No publicar automáticamente un contenido cuyo número de alerta sobrepase un determinado umbral',
+	'label_depublier_seuil' => 'Umbral de no publicación',
+	'label_mediabox' => 'MediaBox',
+	'label_mediabox_long' => 'No utilizar el MediaBox para mostra el formulario',
+	'label_motif' => 'El motivo de su alerta',
+	'label_notif_publication' => 'A quién se notifica una nueva alerta',
+	'label_select_motif' => 'Seleccionar su motivo',
+	'label_select_motifs' => 'Motivos a seleccionar',
+	'label_texte' => 'Su explicación',
+	'lien_signalement_objet' => 'Alertar sobre:',
+	'lien_vider_selection' => 'Desmarcar',
+	'lien_voir_signalements_objet' => 'Ver las alertas sobre este objeto',
+
+	// M
+	'mail_lien_refuser_commentaire' => 'Invalidar esta alerta',
+	'mail_ne_repondez_pas' => 'No responda a este correo electrónico. La dirección del contenido de alerta es:',
+	'mail_titre_signalement' => 'Alerta',
+	'motif_option_actes_danger' => 'Actos peligrosos o perniciosos (drogas, explosivos, suicidios, mutilaciones...)',
+	'motif_option_autre' => 'Otro',
+	'motif_option_droits_auteurs' => 'Violación de mis derechos (derechos de autor, ataque a mi vida privada...)',
+	'motif_option_enfants' => 'Maltrato a la infancia',
+	'motif_option_haine' => 'Contenido que incita al odio (apología del odio, acoso, racismo, abuso de personas vulnerables...)',
+	'motif_option_sexe' => 'Contenido de carácter sexual (acto sexual, nudismo...)',
+	'motif_option_spam' => 'Spam (publicidad, estafa/fraude, contenido engañoso...)',
+	'motif_option_violence' => 'Contenido violento o cruento (agresión, incitación a la violencia...)',
+
+	// S
+	'signalement' => 'Alerta',
+	'signalement_ajouter' => 'Alertar sobre este contenido',
+	'signalement_enlever' => 'Quitar su alerta',
+	'signalements' => 'Alertas',
+	'signalements_aucun' => 'Ninguna',
+	'signalements_publies' => 'Validadas',
+	'signalements_refuses' => 'Invalidadas',
+	'signalements_tous' => 'Todas',
+
+	// T
+	'texte_signalements' => 'Alertas de:',
+	'titre_cfg_signalement' => 'Configurar las alertas',
+	'titre_selection_action' => 'Selección',
+	'titre_signalement_suivi' => 'Seguimiento de las alertas',
+	'titre_signalements' => 'Alertas',
+	'tout_voir' => 'Ver todo'
+);
+
+?>
