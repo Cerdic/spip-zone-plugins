13e3d07d8c2a5ede8606281ca72ff2c6c1eec8b6
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-06-18 03:37:27 +0000

    langues (paquet-xmlrpc)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@73686 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ xmlrpc/trunk/lang/paquet-xmlrpc_es.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-xmlrpc?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// X
+	'xmlrpc_description' => 'API xml-rpc cliente y servidor',
+	'xmlrpc_slogan' => 'Servidor y cliente xml-rpc'
+);
+
+?>
