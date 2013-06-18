0291307991b25c05cc52a7d9bf6f61e698aeb053
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-06-18 03:37:18 +0000

    langues (paquet-podcast)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@73680 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ podcast/trunk/lang/paquet-podcast_es.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-podcast?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// P
+	'podcast_description' => 'Un plugin para poner a disposición uno o varios flujos de podcast en su sitio',
+	'podcast_nom' => 'Podcasts',
+	'podcast_slogan' => 'Uno o varios flujos de podcast en su sitio'
+);
+
+?>
