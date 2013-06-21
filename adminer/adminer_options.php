f2e5ea66806852ffe1aba5fc7009f473f4122b0f
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-06-21 08:36:23 +0000

    Menu et exec adminer réservé aux webmestres
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@73772 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- adminer/adminer_options.php
+++ adminer/adminer_options.php
@@ -10,9 +10,12 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 // aiguiller sur adminer si les bonnes conditions
 if (strpos($_SERVER['REQUEST_URI'],"prive.php")!==false AND !_DIR_RESTREINT){
 	if (
-	((in_array(_request('file'),array('default.css','functions.js','favicon.ico'))
-		AND _request('version'))
+	(($f=_request('file') AND in_array($f,array('default.css','functions.js','favicon.ico')) AND _request('version'))
 	OR ($_COOKIE['spip_adminer']))
 		AND !_request('page'))
 		$GLOBALS['fond'] = 'adminer';
+}
+
+function autoriser_adminer_menu_dist($faire,$quoi,$id,$qui,$options){
+	return autoriser('webmestre','',$id,$qui,$options);
 }
--- adminer/exec/adminer.php
+++ adminer/exec/adminer.php
@@ -9,6 +9,13 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 
 function exec_adminer_dist(){
 
+	include_spip("inc/autoriser");
+	if (!autoriser('menu','adminer')){
+		include_spip("inc/minipres");
+		echo minipres(_T('info_acces_interdit'));
+		die();
+	}
+
 	if (intval($GLOBALS['spip_version_branche'])<3)
 		$redir = _DIR_PLUGIN_ADMINER."index.php";
 	else
