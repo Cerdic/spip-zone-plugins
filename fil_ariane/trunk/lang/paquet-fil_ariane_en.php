ae3b07b6203af82b3e4720460d2548cf58c6d2c7
cyril@cym.fr <cyril@cym.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-06-21 13:15:46 +0000

    on ajoute la description du plugin en anglais;
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@73782 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ fil_ariane/trunk/lang/paquet-fil_ariane_en.php
@@ -0,0 +1,12 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	'fil_ariane_slogan'         => "An anchor to built cute bread crumbs menus...",
+	'fil_ariane_description'    => "This ultra-simple-to-use bread crumbs menu, automatically browses the tree-structure of any SPIP editorial object.",
+);
+
+?>
