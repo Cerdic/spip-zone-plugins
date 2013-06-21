0ae8e7f741e37511e3e7e7de1b6f86c3242f24c3
cyril@cym.fr <cyril@cym.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-06-21 09:04:42 +0000

    formulaire d'ajout rapide d'activité;
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@73774 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- dayfill/trunk/lang/dayfill_fr.php
+++ dayfill/trunk/lang/dayfill_fr.php
@@ -20,9 +20,21 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// E
 	'explication_id_auteur'         => "Sélectionnez des collaborateurs potentiels parmi tous les auteurs du site.",
 
-	//
-	'label_auteurs_projet'          => "Déjà attribués au projet",
+	// L
 	'label_auteurs_potentiels'      => "Collaborateurs potentiels",
+	'label_auteurs_projet'          => "Déjà attribués au projet",
+	'label_date_action'             => "Date",
+	'label_decrire_action'          => "Décrivez le type d'action effectuée",
+	'label_heure_debut'             => "Début",
+	'label_heure_fin'               => "Fin",
+	'label_heure_de'                => "De : ",
+	'label_heure_a'                 => " à : ",
+	'label_id_projet'               => "ID",
+	'label_quel_client'             => "Pour quel client ?",
+	'label_quel_projet'             => "Dans le cadre de quel projet ?",
+	'label_quel_temps_passe'        => "Combien de temps &agrave; facturer ?",
+	'label_quel_type_action'        => "Quel type d'action a été effectué ?",
+	'label_type_action'             => "Type d'action",
 
 	// T
 	'titre_page_configurer_dayfill' => 'Configuer le gestionnaire d\'activités',
