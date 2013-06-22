1ad55807cbd16dceee2af4b9a87156d0895a4013
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-06-22 03:37:40 +0000

    langues (blocsdepliables)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@73789 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- blocsdepliables/lang/blocsdepliables_fr.php
+++ blocsdepliables/lang/blocsdepliables_fr.php
@@ -1,8 +1,8 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/blocsdepliables/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
- 
+
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// B
@@ -13,20 +13,23 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'cfg_exemple_explication' => 'Explication de cet exemple',
 	'cfg_titre_parametrages' => 'Paramétrages',
 
-	'label_balise_titre' => 'Balise pour les titres des blocs',
-	'label_unique_1' => 'Un seul bloc ouvert sur la page',
-	'explication_unique' => 'L\'ouverture d’un bloc provoquera la fermeture de tous les autres blocs de la page, afin d’en avoir qu’un seul ouvert à la fois.',
-	'label_cookie_1' => 'Mémoriser l\'état des blocs',
+	// E
 	'explication_cookie' => 'L\'état des blocs numérotés sera stocké dans un cookie le temps de la session, afin de conserver l’aspect de la page en cas de retour.',
+	'explication_unique' => 'L\'ouverture d’un bloc provoquera la fermeture de tous les autres blocs de la page, afin d’en avoir qu’un seul ouvert à la fois.',
+
+	// L
 	'label_animation' => 'Animation',
 	'label_animation_aucun' => 'Aucune',
-	'label_animation_normal' => 'Glissement normal',
-	'label_animation_slow' => 'Glissement lent',
 	'label_animation_fast' => 'Glissement rapide',
 	'label_animation_millisec' => 'Glissement durant :',
+	'label_animation_normal' => 'Glissement normal',
+	'label_animation_slow' => 'Glissement lent',
+	'label_balise_titre' => 'Balise pour les titres des blocs',
+	'label_cookie_1' => 'Mémoriser l\'état des blocs',
+	'label_unique_1' => 'Un seul bloc ouvert sur la page',
 
 	// T
-	'titre_page_configurer_blocsdepliables' => 'Blocs dépliables',
+	'titre_page_configurer_blocsdepliables' => 'Blocs dépliables'
 );
 
 ?>
