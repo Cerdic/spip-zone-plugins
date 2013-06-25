87cf4a419d315485127621542c2f0a3e402f4af5
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-06-25 03:37:53 +0000

    langues (tdm)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@73849 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- table_matieres/trunk/lang/tdm_sk.php
+++ table_matieres/trunk/lang/tdm_sk.php
@@ -13,13 +13,13 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 		tag <code>#TABLE_MATIERES.</code>',
 
 	// E
-	'explication_longueur' => 'Nastaví maximálny počet znakov pre každú kotvu.', # MODIF
+	'explication_longueur' => 'Nastaví maximálny počet znakov, ktorý bude platiť každú kotvu.',
 	'explication_min' => 'Nastaví minimálny počet nadpisov v texte, z ktorých sa vytvorí obsah.',
 	'explication_separateur' => 'Určí znak, ktorý sa vloží medzi slová namiesto medzier.',
-	'explication_tdm_flottante' => 'Lorsque la table des matières devient invisible lors du scroll de la page, celle-ci se positionne en haut de l\'écran afin d\'être constamment visible.', # NEW
+	'explication_tdm_flottante' => 'Keď sa pri posúvaní obrazovky obsah stratí z dohľadu, umiestni sa do hornej časti obrazovky, aby ho bolo stále vidieť.',
 
 	// L
-	'label_tdm_flottante' => 'Table des matières flottante', # NEW
+	'label_tdm_flottante' => 'Tabuľka s „plávajúcim” obsahom',
 	'longueur' => 'Dĺžka:',
 
 	// M
