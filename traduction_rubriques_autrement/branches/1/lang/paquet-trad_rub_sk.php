0a1a3c6dabd8e3c464a4993147ac9c22b76ed419
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-06-25 06:53:32 +0000

    - langues - changement d'url - version spip min
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@73856 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- traduction_rubriques_autrement/branches/1/lang/paquet-trad_rub_sk.php
+++ traduction_rubriques_autrement/branches/1/lang/paquet-trad_rub_sk.php
@@ -9,7 +9,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// T
 	'trad_rub_description' => 'Tento zásuvný modul, ktorý bol vytvorený pre viacjazyčné stránky [so sektormi podľa jazyka->http://www.spip-contrib.net/Un-exemple-de-site-multilingue], vám pokiaľ ide o navigáciu a riadenie prekladov rubrík ponúka podobný prístup ako zásuvný modul [Preklad článkov inak,->http://files.spip.org/spip-zone/traduction_articles_autrement.zip] ktorého inštalácia sa odporúča. Keďže spip v predvolených nastaveniach neponúka preklad rubrík, treba si nainštalovať zásuvný modul [Preklad medzi rubrikami.->http://files.spip.org/spip-zone/tradrub.zip]',
-	'trad_rub_nom' => 'Traductions de rubriques autrement', # NEW
+	'trad_rub_nom' => 'Preklady ostatných rubrík',
 	'trad_rub_slogan' => 'Prechádzanie medzi preloženými rubrikami.'
 );
 
