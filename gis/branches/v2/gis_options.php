23020c7cf6cdccf1eb2ca62c65fac52b530d74f5
brunobergot@gmail.com <brunobergot@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-06-26 08:19:36 +0000

    version 2.4.8 : maj de la lib qu'on passe sur contrib maintenant + bugfix pour les controles de zoom + pan avec gg maps API (cf https://github.com/mapstraction/mxn/pull/257)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@73894 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- gis/branches/v2/gis_options.php
+++ gis/branches/v2/gis_options.php
@@ -2,7 +2,7 @@
 
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
-define('_DIR_LIB_GIS','lib/mxn-gis-2.2.2/');
+define('_DIR_LIB_GIS','lib/mxn-gis-2.4.8/');
 
 $GLOBALS['logo_libelles']['id_gis'] = _T('gis:libelle_logo_gis');
 
