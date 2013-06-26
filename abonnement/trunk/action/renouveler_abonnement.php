59cac35d9c396630fabcfd2f631cfa87c29bf2b9
philippeboussin@gmail.com <philippeboussin@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-06-26 20:58:05 +0000

    Renouveler un abonnement recule seulement la date de fin et ne crée pas un nouvel abonnement...
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@73912 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- abonnement/trunk/action/renouveler_abonnement.php
+++ abonnement/trunk/action/renouveler_abonnement.php
@@ -5,7 +5,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 
 /**
  * Action de renouveler un abonnement
- * @param unknown_type $arg
+ * @param int $arg
  * @return unknown_type
  */
 function action_renouveler_abonnement_dist($arg=null) {
@@ -14,14 +14,14 @@ function action_renouveler_abonnement_dist($arg=null) {
 		$arg = $securiser_action();
 	}
 	
-	// Si on a bien un abonnement et qu'on a le droit d'en créer
+	// Si on a bien un abonnement et qu'on a le droit de le modifier
 	if (
 		$id_abonnement = intval($arg)
-		and autoriser('creer', 'abonnement', $id_abonnement)
-		and $abonnement = sql_fetsel('*', 'spip_abonnements', 'id_abonnement = '.$id_abonnement)
+		and autoriser('modifier', 'abonnement', $id_abonnement)
+		and $abonnement = sql_fetsel('id_abonnements_offre, date_debut, date_fin', 'spip_abonnements', 'id_abonnement = '.$id_abonnement)
 	) {
 	
-		$offre = sql_fetsel('*', 'spip_abonnements_offres', 'id_abonnements_offre = '.$abonnement['id_abonnements_offre']);
+		$offre = sql_fetsel('duree, periode', 'spip_abonnements_offres', 'id_abonnements_offre = '.$abonnement['id_abonnements_offre']);
 		
 		// Si l'offre parente A BIEN une durée
 		if (($duree = $offre['duree']) > 0){
@@ -53,22 +53,12 @@ function action_renouveler_abonnement_dist($arg=null) {
 			// Calcul de la date de fin
 			$nouvelle_echeance = date('Y-m-d H:i:s', strtotime($date_depart.$ajout));
 			
-			// On lance la création
+			// On lance la modification
 			include_spip('action/editer_objet');
-			if (
-				$id_renouvellement = objet_inserer('abonnement')
-				and autoriser('modifier', 'abonnement', $id_renouvellement)
-			) {
-				$erreur = objet_modifier('abonnement', $id_renouvellement, array(
-																				'id_abonnements_offre' => $abonnement['id_abonnements_offre'],
-																				'id_auteur' => $abonnement['id_auteur'],
-																				'date_debut' => $date_depart,
-																				'date_fin' => $nouvelle_echeance,
-																				'statut' => 'actif' 
-																				));
+			$erreur = objet_modifier('abonnement', $id_abonnement, array('date_fin' => $nouvelle_echeance, 'statut' => 'actif'));
 			
-				return array($id_renouvellement, $erreur);
-			}
+			return array($id_abonnement, $erreur);
+
 		}
 	}
 	
