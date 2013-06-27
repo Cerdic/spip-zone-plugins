f0d594927a64555d6476b8625a5d3c00bf33fa99
erational@erational.org <erational@erational.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-06-27 03:28:35 +0000

    sur la version SPIP 3, ne plus mentionner la lecture de vidéos. nettoyage des fichiers perimés
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@73913 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- player/trunk/lang/paquet-player_fr.php
+++ player/trunk/lang/paquet-player_fr.php
@@ -6,13 +6,13 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// P
-	'player_description' => 'Ce plugin permet la lecture et l\'affichage de sons ou de vidéos.
+	'player_description' => 'Ce plugin permet la lecture de sons.
 Il ajoute des lecteurs flash adaptés aux formats .mp3.
 Il agit sur tous les <code><docXX|player></code> insérés dans les textes, aussi bien que dans les squelettes.
 
 -* Dans un texte <code><docXX|player></code> affiche un lecteur flash audio (plusieurs lecteurs au choix);
 -* Dans un squelette <code>#MODELE{playliste}</code> permet d\'afficher une playliste des derniers mp3',
-	'player_slogan' => 'Lire des sons et des vidéos'
+	'player_slogan' => 'Lire des sons mp3'
 );
 
 ?>
