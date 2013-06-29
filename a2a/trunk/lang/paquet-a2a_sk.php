384ae984de5e2dd3a018a833b9b91520ec252ea9
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-06-29 03:41:29 +0000

    langues (paquet-a2a)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@73975 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- a2a/trunk/lang/paquet-a2a_sk.php
+++ a2a/trunk/lang/paquet-a2a_sk.php
@@ -8,7 +8,9 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
-	'a2a_description' => 'Tento zásuvný modul vám umožní vytvoriť odkazy na články v chronologickom poradí bez toho, aby ste museli použiť kľúčové slová SPIPu. Môžete ho využívať na vytváranie odkazov typu "Pozri aj".', # MODIF
+	'a2a_description' => 'Tento zásuvný modul vám umožní vytvoriť odkazy na články v chronologickom poradí bez toho, aby ste museli použiť kľúčové slová SPIPu. Môžete ho využívať na vytváranie odkazov typu "Pozri aj".
+
+Táto nová verzia vám umožňuje dopísať vzťahy medzi článkami. Táto funkcia nie je aktivovaná v predvolených nastaveniach, aby sa zachoval spôsob, ako zásuvný modul tradične funguje (prečítajte si dokumentáciu).',
 	'a2a_slogan' => 'Na prepájanie článkov'
 );
 
