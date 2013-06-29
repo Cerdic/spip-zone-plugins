364b9c6a134a340146e38e90a3a2d6d22f76ed0e
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-06-29 03:41:41 +0000

    langues (paquet-player)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@73976 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- player/trunk/lang/paquet-player_sk.php
+++ player/trunk/lang/paquet-player_sk.php
@@ -8,13 +8,13 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// P
-	'player_description' => 'Tento zásuvný modul umožňuje prehrávanie a zobrazenie zvukov alebo videí.
-Pridáva údaje zo zariadení USB vo formáte .mp3.
+	'player_description' => 'Tento zásuvný modul umožňuje prehrávať zvukové súbory.
+Pridáva USB prehrávače na formát .mp3.
 Funguje na všetkých <code><docXX|player></code> vložených v texte, ako aj v šablónach.
 
--* v texte <code><docXX|player></code> zobrazí údaje zo zariadení USB (môžete si vybrať z viacerých zariadení),
--* v šablóne <code>#MODELE{playliste}</code> umožňuje zobraziť zoznam skladieb z posledných mp3', # MODIF
-	'player_slogan' => 'Prehrávanie zvukov a videí' # MODIF
+-* v texte <code><docXX|player></code> zobrazí prehrávač USB (môžete si vybrať z viacerých prehrávačov),
+-* v šablóne <code>#MODELE{playliste}</code> umožňuje zobraziť zoznam skladieb z posledných mp3',
+	'player_slogan' => 'Prehrávanie MP3'
 );
 
 ?>
