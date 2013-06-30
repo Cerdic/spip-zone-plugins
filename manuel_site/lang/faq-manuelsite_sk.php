ed0388e19b8fa353b2e04ef0f26c2f9dfd751108
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-06-30 03:39:45 +0000

    langues (faq-manuelsite)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@74010 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- manuel_site/lang/faq-manuelsite_sk.php
+++ manuel_site/lang/faq-manuelsite_sk.php
@@ -25,14 +25,14 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 -* Vytvorte z nich súbor zip
 -* Tento súbor zip pripojte k článku. Na konci sťahovania sa vás systém spýta, čo chcete so súborom urobiť, napríklad môžete všetky fotky vložiť do portfólia.',
 	'img_nombre_q' => 'Ako ľahko napĺňať portfólio?',
-	'img_ou_doc' => 'Na vkladanie obrázkov do textu článku používa najmä tag <code><imgXX|center>.</code> Ak však chcete zobraziť pod obrázkom jeho nadpis alebo popis, použite tag <code><docXX|center>.</code>',
+	'img_ou_doc' => 'Na vkladanie obrázkov do textu článku používa najmä tag <code><imgXX|center>.</code> Ak však chcete zobraziť pod obrázkom jeho nadpis alebo opis, použite tag <code><docXX|center>.</code>',
 	'img_ou_doc_q' => '<code><imgXX> alebo <docXX>?</code>',
 	'img_q' => 'Akú veľkosť by mala mať moja fotka?',
 
 	// S
 	'son' => 'Uložte si zvukový záznam vo formáte mp3 v režime mono s frekvenciou 11 alebo 22 kHz a bitovou rýchlosťou (rýchlosť pri kompresii) 64 kb/s (alebo viac, ak chcete vyššiu kvalitu).
 	
-Súbor mp3 zaraďte do článku ako obrázok a dajte mu nadpis, prípadne pridajte popis a poďakovanie (spolu)autorom. Nakoniec ho vložte do článku na miesto, na ktoré chcete  <code><docXX|center|player></code>. Na verejne prístupnej stránke sa zobrazí ikona USB kľúča, aby návštevníci mohli zvukový záznam spustiť.
+Súbor mp3 zaraďte do článku ako obrázok a dajte mu nadpis, prípadne pridajte opis a poďakovanie (spolu)autorom. Nakoniec ho vložte do článku na miesto, na ktoré chcete  <code><docXX|center|player></code>. Na verejne prístupnej stránke sa zobrazí ikona USB kľúča, aby návštevníci mohli zvukový záznam spustiť.
 _ {Pozor, maximálna veľkosť súboru je 150 MB alebo dĺžka približne 225 minút}',
 	'son_audacity' => 'Na prácu s audio súborom môžete využiť program Audacity (Mac, Windows, Linux), ktorý sa dá stiahnuť odtiaľto [->http://audacity.sourceforge.net/]. Niekoľko tipov:
 -* Po inštalácii programu budete potrebovať aj knižnicu na kódovanie lame mp3 [->http://audacity.sourceforge.net/help/faq?s=install&item=lame-mp3].
@@ -43,7 +43,7 @@ _ {Pozor, maximálna veľkosť súboru je 150 MB alebo dĺžka približne 225 m
 	'son_q' => 'Ako pridať k článku súbor so zvukom?',
 
 	// T
-	'thumbsites' => 'V rubrike {{@rubrique@}} kliknite na tlačidlo "Odkázať na stránku". Zadajte adresu stránky a potvrďte stránku; systém sa pokúsi získať názov, popis a obrázok stránky online.  Ak je to potrebné, upravte názov a popis. Ak obrázok nebol vytvorený automaticky, urobte snímku obrazovky a vložte ju ako logo stránky s rozmermi 120 x 90 pixelov.',
+	'thumbsites' => 'V rubrike {{@rubrique@}} kliknite na tlačidlo "Odkázať na stránku". Zadajte adresu stránky a potvrďte stránku; systém sa pokúsi získať názov, opis a obrázok stránky online.  Ak to bude potrebné, upravte názov a opis. Ak sa obrázok nevytvorí automaticky, urobte snímku obrazovky s rozmermi 120 x 90 a vložte ju ako logo stránky.',
 	'thumbsites_q' => 'Ako vytvoriť odkaz na (inú) stránku na stránke s odkazmi?',
 	'trier' => 'Čísla pred názvami článkov/rubrík/ dokumentov umožňujúce manipulovať s poradím, v akom sa zobrazí. Syntax tvorí číslo,  za ktorým nasleduje bodka a medzera',
 	'trier_q' => 'Ako nastaviť správne poradie zobrazenia článkov, rubrík alebo pripojených súborov?',
