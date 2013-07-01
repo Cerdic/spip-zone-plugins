516eae4648527569c8e265b52abf9a9a41de6bf5
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-07-01 16:40:57 +0000

    On prend en compte la version de spip en regardant la présence de plugin-dist. Sinon, c'est Spip 2. - Si on est sous Spip 3, on cherche paquet.xml et plugin.xml. Sinon on ne cherche que plugin.xml. - Selon la version de SPIP, les urls vers l'admin ne sont pas les mêmes. Alors on crée des variables pour cela. - On a un compteur de plugins utilisés, plugins inutilisés et le total. - On ajoute un mode 'debug' qui s'affichera si on passe en paramètre url 'debug=1'. C'est juste pour avoir quelques infos rapidement sous les yeux. La mémoire utilisée, memory_limit de php, le temps d'exécution de la page. Je revois l'esthétique dans un prochain commit. - on sort les styles de la page pour pls facilement gérer les css.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@74049 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- mutualisation/trunk/exec/mutualisation.php
+++ mutualisation/trunk/exec/mutualisation.php
@@ -1,9 +1,13 @@
 <?php
+header( 'charset:UTF-8' );
 
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 function exec_mutualisation_dist() {
 	global $auteur_session;
+	$ustart = memory_get_peak_usage(true);
+	$timestart=microtime(true);
+	$memory_limit = strtolower(ini_get('memory_limit'));
 	
 	include_spip('inc/minipres');
 	include_spip('inc/filtres');
@@ -15,6 +19,17 @@ function exec_mutualisation_dist() {
 	$lister_sites = charger_fonction('lister_sites','mutualisation');
 	$sites = $lister_sites();
 
+	if (is_dir('../plugins-dist')) {
+		$version_spip = 3 ;
+		$url_stats = "ecrire/?exec=stats_visites";
+		$url_compresseur = "ecrire/?exec=configurer_avancees#formulaire_configurer_compresseur";
+		$url_admin_plugin = "ecrire/?exec=admin_plugin";
+	} else {
+		$version_spip = 2;
+		$url_stats = "ecrire/?exec=statistiques_visites";
+		$url_compresseur = "ecrire/?exec=config_fonctions#configurer-compresseur";
+		$url_admin_plugin = "ecrire/?exec=admin_plugin";
+	}
 
 	if (!file_exists(_DIR_IMG.'mutualiser.png'))
 		@copy(find_in_path('mutualiser.png'), _DIR_IMG.'mutualiser.png');
@@ -112,7 +127,6 @@ function exec_mutualisation_dist() {
 			$compression .= ($compression!='') ? '+HTTP':'HTTP';
 		if ($compression=='')
 			$compression = _L('Activer');
-	
 		$page .= '<script type="text/javascript">
 		//<![CDATA[
 		tableau_sites.push(["../../'.$GLOBALS['mutualisation_dir'].'/'.$v.'"]);
@@ -127,9 +141,9 @@ function exec_mutualisation_dist() {
 			<td><div id='IMG$nsite' class='taille loading'></div></td>
 			<td><div id='local$nsite' class='taille loading'></div></td>
 			<td><div id='cache$nsite' class='taille loading'></div></td>
-			<td style='text-align:right;'><a href='${url}ecrire/index.php?exec=statistiques_visites'>${stats}</a></td>
-			<td>$adminplugin<a href='${url}ecrire/index.php?exec=admin_plugin'>${cntplugins}</a> <small>${plugins}</small></td>
-			<td><a href='${url}ecrire/index.php?exec=config_fonctions#configurer-compresseur'>$compression</a></td>
+			<td style='text-align:right;'><a href='${url}$url_stats'>${stats}</a></td>
+			<td>$adminplugin<a href='${url}$url_admin_plugin'>${cntplugins}</a> <small>${plugins}</small></td>
+			<td><a href='${url}$url_compresseur'>$compression</a></td>
 			<td style='text-align:right;'>".date_creation_repertoire_site($v)."</td>
 			</tr>\n";
 		$nsite++;
@@ -138,17 +152,17 @@ function exec_mutualisation_dist() {
 
 
 	if ($lsplugs) {
+		$nombre_plugins = count($lsplugs) ;
 		$page .= "<br /><br /><table style='clear:both;'>
 	<thead>
 		<tr>
 			<td>#</td>
-			<td>Plugins utilis&#233;s</td>
+			<td>Plugins utilis&#233;s ($nombre_plugins) </td>
 			<td>Version</td>
 			<td>Sites</td>
 		</tr>
 	</thead>
 	<tbody>";
-	$site = array();
 		foreach ($lsplugs as $plugin => $c){
 			$plnum[count($c)] .= "<tr><td>".count($c)."</td><td>$plugin</td>"
 				."<td>".$versionplug[$plugin]."</td><td>".join(', ', ancre_site($c)).'</td></tr>';
@@ -161,56 +175,92 @@ function exec_mutualisation_dist() {
 		$inutile = array();
 		$extract = array();
 		$list = array();
-		// En spip 3, avec SVP, on liste les plugins dans des sous-répertoires.
-		// Ca peut aller jusqu'a 3 sous-répertoires.
-		// On garde l'ancien principe d'un sous-répertoire pour ne pas casser la compat.
+		
+		if ($version_spip AND $version_spip == 3) {
+			$ustart_glob = memory_get_peak_usage(true);
+			// Ici on est en SPIP 3.
+			// En spip 3, avec SVP, on liste les plugins dans des sous-répertoires.
+			// Ca peut aller jusqu'a 3 sous-répertoires.
+			// On garde l'ancien principe d'un sous-répertoire pour ne pas casser la compat.
+	
+			// correspond à plugins/nom_plugin/fichier.xml
+			if (glob(_DIR_PLUGINS . '*/{paquet,plugin}.xml',GLOB_BRACE)) {
+				foreach (glob(_DIR_PLUGINS . '*/{paquet,plugin}.xml',GLOB_BRACE) as $value) {
+					$list[] = $value;
+				}
+			}
+			// correspond à plugins/auto/nom_plugin/fichier.xml
+			if (glob(_DIR_PLUGINS . '*/*/{paquet,plugin}.xml',GLOB_BRACE)) {
+				foreach (glob(_DIR_PLUGINS . '*/*/{paquet,plugin}.xml',GLOB_BRACE) as $value) {
+					$list[] = $value;
+				}
+			}
+			// correspond à plugins/auto/nom_plugin/x.y.z/fichier.xml
+			if (glob(_DIR_PLUGINS . '*/*/*/{paquet,plugin}.xml',GLOB_BRACE)) {
+				foreach (glob(_DIR_PLUGINS . '*/*/*/{paquet,plugin}.xml',GLOB_BRACE) as $value) {
+					$list[] = $value;
+				}
+			}
+	
+			// Ici on va prendre les chemins d'extrusion uniquement, sans distinction du fichier xml
+			foreach ($list as $value) {
+				$extract[] = str_replace(array('plugin.xml','paquet.xml'), '', $value);
+			}
+			// On dédoublonne
+			$extract = array_unique($extract);
+			foreach ($extract as $url) {
+				// Et on refait une recherche pour paquet.xml d'abord
+				if(glob($url . 'paquet.xml', GLOB_NOSORT)) {
+					$result = glob($url . 'paquet.xml', GLOB_NOSORT);		
+					$result = $result[0] ;
+					// dans paquet.xml on cherche la valeur de l'attribut prefix
+					if (preg_match('/prefix="([^"]*)"/i', file_get_contents($result), $r) 
+						AND !$lsplugs[strtolower(trim($r[1]))])
+							$inutile[] = trim($r[1]);
+				} else { // Si pas de paquet.xml, on cherche plugin.xml
+					$result = glob($url . 'plugin.xml', GLOB_NOSORT);		
+					$result = $result[0] ;
+					// là, on reprend l'ancien code. On cherche la valeur de la balise prefix
+					if (preg_match(',<prefix>([^<]+),ims', file_get_contents($result), $r)
+						AND !$lsplugs[strtolower(trim($r[1]))])
+							$inutile[] = trim($r[1]);
+				}
+			}
+			$uend_glob = memory_get_peak_usage(true);
+			
+		} else if ($version_spip AND $version_spip == 2) {
+			$ustart_glob = memory_get_peak_usage(true);
+			// Si on est sur un spip 2
+			// correspond à plugins/nom_plugin/fichier.xml
+			if (glob(_DIR_PLUGINS . '*/plugin.xml')) {
+				foreach (glob(_DIR_PLUGINS . '*/plugin.xml') as $value) {
+					$list[] = $value;
+				}
+			}
+			// correspond à plugins/auto/nom_plugin/fichier.xml
+			if (glob(_DIR_PLUGINS . '*/*/plugin.xml')) {
+				foreach (glob(_DIR_PLUGINS . '*/*/plugin.xml') as $value) {
+					$list[] = $value;
+				}
+			}
 
-		// correspond à plugins/nom_plugin/fichier.xml
-		if (glob(_DIR_PLUGINS . '*/{paquet,plugin}.xml',GLOB_BRACE)) {
-			foreach (glob(_DIR_PLUGINS . '*/{paquet,plugin}.xml',GLOB_BRACE) as $value) {
-				$list[] = $value;
-			}
-		}
-		// correspond à plugins/auto/nom_plugin/fichier.xml
-		if (glob(_DIR_PLUGINS . '*/*/{paquet,plugin}.xml',GLOB_BRACE)) {
-			foreach (glob(_DIR_PLUGINS . '*/*/{paquet,plugin}.xml',GLOB_BRACE) as $value) {
-				$list[] = $value;
-			}
-		}
-		// correspond à plugins/auto/nom_plugin/x.y.z/fichier.xml
-		if (glob(_DIR_PLUGINS . '*/*/*/{paquet,plugin}.xml',GLOB_BRACE)) {
-			foreach (glob(_DIR_PLUGINS . '*/*/*/{paquet,plugin}.xml',GLOB_BRACE) as $value) {
-				$list[] = $value;
-			}
-		}
-
-		// Ici on va prendre les chemins d'extrusion uniquement, sans distinction du fichier xml
-		foreach ($list as $value) {
-			$extract[] = str_replace(array('plugin.xml','paquet.xml'), '', $value);
-		}
-		// On dédoublonne
-		$extract = array_unique($extract);
-		foreach ($extract as $url) {
-			// Et on refait une recherche pour paquet.xml d'abord
-			if(glob($url . 'paquet.xml', GLOB_NOSORT)) {
-				$result = glob($url . 'paquet.xml', GLOB_NOSORT);		
-				$result = $result[0] ;
-				// dans paquet.xml on cherche la valeur de l'attribut prefix
-				if (preg_match('/prefix="([^"]*)"/i', file_get_contents($result), $r) 
-					AND !$lsplugs[strtolower(trim($r[1]))])
-						$inutile[] = trim($r[1]);
-			} else { // Si pas de paquet.xml, on cherche plugin.xml
-				$result = glob($url . 'plugin.xml', GLOB_NOSORT);		
-				$result = $result[0] ;
+			foreach ($list as $url) {
 				// là, on reprend l'ancien code. On cherche la valeur de la balise prefix
-				if (preg_match(',<prefix>([^<]+),ims', file_get_contents($result), $r)
+				if (preg_match(',<prefix>([^<]+),ims', file_get_contents($url), $r)
 					AND !$lsplugs[strtolower(trim($r[1]))])
 						$inutile[] = trim($r[1]);
 			}
+
+			$uend_glob = memory_get_peak_usage(true);
 		}
+		
+		$inutile = array_map('mb_strtolower', $inutile);
+		sort($inutile);
 
 		if ($inutile) {
-			$page .= "<p><strong>"._L('Plugins inutilis&#233;s :')."</strong> ".join(', ', $inutile)."</p>";
+			$nombre_plugins_inutiles =count($inutile) ;
+			$page .= "<p><strong>"._L('Plugins inutilis&#233;s :')."</strong> ".join(', ', $inutile).".<br />";
+			$page .= "<em>Soit " . $nombre_plugins_inutiles . _L(' plugins inutilis&#233;s') . ".</em></p>";
 		}
 	}
 
@@ -221,30 +271,40 @@ function exec_mutualisation_dist() {
 	$page = minipres($titre, $page);
 	
 	$page = str_replace('</head>', '
-		<style type="text/css">
-		a {color:#5a3463;}
-		table {border-collapse: collapse; border: 1px solid #999; width: 100%;}
-		tr {vertical-align:top;border: 1px solid #999;}
-		.tr0 {background-color:#ddded5}
-		thead tr {font-weight:bold;background-color:#333;color:#fff;}
-		thead tr input {font-weight:normal;font-size:0.9em;}
-		thead tr .unite {font-weight:normal;font-size:0.9em;}
-		td {text-align:left;border-left: 1px solid #ccc;}
-		td em {color:#aaa;}
-		#minipres{width:auto;}
-		.upgrade {text-align: center; padding:0 .5em; display:inline;}
-		.upgrade div {display:inline;}
-		.upgrade input { border: 2px solid red;color:red; background-color:#fff; font-weight:bold;}
-		.erreur {color:red;font-weight:bold;}
-		.taille {text-align: right;}
-		.loading {background: url(../mutualisation/images/loading.gif) left center no-repeat}
-		p { margin-top: 1em;}
-		</style>
+		<link rel="stylesheet" type="text/css" href="../mutualisation/mutualisation.css" />
 		<script src="../prive/javascript/jquery.js" type="text/javascript"></script>
 		<script src="../mutualisation/mutualisation_tailles.js" type="text/javascript"></script>
 		</head>
 		', $page);
 
+	$uend = memory_get_peak_usage(true);
+	$udiff = $uend - $ustart;
+	$udiff_glob = $uend_glob - $ustart_glob ;
+	$timeend=microtime(true);
+	$time=$timeend-$timestart;
+	$page_load_time = number_format($time, 3);
+
+	if (isset($_GET['debug'])) {
+		$page .= "<div class='debug'>";
+		$page .= "<p><strong>Mémoire :</strong> " ;
+		$page .= "<em>Au début :</em> " . memoryUsage($ustart) . " | " ;
+		$page .= "<em>À la fin :</em> " . memoryUsage($uend) . " | " ;
+		$page .= "<em>Différence :</em> " . memoryUsage($udiff) ;
+		$page .= "</p>" ;
+		$page .= "<p><strong>Temps d'exécution :</strong> " ;
+		$page .= "<em>Début du script :</em> " . date("H:i:s", $timestart) . " | " ;
+		$page .= "<em>Fin du script :</em> " . date("H:i:s", $timeend) . " | " ;
+		$page .= "<em>Script exécuté en </em> " . $page_load_time . "sec";
+		$page .= "</p>" ;
+		$page .= "<p><strong>Mémoires allouées PHP :</strong> " . $memory_limit . "</p>";
+		$page .= "<p><strong>Plugins :</strong> " ;
+		$page .= ($nombre_plugins_inutiles + $nombre_plugins ) . " | " ;
+		$page .= $nombre_plugins . " utilisés | " ;
+		$page .= $nombre_plugins_inutiles . " inutilisés | " ;
+		$page .= "</p>" ;
+		$page .= "</div>" ;
+
+	}
 	echo $page;
 }
 
@@ -339,6 +399,18 @@ function ancre_site($c) {
 		$c[$key] = "<a href='#$value'>" . $value . "</a>";
 	}
 	return $c;
+}
+
+function memoryUsage($bytes) {
+        $bytes = (int) $bytes;
+
+        if ($bytes > 1024*1024) {
+            return round($bytes/1024/1024, 2).' MB';
+        } elseif ($bytes > 1024) {
+            return round($bytes/1024, 2).' KB';
+        }
+
+        return $bytes . ' B';
 }
 
 ?>
