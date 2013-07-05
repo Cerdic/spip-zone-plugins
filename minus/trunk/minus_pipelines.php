24369ee186c5dada6b40a57a89de28a5b9301651
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-07-05 19:31:04 +0000

    on n'affiche le message qu'au premier post. On suppose qu'au second post l'auteur est conscient du problème
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@74114 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- minus/trunk/minus_pipelines.php
+++ minus/trunk/minus_pipelines.php
@@ -1,12 +1,20 @@
 <?
 if (!defined('_ECRIRE_INC_VERSION')) return;
-
+function minus_formulaire_charger($flux){
+	if ($flux["args"]["form"] == "editer_article"){
+		$titre = _request("titre");
+		if (prop_minus($titre) < 0.7){ // si moins de 70% de minuscule > il y a un souci -> on le marque en hidden
+		    $flux["data"]["_hidden"].="<input type='hidden' name='titre_trop_majusucules' value='oui' />\n";
+		}
+	}
+    return $flux;
+}
 function minus_formulaire_verifier($flux){
 	if ($flux["args"]["form"] == "editer_article" and !(_request("id_article") > 0)){
 		$titre = _request("titre");
-		if (prop_minus($titre) < 0.7){ // si moins de 70% de minuscule > il y a un souci
+		if (prop_minus($titre) < 0.7 and !_request("titre_trop_majusucules")){ // si moins de 70% de minuscule > il y a un souci ; mais si on n'a déjà signalé une fois, on ne le signale pas une seconde fois
 		    $flux["data"]["titre"] = _T("minus:trop_majuscule");
-			}
+		    }
 		}
 		
 	
