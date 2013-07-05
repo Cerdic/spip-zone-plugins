2c6e7a711725d3ea218eb0ca09c1946cf0c34d21
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-07-05 19:31:05 +0000

    mutualisation du test sur le nombre de majuscule
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@74115 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- minus/trunk/minus_pipelines.php
+++ minus/trunk/minus_pipelines.php
@@ -2,8 +2,7 @@
 if (!defined('_ECRIRE_INC_VERSION')) return;
 function minus_formulaire_charger($flux){
 	if ($flux["args"]["form"] == "editer_article"){
-		$titre = _request("titre");
-		if (prop_minus($titre) < 0.7){ // si moins de 70% de minuscule > il y a un souci -> on le marque en hidden
+		if (trop_majuscules()){ // si on a trop de majuscule, on le marque avec un hidden
 		    $flux["data"]["_hidden"].="<input type='hidden' name='titre_trop_majusucules' value='oui' />\n";
 		}
 	}
@@ -11,8 +10,7 @@ function minus_formulaire_charger($flux){
 }
 function minus_formulaire_verifier($flux){
 	if ($flux["args"]["form"] == "editer_article" and !(_request("id_article") > 0)){
-		$titre = _request("titre");
-		if (prop_minus($titre) < 0.7 and !_request("titre_trop_majusucules")){ // si moins de 70% de minuscule > il y a un souci ; mais si on n'a déjà signalé une fois, on ne le signale pas une seconde fois
+		if (trop_majuscules() and !_request("titre_trop_majusucules")){ // si ion a trop de majuscules, on l'affiche, sauf si on l'a déjà affiché une fois.
 		    $flux["data"]["titre"] = _T("minus:trop_majuscule");
 		    }
 		}
@@ -21,6 +19,11 @@ function minus_formulaire_verifier($flux){
 	return $flux;
     }
 
+function trop_majuscules(){
+	// return True s'il y a trop de majuscule dans le titre
+	$titre = _request("titre");
+	return (prop_minus($titre) < 0.7);
+	}
 function prop_minus($txt){
   // return la proportion de minuscule
   if (mb_strlen($txt, 'UTF-8') > 0){
