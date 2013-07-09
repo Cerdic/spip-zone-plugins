49902c19f0dc11be00b29391ea49cc8e13d996d3
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-07-09 09:56:28 +0000

    langues (mutu)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@74191 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- mutualisation/trunk/lang/mutu_es.php
+++ mutualisation/trunk/lang/mutu_es.php
@@ -1,39 +1,49 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier du plugin mutualisation
+// extrait automatiquement de http://trad.spip.net/tradlang_module/mutu?lang_cible=es
+// ** ne pas modifier le fichier **
 
-if (!defined("_ECRIRE_INC_VERSION")) return;
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// I
-'install_code' => 'Favor ingresar el c&oacute;digo de activaci&oacute;n del sitio:',
-'install_err' => 'Error...',
-'install_site' => 'Instalaci&oacute;n de tu sitio SPIP',
-'install_bd_echec' => 'La creaci&oacute;n de la base de datos @nombase@ fracas&oacute;.',
-'install_bd_reessayer' => 'Volver a intentar',
-'install_bd_cree' => 'La base de datos @nombase@ fue creada correctamente.',
-'install_spip_1' => 'Puedes ',
-'install_spip_2' => 'proseguir con la instalaci&oacute;n de SPIP',
-'install_creation_bd' => 'Creaci&oacute;n de la base de datos @nombase@',
-'install_creer_bd_1' => '&iquest;Quieres ',
-'install_creer_bd_2' => 'crear esta base?',
-'install_creation_bd_site' => 'Creaci&oacute;n de la base de datos del sitio ',
-'install_no_data_connexion' => 'Los datos de conexi&oacute;n @connexion@ no est&aacute;n definidos, imposible crear autom&aacute;ticamente la base.',
-'install_creation_repertoire' => 'Creaci&oacute;n de la carpeta del sitio (@repertoire@)',
-'install_repertoire_inaccessible' => 'No se tienen derechos de escritura en la carpeta @repertoire@',
-'install_creation_bd_site_2' => 'La base de datos @base@ fue creada para el sitio @site@.',
-'install_creer_rep_1' => '&iquest;Quieres ',
-'install_creer_rep_2' => 'crear las carpetas de este sitio?',
-'install_repertoire_noexist' => 'La carpeta del sitio (@repertoire@) no existe',
-'install_repertoire_creer' =>  'Favor crear la carpeta @repertoire@ y sus sub-carpetas:',
-'install_rep_bd_ok' => 'Las carpetas y la base de datos del sitio ya est&aacute;n creadas.',
-'install_creation_rep_ok_1' => 'Creaci&oacute;n de las carpetas OK. Puedes ',
-'install_creation_rep_ok_2' => 'continuar...',
-'install_creation_site' => 'Creaci&oacute;n del sitio @site@',
-'install_reps_crees' => 'Las carpetas del sitio @site@ fueron creadas.'
+	// I
+	'install_bd_cree' => 'La base de datos @nombase@ fue creada correctamente.',
+	'install_bd_echec' => 'La creación de la base de datos @nombase@ fracasó.',
+	'install_bd_reessayer' => 'Volver a intentar',
+	'install_code' => 'Favor ingresar el código de activación del sitio:',
+	'install_code_panel' => 'Veuillez entrer votre mot de passe habituel ou le code d\'activation fourni par l\'hébergeur :', # NEW
+	'install_creation_bd' => 'Creación de la base de datos @nombase@',
+	'install_creation_bd_site' => 'Creación de la base de datos del sitio ',
+	'install_creation_bd_site_2' => 'La base de datos @base@ fue creada para el sitio @site@.',
+	'install_creation_rep_ok_1' => 'Creación de las carpetas OK. Puedes ',
+	'install_creation_rep_ok_2' => 'continuar...',
+	'install_creation_repertoire' => 'Creación de la carpeta del sitio (@repertoire@)',
+	'install_creation_site' => 'Creación del sitio @site@',
+	'install_creer_bd_1' => '¿Quieres ',
+	'install_creer_bd_2' => 'crear esta base?',
+	'install_creer_rep_1' => '¿Quieres ',
+	'install_creer_rep_2' => 'crear las carpetas de este sitio?',
+	'install_err' => 'Error...',
+	'install_no_data_connexion' => 'Los datos de conexión @connexion@ no están definidos, imposible crear automáticamente la base.',
+	'install_rep_bd_ok' => 'Las carpetas y la base de datos del sitio ya están creadas.',
+	'install_repertoire_creer' => 'Favor crear la carpeta @repertoire@ y sus sub-carpetas:',
+	'install_repertoire_inaccessible' => 'No se tienen derechos de escritura en la carpeta @repertoire@',
+	'install_repertoire_noexist' => 'La carpeta del sitio (@repertoire@) no existe',
+	'install_reps_crees' => 'Las carpetas del sitio @site@ fueron creadas.',
+	'install_site' => 'Instalación de tu sitio SPIP',
+	'install_spip_1' => 'Puedes ',
+	'install_spip_2' => 'proseguir con la instalación de SPIP',
+	'install_spip_3' => 'Vous pouvez <a href="@url@">poursuivre l\'installation de SPIP</a>.', # NEW
 
+	// M
+	'message_site_desactive' => 'Veuillez contacter l\'<a href="@lien@">hébergeur</a> pour de plus amples détails', # NEW
+
+	// S
+	'site_non_active' => 'Ce site n\'a pas encore été validé par un administrateur.', # NEW
+	'site_non_demande' => 'Ce site n\'existe pas.', # NEW
+	'site_supprime' => 'Ce site est en attente de suppression définitive.', # NEW
+	'site_suspendu' => 'Ce site est suspendu.' # NEW
 );
 
 ?>
-
--- mutualisation/trunk/lang/mutu_fr.php
+++ mutualisation/trunk/lang/mutu_fr.php
@@ -1,49 +1,47 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier du plugin mutualisation
-
-if (!defined("_ECRIRE_INC_VERSION")) return;
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/mutualisation/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// I
-'install_code' => 'Veuillez entrer le code d\'activation du site :',
-'install_code_panel' => 'Veuillez entrer votre mot de passe habituel ou le code d\'activation fourni par l\'h&eacute;bergeur :',
-'install_err' => 'Erreur...',
-'install_site' => 'Installation de votre site SPIP',
-'install_bd_echec' => 'La cr&#233;ation de la base de donn&#233;es @nombase@ a &#233;chou&#233;.',
-'install_bd_reessayer' => 'R&#233;essayer...',
-'install_bd_cree' => 'La base de donn&#233;es @nombase@ a &#233;t&#233; cr&#233;&#233;e',
-'install_spip_1' => 'Vous pouvez ',
-'install_spip_2' => 'poursuivre l\'installation de SPIP',
-'install_spip_3' => 'Vous pouvez <a href="@url@">poursuivre l\'installation de SPIP</a>.',
-'install_creation_bd' => 'Cr&#233;ation de la base de donn&#233;es @nombase@',
-'install_creer_bd_1' => 'Voulez-vous ',
-'install_creer_bd_2' => 'cr&#233;er cette base ?',
-'install_creation_bd_site' => 'Cr&eacute;ation de la base de donn&#233;es du site ',
-'install_no_data_connexion' => 'Les donn&#233;es de connexion @connexion@ ne sont pas d&#233;finies, impossible de cr&#233;er automatiquement la base.',
-'install_creation_repertoire' => 'Cr&eacute;ation du r&eacute;pertoire du site (@repertoire@)',
-'install_repertoire_inaccessible' => 'Le r&#233;pertoire @repertoire@ n\'est pas accessible en &#233;criture',
-'install_creation_bd_site_2' => 'La base de donn&#233;es @base@ a &#233;t&#233; cr&#233;&#233;e pour le site @site@.',
-'install_creer_rep_1' => 'Voulez-vous ',
-'install_creer_rep_2' => 'cr&#233;er les r&#233;pertoires de ce site ?',
-'install_repertoire_noexist' => 'Le r&eacute;pertoire du site (@repertoire@) n\'existe pas',
-'install_repertoire_creer' =>  'Veuillez cr&#233;er le r&#233;pertoire @repertoire@ et ses sous r&#233;pertoires:',
-'install_rep_bd_ok' => 'Les r&#233;pertoires et la base de donn&#233;e du site sont maintenant cr&#233;&#233;s.',
-'install_creation_rep_ok_1' => 'Cr&#233;ation des r&#233;pertoires OK. Vous pouvez ',
-'install_creation_rep_ok_2' => 'continuer...',
-'install_creation_site' => 'Creation du site @site@',
-'install_reps_crees' => 'Les r&#233;pertoires du site @site@ ont &#233;t&#233; cr&#233;&#233;s.',
+	// I
+	'install_bd_cree' => 'La base de données @nombase@ a été créée',
+	'install_bd_echec' => 'La création de la base de données @nombase@ a échoué.',
+	'install_bd_reessayer' => 'Réessayer...',
+	'install_code' => 'Veuillez entrer le code d\'activation du site :',
+	'install_code_panel' => 'Veuillez entrer votre mot de passe habituel ou le code d\'activation fourni par l\'hébergeur :',
+	'install_creation_bd' => 'Création de la base de données @nombase@',
+	'install_creation_bd_site' => 'Création de la base de données du site ',
+	'install_creation_bd_site_2' => 'La base de données @base@ a été créée pour le site @site@.',
+	'install_creation_rep_ok_1' => 'Création des répertoires OK. Vous pouvez ',
+	'install_creation_rep_ok_2' => 'continuer...',
+	'install_creation_repertoire' => 'Création du répertoire du site (@repertoire@)',
+	'install_creation_site' => 'Creation du site @site@',
+	'install_creer_bd_1' => 'Voulez-vous ',
+	'install_creer_bd_2' => 'créer cette base ?',
+	'install_creer_rep_1' => 'Voulez-vous ',
+	'install_creer_rep_2' => 'créer les répertoires de ce site ?',
+	'install_err' => 'Erreur...',
+	'install_no_data_connexion' => 'Les données de connexion @connexion@ ne sont pas définies, impossible de créer automatiquement la base.',
+	'install_rep_bd_ok' => 'Les répertoires et la base de donnée du site sont maintenant créés.',
+	'install_repertoire_creer' => 'Veuillez créer le répertoire @repertoire@ et ses sous répertoires:',
+	'install_repertoire_inaccessible' => 'Le répertoire @repertoire@ n\'est pas accessible en écriture',
+	'install_repertoire_noexist' => 'Le répertoire du site (@repertoire@) n\'existe pas',
+	'install_reps_crees' => 'Les répertoires du site @site@ ont été créés.',
+	'install_site' => 'Installation de votre site SPIP',
+	'install_spip_1' => 'Vous pouvez ',
+	'install_spip_2' => 'poursuivre l\'installation de SPIP',
+	'install_spip_3' => 'Vous pouvez <a href="@url@">poursuivre l\'installation de SPIP</a>.',
 
-// M
-'message_site_desactive' => 'Veuillez contacter l\'<a href="@lien@">h&eacute;bergeur</a> pour de plus amples d&eacute;tails',
+	// M
+	'message_site_desactive' => 'Veuillez contacter l\'<a href="@lien@">hébergeur</a> pour de plus amples détails',
 
-// S
-'site_supprime' => 'Ce site est en attente de suppression d&eacute;finitive.',
-'site_suspendu' => 'Ce site est suspendu.',
-'site_non_active' => 'Ce site n\'a pas encore &eacute;t&eacute; valid&eacute; par un administrateur.',
-'site_non_demande' => 'Ce site n\'existe pas.',
+	// S
+	'site_non_active' => 'Ce site n\'a pas encore été validé par un administrateur.',
+	'site_non_demande' => 'Ce site n\'existe pas.',
+	'site_supprime' => 'Ce site est en attente de suppression définitive.',
+	'site_suspendu' => 'Ce site est suspendu.'
 );
 
 ?>
-
