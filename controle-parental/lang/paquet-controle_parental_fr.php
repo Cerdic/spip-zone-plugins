5fbfebcfda0eaeb87a0e529b94790b0afd38bdee
olivier.gautier@ac-rouen.fr <olivier.gautier@ac-rouen.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-07-10 11:20:07 +0000

    Déplacement du projet à un autre endroit
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@74216 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- controle-parental/lang/paquet-controle_parental_fr.php
+++
@@ -1,9 +0,0 @@
-<?php
-// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
-$GLOBALS[$GLOBALS['idx_lang']] = array(
-'controle_parental_slogan' => 'Mod&#232;le pr&#233;sentant des outils pour le contr&#244;le parental',
-'controle_parental_description' => 'Ce plugin vous propose un mod&#232;le &#60;&nbsp;controle-parental|&nbsp;&#62; proposant des informations concernant la gestion du contr&#244;le parental.
-Les textes ont &#233;t&#233; cr&#233;&#233;s par le [GAPASE->http://www.ac-rouen.fr/l-academie/politiques-educatives/le-gapase-42196.kjsp] de l\'acad&#233;mie de Rouen, sous l\'autorit&#233; du [Conseiller s&#233;curit&#233; aupr&#232;s du recteur->http://www.ac-rouen.fr/boite-a-outils/annuaires/conseiller-securite-aupres-du-recteur-8402.kjsp] de l\'acad&#233;mie de Rouen.',
-);
-?>
