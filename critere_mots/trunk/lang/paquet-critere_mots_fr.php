8970e9afee264081fafe2d088b79047a9fce8490
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-07-12 03:37:02 +0000

    langues (paquet-critere_mots)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@74239 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- critere_mots/trunk/lang/paquet-critere_mots_fr.php
+++ critere_mots/trunk/lang/paquet-critere_mots_fr.php
@@ -1,19 +1,13 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-critere_mots
-// Langue: fr
-// Date: 01-08-2012 13:09:46
-// Items: 2
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/critere_mots/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// C
+	// C
 	'critere_mots_description' => 'Permet d\'afficher les éléments qui ont plusieurs mots clés en communs, via la variable $mots[] passée dans l\'environnement',
-	'critere_mots_slogan' => 'Sélectionner des objets ayant des mots clés en communs',
+	'critere_mots_slogan' => 'Sélectionner des objets ayant des mots clés en communs'
 );
+
 ?>
