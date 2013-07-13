f4ab5f6800c18bdb8257f772f597f11bcb4161ef
amaury.adon@laposte.net <amaury.adon@laposte.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-07-13 21:55:39 +0000

    on version les événements à l'installation du plugin (merci denisb) et on commence à modifier le formulaire pour l'ajout obligatoire d'un mot clé lors de la création (pas encore fonctionnel).
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@74253 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- seminaire/trunk/seminaire_administrations.php
+++ seminaire/trunk/seminaire_administrations.php
@@ -9,6 +9,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 
 	include_spip('inc/cextras');
 	include_spip('base/seminaire');
+	include_spip('inc/meta');
 
 
 /**
@@ -17,13 +18,22 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 function seminaire_upgrade($nom_meta_base_version, $version_cible) {
 	$maj = array();
 	cextras_api_upgrade(seminaire_declarer_champs_extras(), $maj['create']);
-	/**activer les mots clés et leur configuration avancée s'ils ne le sont pas déjà**/
+/**activer les mots clés et leur configuration avancée s'ils ne le sont pas déjà**/
 	if ($GLOBALS['meta']['articles_mots']!=oui){
 	 	ecrire_meta("articles_mots", "oui");
 	 	ecrire_meta("config_precise_groupes", "oui");
 	 	ecrire_meta("documents_objets", "spip_evenements");	
 	 	}
- 	/**Creer le groupe de mots clés Type pour les types d'événements**/
+/**activer les révisions sur les événements**/
+	// ce qui existe déjà
+	$versions = unserialize($GLOBALS['meta']['objets_versions']);
+	// ce que j'ajoute
+	$versionnage_des_evenements = array('spip_evenements');
+	// merge des 2
+	$versions = array_merge($versions,$versionnage_des_evenements);
+	// balançons dans meta
+	ecrire_meta('objets_versions',serialize($versions)); 
+ /**Creer le groupe de mots clés Type pour les types d'événements**/
 	if (sql_countsel('spip_mots', "titre IN ('seminaire','groupe de travail','evenement important')") == 0) 
    	{
 			$id_groupe = sql_insertq('spip_groupes_mots', 
@@ -40,7 +50,7 @@ function seminaire_upgrade($nom_meta_base_version, $version_cible) {
 		if (sql_error() != '') $Terreur[] = (_T('erreur_creation_mot_cle')).$st.': '.sql_error();
     	};
 	};
-	/** création du groupe de mots clés Catégorie et de ses mots cles pours les équipes **/
+/** création du groupe de mots clés Catégorie et de ses mots cles pours les équipes **/
     if (sql_countsel('spip_mots', "titre IN ('Algèbre, Dynamique et Topologie','Analyse Appliquée', 'Analyse et Géométrie', 'FRUMAM', 'Géométrie et Singularités', 'Guide d’ondes et milieux stratiﬁés', 'Probabilités et statistiques', 'Séminaire des doctorants', 'Théorie des nombres')") == 0) 
     {
         $id_groupe = sql_insertq('spip_groupes_mots',array('titre'=>'Catégorie', 'descriptif'=> _T('seminaire:mots_cles_categories'), 'tables_liees'=>'articles', 'minirezo'=>'oui','comite'=>'oui')
@@ -57,10 +67,10 @@ function seminaire_upgrade($nom_meta_base_version, $version_cible) {
     }
 
 	$maj['1.0.1'] = array(
-	/*Copie de abstract vers descriptif*/
+/*Copie de abstract vers descriptif*/
 	array('sql_update',"spip_evenements", array('descriptif'=>'abstract')),
 	array('sql_alter',"TABLE spip_evenements DROP abstract"),
-	/*on change name en attendee*/
+/*on change name en attendee*/
 	array('sql_alter',"TABLE spip_evenements ADD attendee text NOT NULL"),
 	array('sql_update',"spip_evenements", array('attendee'=>'name')),
 	array('sql_alter',"TABLE spip_evenements DROP name"),
--- seminaire/trunk/seminaire_pipelines.php
+++ seminaire/trunk/seminaire_pipelines.php
@@ -4,4 +4,14 @@ function seminaire_insert_head_css($flux) {
     $flux .= "<link rel='stylesheet' type='text/css' media='all' href='$css' />\n";
     return $flux;
 }
+
+function seminaire_post_insertion($flux) {
+	if ($flux['args']['table'] == 'spip_evenements') {
+		sql_insertq("spip_mots_liens", array(
+			'id_mot' => _request('id_mot'),
+			'id_article' =>$flux['args']['id_objet'],
+			'objet' =>'evenement'));
+	}
+}
+
 ?>
