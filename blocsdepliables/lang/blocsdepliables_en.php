9c2e81db7850f4d055349f916882bf5c2764c817
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-07-14 03:36:33 +0000

    langues (blocsdepliables)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@74256 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ blocsdepliables/lang/blocsdepliables_en.php
@@ -0,0 +1,37 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/blocsdepliables?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'blocsdepliables_titre' => 'Expanding containers blocks',
+
+	// C
+	'cfg_exemple' => 'Example',
+	'cfg_exemple_explication' => 'Example explanation',
+	'cfg_titre_parametrages' => 'Parameters',
+
+	// E
+	'explication_cookie' => 'L\'état des blocs numérotés sera stocké dans un cookie le temps de la session, afin de conserver l’aspect de la page en cas de retour.', # NEW
+	'explication_unique' => 'L\'ouverture d’un bloc provoquera la fermeture de tous les autres blocs de la page, afin d’en avoir qu’un seul ouvert à la fois.', # NEW
+
+	// L
+	'label_animation' => 'Animation',
+	'label_animation_aucun' => 'None',
+	'label_animation_fast' => 'Fast slide',
+	'label_animation_millisec' => 'Slide duration :',
+	'label_animation_normal' => 'Normal slide',
+	'label_animation_slow' => 'Slow slide',
+	'label_balise_titre' => 'Tag for the blocks title',
+	'label_cookie_1' => 'Remember the blocks state',
+	'label_unique_1' => 'Un seul bloc ouvert sur la page', # NEW
+
+	// T
+	'titre_page_configurer_blocsdepliables' => 'Expanding containers blocks'
+);
+
+?>
