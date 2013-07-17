cf8b91ecdfa7f9d766cd672d27558cabafa1c04c
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-07-17 00:55:09 +0000

    forme_livre : lister automatiquement à partir des ficer automatiquement à partir des fichier dans le path de SPIP
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@74329 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spip-bible/trunk/inc/bible_tableau.php
+++ spip-bible/trunk/inc/bible_tableau.php
@@ -17,7 +17,6 @@ function bible_tableau($i,$lang=''){
 			'options'=>array('na28','na27')
 		)
 	);
-	$forme_livre = array('abbr','complete','raccourcie');
 	$nombre_chapitres = array(50,40,27,36,34,24,21,4,31,24,22,25,29,36,10,13,14,16,10,16,15,42,150,31,51,8,1,51,66,52,5,6,48,13,14,3,9,1,4,7,3,3,3,2,14,14,28,16,24,21,28,16,16,13,6,6,4,4,5,3,6,4,3,1,13,5,5,3,5,1,1,1,22);
 	
 	$deutero = array(17,18,19,20,21,28,32);
@@ -2988,8 +2987,6 @@ $tableau_separateur = array(
 			return $livre_gateways;
 		case 'livres':
 			return $tableau_livres;
-		case 'forme_livre':
-			return $forme_livre;
 		case 'original':
 			return $tableau_langue_original;
 		case 'unbound':
