6b9a196a603b9b3d37c46218c897e1505e122f62
booz@rezo.net <booz@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-07-19 19:53:56 +0000

    oups
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@74382 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- plugins_seenthis/autoembed/autoembed.php
+++ plugins_seenthis/autoembed/autoembed.php
@@ -84,11 +84,11 @@ function embed_url($url) {
 				if ($html) $code_ae = "<div class='oembed-container'>$html</div>";
 			}
 		}
-		else if (preg_match(",^http://[^\"\'\`\<\>\@\*\$]*?\.mp3$,i", $url)) {
+		else if (preg_match(",^https?://[^\"\'\`\<\>\@\*\$]*?\.mp3$,i", $url)) {
 
 			$html = file_get_contents(dirname(__FILE__).'/modeles/mp3.html');
 			$html = str_replace('{source}', htmlspecialchars($url), $html);
-			$url_dewplayer = 'http://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['SCRIPT_NAME']).'/modeles/dewplayer.swf';
+			$url_dewplayer = 'http://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['SCRIPT_NAME']).'/autoembed/modeles/dewplayer.swf';
 			$html = str_replace('{dewplayer}', $url_dewplayer, $html);
 			if ($html) $code_ae = "<div class='oembed-container'>$html</div>";
 		}
