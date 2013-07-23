e729286710cd2d88bc34a16a6d4a28504e538ce2
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-07-23 22:45:06 +0000

    À minima, utiliser la manière d'installer de la 2.1 avec la fonction upgrade() directement (à l'aveugle, en espérant de rien casser).
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@74440 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- elearning/base/elearning_installation.php
+++ elearning/base/elearning_installation.php
@@ -8,66 +8,28 @@
 #-----------------------------------------------------------------#
 
 // La fonction de base appelée par le gestionnaire de plugins
-function elearning_install($install){
+function elearning_upgrade($nom_meta_version_base, $version_cible){
+	include_spip('inc/meta');
+	include_spip('base/abstract_sql');
 	
-	//recupère les informations de plugin.xml
-	include_spip('inc/plugin');
-	$infos = plugin_get_infos(_DIR_PLUGIN_ELEARNING);
-	$version_du_plugin = $infos['version'];
+	$current_version = 0.0;
 	
-	switch($install) {
-		case 'test':
-			return isset($GLOBALS['meta']['elearning_version_base']) AND ($GLOBALS['meta']['elearning_version_base']>=$version_du_plugin);
-		case 'install':
-			elearning_maj();
-			break;
-		case 'uninstall':
-			elearning_vider_tables();
-			break;
+	if (
+		!isset($GLOBALS['meta'][$nom_meta_version_base])
+		or
+		(($current_version = $GLOBALS['meta'][$nom_meta_version_base]) != $version_cible)
+	){
+		if (version_compare($current_version, '0.0', '<=')){
+			ecrire_meta($nom_meta_version_base, $current_version=$version_cible, 'non');
+		}
 	}
-	
 }
 
 // Supprimer les tables du plugin
-function elearning_vider_tables() {
+function elearning_vider_tables($nom_meta_version_base) {
 	//include_spip('base/abstract_sql');
 	/* Blabla effacer les tables */
-	effacer_meta('elearning_version_base');
-}
-
-// Met à jour le numéro de version dans les métas
-function elearning_maj_version(&$v1, $v2) {
-	echo "MAJ E-Learning : $v1 =&gt; $v2<br />";
-	ecrire_meta('elearning_version_base', $v1=$v2, 'non');
-}
-
-// Là où se trouve les mises à jour
-function elearning_maj(){
-	
-	//recupère les informations de plugin.xml
-	include_spip('inc/plugin');
-	$infos = plugin_get_infos(_DIR_PLUGIN_ELEARNING);
-	$version_du_plugin = $infos['version'];
-	$version_en_cours = isset($GLOBALS['meta']['elearning_version_base'])
-		?$GLOBALS['meta']['elearning_version_base']
-		:0.0;
-	
-	// On met à jour seulement si les versions sont différentes
-	if ($version_en_cours != $version_du_plugin){
-		
-		//include_spip('base/create');
-		//include_spip('base/abstract_sql');
-				
-		if ($version_en_cours == 0.0){
-			elearning_maj_version($version_en_cours, 0.1);
-		}
-		
-		/*if ($current_version < ($version_de_test = 0.11)){
-			//$desc = $showtable("spip_jeux", true);
-			
-			elearning_maj_version($current_version, $test_version);
-		}*/
-	}
+	effacer_meta($nom_meta_version_base);
 }
 
 ?>
