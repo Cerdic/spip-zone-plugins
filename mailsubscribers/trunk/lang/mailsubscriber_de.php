6958cb6c402e298040085843b05d294f4047f589
klaus@spip.de <klaus@spip.de@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-07-25 09:11:36 +0000

    premiere version en allemand
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@74456 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ mailsubscribers/trunk/lang/mailsubscriber_de.php
@@ -0,0 +1,98 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'bouton_importer' => 'Importieren',
+	'bouton_previsu_importer' => 'Vorschau',
+
+	// C
+	'confirmsubscribe_sujet_email' => '[@nom_site_spip@] Bestätigung Newsletter-Anmeldung',
+	'confirmsubscribe_titre_email' => 'Bestätigung der Newsletter-Anmeldung',
+	'confirmsubscribe_texte_email_1' => 'Sie haben sich mit der Adresse @email@ für den Bezug des Newsletters angemeldet.',
+	'confirmsubscribe_texte_email_2' => 'Um die Anmeldung abzuschließen, klicken Sie bitte auf den Link:
+@url_confirmsubscribe@',
+	'confirmsubscribe_texte_email_3' => 'Sollte es sich um einen Irrtum handeln, oder Sie es sich anders überlegt haben, brauchen Sie nichts zu unternehmen. Die Anmeldung wird automatisch gelöscht.',
+	'confirmsubscribe_texte_email_envoye' => 'Eine Bestätigungsmail wurde an die Mailadresse geschickt.',
+
+	// E
+	'erreur_adresse_existante' => 'Diese Adresse ist bereits in der Liste eingetragen.',
+	// I
+	'icone_creer_mailsubscriber' => 'Abeonnement hinzufügen',
+	'icone_modifier_mailsubscriber' => 'Abonnement ändern',
+	'info_1_mailsubscriber' => '1 Abonnent',
+	'info_aucun_mailsubscriber' => 'Keine Abonnenten',
+	'info_nb_mailsubscribers' => '@nb@ Abonnenten',
+	'info_1_adresse_a_importer' => '1 Adresse importieren',
+	'info_nb_adresses_a_importer' => '@nb@ Adressen importieren',
+	'info_statut_prepa' => 'nicht angemeldet',
+	'info_statut_prop' => 'wartend',
+	'info_statut_valide' => 'angmeldet',
+	'info_statut_refuse' => 'abgelehnt',
+	'info_statut_poubelle' => 'gelöscht',
+
+	// L
+	'label_listes' => 'Listen',
+	'label_email' => 'Email',
+	'label_lang' => 'Sprache',
+	'label_nom' => 'Name',
+	'label_optin' => 'Opt-in',
+	'label_statut' => 'Status',
+	'label_mailsubscriber_optin' => 'Ich möchten den Newsletter erhalten',
+	'label_file_import' => 'Dateiimport',
+	'label_toutes_les_listes' => 'Alle',
+	'label_desactiver_notif_1' => 'Benachrichtigung über die Anmeldung bei diesem Importvorgang deaktivieren',
+	'label_vider_table_1' => 'Alle Adressen in der Datenbank vor dem Import löschen',
+
+	// M
+	'mailsubscribers_tous' => 'Alle',
+	'mailsubscribers_valide' => 'Angemeldet',
+	'mailsubscribers_prepa' => 'Nicht angemeldet',
+	'mailsubscribers_prop' => 'Unbestätigt',
+	'mailsubscribers_refuse' => 'Abgemeldet',
+	'mailsubscribers_poubelle' => 'Gelöscht',
+
+	// S
+	'subscribe_sujet_email' => '[@nom_site_spip@] Newsletter-Anmeldung',
+	'subscribe_titre_email' => 'Newsletter-Anmeldung',
+	'subscribe_texte_email_1' => 'Ihre Nesletter-Anmeldung mit der Adresse email @email@ wurde entgegengenommen.',
+	'subscribe_deja_texte' => 'Die Mailadresse @email@ befindet sich bereits in der Empfängerliste.',
+	'subscribe_texte_email_2' => 'Vielen Dank für Ihr Interesse an @nom_site_spip@.',
+	'subscribe_texte_email_3' => 'Falls ein Irrtum vorliegt, oder Sie Ihre Meinung ändern, können Sie sich jederzeit bei der folgenden Adresse abmelden : @url_unsubscribe@',
+
+	// U
+	'unsubscribe_sujet_email' => '[@nom_site_spip@] Newsletter Abmeldung',
+	'unsubscribe_titre_email' => 'Newsletter Abmeldung',
+	'unsubscribe_texte_confirmer_email_1' => 'Bitte bestätigen Sie die Abmeldung der Adresse email @email@ durch einen Klick auf die Schaltfläche: ',
+	'unsubscribe_texte_email_1' => 'Die Adresse email @email@ wurde aus der Liste entfernt.',
+	'unsubscribe_deja_texte' => 'Die Adresse email @email@ befindet sich nicht in der Liste.',
+	'unsubscribe_texte_email_2' => 'Bitte besuchen Sie @nom_site_spip@ bald wieder.',
+	'unsubscribe_texte_email_3' => 'Falls ein Irtum vorliegt, oder Sie Ihre Meinung ändern, können Sie sich jederzeit unter der folgenden Adresse wieder anmelden:
+@url_subscribe@',
+
+	// T
+	'texte_ajouter_mailsubscriber' => 'Newsletter-Abonnent hinzufügen',
+	'texte_avertissement_import' => 'Die Spalte <tt>Status</tt> wird zur bereitgestellt: Daten werden unverändert importiert, vorhandene Mailadressen werden gelöscht.',
+	'texte_changer_statut_mailsubscriber' => 'Dieser Abonnent ist :',
+	'titre_langue_mailsubscriber' => 'Sprache des Abonnenten',
+	'titre_logo_mailsubscriber' => 'Logo des Abonnenten',
+	'titre_mailsubscriber' => 'Abonnent',
+	'titre_mailsubscribers' => 'Abonnenten',
+	'titre_export_mailsubscribers' => 'Abonnenten exportieren',
+	'titre_export_mailsubscribers_all' => 'Alle Adressen exportieren',
+	'titre_import_mailsubscribers' => 'Adressen importieren',
+	'titre_listes_de_diffusion' => 'Newsletter',
+
+
+	'texte_statut_pas_encore_inscrit' => 'nicht angemeldet',
+	'texte_statut_en_attente_confirmation' => 'unbestätigt',
+	'texte_statut_valide' => 'aktiv',
+	'texte_statut_refuse' => 'abgelehnt',
+
+);
+
+?>
---
+++ mailsubscribers/trunk/lang/paquet-mailsubscribers_de.php
@@ -0,0 +1,13 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// M
+	'mailsubscribers_description' => 'E-Mail Newsletter-Anmeldung',
+	'mailsubscribers_slogan' => 'E-Mail Newsletter-Anmeldung',
+);
+
+?>
