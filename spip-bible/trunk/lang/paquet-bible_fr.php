ce5a5dcc4f2f1a8fce2f630e32a75a4875714593
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-07-27 14:39:08 +0000

    raccourcissement du descriptif, les détails sont dans la doc
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@74483 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spip-bible/trunk/lang/paquet-bible_fr.php
+++ spip-bible/trunk/lang/paquet-bible_fr.php
@@ -13,19 +13,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 // B
-	'bible_description' => 'Le plugin Spip-Bible permet de citer rapidement des extraits de la Bible dans le texte d’un article. Sous SPIP 2.0 avec SPIP-Bonux, propose un "presse-papier" biblique à côté des formulaires d\'éditions. Vous pouvez aussi utilisez un modéle, mais cette méthode est désuéte et déconseillée.
-
-Ex : <code><bible|passage=Gn1,2-2,1></code> citera la Genèse du chapitre 1, verset 2 au chapitre 2, verset 1 inclu.
-
-On peut passer des options au modéle. La valeur des options est "non" ou "oui", sauf pour traduction.
-
-Ces options sont :
--* numeros : Pour afficher les numéros de versets et de chapitre dans le corps de texte.
--* retour : Pour faire des retours chariots entre les versets
--* ref : Pour afficher les références à la suite du passage
--* traduction : Pour choisir la traduction.
-
-La liste des traductions disponibles est sur [Spip-Contrib->http://www.spip-contrib.net/Spip-Bible-traductions-disponibles]',
+	'bible_description' => 'Le plugin Spip-Bible permet de citer rapidement des extraits de la Bible dans le texte d’un article. Soit avec un modèle, soit avec un copier-coller.',
 	'bible_slogan' => 'Pour faire rapidement des citations de la Bible',
 );
 ?>
