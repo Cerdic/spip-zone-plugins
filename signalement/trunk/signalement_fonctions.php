410dfd853d047d3be37de17586cd9d57f67db7f9
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-07-28 20:13:18 +0000

    Avoir un contexte un peu plus précis dans la liste de signalement permettant par exemple d'avoir un signalement pour un objet particulier
    Version 1.2.0 stable et 2012-2013
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@74497 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- signalement/trunk/signalement_fonctions.php
+++ signalement/trunk/signalement_fonctions.php
@@ -13,7 +13,7 @@
 
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
-function lister_signalements(){
+function lister_signalements($objet='',$id_objet=''){
 	$signalements = array(
 		'actes_danger' => _T('signalement:motif_option_actes_danger'),
 		'enfants' => _T('signalement:motif_option_enfants'),
@@ -24,9 +24,15 @@ function lister_signalements(){
 		'violence' => _T('signalement:motif_option_violence'),
 		'autre' => _T('signalement:motif_option_autre')
 	);
-	
-	pipeline('signalement_liste',$signalements);
-	
+	$signalements = pipeline('signalement_liste',
+		array(
+			'args' => array(
+				'objet'=>$objet,
+				'id_objet'=>$id_objet
+			),
+			'data' => $signalements
+		)
+	);
 	return $signalements;
 }
 ?>
