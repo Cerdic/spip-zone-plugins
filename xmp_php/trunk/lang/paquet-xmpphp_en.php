ae9c2bdba0020cea821ace8810f54f2f6f436e19
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-07-29 17:19:38 +0000

    Début du passage en SPIP 3.0 via plugonet
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@74510 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ xmp_php/trunk/lang/paquet-xmpphp_en.php
@@ -0,0 +1,19 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-xmpphp
+// Langue: en
+// Date: 29-07-2013 19:16:17
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// X
+	'xmpphp_description' => 'Retrieval of documents XMP datas',
+	'xmpphp_slogan' => 'Retrieval of documents XMP datas',
+);
+?>
---
+++ xmp_php/trunk/lang/paquet-xmpphp_fr.php
@@ -0,0 +1,19 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-xmpphp
+// Langue: fr
+// Date: 29-07-2013 19:16:17
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// X
+	'xmpphp_description' => 'Récupération des données XMP des documents',
+	'xmpphp_slogan' => 'Récupération des données XMP des documents',
+);
+?>
