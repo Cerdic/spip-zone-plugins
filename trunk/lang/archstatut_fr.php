b85078d4b34f4d22fe3f0cadc47c194cf78d5d37
luc.tech@free.fr <luc.tech@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-08-03 18:02:26 +0000

    Création du plugin. Testé en local. Avis et retours sur le principe et le codage sont bienvenus ! Archivage d'articles et de brèves par l'ajout d'un statut "archive" ("archiv" pour les brèves) manipulable comme les autres statuts. Ils ne sont pas supprimés mais n'apparaissent plus sur le site public. Dans l'interface privée, des pavés "liste d'articles archivés" ont été rajoutés (rubrique, mot, auteurs).'
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@74561 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ trunk/lang/archstatut_fr.php
@@ -0,0 +1,25 @@
+﻿<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'archstatut_titre' => 'Archivage titre',
+	
+	// I
+	'info_article_archive' => 'Article archivé',
+	'info_breve_archive' => 'Brève archivée',
+	'info_tous_articles_archives' => 'Tous les article archivés',
+	'info_tous_breves_archives' => 'Toutes les brèves archivées',
+	
+	// T
+	'titre_page_configurer_archivage' => 'Archivage',
+	'texte_statut_archive' => 'archivé',
+	'texte_statut_archive_breve' => 'archivée',
+
+);
+
+?>
