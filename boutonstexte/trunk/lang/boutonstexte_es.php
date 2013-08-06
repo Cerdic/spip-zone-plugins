3cd69d8b3d4117c610c1f2052acfa74d4a30a865
severo@rednegra.net <severo@rednegra.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-08-06 03:40:21 +0000

    langues (boutonstexte)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@74586 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ boutonstexte/trunk/lang/boutonstexte_es.php
@@ -0,0 +1,40 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/boutonstexte?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'augmenter_police' => 'Aumentar fuente',
+
+	// B
+	'boutonstexte_nom' => 'Botones en el Texto',
+
+	// D
+	'diminuer_police' => 'Disminuir fuente',
+	'donnees_techniques' => 'Datos técnicos - no modificar',
+
+	// L
+	'label_selector' => 'Expresión CSS o extendida jQuery',
+	'label_skin' => 'Apariencia de los botones',
+	'label_txtBackSpip' => 'Texto de retorno spip',
+	'label_txtOnly' => 'Solamente texto *',
+	'label_txtSizeDown' => 'Texto disminuir *',
+	'label_txtSizeUp' => 'Texto aumentar *',
+	'legend_apparence' => 'Tema gráfico', # a mejorar, quizás
+
+	// R
+	'retour_a_spip' => 'Volver a la página completa',
+
+	// S
+	'selecteur' => 'Selector del texto a procesar',
+
+	// T
+	'texte_seulement' => 'Solamente texto',
+	'textes_icones' => 'Textos de los tooltips', # a mejorar quizás
+);
+
+?>
