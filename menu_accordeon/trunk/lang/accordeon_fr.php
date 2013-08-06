94a78a41858f30d99c35d7104890ce55f9cdcddf
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-08-06 03:58:45 +0000

    pouvoir définir les options du menu directement au niveau du squelette, et non pas au niveau du CFG. Utile pour mettre en œuvre des accordéons dans des squelettes génériques. Pour ce faire, créer les squelettes accordeon/identifiant.html et accordeon/options.html.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@74590 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- menu_accordeon/trunk/lang/accordeon_fr.php
+++ menu_accordeon/trunk/lang/accordeon_fr.php
@@ -9,8 +9,9 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	
 	// E
 	'explication_identifiant'=>'.class ou #identifiant de votre menu, par défaut .accordeon',
+	'explication_identifiant_ver'=>'.class ou #identifiant de votre menu, verrouillé. Modifiable par le squelette <code>accordeon/identifiant.html</code>.',
 	'explication_options' => 'Vous pouvez mettre ici les options à passer au script d\'effet accordéon. <a href="http://jqueryui.com/demos/accordion/">Voir la documentation d\'accordéon</a>. Ne pas mettre les accolades. Saisir par exemple <br /><strong>collapsible: true, active: $(".accordeon > li.on > a,.accordeon > li:first-child > a").last()</strong><br /> pour ouvrir le menu de classe accordeon sur l\'élément courant ou sinon sur le premier du DOM',
-
+	'explication_options_ver' => 'Les options passée au script d\'effet accordéon sont verrouillées. Le webmestre peut les changer en modifiant le squelette <code>accordeon/options.html</code>. <a href="http://jqueryui.com/demos/accordion/">Voir la documentation d\'accordéon</a>.',
 	// L
 	'label_identifiant'=>'Identifiant du menu',
 	'label_options' => 'Animation',
