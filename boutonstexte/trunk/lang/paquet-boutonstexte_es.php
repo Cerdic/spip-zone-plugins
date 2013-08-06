b76ef2080923ce05eb55c28d3bb8e1240a37c0dc
severo@rednegra.net <severo@rednegra.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-08-06 03:40:22 +0000

    langues (paquet-boutonstexte)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@74587 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ boutonstexte/trunk/lang/paquet-boutonstexte_es.php
@@ -0,0 +1,18 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-boutonstexte?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'boutonstexte_description' => 'Añade botones para modificar el aspecto de los textos, como aumentar/disminuir la fuente. Los botones están insertados antes de cada div de clase texte del contenido, a no ser que ya estén en la página (imágenes de clase textsizeup, textsizedown o textonly).
+
+La configuración opcional se puede habilitar instalando el plugin cfg. Ciertos elementos pueden ser forzados por javascript.',
+	'boutonstexte_nom' => 'Botones en el Texto',
+	'boutonstexte_slogan' => 'Botones para manejar el tamaño de los carácteres'
+);
+
+?>
