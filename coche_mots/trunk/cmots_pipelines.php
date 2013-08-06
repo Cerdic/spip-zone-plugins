21000ea7f55bac8bb1b059bfc6695d2c7220d6c7
joseph@larmarange.net <joseph@larmarange.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-08-06 15:52:09 +0000

    Vérifier également que l'on a le droit de modifier un objet
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@74603 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- coche_mots/trunk/cmots_pipelines.php
+++ coche_mots/trunk/cmots_pipelines.php
@@ -5,15 +5,20 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 
 function cmots_recuperer_fond($flux){
 	if ($flux['args']['fond'] == 'prive/objets/liste/mots_associer-recherche' OR $flux['args']['fond'] == 'prive/objets/liste/mots_associer-select') {
-		$flux['data']['texte'] = recuperer_fond(
-			'prive/inclure/coche_mots',
-			array(
-				'id_groupe' => $flux['args']['contexte']['id_groupe'],
-				'id_objet' => $flux['args']['contexte']['id_objet'],
-				'objet' => $flux['args']['contexte']['objet'],
-				'retour' => self() 
-			)
-		).$flux['data']['texte'];
+		$id_objet = $flux['args']['contexte']['id_objet'];
+		$objet = $flux['args']['contexte']['objet'];
+		$id_groupe = $flux['args']['contexte']['id_groupe'];
+		if (autoriser('modifier',$objet,$id_objet)) {
+			$flux['data']['texte'] = recuperer_fond(
+				'prive/inclure/coche_mots',
+				array(
+					'id_groupe' => $id_groupe,
+					'id_objet' => $id_objet,
+					'objet' => $objet,
+					'retour' => self() 
+				)
+			).$flux['data']['texte'];
+		}
 	}
 	return $flux;
 }
