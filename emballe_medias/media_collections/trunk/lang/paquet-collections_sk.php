a4daaf750a9c0163769240c93e26291851a69e9b
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-08-08 03:39:01 +0000

    langues (paquet-collections)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@74632 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ emballe_medias/media_collections/trunk/lang/paquet-collections_sk.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-collections?lang_cible=sk
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'collections_description' => 'Vytvorí kolekcie z multimédií, albumov, atď.',
+	'collections_nom' => 'Kolekcie z multimédií',
+	'collections_slogan' => 'Tvorte kolekcií z multimédií'
+);
+
+?>
