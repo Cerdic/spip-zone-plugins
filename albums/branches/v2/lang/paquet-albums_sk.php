373ca993e43d22bcae58da29767d63f09f53224c
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-08-08 03:38:37 +0000

    langues (paquet-albums)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@74621 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ albums/branches/v2/lang/paquet-albums_sk.php
@@ -0,0 +1,19 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-albums?lang_cible=sk
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'albums_description' => 'Ce plugin ajoute un nouvel objet éditorial \'album\' que l\'on peut lier aux autres objets.
+		Les albums sont des conteneurs pour les documents : ils vous permettent de réaliser des groupes de documents.
+		L\'insertion de séries de documents au fil du texte s\'en trouve simplifiée.', # NEW
+	'albums_nom' => 'Albumy',
+	'albums_slogan' => 'Zoskupte svoje dokumenty do albumov',
+	'albums_titre' => 'Albumy'
+);
+
+?>
