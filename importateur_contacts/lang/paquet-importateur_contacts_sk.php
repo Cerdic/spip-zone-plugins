8899cf9ac706472bd968f8ed23341c2dc33b8aed
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-08-08 03:39:06 +0000

    langues (paquet-importateur_contacts)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@74634 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ importateur_contacts/lang/paquet-importateur_contacts_sk.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-importateur_contacts?lang_cible=sk
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// I
+	'importateur_contacts_description' => 'Umožní vám nahrať kontakty z viacerých internetových služieb.',
+	'importateur_contacts_nom' => 'Nahrávač kontaktov',
+	'importateur_contacts_slogan' => 'Formulár na nahrávanie kontaktov'
+);
+
+?>
