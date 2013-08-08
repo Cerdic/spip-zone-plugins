5d654173aa77bb0197f526d4ab751a5f1a4ca673
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-08-08 03:39:19 +0000

    langues (paquet-squirrel_chimp)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@74640 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ squirrels_love_chimps/trunk/lang/paquet-squirrel_chimp_sk.php
@@ -0,0 +1,23 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-squirrel_chimp?lang_cible=sk
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// S
+	'squirrel_chimp_description' => 'Ce plugin constitue le coeur d\'un set de plugins qui on pour but de d\'intégerer connecter SPIP  avec [MailChimp->http://mailchimp.com/]
+	
+		Il met à disposition l\'api de Mailchimp ainsi qu\'un framework permettant d\'ajouter facilement d\'autre plugins
+		
+		Existent pour le moment les plugins suivants
+		-* [Squirrels Love Chimps Lists->http://files.spip.org/spip-zone/squirrels_love_chimps_lists.zip](squirrel_chimp_lists)
+		-* [Squirrels Love Chimps Lists us->http://files.spip.org/spip-zone/squirrels_love_chimps_lists_plus.zip](squirrel_chimp_lists_plus)	
+		-* [Squirrels Love Chimps Campagnes->http://files.spip.org/spip-zone/squirrels_love_chimps_campaigns.zip](squirrel_chimp_campaigns)', # NEW
+	'squirrel_chimp_nom' => 'Squirrels Love Chimps = Veveričky majú rady šímpanzov',
+	'squirrel_chimp_slogan' => 'Prihlásiť sa na SPIP cez Mailchimp'
+);
+
+?>
