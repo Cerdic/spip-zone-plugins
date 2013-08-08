72de2a05baf69c302f97e0bbb5452f189b3e8664
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-08-08 03:39:21 +0000

    langues (paquet-squirrel_chimp_lists)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@74641 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ squirrels_love_chimps_lists/trunk/lang/paquet-squirrel_chimp_lists_sk.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-squirrel_chimp_lists?lang_cible=sk
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// S
+	'squirrel_chimp_lists_description' => 'Gestion de mailing listes de [Mailchimp->http://mailchimp.com/] inspirée des fonctionnalités du plugin Wordpress [Autochimp->http://wordpress.org/extend/plugins/autochimp/] avec quelques "emprunts" de code du plugin SPIP "MailChimp de [Ben->http://www.nursit.net]', # NEW
+	'squirrel_chimp_lists_nom' => 'E-mailové skupiny Squirrels Love Chimps',
+	'squirrel_chimp_lists_slogan' => 'Riadenie e-mailových skupín Squirrels Love Chimps'
+);
+
+?>
