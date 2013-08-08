4e8b33e9b317533c373d8b4e90d231eda7303914
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-08-08 03:38:38 +0000

    langues (paquet-amap)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@74622 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ amap/trunk/lang/paquet-amap_sk.php
@@ -0,0 +1,17 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-amap?lang_cible=sk
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'amap_description' => 'Le plugin est développé pour gérer les paniers et adhérents (dit amapiens).
+Les paniers sont créés et attribués à un amapien, ce dernier pourra mettre à disposition un panier s\'il n’est pas présent pour le récupérer le jour prévu.', # NEW
+	'amap_nom' => 'AMAP',
+	'amap_slogan' => 'Riadenie košíkov a členov pre AMAP'
+);
+
+?>
