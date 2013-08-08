e97de3602a1611ab6c47df47e5e217257583e690
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-08-08 03:39:26 +0000

    langues (paquet-sedna)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@74644 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ sedna/trunk/lang/paquet-sedna_sk.php
@@ -0,0 +1,17 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-sedna?lang_cible=sk
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// S
+	'sedna_description' => 'Sedna slúži na kumulovanie informácií zo zdroja RSS (a ATOM a i.) zo syndikovaných stránok SPIPu. Jej vzhľad bol inšpirovaný programom Safari RSS.
+
+Jej cieľom je stále mať jednoduchý kód a rýchlo sa spúšťať. A trochu aj estetický vzhľad.',
+	'sedna_slogan' => 'Kumulovanie RSS'
+);
+
+?>
