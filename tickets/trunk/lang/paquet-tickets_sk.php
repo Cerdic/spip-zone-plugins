e0ad1b0670230d9ce442456e0a087a533c11aa50
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-08-08 03:39:24 +0000

    langues (paquet-tickets)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@74643 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ tickets/trunk/lang/paquet-tickets_sk.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-tickets?lang_cible=sk
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// T
+	'tickets_description' => '{Tickets} est un outil de suivi de bugs directement intégré à l\'espace privé de SPIP. Il est conçu pour faciliter la phase de développement du site (notamment les squelettes du site public).', # NEW
+	'tickets_nom' => 'Lístky',
+	'tickets_slogan' => 'Systém na sledovanie chýb'
+);
+
+?>
