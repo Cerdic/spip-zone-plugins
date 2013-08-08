9eecae90968df595f8a07fb64f3af510e9f5979c
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-08-08 03:38:56 +0000

    langues (paquet-doc2img)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@74631 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ doc2img/trunk/lang/paquet-doc2img_sk.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-doc2img?lang_cible=sk
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// D
+	'doc2img_description' => 'Ce plugin permet de convertir à la volée des pdf, tiff et autres types de document en image ou série d\'images. Ces images sont ensuite exploitables via une boucle spécifique.', # NEW
+	'doc2img_nom' => 'Dokumenty na obrázky',
+	'doc2img_slogan' => 'Meňte textové dokumenty za pochodu'
+);
+
+?>
