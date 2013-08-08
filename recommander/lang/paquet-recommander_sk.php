220906d9e950ad98a614cc6d0e9866d9bd2d5bb0
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-08-08 03:39:16 +0000

    langues (paquet-recommander)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@74638 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ recommander/lang/paquet-recommander_sk.php
@@ -0,0 +1,22 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-recommander?lang_cible=sk
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// R
+	'recommander_description' => 'Funkcia Odporučiť stránku priateľovi
+
+Môžete pridať do hocijakej šablóny vo formulári:
+<code>#RECOMMANDER{nadpis stránky,adresa stránky,úvod stránky,hlavný text}</code>
+_ napríklad
+<code>#RECOMMANDER{#TITRE,#URL_ARTICLE,#INTRODUCTION}</code>
+_ adresa, úvod a hlavný text sú nepovnné',
+	'recommander_nom' => 'Odporučiť priateľovi',
+	'recommander_slogan' => 'Funkcia Odporučiť stránku priateľovi'
+);
+
+?>
