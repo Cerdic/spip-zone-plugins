aebb3c22ef4438691ee98d40b26debfda6d6e507
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-08-08 03:38:36 +0000

    langues (paquet-accesrestreint)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@74620 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ acces_restreint/trunk/lang/paquet-accesrestreint_sk.php
@@ -0,0 +1,20 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-accesrestreint?lang_cible=sk
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'accesrestreint_description' => '_ V každej zóne sú rubriky.
+
+_ Autori môžu byť so zónami prepojení cez prístupoé práva.
+
+_ Zavedené cyly SPIPu sa upravia  podľa zistených prístupových práv návštevníka.',
+	'accesrestreint_nom' => 'Obmedzený prístup',
+	'accesrestreint_slogan' => 'Riadenie zón s obmedzeným prístupom'
+);
+
+?>
