4627228cc719337706c787cf07250daf5f5adf27
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-08-08 03:38:42 +0000

    langues (paquet-bouspip)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@74625 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ boussole_spip/trunk/lang/paquet-bouspip_sk.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-bouspip?lang_cible=sk
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'bouspip_description' => 'Tento zásuvný modul uvádza názov, adresu a opis všetkých stránok Galaxie SPIPu. Musí sa používať so zásuvným modulom Kompas v serverovom režime, aby boli jeho údaje poskytnuté cez internet všetkým stránkam, ktoré budú chcieť zobrazenie kompasu SPIPu.',
+	'bouspip_nom' => 'Kompas SPIP',
+	'bouspip_slogan' => 'Dobré adresy v galaxii SPIPu.'
+);
+
+?>
