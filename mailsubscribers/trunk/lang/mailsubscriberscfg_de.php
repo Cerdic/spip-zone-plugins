f266e0e4aa55220e86b6c1dd6003772bb8d43c54
klaus@spip.de <klaus@spip.de@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-08-09 11:06:49 +0000

    traduction allemande + coquilles
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@74670 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ mailsubscribers/trunk/lang/mailsubscriberscfg_de.php
@@ -0,0 +1,32 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// L
+	'label_double_optin' => 'Validation des inscriptions',
+	'label_double_optin_0' => 'Einfache Anmeldung',
+	'label_double_optin_1' => 'Doppeltes Opt-in (Bestätigungsmail mit Überprüfungslink)',
+
+	'label_list_id' => 'Bezeichner',
+	'label_list_titre' => 'Name der Liste',
+	'label_list_status' => 'Status',
+	'label_status_open' => 'Offen',
+	'label_status_close' => 'Geschlossen',
+	'label_status_delete' => 'Gelöscht',
+	'label_status_delete_explication' => 'Diese Liste und ihre Abonnenten werden gelöscht',
+
+	'label_proposer_signup_optin_1'=> 'Newsletter-Abonnenment zusammen mit der Anmeldung bei der Website anbieten',
+
+	'legend_listes_diffusion' => 'Verteiler/Listen',
+
+	// T
+	'titre_page_configurer_mailsubscriber' => 'Abonnieren des Newsletter',
+	'cfg_titre_parametrages' => 'Konfiguration der Newsletter-Anmeldung',
+
+);
+
+?>
---
+++ mailsubscribers/trunk/lang/newsletter_de.php
@@ -0,0 +1,25 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'bouton_subscribe' => 'Newsletter bestellen',
+	'bouton_unsubscribe' => 'Newsletter abbestellen',
+
+	// L
+	'label_email_subscribe' => 'Email',
+	'label_email_unsubscribe' => 'Email',
+	'label_link_unsubscribe' => 'Diesen Newsletter abbestellen',
+
+	// S
+	'subscribe_message_ok' => 'Ihre ANmeldung unter der Adresse @email@ wird bearbeitet.',
+	'unsubscribe_message_ok' => 'Die Adresse @email@ wurde aus der Empfängerliste gestrichen.',
+
+
+);
+
+?>
