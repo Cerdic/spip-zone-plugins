c7a3e3dd341972fb97b6355ad7dbd984101c3ce7
klaus@spip.de <klaus@spip.de@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-08-11 03:38:46 +0000

    langues (paquet-saveauto)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@74703 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ saveauto/trunk/lang/paquet-saveauto_de.php
@@ -0,0 +1,20 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-saveauto?lang_cible=de
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// S
+	'saveauto_description' => 'Der Backupmanager "Automatisches Backup" erstellt regelmäßig vollständige Sicherungen der MySQL-Datenbank von SPIP als.zip oder .sql Datei.
+
+Die Sicherungen werden in einem Verzeichnis auf dem Server gespeichert (Grundeinstellung: /tmp/dump) und können per Mail verschickt werden. Die Sicherungsdateien können ab einem einstellbaren Alter automatisch gelöscht werden.
+
+Est steht ein Interface zum manuellen Start des Backups und für die Vewaltung der Sicherungsdateien zur Verfügung.',
+	'saveauto_nom' => 'Automatisches Backup',
+	'saveauto_slogan' => 'Automatisches Backup der MySQL-Dtanbank von SPIP'
+);
+
+?>
