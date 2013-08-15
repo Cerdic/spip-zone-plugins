bbc26a43de174cfe023822e6ab92319a3a8ff53f
lesage.sylvain@laposte.net <lesage.sylvain@laposte.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-08-15 15:01:54 +0000

    Page de configuration (pompée sur le plugin fulltext !)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@74776 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ ocr/trunk/formulaires/configurer_ocr.php
@@ -0,0 +1,80 @@
+<?php
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+function formulaires_configurer_ocr_charger_dist(){
+	//Recuperation de la configuration
+	$ocr = @unserialize($GLOBALS['meta']['ocr']);
+	if(!is_array($ocr)){
+		$ocr = array();
+	}
+	//Valeurs prealablement saisie ou par defaut/d'exemple 
+	$valeur = array(
+		'intervalle_cron' =>  $ocr['intervalle_cron'] ? $ocr['intervalle_cron'] : 600,
+		'nb_docs' =>  $ocr['nb_docs'] ? $ocr['nb_docs'] : 5,
+		
+		'ocr_bin' => $ocr['ocr_bin'] ? $ocr['ocr_bin'] : '/usr/bin/tesseract',
+		'ocr_opt' => $ocr['ocr_opt'] ? $ocr['ocr_opt'] : '-fra',
+	);
+	return $valeur;
+}
+function formulaires_configurer_ocr_verifier_dist(){
+	$erreurs = array();
+	//Il faut au moins une seconde
+	if((!_request('intervalle_cron'))||(_request('intervalle_cron') < 1)){
+		$erreurs['intervalle_cron'] = _T('ocr:erreur_intervalle_cron');
+	}
+	//Il faut au moins une documents a la fois
+	if((!_request('nb_docs'))||(_request('nb_docs') < 1)){
+		$erreurs['nb_docs'] = _T('ocr:erreur_nb_docs');
+	}	
+
+	/**
+	 * On teste les binaires
+	 */
+	$binaire = 'ocr_bin';
+	/**
+	 * Pas de binaire => on doit en avoir un pour récupérer le contenu
+	 */
+	if(!_request($binaire)){
+		$erreurs[$binaire] = _T('ocr:erreur_ocr_bin');
+	}else{
+		/**
+		 * On teste avec la commande de base ...
+		 * Le code de retour normal doit être 0
+		 */
+		@exec(_request($binaire),$retour_bin,$retour_bin_int);
+		if($retour_bin_int != 0){
+			/**
+			 * Sinon on fait un test que le binaire est executable 
+			 * Cela nécessite un chemin complet du binaire
+			 */
+			@exec('test -x '._request($binaire),$retour_bin,$retour_bin_int);
+			if($retour_bin_int != 0){
+				$erreurs[$binaire] = _T('ocr:erreur_binaire_indisponible');
+			}
+		}
+	}
+	
+	if(count($erreurs) > 0){
+		$erreurs['message_erreur'] = _T('ocr:erreur_verifier_configuration');
+	}
+	return $erreurs;
+}
+
+function formulaires_configurer_ocr_traiter_dist(){
+	//Recuperation de la configuration et serialization
+	$ocr = serialize(array(
+		'intervalle_cron' => intval(_request('intervalle_cron')),
+		'nb_docs' => intval(_request('nb_docs')),
+		
+		'ocr_bin' => _request('ocr_bin'),
+		'ocr_opt' => _request('ocr_opt'),
+	));
+	//Insere ou update ?
+	ecrire_meta('ocr',$ocr);
+	$res = array('message_ok'=>_T('ocr:message_ok_configuration'));
+	return $res;
+	
+}
+?>
--- ocr/trunk/lang/ocr_fr.php
+++ ocr/trunk/lang/ocr_fr.php
@@ -13,6 +13,25 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'cfg_exemple_explication' => 'Explication de cet exemple',
 	'cfg_titre_parametrages' => 'Paramétrages',
 
+	// E
+	'erreur_binaire_indisponible' => 'Ce logiciel n\'est pas disponible sur le serveur.',
+	'erreur_intervalle_cron' => 'Vous devez indiquer un intervalle supérieur à une seconde.',
+	'erreur_nb_docs' => 'Vous devez indiquer un nombre de documents à traiter par itération supérieur à un.',
+	'erreur_ocr_bin' => 'Vous devez renseigner le binaire à utiliser pour la reconnaissance de caractères',
+	'erreur_verifier_configuration' => 'Il y a des erreurs de configuration.',
+	'explication_option_readonly' => 'Cette option est forcée sur ce site et n\'est donc pas configurable.',
+	
+	// G
+	'general' => 'Général',
+
+	// I
+	'indiquer_chemin_bin' => 'Indiquer le chemin vers le binaire traitant la reconnaissance de caractères',
+	'indiquer_options_bin' => 'Indiquer les options pour la reconnaissance de caractères',
+	'intervalle_cron' => 'Intervalle de temps entre deux passages du CRON (en secondes).',
+	
+	// N
+	'nombre_documents' => 'Nombre de documents à traiter par itération du CRON',
+	
 	// T
 	'titre_page_configurer_ocr' => 'OCR',
 );
