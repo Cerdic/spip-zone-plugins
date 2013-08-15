bddf16126cf90059ba55ce04ee5c351afb66d4c3
lesage.sylvain@laposte.net <lesage.sylvain@laposte.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-08-15 14:15:42 +0000

    Initialisation avec La Fabrique
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@74773 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ ocr/trunk/lang/paquet-ocr_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// O
+	'ocr_description' => 'Le plugin permet d\'analyser un fichier image (typiquement un document scanné) pour extraire le texte contenu. Il utilise la librairie d\'OCR (Optical character recognition) [tesseract->http://code.google.com/p/tesseract-ocr/wiki/ReadMe].',
+	'ocr_nom' => 'ocr',
+	'ocr_slogan' => 'Extraire un texte d\'une image par OCR',
+);
+
+?>
