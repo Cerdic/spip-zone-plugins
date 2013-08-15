7e207017cd74a03d95284b4b3f2785108a6aea58
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-08-15 13:46:46 +0000

    Dépôt initial du plugin. Ce plugin va lister sur une page toutes les pages de configuration offertes par les plugins actifs du site (vérouillés ou non). Ça sera plus simple pour s'y retrouver au lieu d'aller fouiller à droite et à gauche.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@74772 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ lister_config/trunk/lang/lister_config_fr.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/autorite/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// D
+	'description_page' => 'Vous pouvez trouver ci-dessous la liste des plugins actifs du site proposant une page de configuration du type <code>?exec=configurer_prefixe-plugin</code>.',
+	// T
+	'titre_lister_config' => 'Les pages de configuration',
+	'titre_page' => 'Pages de configuration des plugins actifs du site'
+
+);
+
+?>
