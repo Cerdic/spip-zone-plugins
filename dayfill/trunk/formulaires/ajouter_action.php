e8aed2981b41be884b7130398b392d1d88203abf
cyril@cym.fr <cyril@cym.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-08-19 18:11:53 +0000

    amélioration du formulaire de saisie d'une action;
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@74843 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- dayfill/trunk/formulaires/ajouter_action.php
+++ dayfill/trunk/formulaires/ajouter_action.php
@@ -12,6 +12,8 @@ function formulaires_ajouter_action_charger_dist()
 		'id_auteur'       => $id_auteur,
 		'date_debut'      => $date_debut,
 		'date_fin'        => $date_fin,
+		'heure_debut'     => date('H:i', strtotime('now - 10 minute')),
+		'heure_fin'       => date('H:i'),
 		'type_activite'   => $type_activite,
 		'id_organisation' => $id_organisation,
 		'id_projet'       => $id_projet,
