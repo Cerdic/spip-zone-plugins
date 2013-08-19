b17aa491d7a30ead7207451322c375a4602a5761
cyril@cym.fr <cyril@cym.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-08-19 09:58:36 +0000

    déplacement des fichiers mis dans trunk/trunk par erreur;
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@74826 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spipmine/trunk/lang/spipmine_fr.php
+++ spipmine/trunk/lang/spipmine_fr.php
@@ -1,45 +1,57 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
 
-    if (!defined('_ECRIRE_INC_VERSION')) return;
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-    //A
-    'aller_projet' => 'Aller au projet',
-    'aller_projet_num' => 'Aller au projet N°',
-    'attribuer_maquette_objet' => 'Attribuer une maquette à l\'objet',
-    //C
-    'choisir_benchmark_projet' => 'Choisir un benchmark pour le projet',
-    'choisir_cdc_objet' => 'Choisir un cahier des charges pour l\'objet',
-    'choisir_cdev_objet' => 'Choisir un cahier de développement pour l\'objet',
-    'commentaires' => "Commentaires",
+	//A
+	'ajouter_client'           => "Ajouter un client",
+	'ajouter_activite'         => "Ajouter une activité",
+	'aller_projet'             => 'Aller au projet',
+	'aller_projet_num'         => 'Aller au projet N°',
+	'attribuer_maquette_objet' => 'Attribuer une maquette à l\'objet',
+
+	//C
+	'creer_facture'            => "Créer une facture",
+	'choisir_benchmark_projet' => 'Choisir un benchmark pour le projet',
+	'choisir_cdc_objet'        => 'Choisir un cahier des charges pour l\'objet',
+	'choisir_cdev_objet'       => 'Choisir un cahier de développement pour l\'objet',
+	'commentaires'             => "Commentaires",
 
 
-    //E
-    'enjeux_client' => "Enjeux pour le client",
+	//E
+	'enjeux_client'            => "Enjeux pour le client",
 
-    //L
-    'label_factures_liees' => "Factures liées",
+	// F
+	'faire_devis'              => "Faire un devis",
 
-    //M
-    'methode' => "Méthodologie proposée",
+	//L
+	'label_factures_liees'     => "Factures liées",
 
-    //O
-    'objectifs' => 'Objectifs à atteindre',
+	//M
+	'methode'                  => "Méthodologie proposée",
 
-    //P
-    'projet_precedent' => 'Projet précédent',
-    'projet_suivant' => 'Projet suivant',
+	// N
+	'nouveau_contact'          => "Nouveau contact",
+	'nouvelle_organisation'    => "Nouvelle organisation",
 
-    // V
-    'voir_benchmark_projet' => 'Voir le benchmark sites du projet',
-    'voir_cdev_num' => 'Voir le cahier de développement N°',
-    'voir_cdc_num' => 'Voir le cahier des charges N°',
-    'voir_clients' => 'Voir tous les clients',
-    'voir_en_ligne' => 'Voir en ligne...',
-    'voir_maquette_num' => 'Voir la maquette N°',
-    'voir_projet_num' => 'Voir le projet N°',
+	//O
+	'objectifs'                => 'Objectifs à atteindre',
+
+	//P
+	'page_ajouter_client'      => "Ajouter un client",
+	'projet_precedent'         => 'Projet précédent',
+	'projet_suivant'           => 'Projet suivant',
+
+	// V
+	'voir_benchmark_projet'    => 'Voir le benchmark sites du projet',
+	'voir_cdev_num'            => 'Voir le cahier de développement N°',
+	'voir_cdc_num'             => 'Voir le cahier des charges N°',
+	'voir_clients'             => 'Voir tous les clients',
+	'voir_en_ligne'            => 'Voir en ligne...',
+	'voir_maquette_num'        => 'Voir la maquette N°',
+	'voir_projet_num'          => 'Voir le projet N°',
 
 );
 ?>
