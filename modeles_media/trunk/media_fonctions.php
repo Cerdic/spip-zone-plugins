451b542e18fdb18ba9450e342409c332406f8099
joseph@larmarange.net <joseph@larmarange.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-08-19 15:01:55 +0000

    Fixer le lien adéquat vers le doc lorsque l'on est avec une connexion distante
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@74836 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- modeles_media/trunk/media_fonctions.php
+++ modeles_media/trunk/media_fonctions.php
@@ -1,12 +1,12 @@
 <?php
 
-// Sécurité
+// SÃ©curitÃ©
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
-// Calcule la légende d'un document
-// Balise placée dans une boucle DOCUMENTS et appelée dans un modèle <media>
-// Les paramètres legende, titre, descriptif, credits, poids et type sont récupérés dans l'environnement
-// Syntaxe (premier argument précise si en div ou en dl, le second la largeur de la légende) :
+// Calcule la lÃ©gende d'un document
+// Balise placÃ©e dans une boucle DOCUMENTS et appelÃ©e dans un modÃ¨le <media>
+// Les paramÃ¨tres legende, titre, descriptif, credits, poids et type sont rÃ©cupÃ©rÃ©s dans l'environnement
+// Syntaxe (premier argument prÃ©cise si en div ou en dl, le second la largeur de la lÃ©gende) :
 // #MEDIA_LEGENDE{'dl',#GET{width}} ou #MEDIA_LEGENDE{'div',#GET{width}} 
 function balise_MEDIA_LEGENDE_dist($p) {
 	$conteneur = interprete_argument_balise(1,$p);
@@ -34,12 +34,12 @@ function calculer_balise_MEDIA_LEGENDE($conteneur,$width,$sql_id_document,$sql_t
 	$env_type = $args['type'];
 	$env_poids = $args['poids'];
 	
-	// Doit-on afficher une légende ?
+	// Doit-on afficher une lÃ©gende ?
 	if ($env_legende || $env_titre || $env_descriptif || $env_credits || $env_poids || $env_type) {
 		$media_largeur_max_legende = isset($GLOBALS['meta']['media_largeur_max_legende']) ? $GLOBALS['meta']['media_largeur_max_legende'] : 120;
 		$media_largeur_min_legende = isset($GLOBALS['meta']['media_largeur_min_legende']) ? $GLOBALS['meta']['media_largeur_min_legende'] : 350;
 		$width = is_numeric($width) ? min($media_largeur_max_legende,max($media_largeur_min_legende,intval($width))) : $media_largeur_max_legende;
-		// Y a-t-il un modèle légende à utiliser ?
+		// Y a-t-il un modÃ¨le lÃ©gende Ã  utiliser ?
 		if ($env_legende && find_in_path('modeles/legende_'.$env_legende.'.html')) {
 			$ret = recuperer_fond('modeles/legende_'.$env_legende,array(
 				'id' => $sql_id_document,
@@ -82,7 +82,7 @@ function calculer_balise_MEDIA_LEGENDE($conteneur,$width,$sql_id_document,$sql_t
 			$notes = calculer_notes();
 			if ($notes)
 				$notes = '<p class="notes">'.PtoBR($notes).'</p>';
-			// Crédits
+			// CrÃ©dits
 			if ($env_credits && $env_credits!='credits') {
 				$credits = typo($env_credits);
 				$crayons_credits = '';
@@ -121,18 +121,18 @@ function calculer_balise_MEDIA_LEGENDE($conteneur,$width,$sql_id_document,$sql_t
 	return $ret;
 }
 
-// Renvoie un espace si on doit afficher une légende
+// Renvoie un espace si on doit afficher une lÃ©gende
 // Rien sinon
-// Balise placée dans une boucle DOCUMENTS et appelée dans un modèle <media>
+// Balise placÃ©e dans une boucle DOCUMENTS et appelÃ©e dans un modÃ¨le <media>
 function balise_MEDIA_AFFICHER_LEGENDE_dist($p) {
 	$conteneur = interprete_argument_balise(1,$p);
 	$p->code = "\$Pile[0]['args']['legende'] || \$Pile[0]['args']['titre'] || \$Pile[0]['args']['descriptif'] || \$Pile[0]['args']['credits'] || \$Pile[0]['args']['type'] || \$Pile[0]['args']['poids'] ? ' ' : ''";
 	return $p;
 }
 
-// Renvoie le fichier d'une image retaillée selon les paramètres passés au modèle
-// Balise placée dans une boucle DOCUMENTS et appelée dans un modèle <media>
-// Les paramètres taille, hauteur, largeur, alt et titre sont récupérés dans l'environnement.
+// Renvoie le fichier d'une image retaillÃ©e selon les paramÃ¨tres passÃ©s au modÃ¨le
+// Balise placÃ©e dans une boucle DOCUMENTS et appelÃ©e dans un modÃ¨le <media>
+// Les paramÃ¨tres taille, hauteur, largeur, alt et titre sont rÃ©cupÃ©rÃ©s dans l'environnement.
 // Exemple de syntaxe : 
 // #MEDIA_IMAGE_RETAILLEE{#LOGO_DOCUMENT} ou #MEDIA_IMAGE_RETAILLEE{#URL_DOCUMENT}
 function balise_MEDIA_IMAGE_RETAILLEE_dist($p) {
@@ -194,9 +194,9 @@ function calculer_balise_MEDIA_IMAGE_RETAILLEE($image,$args,$sql_titre,$sql_type
 	return $img;
 }
 
-// Calcule le lien, si lien demandé, sur le document
-// Balise placée dans une boucle DOCUMENTS et appelée dans un modèle <media>
-// Les paramètres lien, titre_lien et sont récupérés dans l'environnement.
+// Calcule le lien, si lien demandÃ©, sur le document
+// Balise placÃ©e dans une boucle DOCUMENTS et appelÃ©e dans un modÃ¨le <media>
+// Les paramÃ¨tres lien, titre_lien et sont rÃ©cupÃ©rÃ©s dans l'environnement.
 // Exemple de syntaxe : 
 // #MEDIA_LIEN{#LOGO_DOCUMENT} ou #MEDIA_LIEN{#MEDIA_IMAGE_RETAILLEE{#LOGO_DOCUMENT}}
 function balise_MEDIA_LIEN_dist($p) {
@@ -204,36 +204,37 @@ function balise_MEDIA_LIEN_dist($p) {
 	$forcer_lien = interprete_argument_balise(2,$p);
 	$forcer_lien = is_null($forcer_lien) ? "''" : $forcer_lien;
 	$id_document = champ_sql('id_document', $p);
+	$url_document = generer_generer_url_arg('document', $p, $id_document);
 	$connect = '';
 	if (isset($p->boucles[$p->id_boucle]))
 		$connect = $p->boucles[$p->id_boucle]->sql_serveur;
 	$connect = _q($connect);
-	$p->code = "calculer_balise_MEDIA_LIEN($objet,$forcer_lien,$id_document,\$Pile[0]['args'],$connect)";
+	$p->code = "calculer_balise_MEDIA_LIEN($objet,$forcer_lien,$id_document,$url_document,\$Pile[0]['args'],$connect)";
 	return $p;
 }
 
-function calculer_balise_MEDIA_LIEN($objet,$forcer_lien,$id_document,$args,$connect='') {
+function calculer_balise_MEDIA_LIEN($objet,$forcer_lien,$id_document,$url_document,$args,$connect='') {
 	$lien = $args['lien'];
 	$titre_lien = $args['titre_lien'];
 	$titre = $args['titre'];
 	
-	// A-t-on demandé un lien
+	// A-t-on demandÃ© un lien
 	if (!$lien && !$forcer_lien)
 		return $objet;
-	// Si lien non spécifique, on pointe sur le document
+	// Si lien non spÃ©cifique, on pointe sur le document (en se basant sur $url_document pour ne pas pointer sur spip.php?page=document)
 	if ($lien=='lien' || !$lien) {
 		$lien = 'doc'.$id_document;
-		// Si on pointe sur le document, que titre_lien n'est pas spécifié mais qu'on a spécifié un titre au document, on prend le titre spécifique
+		// Si on pointe sur le document, que titre_lien n'est pas spÃ©cifiÃ© mais qu'on a spÃ©cifiÃ© un titre au document, on prend le titre spÃ©cifique
 		if (!$titre_lien && $titre && $titre!='titre')
 			$titre_lien = $titre;
+		$l = calculer_url($lien, $titre_lien, 'tout', $connect);
+		$l['url'] = $url_document;
 	}
-	$l = calculer_url($lien, $titre_lien, 'tout', $connect);
+	else $l = calculer_url($lien, $titre_lien, 'tout', $connect);
+	
 	if (!$l['url']) 
 		return $object;
-	if ($connect) #Si connexion précisée, alors on fournit une URL absolue
-		$a = '<a href="'.url_absolue($l['url']).'"';
-	else
-		$a = '<a href="'.$l['url'].'"';
+	$a = '<a href="'.$l['url'].'"';
 	$a .= $l['class'] ? ' class="'.$l['class'].'"' : '';
 	$a .= $l['titre'] ? ' title="'.attribut_html($l['titre']).'"' : '';
 	$a .= $l['lang'] ? ' hreflang="'.$l['lang'].'"' : '';
@@ -242,9 +243,9 @@ function calculer_balise_MEDIA_LIEN($objet,$forcer_lien,$id_document,$args,$conn
 	return $a.$objet.'</a>';
 }
 
-// Renvoie la taille du fichier après redimensionnement si besoin
-// Balise placée dans une boucle DOCUMENTS et appelée dans un modèle <media>
-// Les paramètres taille, hauteur et largeur sont récupérés dans l'environnement.
+// Renvoie la taille du fichier aprÃ¨s redimensionnement si besoin
+// Balise placÃ©e dans une boucle DOCUMENTS et appelÃ©e dans un modÃ¨le <media>
+// Les paramÃ¨tres taille, hauteur et largeur sont rÃ©cupÃ©rÃ©s dans l'environnement.
 // Exemple de syntaxe : 
 // #MEDIA_TAILLE{largeur} ou #MEDIA_TAILLE{hauteur}
 function balise_MEDIA_TAILLE_dist($p) {
@@ -272,12 +273,12 @@ function calculer_balise_MEDIA_TAILLE($dim,$args,$sql_largeur,$sql_hauteur){
 		'moyen' => 320,
 		'grand' => 640
 	);
-	// Une taille par défaut si le document n'en n'a pas
+	// Une taille par dÃ©faut si le document n'en n'a pas
 	if (!is_numeric($sql_hauteur) || intval($sql_hauteur)<=0)
 		$sql_hauteur = isset($GLOBALS['meta']['media_taille_grand_hauteur']) ? $GLOBALS['meta']['media_taille_grand_hauteur'] : 480;
 	if (!is_numeric($sql_largeur) || intval($sql_largeur)<=0)
 		$sql_largeur = isset($GLOBALS['meta']['media_taille_grand_largeur']) ? $GLOBALS['meta']['media_taille_grand_largeur'] : 640;
-	// Hauteur visée (on peut avoir passé une hauteur en %)
+	// Hauteur visÃ©e (on peut avoir passÃ© une hauteur en %)
 	if (substr(trim($hauteur),-1)=='%')
 		$hauteur = trim($hauteur);
 	elseif (is_numeric($hauteur) && intval($hauteur)>0)
@@ -292,7 +293,7 @@ function calculer_balise_MEDIA_TAILLE($dim,$args,$sql_largeur,$sql_hauteur){
 		$hauteur = intval($taille);
 	else
 		$hauteur = $sql_hauteur;
-	// Largeur visée (on peut avoir passé une largeur en %)
+	// Largeur visÃ©e (on peut avoir passÃ© une largeur en %)
 	if (substr(trim($largeur),-1)=='%')
 		$largeur = trim($largeur);
 	elseif (is_numeric($largeur) && intval($largeur)>0)
@@ -307,7 +308,7 @@ function calculer_balise_MEDIA_TAILLE($dim,$args,$sql_largeur,$sql_hauteur){
 		$largeur = intval($taille);
 	else
 		$largeur = $sql_largeur;
-	// Doit-on redimensionner ? Si une deux dimensions est exprimée en %, on ne redimensionne pas.
+	// Doit-on redimensionner ? Si une deux dimensions est exprimÃ©e en %, on ne redimensionne pas.
 	if (substr($hauteur,-1)=='%' || substr($largeur,-1)=='%')
 		$t = array('largeur' => $largeur, 'hauteur' => $hauteur);
 	elseif ($sql_hauteur > $hauteur || $sql_largeur > $largeur) {
@@ -318,7 +319,7 @@ function calculer_balise_MEDIA_TAILLE($dim,$args,$sql_largeur,$sql_hauteur){
 	return $dim ? $t[$dim] : $t;
 }
 
-// Renvoie un tableau des paramètres à ignorer à utiliser avec env_to_params et env_to_attribute
+// Renvoie un tableau des paramÃ¨tres Ã  ignorer Ã  utiliser avec env_to_params et env_to_attribute
 // Exemple : [(#ENV*|env_to_attributs{#MEDIA_IGNORE_PARAMS})] ou [(#ENV*|env_to_params{#MEDIA_IGNORE_PARAMS})]
 function balise_MEDIA_IGNORE_PARAMS_dist($p) {
 	$p->code = "array('id_media', 'legende','titre','descriptif','credits','poids','type','taille','hauteur','largeur','class','lien','lien_class')";
@@ -326,11 +327,11 @@ function balise_MEDIA_IGNORE_PARAMS_dist($p) {
 }
 
 
-// Sécurité
+// SÃ©curitÃ©
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
-// Filtre media_generer_vignette pour générer une vignette automatique à partir du fichier
-// Recherche l'existence d'un filtre media_generer_vignette_ext et renvoie le résultat de ce filtre, sinon rien
+// Filtre media_generer_vignette pour gÃ©nÃ©rer une vignette automatique Ã  partir du fichier
+// Recherche l'existence d'un filtre media_generer_vignette_ext et renvoie le rÃ©sultat de ce filtre, sinon rien
 // media_generer_vignette_ext doit renvoyer l'url de la vignette
 function filtre_media_generer_vignette_dist($fichier,$ext) {
 	$f = charger_fonction('media_generer_vignette_'.$ext,'filtre',true);
@@ -341,7 +342,7 @@ function filtre_media_generer_vignette_dist($fichier,$ext) {
 }
 
 // Pour les images jpg, png et gif, on renvoie simplement $fichier
-// Le redimensionnement est assuré par le paramètre taille transmis aux modèles <media>
+// Le redimensionnement est assurÃ© par le paramÃ¨tre taille transmis aux modÃ¨les <media>
 function filtre_media_generer_vignette_jpg_dist($fichier) {return $fichier;}
 function filtre_media_generer_vignette_png_dist($fichier) {return $fichier;}
 function filtre_media_generer_vignette_gif_dist($fichier) {return $fichier;}
