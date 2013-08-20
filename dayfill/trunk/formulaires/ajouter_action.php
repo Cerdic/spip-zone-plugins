dda7f80e3b1d93322ad0f2fc44702ccd479705bc
cyril@cym.fr <cyril@cym.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-08-20 18:26:33 +0000

    on avance sur le formulaire de saisie d'une activité; l'affichage peut changer en fonction du profil de celui ou celle qui saisit les activités; les champs "projet" et "facture" sont cachés tant que l'organisation n'est pas précisée; le champ "type d'activité" n'apparaît (en js) que si l'activité a d'abord été décrite; reste à coder la partie "traiter";
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@74859 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- dayfill/trunk/formulaires/ajouter_action.php
+++ dayfill/trunk/formulaires/ajouter_action.php
@@ -8,26 +8,59 @@ function formulaires_ajouter_action_charger_dist()
 	include_spip('inc/session');
 	$id_auteur = session_get('id_auteur');
 
+	$profil = _request('profil');
+	if (!$profil) $profil = 'operateur';
+
 	return array(
-		'id_auteur'       => $id_auteur,
-		'date_debut'      => date('Y-m-d H:i:s'),
-		'date_fin'        => date('Y-m-d H:i:s'),
-		'heure_debut'     => date('H:i', strtotime('now - 10 minute')),
-		'heure_fin'       => date('H:i'),
-		'type_activite'   => $type_activite,
-		'id_organisation' => $id_organisation,
-		'id_projet'       => $id_projet,
-		'detail_activite' => $detail_activite,
-		'editable'        => 'oui'
+		'id_auteur'           => $id_auteur,
+		'date_debut'          => date('Y-m-d H:i:s'),
+		'date_fin'            => date('Y-m-d H:i:s'),
+		'heure_debut'         => date('H:i', strtotime('now - 10 minute')),
+		'heure_fin'           => date('H:i'),
+		'nb_heures_passees'   => '',
+		'nb_heures_facturees' => '',
+		'type_activite'       => $type_activite,
+		'detail_activite'     => $detail_activite,
+		'id_organisation'     => $id_organisation,
+		'id_projet'           => $id_projet,
+		'profil'              => $profil,
+		'editable'            => 'oui'
 	);
 }
 
 function formulaires_ajouter_action_verifier_dist()
 {
-	return array(
-		'message_erreur' => 'Erreur temporaire !'
-	);
+
+	if ($profil = _request('changer_profil')) {
+		set_request('profil', $profil);
+		return array('message_ok' => _T('dayfill:info_profil_change'));
+	}
+
+	if (!_request('enregistrer_activite')) return array('message_ok' => 'Le formulaire a été actualisé offrant des champs supplémentaires à saisir.');
+
+	$erreurs = array();
+	// verifier que les champs obligatoires sont bien la :
+	foreach (array(
+		         'id_auteur',
+		         'nb_heures_passees',
+		         'nb_heures_facturees',
+		         'date_debut',
+		         'date_fin',
+		         'detail_activite',
+		         'id_organisation',
+		         'id_projet'
+	         ) as $obligatoire) {
+		if (!_request($obligatoire)) {
+			$erreurs[$obligatoire] = _T('info_obligatoire');
+		}
+	}
+
+	if (count($erreurs))
+		$erreurs['message_erreur'] = _T('dayfill:erreur_saisie_invalide');
+
+	return $erreurs;
 }
+
 
 function formulaires_ajouter_action_traiter_dist()
 {
--- dayfill/trunk/lang/projets_activite_fr.php
+++ dayfill/trunk/lang/projets_activite_fr.php
@@ -7,51 +7,51 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
-	'ajouter_lien_projets_activite' => 'Ajouter cette activité',
+	'ajouter_lien_projets_activite'         => 'Ajouter cette activité',
 
 	// E
-	'explication_date_debut' => 'Indiquez le moment où l\'activité a démarré',
-	'explication_date_fin' => 'Indiquez le moment où l\'activité a pris fin',
-	'explication_descriptif' => 'Décrivez l\'activité',
-	'explication_id_auteur' => 'Indiquez l\'auteur de l\'activité',
-	'explication_nb_heures_decomptees' => 'Indiquez la durée de l\'activité qui doit réellement être prise en compte, en heures.',
-	'explication_nb_heures_passees' => 'Indiquez la durée brute de l\'activité, en heures.',
+	'explication_date_debut'                => 'Indiquez le moment où l\'activité a démarré',
+	'explication_date_fin'                  => 'Indiquez le moment où l\'activité a pris fin',
+	'explication_descriptif'                => 'Décrivez l\'activité',
+	'explication_id_auteur'                 => 'Indiquez l\'auteur de l\'activité',
+	'explication_nb_heures_decomptees'      => 'Indiquez la durée de l\'activité qui doit réellement être prise en compte, en heures.',
+	'explication_nb_heures_passees'         => 'Indiquez la durée brute de l\'activité, en heures.',
 
 	// I
-	'icone_creer_projets_activite' => 'Créer une activité',
-	'icone_modifier_projets_activite' => 'Modifier cette activité',
-	'info_1_projets_activite' => 'Une activité',
-	'info_aucun_projets_activite' => 'Aucune activité',
-	'info_nb_projets_activites' => '@nb@ activités',
-	'info_projets_activites_auteur' => 'Les activités de cet auteur',
+	'icone_creer_projets_activite'          => 'Créer une activité',
+	'icone_modifier_projets_activite'       => 'Modifier cette activité',
+	'info_1_projets_activite'               => 'Une activité',
+	'info_aucun_projets_activite'           => 'Aucune activité',
+	'info_nb_projets_activites'             => '@nb@ activités',
+	'info_projets_activites_auteur'         => 'Les activités de cet auteur',
 
 	// L
-	'label_auteur_activite' => "Auteur",
-	'label_date_activite' => "Date",
-	'label_date_debut' => 'Heure debut',
-	'label_date_fin' => 'Heure fin',
-	'label_date_debut_fin' => 'Quand',
-	'label_descriptif' => 'Descriptif',
-	'label_duree_activite' => "Durée",
-	'label_id_auteur' => 'Auteur',
-	'label_id_projet' => 'Projet',
-	'label_nb_heures_decomptees' => "Nombre d'heures decomptées",
-	'label_nb_heures_passees' => "Nombre d'heures passées",
+	'label_auteur_activite'                 => "Auteur",
+	'label_date_activite'                   => "Date",
+	'label_date_debut'                      => 'Heure debut',
+	'label_date_fin'                        => 'Heure fin',
+	'label_date_debut_fin'                  => 'Quand',
+	'label_descriptif'                      => 'Descriptif',
+	'label_duree_activite'                  => "Durée",
+	'label_id_auteur'                       => 'Auteur',
+	'label_id_projet'                       => 'Projet',
+	'label_nb_heures_decomptees'            => "Nombre d'heures decomptées",
+	'label_nb_heures_passees'               => "Nombre d'heures passées",
 
 	// R
-	'retirer_lien_projets_activite' => 'Retirer cette activité',
-	'retirer_tous_liens_projets_activites' => 'Retirer toutes les activités',
+	'retirer_lien_projets_activite'         => 'Retirer cette activité',
+	'retirer_tous_liens_projets_activites'  => 'Retirer toutes les activités',
 
 	// T
-	'texte_ajouter_projets_activite' => 'Ajouter une activité',
+	'texte_ajouter_projets_activite'        => 'Ajouter une activité',
 	'texte_changer_statut_projets_activite' => 'Cette activité est :',
 	'texte_creer_associer_projets_activite' => 'Créer et associer une activité',
-	'titre_langue_projets_activite' => 'Langue de cette activité',
-	'titre_logo_projets_activite' => 'Logo de cette activité',
-	'titre_projets_activite' => 'Activité',
-	'titre_projets_activites' => 'Activités',
-	'titre_projets_activites_rubrique' => 'Activités de la rubrique',
-	'titre_projets_activite_projet' => "Activités liées au projet",
+	'titre_langue_projets_activite'         => 'Langue de cette activité',
+	'titre_logo_projets_activite'           => 'Logo de cette activité',
+	'titre_projets_activite'                => 'Activité',
+	'titre_projets_activites'               => 'Activités',
+	'titre_projets_activites_rubrique'      => 'Activités de la rubrique',
+	'titre_projets_activite_projet'         => "Activités liées au projet",
 );
 
 ?>
