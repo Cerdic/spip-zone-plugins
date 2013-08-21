504980ac73bd16de9bc849ffed53f95b2b3b7d0c
klaus@spip.de <klaus@spip.de@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-08-21 15:29:12 +0000

    déscriptif allemand pour le facteur / Briefträger
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@74866 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ facteur/trunk/lang/paquet-facteur_de.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/facteur/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// F
+	'facteur_description' => 'Der Briefträger befördert Emails im HTML-Format, im Nur-Text-Format oder in beiden, egal ob per SMTP oder nicht.',
+	'facteur_nom' => 'Briefräger',
+	'facteur_slogan' => 'Er befördert ihre Emails'
+);
+
+?>
