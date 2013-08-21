de376ca9b4a556abdae447bf6495361f696d7209
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-08-21 23:33:21 +0000

    attention : PtoBR insère un saut de ligne en fin de texte, ce qui faisait que #NOM avait aussi un saut de ligne en fin de texte, ce qui 1) était pas correct du pt de vue de l'insertion dans les balise html 2) posait souci pour l'affichage des résultats formidables
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@74874 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- titres_typographies/trunk/titres_typographies_options.php
+++ titres_typographies/trunk/titres_typographies_options.php
@@ -1,5 +1,5 @@
 <?php
 
-define('_TRAITEMENT_TYPO_SANS_NUMERO', 'PtoBR(propre(supprimer_numero(%s), $connect, $Pile[0]))');
+define('_TRAITEMENT_TYPO_SANS_NUMERO', 'trim(PtoBR(propre(supprimer_numero(%s)), $connect, $Pile[0]))');
 
 ?>
