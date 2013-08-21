bc6b25a698a6d5ad1ec9e95aca19cc2aa4062593
esj@rezo.net <esj@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-08-21 07:39:37 +0000

    [source:_plugins_/iterateurs iterateurs]: Report de [http://core.spip.org/projects/spip/repository/revisions/20775 SPIP 20775]: les chaînes de langues calculées dans les squelettes.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@74863 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- iterateurs/public/compiler.php
+++ iterateurs/public/compiler.php
@@ -774,11 +774,26 @@ function compile_cas($tableau, $descr, &$boucles, $id_boucle) {
 
 		case 'idiome':
 			$l = array();
-			foreach ($p->arg as $k => $v) {
-			  if ($k) $l[]= _q($k).' => '.calculer_liste($v,$descr,$boucles,$id_boucle);
+			$code = '';
+			foreach ($p->arg as $k => $v){
+				$_v = calculer_liste($v, $descr, $boucles, $id_boucle);
+				if ($k) 
+					$l[] = _q($k) . ' => ' . $_v;
+				else $code = $_v;
 			}
-			$l = !$l ? '' : (", array(".implode(",\n",$l).")");
-			$code = "_T('" . $p->module . ":" .$p->nom_champ . "'$l)";
+			/// Si le module n'est pas fourni,
+			/// l'expliciter sauf si calcule
+			if ($p->module) {
+				$m = $p->module .':'.$p->nom_champ;
+			} elseif ($p->nom_champ) {
+				$m = MODULES_IDIOMES .':'.$p->nom_champ;
+			} else  $m = '';
+
+			$code = (!$code ? "'$m'" :
+					($m ? "'$m' . $code" : 
+						("(strpos(\$x=$code, ':') ? \$x : ('" . MODULES_IDIOMES . ":' . \$x))")))
+				. (!$l ? '' : (", array(" . implode(",\n", $l) . ")"));
+			$code = "_T($code)";
 			if ($p->param) {
 				$p->id_boucle = $id_boucle;
 				$p->boucles = &$boucles;
