37ba61fda616a69c448373bb23fb4dfb62e2bf51
klaus@spip.de <klaus@spip.de@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-08-21 16:04:43 +0000

    traduction allemande
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@74869 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ mailshot/trunk/lang/paquet-mailshot_de.php
@@ -0,0 +1,12 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// M
+	'mailshot_slogan' => 'Versand von Massenmails',
+	'mailshot_description' => 'Versand von Massenmails',
+);
+?>
