ced115011ec98e51d8f8c6366eaf16bc69faef95
klaus@spip.de <klaus@spip.de@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-08-21 15:59:59 +0000

    traduction allemande
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@74868 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ mailshot/trunk/lang/newsletter_de.php
@@ -0,0 +1,26 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'bouton_envoyer' => 'Senden !',
+	'bouton_tester_envoi' => 'Testen !',
+
+	// I
+	'info_tester_newsletter' => 'Einzelsendung testen',
+	'info_envoi_unique_newsletter' => 'An 1 Empfänger senden',
+	'info_envoyer_newsletter' => 'Newsletter senden',
+	'info_test_envoye' => 'Testversand des Newsletter an @email@ abgeschlossen',
+	'info_envoi_unique_reussi' => 'Der Newsletter wurde an @email@ gesendet.',
+
+	// L
+	'label_email_test' => 'Mailadresse',
+	'label_liste' => 'an die Abonnenten von ',
+
+);
+
+?>
