84d857caf76f39e85d2b72a7accbc61ebc80d958
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-08-26 18:20:26 +0000

    une seule couleur choisie : au singulier
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@74972 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ saisie_choix_couleur/trunk/lang/saisie_choix_couleur_fr.php
@@ -0,0 +1,17 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/saisies/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	"choix_couleur_datas_explication"=>'Vous devez indiquez un choix par ligne sous la forme "code couleur CSS|Label du choix". Le code couleur ne doit PAS être précédé de #.',
+	"choix_couleur_defaut_choix1"=>"Couleur 1",
+	"choix_couleur_defaut_choix2"=>"Couleur 2",
+	"choix_couleur_defaut_choix3"=>"Couleur 3",
+	"choix_couleur_titre"=>"Un choix de couleurs",
+	"choix_couleur_explication"=>"Permet de choisir une couleurs parmis plusieurs.",
+	
+	
+);
+
+?>
