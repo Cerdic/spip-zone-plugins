498016ffe8ebd9603d099ec207158ed4fdcf6cc1
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-08-27 03:37:18 +0000

    langues (paquet-intranet)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@74987 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ intranet/trunk/lang/paquet-intranet_es.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-intranet?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// I
+	'intranet_description' => 'Autorizar únicamente a los usuarios conectados a visitar el sitio.',
+	'intranet_nom' => 'Intranet/Extranet',
+	'intranet_slogan' => 'Autorizar únicamente a los usuarios conectados a visitar el sitio.'
+);
+
+?>
