e067b166d821f264cdfdc19ccb0c96da6c9f59dd
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-08-27 03:37:23 +0000

    langues (paquet-openinviter)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@74991 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ openinviter/lang/paquet-openinviter_es.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-openinviter?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// O
+	'openinviter_description' => 'Integración de la librería OpenInviter con el API del plugin Importador de contactos.',
+	'openinviter_slogan' => 'Integración de la librería OpenInviter.'
+);
+
+?>
