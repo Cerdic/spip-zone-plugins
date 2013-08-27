759182736e6cac5cd828bc128edc03d9914a87f2
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-08-27 03:37:15 +0000

    langues (paquet-importateur_contacts)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@74985 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ importateur_contacts/lang/paquet-importateur_contacts_es.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-importateur_contacts?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// I
+	'importateur_contacts_description' => 'Permite importar contactos para ayudar a uno o a varios servicios web.',
+	'importateur_contacts_nom' => 'Importador de contactos',
+	'importateur_contacts_slogan' => 'Formulario de importación de contactos.'
+);
+
+?>
