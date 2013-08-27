2e134a5cadbdf30cf68f073147b2457dea357d33
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-08-27 03:37:22 +0000

    langues (openinviter)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@74990 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ openinviter/lang/openinviter_es.php
@@ -0,0 +1,20 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/openinviter?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'champ_email_label' => 'Su dirección de correo electrónico @fournisseur@',
+	'champ_password_explication' => 'No se guardará en la memoria.',
+	'champ_password_label' => 'Contraseña',
+
+	// E
+	'erreur_authentification' => 'Error de autenticación. Compruebe la dirección y la contraseña y vuelva a intentarlo.',
+	'erreur_generale' => 'Un error interno impide la recuperación de sus contactos.'
+);
+
+?>
