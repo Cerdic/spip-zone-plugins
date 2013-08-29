cc66cbf5d8fc385ae1a5565c2669a64ce7dcd2a0
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-08-29 03:37:18 +0000

    langues (paquet-todo)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@75063 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- todo/trunk/lang/paquet-todo_es.php
+++ todo/trunk/lang/paquet-todo_es.php
@@ -8,7 +8,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// T
-	'todo_description' => 'Añade nuevos accesos directos tipográficos que permiten describir de forma simple listas de cosas para hacer en un contenido SPIP. Esta versión 2 amplía la lista de estatus disponibles y permite la caracterización de las tareas por prioridad, de las etiquetas y de las informaciones tipificadas como fechas de inicio y de fin o números de cometido y de versión. ',
+	'todo_description' => 'Añade nuevos accesos directos tipográficos que permiten describir de forma simple listas de cosas para hacer en un contenido SPIP. Esta versión 2 amplía la lista de estatus disponibles y permite la caracterización de las tareas por prioridad, de las etiquetas y de la información tipificada como fechas de inicio y de fin o números de cometido y de versión. ',
 	'todo_slogan' => 'Listar rapidemente cosas para hacer'
 );
 
