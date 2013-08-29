8be71d14b8d673d0c8f7ef88a5064a9a707f3a48
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-08-29 03:36:54 +0000

    langues (paquet-bouspip)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@75052 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- boussole_spip/trunk/lang/paquet-bouspip_es.php
+++ boussole_spip/trunk/lang/paquet-bouspip_es.php
@@ -8,7 +8,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// B
-	'bouspip_description' => 'Este plugin proporciona el nombre, la url y la descripción de todos los sitios de la Galaxia SPIP. Debe ser utilizado con el plugin Brújula en modo servidor a fin de proporcionar, a través de un servicio web, sus informaciones a todos los sitios deseando mostrar la brújula SPIP. ',
+	'bouspip_description' => 'Este plugin proporciona el nombre, la url y la descripción de todos los sitios de la Galaxia SPIP. Debe ser utilizado con el plugin Brújula en modo servidor a fin de proporcionar, a través de un servicio web, su información a todos los sitios deseando mostrar la brújula SPIP. ',
 	'bouspip_nom' => 'Brújula SPIP',
 	'bouspip_slogan' => '¡Las buenas direcciones de la galaxia SPIP!'
 );
