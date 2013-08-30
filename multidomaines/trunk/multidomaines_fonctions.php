3eba3c960fbc0798936fc753f2993ec41907cb5a
pierrekuhn82@gmail.com <pierrekuhn82@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-08-30 19:13:51 +0000

    Une fonction pas appeler qu'on rajoute et correction du lien vers la doc.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@75144 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- multidomaines/trunk/multidomaines_fonctions.php
+++ multidomaines/trunk/multidomaines_fonctions.php
@@ -3,30 +3,20 @@
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 function calculer_URL_SECTEUR($id_rubrique) {
-
-include_spip('inc/config');
-
+	include_spip('inc/config');
 	$id_secteur = sql_getfetsel("id_secteur", "spip_rubriques", "id_rubrique=" . intval($id_rubrique));
-
 	$url = lire_config('multidomaines/editer_url_' .$id_secteur);
-
 	if (empty($url)) {$url = lire_config('multidomaines/editer_url');}
 	if (empty($url)) {$url = lire_config('adresse_site');}
-	
 	return trim($url,'/'). '/';
 }
 
 function calculer_URL_RUBRIQUE($id_rubrique) {
-
-include_spip('inc/config');
-
+	include_spip('inc/config');
 	$id_secteur = sql_getfetsel("id_rubrique", "spip_rubriques", "id_rubrique=" . intval($id_rubrique));
-
 	$url = lire_config('multidomaines/editer_url_' .$id_secteur);
-
 	if (empty($url)) {$url = lire_config('multidomaines/editer_url');}
 	if (empty($url)) {$url = lire_config('adresse_site');}
-	
 	return trim($url,'/'). '/';
 }
 
