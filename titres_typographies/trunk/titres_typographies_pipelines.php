ee6ab24a7d75d4479ede27b39adbb26da8dc1fa7
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-01 23:06:45 +0000

    appliquer aussi aux titres de forum
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@75214 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ titres_typographies/trunk/titres_typographies_pipelines.php
@@ -0,0 +1,12 @@
+<?php
+// Sécurité
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+function titres_typographies_declarer_tables_interfaces($interfaces){
+	if (isset($interfaces['table_des_traitements']['TITRE']['forums'])){
+		$interfaces['table_des_traitements']['TITRE']['forums']= "trim(PtoBR(liens_nofollow(safehtml(".str_replace("%s","interdit_html(%s)",_TRAITEMENT_RACCOURCIS)."))))";
+	}
+	return $interfaces;
+}
+
+?>
