04997b548f09546cafd3555a1880f918017a18cc
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-03 03:38:40 +0000

    langues (paquet-ocr)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@75232 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- ocr/trunk/lang/paquet-ocr_fr.php
+++ ocr/trunk/lang/paquet-ocr_fr.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/ocr/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
@@ -8,7 +8,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// O
 	'ocr_description' => 'Le plugin permet d\'analyser un fichier image (typiquement un document scanné) pour extraire le texte contenu. Il utilise la librairie d\'OCR (Optical character recognition) [tesseract->http://code.google.com/p/tesseract-ocr/wiki/ReadMe].',
 	'ocr_nom' => 'ocr',
-	'ocr_slogan' => 'Extraire un texte d\'une image par OCR',
+	'ocr_slogan' => 'Extraire un texte d\'une image par OCR'
 );
 
 ?>
