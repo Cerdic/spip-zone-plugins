ecafe3296d31e9623a46ac60ab8a27a0a5834365
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-04 17:13:14 +0000

    loger les envois incomplets
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@75273 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- mailshot/trunk/genie/mailshot_bulksend.php
+++ mailshot/trunk/genie/mailshot_bulksend.php
@@ -14,10 +14,15 @@ function genie_mailshot_bulksend_dist($t){
 		// TODO : limiter la cadence en fonction de la config
 		include_spip('inc/mailshot');
 		list($periode,$nb) = mailshot_cadence();
-		mailshot_envoyer_lot($nb);
+		$nb_sent = mailshot_envoyer_lot($nb);
 		// dire qu'on a pas fini si mode boost pour se relancer aussi vite que possible
 		if (lire_config("mailshot/boost_send")=='oui')
 			return -($t-$periode);
+		elseif ($nb_sent<$nb){
+			// il faudrait lancer un jobpour finir en async l'envoi du nombre exact demande ?
+			// pour le moment on log pour voir
+			spip_log("envoi incomplet : $nb_sent au lieu de $nb","bulksend"._LOG_INFO_IMPORTANTE);
+		}
 	}
 	return 0;
 }
