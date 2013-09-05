3f9895d9377471391ea481d04f7d0074f8045242
erational@erational.org <erational@erational.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-05 04:29:39 +0000

    un paquet pour spip3 passage en stable et version 1.0 pour respecter les conventions habituelles.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@75275 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ fpdf/lang/paquet-fpdf_fr.php
@@ -0,0 +1,20 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-fpdf
+// Langue: fr
+// Date: 05-09-2013 04:23:56
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// F
+	'fpdf_description' => 'Ce plugin sert à générer des pdfs via la librairie fpdf.
+		Adapter le en vous servant des tutoriaux du dossier demo.',
+	'fpdf_slogan' => 'Librairie FPDF / FPDI pour fabriquer des pdfs.',
+);
+?>
