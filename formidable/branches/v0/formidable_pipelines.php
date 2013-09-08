c321a2550cdbaf015aef8edcca7728af2bc14d8b
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-08 13:04:38 +0000

    vie privée : hasher les ip des réponse au bout du délai _CNIL_PERIODE_FORMIDABLE (code inspiré du plugin-dist forum). Par défaut 4 mois de 31 jour
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@75310 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- formidable/branches/v0/formidable_pipelines.php
+++ formidable/branches/v0/formidable_pipelines.php
@@ -25,6 +25,28 @@ function formidable_optimiser_base_disparus($flux){
 	
 	// On génère la suppression
 	$flux['data'] += optimiser_sansref('spip_formulaires_reponses', 'id_formulaires_reponse', $res);
+
+	// CNIL -- Informatique et libertes
+	//
+	// masquer le numero IP des vieilles réponses
+	//
+	## date de reference = 4 mois
+	## definir a 0 pour desactiver
+	if (!defined('_CNIL_PERIODE_FORMIDABLE')) {
+		define('_CNIL_PERIODE_FORMIDABLE', 3600*24*31*4);
+	}
+	
+	if (_CNIL_PERIODE_FORMIDABLE) {
+		$critere_cnil = 'date<"'.date('Y-m-d', time()-_CNIL_PERIODE_FORMIDABLE).'"'
+			. ' AND statut != "spam"'
+			. ' AND (ip LIKE "%.%" OR ip LIKE "%:%")'; # ipv4 ou ipv6
+		$c = sql_countsel('spip_formulaires_reponses', $critere_cnil);
+		if ($c>0) {
+			spip_log("CNIL: masquer IP de $c réponses anciennes à formidable");
+			sql_update('spip_formulaires_reponses', array('ip' => 'MD5(ip)'), $critere_cnil);
+		}
+	}
+
 	return $flux;
 }
 
