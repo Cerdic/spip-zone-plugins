502946325f37c0e5da76dabd27af8798b50d4720
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-10 21:58:32 +0000

    langues (noie)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@75561 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- noie/lang/noie_km.php
+++ noie/lang/noie_km.php
@@ -7,10 +7,6 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-	// N
-	'noie_info' => 'Pour naviguer de façon plus satisfaisante sur ce site et le reste du Web, nous vous recommandons d\'<strong>actualiser votre <a href="http://www.microsoft.com/france/windows/internet-explorer/">Internet Explorer</a></strong> ou d\'essayer un autre navigateur populaire comme <a href="http://www.mozilla-europe.org/fr/firefox/">Firefox</a>, <a href="http://www.google.fr/chrome">Chrome</a>, <a href="http://www.opera.com/">Opera</a> ou <a href="http://www.apple.com/fr/safari/">Safari</a>.', # NEW
-	'noie_titre' => 'Attention, votre Internet Explorer n\'est pas à jour !', # NEW
-
 	// T
 	'toocool_alt' => 'ត្រជាក់អារម្មណ៍ពេក សំរាប់ អិនរើណែតអិចក្សផ្លរើ',
 	'toocool_info' => 'សៃថ៍ ដែលអ្នកព្យាយាមបង្ហាញ ត្រូវបានវាយតំលៃ « ល្អពេក សំរាប់ អិនរើណែតអិចក្សផ្លរើ »។
--- noie/lang/noie_tr.php
+++ noie/lang/noie_tr.php
@@ -7,10 +7,6 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-	// N
-	'noie_info' => 'Pour naviguer de façon plus satisfaisante sur ce site et le reste du Web, nous vous recommandons d\'<strong>actualiser votre <a href="http://www.microsoft.com/france/windows/internet-explorer/">Internet Explorer</a></strong> ou d\'essayer un autre navigateur populaire comme <a href="http://www.mozilla-europe.org/fr/firefox/">Firefox</a>, <a href="http://www.google.fr/chrome">Chrome</a>, <a href="http://www.opera.com/">Opera</a> ou <a href="http://www.apple.com/fr/safari/">Safari</a>.', # NEW
-	'noie_titre' => 'Attention, votre Internet Explorer n\'est pas à jour !', # NEW
-
 	// T
 	'toocool_alt' => 'IE için aşırı "cool"',
 	'toocool_info' => 'Görüntülemek istediğiniz site « Internet Explorer için fazla » diye nitelendirilmiştir.
