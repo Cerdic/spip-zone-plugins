178daef630464895b86ff7e2c39acbf300fb9ae3
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-10 22:00:11 +0000

    langues (paquet-squirrel_chimp_lists)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@75654 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- squirrels_love_chimps_lists/trunk/lang/paquet-squirrel_chimp_lists_sk.php
+++ squirrels_love_chimps_lists/trunk/lang/paquet-squirrel_chimp_lists_sk.php
@@ -8,7 +8,6 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// S
-	'squirrel_chimp_lists_description' => 'Gestion de mailing listes de [Mailchimp->http://mailchimp.com/] inspirée des fonctionnalités du plugin Wordpress [Autochimp->http://wordpress.org/extend/plugins/autochimp/] avec quelques "emprunts" de code du plugin SPIP "MailChimp de [Ben->http://www.nursit.net]', # NEW
 	'squirrel_chimp_lists_nom' => 'E-mailové skupiny Squirrels Love Chimps',
 	'squirrel_chimp_lists_slogan' => 'Riadenie e-mailových skupín Squirrels Love Chimps'
 );
