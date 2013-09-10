a7955796f971a4590eb94e9c46ab5998264a8c13
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-10 22:00:21 +0000

    langues (thumbsites)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@75662 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- thumbsites/trunk/lang/thumbsites_fa.php
+++ thumbsites/trunk/lang/thumbsites_fa.php
@@ -8,30 +8,22 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// B
-	'bouton_copier_comme_logo' => 'Définir comme logo', # NEW
 	'bouton_rafraichir' => 'تازه‌سازي صورتك',
 
 	// C
 	'cfg_descriptif' => 'اين صفحه به شما اجازه مي‌دهد تا پلاگين تام‌سايتز (Thumbsites) را پيكربندي كنيد، و به ويژه، سرور ريزنش‌ها را انتخاب كنيد. براي اطلاعات بيشتر به اينجا مراجعه كنيد: <a href="http://www.spip-contrib.net/?article2584">documentation sur contrib</a>',
 	'cfg_inf_apercite_presentation' => '<a href="http://www.apercite.fr">apercite.fr</a> خدمات رايگاني را پيشنهاد مي‌دهد كه نيازي به شناسايي ندارد.',
-	'cfg_inf_choix_serveur' => 'Choisissez le serveur qui fournira les vignettes de vos sites référencés et compléter éventuellement son paramétrage.', # NEW
-	'cfg_inf_miwim_presentation' => 'Pour utiliser ce service vous devez mettre un lien vers le site <a href="http://thumbs.miwim.fr">Miwim</a>. La présence de ce lien est vérifié périodiquement par un script.', # NEW
 	'cfg_inf_rotothumb_presentation' => 'براي استفاده از اين خدمت لازم نيست ثبت نام كنيد اما بايد يك پيوند با سايت <a href="http://www.robothumb.com">Robothumb</a> برقرار كنيد. تدوام اين پيوند به صورت دوره‌اي توسط سرور‌هاي خودشان بررسي مي‌شود. ',
 	'cfg_inf_websnapr_presentation' => 'براي استفاده از اين خدمت بايد در سايت <a href="http://www.websnapr.com">Websnapr</a> ثبت نام كنيد. نسخه‌ي رايگان مي‌تواند 250 هزار كاپچبر (گيراندازي) سايت را در طول يك ماه و با آهنگ 80 كاپچر در ساعت پردازش كند. ',
 	'cfg_itm_serveur_apercite' => 'Apercite.fr',
-	'cfg_itm_serveur_miwim' => 'Miwim', # NEW
 	'cfg_itm_serveur_robothumb' => 'Robothumb',
 	'cfg_itm_serveur_websnapr' => 'Websnapr',
 	'cfg_itm_taille_vignette' => '@taille@ پيكسل',
 	'cfg_lbl_cle' => 'كليد شما',
 	'cfg_lbl_duree_cache' => 'دوره‌ي حافظه‌ي نزديك (به روز)',
-	'cfg_lbl_serveur' => 'Serveur', # NEW
 	'cfg_lbl_taille_vignette' => 'اندازه‌ي ريزنقش‌ها',
-	'cfg_lbl_usage' => 'Conditions d\'utilisation', # NEW
 	'cfg_lgd_cache' => 'حافظه‌‌ي نزديك',
 	'cfg_lgd_choix_serveur' => 'سرور', # MODIF
-	'cfg_titre_form' => 'Configurer Thumbsites', # NEW
-	'cfg_titre_page' => 'Thumbsites', # NEW
 
 	// T
 	'titre_thumbshot_site' => 'ريزنقش سايت'
