2fa29f7917f78a23617b31e4a463f5496e263c14
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-10 21:59:17 +0000

    langues (ppp)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@75603 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- porte_plume_extras/partout/lang/ppp_ru.php
+++ porte_plume_extras/partout/lang/ppp_ru.php
@@ -12,17 +12,10 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// C
 	'cfg_attention' => 'Внимание',
-	'cfg_description' => 'en fonction de vos squelettes, veillez à n\'activer le Porte Plume que sur des champs pour lesquels l\'utilisation des raccourcis n\'engendrera pas d\'erreur xhtml.', # NEW
 	'cfg_titre' => 'Porte Plume Partout',
 
-	// E
-	'explication_personnalisation' => 'Indiquez la cible des éléments qui utiliseront la barre typographique (Expression CSS ou étendue jQuery).', # NEW
-
 	// L
-	'label_hauteur_champ' => 'Demi-hauteur de l\'écran', # NEW
-	'label_personnalisation' => 'Sélecteur personnalisé', # NEW
 	'legend_barre_typo' => 'Добавить панель инструментов для :',
-	'legend_hauteur_champ' => 'Hauteur du champ texte pour les articles', # NEW
 	'legend_personnalisation' => 'Персонализация',
 
 	// S
