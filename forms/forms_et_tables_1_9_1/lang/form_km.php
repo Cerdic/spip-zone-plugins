126be8dba0392fc7573daff43a2239ea96d3ffdf
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-10 21:57:23 +0000

    langues (form)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@75498 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- forms/forms_et_tables_1_9_1/lang/form_km.php
+++ forms/forms_et_tables_1_9_1/lang/form_km.php
@@ -8,7 +8,6 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
-	'articles_utilisant' => 'Articles utilisant ce formulaire', # NEW
 	'aucune_reponse' => 'គ្មានចំលើយ',
 
 	// C
@@ -21,13 +20,6 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'icone_ajouter_donnees' => 'បន្ថែម ចំលើយ',
 	'icone_creer_table' => 'បង្កើត មួយបែបទថ្មី',
 	'importer_donnees_csv' => 'នាំចូល ចំលើយ',
-	'info_supprimer_formulaire' => 'Voulez-vous vraiment supprimer ce formulaire ?', # NEW
-	'info_supprimer_formulaire_reponses' => 'Des Réponse sont associées à ce formulaire. 
-Voulez-vous vraiment le supprimer ?', # NEW
-
-	// L
-	'lien_retirer_donnee_liante' => 'Retirer le lien depuis cette réponse', # NEW
-	'lien_retirer_donnee_liee' => 'Retirer cette réponse', # NEW
 
 	// N
 	'nombre_reponses' => '@nombre@ ចំលើយ',
@@ -35,19 +27,12 @@ Voulez-vous vraiment le supprimer ?', # NEW
 
 	// S
 	'suivi_reponses' => 'មើល រាល់ចំលើយ',
-	'supprimer_formulaire' => 'Supprimer ce formulaire', # NEW
 
 	// T
-	'telecharger_reponses' => 'Téléchargez les réponses', # NEW
 	'texte_donnee_statut' => 'ស្ថានភាព នៃចំលើយនេះ',
 	'texte_statut_poubelle' => 'ចំលើយ ត្រូវបានលុបចេញ',
-	'texte_statut_prepa' => 'Réponse en cours de rédaction', # NEW
-	'texte_statut_prop' => 'Réponse proposée', # NEW
-	'texte_statut_publie' => 'Réponse publiée', # NEW
-	'texte_statut_refuse' => 'Réponse refusée', # NEW
 	'titre_formulaire' => 'ចំណងជើង នៃបែបបទ',
 	'toutes_tables' => 'គ្រប់បែបបទ',
-	'type_des_tables' => 'Formulaires', # NEW
 
 	// U
 	'une_reponse' => 'មួយចំលើយ'
