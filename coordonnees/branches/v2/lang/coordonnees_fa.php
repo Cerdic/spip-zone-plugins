fd20cba67f4a876712483b04bd3b0937f99d219c
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-10 21:56:27 +0000

    langues (coordonnees)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@75447 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- coordonnees/branches/v2/lang/coordonnees_fa.php
+++ coordonnees/branches/v2/lang/coordonnees_fa.php
@@ -48,8 +48,6 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'label_numero' => 'شماره تلفن',
 	'label_objets_actifs' => 'موضوع‌ها',
 	'label_pays' => 'كشورها',
-	'label_region' => 'Région', # NEW
-	'label_telephone' => 'Téléphone', # NEW
 	'label_titre' => 'تيتر',
 	'label_type_adresse' => 'نوع نشاني',
 	'label_type_email' => 'نوع ايميل ',
@@ -74,37 +72,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'supprimer_numero' => 'حذف اين شماره تلفن',
 
 	// T
-	'titre_coordonnees' => 'دفتر تلفن',
-	'type_adr_dom' => 'Résidentielle', # NEW
-	'type_adr_home' => 'Personnelle', # NEW
-	'type_adr_intl' => 'Étrangère', # NEW
-	'type_adr_parcel' => 'Parcelle', # NEW
-	'type_adr_postal' => 'Postale (en poste restante)', # NEW
-	'type_adr_pref' => 'Principale', # NEW
-	'type_adr_work' => 'Professionnelle', # NEW
-	'type_email_internet' => 'Internet', # NEW
-	'type_email_pref' => 'Préféré', # NEW
-	'type_email_x400' => 'X.400', # NEW
-	'type_mel_home' => 'Personnel', # NEW
-	'type_mel_intl' => 'International', # NEW
-	'type_mel_work' => 'Professionnel', # NEW
-	'type_tel_bbs' => 'Service de messagerie', # NEW
-	'type_tel_car' => 'Voiture', # NEW
-	'type_tel_cell' => 'Portable', # NEW
-	'type_tel_dsl' => 'box DSL', # NEW
-	'type_tel_fax' => 'Télécopie', # NEW
-	'type_tel_home' => 'Résidence', # NEW
-	'type_tel_isdn' => 'RNIS ...ou DSL', # NEW
-	'type_tel_modem' => 'MoDem informatique', # NEW
-	'type_tel_msg' => 'à messagerie', # NEW
-	'type_tel_pager' => 'Bipeur', # NEW
-	'type_tel_pcs' => 'Service de communication personnel', # NEW
-	'type_tel_pref' => 'Favori', # NEW
-	'type_tel_text' => 'Texto', # NEW
-	'type_tel_textphone' => 'Retranscripteur texte', # NEW
-	'type_tel_video' => 'Visioconférence', # NEW
-	'type_tel_voice' => 'Vocal', # NEW
-	'type_tel_work' => 'Professionnel' # NEW
+	'titre_coordonnees' => 'دفتر تلفن'
 );
 
 ?>
