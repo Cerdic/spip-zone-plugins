9c0a1695c200b2e74b1c53937148568d3f694d6c
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-10 21:57:25 +0000

    langues (table)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@75500 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- forms/forms_et_tables_1_9_1/lang/table_km.php
+++ forms/forms_et_tables_1_9_1/lang/table_km.php
@@ -8,7 +8,6 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
-	'articles_utilisant' => 'Articles utilisant cette table', # NEW
 	'aucune_reponse' => 'គ្មានទិន្នន័យ',
 
 	// C
@@ -22,11 +21,9 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'icone_creer_table' => 'បង្កើត មួយតារាងថ្មី',
 	'importer_donnees_csv' => 'នាំចូល ទិន្នន័យ',
 	'info_supprimer_formulaire' => 'អ្នកប្រាដចង់ លុបចេញ តារាងនេះ?',
-	'info_supprimer_formulaire_reponses' => 'Des données sont associées à cette table. Voulez-vous vraiment la supprimer ?', # NEW
 
 	// L
 	'lien_retirer_donnee_liante' => 'ដកចេញ តំណភ្ជាប់ ពីទិន្នន័យនេះ',
-	'lien_retirer_donnee_liee' => 'Retirer cette donnée', # NEW
 
 	// N
 	'nombre_reponses' => '@nombre@ ទិន្នន័យ',
@@ -37,13 +34,8 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'supprimer_formulaire' => 'លុបចេញ តារាងនេះ',
 
 	// T
-	'telecharger_reponses' => 'Téléchargez les données', # NEW
 	'texte_donnee_statut' => 'ស្ថានភាព នៃទិន្នន័យនេះ',
 	'texte_statut_poubelle' => 'ទិន្នន័យ ត្រូវបានលុបចេញ',
-	'texte_statut_prepa' => 'Donnée en cours de rédaction', # NEW
-	'texte_statut_prop' => 'Donnée proposée', # NEW
-	'texte_statut_publie' => 'Donnée publiée', # NEW
-	'texte_statut_refuse' => 'Donnée refusée', # NEW
 	'titre_formulaire' => 'ចំណងជើង នៃតារាង',
 	'toutes_tables' => 'គ្រប់តារាង',
 	'type_des_tables' => 'តារាង',
