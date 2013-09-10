fd64c84a251766f8a2ce4cecc4c5629e8a3edf3f
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-10 21:57:15 +0000

    langues (entravaux)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@75490 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- en_travaux/trunk/lang/entravaux_it.php
+++ en_travaux/trunk/lang/entravaux_it.php
@@ -15,23 +15,15 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'erreur_droit' => 'Vous devez être Webmestre pour utiliser le plugin En Travaux !',
 
 	// I
-	'info_disallow_robot' => 'Votre site est protégé contre les robots et moteur de recherche pendant la maintenance.', # NEW
-	'info_maintenance_en_cours' => 'Le site est en maintenance ! Seuls les webmestres du site peuvent voir le site public et accèder à l\'espace privé.', # NEW
 	'info_message' => 'Questa pagina consente di visualizzare un messaggio temporaneo su tutte le pagine del sito durante una sessione di manutenzione.',
 	'info_travaux_texte' => 'Questo sito è stato in manutenzione.
 _ Torna più tardi...',
-
-	// L
-	'label_disallow_robots' => 'Protéger le site contre les robots et moteur de recherche', # NEW
 
 	// M
 	'message_temporaire' => 'Il vostro messaggio di manutenzione:',
 
 	// P
-	'parametrage_page_travaux' => 'Configurazione della pagina di manutenzione',
-
-	// T
-	'texte_lien_publier' => 'Rendre le site public' # NEW
+	'parametrage_page_travaux' => 'Configurazione della pagina di manutenzione'
 );
 
 ?>
