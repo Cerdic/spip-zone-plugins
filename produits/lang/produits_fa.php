f5f6a110f30db552529f76acd34e6efcb308bfa9
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-10 21:59:22 +0000

    langues (produits)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@75608 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- produits/lang/produits_fa.php
+++ produits/lang/produits_fa.php
@@ -8,11 +8,6 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// C
-	'configurer_editer_ttc_explication' => 'Les prix des produits sont éditables en TTC et non plus en HT', # NEW
-	'configurer_editer_ttc_label' => 'Editer les produits en TTC', # NEW
-	'configurer_precision_ttc_explication' => 'Indiquer la précision d\'arrondi après la virgule pour les prix TTC (par défaut 2)', # NEW
-	'configurer_precision_ttc_label' => 'Précision sur les prix TTC', # NEW
-	'configurer_taxe_defaut_explication' => 'Saisir la taxe par défaut applicable aux produits sous la forme 0.196 pour une TVA à 19,6% par exemple (cette valeur peut être surchargée ensuite pour chaque produit). Attention cependant à ne pas configurer à nouveau cette taxe dans un autre plugin (Paypal par exemple), sinon laissez 0.', # NEW
 	'configurer_taxe_defaut_label' => 'ماليات پيش‌ گزيده',
 	'configurer_titre' => 'پيكربندي كالا‌ها',
 
@@ -21,17 +16,10 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'info_aucun_produit' => 'هيج كالا',
 	'info_nb_produits' => '@nb@ كالا',
 
-	// L
-	'limiter_ajout_explication' => 'Restreindre l\'ajout de produits à un ou plusieurs secteurs', # NEW
-	'limiter_ajout_label' => 'Limiter', # NEW
-	'limiter_ident_secteur_explication' => 'Choisir le ou les secteurs concernés', # NEW
-	'limiter_ident_secteur_label' => 'Choix', # NEW
-
 	// P
 	'produit_bouton_ajouter' => 'افزودن يك كالا',
 	'produit_champ_descriptif_label' => 'شرح مختصر',
 	'produit_champ_prix_ht_label' => 'قيمت خالص (بي‌عوارض)',
-	'produit_champ_prix_ttc_label' => 'Prix TTC', # NEW
 	'produit_champ_reference_label' => 'ارجاع',
 	'produit_champ_rubrique_label' => 'بخش ',
 	'produit_champ_taxe_explication' => 'قيمت پيش‌ گزيده روي اين سايت: @taxe@.مي‌توانيد اين ميدان را خالي بگذاريد.',
@@ -44,13 +32,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'produit_numero' => 'كالاي شماره‌ي ',
 	'produit_reference' => 'ارجاع "@reference@"',
 	'produit_statut' => 'تغيير وضعيت:‌',
-	'produits_titre' => 'كالاها',
-
-	// S
-	'secteurs_autorises' => 'Choisir une rubrique dans un des secteurs autorisés : @secteurs@', # NEW
-
-	// T
-	'tous_les_produits' => 'Tous les produits' # NEW
+	'produits_titre' => 'كالاها'
 );
 
 ?>
