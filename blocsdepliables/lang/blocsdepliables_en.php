be6be742391d1483e80b84abc63565ca4d20e855
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-10 21:55:55 +0000

    langues (blocsdepliables)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@75418 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- blocsdepliables/lang/blocsdepliables_en.php
+++ blocsdepliables/lang/blocsdepliables_en.php
@@ -15,10 +15,6 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'cfg_exemple_explication' => 'Example explanation',
 	'cfg_titre_parametrages' => 'Parameters',
 
-	// E
-	'explication_cookie' => 'L\'état des blocs numérotés sera stocké dans un cookie le temps de la session, afin de conserver l’aspect de la page en cas de retour.', # NEW
-	'explication_unique' => 'L\'ouverture d’un bloc provoquera la fermeture de tous les autres blocs de la page, afin d’en avoir qu’un seul ouvert à la fois.', # NEW
-
 	// L
 	'label_animation' => 'Animation',
 	'label_animation_aucun' => 'None',
@@ -28,7 +24,6 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'label_animation_slow' => 'Slow slide',
 	'label_balise_titre' => 'Tag for the blocks title',
 	'label_cookie_1' => 'Remember the blocks state',
-	'label_unique_1' => 'Un seul bloc ouvert sur la page', # NEW
 
 	// T
 	'titre_page_configurer_blocsdepliables' => 'Expanding containers blocks'
