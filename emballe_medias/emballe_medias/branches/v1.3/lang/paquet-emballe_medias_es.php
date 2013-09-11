77d9dbfc18cae1be3eb592d763709eb48d59df96
lesage.sylvain@laposte.net <lesage.sylvain@laposte.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-11 14:32:51 +0000

    Traduction en espagnol pour v1.3 (copie violente depuis trunk)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@75749 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ emballe_medias/emballe_medias/branches/v1.3/lang/paquet-emballe_medias_es.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-emballe_medias?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// E
+	'emballe_medias_description' => 'Formulario configurable que envuelve los medias desde el espacio público...',
+	'emballe_medias_nom' => 'Envuelve Medias',
+	'emballe_medias_slogan' => 'Envolver sus documentos correctamente'
+);
+
+?>
