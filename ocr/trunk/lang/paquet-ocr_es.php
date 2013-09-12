fb25bf6902be4ca291bae02f357c73d1152cda93
severo@rednegra.net <severo@rednegra.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-12 03:36:59 +0000

    langues (paquet-ocr)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@75764 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ ocr/trunk/lang/paquet-ocr_es.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-ocr?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// O
+	'ocr_description' => 'El plugin permite analizar un archivo imagen (por ejemplo un documento escaneado) para extraer el texto contenido. Utiliza la librería de OCR (Optical character recognition) [tesseract->http://code.google.com/p/tesseract-ocr/wiki/ReadMe].',
+	'ocr_nom' => 'ocr',
+	'ocr_slogan' => 'Extraer un texto de una imagen por OCR'
+);
+
+?>
