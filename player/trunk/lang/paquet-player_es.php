fbeebe7b688de2388c24a4450c84253de9b01700
severo@rednegra.net <severo@rednegra.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-13 03:36:23 +0000

    langues (paquet-player)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@75801 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ player/trunk/lang/paquet-player_es.php
@@ -0,0 +1,20 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-player?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// P
+	'player_description' => 'Este plugin permite la reproducción de sonidos.
+Añade reproductores flash adaptados a los formatos .mp3.
+Actúa sobre todos los <code><docXX|player></code> insertados en los textos, de la misma manera en los esqueletos.
+
+-* En un texto <code><docXX|player></code> muestra un reproductor flash audio (varios reproductores para elegir) ;
+-* En un esqueleto <code>#MODELE{playliste}</code> permite mostrar una playlist de los últimos mp3.',
+	'player_slogan' => 'Reproducir sonidos mp3'
+);
+
+?>
