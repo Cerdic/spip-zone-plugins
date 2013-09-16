5d9d5a98df2cad5dea2e7ab69abcc917c05763a2
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-16 09:11:04 +0000

    les noisettes dans le squelette
    Le fichier de langue du paquet.xml
    
    On renomme le titre du squelette
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@75903 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ trad-lang/squelette/trunk/lang/paquet-tradlang_skel_fr.php
@@ -0,0 +1,13 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/trad-lang/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// T
+	'tradlang_skel_description' => 'Le squelette utilisé par [le Tradlang officiel->http://trad.spip.net].',
+	'tradlang_skel_slogan' => 'Squelette pour Tradlang'
+);
+
+?>
