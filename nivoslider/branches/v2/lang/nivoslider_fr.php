0d19db8abef141c07d629b6efa04ed7554d12974
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-16 10:31:41 +0000

    branchons la v2 avant modifs significatives du plugin
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@75904 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ nivoslider/branches/v2/lang/nivoslider_fr.php
@@ -0,0 +1,62 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/cfg/lang/
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	// A
+	// B
+	'bouton_effacer' => 'Effacer',
+	// C
+	'controls' => 'Contrôles',
+	'cfg_title_nivoslider' => 'Configuration du carrousel NivoSlider',
+	// D
+	'default' => 'Diaporama par défaut',
+	// E
+	// F
+	// G
+	'general' => 'Apparence générale',
+	// H
+	// I
+	// J
+	// K
+	// L
+	'label_animSpeed' => 'Vitesse de l\'animation',
+	'label_height' => 'Hauteur (px)',
+	'label_width' => 'Largeur (px)',
+	'label_effect' => 'Effet de transition',
+	'label_slices' => 'Nombre de tranches',
+	'label_pauseTime' => 'Pause entre les images',
+	'label_imageAlign' => 'Recadrer',
+	'label_imageBackcolor' => 'Couleur de fond',
+	'label_false' => 'non',
+	'label_true' => 'oui',
+	'label_directionNav' => 'Activer les flèches de navigation',
+	'label_directionNavHide' => 'Cacher les fl&egrave;ches (afficher au survol)',
+	'label_keyboardNav' => 'Utiliser les fl&eacute;ches droite &amp; gauche du clavier',
+	'label_pauseOnHover' => 'Pause au survol',
+	'label_controlNavThumbs' => 'Activer les miniatures',
+	'label_controlNav' => 'Afficher les puces',
+	'label_bottomMargin' => 'Marge sous le slider (px)',
+	'label_captionBackcolor' => 'Couleur du fond',
+	'label_captionFrontcolor' => 'Couleur du texte',
+	'label_captionOpacity' => 'Transparence',
+	'label_id_defaut' => 'Article utilisé comme réservoir d\'images lors de l\'inclusion du modèle [(#MODELE{nivoslider})] sans paramètre <i>id_article</i> à partir d\'un squelette :',
+	'legend' => 'Légendes',
+	// M
+	// N
+	// O
+	// P
+	// Q
+	// R
+	// S
+	// T
+	// U
+	// V
+	// W
+	// X
+	// Y
+	// Z
+);
+
+?>
