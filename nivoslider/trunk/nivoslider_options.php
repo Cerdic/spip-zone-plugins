5acf171286acc675751df860f370e309761259ae
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-17 09:06:50 +0000

    Renommer le dossier js/ en javascript/ (convention SPIP) Plus de js dans le modele : il passe en script externe javascript/nivoslider.init.js Lorsqu'un slider est dans la page, on insere le js de chargement en pied de page, avec un jQuery.getScript sur le js packé de nivoslider et le js init des sliders en callback. Permet un chargement totalement async du slider (js + images).
    Le plugin passe en test
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@75935 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- nivoslider/trunk/nivoslider_options.php
+++ nivoslider/trunk/nivoslider_options.php
@@ -19,9 +19,12 @@ function nivoslider_affichage_final($texte){
 	if (($GLOBALS['html'] OR test_espace_prive())
 	  AND strpos($texte,"nivoSlider")!==false
 		AND stripos($texte,"</script>")
-	  AND $p = stripos($texte,"</head>")
+	  AND $p = stripos($texte,"</body>")
 	){
-		$ins = '<script src="'.find_in_path('js/jquery.nivo.slider.pack.js').'" type="text/javascript"></script>';
+		$js = find_in_path('javascript/jquery.nivo.slider.pack.js');
+		lire_fichier(find_in_path('javascript/nivoslider.init.js'),$init);
+		$ins = '<script type="text/javascript">/*<![CDATA[*/
+jQuery.getScript("'.$js.'",function(){'.$init.'});/*]]>*/</script>';
 		$texte = substr_replace($texte,$ins,$p,0);
 	}
 	return $texte;
