5702357f03038abc1f9f27f2b8e1f553acaaf20a
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-17 09:12:18 +0000

    utiliser correctement l'API de champs extras
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@75936 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- activite_editoriale/trunk/base/activite_editoriale.php
+++ activite_editoriale/trunk/base/activite_editoriale.php
@@ -7,21 +7,21 @@ function activite_editoriale_declarer_champs_extras($champs = array()){
 		'options'=>array(
 			'nom'=>'extras_delai',
 			'sql' => "tinytext NOT NULL DEFAULT ''", // declaration sql
-			'rechercher'=>false
-			),
-		'label' => 'activite_editoriale:delai', // chaine de langue 'mon_plug:mon_label'
-		'explication' => 'activite_editoriale:delai_precisions', // precisions sur le champ
-		'obligatoire' => false, // 'true', 'false' ou ''
+			'rechercher'=>false,
+            'label' => _T('activite_editoriale:delai'), // chaine de langue 'mon_plug:mon_label'
+            'explication' => 'activite_editoriale:delai_precisions', // precisions sur le champ
+            'obligatoire' => false, // 'true', 'false' ou ''
+            )
 			);
 	$champs['spip_rubriques']['extras_identifiants'] = array(
 	'saisie'=>'input',
 		'options'=>array(
 			'nom'=>'extras_identifiants',
 			'rechercher'=>false,
-			'sql' => "tinytext NOT NULL DEFAULT ''"), // declaration sql
-		'label' => 'activite_editoriale:identifiants', // chaine de langue 'mon_plug:mon_label'
-		'explication' => 'activite_editoriale:identifiants_precisions', // precisions sur le champ
-		'obligatoire' => false, // 'true', 'false' ou ''
+            'label' => _T('activite_editoriale:identifiants'), // chaine de langue 'mon_plug:mon_label'
+            'explication' => 'activite_editoriale:identifiants_precisions', // precisions sur le champ
+            'obligatoire' => false, // 'true', 'false' ou ''
+			'sql' => "tinytext NOT NULL DEFAULT ''") // declaration sql
 	);
 	return $champs;
 }
