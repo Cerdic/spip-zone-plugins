0dd5d0300ddd67792d82ffc1256949c7944472cf
pierre.troller@gmail.com <pierre.troller@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-20 13:42:13 +0000

    c'est l'invité qui recevait la notification de son acceptation, non pas l'emetteur de l'invitation...
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@75985 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- amis/action/ajouterami.php
+++ amis/action/ajouterami.php
@@ -31,7 +31,7 @@ function action_ajouterami_dist(){
 		if ($row=sql_fetsel('*','spip_amis',array('id_auteur='.intval($id_ami),'id_ami='.intval($id_auteur),"statut='prop'"))){
 			sql_updateq('spip_amis',array('statut'=>'publie','date'=>'NOW()'),array('id_auteur='.intval($id_ami),'id_ami='.intval($id_auteur)));
 			$notification = charger_fonction('notifications','inc');
-			$notification('ajouterami',$id_ami,array('id_auteur' => $id_auteur));
+			$notification('ajouterami',$id_auteur,array('id_auteur' => $id_ami));
 		}
 		else {
 			// sinon lancer une invitation
