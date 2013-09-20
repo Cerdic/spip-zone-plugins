d2762ac798b8c97acdb50c0ea03c62e0a1053f0e
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-20 16:07:26 +0000

    Debut migration du plugin vers SPIP 3, debug a faire
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@75988 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ rechremp/trunk/lang/paquet-rechremp_fr.php
@@ -0,0 +1,19 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-rechremp
+// Langue: fr
+// Date: 20-09-2013 16:36:17
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// R
+	'rechremp_description' => 'Rechercher les occurences d\'un texte et les remplacer par un autre texte, dans les champs de type texte des objets SPIP.',
+	'rechremp_slogan' => 'Rechercher des chaines dans la BDD SPIP et eventuellement les remplacer par une autre chaine',
+);
+?>
