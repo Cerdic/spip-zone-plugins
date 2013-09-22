8178cd911199494d9d1d42522405de97dd1240e7
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-22 17:02:21 +0000

    langues (paquet-ieconfig)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76190 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- ieconfig/trunk/lang/paquet-ieconfig_fr.php
+++ ieconfig/trunk/lang/paquet-ieconfig_fr.php
@@ -6,7 +6,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// I
-	'ieconfig_description' => 'Ce plugin permet d\'exporter la configuration de SPIP et des plugins compatibles dans un seul et unique fichier au format YAML. Ces fichiers de configuration peuvent ensuite être importés.',
+	'ieconfig_description' => 'Ce plugin permet d’exporter la configuration de SPIP et des plugins compatibles dans un seul et unique fichier au format YAML. Ces fichiers de configuration peuvent ensuite être importés.',
 	'ieconfig_slogan' => 'Importer, exporter des configurations de plugins'
 );
 
