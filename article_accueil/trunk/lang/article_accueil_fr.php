29f10d5f3b2fe3fa23aa0d21f70f9b1e8f979352
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-22 17:00:21 +0000

    langues (article_accueil)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76089 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- article_accueil/trunk/lang/article_accueil_fr.php
+++ article_accueil/trunk/lang/article_accueil_fr.php
@@ -6,14 +6,14 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
-	'article_accueil' => 'Article d\'accueil',
+	'article_accueil' => 'Article d’accueil',
 	'aucun_article_accueil' => 'Aucun article',
 
 	// L
-	'label_id_article_accueil' => 'Article d\'accueil',
+	'label_id_article_accueil' => 'Article d’accueil',
 
 	// R
-	'rubrique_article_en_accueil' => 'Article d\'accueil :'
+	'rubrique_article_en_accueil' => 'Article d’accueil :'
 );
 
 ?>
