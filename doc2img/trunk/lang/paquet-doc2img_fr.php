bde6667f6be19b7d5c99b46a76ccf0ac3eeb6dbf
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-22 17:01:38 +0000

    langues (paquet-doc2img)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76156 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- doc2img/trunk/lang/paquet-doc2img_fr.php
+++ doc2img/trunk/lang/paquet-doc2img_fr.php
@@ -6,7 +6,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// D
-	'doc2img_description' => 'Ce plugin permet de convertir à la volée des pdf, tiff et autres types de document en image ou série d\'images. Ces images sont ensuite exploitables via une boucle spécifique.',
+	'doc2img_description' => 'Ce plugin permet de convertir à la volée des pdf, tiff et autres types de document en image ou série d’images. Ces images sont ensuite exploitables via une boucle spécifique.',
 	'doc2img_nom' => 'Documents en Images',
 	'doc2img_slogan' => 'Convertir des documents textuels à la volée'
 );
