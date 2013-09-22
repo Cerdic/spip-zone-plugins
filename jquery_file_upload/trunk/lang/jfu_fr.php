dfeccfaa3cd56c35b5d7c491344924ec8d6b21da
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-22 17:02:31 +0000

    langues (jfu)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76200 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- jquery_file_upload/trunk/lang/jfu_fr.php
+++ jquery_file_upload/trunk/lang/jfu_fr.php
@@ -20,7 +20,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'erreur_fichier_trop_petit' => 'Le fichier est trop petit.',
 	'erreur_invalid_file_type' => 'Type de fichier invalide.',
 	'erreur_max_number_files' => 'Vous avez dépassé le nombre maximal de fichiers',
-	'erreur_message_erreur' => 'Erreur :',
+	'erreur_message_erreur' => 'Erreur :',
 	'erreur_uploadedbytes' => 'Le nombre de bytes envoyés dépassent la taille maximale',
 
 	// L
@@ -28,9 +28,9 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'legend_mise_en_ligne_unique' => 'Choisissez le fichier...',
 
 	// M
-	'message_file_size_limit' => 'La taille maximale d\'un fichier est de @taille@ MB',
-	'message_queue_limit_max' => 'La limite maximale du nombre de fichiers dans la file d\'attente est @nb@.',
-	'message_types_fichiers_autorises' => 'L\'ensemble des extensions de fichier autorisées est : @types@',
+	'message_file_size_limit' => 'La taille maximale d’un fichier est de @taille@ MB',
+	'message_queue_limit_max' => 'La limite maximale du nombre de fichiers dans la file d’attente est @nb@.',
+	'message_types_fichiers_autorises' => 'L’ensemble des extensions de fichier autorisées est : @types@',
 
 	// T
 	'temps_passe' => 'passé',
