9b60f51df8e26f2e85139b494f97e666008c629c
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-22 17:02:38 +0000

    langues (paquet-licence)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76207 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- licence/lang/paquet-licence_fr.php
+++ licence/lang/paquet-licence_fr.php
@@ -6,7 +6,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// L
-	'licence_description' => 'Permet de relier une licence d\'utilisation à un article ou un document',
+	'licence_description' => 'Permet de relier une licence d’utilisation à un article ou un document',
 	'licence_nom' => 'Licence',
 	'licence_slogan' => 'Une Licence pour articles et documents'
 );
