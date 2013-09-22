a1df55af5ba18b092486bf2dbc38dbd666b9cf4d
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-22 17:01:24 +0000

    langues (paquet-diogene)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76143 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- diogene/diogene/trunk/lang/paquet-diogene_fr.php
+++ diogene/diogene/trunk/lang/paquet-diogene_fr.php
@@ -6,9 +6,9 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// D
-	'diogene_description' => 'Le plugin Diogène permet de créer des masques de formulaires spécifiques par secteur sur les trois objets spécifiques que sont :
--* les articles;
--* les rubriques;
+	'diogene_description' => 'Le plugin Diogène permet de créer des masques de formulaires spécifiques par secteur sur les trois objets spécifiques que sont :
+-* les articles ;
+-* les rubriques ;
 -* les sites
 
 Il permet ainsi de définir en fonction d’un secteur particulier, un masque de formulaire par objet, ajoutant ou enlevant ainsi des champs afin de rendre le formulaire d’édition conforme à ce que l’on souhaite réellement publier dans ces secteurs mais également rassemblant au sein d’un même formulaire d’édition l’ensemble des informations permettant de publier rapidement.',
