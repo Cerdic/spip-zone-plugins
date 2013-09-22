a04d0e7240bfd0ef9d62b9426bd848950bf6b90e
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-22 17:00:31 +0000

    langues (blocsdepliables)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76097 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- blocsdepliables/lang/blocsdepliables_fr.php
+++ blocsdepliables/lang/blocsdepliables_fr.php
@@ -14,8 +14,8 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'cfg_titre_parametrages' => 'Paramétrages',
 
 	// E
-	'explication_cookie' => 'L\'état des blocs numérotés sera stocké dans un cookie le temps de la session, afin de conserver l’aspect de la page en cas de retour.',
-	'explication_unique' => 'L\'ouverture d’un bloc provoquera la fermeture de tous les autres blocs de la page, afin d’en avoir qu’un seul ouvert à la fois.',
+	'explication_cookie' => 'L’état des blocs numérotés sera stocké dans un cookie le temps de la session, afin de conserver l’aspect de la page en cas de retour.',
+	'explication_unique' => 'L’ouverture d’un bloc provoquera la fermeture de tous les autres blocs de la page, afin d’en avoir qu’un seul ouvert à la fois.',
 
 	// L
 	'label_animation' => 'Animation',
@@ -25,7 +25,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'label_animation_normal' => 'Glissement normal',
 	'label_animation_slow' => 'Glissement lent',
 	'label_balise_titre' => 'Balise pour les titres des blocs',
-	'label_cookie_1' => 'Mémoriser l\'état des blocs',
+	'label_cookie_1' => 'Mémoriser l’état des blocs',
 	'label_unique_1' => 'Un seul bloc ouvert sur la page',
 
 	// T
