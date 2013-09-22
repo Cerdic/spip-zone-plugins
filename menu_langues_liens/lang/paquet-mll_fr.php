7cc82ba47a44a63c45cddaa7f4479c869a7a478f
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-22 17:02:48 +0000

    langues (paquet-mll)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76217 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- menu_langues_liens/lang/paquet-mll_fr.php
+++ menu_langues_liens/lang/paquet-mll_fr.php
@@ -6,7 +6,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// M
-	'mll_description' => 'Remplace le menu déroulant de langues par un menu cliquable. Ce menu n\'est affiché que s\'il y a au moins 2 langues vraiment utilisées dans le site.',
+	'mll_description' => 'Remplace le menu déroulant de langues par un menu cliquable. Ce menu n’est affiché que s’il y a au moins 2 langues vraiment utilisées dans le site.',
 	'mll_nom' => 'Menu de langues avec liens',
 	'mll_slogan' => 'Afficher les langues sous forme de liens cliquables'
 );
