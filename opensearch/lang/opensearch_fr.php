10db9dc2a8fd2cccf309371cbe5ed5abbead0f35
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-22 17:03:35 +0000

    langues (opensearch)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76258 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- opensearch/lang/opensearch_fr.php
+++ opensearch/lang/opensearch_fr.php
@@ -7,9 +7,9 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// C
 	'cfg_boite_opensearch' => 'Configuration du plugin OpenSearch',
-	'cfg_descr_opensearch' => 'OpenSearch : Moteur de recherche personnalisée pour votre site.',
+	'cfg_descr_opensearch' => 'OpenSearch : Moteur de recherche personnalisée pour votre site.',
 	'cfg_inf_id_rubrique' => 'Sélectionnez la rubrique dans laquelle effectuer la recherche.',
-	'cfg_inf_page_recherche' => 'Indiquez le squelette à utiliser pour la recherche sur le site (exemple : recherche pour le squelette recherche.html)',
+	'cfg_inf_page_recherche' => 'Indiquez le squelette à utiliser pour la recherche sur le site (exemple : recherche pour le squelette recherche.html)',
 	'cfg_lbl_id_rubrique' => 'Rubrique cible',
 	'cfg_lbl_page_recherche' => 'Squelette de recherche',
 	'cfg_option_tout' => 'Tout le site',
