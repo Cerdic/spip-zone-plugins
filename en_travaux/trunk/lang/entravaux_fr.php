7185619f5c79ef482b4313f9d5ae70e61bfe178a
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-22 17:01:53 +0000

    langues (entravaux)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76169 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- en_travaux/trunk/lang/entravaux_fr.php
+++ en_travaux/trunk/lang/entravaux_fr.php
@@ -10,11 +10,11 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// E
 	'en_travaux' => 'En travaux',
-	'erreur_droit' => 'Vous devez être Webmestre pour utiliser le plugin En Travaux !',
+	'erreur_droit' => 'Vous devez être Webmestre pour utiliser le plugin En Travaux !',
 
 	// I
 	'info_disallow_robot' => 'Votre site est protégé contre les robots et moteur de recherche pendant la maintenance.',
-	'info_maintenance_en_cours' => 'Le site est en maintenance ! Seuls les webmestres du site peuvent voir le site public et accèder à l\'espace privé.',
+	'info_maintenance_en_cours' => 'Le site est en maintenance ! Seuls les webmestres du site peuvent voir le site public et accèder à l’espace privé.',
 	'info_message' => 'Vous pouvez activer la maintenance du site et configurer un message temporaire sur toute les pages du site pendant une phase de maintenance.',
 	'info_travaux_texte' => 'Ce site est actuellement en travaux.
 _ Revenez plus tard.',
@@ -23,7 +23,7 @@ _ Revenez plus tard.',
 	'label_disallow_robots' => 'Protéger le site contre les robots et moteur de recherche',
 
 	// M
-	'message_temporaire' => 'Votre message temporaire :',
+	'message_temporaire' => 'Votre message temporaire :',
 
 	// P
 	'parametrage_page_travaux' => 'Paramétrage de la page temporaire',
