c6b2e21fa7c621d987f0138d9dd48e34aaf0c5ab
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-22 17:04:17 +0000

    langues (produits)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76286 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- produits/lang/produits_fr.php
+++ produits/lang/produits_fr.php
@@ -8,7 +8,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// C
 	'configurer_editer_ttc_explication' => 'Les prix des produits sont éditables en TTC et non plus en HT',
 	'configurer_editer_ttc_label' => 'Editer les produits en TTC',
-	'configurer_precision_ttc_explication' => 'Indiquer la précision d\'arrondi après la virgule pour les prix TTC (par défaut 2)',
+	'configurer_precision_ttc_explication' => 'Indiquer la précision d’arrondi après la virgule pour les prix TTC (par défaut 2)',
 	'configurer_precision_ttc_label' => 'Précision sur les prix TTC',
 	'configurer_taxe_defaut_explication' => 'Saisir la taxe par défaut applicable aux produits sous la forme 0.196 pour une TVA à 19,6% par exemple (cette valeur peut être surchargée ensuite pour chaque produit).',
 	'configurer_taxe_defaut_label' => 'Taxe par défaut',
@@ -20,7 +20,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'info_nb_produits' => '@nb@ produits',
 
 	// L
-	'limiter_ajout_explication' => 'Restreindre l\'ajout de produits à un ou plusieurs secteurs',
+	'limiter_ajout_explication' => 'Restreindre l’ajout de produits à un ou plusieurs secteurs',
 	'limiter_ajout_label' => 'Limiter',
 	'limiter_ident_secteur_explication' => 'Choisir le ou les secteurs concernés',
 	'limiter_ident_secteur_label' => 'Choix',
@@ -32,20 +32,20 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'produit_champ_prix_ttc_label' => 'Prix TTC',
 	'produit_champ_reference_label' => 'Référence',
 	'produit_champ_rubrique_label' => 'Rubrique',
-	'produit_champ_taxe_explication' => 'Valeur par défaut sur le site : @taxe@. Ce champ peut être laissé vide.',
+	'produit_champ_taxe_explication' => 'Valeur par défaut sur le site : @taxe@. Ce champ peut être laissé vide.',
 	'produit_champ_taxe_label' => 'Taxe',
 	'produit_champ_texte_label' => 'Texte',
 	'produit_champ_titre_label' => 'Titre',
 	'produit_editer' => 'Éditer le produit',
-	'produit_modifier' => 'Modifier le produit :',
+	'produit_modifier' => 'Modifier le produit :',
 	'produit_nouveau' => 'Nouveau produit',
 	'produit_numero' => 'Produit n°',
 	'produit_reference' => 'Référence "@reference@"',
-	'produit_statut' => 'Modifier le statut :',
+	'produit_statut' => 'Modifier le statut :',
 	'produits_titre' => 'Produits',
 
 	// S
-	'secteurs_autorises' => 'Choisir une rubrique dans un des secteurs autorisés : @secteurs@',
+	'secteurs_autorises' => 'Choisir une rubrique dans un des secteurs autorisés : @secteurs@',
 
 	// T
 	'tous_les_produits' => 'Tous les produits'
