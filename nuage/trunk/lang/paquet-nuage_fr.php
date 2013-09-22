ec154a9688695a5e1e63d0c8bd1f615eff6afdf3
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-22 17:03:24 +0000

    langues (paquet-nuage)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76249 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- nuage/trunk/lang/paquet-nuage_fr.php
+++ nuage/trunk/lang/paquet-nuage_fr.php
@@ -6,12 +6,12 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// N
-	'nuage_description' => 'Ce plugin permet d’afficher les mots-clés en vrac, en faisant varier la taille de la police en fonction du nombre d\'articles associés au mot-clé. En d\'autres termes, spip permet de créer des « tags » !
+	'nuage_description' => 'Ce plugin permet d’afficher les mots-clés en vrac, en faisant varier la taille de la police en fonction du nombre d’articles associés au mot-clé. En d’autres termes, spip permet de créer des « tags » !
 
-	Plusieurs modèles disponibles :
--* article_nuage : affiche les mots-clés de l\'article la taille variant en fonction de la fréquence des mots dans les articles du site
--* rubrique_nuage : affiche les mots-clés des articles de la branche, la taille variant en fonction de la fréquence des mots dans les articles de cette branche
--* nuage_popularite : affiche les mots-clés du site (ou d\'un groupe), la taille variant en fonction de sa popularité relative (somme des popularité des articles ayant ce mot)',
+	Plusieurs modèles disponibles :
+-* article_nuage : affiche les mots-clés de l’article la taille variant en fonction de la fréquence des mots dans les articles du site
+-* rubrique_nuage : affiche les mots-clés des articles de la branche, la taille variant en fonction de la fréquence des mots dans les articles de cette branche
+-* nuage_popularite : affiche les mots-clés du site (ou d’un groupe), la taille variant en fonction de sa popularité relative (somme des popularité des articles ayant ce mot)',
 	'nuage_slogan' => 'Afficher les mots-clés en faisant varier la taille de la police'
 );
 
