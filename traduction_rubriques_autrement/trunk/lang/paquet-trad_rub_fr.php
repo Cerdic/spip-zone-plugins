8593821869f4be63fa7e22c204cc1b9db4a49fe1
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-22 17:05:32 +0000

    langues (paquet-trad_rub)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76357 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- traduction_rubriques_autrement/trunk/lang/paquet-trad_rub_fr.php
+++ traduction_rubriques_autrement/trunk/lang/paquet-trad_rub_fr.php
@@ -6,7 +6,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// T
-	'trad_rub_description' => 'Ce plugin, déstiné pour les sites mutlilingues avec [un secteur par langue->http://www.spip-contrib.net/Un-exemple-de-site-multilingue], vous offre une approche similaire au niveau de la navigation et de la gestion de traductions des rubriques à celle du plugin [Traduction articles autrement->http://files.spip.org/spip-zone/traduction_articles_autrement.zip] dont l\'installation est recommandé. Comme spip ne propose pas nativement la traduction des rubriques l\'installation du plugin [Traduction entre rubrique->http://files.spip.org/spip-zone/tradrub.zip] est nécessaire.',
+	'trad_rub_description' => 'Ce plugin, déstiné pour les sites mutlilingues avec [un secteur par langue->http://www.spip-contrib.net/Un-exemple-de-site-multilingue], vous offre une approche similaire au niveau de la navigation et de la gestion de traductions des rubriques à celle du plugin [Traduction articles autrement->http://files.spip.org/spip-zone/traduction_articles_autrement.zip] dont l’installation est recommandé. Comme spip ne propose pas nativement la traduction des rubriques l’installation du plugin [Traduction entre rubrique->http://files.spip.org/spip-zone/tradrub.zip] est nécessaire.',
 	'trad_rub_nom' => 'Traductions de rubriques autrement',
 	'trad_rub_slogan' => 'Naviguer entre les rubriques traduites.'
 );
