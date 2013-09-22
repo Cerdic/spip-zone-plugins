4304b95682b608291f7b4a9abbe7da61d7f02e18
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-22 17:02:09 +0000

    langues (paquet-fulltext)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76181 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- fulltext/lang/paquet-fulltext_fr.php
+++ fulltext/lang/paquet-fulltext_fr.php
@@ -6,7 +6,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// F
-	'fulltext_description' => 'Ce plugin permet d\'une part d\'exploiter le mode de recherche FULLTEXT de MySQL et d\'améliorer ainsi énormément les recherches par rapport au fonctionnement natif de SPIP, et d\'autre part d\'indexer le contenu de certains documents.
+	'fulltext_description' => 'Ce plugin permet d’une part d’exploiter le mode de recherche FULLTEXT de MySQL et d’améliorer ainsi énormément les recherches par rapport au fonctionnement natif de SPIP, et d’autre part d’indexer le contenu de certains documents.
 _ Pour un fonctionnement optimal, certains programmes complémentaires sont nécessaires sur le serveur et doivent être paramétrés dans <code>mes_options.php</code> ou via le panneau de configuration.
 _ Voir la documentation du plugin pour plus de détails.',
 	'fulltext_slogan' => 'Indexation FULLTEXT pour accélérer les recherches'
