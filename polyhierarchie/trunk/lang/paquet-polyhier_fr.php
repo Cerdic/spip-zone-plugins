ed204bacaaec2a3b4b862dbded47fc5ee813d009
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-22 17:04:09 +0000

    langues (paquet-polyhier)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76278 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- polyhierarchie/trunk/lang/paquet-polyhier_fr.php
+++ polyhierarchie/trunk/lang/paquet-polyhier_fr.php
@@ -7,7 +7,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// P
 	'polyhier_description' => 'Plugin permettant de créer des liens transversaux en rattachant des rubriques
-	et articles à d\'autres rubriques que leur parent direct.',
+	et articles à d’autres rubriques que leur parent direct.',
 	'polyhier_slogan' => 'Rattacher un article ou une rubrique à plusieurs rubriques parentes'
 );
 
