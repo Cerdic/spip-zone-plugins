99a8843c73060613419f851c5c6f02a72cc934b1
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-22 17:03:25 +0000

    langues (ocr)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76250 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- ocr/trunk/lang/ocr_fr.php
+++ ocr/trunk/lang/ocr_fr.php
@@ -6,30 +6,30 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
-	'analyser_erreur_1' => 'Appel incorrect de l\'executable d\'analyse OCR',
+	'analyser_erreur_1' => 'Appel incorrect de l’executable d’analyse OCR',
 	'analyser_erreur_2' => 'Problème de mémoire',
-	'analyser_erreur_3' => 'Impossible d\'analyser le fichier, il doit être dans un format non pris en charge.',
+	'analyser_erreur_3' => 'Impossible d’analyser le fichier, il doit être dans un format non pris en charge.',
 	'analyser_erreur_autre' => 'Erreur inconnue',
 	'analyser_erreur_document_inexistant' => 'Document inexistant',
 	'analyser_erreur_executable_introuvable' => 'Exécutable introuvable',
-	'analyser_erreur_fichier_resultat' => 'Le fichier de résultat de l\'analyse OCR n\'existe pas ou n\'est pas lisible.',
+	'analyser_erreur_fichier_resultat' => 'Le fichier de résultat de l’analyse OCR n’existe pas ou n’est pas lisible.',
 
 	// C
 	'cfg_bouton_test' => 'Tester',
 	'cfg_exemple' => 'Exemple',
 	'cfg_exemple_explication' => 'Explication de cet exemple',
 	'cfg_titre_parametrages' => 'Paramétrages',
-	'cfg_titre_test' => 'Test de l\'analyse OCR',
+	'cfg_titre_test' => 'Test de l’analyse OCR',
 	'configuration_ocr' => 'Analyse OCR',
 
 	// E
-	'erreur_binaire_indisponible' => 'Ce logiciel n\'est pas disponible sur le serveur.',
+	'erreur_binaire_indisponible' => 'Ce logiciel n’est pas disponible sur le serveur.',
 	'erreur_intervalle_cron' => 'Vous devez indiquer un intervalle supérieur à une seconde.',
 	'erreur_nb_docs' => 'Vous devez indiquer un nombre de documents à traiter par itération supérieur à un.',
 	'erreur_ocr_bin' => 'Vous devez renseigner le binaire à utiliser pour la reconnaissance de caractères',
 	'erreur_taille_texte_max' => 'Vous devez indiquer un nombre de caractères max supérieur à un.',
 	'erreur_verifier_configuration' => 'Il y a des erreurs de configuration.',
-	'explication_option_readonly' => 'Cette option est forcée sur ce site et n\'est donc pas configurable.',
+	'explication_option_readonly' => 'Cette option est forcée sur ce site et n’est donc pas configurable.',
 
 	// G
 	'general' => 'Général',
@@ -50,10 +50,10 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// S
 	'statistiques_bouton_tout' => 'Relancer tout',
-	'statistiques_label_nb_err' => 'Erreur lors de l\'analyse, ou pas analysables',
+	'statistiques_label_nb_err' => 'Erreur lors de l’analyse, ou pas analysables',
 	'statistiques_label_nb_non' => 'Pas encore analysés',
 	'statistiques_label_nb_oui' => 'Analysés',
-	'statistiques_message_relance' => 'L\'analyse OCR a été relancée sur tous les documents',
+	'statistiques_message_relance' => 'L’analyse OCR a été relancée sur tous les documents',
 	'statistiques_titre' => 'Statistiques',
 
 	// T
@@ -61,9 +61,9 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'test_erreur_id_document' => 'Numéro de document non valide.',
 	'test_erreur_regarder_logs' => '@message@ - voir le fichier de log pour plus de détails.',
 	'test_label_id_document' => 'Document à analyser',
-	'test_label_resultat' => 'Résultat de l\'analyse',
-	'test_message_resultat' => 'Voici le résultat de l\'analyse OCR.',
-	'titre_page_configurer_ocr' => 'Plugin d\'analyse OCR'
+	'test_label_resultat' => 'Résultat de l’analyse',
+	'test_message_resultat' => 'Voici le résultat de l’analyse OCR.',
+	'titre_page_configurer_ocr' => 'Plugin d’analyse OCR'
 );
 
 ?>
