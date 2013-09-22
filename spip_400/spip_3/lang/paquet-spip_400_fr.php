bd51b15ad93b57915c769a84d184942be18a754f
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-22 17:04:55 +0000

    langues (paquet-spip_400)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76323 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spip_400/spip_3/lang/paquet-spip_400_fr.php
+++ spip_400/spip_3/lang/paquet-spip_400_fr.php
@@ -6,12 +6,12 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// S
-	'spip_400_description' => 'Ce plugin complète la distribution de SPIP en proposant des modèles de pages d\'erreur HTTP ({codes 401 et 404}) avec un texte explicatif et la possibilité pour l\'internaute de transmettre un "ticket de bug" au webmestre du site.
+	'spip_400_description' => 'Ce plugin complète la distribution de SPIP en proposant des modèles de pages d’erreur HTTP ({codes 401 et 404}) avec un texte explicatif et la possibilité pour l’internaute de transmettre un "ticket de bug" au webmestre du site.
 
-Il propose notamment : 
--* un message sur les pages publiques pour que l\'internaute ne se perde pas;
--* l\'envoi d\'un mail au webmestre avec une info complète sur l\'erreur en question ({utilisateur SPIP, URL, REFERER, backtrace PHP, etc}); 
--* l\'écriture de messages de LOG dans un fichier spécifique;',
+Il propose notamment : 
+-* un message sur les pages publiques pour que l’internaute ne se perde pas ;
+-* l’envoi d’un mail au webmestre avec une info complète sur l’erreur en question ({utilisateur SPIP, URL, REFERER, backtrace PHP, etc}) ; 
+-* l’écriture de messages de LOG dans un fichier spécifique ;',
 	'spip_400_nom' => 'SPIP 400',
 	'spip_400_slogan' => 'Gestion poussée des erreurs HTTP (401, 404) pour SPIP'
 );
