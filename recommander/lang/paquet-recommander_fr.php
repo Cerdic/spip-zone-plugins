cb21be61237706d03dbf65c8b21a852e96e61ac8
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-22 17:04:24 +0000

    langues (paquet-recommander)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76293 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- recommander/lang/paquet-recommander_fr.php
+++ recommander/lang/paquet-recommander_fr.php
@@ -8,11 +8,11 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// R
 	'recommander_description' => 'Fonction recommander une page à un ami.
 
-On l\'ajoute dans n\'importe quel squelette sous la forme :
+On l’ajoute dans n’importe quel squelette sous la forme :
 <code>#RECOMMANDER{titre de la page,url de la page,intro de la page,sujet}</code>
 _ par exemple
 <code>#RECOMMANDER{#TITRE,#URL_ARTICLE,#INTRODUCTION}</code>
-_ l\'url, l\'intro et le sujet sont facultatifs',
+_ l’url, l’intro et le sujet sont facultatifs',
 	'recommander_nom' => 'Recommander à un ami',
 	'recommander_slogan' => 'Fonction recommander une page à un ami'
 );
