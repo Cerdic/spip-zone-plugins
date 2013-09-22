c6181d31e866d963886e488377f6781c565fa28e
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-22 17:02:43 +0000

    langues (paquet-manuelsite)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76212 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- manuel_site/lang/paquet-manuelsite_fr.php
+++ manuel_site/lang/paquet-manuelsite_fr.php
@@ -6,7 +6,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// M
-	'manuelsite_description' => 'Ce plugin installe une icône d\'aide permettant d\'afficher depuis n\'importe quelle page de l\'espace privé le manuel de rédaction du site. Ce manuel est un article du site. Le plugin fournit également un jeu d\'item de Foire Aux Questions courants pouvant être insérés facilement dans votre manuel.',
+	'manuelsite_description' => 'Ce plugin installe une icône d’aide permettant d’afficher depuis n’importe quelle page de l’espace privé le manuel de rédaction du site. Ce manuel est un article du site. Le plugin fournit également un jeu d’item de Foire Aux Questions courants pouvant être insérés facilement dans votre manuel.',
 	'manuelsite_nom' => 'Manuel de rédaction du site',
 	'manuelsite_slogan' => 'Un manuel spécifique à votre site pour vos rédacteurs'
 );
