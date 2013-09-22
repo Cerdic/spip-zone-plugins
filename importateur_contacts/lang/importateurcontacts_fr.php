64ed483a7ff9d86786c387c1a1e6595a7ee0faaf
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-22 17:02:23 +0000

    langues (importateurcontacts)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76192 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- importateur_contacts/lang/importateurcontacts_fr.php
+++ importateur_contacts/lang/importateurcontacts_fr.php
@@ -8,20 +8,20 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// C
 	'configurer_fournisseur_active_avec_moteur' => 'Activé avec @moteur@',
 	'configurer_fournisseur_desactive' => 'Désactivé',
-	'configurer_titre' => 'Configurer les services d\'importation de contacts',
+	'configurer_titre' => 'Configurer les services d’importation de contacts',
 
 	// E
-	'email_liste' => 'Une liste d\'email',
+	'email_liste' => 'Une liste d’email',
 	'email_simple' => 'Un simple email',
-	'erreur_aucun_fournisseur' => 'Aucun service pouvant fournir des contacts n\'a été trouvé. Veuillez installer au moins une librairie d\'importation de contacts.',
-	'erreur_aucun_fournisseur_configure' => 'Aucun service d\'import de contacts n\'est activé.',
+	'erreur_aucun_fournisseur' => 'Aucun service pouvant fournir des contacts n’a été trouvé. Veuillez installer au moins une librairie d’importation de contacts.',
+	'erreur_aucun_fournisseur_configure' => 'Aucun service d’import de contacts n’est activé.',
 
 	// I
 	'importer_bouton_recuperer_contacts' => 'Récupérer mes contacts',
 	'importer_fournisseur_label' => 'Choisissez le service avec lequel récupérer vos contacts',
-	'info_aucun_contact' => 'Aucun contact n\'a été trouvé pour ce compte.',
+	'info_aucun_contact' => 'Aucun contact n’a été trouvé pour ce compte.',
 	'info_nb_contacts' => '@nb@ contacts ont été récupérés.',
-	'info_nb_contacts_erreur' => '@nb@ contacts ont été récupérés mais n\'ont pu être utilisés.',
+	'info_nb_contacts_erreur' => '@nb@ contacts ont été récupérés mais n’ont pu être utilisés.',
 	'info_titre' => 'Importateur de contacts'
 );
 
