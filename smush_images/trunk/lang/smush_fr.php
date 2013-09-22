18e78c7af26961d3fbc4e4af303dfdc6b0c1bb6b
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-22 17:04:44 +0000

    langues (smush)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76312 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- smush_images/trunk/lang/smush_fr.php
+++ smush_images/trunk/lang/smush_fr.php
@@ -6,18 +6,18 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// E
-	'erreur_copie_locale' => 'Le fichier n\'a pu être copié localement (@src@).',
+	'erreur_copie_locale' => 'Le fichier n’a pu être copié localement (@src@).',
 	'erreur_pas_image' => 'Le fichier ne semble pas être une image de type png, jpg ou gif (@src@).',
 
 	// I
 	'info_gain' => 'Le gain est de @gain@ octets soit @gain_octets@ (@percent@%).',
 	'info_gain_simple' => 'Le gain est de @gain@ octets (@percent@%).',
 	'info_impossible_utilisation_locale' => 'Les logiciels nécessaires à une utilisation locale ne sont pas tous présents.',
-	'info_logiciel_nok' => 'Le logiciel "@logiciel@" n\'est pas disponible',
-	'info_logiciel_nok_tous' => 'L\'ensemble des logiciels nécessaires ne sont pas utilisables. Vous ne pouvez pas utiliser smush en local.',
+	'info_logiciel_nok' => 'Le logiciel "@logiciel@" n’est pas disponible',
+	'info_logiciel_nok_tous' => 'L’ensemble des logiciels nécessaires ne sont pas utilisables. Vous ne pouvez pas utiliser smush en local.',
 	'info_logiciel_ok' => 'Le logiciel "@logiciel@" semble utilisable',
-	'info_logiciel_ok_tous' => 'L\'ensemble des logiciels nécessaires sont utilisables. Vous pouvez utiliser smush en local.',
-	'info_taille' => 'Taille : @taille@ (@octets@).',
+	'info_logiciel_ok_tous' => 'L’ensemble des logiciels nécessaires sont utilisables. Vous pouvez utiliser smush en local.',
+	'info_taille' => 'Taille : @taille@ (@octets@).',
 	'info_titre_smush' => 'Smush',
 
 	// L
@@ -25,7 +25,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'label_url_test' => 'Url de votre image à tester',
 
 	// S
-	'smush_conf_descriptif' => 'Le plugin Smush permet de réduire la taille des images d\'un site au minimum en utilisant des logiciels spécifiques sur le serveur. Cette page permet de le configurer.',
+	'smush_conf_descriptif' => 'Le plugin Smush permet de réduire la taille des images d’un site au minimum en utilisant des logiciels spécifiques sur le serveur. Cette page permet de le configurer.',
 
 	// T
 	'titre_test_logiciels' => 'Test de présence des logiciels',
