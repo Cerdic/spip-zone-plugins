9eeebef8c92ed917245ecb8903f8b9256d1c7729
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-22 17:02:54 +0000

    langues (paquet-mes_fichiers)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76221 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- mes_fichiers/trunk/lang/paquet-mes_fichiers_fr.php
+++ mes_fichiers/trunk/lang/paquet-mes_fichiers_fr.php
@@ -6,8 +6,8 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// M
-	'mes_fichiers_description' => 'Ce plugin fabrique un fichier d\'archives contenant les données de personnalisation du site comme le dernier dump de sauvegarde de la base, les dossiers des squelettes personnalisés, le dossier des images...
-Le fichier d\'archives est créé dans <code>tmp/mes_fichiers/</code> et contient dans son nom la date de création.
+	'mes_fichiers_description' => 'Ce plugin fabrique un fichier d’archives contenant les données de personnalisation du site comme le dernier dump de sauvegarde de la base, les dossiers des squelettes personnalisés, le dossier des images...
+Le fichier d’archives est créé dans <code>tmp/mes_fichiers/</code> et contient dans son nom la date de création.
 
 Il est aussi possible de lancer une sauvegarde régulière automatique de tous les fichiers de personnalisation combinée à un nettoyage des archives obsolètes et de notifier toutes les actions.',
 	'mes_fichiers_nom' => 'Mes fichiers',
