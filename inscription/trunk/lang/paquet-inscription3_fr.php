32a2102a577773c6e9519777b56402eb11276f25
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-22 17:02:26 +0000

    langues (paquet-inscription3)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76195 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- inscription/trunk/lang/paquet-inscription3_fr.php
+++ inscription/trunk/lang/paquet-inscription3_fr.php
@@ -6,7 +6,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// I
-	'inscription3_description' => 'Plugin permettant d\'avoir une gestion plus fine des comptes utilisateurs,
+	'inscription3_description' => 'Plugin permettant d’avoir une gestion plus fine des comptes utilisateurs,
 lors de leur inscription et de la modification de leur profil.',
 	'inscription3_nom' => 'Inscription 3',
 	'inscription3_slogan' => 'Étendre les informations sur les auteurs'
