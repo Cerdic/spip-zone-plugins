6484b76fa9bb0346c4cb7d5ec318d9447a07194f
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-22 17:04:35 +0000

    langues (paquet-seo)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76304 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- seo/trunk/lang/paquet-seo_fr.php
+++ seo/trunk/lang/paquet-seo_fr.php
@@ -6,7 +6,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// S
-	'seo_description' => 'SEO est un plugin qui ajoute la possibilité d\'insérer dans la partie head de votre site web : meta tags, URL canonique, Google webmaster tools Meta Code et Google Analytics javascript. Il est configurable dans la page de configuration de SPIP ainsi que dans chaque rubrique et article pour les meta tags.',
+	'seo_description' => 'SEO est un plugin qui ajoute la possibilité d’insérer dans la partie head de votre site web : meta tags, URL canonique, Google webmaster tools Meta Code et Google Analytics javascript. Il est configurable dans la page de configuration de SPIP ainsi que dans chaque rubrique et article pour les meta tags.',
 	'seo_nom' => 'SEO',
 	'seo_slogan' => 'Search Engine Optimisation'
 );
