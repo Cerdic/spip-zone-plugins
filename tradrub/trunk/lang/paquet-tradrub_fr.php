294e866f84b5b847eb4ddd04e0aeed4fbe992d3a
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-22 17:05:27 +0000

    langues (paquet-tradrub)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76352 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- tradrub/trunk/lang/paquet-tradrub_fr.php
+++ tradrub/trunk/lang/paquet-tradrub_fr.php
@@ -6,7 +6,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// T
-	'tradrub_description' => 'Ce plugin rajoute un système inspiré par celui des traductions des articles : il ajoute un champ id_trad à la table rubriques et lie ainsi les traductions des rubriques.',
+	'tradrub_description' => 'Ce plugin rajoute un système inspiré par celui des traductions des articles : il ajoute un champ id_trad à la table rubriques et lie ainsi les traductions des rubriques.',
 	'tradrub_nom' => 'Traduction entre rubriques',
 	'tradrub_slogan' => 'Gestion de liens de traduction entre les rubriques'
 );
