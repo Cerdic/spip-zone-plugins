10ec7435bd922dc4b12c47e8f9b4c4a0367dbfef
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-22 17:01:56 +0000

    langues (facd)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76172 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- facd/lang/facd_fr.php
+++ facd/lang/facd_fr.php
@@ -8,12 +8,12 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// B
 	'bouton_relancer_tout' => 'Reconvertir tous les documents',
 	'bouton_relancer_tout_format' => 'Reconvertir tous les documents au format @format@',
-	'bouton_relancer_tout_format_message' => 'Êtes-vous sûr de vouloir relancer l\'encodage de tous les fichiers au format @format@ ?',
-	'bouton_relancer_tout_message' => 'Êtes-vous sûr de vouloir relancer l\'encodage de tous les fichiers ?',
+	'bouton_relancer_tout_format_message' => 'Êtes-vous sûr de vouloir relancer l’encodage de tous les fichiers au format @format@ ?',
+	'bouton_relancer_tout_message' => 'Êtes-vous sûr de vouloir relancer l’encodage de tous les fichiers ?',
 
 	// E
-	'erreur_document_plus_disponible' => 'Ce document n\'est plus disponible sur le site',
-	'explication_file_attente' => 'Cette page liste les documents présents dans la file d\'attente. Ceux-ci seront convertis automatiquement par CRON.',
+	'erreur_document_plus_disponible' => 'Ce document n’est plus disponible sur le site',
+	'explication_file_attente' => 'Cette page liste les documents présents dans la file d’attente. Ceux-ci seront convertis automatiquement par CRON.',
 
 	// I
 	'info_document_conversion' => 'Ce document est en cours de conversion',
@@ -24,15 +24,15 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'info_statut_conversion_erreur' => 'En erreur',
 	'info_statut_conversion_non' => 'En attente',
 	'info_statut_conversion_oui' => 'Converti',
-	'info_tous_docs_facd' => 'Documents de la file d\'attente',
-	'info_voir_log_erreur' => 'Voir le log d\'erreur',
+	'info_tous_docs_facd' => 'Documents de la file d’attente',
+	'info_voir_log_erreur' => 'Voir le log d’erreur',
 
 	// L
 	'label_relancer_conversion' => 'Relancer la conversion',
 	'lien_convertir_document' => 'Convertir ce document',
 	'lien_recharger' => 'Recharger',
-	'liste_attente_aucun' => 'Aucun document dans la file d\'attente.',
-	'liste_attente_tous' => 'Documents présents dans la file d\'attente',
+	'liste_attente_aucun' => 'Aucun document dans la file d’attente.',
+	'liste_attente_tous' => 'Documents présents dans la file d’attente',
 	'liste_convert_aucun' => 'Aucun document converti',
 	'liste_convert_tous' => 'Documents convertis',
 
@@ -48,9 +48,9 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'thead_nombre' => 'Nombre',
 	'thead_statut' => 'Statut',
 	'titre_log_conversion' => 'Contenu du log de conversion #@id@',
-	'titre_page_file' => 'File d\'attente du module de conversion de medias',
+	'titre_page_file' => 'File d’attente du module de conversion de medias',
 	'titre_page_file_convertis_jour' => 'Conversions par date',
-	'titre_page_file_menu' => 'File d\'attente de conversion',
+	'titre_page_file_menu' => 'File d’attente de conversion',
 
 	// V
 	'version_encodee_de' => 'Ce document est une conversion du document @id_orig@'
