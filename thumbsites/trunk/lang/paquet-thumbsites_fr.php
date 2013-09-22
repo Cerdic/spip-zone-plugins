eaa12ccf0a520c703e6610ffaad7b8c207478666
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-22 17:05:14 +0000

    langues (paquet-thumbsites)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76341 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- thumbsites/trunk/lang/paquet-thumbsites_fr.php
+++ thumbsites/trunk/lang/paquet-thumbsites_fr.php
@@ -6,7 +6,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// T
-	'thumbsites_description' => 'Thumbsites fournit un système de capture d\'écran de site générée par des serveurs dédiés. Le plugin propose des balises, des filtres et des modèles pour afficher la vignette d\'un site donné. Ce plugin gère aussi un cache qui permet de pallier les indisponibilités fréquentes des serveurs et d\'accélérer les affichages. Une configuration est disponible dans l\'espace privé.',
+	'thumbsites_description' => 'Thumbsites fournit un système de capture d’écran de site générée par des serveurs dédiés. Le plugin propose des balises, des filtres et des modèles pour afficher la vignette d’un site donné. Ce plugin gère aussi un cache qui permet de pallier les indisponibilités fréquentes des serveurs et d’accélérer les affichages. Une configuration est disponible dans l’espace privé.',
 	'thumbsites_slogan' => 'Une vignette pour vos sites référencés'
 );
 
