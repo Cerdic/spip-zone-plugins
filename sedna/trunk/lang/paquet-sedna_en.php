7d85b0c5516c798b46ea4be7e1fb3d6fcf37157d
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-22 17:06:07 +0000

    langues (paquet-sedna)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76390 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- sedna/trunk/lang/paquet-sedna_en.php
+++ sedna/trunk/lang/paquet-sedna_en.php
@@ -8,7 +8,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// S
-	'sedna_description' => 'Sedna is an aggregator (RSS and ATOM etc.) based on SPIP\'s syndicated sites. It is based loosely on "Safari RSS" for presentation.
+	'sedna_description' => 'Sedna is an aggregator (RSS and ATOM etc.) based on SPIP’s syndicated sites. It is based loosely on "Safari RSS" for presentation.
 
 Its purpose is to be and remain simple in its code, and quick to load. Aesthetics a bit, too.',
 	'sedna_slogan' => 'RSS aggregator'
--- sedna/trunk/lang/paquet-sedna_fr.php
+++ sedna/trunk/lang/paquet-sedna_fr.php
@@ -6,7 +6,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// S
-	'sedna_description' => 'Sedna est un agrégateur de flux RSS (et ATOM etc) basé sur les sites syndiqués de SPIP. Il s’inspire vaguement de « Safari RSS » pour la présentation.
+	'sedna_description' => 'Sedna est un agrégateur de flux RSS (et ATOM etc) basé sur les sites syndiqués de SPIP. Il s’inspire vaguement de « Safari RSS » pour la présentation.
 
 Son objectif est d’être et de rester simple dans son code, et rapide à charger. Esthétique un peu, aussi.',
 	'sedna_slogan' => 'Agrégateur RSS'
