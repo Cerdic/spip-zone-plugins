51cd128569e8b508fc591d341fa4a629471fd3b8
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-22 17:02:58 +0000

    langues (formulaires)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76225 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- messagerie/branches/messagerie_2_0/lang/formulaires_fr.php
+++ messagerie/branches/messagerie_2_0/lang/formulaires_fr.php
@@ -6,16 +6,16 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// E
-	'email_invalide' => 'Cette adresse mail n\'est pas valide',
-	'erreur_enregistrement' => 'Une erreur est survenue lors de l\'enregistrement',
+	'email_invalide' => 'Cette adresse mail n’est pas valide',
+	'erreur_enregistrement' => 'Une erreur est survenue lors de l’enregistrement',
 
 	// I
 	'info_obligatoire' => '*',
 	'info_obligatoire_legende' => 'champs obligatoires',
-	'info_obligatoire_rappel' => 'Cette information est obligatoire !',
+	'info_obligatoire_rappel' => 'Cette information est obligatoire !',
 
 	// V
-	'valeur_incorrecte' => 'Cette valeur n\'est pas correcte'
+	'valeur_incorrecte' => 'Cette valeur n’est pas correcte'
 );
 
 ?>
