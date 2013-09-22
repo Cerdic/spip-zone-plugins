a3c3b8a18cf93f51b96a60125e2be970a4700198
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-22 17:02:41 +0000

    langues (paquet-mediaspip_player)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76210 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- mediaspip_player/trunk/lang/paquet-mediaspip_player_fr.php
+++ mediaspip_player/trunk/lang/paquet-mediaspip_player_fr.php
@@ -6,7 +6,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// M
-	'mediaspip_player_description' => 'Lecteur multimédia HTML5 s\'appuyant sur les balises audio et video et disposant d\'une solution de repli en flash (via [Flowplayer->http://flowplayer.org]) pour les navigateurs anciens',
+	'mediaspip_player_description' => 'Lecteur multimédia HTML5 s’appuyant sur les balises audio et video et disposant d’une solution de repli en flash (via [Flowplayer->http://flowplayer.org]) pour les navigateurs anciens',
 	'mediaspip_player_nom' => 'Lecteur de médias de MediaSPIP',
 	'mediaspip_player_slogan' => 'Lecteur multimédia HTML5 pour MediaSPIP'
 );
