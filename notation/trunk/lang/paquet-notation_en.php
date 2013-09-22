1f668ca2c43c742fec82408a0aafedbba08fb080
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-22 17:03:20 +0000

    langues (paquet-notation)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76245 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- notation/trunk/lang/paquet-notation_en.php
+++ notation/trunk/lang/paquet-notation_en.php
@@ -9,7 +9,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// N
 	'notation_description' => 'Defines the <code>#FORMULAIRE_NOTATION</code> tag which allows the capture and the display of the rating of a loop element.
-_ Defines the models <code><autxx|notation|id_article=yy></code> which gives the author\'s rating <i>xx</i> for the article <i>yy</i>
+_ Defines the models <code><autxx|notation|id_article=yy></code> which gives the author’s rating <i>xx</i> for the article <i>yy</i>
 and <code><notation|topten></code> which gives the 10 best articles.',
 	'notation_nom' => 'Rating',
 	'notation_slogan' => 'Assign a rating to SPIP elements'
--- notation/trunk/lang/paquet-notation_fr.php
+++ notation/trunk/lang/paquet-notation_fr.php
@@ -6,8 +6,8 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// N
-	'notation_description' => 'Définition de la balise <code>#FORMULAIRE_NOTATION</code> permettant la saisie et l\'affichage de la note d\'un élément de boucle.
-_ Définition des modèles <code><autxx|notation|id_article=yy></code> qui donne la note de l\'auteur <i>xx</i> pour l\'article <i>yy</i>
+	'notation_description' => 'Définition de la balise <code>#FORMULAIRE_NOTATION</code> permettant la saisie et l’affichage de la note d’un élément de boucle.
+_ Définition des modèles <code><autxx|notation|id_article=yy></code> qui donne la note de l’auteur <i>xx</i> pour l’article <i>yy</i>
 et <code><notation|topten></code> qui donne les 10 meilleurs articles.',
 	'notation_nom' => 'Notation',
 	'notation_slogan' => 'Attribuer une note aux éléments de SPIP'
