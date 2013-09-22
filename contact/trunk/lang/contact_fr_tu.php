cc5fee0bbb34ba1d49cde19c2f7f43888f48d33a
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-22 17:00:57 +0000

    langues (contact)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76121 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- contact/trunk/lang/contact_fr_tu.php
+++ contact/trunk/lang/contact_fr_tu.php
@@ -8,7 +8,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
-	'alerte_effacement' => 'Confirme l\'effacement des données de configuration de contact.',
+	'alerte_effacement' => 'Confirme l’effacement des données de configuration de contact.',
 
 	// B
 	'bouton_effacer' => 'Gommer',
@@ -16,7 +16,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// C
 	'champ_adresse' => 'Adresse',
 	'champ_civi_explication' => 'Réglages avancés du champ civilité',
-	'champ_civi_mademoiselle_ok' => 'Proposer un choix « Mademoiselle » dans le champ civilité',
+	'champ_civi_mademoiselle_ok' => 'Proposer un choix « Mademoiselle » dans le champ civilité',
 	'champ_civilite' => 'Civilité',
 	'champ_code_postal' => 'Code postal',
 	'champ_etat' => 'État',
@@ -24,7 +24,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'champ_infolettre' => 'Infolettre',
 	'champ_mail' => 'Mail',
 	'champ_nom' => 'Nom',
-	'champ_obligatoire' => 'Obligatoire ?',
+	'champ_obligatoire' => 'Obligatoire ?',
 	'champ_organisation' => 'Organisation',
 	'champ_pays' => 'Pays',
 	'champ_portable' => 'Portable',
@@ -34,46 +34,46 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'champ_texte' => 'Texte',
 	'champ_ville' => 'Ville',
 	'configuration_barre_forum' => 'Afficher la barre typo des forums.',
-	'configuration_barre_impossible' => 'Un autre plugin interdit l\'usage du porte-plume dans l\'espace public, les réglages ci-dessous seront sans effets.',
+	'configuration_barre_impossible' => 'Un autre plugin interdit l’usage du porte-plume dans l’espace public, les réglages ci-dessous seront sans effets.',
 	'configuration_barre_oui' => 'Afficher la barre typo complète.',
 	'configuration_champs_message' => 'Réglage du porte-plume.',
 	'configuration_champs_message_explication' => 'Réglage de la barre typographique associée au texte du message.',
 	'configuration_destinataires' => 'Destinataires',
-	'configuration_destinataires_explication' => 'Choisis un ou plusieurs auteurs que les visiteurs pourront contacter. Si rien n\'est défini, c\'est l\'auteur numéro 1 (celui qui a installé le site) qui sera utilisé.',
+	'configuration_destinataires_explication' => 'Choisis un ou plusieurs auteurs que les visiteurs pourront contacter. Si rien n’est défini, c’est l’auteur numéro 1 (celui qui a installé le site) qui sera utilisé.',
 	'configuration_destinataires_plusieurs' => 'Permettre aux visiteurs de choisir <b>plusieurs</b> destinataires parmi la liste sélectionnée ci-dessus',
 	'configuration_destinataires_plusieurs_et' => '... majorée par les auteurs éventuellement transmis au formulaire.',
-	'configuration_destinataires_plusieurs_ou' => '... ou parmi le groupe d\'auteurs éventuellement transmis au formulaire',
+	'configuration_destinataires_plusieurs_ou' => '... ou parmi le groupe d’auteurs éventuellement transmis au formulaire',
 	'configuration_destinataires_tous' => 'Mettre <b>tous</b> ces auteurs en destinataires du courriel',
-	'configuration_destinataires_tous_et' => '... leur ajouter le groupe d\'auteurs éventuellement transmis au formulaire',
+	'configuration_destinataires_tous_et' => '... leur ajouter le groupe d’auteurs éventuellement transmis au formulaire',
 	'configuration_destinataires_tous_ou' => '... ou seulement ceux éventuellement transmis au formulaire',
 	'configuration_destinataires_un' => 'Forcer les visiteurs à choisir <b>un seul</b> destinataire parmi la liste sélectionnée ci-dessus',
 	'configuration_destinataires_un_et' => '... majorée par les auteurs éventuellement transmis au formulaire.',
-	'configuration_destinataires_un_ou' => '... ou parmi le groupe d\'auteurs éventuellement transmis au formulaire',
+	'configuration_destinataires_un_ou' => '... ou parmi le groupe d’auteurs éventuellement transmis au formulaire',
 	'configuration_intro' => 'Introduction',
 	'configuration_intro_explication' => 'Écris éventuellement un texte qui introduira le formulaire.',
 	'configuration_no_barre' => 'Ne pas proposer de barre typo.',
 	'configuration_no_previsualisation' => 'Afficher la barre typo sans le bouton de prévisualisation.',
 	'configuration_pj' => 'Pièces jointes',
-	'configuration_pj_autoriser' => 'Autoriser l\'envoi de pièces jointes',
+	'configuration_pj_autoriser' => 'Autoriser l’envoi de pièces jointes',
 	'configuration_pj_facteur_absent' => 'Pour utiliser cette fonction, tu dois installer le plugin Facteur.',
 	'configuration_pj_nb_max' => 'Nombre maximal de pièces jointes',
-	'configuration_regle_liste_explication' => 'Choisis une règle de calcul de la liste des destinataires potentiels et de la méthode de sélection proposée à l\'internaute.',
+	'configuration_regle_liste_explication' => 'Choisis une règle de calcul de la liste des destinataires potentiels et de la méthode de sélection proposée à l’internaute.',
 	'configuration_texteinfolettre' => 'Texte infolettre',
 	'configuration_texteinfolettre_explication' => 'Change éventuellement le texte associé à la case à cocher infolettre.',
-	'consulter_memoire' => 'Ce message est enregistré sur ton site :',
+	'consulter_memoire' => 'Ce message est enregistré sur ton site :',
 	'courriel_de' => 'De',
 
 	// D
 	'deplacement_possible' => 'Déplacement vertical par glissé-déposé',
 	'description_menu_contact' => 'Élément de menu vers la page du formulaire de contact',
-	'description_page-contact' => 'Acessible via <i>spip.php?page=contact</i>, cette page optionnelle permet de fournir un formulaire de contact (pense à ajouter un lien dans tes menus).',
+	'description_page-contact' => 'Acessible via <i>spip.php ?page=contact</i>, cette page optionnelle permet de fournir un formulaire de contact (pense à ajouter un lien dans tes menus).',
 
 	// F
 	'form_destinataire' => 'Choisis un destinataire',
 	'form_destinataires' => 'Choisis les destinataires',
 	'form_pj_ajouter_pluriel' => 'Ajouter des pièces jointes',
 	'form_pj_ajouter_singulier' => 'Ajouter une pièce jointe',
-	'form_pj_fichier_ajoute' => 'Le fichier a bien été ajouté : ',
+	'form_pj_fichier_ajoute' => 'Le fichier a bien été ajouté : ',
 	'form_pj_importer' => 'Importer un fichier',
 	'form_pj_previsu_pluriel' => 'Pièces jointes',
 	'form_pj_previsu_singulier' => 'Pièce jointe',
@@ -82,11 +82,11 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'forum_attention_nbre_caracteres' => '<b>Attention !</b> ton message doit contenir au moins @nbre_caract@ caractères.',
 
 	// H
-	'horodatage' => 'Formulaire posté le : @horodatage@.',
+	'horodatage' => 'Formulaire posté le : @horodatage@.',
 
 	// I
-	'inforigine' => 'Information transmise par le formulaire : ',
-	'informez_moi' => 'J\'accepte de recevoir, par courrier électronique, des informations émanant de votre organisation.',
+	'inforigine' => 'Information transmise par le formulaire : ',
+	'informez_moi' => 'J’accepte de recevoir, par courrier électronique, des informations émanant de votre organisation.',
 	'infos_collecte' => 'Informations collectées',
 	'infos_supplementaires' => 'Informations supplémentaires',
 
@@ -109,9 +109,9 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// S
 	'sauv_message' => 'Sauvegarder les messages de contact',
-	'sauv_message_explication' => 'Permet d\'enregistrer les messages de contacts et de les afficher dans une messagerie dédiée.',
+	'sauv_message_explication' => 'Permet d’enregistrer les messages de contacts et de les afficher dans une messagerie dédiée.',
 	'sauv_message_ok' => 'Sauvegarder les messages de contacts',
-	'succes' => 'Merci, ton message a bien été envoyé.<br />Nous te répondrons dès que possible.<br />L\'équipe de @equipe_site@',
+	'succes' => 'Merci, ton message a bien été envoyé.<br />Nous te répondrons dès que possible.<br />L’équipe de @equipe_site@',
 
 	// T
 	'titre' => 'Formulaire de contact',
