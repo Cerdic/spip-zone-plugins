f495bbb4297282d8e4dd59d135ab8d891ab17bd1
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-22 17:00:07 +0000

    langues (abomailmans)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76078 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- abomailmans/trunk/lang/abomailmans_fr.php
+++ abomailmans/trunk/lang/abomailmans_fr.php
@@ -8,14 +8,14 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// A
 	'abomailman' => 'Liste de diffusion ou discussions',
 	'abomailmans' => 'Listes de diffusion ou discussions',
-	'abonne' => 'Je m\'abonne',
+	'abonne' => 'Je m’abonne',
 	'activation' => 'Activation',
 	'active' => 'Active',
 	'aucune_langue' => 'Aucune spécifique',
 
 	// B
 	'bouton_listes_diffusion' => 'Les listes de diffusion',
-	'btn_abonnement' => 'S\'abonner',
+	'btn_abonnement' => 'S’abonner',
 	'btn_desabonnement' => 'Se désabonner',
 
 	// C
@@ -30,10 +30,10 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// E
 	'email' => 'E-mail',
 	'email_abonnement' => 'Votre adresse e-mail',
-	'email_envoye' => 'L\'e-mail a été envoyé à la liste de diffusion : @liste@.',
+	'email_envoye' => 'L’e-mail a été envoyé à la liste de diffusion : @liste@.',
 	'email_oublie' => 'Vous avez oublié votre adresse e-mail',
-	'emailliste_abomailman' => 'L\'adresse e-mail de la liste',
-	'emailliste_abosympa' => 'L\'adresse e-mail de l\'administrateur Sympa',
+	'emailliste_abomailman' => 'L’adresse e-mail de la liste',
+	'emailliste_abosympa' => 'L’adresse e-mail de l’administrateur Sympa',
 	'emailliste_subscribe' => 'Abonnement',
 	'emailliste_unsubscribe' => 'Désabonnement',
 	'emails_a_renseigner' => 'Emails à renseigner',
@@ -42,15 +42,15 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'envoi_liste_parametres' => 'Liste des paramètres [Facultatif]',
 	'envoi_parametres' => 'Paramètres',
 	'envoi_regulier' => 'Envois automatiques',
-	'envoi_regulier_info' => 'Laisser vide pour ne pas avoir d\'envois automatiques',
+	'envoi_regulier_info' => 'Laisser vide pour ne pas avoir d’envois automatiques',
 	'envoi_regulier_tous_les' => 'Envoi tous les',
 	'envoi_vers' => 'envoi vers',
 	'envoyer_courier' => 'Envoyer un courrier',
-	'envoyer_courier_liste' => 'Envoyer ce courrier à cette liste de diffusion :',
+	'envoyer_courier_liste' => 'Envoyer ce courrier à cette liste de diffusion :',
 	'envoyer_mailmans' => 'Sélectionner le modèle et son contenu',
-	'erreur_email_liste_oublie' => 'L\'adresse email de la liste est obligatoire',
-	'erreur_nobot' => 'Votre inscription n\' a pu être effectué à cause d\'un problème technique',
-	'explication_email_subscribe' => 'Email d\'abonnement, de style <code>suffixe+subscribe@exemple.org</code>',
+	'erreur_email_liste_oublie' => 'L’adresse email de la liste est obligatoire',
+	'erreur_nobot' => 'Votre inscription n’ a pu être effectué à cause d’un problème technique',
+	'explication_email_subscribe' => 'Email d’abonnement, de style <code>suffixe+subscribe@exemple.org</code>',
 	'explication_email_sympa' => 'Si ce champ est renseigné, la liste est considérée comme une liste de serveur "Sympa", dans le cas contraire comme une liste "Mailman" ou "ezmlm".',
 	'explication_email_unsubscribe' => 'Email de désabonnement',
 
@@ -67,7 +67,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'inscription_lettres_legende' => 'Abonnement aux listes de diffusion et de discussions',
 
 	// J
-	'je_m_abonne' => 'Cochez pour valider l\'abonnement ou le désabonnement.',
+	'je_m_abonne' => 'Cochez pour valider l’abonnement ou le désabonnement.',
 
 	// L
 	'label_etat_liste' => 'État de la liste',
@@ -80,20 +80,20 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'legende_inscriptions_ml' => 'Inscription aux listes de discussion',
 	'legende_inscriptions_news' => 'Inscription aux listes de diffusion',
 	'les_listes_mailmans' => 'Les listes mailmans, sympa ou ezmlm renseignées',
-	'lire_article' => 'Lire l\'article',
+	'lire_article' => 'Lire l’article',
 	'liste_creee' => 'La liste numéro @id@ (@titre@) a été créée.',
-	'liste_non_existante' => 'La liste demandée n\'existe pas ou a été supprimée',
-	'liste_oublie' => 'Vous avez oublié de cocher une liste !',
+	'liste_non_existante' => 'La liste demandée n’existe pas ou a été supprimée',
+	'liste_oublie' => 'Vous avez oublié de cocher une liste !',
 	'liste_supprimee' => 'La liste numéro @id@ (@titre@) a été supprimée.',
 	'liste_updatee' => 'La liste numéro @id@ (@titre@) a été mise à jour.',
 
 	// M
 	'message' => 'Introduction à votre courrier, avant le contenu issu du site',
 	'message_confirm_suite' => 'Pour valider votre demande, répondez à la demande de confirmation que vous allez recevoir par mail.',
-	'message_confirmation_a' => 'Une demande d\'abonnement aux listes suivantes vient d\'être envoyée :',
-	'message_confirmation_d' => 'Une demande de désabonnement aux listes suivantes vient d\'être envoyée. ',
-	'message_confirmation_unique_a' => 'Une demande d\'abonnement à la liste suivante vient d\'être envoyée :',
-	'message_confirmation_unique_d' => 'Une demande de désabonnement à la liste suivante vient d\'être envoyée. ',
+	'message_confirmation_a' => 'Une demande d’abonnement aux listes suivantes vient d’être envoyée :',
+	'message_confirmation_d' => 'Une demande de désabonnement aux listes suivantes vient d’être envoyée. ',
+	'message_confirmation_unique_a' => 'Une demande d’abonnement à la liste suivante vient d’être envoyée :',
+	'message_confirmation_unique_d' => 'Une demande de désabonnement à la liste suivante vient d’être envoyée. ',
 	'mot' => 'Et lister les articles du mot clé',
 
 	// N
@@ -101,7 +101,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'nouveau_abomailman' => 'Nouvelle liste de diffusion',
 
 	// P
-	'pas_template_txt' => 'Il n\'y a pas de version texte pour ce modèle',
+	'pas_template_txt' => 'Il n’y a pas de version texte pour ce modèle',
 	'periodicite' => ' jours.',
 	'prenom' => 'Prénom',
 	'previsu_html' => 'html',
@@ -114,9 +114,9 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'souhaite_rester' => 'Je souhaite rester informé-e',
 	'sujet' => 'Sujet du courrier',
 	'sujet_obligatoire' => 'Le sujet est obligatoire.',
-	'suppression_definitive' => 'Suppression définitive !',
+	'suppression_definitive' => 'Suppression définitive !',
 	'supprimer' => 'Supprimer',
-	'sympa_message_confirmation' => 'Un email de validation a été envoyé à l\'adresse : ',
+	'sympa_message_confirmation' => 'Un email de validation a été envoyé à l’adresse : ',
 
 	// T
 	'template' => 'Choisissez le modèle et son contenu',
@@ -129,7 +129,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// V
 	'verifier_formulaire' => 'Vérifiez le remplissage du formulaire.',
-	'veut_s_abonner' => 'veut s\'abonner',
+	'veut_s_abonner' => 'veut s’abonner',
 	'veut_se_desabonner' => 'veut se désabonner',
 	'voir_modele_depuis' => 'Voir un exemple du modèle avec',
 	'votre_email' => 'Votre email'
