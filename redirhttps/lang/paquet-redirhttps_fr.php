da7cadb43b87bba17c8d1fea87daaf5d50d003aa
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-22 17:04:25 +0000

    langues (paquet-redirhttps)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76294 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- redirhttps/lang/paquet-redirhttps_fr.php
+++ redirhttps/lang/paquet-redirhttps_fr.php
@@ -8,7 +8,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// R
 	'redirhttps_description' => 'Redirection vers HTTPS quand le contenu le nécessite (espace privé, login, pages publiques avec utilisateur connecté)',
 	'redirhttps_nom' => 'Redirections HTTPS',
-	'redirhttps_slogan' => 'Sécuriser l\'accès à certaines pages du site'
+	'redirhttps_slogan' => 'Sécuriser l’accès à certaines pages du site'
 );
 
 ?>
