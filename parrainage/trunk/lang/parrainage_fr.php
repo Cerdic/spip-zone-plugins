376bcb532587ba30c4d8f615f281ef165fcb941f
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-22 17:03:43 +0000

    langues (parrainage)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76266 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- parrainage/trunk/lang/parrainage_fr.php
+++ parrainage/trunk/lang/parrainage_fr.php
@@ -14,10 +14,10 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'configuration_titre' => 'Configuration du parrainage',
 
 	// E
-	'erreur_aucun_contact' => 'Vous n\'avez importé aucun contact.',
-	'erreur_aucun_filleul' => 'Vous n\'avez sélectionné aucun filleul.',
-	'erreur_inscription_desactivee' => 'L\'inscription au site est désactivée. Vous ne pouvez donc parrainer personne.',
-	'erreur_invitation_invalide' => 'Votre invitation ne correspond pas à l\'adresse de courriel indiquée.',
+	'erreur_aucun_contact' => 'Vous n’avez importé aucun contact.',
+	'erreur_aucun_filleul' => 'Vous n’avez sélectionné aucun filleul.',
+	'erreur_inscription_desactivee' => 'L’inscription au site est désactivée. Vous ne pouvez donc parrainer personne.',
+	'erreur_invitation_invalide' => 'Votre invitation ne correspond pas à l’adresse de courriel indiquée.',
 	'erreur_invitation_obligatoire' => 'Vous devez avoir une invitation pour vous inscrire sur le site.',
 
 	// F
@@ -28,49 +28,49 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'filleul_statut' => 'Statut',
 
 	// I
-	'inscription_code_invitation_label' => 'Code d\'invitation',
+	'inscription_code_invitation_label' => 'Code d’invitation',
 	'invitation_message' => '@nom_parrain@ vous invite à rejoindre le site @site@.',
 	'invitation_sujet' => '@nom@ vous invite à rejoindre le site @site@',
-	'invitation_url' => 'Rendez-vous à l\'adresse suivante pour vous inscrire :',
+	'invitation_url' => 'Rendez-vous à l’adresse suivante pour vous inscrire :',
 
 	// M
-	'message_insciption_fermee' => 'Attention, l\'inscription au site est fermée. Vous devez l\'activer pour utiliser ce plugin',
-	'message_ouvrir_visiteur' => 'Si vous souhaitez ouvrir l\'inscription au site à de nouveaux visiteurs, vous pouvez activer cette option ci-dessous (la configuration du plugin "Parrainage" oblige le nouvel inscrit à bénéficier d\'un code d\'invitation) : ',
+	'message_insciption_fermee' => 'Attention, l’inscription au site est fermée. Vous devez l’activer pour utiliser ce plugin',
+	'message_ouvrir_visiteur' => 'Si vous souhaitez ouvrir l’inscription au site à de nouveaux visiteurs, vous pouvez activer cette option ci-dessous (la configuration du plugin "Parrainage" oblige le nouvel inscrit à bénéficier d’un code d’invitation) : ',
 
 	// P
-	'parrainage_inviter' => 'Envoyer l\'invitation',
-	'parrainage_message_aucun' => 'Aucune de ces personnes n\'a besoin d\'être invitée.',
+	'parrainage_inviter' => 'Envoyer l’invitation',
+	'parrainage_message_aucun' => 'Aucune de ces personnes n’a besoin d’être invitée.',
 	'parrainage_message_aucun_1' => 'Cette personne est déjà invitée ou inscrite.',
-	'parrainage_message_erreur' => 'Au moins une invitation n\'a pas été envoyée correctement.',
+	'parrainage_message_erreur' => 'Au moins une invitation n’a pas été envoyée correctement.',
 	'parrainage_message_label' => 'Votre message',
-	'parrainage_message_ok_pluriel' => '@nombre@ invitations ont bien été programmées, elles sont maintenant en cours d\'envoi.',
-	'parrainage_message_ok_singulier' => 'Une invitation a bien été programmée, elle est maintenant en cours d\'envoi.',
+	'parrainage_message_ok_pluriel' => '@nombre@ invitations ont bien été programmées, elles sont maintenant en cours d’envoi.',
+	'parrainage_message_ok_singulier' => 'Une invitation a bien été programmée, elle est maintenant en cours d’envoi.',
 	'parrainage_supprime_nb' => '@nb@ contacts supprimés',
 	'parrainage_supprime_un' => '@nb@ contact supprimé',
 	'parrainage_supprimer_filleul' => 'Supprimer ce contact',
-	'parrainage_supprimer_filleul_confirmation' => 'Êtes-vous sûr de vouloir supprimer @nom@ de vos contacts ?',
+	'parrainage_supprimer_filleul_confirmation' => 'Êtes-vous sûr de vouloir supprimer @nom@ de vos contacts ?',
 	'parrainage_supprimer_filleuls' => 'Supprimer les contacts sélectionnés',
-	'parrainage_supprimer_filleuls_confirmation' => 'Êtes-vous sûr de vouloir supprimer les contacts sélectionnés de vos contacts ?',
+	'parrainage_supprimer_filleuls_confirmation' => 'Êtes-vous sûr de vouloir supprimer les contacts sélectionnés de vos contacts ?',
 	'plugin_nom' => 'Parrainage',
 
 	// S
 	'selectionner_rien' => 'Tout désélectionner',
 	'selectionner_tout' => 'Tout sélectionner',
 	'statut_contact' => 'Contact',
-	'statut_contact_explication' => 'Cette personne est dans votre carnet d\'adresse. Vous pouvez l\'inviter à rejoindre le site.',
+	'statut_contact_explication' => 'Cette personne est dans votre carnet d’adresse. Vous pouvez l’inviter à rejoindre le site.',
 	'statut_deja_inscrit' => 'Déjà inscrit',
-	'statut_deja_inscrit_explication' => 'Cette personne est déjà inscrite sur le site, mais n\'est pas votre filleul !',
-	'statut_en_cours' => 'Invitation en cours d\'envoi',
-	'statut_en_cours_explication' => 'Votre message d\'invitation est en cours d\'expédition.',
+	'statut_deja_inscrit_explication' => 'Cette personne est déjà inscrite sur le site, mais n’est pas votre filleul !',
+	'statut_en_cours' => 'Invitation en cours d’envoi',
+	'statut_en_cours_explication' => 'Votre message d’invitation est en cours d’expédition.',
 	'statut_filleul' => 'Filleul',
 	'statut_filleul_depuis' => 'Filleul inscrit depuis @date@',
-	'statut_filleul_explication' => 'Grâce à vous, cette personne s\'est inscrite sur le site : vous êtes son parrain.',
+	'statut_filleul_explication' => 'Grâce à vous, cette personne s’est inscrite sur le site : vous êtes son parrain.',
 	'statut_invite' => 'Invité @duree@',
 	'statut_invite_explication' => 'Vous avez déjà invité cette personne il y a peu de temps.',
 	'statut_sans_nouvelles' => 'Pas de nouvelles depuis @date@',
-	'statut_sans_nouvelles_explication' => 'Vous avez invité cette personne il y a un certain temps, mais elle n\'est jamais venue.',
+	'statut_sans_nouvelles_explication' => 'Vous avez invité cette personne il y a un certain temps, mais elle n’est jamais venue.',
 	'statut_visite' => 'A visité le site',
-	'statut_visite_explication' => 'Cette personne a déjà suivi votre invitation à venir sur le site, mais ne s\'est pas encore inscrite.'
+	'statut_visite_explication' => 'Cette personne a déjà suivi votre invitation à venir sur le site, mais ne s’est pas encore inscrite.'
 );
 
 ?>
