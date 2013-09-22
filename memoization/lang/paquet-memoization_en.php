2e5b8cacd4049d1545dbaf542e5b357a1afb5144
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-22 17:02:46 +0000

    langues (paquet-memoization)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76215 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- memoization/lang/paquet-memoization_en.php
+++ memoization/lang/paquet-memoization_en.php
@@ -9,7 +9,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// M
 	'memoization_description' => 'Memoization is a computer science term, which Wikipedia defines as "a technique of reducing the execution time of a function by storing the results once on the other." It is also the name of a new function library for PHP, which follows my experiments XCache.',
-	'memoization_slogan' => 'Various methods to process SPIP\'s cache.'
+	'memoization_slogan' => 'Various methods to process SPIP’s cache.'
 );
 
 ?>
--- memoization/lang/paquet-memoization_fr.php
+++ memoization/lang/paquet-memoization_fr.php
@@ -6,7 +6,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// M
-	'memoization_description' => 'Mémoïzation est un terme d’informatique, dont Wikipédia donne la définition suivante : « une technique consistant à réduire le temps d’exécution d’une fonction en mémorisant ses résultats d’une fois sur l’autre ». C’est aussi le nom d’une nouvelle librairie de fonctions pour PHP, qui fait suite à mes expérimentations sur XCache.',
+	'memoization_description' => 'Mémoïzation est un terme d’informatique, dont Wikipédia donne la définition suivante : « une technique consistant à réduire le temps d’exécution d’une fonction en mémorisant ses résultats d’une fois sur l’autre ». C’est aussi le nom d’une nouvelle librairie de fonctions pour PHP, qui fait suite à mes expérimentations sur XCache.',
 	'memoization_slogan' => 'Gestion du cache de SPIP par diverses méthodes.'
 );
 
