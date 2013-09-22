bc244a55be617fe6e0eb40ff5b01a9dbe2e29b53
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-22 17:03:32 +0000

    langues (paquet-openid)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76255 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- openid/lang/paquet-openid_fr.php
+++ openid/lang/paquet-openid_fr.php
@@ -6,8 +6,8 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// O
-	'openid_description' => 'Un utilisateur qui souhaite utiliser son OpenID pour accéder au site doit l\'entrer comme adresse de son site web dans son profil. Ensuite, au lieu de mettre son login ou adresse email, il suffit d\'entrer son OpenID pour se logguer.',
-	'openid_slogan' => 'Authentification des auteurs et visiteurs à l\'aide de leur OpenID'
+	'openid_description' => 'Un utilisateur qui souhaite utiliser son OpenID pour accéder au site doit l’entrer comme adresse de son site web dans son profil. Ensuite, au lieu de mettre son login ou adresse email, il suffit d’entrer son OpenID pour se logguer.',
+	'openid_slogan' => 'Authentification des auteurs et visiteurs à l’aide de leur OpenID'
 );
 
 ?>
