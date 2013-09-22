8021fa0ff043bd5ac675c72b6fd343e846f4bc4b
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-22 17:02:19 +0000

    langues (paquet-gravatar)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76188 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- gravatar/lang/paquet-gravatar_fr.php
+++ gravatar/lang/paquet-gravatar_fr.php
@@ -6,17 +6,17 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// G
-	'gravatar_description' => 'Permet d\'utiliser un cache pour stocker les gravatars.
-_ À utiliser dans une boucle de cette manière : <code>#GRAVATAR{email, taille, url image par
+	'gravatar_description' => 'Permet d’utiliser un cache pour stocker les gravatars.
+_ À utiliser dans une boucle de cette manière : <code>#GRAVATAR{email, taille, url image par
 défaut}</code>
-_ Exemple : <code>#GRAVATAR{#EMAIL,80,#URL_SITE_SPIP/defaut-gravatar.gif}</code>
+_ Exemple : <code>#GRAVATAR{#EMAIL,80,#URL_SITE_SPIP/defaut-gravatar.gif}</code>
 
-Étend la balise #LOGO_AUTEUR de manière à prendre en compte le gravatar d\'un auteur s\'il existe, y compris dans les forums et pétitions.
+Étend la balise #LOGO_AUTEUR de manière à prendre en compte le gravatar d’un auteur s’il existe, y compris dans les forums et pétitions.
 _ Permet de configurer une image par defaut, et la taille des images.
 
 Fournit le filtre <code>|gravatar</code>, à utiliser par exemple comme 
 <code>[(#EMAIL|gravatar|image_reduire{80})]</code>.',
-	'gravatar_slogan' => 'Afficher le Gravatar d\'un auteur ou d\'un contributeur de forum'
+	'gravatar_slogan' => 'Afficher le Gravatar d’un auteur ou d’un contributeur de forum'
 );
 
 ?>
