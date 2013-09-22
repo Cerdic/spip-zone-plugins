0d3ed835b15f8b90f32fd911080ab1d0a984f504
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-22 17:03:12 +0000

    langues (paquet-mutualisation)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76238 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- mutualisation/trunk/lang/paquet-mutualisation_fr.php
+++ mutualisation/trunk/lang/paquet-mutualisation_fr.php
@@ -6,8 +6,8 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// M
-	'mutualisation_description' => 'Ce plugin contient quelques fonctions facilitant la mutualisation de sites : notamment <code>demarrer_site()</code>, <code>prefixe_mutualisation()</code> et <code>mutualiser_creer()</code>.
-		Attention ce plugin ne s\'installe pas comme les autres (cf. documentation).',
+	'mutualisation_description' => 'Ce plugin contient quelques fonctions facilitant la mutualisation de sites : notamment <code>demarrer_site()</code>, <code>prefixe_mutualisation()</code> et <code>mutualiser_creer()</code>.
+		Attention ce plugin ne s’installe pas comme les autres (cf. documentation).',
 	'mutualisation_nom' => 'Mutualisation facile',
 	'mutualisation_slogan' => 'Créer une ferme à SPIP'
 );
