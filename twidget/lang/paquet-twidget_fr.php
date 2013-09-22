5a5fd20a8c1f61313f504835e699ebb88b8b4cc7
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-22 17:05:33 +0000

    langues (paquet-twidget)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76358 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- twidget/lang/paquet-twidget_fr.php
+++ twidget/lang/paquet-twidget_fr.php
@@ -7,11 +7,11 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// T
 	'twidget_description' => 'Un widget Twitter facile a installer
-_ Ajoutez une inclusion dans vos squelettes pour afficher un widget twitter :
+_ Ajoutez une inclusion dans vos squelettes pour afficher un widget twitter :
 _ <code>#INCLURE{fond=inclure/twidget_profile}</code>
  ou <code>#INCLURE{fond=inclure/twidget_search}</code>
 
- Le plugin fait office de proxy afin qu\'aucune requête ne soit faite par vos visiteurs vers twitter et éviter tout traçage possible de leur activité (préservation de leur vie personnelle).',
+ Le plugin fait office de proxy afin qu’aucune requête ne soit faite par vos visiteurs vers twitter et éviter tout traçage possible de leur activité (préservation de leur vie personnelle).',
 	'twidget_slogan' => 'Un widget Twitter facile a installer'
 );
 
--- twidget/lang/paquet-twidget_nl.php
+++ twidget/lang/paquet-twidget_nl.php
@@ -11,7 +11,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'twidget_description' => 'Twitter widget eenvoudig te installeren
 _ Zet een inclusie in je skeletten om de widget te wijzen:
 _ <code>#INCLURE{fond=inclure/twidget_profile,user=GusLeLapin}</code>
- of <code>#INCLURE{fond=inclure/twidget_search,search=#SPIP,title=\'Suivez twitter\',subject=\'SPIP\'}</code>
+ of <code>#INCLURE{fond=inclure/twidget_search,search=#SPIP,title=’Suivez twitter’,subject=’SPIP’}</code>
 
 De plugin acteer als een  proxy om geen rechtstreekse verzoek van je bezoekers naar Twitter te provoceren en zo het sporen van hun activiteiten te voorkomen  (verdediging van de privacy).', # MODIF
 	'twidget_slogan' => 'Twitter widget eenvoudig te installeren'
