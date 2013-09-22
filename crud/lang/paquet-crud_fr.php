760b665765271a4888cfc7f4aea4c18806e9d037
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-22 17:01:21 +0000

    langues (paquet-crud)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76140 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- crud/lang/paquet-crud_fr.php
+++ crud/lang/paquet-crud_fr.php
@@ -6,8 +6,8 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// C
-	'crud_description' => 'Une interface C(r)UD pour SPIP qui peut s\'utiliser par l\'intermédiaire d\'une action définie par son url ou par appel direct.',
-	'crud_slogan' => 'Interface de création, mise à jour et suppression d\'un objet'
+	'crud_description' => 'Une interface C(r)UD pour SPIP qui peut s’utiliser par l’intermédiaire d’une action définie par son url ou par appel direct.',
+	'crud_slogan' => 'Interface de création, mise à jour et suppression d’un objet'
 );
 
 ?>
