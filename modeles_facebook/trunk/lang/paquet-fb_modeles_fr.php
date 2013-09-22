3d42344a770242e1bc0be62cbdf8fd78f34e3a20
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-22 17:03:04 +0000

    langues (paquet-fb_modeles)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76230 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- modeles_facebook/trunk/lang/paquet-fb_modeles_fr.php
+++ modeles_facebook/trunk/lang/paquet-fb_modeles_fr.php
@@ -6,14 +6,14 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// F
-	'fb_modeles_description' => 'Ce plugin propose un ensemble de modèles SPIP pour intégrer simplement et rapidement les modules sociaux du site Facebook :
+	'fb_modeles_description' => 'Ce plugin propose un ensemble de modèles SPIP pour intégrer simplement et rapidement les modules sociaux du site Facebook :
 
--*  {{send}} : le bouton « Envoyer » classique (avec commentaire)
--*  {{like}} : le bouton « J\'aime » classique (avec commentaire)
--*  {{like box}} : liste des commentaires des utilisateurs qui ont « aimé »
--*  {{activity}} : liste des activités récentes d\'une page ou d\'un profil
--*  {{comments}} : liste des commentaires et bouton permettant d\'en écrire un
--*  {{live stream}} (expérimental) : boîte de discussion type « chat »',
+-*  {{send}} : le bouton « Envoyer » classique (avec commentaire)
+-*  {{like}} : le bouton « J’aime » classique (avec commentaire)
+-*  {{like box}} : liste des commentaires des utilisateurs qui ont « aimé »
+-*  {{activity}} : liste des activités récentes d’une page ou d’un profil
+-*  {{comments}} : liste des commentaires et bouton permettant d’en écrire un
+-*  {{live stream}} (expérimental) : boîte de discussion type « chat »',
 	'fb_modeles_nom' => 'Facebook Models',
 	'fb_modeles_slogan' => 'Modèles pour inclure les modules sociaux Facebook'
 );
