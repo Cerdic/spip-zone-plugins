e7036b68459d9886017e5a3ec71a1288762274a6
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-22 17:05:08 +0000

    langues (svpstats)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76335 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- svp_stats/trunk/lang/svpstats_fr.php
+++ svp_stats/trunk/lang/svpstats_fr.php
@@ -10,8 +10,8 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'bulle_actualiser_stats' => 'Actualiser les statistiques des plugins',
 
 	// I
-	'info_actualisation_stats_cron' => 'Les statistiques d\'utilisation des plugins sont actualisées automatiquement tous les @periode@ jour(s).',
-	'info_boite_statistiques' => '<strong>Vous avez activé les statistiques d\'utilisation des plugins.</strong><p>Celles-ci sont actualisées tous les @periode@ jour(s). Néanmoins, vous avez la possibilité de lancer, à tout moment, une mise à jour manuelle.</p>',
+	'info_actualisation_stats_cron' => 'Les statistiques d’utilisation des plugins sont actualisées automatiquement tous les @periode@ jour(s).',
+	'info_boite_statistiques' => '<strong>Vous avez activé les statistiques d’utilisation des plugins.</strong><p>Celles-ci sont actualisées tous les @periode@ jour(s). Néanmoins, vous avez la possibilité de lancer, à tout moment, une mise à jour manuelle.</p>',
 	'info_nbr_sites_0' => 'Utilisé dans aucun site',
 	'info_nbr_sites_1' => 'Utilisé dans 1 site',
 	'info_nbr_sites_n' => 'Utilisé dans @nb@ sites'
