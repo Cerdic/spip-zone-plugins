5ce276448e46d561d862fc6dece4cfab08b34201
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-22 17:03:08 +0000

    langues (paquet-multilang)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76234 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- multilang/trunk/lang/paquet-multilang_fr.php
+++ multilang/trunk/lang/paquet-multilang_fr.php
@@ -7,7 +7,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// M
 	'multilang_description' => 'Ce plugin rajoute un menu de langues du type <code>[fr] [en] [it]</code> au-dessus de chaque formulaire, en fonction des langues activées dans la configuration du site.',
-	'multilang_slogan' => 'Gérer l\'affichage du contenu des tags multi par un menu'
+	'multilang_slogan' => 'Gérer l’affichage du contenu des tags multi par un menu'
 );
 
 ?>
