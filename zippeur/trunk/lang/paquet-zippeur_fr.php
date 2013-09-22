d682f50aa151dc6443787d831dd60563575d45bf
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-22 17:43:26 +0000

    fichier de langue oublié
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76402 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ zippeur/trunk/lang/paquet-zippeur_fr.php
@@ -0,0 +1,19 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-zippeur
+// Langue: fr
+// Date: 22-09-2013 17:53:07
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// Z
+	'zippeur_description' => 'Il est parfois utile, notamment quand un article comprend beaucoup de documents joints, de proposer l’ensemble des documents sous forme de zip. Ce plugin permet à SPIP de générer une archive zip à partir d’une liste de documents.',
+	'zippeur_slogan' => 'Zippez facilement vos fichiers',
+);
+?>
