31160ba5d086f38d8b1b1fd15e0b2aa09f184f16
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-22 17:00:38 +0000

    langues (paquet-boutonstexte)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76104 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- boutonstexte/trunk/lang/paquet-boutonstexte_fr.php
+++ boutonstexte/trunk/lang/paquet-boutonstexte_fr.php
@@ -6,7 +6,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// B
-	'boutonstexte_description' => 'Rajoute des boutons pour l\'affichage des textes comme augmenter/diminuer police. Les boutons sont rajoutés avant chaque div de classe texte du contenu si ils ne sont pas déjà présents dans la page (images de classe textsizeup, textsizedown ou textonly)
+	'boutonstexte_description' => 'Rajoute des boutons pour l’affichage des textes comme augmenter/diminuer police. Les boutons sont rajoutés avant chaque div de classe texte du contenu si ils ne sont pas déjà présents dans la page (images de classe textsizeup, textsizedown ou textonly)
 
 La configuration optionnelle peut se faire en installant le plugin cfg. Certains éléments peuvent être forcés par javascript.',
 	'boutonstexte_nom' => 'Boutons dans le Texte',
