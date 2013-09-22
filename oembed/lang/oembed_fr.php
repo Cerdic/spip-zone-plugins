0f768b0c950dfe3a551031e8433e0515ee7f17ea
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-22 17:03:29 +0000

    langues (oembed)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76252 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- oembed/lang/oembed_fr.php
+++ oembed/lang/oembed_fr.php
@@ -9,18 +9,18 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'aucun_provider' => 'Aucun provider',
 
 	// E
-	'erreur_ajout_provider' => 'Erreur lors de l\'ajout du provider.',
+	'erreur_ajout_provider' => 'Erreur lors de l’ajout du provider.',
 	'erreur_scheme_doublon' => 'Ce scheme est déjà présent dans la liste.',
 	'explication_scheme' => 'utilisez * comme wildcard',
-	'explication_upload_url' => 'Indiquez simplement l\'URL de la page qui contient le document pour les sites @hosts@',
+	'explication_upload_url' => 'Indiquez simplement l’URL de la page qui contient le document pour les sites @hosts@',
 
 	// I
 	'info_1_provider' => '1 provider',
 	'info_nb_providers' => '@nb@ providers',
 
 	// L
-	'label_detecter_lien' => 'Détecter automatiquement les providers (ignorer la liste blanche) ?',
-	'label_embed_auto' => 'Transformer automatiquement les URLs insérées dans le texte ?',
+	'label_detecter_lien' => 'Détecter automatiquement les providers (ignorer la liste blanche) ?',
+	'label_embed_auto' => 'Transformer automatiquement les URLs insérées dans le texte ?',
 	'label_endpoint' => 'Endpoint',
 	'label_maxheight' => 'Hauteur maxi. par défaut (px)',
 	'label_maxwidth' => 'Largeur maxi. par défaut (px)',
