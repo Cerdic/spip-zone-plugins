a9c74a607417cfecebb2c5d65a2d60ed12618d4a
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-22 17:03:48 +0000

    langues (piecon)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76271 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- piecon/trunk/lang/piecon_fr.php
+++ piecon/trunk/lang/piecon_fr.php
@@ -9,16 +9,16 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'cfg_titre_piecon' => 'Configuration de Piecon',
 
 	// E
-	'explication_fallback' => 'Le fallback est la méthode utilisée pour changer soit le title, soit la favicon de la page. Il peut avoir 3 valeurs :<br />
-"false", la valeur par défaut, ne changera que la favicon lorsque c\'est possible<br />
+	'explication_fallback' => 'Le fallback est la méthode utilisée pour changer soit le title, soit la favicon de la page. Il peut avoir 3 valeurs :<br />
+"false", la valeur par défaut, ne changera que la favicon lorsque c’est possible<br />
 "true", seul le title sera modifié en indiquant un pourcentage même si le navigateur accepte le changement de favicon<br />
-"force", changera la favicon lorsque c\'est possible ainsi que le title de la page;',
+"force", changera la favicon lorsque c’est possible ainsi que le title de la page ;',
 
 	// L
 	'label_background' => 'Couleur de fond',
 	'label_color' => 'Couleur',
 	'label_fallback' => 'Méthode par défaut',
-	'label_shadow' => 'Couleur d\'ombre',
+	'label_shadow' => 'Couleur d’ombre',
 
 	// O
 	'option_fallback_false' => 'false',
