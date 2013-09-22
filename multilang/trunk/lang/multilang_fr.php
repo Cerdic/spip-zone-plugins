416af6a5b0e8eba263a5a5cd84ba3793c3c92e27
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-22 17:03:07 +0000

    langues (multilang)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76233 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- multilang/trunk/lang/multilang_fr.php
+++ multilang/trunk/lang/multilang_fr.php
@@ -6,14 +6,14 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// C
-	'cfg_activer' => 'Activer Multilang sur la page d\'édition :',
+	'cfg_activer' => 'Activer Multilang sur la page d’édition :',
 	'cfg_boite_multilang' => 'Configuration du plugin Multilang',
-	'cfg_descr_multilang' => 'Multilang : Plugin de gestion des tags multi',
+	'cfg_descr_multilang' => 'Multilang : Plugin de gestion des tags multi',
 	'cfg_effacer' => 'Re-initialiser',
-	'cfg_explication_crayons' => 'Nécessite que multilang soit activé dans l\'espace public du site.',
+	'cfg_explication_crayons' => 'Nécessite que multilang soit activé dans l’espace public du site.',
 	'cfg_langues' => 'Langues utilisées',
 	'cfg_lbl_crayons' => 'Utiliser multilang dans les crayons',
-	'cfg_lbl_espace_public' => 'Utiliser multilang dans l\'espace public du site',
+	'cfg_lbl_espace_public' => 'Utiliser multilang dans l’espace public du site',
 	'cfg_lbl_formstables' => 'des forms&tables',
 	'cfg_lbl_siteconfig' => 'de la configuration du site',
 	'cfg_titre_multilang' => 'Multilang',
@@ -21,13 +21,13 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'configuration_multilang' => 'Multilang',
 
 	// E
-	'explication_langues_utilisees' => 'Sélectionnez les langues à utiliser pour le menu de multilang. N\'en sélectionnez aucune pour toutes les utiliser.',
+	'explication_langues_utilisees' => 'Sélectionnez les langues à utiliser pour le menu de multilang. N’en sélectionnez aucune pour toutes les utiliser.',
 
 	// L
 	'label_langues_utilisees' => 'Langues utilisées par multilang',
 	'lien_desactiver' => 'Tout',
-	'lien_multi_title' => 'Éditer les champs multilingues en «@lang@»',
-	'lien_multi_title_sans' => 'La version «@lang@» est incomplète. Cliquez pour l\'éditer.',
+	'lien_multi_title' => 'Éditer les champs multilingues en « @lang@ »',
+	'lien_multi_title_sans' => 'La version « @lang@ » est incomplète. Cliquez pour l’éditer.',
 
 	// M
 	'message_champs_readonly' => 'Les champs multilingues sont en lecture seule, choisissez une langue pour les modifier'
