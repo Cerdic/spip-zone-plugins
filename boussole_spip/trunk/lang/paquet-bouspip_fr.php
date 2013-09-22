31256f077fc9c541ffb66677d1495b62f7b956b4
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-22 17:00:36 +0000

    langues (paquet-bouspip)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76102 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- boussole_spip/trunk/lang/paquet-bouspip_fr.php
+++ boussole_spip/trunk/lang/paquet-bouspip_fr.php
@@ -6,9 +6,9 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// B
-	'bouspip_description' => 'Ce plugin fournit le nom, l\'url et la description de tous les sites de la Galaxie SPIP. Il doit être utilisé avec le plugin Boussole en mode serveur afin de fournir, via un service web, ses informations à tous les sites souhaitant afficher la boussole SPIP.',
+	'bouspip_description' => 'Ce plugin fournit le nom, l’url et la description de tous les sites de la Galaxie SPIP. Il doit être utilisé avec le plugin Boussole en mode serveur afin de fournir, via un service web, ses informations à tous les sites souhaitant afficher la boussole SPIP.',
 	'bouspip_nom' => 'Boussole SPIP',
-	'bouspip_slogan' => 'Les bonnes adresses de la galaxie SPIP !'
+	'bouspip_slogan' => 'Les bonnes adresses de la galaxie SPIP !'
 );
 
 ?>
