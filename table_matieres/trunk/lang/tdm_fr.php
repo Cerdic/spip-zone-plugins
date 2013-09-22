c03aecafe246893290d6bbec5360389dc2e4cef5
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-22 17:05:10 +0000

    langues (tdm)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76337 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- table_matieres/trunk/lang/tdm_fr.php
+++ table_matieres/trunk/lang/tdm_fr.php
@@ -6,15 +6,15 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
-	'automatique' => 'La table des matières est-elle générée automatiquement ?',
+	'automatique' => 'La table des matières est-elle générée automatiquement ?',
 	'automatique_desription' => 'Désactivez cette option si vous placez vous-même
 		la balise <code>#TABLE_MATIERES</code> dans vos squelettes.',
 
 	// E
 	'explication_longueur' => 'Définit le nombre de caractères maximum que constituera chaque ancres.',
-	'explication_min' => 'Définit le nombre minimal d\'intertitres d\'un texte à partir duquel une table des matières sera affichée.',
-	'explication_separateur' => 'Définit le caractère qui s\'intercalera entre chaque mot pour remplacer les espaces.',
-	'explication_tdm_flottante' => 'Lorsque la table des matières devient invisible lors du scroll de la page, celle-ci se positionne en haut de l\'écran afin d\'être constamment visible.',
+	'explication_min' => 'Définit le nombre minimal d’intertitres d’un texte à partir duquel une table des matières sera affichée.',
+	'explication_separateur' => 'Définit le caractère qui s’intercalera entre chaque mot pour remplacer les espaces.',
+	'explication_tdm_flottante' => 'Lorsque la table des matières devient invisible lors du scroll de la page, celle-ci se positionne en haut de l’écran afin d’être constamment visible.',
 
 	// L
 	'label_tdm_flottante' => 'Table des matières flottante',
