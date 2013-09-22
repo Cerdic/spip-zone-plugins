4fb91fc51d6b93648847b86b32015e864cbcd1a5
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-22 17:04:45 +0000

    langues (paquet-smush)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76313 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- smush_images/trunk/lang/paquet-smush_fr.php
+++ smush_images/trunk/lang/paquet-smush_fr.php
@@ -6,7 +6,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// S
-	'smush_description' => 'Le plugin Smush permet de réduire la taille des images d\'un site au minimum en utilisant des logiciels spécifiques sur le serveur.',
+	'smush_description' => 'Le plugin Smush permet de réduire la taille des images d’un site au minimum en utilisant des logiciels spécifiques sur le serveur.',
 	'smush_slogan' => 'Réduire ses images à peau de chagrin.'
 );
 
