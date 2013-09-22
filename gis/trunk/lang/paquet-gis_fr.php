834e9d1b18b370a3e4abfe25fad194cf1df7c903
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-22 17:02:16 +0000

    langues (paquet-gis)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76185 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- gis/trunk/lang/paquet-gis_fr.php
+++ gis/trunk/lang/paquet-gis_fr.php
@@ -7,7 +7,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// G
 	'gis_description' => 'Ce plugin permet de créer des points géolocalisés qui peuvent être attachés aux objets de SPIP afin de les afficher sur des cartes dans les pages de votre site. Ces dernières peuvent utiliser les tuiles de différents fournisseurs grâce à la librairie Leaflet.',
-	'gis_slogan' => 'Système d\'information géographique'
+	'gis_slogan' => 'Système d’information géographique'
 );
 
 ?>
