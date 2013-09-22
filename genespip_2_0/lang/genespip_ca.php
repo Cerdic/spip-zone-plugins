d7500988dcea49a310cc6de58bbfc07571b90cdd
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-22 17:02:17 +0000

    langues (genespip)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76186 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- genespip_2_0/lang/genespip_ca.php
+++ genespip_2_0/lang/genespip_ca.php
@@ -8,10 +8,10 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
-	'a_ete_mise_a_la_poubelle' => 's\'ha posat a la paperera',
-	'acces_non' => 'Drets d\'accés » restringit',
-	'acces_oui' => 'Drets d\'accés» detalls de les fitxes',
-	'acces_restreint' => 'L\'accés a aquest lloc està limitat',
+	'a_ete_mise_a_la_poubelle' => 's’ha posat a la paperera',
+	'acces_non' => 'Drets d’accés » restringit',
+	'acces_oui' => 'Drets d’accés» detalls de les fitxes',
+	'acces_restreint' => 'L’accés a aquest lloc està limitat',
 	'administrateur' => 'Administrador',
 	'adresse' => 'Adreça',
 	'ajout_evenement' => 'Afegir un esdeveniment',
@@ -19,7 +19,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'ajout_media' => 'Afegir un mitjà',
 	'ajouter_nouveau_lien_article_spip_existant' => 'Afegir un nou enllaç cap a un article SPIP existent',
 	'annuler' => 'Anular ',
-	'arborescence_des' => 'Veure l\'arbre',
+	'arborescence_des' => 'Veure l’arbre',
 	'arbre_asc' => 'Arbre ascendent',
 	'article_spip_lie' => 'Núm. article SPIP lligat',
 	'aucune_fiche_possede_numero' => 'Cap fitxa té aquest número',
@@ -35,7 +35,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'centans' => 'Prohibir la publicació de dates de menys de 100 anys',
 	'charger' => 'Carregar',
 	'choisir' => 'Escollir',
-	'cliquer_ici_pour_lier_article_avec_fiche' => 'Fer un clic aquí per enllaçar l\'article a la fitxa',
+	'cliquer_ici_pour_lier_article_avec_fiche' => 'Fer un clic aquí per enllaçar l’article a la fitxa',
 	'confirmer' => 'Confirmar',
 	'contact' => 'Contacte',
 	'continuer' => 'Continuar',
@@ -52,7 +52,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'departement' => 'Departament',
 	'derniere_modif_le' => 'Darrera modificació el ',
 	'derniere_modification' => 'Darrera actualització de la fitxa',
-	'descriptif_cfg' => '<p>Aquesta zona permet configurar les variables de base del connector.</p><p> Per funcionar correctament aquest connector s\'ha d\'instal·lar en paral·lel amb el connector cfg</p>',
+	'descriptif_cfg' => '<p>Aquesta zona permet configurar les variables de base del connector.</p><p> Per funcionar correctament aquest connector s’ha d’instal·lar en paral·lel amb el connector cfg</p>',
 	'detail_fiche' => 'Detall fitxa',
 	'document' => 'Documents', # MODIF
 	'documents' => 'Documents',
@@ -97,17 +97,17 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// I
 	'indication_format_photo' => '<br />Pes màxim 100 Kb',
 	'individu' => 'individu',
-	'info_centans' => '<p>Prohibir sistemàticament la publicació de dates que tinguin menys de 100 anys.<br />Aquesta opció només s\'aplica als visitants.<br />Si aquesta opció està desactivada, és possible limitar la publicació fitxa per fitxa.</p>',
+	'info_centans' => '<p>Prohibir sistemàticament la publicació de dates que tinguin menys de 100 anys.<br />Aquesta opció només s’aplica als visitants.<br />Si aquesta opció està desactivada, és possible limitar la publicació fitxa per fitxa.</p>',
 	'info_deces' => 'Info defunció',
-	'info_doc' => '<p>Connector de genealogia.</p> <p>Publicar la vostra genealogia a Internet a partir d\'un lloc SPIP.</p>',
+	'info_doc' => '<p>Connector de genealogia.</p> <p>Publicar la vostra genealogia a Internet a partir d’un lloc SPIP.</p>',
 	'info_gedcom_etape1' => 'El vostre arbre està buit, importar un fitxer GedCom (1 MB Màx.) o crear una nova fitxa.<br />',
-	'info_gedcom_etape2' => 'Per tal d\'importar el vostre fitxer GedCom, GeneSPIP necessita conèixer el vostre mètode de tractament de l\'entrada "PLAC"<br />GeneSPIP a seleccionar les 5 primeres entrades  "PLAC". Escolliu-ne una que servirà de referència per les noves',
-	'info_gedcom_etape3' => 'A partir de l\'entrada escollida, seleccioneu per cada informació com GeneSPIP l\'ha de traduir ',
+	'info_gedcom_etape2' => 'Per tal d’importar el vostre fitxer GedCom, GeneSPIP necessita conèixer el vostre mètode de tractament de l’entrada "PLAC"<br />GeneSPIP a seleccionar les 5 primeres entrades  "PLAC". Escolliu-ne una que servirà de referència per les noves',
+	'info_gedcom_etape3' => 'A partir de l’entrada escollida, seleccioneu per cada informació com GeneSPIP l’ha de traduir ',
 	'info_mariage' => 'Info casament',
 	'info_multilingue' => '<p>Mostrar la cibertira multilingüe al lloc.<br />Aneu al menú multilingüe de SPIP per desactivar les llengües inútils</p>',
 	'info_naissance' => 'Info naixement',
 	'info_pub' => '<p>Els ingressos publicitaris estan destinats al creador de GeneSPIP. Podeu situar la vostra pròpia cibertira publicitària modificant el contingut del fitxer pub/200x200genespip.html. Aboneu-vos al programa de Google AdSense si voleu crear la vostra cibertira</p>',
-	'info_restriction_acces' => '<p>Limitar l\'accés als detalls de les fitxes al lloc públic</p>',
+	'info_restriction_acces' => '<p>Limitar l’accés als detalls de les fitxes al lloc públic</p>',
 	'info_restriction_acces0' => '<b>0</b>: Limitació als administradors',
 	'info_restriction_acces1' => '<b>1</b> : Limitació als administradors i redactors',
 	'info_restriction_acces2' => '<b>2</b> : Limitació als administradors, redactors i visitants (privat)',
@@ -117,7 +117,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'invalide' => 'Invàlid',
 
 	// J
-	'journal' => 'Diari d\'esdeveniments',
+	'journal' => 'Diari d’esdeveniments',
 
 	// L
 	'le' => 'el',
@@ -127,11 +127,11 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'limitation' => 'Tornar invisibles les dates al lloc públic',
 	'limitation_non' => '(» Dates visibles)',
 	'limitation_oui' => '(» Dates invisibles)',
-	'liste_des_articles' => 'Llista d\'articles SPIP',
+	'liste_des_articles' => 'Llista d’articles SPIP',
 	'liste_des_enfants' => 'Llista dels nens',
 	'liste_des_lieux' => 'Llista dels llocs',
 	'liste_des_personnes_nees' => 'Llista de les persones nascudes',
-	'liste_des_unions' => 'Llista d\'unions',
+	'liste_des_unions' => 'Llista d’unions',
 	'liste_noms' => 'Llista dels noms',
 	'liste_patronyme' => 'Llista de cognoms',
 
