60f213c1fd15885d53adc4d209c5432704667aea
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-22 17:04:13 +0000

    langues (paquet-ppp)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76282 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- porte_plume_extras/partout/lang/paquet-ppp_fr.php
+++ porte_plume_extras/partout/lang/paquet-ppp_fr.php
@@ -6,7 +6,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// P
-	'ppp_description' => 'Porte Plume Partout sert à ajouter la barre d\'édition aux champs DESCRIPTIF, CHAPO et PS',
+	'ppp_description' => 'Porte Plume Partout sert à ajouter la barre d’édition aux champs DESCRIPTIF, CHAPO et PS',
 	'ppp_nom' => 'Porte Plume Partout',
 	'ppp_slogan' => 'Afficher le Porte Plume sur la plupart des champs de saisie'
 );
