6d34adc74b73ab83833eb7c65c1f007be3c370ea
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-22 17:00:20 +0000

    langues (paquet-ancresdouces)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76088 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- ancres_douces/lang/paquet-ancresdouces_fr.php
+++ ancres_douces/lang/paquet-ancresdouces_fr.php
@@ -6,7 +6,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
-	'ancresdouces_description' => 'Ce plugin permet un défilement continu de la page lorsque le visiteur clique sur un lien pointant vers une ancre de l\'article en cours de lecture.',
+	'ancresdouces_description' => 'Ce plugin permet un défilement continu de la page lorsque le visiteur clique sur un lien pointant vers une ancre de l’article en cours de lecture.',
 	'ancresdouces_nom' => 'Ancres Douces',
 	'ancresdouces_slogan' => 'Des ancres en douceur'
 );
