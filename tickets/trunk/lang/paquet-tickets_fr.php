9244f5682d176f953c9dfacef114fb212466fb5f
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-22 17:05:16 +0000

    langues (paquet-tickets)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76343 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- tickets/trunk/lang/paquet-tickets_fr.php
+++ tickets/trunk/lang/paquet-tickets_fr.php
@@ -6,7 +6,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// T
-	'tickets_description' => '{Tickets} est un outil de suivi de bugs directement intégré à l\'espace privé de SPIP. Il est conçu pour faciliter la phase de développement du site (notamment les squelettes du site public).',
+	'tickets_description' => '{Tickets} est un outil de suivi de bugs directement intégré à l’espace privé de SPIP. Il est conçu pour faciliter la phase de développement du site (notamment les squelettes du site public).',
 	'tickets_nom' => 'Tickets',
 	'tickets_slogan' => 'Système de suivi de bugs'
 );
--- tickets/trunk/lang/paquet-tickets_sk.php
+++ tickets/trunk/lang/paquet-tickets_sk.php
@@ -8,6 +8,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// T
+	'tickets_description' => '{Lístky} sú nástrojom na sledovanie chýb priamo začleneným do súkromnej zóny SPIPu. Sú vytvorené na pomoc pri vývoji prvkov stránky (vrátane šablón verejne prístupnej stránky).',
 	'tickets_nom' => 'Lístky',
 	'tickets_slogan' => 'Systém na sledovanie chýb'
 );
