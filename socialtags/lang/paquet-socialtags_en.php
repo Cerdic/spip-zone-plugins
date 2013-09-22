0943a538dbdf7bb1d767d8f09a885be6adb1b1ea
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-22 17:04:47 +0000

    langues (paquet-socialtags)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76315 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- socialtags/lang/paquet-socialtags_en.php
+++ socialtags/lang/paquet-socialtags_en.php
@@ -9,7 +9,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// S
 	'socialtags_description' => 'The Social Tags plugin allows you to add icons to share links to websites like Digg, Facebook, Delicious...
-In addition, if the Google+ 1 plugin is active, it\'s possible to add the button in the list of links in Social tags. 
+In addition, if the Google+ 1 plugin is active, it’s possible to add the button in the list of links in Social tags. 
 It also offers configurable nuts to display badges and "Like" box on Facebook.',
 	'socialtags_slogan' => 'Activate the social link sharing network buttons.'
 );
--- socialtags/lang/paquet-socialtags_fr.php
+++ socialtags/lang/paquet-socialtags_fr.php
@@ -6,8 +6,8 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// S
-	'socialtags_description' => 'Le plugin Social Tags permet d\'ajouter des icônes de partage de liens vers les sites tels que Digg, Facebook, Delicious...
-En outre, si le plugin Google+1 est actif, il est possible de rajouter le bouton correspondant dans la liste des liens de Social tags. Il propose également des noisettes paramétrables permettant d\'afficher les badges et boites de type "J\'aime" de Facebook.',
+	'socialtags_description' => 'Le plugin Social Tags permet d’ajouter des icônes de partage de liens vers les sites tels que Digg, Facebook, Delicious...
+En outre, si le plugin Google+1 est actif, il est possible de rajouter le bouton correspondant dans la liste des liens de Social tags. Il propose également des noisettes paramétrables permettant d’afficher les badges et boites de type "J’aime" de Facebook.',
 	'socialtags_slogan' => 'Activer des boutons de partage de liens sur les réseaux sociaux.'
 );
 
