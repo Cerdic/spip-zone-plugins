a0daf0eacea852199b592c282ed6f956b5cadae4
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-22 17:01:20 +0000

    langues (crud)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76139 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- crud/lang/crud_fr.php
+++ crud/lang/crud_fr.php
@@ -6,17 +6,17 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// E
-	'erreur_action_erronee' => 'CRUD : action @action@ erronée',
-	'erreur_action_inconnue_table' => 'CRUD : action @action@ inconnue pour table @table@',
-	'erreur_article_inconnue' => 'CRUD : l\'article @id@ n\'existe pas',
-	'erreur_creation' => 'CRUD : impossible de créer l\'objet de type "@objet@" (Vérifiez vos droits)',
-	'erreur_info_obligatoire' => 'CRUD : Le champ @info@ est obligatoire',
-	'erreur_objet_inexistant' => 'CRUD : l\'objet @objet@ #@id_objet@ n\'existe pas',
-	'erreur_rubrique_inconnue' => 'CRUD : la rubrique @id@ n\'existe pas',
-	'erreur_suppression' => 'CRUD : erreur de suppression de l\'objet "@objet@" #@id_objet@ (Vérifiez vos droits)',
-	'erreur_table_erronee' => 'CRUD : table @table@ erronée',
-	'erreur_table_inconnue' => 'CRUD : table @table@ inconnue',
-	'erreur_update' => 'CRUD : erreur de mise à jour de l\'objet "@objet@" #@id@ (Vérifiez vos droits)'
+	'erreur_action_erronee' => 'CRUD : action @action@ erronée',
+	'erreur_action_inconnue_table' => 'CRUD : action @action@ inconnue pour table @table@',
+	'erreur_article_inconnue' => 'CRUD : l’article @id@ n’existe pas',
+	'erreur_creation' => 'CRUD : impossible de créer l’objet de type "@objet@" (Vérifiez vos droits)',
+	'erreur_info_obligatoire' => 'CRUD : Le champ @info@ est obligatoire',
+	'erreur_objet_inexistant' => 'CRUD : l’objet @objet@ #@id_objet@ n’existe pas',
+	'erreur_rubrique_inconnue' => 'CRUD : la rubrique @id@ n’existe pas',
+	'erreur_suppression' => 'CRUD : erreur de suppression de l’objet "@objet@" #@id_objet@ (Vérifiez vos droits)',
+	'erreur_table_erronee' => 'CRUD : table @table@ erronée',
+	'erreur_table_inconnue' => 'CRUD : table @table@ inconnue',
+	'erreur_update' => 'CRUD : erreur de mise à jour de l’objet "@objet@" #@id@ (Vérifiez vos droits)'
 );
 
 ?>
