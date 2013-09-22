00289a1fe3a7f92900afeb99baf4239a4fab0496
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-22 17:00:08 +0000

    langues (paquet-abomailmans)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76079 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- abomailmans/trunk/lang/paquet-abomailmans_fr.php
+++ abomailmans/trunk/lang/paquet-abomailmans_fr.php
@@ -6,10 +6,10 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
-	'abomailmans_description' => 'Ce plugin permet via une balise #FORMULAIRE_ABOMAILMAN d\'afficher un formulaire d\'abonnement à l\'ensemble des listes [Mailman->http://www.gnu.org/software/mailman/], [Ezmlm->http://www.ezmlm.org/] ou [Sympa->http://www.sympa.org] renseignées.
-_ Cette inscription se fait via l\'interface privée de SPIP par le menu "Edition Abomailmans les listes de diffusions mailman".
+	'abomailmans_description' => 'Ce plugin permet via une balise #FORMULAIRE_ABOMAILMAN d’afficher un formulaire d’abonnement à l’ensemble des listes [Mailman->http://www.gnu.org/software/mailman/], [Ezmlm->http://www.ezmlm.org/] ou [Sympa->http://www.sympa.org] renseignées.
+_ Cette inscription se fait via l’interface privée de SPIP par le menu "Edition Abomailmans les listes de diffusions mailman".
 _ Ensuite il vous est possible de sélectionner des articles de votre SPIP, par des critères de date, de rubrique et/ou de mot-clé et envoyer leurs contenus à ces listes, en utilisant des squelettes de mise en page du contenu.
-_ Le lien avec les listes Mailman, Ezmlm et Sympa se fait par des envois d\'e-mail.',
+_ Le lien avec les listes Mailman, Ezmlm et Sympa se fait par des envois d’e-mail.',
 	'abomailmans_nom' => 'Abonnement à des listes de diffusion',
 	'abomailmans_slogan' => 'Interfacer vos listes de diffusions mailman, ezmlm ou sympa avec votre SPIP'
 );
