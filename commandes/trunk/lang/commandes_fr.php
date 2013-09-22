5f0f8a0c761cea5f6704d350171af8fe6e090876
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-22 17:00:51 +0000

    langues (commandes)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76116 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- commandes/trunk/lang/commandes_fr.php
+++ commandes/trunk/lang/commandes_fr.php
@@ -20,7 +20,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'commande_date' => 'Date',
 	'commande_date_paiement' => 'Date de paiement',
 	'commande_editer' => 'Éditer la commande',
-	'commande_modifier' => 'Modifier la commande :',
+	'commande_modifier' => 'Modifier la commande :',
 	'commande_montant' => 'Montant',
 	'commande_nouvelle' => 'Nouvelle commande',
 	'commande_numero' => 'Commande ',
@@ -29,24 +29,24 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'commande_statut' => 'Statut',
 	'commandes_titre' => 'Commandes',
 	'configurer_titre' => 'Configurer le plugin Commande',
-	'confirmer_supprimer_commande' => 'Confirmez-vous la suppression de la commande ?',
-	'contact_label' => 'Contact :',
+	'confirmer_supprimer_commande' => 'Confirmez-vous la suppression de la commande ?',
+	'contact_label' => 'Contact :',
 	'contenu' => 'Contenu',
 
 	// D
-	'date_commande_label' => 'Date de création :',
-	'date_envoi_label' => 'Date d’envoi :',
-	'date_maj_label' => 'Date de mise à jour :',
-	'date_paiement_label' => 'Date de paiement :',
+	'date_commande_label' => 'Date de création :',
+	'date_envoi_label' => 'Date d’envoi :',
+	'date_maj_label' => 'Date de mise à jour :',
+	'date_paiement_label' => 'Date de paiement :',
 	'designation' => 'Désignation',
-	'details_commande' => 'Détails de commande :',
+	'details_commande' => 'Détails de commande :',
 
 	// E
 	'etat' => 'État',
 
 	// F
-	'facture_date' => 'Date : <span>@date@</span>',
-	'facture_num' => 'Facture n° <span>@num@</span>',
+	'facture_date' => 'Date : <span>@date@</span>',
+	'facture_num' => 'Facture n° <span>@num@</span>',
 	'facture_titre' => 'Facture',
 
 	// I
@@ -60,7 +60,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'info_nb_commandes' => '@nb@ commandes',
 	'info_nb_commandes_attente_livraison' => '@nb@ commandes à envoyer',
 	'info_nb_commandes_attente_paiement' => '@nb@ commandes en attente de validation',
-	'info_numero_commande' => 'COMMANDE NUMÉRO :',
+	'info_numero_commande' => 'COMMANDE NUMÉRO :',
 	'info_toutes_commandes' => 'Toutes les commandes',
 
 	// L
@@ -77,38 +77,38 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// M
 	'merci_de_votre_commande' => 'Nous avons bien enregistré votre commande et nous vous remercions de votre confiance.',
-	'modifier_commande_statut' => 'Cette commande est :',
+	'modifier_commande_statut' => 'Cette commande est :',
 	'montant' => 'Montant',
 
 	// N
 	'nom_bouton_plugin' => 'Commandes',
-	'notifications_activer_explication' => 'Envoyer par mail des notifications de commande ?',
+	'notifications_activer_explication' => 'Envoyer par mail des notifications de commande ?',
 	'notifications_activer_label' => 'Activer',
 	'notifications_cfg_titre' => 'Notifications',
-	'notifications_client_explication' => 'Envoyer les notifications au client ?',
+	'notifications_client_explication' => 'Envoyer les notifications au client ?',
 	'notifications_client_label' => 'Client',
-	'notifications_expediteur_administrateur_label' => 'Choisir un administrateur :',
+	'notifications_expediteur_administrateur_label' => 'Choisir un administrateur :',
 	'notifications_expediteur_choix_administrateur' => 'un administrateur',
 	'notifications_expediteur_choix_email' => 'un email',
 	'notifications_expediteur_choix_facteur' => 'idem plugin Facteur',
 	'notifications_expediteur_choix_webmaster' => 'un webmestre',
-	'notifications_expediteur_email_label' => 'Email de l\'expéditeur :',
+	'notifications_expediteur_email_label' => 'Email de l’expéditeur :',
 	'notifications_expediteur_explication' => 'Choisir l’expéditeur des notifications pour le vendeur et l’acheteur',
 	'notifications_expediteur_label' => 'Expéditeur',
-	'notifications_expediteur_webmaster_label' => 'Choisir un webmestre :',
-	'notifications_explication' => 'Les notifications permettent d’envoyer des emails suite aux changements de statut des commandes : En attente de validation, en cours, envoyée, partiellement payée, payée, retournée, retour partiel. Cette fonctionnalité nécessite le plugin Notifications Avancées.',
+	'notifications_expediteur_webmaster_label' => 'Choisir un webmestre :',
+	'notifications_explication' => 'Les notifications permettent d’envoyer des emails suite aux changements de statut des commandes : En attente de validation, en cours, envoyée, partiellement payée, payée, retournée, retour partiel. Cette fonctionnalité nécessite le plugin Notifications Avancées.',
 	'notifications_parametres' => 'Paramètres des notifications',
-	'notifications_quand_explication' => 'Quel(s) changement(s) de statut déclenche(nt) l’envoi d’une notification ?',
+	'notifications_quand_explication' => 'Quel(s) changement(s) de statut déclenche(nt) l’envoi d’une notification ?',
 	'notifications_quand_label' => 'Déclenchement',
-	'notifications_vendeur_administrateur_label' => 'Choisir un ou plusieurs administrateurs :',
+	'notifications_vendeur_administrateur_label' => 'Choisir un ou plusieurs administrateurs :',
 	'notifications_vendeur_choix_administrateur' => 'un ou des administrateurs',
 	'notifications_vendeur_choix_email' => 'un ou des emails',
 	'notifications_vendeur_choix_webmaster' => 'un ou des webmestres',
-	'notifications_vendeur_email_explication' => 'Saisir un ou plusieurs email séparés par des virgules :',
-	'notifications_vendeur_email_label' => 'Email(s) du vendeur :',
+	'notifications_vendeur_email_explication' => 'Saisir un ou plusieurs email séparés par des virgules :',
+	'notifications_vendeur_email_label' => 'Email(s) du vendeur :',
 	'notifications_vendeur_explication' => 'Choisir le(s) destinataire(s) des notifications pour les envois au vendeur',
 	'notifications_vendeur_label' => 'Vendeur',
-	'notifications_vendeur_webmaster_label' => 'Choisir un ou plusieurs webmestres :',
+	'notifications_vendeur_webmaster_label' => 'Choisir un ou plusieurs webmestres :',
 
 	// P
 	'parametres_cfg_titre' => 'Paramètres',
@@ -117,9 +117,9 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'passer_la_commande' => 'Passer la commande',
 
 	// R
-	'recapitulatif' => 'Récapitulatif de commande :',
+	'recapitulatif' => 'Récapitulatif de commande :',
 	'reference' => 'Référence',
-	'reference_label' => 'Référence :',
+	'reference_label' => 'Référence :',
 	'reference_ref' => 'Référence @ref@',
 
 	// S
@@ -128,7 +128,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'statut_encours' => 'En cours',
 	'statut_envoye' => 'Envoyée',
 	'statut_erreur' => 'Erreur',
-	'statut_label' => 'Statut :',
+	'statut_label' => 'Statut :',
 	'statut_partiel' => 'Partiellement payée',
 	'statut_paye' => 'Payée',
 	'statut_retour' => 'Retournée',
@@ -140,11 +140,11 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'titre_commandes_a_envoyer' => 'Commandes à envoyer',
 
 	// U
-	'une_commande_de' => 'Une commande de : ',
+	'une_commande_de' => 'Une commande de : ',
 	'une_commande_sur' => 'Une commande sur @nom@',
 
 	// V
-	'votre_commande_sur' => '@nom@ : votre commande'
+	'votre_commande_sur' => '@nom@ : votre commande'
 );
 
 ?>
