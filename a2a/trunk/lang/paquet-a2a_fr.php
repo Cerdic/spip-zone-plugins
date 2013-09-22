62c623eadd68e27a38aae9b323fb16156fc76982
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-22 17:00:06 +0000

    langues (paquet-a2a)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76077 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- a2a/trunk/lang/paquet-a2a_fr.php
+++ a2a/trunk/lang/paquet-a2a_fr.php
@@ -6,9 +6,9 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
-	'a2a_description' => 'Ce plugin permet de lier des articles de façon ponctuelle sans avoir à utiliser les mots-clés de SPIP. Il peut être utilisé pour mettre en place des liens de type « Lire aussi ».
+	'a2a_description' => 'Ce plugin permet de lier des articles de façon ponctuelle sans avoir à utiliser les mots-clés de SPIP. Il peut être utilisé pour mettre en place des liens de type « Lire aussi ».
 	
-	Cette nouvelle version permet en outre de typer les relations entre articles. Cette fonction de typage n\'est pas active par défaut pour conserver le fonctionnement classique du plugin (se référer à la documentation).',
+	Cette nouvelle version permet en outre de typer les relations entre articles. Cette fonction de typage n’est pas active par défaut pour conserver le fonctionnement classique du plugin (se référer à la documentation).',
 	'a2a_slogan' => 'Pour lier des articles'
 );
 
