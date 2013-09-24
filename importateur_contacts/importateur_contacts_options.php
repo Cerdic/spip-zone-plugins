72886d537c17eef22d838dde429d8f92a11d3811
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-24 17:12:39 +0000

    Améliorations du parsing des emails, on gère correctement : -* Nom de la personne <email@domaine.tld> -* email@domaine.tld Nom de la personne -* email@domaine.tld
    Un bouton retour dans le formulaire d'ajout de contacts si on est dans l'étape 2
    
    Une explication dans la partie "liste d'emails"
    
    Version 1.2.2
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76469 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- importateur_contacts/importateur_contacts_options.php
+++ importateur_contacts/importateur_contacts_options.php
@@ -22,60 +22,64 @@ function formulaires_importer_contacts_verifier_importateurcontacts_dist($fourni
 	}
 	else if($email_simple){
 		$email = importateurcontacts_parse_email($email_simple);
-		if (!$email) {
+		if (!$email)
 			$erreurs['email_simple'] = _T('info_email_invalide');
-		}
-		else{
+		else
 			$contacts[] = $email;
-		}
 	}
 	else if($email_liste){
 		$lines = explode("\n", trim($email_liste));
 		$emails = array();
 		foreach($lines as $email){
-			if(strlen($email) > 0){
+			if(strlen(trim($email)) > 0){
 				$email_valide = importateurcontacts_parse_email($email);
 				if(!$email_valide){
 					$erreurs['email_liste'] = _T('info_email_invalide').' "'.$email.'"';
 					break;
-				}else{
+				}else
 					$contacts[] = $email_valide;
-				}
 			}
 		}
 	}
-	if(count($erreurs) == 0){
+	if(count($erreurs) == 0)
 		set_request('contacts',$contacts);
-		spip_log($contacts,'test.'._LOG_ERREUR);
-	}
 	
 	return $erreurs;
 }
 
 /**
  * Parser l'email
- * Gérer deux cas possibles :
- * -* email@domaine.tld Nom de la personne
+ * Gérer trois cas possibles :
  * -* Nom de la personne <email@domaine.tld>
+ * -* email@domaine.tld Nom de la personne
+ * -* email@domaine.tld
  */
 function importateurcontacts_parse_email($email){
 	if(!is_string($email))
 		return false;
 	
-	$email_explode = preg_split("/[\s]+/",$email,2);
-	spip_log($email_explode,'test.'._LOG_ERREUR);
-	
-	if(email_valide($email_explode[0])){
+	// cas email@domaine.tld
+	if(email_valide($email)){
+		$email_explode = preg_split("/[@]+/",$email,2);
+		return array(
+				'email'=> $email,
+				'nom' => $email_explode[0]
+			);
+	}
+	// cas Nom de la personne <email@domaine.tld>
+	else if(preg_match('/(.*) <(.*@.*)>/',$email,$matches) && isset($matches[2]) && email_valide($matches[2])){
+		return array(
+					'email'=> $matches[2],
+					'nom' => $matches[1]
+			);
+	}
+	// Cas email@domaine.tld Nom de la personne
+	else if($email_explode = preg_split("/[\s]+/",$email,2)){
+		spip_log($email_explode,'test.'._LOG_ERREUR);
+		if(isset($email_explode[0]) && email_valide($email_explode[0]))
 		return array(
 				'email'=> $email_explode[0],
-				'nom' => (isset($email_explode[1]) && strlen(trim($email_explode[1])) > 0) ? $email_explode[1] : $email_explode[0]
-			);
-	}else{
-		$email_name = preg_match('/^(?:"?([^@"]+)"?\s)?<?([^>]+@[^>]+)>?$/',$email,$matches);
-		if(isset($matches[1]) && isset($matches[2]))
-		return array(
-				'email'=> $matches[2],
-				'nom' => $matches[1]
+				'nom' => $email_explode[1]
 			);
 	}
 	return false;
