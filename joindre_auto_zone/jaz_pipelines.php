44408c010d9ec0e4ba1806e48b78202fe1c86360
cyril@cym.fr <cyril@cym.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-25 08:38:53 +0000

    ajout des descriptions pour SPIP3; corrections selon suggestions proposées par Sarah : http://contrib.spip.net/Plugin-JAZ#forum463805
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76512 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- joindre_auto_zone/jaz_pipelines.php
+++ joindre_auto_zone/jaz_pipelines.php
@@ -9,48 +9,52 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 include_spip('jaz_fonctions');
 
 /**
- * Détecter la création d'un nouvel auteur (via 'inscription' ou 'editer_auteur') et demander son ajout aux zones
+ * DÃ©tecter la crÃ©ation d'un nouvel auteur (via 'inscription' ou 'editer_auteur') et demander son ajout aux zones
  * @param array $flux
  * @return array
  */
-function jaz_formulaire_traiter($flux){
+function jaz_formulaire_traiter($flux)
+{
 
-    // Si on est sur le formulaire d'inscription d'un nouveau visiteur
-    if ($flux['args']['form']=='inscription'){
-        // on chope le mail pour la requete plus bas...
-        $mail = _request('mail_inscription');
-        if (function_exists('test_inscription'))
-            $f = 'test_inscription';
-        else    $f = 'test_inscription_dist';
+	// Si on est sur le formulaire d'inscription d'un nouveau visiteur
+	if ($flux['args']['form'] == 'inscription') {
+		// on chope le mail pour la requete plus bas...
+		$mail = _request('mail_inscription');
+		$nom  = _request('nom_inscription');
+		if (function_exists('test_inscription'))
+			$f = 'test_inscription';
+		else    $f = 'test_inscription_dist';
 
-        // On teste la validité de l'inscription
-        $desc = $f($mode, $mail, $flux['args']['args'][0], $flux['args']['args'][2]);
+		// On teste la validitÃ© de l'inscription
+		// $desc = $f($mode, $mail, $flux['args']['args'][0], $flux['args']['args'][2]);
+		$desc = $f($mode, $mail, $nom, $flux['args']['args'][2]);
 
-        if (is_array($desc)
-            AND $mail = $desc['email']){
-            include_spip('base/abstract_sql');
+		if (is_array($desc)
+			AND $mail = $desc['email']
+		) {
+			include_spip('base/abstract_sql');
 
-            // On cherche le numéro de l'auteur dont le mail a été saisi
-            $auteur = sql_getfetsel('id_auteur', 'spip_auteurs', 'email="'.$mail.'"');
+			// On cherche le numÃ©ro de l'auteur dont le mail a Ã©tÃ© saisi
+			$auteur = sql_getfetsel('id_auteur', 'spip_auteurs', 'email="' . $mail . '"');
 
-            // On ajoute cet auteur aux zones
-            jaz_ajouter_auteur_zones($auteur);
+			// On ajoute cet auteur aux zones
+			jaz_ajouter_auteur_zones($auteur);
 
-        }
-    }
-    // Si on est sur le formulaire d'édition d'un auteur
-    if ($flux['args']['form']=='editer_auteur'){
-        if (!intval($flux['args']['args'][0])
-            AND intval($flux['data']['id_auteur'])
-        ){
-            $id_auteur = $flux['data']['id_auteur'];
-            include_spip('base/abstract_sql');
+		}
+	}
+	// Si on est sur le formulaire d'Ã©dition d'un auteur
+	if ($flux['args']['form'] == 'editer_auteur') {
+		if (!intval($flux['args']['args'][0])
+			AND intval($flux['data']['id_auteur'])
+		) {
+			$id_auteur = $flux['data']['id_auteur'];
+			include_spip('base/abstract_sql');
 
-            // On ajoute cet auteur aux zones
-            jaz_ajouter_auteur_zones($auteur);
-        }
-    }
-    return $flux;
+			// On ajoute cet auteur aux zones
+			jaz_ajouter_auteur_zones($auteur);
+		}
+	}
+	return $flux;
 }
 
 
