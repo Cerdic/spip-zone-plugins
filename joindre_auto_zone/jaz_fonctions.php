8639d66f0ff35a21780f02578d70fcf2c01837e4
cyril@cym.fr <cyril@cym.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-25 16:40:53 +0000

    retouches fichiers langue;
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76518 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- joindre_auto_zone/jaz_fonctions.php
+++ joindre_auto_zone/jaz_fonctions.php
@@ -9,22 +9,23 @@
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 /**
- * Chercher les zones définies dans mes_options et ajouter l'auteur à ces zones
+ * Chercher les zones dÃ©finies dans mes_options et ajouter l'auteur Ã  ces zones
  * @param $auteur
-  */
-function jaz_ajouter_auteur_zones($auteur) {
+ */
+function jaz_ajouter_auteur_zones($auteur)
+{
 
-     // On cherche les Zones Auto Jointes :
-    $define = (defined('_ZONES_AUTO_JOINTES')) ? _ZONES_AUTO_JOINTES : '';
-    $zones = explode(':', $define);
+	// On cherche les Zones Auto Jointes :
+	$define = (defined('_ZONES_AUTO_JOINTES')) ? _ZONES_AUTO_JOINTES : '';
+	$zones  = explode(':', $define);
 
-    // Pour chacune de ces zones
-    while(list(,$l) = @each($zones)) {
+	// Pour chacune de ces zones
+	while (list(, $l) = @each($zones)) {
 
-        // On ajoute l'auteur à la zone
-        sql_insertq("spip_zones_auteurs", array("id_zone"=>$l,"id_auteur"=>$auteur));
-        spip_log('Auteur '.$auteur.' ajouté à la zone '.$l, jaz);
-    }
+		// On ajoute l'auteur Ã  la zone
+		sql_insertq("spip_zones_auteurs", array("id_zone" => $l, "id_auteur" => $auteur));
+		spip_log('Auteur ' . $auteur . ' ajoutÃ© Ã  la zone ' . $l, jaz);
+	}
 
 }
 
