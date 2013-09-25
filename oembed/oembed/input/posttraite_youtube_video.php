d8e7f0d5ace267d659e47c5b66597bfac5122f34
bmarne@gmail.com <bmarne@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-25 10:00:53 +0000

    On ajoute la prise en compte du paramètre &rel=0 dans les URL Youtube: quand le paramètre est dans l'URL, alors la vidéo "embédée" n'affichera pas les vidéos liées à la fin de la lecture.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76513 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- oembed/oembed/input/posttraite_youtube_video.php
+++ oembed/oembed/input/posttraite_youtube_video.php
@@ -7,13 +7,17 @@
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
-function oembed_input_posttraite_youtube_video_dist($data){
+function oembed_input_posttraite_youtube_video_dist($data,$url_orig){
 
 	$html = $data['html'];
 	if ($e = extraire_balise($html,'embed')
 	  AND $src = extraire_attribut($e,'src')){
 		$data['url'] = $src;
 	}
+	// On regarde si l'attribut rel est placé dans l'URL
+	// pour éviter l'affichage des vidéo liées à la fin de la vidéo
+	if (strpos($url_orig,"&rel=0"))
+		$data['html'] = str_replace('feature=oembed', 'feature=oembed&#038;rel=0', $data['html']);
 	
 	return $data;
 }
