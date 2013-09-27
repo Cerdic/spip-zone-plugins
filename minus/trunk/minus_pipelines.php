7423da061cd94278e0b4cc3c0350f40dbda53737
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-27 12:47:12 +0000

    rendre générique à tout type d'objet
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76555 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- minus/trunk/minus_pipelines.php
+++ minus/trunk/minus_pipelines.php
@@ -1,7 +1,8 @@
 <?
 if (!defined('_ECRIRE_INC_VERSION')) return;
 function minus_formulaire_charger($flux){
-	if ($flux["args"]["form"] == "editer_article" or $flux["args"]["form"]=="editer_evenement"){
+	
+    if (substr($flux["args"]["form"],0,7) == "editer_"){
 		if (trop_majuscules()){ // si on a trop de majuscule, on le marque avec un hidden
 		    $flux["data"]["_hidden"].="<input type='hidden' name='titre_trop_majusucules' value='oui' />\n";
 		}
@@ -9,7 +10,7 @@ function minus_formulaire_charger($flux){
     return $flux;
 }
 function minus_formulaire_verifier($flux){
-	if (($flux["args"]["form"] == "editer_article" and !(_request("id_article") > 0)) or ($flux["args"]["form"]=="editer_evenement" and !(_request("id_evenement") > 0))){
+	if (substr($flux["args"]["form"],0,7) == "editer_" and !(_request("id_".substr($flux["args"]["form"],7))>0)){
 		if (trop_majuscules() and !_request("titre_trop_majusucules")){ // si ion a trop de majuscules, on l'affiche, sauf si on l'a déjà affiché une fois.
 		    $flux["data"]["titre"] = _T("minus:trop_majuscule");
 		    }
