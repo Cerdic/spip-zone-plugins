231254bde8948ee27967273932ba8816dc946665
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-27 07:50:06 +0000

    typo
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76546 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- diogene/diogene_complements/diogene_licence/trunk/lang/paquet-diogene_licence_fr.php
+++ diogene/diogene_complements/diogene_licence/trunk/lang/paquet-diogene_licence_fr.php
@@ -6,9 +6,9 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// D
-	'diogene_licence_description' => 'Complément "licence" pour "Diogene" (utilise le plugin Licences)',
-	'diogene_licence_nom' => 'Diogene - Licence',
-	'diogene_licence_slogan' => 'Complément "licence" pour "Diogene" (utilise le plugin Licences)'
+	'diogene_licence_description' => 'Ajoute la possibilité de sélectionner la licence d\'un objet depuis son formulaire d\'édition si cette option est activée dans le masque de formulaire de Diogène',
+	'diogene_licence_nom' => 'Diogène - Licence',
+	'diogene_licence_slogan' => 'Complément "licence" pour "Diogène"'
 );
 
 ?>
