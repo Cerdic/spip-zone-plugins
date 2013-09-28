a4c422c4ca91f9c1c6173c70479979c068039f2d
brunobergot@gmail.com <brunobergot@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-28 11:45:47 +0000

    Version 1.0 : compat SPIP 2.1 et 3.0 + utilise spip bonux pour la config en 2.1
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76566 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- jquery_lazyload/trunk/lang/lazyload_fr.php
+++ jquery_lazyload/trunk/lang/lazyload_fr.php
@@ -1,22 +1,21 @@
 <?php
-// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-if (!defined("_ECRIRE_INC_VERSION")) return;
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 	
 	//C
-	'cfg_boite_lazyload' => 'Configuration du plugin jQuery Lazy Load',
-	'cfg_descr_lazyload' => 'Lazy Load : Plugin jQuery Lazy Load pour SPIP',
-	'cfg_inf_selecteur' => 'S&eacute;lecteur des images sur lesquelles Lazy Load sera appliqué (expression CSS ou &eacute;tendue jQuery).',
-	'cfg_inf_distance' => 'Distance du bord de la fen&ecirc;tre &agrave; laquelle les images seront charg&eacute;es.',
-	'cfg_inf_event' => '&Eacute;v&eacute;nement pour d&eacute;clencher l\'affichage des images (click, mouseover, etc).',
+	'cfg_inf_selecteur' => 'Sélecteur des images sur lesquelles Lazy Load sera appliqué (expression CSS ou étendue jQuery).',
+	'cfg_inf_distance' => 'Distance du bord de la fenêtre à laquelle les images seront chargées.',
+	'cfg_inf_event' => 'Événement pour déclencher l\'affichage des images (click, mouseover, etc).',
 	'cfg_inf_effect' => 'Effet de transition lors du chargement des images (fadeIn, slideDown, etc).',
 	
-	'cfg_lbl_selecteur' => 'S&eacute;lecteur',
+	'cfg_lbl_selecteur' => 'Sélecteur',
 	'cfg_lbl_distance' => 'Distance',
-	'cfg_lbl_event' => '&Eacute;v&eacute;nement',
+	'cfg_lbl_event' => 'Événement',
 	'cfg_lbl_effect' => 'Effet',
 	'cfg_titre_lazyload' => 'Lazy Load'
 	
 );
+
 ?>
