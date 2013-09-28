99fa8f2e24675451426af4d564fdb9baed3a0058
p@henix.be <p@henix.be@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-28 22:07:55 +0000

    Utiliser les class de foundation pour rendre les iframes responsives et détecter le cas de Vimeo.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76583 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- foundation-4-spip/trunk/foundation_fonctions.php
+++ foundation-4-spip/trunk/foundation_fonctions.php
@@ -11,8 +11,20 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 
 
 /*
-*   Rendre les iframes responsive via un filtre.
+*   Rendre les iframes responsive via un filtre et la classe flex-video de Foundation.
 */
 function iframe_responsive($texte) {
-    return preg_replace('/<iframe(.+)><\/iframe>/', '<div class="iframe_container"><iframe $1></iframe></div>', $texte);
+
+    // Function de callback
+    function responsive($matches) {
+        // Dans le cas de vimeo, il faut ajouter une classe
+        if (strpos($matches[0], 'vimeo')) $vimeo = ' vimeo';
+        else $vimeo = '';
+
+        // On revoie la bonne structure html d'iframe.
+        return '<div class="flex-video'.$vimeo.'"><iframe '.$matches[0].'></iframe></div>';
+    }
+    
+    // On détecte tout les iFrames et on les rends responsives.
+    return preg_replace_callback('/<iframe(.+)><\/iframe>/', 'responsive', $texte);
 }
