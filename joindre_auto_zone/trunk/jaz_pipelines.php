256a83a3fcf4bd3c1352cad5e671a5b313520e4c
marcimat@rezo.net <marcimat@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-28 14:24:00 +0000

    - formulaire de configuration des zones d'accès à affecter automatiquement (à défaut du define renseigné) - lors de l'affectation, utiliser la fonction zone_lier() prévue par accès restreint.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76573 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- joindre_auto_zone/trunk/jaz_pipelines.php
+++ joindre_auto_zone/trunk/jaz_pipelines.php
@@ -6,10 +6,13 @@
  */
 
 if (!defined("_ECRIRE_INC_VERSION")) return;
-include_spip('jaz_fonctions');
+
 
 /**
- * Détecter la création d'un nouvel auteur (via 'inscription' ou 'editer_auteur') et demander son ajout aux zones
+ * Détecter la création d'un nouvel auteur et demander son ajout aux zones
+ *
+ * On détecte via 'inscription' ou 'editer_auteur'
+ * 
  * @param array $flux
  * @return array
  */
@@ -57,5 +60,36 @@ function jaz_formulaire_traiter($flux)
 	return $flux;
 }
 
+
+
+/**
+ * Chercher les zones automatiques à joindre et ajouter l'auteur à ces zones
+ *
+ * Les zones peuvent être indiquées soit :
+ * - par la constante `_ZONES_AUTO_JOINTES` (dans `mes_options.php` par exemple)
+ *   tel que `define('_ZONES_AUTO_JOINTES', '2:4')` où 2 et 4 sont des identifiants de zone
+ * - soit, à défaut de constante, par les zones indiquées dans le formulaire de configuration
+ *   du plugin (meta `jaz/zones_automatiques`).
+ * 
+ * @param int $id_auteur Identifiant d'auteur
+ */
+function jaz_ajouter_auteur_zones($id_auteur)
+{
+
+	// On cherche les Zones Auto Jointes :
+	if (defined('_ZONES_AUTO_JOINTES')) {
+		$zones = explode(':', _ZONES_AUTO_JOINTES);
+	} else {
+		include_spip('inc/config');
+		$zones = explode(',', lire_config('jaz/zones_automatiques'));
+	}
+
+	if ($zones) {
+		include_spip('action/editer_zone');
+		zone_lier($zones, 'auteur', $id_auteur);
+		spip_log('Auteur ' . $id_auteur . ' ajouté aux zones : ' . implode(', ', $zones), 'jaz');
+	}
+
+}
 
 ?>
--- joindre_auto_zone/trunk/lang/jaz_fr.php
+++ joindre_auto_zone/trunk/lang/jaz_fr.php
@@ -2,6 +2,11 @@
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+	'titre_page_configurer_jaz' => 'Configurer « Joindre Automatiquement une Zone »',
 
+	'cfg_titre_parametrages' => 'Paramétrages',
+	'cfg_zones_automatiques_label' => 'Zones restreintes à rejoindre',
+	'cfg_zones_automatiques_explication' => 'Les zones d\'accès restreintes sélectionnées
+		seront automatiquement affectées à un nouvel auteur créé ou inscrit sur le site',
 );
 ?>
