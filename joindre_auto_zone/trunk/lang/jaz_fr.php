b1e46c4e2ccd593e5b9e7412fb8be3efee25e55d
marcimat@rezo.net <marcimat@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-28 14:34:59 +0000

    Message d'avertissement dans la configuration si la constante est définie. + j'ai oublié de dire sur r76573 qu'on déplace la fonction dans jaz_fonctions dans le fichier de pipeline, puisqu'elle ne sert qu'ici ; ça évitera la lecture de 2 fichiers inutilement.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76574 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- joindre_auto_zone/trunk/lang/jaz_fr.php
+++ joindre_auto_zone/trunk/lang/jaz_fr.php
@@ -8,5 +8,9 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'cfg_zones_automatiques_label' => 'Zones restreintes à rejoindre',
 	'cfg_zones_automatiques_explication' => 'Les zones d\'accès restreintes sélectionnées
 		seront automatiquement affectées à un nouvel auteur créé ou inscrit sur le site',
+
+	'cfg_define_zones_automatiques' => 'La constante <code>_ZONES_AUTO_JOINTES</code> est
+		définie (certainement dans le fichier <code>config/mes_options.php</code>).
+		Cette constante est prioritaire sur cette configuration qui ne sera donc pas effective.',
 );
 ?>
