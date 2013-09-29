a7439541955a8d34ad8728eb01241326a024e9cd
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-09-29 21:23:35 +0000

    langues (saveauto)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76680 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- saveauto/trunk/lang/saveauto_ca.php
+++ saveauto/trunk/lang/saveauto_ca.php
@@ -11,41 +11,41 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'colonne_nom' => 'Cognom',
 
 	// E
-	'erreur_impossible_creer_verifier' => 'Impossible crear el fitxer @fichier@, verifiqueu els drets d\'escriptura del directori @rep_bases@.',
+	'erreur_impossible_creer_verifier' => 'Impossible crear el fitxer @fichier@, verifiqueu els drets d’escriptura del directori @rep_bases@.',
 	'erreur_impossible_liste_tables' => 'Impossible llistar les taules de la base de dades.',
 	'erreur_probleme_donnees_corruption' => 'Problema amb les dades de @table@, és possible que estiguin corruptes!',
-	'erreur_repertoire_inaccessible' => 'El directori @rep@ és inaccessible a l\'escriptura.',
+	'erreur_repertoire_inaccessible' => 'El directori @rep@ és inaccessible a l’escriptura.',
 
 	// H
 	'help_mail_max_size' => 'Algunes bases de dades poden sobrepassar la mida màxima permesa en els fitxers adjunts per un correu electrònic. Consulteu al vostre proveïdor de correu electrònic per tal de saber la mida màxima autoritzada. El límit predeterminat és de 2MB.', # MODIF
-	'help_obsolete' => 'Determina a partir de quants dies un arxiu es considera obsolet i automàticament l\'elimina del servidor.
+	'help_obsolete' => 'Determina a partir de quants dies un arxiu es considera obsolet i automàticament l’elimina del servidor.
              Poseu-hi -1 per desactivar aquesta funció', # MODIF
 	'help_prefixe' => 'Opcional: posar un prefix al nom del fitxer de la còpia de seguretat', # MODIF
-	'help_restauration' => '<strong>Advertència!!!</strong> Les còpies de seguretat realitzades no estan <strong>en el format de les d\'SPIP</strong>:
-                Inútil provar utilitzar-les amb l\'eina d\'administració d\'Spip.<br /><br />
+	'help_restauration' => '<strong>Advertència!!!</strong> Les còpies de seguretat realitzades no estan <strong>en el format de les d’SPIP</strong>:
+                Inútil provar utilitzar-les amb l’eina d’administració d’Spip.<br /><br />
              Per qualsevol restauració cal utilitzar la interfície <strong>phpmyadmin</strong> del vostre
              servidor de base de dades: a la pestanya <strong>"SQL"</strong> utilitzeu el botó
              <strong>"Ubicació del fitxer de text"</strong> per seleccionar el fitxer de la còpia de seguretat
              (marcar la opció "gzip" si és necessari) i valideu tot seguit.<br /><br />
              Les còpies de seguretat <strong>xxxx.gz</strong> o <strong>xxx.sql</strong> contenen un fitxer en format SQL amb les comandes
-             que permeten <strong>esborrar</strong> les taules existents d\'SPIP i <strong>substituir-les</strong> per les
+             que permeten <strong>esborrar</strong> les taules existents d’SPIP i <strong>substituir-les</strong> per les
              dades arxivades. Per tant, les dades <strong>més recents</strong> que les de la còpia de seguretat es <strong>PERDRAN</strong>!', # MODIF
 
 	// I
 	'info_sql_base' => 'Base de dades: ',
 	'info_sql_compatible_phpmyadmin' => 'Fitxer SQL 100% compatible PHPMyadmin',
 	'info_sql_date' => 'Data: ',
-	'info_sql_debut_fichier' => 'Inici de l\'arxiu',
+	'info_sql_debut_fichier' => 'Inici de l’arxiu',
 	'info_sql_donnees_table' => 'Dades de @table@', # MODIF
 	'info_sql_fichier_genere' => 'Aquest fitxer està generat pel connector "saveauto"', # MODIF
-	'info_sql_fin_fichier' => 'Final de l\'arxiu',
+	'info_sql_fin_fichier' => 'Final de l’arxiu',
 	'info_sql_ipclient' => 'IP Client: ',
 	'info_sql_mysqlversion' => 'Versió mySQL : ', # MODIF
 	'info_sql_os' => 'Sistema Operatiu del Servidor: ',
 	'info_sql_phpversion' => 'Versió PHP: ',
 	'info_sql_plugins_utilises' => '@nb@ connectors utilitzats:',
 	'info_sql_serveur' => 'Servidor: ',
-	'info_sql_spip_version' => 'Versió d\'SPIP: ',
+	'info_sql_spip_version' => 'Versió d’SPIP: ',
 	'info_sql_structure_table' => 'Estructura de la taula @table@',
 
 	// L
