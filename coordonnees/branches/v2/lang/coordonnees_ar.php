20c5f6d4ee26e069ecdb388bad6eeada6187b464
superyms@yahoo.fr <superyms@yahoo.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-10-03 21:13:21 +0000

    On continue à nettoyer les fichiers
    La langue ar devrait être supprimée car trop peu traduite (33.33 %)
    
    
    langues (coordonnees)
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76904 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- coordonnees/branches/v2/lang/coordonnees_ar.php
+++ coordonnees/branches/v2/lang/coordonnees_ar.php
@@ -14,11 +14,8 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'ajouter_adresse' => 'إضافة عنوان',
 	'ajouter_email' => 'إضافة بريد إلكتروني',
 	'ajouter_telephone' => 'إضافة رقم',
-	'articles' => 'Articles', # NEW
-	'auteurs' => 'Auteurs', # NEW
 
 	// C
-	'configuration_coordonnees' => 'Configuration des coordonnées', # NEW
 	'confirmer_suppression_adresse' => 'هل تريد حقاحذف هذا العنوان؟',
 	'confirmer_suppression_email' => 'هل تريد حقاحذف هذا البريد الإلكتروني؟',
 	'confirmer_suppression_numero' => 'هل تريد حقا حذف هذا الرقم؟',
@@ -28,21 +25,11 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'editer_email' => 'تعديل بريد إلكتروني',
 	'editer_numero' => 'تعديل رقم',
 	'emails' => 'البريد الإلكتروني',
-	'explication_objets_actifs' => 'Sur quels objets éditoriaux proposer les coordonnées ?', # NEW
-	'explication_type_adresse' => 'Le type peut être \'perso\' ou \'pro\'.', # NEW
-	'explication_type_email' => 'Le type peut être \'perso\' ou \'pro\'.', # NEW
-	'explication_type_numero' => 'Le type peut être \'perso\' ou \'pro\' ou \'fax\'.', # NEW
-
-	// F
-	'fax' => 'الفاكس.',
 
 	// I
-	'info_adresse_utilisee_par' => 'Adresse utilisée par :', # NEW
-	'info_email_utilise_par' => 'Courriel utilisé par :', # NEW
 	'info_gauche_numero_adresse' => 'عدد العنوان',
 	'info_gauche_numero_email' => 'عدد البريد الإلكتروني',
 	'info_gauche_numero_numero' => 'عدد الرقم',
-	'info_numero_utilise_par' => 'Numéro utilisé par :', # NEW
 	'item_nouveau_numero' => 'رقم جديد',
 	'item_nouvel_email' => 'بريد إلكتروني جديد',
 	'item_nouvelle_adresse' => 'عنوان جديد',
@@ -53,38 +40,9 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'label_complement' => 'Complément', # MODIF
 	'label_email' => 'بريد إلكتروني',
 	'label_numero' => 'رقم',
-	'label_objets_actifs' => 'Objets', # NEW
 	'label_pays' => 'البلد',
-	'label_telephone' => 'Téléphone', # NEW
-	'label_titre' => 'Titre', # NEW
-	'label_type_adresse' => 'Type d\'adresse', # NEW
-	'label_type_email' => 'Type de courriel', # NEW
-	'label_type_numero' => 'Type de numéro', # NEW
 	'label_ville' => 'المدينة',
-	'label_voie' => 'العنوان',
-
-	// M
-	'mobile' => 'النقال',
-	'modifier_adresse' => 'Modifier cette adresse', # NEW
-	'modifier_email' => 'Modifier ce courriel', # NEW
-	'modifier_numero' => 'Modifier ce numéro', # NEW
-
-	// N
-	'nouveau_numero' => 'Nouveau numéro', # NEW
-	'nouvel_email' => 'Nouveau courriel', # NEW
-	'nouvelle_adresse' => 'Nouvelle adresse', # NEW
-	'numeros' => 'Numéros', # NEW
-
-	// R
-	'rubriques' => 'Rubriques', # NEW
-
-	// S
-	'supprimer_adresse' => 'Supprimer cette adresse', # NEW
-	'supprimer_email' => 'Supprimer ce courriel', # NEW
-	'supprimer_numero' => 'Supprimer ce numéro', # NEW
-
-	// T
-	'titre_coordonnees' => 'Coordonnées' # NEW
+	'label_voie' => 'العنوان' # MODIF
 );
 
 ?>
