3099f3493833188547df1559c02c32aaa75fb6b5
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-10-03 21:13:25 +0000

    On continue à nettoyer les fichiers
    La langue km devrait être supprimée car trop peu traduite (18.18 %)
    La langue sv devrait être supprimée car trop peu traduite (9.09 %)
    
    
    langues (flickrcc)
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76908 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- flickr_cc/lang/flickrcc_sv.php
+++ flickr_cc/lang/flickrcc_sv.php
@@ -1,37 +1,14 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// extrait automatiquement de http://www.spip.net/trad-lang/
+// extrait automatiquement de http://trad.spip.net/tradlang_module/flickrcc?lang_cible=sv
 // ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-	// A
-	'ajouter_image_flickr' => 'Ajouter une image Flickr', # NEW
-
-	// C
-	'cc_commerciale' => 'CC - Utilisation commerciale possible', # NEW
-	'cc_modification' => 'CC - Modification possible', # NEW
-	'cc_pas_de_restriction' => 'Pas de restrictions connues', # NEW
-	'cc_paternite' => 'Creative Commons - Paternité', # NEW
-
 	// F
-	'fermer' => 'Stäng',
-	'flickr_cc_explication' => '<p>Recherchez des images en Creative Commons sur Flickr en indiquant les termes de la recherche ci-dessus. Précisez éventuellement les natures de licences auxquelles sont soumises les images.</p>
-<p>Cliquez sur la (ou les) images(s) choisie(s) parmi les résultats de la recherche : elle est immédiatement associée à votre article. Cliquez sur le nom de l\'auteur pour accéder à la page Flickr de cette image.</p>
-<p>Ces images sont mises à disposition par leurs auteurs respectifs : pour respecter leurs droits et leur travail,</p>
-<ul><li>ne supprimez pas la mention de l\'auteur et le lien vers leur page Flickr ;</li>
-<li>respectez rigoureusement la licence Creative Commons associée à chaque image (en particulier, n\'utilisez pas une image dans un cadre commercial si la licence de l\'autorise pas ;</li>
-<li>si l\'auteur de l\'image vous le demande, vous devez retirer l\'image de votre site dans un délai de 24 heures.</li>
-</ul>
-<p>Ce produit utilise l’API Flickr mais n’est ni soutenu, ni certifié par Flickr.</p>', # NEW
-
-	// R
-	'rechercher_flickr' => 'Rechercher sur Flickr', # NEW
-	'resultat_par_date' => 'par date', # NEW
-	'resultat_par_interet' => 'par intérêt', # NEW
-	'resultat_par_pertinence' => 'par pertinence' # NEW
+	'fermer' => 'Stäng'
 );
 
 ?>
