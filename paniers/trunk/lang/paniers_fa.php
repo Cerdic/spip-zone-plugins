48fe62ee00b9861913bd01ac8c42aa361422b6ce
davoodhossein@yahoo.com <davoodhossein@yahoo.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-10-03 21:13:38 +0000

    On continue à nettoyer les fichiers
    langues (paniers)
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76919 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- paniers/trunk/lang/paniers_fa.php
+++ paniers/trunk/lang/paniers_fa.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// extrait automatiquement de http://trad.spip.org
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paniers?lang_cible=fa
 // ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
@@ -10,22 +10,10 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// A
 	'action_ajouter' => 'افزودن به سبد',
 
-	// C
-	'configurer_titre' => 'Configurer le plugin Paniers', # NEW
-
-	// L
-	'limite_enregistres_label' => 'Visiteurs enregistrés', # NEW
-	'limite_ephemere_label' => 'Visiteurs non enregistrés', # NEW
-	'limite_explication' => 'Saisir la durée de vie (en heures) des paniers selon le type de visiteurs', # NEW
-	'limite_titre' => 'Durée de vie des paniers', # NEW
-
 	// M
 	'mon_panier' => 'سبد من',
 	'mon_panier_modifier' => 'تعديل سبد من',
 	'mon_panier_voir' => 'سبدم را ببين',
-
-	// N
-	'nom_bouton_plugin' => 'Paniers', # NEW
 
 	// P
 	'panier_description' => 'شرح',
