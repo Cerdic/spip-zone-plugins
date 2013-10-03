9215dddccaf81d444b958f20575aae39aabdcea3
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-10-03 21:13:24 +0000

    On continue à nettoyer les fichiers
    La langue it devrait être supprimée car trop peu traduite (33.33 %)
    
    
    langues (paquet-entravaux)
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76907 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- en_travaux/trunk/lang/paquet-entravaux_it.php
+++ en_travaux/trunk/lang/paquet-entravaux_it.php
@@ -1,18 +1,14 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-entravaux
-// Langue: it
-// Date: 06-01-2012 11:21:19
-// Items: 1
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-entravaux?lang_cible=it
+// ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// E
-	'entravaux_description' => 'Consente di visualizzare un messaggio personalizzabile durante una sessione di manutenzione su tutte le pagine del sito pubblico.',
+	// E
+	'entravaux_description' => 'Consente di visualizzare un messaggio personalizzabile durante una sessione di manutenzione su tutte le pagine del sito pubblico.'
 );
+
 ?>
