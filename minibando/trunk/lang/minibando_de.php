1f10697c95d88ee4fd5d62068ac75cd2a2f64007
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-10-03 21:13:33 +0000

    On continue à nettoyer les fichiers
    La langue de devrait être supprimée car trop peu traduite (33.33 %)
    
    
    langues (minibando)
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76916 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- minibando/trunk/lang/minibando_de.php
+++ minibando/trunk/lang/minibando_de.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// extrait automatiquement de http://trad.spip.org
+// extrait automatiquement de http://trad.spip.net/tradlang_module/minibando?lang_cible=de
 // ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
@@ -9,12 +9,9 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// M
 	'mode_debug' => 'Režim ladenia',
-	'mode_inclure' => 'Mode inclure', # NEW
-	'mode_profile' => 'Mode profile', # NEW
 
 	// T
-	'titre_debug' => 'Debug', # NEW
-	'titre_outils_rapides' => 'Création' # NEW
+	'titre_outils_rapides' => 'Création' # MODIF
 );
 
 ?>
