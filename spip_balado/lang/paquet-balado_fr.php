5bd9f4c5c002b2199610963407ffbf71ef35b71f
amaury.adon@laposte.net <amaury.adon@laposte.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-10-04 11:00:00 +0000

    Passage en spip 3 avec plugonet changement d'un entite_unicode en charset2unicode Ca passe avec les premiers tests mais je ne les ai pas beaucoup approfondi. Ca en fera un de moins dans la liste de Ybbet ;-)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76943 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ spip_balado/lang/paquet-balado_fr.php
@@ -0,0 +1,19 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-balado
+// Langue: fr
+// Date: 04-10-2013 08:42:26
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// B
+	'balado_description' => 'Ce plugin vise essentiellement à rajouter simplement le support du podcast dans les squelettes pour SPIP. Il utilise la syntaxe spécifique à iTunes, mais fonctionne très bien avec Rhythmbox sous GNU/Linux Ubuntu',
+	'balado_slogan' => 'Générer un flux RSS spécifique à iTunes',
+);
+?>
