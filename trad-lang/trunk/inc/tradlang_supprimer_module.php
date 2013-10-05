2251a47a70c0ab856c077c5663792c472544a032
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-10-05 11:15:14 +0000

    Amélioration de la fonction de suppression de module
    On supprime tout, pas simplement les objets correspondants
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76980 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- trad-lang/trunk/inc/tradlang_supprimer_module.php
+++ trad-lang/trunk/inc/tradlang_supprimer_module.php
@@ -2,20 +2,48 @@
 
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
+/**
+ * Fonction de suppression de module
+ * 
+ * @param int $id_tradlang_module
+ * 	Identifiant numérique du module à supprimer
+ * @return bool
+ */
 function inc_tradlang_supprimer_module($id_tradlang_module){
-	if(intval($id_tradlang_module)){
-		$module = sql_getfetsel('module','spip_tradlang_modules','id_tradlang_module='.intval($id_tradlang_module));
+	if(intval($id_tradlang_module) > 0){
+		/**
+		 * Le module existe-t-il ?
+		 */
+		$module = sql_getfetsel('id_tradlang_module','spip_tradlang_modules','id_tradlang_module='.intval($id_tradlang_module));
 		if($module){
-			$traductions = sql_delete('spip_tradlangs','module='.sql_quote($module));
-			sql_delete('spip_tradlang_modules','module='.sql_quote($module));
-			sql_delete('spip_tradlangs_bilans','module='.sql_quote($module));
-			return $traductions;
-		}else{
+			/**
+			 * Suppression des versions des chaines de langue
+			 */
+			$tradlangs = sql_allfetsel('id_tradlang','spip_tradlangs','id_tradlang_module='.intval($module));
+			$tradlangs_supprimer = array();
+			foreach($tradlangs as $tradlang){
+				$tradlangs_supprimer[] = $tradlang['id_tradlang'];
+			}
+			if(count($tradlangs_supprimer)){
+				sql_delete('spip_versions','objet="tradlang" AND '.sql_in('id_objet',$tradlangs_supprimer));
+				sql_delete('spip_versions_fragments','objet="tradlang" AND '.sql_in('id_objet',$tradlangs_supprimer));
+			}
+			/**
+			 * Suppression des chaines de langue, du module et des bilans
+			 */
+			sql_delete('spip_tradlangs','id_tradlang_module='.intval($module));
+			sql_delete('spip_tradlang_modules','id_tradlang_module='.intval($module));
+			sql_delete('spip_tradlangs_bilans','id_tradlang_module='.intval($module));
+			/**
+			 * Suppression des versions du module
+			 */
+			sql_delete('spip_versions_fragments','objet="tradlang_module" AND id_objet='.intval($module));
+			sql_delete('spip_versions','objet="tradlang_module" AND id_objet='.intval($module));
+			return true;
+		}else
 			return false;
-		}
-	}else{
+	}else
 		return false;
-	}
 	
 }
 ?>
