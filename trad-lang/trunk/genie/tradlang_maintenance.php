ea84f143fda3d8fb519188a3a26d1057a42debca
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-10-05 10:47:21 +0000

    CRON de maintenance de tradlang : - On utilise sql_allfetsel + foreach pour récupérer les langues les plus utilisées (plus rapide que sql_select + while + sql_fetch) - On ajoute la suppression des versions et fragments de chaines de langue disparues au cas où il y aurait eu un problème ou une intervention à même la base de donnée
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76975 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- trad-lang/trunk/genie/tradlang_maintenance.php
+++ trad-lang/trunk/genie/tradlang_maintenance.php
@@ -3,7 +3,7 @@
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 /**
- * Génie des opérations de maintenance de tradlang
+ * Génie des opérations de maintenance journalières de tradlang
  */
 function genie_tradlang_maintenance_dist($t) {
 	
@@ -15,11 +15,24 @@ function genie_tradlang_maintenance_dist($t) {
 		include_spip('inc/config');
 	$nb_langues = lire_config('tradlang/limiter_langues_bilan_nb','10');
 	$langues_utilisees = array();
-	$langues = sql_select('lang','spip_tradlangs','statut="OK"',"lang","COUNT(*) DESC","0,$nb_langues");
-	while($langue = sql_fetch($langues)){
+	$langues = sql_allfetsel('lang','spip_tradlangs','statut="OK"',"lang","COUNT(*) DESC","0,$nb_langues");
+	foreach($langues as $langue){
 		$langues_utilisees[] = $langue['lang'];
 	}
 	ecrire_meta('tradlang_langues_max',serialize($langues_utilisees));
+	
+	/**
+	 * Suppression des versions des tradlangs disparus
+	 */
+	$tradlang_disparus = sql_allfetsel('versions.id_objet','spip_versions AS versions','versions.objet="tradlang" AND NOT EXISTS(SELECT * FROM spip_tradlangs AS tradlangs WHERE versions.id_objet = tradlangs.id_tradlang)','versions.id_objet');
+	$disparus = array();
+	foreach($tradlang_disparus as $tradlang){
+		$disparus[] = $tradlang['id_objet'];
+	}
+	if(count($disparus) && count($disparus) > 0){
+		sql_delete('spip_versions','objet="tradlang" AND '.sql_in('id_objet',$disparus));
+		sql_delete('spip_versions_fragments','objet="tradlang" AND '.sql_in('id_objet',$disparus));
+	}
 	return 0;
 }
 ?>
