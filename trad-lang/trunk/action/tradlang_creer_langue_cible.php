875b45c97bd5fcd4c20d6f6ec4fef30416131547
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-10-05 11:15:59 +0000

    Petites améliorations de la création de langue de module
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76981 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- trad-lang/trunk/action/tradlang_creer_langue_cible.php
+++ trad-lang/trunk/action/tradlang_creer_langue_cible.php
@@ -1,36 +1,34 @@
 <?php
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
 /**
  * Action permettant de creer une langue cible depuis la langue mère d'un module
  * 
  * @return 
  */
-
-if (!defined("_ECRIRE_INC_VERSION")) return;
-
 function action_tradlang_creer_langue_cible_dist(){
 	$securiser_action = charger_fonction('securiser_action', 'inc');
 	$arg = $securiser_action();
-	if (!preg_match(",^(\w+)$,", $arg, $r))
-		spip_log("action_tradlang_creer_langue_cible $arg pas compris");
+	if (!preg_match(",^(\w+)$,", $arg, $r)){
+		spip_log("action_tradlang_creer_langue_cible $arg pas compris",'tradlang.'._LOG_ERREUR);
+		return false;
+	}
 
 	$id_tradlang_module = intval($arg);
 
 	include_spip('inc/autoriser');
 	$lang_crea = _request('lang_crea');
-	if($lang_crea && $id_tradlang_module && autoriser('modifier','tradlang')){
+	if($lang_crea && intval($arg) && autoriser('modifier','tradlang')){
 		// Import de la langue mere
-		$infos_module = sql_fetsel('*','spip_tradlang_modules','id_tradlang_module='.intval($id_tradlang_module));
+		$infos_module = sql_fetsel('*','spip_tradlang_modules','id_tradlang_module='.intval($arg));
 		if(!$infos_module)
-			spip_log('tradlang_creer_langue_cible : infos_module non existant','tradlang');
+			spip_log('tradlang_creer_langue_cible : infos_module non existant','tradlang.'._LOG_ERREUR);
 		else{
 			$ajouter_code_langue = charger_fonction('tradlang_ajouter_code_langue','inc');
 			$ajouter_code_langue($infos_module,$lang_crea);
 		}
-		/**
-		 * Le cache est invalidé dans $ajouter_code_langue
-		 */
 	}else
-		spip_log("action_tradlang_creer_langue_cible : Module $module_nom inexistant","tradlang");
+		spip_log("action_tradlang_creer_langue_cible : Module $module_nom inexistant",'tradlang.'._LOG_ERREUR);
 
 	$redirect = _request('redirect');
 	if($redirect){
