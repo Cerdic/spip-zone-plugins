ce90e7106bce268d8932132b7f8aad666c5090f4
esj@rezo.net <esj@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-10-05 20:45:29 +0000

    [source:_plugins_/rainette Rainette] SVN n'étant pas capable de faire un "merge" de la v1.4.2 et de la v1.5 au motif que celle-ci s'est d'abord appelée v1.4.3 (i.e. qu'on a fait un "mv" qui est en fait un "cp+rm" au rabais), on est obligé de faire disparaître la branche v1 provisoirement pour renommer en v1 la v1.5.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@77002 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- rainette/branches/v1/rainette_fonctions.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Fichier de definition des balises du plugin utilisables dans les squelettes et modeles
-include_spip('public/rainette_balises');
-
-// Fichier de definition des filtres du plugin utilisables dans les squelettes et modeles
-include_spip('inc/rainette_filtres');
-?>
