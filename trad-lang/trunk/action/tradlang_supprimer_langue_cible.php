9416ad7cb3e714b71e7f838bea579ad99db81806
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-10-05 10:44:05 +0000

    Action de suppression d'une langue d'un module
    - Si les arguments passés à l'action ne sont pas bons => return false;
    - On supprime les versions des items de langues et leurs fragments
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@76974 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- trad-lang/trunk/action/tradlang_supprimer_langue_cible.php
+++ trad-lang/trunk/action/tradlang_supprimer_langue_cible.php
@@ -14,15 +14,34 @@ function action_tradlang_supprimer_langue_cible_dist(){
 	$securiser_action = charger_fonction('securiser_action', 'inc');
 	$arg = $securiser_action();
 	
-	if (!preg_match(",^(\w+)\/(\w+)$,", $arg, $r))
+	if (!preg_match(",^(\w+)\/(\w+)$,", $arg, $r)){
 		spip_log("action_tradlang_creer_langue_cible $arg pas compris","tradlang");
-	
+		return false;
+	}
 	$id_tradlang_module = intval($r[1]);
 	$lang_cible = $r[2];
 
 	include_spip('inc/autoriser');
 	if($lang_cible && intval($id_tradlang_module) && autoriser('modifier','tradlang') && !sql_countsel('spip_tradlangs','id_tradlang_module='.intval($id_tradlang_module).' AND lang='.sql_quote($lang_cible).' AND statut="0K"')){
+		/**
+		 * Suppression des versions
+		 */
+		$tradlangs = sql_allfetsel('id_tradlang','spip_tradlangs','id_tradlang_module='.intval($id_tradlang_module).' AND lang='.sql_quote($lang_cible));
+		$tradlangs_supprimer = array();
+		foreach($tradlangs as  $tradlang){
+			$tradlangs_supprimer[] = $tradlang['id_tradlang'];
+		}
+		if(count($tradlangs_supprimer)){
+			sql_delete('spip_versions','objet="tradlang" AND '.sql_in('id_objet',$tradlangs_supprimer));
+			sql_delete('spip_versions_fragments','objet="tradlang" AND '.sql_in('id_objet',$tradlangs_supprimer));
+		}
+		/**
+		 * Suppression des chaînes de langue
+		 */
 		sql_delete('spip_tradlangs','id_tradlang_module='.intval($id_tradlang_module).' AND lang='.sql_quote($lang_cible));
+		/**
+		 * Suppression des bilans de cette langue
+		 */
 		sql_delete('spip_tradlangs_bilans','id_tradlang_module='.intval($id_tradlang_module).' AND lang='.sql_quote($lang_cible));
 		include_spip('inc/invalideur');
 		suivre_invalideur('1');
