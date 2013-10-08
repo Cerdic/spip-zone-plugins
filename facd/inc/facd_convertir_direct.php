1c785ca541c342e49df8e252b29caf0446da0f35
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-10-08 10:09:55 +0000

    Si on affiche le log d'une conversion, avoir un bouton pour ne plus l'afficher
    Version 0.3.0
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@77073 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- facd/inc/facd_convertir_direct.php
+++ facd/inc/facd_convertir_direct.php
@@ -21,29 +21,24 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
  */
 function inc_facd_convertir_direct_dist(){
 	if(function_exists('fsockopen')){
-		spip_log('Appel de facd_convertir en fsokopen ','facd');
 		$nb_conversions = sql_countsel('spip_facd_conversions', "statut='en_cours'");
 		if($nb_conversions == 0){
 			$url = generer_url_action('facd_traiter_conversion','','',true);
 			$parts=parse_url($url);
 			$fp = fsockopen($parts['host'],
-		        isset($parts['port'])?$parts['port']:80,
-		        $errno, $errstr, 30);
+				isset($parts['port'])?$parts['port']:80, $errno, $errstr, 30);
 			if ($fp) {
-		    	$out = "GET ".$parts['path']."?".$parts['query']." HTTP/1.1\r\n";
-	    		$out.= "Host: ".$parts['host']."\r\n";
-	    		$out.= "Connection: Close\r\n\r\n";	
+				$out = "GET ".$parts['path']."?".$parts['query']." HTTP/1.1\r\n";
+				$out.= "Host: ".$parts['host']."\r\n";
+				$out.= "Connection: Close\r\n\r\n";	
 				fwrite($fp, $out);
 				fclose($fp);
-			}else{
+			}else
 				spip_log('fsockopen ne semble pas fonctionner','facd');
-			}
-		}else{
+		}else
 			spip_log('Une conversion est déjà en cours','facd');
-		}
-	}else{
+	}else
 		spip_log('fsockopen ne semble pas disponible','facd');
-	}
 	return;
 }
 
