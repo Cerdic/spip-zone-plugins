ea0be8fe1b7f512803d3687f98428e399fb2b7c7
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-10-10 21:35:16 +0000

    langues es (paquet-parrainage)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@77136 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ parrainage/trunk/lang/paquet-parrainage_es.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-parrainage?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// P
+	'parrainage_description' => 'Permite importar contactos tras enviar en masa invitaciones para unirse al grupo. Si las personas se registran, se conserva un vínculo con aquél que las ha invitado (el padrino).',
+	'parrainage_slogan' => 'Proponer a los usuarios una herramienta de apadrinamiento.'
+);
+
+?>
