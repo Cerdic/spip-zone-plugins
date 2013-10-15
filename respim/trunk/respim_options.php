9723740499a9bec379b2bee399a8a327b4d2ff73
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-10-15 15:09:51 +0000

    On ameliore le concept qui fonctionne maintenant par enrichissement progressif en 3 couches :
    - Couche HTML 
        * l'image JPG en DATA URI très compressée est affichée par défaut (affichage sans style par exemple)
    - Couche CSS
        * l'image JPG très compressée est affichée en opacité 0.7 en guise de fallback le temps que la bonne image se charge (connexion foireuse en mobilité)
        * une media-querie indique quelle image charger en fonction de la taille de la fenêtre et de la résolution de l'écran
        * la bonne image viens s'afficher par dessus le JPG de fallback quand elle arrive (en background-image dans un :after judicieusement positionné), donnant un rendu "idéal" pour le périphérique, ainsi qu'en dessous de <img> (en background-image toujours)
    - Couche JS
        * on injecte en asynchrone, quand tout est chargé (window.onload) un style qui passe le fallback JPG en opacité 0.01 et masque le :after pour afficher la background-image située en dessous de <img>. Celle-ci reste atteignable à la souris (clic droit) et affichée, lisible par les lecteurs d'écran.
        
     
     Ainsi :
     - si on a pas JS on ne perd que la fonction "enregistrer cette image" mais on a un bon rendu et le contenu sémantique
     - si on a pas JS ni CSS on a l'image de fallback dégradée, qui est inclue dans le HTML
     - on a un seul hit par image (mais on paye le poids du fallback en DATA URI en plus)
     - si on a une connexion très dégradée (latence foireuse), on a reçu en un coup le contenu avec les images de fallback en DATA URI, dans le HTML, qui donnent sens au contenu sans devoir attendre chaque image qui sera longue a charger
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@77239 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- respim/trunk/respim_options.php
+++ respim/trunk/respim_options.php
@@ -27,9 +27,7 @@ function respim_markup($img, $rwd_images, $width, $height){
 	if (strpos($class,"respim")!==false) return $img;
 	ksort($rwd_images);
 	$cid = "c".crc32(serialize($rwd_images));
-	$style =
-"img.respim{opacity:0.01;max-width:100%;height:auto;}"
-."b.respwrapper{background-size:100%;background-repeat:no-repeat;display:inline-block;max-width:100%}";
+	$style = "";
 	if ($class) $class = " $class";
 	$class = "$cid$class";
 	$img = inserer_attribut($img,"class","respim-fallback $class");
@@ -48,16 +46,18 @@ function respim_markup($img, $rwd_images, $width, $height){
 	$prev_width = 0;
 	$medias = array();
 	foreach ($rwd_images as $w=>$file){
+		// $file = "filedelay.api/5/$file"; // debug : injecter une tempo dans le chargement de l'image pour tester l'enrichissement progressif
+		// $file = $file."?rwd"; // debug  : etre sur qu'on charge bien l'image issue des medias queries
 		$mw = ($prev_width?"(min-width:{$prev_width}px)":"(max-width:{$w}px)");
 		$mw20 = ($prev_width?"(min-width:".round($prev_width/2)."px)":"(max-width:".round($w/2)."px)");
 		$mw15 = ($prev_width?"(min-width:".round($prev_width/1.5)."px)":"(max-width:".round($w/1.5)."px)");
-		// ?rwd pour le debug (etre sur qu'on charge bien l'image issue des medias queries)
-		$medias[$w] = "@media screen and $mw,screen and (-webkit-min-device-pixel-ratio: 2) and $mw20,screen and (-webkit-min-device-pixel-ratio: 1.5) and $mw15,screen and (min--moz-device-pixel-ratio: 2) and $mw20,screen and (min--moz-device-pixel-ratio: 1.5) and $mw15{b.$cid{background-image:url($file?rwd);}}";
+		$medias[$w] = "@media screen and $mw,screen and (-webkit-min-device-pixel-ratio: 2) and $mw20,screen and (-webkit-min-device-pixel-ratio: 1.5) and $mw15,screen and (min--moz-device-pixel-ratio: 2) and $mw20,screen and (min--moz-device-pixel-ratio: 1.5) and $mw15{b.$cid,b.$cid:after{background-image:url($file?);}}";
 		$prev_width = $w+1;
 	}
-	$style .= "b.$cid{background-image:url($file);}";
+	// en premier : la plus grande image par defaut
+	$style .= "b.$cid,b.$cid:after{background-image:url($file)}";
+	// puis surcharge en Media Queries
 	$style .= implode("",$medias);
-
 
 	$out = "<!--[if IE]>$img<![endif]-->\n";
 	$img = inserer_attribut($img,"src",$fallback_file);
@@ -150,6 +150,20 @@ function respim_affichage_final($texte){
 		if (count($replace))
 			$texte = str_replace(array_keys($replace),array_values($replace),$texte);
 		#var_dump(spip_timer());
+		// les styles communs a toutes les images responsive en cours de chargement
+		$ins = "<style type='text/css'>"."img.respim{opacity:0.70;max-width:100%;height:auto;}"
+		."b.respwrapper{display:inline-block;max-width:100%;position:relative;background-size:100%;background-repeat:no-repeat;}"
+		."b.respwrapper:after{position:absolute;top:0;left:0;right:0;bottom:0;background-size:100%;background-repeat:no-repeat;display:inline-block;max-width:100%;content:\"\"}"
+		."</style>";
+		// le script qui est appele post-chargement pour finir le rendu (rend les images enregistrables par clic-droit aussi)
+		$async_style = "html img.respim{opacity:0.01}html b.respwrapper:after{display:none;}";
+		$ins .= "<script>window.onload = function(){"
+		  ."var sa = document.createElement('style'); sa.type = 'text/css';"
+		  ."sa.innerHTML = '$async_style';"
+		  ."var s = document.getElementsByTagName('style')[0]; s.parentNode.insertBefore(sa, s);}</script>";
+		// inserer abant le premier <script> ou <link a defaut
+		if ($p = strpos($texte,"<link") OR $p = strpos($texte,"<script") OR $p = strpos($texte,"</head"))
+			$texte = substr_replace($texte,"<!--[if !IE]-->$ins<!--[endif]-->\n",$p,0);
 	}
 	return $texte;
 }
