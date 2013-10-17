ebd434fed33c5596a7ad860c03b10a251b274393
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-10-17 22:01:30 +0000

    [Salvatore] [source:_plugins_/diogene/diogene_complements/diogene_geo/trunk/lang/ paquet-diogene_geo] Export depuis http://trad.spip.net de la langue es
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@77347 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- diogene/diogene_complements/diogene_geo/trunk/lang/paquet-diogene_geo_es.php
+++ diogene/diogene_complements/diogene_geo/trunk/lang/paquet-diogene_geo_es.php
@@ -8,9 +8,9 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// D
-	'diogene_geo_description' => 'Complemento geográfico para "Diógenes"', # MODIF
-	'diogene_geo_nom' => 'Diógenes - Geo', # MODIF
-	'diogene_geo_slogan' => 'Complemento geográfico para "Diógenes"' # MODIF
+	'diogene_geo_description' => 'Añade la posibilidad de seleccionar una posición geográfica de un objeto desde su formulario de edición si esta opción está activada en la plantilla de formulario de Diógenes',
+	'diogene_geo_nom' => 'Diógenes - Geo',
+	'diogene_geo_slogan' => 'Complemento geográfico para "Diógenes"'
 );
 
 ?>
