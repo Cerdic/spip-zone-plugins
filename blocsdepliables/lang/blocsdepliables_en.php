a448ee61184753129815dbff46d673f7cce13c5e
tcharlss@hotmail.fr <tcharlss@hotmail.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-10-17 21:59:59 +0000

    [Salvatore] [source:_plugins_/blocsdepliables/lang/ blocsdepliables] Export depuis http://trad.spip.net de la langue en
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@77308 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- blocsdepliables/lang/blocsdepliables_en.php
+++ blocsdepliables/lang/blocsdepliables_en.php
@@ -15,6 +15,10 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'cfg_exemple_explication' => 'Example explanation',
 	'cfg_titre_parametrages' => 'Parameters',
 
+	// E
+	'explication_cookie' => 'The state of the numbered blocks will be stocked in a cookie during the session, in order to keep the page’s aspect for next visit.', # RELIRE
+	'explication_unique' => 'Opening a block will trigger all the other blocks on the page to close, in order to have a single opened block at the time.', # RELIRE
+
 	// L
 	'label_animation' => 'Animation',
 	'label_animation_aucun' => 'None',
@@ -24,6 +28,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'label_animation_slow' => 'Slow slide',
 	'label_balise_titre' => 'Tag for the blocks title',
 	'label_cookie_1' => 'Remember the blocks state',
+	'label_unique_1' => 'A single block opened on the page', # RELIRE
 
 	// T
 	'titre_page_configurer_blocsdepliables' => 'Expanding containers blocks'
