23e2bf60fb0d26719e184701f901f9f2e1623051
tcharlss@hotmail.fr <tcharlss@hotmail.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-10-17 22:00:53 +0000

    [Salvatore] [source:_plugins_/corbeille/trunk/lang/ corbeille] Export depuis http://trad.spip.net de la langue en
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@77330 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- corbeille/trunk/lang/corbeille_en.php
+++ corbeille/trunk/lang/corbeille_en.php
@@ -7,14 +7,32 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
+	// A
+	'articles_corbeille_tous' => '@nb@ articles in the trash',
+	'articles_corbeille_un' => '1 article in the trash',
+	'aucun_element_supprime' => 'Nothing was removed from the trash',
+	'auteurs_corbeille_un' => '1 autor in the trash',
+
+	// B
+	'breves_corbeille_tous' => '@nb@ news item in the trash',
+	'breves_corbeille_un' => '1 news item in the trash',
+
 	// C
 	'corbeille' => 'Recycle Bin',
 
 	// E
 	'effacer' => 'Erase',
+	'effacer_tout' => 'Erase all', # RELIRE
+	'element_supprime' => '@ids@ was removed from the trash',
+	'elements_supprimes' => '@ids@ were removed from the trash',
 
 	// R
 	'readme' => 'This page allows the {{permanent deletion}} of documents sent to the recycle bin. You may leave documents in the recycle bin as long as you wish. It’s {{up to you}} to put out the rubbish.',
+
+	// S
+	'selectionner' => 'select',
+	'sites_corbeille_tous' => '@nb@ websites in the trash',
+	'sites_corbeille_un' => '1 website in the trash',
 
 	// V
 	'voir_detail' => 'see details'
