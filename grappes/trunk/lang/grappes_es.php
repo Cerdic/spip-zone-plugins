6abf8486efc9d7d17a734c4becbe7e550c9b30dc
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-10-17 22:03:52 +0000

    [Salvatore] [source:_plugins_/grappes/trunk/lang/ grappes] Export depuis http://trad.spip.net de la langue es
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@77380 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- grappes/trunk/lang/grappes_es.php
+++ grappes/trunk/lang/grappes_es.php
@@ -1,69 +1,72 @@
 <?php
-
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/grappes?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-// A
-'action_inconnue' => 'Acci&oacute;n "@action@" desconocida',
-'autoriser_associer_non' => 'No tiene autorisaci&oacute;n para efectuar esta acci&oacute;n.',
 
-// B
-'bouton_ajouter' => 'A&ntilde;adir',
-'bouton_chercher' => 'Buscar',
-'bouton_checkbox_qui_administrateurs' => 'Administradores y administradoras del sitio',
-'bouton_checkbox_qui_redacteurs' => 'Redactoras y redactores',
+	// A
+	'action_inconnue' => 'Acción "@action@" desconocida',
+	'autoriser_associer_non' => 'No está autorizado para realizar esta acción.', # MODIF
 
-// D
-'delier' => 'Disociar',
+	// B
+	'bouton_ajouter' => 'Añadir',
+	'bouton_checkbox_qui_administrateurs' => 'Administradores y administradoras del sitio',
+	'bouton_checkbox_qui_id_admin' => 'El creador del racimo (¿Usted?)', # MODIF
+	'bouton_checkbox_qui_redacteurs' => 'Redactoras y redactores',
+	'bouton_chercher' => 'Buscar',
 
-// I
-'icone_creation_grappe' => 'Crear un nuevo racimo',
-'icone_modifier_grappe' => 'Modificar este racimo',
-'icone_supprimer_grappe' => 'Suprimir este racimo',
-'icone_voir_toutes_grappes' => 'Ver todos los racimos',
-'info_acces_0minirezo' => 'Administradores/as',
-'info_acces_1comite' => 'Redactores/as',
-'info_changer_nom_grappe' => 'Cambiar el nombre del racimo',
-'info_creation_grappes' => 'Crear o modificar racimos de elementos',
-'info_grappes_association' => '&iquest;Cu&acute;les objetos pueden ser asociados al racimo?',
-'info_lier_auteurs' => 'Autores/as',
-'info_lier_articles' => 'Art&iacute;culos',
-'info_lier_breves' => 'Notas breves',
-'info_lier_documents' => 'Documentos',
-'info_lier_mots' => 'Palabras',
-'info_lier_grappes' => 'Racimos',
-'info_lier_rubriques' => 'Secciones',
-'info_lier_sites' => 'Sitios',
-'info_lier_syndic' => 'Sitios',
-'info_nom_grappe' => 'Nombre del racimo',
-'info_qui_peut_lier' => '&iquest;Qui&eacute;n puede asociar objetos al racimo?',
-'info_rechercher' => 'Buscar', // buscar nuevos objetos para asociar
+	// D
+	'delier' => 'Disociar',
+	'delier_tout' => 'Desvincular todo',
 
-'item_groupes_association_articles' => 'Los art&iacute;culos',
-'item_groupes_association_auteurs' => 'Las autoras y autores',
-'item_groupes_association_breves' => 'Las notas breves',
-'item_groupes_association_documents' => 'Los documentos',
-'item_groupes_association_grappes' => 'Los racimos',
-'item_groupes_association_groupes_mots' => 'Los grupos de palabras',
-'item_groupes_association_mots' => 'Las palabras',
-'item_groupes_association_rubriques' => 'Las secciones',
-'item_groupes_association_syndic' => 'Los sitios',
+	// I
+	'icone_creation_grappe' => 'Crear un nuevo racimo',
+	'icone_modifier_grappe' => 'Modificar este racimo',
+	'icone_supprimer_grappe' => 'Suprimir este racimo',
+	'icone_voir_toutes_grappes' => 'Ver todos los racimos',
+	'info_1_grappe' => '1 racimo',
+	'info_aucune_grappe' => 'Ningún racimo',
+	'info_changer_nom_grappe' => 'Cambiar el nombre del racimo',
+	'info_creation_grappes' => 'Crear o modificar racimos de elementos', # MODIF
+	'info_grappes_association' => '¿Qué objetos pueden asociarse al racimo?', # MODIF
+	'info_grappes_miennes' => 'Mis racimos',
+	'info_grappes_toutes' => 'Todos los racimos',
+	'info_nb_grappes' => '@nb@ racimos',
+	'info_nom_grappe' => 'Nombre del racimo',
+	'info_qui_peut_lier' => '¿Quién puede asociar objetos al racimo?', # MODIF
+	'info_rechercher' => 'Buscar',
+	'item_groupes_association_articles' => 'Los artículos',
+	'item_groupes_association_auteurs' => 'Las autoras y autores',
+	'item_groupes_association_breves' => 'Las notas breves',
+	'item_groupes_association_documents' => 'Los documentos',
+	'item_groupes_association_grappes' => 'Los racimos',
+	'item_groupes_association_groupes_mots' => 'Los grupos de palabras',
+	'item_groupes_association_mots' => 'Las palabras',
+	'item_groupes_association_rubriques' => 'Las secciones',
+	'item_groupes_association_syndic' => 'Los sitios',
 
-// L
-'lier' => 'Asociar',
+	// L
+	'label_acces' => 'Acceso',
+	'label_liaisons' => 'Objetos',
+	'label_type' => 'Tipo',
+	'lier' => 'Asociar',
 
-// P
-'pas_de_identifiant' => 'Nombre desconocido',
+	// P
+	'pas_de_identifiant' => 'Nombre desconocido',
 
-// T
-'texte_descriptif' => 'Descripci&oacute;n',
+	// T
+	'texte_descriptif' => 'Descripción',
+	'titre_grappe' => 'Racimo',
+	'titre_grappes' => 'Racimos',
+	'titre_logo_grappe' => 'Logo del racimo',
+	'titre_nouvelle_grappe' => 'Nuevo racimo',
+	'titre_page_grappes' => 'Racimos',
 
-'titre_grappe' => 'Racimo',
-'titre_nouvelle_grappe' => 'Nuevo racimo',
-'titre_page_grappes' => 'Racimos',
-
-// V
-'voir' => 'Ver',
+	// V
+	'voir' => 'Ver'
 );
 
 ?>
