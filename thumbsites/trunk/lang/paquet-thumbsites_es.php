dffe3b9cbdfbb9fff76bfb506380aebd437c6da9
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-10-17 22:07:51 +0000

    [Salvatore] [source:_plugins_/thumbsites/trunk/lang/ paquet-thumbsites] Export depuis http://trad.spip.net de la langue es
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@77486 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ thumbsites/trunk/lang/paquet-thumbsites_es.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-thumbsites?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// T
+	'thumbsites_description' => 'Thumbsites proporciona un sistema de captura de pantalla de sitio generada por los servidores dedicados. El plugin propone etiquetas, filtros y modelos para mostrar la miniatura de un sitio dado. Este plugin genera también una caché que permite paliar las indisponibilidades frecuentes de servidores y acelerar las visualizaciones. Una configuración está disponible en el espacio privado.',
+	'thumbsites_slogan' => 'Una miniatura para sus sitios referenciados'
+);
+
+?>
