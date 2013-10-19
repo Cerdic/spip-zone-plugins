42bff94cd72d9c230b35ec76bd760849b3f09cdf
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-10-19 20:26:55 +0000

    tenir compte de afficher_si_remplissage même si aucun afficher_si
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@77564 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- saisies/inc/saisies.php
+++ saisies/inc/saisies.php
@@ -470,4 +470,20 @@ function saisies_afficher_si($saisies) {
 	return false;
 }
 
+
+/*
+ * Le tableau de saisies a-t-il une option afficher_si_remplissage ?
+ *
+ * @param array $saisies Un tableau de saisies
+ * @return boolean
+ */
+function saisies_afficher_si_remplissage($saisies) {
+	$saisies = saisies_lister_par_nom($saisies,true);
+	// Dès qu'il y a au moins une option afficher_si_remplissage, on l'active
+	foreach ($saisies as $saisie) {
+		if (isset($saisie['options']['afficher_si_remplissage']))
+			return true;
+	}
+	return false;
+}
 ?>
