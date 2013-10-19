dda94650c3043227b5199c9e530d945b869b0f5b
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-10-19 17:49:44 +0000

    insertion de l'option lors de la saisie d'une entrée + chaîne de langue + licence
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@77556 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ formidable_reponses_commentees/trunk/formidable_reponses_commentees_pipelines.php
@@ -0,0 +1,44 @@
+<?php
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+function formidable_reponses_commentees_formulaire_verifier($flux){
+    
+    if ($flux['args']['form'] == "construire_formulaire" and $nom_ou_id = _request('configurer_saisie')){
+        $identifiant = 'constructeur_formulaire_'.$flux['args']['args'][0];
+        $formulaire_actuel = session_get($identifiant);
+        if ($nom_ou_id[0] == '@') {
+			$saisies_actuelles = saisies_lister_par_identifiant($formulaire_actuel);
+			$name = $saisies_actuelles[$nom_ou_id]['options']['nom'];
+		} else {
+			$saisies_actuelles = saisies_lister_par_nom($formulaire_actuel);
+			$name = $nom_ou_id;
+		}
+        $nom = 'configurer_' . $name;
+        // saisie inexistante => on sort
+		if (!isset($saisies_actuelles[$nom_ou_id])) {
+			return $flux;
+		}
+        foreach ($flux['data'] as $config){
+    //        var_dump($config);
+            if (gettype($config) == "array"){
+                foreach ($config as $fieldset){
+                    if ($fieldset['saisie'] == 'fieldset' and $fieldset['options']['label']=="<:saisies:option_groupe_affichage:>"){
+
+                        $flux['data'][$nom] = saisies_inserer($flux['data'][$nom], array(
+                                'saisie' => 'textarea',
+                                'options' => array(
+                                    'nom' => "saisie_modifiee_${name}[affichage][commentaire_apres_reponse]",
+                                    'rows' => 10,
+                                    'label' => _T('formidable_reponses_commentees:commentaire_apres_reponse_label'),
+                                    'explication' => _T('formidable_reponses_commentees:commentaire_apres_reponse_explication')
+                                )
+                            ));                            
+                        }
+                    }
+                }
+            }
+        }
+    
+    return $flux;
+    }
+?>
---
+++ formidable_reponses_commentees/trunk/lang/formidable_reponses_commentees_fr.php
@@ -0,0 +1,10 @@
+<?php
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	'commentaire_apres_reponse_label'=>'Commentaire à afficher après la réponse',
+	'commentaire_apres_reponse_explication'=>'Saisissez un commentaire qui sera fournie à l\'utilisateur après qu\'il ait envoyé sa réponse. Vous pouvez utiliser les raccourcis de SPIP.'
+)
+;
+?>
