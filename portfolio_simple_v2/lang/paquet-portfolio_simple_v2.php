3d6e0b647259cd994e9d96bfd25efb9b2cb43d9b
ydikoi@me.com <ydikoi@me.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-10-21 11:24:58 +0000

    Ajout de la description du plugin ; corrections cosmétiques sur affichage de la photo (en cas d'absence d'EXIF)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@77586 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ portfolio_simple_v2/lang/paquet-portfolio_simple_v2.php
@@ -0,0 +1,22 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-photo_infos
+// Langue: fr
+// Date: 14-11-2011 09:27:17
+// Items: 3
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// P
+	'portfolio_simple_v2_description' => 'Un plugin permettant la mise en œuvre rapide et simple d\'un portfolio ou d\'un book.
+
+	Il suffit simplement de déposer les images dans une rubrique, tout le reste (titre, description, …) étant optionnels',
+	'portfolio_simple_v2_nom' => 'Portfolio simple v2',
+	'portfolio_simple_v2_slogan' => 'Mettez vos images dans une rubrique, et hop : voilà votre book !',
+);
+?>
