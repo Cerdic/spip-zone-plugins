a7ca755acb79071a52c71d42d19543babddc40e7
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-10-22 11:18:52 +0000

    [Salvatore] [source:_plugins_/formidable_reponses_commentees/trunk/lang/ formidable_reponses_commentees] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@77604 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- formidable_reponses_commentees/trunk/lang/formidable_reponses_commentees_fr.php
+++ formidable_reponses_commentees/trunk/lang/formidable_reponses_commentees_fr.php
@@ -1,10 +1,13 @@
 <?php
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/formidable_reponses_commentees/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-	'commentaire_apres_reponse_label'=>'Commentaire à afficher après la réponse',
-	'commentaire_apres_reponse_explication'=>'Saisissez un commentaire qui sera fournie à l\'utilisateur après qu\'il ait envoyé sa réponse. Vous pouvez utiliser les raccourcis de SPIP.'
-)
-;
+
+	// C
+	'commentaire_apres_reponse_explication' => 'Saisissez un commentaire qui sera fournie à l’utilisateur après qu’il ait envoyé sa réponse. Vous pouvez utiliser les raccourcis de SPIP.',
+	'commentaire_apres_reponse_label' => 'Commentaire à afficher après la réponse'
+);
+
 ?>
