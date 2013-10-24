9d6095513bdf1419ea6af9665acbe7764cf7c706
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-10-24 23:21:03 +0000

    On met à jour la librairie jquery.mousewheel
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@77637 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- mediaspip_player/trunk/mediaspip_player_options.php
+++ mediaspip_player/trunk/mediaspip_player_options.php
@@ -5,7 +5,7 @@
  *
  * Auteurs :
  * kent1 (http://www.kent1.info - kent1@arscenic.info)
- * 2010-2012 - Distribué sous licence GNU/GPL
+ * 2010-2013 - Distribué sous licence GNU/GPL
  * 
  * Fichier d'options
  */
@@ -15,7 +15,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
  * On définit _DIR_LIB_MOUSEWHEEL
  * https://github.com/brandonaaron/jquery-mousewheel/tags
  */
-define('_DIR_LIB_MOUSEWHEEL','lib/jquery-mousewheel-3.0.6/');
+define('_DIR_LIB_MOUSEWHEEL','lib/jquery-mousewheel-3.1.4/');
 
 /**
  * On force le fait que l'on puisse ajouter des documents sur les documents
