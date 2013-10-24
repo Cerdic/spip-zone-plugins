496c490c315aff69978722f16f50772d76526d55
amaury.adon@laposte.net <amaury.adon@laposte.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-10-24 08:19:32 +0000

    un fichier un peu propre avec des choses qui fonctionnent et pas plein de lignes de test et de debug c'est mieux
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@77618 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- seminaire/branches/v1.5/seminaire_pipelines.php
+++ seminaire/branches/v1.5/seminaire_pipelines.php
@@ -1,7 +1,6 @@
 <?php
 function seminaire_insert_head($flux){
     $flux .= '<link rel="stylesheet" type="text/css" href="'.find_in_path('inc-css-seminaire.css.html').'" type="text/css" media="all" />'."\n";
-	$flux .="\n".'<link rel="stylesheet" href="'._DIR_LIB_SEMINAIRE.'fancybox/jquery.fancybox-1.3.4.css" type="text/css" media="all" />'."\n";
     return $flux;
 }
 ?>
