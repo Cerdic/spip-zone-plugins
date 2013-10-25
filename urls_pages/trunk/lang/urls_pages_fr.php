be909df19fe4b74ecd687fcda94a42da1f4f9954
tcharlss@hotmail.fr <tcharlss@hotmail.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-10-25 18:01:36 +0000

    On essaie de rendre le formulaire un poil plus sobre et surtout plus compréhensible. Nettoyage du fichier de langue
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@77649 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- urls_pages/trunk/lang/urls_pages_fr.php
+++ urls_pages/trunk/lang/urls_pages_fr.php
@@ -9,25 +9,23 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'cfg_titre_parametrages' => 'URLs pages personnalisées',
 
 	// L
-	'label_rewritebase' => 'RewriteBase',
 	'label_liste_pages' => 'Pages et urls correspondantes',
 	'label_code_htaccess' => 'Code .htaccess',
 
 	// E
 	'explication_formulaire_1' => 'Ce formulaire permet de mettre en place des urls personnalisées pour les squelettes ne correspondant à aucun objet éditorial : les pages. Leurs urls seront prises en charge par la balise <tt>#URL_PAGE</tt>.',
-	'explication_code_htacess' => 'Copiez le code ci-dessous dans le fichier <tt>.htaccess</tt> afin d\'activer la redirection.',
-	'explication_rewritebase' => 'Si présente, copier la directive RewriteBase du fichier <tt>.htacess</tt>',
-	'explication_ecriture_url' => 'N\'indiquez que la partie figurant après la racine du site :<br>
-	<tt>@url_site@/<ins>url personnalisée</ins></tt>',
+	'explication_code_htacess' => 'Après validation du formulaire, copiez le code ci-dessous dans le fichier <tt>.htaccess</tt>.',
+	'explication_rewritebase' => 'Si votre fichier <tt>.htaccess</tt> comporte une directive RewriteBase, indiquez la ici.',
 	'explication_dossier' => 'Dossier contenant les squelettes',
 	'erreur_url_non_libre' => 'Cette url est déjà utilisée',
 	'erreur_url_doublon' => 'Une url doit être unique',
+	'erreur_pages_obsoletes' => 'Les pages suivantes sont enregistrées en configuration mais ne sont plus actives. Les squelettes correspondants ont du être renommés, supprimés, ou le plugin d\'où ils proviennent désactivé. Il est recommandé de vider ces champs.',
 
 	// I
 	'info_aucun_squelette' => 'Aucun squelette n\'a été trouvé',
 
 	// M
-	'message_ok_code' => '. <br>Copiez le code présent en fin du formulaire dans le fichier .htaccess'
+	'message_ok_code' => '. <br>N\'oubliez pas de copier le code présent en fin du formulaire dans le fichier .htaccess'
 );
 
 ?>
