6c48f8902f9ac92f3d4288192ed019a24f73ed1f
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-10-27 11:53:46 +0000

    [Salvatore] [source:_plugins_/pages/trunk/lang/ pages] Export depuis http://trad.spip.net de la langue sk
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@77703 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- pages/trunk/lang/pages_sk.php
+++ pages/trunk/lang/pages_sk.php
@@ -8,15 +8,23 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
-	'aucune_page' => 'Momentálne žiadne stránky neexistujú.', # MODIF
+	'aucune_page' => 'Momentálne tu nie je žiadna stránka.',
 
 	// C
 	'convertir_article' => 'Zmeniť na článok',
 	'convertir_page' => 'Zmeniť na stránku',
 	'creer_page' => 'Vytvoriť novú stránku',
 
+	// E
+	'erreur_champ_page_doublon' => 'Tento identifikátor už existuje',
+	'erreur_champ_page_format' => 'Malé písmená alebo jeden "_"',
+	'erreur_champ_page_taille' => 'Maximum je 255 znakov',
+
+	// L
+	'label_champ_page' => 'Stránka:',
+
 	// M
-	'modifier_page' => 'Upraviť stránku:', # MODIF
+	'modifier_page' => 'Upraviť stránku:',
 
 	// P
 	'pages_uniques' => 'Jedinečné stránky',
