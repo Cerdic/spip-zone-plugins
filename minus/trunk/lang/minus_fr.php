b512f1d3f2b65015a8e6ee03b0431b07c19d4fbe
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-10-27 11:53:32 +0000

    [Salvatore] [source:_plugins_/minus/trunk/lang/ minus] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@77698 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- minus/trunk/lang/minus_fr.php
+++ minus/trunk/lang/minus_fr.php
@@ -1,13 +1,12 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_core_/plugins/porte_plume/lang/
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/minus/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-	// P
-	'trop_majuscule' => "Votre titre contient beaucoup de majuscules. Êtes vous sûr de ne pas vouloir les mettre en minuscules ? Sur Internet, les majuscules donnent l'impression que l'on crie : utilisez-les avec parcimonie."
+	// T
+	'trop_majuscule' => 'Votre titre contient beaucoup de majuscules. Êtes vous sûr de ne pas vouloir les mettre en minuscules ? Sur Internet, les majuscules donnent l’impression que l’on crie : utilisez-les avec parcimonie.'
 );
 
 ?>
-
