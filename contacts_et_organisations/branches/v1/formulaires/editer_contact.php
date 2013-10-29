621885193c33cd95c597016c1bd4d83e40476d62
marcimat@rezo.net <marcimat@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-10-29 16:16:19 +0000

    Encore un problème dans les redirections à la création d'un contact sur la page ?exec=contacts. Je ne suis même pas certain que cette correction n'ajoute pas d'autres problèmes ailleurs.
    La version pour SPIP 3 elle fonctionne parfaitement. C'est toujours cette version pour SPIP 2.1 qui ennuie.
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@77758 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- contacts_et_organisations/branches/v1/formulaires/editer_contact.php
+++ contacts_et_organisations/branches/v1/formulaires/editer_contact.php
@@ -18,7 +18,11 @@ function formulaires_editer_contact_verifier_dist($id_contact='new', $id_organis
 function formulaires_editer_contact_traiter_dist($id_contact='new', $id_organisation=0, $redirect=''){
 	if ($redirect) refuser_traiter_formulaire_ajax();
 	$res = formulaires_editer_objet_traiter('contact', $id_contact, $id_organisation, 0, $redirect);
+
 	if ($redirect) {
+		if (!parametre_url($redirect, 'id_contact')) {
+			$redirect = parametre_url($redirect, 'id_contact', $res['id_contact']);
+		}
 		$res['redirect'] = $redirect;
 	}
 	return $res;
