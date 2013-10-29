5ecd1eb36d5c322e90380537f5b35a2d71d2a235
severo@rednegra.net <severo@rednegra.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-10-29 16:48:57 +0000

    On remplace par la librairie forkée
    Version v1.0.0 récupérée sur https://github.com/koenpunt/chosen/releases/
    
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@77761 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- chosen/branches/option_adding/lang/paquet-chosen_fr.php
+++ chosen/branches/option_adding/lang/paquet-chosen_fr.php
@@ -8,9 +8,10 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// C
 	'chosen_description' => '[Chosen->http://harvesthq.github.com/chosen/] est une librairie JavaScript qui améliore l’expérience utilisateur des sélecteurs dans les formulaires HTML.
 
-La classe CSS <code>chosen</code> sur un <code><select></code> chargera automatiquement Chosen dessus.',
-	'chosen_nom' => 'Chosen',
-	'chosen_slogan' => 'Intégrer la librairie Chosen dans SPIP'
+La classe CSS <code>chosen</code> sur un <code><select></code> chargera automatiquement Chosen dessus.
+Cette branche du plugin Chosen est basée sur le fork de koenpunt - version 1.0.0 - voir https://github.com/koenpunt/chosen/releases/',
+	'chosen_nom' => 'Chosen (fork de koenpunt)',
+	'chosen_slogan' => 'Intégrer la librairie Chosen dans SPIP (fork de koenpunt)'
 );
 
 ?>
