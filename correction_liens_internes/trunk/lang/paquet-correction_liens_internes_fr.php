665f78a1e27f2ea473b2944b04b54f3f1831e237
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-10-29 16:29:25 +0000

    urls_decoder_url n'est pas dispo en spip 2.1 -> donc compatible que 3.0 -> on passe en paquet.xml
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@77759 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ correction_liens_internes/trunk/lang/paquet-correction_liens_internes_fr.php
@@ -0,0 +1,20 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-correction_liens_internes
+// Langue: fr
+// Date: 29-10-2013 17:24:58
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// C
+	'correction_liens_internes_description' => 'Corrige automatiquement les liens internes insérés par copier-coller de l\'url. <code>[texte->http://www.example.com/spip.php?article1]</code>, inséré dans le site <code>example.com</code> devient <code>[texte->article1]</code>.
+        ',
+	'correction_liens_internes_slogan' => 'Liens internes : soyez beaux!',
+);
+?>
