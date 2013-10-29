228040f6e546b980e8309a49d03aff6dc11c9c53
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-10-29 14:27:16 +0000

    Oula, les deux nouveaux champs de ce plugin doivent aussi être versionnées.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@77752 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- agenda_dates_floues/trunk/agendaflou_pipelines.php
+++ agenda_dates_floues/trunk/agendaflou_pipelines.php
@@ -9,19 +9,23 @@ function agendaflou_declarer_tables_objets_sql($flux){
 	$flux['spip_evenements']['field']['date_fin_floue'] = 'varchar(10) not null default ""';
 	$flux['spip_evenements']['champs_editables'][] = 'date_debut_floue';
 	$flux['spip_evenements']['champs_editables'][] = 'date_fin_floue';
+	$flux['spip_evenements']['champs_versionnes'][] = 'date_debut_floue';
+	$flux['spip_evenements']['champs_versionnes'][] = 'date_fin_floue';
 	
 	return $flux;
 }
 
 // Ajouter les champs dans le HTML
 function agendaflou_formulaire_fond($flux){
-	// Ajouter les champs de dates floues
-	$dates_floues = recuperer_fond('formulaires/inc-agendaflou', $flux['args']['contexte']);
-	$flux['data'] = preg_replace('@<li[^>]*class=(\'|")editer_repetitions[^\'"]*\\1[^>]*>@is', "$dates_floues$0", $flux['data']);
+	if ($flux['args']['form'] == 'editer_evenement'){
+		// Ajouter les champs de dates floues
+		$dates_floues = recuperer_fond('formulaires/inc-agendaflou', $flux['args']['contexte']);
+		$flux['data'] = preg_replace('@<li[^>]*class=(\'|")editer_repetitions[^\'"]*\\1[^>]*>@is', "$dates_floues$0", $flux['data']);
 	
-	// Ajouter une case pour afficher/masquer les dates floues
-	$masque = '<label for="utiliser_dates_floues">'._T('agendaflou:date_floue_utiliser').'</label><input onclick="if (this.checked==false) { $(\'.editer_dates_floues\').hide(\'fast\').find(\'option:selected\').removeAttr(\'selected\');} else {$(\'.editer_dates_floues\').show(\'fast\');}" id="utiliser_dates_floues" class="checkbox" type="checkbox" '.(($flux['args']['contexte']['date_debut_floue'] or $flux['args']['contexte']['date_fin_floue']) ? 'checked="checked"' : '').'/>';
-	$flux['data'] = preg_replace('@<input[^>]*name=(\'|")horaire\\1[^>]*>@is', "$0$masque", $flux['data']);
+		// Ajouter une case pour afficher/masquer les dates floues
+		$masque = '<label for="utiliser_dates_floues">'._T('agendaflou:date_floue_utiliser').'</label><input onclick="if (this.checked==false) { $(\'.editer_dates_floues\').hide(\'fast\').find(\'option:selected\').removeAttr(\'selected\');} else {$(\'.editer_dates_floues\').show(\'fast\');}" id="utiliser_dates_floues" class="checkbox" type="checkbox" '.(($flux['args']['contexte']['date_debut_floue'] or $flux['args']['contexte']['date_fin_floue']) ? 'checked="checked"' : '').'/>';
+		$flux['data'] = preg_replace('@<input[^>]*name=(\'|")horaire\\1[^>]*>@is', "$0$masque", $flux['data']);
+	}
 	
 	return $flux;
 }
