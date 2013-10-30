dffbac48e0de212b49a6fb736b6ed5961239ea55
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-10-30 16:19:27 +0000

    Robustesse du JS : ne pas reposer sur $ mais sur jQuery
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@77785 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- saisies/javascript/saisies.js
+++ saisies/javascript/saisies.js
@@ -1,15 +1,15 @@
-$(function(){
+jQuery(function(){
 	saisies_fieldset_pliable();
 	onAjaxLoad(saisies_fieldset_pliable);
 });
 
 function saisies_fieldset_pliable(){
 	// On cherche les groupes de champs pliables
-	$('li.fieldset.pliable')
+	jQuery('li.fieldset.pliable')
 		.each(function(){
-			var li = $(this);
-			var ul = $(this).find('> fieldset > ul');
-			var legend = $(this).find('> fieldset > .legend');
+			var li = jQuery(this);
+			var ul = jQuery(this).find('> fieldset > ul');
+			var legend = jQuery(this).find('> fieldset > .legend');
 			
 			// S'il est déjà plié on cache le contenu
 			if (li.is('.plie'))
