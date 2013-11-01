2654ad99581cd60282849588d855b830c207c37d
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-01 17:37:27 +0000

    Amélioration de la synchronisation manuelle d'un site en utilisant l'API d'édition des sites.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@77833 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- boussole/trunk/action/client_synchroniser_site.php
+++ boussole/trunk/action/client_synchroniser_site.php
@@ -41,36 +41,31 @@ function action_client_synchroniser_site_dist(){
 		if ($boussole AND ($id_syndic = intval($id_site))) {
 			// Récupération des données du site dans la boussole
 			// -- trouver l'alias du site connaissant la boussole et son id_syndic
-			$site = sql_getfetsel(
-						'aka_site',
-						'spip_boussoles',
-						array(
-							'aka_boussole=' . sql_quote($boussole),
-							'id_syndic=' . sql_quote($id_syndic)));
-			$donnees_site = sql_fetsel(
-								array('nom_objet', 'slogan_objet', 'descriptif_objet', 'logo_objet'),
-								'spip_boussoles_extras',
-								array(
-									'aka_boussole=' . sql_quote($boussole),
-									'type_objet=' . sql_quote('site'),
-									'aka_objet=' . sql_quote($site)));
-
+			$from = array('spip_boussoles as b', 'spip_boussoles_extras as x');
+			$select = array('x.nom_objet', 'x.slogan_objet', 'x.descriptif_objet', 'x.logo_objet');
+			$where = array(
+						'b.aka_boussole=' . sql_quote($boussole), 'id_syndic=' . sql_quote($id_syndic),
+						'b.aka_boussole=x.aka_boussole', 'b.aka_site=x.aka_objet',
+						'x.type_objet=' . sql_quote('site'));
+			$donnees_site = sql_fetsel($select, $from, $where);
 
 			// Mettre à jour le nom et le descriptif du site dans la table spip_syndic avec les données de la boussole
 			if ($donnees_site) {
 				// Mise à jour en BDD des informations du site
 				include_spip('inc/filtres');
-				sql_updateq('spip_syndic',
-							array(
-								'nom_site'=> extraire_multi($donnees_site['nom_objet']),
-								'descriptif'=> extraire_multi($donnees_site['descriptif_objet'])),
-							'id_syndic=' . sql_quote($id_syndic));
+				include_spip('action/editer_site');
+				$contenu = array(
+							'nom_site'=> extraire_multi($donnees_site['nom_objet']),
+							'descriptif'=> extraire_multi($donnees_site['descriptif_objet']));
+				$erreur = site_modifier($id_syndic, $contenu);
 
-				// Mise à jour de son logo normal ("on")
-				$iconifier = charger_fonction('iconifier_site', 'inc');
-				$iconifier($id_syndic, 'on', $donnees_site['logo_objet']);
+				if (!$erreur) {
+					// Mise à jour de son logo normal ("on")
+					$iconifier = charger_fonction('iconifier_site', 'inc');
+					$iconifier($id_syndic, 'on', $donnees_site['logo_objet']);
 
-				spip_log("ACTION SYNCHRONISER SITE : id_syndic = $id_site - boussole = $boussole", 'boussole' . _LOG_INFO);
+					spip_log("ACTION SYNCHRONISER SITE : id_syndic = $id_site - boussole = $boussole", 'boussole' . _LOG_INFO);
+				}
 			}
 		}
 	}
