d1258d165a835104b5abd5a82e81ee21af0308ad
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-06 09:58:34 +0000

    Espace à la fin du fichier d'options qui provoquait pas mal d'erreurs de headers et blabla
    contrib.spip.net plutôt que spip-contrib.net
    
    Version 0.2.1
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@78003 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- wp2spip/wp2spip_options.php
+++ wp2spip/wp2spip_options.php
@@ -1,11 +1,18 @@
 <?php
-	function balise_WP2SPIP_DUMP ($p) {
-		spip_log('wp2spip dump Start');
-		$wp2spip=recuperer_fond ('wp2spip');
-		$ou='wp2spip.xml';
-		ecrire_fichier(_DIR_DUMP . $ou, $wp2spip);
-		spip_log('wp2spip dump Done');
-		$p->code = "'Base Wordpress convertie au format xml SPIP 2.1.12  !<br/><br/>Rendez-vous &agrave; la page <a href=\'?exec=admin_tech\' title=\'\'>Maintenance</a> du site et importer la base wp2spip.xml<br/><br/>Attention cette restauration remplacera votre base SPIP actuelle!!'";
-		return $p;
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+function balise_WP2SPIP_DUMP ($p) {
+	spip_log('wp2spip dump Start');
+	$wp2spip=recuperer_fond('wp2spip');
+	$ou='wp2spip.xml';
+	if(!is_dir(_DIR_DUMP)){
+		include_spip('inc/flock');
+		sous_repertoire(_DIR_DUMP);	
 	}
-?> 
+	ecrire_fichier(_DIR_DUMP . $ou, $wp2spip);
+	spip_log('wp2spip dump Done');
+	$p->code = "'Base Wordpress convertie au format xml SPIP 2.1.12  !<br/><br/>Rendez-vous &agrave; la page <a href=\'?exec=admin_tech\' title=\'\'>Maintenance</a> du site et importer la base wp2spip.xml<br/><br/>Attention cette restauration remplacera votre base SPIP actuelle!!'";
+	return $p;
+}
+?>
