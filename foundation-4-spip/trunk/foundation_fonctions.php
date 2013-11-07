0ea1a581b904f7d1026629db3021117a1fb7aa39
p@henix.be <p@henix.be@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-07 17:26:52 +0000

    Sortir la fonction pour pouvoir utiliser plusieurs fois le filtre |image_responsive.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@78037 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- foundation-4-spip/trunk/foundation_fonctions.php
+++ foundation-4-spip/trunk/foundation_fonctions.php
@@ -10,21 +10,20 @@
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 
+// Function de callback
+function responsive($matches) {
+    // Dans le cas de vimeo, il faut ajouter une classe
+    if (strpos($matches[0], 'vimeo')) $vimeo = ' vimeo';
+    else $vimeo = '';
+
+    // On revoie la bonne structure html d'iframe.
+    return '<div class="flex-video'.$vimeo.'"><iframe '.$matches[0].'></iframe></div>';
+}
+
 /*
 *   Rendre les iframes responsive via un filtre et la classe flex-video de Foundation.
 */
 function iframe_responsive($texte) {
-
-    // Function de callback
-    function responsive($matches) {
-        // Dans le cas de vimeo, il faut ajouter une classe
-        if (strpos($matches[0], 'vimeo')) $vimeo = ' vimeo';
-        else $vimeo = '';
-
-        // On revoie la bonne structure html d'iframe.
-        return '<div class="flex-video'.$vimeo.'"><iframe '.$matches[0].'></iframe></div>';
-    }
-    
     // On détecte tout les iFrames et on les rends responsives.
     return preg_replace_callback('/<iframe(.+)><\/iframe>/', 'responsive', $texte);
 }
