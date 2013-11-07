9316bf37e230b7977cf573708387a9483f714050
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-07 04:38:55 +0000

    [Salvatore] [source:_plugins_/reservation_evenement/trunk/lang/ reservations_detail] Export depuis http://trad.spip.net
    La langue 'de' devrait Ãªtre supprimÃ©e car trop peu traduite (31.58 %)
    La langue 'en' devrait Ãªtre supprimÃ©e car trop peu traduite (31.58 %)
    
    
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@78031 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- reservation_evenement/trunk/lang/reservations_detail_de.php
+++ reservation_evenement/trunk/lang/reservations_detail_de.php
@@ -1,26 +1,23 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/reservations_detail?lang_cible=de
+// ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
-
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
+	// I
+	'info_1_reservations_detail' => 'Ein Anmeldungsdetail',
+	'info_aucun_reservations_detail' => 'Keine Anmeldungsdetail',
+	'info_nb_reservations_details' => '@nb@ Anmeldungsdetails',
 
+	// L
+	'label_descriptif' => 'Beschreibung',
 
-    // I
-    'info_1_reservations_detail' => 'Ein Anmeldungsdetail',
-    'info_aucun_reservations_detail' => 'Keine Anmeldungsdetail',
-    'info_nb_reservations_details' => '@nb@ Anmeldungsdetails',
-    
-    //L
-    'label_descriptif'=>'Beschreibung',
-
-
-    // T
-
-    'titre_reservations_detail' => 'Anmeldungsdetail',
-    'titre_reservations_details' => 'Anmeldungsdetails',
+	// T
+	'titre_reservations_detail' => 'Anmeldungsdetail',
+	'titre_reservations_details' => 'Anmeldungsdetails'
 );
 
 ?>
--- reservation_evenement/trunk/lang/reservations_detail_en.php
+++ reservation_evenement/trunk/lang/reservations_detail_en.php
@@ -1,25 +1,23 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/reservations_detail?lang_cible=en
+// ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
-
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-
-
 
 	// I
 	'info_1_reservations_detail' => '1 booking detail',
 	'info_aucun_reservations_detail' => 'No booking detail',
 	'info_nb_reservations_details' => '@nb@ booking details',
 
-    //L
-    'label_descriptif'=>'Descriptif',
+	// L
+	'label_descriptif' => 'Descriptif',
 
 	// T
-
 	'titre_reservations_detail' => 'Booking detail',
-	'titre_reservations_details' => 'Booking detail',
+	'titre_reservations_details' => 'Booking detail'
 );
 
 ?>
