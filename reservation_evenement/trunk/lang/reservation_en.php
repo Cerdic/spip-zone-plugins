53a07237a3f9cca31f26cb717825b481360fc130
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-07 04:38:49 +0000

    [Salvatore] [source:_plugins_/reservation_evenement/trunk/lang/ reservation] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@78029 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- reservation_evenement/trunk/lang/reservation_en.php
+++ reservation_evenement/trunk/lang/reservation_en.php
@@ -1,24 +1,25 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/reservation?lang_cible=en
+// ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
-
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
 	'ajouter_lien_reservation' => 'Add a booking',
 
-    // B
-    'bonjour' => 'Hello',
-    
-    //D
-    'details_reservation' => 'Booking details:',
-    'designation' => 'Description',
-	
-    //E
-    'explication_login'=>'<a rel="nofollow" class="login_modal" href="@url@" title="@titre_login@">Please log in</a> if you are already registered on this site',    
-    'erreur_email_utilise'=>'This email address is already in use, please log in using this email as login or take another email',
+	// B
+	'bonjour' => 'Hello',
+
+	// D
+	'designation' => 'Description',
+	'details_reservation' => 'Booking details:',
+
+	// E
+	'erreur_email_utilise' => 'This email address is already in use, please log in using this email as login or take another email',
+	'explication_login' => '<a rel="nofollow" class="login_modal" href="@url@" title="@titre_login@">Please log in</a> if you are already registered on this site',
 
 	// I
 	'icone_creer_reservation' => 'Create a booking',
@@ -33,82 +34,79 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'label_date_paiement' => 'Payment date:',
 	'label_donnees_auteur' => 'Information author:',
 	'label_email' => 'Email:',
-	'label_enregistrer' => 'I want to register on this site:',	
+	'label_enregistrer' => 'I want to register on this site:',
 	'label_id_auteur' => 'Id auteur:',
 	'label_maj' => 'Update:',
 	'label_mot_passe' => 'Password:',
-	'label_mot_passe2' => 'Repeat the password:',		
+	'label_mot_passe2' => 'Repeat the password:',
 	'label_nom' => 'Name:',
 	'label_reference' => 'Reference:',
 	'label_statut' => 'Status',
-	'label_statut_defaut' => 'Default Status:',	
+	'label_statut_defaut' => 'Default Status:',
 	'label_type_paiement' => 'Payment type:',
-	
-    //M
-    'merci_de_votre_reservation' => 'Thank you, your booking has been registered.',
-    'montant' => 'Amount',
-    
-    
-    //N
-    'notifications_activer_explication' => 'Send email alerts of bookings?',
-    'notifications_activer_label' => 'Activate',
-    'notifications_cfg_titre' => 'Alerts',
-    'notifications_client_explication' => 'Send the alerts to the customer?',
-    'notifications_client_label' => 'Client',
-    'notifications_expediteur_administrateur_label' => 'Select an administrator:',
-    'notifications_expediteur_choix_administrateur' => 'un administrateur',
-    'notifications_expediteur_choix_email' => 'an e-mail',
-    'notifications_expediteur_choix_facteur' => 'idem plugin Mailman',
-    'notifications_expediteur_choix_webmaster' => 'a webmaster',
-    'notifications_expediteur_email_label' => 'E-mail of sender:',
-    'notifications_expediteur_explication' => 'Choose the alerts\' sender for the seller and buyer',
-    'notifications_expediteur_label' => 'Sender',
-    'notifications_expediteur_webmaster_label' => 'Select a webmaster:',
-    'notifications_explication' => 'Alerts are used to send emails after changes in the booking status : in Waitinglist, accepted, refused, in the dustbin',
-    'notifications_parametres' => 'Alert settings',
-    'notifications_quand_explication' => 'Which status change triggers the sending of an alert?',
-    'notifications_quand_label' => 'Trigger',
-    'notifications_vendeur_administrateur_label' => 'Select one or more administrators:',
-    'notifications_vendeur_choix_administrateur' => 'one or more administrators',
-    'notifications_vendeur_choix_email' => 'one or more emails',
-    'notifications_vendeur_choix_webmaster' => 'one or more webmasters',
-    'notifications_vendeur_email_explication' => 'Enter one or more email separated by commas:',
-    'notifications_vendeur_email_label' => 'Emails for vendor alerts:',
-    'notifications_vendeur_explication' => 'Choose (the) recipient(s) for the vendor alerts',
-    'notifications_vendeur_label' => 'Vendor',
-    'notifications_vendeur_webmaster_label' => 'Select one or more webmasters:',
+
+	// M
+	'merci_de_votre_reservation' => 'Thank you, your booking has been registered.',
+	'montant' => 'Amount',
+
+	// N
+	'notifications_activer_explication' => 'Send email alerts of bookings?',
+	'notifications_activer_label' => 'Activate',
+	'notifications_cfg_titre' => 'Alerts',
+	'notifications_client_explication' => 'Send the alerts to the customer?',
+	'notifications_client_label' => 'Client',
+	'notifications_expediteur_administrateur_label' => 'Select an administrator:',
+	'notifications_expediteur_choix_administrateur' => 'un administrateur',
+	'notifications_expediteur_choix_email' => 'an e-mail',
+	'notifications_expediteur_choix_facteur' => 'idem plugin Mailman',
+	'notifications_expediteur_choix_webmaster' => 'a webmaster',
+	'notifications_expediteur_email_label' => 'E-mail of sender:',
+	'notifications_expediteur_explication' => 'Choose the alerts’ sender for the seller and buyer',
+	'notifications_expediteur_label' => 'Sender',
+	'notifications_expediteur_webmaster_label' => 'Select a webmaster:',
+	'notifications_explication' => 'Alerts are used to send emails after changes in the booking status : in Waitinglist, accepted, refused, in the dustbin',
+	'notifications_parametres' => 'Alert settings',
+	'notifications_quand_explication' => 'Which status change triggers the sending of an alert?',
+	'notifications_quand_label' => 'Trigger',
+	'notifications_vendeur_administrateur_label' => 'Select one or more administrators:',
+	'notifications_vendeur_choix_administrateur' => 'one or more administrators',
+	'notifications_vendeur_choix_email' => 'one or more emails',
+	'notifications_vendeur_choix_webmaster' => 'one or more webmasters',
+	'notifications_vendeur_email_explication' => 'Enter one or more email separated by commas:',
+	'notifications_vendeur_email_label' => 'Emails for vendor alerts:',
+	'notifications_vendeur_explication' => 'Choose (the) recipient(s) for the vendor alerts',
+	'notifications_vendeur_label' => 'Vendor',
+	'notifications_vendeur_webmaster_label' => 'Select one or more webmasters:',
 
 	// R
-    'recapitulatif' => 'Booking summary:',	
-    'reservation_client' => 'Client',
-    'reservation_date' => 'Date:',
-    'reservation_de' => 'Booking from',     
-    'reaservation_montant' => 'Amount',
-    'reaservation_nouvelle' => 'New booking',
-    'reservation_numero' => 'Booking:',
-    'reservation_reference_numero' => 'Reference Nr ',
-	
-
+	'reaservation_montant' => 'Amount',
+	'reaservation_nouvelle' => 'New booking',
+	'recapitulatif' => 'Booking summary:',
+	'reservation_client' => 'Client',
+	'reservation_date' => 'Date:',
+	'reservation_de' => 'Booking from',
+	'reservation_numero' => 'Booking:',
+	'reservation_reference_numero' => 'Reference Nr ',
 
 	// T
 	'texte_ajouter_reservation' => 'Add a booking',
 	'texte_changer_statut_reservation' => 'This booking is:',
-	'texte_exporter' => 'export',	
-    'texte_statut_attente'    => 'in waiting list',
-    'texte_statut_accepte'     => 'accepted',
-    'texte_statut_encours'     => 'ongoing',    
-    'texte_statut_refuse'   => 'refused',
-    'texte_statut_poubelle' => 'in the dustbin',			
-	'texte_voir' => 'view',	
+	'texte_exporter' => 'export',
+	'texte_statut_accepte' => 'accepted',
+	'texte_statut_attente' => 'in waiting list',
+	'texte_statut_encours' => 'ongoing',
+	'texte_statut_poubelle' => 'in the dustbin',
+	'texte_statut_refuse' => 'refused',
+	'texte_voir' => 'view',
 	'titre_reservation' => 'Booking',
 	'titre_reservations' => 'Bookings',
 
+	// U
+	'une_reservation_de' => 'A booking from: ',
+	'une_reservation_sur' => 'A booking on @nom@',
 
-    // U
-    'une_reservation_de' => 'A booking from: ',
-    'une_reservation_sur' => 'A booking on @nom@',
+	// V
+	'votre_reservation_sur' => '@nom@: your booking'
+);
 
-    // V
-    'votre_reservation_sur' => '@nom@: your booking'
-    );
 ?>
--- reservation_evenement/trunk/lang/reservation_fr.php
+++ reservation_evenement/trunk/lang/reservation_fr.php
@@ -1,25 +1,24 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/reservation_evenement/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
-
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
-	'affichage_par' => 'Affichage par :',	
+	'affichage_par' => 'Affichage par :',
 	'ajouter_lien_reservation' => 'Ajouter cette réservation',
 
-    // B
-    'bonjour' => 'Bonjour',
-    
-    //D
-    'details_reservation' => 'Détails de la réservation :',
-    'designation' => 'Désignation',
-	
-    //E
-    'explication_login'=>'<a rel="nofollow" class="login_modal" href="@url@" title="@titre_login@">Connectez-vous</a> si vous êtes déjà enregistré sur ce site',    
-    'erreur_email_utilise'=>'Cette adresse email est déjà utilisé, veuillez vous connecter ou utliser une autre adresse',
+	// B
+	'bonjour' => 'Bonjour',
+
+	// D
+	'designation' => 'Désignation',
+	'details_reservation' => 'Détails de la réservation :',
+
+	// E
+	'erreur_email_utilise' => 'Cette adresse email est déjà utilisé, veuillez vous connecter ou utliser une autre adresse',
+	'explication_login' => '<a rel="nofollow" class="login_modal" href="@url@" title="@titre_login@">Connectez-vous</a> si vous êtes déjà enregistré sur ce site',
 
 	// I
 	'icone_creer_reservation' => 'Créer une réservation',
@@ -30,93 +29,92 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'info_reservations_auteur' => 'Les réservations de cet auteur',
 
 	// L
-	'label_date' => 'Date :',
-	'label_date_paiement' => 'Date de paiement :',
-	'label_donnees_auteur' => 'Données Auteur :',
-	'label_email' => 'Email :',
-	'label_enregistrer' => 'Je veux m\'enregistrer sur ce site :',	
-	'label_id_auteur' => 'Id auteur :',
-	'label_maj' => 'maj :',
-	'label_mot_passe' => 'Mot de passe :',
-	'label_mot_passe2' => 'Répétez le mot de passe :',		
-	'label_nom' => 'Nom :',
-	'label_reference' => 'Référence :',
-	'label_reservation' => 'Réservation :',	
-	'label_statut' => 'Statut :',
-	'label_statut_defaut' => 'Statut par défaut :',	
-	'label_type_paiement' => 'Type de paiemement :',
-	
-    //M
-    'merci_de_votre_reservation' => 'Nous avons bien enregistré votre réservation et nous vous remercions de votre confiance.',
-    'montant' => 'Montant',
-    
-    
-    //N
-    'notifications_activer_explication' => 'Envoyer par mail des notifications de réservation ?',
-    'notifications_activer_label' => 'Activer',
-    'notifications_cfg_titre' => 'Notifications',
-    'notifications_client_explication' => 'Envoyer les notifications au client ?',
-    'notifications_client_label' => 'Client',
-    'notifications_expediteur_administrateur_label' => 'Choisir un administrateur :',
-    'notifications_expediteur_choix_administrateur' => 'un administrateur',
-    'notifications_expediteur_choix_email' => 'un email',
-    'notifications_expediteur_choix_facteur' => 'idem plugin Facteur',
-    'notifications_expediteur_choix_webmaster' => 'un webmestre',
-    'notifications_expediteur_email_label' => 'Saisir un email :',
-    'notifications_expediteur_explication' => 'Choisir l’expéditeur des notifications pour le vendeur et l’acheteur',
-    'notifications_expediteur_label' => 'Expéditeur',
-    'notifications_expediteur_webmaster_label' => 'Choisir un webmestre :',
-    'notifications_explication' => 'Les notifications permettent d’envoyer des emails suite aux changements de statut des réservations : Liste d\'attente, accepté, refusé, à la poubelle',
-    'notifications_parametres' => 'Paramètres des notifications',
-    'notifications_quand_explication' => 'Quel(s) changement(s) de statut déclenche(nt) l’envoi d’une notification ?',
-    'notifications_quand_label' => 'Déclenchement',
-    'notifications_vendeur_administrateur_label' => 'Choisir un ou plusieurs administrateurs :',
-    'notifications_vendeur_choix_administrateur' => 'un ou des administrateurs',
-    'notifications_vendeur_choix_email' => 'un ou des emails',
-    'notifications_vendeur_choix_webmaster' => 'un ou des webmestres',
-    'notifications_vendeur_email_explication' => 'Saisir un ou plusieurs email séparés par des virgules :',
-    'notifications_vendeur_email_label' => 'Email(s) :',
-    'notifications_vendeur_explication' => 'Choisir le(s) destinataire(s) des notifications pour les envois au vendeur',
-    'notifications_vendeur_label' => 'Vendeur',
-    'notifications_vendeur_webmaster_label' => 'Choisir un ou plusieurs webmestres :',
+	'label_date' => 'Date :',
+	'label_date_paiement' => 'Date de paiement :',
+	'label_donnees_auteur' => 'Données Auteur :',
+	'label_email' => 'Email :',
+	'label_enregistrer' => 'Je veux m’enregistrer sur ce site :',
+	'label_id_auteur' => 'Id auteur :',
+	'label_maj' => 'maj :',
+	'label_mot_passe' => 'Mot de passe :',
+	'label_mot_passe2' => 'Répétez le mot de passe :',
+	'label_nom' => 'Nom :',
+	'label_reference' => 'Référence :',
+	'label_reservation' => 'Réservation :',
+	'label_statut' => 'Statut :',
+	'label_statut_defaut' => 'Statut par défaut :',
+	'label_type_paiement' => 'Type de paiemement :',
+
+	// M
+	'merci_de_votre_reservation' => 'Nous avons bien enregistré votre réservation et nous vous remercions de votre confiance.',
+	'montant' => 'Montant',
+
+	// N
+	'notifications_activer_explication' => 'Envoyer par mail des notifications de réservation ?',
+	'notifications_activer_label' => 'Activer',
+	'notifications_cfg_titre' => 'Notifications',
+	'notifications_client_explication' => 'Envoyer les notifications au client ?',
+	'notifications_client_label' => 'Client',
+	'notifications_expediteur_administrateur_label' => 'Choisir un administrateur :',
+	'notifications_expediteur_choix_administrateur' => 'un administrateur',
+	'notifications_expediteur_choix_email' => 'un email',
+	'notifications_expediteur_choix_facteur' => 'idem plugin Facteur',
+	'notifications_expediteur_choix_webmaster' => 'un webmestre',
+	'notifications_expediteur_email_label' => 'Saisir un email :',
+	'notifications_expediteur_explication' => 'Choisir l’expéditeur des notifications pour le vendeur et l’acheteur',
+	'notifications_expediteur_label' => 'Expéditeur',
+	'notifications_expediteur_webmaster_label' => 'Choisir un webmestre :',
+	'notifications_explication' => 'Les notifications permettent d’envoyer des emails suite aux changements de statut des réservations : Liste d’attente, accepté, refusé, à la poubelle',
+	'notifications_parametres' => 'Paramètres des notifications',
+	'notifications_quand_explication' => 'Quel(s) changement(s) de statut déclenche(nt) l’envoi d’une notification ?',
+	'notifications_quand_label' => 'Déclenchement',
+	'notifications_vendeur_administrateur_label' => 'Choisir un ou plusieurs administrateurs :',
+	'notifications_vendeur_choix_administrateur' => 'un ou des administrateurs',
+	'notifications_vendeur_choix_email' => 'un ou des emails',
+	'notifications_vendeur_choix_webmaster' => 'un ou des webmestres',
+	'notifications_vendeur_email_explication' => 'Saisir un ou plusieurs email séparés par des virgules :',
+	'notifications_vendeur_email_label' => 'Email(s) :',
+	'notifications_vendeur_explication' => 'Choisir le(s) destinataire(s) des notifications pour les envois au vendeur',
+	'notifications_vendeur_label' => 'Vendeur',
+	'notifications_vendeur_webmaster_label' => 'Choisir un ou plusieurs webmestres :',
+
+	// P
+	'par_articles' => 'articles',
+	'par_evenements' => 'événements',
+	'par_reservations' => 'réservations',
 
 	// R
-    'remerciement'=>'Nous vous remercions pour votre inscription<br/>Cordialement',    
-    'recapitulatif' => 'Récapitulatif de la réservation :',	
-    'reservation_client' => 'Client',
-    'reservation_date' => 'Date :',
-    'reservation_de' => 'Réservation de',     
-    'reservation_enregistre'=>'Votre Inscription a bien été enregistré. Vous recevrez un email de confirmation. Si aucun mail ne vous est parvenu, verifiez dans votre dossier spam.',    
-    'reaservation_montant' => 'Montant',
-    'reaservation_nouvelle' => 'Nouvelle réservation',
-    'reservation_numero' => 'Réservation :',
-    'reservation_reference_numero' => 'Référence n° ',
-	
-    //P
-    'par_articles'=>'articles',
-    'par_evenements'=>'événements',
-    'par_reservations'=>'réservations',    
+	'reaservation_montant' => 'Montant',
+	'reaservation_nouvelle' => 'Nouvelle réservation',
+	'recapitulatif' => 'Récapitulatif de la réservation :',
+	'remerciement' => 'Nous vous remercions pour votre inscription<br/>Cordialement',
+	'reservation_client' => 'Client',
+	'reservation_date' => 'Date :',
+	'reservation_de' => 'Réservation de',
+	'reservation_enregistre' => 'Votre Inscription a bien été enregistré. Vous recevrez un email de confirmation. Si aucun mail ne vous est parvenu, verifiez dans votre dossier spam.',
+	'reservation_numero' => 'Réservation :',
+	'reservation_reference_numero' => 'Référence n° ',
 
 	// T
 	'texte_ajouter_reservation' => 'Ajouter une réservation',
 	'texte_changer_statut_reservation' => 'Cette réservation est :',
-	'texte_exporter' => 'exporter',	
-    'texte_statut_attente'    => ' dans liste d\'attente',
-    'texte_statut_attente_paiement'    => ' en attente du paiement',    
-    'texte_statut_accepte'     => ' accepté',
-    'texte_statut_encours'     => ' en cours',    
-    'texte_statut_refuse'   => ' refusé',
-    'texte_statut_poubelle' => ' à la poubelle',			
-	'texte_voir' => 'voir',	
+	'texte_exporter' => 'exporter',
+	'texte_statut_accepte' => ' accepté',
+	'texte_statut_attente' => ' dans liste d’attente',
+	'texte_statut_attente_paiement' => ' en attente du paiement',
+	'texte_statut_encours' => ' en cours',
+	'texte_statut_poubelle' => ' à la poubelle',
+	'texte_statut_refuse' => ' refusé',
+	'texte_voir' => 'voir',
 	'titre_reservation' => 'Réservation',
 	'titre_reservations' => 'Réservations',
 
+	// U
+	'une_reservation_de' => 'Une réservation de : ',
+	'une_reservation_sur' => 'Une réservation sur @nom@',
 
-    // U
-    'une_reservation_de' => 'Une réservation de : ',
-    'une_reservation_sur' => 'Une réservation sur @nom@',
+	// V
+	'votre_reservation_sur' => '@nom@ : votre réservation'
+);
 
-    // V
-    'votre_reservation_sur' => '@nom@ : votre réservation'
-    );
 ?>
