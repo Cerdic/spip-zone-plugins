af6f8058a7d0f1aa65091ade3bf38c8caa4ccd23
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-07 04:38:53 +0000

    [Salvatore] [source:_plugins_/reservation_evenement/trunk/lang/ reservation_evenement] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@78030 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- reservation_evenement/trunk/lang/reservation_evenement_de.php
+++ reservation_evenement/trunk/lang/reservation_evenement_de.php
@@ -1,21 +1,23 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/reservation_evenement?lang_cible=de
+// ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
- 
+
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cfg_titre_parametrages' => 'Einstellungen',
+
+	// L
+	'label_statut_defaut' => 'Standartstatus einer Anmeldunf',
 
 	// R
 	'reservation_evenement_titre' => 'Event Anmeldung',
 
-	// C
-	'cfg_titre_parametrages' => 'Einstellungen',
-	
-    //L
-    'label_statut_defaut' => 'Standartstatus einer Anmeldunf', 
-
 	// T
-	'titre_page_configurer_reservation_evenement' => 'Einstellungen Event Anmeldung',
+	'titre_page_configurer_reservation_evenement' => 'Einstellungen Event Anmeldung'
 );
 
 ?>
--- reservation_evenement/trunk/lang/reservation_evenement_en.php
+++ reservation_evenement/trunk/lang/reservation_evenement_en.php
@@ -1,21 +1,23 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/reservation_evenement?lang_cible=en
+// ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
- 
+
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cfg_titre_parametrages' => 'Settings',
+
+	// L
+	'label_statut_defaut' => 'Default status of a booking',
 
 	// R
 	'reservation_evenement_titre' => 'Event bookings',
 
-	// C
-	'cfg_titre_parametrages' => 'Settings',
-	
-    //L
-    'label_statut_defaut' => 'Default status of a booking', 
-
 	// T
-	'titre_page_configurer_reservation_evenement' => 'Settings event bookings',
+	'titre_page_configurer_reservation_evenement' => 'Settings event bookings'
 );
 
 ?>
