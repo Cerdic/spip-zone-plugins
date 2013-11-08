c0a296c2e6cd6b66d86e1c614b62ac663362a1aa
severo@rednegra.net <severo@rednegra.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-08 15:56:07 +0000

    On merge la branche option_adding, qui permet, quand on utilise chosen, de créer directement des nouveaux mots par javascript.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@78062 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- diogene/diogene_complements/diogene_mots/trunk/lang/diogene_mots_fr.php
+++ diogene/diogene_complements/diogene_mots/trunk/lang/diogene_mots_fr.php
@@ -9,7 +9,11 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'form_legend' => 'Sélecteur de mots-clés',
 	'form_legend_public' => 'Mots-clés',
 
+	// E
+	'erreur_confirmer_creation_mots_nouveaux' => 'Confirmer tout de même la création dans ce groupe ?',
+	'erreur_mot_dans_autre_groupe' => "Le mot @mot@ existe déjà dans le groupe @groupe@. ",
 	// L
+	'label_mots_creer_dans_public' => "Permettre la création de nouveaux mots dans ces groupes",
 	'label_mots_facultatifs' => 'Groupes de mots facultatifs',
 	'label_mots_obligatoires' => 'Groupes de mots obligatoires'
 );
