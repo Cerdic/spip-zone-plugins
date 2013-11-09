201c185ede427337b0a35e7dfeb71bec0001a737
pierrekuhn82@gmail.com <pierrekuhn82@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-09 10:04:19 +0000

    Une balise #URL_DOCUMENT_ABOSLU qui fonctionne.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@78125 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- multidomaines/trunk/balise/url_document_absolu.php
+++ multidomaines/trunk/balise/url_document_absolu.php
@@ -2,12 +2,7 @@
 
 function balise_URL_DOCUMENT_ABSOLU_dist($p) {
 	include_spip('balise/url_');
-	$id_breve = interprete_argument_balise(1,$p);
-	if (strlen(trim($id_breve))==0)
-	{
-		$id_breve = calculer_balise('id_document', $p)->code;
-	}
-	$p->code = "calculer_URL_SECTEUR(sinon(sql_getfetsel('id_article','spip_documents','id_document' . intval($id_document)),0)).".generer_generer_url('document', $p);
+	$p->code = 'lire_config("multidomaines/editer_url").'.generer_generer_url('document', $p);
 	$p->interdire_scripts = false;
 	return $p;
 }
