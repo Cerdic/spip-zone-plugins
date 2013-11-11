ee53997e12ad3c6116cddf355e5b788b522eefb4
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-11 15:22:22 +0000

    Ajout des fichiers de langue du plugin et du paquet.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@78382 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ questions_reponses/trunk/lang/paquet-qr_fr.php
@@ -0,0 +1,13 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// T
+	'qr_description' => 'Ajoute de nouveaux raccourcis typographiques permettant de décrire de manière simple des listes de questions-réponses (FAQ) dans un contenu SPIP.',
+	'qr_nom' => 'Questions-Réponses',
+	'qr_slogan' => 'Créer simplement une liste de questions et de réponses'
+);
+
+?>
---
+++ questions_reponses/trunk/lang/qr_fr.php
@@ -0,0 +1,12 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	// O
+	'outil_inserer_qr' => 'Insérer une nouvelle FAQ',
+	'outil_inserer_question' => 'Insérer une nouvelle question',
+	'outil_inserer_titre' => 'Insérer un titre pour la FAQ',
+);
+
+?>
