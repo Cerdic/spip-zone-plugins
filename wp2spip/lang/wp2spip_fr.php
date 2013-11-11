fd3075c495c65e360b162fd6db9c23525108f4d8
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-11 12:00:29 +0000

    On va internationaliser certaines choses
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@78367 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ wp2spip/lang/wp2spip_fr.php
@@ -0,0 +1,24 @@
+<?php
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	// A
+	'alerte_generation_fichier' => 'Le fichier de restauration sera place dans le répertoire tmp/dump',
+
+	// C
+	
+	// E
+	'erreur_fichier_non_cree' => 'Le fichier de restauration n\'a pas été créé.',
+	
+	// L
+	'label_generer_fichier' => 'Générer le fichier au format SPIP',
+	
+	// M
+	'message_attention_import' => 'Attention, cette restauration remplacera votre base SPIP actuelle.',
+	'message_conversion_ok' => 'Base Wordpress convertie au format xml SPIP 2.1.12 !',
+	
+	// T
+	'titre_page_migration' => 'Migration Wordpress 2 SPIP',
+);
+?>
