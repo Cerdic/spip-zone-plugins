f030be42b60f02876bd69d33712d90d798f08ca3
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-11 14:59:44 +0000

    Utiliser la balise #PLUGIN plutot qu'une boucle sur spip_meta
    Améliorer la pages de stats + internationalisation
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@78379 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- wp2spip/lang/wp2spip_fr.php
+++ wp2spip/lang/wp2spip_fr.php
@@ -9,16 +9,33 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// C
 	
 	// E
+	'erreur_aucun_tag' => 'Aucun tag trouvé.',
 	'erreur_fichier_non_cree' => 'Le fichier de restauration n\'a pas été créé.',
+	'erreur_non_webmestre' => 'Vous devez avoir les droits de webmestre.',
 	
+	// I
+	'info_erreur_aucun_mot' => 'Aucun mot-clé trouvé!',
+	'info_erreur_aucune_rubrique' => 'Veuillez créer au moins une catégorie dans Wordpress.',
+	'info_nombre_documents' => '@nb@ documents.',
+	'info_nombre_documents_orphelins' => '@nb@ documents orphelins qui ne seront pas importés',
+	'info_nombre_posts' => '@nb@ posts ou articles publiés.',
+	'info_nombre_pages' => '@nb@ pages uniques (installez le plugin pages uniques si ce n\'est pas déjà fait, sinon ce seront des articles à classer et à publier).',
+
 	// L
 	'label_generer_fichier' => 'Générer le fichier au format SPIP',
 	
 	// M
+	'message_aller_maintenance' => 'Rendez-vous à la page <a href=\'@url@\' title=\'\'>Maintenance</a> du site et importer la base wp2spip.xml.',
 	'message_attention_import' => 'Attention, cette restauration remplacera votre base SPIP actuelle.',
 	'message_conversion_ok' => 'Base Wordpress convertie au format xml SPIP 2.1.12 !',
+	'message_notice_site_vide' => 'Votre site SPIP ne semble pas vide, tout son contenu sera écrasé si vous restaurez la base avec le fichier créé depuis Wordpress.',
+	'mot' => '1 mot-clé',
+	'mots' => '@nb@ mots-clés',
 	
 	// T
-	'titre_page_migration' => 'Migration Wordpress 2 SPIP',
+	'tag' => '1 tag',
+	'tags' => '@nb@ tags',
+	'titre_contenu_base' => 'Contenu de la base de données Wordpress',
+	'titre_page_migration' => 'Migration de Wordpress vers SPIP',
 );
 ?>
