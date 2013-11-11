b572493f86bc07ececb567fbbef0e20cd49a1a41
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-11 22:36:58 +0000

    Le cadre du plugin est posé. La liste des articles s'enrichit d'une ligne thad pour des actions et d'une colonne pour le choix. Il faut maintenant aller plus loin...
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@78402 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ actions_multiples/trunk/am_pipelines.php
@@ -0,0 +1,61 @@
+<?php
+
+// Sécurité
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+if (!defined('_AM_PATTERN_TR'))
+	define('_AM_PATTERN_TR', '%(<tr[^>]*>)(.*?)</tr>%s');
+
+function am_insert_head_css($flux) {
+//	$flux .= '<link rel="stylesheet" href="'.find_in_path('css/qr.css').'" type="text/css" media="all" />';
+	return $flux;
+}
+
+function am_header_prive($flux) {
+//	$flux .= '<script src="'.find_in_path('js/qr.js').'" type="text/javascript"></script>';
+	return $flux;
+}
+
+$GLOBALS['am_listes'] = array(
+	'prive/objets/liste/articles',
+);
+
+function am_recuperer_fond($flux) {
+	include_spip('inc/filtres');
+
+	if (in_array($flux['args']['fond'], $GLOBALS['am_listes'])) {
+		$fond = $flux['args']['fond'];
+		$texte = $flux['data']['texte'];
+		$objet = array_pop(explode('/', $fond));
+
+		if ($fond == 'prive/objets/liste/articles') {
+			// Ajout d'une colonne en première position dans le thead
+			$thead = extraire_balise($texte, 'thead');
+			if (preg_match(_AM_PATTERN_TR, $thead, $balises_tr)) {
+				$choix = recuperer_fond('inclure/am_th_choix', $contexte);
+				$texte = str_replace($balises_tr[2], $choix . $balises_tr[2], $texte);
+			}
+
+			// Ajout d'une ligne de boutons d'action en fin du thead de la table
+			$contexte = array();
+			$actions = recuperer_fond("inclure/am_$objet", $contexte);
+			$texte = str_replace('</thead>', $actions . '</thead>', $texte);
+
+			// Ajout pour chaque ligne du body d'une colonne en première position afin de faire
+			// les choix des objets.
+			// TODO : le problème est de récupérer l'id de l'objet pour le passer au fond
+			$tbody = extraire_balise($texte, 'tbody');
+			if (preg_match_all(_AM_PATTERN_TR, $tbody, $balises_tr)) {
+				foreach($balises_tr[2] as $_balise) {
+					$choix = recuperer_fond('inclure/am_td_choix', $contexte);
+					$texte = str_replace($_balise, $choix . $_balise, $texte);
+				}
+			}
+		}
+
+		$flux['data']['texte'] = $texte;
+	}
+
+	return $flux;
+}
+?>
