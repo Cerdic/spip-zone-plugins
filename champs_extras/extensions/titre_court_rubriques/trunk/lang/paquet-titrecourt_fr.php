f897127201d6f221909405ffb81c04ecddc75195
marcimat@rezo.net <marcimat@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-14 19:16:02 +0000

    Correction de coquille depuis r78380 + Autres corrections sur le préfixe dans la clé de chaîne de langue et pas le lien sur le texte d'auteur
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@78496 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- champs_extras/extensions/titre_court_rubriques/trunk/lang/paquet-titrecourt_fr.php
+++ champs_extras/extensions/titre_court_rubriques/trunk/lang/paquet-titrecourt_fr.php
@@ -1,7 +1,7 @@
 <?php
 if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-	'titre_court_nom' => 'Titre Court pour Rubriques',
+	'titrecourt_nom' => 'Titre Court pour Rubriques',
 	'titrecourt_description' => 'Ajoute un champ "titre_court" sur les rubriques de SPIP.',
 	'titrecourt_slogan' => 'Pour apprendre &agrave; ajouter un champs extra',
 	
