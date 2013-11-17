017180f005c3480f0041d6a4021de869a737b3f0
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-17 16:00:36 +0000

    sauvegarde intermédiaire
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@78564 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- actions_multiples/trunk/am_pipelines.php
+++ actions_multiples/trunk/am_pipelines.php
@@ -6,6 +6,9 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 if (!defined('_AM_PATTERN_TR'))
 	define('_AM_PATTERN_TR', '%(<tr[^>]*>)(.*?)</tr>%s');
 
+if (!defined('_AM_PATTERN_ID'))
+	define('_AM_PATTERN_ID', "%<td([^>]*(?:id=([\"'])([^\"']+)\2)[^>]*|)>\n(.*?)\n</td>%s");
+
 function am_insert_head_css($flux) {
 //	$flux .= '<link rel="stylesheet" href="'.find_in_path('css/qr.css').'" type="text/css" media="all" />';
 	return $flux;
@@ -28,34 +31,38 @@ function am_recuperer_fond($flux) {
 		$texte = $flux['data']['texte'];
 		$objet = array_pop(explode('/', $fond));
 
-		if ($fond == 'prive/objets/liste/articles') {
-			// TODO : il faut entourer la table d'un formulaire pour utiliser les checkbox
-			// -- peut etre que le plus facile serait d'inclure le texte de la table dans le formulaire
-			//    via un recuperer_fond
+		// Ajout d'une colonne en première position dans le thead pour coincider avec le tbody
+		$thead = extraire_balise($texte, 'thead');
+		if (preg_match(_AM_PATTERN_TR, $thead, $balises_tr)) {
+			$choix = recuperer_fond('inclure/am_th_choix', $contexte);
+			$texte = str_replace($balises_tr[2], $choix . $balises_tr[2], $texte);
+		}
 
-			// Ajout d'une colonne en première position dans le thead
-			$thead = extraire_balise($texte, 'thead');
-			if (preg_match(_AM_PATTERN_TR, $thead, $balises_tr)) {
-				$choix = recuperer_fond('inclure/am_th_choix', $contexte);
-				$texte = str_replace($balises_tr[2], $choix . $balises_tr[2], $texte);
-			}
+		// Ajout d'une ligne de boutons d'action en fin du thead de la table
+		$contexte = array();
+		$actions = recuperer_fond("inclure/am_$objet", $contexte);
+		$texte = str_replace('</thead>', $actions . '</thead>', $texte);
 
-			// Ajout d'une ligne de boutons d'action en fin du thead de la table
-			$contexte = array();
-			$actions = recuperer_fond("inclure/am_$objet", $contexte);
-			$texte = str_replace('</thead>', $actions . '</thead>', $texte);
-
-			// Ajout pour chaque ligne du body d'une colonne en première position afin de faire
-			// les choix des objets.
-			// TODO : le problème est de récupérer l'id de l'objet pour le passer au fond
-			$tbody = extraire_balise($texte, 'tbody');
-			if (preg_match_all(_AM_PATTERN_TR, $tbody, $balises_tr)) {
-				foreach($balises_tr[2] as $_balise) {
+		// Ajout pour chaque ligne du body d'une colonne en première position afin de faire
+		// les choix des objets.
+		// TODO : le problème est de récupérer l'id de l'objet pour le passer au fond
+		$tbody = extraire_balise($texte, 'tbody');
+		if (preg_match_all(_AM_PATTERN_TR, $tbody, $balises_tr)) {
+			foreach($balises_tr[2] as $_balise) {
+				// Trouver la valeur de l'id de l'objet en cours
+				if (preg_match(_AM_PATTERN_ID, $_balise, $matches)){
+					// Insérer la colonne de choix
+					$contexte = array('nom' => "ids_$objet", 'valeur' => 1);
 					$choix = recuperer_fond('inclure/am_td_choix', $contexte);
 					$texte = str_replace($_balise, $choix . $_balise, $texte);
 				}
 			}
 		}
+
+		// Inclure le texte de la table dans un formulaire pour choisir les objets
+		$contexte = array('liste' => $texte);
+		$choix = recuperer_fond('formulaires/actionner', $contexte);
+
 
 		$flux['data']['texte'] = $texte;
 	}
