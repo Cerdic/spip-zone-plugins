b2a881ae7784c0f7278c9cbd54af23382c27e129
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-19 04:39:26 +0000

    [Salvatore] [source:_plugins_/crayons/lang/ crayons] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@78611 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- crayons/lang/crayons_nl.php
+++ crayons/lang/crayons_nl.php
@@ -8,7 +8,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
-	'activation_barre_impossible' => 'Een andere plugin verbiedt het gebruik van pen in de openbare ruimte.',
+	'activation_barre_impossible' => 'Een andere plugin verbiedt het gebruik van De Penhouder in de publieke site.',
 	'annuler' => 'Annuleren',
 
 	// B
@@ -23,66 +23,67 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'editer_@type@_@id@' => '@type@ @id@ in geheel uitgeven',
 	'editer_tout' => 'Alles uitgeven',
 	'effets' => 'Gevolgen',
-	'explication_exec_autorise' => 'Toegestaan ​​om pagina’s te wijzigen
-  (?exec=...) : (, te scheiden exec - * voor allen).',
-	'explication_introduction_article' => 'De introductie van een functie geeft de beschrijving van het item, anders geeft de eerste 600 karakters (hoed en tekst).',
-	'explication_reduire_logo' => 'Verminder het logo als het hoger is dan 
+	'explication_exec_autorise' => 'Pagina’s die mogen worden gewijzigd
+  (?exec=...) : (, om de exec’s te scheiden - * voor alle).',
+	'explication_introduction_article' => 'De introductie toont de beschrijving  van een artikel, anders geeft het de eerste 600 tekens weer (kop en tekst).',
+	'explication_reduire_logo' => 'Verklein het logo als het hoger is dan 
    de maximale grootte in pixels. Voorbeeld: 100. (0 = nooit)',
 
 	// I
 	'info_experimental' => 'Experimenteel',
 
 	// L
-	'label_activer_barre_typo' => 'Draai het typografie roer in de potloden ?',
-	'label_activer_crayons_prive' => 'Schakel potloden voor de private partij.',
-	'label_barre_typo' => 'Typografische bar',
-	'label_clickhide' => 'Te klikken buiten het masker potloden',
-	'label_effets' => 'Selectie-effecten',
-	'label_espaceprive' => 'Potloden in de prive-sector ?',
-	'label_exec_autorise' => 'pagina’s geautoriseerde',
+	'label_activer_barre_typo' => 'De typografische knoppenbalk in De Krijtjes activeren?',
+	'label_activer_crayons_prive' => 'Schakel Krijtjes in voor het private deel.',
+	'label_barre_typo' => 'Typografische knoppenbalk',
+	'label_clickhide' => 'Een klik erbuiten verbergt Krijtjes',
+	'label_effets' => 'Effectenkeuze',
+	'label_espaceprive' => 'Krijtjes in het private deel?',
+	'label_exec_autorise' => 'Toegestane bladzijdes',
 	'label_filet' => 'Net',
-	'label_message' => 'Het krijgen van een bericht',
-	'label_msg_abandon' => 'Abandon zuid',
-	'label_msg_no_change' => 'Als er geen verandering',
-	'label_reduire_logo' => 'Verminderen logos',
-	'label_upload_document' => 'Bijvoegen van documenten door drag en drop op het article.texte potlood toelaten',
-	'label_yellow_fade' => 'Fade',
-	'legend_controleur_hyperlien' => 'Hyperlien',
-	'legend_controleur_logo' => 'Kies een logo :',
-	'legend_controleur_logo_remplacer' => 'Vervang deze logo',
-	'legend_controleur_qui' => 'Dat ?',
+	'label_message' => 'Een bericht krijgen',
+	'label_msg_abandon' => 'Bij het verlaten',
+	'label_msg_no_change' => 'Indien geen verandering',
+	'label_reduire_logo' => 'Logo’s verkleinen',
+	'label_upload_document' => 'Bijvoegen van documenten mogelijk maken via slepen en plakken op het Krijtje',
+	'label_yellow_fade' => 'Vervagen',
+	'legend_controleur_fichier_remplacer' => 'Document "@extension@" vervangen',
+	'legend_controleur_hyperlien' => 'Hyperlink',
+	'legend_controleur_logo' => 'Een logo kiezen:',
+	'legend_controleur_logo_remplacer' => 'Vervang dit logo',
+	'legend_controleur_qui' => 'Wat?',
 	'legend_controleur_vignette' => 'Selecteer een miniatuur :',
 	'legend_controleur_vignette_remplacer' => 'Vervang deze tag',
 	'legend_documents' => 'Upload van documenten',
-	'legend_editer_prive' => 'Bewerk de private deel (experimentele)',
+	'legend_editer_prive' => 'Bewerk het private deel (experimenteel)',
 	'legend_introduction_article' => 'Aanhef van artikel',
 
 	// M
-	'message_image_obligatoire' => 'U moet kiezen voor een foto',
+	'message_image_obligatoire' => 'U moet een afbeelding kiezen',
 	'messages' => 'Berichten',
 	'modifie_par_ailleurs' => 'Voorts gewijzigd',
 
 	// N
-	'non_autorise' => 'Niet - toegelaten',
-	'non_implemente' => 'Niet geïmplementeerds',
+	'non_autorise' => 'Niet toegelaten',
+	'non_implemente' => 'Niet geïmplementeerd',
 
 	// P
 	'pas_de_modification' => 'Geen wijziging',
 	'pas_de_valeur' => 'Geen waarde',
 
 	// S
-	'sauvegarder' => 'De wijzigingen beschermen?',
-	'svp_copier_coller' => 'Désolé, copiez/collez vos changements et recommencez',
+	'sauvegarder' => 'De wijzigingen opslaan?',
+	'svp_copier_coller' => 'Sorry, kopieer/plak uw wijzigingen en begin opnieuw',
 
 	// T
-	'titre_config_crayons' => 'Configureren potloden',
+	'titre_config_crayons' => 'Krijtjes configureren',
 	'titre_crayons' => 'Podloden',
 
 	// U
 	'update_impossible' => 'De update is niet geslaagd.',
 
 	// V
-	'veuillez_patienter' => 'Gelieve geduld te oefenen…'
+	'veuillez_patienter' => 'Een ogenblik geduld…'
 );
 
 ?>
