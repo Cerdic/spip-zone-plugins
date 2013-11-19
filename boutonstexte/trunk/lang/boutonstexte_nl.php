e65424e26bee47fd93d3b25fd74cc3f4514fb914
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-19 04:39:20 +0000

    [Salvatore] [source:_plugins_/boutonstexte/trunk/lang/ boutonstexte] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@78609 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- boutonstexte/trunk/lang/boutonstexte_nl.php
+++ boutonstexte/trunk/lang/boutonstexte_nl.php
@@ -10,9 +10,21 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// A
 	'augmenter_police' => 'Lettertype vergroten',
 
+	// B
+	'boutonstexte_nom' => 'Knoppen in de tekst',
+
 	// D
 	'diminuer_police' => 'Lettertype verkleinen',
 	'donnees_techniques' => 'Technnische gegevens niet te veranderen',
+
+	// L
+	'label_selector' => 'CSS of jQuery code',
+	'label_skin' => 'Uiterlijk van de knoppen',
+	'label_txtBackSpip' => 'Tekst terug SPIP',
+	'label_txtOnly' => 'Alleen tekst *',
+	'label_txtSizeDown' => 'Tekst verkleinen *',
+	'label_txtSizeUp' => 'Tekst vergroten *',
+	'legend_apparence' => 'Opmaak',
 
 	// R
 	'retour_a_spip' => 'Terug naar de volledige pagina',
