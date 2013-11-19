3ac025d124f0d0ad4b5c2b209903c652553b67a7
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-19 04:39:52 +0000

    [Salvatore] [source:_plugins_/notation/trunk/lang/ notation] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@78616 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ notation/trunk/lang/notation_nl.php
@@ -0,0 +1,122 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/notation?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'acces' => 'Toegankelijkheid',
+	'afficher_tables' => 'Toon de scores',
+	'aide' => 'Hulp',
+	'articles' => 'Artikelen',
+	'auteur' => 'Auteur',
+
+	// B
+	'bouton_radio_fermee' => 'Gesloten',
+	'bouton_radio_ouvert' => 'Open',
+	'bouton_voter' => 'Stemmen',
+
+	// C
+	'change_note_label' => 'Score mag worden aangepast',
+	'configuration_notation' => 'Scores configureren',
+	'creation' => 'Aanmaken tabellen',
+	'creation_des_tables_mysql' => 'Aanmaken tabellen',
+	'cree' => 'Tabellen aangemaakt',
+	'creer_tables' => 'Tabellen aanmaken',
+
+	// D
+	'date' => 'datum',
+	'derniers_votes' => 'Laatste scores',
+	'destruction' => 'Verwijderen tabellen',
+	'detruire' => '<strong style="color:red">Let op, dit commando vernietigt alle score-tabellen!</strong><br />Gebruik dit alleen om de score plugin opnieuw te initialiseren...',
+	'detruit' => 'Tabellen verwijderd...',
+
+	// E
+	'effacer_tables' => 'Tabellen wissen',
+	'err_balise' => '[ NOTATION_ERR: lus buiten artikel ]',
+	'err_db_notation' => '[ NOTATION ERREUR: slechts een enkele score per artikel ]',
+	'exemple' => 'Verdeling scores (score = 5, weegfactor = @ponderation@) : ',
+	'explication_accepter_note' => 'Indien "gesloten" kan score per object worden geactiveerd.',
+
+	// I
+	'info_acces' => 'Score openen: ',
+	'info_etoiles' => 'Hier kun je de maximale score aangeven (het aantal sterren tussen 1 en 10 met 5 als standaardwaarde).<br />
+                    <strong style="color:red">/!\\ Let op</strong>: verander deze waarde niet achteraf, want bestaande scores worden niet aangepast...<br />
+                    Wanneer deze parameter is ingesteld, moet hij niet meer worden veranderd.',
+	'info_fonctionnement_note' => 'Geven van scores',
+	'info_ip' => 'Om scores zo eenvoudig mogelijk te controleren, worden ze geregistreerd op het IP-adres van de stemmer. Dit voorkomt meerdere stemmen per gebruiker.<br />
+                Auteurs stemmen met hun ID.<br />
+                Voor een enkele stem per gebruiker, beperk je stemming tot een <b>enkele</b> geregistreerde persoon (hierboven).',
+	'info_modifications' => 'Scores aanpassen',
+	'info_ponderation' => 'De weegfactor geeft meer waarde aan items die voldoende stemmen hebben.<br />Geef het minimum aantal stemmen om de score betrouwbaar te laten zijn.',
+	'ip' => 'IP',
+	'item_adm' => 'voor beheerders ',
+	'item_all' => 'voor iedereen ',
+	'item_aut' => 'voor auteurs ',
+	'item_id' => 'enkele stem per gebruiker ',
+	'item_ide' => 'voor geregistreerde personen ',
+	'item_ip' => 'een stem per IP-adres',
+
+	// J
+	'jaidonnemonavis' => 'Ik heb mijn oordeel gegeven!',
+	'jaime' => 'Leuk',
+	'jaimepas' => 'Niet leuk',
+	'jaimeplus' => 'Niet leuk meer',
+	'jechangedavis' => 'Ik trek mijn stem in',
+
+	// L
+	'label_accepter_note' => 'Status score op alle objecten',
+
+	// M
+	'moyenne' => 'Gemiddelde',
+	'moyennep' => 'Gewogen gemiddelde',
+
+	// N
+	'nb_etoiles' => 'Waarde van de scores',
+	'nbobjets_note' => 'Aantal objecten met een score: ',
+	'nbvotes' => 'Stemmen',
+	'nbvotes_moyen' => 'Gemiddeld aantal stemmen per object: ',
+	'nbvotes_total' => 'Totaal aantal stemmen op de site: ',
+	'notation' => 'Scores',
+	'note' => 'Score: ',
+	'note_1' => 'Score: 1',
+	'note_10' => 'Score: 10',
+	'note_2' => 'Score: 2',
+	'note_3' => 'Score: 3',
+	'note_4' => 'Score: 4',
+	'note_5' => 'Score: 5',
+	'note_6' => 'Score: 6',
+	'note_7' => 'Score: 7',
+	'note_8' => 'Score: 8',
+	'note_9' => 'Score: 9',
+	'note_pond' => 'Gewogen score',
+	'notes' => 'Scores',
+
+	// O
+	'objets' => 'Objecten',
+
+	// P
+	'param' => 'Parameters',
+	'ponderation' => 'Weging van de Score',
+
+	// T
+	'titre_ip' => 'Instelling aanpassen:',
+	'topnb' => 'De 10 meest beoordeelde objecten',
+	'topten' => 'De 10 beste scores',
+	'toptenp' => 'De 10 beste gewogen scores',
+	'totaux' => 'Totaal',
+
+	// V
+	'valeur_nb_etoiles' => 'Score van 1 tot ',
+	'valeur_ponderation' => 'Weegfactor',
+	'vos_notes' => 'Je 5 beste scores',
+	'vote' => 'stem',
+	'voter' => 'Stemmen: ',
+	'votes' => 'stemmen',
+	'votre_note' => 'Jouw beoordeling'
+);
+
+?>
