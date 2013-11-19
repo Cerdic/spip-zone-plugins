0dc035ccb1763f3ac70423d2734a37e28a858c6c
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-19 14:02:43 +0000

    Ben non… autant définir les autorisations par défaut pour n'importe quel type de collection, donc pour n'importe quels objets. Donc on enlève le $quoi du nom des fonctions.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@78628 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- http/trunk/http_autorisations.php
+++ http/trunk/http_autorisations.php
@@ -5,23 +5,24 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 
 function http_autoriser($flux){return $flux;}
 
-// Voir une liste d'articles par HTTP : tout le monde a le droit de voir des listes d'articles
-function autoriser_article_get_collection_dist($faire, $quoi, $id, $qui, $options){
+// Voir une liste d'objet par HTTP : tout le monde a le droit de voir des listes
+function autoriser_get_collection_dist($faire, $quoi, $id, $qui, $options){
 	return true;
 }
-// Voir un article par HTTP : on redirige vers la fonction pour voir un article
-function autoriser_article_get_ressource_dist($faire, $quoi, $id, $qui, $options){
-	return autoriser('voir', 'article', $id, $qui, $options);
+// Voir un objet par HTTP : on redirige vers la fonction pour voir l'objet
+function autoriser_get_ressource_dist($faire, $quoi, $id, $qui, $options){
+	return autoriser('voir', $quoi, $id, $qui, $options);
 }
-// Ajouter un article par HTTP : on redirige vers la création d'article
-function autoriser_article_post_collection_dist($faire, $quoi, $id, $qui, $options){
-	return autoriser('creer', 'article', $id, $qui, $options);
+// Ajouter un objet par HTTP : on redirige vers la création de l'objet
+function autoriser_post_collection_dist($faire, $quoi, $id, $qui, $options){
+	return autoriser('creer', $quoi, $id, $qui, $options);
 }
-// Modifier un article par HTTP : on redirige vers la modification
-function autoriser_article_put_ressource_dist($faire, $quoi, $id, $qui, $options){
-	return autoriser('modifier', 'article', $id, $qui, $options);
+// Modifier un objet par HTTP : on redirige vers la modification
+function autoriser_put_ressource_dist($faire, $quoi, $id, $qui, $options){
+	return autoriser('modifier', $quoi, $id, $qui, $options);
 }
-// Supprimer un article par HTTP : on redirige vers l'institution de l'article
-function autoriser_article_delete_ressource_dist($faire, $quoi, $id, $qui, $options){
-	return autoriser('instituer', 'article', $id, $qui, $options);
+// Supprimer un objet par HTTP : soit il existe une autorisation de suppression soit sinon l'institution
+function autoriser_delete_ressource_dist($faire, $quoi, $id, $qui, $options){
+	return autoriser('supprimer', $quoi, $id, $qui, $options) or autoriser('instituer', $quoi, $id, $qui, $options);
 }
+
