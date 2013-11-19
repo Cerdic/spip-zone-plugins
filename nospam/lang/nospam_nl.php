4e4b8422aa9f6d5c8fa4f3ec6f253e0eb9341124
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-19 04:39:50 +0000

    [Salvatore] [source:_plugins_/nospam/lang/ nospam] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@78615 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ nospam/lang/nospam_nl.php
@@ -0,0 +1,29 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/nospam?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// E
+	'erreur_attributs_html_interdits' => 'HTML-attributen <tt>class</tt> of <tt>style</tt> zijn niet toegestaan!',
+	'erreur_blacklist' => 'U staat op de zwarte lijst en mag geen verdere berichten plaatsen.',
+	'erreur_jeton' => 'Dit bericht kan niet worden aanvaard. Voer het nogmaals in!',
+	'erreur_spam' => 'Dit bericht kan niet worden aanvaard!',
+	'erreur_spam_doublon' => 'Er bestaat al een identiek bericht!',
+	'erreur_spam_ip' => 'Trop de commentaires nuit à la qualité !',
+	'erreur_url_deja_spammee' => 'Dit bericht bevat koppelingen die op SPAM lijken. Verwijder ze aub.',
+
+	// F
+	'forum_saisie_texte_info' => 'Dit formulier accepteert alleen SPIP code <code>[-&gt;url] {{gras}} {italique} &lt;quote&gt; &lt;code&gt;</code> &lt;cadre&gt;</cadre> en HTML code <code>&lt;q&gt; &lt;del&gt; &lt;ins&gt;</code>. Op paragrafen aan te maken, kun je gewoon een paar regels leeg laten.',
+
+	// I
+	'info_ip_suspecte' => 'Uw IP-adres wordt ervan verdacht SPAM te verzenden. Laat uw goede bedoelingen blijken.',
+
+	// L
+	'label_message_licite' => 'Dit is geen verkeerd bericht'
+);
+
+?>
