b40ac61e1ae13012b5f5928d5f16e660e4feba65
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-19 04:39:54 +0000

    [Salvatore] [source:_plugins_/notation/trunk/lang/ paquet-notation] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@78617 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ notation/trunk/lang/paquet-notation_nl.php
@@ -0,0 +1,18 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-notation?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// N
+	'notation_description' => 'Stelt de tag <code>#FORMULAIRE_NOTATION</code> in werking die de registratie van beoordelingen toelaat.
+_ Het model <code><autxx|notation|id_article=yy></code> geeft de beoordeling voor auteur <i>xx</i> voor artikel <i>yy</i>
+en <code><notation|topten></code> toont de 10 best beoordeelde artikelen.',
+	'notation_nom' => 'Scores',
+	'notation_slogan' => 'Geef een beoordeling voor SPIP elementen'
+);
+
+?>
