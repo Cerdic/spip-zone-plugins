b7501d7fa948c38420c369671aa3f3befde9fd0a
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-20 04:40:13 +0000

    [Salvatore] [source:_plugins_/mesfavoris/lang/ paquet-mesfavoris] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@78646 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ mesfavoris/lang/paquet-mesfavoris_nl.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-mesfavoris?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// M
+	'mesfavoris_description' => 'Selecteer een object om objecten (artikelen, rubrieken ...) als favoriet aan te merken voor een aangemelde bezoeker',
+	'mesfavoris_slogan' => 'Keuze van favorieten'
+);
+
+?>
