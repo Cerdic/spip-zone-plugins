60399ef89e305e0a71c14a201fdddaec5304fdac
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-20 04:40:38 +0000

    [Salvatore] [source:_plugins_/tooltip/lang/ tooltip] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@78653 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ tooltip/lang/tooltip_nl.php
@@ -0,0 +1,21 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/tooltip?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// E
+	'explication_selecteur' => 'Vermeld de doel-elementen die tooltips activeren. (uitgebreide jQuery of CSS expressie)',
+
+	// L
+	'label_selecteur' => 'Selector',
+
+	// T
+	'titre_menu_tooltips' => 'Tooltips',
+	'titre_page_configurer_tooltips' => 'Configuratie van Tooltips'
+);
+
+?>
