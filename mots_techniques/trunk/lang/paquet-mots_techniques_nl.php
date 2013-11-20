de9a6ff6a8d36e877b0ac56a31eaf4c421333e82
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-20 04:40:23 +0000

    [Salvatore] [source:_plugins_/mots_techniques/trunk/lang/ paquet-mots_techniques] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@78650 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ mots_techniques/trunk/lang/paquet-mots_techniques_nl.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-mots_techniques?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// M
+	'mots_techniques_description' => 'Standaard filteren de lussen MOTS en GROUPES_MOTS technische trefwoorden uit. Door toevoeging van <code>{tout}</code> kun je toch alle trefwoorden tonen en de conditie <code>{technique=oui}</code> filtert op technische trefwoorden.',
+	'mots_techniques_slogan' => 'Voeg de specificatie "technisch" toe aan groepen van trefwoorden.',
+	'mots_techniques_titre' => 'Technische trefwoorden'
+);
+
+?>
