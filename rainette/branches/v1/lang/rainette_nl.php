9e5ea34c6975371d8434f1e4489a09ef43b4aa4f
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-20 04:40:32 +0000

    [Salvatore] [source:_plugins_/rainette/branches/v1/lang/ rainette] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@78652 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ rainette/branches/v1/lang/rainette_nl.php
@@ -0,0 +1,148 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/rainette?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'coucher_soleil' => 'zon onder',
+
+	// D
+	'demain' => 'morgen',
+	'derniere_maj' => 'aangepast',
+	'direction_E' => 'oost',
+	'direction_ENE' => 'oost noord-oost',
+	'direction_ESE' => 'oost zuid-oost',
+	'direction_N' => 'noord',
+	'direction_NE' => 'noord-oost',
+	'direction_NNE' => 'noord noord-oost',
+	'direction_NNW' => 'noord noord-west',
+	'direction_NW' => 'noord-west',
+	'direction_S' => 'zuid',
+	'direction_SE' => 'zuid-oost',
+	'direction_SSE' => 'zuid zuid-oost',
+	'direction_SSW' => 'zuid zuid-west',
+	'direction_SW' => 'zuid-west',
+	'direction_W' => 'west',
+	'direction_WNW' => 'west noord-west',
+	'direction_WSW' => 'west zuid-west',
+
+	// H
+	'humidite' => 'vochtigheid',
+
+	// J
+	'jour' => 'dag',
+
+	// L
+	'latitude' => 'breedte',
+	'lever_soleil' => 'zonsopgang',
+	'longitude' => 'lengte',
+
+	// M
+	'meteo' => 'weer',
+	'meteo_0' => 'tornado',
+	'meteo_1' => 'tropische storm',
+	'meteo_10' => 'aanvriezende regen',
+	'meteo_11' => 'regenbuien',
+	'meteo_12' => 'regen',
+	'meteo_13' => 'enkele sneeuwvlokken',
+	'meteo_14' => 'lichte sneeuwbuien',
+	'meteo_15' => 'stuifsneeuw',
+	'meteo_16' => 'sneeuw',
+	'meteo_17' => 'hagel',
+	'meteo_18' => 'natte sneeuw',
+	'meteo_19' => 'stof',
+	'meteo_2' => 'orkaan',
+	'meteo_20' => 'nevelig',
+	'meteo_21' => 'mist',
+	'meteo_22' => 'nevel',
+	'meteo_23' => 'windstoten',
+	'meteo_24' => 'wind',
+	'meteo_25' => 'koud',
+	'meteo_26' => 'bewolkt',
+	'meteo_27' => 'overwegend bewolkt',
+	'meteo_28' => 'zeer bewolkt',
+	'meteo_29' => 'lichtbewolkt',
+	'meteo_3' => 'hevige onweer',
+	'meteo_30' => 'overwegend zonnig',
+	'meteo_31' => 'heldere nacht',
+	'meteo_32' => 'zon',
+	'meteo_33' => 'heldere maan',
+	'meteo_34' => 'zon met enkele wolken',
+	'meteo_35' => 'regen- en hagelbuien',
+	'meteo_36' => 'hitte',
+	'meteo_37' => 'kans op lokaal onweer',
+	'meteo_38' => 'kans op onweer',
+	'meteo_39' => 'kans op onweer',
+	'meteo_4' => 'onweer',
+	'meteo_40' => 'enkele buien',
+	'meteo_41' => 'zware sneeuwval',
+	'meteo_42' => 'enkele sneeuwbuien',
+	'meteo_43' => 'zware sneeuwbuien',
+	'meteo_44' => 'zonnig',
+	'meteo_45' => 'onweersbuien',
+	'meteo_46' => 'sneeuwbuien',
+	'meteo_47' => 'plaatselijk onweer',
+	'meteo_5' => 'regen en sneeuw',
+	'meteo_6' => 'aanvriezende regen',
+	'meteo_7' => 'aanvriezende sneeuw',
+	'meteo_8' => 'aanvriezende motregen',
+	'meteo_9' => 'motregen',
+	'meteo_conditions' => 'actuele weer',
+	'meteo_consultation' => 'Raadpleeg het weer voor @ville@',
+	'meteo_de' => 'Weerbericht voor @ville@',
+	'meteo_na' => 'onbekend',
+	'meteo_previsions' => 'vooruitzicht voor',
+	'meteo_previsions_aujourdhui' => 'Voooruitzicht voor vandaag',
+	'meteo_previsions_n_jours' => 'Vooruitzicht voor @nbj@ dagen',
+
+	// N
+	'nuit' => 'nacht',
+
+	// P
+	'point_rosee' => 'dauwpunt',
+	'pression' => 'druk',
+
+	// R
+	'risque_precipitation' => 'kans neerslag',
+
+	// S
+	'station_observation' => 'weerstation',
+
+	// T
+	'temperature_max' => 'max.',
+	'temperature_min' => 'min.',
+	'temperature_ressentie' => 'gevoel',
+	'tendance_symbole_falling' => '↓',
+	'tendance_symbole_rising' => '↑',
+	'tendance_symbole_steady' => '→',
+	'tendance_texte_falling' => 'dalend',
+	'tendance_texte_rising' => 'stijgend',
+	'tendance_texte_steady' => 'stabiel',
+
+	// U
+	'unite_angle_metrique' => '°',
+	'unite_angle_standard' => '°',
+	'unite_distance_metrique' => 'km',
+	'unite_distance_standard' => 'mijlen',
+	'unite_pourcentage_metrique' => '%',
+	'unite_pourcentage_standard' => '%',
+	'unite_precipitation_metrique' => 'mm',
+	'unite_precipitation_standard' => 'inches',
+	'unite_pression_metrique' => 'mbar',
+	'unite_pression_standard' => 'inches',
+	'unite_temperature_metrique' => '°C',
+	'unite_temperature_standard' => '°F',
+	'unite_vitesse_metrique' => 'km/u',
+	'unite_vitesse_standard' => 'mph',
+
+	// V
+	'valeur_indeterminee' => 'N/A',
+	'vent' => 'wind',
+	'visibilite' => 'zicht'
+);
+
+?>
