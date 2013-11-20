6eed6b8b460ff37e0ba2be2c93b6691e43802f0d
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-20 04:40:19 +0000

    [Salvatore] [source:_plugins_/modeles_facebook/trunk/lang/ paquet-fb_modeles] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@78648 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ modeles_facebook/trunk/lang/paquet-fb_modeles_nl.php
@@ -0,0 +1,23 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-fb_modeles?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// F
+	'fb_modeles_description' => 'Deze plugin bevat een set SPIP modellen voor een simpele integratie van de sociale modules van Facebook:
+
+-*  {{send}}: de klassieke knop « Send » (met commentaar)
+-*  {{like}}: de klassieke knop « Like » (met commentaar)
+-*  {{like box}}: lijst van gebruikers die op « Like » drukten
+-*  {{activity}}: lijst van recente activiteiten van een bladzijde of profiel
+-*  {{comments}}: lijst van commentaren met een knop om er een te schrijven
+-*  {{live stream}} (experimenteel): discussie-box « chat »',
+	'fb_modeles_nom' => 'Facebook Modellen',
+	'fb_modeles_slogan' => 'Modellen voor Facebook-modules'
+);
+
+?>
