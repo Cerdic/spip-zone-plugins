a7e06159c234532e8a6ffa4451c7a04f16a2de49
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-20 04:40:21 +0000

    [Salvatore] [source:_plugins_/mots_techniques/trunk/lang/ motstechniques] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@78649 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ mots_techniques/trunk/lang/motstechniques_nl.php
@@ -0,0 +1,17 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/motstechniques?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'bouton_mots_cles_techniques' => 'Moeten technische trefwoorden standaard verborgen worden op de publieke site?',
+
+	// I
+	'info_mots_cles_techniques' => 'Groep van technische trefwoorden: '
+);
+
+?>
