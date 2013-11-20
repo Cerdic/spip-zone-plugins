52286cf9506ea7284651967313683652177124c9
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-20 04:39:49 +0000

    [Salvatore] [source:_plugins_/boutonstexte/trunk/lang/ paquet-boutonstexte] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@78639 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ boutonstexte/trunk/lang/paquet-boutonstexte_nl.php
@@ -0,0 +1,18 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-boutonstexte?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'boutonstexte_description' => 'Invoegen van knoppen voor het vergroten/verkleinen van de lettergrootte van teksten. De knoppen worden ingevoegd voor iedere "div" met class "texte" als ze niet al op de bladzijde aanwezig zijn (afbeeldingen met class "textsizeup", "textsizedown" of "textonly").
+
+De optionele configuratie kan met plugin cfg worden gedaan. Bepaalde elementen kunnen met javascript worden geforceerd.',
+	'boutonstexte_nom' => 'Knoppen in de tekst',
+	'boutonstexte_slogan' => 'Knoppen om de lettergrootte aan te passen'
+);
+
+?>
