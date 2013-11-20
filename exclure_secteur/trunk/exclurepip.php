d26953cba831e278171b89dd8c49ed076f57d4a0
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-20 18:37:09 +0000

    contourner un bug de spip sur calculer_langues_utilisees() qui oublie d'interpréter le contenu de ->where
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@78657 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- exclure_secteur/trunk/exclurepip.php
+++ exclure_secteur/trunk/exclurepip.php
@@ -3,7 +3,7 @@ include_spip('inc/exclure_utils');
 
 function exclure_sect_pre_boucle(&$boucle){
     
-    if ($boucle->modificateur['tout_voir'] or ($boucle->modificateur['tout'] and lire_config('secteur/tout') == 'oui') or test_espace_prive()==1){
+    if ($boucle->modificateur['tout_voir'] or ($boucle->modificateur['tout'] and lire_config('secteur/tout') == 'oui') or test_espace_prive()==1 or $boucle->nom=='calculer_langues_utilisees'){
         return $boucle;
     }
     $type = $boucle->id_table;
