97351734a67c15402dc2629f0eb3dafe71b01b79
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-20 04:40:11 +0000

    [Salvatore] [source:_plugins_/mesfavoris/lang/ mesfavoris] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@78645 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ mesfavoris/lang/mesfavoris_nl.php
@@ -0,0 +1,43 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/mesfavoris?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'add' => 'Toevoegen',
+	'add_to' => 'Mijn selectie toevoegen',
+	'ajoute_le' => 'Toegevoegd op',
+
+	// C
+	'configurer' => 'Configureren',
+
+	// I
+	'info_nombre_favoris_objet' => ' @nb@ keer aan favorieten toegevoegd.', # Pas utile dans le plugin lui-même mais peut l'être dans les squelettes
+
+	// L
+	'label_style_formulaire16' => 'Klein (16px)',
+	'label_style_formulaire24' => 'Gemiddeld (24px)',
+	'label_style_formulaire32' => 'Groot (32px)',
+	'legend_formulaire_public' => 'Publiek formulier',
+	'legend_style_formulaire' => 'Formulierstijl',
+	'login_first' => 'Voor deze actie moet je aangemeld zijn. Doe dat eerst',
+
+	// M
+	'mes_favoris' => 'Mijn favorieten',
+
+	// R
+	'remove' => 'Intrekken',
+	'remove_from' => 'Mijn selectie intrekken',
+
+	// S
+	'ses_favoris' => 'Haar/zijn favorieten',
+
+	// V
+	'vos_favoris' => 'Uw favorieten' # Utile pour modifier le titre de l'inclusion
+);
+
+?>
