5d0b075eae0718785f2b749d9678c76eacba3561
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-21 04:39:33 +0000

    [Salvatore] [source:_plugins_/nuage/trunk/lang/ paquet-nuage] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@78671 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ nuage/trunk/lang/paquet-nuage_nl.php
@@ -0,0 +1,20 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-nuage?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// N
+	'nuage_description' => 'Deze plugin toont trefwoorden kriskras door elkaar, waarbij de lettergrootte afhankelijk is van het aantal artikelen dat aan dat trefwoord gekoppeld is. Met andere woorden: zo kan SPIP  « tags » maken!
+
+	Meerdere modellen zijn beschikbaar:
+-* article_nuage: toont de trefwoorden van een artikel met een grootte die afhangt van het aantal gekoppelde artikelen van de site
+-* rubrique_nuage: toont de trefwoorden van de artikelen van een hoofdrubriek met een grootte die afhangt van het aantal gekoppelde artikelen in die hoofdrubriek
+-* nuage_popularite: toont de trefwoorden van de site (of van een groep) met een grootte die afhangt van hun relatieve populariteit (de som van de populariteit van alle artikelen met dat trefwoord)',
+	'nuage_slogan' => 'Trefwoorden in verschillende lettergroottes tonen'
+);
+
+?>
