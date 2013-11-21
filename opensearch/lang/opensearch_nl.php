13aaff978ca9c79cbda31ed41d8795b7ada2389a
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-21 04:39:36 +0000

    [Salvatore] [source:_plugins_/opensearch/lang/ opensearch] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@78672 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ opensearch/lang/opensearch_nl.php
@@ -0,0 +1,21 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/opensearch?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cfg_boite_opensearch' => 'Configuratie van plugin OpenSearch',
+	'cfg_descr_opensearch' => 'OpenSearch: Gepersonaliseerde zoekmachine voor je site.',
+	'cfg_inf_id_rubrique' => 'Kies de rubriek waarin gezocht moet worden.',
+	'cfg_inf_page_recherche' => 'Geef het te gebruiken skelet voor het zoeken op de site (bijvoorbeeld: "recherche" voor het skelet recherche.html)',
+	'cfg_lbl_id_rubrique' => 'Zoekrubriek',
+	'cfg_lbl_page_recherche' => 'Zoekskelet',
+	'cfg_option_tout' => 'De gehele site',
+	'cfg_titre_opensearch' => 'OpenSearch'
+);
+
+?>
