18d1dc909e6bda816675799088cee8584b82dc89
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-21 04:39:49 +0000

    [Salvatore] [source:_plugins_/seo/trunk/lang/ paquet-seo] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@78674 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ seo/trunk/lang/paquet-seo_nl.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-seo?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// S
+	'seo_description' => 'SEO is een plugin die de head van je site kan aanpassen met: meta tags, canonieke URL, Google webmaster tools Meta Code en Google Analytics javascript. Je kunt hem configureren met een configuratie-bladzijde in SPIP. Meta tags kun je ook in iedere rubriek en ieder artikel instellen.',
+	'seo_nom' => 'SEO',
+	'seo_slogan' => 'Optimalisatie voor zoekmachines'
+);
+
+?>
