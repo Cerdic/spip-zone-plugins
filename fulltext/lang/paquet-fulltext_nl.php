2991ec7b03c844802db63a83f12ef772abbc51a9
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-21 04:39:23 +0000

    [Salvatore] [source:_plugins_/fulltext/lang/ paquet-fulltext] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@78669 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ fulltext/lang/paquet-fulltext_nl.php
@@ -0,0 +1,17 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-fulltext?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// F
+	'fulltext_description' => 'Deze plugin introduceert de FULLTEXT zoekmethode van MySQL en verbetert zo de oorspronkelijke zoekfunctionaliteit van SPIP aanzienlijk. Maar het indexeert ook de inhoud van bepaalde types documenten.
+_ Voor een optimaal functioneren zijn enkele aanvullende programma’s op de server nodig waarvan de parameters in <code>mes_options.php</code> worden geplaatst of via het configuratiepaneel moeten worden ingesteld.
+_ Bekijk de documentatie van de plugin voor meer details.',
+	'fulltext_slogan' => 'FULLTEXT indexeert voor sneller zoeken'
+);
+
+?>
