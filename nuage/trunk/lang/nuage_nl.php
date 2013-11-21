f85b80d8976486b33dc25d9fe03599bd678a401e
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-21 04:39:31 +0000

    [Salvatore] [source:_plugins_/nuage/trunk/lang/ nuage] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@78670 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ nuage/trunk/lang/nuage_nl.php
@@ -0,0 +1,19 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/nuage?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// N
+	'noisette_description' => 'Toon een wolk van trefwoorden',
+	'noisette_label_afficher_titre' => 'De titel tonen:',
+	'noisette_label_niveau_titre' => 'Kies het niveau van de titel',
+	'noisette_nom_noisette' => 'Wolk',
+	'noisette_titre_defaut' => 'Tags',
+	'noisette_titre_noisette' => 'Titel:'
+);
+
+?>
