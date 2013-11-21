6f3463461f4b052e05d60b3d2bd93e227b091c19
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-21 04:39:20 +0000

    [Salvatore] [source:_plugins_/fulltext/lang/ fulltext] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@78668 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- fulltext/lang/fulltext_nl.php
+++ fulltext/lang/fulltext_nl.php
@@ -8,21 +8,25 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
-	'accents_pas_pris' => 'Met accenten wordt geen rekening gehouden ("déjà" of "deja" geven beiden o.a. de volgende resultaten: "déjà", "dejà", "déja"...).',
-	'asie' => 'Azië',
-	'asterisque_terminale' => 'levert geen resultaten op: het sterretje moet het laaste teken zijn', # MODIF
-	'aussi' => 'ook',
+	'accents_pas_pris' => 'Met accenten wordt geen rekening gehouden ("déjà" of "deja" geven beide o.a. de volgende resultaten: "déjà", "dejà", "déja"...).',
+	'activer_indexation' => 'Het indexeren van @ext@ bestanden activeren',
+	'asie' => 'asie',
+	'asterisque_terminale' => 'levert geen resultaten op: het sterretje moet het laaste teken zijn',
+	'aussi' => 'aussi',
 
 	// C
 	'casse_indifferente' => 'Hoofdletters of kleine letters in woorden hebben geen effect.',
-	'configurer_egalement_doc' => 'Vous pouvez également configurer l’indexation des documents :', # MODIF
+	'configuration_indexation_document' => 'Configuratie voor het indexeren van documenten',
+	'configurer_egalement_doc' => 'Je kunt ook het indexeren van documenten configureren:',
 	'convertir_myisam' => 'Converteer naar MyISAM',
-	'convertir_toutes' => 'Converteer alle tekstConverteer alle tekstvelden naar MyISAM',
+	'convertir_toutes' => 'Converteer alle tekstvelden naar MyISAM',
 	'convertir_utf8' => 'converteer naar UTF-8 om de coherentie te herstellen',
 	'creer_tous' => 'Maak alle voorgestelde FULLTEXT indexen aan',
 
 	// D
-	'documents_proteges' => 'Documents protégés', # MODIF
+	'des_utilisations' => '@nb@ keer gebruikt',
+	'descriptif' => 'Omschrijving',
+	'documents_proteges' => 'Beveiligde documenten',
 
 	// E
 	'enfan' => 'kind',
@@ -31,65 +35,90 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'enfanter' => 'kinderlijk',
 	'enfantillage' => 'kindsheid',
 	'enfants' => 'kinderen',
-	'erreur_doc_bin' => 'Vous devez renseigner le binaire à utiliser pour extraire les .doc', # MODIF
-	'erreur_intervalle_cron' => 'Vous devez indiquer un intervalle supérieur à une seconde.', # MODIF
-	'erreur_nb_docs' => 'Vous devez indiquer un nombre de documents à traiter par itération supérieur à un.', # MODIF
-	'erreur_pdf_bin' => 'Vous devez renseigner le binaire à utiliser pour extraire les .pdf', # MODIF
-	'erreur_ppt_bin' => 'Vous devez renseigner le binaire à utiliser pour extraire les .ppt', # MODIF
-	'erreur_taille_index' => 'Il faut au moins indexer un caractère.', # MODIF
-	'erreur_xls_bin' => 'Vous devez renseigner le binaire à utiliser pour extraire les .xls', # MODIF
+	'erreur_binaire_indisponible' => 'Dit programma is niet op de server beschikbaar',
+	'erreur_doc_bin' => 'Raadpleeg de te gebruiken binary om .doc bestanden uit te pakken',
+	'erreur_intervalle_cron' => 'Je moet een interval van meer dan een seconde aangeven.',
+	'erreur_nb_docs' => 'Je moet een aantal te behandelen documenten aangeven dat hoger is dan een.',
+	'erreur_pdf_bin' => 'Raadpleeg de te gebruiken binary om .pdf bestanden uit te pakken',
+	'erreur_ppt_bin' => 'Raadpleeg de te gebruiken binary om .ppt bestanden uit te pakken',
+	'erreur_taille_index' => 'Ten minste één teken moet worden geïndexeerd.',
+	'erreur_verifier_configuration' => 'Er zitten fouten in de configuratie.',
+	'erreur_xls_bin' => 'Raadpleeg de te gebruiken binary om .xls bestanden uit te pakken',
 	'et' => 'en',
 	'etranger' => 'vreemdeling',
 	'exemples' => 'Voorbeelden',
+	'explication_option_readonly' => 'Deze optie is op deze site verplicht en kan dus niet worden ge configureerd.',
 
 	// F
 	'fant' => 'ind',
 	'fonctionnement_recherche' => 'Toelichting bij het zoeken op deze site ',
 	'fulltext_cree' => 'FULLTEXT aangemaakt',
+	'fulltext_creer' => 'Maak index @index@',
+	'fulltext_documentation' => 'Raadpleeg voor meer informatie over de configuratie de online documentatie:',
+	'fulltext_documents' => 'Fulltext - Documenten',
+	'fulltext_index' => 'Fulltext - Index',
+
+	// G
+	'general' => 'Algemeen',
 
 	// I
+	'id' => 'ID',
 	'il_faut_myisam' => 'MyISAM is verplicht',
 	'incoherence_charset' => 'Jouw site en de site van de database komen niet geheel overeen. Dit kan tot verkeerde resultaten leiden bij zoekopdrachten die letters met accenten bevatten:',
 	'index_regenere' => 'tekstveld opnieuw aangamaakt',
 	'index_reinitialise' => 'De documenten met een foutmelding zijn opnieuw geïnitialiseerd',
+	'index_reinitialise_ptg' => 'De beveiligde documenten zijn allemaal opnieuw geïnitialiseerd',
+	'index_reinitialise_totalement' => 'Alle documenten zijn opnieuw geïnitialiseerd',
 	'index_supprime' => 'index verwijderd',
+	'indiquer_chemin_bin' => 'Geef het pad naar het binaire programma dat de indexatie verzorgt van',
+	'indiquer_options_bin' => 'Geef de opties voor indexatie van',
+	'infos' => 'Gegevens',
+	'infos_documents_proteges' => 'Hier vind je een lijst van beveiligde documenten die dus niet door Fulltext  worden geïndexeerd',
+	'infos_fulltext_document' => 'Hier geef je op welke documenttypes door Fulltext moeten worden geïndexeerd, met welke binary en met welke opties.',
+	'intervalle_cron' => 'Tijdsinterval tussen twee CRON-jobs (in seconden).',
 
 	// L
 	'liste_tables_connues' => 'Hier volgt een lijst van tekstvelden die beschikbaar zijn voor zoekopdrachten. Je kunt meer FULLTEXT elementen toevoegen — zie de  toelichting bij',
+	'logo' => 'Logo',
 
 	// M
 	'mais_pas' => 'maar NIET',
-	'message_ok_configuration' => 'Enregistrement de vos préférences terminée', # MODIF
-	'message_ok_update_configuration' => 'Mise à jour de vos préférences terminée', # MODIF
+	'message_ok_configuration' => 'Registratie van je voorkeuren is beëindigd',
+	'message_ok_update_configuration' => 'Aanpassen van je voorkeuren is beëindigd',
 
 	// N
-	'nb_index' => 'Indexés', # MODIF
-	'nb_non_index' => 'Non-indexés', # MODIF
-	'nb_ptg' => 'Protégés (non-indexés)', # MODIF
-	'necessite_version_php' => 'Nécessite PHP 5.2 au minimum, ainsi que l’option -enable-zip.', # MODIF
-	'nombre_caracteres' => 'Nombre de caractères indexés (depuis le debut du document).', # MODIF
-	'nombre_documents' => 'Nombre de documents à traiter par itération du CRON', # MODIF
+	'nb_err' => 'Indexeerfout',
+	'nb_index' => 'Geïndexeerd',
+	'nb_non_index' => 'Niet-geïndexeerd',
+	'nb_ptg' => 'Beveiligd (niet-geïndexeerd)',
+	'necessite_version_php' => 'Minimaal PHP 5.2 is benodigd, alsmede de optie -enable-zip.',
+	'nombre_caracteres' => 'Aantal geïndexeerde tekens (vanaf het begin van het document).',
+	'nombre_documents' => 'Aantal te verwerken documenten per  CRON-job',
 
 	// O
 	'ou_bien' => 'of ook',
 
 	// P
+	'pas_document_ptg' => 'Er zijn geen beveiligde documenten.',
 	'pas_index' => 'Geen FULLTEXT index',
 	'premier_soit' => 'OFWEL',
 
 	// Q
-	'que_des_exemples' => 'NB : les adresses de binaires et options proposées ici ab initio ne sont que des exemples.', # MODIF
+	'que_des_exemples' => 'NB: de adressen van de hier voorgestelde binaries zijn slechts voorbeelden.',
 
 	// R
 	'regenerer_tous' => 'Maak alle FULLTEXT indexen opnieuw aan',
 	'reinitialise_index_doc' => 'Initialiseer de documenten die een foutmelding geven opnieuw',
+	'reinitialise_index_ptg' => 'Initialiseer de indexatie van beveiligde documenten opnieuw',
+	'reinitialise_totalement_doc' => 'Initialiseer het indexeren van alle documenten opnieuw',
 	'reserve_webmestres' => 'Pagina voorbehouden aan webmasters',
-	'retour_configuration_fulltext' => 'Retour à la configuration de Fulltext', # MODIF
+	'retour_configuration_fulltext' => 'Terug naar de Fulltext configuratie',
 	'retourne' => 'Levert tekst op die het volgende bevat',
 
 	// S
 	'sequence_exacte' => 'de exacte zin',
 	'soit' => 'OF',
+	'statistiques_indexation' => 'Statistieken van het indexeren van de documenten:',
 	'supprimer' => 'Verwijderen',
 
 	// T
@@ -97,9 +126,14 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'table_format' => 'Het formaat van dit tekstveld is',
 	'table_non_reconnue' => 'onherkenbaar tekstveld',
 	'textes_premier' => 'maar toont eerst tekst die het volgende bevat',
+	'titre_page_fulltext_index' => 'Configuratie van de zoekindexen',
 
 	// U
-	'utiliser_operateurs_logiques' => 'De zoekfunctie werkt volgens logische standaarden.'
+	'une_utilisation' => '1 keer gebruikt',
+	'utiliser_operateurs_logiques' => 'De zoekfunctie werkt volgens logische standaarden.',
+
+	// V
+	'voir_doc_ptg' => 'Toon de beveiligde documenten'
 );
 
 ?>
