18892e8b7fcb6f22c87f31235c47f5995816b290
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-21 04:39:47 +0000

    [Salvatore] [source:_plugins_/seo/trunk/lang/ seo] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@78673 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ seo/trunk/lang/seo_nl.php
@@ -0,0 +1,72 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/seo?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// S
+	'S.E.O' => 'SEO',
+
+	// A
+	'alexa' => 'Alexa',
+	'alexa_activate' => 'Alexa activeren',
+	'alexa_id' => 'Identificatie van de site voor Alexa',
+
+	// B
+	'bing_webmaster' => 'Bing Webmaster Tools',
+	'bing_webmaster_activate' => 'Bing Webmaster Tools activeren',
+	'bing_webmaster_id' => 'Meta code voor de verificatie',
+
+	// C
+	'canonical_url' => 'URL Canonicals',
+	'canonical_url_activate' => 'De meta voor URL Canonicals activeren',
+
+	// E
+	'explication_description' => 'Het is beter de lengte te beperken tot maximaal 160 tekens.',
+	'explication_title' => 'Het is beter een lengte van 5 tot 70 tekens aan te houden.',
+
+	// F
+	'forcer_squelette_descriptif' => 'De meta van SEO hebben voorrang boven die van de skeletten',
+	'forcer_squelette_label' => 'Laad de metas voor alle skeletten',
+
+	// G
+	'google_analytics' => 'Google Analytics',
+	'google_analytics_activate' => 'Google Analytics activeren',
+	'google_analytics_id' => 'Google Analytics web property ID',
+	'google_webmaster_tools' => 'Google Webmaster Tools',
+	'google_webmaster_tools_activate' => 'Google Webmaster Tools activeren',
+	'google_webmaster_tools_id' => 'Meta code voor de verificatie',
+
+	// I
+	'info_count_max' => 'Caractères pour atteindre la limite optimale: ',
+	'insert_head' => 'Automatisch invoegen in #INSERT_HEAD',
+	'insert_head_activate' => 'Activeer het automatisch invoegen',
+	'insert_head_descriptif' => 'Automatisch invoegen van de SEO configuratie in de &lt;head&gt;',
+
+	// M
+	'meta_author' => 'Auteur:',
+	'meta_copyright' => 'Copyright:',
+	'meta_description' => 'Omschrijving:',
+	'meta_keywords' => 'Trefwoorden:',
+	'meta_page_description_sommaire_value' => 'Waarde van de Omschrijving van de Pagina + Waarde van de Meta van de Beginpagina',
+	'meta_page_description_value' => 'Waarde van de Omschrijving van de Pagina',
+	'meta_page_title_sommaire_value' => 'Waarde van de Titel van de Pagina + Waarde van de Meta van de Beginpagina',
+	'meta_page_title_value' => 'Waarde van de Titel van de Pagina',
+	'meta_robots' => 'Robots:',
+	'meta_sommaire_value' => 'Waarde van de Meta van de Beginpagina',
+	'meta_tags' => 'Meta Tags',
+	'meta_tags_activate' => 'Activeer de editoriële Meta tags',
+	'meta_tags_default' => 'Standaardwaarde van de meta tags (voor artikelen en rubrieken)',
+	'meta_tags_edit_activate' => 'Activeer de aanpassing van meta tags in rubrieken en artikelen',
+	'meta_tags_editing' => 'Aanpassing van meta tags',
+	'meta_tags_sommaire' => 'Waarde van de meta tags van de beginpagina (sommaire)',
+	'meta_title' => 'Titel:',
+
+	// S
+	'seo' => 'Optimalisatie voor zoekmachines'
+);
+
+?>
