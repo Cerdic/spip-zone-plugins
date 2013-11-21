88472c0edfb30d3252266aa3284e731804cc5480
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-21 04:39:14 +0000

    [Salvatore] [source:_plugins_/criteres_suivant_precedent/lang/ suivant_precedent] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@78667 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ criteres_suivant_precedent/lang/suivant_precedent_nl.php
@@ -0,0 +1,20 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/suivant_precedent?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'alt_lien_next' => 'Volgende',
+	'alt_lien_prev' => 'Vorige',
+
+	// N
+	'noisette_description' => 'Biedt navigatie-links naar het vorige en volgende artikel',
+	'noisette_label_meme_rubrique' => 'Toon uitsluitend links naar artikelen binnen dezelfde rubriek',
+	'noisette_nom' => 'Hyperlinks Vorige / Volgende'
+);
+
+?>
