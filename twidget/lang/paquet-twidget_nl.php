7d40705193f762c65f28811f3575c5b0cd7a2565
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-21 04:39:55 +0000

    [Salvatore] [source:_plugins_/twidget/lang/ paquet-twidget] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@78675 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- twidget/lang/paquet-twidget_nl.php
+++ twidget/lang/paquet-twidget_nl.php
@@ -8,13 +8,13 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// T
-	'twidget_description' => 'Twitter widget eenvoudig te installeren
-_ Zet een inclusie in je skeletten om de widget te wijzen:
+	'twidget_description' => 'Eenvoudig te installeren Twitter widget 
+_ Zet een inclusie in je skeletten om de widget te tonen:
 _ <code>#INCLURE{fond=inclure/twidget_profile,user=GusLeLapin}</code>
  of <code>#INCLURE{fond=inclure/twidget_search,search=#SPIP,title=\'Suivez twitter\',subject=\'SPIP\'}</code>
 
-De plugin acteer als een  proxy om geen rechtstreekse verzoek van je bezoekers naar Twitter te provoceren en zo het sporen van hun activiteiten te voorkomen  (verdediging van de privacy).', # MODIF
-	'twidget_slogan' => 'Twitter widget eenvoudig te installeren'
+De plugin gebruikt een proxy om geen rechtstreeks verzoek van je bezoekers naar Twitter te provoceren en zo de sporen van hun activiteiten verborgen te houden (bescherming van de privacy).',
+	'twidget_slogan' => 'Eenvoudig te installeren Twitter widget'
 );
 
 ?>
