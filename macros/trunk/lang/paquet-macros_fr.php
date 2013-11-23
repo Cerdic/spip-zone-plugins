663297ff361a129c19b44aa5684eb1d58a8b5f34
bystrano@gmx.ch <bystrano@gmx.ch@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-23 16:43:25 +0000

    commit initial - généré par la Fabrique
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@78739 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ macros/trunk/lang/paquet-macros_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// M
+	'macros_description' => 'Introduit un système de macros.',
+	'macros_nom' => 'Macros',
+	'macros_slogan' => 'Génération automatique de squelettes',
+);
+
+?>
---
+++ macros/trunk/macros_fonctions.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Fonctions utiles au plugin Macros
+ *
+ * @plugin     Macros
+ * @copyright  2013
+ * @author     Michel Bystranowski
+ * @licence    GNU/GPL
+ * @package    SPIP\Macros\Fonctions
+ */
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+
+?>
