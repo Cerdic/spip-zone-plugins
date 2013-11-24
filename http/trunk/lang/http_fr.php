795a4614322896903a7561ddab24ee45884c4305
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-24 04:40:25 +0000

    [Salvatore] [source:_plugins_/http/trunk/lang/ http] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@78770 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- http/trunk/lang/http_fr.php
+++ http/trunk/lang/http_fr.php
@@ -1,13 +1,16 @@
 <?php
-
-// Sécurité
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/http/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// E
 	'erreur_401_message' => 'Vous n’avez pas le droit d’accéder à cette ressource.',
-	'erreur_404_titre' => 'Accès interdit',
 	'erreur_404_message' => 'Vous avez demandez une ressource qui n’existe pas ou qui n’a pas été trouvée.',
 	'erreur_404_titre' => 'La ressource n’a pas été trouvée',
 	'erreur_415_message' => 'Votre requête est dans un format inconnu, non supporté par ce serveur.',
-	'erreur_415_titre' => 'Format inconnu',
+	'erreur_415_titre' => 'Format inconnu'
 );
+
+?>
