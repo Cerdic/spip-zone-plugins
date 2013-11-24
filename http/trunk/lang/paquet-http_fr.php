4d11de001b823d421243367e6372aac7f23a79d2
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-24 04:40:27 +0000

    [Salvatore] [source:_plugins_/http/trunk/lang/ paquet-http] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@78771 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- http/trunk/lang/paquet-http_fr.php
+++ http/trunk/lang/paquet-http_fr.php
@@ -1,12 +1,14 @@
 <?php
-
-// Sécurité
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/http/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-'http_nom' => 'Serveur HTTP abstrait',
-'http_description' => 'Ce plugin fournit une API pour que d\'autres plugins puissent implémenter plus facilement des services utilisant les méthodes HTTP.',
-'http_slogan' => 'Gestion abstraite des méthodes HTTP.',
+
+	// H
+	'http_description' => 'Ce plugin fournit une API pour que d’autres plugins puissent implémenter plus facilement des services utilisant les méthodes HTTP.',
+	'http_nom' => 'Serveur HTTP abstrait',
+	'http_slogan' => 'Gestion abstraite des méthodes HTTP.'
 );
 
 ?>
