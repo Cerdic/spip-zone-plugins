7d0d19cc4d0c2dd34a5e91d8289c75b48194daa3
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-24 17:53:57 +0000

    On conserve les checkbox actives. Néanmoins on affiche deux fois le message de retour... ???
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@78792 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- actions_multiples/trunk/formulaires/mao_actionner.php
+++ actions_multiples/trunk/formulaires/mao_actionner.php
@@ -7,6 +7,8 @@ function formulaires_mao_actionner_charger_dist($liste, $objet) {
 
 	$valeurs['liste'] = $liste;
 	$valeurs['objet'] = $objet;
+	$ids = _request("ids_${objet}");
+	$valeurs["ids_${objet}"] =  $ids ? $ids : array();
 
 	return $valeurs;
 }
@@ -22,6 +24,7 @@ function formulaires_mao_actionner_verifier_dist($liste, $objet) {
 function formulaires_mao_actionner_traiter_dist($liste, $objet) {
 	$retour = array();
 
+	$ids = _request("ids_${objet}");
 
 	$retour['message_ok'] = 'ok';
 	$retour['editable'] = true;
