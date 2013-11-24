e8faadd0c6a3e51924951bc5e60a117471debf66
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-24 04:40:12 +0000

    [Salvatore] [source:_plugins_/bandeau/lang/ bando] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@78767 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ bandeau/lang/bando_nl.php
@@ -0,0 +1,44 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/bando?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// I
+	'icone_accueil' => 'Beginpagina',
+	'icone_administration' => 'Onderhoud',
+	'icone_configuration' => 'Configuratie',
+	'icone_edition' => 'Uitgave',
+	'icone_langage' => 'Mijn taal',
+	'icone_mes_infos' => 'Mijn gegevens',
+	'icone_preferences' => 'Mijn voorkeuren',
+	'icone_reactions' => 'Geäctiveerd',
+	'icone_squelette' => 'Skeletten',
+	'icone_suivi_edito' => 'Publicatie',
+	'icone_suivi_publication' => 'Publicaties opvolgen',
+	'icone_visiter_site' => 'Naar de publieke site',
+
+	// L
+	'label_adresses_secondaires' => 'ALIAS-adressen van de publieke site',
+	'label_bando_outils' => 'Knoppenbalk',
+	'label_bando_outils_afficher' => 'Toon de knoppenbalk',
+	'label_bando_outils_masquer' => 'Verberg de knoppenbalk',
+	'label_choix_langue' => 'Kies je taal',
+	'label_slogan_site' => 'Slogan van de site',
+	'label_taille_ecran' => 'Schermgrootte',
+	'label_texte_et_icones_navigation' => 'Navigatiemenu',
+	'label_texte_et_icones_page' => 'Op de pagina tonen',
+
+	// T
+	'texte_redirection_adresses_secondaires' => 'Aliassen zijn alternatieve URLs waarmee de site ook bereikt kan worden.
+		Deze adressen worden automatisch naar het hoofdadres doorgestuurd. Vermeld één adres per regel',
+	'titre_config_langage' => 'Configureer de taal',
+	'titre_configurer_preferences' => 'Configureer je voorkeuren',
+	'titre_identite_site' => 'Identiteit van de site',
+	'titre_infos_perso' => 'Mijn persoonlijke gegevens'
+);
+
+?>
