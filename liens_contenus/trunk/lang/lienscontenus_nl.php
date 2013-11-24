73881909c46ade45d2b4e532025b45983041e978
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-24 04:40:31 +0000

    [Salvatore] [source:_plugins_/liens_contenus/trunk/lang/ lienscontenus] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@78772 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ liens_contenus/trunk/lang/lienscontenus_nl.php
@@ -0,0 +1,59 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/lienscontenus?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'alerte_publie_contenant' => 'Let op, deze inhoud is gepubliceerd, maar bevat links naar niet-gepubliceerde inhoud!',
+	'alerte_publie_contenant_ko' => 'Let op, deze inhoud is gepubliceerd, bevat links naar niet-bestaande inhoud!',
+	'aucun_objets_avec_lien_depuis_courant' => 'Deze inhoud bevat geen enkele link naar ander inhoud.',
+	'aucun_objets_avec_lien_vers_courant' => 'Er bestaat geen link naar deze inhoud.',
+
+	// C
+	'confirmation_depublication' => 'Let op, interne links wijzen naar deze inhoud !\\n\\nVerandering van de status heeft invloed op de werking van de site.\\n\\nWil je echt de status aanpassen?',
+	'confirmation_publication' => 'Let op, deze inhoud contient un lien pointant vers un contenu qui n’est pas publié !\\n\\nVerandering van de status heeft invloed op de werking van de site.\\n\\nWil je echt de status aanpassen?',
+	'confirmation_suppression' => 'Let op, des liens internes pointent vers deze inhoud !\\n\\nVerwijdering heeft invloed op de werking van de site.\\n\\nWil je echt verwijderen?',
+
+	// I
+	'inexistant' => 'niet-bestaand (@id_objet@)',
+	'information_element_contenu' => 'Let op, interne links wijzen naar deze inhoud !',
+
+	// L
+	'legende_liens_faux_objets' => 'Rode en doorgestreepte links wijzen op niet-bestaande inhoud.',
+	'liens_entre_contenus' => 'Links tussen inhoud',
+
+	// O
+	'objets_avec_liens_depuis_courant' => 'Deze inhoud heeft links naar:',
+	'objets_avec_liens_vers_courant' => 'Andere inhoud verwijst naar deze:',
+
+	// S
+	'statut_poubelle' => 'In de prullenbak',
+	'statut_prepa' => 'In voorbereiding',
+	'statut_prop' => 'Voorgesteld',
+	'statut_publie' => 'Gepubliceerd',
+	'statut_refuse' => 'Geweigerd',
+
+	// T
+	'type_article' => '@titre@ (@id_objet@)',
+	'type_article_inexistant' => 'Niet-bestaand artikel (@id_objet@)',
+	'type_auteur' => '@titre@',
+	'type_auteur_inexistant' => 'Niet-bestaande auteur (@id_objet@)',
+	'type_breve' => '@titre@ (@id_objet@)',
+	'type_breve_inexistant' => 'Niet-bestaand nieuwsbericht (@id_objet@)',
+	'type_document' => '@titre@ (@id_objet@)',
+	'type_document_inexistant' => 'Niet-bestaand document (@id_objet@)',
+	'type_forum' => '@titre@ (@id_objet@)',
+	'type_forum_inexistant' => 'Niet-bestaand bericht (@id_objet@)',
+	'type_modele' => 'Model "@id_objet@"',
+	'type_modele_inexistant' => 'Niet-bestaand model (@id_objet@)',
+	'type_rubrique' => '@titre@ (@id_objet@)',
+	'type_rubrique_inexistant' => 'Niet-bestaande rubriek (@id_objet@)',
+	'type_syndic' => '@titre@ (@id_objet@)',
+	'type_syndic_inexistant' => 'Niet-bestaande site (@id_objet@)'
+);
+
+?>
