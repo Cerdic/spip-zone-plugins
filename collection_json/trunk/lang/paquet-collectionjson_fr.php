e21d64c6019639b9b2f254ce9c6d3f9cc892e01a
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-24 04:40:16 +0000

    [Salvatore] [source:_plugins_/collection_json/trunk/lang/ paquet-collectionjson] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@78768 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- collection_json/trunk/lang/paquet-collectionjson_fr.php
+++ collection_json/trunk/lang/paquet-collectionjson_fr.php
@@ -1,9 +1,13 @@
 <?php
-
-// Sécurité
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/collection_json/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
 	'collectionjson_nom' => 'API Collection+JSON',
-	'collectionjson_slogan' => 'Implémentation d’une API REST au format JSON pour les objets de SPIP',
+	'collectionjson_slogan' => 'Implémentation d’une API REST au format JSON pour les objets de SPIP'
 );
+
+?>
