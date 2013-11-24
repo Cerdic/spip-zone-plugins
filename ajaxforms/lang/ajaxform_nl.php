fba59f8e61c76b64ce42885434be5aa0dd8c02de
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-24 04:40:02 +0000

    [Salvatore] [source:_plugins_/ajaxforms/lang/ ajaxform] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@78766 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ ajaxforms/lang/ajaxform_nl.php
@@ -0,0 +1,32 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/ajaxform?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'aucun_fichier_recu' => 'Je moet een bestand kiezen.',
+
+	// C
+	'changer_document' => 'Het document veranderen',
+	'confirmer_suppression' => 'De verwijdering werd uitgevoerd.',
+
+	// D
+	'document_ajoute' => 'Het document werd toegevoegd.',
+
+	// E
+	'erreur_ajout_document' => 'Tijdens het toevoegen van het document is een fout opgetreden.',
+
+	// L
+	'logo' => 'Logo',
+	'logo_maj' => 'Het logo werd aangepast.',
+	'logo_survol' => 'Hover logo',
+
+	// M
+	'mauvaise_reception' => 'Tijdens de ontvangst van het bestand is een fout opgetreden. Probeer opnieuw...'
+);
+
+?>
