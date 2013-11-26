574c83002f2c23752ed4d29f34273549c7c2e318
erational@erational.org <erational@erational.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-26 09:36:29 +0000

    l'ancienne API ne reponds plus. on propose une nouvelle version basée sur l'API2. "Custom search"
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@78819 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- google_ajax_search/googleajaxsearch_pipeline.php
+++ google_ajax_search/googleajaxsearch_pipeline.php
@@ -1,35 +1,18 @@
 <?php
-// --------------------------------
-// partie publique
-// --------------------------------
-function googleajaxsearch_insert_head($flux){
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
-    $langue_site = $GLOBALS['meta']['langue_site'];
-    $nom_site = $GLOBALS['meta']['nom_site'];
-    $adresse_site = $GLOBALS['meta']['adresse_site'];
-        
-		
-		$flux.='
-    <!-- google ajax api search -->
-    <script type="text/javascript" src="http://www.google.com/jsapi"></script>
+function googleajaxsearch_insert_head($flux){
+   
+    // https://developers.google.com/custom-search/docs/element
+    $flux.='<!-- google search -->
+    <script src="https://www.google.com/jsapi"></script>
     <script type="text/javascript">
-    google.load("search", "1", {"language" : "'.$langue_site.'"});
-    function OnLoadGoogle() {
-      var recherche = $(\'#searchcontrol span\').text();
-      var searchControl = new google.search.SearchControl();      
-      var siteSearch = new google.search.WebSearch();
-      siteSearch.setUserDefinedLabel("'.supprimer_tags($nom_site).'");
-      siteSearch.setUserDefinedClassSuffix("siteSearch");
-      siteSearch.setSiteRestriction("'.$adresse_site.'");
-	  searchControl.setResultSetSize(google.search.Search.LARGE_RESULTSET)
-      options = new google.search.SearcherOptions();
-      options.setExpandMode(google.search.SearchControl.EXPAND_MODE_OPEN);
-      searchControl.addSearcher(siteSearch,options);
-      searchControl.draw(document.getElementById("searchcontrol"));
-      searchControl.execute(recherche);
-    }     
-    google.setOnLoadCallback(OnLoadGoogle);
+        google.load(\'search\', \'1\');
+        google.setOnLoadCallback(function(){
+          new google.search.CustomSearchControl().draw(\'searchcontrol\');
+        }, true);
     </script>';
+        
 	
 	return $flux;
 }
---
+++ google_ajax_search/lang/paquet-googleajaxsearch_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// M
+	'googleajaxsearch_description' => 'Anciennement appelé google ajaxsearch, Custom search permet de placer un widget de formulaire de recherche google.',
+	'googleajaxsearch_nom' => 'Google Custom Search',
+	'googleajaxsearch_slogan' => 'Formulaire de recherche google',
+);
+
+?>
