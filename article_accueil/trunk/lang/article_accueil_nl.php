e7aec71adc540ccb270fffedc5aa49de166ecc7c
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-27 04:38:35 +0000

    [Salvatore] [source:_plugins_/article_accueil/trunk/lang/ article_accueil] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@78849 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ article_accueil/trunk/lang/article_accueil_nl.php
@@ -0,0 +1,21 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/article_accueil?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'article_accueil' => 'Introductie-artikel',
+	'aucun_article_accueil' => 'Geen artikel',
+
+	// L
+	'label_id_article_accueil' => 'Introductie-artikel',
+
+	// R
+	'rubrique_article_en_accueil' => 'Introductie-artikel:'
+);
+
+?>
