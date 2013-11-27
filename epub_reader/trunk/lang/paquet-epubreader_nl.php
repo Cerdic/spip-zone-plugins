b7d793f4147c5c8a61ee5388cf6b3d7ce47f1722
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-27 04:39:06 +0000

    [Salvatore] [source:_plugins_/epub_reader/trunk/lang/ paquet-epubreader] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@78861 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ epub_reader/trunk/lang/paquet-epubreader_nl.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-epubreader?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// E
+	'epubreader_description' => 'Lees [ePUB->http://nl.wikipedia.org/wiki/EPUB] bestanden direct in de site',
+	'epubreader_nom' => 'ePUB Lezer',
+	'epubreader_slogan' => 'ePUB bestanden lezen'
+);
+
+?>
