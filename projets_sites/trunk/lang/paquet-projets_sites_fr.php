d9b97691f72b5ac01e8ab53f14c5877e4ce9d56a
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-27 00:18:54 +0000

    Il y a eu un gros oops
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@78842 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ projets_sites/trunk/lang/paquet-projets_sites_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// P
+	'projets_sites_description' => 'Ce plugin va vous permettre d\'ajouter à vos projets les infos sur vos sites en production, en recette et en développement.',
+	'projets_sites_nom' => 'Projets - Sites internet',
+	'projets_sites_slogan' => 'Ayez sous la main vos infos de sites en production, récette et développement',
+);
+
+?>
