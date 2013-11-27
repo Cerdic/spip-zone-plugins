c48e5bfb6aa156a3f1f487480f8d2d28c48c641f
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-27 04:39:03 +0000

    [Salvatore] [source:_plugins_/edition_directe/trunk/lang/ paquet-edition_directe] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@78860 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ edition_directe/trunk/lang/paquet-edition_directe_nl.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-edition_directe?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// E
+	'edition_directe_description' => 'SPIP objecten direct aanpassen vanaf hun pagina in de privé-omgeving. De direct aan te passen objecten moeten in de configuratie worden geactiveerd',
+	'edition_directe_slogan' => 'Pas SPIP objecten direct aan vanaf hun pagina'
+);
+
+?>
