2d58da3071be59440ba7d71a3a5de0c09731e3a1
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-27 04:38:41 +0000

    [Salvatore] [source:_plugins_/blocsdepliables/lang/ paquet-blocsdepliables] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@78852 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ blocsdepliables/lang/paquet-blocsdepliables_nl.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-blocsdepliables?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'blocsdepliables_description' => 'Door op de titel te klikken maak je blokken inhoud zichtbaar of onzichtbaar.',
+	'blocsdepliables_nom' => 'Openvouwbare Blokken',
+	'blocsdepliables_slogan' => 'Openvouwbare blokken.'
+);
+
+?>
