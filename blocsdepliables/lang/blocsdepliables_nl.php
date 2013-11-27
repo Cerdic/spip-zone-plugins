87174fd6533efd222dc399f0c239f5dc18c3a50f
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-27 04:38:39 +0000

    [Salvatore] [source:_plugins_/blocsdepliables/lang/ blocsdepliables] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@78851 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ blocsdepliables/lang/blocsdepliables_nl.php
@@ -0,0 +1,37 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/blocsdepliables?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'blocsdepliables_titre' => 'Openvouwbare Blokken',
+
+	// C
+	'cfg_exemple' => 'Voorbeeld',
+	'cfg_exemple_explication' => 'Uitleg van dit voorbeeld',
+	'cfg_titre_parametrages' => 'Parameters',
+
+	// E
+	'explication_cookie' => 'De status van de genummerde blokken wordt gedurende de sessie in een cookie opgeslagen, zodat de pagina er bij het terugkeren identiek uit ziet.',
+	'explication_unique' => 'Het openen van een blok veroorzaakt het sluiten van alle andere blokken op de bladzijde. Er is er dus maar één tegelijk geopend.',
+
+	// L
+	'label_animation' => 'Animatie',
+	'label_animation_aucun' => 'Geen',
+	'label_animation_fast' => 'Snel glijden',
+	'label_animation_millisec' => 'Glijden gedurende:',
+	'label_animation_normal' => 'Normaal glijden',
+	'label_animation_slow' => 'Langzaam glijden',
+	'label_balise_titre' => 'Baken voor de titels van de blokken',
+	'label_cookie_1' => 'De status van de blokken onthouden',
+	'label_unique_1' => 'Eén enkel geopend blok per bladzijde',
+
+	// T
+	'titre_page_configurer_blocsdepliables' => 'Openvouwbare Blokken'
+);
+
+?>
