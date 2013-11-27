4b1371444d8898c4833c465d73d00623a3fc82ae
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-27 04:38:37 +0000

    [Salvatore] [source:_plugins_/article_accueil/trunk/lang/ paquet-article_accueil] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@78850 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ article_accueil/trunk/lang/paquet-article_accueil_nl.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-article_accueil?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'article_accueil_description' => 'Met deze plugin kun je een introductie-artikel aan een rubriek toewijzen. Het veld id_article_accueil kan daarna in een skelet worden gebruikt.',
+	'article_accueil_nom' => 'Introductie-artikel',
+	'article_accueil_slogan' => 'Een introductie-artikel aan rubrieken toewijzen'
+);
+
+?>
