1c94a4b105d75f96422347e591bd7de247677bf5
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-27 04:38:59 +0000

    [Salvatore] [source:_plugins_/doc2img/trunk/lang/ paquet-doc2img] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@78858 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ doc2img/trunk/lang/paquet-doc2img_nl.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-doc2img?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// D
+	'doc2img_description' => 'Met deze plugin converteer je pdf, tiff en andere types documenten direct in een afbeelding of een serie afbeeldingen. Die afbeeldingen kun je vervolgens met een specifieke lus gebruiken.',
+	'doc2img_nom' => 'Document als Afbeelding',
+	'doc2img_slogan' => 'Converteer tekstdocumenten direct'
+);
+
+?>
