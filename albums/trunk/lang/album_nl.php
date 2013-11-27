2b5a092b9a18f81247d421cc67e77564e5f0dde4
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-27 04:38:26 +0000

    [Salvatore] [source:_plugins_/albums/trunk/lang/ album] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@78847 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ albums/trunk/lang/album_nl.php
@@ -0,0 +1,113 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/album?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'bouton_associer' => 'Koppelen',
+	'bouton_dissocier' => 'Ontkoppelen',
+	'bouton_valider_deplacer_documents' => 'De wijzigingen registreren',
+
+	// C
+	'cfg_titre_albums' => 'Albums',
+
+	// E
+	'erreur_deplacement' => 'De aanpassing kon niet worden uitgevoerd',
+
+	// F
+	'filtre_avec_contenu' => 'Gevuld',
+	'filtre_non_vus' => 'Ongezien',
+	'filtre_sans_contenu' => 'Leeg',
+	'filtre_tous' => 'Alles',
+	'filtre_tous_contenus' => 'Alle inhoud',
+	'filtre_vus' => 'Gezien',
+
+	// H
+	'heading_contenu' => 'Inhoud',
+	'heading_descriptif' => 'Omschrijving',
+	'heading_id' => 'ID',
+	'heading_infos' => 'Info',
+	'heading_liaison' => 'Koppeling',
+	'heading_logo' => 'Logo',
+	'heading_titre' => 'Titel',
+
+	// I
+	'icone_ajouter' => 'Toevoegen',
+	'icone_ajouter_album' => 'Een album toevoegen',
+	'icone_associer_album' => 'Een bestaand album koppelen',
+	'icone_creer_album' => 'Een nieuw album maken',
+	'icone_delier_album' => 'Dit album weghalen',
+	'icone_fermer' => 'Sluiten',
+	'icone_modifier_album' => 'Dit album aanpassen',
+	'info_1_album' => '1 album',
+	'info_1_association' => 'Gekoppeld aan 1 object',
+	'info_album_vide' => 'Leeg album',
+	'info_aucun_album' => 'Geen enkel album',
+	'info_aucun_album_criteres' => 'Geen enkel album voldoet aan deze criteria',
+	'info_aucun_album_supplementaire' => 'Geen enkel aanvullend album',
+	'info_aucun_album_vide' => 'Geen leeg album',
+	'info_aucune_association' => 'Dit album is aan geen enkel object gekoppeld',
+	'info_dans_objet' => 'in',
+	'info_documents_joints_albums' => 'Zorg ervoor het activeren van het toevoegen van documenten aan albums',
+	'info_js_prive' => 'animatie is niet actief in de privé-omgeving',
+	'info_nb_albums' => '@nb@ albums',
+	'info_nb_associations' => 'Gekoppeld aan @nb@ objecten',
+	'info_nouvel_album' => 'Nieuw album',
+	'info_publier' => 'publiceren',
+	'info_recherche_album_zero' => 'Geen enkel resultaat voor « @cherche_album@ »',
+	'info_resultat_recherche_aucun' => 'Geen enkel resultaat',
+	'info_resultat_recherche_plus' => '@nb@ resultaten',
+	'info_resultat_recherche_un' => '1 resultaat',
+	'info_statut_modele' => 'Niet-gepubliceerd album',
+
+	// L
+	'label_activer_album_objets' => 'Activeer de volgende inhoud voor albums:',
+	'label_case_utiliser_titre_defaut' => 'Standaard wordt de titel van het bovenliggende object gebruikt',
+	'label_modele_alignement' => 'Aliniëring',
+	'label_modele_alignement_centre' => 'Gecentreerd',
+	'label_modele_alignement_defaut' => 'Standaard',
+	'label_modele_alignement_droite' => 'Rechts',
+	'label_modele_alignement_gauche' => 'Links',
+	'label_modele_identifiant' => 'Albumnummer',
+	'label_modele_parcourir_albums' => 'Door de albums gaan',
+	'label_modele_titre' => 'Aangepaste titel',
+	'label_modele_variante' => 'Variant',
+	'label_modele_variante_defaut' => 'Miniaturen (standaard)',
+	'label_modifier_balise' => 'Pas het baken aan',
+	'label_titre' => 'Titel',
+	'label_utiliser_titre_defaut' => 'Titel van een nieuw album',
+
+	// M
+	'message_balise_inseree_succes' => 'Het baken is in de tekst opgenomen',
+
+	// O
+	'onglet_associer_album' => 'Koppelen',
+	'onglet_configurer_base' => 'Albums',
+	'onglet_configurer_outils' => 'Tools',
+	'onglet_creer_album' => 'Maken',
+	'onglet_mes_albums' => 'Mijn albums',
+	'onglet_parents_tous' => 'Alle objecten',
+	'onglet_tous' => 'Alles',
+
+	// T
+	'texte_ajouter_album' => 'Een album toevoegen',
+	'texte_associer_album' => 'Een bestaand album koppelen',
+	'texte_changer_statut' => 'De status aanpassen',
+	'texte_creer_album' => 'Een nieuw album maken',
+	'texte_modifier' => 'Aanpassen',
+	'texte_statut_poubelle' => 'naar de prullenbak',
+	'texte_statut_prepa' => 'niet-gepubliceerd',
+	'texte_statut_publie' => 'gepubliceerd',
+	'titre_album' => 'Album',
+	'titre_album_sans_titre' => 'Album zonder titel',
+	'titre_albums' => 'Albums',
+	'titre_inserer_modele_album' => 'een album',
+	'titre_logo_album' => 'Logo',
+	'titre_page_configurer_albums' => 'Albums configureren'
+);
+
+?>
