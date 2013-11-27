7ebb12aee013b2b418230fcd75df927f77ad6f31
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-27 04:39:10 +0000

    [Salvatore] [source:_plugins_/facd/lang/ paquet-facd] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@78863 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ facd/lang/paquet-facd_nl.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-facd?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// F
+	'facd_description' => 'Beheer van een wachtrij voor de conversie van documenten.',
+	'facd_slogan' => 'Beheer van een wachtrij voor de conversie van documenten'
+);
+
+?>
