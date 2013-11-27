85a59963bcde859d41172a6eda018c81568246f5
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-27 04:38:33 +0000

    [Salvatore] [source:_plugins_/albums/trunk/lang/ paquet-albums] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@78848 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ albums/trunk/lang/paquet-albums_nl.php
@@ -0,0 +1,19 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-albums?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'albums_description' => 'Deze plugin voegt een nieuw ’album’ object toe dat kan worden gekoppeld aan andere objecten.
+		Albums zijn containers voor documenten: je kunt er documenten mee groeperen.
+		Het aan een tekst toevoegen van een serie documenten wordt erdoor vereenvoudigd.',
+	'albums_nom' => 'Albums',
+	'albums_slogan' => 'Groepeer documenten als een album',
+	'albums_titre' => 'Albums'
+);
+
+?>
