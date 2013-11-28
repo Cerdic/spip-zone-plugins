7dd95a98063d3682dc59e7b94b36064d9bfaad2d
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-28 04:39:37 +0000

    [Salvatore] [source:_plugins_/mes_fichiers/trunk/lang/ paquet-mes_fichiers] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@78903 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ mes_fichiers/trunk/lang/paquet-mes_fichiers_nl.php
@@ -0,0 +1,19 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-mes_fichiers?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// M
+	'mes_fichiers_description' => 'Deze plugin maakt een archiefbestand waarin de personalisatie van de site is opgenomen, zoals de laatste dump van de database, de map van aangepaste skeletten, de map met afbeeldingen...
+Het archiefbestand wordt gemaakt in <code>tmp/mes_fichiers/</code> met een naam die de aanmaakdatum bevat.
+
+Tevens is het mogelijk automatisch een regelmatige backup van de personalisatie van je site te maken. Verouderde bestanden kunnen automatisch worden verwijderd. Alle acties worden ondersteund met berichtgeving per email.',
+	'mes_fichiers_nom' => 'Mijn bestanden',
+	'mes_fichiers_slogan' => 'Archiveer de personalisaties van je site'
+);
+
+?>
