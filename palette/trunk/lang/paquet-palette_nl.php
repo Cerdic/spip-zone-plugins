5e7639746b5c60be471134aac0d6c760065a2730
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-28 04:39:43 +0000

    [Salvatore] [source:_plugins_/palette/trunk/lang/ paquet-palette] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@78905 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ palette/trunk/lang/paquet-palette_nl.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-palette?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// P
+	'palette_description' => 'Geef class palette aan een input om er een kleurselectie aan toe te voegen: <code><input class="palette" name="couleur" value="#dfdfdf" /></code>',
+	'palette_slogan' => 'Kleurselectie'
+);
+
+?>
