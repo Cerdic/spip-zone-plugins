188f4f8a21f1bf32ed9add6db72a452696e3341e
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-28 04:39:51 +0000

    [Salvatore] [source:_plugins_/porte_plume_extras/partout/lang/ paquet-ppp] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@78908 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ porte_plume_extras/partout/lang/paquet-ppp_nl.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-ppp?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// P
+	'ppp_description' => 'Porte Plume Partout voegt de typografische knoppenbalk toe aan de velden DESCRIPTIF, CHAPO en PS',
+	'ppp_nom' => 'Porte Plume Partout',
+	'ppp_slogan' => 'Gebruik Porte Plume bij de meeste invoervelden'
+);
+
+?>
