2db6cf06e0a7d0bc72dbc085ab26e0319692b055
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-28 04:39:56 +0000

    [Salvatore] [source:_plugins_/recommander/lang/ paquet-recommander] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@78910 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ recommander/lang/paquet-recommander_nl.php
@@ -0,0 +1,22 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-recommander?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// R
+	'recommander_description' => 'Een bladzijde aanbevelen aan een vriend(in).
+
+Aan ieder willekeurig skelet kan een tag worden toegevoegd onder de vorm:
+<code>#RECOMMANDER{titel van de bladzijde,url van de bladzijde,intro van de bladzijde,onderwerp}</code>
+_ bijvoorbeeld:
+<code>#RECOMMANDER{#TITRE,#URL_ARTICLE,#INTRODUCTION}</code>
+_ de url, de introductie en het onderwerp zijn facultatief',
+	'recommander_nom' => 'Aan een vriend(in) aanbevelen',
+	'recommander_slogan' => 'Functie om een bladzijde aan een vriend(in) aan te bevelen'
+);
+
+?>
