b24c81c01dc5a5faaece84c2dc6b7e9279854412
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-28 04:39:46 +0000

    [Salvatore] [source:_plugins_/paypal/lang/ paypal] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@78906 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ paypal/lang/paypal_nl.php
@@ -0,0 +1,50 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paypal?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'api_prod' => 'Productie API',
+	'api_test' => 'Test API',
+
+	// C
+	'configuration_paypal' => 'Paypal',
+
+	// D
+	'description_paypal' => 'Configureer het online betaalsysteem Paypal',
+
+	// E
+	'explication_account' => 'Correspondeert met het emailadres van het account van de verkoper',
+	'explication_environnement' => 'Je kunt in dit formulier de gegevens invullen
+		die betrekking hebben op de testsite van Paypal (Sandbox, voor ontwikkeling),
+		maar ook die welke bestemd zijn voor echte transacties.
+		Welke omgeving wil je gebruiken?',
+	'explication_identifiant_vendeur' => 'Correspondeert met het emailadres van het account van de verkoper',
+	'explication_soumission' => 'Op welke bladzijde van Paypal is het formulier ingeschreven?',
+	'explication_tax' => 'Eventueel het toe te passen BTW-percentage',
+
+	// I
+	'info_bouton_paypal' => 'Doe je betalingen via PayPal:
+		een oplossing die snel, veilig en gratis is',
+
+	// L
+	'label_account' => 'Account (email)',
+	'label_currency_code' => 'Munteenheid',
+	'label_currency_code_eur' => 'Euro (€)',
+	'label_currency_code_usd' => 'Dollar ($)',
+	'label_environnement' => 'Omgeving',
+	'label_environnement_prod' => 'Productieomgeving',
+	'label_environnement_test' => 'Testomgeving',
+	'label_identifiant_vendeur' => 'Identificatie van de verkoper',
+	'label_password' => 'API Password',
+	'label_signature' => 'Ondertekening',
+	'label_soumission' => 'Inschrijven van het formulier',
+	'label_tax' => 'Belasting',
+	'label_username' => 'API Username (gebruikersnaam)'
+);
+
+?>
