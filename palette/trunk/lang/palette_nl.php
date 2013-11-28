c7371ceb212632e0fadbcc75555f244f634e274c
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-28 04:39:41 +0000

    [Salvatore] [source:_plugins_/palette/trunk/lang/ palette] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@78904 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ palette/trunk/lang/palette_nl.php
@@ -0,0 +1,26 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/palette?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'config_desc' => '<strong>Palette</strong> laadt automatisch
+		een kleurselectie toe aan een invoerveld
+		door hem eenvoudig de CSS class « palette » te geven',
+	'config_exemple' => 'Voorbeeld:',
+	'config_lien_doc' => '<a href="http://contrib.spip.net/Palette" class="spip_out">Zie documentatie</a>',
+
+	// F
+	'fermer' => 'Sluit het palet',
+
+	// P
+	'palette' => 'Palet(te)',
+	'palette_ecrire' => 'Activeer Palet in het privé-gedeelte',
+	'palette_public' => 'Activeer Palet op de publieke site'
+);
+
+?>
