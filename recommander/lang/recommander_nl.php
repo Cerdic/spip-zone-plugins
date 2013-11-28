9412e0fc266abded56a45affa538752df8ee3fa7
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-28 04:39:54 +0000

    [Salvatore] [source:_plugins_/recommander/lang/ recommander] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@78909 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ recommander/lang/recommander_nl.php
@@ -0,0 +1,20 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/recommander?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// D
+	'destinataire' => 'Bestemming',
+
+	// R
+	'recommander' => 'Beveel deze bladzijde aan',
+	'recommander_lecture' => 'Hallo, @from@ adviseert je deze bladzijde te lezen: ',
+	'recommander_message' => 'Recommander',
+	'recommander_titre' => 'Te lezen op @nom_site@ - '
+);
+
+?>
