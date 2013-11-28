b17184ec3ead5ac1809fd65841c2842c775b1dc7
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-28 00:23:12 +0000

    On classe les champs par ordre alphabétique, c'est plus compréhensible. Dans la documentation, on s'en fiche un peu de savoir l'ordre des champs dans la table. Le répertoire c'est 'extra' sans 's' ON rajoute la class 'formulaire_spip' pour bénéficier un peu des feuilles de styles fournis par SPIP. Le lien vers la page de documentation s'affiche que si on est webmestre. Un simple rédacteur n'a pas besoin de ce type d'info. On incrémente.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@78893 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- doc_tech/trunk/doc_tech_autorisations.php
+++ doc_tech/trunk/doc_tech_autorisations.php
@@ -14,10 +14,30 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 
 /**
  * Fonction d'appel pour le pipeline
- * @pipeline autoriser */
+ * @pipeline autoriser 
+ */
 function doc_tech_autoriser(){}
 
-
-
+/**
+ * Le lien vers la page de documentation technique ne s'affiche 
+ * que si on est webmestre
+ *
+ * @param string $faire
+ *              L'action à faire
+ * @param string $type
+ *              Le type d'objet sur lequel porte l'action
+ * @param int $id
+ *              L'identifiant numérique de l'objet
+ * @param array $qui
+ *              Les éléments de session de l'utilisateur en cours
+ * @param array $opt
+ *              Les options
+ * @return boolean true/false
+ *              true si autorisé, false sinon
+ */
+function autoriser_doc_tech_menu_dist($faire, $type, $id, $qui, $opt) {
+	if(($qui['webmestre'] == 'oui') && $qui['statut'] == '0minirezo')
+		return true;
+}
 
 ?>
