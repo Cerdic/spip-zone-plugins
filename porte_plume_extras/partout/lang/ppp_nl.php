92c18c4ed43cb8404c79add78095f50de300ef35
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-28 04:39:49 +0000

    [Salvatore] [source:_plugins_/porte_plume_extras/partout/lang/ ppp] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@78907 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ porte_plume_extras/partout/lang/ppp_nl.php
@@ -0,0 +1,32 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/ppp?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'bouton_mettre_a_jour' => 'Aanpassen',
+
+	// C
+	'cfg_attention' => 'Let op',
+	'cfg_description' => 'in functie van je skeletten moet je ervoor zorgen Porte Plume niet te openen in velden waarbij het gebruik xhtml-fouten zou kunnen veroorzaken.',
+	'cfg_titre' => 'Porte Plume Partout',
+
+	// E
+	'explication_personnalisation' => 'Vermeld het kenmerk van de elementen die de  typografische toolbar gebruiken (CSS of uitgebreide jQuery).',
+
+	// L
+	'label_hauteur_champ' => 'Halve schermhoogte',
+	'label_personnalisation' => 'Persoonlijke keuze',
+	'legend_barre_typo' => 'Activeer de typografische knoppenbalk op:',
+	'legend_hauteur_champ' => 'Hoogte van het tekstveld voor artikelen',
+	'legend_personnalisation' => 'Personalisering',
+
+	// S
+	'supprimer' => 'Zet alle standaardwaardes terug'
+);
+
+?>
