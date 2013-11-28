b2b3ce4923ae1d376b4d03315fdb9f5d6e826c04
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-28 04:40:03 +0000

    [Salvatore] [source:_plugins_/socialtags/lang/ paquet-socialtags] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@78912 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ socialtags/lang/paquet-socialtags_nl.php
@@ -0,0 +1,17 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-socialtags?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// S
+	'socialtags_description' => 'Met plugin Social Tags kun je iconen toevoegen om links te delen op sites als Digg, Facebook, Delicious...
+Wanneer je plugin Google+1 hebt geactiveerd, kan ook hiervoor een knop aan de lijst van Social tags worden toegevoegd.
+Bovendien kun je met deze plugin Facebook badges en een "Like" box opnemen.',
+	'socialtags_slogan' => 'Activeer knoppen om links te delen op sociale netwerken.'
+);
+
+?>
