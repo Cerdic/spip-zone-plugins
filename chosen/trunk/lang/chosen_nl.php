f86c76d93ee057dec669f196c5530d9ee1303a22
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-28 04:39:15 +0000

    [Salvatore] [source:_plugins_/chosen/trunk/lang/ chosen] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@78897 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ chosen/trunk/lang/chosen_nl.php
@@ -0,0 +1,31 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/chosen?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cfg_exemple' => 'Voorbeeld',
+	'cfg_exemple_explication' => 'Uitleg van dit voorbeeld',
+	'cfg_titre_parametrages' => 'Instellingen',
+	'chosen_titre' => 'Chosen',
+
+	// E
+	'explication_selecteur' => 'Vermeld de doelgroep van elementen die Chosen in werking stellen. (CSS of uitgebreide jQuery)',
+
+	// L
+	'label_active' => 'Activeer "Chosen" in de publieke site',
+	'label_selecteur' => 'Keuzetoets',
+	'lang_create_option' => 'Aanmaken',
+	'lang_no_result' => 'Geen enkel resultaat',
+	'lang_select_an_option' => 'Kies een optie',
+	'lang_select_some_option' => 'Kies een of meer opties',
+
+	// T
+	'titre_page_configurer_chosen' => 'Chosen configureren'
+);
+
+?>
