00310443ec0485987a8435d8ba06fceb1b5fdccb
severo@rednegra.net <severo@rednegra.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-28 04:39:26 +0000

    [Salvatore] [source:_plugins_/diogene/diogene_complements/diogene_mots/trunk/lang/ diogene_mots] Export depuis http://trad.spip.net de la langue es
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@78901 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- diogene/diogene_complements/diogene_mots/trunk/lang/diogene_mots_es.php
+++ diogene/diogene_complements/diogene_mots/trunk/lang/diogene_mots_es.php
@@ -7,11 +7,17 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
+	// E
+	'erreur_confirmer_creation_mots_nouveaux' => '¿Confirmar la creación en este grupo?', # RELIRE
+	'erreur_mot_dans_autre_groupe' => 'La palabra-clave @mot@ ya existe en el grupo @groupe@. ',
+
 	// F
 	'form_legend' => 'Selector de palabras clave',
 	'form_legend_public' => 'Palabras clave',
 
 	// L
+	'label_montrer_titre_et_descriptif' => 'Si existe la descripción de la palabra-clave, mostrarla entre paréntesis.',
+	'label_mots_creer_dans_public' => 'Permitir la creación de nuevas palabras-clave en estos grupos',
 	'label_mots_facultatifs' => 'Grupos de palabras facultativas',
 	'label_mots_obligatoires' => 'Grupos de palabras obligatorias'
 );
