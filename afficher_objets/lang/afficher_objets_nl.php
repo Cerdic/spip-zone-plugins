4f938ba6ca37bd22930cbbb27df5a1c728318678
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-28 04:39:10 +0000

    [Salvatore] [source:_plugins_/afficher_objets/lang/ afficher_objets] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@78896 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ afficher_objets/lang/afficher_objets_nl.php
@@ -0,0 +1,20 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/afficher_objets?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// I
+	'info_statut' => 'Status',
+
+	// N
+	'nb_articles' => '@nb@ artikelen',
+
+	// R
+	'rang' => 'Positie'
+);
+
+?>
