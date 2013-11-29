0dd1ae571e939957e6d62502e6f6a83d5109b6a9
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-29 14:44:44 +0000

    On utilise valeur_non = non pour le champ séminaire
    On nécessite la dernière version de saisies pour que la valeur_non soit bien prise en compte dans la saisie oui_non
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@78961 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- seminaire/trunk/base/seminaire.php
+++ seminaire/trunk/base/seminaire.php
@@ -26,6 +26,8 @@ function seminaire_declarer_champs_extras($champs = array()){
 			'nom' => 'seminaire',
 			'label' => _T('seminaire:label_seminaire'), 
 			'sql' => "varchar(3) NOT NULL DEFAULT 'non'", // declaration sql
+			'valeur_non' => "non",
+			'defaut' => 'non',
 			'rechercher'=>false
 	));
 	$champs['spip_evenements']['attendee'] = array(
