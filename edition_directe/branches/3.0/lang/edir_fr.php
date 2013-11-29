2d79c62a1bfd3958f0358036c07e924807a85c01
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-11-29 17:07:34 +0000

    langues
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@78964 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- edition_directe/branches/3.0/lang/edir_fr.php
+++ edition_directe/branches/3.0/lang/edir_fr.php
@@ -6,11 +6,11 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
-	'activer_edition_directe' => 'Activer l\'édition directe pour :',
-	'activer_edition_directe_objet' => 'Activer l\'édition directe pour l\'objet ',
+	'activer_edition_directe' => 'Activer l’édition directe pour :',
+	'activer_edition_directe_objet' => 'Activer l’édition directe pour l’objet ',
 
 	// D
-	'desactiver_edition_directe_objet' => 'Désactiver l\'édition directe pour l\'objet ',
+	'desactiver_edition_directe_objet' => 'Désactiver l’édition directe pour l’objet ',
 
 	// T
 	'titre_plugin' => 'Edition directe',
---
+++ edition_directe/branches/3.0/lang/edir_nl.php
@@ -0,0 +1,24 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/edir?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'activer_edition_directe' => 'Activeer directe aanpassing voor:',
+	'activer_edition_directe_objet' => 'Activeer directe aanpassing voor het object',
+
+	// D
+	'desactiver_edition_directe_objet' => 'Stop directe aanpassing van het object',
+
+	// T
+	'titre_plugin' => 'Directe aanpassing',
+
+	// V
+	'vider_cache' => 'Maak de cache leeg'
+);
+
+?>
