85f6aa34bd84920840c998fdd1b456e7fd492bd0
p@henix.be <p@henix.be@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-12-02 21:56:40 +0000

    On introduit la balise #COLONNES
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79078 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- foundation-4-spip/trunk/foundation_fonctions.php
+++ foundation-4-spip/trunk/foundation_fonctions.php
@@ -27,3 +27,78 @@ function iframe_responsive($texte) {
     // On détecte tout les iFrames et on les rends responsives.
     return preg_replace_callback('/<iframe(.+)><\/iframe>/', 'responsive', $texte);
 }
+
+/*
+*   Pas très élégant, cette fonction va renvoyer le nombre en toutes lettre.
+*   Dans ce cas si, cela suffit largement puisqu'il n'y a que 12 chiffres possible.
+*   C'est utiliser pour foundation 2 et 3.
+*/
+function toWords($number) {
+
+    // Ce tableau fait office de table de conversion.
+    $conversion = array(
+        1 => 'one',
+        2 => 'two',
+        3 => 'three',
+        4 => 'four',
+        5 => 'five',
+        6 => 'six',
+        7 => 'seven',
+        8 => 'eight',
+        9 => 'nine',
+        11 => 'eleven',
+        12 => 'twelve'
+        );
+
+    // On revoie la bonne valeur.
+    if (is_numeric($number))
+        return $conversion[$number];
+    else return false;
+}
+
+
+/*
+*   Cette fonction va lire la configuration de foundation et determiner quel syntaxe doit être utilisé.
+*/
+function trouver_syntaxe_foundation($nombre_colonnes, $type) {
+
+    // On récupère la configuration
+    $config = lire_config('foundation');
+
+    // Version qui utilise un système large-X ou small-X. J'appel ce groupe les colnum.
+    $colnum = array(5,4);
+
+    // Les versions qui utilise des lettres => les colletr
+    $colettr = array(2,3);
+
+    // on cherche
+    // Si on est dans une vesion numérique de foundation, on retourne la syntaxe
+    if (in_array($config['variante'], $colnum)) 
+        return $type.'-'.$nombre_colonnes;
+    // Sinon, on démarrer le moteur de conversion de nombre, et on renvoie la bonne class
+    elseif (in_array($config['variante'], $colettr)) 
+        return toWords($nombre_colonnes);
+}
+
+/*
+*   Cette balise va permettre de rendre le squelette compatible avec toutes les versions de Foundation.
+*   La syntaxe est la suivante:
+*
+*   #COLONNES{nombre,type}
+*   nombre: le nombre de colonne foundation
+*   (optionnel) type: Dans le cas des version utilisant une syntaxe avec prefix, on lui passe le type (défaut: large)
+*/
+function balise_COLONNES_dist($p) {
+    // On récupère les paramètres de la balise.
+    $nombre_colonnes = interprete_argument_balise(1, $p);
+    $type = interprete_argument_balise(2, $p);
+
+    // On met une valeur par défaut à type.
+    if (!$type) $type = 'large';
+
+    $p->code = "trouver_syntaxe_foundation($nombre_colonnes, $type).' columns'";
+    $p->interdire_scripts = false;
+    return $p;
+}
+
+?>
