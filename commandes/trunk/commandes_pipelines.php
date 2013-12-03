22c2281f874173f1e622d24b9f2c133203b2bb52
tcharlss@hotmail.fr <tcharlss@hotmail.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-12-03 19:43:11 +0000

    Retour partiel sur r72558 Boulette : ne pas mettre systématiquement à jour  les dates de paiement et d'envoi lors du changement de statut. Les dates effectives de paiement et d'envoi ne correspondent pas forcément au moment ou le rédacteur change le satut de la commande. On ne le fait donc que lorsque celles-ci n'ont pas été définies, en cas d'oubli. Donc on zappe la pipeline pre-edition et on passe ça en post-edition, juste avant l'envoi des notifications. Incrémentation de z
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79122 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- commandes/trunk/commandes_pipelines.php
+++ commandes/trunk/commandes_pipelines.php
@@ -113,68 +113,66 @@ function commandes_affiche_auteurs_interventions($flux) {
 
 
 /**
- * Mettre à jour les dates de paiement ou d'envoi
- * en amont de la modification du statut d'une commande
+ * Traitements après l'édition d'une commande
+ * Quand on  modifie le satut d'une commande :
+ * - Mettre à jour les dates de paiement et d'envoi si par mégarde ça n'a pas été fait manuellement
+ * - Envoyer les notifications 
  *
  * @param array $flux
- *	$flux['args'][x] = arguments (action, table, id_objet etc.)
- *	$flux['data'][x] = champs modifiés (statut, date etc.)
- * @return array $flux
-**/
-function commandes_pre_edition($flux){
-
-	if (
-		($action=$flux['args']['action']) == 'instituer' // action instituer
-		and ($table=$flux['args']['table']) == table_objet_sql('commande') // on institue une commande
-		and $statut = $flux['data']['statut']
-		and $date = date('Y-m-d H:i:s') // todo : copier la date de maj ?
-	) {
-		switch ($statut) {
-			case 'paye';
-				$flux['data']['date_paiement'] = $date;
-				break;
-			case 'envoye';
-				$flux['data']['date_envoi'] = $date;
-				break;
-		}
-	}
-
-	return($flux);
-}
-
-
-/**
- * Envoyer les notifications 
- * en aval de la modification du statut d'une commande
- *
- * @param array $flux
- *	$flux['args'][x] = arguments (table, id_objet, action, statut_ancien, date_ancienne, id_parent_ancien)
- *	$flux['data'][x] = champs modifiés (statut, date_paiement etc.)
+ *     $flux['args'][x] = arguments (table, id_objet, action, statut_ancien, date_ancienne, id_parent_ancien)
+ *     $flux['data'][x] = champs modifiés (statut, date_paiement etc.)
  * @return array $flux
 **/
 function commandes_post_edition($flux){
 
 	if (
-		($action=$flux['args']['action']) == 'instituer' // action instituer
-		and ($table=$flux['args']['table']) == table_objet_sql('commande') // on institue une commande
-		and ($statut_ancien=$flux['args']['statut_ancien']) != ($statut=$flux['data']['statut']) // le statut a été modifié
+		($action = $flux['args']['action']) == 'instituer' // action instituer
+		and ($table = $flux['args']['table']) == table_objet_sql('commande') // on institue une commande
+		and ($statut_ancien = $flux['args']['statut_ancien']) != ($statut = $flux['data']['statut']) // le statut a été modifié
 		and $id_commande = $flux['args']['id_objet'] // on a bien un identifiant pour la commande
-		and include_spip('inc/config')
-		and $config = lire_config('commandes')
-		and $quand = $config['quand'] ? $config['quand'] : array()
-		and ($config['activer']) // les notifications sont activées
-		and (in_array($statut, $quand)) // le nouveau statut est valide pour envoyer une notification
-		and ($notifications = charger_fonction('notifications', 'inc', true)) // la fonction est bien chargée
 	) {
-		// Déterminer l'expediteur
-		$options = array();
-		if( $config['expediteur'] != "facteur" )
-			$options['expediteur'] = $config['expediteur_'.$config['expediteur']];
 
-		// Envoyer au vendeur et au client
-		$notifications('commande_vendeur', $id_commande, $options);
-		if($config['client'])
-			$notifications('commande_client', $id_commande, $options);
+		// Mise à jour des dates de paiement ou d'envoi si ça n'a pas été fait manuellement
+		if (
+			in_array($statut, array('paye','envoye'))
+			and $maj = sql_getfetsel('maj', table_objet_sql('commande'), "id_commande=".sql_quote($id_commande) )
+			and $date_paiement = sql_getfetsel('date_paiement', table_objet_sql('commande'), "id_commande=".sql_quote($id_commande) )
+			and $date_envoi = sql_getfetsel('date_envoi', table_objet_sql('commande'), "id_commande=".sql_quote($id_commande) )
+			and include_spip('action/editer_objet')
+		) {
+			if (
+				$statut == 'paye'
+				and intval($date_paiement) == 0
+			){
+				objet_modifier('commande', $id_commande, array('date_paiement'=>$maj));
+			}
+			if (
+				$statut == 'envoye'
+				and intval($date_envoi) == 0
+			){
+				objet_modifier('commande', $id_commande, array('date_envoi'=>$maj));
+			}
+		}
+
+		// Envoi des notifications
+		if (
+			include_spip('inc/config')
+			and $config = lire_config('commandes')
+			and $quand = $config['quand'] ? $config['quand'] : array()
+			and ($config['activer']) // les notifications sont activées
+			and (in_array($statut, $quand)) // le nouveau statut est valide pour envoyer une notification
+			and ($notifications = charger_fonction('notifications', 'inc', true)) // la fonction est bien chargée
+		) {
+			// Déterminer l'expediteur
+			$options = array();
+			if( $config['expediteur'] != "facteur" )
+				$options['expediteur'] = $config['expediteur_'.$config['expediteur']];
+
+			// Envoyer au vendeur et au client
+			$notifications('commande_vendeur', $id_commande, $options);
+			if($config['client'])
+				$notifications('commande_client', $id_commande, $options);
+		}
 	}
 
 	return($flux);
