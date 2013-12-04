57d1038793323f683da174197e0e0518a02c9939
tcharlss@hotmail.fr <tcharlss@hotmail.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-12-04 11:08:47 +0000

    Correction de la construction du lien : cette fonction ne devrait renvoyer quelque chose que si le champ correspondant du formulaire de configuration a été rempli. Incrémentation de z. cf. http://contrib.spip.net/Manuel-de-redaction-du-site#forum471101
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79136 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- manuel_site/prive/squelettes/inclure/manuelsite_article_fonctions.php
+++ manuel_site/prive/squelettes/inclure/manuelsite_article_fonctions.php
@@ -8,16 +8,18 @@
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 function construire_lien($t){
-	if (version_compare($GLOBALS['spip_version_branche'], '3.O', '>=')){
-		$table = table_objet_sql($t);
-		$trouver_table = charger_fonction('trouver_table', 'base');
-		$desc = $trouver_table($table);		
-	}else{
-		$desc['url_edit'] = concat($t,'_edit');
-		
+	if ($t) {
+		if (version_compare($GLOBALS['spip_version_branche'], '3.O', '>=')){
+			$table = table_objet_sql($t);
+			$trouver_table = charger_fonction('trouver_table', 'base');
+			$desc = $trouver_table($table);		
+		} else {
+			$desc['url_edit'] = concat($t,'_edit');
+		}
+		$url = parametre_url(generer_url_ecrire($desc['url_edit']),'new','oui');
+	} else {
+		$url = '';
 	}
-	$url = parametre_url(generer_url_ecrire($desc['url_edit']),'new','oui');
-	
 	return $url;
 }
 
