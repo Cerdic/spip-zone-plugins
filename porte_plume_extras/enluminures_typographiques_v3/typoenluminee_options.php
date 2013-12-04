f999da39b1bf55d82b266e0808ddab89e2a8004f
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-12-04 07:49:54 +0000

    On se prépare à SPIP 3.0.14, SPIP 2.1.25 et SPIP 2.0.24
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79128 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ porte_plume_extras/enluminures_typographiques_v3/typoenluminee_options.php
@@ -0,0 +1,11 @@
+<?php
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+// autoriser les tag <b>, <i>, <em>, <sc> et <br />
+// dans le nom/signature d'un auteur.
+// par défaut, <multi> est toujours autorisé.
+// Cf : http://www.spip.net/fr_article5666.html
+// Cf : http://core.spip.org/projects/spip/repository/revisions/21016 à 21018
+if (!defined('_TAGS_LOGIN'))  define('_TAGS_LOGIN', 'b, i, em, sc, br');
+
+?>
