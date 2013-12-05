ea7544962162a14b4ddad0a6913aa3da3781f40f
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-12-05 04:40:12 +0000

    [Salvatore] [source:_plugins_/slogan/lang/ slogan] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79162 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ slogan/lang/slogan_nl.php
@@ -0,0 +1,20 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/slogan?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'config_info_enregistree' => 'De configuratie is opgeslagen',
+
+	// I
+	'info_slogan_site' => 'Slagzin van de site',
+
+	// T
+	'titre_identite_site' => 'Identiteit van de site'
+);
+
+?>
