675f2c14e01549f24b31b4353f88a300381770b9
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-12-05 04:40:14 +0000

    [Salvatore] [source:_plugins_/socialtags/lang/ socialtags] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79163 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ socialtags/lang/socialtags_nl.php
@@ -0,0 +1,77 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/socialtags?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cfg_activer_javascript' => 'Activeer Javascript.',
+	'cfg_badge_fb' => 'Facebook Badge/Widget',
+	'cfg_badge_fb_existe' => 'Wanneer je een Facebook Badge hebt (<a href="http://www.facebook.com/facebook-widgets/" class="spip_out">kijk op de Facebook site</a>), geef dan hieronder de inhoud, in JS of in HTML.',
+	'cfg_badge_html' => 'HTML Badge',
+	'cfg_badge_html_ajouter_1' => '(voeg toe aan je skelet',
+	'cfg_badge_html_ajouter_2' => ', of in een artikel',
+	'cfg_badge_js' => 'JS Badge',
+	'cfg_badge_js_1' => ' (automatisch opgenomen in <code>@code@</code>)',
+	'cfg_en_bas' => 'Onderaan iedere bladzijde',
+	'cfg_exemples' => 'Voorbeelden (gebruik makend van de dist-skeletten) :',
+	'cfg_explication_boite_info' => 'Kies hier de te delen sociale sites voor jouw artikelen.',
+	'cfg_inserer' => 'Voeg het social-tags-blok onder het volgende baken in:',
+	'cfg_insertion' => 'Invoegen in bladzijdes:',
+	'cfg_liste_site' => 'Liijst van sociale sites',
+	'cfg_plusone' => 'Google+1',
+	'cfg_plusone_existe' => 'Om Google+1 te gebruiken, moet je een plugin activeren: (<a href="http://contrib.spip.net/Google-1" class="spip_out">hulp op spip-contrib</a>).',
+	'cfg_referer' => 'Toon de badge en knopppen uitsluitend voor bezoekers afkomstig van die sites.',
+	'cfg_selector' => 'Keuze:',
+	'cfg_sous_bloc' => 'Onder het titelblok van artikelbladzijdes',
+	'cfg_sous_div' => 'Onder de div met naam id=socialtags',
+	'cfg_titre_boite_info' => 'Configuratie van de plugin Social tags',
+	'cfg_titre_socialtags' => 'Social tags',
+	'cfg_tout_cocher' => 'Alles selecteren',
+	'cfg_tout_decocher' => 'Selectei verwijderen',
+	'cfg_wopen' => 'Pop-up modus',
+	'cfg_wopen_exp' => 'Open de link in een nieuw venster',
+
+	// D
+	'description_noisette_badge_facebook' => 'Toon de gedefinieerde Facebook badge op de configuratiebladzijde',
+	'description_noisette_facebook_like' => 'Toon de Facebook <b>Like</b> of <b>Aanbeveel</b> knop en eventueel de <b>Verzend</b> knop',
+	'description_noisette_facebook_like_box' => 'Toon de Facebook <b>Like</b> box. Ze toont het aantal fans voor pagina',
+
+	// N
+	'noisette_action_like' => 'Like',
+	'noisette_action_recommand' => 'Aanbevelen',
+	'noisette_colorscheme_dark' => 'Donker lettertype',
+	'noisette_colorscheme_light' => 'Helder lettertype',
+	'noisette_label_action' => 'Actie',
+	'noisette_label_afficher_send' => 'Zend knop?',
+	'noisette_label_afficher_titre_noisette' => 'Een titel tonen?',
+	'noisette_label_border_color' => 'Geef de kleurcode van de rand van de box (in formaat #123ABC)',
+	'noisette_label_colorscheme' => 'Weergave',
+	'noisette_label_header' => 'De titel van de box « Vind ons op Facebook » tonen?',
+	'noisette_label_height' => 'Hoogte',
+	'noisette_label_href' => 'URL van de Facebook bladzijde',
+	'noisette_label_layout' => 'Tekst',
+	'noisette_label_niveau_titre' => 'Kies de hoogte van de titel',
+	'noisette_label_show_faces' => 'Profielfoto’s onder de knop weergeven?',
+	'noisette_label_stream' => 'De feed van de artikelen van de Facebook pagina weergeven?',
+	'noisette_label_titre_noisette' => 'Titel:',
+	'noisette_label_titre_noisette_perso' => 'Aangepaste titel:',
+	'noisette_label_width' => 'Breedte',
+	'noisette_layout_box_count' => 'Totaal uitsluitend boven',
+	'noisette_layout_button_count' => 'Totaal uuitsluitend rechts',
+	'noisette_layout_standard' => 'Volledige tekst rechts',
+	'noisette_titre_perso' => 'Aangepaste titel',
+	'nom_noisette_badge_facebook' => 'Facebook Badge',
+	'nom_noisette_facebook_like' => 'Facebook Like-knop',
+	'nom_noisette_facebook_like_box' => 'Facebook Like-box',
+
+	// T
+	'titre_menu' => 'Social tags',
+	'titre_mon_badge_facebook' => 'Mijn Facebook Badge',
+	'titre_sur_facebook' => 'Op Facebook'
+);
+
+?>
