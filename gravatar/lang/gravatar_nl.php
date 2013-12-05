54403d44c4bc8e21fd0b0a5e6b0922ba79aed481
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-12-05 04:39:41 +0000

    [Salvatore] [source:_plugins_/gravatar/lang/ gravatar] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79158 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ gravatar/lang/gravatar_nl.php
@@ -0,0 +1,35 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/gravatar?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cfg_descr_gravatar' => 'Configureer de weergave van gravatars',
+	'cfg_titre_gravatar' => 'Gravatars',
+
+	// E
+	'explication_image_defaut' => 'Wanneer je geen gebruikersafbeelding hebt, gebruik je:',
+
+	// G
+	'gravatar_info' => 'Om hier je gebruikersafbeelding te tonen moet je je eerst registreren op <a href="http://www.gravatar.com/" rel="external nofollow" class="spip_out">gravatar.com</a> (gratis en pijnloos).',
+	'gravatar_info_forum' => 'Om je gebruikersafbeelding bij je bericht te tonen moet je je eerst registreren op<a href="http://www.gravatar.com/" rel="external nofollow" class="spip_out">gravatar.com</a> (gratuit et indolore). Vergeet niet om hier je e-mailadres te vermelden.',
+
+	// L
+	'label_image_defaut' => 'Standaardafbeelding',
+	'label_image_defaut_404' => 'geen afbeelding',
+	'label_image_defaut_gravatar' => 'Gravatar logo',
+	'label_image_defaut_identicon' => '<i>Identicon</i> (gegenereerd uit het e-mailadres)',
+	'label_image_defaut_monsterid' => '<i>MonsterID</i> (gegenereerd uit het e-mailadres)',
+	'label_image_defaut_png' => '<tt>images/gravatar.png</tt>',
+	'label_image_defaut_wavatar' => '<i>Wavatar</i> (gegenereerd uit het e-mailadres)',
+	'label_taille' => 'Afmeting van de gebruikersafbeelding',
+
+	// T
+	'titre_gravatar_auteur' => 'Gravatar'
+);
+
+?>
