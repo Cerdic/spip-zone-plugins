604f984a98064cd343323dea442ce7ceed5c3a72
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-12-05 04:40:20 +0000

    [Salvatore] [source:_plugins_/tablesorter/lang/ paquet-tablesorter] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79164 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ tablesorter/lang/paquet-tablesorter_nl.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-tablesorter?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// T
+	'tablesorter_description' => 'Met deze plugin kun je tabellen die class CSS "spip" hebben sorteren door op de kop van een kolom te klikken. (Dit houdt in dat de HTML tabel dus de bakens thead en tbody moet bevatten.) Alle sorteeropties vind je op [->http://tablesorter.com].',
+	'tablesorter_slogan' => 'Sorteer tabellen met tableSorter!'
+);
+
+?>
