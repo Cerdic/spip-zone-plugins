f97c4bb43f433a13a67662c5ef2e72d74a833b13
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-12-05 04:40:08 +0000

    [Salvatore] [source:_plugins_/simplog/trunk/lang/ simplog] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79160 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ simplog/trunk/lang/simplog_nl.php
@@ -0,0 +1,45 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/simplog?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'actualiser' => 'Werk de lijst bij',
+
+	// C
+	'clic_rafraichir' => 'Laadt het bestand opnieuw',
+	'colonne_date' => 'Datum',
+	'colonne_gravite' => 'Ernst',
+	'colonne_hit' => 'Hit',
+	'colonne_ip' => 'IP',
+	'colonne_pid' => 'PID',
+	'colonne_texte' => 'Tekst',
+
+	// D
+	'description_simplog' => 'Op deze bladzijde kun je op eenvoudige wijze de logbestanden van SPIP inzien.',
+
+	// F
+	'fichier' => 'Bestand« @fichier@ »',
+
+	// I
+	'info_hit_pri' => 'Privé',
+	'info_hit_pub' => 'Openbaar',
+
+	// L
+	'liste_fic' => 'Lijst van logs',
+	'logs' => 'Logs raadplegen',
+
+	// M
+	'message_fichier_choisir' => 'Kies een te analyseren bestand.',
+	'message_fichier_inconnu' => 'Het bestand "@fichier@" is niet beschikbaar.',
+
+	// T
+	'tri_croissant' => 'Oplopend sorteren',
+	'tri_decroissant' => 'Aflopend sorteren'
+);
+
+?>
