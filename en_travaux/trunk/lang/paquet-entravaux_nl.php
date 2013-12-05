bcc74ccbb15676f8f46bd8f0ab1a18a6a0ddf45a
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-12-05 04:39:37 +0000

    [Salvatore] [source:_plugins_/en_travaux/trunk/lang/ paquet-entravaux] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79157 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ en_travaux/trunk/lang/paquet-entravaux_nl.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-entravaux?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// E
+	'entravaux_description' => 'Voordat je site in onderhoud gaat, activeer je met deze plugin een waarschuwingsbladzijde, {{in plaats}} van de publieke site. Let op: alleen de webmaster heeft nog toegang tot het privé-gedeelte en de publieke site.',
+	'entravaux_nom' => 'Werk in uitvoering',
+	'entravaux_slogan' => 'Geef een onderhoudsfase aan'
+);
+
+?>
