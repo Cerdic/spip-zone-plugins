b4e39ab993f7fd35a9c1d5a713301da6f7e9f754
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-12-05 04:39:35 +0000

    [Salvatore] [source:_plugins_/en_travaux/trunk/lang/ entravaux] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79156 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ en_travaux/trunk/lang/entravaux_nl.php
@@ -0,0 +1,37 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/entravaux-193?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'activer_message' => 'Het onderhoud activeren',
+
+	// E
+	'en_travaux' => 'Werk in uitvoering',
+	'erreur_droit' => 'Je moet webmaster zijn om deze plugin te kunnen gebruiken!',
+
+	// I
+	'info_disallow_robot' => 'Tijdens onderhoud is je website beschermd tegen robots en zoekmachines.',
+	'info_maintenance_en_cours' => 'De site is in onderhoud! Alleen de webmaster heeft toegang tot de publieke site en het privé-gedeelte.',
+	'info_message' => 'Je kunt onderhoud van de site activeren en op alle bladzijdes een tijdelijke waarschuwingsboodschap weergeven.',
+	'info_travaux_texte' => 'Deze site is momenteel in onderhoud.
+_ Kom later terug.',
+
+	// L
+	'label_disallow_robots' => 'Bescherm de site tegen robots en zoekmachines',
+
+	// M
+	'message_temporaire' => 'Je tijdelijke boodschap:',
+
+	// P
+	'parametrage_page_travaux' => 'Instelling van de tijdelijke badzijde',
+
+	// T
+	'texte_lien_publier' => 'Maak de site weer toegankelijk'
+);
+
+?>
