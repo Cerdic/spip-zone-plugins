06193737794237b029c8ecc7d4c1407d0bec7f7c
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-12-06 04:37:57 +0000

    [Salvatore] [source:_plugins_/pages/trunk/lang/ paquet-pages] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79192 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ pages/trunk/lang/paquet-pages_nl.php
@@ -0,0 +1,17 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-pages?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// P
+	'pages_description' => 'Met deze plugin kun je bladzijdes met artikelen maken die niet in de hiërarchie van de site zijn opgenomen.
+Ze kunnen worden gekoppeld aan de naam van een skelet.
+Op deze manier kunnen bladzijdes zoals wettelijke vermeldingen, contactinformatie, disclaimers, enz. worden gemaakt.',
+	'pages_slogan' => 'Bladzijdes zonder rubriek'
+);
+
+?>
