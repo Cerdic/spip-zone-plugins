3557216b0e07404a99bd7c8f4aefa9f387d993be
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-12-06 04:37:55 +0000

    [Salvatore] [source:_plugins_/pages/trunk/lang/ pages] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79191 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ pages/trunk/lang/pages_nl.php
@@ -0,0 +1,37 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/pages?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'aucune_page' => 'Er is momenteel geen enkele bladzijde.',
+
+	// C
+	'convertir_article' => 'In een artikel omzetten',
+	'convertir_page' => 'In een bladzijde omzetten',
+	'creer_page' => 'Een nieuwe bladzijde maken',
+
+	// E
+	'erreur_champ_page_doublon' => 'Deze identificatie bestaat al',
+	'erreur_champ_page_format' => 'Uitsluitend kleine letters, cijfers of "_"',
+	'erreur_champ_page_taille' => 'maximaal 255 tekens',
+
+	// L
+	'label_champ_page' => 'Bladzijde:',
+
+	// M
+	'modifier_page' => 'Aanpassen van bladzijde:',
+
+	// P
+	'pages_uniques' => 'Unieke bladzijdes',
+
+	// T
+	'titre_page' => 'Bladzijde',
+	'toutes_les_pages' => 'Alle bladzijdes'
+);
+
+?>
