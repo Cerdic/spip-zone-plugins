6e06d0c1720405de6098a739b70a7441196c1002
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-12-07 11:17:39 +0000

    Suivre le nomage du core : http://core.spip.org/projects/spip/repository/revisions/21032
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79211 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- porte_plume_extras/enluminures_typographiques_v3/typoenluminee_options.php
+++ porte_plume_extras/enluminures_typographiques_v3/typoenluminee_options.php
@@ -6,6 +6,6 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 // par défaut, <multi> est toujours autorisé.
 // Cf : http://www.spip.net/fr_article5666.html
 // Cf : http://core.spip.org/projects/spip/repository/revisions/21016 à 21018
-if (!defined('_TAGS_LOGIN'))  define('_TAGS_LOGIN', 'b, i, em, sc, br');
+if (!defined('_TAGS_NOM_AUTEUR'))  define('_TAGS_NOM_AUTEUR', 'b, i, em, sc, br');
 
 ?>
