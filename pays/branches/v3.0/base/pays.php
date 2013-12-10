c3f4bcd777ec231be8b317697922d1f94f4f3749
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-12-10 00:39:27 +0000

    L'update de la v2 à la v3 se fait sans soucis. La table est bien mise à jour. > Ce qui est 'bizarre' c'est que sql_insertq n'a pas besoin de sql_quote au contraire de sql_update… Le fichier de peuplement est à mis à jour en conséquence.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79255 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- pays/branches/v3.0/base/pays.php
+++ pays/branches/v3.0/base/pays.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Plugin Pays pour Spip 2.0
+ * Plugin Pays pour Spip 3.0
  * Licence GPL
  * Auteur Organisation Internationale de Normalisation http://www.iso.org/iso/fr/country_codes/iso_3166_code_lists.htm
  * Cedric Morin et Collectif SPIP pour version spip_geo_pays
--- pays/branches/v3.0/lang/paquet-pays_fr.php
+++ pays/branches/v3.0/lang/paquet-pays_fr.php
@@ -13,7 +13,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 // P
-	'pays_description' => 'Ce plugin fournit la liste officielle des pays élaborée par l\'[Organisation Internationale de Normalisation->http://www.iso.org/iso/fr/country_codes/iso_3166_code_lists.htm]. La table des pays inclut le champ numérique id_pays issu du plugin spip_geographie, le code ISO3166-1 et le nom du pays dans différents langages.',
+	'pays_description' => 'Ce plugin fournit la liste officielle des pays élaborée par l\'[Organisation Internationale de Normalisation->http://www.iso.org/iso/fr/country_codes/iso_3166_code_lists.htm]. La table des pays inclut le champ numérique id_pays issu du plugin spip_geographie, le code ISO 3166-1 alpha2, le code alpha3, le code numérique et le nom du pays dans différents langages.',
 	'pays_nom' => 'Pays ISO 3166-1',
 	'pays_slogan' => 'Liste officielle des pays',
 );
