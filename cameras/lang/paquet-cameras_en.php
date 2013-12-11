fbfb34559394c7304b947c46dac050ecba226777
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-12-11 04:37:47 +0000

    [Salvatore] [source:_plugins_/cameras/lang/ paquet-cameras] Export depuis http://trad.spip.net
    La langue 'en' devrait Ãªtre supprimÃ©e car trop peu traduite (33.33 %)
    
    
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79275 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- cameras/lang/paquet-cameras_en.php
+++ cameras/lang/paquet-cameras_en.php
@@ -1,8 +1,14 @@
 <?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-cameras?lang_cible=en
+// ** ne pas modifier le fichier **
+
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-	'cameras_nom' => 'Cameras',
+
+	// C
+	'cameras_nom' => 'Cameras'
 );
 
 ?>
--- cameras/lang/paquet-cameras_fr.php
+++ cameras/lang/paquet-cameras_fr.php
@@ -1,10 +1,14 @@
 <?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/cameras/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-	'cameras_nom' => 'Cam&eacute;ras',
-	'cameras_slogan' => 'Plugin de g&eacute;omarquage des cam&eacute;ras',
-	'cameras_description' => 'Ce plugin permet de g&eacute;olocaliser des cam&eacute;ras de vid&eacute;osurveillances.',
+
+	// C
+	'cameras_description' => 'Ce plugin permet de géolocaliser des caméras de vidéosurveillances.',
+	'cameras_nom' => 'Caméras',
+	'cameras_slogan' => 'Plugin de géomarquage des caméras'
 );
 
 ?>
