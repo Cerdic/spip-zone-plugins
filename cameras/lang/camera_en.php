163fe4530a6d84157290dc115f0637c9d32148c9
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-12-11 04:37:43 +0000

    [Salvatore] [source:_plugins_/cameras/lang/ camera] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79274 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- cameras/lang/camera_en.php
+++ cameras/lang/camera_en.php
@@ -1,98 +1,100 @@
 <?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/camera?lang_cible=en
+// ** ne pas modifier le fichier **
+
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// B
-	'bouton_pointer_sur_carte' => 'Point on the map',
 	'bouton_deplacer_camera' => 'Drag camera',
+	'bouton_pointer_sur_carte' => 'Point on the map',
 	'bouton_saisie_manuelle' => 'Manual entry',
 
-	//C
-	'cfg_titre_menu_camera' => 'Cameras',
+	// C
+	'cameras_recents' => 'Most recent published cameras',
 	'cfg_titre_camera' => 'Camera plugin configuration',
-	'cameras_recents'	=> 'Most recent published cameras',
+	'cfg_titre_menu_camera' => 'Cameras',
 
 	// I
 	'icone_creer_camera' => 'Create a camera',
 	'icone_modifier_camera' => 'Edit this camera',
-	'info_aucun_camera' => 'No cameras',
 	'info_1_camera' => 'One camera',
-	'info_nb_cameras' => '@nb@ cameras',
+	'info_aucun_camera' => 'No cameras',
+	'info_cameras_auteur' => 'This authors’ cameras',
 	'info_cameras_valider' => 'Cameras requiring validation',
-	'info_cameras_auteur' => 'This authors\' cameras',
 	'info_inconnu' => 'unknown',
-	
+	'info_nb_cameras' => '@nb@ cameras',
+
 	// L
+	'label_actualite_globale' => 'Global news',
+	'label_actualite_locale' => 'Local news',
+	'label_angle' => 'Angle/ground',
+	'label_apparence' => 'Appearance',
+	'label_apparence_boite' => 'Boxed',
+	'label_apparence_dome' => 'Dome',
+	'label_apparence_encastre' => 'Embeded',
+	'label_apparence_nue' => 'Bare',
+	'label_apparence_radar' => 'Radar',
+	'label_aucune_description' => 'No description',
+	'label_choisir_apparence' => 'Choose camera’s apparence',
+	'label_config_auth' => 'Disable camera edition',
 	'label_config_rubrique' => 'Default section',
 	'label_config_rubrique_auto' => 'Automatic section',
 	'label_config_titre' => 'Automatic title',
-	'label_config_auth' => 'Disable camera edition',
-	'label_oui' => 'Yes',
-	'label_non' => 'No',
-	'label_titre' => 'Location',
-	'label_type' => 'Camera type',
-	'label_apparence' => 'Appearance',
-	'label_choisir_apparence' => 'Choose camera\'s apparence',
-	'label_apparence_nue' => 'Bare',
-	'label_apparence_dome' => 'Dome',
-	'label_apparence_encastre' => 'Embeded',
-	'label_apparence_boite' => 'Boxed',
-	'label_apparence_radar' => 'Radar',
-	'label_zone'	=>	'Zone',
+	'label_date' => 'Camera signaled on',
+	'label_description' => 'Description',
+	'label_direction' => 'Direction',
+	'label_height' => 'Height',
 	'label_lat' => 'Latitude',
 	'label_lon' => 'Longitude',
-	'label_angle' => 'Angle/ground',
-	'label_direction' => 'Direction',
-	'label_zoom'	=> 'Zoomable?',
-	'label_orientable'	=>	'Orientable',
-	'label_description' => 'Description',
+	'label_non' => 'No',
 	'label_non_renseigne' => 'No data entered',
 	'label_non_renseignee' => 'No data entered',
-	'label_aucune_description' => 'No description',
-	'label_op_type' => 'Operator type',
 	'label_op_choisir_type' => 'Public or private ?',
-	'label_op_type_public' => 'Public',
-	'label_op_type_prive' => 'Private',
 	'label_op_name' => 'Operator name',
-	'label_height' => 'Height',
+	'label_op_type' => 'Operator type',
+	'label_op_type_prive' => 'Private',
+	'label_op_type_public' => 'Public',
+	'label_orientable' => 'Orientable',
+	'label_oui' => 'Yes',
 	'label_surveillance' => 'Surveillance',
 	'label_surveillance_choisir_type' => 'Which surveillance type ?',
+	'label_surveillance_type_banque' => 'Bank',
+	'label_surveillance_type_batiment' => 'Building',
+	'label_surveillance_type_commerce' => 'Shop',
 	'label_surveillance_type_municipale' => 'Town',
 	'label_surveillance_type_parking' => 'Parking',
 	'label_surveillance_type_traffic' => 'Traffic',
-	'label_surveillance_type_commerce' => 'Shop',
-	'label_surveillance_type_banque' => 'Bank',
-	'label_surveillance_type_batiment' => 'Building',
-	'label_date' => 'Camera signaled on',
+	'label_titre' => 'Location',
+	'label_type' => 'Camera type',
+	'label_zone' => 'Zone',
+	'label_zoom' => 'Zoomable?',
 	'lien_ajouter_camera' => 'Add this camera',
 	'lien_retirer_camera' => 'Remove this camera',
 	'lien_retirer_cameras' => 'Remove all cameras',
-	'label_actualite_locale' => 'Local news',
-	'label_actualite_globale' => 'Global news',
-	
-	//M
+
+	// M
 	'modifier' => 'Edit',
 
-	//S
-	'slogan_site_cameras'=>'Collaborative mapping and resources against video surveillance',
-	
+	// S
+	'slogan_site_cameras' => 'Collaborative mapping and resources against video surveillance',
+
 	// T
-	'titre_formulaire_social'	=> 'Social data',
-	'titre_formulaire_technique' => 'Technical data',
-	'titre_formulaire_infos'  => 'Additional information',
-	'titre_formulaire_position' => 'Camera coordinates',
-	'titre_camera' => 'Camera',
-	'titre_cameras' => 'Cameras',
-	'titre_cameras_rubrique' => 'This section\'s cameras',
-	'titre_langue_camera' => 'Camera language',
-	'titre_logo_camera' => 'Camera logo',
-	'texte_creer_associer' => 'Create and link a camera',
 	'texte_ajouter_camera' => 'Add a camera',
 	'texte_changer_statut' => 'This camera is :',
-	'texte_explication_email_secu' => 'Your email will only be viewable by the website admin. In case of doubt, use a dedicated email adress.'
-	
-
+	'texte_creer_associer' => 'Create and link a camera',
+	'texte_explication_email_secu' => 'Your email will only be viewable by the website admin. In case of doubt, use a dedicated email adress.',
+	'titre_camera' => 'Camera',
+	'titre_cameras' => 'Cameras',
+	'titre_cameras_rubrique' => 'This section’s cameras',
+	'titre_formulaire_infos' => 'Additional information',
+	'titre_formulaire_position' => 'Camera coordinates',
+	'titre_formulaire_social' => 'Social data',
+	'titre_formulaire_technique' => 'Technical data',
+	'titre_langue_camera' => 'Camera language',
+	'titre_logo_camera' => 'Camera logo'
 );
 
 ?>
--- cameras/lang/camera_ru.php
+++ cameras/lang/camera_ru.php
@@ -1,97 +1,99 @@
 <?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/camera?lang_cible=ru
+// ** ne pas modifier le fichier **
+
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// B
-	'bouton_pointer_sur_carte' => 'Отметить на карте',
 	'bouton_deplacer_camera' => 'Перенести камеру',
+	'bouton_pointer_sur_carte' => 'Отметить на карте',
 	'bouton_saisie_manuelle' => 'Ввести местоположение вручную',
 
-	//C
-	'cfg_titre_menu_camera' => 'Камеры',
+	// C
+	'cameras_recents' => 'Последние добавленные камеры',
 	'cfg_titre_camera' => 'Конфигурация плагина камеры',
-	'cameras_recents'	=> 'Последние добавленные камеры',
+	'cfg_titre_menu_camera' => 'Камеры',
 
 	// I
 	'icone_creer_camera' => 'Добавить камеру',
 	'icone_modifier_camera' => 'Редактировать эту камеру',
-	'info_aucun_camera' => 'Нет камер',
 	'info_1_camera' => 'Одна камера',
-	'info_nb_cameras' => '@nb@ камер',
-	'info_cameras_valider' => 'Камеры, требующие подтверждения',
+	'info_aucun_camera' => 'Нет камер',
 	'info_cameras_auteur' => 'Камеры этого пользователя',
+	'info_cameras_valider' => 'Камеры, требующие подтверждения',
 	'info_inconnu' => 'неизвестный',
-	
+	'info_nb_cameras' => '@nb@ камер',
+
 	// L
+	'label_actualite_globale' => 'глобальные новости',
+	'label_actualite_locale' => 'местные новости',
+	'label_angle' => 'Угол к земле',
+	'label_apparence' => 'Внешний вид',
+	'label_apparence_boite' => 'Корпусная',
+	'label_apparence_dome' => 'Купольная',
+	'label_apparence_encastre' => 'Встроенная',
+	'label_apparence_nue' => 'Простейшая',
+	'label_apparence_radar' => 'Радар',
+	'label_aucune_description' => 'Нет описания',
+	'label_choisir_apparence' => 'Выбрать внешний вид камеры',
+	'label_config_auth' => 'Отключить редактирование камеры',
 	'label_config_rubrique' => 'Раздел по умолчанию',
 	'label_config_rubrique_auto' => 'Автоматический раздел',
 	'label_config_titre' => 'Автоматическое название',
-	'label_config_auth' => 'Отключить редактирование камеры',
-	'label_oui' => 'Да',
-	'label_non' => 'Нет',
-	'label_titre' => 'Местонахождение',
-	'label_type' => 'Тип камеры',
-	'label_apparence' => 'Внешний вид',
-	'label_choisir_apparence' => 'Выбрать внешний вид камеры',
-	'label_apparence_nue' => 'Простейшая',
-	'label_apparence_dome' => 'Купольная',
-	'label_apparence_encastre' => 'Встроенная',
-	'label_apparence_boite' => 'Корпусная',
-	'label_apparence_radar' => 'Радар',
-	'label_zone' => 'Зона',
+	'label_description' => 'Описание',
+	'label_direction' => 'Направление',
+	'label_height' => 'Высота',
 	'label_lat' => 'Широта',
 	'label_lon' => 'Долгота',
-	'label_angle' => 'Угол к земле',
-	'label_direction' => 'Направление',
-	'label_zoom'	=> 'Может приближать?',
-	'label_orientable'	=>	'Может менять положение?',
-	'label_description' => 'Описание',
+	'label_non' => 'Нет',
 	'label_non_renseigne' => 'Данные не введены',
 	'label_non_renseignee' => 'Данные не введены',
-	'label_aucune_description' => 'Нет описания',
-	'label_op_type' => 'Тип оператора',
 	'label_op_choisir_type' => 'Государственная или частная?',
-	'label_op_type_public' => 'Государственная',
-	'label_op_type_prive' => 'Частная',
 	'label_op_name' => 'Имя оператора',
-	'label_height' => 'Высота',
+	'label_op_type' => 'Тип оператора',
+	'label_op_type_prive' => 'Частная',
+	'label_op_type_public' => 'Государственная',
+	'label_orientable' => 'Может менять положение?',
+	'label_oui' => 'Да',
 	'label_surveillance' => 'Обзор',
 	'label_surveillance_choisir_type' => 'Какой тип наблюдения?',
+	'label_surveillance_type_banque' => 'Банк',
+	'label_surveillance_type_batiment' => 'Здание',
+	'label_surveillance_type_commerce' => 'Магазин',
 	'label_surveillance_type_municipale' => 'Город',
 	'label_surveillance_type_parking' => 'Парковка',
 	'label_surveillance_type_traffic' => 'Автотраффик',
-	'label_surveillance_type_commerce' => 'Магазин',
-	'label_surveillance_type_banque' => 'Банк',
-	'label_surveillance_type_batiment' => 'Здание',
+	'label_titre' => 'Местонахождение',
+	'label_type' => 'Тип камеры',
+	'label_zone' => 'Зона',
+	'label_zoom' => 'Может приближать?',
 	'lien_ajouter_camera' => 'Добавить камеру',
 	'lien_retirer_camera' => 'Удалить камеру',
 	'lien_retirer_cameras' => 'Удалить все камеры',
-	'label_actualite_locale' => 'местные новости',
-	'label_actualite_globale' => 'глобальные новости',
-	
-	//M
+
+	// M
 	'modifier' => 'Изменить',
 
-	//S
-	'slogan_site_cameras'=>'Коллективная картография и ресурсы против видеонаблюдения',
-	
+	// S
+	'slogan_site_cameras' => 'Коллективная картография и ресурсы против видеонаблюдения',
+
 	// T
-	'titre_formulaire_social'	=> 'Социальная информация',
-	'titre_formulaire_technique' => 'Техническая информация',
-	'titre_formulaire_infos'  => 'Дополнительная информация',
-	'titre_formulaire_position' => 'Координаты камеры',
+	'texte_ajouter_camera' => 'Добавить камеру',
+	'texte_changer_statut' => 'Эта камера :',
+	'texte_creer_associer' => 'Создать и прикрепить камеру',
+	'texte_explication_email_secu' => 'После регистрации ваш почтовый ящик будет виден только администраторам ресурса.',
 	'titre_camera' => 'Камера',
 	'titre_cameras' => 'Камеры',
 	'titre_cameras_rubrique' => 'Камеры этого раздела',
+	'titre_formulaire_infos' => 'Дополнительная информация',
+	'titre_formulaire_position' => 'Координаты камеры',
+	'titre_formulaire_social' => 'Социальная информация',
+	'titre_formulaire_technique' => 'Техническая информация',
 	'titre_langue_camera' => 'Язык камеры',
-	'titre_logo_camera' => 'Логотип камеры',
-	'texte_creer_associer' => 'Создать и прикрепить камеру',
-	'texte_ajouter_camera' => 'Добавить камеру',
-	'texte_changer_statut' => 'Эта камера :',
-	'texte_explication_email_secu' => 'После регистрации ваш почтовый ящик будет виден только администраторам ресурса.'
-	
-
+	'titre_logo_camera' => 'Логотип камеры'
 );
 
 ?>
