224d0fcb3753a2f57078ab98d732dd976bd3290d
cyril@cym.fr <cyril@cym.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-12-13 13:58:00 +0000

    Ajout des fichiers de "evaluations_concours";
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79317 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ evaluations_concours/trunk/evaluations_concours/lang/eval_concours_fr.php
@@ -0,0 +1,23 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+//
+///  Fichier produit par PlugOnet
+// Module: paquet-spipr_dist
+// Langue: fr
+// Date: 29-11-2012 16:35:29
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+    // C
+    'cfg_titre_parametrages' => "Paramétrages du plugin Concours Photo",
+
+    // E
+    'erreur_aucun_document'  => 'Aucune image dans la rubrique @id_rubrique@',
+
+);
+?>
+
---
+++ evaluations_concours/trunk/evaluations_concours/lang/paquet-eval_concours_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/compositions/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+    // R
+    'eval_concours_nom'         => "Critique d'images - Concours photos",
+    'eval_concours_slogan'      => "Permet d'évaluer des objets SPIP de type images (documents)",
+    'eval_concours_description' => "Ce plugin s'utilise avec le plugin Evaluations; il propose une composition de rubrique, listant tous les documents d'une branche.",
+
+);
+
+?>
