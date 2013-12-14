cc0164f15516111299fb17dc095719f99311e311
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-12-14 16:48:21 +0000

    Correction de la description
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79364 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- faq/trunk/lang/paquet-faq_fr.php
+++ faq/trunk/lang/paquet-faq_fr.php
@@ -7,8 +7,8 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// T
 	'faq_description' => 'Ajoute de nouveaux raccourcis typographiques permettant de décrire de manière simple des <abbr title="Foire Aux Questions">FAQ</abbr> dans un contenu SPIP.
 
-	Les raccourcis doivent être utilisés à l\'intérieur du sélecteur &#171;<code>faq</code>&#187;. Une configuration permet d\'ajouter des ancres, de rendre la FAQ dépliable, de charger des styles par défaut...
-	La structure HTML produite est, par défaut, basée sur une liste de définitions.',
+	Les raccourcis doivent être utilisés à l\'intérieur de la balise <code><faq></faq></code>. Une configuration permet d\'ajouter des ancres, de rendre la FAQ dépliable, de charger des styles par défaut...
+	La structure HTML produite est, par défaut, toujours basée sur une liste de définitions.',
 	'faq_nom' => 'FAQ',
 	'faq_slogan' => 'Créer simplement une FAQ'
 );
