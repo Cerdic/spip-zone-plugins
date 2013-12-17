924c24f27b5485cce108b67d45e9c5b2b777cb74
chankalan@free.fr <chankalan@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-12-17 19:38:01 +0000

    pas de pipeline pour quelques styles voyons...
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79420 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- basicone/basicone_pipelines.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function basicone_header_prive($flux){
-    $flux .= '<link rel="stylesheet" href="' . _DIR_PLUGIN_BASICONE  .'css/basicone.css" type="text/css" media="all" />';
-    return $flux;
-}
-
-?>
