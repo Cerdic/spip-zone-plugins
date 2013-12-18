e7e91b716978b31a8c081bc1b5773a76aaaed399
severo@rednegra.net <severo@rednegra.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-12-18 12:02:41 +0000

    Test de la connexion (@erational - emailtospip) + ajout des options de connexion.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79431 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- imap/lang/imap_fr.php
+++ imap/lang/imap_fr.php
@@ -11,6 +11,8 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'cfg_email_pwd' => 'Mot de passe',
 	'cfg_hote_imap' => 'Adresse du serveur IMAP',
 	'cfg_hote_imap_explication' => 'ex. imap.gmail.com',   
+	'cfg_hote_options' => 'Options de connexion',
+	'cfg_hote_options_explication' => 'Voir <a href="http://php.net/manual/fr/function.imap-open.php">les options possibles</a>',
 	'cfg_hote_port' => 'Port',
 	'cfg_hote_port_explication' => '143, 993 (SSL) ou 993/imap/ssl (gmail) - <a href="http://php.net/manual/fr/function.imap-open.php">Infos</a>',
 	'cfg_inbox' => 'Dossier distant',
@@ -20,6 +22,10 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'imap_titre' => 'IMAP',
 
 	// T
+	'test_connexion' => 'Test de connexion au serveur IMAP',
+	'test_connexion_ok' => 'Authentification réussie !',
+	'test_connexion_notok' => 'Erreur : Impossible de se connecter à<br /><i>@connexion@</i>',
+	'test_librairie_installee_notok' => 'Erreur : la librairie PHP "imap" n\'est pas installée - Voir <a href="http://www.php.net/manual/en/imap.setup.php">la documentation</a>.',
 	'titre_page_configurer_imap' => 'Connexion IMAP',
 
 );
---
+++ imap/prive/squelettes/contenu/configurer_imap_fonctions.php
@@ -0,0 +1,32 @@
+<?php
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+//
+// tester la connection imap
+//
+function imap_test_connexion() {
+	include_spip('inc/config');
+
+	$email = lire_config('imap/email');
+	$email_pwd = lire_config('imap/email_pwd');
+	$hote_imap = lire_config('imap/hote_imap');
+	$hote_port = lire_config('imap/hote_port');
+	$hote_options = lire_config('imap/hote_options');
+	$hote_inbox = lire_config('imap/inbox'); 
+
+	if (!function_exists("imap_open")) {
+		return _T('imap:test_librairie_installee_notok');
+	} else if ($hote_imap!="") {
+		// test connexion
+		$connexion = '{'.$hote_imap.':'.$hote_port.$hote_options.'}'.$hote_inbox;
+		$mbox = @imap_open($connexion, $email, $email_pwd);
+
+		if (FALSE === $mbox) {
+			return _T('imap:test_connexion_notok',array('connexion'=>$connexion));
+		} else {
+			return _T('imap:test_connexion_ok');
+		}
+	}
+
+	return;
+}
