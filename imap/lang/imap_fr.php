d7827b81b04e26a988f73e57a8d202cb0a9237a7
severo@rednegra.net <severo@rednegra.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-12-18 11:16:28 +0000

    Formulaire de configuration - tout copié de emailtospip (@erational).
    Voir http://zone.spip.org/trac/spip-zone/browser/_plugins_/emailtospip
    
    
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79429 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- imap/lang/imap_fr.php
+++ imap/lang/imap_fr.php
@@ -5,8 +5,22 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
+	// C
+	'cfg_email' => 'Email', 
+	'cfg_email_explication' => 'Email ou identifiant de la boite email',   
+	'cfg_email_pwd' => 'Mot de passe',
+	'cfg_hote_imap' => 'Adresse du serveur IMAP',
+	'cfg_hote_imap_explication' => 'ex. imap.gmail.com',   
+	'cfg_hote_port' => 'Port',
+	'cfg_hote_port_explication' => '143, 993 (SSL) ou 993/imap/ssl (gmail) - <a href="http://php.net/manual/fr/function.imap-open.php">Infos</a>',
+	'cfg_inbox' => 'Dossier distant',
+	'cfg_titre_parametrages' => 'Paramètres de connexion',
+
 	// I
 	'imap_titre' => 'IMAP',
+
+	// T
+	'titre_page_configurer_imap' => 'Connexion IMAP',
 
 );
 
