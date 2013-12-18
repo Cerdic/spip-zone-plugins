8cf796dd110b379a5b0325a7394e15ab22092293
severo@rednegra.net <severo@rednegra.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-12-18 10:34:52 +0000

    Nouveau plugin pour gérer la connexion IMAP
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79426 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ imap/lang/paquet-imap_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'imap_description' => 'Configuration et connexion à un serveur de mail IMAP',
+	'imap_nom' => 'IMAP',
+	'imap_slogan' => 'Connexion à un serveur IMAP',
+);
+
+?>
