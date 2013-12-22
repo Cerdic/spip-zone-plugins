c448a32500a431d7e27012cc8a6999e0551b16fc
bystrano@gmx.ch <bystrano@gmx.ch@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-12-22 12:20:03 +0000

    commit initial - généré par la Fabrique
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79481 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ saisie_liste/trunk/lang/paquet-saisie_liste_objets_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// S
+	'saisie_liste_objets_description' => '',
+	'saisie_liste_objets_nom' => 'Saisie liste d\'objets',
+	'saisie_liste_objets_slogan' => 'Une saisie pour des listes ordonnées d\'objets.',
+);
+
+?>
---
+++ saisie_liste/trunk/lang/saisie_liste_objets_fr.php
@@ -0,0 +1,12 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// S
+	'saisie_liste_objets_titre' => 'Saisie liste d\'objets',
+);
+
+?>
