f886c698082f9013a4bcc73f2fc386fbf2543a8c
bystrano@gmx.ch <bystrano@gmx.ch@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-12-22 12:20:38 +0000

    charge les valeurs postées dans le form de test ajax
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79516 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- saisie_liste/trunk/formulaires/tester_saisie_liste_objets_ajax_yaml.php
+++ saisie_liste/trunk/formulaires/tester_saisie_liste_objets_ajax_yaml.php
@@ -1,5 +1,12 @@
 <?php
 
+function formulaires_tester_saisie_liste_objets_ajax_yaml_charger_dist () {
+
+  return array(
+               'liste_1' => _request('liste_1'),
+         );
+}
+
 function formulaires_tester_saisie_liste_objets_ajax_yaml_verifier_dist () {
 
   if (liste_objets_verifier('liste_1')) return;
@@ -10,5 +17,5 @@ function formulaires_tester_saisie_liste_objets_ajax_yaml_traiter_dist () {
 
   if (liste_objets_traiter('liste_1')) return;
 
-  var_dump(_request('liste_1'));
+  /* var_dump(_request('liste_1')); */
 }
