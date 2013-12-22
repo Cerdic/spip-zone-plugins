811217aac7f5c8997e99999089b4e7a00bbb3a2b
bystrano@gmx.ch <bystrano@gmx.ch@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-12-22 12:20:24 +0000

    sort le code de la closure, c'est pas nécessaire.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79501 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- saisie_liste/trunk/js/jquery.saisie_liste_objets.js
+++ saisie_liste/trunk/js/jquery.saisie_liste_objets.js
@@ -1,63 +1,59 @@
-(function () {
+$.fn.saisieListeObjets = function( options ) {
 
-    $.fn.saisieListeObjets = function( options ) {
+    this.nom = options.nom;
 
-        this.nom = options.nom;
+    var self = this,
+        defaut_sortable = {
+            // valeurs par défaut pour sortable
+            containement: 'parent',
+            cursor: 'move',
+            placeholder: 'ui-state-highlight'
+        };
 
-        var self = this,
-            defaut_sortable = {
-                // valeurs par défaut pour sortable
-                containement: 'parent',
-                cursor: 'move',
-                placeholder: 'ui-state-highlight'
-            };
+    options = $.extend(true,
+                       {
+                           sortable: {
+                               update: function () { return; }
+                           }
+                       },
+                       options
+                      );
 
-        options = $.extend(true,
-                           {
-                               sortable: {
-                                   update: function () { return; }
-                               }
-                           },
-                           options
-                          );
+    options.sortable.update = calculerFonctionUpdate(options.sortable.update);
+    options.sortable = $.extend(defaut_sortable, options.sortable);
 
-        options.sortable.update = calculerFonctionUpdate(options.sortable.update);
-        options.sortable = $.extend(defaut_sortable, options.sortable);
+    // numéroter les li's
+    this.find('> li').each(function (index, li) {
+        $(li).data('index_objet', index);
+    });
 
-        // numéroter les li's
-        this.find('> li').each(function (index, li) {
-            $(li).data('index_objet', index);
+    this.sortable(options.sortable);
+
+    return this;
+
+    // retourne une fonction qui commence par mettre à jour la valeur
+    // du champs hidden 'nom-liste[permutations]' puis execute le
+    // callback donné.
+    function calculerFonctionUpdate (callback) {
+
+        return function (event, ui) {
+            self.parent().find('input[name*="permutations"]')
+                .attr('value', calculerPermutation());
+            if (typeof(callback === 'function')) {
+                callback.call(self, event, ui);
+            }
+        };
+    }
+
+    // parcours les objets dans l'ordre affiché et se sert de la
+    // numérotation de départ pour trouver la permutation qui a été
+    // faite.
+    function calculerPermutation () {
+        var permutations = [];
+
+        self.find('> li').each(function (index, li) {
+            permutations.push($(li).data('index_objet'));
         });
-
-        this.sortable(options.sortable);
-
-        return this;
-
-        // retourne une fonction qui commence par mettre à jour la valeur
-        // du champs hidden 'nom-liste[permutations]' puis execute le
-        // callback donné.
-        function calculerFonctionUpdate (callback) {
-
-            return function (event, ui) {
-                self.parent().find('input[name*="permutations"]')
-                    .attr('value', calculerPermutation());
-                if (typeof(callback === 'function')) {
-                    callback.call(self, event, ui);
-                }
-            };
-        }
-
-        // parcours les objets dans l'ordre affiché et se sert de la
-        // numérotation de départ pour trouver la permutation qui a été
-        // faite.
-        function calculerPermutation () {
-            var permutations = [];
-
-            self.find('> li').each(function (index, li) {
-                permutations.push($(li).data('index_objet'));
-            });
-            return permutations.join(',');
-        }
-    };
-
-})();
+        return permutations.join(',');
+    }
+};
