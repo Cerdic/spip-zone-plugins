9548d71e2f566638d888bff3c6e81a9f1ec39c96
bystrano@gmx.ch <bystrano@gmx.ch@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-12-22 12:21:20 +0000

    passe le plugin en "test"
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79561 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- saisie_liste/trunk/lang/paquet-saisie_liste_fr.php
+++ saisie_liste/trunk/lang/paquet-saisie_liste_fr.php
@@ -6,7 +6,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// S
-	'saisie_liste_description' => ' <a href="?exec=exemples_saisie_liste">Quelques exemples</a>',
+	'saisie_liste_description' => "<a href='?exec=exemples_saisie_liste'>Quelques exemples d'utilisation</a>",
 	'saisie_liste_nom' => 'Saisie liste',
 	'saisie_liste_slogan' => 'Une saisie pour toutes sortes de listes.',
 );
