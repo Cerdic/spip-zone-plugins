d98ad2716e98720428ebb3e61e8f058066a7945d
bystrano@gmx.ch <bystrano@gmx.ch@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-12-22 12:20:31 +0000

    ajoute un test avec inclure et defaut
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79510 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- saisie_liste/trunk/formulaires/tester_saisie_liste_objets.php
+++ saisie_liste/trunk/formulaires/tester_saisie_liste_objets.php
@@ -15,11 +15,17 @@ function formulaires_tester_saisie_liste_objets_charger_dist () {
 function formulaires_tester_saisie_liste_objets_verifier_dist () {
 
   if (liste_objets_verifier('liste_1')) return;
+  if (liste_objets_verifier('liste_2')) return;
 
 }
 
 function formulaires_tester_saisie_liste_objets_traiter_dist () {
 
   if (liste_objets_traiter('liste_1')) return;
+  if (liste_objets_traiter('liste_2')) return;
 
+  return array(
+               'message_ok' => var_export(_request('liste_1'), TRUE) . '<br>' .
+                               var_export(_request('liste_2'), TRUE),
+               );
 }
