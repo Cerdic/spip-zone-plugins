88c60d021e41dab2bf3f006964b12a1bcbe58916
bystrano@gmx.ch <bystrano@gmx.ch@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-12-22 12:20:17 +0000

    cache le premier bouton "monter" et le dernier"descendre", pas pertinents
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79495 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- saisie_liste/trunk/saisie_liste_objets_pipelines.php
+++ saisie_liste/trunk/saisie_liste_objets_pipelines.php
@@ -10,7 +10,10 @@ function saisie_liste_objets_jqueryui_plugins ($scripts) {
 
 function saisie_liste_objets_insert_head ($flux) {
 
-  return $flux . '<script src="' . find_in_path('js/jquery.saisie_liste_objets.js') . '" type="text/javascript"></script>';
+  $flux .= '<link rel="stylesheet" href="' . find_in_path('saisie_liste_objets.css') . '" />';
+  $flux .= '<script src="' . find_in_path('js/jquery.saisie_liste_objets.js') . '" type="text/javascript"></script>';  
+
+  return $flux;
 }
 
 function saisie_liste_objets_header_prive ($flux) {
