555c80064c1489a7ee6c7d829f0c868018e789f0
bystrano@gmx.ch <bystrano@gmx.ch@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-12-22 12:20:35 +0000

    ajoute une valeur par défaut au test ajax
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79514 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- saisie_liste/trunk/formulaires/tester_saisie_liste_objets_ajax_yaml.php
+++ saisie_liste/trunk/formulaires/tester_saisie_liste_objets_ajax_yaml.php
@@ -1,17 +1,5 @@
 <?php
 
-function formulaires_tester_saisie_liste_objets_ajax_yaml_charger_dist () {
-
-  return array(
-         'liste_1' => array(
-                    0 => array(
-                           'nom'         => 'Un bel objet',
-                           'description' => 'bla bla bla bla',
-                         ),
-                  ),
-         );
-}
-
 function formulaires_tester_saisie_liste_objets_ajax_yaml_verifier_dist () {
 
   if (liste_objets_verifier('liste_1')) return;
