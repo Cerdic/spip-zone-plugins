e60d1d5dd3e0b801950ab32971f217872dfb6793
bystrano@gmx.ch <bystrano@gmx.ch@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-12-22 12:20:06 +0000

    charge les valeurs des saisies de type input correctement
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79484 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ saisie_liste/trunk/formulaires/tester_saisie_liste_objets.php
@@ -0,0 +1,13 @@
+<?php
+
+function formulaires_tester_saisie_liste_objets_charger_dist () {
+
+    return array(
+                 'liste_1' => array(
+                                    0 => array(
+                                               'titre_objet' => 'Un bel objet',
+                                               'description_objet' => 'bla bla bla bla',
+                                               ),
+                                    ),
+                 );
+}
--- saisie_liste/trunk/saisie_liste_objets_fonctions.php
+++ saisie_liste/trunk/saisie_liste_objets_fonctions.php
@@ -12,3 +12,24 @@ function liste_objets_numeroter_saisie ($saisie, $nom, $no) {
 
   return $saisie;
 }
+
+function extraire_nom_saisie ($saisie) {
+
+  preg_match('/name="([^"]+)"/xi', $saisie, $matches);
+  return $matches[1];
+}
+
+function liste_objets_charger_valeur_saisie ($saisie, $valeur) {
+
+  if (preg_match('/value="([^"]+)"/i', $saisie, $matches) === 1) {
+    $saisie = preg_replace('/value="([^"]+)"/i',
+                           'value="' . $valeur . '"',
+                           $saisie);
+  } else {
+    $saisie = preg_replace('/<input/i',
+                           '<input value="' . $valeur . '"',
+                           $saisie);
+  }
+
+  return $saisie;
+}
