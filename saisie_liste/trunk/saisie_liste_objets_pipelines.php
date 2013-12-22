315e5f596e1079617b9eb1897362eccdf736f247
bystrano@gmx.ch <bystrano@gmx.ch@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-12-22 12:20:15 +0000

    insertion du futur js…
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79493 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- saisie_liste/trunk/saisie_liste_objets_pipelines.php
+++ saisie_liste/trunk/saisie_liste_objets_pipelines.php
@@ -7,3 +7,13 @@ function saisie_liste_objets_jqueryui_plugins ($scripts) {
   }
   return $scripts;
 }
+
+function saisie_liste_objets_insert_head ($flux) {
+
+  return $flux . '<script src="' . find_in_path('js/jquery.saisie_liste_objets.js') . '" type="text/javascript"></script>';
+}
+
+function saisie_liste_objets_header_prive ($flux) {
+
+  return saisie_liste_objets_insert_head($flux);
+}
