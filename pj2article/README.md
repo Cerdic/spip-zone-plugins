e78dd1459a4387c27f10f67be18f699c0cd014a9
severo@rednegra.net <severo@rednegra.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-12-24 14:56:34 +0000

    Nouveau plugin pj2article pour importer des pieces jointes.
    Voir le README pour les explications.
    
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79609 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ pj2article/README.md
@@ -0,0 +1,23 @@
+# Plugin PJ2ARTICLE
+
+## Objectif
+
+Ce plugin sert à créer des articles à partir d'emails. Il récupère les
+pièces jointes dans une boite mail, via IMAP, et crée un article pour
+chaque pièce jointe, avec le plugin doc2article.
+
+Les mails sont pris depuis une boîte aux lettres (INBOX par défaut),
+traités, puis déplacés dans une autre boîte aux lettres (Trash par défaut).
+
+Si un mail ne contient pas de pièce jointe, pas de traitement. Si un mail contient une pièce jointe, elle est importée comme article. Si un mail contient plusieurs pièces jointes, elles sont zippées, puis le zip est importé comme article.
+
+## Fonctionnalités
+
+Ce plugin fournit :
+
+* la configuration des boites d'entrée et de sortie des mails
+
+## TODO :
+
+* logo
+* fonction d'import des fichiers sous forme d'articles
---
+++ pj2article/lang/pj2article_fr.php
@@ -0,0 +1,25 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cfg_errorbox' => 'Boîte d\'erreur',
+	'cfg_errorbox_explication' => 'Choisir dans quelle boîte aux lettres déplacer les emails après une erreur de traitement.',
+	'cfg_inbox' => 'Boîte d\'entrée',
+	'cfg_inbox_explication' => 'Choisir depuis quelle boîte aux lettres les pièces jointes seront importées.',
+	'cfg_outbox' => 'Boîte de sortie',
+	'cfg_outbox_explication' => 'Choisir dans quelle boîte aux lettres déplacer les emails uen fois traités.',
+	'cfg_titre_parametrages' => 'Paramètres d\'importation des pièces jointes',
+
+	// P
+	'pj2article_titre' => 'pj2article',
+
+	// T
+	'titre_page_configurer_pj2article' => 'Importation des pièces jointes',
+
+);
+
+?>
