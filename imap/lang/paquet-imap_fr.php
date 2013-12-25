0cfcd52ad4d098d6f23f97462b2587d53d4c8738
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-12-25 04:38:45 +0000

    [Salvatore] [source:_plugins_/imap/lang/ paquet-imap] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79614 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- imap/lang/paquet-imap_fr.php
+++ imap/lang/paquet-imap_fr.php
@@ -1,14 +1,14 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/imap/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-	// A
+	// I
 	'imap_description' => 'Configuration et connexion à un serveur de mail IMAP',
 	'imap_nom' => 'IMAP',
-	'imap_slogan' => 'Connexion à un serveur IMAP',
+	'imap_slogan' => 'Connexion à un serveur IMAP'
 );
 
 ?>
