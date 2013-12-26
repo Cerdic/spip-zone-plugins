a2defdf3f6dee450b46cb00fbf7c97fc5363cc76
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-12-26 04:39:40 +0000

    [Salvatore] [source:_plugins_/opensearch/lang/ paquet-opensearch] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79624 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ opensearch/lang/paquet-opensearch_nl.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-opensearch?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// O
+	'opensearch_description' => 'Plugin die bezoekers kan voorstellen om jouw site als aangepaste zoekmachine aan hun browser toe te voegen.',
+	'opensearch_slogan' => 'Jouw site als zoekmachine'
+);
+
+?>
