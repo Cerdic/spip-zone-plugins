238bc331063c74685a610e8698538704e08668d2
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-12-26 04:39:43 +0000

    [Salvatore] [source:_plugins_/polyhierarchie/trunk/lang/ polyhier] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79625 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ polyhierarchie/trunk/lang/polyhier_nl.php
@@ -0,0 +1,28 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/polyhier?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'articles_associes' => 'Gekoppelde artikelen',
+
+	// L
+	'label_autre_parent' => 'Ook in de rubriek',
+	'label_autres_parents' => 'Ook in de rubrieken',
+	'label_parents' => 'In de rubrieken',
+
+	// P
+	'parent_obligatoire' => 'Je moet ten minste één rubriek aangeven',
+
+	// R
+	'rubriques_associees' => 'Gekoppelde rubrieken',
+
+	// T
+	'titre_polyhierarchie' => 'Polyhierarchie'
+);
+
+?>
