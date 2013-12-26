7497022db7509990691855beb548b0aeb1475dc5
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-12-26 04:39:46 +0000

    [Salvatore] [source:_plugins_/polyhierarchie/trunk/lang/ paquet-polyhier] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79626 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ polyhierarchie/trunk/lang/paquet-polyhier_nl.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-polyhier?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// P
+	'polyhier_description' => 'Met deze plugin kunnen rubrieken en artikelen transversaal worden gekoppeld met andere rubrieken dan hun directe rubriek.',
+	'polyhier_slogan' => 'Koppel een artikel of een subrubriek aan meerdere rubrieken'
+);
+
+?>
