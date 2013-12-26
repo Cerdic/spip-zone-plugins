4e5c5fba93c2eacc1855ae6caae36b709de3c79a
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-12-26 04:39:21 +0000

    [Salvatore] [source:_plugins_/embed_code/trunk/lang/ embed_code] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79621 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ embed_code/trunk/lang/embed_code_nl.php
@@ -0,0 +1,28 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/embed_code?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cfg_label_hauteur' => 'Standaard hoogte',
+	'cfg_label_largeur' => 'Standaard breedte',
+	'cfg_titre' => 'Configuratie van plugin Embed code',
+
+	// I
+	'info_embed_code' => 'Deze media integreren',
+
+	// L
+	'label_code_embed' => 'Te kopiëren code',
+	'label_hauteur' => 'Hoogte',
+	'label_largeur' => 'Breedte',
+	'lien_preview_embed' => 'Bekijk het resultaat vooraf',
+
+	// M
+	'message_document_inexistant' => 'Dit document is niet beschikbaar op site <a href="@url_site@">@nom_site@</a>.'
+);
+
+?>
