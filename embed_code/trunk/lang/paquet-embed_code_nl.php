bcbc426ff76ef7be20bb668cab17a98bd44eab80
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-12-26 04:39:28 +0000

    [Salvatore] [source:_plugins_/embed_code/trunk/lang/ paquet-embed_code] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79622 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ embed_code/trunk/lang/paquet-embed_code_nl.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-embed_code?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// E
+	'embed_code_description' => 'Maak code aan voor het opnemen van documenten in andere sites.',
+	'embed_code_nom' => 'Embed code',
+	'embed_code_slogan' => 'Aanmaken van ingebedde code voor documenten'
+);
+
+?>
