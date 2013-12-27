aa5bd9af4ac7a6b34bf4ec5a884bf93b7d6cfb2d
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-12-27 04:40:28 +0000

    [Salvatore] [source:_plugins_/multilang/trunk/lang/ paquet-multilang] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79637 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ multilang/trunk/lang/paquet-multilang_nl.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-multilang?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// M
+	'multilang_description' => 'Deze plugin voegt een taalkeuzemenu toe van het type <code>[fr] [en] [nl]</code> bovenaan ieder formulier en gebaseerd op de taalinstellingen van de site.',
+	'multilang_slogan' => 'Regel de titels van de labels aan de hand van een menu'
+);
+
+?>
