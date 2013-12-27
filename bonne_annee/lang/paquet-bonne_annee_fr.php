062b01b4353b19d3d705e15931405971865d6b4d
pierrekuhn82@gmail.com <pierrekuhn82@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-12-27 09:32:41 +0000

    Oubli du fichier de langue
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79648 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ bonne_annee/lang/paquet-bonne_annee_fr.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/bonne_annee/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'bonne_annee_description' => 'Affiche tous les ans, durant le mois de janvier, un message souhaitant une bonne ann&#233;e.
+Ajouter <code>#INCLURE{fond=bonne_annee}</code> dans vos pages.',
+	'bonne_annee_nom' => 'Bonne année',
+	'bonne_annee_slogan' => 'Souhaiter la bonne ann&#233;e sur votre site'
+);
+
+?>
