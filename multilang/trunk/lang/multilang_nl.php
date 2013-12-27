e4a855a3da2e1e22315cbc357d0b0ae79cfd8e0f
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-12-27 04:40:24 +0000

    [Salvatore] [source:_plugins_/multilang/trunk/lang/ multilang] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79636 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ multilang/trunk/lang/multilang_nl.php
@@ -0,0 +1,38 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/multilang?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cfg_activer' => 'Multilang activeren op de redactiepagina:',
+	'cfg_boite_multilang' => 'Configuratie van plugin Multilang',
+	'cfg_descr_multilang' => 'Multilang: Plugin voor het beheer van meertalige tags',
+	'cfg_effacer' => 'Herinitialiseren',
+	'cfg_explication_crayons' => 'Multilang moet in de publieke site zijn geäctiveerd.',
+	'cfg_langues' => 'Gebruikte talen',
+	'cfg_lbl_crayons' => 'Gebruik Multilang in Crayons',
+	'cfg_lbl_espace_public' => 'Gebruik Multilang in de publieke site',
+	'cfg_lbl_formstables' => 'van forms&tables',
+	'cfg_lbl_siteconfig' => 'van de configuratie van de site',
+	'cfg_titre_multilang' => 'Multilang',
+	'champ_numero' => 'Nummer',
+	'configuration_multilang' => 'Multilang',
+
+	// E
+	'explication_langues_utilisees' => 'Kies de voor het menu van Multilang te gebruiken talen. Maak geen keuze wanneer je alle talen wilt gebruiken.',
+
+	// L
+	'label_langues_utilisees' => 'Door Multilang gebruikte talen',
+	'lien_desactiver' => 'Alles',
+	'lien_multi_title' => 'Pas de meertalige velden aan in het « @lang@ »',
+	'lien_multi_title_sans' => 'De versie « @lang@ » is onvolledig. Klik om hem aan te passen.',
+
+	// M
+	'message_champs_readonly' => 'De meertalige velden kunnen alleen worden gelezen, kies een taal om ze aan te passen'
+);
+
+?>
