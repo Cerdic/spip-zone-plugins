fe628010a0d17a2dcfc0c521c1bd98b63d1856ba
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-12-27 04:40:40 +0000

    [Salvatore] [source:_plugins_/pj2article/lang/ pj2article] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79641 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- pj2article/lang/pj2article_fr.php
+++ pj2article/lang/pj2article_fr.php
@@ -1,25 +1,24 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/pj2article/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// C
-	'cfg_errorbox' => 'Boîte d\'erreur',
+	'cfg_errorbox' => 'Boîte d’erreur',
 	'cfg_errorbox_explication' => 'Choisir dans quelle boîte aux lettres déplacer les emails après une erreur de traitement.',
-	'cfg_inbox' => 'Boîte d\'entrée',
+	'cfg_inbox' => 'Boîte d’entrée',
 	'cfg_inbox_explication' => 'Choisir depuis quelle boîte aux lettres les pièces jointes seront importées.',
 	'cfg_outbox' => 'Boîte de sortie',
 	'cfg_outbox_explication' => 'Choisir dans quelle boîte aux lettres déplacer les emails uen fois traités.',
-	'cfg_titre_parametrages' => 'Paramètres d\'importation des pièces jointes',
+	'cfg_titre_parametrages' => 'Paramètres d’importation des pièces jointes',
 
 	// P
 	'pj2article_titre' => 'pj2article',
 
 	// T
-	'titre_page_configurer_pj2article' => 'Importation des pièces jointes',
-
+	'titre_page_configurer_pj2article' => 'Importation des pièces jointes'
 );
 
 ?>
