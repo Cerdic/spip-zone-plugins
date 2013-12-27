0b007b8b193484bc4ba60143d799bc966106d900
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-12-27 04:40:31 +0000

    [Salvatore] [source:_plugins_/noizetier/trunk/lang/ noizetier] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79638 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ noizetier/trunk/lang/noizetier_nl.php
@@ -0,0 +1,153 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/noizetier?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'apercu' => 'Overzicht',
+
+	// B
+	'bloc_sans_noisette' => 'Dit blok bevat geen nootjes.',
+
+	// C
+	'choisir_noisette' => 'Kies het nootje dat je wilt toevoegen:',
+	'compositions_non_installe' => '<b>Plugin Composities:</b> deze plugin is niet op de site geïnstalleerd. Dat is niet vereist, maar wanneer hij is geactiveerd, kun je direct composities aanwijzen.',
+
+	// D
+	'description_bloc_contenu' => 'Voornaamste inhoud van iedere bladzijde.',
+	'description_bloc_extra' => 'Extra contextuele informatie voor iedere bladzijde.',
+	'description_bloc_navigation' => 'Informatie ten behoeve van de navigatie op iedere bladzijde.',
+	'description_bloctexte' => 'De titel is optioneel. In de tekst kun je de typografische afkortingen van SPIP gebruiken.',
+
+	// E
+	'editer_composition' => 'Deze compositie aanpassen',
+	'editer_composition_heritages' => 'Het erfgoed bepalen',
+	'editer_configurer_page' => 'De nootjes van deze bladzijde configureren',
+	'editer_exporter_configuration' => 'De configuratie exporteren',
+	'editer_importer_configuration' => 'Een configuratie importeren.',
+	'editer_noizetier_explication' => 'Kies de bladzijde waarop je de nootjes wilt configureren.',
+	'editer_noizetier_titre' => 'Nootjes beheren',
+	'editer_nouvelle_page' => 'Een nieuwe bladzijde / compositie maken',
+	'erreur_aucune_noisette_selectionnee' => 'Je moet een nootje kiezen!',
+	'erreur_doit_choisir_noisette' => 'Je moet een nootje kiezen.',
+	'erreur_mise_a_jour' => 'Er is een fout opgetreden bij het aanpassen van de database.',
+	'explication_code' => 'LET OP: voor gevorderde gebruikers. Je kunt SPIP code (lussen en bakens) gebruiken die als in een skelet worden geïnterpreteerd. Het nootje zal zo teogang hebben tot alle omgevingsvariabelen van de bladzijde.',
+	'explication_description_code' => 'Voor intern gebruik. Wordt niet op de publieke site weergegeven.',
+	'explication_glisser_deposer' => 'Je kunt een nootje toevoegen of met schuiven en plakken herschikken.',
+	'explication_heritages_composition' => 'Je kunt hier de composities vastleggen die door onderliggende  objecten zullen worden geërfd.',
+	'explication_noizetier_css' => 'Je kunt eventueel extra CSS classes aan het nootje toevoegen.',
+	'explication_raccourcis_typo' => 'Je kunt de typografische snelkoppelingen van SPIP gebruiken.',
+
+	// F
+	'formulaire_ajouter_noisette' => 'Een nootje toevoegen',
+	'formulaire_composition' => 'Identificatie van de compositie',
+	'formulaire_composition_explication' => 'Geef een uniek trefwoord (kleine letters, zonder spatie, haakje of accent) om deze compositie te kenmerken.<br />Bijvoorbeeld: <i>mijncompo</i>.',
+	'formulaire_composition_mise_a_jour' => 'Compositie aangepast',
+	'formulaire_configurer_bloc' => 'Configureer het blok:',
+	'formulaire_configurer_page' => 'Configureer de bladzijde:',
+	'formulaire_deplacer_bas' => 'Omlaag verplaatsen',
+	'formulaire_deplacer_haut' => 'Omhoog verplaatsen',
+	'formulaire_description' => 'Omschrijving',
+	'formulaire_description_explication' => 'Je kunt de SPIP snelkoppelingen gebruiken en in het bijzonder &lt;multi&gt;.',
+	'formulaire_erreur_format_identifiant' => 'De identificatie mag alleen uit kleine letters zonder accent, cijfers of een _ (underscore) bestaan.',
+	'formulaire_icon' => 'Ikoon',
+	'formulaire_icon_explication' => 'Je mag het relatieve pad naar het ikoon gebruiken (bijvoorbeeld: <i>images/object-lijst-inhoud.png</i>).',
+	'formulaire_identifiant_deja_pris' => 'Deze identificatie is al in gebruik!',
+	'formulaire_import_compos' => 'De composities van de notenboom iimporteren',
+	'formulaire_import_fusion' => 'Met de huidige configuratie samenvoegen',
+	'formulaire_import_remplacer' => 'De huidige configuratie vervangen',
+	'formulaire_liste_compos_config' => 'Dit configuratiebestand bepaalt de volgende composities:',
+	'formulaire_liste_pages_config' => 'Dit configuratiebestand bepaalt de nootjes op de volgende bladzijden:',
+	'formulaire_modifier_composition' => 'Deze compositie aanpassen:',
+	'formulaire_modifier_composition_heritages' => 'Pas het erven aan',
+	'formulaire_modifier_noisette' => 'Dit nootje aanpassen',
+	'formulaire_modifier_page' => 'Deze bladzijde aanpassen',
+	'formulaire_noisette_sans_parametre' => 'Dit nootje heeft geen parameters.',
+	'formulaire_nom' => 'Titel',
+	'formulaire_nom_explication' => 'Je kunt het baken &lt;multi&gt; gebruiken.',
+	'formulaire_nouvelle_composition' => 'Nieuwe compositie',
+	'formulaire_obligatoire' => 'Verplicht veld',
+	'formulaire_supprimer_noisette' => 'Verwijder dit nootje',
+	'formulaire_supprimer_noisettes_page' => 'Verwijder de nootjes van deze bladzijde',
+	'formulaire_supprimer_page' => 'Verwijder deze bladzijde',
+	'formulaire_type' => 'Type bladzijde',
+	'formulaire_type_explication' => 'Geef aan op welk object deze comositie is gebaseerd of dat je een onafhankelijke bladzijde wilt maken.',
+	'formulaire_type_import' => 'Type import',
+	'formulaire_type_import_explication' => 'Je kunt het configuratiebestand samenvoegen met de huidige configuratie (de nootjes van iedere bladzijde worden aan de al gedefinieerde nootjes toegevoegd) of de huidige configuratie vervangen.',
+
+	// I
+	'icone_introuvable' => 'Ikoon niet te vinden!',
+	'ieconfig_ne_pas_importer' => 'Niet importeren',
+	'ieconfig_noizetier_export_explication' => 'Exporteert de configuratie van de nootjes en de composities.',
+	'ieconfig_noizetier_export_option' => 'In deze export opnemen?',
+	'ieconfig_non_installe' => '<b>Plugin Import/Export van configuraties:</b> deze plugin is niet op de site geïnstalleerd. Dat is niet noodzakelijk, maar wanneer deze is geäctiveerd, kun je eenvoudig nootjes in- en uitvoeren.',
+	'ieconfig_probleme_import_config' => 'Er is een probleem opgetreden bij het importeren van de configuratie.',
+	'info_composition' => 'COMPOSITIE:',
+	'info_page' => 'BLADZIJDE:',
+	'installation_tables' => 'Tabellen van plugin Notenboom geïnstalleerd.<br />',
+	'item_titre_perso' => 'aangepaste titel',
+
+	// L
+	'label_afficher_titre_noisette' => 'Toon een titel voor de nootjes?',
+	'label_code' => 'SPIP code:',
+	'label_description_code' => 'Omschrijving:',
+	'label_niveau_titre' => 'Niveau van de titel:',
+	'label_noizetier_css' => 'CSS classes:',
+	'label_texte' => 'Tekst:',
+	'label_texte_introductif' => 'Introductietekst (optioneel):',
+	'label_titre' => 'Titel:',
+	'label_titre_noisette' => 'Titel van het nootje:',
+	'label_titre_noisette_perso' => 'Aangepaste titel:',
+	'liste_icones' => 'Lijst van ikonen',
+	'liste_pages' => 'Lijst van bladzijdes',
+
+	// M
+	'masquer' => 'Verbergen',
+	'mode_noisettes' => 'Nootjes aanpassen',
+	'modif_en_cours' => 'Bezig met aanpassen',
+	'modifier_dans_prive' => 'Aanpassen in de privé-ruimte',
+
+	// N
+	'ne_pas_definir_d_heritage' => 'Erven niet bepalen',
+	'noisette_numero' => 'nootje nummer:',
+	'noisettes_composition' => 'nootjes specifiek voor compositie <i>@composition@</i>:',
+	'noisettes_disponibles' => 'beschikbare nootjes',
+	'noisettes_page' => 'nootjes specifiek voor bladzijde <i>@type@</i>:',
+	'noisettes_toutes_pages' => 'nootjes voor alle bladzijdes:',
+	'noizetier' => 'Notenboom',
+	'nom_bloc_contenu' => 'Inhoud',
+	'nom_bloc_extra' => 'Extra',
+	'nom_bloc_navigation' => 'Navigatie',
+	'nom_bloctexte' => 'Blok vrije tekst',
+	'nom_codespip' => 'Vrije SPIP code',
+	'non' => 'Nee',
+	'notice_enregistrer_rang' => 'Klik op Vastleggen om de volgorde van de nootjes op te slaan.',
+
+	// O
+	'operation_annulee' => 'Handeling geannuleerd.',
+	'oui' => 'Ja',
+
+	// P
+	'page' => 'Bladzijde',
+	'page_autonome' => 'Autonome bladzijde',
+	'probleme_droits' => 'Je hebt onvoldoende rechten om deze aanpassing uit te voeren.',
+
+	// Q
+	'quitter_mode_noisettes' => 'Verlaat het aanpassen van de nootjes',
+
+	// R
+	'retour' => 'Terug',
+
+	// S
+	'suggestions' => 'Suggesties',
+
+	// W
+	'warning_noisette_plus_disponible' => 'LET OP: dit nootje is niet meer beschikbaar.',
+	'warning_noisette_plus_disponible_details' => 'Het skelet van dit nootje (<i>@squelette@</i>) is niet langer toegankelijk. Mogelijk maakt het gebruik van een niet langer aktieve plugin.'
+);
+
+?>
