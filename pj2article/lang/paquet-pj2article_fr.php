3d4db851df433dbd87bb0e744060c6daa5d17f5f
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-12-27 04:40:42 +0000

    [Salvatore] [source:_plugins_/pj2article/lang/ paquet-pj2article] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79642 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- pj2article/lang/paquet-pj2article_fr.php
+++ pj2article/lang/paquet-pj2article_fr.php
@@ -1,14 +1,14 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/pj2article/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// P
-	'pj2article_description' => 'Importer l\'ensemble des pièces jointes d\'une boîte mail et créer un article par fichier automatiquement',
+	'pj2article_description' => 'Importer l’ensemble des pièces jointes d’une boîte mail et créer un article par fichier automatiquement',
 	'pj2article_nom' => 'Importation des pièces jointes',
-	'pj2article_slogan' => 'Importer l\'ensemble des pièces jointes d\'une boîte mail et créer un article par fichier automatiquement',
+	'pj2article_slogan' => 'Importer l’ensemble des pièces jointes d’une boîte mail et créer un article par fichier automatiquement'
 );
 
 ?>
