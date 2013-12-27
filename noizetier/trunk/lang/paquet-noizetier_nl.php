6a00c66f8cae6775f26c454370e5fdabc1d631ef
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-12-27 04:40:33 +0000

    [Salvatore] [source:_plugins_/noizetier/trunk/lang/ paquet-noizetier] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79639 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ noizetier/trunk/lang/paquet-noizetier_nl.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-noizetier?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// N
+	'noizetier_description' => 'Notenboom levert een interface in de privé-ruimte waarmee voor ieder bladzijde nootjes kunnen worden toegevoegd aan de inhoud, navigatie en extra onderdelen. De nootjes vervolledigen zo de door het skelet bepaalde standaard inhoud.',
+	'noizetier_nom' => 'Notenboom (NoiZetier)',
+	'noizetier_slogan' => 'Beheer van nootjes, oorspronkelijke functionaliteit van squelettes Z'
+);
+
+?>
