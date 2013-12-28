20555c9bf17fa057b1ef19bac5df2533c97f4474
severo@rednegra.net <severo@rednegra.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-12-28 04:41:14 +0000

    [Salvatore] [source:_plugins_/pj2article/lang/ pj2article] Export depuis http://trad.spip.net de la langue es
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79665 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ pj2article/lang/pj2article_es.php
@@ -0,0 +1,26 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/pj2article?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cfg_errorbox' => 'Buzón de errores',
+	'cfg_errorbox_explication' => 'Elegir en que buzón mover los emails si aparece un error de tratamiento.',
+	'cfg_inbox' => 'Buzón de entrada',
+	'cfg_inbox_explication' => 'Elegir desde que buzón importar los archivos adjuntos.',
+	'cfg_outbox' => 'Buzón de salida',
+	'cfg_outbox_explication' => 'Elegir en que buzón desplazar los emails una vez tratados.',
+	'cfg_titre_parametrages' => 'Parámetros de importación de los archivos adjuntos',
+
+	// P
+	'pj2article_titre' => 'Importar adjuntos de emails como artículos',
+
+	// T
+	'titre_page_configurer_pj2article' => 'Importación de los archivos adjuntos a los emails'
+);
+
+?>
