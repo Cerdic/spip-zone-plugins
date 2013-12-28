9639535c16790a564137b59326e7447b5ffbc137
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-12-28 04:41:04 +0000

    [Salvatore] [source:_plugins_/jquery_file_upload/trunk/lang/ jfu] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79663 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ jquery_file_upload/trunk/lang/jfu_nl.php
@@ -0,0 +1,42 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/jfu?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'analyze_document' => 'Analyse van het document',
+
+	// B
+	'bouton_cancel_upload' => 'Annuleer het online vertonen',
+	'bouton_delete' => 'Verwijderen',
+	'bouton_start' => 'Verzenden',
+	'bouton_start_upload' => 'Start het online vertonen',
+
+	// E
+	'erreur_emptyresult' => 'De terugzending is leeg',
+	'erreur_fichier_trop_gros' => 'Het bestand is te groot.',
+	'erreur_fichier_trop_petit' => 'Het bestand is te klein.',
+	'erreur_invalid_file_type' => 'Ongeldig type bestand.',
+	'erreur_max_number_files' => 'Je hebt het maximum aantal bestanden overschreden',
+	'erreur_message_erreur' => 'Fout:',
+	'erreur_uploadedbytes' => 'Het verzonden aantal bytes overschrijdt het maximum',
+
+	// L
+	'legend_mise_en_ligne_multiple' => 'Kies de bestanden...',
+	'legend_mise_en_ligne_unique' => 'Kies het bestand...',
+
+	// M
+	'message_file_size_limit' => 'De maximale grootte van een bestand is @taille@ MB',
+	'message_queue_limit_max' => 'Het maximum aantal bestanden in de wachtrij is@nb@.',
+	'message_types_fichiers_autorises' => 'De toegelaten extensies zijn: @types@',
+
+	// T
+	'temps_passe' => 'gedaan',
+	'temps_restant' => 'overblijvend'
+);
+
+?>
