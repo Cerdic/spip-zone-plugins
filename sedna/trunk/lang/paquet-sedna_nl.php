575ebe8fb39484d83443893557b8a45deaf835bc
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-12-28 04:41:40 +0000

    [Salvatore] [source:_plugins_/sedna/trunk/lang/ paquet-sedna] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79670 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ sedna/trunk/lang/paquet-sedna_nl.php
@@ -0,0 +1,17 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-sedna?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// S
+	'sedna_description' => 'Sedna voegt RSS (en ATOM enz.) feeds samen gebaseerd op de in SPIP gevolgde sites. De presentatie is enigzins gebaseerd  op « Safari RSS ».
+
+Het objectief van de plugin is eenvoudig in zijn code te zijn en te blijven, en snel te laden. En ook een beetje esthetisch.',
+	'sedna_slogan' => 'RSS agregator'
+);
+
+?>
