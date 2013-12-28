b1855e243d86bc5b849ca63eb27d0cc71fb49873
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-12-28 04:40:56 +0000

    [Salvatore] [source:_plugins_/crud/lang/ paquet-crud] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79662 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ crud/lang/paquet-crud_nl.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-crud?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'crud_description' => 'Een C(r)UD interface voor SPIP die kan worden gebruikt door middel van een in de URL gedefinieerde actie of een directe aanroep.',
+	'crud_slogan' => 'Interface voor het maken, aanpassen en verwijderen van een object'
+);
+
+?>
