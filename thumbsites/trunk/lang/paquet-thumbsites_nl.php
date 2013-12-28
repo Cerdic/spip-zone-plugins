1139ac5c1e2f4a4edf2b5cc99016aef3d0a02544
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-12-28 04:41:31 +0000

    [Salvatore] [source:_plugins_/thumbsites/trunk/lang/ paquet-thumbsites] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79668 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ thumbsites/trunk/lang/paquet-thumbsites_nl.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-thumbsites?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// T
+	'thumbsites_description' => 'Thumbsites levert een systeem voor screenshots van sites, aangemaakt op toegewezen servers. De plugin bied bakens, filters en modellen om een miniatuurweergave van een bepaalde site te tonen. Deze plugin beheert ook een cache die inspringt wanneer de servers niet bereikbaar zijn en het vertonen van de afbeelding versnelt. Configuratie gebeurt in de privé-ruimte.',
+	'thumbsites_slogan' => 'Een miniatuurafbeelding van gerefereerde sites'
+);
+
+?>
