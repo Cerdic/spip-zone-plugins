298b4917903db7050745e531c31e732536331971
gildas.cotomale@gmail.com <gildas.cotomale@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-12-28 13:37:55 +0000

    erreur dans r73009 : ne pas interrompre le traitement de la balise quand il n'y a pas de destination (puisqu'heureusement elle ne renvoie rien) sinon on provoque une erreur de compilation (signalée par Manu http://contrib.spip.net/Associaspip-2-2?lang=en#forum472540 )
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79678 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- Associaspip/trunk/balise/editeur_destinations.php
+++ Associaspip/trunk/balise/editeur_destinations.php
@@ -14,19 +14,17 @@ if (!defined('_ECRIRE_INC_VERSION'))
 include_spip('inc/association_comptabilite');
 
 function balise_EDITEUR_DESTINATIONS_dist ($p) {
-	if ($GLOBALS['association_metas']['destinations']) // on recupere dans l'environement id_dest,montant_dest, et defaut_dest qui doivent donc etre assignees par la fonction charger du formulaire contenant la balise
-		return calculer_balise_dynamique($p, 'EDITEUR_DESTINATIONS', array('id_dest', 'montant_dest', 'defaut_dest') );
-	else
-		return '';
+	// on recupere dans l'environement id_dest, montant_dest, et defaut_dest qui doivent donc etre assignees par la fonction charger du formulaire contenant la balise
+	return calculer_balise_dynamique($p, 'EDITEUR_DESTINATIONS', array('id_dest', 'montant_dest', 'defaut_dest') );
 }
 
 function balise_EDITEUR_DESTINATIONS_dyn($id_dest, $montant_dest, $defaut_dest) {
-	$destinations_id_montant = array();
+	$destinations = array();
 	if (($id_dest) && ($montant_dest))
-		foreach ($id_dest as $k => $v) {
-			$destinations_id_montant[$v] = $montant_dest[$k];
+		foreach ($id_dest as $k => $v) { // creer un tableau qui associe $id_dest et $montant_dest
+			$destinations[$v] = $montant_dest[$k];
 		}
-	return filtre_selecteur_compta_destinations($destinations_id_montant, $defaut_dest);
+	return filtre_selecteur_compta_destinations($destinations, $defaut_dest);
 }
 
 ?>
