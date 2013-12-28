9bf143e6150b5892b381e0df5458bd1c2dd2c48f
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-12-28 04:40:41 +0000

    [Salvatore] [source:_plugins_/bonne_annee/lang/ paquet-bonne_annee] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79657 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- bonne_annee/lang/paquet-bonne_annee_fr.php
+++ bonne_annee/lang/paquet-bonne_annee_fr.php
@@ -1,15 +1,15 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/bonne_annee/trunk/lang/
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/bonne_annee/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-	// A
-	'bonne_annee_description' => 'Affiche tous les ans, durant le mois de janvier, un message souhaitant une bonne ann&#233;e.
+	// B
+	'bonne_annee_description' => 'Affiche tous les ans, durant le mois de janvier, un message souhaitant une bonne année.
 Ajouter <code>#INCLURE{fond=bonne_annee}</code> dans vos pages.',
 	'bonne_annee_nom' => 'Bonne année',
-	'bonne_annee_slogan' => 'Souhaiter la bonne ann&#233;e sur votre site'
+	'bonne_annee_slogan' => 'Souhaiter la bonne année sur votre site'
 );
 
 ?>
