a0e369ad282cadc152773030840cf917f2fbb764
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-12-28 04:40:52 +0000

    [Salvatore] [source:_plugins_/critere_mots/trunk/lang/ paquet-critere_mots] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79660 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ critere_mots/trunk/lang/paquet-critere_mots_nl.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-critere_mots?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'critere_mots_description' => 'Laat toe elementen te tonen die meerdere trefwoorden gemeen hebben door middel van de omgevingsvariabele $mots[]',
+	'critere_mots_slogan' => 'Objecten selecteren die gemeenschappelijke trefwoorden hebben'
+);
+
+?>
