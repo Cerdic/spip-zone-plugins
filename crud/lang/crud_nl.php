155a2b714b392c091b4be9b2c339b3975f0a039c
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-12-28 04:40:54 +0000

    [Salvatore] [source:_plugins_/crud/lang/ crud] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79661 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ crud/lang/crud_nl.php
@@ -0,0 +1,24 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/crud?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// E
+	'erreur_action_erronee' => 'CRUD: foutieve actie @action@',
+	'erreur_action_inconnue_table' => 'CRUD: onbekende actie @action@ voor tabel @table@',
+	'erreur_article_inconnue' => 'CRUD: artikel @id@ bestaat niet',
+	'erreur_creation' => 'CRUD:  het object met type "@objet@" kan niet worden gemaakt (Controleer je rechten)',
+	'erreur_info_obligatoire' => 'CRUD: Het veld @info@ is verplicht',
+	'erreur_objet_inexistant' => 'CRUD: object @objet@ #@id_objet@ bestaat niet',
+	'erreur_rubrique_inconnue' => 'CRUD: rubriek @id@ bestaat niet',
+	'erreur_suppression' => 'CRUD: fout bij verwijdering van object "@objet@" #@id_objet@ (Controleer je rechten)',
+	'erreur_table_erronee' => 'CRUD: foutieve tabel @table@',
+	'erreur_table_inconnue' => 'CRUD: onbekende tabel @table@',
+	'erreur_update' => 'CRUD: fout bij het aanpassen van object "@objet@" #@id@ (Controleer je rechten)'
+);
+
+?>
