2a53f6f97aab411210436246bc7b0d1e616fc37d
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-12-28 04:41:29 +0000

    [Salvatore] [source:_plugins_/thumbsites/trunk/lang/ thumbsites] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79667 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ thumbsites/trunk/lang/thumbsites_nl.php
@@ -0,0 +1,40 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/thumbsites?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'bouton_copier_comme_logo' => 'Definiëren als logo',
+	'bouton_rafraichir' => 'Ververs de afbeelding',
+
+	// C
+	'cfg_descriptif' => 'Op deze bladzijde kun je de plugin Thumbsites configureren, en in het bijzonder de afbeeldingsserver kiezen. Kijk voor meer informatie op <a href="http://contrib.spip.net/?article2584">de documentatie op contrib</a>',
+	'cfg_inf_apercite_presentation' => '<a href="http://www.apercite.fr">apercite.fr</a> biedt een gratis service die geen identificatie vereist.',
+	'cfg_inf_choix_serveur' => 'Kies de server die de afbeeldingen van de gerefereerde sites gaat leveren en vul eventuele parameters aan.',
+	'cfg_inf_miwim_presentation' => 'Om deze service te gebruiken moet je een link maken naar de site <a href="http://thumbs.miwim.fr">Miwim</a>. De aanwezigheid van de link wordt regelmatig door een script gecontroleerd.',
+	'cfg_inf_rotothumb_presentation' => 'Om deze service te gebruiken hoef je je niet te regsitreren, maar je moet een link plaatsen naar de site <a href="http://www.robothumb.com">Robothumb</a>. De aanwezigheid van de link wordt regelmatig gecontroleerd.',
+	'cfg_inf_websnapr_presentation' => 'Om deze service te gebruiken moet je zijn ingeschreven op de site <a href="http://www.websnapr.com">Websnapr</a>. De gratis versie laat 250.000 site captures per maand toe, met een ritme van 80 captures per uur.',
+	'cfg_itm_serveur_apercite' => 'Apercite.fr',
+	'cfg_itm_serveur_miwim' => 'Miwim',
+	'cfg_itm_serveur_robothumb' => 'Robothumb',
+	'cfg_itm_serveur_websnapr' => 'Websnapr',
+	'cfg_itm_taille_vignette' => '@taille@ pixels',
+	'cfg_lbl_cle' => 'Je sleutel',
+	'cfg_lbl_duree_cache' => 'Duur van de cache (in dagen)',
+	'cfg_lbl_serveur' => 'Server',
+	'cfg_lbl_taille_vignette' => 'Afmeting van de afbeeldingen',
+	'cfg_lbl_usage' => 'Gebruiksvoorwaarden',
+	'cfg_lgd_cache' => 'Cache',
+	'cfg_lgd_choix_serveur' => 'Service',
+	'cfg_titre_form' => 'Thumbsites configureren',
+	'cfg_titre_page' => 'Thumbsites',
+
+	// T
+	'titre_thumbshot_site' => 'MINIAFBEELDING VAN DE SITE'
+);
+
+?>
