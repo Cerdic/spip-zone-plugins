b328376c21b3ea595f3937c58ae4ad373632e087
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-12-29 04:38:54 +0000

    [Salvatore] [source:_plugins_/messagerie/branches/messagerie_2_0/lang/ ecrire_message] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79691 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ messagerie/branches/messagerie_2_0/lang/ecrire_message_nl.php
@@ -0,0 +1,38 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/ecrire_message?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'a_un_ami' => 'Een bericht schrijven',
+
+	// E
+	'envoye_a' => 'Aan ',
+	'envoyer_un_message' => 'een bericht verzenden',
+
+	// L
+	'label_destinataire' => 'Geadresseerde',
+	'label_effacer' => 'wissen',
+	'label_envoyer' => 'Verzenden',
+	'label_marquer_lus' => 'markeren als gelezen',
+	'label_marquer_non_lus' => 'markeren als ongelezen',
+	'label_objet' => 'Onderwerp',
+	'label_texte_message' => 'Bericht',
+
+	// M
+	'message_envoye' => 'Je bericht werd verzonden',
+	'message_envoye_erreur' => 'Er is een fout opgetreden, je bericht werd niet verzonden',
+
+	// S
+	'selection_aucun' => 'geen',
+	'selection_lus' => 'gelezen',
+	'selection_non_lus' => 'ongelezen',
+	'selection_selectionner' => 'kiezen',
+	'selection_tous' => 'alles'
+);
+
+?>
