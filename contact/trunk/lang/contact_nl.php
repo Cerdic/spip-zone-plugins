eba663d6b1dcbee4efaa9e9bce35881973699e15
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-12-29 04:38:39 +0000

    [Salvatore] [source:_plugins_/contact/trunk/lang/ contact] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79688 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ contact/trunk/lang/contact_nl.php
@@ -0,0 +1,124 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/contact?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'alerte_effacement' => 'Bevestig het wissen van de configuratiegegevens van Contact.',
+
+	// B
+	'bouton_effacer' => 'Wissen',
+
+	// C
+	'champ_adresse' => 'Adres',
+	'champ_civi_explication' => 'Gevorderde instellingen van het veld aanhef',
+	'champ_civi_mademoiselle_ok' => 'Stel de keuze « Mejuffrouw » voor in het veld aanhef',
+	'champ_civilite' => 'Aanhef',
+	'champ_code_postal' => 'Postcode',
+	'champ_etat' => 'Staat',
+	'champ_impose' => 'verplicht veld',
+	'champ_infolettre' => 'Infobrief',
+	'champ_mail' => 'Mail',
+	'champ_nom' => 'Naam',
+	'champ_obligatoire' => 'Verplicht',
+	'champ_organisation' => 'Organisatie',
+	'champ_pays' => 'Land',
+	'champ_portable' => 'GSM',
+	'champ_prenom' => 'Voornaam',
+	'champ_sujet' => 'Onderwerp',
+	'champ_telephone' => 'Telefoon',
+	'champ_texte' => 'Tekst',
+	'champ_ville' => 'Plaats',
+	'configuration_barre_forum' => 'Toon een forum knoppenbalk.',
+	'configuration_barre_impossible' => 'Een andere plugin verhindert het gebruik van Penhouder (Porte-Plume) in de publieke site en onderstaande instellingen hebben dan ook geen effect.',
+	'configuration_barre_oui' => 'Toon de volledige knoppenbalk.',
+	'configuration_champs_message' => 'Instelling van Penhouder (Porte-Plume).',
+	'configuration_champs_message_explication' => 'Instelling van de typografische knoppenbalk van het tekstveld.',
+	'configuration_destinataires' => 'Geadresseerden',
+	'configuration_destinataires_explication' => 'Kies een of meerder auteurs waarmee bezoekers contact kunnen opnemen. Wanneer niets is gedefinieerd, wordt auteur nummer 1 (die de website installeerde) gebruikt.',
+	'configuration_destinataires_plusieurs' => 'Laat bezoekers <b>meerdere</b> geadresseerden kiezen uit onderstaande lijst',
+	'configuration_destinataires_plusieurs_et' => '... aangevuld met de auteurs die op het formulier zijn aangegeven.',
+	'configuration_destinataires_plusieurs_ou' => '... waaronder de auteurs die eventueel op het formulier zijn aangegeven',
+	'configuration_destinataires_tous' => 'Addresseer de mail aan <b>alle</b> auteurs',
+	'configuration_destinataires_tous_et' => '... aangevuld met de auteurs die op het formulier zijn aangegeven',
+	'configuration_destinataires_tous_ou' => '... of slechts degene die eventueel op het formulier zijn aangegeven',
+	'configuration_destinataires_un' => 'Forceer bezoekers <b>een enkele</b> geadresseerde te kiezen uit onderstaande lijst',
+	'configuration_destinataires_un_et' => '... aangevuld met de auteurs die op het formulier zijn aangegeven.',
+	'configuration_destinataires_un_ou' => '... waaronder de auteurs die eventueel op het formulier zijn aangegeven',
+	'configuration_intro' => 'Introductie',
+	'configuration_intro_explication' => 'Schrijf eventueel een introductietekst voor het formulier.',
+	'configuration_no_barre' => 'Geen knoppenbalk aanbieden.',
+	'configuration_no_previsualisation' => 'Toon de knoppenbalk zonder de preview-knop.',
+	'configuration_pj' => 'Bijlagen',
+	'configuration_pj_autoriser' => 'Sta bijlagen toe',
+	'configuration_pj_facteur_absent' => 'Voor deze functie is de plugin Postbode (Facteur) vereist.',
+	'configuration_pj_nb_max' => 'Maximum aantal bijlagen',
+	'configuration_regle_liste_explication' => 'Kies een formule voor de lijst van potentiële geadresseerden en de voorgestelde selectiemethode.',
+	'configuration_texteinfolettre' => 'Tekst infobrief',
+	'configuration_texteinfolettre_explication' => 'Kies eventueel de tekst bij de keuze infobrief.',
+	'consulter_memoire' => 'Dit bericht is op je site geregistreerd:',
+	'courriel_de' => 'Van',
+
+	// D
+	'deplacement_possible' => 'Vertikaal verplaatsen met schuiven en plakken',
+	'description_menu_contact' => 'Menu onderdeel naar de bladzijde van het contactformulier',
+	'description_page-contact' => 'Deze optionele bladzijde, toegankelijk via <i>spip.php ?page=contact</i>, bied een contactformulier (voeg de link aan je menu toe).',
+
+	// F
+	'form_destinataire' => 'Kies een geadresseerde',
+	'form_destinataires' => 'Kies de geadresseerden',
+	'form_pj_ajouter_pluriel' => 'Voeg bijlagen toe',
+	'form_pj_ajouter_singulier' => 'Voeg een bijlage toe',
+	'form_pj_fichier_ajoute' => 'Het bestand is toegevoegd: ',
+	'form_pj_importer' => 'Een bestand importeren',
+	'form_pj_previsu_pluriel' => 'Bijlagen',
+	'form_pj_previsu_singulier' => 'Bijlage',
+	'form_pj_supprimer' => 'Verwijderen',
+	'form_pj_titre' => 'Titel van het bestand',
+	'forum_attention_nbre_caracteres' => '<b>Let op!</b> Je bericht moet ten minste @nbre_caract@ tekens bevatten.',
+
+	// H
+	'horodatage' => 'Formulier geplaatst op: @horodatage@.',
+
+	// I
+	'inforigine' => 'Op het formulier doorgestuurde informatie: ',
+	'informez_moi' => 'Ik accepteer om per email informatie over deze organisatie te ontvangen.',
+	'infos_collecte' => 'Verzamelde informatie',
+	'infos_supplementaires' => 'Extra informatie',
+
+	// M
+	'madame' => 'Mevrouw',
+	'mademoiselle' => 'Mejuffrouw',
+	'message_redemander_pass' => 'Je e-mailadres "@email@" werd al geregistreerd bij een eerder contactbericht. Vraag om je account aan te passen een <a href="@url_pass@">wijziging van het wachtwoord</a> en identificeer jezelf een eerste keer.',
+	'modele_label_auteur' => 'Vooraf een auteur selecteren (facultatief)?',
+	'modele_nom_formulaire' => 'het geavanceerde contactformulier',
+	'monsieur' => 'Mijnheer',
+	'msg_messagerie' => 'Contactberichtensysteem',
+
+	// N
+	'noisette_label_afficher_titre_noisette' => 'Een titel tonen?',
+	'noisette_label_niveau_titre' => 'Kies de nieuwe titel',
+	'noisette_label_titre_noisette' => 'Titel:',
+	'noisette_label_titre_noisette_perso' => 'Aangepaste titel:',
+	'noisette_titre_perso' => 'Aangepaste titel',
+
+	// P
+	'par_qui' => 'Door: ',
+	'preselection' => 'Verwijder de selectie van geadresseerden',
+
+	// S
+	'sauv_message' => 'Contactberichten opslaan',
+	'sauv_message_explication' => 'Sla contactberichten op en toon ze in een speciale mailbox.',
+	'sauv_message_ok' => 'Contactberichten opslaan',
+	'succes' => 'Bedankt, je bericht werd verzonden.<br />Wij antwoorden zo spoedig mogelijk.<br />Het team van @equipe_site@',
+
+	// T
+	'titre' => 'Contactformulier',
+	'titre_court' => 'Contact'
+);
+
+?>
