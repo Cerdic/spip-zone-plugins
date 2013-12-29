648b736ea4150725546cda2c324ecc4765e6e756
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-12-29 04:38:56 +0000

    [Salvatore] [source:_plugins_/messagerie/branches/messagerie_2_0/lang/ formulaires] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79692 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ messagerie/branches/messagerie_2_0/lang/formulaires_nl.php
@@ -0,0 +1,23 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/formulaires?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// E
+	'email_invalide' => 'Dit e-mailadres is niet geldig',
+	'erreur_enregistrement' => 'Tijdens het registreren is een fout opgetreden',
+
+	// I
+	'info_obligatoire' => '*',
+	'info_obligatoire_legende' => 'verplichte velden',
+	'info_obligatoire_rappel' => 'Deze informatie is verplicht!',
+
+	// V
+	'valeur_incorrecte' => 'Deze waarde is onjuist'
+);
+
+?>
