6653f70a0086b5c3e58151ad523cc305229b8996
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-12-29 04:38:58 +0000

    [Salvatore] [source:_plugins_/messagerie/branches/messagerie_2_0/lang/ messagerie] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79693 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ messagerie/branches/messagerie_2_0/lang/messagerie_nl.php
@@ -0,0 +1,33 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/messagerie?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'aucun_message_envoye' => 'Je hebt geen enkel bericht verzonden.',
+	'aucun_message_recu' => 'Je hebt geen enkel bericht ontvangen.',
+
+	// L
+	'label_selection' => 'Kiezen',
+
+	// M
+	'mail_sujet_envoyermessage' => '[@nom_site@] Je hebt een nieuw bericht',
+	'mail_texte_envoyermessage' => '@nom@ je hebt een nieuw bericht verzonden (@url_site@)!',
+
+	// R
+	'replier' => 'Dit bericht intrekken',
+	'repondre_message' => 'Dit bericht beantwoorden',
+
+	// T
+	'texte_des_nouveaux_messages' => 'Je hebt <b>@nb@</b> nieuwe berichten',
+	'texte_signature_email' => '----
+Bericht verzonden door @nom_site@ (@url_site@)',
+	'texte_un_nouveau_message' => 'Je hebt <b>een</b> nieuw bericht',
+	'titre_message_general' => 'Algemene aankondiging'
+);
+
+?>
