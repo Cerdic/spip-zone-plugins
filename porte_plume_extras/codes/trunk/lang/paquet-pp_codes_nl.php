54ad53682b89454cb8685949aafcc323da234028
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-12-31 04:39:39 +0000

    [Salvatore] [source:_plugins_/porte_plume_extras/codes/trunk/lang/ paquet-pp_codes] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79763 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ porte_plume_extras/codes/trunk/lang/paquet-pp_codes_nl.php
@@ -0,0 +1,17 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-pp_codes?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// P
+	'pp_codes_description' => 'Beheer van een knoppenbalk met informatica codes door middel van de knoppenbalk van Porte Plume, de Penhouder.
+Voegt ook een snelkoppeling <code>[->ecrire/inc_versions.php#trac]</code> toe.',
+	'pp_codes_nom' => 'Informatica-codes voor de Penhouder',
+	'pp_codes_slogan' => 'Voegt knoppen toe aan Porte Plume, de Penhouder, voor informatica-codes'
+);
+
+?>
