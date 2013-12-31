521dea7cfee01bdd24234b93a228d4697bd4129f
klaus@spip.de <klaus@spip.de@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-12-31 04:38:39 +0000

    [Salvatore] [source:_plugins_/auteurs_syndic/trunk/lang/ paquet-auteurs_syndic] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79742 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ auteurs_syndic/trunk/lang/paquet-auteurs_syndic_nl.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-auteurs_syndic?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'auteurs_syndic_description' => 'Biedt de mogelijkheid om auteurs toe te voegen aan gesyndiceerde sites',
+	'auteurs_syndic_nom' => 'Auteurs voor gesyndiceerde sites',
+	'auteurs_syndic_slogan' => 'Biedt de mogelijkheid om auteurs toe te voegen aan gesyndiceerde sites'
+);
+
+?>
