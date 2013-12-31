5f401bef396ec7ad695965caaf8bd4fa873ec238
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-12-31 04:39:16 +0000

    [Salvatore] [source:_plugins_/emballe_medias/media_collections/trunk/lang/ paquet-collections] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79756 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ emballe_medias/media_collections/trunk/lang/paquet-collections_nl.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-collections?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'collections_description' => 'Maak mediacollecties, albums...',
+	'collections_nom' => 'Mediacollecties',
+	'collections_slogan' => 'Maak mediacollecties'
+);
+
+?>
