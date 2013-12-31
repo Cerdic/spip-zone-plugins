fe1cc150a6c452285bc4e805939b1c911b1df3c2
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-12-31 04:39:00 +0000

    [Salvatore] [source:_plugins_/critere_mots/trunk/lang/ paquet-critere_mots] Export depuis http://trad.spip.net de la langue en
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79750 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ critere_mots/trunk/lang/paquet-critere_mots_en.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-critere_mots?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'critere_mots_description' => 'Allows the display of elements which have multiple keywords in common using variable $mots[], passed in the environnement',
+	'critere_mots_slogan' => 'Select objects which have keywords in common'
+);
+
+?>
