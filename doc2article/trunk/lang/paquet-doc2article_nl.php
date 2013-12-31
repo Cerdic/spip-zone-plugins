769cc82cca83281cc3a4a12263da144eb7c8bbcb
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-12-31 04:39:08 +0000

    [Salvatore] [source:_plugins_/doc2article/trunk/lang/ paquet-doc2article] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79753 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ doc2article/trunk/lang/paquet-doc2article_nl.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-doc2article?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// D
+	'doc2article_description' => 'Importeer alle bestanden uit de map tmp/upload en maak van ieder bestand automatisch een artikel.',
+	'doc2article_slogan' => 'Importeer alle bestanden uit de map tmp/upload en maak van ieder bestand automatisch een artikel'
+);
+
+?>
