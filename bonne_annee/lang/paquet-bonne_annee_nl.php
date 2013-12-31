41050e82506c701a020efeda5733cc3ad9c81d3d
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-12-31 04:38:42 +0000

    [Salvatore] [source:_plugins_/bonne_annee/lang/ paquet-bonne_annee] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79743 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ bonne_annee/lang/paquet-bonne_annee_nl.php
@@ -0,0 +1,17 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-bonne_annee?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'bonne_annee_description' => 'Toon in de maand januari van ieder jaar een nieuwjaarswens.
+Voeg <code>#INCLURE{fond=bonne_annee}</code> toe aan je bladzijdes.',
+	'bonne_annee_nom' => 'Nieuwjaarswens',
+	'bonne_annee_slogan' => 'Nieuwjaarswensen op je site'
+);
+
+?>
