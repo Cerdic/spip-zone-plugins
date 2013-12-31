e083f71307732b176548d0444b1a2b1bc5e5075e
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-12-31 04:38:37 +0000

    [Salvatore] [source:_plugins_/auteur_evenement/trunk/lang/ paquet-auteur_evenement] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79741 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ auteur_evenement/trunk/lang/paquet-auteur_evenement_nl.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-auteur_evenement?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'auteur_evenement_description' => 'Koppel auteurs aan een evenement',
+	'auteur_evenement_nom' => 'Evenement-auteurs',
+	'auteur_evenement_slogan' => 'Koppel auteurs aan een evenement'
+);
+
+?>
