c45d0f7f9a1194b017f81996f8ad5e08f661acf2
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-12-31 04:39:14 +0000

    [Salvatore] [source:_plugins_/emballe_medias/media_collections/trunk/lang/ collection] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79755 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ emballe_medias/media_collections/trunk/lang/collection_nl.php
@@ -0,0 +1,148 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/collection?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'ajouter_lien_collection' => 'Deze collectie toevoegen',
+
+	// B
+	'bouton_diogene_statut_poubelle_normal' => 'Doe je collectie in de prullenbak',
+	'bouton_diogene_statut_poubelle_normal_pas_auteur' => 'Doe deze collectie in de prullenbak',
+	'bouton_dissocier' => 'Ontkoppelen',
+	'bouton_fermer_collection' => 'Maak deze collectie persoonlijk',
+	'bouton_media_associer' => 'Aan een collectie koppelen',
+	'bouton_ouvrir_collection' => 'Maak deze collectie collaboratief',
+	'bouton_quitter_collection' => 'Niet langer aan deze collectie samenwerken',
+	'bouton_rejoindre_collection' => 'Aan deze collectie samenwerken',
+
+	// C
+	'collections_titre' => 'Collecties',
+
+	// D
+	'description_page_collections' => 'Alle collecties van de site.',
+	'diogene_champ_collection' => 'Aan een collectie toevoegen',
+
+	// E
+	'erreur_association_collection' => 'De toevoeging aan de collectie is mislukt.',
+	'erreur_autorisation_statut_modifier' => 'Je mag deze collectie niet aanpassen.',
+	'erreur_collection_non_existante' => 'Deze collectie bestaat niet.',
+	'erreur_media_document' => 'Let op. Het type van dit document komt niet overeen met het genre van de collectie.',
+	'erreur_selectionner_un_media' => 'Kies tenminste één type media.',
+	'explication_genre' => 'Welke types media mogen aan deze collectie worden toegevoegd?',
+	'explication_genre_defaut' => 'Standaardwaarde voor de mediatypes die mogen worden toegevoegd (voor versnelde aanmaak).',
+	'explication_medias_ajouter' => 'Onderstaande media zijn niet aan de collectie gekoppeld maar komen wel overeen met het genre. Kies welke je wilt toevoegen.',
+	'explication_medias_modifier' => 'Onderstaande media zijn aan de collectie gekoppeld. Om bepaalde types te ontkoppelen, klik je op de bijbehorende verwijderknop.',
+	'explication_medias_modifier_drag' => 'Je kunt ze herschikken door middel van schuiven / plakken.',
+	'explication_participer' => 'Klik op onderstaande knop om aan deze collectie bij te dragen.',
+	'explication_participer_non' => 'Klik op onderstaande knop om niet langer aan deze collectie bij te dragen.',
+	'explication_type' => 'Wat is de toepassing van de collectie?',
+	'explication_type_defaut' => 'Standaardwaarde voor de toepassing van de collectie (voor versnelde aanmaak).',
+
+	// G
+	'genre_mixed' => 'Mix (alle types)',
+	'genre_musique' => 'Geluid (uitsluitend audio: muziek...)',
+	'genre_photo' => 'Afbeeldingen (alleen plaatjes: foto`s, ikonen...)',
+	'genre_video' => 'Video (alleen beeldfragmenten)',
+
+	// I
+	'icone_creer_collection' => 'Een collectie maken',
+	'icone_modifier_collection' => 'Deze collectie aanpassen',
+	'info_1_collection' => 'één collectie',
+	'info_aucun_collection' => 'Geen collectie beschikbaar',
+	'info_collaborateurs' => 'De deelnemers:',
+	'info_collection_creation' => 'Collectie gemaakt op <abbr title="@date_abbr@">@date@</abbr> <span class="auteurs">door @auteurs@</span>.',
+	'info_collection_maj' => 'Aangepast: ',
+	'info_collections_auteur' => 'de collecties van deze auteur',
+	'info_media_dans_collection' => 'In collectie: ',
+	'info_media_dans_collections' => 'In @nb@ collecties: ',
+	'info_medias_1' => 'Eén media',
+	'info_medias_aucun' => 'Geen media',
+	'info_medias_nb' => '@nb@ media',
+	'info_nb_collections' => '@nb@ collecties',
+	'info_non_autorise_creer_collection' => 'Auteur "@id_auteur@" mag geen collectie maken.',
+	'info_non_autorise_modifier_collection' => 'Auteur "@id_auteur@" mag collectie @id@ niet aanpassen.',
+	'info_rang_collection' => 'Volgorde in de collection',
+
+	// L
+	'label_choix_collection' => 'Aan de collectie koppelen',
+	'label_collections_ajouter' => 'Aan de collectie toevoegen',
+	'label_collections_retirer' => 'Van de collectie verwijderen',
+	'label_date' => 'Datum',
+	'label_date_point' => 'Datum:',
+	'label_descriptif' => 'Omschrijving',
+	'label_descriptif_point' => 'Omschrijving:',
+	'label_genre' => 'Mediatypes',
+	'label_genre_defaut' => 'Standaard mediatypes',
+	'label_genre_point' => 'Mediatypes:',
+	'label_id_admin' => 'Beheerder van de collectie',
+	'label_id_admin_point' => 'Beheerder van de collectie:',
+	'label_mediabox' => 'MediaBox',
+	'label_mediabox_long' => 'MediaBox niet gebruiken voor het associatieformulier van een collectie.',
+	'label_titre' => 'Titel',
+	'label_titre_creer_rapide' => 'Titel van de collectie',
+	'label_titre_point' => 'Titel:',
+	'label_type' => 'Toepassing',
+	'label_type_defaut' => 'Standaard toepassing',
+	'label_type_point' => 'Toepassing:',
+	'legende_collection' => 'Collecties',
+	'legende_medias_collection_ajouter' => 'Media toevoegen',
+	'legende_medias_collection_modifier' => 'De medialijst aanpassen',
+	'lien_editer_collection' => 'Deze collectie bewerken',
+	'lien_toutes_collections' => 'Alle collecties',
+
+	// M
+	'message_aucune_collection_critere' => 'Geen enkele collectie komt overeen met jouw criteria.', # Utile pour des squelettes
+	'message_aucune_collection_publiee' => 'Geen enkele collectie wordt momenteel gepubliceerd.', # Utile pour des squelettes
+	'message_collection_aucun_media_publie' => 'Geen enkele van jouw media komt overeen met het genre van de collectie.',
+	'message_collection_media_utiliser_autres_auteurs' => 'Je mag media van ander auteurs koppelen door gebruik te maken van het toevoegformulier op elke mediabladzijde.',
+	'message_collection_media_utiliser_interface' => 'Publiceer een of meer media via de toegewezen interface om ze toe te kunnen voegen aan deze collectie.',
+	'message_collection_reorganisee' => 'De collection werd gereorganiseerd.',
+	'message_collection_vide' => 'Deze collectie is leeg',
+	'message_nombre_ajoute' => '1 media is aan de collectie gekoppeld.',
+	'message_nombre_ajoutes' => '@nb@ aan de collectie gekoppelde media.',
+	'message_nombre_dissocie' => '1 media is van de collectie ontkoppeld.',
+	'message_nombre_dissocies' => '@nb@ van de collectie ontkoppelde media.',
+
+	// O
+	'optgroup_collections_autres' => 'De andere collecties (collaboratief)',
+	'optgroup_collections_votre' => 'Jouw collecties',
+	'option_erreur_genre' => 'Het type komt niet overeen met het media: @genre@',
+
+	// P
+	'publier_une_collection' => 'Een collectie',
+	'publier_une_collection_desc' => 'Een mediacollectie maken.',
+
+	// R
+	'retirer_lien_collection' => 'Deze collectie intrekken',
+	'retirer_tous_liens_collections' => 'Alle collecties intrekken',
+
+	// T
+	'tab_associer_collection' => 'Media van de collectie',
+	'tab_editer_collection' => 'Aanpassing van de collectie',
+	'texte_ajouter_collection' => 'Een collectie toevoegen',
+	'texte_changer_statut_collection' => 'Deze collectie is:',
+	'texte_creer_associer_collection' => 'Een collectie maken en koppelen',
+	'texte_statut_publie' => 'gepubliceerd',
+	'title_voir_media' => 'Media bekijken: @titre@',
+	'titre_collection' => 'Collectie',
+	'titre_collections' => 'Collecties',
+	'titre_collections_rubrique' => 'Rubrieken van de rubriek',
+	'titre_creer_collection_rapide' => 'Een collectie maken',
+	'titre_langue_collection' => 'Taal van deze collectie',
+	'titre_logo_collection' => 'Logo van deze collectie',
+	'titre_media' => 'Media #@rang@: @titre@',
+	'titre_modifier_collection' => 'Aanpassen van collectie: @titre@',
+	'titre_modifier_collection_diogene' => 'Deze collectie aanpassen',
+	'titre_page_collections' => 'Collecties',
+	'titre_page_configurer_collections' => 'Collecties configureren',
+	'titre_participer' => 'Deelnemen',
+	'type_coop' => 'Collaboratief',
+	'type_perso' => 'Persoonlijk'
+);
+
+?>
