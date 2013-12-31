9901ee9d4581e3485ff935ca128219d473bb6a79
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-12-31 04:39:11 +0000

    [Salvatore] [source:_plugins_/doc2article/trunk/lang/ paquet-doc2article] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79754 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- doc2article/trunk/lang/paquet-doc2article_fr.php
+++ doc2article/trunk/lang/paquet-doc2article_fr.php
@@ -1,19 +1,13 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-doc2article
-// Langue: fr
-// Date: 28-06-2012 10:49:24
-// Items: 2
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/doc2article/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// D
-	'doc2article_description' => 'Importer l\'ensemble des fichiers du répertoire tmp/upload et créer un article par fichier automatiquement.',
-	'doc2article_slogan' => 'Importer l\'ensemble des fichiers du répertoire tmp/upload et créer un article par fichier automatiquement',
+	// D
+	'doc2article_description' => 'Importer l’ensemble des fichiers du répertoire tmp/upload et créer un article par fichier automatiquement.',
+	'doc2article_slogan' => 'Importer l’ensemble des fichiers du répertoire tmp/upload et créer un article par fichier automatiquement'
 );
+
 ?>
