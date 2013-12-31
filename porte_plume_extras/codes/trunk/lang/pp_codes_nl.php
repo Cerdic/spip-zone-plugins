a8b464958dbdc5424ab2f5e95c10e461d8ec7200
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-12-31 04:39:37 +0000

    [Salvatore] [source:_plugins_/porte_plume_extras/codes/trunk/lang/ pp_codes] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79762 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ porte_plume_extras/codes/trunk/lang/pp_codes_nl.php
@@ -0,0 +1,35 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/pp_codes?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// L
+	'label_activer_barre_edition' => 'Redactiebalk',
+	'label_activer_barre_forum' => 'Forumbalk',
+	'label_outils_actifs' => 'Actieve tools',
+
+	// O
+	'outil_inserer_cadre_css' => 'Geformatteerde CSS-code invoegen (kader)',
+	'outil_inserer_cadre_html' => 'Geformatteerde HTML-code invoegen (kader)',
+	'outil_inserer_cadre_php' => 'Geformatteerde PHP-code invoegen (kader)',
+	'outil_inserer_cadre_spip' => 'Geformatteerde SPIP-code invoegen (kader)',
+	'outil_inserer_cadre_xml' => 'Geformatteerde XML-code invoegen (kader)',
+	'outil_inserer_kbd' => 'Invoer toetsenbord invoegen (kdb)',
+	'outil_inserer_lien_trac' => 'Een link naar de trac van SPIP invoegen',
+	'outil_inserer_pre' => 'Geformatteerde code invoegen (pre)',
+	'outil_inserer_samp' => 'Exitcode invoeren (samp)',
+	'outil_inserer_var' => 'Een variabele invoeren (var)',
+
+	// P
+	'pp_codes' => 'Informatica-codes voor de Penhouder',
+
+	// T
+	'titre_activer_extension_sur' => 'Op welke knoppenbalk(en) activeren?',
+	'titre_configurer_pp_codes' => 'Configureer de extensie voor informatica-codes voor de Penhouder'
+);
+
+?>
