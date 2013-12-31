c0957c7c96769b53c764d4f459ec3f38f09c6d4d
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2013-12-31 04:38:49 +0000

    [Salvatore] [source:_plugins_/corbeille/trunk/lang/ corbeille] Export depuis http://trad.spip.net de la langue en
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79745 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- corbeille/trunk/lang/corbeille_en.php
+++ corbeille/trunk/lang/corbeille_en.php
@@ -11,6 +11,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'articles_corbeille_tous' => '@nb@ articles in the trash',
 	'articles_corbeille_un' => '1 article in the trash',
 	'aucun_element_supprime' => 'Nothing was removed from the trash',
+	'auteurs_corbeille_tous' => '@nb@ authors in the trash',
 	'auteurs_corbeille_un' => '1 autor in the trash',
 
 	// B
@@ -22,15 +23,23 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// E
 	'effacer' => 'Erase',
-	'effacer_tout' => 'Erase all', # RELIRE
+	'effacer_tout' => 'Erase all',
 	'element_supprime' => '@ids@ was removed from the trash',
 	'elements_supprimes' => '@ids@ were removed from the trash',
+
+	// F
+	'forums_prives_corbeille_tous' => '@nb@ private forum messages in the trash',
+	'forums_prives_corbeille_un' => '1 private forum message in the trash',
+	'forums_publics_corbeille_tous' => '@nb@ public forum messages in the trash',
+	'forums_publics_corbeille_un' => '1 public forum message in the trash',
 
 	// R
 	'readme' => 'This page allows the {{permanent deletion}} of documents sent to the recycle bin. You may leave documents in the recycle bin as long as you wish. It’s {{up to you}} to put out the rubbish.',
 
 	// S
 	'selectionner' => 'select',
+	'signatures_corbeille_tous' => '@nb@ signatures in the trash',
+	'signatures_corbeille_un' => '1 signature in the trash',
 	'sites_corbeille_tous' => '@nb@ websites in the trash',
 	'sites_corbeille_un' => '1 website in the trash',
 
