1f553e87a5c91773e0837913916c09ee10030f23
severo@rednegra.net <severo@rednegra.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-01-01 04:39:45 +0000

    [Salvatore] [source:_plugins_/pj2article/lang/ pj2article] Export depuis http://trad.spip.net de la langue es
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79779 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- pj2article/lang/pj2article_es.php
+++ pj2article/lang/pj2article_es.php
@@ -10,14 +10,28 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// C
 	'cfg_errorbox' => 'Buzón de errores',
 	'cfg_errorbox_explication' => 'Elegir en que buzón mover los emails si aparece un error de tratamiento.',
+	'cfg_ignorebox' => 'Buzón de emails ignorados',
+	'cfg_ignorebox_explication' => 'Elegir en que buzón desplazar los emails ignorados (sin archivo adjunto).',
+	'cfg_importation' => 'Parámetros de importación',
+	'cfg_importation_mail' => 'Enviar un email al autor luego de la importación',
+	'cfg_importation_zip' => 'Crear un archivo zip si un email contiene varios archivos adjuntos (sino, crear un artículo por archivo adjunto)',
 	'cfg_inbox' => 'Buzón de entrada',
 	'cfg_inbox_explication' => 'Elegir desde que buzón importar los archivos adjuntos.',
+	'cfg_intervalle_cron' => 'Intervalo CRON',
+	'cfg_intervalle_cron_explication' => 'Intervalo de tiempo entre dos pasajes del CRON (en segundos) para la importación de los emails.',
+	'cfg_nombre_mails' => 'Número de emails',
+	'cfg_nombre_mails_explication' => 'Número de emails a procesar por iteración de CRON.',
 	'cfg_outbox' => 'Buzón de salida',
-	'cfg_outbox_explication' => 'Elegir en que buzón desplazar los emails una vez tratados.', # MODIF
+	'cfg_outbox_explication' => 'Elegir en que buzón desplazar los emails una vez tratados.',
+	'cfg_statut' => 'Estatus',
+	'cfg_statut_explication' => 'Seleccione el estatuto de los artículos que serán creados.',
+	'cfg_titre_boites' => 'Buzones',
+	'cfg_titre_edition' => 'Parámetros del artículo creado',
+	'cfg_titre_importation' => 'Parámetros de importación',
 	'cfg_titre_parametrages' => 'Parámetros de importación de los archivos adjuntos',
 
 	// P
-	'pj2article_titre' => 'Importar adjuntos de emails como artículos', # MODIF
+	'pj2article_titre' => 'Importar adjuntos de emails como artículos',
 
 	// T
 	'titre_page_configurer_pj2article' => 'Importación de los archivos adjuntos a los emails'
