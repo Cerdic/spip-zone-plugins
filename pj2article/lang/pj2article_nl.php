30d9a70b8cab63e80fb8ad4a753aac5c704bdea2
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-01-01 04:39:47 +0000

    [Salvatore] [source:_plugins_/pj2article/lang/ pj2article] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79780 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ pj2article/lang/pj2article_nl.php
@@ -0,0 +1,40 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/pj2article?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cfg_errorbox' => 'Foutbox',
+	'cfg_errorbox_explication' => 'Kies naar welke mailbox de foutief verwerkte emails moeten worden verplaatst.',
+	'cfg_ignorebox' => 'Overgeslagen mailboxen',
+	'cfg_ignorebox_explication' => 'Kies in welke mailboxen de overgeslagen emails moeten worden geplaatst (zonder bijlage).',
+	'cfg_importation' => 'Importparameters',
+	'cfg_importation_mail' => 'Stuur de auteur na de import een email',
+	'cfg_importation_zip' => 'Maak een zip-bestand wanneer één email meerdere bijlagen bevat (en maak anders één artikel per bijlage)',
+	'cfg_inbox' => 'Inbox',
+	'cfg_inbox_explication' => 'Kies vanuit welke mailbox de bijlagen moeten worden geïmporteerd.',
+	'cfg_intervalle_cron' => 'CRON interval',
+	'cfg_intervalle_cron_explication' => 'Interval (in seconden) tussen twee CRON jobs voor het importeren van emails.',
+	'cfg_nombre_mails' => 'Aantal emails',
+	'cfg_nombre_mails_explication' => 'Aantal te behandelen emails per CRON job.',
+	'cfg_outbox' => 'Uitbox',
+	'cfg_outbox_explication' => 'Kies naar welke mailbox de verwerkte emails moeten worden verplaatst.',
+	'cfg_statut' => 'Status',
+	'cfg_statut_explication' => 'Kies de status van de gemaakte artikelen.',
+	'cfg_titre_boites' => 'Mailbox',
+	'cfg_titre_edition' => 'Parameters van het gemaakte artikel',
+	'cfg_titre_importation' => 'Importparameters',
+	'cfg_titre_parametrages' => 'Parameters voor het importeren van bijlagen',
+
+	// P
+	'pj2article_titre' => 'Importeren van bijlagen',
+
+	// T
+	'titre_page_configurer_pj2article' => 'Importeren van bijlagen'
+);
+
+?>
