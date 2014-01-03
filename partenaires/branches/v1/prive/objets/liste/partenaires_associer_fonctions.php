2e1dd624b48a059b530d7e8717f0eaf63d94cad4
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-01-03 10:24:56 +0000

    - permettre d'associer les partenaires à un objet au choix (cfg) - gérer les statuts de l'objet partenaire
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79811 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ partenaires/branches/v1/prive/objets/liste/partenaires_associer_fonctions.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Fonctions du squelette associé
+ *
+ * @plugin     Partenaires
+ * @copyright  2014
+ * @author     Rainer
+ * @licence    GNU/GPL
+ * @package    SPIP\Partenaires\Fonctions
+ */
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+
+// pour initiale et afficher_initiale
+include_spip('prive/objets/liste/auteurs_fonctions');
+
+?>
