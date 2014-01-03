dd3d3e59ab28569a0fffee3cd1af6e5a39b87596
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-01-03 09:48:44 +0000

    - permettre d'associer les partenaires à un objet au choix (cfg) - gérer les statuts de l'objet partenaire
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79807 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- partenaires/trunk/partenaires_pipelines.php
+++ partenaires/trunk/partenaires_pipelines.php
@@ -16,6 +16,17 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 function partenaires_affiche_milieu($flux) {
 	$texte = "";
 	$e = trouver_objet_exec($flux['args']['exec']);
+	
+	// partenaires sur les objets sélectionnées
+	$config=lire_config('partenaires',array());
+	$objets_partenaires=isset($config['objets'])?$config['objets']:array();
+	if (!$e['edition'] AND in_array($e['type'], $objets_partenaires)) {
+		$texte .= recuperer_fond('prive/objets/editer/liens', array(
+			'table_source' => 'partenaires',
+			'objet' => $e['type'],
+			'id_objet' => $flux['args'][$e['id_table_objet']]
+		));
+	}
 
 
 
