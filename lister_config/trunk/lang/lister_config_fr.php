bbf2a0b3a476476364faaddd3630ec876b55b66b
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-01-05 20:09:25 +0000

    On ajoute une page listant les pages ?exec=xx accessibles depuis le back-office de SPIP. Proof of concept
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79861 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- lister_config/trunk/lang/lister_config_fr.php
+++ lister_config/trunk/lang/lister_config_fr.php
@@ -7,8 +7,15 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// D
 	'description_page' => 'Vous pouvez trouver ci-dessous la liste des plugins actifs du site proposant une page de configuration du type <code>?exec=configurer_prefixe-plugin</code>.',
+	'description_lister_exec' => '<strong>Mise en garde !</strong> Faites attention à l\'utilisation de ces pages. <br/>On ne liste pas ici les pages de vue et d\'édition d\'un objet éditorial comme les pages d\'un article, de brèves, de mots clés, etc. <br /><em>Cette page est un proof of concept.</em>',
+
+	// I
+	'intertitre_exec' => 'Selon exec/*.php',
+	'intertitre_exec_contenu' => 'Selon squelettes/contenu',
+
 	// T
 	'titre_lister_config' => 'Les pages de configuration',
+	'titre_lister_exec' => 'La liste des pages ?exec=xxx',
 	'titre_page' => 'Pages de configuration des plugins actifs du site'
 
 );
