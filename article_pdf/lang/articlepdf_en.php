0edfd062a5064ecfade374b63415283c567edc56
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-01-07 04:38:19 +0000

    [Salvatore] [source:_plugins_/article_pdf/lang/ articlepdf] Export depuis http://trad.spip.net de la langue en
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79876 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- article_pdf/lang/articlepdf_en.php
+++ article_pdf/lang/articlepdf_en.php
@@ -1,10 +1,29 @@
 <?php
-
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/articlepdf?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-'enregistrer_pdf'=>"Save this article in PDF",
-);
 
+	// C
+	'copyright' => 'Copyright © ',
+
+	// D
+	'date_de_mise_en_ligne' => 'Publication date: ',
+	'date_de_parution' => 'Creation date: ',
+	'description' => 'Description: ',
+
+	// E
+	'enregistrer_pdf' => 'Save this article in PDF',
+	'extrait_de' => 'Extract of ',
+
+	// P
+	'post_scriptum' => 'PS: ',
+
+	// T
+	'tous_droits_reserves' => ' - All rights reserved'
+);
 
 ?>
