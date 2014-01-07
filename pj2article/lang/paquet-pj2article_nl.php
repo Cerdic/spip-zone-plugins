8774b1acd402531897347fdd536d142c45846950
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-01-07 04:38:42 +0000

    [Salvatore] [source:_plugins_/pj2article/lang/ paquet-pj2article] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79879 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ pj2article/lang/paquet-pj2article_nl.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-pj2article?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// P
+	'pj2article_description' => 'Importeer alle bijvoegsels uit een mailbox en maak automatisch één artikel per bestand',
+	'pj2article_nom' => 'Importeren van bijvoegsels',
+	'pj2article_slogan' => 'Importeer alle bijvoegsels uit een mailbox en maak automatisch één artikel per bestand'
+);
+
+?>
