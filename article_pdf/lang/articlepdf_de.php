cd06172802bd13b582c89bb1ecf7ea65ea030a80
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-01-07 04:38:24 +0000

    [Salvatore] [source:_plugins_/article_pdf/lang/ articlepdf] Export depuis http://trad.spip.net
    La langue 'de' devrait Ãªtre supprimÃ©e car trop peu traduite (12.50 %)
    
    
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79878 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- article_pdf/lang/articlepdf_de.php
+++ article_pdf/lang/articlepdf_de.php
@@ -1,10 +1,14 @@
 <?php
-
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/articlepdf?lang_cible=de
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-'enregistrer_pdf'=>"Als PDF speichern",
-);
 
+	// E
+	'enregistrer_pdf' => 'Als PDF speichern'
+);
 
 ?>
--- article_pdf/lang/articlepdf_fr.php
+++ article_pdf/lang/articlepdf_fr.php
@@ -1,27 +1,27 @@
 <?php
-
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/article_pdf/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// C
-	'copyright' => "Copyright © ",
-	
-	// D
-	'date_de_mise_en_ligne' => "Date de mise en ligne : ",
-	'date_de_parution'=> "Date de parution : ",
-	'description' => 'Description : ',
-	
-	// E
-	'enregistrer_pdf'=>"Enregistrer au format PDF",
-	'extrait_de' => "Extrait du ",
-	
-	// P
-	'post_scriptum' => "Post-scriptum : ",
-	
-	// T
-	'tous_droits_reserves' => ' - Tous droits réservés',
-);
+	'copyright' => 'Copyright © ',
 
+	// D
+	'date_de_mise_en_ligne' => 'Date de mise en ligne : ',
+	'date_de_parution' => 'Date de parution : ',
+	'description' => 'Description : ',
+
+	// E
+	'enregistrer_pdf' => 'Enregistrer au format PDF',
+	'extrait_de' => 'Extrait du ',
+
+	// P
+	'post_scriptum' => 'Post-scriptum : ',
+
+	// T
+	'tous_droits_reserves' => ' - Tous droits réservés'
+);
 
 ?>
