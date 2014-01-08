4a868e14b4cf5eede06ca1de5a31609d7bdcad4b
severo@rednegra.net <severo@rednegra.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-01-08 04:39:15 +0000

    [Salvatore] [source:_plugins_/article_pdf/lang/ articlepdf] Export depuis http://trad.spip.net de la langue es
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79902 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ article_pdf/lang/articlepdf_es.php
@@ -0,0 +1,29 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/articlepdf?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'copyright' => 'Copyright © ',
+
+	// D
+	'date_de_mise_en_ligne' => 'Fecha de publicación en línea: ',
+	'date_de_parution' => 'Fecha de redacción: ',
+	'description' => 'Descripción: ',
+
+	// E
+	'enregistrer_pdf' => 'Grabar en formato PDF',
+	'extrait_de' => 'Extraído de ',
+
+	// P
+	'post_scriptum' => 'Posdata: ',
+
+	// T
+	'tous_droits_reserves' => ' - Todos derechos reservados'
+);
+
+?>
