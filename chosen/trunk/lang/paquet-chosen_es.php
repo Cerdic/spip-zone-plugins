94a214b97360c4e688092cb4f1b2be84779af78a
raquelsbujaldon@gmail.com <raquelsbujaldon@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-01-12 04:38:26 +0000

    [Salvatore] [source:_plugins_/chosen/trunk/lang/ paquet-chosen] Export depuis http://trad.spip.net de la langue es
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@79971 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- chosen/trunk/lang/paquet-chosen_es.php
+++ chosen/trunk/lang/paquet-chosen_es.php
@@ -10,9 +10,14 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// C
 	'chosen_description' => '[Chosen->http://harvesthq.github.com/chosen/] es una biblioteca JavaScript que mejora la experiencia usuario de los selectores en los formularios HTML.
 
-La classe CSS <code>chosen</code> sur un <code><select></code> chargera automatiquement Chosen dessus.', # MODIF
-	'chosen_nom' => 'Chosen', # MODIF
-	'chosen_slogan' => 'Integrar la biblioteca Chosen en SPIP' # MODIF
+La clase CSS <code>chosen</code> en un <code><select></code> cambiará automáticamente Chosen arriba.
+Esta rama del plugin Chosen se basa en el fork de koenpunt - versión 1.0.0 - ver https://github.com/koenpunt/chosen/releases/.
+
+Con este fork, Chosen permite <a href=\'https://github.com/harvesthq/chosen/pull/166\'>crear nuevas opciones</a> en un &lt;select&gt; existente, a condición de que tenga la clase ’chosen-create-option’.
+Cuando chosen crea una nueva &lt;option&gt; (la palabra ’nuevo’ por ejemplo) en un &lt;select&gt;, ésta toma la siguiente forma: <code>&lt;option selected=\'selected\' value=\'chosen_nuevo\'&gt;nuevo&lt;/option&gt;</code>.
+Apreciar el prefijo ’chosen_’ añadido en el parámetro ’value’ para permitir diferenciar las &lt;option&gt; creadas por Chosen.',
+	'chosen_nom' => 'Chosen (fork de koenpunt)',
+	'chosen_slogan' => 'Integrar la biblioteca Chosen en SPIP (fork de koenpunt)'
 );
 
 ?>
