5d1ec4fa4ca5db902370cdc6d506d709bbebe2d8
gildas.cotomale@gmail.com <gildas.cotomale@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-01-13 21:43:12 +0000

    permettre au webmestre de pouvoir définir des licences propres au site (non encore documenté). cf. http://contrib.spip.net/Une-licence-pour-un-article?lang=fr#forum473256
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@80025 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- licence/inc/licence.php
+++ licence/inc/licence.php
@@ -10,105 +10,100 @@
 
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
-$GLOBALS['licence_licences'] = array (
-			"1" 	=> array(
-				# nom de la licence
-				"name" 	=> _T('licence:titre_copyright'),
-				# numero d'identifiacation de la licence
-				"id"		=> "1",
-				# nom de l'icone de la licence (optionnel)
-				# l'icone devra être placé dans le répertoire img_pack du plugin
-				"icon"		=> "copyright.png",
-				# Lien documentaire vers la licence (optionnel)
-				"link"		=> "",
-				# Description un peu plus détaillée de la licence
-				"description" 	=> _T('licence:description_copyright'),
-				# Une abbréviation commune à toutes les langues
-				"abbr" => 'copyright'),
-			"2" 	=> array(
-				"name" 		=> 	_T('licence:titre_gpl'),
-				"id"		=> "2",
+if (!defined('MES_LICENCES')) define('MES_LICENCES', array());
+
+$GLOBALS['licence_licences'] = array_merge(array (
+		"1" => array(
+				"name"		=> _T('licence:titre_copyright'), // nom de la licence
+				"id"		=> "1", // numero d'identifiacation de la licence
+				"icon"		=> "copyright.png", // nom de l'icone de la licence (optionnel),  l'icone devra être placé dans le répertoire img_pack du plugin
+				"link"		=> "", // lien documentaire vers la licence (optionnel)
+				"description" => _T('licence:description_copyright'), // description un peu plus détaillée de la licence
+				"abbr"		=> 'copyright'), // une abbréviation commune à toutes les langues
+		"2" => array(
+				"name"		=> 	_T('licence:titre_gpl'), // nom de la licence
+				"id"		=> "2", // numero d'identifiacation de la licence
 				"icon"		=> "gnu-gpl.png",
 				"link"		=> _T('licence:lien_gpl'),
 				"description" => _T('licence:description_gpl'),
-				"abbr" => "GPL"),
-			"3" 	=> array(
-				"name" 		=> _T('licence:titre_cc_by'),
-				"id"		=> "3",
+				"abbr"		=> "GPL"),
+		"3" => array(
+				"name"		=> _T('licence:titre_cc_by'), // nom de la licence
+				"id"		=> "3", // numero d'identifiacation de la licence
 				"icon"		=> "cc-by.png",
 				"link"		=> _T('licence:lien_cc_by'),
 				"description" => _T('licence:description_cc_by'),
-				"abbr" => "cc-by"),
-			"4" 	=> array(
-				"name" 		=> _T('licence:titre_cc_by_nd'),
-				"id"		=> "4",
+				"abbr"		=> "cc-by"),
+		"4" => array(
+				"name"		=> _T('licence:titre_cc_by_nd'), // nom de la licence
+				"id"		=> "4", // numero d'identifiacation de la licence
 				"icon"		=> "cc-by-nd.png",
 				"link"		=> _T('licence:lien_cc_by_nd'),
 				"description" => _T('licence:description_cc_by_nd'),
-				"abbr" 		=> "cc-by-nd"),
-			"5" 	=> array(
-				"name" 		=> _T('licence:titre_cc_by_nc_nd'),
-				"id"		=> "5",
+				"abbr"		=> "cc-by-nd"),
+		"5" => array(
+				"name"		=> _T('licence:titre_cc_by_nc_nd'), // nom de la licence
+				"id"		=> "5", // numero d'identifiacation de la licence
 				"icon"		=> "cc-by-nc-nd.png",
 				"link"		=> _T('licence:lien_cc_by_nc_nd'),
 				"description" => _T('licence:description_cc_by_nc_nd'),
-				"abbr" 		=> "cc-by-nc-nd"),
-			"6" 	=> array(
-				"name" 		=> _T('licence:titre_cc_by_nc'),
-				"id"		=> "6",
+				"abbr"		=> "cc-by-nc-nd"),
+		"6" => array(
+				"name"		=> _T('licence:titre_cc_by_nc'), // nom de la licence
+				"id"		=> "6", // numero d'identifiacation de la licence
 				"icon"		=> "cc-by-nc.png",
 				"link"		=> _T('licence:lien_cc_by_nc'),
 				"description" => _T('licence:description_cc_by_nc'),
-				"abbr" 		=> "cc-by-nc"),
-			"7" 	=> array(
-				"name" 		=> _T('licence:titre_cc_by_nc_sa'),
-				"id"		=> "7",
+				"abbr"		=> "cc-by-nc"),
+		"7" => array(
+				"name"		=> _T('licence:titre_cc_by_nc_sa'), // nom de la licence
+				"id"		=> "7", // numero d'identifiacation de la licence
 				"icon"		=> "cc-by-nc-sa.png",
 				"link"		=> _T('licence:lien_cc_by_nc_sa'),
 				"description" => _T('licence:description_cc_by_nc_sa'),
-				"abbr" 		=> "cc-by-nc-sa"),
-			"8" 	=> array(
-				"name" 		=> _T('licence:titre_cc_by_sa'),
-				"id"		=> "8",
+				"abbr"		=> "cc-by-nc-sa"),
+		"8" => array(
+				"name"		=> _T('licence:titre_cc_by_sa'), // nom de la licence
+				"id"		=> "8", // numero d'identifiacation de la licence
 				"icon"		=> "cc-by-sa.png",
 				"link"		=> _T('licence:lien_cc_by_sa'),
 				"description" => _T('licence:description_cc_by_sa'),
-				"abbr" 		=> "cc-by-sa"),
-			"9" 	=> array(
-				"name" 		=> _T('licence:titre_art_libre'),
-				"id"		=> "9",
+				"abbr"		=> "cc-by-sa"),
+		"9" => array(
+				"name"		=> _T('licence:titre_art_libre'), // nom de la licence
+				"id"		=> "9", // numero d'identifiacation de la licence
 				"icon"		=> "copyleft.png",
 				"link"		=> _T('licence:lien_art_libre'),
 				"description" => _T('licence:description_art_libre'),
-				"abbr" 		=> "lal"),
-			"10" 	=> array(
-				"name" 		=> _T('licence:titre_gfdl'),
-				"id"		=> "10",
+				"abbr"		=> "lal"),
+		"10" => array(
+				"name"		=> _T('licence:titre_gfdl'), // nom de la licence
+				"id"		=> "10", // numero d'identifiacation de la licence
 				"icon"		=> "gnu-gfdl.png",
 				"link"		=> _T('licence:lien_gfdl'),
 				"description" => _T('licence:description_gfdl'),
-				"abbr" 		=> "gfdl"),
-			"11" 	=> array(
-				"name" 		=> _T('licence:titre_wtfpl'),
-				"id"		=> "11",
+				"abbr"		=> "gfdl"),
+		"11" => array(
+				"name"		=> _T('licence:titre_wtfpl'), // nom de la licence
+				"id"		=> "11", // numero d'identifiacation de la licence
 				"icon"		=> "wtfpl.png",
 				"link"		=> _T('licence:lien_whfpl'),
 				"description" => _T('licence:description_wtfpl'),
-				"abbr" 		=> "wtfpl"),
-			"12" 	=> array(
-				"name" 		=> _T('licence:titre_cc0'),
-				"id"		=> "12",
+				"abbr"		=> "wtfpl"),
+		"12" => array(
+				"name"		=> _T('licence:titre_cc0'), // nom de la licence
+				"id"		=> "12", // numero d'identifiacation de la licence
 				"icon"		=> "cc-zero-publicdomain.png",
 				"link"		=> _T('licence:lien_cc0'),
 				"description" => _T('licence:description_cc0'),
-				"abbr" 		=> "cc0"),
-			"0" 	=> array(
-				"name" 		=> _T('licence:titre_autre'),
-				"id"		=> "0",
+				"abbr"		=> "cc0"),
+		"0" => array(
+				"name"		=> _T('licence:titre_autre'), // nom de la licence
+				"id"		=> "0", // numero d'identifiacation de la licence
 				"icon"		=> "",
 				"link"		=> "",
 				"description" => _T('licence:description_autre'),
-				"abbr" 		=> "???"),
-);
+				"abbr"		=> "???"),
+), MES_LICENCES);
 
 ?>
