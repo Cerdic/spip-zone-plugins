b433d26c0ec3778fe116c19bc30d8594ef04ef13
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-01-14 21:14:22 +0000

    Evolution du plugin sur une idée originale de YannX et Maieul lors de la SPIPnoz 2013. Sur la page ?exec=lister_plugins, on génère un paquet.xml lié au site en cours. Vous pourriez utiliser cette trame pour batir un fichier paquet.xml, Utilisable comme pseudo-plugin pour reconfigurer votre site (cf. mes_fichiers). ON passe aussi les pages du plugin en onglet pour ne pas surcharger le menu configuration.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@80043 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- lister_config/trunk/lang/lister_config_fr.php
+++ lister_config/trunk/lang/lister_config_fr.php
@@ -8,6 +8,10 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// D
 	'description_page' => 'Vous pouvez trouver ci-dessous la liste des plugins actifs du site proposant une page de configuration du type <code>?exec=configurer_prefixe-plugin</code>.',
 	'description_lister_exec' => '<strong>Mise en garde !</strong> Faites attention à l\'utilisation de ces pages. <br/>On ne liste pas ici les pages de vue et d\'édition d\'un objet éditorial comme les pages d\'un article, de brèves, de mots clés, etc. <br /><em>Cette page est un proof of concept.</em>',
+	'description_lister_plugins' => 'G&eacute;n&eacute;ration du fichier d\'appel des plugins n&eacute;cessaires au site ',
+	'description_utiliser_plugins' => 'Vous pourriez utiliser cette trame pour batir un fichier <em>paquet.xml</em>,<br />
+	Utilisable comme pseudo-plugin pour reconfigurer votre site (cf. mes_fichiers) !<br />
+	(vous pouvez aussi passer vos squelettes dans ce plugin, qui pourra faciliter vos migrations).',
 
 	// I
 	'intertitre_exec' => 'Selon exec/*.php',
@@ -16,6 +20,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// T
 	'titre_lister_config' => 'Les pages de configuration',
 	'titre_lister_exec' => 'La liste des pages ?exec=xxx',
+	'titre_lister_plugins' => 'Les plugins nécessaires au site',
 	'titre_page' => 'Pages de configuration des plugins actifs du site'
 
 );
