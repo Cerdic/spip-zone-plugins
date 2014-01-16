111351e94184339fd046021003beb89b9b11a7e7
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-01-16 21:14:52 +0000

    paquet.xml minimum + logo + chaînes de langue
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@80086 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ ctan/trunk/lang/paquet-ctan_fr.php
@@ -0,0 +1,19 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-correction_liens_internes
+// Langue: fr
+// Date: 29-10-2013 17:24:58
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// C
+	'correction_liens_internes_description' => 'Modèles pour récuperer des données sur le CTAN au sujet',
+	'ctan_slogan' => 'Le monde SPIP rencontre encore plus le monde TeX!',
+);
+?>
