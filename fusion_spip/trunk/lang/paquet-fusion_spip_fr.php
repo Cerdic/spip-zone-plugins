ffc63e49057bbf8821018b4510055b597792a4a7
nicolas.dorigny@gmail.com <nicolas.dorigny@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-01-19 20:29:20 +0000

    oups, oublié les svn add et delete
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@80136 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ fusion_spip/trunk/lang/paquet-fusion_spip_fr.php
@@ -0,0 +1,12 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// F
+	'fusion_spip_description' => 'Importer tous les contenus d’un site source, objets, liaisons, documents en mettant à jour les identifiants des objets, les liens internes et les modèles.',
+	'fusion_spip_slogan' => 'Importer et fusionner tout le contenu d\'un autre site'
+);
+
+?>
