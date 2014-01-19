d028a27ddecbd8192c51344c8ed7b6e53b40498a
frodoo@riseup.net <frodoo@riseup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-01-19 04:38:03 +0000

    [Salvatore] [source:_plugins_/cameras/lang/ camera] Export depuis http://trad.spip.net de la langue grc
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@80118 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ cameras/lang/camera_grc.php
@@ -0,0 +1,98 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/camera?lang_cible=grc
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'bouton_deplacer_camera' => 'Μετακινείστε την κάμερα',
+	'bouton_pointer_sur_carte' => 'Σημαδέψετε στο χάρτη',
+	'bouton_saisie_manuelle' => 'Εισάγετε',
+
+	// C
+	'cameras_recents' => 'Τελευταίες δημοσιεύσεις καμερών',
+	'cfg_titre_menu_camera' => 'Κάμερες',
+
+	// I
+	'icone_creer_camera' => 'Δημιουργήστε μια κάμερα',
+	'icone_modifier_camera' => 'Επεξεργασία κάμερας',
+	'info_1_camera' => 'Μια κάμερα',
+	'info_aucun_camera' => 'Καμία κάμερα',
+	'info_cameras_auteur' => 'Οι κάμερες αυτού του χρήστη',
+	'info_cameras_valider' => '
+Κάμερες για επικύρωση',
+	'info_inconnu' => 'άγνωστο',
+
+	// L
+	'label_actualite_globale' => 'Διεθνή νέα',
+	'label_actualite_locale' => 'Τοπικά νέα',
+	'label_angle' => 'Γωνία σε σχέση με έδαφος',
+	'label_apparence' => 'Εμφάνιση κάμερας',
+	'label_apparence_dome' => 'Κάμερα καρούμπαλο',
+	'label_apparence_encastre' => 'Ενσωματωμένη',
+	'label_apparence_radar' => 'Ραντάρ',
+	'label_aucune_description' => 'Καμιά περιγραφή',
+	'label_choisir_apparence' => 'Διαλέξτε εμφάνιση',
+	'label_config_auth' => 'Απαγορεύεται η επεξεργασία αυτής της κάμερας',
+	'label_config_rubrique' => 'Προεπιλογή κατηγορίας',
+	'label_config_rubrique_auto' => 'Αυτόματη κατηγορία',
+	'label_config_titre' => 'Αυτόματος τίτλος',
+	'label_date' => 'Κάμερα αποθηκεύτηκε στις',
+	'label_description' => 'Περιγραφή',
+	'label_direction' => 'Κατεύθυνση',
+	'label_height' => 'Ύψος εγκατάστασης',
+	'label_lat' => 'Γεωγραφικό πλάτος',
+	'label_lon' => 'Γεωγραφικό μήκος',
+	'label_non' => 'Οχι',
+	'label_non_renseigne' => 'Δεν ύπαρχουν πληροφορίες',
+	'label_non_renseignee' => 'Δεν ύπαρχουν πληροφορίες',
+	'label_nord' => 'Από το βορρά',
+	'label_op_choisir_type' => 'Ιδιωτικό ή δημόσιο ?',
+	'label_op_name' => 'Όνομα κατασκευαστή',
+	'label_op_type' => 'Είδος υπολογιστή',
+	'label_op_type_prive' => 'Ιδιωτικό',
+	'label_op_type_public' => 'Δημόσιο',
+	'label_orientable' => 'Πιθανότητα για orienter ?', # RELIRE
+	'label_oui' => 'Ναι',
+	'label_sol' => 'Απότ ο έδαφος',
+	'label_surveillance' => 'Παρακολούθηση',
+	'label_surveillance_choisir_type' => 'Τύπος παρακολούθησης ?',
+	'label_surveillance_type_banque' => 'Τράπεζες',
+	'label_surveillance_type_batiment' => 'Κτίρια',
+	'label_surveillance_type_commerce' => 'Καταστήματα',
+	'label_surveillance_type_municipale' => 'Του δήμου',
+	'label_surveillance_type_parking' => 'Στάθμευση',
+	'label_surveillance_type_traffic' => 'Κυκλοφορίας',
+	'label_titre' => 'Τοποθεσία',
+	'label_type' => 'Τύπος κάμερας',
+	'label_zone' => 'Περιοχή',
+	'label_zoom' => 'Πιθανότητα για zoom ?',
+	'lien_ajouter_camera' => 'Προσθέστε αυτή τη κάμερα',
+	'lien_retirer_camera' => 'Αποσύρετε αυτή τη κάμερα',
+	'lien_retirer_cameras' => 'Αποσύρετε όλες τις κάμερες',
+
+	// M
+	'modifier' => 'Επεξεργασία',
+
+	// S
+	'slogan_site_cameras' => 'Συλλογική χαρτογράφηση και υλικό ενάντια στην βίντεο - παρακολούθηση',
+
+	// T
+	'texte_ajouter_camera' => 'Προσθέστε μια κάμερα',
+	'texte_changer_statut' => 'Αυτή η κάμερα είναι :',
+	'texte_creer_associer' => 'Δημιουργήστε και ενώστε μια κάμερα',
+	'texte_explication_email_secu' => 'Η ηληεκτρονική σας διεύθυνση φαίνεται μόνο στυς διαχειρηστές. Σε περίπτωση αμφιβολίας χρησιμοποιείστε τη κατάλληλη διεύθυνση',
+	'titre_camera' => 'κάμερα',
+	'titre_cameras' => 'κάμερες',
+	'titre_cameras_rubrique' => 'Κάμερες της κατηγορίας',
+	'titre_formulaire_infos' => 'Επιπρόσθετες πληροφορίες',
+	'titre_formulaire_position' => 'Συντεταγμένες τις κάμερας',
+	'titre_formulaire_technique' => 'Τεχνικές πληροφορίες',
+	'titre_langue_camera' => 'Γλώσσα της κάμερας',
+	'titre_logo_camera' => 'Εικονίδιο κάμερας'
+);
+
+?>
