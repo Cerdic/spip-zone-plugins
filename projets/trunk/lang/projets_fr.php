e358c4052dbbb3d31790247a541651a66938e414
cyril@cym.fr <cyril@cym.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-01-22 18:59:26 +0000

    on donne quelques explications dans les pages de l'espace pricé, à la fois sur le plugin lui même, mais aussi sur l'objet "cadre de projet";
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@80210 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- projets/trunk/lang/projets_fr.php
+++ projets/trunk/lang/projets_fr.php
@@ -5,8 +5,29 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
+    // C
+    'cadres_aide'   => "On peut caractériser un projet selon le cadre dans lequel il a été entrepris.
+
+Par exemple, tel projet pourrait être entrepris dans le cadre :
+- d'une prestation forfaitaire,
+- d'un devis,
+- en régie,  ou encore
+- dans un cadre purement personnel
+
+Les cadres de projet évitent l'utilisation des mots clé pour caractériser les projets.
+
+Vous pouvez créer autant de cadres de projet que nécessaire.
+",
+
 	// P
 	'projets_titre' => 'Projets',
+    'projets_aide'  => "Cette page liste tous les projets gérés dans ce site.
+
+Il est possible de filtrer les projets selon leur statut dans la boite ci-dessus, et de les trier selon les champs indiqués en couleur dans le tableau ci-contre.
+
+[Voir la doc en ligne->@lien@].",
+
+
 );
 
 ?>
