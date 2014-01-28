a3736bf06c10cb57c7385c54b2912a24249fa9c5
cyril@cym.fr <cyril@cym.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-01-28 18:25:51 +0000

    quelques améliorations pour la saisie des activités;
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@80300 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ dayfill/trunk/formulaires/ajouter_ligne_action.php
@@ -0,0 +1,18 @@
+<?php
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+
+function formulaires_ajouter_action_ligne_charger_dist() {
+	return;
+}
+
+function formulaires_ajouter_action_ligne_verifier_dist() {
+	return;
+}
+
+function formulaires_ajouter_action_ligne_traiter_dist() {
+	return;
+}
+
+?>
