780282320310dec3a6f76e735947b9dc400e3916
cyril@cym.fr <cyril@cym.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-01-28 18:26:37 +0000

    modifs cosmétiques;
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@80301 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- dayfill/branches/1.0/formulaires/ajouter_action.php
+++ dayfill/branches/1.0/formulaires/ajouter_action.php
@@ -8,119 +8,120 @@
  * - qui
  * - quoi
  * - quand
- * L'action est saisie, mais sans projet qui sera mis à la fin
+ * L'action est saisies, mais sans projet qui sera mis à la fin
  */
 
-function formulaires_ajouter_action_charger_dist(){
+function formulaires_ajouter_action_charger_dist()
+{
 	$valeurs = array(
-					'date_projet'=>'',
-					'id_projet'=>'',
-					'id_type_action'=>'',
-					'action_projet'=>'',
-					'type_action'=>'',
-					'id_user'=>'',
-					'numero_forfait'=>'',
-					'numero_facture'=>'',
-					'nb_heures_passees'=>'',
-					'heure_debut'=>'',
-					'heure_fin'=>'',
-					'nb_heures_passees_calculees'=>'',
-					'nb_heures_facturees'=>'',
-					'id_facture'=>''
-					);
-	
+		'date_projet'                 => '',
+		'id_projet'                   => '',
+		'id_type_action'              => '',
+		'action_projet'               => '',
+		'type_action'                 => '',
+		'id_user'                     => '',
+		'numero_forfait'              => '',
+		'numero_facture'              => '',
+		'nb_heures_passees'           => '',
+		'heure_debut'                 => '',
+		'heure_fin'                   => '',
+		'nb_heures_passees_calculees' => '',
+		'nb_heures_facturees'         => '',
+		'id_facture'                  => ''
+	);
+
 	return $valeurs;
 }
 
-function formulaires_ajouter_action_verifier_dist(){
+function formulaires_ajouter_action_verifier_dist()
+{
 	$erreurs = array();
-	
+
 	// verifier que les champs obligatoires sont bien la :
-	foreach(array('action_projet','nb_heures_facturees','nb_heures_passees','date_projet') as $obligatoire)
+	foreach (array('action_projet', 'nb_heures_facturees', 'nb_heures_passees', 'date_projet') as $obligatoire)
 		if (!_request($obligatoire)) $erreurs[$obligatoire] = 'Ce champ est obligatoire';
-		
+
 	if (count($erreurs))
-		$erreurs['message_erreur'] = 'Votre saisie contient des erreurs !';
-		
+		$erreurs['message_erreur'] = 'Votre saisies contient des erreurs !';
+
 	return $erreurs;
 }
 
-function formulaires_ajouter_action_traiter_dist(){
-	
-	
+function formulaires_ajouter_action_traiter_dist()
+{
+
+
 	//Faire le traitement d'insertion dans la base!!!!!
-	$id_projet			=	_request('id_projet');
-	
-	
+	$id_projet = _request('id_projet');
+
+
 	//Partie qui inverse la date pour l'inserer dans la base mysql dans le bonne ordre
-	$date_projet		=	_request('date_projet');	
-	$date_decoupe		=	explode("/",$date_projet);
-	$date_correct		=	$date_decoupe[2].'-'.$date_decoupe[1].'-'.$date_decoupe[0];
-	
+	$date_projet  = _request('date_projet');
+	$date_decoupe = explode("/", $date_projet);
+	$date_correct = $date_decoupe[2] . '-' . $date_decoupe[1] . '-' . $date_decoupe[0];
+
 	//$date_projet		=	"[(#DATE|affdate{'d/m/Y'})]";
-	//$date_projet		=	date("m/d/Y");	
-	
+	//$date_projet		=	date("m/d/Y");
+
 	//Calcul du temps
-	if( _request('nb_heures_passees') ){
+	if (_request('nb_heures_passees')) {
 		$resultat_final = _request('nb_heures_passees');
+	} else {
+		$debut = _request('heure_debut');
+		$fin   = _request('heure_fin');
+
+		$heuredebut = explode(":", $debut);
+		$heurefin   = explode(":", $fin);
+
+		$toalenminute_debut = ($heuredebut[0] * 60) + $heuredebut[1];
+		$toalenminute_fin   = ($heurefin[0] * 60) + $heurefin[1];
+
+		$difference = $toalenminute_fin - $toalenminute_debut;
+
+		$resultat_final = $difference / 60;
 	}
-	else {
-		$debut		=	_request('heure_debut');
-		$fin		=	_request('heure_fin');
-		
-		$heuredebut	=	explode(":",$debut);
-		$heurefin	=	explode(":",$fin);
-		
-		$toalenminute_debut	=	($heuredebut[0] * 60) + $heuredebut[1];
-		$toalenminute_fin	=	($heurefin[0] * 60) + $heurefin[1];
-		
-		$difference			=	$toalenminute_fin - $toalenminute_debut;
-		
-		$resultat_final		=	$difference / 60;
-	}
-	
+
 	$action_projet = addslashes(_request('action_projet'));
-	
+
 	// Requete maj projet
-	$sql_maj_projet = "UPDATE spip_projets SET date_maj = '".date('Y-m-d H:i:s')."' WHERE id_projet = "._request('id_projet');
-	$maj_projet =	spip_query($sql_maj_projet);
-	
+	$sql_maj_projet = "UPDATE spip_projets SET date_maj = '" . date('Y-m-d H:i:s') . "' WHERE id_projet = " . _request('id_projet');
+	$maj_projet     = spip_query($sql_maj_projet);
+
 	//Requete d'insertion
-	$requete_insertion	=	"
+	$requete_insertion = "
 							INSERT INTO `spip_actions` (
-								`id_projet`, 
-								`id_type_action`, 
-								`date_action`, 
-								`action`, 
-								`type_action`, 
-								`id_user`, 
-								`heure_debut`, 
-								`heure_fin`, 
-								`nb_heures_passees`, 
+								`id_projet`,
+								`id_type_action`,
+								`date_action`,
+								`action`,
+								`type_action`,
+								`id_user`,
+								`heure_debut`,
+								`heure_fin`,
+								`nb_heures_passees`,
 								`nb_heures_decomptees`,
 								`id_facture`
 							) VALUES (
-								'"._request('id_projet')."',
-								'"._request('id_type_action')."',
-								'".$date_correct."',
-								'".$action_projet."',
-								'"._request('type_action')."',
-								'"._request('id_user')."',
-								'"._request('heure_debut')."',
-								'"._request('heure_fin')."',
-								'".$resultat_final."',
-								'"._request('nb_heures_facturees')."',
-								'"._request('id_facture')."'
+								'" . _request('id_projet') . "',
+								'" . _request('id_type_action') . "',
+								'" . $date_correct . "',
+								'" . $action_projet . "',
+								'" . _request('type_action') . "',
+								'" . _request('id_user') . "',
+								'" . _request('heure_debut') . "',
+								'" . _request('heure_fin') . "',
+								'" . $resultat_final . "',
+								'" . _request('nb_heures_facturees') . "',
+								'" . _request('id_facture') . "'
 								)
 							";
 
-	$query				=	spip_query($requete_insertion);
-	
-	if($query = true) { 
-		return array('message_ok'=>'Action ajoutée correctement !'); 
-	} 
-	else { 
-		return array('message_ok'=>'Erreur lors de l\'ajout de l\'action !'); 
+	$query = spip_query($requete_insertion);
+
+	if ($query = true) {
+		return array('message_ok' => 'Action ajoutée correctement !');
+	} else {
+		return array('message_ok' => 'Erreur lors de l\'ajout de l\'action !');
 	}
 }
 
