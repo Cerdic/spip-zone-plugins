ae3bfa58dc3d5bb3f5f3dc0dbd112933359e7441
cyril@cym.fr <cyril@cym.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-01-28 18:52:33 +0000

    une icone et un changement de nom pour « Dayfill - Liens factures »;
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@80303 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- dayfill_factures/trunk/lang/paquet-dayfill_factures_fr.php
+++ dayfill_factures/trunk/lang/paquet-dayfill_factures_fr.php
@@ -6,9 +6,9 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// D
-	'dayfill_factures_nom'         => "DayFill - Factures &amp; Activités",
+	'dayfill_factures_nom'         => "DayFill - Liens Factures",
 	'dayfill_factures_slogan'      => "Lier des activités et des factures",
-	'dayfill_factures_description' => "Une fois le plugin Dayfill Factures activé, la vue d'une facture affiche les activités liées. De même la saisie d'une activité peut être complétée par le choix d'une facture liée.",
+	'dayfill_factures_description' => "Une fois le plugin Dayfill Factures activé, la saisie d'une activité peut être complétée par le choix d'une facture liée. De même la vue d'une facture affiche les activités liées.",
 
 );
 
