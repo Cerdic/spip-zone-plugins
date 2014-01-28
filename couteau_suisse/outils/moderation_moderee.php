31b7a002684cdcf2978535fafa49048f0a53ad15
patfr@ifrance.com <patfr@ifrance.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-01-28 19:08:36 +0000

    Modération modérée : attention à ne pas transfomer les messages privés en message publics ! (The_Moye)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@80304 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- couteau_suisse/outils/moderation_moderee.php
+++ couteau_suisse/outils/moderation_moderee.php
@@ -1,11 +1,12 @@
 <?php
-// Pour publier directement les messages d'auteurs autorises par moderation moderee
+// Pour publier directement les messages publics d'auteurs autorises par moderation moderee
 function moderation_vip($flux){
 	global $visiteur_session;
 	include_spip('inc/session');
 	if( $visiteur_session
 		&& $flux['args']['table']=='spip_forum'
 		&& $flux['args']['action']=='instituer'
+		&& $flux['data']['statut']!='prive'
 		&& defined('_MOD_MOD_'.$visiteur_session['statut'])
 	)
 		$flux['data']['statut']='publie';
