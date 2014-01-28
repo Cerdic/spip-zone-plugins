06424007dac41118c945339c8cc9c66aeb127a46
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-01-28 04:39:17 +0000

    [Salvatore] [source:_plugins_/depublie/lang/ depublie] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@80282 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- depublie/lang/depublie_fr.php
+++ depublie/lang/depublie_fr.php
@@ -1,33 +1,29 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/depublie/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
-
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-	
-	//C
-	'cfg_titre_parametrages'=>'Paramétrages des dépublications',
-	'configurer_rubrique_depublie' =>'Afficher le formulaire de dépublication requiert un identifiant de rubrique ou de secteur, plusieurs sont possibles avec des virgules',
-	'configurer_statut_depublie'=>'Choisissez le statut à donner à la dépublication',
-	'configurer_duree_publication' => 'Noter une durée de publication si le statut passe à publier, ne rien mettre sinon',
-	
 
-	//L
-	'label_jour'=>'Jours',
-	'label_mois'=>'Mois',
-	'label_publication_duree'=>'Durée',
-	'label_publication_periode'=>'Période',
-	'label_rubrique_depublie'=>'Identifiants de rubrique(s)',
-	'label_secteur_depublie'=>'Identifiants de secteur(s)',
+	// C
+	'cfg_titre_parametrages' => 'Paramétrages des dépublications',
+	'configurer_duree_publication' => 'Noter une durée de publication si le statut passe à publier, ne rien mettre sinon',
+	'configurer_rubrique_depublie' => 'Afficher le formulaire de dépublication requiert un identifiant de rubrique ou de secteur, plusieurs sont possibles avec des virgules',
+	'configurer_statut_depublie' => 'Choisissez le statut à donner à la dépublication',
+
+	// L
+	'label_jour' => 'Jours',
+	'label_mois' => 'Mois',
+	'label_publication_duree' => 'Durée',
+	'label_publication_periode' => 'Période',
+	'label_rubrique_depublie' => 'Identifiants de rubrique(s)',
+	'label_secteur_depublie' => 'Identifiants de secteur(s)',
 	'label_statut_depublie' => 'Statut',
 
-		
-	//T
-	'texte_date_depublication' => 'Date de dépublication en ligne :',
-	'texte_date_depublication_nonaffichee' =>'Ne pas utiliser la date de dépublication',
-	'titre_page_configurer_depublies'=>'Configurer les dépublications',	
-	
-	);
+	// T
+	'texte_date_depublication' => 'Date de dépublication en ligne :',
+	'texte_date_depublication_nonaffichee' => 'Ne pas utiliser la date de dépublication',
+	'titre_page_configurer_depublies' => 'Configurer les dépublications'
+);
 
 ?>
