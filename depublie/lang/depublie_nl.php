ffacd9f29c48b281cfbb04accf5edc6cf0b8b195
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-01-29 04:41:28 +0000

    [Salvatore] [source:_plugins_/depublie/lang/ depublie] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@80312 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ depublie/lang/depublie_nl.php
@@ -0,0 +1,31 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/depublie?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cfg_titre_parametrages' => 'Parameters voor depublicatie',
+	'configurer_duree_publication' => 'Geef een publicatieduur aan als de status in gepubliceerd verandert, laat anders leeg',
+	'configurer_rubrique_depublie' => 'Het weergeven van het depublicatie-formulier vereist de identificatie van een of meerdere hoofd- en subrubrieken, gescheiden door een komma',
+	'configurer_statut_depublie' => 'Kies de tot te wijzen statut bij depublicatie',
+
+	// L
+	'label_jour' => 'Dagen',
+	'label_mois' => 'Maanden',
+	'label_publication_duree' => 'Duur',
+	'label_publication_periode' => 'Periode',
+	'label_rubrique_depublie' => 'Identificatie van subrubriek(en)',
+	'label_secteur_depublie' => 'Identificatie van hoofdrubriek(en)',
+	'label_statut_depublie' => 'Status',
+
+	// T
+	'texte_date_depublication' => 'Datum van depublicatie:',
+	'texte_date_depublication_nonaffichee' => 'De datum van depublicatie niet gebruiken',
+	'titre_page_configurer_depublies' => 'Depublicaties configureren'
+);
+
+?>
