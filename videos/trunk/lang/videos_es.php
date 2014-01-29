d700d98086bee2b4022deb3561b1506b59c8846e
danielquisbert.info@gmail.com <danielquisbert.info@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-01-29 04:42:06 +0000

    [Salvatore] [source:_plugins_/videos/trunk/lang/ videos] Export depuis http://trad.spip.net de la langue es
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@80315 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ videos/trunk/lang/videos_es.php
@@ -0,0 +1,42 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/videos?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cfg_description_doc' => 'Documentación',
+	'cfg_description_titre' => 'Configuración del plugin Video(s)',
+	'cfg_explication_liste_definition' => 'Encapsular el vídeo en un dl/dt/dd en SPIP se puede por su gestión de documentos.', # RELIRE
+	'cfg_explication_titre_descriptif' => 'Mostrar el título y la descripción del video. Esta opción se puede reemplazar en la llamada a la modelo.',
+	'cfg_label_hauteur' => 'Altura predeterminada',
+	'cfg_label_largeur' => 'Ancho predeterminado',
+	'cfg_label_liste_definition' => 'Lista de definiciones HTML',
+	'cfg_label_responsive' => 'Tamaño mediano',
+	'cfg_label_titre_descriptif' => 'Título y descripción',
+	'cfg_label_wmode' => 'Wmode predeterminado',
+	'cfg_titre_configurations_communes' => 'Configuraciones communes',
+	'cfg_titre_configurations_dailymotion' => 'Configuraciones Dailymotion',
+	'cfg_titre_configurations_html5' => 'Configuración HTML5 / Alternativo',
+	'cfg_titre_configurations_vimeo' => 'Configuraciones Vimeo',
+	'cfg_titre_configurations_youtube' => 'Configuraciones Youtube',
+
+	// E
+	'explication_ajouter_video' => '(video remoto YouTube, Vimeo, DailyMotion)',
+
+	// L
+	'label_ajouter_autre_video' => 'Añadir otro video',
+	'label_ajouter_video' => 'Añadir un video',
+	'label_url' => 'Dirección del video',
+
+	// T
+	'titre_admin' => 'Plugin Video(s)',
+
+	// V
+	'valider' => 'Añadir video'
+);
+
+?>
