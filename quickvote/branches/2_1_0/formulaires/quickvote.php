ee5d61726f90f675d6f45642ba9c18a245980f9d
gildas.cotomale@gmail.com <gildas.cotomale@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-01-30 21:31:34 +0000

    on passe par un squelette (à améliorer) pour faciliter la personnalisation
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@80338 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- quickvote/branches/2_1_0/formulaires/quickvote.php
+++ quickvote/branches/2_1_0/formulaires/quickvote.php
@@ -39,60 +39,61 @@ function quickvote_resultat($id_quickvote) {
      $str_resultat .= "<table>";
      foreach ($vote as $k=>$val) {
                    if ($nb_vote)
-                          $resultat_pt = round(($val/$nb_vote)*100);
-                        else
-                          $resultat_pt = 0;
-                   $reponse_intitule = typo($k);
-                   $str_resultat .= "<tr><td>$reponse_intitule</td><td>$resultat_pt %</td></tr>";
+                         $resultat_pt = round(($val/$nb_vote)*100);
+                    else
+                         $resultat_pt = 0;
+                    $reponse_intitule = typo($k);
+                    $str_resultat .= "<tr><td>$reponse_intitule</td><td>$resultat_pt %</td></tr>";
      }
      $str_resultat .= "</table>";
 
-     if ($nb_vote ==0)
-                    $str_resultat .= "<div class='nb_vote'>"._T("quickvote:resultat_0_vote")."</div>";
-     else if ($nb_vote ==1)
-                    $str_resultat .= "<div class='nb_vote'>"._T("quickvote:resultat_nb_vote")."</div>";
-          else
-                    $str_resultat .= "<div class='nb_vote'>"._T("quickvote:resultat_nb_votes",array("nb"=>$nb_vote))."</div>";
+     if ($nb_vote==0)
+          $str_resultat .= "<div class='nb_vote'>"._T("quickvote:resultat_0_vote")."</div>";
+     else if ($nb_vote==1)
+          $str_resultat .= "<div class='nb_vote'>"._T("quickvote:resultat_nb_vote")."</div>";
+     else
+          $str_resultat .= "<div class='nb_vote'>"._T("quickvote:resultat_nb_votes",array("nb"=>$nb_vote))."</div>";
 
      return $str_resultat;
-
-
 }
 
 
 //
 //  CVT pour traiter le vote
 //
+
+
 /**
  * Charger
  *
- *
  */
-
 function formulaires_quickvote_charger_dist($id_quickvote,$skip_vote='non',$masquer_question='non'){
-    $valeurs = "";
-    $id_quickvote = intval($id_quickvote);
-    $valeurs['id'] = $id_quickvote;
-    $ip	= $GLOBALS['ip'];
+     $valeurs = "";
+     $id_quickvote = intval($id_quickvote);
+     $valeurs['id'] = $id_quickvote;
+     $ip = $GLOBALS['ip'];
 
-    // masque question ?
-    if ($masquer_question=='oui') {
-           $valeurs['masquer_question'] = 'oui';
-    }
+     // masque question ?
+     if ($masquer_question=='oui') {
+          $valeurs['masquer_question'] = 'oui';
+     }
 
-    // est ce que le sondage est cloturé ?
-    if (($row = sql_fetsel("actif", "spip_quickvotes", "id_quickvote = $id_quickvote AND actif = 0")) || ($skip_vote=='oui')){
-  	        $valeurs['editable'] = false;
-            $valeurs['message_ok'] = quickvote_resultat($id_quickvote);
-    }
-    // deja vote
-    else if ($row = sql_fetsel("ip", "spip_quickvotes_votes", "id_quickvote = $id_quickvote AND ip='$ip'")){
-  	      $valeurs['editable'] = false;
-          $valeurs['message_ok'] = quickvote_resultat($id_quickvote);
-    }
+     // est ce que le sondage est cloturé ?
+     if (($row = sql_fetsel("actif", "spip_quickvotes", "id_quickvote = $id_quickvote AND actif = 0")) || ($skip_vote=='oui')) {
+          $valeurs['editable'] = false;
+//          $valeurs['message_ok'] = quickvote_resultat($id_quickvote);
+          $valeurs['message_ok'] = recuperer_fond('modeles/quickvote', array('id'=>$id_quickvote) );
+     }
+     // deja vote
+     else if ($row = sql_fetsel("ip", "spip_quickvotes_votes", "id_quickvote = $id_quickvote AND ip='$ip'")){
+          $valeurs['editable'] = false;
+//          $valeurs['message_ok'] = quickvote_resultat($id_quickvote);
+          $valeurs['message_ok'] = recuperer_fond('modeles/quickvote', array('id'=>$id_quickvote) );
+     }
 
-    $valeurs['time_invalidateur']=time().'-'.rand();  // on passe une valeur pour invalider systematiquement le cache
-    return $valeurs;
+     $valeurs['time_invalidateur'] = time().'-'.rand();// on passe une valeur pour invalider systematiquement le cache
+
+     return $valeurs;
 }
 
 
@@ -100,13 +101,12 @@ function formulaires_quickvote_charger_dist($id_quickvote,$skip_vote='non',$masq
  * Verifer
  *
  */
-
 function formulaires_quickvote_verifier_dist($id_quickvote){
-    	$erreurs = array();
-      if (!_request('quickvote'))
-         $erreurs['message_erreur'] = _T("quickvote:erreur_pasvote");
+     $erreurs = array();
+     if (!_request('quickvote'))
+          $erreurs['message_erreur'] = _T("quickvote:erreur_pasvote");
 
-      return $erreurs;
+     return $erreurs;
 }
 
 
@@ -114,34 +114,29 @@ function formulaires_quickvote_verifier_dist($id_quickvote){
  * Traiter
  *
  */
-
 function formulaires_quickvote_traiter_dist($id_quickvote){
-	   // Effectuer des traitements
-
-     	// invalider les caches  (utile ?)
-	   include_spip('inc/invalideur');
-	   suivre_invalideur("id='id_quickvote/$id_quickvote'");
+     // invalider les caches  (utile ?)
+     include_spip('inc/invalideur');
+     suivre_invalideur("id='id_quickvote/$id_quickvote'");
 
      // SQL
      $ip = $GLOBALS['ip'];
      // sécurité le formulaire peut etre chargé à plusieurs endroits à la fois: on n'enregistre que le 1er vote de cette IP, les autres votes sont ignorés
      if (!$row = sql_fetsel("ip", "spip_quickvotes_votes", "id_quickvote = $id_quickvote AND ip='$ip'"))  {
-           $requete_sql = array();
-           $requete_sql['id_quickvote']  = $id_quickvote;
-           $requete_sql['reponse'] = _request('quickvote');
-           $requete_sql['ip']	= $ip;
-           $n = sql_insertq('spip_quickvotes_votes', $requete_sql);
-    }
+          $requete_sql = array();
+          $requete_sql['id_quickvote']  = $id_quickvote;
+          $requete_sql['reponse'] = _request('quickvote');
+          $requete_sql['ip'] = $ip;
+          $n = sql_insertq('spip_quickvotes_votes', $requete_sql);
+     }
 
-    // Valeurs de retours
-    return array(
-       'message_ok' => quickvote_resultat($id_quickvote),
-       'editable' => false
-    );
+     // Valeurs de retours
+     return array(
+//          'message_ok' => quickvote_resultat($id_quickvote),
+          'message_ok' => recuperer_fond('modeles/quickvote', array('id'=>$id_quickvote) ),
+          'editable' => false
+     );
 }
-
-
-
 
 
 ?>
