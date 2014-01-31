89254c7b7ba362ee3ec56539ac298713afa2e424
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-01-31 04:38:09 +0000

    [Salvatore] [source:_plugins_/boussole_spip/trunk/lang/ boussole-spip] Export depuis http://trad.spip.net de la langue en
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@80339 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- boussole_spip/trunk/lang/boussole-spip_en.php
+++ boussole_spip/trunk/lang/boussole-spip_en.php
@@ -8,14 +8,14 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// D
-	'descriptif_boussole_spip' => 'SPIP compass gathers "official" websites of the SPIP galaxy. For each website, logo, name, slogan and description are defined. Do not hesitate to use this compass on your own website for guiding visitors into the SPIP Galaxy.',
+	'descriptif_boussole_spip' => 'SPIP compass gathers "official" websites of the SPIP galaxy. For each website, logo, name, slogan and description are defined. Do not hesitate to use this compass on your own website for guiding visitors into the SPIP galaxy.',
 	'descriptif_site_spip_blog' => 'SPIP is a collaborative project, so SPIP-BLOG.net  gathers technical notes and announcements as well as self-mockeries, trolls and humorous articles. In this regard, it reflects the SPIP community spirit: tenderness first!.',
 	'descriptif_site_spip_contrib' => 'SPIP-CONTRIB.net is a collaborative website where external contributions are published: plugins, scripts, functions, templates, documentations, tip and tricks... are given to the community (e.g. download links) by SPIP users. Forums preserve relationship between users and developers.',
 	'descriptif_site_spip_core' => 'CORE.SPIP.org gathers modification history of SPIP source code, issues tracking to manage bugs and improvements and a SPIP releases download area.',
-	'descriptif_site_spip_demo' => 'DEMO.SPIP.net is a test website reset every night. In one click and without having to install it, you can test, as administrator or editor, the SPIP latest stable release.', # MODIF
-	'descriptif_site_spip_doc' => 'DOC.SPIP.net is where every fonction from the SPIP source code is detailled, analysed and comment (name, place, parameters, meaning and use).', # MODIF
-	'descriptif_site_spip_edgard' => 'Edgard is the faithfull and tireless companion of SPIP IRC where he shares his knowledge, with tenderness and humor. From his home, EDGARD.SPIP.org brings advices on IRC, answers and happiness. Edgard is definitely a robot (but he doesn’t know about it...)', # MODIF
-	'descriptif_site_spip_forum' => 'FORUM.SPIP.net is the place to exchange between SPIP users and get help. This site, translated in 10 languages, has four main sections: installation and update, private area usage, website administration, management and configuration, templates creation.', # MODIF
+	'descriptif_site_spip_demo' => 'DEMO.SPIP.net is a test website reset every night. In one click and without having to install it, you can test, as administrator or editor, the SPIP latest stable release.',
+	'descriptif_site_spip_doc' => 'CODE.SPIP.net is a place where every API, fonction, constant, etc from the SPIP source code is detailed, analysed and comment (name, parameters, description and usage).',
+	'descriptif_site_spip_edgard' => 'Edgard is the faithfull and tireless companion of SPIP IRC where he shares his knowledge, with tenderness and humor. From his home, EDGARD.SPIP.net, he brings advices on IRC, answers and happiness. Edgard is definitely a robot, but he doesn’t know about it...',
+	'descriptif_site_spip_forum' => 'FORUM.SPIP.net is the place to exchange between SPIP users and get help. This site, translated in 10 languages, is organised in four main sections: installation and update, private area usage, website administration, management and configuration, templates creation.',
 	'descriptif_site_spip_herbier' => 'SPIP herbarium is not just a link directory. No. It’s a memory album of websites made with SPIP, at one moment of their existence. It tells a web story , the one of SPIP, since 2001. This herbarium gathers websites we wanted to show, because their content was interresting and their design were too, fonctions skilfully implemented, or just nice designs. Websites we remember, where we came back, which were some kind of references to their time in one way or another.',
 	'descriptif_site_spip_info' => 'In a few pages, SPIP-INFO.net answers to whom is wondering about SPIP ability to fulfil his specific website objectives. Individuals, NGOs and companies will find useful information to make their own choice.',
 	'descriptif_site_spip_irc' => 'SPIP community never sleeps! A SPIP IRC channel opened 24/7 to everyone: http://webchat.freenode.net/?channels=#spip',
@@ -23,18 +23,18 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'descriptif_site_spip_net' => 'SPIP.net is the official website for users and webmasters willing to install a SPIP website, to understand SPIP language (loops, markers and filters), to write and use SPIP templates. Glossary, tutorials, advices and a downloading area of SPIP old versions are available. SPIP.net is translated in more than 20 languages.',
 	'descriptif_site_spip_party' => 'To spend some time on coding SPIP: it’s nice. To meet users and developers in real life, with cakes and fruit juice  (or any other beverage...): it’s better! SPIP-PARTY.net provides information about past meetings (minutes, photos) and coming events (date, agenda).',
 	'descriptif_site_spip_plugin' => 'PLUGINS.SPIP.net is a complete directory of add-ons SPIP (plugins, skeletons, themes). For each add-on are indicated: description, author, licence, compatibility per SPIP version, last updates, status of translations, statistics, documentation and download links.',
-	'descriptif_site_spip_programmer' => 'PROGRAMMER.SPIP.net is for developpers or webmasters who have knowledge in PHP, SQL, HTML, CSS and JavaScript. PROGRAMMER.SPIP.org shows most of the functions (APIs, overloads, pipelines,...) of SPIP with numerous coding exemples. You can download the entire website in pdf format under free licence cc-by-sa. PROGRAMMER.SPIP.org is available in French, English and Spanish.', # MODIF
-	'descriptif_site_spip_sedna' => 'SEDNA.SPIP.net is the RSS feeder on news about the SPIP Galaxy. Last informations from more than 70 websites about SPIP can be consulted.', # RELIRE
+	'descriptif_site_spip_programmer' => 'PROGRAMMER.SPIP.net is for developpers or webmasters skilled in PHP, SQL, HTML, CSS and JavaScript. PROGRAMMER.SPIP.net shows most of SPIP technical features (API, overloading, pipeline,...) with numerous coding examples. You can download the entire website in pdf format under free licence cc-by-sa. PROGRAMMER.SPIP.net is translated in French, English and Spanish.',
+	'descriptif_site_spip_sedna' => 'SEDNA.SPIP.net is the RSS feeder on news about the SPIP galaxy. Latest news from more than 70 websites can be read.',
 	'descriptif_site_spip_test' => 'On GRML.EU, install easily your own SPIP website (last development version). With mutualisationability of SPIP: only one installation of SPIP "core" for numerous websites, you are "home", on "your" website, with "your" configuration. Very convenient for SPIP trainings.',
 	'descriptif_site_spip_trad' => 'The translator space welcome those who want to help the SPIP user community by participating to translation work of SPIP itself and its miscellaneous contributions',
 	'descriptif_site_spip_twit' => 'SPIP.ORG allows to users to publish short messages on the web (140 character maximum) to public or friends. he publishing can be done on the website, RSS feed or instant messaging programmes.',
 	'descriptif_site_spip_user' => 'SPIP@REZO.net is a helping list of SPIP users. The archives of this list can be consulted on http://archives.rezo.net/archives/spip.mbox/ or, even on the blog http://blog.gmane.org/gmane.comp.web.spip.user.',
-	'descriptif_site_spip_video' => 'Video website description', # MODIF
+	'descriptif_site_spip_video' => 'MEDIAS.SPIP.net is a website to share videos about SPIP. Every SPIP user could contribute by posting video about SPIP tutorials, seminars, training sessions... Shared videos must be licence free in order to allow everybody to use it freely. ',
 	'descriptif_site_spip_zine' => 'SPIPZine website description',
 	'descriptif_site_spip_zone' => 'Development area ruled by a chart, ZONE.SPIP.org give access to all contributions versioning (plugins, skeletons, themes, models, tools,...).',
 
 	// N
-	'nom_boussole_spip' => 'SPIP Galaxy', # MODIF
+	'nom_boussole_spip' => 'SPIP Compass',
 	'nom_groupe_spip_actualite' => 'News',
 	'nom_groupe_spip_aide' => 'Support',
 	'nom_groupe_spip_decouverte' => 'Discovery',
