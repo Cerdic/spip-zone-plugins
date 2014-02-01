5e5816f5c7c1efc0e16de5d15ff4a1a79c582199
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-02-01 04:39:10 +0000

    [Salvatore] [source:_plugins_/table_matieres/trunk/lang/ paquet-tablematieres] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@80374 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ table_matieres/trunk/lang/paquet-tablematieres_nl.php
@@ -0,0 +1,17 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-tablematieres?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// T
+	'tablematieres_description' => 'Koppelt een anker aan iedere tussentitel van het veld #TEXTE van een artikel,
+	alsmede een tag #TABLE_MATIERES (facultatief) die binnen een ARTICLES-lus kan worden opgenomen.',
+	'tablematieres_nom' => 'Inhoudsopgave',
+	'tablematieres_slogan' => 'Een overzicht van een artikel maken'
+);
+
+?>
