6ee7820a4f12f1201458f6356ecaf093c3d7344f
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-02-01 11:35:48 +0000

    eviter un conflit avec les fichiers de langues du plugin notifications
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@80380 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- notifications_archive/trunk/lang/notifications_archive_fr.php
+++ notifications_archive/trunk/lang/notifications_archive_fr.php
@@ -2,25 +2,59 @@
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
- 
+
+
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
+
+	// A
+	'ajouter_lien_notification' => 'Ajouter cette notifications',
+	
 	//E
 	'explication_duree'=>'Pour ne jamais les éliminer, laissez ce champs vide.',
 
 	//F
 	'fieldset_notification'=>'Paramêtres pour la notification "@notification@"',
+
+	// I
+	'icone_creer_notification' => 'Créer une notification',
+	'icone_modifier_notification' => 'Modifier cette notification',
+	'info_1_notification' => 'Une notification',
+	'info_aucun_notification' => 'Aucune notifications',
+	'info_nb_notifications' => '@nb@ notifications',
+	'info_notifications_auteur' => 'Les notifications de cet auteur',
+
+	// L
+	'label_activer'=>'Activer l\'archivage pour la notification @notification@',
+    'label_duree'=>' Nombre de jours après lesquels ces notifications seront éliminées.',		
+	'label_envoi' => 'Envoi :',
+	'label_id_notification' => 'Id Objet',
+	'label_lie_a' => 'Lié à :',
+	'label_notification' => 'Objet',
+	'label_recipients' => 'Récipients',
+	'label_sujet' => 'Sujet',
+	'label_texte' => 'Texte',
+	'label_type' => 'Type',
 	
-    //L
-    
-    'label_activer'=>'Activer l\'archivage pour la notification @notification@',
-    'label_duree'=>' Nombre de jours après lesquels ces notifications seront éliminées.',
-				
 	// N
-	'notifications_archive_titre' => 'Archive notifications',
-		
+	'notifications_archive_titre' => 'Archive notifications',	
+
+	// R
+	'retirer_lien_notification' => 'Retirer cette notification',
+	'retirer_tous_liens_notifications' => 'Retirer toutes les notification',
+
 	// T
-	'titre_page_configurer_notifications_archive' => 'Paramêtres Plugin Archive Notifications',
+	'texte_ajouter_notification' => 'Ajouter une notification',
+	'texte_changer_statut_notification' => 'Cette notification est :',
+	'texte_creer_associer_notification' => 'Créer et associer une notification',
+	'titre_langue_notification' => 'Langue de cette notification',
+	'titre_logo_notification' => 'Logo de cette notification',
+	'titre_notification' => 'Notification',
+	'titre_notifications' => 'Notifications',
+	'titre_notifications_rubrique' => 'Notifications de la rubrique',
+	'titre_page_configurer_notifications_archive' => 'Paramêtres Plugin Archive Notifications',		
+
+
 );
 
 ?>
