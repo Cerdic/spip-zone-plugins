ac323115c4e8645cc1cc44c0f5e70fc741cfe82f
fil@rezo.net <fil@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-02-03 11:12:53 +0000

    ce M majuscule me rendait FOU
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@80423 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- orthotypo/trunk/lang/orthotypo_fr.php
+++ orthotypo/trunk/lang/orthotypo_fr.php
@@ -20,7 +20,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'explication_exposants' => 'Am&eacute;liorer le rendu typographique d\'abr&eacute;viations fr&eacute;quentes par la mise en exposant de leurs &eacute;l&eacute;ments et/ou leur correction. Les abr&eacute;viations obtenues sont conformes &agrave; celles de l\'Imprimerie nationale telles qu\'indiqu&eacute;es dans le {Lexique des r&egrave;gles typographiques en usage &agrave; l\'Imprimerie nationale} (article &laquo;&nbsp;Abr&eacute;viations&nbsp;&raquo;, presses de l\'Imprimerie nationale, Paris, 2002).',
 
 	'label_mois_1' => 'Eviter de couper les dates en fin de ligne',
-	'explication_mois' => 'Corriger les dates au format "10 Mars" en remplaçant l\'espace entre le jour et le mois par un espace ins&#233;cable.',
+	'explication_mois' => 'Corriger les dates au format "10 mars" en remplaçant l\'espace entre le jour et le mois par un espace ins&#233;cable.',
 
 	'label_caps_1' => 'Styler les mots en lettres capitales',
 	'explication_caps' => 'Encadrer les mots écrits en lettres capitales dans une balise <code>&lt;span class="caps"></code>. Par exemple, <code>ONU</code> devient <code>&lt;span class="caps">ONU&lt;/span></code>. De ce fait, les mots en lettres capitales peuvent être stylés en css',
