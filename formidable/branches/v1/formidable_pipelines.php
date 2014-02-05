628488911b9637a559a462ee5ea9f9a5e2cf77b4
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-02-05 09:11:52 +0000

    Attention : on branche le trunk en v1 qui reste la version stable. Le trunk va subir du nettoyage/refactoiring et passe en v2-dev
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@80488 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ formidable/branches/v1/formidable_pipelines.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * Utilisation de pipelines
+ * 
+ * @package SPIP\Formidable\Pipelines
+**/
+
+// Sécurité
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+
+/**
+ * Optimiser la base de donnée en enlevant les liens de formulaires supprimés
+ * 
+ * @pipeline optimiser_base_disparus
+ * @param array $flux
+ *     Données du pipeline
+ * @return array
+ *     Données du pipeline
+ */
+function formidable_optimiser_base_disparus($flux){
+	// Les réponses qui sont à la poubelle
+	$res = sql_select(
+		'id_formulaires_reponse AS id',
+		'spip_formulaires_reponses',
+		'statut = '.sql_quote('poubelle')
+	);
+	
+	// On génère la suppression
+	$flux['data'] += optimiser_sansref('spip_formulaires_reponses', 'id_formulaires_reponse', $res);
+	
+		//
+	// CNIL -- Informatique et libertes
+	//
+	// masquer le numero IP des vieilles réponses
+	//
+	## date de reference = 4 mois
+	## definir a 0 pour desactiver
+	## même constante que pour les forums
+	if (!defined('_CNIL_PERIODE')) {
+		define('_CNIL_PERIODE', 3600*24*31*4);
+	}
+	
+	if (_CNIL_PERIODE) {
+		$critere_cnil = 'date<"'.date('Y-m-d', time()-_CNIL_PERIODE).'"'
+			. ' AND statut != "spam"'
+			. ' AND (ip LIKE "%.%" OR ip LIKE "%:%")'; # ipv4 ou ipv6
+		$c = sql_countsel('spip_formulaires_reponses', $critere_cnil);
+		if ($c>0) {
+			spip_log("CNIL: masquer IP de $c réponses anciennes à formidable");
+			sql_update('spip_formulaires_reponses', array('ip' => 'MD5(ip)'), $critere_cnil);
+		}
+	}
+	
+	return $flux;
+}
+
+?>
