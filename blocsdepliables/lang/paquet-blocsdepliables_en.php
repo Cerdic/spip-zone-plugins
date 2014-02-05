b4ef8f2a72487250267984b11e74cf6f15379bbb
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-02-05 04:40:34 +0000

    [Salvatore] [source:_plugins_/blocsdepliables/lang/ paquet-blocsdepliables] Export depuis http://trad.spip.net de la langue en
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@80480 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ blocsdepliables/lang/paquet-blocsdepliables_en.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-blocsdepliables?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'blocsdepliables_description' => 'Create blocks that can be opened or closed by clicking on their titles.',
+	'blocsdepliables_nom' => 'Folding Blocks',
+	'blocsdepliables_slogan' => 'Folding Blocks.'
+);
+
+?>
