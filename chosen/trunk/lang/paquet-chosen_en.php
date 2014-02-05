bf88beff3fd9fa3374de0a72c85822dd83382c00
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-02-05 04:40:42 +0000

    [Salvatore] [source:_plugins_/chosen/trunk/lang/ paquet-chosen] Export depuis http://trad.spip.net de la langue en
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@80482 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- chosen/trunk/lang/paquet-chosen_en.php
+++ chosen/trunk/lang/paquet-chosen_en.php
@@ -10,7 +10,11 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// C
 	'chosen_description' => '[Chosen->http://harvesthq.github.com/chosen/] is a JavaScript library that enhances the user experience of selectors in HTML forms.
 
-The CSS class <code>chosen</code> on a <code><select></code> automatically load Chosen above.', # MODIF
+The CSS class <code>chosen</code> on a <code><select></code> will automatically load Chosen above. This leaf of the plugin is based on the fork of koenpunt - version 1.0.0 - see https://github.com/koenpunt/chosen/releases/.
+
+Using this fork, Chosen allows to <a href=\'https://github.com/harvesthq/chosen/pull/166\'>create ne< options</a> in an existing &lt;select&gt; tag, under the condition that it has a class of ’chosen-create-option’.
+When Chosen creates a new  &lt;option&gt; (for example word ’dummy’) in a &lt;select&gt;, it takes the following format: <code>&lt;option selected=\'selected\' value=\'chosen_dummy\'&gt;dummy&lt;/option&gt;</code>.
+Take notice of the prefix ’chosen_’ that is added in the ’value’ parameter in order to distinguish the &lt;option&gt; values created by Chosen.',
 	'chosen_nom' => 'Chosen (koenpunt’s fork)',
 	'chosen_slogan' => 'Integrate the Chosen library in SPIP (koenpunt’s fork)'
 );
