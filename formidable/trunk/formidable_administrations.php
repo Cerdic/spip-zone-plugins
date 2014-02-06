af88321deae7a2a3b39a473d383c18b22638362a
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-02-06 12:08:10 +0000

    a l'installation de formidable, import automatique de tous les formulaires et sondages de forms&tables (mais il manque la prise en compte du statut, qui serait mieux, et surtout l'import des données, prochain step)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@80535 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- formidable/trunk/formidable_administrations.php
+++ formidable/trunk/formidable_administrations.php
@@ -30,7 +30,9 @@ function formidable_upgrade($nom_meta_base_version, $version_cible){
 			'spip_formulaires_reponses',
 			'spip_formulaires_reponses_champs',
 			'spip_formulaires_liens')),
+		array('formidable_importer_forms'),
 	);
+
 	// Ajout du choix de ce qu'on affiche à la fin des traitements
 	$maj['0.4.0'] = array(array('maj_tables',array('spip_formulaires')));
 	// Ajout d'une URL de redirection
@@ -70,4 +72,46 @@ function formidable_vider_tables($nom_meta_base_version){
 	effacer_meta($nom_meta_base_version);
 }
 
+/**
+ * Importer les formulaires de f&t
+ */
+function formidable_importer_forms(){
+	$trouver_table = charger_fonction("trouver_table","base");
+	if ($trouver_table('spip_forms')){
+		sql_alter("TABLE spip_forms ADD id_formulaire bigint(21) NOT NULL DEFAULT 0");
+
+		include_spip("echanger/formulaire/forms");
+
+		$forms = sql_allfetsel("*","spip_forms",'id_formulaire=0 AND type_form='.sql_quote('')." OR type_form=".sql_quote('sondage'),'','id_form');
+		foreach($forms as $form){
+			$formulaire = array();
+			// configurer le formulaire (titre etc)
+			forms_configure_formulaire($form,$formulaire);
+
+			$fields = sql_allfetsel("*","spip_forms_champs","id_form=".intval($form['id_form']),"","rang");
+			foreach($fields as $field){
+				$choix = sql_allfetsel("*","spip_forms_champs_choix","id_form=".intval($form['id_form'])." AND champ=".sql_quote($field['champ']),'','rang');
+				if (count($choix))
+					$field['choix'] = $choix;
+
+				if ($saisie = forms_champ_vers_saisie($field))
+					$formulaire['saisies'][] = $saisie;
+			}
+
+			// les traitements
+			forms_configure_traitement_formulaire($form,$formulaire);
+			$id_formulaire = forms_importe_en_base($formulaire);
+			spip_log("Import spip_forms #".$form['id_form']." en spip_formulaires #$id_formulaire","maj"._LOG_INFO_IMPORTANTE);
+
+			sql_update('spip_forms',array('id_formulaire'=>$id_formulaire),'id_form='.intval($form['id_form']));
+
+			if (time()>_TIME_OUT)
+				return;
+		}
+
+	}
+
+	include_spip("inc/drapeau_edition");
+	debloquer_tous($GLOBALS['visiteur_session']['id_auteur']);
+}
 ?>
