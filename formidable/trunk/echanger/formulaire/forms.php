3da504750eb0bcd28421eba1b1c1fc8f14fef2ab
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-02-06 11:01:25 +0000

    refactoring est completion de l'import depuis xml forms&tables (il manque encore les fieldset et les file)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@80531 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- formidable/trunk/echanger/formulaire/forms.php
+++ formidable/trunk/echanger/formulaire/forms.php
@@ -13,158 +13,62 @@ function echanger_formulaire_forms_importer_dist($fichier){
 		foreach($arbre['forms'] as $forms){
 			foreach ($forms['form'] as $form){
 				$formulaire = array();
-				
-				// Le titre
-				$titre = trim(spip_xml_aplatit($form['titre']));
-				$formulaire['titre'] = $titre ? $titre : _T('info_sans_titre');
-				
-				// L'identifiant il faut le générer
-				$formulaire['identifiant'] = 'form_'.time();
-				
-				// Le descriptif
-				$descriptif = trim(spip_xml_aplatit($form['descriptif']));
-				$formulaire['descriptif'] = $descriptif ? $descriptif : '';
-				
-				// Le message de retour si ok
-				$message_retour = trim(spip_xml_aplatit($form['texte']));
-				$formulaire['message_retour'] = $message_retour ? $message_retour : '';
 
-				// Les champs
-				$formulaire['saisies'] = array();
+				$form_source = array(
+					'id_form' => intval(trim(spip_xml_aplatit($form['id_form']))),
+					'titre' => trim(spip_xml_aplatit($form['titre'])),
+					'descriptif' => trim(spip_xml_aplatit($form['descriptif'])),
+					'texte' => trim(spip_xml_aplatit($form['texte'])),
+					'email' => unserialize(trim(spip_xml_aplatit($form['email']))),
+					'moderation' => trim(spip_xml_aplatit($form['moderation'])),
+					'modifiable' => trim(spip_xml_aplatit($form['modifiable'])),
+					'multiple' => trim(spip_xml_aplatit($form['multiple'])),
+					'champconfirm' => trim(spip_xml_aplatit($form['champconfirm'])),
+				);
+
+				// configurer le formulaire (titre etc)
+				forms_configure_formulaire($form_source,$formulaire);
+
+				// ajouter les champs de saisies
 				foreach($form['fields'] as $fields){
 					foreach($fields['field'] as $field){
-						// Le truc par défaut
-						$saisie = array(
-							'saisie' => 'input',
-							'options' => array('size'=>40)
+						$champ = array(
+							'champ'=>trim(spip_xml_aplatit($field['champ'])),
+							'titre'=>trim(spip_xml_aplatit($field['titre'])),
+							'type'=>trim(spip_xml_aplatit($field['type'])),
+							'obligatoire'=>trim(spip_xml_aplatit($field['obligatoire'])),
+							'taille'=>trim(spip_xml_aplatit($field['taille'])),
+							'extra_info'=>trim(spip_xml_aplatit($field['extra_info'])),
+							'aide'=>trim(spip_xml_aplatit($field['aide'])),
+							'saisie'=>trim(spip_xml_aplatit($field['saisie'])),
 						);
-						
-						// On essaye de traduire tous les types de champs
-						$type = trim(spip_xml_aplatit($field['type']));
-						switch ($type){
-							case 'texte':
-								$saisie['saisie'] = 'textarea';
-								unset($saisie['options']['size']);
-								$saisie['options']['rows'] = 5;
-								$saisie['options']['cols'] = 40;
-								break;
-							case 'password':
-								$saisie['options']['type'] = 'password';
-								break;
-							case 'date':
-								$saisie['saisie'] = 'date';
-								$saisie['verifier'] = array(
-									'type' => 'date'
-								);
-								break;
-							case 'num':
-							case 'monnaie':
-								$saisie['verifier'] = array(
-									'type' => 'entier'
-								);
-								if ($taille = trim(spip_xml_aplatit($field['taille'])))
-									$saisie['verifier']['options'] = array('max' => (pow(10, $taille)-1));
-								break;
-							case 'email':
-								$saisie['verifier'] = array(
-									'type' => 'email'
-								);
-								break;
-							case 'telephone':
-								$saisie['verifier'] = array(
-									'type' => 'telephone'
-								);
-								break;
-							case 'select':
-								unset($saisie['options']['size']);
-								$liste = trim(spip_xml_aplatit($field['extra_info']));
-								if ($liste == 'radio')
-									$saisie['saisie'] = 'radio';
-								else
-									$saisie['saisie'] = 'selection';
-								break;
-							case 'multiple':
-								$saisie['saisie'] = 'checkbox';
-								unset($saisie['options']['size']);
-								break;
-							case 'fichier':
-							case 'separateur':
-								$saisie = null;
-						}
-						
-						// On continue seulement si on a toujours une saisie
-						if ($saisie){
-							// Les choix pour les types select et multiple
-							if(isset($field['les_choix']) and is_array($field['les_choix'])){
-								$saisie['options']['datas'] = array();
-								foreach($field['les_choix'] as $les_choix){
-									foreach($les_choix['un_choix'] as $un_choix){
-										$choix = trim(spip_xml_aplatit($un_choix['choix']));
-										$titre = trim(spip_xml_aplatit($un_choix['titre']));
-										$saisie['options']['datas'][$choix] = $titre;
-									}
+
+						// Les choix pour les types select et multiple
+						if(isset($field['les_choix']) and is_array($field['les_choix'])){
+							$champ['choix'] = array();
+							foreach($field['les_choix'] as $les_choix){
+								foreach($les_choix['un_choix'] as $un_choix){
+									$champ['choix'][] = array(
+										'choix'=>trim(spip_xml_aplatit($un_choix['choix'])),
+										'titre'=>trim(spip_xml_aplatit($un_choix['titre'])),
+									);
 								}
 							}
-						
-							// Le nom
-							$saisie['options']['nom'] = trim(spip_xml_aplatit($field['champ']));
-						
-							// Le label
-							$saisie['options']['label'] = trim(spip_xml_aplatit($field['titre']));
-						
-							// Obligatoire
-							if (trim(spip_xml_aplatit($field['obligatoire'])) == 'oui')
-								$saisie['options']['obligatoire'] = 'on';
-						
-							// Explication éventuelle
-							if ($explication = trim(spip_xml_aplatit($field['aide'])))
-								$saisie['options']['explication'] = $explication;
-						
-							// On ajoute enfin la saisie
-							$formulaire['saisies'][] = $saisie;
 						}
+
+						if ($saisie = forms_champ_vers_saisie($champ))
+							$formulaire['saisies'][] = $saisie;
 					}
 				}
 				
-				// Les traitements
-				$formulaire['traitements'] = array();
-				
-				// Le traitement email
-				$config_email = unserialize(trim(spip_xml_aplatit($form['email'])));
-				if (is_array($config_email)){
-					if ($email_defaut = $config_email['defaut'])
-						$formulaire['traitements']['email'] = array(
-							'destinataires_plus' => $email_defaut
-						);
-				}
-				
-				// Le traitement enregistrement
-				$formulaire['traitements']['enregistrement'] = array(
-					'moderation' => (trim(spip_xml_aplatit($form['moderation'])) == 'priori') ? 'priori' : 'posteriori',
-					'modifiable' => (trim(spip_xml_aplatit($form['modifiable'])) == 'oui') ? 'on' : '',
-					'multiple' => (trim(spip_xml_aplatit($form['multiple'])) == 'non') ? '' : 'on'
-				);
-				
-				include_spip('action/editer_formulaire');
-				// On insère un nouveau formulaire
-				$id_formulaire = insert_formulaire();
-				// Si ça a marché on modifie les champs de base
-				if ($id_formulaire > 0 and !($erreur = formulaire_set($id_formulaire, $formulaire))){
-					// Et ensuite les saisies et les traitements
-					$ok = sql_updateq(
-						'spip_formulaires',
-						array(
-							'saisies' => serialize($formulaire['saisies']),
-							'traitements' => serialize($formulaire['traitements'])
-						),
-						'id_formulaire = '.$id_formulaire
-					);
-				}
+				// les traitements
+				forms_configure_traitement_formulaire($form_source,$formulaire);
+				$id_formulaire = forms_importe_en_base($formulaire);
 			}
 		}
 	}
 	
-	if ($id_formulaire and $ok){
+	if ($id_formulaire){
 		return $id_formulaire;
 	}
 	else{
@@ -172,4 +76,219 @@ function echanger_formulaire_forms_importer_dist($fichier){
 	}
 }
 
-?>
+/**
+ * Importer le tableau $formulaire en base
+ * @param array $formulaire
+ * @return bool|int
+ */
+function forms_importe_en_base($formulaire){
+	include_spip('action/editer_formulaire');
+	// On insère un nouveau formulaire
+	$id_formulaire = formulaire_inserer();
+
+	if (is_array($formulaire['saisies']))
+		$formulaire['saisies'] = serialize($formulaire['saisies']);
+	if (is_array($formulaire['traitements']))
+		$formulaire['traitements'] = serialize($formulaire['traitements']);
+
+	// si l'identifiant existe deja (multiples imports du meme form)
+	// le dater
+	if (sql_countsel("spip_formulaires","identifiant=".sql_quote($formulaire['identifiant']))){
+		$formulaire['identifiant'] .= "_".date('Ymd_His');
+	}
+
+	// Si ok on modifie les champs de base
+	if ($id_formulaire>0
+		AND !($erreur = formulaire_modifier($id_formulaire, $formulaire))){
+
+		return $id_formulaire;
+	}
+
+	return false;
+}
+
+/**
+ * Configuration de l'objet formulaire formidable a partir du form f&t
+ * @param array $form
+ * @param array $formulaire
+ */
+function forms_configure_formulaire($form,&$formulaire){
+
+	// Le titre
+	$formulaire['titre'] = ($form['titre'] ? $form['titre'] : _T('info_sans_titre'));
+
+	// Generer un identifiant
+	// si id_form fourni, on s'en sert
+	if (isset($form['id_form'])){
+		$formulaire['identifiant'] = "form_import_".$form['id_form'];
+	}
+	else {
+		$formulaire['identifiant'] = "form_import_".preg_replace(",\W,","_",strtolower($formulaire['titre']));
+	}
+
+	// Le descriptif
+	$formulaire['descriptif'] = (isset($form['descriptif']) ? $form['descriptif'] : '');
+
+	// Le message de retour si ok
+	$formulaire['message_retour'] = (isset($form['texte']) ? $form['texte'] : '');
+
+	if (!isset($formulaire['traitements']))
+		$formulaire['traitements'] = array();
+
+	if (!isset($formulaire['saisies']))
+		$formulaire['saisies'] = array();
+}
+
+/**
+ * Configurer les traitements
+ *
+ * @param array $form
+ * @param array $formulaire
+ */
+function forms_configure_traitement_formulaire($form,&$formulaire){
+	// Le traitement email
+	if ($form['champconfirm']){
+		if (!isset($formulaire['traitements']['email']))
+			$formulaire['traitements']['email'] = array();
+		$formulaire['traitements']['email']['champ_courriel_destinataire_form'] = $form['champconfirm'];
+	}
+
+	if (is_array($form['email'])){
+
+		if ($email_defaut = $form['email']['defaut']){
+			if (!isset($formulaire['traitements']['email']))
+				$formulaire['traitements']['email'] = array();
+			$formulaire['traitements']['email']['destinataires_plus'] = $email_defaut;
+		}
+
+		// TODO email route : feature qui n'existe pas dans formidable
+		if ($route = $form['email']['route']){
+
+		}
+	}
+
+	// Le traitement enregistrement : toujours
+	$formulaire['traitements']['enregistrement'] = array(
+		'moderation' => ($form['moderation'] == 'priori') ? 'priori' : 'posteriori',
+		'modifiable' => ($form['modifiable'] == 'oui') ? 'on' : '',
+		'multiple' => ($form['multiple'] == 'non') ? '' : 'on'
+	);
+
+}
+
+
+/**
+ * Transforme un champ f&t en Saisie
+ * @param array $champ
+ *   string champ
+ *   string titre
+ *   string type
+ *   string obligatoire
+ *   string taille
+ *   string aide
+ *   string extra_info
+ *   string saisie oui/non
+ *   array choix
+ *     string choix
+ *     string titre
+ * @return array|bool
+ */
+function forms_champ_vers_saisie($champ){
+
+	// Le truc par défaut
+	$saisie = array(
+		'saisie' => 'input',
+		'options' => array('size'=>40)
+	);
+
+	// On essaye de traduire tous les types de champs
+	$type = $champ['type'];
+	switch ($type){
+		case 'texte':
+			$saisie['saisie'] = 'textarea';
+			unset($saisie['options']['size']);
+			$saisie['options']['rows'] = 5;
+			$saisie['options']['cols'] = 40;
+			break;
+		case 'password':
+			$saisie['options']['type'] = 'password';
+			break;
+		case 'date':
+			$saisie['saisie'] = 'date';
+			$saisie['verifier'] = array(
+				'type' => 'date'
+			);
+			break;
+		case 'num':
+		case 'monnaie':
+			$saisie['verifier'] = array(
+				'type' => 'entier'
+			);
+			if ($taille = $champ['taille'])
+				$saisie['verifier']['options'] = array('max' => (pow(10, $taille)-1));
+			break;
+		case 'email':
+			$saisie['verifier'] = array(
+				'type' => 'email'
+			);
+			break;
+		case 'telephone':
+			$saisie['verifier'] = array(
+				'type' => 'telephone'
+			);
+			break;
+		case 'select':
+			unset($saisie['options']['size']);
+			$liste = $champ['extra_info'];
+			if ($liste == 'radio')
+				$saisie['saisie'] = 'radio';
+			else
+				$saisie['saisie'] = 'selection';
+			break;
+		case 'multiple':
+			$saisie['saisie'] = 'checkbox';
+			unset($saisie['options']['size']);
+			break;
+		case 'fichier':
+			// TODO saisie file
+		case 'separateur':
+			// TODO fieldset ?
+			$saisie = null;
+	}
+
+	// On continue seulement si on a toujours une saisie
+	if (!$saisie)
+		return false;
+
+	// Les choix pour les types select et multiple
+	if(isset($champ['choix']) and is_array($champ['choix'])){
+		$saisie['options']['datas'] = array();
+		foreach($champ['choix'] as $un_choix){
+			$choix = $un_choix['choix'];
+			$titre = $un_choix['titre'];
+			$saisie['options']['datas'][$choix] = $titre;
+		}
+	}
+
+	// Le nom
+	$saisie['options']['nom'] = $champ['champ'];
+
+	// Le label
+	$saisie['options']['label'] = $champ['titre'];
+
+	// Obligatoire
+	if ($champ['obligatoire'] == 'oui')
+		$saisie['options']['obligatoire'] = 'on';
+
+	// Explication éventuelle
+	if ($explication = $champ['aide'])
+		$saisie['options']['explication'] = $explication;
+
+	if ($champ['saisie']=='non'){
+		$saisie['options']['disable'] = 'on';
+		// masquer en JS, fallback
+		$saisie['options']['afficher_si'] = 'false';
+	}
+
+	return $saisie;
+}
