81e7525e2e7f06c14254ee0139ec59276623e6c0
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-02-10 13:17:25 +0000

    genie optimiser : supprimer - les formulaires a la poubelles - les reponses associees a un formulaire supprimé - les reponses a la poubelle - les champs des reponses associés à une réponse supprimée
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@80616 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- formidable/trunk/formidable_pipelines.php
+++ formidable/trunk/formidable_pipelines.php
@@ -20,17 +20,50 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
  *     Données du pipeline
  */
 function formidable_optimiser_base_disparus($flux){
+	// Les formulaires qui sont à la poubelle
+	$res = sql_select(
+		'id_formulaire AS id',
+		'spip_formulaires',
+		'statut='.sql_quote('poubelle')
+	);
+
+	// On génère la suppression
+	$flux['data'] += optimiser_sansref('spip_formulaires', 'id_formulaire', $res);
+
+
+	# les reponses qui sont associees a un formulaire inexistant
+	$res = sql_select("R.id_formulaire AS id",
+		        "spip_formulaires_reponses AS R
+		        LEFT JOIN spip_formulaires AS F
+		          ON R.id_formulaire=F.id_formulaire",
+			 "R.id_formulaire > 0
+			 AND F.id_formulaire IS NULL");
+
+	$flux['data'] += optimiser_sansref('spip_formulaires_reponses', 'id_formulaire', $res);
+
+
 	// Les réponses qui sont à la poubelle
 	$res = sql_select(
 		'id_formulaires_reponse AS id',
 		'spip_formulaires_reponses',
-		'statut = '.sql_quote('poubelle')
+		sql_in('statut',array('refuse','poubelle'))
 	);
 	
 	// On génère la suppression
 	$flux['data'] += optimiser_sansref('spip_formulaires_reponses', 'id_formulaires_reponse', $res);
-	
-		//
+
+
+	// les champs des reponses associes a une reponse inexistante
+	$res = sql_select("C.id_formulaires_reponse AS id",
+		        "spip_formulaires_reponses_champs AS C
+		        LEFT JOIN spip_formulaires_reponses AS R
+		          ON C.id_formulaires_reponse=R.id_formulaires_reponse",
+			 "C.id_formulaires_reponse > 0
+			 AND R.id_formulaires_reponse IS NULL");
+
+	$flux['data'] += optimiser_sansref('spip_formulaires_reponses_champs', 'id_formulaires_reponse', $res);
+
+	//
 	// CNIL -- Informatique et libertes
 	//
 	// masquer le numero IP des vieilles réponses
