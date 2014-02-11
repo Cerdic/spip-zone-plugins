f79127516c453da6b5faf2192f4818bd6a76059a
severo@rednegra.net <severo@rednegra.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-02-11 04:39:45 +0000

    [Salvatore] [source:_plugins_/plugonet/trunk/lang/ paquet-plugonet] Export depuis http://trad.spip.net de la langue es
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@80639 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ plugonet/trunk/lang/paquet-plugonet_es.php
@@ -0,0 +1,17 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-plugonet?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// P
+	'plugonet_description' => 'Este plugin permite producir el nuevo archivo paquet.xml a partir del archivo plugin.xml. Varias opciones de verificación son disponibles para limpiar previamente ciertas balizas del archivo original.
+
+Además, PlugOnet permite validar archivos paquet.xml escritos manualmente y provee una ayuda sobre cada baliza de la nueva DTD.',
+	'plugonet_slogan' => 'Migrar de la DTD "plugin" a la DTD "paquet"'
+);
+
+?>
