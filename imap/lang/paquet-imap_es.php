326e80b7d8c36e5557212f015aac439b25cb7dec
severo@rednegra.net <severo@rednegra.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-02-11 04:39:26 +0000

    [Salvatore] [source:_plugins_/imap/lang/ paquet-imap] Export depuis http://trad.spip.net de la langue es
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@80633 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ imap/lang/paquet-imap_es.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-imap?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// I
+	'imap_description' => 'Configuración y conexión a un servidor de correos electrónicos IMAP',
+	'imap_nom' => 'IMAP',
+	'imap_slogan' => 'Conexión a un servidor IMAP'
+);
+
+?>
