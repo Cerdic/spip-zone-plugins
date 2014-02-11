f06e3b15d146c0e9707e066013eecca7246f9734
severo@rednegra.net <severo@rednegra.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-02-11 04:39:03 +0000

    [Salvatore] [source:_plugins_/critere_mots/trunk/lang/ paquet-critere_mots] Export depuis http://trad.spip.net de la langue es
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@80625 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ critere_mots/trunk/lang/paquet-critere_mots_es.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-critere_mots?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'critere_mots_description' => 'Permite mostrar los elementos que tienen varias palabras-claves en común, mediante la variable $mots[] pasada en el entorno',
+	'critere_mots_slogan' => 'Seleccionar objetos que tienen palabras-claves en común'
+);
+
+?>
