eff4b11aeb01961d49b125881bedbf8f64738d5c
severo@rednegra.net <severo@rednegra.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-02-11 04:39:39 +0000

    [Salvatore] [source:_plugins_/pj2article/lang/ paquet-pj2article] Export depuis http://trad.spip.net de la langue es
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@80637 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ pj2article/lang/paquet-pj2article_es.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-pj2article?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// P
+	'pj2article_description' => 'Importar todos los archivos adjuntos de un buzón de correos electrónicos y crear automáticamente un artículo por archivo',
+	'pj2article_nom' => 'Importación de archivos adjuntos',
+	'pj2article_slogan' => 'Importar todos los archivos adjuntos de un buzón de correos electrónicos y crear automáticamente un artículo por archivo'
+);
+
+?>
