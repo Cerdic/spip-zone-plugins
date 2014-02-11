efeffa7852586c6b687e711636776fb6b26da9c8
severo@rednegra.net <severo@rednegra.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-02-11 04:39:24 +0000

    [Salvatore] [source:_plugins_/http/trunk/lang/ paquet-http] Export depuis http://trad.spip.net de la langue es
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@80632 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ http/trunk/lang/paquet-http_es.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-http?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// H
+	'http_description' => 'Este plugin provee una API para que otros plugins puedan implementar más fácilmente servicios que utilizan los  métodos HTTP.',
+	'http_nom' => 'Servidor HTTP abstracto',
+	'http_slogan' => 'Gestión abstracta de los métodos HTTP.'
+);
+
+?>
