1a51b56dc03ba2a9d26091288d5736209557104c
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-02-12 18:01:56 +0000

    Utiliser des chaînes de langue
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@80661 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- duplicator/trunk/lang/duplicator_fr.php
+++ duplicator/trunk/lang/duplicator_fr.php
@@ -4,13 +4,13 @@
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 // A
-	'aide_choix_champs' => 'Liste des noms des champs SQL s&eacute;par&eacute;s par des ","<br/><em>Laisser vide pour s&eacute;lectionner tous les champs principaux.</em>',
+	'aide_choix_champs' => 'Liste des noms des champs SQL séparés par des ","<br/><em>Laisser vide pour sélectionner tous les champs principaux.</em>',
 	'action_dupliquer_article' => "Dupliquer cet article",
 	'action_dupliquer_rubrique' => "Dupliquer cette rubrique",
 
 // C
-	'choix_type' => 'S&eacute;lectionner le ou les mode(s) de duplication',
-	'choix_champs' => 'Choisir les champs &agrave; dupliquer',
+	'choix_type' => 'Sélectionner le ou les mode(s) de duplication',
+	'choix_champs' => 'Choisir les champs à dupliquer',
 
 	'configuration_duplicator' => 'Configuration de Duplicator',
 
@@ -22,22 +22,24 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'icone_dupliquer' => 'Dupliquer la rubrique',
 
 // L
-	'label_art_champs' => 'Liste des champs &agrave; dupliquer pour chaque article :',
-	'label_rub_champs' => 'Liste des champs &agrave; dupliquer pour chaque rubrique :',
+	'label_art_champs' => 'Liste des champs à dupliquer pour chaque article :',
+	'label_rub_champs' => 'Liste des champs à dupliquer pour chaque rubrique :',
 
 
-// M	
+// M
 	'message_annuler' => 'Annuler',
+	'message_avertissement_article' => 'Êtes-vous sûr de vouloir dupliquer cet article ?',
+	'message_avertissement_rubrique' => 'Êtes-vous sûr de vouloir dupliquer tout le contenu de cette rubrique ?',
 	'message_confirmer' => 'Confirmer',	
 	
-	'operation_executee' => "L'op&eacute;ration a bien &eacute;t&eacute; ex&eacute;cut&eacute;e.",
-	'operation_annulee' => "L'op&eacute;ration a &eacute;t&eacute; annul&eacute;e.",
-	'operation_retour_ok' => "Se rendre dans la rubrique copi&eacute;e.",
+	'operation_executee' => "L'opération a bien été exécutée.",
+	'operation_annulee' => "L'opération a été annulée.",
+	'operation_retour_ok' => "Se rendre dans la rubrique copiée.",
 	'operation_retour_ko' => "Retour aux rubriques.",
 
 	'icone_dupliquer_article' => "Dupliquer l'article",
 
-	'operation_retour_ok_article' => "Se rendre dans l'article dupliqu&eacute;.",
+	'operation_retour_ok_article' => "Se rendre dans l'article dupliqué.",
 	'operation_retour_ko_article' => "Retour aux articles.",
 
 	'texte_duplicator' => "Appliquer la duplication aux rubriques et/ou articles"
