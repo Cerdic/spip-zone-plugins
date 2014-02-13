b8d6fddccda46e6e037f8651da88ae3f6a5f7054
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-02-13 17:17:24 +0000

    Des broutilles
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@80675 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- duplicator/trunk/duplicator_pipelines.php
+++ duplicator/trunk/duplicator_pipelines.php
@@ -1,8 +1,8 @@
 <?php
 
 /***************************************************************************\
- * Plugin Duplicator pour Spip 2.0
- * Licence GPL (c) 2010 - Apsulis
+ * Plugin Duplicator pour Spip 3.0
+ * Licence GPL (c) 2010-2014 - Apsulis
  * Duplication de rubriques et d'articles
  *
 \***************************************************************************/
@@ -12,14 +12,14 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 function duplicator_boite_infos($flux){
 	$type = $flux['args']['type'];
 	if(autoriser("webmestre")){
-		if ( (lire_config('duplicator/config/duplic_rubrique')=="oui") ){
-			if ( ($id = intval($flux['args']['id'])) && ($type=='rubrique') ) {
+		if ((lire_config('duplicator/config/duplic_rubrique')=="oui")){
+			if (($id = intval($flux['args']['id'])) && ($type=='rubrique')){
 				$contexte = array('id_objet'=>$id,'objet'=>$type);
 				$flux["data"] .= recuperer_fond("noisettes/bouton_duplicator", $contexte);
 			}
 		}
-		if ( (lire_config('duplicator/config/duplic_article')=="oui") ){
-			if ( ($id = intval($flux['args']['id'])) && ($type=='article') ) {
+		if ((lire_config('duplicator/config/duplic_article')=="oui")){
+			if (($id = intval($flux['args']['id'])) && ($type=='article')){
 				$contexte = array('id_objet'=>$id,'objet'=>$type);
 				$flux["data"] .= recuperer_fond("noisettes/bouton_duplicator", $contexte);
 			}
