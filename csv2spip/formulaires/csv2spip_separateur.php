296fb6379e1c451d6f0bd634d209383873e68cc4
tofulm@gmail.com <tofulm@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-02-16 21:02:08 +0000

    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@80778 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ csv2spip/formulaires/csv2spip_separateur.php
@@ -0,0 +1,29 @@
+<?php
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+function formulaires_csv2spip_separateur_charger_dist(){
+    $valeurs = array(
+        "separateur"  => lire_config("csv2spip_separateur"),
+    );
+return $valeurs;
+}
+
+function formulaires_csv2spip_separateur_verifier_dist(){
+        
+    $erreurs = array();
+    //champs obligatoire
+    if (!_request(separateur)) $erreurs['separateur'] = _T('csv2spip:obligatoire');
+    return $erreurs;
+}
+
+function formulaires_csv2spip_separateur_traiter_dist(){
+    $separateur   = _request('separateur');
+    
+    ecrire_meta("csv2spip_separateur",_request('separateur'));
+    $retour = array();
+    $retour['message_ok'] = _T('csv2spip:nouveau_separateur',array("separateur"=>$separateur));
+    //$retour['redirect'] = "ecrire/?exec=csv2spip";
+
+    return $retour;
+}
+?>
---
+++ csv2spip/lang/paquet-csv2spip_fr.php
@@ -0,0 +1,8 @@
+<?php
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	'fabrique_titre' => 'csv2spip',
+	'fabrique_slogan' => 'Outil pour la gestion des fichiers csv.',
+	'fabrique_description' => 'Ce plugin permet de générer  des fichiers csv ',
+);
+?>
