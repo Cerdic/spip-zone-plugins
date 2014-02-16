b23394db9ea9753a82000a28704e5a5bef23ae42
tofulm@gmail.com <tofulm@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-02-16 21:24:12 +0000

    oups ll
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@80779 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- csv2spip/lang/paquet-csv2spip_fr.php
+++
@@ -1,8 +0,0 @@
-<?php
-$GLOBALS[$GLOBALS['idx_lang']] = array(
-
-	'fabrique_titre' => 'csv2spip',
-	'fabrique_slogan' => 'Outil pour la gestion des fichiers csv.',
-	'fabrique_description' => 'Ce plugin permet de générer  des fichiers csv ',
-);
-?>
