fe9889a48ae63a278c9e350745ea6309e668efdd
severo@rednegra.net <severo@rednegra.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-02-17 11:22:10 +0000

    tickets - une vue/crayon par groupe de mots-clés
    au lieu de tout regrouper dans un seul select (Eric).
    Il reste à ajouter les mêmes champs dans le formulaire
    d'édition.
    
    A noter : le crayon mots_ticket reste fonctionnel, si
    on veut conserver le fonctionnement précédent (tous les
    groupes dans le même select).
    
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@80800 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- tickets/trunk/tickets_options.php
+++ tickets/trunk/tickets_options.php
@@ -24,17 +24,31 @@ function valeur_champ_mots_ticket($table, $id, $champ) {
 }
 
 /**
+ * Récupérer dans la base la liste des mots-clés liés au ticket pour
+ * groupe de mots donné
+ * http://marcimat.magraine.net/Crayon-de-mots-pour-un-article
+ */
+function valeur_champ_groupemots_ticket($table, $id, $champ) {
+	return valeur_champ_mots_objet($table, $id, $champ);
+}
+
+/**
  * Récupérer dans la base la liste des mots-clés liés à l'objet
  * http://marcimat.magraine.net/Crayon-de-mots-pour-un-article
  */
-function valeur_champ_mots_objet($table, $id, $champ) {
+function valeur_champ_mots_objet($table, $ids, $champ) {
+	list($id_objet, $id_groupe) = explode('-', $ids);
 	list(, $objet) = explode('_', $champ);
 
-	$valeurs = sql_allfetsel("m.id_mot", "spip_mots AS m, spip_mots_liens AS ml", array(
+	$where = array(
 		"m.id_mot = ml.id_mot",
-		"ml.id_objet=".sql_quote($id),
+		"ml.id_objet=".sql_quote($id_objet),
 		"ml.objet=".sql_quote($objet)
-	));
+	);
+	if ($id_groupe > 0)
+		$where[] = "m.id_groupe=" . sql_quote($id_groupe);
+
+	$valeurs = sql_allfetsel("m.id_mot", "spip_mots AS m, spip_mots_liens AS ml", $where);
 	$valeurs = array_map('array_shift', $valeurs);
 
 	return $valeurs;
@@ -49,22 +63,32 @@ function mots_ticket_revision($id, $colonnes, $type_objet) {
 }
 
 /**
+ * Modifier dans la base la liste des mots-clés liés au ticket pour un
+ * groupe de mots donné
+ * http://marcimat.magraine.net/Crayon-de-mots-pour-un-article
+ */
+function groupemots_ticket_revision($id, $colonnes, $type_objet) {
+	return mots_objet_revision($id, $colonnes, $type_objet, 'groupemots_ticket');
+}
+
+/**
  * Modifier dans la base la liste des mots-clés liés à l'objet
  * http://marcimat.magraine.net/Crayon-de-mots-pour-un-article
  */
-function mots_objet_revision($id, $colonnes, $type_objet, $champ = '') {
+function mots_objet_revision($ids, $colonnes, $type_objet, $champ = '') {
 	if (!$champ) return false;
+
+	list($id_objet, $id_groupe) = explode('-', $ids);
+	list(, $type_liaison) = explode('_', $champ);
 
 	/* On vérifie qu'on a le droit d'associer des mots l'objet
 	 * il serait mieux de le vérifier pour chaque groupe de mots (voir
 	 * les options de la fonction autoriser_associermots_dist)
 	 */
-	if (!autoriser('associermots',$type_objet,$id)) return false;
-
-	list(, $type_liaison) = explode('_', $champ);
+	if (!autoriser('associermots',$type_liaison,$id_objet)) return false;
 
 	// actuellement en bdd
-	$old = valeur_champ_mots_objet($type_objet, $id, $champ);
+	$old = valeur_champ_mots_objet($type_objet, $ids, $champ);
 	// ceux qu'on veut maintenant (on vérifie que ce sont des indices)
 	$new = array_filter(explode(',', $colonnes[$champ]),'is_numeric');
 	// les mots à supprimer
@@ -74,10 +98,10 @@ function mots_objet_revision($id, $colonnes, $type_objet, $champ = '') {
 
 	include_spip('action/editer_liens');
 	if ($del) {
-		objet_dissocier(array('mot'=>$del), array($type_liaison => $id));
+		objet_dissocier(array('mot'=>$del), array($type_liaison => $id_objet));
 	}
 	if ($add) {
-		objet_associer(array('mot'=>$add), array($type_liaison => $id));
+		objet_associer(array('mot'=>$add), array($type_liaison => $id_objet));
 	}
 
 	return true;
