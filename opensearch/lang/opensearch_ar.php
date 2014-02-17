a6bee7127344c132a9a5800eecc7a00e65919737
george@middleeastwatch.net <george@middleeastwatch.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-02-17 04:40:00 +0000

    [Salvatore] [source:_plugins_/opensearch/lang/ opensearch] Export depuis http://trad.spip.net de la langue ar
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@80788 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ opensearch/lang/opensearch_ar.php
@@ -0,0 +1,21 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/opensearch?lang_cible=ar
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cfg_boite_opensearch' => 'إعداد ملحق البحث المفتوح OpenSearch',
+	'cfg_descr_opensearch' => 'البحث المفتوح OpenSearch: محرك بحث مخصص لموقعك.',
+	'cfg_inf_id_rubrique' => 'تحديد القسم الذي سيتم البحث فيه.',
+	'cfg_inf_page_recherche' => 'تحديد الصفحة النموذجية التي ستستخدم للبحث في الموقع (مثلاً: البحث للصفحة النموذجية recherche.html)',
+	'cfg_lbl_id_rubrique' => 'القسم الهدف',
+	'cfg_lbl_page_recherche' => 'الصفحة النموذجية للبحث',
+	'cfg_option_tout' => 'كل الموقع',
+	'cfg_titre_opensearch' => 'البحث المفتوح OpenSearch'
+);
+
+?>
