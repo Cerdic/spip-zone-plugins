cd8c108fc283cbdf8c49b094438999a4e1ad255a
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-02-17 04:39:54 +0000

    [Salvatore] [source:_plugins_/modeles_media/trunk/lang/ media] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@80786 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- modeles_media/trunk/lang/media_fr.php
+++ modeles_media/trunk/lang/media_fr.php
@@ -1,25 +1,32 @@
 <?php
-
-// Sécurité
-if (!defined("_ECRIRE_INC_VERSION")) return;
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/modeles_media/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-	
+
+	// C
 	'credits' => 'Crédits :',
-	'erreur_saisies' => 'Votre saisie contient des erreurs !',
+
+	// E
+	'erreur_saisies' => 'Votre saisie contient des erreurs !',
 	'erreur_taille' => 'Vous devez saisir un nombre entier positif.',
-	'explication_alt' => 'Vous pouvez personnaliser le texte alternatif affiché si le navigateur ne peut afficher le média (pour les images, il s\'agit de l\'attribut <i>alt</i>). Laissez vide pour que le texte alternatif soit généré automatiquement.',
+	'explication_alt' => 'Vous pouvez personnaliser le texte alternatif affiché si le navigateur ne peut afficher le média (pour les images, il s’agit de l’attribut <i>alt</i>). Laissez vide pour que le texte alternatif soit généré automatiquement.',
 	'explication_configurer_media' => 'Vous pouvez définir quatre tailles standards utilisables avec les modèles &lt;media&gt; via le paramètre <i>|taille</i>.',
 	'explication_descriptif' => 'Les raccourcis typographiques de SPIP sont acceptés.',
 	'explication_lien' => 'Vous pouvez saisie une URL de la forme http://monsite.net, une adresse mail ou un raccourci SPIP (rub1, art3, breve4...).',
-	'explication_titre_lien' => 'Vous pouvez personnaliser l\'attribut <i>title</i> du lien. Laissez vide pour que l\'attribut <i>title</i> soit généré automatiquement.',
+	'explication_titre_lien' => 'Vous pouvez personnaliser l’attribut <i>title</i> du lien. Laissez vide pour que l’attribut <i>title</i> soit généré automatiquement.',
 	'extension' => 'Extension :',
+
+	// H
 	'hauteur' => 'Hauteur :',
+
+	// I
 	'info_configurer_media_titre' => 'Modèles &lt;media&gt; : configurer les tailles par défaut',
-	'info_inclusion_icone' => 'Inclusion de l\'ic&ocirc;ne&nbsp;:',
+	'info_inclusion_icone' => 'Inclusion de l’icône :',
 	'item_afficher' => 'afficher',
+	'item_icone' => 'afficher l’icône',
 	'item_insert' => 'inclure le document',
-	'item_icone' => 'afficher l\'icône',
 	'item_legende_complete' => 'légende complète',
 	'item_legende_simple' => 'légende simple (titre + descriptif)',
 	'item_lien_document' => 'lien sur le document',
@@ -32,12 +39,14 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'item_taille_moyen' => 'moyen',
 	'item_taille_petit' => 'petit',
 	'item_vignette' => 'afficher la vignette',
+
+	// L
 	'label_alt' => 'Texte alternatif :',
 	'label_configurer_media_largeur_legende' => 'Largeur des légendes :',
-	'label_configurer_media_taille_grand' => 'Taille \'grand\' :',
-	'label_configurer_media_taille_icone' => 'Taille \'icone\' :',
-	'label_configurer_media_taille_moyen' => 'Taille \'moyen\' :',
-	'label_configurer_media_taille_petit' => 'Taille \'petit\' :',
+	'label_configurer_media_taille_grand' => 'Taille ’grand’ :',
+	'label_configurer_media_taille_icone' => 'Taille ’icone’ :',
+	'label_configurer_media_taille_moyen' => 'Taille ’moyen’ :',
+	'label_configurer_media_taille_petit' => 'Taille ’petit’ :',
 	'label_credits' => 'Crédits :',
 	'label_descriptif' => 'Descriptif :',
 	'label_hauteur' => 'Hauteur (en pixels) :',
@@ -54,15 +63,20 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'label_type' => 'Type de document :',
 	'label_variante' => 'Que souhaitez-vous ?',
 	'largeur' => 'Largeur :',
+
+	// M
 	'maj' => 'Mis à jour le :',
 	'mime_type' => 'Type MIME :',
 	'modeles_media' => 'Modèles &lt;media&gt;',
+
+	// N
 	'nom_media' => 'un document',
+
+	// P
 	'pixels_hauteur' => 'pixels de hauteur',
 	'pixels_largeur' => 'pixels de largeur',
 	'pixels_maximum' => 'pixels maximum',
-	'pixels_minimum' => 'pixels minimum',
-
+	'pixels_minimum' => 'pixels minimum'
 );
 
 ?>
