5e4a040b7044a3a96b702a0110348e80343c26ee
rj@hodo.fr <rj@hodo.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-02-17 04:40:05 +0000

    [Salvatore] [source:_plugins_/persona/trunk/lang/ persona] Export depuis http://trad.spip.net de la langue en
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@80790 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ persona/trunk/lang/persona_en.php
@@ -0,0 +1,26 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/persona?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'affichage' => 'Display', # RELIRE
+
+	// C
+	'cacher_login_classique' => 'Hide?', # RELIRE
+	'configurer_titre' => 'Configure Persona', # RELIRE
+
+	// E
+	'explication_cacher_login_classique' => 'If this box is checked, the login form will be hidden by default on the connection page. Only the "Persona" button will be visible. Under this button, a link can unmask the form.',
+	'explication_login' => 'You can sign in with your Persona Id (<a href="http://en.wikipedia.org/wiki/Mozilla_Persona" target="_blank">help</a>)', # RELIRE
+
+	// F
+	'form_afficher' => 'Display the standard login form', # RELIRE
+	'form_masquer' => 'Hide the standard login form.' # RELIRE
+);
+
+?>
