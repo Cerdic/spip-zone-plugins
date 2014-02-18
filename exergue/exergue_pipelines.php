d0c0186d8eca17c9864db63418cd805573d0b352
booz@rezo.net <booz@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-02-18 18:40:20 +0000

    span ou div
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@80851 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- exergue/exergue_pipelines.php
+++ exergue/exergue_pipelines.php
@@ -2,10 +2,15 @@
 
 function exergue_pre_propre($letexte) {
 	
-	if (preg_match_all(",<(exergue)>(.*)<\/(exergue)>,UimsS",
+	if (preg_match_all(",<(exergue)>(.*)<\/(exergue)>,Uims",
 	$letexte, $regs, PREG_SET_ORDER)) {
-		foreach ($regs as $reg) {		
-			$letexte = str_replace($reg[0], "<div class=\"spip_exergue\">" . $reg[2] . "</div>", $letexte);
+		foreach ($regs as $reg) {							
+			// ne pas mettre le <div...> s'il n'y a qu'une ligne
+			if (is_int(strpos($reg[0],"\n"))) {
+				$letexte = str_replace($reg[0], "<div class=\"spip_exergue\">"."\n\n" . $reg[2] . "</div>", $letexte);
+			} else {
+				$letexte = str_replace($reg[0], "<span class=\"spip_exergue\">" . $reg[2] . "</span>", $letexte);		
+			}
 		}
 	}
 	return $letexte;
