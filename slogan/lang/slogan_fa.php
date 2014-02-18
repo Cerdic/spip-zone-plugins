c6ffcbf5e901b5508ef15c69fc95ba40e53ba057
davoodhossein@yahoo.com <davoodhossein@yahoo.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-02-18 04:41:17 +0000

    [Salvatore] [source:_plugins_/slogan/lang/ slogan] Export depuis http://trad.spip.net de la langue fa
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@80822 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ slogan/lang/slogan_fa.php
@@ -0,0 +1,20 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/slogan?lang_cible=fa
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'config_info_enregistree' => 'پيكربندي ثبت شده است ',
+
+	// I
+	'info_slogan_site' => 'شعار سايت شما',
+
+	// T
+	'titre_identite_site' => 'هويت سايت'
+);
+
+?>
