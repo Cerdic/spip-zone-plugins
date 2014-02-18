87ffd3a7de6f86a96682a60367c2e968bc57232d
davoodhossein@yahoo.com <davoodhossein@yahoo.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-02-18 04:40:39 +0000

    [Salvatore] [source:_plugins_/article_accueil/trunk/lang/ article_accueil] Export depuis http://trad.spip.net de la langue fa
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@80818 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ article_accueil/trunk/lang/article_accueil_fa.php
@@ -0,0 +1,21 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/article_accueil?lang_cible=fa
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'article_accueil' => 'مقاله پذيرش ',
+	'aucun_article_accueil' => 'هيچ مقاله ',
+
+	// L
+	'label_id_article_accueil' => 'مقاله پذيرش',
+
+	// R
+	'rubrique_article_en_accueil' => 'مقاله پذيرش :'
+);
+
+?>
