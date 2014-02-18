1c931be4403d65fda74d497d54b800d55f0d880f
toutati@free.fr <toutati@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-02-18 18:35:09 +0000

    On peut créer un produit dans une rubrique
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@80850 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- produits/trunk/produits_pipelines.php
+++ produits/trunk/produits_pipelines.php
@@ -16,7 +16,7 @@ function produits_affiche_enfants($flux){
 		);
 	
 		if (autoriser('creerproduitdans', 'rubrique', $flux['args']['id_rubrique'])){
-			//$flux['data'] .= icone_inline(_T('produits:produit_bouton_ajouter'), generer_url_ecrire('produit_edit', 'nouveau=oui&id_rubrique='.$flux['args']['id_rubrique']), find_in_path('prive/themes/spip/images/produits-24.png'), 'creer.gif', 'right');
+			$flux['data'] .= icone_verticale(_T('produit:icone_creer_produit'), generer_url_ecrire('produit_edit', 'id_rubrique='.$flux['args']['id_rubrique']), find_in_path('prive/themes/spip/images/produits-24.png'), 'new', 'right');
 		}
 	}
 	
