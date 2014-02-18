54a5020fe676767d8113d895e0580af18a2e983b
davoodhossein@yahoo.com <davoodhossein@yahoo.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-02-18 04:40:41 +0000

    [Salvatore] [source:_plugins_/article_pdf/lang/ articlepdf] Export depuis http://trad.spip.net de la langue fa
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@80819 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ article_pdf/lang/articlepdf_fa.php
@@ -0,0 +1,29 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/articlepdf?lang_cible=fa
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'copyright' => 'كپي رايت © ',
+
+	// D
+	'date_de_mise_en_ligne' => 'تاريخ نشر : ',
+	'date_de_parution' => 'تاريخ خلق : ',
+	'description' => 'توصيف : ',
+
+	// E
+	'enregistrer_pdf' => 'ثبت در قالب پي.دي.اف ',
+	'extrait_de' => 'استخراج از ',
+
+	// P
+	'post_scriptum' => 'پي.اس (پست اسكريپت) : ',
+
+	// T
+	'tous_droits_reserves' => '- تمام حقوق محفوظ'
+);
+
+?>
