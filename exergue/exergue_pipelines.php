598f75fd9b13f3aa57b4ee71ea9e0f5d2334c966
booz@rezo.net <booz@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-02-19 18:46:06 +0000

    pouvoir indiquer par un numero l'exergue concerné par une ancre. [exergue3<-] => ancre la 3e exergue. (permet de placer l'ancre de l'exergue après l'exergue). exemple : http://www.fluxetfixe.org/Avec-moderation
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@80932 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- exergue/exergue_pipelines.php
+++ exergue/exergue_pipelines.php
@@ -37,11 +37,18 @@ function exergue_insert_head($flux) {
 		var exergue_tab = new Array();	
 		var nb_ancres = 0 ;
 
-		$('a[name=exergue], .spip_exergue').each(function(i){
+		$("a[name^='exergue'], .spip_exergue").each(function(i){
 			/* si c'est une ancre on sait que le prochain exergue a cette ancre */
 			if(!$(this).hasClass('spip_exergue')){
-				var index_exergue = i - nb_ancres;
-				exergue_tab['exergue' + index_exergue] = $(this);
+				// a[name^='exergue']
+				var ancre_numerotee = $(this).attr('name').match(/[0-9]+/g) ;
+ // var res = str.match(/ain/g); 
+				if(ancre_numerotee){
+					exergue_tab['exergue' + ancre_numerotee] = $(this);			
+				}else{	
+					var index_exergue = i + 1 - nb_ancres;
+					exergue_tab['exergue' + index_exergue] = $(this);
+				}
 				nb_ancres ++ ;
 			}
 		});
@@ -49,9 +56,12 @@ function exergue_insert_head($flux) {
 		$('.spip_exergue').each(function(i){
 			var content = $(this).html();
 			/* Soit il y a une ancre [exergue<-] dans le texte et on place l'exergue suivant à cet endroit, soit il n'y en a pas et on place l'exergue avant la balise */
+			i ++ ;
 			if(exergue_tab['exergue' + i]){
 				exergue_tab['exergue' + i].before('<span class="exergue">«&nbsp;'+ guillemets_check(capitaliseFirstLetter(content)) +'&nbsp;»</span>');
 				exergue_tab['exergue' + i].remove();
+				console.log(exergue_tab);
+
 			}else{
 				$(this).before('<span class="exergue">«&nbsp;'+ guillemets_check(capitaliseFirstLetter(content)) +'&nbsp;»</span>');
 			}
