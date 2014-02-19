f9def5280e8a3047a8ac4c9eb2eac75db57e451e
cerf@kraal.fr <cerf@kraal.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-02-19 04:40:00 +0000

    [Salvatore] [source:_plugins_/gravatar/lang/ paquet-gravatar] Export depuis http://trad.spip.net de la langue fr_tu
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@80887 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ gravatar/lang/paquet-gravatar_fr_tu.php
@@ -0,0 +1,24 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-gravatar?lang_cible=fr_tu
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// G
+	'gravatar_description' => 'Permet d’utiliser un cache pour stocker les gravatars.
+_ À utiliser dans une boucle de cette manière : <code>#GRAVATAR{email, taille, url image par
+défaut}</code>
+_ Exemple : <code>#GRAVATAR{#EMAIL,80,#URL_SITE_SPIP/defaut-gravatar.gif}</code>
+
+Étend la balise #LOGO_AUTEUR de manière à prendre en compte le gravatar d’un auteur s’il existe, y compris dans les forums et pétitions.
+_ Permet de configurer une image par defaut, et la taille des images.
+
+Fournit le filtre <code>|gravatar</code>, à utiliser par exemple comme 
+<code>[(#EMAIL|gravatar|image_reduire{80})]</code>.',
+	'gravatar_slogan' => 'Afficher le Gravatar d’un auteur ou d’un contributeur de forum'
+);
+
+?>
