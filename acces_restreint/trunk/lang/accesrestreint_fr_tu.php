842d66fb20a92247bbfd7acc34e17cc39ee864fe
cerf@kraal.fr <cerf@kraal.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-02-19 04:39:19 +0000

    [Salvatore] [source:_plugins_/acces_restreint/trunk/lang/ accesrestreint] Export depuis http://trad.spip.net de la langue fr_tu
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@80872 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- acces_restreint/trunk/lang/accesrestreint_fr_tu.php
+++ acces_restreint/trunk/lang/accesrestreint_fr_tu.php
@@ -10,9 +10,11 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// A
 	'ajouter_droits_auteur' => 'M’ajouter les droits d’accès à cette zone',
 	'aucune_zone' => 'Aucune zone',
-	'auteurs' => 'auteurs', # MODIF
+	'auteur' => '@nb@ auteur',
+	'auteurs' => '@nb@ auteurs',
 
 	// B
+	'bouton_configurer_acces' => 'Configuration des accès .htaccess',
 	'bouton_creer_la_zone' => 'Créer la nouvelle zone',
 
 	// C
@@ -20,15 +22,20 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'confirmer_ajouter_auteurs' => 'Es-tu sûr de vouloir ajouter cet auteur à la zone ?',
 	'confirmer_retirer_auteur_zone' => 'Es-tu sûr de vouloir retirer cet auteur de la zone ?',
 	'confirmer_retirer_auteurs' => 'Es-tu sûr de vouloir retirer tous les auteurs de cette zone ?',
+	'confirmer_retirer_rubrique_zone' => 'Es-tu sûr de vouloir retirer cette rubrique de cette zone ?',
 	'confirmer_supprimer_zone' => 'Es-tu sûr de vouloir supprimer cette zone ?',
 	'creer_zone' => 'Créer une nouvelle zone',
 
 	// D
 	'descriptif' => 'Descriptif',
 
+	// E
+	'explication_creer_htaccess' => 'Cette option interdit la lecture des documents joints si le texte auquel ils se rattachent n’est pas publi&eacute',
+
 	// I
 	'icone_menu_config' => 'Accès Restreint',
 	'icone_supprimer_zone' => 'Supprimer cette zone',
+	'info_1_zone' => '1 zone',
 	'info_acces_restreint' => 'L’accès à cette page est restreint. Identifie toi pour y accéder',
 	'info_ajouter_auteur' => 'Ajouter cet auteur',
 	'info_ajouter_auteurs' => 'Ajouter tous les auteurs',
@@ -38,12 +45,18 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'info_aucune_zone' => 'Aucune zone',
 	'info_auteurs_lies_zone' => 'Les auteurs ayant accès à cette zone',
 	'info_lien_action_proteger' => 'Protéger l’accès à cette rubrique.',
+	'info_nb_zones' => '@nb@ zones',
 	'info_page' => 'Cette page te permets de gérer les zones d’accès restreint de ton site',
 	'info_retirer_auteurs' => 'Retirer tous les auteurs',
 	'info_retirer_zone' => 'Enlever de la zone',
 	'info_retirer_zones' => 'Enlever de toutes les zones',
 	'info_rubrique_dans_zone' => 'Cette rubrique fait partie de la zone :',
 	'info_rubrique_dans_zones' => 'Cette rubrique fait partie des zones :',
+
+	// L
+	'label_creer_htaccess' => 'Accès aux document joints par leur URL',
+	'label_creer_htaccess_non' => 'autoriser la lecture',
+	'label_creer_htaccess_oui' => 'interdire la lecture',
 
 	// M
 	'modifier_zone' => 'Modifier la zone',
@@ -55,13 +68,16 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'publique' => 'Publique',
 
 	// R
-	'rubriques' => 'rubriques', # MODIF
+	'rubrique' => '@nb@ rubrique',
+	'rubriques' => '@nb@ rubriques',
 	'rubriques_zones_acces' => 'Rubriques de la zone',
 
 	// S
 	'selectionner_une_zone' => 'Sélectionner une zone',
 
 	// T
+	'texte_ajouter_zone' => 'Ajouter une zone',
+	'texte_creer_associer_zone' => 'Créer et associer une zone',
 	'titre' => 'Titre',
 	'titre_ajouter_zone' => 'Rejoindre la zone',
 	'titre_boite_protegee_non' => 'Accès non protégé',
@@ -69,6 +85,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'titre_cadre_modifier_zone' => 'Modifier une zone',
 	'titre_page_config' => 'Configuration des accès',
 	'titre_table' => 'Toutes les zones d’accès',
+	'titre_zone_acces' => 'Zone d’accès restreint',
 	'titre_zones_acces' => 'Zones d’accès restreint',
 	'toutes' => 'Toutes',
 
