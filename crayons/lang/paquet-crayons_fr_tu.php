f6d013149f69d24aa06ecf96a48cf1f00a7cd68a
cerf@kraal.fr <cerf@kraal.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-02-19 04:39:40 +0000

    [Salvatore] [source:_plugins_/crayons/lang/ paquet-crayons] Export depuis http://trad.spip.net de la langue fr_tu
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@80880 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ crayons/lang/paquet-crayons_fr_tu.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-crayons?lang_cible=fr_tu
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'crayons_description' => 'Édition rapide depuis les pages publiques du site.',
+	'crayons_nom' => 'Crayons',
+	'crayons_slogan' => 'Édition rapide depuis les pages publiques du site'
+);
+
+?>
