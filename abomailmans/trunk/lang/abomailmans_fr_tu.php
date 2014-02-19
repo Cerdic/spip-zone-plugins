548dfb9baf1c3a14b2159a839bb1d66cfe288ea9
cerf@kraal.fr <cerf@kraal.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-02-19 04:39:13 +0000

    [Salvatore] [source:_plugins_/abomailmans/trunk/lang/ abomailmans] Export depuis http://trad.spip.net de la langue fr_tu
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@80870 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ abomailmans/trunk/lang/abomailmans_fr_tu.php
@@ -0,0 +1,140 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/abomailmans?lang_cible=fr_tu
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'abomailman' => 'Liste de diffusion ou discussions',
+	'abomailmans' => 'Listes de diffusion ou discussions',
+	'abonne' => 'Je m’abonne',
+	'activation' => 'Activation',
+	'active' => 'Active',
+	'aucune_langue' => 'Aucune spécifique',
+
+	// B
+	'bouton_listes_diffusion' => 'Les listes de diffusion',
+	'btn_abonnement' => 'S’abonner',
+	'btn_desabonnement' => 'Se désabonner',
+
+	// C
+	'choisir_liste' => 'Tu dois choisir une liste.',
+	'contenu_date' => 'Contenu à partir de cette date',
+	'creation_droits_insuffisants' => 'Tes droits sont insuffisants...',
+
+	// D
+	'desactive' => 'Désactivée',
+	'destinataire' => 'Destinataire',
+
+	// E
+	'email' => 'E-mail',
+	'email_abonnement' => 'Ton adresse e-mail',
+	'email_envoye' => 'L’e-mail a été envoyé à la liste de diffusion : @liste@.',
+	'email_oublie' => 'Tu as oublié ton adresse e-mail',
+	'emailliste_abomailman' => 'L’adresse e-mail de la liste',
+	'emailliste_abosympa' => 'L’adresse e-mail de l’administrateur Sympa',
+	'emailliste_subscribe' => 'Abonnement',
+	'emailliste_unsubscribe' => 'Désabonnement',
+	'emails_a_renseigner' => 'Emails à renseigner',
+	'envoi_apercu' => 'Aperçu',
+	'envoi_confirmer' => 'Confirmer et envoyer',
+	'envoi_liste_parametres' => 'Liste des paramètres [Facultatif]',
+	'envoi_parametres' => 'Paramètres',
+	'envoi_regulier' => 'Envois automatiques',
+	'envoi_regulier_info' => 'Laisser vide pour ne pas avoir d’envois automatiques',
+	'envoi_regulier_tous_les' => 'Envoi tous les',
+	'envoi_vers' => 'envoi vers',
+	'envoyer_courier' => 'Envoyer un courrier',
+	'envoyer_courier_liste' => 'Envoyer ce courrier à cette liste de diffusion :',
+	'envoyer_mailmans' => 'Sélectionner le modèle et son contenu',
+	'erreur_email_liste_oublie' => 'L’adresse email de la liste est obligatoire',
+	'erreur_nobot' => 'Ton inscription n’ a pu être effectué à cause d’un problème technique',
+	'explication_email_subscribe' => 'Email d’abonnement, de style <code>suffixe+subscribe@exemple.org</code>',
+	'explication_email_sympa' => 'Si ce champ est renseigné, la liste est considérée comme une liste de serveur "Sympa", dans le cas contraire comme une liste "Mailman" ou "ezmlm".',
+	'explication_email_unsubscribe' => 'Email de désabonnement',
+
+	// I
+	'icone_ajouter_liste' => 'Ajouter une nouvelle liste',
+	'icone_envoyer_mail_liste' => 'Envoyer un e-mail aux lettres à partir du contenu de ce site',
+	'icone_modifier_abomailman' => 'Modifier la liste',
+	'icone_retour_abomailman' => 'Retour à la liste',
+	'info_abomailman_aucun' => 'Aucune liste',
+	'info_abomailmans_1' => 'Une liste',
+	'info_abomailmans_nb' => '@nb@ listes',
+	'info_sisympa' => '[Obligatoire si liste Sympa]',
+	'insciption_listes_legende' => 'Abonnement aux listes de diffusion',
+	'inscription_lettres_legende' => 'Abonnement aux listes de diffusion et de discussions',
+
+	// J
+	'je_m_abonne' => 'Cochez pour valider l’abonnement ou le désabonnement.',
+
+	// L
+	'label_etat_liste' => 'État de la liste',
+	'label_type_abo' => 'Type',
+	'label_type_ml' => 'Liste de discussion',
+	'label_type_news' => 'Liste de diffusion',
+	'langue_liste' => 'Langue de la liste',
+	'legende_inscription_ml' => 'Inscription à la liste de discussion',
+	'legende_inscription_news' => 'Inscription à la liste de diffusion',
+	'legende_inscriptions_ml' => 'Inscription aux listes de discussion',
+	'legende_inscriptions_news' => 'Inscription aux listes de diffusion',
+	'les_listes_mailmans' => 'Les listes mailmans, sympa ou ezmlm renseignées',
+	'lire_article' => 'Lire l’article',
+	'liste_creee' => 'La liste numéro @id@ (@titre@) a été créée.',
+	'liste_non_existante' => 'La liste demandée n’existe pas ou a été supprimée',
+	'liste_oublie' => 'Tu as oublié de cocher une liste !',
+	'liste_supprimee' => 'La liste numéro @id@ (@titre@) a été supprimée.',
+	'liste_updatee' => 'La liste numéro @id@ (@titre@) a été mise à jour.',
+
+	// M
+	'message' => 'Introduction à ton courrier, avant le contenu issu du site',
+	'message_confirm_suite' => 'Pour valider votre demande, réponds à la demande de confirmation que tu vas recevoir par mail.',
+	'message_confirmation_a' => 'Une demande d’abonnement aux listes suivantes vient d’être envoyée :',
+	'message_confirmation_d' => 'Une demande de désabonnement aux listes suivantes vient d’être envoyée. ',
+	'message_confirmation_unique_a' => 'Une demande d’abonnement à la liste suivante vient d’être envoyée :',
+	'message_confirmation_unique_d' => 'Une demande de désabonnement à la liste suivante vient d’être envoyée. ',
+	'mot' => 'Et lister les articles du mot clé',
+
+	// N
+	'nom' => 'Nom et prénom (facultatif)',
+	'nouveau_abomailman' => 'Nouvelle liste de diffusion',
+
+	// P
+	'pas_template_txt' => 'Il n’y a pas de version texte pour ce modèle',
+	'periodicite' => ' jours.',
+	'prenom' => 'Prénom',
+	'previsu_html' => 'html',
+	'previsu_txt' => 'texte',
+
+	// R
+	'rubrique' => 'Et lister les articles de la rubrique',
+
+	// S
+	'souhaite_rester' => 'Je souhaite rester informé-e',
+	'sujet' => 'Sujet du courrier',
+	'sujet_obligatoire' => 'Le sujet est obligatoire.',
+	'suppression_definitive' => 'Suppression définitive !',
+	'supprimer' => 'Supprimer',
+	'sympa_message_confirmation' => 'Un email de validation a été envoyé à l’adresse : ',
+
+	// T
+	'template' => 'Choisissez le modèle et son contenu',
+	'template_defaut' => 'Modèle par défaut',
+	'template_defaut_info' => 'Si le fichier modele_choisi.txt.html existe, la newsletter sera envoyée en mode html + texte. Sinon seule la version html sera expédiée.',
+	'texte_descriptif' => 'Descriptif',
+	'titre_abomailman' => 'Titre de la liste',
+	'titre_liste_obligatoire' => 'Le titre de la liste est obligatoire',
+	'toute_liste' => 'Toutes les listes de diffusion',
+
+	// V
+	'verifier_formulaire' => 'Vérifie le remplissage du formulaire.',
+	'veut_s_abonner' => 'veut s’abonner',
+	'veut_se_desabonner' => 'veut se désabonner',
+	'voir_modele_depuis' => 'Voir un exemple du modèle avec',
+	'votre_email' => 'Ton email'
+);
+
+?>
