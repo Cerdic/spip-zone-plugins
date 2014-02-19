218290fdc2046ef83abcbf921f097b8f7df15bd4
cerf@kraal.fr <cerf@kraal.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-02-19 04:41:11 +0000

    [Salvatore] [source:_plugins_/sedna/trunk/lang/ paquet-sedna] Export depuis http://trad.spip.net de la langue fr_tu
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@80911 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ sedna/trunk/lang/paquet-sedna_fr_tu.php
@@ -0,0 +1,17 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-sedna?lang_cible=fr_tu
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// S
+	'sedna_description' => 'Sedna est un agrégateur de flux RSS (et ATOM etc) basé sur les sites syndiqués de SPIP. Il s’inspire vaguement de « Safari RSS » pour la présentation.
+
+Son objectif est d’être et de rester simple dans son code, et rapide à charger. Esthétique un peu, aussi.',
+	'sedna_slogan' => 'Agrégateur RSS'
+);
+
+?>
