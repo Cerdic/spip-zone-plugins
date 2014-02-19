21091b53dbcfcd36b6a22644a5a6ec34a8b64060
cerf@kraal.fr <cerf@kraal.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-02-19 04:40:25 +0000

    [Salvatore] [source:_plugins_/palette/trunk/lang/ paquet-palette] Export depuis http://trad.spip.net de la langue fr_tu
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@80896 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ palette/trunk/lang/paquet-palette_fr_tu.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-palette?lang_cible=fr_tu
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// P
+	'palette_description' => 'Donne la classe palette à un input pour lui ajouter un sélecteur de couleurs : <code><input class="palette" name="couleur" value="#dfdfdf" /></code>',
+	'palette_slogan' => 'Sélecteur de couleurs'
+);
+
+?>
