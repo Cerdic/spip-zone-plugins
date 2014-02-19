82470470673875f4e406ea344e5fbb01c0685c81
cerf@kraal.fr <cerf@kraal.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-02-19 04:40:38 +0000

    [Salvatore] [source:_plugins_/saveauto/trunk/lang/ paquet-saveauto] Export depuis http://trad.spip.net de la langue fr_tu
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@80899 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ saveauto/trunk/lang/paquet-saveauto_fr_tu.php
@@ -0,0 +1,23 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-saveauto?lang_cible=fr_tu
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// S
+	'saveauto_description' => 'Permet de réaliser une sauvegarde MySQL de toute la base de données utilisée par SPIP.
+			Le fichier .zip (ou .sql) obtenu est stocké dans un répertoire (par défaut /tmp/dump, configurable)
+			et peut être envoyé par mail.
+
+			Les sauvegardes stockées considérées comme obsolètes (en fonction du paramètre de config correspondant)
+			sont automatiquement détruites.
+
+			Une interface permet de déclencher manuellement les sauvegardes et de gérer les fichiers générés',
+	'saveauto_nom' => 'Sauvegarde automatique',
+	'saveauto_slogan' => 'Sauvegarde MySQL automatique de la base de données de SPIP'
+);
+
+?>
