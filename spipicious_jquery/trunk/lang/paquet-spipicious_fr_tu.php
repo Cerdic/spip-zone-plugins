746ebbd55a58ab460c2c3c0f420f838552b7e651
cerf@kraal.fr <cerf@kraal.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-02-19 04:40:44 +0000

    [Salvatore] [source:_plugins_/spipicious_jquery/trunk/lang/ paquet-spipicious] Export depuis http://trad.spip.net de la langue fr_tu
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@80901 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ spipicious_jquery/trunk/lang/paquet-spipicious_fr_tu.php
@@ -0,0 +1,17 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-spipicious?lang_cible=fr_tu
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// S
+	'spipicious_description' => 'Permet aux visiteurs authentifiés d’ajouter des tags (mots-clés) aux différents objets.
+_ Les mots-clés sont ajoutés dans un groupe configurable (par défaut ’{{- tags -}}’)
+_ Icone de [Pawel Kadysz->http://oneseventyseven.com/]',
+	'spipicious_slogan' => 'Tagger tous les objets'
+);
+
+?>
