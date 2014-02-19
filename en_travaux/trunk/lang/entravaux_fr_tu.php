10651d2dc3fde7968b7aae6022b5f63ffaf6e11a
cerf@kraal.fr <cerf@kraal.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-02-19 04:39:45 +0000

    [Salvatore] [source:_plugins_/en_travaux/trunk/lang/ entravaux] Export depuis http://trad.spip.net de la langue fr_tu
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@80881 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ en_travaux/trunk/lang/entravaux_fr_tu.php
@@ -0,0 +1,37 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/entravaux-193?lang_cible=fr_tu
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'activer_message' => 'Activer la maintenance',
+
+	// E
+	'en_travaux' => 'En travaux',
+	'erreur_droit' => 'Tu dois être Webmestre pour utiliser le plugin En Travaux !',
+
+	// I
+	'info_disallow_robot' => 'Ton site est protégé contre les robots et moteur de recherche pendant la maintenance.',
+	'info_maintenance_en_cours' => 'Le site est en maintenance ! Seuls les webmestres du site peuvent voir le site public et accèder à l’espace privé.',
+	'info_message' => 'Tu peux activer la maintenance du site et configurer un message temporaire sur toute les pages du site pendant une phase de maintenance.',
+	'info_travaux_texte' => 'Ce site est actuellement en travaux.
+_ Reviens plus tard.',
+
+	// L
+	'label_disallow_robots' => 'Protéger le site contre les robots et moteur de recherche',
+
+	// M
+	'message_temporaire' => 'Ton message temporaire :',
+
+	// P
+	'parametrage_page_travaux' => 'Paramétrage de la page temporaire',
+
+	// T
+	'texte_lien_publier' => 'Rendre le site public'
+);
+
+?>
