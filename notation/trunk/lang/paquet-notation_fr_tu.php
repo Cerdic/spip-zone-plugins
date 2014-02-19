e0e39fad4f08587de039f50f5e8e2ef657b2a561
cerf@kraal.fr <cerf@kraal.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-02-19 04:40:21 +0000

    [Salvatore] [source:_plugins_/notation/trunk/lang/ paquet-notation] Export depuis http://trad.spip.net de la langue fr_tu
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@80895 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ notation/trunk/lang/paquet-notation_fr_tu.php
@@ -0,0 +1,18 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-notation?lang_cible=fr_tu
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// N
+	'notation_description' => 'Définition de la balise <code>#FORMULAIRE_NOTATION</code> permettant la saisie et l’affichage de la note d’un élément de boucle.
+_ Définition des modèles <code><autxx|notation|id_article=yy></code> qui donne la note de l’auteur <i>xx</i> pour l’article <i>yy</i>
+et <code><notation|topten></code> qui donne les 10 meilleurs articles.',
+	'notation_nom' => 'Notation',
+	'notation_slogan' => 'Attribuer une note aux éléments de SPIP'
+);
+
+?>
