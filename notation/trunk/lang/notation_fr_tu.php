022224213e8ce165e86d60eace51b51a3236b980
cerf@kraal.fr <cerf@kraal.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-02-19 04:40:19 +0000

    [Salvatore] [source:_plugins_/notation/trunk/lang/ notation] Export depuis http://trad.spip.net de la langue fr_tu
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@80894 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ notation/trunk/lang/notation_fr_tu.php
@@ -0,0 +1,122 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/notation?lang_cible=fr_tu
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'acces' => 'Accessibilité',
+	'afficher_tables' => 'Afficher les notes',
+	'aide' => 'Aide',
+	'articles' => 'Articles',
+	'auteur' => 'Auteur',
+
+	// B
+	'bouton_radio_fermee' => 'Fermée',
+	'bouton_radio_ouvert' => 'Ouverte',
+	'bouton_voter' => 'Voter',
+
+	// C
+	'change_note_label' => 'Autoriser les votants à modifier leur note',
+	'configuration_notation' => 'Configurer les notations',
+	'creation' => 'Création des tables',
+	'creation_des_tables_mysql' => 'Création des tables',
+	'cree' => 'Tables crées',
+	'creer_tables' => 'Créer les tables',
+
+	// D
+	'date' => 'date',
+	'derniers_votes' => 'Derniers votes',
+	'destruction' => 'Destruction des tables',
+	'detruire' => '<strong style="color:red">Attention, cette commande va détruire les tables du plugin !</strong><br />Tu ne dois l’utiliser que si tu veux désactiver le plugin...',
+	'detruit' => 'Tables détruites...',
+
+	// E
+	'effacer_tables' => 'Effacer les tables',
+	'err_balise' => '[ NOTATION_ERR : balise en dehors d’un article ]',
+	'err_db_notation' => '[ NOTATION ERREUR : une seule notation par article ]',
+	'exemple' => 'Distribution des notes (note = 5, facteur de pondération = @ponderation@) : ',
+	'explication_accepter_note' => 'Si "fermée", la notation sera activable au cas par cas sur les objets ayant cette fonctionnalité.',
+
+	// I
+	'info_acces' => 'Ouvrir le vote : ',
+	'info_etoiles' => 'Ce paramètre te permet de modifier la valeur maximale de la note (le nombre d’étoiles, entre 1 et 10, et 5 par défaut).<br />
+                    <strong style="color:red">/ !\\ Attention</strong> : tu ne dois pas toucher à ce paramètre une fois la notation engagée car les notes ne seront pas recalculées et cela peut provoquer des incohérences dans la notation...<br />
+                    Ce paramètre doit être fixé une fois pour toute à la création des notes.',
+	'info_fonctionnement_note' => 'Fonctionnement de la notation',
+	'info_ip' => 'Pour être le plus facile possible d’utilisation, la note est fixée sur l’adresse IP du votant, ce qui évite deux votes successifs dans la base, avec quelques inconvénients... en particulier si tu gères des votes d’auteurs.<br />
+                Dans ce cas, on fixe la note sur l’identifiant de l’utilisateur (quand celui-ci est enregistré, bien sûr).<br />
+                Si tu veux garantir l’unicité de la note, limite le vote aux <b>seules</b> personnes enregistrées (ci-dessus).',
+	'info_modifications' => 'Modifications des notes',
+	'info_ponderation' => 'Le facteur de pondération permet d’accorder plus de valeur aux articles ayant reçu suffisamment de votes. <br /> Entre ci-dessous le nombre de votes au delà duquel tu penses que la note est fiable.',
+	'ip' => 'IP',
+	'item_adm' => 'aux administrateurs ',
+	'item_all' => 'à tous ',
+	'item_aut' => 'aux auteurs ',
+	'item_id' => 'un vote par utilisateur ',
+	'item_ide' => 'aux personnes enregistrées ',
+	'item_ip' => 'un vote par IP',
+
+	// J
+	'jaidonnemonavis' => 'J’ai donné mon avis !',
+	'jaime' => 'J’aime',
+	'jaimepas' => 'Je n’aime pas',
+	'jaimeplus' => 'Je n’aime plus',
+	'jechangedavis' => 'Je retire mon avis',
+
+	// L
+	'label_accepter_note' => 'Statut de la notation sur tous les objets',
+
+	// M
+	'moyenne' => 'Moyenne',
+	'moyennep' => 'Moyenne ponderée',
+
+	// N
+	'nb_etoiles' => 'Valeur des notes',
+	'nbobjets_note' => 'Nombre d’objets ayant une note : ',
+	'nbvotes' => 'Nombre de votes',
+	'nbvotes_moyen' => 'Nombre de votes moyens par objet : ',
+	'nbvotes_total' => 'Nombre de votes total sur le site : ',
+	'notation' => 'Notations',
+	'note' => 'Note : ',
+	'note_1' => 'Note : 1',
+	'note_10' => 'Note : 10',
+	'note_2' => 'Note : 2',
+	'note_3' => 'Note : 3',
+	'note_4' => 'Note : 4',
+	'note_5' => 'Note : 5',
+	'note_6' => 'Note : 6',
+	'note_7' => 'Note : 7',
+	'note_8' => 'Note : 8',
+	'note_9' => 'Note : 9',
+	'note_pond' => 'Notes ponderées',
+	'notes' => 'Notes',
+
+	// O
+	'objets' => 'Objets',
+
+	// P
+	'param' => 'Paramétrage',
+	'ponderation' => 'Pondération de la note',
+
+	// T
+	'titre_ip' => 'Mode de fonctionnement :',
+	'topnb' => 'Les 10 objets les plus notés',
+	'topten' => 'Les 10 meilleures notes',
+	'toptenp' => 'Les 10 meilleures notes (pondérées)',
+	'totaux' => 'Totaux',
+
+	// V
+	'valeur_nb_etoiles' => 'Notation de 1 à ',
+	'valeur_ponderation' => 'Facteur de pondération',
+	'vos_notes' => 'Vos 5 meilleurs notes',
+	'vote' => 'vote',
+	'voter' => 'Voter : ',
+	'votes' => 'votes',
+	'votre_note' => 'Ta note'
+);
+
+?>
