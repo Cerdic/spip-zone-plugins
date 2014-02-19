807cd0039c062891f7e02d09128cd12f38bb818c
cerf@kraal.fr <cerf@kraal.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-02-19 04:40:50 +0000

    [Salvatore] [source:_plugins_/thumbsites/trunk/lang/ paquet-thumbsites] Export depuis http://trad.spip.net de la langue fr_tu
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@80903 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ thumbsites/trunk/lang/paquet-thumbsites_fr_tu.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-thumbsites?lang_cible=fr_tu
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// T
+	'thumbsites_description' => 'Thumbsites fournit un système de capture d’écran de site générée par des serveurs dédiés. Le plugin propose des balises, des filtres et des modèles pour afficher la vignette d’un site donné. Ce plugin gère aussi un cache qui permet de pallier les indisponibilités fréquentes des serveurs et d’accélérer les affichages. Une configuration est disponible dans l’espace privé.',
+	'thumbsites_slogan' => 'Une vignette pour tes sites référencés'
+);
+
+?>
