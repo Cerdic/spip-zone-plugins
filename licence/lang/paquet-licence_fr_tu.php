5bc8ca2eab9dba121a974b8bcece2840a43f58d5
cerf@kraal.fr <cerf@kraal.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-02-19 04:40:06 +0000

    [Salvatore] [source:_plugins_/licence/lang/ paquet-licence] Export depuis http://trad.spip.net de la langue fr_tu
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@80889 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ licence/lang/paquet-licence_fr_tu.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-licence?lang_cible=fr_tu
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// L
+	'licence_description' => 'Permet de relier une licence d’utilisation à un article ou un document',
+	'licence_nom' => 'Licence',
+	'licence_slogan' => 'Une Licence pour articles et documents'
+);
+
+?>
