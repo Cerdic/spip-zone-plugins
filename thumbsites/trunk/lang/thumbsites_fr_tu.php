f7120e7fd486d8c12107772843b486869f95740e
cerf@kraal.fr <cerf@kraal.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-02-19 04:40:48 +0000

    [Salvatore] [source:_plugins_/thumbsites/trunk/lang/ thumbsites] Export depuis http://trad.spip.net de la langue fr_tu
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@80902 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- thumbsites/trunk/lang/thumbsites_fr_tu.php
+++ thumbsites/trunk/lang/thumbsites_fr_tu.php
@@ -7,16 +7,31 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
+	// B
+	'bouton_copier_comme_logo' => 'Définir comme logo',
+	'bouton_rafraichir' => 'Rafraichir la vignette',
+
 	// C
 	'cfg_descriptif' => 'Cette page te permet de configurer le plugin Thumbsites, et, en particulier, de choisir le serveur de vignettes. Pour en savoir plus, consultez la <a href="http://contrib.spip.net/?article2584">documentation sur contrib</a>',
+	'cfg_inf_apercite_presentation' => '<a href="http://www.apercite.fr">apercite.fr</a> propose un service gratuit qui ne nécessite pas d’identification.',
+	'cfg_inf_choix_serveur' => 'Choisis le serveur qui fournira les vignettes de tes sites référencés et compléter éventuellement son paramétrage.',
+	'cfg_inf_miwim_presentation' => 'Pour utiliser ce service tu dois mettre un lien vers le site <a href="http://thumbs.miwim.fr">Miwim</a>. La présence de ce lien est vérifié périodiquement par un script.',
 	'cfg_inf_rotothumb_presentation' => 'Pour utiliser ce service tu ne dois pas vous enregistrer mais tu dois mettre un lien vers le site <a href="http://www.robothumb.com">Robothumb</a>. La présence de ce lien est vérifiée périodiquement par leur soin.',
 	'cfg_inf_websnapr_presentation' => 'Pour utiliser ce service tu dois être inscrit sur le site <a href="http://www.websnapr.com">Websnapr</a>. La version gratuite permet de traiter de 250.000 captures de site par mois, à un rythme de 80 captures par heure.',
+	'cfg_itm_serveur_apercite' => 'Apercite.fr',
+	'cfg_itm_serveur_miwim' => 'Miwim',
 	'cfg_itm_serveur_robothumb' => 'Robothumb',
 	'cfg_itm_serveur_websnapr' => 'Websnapr',
 	'cfg_itm_taille_vignette' => '@taille@ pixels',
 	'cfg_lbl_cle' => 'Ta clé',
+	'cfg_lbl_duree_cache' => 'Durée du cache (en jours)',
+	'cfg_lbl_serveur' => 'Serveur',
 	'cfg_lbl_taille_vignette' => 'Taille des vignettes',
-	'cfg_lgd_choix_serveur' => 'Serveur', # MODIF
+	'cfg_lbl_usage' => 'Conditions d’utilisation',
+	'cfg_lgd_cache' => 'Cache',
+	'cfg_lgd_choix_serveur' => 'Service',
+	'cfg_titre_form' => 'Configurer Thumbsites',
+	'cfg_titre_page' => 'Thumbsites',
 
 	// T
 	'titre_thumbshot_site' => 'VIGNETTE DU SITE'
