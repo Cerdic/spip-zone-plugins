53f9f09c110d721dfc761131a1c92a00399ed7b7
cerf@kraal.fr <cerf@kraal.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-02-19 04:40:08 +0000

    [Salvatore] [source:_plugins_/liens_contenus/trunk/lang/ lienscontenus] Export depuis http://trad.spip.net de la langue fr_tu
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@80890 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- liens_contenus/trunk/lang/lienscontenus_fr_tu.php
+++ liens_contenus/trunk/lang/lienscontenus_fr_tu.php
@@ -9,30 +9,51 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
 	'alerte_publie_contenant' => 'Attention, ce contenu est publié, mais contient des liens vers des contenus qui ne le sont pas !',
+	'alerte_publie_contenant_ko' => 'Attention, ce contenu est publié, mais contient des liens vers des contenus qui n’existent pas !',
 	'aucun_objets_avec_lien_depuis_courant' => 'Ce contenu ne contient aucun lien vers un autre contenu.',
 	'aucun_objets_avec_lien_vers_courant' => 'Aucun autre contenu ne contient de lien vers celui-ci.',
 
 	// C
-	'confirmation_depublication' => 'ttention, un contenu publié pointe vers celui-ci, et sera affecté si tu le dépublies !nnVeux-tu vraiment changer le statut ?', # MODIF
-	'confirmation_publication' => 'Attention, un contenu vers lequel pointe celui-ci n’est pas publié !nnVeux-tu vraiment changer le statut ?', # MODIF
-	'confirmation_suppression' => 'Attention, un contenu publié pointe vers celui-ci, et sera affecté si tu le supprimes !nnVeux-tu vraiment le supprimer ?', # MODIF
+	'confirmation_depublication' => 'Attention, des liens internes pointent vers ce contenu !\\n\\nSon changement de statut aurait des répercussions sur le fonctionnement du site.\\n\\nVeux-tu vraiment changer le statut ?',
+	'confirmation_publication' => 'Attention, ce contenu contient un lien pointant vers un contenu qui n’est pas publié !\\n\\nSon changement de statut aurait des répercussions sur le fonctionnement du site.\\n\\nVeux-tu vraiment changer le statut ?',
+	'confirmation_suppression' => 'Attention, des liens internes pointent vers ce contenu !\\n\\nSa suppression aurait des répercussions sur le fonctionnement du site.\\n\\nVeux-tu vraiment le supprimer ?',
 
 	// I
-	'information_element_contenu' => 'Attention, un autre contenu pointe vers celui-ci !', # MODIF
+	'inexistant' => 'inexistant (@id_objet@)',
+	'information_element_contenu' => 'Attention, des liens internes pointent vers ce contenu !',
 
 	// L
 	'legende_liens_faux_objets' => 'Les liens en rouge et barrés indiquent des contenus liés qui n’existent pas.',
+	'liens_entre_contenus' => 'Liens entre contenus',
 
 	// O
-	'objets_avec_liens_depuis_courant' => 'Ce contenu contient des liens vers ceux-ci :', # MODIF
-	'objets_avec_liens_vers_courant' => 'Ces contenus contiennent des liens vers celui-ci :', # MODIF
+	'objets_avec_liens_depuis_courant' => 'Ce contenu contient des liens vers ces autres :',
+	'objets_avec_liens_vers_courant' => 'Ces autres contenus contiennent des liens vers celui-ci :',
 
 	// S
 	'statut_poubelle' => 'À la poubelle',
 	'statut_prepa' => 'En préparation',
 	'statut_prop' => 'Proposé',
 	'statut_publie' => 'Publié',
-	'statut_refuse' => 'Refusé'
+	'statut_refuse' => 'Refusé',
+
+	// T
+	'type_article' => '@titre@ (@id_objet@)',
+	'type_article_inexistant' => 'Article inexistant (@id_objet@)',
+	'type_auteur' => '@titre@',
+	'type_auteur_inexistant' => 'Auteur inexistant (@id_objet@)',
+	'type_breve' => '@titre@ (@id_objet@)',
+	'type_breve_inexistant' => 'Brève inexistante (@id_objet@)',
+	'type_document' => '@titre@ (@id_objet@)',
+	'type_document_inexistant' => 'Document inexistant (@id_objet@)',
+	'type_forum' => '@titre@ (@id_objet@)',
+	'type_forum_inexistant' => 'Message inexistant (@id_objet@)',
+	'type_modele' => 'Modèle "@id_objet@"',
+	'type_modele_inexistant' => 'Modèle inexistant (@id_objet@)',
+	'type_rubrique' => '@titre@ (@id_objet@)',
+	'type_rubrique_inexistant' => 'Rubrique inexistante (@id_objet@)',
+	'type_syndic' => '@titre@ (@id_objet@)',
+	'type_syndic_inexistant' => 'Site inexistant (@id_objet@)'
 );
 
 ?>
