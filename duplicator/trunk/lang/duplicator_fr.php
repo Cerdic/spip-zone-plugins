802b497987aae97d687a900330fec87ff3342b7e
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-02-20 11:07:37 +0000

    Un bouton pour ne dupliquer que l'arborescence
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@80952 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- duplicator/trunk/lang/duplicator_fr.php
+++ duplicator/trunk/lang/duplicator_fr.php
@@ -3,33 +3,37 @@
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// A
+	// A
 	'aide_choix_champs' => 'Liste des noms des champs SQL séparés par des ","<br/><em>Laisser vide pour sélectionner tous les champs principaux.</em>',
 	'action_dupliquer_article' => "Dupliquer cet article",
 	'action_dupliquer_rubrique' => "Dupliquer cette rubrique",
+	'action_dupliquer_rubrique_arbo' => 'Dupliquer l\'arborescence de la rubrique',
+	
+	// B
+	'bouton_confirmer' => 'Confirmer',
 
-// C
+	// C
 	'choix_type' => 'Sélectionner le ou les mode(s) de duplication',
 	'choix_champs' => 'Choisir les champs à dupliquer',
 
 	'configuration_duplicator' => 'Configuration de Duplicator',
 
-// D
+	// D
 	'dupli_art' => "Duplication sur les articles",
 	'dupli_rub' => "Duplication sur les rubriques",
 
-// I
+	// I
 	'icone_dupliquer' => 'Dupliquer la rubrique',
 
-// L
+	// L
 	'label_art_champs' => 'Liste des champs à dupliquer pour chaque article :',
 	'label_rub_champs' => 'Liste des champs à dupliquer pour chaque rubrique :',
 
-
-// M
+	// M
 	'message_annuler' => 'Annuler',
 	'message_avertissement_article' => 'Êtes-vous sûr de vouloir dupliquer cet article ?',
 	'message_avertissement_rubrique' => 'Êtes-vous sûr de vouloir dupliquer tout le contenu de cette rubrique ?',
+	'message_avertissement_rubrique_arbo' => 'Êtes-vous sûr de vouloir dupliquer seulement l\'arborescence de cette rubrique ?',
 	'message_confirmer' => 'Confirmer',	
 	
 	'operation_executee' => "L'opération a bien été exécutée.",
