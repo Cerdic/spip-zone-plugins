c5a3df2d9e9fd6e57c285b17498331c4247156c3
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-02-22 04:39:02 +0000

    [Salvatore] [source:_plugins_/menu_langues_liens/lang/ mll] Export depuis http://trad.spip.net de la langue en
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@80976 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- menu_langues_liens/lang/mll_en.php
+++ menu_langues_liens/lang/mll_en.php
@@ -12,15 +12,17 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// E
 	'explication_langues_invisibles' => 'Allows to remove some languages ​​from the menu',
+	'explication_separateur' => 'Text used to divide the languages in the menu',
 
 	// L
 	'label_format' => 'Languages display format',
 	'label_format_abrege' => 'Shortened (for instance, Fr)',
 	'label_format_complet' => 'Complete (for instance, Français)',
 	'label_format_liste' => 'Languages list display format',
-	'label_format_liste_bloc' => 'A block divided by spaces', # MODIF
+	'label_format_liste_bloc' => 'A block divided by spaces',
 	'label_format_liste_liste' => 'A bulleted list',
-	'label_langues_invisibles' => 'Invisible languages in the​​ menu'
+	'label_langues_invisibles' => 'Invisible languages in the​​ menu',
+	'label_separateur' => 'Language divider'
 );
 
 ?>
