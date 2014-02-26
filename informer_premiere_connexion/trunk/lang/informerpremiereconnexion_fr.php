180fc9df5c700f4d5580e819ef7cd234530be79b
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-02-26 04:36:59 +0000

    [Salvatore] [source:_plugins_/informer_premiere_connexion/trunk/lang/ informerpremiereconnexion] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@81057 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- informer_premiere_connexion/trunk/lang/informerpremiereconnexion_fr.php
+++ informer_premiere_connexion/trunk/lang/informerpremiereconnexion_fr.php
@@ -1,10 +1,16 @@
 <?php
-
-// Sécurité
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/informer_premiere_connexion/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
 	'configuration_texte_label' => 'Message à afficher à l’utilisateur⋅trice qui vient de confirmer son inscription',
 	'configuration_titre' => 'Configurer le message de première connexion',
-	'texte_defaut' => 'Votre compte utilisateur a bien été créé !',
+
+	// T
+	'texte_defaut' => 'Votre compte utilisateur a bien été créé !'
 );
+
+?>
