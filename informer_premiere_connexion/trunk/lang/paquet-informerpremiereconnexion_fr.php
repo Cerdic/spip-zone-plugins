d06b728a051e10326a6459c71e9e23e7343d6ac9
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-02-26 04:37:01 +0000

    [Salvatore] [source:_plugins_/informer_premiere_connexion/trunk/lang/ paquet-informerpremiereconnexion] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@81058 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- informer_premiere_connexion/trunk/lang/paquet-informerpremiereconnexion_fr.php
+++ informer_premiere_connexion/trunk/lang/paquet-informerpremiereconnexion_fr.php
@@ -1,9 +1,11 @@
 <?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/informer_premiere_connexion/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-	// A
+	// I
 	'informerpremiereconnexion_description' => 'Lors de la toute première connexion après inscription, ce plugin ajoute un message en haut de la page, avec un texte explicatif que les admins du site peuvent modifier. Si javascript est présent, on peut fermer le bloc contenant le message.',
 	'informerpremiereconnexion_nom' => 'Informer à la première connexion',
 	'informerpremiereconnexion_slogan' => 'Afficher un message explicatif lors de la première connexion'
