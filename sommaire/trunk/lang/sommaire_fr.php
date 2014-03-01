fa2d11856502b221c9e768838dbaa597e19787ae
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-03-01 09:19:01 +0000

    Ajout d'une configuration permettant d'afficher le sommaire comme une liste à puces ou une liste ordonnée.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@81126 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- sommaire/trunk/lang/sommaire_fr.php
+++ sommaire/trunk/lang/sommaire_fr.php
@@ -20,6 +20,10 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'label_sommaire_automatique_ondemand' => 'Insertion dans le texte des articles à la demande',
 	'explication_sommaire_automatique_ondemand' => 'Le sommaire est inséré uniquement en présence du raccourci <tt>&lt;sommaire&gt;</tt> dans le texte des articles.',
 	'label_sommaire_automatique_on' => 'Insertion automatique sur tous les articles',
+	'explication_numerotation_sommaire' => 'Le sommaire peut être présenté comme une liste à puces ou une liste numérotée.',
+	'label_sommaire_automatique_numerote' => 'Type de liste pour le sommaire',
+	'label_sommaire_automatique_numerote_off' => 'liste à puces',
+	'label_sommaire_automatique_numerote_on' => 'liste numérotée',
 
 	// T
 	'titre_page_configurer_sommaire' => 'Sommaire automatique',
