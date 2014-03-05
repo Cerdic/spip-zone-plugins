7e9667b3a684a9490446427337ab2be005b0e9d4
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-03-05 16:31:02 +0000

    [Salvatore] [source:_plugins_/informer_premiere_connexion/trunk/lang/ informerpremiereconnexion] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@81205 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ informer_premiere_connexion/trunk/lang/informerpremiereconnexion_nl.php
@@ -0,0 +1,18 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/informerpremiereconnexion?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'configuration_texte_label' => 'Te tonen bericht bij een inschrijvingsbevestiging van een nieuwe gebruik(ste)er',
+	'configuration_titre' => 'Het bericht bij een eerste verbinding configureren',
+
+	// T
+	'texte_defaut' => 'Je account is klaar voor gebruik!'
+);
+
+?>
