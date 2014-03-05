b7e3cf7a211bdf671ccd7ee4d6ee8c739adf7a56
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-03-05 16:30:56 +0000

    [Salvatore] [source:_plugins_/http/trunk/lang/ http] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@81203 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ http/trunk/lang/http_nl.php
@@ -0,0 +1,18 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/http?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// E
+	'erreur_401_message' => 'U heeft geen recht van toegang tot deze gegevens.',
+	'erreur_404_message' => 'U heeft om gegevens gevraagd die niet bestaan of niet konden worden gevonden.',
+	'erreur_404_titre' => 'De gegevens werden niet gevonden',
+	'erreur_415_message' => 'Uw verzoek is in een voor de server onbekend formaat.',
+	'erreur_415_titre' => 'Onbekend formaat'
+);
+
+?>
