fee43fb894fbaec7a1421b56c5f26da24b4a67f9
p@henix.be <p@henix.be@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-03-07 18:28:15 +0000

    Commentaire et description des fonctions
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@81262 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- foundation-4-spip/trunk/foundation_fonctions.php
+++ foundation-4-spip/trunk/foundation_fonctions.php
@@ -10,7 +10,13 @@
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 
-// Function de callback
+/**
+ * Fonction de callback
+ * A chaque iFrame, on encadre de div.flex-video.
+ * Si vimeo est détecté, on ajoute la class vimeo
+ * @param  string $matches iframe
+ * @return string          iframe encadrée
+ */
 function responsive($matches) {
     // Dans le cas de vimeo, il faut ajouter une classe
     if (strpos($matches[0], 'vimeo')) $vimeo = ' vimeo';
@@ -20,19 +26,23 @@ function responsive($matches) {
     return '<div class="flex-video'.$vimeo.'"><iframe '.$matches[0].'></iframe></div>';
 }
 
-/*
-*   Rendre les iframes responsive via un filtre et la classe flex-video de Foundation.
-*/
+/**
+ * Rendre les iframes responsive via un filtre et la classe flex-video de Foundation.
+ * @param  string $texte HTML pouvant contenir des iFrames
+ * @return string        HTML avec les iFrames modifiée pour être responsive.
+ */
 function iframe_responsive($texte) {
     // On détecte tout les iFrames et on les rends responsives.
     return preg_replace_callback('/<iframe(.+)><\/iframe>/', 'responsive', $texte);
 }
 
-/*
-*   Pas très élégant, cette fonction va renvoyer le nombre en toutes lettre.
-*   Dans ce cas si, cela suffit largement puisqu'il n'y a que 12 chiffres possible.
-*   C'est utiliser pour foundation 2 et 3.
-*/
+/**
+ * Pas très élégant, cette fonction va renvoyer le nombre en toutes lettre.
+ *  Dans ce cas si, cela suffit largement puisqu'il n'y a que 12 chiffres possible.
+ *  C'est utiliser pour foundation 2 et 3.
+ * @param  int $number nombre de colonne foundation.
+ * @return string         string définissant la largeur de la colonne foundation.
+ */
 function toWords($number) {
 
     // Ce tableau fait office de table de conversion.
@@ -57,9 +67,12 @@ function toWords($number) {
 }
 
 
-/*
-*   Cette fonction va lire la configuration de foundation et determiner quel syntaxe doit être utilisé.
-*/
+/**
+ * Cette fonction va lire la configuration de foundation et determiner quel syntaxe doit être utilisé.
+ * @param  int $nombre_colonnes Nombre de colonne désiré
+ * @param  string $type            Foundation 4/5, type de colonne (large, medium, small)
+ * @return string                  class foundation applicable directement.
+ */
 function trouver_syntaxe_foundation($nombre_colonnes, $type) {
 
     // On récupère la configuration
