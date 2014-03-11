5010546e41186a41051e6065ec277159a5a34bbe
xdjuj@apsulis.com <xdjuj@apsulis.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-03-11 08:00:56 +0000

    Ca ça n'aurait jamais dû partir avec le commit... On le remet.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@81298 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spip_piwik/spip_piwik_2_0/piwik_pipelines.php
+++ spip_piwik/spip_piwik_2_0/piwik_pipelines.php
@@ -122,62 +122,62 @@ function piwik_head_js($options=array()){
  * @return array $flux
  * 		Le contexte du pipeline modifié auquel on a ajouté nos erreurs potentielles
  */
-// function piwik_formulaire_verifier($flux){
-// 	if($flux['args']['form'] == 'configurer_piwik'){
-// 		$obligatoires = array('token','user','urlpiwik');
-// 		foreach($obligatoires as $obligatoire){
-// 			if(!_request($obligatoire)){
-// 				$flux['data'][$obligatoire] = _T('info_obligatoire');
-// 			}
-// 		}
+function piwik_formulaire_verifier($flux){
+	if($flux['args']['form'] == 'configurer_piwik'){
+		$obligatoires = array('token','user','urlpiwik');
+		foreach($obligatoires as $obligatoire){
+			if(!_request($obligatoire)){
+				$flux['data'][$obligatoire] = _T('info_obligatoire');
+			}
+		}
 		
-// 		$piwik_token = _request('token');
-// 		if (!$flux['data']['token'] && !preg_match('/^[a-f0-9]{32}$/i',$piwik_token)) {
-// 			$flux['data']['token'] = _T('piwik:cfg_erreur_token');
-// 			return $flux;
-// 		}
-// 		$piwik_url = 'http://'._request('urlpiwik').'/';
+		$piwik_token = _request('token');
+		if (!$flux['data']['token'] && !preg_match('/^[a-f0-9]{32}$/i',$piwik_token)) {
+			$flux['data']['token'] = _T('piwik:cfg_erreur_token');
+			return $flux;
+		}
+		$piwik_url = 'http://'._request('urlpiwik').'/';
 		
-// 		$piwik_recuperer_data = charger_fonction('piwik_recuperer_data','inc');
+		$piwik_recuperer_data = charger_fonction('piwik_recuperer_data','inc');
 		
-// 		/**
-// 		 * Vérifier la correspondance nom d'utilisateur/ token
-// 		 * Qui nous permettra par la suite de définir d'autres choses
-// 		 */
-// 		$method_verif_user = 'UsersManager.getUser';
-// 		$options_user = array('userLogin'=>_request('user'));
-// 		$datas_user = $piwik_recuperer_data($piwik_url,$piwik_token,'',$method_verif_user,'PHP',$options_user);
-// 		if(is_array($datas_user = unserialize($datas_user))){
-// 			if(!$flux['data']['user'] && $flux['data']['result'] == 'error'){
-// 				$flux['data']['user'] = _T('piwik:cfg_erreur_user_token');
-// 			}
-// 			/**
-// 			 * Vérifier que ce token est un token admin
-// 			 * Si non : mettre une meta comme quoi il n'est pas admin pour créer des sites
-// 			 */
-// 			$method_verif_user_bis = 'UsersManager.getUsers';
-// 			$datas_user_bis = $piwik_recuperer_data($piwik_url,$piwik_token,'',$method_verif_user_bis,'PHP');
-// 			$datas_user_bis = unserialize($datas_user_bis);
-// 			if(is_array($datas_user_bis) && ($datas_user_bis['result'] == 'error')){
-// 				ecrire_meta('piwik_admin', 'non');
-// 			}else{
-// 				ecrire_meta('piwik_admin', 'oui');
-// 				unset($flux['data']['user']);
-// 			}
-// 		}
+		/**
+		 * Vérifier la correspondance nom d'utilisateur/ token
+		 * Qui nous permettra par la suite de définir d'autres choses
+		 */
+		$method_verif_user = 'UsersManager.getUser';
+		$options_user = array('userLogin'=>_request('user'));
+		$datas_user = $piwik_recuperer_data($piwik_url,$piwik_token,'',$method_verif_user,'PHP',$options_user);
+		if(is_array($datas_user = unserialize($datas_user))){
+			if(!$flux['data']['user'] && $flux['data']['result'] == 'error'){
+				$flux['data']['user'] = _T('piwik:cfg_erreur_user_token');
+			}
+			/**
+			 * Vérifier que ce token est un token admin
+			 * Si non : mettre une meta comme quoi il n'est pas admin pour créer des sites
+			 */
+			$method_verif_user_bis = 'UsersManager.getUsers';
+			$datas_user_bis = $piwik_recuperer_data($piwik_url,$piwik_token,'',$method_verif_user_bis,'PHP');
+			$datas_user_bis = unserialize($datas_user_bis);
+			if(is_array($datas_user_bis) && ($datas_user_bis['result'] == 'error')){
+				ecrire_meta('piwik_admin', 'non');
+			}else{
+				ecrire_meta('piwik_admin', 'oui');
+				unset($flux['data']['user']);
+			}
+		}
 	
-// 		/**
-// 		 * Récupération de la liste des sites où cet utilisateur 
-// 		 * a les droits d'admin
-// 		 */
-// 		$method = 'SitesManager.getSitesWithAdminAccess';
-// 		$datas = $piwik_recuperer_data($piwik_url,$piwik_token,'',$method,'PHP');
-// 		if(!$flux['data']['urlpiwik'] && !is_array(unserialize($datas))){
-// 			$flux['data']['urlpiwik'] = _T('piwik:cfg_erreur_recuperation_data');
-// 		}else{
-// 			ecrire_meta('piwik_sites_dispo', $datas);	
-// 		}
-// 	}
-// 	return $flux;
-// }
+		/**
+		 * Récupération de la liste des sites où cet utilisateur 
+		 * a les droits d'admin
+		 */
+		$method = 'SitesManager.getSitesWithAdminAccess';
+		$datas = $piwik_recuperer_data($piwik_url,$piwik_token,'',$method,'PHP');
+		if(!$flux['data']['urlpiwik'] && !is_array(unserialize($datas))){
+			$flux['data']['urlpiwik'] = _T('piwik:cfg_erreur_recuperation_data');
+		}else{
+			ecrire_meta('piwik_sites_dispo', $datas);	
+		}
+	}
+	return $flux;
+}
 ?>
