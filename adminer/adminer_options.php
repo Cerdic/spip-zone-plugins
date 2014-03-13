4669034a720c64b79ce220dfce3d552cac8401c1
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-03-13 18:25:05 +0000

    bugfix de la pagination en mode sqlite, corrections CSS : afficher le bouton deconnexion et masquer le form de changement de base, injecter les CSS minifee avant d'appeler adminer.php pour limiter le FOUC, mais les reinjecter apres aussi pour contrer les css envoyees par adminer.php (oui c'est crade mais bon)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@81356 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- adminer/adminer_options.php
+++ adminer/adminer_options.php
@@ -10,9 +10,14 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 // aiguiller sur adminer si les bonnes conditions
 if (strpos($_SERVER['REQUEST_URI'],"prive.php")!==false AND !_DIR_RESTREINT){
 	if (
-	(($f=_request('file') AND in_array($f,array('default.css','functions.js','favicon.ico')) AND _request('version'))
-	OR ($_COOKIE['spip_adminer']))
-		AND (!_request('page') OR (_request('server') AND _request('username'))))
+	(  ($f=_request('file') AND in_array($f,array('default.css','functions.js','favicon.ico')) AND _request('version'))
+	  OR ($_COOKIE['spip_adminer'])
+	)
+	AND (
+		!_request('page')
+		  OR ((_request('username') OR _request('db')) AND (_request('server') OR _request('sqlite') OR _request('sqlite2')))
+	)
+	)
 		$GLOBALS['fond'] = 'adminer';
 }
 
