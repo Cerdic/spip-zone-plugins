5747e109009042c9284d9cbf54799f543a3157d2
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-03-14 10:11:08 +0000

    Premier packaging de la librairie PHPExcel.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@81370 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ phpexcel/trunk/lang/paquet-phpexcel_fr.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// Q
+	'phpexcel_description' => 'PHPExcel (http://www.phpexcel.net/) est une librairie PHP permettant de lire et d\'écrire des fichiers au format Excel (2007 au maximum) en utilisant toutes les propriétés et les fonctions disponibles (feuille, format de cellule, formule...). Elle nécessite PHP 5.2.9 à minima ainsi que les extensions php_zip, php_xml et php_gd2.
+
+Ce plugin permet d\'utiliser cette librairie dans SPIP.',
+	'phpexcel_slogan' => 'Utiliser la librairie PHPExcel dans SPIP',
+);
+
+?>
