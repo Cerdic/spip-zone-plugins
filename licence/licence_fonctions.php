c56e7322baf5c8cc3045438ee4a87e67072309bf
brunobergot@gmail.com <brunobergot@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-03-14 10:58:03 +0000

    version 0.5.2 : fix filtre licence_affiche
    ne pas générer une balise img avec un src vide si logo_non vaut non
    
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@81372 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- licence/licence_fonctions.php
+++ licence/licence_fonctions.php
@@ -14,6 +14,8 @@ function licence_affiche($id_licence,$logo_non,$lien_non){
 	$licence = $GLOBALS['licence_licences'][$id_licence];
 	if (isset($licence['icon']) AND $logo_non != 'non')
 		$licence['icon'] = "img_pack/".$licence['icon'];
+	else
+		$licence['icon'] = '';
 	if ($lien_non == 'non')
 		$licence['link'] = '';
 	return recuperer_fond('licence/licence',$licence);
