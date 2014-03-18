2186caaffc5e84682362c68b5cb6e44cd7839780
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-03-18 04:40:11 +0000

    [Salvatore] [source:_plugins_/diogene/diogene_complements/diogene_mots/trunk/lang/ diogene_mots] Export depuis http://trad.spip.net de la langue en
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@81429 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- diogene/diogene_complements/diogene_mots/trunk/lang/diogene_mots_en.php
+++ diogene/diogene_complements/diogene_mots/trunk/lang/diogene_mots_en.php
@@ -7,11 +7,17 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
+	// E
+	'erreur_confirmer_creation_mots_nouveaux' => 'Confirm the creation in this group?',
+	'erreur_mot_dans_autre_groupe' => 'Keyword @mot@ already exists in group @groupe@. ',
+
 	// F
 	'form_legend' => 'Keyword selector',
 	'form_legend_public' => 'Keywords',
 
 	// L
+	'label_montrer_titre_et_descriptif' => 'When it exists, show the keyword description between brackets',
+	'label_mots_creer_dans_public' => 'Allow for creation of new keywords in these groups',
 	'label_mots_facultatifs' => 'Optional keywords groups',
 	'label_mots_obligatoires' => 'Required keyword groups'
 );
