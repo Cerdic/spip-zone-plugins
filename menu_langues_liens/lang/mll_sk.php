5ad87083e0b283f4662b370d4c29254242704fdc
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-03-22 04:38:58 +0000

    [Salvatore] [source:_plugins_/menu_langues_liens/lang/ mll] Export depuis http://trad.spip.net de la langue sk
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@81566 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- menu_langues_liens/lang/mll_sk.php
+++ menu_langues_liens/lang/mll_sk.php
@@ -12,15 +12,17 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// E
 	'explication_langues_invisibles' => 'Umožňuje odstrániť z menu niektoré jazyky',
+	'explication_separateur' => 'Text sa používa na odde­lenie jazy­kov v menu',
 
 	// L
 	'label_format' => 'Formát zobrazenia jazykov',
 	'label_format_abrege' => 'Skrátený (napríklad Sk)',
 	'label_format_complet' => 'Úplný (napríklad Slovenčina)',
 	'label_format_liste' => 'Formát zoznamu jazykov',
-	'label_format_liste_bloc' => 'Blok oddelený medzerami', # MODIF
+	'label_format_liste_bloc' => 'Blok oddelený medzerami (nastavený oddeľovač)',
 	'label_format_liste_liste' => 'Zoznam s odrážkami',
-	'label_langues_invisibles' => 'Jazyky, ktoré nebudú zobrazené v menu'
+	'label_langues_invisibles' => 'Jazyky, ktoré nebudú zobrazené v menu',
+	'label_separateur' => 'Oddeľovač jazykov'
 );
 
 ?>
