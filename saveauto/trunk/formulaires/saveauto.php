870de5650ea2c7e44a7e6129ac719a3711c98f20
cy_altern@yahoo.fr <cy_altern@yahoo.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-03-27 21:28:06 +0000

    lister les tables non-spip et récuperer la liste des tables sélectionnées par la configuration dans l'interface de sauvegarde (cf http://contrib.spip.net/Le-plugin-saveauto-sauvegarde-automatique-de-la-base#forum474652)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@81640 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- saveauto/trunk/formulaires/saveauto.php
+++ saveauto/trunk/formulaires/saveauto.php
@@ -73,23 +73,4 @@ function formulaires_saveauto_traiter_dist() {
 	return $retour;
 }
 
-/**
- * Lister les tables non-SPIP de la base
- * @return array
- **/ 
- function saveauto_lister_tables_ext($serveur='') {
-	spip_connect($serveur);
-	$connexion = $GLOBALS['connexions'][$serveur ? $serveur : 0];
-	$prefixe = $connexion['prefixe'];
-
-	$p = '/^' . $prefixe . '/';
-	$res = array();
-	foreach(sql_alltable('%',$serveur) as $t) {
-		if (!preg_match($p, $t)) 
-			$res[]= $t;
-	}
-	sort($res);
-	return $res;
-}
-
 ?>
