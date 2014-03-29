4086be440b104598f25fbb3563cb99100a496149
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-03-29 04:40:09 +0000

    [Salvatore] [source:_plugins_/inserer_modeles/trunk/lang/ paquet-inserer_modeles] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@81656 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- inserer_modeles/trunk/lang/paquet-inserer_modeles_fr.php
+++ inserer_modeles/trunk/lang/paquet-inserer_modeles_fr.php
@@ -1,18 +1,13 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-// Fichier produit par PlugOnet
-// Module: paquet-inserer_modeles
-// Langue: fr
-// Date: 12-12-2011 23:52:09
-// Items: 2
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/inserer_modeles/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// I
-	'inserer_modeles_description' => 'Ce plugin fournit une aide à l\'insertion des modèles dans vos textes à travers des formulaires de saisies. Ces formulaires sont accessibles dans la colonne de droite ou via un bouton dans le porte-plume.',
-	'inserer_modeles_slogan' => 'Une aide à l\'insertion des modèles dans vos textes',
+	// I
+	'inserer_modeles_description' => 'Ce plugin fournit une aide à l’insertion des modèles dans vos textes à travers des formulaires de saisies. Ces formulaires sont accessibles dans la colonne de droite ou via un bouton dans le porte-plume.',
+	'inserer_modeles_slogan' => 'Une aide à l’insertion des modèles dans vos textes'
 );
+
 ?>
