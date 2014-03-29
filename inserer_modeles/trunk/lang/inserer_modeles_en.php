8463662db6ad8c0713f9a0d609cdfbf9a920f0cb
rj@hodo.fr <rj@hodo.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-03-29 04:40:05 +0000

    [Salvatore] [source:_plugins_/inserer_modeles/trunk/lang/ inserer_modeles] Export depuis http://trad.spip.net de la langue en
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@81654 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ inserer_modeles/trunk/lang/inserer_modeles_en.php
@@ -0,0 +1,46 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/inserer_modeles?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'bouton_choisir' => 'Select',
+	'bouton_inserer' => 'Insert',
+
+	// C
+	'choisir_modele' => 'What do you want to add?',
+
+	// E
+	'erreur_choix_modele' => 'You must choose a template.',
+
+	// I
+	'item_center' => 'center',
+	'item_left' => 'left',
+	'item_right' => 'right',
+
+	// L
+	'label_alignement' => 'Alignment :',
+	'label_id_document' => 'Document number:',
+	'label_modele' => 'Template:',
+
+	// M
+	'message_code_insere' => 'The tag has been inserted into the text.',
+	'message_double_clic' => 'Double-click to insert the template in the text.',
+	'message_inserer_code' => 'You can copy/paste the template code into your text. Double-click on the code to insert automatically into the text field.',
+
+	// N
+	'nom_media' => 'a document',
+
+	// O
+	'outil_inserer_modeles' => 'Insert a template',
+
+	// T
+	'titre_inserer' => 'Insrt @modele@',
+	'titre_inserer_modeles' => 'Insert a template'
+);
+
+?>
