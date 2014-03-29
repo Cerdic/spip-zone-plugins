85a29295673a7d4c80f92efa19c27e4b6970337f
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-03-29 04:40:07 +0000

    [Salvatore] [source:_plugins_/inserer_modeles/trunk/lang/ inserer_modeles] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@81655 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- inserer_modeles/trunk/lang/inserer_modeles_fr.php
+++ inserer_modeles/trunk/lang/inserer_modeles_fr.php
@@ -1,25 +1,44 @@
 <?php
-if (!defined("_ECRIRE_INC_VERSION")) return;
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/inserer_modeles/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-	
-	'titre_inserer_modeles' => 'Insérer un modèle',
-	'choisir_modele' => 'Que souhaitez-vous insérer ?',
+
+	// B
 	'bouton_choisir' => 'Choisir',
 	'bouton_inserer' => 'Insérer',
+
+	// C
+	'choisir_modele' => 'Que souhaitez-vous insérer ?',
+
+	// E
+	'erreur_choix_modele' => 'Vous devez choisir un modèle.',
+
+	// I
 	'item_center' => 'au centre',
 	'item_left' => 'à gauche',
 	'item_right' => 'à droite',
-	'titre_inserer' => 'Insérer @modele@',
-	'erreur_choix_modele' => 'Vous devez choisir un modèle.',
+
+	// L
 	'label_alignement' => 'Alignement :',
 	'label_id_document' => 'Document numéro :',
 	'label_modele' => 'Modèle :',
+
+	// M
 	'message_code_insere' => 'La balise a été insérée dans le texte.',
 	'message_double_clic' => 'Double-cliquez pour insérer le modèle dans le texte.',
-	'message_inserer_code' => 'Vous pouvez copier/coller le code du modèle dans votre texte. Un double-clic sur le code l\'insérera automatiquement dans le champ Texte.',
+	'message_inserer_code' => 'Vous pouvez copier/coller le code du modèle dans votre texte. Un double-clic sur le code l’insérera automatiquement dans le champ Texte.',
+
+	// N
 	'nom_media' => 'un document',
+
+	// O
 	'outil_inserer_modeles' => 'Insérer un modèle',
+
+	// T
+	'titre_inserer' => 'Insérer @modele@',
+	'titre_inserer_modeles' => 'Insérer un modèle'
 );
 
 ?>
