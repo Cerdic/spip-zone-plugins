82a9f1eb8df89a6dcce1e3db8940fa1b8382ab57
rj@hodo.fr <rj@hodo.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-03-30 03:39:07 +0000

    [Salvatore] [source:_plugins_/modeles_media/trunk/lang/ media] Export depuis http://trad.spip.net de la langue en
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@81661 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ modeles_media/trunk/lang/media_en.php
@@ -0,0 +1,84 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/media?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'credits' => 'Credits',
+
+	// E
+	'erreur_saisies' => 'Your input has errors!',
+	'erreur_taille' => 'You must enter a positive integer.',
+	'explication_alt' => 'You can customize the alternate text to display if the browser can not display the media (for images, it is <i>alt</i> attribute ). Leave blank for the alternative text to be automatically generated.',
+	'explication_configurer_media' => 'You can define four standard sizes usable with &lt;media&gt; models with the parameter <i>|taille</i>.',
+	'explication_descriptif' => 'SPIP typographical shortcuts are allowed.',
+	'explication_lien' => 'You can input a URL such as http://mysite.net, an email address or a SPIP shortcut (rub1, art3, breve4 ...).',
+	'explication_titre_lien' => 'You can customize the <i>title</i> attribute of the link. Leave empty for <i>title</i> attribute to be automatically generated.',
+	'extension' => 'Extension:',
+
+	// H
+	'hauteur' => 'Height:',
+
+	// I
+	'info_configurer_media_titre' => '&lt;media&gt; templates: configure the default sizes',
+	'info_inclusion_icone' => 'Inclusion of the icon:',
+	'item_afficher' => 'display',
+	'item_icone' => 'display the icon',
+	'item_insert' => 'embed the document',
+	'item_legende_complete' => 'full caption',
+	'item_legende_simple' => 'simple caption (title + description)',
+	'item_lien_document' => 'link to the document',
+	'item_ne_pas_afficher' => 'no caption',
+	'item_ne_pas_redimensionner' => 'no resize',
+	'item_pas_de_lien' => 'no link',
+	'item_personnaliser' => 'custom',
+	'item_taille_grand' => 'large',
+	'item_taille_icone' => 'icon',
+	'item_taille_moyen' => 'medium',
+	'item_taille_petit' => 'small',
+	'item_vignette' => 'display the thumbnail',
+
+	// L
+	'label_alt' => 'Alternative text:',
+	'label_configurer_media_largeur_legende' => 'Captions width',
+	'label_configurer_media_taille_grand' => 'Size ’grand’ (large):',
+	'label_configurer_media_taille_icone' => 'Size ’icone’ (icon):',
+	'label_configurer_media_taille_moyen' => 'Size ’moyen’ (medium):',
+	'label_configurer_media_taille_petit' => 'Size ’petit’ (small):',
+	'label_credits' => 'Credits',
+	'label_descriptif' => 'Description:',
+	'label_hauteur' => 'Height (in pixels):',
+	'label_id_document' => 'Document number:',
+	'label_largeur' => 'Width (in pixels):',
+	'label_legende' => 'Caption:',
+	'label_legende_personnalisee' => 'Custom caption',
+	'label_lien' => 'Link:',
+	'label_poids' => 'File size (in bytes):',
+	'label_taille' => 'Image size:',
+	'label_taille_personnalisee' => 'Custom size',
+	'label_titre' => 'Title:',
+	'label_titre_lien' => 'Link Title:',
+	'label_type' => 'Type of document:',
+	'label_variante' => 'What do you want?',
+	'largeur' => 'Width:',
+
+	// M
+	'maj' => 'Updated on:',
+	'mime_type' => 'MIME type:',
+	'modeles_media' => '&lt;media&gt; templates',
+
+	// N
+	'nom_media' => 'A document',
+
+	// P
+	'pixels_hauteur' => 'pixels high',
+	'pixels_largeur' => 'pixels wide',
+	'pixels_maximum' => 'maximum pixels',
+	'pixels_minimum' => 'minimum pixels'
+);
+
+?>
