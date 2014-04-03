b34b49e1eb155b64017e29cd0a520bfe95552471
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-04-03 03:40:28 +0000

    [Salvatore] [source:_plugins_/tickets/trunk/lang/ paquet-tickets] Export depuis http://trad.spip.net de la langue en
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@81737 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- tickets/trunk/lang/paquet-tickets_en.php
+++ tickets/trunk/lang/paquet-tickets_en.php
@@ -8,9 +8,9 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// T
-	'tickets_description' => '{Bug reports} is a bug tracking tool integrated into SPIP. It is designed to facilitate the development phase of the site.', # MODIF
+	'tickets_description' => '{Bug reports} is a bug tracking tool integrated into SPIP, both in the private and public area. It is designed to facilitate the development phase of the site, but it can also be used for bug tracking on an operational site.',
 	'tickets_nom' => 'Bug reports',
-	'tickets_slogan' => 'Bug tracking system' # MODIF
+	'tickets_slogan' => 'Bug tracking system'
 );
 
 ?>
