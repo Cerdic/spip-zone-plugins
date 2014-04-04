fad75d658a2a3a940ed9cd7af05b7b45cb588de8
xdjuj@apsulis.com <xdjuj@apsulis.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-04-04 16:13:02 +0000

    Permettre la suppression TOTALE de toute une arborescence (ou simplement la rubrique en cours), et supprimer également les sous rubriques de l'arborescence.
    Il est donc possible de :
    - vider une rubrique (de ses articles)
    - supprimer toute l'arbo d'une rubrique (= supprimer tous les articles et toutes les rubriques qu'elle contient)
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@81762 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- vider_rubrique/vider_rubrique_options.php
+++ vider_rubrique/vider_rubrique_options.php
@@ -1,5 +1,4 @@
 <?php
-
 /***************************************************************************\
  * Plugin Vider Rubrique pour Spip 3.0
  * Licence GPL (c) 2012 - Apsulis
@@ -18,4 +17,20 @@ function vider_rubrique_objet_poubelle($objet,$id_objet,$statut){
 		$res = array('message_ok'=>_T('info_modification_enregistree'));
 	}
 	return $res;
+}
+
+function supprimer_objet($type,$id){
+	return true;
+}
+
+function supprimer_rubrique($liste_id) {
+	spip_log("Suppression de la rubrique : $value.",'vider_rubrique');
+	$supprimer_rubrique = charger_fonction('supprimer_rubrique','action');
+
+	$les_id = array_reverse(explode(",",$liste_id));
+	foreach ($les_id as $key => $value) {
+		$supprimer_rubrique($value);
+	}
+
+	return true;
 }
