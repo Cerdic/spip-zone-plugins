a7e48fb46c2b73a04d78bdeb9779a8e1d6cdf2f7
marcimat@rezo.net <marcimat@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-04-07 14:32:44 +0000

    2 corrections : 1) éviter que la classe dropdown ne se retrouve sur des éléments de menu n'en ayant pas besoin (si un des éléments n'a pas de sous-menu). 2) le menu déroulant ne se refermait pas en recliquant dessus car il faut dans le code HTML l'élément 'data-toggle=dropdown'.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@81784 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- bootstrap/branches/v1/bootstrap2spip/js/menu.dropdown.js
+++ bootstrap/branches/v1/bootstrap2spip/js/menu.dropdown.js
@@ -5,9 +5,9 @@ jQuery(function(){
 	jQuery('.navbar .menu-items .menu-items').closest('.navbar').each(function(){
 		jQuery(this)
 			.find('.menu-items').eq(0)
-			.children('.item').addClass('dropdown')
+			.children('.item').children('.menu-items').parent().addClass('dropdown')
 			.children('.menu-items').addClass('dropdown-menu')
-			.siblings('a').addClass('dropdown-toggle').append('<b class="caret"></b>').dropdown()
+			.siblings('a').addClass('dropdown-toggle').attr('data-toggle', 'dropdown').append('<b class="caret"></b>').dropdown()
 			.siblings('.menu-items')
 			.find('.menu-items').hide();
 	});
