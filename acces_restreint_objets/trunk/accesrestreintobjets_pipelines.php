aaf9fc7d69698897452587f64fb4b1dcc6ecc5cf
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-04-11 13:36:40 +0000

    Meilleur emplacement de la boite de liaison de zone. Dans affiche_milieu, et non pas dans le #wysiwyg.
    PS : c'est quand même bien pourrie cet ergonomie des choses "annexes", une fois qu'on a 15 plugins qui ajoutent des boites. Avant, après, au milieu, sur les côtés. Arg quoi.
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@81823 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- acces_restreint_objets/trunk/accesrestreintobjets_pipelines.php
+++ acces_restreint_objets/trunk/accesrestreintobjets_pipelines.php
@@ -3,25 +3,22 @@
 // Sécurité
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
-// Ajouter le formulaire de config
 function accesrestreintobjets_affiche_milieu($flux){
+	// Ajouter le formulaire de config
 	if ($flux["args"]["exec"] == "configurer_accesrestreint") {
 		$flux["data"] =  recuperer_fond('prive/squelettes/inclure/configurer',array('configurer'=>'configurer_accesrestreintobjets')).$flux["data"];
 	}
-	return $flux;
-}
-
-function accesrestreintobjets_afficher_contenu_objet($flux){
-	include_spip('inc/config');
-	include_spip('inc/autoriser');
-	$objets_ok = lire_config('accesrestreintobjets/objets');
-	
-	if (
+	// Ajouter la config des zones sur la vue de chaque objet autorisé
+	elseif (
+		$exec = trouver_objet_exec($flux['args']['exec'])
+		and include_spip('inc/config')
+		and include_spip('inc/autoriser')
+		and $objets_ok = lire_config('accesrestreintobjets/objets')
 		// Si on a les arguments qu'il faut
-		$type = $flux['args']['type']
-		and $id = intval($flux['args']['id_objet'])
+		and $type = $exec['type']
+		and $id = intval($flux['args'][$exec['id_table_objet']])
 		// Si on est sur un objet restrictible
-		and in_array(table_objet_sql($flux['args']['type']), $objets_ok)
+		and in_array($exec['table_objet_sql'], $objets_ok)
 		// Et que l'on peut configurer le site
 		and autoriser('configurer')
 	) {
@@ -33,7 +30,12 @@ function accesrestreintobjets_afficher_contenu_objet($flux){
 				'id_objet' => $id,
 			)
 		);
-		$flux['data'] = $liens.$flux['data'];
+		if ($liens){
+			if ($pos = strpos($flux['data'],'<!--affiche_milieu-->'))
+				$flux['data'] = substr_replace($flux['data'], $liens, $pos, 0);
+			else
+				$flux['data'] .= $liens;
+		}
 	}
 	
 	return $flux;
