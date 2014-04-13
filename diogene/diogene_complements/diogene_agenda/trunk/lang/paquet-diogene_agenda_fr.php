a9a9c6b7497dc802a711a68fe0e0463a11fb9e90
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-04-13 13:31:03 +0000

    Le fichier de langue du paquet
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@81860 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ diogene/diogene_complements/diogene_agenda/trunk/lang/paquet-diogene_agenda_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// D
+	'diogene_agenda_description' => 'Ajoute la possibilité d\'ajouter un évènement à un article depuis son propre formulaire.',
+	'diogene_agenda_nom' => 'Diogène - Agenda',
+	'diogene_agenda_slogan' => 'Complément agenda pour "Diogène"'
+
+);
+
+?>
