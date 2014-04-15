398dfaf35d3f0f72169fff0cf373b76f5ac1a44e
p@henix.be <p@henix.be@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-04-15 09:08:39 +0000

    Intendation
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@81911 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- foundation-4-spip/trunk/foundation_fonctions.php
+++ foundation-4-spip/trunk/foundation_fonctions.php
@@ -18,12 +18,12 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
  * @return string          iframe encadrée
  */
 function responsive($matches) {
-    // Dans le cas de vimeo, il faut ajouter une classe
-    if (strpos($matches[0], 'vimeo')) $vimeo = ' vimeo';
-    else $vimeo = '';
+  // Dans le cas de vimeo, il faut ajouter une classe
+  if (strpos($matches[0], 'vimeo')) $vimeo = ' vimeo';
+  else $vimeo = '';
 
-    // On revoie la bonne structure html d'iframe.
-    return '<div class="flex-video'.$vimeo.'"><iframe '.$matches[0].'></iframe></div>';
+  // On revoie la bonne structure html d'iframe.
+  return '<div class="flex-video'.$vimeo.'"><iframe '.$matches[0].'></iframe></div>';
 }
 
 /**
@@ -32,8 +32,8 @@ function responsive($matches) {
  * @return string        HTML avec les iFrames modifiée pour être responsive.
  */
 function iframe_responsive($texte) {
-    // On détecte tout les iFrames et on les rends responsives.
-    return preg_replace_callback('/<iframe(.+)><\/iframe>/', 'responsive', $texte);
+  // On détecte tout les iFrames et on les rends responsives.
+  return preg_replace_callback('/<iframe(.+)><\/iframe>/', 'responsive', $texte);
 }
 
 /**
@@ -45,25 +45,25 @@ function iframe_responsive($texte) {
  */
 function toWords($number) {
 
-    // Ce tableau fait office de table de conversion.
-    $conversion = array(
-        1 => 'one',
-        2 => 'two',
-        3 => 'three',
-        4 => 'four',
-        5 => 'five',
-        6 => 'six',
-        7 => 'seven',
-        8 => 'eight',
-        9 => 'nine',
-        11 => 'eleven',
-        12 => 'twelve'
-        );
+  // Ce tableau fait office de table de conversion.
+  $conversion = array(
+                      1 => 'one',
+                      2 => 'two',
+                      3 => 'three',
+                      4 => 'four',
+                      5 => 'five',
+                      6 => 'six',
+                      7 => 'seven',
+                      8 => 'eight',
+                      9 => 'nine',
+                      11 => 'eleven',
+                      12 => 'twelve'
+                      );
 
     // On revoie la bonne valeur.
-    if (is_numeric($number))
-        return $conversion[$number];
-    else return false;
+  if (is_numeric($number))
+    return $conversion[$number];
+  else return false;
 }
 
 
@@ -75,22 +75,22 @@ function toWords($number) {
  */
 function trouver_syntaxe_foundation($nombre_colonnes, $type) {
 
-    // On récupère la configuration
-    $config = lire_config('foundation');
+  // On récupère la configuration
+  $config = lire_config('foundation');
 
-    // Version qui utilise un système large-X ou small-X. J'appel ce groupe les colnum.
-    $colnum = array(4,5);
+  // Version qui utilise un système large-X ou small-X. J'appel ce groupe les colnum.
+  $colnum = array(4,5);
 
-    // Les versions qui utilise des lettres => les colletr
-    $colettr = array(2,3);
+  // Les versions qui utilise des lettres => les colletr
+  $colettr = array(2,3);
 
-    // on cherche
-    // Si on est dans une vesion numérique de foundation, on retourne la syntaxe
-    if (in_array($config['variante'], $colnum))
-        return $type.'-'.$nombre_colonnes;
-    // Sinon, on démarrer le moteur de conversion de nombre, et on renvoie la bonne class
-    elseif (in_array($config['variante'], $colettr))
-        return toWords($nombre_colonnes);
+  // on cherche
+  // Si on est dans une vesion numérique de foundation, on retourne la syntaxe
+  if (in_array($config['variante'], $colnum))
+    return $type.'-'.$nombre_colonnes;
+  // Sinon, on démarrer le moteur de conversion de nombre, et on renvoie la bonne class
+  elseif (in_array($config['variante'], $colettr))
+    return toWords($nombre_colonnes);
 }
 
 /*
@@ -102,14 +102,14 @@ function trouver_syntaxe_foundation($nombre_colonnes, $type) {
 *   (optionnel) type: Dans le cas des version utilisant une syntaxe avec prefix, on lui passe le type (défaut: large)
 */
 function balise_COLONNES_dist($p) {
-    // On récupère les paramètres de la balise.
-    $nombre_colonnes = interprete_argument_balise(1, $p);
-    $type = interprete_argument_balise(2, $p);
+  // On récupère les paramètres de la balise.
+  $nombre_colonnes = interprete_argument_balise(1, $p);
+  $type = interprete_argument_balise(2, $p);
 
-    // On met une valeur par défaut à type.
-    if (!$type) $type = 'large';
+  // On met une valeur par défaut à type.
+  if (!$type) $type = 'large';
 
-    $p->code = "trouver_syntaxe_foundation($nombre_colonnes, $type).' columns'";
-    $p->interdire_scripts = false;
-    return $p;
+  $p->code = "trouver_syntaxe_foundation($nombre_colonnes, $type).' columns'";
+  $p->interdire_scripts = false;
+  return $p;
 }
