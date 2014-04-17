d54d1c10b625c59afefbb97108f61cf8b17210d5
cyril@cym.fr <cyril@cym.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-04-17 15:52:31 +0000

    Sur la proposition de Cédric on n'utilise plus le radio bouton pour paramétrer la meta "responsive", et on lui préfère un checkbox dont le lable est plus explicite; on déplace l'initialistion d'une valeur par défaut pour cette meta, à l'installation du plugin; pour mémoire, le commit précédent corrigeait une erreur qui faisait que la valeur de la coche était toujours oui, même si celle-ci était décochée;
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@81942 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- videos/trunk/base/videos_sql.php
+++ videos/trunk/base/videos_sql.php
@@ -5,27 +5,33 @@ function videos_upgrade($nom_meta_base_version,$version_cible){
 	$current_version = 0.0;
 	if (   (!isset($GLOBALS['meta'][$nom_meta_base_version]) )
 			|| (($current_version = $GLOBALS['meta'][$nom_meta_base_version])!=$version_cible)){
+
+		if ($$current_version == '0.0'){
+			include_spip('inc/config');
+			ecrire_config('videos/responsive', 'oui');
+		}
+
 		if (version_compare($current_version,'0.1','<')){
 			include_spip('base/abstract_sql');
-			
+
 			/* Mettre à jour la table DOCUMENTS pour regrouper les extensions "distantes" (changement de nomenclature) */
 			sql_updateq('spip_documents',array('extension'=>'dist_daily'),"extension='dailym'");
 			sql_updateq('spip_documents',array('extension'=>'dist_vimeo'),"extension='vimeo'");
 			sql_updateq('spip_documents',array('extension'=>'dist_youtu'),"extension='youtube'");
-			
+
 			/* Ajouter les nouvelles extensions en base pour qu'elles soient prises en compte comme des vidéos */
 			sql_insertq('spip_types_documents', array( 'extension'=>'dist_daily', 'titre'=>'Dailymotion','inclus'=>'embed', 'upload'=>'oui' ) );
 			sql_insertq('spip_types_documents', array( 'extension'=>'dist_vimeo', 'titre'=>'Vimeo','inclus'=>'embed', 'upload'=>'oui' ) );
 			sql_insertq('spip_types_documents', array( 'extension'=>'dist_youtu', 'titre'=>'Youtube','inclus'=>'embed', 'upload'=>'oui' ) );
-			
+
 			ecrire_meta($nom_meta_base_version,$current_version="0.1",'non');
 		}
 		if (version_compare($current_version,'0.2','<')){
 			include_spip('base/abstract_sql');
-			
+
 			sql_updateq('spip_documents',array('extension'=>'dist_cubox'),"extension='culturebox'");
 			sql_insertq('spip_types_documents', array( 'extension'=>'dist_cubox', 'titre'=>'CultureBox','inclus'=>'embed', 'upload'=>'oui' ) );
-			
+
 			ecrire_meta($nom_meta_base_version,$current_version="0.2",'non');
 		}
 		if (version_compare($current_version,'0.3','<')){
@@ -40,7 +46,7 @@ function videos_upgrade($nom_meta_base_version,$version_cible){
 			ecrire_meta($nom_meta_base_version,$current_version=$version_cible,'non');
 		}
 	}
-	
+
 	// Si présence du champ MEDIA : on MAJ
 	$trouver_table=charger_fonction('trouver_table','base');	
 	$desc = $trouver_table('spip_types_documents');
@@ -62,7 +68,7 @@ function videos_install($action,$prefix,$version_cible){
 		case 'uninstall':
 			videos_vider_tables('videos_base_version');
 			videos_vider_tables('videos');
-		 	break;
+			break;
 	}
 }
 
