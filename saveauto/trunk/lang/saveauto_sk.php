e25034b39e56808bc37cac32a7104817965ec250
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-04-24 03:39:38 +0000

    [Salvatore] [source:_plugins_/saveauto/trunk/lang/ saveauto] Export depuis http://trad.spip.net de la langue sk
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@82084 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- saveauto/trunk/lang/saveauto_sk.php
+++ saveauto/trunk/lang/saveauto_sk.php
@@ -19,19 +19,21 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'erreur_impossible_liste_tables' => 'Tabuľky z databázy sa nedajú vypísať.',
 	'erreur_probleme_donnees_corruption' => 'Problém s údajmi v tabuľke @table@ table, môže byť poškodená!',
 	'erreur_repertoire_inaccessible' => 'Do priečinka @rep@ sa nedá zapisovať.',
+	'erreur_repertoire_perso_inaccessible' => 'Zadaný priečinok @rep@ je neprístup­ný. Namies­to neho sa preto použije bežný zálo­ho­va­cí prieči­nok SPIPu',
 
 	// H
 	'help_cfg_generale' => 'Tieto nastavenia sa použijú na všetky zálohy – manuálne aj automatické',
 	'help_contenu' => 'Zvoľte si vlastnosti obsahu svojho súboru zálohy.',
 	'help_contenu_auto' => 'Zvoľte si obsah automatických záloh.',
 	'help_frequence' => 'Zadajte frekvenciu automatického zálohovania v dňoch',
-	'help_liste_tables' => 'Podľa predvolených nastavení sa exportujú všetky tabuľky okrem tabuliek @noexport@. Ak chcete presne určiť, ktoré tabuľky sa majú zálohovať, odznačte toto políčko, čím otvoríte ich zoznam.', # MODIF
+	'help_liste_tables' => 'Podľa predvolených nastavení sa exportujú všetky tabuľky okrem tabuliek @noexport@. Ak chcete presne určiť, ktoré tabuľky sa majú zálohovať (ako aj tabuľky, ktoré nevytvoril SPIP), odznačte toto políčko, čím otvoríte ich zoznam.',
 	'help_mail_max_size' => 'Zadajte maximálnu veľkosť súboru databázy v MB, po ktorej prekročení nebude odoslaný e-mail (hodnotu je dobré si overiť u poskytovateľa e-mailu).',
 	'help_max_zip' => 'Súbor zálohy bude automaticky "zazipovaný", ak jeho veľkosť bude menšia ako limit. Zadajte limit v megabytoch (Limit je potrebný na to, aby sa zabránilo zrúteniu servera preto, že bol vytvorený príliš veľký súbor zip)',
 	'help_notif_active' => 'Ak chcete mať prehľad o každom automatickom spracovaní, aktivujte si zasielanie oznamov. Pri automatickom zálohovaní dostanete na e-mail vygenerovaný súbor, či nie je príliš veľký a oznam o tom, že zásuvný modul Poštár je aktivovaný.',
 	'help_notif_mail' => 'Zadajte adresy oddelené čiarkami ",". Budú pridané k adrese webmastera stránky.',
 	'help_obsolete' => 'Zadajte dĺžku trvania databáz v dňoch',
 	'help_prefixe' => 'Zadajte predponu, ktorá bude pripojená k názvu každého súboru databázy',
+	'help_repertoire' => 'Ak chcete použiť iný odkladací priečinok na zálohy SPIPu, uveďte celú adresu stránky (na konci s /)',
 	'help_restauration' => '<strong>Pozor!!!</strong> vytvorené zálohy <strong>nie sú vo formáte záloh SPIPu</strong> a nemôže sa na ne použiť nástroj SPIPu na obnovenie údajov.<br /><br />
             
 
@@ -69,8 +71,10 @@ Tieto databázy obsahujú príkazy na <strong>vymazanie</strong> tabuliek vašej
 	'label_notif_mail' => 'E-mailová adresa na oznamy',
 	'label_obsolete_jours' => 'Zachovávanie záloh',
 	'label_prefixe_sauvegardes' => 'Predpona',
+	'label_repertoire_sauvegardes' => 'Priečinok',
 	'label_sauvegarde_reguliere' => 'Aktivovať pravidelné zálohovanie',
 	'label_structure' => 'Štruktúra tabuliek',
+	'label_tables_non_spip' => 'Tabuľky, ktoré nevytvoril SPIP',
 	'label_toutes_tables' => 'Zálohovať všetky tabuľky SPIPu',
 	'legend_cfg_generale' => 'Všeobecné vlastnosti databáz',
 	'legend_cfg_notification' => 'Oznamy',
@@ -87,7 +91,7 @@ Tieto databázy obsahujú príkazy na <strong>vymazanie</strong> tabuliek vašej
 	'message_telechargement_nok' => 'Chyba pri sťahovaní.',
 
 	// T
-	'titre_boite_historique' => 'Zálohy MySQL dostupné na stiahnutie', # MODIF
+	'titre_boite_historique' => 'Zálohy MySQL dostupné na stiahnutie na  @dossier@',
 	'titre_boite_sauver' => 'Vytvoriť zálohu MySQL',
 	'titre_page_configurer' => 'Nastavenia modulu Automatická záloha',
 	'titre_page_saveauto' => 'Zálohovať databázu vo formáte MySQL',
