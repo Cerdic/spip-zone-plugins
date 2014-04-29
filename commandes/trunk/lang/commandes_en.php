364efa16f3e632343817bd711c2a972d209516a2
tcharlss@hotmail.fr <tcharlss@hotmail.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-04-29 03:39:28 +0000

    [Salvatore] [source:_plugins_/commandes/trunk/lang/ commandes] Export depuis http://trad.spip.net de la langue en
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@82167 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- commandes/trunk/lang/commandes_en.php
+++ commandes/trunk/lang/commandes_en.php
@@ -8,6 +8,10 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
+	'abbr_hors_taxe' => 'exc. VAT', # RELIRE
+	'abbr_prix_unitaire' => 'Price', # RELIRE
+	'abbr_quantite' => 'Qty', # RELIRE
+	'abbr_toutes_taxes_comprises' => 'inc. VAT', # RELIRE
 	'action_facture' => 'Invoice',
 	'action_modifier' => 'Modify',
 	'action_supprimer' => 'Delete',
@@ -36,15 +40,21 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'contenu' => 'Content',
 
 	// D
-	'date_commande_label' => 'Date of creation:', # MODIF
-	'date_envoi_label' => 'Date of dispatch:', # MODIF
-	'date_maj_label' => 'Date of last update:', # MODIF
-	'date_paiement_label' => 'Date of payment:', # MODIF
+	'date_commande_label' => 'Date of creation:',
+	'date_commande_label_court' => 'Creation',
+	'date_envoi_label' => 'Date of dispatch:',
+	'date_envoi_label_court' => 'Shipping',
+	'date_maj_label' => 'Date of last update:',
+	'date_paiement_label' => 'Date of payment:',
+	'date_paiement_label_court' => 'Payment',
 	'designation' => 'Designation',
 	'details_commande' => 'Details of the order:',
 
 	// E
 	'etat' => 'Status',
+	'explication_accueil_encours' => 'Indicate the active orders on the homepage ?',
+	'explication_choix_statuts_actifs' => 'Status corresponding to active orders',
+	'explication_statuts_actifs' => 'According to their status, some orders may require your attention. They are « active » orders, which need an action on your part. You can display them on the homepage, in the list of the items awaiting evaluation.', # RELIRE
 	'explications_notifications_statuts' => 'Notification in case of status change',
 	'explications_notifications_statuts_aucune' => 'No notification in case of status change',
 
@@ -55,20 +65,35 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// I
 	'info_1_commande' => '1 order',
+	'info_1_commande_active' => '1 active order',
 	'info_1_commande_statut_attente' => '1 order awaiting validation',
+	'info_1_commande_statut_encours' => '1 ongoing order',
+	'info_1_commande_statut_envoye' => '1 sent order',
+	'info_1_commande_statut_erreur' => '1 erroneous order',
 	'info_1_commande_statut_partiel' => '1 order partially paid',
 	'info_1_commande_statut_paye' => '1 paid order',
+	'info_1_commande_statut_retour' => '1 returned order',
+	'info_1_commande_statut_retour_partiel' => '1 order partially returned',
+	'info_aucun_client' => 'No author is associated to the order',
 	'info_aucun_commande' => 'No orders',
+	'info_commande_vide' => 'The order is empty',
 	'info_commandes' => 'Orders',
 	'info_date_envoi_vide' => 'order not sent',
 	'info_date_non_definie' => 'not defined',
 	'info_date_paiement_vide' => 'order not paid for',
 	'info_nb_commandes' => '@nb@ orders',
+	'info_nb_commandes_actives' => '@nb@ active orders',
 	'info_nb_commandes_statut_attente' => '@nb@ orders awaiting validation',
+	'info_nb_commandes_statut_envoye' => '@nb@ sent orders',
+	'info_nb_commandes_statut_erreur' => '@nb@ erroneous orders',
 	'info_nb_commandes_statut_partiel' => '@nb@ orders partially paid ',
 	'info_nb_commandes_statut_paye' => '@nb@ paid orders',
+	'info_nb_commandes_statut_retour' => '@nb@ returned orders',
+	'info_nb_commandes_statut_retour_partiel' => '@nb@ partially returned orders',
+	'info_nb_commandse_statut_encours' => '@nb@ ongoing orders',
 	'info_numero' => 'ORDER NUMBER:',
 	'info_numero_commande' => 'ORDER NUMBER:',
+	'info_sans_descriptif' => 'Without description',
 	'info_toutes_commandes' => 'All orders',
 
 	// L
@@ -79,10 +104,16 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'label_filtre_tous_clients' => 'All clients',
 	'label_filtre_tous_statuts' => 'All states',
 	'label_filtre_toutes_dates' => 'All dates',
+	'label_infos' => 'Infos',
 	'label_objet' => 'Object',
+	'label_objets' => 'Objects',
 	'label_passee_le' => 'Ordered on',
 	'label_payee_le' => 'paid on the',
+	'label_prix_unitaire' => 'Price', # RELIRE
+	'label_quantite' => 'Quantity',
 	'label_recherche' => 'Search',
+	'label_statuts_actifs' => 'Status',
+	'label_taxe' => 'Tax',
 
 	// M
 	'merci_de_votre_commande' => 'We have registered your order and we appreciate your trust.',
@@ -146,13 +177,18 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// T
 	'texte_changer_statut_commande' => 'This order is :',
-	'titre_adresse_commande' => 'Adress associated to the order', # RELIRE
+	'titre_adresse_client' => 'Client’s address',
+	'titre_adresse_commande' => 'Address associated with the order',
+	'titre_adresse_contact' => 'Contact’s address',
 	'titre_adresses_associees' => 'Associated adresses ',
+	'titre_adresses_client' => 'Client’s addresses',
+	'titre_adresses_commande' => 'Addresses associated with the order',
 	'titre_commandes_actives' => 'Active orders',
 	'titre_commandes_auteur' => 'Orders from the author',
 	'titre_contenu_commande' => 'Details of the order',
 	'titre_informations_client' => 'Client',
-	'titre_logo_commande' => 'Logo', # MODIF
+	'titre_logo_commande' => 'Logo',
+	'titre_statuts_actifs_parametres' => 'Active orders',
 
 	// U
 	'une_commande_de' => 'An order of: ',
