9369e0650c5068e9a5c502dbe63fb65131a3f469
tcharlss@hotmail.fr <tcharlss@hotmail.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-05-02 13:00:22 +0000

    Coquille : "declinaisons" en trop
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@82216 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- commandes/trunk/lang/paquet-commandes_fr.php
+++ commandes/trunk/lang/paquet-commandes_fr.php
@@ -6,9 +6,9 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// S
-	'commandes_declinaisons_description' => 'Permet de g&#233;rer des commandes.',
-	'commandes_declinaisons_nom' => 'Commandes',
-	'commandes_declinaisons_slogan' => 'G&#233;rer des commandes',
+	'commandes_description' => 'Permet de g&#233;rer des commandes.',
+	'commandes_nom' => 'Commandes',
+	'commandes_slogan' => 'G&#233;rer des commandes',
 );
 
 ?>
