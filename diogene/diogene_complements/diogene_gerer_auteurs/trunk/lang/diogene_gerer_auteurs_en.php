3e275fa059e426ad3159bd84fb805eb3931f04a6
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-05-05 03:39:19 +0000

    [Salvatore] [source:_plugins_/diogene/diogene_complements/diogene_gerer_auteurs/trunk/lang/ diogene_gerer_auteurs] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@82242 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- diogene/diogene_complements/diogene_gerer_auteurs/trunk/lang/diogene_gerer_auteurs_en.php
+++ diogene/diogene_complements/diogene_gerer_auteurs/trunk/lang/diogene_gerer_auteurs_en.php
@@ -8,17 +8,15 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// E
-	'explication_cfg_ajout_auteurs' => 'Enable to add / delete authors on articles (use the plugin diogene_gerer_auteurs)',
-	'explication_diogene_gerer_auteurs' => 'To add another person as author, it has to be member of the website (You only can add one people each time you validate the form).',
-	'explication_gerer_auteurs_remove' => 'To delete one or more authors, select them and validate the form.',
+	'explication_cfg_ajout_auteurs' => 'Enable to add / delete authors on articles (use the plugin diogene_gerer_auteurs)', # MODIF
+	'explication_diogene_gerer_auteurs' => 'To add another person as author, it has to be member of the website (You only can add one people each time you validate the form).', # MODIF
 
 	// F
 	'form_legend' => 'The authors',
 
 	// L
 	'label_cfg_ajout_auteurs' => 'Add / Delete authors',
-	'label_diogene_gerer_auteurs' => 'Add an author',
-	'label_gerer_auteurs_remove' => 'Delete author(s)',
+	'label_diogene_gerer_auteurs' => 'Add an author', # MODIF
 
 	// M
 	'message_impossible_supprimer_auteur' => 'You can’t remove yourself from the list of authors. You will lose your editing rights.'
