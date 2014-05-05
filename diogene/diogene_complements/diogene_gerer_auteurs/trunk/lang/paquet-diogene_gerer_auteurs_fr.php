1d3b1a17d8c74ce83049bb38accd86df56bd4444
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-05-05 03:39:24 +0000

    [Salvatore] [source:_plugins_/diogene/diogene_complements/diogene_gerer_auteurs/trunk/lang/ paquet-diogene_gerer_auteurs] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@82243 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- diogene/diogene_complements/diogene_gerer_auteurs/trunk/lang/paquet-diogene_gerer_auteurs_fr.php
+++ diogene/diogene_complements/diogene_gerer_auteurs/trunk/lang/paquet-diogene_gerer_auteurs_fr.php
@@ -6,7 +6,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// D
-	'diogene_gerer_auteurs_description' => 'Permet d\'ajouter via Diogene un champ afin d’associer ou dissocier un ou plusieurs auteurs sur un formulaire d’édition d’objet.',
+	'diogene_gerer_auteurs_description' => 'Permet d’ajouter via Diogene un champ afin d’associer ou dissocier un ou plusieurs auteurs sur un formulaire d’édition d’objet.',
 	'diogene_gerer_auteurs_nom' => 'Diogene - Auteurs',
 	'diogene_gerer_auteurs_slogan' => 'Complément "auteurs" pour "Diogene"'
 );
