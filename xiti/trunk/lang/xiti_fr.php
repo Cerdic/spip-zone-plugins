1170e2ed7578c46a6459277eb8ea357449046fe7
cyp@lipousse.no-ip.org <cyp@lipousse.no-ip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-05-12 15:28:09 +0000

    version pour SPIP3
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@82327 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- xiti/trunk/lang/xiti_fr.php
+++
@@ -1,8 +0,0 @@
-<?php
-$GLOBALS[$GLOBALS['idx_lang']] = array(
-	'bouton_xiti' => 'XITI',
-	'form_xiti' => 'Formulaire de configuration',
-	'intro_xiti' => 'Information d\'installation',
-	'titre_xiti' => 'Configuration du module XITI'
-);
-?>
