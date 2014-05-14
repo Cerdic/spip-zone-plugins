f911e4c793b452dc9d97be376eaf1cc0e2daefe0
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-05-14 17:49:38 +0000

    Gérer le cas ou statuts_images sur les articles serait déjà un array pour une raison X ou Y
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@82368 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- archive/trunk/base/archive.php
+++ archive/trunk/base/archive.php
@@ -23,16 +23,25 @@ function archive_declarer_tables_objets_sql($tables){
 	 */
 	$tables['spip_articles']['statut_titres']['archive'] = "archive:info_article_archive";
 	$tables['spip_articles']['statut_textes_instituer']['archive'] = "archive:texte_statut_archive";
-	$tables['spip_articles']['statut_images']['prepa'] = "puce-preparer-8.png";
-	$tables['spip_articles']['statut_images']['prop'] = "puce-proposer-8.png";
-	$tables['spip_articles']['statut_images']['publie'] = "puce-publier-8.png";
-	$tables['spip_articles']['statut_images']['refuse'] = "puce-refuser-8.png";
-	$tables['spip_articles']['statut_images']['poubelle'] = "puce-supprimer-8.png";
-	$tables['spip_articles']['statut_images']['archive'] = "puce-archiver-8.png";
-	if(is_array($tables['spip_articles']['statut'][0]['exception']))
-		$tables['spip_articles']['statut'][0]['exception'][] = 'archive';
+	
+	if(!is_array($tables['spip_articles']['statut_images']))
+		$tables['spip_articles']['statut_images'] = array(
+													'prepa' => "puce-preparer-8.png",
+													'prop' => "puce-proposer-8.png",
+													'publie' => "puce-publier-8.png",
+													'refuse' => "puce-refuser-8.png",
+													'poubelle' => "puce-supprimer-8.png",
+													'archive' => "puce-archiver-8.png");
 	else
+		$tables['spip_articles']['statut_images']['archive'] = "puce-archiver-8.png";
+	
+	/**
+	 * Si critère archive dans la boucle, on le considère comme une exception de statut, il ne forcera pas le statut à publie
+	 */
+	if(!is_array($tables['spip_articles']['statut'][0]['exception']))
 		$tables['spip_articles']['statut'][0]['exception'] = array($tables['spip_articles']['statut'][0]['exception'],'archive');
+	else
+		$tables['spip_articles']['statut'][0]['exception'][] = 'archive';
 	
 	return $tables;
 }
