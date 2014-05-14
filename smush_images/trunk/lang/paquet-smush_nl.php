6ed69774b2e3c424a6662781c607a6823893d510
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-05-14 03:41:11 +0000

    [Salvatore] [source:_plugins_/smush_images/trunk/lang/ paquet-smush] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@82351 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ smush_images/trunk/lang/paquet-smush_nl.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-smush?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// S
+	'smush_description' => 'Met plugin Smush kun je afbeeldingen van een site tot een minimum reduceren door gebruik te maken van specifieke software op de server.',
+	'smush_slogan' => 'Reduceer afbeeldingen tot het minimum.'
+);
+
+?>
