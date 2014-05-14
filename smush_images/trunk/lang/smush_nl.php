d943b7873418ae5ef5f447dca0f61cc844601784
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-05-14 03:41:04 +0000

    [Salvatore] [source:_plugins_/smush_images/trunk/lang/ smush] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@82350 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ smush_images/trunk/lang/smush_nl.php
@@ -0,0 +1,39 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/smush?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// E
+	'erreur_copie_locale' => 'Het bestand kon niet lokaal worden gekopieerd (@src@).',
+	'erreur_pas_image' => 'Het bestand lijkt geen afbeelding in het formaat png, jpg of gif (@src@).',
+
+	// I
+	'info_gain' => 'De winst bedraagt @gain@ bytes ofwel @gain_octets@ (@percent@%).',
+	'info_gain_simple' => 'De winst bedraagt @gain@ bytes (@percent@%).',
+	'info_impossible_utilisation_locale' => 'De benodigde software voor lokaal gebruik is niet compleet.',
+	'info_logiciel_nok' => 'De software "@logiciel@" is niet beschikbaar',
+	'info_logiciel_nok_tous' => 'De benodigde software is niet bruikbaar. Je kunt Smush niet lokaal gebruiken.',
+	'info_logiciel_ok' => 'De software "@logiciel@" lijkt bruikbaar',
+	'info_logiciel_ok_tous' => 'De benodigde software is bruikbaar. Je kunt Smush lokaal gebruiken.',
+	'info_taille' => 'Grootte: @taille@ (@octets@).',
+	'info_titre_smush' => 'Smush',
+
+	// L
+	'label_eviter_traitement_auto' => 'Voorkom een automatische verwerking van alle door SPIP gegenereerde afbeeldingen',
+	'label_url_test' => 'URL van de testafbeelding',
+
+	// S
+	'smush_conf_descriptif' => 'Met plugin Smush kun je de grootte van afbeeldingen op een site tot een minimum reduceren, gebruik makend van op de server aanwezige specifieke software. Op deze bladzijde doe je de configuratie.',
+
+	// T
+	'titre_test_logiciels' => 'Aanwezigheidstest voor software',
+	'titre_testeur_smush' => 'Op een afbeelding testen',
+	'titre_version_originale' => 'Originele versie',
+	'titre_version_smushed' => 'Geoptimaliseerde versie'
+);
+
+?>
