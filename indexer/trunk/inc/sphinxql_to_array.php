fa8fe69f597a732b7ba4dc8ab335db32cbc1d585
fil@rezo.net <fil@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-05-16 21:24:11 +0000

    pagination a coups de array_pad() ; attention l'insertion via preg_replace est VRAIMENT a revoir
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@82471 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- indexer/trunk/inc/sphinxql_to_array.php
+++ indexer/trunk/inc/sphinxql_to_array.php
@@ -3,11 +3,26 @@
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 
-function inc_sphinxql_to_array_dist($u){
-	include_spip('inc/indexer');
+function inc_sphinxql_to_array_dist($u, $debut=''){
+   include_spip('inc/indexer');
+
+   // hacks pour la pagination
+   // attention ce preg_replace est vraiment tres fragileÉÉÉ ˆ revoir TODO.
+   if ($debut = intval($debut)) {
+       $u = preg_replace('/ FACET /', " LIMIT $debut,20 FACET ", $u, 1);
+   }
 
 	$sphinx = new Sphinx\SphinxQL(SPHINX_SERVER_HOST, SPHINX_SERVER_PORT);
 
-	return $sphinx->allfetsel($u);
+	$all = $sphinx->allfetsel($u);
+
+	// pagination : rajouter $debut elements videsÉ
+	if ($debut) $all['docs'] = array_pad($all['docs'], -$debut - count($all['docs']), 0);
+
+	$total = $all['meta']['total'];
+	if ($total > count($all['docs']))
+		$all['docs'] = array_pad($all['docs'], $total - count($all['docs']), 0);
+
+	return $all;
 }
 
