8b1cbfcebc9866c6f1854eacf89fd83f5ce32b26
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-05-17 10:24:41 +0000

    Tag '/tags/v0.12.0' created from /branches/v0.12.0:82531
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@82534 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ markdown/tags/v0.12.0/tests/parsedown.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Test unitaire des raccourcis Markdown dans SPIP
+ *
+ */
+
+	$test = 'parsedown';
+	$remonte = "../";
+	while (!is_dir($remonte."ecrire"))
+		$remonte = "../$remonte";
+	require $remonte.'tests/test.inc';
+	find_in_path("inc/texte.php",'',true);
+
+	$GLOBALS['spip_lang'] = 'en'; // corrections typo
+	$GLOBALS['class_spip_plus'] = '';
+	$GLOBALS['class_spip'] = '';
+	define('_MARKDOWN_HMINI',1);
+	define('_MARKDOWN_SYNTAXE_PAR_DEFAUT','spip');
+
+	//
+	// hop ! on y va
+	//
+	$err = tester_fun('propre', essais_parsedown());
+	
+	// si le tableau $err est pas vide ca va pas
+	if ($err) {
+		die ('<dl>' . join('', $err) . '</dl>');
+	}
+
+	echo "OK";
+	
+
+	function essais_parsedown(){
+
+		$tests = preg_files(_DIR_PLUGIN_MARKDOWN."lib/parsedown/tests/data/",'\.md$');
+
+		$markdown = $expected = "";
+		$essais = array ();
+
+		foreach($tests as $t){
+			lire_fichier($t,$markdown);
+			lire_fichier(substr($t,0,-3).".html",$expected);
+			$expected = str_replace("\r\n", "\n", $expected);
+			$expected = str_replace("\r", "\n", $expected);
+			$essais[basename($t,".md")] = array(
+				$expected , "<md>$markdown</md>"
+			);
+		}
+
+		return $essais;
+	}
+
+
+
+?>
