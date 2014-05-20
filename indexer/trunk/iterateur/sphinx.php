717332a3abf00fbf3753570991f448c161eb0706
marcimat@rezo.net <marcimat@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-05-20 09:46:04 +0000

    Indiquer que l'on enlève le html dans les snippets.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@82609 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- indexer/trunk/iterateur/sphinx.php
+++ indexer/trunk/iterateur/sphinx.php
@@ -276,7 +276,7 @@ class IterateurSPHINX implements Iterator {
 		if (!$desc['phrase'] OR !$desc['champ']) {
 			return false;
 		}
-		$this->queryApi->select("SNIPPET($desc[champ], " . $this->quote($desc['phrase']) . ", 'limit=$desc[limit]') AS $desc[as]");
+		$this->queryApi->select("SNIPPET($desc[champ], " . $this->quote($desc['phrase']) . ", 'limit=$desc[limit],html_strip_mode=strip') AS $desc[as]");
 		return true;
 	}
 
