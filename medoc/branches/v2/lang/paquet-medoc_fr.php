ac777333775508a1cda365da240b745a96dd7a3e
toutati@free.fr <toutati@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-05-20 21:52:33 +0000

    lang fr du paquet et corrections mineures
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@82640 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ medoc/branches/v2/lang/paquet-medoc_fr.php
@@ -0,0 +1,17 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/agenda/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// M
+	'medoc_description' => 'Patch unifiant le comportement et la syntaxe d\'insertion des documents, pour soigner l\'inconstance d\'affichage dont souffrent les documents de SPIP. Désormais, c\'est vous qui décidez !
+- Le modèle <doc> affiche toujours une vignette cliquable
+- Syntaxe d\'insertion unifiée. Exemples : <docXXX|left>, <docXXX|img|right>, <docXXX|emb|center>
+- Rétrocompatibilité préservée',
+	'medoc_nom' => 'Modèle <doc> unifié',
+	'medoc_slogan' => 'L\'insertion de documents simplifiée !'
+);
+
+?>
