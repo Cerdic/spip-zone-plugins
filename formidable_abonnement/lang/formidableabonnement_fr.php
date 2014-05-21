bf81d81248e274075e4cdf95e6bff6616898ee98
toutati@free.fr <toutati@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-05-21 20:15:51 +0000

    Un plugin pour formidable qui ne fait aucun traitement il permet de choisir dans les "traitements d'un formulaire" du plugin formidable  les champs à traiter par la suite puis il renvoit un pipeline traiter_formidableabonnement à utiliser avec tous systèmes d'abonnement qui requiert email, prenom, nom, organisme et si abonnement ou desabonnement exemple d'utilisation ensuite:
    function nomduplugin_traiter_formidableabonnement($flux){
    	if ($flux['args']['choix_abo'] && $flux['args']['email']){
    		$choix_abo = $flux['args']['choix_abo'];  //abonnement ou desabonnement
    		$email_abo = $flux['args']['email'];  //email
    		//… etc
    		
    		//vos traitements 
    		spip_log("$choix_abo pour $email_abo","formidableabonnement");
    	}
    
    	return $flux;
    }
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@82665 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ formidable_abonnement/lang/formidableabonnement_fr.php
@@ -0,0 +1,29 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// F
+	'formidableabonnement_titre' => 'Formulaires d\'abonnement',
+
+	'traiter_abonnement_titre' => 'Abonnement',
+	'traiter_abonnement_description' => 'Traiter un abonnement à la saisie du formulaire',
+
+	'traiter_abonnement_champ_email_label' => 'Champ de l\'Email',
+	'traiter_abonnement_champ_choix_abo_label' => 'Champ de l\'abonnement',
+	'traiter_abonnement_champ_choix_abo_explication' => 'Le champ qui propose abonnement ou désabonnement',
+	'traiter_abonnement_champ_abo_oui'=>'Valeur pour l\'abonnement',
+	'traiter_abonnement_champ_abo_non'=>'Valeur pour le désabonnement',
+	'traiter_abonnement_option_champ_nom_label' => 'Champ du Nom',
+	'traiter_abonnement_option_champ_prenom_label' => 'Champ du Prénom',
+	'traiter_abonnement_option_champ_organisme_label' => 'Champ de l\'Organisme',
+
+	'traiter_abonnement_option_message_label' => 'Message après abonnement réussi',
+	'traiter_abonnement_necessite_explication' => 'Pour la prise en charge de l\'abonnement, il est nécessaire d\'enregistrer les résultats dans la base de données.',
+
+
+);
+
+?>
---
+++ formidable_abonnement/lang/paquet-formidableabonnement_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// F
+	'formidableabonnement_description' => 'Outil qui permet de générer un pipeline d\'abonnement lors de la saisie d\'un formulaire du plugin Formidable',
+	'formidableabonnement_nom' => 'Formulaire d\'abonnement Formidable',
+	'formidableabonnement_slogan' => 'Extension du plugin formidable',
+);
+
+?>
