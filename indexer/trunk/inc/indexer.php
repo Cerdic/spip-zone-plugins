6caef1bd42b643631730ade4b608c9abf317b0ab
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-05-22 16:58:28 +0000

    Tout comme pour indexer_sources(), on déporte la récupération d'un indexeur valide dans une fonction indexer_indexer() utilisable plusieurs fois.
    On s'insère dans le pipeline "post_edition", et dès qu'il y a une modif sur un objet, on cherche si on a une ou plusieurs de nos sources enregistrées qui correspondent à l'objet en cours d'édition.
    
    Si c'est le cas, on appelle l'indexeur et on lui donne à remplacer (ou ajouter) le document issu de la source en question (ou des sources).
    
    Sans rien toucher, le mécanisme teste avec l'alias donné à la source ("articles", "fiches", etc) MAIS si on veut un truc plus bizarre, ou surtout si on a plusieurs sources différentes travaillant sur le même type d'objet, alors la classe de source peut implémenter une méthode getObjet() qui doit retourner "patate" (par exemple).
    
    Testé aussi sur un objet non-spip, pour l'instant ça marche.
    
    Là on réindexe le doc directement dans le pipeline, et ça ne me parait pas lent. Si on trouve que ça gêne, on pourra déporter l'indexation dans un job. Mais pour l'instant ça m'a l'air d'aller.
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@82681 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- indexer/trunk/inc/indexer.php
+++ indexer/trunk/inc/indexer.php
@@ -17,23 +17,59 @@ $loader->add('Indexer', _DIR_PLUGIN_INDEXER . 'lib');
 $loader->add('Sphinx',  _DIR_PLUGIN_INDEXER . 'lib');
 $loader->addPsr4('Spip\\Indexer\\Sources\\',  _DIR_PLUGIN_INDEXER . 'Sources');
 
-
 $loader->register();
+
+/**
+ * Renvoyer un indexeur configuré avec un (et peut-être un jour plusieurs) lieu de stockage des choses à indexer
+ *
+ * Par défaut renvoie l'indexeur avec le stockage Sphinx intégré en interne et les paramètres des define()
+ * 
+ * @pipeline_appel indexer_indexer
+ * @return Retourne un objet Indexer, ayant été configuré avec la méthode registerStorage()
+ */
+function indexer_indexer(){
+	static $indexer = null;
+	
+	if (is_null($indexer)){
+		// On crée un indexeur
+		$indexer = new Indexer\Indexer();
+	
+		// On tente de le configurer avec Sphinx et les define()
+		try {
+			$indexer->registerStorage(
+			    new Indexer\Storage\Sphinx(
+			        new Sphinx\SphinxQL\SphinxQL(SPHINX_SERVER_HOST, SPHINX_SERVER_PORT), SPHINX_DEFAULT_INDEX)
+			);
+		} catch( \Exception $e ) {
+			if (!$message = $e->getMessage())
+			    $message = _L('Erreur inconnue');
+			die("<p class='erreur'>$message</p>");
+		}
+		
+		// On le fait passer dans un pipeline
+		$indexer = pipeline('indexer_indexer', $indexer);
+	}
+	
+	return $indexer;
+}
 
 /**
  * Renvoyer les sources de données disponibles dans le site
  * 
  * Un pipeline "indexer_sources" est appelée avec la liste par défaut, permettant de retirer ou d'ajouter des sources.
  *
- * @pipeline_appel insexer_sources
+ * @pipeline_appel indexer_sources
  * @return Sources Retourne un objet Sources listant les sources enregistrées avec la méthode register()
  */
 function indexer_sources(){
 	static $sources = null;
 	
 	if (is_null($sources)){
+		// On crée la liste des sources
 		$sources = new Indexer\Sources\Sources();
+		// Par défaut on enregistre les articles du SPIP
 		$sources->register('articles', new Spip\Indexer\Sources\Articles());
+		// On passe les sources dans un pipeline
 		$sources = pipeline('indexer_sources', $sources);
 	}
 	
---
+++ indexer/trunk/indexer_pipelines.php
@@ -0,0 +1,59 @@
+<?php
+
+// Sécurité
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+/**
+ * Réindexer des choses lorsqu'il y a des modifications sur un objet
+ *
+ * @pipeline post_edition
+ * @param array $flux Arguments et contenu du pipeline "post_edition"
+ * @return Retourne le flux d'origine mais possiblement modifié
+ */
+function indexer_post_edition($flux){
+	include_spip('base/connect_sql'); // pour être sûr d'avoir objet_type()
+	
+	// On trouve le type de l'objet
+	if (isset($flux['args']['type'])){
+		$objet = $flux['args']['type'];
+	}
+	elseif (isset($flux['args']['table'])){
+		$objet = objet_type($flux['args']['table']);
+	}
+	// On trouve son identifiant
+	if (isset($flux['args']['id_objet'])){
+		$id_objet = $flux['args']['id_objet'];
+	}
+	
+	// Si on a ce qu'il faut, on continue
+	if ($objet and $id_objet){
+		include_spip('inc/indexer');
+		
+		// On récupère toutes les sources compatibles avec l'indexation
+		$sources = indexer_sources();
+		
+		// On parcourt toutes les sources et on garde celles on un rapport avec l'objet du pipeline
+		foreach ($sources as $alias => $source){
+			// Si une méthode pour définir explicitement existe, on l'utilise
+			if (method_exists($source, 'getObjet')){
+				$objet_source = $source->getObjet();
+			}
+			// Sinon on cherche avec l'alias donné à la source
+			else{
+				$objet_source = objet_type(strtolower($alias));
+			}
+			
+			// Si l'objet de la source est le même que dans l'édition, on met à jour l'indexation de l'objet
+			if ($objet_source == $objet){
+				// On va chercher l'indexeur du SPIP
+				$indexer = indexer_indexer();
+				// On va chercher l'unique document à indexer
+				$document = $source->getDocuments($id_objet, $id_objet+1); // +1 car le test est normalement : id < $end
+				// Et on le remplace (ou ajoute) dans l'indexation
+				$indexer->replaceDocuments($document);
+			}
+		}
+	}
+	
+	return $flux;
+}
