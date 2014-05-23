917d0aab5167c82828791f1b8a8c2249e41d3035
toutati@free.fr <toutati@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-05-23 13:48:00 +0000

    en profiter pour dupliquer le dossier en formidable_participation
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@82702 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ formidable_participation/traiter/inscription.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Traitement inscription à la saisie d'un formulaire
+ *
+ * @plugin     Formulaires d'inscription
+ * @copyright  2014
+ * @author     Anne-lise Martenot
+ * @licence    GNU/GPL
+ * @package    SPIP\Formidableinscription\traiter\inscription
+ */
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+function traiter_inscription_dist($args, $retours){
+	$formulaire = $args['formulaire'];
+	$options = $args['options'];
+	$saisies = unserialize($formulaire['saisies']);
+	$traitements = unserialize($formulaire['traitements']);
+	$champs = saisies_lister_champs($saisies);
+
+	// saisies dans le formulaire
+	if ($options['champ_choix_abo'])
+	  $choix_abo = _request($options['champ_choix_abo']);
+
+	if ($options['champ_email_abo'])
+	  $email_abo = _request($options['champ_email_abo']);
+	
+	
+	if ($options['champ_nom_abo'])
+	  $nom_abo = _request($options['champ_nom_abo']);
+	
+	
+	if ($options['champ_prenom_abo'])
+	  $prenom_abo = _request($options['champ_prenom_abo']);
+	
+	if ($options['champ_organisme_abo'])
+	  $organisme_abo = _request($options['champ_organisme_abo']);
+		  
+	if ($options['champ_choix_abo']){
+		  $choix_abo = _request($options['champ_choix_abo']);
+		  
+		  if ($options['choix_abo_oui'])
+		  	  $abo_oui = $options['choix_abo_oui'];
+		  
+		  if($choix_abo == $abo_oui) $choix_abo='inscription';
+		  else $choix_abo='desinscription';
+	}
+	  
+	$options = array(
+		'choix_abo' => $choix_abo,
+		'email' => $email_abo,
+		'nom' => $nom_abo,
+		'prenom' => $prenom_abo,
+		'organisme' => $organisme_abo,
+		'id_auteur' => (isset($GLOBALS['visiteur_session']['id_auteur'])?$GLOBALS['visiteur_session']['id_auteur']:0),
+		'parrain' => 'form'.$formulaire['id_formulaire'].':'.$formulaire['identifiant'],
+		'tracking_id' => $id_formulaires_reponse,
+		'traiter_inscription' => false,
+	);
+	
+	// fabrique le pipeline traiter_formidableinscription.
+	$pipeline = pipeline('traiter_formidableinscription',array('args'=>$options,'data'=>$pipeline));
+
+	spip_log("$choix_abo pour $email_abo","formidable_abo");
+	
+	return $retours;
+}
