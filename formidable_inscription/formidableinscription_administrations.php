5e1ea7557f968795e4cec59b988575b729032cd5
toutati@free.fr <toutati@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-05-23 20:29:11 +0000

    nomme correctement et évite de retraiter l'inscription derrière
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@82714 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- formidable_inscription/formidableinscription_administrations.php
+++ formidable_inscription/formidableinscription_administrations.php
@@ -11,7 +11,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 
 
 /**
- * Fonction d'installation et de mise à jour du plugin Formulaires de abonnement.
+ * Fonction d'installation et de mise à jour du plugin Formulaires d'inscription
  *
  * @param string $nom_meta_base_version
  *     Nom de la meta informant de la version du schéma de données du plugin installé dans SPIP
@@ -28,7 +28,7 @@ function formidableinscription_upgrade($nom_meta_base_version, $version_cible) {
 
 
 /**
- * Fonction de désinstallation du plugin Formulaires de abonnement.
+ * Fonction de désinstallation du plugin Formulaires d'inscription
  *
  * @param string $nom_meta_base_version
  *     Nom de la meta informant de la version du schéma de données du plugin installé dans SPIP
--- formidable_inscription/traiter/inscription.php
+++ formidable_inscription/traiter/inscription.php
@@ -19,39 +19,39 @@ function traiter_inscription_dist($args, $retours){
 	$champs = saisies_lister_champs($saisies);
 
 	// saisies dans le formulaire
-	if ($options['champ_choix_abo'])
-	  $choix_abo = _request($options['champ_choix_abo']);
+	if ($options['champ_choix_inscription'])
+	  $choix_inscription = _request($options['champ_choix_inscription']);
 
-	if ($options['champ_email_abo'])
-	  $email_abo = _request($options['champ_email_abo']);
+	if ($options['champ_email_inscription'])
+	  $email_inscription = _request($options['champ_email_inscription']);
 	
 	
-	if ($options['champ_nom_abo'])
-	  $nom_abo = _request($options['champ_nom_abo']);
+	if ($options['champ_nom_inscription'])
+	  $nom_inscription = _request($options['champ_nom_inscription']);
 	
 	
-	if ($options['champ_prenom_abo'])
-	  $prenom_abo = _request($options['champ_prenom_abo']);
+	if ($options['champ_prenom_inscription'])
+	  $prenom_inscription = _request($options['champ_prenom_inscription']);
 	
-	if ($options['champ_organisme_abo'])
-	  $organisme_abo = _request($options['champ_organisme_abo']);
+	if ($options['champ_organisme_inscription'])
+	  $organisme_inscription = _request($options['champ_organisme_inscription']);
 		  
-	if ($options['champ_choix_abo']){
-		  $choix_abo = _request($options['champ_choix_abo']);
+	if ($options['champ_choix_inscription']){
+		  $choix_inscription = _request($options['champ_choix_inscription']);
 		  
-		  if ($options['choix_abo_oui'])
-		  	  $abo_oui = $options['choix_abo_oui'];
+		  if ($options['choix_inscription_oui'])
+		  	  $inscription_oui = $options['choix_inscription_oui'];
 		  
-		  if($choix_abo == $abo_oui) $choix_abo='inscription';
-		  else $choix_abo='desinscription';
+		  if($choix_inscription == $inscription_oui) $choix_inscription='inscription';
+		  else $choix_inscription='desinscription';
 	}
 	  
 	$options = array(
-		'choix_abo' => $choix_abo,
-		'email' => $email_abo,
-		'nom' => $nom_abo,
-		'prenom' => $prenom_abo,
-		'organisme' => $organisme_abo,
+		'choix_inscription' => $choix_inscription,
+		'email' => $email_inscription,
+		'nom' => $nom_inscription,
+		'prenom' => $prenom_inscription,
+		'organisme' => $organisme_inscription,
 		'id_auteur' => (isset($GLOBALS['visiteur_session']['id_auteur'])?$GLOBALS['visiteur_session']['id_auteur']:0),
 		'parrain' => 'form'.$formulaire['id_formulaire'].':'.$formulaire['identifiant'],
 		'tracking_id' => $id_formulaires_reponse,
@@ -61,7 +61,10 @@ function traiter_inscription_dist($args, $retours){
 	// fabrique le pipeline traiter_formidableinscription.
 	$pipeline = pipeline('traiter_formidableinscription',array('args'=>$options,'data'=>$pipeline));
 
-	spip_log("$choix_abo pour $email_abo","formidable_abo");
+	spip_log("$choix_inscription pour $email_inscription","formidable_inscription");
+	
+	// noter qu'on a deja fait le boulot, pour ne pas risquer double appel
+	$retours['traitements']['inscription'] = true;
 	
 	return $retours;
 }
