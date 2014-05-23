3a50b22b8b1ac3159c11f583f05b4e6389a5acf7
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-05-23 07:05:52 +0000

    report du trunk dans la branches
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@82695 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ medias_nettoyage/branches/v0/lang/medias_nettoyage_fr.php
@@ -0,0 +1,71 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'aucun_document_orphelin' => 'Il n\'y a aucun document orphelin',
+
+	'cfg_titre_parametrages' => 'Paramétrages',
+
+	// D
+	'des_documents' => '@nb@ documents',
+	'des_repertoires' => '@nb@ répertoires',
+
+	// E
+	'explication_activation_horaires' => 'Désirez-vous que le nettoyage se fasse à tout moment de la journée ? Ou juste dans une tranche horaire pour ne pas pénaliser les performances du site ?',
+	'explication_horaires' => 'Veuillez sélectionner la tranche horaire dans laquel le nettoyage de la médiathèque pourra se faire. <em>(Par défaut entre 00h00 et 06h00)</em>',
+
+	// H
+	'horaire_00_06' => '00h00 - 06h00',
+	'horaire_06_12' => '06h00 - 12h00',
+	'horaire_12_18' => '12h00 - 18h00',
+	'horaire_18_24' => '18h00 - 24h00',
+
+	// I
+	'info_nom' => 'Nom',
+	'info_nombre' => 'Nombre',
+	'info_nombre_abbr' => 'Nb',
+	'info_taille' => 'Taille',
+	'info_nombre_documents_bdd' => 'Documents non-distants',
+	'info_nombre_documents_bdd_orphelins' => 'Documents non-distants sans fichiers physiques',
+	'info_nombre_documents_bdd_complet' => 'Tous les documents',
+	'info_nombre_documents_repertoire' => 'Fichiers dans les répertoires d\'extensions',
+	'info_nombre_documents_repertoire_complet' => 'Tous les fichiers',
+	'info_nombre_documents_repertoire_orphelins' => 'Fichiers orphelins avec extensions (sauf logos)',
+	'info_nombre_logos_fichiers' => 'Logos des objets de SPIP',
+	'info_nombre_logos_fichiers_off' => 'Logos en mode "off"',
+	'info_nombre_logos_fichiers_on' => 'Logos en mode "on"',
+	'info_nombre_repertoires_racine' => 'Répertoires à la racine',
+	'info_titre_bdd' => 'En base de données',
+	'info_titre_logos_img' => 'Les logos dans IMG/',
+	'info_titre_repertoire_img' => 'Répertoire IMG/',
+	'info_titre_repertoire_orphelins' => 'Répertoire IMG/orphelins',
+
+	// L
+	'label_activation_horaires' => 'Activer le nettoyage par tranche horaires',
+	'label_horaires' => 'Tranches horaires',
+
+	'non' => 'Non <em>(peut faire ralentir votre site)</em>',
+
+	// O
+	'onglet_titre_rep_orphelins' => 'Documents orphelins',
+	'onglet_titre_rep_img' => 'Répertoire IMG/',
+	'onglet_titre_tabbord' => 'Tableau de bord',
+	'oui' => 'Oui <strong>(recommandé)</strong>',
+
+	// T
+	'titre_configurer_medias_nettoyage' => 'Configurer le nettoyage de la médiathèque',
+	'titre_page_configurer_medias_nettoyage' => 'Configurer le nettoyage de la médiathèque',
+	'titre_page_medias_rep_orphelins' => 'Documents dans le répertoire "orphelins"',
+	'titre_page_medias_rep_img' => 'Liste des répertoires dans IMG/',
+	'titre_page_medias_tabbord' => 'Tableau de bord - Documents',
+	'titre_tableau_documents_orphelins' => 'Documents orphelins',
+	'titre_tableau_documents_repertoire_orphelins' => 'Contenu dans IMG/orphelins',
+
+	// U
+	'un_document' => '@nb@ document',
+	'un_repertoire' => '@nb@ répertoire',
+);
+?>
