49a7e5840030320e9e37f1dcee9a61266aa696e5
toutati@free.fr <toutati@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-05-23 14:19:23 +0000

    On dit une participation normalement.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@82706 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- formidable_participation/lang/formidable_participation_fr.php
+++ formidable_participation/lang/formidable_participation_fr.php
@@ -6,22 +6,22 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// F
-	'formidableparticipation_titre' => 'Formulaires d\'participation',
+	'formidableparticipation_titre' => 'Formulaires de participation',
 
 	'traiter_participation_titre' => 'Participation',
-	'traiter_participation_description' => 'Traiter un participation à la saisie du formulaire',
+	'traiter_participation_description' => 'Traiter une participation à la saisie du formulaire',
 
 	'traiter_participation_champ_email_label' => 'Champ de l\'Email',
-	'traiter_participation_champ_choix_abo_label' => 'Champ de l\'participation',
-	'traiter_participation_champ_choix_abo_explication' => 'Le champ qui propose participation ou désparticipation',
-	'traiter_participation_champ_abo_oui'=>'Valeur pour l\'participation',
-	'traiter_participation_champ_abo_non'=>'Valeur pour le désparticipation',
+	'traiter_participation_champ_choix_abo_label' => 'Champ de la participation',
+	'traiter_participation_champ_choix_abo_explication' => 'Le champ qui propose participation ou non-participation',
+	'traiter_participation_champ_abo_oui'=>'Valeur pour la participation',
+	'traiter_participation_champ_abo_non'=>'Valeur pour le non-participation',
 	'traiter_participation_option_champ_nom_label' => 'Champ du Nom',
 	'traiter_participation_option_champ_prenom_label' => 'Champ du Prénom',
 	'traiter_participation_option_champ_organisme_label' => 'Champ de l\'Organisme',
 
-	'traiter_participation_option_message_label' => 'Message après participation réussi',
-	'traiter_participation_necessite_explication' => 'Pour la prise en charge de l\'participation, il est nécessaire d\'enregistrer les résultats dans la base de données.',
+	'traiter_participation_option_message_label' => 'Message après participation réussie',
+	'traiter_participation_necessite_explication' => 'Pour la prise en charge de la participation, il est nécessaire d\'enregistrer les résultats dans la base de données.',
 
 
 );
