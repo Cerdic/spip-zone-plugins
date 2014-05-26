56ccfceca4075bc30df8a8097f853a3948d1924b
fil@rezo.net <fil@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-05-26 13:45:55 +0000

    pensez à moi qui n'ai pas de crochets [] pour désigner les array()
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@82748 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- indexer/trunk/Sources/Articles.php
+++ indexer/trunk/Sources/Articles.php
@@ -10,7 +10,7 @@ class Articles extends SpipDocuments {
 
     public function getDocuments($start = 0, $end = 0, $column = 'id_article') {
 
-        $where = [];
+        $where = array();
         if ($start) $where[] = "$column >= $start";
         if ($end)   $where[] = "$column < $end";
 
@@ -23,7 +23,7 @@ class Articles extends SpipDocuments {
             '' // Limit
         );
 
-        $documents = [];
+        $documents = array();
         foreach ($all as $article) {
             $documents[] = $this->createDocumentArticle($article);
         }
@@ -35,7 +35,7 @@ class Articles extends SpipDocuments {
     public function createDocumentArticle($article) {
          $id = $article['id_article'];
 
-         return new Document([
+         return new Document(array(
             'id'           => $this->getObjectId('article', $id),
             'title'        => supprimer_numero($article['titre']),
             'summary'      => $article['surtitre'] . $article['soustitre'] . $article['chapo'],
@@ -43,19 +43,19 @@ class Articles extends SpipDocuments {
             'date'         => (substr($article['date_redac'],0,4) == '0000') ? $article['date'] : $article['date_redac'],
             'uri'          => generer_url_entite_absolue($id, 'article'),
             'properties'   =>
-            [
+            array(
                 'authors'  => $this->getAuthorsProperties('article', $id),
                 'tags'     => $this->getTagsProperties('article', $id),
                 'objet'    => 'article',
                 'id_objet' => $id,
                 'lang'     => $article['lang']
-            ]
-        ]);
+            )
+        ));
     }
 
 
     public function getBounds() {
-        return $bornes = sql_fetsel(['MIN(id_article) AS min', 'MAX(id_article) AS max'], 'spip_articles');
+        return $bornes = sql_fetsel(array('MIN(id_article) AS min', 'MAX(id_article) AS max'), 'spip_articles');
     }
 
 
--- indexer/trunk/Sources/SpipDocuments.php
+++ indexer/trunk/Sources/SpipDocuments.php
@@ -44,16 +44,16 @@ class SpipDocuments implements SourceInterface {
 
     public function getAuthorsProperties($objet, $id_objet) {
         if ($this->tables_liens) {
-            $auteurs = sql_allfetsel('a.nom', 'spip_auteurs AS a, spip_auteurs_liens AS al', [
+            $auteurs = sql_allfetsel('a.nom', 'spip_auteurs AS a, spip_auteurs_liens AS al', array(
                 "al.id_objet = " . intval($id_objet),
                 "al.objet    = " . sql_quote($objet),
                 "a.id_auteur = al.id_auteur",
-            ]);
+            ));
         } else {
-            $auteurs = sql_allfetsel('a.nom', 'spip_auteurs AS a, spip_auteurs_articles AS al', [
+            $auteurs = sql_allfetsel('a.nom', 'spip_auteurs AS a, spip_auteurs_articles AS al', array(
                 "al.id_article = " . intval($id_objet),
                 "a.id_auteur = al.id_auteur",
-            ]);
+            ));
         }
         return array_map('array_shift', $auteurs);
     }
@@ -61,23 +61,23 @@ class SpipDocuments implements SourceInterface {
 
     public function getTagsProperties($objet, $id_objet) {
         if ($this->tables_liens) {
-            $tags = sql_allfetsel('m.titre', 'spip_mots AS m, spip_mots_liens AS ml', [
+            $tags = sql_allfetsel('m.titre', 'spip_mots AS m, spip_mots_liens AS ml', array(
                 "ml.id_objet = " . intval($id_objet),
                 "ml.objet    = " . sql_quote($objet),
                 "m.id_mot = ml.id_mot",
-            ]);
+            ));
         } else {
-            $tags = sql_allfetsel('m.titre', 'spip_mots AS m, spip_mots_articles AS ml', [
+            $tags = sql_allfetsel('m.titre', 'spip_mots AS m, spip_mots_articles AS ml', array(
                 "ml.id_article = " . intval($id_objet),
                 "m.id_mot = ml.id_mot",
-            ]);
+            ));
         }
         return array_map('array_shift', $tags);
     }
 
 
     public function getBounds() {
-        return ['min' => 0, 'max' => 0];
+        return array('min' => 0, 'max' => 0);
     }
 
     /**
@@ -88,12 +88,12 @@ class SpipDocuments implements SourceInterface {
     **/
     public function getParts($count) {
         $bornes = $this->getBounds();
-        $parts = [];
+        $parts = array();
         for ($i = $bornes['min']; $i <= $bornes['max']; $i += $count) {
-            $parts[] = [
+            $parts[] = array(
                 'start' => $i,
                 'end'   => $i + $count
-            ];
+            );
         }
         return $parts;
     }
