94f63e413f2857c4d9e579dfbfc4af599d922bc2
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-05-28 00:08:46 +0000

    On liste automatiquement les objets ayant un fichier de lang. exemple : lang/forum_fr.php, lang/projet_fr.php lang/projets_cadre_fr.php, etc.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@82776 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- doc_tech/trunk/doc_tech_fonctions.php
+++ doc_tech/trunk/doc_tech_fonctions.php
@@ -9,7 +9,29 @@
  * @package    SPIP\Doc_tech\Fonctions
  */
 
-if (!defined('_ECRIRE_INC_VERSION')) return;
+if (!defined('_ECRIRE_INC_VERSION')) {
+    return;
+}
 
+function doc_tech_lister_objet()
+{
+    include_spip('base/objets');
+    include_spip('inc/config');
+    $objets_principales =  array_keys(lister_tables_principales());
+    // On va prendre la langue du site comme référence pour la langue de l'objet
+    $langue_site = lire_config('langue_site');
+
+    foreach ($objets_principales as $objet) {
+        $type = objet_type($objet);
+        // On recherche les onjet ayant une chaîne de langue selon le  type
+        // Exemple : lang/forum_fr.php
+        // lang/projet_fr.php
+        $lang = find_in_path("lang/" . $type . "_" . $langue_site . ".php");
+        if ($lang) {
+            $liste_objet[] = $type;
+        }
+    }
+    return $liste_objet;
+}
 
 ?>
