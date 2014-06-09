e1ae9a1e4f982a7881bb1940db8bfbe61a74450b
suske@brubel.net <suske@brubel.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-06-09 15:58:15 +0000

    oups
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@82913 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- odt2spip/trunk/odt2spip_pipelines.php
+++ odt2spip/trunk/odt2spip_pipelines.php
@@ -31,12 +31,6 @@ function odt2spip_affiche_gauche($flux){
 		$out = recuperer_fond('formulaires/odt2spip', array('id_rubrique' => $id_rubrique, 'exec' => 'rubrique'));
 		$flux['data'] .= $out;
 	}
-	if ($flux['args']['exec']=='article'
-	  AND $id_article = $flux['args']['id_article']
-	  AND autoriser('ecrire')){
-		$out = recuperer_fond('formulaires/odt2spip', array('id_article' => $id_article));
-		$flux['data'] .= $out;
-	}
 	elseif ($flux['args']['exec']=='article' 
 		AND $id_article = $flux['args']['id_article']
 		AND autoriser('modifier', 'article', $id_article)){
