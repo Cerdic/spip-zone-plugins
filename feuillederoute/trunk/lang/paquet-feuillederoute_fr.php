137c7cf6c7bfabe29c7b74ea721c2472711ddefe
chankalan@free.fr <chankalan@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-06-12 06:58:55 +0000

    description du plugin : un brin tâtillon, mais c'est plus juste
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@82976 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- feuillederoute/trunk/lang/paquet-feuillederoute_fr.php
+++ feuillederoute/trunk/lang/paquet-feuillederoute_fr.php
@@ -4,7 +4,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// F
-	'feuillederoute_description' => 'Ce plugin ajoute un bouton d\'administration sur le site public pour afficher/masquer un texte simple, hors tout contexte éditorial. Il est sensé être un moyen de communication entre webmestres, administrateurs et rédacteurs.
+	'feuillederoute_description' => 'Ce plugin ajoute un bouton d\'administration sur chaque page du site, publique comme privée, pour afficher/masquer un texte simple, hors tout contexte éditorial. Il est sensé être un moyen de communication entre webmestres, administrateurs et rédacteurs.
 	Il est compatible avec le plugin Minibando.',
 	'feuillederoute_nom' => 'Feuille de Route',
 	'feuillederoute_slogan' => 'Un bouton d\'admin pour partager des informations hors publication.'
