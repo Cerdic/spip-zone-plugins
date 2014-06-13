195145ee41afb47011e99bfc4671c68888c04bf3
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-06-13 08:45:10 +0000

    Tous les critères de tri des menus sont reversibles en les prefixant par un point d'exclamation ! Permet de supprimer 4 parametres du menu rubriques_completes, c'est pas du luxe... (+upgrade de base qui rend ça transparent) On pourrait aussi fusionner les 2 parametres tri numérique et tri alpha en permettant la saisie de "num titre" et en gérant derrière automatiquement pour que ça marche
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@83018 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- menus/trunk/menus_administrations.php
+++ menus/trunk/menus_administrations.php
@@ -21,9 +21,58 @@ function menus_upgrade($nom_meta_version_base, $version_cible){
 	$maj['0.5.1'] = array(
 		array('sql_updateq', 'spip_menus_entrees', array('type_entree'=>'rubriques_completes'), 'type_entree = '.sql_quote('rubriques')),
 	);
-	
+	$maj['0.5.2'] = array(
+		array('menus_fusionne_critere_tri_inverse'),
+	);
+
 	include_spip('base/upgrade');
 	maj_plugin($nom_meta_version_base, $version_cible, $maj);
+}
+
+function menus_fusionne_critere_tri_inverse(){
+
+	$res = sql_select("*","spip_menus_entrees","","","id_menus_entree");
+	while ($row = sql_fetch($res)){
+		#var_dump($row);
+		$params = unserialize($row['parametres']);
+
+		$change = false;
+		if (isset($params['tri_num_inverse'])
+			AND $params['tri_num_inverse']=='oui'
+		  AND strlen($params['tri_num'])){
+			$params['tri_num'] = "!".$params['tri_num'];
+			$change = true;
+			unset($params['tri_num_inverse']);
+		}
+		if (isset($params['tri_alpha_inverse'])
+			AND $params['tri_alpha_inverse']=='oui'
+		  AND strlen($params['tri_alpha'])){
+			$params['tri_alpha'] = "!".$params['tri_alpha'];
+			$change = true;
+			unset($params['tri_alpha_inverse']);
+		}
+		if (isset($params['tri_num_articles_inverse'])
+			AND $params['tri_num_articles_inverse']=='oui'
+		  AND strlen($params['tri_num_articles'])){
+			$params['tri_num_articles'] = "!".$params['tri_num_articles'];
+			$change = true;
+			unset($params['tri_num_articles_inverse']);
+		}
+		if (isset($params['tri_alpha_articles_inverse'])
+			AND $params['tri_alpha_articles_inverse']=='oui'
+		  AND strlen($params['tri_alpha_articles'])){
+			$params['tri_alpha_articles'] = "!".$params['tri_alpha_articles'];
+			$change = true;
+			unset($params['tri_alpha_articles_inverse']);
+		}
+
+		if ($change){
+			$params = serialize($params);
+			sql_updateq("spip_menus_entrees",array('parametres'=>$params),"id_menus_entree=".intval($row['id_menus_entree']));
+			#var_dump($params);
+		}
+
+	}
 }
 
 // Désinstallation
