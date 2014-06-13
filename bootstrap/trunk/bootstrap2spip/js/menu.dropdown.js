49f36238b44949d03d739366a800b146c39fe4e9
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-06-13 07:40:56 +0000

    supprimer l'eventuelle class nav du sous menu, ajoutée par le plugin menus (qui ne sait pas si on fait un menu deroulant ou un menu liste vertical)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@83013 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- bootstrap/trunk/bootstrap2spip/js/menu.dropdown.js
+++ bootstrap/trunk/bootstrap2spip/js/menu.dropdown.js
@@ -6,7 +6,7 @@ jQuery(function(){
 		jQuery(this)
 			.find('.menu-items').eq(0)
 			.children('.item').children('.menu-items').parent().addClass('dropdown')
-			.children('.menu-items').addClass('dropdown-menu')
+			.children('.menu-items').addClass('dropdown-menu').removeClass('nav')
 			.siblings('a').addClass('dropdown-toggle').attr('data-toggle', 'dropdown').append('<b class="caret"></b>').dropdown()
 			.siblings('.menu-items')
 			.find('.menu-items').hide();
