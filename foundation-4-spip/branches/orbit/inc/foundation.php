7ba7d67eeb701e6f9ff59735793b0e90098aef0a
p@henix.be <p@henix.be@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-06-17 17:38:41 +0000

    Les fonctions qui récupère CSS et javascript dans le inc/foundation
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@83331 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- foundation-4-spip/branches/orbit/inc/foundation.php
+++ foundation-4-spip/branches/orbit/inc/foundation.php
@@ -20,60 +20,52 @@ function responsive($matches) {
   return wrap('<iframe '.$matches[0].'></iframe>', '<div class="flex-video'.$vimeo.'">');;
 }
 
+
 /**
- * Pas très élégant, cette fonction va renvoyer le nombre en toutes lettre.
- * Dans ce cas si, cela suffit largement puisqu'il n'y a que 12 chiffres possible.
- * C'est utiliser pour foundation 2 et 3.
- * @param  int $number nombre de colonne foundation.
- * @return string         string définissant la largeur de la colonne foundation.
+ * Récupération des fichier javascript de foundation
  */
-function toWords($number) {
+function foundation_get_js() {
 
-  // Ce tableau fait office de table de conversion.
-  $conversion = array(
-                      1 => 'one',
-                      2 => 'two',
-                      3 => 'three',
-                      4 => 'four',
-                      5 => 'five',
-                      6 => 'six',
-                      7 => 'seven',
-                      8 => 'eight',
-                      9 => 'nine',
-                      11 => 'eleven',
-                      12 => 'twelve'
-                      );
+  $flux = '';
 
-    // On revoie la bonne valeur.
-  if (is_numeric($number))
-    return $conversion[$number];
-  else return false;
+  // On lit la configuration du plugin pour savoir quel version de Foundation charger.
+  $config = lire_config('foundation');
+
+  // On renvoie le flux head avec le squelette foundation correspondant.
+  if ($config['variante'] == '3')
+    $flux = $flux.recuperer_fond('inclure/head-foundation-3');
+  elseif ($config['variante'] == '4')
+    $flux = $flux.recuperer_fond('inclure/head-foundation-4');
+  elseif ($config['variante'] == '5')
+    $flux = $flux.recuperer_fond('inclure/head-foundation-5');
+  // Si foundation est désactivé, on revoie directement le flux, sans aller chercher le head-foundation.
+  elseif ($config['variante'] == '0')
+    return $flux;
+
+  // Charger le head commun a foundation
+  $flux = $flux.recuperer_fond('inclure/head-foundation');
+
+  return $flux;
 }
 
 /**
- * Cette fonction va lire la configuration de foundation et determiner quel syntaxe doit être utilisé.
- * @param  int $nombre_colonnes Nombre de colonne désiré
- * @param  string $type            Foundation 4/5, type de colonne (large, medium, small)
- * @return string                  class foundation applicable directement.
+ * Récupération des fichier css de foundation
  */
-function trouver_syntaxe_foundation($nombre_colonnes, $type) {
-
-  // On récupère la configuration
+function foundation_get_css() {
+  $flux = '';
+  // On lit la configuration du plugin pour savoir quel version de Foundation charger.
   $config = lire_config('foundation');
 
-  // Version qui utilise un système large-X ou small-X. J'appel ce groupe les colnum.
-  $colnum = array(4,5);
+  // On renvoie le flux head avec le squelette foundation correspondant.
+  if ($config['variante'] == '3')
+    $flux = $flux.recuperer_fond('inclure/css/head-foundation-3');
+  elseif ($config['variante'] == '4')
+    $flux = $flux.recuperer_fond('inclure/css/head-foundation-4');
+  elseif ($config['variante'] == '5')
+    $flux = $flux.recuperer_fond('inclure/css/head-foundation-5');
+  // Si foundation est désactivé, on revoie directement le flux, sans aller chercher le head-foundation.
+  elseif ($config['variante'] == '0')
+    return $flux;
 
-  // Les versions qui utilise des lettres => les colletr
-  $colettr = array(2,3);
-
-  // on cherche
-  // Si on est dans une vesion numérique de foundation, on retourne la syntaxe
-  if (in_array($config['variante'], $colnum))
-    return $type.'-'.$nombre_colonnes;
-  // Sinon, on démarrer le moteur de conversion de nombre, et on renvoie la bonne class
-  elseif (in_array($config['variante'], $colettr)) {
-    include_spip('inc/foundation');
-    return toWords($nombre_colonnes);
-  }
+  return $flux;
 }
