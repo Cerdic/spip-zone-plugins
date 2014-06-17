68798a443dc85887f081cee5191899abfd4ce337
phenix-factory <phenix-factory@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-06-17 17:38:41 +0000

    Mise à jour description.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@83143 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- foundation-4-spip/branches/orbit/lang/paquet-foundation_fr.php
+++ foundation-4-spip/branches/orbit/lang/paquet-foundation_fr.php
@@ -6,9 +6,11 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// F
-	'foundation_description' => 'Plugin qui intégre le framework Foundation à l\'interface public de SPIP.',
+	'foundation_description' => 'Plugin qui intégre le framework Foundation à l\'interface public de SPIP.
+    
+    Foundation-4-SPIP installe également les plugins indispensables pour un SPIP.',
 	'foundation_nom' => 'Foundation-4-spip',
-	'foundation_slogan' => 'Foundation grid',
+	'foundation_slogan' => 'Foundation grid.',
 );
 
 ?>
