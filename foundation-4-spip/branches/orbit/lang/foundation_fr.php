c6a36be13b1ec9b10f97f6a82dd49849cb739551
phenix-factory <phenix-factory@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-06-17 17:38:41 +0000

    Internationnalisation.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@83165 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- foundation-4-spip/branches/orbit/lang/foundation_fr.php
+++ foundation-4-spip/branches/orbit/lang/foundation_fr.php
@@ -4,17 +4,20 @@
 if (!defined('_ECRIRE_INC_VERSION')) return;
  
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-
-	// F
-	'foundation_titre' => 'foundation-4-spip',
-
-	// C
-	'cfg_exemple' => 'Exemple',
-	'cfg_exemple_explication' => 'Explication de cet exemple',
-	'cfg_titre_parametrages' => 'Paramétrages',
-
-	// T
-	'titre_page_configurer_foundation' => 'Configuration de Fondation',
+    'cfg_exemple' => 'Exemple',
+    'cfg_exemple_explication' => 'Explication de cet exemple',
+    'cfg_titre_parametrages' => 'Paramétrages',
+    'desactiver' => 'Désactiver Foundation',
+    'foundation_titre' => 'foundation-4-spip',
+    'titre_page_configurer_foundation' => 'Configuration de Fondation',
+    '3' => 'Foundation 3',
+    '4' => 'Foundation 4',
+    'activer_javascript' => 'Activer les javascripts de foundation ?',
+    'activer_javascript_explication' => 'Si vous n\'utilisez pas les composants javascripts de foundation il est inutile de les charger dans SPIP.',
+    'fichier_htc' => 'Activer le fichier boxsizing.htc ?',
+    'fichier_htc_explication' => 'Ce fichier peu améliorer la gestion de foundation sous IE7/8. Il faudra activer les fichier .htc dans le fichier .htaccess en ajoutant "AddType text/x-component .htc".',
+    'version' => 'Version de foundation',
+    'version_explication' => 'Choisir la version de foundation à utiliser dans l\'espace public.',
 );
 
 ?>
