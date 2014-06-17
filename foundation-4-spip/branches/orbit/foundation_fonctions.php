3b729d370c543a445513013b87d864860115047c
p@henix.be <p@henix.be@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-06-17 17:38:41 +0000

    On allège le fichier foundation_fonctions.php
    On créer un dossier inc avec un foundation.php
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@83324 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- foundation-4-spip/branches/orbit/foundation_fonctions.php
+++ foundation-4-spip/branches/orbit/foundation_fonctions.php
@@ -9,29 +9,13 @@
  */
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
-
-/**
- * Fonction de callback
- * A chaque iFrame, on encadre de div.flex-video.
- * Si vimeo est détecté, on ajoute la class vimeo
- * @param  string $matches iframe
- * @return string          iframe encadrée
- */
-function responsive($matches) {
-  // Dans le cas de vimeo, il faut ajouter une classe
-  if (strpos($matches[0], 'vimeo')) $vimeo = ' vimeo';
-  else $vimeo = '';
-
-  // On revoie la bonne structure html d'iframe.
-  return '<div class="flex-video'.$vimeo.'"><iframe '.$matches[0].'></iframe></div>';
-}
-
 /**
  * Rendre les iframes responsive via un filtre et la classe flex-video de Foundation.
  * @param  string $texte HTML pouvant contenir des iFrames
  * @return string        HTML avec les iFrames modifiée pour être responsive.
  */
 function filtre_iframe_responsive($texte) {
+  include_spip('inc/foundation');
   // On détecte tout les iFrames et on les rends responsives.
   return preg_replace_callback('/<iframe(.+)><\/iframe>/', 'responsive', $texte);
 }
---
+++ foundation-4-spip/branches/orbit/inc/foundation.php
@@ -0,0 +1,18 @@
+<?php
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+/**
+ * Fonction de callback utiliser par le filtre |iframe_responsive
+ * A chaque iFrame, on encadre de div.flex-video.
+ * Si vimeo est détecté, on ajoute la class vimeo
+ * @param  string $matches iframe
+ * @return string          iframe encadrée
+ */
+function responsive($matches) {
+  // Dans le cas de vimeo, il faut ajouter une classe
+  if (strpos($matches[0], 'vimeo')) $vimeo = ' vimeo';
+  else $vimeo = '';
+
+  // On revoie la bonne structure html d'iframe.
+  return '<div class="flex-video'.$vimeo.'"><iframe '.$matches[0].'></iframe></div>';
+}
