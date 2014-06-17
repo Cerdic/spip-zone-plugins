6de9b9766bd4843774c39171220c6e2600f9d929
phenix-factory <phenix-factory@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-06-17 17:38:41 +0000

    On fonctionne par version Majeur de foundation plutôt que par version précise, c'es plus simple pour la maintenance.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@83133 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- foundation-4-spip/branches/orbit/foundation_pipelines.php
+++ foundation-4-spip/branches/orbit/foundation_pipelines.php
@@ -19,8 +19,8 @@ function foundation_insert_head ($flux) {
     // On lit la configuration du plugin pour savoir quel version de Foundation charger.
     $config = lire_config('foundation');
 
-    if ($config['foundation_version'] == '4.1.2') return $flux.recuperer_fond('inclure/head-foundation-4.1.2');
-    elseif ($config['foundation_version'] == '3.2.5') return $flux.recuperer_fond('inclure/head-foundation-3.2.5');
+    if ($config['foundation_version'] == '4') return $flux.recuperer_fond('inclure/head-foundation-4');
+    elseif ($config['foundation_version'] == '3') return $flux.recuperer_fond('inclure/head-foundation-3');
 }
 
 ?>
