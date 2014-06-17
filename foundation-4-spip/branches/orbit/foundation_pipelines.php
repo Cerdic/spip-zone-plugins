1ca252c00df6decbe91453d41e93a3f9d704bf1d
p@henix.be <p@henix.be@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-06-17 17:38:41 +0000

    On sépare le CSS du javascript dans les pipelines idoine (oui, idoine!)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@83216 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- foundation-4-spip/branches/orbit/foundation_pipelines.php
+++ foundation-4-spip/branches/orbit/foundation_pipelines.php
@@ -32,4 +32,23 @@ function foundation_insert_head ($flux) {
     return $flux;
 }
 
+/*
+*   Pipeline Insert_head_css
+*/
+function foundation_insert_head_css ($flux) {
+
+    // On lit la configuration du plugin pour savoir quel version de Foundation charger.
+    $config = lire_config('foundation');
+
+    // On renvoie le flux head avec le squelette foundation correspondant.
+    if ($config['variante'] == '3') $flux = $flux.recuperer_fond('inclure/css/head-foundation-3');
+    elseif ($config['variante'] == '4') $flux = $flux.recuperer_fond('inclure/css/head-foundation-4');
+    elseif ($config['variante'] == '5') $flux = $flux.recuperer_fond('inclure/css/head-foundation-5');
+    // Si foundation est désactivé, on revoie directement le flux, sans aller chercher le head-foundation.
+    elseif ($config['variante'] == '0') return $flux;
+
+    return $flux;
+}
+
+
 ?>
