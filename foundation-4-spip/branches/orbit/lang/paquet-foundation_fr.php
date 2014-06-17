eeebee5553b5c63804973e7a4b1b85a129496938
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-06-17 17:38:41 +0000

    Quelques corrections dans le paquet.xml (c'est un outil pas un squelette). Améliorer le slogan. Néanmoins, doper la description ne ferait pas de mal !
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@83180 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- foundation-4-spip/branches/orbit/lang/paquet-foundation_fr.php
+++ foundation-4-spip/branches/orbit/lang/paquet-foundation_fr.php
@@ -8,7 +8,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// F
 	'foundation_description' => 'Plugin qui intégre le framework Foundation à l\'interface public de SPIP.',
 	'foundation_nom' => 'Foundation-4-spip',
-	'foundation_slogan' => 'Foundation grid.',
+	'foundation_slogan' => 'Utiliser le framework Foundation dans vos squelettes',
 );
 
 ?>
