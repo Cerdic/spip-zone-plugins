661c1055f5ba1c4855a7837f7a27c545d412ef19
chankalan@free.fr <chankalan@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-06-19 10:30:32 +0000

    ok pour configuration du plugin + test paramétrage de la méthode de génération des vignettes dans les fonctions avancées(merci Beurt)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@83367 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- sjcycle/trunk/formulaires/configurer_sjcycle.php
+++ sjcycle/trunk/formulaires/configurer_sjcycle.php
@@ -2,21 +2,59 @@
 // Securite
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
-// function formulaires_configurer_sjcycle_charger(){
-// 	
-// 	// verifier avant tout la config de SPIP
-// 	$erreurs = array();
-// 	// choix de la methode de traitement des images par le serveur
-// 	if (!lire_config('image_process')){
-// 		$erreurs['message_erreur'] = _T('sjcycle:erreur_config_image_process');
-// 		return $erreurs;
-// 	}
-// 	// Generation de miniatures des images inactive
-// 	if (lire_config('creer_preview')!='oui') {
-// 		$erreurs['message_erreur'] = _T('sjcycle:erreur_config_creer_preview');
-// 		return $erreurs;
-// 	}
-// 	return $erreurs;
-// 	
-// }
+function formulaires_configurer_sjcycle_charger(){
+	
+	// verifier avant tout la config de SPIP
+	$erreurs = array();
+	// choix de la methode de traitement des images par le serveur
+	if (!lire_config('image_process')){
+		$erreurs['message_erreur'] = _T('sjcycle:erreur_config_image_process');
+		return $erreurs;
+	}
+	// Generation de miniatures des images inactive
+	if (lire_config('creer_preview')!='oui') {
+		$erreurs['message_erreur'] = _T('sjcycle:erreur_config_creer_preview');
+		return $erreurs;
+	}
+	
+	// la fonction ci-dessus necessite de contextualiser le chargement
+	// (dans le formulaires/configurer_sjcycle.html on retrouve des valeurs par defaut lorsqu'elles sont initialisees dans sjcycle_administrations.php)
+	$config = lire_config('sjcycle');
+	$valeurs = array(
+		'largeurmax' => $config['largeurmax'],
+		'hauteurmax' => '',
+		'autoheight' => '',
+		'timeout' => $config['timeout'],
+		'speed' => $config['speed'],
+		'delay' => '',
+		'fx' => '',
+		'tilevertical' => '',
+		'carouselvisible' => '',
+		'carouseloffset' => '',
+		'carouselslidedimension' => '',
+		'carouselvertical' => '',
+		'carouselfluid' => '',
+		'reverse' => '',
+		'sync' => '',
+		'pauseonhover' => '',
+		'pauseonhovercontent' => '',
+		'random' => '',
+		'next' => '',
+		'perv' => '',
+		'allowwrap' => '',
+		'paused' => '',
+		'pager' => '',
+		'caption' => '',
+		'captiontemplate' => '',
+		'overlay' => '',
+		'overlaytemplate' => '',
+		'backgroundcolor' => '',
+		'tooltip' => '',
+		'tooltip_carac' => '',
+		'mediabox' => '',
+		'afficher_aide' => $config['afficher_aide']
+	);
+	return $valeurs;
+
+}
 ?>
