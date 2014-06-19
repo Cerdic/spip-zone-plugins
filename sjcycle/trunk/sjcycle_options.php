d90ba9816de308a6965f8819f54b605bd4f38f8d
chankalan@free.fr <chankalan@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-06-19 08:32:35 +0000

    changement de modèle dans les langues + coquilles
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@83364 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- sjcycle/trunk/sjcycle_options.php
+++ sjcycle/trunk/sjcycle_options.php
@@ -4,8 +4,5 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 
 // Le serveur d'aide en ligne pour ce plugin
 $GLOBALS['help_server'][] = url_de_base(1) . str_replace("../", "", _DIR_PLUGIN_SJCYCLE) . "aide/";
-// commente tant que l'aide n'est pas redigee pour le nouveau plugin
-// en meme temps ce serait bien de renvoyer directement sur http://jquery.malsup.com/cycle2/api/ 
-// car c'est tout pareil. Les parametres configurables depuis la page de config le sont également à la rédaction lors de l'inclusion.
 
 ?>
