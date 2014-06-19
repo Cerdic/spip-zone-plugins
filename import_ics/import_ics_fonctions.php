a8922d4e79c28a00481f7ad3bf410f07585ad517
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-06-19 23:59:14 +0000

    On ajoute le lien vers la doc même en chantier
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@83395 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- import_ics/import_ics_fonctions.php
+++ import_ics/import_ics_fonctions.php
@@ -1,50 +1,49 @@
 <?php
 //require_once 'lib/iCalcreator.class.php';/*appeler la librairie qui se trouve dans le plugin icalendar*/
-function mon_filtre($url){
-$config = array("unique_id" => "latp",
-    "url" => $url);
+function mon_filtre ($url) {
+    $config = array("unique_id" => "latp",
+        "url" => $url);
 //var_dump($url);
-$v = new vcalendar($config);
+    $v = new vcalendar($config);
 
-$v->parse();
+    $v->parse();
 
-while ($comp = $v->getComponent())
-{
-echo "<div>";
+    while ($comp = $v->getComponent()) {
+        echo "<div>";
 
-	/*date de début*/
-   $dtstart_array = $comp->getProperty("dtstart", 1, TRUE);
-    $dtstart = $dtstart_array["value"];
-    $startDate = "{$dtstart["year"]}-{$dtstart["month"]}-{$dtstart["day"]}";
-    echo "start: ", $startDate;
-    if (!in_array("DATE", $dtstart_array["params"])) {
-        $startTime = "{$dtstart["hour"]}:{$dtstart["min"]}:{$dtstart["sec"]}";
-        echo "T", $startTime;
+        /*date de début*/
+        $dtstart_array = $comp->getProperty("dtstart", 1, true);
+        $dtstart = $dtstart_array["value"];
+        $startDate = "{$dtstart["year"]}-{$dtstart["month"]}-{$dtstart["day"]}";
+        echo "start: ", $startDate;
+        if (!in_array("DATE", $dtstart_array["params"])) {
+            $startTime = "{$dtstart["hour"]}:{$dtstart["min"]}:{$dtstart["sec"]}";
+            echo "T", $startTime;
+        }
+        echo "\n";
+
+        /*date de fin*/
+        $dtend_array = $comp->getProperty("dtend", 1, true);
+        $dtend = $dtend_array["value"];
+        $endDate = "{$dtend["year"]}-{$dtend["month"]}-{$dtend["day"]}";
+        echo "end: ", $endDate;
+        if (!in_array("DATE", $dtend_array["params"])) {
+            $endTime = "{$dtend["hour"]}:{$dtend["min"]}:{$dtend["sec"]}";
+            echo "T", $endTime;
+        }
+        echo "\n";
+        /*attendee*/
+        $attendee = $comp->getProperty("attendee");
+        echo "<strong>attendee : ", str_replace('MAILTO:', '', $attendee)."</strong><br/>";
+        /*summary*/
+        $summary_array = $comp->getProperty("summary", 1, true);
+        echo "summary : ", str_replace('SUMMARY:', '', $summary_array["value"]), "\n";
+        /*categorie*/
+        $categories = $comp->getProperty("categories");
+        echo "<strong>categories : ", $categories."</strong><br/>";
+
+        echo "</div>";
     }
-    echo "\n";
-
-    /*date de fin*/
-   $dtend_array = $comp->getProperty("dtend", 1, TRUE);
-    $dtend = $dtend_array["value"];
-    $endDate = "{$dtend["year"]}-{$dtend["month"]}-{$dtend["day"]}";
-    echo "end: ", $endDate;
-    if (!in_array("DATE", $dtend_array["params"])) {
-        $endTime = "{$dtend["hour"]}:{$dtend["min"]}:{$dtend["sec"]}";
-        echo "T", $endTime;
-    }
-    echo "\n";
-    /*attendee*/
-    $attendee = $comp->getProperty( "attendee" );
-    echo "<strong>attendee : ", str_replace('MAILTO:','',$attendee)."</strong><br/>";
-    /*summary*/
-    $summary_array = $comp->getProperty("summary", 1, TRUE);
-    echo "summary : ", str_replace('SUMMARY:', '', $summary_array["value"]), "\n";
-    /*categorie*/
-    $categories = $comp->getProperty( "categories" );
-    echo "<strong>categories : ", $categories."</strong><br/>";
-
-echo "</div>";
-}
 
 //return $url;
 }
