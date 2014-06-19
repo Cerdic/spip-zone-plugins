eaf248788b20d602b7f33730d8da20fdae1b1662
chankalan@free.fr <chankalan@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-06-19 09:18:15 +0000

    fonction charger commentée, le paramétrage du plugin fonctionne mais pas le message d'avertissement
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@83366 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- sjcycle/trunk/formulaires/configurer_sjcycle.php
+++ sjcycle/trunk/formulaires/configurer_sjcycle.php
@@ -2,21 +2,21 @@
 // Securite
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
-function formulaires_configurer_sjcycle_charger(){
-
-	// verifier avant tout la config de SPIP
-	$erreurs = array();
-	// choix de la methode de traitement des images par le serveur
-	if (!lire_config('image_process')){
-		$erreurs['message_erreur'] = _T('sjcycle:erreur_config_image_process');
-		return $erreurs;
-	}
-	// Generation de miniatures des images inactive
-	if (lire_config('creer_preview')!='oui') {
-		$erreurs['message_erreur'] = _T('sjcycle:erreur_config_creer_preview');
-		return $erreurs;
-	}
-	return $erreurs;
-	
-}
+// function formulaires_configurer_sjcycle_charger(){
+// 	
+// 	// verifier avant tout la config de SPIP
+// 	$erreurs = array();
+// 	// choix de la methode de traitement des images par le serveur
+// 	if (!lire_config('image_process')){
+// 		$erreurs['message_erreur'] = _T('sjcycle:erreur_config_image_process');
+// 		return $erreurs;
+// 	}
+// 	// Generation de miniatures des images inactive
+// 	if (lire_config('creer_preview')!='oui') {
+// 		$erreurs['message_erreur'] = _T('sjcycle:erreur_config_creer_preview');
+// 		return $erreurs;
+// 	}
+// 	return $erreurs;
+// 	
+// }
 ?>
