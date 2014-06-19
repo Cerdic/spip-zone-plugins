f6322a4232c1875854ea24d3257d0743828bf0fb
p@henix.be <p@henix.be@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-06-19 16:37:14 +0000

    Utiliser l'API de saisie pour les formulaires
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@83381 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- foundation-4-spip/trunk/formulaires/configurer_foundation.php
+++ foundation-4-spip/trunk/formulaires/configurer_foundation.php
@@ -1,16 +1,15 @@
 <?php
-function formulaires_configurer_foundation_charger() {
-
+function formulaires_configurer_foundation_saisies() {
   // Lire le fichier YAML qui contient la structure du formulaire.
   include_spip('inc/yaml');
   $formulaire = yaml_decode_file(find_in_path('formulaires/configurer_foundation.yaml'));
 
+  return $formulaire;
+}
+
+function formulaires_configurer_foundation_charger() {
   // Lire la configuration de foundation
   $config = lire_config('foundation');
-
-  // Ajouter le formulaire au contexte pour utiliser #GENERER_SAISIES
-  // On utilise un _ devant le nom ENV pour évité les traitements automatiques.
-  $config['_form_saisies'] = $formulaire;
 
   return $config;
 }
