3e789c529af6c482b3ba6fa72865245e95ad0afe
joseph@larmarange.net <joseph@larmarange.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-06-21 14:50:34 +0000

    coquille
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@83417 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- zotspip/trunk/genie/maj_zotspip.php
+++ zotspip/trunk/genie/maj_zotspip.php
@@ -2,7 +2,7 @@
 
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
-function genie_maj__zotspip_dist($t) {
+function genie_maj_zotspip_dist($t) {
 	include_spip('inc/zotspip');
 	return zotspip_maj();
 }
