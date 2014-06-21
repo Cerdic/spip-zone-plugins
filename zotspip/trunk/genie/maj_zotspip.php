e9e49ed11c9ad8cd89e6a3e07991c7e059764e7e
joseph@larmarange.net <joseph@larmarange.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-06-21 14:49:14 +0000

    simplification du CRON
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@83416 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- zotspip/trunk/genie/maj_zotspip.php
+++ zotspip/trunk/genie/maj_zotspip.php
@@ -2,9 +2,9 @@
 
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
-function genie_maj_collections_zotspip_dist($t) {
+function genie_maj__zotspip_dist($t) {
 	include_spip('inc/zotspip');
-	return zotspip_maj_collections();
+	return zotspip_maj();
 }
 
 ?>
--- zotspip/trunk/zotspip_cron.php
+++ zotspip/trunk/zotspip_cron.php
@@ -1,9 +1,7 @@
 <?php
 
 function zotspip_taches_generales_cron($taches){
-	$taches['maj_items_zotspip'] = 3600*6; // toutes les 6 heures
-	$taches['nettoyer_zotspip'] = 3600*6; // toutes les 6 heures
-	$taches['maj_collections_zotspip'] = 3600*6; // toutes les 6 heures
+	$taches['maj_zotspip'] = 3600*4; // toutes les 4 heures
 	$taches['maj_schema_zotero'] = 3600*24*30; // une fois par mois
 	return $taches;
 }
