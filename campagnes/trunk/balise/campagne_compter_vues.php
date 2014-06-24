e284e19770ee811768f06bf6d694479e497911da
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-06-24 16:40:37 +0000

    Ne pas faire des requêtes SQL pour rien, ça ne gènait pas l'insertion mais plein d'erreur dans les logs de duplicate.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@83453 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- campagnes/trunk/balise/campagne_compter_vues.php
+++ campagnes/trunk/balise/campagne_compter_vues.php
@@ -4,14 +4,14 @@
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 function balise_CAMPAGNE_COMPTER_VUES($p) {
-	return calculer_balise_dynamique($p, 'CAMPAGNE_COMPTER_VUES', array('id_campagne'));
+	return calculer_balise_dynamique($p, 'CAMPAGNE_COMPTER_VUES', array('id_campagne', 'id_encart'));
 }
 
 function balise_CAMPAGNE_COMPTER_VUES_stat($args, $context_compil) {
 	return $args;
 }
 
-function balise_CAMPAGNE_COMPTER_VUES_dyn($id_campagne) {
+function balise_CAMPAGNE_COMPTER_VUES_dyn($id_campagne, $id_encart) {
 	static $fait = array();
 	
 	// On s'assure que le comptage d'une même pub n'est fait qu'une seule fois par hit et pas par un robot
@@ -19,17 +19,26 @@ function balise_CAMPAGNE_COMPTER_VUES_dyn($id_campagne) {
 		include_spip('inc/campagnes');
 		$infos = campagnes_recuperer_infos_visiteur();
 		
-		$id_encart = sql_getfetsel('id_encart', 'spip_campagnes', 'id_campagne = '.$id_campagne);
+		if (!$id_encart) {
+			$id_encart = sql_getfetsel('id_encart', 'spip_campagnes', 'id_campagne = '.$id_campagne);
+		}
 		$page = self();
 		
 		// On ajoute la date et la pub
 		$infos = array_merge($infos, array('id_campagne' => $id_campagne, 'id_encart' => $id_encart, 'page' => $page, 'date' => 'NOW()'));
 		
-		// On enregistre l'affichage
-		$ok = sql_insertq(
-			'spip_campagnes_vues',
-			$infos
-		);
+		// On enregistre l'affichage si pas déjà fait
+		if (!sql_fetsel('cookie', 'spip_campagnes_vues', array(
+			'id_campagne = '.$id_campagne,
+			'id_encart = '.$id_encart,
+			'cookie = '.sql_quote($infos['cookie']),
+			'date = '.sql_quote(date('Y-m-d'))
+		))) {
+			$ok = sql_insertq(
+				'spip_campagnes_vues',
+				$infos
+			);
+		}
 		
 		// On marque que c'est fait
 		if ($ok !== false) $fait[$id_campagne] = true;
