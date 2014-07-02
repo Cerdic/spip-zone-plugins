f96981ba40a3552897913e0bcc4ea9d7bd8154a8
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-07-02 22:12:29 +0000

    On permet de choisir la barre de raccourcis typo qui sera utilisé sur le descriptif (peetdu). Pour garder le même fonctionnement, on met par défaut la barre d'édition complète.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@83530 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- titre_de_logo/trunk/lang/titre_logo_fr.php
+++ titre_de_logo/trunk/lang/titre_logo_fr.php
@@ -6,26 +6,37 @@
 // Langue: fr
 // Date: 21-05-2014 15:20:06
 
-if (!defined('_ECRIRE_INC_VERSION')) return;
+if (!defined('_ECRIRE_INC_VERSION')) {
+    return;
+}
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
+// B
+    'barre_edition' => 'Barre d\'édition complète',
+    'barre_forum' => 'Barre d\'édition simple utilisée pour les forums.',
+
 // C
-	'cfg_titre_parametrages' => 'Paramétrer les titres et descriptifs de logo',
-	'cfg_explication_parametrages' => 'Vous pouvez activer l\'interface d\'édition du titre et du descriptif de logo pour les objets souhaités.',
+    'cfg_titre_parametrages' => 'Paramétrer les titres et descriptifs de logo',
+    'cfg_explication_parametrages' => 'Vous pouvez activer l\'interface d\'édition du
+    titre et du descriptif de logo pour les objets souhaités.',
 
 // E
-	'explication_parametrages' => '',
+    'explication_parametrages' => '',
+    'explication_inserer_barre' => 'Le descriptif du logo peut utiliser une barre typographique
+    et donc, les raccourcis typographiques de SPIP. A vous de choisir laquelle vous convient.',
 
 // L
-	'label_objets_titre_logo' => 'Activer le titre de logo et son descriptif pour les contenus :',
-	'label_descriptif_logo' => 'Descriptif de logo',
-	'label_titre_logo' => 'Titre de logo',
+    'label_objets_titre_logo' => 'Activer le titre de logo et son descriptif pour les contenus :',
+    'label_descriptif_logo' => 'Descriptif de logo',
+    'label_titre_logo' => 'Titre de logo',
+    'label_inserer_barre' => 'Barre d\'outils d\'édition du descriptif',
 
 // N
-	'notice_logo_inactif_titre' => 'Avertissement',
-	'notice_logo_inactif_explication' => 'Vous n\'avez pas encore activé l\'utilisation des logos pour votre site. Veuillez consulter la page <strong>Configuration &gt; Contenu du site &gt; Les logos</strong>',
+    'notice_logo_inactif_titre' => 'Avertissement',
+    'notice_logo_inactif_explication' => 'Vous n\'avez pas encore activé l\'utilisation des logos
+    pour votre site. Veuillez consulter la page <strong>Configuration &gt; Contenu du site &gt; Les logos</strong>',
 // T
-	'titre_page_parametrages' => 'Titre de logo',
+    'titre_page_parametrages' => 'Titre de logo',
 );
 ?>
