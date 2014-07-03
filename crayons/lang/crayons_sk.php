c1f092108124068cd4b1a5c0444fbbe96169a03c
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-07-03 03:43:03 +0000

    [Salvatore] [source:_plugins_/crayons/lang/ crayons] Export depuis http://trad.spip.net de la langue sk
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@83536 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- crayons/lang/crayons_sk.php
+++ crayons/lang/crayons_sk.php
@@ -8,6 +8,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
+	'absence_cle_primaire' => 'Bez primárneho kľúča!',
 	'activation_barre_impossible' => 'Iný zásuvný modul bráni použitiu porte-plume na verejne prístupnej stránke.',
 	'annuler' => 'Zrušiť',
 
@@ -24,10 +25,10 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'editer_tout' => 'Upravovať ako celok',
 	'effets' => 'Efekty',
 	'explication_exec_autorise' => 'Stránky, na ktorých je povolené vykonávať zmeny
-  (?exec=...): (, na oddelenie viacerých stránok, * pre všetky).', # MODIF
-	'explication_introduction_article' => 'Ako úvod článku sa zobrazí opis článku, ak nejaký existuje, v opačnom prípade sa zobrazí prvých 600 znakov (úvodného odseku a textu).', # MODIF
+  (?exec=...): (, na oddelenie viacerých stránok, exec -* pre všetky).',
+	'explication_introduction_article' => 'Úvod článku zobrazí opis článku, ak nejaký existuje, inak sa zobrazí prvých 600 znakov (perex, potom text).',
 	'explication_reduire_logo' => 'Zmenšiť veľkosť loga, ak presahuje
-  maximálnu veľkosť (v pixeloch). Príklad: 100 (0 = nikdy).', # MODIF
+  maximálnu veľkosť zadanú v pixeloch. Príklad: 100. (0 = nezmenšovať).',
 
 	// I
 	'info_experimental' => 'Experimentálny',
