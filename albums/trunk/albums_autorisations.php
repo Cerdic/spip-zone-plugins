b5083b9a92d58c7875ea32667934eca74bb1f250
tcharlss@hotmail.fr <tcharlss@hotmail.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-07-07 11:04:00 +0000

    Affinage des autorisations : modifs, ajouts et 1 renommage.
    Bon, les admins complets peuvent tout faire, je ne le re-précise pas à chaque fois.
    
    - ajouteralbum : ajout activé pour l'objet et avoir le droit de modifier l'objet. Ca fait que quand on est sur la fiche d'un objet qu'on ne peut modifier, les albums sont cachés, comme les documents.
    - album_modifier : il faut être un des auteurs et avoir le droit de modifier tous les objets auquel l'album est lié .
    - album_associer (à un objet donné) : pouvoir modifier l'objet
    - album_dissocier (d'un objet donné) : pouvoir associer, et que l'album ne soit pas inséré dans le texte
    - deplacerdocumentsalbums : dans le contexte d'un objet, avoir le droit de modifier tous les albums liés
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@83593 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- albums/trunk/albums_autorisations.php
+++ albums/trunk/albums_autorisations.php
@@ -1,142 +1,395 @@
 <?php
 /**
- * Fonctions d'autorisations du plugin Albums
+ * Définit les autorisations du plugin Albums
  *
  * @plugin     Albums
- * @copyright  2013
+ * @copyright  2014
  * @author     Romy Tetue, Charles Razack
  * @licence    GNU/GPL
  * @package    SPIP\Albums\Autorisations
-**/
+ */
 
+// Sécurité
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 
 /**
- * @param string $faire L'action
- * @param string $type Le type d'objet
- * @param int $id L'identifiant numérique de l'objet
- * @param array $qui Les informations de session de l'auteur
- * @param array $opt Des options
- * @return boolean true/false
-*/
-
-
-/**
- * Declaration vide pour ce pipeline.
- */
+ * Fonction d'appel pour le pipeline
+ * @pipeline autoriser */
 function albums_autoriser(){}
 
-/**
- * Autorisation a afficher l'icone de creation rapide
- * = autorisation creer
- */ 
-function autoriser_albumcreer_menu_dist($faire, $type='', $id=0, $qui=NULL, $opts=NULL){
-	return autoriser('creer','album',$id,$qui,$opts);
-}
 
 /**
- * Autorisation a afficher les albums dans le menu
- * = autorisation creer
- */ 
-function autoriser_albums_menu_dist($faire, $type='', $id=0, $qui=NULL, $opts=NULL){
-	return autoriser('creer','album',$id,$qui,$opts);
-}
-
-/**
- * Autorisation a administrer les albums
- * Admins et redacteurs
- */ 
-function autoriser_album_administrer_dist($faire, $type, $id, $qui, $opts) {
-	return autoriser('creer','album',$id,$qui,$opts);
-}
-
-/**
- * Autorisation a ajouter (creer ou associer) un album a un objet
- * Il faut avoir le droit de modifier l'objet
+ * Autorisation à afficher l'icone de création rapide.
+ *
+ * Il faut être autorisé à créer un album.
+ *
+ * @see autoriser_album_creer_dist()
+ *
+ * @param  string $faire Action demandée
+ * @param  string $type  Type d'objet sur lequel appliquer l'action
+ * @param  int    $id    Identifiant de l'objet
+ * @param  array  $qui   Description de l'auteur demandant l'autorisation
+ * @param  array  $opts  Options de cette autorisation
+ * @return bool          true s'il a le droit, false sinon
  */
-function autoriser_ajouteralbum_dist($faire, $type, $id, $qui, $opts) {
-	if (!intval($id) OR !$type) return false;
-	return autoriser('modifier', $type, $id, $qui);
+function autoriser_albumcreer_menu_dist($faire, $type, $id, $qui, $opts){
+	return autoriser('creer','album',$id,$qui,$opts);
 }
 
+
 /**
- * Autorisation a creer un album
- * Admins et redacteurs
- */ 
+ * Autorisation de voir les albums dans le menu d'édition.
+ *
+ * Il faut être autorisé à administrer les albums.
+ *
+ * @see autoriser_albums_administrer_dist()
+ *
+ * @param  string $faire Action demandée
+ * @param  string $type  Type d'objet sur lequel appliquer l'action
+ * @param  int    $id    Identifiant de l'objet
+ * @param  array  $qui   Description de l'auteur demandant l'autorisation
+ * @param  array  $opts  Options de cette autorisation
+ * @return bool          true s'il a le droit, false sinon
+ */
+function autoriser_albums_menu_dist($faire, $type, $id, $qui, $opts){
+	return autoriser('administrer','albumotheque',$id,$qui,$opts);
+}
+
+
+/**
+ * Autorisation de créer un album
+ *
+ * Il faut être admin ou rédacteur.
+ *
+ * @param  string $faire Action demandée
+ * @param  string $type  Type d'objet sur lequel appliquer l'action
+ * @param  int    $id    Identifiant de l'objet
+ * @param  array  $qui   Description de l'auteur demandant l'autorisation
+ * @param  array  $opts  Options de cette autorisation
+ * @return bool          true s'il a le droit, false sinon
+**/
 function autoriser_album_creer_dist($faire, $type, $id, $qui, $opts) {
 	return in_array($qui['statut'], array('0minirezo', '1comite'));
 }
 
-/**
- * Autorisation a associer/dissocier un album a un objet
- * Il faut etre admin ou pouvoir modifier l'objet
- */
-function autoriser_associeralbum_dist($faire, $type, $id, $qui, $opts) {
-	if (!intval($id) OR !$type) return false;
-	return autoriser('modifier', $type, $id, $qui);
-}
 
 /**
- * Autorisation a voir les fiches completes
- * Tout le monde
- */ 
+ * Autorisation à voir les fiches complètes.
+ *
+ * Open bar pour tout le monde.
+ *
+ * @param  string $faire Action demandée
+ * @param  string $type  Type d'objet sur lequel appliquer l'action
+ * @param  int    $id    Identifiant de l'objet
+ * @param  array  $qui   Description de l'auteur demandant l'autorisation
+ * @param  array  $opts  Options de cette autorisation
+ * @return bool          true s'il a le droit, false sinon
+ */
 function autoriser_album_voir_dist($faire, $type, $id, $qui, $opts) {
 	return true;
 }
 
+
 /**
- * Autorisation a modifier un album
- * il faut etre admin ou auteur de l'album
- */ 
-function autoriser_album_modifier_dist($faire, $type, $id, $qui, $opts) {
-	$id = sql_quote($id);
-	if (!function_exists('auteurs_objet'))
-		include_spip('inc/auth'); // pour auteurs_objet si espace public
-
-	return
-		auteurs_objet($type, $id, "id_auteur=".$qui['id_auteur'])
-		OR in_array($qui['statut'], array('0minirezo'));
-
+ * Autorisation à accéder à l'albumothèque.
+ *
+ * Il faut être admin ou rédacteur.
+ *
+ * @param  string $faire Action demandée
+ * @param  string $type  Type d'objet sur lequel appliquer l'action
+ * @param  int    $id    Identifiant de l'objet
+ * @param  array  $qui   Description de l'auteur demandant l'autorisation
+ * @param  array  $opts  Options de cette autorisation
+ * @return bool          true s'il a le droit, false sinon
+ */
+function autoriser_albumotheque_administrer_dist($faire, $type, $id, $qui, $opts) {
+	return in_array($qui['statut'], array('0minirezo', '1comite'));
 }
 
+
 /**
- * Autorisation a supprimer definitivement un album
- * Il faut etre admin, que l'album soit vide, a la poubelle et inutilise
+ * Autorisation à ajouter un album à un objet.
+ *
+ * Il faut que l'ajout d'albums sur le type d'objet soit activé,
+ * et pouvoir modifier l'objet (ou l'éditer en cas de création par un rédacteur,
+ * qui correspond au hack id_objet = 0-id_auteur)
+ * ou être admin complet.
+ *
+ * @note
+ * Attention, les 2ème et 3ème arguments se réfèrent à l'objet, pas à l'album.
+ * Pour le hack de l'identifiant négatif, voir les notes du pipeline «affiche_gauche».
+ * Autre hack pénible : des fois l'autorisation à modifier un objet renvoie un faux négatif,
+ * c'est la cas dans un appel depuis le pipeline post_insertion, car l'auteur n'a pas encore été lié à l'objet.
+ * Dans ce cas il faut contourner le problème en vérifiant si l'objet vient juste d'être créé.
+ * cf. autorisation à modifier un album : même combat.
+ *
+ * @example
+ *     ```
+ *     #AUTORISER{ajouteralbum,#OBJET,#ID_OBJET}
+ *     ```
+ *
+ * @param  string $faire Action demandée
+ * @param  string $type  Type d'objet auquel on veut ajouter un album
+ * @param  int    $id    Identifiant de l'objet auquel on veut ajouter un album
+ * @param  array  $qui   Description de l'auteur demandant l'autorisation
+ * @param  array  $opts  Options de cette autorisation
+ * @return bool          true s'il a le droit, false sinon
+ */
+function autoriser_ajouteralbum_dist($faire, $type, $id, $qui, $opts) {
+	include_spip('inc/config');
+	return
+		(
+			in_array(table_objet_sql($type),lire_config('albums/objets', array()))
+		)
+		AND
+		(
+			// identifiant positif : cas «normal»
+			(
+				$id>0
+				AND
+				(
+					($qui['statut'] == '0minirezo' AND !$qui['restreint'])
+					// il faut être autorisé à modifier l'objet...
+					OR autoriser('modifier', $type, $id, $qui, $opts)
+					// ...mais ça donne un faux négatif depuis le pipeline «post_insertion» (cf. note),
+					// dans ce cas là on vérifie si l'objet est récent et qu'on a le droit d'écrire.
+					OR (
+						(time()-strtotime(generer_info_entite($id,$type,'date'))) < (60*1) // age < 1 min, cf. note
+						AND autoriser('ecrire', $type, $id, $qui, $opts)
+					) 
+				)
+			)
+			// identifiant négatif : objet nouveau pas encore enregistré en base (cf. note)
+			OR
+			(
+				$id<0
+				AND
+				(
+					abs($id) == $qui['id_auteur']
+					AND autoriser('ecrire', $type, $id, $qui, $opts)
+				)
+			)
+		);
+}
+
+
+/**
+ * Autorisation à modifier un album.
+ *
+ * Il faut être l'auteur et avoir le droit de modifier tous les objets auxquels l'album est lié,
+ * ou qu'il s'agisse d'un album en cours de création (vide, pas d'auteur et récent, cf. note).
+ * ou être admin complet.
+ *
+ * @note
+ * Hack pénible : quand on ajoute des documents à un nouvel album pas encore enregistré en base,
+ * Des liens temporaires sont créés dans la table spip_documents_liens.
+ * Ceux-ci seront modifiés une fois l'album inséré en base, via le pipeline «post_insertion» du plugin Médias.
+ * Il vérifie avant de procéder qu'il y ait l'autorisation de modifier l'album.
+ * Mais lorsque ce pipeline est appelé, aucun auteur n'a encore été lié à l'album.
+ * Pour ne pas renvoyer un faux négatif dans ce cas là, on vérifie si l'album
+ * vient juste d'être créé selon ces critères : âge, sans auteur et vide.
+ * cf. autorisation à ajouter un album : même combat.
+ *
+ * @param  string $faire Action demandée
+ * @param  string $type  Type d'objet sur lequel appliquer l'action
+ * @param  int    $id    Identifiant de l'objet
+ * @param  array  $qui   Description de l'auteur demandant l'autorisation
+ * @param  array  $opts  Options de cette autorisation
+ * @return bool          true s'il a le droit, false sinon
+ */
+function autoriser_album_modifier_dist($faire, $type, $id, $qui, $opts) {
+	$id = intval($id);
+	include_spip('action/editer_liens');
+
+	// être un des auteurs de l'album
+	$auteurs_album = array();
+	if (is_array($liens_auteurs = objet_trouver_liens(array('auteur'=>'*'),array('album'=>$id))))
+		foreach($liens_auteurs as $a)
+			$auteurs_album[] = $a['id_auteur'];
+	$auteur_album = in_array($qui['id_auteur'],$auteurs_album) ? true : false;
+
+	// droit de modifier tous les objets auxquels l'album est lié
+	// l'album peut être lié à un nouvel objet pas encore enregistré en base,
+	// dans ce cas id_objet est négatif
+	$autorise_modifier_objets_lies = true;
+	if (is_array($liens_objets = objet_trouver_liens(array('album'=>$id),'*'))){
+		foreach($liens_objets as $l) {
+			$objet = $l['objet'];
+			$id_objet = $l['id_objet'];
+			if (
+				($id_objet>0 AND !autoriser('modifier',$objet,$id_objet))
+				OR ($id_objet<0 AND !autoriser('ecrire',$objet,$id_objet))
+			) {
+				$autorise_modifier_objets_lies = false;
+				break;
+			}
+		}
+	}
+
+	// cas d'un album tout juste inséré en base (cf. note)
+	// pas d'auteur, vide et créé il y a moins de 1 min
+	$nouvel_album =
+		(
+			!count($auteurs_album)
+			AND !sql_countsel("spip_documents_liens", "objet='album' AND id_objet=".$id) // vide
+			AND ((time()-strtotime(sql_getfetsel("date","spip_albums","id_album=".$id))) < (60*1)) // age < 1 min
+		) ?
+		true : false;
+
+	// les admins complets ont tous les droits !
+	$admin_complet = ($qui['statut'] == '0minirezo' AND !$qui['restreint']) ?
+		true : false;
+
+	return
+		($auteur_album AND $autorise_modifier_objets_lies)
+		OR $nouvel_album
+		OR $admin_complet;
+}
+
+
+/**
+ * Autorisation à supprimer définitivement un album.
+ *
+ * Il faut qu'il soit vide et inutilisé, + non publié si on est pas admin complet
+ *
+ * @param  string $faire Action demandée
+ * @param  string $type  Type d'objet sur lequel appliquer l'action
+ * @param  int    $id    Identifiant de l'objet
+ * @param  array  $qui   Description de l'auteur demandant l'autorisation
+ * @param  array  $opts  Options de cette autorisation
+ * @return bool          true s'il a le droit, false sinon
  */
 function autoriser_album_supprimer_dist($faire, $type, $id, $qui, $opts) {
-	$id = sql_quote($id);
-	$statut = sql_fetsel("statut", "spip_albums", "id_album='$id'");
-	$docs = sql_fetsel("id_objet", "spip_documents_liens", "objet='$type' AND id_objet='$id'");
-	$liaison = sql_fetsel("id_album", "spip_albums_liens", "id_album='$id'");
+	$id = intval($id);
+	$statut = sql_getfetsel("statut", "spip_albums", "id_album=$id");
+	$documents = sql_countsel("spip_documents_liens", "objet=".sql_quote($type)."AND id_objet=$id");
+	$liaisons = sql_countsel("spip_albums_liens", "id_album=$id");
 	return
-		!in_array($liaison['id_album'], array($id)) # inutilise (sans lien vers d'autres objets)
-		AND !in_array($docs['id_objet'], array($id)) # vide (sans document ayant un lien vers l'album)
-		AND in_array($statut['statut'], array('poubelle')) # a la poubelle
-		AND ( $qui['statut'] == '0minirezo' AND !$qui['restreint'] ); # statut: admin
+		!$liaisons # inutilisé
+		AND !$documents # vide
+		AND
+		(
+			(
+				($statut != 'publie') #non publié
+				AND (autoriser('modifier', $type, $id, $qui)) #auteur ou admin
+			)
+			OR
+			(
+				$qui['statut'] == '0minirezo' AND !$qui['restreint'] #admin complet
+			)
+		);
 }
 
+
 /**
- * Auto-association d'albums a du contenu editorial qui le reference
- * par defaut true pour tous les objets
+ * Autorisation à associer un album à un objet donné.
+ *
+ * Il faut pouvoir modifier l'objet
+ * ou être admin complet
+ *
+ * @note
+ * Les infos sur l'objet dont dans les options (5ème paramètre)
+ *
+ * @example
+ *     ```
+ *     #AUTORISER{associer,album,#ID_ALBUM,'',#ARRAY{objet,#OBJET,id_objet,#ID_OBJET}}
+ *     ```
+ *
+ * @param  string $faire Action demandée
+ * @param  string $type  Type d'objet auquel on veut associer ou dissocier un album
+ * @param  int    $id    Identifiant de l'objet auquel on veut associer ou dissocier un album
+ * @param  array  $qui   Description de l'auteur demandant l'autorisation
+ * @param  array  $opts  Options de cette autorisation
+ *                       Doit contenir les clés `objet` et `id_objet`
+ *                       pour rensigner le type et l'identifiant de l'objet
+ * @return bool          true s'il a le droit, false sinon
+ */
+function autoriser_album_associer_dist($faire, $type, $id, $qui, $opts) {
+	return 
+		($qui['statut'] == '0minirezo' AND !$qui['restreint'])
+		OR (autoriser('modifier', $opts['objet'], $opt['id_objet'], $qui));
+}
+
+
+/**
+ * Autorisation à dissocier un album d'un objet donné.
+ *
+ * Il faut être autorisé à associer un album à l'objet,
+ * et qu'il ne soit pas inséré dans le texte.
+ *
+ * @note
+ * Les infos sur l'objet dont dans les options (5ème paramètre)
+ *
+ * @example
+ *     ```
+ *     #AUTORISER{dissocier,album,#ID_ALBUM,'',#ARRAY{objet,#OBJET,id_objet,#ID_OBJET}}
+ *     ```
+ *
+ * @param  string $faire Action demandée
+ * @param  string $type  Type d'objet sur lequel appliquer l'action
+ * @param  int    $id    Identifiant de l'objet
+ * @param  array  $qui   Description de l'auteur demandant l'autorisation
+ * @param  array  $opts  Options de cette autorisation
+ *                       Doit contenir les clés `objet` et `id_objet`
+ *                       pour rensigner le type et l'identifiant de l'objet
+ * @return bool          true s'il a le droit, false sinon
+ */
+function autoriser_album_dissocier_dist($faire, $type, $id, $qui, $opts) {
+	return
+		autoriser('associer', 'album', $id, $qui, $opts)
+		AND (sql_getfetsel('vu', "spip_albums_liens", "id_album=".intval($id)." AND objet=".sql_quote($opts['objet'])." AND id_objet=".intval($opts['id_objet']))=='non');
+}
+
+
+/**
+ * Auto-association d'albums à du contenu éditorial qui le référence.
+ *
+ * Par défaut true pour tous les objets.
+ *
+ * @param  string $faire Action demandée
+ * @param  string $type  Type d'objet sur lequel appliquer l'action
+ * @param  int    $id    Identifiant de l'objet
+ * @param  array  $qui   Description de l'auteur demandant l'autorisation
+ * @param  array  $opts  Options de cette autorisation
+ * @return bool          true s'il a le droit, false sinon
  */
 function autoriser_autoassocieralbum_dist($faire, $type, $id, $qui, $opts) {
 	return true;
 }
 
+
 /**
- * Lister les auteurs d'un album
- * 
- * Fonction générique utilisée, retourne une liste des id_auteur trouvés
+ * Autorisation à déplacer des documents.
  *
- * @param int $id_album Identifiant de l'album
- * @param string $cond  Condition en plus dans le where de la requête
- * @return array|bool
- *     - array : liste des id_auteur trouvés
- *     - false : serveur SQL indisponible
+ * Il faut être admin complet
+ * ou dans le contexte d'un objet, avoir le droit de modifier tous les albums liés.
+ *
+ * @param  string $faire Action demandée
+ * @param  string $type  Type d'objet sur lequel appliquer l'action
+ * @param  int    $id    Identifiant de l'objet
+ * @param  array  $qui   Description de l'auteur demandant l'autorisation
+ * @param  array  $opts  Options de cette autorisation
+ * @return bool          true s'il a le droit, false sinon
  */
-function auteurs_objet($objet='album', $id_objet, $cond=''){
-	return sql_allfetsel("id_auteur", "spip_auteurs_liens", "objet='$objet' AND id_objet=".sql_quote($id_objet). ($cond ? " AND $cond" : ''));
+function autoriser_deplacerdocumentsalbums_dist($faire, $type, $id, $qui, $opts) {
+	$autorise_modifier_albums = false;
+	if ($type AND $id=intval($id)){
+		$autorise_modifier_albums = true;
+		include_spip('action/editer_liens');
+		if (is_array($liens_albums = objet_trouver_liens(array('album'=>'*'),array($type=>$id)))){
+			foreach($liens_albums as $l) {
+				if (!autoriser('modifier','album',$l['id_album'])) {
+					$autorise_modifier_albums = false;
+					break;
+				}
+			}
+		}
+	}
+	return
+		($qui['statut'] == '0minirezo' AND !$qui['restreint'])
+		OR $autorise_modifier_albums;
 }
+
 
 ?>
