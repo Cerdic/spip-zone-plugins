2585a5a8e6107aab32119b1d46296803ee25618c
tcharlss@hotmail.fr <tcharlss@hotmail.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-07-07 14:00:56 +0000

    ajout de nouvelles chaînes de langue,  et nettoyage de celles obsolètes
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@83599 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- albums/trunk/lang/album_fr.php
+++ albums/trunk/lang/album_fr.php
@@ -6,94 +6,101 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// B
-	'bouton_associer' => 'Associer',
+	'bouton_associer' => 'Ajouter cet album',
 	'bouton_dissocier' => 'Détacher',
 	'bouton_valider_deplacer_documents' => 'Enregistrer les changements',
+	'bouton_editer_texte_album' => 'Editer le texte',
+	'bouton_vider' => 'Vider',
+	'bouton_supprimer' => 'Supprimer',
 
 	// C
 	'cfg_titre_albums' => 'Albums',
+	'c_albumotheque_titre_presentation' => 'Les albums',
+	'c_albumotheque_presentation' => 'Les albums servent à composer des groupes de documents. Bla bla.', # à finir
+	'c_albumotheque_titre_filtres' => 'Filtrer la sélection',
+	'c_albumotheque_filtres' => 'Les filtres permettent d\'activer certains critères
+		afin de restreindre la sélection des albums. Un clic sur une entrée active le filtre, un autre clic le désactive.
+		En les combinant, on peut facilement retrouver n\'importe quel album.
+		Quand les listes sont trop longues, des champs de recherche permettent de retrouver un objet précis.', # à finir
 
 	// E
 	'erreur_deplacement' => 'Le traitement n’a pas pu être effectué',
+	'explication_album_numero' => 'Un ou plusieurs identifiants séparés par une virgule',
 
 	// F
-	'filtre_avec_contenu' => 'Remplis',
-	'filtre_non_vus' => 'Non vus',
-	'filtre_sans_contenu' => 'Vides',
-	'filtre_tous' => 'Tous',
-	'filtre_tous_contenus' => 'Tous les contenus',
-	'filtre_vus' => 'Vus',
-
-	// H
-	'heading_contenu' => 'Contenu',
-	'heading_descriptif' => 'Description',
-	'heading_id' => 'ID',
-	'heading_infos' => 'Infos',
-	'heading_liaison' => 'Liaison',
-	'heading_logo' => 'Logo',
-	'heading_titre' => 'Titre',
+	'filtre_medias' => 'Types de Documents',
+	'filtre_orphelins' => 'Orphelins',
+	'filtre_utilisations' => 'Utilisations',
+	'filtre_types_utilisations' => 'Types d\'utilisations',
+	'filtre_vus' => 'Insérés dans un texte',
+	'filtre_non_vus' => 'Non insérés dans un texte',
+	'filtre_extensions' => 'Extensions',
 
 	// I
-	'icone_ajouter' => 'Ajouter',
 	'icone_ajouter_album' => 'Ajouter un album',
-	'icone_associer_album' => 'Associer un album existant',
 	'icone_creer_album' => 'Créer un nouvel album',
-	'icone_delier_album' => 'Enlever cet album',
-	'icone_fermer' => 'Fermer',
 	'icone_modifier_album' => 'Modifier cet album',
 	'info_1_album' => '1 album',
-	'info_1_association' => 'Associé à 1 objet',
-	'info_album_vide' => 'Album vide',
+	'info_1_utilisation' => '1 Utilisation',
 	'info_aucun_album' => 'Aucun album',
-	'info_aucun_album_criteres' => 'Aucun album ne correspond à ces critères',
-	'info_aucun_album_supplementaire' => 'Aucun album supplémentaire',
-	'info_aucun_album_vide' => 'Aucun album vide',
-	'info_aucune_association' => 'Cet album n’est associé à aucun objet',
-	'info_dans_objet' => 'dans des',
-	'info_documents_joints_albums' => 'Veillez à activer l’ajout de documents-joints aux albums',
-	'info_js_prive' => 'animation désactivée dans l’espace privé',
 	'info_nb_albums' => '@nb@ albums',
-	'info_nb_associations' => 'Associé à @nb@ objets',
+	'info_nb_utilisations' => '@nb@ utilisations',
 	'info_nouvel_album' => 'Nouvel album',
-	'info_publier' => 'publier',
-	'info_recherche_album_zero' => 'Aucun résultat pour « @cherche_album@ »',
-	'info_resultat_recherche_aucun' => 'Aucun résultat',
-	'info_resultat_recherche_plus' => '@nb@ résultats',
-	'info_resultat_recherche_un' => '1 résultat',
-	'info_statut_modele' => 'Album non publié',
 
 	// L
 	'label_activer_album_objets' => 'Activer les albums pour les contenus :',
-	'label_case_utiliser_titre_defaut' => 'Par défaut, utiliser le titre de l’objet parent',
+	'label_case_utiliser_titre_defaut' => 'Par défaut, utiliser le titre de l’objet lié',
 	'label_modele_alignement' => 'Alignement',
 	'label_modele_alignement_centre' => 'Centré',
-	'label_modele_alignement_defaut' => 'Défaut',
 	'label_modele_alignement_droite' => 'Droite',
 	'label_modele_alignement_gauche' => 'Gauche',
 	'label_modele_identifiant' => 'Numéro de l’album',
+	'label_modele_titre_perso' => 'Titre personnalisé',
+	'label_modele_choisir' => 'Choix du modèle',
+	'label_modele_labels_images' => 'Label de chaque image',
+	'label_modele_largeur_images' => 'Largeur maximale',
+	'label_modele_hauteur_images' => 'Hauteur maximale',
+	'label_modele_placeholder_dimension' => 'Taille en px, sans l\'unité',
+	'label_modele_recadrer_images' => 'Recadrer les images',
+	'label_modele_trier' => 'Trier par :',
+	'label_modele_defaut' => 'Défaut',
+	'label_modele_tri_id' => 'N° du document',
+	'label_modele_tri_titre' => 'Titre',
+	'label_modele_tri_media' => 'Type du document',
+	'label_modele_tri_date' => 'Date',
+	'label_modele_metas' => 'Informations sur le document :',
+	'label_modele_meta_extension' => 'Extension',
+	'label_modele_meta_taille' => 'Taille',
+	'label_modele_meta_dimensions' => 'Dimensions',
+	'label_modele_alias_vignettes' => 'Vignettes',
+	'label_modele_nom_vignettes' => 'un album (vignettes)',
+	'label_modele_description_vignettes' => 'Vue d\'images sous forme de vignettes',
+	'label_modele_alias_liste' => 'Liste',
+	'label_modele_nom_liste' => 'un album (liste)',
+	'label_modele_description_liste' => 'Vue des documents sous forme de liste',
 	'label_modele_parcourir_albums' => 'Parcourir les albums',
-	'label_modele_titre' => 'Titre personnalisé',
-	'label_modele_variante' => 'Variante',
-	'label_modele_variante_defaut' => 'Vignettes (défaut)',
-	'label_modifier_balise' => 'Modifier la balise',
 	'label_titre' => 'Titre',
+	'label_descriptif' => 'Descriptif',
 	'label_utiliser_titre_defaut' => 'Titre d’un nouvel album',
+	'label_album_numero' => 'Album numéro',
+
 
 	// M
 	'message_balise_inseree_succes' => 'La balise a été insérée dans le texte',
+	'message_avertissement_cfg_documents' => 'Attention ! L\'ajout de documents aux albums est désactivé. L\'activation est nécessaire au bon fonctionnement des albums.',
+	'message_activer_cfg_documents' => 'Dans le formulaire de configuration des documents joints, cochez la case «Albums».',
+	'message_vider' => 'Retirer tous les docments ?',
+	'message_supprimer' => 'Supprimer définitivement ?',
+	'message_album_non_editable' => 'Cet album n\'est pas éditable : il est utilisé par un ou pusieurs objets que vous ne pouvez pas modifier.',
+	'message_id_album_ajoute' => 'L\'album N° @id_album@ a été ajouté.',
+	'message_1_album_ajoute' => '1 album a été ajouté.',
+	'message_nb_albums_ajoutes' => '@nb@ albums ont été ajoutés.',
 
 	// O
-	'onglet_associer_album' => 'Associer',
-	'onglet_configurer_base' => 'Albums',
+	'onglet_configurer_options' => 'Options',
 	'onglet_configurer_outils' => 'Outils',
-	'onglet_creer_album' => 'Créer',
-	'onglet_mes_albums' => 'Mes albums',
-	'onglet_parents_tous' => 'Tous les objets',
-	'onglet_tous' => 'Tous',
 
 	// T
-	'texte_ajouter_album' => 'Ajouter un album',
-	'texte_associer_album' => 'Associer un album existant',
 	'texte_changer_statut' => 'Modifier le statut',
 	'texte_creer_album' => 'Créer un nouvel album',
 	'texte_modifier' => 'Modifier',
@@ -101,11 +108,13 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'texte_statut_prepa' => 'non publié',
 	'texte_statut_publie' => 'publié en ligne',
 	'titre_album' => 'Album',
-	'titre_album_sans_titre' => 'Album sans titre',
 	'titre_albums' => 'Albums',
-	'titre_inserer_modele_album' => 'un album',
 	'titre_logo_album' => 'Logo',
-	'titre_page_configurer_albums' => 'Configurer les Albums'
+	'titre_page_configurer_albums' => 'Configurer les Albums',
+	'titre_documents_deplaces' => 'Documents déplacés',
+	'texte_personnaliser_balise_album' => 'Personnaliser la balise',
+	'texte_activer_ajout_albums' => 'Vous pouvez activer l’interface d’ajout d\'albums aux articles, rubriques et autres.
+		Comme les documents des portfolios, les albums peuvent ensuite être référencés dans le texte, ou affichés séparément.',
 );
 
 ?>
