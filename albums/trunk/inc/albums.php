ac44e4327435a4b0344db91854f3b2a8c47da9d0
tcharlss@hotmail.fr <tcharlss@hotmail.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-07-07 10:46:59 +0000

    Ajout de 2 fonctions : - supprimer "proprement" un album - vider un album On vire la fonction «editer_liens_albums» +phpDoc
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@83590 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- albums/trunk/inc/albums.php
+++ albums/trunk/inc/albums.php
@@ -1,82 +1,156 @@
 <?php
+/**
+ * Fonctions du plugin Albums
+ *
+ * @plugin     Albums
+ * @copyright  2014
+ * @author     Romy Tetue, Charles Razack
+ * @licence    GNU/GPL
+ * @package    SPIP\Albums\Fonctions
+ */
+
+// Sécurité
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
+
 /**
- * Associer/dissocier un album a un objet editorial
- * Utilise par les boutons d actions
+ * Supprimer proprement un ou plusieurs albums
  *
- * @param string $action
- * 		associer ou dissocier
- * @param int $id_album
- * @param string $objet
- * @param int $id_objet
+ * @param int|string|array $id_albums
+ *     Identifiant unique ou tableau d'identifiants des albums
+ * @param bool $supprimer_docs_orphelins
+ *     true pour supprimer les documents rendus orphelins
  */
-function editer_liens_album($action,$id_album,$objet,$id_objet){
+function supprimer_albums($ids_albums, $supprimer_docs_orphelins=false) {
 
-	if (!$action OR (!in_array($action,array('associer','dissocier'))) OR !$id_album OR !$objet OR !$id_objet)
-		return;
+	if (!$ids_albums) return false;
+	if (!is_array($ids_albums)) $ids_albums = array(intval($ids_albums));
 
-	include_spip('inc/autoriser');
-	if (intval($id_objet) AND autoriser('associeralbum', $objet, $id_objet)){
-		include_spip('action/editer_liens');
-		switch ($action) {
-			case 'associer' :
-				objet_associer(array('album'=>$id_album), array($objet=>$id_objet));
-				break;
-			case 'dissocier' :
-				objet_dissocier(array('album'=>$id_album), array($objet=>$id_objet));
-				break;
-		}
-	}
+	// Vider les albums de leurs documents
+	vider_albums($ids_albums,$supprimer_docs_orphelins);
+
+	// Nettoyer la table de liens
+	include_spip('action/editer_liens');
+	objet_dissocier(array('album'=>$ids_albums),'*');
+
+	// Supprimer les albums
+	$in_albums = sql_in('id_album', $ids_albums);
+	sql_delete(table_objet_sql('album'), $in_albums);
+
+	// Invalider les caches
+	include_spip('inc/invalideur');
+	suivre_invalideur("id='album/$id_album'");
+
+	return;
 }
 
 
 /**
- * Lister les modeles albums
- * modele principal et $modele
+ * Vider un ou plusieurs albums de leurs documents.
  *
- * @param bool $variante
- *		si true, renvoie une liste des variantes uniquement
- * @staticvar array $liste_modeles
+ * @note
+ * Impossible de retirer en une fois tous les documents d'un album
+ * via le bouton d'action `dissocier_document` du plugin Médias.
+ * Il faut lui passer en paramètre un des 3 modes pour les documents :
+ *
+ * - les images en mode image : `I/image`
+ * - les images en mode document : `I/document`
+ * - les documents en mode document : 'D/document'
+ * 
+ * Cf. fonction `dissocier_document` dans `action/dissocier_document.php`.
+ * 
+ * @param int|string|array $id_albums
+ *     Identifiant unique ou tableau d'identifiants des albums
+ * @param bool $supprimer_docs_orphelins
+ *     true pour supprimer les documents rendus orphelins
+ * @return array
+ *     tableau des albums vidés et ceux laissés intacts
+ *     [succes => [x,y,z]],[erreurs => [x,y,z]]
+ */
+function vider_albums($ids_albums, $supprimer_docs_orphelins=false) {
+
+	if (!$ids_albums) return false;
+	if (!is_array($ids_albums)) $ids_albums = array($ids_albums);
+	$supprimer_docs_orphelins = ($supprimer_docs_orphelins==true) ? 'suppr' : '';
+
+	include_spip('inc/autoriser');
+	include_spip('action/editer_liens');
+	foreach($ids_albums as $id_album) {
+		if (
+			$id_album = intval($id_album)
+			AND autoriser('modifier','album',$id_album)
+			AND count(objet_trouver_liens(array('document'=>'*'),array('album'=>$id_album)))
+		){
+			$dissocier_document = charger_fonction('dissocier_document','action');
+			$modes_documents = array('I/image','I/document','D/document'); // cf. @note
+			foreach($modes_documents as $mode) {
+				$dissocier_document("${id_album}-album-${mode}-${supprimer_docs_orphelins}");
+				// dès que l'album est vide, on arrête
+				if (!count(objet_trouver_liens(array('document'=>'*'),array('album'=>$id_album))))
+					break;
+			}
+			$succes[] = $id_album;
+		} else {
+			$erreurs[] = $id_album;
+		}
+	}
+
+	return array('succes'=>$succes, 'erreurs'=>$erreurs);
+}
+
+
+/**
+ * Lister les formulaire yaml des modèles «album» disponibles dans les dossiers modeles/
+ *
+ * Les premiers modèles retournés sont les modèles du plugin (album.yaml et album_liste.yaml)
+ *
+ * @staticvar array $liste_modeles_albums
  * @return array
  */
-function lister_modeles_albums($original=false){
-	static $liste_modeles = false;
+function albums_lister_modeles(){
 
-	if ($liste_modeles === false) {
-		$liste_modeles = array();
-		$match = ($original) ? "album[.]yaml$" : "[album]*[.]yaml$";
-		$liste = find_all_in_path(_DIR_YAML_MODELES_ALBUM, $match);
+	static $liste_modeles_albums = false;
+	if ($liste_modeles_albums === false) {
+		$liste_modeles_albums = array();
+
+		// d'abord, on liste les 2 modèles par défaut afin qu'ils soient en tête de liste
+		foreach(array('album.yaml','album_liste.yaml') as $modele)
+			$pre_liste[$modele] = find_in_path($modele,'modeles/');
+		// ensuite, on liste tous les modèles
+		$liste = find_all_in_path('modeles/', "album(_.*)?\.yaml$");
+		// puis on mélange
+		$liste = array_merge($pre_liste,$liste);
 
 		if (count($liste)){
 			include_spip('inc/yaml');
-			foreach($liste as $modele => $chemin)
-				$liste_modeles[$modele] = yaml_charger_inclusions(yaml_decode_file($chemin));
+			foreach($liste as $fichier => $chemin)
+				$liste_modeles_albums[$fichier] = yaml_charger_inclusions(yaml_decode_file($chemin));
 		}
 	}
 
-	return $liste_modeles;
+	return $liste_modeles_albums;
 }
 
+
 /**
- * Charger les informations d un modele
+ * Charger les informations d'un formulaire yaml de modèle «album»
  *
- * @staticvar array $infos_$modele
+ * @staticvar array $infos_modeles_album
  * @return array
  */
-function charger_infos_modele_album($modele){
-	static $infos_modele = array();
+function infos_modele_album($fichier){
 
-	if (!isset($infos_modele[$modele])) {
-		if (substr($modele,-5)!='.yaml')
-			$modele .= '.yaml';
-		if ($chemin = find_in_path($modele, _DIR_YAML_MODELES_ALBUM)) {
+	static $infos_modeles_album = array();
+	if (!isset($infos_modeles_album[$fichier])) {
+		if (substr($fichier,-5) != '.yaml')
+			$formulaire .= '.yaml';
+		if ($chemin = find_in_path($fichier,'modeles/')) {
 			include_spip('inc/yaml');
-			$infos_modele[$modele] = yaml_charger_inclusions(yaml_decode_file($chemin));
+			$infos_modeles_album[$fichier] = yaml_charger_inclusions(yaml_decode_file($chemin));
 		}
 	}
 
-	return $infos_modele[$modele];
+	return $infos_modeles_album[$fichier];
 }
 
 ?>
