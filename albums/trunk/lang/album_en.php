95866d6304316a42137983f0118382dea04dfc41
tcharlss@hotmail.fr <tcharlss@hotmail.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-07-08 03:41:30 +0000

    [Salvatore] [source:_plugins_/albums/trunk/lang/ album] Export depuis http://trad.spip.net de la langue en
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@83610 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- albums/trunk/lang/album_en.php
+++ albums/trunk/lang/album_en.php
@@ -8,104 +8,107 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// B
-	'bouton_associer' => 'Bind', # ou alors : link associate ?
-	'bouton_dissocier' => 'Unbind',
+	'bouton_associer' => 'Add this album', # ou alors : link associate ?
+	'bouton_dissocier' => 'Remove',
+	'bouton_editer_texte_album' => 'Edit text',
+	'bouton_supprimer' => 'Delete',
 	'bouton_valider_deplacer_documents' => 'Save changes',
+	'bouton_vider' => 'Empty',
 
 	// C
+	'c_albumotheque_titre_filtres' => 'Filter selection',
+	'c_albumotheque_titre_presentation' => 'Albums',
 	'cfg_titre_albums' => 'Albums',
 
 	// E
 	'erreur_deplacement' => 'Treatment could not be performed',
+	'explication_album_numero' => 'One or several ids comma-separated',
 
 	// F
-	'filtre_avec_contenu' => 'filled',
+	'filtre_extensions' => 'File extensions', # RELIRE
+	'filtre_medias' => 'Documents types',
 	'filtre_non_vus' => 'Not inserted in text',
-	'filtre_sans_contenu' => 'Empty',
-	'filtre_tous' => 'All',
-	'filtre_tous_contenus' => 'All contents',
+	'filtre_orphelins' => 'Orphans',
+	'filtre_types_utilisations' => 'Types of uses',
+	'filtre_utilisations' => 'Use',
 	'filtre_vus' => 'Inserted in text',
 
-	// H
-	'heading_contenu' => 'Content',
-	'heading_descriptif' => 'Description',
-	'heading_id' => 'ID',
-	'heading_infos' => 'Infos',
-	'heading_liaison' => 'Bind',
-	'heading_logo' => 'Logo',
-	'heading_titre' => 'Title',
-
 	// I
-	'icone_ajouter' => 'Add',
 	'icone_ajouter_album' => 'Add an album',
-	'icone_associer_album' => 'Bind an existing album',
 	'icone_creer_album' => 'Create a new album',
-	'icone_delier_album' => 'Remove this album',
-	'icone_fermer' => 'Close',
 	'icone_modifier_album' => 'Edit this album',
 	'info_1_album' => '1 album',
-	'info_1_association' => 'Associated to 1 object',
-	'info_album_vide' => 'Album is empty',
+	'info_1_utilisation' => '1 Use',
 	'info_aucun_album' => 'No album',
-	'info_aucun_album_criteres' => 'No album match these criteria',
-	'info_aucun_album_supplementaire' => 'No additional album',
-	'info_aucun_album_vide' => 'No empty album',
-	'info_aucune_association' => 'This album isn’t associated to any object',
-	'info_dans_objet' => 'in',
-	'info_documents_joints_albums' => 'Enable the interface to add documents to albums',
-	'info_js_prive' => 'animation desactivated in the private zone',
 	'info_nb_albums' => '@nb@ albums',
-	'info_nb_associations' => 'Associated to @nb@ objects',
+	'info_nb_utilisations' => '@nb@ uses',
 	'info_nouvel_album' => 'New album',
-	'info_publier' => 'publish',
-	'info_recherche_album_zero' => 'No result for « @cherche_album@ »',
-	'info_resultat_recherche_aucun' => 'No result',
-	'info_resultat_recherche_plus' => '@nb@ results',
-	'info_resultat_recherche_un' => '1 result',
-	'info_statut_modele' => 'Album unpublished',
 
 	// L
 	'label_activer_album_objets' => 'Enable albums for the contents :',
+	'label_album_numero' => 'Album number', # RELIRE
 	'label_case_utiliser_titre_defaut' => 'By default, use the parent object’s title',
+	'label_descriptif' => 'Description',
+	'label_modele_alias_liste' => 'List',
+	'label_modele_alias_vignettes' => 'Thumbnails',
 	'label_modele_alignement' => 'Alignment',
 	'label_modele_alignement_centre' => 'Center',
-	'label_modele_alignement_defaut' => 'Default',
 	'label_modele_alignement_droite' => 'Right',
 	'label_modele_alignement_gauche' => 'Left',
+	'label_modele_choisir' => 'Model choice',
+	'label_modele_defaut' => 'Default',
+	'label_modele_description_liste' => 'View documents as a list',
+	'label_modele_description_vignettes' => 'View images as thumbnails',
+	'label_modele_hauteur_images' => 'Max height',
 	'label_modele_identifiant' => 'Album id',
+	'label_modele_labels_images' => 'Label for each image',
+	'label_modele_largeur_images' => 'Max width',
+	'label_modele_meta_dimensions' => 'Dimensions',
+	'label_modele_meta_extension' => 'Extension',
+	'label_modele_meta_taille' => 'Size',
+	'label_modele_metas' => 'Show files infos :', # RELIRE
+	'label_modele_nom_liste' => 'an album (list)',
+	'label_modele_nom_vignettes' => 'an album (thumbnails)',
 	'label_modele_parcourir_albums' => 'Browse albums',
-	'label_modele_titre' => 'Custom title',
-	'label_modele_variante' => 'Variant',
-	'label_modele_variante_defaut' => 'Thumbnails (default)',
-	'label_modifier_balise' => 'Edit Tag',
+	'label_modele_placeholder_dimension' => 'Size in pixels, without unity', # RELIRE
+	'label_modele_recadrer_images' => 'Crop images',
+	'label_modele_titre_perso' => 'Custom title',
+	'label_modele_tri_date' => 'Date',
+	'label_modele_tri_id' => 'Document number',
+	'label_modele_tri_media' => 'Document type',
+	'label_modele_tri_titre' => 'Title',
+	'label_modele_trier' => 'Sort by :',
 	'label_titre' => 'Title',
 	'label_utiliser_titre_defaut' => 'Title of a new album',
 
 	// M
+	'message_1_album_ajoute' => '1 album has been added.',
+	'message_activer_cfg_documents' => 'In the document configuration form located in the «site content» menu, tick the « Albums » checkbox.',
+	'message_album_non_editable' => 'This album cannot be edited : it’s in use by one or several objects that you can’t edit.',
+	'message_avertissement_cfg_documents' => 'Warning ! Adding documents to albums is not enabled. This is a necessary option for the albums to work properly.', # RELIRE
 	'message_balise_inseree_succes' => 'The tag was inserted in the text',
+	'message_id_album_ajoute' => 'The album @id_album@ has been added.',
+	'message_nb_albums_ajoutes' => '@nb@ albums have been added.',
+	'message_supprimer' => 'Delete permanently ?',
+	'message_vider' => 'Remove all the documents ?',
 
 	// O
-	'onglet_associer_album' => 'Bind',
-	'onglet_configurer_base' => 'Albums',
+	'onglet_configurer_options' => 'Options',
 	'onglet_configurer_outils' => 'Tools',
-	'onglet_creer_album' => 'Create',
-	'onglet_mes_albums' => 'My albums',
-	'onglet_parents_tous' => 'All objects',
-	'onglet_tous' => 'All',
 
 	// T
-	'texte_ajouter_album' => 'Add an album',
-	'texte_associer_album' => 'Bind an existing album',
+	'texte_activer_ajout_albums' => 'You can enable the interface to add albums to articles, sections and so on.
+In a similar way to documents from portfolios, albums may then be referenced in the texts or displayed separately.', # RELIRE
 	'texte_changer_statut' => 'Album status',
 	'texte_creer_album' => 'Create a new album',
 	'texte_modifier' => 'Edit',
+	'texte_personnaliser_balise_album' => 'Customize tag',
 	'texte_statut_poubelle' => 'to the dustbin',
 	'texte_statut_prepa' => 'Unpublished',
 	'texte_statut_publie' => 'published online',
 	'titre_album' => 'Album',
-	'titre_album_sans_titre' => 'Titleless album',
 	'titre_albums' => 'Albums',
-	'titre_inserer_modele_album' => 'album',
+	'titre_documents_deplaces' => 'Documents moved',
 	'titre_logo_album' => 'Logo',
 	'titre_page_configurer_albums' => 'Configure Albums'
 );
