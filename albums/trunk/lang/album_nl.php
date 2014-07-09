13190abe5eaeb057a5668b20fde39c219305cdbe
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-07-09 03:42:14 +0000

    [Salvatore] [source:_plugins_/albums/trunk/lang/ album] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@83619 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- albums/trunk/lang/album_nl.php
+++ albums/trunk/lang/album_nl.php
@@ -8,37 +8,64 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// B
-	'bouton_associer' => 'Koppelen', # MODIF
+	'bouton_associer' => 'Voeg dit album toe',
 	'bouton_dissocier' => 'Ontkoppelen',
+	'bouton_editer_texte_album' => 'De tekst aanpassen',
+	'bouton_supprimer' => 'Verwijderen',
 	'bouton_valider_deplacer_documents' => 'De wijzigingen registreren',
+	'bouton_vider' => 'Ledigen',
 
 	// C
+	'c_albumotheque_filtres' => 'Met filters pas je bepaal de criteria toe
+		om de selectie van albums te beperken. Een klik activeert het filter, een tweede klik desactiveert het.
+		Combineer ze om gemakkelijk een bepaald album terug te vinden.
+		Wanneer de lijsten te lang zijn kun je met zoekvelden een specifiek object terugvinden.',
+	'c_albumotheque_titre_filtres' => 'De selectie filteren',
+	'c_albumotheque_titre_presentation' => 'De albums',
 	'cfg_titre_albums' => 'Albums',
 
 	// E
 	'erreur_deplacement' => 'De aanpassing kon niet worden uitgevoerd',
+	'explication_album_numero' => 'Een of meer identificaties, gescheiden door een komma',
 
 	// F
-	'filtre_non_vus' => 'Ongezien', # MODIF
-	'filtre_vus' => 'Gezien', # MODIF
+	'filtre_extensions' => 'Extensies',
+	'filtre_medias' => 'Documenttypes',
+	'filtre_non_vus' => 'Niet in een tekst ingevoegd',
+	'filtre_orphelins' => 'Wezen',
+	'filtre_types_utilisations' => 'Soorten van toepassingen',
+	'filtre_utilisations' => 'Toepassingen',
+	'filtre_vus' => 'In een tekst ingevoegd',
 
 	// I
 	'icone_ajouter_album' => 'Een album toevoegen',
 	'icone_creer_album' => 'Een nieuw album maken',
 	'icone_modifier_album' => 'Dit album aanpassen',
 	'info_1_album' => '1 album',
+	'info_1_utilisation' => '1 toepassing',
 	'info_aucun_album' => 'Geen enkel album',
 	'info_nb_albums' => '@nb@ albums',
+	'info_nb_utilisations' => '@nb@ toepassingen',
 	'info_nouvel_album' => 'Nieuw album',
 
 	// L
 	'label_activer_album_objets' => 'Activeer de volgende inhoud voor albums:',
-	'label_case_utiliser_titre_defaut' => 'Standaard wordt de titel van het bovenliggende object gebruikt', # MODIF
+	'label_album_numero' => 'Albumnummer',
+	'label_case_utiliser_titre_defaut' => 'Standaard wordt de titel van het bovenliggende object gebruikt',
+	'label_descriptif' => 'Omschrijving',
+	'label_modele_alias_liste' => 'Lijst',
+	'label_modele_alias_vignettes' => 'Vignetten',
 	'label_modele_alignement' => 'Aliniëring',
 	'label_modele_alignement_centre' => 'Gecentreerd',
 	'label_modele_alignement_droite' => 'Rechts',
 	'label_modele_alignement_gauche' => 'Links',
+	'label_modele_choisir' => 'Modelkeuze',
+	'label_modele_defaut' => 'Standaard',
+	'label_modele_description_vignettes' => 'Toon afbeeldingen als vignetten',
+	'label_modele_hauteur_images' => 'Maximale hoogte',
 	'label_modele_identifiant' => 'Albumnummer',
+	'label_modele_labels_images' => 'Label van iedere afbeelding',
+	'label_modele_largeur_images' => 'Maximale breedte',
 	'label_modele_parcourir_albums' => 'Door de albums gaan',
 	'label_titre' => 'Titel',
 	'label_utiliser_titre_defaut' => 'Titel van een nieuw album',
