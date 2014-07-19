67967034ed96447b496420a957c56ee2d9b91a8c
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-07-19 03:40:26 +0000

    [Salvatore] [source:_plugins_/albums/trunk/lang/ album] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@83711 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- albums/trunk/lang/album_fr.php
+++ albums/trunk/lang/album_fr.php
@@ -19,7 +19,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 		En les combinant, on peut facilement retrouver n’importe quel album.
 		Quand les listes sont trop longues, des champs de recherche permettent de retrouver un objet précis.',
 	'c_albumotheque_presentation' => 'Les albums complètent la gestion des documents de SPIP en permettant de composer des groupes de documents.
-		On peut les ajouter aux objets de la même façon que les documents «individuels».',
+		On peut les ajouter aux objets de la même façon que les documents « individuels ».',
 	'c_albumotheque_titre_filtres' => 'Filtrer la sélection',
 	'c_albumotheque_titre_presentation' => 'Les albums',
 	'cfg_titre_albums' => 'Albums',
