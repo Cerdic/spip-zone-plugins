38e2126af24b37ec040f9b78e217e73f73bda77e
tcharlss@hotmail.fr <tcharlss@hotmail.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-07-23 03:41:49 +0000

    [Salvatore] [source:_plugins_/albums/trunk/lang/ album] Export depuis http://trad.spip.net de la langue es
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@83826 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- albums/trunk/lang/album_es.php
+++ albums/trunk/lang/album_es.php
@@ -8,56 +8,123 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// B
-	'bouton_associer' => 'Asociar', # MODIF
+	'bouton_associer' => 'Añadir este álbum', # RELIRE
 	'bouton_dissocier' => 'Desatar',
+	'bouton_editer_texte_album' => 'Editar texto',
+	'bouton_supprimer' => 'Suprimir', # RELIRE
 	'bouton_valider_deplacer_documents' => 'Guardar cambios',
+	'bouton_vider' => 'Vaciar',
 
 	// C
+	'c_albumotheque_filtres' => 'Los filtros se utilizan para restringir la selección de álbumes. Aparecen dependiendo del número de álbumes y sus usos.
+<br>Con un solo clic se activa el filtro, otro clic lo desactiva. Por lo tanto, uno puede encontrar fácilmente cualquier álbum.
+<br>Cuando las listas son muy largas, campos de búsqueda permiten encontrar objetos específicos.', # RELIRE
+	'c_albumotheque_presentation' => '¡ Bienvenido a la « albumothèque » !<br>
+Se puede crear álbumes independientes en esta página, o crear álbumes asociados a los objetos de sus respectivas páginas.
+<br>Cada álbum es editable en el sitio (de edición de texto y manipulación de documentos), o visitando su página.', # RELIRE
+	'c_albumotheque_titre_filtres' => 'Filtrar la selección', # RELIRE
+	'c_albumotheque_titre_presentation' => 'Álbumes',
 	'cfg_titre_albums' => 'Álbumes',
 
 	// E
 	'erreur_deplacement' => 'El tratamiento no se ha podido efectuar',
+	'explication_album_numero' => 'Números separados por comas',
+	'explication_deplacer_documents' => '<strong>Experimental</strong> : es posible mover documentos entre álbumes haciendo un arrastrar y soltar.
+Si tienes los derechos necesarios, el cursor cambia a su informe general que la acción es posible.
+Una vez que se hicieron los viajes, un formulario aparece en la parte inferior de la lista para guardar los cambios.', # RELIRE
 
 	// F
-	'filtre_non_vus' => 'No encontrados', # MODIF
-	'filtre_vus' => 'Encontrados', # MODIF
+	'filtre_extensions' => 'Extensiones',
+	'filtre_medias' => 'Tipos de Documentos',
+	'filtre_non_vus' => 'No incluídos en el texto',
+	'filtre_orphelins' => 'Autónomos',
+	'filtre_types_utilisations' => 'Tipos de usos', # RELIRE
+	'filtre_utilisations' => 'Usos',
+	'filtre_vus' => ' Incluídos en el texto',
 
 	// I
 	'icone_ajouter_album' => 'Añadir un álbum',
 	'icone_creer_album' => 'Crear un nuevo álbum',
 	'icone_modifier_album' => 'Modificar este álbum',
 	'info_1_album' => '1 álbum',
+	'info_1_utilisation' => '1 Uso',
 	'info_aucun_album' => 'Ningún álbum',
 	'info_nb_albums' => '@nb@ álbumes',
+	'info_nb_utilisations' => '@nb@ usos',
 	'info_nouvel_album' => 'Nuevo álbum',
 
 	// L
 	'label_activer_album_objets' => 'Activar los álbumes por contenidos:',
-	'label_case_utiliser_titre_defaut' => 'Por defecto, utilizar el título del objeto predecesor', # MODIF
+	'label_activer_deplacer_documents' => 'Arrastrar y soltar', # RELIRE
+	'label_album_numero' => 'Número(s)',
+	'label_case_deplacer_documents' => 'Mover documentos entre álbumes haciendo un arrastrar y soltar.', # RELIRE
+	'label_case_utiliser_titre_defaut' => 'Por defecto, proponer el título del objeto predecesor',
+	'label_descriptif' => 'Descriptivo',
+	'label_modele_alias_liste' => 'Lista',
+	'label_modele_alias_vignettes' => 'Miniaturas',
 	'label_modele_alignement' => 'Vinculación',
 	'label_modele_alignement_centre' => 'Centrado',
 	'label_modele_alignement_droite' => 'Derecha',
 	'label_modele_alignement_gauche' => 'Izquierda',
+	'label_modele_choisir' => 'Selección del modelo', # RELIRE
+	'label_modele_defaut' => 'Por defecto', # RELIRE
+	'label_modele_description_liste' => 'Ver los documentos como lista', # RELIRE
+	'label_modele_description_vignettes' => 'Ver imágenes como miniaturas', # RELIRE
+	'label_modele_hauteur_images' => 'Altura máxima',
 	'label_modele_identifiant' => 'Número del álbum',
+	'label_modele_labels_images' => 'Etiquetar cada imagen', # RELIRE
+	'label_modele_largeur_images' => 'Anchura máxima', # RELIRE
+	'label_modele_meta_dimensions' => 'Dimensiones',
+	'label_modele_meta_extension' => 'Extensión',
+	'label_modele_meta_taille' => 'Tamaño',
+	'label_modele_metas' => 'Información del documento :', # RELIRE
+	'label_modele_nom_liste' => 'álbum (lista)',
+	'label_modele_nom_vignettes' => 'álbum (miniaturas)',
 	'label_modele_parcourir_albums' => 'Hojear los álbumes',
+	'label_modele_placeholder_dimension' => 'Tamaño, sin unidad', # RELIRE
+	'label_modele_recadrer_images' => 'Recortar imágenes', # RELIRE
+	'label_modele_titre_perso' => 'Título personalizado',
+	'label_modele_tri_date' => 'Fecha', # RELIRE
+	'label_modele_tri_id' => 'Número del documento',
+	'label_modele_tri_media' => 'Tipo de documento',
+	'label_modele_tri_titre' => 'Titulo',
+	'label_modele_trier' => 'Ordenar por :',
+	'label_onglet_ajouter_choisir' => 'Asociar álbumes existentes', # RELIRE
+	'label_onglet_ajouter_creer' => 'Crear y asociar un álbum', # RELIRE
 	'label_titre' => 'Título',
 	'label_utiliser_titre_defaut' => 'Título de un nuevo álbum',
 
 	// M
+	'message_1_album_ajoute' => '1 álbum ha sido añadido.',
+	'message_activer_cfg_documents' => 'En el formulario de configuración de los documentos adjuntos, marque la casilla "Álbumes".', # RELIRE
+	'message_album_non_editable' => 'No se puede editar este álbum : es utilizada por los objetos que no se pueden cambiar.', # RELIRE
+	'message_avertissement_cfg_documents' => 'Cuidado! Adición de documentos a los álbumes está desactivado. La activación es necesaria para el buen funcionamiento de los álbumes.', # RELIRE
 	'message_balise_inseree_succes' => 'La etiqueta se ha insertado en el texto',
+	'message_id_album_ajoute' => 'El álbum número @id_album@ ha sido añadido.',
+	'message_nb_albums_ajoutes' => 'Se han añadido @nb@ álbumes.',
+	'message_supprimer' => '¿ Suprimir permanentemente ?', # RELIRE
+	'message_vider' => '¿ Retirar todos los documentos ?',
 
 	// O
+	'onglet_ajouter_choisir' => 'Elijar álbumes', # RELIRE
+	'onglet_ajouter_creer' => 'Nuevo álbum',
+	'onglet_configurer_options' => 'Opciones',
 	'onglet_configurer_outils' => 'Herramientas',
 
 	// T
+	'texte_activer_ajout_albums' => 'Puedes activar la interfaz para añadir álbumes a los artículos, las secciones y otros.
+Como los documentos, después, se pueden ser incluídos en el texto, o mostrados por separado.',
 	'texte_changer_statut' => 'Modificar el estatus',
 	'texte_creer_album' => 'Crear un nuevo álbum',
+	'texte_double_clic_inserer_balise' => 'Haga doble clic para insertar la etiqueta en el texto', # RELIRE
 	'texte_modifier' => 'Modificar',
+	'texte_personnaliser_balise_album' => 'Personalizar la etiqueta',
 	'texte_statut_poubelle' => 'a la basura',
 	'texte_statut_prepa' => 'no publicado',
 	'texte_statut_publie' => 'publicado en línea',
 	'titre_album' => 'Álbum',
 	'titre_albums' => 'Álbumes',
+	'titre_documents_deplaces' => 'Documentos desplazados', # RELIRE
 	'titre_logo_album' => 'Logotipo',
 	'titre_page_configurer_albums' => 'Configurar los Álbumes'
 );
