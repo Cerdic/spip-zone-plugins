01b06e1a96f80a671fe7ed1aa38c3c8f072ac8fc
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-07-25 21:24:28 +0000

    On n'affiche pas le lien dans le menu si on n'est pas webmestre du site. Merci RastaPopoulos pour l'aide sur l'autorisation de MENU et non bouton. De plus, si mon identifiant est 'identifiant_avec_underscore', l'appel à l'autorisation sera autoriser_identifiantavecunderscore_dist.Faudrait qu'on mette à jour la doc sur http://programmer.spip.net/Definir-des-boutons
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@83864 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- info_spip/trunk/info_spip_autorisations.php
+++ info_spip/trunk/info_spip_autorisations.php
@@ -54,4 +54,105 @@ function autoriser_infospip_configurer_dist($faire, $type, $id, $qui, $opt)
     return $qui['statut'] == '0minirezo' and $qui['webmestre'] == 'oui';
 }
 
+// ------
+// Là, on va s'occuper des autorisations des liens vers les pages
+// du plugin dans les menu, pour SPIP 2
+// ------
+/**
+ * Autorisation pour afficher le lien vers la page ?exec=configurer_info_spip
+ *
+ * @param  string $faire Action demandée
+ * @param  string $type  Type d'objet sur lequel appliquer l'action
+ * @param  int    $id    Identifiant de l'objet
+ * @param  array  $qui   Description de l'auteur demandant l'autorisation
+ * @param  array  $opt   Options de cette autorisation
+ * @return bool          true s'il a le droit, false sinon
+**/
+function autoriser_configurer_info_spip_bouton_dist($faire, $type, $id, $qui, $opt)
+{
+    return $qui['statut'] == '0minirezo' and $qui['webmestre'] == 'oui';
+}
+
+/**
+ * Autorisation pour afficher le lien vers la page ?exec=info_config
+ *
+ * @param  string $faire Action demandée
+ * @param  string $type  Type d'objet sur lequel appliquer l'action
+ * @param  int    $id    Identifiant de l'objet
+ * @param  array  $qui   Description de l'auteur demandant l'autorisation
+ * @param  array  $opt   Options de cette autorisation
+ * @return bool          true s'il a le droit, false sinon
+**/
+function autoriser_info_config_bouton_dist($faire, $type, $id, $qui, $opt)
+{
+    return $qui['statut'] == '0minirezo' and $qui['webmestre'] == 'oui';
+}
+
+/**
+ * Autorisation pour afficher le lien vers la page ?exec=configurer_info_spip
+ * Pour le plugin Bando
+ *
+ * @param  string $faire Action demandée
+ * @param  string $type  Type d'objet sur lequel appliquer l'action
+ * @param  int    $id    Identifiant de l'objet
+ * @param  array  $qui   Description de l'auteur demandant l'autorisation
+ * @param  array  $opt   Options de cette autorisation
+ * @return bool          true s'il a le droit, false sinon
+**/
+function autoriser_configurer_info_spip_bando_bouton_dist($faire, $type, $id, $qui, $opt)
+{
+    return $qui['statut'] == '0minirezo' and $qui['webmestre'] == 'oui';
+}
+
+/**
+ * Autorisation pour afficher le lien vers la page ?exec=info_config
+ * Pour le plugin Bando
+ *
+ * @param  string $faire Action demandée
+ * @param  string $type  Type d'objet sur lequel appliquer l'action
+ * @param  int    $id    Identifiant de l'objet
+ * @param  array  $qui   Description de l'auteur demandant l'autorisation
+ * @param  array  $opt   Options de cette autorisation
+ * @return bool          true s'il a le droit, false sinon
+**/
+function autoriser_info_config_bando_bouton_dist($faire, $type, $id, $qui, $opt)
+{
+    return $qui['statut'] == '0minirezo' and $qui['webmestre'] == 'oui';
+}
+
+// ------
+// Là, on va s'occuper des autorisations des liens vers les pages
+// du plugin dans les menu, pour SPIP 3.
+// cf. `menu` à la place de `bouton`
+// ------
+/**
+ * Autorisation pour afficher le lien vers la page ?exec=configurer_info_spip
+ *
+ * @param  string $faire Action demandée
+ * @param  string $type  Type d'objet sur lequel appliquer l'action
+ * @param  int    $id    Identifiant de l'objet
+ * @param  array  $qui   Description de l'auteur demandant l'autorisation
+ * @param  array  $opt   Options de cette autorisation
+ * @return bool          true s'il a le droit, false sinon
+**/
+function autoriser_configurerinfospip_menu_dist($faire, $type, $id, $qui, $opt)
+{
+    return $qui['statut'] == '0minirezo' and $qui['webmestre'] == 'oui';
+}
+
+/**
+ * Autorisation pour afficher le lien vers la page ?exec=info_config
+ *
+ * @param  string $faire Action demandée
+ * @param  string $type  Type d'objet sur lequel appliquer l'action
+ * @param  int    $id    Identifiant de l'objet
+ * @param  array  $qui   Description de l'auteur demandant l'autorisation
+ * @param  array  $opt   Options de cette autorisation
+ * @return bool          true s'il a le droit, false sinon
+**/
+function autoriser_infoconfig_menu_dist($faire, $type, $id, $qui, $opt)
+{
+    return $qui['statut'] == '0minirezo' and $qui['webmestre'] == 'oui';
+}
+
 ?>
