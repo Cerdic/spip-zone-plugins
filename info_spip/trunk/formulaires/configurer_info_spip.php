822b08e689d22f839b13b7663a9a093f15578634
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-07-25 22:14:23 +0000

    On refait le fichier formulaires/configurer_info_spip.php qui avait l'air mal encodé… Ajustement de css pour SPIP 2.1
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@83870 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- info_spip/trunk/formulaires/configurer_info_spip.php
+++ info_spip/trunk/formulaires/configurer_info_spip.php
@@ -1,20 +1,22 @@
-﻿<?php
+<?php
+/**
+ * Fichier de configurer du Plugin SPIP
+ *
+ * @plugin     Info SPIP
+ * @copyright  2013-2014
+ * @author     Teddy Payet
+ * @licence    GNU/GPL
+ * @package    SPIP\Info_SPIP\Configurer
+ */
 
-/*
-* Plugin Info SPIP
-* (c) 2014 Teddy Payet
-* Distribue sous licence GPL
-*
-*/
-
-if (!defined("_ECRIRE_INC_VERSION")) {
+if (!defined('_ECRIRE_INC_VERSION')) {
     return;
 }
 
 include_spip('inc/config');
 include_spip('inc/meta');
 
-function formulaires_configurer_info_spip_charger_dist()
+function formulaires_configurer_info_spip_charger_dist ()
 {
     $valeurs = array();
 
@@ -24,9 +26,10 @@ function formulaires_configurer_info_spip_charger_dist()
     return $valeurs;
 }
 
-function formulaires_configurer_info_spip_verifier_dist()
+function formulaires_configurer_info_spip_verifier_dist ()
 {
     $erreurs = array();
+
     foreach (array('cle', 'actif') as $obligatoire) {
         if (!_request($obligatoire)) {
             $erreurs[$obligatoire] = _T('info_obligatoire');
@@ -35,14 +38,14 @@ function formulaires_configurer_info_spip_verifier_dist()
     return $erreurs;
 }
 
-function formulaires_configurer_info_spip_traiter_dist()
+function formulaires_configurer_info_spip_traiter_dist ()
 {
     $res = array();
 
-    $res['cle']         = _request('cle');
-    $res['actif']       = _request('actif');
-    $res['type_site']   = _request('type_site');
-    $res['modules']     = _request('modules');
+    $res['cle'] = _request('cle');
+    $res['actif'] = _request('actif');
+    $res['type_site'] = _request('type_site');
+    $res['modules'] = _request('modules');
 
     if (ecrire_meta('info_spip', @serialize($res), 'non')) {
         $res['message_erreur'] = _T('info_spip:enregistrement_ko');
@@ -52,5 +55,4 @@ function formulaires_configurer_info_spip_traiter_dist()
 
     return $res;
 }
-
 ?>
