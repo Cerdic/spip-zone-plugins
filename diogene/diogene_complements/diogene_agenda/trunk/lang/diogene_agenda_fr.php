e6698db70562cccd29f8b50d7bb036309b06f430
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-07-26 11:33:04 +0000

    é pas è sur "événement"
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@83875 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- diogene/diogene_complements/diogene_agenda/trunk/lang/diogene_agenda_fr.php
+++ diogene/diogene_complements/diogene_agenda/trunk/lang/diogene_agenda_fr.php
@@ -5,16 +5,17 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// E
-	'evenement_supprimer' => 'Supprimer l\'évènement',
+	'evenement_supprimer' => 'Supprimer l\'événement',
 
 	// F
-	'form_legend_agenda' => 'Évènement',
+	'form_legend_agenda' => 'Événement',
 
 	// L
 	'label_agenda_caches' => 'Champs de l\'agenda à ne pas afficher',
-	'label_agenda_legende' => 'Légende de la partie "évènement" du formulaire',
-	'label_agenda_obligatoire' => 'L\'évènement est obligatoire',
-	'label_cfg_ajout_agenda' => 'Évènement',
+	'label_agenda_legende' => 'Légende de la partie "événement" du formulaire',
+	'label_agenda_multiple' => 'Permettre de lier plusieurs événements à un article',
+	'label_agenda_obligatoire' => 'L\'événement est obligatoire',
+	'label_cfg_ajout_agenda' => 'Événement',
 );
 
 ?>
