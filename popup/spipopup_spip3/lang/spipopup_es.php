c2acde72351e13fe0211d7fd0d58b87570796e47
danielquisbert.info@gmail.com <danielquisbert.info@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-08-02 03:38:21 +0000

    [Salvatore] [source:_plugins_/popup/spipopup_spip3/lang/ spipopup] Export depuis http://trad.spip.net de la langue es
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@83993 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ popup/spipopup_spip3/lang/spipopup_es.php
@@ -0,0 +1,70 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/spipopup?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'btn_fermer_fenetre' => 'acerca', # RELIRE
+	'btn_fermer_fenetre_ttl' => 'Cerrar esta ventana',
+	'btn_haut_page' => 'Principio de la página',
+	'btn_haut_page_ttl' => 'Volver arriba',
+	'btn_imprimer' => 'Imprimir',
+	'btn_imprimer_ttl' => 'Imprimir esta página',
+
+	// D
+	'descr_cfg' => 'Documentación para más información del Plugin: [spip-contrib.net/ ?article3573->http://contrib.spip.net/?article3573]',
+
+	// E
+	'editor' => 'Enlace editorial popup', # RELIRE
+	'editor_enregistrer' => 'Registrar',
+	'editor_erreur_entree_obligatoire' => 'Esta entrada es obligatoria!',
+	'editor_lien' => 'Introduzca el enlace (URL u objeto SPIP)',
+	'editor_skel' => 'Página esqueleto SPIP
+',
+	'editor_texte' => 'Texto del enlace',
+	'editor_titre' => 'Título del enlace (al pasar el mouse)',
+	'editor_titre_inserer_lien_popup' => 'Inserte un popup link', # RELIRE
+
+	// H
+	'height' => 'Altura (en pixeles)',
+
+	// I
+	'inserer_lien_popup' => 'Inserte un enlace en la ventana popup',
+
+	// L
+	'legend_cfg_balise' => 'En la ventana externa',
+
+	// N
+	'nom' => 'Baliza #POPUP',
+	'nouvelle_fenetre' => '[Nueva ventana]',
+
+	// O
+	'options_popup' => 'Opciones JavaScript pasadas a la nueva ventana',
+	'options_popup_comment' => 'Escriba aquí una serie de opciones que se pasarán a la nueva ventana en forma de una tabla como "variable1: valor1, variable2: valor2, ..." (por ejemplo: "location : 0, scrollbars : 1"}).',
+
+	// P
+	'popup_titre' => 'Caja de diálogo',
+
+	// R
+	'retour_fenetre' => '[Volver a la ventana principal]',
+
+	// S
+	'skel_defaut' => 'Plantilla predeterminada utilizada para mostrar el contenido de la ventana',
+	'skel_defaut_comment' => 'Inicialmente el esqueleto "popup_defaut.html" en la raíz del plugin.',
+	'spipopup' => 'SPIPopup',
+
+	// T
+	'titre_descr_cfg' => 'Configuración de la función "Popup"',
+	'titre_popup' => 'Nombre de la ventana de JavaScript',
+	'titre_popup_comment' => 'Más tarde, puede utilizar este nombre para designar en el script ("window.popup").',
+
+	// W
+	'width' => 'Ancho (en píxeles)',
+	'width_and_height' => 'Tamaño de la ventana'
+);
+
+?>
