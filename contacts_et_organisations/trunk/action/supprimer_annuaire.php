c0b8ba7270c6e56ce84bf9cadad8c83aaeb6b382
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-08-04 15:41:35 +0000

    On est sur la fonction action_supprimer_annuaire_dist et non action_supprimer_contact_dist Une petite class supplémentaire qui est la plus utilisée
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@84017 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- contacts_et_organisations/trunk/action/supprimer_annuaire.php
+++ contacts_et_organisations/trunk/action/supprimer_annuaire.php
@@ -4,7 +4,7 @@
  * Supprimer un annuaire
  *
  * @plugin Contacts & Organisations pour Spip 3.0
- * @license GPL (c) 2009 - 2013
+ * @license GPL (c) 2009 - 2014
  * @author Cyril Marion, Matthieu Marcillaud, RastaPopoulos
  *
  * @package SPIP\Contacts\Actions
@@ -16,8 +16,7 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
  * Action pour supprimer un annuaire
  * 
  * @param null|int $arg
- *     Couple `type/id` où `type` est le type d'objet (organisation ou contact)
- *     et `id` son identifiant. En absence utilise l'argument de l'action sécurisée.
+ *     `id` de l'annuaire. En absence de `id`, utilise l'argument de l'action sécurisée.
 **/
 function action_supprimer_annuaire_dist($arg=null) {
 	if (is_null($arg)){
@@ -30,6 +29,6 @@ function action_supprimer_annuaire_dist($arg=null) {
 		sql_delete('spip_annuaires', 'id_annuaire = '.$id_annuaire);
 	}
 	else {
-		spip_log("action_supprimer_contact_dist $arg pas compris");
+		spip_log("action_supprimer_annuaire_dist $arg pas compris");
 	}
 }
