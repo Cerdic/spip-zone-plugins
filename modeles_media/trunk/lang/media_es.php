6a815ba5d1a0a1c8f501ccd64212f2d45139b57d
danielquisbert.info@gmail.com <danielquisbert.info@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-08-05 03:37:01 +0000

    [Salvatore] [source:_plugins_/modeles_media/trunk/lang/ media] Export depuis http://trad.spip.net de la langue es
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@84025 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ modeles_media/trunk/lang/media_es.php
@@ -0,0 +1,57 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/media?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'credits' => 'Créditos:',
+
+	// E
+	'erreur_saisies' => 'Su entrada contiene errores!',
+	'erreur_taille' => 'Debe introducir un número entero positivo.',
+	'explication_alt' => 'Usted puede personalizar el texto alternativo que se mostrará si el navegador no puede mostrar los medios de comunicación (imágenes, es atributo <i>alt</ i>). Dejar en blanco para el texto alternativo que se genera automáticamente.',
+	'explication_configurer_media' => 'Se pueden definir cuatro tamaños estándar para su uso con los modelos <media> a través del parámetro <i>|taille</i>.',
+	'explication_descriptif' => 'Se permite atajos tipográficos de SPIP.',
+	'explication_lien' => 'Puede introducir una URL de la forma http://monsite.net, una dirección de correo electrónico o un atajo SPIP (rub1, art3, breve4...).',
+	'explication_titre_lien' => 'Usted puede personalizar el atributo  <i>title</ i> enlace. Dejar en blanco para  atributo <i>title</ i> se genera automáticamente.', # RELIRE
+	'extension' => 'Extensión :',
+
+	// H
+	'hauteur' => 'Altura:',
+
+	// I
+	'info_configurer_media_titre' => 'Modelos &lt;media&gt;: configura los tamaños por default',
+	'info_inclusion_icone' => 'Inclusión del icono:',
+	'item_afficher' => 'fijar', # RELIRE
+	'item_icone' => 'mostrará el icono',
+	'item_insert' => 'incluir el docuemento',
+	'item_legende_complete' => 'leyenda completa',
+	'item_legende_simple' => 'leyenda simple (título + descripción)',
+	'item_lien_document' => 'enlace al documento',
+	'item_ne_pas_afficher' => 'no mostrar',
+	'item_ne_pas_redimensionner' => 'no redimensionar',
+	'item_pas_de_lien' => 'sin conexión',
+	'item_personnaliser' => 'personalizar',
+	'item_taille_grand' => 'grande',
+	'item_taille_icone' => 'icono',
+	'item_taille_moyen' => 'promedio',
+	'item_taille_petit' => 'pequeño',
+	'item_vignette' => 'vista en miniatura',
+
+	// L
+	'label_alt' => 'Texto alternativo:',
+	'label_configurer_media_largeur_legende' => 'Ancho subtítulos:',
+	'label_configurer_media_taille_grand' => 'Tamaño ’grande’:',
+	'label_configurer_media_taille_icone' => 'Tamaño ’icono’:',
+	'label_configurer_media_taille_moyen' => 'Tamaño ’mediano’:',
+	'label_configurer_media_taille_petit' => 'Tamaño ’pequeño’:',
+	'label_credits' => 'Créditos:',
+	'label_descriptif' => 'Descripción:',
+	'label_hauteur' => 'Altura (en pixeles) :'
+);
+
+?>
