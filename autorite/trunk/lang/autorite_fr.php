938184532b82a6f8430dff55baafc25012079e07
peetdu@gmail.com <peetdu@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-08-15 09:21:58 +0000

    afficher les rubriques liees aux auteurs non admin
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@84204 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- autorite/trunk/lang/autorite_fr.php
+++ autorite/trunk/lang/autorite_fr.php
@@ -77,6 +77,8 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'icone_menu_config' => 'Autorité',
 	'infos_selection' => '(vous pouvez sélectionner plusieurs secteurs avec la touche shift)',
 	'interdire_admin' => 'Cochez les cases ci-dessous pour interdire aux administrateurs de créer',
+	'info_gere_rubriques' => 'Gère les rubriques suivantes :',
+	'info_gere_rubriques_2' => 'Je gère les rubriques suivantes :',
 
 	// M
 	'mots_cles_qui' => '<strong>Mots-clés :</strong> qui peut créer et éditer les mots-clés :',
