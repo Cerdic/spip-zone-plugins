52fc127219f3ad840bb5e704b13883ec5bb79b23
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-08-16 20:16:13 +0000

    Petite erreur dans le fieldset
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@84215 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- info_spip/trunk/info_spip_fonctions.php
+++ info_spip/trunk/info_spip_fonctions.php
@@ -46,4 +46,15 @@ function lister_extensions_php ()
 
     return array();
 }
+
+function lister_config_php ()
+{
+    if (function_exists('ini_get_all')) {
+        $extensions = ini_get_all();
+        natcasesort($extensions);
+        return $extensions;
+    }
+
+    return array();
+}
 ?>
