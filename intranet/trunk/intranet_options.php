c254baab8dc4a2dee5132231ba08ad0f665631bc
ben.spip@gmail.com <ben.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-08-18 15:58:52 +0000

    Version 0.4  : N’autoriser que les utilisateurs loggués (ou à l’intérieur d’un réseau interne) à visiter le site. ... la nouveauté étant à l'intérieur d'un réseau interne
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@84261 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- intranet/trunk/intranet_options.php
+++ intranet/trunk/intranet_options.php
@@ -11,7 +11,44 @@
  * Autoriser a voir le site en Intranet : par defaut toute personne identifiée
  * @return mixed
  */
-function autoriser_intranet_dist(){ return (isset($GLOBALS['visiteur_session']['id_auteur']) && $GLOBALS['visiteur_session']['id_auteur'] > 0); }
+function autoriser_intranet_dist()
+{
+    $autoriser = false;
+    // On laisse en premier le cas du user connecté, cas le plus courant pour l'utilisation de ce plugin
+    if (isset($GLOBALS['visiteur_session']['id_auteur']) &&    $GLOBALS['visiteur_session']['id_auteur'] > 0    )
+    {
+        $autoriser = true;
+    } else {
+        /*cas intranet definit par une IP ou un range d'ip ...à faire : compatibilité IPV6 */
+        if (!function_exists('lire_config'))
+            include_spip('inc/config');
+
+        //récupération ip du client
+        if (isset($_SERVER['REMOTE_ADDR'])) $ip = $_SERVER['REMOTE_ADDR'];
+        $long_ip = ip2long($ip);
+
+        $ranges = explode(',', lire_config('intranet/plageip', ' '));
+        foreach ($ranges as $range) {
+            //Range d'ip contenant - comme séparateur
+            if (preg_match ("/-/",$range))  {
+                $ranges_2 = explode ('-', $range) ;
+                $low_long_ip = ip2long($ranges_2[0]);
+                $high_long_ip = ip2long($ranges_2[1]);
+                if ($long_ip <= $high_long_ip && $low_long_ip <= $long_ip) {
+                    $autoriser = true;
+                    break; // on a trouvé une ip bonne on ne continue pas plus loin
+                }
+            } // Ip individuelle
+            else {
+                if ($long_ip == ip2long($range)) {
+                    $autoriser = true;
+                    break;
+                }
+            }
+        }
+    }
+    return $autoriser;
+}
 
 // dans le site public
 // si auteur pas autorise : placer sur un cache dedie
--- intranet/trunk/lang/paquet-intranet_fr.php
+++ intranet/trunk/lang/paquet-intranet_fr.php
@@ -8,7 +8,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// I
 	'intranet_description' => 'N’autoriser que les utilisateurs loggués à visiter le site.',
 	'intranet_nom' => 'Intranet/Extranet',
-	'intranet_slogan' => 'N’autoriser que les utilisateurs loggués à visiter le site'
+	'intranet_slogan' => 'N’autoriser que les utilisateurs loggués (ou à l’intérieur d’un réseau interne) à visiter le site.'
 );
 
 ?>
