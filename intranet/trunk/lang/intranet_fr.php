1630463549117b459b2b45a3dad5aa64a5b5f1ac
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-08-19 03:40:28 +0000

    [Salvatore] [source:_plugins_/intranet/trunk/lang/ intranet] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@84271 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- intranet/trunk/lang/intranet_fr.php
+++ intranet/trunk/lang/intranet_fr.php
@@ -10,17 +10,17 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// E
 	'explications_message' => 'Message personnalisé à afficher au dessus du formulaire d’identification.',
-    'explications_pages_intranet' => 'Ajouter certaines pages SPIP spécifiques accessibles sans être connectés (à séparer par des virgules ",").',
-    'explications_plageip' => "IP ou plage d'IP permettant de consulter l'ensemble du site sans être connecté. Exemple: <code>10.5.0.1-10.5.22.13,10.6.134.132</code> " ,
+	'explications_pages_intranet' => 'Ajouter certaines pages SPIP spécifiques accessibles sans être connectés (à séparer par des virgules ",").',
+	'explications_plageip' => 'IP ou plage d’IP permettant de consulter l’ensemble du site sans être connecté. Exemple : <code>10.5.0.1-10.5.22.13,10.6.134.132</code> ',
 
 	// I
 	'info_intranet' => 'Identification obligatoire',
-    'info_intranet_texte' => 'Ce site n’est accessible qu’aux personnes identifiées.',
+	'info_intranet_texte' => 'Ce site n’est accessible qu’aux personnes identifiées.',
 
-    // L
+	// L
 	'label_message' => 'Personnalisation du message',
-    'label_plageip' => 'Plage IP',
-	'label_pages_intranet' => 'Pages accessibles'
+	'label_pages_intranet' => 'Pages accessibles',
+	'label_plageip' => 'Plage IP'
 );
 
 ?>
