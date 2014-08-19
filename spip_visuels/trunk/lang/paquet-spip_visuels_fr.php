a1e26cd96f9c9fcffa58ac8505fdbe171f59df68
hadrien@apsulis.com <hadrien@apsulis.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-08-19 14:58:55 +0000

    Une description pour le plugin visuels
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@84274 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spip_visuels/trunk/lang/paquet-spip_visuels_fr.php
+++ spip_visuels/trunk/lang/paquet-spip_visuels_fr.php
@@ -6,7 +6,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// S
-	'spip_visuels_description' => '',
+	'spip_visuels_description' => 'Gestion de visuels pour les articles selon leur contexte d\'affichage sur le site public',
 	'spip_visuels_nom' => 'Visuels',
 	'spip_visuels_slogan' => '',
 );
