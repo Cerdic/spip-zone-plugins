6dae7f7b13ad5946a0a0e3f448840338ea143936
nicolas.dorigny@gmail.com <nicolas.dorigny@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-08-22 16:09:51 +0000

    Sur les pages articles, les boutons "Créer une rubrique" ou "Ecrire un article" ne passaient pas en paramètre l'id_parent ou l'id_rubrique, ces nouveaux objets n'étaient donc pas directement positionnés dans la rubrique en cours. Pas très pratique. Le filtre ajoute l'id_rubrique dans le contexte en le déduisant de id_article s'il est défini. Peut être extensible à d'autres objets ?
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@84292 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- minibando/trunk/formulaires/administration_fonctions.php
+++ minibando/trunk/formulaires/administration_fonctions.php
@@ -2,4 +2,11 @@
 
 include_spip('inc/bandeau');
 
+function minibando_definir_rubrique_contexte($contexte) {
+    if( !isset($contexte['id_rubrique']) && isset($contexte['id_article']) && $contexte['id_article'] ){
+        $contexte['id_rubrique'] = sql_getfetsel('id_rubrique','spip_articles','id_article='.(int)$contexte['id_article']);
+    }
+    return $contexte;
+}
+
 ?>
