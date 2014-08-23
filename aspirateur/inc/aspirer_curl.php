98d806c0b412660e05df995ff993130c3568b43b
toutati@free.fr <toutati@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-08-23 15:25:15 +0000

    fns inutiles
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@84302 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- aspirateur/inc/aspirer_curl.php
+++ aspirateur/inc/aspirer_curl.php
@@ -112,19 +112,6 @@ function isoler_contenu($chaine){
 	return $chaine;   
 }
 
-function deleteNode($node) {
-    deleteChildren($node);
-    $parent = $node->parentNode;
-    $oldnode = $parent->removeChild($node);
-} 
-
-function deleteChildren($node) {
-    while (isset($node->firstChild)) {
-        deleteChildren($node->firstChild);
-        $node->removeChild($node->firstChild);
-    }
-} 
-
 
 /**
  * 
