ded941a3eaba0e5deb12c0426ce3484c2da613b8
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-08-25 13:11:13 +0000

    Mieux gérer les changements de statuts
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@84335 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- emballe_medias/emballe_medias/trunk/formulaires/media_edition_lot.php
+++ emballe_medias/emballe_medias/trunk/formulaires/media_edition_lot.php
@@ -6,7 +6,7 @@
  * kent1 (http://www.kent1.info - kent1@arscenic.info)
  * b_b (http://http://www.weblog.eliaz.fr)
  *
- * © 2008/2013 - Distribue sous licence GNU/GPL
+ * © 2008/2014 - Distribue sous licence GNU/GPL
  *
  * Formulaire d'édition par lot de médias
  **/
@@ -20,30 +20,30 @@ function formulaires_media_edition_lot_charger_dist(){
 	$diogene = sql_fetsel('*','spip_diogenes','objet="emballe_media"');
 	$valeurs['id_secteur'] = $valeurs['id_parent'] = $diogene['id_secteur'];
 	$valeurs['id_diogene'] = $diogene['id_diogene'];
+
 	if($diogene['champs_ajoutes'])
 		$valeurs['diogene_args'] = array('champs_ajoutes'=>$diogene['champs_ajoutes']);
-	$valeurs['statuts_medias'] = _request('statuts_medias') ? _request('statuts_medias') : 'prepa';	
-	if(_request('change_statut_prop')){
+
+	$valeurs['statuts_medias'] = _request('statuts_medias') ? _request('statuts_medias') : 'prepa';
+	if(_request('change_statut_prop') && !_request('validation')){
 		$valeurs['debut_medias'] = 0;
 		$valeurs['statuts_medias'] = 'prop';
 		set_request('statuts_medias','prop');
 	}
-	if(_request('change_statut_publie')){
+	if(_request('change_statut_publie') && !_request('validation')){
 		$valeurs['debut_medias'] = 0;
 		$valeurs['statuts_medias'] = 'publie';
 		set_request('statuts_medias','publie');
 	}
-	if(_request('change_statut_prepa')){
+	if(_request('change_statut_prepa') && !_request('validation')){
 		$valeurs['debut_medias'] = 0;
 		$valeurs['statuts_medias'] = 'prepa';
 		set_request('statuts_medias','prepa');
 	}
-	
-	$valeurs['statut'] = $valeurs['statuts_medias'];
+
 	unset($valeurs['id_article']);
 	$valeurs['medias'] = _request('medias');
-	set_request('statut',$valeurs['statut']);
-	$valeurs['_pipeline'] = array('editer_contenu_objet',array('type'=>'article'));
+	set_request('statut_medias',$valeurs['statut_medias']);
 
 	$valeurs['config'] = $GLOBALS['meta'];
 	include_spip('inc/autoriser');
@@ -82,7 +82,6 @@ function formulaires_media_edition_lot_verifier_dist(){
 		}
 	}
 	if (count($erreurs)) $erreurs['message_erreur'] = _T('emballe_medias:verifier_formulaire');
-	
 	return $erreurs;
 }
 
@@ -92,8 +91,9 @@ function formulaires_media_edition_lot_traiter_dist(){
 		return $res;
 
 	$medias_a_traiter = _request('medias');
-	
+
 	$statut = _request('statut');
+
 	if(($statut && !in_array($statut,array('prop','prepa','publie','poubelle'))) || ($statut == ''))
 		set_request('statut',NULL);
 
@@ -108,6 +108,7 @@ function formulaires_media_edition_lot_traiter_dist(){
 			$i++;
 		}
 	}
+
 	foreach($medias_a_traiter as $id_article){
 		if($titre == ''){
 			$titre_envoi = sql_getfetsel('titre','spip_articles','id_article='.intval($id_article));
