22a3631a343ba6ca2c6eea1e7d9e4929c7405638
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-08-30 03:44:19 +0000

    [Salvatore] [source:_plugins_/archive/trunk/lang/ archive] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@84407 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- archive/trunk/lang/archive_en.php
+++ archive/trunk/lang/archive_en.php
@@ -1,53 +1,52 @@
 <?php
-
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/archive/trunk/lang/
+// extrait automatiquement de http://trad.spip.net/tradlang_module/archive?lang_cible=en
+// ** ne pas modifier le fichier **
 
-if (!defined("_ECRIRE_INC_VERSION")) return;
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-	//A
+	// A
 	'arch_auto' => 'Automatic archiving: ',
 	'articles_archives' => 'Archived articles:',
-	
-	//C
+
+	// C
 	'choix_rubrique' => 'Choice of the section to use:',
-	
-	//C
+
+	// D
 	'doc_complete' => 'Complete documentation',
-	
-	//I
+
+	// I
 	'info_article_archive' => 'Archived article',
 	'info_defaut_desc' => 'You can define here the settings for automatic archiving',
 
-	//J
-	'jours_archive' => 'Archive articles of more than:',
+	// J
 	'jours' => 'day(s)',
-	
+	'jours_archive' => 'Archive articles of more than:',
+
 	// L
 	'label_archiver_publier' => 'The archives are visible in the public space',
 	'legend_arch_auto' => 'Settings for automatic archiving',
-	
-	//N
-	'non' => 'No',
+
+	// N
 	'nom_plugin' => 'Archive plugin',
-	
-	//O
+	'non' => 'No',
+
+	// O
 	'oui' => 'Yes',
-	
+
 	// R
-	'rubriques_archiver' => 'Section to archive:',
 	'rappel_utilisation' => 'Reminder on how to use it:',
-	
+	'rubriques_archiver' => 'Section to archive:',
+
 	// T
 	'texte_statut_archive' => 'archived',
 	'titre_archive' => 'Archive plugin',
 	'titre_archives_rubrique' => 'The archives of this section',
 
-	//V
+	// V
 	'version' => 'Current version:'
-
 );
 
 ?>
--- archive/trunk/lang/archive_fr.php
+++ archive/trunk/lang/archive_fr.php
@@ -1,53 +1,50 @@
 <?php
-
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
 // Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/archive/trunk/lang/
-
-if (!defined("_ECRIRE_INC_VERSION")) return;
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-	//A
-	'arch_auto' => 'Archivage automatique : ',
-	'articles_archives' => 'Les articles archiv&eacute;s :',
-	
-	//C
-	'choix_rubrique' => 'Choix de la rubrique &agrave; utiliser :',
-	
-	//C
-	'doc_complete' => 'Documentation compl&eacute;te',
-	
-	//I
-	'info_article_archive' => 'Article archivé',
-	'info_defaut_desc' => 'Vous pouvez d&eacute;finir ici les param&egrave;tres d\'archivage automatique',
+	// A
+	'arch_auto' => 'Archivage automatique : ',
+	'articles_archives' => 'Les articles archivés :',
 
-	//J
-	'jours_archive' => 'Archiver les articles de plus de :',
+	// C
+	'choix_rubrique' => 'Choix de la rubrique à utiliser :',
+
+	// D
+	'doc_complete' => 'Documentation compléte',
+
+	// I
+	'info_article_archive' => 'Article archivé',
+	'info_defaut_desc' => 'Vous pouvez définir ici les paramètres d’archivage automatique',
+
+	// J
 	'jours' => 'jour(s)',
-	
+	'jours_archive' => 'Archiver les articles de plus de :',
+
 	// L
-	'label_archiver_publier' => 'Les archives sont visibles dans l\'espace public',
-	'legend_arch_auto' => 'Param&egrave;tres d\'archivage automatique',
-	
-	//N
-	'non' => 'Non',
+	'label_archiver_publier' => 'Les archives sont visibles dans l’espace public',
+	'legend_arch_auto' => 'Paramètres d’archivage automatique',
+
+	// N
 	'nom_plugin' => 'Le plugin Archive',
-	
-	//O
+	'non' => 'Non',
+
+	// O
 	'oui' => 'Oui',
-	
+
 	// R
-	'rubriques_archiver' => 'Rubriques &agrave; archiver :',
-	'rappel_utilisation' => 'Rappel sur comment l\'utiliser :',
-	
+	'rappel_utilisation' => 'Rappel sur comment l’utiliser :',
+	'rubriques_archiver' => 'Rubriques à archiver :',
+
 	// T
 	'texte_statut_archive' => 'archivé',
 	'titre_archive' => 'Le plugin Archive',
 	'titre_archives_rubrique' => 'Les archives de cette rubrique',
 
-	//V
-	'version' => 'Version en cours :'
-
+	// V
+	'version' => 'Version en cours :'
 );
 
 ?>
