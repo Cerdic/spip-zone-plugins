cb710ecd993626d645ea36a4a051e47b15af465d
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-09-06 03:40:32 +0000

    [Salvatore] [source:_plugins_/alerte_urgence/trunk/lang/ alerte] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@84503 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- alerte_urgence/trunk/lang/alerte_fr.php
+++ alerte_urgence/trunk/lang/alerte_fr.php
@@ -1,14 +1,15 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/alerte_urgence/lang/
-if (!defined("_ECRIRE_INC_VERSION")) return;
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/alerte_urgence/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+
 	// C
-	'configurer_texte_label' => 'Alerte d’urgence à diffuser :',
+	'configurer_texte_label' => 'Alerte d’urgence à diffuser :',
 	'configurer_titre' => 'Configurer l’alerte d’urgence',
 	'configurer_utilisateurs_explication' => 'Par défaut, seuls les administrateurs complets peuvent modifier l’alerte. On peut sélectionner ici des utilisateurs supplémentaires.',
-	'configurer_utilisateurs_label' => 'Utilisateurs supplémentaires autorisés',
+	'configurer_utilisateurs_label' => 'Utilisateurs supplémentaires autorisés'
 );
 
 ?>
