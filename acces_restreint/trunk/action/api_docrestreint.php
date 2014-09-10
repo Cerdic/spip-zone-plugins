191bc76c772ae8c5a69ff20e2037193653292235
marcimat@rezo.net <marcimat@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-09-10 14:02:12 +0000

    Petit reformattage pour y voir un rien plus clair dans la fonction api_docrestreint. À suivre...
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@84556 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- acces_restreint/trunk/action/api_docrestreint.php
+++ acces_restreint/trunk/action/api_docrestreint.php
@@ -15,7 +15,8 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 include_spip('inc/headers');
 
 /**
- * acces aux documents joints securise
+ * Acces aux documents joints securise
+ * 
  * verifie soit que le demandeur est authentifie
  * soit que le document est publie, c'est-a-dire
  * joint a au moins 1 article, breve ou rubrique publie
@@ -26,104 +27,100 @@ include_spip('inc/headers');
  * @param null $arg
  */
 function action_api_docrestreint_dist($arg=null) {
-	$status = 404;
 
-	if (is_null($arg))
-		$arg =_request('arg');
+	// Obtenir l'argument '{id_document}/{cle_action}/{chemin_fichier.ext}'
+	if (is_null($arg)) {
+		$arg = _request('arg');
+	}
+	$arg = explode("/", $arg);
 
-	$arg = explode("/",$arg);
+	// Supprimer et vider les buffers qui posent des problemes de memory limit
+	accesrestreint_vider_buffers();
 
-	// supprimer et vider les buffers qui posent des problemes de memory limit
-	// http://www.php.net/manual/en/function.readfile.php#81032
-	@ini_set("zlib.output_compression","0"); // pour permettre l'affichage au fur et a mesure
-	@ini_set("output_buffering","off");
-	@ini_set('implicit_flush', 1);
-	@ob_implicit_flush(1);
-	$level = ob_get_level();
-	while ($level--){
-		@ob_end_clean();
+	// manque des arguments : 404
+	if (count($arg) < 3) {
+		accesrestreint_afficher_404_document();
+		return;
 	}
 
+	// Séparer les 3 arguments
+	// file ($f) exige pour eviter le scan id_document par id_document
+	$id_document = intval(array_shift($arg));
+	$cle         = array_shift($arg);
+	$f           = implode("/", $arg);
 
-	if (count($arg)>=3){
+	/**
+	 * URL de test de fonctionnement
+	 * @see accesrestreint_gerer_htaccess()
+	 */
+	if ($id_document==0 AND $cle==1 AND $f=="test/.test") {
+		echo "OK";
+		return;
+	}
 
-		$id_document = intval(array_shift($arg));
-		$cle = array_shift($arg);
-		// file exige pour eviter le scan id_document par id_document
-		$f = implode("/",$arg);
+	include_spip('inc/documents');
 
-		if ($id_document==0
-			AND $cle==1
-		  AND $f=="test/.test") {
-			echo "OK";
-			return;
-		}
+	$file = get_spip_doc($f);
+	spip_log($file, 'dbg');
 
-		include_spip('inc/documents');
+	$status = $doc = false;
+	$dossiers_a_exclure = array('nl');
 
-		$file = get_spip_doc($f);
-		spip_log($file,'dbg');
+	// securite : on refuse tout ../ ou url absolue
+	if ((strpos($f, '../') !== false) OR (preg_match(',^\w+://,', $f))) {
+		$status = 403;
+	} elseif (!file_exists($file) OR !is_readable($file)) {
+		$status = 404;
+	}
 
-		$status = $dcc = false;
-		$dossiers_a_exclure = array('nl');
-		
-		// securite : on refuse tout ../ ou url absolue
-		if (strpos($f,'../') !== false
-		  OR preg_match(',^\w+://,', $f)) {
-			$status = 403;
-		}
-		else if (!file_exists($file) OR !is_readable($file)) {
+	// Si c'est dans un sous-dossier explicitement utilisé pour autre chose que les documents
+	// (exemple : les newsletters)
+	// et bien on ne teste pas l'accès
+	elseif (preg_match('%^(' . join('|', $dossiers_a_exclure) . ')/%', $f)){
+		$status = 200;
+	}
+	else {
+		$where = "documents.fichier=".sql_quote(set_spip_doc($file))
+		. ($id_document ? " AND documents.id_document=".intval($id_document): '');
+		spip_log($where,'dbg');
+
+		$doc = sql_fetsel("documents.id_document, documents.titre, documents.fichier, types.mime_type, types.inclus, documents.extension", "spip_documents AS documents LEFT JOIN spip_types_documents AS types ON documents.extension=types.extension",$where);
+		spip_log($doc,'dbg');
+		if (!$doc) {
 			$status = 404;
 		}
-		// Si c'est dans un sous-dossier explicitement utilisé pour autre chose que les documents
-		// (exemple : les newsletters)
-		// et bien on ne teste pas l'accès
-		elseif (preg_match('%^(' . join('|', $dossiers_a_exclure) . ')/%', $f)){
-			$status = 200;
-		}
 		else {
-			$where = "documents.fichier=".sql_quote(set_spip_doc($file))
-			. ($id_document ? " AND documents.id_document=".intval($id_document): '');
-			spip_log($where,'dbg');
 
-			$doc = sql_fetsel("documents.id_document, documents.titre, documents.fichier, types.mime_type, types.inclus, documents.extension", "spip_documents AS documents LEFT JOIN spip_types_documents AS types ON documents.extension=types.extension",$where);
-			spip_log($doc,'dbg');
-			if (!$doc) {
-				$status = 404;
+			// ETag pour gerer le status 304
+			$ETag = md5($file . ': '. filemtime($file));
+			if (isset($_SERVER['HTTP_IF_NONE_MATCH'])
+			  AND $_SERVER['HTTP_IF_NONE_MATCH'] == $ETag) {
+				http_status(304); // Not modified
+				exit;
 			}
 			else {
+				header('ETag: '.$ETag);
+			}
 
-				// ETag pour gerer le status 304
-				$ETag = md5($file . ': '. filemtime($file));
-				if (isset($_SERVER['HTTP_IF_NONE_MATCH'])
-				  AND $_SERVER['HTTP_IF_NONE_MATCH'] == $ETag) {
-					http_status(304); // Not modified
-					exit;
-				}
-				else {
-					header('ETag: '.$ETag);
-				}
+			//
+			// Verifier les droits de lecture du document
 
-				//
-				// Verifier les droits de lecture du document
-
-				// en controlant la cle passee en argument si elle est dispo
-				// (perf issue : toutes les urls ont en principe cette cle fournie dans la page au moment du calcul de la page)
-				if ($cle){
-					include_spip('inc/securiser_action');
-					if (!verifier_cle_action($doc['id_document'].','.$f, $cle)) {
-						spip_log("acces interdit $cle erronee");
-						$status = 403;
-					}
+			// en controlant la cle passee en argument si elle est dispo
+			// (perf issue : toutes les urls ont en principe cette cle fournie dans la page au moment du calcul de la page)
+			if ($cle){
+				include_spip('inc/securiser_action');
+				if (!verifier_cle_action($doc['id_document'].','.$f, $cle)) {
+					spip_log("acces interdit $cle erronee");
+					$status = 403;
 				}
-				// en verifiant le droit explicitement sinon, plus lent !
-				else {
-					if (!function_exists("autoriser"))
-						include_spip("inc/autoriser");
-					if (!autoriser('voir', 'document', $doc['id_document'])) {
-						$status = 403;
-						spip_log("acces interdit $cle erronee");
-					}
+			}
+			// en verifiant le droit explicitement sinon, plus lent !
+			else {
+				if (!function_exists("autoriser"))
+					include_spip("inc/autoriser");
+				if (!autoriser('voir', 'document', $doc['id_document'])) {
+					$status = 403;
+					spip_log("acces interdit $cle erronee");
 				}
 			}
 		}
@@ -138,10 +135,7 @@ function action_api_docrestreint_dist($arg=null) {
 		break;
 
 	case 404:
-		http_status(404);
-		include_spip('inc/minipres');
-		echo minipres(_T('erreur').' 404',
-			_T('medias:info_document_indisponible'),"",true);
+		accesrestreint_afficher_404_document();
 		break;
 
 	default:
@@ -178,4 +172,31 @@ function action_api_docrestreint_dist($arg=null) {
 
 }
 
-?>
+/**
+ * Supprimer et vider les buffers qui posent des problemes de memory limit
+ *
+ * @link http://www.php.net/manual/en/function.readfile.php#81032
+ * 
+ * @return void
+**/
+function accesrestreint_vider_buffers() {
+	@ini_set("zlib.output_compression","0"); // pour permettre l'affichage au fur et a mesure
+	@ini_set("output_buffering","off");
+	@ini_set('implicit_flush', 1);
+	@ob_implicit_flush(1);
+	$level = ob_get_level();
+	while ($level--){
+		@ob_end_clean();
+	}
+}
+
+/**
+ * Retourne une page 404 indiquant un document introuvable
+ * 
+ * @return void
+**/
+function accesrestreint_afficher_404_document() {
+	http_status(404);
+	include_spip('inc/minipres');
+	echo minipres(_T('erreur') . ' 404', _T('medias:info_document_indisponible'), "", true);
+}
