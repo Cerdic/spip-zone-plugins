21246f84fdfcb698ac56cec7abbe532eb206f782
ben.spip@gmail.com <ben.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-09-10 10:07:59 +0000

    diverses améliorations du plugin
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@84549 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- wp_import/trunk/formulaires/import_wordpress.php
+++ wp_import/trunk/formulaires/import_wordpress.php
@@ -4,7 +4,63 @@
 function formulaires_import_wordpress_charger()
 {
 
-    $valeurs['mes_saisies'] = mes_saisies_import_wordpress();
+    $valeurs['mes_saisies'] =
+        array(
+            array(
+                'saisie' => 'input',
+                'options' => array(
+                    'nom' => 'id_parent',
+                    'label' => _T('wp_import:id_parent_label'),
+                    'explication' => _T('wp_import:id_parent_explication'),
+                    'obligatoire' => 'oui',
+                    'defaut' => 0
+                )),
+
+            array(
+                'saisie' => 'input',
+                'options' => array(
+                    'nom' => 'document_xml',
+                    'label' => _T('wp_import:document_xml_label'),
+                    'explication' => _T('wp_import:document_xml_explication')
+                )),
+            array(
+                'saisie' => 'case',
+                'options' => array(
+                    'nom' => 'menage',
+                    'label' => _T('wp_import:menage_label'),
+                    'explication' => _T('wp_import:menage_explication')
+                )),
+            array(
+                'saisie' => 'case',
+                'options' => array(
+                    'nom' => 'auteurs',
+                    'label' => _T('wp_import:auteurs_label'),
+                    'explication' => _T('wp_import:auteurs_explication')
+                )),
+            array(
+                'saisie' => 'case',
+                'options' => array(
+                    'nom' => 'rubriques',
+                    'label' => _T('wp_import:rubriques_label'),
+                    'explication' => _T('wp_import:rubriques_explication')
+                )),
+            array(
+                'saisie' => 'case',
+                'options' => array(
+                    'nom' => 'documents',
+                    'label' => _T('wp_import:documents_label'),
+                    'explication' => _T('wp_import:documents_explication')
+                )),
+            array(
+                'saisie' => 'case',
+                'options' => array(
+                    'nom' => 'articles',
+                    'label' => _T('wp_import:articles_label'),
+                    'explication' => _T('wp_import:articles_explication')
+                ))
+
+        );
+
 
     return $valeurs;
 }
@@ -14,6 +70,16 @@ function formulaires_import_wordpress_verifier_dist()
 {
 
     $erreurs = array();
+    $fichier = _DIR_TMP . _request('document_xml');
+    if (_request('document_xml')) {
+        if (!file_exists($fichier)) {
+            $erreurs['document_xml'] = _T('wp_import:erreur_fichier', array('fichier' => "$fichier"));
+            $erreurs['message_erreur'] = _T('wp_import:erreur_generale');
+        }
+    } else {
+        $erreurs['document_xml'] = _T('wp_import:erreur_fichier_vide');
+    }
+
     return $erreurs;
 }
 
@@ -46,17 +112,9 @@ function wp_import_import_wordpress()
 
 
     $chemin_temp = sous_repertoire(_DIR_TMP, 'wordpress');
-    $doc = & $_FILES['document_xml'];
-    $annee = _request('id_rubrique');
-    $nom_fichier = $doc['name'];
-    $chemin_fichier = $chemin_temp . $nom_fichier;
+    $chemin_fichier = _DIR_TMP . _request('document_xml');
     include_spip('inc/getdocument');
 
-    if (!deplacer_fichier_upload($doc['tmp_name'], $chemin_fichier))
-        $erreurs['document_csv'] = _T('copie_document_impossible');
-    else {
-
-    }
 
     if (file_exists($chemin_fichier)) {
 
@@ -66,26 +124,37 @@ function wp_import_import_wordpress()
         $arbre = spip_xml_load($chemin_fichier);
         $arbre = array_shift($arbre);
         $arbre = $arbre[0]['channel'][0];
-
         //var_dump(array_keys($arbre));
-        sql_query('TRUNCATE TABLE spip_documents ');
-        sql_query('TRUNCATE TABLE spip_documents_liens');
-        sql_query('TRUNCATE TABLE spip_articles');
-        sql_query('TRUNCATE TABLE spip_rubriques');
-        sql_query('TRUNCATE TABLE spip_rubriques_liens');
-        sql_query('DELETE FROM spip_auteurs where id_auteur>1 ');
-        sql_query('TRUNCATE TABLE spip_auteurs_liens ');
-        sql_query('ALTER TABLE `spip_auteurs` AUTO_INCREMENT =1');
+
+        $menage = _request('menage');
+        if ($menage) {
+            spip_log("Vidage des tables ", "wp_import" . _LOG_INFO_IMPORTANTE);
+            spip_query("DELETE FROM   'spip_documents' ");
+            spip_query("DELETE FROM  'spip_documents_liens' ");
+            spip_query("DELETE FROM  'spip_articles' ");
+            spip_query("DELETE FROM  'spip_rubriques' ");
+            spip_query("DELETE FROM  'spip_rubriques_liens'");
+            spip_query('DELETE FROM spip_auteurs where id_auteur>1 ');
+            spip_query("DELETE FROM  'spip_auteurs_liens' ");
+            spip_query('ALTER TABLE `spip_auteurs` AUTO_INCREMENT =1');
+        } else {
+            spip_log("Pas de ménage", "wp_import" . _LOG_INFO_IMPORTANTE);
+        }
+
         $tab_document = array();
         include_spip("action/ajouter_documents");
+        $cpt_auteurs=0;
+        $cpt_rubriques=0;
+        $cpt_articles=0;
 
         foreach ($arbre as $type => $a) {
 
             switch ($type) {
-
                 // Importation des auteurs
-
                 case "wp:author":
+                    //sortir de suite si l'on n'a pas coché l'importation des auteurs
+                    if (! _request("auteurs"))
+                        break;
                     include_spip('action/editer_auteur');
                     foreach ($a as $auteur) {
                         $nom = twp($auteur['wp:author_display_name'][0]);
@@ -98,6 +167,9 @@ function wp_import_import_wordpress()
                         $id_auteur = auteur_inserer();
                         auteur_modifier($id_auteur, $data_auteur);
                         $tab_auteur[$auteur['wp:author_login'][0]] = $id_auteur;
+                        spip_log("Auteur $id_auteur créé ( ". $auteur['wp:author_login'][0] ." ) ", "wp_import" . _LOG_INFO_IMPORTANTE);
+                        $cpt_auteurs++;
+
                     }
                     break;
                 case "wp:term":
@@ -107,18 +179,28 @@ function wp_import_import_wordpress()
                 // Importation des rubriques
 
                 case "wp:category":
+                    //sortir de suite si l'on n'a pas coché l'importation des rubriques
+                    if (! _request("rubriques"))
+                        break;
                     include_spip('action/editer_rubrique');
+                    $id_parent_rubrique=_request("id_parent");
 
                     foreach ($a as &$cat) {
-                        $data_rub = array('titre' => twp($cat['wp:cat_name'][0]));
-                        $id_rub = rubrique_inserer(0);
+                        $data_rub = array(
+                            'titre' => twp($cat['wp:cat_name'][0]),
+                            'id_parent' => "$id_parent_rubrique" ) ;
+                        $id_rub = rubrique_inserer($id_parent_rubrique);
+                        spip_log("Création rubrique $id_rub (id_parent : $id_parent_rubrique) ", "wp_import" . _LOG_INFO_IMPORTANTE);
                         $cat["id"] = $id_rub;
                         $tab_cat[$cat['wp:category_nicename'][0]] = $id_rub;
                         rubrique_modifier($id_rub, $data_rub);
-
+                       // var_dump ($tab_cat) ;
+                         // die ('plouf') ;
                     }
                     foreach ($a as $cat) {
-                        $id_parent = $tab_cat[twp($cat['wp:category_parent'][0])] + 0;
+                        $id_parent = $tab_cat[twp($cat['wp:category_nicename'][0])] + 0;
+                        spip_log("Modif rubrique parent $id_parent ) ", "wp_import" . _LOG_INFO_IMPORTANTE);
+
                         $data_rub = array('id_parent' => $id_parent);
                         rubrique_modifier($cat["id"], $data_rub);
                     }
@@ -127,12 +209,13 @@ function wp_import_import_wordpress()
                 // Importation des articles et documents
 
                 case "item":
+                    //sortir de suite si l'on n'a pas coché l'importation des articles
+                    if (! _request("documents"))
+                        break;
                     include_spip('action/editer_article');
                     foreach ($a as $item) {
                         switch ($item['wp:post_type'][0]) {
                             case 'attachment':
-
-
                                 $data_document = array(
                                     'titre' => $item['title'][0],
                                     'descriptif' => twp($item['description'][0]),
@@ -159,8 +242,8 @@ function wp_import_import_wordpress()
                                     'mode' => 'document');
 
                                 $ajouter_un_document = charger_fonction('ajouter_un_document', 'action');
-                                $id_document = $ajouter_un_document(0, $file, '', 0, 'document');
-                                document_modifier($id_document, $data_document);
+                                //$id_document = $ajouter_un_document(0, $file, '', 0, 'document');
+                                //document_modifier($id_document, $data_document);
 
                                 $tab_document[basename($item['wp:attachment_url'][0])] = $id_document;
                                 break;
@@ -178,13 +261,18 @@ function wp_import_import_wordpress()
             switch ($type) {
 
                 case "item":
+                    //sortir de suite si l'on n'a pas coché l'importation des articles
+                    if (! _request("articles"))
+                        break;
                     include_spip('action/editer_article');
                     foreach ($a as $item) {
                         switch ($item['wp:post_type'][0]) {
                             case "page":
                             case 'post':
                                 $statut = 'publie';
-                                if ($item['wp:status'][0] == 'publish') $statut = 'publie';
+                            if ($item['wp:status'][0] == 'publish') $statut = 'publie';
+                            if ($item['wp:post_id'][0] != 2882)
+                                break;
 
                                 $data_article = array(
                                     'titre' => $item['title'][0],
@@ -192,6 +280,7 @@ function wp_import_import_wordpress()
                                     'statut' => 'publie',
                                     'texte' => html_to_spip(twp($item['content:encoded'][0]), $tab_document),
                                     'date_modif' => $item['post_date'][0]);
+
                                 $categorys = "";
                                 $categorys = preg_array_key_exists('/^category/', $item);
                                 if (is_array($categorys)) {
@@ -204,6 +293,7 @@ function wp_import_import_wordpress()
                                     $tab_id_rub = array(-1);
                                     $data_article['page'] = $item['wp:post_name'][0];
                                 }
+
                                 $id_article = article_inserer(array_shift($tab_id_rub));
                                 article_modifier($id_article, $data_article);
                                 if (!empty($tab_id_rub)) {
@@ -212,10 +302,14 @@ function wp_import_import_wordpress()
                                 }
                                 $data_article = array(
                                     'statut' => 'publie',
-                                    'date' => $item['post_date'][0]);
+                                    'date' => $item['wp:post_date'][0]  );
+
                                 sql_updateq('spip_articles', $data_article, $id_article);
 
-                                //lier l'article à un auteur
+                                //Supprimer l'auteur (SPIP) qui a créé l'article
+                                sql_delete('spip_auteurs_liens', 'id_objet='.$id_article.' AND objet="article"');
+
+                                //puis lier l'article SPIP à l'auteur WORDPRESS
                                 auteur_associer($tab_auteur[$item['dc:creator'][0] . ''], array("article" => $id_article));
                                 break;
 
@@ -310,38 +404,5 @@ function donne_nom_cat($cat)
     return $matches[1];
 }
 
-
-function mes_saisies_import_wordpress()
-{
-
-    $options_fichier = array(
-        'nom' => 'document_xml',
-        'label' => 'Fichier',
-        'explication' => 'Fichier xml exporté depuis Wordpress',
-    );
-
-
-    $mes_saisies = array(
-        //Champ titre
-        array(
-            'saisie' => 'input',
-            'options' => array(
-                'nom' => 'id_rubrique',
-                'label' => 'Rubrique',
-                'obligatoire' => 'oui',
-                'defaut' => 0
-            )),
-
-        //Champ document
-        array(
-            'saisie' => 'fichier_wp',
-            'options' => $options_fichier
-        ),
-
-
-    );
-
-    return $mes_saisies;
-}
 
 ?>
--- wp_import/trunk/lang/wp_import_fr.php
+++ wp_import/trunk/lang/wp_import_fr.php
@@ -11,8 +11,31 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'etape2_texte' => "Copier l'ensemble le répertoire \"wp-content/uploads\" de votre wordpress dans le répertoire tmp/wordpress de votre site.Cette étape est facultative si votre serveur possède un accès à internet et que le nombre de vos documents à importer est réduit.",
 	'etape3_titre' => "Lancer l'importation",
 	'etape3_texte' => " ",
-	
-	'migration' => "Migration depuis Wordpress",
+
+    "document_xml_explication" => "Nom du fichier xml exporté depuis Wordpress à placer dans le repertoire tmp de votre site SPIP" ,
+    "document_xml_label" => "fichier xml" ,
+
+    "id_rubrique_label" => "N° rubrique",
+    "id_rubrique_explication" => "Le Numéro de la rubrique qui recevra les articles créés ",
+
+    "auteurs_label" => "Auteurs ? ",
+    "auteurs_explication" => "Importer les auteurs ",
+
+    "rubriques_label" => "Rubriques ?",
+    "rubriques_explication" => "Importer les rubriques ",
+
+    "articles_label" => "Articles ? ",
+    "articles_explication" => "Importer les articles",
+
+    "_label" => "",
+    "_explication" => "",
+
+
+    "menage_label" => "Ménage",
+    "menage_explication" => "Avant d'importer faire un ménage par le vide : (supprimer les rubriques, documents, articles et auteurs)",
+
+
+    'migration' => "Migration depuis Wordpress",
 	'migration2' => "Migration de Wordpress vers SPIP",
 	'mode_d_emploi' => "Mode d'emploi",
 	
