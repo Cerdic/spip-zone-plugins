e71124cff85d8c2a9832054e6048979ba275a1fa
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-09-10 10:02:02 +0000

    inutile de passer par le nom long du livre dans le cas de lire_la_bible, en plus on n'avait pas tous mis
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@84548 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spip-bible/trunk/traduction/lire.php
+++ spip-bible/trunk/traduction/lire.php
@@ -27,13 +27,14 @@ function lire_petit_livre($livre,$chapitre_debut,$verset_debut,$chapitre_fin,$ve
 
 }
 function recuperer_passage_lire($livre,$chapitre_debut,$verset_debut,$chapitre_fin,$verset_fin,$lire,$lang){
-	$param_cache = array(	'version'=>2,
+	$param_cache = array(	'version'=>3,
 				'livre'=>$livre,
 				'chapitre_debut'=>$chapitre_debut,
 				'verset_debut'=>$verset_debut,
 				'chapitre_fin'=>$chapitre_fin,
 				'verset_fin'=>$verset_fin,
 				'lire'=>$lire);
+	
 	//Vérifions qu'on a pas en cache
 	if (_NO_CACHE == 0){
 		include_spip('inc/bible_cache');
@@ -47,10 +48,7 @@ function recuperer_passage_lire($livre,$chapitre_debut,$verset_debut,$chapitre_f
 	
 	list($chapitre_debut,$verset_debut,$chapitre_fin,$verset_fin,$petit) = lire_petit_livre($livre,$chapitre_debut,$verset_debut,$chapitre_fin,$verset_fin,$lang);
 	
-	//determination de lu livre
-	
-	$tableau = bible_tableau('lire_la_bible');
-	$livre =  $tableau[$livre];
+
 	$tableau_resulat = array();
 	
 		//recuperation du passage
