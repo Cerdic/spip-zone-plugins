234fc7f5b56f96406dcc53dcefd24f43a365bacd
marcimat@rezo.net <marcimat@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-09-10 23:18:30 +0000

    Proposition de modification du fonctionnement de api_docrestreint (qui décide de l'affichage ou non d'un document pour un visiteur).
    Le code est réécrit pour limiter l'indentation (if ... return)
    Factorisation de quelques parties appelées fréquemment.
    
    On place dans un objet toutes les infos connues du document à analyser ; 
    Cet objet traverse un nouveau pipeline 'accesrestreint_pre_vue_document'; 
    si le 'status' dans l'objet est renseigné après ce passage, 
    il est utilisé pour ce document. 
    
    Sinon, c'est le mécanisme précédent qui s'applique,
    à savoir : si ce document n'est pas présent dans spip_documents => 404, et s'il est
    présent, il faut l'autorisation de le voir, par clé d'action ou par autoriser().
    
    Un autre pipeline est aussi créé (peut être inutile ?) nommé 'accesrestreint_repertoires_toujours_autorises'
    permettant de définir des sous répertoires de IMG dont on autorise systématiquement l'accès. 
    
    Ces deux pipelines (n'hésitez pas à trouver de meilleurs nommages) devraient permettre 
    de gérer plus finement l'accès restreint aux documents qui sont hors de la médiathèque.
    Actuellement ils étaient systématiquement refusés, hormis ceux du sous répertoire 'nl' (newsletter).
    
    Cela devrait permettre de gérer des documents IMG/truc/muche.tld qui nécessitent par exemple simplement d'être identifié
    pour être vus. De même, le plugin newsletter pourrait maintenant indiquer que son répertoire est ouvert, soit avec
    le pipeline 'accesrestreint_pre_vue_document', soit avec 'accesrestreint_repertoires_toujours_autorises' si on le garde, 
    évitant ce petit code en dur dans accès restreint.
    
    
    
    
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@84562 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- acces_restreint/trunk/action/api_docrestreint.php
+++ acces_restreint/trunk/action/api_docrestreint.php
@@ -24,6 +24,9 @@ include_spip('inc/headers');
  * URLs de la forme :
  * docrestreint.api/id/cle/file
  *
+ * @pipeline_appel accesrestreint_repertoires_toujours_autorises
+ * @pipeline_appel accesrestreint_pre_vue_document
+ * 
  * @param null $arg
  */
 function action_api_docrestreint_dist($arg=null) {
@@ -43,137 +46,97 @@ function action_api_docrestreint_dist($arg=null) {
 		return;
 	}
 
+
 	// Séparer les 3 arguments
 	// file ($f) exige pour eviter le scan id_document par id_document
 	$id_document = intval(array_shift($arg));
 	$cle         = array_shift($arg);
 	$f           = implode("/", $arg);
 
-	/**
-	 * URL de test de fonctionnement
-	 * @see accesrestreint_gerer_htaccess()
-	 */
-	if ($id_document==0 AND $cle==1 AND $f=="test/.test") {
+	// Objet reprenant toutes les infos connues
+	$Document = new Accesrestreint_document($f, $id_document, $cle);
+
+	// Simple test de fonctionnement ?
+	if ($Document->est_un_test()) {
 		echo "OK";
 		return;
 	}
 
-	include_spip('inc/documents');
-
-	$file = get_spip_doc($f);
-	spip_log($file, 'dbg');
-
-	// securite : on refuse tout ../ ou url absolue
-	if ((strpos($f, '../') !== false) OR (preg_match(',^\w+://,', $f))) {
-		accesrestreint_afficher_erreur_document(403);
+	// Insécurisé ( chemin ../ ou absolu ) : 403
+	if ($Document->est_insecurise()) {
+		$Document->status = 403;
+		accesrestreint_afficher_document_selon_status($Document);
 		return;
 	}
 
-	// inexistant ou illisible : 404
-	if (!file_exists($file) OR !is_readable($file)) {
-		accesrestreint_afficher_erreur_document(404);
+	// inexistant ou non lisible : 404
+	if (!$Document->est_existant() OR !$Document->est_lisible()) {
+		$Document->status = 404;
+		accesrestreint_afficher_document_selon_status($Document);
 		return;
 	}
 
-	$status = $doc = false;
-	$dossiers_a_exclure = array('nl');
+	/**
+	 * Liste les sous répertoires de IMG qui donnent accès aux fichiers
+	 * systématiquement (sans autorisations particulières).
+	**/
+	$repertoires_autorises = pipeline('accesrestreint_repertoires_toujours_autorises', array('nl'));
 
-	// Si c'est dans un sous-dossier explicitement utilisé pour autre chose que les documents
-	// (exemple : les newsletters)
-	// et bien on ne teste pas l'accès
-	if (preg_match('%^(' . join('|', $dossiers_a_exclure) . ')/%', $f)){
-		$status = 200;
+	if ($Document->est_dans_repertoire($repertoires_autorises)) {
+		$Document->statut = 200;
+		accesrestreint_afficher_document_selon_status($Document);
+		return;
 	}
-	else {
-		$where = "documents.fichier=".sql_quote(set_spip_doc($file))
-		. ($id_document ? " AND documents.id_document=".intval($id_document): '');
-		spip_log($where,'dbg');
 
-		$doc = sql_fetsel("documents.id_document, documents.titre, documents.fichier, types.mime_type, types.inclus, documents.extension", "spip_documents AS documents LEFT JOIN spip_types_documents AS types ON documents.extension=types.extension",$where);
-		spip_log($doc,'dbg');
-		if (!$doc) {
-			$status = 404;
-		}
-		else {
+	/**
+	 * Propose à des plugins de modifier le Document
+	 *
+	 * Particulièrement pour donner le status http qui convient pour ce document.
+	 *
+	 * Si ce 'status' est renseigné par un plugin : on affiche le document (ou l'erreur) avec le statut trouvé
+	 * 
+	 * Sinon, il sera calculé automatiquement ensuite :
+	 * 
+	 * - si le document ne fait pas partie de spip_documents : 404
+	 * - sinon, test d'autorisation de voir ce document (par clé d'action si indiquée, sinon par l'api autoriser)
+	**/
+	$Document = pipeline('accesrestreint_pre_vue_document', $Document);
 
-			// ETag pour gerer le status 304
-			$ETag = md5($file . ': '. filemtime($file));
-			if (isset($_SERVER['HTTP_IF_NONE_MATCH'])
-			  AND $_SERVER['HTTP_IF_NONE_MATCH'] == $ETag) {
-				http_status(304); // Not modified
-				exit;
-			}
-			else {
-				header('ETag: '.$ETag);
-			}
+	if ($Document->status) {
+		accesrestreint_afficher_document_selon_status($Document);
+		return;
+	}
 
-			//
-			// Verifier les droits de lecture du document
+	// Un document sans 'status' doit être analysé
+	// Est-ce un document SPIP ? Non = 404
+	$doc = $Document->get_spip_document();
 
-			// en controlant la cle passee en argument si elle est dispo
-			// (perf issue : toutes les urls ont en principe cette cle fournie dans la page au moment du calcul de la page)
-			if ($cle){
-				include_spip('inc/securiser_action');
-				if (!verifier_cle_action($doc['id_document'].','.$f, $cle)) {
-					spip_log("acces interdit $cle erronee");
-					$status = 403;
-				}
-			}
-			// en verifiant le droit explicitement sinon, plus lent !
-			else {
-				if (!function_exists("autoriser"))
-					include_spip("inc/autoriser");
-				if (!autoriser('voir', 'document', $doc['id_document'])) {
-					$status = 403;
-					spip_log("acces interdit $cle erronee");
-				}
-			}
-		}
+	if (!$doc) {
+		$Document->status = 404;
+		accesrestreint_afficher_document_selon_status($Document);
+		return;
 	}
 
 
-	switch($status) {
+	// ETag pour gerer le status 304
+	$ETag = $Document->get_Etag();
 
-	case 403:
-		accesrestreint_afficher_erreur_document(403);
-		break;
-
-	case 404:
-		accesrestreint_afficher_erreur_document(404);
-		break;
-
-	default:
-		header("Content-Type: ". $doc['mime_type']);
-		// pour les images ne pas passer en attachment
-		// sinon, lorsqu'on pointe directement sur leur adresse,
-		// le navigateur les downloade au lieu de les afficher
-
-		if ($doc['inclus']=='non') {
-
-			$f = basename($file);
-			// ce content-type est necessaire pour eviter des corruptions de zip dans ie6
-			header('Content-Type: application/octet-stream');
-
-			header("Content-Disposition: attachment; filename=\"$f\";");
-			header("Content-Transfer-Encoding: binary");
-
-			// fix for IE catching or PHP bug issue
-			header("Pragma: public");
-			header("Expires: 0"); // set expiration time
-			header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
-
-		}
-		else {
-			header("Expires: 3600"); // set expiration time
-		}
-
-		if ($cl = filesize($file))
-			header("Content-Length: ". $cl);
-
-		readfile($file);
-		break;
+	if (isset($_SERVER['HTTP_IF_NONE_MATCH'])
+	  AND $_SERVER['HTTP_IF_NONE_MATCH'] == $ETag) {
+		$Document->status = 304; // Not modified
+		accesrestreint_afficher_document_selon_status($Document);
+		exit;
 	}
 
+	header('ETag: ' . $ETag);
+
+	// Verifier les droits de lecture du document
+	if (!$Document->est_autorise()) {
+		$Document->status = 403;
+	}
+
+	// envoyer le document
+	accesrestreint_afficher_document_selon_status($Document);
 }
 
 /**
@@ -195,6 +158,31 @@ function accesrestreint_vider_buffers() {
 }
 
 /**
+ * Envoie le document au navigateur, en fonction de son status http
+ *
+ * @uses accesrestreint_afficher_erreur_document()
+ * @uses accesrestreint_afficher_document()
+ *
+ * @param Accesrestreint_document $Document
+ * @return void
+**/
+function accesrestreint_afficher_document_selon_status(Accesrestreint_document $Document) {
+	switch ($Document->status) {
+
+		case 304:
+		case 403:
+		case 404:
+			accesrestreint_afficher_erreur_document($Document->status);
+			break;
+
+		default:
+			accesrestreint_afficher_document($Document);
+			break;
+	}
+}
+
+
+/**
  * Affiche une page indiquant un document introuvable ou interdit
  *
  * @param string $status
@@ -205,6 +193,11 @@ function accesrestreint_afficher_erreur_document($status = 404) {
 
 	switch ($status)
 	{
+		case 304:
+			// not modified : sortir de suite !
+			http_status(304);
+			exit;
+
 		case 403:
 			include_spip('inc/minipres');
 			echo minipres("","","",true);
@@ -216,4 +209,276 @@ function accesrestreint_afficher_erreur_document($status = 404) {
 			echo minipres(_T('erreur') . ' 404', _T('medias:info_document_indisponible'), "", true);
 			break;
 	}
+}
+
+/**
+ * Envoie le document au navigateur
+ *
+ * Soit en document attaché, soit en direct
+ *
+ * @param Accesrestreint_document $Document
+ * @return void
+**/
+function accesrestreint_afficher_document(Accesrestreint_document $Document) {
+
+	$chemin_fichier = $Document->get_chemin_complet_fichier();
+
+	// document décrit dans la table spip_documents ?
+	if ($doc = $Document->get_spip_document()) {
+		if ($doc['mime_type']) {
+			header("Content-Type: " . $doc['mime_type']);
+		}
+
+		// pour les images ne pas passer en attachment
+		// sinon, lorsqu'on pointe directement sur leur adresse,
+		// le navigateur les downloade au lieu de les afficher
+		if ($doc['inclus'] == 'non') {
+			$f = basename($chemin_fichier);
+			// ce content-type est necessaire pour eviter des corruptions de zip dans ie6
+			header('Content-Type: application/octet-stream');
+
+			header("Content-Disposition: attachment; filename=\"$f\";");
+			header("Content-Transfer-Encoding: binary");
+
+			// fix for IE catching or PHP bug issue
+			header("Pragma: public");
+			header("Expires: 0"); // set expiration time
+			header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+		} else {
+			header("Expires: 3600"); // set expiration time
+		}
+	} else {
+		header("Expires: 3600"); // set expiration time
+	}
+
+
+	if ($size = filesize($chemin_fichier)) {
+		header("Content-Length: ". $size);
+	}
+
+	readfile($chemin_fichier);
+}
+
+
+/**
+ * Décrit un document qu'un utilisateur cherche à afficher.
+ *
+ * En aura t'il le droit ?
+**/
+class Accesrestreint_document {
+
+	/**
+	 * Identifiant du document demandé, si connu
+	 *
+	 * 0 si document inconnu.
+	 * 
+	 * @var int
+	**/
+	private $id_document = 0;
+
+	/**
+	 * Clé d'action auteur, si connue
+	 *
+	 * 0 si inconnue
+	 *
+	 * @var int|string 
+	**/
+	private $cle_action = 0;
+
+	/**
+	 * Chemin du fichier demandé, depuis IMG/
+	 *
+	 * @var string 
+	**/
+	private $_fichier = "";
+
+	/**
+	 * Status HTTP à utiliser pour ce document
+	 *
+	 * @var string|int
+	**/
+	public $status = "";
+
+
+	/**
+	 * Constructeur
+	 *
+	 * @param int $_fichier
+	 *     Chemin du fichier demandé, depuis IMG/
+	 * @param int $id_document
+	 *     Identifiant du document, si connu
+	 * @param int|string $cle_action
+	 *     Clé d'action auteur, si connue
+	 * @return 
+	**/
+	public function __construct($_fichier, $id_document = 0, $cle_action = 0) {
+		$this->_fichier = $_fichier;
+		$this->id_document = $id_document;
+		$this->cle_action  = $cle_action;
+	}
+
+	/**
+	 * Test si le document demandé vérifie simplement le fonctionnement correct du .htaccess dans IMG posé par Acces Restreint
+	 * 
+	 * @see accesrestreint_gerer_htaccess()
+	 * 
+	 * return bool
+	 *     True si document de test
+	 */
+	public function est_un_test() {
+		return  $this->id_document == 0
+			AND $this->cle_action == 1
+			AND $this->_fichier == "test/.test";
+	}
+
+
+	/**
+	 * Test si le document demandé n'a pas un chemin d'accès sécurisé
+	 *
+	 * Par exemple avec ../ ou url absolue http://
+	 * 
+	 * @return bool
+	 *     True si le chemin est insécurisé
+	**/
+	public function est_insecurise() {
+		return (strpos($this->_fichier, '../') !== false)
+			OR (preg_match(',^\w+://,', $this->_fichier));
+	}
+
+	/**
+	 * Test si le fichier existe
+	 *
+	 * @return bool True si existe
+	**/
+	public function est_existant() {
+		$chemin_fichier = $this->get_chemin_complet_fichier();
+		return file_exists($chemin_fichier);
+	}
+
+	/**
+	 * Test si le fichier est accessible en lecture
+	 *
+	 * @return bool True si lisible
+	**/
+	public function est_lisible() {
+		$chemin_fichier = $this->get_chemin_complet_fichier();
+		return is_readable($chemin_fichier);
+	}
+
+
+	/**
+	 * Test si un document est dans un sous-répertoire à la racine de IMG
+	 *
+	 * @param string|array $repertoires
+	 *     Nom d'un ou plusieurs répertoires
+	 * @return bool
+	 *     True si le document est contenu dans un de ces répertoires
+	**/
+	public function est_dans_repertoire($repertoires) {
+		if (!$repertoires) {
+			return false;
+		}
+		if (is_string($repertoires)) {
+			$repertoires = array($repertoires);
+		}
+		$repertoires = array_filter($repertoires);
+
+		return preg_match('%^(' . implode('|', $repertoires) . ')/%', $this->_fichier);
+	}
+
+
+	/**
+	 * Test l'autorisation de voir ce document (spip)
+	 *
+	 * @return bool True si on peut voir le document
+	**/
+	public function est_autorise() {
+		$doc = $this->get_spip_document();
+		if (!$doc) {
+			spip_log("acces interdit, document hors de spip_documents");
+			return false;
+		}
+
+		// en controlant la cle passee en argument si elle est dispo
+		// (perf issue : toutes les urls ont en principe cette cle fournie dans la page au moment du calcul de la page)
+		if ($this->cle_action){
+			include_spip('inc/securiser_action');
+			if (!verifier_cle_action($doc['id_document'].','.$this->_fichier, $this->cle_action)) {
+				spip_log("acces interdit $this->cle_action erronee");
+				return false;
+			}
+			return true;
+		}
+		
+		// en verifiant le droit explicitement sinon, plus lent !
+		if (!function_exists("autoriser")) {
+			include_spip("inc/autoriser");
+		}
+
+		if (!autoriser('voir', 'document', $doc['id_document'])) {
+			spip_log("acces interdit, pas autorise a voir le document " . $doc['id_document']);
+			return false;
+		}
+
+		return true;
+	}
+
+
+	/**
+	 * Retourne la description du document dans la table spip_documents (et spip_types_documents)
+	 *
+	 * Seulement si ce fichier est un document dans SPIP
+	 *
+	 * @return array Description du document dans SPIP
+	**/
+	public function get_spip_document() {
+		static $doc = null;
+		if (is_null($doc)) {
+			include_spip('inc/documents');
+			$where = "documents.fichier = ".sql_quote(set_spip_doc($this->get_chemin_complet_fichier()))
+				. ($this->id_document ? " AND documents.id_document=".intval($this->id_document): '');
+			spip_log($where, 'dbg');
+
+			$doc = sql_fetsel(
+				"documents.id_document, documents.titre, documents.fichier, types.mime_type, types.inclus, documents.extension",
+				"spip_documents AS documents LEFT JOIN spip_types_documents AS types ON documents.extension=types.extension",
+				$where
+			);
+
+			spip_log($doc, 'dbg');
+			if (!$doc) {
+				$doc = array();
+			}
+		}
+		return $doc;
+	}
+
+	/**
+	 * Calcule et retourne le chemin complet  du fichier (depuis la racine du site)
+	 *
+	 * @return string
+	**/
+	public function get_chemin_complet_fichier() {
+		static $fichier = null;
+		if (is_null($fichier)) {
+			include_spip('inc/documents');
+			$fichier = get_spip_doc($this->_fichier);
+			spip_log($fichier, 'dbg');
+		}
+		return $fichier;
+	}
+
+	/**
+	 * Calcule et retourne le hash Etag du fichier
+	 *
+	 * @return string
+	**/
+	public function get_ETag() {
+		static $ETag = null;
+		if (is_null($ETag)) {
+			$ETag = md5($this->get_chemin_complet_fichier() . ': '. filemtime($this->get_chemin_complet_fichier()));
+		}
+		return $ETag;
+	}
+
 }
