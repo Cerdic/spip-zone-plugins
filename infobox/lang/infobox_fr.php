7e70ebd199e163a67c2c54edd1bd1ebf0d5775fa
spip.franck@lien-d-amis.net <spip.franck@lien-d-amis.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-09-14 10:22:56 +0000

    Ce n'est plus spip-contrib. mais contrib.spip
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@84597 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- infobox/lang/infobox_fr.php
+++ infobox/lang/infobox_fr.php
@@ -18,7 +18,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	
 	'cfg_titre_plugin'				=> 'Plugin Infobox',
 	'cfg_titre_descriptif'			=> 'Infobox - Les squelettes parlent aux r&eacute;dacteurs !',
-	'cfg_texte_descriptif'			=> 'D&egrave;s l\'activation du plugin, les concepteurs de squelettes disposent d\'une nouvelle balise, la balise "#INFOBOX".<p>Cette balise accepte 2 param&egrave;tres : "class" qui indique la pseudo-class contenant le message, et "message" le message proprement dit.</p><p><a href="http://www.spip-contrib.net/Plugin-Infobox?var_mode=preview" class="spip_out">Doc en ligne, forum, FAQ sur SPIP-Contrib</a></p>',
+	'cfg_texte_descriptif'			=> 'D&egrave;s l\'activation du plugin, les concepteurs de squelettes disposent d\'une nouvelle balise, la balise "#INFOBOX".<p>Cette balise accepte 2 param&egrave;tres : "class" qui indique la pseudo-class contenant le message, et "message" le message proprement dit.</p><p><a href="http://contrib.spip.net/Plugin-Infobox-Les-squelettes" class="spip_out">Doc en ligne, forum, FAQ sur SPIP-Contrib</a></p>',
 	'cfg_nom_boite'					=> 'Formulaire de configuration du plugin "Infobox"',
 	'cfg_nom_plugin'				=> 'infobox'
 
