8232c4243e68b69796feeeceeb6f506c80cd6783
brunobergot@gmail.com <brunobergot@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-09-14 16:44:39 +0000

    tiens, un oups :p
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@84606 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- gis/trunk/lang/gis_fr.php
+++ gis/trunk/lang/gis_fr.php
@@ -47,7 +47,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'cfg_lbl_maptype_hybride' => 'Hybride',
 	'cfg_lbl_maptype_relief' => 'Relief',
 	'cfg_lbl_maptype_satellite' => 'Satellite',
-	'cfg_titre_gis' => 'Configuration de GIS'
+	'cfg_titre_gis' => 'Configuration de GIS',
 
 	// E
 	'editer_gis_editer' => 'Modifier ce point',
