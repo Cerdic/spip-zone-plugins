15ab09c01e8404b77d072d6280a5133f55a93231
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-09-16 12:23:36 +0000

    On ajoute les chaines de langue des champs extras de la table spip_projets. Up de z.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@84697 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ rss_commits/trunk/lang/projet_fr.php
@@ -0,0 +1,25 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+    return;
+}
+
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+    // E
+    'explication_versioning_path' => '-',
+    'explication_versioning_rss' => '-',
+    'explication_versioning_trac' => '-',
+    'explication_versioning_type' => '-',
+
+    // L
+    'label_versioning_path' => 'Chemin du dépôt',
+    'label_versioning_rss' => 'RSS des commits du dépôt',
+    'label_versioning_trac' => 'Trac du dépôt',
+    'label_versioning_type' => 'Type de versioning',
+
+);
+
+?>
