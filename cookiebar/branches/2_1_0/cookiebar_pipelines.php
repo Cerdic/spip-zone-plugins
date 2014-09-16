e4081046341700a770f05f7665d3325c71e1de81
erational@erational.org <erational@erational.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-09-16 09:23:46 +0000

    version cookiebar pour SPIP 2.1
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@84692 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ cookiebar/branches/2_1_0/cookiebar_pipelines.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Plugin Cookie bar pour Spip 2.1
+ *
+ */
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+/**
+ * Inserer la CSS de cookie bar
+ *
+ * @param $flux
+ * @return mixed
+ */
+function cookiebar_insert_head_css($flux){
+  $flux .= '<link rel="stylesheet" type="text/css" href="'.find_in_path("css/jquery.cookiebar.css").'" />';
+  return $flux;
+}
+
+/**
+ * Inserer le javascript de cookiebar
+ *
+ * @param $flux
+ * @return mixed
+ */
+function cookiebar_insert_head($flux){
+  //$js_cookiebar = parametre_url(generer_url_public('jquery.cookiebar.js'), 'lang', $lang);
+  $js_cookiebar = produire_fond_statique("jquery.cookiebar.js", array("lang"=>$lang));
+
+  $flux .= 
+           "<script type='text/javascript' src='$js_cookiebar'></script>\n"
+         . "<script type='text/javascript' src='".find_in_path('js/jquery.cookiebar.call.js')."'></script>";
+  
+  return $flux;
+}
+
+?>
