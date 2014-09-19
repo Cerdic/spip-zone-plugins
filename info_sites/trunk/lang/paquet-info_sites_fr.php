d028747d1fe297a2f6534c505074fba3236c7dfb
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-09-19 15:12:13 +0000

    Un oups...
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@84756 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- info_sites/trunk/lang/paquet-info_sites_fr.php
+++ info_sites/trunk/lang/paquet-info_sites_fr.php
@@ -8,10 +8,7 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
     // S
-    'info_sites_description' => 'Dans l’informatique, la info_sites est la surveillance du bon fonctionnement d’un
-    système ou d’une activité. Avec ce plugin vous pourrez tenir à jour une fiche technique de chaque site que vous
-    avez réalisé. De plus si vous utilisez le plugin \"Info SPIP\' sur chacun de ces sites, vous pourrez mettre à
-    jour les informations de votre site automatiquement.',
+    'info_sites_description' => 'Dans l’informatique, la supervision est la surveillance du bon fonctionnement d’un système ou d’une activité. Avec ce plugin, vous pourrez tenir à jour une fiche technique de chaque site que vous avez réalisé. De plus, si vous utilisez le plugin \"Info SPIP\' sur chacun de ces sites, vous pourrez mettre à jour les informations de votre site automatiquement.',
     'info_sites_nom' => 'Info Sites',
     'info_sites_slogan' => 'Garder un oeil sur vos sites',
 );
