d9d7f15e3f39e6979faa3c13b8d4b5ea3b278477
marcimat@rezo.net <marcimat@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-10-01 03:37:47 +0000

    [Salvatore] [source:_plugins_/boussole_spip/trunk/lang/ paquet-bouspip] Export depuis http://trad.spip.net de la langue eo
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@84932 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ boussole_spip/trunk/lang/paquet-bouspip_eo.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-bouspip?lang_cible=eo
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'bouspip_description' => 'Tiu kromprogramo provizas la nomon, la retadreson kal la priskribon de ĉiuj retejoj de la SPIP-Galakcio. Oni devas uzi ĝin per la kromprogramo Orientigilo en servila funkciado por disdoni, tra reta servo, ĝiajn informojn al ĉiuj retejoj kiuj deziras afiŝi la SPIP-orientigilon.',
+	'bouspip_nom' => 'SPIP orientigilo',
+	'bouspip_slogan' => 'La bonaj adresoj de la SPIP-galakcio !'
+);
+
+?>
