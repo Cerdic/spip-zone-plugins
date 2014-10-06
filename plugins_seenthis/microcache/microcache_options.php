880a1d5d4e267005a570aaf07ae6a590d9a12ba1
fil@rezo.net <fil@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-10-06 20:01:12 +0000

    migration reversible vers memoization
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@85026 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- plugins_seenthis/microcache/microcache_options.php
+++ plugins_seenthis/microcache/microcache_options.php
@@ -1,5 +1,9 @@
 <?php
 
+// FALSE pour new style = memoization, TRUE pour old style = fichier dans local/
+if (FALSE) {
+
+
 function supprimer_microcache($id, $fond) {
 	include_spip("inc/microcache");
 	_supprimer_microcache($id, $fond);
@@ -14,5 +18,44 @@ function esi_microcache($id, $fond) {
 	include_spip("inc/microcache");
 	return _esi_microcache($id, $fond);
 }
+
+
+
+} else {
+
+
+
+function microcache_key($id, $fond) {
+	include_spip('memoization_options');
+	if (!function_exists('cache_set')) return false;
+	return $fond.'-'. (is_numeric($id) ? $id : md5($id));
+}
+
+function supprimer_microcache($id, $fond) {
+	if ($key = microcache_key($id, $fond))
+		return cache_del($key);
+}
+
+function microcache($id, $fond, $calcul=false) {
+	$key = microcache_key($id, $fond);
+	if (!$key
+	OR $calcul
+	OR in_array($_GET['var_mode'], array('recalcul', 'debug'))
+	OR is_null($contenu = cache_get($key))) {
+		$contenu = recuperer_fond($fond, array('id'=>$id));
+		if ($key
+		AND $_GET['var_mode'] != 'inclure'
+		AND !$_POST
+		) {
+			cache_set($key, $contenu, $ttl = 7*24*3600);
+		}
+	}
+	return $contenu;
+}
+
+
+
+}
+
 
 ?>
