8c8af387ac1c264c9483af9b2b53217e77c94e04
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-10-08 03:41:02 +0000

    [Salvatore] [source:_plugins_/abonnements/trunk/lang/ abonnement] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@85049 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- abonnements/trunk/lang/abonnement_fr.php
+++ abonnements/trunk/lang/abonnement_fr.php
@@ -1,23 +1,23 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/abonnements/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
 	'ajouter_lien_abonnement' => 'Ajouter cet abonnement',
-	
+
 	// C
 	'champ_date_debut_label' => 'Début',
 	'champ_date_fin_allonger_label' => 'Vous pouvez modifier la date d’échéance',
 	'champ_date_fin_label' => 'Échéance',
-	'champ_id_abonnements_offre_label' => 'Offre d\'abonnement',
+	'champ_id_abonnements_offre_label' => 'Offre d’abonnement',
 	'champ_id_auteur_label' => 'Utilisateur',
-	
+
 	// E
 	'erreur_id_abonnements_offre' => 'Vous devez créer un abonnement pour une offre existante.',
-	
+
 	// I
 	'icone_creer_abonnement' => 'Créer un abonnement',
 	'icone_modifier_abonnement' => 'Modifier cet abonnement',
@@ -34,23 +34,23 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'info_nb_abonnements_inactifs' => '@nb@ abonnements inactifs',
 	'info_numero_abbr' => 'n°',
 	'info_numero_abbr_maj' => 'N°',
-	
+
 	// J
 	'job_desactivation' => 'Désactivation de l’abonnement @id@',
-	
+
 	// N
-	'notification_echeance_sujet_jours' => 'Votre abonnement se termine dans @duree@ jour(s) !',
-	'notification_echeance_sujet_mois' => 'Votre abonnement se termine dans @duree@ mois !',
 	'notification_echeance_corps' => '<p>Bonjour @nom@,</p>
 		<p>Vous recevez ce courriel car vous êtes abonné au site @nom_site_spip@ avec l’offre "@offre@".</p>
-		<p>Votre abonnement arrive à échéance dans : <strong>@echeance@</strong>.<br/>
+		<p>Votre abonnement arrive à échéance dans : <strong>@echeance@</strong>.<br/>
 		Nous vous invitons donc à le renouveler avant son expiration.</p>
 		<p>Merci de votre confiance, et n’hésitez pas à nous contacter pour toute information complémentaire.</p>',
-	
+	'notification_echeance_sujet_jours' => 'Votre abonnement se termine dans @duree@ jour(s) !',
+	'notification_echeance_sujet_mois' => 'Votre abonnement se termine dans @duree@ mois !',
+
 	// R
 	'retirer_lien_abonnement' => 'Retirer cet abonnement',
 	'retirer_tous_liens_abonnements' => 'Retirer tous les abonnements',
-	
+
 	// S
 	'statut_actif' => 'actif',
 	'statut_inactif' => 'désactivé',
@@ -61,10 +61,10 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'texte_creer_associer_abonnement' => 'Créer et associer un abonnement',
 	'titre_abonnement' => 'Abonnement',
 	'titre_abonnements' => 'Abonnements',
-	'titre_abonnements_suivre' => 'Suivre les abonnements',
 	'titre_abonnements_rubrique' => 'Abonnements de la rubrique',
+	'titre_abonnements_suivre' => 'Suivre les abonnements',
 	'titre_langue_abonnement' => 'Langue de cet abonnement',
-	'titre_logo_abonnement' => 'Logo de cet abonnement',
+	'titre_logo_abonnement' => 'Logo de cet abonnement'
 );
 
 ?>
