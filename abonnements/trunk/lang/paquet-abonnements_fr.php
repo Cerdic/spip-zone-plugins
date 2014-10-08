e94389a92ec9bf5b0ce52f596e3f9ccbe3347d2a
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-10-08 03:41:16 +0000

    [Salvatore] [source:_plugins_/abonnements/trunk/lang/ paquet-abonnements] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@85051 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- abonnements/trunk/lang/paquet-abonnements_fr.php
+++ abonnements/trunk/lang/paquet-abonnements_fr.php
@@ -1,18 +1,18 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/abonnements/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
-	'abonnements_description' => 'Ce plugin a pour but de regrouper tout ce qui est commun aux différents types d\'abonnements possibles (à des zones restreintes, à des contenus précis, à une version papier pourquoi pas aussi...).
+	'abonnements_description' => 'Ce plugin a pour but de regrouper tout ce qui est commun aux différents types d’abonnements possibles (à des zones restreintes, à des contenus précis, à une version papier pourquoi pas aussi...).
 
-Il permet de définir les abonnements proposés par le site, et gère les personnes qui y sont abonnés, désactivant l\'abonnement au bout d\'un temps donné.
+Il permet de définir les abonnements proposés par le site, et gère les personnes qui y sont abonnés, désactivant l’abonnement au bout d’un temps donné.
 
-Comme il peut y avoir toute sorte de cas, ce n\'est pas ce plugin qui décide quels droits sont donnés lors d\'un abonnement. C\'est à d\'autres plugins d\'implémenter cela, par exemple pour lier alors automatiquement un utilisateur à une zone restreinte.',
+Comme il peut y avoir toute sorte de cas, ce n’est pas ce plugin qui décide quels droits sont donnés lors d’un abonnement. C’est à d’autres plugins d’implémenter cela, par exemple pour lier alors automatiquement un utilisateur à une zone restreinte.',
 	'abonnements_nom' => 'Abonnements',
-	'abonnements_slogan' => 'Donner des droits pendant un certain temps',
+	'abonnements_slogan' => 'Donner des droits pendant un certain temps'
 );
 
 ?>
