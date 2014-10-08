d4babc3d70f758b20f5872e280cb0dfd27ce0801
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-10-08 03:41:22 +0000

    [Salvatore] [source:_plugins_/abonnements_zones/trunk/lang/ paquet-abozones] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@85052 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- abonnements_zones/trunk/lang/paquet-abozones_fr.php
+++ abonnements_zones/trunk/lang/paquet-abozones_fr.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/abonnements_zones/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
@@ -8,7 +8,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// A
 	'abozones_description' => 'Ce plugin permet de lier des zones restreintes du plugin Accès Restreint à des offres d’abonnement. Tout nouvel abonné à ces offres sera alors automatiquemnt ajouté aux zones, et inversement lors de la désactivation de l’abonnement.',
 	'abozones_nom' => 'Abonnements à des zones restreintes',
-	'abozones_slogan' => 'Lier des offres d’abonnement à des zones restreintes',
+	'abozones_slogan' => 'Lier des offres d’abonnement à des zones restreintes'
 );
 
 ?>
