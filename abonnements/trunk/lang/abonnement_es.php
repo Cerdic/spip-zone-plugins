6b1f37df25e0103d30d17c93acb9448f5be504c4
alfdich@yahoo.fr <alfdich@yahoo.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-10-10 03:40:25 +0000

    [Salvatore] [source:_plugins_/abonnements/trunk/lang/ abonnement] Export depuis http://trad.spip.net de la langue es
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@85082 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ abonnements/trunk/lang/abonnement_es.php
@@ -0,0 +1,44 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/abonnement?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'ajouter_lien_abonnement' => 'Añadir esta inscripción',
+
+	// C
+	'champ_date_debut_label' => 'Principio',
+	'champ_date_fin_allonger_label' => 'Usted puede modificar la fecha de vencimiento',
+	'champ_date_fin_label' => 'Vencimiento',
+	'champ_id_abonnements_offre_label' => 'Oferta de iscripción',
+	'champ_id_auteur_label' => 'Usuario',
+
+	// E
+	'erreur_id_abonnements_offre' => 'Usted debe crear una inscripción para una oferta existente  ',
+
+	// I
+	'icone_creer_abonnement' => 'Crear  una inscripción',
+	'icone_modifier_abonnement' => 'Modificar esta inscripción',
+	'icone_renouveler_abonnement' => 'Renovar esta inscripción',
+	'info_1_abonnement' => 'Una inscripción',
+	'info_1_abonnement_actif' => 'Una inscripción activa',
+	'info_1_abonnement_inactif' => 'Una inscripción inactiva',
+	'info_abonnements_auteur' => 'Las inscripciones de este autor',
+	'info_aucun_abonnement' => 'Ninguna inscripción',
+	'info_aucun_abonnement_actif' => 'Ninguna inscripción activa',
+	'info_aucun_abonnement_inactif' => 'Ninguna inscripción inactiva',
+	'info_nb_abonnements' => '@nb@  inscripciones', # RELIRE
+	'info_nb_abonnements_actifs' => '@nb@  inscripciones activas', # RELIRE
+	'info_nb_abonnements_inactifs' => '@nb@ inscripciones inactivos', # RELIRE
+	'info_numero_abbr' => 'n°',
+	'info_numero_abbr_maj' => 'N°',
+
+	// J
+	'job_desactivation' => 'Desactivación de la inscripción @id@' # RELIRE
+);
+
+?>
