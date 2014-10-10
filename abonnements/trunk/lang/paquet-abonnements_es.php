826e35d3542c4610f4ccae5580c58a5067c9e2e3
alfdich@yahoo.fr <alfdich@yahoo.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-10-10 03:40:36 +0000

    [Salvatore] [source:_plugins_/abonnements/trunk/lang/ paquet-abonnements] Export depuis http://trad.spip.net de la langue es
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@85083 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ abonnements/trunk/lang/paquet-abonnements_es.php
@@ -0,0 +1,21 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-abonnements?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'abonnements_description' => 'Este plugin tiene el objetivo de regrupar todo lo que es común a los diferentes tipos de inscripciones posibles (zonas restringidas, contenidos precisos, o porque no ...  a una versión papel...).
+
+Permite de definir las inscripciones propuestas por el sitio, y gestiona las personas que se han inscrito , desactivando la inscripción al cabo de un cierto tiempo.
+
+Como pueden haber varios casos diferentes, no es este plugin el que define los derechos que son dados cuando se hace una inscripción. Son otros plugins los que van a implementar esto, por ejemplo para vincular un utilizador a una zona restringida.
+',
+	'abonnements_nom' => 'Inscripciones ',
+	'abonnements_slogan' => 'Dar derechos durante un cierto tiempo'
+);
+
+?>
