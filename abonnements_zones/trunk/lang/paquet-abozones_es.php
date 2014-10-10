bdaeb9ae3c071538aff75bc54e60b8cb4a0fc589
alfdich@yahoo.fr <alfdich@yahoo.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-10-10 03:40:46 +0000

    [Salvatore] [source:_plugins_/abonnements_zones/trunk/lang/ paquet-abozones] Export depuis http://trad.spip.net de la langue es
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@85084 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ abonnements_zones/trunk/lang/paquet-abozones_es.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-abozones?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'abozones_description' => 'Este plugin permite de vincular zonas restringidas  del plugin Acceso Restringido (Accès Restreint ) a ofertas de inscripción. Todos los nuevos inscritos  a estas ofertas serán entonces automaticamente añadidos a las zonas e inversamente cuando se desactive la inscripción.',
+	'abozones_nom' => 'Inscripciones a zonas restringidas',
+	'abozones_slogan' => 'Vincular zonas de ofertas de inscripción a zonas restringidas.'
+);
+
+?>
