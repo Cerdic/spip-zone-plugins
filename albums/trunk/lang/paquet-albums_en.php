be63c62237b6666a0eb932aca06c7be174fd10e4
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-10-15 23:39:14 +0000

    [Salvatore] [source:_plugins_/albums/trunk/lang/ paquet-albums] Export depuis http://trad.spip.net de la langue en
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@85266 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- albums/trunk/lang/paquet-albums_en.php
+++ albums/trunk/lang/paquet-albums_en.php
@@ -11,14 +11,14 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'albums_description' => 'This plugin extends the documents management by adding the possibility to create groups of documents.
 Albums are editorial objects which act as containers for series of documents.
 They can be autonomous or associated to other objects.
-In practice, you have the choice when adding documents to an object : they can be added individually, or grouped inside albums.
-<br>2 models « album » are available :
+In practice, you have the choice when adding documents to an object: they can be added individually, or grouped inside albums.
+<br>2 «album» models are available:
 - The basic model is to be used for galleries of images
-- A variation displays documents as a list.
-<br>A form allow you to customize and insert &lt;album&gt; tags inside texts.
-To make it available, you need to install 2 plugins : «YAML» and «Saisies», which are not mandatory dependencies.', # RELIRE
+- A version displays documents as a list.
+<br>A form allows you to customize and insert &lt;album&gt; tags inside texts.
+To make it available, you should install 2 plugins: «YAML» and «Saisies», which are no mandatory dependencies.',
 	'albums_nom' => 'Albums',
-	'albums_slogan' => 'Manage groups of documents as albums' # RELIRE
+	'albums_slogan' => 'Manage groups of documents as albums'
 );
 
 ?>
