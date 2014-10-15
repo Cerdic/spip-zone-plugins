df4d53b10a97c269cdeb9201b306b7180ecc72c5
hanjo@allez-allier.com <hanjo@allez-allier.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-10-15 23:39:23 +0000

    [Salvatore] [source:_plugins_/albums/trunk/lang/ paquet-albums] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@85267 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- albums/trunk/lang/paquet-albums_nl.php
+++ albums/trunk/lang/paquet-albums_nl.php
@@ -10,9 +10,15 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// A
 	'albums_description' => 'Deze plugin voegt een nieuw ’album’ object toe dat kan worden gekoppeld aan andere objecten.
 		Albums zijn containers voor documenten: je kunt er documenten mee groeperen.
-		Het aan een tekst toevoegen van een serie documenten wordt erdoor vereenvoudigd.', # MODIF
+		Ze kunnen zelfstandig worden gebruikt of aan andere objecten worden gekoppeld.
+		In de praktijk betekent het dat je op het moment van toevoegen van documenten de keuze hebt: je kunt ze individueel of als album toevoegen.
+		<br>2 «album» modellen zijn beschikbaar:
+		- het basismodel voor een galerij van afbeeldingen.
+		- een variant in lijstvorm voor allerlei soorten documenten.
+		<br>Een formulier maakt aanpassingen mogelijk en je kunt er de &lt;album&gt; tags mee in de tekst invoegen.
+		Voor een optimaal gebruik zijn de plugins «YAML» et «Saisies» vereist, maar dit is niet verplicht.',
 	'albums_nom' => 'Albums',
-	'albums_slogan' => 'Groepeer documenten als een album' # MODIF
+	'albums_slogan' => 'Groepeer documenten in de vorm van een album'
 );
 
 ?>
