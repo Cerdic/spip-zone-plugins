47cf61adf1356e02144dd5494c61fde7e442b88a
telopa@gmail.com <telopa@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-10-16 03:45:50 +0000

    [Salvatore] [source:_plugins_/prix/lang/ prix] Export depuis http://trad.spip.net de la langue pt_br
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@85277 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ prix/lang/prix_pt_br.php
@@ -0,0 +1,22 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/prix?lang_cible=pt_br
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// L
+	'label_prix_ht' => 'Preço HT',
+	'label_prix_ttc' => 'Preço TTC',
+	'label_taxes' => 'Taxas',
+	'label_total_ht' => 'Total HT',
+	'label_total_ttc' => 'Total TTC',
+
+	// P
+	'prix_ht' => '@prix@ HT',
+	'prix_ttc' => '@prix@ TTC'
+);
+
+?>
