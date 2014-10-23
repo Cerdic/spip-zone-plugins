96b285c877fbd804629e06aa33c88b37c3889954
gilles.vincent@gmail.com <gilles.vincent@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-10-23 00:59:35 +0000

    Indentations
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@85468 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- declinaisons/trunk/declinaisons_pipelines.php
+++ declinaisons/trunk/declinaisons_pipelines.php
@@ -9,45 +9,45 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 
 
 function declinaisons_recuperer_fond($flux){
-    $fond=$flux['args']['fond'];
-    $contexte=$flux['args']['contexte'];
+	$fond=$flux['args']['fond'];
+	$contexte=$flux['args']['contexte'];
 
-    // inclure le champ déclinaison
-    if ($fond == 'formulaires/prix'){
-        include_spip('inc/config');
-        $afficher_prix =recuperer_fond('formulaires/inc-prix_affichage',$contexte);
-        $declinaison_champs=recuperer_fond('formulaires/inc-prix_champ',$contexte);
-        
-        $patterns = array('/<!--fini champs!-->/','/<div class="liste prix">(.*?)<\/div>/ims');
-        $replacements = array('<!--fini champs!-->'.$declinaison_champs,$afficher_prix);                        
-       $rep= preg_replace($patterns,$replacements,$flux['data']['texte'],1);
-        
-        $flux['data']['texte'] = $rep;
-        }
-        
-    
-    return $flux;
+	// inclure le champ déclinaison
+	if ($fond == 'formulaires/prix'){
+		include_spip('inc/config');
+		$afficher_prix =recuperer_fond('formulaires/inc-prix_affichage',$contexte);
+		$declinaison_champs=recuperer_fond('formulaires/inc-prix_champ',$contexte);
+		
+		$patterns = array('/<!--fini champs!-->/','/<div class="liste prix">(.*?)<\/div>/ims');
+		$replacements = array('<!--fini champs!-->'.$declinaison_champs,$afficher_prix);                        
+		$rep= preg_replace($patterns,$replacements,$flux['data']['texte'],1);
+		
+		$flux['data']['texte'] = $rep;
+	}
+		
+	
+	return $flux;
 }
 
 function declinaisons_formulaire_charger($flux){
- $form=$flux['args']['form'];
- 
- // cré un contact si pas encore existant
- if($form == 'prix'){
-    $flux['data']['_hidden'].='<input type="hidden" name="objet_titre" value="declinaison">';      
-    }
-     return($flux);
+	$form=$flux['args']['form'];
+
+	// cré un contact si pas encore existant
+	if($form == 'prix'){
+		$flux['data']['_hidden'].='<input type="hidden" name="objet_titre" value="declinaison">';      
+	}
+	return($flux);
 }
 
 // declare l'object pour le Plugin shop https://github.com/abelass/shop
 function declinaisons_shop_objets($flux){
 	
 	$flux['data']['declinaisons']=array(
-            'action'=>'declinaisons',
-            'nom_action'=>_T('declinaison:titre_declinaisons'),
-            'icone'=>'declinaisons-16.png',
-            );
+			'action'=>'declinaisons',
+			'nom_action'=>_T('declinaison:titre_declinaisons'),
+			'icone'=>'declinaisons-16.png',
+			);
 
-    return $flux;
+	return $flux;
 }
 ?>
