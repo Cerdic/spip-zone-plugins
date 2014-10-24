ded29914b335e638ee6500b1f40a2d984d8fa264
gilles.vincent@gmail.com <gilles.vincent@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-10-24 14:08:08 +0000

    typo de langue
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@85533 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- abonnements_zones/trunk/lang/paquet-abozones_fr.php
+++ abonnements_zones/trunk/lang/paquet-abozones_fr.php
@@ -6,7 +6,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
-	'abozones_description' => 'Ce plugin permet de lier des zones restreintes du plugin Accès Restreint à des offres d’abonnement. Tout nouvel abonné à ces offres sera alors automatiquemnt ajouté aux zones, et inversement lors de la désactivation de l’abonnement.',
+	'abozones_description' => 'Ce plugin permet de lier des zones restreintes du plugin Accès Restreint à des offres d’abonnement. Tout nouvel abonné à ces offres sera alors automatiquement ajouté aux zones, et inversement lors de la désactivation de l’abonnement.',
 	'abozones_nom' => 'Abonnements à des zones restreintes',
 	'abozones_slogan' => 'Lier des offres d’abonnement à des zones restreintes'
 );
