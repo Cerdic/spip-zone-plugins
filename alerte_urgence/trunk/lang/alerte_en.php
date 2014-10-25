3758f8f3c4bb0f4e98a8603bab83f903c74ffde8
gilles.vincent@gmail.com <gilles.vincent@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-10-25 03:43:36 +0000

    [Salvatore] [source:_plugins_/alerte_urgence/trunk/lang/ alerte] Export depuis http://trad.spip.net de la langue en
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@85564 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ alerte_urgence/trunk/lang/alerte_en.php
@@ -0,0 +1,17 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/alerte?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'configurer_texte_label' => 'Emergency alert to disseminate:',
+	'configurer_titre' => 'Configure the emergency alert',
+	'configurer_utilisateurs_explication' => 'By default, only complete administrators can change the alert. Here you can select additional users.',
+	'configurer_utilisateurs_label' => 'Additional authorized users'
+);
+
+?>
