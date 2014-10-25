ea713bb6b1603409a5e2991caec80b4350cd28ce
gilles.vincent@gmail.com <gilles.vincent@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-10-25 03:43:45 +0000

    [Salvatore] [source:_plugins_/bonne_annee/lang/ paquet-bonne_annee] Export depuis http://trad.spip.net de la langue en
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@85565 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ bonne_annee/lang/paquet-bonne_annee_en.php
@@ -0,0 +1,17 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-bonne_annee?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'bonne_annee_description' => 'Displays every year during the month of January, a message wishing a happy new year.
+Add <code>#INCLURE{fond=bonne_annee}</code> in your pages.',
+	'bonne_annee_nom' => 'Happy new year',
+	'bonne_annee_slogan' => 'Wish a happy new year on your site'
+);
+
+?>
