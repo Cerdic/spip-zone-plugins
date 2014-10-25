3d09381305fe411cf24d1287ddbf6eefea5203ca
gilles.vincent@gmail.com <gilles.vincent@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-10-25 03:46:21 +0000

    [Salvatore] [source:_plugins_/informer_premiere_connexion/trunk/lang/ informerpremiereconnexion] Export depuis http://trad.spip.net de la langue en
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@85584 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ informer_premiere_connexion/trunk/lang/informerpremiereconnexion_en.php
@@ -0,0 +1,18 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/informerpremiereconnexion?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'configuration_texte_label' => 'Message to display to the user who has confirmed their registration',
+	'configuration_titre' => 'Configure the first connection message',
+
+	// T
+	'texte_defaut' => 'Your user account has been created!'
+);
+
+?>
