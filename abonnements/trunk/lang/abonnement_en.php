fb02317a86b3ffb8acadbb4577a4c608c1f3e041
gilles.vincent@gmail.com <gilles.vincent@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-10-25 03:43:15 +0000

    [Salvatore] [source:_plugins_/abonnements/trunk/lang/ abonnement] Export depuis http://trad.spip.net de la langue en
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@85561 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ abonnements/trunk/lang/abonnement_en.php
@@ -0,0 +1,72 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/abonnement?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'ajouter_lien_abonnement' => 'Add a subscription',
+
+	// C
+	'champ_date_debut_label' => 'Start',
+	'champ_date_fin_allonger_label' => 'You can change the due date',
+	'champ_date_fin_label' => 'Due date',
+	'champ_id_abonnements_offre_label' => 'Subscription offer',
+	'champ_id_auteur_label' => 'User',
+
+	// E
+	'erreur_id_abonnements_offre' => 'You must create a subscription for an existing offer.',
+
+	// I
+	'icone_creer_abonnement' => 'Create a subscription',
+	'icone_modifier_abonnement' => 'Edit this subscription',
+	'icone_renouveler_abonnement' => 'Renew this subscription',
+	'info_1_abonnement' => 'A subscription',
+	'info_1_abonnement_actif' => 'An active subscription',
+	'info_1_abonnement_inactif' => 'An inactive subscription',
+	'info_abonnements_auteur' => 'Subscriptions of this user',
+	'info_aucun_abonnement' => 'No subscription',
+	'info_aucun_abonnement_actif' => 'No active subscription',
+	'info_aucun_abonnement_inactif' => 'No inactive subscription',
+	'info_nb_abonnements' => '@nb@ subscriptions',
+	'info_nb_abonnements_actifs' => '@nb@ active subscriptions',
+	'info_nb_abonnements_inactifs' => '@nb@ inactive subscriptions',
+	'info_numero_abbr' => '#',
+	'info_numero_abbr_maj' => '#',
+
+	// J
+	'job_desactivation' => 'Deactivation of the subscription @id@',
+
+	// N
+	'notification_echeance_corps' => '<p>Bonjour @nom@,</p>
+		<p>You are receiving this email because you are subscribed to the site @nom_site_spip@ with the offer "@offre@".</p>
+		<p>Your subscription expires in: <strong>@echeance@</strong>.<br/>
+		We invite you to renew it before it expires.</p>
+		<p>Thank you for your trust, and do not hesitate to contact us for any further information.</p>',
+	'notification_echeance_sujet_jours' => 'Your subscription ends in @duree@ day(s) !',
+	'notification_echeance_sujet_mois' => 'Your subscription ends in @duree@ month(s)!',
+
+	// R
+	'retirer_lien_abonnement' => 'Remove this subscription',
+	'retirer_tous_liens_abonnements' => 'Remove all subscriptions',
+
+	// S
+	'statut_actif' => 'active',
+	'statut_inactif' => 'disabled',
+
+	// T
+	'texte_ajouter_abonnement' => 'Add a subscription',
+	'texte_changer_statut_abonnement' => 'This subscription is:',
+	'texte_creer_associer_abonnement' => 'Create and assign a subscription',
+	'titre_abonnement' => 'Subscription',
+	'titre_abonnements' => 'Subscriptions',
+	'titre_abonnements_rubrique' => 'Subscriptions of the section',
+	'titre_abonnements_suivre' => 'Follow this subscription',
+	'titre_langue_abonnement' => 'Language of this subscription',
+	'titre_logo_abonnement' => 'Logo of this subscription'
+);
+
+?>
