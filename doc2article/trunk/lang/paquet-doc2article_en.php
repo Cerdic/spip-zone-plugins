6122f9e314934cc03a5f212743a984689db13784
gilles.vincent@gmail.com <gilles.vincent@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-10-25 03:45:53 +0000

    [Salvatore] [source:_plugins_/doc2article/trunk/lang/ paquet-doc2article] Export depuis http://trad.spip.net de la langue en
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@85581 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ doc2article/trunk/lang/paquet-doc2article_en.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-doc2article?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// D
+	'doc2article_description' => 'Import all the files from tmp/upload and automatically create an article per file.',
+	'doc2article_slogan' => 'Import all the files from tmp/upload and automatically create an article per file.'
+);
+
+?>
