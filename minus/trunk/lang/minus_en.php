6f4c694a4c1e2552f01b45f50393e90db4d3677f
gilles.vincent@gmail.com <gilles.vincent@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-10-25 03:46:56 +0000

    [Salvatore] [source:_plugins_/minus/trunk/lang/ minus] Export depuis http://trad.spip.net de la langue en
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@85588 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ minus/trunk/lang/minus_en.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/minus?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// T
+	'trop_majuscule' => 'Your title contains a lot of capital. Are you sure you do not want to put them in lower case? On the Internet, uppercase give the impression that you shout: use them sparingly.'
+);
+
+?>
