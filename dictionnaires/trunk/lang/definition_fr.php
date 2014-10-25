2595f1c159c7866c38ead78faea68b261892a21e
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-10-25 03:44:59 +0000

    [Salvatore] [source:_plugins_/dictionnaires/trunk/lang/ definition] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@85574 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- dictionnaires/trunk/lang/definition_fr.php
+++ dictionnaires/trunk/lang/definition_fr.php
@@ -1,31 +1,39 @@
 <?php
-
-// Sécurité
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/dictionnaires/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-'bulledaide_definitions' => 'Consulter le glossaire du site (sigles et définitions)',
-'changer_statut' => 'Cette définition est :',
-'champ_casse_label' => 'Sensibilité de la casse',
-'champ_casse_label_case' => 'La détection de ce terme sera sensible aux minuscules et majuscules.',
-'champ_statut_label' => 'Statut',
-'champ_termes_explication' => 'Des termes supplémentaires qui permettront de détecter la définition, séparés par des virgules.',
-'champ_termes_label' => 'Termes',
-'champ_texte_label' => 'Texte',
-'champ_titre_label' => 'Titre',
-'champ_type_label_case' => 'Est-ce un sigle, un acronyme ou autre abrévation ?',
-'champ_type_label' => 'Abréviation',
-'icone_creer_definition' => 'Ajouter une nouvelle définition',
-'icone_modifier_definition' => 'Modifier cette définition',
-'info_1_definition' => 'Une définition',
-'info_aucun_definition' => 'Aucune définition',
-'info_nb_definitions' => '@nb@ définitions',
-'info_nouveau_definition' => 'Nouvelle définition',
-'info_numero_definition' => 'Définition numéro :',
-'titre_definition' => 'Définition',
-'titre_definition_creer' => 'Créer une nouvelle définition',
-'titre_definitions' => 'Définitions',
-'titre_logo_definition' => 'Logo de cette définition',
+
+	// B
+	'bulledaide_definitions' => 'Consulter le glossaire du site (sigles et définitions)',
+
+	// C
+	'champ_casse_label' => 'Sensibilité de la casse',
+	'champ_casse_label_case' => 'La détection de ce terme sera sensible aux minuscules et majuscules.',
+	'champ_statut_label' => 'Statut',
+	'champ_termes_explication' => 'Des termes supplémentaires qui permettront de détecter la définition, séparés par des virgules.',
+	'champ_termes_label' => 'Termes',
+	'champ_texte_label' => 'Texte',
+	'champ_titre_label' => 'Titre',
+	'champ_type_label' => 'Abréviation',
+	'champ_type_label_case' => 'Est-ce un sigle, un acronyme ou autre abrévation ?',
+	'changer_statut' => 'Cette définition est :',
+
+	// I
+	'icone_creer_definition' => 'Ajouter une nouvelle définition',
+	'icone_modifier_definition' => 'Modifier cette définition',
+	'info_1_definition' => 'Une définition',
+	'info_aucun_definition' => 'Aucune définition',
+	'info_nb_definitions' => '@nb@ définitions',
+	'info_nouveau_definition' => 'Nouvelle définition',
+	'info_numero_definition' => 'Définition numéro :',
+
+	// T
+	'titre_definition' => 'Définition',
+	'titre_definition_creer' => 'Créer une nouvelle définition',
+	'titre_definitions' => 'Définitions',
+	'titre_logo_definition' => 'Logo de cette définition'
 );
 
 ?>
