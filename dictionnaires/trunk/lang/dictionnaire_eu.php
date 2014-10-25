8d36db45c869df635d8197436069fc5945779fab
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-10-25 03:45:05 +0000

    [Salvatore] [source:_plugins_/dictionnaires/trunk/lang/ dictionnaire] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@85575 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- dictionnaires/trunk/lang/dictionnaire_eu.php
+++ dictionnaires/trunk/lang/dictionnaire_eu.php
@@ -1,33 +1,41 @@
 <?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/dictionnaire?lang_cible=eu
+// ** ne pas modifier le fichier **
 
-// Sécurité
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-'champ_actif_explication' => 'Aktiboa den hiztegi baten terminoak gunearen edukian detektatuak izanen dira.',
-'champ_actif_label' => 'Actibatua',
-'champ_actif_non' => 'Inaktiboa',
-'champ_actif_oui' => 'Aktiboa',
-'champ_descriptif_label' => 'Deskribapena',
-'champ_titre_label' => 'Titulua',
-'champ_type_defaut_label' => 'Lehenetsitako laburdura',
-'champ_type_defaut_label_case' => 'Hiztegi honen definizio berriak laburdurak izanen dira, llehenespen gisa.',
-'configurer_champ_remplacer_premier_abbr_label' => 'Laburdura baten lehen agerpena soilik ordezkatu',
-'configurer_champ_remplacer_premier_defaut_label' => 'Definizio baten lehen agerpena soilik ordezkatu',
-'configurer_titre' => 'Hiztegiak konfiguratu',
-'icone_creer_dictionnaire' => 'Hiztegi berri bat gehitu',
-'icone_modifier_dictionnaire' => 'Hiztegi hau aldatu',
-'icone_supprimer_dictionnaire' => 'Hiztegi hau kendu',
-'importer_acronymes_descriptif' => 'Siglo eta akronimoak',
-'importer_acronymes_titre' => 'Sigloak',
-'info_1_dictionnaire' => 'Hiztegi bat',
-'info_aucun_dictionnaire' => 'Hiztegirik ez',
-'info_nb_dictionnaires' => '@nb@ hiztegi',
-'info_nouveau_dictionnaire' => 'Hiztegi berria',
-'info_numero_dictionnaire' => 'Zenbatgarren hiztegia :',
-'titre_dictionnaire' => 'Hiztegia',
-'titre_dictionnaires' => 'Hiztegiak',
-'titre_logo_dictionnaire' => 'Hiztegi honen logotipoa',
+
+	// C
+	'champ_actif_explication' => 'Aktiboa den hiztegi baten terminoak gunearen edukian detektatuak izanen dira.',
+	'champ_actif_label' => 'Actibatua',
+	'champ_actif_non' => 'Inaktiboa',
+	'champ_actif_oui' => 'Aktiboa',
+	'champ_descriptif_label' => 'Deskribapena',
+	'champ_titre_label' => 'Titulua',
+	'champ_type_defaut_label' => 'Lehenetsitako laburdura',
+	'champ_type_defaut_label_case' => 'Hiztegi honen definizio berriak laburdurak izanen dira, llehenespen gisa.',
+	'configurer_champ_remplacer_premier_abbr_label' => 'Laburdura baten lehen agerpena soilik ordezkatu',
+	'configurer_champ_remplacer_premier_defaut_label' => 'Definizio baten lehen agerpena soilik ordezkatu',
+	'configurer_titre' => 'Hiztegiak konfiguratu',
+
+	// I
+	'icone_creer_dictionnaire' => 'Hiztegi berri bat gehitu',
+	'icone_modifier_dictionnaire' => 'Hiztegi hau aldatu',
+	'icone_supprimer_dictionnaire' => 'Hiztegi hau kendu',
+	'importer_acronymes_descriptif' => 'Siglo eta akronimoak',
+	'importer_acronymes_titre' => 'Sigloak',
+	'info_1_dictionnaire' => 'Hiztegi bat',
+	'info_aucun_dictionnaire' => 'Hiztegirik ez',
+	'info_nb_dictionnaires' => '@nb@ hiztegi',
+	'info_nouveau_dictionnaire' => 'Hiztegi berria',
+	'info_numero_dictionnaire' => 'Zenbatgarren hiztegia :',
+
+	// T
+	'titre_dictionnaire' => 'Hiztegia',
+	'titre_dictionnaires' => 'Hiztegiak',
+	'titre_logo_dictionnaire' => 'Hiztegi honen logotipoa'
 );
 
 ?>
--- dictionnaires/trunk/lang/dictionnaire_fr.php
+++ dictionnaires/trunk/lang/dictionnaire_fr.php
@@ -1,36 +1,42 @@
 <?php
-
-// Sécurité
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/dictionnaires/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-'changer_statut' => 'Ce dictionnaire est :',
-'champ_actif_explication' => 'Un dictionnaire actif aura ses termes détectés dans les contenus du site.',
-'champ_actif_label' => 'Activé',
-'champ_actif_non' => 'Inactif',
-'champ_actif_oui' => 'Actif',
-'champ_descriptif_label' => 'Descriptif',
-'champ_titre_label' => 'Titre',
-'champ_type_defaut_label' => 'Abréviation par défaut',
-'champ_type_defaut_label_case' => 'Les nouvelles définitions de ce dictionnaire seront par défaut des abréviations.',
-'configurer_champ_remplacer_premier_abbr_label' => 'Remplacer uniquement la première occurence d\'une abbréviation',
-'configurer_champ_remplacer_premier_defaut_label' => 'Remplacer uniquement la première occurence d\'une définition',
-'configurer_titre' => 'Configurer les dictionnaires',
-'confirmer_supprimer_dictionnaire' => 'Voulez-vous réellement supprimer ce dictionnaire et ses définitions ?',
-'icone_creer_dictionnaire' => 'Ajouter un nouveau dictionnaire',
-'icone_modifier_dictionnaire' => 'Modifier ce dictionnaire',
-'icone_supprimer_dictionnaire' => 'Supprimer ce dictionnaire',
-'importer_acronymes_descriptif' => 'Sigles et acronymes',
-'importer_acronymes_titre' => 'Sigles',
-'info_1_dictionnaire' => 'Un dictionnaire',
-'info_aucun_dictionnaire' => 'Aucun dictionnaire',
-'info_nb_dictionnaires' => '@nb@ dictionnaires',
-'info_nouveau_dictionnaire' => 'Nouveau dictionnaire',
-'info_numero_dictionnaire' => 'Dictionnaire numéro :',
-'info_supprimer_dictionnaire' => 'supprimer ce dictionnaire',
-'titre_dictionnaire' => 'Dictionnaire',
-'titre_dictionnaires' => 'Dictionnaires',
-'titre_logo_dictionnaire' => 'Logo de ce dictionnaire',
+
+	// C
+	'champ_actif_explication' => 'Un dictionnaire actif aura ses termes détectés dans les contenus du site.',
+	'champ_actif_label' => 'Activé',
+	'champ_actif_non' => 'Inactif',
+	'champ_actif_oui' => 'Actif',
+	'champ_descriptif_label' => 'Descriptif',
+	'champ_titre_label' => 'Titre',
+	'champ_type_defaut_label' => 'Abréviation par défaut',
+	'champ_type_defaut_label_case' => 'Les nouvelles définitions de ce dictionnaire seront par défaut des abréviations.',
+	'changer_statut' => 'Ce dictionnaire est :',
+	'configurer_champ_remplacer_premier_abbr_label' => 'Remplacer uniquement la première occurence d’une abbréviation',
+	'configurer_champ_remplacer_premier_defaut_label' => 'Remplacer uniquement la première occurence d’une définition',
+	'configurer_titre' => 'Configurer les dictionnaires',
+	'confirmer_supprimer_dictionnaire' => 'Voulez-vous réellement supprimer ce dictionnaire et ses définitions ?',
+
+	// I
+	'icone_creer_dictionnaire' => 'Ajouter un nouveau dictionnaire',
+	'icone_modifier_dictionnaire' => 'Modifier ce dictionnaire',
+	'icone_supprimer_dictionnaire' => 'Supprimer ce dictionnaire',
+	'importer_acronymes_descriptif' => 'Sigles et acronymes',
+	'importer_acronymes_titre' => 'Sigles',
+	'info_1_dictionnaire' => 'Un dictionnaire',
+	'info_aucun_dictionnaire' => 'Aucun dictionnaire',
+	'info_nb_dictionnaires' => '@nb@ dictionnaires',
+	'info_nouveau_dictionnaire' => 'Nouveau dictionnaire',
+	'info_numero_dictionnaire' => 'Dictionnaire numéro :',
+	'info_supprimer_dictionnaire' => 'supprimer ce dictionnaire',
+
+	// T
+	'titre_dictionnaire' => 'Dictionnaire',
+	'titre_dictionnaires' => 'Dictionnaires',
+	'titre_logo_dictionnaire' => 'Logo de ce dictionnaire'
 );
 
 ?>
