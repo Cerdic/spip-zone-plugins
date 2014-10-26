c7b7c935937a88b25801da8c94c0f961083adb63
gilles.vincent@gmail.com <gilles.vincent@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-10-26 04:41:01 +0000

    [Salvatore] [source:_plugins_/contacts_et_organisations/trunk/lang/ contacts] Export depuis http://trad.spip.net de la langue en
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@85631 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- contacts_et_organisations/trunk/lang/contacts_en.php
+++ contacts_et_organisations/trunk/lang/contacts_en.php
@@ -8,8 +8,25 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
-	'aucun_contact' => 'There is no contact !', # MODIF
-	'aucune_organisation' => 'There is no organisation', # MODIF
+	'annuaire' => 'Directory',
+	'annuaire_ajouter' => 'Add a directory',
+	'annuaire_aucun' => 'No directory',
+	'annuaire_champ_descriptif_label' => 'Description',
+	'annuaire_champ_identifiant_label' => 'Id',
+	'annuaire_champ_titre_label' => 'Title',
+	'annuaire_creer' => 'Create a new directory',
+	'annuaire_creer_associer' => 'Create and assign a new directory',
+	'annuaire_editer' => 'Edit this directory',
+	'annuaire_logo' => 'Logo of this directory',
+	'annuaire_supprimer' => 'Delete this directory',
+	'annuaire_un' => 'One directory',
+	'annuaires' => 'Directories',
+	'annuaires_nb' => '@nb@ directories',
+	'annuaires_tout' => 'Every directories',
+	'associe_a' => 'assigned to:',
+	'associer_a' => 'Assign to:',
+	'aucun_contact' => 'There is no contact !',
+	'aucune_organisation' => 'There is no organization !',
 	'auteur_associe_est_a_la_poubelle' => 'The associated author is in the rubbish bin! This author will be deleted in a few days.',
 	'auteur_associe_inexistant' => 'The associated author does not exist! The author must have been put in the rubbish bin and has been deleted.',
 	'auteur_lie' => 'Id of the linked author',
@@ -23,31 +40,40 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// C
 	'cfg_activer_squelettes_publics_zpip1' => 'Public skeleton of ZPIP v1',
-	'cfg_activer_squelettes_publics_zpip1_explication' => 'Enable public templates for ZPIP version 1, which allow to browse contacts and organisations in the public area?', # MODIF
-	'cfg_afficher_infos_sur_auteurs' => 'Detail display of the authors ?', # MODIF
-	'cfg_afficher_infos_sur_auteurs_explication' => 'View contact or organisation information on the authors pages in the private area?', # RELIRE
-	'cfg_associer_aux_auteurs' => 'Associate to the authors ?', # MODIF
+	'cfg_activer_squelettes_publics_zpip1_explication' => 'Enable public templates for ZPIP version 1, which allow to browse contacts and organisations in the public area?',
+	'cfg_afficher_infos_sur_auteurs' => 'Detail display of the authors ?',
+	'cfg_afficher_infos_sur_auteurs_explication' => 'View contact or organization information on the authors pages in the private area?',
+	'cfg_associer_aux_auteurs' => 'Associate to the authors ?',
 	'cfg_associer_aux_auteurs_explication' => 'Allow to associate contacts or organizations to authors. This adds a form to associate an author on the contact pages or organization, and conversely it adds a form to link to a contact or an organization on the authors pages.',
-	'cfg_relations_avec_auteurs' => 'Relationship with authors', # MODIF
-	'cfg_supprimer_reciproquement_auteurs_et_contacts' => 'Mutually delete authors and contacts? ', # MODIF
+	'cfg_lier_contacts_objets_explication' => 'Allow to associate contacts with the following editorial objects. This function displays the contact selector within the administration pages of these objects.',
+	'cfg_lier_contacts_objets_label' => 'Associate the contacts',
+	'cfg_lier_organisations_objets_explication' => 'Allow to associate organizations with the following editorial objects. This function displays the organization selector in the administration pages of these objects.',
+	'cfg_lier_organisations_objets_label' => 'Link with organizations',
+	'cfg_relations_avec_auteurs' => 'Relationship with authors',
+	'cfg_relations_avec_objets' => 'Relationship with other editorial objects (other than authors)',
+	'cfg_supprimer_reciproquement_auteurs_et_contacts' => 'Mutually delete authors and contacts? ',
 	'cfg_supprimer_reciproquement_auteurs_et_contacts_explication' => 'By activating this option, when a contact (or an organisation) is deleted, the author associated with this contact, if one exists, sees their status move to the rubbish bin. Inversely, in the same way, if an author is put in the rubbish bin, the contact associated is deleted. This option can be useful on certain sites in order to avoid contacts left without an author (if the latter has been deleted) but be vigilent: deletion is definitive and authors are put in the rubbish ben even if they have written articles...',
+	'cfg_utiliser_annuaires_explication' => 'Activate the possibility to put your contacts in several different directories.',
+	'cfg_utiliser_annuaires_label' => 'Use several directories',
 	'changer' => 'Change',
 	'chercher_contact' => 'Search',
 	'chercher_organisation' => 'Search',
 	'chercher_statut' => 'Status',
-	'confirmer_delier_contact' => 'Are you sure you want to unlink this organisation from this contact?', # MODIF
-	'confirmer_delier_organisation' => 'Are you sure you want to unlink this contact from this organisation?', # MODIF
-	'confirmer_delier_organisation_rubrique' => 'Are you sure you want to unlink this organisation from this section?', # MODIF
-	'confirmer_supprimer_contact' => 'Are you sure you want to delete all information about this contact?', # MODIF
-	'confirmer_supprimer_organisation' => 'Are you sure you want to delete all the information about this organisation?', # MODIF
+	'confirmer_delier_contact' => 'Are you sure you want to unlink this organization from this contact?',
+	'confirmer_delier_organisation' => 'Are you sure you want to unlink this contact from this organization?',
+	'confirmer_delier_organisation_rubrique' => 'Are you sure you want to unlink this organization from this section?',
+	'confirmer_supprimer_contact' => 'Are you sure you want to delete all information about this contact?',
+	'confirmer_supprimer_organisation' => 'Are you sure you want to delete all the information about this organization?',
 	'contact' => 'Contact',
 	'contact_ajouter' => 'Add a contact',
+	'contact_ajouter_associe_a' => 'Add a contact assigned to:',
 	'contact_ajouter_lien' => 'Add this contact',
 	'contact_associe_a_auteur_numero' => 'Link to author number',
 	'contact_associer_a_auteur' => 'Link to an author',
 	'contact_aucun' => 'No contact',
 	'contact_creer' => 'Create a contact',
 	'contact_creer_associer' => 'Create and associate a contact',
+	'contact_creer_dans_cet_annuaire' => 'Create a new contact in this directory',
 	'contact_editer' => 'Edit this contact',
 	'contact_logo' => 'Contact logo',
 	'contact_nouveau_titre' => 'New contact',
@@ -58,6 +84,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'contact_voir' => 'See',
 	'contacts' => 'Contacts',
 	'contacts_nb' => '@nb@ contacts',
+	'contacts_tout' => 'All contacts',
 	'creer_auteur_contact' => 'Create a new author and link it to this contact',
 	'creer_auteur_organisation' => 'Create a new author and link it to this organization',
 
@@ -69,12 +96,14 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'delier_organisation' => 'Disassociate',
 
 	// E
+	'erreur_annuaire_identifiant_existant' => 'This id is already used by one of your directories',
 	'est_un_contact' => 'This author is set as a contact.',
 	'est_une_organisation' => 'This author is set as an organization.',
 	'explication_activite' => 'Activity of the organization : NGO, education, edition...',
 	'explication_contacts_ou_organisations' => 'You can set this author as a contact or as an organization.Vous pouvez définir cet auteur
   Additional fields can be then filled from the author modification page.',
 	'explication_identification' => 'Organisation identification : VAT, etc.',
+	'explication_ouvertures' => 'Days, times, seasons...',
 	'explication_statut_juridique' => 'company, organisation, ...',
 	'explication_supprimer_contact' => 'Deleting this contact will remove all the additional informations which have been filled on the author page.',
 	'explication_supprimer_organisation' => 'Deleting this organization will remove all the additional informations which have been filled on the author page.',
@@ -98,6 +127,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// L
 	'label_activite' => 'Activity',
+	'label_auteur_associe' => 'Assigned author',
 	'label_civilite' => 'Gender',
 	'label_date_creation' => 'Date of creation',
 	'label_date_naissance' => 'Date of birth',
@@ -109,6 +139,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'label_nom_organisation' => 'Organisation',
 	'label_organisation' => 'Linked organisation',
 	'label_organisation_parente' => 'Parent organization',
+	'label_ouvertures' => 'Opening hours',
 	'label_prenom' => 'First name',
 	'label_prenom_nom' => 'First + last name',
 	'label_pseudo' => 'Pseudo',
@@ -116,8 +147,10 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'label_recherche_contacts' => 'Search in contacts',
 	'label_recherche_organisations' => 'Search in organisations',
 	'label_statut_juridique' => 'Status',
+	'label_tarifs' => 'Prices',
 	'label_telephone' => 'Phone',
 	'label_type_liaison' => 'Link',
+	'label_url_site' => 'Website',
 	'lier_ce_contact' => 'Attach this contact',
 	'lier_cet_auteur' => 'Link',
 	'lier_cette_organisation' => 'Attach to this organisation',
@@ -142,6 +175,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'organisation_aucun' => 'No organization',
 	'organisation_creer' => 'Create an organization',
 	'organisation_creer_associer' => 'Create and associate an organization',
+	'organisation_creer_dans_cet_annuaire' => 'Create a new organisation in this directory',
 	'organisation_creer_fille' => 'Create a child organization',
 	'organisation_editer' => 'Edit this organization',
 	'organisation_logo' => 'Organization logo',
@@ -153,6 +187,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'organisation_voir' => 'See',
 	'organisations' => 'Organisations',
 	'organisations_nb' => '@nb@ organizations',
+	'organisations_tout' => 'All the organisations',
 
 	// P
 	'prenom' => 'First name',
@@ -160,6 +195,8 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// R
 	'recherche_de' => 'Search for "@recherche@"',
 	'rechercher' => 'Search',
+	'resume_annuaire' => 'Summary of your directory',
+	'resume_annuaires' => 'Summary of your diretories',
 
 	// S
 	'statut_juridique' => 'Legal status',
@@ -170,7 +207,9 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// T
 	'titre_contact' => 'Contact details',
+	'titre_contacts' => 'Contacts',
 	'titre_organisation' => 'Organization details',
+	'titre_organisations' => 'Organizations',
 	'titre_page_configurer_contacts_et_organisations' => 'Configure Contacts & Organizations',
 	'titre_page_contacts' => 'Contacts management',
 	'titre_page_organisations' => 'Organisations management',
