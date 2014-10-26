7e9557f44e3c374883d86bc7f614dc8b854094c7
gilles.vincent@gmail.com <gilles.vincent@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-10-26 04:40:25 +0000

    [Salvatore] [source:_plugins_/abonnements_zones/trunk/lang/ paquet-abozones] Export depuis http://trad.spip.net de la langue en
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@85627 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- abonnements_zones/trunk/lang/paquet-abozones_en.php
+++ abonnements_zones/trunk/lang/paquet-abozones_en.php
@@ -8,7 +8,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
-	'abozones_description' => 'This plugin allows you to link restricted areas of the plugin "Acces Restreint" to subscription offers. All new subscribers to these offers will be automatically added to zones, and vice versa when disabling the subscription.', # MODIF
+	'abozones_description' => 'This plugin allows you to link restricted areas of the plugin "Acces Restreint" to subscription offers. All new subscribers to these offers will be automatically added to zones, and vice versa when disabling the subscription.',
 	'abozones_nom' => 'Subscriptions to restricted areas',
 	'abozones_slogan' => 'Link subscription offers to restricted areas'
 );
