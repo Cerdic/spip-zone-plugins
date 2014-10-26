5156ee37888470559bf134ff7f95aed437988481
gilles.vincent@gmail.com <gilles.vincent@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-10-26 04:40:18 +0000

    [Salvatore] [source:_plugins_/abonnements/trunk/lang/ abonnementsoffre] Export depuis http://trad.spip.net de la langue en
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@85626 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ abonnements/trunk/lang/abonnementsoffre_en.php
@@ -0,0 +1,53 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/abonnementsoffre?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'ajouter_lien_abonnementsoffre' => 'Add this subscription offer',
+
+	// C
+	'champ_descriptif_label' => 'Description',
+	'champ_duree_0' => 'No limit',
+	'champ_duree_label' => 'Duration',
+	'champ_periode_choix_heures' => 'Hours',
+	'champ_periode_choix_jours' => 'Days',
+	'champ_periode_choix_mois' => 'Month',
+	'champ_periode_label' => 'Period type',
+	'champ_periode_nb_heures' => '@nb@ hour(s)',
+	'champ_periode_nb_jours' => '@nb@ day(s)',
+	'champ_periode_nb_mois' => '@nb@ months',
+	'champ_prix_0' => 'Free',
+	'champ_prix_label' => 'Price',
+	'champ_titre_label' => 'Title',
+	'configurer_notifications' => 'Configure the notifications',
+	'configurer_notifications_quand' => 'When ?',
+
+	// I
+	'icone_creer_abonnementsoffre' => 'Create a subscription offer',
+	'icone_modifier_abonnementsoffre' => 'Modify this subscription offer',
+	'info_1_abonnementsoffre' => 'A subscription offer',
+	'info_abonnementsoffres_auteur' => 'Subscription offers of this author',
+	'info_aucun_abonnementsoffre' => 'No subscription offer',
+	'info_nb_abonnementsoffres' => '@nb@ subscription offers',
+
+	// R
+	'retirer_lien_abonnementsoffre' => 'Remove this subscription offer',
+	'retirer_tous_liens_abonnementsoffres' => 'Remove all subscription offers',
+
+	// T
+	'texte_ajouter_abonnementsoffre' => 'Add a subscription offer',
+	'texte_changer_statut_abonnementsoffre' => 'This subscription offer is:',
+	'texte_creer_associer_abonnementsoffre' => 'Create and associate a subscription offer',
+	'titre_abonnementsoffre' => 'Subscription offer',
+	'titre_abonnementsoffres' => 'Subscription offers',
+	'titre_abonnementsoffres_rubrique' => 'Subscription offers of the section',
+	'titre_langue_abonnementsoffre' => 'Language in this subscription offer',
+	'titre_logo_abonnementsoffre' => 'Logo of this subscription offer'
+);
+
+?>
