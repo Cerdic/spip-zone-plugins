bf8301f891921648493ca065fcdc1dbe0463a0f7
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-10-27 14:38:21 +0000

    Utiliser une autorisation pour afficher le champ dans le formulaire dater
    On affiche ce champ quand : 
    
    - Si les confs rubriques et secteurs est configurée, afficher ce champs si on est dans les bonnes rubriques et que l'on soit autorisé à modifier l'article ou à le dater
    - Si les confs rubriques et secteurs ne sont pas configurées, on peut mettre la date de dépublication sur les articles quand on peut le modifier ou le dater (permet de rendre le plugin fonctionnel sur tout le site par défaut sans configuration spécifique)
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@85680 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ depublie/depublie_autoriser.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Déclarations des autorisations
+ *
+ * @author     Anne-lise Martenot (http://elastick.net)
+ * @plugin     Depublie
+ * @copyright  2014
+ * @licence    GNU/GPL
+ * @package    SPIP\Depublie\Pipelines
+ */
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+function depublie_autoriser(){}
+
+/**
+ * Autorisation d'afficher le champ de dépublication sur un article
+ */
+function autoriser_depublierafficher($faire,$type,$id,$qui,$opt){
+	if(!function_exists('lire_config')){
+		include_spip('inc/config');
+	}
+	if((count(explode(',',lire_config('depublie/rubrique_depublie'))) + count(explode(',',lire_config('depublie/rubrique_depublie')))) > 0){
+		$table = table_objet_sql($type);
+		$id_table_objet = id_table_objet($type);
+		$infos_rubrique = sql_fetsel('id_rubrique,id_secteur',$table,"$id_table_objet = ".intval($id));
+		if(in_array($infos_rubrique['id_rubrique'],lire_config('depublie/rubrique_depublie',array())) || in_array($infos_rubrique['id_secteur'],lire_config('depublie/secteur_depublie',array())))
+			return autoriser('modifier',$type,$id,$qui,$opt) OR autoriser('dater',$type,$id,$qui,$opt);
+	}
+	else{
+		return autoriser('modifier',$type,$id,$qui,$opt) OR autoriser('dater',$type,$id,$qui,$opt);
+	}
+	return false;
+}
+
+?>
