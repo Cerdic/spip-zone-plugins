c6cc71bd151489dba33971cc0b4c4ffd433f866c
klaus@spip.de <klaus@spip.de@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-11-04 17:54:29 +0000

    traduction allemande v1
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@85829 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ skeleditor/trunk/lang/skeleditor_de.php
@@ -0,0 +1,95 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	// A
+	'action_supprimer'=>"Löschen",
+  'action_download'=>"Herunterladen",
+	'action_nouveau'=>'Neue Datei',
+	'action_upload'=>'Hochladen',
+
+  // C
+  'creer'=>'Anlegen',
+
+	'copy_comment' => '  Kopie vom @date@ durch @nom@
+  Originaldatei @source@',
+  
+  // E  
+  'editer_skel'=>'Skelett bearbeiten',
+  'editer_skel_debug' =>'Skelett bearbeiten (Debugmodus)',
+  'effacer_confirme'=>'Wollen sie diese Datei wirklich löschen ?',
+	'erreur_fichier_supprime' => 'Diese Datei wurde gelöscht, während sie an ihr gearbeitet haben. Inhalt kopieren und falls erforderlich neue Datei anlegen.',
+	'erreur_fichier_modif_coucourante' => 'Diese Datei wurde geändert, während sie an ihr gearbeitet haben. Dies ist die neue Version:',
+	'erreur_fichier_modif_coucourante_votre_version' => 'Mit ihrer Version zusammenführen und speichern:',
+
+	'erreur_overwrite'=>"Unmöglich, diese Datei existiert bereits",
+	'erreur_type_interdit' => 'Dieser Dateityp ist nicht zulässig',
+	'erreur_fichier_inexistant' => 'Diese Datei existiert nicht',
+	'erreur_creation_sous_dossier' => 'Das Unterverzeichnis @dir@ kann nicht angelegt werden.',
+	'erreur_ecrire_dans_sous_dossier' => 'In das Unterverzeichnis @dir@ kann  nicht geschrieben werden.',
+
+	'erreur_ouverture_fichier'=>"Fehler: Datei kann nicht geöffnet werden.",
+  'erreur_ecriture_fichier'=>"Datei kann nicht gespeichert werden.",
+  'erreur_edition_ecriture'=>"Fehler: Datei kann nicht geändert werden.",
+  'erreur_ouvert_ecrit'=>"Fehler: Datei kann nicht geöffnet oder geändert werden.",
+  'erreur_droits'=>"Fehler: Datei kann nicht angelegt werden. Überprüfen sie die Zugriffsrechte für ihr Skelett-Verzeichnis.",
+  'erreur_sansgene'=>'Fehler: Vorgang nicht zulässig',
+  'erreur_parsing'=>'Schwerer Fehler bei der Analyse der Schleifen (boucles)',
+  
+	'explications_filename_fichier_chemin' => 'Sie können einen vollständigen Pfad angeben, um die Datei in einem Unterverzeichnis zu speichern (z.B. "modeles/dokument.html").',
+	'explications_creer_dossier_squelettes' => 'Zum EInsatz des Skelett-Editors legen sie ein Verzeichnis<b>squelettes/</b> im Wurzelverzeichnis ihrer Website an, falls es nicht bereits existiert, und ordnen sie ihm Schreibrechte zu.',
+  // F
+  'fichier'=>"Datei:",
+  'fichier_enregistre'=>"Datei wurde gespeichert",
+	
+  'fichier_sauvegarde_date'=>"Datei gesichert @ ",    
+  'fichier_efface_ok'=>'Datei gelöscht!',
+  'fichier_choix'=>'Wählen sie die Datei zum Anzeigen oder Bearbeiten.',
+  'fichier_upload'=>'Datei hochladen',
+  'fichier_upload_ok'=>'Datei erfolgreich hochgeladen!',
+
+	'info_copie'=>'Beim Speichern wird die Datei in das Verzeichnis <em>@dir@</em> geschrieben.',
+
+	// L
+	'label_creer_fichier' => 'Datei anlegen',
+	'label_code' => 'Inhalt',
+	'label_file' => 'Datei',
+	'label_filename_squelette' => 'Name',
+	'label_renommer_fichier' => 'Umbenennen oder verschieben',
+	'label_nouveau_fichier' => 'Neue Datei',
+
+  // P
+  'parseur_titre' => 'Schleifentester',
+  'parseur_contenu' => 'Inahlt :',
+  'parseur_avant' => 'Davor',
+  'parseur_milieu' => 'Mitte',
+  'parseur_apres' => 'Danach',
+  'parseur_altern' => 'Alternative',
+  'parseur_param' => 'Parameter:',
+  'parseur_horsboucle' => 'AUSSERHALB DER SCHLEIFE',
+ 
+  
+  // R 
+  'rechercher' => 'Suchen',
+  'rechercher_ou_remplacer' => 'oder',
+  'remplacer' => 'ersetzen',
+  'remplacer_par' => 'durch',  
+  'repertoire' => 'Verzeichnis',
+
+  
+  // S
+	'squelettes' => 'CSS&amp;Skelette',
+  'sauver'=>'Sauver', 
+  'skeleditor_description'=>"Dateien des aktuellen Skeletts können bearbeitet werden.",
+  'skeleditor_dossier'=>"Skelett-Verzeichnis: ",
+  
+  // T
+  'target'=>'Position',
+  
+  // U
+  'upload'=>'Uploader',  
+);
+
+
+?>
