788282f0900ad16505b52642265f7d059cd378f6
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-11-06 11:29:01 +0000

    Indentation
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@85892 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- fulltext/trunk/lib/fonctions_zip.php
+++ fulltext/trunk/lib/fonctions_zip.php
@@ -9,33 +9,35 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 // Si vous ne pouvez pas utiliser ZipArchive (librairies manquantes ou vieille version de PHP), vous pouvez utiliser la librairie PclZip ( http://www.phpconcept.net/pclzip )
 
 function docx2text($filename) {
-    return readZippedXML($filename, "word/document.xml");
+	return readZippedXML($filename, "word/document.xml");
 }
+
 function odt2text($filename) {
-    return readZippedXML($filename, "content.xml");
+	return readZippedXML($filename, "content.xml");
 }
+
 function readZippedXML($archiveFile, $dataFile) {
-    // Creation d'une archive ZIP
-    $zip = new ZipArchive;
+	// Creation d'une archive ZIP
+	$zip = new ZipArchive;
 
-    // Ouvrir l'archive ZIP
-    if (true === $zip->open($archiveFile)) {
-        // Si c'est bon, rechercher du fichier de donnees (passe en argument) dans l'archive.
-        if (($index = $zip->locateName($dataFile)) !== false) {
-            //Si le fichier est trouve, lire les chaines de caracteres.
-            $data = $zip->getFromIndex($index);
-            // Fermez le zip
-            $zip->close();
-            // Charger le XML d'une chaine de caractere
-            // Passez les errors et les warnings
-            $xml = DOMDocument::loadXML($data, LIBXML_NOENT | LIBXML_XINCLUDE | LIBXML_NOERROR | LIBXML_NOWARNING);
-            // Renvoyer les donnees  sans les tags XML de formatage
-            return strip_tags($xml->saveXML());
-        }
-        $zip->close();
-    }
+	// Ouvrir l'archive ZIP
+	if (true === $zip -> open($archiveFile)) {
+		// Si c'est bon, rechercher du fichier de donnees (passe en argument) dans l'archive.
+		if (($index = $zip -> locateName($dataFile)) !== false) {
+			//Si le fichier est trouve, lire les chaines de caracteres.
+			$data = $zip -> getFromIndex($index);
+			// Fermez le zip
+			$zip -> close();
+			// Charger le XML d'une chaine de caractere
+			// Passez les errors et les warnings
+			$xml = DOMDocument::loadXML($data, LIBXML_NOENT | LIBXML_XINCLUDE | LIBXML_NOERROR | LIBXML_NOWARNING);
+			// Renvoyer les donnees  sans les tags XML de formatage
+			return strip_tags($xml -> saveXML());
+		}
+		$zip -> close();
+	}
 
-    // En cas d'echec, on renvoit une chaine vide
-    return "";
-} 
+	// En cas d'echec, on renvoit une chaine vide
+	return "";
+}
 ?>
