d814c3e566709689e9f3f4ce72cbf8ea1e361bde
marcimat@rezo.net <marcimat@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-11-06 09:28:29 +0000

    Sélectionner le bon élément du switcher (plus simplement)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@85877 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- zen-garden/trunk/inclure/zengarden_switcher.js
+++ zen-garden/trunk/inclure/zengarden_switcher.js
@@ -3,7 +3,6 @@ jQuery(function() {
 		.change(function(){
 			jQuery(this).parents('form').get(0).submit();
 		})
-	  .siblings("input[type=submit]").hide();
-        jQuery('#switcher_zen select option[value="'+theme_selected+'"]')
-        .attr('selected', 'selected');
+		.val(theme_selected)
+		.siblings("input[type=submit]").hide();
 });
