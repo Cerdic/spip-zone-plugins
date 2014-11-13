6fc07ffeebefad9e93b77534ff34c5089c10e7c6
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-11-13 23:16:40 +0000

    correction pour le plugin fonctionne sans Z + compatible 3.1, même si interface toujours aussi à l'arrache (mais les grèves, c'est toujours à l'arrache)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@86063 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- greves/greves_pipelines.php
+++ greves/greves_pipelines.php
@@ -2,9 +2,15 @@
 if (!defined("_ECRIRE_INC_VERSION")) return;
 function greves_styliser($flux){
 	if($id_greve=greve_active() and !test_espace_prive()){
-		if(substr_count($flux['args']['fond'],'/')==0 and ($flux['args']['contexte']['type'] or $flux['args']['contexte']['page']) and $flux['args']['fond']!='inc-head' and $flux['args']['fond']!='inc-entete' and $flux['args']['contexte']['page']!='login'){	
+		if(substr_count($flux['args']['fond'],'/')==0 
+ 
+			and $flux['args']['fond']!='inc-head' 
+			and $flux['args']['fond']!='inc-entete' 
+			and $flux['args']['contexte']['page']!='login'
+		){	
 			$flux['data'] 				= 	str_replace('.html','',find_in_path('greve.html'));
 			$flux['args']['fond']		= 	'greve';
+			var_dump($flux);
 		}
 	}
 	return $flux;	
