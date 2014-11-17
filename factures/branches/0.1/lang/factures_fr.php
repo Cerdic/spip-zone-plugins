4df6e0ce4bd3f1e228763e6ead73299dd22d1da9
marcimat@rezo.net <marcimat@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-11-17 17:17:17 +0000

    en utf8
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@86129 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- factures/branches/0.1/lang/factures_fr.php
+++ factures/branches/0.1/lang/factures_fr.php
@@ -27,9 +27,9 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'editer_facturation_titre' => 'Editer une facture',
 	'explication_liste_factures' => 'Objectif : un tableau, avec pagination, moteur de recherche, possibilit&eacute; de tri (clic sur colonne) et de filtre (s&eacute;lection d\'un mot cl&eacute;, p.ex. le type de document : facture ou devis)',
 	'explications_page_factures' => 'Sur cette page :<br />
-- liste des factures &amp; devis<br />
+- liste des factures & devis<br />
 - bouton cr&eacute;er facture',
-	'explications_page_config_factures' => 'essentiellement choix du groupe de mots cl&eacute; d&eacute;finissant les types de factures (facture, devis, proforma, avoir) et les organisations &eacute;mettrices de factures (au moins une, celle de celui qui facture); si pas de groupe pr&eacute;-d&eacute;fini on en cr&eacute;e un; si pas d\'Organisation pr&eacute;-d&eacute;finie, on en cr&eacute;e une.',
+	'explications_page_config_factures' => 'essentiellement choix du groupe de mots cl&eacute; définissant les types de factures (facture, devis, proforma, avoir) et les organisations &eacute;mettrices de factures (au moins une, celle de celui qui facture); si pas de groupe pré-défini on en crée un; si pas d\'Organisation pré-définie, on en crée une.',
 
 	// F
 	'fin_validite' => "Fin de validité",
@@ -45,8 +45,8 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'num_facture' => "Numéro facture",
 
 	// T
-	'titre_page_factures' => 'G&eacute;rer les Factures &amp; Devis',
-	'titre_page_config_factures' => 'Configuration du plugin Factures &amp; Devis',
+	'titre_page_factures' => 'Gérer les Factures & Devis',
+	'titre_page_config_factures' => 'Configuration du plugin Factures & Devis',
 	'titre_liste_factures' => 'Liste des factures existantes',
 
 	// Z
