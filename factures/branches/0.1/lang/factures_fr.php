6ccf562490865eca989910d070aac01917b91a8a
marcimat@rezo.net <marcimat@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-11-17 17:07:40 +0000

    Des chaînes de langue manquantes.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@86128 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- factures/branches/0.1/lang/factures_fr.php
+++ factures/branches/0.1/lang/factures_fr.php
@@ -8,15 +8,20 @@
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-	
-	// B
-	'bouton_factures' => 'Factures &amp; Devis',
-	'bouton_configurer_factures' => 'Configurer Factures &amp; Devis',
 
-    // C
-    'choisir_groupe_mots_facture' => 'Choisir un groupe de mots clé pour définir le type de facture (avoir, devis, proforma...).',
-    'choisir_organisation' => 'Choisir une organisation (en général la votre...) qui sera l\'emetteur de toutes les factures.',
-    'choisir_auteur_emetteur' => 'Choisir un auteur (en général vous ou celui attaché à votre organisation...) qui sera l\'emetteur de toutes les factures.',
+	// B
+	'bouton_factures' => 'Factures & Devis',
+	'bouton_configurer_factures' => 'Configurer Factures & Devis',
+
+	// C
+	'choisir_groupe_mots_facture' => 'Choisir un groupe de mots clé pour définir le type de facture (avoir, devis, proforma...).',
+	'choisir_organisation' => 'Choisir une organisation (en général la votre...) qui sera l\'emetteur de toutes les factures.',
+	'choisir_auteur_emetteur' => 'Choisir un auteur (en général vous ou celui attaché à votre organisation...) qui sera l\'emetteur de toutes les factures.',
+
+	// D
+	'date_facture' => 'Date facture',
+	'date_payement' => 'Date payement',
+	'delais_validite' => 'Délais de validité',
 
 	// E
 	'editer_facturation_titre' => 'Editer une facture',
@@ -26,16 +31,26 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 - bouton cr&eacute;er facture',
 	'explications_page_config_factures' => 'essentiellement choix du groupe de mots cl&eacute; d&eacute;finissant les types de factures (facture, devis, proforma, avoir) et les organisations &eacute;mettrices de factures (au moins une, celle de celui qui facture); si pas de groupe pr&eacute;-d&eacute;fini on en cr&eacute;e un; si pas d\'Organisation pr&eacute;-d&eacute;finie, on en cr&eacute;e une.',
 
+	// F
+	'fin_validite' => "Fin de validité",
+
+	// L
+	'libelle_facture' => "Libellé facture",
+
+	// M
+	'montant' => "Montant",
+
 	// N
-	'nom_defaut' => 'Fiche facture par d&eacute;faut',
+	'nom_defaut' => 'Fiche facture par défaut',
+	'num_facture' => "Numéro facture",
 
 	// T
 	'titre_page_factures' => 'G&eacute;rer les Factures &amp; Devis',
 	'titre_page_config_factures' => 'Configuration du plugin Factures &amp; Devis',
 	'titre_liste_factures' => 'Liste des factures existantes',
 
-    // Z
-    'z_facture' => 'Test pour voir si le plugin factures était bien pris en compte',
+	// Z
+	'z_facture' => 'Test pour voir si le plugin factures était bien pris en compte',
 
 );
 ?>
