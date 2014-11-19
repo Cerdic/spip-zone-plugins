47875ecececaf5d709108337847a17c49a5ba393
frederic.bouteille.mae@gmail.com <frederic.bouteille.mae@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-11-19 10:26:10 +0000

    Ajout du FORMULAIRE_JAIME_COEUR pour un ti coeur et un décompte des clics dessus
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@86162 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ notation/trunk/balise/formulaire_jaime_coeur.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Plugin Notation
+ * par JEM (jean-marc.viglino@ign.fr) / b_b / Matthieu Marcillaud
+ *
+ * Copyright (c) 2008
+ * Logiciel libre distribue sous licence GNU/GPL.
+ *
+ */
+if (!defined("_ECRIRE_INC_VERSION")) return;	#securite
+
+
+/**
+ * Ce formulaire permet de noter des objets de SPIP.
+ * Par defaut, l'objet et son identifiant sont pris dans la boucle
+ * <BOUCLE_(ARTICLES){id_article}> #FORMULAIRE_JAIME_JAIMEPAS ...
+ * 
+ * Mais il est possible de forcer un objet particulier :
+ * #FORMULAIRE_JAIME_JAIMEPAS{article,8}
+ * 
+ */
+function balise_FORMULAIRE_JAIME_COEUR ($p) {
+	include_spip("balise/formulaire_notation");
+	// on prend nom de la cle primaire de l'objet pour calculer sa valeur
+	$_id_objet = $p->boucles[$p->id_boucle]->primary;
+	return calculer_balise_dynamique(
+		$p,
+		'FORMULAIRE_JAIME_COEUR',
+		array(
+			'NOTATION_TYPE_BOUCLE', // demande du type d'objet
+			$_id_objet
+		)
+	);
+}
+
+function balise_FORMULAIRE_JAIME_COEUR_stat($args, $filtres) {
+	include_spip("balise/formulaire_notation");
+	return balise_FORMULAIRE_NOTATION_stat($args,$filtres);
+}
+
+?>
