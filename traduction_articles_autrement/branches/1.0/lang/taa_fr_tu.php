76e86c896fe720bc4578d259621b64826aead8ab
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-11-22 10:19:53 +0000

    salvatore
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@86226 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- traduction_articles_autrement/branches/1.0/lang/taa_fr_tu.php
+++ traduction_articles_autrement/branches/1.0/lang/taa_fr_tu.php
@@ -7,7 +7,12 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
+	// A
+	'affichage_liste_compacte_desactive' => 'Désactiver l’affichage compacte des listes des articles',
+
 	// C
+	'cfg_limiter_secteur' => 'Ne pas afficher la barre de langue dans le secteur :',
+	'cfg_utiliser_langues' => 'Utiliser les langues suivantes :',
 	'changer_langue' => 'Changer la langue de l’article',
 
 	// F
@@ -24,10 +29,14 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// O
 	'options' => 'Modifier les options de langues',
+	'oui' => 'Oui',
+
+	// R
+	'rang' => 'Rang',
 
 	// T
 	'titre_plugin' => 'Traduction d’articles autrement',
-	'traductions' => 'Traductions', # MODIF
+	'traductions' => 'Trad',
 
 	// V
 	'voir_traductions' => 'Voir le détail des traductions'
