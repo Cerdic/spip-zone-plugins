379302a4eb2662d861874b281c8406140cbf6d46
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-11-24 15:24:56 +0000

    Bug de perfo sans cache, on fait en sorte que le cache du squelette soit dépendant du contenu, pour qu'il se recalcule auto. Merci cerdic !
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@86261 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- alerte_urgence/trunk/alerte_urgence_pipelines.php
+++ alerte_urgence/trunk/alerte_urgence_pipelines.php
@@ -15,8 +15,16 @@ function alerte_urgence_affichage_final($flux) {
 		$GLOBALS['html'] // si c'est bien du HTML
 		and (strpos($flux,'<body')) !== false // et qu'on a une balise <body>
 	) {
+		include_spip('inc/config');
 		//$flux = substr_replace($flux, recuperer_fond('inclure/alerte_urgence'), $p, 0);
-		$flux = preg_replace('|<body[^>]*>|is', '$0'.recuperer_fond('inclure/alerte_urgence'), $flux);
+		$flux = preg_replace(
+			'|<body[^>]*>|is',
+			'$0'.recuperer_fond(
+				'inclure/alerte_urgence',
+				array('signature' => lire_config('alerte_urgence/texte'))
+			),
+			$flux
+		);
 	}
 	return $flux;
 }
