31185729f7ffa40af1916baf2c3811c3f9e72675
erational@erational.org <erational@erational.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-11-25 15:24:47 +0000

    compat 3.1 ca marche :)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@86271 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- bellespuces/bellespuces.php
+++ bellespuces/bellespuces.php
@@ -1,9 +1,11 @@
 <?php
 
-	function bellespuces_pre_typo($texte) {
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+function bellespuces_pre_typo($texte) {
         $texte = preg_replace('/^-\s+/m','-* ',$texte);
         return $texte;
-    }
+}
 
 
 ?>
