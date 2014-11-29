65e01ac284daf52dc97a8f4ba5dc670c8d1b2ba3
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-11-29 09:17:23 +0000

    Juste un peu de PHPdoc
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@86335 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- lister_logos/trunk/prive/squelettes/contenu/lister_logos_fonctions.php
+++ lister_logos/trunk/prive/squelettes/contenu/lister_logos_fonctions.php
@@ -7,6 +7,21 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 include_spip('inc/chercher_logo');
 include_spip('base/objets');
 
+/**
+ * Récupérer les infos à partir du fichier de logo
+ *
+ * @param  string  $fichier
+ *         Chemin ou nom du fichier de logo
+ * @param  int     $index
+ *         - Si l'index est `null`, on affichera un tableau représentant le résultat de preg_match()
+ *         - `$index = 0` : retourne `$fichier` ;
+ *         - `$index = 1` : le type de logo de l'objet. cf. art, rub, mot, etc.
+ *         - `$index = 2` : l'état du logo. cf. `on` pour normal, `off` pour survol.
+ *         - `$index = 3` : l'extension du fichier.
+ * @return array|string
+ *         Si l'index est `null`, on retournera le résultat que `preg_match()`, soit un tableau.
+ *         Si l'index est une valeur numérique (<4), on retourne la valeur du tableau correspondant à l'index.
+ */
 function logo_infos($fichier, $index = null)
 {
     // Fonction one ne peut plus simple.
@@ -17,6 +32,17 @@ function logo_infos($fichier, $index = null)
     return $r;
 }
 
+/**
+ * Avoir l'état du logo
+ *
+ * @uses   logo_infos()
+ * @param  string $fichier
+ *         Le fichier du logo.
+ * @return string
+ *         Retourne l'état du logo :
+ *         - Logo normal (on) ;
+ *         - Logo de survol (off).
+ */
 function logo_etat($fichier)
 {
     $infos = logo_infos($fichier);
