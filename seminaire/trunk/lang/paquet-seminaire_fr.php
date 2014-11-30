cd02492fac7631ca5d5b1bc73d738575e43b6eee
amaury.adon@laposte.net <amaury.adon@laposte.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-11-30 14:57:34 +0000

    coquille
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@86374 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- seminaire/trunk/lang/paquet-seminaire_fr.php
+++ seminaire/trunk/lang/paquet-seminaire_fr.php
@@ -8,7 +8,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// S
 	'seminaire_description' => 'Chaque événement est associé à des mots-clés qui permettent, d’une part, de fixer son type, séminaire, colloque, groupe de travail... et d’autre part, de définir le laboratoire ou l’organisme responsable. La gestion des événements est assurée par le plugin Agenda.',
 	'seminaire_nom' => 'Séminaire',
-	'seminaire_slogan' => 'Gérer les événements d\\\'un laboratoire de recherche',
+	'seminaire_slogan' => 'Gérer les événements d\'un laboratoire de recherche',
 );
 
 ?>
