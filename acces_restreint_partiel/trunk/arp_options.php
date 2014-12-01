31103e5be35156a002469999e268411303b4513d
bruno.caillard@free.fr <bruno.caillard@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-12-01 08:53:31 +0000

    Surcharge de la fonction de protection des urls de document du plugin Accès Restreint
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@86392 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- acces_restreint_partiel/trunk/arp_options.php
+++ acces_restreint_partiel/trunk/arp_options.php
@@ -21,5 +21,47 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
  * 
  */
 
+// Surcharge de la fonction de protection des url de Accès Restreint
+// présente dans /urls/generer_url_document.php
+// on fait en sorte de lairsser la protection active sauf pour les images
+function urls_generer_url_document($id, $args='', $ancre='', $public=null, $connect='') {
+	include_spip('inc/autoriser');
+	include_spip('inc/documents');
+
+	// Ajout par rapport à la fonction d'origine dans Accès Restreint
+	//---------------------------------------------------------------
+//	if (!autoriser('voir', 'document', $id)) return '';
+	$motif = "`^(?:jpg|bmp|png|gif)\/(.*)$`isU";
+	$f = $r['fichier'];
+	preg_match($motif, $f,$resultat);
+	if (sizeof($resultat) != 0)
+		return get_spip_doc($f);
+	// FIN ajout
+
+	$r = sql_fetsel("fichier,distant", "spip_documents", "id_document=".intval($id));
+
+	if (!$r) return '';
+
+	$f = $r['fichier'];
+
+	if ($r['distant'] == 'oui') return $f;
+
+	// Si droit de voir tous les docs, pas seulement celui-ci
+	// il est inutilement couteux de rajouter une protection
+	$r = (autoriser('voir', 'document'));
+	if (($r AND $r !== 'htaccess'))
+		return get_spip_doc($f);
+
+	include_spip('inc/securiser_action');
+
+	// cette url doit etre publique !
+	$cle = calculer_cle_action($id.','.$f);
+
+	// renvoyer une url plus ou moins jolie
+	if ($GLOBALS['meta']['creer_htaccess'])
+		return _DIR_RACINE."docrestreint.api/$id/$cle/$f";
+	else
+		return get_spip_doc($f)."?$id/$cle";
+}
 
 ?>
