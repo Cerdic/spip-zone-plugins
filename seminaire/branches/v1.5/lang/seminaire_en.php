ef86d20a29958130e06dc01e985768a0e05b31ec
job@pverrier.fr <job@pverrier.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-12-01 15:19:14 +0000

    Remplacé squelette inc-css-seminaire.css.html utilisé pour produire les CSS par une feuille de styles normale, revu pipeline ; Ajouté traductions anglaises.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@86400 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- seminaire/branches/v1.5/lang/seminaire_en.php
+++ seminaire/branches/v1.5/lang/seminaire_en.php
@@ -1,10 +1,38 @@
 <?php
-$GLOBALS[$GLOBALS['idx_lang']] = array(
-
-	'evenement_titre'			 =>		'Event&rsquo;s Title',
-	'exemple'					 =>		'For example : CMI - R164'
-
+$GLOBALS[$GLOBALS['idx_lang']] = array(          
+  //pour le formulaire de séminaires
+  'titre_evenement'                   => 'Title to specify',
+  'evenement_titre'                   => 'Title of the event',
+  'name'                              => 'Name of the contributor (Name)',
+  'attention_mot_cle'                 => 'You must IMPERATIVLY put a keyword on the event',
+  'origin'                            => 'Origin institute of the contributor',
+  'abstract'                          => 'Summary',
+  'notes'                             => 'Notes of the last minute',
+  'lieu'                              => 'Place',
+  'precisions_name'                   => 'the name will be published on the calendar and on the mini calendar',
+  'precisions_origin'                 => 'Published only in the details of the event',
+  'precisions_abstract'               => 'to put some LATEX code, add the markers <math> </math> at the beginning and at the end of the text (ex :  <math>The fraction $\frac{1}{2}$</math>)',
+  'precisions_notes'                  => 'Published only in the details of the event',
+  'apreciser'                         => 'To specify',
+  'plus'                              => 'Further details',
+  'a_venir'                           => 'Next events',
+  'choix_mot'                         => 'Type of event',
+  'attention_type'                    => 'The choice of event type determines the sort which will be applied.', 
+  'oubli_titre'                       => 'Don’t forget to put the title of the event',
+  'exemple'                           => 'For example: CMI - Room R164',
+  'aujour'                            => 'Events of the present day',
+  'lien_tout_replier'                 => 'Without summary',
+  'lien_tout_deplier'                 => 'With summary',
+  'titre_agenda'                      => 'Scientific calendar',
+                                        
+  //pour les groupes de mots clés
+  'mots_categorie_kitcnrs'            => 'The keywords to assign to the articles for a better sort of the events',
+  'mots_cles_techniques_kitcnrs'      => 'The keywords to detect more finely the different types of events',
+  'type'                              => 'Type of event',
+  'evenements_depuis_debut'           => 'Archives',           
+  'creer_evenement'                   => 'Add an event', 
+  'erreur_install_groupe_categories'  => 'error creating the group',
+  'mots_cles_categories'              => 'The different categories of regular events, team works and exceptional events',
+  'abonnement'                        => 'Subscribe to the calendar',
 );
-
-
 ?>
--- seminaire/branches/v1.5/seminaire_pipelines.php
+++ seminaire/branches/v1.5/seminaire_pipelines.php
@@ -1,6 +1,6 @@
 <?php
 function seminaire_insert_head($flux){
-    $flux .= '<link rel="stylesheet" type="text/css" href="'.find_in_path('inc-css-seminaire.css.html').'" type="text/css" media="all" />'."\n";
+    $flux .= '<link rel="stylesheet" type="text/css" href="'.find_in_path('styles/seminaire.css').'" type="text/css" media="all" />'."\n";
     return $flux;
 }
 ?>
