9016e145f78abe52d311634181c3f297d51fd770
gouz@root-me.org <gouz@root-me.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-12-02 11:59:45 +0000

    modification genie/imap_feedback.php : on ne fait rien si pas de connection, activation du genie si config['mailer']=='defaut', correction coquilles
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@86415 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- mailshot/trunk/genie/imap_feedback.php
+++ mailshot/trunk/genie/imap_feedback.php
@@ -12,54 +12,57 @@ function genie_mailjet_feedback_dist($t){
 	include_spip('inc/mailshot');
 	include_spip('inc/config');
 	
-        $username = "USER@gmail.com";
-        $password = "PASSWORD";
+        $username = "";
+        $password = "";
         $hostname = "{imap.gmail.com:993/imap/ssl}INBOX";        
 
         $return_path_email = lire_config("facteur_adresse_envoi_email");
         
         $inbox = imap_open($hostname, $username, $password, OP_READONLY);
-	$emails = imap_search($inbox,'UNSEEN TO "'.$return_path_email.'"');
-	
-	// toutes les campagnes envoyees depuis moins de 10jours (au dela on poll plus les stats)
-	$campagnes = sql_allfetsel("*","spip_mailshots", sql_in('statut',array('processing','end')) . " AND date_start>".sql_quote(date('Y-m-d H:i:s',strtotime("-10 day"))),'','date_start','0,10');
-	foreach($campagnes as $campagne){
+        
+        if ($inbox){
+                $emails = imap_search($inbox,'UNSEEN TO "'.$return_path_email.'"');
                 
-                // recuperer l'id campagne
-                $tracking_id = "mailshot".$campagne['id_mailshot'];
-                
-                // traiter les emails
-                if ($emails){
-                        foreach($emails as $email_number) {   
-                                $message_lines = explode("\n", imap_fetchbody($inbox,$email_number,2));
-                                $return_status_code = $original_recipient = "";
-                                if (is_array($message_lines) && count($message_lines)) {
-                                    foreach($message_lines as $l) {
-                                        $l = trim($l);
-                                        if ( preg_match("/^Status:/i", $l) ){
-                                            $return_status_code = intval(str_replace('.', '', explode(":",$l)[1]));
+                // toutes les campagnes envoyees depuis moins de 10jours (au dela on poll plus les stats)
+                $campagnes = sql_allfetsel("*","spip_mailshots", sql_in('statut',array('processing','end')) . " AND date_start>".sql_quote(date('Y-m-d H:i:s',strtotime("-10 day"))),'','date_start','0,10');
+                foreach($campagnes as $campagne){
+                        
+                        // recuperer l'id campagne
+                        $tracking_id = "mailshot".$campagne['id_mailshot'];
+                        
+                        // traiter les emails
+                        if ($emails){
+                                foreach($emails as $email_number) {   
+                                        $message_lines = explode("\n", imap_fetchbody($inbox,$email_number,2));
+                                        $return_status_code = $original_recipient = "";
+                                        if (is_array($message_lines) && count($message_lines)) {
+                                            foreach($message_lines as $l) {
+                                                $l = trim($l);
+                                                if ( preg_match("/^Status:/i", $l) ){
+                                                    $return_status_code = intval(str_replace('.', '', explode(":",$l)[1]));
+                                                }
+                                                if ( preg_match("/^Original-Recipient: rfc822;/i", $l) ){
+                                                    $original_recipient = explode(";",$l)[1];
+                                                }
+                                            }
                                         }
-                                        if ( preg_match("/^Original-Recipient: rfc822;/i", $l) ){
-                                            $original_recipient = explode(";",$l)[1];
-                                        }
-                                    }
-                                }
-                                if ($row = sql_fetsel("*","spip_mailshots_destinataires","id_mailshot=".intval($campagne['id_mailshot'])." AND email=".sql_quote($original_recipient))){
-                                        $event = "";
-                                        if ($return_status_code[1] == 4 AND $row['statut']!=='fail'){
-                                                $event = 'reject';
-                                        }
-                                        elseif ($return_status_code[1] == 5 AND $row['statut']!=='fail'){
-                                                $event = 'hard_bounce';
-                                        }
-                                        if ($event) {
-                                                #var_dump("$event : ".$email->to_email);
-                                                $feedback($event,$original_recipient,$tracking_id);
+                                        if ($row = sql_fetsel("*","spip_mailshots_destinataires","id_mailshot=".intval($campagne['id_mailshot'])." AND email=".sql_quote($original_recipient))){
+                                                $event = "";
+                                                if ($return_status_code[1] == 4 AND $row['statut']!=='fail'){
+                                                        $event = 'reject';
+                                                }
+                                                elseif ($return_status_code[1] == 5 AND $row['statut']!=='fail'){
+                                                        $event = 'hard_bounce';
+                                                }
+                                                if ($event) {
+                                                        #var_dump("$event : ".$email->to_email);
+                                                        $feedback($event,$original_recipient,$tracking_id);
+                                                }
                                         }
                                 }
                         }
-		}
-	}
-
+                }
+                imap_close($inbox);
+        }
 	return 0;
 }
