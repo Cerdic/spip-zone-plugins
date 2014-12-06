bfcb3c8d7838ff9304f2b6584a2f7c6f6c5fc2f6
gilles.vincent@gmail.com <gilles.vincent@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-12-06 22:40:59 +0000

    revert de [86508]
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@86518 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- saisies/trunk/lang/paquet-saisies_fr.php
+++ saisies/trunk/lang/paquet-saisies_fr.php
@@ -6,11 +6,11 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// S
-	'saisies_description' => 'Ce plugin permet de faciliter l&#8217;&#233;criture de champs de formulaires en proposant une
-		balise #SAISIE. Le HTML g&#233;n&#233;r&#233; est compatible avec la nomenclature des formulaires
-		propos&#233;e par SPIP > 2.0 et avec le plugin de configuration CFG.',
+	'saisies_description' => 'Ce plugin permet de faciliter l’écriture de champs de formulaires en proposant une
+		balise #SAISIE. Le HTML généré est compatible avec la nomenclature des formulaires
+		proposée par SPIP > 2.0 et avec le plugin de configuration CFG.',
 	'saisies_nom' => 'Saisies pour formulaires',
-	'saisies_slogan' => '&#201;crire facilement des champs de formulaires.',
+	'saisies_slogan' => 'Écrire facilement des champs de formulaires.',
 	'saisies_titre' => 'Saisies pour formulaires'
 );
 
