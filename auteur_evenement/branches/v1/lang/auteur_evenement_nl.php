d14431073c457786f853fed384347cf4ec4dffe4
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-12-06 09:48:13 +0000

    salvatore
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@86506 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ auteur_evenement/branches/v1/lang/auteur_evenement_nl.php
@@ -0,0 +1,17 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/auteur_evenement?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'auteur_evenement_titre' => 'Evenement-auteurs',
+
+	// I
+	'info_evenements_auteur' => 'De evenementen van deze auteur'
+);
+
+?>
