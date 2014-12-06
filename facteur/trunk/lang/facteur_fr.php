1cc73fdb781ef52c1c39f66bfa47b5be12a65fbc
gilles.vincent@gmail.com <gilles.vincent@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-12-06 22:34:32 +0000

    revert de [86510]
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@86516 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- facteur/trunk/lang/facteur_fr.php
+++ facteur/trunk/lang/facteur_fr.php
@@ -6,58 +6,58 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// C
-	'config_info_enregistree' => 'La configuration du facteur a bien &#0233;t&#0233; enregistr&#0233;e',
-	'configuration_adresse_envoi' => 'Configuration de l&#8217;adresse d&#8217;envoi',
+	'config_info_enregistree' => 'La configuration du facteur a bien été enregistrée',
+	'configuration_adresse_envoi' => 'Configuration de l’adresse d’envoi',
 	'configuration_facteur' => 'Facteur',
 	'configuration_mailer' => 'Configuration du mailer',
-	'configuration_smtp' => 'Choix de la m&#0233;thode d&#8217;envoi de mail',
-	'configuration_smtp_descriptif' => 'Si vous n&#8217;&#0234;tes pas s&#0251;rs, choisissez la fonction mail de PHP.',
-	'corps_email_de_test' => 'Ceci est un email de test accentu&#0233;',
+	'configuration_smtp' => 'Choix de la méthode d’envoi de mail',
+	'configuration_smtp_descriptif' => 'Si vous n’êtes pas sûrs, choisissez la fonction mail de PHP.',
+	'corps_email_de_test' => 'Ceci est un email de test accentué',
 
 	// E
-	'email_envoye_par' => 'Envoy&#0233; par @site@',
-	'email_test_envoye' => 'L&#8217;email de test a correctement &#0233;t&#0233; envoy&#0233;. Si vous ne le recevez pas correctement, v&#0233;rifiez la configuration de votre serveur ou contactez un administrateur du serveur.',
+	'email_envoye_par' => 'Envoyé par @site@',
+	'email_test_envoye' => 'L’email de test a correctement été envoyé. Si vous ne le recevez pas correctement, vérifiez la configuration de votre serveur ou contactez un administrateur du serveur.',
 	'erreur' => 'Erreur',
-	'erreur_dans_log' => '&#160;: consultez le fichier log pour plus de d&#0233;tails',
-	'erreur_generale' => 'Il y a une ou plusieurs erreurs de configuration. Veuillez v&#0233;rifier le contenu du formulaire.',
-	'erreur_invalid_host' => 'Ce nom d&#8217;h&#0244;te n&#8217;est pas correct',
-	'erreur_invalid_port' => 'Ce num&#0233;ro de port n&#8217;est pas correct',
+	'erreur_dans_log' => ' : consultez le fichier log pour plus de détails',
+	'erreur_generale' => 'Il y a une ou plusieurs erreurs de configuration. Veuillez vérifier le contenu du formulaire.',
+	'erreur_invalid_host' => 'Ce nom d’hôte n’est pas correct',
+	'erreur_invalid_port' => 'Ce numéro de port n’est pas correct',
 
 	// F
-	'facteur_adresse_envoi_email' => 'Email&#160;:',
-	'facteur_adresse_envoi_nom' => 'Nom&#160;:',
-	'facteur_bcc' => 'Copie Cach&#0233;e (BCC)&#160;:',
-	'facteur_cc' => 'Copie (CC)&#160;:',
-	'facteur_copies' => 'Copies&#160;:',
-	'facteur_copies_descriptif' => 'Un email sera envoy&#0233; en copie aux adresses d&#0233;finies. Une seule adresse en copie et/ou une seule adresse en copie cach&#0233;e.',
-	'facteur_filtre_accents' => 'Transformer les accents en leur entit&#0233;s html (utile pour Hotmail notamment).',
-	'facteur_filtre_css' => 'Transformer les styles contenus entre <head> et </head> en des styles "en ligne", utile pour les webmails car les styles en ligne ont la priorit&#0233; sur les styles externes.',
-	'facteur_filtre_images' => 'Embarquer les images r&#0233;f&#0233;renc&#0233;es dans les emails',
+	'facteur_adresse_envoi_email' => 'Email :',
+	'facteur_adresse_envoi_nom' => 'Nom :',
+	'facteur_bcc' => 'Copie Cachée (BCC) :',
+	'facteur_cc' => 'Copie (CC) :',
+	'facteur_copies' => 'Copies :',
+	'facteur_copies_descriptif' => 'Un email sera envoyé en copie aux adresses définies. Une seule adresse en copie et/ou une seule adresse en copie cachée.',
+	'facteur_filtre_accents' => 'Transformer les accents en leur entités html (utile pour Hotmail notamment).',
+	'facteur_filtre_css' => 'Transformer les styles contenus entre <head> et </head> en des styles "en ligne", utile pour les webmails car les styles en ligne ont la priorité sur les styles externes.',
+	'facteur_filtre_images' => 'Embarquer les images référencées dans les emails',
 	'facteur_filtre_iso_8859' => 'Convertir en ISO-8859-1',
 	'facteur_filtres' => 'Filtres',
-	'facteur_filtres_descriptif' => 'Des filtres peuvent &#0234;tre appliqu&#0233;s aux emails au moment de l&#8217;envoi.',
-	'facteur_smtp_auth' => 'Requiert une authentification&#160;:',
+	'facteur_filtres_descriptif' => 'Des filtres peuvent être appliqués aux emails au moment de l’envoi.',
+	'facteur_smtp_auth' => 'Requiert une authentification :',
 	'facteur_smtp_auth_non' => 'non',
 	'facteur_smtp_auth_oui' => 'oui',
-	'facteur_smtp_host' => 'H&#0244;te&#160;:',
-	'facteur_smtp_password' => 'Mot de passe&#160;:',
-	'facteur_smtp_port' => 'Port&#160;:',
-	'facteur_smtp_secure' => 'Connexion s&#0233;curis&#0233;e&#160;:',
+	'facteur_smtp_host' => 'Hôte :',
+	'facteur_smtp_password' => 'Mot de passe :',
+	'facteur_smtp_port' => 'Port :',
+	'facteur_smtp_secure' => 'Connexion sécurisée :',
 	'facteur_smtp_secure_non' => 'non',
 	'facteur_smtp_secure_ssl' => 'SSL',
 	'facteur_smtp_secure_tls' => 'TLS',
 	'facteur_smtp_sender' => 'Retour des erreurs (optionnel)',
-	'facteur_smtp_sender_descriptif' => 'D&#0233;finit dans l&#8217;ent&#0234;te du mail l&#8217;adresse email de retour des erreurs (ou Return-Path), et lors d&#8217;un envoi via la m&#0233;thode SMTP cela d&#0233;finit aussi l&#8217;adresse de l&#8217;envoyeur.',
-	'facteur_smtp_username' => 'Nom d&#8217;utilisateur&#160;:',
+	'facteur_smtp_sender_descriptif' => 'Définit dans l’entête du mail l’adresse email de retour des erreurs (ou Return-Path), et lors d’un envoi via la méthode SMTP cela définit aussi l’adresse de l’envoyeur.',
+	'facteur_smtp_username' => 'Nom d’utilisateur :',
 
 	// M
-	'message_identite_email' => 'La configuration du plugin "facteur" surcharge cette adresse email pour l&#8217;envoi de courriels.',
+	'message_identite_email' => 'La configuration du plugin "facteur" surcharge cette adresse email pour l’envoi de courriels.',
 
 	// N
-	'note_test_configuration' => 'Un email sera envoy&#0233; &#0224; l&#8217;adresse d&#8217;envoi d&#0233;finie (ou celle du webmaster).',
+	'note_test_configuration' => 'Un email sera envoyé à l’adresse d’envoi définie (ou celle du webmaster).',
 
 	// P
-	'personnaliser' => 'Personnaliser ces r&#0233;glages',
+	'personnaliser' => 'Personnaliser ces réglages',
 
 	// T
 	'tester' => 'Tester',
@@ -65,7 +65,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// U
 	'utiliser_mail' => 'Utiliser la fonction mail de PHP',
-	'utiliser_reglages_site' => 'Utiliser les r&#0233;glages du site SPIP&#160;: le nom affich&#0233; sera le nom du site SPIP et l&#8217;adresse email sera celle du webmaster',
+	'utiliser_reglages_site' => 'Utiliser les réglages du site SPIP : le nom affiché sera le nom du site SPIP et l’adresse email sera celle du webmaster',
 	'utiliser_smtp' => 'Utiliser SMTP',
 
 	// V
