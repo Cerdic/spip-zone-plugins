1dd21f5225c2fcd870ff0e5f1c66cc1fbffc3af7
gilles.vincent@gmail.com <gilles.vincent@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-12-06 11:14:30 +0000

    Utilisons des textes encod&#233;s correctement. -- conversion via xml2asc de HTML-XML-utils
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@86509 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- crayons/lang/crayons_fr.php
+++ crayons/lang/crayons_fr.php
@@ -6,81 +6,81 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
-	'absence_cle_primaire' => 'Pas de clé primaire !',
-	'activation_barre_impossible' => 'Un autre plugin interdit l’usage du porte-plume dans l’espace public.',
+	'absence_cle_primaire' => 'Pas de cl&#233; primaire&#160;!',
+	'activation_barre_impossible' => 'Un autre plugin interdit l&#8217;usage du porte-plume dans l&#8217;espace public.',
 	'annuler' => 'Annuler',
 
 	// B
 	'bouton_supprimer' => 'Supprimer',
 
 	// D
-	'deja_modifie' => 'Déjà modifié',
-	'donnees_mal_formatees' => 'Données mal formatées',
+	'deja_modifie' => 'D&#233;j&#224; modifi&#233;',
+	'donnees_mal_formatees' => 'Donn&#233;es mal format&#233;es',
 
 	// E
-	'editer' => 'Éditer',
-	'editer_@type@_@id@' => 'Éditer @type@ @id@ en entier',
-	'editer_tout' => 'Éditer tout',
+	'editer' => '&#201;diter',
+	'editer_@type@_@id@' => '&#201;diter @type@ @id@ en entier',
+	'editer_tout' => '&#201;diter tout',
 	'effets' => 'Effets',
-	'explication_exec_autorise' => 'Pages autorisées à la modification
-        (?exec=...) : (, pour séparer les exec - * pour toutes).',
-	'explication_introduction_article' => 'L’introduction d’un article affiche le descriptif de l’article,
-    sinon affiche ses 600 premiers caractères (chapeau puis texte).',
-	'explication_reduire_logo' => 'Réduire le logo s’il dépasse
-        la taille maximale indiquée en pixels. Exemple : 100. (0 = jamais)',
+	'explication_exec_autorise' => 'Pages autoris&#233;es &#224; la modification
+        (?exec=...)&#160;: (, pour s&#233;parer les exec - * pour toutes).',
+	'explication_introduction_article' => 'L&#8217;introduction d&#8217;un article affiche le descriptif de l&#8217;article,
+    sinon affiche ses 600 premiers caract&#232;res (chapeau puis texte).',
+	'explication_reduire_logo' => 'R&#233;duire le logo s&#8217;il d&#233;passe
+        la taille maximale indiqu&#233;e en pixels. Exemple&#160;: 100. (0 = jamais)',
 
 	// I
-	'info_experimental' => 'Expérimental',
+	'info_experimental' => 'Exp&#233;rimental',
 
 	// L
-	'label_activer_barre_typo' => 'Activer la barre typographique dans les crayons ?',
-	'label_activer_crayons_prive' => 'Activer crayons pour la partie privée.',
+	'label_activer_barre_typo' => 'Activer la barre typographique dans les crayons&#160;?',
+	'label_activer_crayons_prive' => 'Activer crayons pour la partie priv&#233;e.',
 	'label_barre_typo' => 'Barre typographique',
 	'label_clickhide' => 'Un clic en-dehors masque les crayons',
 	'label_effets' => 'Choix des effets',
-	'label_espaceprive' => 'Crayons dans le privé ?',
-	'label_exec_autorise' => 'Pages autorisées',
+	'label_espaceprive' => 'Crayons dans le priv&#233;&#160;?',
+	'label_exec_autorise' => 'Pages autoris&#233;es',
 	'label_filet' => 'Filet',
 	'label_message' => 'Obtenir un message',
 	'label_msg_abandon' => 'Sur abandon',
 	'label_msg_no_change' => 'Si pas de changement',
-	'label_reduire_logo' => 'Réduire les logos',
-	'label_upload_document' => 'Autoriser l’ajout de documents par glisser/déposer sur le crayon article.texte',
+	'label_reduire_logo' => 'R&#233;duire les logos',
+	'label_upload_document' => 'Autoriser l&#8217;ajout de documents par glisser/d&#233;poser sur le crayon article.texte',
 	'label_yellow_fade' => 'Fade',
 	'legend_controleur_fichier_remplacer' => 'Remplacer ce document "@extension@"',
 	'legend_controleur_hyperlien' => 'Hyperlien',
-	'legend_controleur_logo' => 'Choisir un logo :',
+	'legend_controleur_logo' => 'Choisir un logo&#160;:',
 	'legend_controleur_logo_remplacer' => 'Remplacer ce logo',
-	'legend_controleur_qui' => 'Qui ?',
-	'legend_controleur_vignette' => 'Choisir une vignette :',
+	'legend_controleur_qui' => 'Qui&#160;?',
+	'legend_controleur_vignette' => 'Choisir une vignette&#160;:',
 	'legend_controleur_vignette_remplacer' => 'Remplacer cette vignette',
 	'legend_documents' => 'Upload de documents',
-	'legend_editer_prive' => 'Éditer l’espace privé (expérimental)',
-	'legend_introduction_article' => 'Texte introductif de l’article',
+	'legend_editer_prive' => '&#201;diter l&#8217;espace priv&#233; (exp&#233;rimental)',
+	'legend_introduction_article' => 'Texte introductif de l&#8217;article',
 
 	// M
 	'message_image_obligatoire' => 'Il faut choisir une image',
 	'messages' => 'Messages',
-	'modifie_par_ailleurs' => 'Modifié par ailleurs',
+	'modifie_par_ailleurs' => 'Modifi&#233; par ailleurs',
 
 	// N
-	'non_autorise' => 'Non autorisé',
-	'non_implemente' => 'Non implémenté',
+	'non_autorise' => 'Non autoris&#233;',
+	'non_implemente' => 'Non impl&#233;ment&#233;',
 
 	// P
 	'pas_de_modification' => 'Pas de modification',
 	'pas_de_valeur' => 'Pas de valeur',
 
 	// S
-	'sauvegarder' => 'Sauvegarder les modifications ?',
-	'svp_copier_coller' => 'Désolé, copiez/collez vos changements et recommencez',
+	'sauvegarder' => 'Sauvegarder les modifications&#160;?',
+	'svp_copier_coller' => 'D&#233;sol&#233;, copiez/collez vos changements et recommencez',
 
 	// T
 	'titre_config_crayons' => 'Configurer les crayons',
 	'titre_crayons' => 'Crayons',
 
 	// U
-	'update_impossible' => 'La mise à jour a échoué.',
+	'update_impossible' => 'La mise &#224; jour a &#233;chou&#233;.',
 
 	// V
 	'veuillez_patienter' => 'Veuillez patienter...'
