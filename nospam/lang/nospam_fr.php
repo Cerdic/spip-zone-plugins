77eb3536bcdefbd557d1c45951067cc9a0ddabbe
gilles.vincent@gmail.com <gilles.vincent@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-12-06 11:14:42 +0000

    Utilisons des textes encod&#233;s correctement. -- conversion via xml2asc de HTML-XML-utils
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@86512 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- nospam/lang/nospam_fr.php
+++ nospam/lang/nospam_fr.php
@@ -6,22 +6,22 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// E
-	'erreur_attributs_html_interdits' => 'Il n’est pas permis d’insérer des images ni d’utiliser les attributs html <tt>class</tt> ou <tt>style</tt>',
-	'erreur_blacklist' => 'Vous avez été mis sur liste noire, vous ne pouvez plus poster de message.',
-	'erreur_jeton' => 'Impossible de prendre en compte votre message. Merci de le soumettre à nouveau !',
-	'erreur_spam' => 'Impossible de prendre en compte votre message !',
-	'erreur_spam_doublon' => 'Un message identique existe déjà !',
-	'erreur_spam_ip' => 'Trop de commentaires nuit à la qualité !',
-	'erreur_url_deja_spammee' => 'Ce message contient des liens suspects qui ressemblent à du SPAM. Merci de les retirer.',
+	'erreur_attributs_html_interdits' => 'Il n&#8217;est pas permis d&#8217;ins&#233;rer des images ni d&#8217;utiliser les attributs html <tt>class</tt> ou <tt>style</tt>',
+	'erreur_blacklist' => 'Vous avez &#233;t&#233; mis sur liste noire, vous ne pouvez plus poster de message.',
+	'erreur_jeton' => 'Impossible de prendre en compte votre message. Merci de le soumettre &#224; nouveau&#160;!',
+	'erreur_spam' => 'Impossible de prendre en compte votre message&#160;!',
+	'erreur_spam_doublon' => 'Un message identique existe d&#233;j&#224;&#160;!',
+	'erreur_spam_ip' => 'Trop de commentaires nuit &#224; la qualit&#233;&#160;!',
+	'erreur_url_deja_spammee' => 'Ce message contient des liens suspects qui ressemblent &#224; du SPAM.&#160;Merci de les retirer.',
 
 	// F
-	'forum_saisie_texte_info' => 'Pour la mise en forme de votre message, ce formulaire n’accepte que les raccourcis SPIP <code>[-&gt;url] {{gras}} {italique} &lt;quote&gt; &lt;code&gt;</code> &lt;cadre&gt;</cadre> et le code HTML <code>&lt;q&gt; &lt;del&gt; &lt;ins&gt;</code>. Pour créer des paragraphes, laissez simplement des lignes vides.',
+	'forum_saisie_texte_info' => 'Pour la mise en forme de votre message, ce formulaire n&#8217;accepte que les raccourcis&#160;SPIP <code>[-&gt;url] {{gras}} {italique} &lt;quote&gt; &lt;code&gt;</code> &lt;cadre&gt;</cadre> et le code&#160;HTML <code>&lt;q&gt; &lt;del&gt; &lt;ins&gt;</code>. Pour cr&#233;er des paragraphes, laissez simplement des lignes vides.',
 
 	// I
-	'info_ip_suspecte' => 'Votre adresse IP est suspectée de procéder à du SPAM. Merci de confirmer votre bonne intention.',
+	'info_ip_suspecte' => 'Votre adresse IP est suspect&#233;e de proc&#233;der &#224; du SPAM.&#160;Merci de confirmer votre bonne intention.',
 
 	// L
-	'label_message_licite' => 'Ceci n’est pas un message abusif'
+	'label_message_licite' => 'Ceci n&#8217;est pas un message abusif'
 );
 
 ?>
