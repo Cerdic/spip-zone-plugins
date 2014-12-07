c06b0cc948830ddd9dd67a9709015ac1abb279e1
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-12-07 10:49:33 +0000

    110px de haut pour le player soundcloud sinon c'est trop juste
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@86528 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- oembed/oembed/input/pretraite_soundcloud_com.php
+++ oembed/oembed/input/pretraite_soundcloud_com.php
@@ -10,7 +10,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 function oembed_input_pretraite_soundcloud_com_dist($data_url,$args){
 
 	if (!isset($args['maxheight']) OR !$args['maxheight'])
-		$data_url = parametre_url($data_url,"maxheight","81","&");
+		$data_url = parametre_url($data_url,"maxheight","110","&");
 
 	return $data_url;
 }
