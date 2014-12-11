cc783bd8e95036f19e9da61f2b7f34858975fb23
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-12-11 22:13:56 +0000

    [Salvatore] [source:_plugins_/timezone/lang/ timezone] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@86667 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- timezone/lang/timezone_fr.php
+++ timezone/lang/timezone_fr.php
@@ -1,13 +1,12 @@
 <?php
-
-// This is a SPIP language file  --  Ceci est un fichier langue de SPIP nommé admin_lang genere le NOW()
-// langue / language = fr
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/timezone/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// L
-	'label_choisir_timezone' => 'Choisir le fuseau horaire du site',
-
+	'label_choisir_timezone' => 'Choisir le fuseau horaire du site'
 );
 
 ?>
