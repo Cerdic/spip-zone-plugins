d8410852f721a77f43bbdfd7785f3d8f973f2853
rporto@calepino.com <rporto@calepino.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-12-11 22:08:40 +0000

    [Salvatore] [source:_plugins_/acces_restreint/trunk/lang/ accesrestreint] Export depuis http://trad.spip.net de la langue pt_br
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@86632 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- acces_restreint/trunk/lang/accesrestreint_pt_br.php
+++ acces_restreint/trunk/lang/accesrestreint_pt_br.php
@@ -9,41 +9,57 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
 	'ajouter_droits_auteur' => 'Conceder-me direitos de acesso a esta área',
-	'aucune_zone' => 'Nenhuma zona',
-	'auteurs' => 'autores', # MODIF
+	'aucune_zone' => 'Nenhuma área',
+	'auteur' => '@nb@ autor',
+	'auteurs' => '@nb@ autores',
 
 	// B
+	'bouton_configurer_acces' => 'Configuração dos acessos .htaccess',
 	'bouton_creer_la_zone' => 'Criar a nova área',
 
 	// C
-	'colonne_id' => 'Nº.',
-	'confirmer_ajouter_auteurs' => 'Você quer realmente incluir este autor na zona?',
-	'confirmer_retirer_auteur_zone' => 'Você quer mesmo retirar este autor da zona?',
-	'confirmer_retirer_auteurs' => 'Voc6e quer mesmo retirar todos os autores da zona?',
-	'confirmer_supprimer_zone' => 'Você quer realmente excluir esta zona?',
+	'colonne_id' => 'Nº',
+	'confirmer_ajouter_auteurs' => 'Você quer realmente incluir este autor na área?',
+	'confirmer_retirer_auteur_zone' => 'Você quer mesmo retirar este autor da área?',
+	'confirmer_retirer_auteurs' => 'Você quer mesmo retirar todos os autores da área?',
+	'confirmer_retirer_rubrique_zone' => 'Tem certeza de que quer excluir esta seção desta área?',
+	'confirmer_supprimer_zone' => 'Você quer realmente excluir esta área?',
 	'creer_zone' => 'Criar uma nova área',
 
 	// D
 	'descriptif' => 'Descrição rápida',
 
+	// E
+	'explication_creer_htaccess' => 'Esta opção impede a leitura dos documentos anexados se o texto ao qual estão vinculados não estiver publicado',
+
 	// I
 	'icone_menu_config' => 'Acesso Restrito',
 	'icone_supprimer_zone' => 'Excluir está área',
+	'info_1_zone' => '1 área',
 	'info_acces_restreint' => 'O acesso a esta página é restrito. Identifique-se para acessá-la',
 	'info_ajouter_auteur' => 'Incluir este autor',
 	'info_ajouter_auteurs' => 'Incluir todos os autores',
 	'info_ajouter_zones' => 'Incluir todas as áreas',
 	'info_aucun_acces' => 'Nenhum acesso permitido',
-	'info_aucun_auteur' => 'Nenhum autor na zona',
-	'info_aucune_zone' => 'Nenhuma zona',
+	'info_aucun_auteur' => 'Nenhum autor na área',
+	'info_aucune_zone' => 'Nenhuma área',
 	'info_auteurs_lies_zone' => 'Os autores com acesso a esta área',
+	'info_lien_action_proteger' => 'Proteger o acesso a esta seção.',
+	'info_nb_zones' => '@nb@ áreas',
 	'info_page' => 'esta página permite-lhe gerar as áreas de acesso restrito do seu site',
 	'info_retirer_auteurs' => 'Excluir todos os autores',
 	'info_retirer_zone' => 'Retirar da área',
 	'info_retirer_zones' => 'retirar de todas as áreas',
+	'info_rubrique_dans_zone' => 'Esta seção faz arte da área:',
+	'info_rubrique_dans_zones' => 'Esta seção faz parte das áreas:',
+
+	// L
+	'label_creer_htaccess' => 'Acesso aos documentos anexados pelo seu URL',
+	'label_creer_htaccess_non' => 'autorizar a leitura',
+	'label_creer_htaccess_oui' => 'restringir a leitura',
 
 	// M
-	'modifier_zone' => 'Alterar a zona',
+	'modifier_zone' => 'Alterar a área',
 
 	// P
 	'page_zones_acces' => 'Acesso Restrito',
@@ -52,18 +68,24 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'publique' => 'Publicado',
 
 	// R
-	'rubriques' => 'seções', # MODIF
+	'rubrique' => '@nb@ seção',
+	'rubriques' => '@nb@ seções',
 	'rubriques_zones_acces' => 'Seções da área',
 
 	// S
 	'selectionner_une_zone' => 'Selecionar uma área',
 
 	// T
+	'texte_ajouter_zone' => 'Incluir uma área',
+	'texte_creer_associer_zone' => 'Criar e associar uma área',
 	'titre' => 'Título',
 	'titre_ajouter_zone' => 'Aderir à área',
-	'titre_cadre_modifier_zone' => 'Alterar uma zona',
+	'titre_boite_protegee_non' => 'Acesso livre',
+	'titre_boite_protegee_oui' => 'Acesso protegido',
+	'titre_cadre_modifier_zone' => 'Alterar uma área',
 	'titre_page_config' => 'Configuração dos acessos',
 	'titre_table' => 'Todas as áreas de acesso',
+	'titre_zone_acces' => 'Área de acesso restrito',
 	'titre_zones_acces' => 'Áreas de acesso restrito',
 	'toutes' => 'Todas',
 
