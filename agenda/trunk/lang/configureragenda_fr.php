5790db5b9dc36f1cb8c617e88c2c59c853aa9c71
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-12-11 22:08:56 +0000

    [Salvatore] [source:_plugins_/agenda/trunk/lang/ configureragenda] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@86634 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- agenda/trunk/lang/configureragenda_fr.php
+++ agenda/trunk/lang/configureragenda_fr.php
@@ -5,26 +5,27 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-
-	'titre_configuration' => 'Affichage de l\'Agenda',
-	'label_titre' => 'Titre de la page',
-	'label_descriptif' => 'Descriptif',
-	'label_insert_head_css_1' => 'Insérer automatiquement les styles par défaut de l\'agenda',
-	'label_url_evenement' => 'Affichage d\'un événement',
-	'label_url_evenement_article' => 'sur la page de l\'article associé',
-	'label_url_evenement_evenement' => 'sur une page dédiée pour chaque événement',
-	'label_affichage_duree' => 'Lister les événements sur',
+	// L
 	'label_affichage_debut' => 'Début de la liste',
 	'label_affichage_debut_date_jour' => 'Date du jour',
 	'label_affichage_debut_date_veille' => 'Date de la veille',
+	'label_affichage_debut_debut_mois' => 'Début du mois',
+	'label_affichage_debut_debut_mois_1' => 'Début d’année',
+	'label_affichage_debut_debut_mois_prec' => 'Début du mois précédent',
 	'label_affichage_debut_debut_semaine' => 'Début de la semaine',
 	'label_affichage_debut_debut_semaine_prec' => 'Début de la semaine précédente',
-	'label_affichage_debut_debut_mois' => 'Début du mois',
-	'label_affichage_debut_debut_mois_prec' => 'Début du mois précédent',
-	'label_affichage_debut_debut_mois_1' => 'Début d\'année',
 	'label_affichage_debut_mois_passe' => '@mois@ précédent',
+	'label_affichage_duree' => 'Lister les événements sur',
+	'label_descriptif' => 'Descriptif',
+	'label_insert_head_css_1' => 'Insérer automatiquement les styles par défaut de l’agenda',
+	'label_titre' => 'Titre de la page',
+	'label_url_evenement' => 'Affichage d’un événement',
+	'label_url_evenement_article' => 'sur la page de l’article associé',
+	'label_url_evenement_evenement' => 'sur une page dédiée pour chaque événement',
+	'legend_presentation_agenda' => 'Présentation de l’agenda',
 
-	'legend_presentation_agenda' => 'Présentation de l\'agenda',
+	// T
+	'titre_configuration' => 'Affichage de l’Agenda'
 );
 
 ?>
