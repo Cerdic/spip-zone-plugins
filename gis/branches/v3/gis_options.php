492d40dea7b605a4899014c29f85da69201aaf5d
brunobergot@gmail.com <brunobergot@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-12-11 11:01:01 +0000

    oups
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@86581 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- gis/branches/v3/gis_options.php
+++ gis/branches/v3/gis_options.php
@@ -2,7 +2,7 @@
 
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
-define('_DIR_LIB_GIS','lib/mxn-gis-2.4.8/');
+define('_DIR_LIB_GIS','lib/mxn-gis-2.4.11/');
 
 $GLOBALS['logo_libelles']['id_gis'] = _T('gis:libelle_logo_gis');
 
