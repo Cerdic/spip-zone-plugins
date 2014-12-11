45db13d9e5e4be2a61570bb2f6f829ec97f5fb22
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-12-11 22:10:52 +0000

    [Salvatore] [source:_plugins_/depublie/lang/ depublie] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@86647 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- depublie/lang/depublie_fr.php
+++ depublie/lang/depublie_fr.php
@@ -11,20 +11,19 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'configurer_rubrique_depublie' => 'N’activer la dépublication que sur les rubriques et secteurs suivants (séparer les identifiants par des virgules).',
 	'configurer_statut_depublie' => 'Choisissez le statut à donner lors de la dépublication',
 
-	//D
+	// D
 	'date_depublie' => 'Change le',
-	
+
 	// E
 	'erreur_date_superieure' => 'La date de dépublication doit être supérieure à la date de publication',
 
-	//I
+	// I
 	'icone_configurer_depublie' => 'Configurer les dépublications',
 	'icone_voir_depublie' => 'Liste des dépublications',
-	'info_objet' => 'Objet',
 	'info_1_objet' => 'Un objet',
 	'info_nb_objets' => '@nb@ objets',
-	
-	
+	'info_objet' => 'Objet',
+
 	// L
 	'label_jour' => 'Jours',
 	'label_mois' => 'Mois',
@@ -34,14 +33,14 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'label_secteur_depublie' => 'Identifiants de secteur(s)',
 	'label_statut_depublie' => 'Statut',
 
-	//S
+	// S
 	'statut_futur' => 'Statut',
-	
+
 	// T
 	'texte_date_depublication' => 'Date de dépublication en ligne :',
 	'texte_date_depublication_nonaffichee' => 'Ne pas utiliser la date de dépublication',
 	'titre_page_configurer_depublies' => 'Configurer les dépublications',
-	'titre_page_depublies_objets' => 'Les objets avec un changement de statut programmé',
+	'titre_page_depublies_objets' => 'Les objets avec un changement de statut programmé'
 );
 
 ?>
