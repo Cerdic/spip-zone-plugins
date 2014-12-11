a60917143d56ad5c7d16be2edd0034472bcb83ed
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-12-11 22:13:36 +0000

    [Salvatore] [source:_plugins_/skeleditor/trunk/lang/ skeleditor] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@86665 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- skeleditor/trunk/lang/skeleditor_de.php
+++ skeleditor/trunk/lang/skeleditor_de.php
@@ -1,95 +1,93 @@
 <?php
-
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/skeleditor?lang_cible=de
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+
 	// A
-	'action_supprimer'=>"Löschen",
-  'action_download'=>"Herunterladen",
-	'action_nouveau'=>'Neue Datei',
-	'action_upload'=>'Hochladen',
+	'action_download' => 'Herunterladen',
+	'action_nouveau' => 'Neue Datei',
+	'action_supprimer' => 'Löschen',
+	'action_upload' => 'Hochladen',
 
-  // C
-  'creer'=>'Anlegen',
-
+	// C
 	'copy_comment' => '  Kopie vom @date@ durch @nom@
   Originaldatei @source@',
-  
-  // E  
-  'editer_skel'=>'Skelett bearbeiten',
-  'editer_skel_debug' =>'Skelett bearbeiten (Debugmodus)',
-  'effacer_confirme'=>'Wollen sie diese Datei wirklich löschen ?',
-	'erreur_fichier_supprime' => 'Diese Datei wurde gelöscht, während sie an ihr gearbeitet haben. Inhalt kopieren und falls erforderlich neue Datei anlegen.',
+	'creer' => 'Anlegen',
+
+	// E
+	'editer_skel' => 'Skelett bearbeiten',
+	'editer_skel_debug' => 'Skelett bearbeiten (Debugmodus)',
+	'effacer_confirme' => 'Wollen sie diese Datei wirklich löschen ?',
+	'erreur_creation_sous_dossier' => 'Das Unterverzeichnis @dir@ kann nicht angelegt werden.',
+	'erreur_droits' => 'Fehler: Datei kann nicht angelegt werden. Überprüfen sie die Zugriffsrechte für ihr Skelett-Verzeichnis.',
+	'erreur_ecrire_dans_sous_dossier' => 'In das Unterverzeichnis @dir@ kann  nicht geschrieben werden.',
+	'erreur_ecriture_fichier' => 'Datei kann nicht gespeichert werden.',
+	'erreur_edition_ecriture' => 'Fehler: Datei kann nicht geändert werden.',
+	'erreur_fichier_inexistant' => 'Diese Datei existiert nicht',
 	'erreur_fichier_modif_coucourante' => 'Diese Datei wurde geändert, während sie an ihr gearbeitet haben. Dies ist die neue Version:',
 	'erreur_fichier_modif_coucourante_votre_version' => 'Mit ihrer Version zusammenführen und speichern:',
-
-	'erreur_overwrite'=>"Unmöglich, diese Datei existiert bereits",
+	'erreur_fichier_supprime' => 'Diese Datei wurde gelöscht, während sie an ihr gearbeitet haben. Inhalt kopieren und falls erforderlich neue Datei anlegen.',
+	'erreur_ouvert_ecrit' => 'Fehler: Datei kann nicht geöffnet oder geändert werden.',
+	'erreur_ouverture_fichier' => 'Fehler: Datei kann nicht geöffnet werden.',
+	'erreur_overwrite' => 'Unmöglich, diese Datei existiert bereits',
+	'erreur_parsing' => 'Schwerer Fehler bei der Analyse der Schleifen (boucles)',
+	'erreur_sansgene' => 'Fehler: Vorgang nicht zulässig',
 	'erreur_type_interdit' => 'Dieser Dateityp ist nicht zulässig',
-	'erreur_fichier_inexistant' => 'Diese Datei existiert nicht',
-	'erreur_creation_sous_dossier' => 'Das Unterverzeichnis @dir@ kann nicht angelegt werden.',
-	'erreur_ecrire_dans_sous_dossier' => 'In das Unterverzeichnis @dir@ kann  nicht geschrieben werden.',
-
-	'erreur_ouverture_fichier'=>"Fehler: Datei kann nicht geöffnet werden.",
-  'erreur_ecriture_fichier'=>"Datei kann nicht gespeichert werden.",
-  'erreur_edition_ecriture'=>"Fehler: Datei kann nicht geändert werden.",
-  'erreur_ouvert_ecrit'=>"Fehler: Datei kann nicht geöffnet oder geändert werden.",
-  'erreur_droits'=>"Fehler: Datei kann nicht angelegt werden. Überprüfen sie die Zugriffsrechte für ihr Skelett-Verzeichnis.",
-  'erreur_sansgene'=>'Fehler: Vorgang nicht zulässig',
-  'erreur_parsing'=>'Schwerer Fehler bei der Analyse der Schleifen (boucles)',
-  
-	'explications_filename_fichier_chemin' => 'Sie können einen vollständigen Pfad angeben, um die Datei in einem Unterverzeichnis zu speichern (z.B. "modeles/dokument.html").',
 	'explications_creer_dossier_squelettes' => 'Zum EInsatz des Skelett-Editors legen sie ein Verzeichnis<b>squelettes/</b> im Wurzelverzeichnis ihrer Website an, falls es nicht bereits existiert, und ordnen sie ihm Schreibrechte zu.',
-  // F
-  'fichier'=>"Datei:",
-  'fichier_enregistre'=>"Datei wurde gespeichert",
-	
-  'fichier_sauvegarde_date'=>"Datei gesichert @ ",    
-  'fichier_efface_ok'=>'Datei gelöscht!',
-  'fichier_choix'=>'Wählen sie die Datei zum Anzeigen oder Bearbeiten.',
-  'fichier_upload'=>'Datei hochladen',
-  'fichier_upload_ok'=>'Datei erfolgreich hochgeladen!',
+	'explications_filename_fichier_chemin' => 'Sie können einen vollständigen Pfad angeben, um die Datei in einem Unterverzeichnis zu speichern (z.B. "modeles/dokument.html").',
 
-	'info_copie'=>'Beim Speichern wird die Datei in das Verzeichnis <em>@dir@</em> geschrieben.',
+	// F
+	'fichier' => 'Datei:',
+	'fichier_choix' => 'Wählen sie die Datei zum Anzeigen oder Bearbeiten.',
+	'fichier_efface_ok' => 'Datei gelöscht!',
+	'fichier_enregistre' => 'Datei wurde gespeichert',
+	'fichier_sauvegarde_date' => 'Datei gesichert @ ',
+	'fichier_upload' => 'Datei hochladen',
+	'fichier_upload_ok' => 'Datei erfolgreich hochgeladen!',
+
+	// I
+	'info_copie' => 'Beim Speichern wird die Datei in das Verzeichnis <em>@dir@</em> geschrieben.',
 
 	// L
-	'label_creer_fichier' => 'Datei anlegen',
 	'label_code' => 'Inhalt',
+	'label_creer_fichier' => 'Datei anlegen',
 	'label_file' => 'Datei',
 	'label_filename_squelette' => 'Name',
-	'label_renommer_fichier' => 'Umbenennen oder verschieben',
 	'label_nouveau_fichier' => 'Neue Datei',
+	'label_renommer_fichier' => 'Umbenennen oder verschieben',
 
-  // P
-  'parseur_titre' => 'Schleifentester',
-  'parseur_contenu' => 'Inahlt :',
-  'parseur_avant' => 'Davor',
-  'parseur_milieu' => 'Mitte',
-  'parseur_apres' => 'Danach',
-  'parseur_altern' => 'Alternative',
-  'parseur_param' => 'Parameter:',
-  'parseur_horsboucle' => 'AUSSERHALB DER SCHLEIFE',
- 
-  
-  // R 
-  'rechercher' => 'Suchen',
-  'rechercher_ou_remplacer' => 'oder',
-  'remplacer' => 'ersetzen',
-  'remplacer_par' => 'durch',  
-  'repertoire' => 'Verzeichnis',
+	// P
+	'parseur_altern' => 'Alternative',
+	'parseur_apres' => 'Danach',
+	'parseur_avant' => 'Davor',
+	'parseur_contenu' => 'Inahlt :',
+	'parseur_horsboucle' => 'AUSSERHALB DER SCHLEIFE',
+	'parseur_milieu' => 'Mitte',
+	'parseur_param' => 'Parameter:',
+	'parseur_titre' => 'Schleifentester',
 
-  
-  // S
-	'squelettes' => 'CSS&amp;Skelette',
-  'sauver'=>'Sauver', 
-  'skeleditor_description'=>"Dateien des aktuellen Skeletts können bearbeitet werden.",
-  'skeleditor_dossier'=>"Skelett-Verzeichnis: ",
-  
-  // T
-  'target'=>'Position',
-  
-  // U
-  'upload'=>'Uploader',  
+	// R
+	'rechercher' => 'Suchen',
+	'rechercher_ou_remplacer' => 'oder',
+	'remplacer' => 'ersetzen',
+	'remplacer_par' => 'durch',
+	'repertoire' => 'Verzeichnis',
+
+	// S
+	'sauver' => 'Sauver',
+	'skeleditor_description' => 'Dateien des aktuellen Skeletts können bearbeitet werden.',
+	'skeleditor_dossier' => 'Skelett-Verzeichnis: ',
+	'squelettes' => 'CSS&Skelette',
+
+	// T
+	'target' => 'Position',
+
+	// U
+	'upload' => 'Uploader'
 );
-
 
 ?>
--- skeleditor/trunk/lang/skeleditor_en.php
+++ skeleditor/trunk/lang/skeleditor_en.php
@@ -1,63 +1,63 @@
 <?php
-
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/skeleditor?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-  // C
-  'creer'=>'Create', 
-  
-  // E  
-  'editer_skel'=>'Edit the template',
-  'editer_skel_debug' =>'Edit the template (debug mode)',
-  'effacer'=>"delete",
-  'effacer_confirme'=>'Do you really want to delete this file ?',
-  'erreur_ouverture_fichier'=>"error: can not open the file",
-  'erreur_ecriture_fichier'=>"error: can write the file",
-  'erreur_edition_ecriture'=>"error: not writable file",
-  'erreur_ouvert_ecrit'=>"error: can not open or edit this file",
-  'erreur_droits'=>"error: can not create the file. please check the template direction rights",
-  'erreur_overwrite'=>"error: this file already exists",
-  'erreur_sansgene'=>'Error: unauthorized action',
-  'erreur_parsing'=>'Fatal error while parsing the BOUCLES',
-  
-  // F
-  'fichier'=>"file",
-  'fichier_sauvegarde_date'=>"file saved @ ",    
-  'fichier_efface_ok'=>'File deleted successfully !',
-  'fichier_choix'=>'Please choose the file you want to edit or view.',
-  'fichier_nouveau'=>'New file',
-  'fichier_upload'=>'Upload a file',
-  'fichier_upload_ok'=>'File uploaded successfully !',
-  
-  // P
-  'parseur_titre' => 'Loop parser',
-  'parseur_contenu' => 'Content',
-  'parseur_avant' => 'Before',
-  'parseur_milieu' => 'Inside',
-  'parseur_apres' => 'After',
-  'parseur_altern' => 'Alternate',
-  'parseur_param' => 'Parameters:',
-  'parseur_horsboucle' => 'OUTSIDE THE LOOPS',
- 
-  
-  // R 
-  'rechercher' => 'Search',
-  'rechercher_ou_remplacer' => 'or',
-  'remplacer' => 'replace',
-  'remplacer_par' => 'it by',  
-  'repertoire'=>'directory',
-  
-  // S
-  'sauver'=>'Save', 
-  'skeleditor_description'=>"Allows to edit the files of the current template",
-  'skeleditor_dossier'=>"template directory: ",
-  
-  // T
-  'target'=>'target',
-  'telecharger'=>"download",
-  
-  // U
-  'upload'=>'Upload',  
+
+	// C
+	'creer' => 'Create',
+
+	// E
+	'editer_skel' => 'Edit the template',
+	'editer_skel_debug' => 'Edit the template (debug mode)',
+	'effacer_confirme' => 'Do you really want to delete this file ?',
+	'erreur_droits' => 'error: can not create the file. please check the template direction rights',
+	'erreur_ecriture_fichier' => 'error: can write the file',
+	'erreur_edition_ecriture' => 'error: not writable file',
+	'erreur_ouvert_ecrit' => 'error: can not open or edit this file',
+	'erreur_ouverture_fichier' => 'error: can not open the file',
+	'erreur_overwrite' => 'error: this file already exists',
+	'erreur_parsing' => 'Fatal error while parsing the BOUCLES',
+	'erreur_sansgene' => 'Error: unauthorized action',
+
+	// F
+	'fichier' => 'file',
+	'fichier_choix' => 'Please choose the file you want to edit or view.',
+	'fichier_efface_ok' => 'File deleted successfully !',
+	'fichier_sauvegarde_date' => 'file saved @ ',
+	'fichier_upload' => 'Upload a file',
+	'fichier_upload_ok' => 'File uploaded successfully !',
+
+	// P
+	'parseur_altern' => 'Alternate',
+	'parseur_apres' => 'After',
+	'parseur_avant' => 'Before',
+	'parseur_contenu' => 'Content',
+	'parseur_horsboucle' => 'OUTSIDE THE LOOPS',
+	'parseur_milieu' => 'Inside',
+	'parseur_param' => 'Parameters:',
+	'parseur_titre' => 'Loop parser',
+
+	// R
+	'rechercher' => 'Search',
+	'rechercher_ou_remplacer' => 'or',
+	'remplacer' => 'replace',
+	'remplacer_par' => 'it by',
+	'repertoire' => 'directory',
+
+	// S
+	'sauver' => 'Save',
+	'skeleditor_description' => 'Allows to edit the files of the current template',
+	'skeleditor_dossier' => 'template directory: ',
+
+	// T
+	'target' => 'target',
+
+	// U
+	'upload' => 'Upload'
 );
 
 ?>
