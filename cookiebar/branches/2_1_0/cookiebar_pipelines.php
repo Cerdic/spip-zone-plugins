23e6c8a024f5bfe08e9e379efea463ad683c53ce
erational@erational.org <erational@erational.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-12-16 16:49:08 +0000

    report z86732
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@86733 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- cookiebar/branches/2_1_0/cookiebar_pipelines.php
+++ cookiebar/branches/2_1_0/cookiebar_pipelines.php
@@ -25,6 +25,7 @@ function cookiebar_insert_head_css($flux){
  */
 function cookiebar_insert_head($flux){
   //$js_cookiebar = parametre_url(generer_url_public('jquery.cookiebar.js'), 'lang', $lang); 
+  if (!$lang) $lang = $GLOBALS["spip_lang"];
   $js_cookiebar = produire_fond_statique("jquery.cookiebar.js", array("lang"=>$lang));
 
   $flux .= 
