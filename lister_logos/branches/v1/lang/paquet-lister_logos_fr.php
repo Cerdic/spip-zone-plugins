fea6c42b4846a57707f9352bf48088dc83f7c9a5
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-12-18 22:19:36 +0000

    On copie le trunk vers la branche v1 avant modification du plugin
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@86774 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ lister_logos/branches/v1/lang/paquet-lister_logos_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// L
+	'lister_logos_description' => 'Ce plugin vous permet de lister sur une page tous les logos présents dans le répertoire "IMG/". Vous pourrez retrouver l\'objet éditorial associé au logo.',
+	'lister_logos_nom' => 'Lister les logos',
+	'lister_logos_slogan' => 'En un coup d\'oeil, tous vos logos.',
+);
+
+?>
---
+++ lister_logos/branches/v1/lister_logos_options.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Options du plugin Lister les logosau chargement
+ *
+ * @plugin     Lister les logos
+ * @copyright  2014
+ * @author     Teddy Payet
+ * @licence    GNU/GPL
+ * @package    SPIP\Lister_logos\Options
+ */
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+
+?>
