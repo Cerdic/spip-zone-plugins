594169d697c052a5d7e16c8d89446f2f33101dba
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-12-27 23:03:48 +0000

    - On passe par la fonction native de SPIP 3.0 qui donne les infos d'un objet : objet_info(, ) / Ce qui simplifie fortement l'écriture ; - On définit avec le plugin la constante _LOGO_MAX_SIZE si elle n'est pas présente dans mes_options.php ou ailleurs. Si elle n'est pas définie, on prend la valeur en ko de  ; - Up de y : 2.1.0.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@86844 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- lister_logos/trunk/lister_logos_options.php
+++ lister_logos/trunk/lister_logos_options.php
@@ -11,5 +11,14 @@
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
+if (
+    !defined('_LOGO_MAX_SIZE')
+    and ini_get('upload_max_filesize')
+) {
+    // Si on a `upload_max_filesize` renseigné dans le fichier php.ini,
+    // autant s'en servir pour la constante.
+    $upload_max_filesize = floatval(ini_get('upload_max_filesize')) * 1024;
+    define('_LOGO_MAX_SIZE', $upload_max_filesize);
+}
 
 ?>
