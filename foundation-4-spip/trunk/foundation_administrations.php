f9fa543230ce42f4d0f12d114dbd6f291bba0b60
p@henix.be <p@henix.be@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-12-30 15:45:15 +0000

    Correction d'un commentaire
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@86943 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- foundation-4-spip/trunk/foundation_administrations.php
+++ foundation-4-spip/trunk/foundation_administrations.php
@@ -21,7 +21,7 @@ function foundation_upgrade($nom_meta_base_version, $version_cible) {
     $maj = array();
 
      $config_default = array(
-        // Activer par défaut la version 4 de foundation. Désactiver le reste.
+        // Activer par défaut la version 5 de foundation. Désactiver le reste.
         'variante' => 5,
         'javascript' => '',
         'fichier_htc' => '',
