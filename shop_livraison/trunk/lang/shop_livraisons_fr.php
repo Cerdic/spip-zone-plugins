106de389a24d82cf526affbd4d835461e78c4537
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2014-12-30 04:33:39 +0000

    [Salvatore] [source:_plugins_/shop_livraison/trunk/lang/ shop_livraisons] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@86936 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- shop_livraison/trunk/lang/shop_livraisons_fr.php
+++ shop_livraison/trunk/lang/shop_livraisons_fr.php
@@ -1,20 +1,20 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/shop_livraison/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
- 
-$GLOBALS[$GLOBALS['idx_lang']] = array(
 
-	// S
-	'shop_livraison_titre' => 'Shop Livraisons',
+$GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// C
 	'cfg_exemple' => 'Exemple',
 	'cfg_exemple_explication' => 'Explication de cet exemple',
 	'cfg_titre_parametrages' => 'Paramétrages',
 
+	// S
+	'shop_livraison_titre' => 'Shop Livraisons',
+
 	// T
-	'titre_page_configurer_shop_livraison' => 'Shop Livraisons',
+	'titre_page_configurer_shop_livraison' => 'Shop Livraisons'
 );
 
 ?>
