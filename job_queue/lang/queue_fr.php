cf2f1c0e80ec25d46a4d735010adfe5ad50acce9
code@archiloque.net <code@archiloque.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-01-03 23:37:12 +0000

    job_queue : utilisation du fichier de langue pour le titre de l'écran d'admin
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@86958 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- job_queue/lang/queue_fr.php
+++ job_queue/lang/queue_fr.php
@@ -1,16 +1,14 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/_stable_/acces_restreint/lang/
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-
+	'job_queue' => 'Job Queue',
 	'executer_maintenant' => 'Executer maintenant',
 	'purger_queue' => 'Purger la liste de travaux',
 	'nb_jobs_in_queue' => '@nb@ travaux en attente',
 	'one_job_in_queue' => '1 travail en attente',
 	'next_job_in_nb_sec' => 'Prochain travail dans @nb@ s',
-
 );
 
 ?>
