91c6fbdfa4b707dcdbb40590aacc8092a7456448
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-01-05 18:32:55 +0000

    Pétouille et incrément de version
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@86990 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- contact/trunk/contact_pipelines.php
+++ contact/trunk/contact_pipelines.php
@@ -3,7 +3,7 @@
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 /**
- * Pipeline jqueryui_plugins (SPIP) pour demander au plugin l'insertion des scripts pour .sortable()
+ * Pipeline jqueryui_plugins (jQuery UI) pour demander au plugin l'insertion des scripts pour .sortable()
  *
  * @param array $plugins
  * @return array
