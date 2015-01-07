9763b91e5275bd267daf5c7d000c0ca7d42d56b9
erational@erational.org <erational@erational.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-01-07 09:54:52 +0000

    un plugin basique pour vérifier si ces plugins actifs sont compatibles SPIP3.1 (vérification uniquement sur les bornes déclarées)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@87012 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ verifie31/lang/verifie31_fr.php
@@ -0,0 +1,28 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+    return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+    // B
+    'bilan' => '@total@ plugin(s) actif(s) - @total_notok@ plugin(s) incompatible(s)',
+
+    // L
+    'liste_plugins' => 'Liste des plugins actifs',
+
+    // T
+    'titre_verifie31' => 'Vérifier la compatibilité des plugins à SPIP 3.1',
+
+    // P
+   'plugin_nom' => 'Nom du plugin', 
+   'plugin_borne' => 'Bornes',
+   'plugin_compat31' => 'Compatible SPIP 3.1',
+   'plugin_compat31_ok' => 'OK',
+   'plugin_compat31_notok' => 'Plugin incompatible ou à vérifier',
+
+);
+
+?>
