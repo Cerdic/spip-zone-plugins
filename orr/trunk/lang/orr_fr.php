e2bf254e8605e71c54c9305298bf17624de8de57
jeanmarie.listes@cousumain.info <jeanmarie.listes@cousumain.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-01-08 10:11:46 +0000

    Indentation : des tab à la place des espaces (dites moi si c'est l'inverse, je n'ai pas retrouvé le mail qui en parlait il a qqs temps)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@87033 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- orr/trunk/lang/orr_fr.php
+++ orr/trunk/lang/orr_fr.php
@@ -6,7 +6,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
-    'ajouter_lien_orr_autorisation' => 'Ajouter le lien de cette autorisation',
+	'ajouter_lien_orr_autorisation' => 'Ajouter le lien de cette autorisation',
 	'ajouter_lien_orr_reservation' => 'Ajouter cette reservation',
 	'ajouter_lien_orr_ressource' => 'Ajouter cette ressource',
 	'affichage_semaine' => 'Vue semaine',
@@ -18,13 +18,13 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'affiche' => 'Affiché',
 	'affiche_mots' => 'afficher le sélecteur de mots clé',
 	'affiche_calendrier' => 'afficher le calendrier',
-    'auteur' => 'Auteur',
-	
+	'auteur' => 'Auteur',
+
 	// C
 	'couleur' => 'Couleur',
 	'ctrl_clic' => 'Ctrl + clic pour sélection multiple',
 	'creation_autorisation_interdite' => 'Vous n\'avez pas les droits pour créer des réservations pour cette ressource',
-    'creer' => 'Creer',
+	'creer' => 'Creer',
 	// D
 	'date_de_debut' => 'Date de début',
 	'date_debut' => 'Début', 
@@ -40,19 +40,19 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'explication_orr_date_fin' => 'Date de fin de la réservation',
 	'explication_orr_reservation_nom' => 'Nom de votre réservation (ex : réunion CA)',
 	'explication_orr_ressource_nom' => 'Nom de la ressource',
-	
+
 	// F 
 	'filtrer_par_mots' => 'Sélection des ressources par mots clés',
 	
 	// G 
 	'gerer_autorisations' => 'Gérer les autorisations',
-    'grappe' => 'Grappe',
+	'grappe' => 'Grappe',
 
-    // I
-    'info_aucun_orr_autorisation' => 'Aucune autorisation',
-    'info_1_orr_autorisation' => '1 autorisation',
-    'info_nb_orr_autorisations' => '@nb@ autorisations',
-    'icone_creer_orr_autorisation' => 'Créer une autorisation',
+	// I
+	'info_aucun_orr_autorisation' => 'Aucune autorisation',
+	'info_1_orr_autorisation' => '1 autorisation',
+	'info_nb_orr_autorisations' => '@nb@ autorisations',
+	'icone_creer_orr_autorisation' => 'Créer une autorisation',
 	'icone_creer_orr_reservation' => 'Créer une reservation',
 	'icone_modifier_orr_reservation' => 'Modifier cette reservation',
 	'info_1_orr_reservation' => 'Une reservation',
@@ -67,13 +67,13 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'info_nb_orr_ressources' => '@nb@ ressources',
 	'info_orr_ressources_auteur' => 'Les ressources de cet auteur',
 	'id_autorisation_incorrecte' => 'l\'identifiant de cette autorisation est incorrect',
-	
+
 	// J
 	'jour_precedent' => 'Jour précédent',
 	'jour_suivant' => 'Jour suivant',
 
-    // L
-    'label_orr_autorisation_nom'  => 'Nom de l\'autorisation',
+	// L
+	'label_orr_autorisation_nom'  => 'Nom de l\'autorisation',
 	'label_orr_ressource_couleur' => 'Couleur de la ressource',
 	'label_orr_date_debut' => 'Date de début',
 	'label_orr_date_fin' => 'Date de fin',
@@ -86,8 +86,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'legend_heures_defaut' => 'Heures par défaut de début et fin des réservations',
 	'label_heure_debut' => 'Heure de début',
 	'label_heure_fin' => 'Heure de fin',
-	
-	
+
 	// M
 	'masque' => 'Masqué',
 	'masque_mots' => 'masquer le sélecteur de mots clé',
@@ -98,26 +97,26 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'mois_actuel' => 'Mois en cours',
 	'mois' => 'Mois',
 
-    // N
-    'nom_orr_autorisation_ressource' => 'Ressources associées',
-    'nom_de_la_reservation'=>'Nom de la Réservation',
+	// N
+	'nom_orr_autorisation_ressource' => 'Ressources associées',
+	'nom_de_la_reservation'=>'Nom de la Réservation',
 
 	// O
 	'orr_titre' => 'ORR',
-	
-    // R
-    'retirer_lien_orr_autorisation' => 'Supprimer le lien de cette autorisation',
-    'retirer_tous_liens_orr_autorisations' => 'Supprimer tous les liens des autorisations',
+
+	// R
+	'retirer_lien_orr_autorisation' => 'Supprimer le lien de cette autorisation',
+	'retirer_tous_liens_orr_autorisations' => 'Supprimer tous les liens des autorisations',
 	'retirer_lien_orr_reservation' => 'Retirer cette reservation',
 	'retirer_tous_liens_orr_reservations' => 'Retirer toutes les reservations',
 	'retirer_lien_orr_ressource' => 'Retirer cette ressource',
 	'retirer_tous_liens_orr_ressources' => 'Retirer toutes les ressources',
 	'reservation_enregistree' => 'La réservation est enregistrée',
-    'ressources' => 'Ressources',
-    'ressource_obligatoire' => 'Il faut sélectionner au moins une ressource !',
-    'ressource_active' => 'La ressource sélectionnée :',
-    'reserver' => 'Réserver',
-	
+	'ressource_obligatoire' => 'Il faut sélectionner au moins une ressource !',
+	'ressource_active' => 'La ressource sélectionnée :',
+	'ressources' => 'Ressources',
+	'reserver' => 'Réserver',
+
 	// S
 	'suppression_autorisation_interdite' => 'Vous n\'avez pas les droits pour supprimer cette autorisation',
 	'suppression_autorisation_impossible_ressources_liees' => 'Cette autorisation est liée à des ressources: vous ne pouvez pas la supprimer',
@@ -127,13 +126,13 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'semaine_precedente' => 'Semaine précédente',
 	'semaine_suivante' => 'Semaine suivante',
 	'supprimer_parametre_incorrect' => 'un des parametres transmis est incorrect pour supprimer cette ressource',
-    'seulement_si_propriétaire' => 'Autoriser la modification et la suppression seulement de ses propres réservations',
-    'statut' => 'Statut',
-    'selectionner_droit_statut' => 'Sélectionnez les droits du statut sélectionné',
+	'seulement_si_propriétaire' => 'Autoriser la modification et la suppression seulement de ses propres réservations',
+	'statut' => 'Statut',
+	'selectionner_droit_statut' => 'Sélectionnez les droits du statut sélectionné',
 
-    // T
-    'texte_ajouter_orr_autorisation' => 'Ajouter une autorisation',
-    'texte_creer_associer_orr_autorisation' => 'Créer et lier une autorisation',
+	// T
+	'texte_ajouter_orr_autorisation' => 'Ajouter une autorisation',
+	'texte_creer_associer_orr_autorisation' => 'Créer et lier une autorisation',
 	'texte_ajouter_orr_reservation' => 'Ajouter une reservation',
 	'texte_changer_statut_orr_reservation' => 'Cette reservation est :',
 	'texte_creer_associer_orr_reservation' => 'Créer et associer une reservation',
@@ -154,13 +153,13 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'titre_types_autorisations' => 'Les types d\'autorisations',
 	'type' => 'Type',
 	'titre_page_configurer' => 'Configurer ORR',
-    'type_utilisateur' => 'Quel type d\'utilisateur voulez vous ?',
+	'type_utilisateur' => 'Quel type d\'utilisateur voulez vous ?',
 
-    // V
-    'valeur_autorisation' => 'Droits',
-    'voir_autorisations' => 'Voir les autorisations',
-    'voir' => 'Voir',
-    
+	// V
+	'valeur_autorisation' => 'Droits',
+	'voir_autorisations' => 'Voir les autorisations',
+	'voir' => 'Voir',
+
 );
 
 ?>
