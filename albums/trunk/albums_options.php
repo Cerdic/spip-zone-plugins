8e7d7a05d22401f08eab3998e0c22683030a7c5f
tcharlss@hotmail.fr <tcharlss@hotmail.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-01-08 23:42:05 +0000

    Petit remaniement des modèles.
    Ajout de constantes de personnalisation pour définir les valeurs par défaut de certains paramètres.  Ça permet de gérer assez finement l'apparence par défaut des albums sans avoir à surcharger les squelettes des modèles. Dans certains cas, on peut ajouter le suffixe `_VIGNETTES` ou `_LISTE` pour cibler une varitante spécifique. Voir `albums_options.php` pour le détail.
    * `_ALBUMS_PAGINATION` : pas de pagination
    * `_ALBUMS_BALISE_TITRE` : balise encapsulant le titre
    *`_ALBUMS_POSITION_LEGENDE` : position de la légende (titre et descriptif)
    * `_ALBUMS_RECADRER` : recadrer ou non les vignettes
    * `_ALBUMS_TAILLE_PREVIEW` : taille max des vignettes (supplanté par l'option `taille maximale des vignettes` dans le menu `fonctions_avancées`)
    
    Nouveaux paramètres :
    * `position_legende` : `top` ou `bottom`, défaut = ça dépend du modèle.
    * `pagination` : nombre, défaut = 20.
    *`type_pagination` : `page | precedent_suivant | page_precedent_suivant`, défaut = `page`.
    
    Possibilité de placer la légende (titre et descriptif) au dessus de l'album, comme pour les versions antérieures, avec le paramètre `position_legende` (`top` ou `bottom`).
    
    Le titre des albums est encapsulé par défaut dans une balise `<strong>` au lieu de `<span>`. On la sort de la `<div>` dans laquelle est se trouvait, qui était superflue.
    On a donc maintenant :
    {{{
    <div class='caption'>
      <strong class='titre_album'>titre</strong>
      <div class='descriptif_album'>descriptif</div>
    </div>
    }}}
    
    Ajout de la pagination, par défaut, on affiche 20 documents. 
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@87043 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ albums/trunk/albums_options.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * options du plugin Albums
+ *
+ * @plugin     Albums
+ * @copyright  2015
+ * @author     Tetue, Charles Razack
+ * @licence    GNU/GPL
+ * @package    SPIP\Albums\Options
+ */
+
+// Sécurité
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+// Paramètres par défaut des modèles
+// Les suffixes `_VIGNETTES` et `_LISTE` pour restreindre à une variante
+
+// Dimension maximale des vignettes : nombre
+if (!defined('_ALBUMS_TAILLE_PREVIEW'))                define('_ALBUMS_TAILLE_PREVIEW',120);
+
+// Recadrer ou non les vignettes
+if (!defined('_ALBUMS_RECADRER'))                      define('_ALBUMS_RECADRER',false);
+
+// Pas de pagination (nombre de documents affichés en même temps) : nombre
+if (!defined('_ALBUMS_PAGINATION'))                    define('_ALBUMS_PAGINATION',20);
+if (!defined('_ALBUMS_PAGINATION_VIGNETTES'))          define('_ALBUMS_PAGINATION_VIGNETTES','');
+if (!defined('_ALBUMS_PAGINATION_LISTE'))              define('_ALBUMS_PAGINATION_LISTE','');
+
+// Balise dans laquelle est placé le titre : balise HTML *sans les chevrons*, ex.: `span`, `h3`, `strong`...
+if (!defined('_ALBUMS_BALISE_TITRE'))                  define('_ALBUMS_BALISE_TITRE','strong');
+if (!defined('_ALBUMS_BALISE_TITRE_VIGNETTES'))        define('_ALBUMS_BALISE_TITRE_VIGNETTES','');
+if (!defined('_ALBUMS_BALISE_TITRE_LISTE'))            define('_ALBUMS_BALISE_TITRE_LISTE','');
+
+// position de la légende (titre et descriptif) : `top` ou `bottom`
+if (!defined('_ALBUMS_POSITION_LEGENDE'))              define('_ALBUMS_POSITION_LEGENDE','top');
+if (!defined('_ALBUMS_POSITION_LEGENDE_VIGNETTES'))    define('_ALBUMS_POSITION_LEGENDE_VIGNETTES','bottom');
+if (!defined('_ALBUMS_POSITION_LEGENDE_LISTE'))        define('_ALBUMS_POSITION_LEGENDE_LISTE','');
+
+?>
