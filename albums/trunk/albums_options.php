fa80cfcb51595a9c7e4db2e75a3732abf6cff856
tcharlss@hotmail.fr <tcharlss@hotmail.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-01-09 00:34:57 +0000

    suite r87043 : pour le modèle «liste», ajout d'une constante _ALBUMS_LISTE_METAS pour les infos des fichiers affichées par défaut, on ne met pas tout ça dans un `<strong>` mais dans un simple `<span>` + broutilles.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@87044 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- albums/trunk/albums_options.php
+++ albums/trunk/albums_options.php
@@ -13,27 +13,30 @@
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 // Paramètres par défaut des modèles
-// Les suffixes `_VIGNETTES` et `_LISTE` pour restreindre à une variante
+// Les suffixes `_VIGNETTES` et `_LISTE` pour restreindre certains à une variante
 
-// Dimension maximale des vignettes : nombre
-if (!defined('_ALBUMS_TAILLE_PREVIEW'))                define('_ALBUMS_TAILLE_PREVIEW',120);
-
-// Recadrer ou non les vignettes
-if (!defined('_ALBUMS_RECADRER'))                      define('_ALBUMS_RECADRER',false);
-
-// Pas de pagination (nombre de documents affichés en même temps) : nombre
+// [tous] Pas de pagination (nombre de documents affichés en même temps) : nombre
 if (!defined('_ALBUMS_PAGINATION'))                    define('_ALBUMS_PAGINATION',20);
 if (!defined('_ALBUMS_PAGINATION_VIGNETTES'))          define('_ALBUMS_PAGINATION_VIGNETTES','');
 if (!defined('_ALBUMS_PAGINATION_LISTE'))              define('_ALBUMS_PAGINATION_LISTE','');
 
-// Balise dans laquelle est placé le titre : balise HTML *sans les chevrons*, ex.: `span`, `h3`, `strong`...
+// [tous] Balise dans laquelle est placé le titre : balise HTML *sans les chevrons*, ex.: `span`, `h3`, `strong`...
 if (!defined('_ALBUMS_BALISE_TITRE'))                  define('_ALBUMS_BALISE_TITRE','strong');
 if (!defined('_ALBUMS_BALISE_TITRE_VIGNETTES'))        define('_ALBUMS_BALISE_TITRE_VIGNETTES','');
 if (!defined('_ALBUMS_BALISE_TITRE_LISTE'))            define('_ALBUMS_BALISE_TITRE_LISTE','');
 
-// position de la légende (titre et descriptif) : `top` ou `bottom`
+// [tous] Position de la légende (titre et descriptif) : `top` ou `bottom`
 if (!defined('_ALBUMS_POSITION_LEGENDE'))              define('_ALBUMS_POSITION_LEGENDE','top');
 if (!defined('_ALBUMS_POSITION_LEGENDE_VIGNETTES'))    define('_ALBUMS_POSITION_LEGENDE_VIGNETTES','bottom');
 if (!defined('_ALBUMS_POSITION_LEGENDE_LISTE'))        define('_ALBUMS_POSITION_LEGENDE_LISTE','');
+
+// [vignettes] Dimension maximale des vignettes : nombre
+if (!defined('_ALBUMS_TAILLE_PREVIEW'))                define('_ALBUMS_TAILLE_PREVIEW',120);
+
+// [vignettes] Recadrer ou non les vignettes
+if (!defined('_ALBUMS_RECADRER'))                      define('_ALBUMS_RECADRER',false);
+
+// [liste] Informations affichées pour chaque fichier, séparées par un tiret `-` : 'extension-taille-dimensions'
+if (!defined('_ALBUMS_LISTE_METAS'))                   define('_ALBUMS_LISTE_METAS','');
 
 ?>
