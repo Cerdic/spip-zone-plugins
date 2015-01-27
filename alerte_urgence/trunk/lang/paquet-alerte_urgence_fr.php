6dacbe64eab187d7e401cfac09d09521c305d967
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-01-27 11:36:53 +0000

    Ajouter un fichier de langue pour le paquet, c'est plus propre et traduisible.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@87202 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ alerte_urgence/trunk/lang/paquet-alerte_urgence_fr.php
@@ -0,0 +1,12 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/alerte_urgence/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	'alerte_urgence_description' => 'Ce plugin permet de rédiger et d’afficher un message urgent à vos visiteurs. Par défaut il est ajouté en haut de chaque page du site. Il est possible de personnaliser son style et son emplacement.',
+	'alerte_urgence_nom' => 'Alerte d’urgence',
+	'alerte_urgence_slogan' => 'Afficher un message urgent à vos visiteurs, rapidement et très visible.'
+);
+
+?>
