ba8e7c5ac3a63586717774aaeb9b42d6ce0ea262
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-01-27 11:14:44 +0000

    Ajout d'une nouvelle fonctionnalité : il est désormais possible de personnaliser l'insertion de l'alerte à n'importe quel endroit de vos gabarits HTML. Il suffit pour cela d'ajouter le code `<!-- inserer_alerte_urgence -->` où vous le désirez. Le bloc s'insérera alors à cet endroit. On ajoute un article de documentation.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@87201 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- alerte_urgence/trunk/alerte_urgence_pipelines.php
+++ alerte_urgence/trunk/alerte_urgence_pipelines.php
@@ -13,19 +13,34 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 function alerte_urgence_affichage_final($flux) {
 	if (
 		$GLOBALS['html'] // si c'est bien du HTML
-		and (strpos($flux,'<body')) !== false // et qu'on a une balise <body>
+		and (
+			($insertion = strpos($flux, '<!-- inserer_alerte_urgence -->')) !== false // et qu'on a la chaîne d'insertion
+			or
+			strpos($flux,'<body') !== false // ou qu'on a une balise <body>
+		)
 	) {
 		include_spip('inc/config');
-		//$flux = substr_replace($flux, recuperer_fond('inclure/alerte_urgence'), $p, 0);
-		$flux = preg_replace(
-			'|<body[^>]*>|is',
-			'$0'.recuperer_fond(
-				'inclure/alerte_urgence',
-				array('signature' => lire_config('alerte_urgence/texte'))
-			),
-			$flux
+		
+		// On génère l'alerte
+		$alerte = recuperer_fond(
+			'inclure/alerte_urgence',
+			array('signature' => lire_config('alerte_urgence/texte'))
 		);
+		
+		// Si c'est l'insertion, on remplace
+		if ($insertion !== false) {
+			$flux = str_replace('<!-- inserer_alerte_urgence -->', $alerte, $flux);
+		}
+		// Sinon on la met au début du body
+		else {
+			$flux = preg_replace(
+				'|<body[^>]*>|is',
+				'$0'.$alerte,
+				$flux
+			);
+		}
 	}
+	
 	return $flux;
 }
 
