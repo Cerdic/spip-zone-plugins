4f7fdb1ae110d18f9b072439d5330c11756f4de1
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-01-28 04:33:01 +0000

    [Salvatore] [source:_plugins_/alerte_urgence/trunk/lang/ paquet-alerte_urgence] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@87214 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- alerte_urgence/trunk/lang/paquet-alerte_urgence_fr.php
+++ alerte_urgence/trunk/lang/paquet-alerte_urgence_fr.php
@@ -4,6 +4,8 @@
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
 	'alerte_urgence_description' => 'Ce plugin permet de rédiger et d’afficher un message urgent à vos visiteurs. Par défaut il est ajouté en haut de chaque page du site. Il est possible de personnaliser son style et son emplacement.',
 	'alerte_urgence_nom' => 'Alerte d’urgence',
 	'alerte_urgence_slogan' => 'Afficher un message urgent à vos visiteurs, rapidement et très visible.'
