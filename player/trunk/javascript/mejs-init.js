e801312b1dc8e156c85486b1086cf9c9b57ab6d6
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-01-29 12:13:32 +0000

    Charger la CSS principale plus tot, car sinon l'init JS peut avoir un rendu aleatoire
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@87234 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- player/trunk/javascript/mejs-init.js
+++ player/trunk/javascript/mejs-init.js
@@ -1,17 +1,28 @@
 var mejsloader;
 (function(){
-	if (typeof mejsloader == "undefined")
-		mejsloader = {gs:null,plug:{},css:{},init:null,c:0};
-	if (!mejsloader.init){
-		mejsloader.init = function (){
-			if (!(mejsloader.gs===true)) return;
+	var L=mejsloader;
+	if (typeof L == "undefined")
+		L = {gs:null,plug:{},css:{},init:null,c:0,cssload:null};
+	if (!L.init){
+		L.cssload = function (f){
+			if (typeof L.css[f]=="undefined"){
+				L.css[f] = true;
+				var stylesheet = document.createElement('link');
+				stylesheet.href = f;
+				stylesheet.rel = 'stylesheet';
+				stylesheet.type = 'text/css';
+				document.getElementsByTagName('head')[0].appendChild(stylesheet);
+			}
+		}
+		L.init = function (){
+			if (!(L.gs===true)) return;
 			(function ($){
 				jQuery("audio.mejs,video.mejs").not('.done').each(function (){
 					var me = jQuery(this).addClass('done');
 					//console.log(this);
 					var id;
 					if (!(id = me.attr('id'))){
-						id = "mejs-"+(me.attr('data-id'))+"-"+(mejsloader.c++);
+						id = "mejs-"+(me.attr('data-id'))+"-"+(L.c++);
 						me.attr('id', id);
 					}
 					var opt = {options: {}, plugins: {}, css: []}, i, v;
@@ -22,30 +33,23 @@ var mejsloader;
 						var run = true;
 						//console.log(css);
 						for (var c in opt.css){
-							if (typeof mejsloader.css[opt.css[c]]=="undefined"){
-								mejsloader.css[opt.css[c]] = true;
-								var stylesheet = document.createElement('link');
-								stylesheet.href = opt.css[c];
-								stylesheet.rel = 'stylesheet';
-								stylesheet.type = 'text/css';
-								document.getElementsByTagName('head')[0].appendChild(stylesheet);
-							}
+							L.cssload(opt.css[c]);
 						}
 						for (var p in opt.plugins){
 							//console.log(p);
-							//console.log(mejsloader.plug[p]);
+							//console.log(L.plug[p]);
 							// load this plugin
-							if (typeof mejsloader.plug[p]=="undefined"){
+							if (typeof L.plug[p]=="undefined"){
 								//console.log("Load Plugin "+p);
 								run = false;
-								mejsloader.plug[p] = false;
+								L.plug[p] = false;
 								jQuery.getScript(opt.plugins[p], function (){
-									mejsloader.plug[p] = true;
+									L.plug[p] = true;
 									runthisplayer();
 								});
 							}
 							// this plugin is loading
-							else if (mejsloader.plug[p]==false){
+							else if (L.plug[p]==false){
 								//console.log("Plugin "+p+" loading...");
 								run = false;
 							}
@@ -76,12 +80,15 @@ var mejsloader;
 			})(jQuery);
 		}
 	}
-	if (!mejsloader.gs){
-		mejsloader.gs = jQuery.getScript(mejspath,function(){
-			mejsloader.gs = true;
-			mejsloader.init(); // init immediate des premiers players dans la page
-			jQuery(mejsloader.init); // init exhaustive de tous les players
-			onAjaxLoad(mejsloader.init); // init lors d'un load ajax
+	if (!L.gs){
+		if (typeof mejscss !== "undefined"){
+			L.cssload(mejscss);
+		}
+		L.gs = jQuery.getScript(mejspath,function(){
+			L.gs = true;
+			L.init(); // init immediate des premiers players dans la page
+			jQuery(L.init); // init exhaustive de tous les players
+			onAjaxLoad(L.init); // init lors d'un load ajax
 		});
 	}
 })();
