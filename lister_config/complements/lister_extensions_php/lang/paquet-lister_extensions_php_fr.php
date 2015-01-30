a59971b03468f6d277e0a5bc9f33d22e5657ae02
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-01-30 00:56:39 +0000

    Mauvais copier-coller
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@87247 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- lister_config/complements/lister_extensions_php/lang/paquet-lister_extensions_php_fr.php
+++ lister_config/complements/lister_extensions_php/lang/paquet-lister_extensions_php_fr.php
@@ -7,9 +7,9 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// L
-	'lister_exec_description' => 'Ce plugin permet de lister les extensions PHP chargées dans SPIP.',
-	'lister_exec_nom' => 'Lister les extensions PHP chargées',
-	'lister_exec_slogan' => 'Le support de votre application.'
+	'lister_extensions_php_description' => 'Ce plugin permet de lister les extensions PHP chargées dans SPIP.',
+	'lister_extensions_php_nom' => 'Lister les extensions PHP chargées',
+	'lister_extensions_php_slogan' => 'Le support de votre application.'
 );
 
 ?>
