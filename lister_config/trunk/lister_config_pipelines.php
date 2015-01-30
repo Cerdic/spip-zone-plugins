40430839a78ae9de598a2bc5822a08804cefbfbb
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-01-30 00:43:07 +0000

    On change de mécanique. Ce plugin devient le plugin central. On peut lui greffer des petits plugins par le biais d'un pipeline. Chaque plugin rajoutera une ou plusieurs pages dans le menu de la page ?exec=lister_config. La documentation viendra plus tard avec plsu d'explications. Ce plugin devenait un peu trop chargé, il reprend sa fonction première : lister les pages de configurations. Mais avec les plugins satellitaires, on peut faire son marché et prendre ce qui nous intéresse vraiment. On passe en v1.0
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@87243 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ lister_config/trunk/lister_config_pipelines.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Lister les pages de configurations
+ * Licence GPL
+ * Auteur : Teddy Payet
+ *
+ */
+
+if (!defined("_ECRIRE_INC_VERSION")) {
+    return;
+}
+function lister_config_listermenu($flux)
+{
+    return $flux;
+}
+
+
+?>
