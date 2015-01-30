ad90178a210c8524028dc39f5321db4782faf219
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-01-30 00:31:58 +0000

    On copie le trunk dans la branche v0
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@87242 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ lister_config/branches/v0/lang/paquet-lister_config_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/autorite/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'lister_config_description' => 'Ce plugin permet de lister sur une page tous les liens vers les pages de configuration des plugins.',
+	'lister_config_nom' => 'Lister les pages de configuration',
+	'lister_config_slogan' => 'Une page pour les configurer tous!'
+);
+
+?>
