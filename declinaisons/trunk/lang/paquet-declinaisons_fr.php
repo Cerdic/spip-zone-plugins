8050630f98f84f9d89e4df66d29c4fa071733122
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-02-02 04:33:16 +0000

    [Salvatore] [source:_plugins_/declinaisons/trunk/lang/ paquet-declinaisons] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@87327 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- declinaisons/trunk/lang/paquet-declinaisons_fr.php
+++ declinaisons/trunk/lang/paquet-declinaisons_fr.php
@@ -1,14 +1,14 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/declinaisons/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-	// S
-	'declinaisons_description' => 'Gère les différentes déclinaisons d\'un produit',
+	// D
+	'declinaisons_description' => 'Gère les différentes déclinaisons d’un produit',
 	'declinaisons_nom' => 'Déclinaisons Produit',
-	'declinaisons_slogan' => 'Gérer des déclinaisons d\'un produit',
+	'declinaisons_slogan' => 'Gérer des déclinaisons d’un produit'
 );
 
 ?>
