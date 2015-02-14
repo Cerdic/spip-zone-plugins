f6feed9c0e50ce5dbf2998ceb768cd0f67176fb5
tcharlss@hotmail.fr <tcharlss@hotmail.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-02-14 00:57:25 +0000

    Petite pétouille dans le slogan : le plugin sert non pas à utiliser le framework, mais à l'implémenter. + oubli dans le message du commit initial : la page de démo se trouve à l'adresse `spip.php?page=demo/democommerce`.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@87436 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- democommerce/trunk/lang/paquet-democommerce_fr.php
+++ democommerce/trunk/lang/paquet-democommerce_fr.php
@@ -9,7 +9,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'democommerce_description' => '<p>Sous SPIP, il existe un framework permettant de faire du ecommerce. Il est complètement fonctionnel, modulaire, extensible, et permet de faire de la vente dans le confort de notre CMS favori.</p><p>Il se compose d\'une suite de plugins complémentaires, qui prennent en charge chacun un aspect : gestion des produits, des commandes, des paniers, des transactions, etc.</p><p>Le présent plugin s\'adresse aux gens qui écrivent des squelettes, il a un objectif didactique : montrer par l\'exemple <em>les bases</em> de la mise en oeuvre du framework, en expliquant les éléments à placer dans vos squelettes.C\'est relativement simple : en 3 squelettes, au moyen de quelques boutons d\'actions et formulaires, vous avez déjà le minimum nécessaire pour vendre des choses sur votre site.</p>
 <p>Rendez-vous sur la page <strong><code>votresite.tld/spip.php?page=demo/democommerce</code></strong> et laissez vous guider.</p>',
 	'democommerce_nom' => 'Démonstration du framework ecommerce',
-	'democommerce_slogan' => 'Utiliser le framework ecommerce dans vos squelettes par l\'exemple.',
+	'democommerce_slogan' => 'Implémenter le framework ecommerce dans vos squelettes par l\'exemple.',
 );
 
 ?>
