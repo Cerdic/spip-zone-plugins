29cb4e6bf199a4b30fdb294e79504d235dc67350
cyril@cym.fr <cyril@cym.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-02-23 18:48:17 +0000

    Première série de modifs pour une version SPIP 3.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@87604 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- onglets_texte/trunk/onglets_texte_pipelines.php
+++ onglets_texte/trunk/onglets_texte_pipelines.php
@@ -5,7 +5,12 @@
 
 function onglets_texte_insert_head($flux){
 	$flux .= '<script type="text/javascript" src="'.find_in_path('javascript/mes_onglets.js').'"></script>';
+	return $flux;
+}
+
+function onglets_texte_insert_head_css($flux){
 	$flux .= '<link rel="stylesheet" href="'.find_in_path('css/tab.css').'" type="text/css" media="all" />';
 	return $flux;
 }
+
 ?>
