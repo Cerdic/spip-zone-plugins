559ad81a648c60264b28e77b44729a085e8cac7a
pierre.troller@gmail.com <pierre.troller@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-02-27 10:05:50 +0000

    Deux nouvelles options de notifications des forums publics, les admins restreints (l'ancienne version ne prévenait que les admins de la rubrique, pas des parentes) et une liste d'adresses explicites (demande sur users)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@87660 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- notifications/trunk/notifications/forumvalide.php
+++ notifications/trunk/notifications/forumvalide.php
@@ -63,6 +63,48 @@ function notifications_forumvalide_dist($quoi, $id_forum, $options) {
 		}
 	}
 
+
+// Prevenir les admins restreints de la rubrique et des parentes  lors de messages de forum d'article ou de rubrique
+// c'est ici because dans notifications_pipelines la fonction de dédoublonnage le fait sauter (?)
+
+if ($GLOBALS['notifications']['forums_admins_restreints']) {
+         if ($t['objet']=='rubrique'){
+         $id_rubrique = $t['id_objet'];
+         }
+         if ($t['objet']=='article'){
+         include_spip('base/abstract_sql');
+         $t = sql_fetsel("id_rubrique", "spip_articles", "id_article=" . intval($t['id_objet']));
+         $id_rubrique = $t['id_rubrique'];
+         }
+         if ($GLOBALS['notifications']['forums_limiter_rubriques']){
+             $limites = $GLOBALS['notifications']['forums_limiter_rubriques'];
+             $forums_limiter_rubriques = explode(",",$limites);
+     } else {
+             $forums_limiter_rubriques = array($id_rubrique);
+         }
+         if (in_array($id_rubrique,$forums_limiter_rubriques)){
+             while ($id_rubrique){
+                 $hierarchie[] = $id_rubrique;
+                 $res = sql_fetsel("id_parent", "spip_rubriques", "id_rubrique=" . intval($id_rubrique));
+                 if (!$res){ // rubrique inexistante
+                     $id_rubrique = 0;
+                     break;
+                 }
+                 $id_parent = $res['id_parent'];
+                 $id_rubrique = $id_parent;
+             }
+             //les admins de la rub et de ses parents
+             $result = sql_select(
+                 "auteurs.email,auteurs.id_auteur,lien.id_objet as id_rubrique",
+                 "spip_auteurs AS auteurs JOIN spip_auteurs_liens AS lien ON auteurs.id_auteur=lien.id_auteur ",
+                 "lien.objet='rubrique' AND ".sql_in('lien.id_objet',sql_quote($hierarchie))." AND auteurs.statut='0minirezo'");
+         while ($qui = sql_fetch($result)) {
+                     $tous[] = $qui['email'];
+             }
+         }
+     }
+/////////////////////////////
+
 	$options['forum'] = $t;
 	$destinataires = pipeline('notifications_destinataires',
 		array(
@@ -84,6 +126,34 @@ function notifications_forumvalide_dist($quoi, $id_forum, $options) {
 		$texte = $email_notification_forum($t, $email, $contexte);
 		notifications_envoyer_mails_texte_ou_html($email, $texte);
 	}
+
+// une liste d'adresses renseignées dans config possiblement liste de diffusion
+	if ($GLOBALS['notifications']['forums_liste']){
+
+	//construction du mail  pour envoyer_mail
+	$titre="$t[titre]";
+	$sujet=""._T('forum:info_1_message_forum')." '$titre'";
+	$charset = lire_meta('charset');
+
+	if ($GLOBALS['meta']['facteur_adresse_envoi'] == 'oui' AND $GLOBALS['meta']['facteur_adresse_envoi_email'])
+			$from_email = $GLOBALS['meta']['facteur_adresse_envoi_email'];
+		else
+			$from_email = $email_webmaster;
+		// nom denvoi depuis config facteur
+		if ($GLOBALS['meta']['facteur_adresse_envoi'] == 'oui' AND $GLOBALS['meta']['facteur_adresse_envoi_nom'])
+		$from_nom = $GLOBALS['meta']['facteur_adresse_envoi_nom'];
+
+	foreach (explode(',', $GLOBALS['notifications']['forums_liste']) as $to_email) {
+		$email_notification_forum = charger_fonction('email_notification_forum','inc');
+		$body = $email_notification_forum($t, $to_email);
+		include_spip('classes/facteur');
+		$body = Facteur::html2text($body);
+		$envoyer_mail = charger_fonction('envoyer_mail','inc/');
+		$envoyer_mail($to_email, $sujet, $body, $from_email, $headers);
+		}
+	} 
+///////////////////////////
+
 
 }
 
