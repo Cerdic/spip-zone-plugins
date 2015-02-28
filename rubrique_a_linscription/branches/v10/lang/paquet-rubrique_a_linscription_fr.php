b049edd9df072b327b01fbd4de69bf1623a63903
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-02-28 11:17:17 +0000

    un descriptif plus juste de ce que fait le plugin
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@87674 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- rubrique_a_linscription/branches/v10/lang/paquet-rubrique_a_linscription_fr.php
+++ rubrique_a_linscription/branches/v10/lang/paquet-rubrique_a_linscription_fr.php
@@ -13,7 +13,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 // R
-	'rubrique_a_linscription_description' => 'Si le formulaire d\'inscription est passé avec l\'argument 0minirezo (<code>#FORMULAIRE_INSCRIPTION{0minirezo}</code>), une rubrique est automatiquement créée, où l\'auteur est admin restreint.',
+	'rubrique_a_linscription_description' => 'Création automatique de rubrique à l\'inscription d\'un auteur. Éventuellement avec administration restreinte.',
 	'rubrique_a_linscription_slogan' => 'Créer une rubrique à l\'inscription d\'un admin',
 );
 ?>
