2a120cd086ea647c1a7e4a23d550983c36ab2e6b
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-03-02 11:34:47 +0000

    Instagram est un fournisseur de photos, on ne veut pas de leur mouchard social iframe+js
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@87683 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ oembed/oembed/input/posttraite_instagram_rich.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Plugin oEmbed
+ * Licence GPL3
+ *
+ */
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+function oembed_input_posttraite_instagram_rich_dist($data){
+
+	#var_dump($data);
+	// Instagram est un fournisseur de photo, on ne veut pas de leur mouchard iframe+js sociaux
+	$data['media'] = 'image';
+	if (isset($data['thumbnail_url'])
+		AND isset($data['thumbnail_width'])
+		AND isset($data['thumbnail_height'])){
+		$data['type'] = 'photo';
+		$data['url'] = $data['thumbnail_url'];
+		$data['width'] = $data['thumbnail_width'];
+		$data['height'] = $data['thumbnail_height'];
+	}
+
+	return $data;
+}
