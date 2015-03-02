aaef4b32427b73a5045f1b087f587e9358f5bbb2
pierre.troller@gmail.com <pierre.troller@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-03-02 13:08:36 +0000

    augmentation de la capacité sql varchar 255 à 1000 + extension aux rubriques et aux auteurs + le plugin est compatible 3.1
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@87686 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- mosaique/base/mosaique.php
+++ mosaique/base/mosaique.php
@@ -2,16 +2,42 @@
 if (!defined("_ECRIRE_INC_VERSION")) return;
  
 function mosaique_declarer_champs_extras($champs = array()) {
+
   $champs['spip_articles']['mosaique'] = array(
       'saisie' => 'mosaique',//Type du champ (voir plugin Saisies)
       'options' => array(
             'nom' => 'mosaique',
             'label' => '',
-            'sql' => "varchar(255) NOT NULL DEFAULT ''",
+    	     'sql' => "varchar(1000) NOT NULL DEFAULT ''",
+            'defaut' => '',// Valeur par defaut
+            'env' => 'oui',
+      ),
+  );
+
+//  return $champs;      
+
+  $champs['spip_rubriques']['mosaique'] = array(
+      'saisie' => 'mosaique',//Type du champ (voir plugin Saisies)
+      'options' => array(
+            'nom' => 'mosaique',
+            'label' => '',
+     	    'sql' => "varchar(1000) NOT NULL DEFAULT ''",
+            'defaut' => '',// Valeur par defaut
+            'env' => 'oui',
+      ),
+  );
+
+  $champs['spip_auteurs']['mosaique'] = array(
+      'saisie' => 'mosaique',//Type du champ (voir plugin Saisies)
+      'options' => array(
+            'nom' => 'mosaique',
+            'label' => '',
+    	     'sql' => "varchar(1000) NOT NULL DEFAULT ''",
             'defaut' => '',// Valeur par defaut
             'env' => 'oui',
       ),
   );
   return $champs;      
+
 }
 ?>
