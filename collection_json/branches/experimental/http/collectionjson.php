529b391e30c7342655a99cb78bedd9c6b8f42fc1
bystrano@gmx.ch <bystrano@gmx.ch@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-03-04 13:28:14 +0000

    le seul moyen de définir la réponse d'une collection soi-même est avec une fonction
    Ces fonctions, au lieu de retourner des objets Response doivent
    retourner des tableaux contenant les éléments du tableau "collection" de
    la réponse. On laisse tomber les autres moyens de surcharge, ça laisse
    déjà pas mal de possibilités.
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@87737 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- collection_json/branches/experimental/http/collectionjson.php
+++ collection_json/branches/experimental/http/collectionjson.php
@@ -130,91 +130,80 @@ function http_collectionjson_get_collection_dist($requete, $reponse){
 	$collection = $requete->attributes->get('collection');
 	$contexte = $requete->query->all();
 
-	// S'il existe une fonction globale, dédiée à ce type de ressource, qui gère TOUTE la requête, on n'utilise QUE ça
-	// Cette fonction doit donc évidemment renvoyer un objet Response valide
+	// S'il existe une fonction dédiée à ce type de ressource, on
+	// n'utilise que ça. Cette fonction doit renvoyer un tableau à
+	// mettre dans la réponse
 	if ($fonction_collection = charger_fonction('get_collection', "http/$format/$collection/", true)){
 
-		return $fonction_collection($requete, $reponse);
+		$retour = $fonction_collection($requete, $reponse);
+		return http_collectionjson_reponse(200, $retour, $requete, $reponse);
 	}
-	// Sinon on essaye de trouver différentes méthodes pour produire le JSON et en déduire les headers
-	// - pour l'instant seulement par un squelette
-	else {
-		// Allons chercher un squelette de base qui génère le JSON de la collection demandée
-		// Le squelette prend en contexte les paramètres du GET uniquement
-		if ($json = recuperer_fond("http/$format/$collection", $contexte)){
-			// On décode ce qu'on a trouvé
-			$json = json_decode($json, true);
-			$retour = $json['collection'];
-		}
-		// Si on ne trouve rien on essaie de s'appuyer sur l'API objet
-		else  {
 
-			include_spip('base/abstract_sql');
-			include_spip('base/objets');
+	// Sinon on essaie de s'appuyer sur l'API objet
+	include_spip('base/abstract_sql');
+	include_spip('base/objets');
 
-			// Si la collection demandée ne correspond pas à une table
-			// d'objet on arrête tout
-			if ( ! in_array(table_objet_sql($collection),
-							array_keys(lister_tables_objets_sql()))) {
-				// On utilise la fonction d'erreur générique pour
-				// renvoyer dans le bon format
-				$fonction_erreur = charger_fonction('erreur', "http/$format/");
-				return $fonction_erreur(404, $requete, $reponse);
-			}
+	// Si la collection demandée ne correspond pas à une table
+	// d'objet on arrête tout
+	if ( ! in_array(table_objet_sql($collection),
+					array_keys(lister_tables_objets_sql()))) {
+		// On utilise la fonction d'erreur générique pour
+		// renvoyer dans le bon format
+		$fonction_erreur = charger_fonction('erreur', "http/$format/");
+		return $fonction_erreur(404, $requete, $reponse);
+	}
 
-			$links = array();
+	$links = array();
 
-			$pagination = 10;
-			$offset = $contexte['offset'] ?: 0;
-			$nb_objets = sql_countsel(table_objet_sql($collection));
+	$pagination = 10;
+	$offset = $contexte['offset'] ?: 0;
+	$nb_objets = sql_countsel(table_objet_sql($collection));
 
-			// On ajoute des liens de pagination
-			if ($offset > 0) {
-				$offset_precedant = max(0, $offset-$pagination);
-				$links[] = array(
-					'rel' => 'prev',
-					'prompt' => _T('public:page_precedente'),
-					'href' => url_absolue(
-						parametre_url(self(), 'offset', $offset_precedant)),
-				);
-			}
-			if (($offset + $pagination) < $nb_objets) {
-				$offset_suivant = $offset + $pagination;
-				$links[] = array(
-					'rel' => 'prev',
-					'prompt' => _T('public:page_suivante'),
-					'href' => url_absolue(
-						parametre_url(self(), 'offset', $offset_suivant)),
-				);
-			}
+	// On ajoute des liens de pagination
+	if ($offset > 0) {
+		$offset_precedant = max(0, $offset-$pagination);
+		$links[] = array(
+			'rel' => 'prev',
+			'prompt' => _T('public:page_precedente'),
+			'href' => url_absolue(
+				parametre_url(self(), 'offset', $offset_precedant)),
+		);
+	}
+	if (($offset + $pagination) < $nb_objets) {
+		$offset_suivant = $offset + $pagination;
+		$links[] = array(
+			'rel' => 'prev',
+			'prompt' => _T('public:page_suivante'),
+			'href' => url_absolue(
+				parametre_url(self(), 'offset', $offset_suivant)),
+		);
+	}
 
-			$table_collection = table_objet_sql($collection);
-			$description = lister_tables_objets_sql($table_collection);
-			$objets = sql_allfetsel('*', $table_collection,'','','',"$offset,$pagination");
+	$table_collection = table_objet_sql($collection);
+	$description = lister_tables_objets_sql($table_collection);
+	$objets = sql_allfetsel('*', $table_collection,'','','',"$offset,$pagination");
 
-			$items = array();
-			foreach ($objets as $objet) {
-				$data = array();
-				foreach ($description['champs_editables'] as $champ){
-					$data[] = array(
-						'name' => $champ,
-						'value' => $objet[$champ],
-					);
-				}
-
-				$items[] = array(
-					'href' => url_absolue(parse_url(self(), PHP_URL_PATH) . $objet[id_table_objet($table_collection)]),
-					'data' => $data,
-				);
-			}
-
-			$retour = array(
-				'href' => url_absolue(parse_url(self(), PHP_URL_PATH)),
-				'links' => $links,
-				'items' => $items,
+	$items = array();
+	foreach ($objets as $objet) {
+		$data = array();
+		foreach ($description['champs_editables'] as $champ){
+			$data[] = array(
+				'name' => $champ,
+				'value' => $objet[$champ],
 			);
 		}
+
+		$items[] = array(
+			'href' => url_absolue(parse_url(self(), PHP_URL_PATH) . $objet[id_table_objet($table_collection)]),
+			'data' => $data,
+		);
 	}
+
+	$retour = array(
+		'href' => url_absolue(parse_url(self(), PHP_URL_PATH)),
+		'links' => $links,
+		'items' => $items,
+	);
 
 	return http_collectionjson_reponse(200, $retour, $requete, $reponse);
 }
@@ -231,87 +220,54 @@ function http_collectionjson_get_ressource_dist($requete, $reponse){
 	$format = $requete->attributes->get('format');
 	$collection = $requete->attributes->get('collection');
 
-	// S'il existe une fonction globale, dédiée à ce type de ressource, qui gère TOUTE la requête, on n'utilise QUE ça
-	// Cette fonction doit donc évidemment renvoyer un objet Response valide
+	// S'il existe une fonction dédiée à ce type de ressource, on
+	// n'utilise que ça. Cette fonction doit renvoyer un tableau à
+	// mettre dans la réponse
 	if ($fonction_ressource = charger_fonction('get_ressource', "http/$format/$collection/", true)){
-		return $fonction_ressource($requete, $reponse);
-	}
-	// Sinon on essaye de trouver différentes méthodes pour produire le JSON et en déduire les headers :
-	// - par une fonction dédiée au JSON
-	// - par un squelette
-	// - par un échafaudage générique
-	else{
-		$json = array();
 
-		// S'il existe une fonction dédiée au contenu d'une ressource de cette collection, on l'utilise
-		// Cette fonction ne doit retourner QUE le contenu JSON
-		if ($fonction_ressource_contenu = charger_fonction('get_ressource_contenu', "http/$format/$collection/", true)){
-			$json = $fonction_ressource_contenu($requete, $reponse);
-		}
-		// Sinon on essaye de le remplir avec un squelette
-		else{
-			// Pour l'instant on va simplement chercher un squelette du type de la ressource
-			// Le squelette prend en contexte les paramètres du GET + l'identifiant de la ressource en essayant de faire au mieux
-			include_spip('base/objets');
-			$ressource = $requete->attributes->get('ressource');
-			$cle = id_table_objet($collection);
-			$contexte = array(
-				$cle => $ressource,
-				'ressource' => $ressource,
-			);
-			$contexte = array_merge($requete->query->all(), $requete->attributes->all(), $contexte);
-
-			if ($skel = trim(recuperer_fond("http/$format/$collection-ressource", $contexte))){
-				// On décode ce qu'on a trouvé pour avoir un tableau PHP
-				$json = json_decode($skel, true);
-			}
-		}
-
-		// Si on n'a toujours aucun contenu json, on en échafaude un avec les API d'objets
-		if (empty($json)){
-			$table_collection = table_objet_sql($collection);
-			$objets = lister_tables_objets_sql();
-
-			// Si la collection fait partie des objets SPIP et qu'on trouve la ligne de l'objet en question
-			// On ne montre par défaut que les champs *éditables*
-			if (
-				isset($objets[$table_collection])
-				and $description = $objets[$table_collection]
-				and $objet = sql_fetsel($description['champs_editables'], $table_collection, "$cle = ".intval($ressource))
-			){
-				include_spip('inc/filtres');
-
-				$data = array();
-				foreach ($objet as $champ=>$valeur){
-					$data[] = array('name' => $champ, 'value' => $valeur);
-				}
-
-				$retour = array(
-					'href' => url_absolue(self()),
-					'items' => array(
-						array(
-							'href' => url_absolue(self()),
-							'links' => array(
-								array('rel' => 'edit', 'href' => $GLOBALS['meta']['adresse_site']."/http.api/$format/$collection/$ressource"),
-								array('rel' => 'alternate', 'type' => 'text/html', 'href' => url_absolue(generer_url_entite($ressource, objet_type($collection)))),
-							),
-							'data' => $data,
-						),
-					)
-				);
-			}
-		}
+		$retour = $fonction_ressource($requete, $reponse);
+		return http_collectionjson_reponse(200, $retour, $requete, $reponse);
 	}
 
-    $retour = $retour ?: $json['collection'];
+	include_spip('base/objets');
 
-    // Si on n'a toujours rien on abandonne
-    if ( ! $retour) {
-        // On utilise la fonction d'erreur générique pour renvoyer
-        // dans le bon format
-        $fonction_erreur = charger_fonction('erreur', "http/$format/");
-        return $fonction_erreur(404, $requete, $reponse);
-    }
+	// Sinon on essaye de déduire par un échafaudage générique
+	$table_collection = table_objet_sql($collection);
+	$objets = lister_tables_objets_sql();
+
+	// Si la collection fait partie des objets SPIP et qu'on trouve la
+	// ligne de l'objet en question.  On ne montre par défaut que les
+	// champs *éditables*. Sinon on renvoie une erreur.
+	if ( ! (isset($objets[$table_collection])
+			and $description = $objets[$table_collection]
+			and $objet = sql_fetsel($description['champs_editables'], $table_collection, "$cle = ".intval($ressource)))) {
+
+		// On utilise la fonction d'erreur générique pour
+		// renvoyer dans le bon format
+		$fonction_erreur = charger_fonction('erreur', "http/$format/");
+		return $fonction_erreur(404, $requete, $reponse);
+	}
+
+	include_spip('inc/filtres');
+
+	$data = array();
+	foreach ($objet as $champ=>$valeur){
+		$data[] = array('name' => $champ, 'value' => $valeur);
+	}
+
+	$retour = array(
+		'href' => url_absolue(self()),
+		'items' => array(
+			array(
+				'href' => url_absolue(self()),
+				'links' => array(
+					array('rel' => 'edit', 'href' => $GLOBALS['meta']['adresse_site']."/http.api/$format/$collection/$ressource"),
+					array('rel' => 'alternate', 'type' => 'text/html', 'href' => url_absolue(generer_url_entite($ressource, objet_type($collection)))),
+				),
+				'data' => $data,
+			),
+		)
+	);
 
 	return http_collectionjson_reponse(200, $retour, $requete, $reponse);
 }
@@ -328,19 +284,18 @@ function http_collectionjson_post_collection_dist($requete, $reponse){
 	$format = $requete->attributes->get('format');
 	$collection = $requete->attributes->get('collection');
 
-	// S'il existe une fonction globale, dédiée à ce type de ressource, qui gère TOUTE la requête, on n'utilise QUE ça
-	// Cette fonction doit donc évidemment renvoyer un objet Response valide
+	// S'il existe une fonction dédiée à ce type de ressource, on
+	// n'utilise que ça. Cette fonction doit renvoyer un tableau à
+	// mettre dans la réponse
 	if ($fonction_ressource = charger_fonction('post_collection', "http/$format/$collection/", true)){
-		$reponse = $fonction_ressource($requete, $reponse);
+		$retour = $fonction_ressource($requete, $reponse);
+		return http_collectionjson_reponse(200, $retour, $requete, $reponse);
 	}
 	// Sinon on échafaude en utilisant l'API des objets
-	else{
-		include_spip('base/objets');
-		$objet= objet_type($collection);
-		$reponse = http_collectionjson_editer_objet($objet, 'new', $requete->getContent(), $requete, $reponse);
-	}
+	include_spip('base/objets');
+	$objet= objet_type($collection);
 
-	return $reponse;
+	return http_collectionjson_editer_objet($objet, 'new', $requete->getContent(), $requete, $reponse);
 }
 
 /**
@@ -355,20 +310,19 @@ function http_collectionjson_put_ressource_dist($requete, $reponse){
 	$format = $requete->attributes->get('format');
 	$collection = $requete->attributes->get('collection');
 
-	// S'il existe une fonction globale, dédiée à ce type de ressource, qui gère TOUTE la requête, on n'utilise QUE ça
-	// Cette fonction doit donc évidemment renvoyer un objet Response valide
+	// S'il existe une fonction dédiée à ce type de ressource, on
+	// n'utilise que ça. Cette fonction doit renvoyer un tableau à
+	// mettre dans la réponse
 	if ($fonction_ressource = charger_fonction('put_ressource', "http/$format/$collection/", true)){
-		$reponse = $fonction_ressource($requete, $reponse);
+		$retour = $fonction_ressource($requete, $reponse);
+		return http_collectionjson_reponse(200, $retour, $requete, $reponse);
 	}
 	// Sinon on échafaude en utilisant l'API des objets
-	else{
-		include_spip('base/objets');
-		$id_objet = intval($requete->attributes->get('ressource'));
-		$objet= objet_type($collection);
-		$reponse = http_collectionjson_editer_objet($objet, $id_objet, $requete->getContent(), $requete, $reponse);
-	}
+	include_spip('base/objets');
+	$id_objet = intval($requete->attributes->get('ressource'));
+	$objet= objet_type($collection);
 
-	return $reponse;
+	return http_collectionjson_editer_objet($objet, $id_objet, $requete->getContent(), $requete, $reponse);
 }
 
 /**
@@ -469,27 +423,10 @@ function http_collectionjson_editer_objet($objet, $id_objet, $contenu, $requete,
 		if ($fonction_ressource = charger_fonction('get_ressource', 'http/collectionjson/', true)){
 			// On ajoute à la requête, l'identitiant de la nouvelle ressource
 			$requete->attributes->set('ressource', $id_objet);
-			$reponse = $fonction_ressource($requete, $reponse);
-		}
-		// Si c'était une création, on renvoie 201
-		if ($new){
-			$code = 201;
+			$retour = $fonction_ressource($requete, $reponse);
 		}
 	}
 
-	$reponse->setCharset('utf-8');
-	$reponse->headers->set('Content-Type', 'application/json');
-	$reponse->setStatusCode($code ?: 200);
-
-    $reponse = pipeline(
-		'http_collectionjson_final',
-		array(
-			'args' => array(
-				'requete' => $requete,
-			),
-			'data' => $reponse,
-		)
-	);
-
-	return $reponse;
+	// Si c'était une création, on renvoie 201
+	return http_collectionjson_reponse($new ? 201 : 200, $retour, $requete, $reponse);
 }
