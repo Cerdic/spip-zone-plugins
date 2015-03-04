8498f05e136d147ccabe74b6bcebf8d2dc1bbdad
code@archiloque.net <code@archiloque.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-03-04 23:20:29 +0000

    plugins_seenthis/date_relative_dynamique : correction pour les changements qui ont entre deux jours et une semaines, partie dépendante sortie dans un fichier séparé, utilisation d'un cache pour limiter les calculs
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@87742 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- plugins_seenthis/date_relative_dynamique/calcul_date.php
+++ plugins_seenthis/date_relative_dynamique/calcul_date.php
@@ -6,27 +6,25 @@ function date_u($date) {
 
 function calcul_date_insert_head($flux) {
 	$lang = $GLOBALS["spip_lang"];
-	
-	
-	$ret = "<script src='spip.php?page=js.calcul_date&lang=$lang' type=\"text/javascript\"></script>";
 
-	
-	
+	$ret = "<script src='spip.php?page=js.calcul_date_lang&lang=$lang' type='text/javascript'></script>";
+	$ret .= "<script src='" . find_in_path("javascript/calcul_date.js") . "' type='text/javascript'></script>";
+
 	return $flux.$ret;
-	
+
 }
 
 
 // Ca, on ne l'utilise plus: on fait un POST et on recupere la date du serveur de cette facon...
 /*
 function calcul_date_affichage_final($flux) {
-	
+
 	$ret .= "
 	<script type='text/javascript'><!--
 	var date_now = \"".date('U')."\";
 	--></script>
 	";
-	
+
 	return str_replace("</head>", $ret."</head>", $flux);
 }
 */
