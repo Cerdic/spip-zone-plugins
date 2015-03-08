b7a3d7bc63c7e6d9526c76e89637b07bab83687d
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-03-08 16:18:52 +0000

    si on utilise des urls basés sur le titre d'article, pouvoir regenerer la BDD -> ne pas définir generer_url_article
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@87781 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- urls_par_numero/trunk/urls/generer_url_article.php
+++ urls_par_numero/trunk/urls/generer_url_article.php
@@ -2,7 +2,17 @@
 
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
-// Pour les fois où on utilise pas des urls propres
-function urls_generer_url_article_dist($id_article, $args='', $ancre='') {
-	return _DIR_RACINE . $id_article . ($args ? "?$args" : '') .($ancre ? "#$ancre" : '');
+$cas_propre = array('propres','libres','arbo','propres_qs','propres2');
+if (!(
+	$GLOBALS['type_urls']=='page'
+	or (
+		$GLOBALS['type_urls']!='page'
+		and in_array($GLOBALS['meta']['type_urls'],$cas_propre)
+	))
+)
+{	
+	// Pour les fois où on utilise pas des urls propres
+	function urls_generer_url_article_dist($id_article, $args='', $ancre='') {
+		return _DIR_RACINE . $id_article . ($args ? "?$args" : '') .($ancre ? "#$ancre" : '');
+	};
 }
