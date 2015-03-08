d9a9c625feae9b8bb61711668a48c460f506b1ee
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-03-08 14:33:39 +0000

    si on a des urls propres, il faut vraiment mettre le numéro dans la table des urls propres, sinon cela ne fait pas la redirection
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@87778 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- urls_par_numero/trunk/urls/generer_url_article.php
+++ urls_par_numero/trunk/urls/generer_url_article.php
@@ -2,6 +2,7 @@
 
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
+// Pour les fois où on utilise pas des urls propres
 function urls_generer_url_article_dist($id_article, $args='', $ancre='') {
 	return _DIR_RACINE . $id_article . ($args ? "?$args" : '') .($ancre ? "#$ancre" : '');
 }
