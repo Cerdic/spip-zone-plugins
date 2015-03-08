0976ad9912855222e1ddcfe70e18664326683e0c
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-03-08 11:56:10 +0000

    paquet.xml + chaîne de paquet + logo
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@87770 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ urls_par_numero/trunk/lang/paquet-urls_par_numero_fr.php
@@ -0,0 +1,12 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	'urls_par_numero_slogan' => 'Court et mémorisable',
+	'urls_par_numero_description' => 'Des urls constituées uniquement du numéro de l\'article. Pour les autres objets, prend le titre de l\'objet au singulier suivit de son numéro.'
+);
+
+?>
