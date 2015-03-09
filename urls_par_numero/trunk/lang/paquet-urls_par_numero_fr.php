b6e7e004692a4016c2374f2cb2b09b1b0644c81e
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-03-09 04:34:24 +0000

    [Salvatore] [source:_plugins_/urls_par_numero/trunk/lang/ paquet-urls_par_numero] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@87797 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- urls_par_numero/trunk/lang/paquet-urls_par_numero_fr.php
+++ urls_par_numero/trunk/lang/paquet-urls_par_numero_fr.php
@@ -1,12 +1,13 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/urls_par_numero/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-	'urls_par_numero_slogan' => 'Court et mémorisable',
-	'urls_par_numero_description' => 'Des urls constituées uniquement du numéro de l\'article. Pour les autres objets, prend le titre de l\'objet au singulier suivit de son numéro.'
+	// U
+	'urls_par_numero_description' => 'Des urls constituées uniquement du numéro de l’article. Pour les autres objets, prend le titre de l’objet au singulier suivit de son numéro.',
+	'urls_par_numero_slogan' => 'Court et mémorisable'
 );
 
 ?>
