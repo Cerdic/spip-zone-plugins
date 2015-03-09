c2322c486dce20d4bccd1085f85818c09b9644fc
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-03-09 21:16:15 +0000

    slogan bien genrée + descriptif correct
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@87811 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- urls_par_numero/trunk/lang/paquet-urls_par_numero_fr.php
+++ urls_par_numero/trunk/lang/paquet-urls_par_numero_fr.php
@@ -6,8 +6,9 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// U
-	'urls_par_numero_description' => 'Des urls constituées uniquement du numéro de l’article. Pour les autres objets, prend le titre de l’objet au singulier suivit de son numéro.',
-	'urls_par_numero_slogan' => 'Court et mémorisable'
+	'urls_par_numero_description' => 'Des urls constituées uniquement du numéro de l\'article. Pour les autres objets, utilise les urls choisies par l\'utilisateur dans la configuration de SPIP ou dans le fichier mes_options.php.',
+	'urls_par_numero_slogan' => 'Courtes et mémorisables',
+
 );
 
 ?>
