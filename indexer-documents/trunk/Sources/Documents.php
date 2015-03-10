f4394e66bbf863a42eacd258308661c06586fc1f
cam.lafit@azerttyu.net <cam.lafit@azerttyu.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-03-10 18:36:15 +0000

    Charger la nouvelle source pour indexer
    * Reprise du code de *indexer* de base pour charger les documents
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@87838 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ indexer-documents/trunk/Sources/Documents.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Spip\Indexer\Sources;
+
+use \Indexer\Sources\Document;
+
+class Documents extends SpipDocuments {
+
+}
---
+++ indexer-documents/trunk/indexer-doc_pipelines.php
@@ -0,0 +1,33 @@
+<?php
+
+// Charge les classes possibles de l'indexer
+require_once _DIR_PLUGIN_INDEXER . 'lib/Composer/Autoload/ClassLoader.php';
+
+$loader = new \Composer\Autoload\ClassLoader();
+
+// register classes with namespaces
+$loader->add('Indexer', _DIR_PLUGIN_INDEXER . 'lib');
+$loader->add('Sphinx',  _DIR_PLUGIN_INDEXER . 'lib');
+$loader->addPsr4('Spip\\Indexer\\Sources\\',  _DIR_PLUGIN_INDEXER-DOC . 'Sources');
+
+$loader->register();
+
+
+/**
+ * Ajouter la source de données documents
+ *
+ *
+ * @param $sources les sources déjà déclarées pour indexer
+ * @return Sources Retourne le flux du pipeline complété
+ */
+function indexer-doc_indexer_sources($sources) {
+
+    if (is_null($sources)){
+		// On crée la liste des sources
+		$sources = new Indexer\Sources\Sources();
+    }
+    // Par défaut on enregistre les articles du SPIP
+    $sources->register('documents', new Spip\Indexer\Sources\Documents());
+
+    return $sources;
+}
