bf596a7856f6ce3af1ad04e1ad5f2ba4e5b292fc
cam.lafit@azerttyu.net <cam.lafit@azerttyu.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-03-10 18:36:25 +0000

    Définition de base des méthodes Sources\Document
    * Pour le moment on remonte uniquement les données connues en base
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@87839 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- indexer-documents/trunk/Sources/Documents.php
+++ indexer-documents/trunk/Sources/Documents.php
@@ -6,4 +6,50 @@ use \Indexer\Sources\Document;
 
 class Documents extends SpipDocuments {
 
+    public function getDocuments($start = 0, $end = 0, $column = 'id_document') {
+
+        $where = array();
+        if ($start) $where[] = "$column >= $start";
+        if ($end)   $where[] = "$column < $end";
+
+        $all = sql_allfetsel(
+            '*',
+            'spip_documents',
+            $where, // Where
+            '', // Gr By
+            '', // Or By
+            '' // Limit
+        );
+
+        $documents = array();
+        foreach ($all as $document) {
+            $documents[] = $this->createDocumentDocument($document);
+        }
+        return $documents;
+    }
+
+    public function createDocumentDocument($document) {
+        $id = $document['id_document'];
+
+        return new Document(array(
+            'id'           => $this->getObjectId('document', $id),
+            'title'        => supprimer_numero($document['titre']),
+            'summary'      => $document['descriptif'],
+            'content'      => $document['descriptif'],
+            'date'         => $document['date'],
+            'uri'          => generer_url_entite_absolue($id, 'document'),
+            'properties'   =>
+            array(
+                'authors'  => $this->getAuthorsProperties('document', $id),
+                'tags'     => $this->getTagsProperties('document', $id),
+                'objet'    => 'document',
+                'id_objet' => $id,
+                'lang'     => isset($document['lang']) ? $document['lang'] : ''
+            )
+        ));
+    }
+
+    public function getBounds() {
+        return $bornes = sql_fetsel(array('MIN(id_document) AS min', 'MAX(id_document) AS max'), 'spip_documents');
+    }
 }
