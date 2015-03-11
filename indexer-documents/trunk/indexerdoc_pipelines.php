a5313e1e4d1f2c92cd20be14fddcf712a7ae5128
cam.lafit@azerttyu.net <cam.lafit@azerttyu.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-03-11 14:37:29 +0000

    Simplifions le chargement de la sources Document pour indexer (RastaPopoulos)
    * Il n'est pas necessaire de recharger toute la partie composer, un include_spip suffit
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@87862 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- indexer-documents/trunk/indexerdoc_pipelines.php
+++ indexer-documents/trunk/indexerdoc_pipelines.php
@@ -1,18 +1,5 @@
 <?php
 
-// Charge les classes possibles de l'indexer
-require_once _DIR_PLUGIN_INDEXER . 'lib/Composer/Autoload/ClassLoader.php';
-
-$loader = new \Composer\Autoload\ClassLoader();
-
-// register classes with namespaces
-$loader->add('Indexer', _DIR_PLUGIN_INDEXER . 'lib');
-$loader->add('Sphinx',  _DIR_PLUGIN_INDEXER . 'lib');
-$loader->addPsr4('Spip\\Indexer\\Sources\\',  _DIR_PLUGIN_INDEXERDOC . 'Sources');
-
-$loader->register();
-
-
 /**
  * Ajouter la source de données documents
  *
@@ -21,6 +8,8 @@ $loader->register();
  * @return Sources Retourne le flux du pipeline complété
  */
 function indexerdoc_indexer_sources($sources) {
+
+    include_spip('Sources/Documents');
 
     if (is_null($sources)){
 		// On crée la liste des sources
