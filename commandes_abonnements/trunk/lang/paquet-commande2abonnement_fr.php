76a7d785527194fcdac1a0786e79999c4c912320
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-03-13 21:19:26 +0000

    Squelette de plugin "Commandes d'abonnements". Pour l'instant ne fait rien. Mais le but : créer ou renouveler des abonnements dont on a commande des offres.
    Concrètement :
    - un utilisateur commande une ou plusieurs offres d'abonnements (= des détails de la commande)
    - un jour la commande est payée (que ce soit mis manuellement ou par Bank)
    - alors ce plugin soit crée un nouvel abonnement entre l'offre et l'utilisateur, soit renouvelle un abonnement déjà existant si on détecte qu'il a déjà un abonnement avec la même offre (= la date de fin qui change)
    
    Mais pour l'instant rien.
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@87944 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ commandes_abonnements/trunk/lang/paquet-commande2abonnement_fr.php
@@ -0,0 +1,13 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/abonnements/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	// C
+	'commande2abonnement_description' => 'Ce plugin fait la liaison entre les Commandes et les Abonnements. Il est possible de commander une offre d’abonnement, et dans ce cas, lorsque la commande est validée comme étant payée, un abonnement est ajouté OU renouvelé (si un avec la même offre est déjà présent) pour l’utilisateur ayant fait la commande.',
+	'commande2abonnement_nom' => 'Commandes d’abonnements',
+	'commande2abonnement_slogan' => 'Permet de commander des abonnements'
+);
+
+?>
