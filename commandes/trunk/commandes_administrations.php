60af33783e2a11dbb1e9321e3ebbf96efd10ae1b
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-03-16 09:29:22 +0000

    Dans SPIP on utilise les tabulations en début de ligne.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@87990 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- commandes/trunk/commandes_administrations.php
+++ commandes/trunk/commandes_administrations.php
@@ -25,50 +25,55 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
  * @return void
 **/
 function commandes_upgrade($nom_meta_base_version, $version_cible) {
-    include_spip('commandes_fonctions');
-    include_spip('inc/config');
-    $maj = array();
+	include_spip('commandes_fonctions');
+	include_spip('inc/config');
+	$maj = array();
 
-    $config = lire_config('commandes');
-    if (!is_array($config)) {
-            $config = array();
-        }
-    $id_webmestre = commandes_id_premier_webmestre();
-    $config = array_merge(array(
-                'duree_vie' => '1',
-                'activer' => '',
-                'quand' => array_keys(commandes_lister_statuts()),
-                'expediteur' => 'webmaster',
-                'expediteur_webmaster' => $id_webmestre,
-                'expediteur_administrateur' => '',
-                'expediteur_email' => '',
-                'vendeur' => 'webmaster',
-                'vendeur_webmaster' => $id_webmestre,
-                'vendeur_administrateur' => '',
-                'vendeur_email' => '',
-                'client' => 'on'
-        ), $config);
+	$config = lire_config('commandes');
+	if (!is_array($config)) {
+		$config = array();
+	}
+	
+	$id_webmestre = commandes_id_premier_webmestre();
+	
+	$config = array_merge(array(
+		'duree_vie'                 => '1',
+		'activer'                   => '',
+		'quand'                     => array_keys(commandes_lister_statuts()),
+		'expediteur'                => 'webmaster',
+		'expediteur_webmaster'      => $id_webmestre,
+		'expediteur_administrateur' => '',
+		'expediteur_email'          => '',
+		'vendeur'                   => 'webmaster',
+		'vendeur_webmaster'         => $id_webmestre,
+		'vendeur_administrateur'    => '',
+		'vendeur_email'             => '',
+		'client'                    => 'on'
+	), $config);
 
-    $maj['create'] = array(
-            array(
-                   'maj_tables', array('spip_commandes','spip_commandes_details'),
-                   ),
-             array(
-                   'ecrire_config','commandes',$config
-                   ),
-            );
-    $maj['0.2']  = array(
-            array('maj_tables', array('spip_commandes_details'))
-            );
-    $maj['0.3']  = array(
-           'ecrire_config','commandes',array('duree_vie'=>3600)
-        );  
-    $maj['0.4']  = array(
-           array('sql_alter', 'TABLE spip_commandes ADD mode varchar(25) not null default ""')
-        ); 
+	$maj['create'] = array(
+		array(
+			'maj_tables', array('spip_commandes','spip_commandes_details'),
+		),
+		array(
+			'ecrire_config','commandes',$config
+		),
+	);
+	
+	$maj['0.2']  = array(
+		array('maj_tables', array('spip_commandes_details'))
+	);
+	
+	$maj['0.3']  = array(
+		'ecrire_config','commandes',array('duree_vie'=>3600)
+	);
+	
+	$maj['0.4']  = array(
+		array('sql_alter', 'TABLE spip_commandes ADD mode varchar(25) not null default ""')
+	); 
 
-    include_spip('base/upgrade');
-    maj_plugin($nom_meta_base_version, $version_cible, $maj);
+	include_spip('base/upgrade');
+	maj_plugin($nom_meta_base_version, $version_cible, $maj);
 }
 
 /**
@@ -79,7 +84,6 @@ function commandes_upgrade($nom_meta_base_version, $version_cible) {
  * @return void
 **/
 function commandes_vider_tables($nom_meta_base_version) {
-
 	sql_drop_table("spip_commandes,spip_commandes_details");
 
 	# Nettoyer les versionnages et forums
