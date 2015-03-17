20b26098cf4130f015562a7d9caf90b150cb29a5
spip@oliver-fr.org <spip@oliver-fr.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-03-17 04:35:10 +0000

    [Salvatore] [source:_plugins_/autorite/trunk/lang/ autorite] Export depuis http://trad.spip.net de la langue oc_ni_mis
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@88033 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ autorite/trunk/lang/autorite_oc_ni_mis.php
@@ -0,0 +1,88 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/autorite?lang_cible=oc_ni_mis
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'activer_mots_cles' => 'Ativà la gestioun per mot clau',
+	'admin_complets' => 'Lu aministratour coumplet',
+	'admin_restreints' => 'Aministratour restrech ?',
+	'administrateur' => 'aministratour',
+	'admins' => 'Lu aministratour',
+	'admins_redacs' => 'Aministratour e Redactour',
+	'admins_rubriques' => 'lu aministratour assouciat a dei rùbrica an :',
+	'auteur_message_advitam' => 'L’autour dóu message, ad vitam',
+
+	// C
+	'config_auteurs' => 'Counfiguracioun dei autour',
+	'config_plugin_qui' => 'Qu pòu <strong>moudificà la counfiguracioun</strong> dei plugin (ativacioun...) ?',
+	'config_site' => 'Counfiguracioun dóu sit',
+	'config_site_qui' => 'Qu pòu <strong>moudificà la counfiguracioun</strong> dóu sit ?',
+	'crayons' => 'Creioun',
+
+	// D
+	'descriptif_1' => 'Aquela pàgina de counfiguracioun es reservada ai webmestre dóu sit :',
+	'droits_des_auteurs' => 'Drech dei autour',
+	'droits_des_redacteurs' => 'Drech dei redatour',
+	'droits_idem_admins' => 'lu meme drech que toui lu aministratour',
+
+	// E
+	'espace_publieur' => 'Espaci pùblicou dubert',
+	'espace_wiki' => 'Espaci wiki',
+	'espace_wiki_mots_cles' => 'Espaci wiki per mot clau',
+
+	// F
+	'forums_qui' => '<strong>Fòrou :</strong> qu pòu moudificà lou countengut dei fòrou :',
+
+	// I
+	'icone_menu_config' => 'Autourità',
+	'info_gere_rubriques' => 'Gerà li rùbrica seguenti :',
+	'info_gere_rubriques_2' => 'Geri li rùbrica seguenti :',
+
+	// M
+	'mots_cles_qui' => '<strong>Mot clau :</strong> qu pòu creà e edità lu mot clau :',
+
+	// N
+	'nouvelles_rubriques' => 'de rùbrica nouveli  a la raïs dóu sit',
+
+	// O
+	'ouvrir_redacs' => 'Durbì ai redatour dóu sit :',
+	'ouvrir_visiteurs_enregistres' => 'Durbì ai visitaire registrat :',
+	'ouvrir_visiteurs_tous' => 'Durbì a toui lu visitaire dóu sit :',
+
+	// P
+	'pas_acces_espace_prive' => '<strong>Mìnga d’achès a l’espaci privat :</strong> lu redatour noun an achès a l’espaci privat.',
+	'personne' => 'Degun',
+	'petitions_qui' => '<strong>Firma :</strong> qu pòu moudificà li firma dei peticioun :',
+	'publication' => 'Publicacioun',
+	'publication_qui' => 'Qu pòu publicà soubre lou sit :',
+
+	// R
+	'redac_tous' => 'Toui lu redatour',
+	'redacs' => 'ai redatour dóu sit',
+	'redacteur' => 'redatour',
+	'refus_1' => '<p>Basta lu webmestre dóu sit',
+
+	// S
+	'sauvegarde_qui' => 'Qu pòu efetuà dei <strong>sauvagarda</strong> ?',
+
+	// T
+	'tous' => 'Toui',
+	'tout_deselectionner' => ' tout deseleciounà',
+
+	// V
+	'visiteur' => 'visitaire',
+	'visiteurs_anonymes' => 'lu visitaire anounime pouòdon creà de nouveli pàgina.',
+	'visiteurs_enregistres' => 'ai visitaire registrat',
+	'visiteurs_tous' => 'a toui lu visitaire dóu sit.',
+
+	// W
+	'webmestre' => 'Le webmestre',
+	'webmestres' => 'Lu webmestre'
+);
+
+?>
