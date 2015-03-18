9d26cb0c85b0c7270d756dca5c66e85b19527dd4
giulia.r@azerttyu.net <giulia.r@azerttyu.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-03-18 04:33:14 +0000

    [Salvatore] [source:_plugins_/autorite/trunk/lang/ autorite] Export depuis http://trad.spip.net de la langue it
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@88047 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- autorite/trunk/lang/autorite_it.php
+++ autorite/trunk/lang/autorite_it.php
@@ -8,74 +8,74 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
-	'admins_redacs' => 'Administrateurs et Rédacteurs', # MODIF
-	'admins_rubriques' => 'les administrateurs associés à des rubriques ont :', # MODIF
-	'attention_crayons' => '<small><strong>Attention.</strong> Les réglages ci-dessous ne peuvent fonctionner que si vous utilisez un plugin proposant une interface d’édition (comme par exemple <a href="http://contrib.spip.net/Les-Crayons">les Crayons</a>).</small>', # MODIF
-	'auteur_message_advitam' => 'L’auteur du message, ad vitam', # MODIF
-	'auteur_message_heure' => 'L’auteur du message, pendant une heure', # MODIF
-	'auteur_modifie_article' => '<strong>Auteur modifie article</strong> : chaque rédacteur peut modifier les articles publiés dont il est l’auteur (et, par conséquent, modérer le forum et la pétition associés).
+	'admins_redacs' => 'Amministratori e Redattori',
+	'admins_rubriques' => 'gli amministratori associati a delle rubriche hanno :',
+	'attention_crayons' => '<small><strong>Attenzione.</strong> Le modifiche qui sotto non possono funzionare se non utilizzi un plug-in con interfaccia di edizione (ad esempio <a href="http://contrib.spip.net/Les-Crayons">les Crayons</a>).</small>', # RELIRE
+	'auteur_message_advitam' => 'L’autore del messaggio, ad vitam', # RELIRE
+	'auteur_message_heure' => 'L’autore del messaggio, per un’ora', # RELIRE
+	'auteur_modifie_article' => '<strong>Autore modifica articolo</strong> : ogni redattore può modificare gli articoli pubblicati dei quali è l’autore (e, di conseguenza, moderare il forum e la le richieste associate).
 	<br />
-	<i>N.B. : cette option s’applique aussi aux visiteurs enregistrés, s’ils sont auteurs et si une interface spécifique est prévue.</i>', # MODIF
-	'auteur_modifie_email' => '<strong>Rédacteur modifie email</strong> : chaque rédacteur peut modifier son email sur sa fiche d’informations personnelles.', # MODIF
-	'auteur_modifie_forum' => '<strong>Auteur modère forum</strong> : chaque rédacteur peut modérer le forum des articles dont il est l’auteur.', # MODIF
-	'auteur_modifie_petition' => '<strong>Auteur modère pétition</strong> : chaque rédacteur peut modérer la pétition des articles dont il est l’auteur.', # MODIF
+	<i>N.B. : questa opzione si applica anche ai visitatori registrati, se sono autori e se è prevista un’interfaccia specifica.</i>', # RELIRE
+	'auteur_modifie_email' => '<strong>Redattore modifica email</strong> : ogni redattore può modificare il suo indirizzo email sulla sua scheda ifnormativa personale.', # RELIRE
+	'auteur_modifie_forum' => '<strong>Autore modera forum</strong> : ogni redattore può moderare il forum per gli articoli dei quali è l’autore.', # RELIRE
+	'auteur_modifie_petition' => '<strong>Autore modera la contestazione</strong> : ogni redattore può moderare la contestazione per gli articoli dei quali è l’autore.', # RELIRE
 
 	// C
-	'config_auteurs_rubriques' => 'Quels types d’auteurs peut-on <b>associer à des rubriques</b> ?', # MODIF
-	'config_auteurs_statut' => 'A la création d’un auteur, quel est le <b>statut par défaut</b> ?', # MODIF
+	'config_auteurs_rubriques' => 'Quali tipi di autori si può <b>associare a delle rubriche</b> ?', # RELIRE
+	'config_auteurs_statut' => 'Creando un autore, qual è lo <b>status di defaut</b> ?', # RELIRE
 
 	// D
-	'deja_defini' => 'Les autorisations suivantes sont déjà définies par ailleurs :', # MODIF
-	'deja_defini_suite' => 'Le plugin « Autorité » ne peut pas les modifier certains des réglages ci-dessous risquent par conséquent de ne pas fonctionner.
-	<br />Pour régler ce problème, vous devrez vérifier si votre fichier <tt>mes_options.php</tt> (ou un autre plugin actif) a défini ces fonctions.', # MODIF
-	'descriptif_1' => 'Cette page de configuration est réservée aux webmestres du site :', # MODIF
-	'descriptif_2' => '<p>Si vous souhaitez modifier cette liste, veuillez éditer le fichier <tt>config/mes_options.php</tt> (le créer le cas échéant) et y indiquer la liste des identifiants des auteurs webmestres, sous la forme suivante :</p>
+	'deja_defini' => 'Le autorizzazioni seguenti sono già state definite altrove :', # RELIRE
+	'deja_defini_suite' => 'Il plug-in « Autorità » non può modificare certe regolazioni e di conseguenza alcune delle seguenti rischiano di non funzionare correttamente.
+	<br />Per risolvere questo problema, devi verificare che il tuo file <tt>mes_options.php</tt> (o un altro plug-in attivo) abbia definito queste funzioni.', # RELIRE
+	'descriptif_1' => 'Questa pagina di configurazione è riservata al webmaster del sito :',
+	'descriptif_2' => '<p>Se desideri modificare questa lista, devi cambiare il file <tt>config/mes_options.php</tt> (o crearlo se necessario) e indicarvi la lista degli identificativi degli altri webmasters, come segue :</p>
 <pre>&lt;?php
   define(
     \'_ID_WEBMESTRES\',
     \'1:5:8\');
 ?&gt;</pre>
-<p>A partir de SPIP 2.1, il est aussi possible de donner les droits de webmestre à un administrateur via la page d’édition de l’auteur.</p>
-<p>A noter : les webmestres définis de cette manière n’ont plus besoin de procéder à l’authentification par FTP pour les opérations délicates (mise à niveau de la base de données, par exemple).</p>
+<p>A partire da SPIP 2.1, è anche possibile dare i diritti di webmaster ad un amministratore tramite la pagina d’editing dell’autore.</p>
+<p>Nota bene : i webmasters così definiti non necessitano più di procedere all’autenticazione FTP per le operazioni più delicate (ad esempio l’aggiornamento del database).</p>
 
-<a href=\'http://contrib.spip.net/Autorite\' class=\'spip_out\'>Cf. documentation</a>', # MODIF
-	'details_option_auteur' => '<small><br />Pour le moment, l’option « auteur » ne fonctionne que pour les auteurs enregistrés (forums sur abonnement, par exemple). Et, si elle est activée, les administrateurs du site ont aussi la capacité d’éditer les forums.
-	</small>', # MODIF
-	'droits_des_redacteurs' => 'Droits des rédacteurs', # MODIF
-	'droits_idem_admins' => 'les mêmes droits que tous les administrateurs', # MODIF
-	'droits_limites' => 'des droits limités à ces rubriques', # MODIF
+<a href=\'http://contrib.spip.net/Autorite\' class=\'spip_out\'>Cf. documentation</a>', # RELIRE
+	'details_option_auteur' => '<small><br />Per il momento, l’opzione « autore » non funziona che per gli autori registrati (forum su abbonamento, per esempio). E, se è attivata, gli amministratori del sito hanno anche la possibilità di modificare i forum.
+	</small>', # RELIRE
+	'droits_des_redacteurs' => 'Diritti dei redattori',
+	'droits_idem_admins' => 'gli stessi diritti di tutti gli amministratori',
+	'droits_limites' => 'dei diritti limitati a queste rubriche',
 
 	// E
-	'effacer_base_option' => '<small><br />L’option recommandée est « personne », l’option standard de SPIP est « les administrateurs » (mais toujours avec une vérification par FTP).</small>', # MODIF
-	'effacer_base_qui' => 'Qui peut <strong>effacer</strong> la base de données du site ?', # MODIF
-	'espace_publieur_detail' => 'Choisissez ci-dessous un secteur à traiter comme un espace de publication ouverte pour les rédacteurs et / ou visiteurs enregistrés (à condition d’avoir une interface, par exemple les crayons et un formulaire pour soumettre l’article) :', # MODIF
-	'espace_publieur_qui' => 'Voulez-vous ouvrir la publication — au-delà des administrateurs :', # MODIF
-	'espace_wiki_detail' => 'Choisissez ci-dessous un secteur à traiter comme un wiki, c’est-à-dire éditable par tous depuis l’espace public (à condition d’avoir une interface, par exemple les crayons) :', # MODIF
-	'espace_wiki_mots_cles_detail' => 'Choisissez ci-dessous les mots clef qui activeront le mode wiki, c’est-à-dire éditable par tous depuis l’espace public (à condition d’avoir une interface, par exemple les crayons)', # MODIF
-	'espace_wiki_mots_cles_qui' => 'Voulez-vous ouvrir ce wiki au-delà des administrateurs :', # MODIF
-	'espace_wiki_qui' => 'Voulez-vous ouvrir ce wiki — au-delà des administrateurs :', # MODIF
+	'effacer_base_option' => '<small><br />L’opzione raccomandata è « nessuno », l’opzione standard di SPIP è « gli amministratori » (ma sempre a seguito di verifica FTP).</small>', # RELIRE
+	'effacer_base_qui' => 'Chi può <strong>cancellare</strong> il database del sito ?', # RELIRE
+	'espace_publieur_detail' => 'Scegliete qui sotto un settore da trattare come spazio di pubblicazione aperto per redattori e /o visitatori registrati (a condizione di avere un’interfaccia, ad esempio i pennarelli, ed un formulario per inviare l’articolo) :', # RELIRE
+	'espace_publieur_qui' => 'Vuoi aprire la pubblicazione — al di là degli amministratori :', # RELIRE
+	'espace_wiki_detail' => 'Scegliete qui sotto un settore da trattare come un wiki, ovvero modificabile da tutti a partire dallo spazio pubblico (a condizione di avere un’interfaccia, per esempio i pennarelli) :', # RELIRE
+	'espace_wiki_mots_cles_detail' => 'Scegliete qui sotto le parole chiave che attiveranno la modalità wiki, ovvero modificabile da tutti a partire dallo spazio pubblico (a condizione di avere un’interfaccia, per esempio i pennarelli)', # RELIRE
+	'espace_wiki_mots_cles_qui' => 'Vuoi aprire questo wiki al di là degli amministratori :', # RELIRE
+	'espace_wiki_qui' => 'Vuoi aprire questo wiki — al di là degli amministratori :', # RELIRE
 
 	// I
 	'icone_menu_config' => 'Autorità',
-	'infos_selection' => '(vous pouvez sélectionner plusieurs secteurs avec la touche shift)', # MODIF
-	'interdire_admin' => 'Cochez les cases ci-dessous pour interdire aux administrateurs de créer', # MODIF
+	'infos_selection' => '(puoi selezionare più settori con il tasto shift)', # RELIRE
+	'interdire_admin' => 'Spunta le caselline seguenti per interdire agli amministratori di creare', # RELIRE
 
 	// M
-	'mots_cles_qui' => '<strong>Mots-clés :</strong> qui peut créer et éditer les mots-clés :', # MODIF
+	'mots_cles_qui' => '<strong>Parole chiave :</strong> chi può creare e modificare le parole chiave :', # RELIRE
 
 	// N
-	'non_webmestres' => 'Ce réglage ne s’applique pas aux webmestres.', # MODIF
-	'note_rubriques' => '(Notez que seuls les administrateurs peuvent créer des rubriques, et, pour les administrateurs restreints, cela ne peut se faire que dans leurs rubriques.)', # MODIF
-	'nouvelles_rubriques' => 'de nouvelles rubriques à la racine du site', # MODIF
-	'nouvelles_sous_rubriques' => 'de nouvelles sous-rubriques dans l’arborescence.', # MODIF
+	'non_webmestres' => 'Questa regolazione non si applica ai webmasters.', # RELIRE
+	'note_rubriques' => '(Da notare che solo gli amministratori possono creare delle rubriche, e, per gli amministratori limitati, ciò può venir fatto solo nelle loro rubriche.)', # RELIRE
+	'nouvelles_rubriques' => 'nuove rubriche alla base del sito', # RELIRE
+	'nouvelles_sous_rubriques' => 'nuove sottorubriche nell’arborescenza.', # RELIRE
 
 	// O
-	'ouvrir_redacs' => 'Ouvrir aux rédacteurs du site :', # MODIF
-	'ouvrir_visiteurs_enregistres' => 'Ouvrir aux visiteurs enregistrés :', # MODIF
-	'ouvrir_visiteurs_tous' => 'Ouvrir à tous les visiteurs du site :', # MODIF
+	'ouvrir_redacs' => 'Aprire ai redattori del sito :',
+	'ouvrir_visiteurs_enregistres' => 'Aprire ai visitatori registrati :',
+	'ouvrir_visiteurs_tous' => 'Aprire a tutti i visitatori del sito :',
 
 	// P
-	'pas_acces_espace_prive' => '<strong>Pas d’accès à l’espace privé :</strong> les rédacteurs n’ont pas accès à l’espace privé.', # MODIF
+	'pas_acces_espace_prive' => '<strong>Accesso allo spazio privato interdetto :</strong> i redattori non hanno accesso allo spazio privato.', # RELIRE
 	'petitions_qui' => '<strong>Signatures :</strong> qui peut modifier les signatures des pétitions :', # MODIF
 
 	// R
