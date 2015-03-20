49db8c33f1995838a6cf268a05c87872b37ae2d4
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-03-20 04:33:48 +0000

    [Salvatore] [source:_plugins_/selections_editoriales/trunk/lang/ paquet-selections_editoriales] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@88090 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- selections_editoriales/trunk/lang/paquet-selections_editoriales_fr.php
+++ selections_editoriales/trunk/lang/paquet-selections_editoriales_fr.php
@@ -1,14 +1,14 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/selections_editoriales/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// S
-	'selections_editoriales_description' => 'Composez des listes de contenu, en leur donnant un titre, un ordre, et éventuellement une description et une image. Cela peut être utilisé pour gérer éditorialement une page d\'accueil par exemple.',
+	'selections_editoriales_description' => 'Composez des listes de contenu, en leur donnant un titre, un ordre, et éventuellement une description et une image. Cela peut être utilisé pour gérer éditorialement une page d’accueil par exemple.',
 	'selections_editoriales_nom' => 'Sélections éditoriales',
-	'selections_editoriales_slogan' => 'Faites des listes de choses intéressantes',
+	'selections_editoriales_slogan' => 'Faites des listes de choses intéressantes'
 );
 
 ?>
