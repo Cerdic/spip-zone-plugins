91e04ef13bce272e57f47c56313f1bba16783d50
spip@oliver-fr.org <spip@oliver-fr.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-03-20 04:33:27 +0000

    [Salvatore] [source:_plugins_/ancres_douces/lang/ paquet-ancresdouces] Export depuis http://trad.spip.net de la langue oc_ni_mis
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@88088 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ ancres_douces/lang/paquet-ancresdouces_oc_ni_mis.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-ancresdouces?lang_cible=oc_ni_mis
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'ancresdouces_description' => 'Aqueu plugin permete un defilamen countìnuou de la pàgina coura lou visitaire clica soubre un estac apountant devers una àncoura de l’article en cours de letura.',
+	'ancresdouces_nom' => 'Àncoura douci',
+	'ancresdouces_slogan' => 'Dei àncoura en douçour'
+);
+
+?>
