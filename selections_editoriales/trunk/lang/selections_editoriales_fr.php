c3e770331557b13258a3c7d87f95c9161b73fb2f
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-03-20 04:33:54 +0000

    [Salvatore] [source:_plugins_/selections_editoriales/trunk/lang/ selections_editoriales] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@88091 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- selections_editoriales/trunk/lang/selections_editoriales_fr.php
+++ selections_editoriales/trunk/lang/selections_editoriales_fr.php
@@ -1,18 +1,18 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/selections_editoriales/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
- 
+
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'configurer_objets_label' => 'Activer l’ajout de sélections sur les contenus :',
 
 	// S
 	'selections_editoriales_titre' => 'Sélections éditoriales',
 
-	// C
-	'configurer_objets_label' => 'Activer l’ajout de sélections sur les contenus :',
-
 	// T
-	'titre_page_configurer_selections_editoriales' => 'Configuration des séléctions éditoriales',
+	'titre_page_configurer_selections_editoriales' => 'Configuration des séléctions éditoriales'
 );
 
 ?>
