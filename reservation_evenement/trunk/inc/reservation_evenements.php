4ddcad01a644a53e5ab8ba9ae57b60a26a63ba5b
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-03-22 16:46:54 +0000

    - plus de paramètres pour la fonction - calculer la branche
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@88146 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- reservation_evenement/trunk/inc/reservation_evenements.php
+++ reservation_evenement/trunk/inc/reservation_evenements.php
@@ -9,7 +9,11 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
  * @param  int $id Id de l'objet
  * @param  string $objet L'objet
  * @param  array $rubrique_reservation Les rubriques de la zone Réservation Évènement
- * @param  array $options Possible valeur array('tableau'=>'oui')
+ * @param  array $options Possible valeur array(
+ * 												'tableau'=>'oui',
+ * 												'where'=>'requete sql',
+ * 												'select=>'les champs',
+ * 												'resultat=>'par_id')
  * @return Bolean/array   array si $options['tableau']=oui
  */
 function rubrique_reservation($id='',$objet,$rubrique_reservation='',$options=array()){
@@ -17,9 +21,13 @@ function rubrique_reservation($id='',$objet,$rubrique_reservation='',$options=ar
 	if(!$rubrique_reservation){
 		include_spip('inc/config');
 		include_spip('formulaires/selecteur/generique_fonctions');
+		include_spip('inc/rubriques');		
 		$config=lire_config('reservation_evenement/',array());
 		$rubrique_reservation=isset($config['rubrique_reservation'])?picker_selected($config['rubrique_reservation'],'rubrique'):'';
 	}
+	//Chercher les rubriques de la branche
+	if(is_array($rubrique_reservation))$rubrique_reservation=explode(',',calcul_branche_in($rubrique_reservation));
+	
 
 	//Si une zone a été définit
 	if(is_array($rubrique_reservation) and count($rubrique_reservation)!=0){
@@ -28,31 +36,63 @@ function rubrique_reservation($id='',$objet,$rubrique_reservation='',$options=ar
 		if($id){
 			//On teste si l'objet se trouve dans la zone
 			if($objet=='article'){
-				$i=sql_getfetsel('id_article','spip_articles','id_article='.$id.' AND id_rubrique IN ('.implode(',',$rubrique_reservation).')');
+				$select='id_article';
+				$from=array('spip_articles');
+				$where=array('id_article='.$id.' AND id_rubrique IN ('.implode(',',$rubrique_reservation).')');				
 			}
 			elseif($objet=='evenement'){
-				$i=sql_getfetsel('e.id_evenement','spip_evenements AS e INNER JOIN spip_articles AS a ON e.id_article=a.id_article','e.id_evenement='.$id.' AND a.id_rubrique IN ('.implode(',',$rubrique_reservation).')');
+				$select='e.id_evenement';
+				$from=array('spip_evenements AS e INNER JOIN spip_articles AS a ON e.id_article=a.id_article');
+				$where=array('e.id_evenement='.$id.' AND a.id_rubrique IN ('.implode(',',$rubrique_reservation).')');
 			}
 			elseif($objet=='rubrique'){
-				$i=sql_getfetsel('id_rubrique','spip_rubriques','id_rubrique='.$id.' AND id_rubrique IN ('.implode(',',$rubrique_reservation).')');
+				$select='id_rubrique';
+				$from=array('spip_rubriques');
+				$where=array('id_rubrique='.$id.' AND id_rubrique IN ('.implode(',',$rubrique_reservation).')');				
 			}
+			if(isset($options['e.id_evenement']))array_push($where,$options['where']);
 					
-			if($i) $return=true; //Objet se trouve dans la zone
+			if(sql_getfetsel($select,$from,$where)) $return=true; //Objet se trouve dans la zone
 			else $return=false;//Objet ne se trouve pas dans la zone
 			
 		}
-		//Afficher les id_articles se tropuvant dans la zone
+		//Afficher les id_articles se trouvant dans la zone
 		elseif(isset($options['tableau']) AND $options['tableau']=='oui'){
 			//On teste si l'objet se trouve dans la zone
 			if($objet=='article'){
-				$sql=sql_select('id_article','spip_articles','id_rubrique IN ('.implode(',',$rubrique_reservation).')');
+				$select=array('id_article');
+				$from=array('spip_articles');
+				$where=array('id_rubrique IN ('.implode(',',$rubrique_reservation).')');				
 			}
+			if($objet=='evenement'){
+				$select=array('e.id_evenement');
+				$from=array('spip_evenements AS e INNER JOIN spip_articles AS a ON e.id_article=a.id_article');
+				$where=array('e.id_evenement_source=0 AND a.id_rubrique IN ('.implode(',',$rubrique_reservation).')');
+			}
+			if($objet=='rubrique'){
+				$select=array('id_rubrique');
+				$from=array('spip_rubriques');
+				$where=array('id_rubrique IN ('.implode(',',$rubrique_reservation).')');	
+			}
+			if(isset($options['where']))array_push($where,$options['where']);
+			if(isset($options['select'])){
+				if($options['select'] != '*')array_push($where,$options['where']);
+				else $select=$options['select'];
+				}			
+								
+			$sql=sql_select($select,$from,$where);
 			
-			if($sql){
-				$ids=array();
+			$ids=array();
+			if(!isset($options['resultat'])){
 				while($data=sql_fetch($sql)){
 					$ids[]=$data['id_'.$objet];
 				}
+			}
+			elseif($options['resultat']=='par_id'){
+				
+				while($data=sql_fetch($sql)){
+					$ids[$data['id_evenement']]=$data;
+				}					
 			}
 			$return=$ids;
 		}
