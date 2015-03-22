bf43b74eb546460c5b98ff7b1a2db21dfcd544e5
spip@oliver-fr.org <spip@oliver-fr.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-03-22 04:33:23 +0000

    [Salvatore] [source:_plugins_/autorite/trunk/lang/ autorite] Export depuis http://trad.spip.net de la langue oc_ni_mis
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@88137 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- autorite/trunk/lang/autorite_oc_ni_mis.php
+++ autorite/trunk/lang/autorite_oc_ni_mis.php
@@ -22,16 +22,23 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'auteur_modifie_article' => '<strong>Autour moudifica article</strong> : cada redatour pòu moudificà lu article publicat que sìgue l’autour.
 	<br />
 	<i>N.B. : aquela oupcioun  s’aplica finda ai visitaire registrat, se soun autour e se una interfaça especifica es prevista.</i>',
+	'auteur_modifie_email' => '<strong>Redatour moudifica e-mail</strong> : cada redatour pòu moudificà lou siéu e-mail soubre lou siéu fueiet d’infourmacioun persounali.',
+	'auteur_modifie_forum' => '<strong>Autour moudera fòrou</strong> : cada redatour pòu moudera lou fòrou dei article qu’es l’autour.',
+	'auteur_modifie_petition' => '<strong>Autour moudera peticioun</strong> : cada redatour pòu moudera la peticioun dei article qu’es l’autour.',
 
 	// C
 	'config_auteurs' => 'Counfiguracioun dei autour',
+	'config_auteurs_rubriques' => 'Que tìpou d’autour si pòu <b>assoucià en dei rùbrica</b> ?',
 	'config_plugin_qui' => 'Qu pòu <strong>moudificà la counfiguracioun</strong> dei plugin (ativacioun...) ?',
 	'config_site' => 'Counfiguracioun dóu sit',
 	'config_site_qui' => 'Qu pòu <strong>moudificà la counfiguracioun</strong> dóu sit ?',
 	'crayons' => 'Creioun',
 
 	// D
+	'deja_defini' => 'Li autourisacioun seguenti soun jà definidi per autre :',
 	'descriptif_1' => 'Aquela pàgina de counfiguracioun es reservada ai webmestre dóu sit :',
+	'details_option_auteur' => '<small><br />Per lou moumen, l’oupcioun « autour » founciouna qu’embé lu autour registrat (fòrou soubre abounamen, per isemple). E, se es ativada, lu aministratour dóu sit an finda la capacità d’édità lu fòrou.
+	</small>',
 	'droits_des_auteurs' => 'Drech dei autour',
 	'droits_des_redacteurs' => 'Drech dei redatour',
 	'droits_idem_admins' => 'lu meme drech que toui lu aministratour',
@@ -43,6 +50,8 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'espace_wiki' => 'Espaci wiki',
 	'espace_wiki_mots_cles' => 'Espaci wiki per mot clau',
 	'espace_wiki_mots_cles_detail' => 'Chausissès aquì souta lu mot clau qu’ativeron lou mòdou wiki, es a dire editable per toui da l’espaci pùblicou (a coundicioun d’avé una interfaça, per isemple lu creioun)',
+	'espace_wiki_mots_cles_qui' => 'Voulès dubrì aqueu wiki delà dai aministratour :',
+	'espace_wiki_qui' => 'Voulès dubrì aqueu wiki — delà dai aministratour :',
 
 	// F
 	'forums_qui' => '<strong>Fòrou :</strong> qu pòu moudificà lou countengut dei fòrou :',
@@ -51,6 +60,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'icone_menu_config' => 'Autourità',
 	'info_gere_rubriques' => 'Gerà li rùbrica seguenti :',
 	'info_gere_rubriques_2' => 'Geri li rùbrica seguenti :',
+	'infos_selection' => '(poudès seleciounà proun setour embé lou tàstou shift)',
 	'interdire_admin' => 'Entacàs li casa aquì souta da prouhibì ai aministratour da creà ',
 
 	// M
