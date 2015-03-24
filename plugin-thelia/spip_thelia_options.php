a51fe76790af86947ddf095894f61adb84805b58
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-03-24 09:49:26 +0000

    inclusion parfois necessaire (spip 3)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@88173 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- plugin-thelia/spip_thelia_options.php
+++ plugin-thelia/spip_thelia_options.php
@@ -22,5 +22,8 @@ $thelia_path = ini_get("include_path") . ":" . _RACINE_THELIA;
 
 include_spip('base/spip_thelia_produits_associes');
 $GLOBALS['liste_des_authentifications']['thelia'] = 'thelia';
+if (!function_exists('lire_config')){
+	include_spip('inc/config');
+}
 $GLOBALS['thelia_statut_nouvel_auteur'] = lire_config("spip_thelia/statutclients_spip_thelia", "6forum");
 
