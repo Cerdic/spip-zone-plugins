952e2d990f6f694a79f211d502b7752e0554e6a2
spip@oliver-fr.org <spip@oliver-fr.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-03-26 04:33:32 +0000

    [Salvatore] [source:_plugins_/autorite/trunk/lang/ paquet-autorite] Export depuis http://trad.spip.net de la langue oc_ni_mis
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@88227 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ autorite/trunk/lang/paquet-autorite_oc_ni_mis.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-autorite?lang_cible=oc_ni_mis
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'autorite_description' => 'Aqueu plugin permete de counfigurà dei autourisacioun diferenti d’aqueli predefinidi.',
+	'autorite_nom' => 'Autourità',
+	'autorite_slogan' => 'Quauque reglage d’autourisacioun (ajustàs lu vouòstre !)'
+);
+
+?>
