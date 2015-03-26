fd3a4668f7540f331ddde2e14ded55a4c42f0cd9
spip@oliver-fr.org <spip@oliver-fr.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-03-26 04:33:37 +0000

    [Salvatore] [source:_plugins_/boussole/trunk/lang/ boussole] Export depuis http://trad.spip.net de la langue oc_ni_mis
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@88228 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ boussole/trunk/lang/boussole_oc_ni_mis.php
@@ -0,0 +1,131 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/boussole?lang_cible=oc_ni_mis
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'bouton_actualiser_boussoles' => 'Atualisà li boussola',
+	'bouton_actualiser_caches' => 'Atualisà lu amagadou',
+	'bouton_boussole' => 'Boussola',
+	'bouton_importer' => 'Empourtà',
+	'bouton_importer_boussole' => 'Empourtà lu sit d’una boussola',
+	'bouton_retirer_boussole' => 'Retirà la boussola',
+	'bouton_retirer_serveur' => 'Retirà lou servidou',
+	'bouton_supprimer' => 'Suprimà',
+	'bouton_synchroniser' => 'Sincronisà li dounada',
+	'bulle_afficher' => 'Afichà en lu moudel',
+	'bulle_aller_site' => 'Anà soubre la pàgina dóu sit referençat',
+	'bulle_cacher' => 'Noun afichà en lu moudel',
+	'bulle_deplacer_bas' => 'Desplaçà en per avau',
+	'bulle_deplacer_haut' => 'Desplaçà en per amount',
+
+	// C
+	'colonne_alias' => 'Alias',
+	'colonne_description_cache' => 'Descricioun',
+	'colonne_fichier_cache' => 'Amagadou',
+	'colonne_nbr_sites' => 'Counten',
+	'colonne_prefixe_plugin' => 'Plugin ?',
+	'colonne_serveur' => 'Servidou',
+	'colonne_titre' => 'Titre',
+	'colonne_url' => 'URL',
+	'colonne_version' => 'Versioun',
+
+	// D
+	'description_noisette_boussole' => 'Afichage estandart d’una boussola. Poudès chausì lou moudel d’afichage (estac testual, logou...) couma pura la siéu counfiguracioun prechisa',
+	'description_noisette_boussole_actualite' => 'Afichage dei article sindicat dei sit d’una boussola segoun lou moudel d’afichage <code>boussole_liste_actualite</code>.',
+	'description_noisette_boussole_contenu_z' => 'Afichage de touti li infourmacioun d’une boussola couma countengut  principal d’una pàgina Z e segoun lou moudel d’afichage <code>boussole_contenu_z</code>.',
+	'description_noisette_boussole_fil_ariane' => 'Afichage dóu fiéu d’Ariana d’una boussola.',
+	'description_page_boussole' => 'Pàgina dei infourmacioun detaiadi d’una boussola',
+
+	// I
+	'info_activite_serveur' => 'En mancança, la founcioun servidou dóu plugin noun es ativada. Poudès l’ativà en chausissent l’oupcioun courrespoundenta aquì souta e en lì atribuissent un noum.',
+	'info_ajouter_serveur' => 'Aqueu fourmulari vi permete de declarà un servidou de boussola. En mancança, lou servidou « spip » es toujou achessible dai sit client.',
+	'info_alias_boussole_manuelle' => 'Sesissès l’alias de la boussola manuala couma definit en lou siéu fichié XML.',
+	'info_boite_boussoles_gerer_serveur' => '<strong>Aquela pàgina es achessible basta ai respounsable dóu sit.</strong><p>Permete de metre a jou manualamen l’amagadou dei boussola ousteladi per aqueu sit servidou. Es poussible de telecargà lu amagadou en clicant soubre lu siéu noum en la lista.</p><p>Un fourmulari permete finda de declarà dei boussola manuali ousteladi soubre lou sit.</p>',
+	'info_boussole_manuelle' => 'Boussola Manuala',
+	'info_cache_boussole' => 'Amagadou de la boussola « @boussole@ »',
+	'info_cache_boussoles' => 'Amagadou dei boussola ousteladi',
+	'info_declarer_boussole_manuelle' => 'Aqueu fourmulari vi permete de declarà una boussola manuala oustelada per aqueu sit Un còu declarada, la boussola devendrà achessible per lu sit client utilisant aqueu servidou.',
+	'info_liste_aucun_serveur' => 'Mìnga servidou es estat counfigurat per lou sit pràtiga.',
+	'info_rubrique_parent' => 'Da creà lu sit de la boussola devès chausì una rùbrica d’acuèlh.',
+	'info_site_boussole' => 'Aqueu sit fa partit de la boussola :',
+	'info_site_boussoles' => 'Aqueu sit fa partit dei boussola :',
+	'info_url_serveur' => 'Sesissès l’URL dóu sit servidou.',
+
+	// L
+	'label_1_boussole' => '@nb@ boussola',
+	'label_1_site' => '@nb@ sit',
+	'label_activite_serveur' => 'Ativà la founcioun servidou ?',
+	'label_actualise_le' => 'Atualisada lou',
+	'label_affiche' => 'Afichat ?',
+	'label_afficher_descriptif' => 'Afichà lou descritiéu dei sit ?',
+	'label_afficher_lien_accueil' => 'Afichà l’estac vers la pàgina d’acuèlh ?',
+	'label_afficher_slogan' => 'Afichà l’eslougan dei sit ?',
+	'label_alias_boussole' => 'Alias de la boussola',
+	'label_ariane_separateur' => 'Separatour :',
+	'label_boussole' => 'Boussola da afichà',
+	'label_descriptif' => 'Descritiéu',
+	'label_fichier_xml' => 'Fichié XML',
+	'label_logo' => 'Logou',
+	'label_mode' => 'Chausissès una boussola',
+	'label_modele' => 'Moudel d’afichage',
+	'label_n_boussoles' => '@nb@ boussola',
+	'label_n_sites' => '@nb@ sit',
+	'label_nom' => 'Noum',
+	'label_nom_serveur' => 'Noum dóu servidou',
+	'label_publier_import' => 'Publicà autoumaticamen lu sit nouvéu. L’estatut dei sit qu’esiston denant l’emport noun serà moudificat',
+	'label_slogan' => 'Eslougan',
+	'label_titre_boussole' => 'Afichà lou titre de la boussola ?',
+	'label_titre_groupe' => 'Afichà lou titre dóu group ?',
+	'label_titre_site' => 'Afichà lou titre dei sit ?',
+	'label_type_bulle' => 'Infourmacioun afichada en la boufa de cada estac',
+	'label_type_description' => 'Descricioun afichada a coustà dóu logou',
+	'label_url' => 'URL',
+	'label_url_serveur' => 'URL dóu servidou',
+	'label_version' => 'Versioun',
+
+	// M
+	'message_nok_0_site_importe' => 'Mìnga sit noun es estat empourtat a partì de la boussola @boussole@.',
+	'message_nok_aucune_boussole_hebergee' => 'Mìnga boussola es encara oustelada soubre lou servidou « @serveur@ ».',
+	'message_nok_boussole_non_hebergee' => 'La boussola « @alias@ » noun es oustelada soubre lou servidou « @serveur@ ».',
+	'message_nok_cache_boussole_indisponible' => 'Lou fichié amagadou de la boussola « @alias@ » noun es dispounible soubre lou servidou « @serveur@ ».',
+	'message_nok_cache_liste_indisponible' => 'Lou fichié amagadou de la lista dei boussola noun es dispounible soubre lou servidou « @serveur@ ».',
+	'message_nok_ecriture_bdd' => 'Errour d’escritura en basa de dounada (taula @table@).',
+	'message_ok_1_site_importe' => 'Basta un sit es estat empourtat a partì de la boussola @boussole@.',
+	'message_ok_boussole_actualisee' => 'La boussola « @fichier@ » es estada metuda a jou.',
+	'message_ok_boussole_ajoutee' => 'La boussola « @fichier@ » es estat ajustada.',
+	'message_ok_serveur_ajoute' => 'Lou servidou « @serveur@ » es estat ajustat (@url@).',
+	'modele_boussole_liste_avec_logo' => 'Lista d’estac embé noum, logou e descritiéu',
+	'modele_boussole_liste_par_groupe' => 'Lista dei estac testual per group',
+	'modele_boussole_panorama' => 'Galerìa dei logou',
+
+	// O
+	'onglet_client' => 'Founcioun Client',
+	'onglet_configuration' => 'Counfiguracioun dóu plugin',
+	'onglet_serveur' => 'Founcioun Servidou',
+	'option_aucune_description' => 'Mìnga descricioun',
+	'option_descriptif_site' => 'Descritiéu dóu sit',
+	'option_nom_site' => 'Noum dóu sit',
+	'option_nom_slogan_site' => 'Noum et eslougan dóu sit',
+	'option_slogan_site' => 'Eslougan dóu sit',
+
+	// T
+	'titre_boite_autres_boussoles' => 'Autri boussola',
+	'titre_boite_infos_boussole' => 'BOUSSOLA D’ALIAS',
+	'titre_boite_logo_boussole' => 'LOGOU DE LA BOUSSOLA',
+	'titre_form_ajouter_boussole' => 'Ajustà o metre a jou una boussola',
+	'titre_form_ajouter_serveur' => 'Declarà un servidou de boussola',
+	'titre_form_configurer_serveur' => 'Counfigurà la founcioun servidou',
+	'titre_formulaire_configurer' => 'Counfiguracioun de l’afichage de la boussola',
+	'titre_liste_boussoles' => 'Lista dei boussola dispounibli a l’afichage',
+	'titre_liste_caches' => 'Lista dei amagadou dei boussola oustelagadi',
+	'titre_page_boussole' => 'Gestioun dei boussola',
+	'titre_page_configurer' => 'Counfiguracioun dóu plugin boussola',
+	'titre_page_importer_boussole' => 'Empourtacioun d’una boussola'
+);
+
+?>
