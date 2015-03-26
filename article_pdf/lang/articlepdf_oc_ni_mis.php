08075d485e0728d368b9b8bee025c24f8f608c69
spip@oliver-fr.org <spip@oliver-fr.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-03-26 04:33:21 +0000

    [Salvatore] [source:_plugins_/article_pdf/lang/ articlepdf] Export depuis http://trad.spip.net de la langue oc_ni_mis
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@88225 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ article_pdf/lang/articlepdf_oc_ni_mis.php
@@ -0,0 +1,24 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/articlepdf?lang_cible=oc_ni_mis
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'copyright' => 'Copyright © ',
+
+	// D
+	'date_de_mise_en_ligne' => 'Data de metuda en ligna : ',
+	'description' => 'Descricioun : ',
+
+	// E
+	'enregistrer_pdf' => 'Registrà au fourmat PDF',
+
+	// P
+	'post_scriptum' => 'Post-Escrich : '
+);
+
+?>
