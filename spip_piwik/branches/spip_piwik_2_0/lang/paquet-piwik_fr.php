6225ae2073de446eae135a9067326c5acded2895
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-03-26 04:34:03 +0000

    [Salvatore] [source:_plugins_/spip_piwik/branches/spip_piwik_2_0/lang/ paquet-piwik] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@88231 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spip_piwik/branches/spip_piwik_2_0/lang/paquet-piwik_fr.php
+++ spip_piwik/branches/spip_piwik_2_0/lang/paquet-piwik_fr.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/spip_piwik/spip_piwik_2_0/lang/
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/spip_piwik/branches/spip_piwik_2_0/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
