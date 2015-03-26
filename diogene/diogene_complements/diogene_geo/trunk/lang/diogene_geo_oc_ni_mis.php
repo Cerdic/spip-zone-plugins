8a0adace94acc5ec6aa707197a125dbd576158c0
spip@oliver-fr.org <spip@oliver-fr.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-03-26 04:33:46 +0000

    [Salvatore] [source:_plugins_/diogene/diogene_complements/diogene_geo/trunk/lang/ diogene_geo] Export depuis http://trad.spip.net de la langue oc_ni_mis
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@88229 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ diogene/diogene_complements/diogene_geo/trunk/lang/diogene_geo_oc_ni_mis.php
@@ -0,0 +1,33 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/diogene_geo?lang_cible=oc_ni_mis
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'adresse' => 'Adressa',
+
+	// G
+	'gis_info_descriptif' => 'Descritiéu dóu pounch',
+	'gis_info_titre' => 'Titre dóu pounch',
+
+	// L
+	'label_geo_forcer_existant' => 'Fourçà la selecioun d’un pounch',
+	'latitude' => 'Latituda',
+	'longitude' => 'Loungituda',
+
+	// M
+	'message_article_geolocaliser' => 'Geoloucalisà aquela pàgina',
+
+	// N
+	'non_localisable' => 'Noun loucalisable',
+	'non_localisable_case' => 'Aqueu media noun es loucalisable',
+
+	// R
+	'recherche' => 'Recerca'
+);
+
+?>
