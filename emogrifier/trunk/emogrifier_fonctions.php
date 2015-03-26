eea68a2f6197749d696495a765b7e38ac711a611
p@henix.be <p@henix.be@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-03-26 11:41:41 +0000

    Cache poussière
    Par défaut, loadHTML envoie des warnings si le html est malformé.
    C'est assez commun avec les mails.
    On ajoute une constante pour pouvoir désactiver ces warnings.
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@88234 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- emogrifier/trunk/emogrifier_fonctions.php
+++ emogrifier/trunk/emogrifier_fonctions.php
@@ -19,6 +19,9 @@ function filtre_emogrifier($html, $fichier_css=_EMOGRIFIER_CSS) {
   include_spip('lib/emogrifier/emogrifier');
 
   $css = file_get_contents(find_in_path($fichier_css));
+  // Pouvoir dire à DOMDocument.loadHTML de râler en silence sur le html mal formé
+  if (!_EMOGRIFIER_LIBXML_ERROR)
+      libxml_use_internal_errors(true);
   $htmldoc = new Emogrifier($html, $css);
 
   return $htmldoc->emogrify();
---
+++ emogrifier/trunk/emogrifier_options.php
@@ -0,0 +1,8 @@
+<?php
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+
+// Par défaut, les erreur de html seront affichée
+if (!defined('_EMOGRIFIER_LIBXML_ERROR'))
+    define('_EMOGRIFIER_LIBXML_ERROR', true);
