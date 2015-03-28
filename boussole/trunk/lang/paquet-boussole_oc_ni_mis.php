5eb20dd81682a4afb8f506c5b8bcfe31fcfafb24
spip@oliver-fr.org <spip@oliver-fr.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-03-28 04:33:25 +0000

    [Salvatore] [source:_plugins_/boussole/trunk/lang/ paquet-boussole] Export depuis http://trad.spip.net de la langue oc_ni_mis
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@88258 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ boussole/trunk/lang/paquet-boussole_oc_ni_mis.php
@@ -0,0 +1,18 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-boussole?lang_cible=oc_ni_mis
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'boussole_description' => 'Instalàs aqueu plugin e aurès achès, en lu vouòstre esqueletrou, a toui lu sit de la Galassìa SPIP en utilisant
+lu moudel, li avelana o la pàgina Z proupausat.
+_ Per lu bricoulaire, poudès finda utilisà aqueu plugin da afichà la vouòstra propri lista de sit e counceure lu vouòstre propri afichage.',
+	'boussole_nom' => 'Boussola',
+	'boussole_slogan' => 'Li bouòni adressa de la galassìa SPIP o de fouòra !'
+);
+
+?>
