e693f6b2d40a2fbf5632b3de2f31818010aa9bd4
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-03-29 17:46:00 +0000

    Utiliser lire_config en lieu et place de $GLOBALS pour une meta
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@88292 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- boussole/trunk/formulaires/configurer_boussole_serveur.php
+++ boussole/trunk/formulaires/configurer_boussole_serveur.php
@@ -5,16 +5,16 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 
 function formulaires_configurer_boussole_serveur_verifier() {
 	$erreurs = array();
+	include_spip('inc/config');
 	if (_request('actif')) {
 		$nom = _request('nom');
 		if (!$nom)
 			$erreurs['nom'] = _T('info_obligatoire');
 		else if (($nom == 'spip')
-			AND (rtrim($GLOBALS['meta']['adresse_site'], '/') != 'http://boussole.spip.net'))
+			AND (rtrim(lire_config('adresse_site'), '/') != 'http://boussole.spip.net'))
 			$erreurs['nom'] = _T('boussole:message_nok_nom_serveur_spip');
 	}
 	return $erreurs;
 }
-
 
 ?>
