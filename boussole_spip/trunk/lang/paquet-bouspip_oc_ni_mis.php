00026c7867c60f74976e6238c705bc94069b13b7
spip@oliver-fr.org <spip@oliver-fr.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-03-29 03:33:16 +0000

    [Salvatore] [source:_plugins_/boussole_spip/trunk/lang/ paquet-bouspip] Export depuis http://trad.spip.net de la langue oc_ni_mis
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@88271 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ boussole_spip/trunk/lang/paquet-bouspip_oc_ni_mis.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-bouspip?lang_cible=oc_ni_mis
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'bouspip_description' => 'Aquestou plugin fournisse lou noum, l’url e la descricioun de toui lu sit de la Galassìa SPIP. Dèu estre utilisat embé lou plugin Boussola en mòdou servidou per fin de fournì, vìa un servici web, li siéu infourmacioun a toui lu sit que vouòrgon afichà la boussola SPIP.',
+	'bouspip_nom' => 'Boussola SPIP',
+	'bouspip_slogan' => 'Li bouòni adressa de la galassìa SPIP !'
+);
+
+?>
