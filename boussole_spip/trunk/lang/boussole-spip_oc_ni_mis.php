5fe5a9901350b49842fb6ac65e21f3ad5d87f6c4
spip@oliver-fr.org <spip@oliver-fr.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-03-29 03:33:10 +0000

    [Salvatore] [source:_plugins_/boussole_spip/trunk/lang/ boussole-spip] Export depuis http://trad.spip.net de la langue oc_ni_mis
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@88270 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ boussole_spip/trunk/lang/boussole-spip_oc_ni_mis.php
@@ -0,0 +1,81 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/boussole-spip?lang_cible=oc_ni_mis
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// D
+	'descriptif_boussole_spip' => 'La boussola SPIP raduna dei sit « officiels » de la galassìa SPIP. Definisse per cada sit, lou siéu logou, lou siéu noum, lou siéu eslougan e la siéu descricioun. Noun esitàs pi da l’utilisà en lu vouòstre propri sit da agulhà lu vouòstre visitaire en la galassìa SPIP.',
+	'descriptif_site_spip_blog' => 'Perqué SPIP es un prouget coulabouratiéu, SPIP-BLOG.net  raduna bilhet tècnicou, autoironìa, imour, troll, anounci divers,... Per acò es ben lou reflès de la coumunità SPIP : en premié e subretout touplen de tendressa.',
+	'descriptif_site_spip_contrib' => 'Sit coulabouratiéu, contrib.spip mete a dispousicioun touti li countribucioun estèrni : plugin, escrit, filtre, esquelètrou, doucumentacioun, counsèu e filèta,... fournit a la coumunità (estac de telecargamen) dai utilisaire de SPIP. Lu siéu fòrou acherta l’estac tra lu desfouloupaire e lu utilisaire.',
+	'descriptif_site_spip_core' => 'Lou sit CORE.SPIP.org raduna l’isòrica dei moudificacioun dóu code de SPIP, una gestioun de bilhet que permete de signalà dei bug e de proupausà dei sugestioun de meliouramen e un espaci de telecargamen dei versioun de SPIP.',
+	'descriptif_site_spip_demo' => 'Sit de test reinicialisat cada nuèch, DEMO.SPIP.net permete a cadun de testà SPIP en la siéu darrièra versioun stable (embé l’estatut de redatour o d’aministratour), en un clic e sensa deure l’instalà.',
+	'descriptif_site_spip_doc' => 'CODE.SPIP.net es un espaci de doucumentacioun dóu lougiciau SPIP per li siéu API, lou siéu code sourgent, e quauqu’uni dei siéu founciounamen tècnicou.',
+	'descriptif_site_spip_edgard' => 'Edgard es lou coumpàgnou fedel e infatigable de SPIP IRC doun interven toujou estoble, embé tendressa e umour. Es da la siéu maioun EDGARD.SPIP.net que pouòrta soubre IRC counsèu, respouòsta e bouòna imour. Achessouriamen, Edgard es un robot (mà noun sembla lou saupre...)',
+	'descriptif_site_spip_forum' => 'FORUM.SPIP.net es lou sit d’escambia e d’entradjuda tra lu utilisaire de SPIP. Lou sit, qu’esista en un desenau de lenga, s’ourganisa autour de quatre grani rùbrica : instalacioun e metuda a jou, utilisacioun de l’espaci privat, aministracioun, gestioun, counfiguracioun dóu sit, creacioun d’esquelètrou',
+	'descriptif_site_spip_herbier' => 'L’erbié de SPIP es pas un anuari couma lu autre, es pas. Es un album souvenença de sit realisat embé SPIP, sesit a un moumen de la siéu esistença soubre lou ret. Cuenta una istòria dóu Web, aquela de SPIP, despì 2001. Aquestou erbié raduna lu sit qu’aven vourgut spenglià, perqué interessant per tau countegut agreablemen metut en valour, tala aventura umana, tala founciounalità gaubiamen realisat, o basta visualamen seduchent. Dei sit que si souvengue, que lì siguen revengut, qu’an fach referença n’en marcant la siéu èpouca o de rif o de raf.',
+	'descriptif_site_spip_info' => 'En quauqui pàgina, SPIP-INFO.net respouònde ai questioun que si pauon aquelu que s’interogon sus la capacità de SPIP da servì de basa au siéu prouget de desfouloupamen d’un sit web. Particulié, assouciacioun, empresa troberon aquì touti li infourmacioun da faire la siéu chausida.',
+	'descriptif_site_spip_irc' => 'La coumunità SPIP, que noun duerme, a un canal IRC (discussioun instantànea per internet) dubert en toui : http://webchat.freenode.net/?channels=#spip',
+	'descriptif_site_spip_mag' => 'Descricioun dóu sit SPIP Mag’',
+	'descriptif_site_spip_net' => 'Per lu utilisaire e lu webmèstre, SPIP.net es lou sit ouficial racoumandat en toui aquelu que vouòlon instalà un sit embé SPIP, n’en capì lou lengage dei blouca, balisa e filtre, escriéure e utilisà les esquelètrou. presenta gloussari, tutorial, consèu, isòrica dei versioun e espaci de telecargamen. SPIP.net es traduch en mai de vint lenga.',
+	'descriptif_site_spip_party' => 'Passà lou siéu tems tra una tastiera e un ecran per codà de SPIP : es ben. Si rescouòntra tra desfouloupaire e utilisaire en la vida ver, si meme, autour d’un pastissoun e d’un jus de frùcha (o autri bevanda...) : es pu ben dau bouòn. SPIP-PARTY.net es lou sit que fa lou chensimen de touti aqueli rescouòntra : passadi (rendiconte, fotò) e da venì (calendari, agènda).',
+	'descriptif_site_spip_plugin' => 'PLUGINS.SPIP.net vòu estre l’anuari coumplet dei mòdulou coumplementari per SPIP (plugin, esquelètrou, tèma). Soun presentat per cada mòdulou : descritiéu, autour, lichença, nivèu de coumpatibilità per versioun de SPIP, darrieri moudificacioun apourtadi, estat dei traducioun, estatìstica d’utilisacioun, estac de doucumentacioun e de telecargamen.',
+	'descriptif_site_spip_sedna' => 'SEDNA.SPIP.net es l’agregatour RSS per touta l’atualità de la galassìa SPIP. Li darrièri infourmacioun de mai de 70 sit gravitant autour de SPIP soun aisatamen counsultable.',
+	'descriptif_site_spip_zine' => 'Descricioun dóu sit SPIPZine',
+
+	// N
+	'nom_boussole_spip' => 'Boussola SPIP',
+	'nom_groupe_spip_actualite' => 'Atualità',
+	'nom_groupe_spip_aide' => 'Entrajuda',
+	'nom_groupe_spip_decouverte' => 'Descuberta',
+	'nom_groupe_spip_extension' => 'Countribucioun',
+	'nom_groupe_spip_reference' => 'Doucumentacioun',
+	'nom_site_spip_blog' => 'SPIP Blog',
+	'nom_site_spip_contrib' => 'contrib.spip',
+	'nom_site_spip_core' => 'SPIP Core',
+	'nom_site_spip_demo' => 'SPIP Demo',
+	'nom_site_spip_doc' => 'SPIP Code',
+	'nom_site_spip_edgard' => 'Edgard',
+	'nom_site_spip_forum' => 'SPIP Fòrou',
+	'nom_site_spip_herbier' => 'L’Erbié de SPIP',
+	'nom_site_spip_info' => 'SPIP info',
+	'nom_site_spip_irc' => 'SPIP IRC',
+	'nom_site_spip_mag' => 'SPIP Mag’',
+	'nom_site_spip_net' => 'SPIP.net',
+	'nom_site_spip_party' => 'SPIP Party',
+	'nom_site_spip_plugin' => 'Plugin SPIP',
+	'nom_site_spip_programmer' => 'Prougramà SPIP',
+	'nom_site_spip_sedna' => 'Sedna',
+	'nom_site_spip_test' => 'SPIP Test',
+	'nom_site_spip_trad' => 'Tradurre SPIP',
+	'nom_site_spip_twit' => 'SPIP.org',
+	'nom_site_spip_user' => 'SPIP User',
+	'nom_site_spip_video' => 'Media SPIP',
+	'nom_site_spip_zine' => 'SPIPZine',
+	'nom_site_spip_zone' => 'SPIP Zone',
+
+	// S
+	'slogan_boussole_spip' => 'Perdut en la galassìa SPIP ?',
+	'slogan_groupe_spip_actualite' => 'Atualità de SPIP',
+	'slogan_groupe_spip_aide' => 'Ajuda e cambi autour de SPIP',
+	'slogan_groupe_spip_decouverte' => 'A la descuberta de SPIP',
+	'slogan_groupe_spip_extension' => 'Estencioun e countribucioun a SPIP',
+	'slogan_groupe_spip_reference' => 'Referença SPIP',
+	'slogan_site_spip_contrib' => 'L’espaci dei countribucioun a SPIP',
+	'slogan_site_spip_core' => 'L’espaci de desfouloupamen dei versioun de SPIP',
+	'slogan_site_spip_doc' => 'La doucumentacioun dóu code de SPIP',
+	'slogan_site_spip_forum' => 'Lou fòrou dei utilisaire de SPIP',
+	'slogan_site_spip_herbier' => 'Una selecioun dei sit remarcable en SPIP',
+	'slogan_site_spip_irc' => 'Venès charà soubre lou chat de SPIP',
+	'slogan_site_spip_mag' => 'La gaseta de SPIP',
+	'slogan_site_spip_plugin' => 'L’anuari dei plugin SPIP',
+	'slogan_site_spip_programmer' => 'La doucumentacioun dei desfouloupaire SPIP',
+	'slogan_site_spip_sedna' => 'Lou flus RSS de la galassìa SPIP',
+	'slogan_site_spip_user' => 'La lista d’entrajuda dei utilisaire de SPIP',
+	'slogan_site_spip_zone' => 'L’espaci de desfouloupamen dei countribucioun a SPIP'
+);
+
+?>
