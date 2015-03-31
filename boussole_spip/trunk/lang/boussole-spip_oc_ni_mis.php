31193baa323969b00f44810017a272e45f72f1bd
spip@oliver-fr.org <spip@oliver-fr.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-03-31 03:33:17 +0000

    [Salvatore] [source:_plugins_/boussole_spip/trunk/lang/ boussole-spip] Export depuis http://trad.spip.net de la langue oc_ni_mis
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@88304 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- boussole_spip/trunk/lang/boussole-spip_oc_ni_mis.php
+++ boussole_spip/trunk/lang/boussole-spip_oc_ni_mis.php
@@ -25,8 +25,11 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'descriptif_site_spip_plugin' => 'PLUGINS.SPIP.net vòu estre l’anuari coumplet dei mòdulou coumplementari per SPIP (plugin, esquelètrou, tèma). Soun presentat per cada mòdulou : descritiéu, autour, lichença, nivèu de coumpatibilità per versioun de SPIP, darrieri moudificacioun apourtadi, estat dei traducioun, estatìstica d’utilisacioun, estac de doucumentacioun e de telecargamen.',
 	'descriptif_site_spip_programmer' => 'Mai destinat ai desfouloupaire o webmèstre que àugon jà dei counouissença en PHP, SQL, HTML, CSS et JavaScript, PROGRAMMER.SPIP.net presenta la plupart dei founciounalità (APIs, soubrescrich, pipeline,...) de SPIP embé touplen d’isemple de code. Lou sit ofra en telecargamen l’integralità dóu siéu countegut au fourmat pdf souta lichença lìbera cc-by-sa. PROGRAMMER.SPIP.net es counsultable en francès, inglès e espagnòu.',
 	'descriptif_site_spip_sedna' => 'SEDNA.SPIP.net es l’agregatour RSS per touta l’atualità de la galassìa SPIP. Li darrièri infourmacioun de mai de 70 sit gravitant autour de SPIP soun aisatamen counsultable.',
+	'descriptif_site_spip_test' => 'Soubre GRML.EU, instalàs simplamen lou vouòstre sit SPIP en la siéu darrièra versioun de desfouloupamen. Gauch ai capacità  de mutualisacioun de SPIP : una instalacioun dóu « core » basta, per un mouloun de sit, sias « sus lou vouòstre », en lou « vouòstre » sit, embé la « vouòstra » counfiguracioun. Counven pròpi ben per lu estàgi de fourmacioun a l’utilisacioun de SPIP.',
 	'descriptif_site_spip_trad' => 'L’espaci dei tradutour aculhe toui aquelu que vouòlon ajustà la coumunità dei utilisaire de SPIP en participant au travai de traducioun de SPIP èu-meme e dei siéu diversi countribucioun.',
+	'descriptif_site_spip_twit' => 'SPIP.ORG permete ai utilisaire de publicà de pichin message sus lou Web (140 caracter massimoum) destinat au public o dei siéu amic. La publicacioun pòu si faire dau sit, dai flus RSS o encara da la messagerìa instantànea.',
 	'descriptif_site_spip_user' => 'SPIP@REZO.net es la lista d’entradjuda tra lu utilisaire de SPIP. Li arquivi d’aquesta lista soun counsultable soubre http://archives.rezo.net/archives/spip.mbox/ o, souta la fourma de blog soubre http://blog.gmane.org/gmane.comp.web.spip.user.',
+	'descriptif_site_spip_video' => 'Lou sit MEDIAS.SPIP es un pounch d’intrada per la difusioun dei video  realisadi per o autour de SPIP. Cada utilisaire de SPIP pòu apourtà la siéu countribucioun en proupausant de suport video nouvèu per dei tutorial, dei counferença, dei fourmacioun... Lou soulet coustregnimen es de coumpartì dei video lìberi de drech per fin que cadun pouòsque li counsultà e li utilisà liberamen.',
 	'descriptif_site_spip_zine' => 'Descricioun dóu sit SPIPZine',
 
 	// N
@@ -75,8 +78,10 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'slogan_site_spip_edgard' => 'Un còu de bot e vaga !',
 	'slogan_site_spip_forum' => 'Lou fòrou dei utilisaire de SPIP',
 	'slogan_site_spip_herbier' => 'Una selecioun dei sit remarcable en SPIP',
+	'slogan_site_spip_info' => 'Per descurbì SPIP e la siéu coumunità',
 	'slogan_site_spip_irc' => 'Venès charà soubre lou chat de SPIP',
 	'slogan_site_spip_mag' => 'La gaseta de SPIP',
+	'slogan_site_spip_net' => 'La doucumentacioun ouficiala e telecargamen de SPIP',
 	'slogan_site_spip_plugin' => 'L’anuari dei plugin SPIP',
 	'slogan_site_spip_programmer' => 'La doucumentacioun dei desfouloupaire SPIP',
 	'slogan_site_spip_sedna' => 'Lou flus RSS de la galassìa SPIP',
