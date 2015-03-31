4add87f527377e3a6e35c15a29bf754f88109a16
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-03-31 08:13:42 +0000

    Un filtre #ENV{recherche}|fulltext_recherche_naturelle_fr
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@88312 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- fulltext/trunk/fulltext_fonctions.php
+++ fulltext/trunk/fulltext_fonctions.php
@@ -2,17 +2,17 @@
 
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
-function balise_AIDE_RECHERCHE($p) {
+function balise_AIDE_RECHERCHE($p){
 	if (!function_exists('recuperer_fond'))
 		include_spip('public/assembler');
 	//    $arg = interprete_argument_balise(1, $p);
 	$mess_aide = str_replace("'", "\'", recuperer_fond('aide_recherche', array('lang' => $GLOBALS['spip_lang'])));
-	$p -> code = "'$mess_aide'";
-	$p -> statut = 'html';
+	$p->code = "'$mess_aide'";
+	$p->statut = 'html';
 	return $p;
 }
 
-function lien_objet_ptg($id, $type, $longueur = 80, $connect = NULL) {
+function lien_objet_ptg($id, $type, $longueur = 80, $connect = NULL){
 	include_spip('inc/liens');
 	$titre = traiter_raccourci_titre($id, $type, $connect);
 	$titre = typo($titre['titre']);
@@ -21,4 +21,61 @@ function lien_objet_ptg($id, $type, $longueur = 80, $connect = NULL) {
 	$url = generer_url_entite($id, $type);
 	return "<a href='$url' class='$type'>" . couper($titre, $longueur) . "</a>";
 }
-?>
+
+
+/**
+ * Un filtre pour mettre en forme #ENV{recherche}|fulltext_recherche_naturelle_fr
+ *
+ * (ne marche qu'avec la langue fr)
+ * - on supprime les s et x pluriels
+ * - on ajoute un * sur les mots pour attraper les pluriels quand un singulier est demande
+ * - si des ET ou OU sont dans la requete on les transforme en + et (rien)
+ * - sinon on ajoute des + pour avoir tous les mots (ET par defaut, chaque mot reduit le perimetre)
+ * - les expressions entre "" sont conservees telles quelles
+ *
+ * @param string $recherche
+ * @param bool $strict
+ *   si false on n'ajoute pas les + sur chaque terme
+ * @return string
+ */
+function fulltext_recherche_naturelle_fr($recherche, $strict = true){
+
+	#var_dump($recherche);
+
+	// supprimer caracteres de ponctuation
+	$recherche = strtr($recherche, "-_;,'+*", "       ");
+
+	if (preg_match(",\b(ET|OU)\b,",$recherche)){
+		$strict = false; // pas la peine de faire + car c'est gere par les ET et OU
+		$recherche = preg_replace(",\bOU\b,"," ",$recherche);
+		$recherche = preg_replace(",\bET\s+,","+",$recherche);
+
+		$recherche = "+".trim($recherche); // le premier mot est un ET
+	}
+
+	$recherche = preg_replace(",\s+,", " ", $recherche);
+
+	// les guillemets sont a conserver et on ne touche pas au contenu de la chaine
+	if (preg_match(',["][^"]+["],Uims',$recherche,$matches)){
+		foreach($matches as $match){
+			// corriger le like dans le $q
+			$word = preg_replace(",\s+,Uims","\x1",$match);
+			$recherche = str_replace($match,$word,$recherche);
+		}
+	}
+
+	$recherche = explode(' ', $recherche);
+	foreach ($recherche as $k => $r){
+		if (strlen($r)>=3 AND substr($r,0,1)!=='"' AND substr($r,-1)!='"'){
+			$r = rtrim($r, "sxSX");
+			$recherche[$k] = ($strict ? "+" : "") . $r . "*";
+		}
+	}
+
+	$recherche = implode(" ", $recherche);
+	$recherche = str_replace("\x1"," ", $recherche);
+
+	#var_dump($recherche);
+
+	return $recherche;
+}
