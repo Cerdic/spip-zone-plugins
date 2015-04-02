92b445be7804a1bf2202b6d0de68b3aadb757f03
spip@oliver-fr.org <spip@oliver-fr.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-04-02 03:33:44 +0000

    [Salvatore] [source:_plugins_/chosen/trunk/lang/ chosen] Export depuis http://trad.spip.net de la langue oc_ni_mis
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@88418 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ chosen/trunk/lang/chosen_oc_ni_mis.php
@@ -0,0 +1,31 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/chosen?lang_cible=oc_ni_mis
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cfg_exemple' => 'Isemple',
+	'cfg_exemple_explication' => 'Espligassioun d’estou isemple',
+	'cfg_titre_parametrages' => 'Parametrage',
+	'chosen_titre' => 'Chosen',
+
+	// E
+	'explication_selecteur' => 'Endicàs lou bressai dei elemen que desgancheran chosen. (Espressioun CSS o estenduda jQuery)',
+
+	// L
+	'label_active' => 'Ativà « Chosen » en lou sit pùblicou',
+	'label_selecteur' => 'Seletour',
+	'lang_create_option' => 'Creà',
+	'lang_no_result' => 'Minga resultat',
+	'lang_select_an_option' => 'Seleciounàs una oupcioun',
+	'lang_select_some_option' => 'Seleciounàs una oupcioun o mai',
+
+	// T
+	'titre_page_configurer_chosen' => 'Counfigurà Chosen'
+);
+
+?>
