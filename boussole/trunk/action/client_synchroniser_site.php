7beef341cbe6f85601601c1cae3b6e0bc1daf97d
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-04-05 15:05:09 +0000

    Mise au point de la fonction de synchronisation d'un site par une boussole en SPIP 3.1.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@88476 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- boussole/trunk/action/client_synchroniser_site.php
+++ boussole/trunk/action/client_synchroniser_site.php
@@ -60,9 +60,21 @@ function action_client_synchroniser_site_dist(){
 				$erreur = site_modifier($id_syndic, $contenu);
 
 				if (!$erreur) {
-					// Mise à jour de son logo normal ("on")
-					$iconifier = charger_fonction('iconifier_site', 'inc');
-					$iconifier($id_syndic, 'on', $donnees_site['logo_objet']);
+					if ($donnees_site['logo_objet']) {
+						include_spip('plugins/installer');
+						// Mise à jour du logo du site normal ("on").
+						// Il faut tenir compte de la version de SPIP car l'API a changé à partir de SPIP 3.1
+						if (spip_version_compare($GLOBALS['spip_version_branche'], '3.0.*', '<=')) {
+							$iconifier = charger_fonction('iconifier_site', 'inc');
+							$iconifier($id_syndic, 'on', $donnees_site['logo_objet']);
+						}
+						else {
+							include_spip('inc/distant');
+							include_spip('action/editer_logo');
+							$fichier = _DIR_RACINE . copie_locale($donnees_site['logo_objet'], 'force');
+							logo_modifier('site', $id_syndic, 'on', $fichier);
+						}
+					}
 
 					spip_log("ACTION SYNCHRONISER SITE : id_syndic = $id_site - boussole = $boussole", _BOUSSOLE_LOG . _LOG_INFO);
 				}
