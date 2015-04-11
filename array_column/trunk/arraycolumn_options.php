9f0b0a14d3789b1d911f90ed0442b1b8cd419e28
p@henix.be <p@henix.be@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-04-11 18:22:54 +0000

    Merge branch 'master' of github.com:phenix-factory/spip_array_column
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@88574 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ array_column/trunk/arraycolumn_options.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Options du plugin array_column pour SPIPau chargement
+ *
+ * @plugin     array_column pour SPIP
+ * @copyright  2014
+ * @author     Phenix
+ * @licence    GNU/GPL
+ * @package    SPIP\Arraycolumn\Options
+ */
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+// On inclu une bonne fois pour toutes array_column
+include_spip('lib/array_column/array_column');
---
+++ array_column/trunk/lang/paquet-arraycolumn_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'arraycolumn_description' => 'Charge la fonction array_column de PHP 5.5 pour les version inférieur.',
+	'arraycolumn_nom' => 'array_column pour SPIP',
+	'arraycolumn_slogan' => 'Utiliser array_column avec les version > PHP5.5',
+);
+
+?>
