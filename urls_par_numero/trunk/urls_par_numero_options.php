2bde7ed5edb03930b2a260467625a59b08efd002
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-04-11 18:27:05 +0000

    urls_propres -> doit aussi renvoyer le tableau de décodage des urls en numéro d'articles dans le cas où on ne précise pas l'entité (notamment pour que urls_decoder_url fonctionnement)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@88575 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- urls_par_numero/trunk/urls_par_numero_options.php
+++ urls_par_numero/trunk/urls_par_numero_options.php
@@ -3,12 +3,23 @@
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 function urls_propres($i, $entite, $args='', $ancre=''){
-	//À partir de SPIP 3.1 on est obligé de surchargé, car les urls purement numériques sont encadrés par des guillemets
 	include_spip("urls/propres");
-	$url = urls_propres_dist($i, $entite, $args='', $ancre='');
-	if ($entite=="article"){//pas de - autour des numéros d'article
-		$url = str_replace("-","",$url);
+	if ($entite=='' and is_numeric($i)){
+		$url = array(
+		   array('id_article'=>$i),
+		   'article',
+		   null,
+		   null
+		   );
 		}
+	else{
+		$url = urls_propres_dist($i, $entite, $args='', $ancre='');
+		// Supprimer les tirets des urls purement numériques (SPIP 3.1 et >)
+		if ($entite=="article"){//pas de - autour des numéros d'article
+			$url = str_replace("-","",$url);
+			}
+		// pour que urls_decoder_url fonctionne correctement
+	}
 	return $url;
 	}
 
