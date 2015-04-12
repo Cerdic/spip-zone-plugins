09970aa220b7014f72f30c10f3fdb89188d6a92b
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-04-12 11:12:22 +0000

    paquet du plugin + chaînes de langue du paquet + logo
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@88590 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ selecteur-rubriques-trialpha/trunk/lang/paquet-selecteur-rubriques-trialpha_fr.php
@@ -0,0 +1,13 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/urls_par_numero/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// U
+	'selecteur-rubriques-trialpha_description' => 'Modifie le sélecteur de rubriques pour afficher les rubriques par ordre alphabétique',
+	'selecteur-rubriques-trialpha_slogan' => 'Pour mieux s\'y retrouver'
+);
+
+?>
