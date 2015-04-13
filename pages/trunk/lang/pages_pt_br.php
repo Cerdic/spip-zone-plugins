78c1967c9d1c9e8d503abda91e870d486f77eec3
tradspip.ptipoi@dfgh.net <tradspip.ptipoi@dfgh.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-04-13 03:33:09 +0000

    [Salvatore] [source:_plugins_/pages/trunk/lang/ pages] Export depuis http://trad.spip.net de la langue pt_br
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@88598 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ pages/trunk/lang/pages_pt_br.php
@@ -0,0 +1,37 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/pages?lang_cible=pt_br
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'aucune_page' => 'Nehuma página por enquanto.',
+
+	// C
+	'convertir_article' => 'Converter em matéria',
+	'convertir_page' => 'Converter em página',
+	'creer_page' => 'Criar uma nova página',
+
+	// E
+	'erreur_champ_page_doublon' => 'Este nome já existe',
+	'erreur_champ_page_format' => 'O nome só pode conter letras, números e traços',
+	'erreur_champ_page_taille' => 'Seqüências de 255 caracteres ao máximo',
+
+	// L
+	'label_champ_page' => 'Página:',
+
+	// M
+	'modifier_page' => 'Alterar a página:',
+
+	// P
+	'pages_uniques' => 'Páginas individuais',
+
+	// T
+	'titre_page' => 'Página',
+	'toutes_les_pages' => 'Todas as páginas'
+);
+
+?>
