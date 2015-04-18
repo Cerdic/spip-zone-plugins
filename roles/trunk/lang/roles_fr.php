6b7def39c3ee631ae839f78aab960e7e3971c2fe
tcharlss@bravecassine.com <tcharlss@bravecassine.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-04-18 15:16:41 +0000

    Utiliser des chaînes de langue dans la noisette dropdown (Ybbet).
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@88664 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- roles/trunk/lang/roles_fr.php
+++ roles/trunk/lang/roles_fr.php
@@ -4,10 +4,18 @@
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+	// A
+	'ajouter_role' => 'Ajouter un rôle',
+
 	// C
 	'choisir_role' => 'Choisir…',
-	'saisir_role' => 'Indiquer un rôle…',
-	'saisir_valeur' => 'Valeur',
+
+	// E
+	'enlever_role' => 'Enlever un rôle',
+
+	// M
+	'modifier_roles' => 'Modifier',
+	'modifier_roles_title' => 'Modifier les rôles',
 
 	// R
 	'roles_titre' => 'Rôles',
@@ -15,6 +23,8 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// S
 	'selectionner_roles' => 'Sélectionner le ou les rôles souhaités',
+	'saisir_role' => 'Indiquer un rôle…',
+	'saisir_valeur' => 'Valeur',
 );
 
 ?>
