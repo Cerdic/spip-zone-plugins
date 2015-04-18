9155a13a879445f1cd4845284f33091eefd2acf7
tcharlss@bravecassine.com <tcharlss@bravecassine.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-04-18 15:12:12 +0000

    - markup correct pour le formulaire. - styles : quelques corrections afin que le dropdown s'affiche comme il faut. - langue : ajout d'un slogan et descriptif. - paquet : rastapopoulos en auteur, up de z.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@88663 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- roles_documents/trunk/lang/roles_documents_fr.php
+++ roles_documents/trunk/lang/roles_documents_fr.php
@@ -9,8 +9,13 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'bouton_modifier_roles' => 'Modifier les rôles',
 
 	// R
+	'roles_documents_description' => 'On a souvent besoin d\'identifier certaines images liées aux articles, et de leur attribuer un rôle pour un besoin spécifique : par exemple choisir une image d\'illustration pour un header, choisir une image servant de logo dans les listes, etc.
+	Ce plugin permet d\'attribuer des rôles aux documents liés. Pour l\'instant il est limité aux images. Par défaut seuls 2 rôles génériques sont proposés : « logo » et « logo de survol ». La liste est à compléter en utilisant le pipeline « lister_roles_documents », les rôles utiles variant selon les besoins du squelette employé.',
+	'roles_documents_nom' => 'Rôles de documents',
+	'roles_documents_slogan' => 'Typer des liaisons de documents',
 	'role_logo' => 'Logo',
 	'role_logo_survol' => 'Logo de survol'
+
 );
 
 ?>
