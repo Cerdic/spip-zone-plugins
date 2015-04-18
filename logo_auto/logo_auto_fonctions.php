2b1b5731825d04dbee100e33635545e5c03e9cf5
tetue@rezo.net <tetue@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-04-18 21:23:59 +0000

    Aussitôt activité, ce super petit plugin génère des logos d'article de façon automatique, à partir des images associées aux articles (sans qu'il soit donc nécessaire de leur ajouter des logos).
    Pas de configuration, pas d'intervention dans les squelettes, just click&play !
    
    D'après : http://romy.tetue.net/logos-automatiques-articles-SPIP
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@88669 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ logo_auto/logo_auto_fonctions.php
@@ -0,0 +1,10 @@
+<?php
+
+function logo_auto_declarer_tables_interfaces($interfaces) {
+	$interfaces["table_des_traitements"]['LOGO_ARTICLE'][]= 'traiter_logo_article(%s, $Pile[$SP][\'id_article\'])';
+	return $interfaces;
+}
+
+function traiter_logo_article($logo_article, $id_article) {
+	return trim(recuperer_fond('logo/article', array('logo_article' => $logo_article, 'id_article' => $id_article)));
+}
