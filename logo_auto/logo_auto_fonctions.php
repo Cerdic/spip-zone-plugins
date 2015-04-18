9d21d3cef8bce8aff2518b441f44594894c3f6e2
fil@rezo.net <fil@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-04-18 22:05:10 +0000

    appliquer aussi à #LOGO_ARTICLE_RUBRIQUE en prenant soin des priorités : - logo d'article - image associée à l'article - logo de rubrique
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@88672 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- logo_auto/logo_auto_fonctions.php
+++ logo_auto/logo_auto_fonctions.php
@@ -2,6 +2,7 @@
 
 function logo_auto_declarer_tables_interfaces($interfaces) {
 	$interfaces["table_des_traitements"]['LOGO_ARTICLE'][]= 'traiter_logo_article(%s, $Pile[$SP][\'id_article\'])';
+	$interfaces["table_des_traitements"]['LOGO_ARTICLE_RUBRIQUE'][]= 'traiter_logo_article(%s, $Pile[$SP][\'id_article\'])';
 	return $interfaces;
 }
 
