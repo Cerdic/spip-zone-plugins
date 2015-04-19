385acd28f0b642a08606d5b6ae6cc28e3f1c6641
peetdu@gmail.com <peetdu@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-04-19 14:08:51 +0000

    slogan plus explicite
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@88677 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- lim/trunk/lang/paquet-lim_fr.php
+++ lim/trunk/lang/paquet-lim_fr.php
@@ -9,10 +9,12 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'lim_description' => '&nbsp;<br>3 parties sont proposées pour allèger le back-office:
 
 	- <b>Forums et pétitions</b> : désactiver l\'affichage de ces blocs dans les pages articles,
+
 	- <b>Logos</b> : si vous utiliser les logos, vous pourrez alors désactiver leur affichage au cas par cas (articles, auteurs, mots-clés, etc.),
-	- <b>Contenus par rubriques</b> : Choisir les rubriques où désactiver les boutons ‘Écrire un nouvel article‘, ‘Écrire une nouvelle brève, ‘Référencer un nouveau site‘',
-	'lim_nom' => 'Lim',
-	'lim_slogan' => 'Less is More : alléger le back-office',
+
+	- <b>Contenus par rubriques</b> : Choisir les rubriques où désactiver l\'édition d\'articles, brèves et sites référencés.',
+	'lim_nom' => 'Lim : Less is More',
+	'lim_slogan' => 'Alléger l\'espace privé de bloc et bouton inutiles dans votre projet',
 );
 
 ?>
