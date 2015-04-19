4c61512907ea77f8f9e0195b712657cca3b98c10
tetue@rezo.net <tetue@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-04-19 16:25:11 +0000

    Automatiser le logo de rubrique, pour toujours en avoir un qui s'affiche : le logo de la rubrique, sinon la première image jointe, sinon le logo du site SPIP.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@88679 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- logo_auto/logo_auto_fonctions.php
+++ logo_auto/logo_auto_fonctions.php
@@ -3,9 +3,14 @@
 function logo_auto_declarer_tables_interfaces($interfaces) {
 	$interfaces["table_des_traitements"]['LOGO_ARTICLE'][]= 'traiter_logo_article(%s, $Pile[$SP][\'id_article\'])';
 	$interfaces["table_des_traitements"]['LOGO_ARTICLE_RUBRIQUE'][]= 'traiter_logo_article(%s, $Pile[$SP][\'id_article\'])';
+	$interfaces["table_des_traitements"]['LOGO_RUBRIQUE'][]= 'traiter_logo_rubrique(%s, $Pile[$SP][\'id_rubrique\'])';
 	return $interfaces;
 }
 
 function traiter_logo_article($logo_article, $id_article) {
 	return trim(recuperer_fond('logo/article', array('logo_article' => $logo_article, 'id_article' => $id_article)));
+}
+
+function traiter_logo_rubrique($logo_rubrique, $id_rubrique) {
+	return trim(recuperer_fond('logo/rubrique', array('logo_rubrique' => $logo_rubrique, 'id_rubrique' => $id_rubrique)));
 }
