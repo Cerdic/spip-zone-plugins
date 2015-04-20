fd398f8aca9709f85a8420ed105530c1abe3b548
toutati@free.fr <toutati@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-04-20 11:57:09 +0000

    filtre sauvage pour passer le document en vu=oui
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@88687 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- logo_auto/logo_auto_fonctions.php
+++ logo_auto/logo_auto_fonctions.php
@@ -1,5 +1,13 @@
 <?php
 
+function document_vu($id_objet,$objet,$id_document){
+	include_spip('action/editer_liens');
+        $objets_lies=array($objet=>$id_objet);
+        $objets_source=array('document'=>$id_document);
+        $qualif = array('vu'=>'oui');
+        objet_associer($objets_source, $objets_lies, $qualif);
+}
+
 function logo_auto_declarer_tables_interfaces($interfaces) {
 	$interfaces["table_des_traitements"]['LOGO_ARTICLE'][]= 'traiter_logo_article(%s, $Pile[$SP][\'id_article\'])';
 	$interfaces["table_des_traitements"]['LOGO_ARTICLE_RUBRIQUE'][]= 'traiter_logo_article(%s, $Pile[$SP][\'id_article\'])';
@@ -7,7 +15,7 @@ function logo_auto_declarer_tables_interfaces($interfaces) {
 	return $interfaces;
 }
 
-function traiter_logo_article($logo_article, $id_article) {
+function traiter_logo_article($logo_article, $id_article) {	
 	return trim(recuperer_fond('logo/article', array('logo_article' => $logo_article, 'id_article' => $id_article)));
 }
 
