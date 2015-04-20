7dfbd6bb28b2ad336476350c36c378b0dec3ec34
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-04-20 15:57:31 +0000

    enleve le log de teste
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@88695 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- reservation_evenement/branches/v1/notifications/reservation_client.php
+++ reservation_evenement/branches/v1/notifications/reservation_client.php
@@ -18,8 +18,6 @@ function notifications_reservation_client_dist($quoi,$id_reservation, $options) 
     $var_reservation = 'i18n_reservation_'.$lang;
     $chaine_statut='sujet_votre_reservation_'.$options['statut'];
 	
-	 spip_log("$var_reservation $chaine_statut",'teste');
-
     if(isset($GLOBALS[$var_reservation][$chaine_statut]))$subject=_T('reservation:'.$chaine_statut,array('nom'=>$GLOBALS['meta']['nom_site']));  
 
     $email=$options['email'];
