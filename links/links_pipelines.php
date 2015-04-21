b441aef0a284fa85a9de03e05555e1edeb7b7a93
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-04-21 10:55:59 +0000

    Initialiser le JS inline en amont dans le head et retablir la compression du js externe + utiliser la globale meta
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@88711 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- links/links_pipelines.php
+++ links/links_pipelines.php
@@ -3,36 +3,35 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 
 function links_insert_head_css($flux) {
 	//Recuperation de la configuration
-	$links = sql_fetsel('valeur', 'spip_meta', 'nom = "links"');
-	$links = unserialize($links['valeur']);
+	$links = unserialize($GLOBALS['meta']['links']);
 	//Styles
 	if($links['style'] == 'on'){
 		$flux .= '<link rel="stylesheet" href="'.find_in_path('css/links.css').'" type="text/css" media="all" />';
 	}
+	//Ouverture d'une nouvelle fenetre : insertion des init js inline, en amont des CSS (perf issue)
+	if($links['window'] == 'on'){
+		$js = 'var js_nouvelle_fenetre=\''._T('links:js_nouvelle_fenetre').'\';';
+		//Ouverture dune nouvelel fenetre sur les liens externes
+		if($links['external'] == 'on'){
+			$js .= 'var links_site = \''.$GLOBALS['meta']['adresse_site'].'\';';
+		}
+		//Ouverture d'une nouvelle fenetre sur les documents (extensions a preciser)
+		if(($links['download'] == 'on')&&($links['doc_list'])){
+			$js .= 'var links_doc = \''.$links['doc_list'].'\';';
+		}
+		$flux = '<script type="text/javascript">'.$js.'</script>' . "\n" . $flux;
+	}
+
 	return $flux;
 }
 
 function links_insert_head($flux) {
 	//Recuperation de la configuration
-	$links = sql_fetsel('valeur', 'spip_meta', 'nom = "links"');
-	$links = unserialize($links['valeur']);
+	$links = unserialize($GLOBALS['meta']['links']);
 
 	//Ouverture d'une nouvelle fenetre
 	if($links['window'] == 'on'){
-		$variables_links = "";
-		//Ouverture d'une nouvelle fenetre sur les liens externes
-		if($links['external'] == 'on'){
-			$variables_links .= 'var links_site = \''.$GLOBALS['meta']['adresse_site'].'\';';
-		}
-		//Ouverture d'une nouvelle fenetre sur les documents (extensions a preciser)
-		if(($links['download'] == 'on')&&($links['doc_list'])){
-			$variables_links .= 'var links_doc = \''.$links['doc_list'].'\';';
-		}
-		$flux .= '
-<!-- Liens explicites -->
-<script type="text/javascript">var js_nouvelle_fenetre=\''._T('links:js_nouvelle_fenetre').'\';'.$variables_links.'</script>
-<script id="spip_liens_explicites" src="'.find_in_path('links.js').'" type="text/javascript"></script>
-<!-- // Liens explicites -->';
+		$flux .= '<script src="'.find_in_path('links.js').'" type="text/javascript"></script>'. "\n";
 	}
 	return $flux;
 }
