457244aa50c58256d1635302ae1e0d5535e4d8d2
peetdu@gmail.com <peetdu@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-04-22 09:20:04 +0000

    Créer un name space pour les fonctions de l’API lim (merci Nicod_)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@88723 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- lim/trunk/inc/lim_api.php
+++ lim/trunk/inc/lim_api.php
@@ -18,14 +18,14 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
  * @type string
  * @return bool
  */
-function verifier_presence_logo($type) {
+function lim_verifier_presence_logo($type) {
 	include_spip('inc/chercher_logo');
 	include_spip('base/objets');
 	$id_objet = id_table_objet($type);
 	$prefixe_logo = _DIR_LOGOS.type_du_logo($id_objet).'*.*';
 	$liste_logos = glob($prefixe_logo);
 
-	// ne pas prenfdre en compte le logo du site (id = 0)
+	// ne pas prendre en compte le logo du site (id = 0)
 	if ($type == 'spip_syndic') {
 		$chercher_logo = charger_fonction('chercher_logo','inc');
 		$logo_du_site = $chercher_logo(0,'id_syndic');
@@ -41,7 +41,7 @@ function verifier_presence_logo($type) {
  * Vérifier si il existe déjà des pétitions
  * @return bool
  */
-function verifier_presence_petitions() {
+function lim_verifier_presence_petitions() {
 	/* recherche de pétitions */
 	if (sql_countsel('spip_petitions', "statut='publie'") > 0) {
 		return true;
@@ -56,38 +56,10 @@ function verifier_presence_petitions() {
  * @param string $objet
  * @return bool
  */
-function verifier_presence_objets($id_rubrique, $objet) {
+function lim_verifier_presence_objets($id_rubrique, $objet) {
 	$table = table_objet_sql($objet);
 	if (sql_countsel($table, "id_rubrique=$id_rubrique") > 0) return true;
 	return false;
-}
-
-/**
- * Traitement des cas d'installation/ désinstalation de plugin(s) intégrant un objet éditable
- * Il faut alors mettre à jour la méta 'lim_logos' (action réalisée dans le formulaire 'configurer_lim_logos.php')
- * 
- * @return void
- */
-function traitement_plugins() {
-	$tables_principales = lister_tables_principales_editables();
-	
-	// traitement du cas d'un nouveau plugin intégrant un objet éditable
-	foreach($tables_principales as $key => $type) {
-		if (is_null(lire_config('lim_logos/'.$type))) {
-			ecrire_config('lim_logos/'.$type, 'oui');
-		}
-	}
-
-	// traitement du cas d'un plugin désinstallé qui intégrait un objet éditable
-	$config_logo = lire_config('lim_logos');
-	$test_table = array_flip($tables_principales);
-	$diff = array_diff_key($config_logo, $test_table);
-
-	if(count($diff) > 0) {
-		foreach ($diff as $key => $value) {
-			effacer_config('lim_logos/'.$key);
-		}
-	}
 }
 
 ?>
