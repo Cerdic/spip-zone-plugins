06853c6bad1bcc86aed772367590a7237bc94d57
spip@oliver-fr.org <spip@oliver-fr.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-04-23 03:33:13 +0000

    [Salvatore] [source:_plugins_/chosen/trunk/lang/ paquet-chosen] Export depuis http://trad.spip.net de la langue oc_ni_mis
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@88731 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ chosen/trunk/lang/paquet-chosen_oc_ni_mis.php
@@ -0,0 +1,23 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-chosen?lang_cible=oc_ni_mis
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'chosen_description' => '[Chosen->http://harvesthq.github.com/chosen/] es una biblioutèca JavaScript que milioura l’esperimen utilisaire dei seletour en lu fourmulari HTML.
+
+La classa CSS <code>chosen</code> soubre d’un <code><select></code> cargerà automaticamen Chosen desoubre.
+Aquela branca dóu plugin Chosen es basada soubre lou fork de koenpunt - version 1.0.0 - vèire https://github.com/koenpunt/chosen/releases/.
+
+Emb’aqueu fork, Chosen permete de <a href=\'https://github.com/harvesthq/chosen/pull/166\'>creà dei oupcioun nouveli</a> en un &lt;select&gt; qu’esiste, s’àugue la classa "chosen-create-option".
+Coura chosen creà una nouvela &lt;option&gt; (lou mot "nouvèu’" per isemple) en un &lt;select&gt;, aquela d’aquì piha la forma seguenta : <code>&lt;option selected=\'selected\' value=\'chosen_nouvèu\'&gt;nouvèu&lt;/option&gt;</code>.
+Ben noutà lou prefis "chosen_" ajustat en lou parametre "value" per permetre de diferencià li &lt;option&gt; creadi da Chosen.',
+	'chosen_nom' => 'Chosen (fork de koenpunt)',
+	'chosen_slogan' => 'Integrà la biblioutèca Chosen en SPIP (fork de koenpunt)'
+);
+
+?>
