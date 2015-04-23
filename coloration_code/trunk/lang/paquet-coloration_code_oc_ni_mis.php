eb41f24bad3cd133651507982fa148c05d4c9835
spip@oliver-fr.org <spip@oliver-fr.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-04-23 03:33:19 +0000

    [Salvatore] [source:_plugins_/coloration_code/trunk/lang/ paquet-coloration_code] Export depuis http://trad.spip.net de la langue oc_ni_mis
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@88732 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ coloration_code/trunk/lang/paquet-coloration_code_oc_ni_mis.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-coloration_code?lang_cible=oc_ni_mis
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'coloration_code_nom' => 'Coulouramen Code',
+	'coloration_code_slogan' => 'Coulouramen sintàssicou dóu code sourgent en lu article'
+);
+
+?>
