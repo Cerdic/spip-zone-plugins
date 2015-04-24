70e33edf235693fdf40b1e8b1d512cdfd281c94d
spip@oliver-fr.org <spip@oliver-fr.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-04-24 03:34:02 +0000

    [Salvatore] [source:_plugins_/comments/trunk/lang/ comments] Export depuis http://trad.spip.net de la langue oc_ni_mis
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@88752 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ comments/trunk/lang/comments_oc_ni_mis.php
@@ -0,0 +1,48 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/comments?lang_cible=oc_ni_mis
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cfg_forum_champs_obligatoires' => 'Camp oubligatori',
+	'cfg_forum_format_fil' => 'Lista o thread ?',
+	'cfg_forum_longueur' => 'Lounguessa dei message',
+	'cfg_forum_longueur_maxi' => 'Lounguessa massimala',
+	'cfg_forum_longueur_mini' => 'Lounguessa minimala',
+	'cfg_label_apparence_permalien_aucun' => 'Mìnga',
+	'cfg_label_apparence_permalien_diese' => 'Dièsis (#)',
+	'cfg_label_email_obligatoire' => 'Rendre oubligatori la sesida d’una adressa e-mail',
+	'comment' => 'coumentari',
+	'comments' => 'coumentari',
+	'comments_h' => 'Lu vouòstre coumentari',
+
+	// D
+	'date_heure_a' => 'a',
+	'date_jour_le' => 'Lou',
+
+	// F
+	'forum_qui_etes_vous' => 'Qu sias ?',
+
+	// L
+	'label_email' => 'E-mail (noun publicat)',
+	'label_nom' => 'Noum',
+	'label_url' => 'Lou vouòstre sit web',
+	'lien_suivre_commentaires' => 'Sègre lu coumentari :',
+
+	// R
+	'reponse_comment_ok' => 'Mercì per lou vouòstre coumentari !',
+
+	// S
+	'saisie_texte_legend' => 'Ajustà aquì lou vouòstre coumentari',
+	'submit1' => 'Previsualisà',
+	'submit2' => 'Counfirmà la spedissioun',
+
+	// T
+	'titre_comments' => 'Coumentari'
+);
+
+?>
