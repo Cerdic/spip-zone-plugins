59fbb94b591796d4f0cc36455e9ef43fd5582983
peetdu@gmail.com <peetdu@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-04-25 09:58:38 +0000

    éviter une notice PHP si la constante header silencieux est déjà déclarée ailleurs
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@88775 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- lim/trunk/lim_options.php
+++ lim/trunk/lim_options.php
@@ -12,6 +12,6 @@
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 // Par défaut, ce plugin rend le header silencieux : voir http://www.spip.net/fr_article4648.html#spip_header_silencieux
-if (!$GLOBALS['spip_header_silencieux']) $GLOBALS['spip_header_silencieux'] = true;
+if (!isset($GLOBALS['spip_header_silencieux'])) $GLOBALS['spip_header_silencieux'] = true;
 
 ?>
