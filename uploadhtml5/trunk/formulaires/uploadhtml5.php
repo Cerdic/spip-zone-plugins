242f37f02aed8b19fcc6c4c5c09bba7445121cc8
p@henix.be <p@henix.be@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-04-25 23:06:53 +0000

    Ce code ne sert à rien ici
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@88803 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- uploadhtml5/trunk/formulaires/uploadhtml5.php
+++ uploadhtml5/trunk/formulaires/uploadhtml5.php
@@ -18,30 +18,6 @@ function formulaires_uploadhtml5_saisies_dist($objet, $id_objet) {
     return $saisies;
 }
 
-function formulaires_uploadhtml5_charger_dist($objet, $id_objet) {
-    // Contexte du formulaire.
-    $contexte = array(
-        '' => '',
-    );
-
-    return $contexte;
-}
-
-/*
- *   Fonction de vérification, cela fonction avec un tableau d'erreur.
- *   Le tableau est formater de la sorte:
- *   if (!_request('NomErreur')) {
- *       $erreurs['message_erreur'] = '';
- *       $erreurs['NomErreur'] = '';
- *   }
- *   Pensez à utiliser _T('info_obligatoire'); pour les éléments obligatoire.
- */
-function formulaires_uploadhtml5_verifier_dist($objet, $id_objet) {
-    $erreurs = array();
-
-    return $erreurs;
-}
-
 function formulaires_uploadhtml5_traiter_dist($objet, $id_objet) {
 
     // upload de la dropzone
