52456c84e570deb34045ac45a038ca3b82785d38
claude.codden@icloud.com <claude.codden@icloud.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-04-25 18:51:47 +0000

    Présentation et amélioration du salon privé.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@88789 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spipcatchat/trunk/phpscripts/get-prive-autorisation.php
+++ spipcatchat/trunk/phpscripts/get-prive-autorisation.php
@@ -2,25 +2,18 @@
 if(isset($_POST['char'])){$utf=$_POST['char'];}else{$utf='utf-8';}
 session_start();
 header('Content-Type: text/html; charset='.$_POST['char']);
-include_once('../fonction/fonction.php');
-if($_POST['admin']) //Pas très élégant l'attribution du code du salon de l'espace privé, mais le projet n'est pas fini (ajout de la fonction de création de salons plus sélecteur)
-{
-$p='0471910101112141518996354';
-echo "1";
-}
-else
-{
-$p='3345895214586785231548974';
-echo "0";
-}
+include_once('../fonction/fonction.php'); //--Les fonctions--//
 function start_prive($p){	
 	if(!file_exists('../db_catchat/'.$p.'/')){
-	if(false!=mkdir('../db_catchat/'.$p.'/',0777))
-	{file_put_contents('../db_catchat/'.$p.'/'.$p.'.catchat','');
-	 file_put_contents('../db_catchat/'.$p.'/.htaccess',utf8_encode('deny from all'));
-	 file_put_contents('../db_catchat/'.$p.'/'.$p.'line.js','{}');
-	}}
-}
+		if(false!=mkdir('../db_catchat/'.$p.'/',0777))
+			{file_put_contents('../db_catchat/'.$p.'/'.$p.'.catchat','');
+			 file_put_contents('../db_catchat/'.$p.'/.htaccess',utf8_encode('deny from all'));
+			 file_put_contents('../db_catchat/'.$p.'/'.$p.'line.js','{}');
+	}}}
+if($_POST['admin']) //Pas très élégant l'attribution du code du salon de l'espace privé, mais le projet n'est pas fini (ajout de la fonction de création de salons plus sélecteur)
+{   $p='0471910101112141518996354';   echo "1";   }
+else
+{   $p='3345895214586785231548974';   echo "0";   }
 start_prive($p);
 require('../obj/salon.class.php');//------Les fichiers à inclure-----//
 if(!file_exists('../db_catchat/')) // --> Si le dossier DB_CATCHAT n'existe pas on le crée
@@ -33,23 +26,21 @@ elseif(!file_exists('../db_catchat/catchat.xml'))
 	$salon->execute('add','',array('Bienvenue','true','null')); start_prive($p);}
  $timer=time();
 	$_SESSION['catchatprivetime'] = $timer;
-	$_SESSION['catchatpriveridsalon'] = $_POST['id_salon'];
 	$_SESSION['catchatprivelogin'] =  $_POST['nom'];
 	$_SESSION['catchatpriveplugin'] = $_POST['url'];
 	$_SESSION['spipcatchatprivestart'] = true;
 	$_SESSION['spipcatchatprivecode'] = $p;
-	$_SESSION['spipcatchatpriveautorite'] = 4;
 	$_SESSION['spipcatchatprivestatut'] = 3;
 	unset($_SESSION['catchatcache'.$p]);
-		if($statut=onlineChat('id'.$_POST['id_auteur'].'_'.$_POST['nom'],'0471910101112141518996354','statut')) 
+		if($statut=onlineChat('id'.$_POST['id_auteur'].'_'.$_POST['nom'],$p,'statut')) 
 		{ $_SESSION['spipcatchatprivestatut'] = statut($statut).'_';}//On récupére l'ancien statut de l'auteur de moins de 4 heure.
 		else
 		{ $_SESSION['spipcatchatprivestatut'] = '3_';}// Si pas de statut on l'impose en -> online code 3
-if(false!=($tabfile=file_get_contents('../db_catchat/0471910101112141518996354/0471910101112141518996354line.js'))){$tableau=json_decode($tabfile,true);}
+if(false!=($tabfile=file_get_contents('../db_catchat/'.$p.'/'.$p.'line.js'))){$tableau=json_decode($tabfile,true);}
 if(is_array($tableau))// Test : On obtient l'autorisation pour cet auteur
 {	foreach($tableau as $key => $value)
 	{	if($timer-1440>=chatdate($value))
-		{ onlineChat($key,'0471910101112141518996354','del'); }	// On supprime les auteurs qui ne sont pas sur le chat depuis plus de 4h // donc qui n'ont pas actualisé automatiquement ce fichier récemment
+		{ onlineChat($key,$Online,'del'); }	// On supprime les auteurs qui ne sont pas sur le chat depuis plus de 4h // donc qui n'ont pas actualisé automatiquement ce fichier récemment
 }	}
-onlineChat($_POST['id_auteur'].'_'.$_POST['nom'],'0471910101112141518996354','record',$_SESSION['spipcatchatprivestatut'].$timer);
+onlineChat($_POST['id_auteur'].'_'.$_POST['nom'],$p,'record',$_SESSION['spipcatchatprivestatut'].$timer);
 ?>
--- spipcatchat/trunk/phpscripts/get-prive-online.php
+++ spipcatchat/trunk/phpscripts/get-prive-online.php
@@ -1,8 +1,8 @@
 <?php session_start();//--On démarre la session
 include('../fonction/fonction.php');//------Les fichiers à inclure-----//
-$url=$_SESSION['spipcatchatprivecode']; $nom=$_SESSION['catchatprivelogin']; //------Le nom de l'auteur de la session et le code du chat
-if(false!=($tabfile=file_get_contents('../db_catchat/0471910101112141518996354/0471910101112141518996354line.js'))){$tableau=json_decode($tabfile,true);} $i=0; $timer= time();
-onlineChat($_GET['auteur'].'_'.$nom,'0471910101112141518996354','record',$_SESSION['spipcatchatprivestatut'].$timer);/* si l'utilisateur n'est pas inscrit, on l'ajoute, et on modifie la date de sa derniere actualisation */
+$p=$_SESSION['spipcatchatprivecode']; $url=$_SESSION['spipcatchatprivecode']; $nom=$_SESSION['catchatprivelogin']; //------Le nom de l'auteur de la session et le code du chat
+if(false!=($tabfile=file_get_contents('../db_catchat/'.$p.'/'.$p.'line.js'))){$tableau=json_decode($tabfile,true);} $i=0; $timer= time();
+onlineChat($_GET['auteur'].'_'.$nom,$p,'record',$_SESSION['spipcatchatprivestatut'].$timer);/* si l'utilisateur n'est pas inscrit, on l'ajoute, et on modifie la date de sa derniere actualisation */
 if(is_array($tableau))// On test l'autorisation pour cet auteur
 {	foreach($tableau as $key => $value)
 	{	 if($timer-140<=chatdate($value))
@@ -15,7 +15,7 @@ if(is_array($tableau))// On test l'autorisation pour cet auteur
 				$infos["login"] = nom($key);// Et on enregistre dans la colonne [login] le pseudo
 				$accounts[$i] = $infos;// Enfin on enregistre le tableau des infos de l'auteur
 		$i++;	}	}	}
-$count = onlineChat('','0471910101112141518996354','count','');// On compte le nombre d'entrées
+$count = onlineChat('',$p,'count','');// On compte le nombre d'entrées
 $t=onlineChat('',$url,'tableau');
 	if($count == 1 && !file_exists('../db_catchat/'.$url.'/mr_propre.txt') && $_SESSION['spipcatchatprivestart'] && (filesize ('../db_catchat/'.$url.'/'.$url.'.catchat') > 100000) )
 	{//--> Nous nous assurons d'être le seul et l'unique membre du salon  //--> On écrase le fichier du salon sur lui même pour plus de rapidité lors de la manipulation de celui-ci. 
