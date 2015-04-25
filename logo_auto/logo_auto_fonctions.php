fcb8f68d677079b6d96c5ad4249434dfd4a1e525
tetue@rezo.net <tetue@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-04-25 14:06:59 +0000

    oups
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@88784 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- logo_auto/logo_auto_fonctions.php
+++ logo_auto/logo_auto_fonctions.php
@@ -1,7 +1,7 @@
 <?php
 
 function balise_LOGO_SPIP($p) {
-	$p->code="filtrer('balise_img', find_in_path('spip.png'), _T('le_alt'), 'spip_logo')";
+	$p->code="filtrer('balise_img', find_in_path('spip.png'), 'SPIP', 'spip_logo')";
 	return $p;
 }
 
