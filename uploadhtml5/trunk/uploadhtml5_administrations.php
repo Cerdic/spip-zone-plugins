04a9368a673329a4541c699047301c6527f7cb16
p@henix.be <p@henix.be@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-04-26 10:57:36 +0000

    Ménage dans les commentaires
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@88813 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- uploadhtml5/trunk/uploadhtml5_administrations.php
+++ uploadhtml5/trunk/uploadhtml5_administrations.php
@@ -29,26 +29,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
  * @return void
 **/
 function uploadhtml5_upgrade($nom_meta_base_version, $version_cible) {
-	$maj = array();
-	# quelques exemples
-	# (que vous pouvez supprimer !)
-	#
-	# $maj['create'] = array(array('creer_base'));
-	#
-	# include_spip('inc/config')
-	# $maj['create'] = array(
-	#	array('maj_tables', array('spip_xx', 'spip_xx_liens')),
-	#	array('ecrire_config', array('uploadhtml5', array('exemple' => "Texte de l'exemple")))
-	#);
-	#
-	# $maj['1.1.0']  = array(array('sql_alter','TABLE spip_xx RENAME TO spip_yy'));
-	# $maj['1.2.0']  = array(array('sql_alter','TABLE spip_xx DROP COLUMN id_auteur'));
-	# $maj['1.3.0']  = array(
-	#	array('sql_alter','TABLE spip_xx CHANGE numero numero int(11) default 0 NOT NULL'),
-	#	array('sql_alter','TABLE spip_xx CHANGE texte petit_texte mediumtext NOT NULL default \'\''),
-	# );
-	# ...
-
+    $maj = array();
 
     $config_defaut = array(
         'max_file_size' => 5, // 5 Mb par défaut
@@ -78,11 +59,5 @@ function uploadhtml5_upgrade($nom_meta_base_version, $version_cible) {
  * @return void
 **/
 function uploadhtml5_vider_tables($nom_meta_base_version) {
-	# quelques exemples
-	# (que vous pouvez supprimer !)
-	# sql_drop_table("spip_xx");
-	# sql_drop_table("spip_xx_liens");
-
-
 	effacer_meta($nom_meta_base_version);
 }
