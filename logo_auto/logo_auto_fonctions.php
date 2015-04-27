832ebc32732ae9aebe04a08a0677e76fae4957e4
toutati@free.fr <toutati@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-04-27 14:36:09 +0000

    Suppression du filtre |document_vu qui permettait de ne pas réafficher dans le portfolio le logo choisit automatiquement
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@88840 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- logo_auto/logo_auto_fonctions.php
+++ logo_auto/logo_auto_fonctions.php
@@ -9,14 +9,6 @@ function traiter_logo_site_spip($logo_site_spip) {
 	return trim(recuperer_fond('logo/site_spip', array('logo_site_spip' => $logo_site_spip)));
 }
 
-function document_vu($id_objet,$objet,$id_document){
-	include_spip('action/editer_liens');
-        $objets_lies=array($objet=>$id_objet);
-        $objets_source=array('document'=>$id_document);
-        $qualif = array('vu'=>'oui');
-        objet_associer($objets_source, $objets_lies, $qualif);
-}
-
 function logo_auto_declarer_tables_interfaces($interfaces) {
 	$interfaces["table_des_traitements"]['LOGO_ARTICLE'][]= 'traiter_logo_article(%s, $Pile[$SP][\'id_article\'])';
 	$interfaces["table_des_traitements"]['LOGO_ARTICLE_RUBRIQUE'][]= 'traiter_logo_article(%s, $Pile[$SP][\'id_article\'])';
