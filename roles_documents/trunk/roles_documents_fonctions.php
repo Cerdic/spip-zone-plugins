960bc8c9c3c5c9a939bda5598ea44a6afb4e55ce
toutati@free.fr <toutati@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-04-27 14:27:36 +0000

    traiter #LOGO_ARTICLE_RUBRIQUE si la balise existe
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@88839 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- roles_documents/trunk/roles_documents_fonctions.php
+++ roles_documents/trunk/roles_documents_fonctions.php
@@ -10,13 +10,17 @@ function document_vu($id_objet,$objet,$id_document){
 
 function roles_documents_declarer_tables_interfaces($interfaces) {
 	$interfaces["table_des_traitements"]['LOGO_ARTICLE'][]= 'traiter_logo_article(%s, $Pile[$SP][\'id_article\'])';
-	$interfaces["table_des_traitements"]['LOGO_ARTICLE_RUBRIQUE'][]= 'traiter_logo_article(%s, $Pile[$SP][\'id_article\'])';
+	$interfaces["table_des_traitements"]['LOGO_ARTICLE_RUBRIQUE'][]= 'traiter_logo_article_rubrique(%s, $Pile[$SP][\'id_article\'])';
 	$interfaces["table_des_traitements"]['LOGO_RUBRIQUE'][]= 'traiter_logo_rubrique(%s, $Pile[$SP][\'id_rubrique\'])';
 	return $interfaces;
 }
 
 function traiter_logo_article($logo_article, $id_article) {	
 	return trim(recuperer_fond('logo/article', array('logo_article' => $logo_article, 'id_article' => $id_article)));
+}
+
+function traiter_logo_article_rubrique($logo_article, $id_article) {	
+	return trim(recuperer_fond('logo/article_rubrique', array('logo_article' => $logo_article, 'id_article' => $id_article)));
 }
 
 function traiter_logo_rubrique($logo_rubrique, $id_rubrique) {
