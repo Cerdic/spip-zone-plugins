7db74a99b8725a5bf3cf9370cfac84409758b660
toutati@free.fr <toutati@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-04-27 10:41:05 +0000

    L'image d'un article avec le role de logo est interprété par #LOGO_ARTICLE, merci tetue et fil car c'est pompé sur logo_auto
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@88837 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ roles_documents/trunk/roles_documents_fonctions.php
@@ -0,0 +1,24 @@
+<?php
+
+function document_vu($id_objet,$objet,$id_document){
+	include_spip('action/editer_liens');
+        $objets_lies=array($objet=>$id_objet);
+        $objets_source=array('document'=>$id_document);
+        $qualif = array('vu'=>'oui');
+        objet_associer($objets_source, $objets_lies, $qualif);
+}
+
+function roles_documents_declarer_tables_interfaces($interfaces) {
+	$interfaces["table_des_traitements"]['LOGO_ARTICLE'][]= 'traiter_logo_article(%s, $Pile[$SP][\'id_article\'])';
+	$interfaces["table_des_traitements"]['LOGO_ARTICLE_RUBRIQUE'][]= 'traiter_logo_article(%s, $Pile[$SP][\'id_article\'])';
+	$interfaces["table_des_traitements"]['LOGO_RUBRIQUE'][]= 'traiter_logo_rubrique(%s, $Pile[$SP][\'id_rubrique\'])';
+	return $interfaces;
+}
+
+function traiter_logo_article($logo_article, $id_article) {	
+	return trim(recuperer_fond('logo/article', array('logo_article' => $logo_article, 'id_article' => $id_article)));
+}
+
+function traiter_logo_rubrique($logo_rubrique, $id_rubrique) {
+	return trim(recuperer_fond('logo/rubrique', array('logo_rubrique' => $logo_rubrique, 'id_rubrique' => $id_rubrique)));
+}
