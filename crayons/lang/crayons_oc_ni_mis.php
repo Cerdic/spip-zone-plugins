699313992b794d13ad5279cbfde58d99940e4a9b
spip@oliver-fr.org <spip@oliver-fr.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-04-28 03:33:53 +0000

    [Salvatore] [source:_plugins_/crayons/lang/ crayons] Export depuis http://trad.spip.net de la langue oc_ni_mis
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@88860 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ crayons/lang/crayons_oc_ni_mis.php
@@ -0,0 +1,64 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/crayons?lang_cible=oc_ni_mis
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'annuler' => 'Anulà',
+
+	// B
+	'bouton_supprimer' => 'Suprimà',
+
+	// D
+	'deja_modifie' => 'Jà moudificat',
+
+	// E
+	'editer' => 'Edità',
+	'editer_@type@_@id@' => 'Edità @type@ @id@ en entié',
+	'editer_tout' => 'Edità tout',
+	'effets' => 'Efet',
+
+	// I
+	'info_experimental' => 'Esperimental',
+
+	// L
+	'label_effets' => 'Chausida dei efet',
+	'label_exec_autorise' => 'Pàgina autourisadi',
+	'label_filet' => 'Ret',
+	'label_message' => 'Outenì un message',
+	'label_yellow_fade' => 'Fade',
+	'legend_controleur_hyperlien' => 'Iperestac',
+	'legend_controleur_logo' => 'Chausì un logou :',
+	'legend_controleur_qui' => 'Qu ?',
+	'legend_controleur_vignette' => 'Chausì una vigneta :',
+	'legend_editer_prive' => 'Edità l’espaci privat (esperimental)',
+
+	// M
+	'message_image_obligatoire' => 'Cau chausì una image',
+	'messages' => 'Message',
+
+	// N
+	'non_autorise' => 'Noun autourisat',
+
+	// P
+	'pas_de_modification' => 'Mìnga moudificacioun',
+
+	// S
+	'sauvegarder' => 'Sauvagarda li moudificacioun ?',
+
+	// T
+	'titre_config_crayons' => 'Counfigurà lu creioun',
+	'titre_crayons' => 'Creioun',
+
+	// U
+	'update_impossible' => 'La metuda a jou a souhit.',
+
+	// V
+	'veuillez_patienter' => 'Vourguès aspera...'
+);
+
+?>
