e46128a75524d5cc0c467c34af6ce612d2144c56
arno@rezo.net <arno@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-04-29 05:24:24 +0000

    Installer <meta charset> au début.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@88895 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- html5_responsive/html5_responsive.php
+++ html5_responsive/html5_responsive.php
@@ -1,11 +1,11 @@
 <?php
 
 function html5_responsive_insert_head($flux) {
-	$flux = "<meta name='viewport' content='user-scalable=no, width=device-width, initial-scale=1.0, maximum-scale=1.0'>
+	$flux = "<meta charset='".lire_meta("charset")."'>
+	<meta name='viewport' content='user-scalable=no, width=device-width, initial-scale=1.0, maximum-scale=1.0'>
 	<meta name='format-detection' content='telephone=no'>
 	<meta name='apple-mobile-web-app-capable' content='yes'>
-	<meta name='apple-mobile-web-app-status-bar-style' content='black'>
-	<meta charset='".lire_meta("charset")."'>"
+	<meta name='apple-mobile-web-app-status-bar-style' content='black'>"
 				.$flux;
 	$flux .= "
 <script type='text/javascript' src='".find_in_path("javascript/liens-standalone.js")."'></script>
