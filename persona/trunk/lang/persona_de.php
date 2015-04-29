37e86e8b50818fc8be4bb1a17833dd7965ce64a4
klaus@spip.de <klaus@spip.de@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-04-29 03:33:23 +0000

    [Salvatore] [source:_plugins_/persona/trunk/lang/ persona] Export depuis http://trad.spip.net de la langue de
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@88894 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ persona/trunk/lang/persona_de.php
@@ -0,0 +1,26 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/persona?lang_cible=de
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'affichage' => 'Anzeigen',
+
+	// C
+	'cacher_login_classique' => 'Ausblenden ?',
+	'configurer_titre' => 'Persona konfigurieren',
+
+	// E
+	'explication_cacher_login_classique' => 'Nach dem Abhaken dieses Kästchens wird das klassische Login-Formular auf der Anemdldeseite standardmäßig ausgeblendet. Es wird nur noch die "Persona" Schaltfläche angezeigt. Darunter wird ein Link das erneute Einblenden ermöglichen.',
+	'explication_login' => 'Sie können sich mit Ihrer Mozilla Persona ID (<a href="http://fr.wikipedia.org/wiki/Mozilla_Persona" target="_blank">aide</a>) anmelden.',
+
+	// F
+	'form_afficher' => 'Klassisches Login-Formular anzeigen',
+	'form_masquer' => 'Klassisches Login-Formular ausblenden'
+);
+
+?>
