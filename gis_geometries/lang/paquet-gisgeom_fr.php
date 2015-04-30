623a1e65b60f271c05d522444215294027b601dd
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-04-30 03:33:29 +0000

    [Salvatore] [source:_plugins_/gis_geometries/lang/ paquet-gisgeom] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@88915 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- gis_geometries/lang/paquet-gisgeom_fr.php
+++ gis_geometries/lang/paquet-gisgeom_fr.php
@@ -1,19 +1,13 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-gisgeom
-// Langue: fr
-// Date: 06-08-2012 18:20:02
-// Items: 2
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/gis_geometries/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// G
+	// G
 	'gisgeom_description' => 'Ce plugin ajoute la prise en charge des formes géométriques à GIS. Il utilise les fonctions spatiales de MySQL disponibles à partir de la version 4.1.',
-	'gisgeom_slogan' => 'Prise en charge de formes géométriques dans GIS',
+	'gisgeom_slogan' => 'Prise en charge de formes géométriques dans GIS'
 );
+
 ?>
