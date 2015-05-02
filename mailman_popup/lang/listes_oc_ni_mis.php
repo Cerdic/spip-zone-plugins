9f50aa153ce686560547c69f0263d6cdfe06f569
spip@oliver-fr.org <spip@oliver-fr.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-05-02 03:34:10 +0000

    [Salvatore] [source:_plugins_/mailman_popup/lang/ listes] Export depuis http://trad.spip.net de la langue oc_ni_mis
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@88954 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ mailman_popup/lang/listes_oc_ni_mis.php
@@ -0,0 +1,54 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/listes?lang_cible=oc_ni_mis
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'ajoutee' => 'L’adressa &lt;@var_email@&gt; serà ajustada a la lista &lt;@liste@&gt; après verificacioun. Mercì de respouòndre au message que ven de v’estre mandat.',
+
+	// C
+	'confirm' => 'Une demanda de counfirmacioun es estada mandada a &lt;@var_email@&gt;.',
+
+	// D
+	'deja' => '&lt;@var_email@&gt; : sias jà inscrich sus la lista &lt;@liste@&gt;.',
+	'desabo' => 'Desabounamen efetuat.',
+
+	// F
+	'fermer' => 'fermà',
+
+	// I
+	'inscription' => 'Inscrissioun a &lt;@liste@&gt;',
+
+	// M
+	'mail_removed' => '
+L’adressa &lt;@var_email@&gt; es estada suprimida de la lista @liste@.
+
+En cas de proublema, o se noun avès demandat estou desabounamen,
+vourguès escriéure a &lt;@responsable@&gt;.
+
+A ben lèu, e mercì.',
+	'merci' => 'Mercì.',
+
+	// P
+	'pasabo' => 'L’adressa &lt;@var_email@&gt; est pas abounada a la lista &lt;@liste@&gt;.',
+	'patientez' => 'Vourguès asperà',
+
+	// Q
+	'quitter' => 'Desabounamen',
+
+	// S
+	'subject_removed' => 'La vouòstra adressa es estada suprimada de la lista @liste@.',
+
+	// T
+	'titrefenetre' => 'Inscrissioun',
+
+	// V
+	'veuillez' => 'Vourguès endicà la vouòstra adressa.',
+	'votreemail' => 'Lou vouòstre e-mail :'
+);
+
+?>
