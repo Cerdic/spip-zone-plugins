4578babe7a2015492c7d3fb93568a6d760d55592
spip@oliver-fr.org <spip@oliver-fr.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-05-02 03:33:41 +0000

    [Salvatore] [source:_plugins_/couteau-kiss/lang/ paquet-ck] Export depuis http://trad.spip.net de la langue oc_ni_mis
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@88951 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ couteau-kiss/lang/paquet-ck_oc_ni_mis.php
@@ -0,0 +1,17 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-ck?lang_cible=oc_ni_mis
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'ck_description' => 'Simplicità, eficacha, laugieretà.
+_ Un coutèu que ten dau bouòn en pocha, basta en una pàgina de counfiguracioun per lu reglage escoundut de SPIP.',
+	'ck_nom' => 'Coutèu KISS',
+	'ck_slogan' => 'Simplificà d’unu reglage SPIP'
+);
+
+?>
