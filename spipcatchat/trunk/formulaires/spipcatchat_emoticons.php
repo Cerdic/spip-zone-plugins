38239e2d92746ee300e76947da0b08697ffd1ce9
claude.codden@icloud.com <claude.codden@icloud.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-05-02 19:48:13 +0000

    Ajout formulaire émoticon
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@88980 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ spipcatchat/trunk/formulaires/spipcatchat_emoticons.php
@@ -0,0 +1,9 @@
+<?php 
+if (!defined('_ECRIRE_INC_VERSION')) return;
+function formulaires_spipcatchat_emoticons_charger_dist($arg1,$arg2) {
+  $valeurs = array();
+  $valeurs['emoticon'] = $arg1;
+  $valeurs['fonction'] = $arg2;
+  return $valeurs;
+}
+?>
