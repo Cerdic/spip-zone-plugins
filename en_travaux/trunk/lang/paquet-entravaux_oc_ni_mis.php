028053f4e2c6b7eebfff47ba73dd94aed7fe797d
spip@oliver-fr.org <spip@oliver-fr.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-05-04 03:34:13 +0000

    [Salvatore] [source:_plugins_/en_travaux/trunk/lang/ paquet-entravaux] Export depuis http://trad.spip.net de la langue oc_ni_mis
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@89029 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ en_travaux/trunk/lang/paquet-entravaux_oc_ni_mis.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-entravaux?lang_cible=oc_ni_mis
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// E
+	'entravaux_description' => 'Da ativà denant una fasa de mantenança, aquestou plugin aficha sus lou còu una pàgina d’avertimen, {{en plaça}} dóu sit pùblicou. Mèfi : basta lou webmèstre achede encara a l’espaci privat e au sit pùblicou.',
+	'entravaux_nom' => 'En Mantenança',
+	'entravaux_slogan' => 'Endicà una frasa de mantenança'
+);
+
+?>
