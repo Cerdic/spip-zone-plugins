271e19b88038f4b1c1eb23d1d1536a2a4d360c6f
spip@oliver-fr.org <spip@oliver-fr.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-05-08 03:32:42 +0000

    [Salvatore] [source:_plugins_/jquery_ui/lang/ jqueryui] Export depuis http://trad.spip.net de la langue oc_ni_mis
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@89124 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ jquery_ui/lang/jqueryui_oc_ni_mis.php
@@ -0,0 +1,64 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/jqueryui?lang_cible=oc_ni_mis
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'afficher_calendrier' => 'Afichà lou calendari',
+
+	// C
+	'cfg_boite_jqueryui' => 'Counfiguracioun de jQuery UI',
+	'cfg_lbl_plugins' => 'Plugin',
+	'cfg_no_css' => 'noun cargà lu CSS da jQuery-UI',
+	'cfg_titre_jqueryui' => 'jQuery UI',
+	'cfg_val_effects_blind' => 'Efet blind',
+	'cfg_val_effects_bounce' => 'Efet bounce',
+	'cfg_val_effects_clip' => 'Efet clip',
+	'cfg_val_effects_core' => 'Efet Core',
+	'cfg_val_effects_drop' => 'Efet drop',
+	'cfg_val_effects_explode' => 'Efet explode',
+	'cfg_val_effects_fold' => 'Efet fold',
+	'cfg_val_effects_highlight' => 'Efet highlight',
+	'cfg_val_effects_pulsate' => 'Efet pulsate',
+	'cfg_val_effects_scale' => 'Efet scale',
+	'cfg_val_effects_shake' => 'Efet shake',
+	'cfg_val_effects_slide' => 'Efet slide',
+	'cfg_val_effects_transfer' => 'Efet transfer',
+	'cfg_val_ui_accordion' => 'UI accordion',
+	'cfg_val_ui_autocomplete' => 'UI autocomplete',
+	'cfg_val_ui_button' => 'UI button',
+	'cfg_val_ui_core' => 'Core UI',
+	'cfg_val_ui_datepicker' => 'UI datepicker',
+	'cfg_val_ui_dialog' => 'UI dialog',
+	'cfg_val_ui_draggable' => 'UI draggable',
+	'cfg_val_ui_droppable' => 'UI droppable',
+	'cfg_val_ui_mouse' => 'UI mouse',
+	'cfg_val_ui_position' => 'UI position',
+	'cfg_val_ui_progressbar' => 'UI progressbar',
+	'cfg_val_ui_resizable' => 'UI resizable',
+	'cfg_val_ui_selectable' => 'UI selectable',
+	'cfg_val_ui_slider' => 'UI slider',
+	'cfg_val_ui_sortable' => 'UI sortable',
+	'cfg_val_ui_tabs' => 'UI tabs',
+	'cfg_val_ui_widget' => 'UI widget',
+
+	// D
+	'date_mois_10_abbr' => 'out.',
+	'date_mois_11_abbr' => 'nouv.',
+	'date_mois_12_abbr' => 'dec.',
+	'date_mois_1_abbr' => 'gen.',
+	'date_mois_2_abbr' => 'febr.',
+	'date_mois_3_abbr' => 'mars',
+	'date_mois_4_abbr' => 'abr.',
+	'date_mois_5_abbr' => 'mai',
+	'date_mois_6_abbr' => 'jugne',
+	'date_mois_7_abbr' => 'julh.',
+	'date_mois_8_abbr' => 'aoust',
+	'date_mois_9_abbr' => 'set.'
+);
+
+?>
