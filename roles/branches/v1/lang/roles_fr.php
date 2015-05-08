43448ca81cd0eab7f2743058a7e7d42a686d2c9d
marcimat@rezo.net <marcimat@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-05-08 07:29:11 +0000

    Branchage de roles : on va intégrer dans la 3.1 les 2 surcharges.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@89127 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ roles/branches/v1/lang/roles_fr.php
@@ -0,0 +1,30 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	// A
+	'ajouter_role' => 'Ajouter un rôle',
+
+	// C
+	'choisir_role' => 'Choisir…',
+
+	// E
+	'enlever_role' => 'Enlever un rôle',
+
+	// M
+	'modifier_roles' => 'Modifier',
+	'modifier_roles_title' => 'Modifier les rôles',
+
+	// R
+	'roles_titre' => 'Rôles',
+	'role_titre'  => 'Rôle',
+
+	// S
+	'selectionner_roles' => 'Sélectionner le ou les rôles souhaités',
+	'saisir_role' => 'Indiquer un rôle…',
+	'saisir_valeur' => 'Valeur',
+);
+
+?>
