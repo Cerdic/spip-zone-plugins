98b46998edca7a2c7236f02d1ef8a95abde49aff
cyril@cym.fr <cyril@cym.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-05-09 23:53:43 +0000

    On remplit la coquille...
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@89271 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ encarts/branches/v2.0/encarts_fonctions.php
@@ -0,0 +1,60 @@
+<?php
+/*
+ * (issu du plugin mediatheque)
+ * (c) 2009 cedric
+ * Distribue sous licence GPL
+ * modifie par marcimat.
+ */
+
+// On liste tous les champs susceptibles de contenir des encarts si on veut que ces derniers soient lies a l objet lorsqu on y fait reference par encartXX
+// la dist ne regarde que chapo et texte, on laisse comme ca, mais ca permet d etendre a descriptif ou toto depuis d autre plugin comme agenda ou grappe
+$GLOBALS['encarts_liste_champs'][] = 'texte';
+$GLOBALS['encarts_liste_champs'][] = 'chapo';
+ 
+
+function inc_marquer_doublons_encart_dist($champs,$id,$type,$id_table_objet,$table_objet,$spip_table_objet, $desc=array(), $serveur=''){
+	$champs_selection=array();
+		foreach ($GLOBALS['encarts_liste_champs'] as $champs_choisis) {
+			if ( isset($champs[$champs_choisis]) )
+			array_push($champs_selection,$champs_choisis);
+		}
+	if (count($champs_selection) == 0)
+		return;
+	if (!$desc){
+		$trouver_table = charger_fonction('trouver_table', 'base');
+		$desc = $trouver_table($table_objet, $serveur);
+	}
+	$load = "";
+	// charger le champ manquant en cas de modif partielle de l	'objet
+	// seulement si le champ existe dans la table demande
+	
+		foreach ($champs_selection as $champs_a_parcourir) {
+			if (isset($desc['field'][$champs_a_parcourir])) {
+			$load = $champs_a_parcourir;
+			$champs_a_traiter .= $champs[$champs_a_parcourir];
+			}
+		}	
+
+	if ($load){
+		$champs[$load] = "";
+		$row = sql_fetsel($load, $spip_table_objet, "$id_table_objet=".sql_quote($id));
+		if ($row AND isset($row[$load]))
+			$champs[$load] = $row[$load];
+	}
+	include_spip('inc/texte');
+	include_spip('base/abstract_sql');
+	$GLOBALS['doublons_encarts_inclus'] = array();
+	traiter_modeles($champs_a_traiter, array('encarts'=>array('encart'))); // detecter les doublons
+	sql_updateq("spip_encarts_liens", array("vu" => 'non'), "id_objet=$id AND objet=".sql_quote($type));
+	if (count($GLOBALS['doublons_encarts_inclus'])){
+		// on repasse par une requete sur spip_encarts pour verifier que les encarts existent bien !
+		$in_liste = sql_in('id_encart',
+			$GLOBALS['doublons_encarts_inclus']);
+		$res = sql_select("id_encart", "spip_encarts", $in_liste);
+		while ($row = sql_fetch($res)) {
+			// Creer le lien s'il n'existe pas deja
+			sql_insertq("spip_encarts_liens", array('id_objet'=>$id, 'objet'=>$type, 'id_encart' => $row['id_encart'], 'vu' => 'oui'));
+			sql_updateq("spip_encarts_liens", array("vu" => 'oui'), "id_objet=$id AND objet=".sql_quote($type)." AND id_encart=" . $row['id_encart']);
+		}
+	}
+}
--- encarts/branches/v2.0/encarts_pipelines.php
+++ encarts/branches/v2.0/encarts_pipelines.php
@@ -10,15 +10,14 @@
  */
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
-	
 
-/*
- * Un fichier de pipelines permet de regrouper
- * les fonctions de branchement de votre plugin
- * sur des pipelines existants.
+/**
+ * Les encarts sont par défaut dans le texte de l'article
+ * mais il peuvent être aussi en marge de ce texte
  */
-
-
+if (!defined('_TYPES_ENCARTS')) {
+        define('_TYPES_ENCARTS', 'encart|marge');
+}
 
 /**
  * Ajout de contenu sur certaines pages,
@@ -31,8 +30,6 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 function encarts_affiche_milieu($flux) {
 	$texte = "";
 	$e = trouver_objet_exec($flux['args']['exec']);
-
-
 
 	// encarts sur les articles
 	if (!$e['edition'] AND in_array($e['type'], array('article'))) {
@@ -68,4 +65,38 @@ function encarts_optimiser_base_disparus($flux){
 	return $flux;
 }
 
-?>
+/**
+ * Traiter les textes contenant des <encart> .... </encart>
+ * ou des <marge>...</marge>
+ * en les remplaçant par un span...
+ *
+ * @param string $texte à analyser
+ * @return texte modifié
+**/
+function encarts_pre_propre($texte) {
+	if (false !== strpos($texte, '<')) {
+		if (preg_match_all(',<(' . _TYPES_ENCARTS . ')>(.*?)</\1>,is', $texte, $regs, PREG_SET_ORDER)) {
+			foreach ($regs as $reg) {
+				$css = 'encart';
+				if ($reg[1] != 'encart') {
+						$css .= " " . $reg[1];
+				}
+				$texte = str_replace($reg[0], "<span class='$css'>".$reg[2]."</span>", $texte);
+			}
+		}
+	}
+	return $texte;
+}
+
+/**
+ * Mettre les vu=oui lorsque l'on met un modèle
+ * d'encart dans un texte.
+ *
+**/
+function encarts_post_edition($flux) {
+	if (!in_array($flux['args']['type'], array('forum','signature'))) {
+		$marquer_doublons_encart = charger_fonction('marquer_doublons_encart', 'inc');
+		$marquer_doublons_encart($flux['data'],$flux['args']['id_objet'],$flux['args']['type'],id_table_objet($flux['args']['type'], $flux['args']['serveur']),$flux['args']['table_objet'],$flux['args']['spip_table_objet'], '', $flux['args']['serveur']);
+	}
+	return $flux;
+}
