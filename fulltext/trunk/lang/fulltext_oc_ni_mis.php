7b60986ffb0d571b803eb0cc0f1d17b151805b52
spip@oliver-fr.org <spip@oliver-fr.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-05-09 03:32:53 +0000

    [Salvatore] [source:_plugins_/fulltext/trunk/lang/ fulltext] Export depuis http://trad.spip.net de la langue oc_ni_mis
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@89180 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ fulltext/trunk/lang/fulltext_oc_ni_mis.php
@@ -0,0 +1,109 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/fulltext?lang_cible=oc_ni_mis
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'accents_pas_pris' => 'Lu achèntou noun soun pihat en conte (« perqué » o « perque », retourneran finda « pèrque », « perque », « perqué »...)',
+	'activer_indexation' => 'Ativà l’indessacioun dei fichié @ext@',
+	'asie' => 'asia',
+	'asterisque_terminale' => 'retournerà ren : l’asterisca * dèu si trouvà a la fin',
+	'aussi' => 'tanben',
+
+	// C
+	'casse_indifferente' => 'La cassa (minùscula/maiùscoula) dei mot recercat es indiferenta.',
+	'configuration_indexation_document' => 'Counfiguracioun de l’indessacioun dei doucumen',
+	'configurer_egalement_doc' => 'Poudès finda counfigurà l’indessacioun dei doucumen :',
+	'convertir_myisam' => 'Counvertì en MyISAM',
+	'convertir_toutes' => 'Counvertì touti li taula en MyISAM',
+	'convertir_utf8' => 'counvertì en UTF-8 da restaurà la couerença',
+	'creer_tous' => 'Creà toui lu ìndice FULLTEXT sugerit',
+
+	// D
+	'des_utilisations' => '@nb@ utilisacioun',
+	'descriptif' => 'Descritiéu',
+	'documents_proteges' => 'Doucumen proutegiat',
+
+	// E
+	'enfan' => 'enfan',
+	'enfance' => 'enfança',
+	'enfant' => 'enfant',
+	'enfanter' => 'enfantà',
+	'enfantillage' => 'enfantilhage',
+	'enfants' => 'enfant',
+	'erreur_binaire_indisponible' => 'Aqueu lougiciau es pas dispounible sus lou servidou.',
+	'erreur_doc_bin' => 'Devès endicà lou binaire da utilisà per traire lu .doc',
+	'erreur_intervalle_cron' => 'Devès endicà un intervàlou superiour a una secounda.',
+	'erreur_nb_docs' => 'Devès endicà un noumbre de doucumen da tratà per iteracioun superiour a un.',
+	'erreur_pdf_bin' => 'Devès endicà lou binaire da utilisà per traire lu .pdf',
+	'erreur_ppt_bin' => 'Devès endicà lou binaire da utilisà per traire lu .ppt',
+	'erreur_taille_index' => 'Cau indessà au màncou un caracter.',
+	'erreur_verifier_configuration' => 'Lì soun dei errour de counfiguracioun.',
+	'erreur_xls_bin' => 'Devès endicà lou binaire da utilisà per traire lu .xls',
+	'et' => 'E',
+	'etranger' => 'estrangié',
+	'exemples' => 'Isemple d’utilisacioun',
+	'explication_option_readonly' => 'Aquesta oupcioun es fourçada soubre aqueu sit e noun es pi counfigurabla.',
+
+	// F
+	'fant' => 'fant',
+	'fonctionnement_recherche' => 'Founciounamen dóu moutour de recerca soubre aqueu sit',
+	'fulltext_cree' => 'FULLTEXT creat',
+	'fulltext_creer' => 'Creà l’indès @index@',
+	'fulltext_documentation' => 'Per mai d’infourmacioun soubre la counfiguracioun, anàs véire la doucumentacioun en ligna :',
+	'fulltext_documents' => 'Fulltext - Doucumen',
+	'fulltext_index' => 'Fulltext - Ìndice',
+
+	// G
+	'general' => 'General',
+
+	// I
+	'id' => 'ID',
+	'il_faut_myisam' => 'cau MyISAM',
+	'incoherence_charset' => 'Una incouerença tra lou charset dóu vouòstre sit e aqueu dei taula de la vouòstra basa de dounada pòu faussà li recercà embé dei caracter achentuat :',
+	'index_regenere' => 'ìndice de la taula regenerat',
+	'index_supprime' => 'ìndice suprimat',
+
+	// L
+	'logo' => 'Lògou',
+
+	// M
+	'mais_pas' => 'mà NOUN',
+
+	// N
+	'nb_err' => 'En errour d’indessacioun',
+	'nb_index' => 'Indessat',
+	'nb_non_index' => 'Noun-indessat',
+	'nb_ptg' => 'Proutegiat (noun-indessat)',
+
+	// O
+	'ou_bien' => 'o ben',
+
+	// P
+	'pas_document_ptg' => 'Lì a màncou de doucumen proutegiat.',
+	'pas_index' => 'Mìnga indès FULLTEXT',
+	'premier_soit' => 'SÌA',
+
+	// R
+	'reserve_webmestres' => 'Pàgina reservada ai webmèstre',
+
+	// S
+	'soit' => 'SÌA',
+	'supprimer' => 'Suprimà',
+
+	// T
+	'titre_page_fulltext_index' => 'Counfiguracioun des ìndice de recercà',
+
+	// U
+	'une_utilisation' => '1 utilisacioun',
+	'utiliser_operateurs_logiques' => 'La recerca utilisa lu ouperatour lògicou lu mai courrent.',
+
+	// V
+	'voir_doc_ptg' => 'Véire lu doucumen proutegiat'
+);
+
+?>
