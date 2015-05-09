93261d1730aff69529f1f3a51cc7ffd0d2e9fdd7
p@henix.be <p@henix.be@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-05-09 13:03:28 +0000

    Formulaire d'upload html5 pour les logo
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@89200 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ uploadhtml5/trunk/formulaires/uploadhtml5_logo.php
@@ -0,0 +1,29 @@
+<?php
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+function formulaires_uploadhtml5_logo_saisies_dist($objet, $id_objet) {
+    $saisies = array(
+        array(
+            'saisie' => 'input',
+            'options' => array(
+                'nom' => 'files_logo[]',
+                'label' => _T('uploadhtml5:upload'),
+                'id' => 'fileupload_logo',
+                'type' => 'file',
+                'attributs' => 'multiple'
+            )
+        )
+    );
+    return $saisies;
+}
+
+function formulaires_uploadhtml5_logo_traiter_dist($objet, $id_objet) {
+    // upload de la dropzone
+    uploadhtml5_uploader_logo($objet, $id_objet, $_FILES['file_logo']['tmp_name']);
+
+    // Donnée de retour.
+    return array(
+        'editable' => true
+    );
+}
