35598d43364e3a854e0302cf18a183776a18c6ee
jack@jack31.net <jack@jack31.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-05-10 08:08:19 +0000

    - Certains modèles comportent des adresses mail, ajout de necessite mailcrypt - Complément du modèle ffe_liste_clubs - Ajout d'une todo
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@89277 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- ffedata/lang/paquet-ffedata_fr.php
+++ ffedata/lang/paquet-ffedata_fr.php
@@ -8,7 +8,8 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// F
 	'ffedata_description' => 'Ce plugin permet de récupérer les informations stockées sur le webservice de la FFE
 	exemples :
--	pour le modèle joueursffe : &lt;joueursffe|Ref=2011&gt;
+-	pour le modèle ffe_joueurs : &lt;ffe_joueurs|Ref=XXXX&gt; ou XXXX est le code du club recherché.
+- pour le modèle ffe_liste_clubs : &lt;ffe_liste_clubs|Ref=XX&gt; ou XX est le numéro du département recherché.
 	',
 	'ffedata_nom' => 'Data FFE',
 	'ffedata_slogan' => 'Les infos du webservice de la Fédération Française des Echecs',
