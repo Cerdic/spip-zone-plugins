a449e695aec6a016edfd40f8fbb9668871db0f84
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-05-13 20:59:51 +0000

    Fonctionne sous SPIP 3 avec SoyezCréateurs (Todo : migration vers ieconfig de Joseph)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@89328 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- savecfg/savecfg_fonctions.php
+++ savecfg/savecfg_fonctions.php
@@ -3,7 +3,7 @@
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 function savecfg_afficher_tout($flux) {
-	if($flux['args']['exec'] == 'cfg' AND _request('cfg')) {
+	if(($flux['args']['exec'] == 'cfg' AND _request('cfg')) OR (strpos($flux['args']['exec'],'configurer_') !== false)) {
 		$flux['data'] = debut_boite_info(true) . recuperer_fond('prive/formulaires_savecfg') . recuperer_fond('prive/formulaire_savecfg_import') . fin_boite_info(true);
 	}
 	return $flux;
