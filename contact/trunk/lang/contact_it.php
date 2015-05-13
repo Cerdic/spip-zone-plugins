9682e9efcb85b6f4e3948c74f7900a60cbac2bd2
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-05-13 15:53:13 +0000

    [Salvatore] [source:_plugins_/contact/trunk/lang/ contact] Export depuis http://trad.spip.net de la langue it
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@89321 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ contact/trunk/lang/contact_it.php
@@ -0,0 +1,126 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/contact?lang_cible=it
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'alerte_effacement' => 'Confermare la cancellazione dei dati di configurazione di contatto.',
+
+	// B
+	'bouton_effacer' => 'Cancellare',
+
+	// C
+	'champ_adresse' => 'Indirizzo',
+	'champ_civi_explication' => ' Regolazione avanzata del campo stato civile',
+	'champ_civi_mademoiselle_ok' => 'Proporre una scelta « Signorina » nel campo stato civile',
+	'champ_civilite' => 'Stato Civile',
+	'champ_code_postal' => 'Codice postale',
+	'champ_etat' => 'Stato',
+	'champ_impose' => 'Campo imposto',
+	'champ_infolettre' => 'Newsletter',
+	'champ_mail' => 'Email',
+	'champ_nom' => 'Cognome',
+	'champ_obligatoire' => 'Obbligatorio',
+	'champ_organisation' => 'Organizzazione',
+	'champ_pays' => 'Paese',
+	'champ_portable' => 'Cellulare',
+	'champ_prenom' => 'Nome',
+	'champ_sujet' => 'Soggetto',
+	'champ_telephone' => 'Telefono fisso',
+	'champ_texte' => 'Testo',
+	'champ_ville' => 'Città',
+	'configuration_barre_forum' => 'Mostrare la barra dei forum.',
+	'configuration_barre_impossible' => 'Un altro plugin impedisce l’uso del porte-plume nello spazio pubblico, le regolazioni qui sopra saranno senza effetto.',
+	'configuration_barre_oui' => 'Mostrare la barra tipo completa.',
+	'configuration_champs_message' => 'Regolazione del  porte-plume.',
+	'configuration_champs_message_explication' => 'Regolazione della barra tipografica associata al testo del messaggio.',
+	'configuration_destinataires' => 'Destinatari',
+	'configuration_destinataires_explication' => 'Scegliete uno o più autori che i visitatori potranno contattare. Non è nulla di definito, è l’autore numero 1 (colui che ha installato il sito) che sarà utilizzato.',
+	'configuration_destinataires_plusieurs' => ' Permettere ai visitatori di scegliere<b>più</b> destinatari tra la lista selezionata sopra',
+	'configuration_destinataires_plusieurs_et' => '... majorée par les auteurs éventuellement transmis au formulaire.',
+	'configuration_destinataires_plusieurs_ou' => '... o trai gruppi di autori eventualmente trasmessi al formulario.',
+	'configuration_destinataires_tous' => '… Mettere <b>tutti </b> questi autori ',
+	'configuration_destinataires_tous_et' => '... aggiungerli il gruppo di autori',
+	'configuration_destinataires_tous_ou' => '... o solamente quelli eventualmente trasmessi al formulario',
+	'configuration_destinataires_un' => 'Costringere i visitatori a scegliere <b>un solo</b> destinatario tra la lista selezionata sopra ',
+	'configuration_destinataires_un_et' => '... majorée par les auteurs éventuellement transmis au formulaire.',
+	'configuration_destinataires_un_ou' => '...o tra il gruppo di autori trasmessi eventualmente al formulario',
+	'configuration_intro' => 'Introduzione',
+	'configuration_intro_explication' => 'Scrivete eventualmente un testo che introdurrà il formulario.',
+	'configuration_no_barre' => 'Non proporre la barra tipo.',
+	'configuration_no_previsualisation' => 'Mostrare la barra tipo senza il bottone della previsualizzazione.',
+	'configuration_pj' => 'Allegati',
+	'configuration_pj_autoriser' => 'Autorizzare l’invio di allegati',
+	'configuration_pj_facteur_absent' => 'Per utilizzare questa funzione, voi dovete installare il plugin Facteur.',
+	'configuration_pj_nb_max' => 'Numero massimo di allegati',
+	'configuration_regle_liste_explication' => 'Scegliete una regola di calcolo della lista dei destinatari potenziali e il metodo di selezione proposto all ’internauta',
+	'configuration_texteinfolettre' => 'Testo newsletter ',
+	'configuration_texteinfolettre_explication' => '. Cambiate eventualmente il testo associato a cocher newsletter ',
+	'consulter_memoire' => 'Questo messaggio è registrato sul vostro sito :',
+	'courriel_de' => 'Da',
+
+	// D
+	'deplacement_possible' => 'Spostamento verticale par glissé-déposé',
+	'description_menu_contact' => ' Elemento del  menù verso il formulario di contatto',
+	'description_page-contact' => 'Acessibile via <i>spip.php ?page=contact</i>, questa pagina di contatto opzionale permette di fornire un formulario di contatto (pensate bene a aggiungere un link nel vostro menù.)',
+
+	// F
+	'form_destinataire' => 'Scegliete un destinatario',
+	'form_destinataires' => 'Scegliete i destinatairi',
+	'form_pj_ajouter_pluriel' => 'Aggiungere degli allegati',
+	'form_pj_ajouter_singulier' => 'Aggiungere un allegato',
+	'form_pj_fichier_ajoute' => 'Il file è stato aggiunto correttamente: ',
+	'form_pj_importer' => 'Importare un file',
+	'form_pj_previsu_pluriel' => 'Allegati',
+	'form_pj_previsu_singulier' => '	Allegato',
+	'form_pj_supprimer' => 'Cancellare',
+	'form_pj_titre' => 'Titolo del file',
+	'forum_attention_nbre_caracteres' => '<b>Attenzione!</b> il votro messaggio deve contenere almeno @nbre_caract@ caratteri.',
+
+	// H
+	'horodatage' => 'Formulario postato il: @horodatage@.',
+
+	// I
+	'inforigine' => 'Informazione transmessa via formulario : ',
+	'informez_moi' => ' Accetto di ricevere, per email, delle informazioni emanate dalla vostra organizzazione.',
+	'infos_collecte' => 'Informazioni raccolte',
+	'infos_supplementaires' => 'Informazioni supplementairi',
+
+	// M
+	'madame' => 'Signora',
+	'mademoiselle' => 'Signorina',
+	'message_erreur_robot' => 'Voi siete un robot. Cattivo robot.',
+	'message_erreur_transmission' => '  Un errore di trasmissione si è verificato, vi preghiamo di rinnovare la vostra domanda',
+	'message_redemander_pass' => 'Il vostro indirizzo email"@email@" è stato già registrato in seguito a un messaggio di contatto. Per validare il vostro  conto, domandate un <a href="@url_pass@">cambiamento di password</a> e identificatevi una prima volta ',
+	'modele_label_auteur' => 'Preselezionare un autore (facoltativo) ?',
+	'modele_nom_formulaire' => 'il formulario di contatto avanzato',
+	'monsieur' => 'Signore',
+	'msg_messagerie' => 'Messaggeria di contatto',
+
+	// N
+	'noisette_label_afficher_titre_noisette' => 'Mostrare un titolo?',
+	'noisette_label_niveau_titre' => 'Scegliere il livello del titolo',
+	'noisette_label_titre_noisette' => 'Titolo:',
+	'noisette_label_titre_noisette_perso' => 'Se titolo personnalizzato :',
+	'noisette_titre_perso' => 'Titolo personalizzato',
+
+	// P
+	'par_qui' => 'Da : ',
+	'preselection' => 'Décocher tous les items de la liste de destinataires',
+
+	// S
+	'sauv_message' => 'Salvare i messaggi di contatto',
+	'sauv_message_explication' => 'Permettere di salvare i messaggi di contatto e di mostrali in una messaggeria dedicata.',
+	'sauv_message_ok' => 'Salvare i messaggi di contatto',
+	'succes' => 'Grazie, il vostro messaggio è stato inviato correttamente. <br />Vi risponderemo non appena possibile. La squadra di @equipe_site@ ',
+
+	// T
+	'titre' => 'Formulario di contatto',
+	'titre_court' => 'Contatto'
+);
+
+?>
