2a8b779197c047d6f679d8d9995343404a2e0ab5
spip@oliver-fr.org <spip@oliver-fr.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-05-15 03:33:01 +0000

    [Salvatore] [source:_plugins_/gravatar/lang/ gravatar] Export depuis http://trad.spip.net de la langue oc_ni_mis
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@89352 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ gravatar/lang/gravatar_oc_ni_mis.php
@@ -0,0 +1,31 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/gravatar?lang_cible=oc_ni_mis
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cfg_titre_gravatar' => 'Gravatar',
+
+	// E
+	'explication_image_defaut' => 'Per aquelu que noun an d’avatar, utilisà :',
+
+	// L
+	'label_image_defaut' => 'Image predefinida',
+	'label_image_defaut_404' => 'mìnga image',
+	'label_image_defaut_gravatar' => 'lògou Gravatar',
+	'label_image_defaut_identicon' => '<i>Identicon</i> (generat d’après l’adressa e-mail)',
+	'label_image_defaut_monsterid' => '<i>MonsterID</i> (generat d’après l’adressa e-mail)',
+	'label_image_defaut_png' => '<tt>images/gravatar.png</tt>',
+	'label_image_defaut_retro' => '<i>Retro</i> (generat d’après l’adressa e-mail)',
+	'label_image_defaut_wavatar' => '<i>Wavatar</i> (generat d’après l’adressa e-mail)',
+	'label_taille' => 'Talha dei gravatar',
+
+	// T
+	'titre_gravatar_auteur' => 'Gravatar'
+);
+
+?>
