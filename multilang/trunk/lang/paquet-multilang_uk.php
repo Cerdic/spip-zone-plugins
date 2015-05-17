3010bf7162fa0ee437bdf9523cd2f3eba1ad73e1
olly.olga@gmail.com <olly.olga@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-05-17 03:35:13 +0000

    [Salvatore] [source:_plugins_/multilang/trunk/lang/ paquet-multilang] Export depuis http://trad.spip.net de la langue uk
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@89379 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ multilang/trunk/lang/paquet-multilang_uk.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-multilang?lang_cible=uk
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// M
+	'multilang_description' => 'Цей плагін додає мовне меню виду <code>[ru] [en] [uk]</code> у верхній частині кожної форми, відповідно до того, які мови вибрано в налаштуваннях сайту.',
+	'multilang_slogan' => 'Управляє відображенням контенту за допомогою декількох тегів мовного меню'
+);
+
+?>
