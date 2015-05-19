a9e7703538d7b3c5971366d62bfe40bac4f402a5
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-05-19 16:14:40 +0000

    ne pas compter les id_auteur==0
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@89537 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- reservation_evenement/trunk/action/editer_reservation.php
+++ reservation_evenement/trunk/action/editer_reservation.php
@@ -1,7 +1,9 @@
 <?php
-if (!defined('_ECRIRE_INC_VERSION')) return;
+if (!defined('_ECRIRE_INC_VERSION'))
+	return;
 
-function reservation_inserer($id_parent=null, $set=null) {
+function reservation_inserer($id_parent = null, $set = null) {
+	include_spip('inc/session');
 	$table_sql = table_objet_sql('reservation');
 	$champs = array();
 	$champs['statut'] = 'encours';
@@ -11,57 +13,49 @@ function reservation_inserer($id_parent=null, $set=null) {
 		$champs = array_merge($champs, $set);
 
 	// Envoyer aux plugins
-	$champs = pipeline('pre_insertion',
-		array(
-			'args' => array(
-				'table' => $table_sql,
-			),
-			'data' => $champs
-		)
-	);
-	
+	$champs = pipeline('pre_insertion', array(
+		'args' => array('table' => $table_sql, ),
+		'data' => $champs
+	));
+
 	$id = sql_insertq($table_sql, $champs);
-	
-	// On pas l'id dans la session	
+
+	// On pas l'id dans la session
 	session_set('id_reservation', $id);
 
-	if ($id){
-		pipeline('post_insertion',
-			array(
-				'args' => array(
-					'table' => $table_sql,
-					'id_objet' => $id,
-				),
-				'data' => $champs
-			)
-		);
+	if ($id) {
+		pipeline('post_insertion', array(
+			'args' => array(
+				'table' => $table_sql,
+				'id_objet' => $id,
+			),
+			'data' => $champs
+		));
 	}
 	return $id;
 }
 
-function reservation_instituer($id_reservation, $c, $calcul_rub=true) {
+function reservation_instituer($id_reservation, $c, $calcul_rub = true) {
 	$table_sql = table_objet_sql('reservation');
-	$trouver_table = charger_fonction('trouver_table','base');
+	$trouver_table = charger_fonction('trouver_table', 'base');
 
 	include_spip('inc/autoriser');
 	include_spip('inc/rubriques');
 	include_spip('inc/modifier');
-	include_spip('inc/config');	
+	include_spip('inc/config');
 	$config = lire_config('reservation_evenement');
-	
-	$row = sql_fetsel('statut,date,id_auteur,email,lang','spip_reservations','id_reservation='.intval($id_reservation));
+
+	$row = sql_fetsel('statut,date,id_auteur,email,lang', 'spip_reservations', 'id_reservation=' . intval($id_reservation));
 	$statut_ancien = $statut = $row['statut'];
 	$date_ancienne = $date = $row['date'];
-	
-	
+
 	$d = isset($c['date']) ? $c['date'] : null;
 	$s = isset($c['statut']) ? $c['statut'] : $statut;
-	
 
 	$champs = array();
 	// cf autorisations dans inc/instituer_objet
 	if ($s != $statut OR ($d AND $d != $date)) {
-		if (autoriser('modifier','reservation', $id_reservation))
+		if (autoriser('modifier', 'reservation', $id_reservation))
 			$statut = $champs['statut'] = $s;
 		else
 			spip_log("editer_reservation $id_reservation refus " . join(' ', $c));
@@ -71,11 +65,11 @@ function reservation_instituer($id_reservation, $c, $calcul_rub=true) {
 		// ou si l'objet est deja date dans le futur
 		// En cas de proposition d'un objet (mais pas depublication), idem
 		if ($champ_date) {
-			if ($champs['statut'] == 'publie'
-			 OR ($champs['statut'] == 'prop' AND !in_array($statut_ancien, array('publie', 'prop')))
-			 OR $d
-			) {
-				if ($d OR strtotime($d=$date)>time())
+			if ($champs['statut'] == 'publie' OR ($champs['statut'] == 'prop' AND !in_array($statut_ancien, array(
+				'publie',
+				'prop'
+			))) OR $d) {
+				if ($d OR strtotime($d = $date) > time())
 					$champs[$champ_date] = $date = $d;
 				else
 					$champs[$champ_date] = $date = date('Y-m-d H:i:s');
@@ -83,145 +77,139 @@ function reservation_instituer($id_reservation, $c, $calcul_rub=true) {
 		}
 	}
 
-    //les champs extras auteur
-    include_spip('cextras_pipelines');
-    $valeurs_extras=array();
-    
+	//les champs extras auteur
+	include_spip('cextras_pipelines');
+	$valeurs_extras = array();
 
-    if(function_exists('champs_extras_objet')){
-        //Charger les définitions pour la création des formulaires
-        $champs_extras_auteurs=champs_extras_objet(table_objet_sql('auteur'));
-        
-        if(is_array($champs_extras_auteurs)){
-            foreach( $champs_extras_auteurs as $value){
-                $valeurs_extras[$value['options']['nom']]=_request($value['options']['nom']); 
-            }
-        }
-        
-        $champs['donnees_auteur']=serialize( $valeurs_extras);
-    }
+	if (function_exists('champs_extras_objet')) {
+		//Charger les définitions pour la création des formulaires
+		$champs_extras_auteurs = champs_extras_objet(table_objet_sql('auteur'));
+
+		if (is_array($champs_extras_auteurs)) {
+			foreach ($champs_extras_auteurs as $value) {
+				$valeurs_extras[$value['options']['nom']] = _request($value['options']['nom']);
+			}
+		}
+
+		$champs['donnees_auteur'] = serialize($valeurs_extras);
+	}
 
 	// Envoyer aux plugins
-	$champs = pipeline('pre_edition',
-		array(
-			'args' => array(
-				'table' => 'spip_reservations',
-				'id_reservation' => $id_reservation,
-				'action'=>'instituer',
-				'statut_ancien' => $statut_ancien,
-				'date_ancienne' => $date_ancienne,
-			),
-			'data' => $champs
-		)
-	);
+	$champs = pipeline('pre_edition', array(
+		'args' => array(
+			'table' => 'spip_reservations',
+			'id_reservation' => $id_reservation,
+			'action' => 'instituer',
+			'statut_ancien' => $statut_ancien,
+			'date_ancienne' => $date_ancienne,
+		),
+		'data' => $champs
+	));
 
-	if (!count($champs)) return '';
-	
+	if (!count($champs))
+		return '';
+
 	// Envoyer les modifs.
-	objet_editer_heritage('reservation', $id_reservation,'', $statut_ancien, $champs);
+	objet_editer_heritage('reservation', $id_reservation, '', $statut_ancien, $champs);
 
 	// Invalider les caches
 	include_spip('inc/invalideur');
 	suivre_invalideur("id='reservation/$id_reservation'");
 
-
 	// Pipeline
-	pipeline('post_edition',
-		array(
-			'args' => array(
-				'table' => 'spip_reservations',
-				'id_reservation' => $id_reservation,
-				'action'=>'instituer',
-				'statut_ancien' => $statut_ancien,
-				'date_ancienne' => $date_ancienne,
-				'id_parent_ancien' => $id_rubrique,
-			),
-			'data' => $champs
-		)
-	);
-	
-	
+	pipeline('post_edition', array(
+		'args' => array(
+			'table' => 'spip_reservations',
+			'id_reservation' => $id_reservation,
+			'action' => 'instituer',
+			'statut_ancien' => $statut_ancien,
+			'date_ancienne' => $date_ancienne,
+			'id_parent_ancien' => $id_rubrique,
+		),
+		'data' => $champs
+	));
+
 	// Les traitements spécifiques
 
-
-	$action=charger_fonction('editer_objet','action');
-	$quantite=_request('quantite');
-	$set=array(
-		'id_reservation'=>$id_reservation,
-		'statut'=>$statut
+	$action = charger_fonction('editer_objet', 'action');
+	$quantite = _request('quantite');
+	$set = array(
+		'id_reservation' => $id_reservation,
+		'statut' => $statut
 	);
-	
+
 	// Gérer les détails des réservations
-	$evenements=_request('id_evenement');
+	$evenements = _request('id_evenement');
 
 	// Si les déclinaisons sont actives on récupère les évenements via le prix
-	 if(test_plugin_actif('declinaisons')){
-		 $evenements=array();
-		if($id_prix_objet=_request('id_objet_prix')){
-			foreach(array_keys($id_prix_objet )AS $id_evenement){
-				$evenements[]=$id_evenement;
+	if (test_plugin_actif('declinaisons')) {
+		$evenements = array();
+		if ($id_prix_objet = _request('id_objet_prix')) {
+			foreach (array_keys($id_prix_objet ) AS $id_evenement) {
+				$evenements[] = $id_evenement;
 			}
 		}
-	 }
-	// Si on n'est pas dans le cas d'une création, on récupère les détails attachées à la réservation
-	if(!is_array($evenements) OR (is_array($evenements) AND count($evenements)==0)){
-			
-		$evenements=array();
-		$sql=sql_select('id_evenement','spip_reservations_details','id_reservation='. $id_reservation);
-		while($data=sql_fetch($sql)){
-			$evenements[]=$data['id_evenement'];
-		}
 	}
-	else  {
-		$set['evenements']=$evenements;
-		set_request('evenements',$evenements);
+	// Si on n'est pas dans le cas d'une création, on récupère les détails attachées à la réservation
+	if (!is_array($evenements) OR (is_array($evenements) AND count($evenements) == 0)) {
+
+		$evenements = array();
+		$sql = sql_select('id_evenement', 'spip_reservations_details', 'id_reservation=' . $id_reservation);
+		while ($data = sql_fetch($sql)) {
+			$evenements[] = $data['id_evenement'];
+		}
+	} else {
+		$set['evenements'] = $evenements;
+		set_request('evenements', $evenements);
 	}
 	// Pour chaque évènement on crée un détail de la réservation
 
-	foreach($evenements AS  $id_evenement){ 
+	foreach ($evenements AS $id_evenement) {
 		// Si aucun détail n'est attaché à l'évènement, on le crée
-		if(!$reservations_detail=sql_fetsel('*','spip_reservations_details','id_reservation='.$id_reservation.' AND id_evenement='.$id_evenement)) {
-			$id_reservations_detail='new';
-			$set['id_prix_objet']=$id_prix_objet[$id_evenement];
-		}
-		else $id_reservations_detail=$reservations_detail['id_reservations_detail'];
-		
+		if (!$reservations_detail = sql_fetsel('*', 'spip_reservations_details', 'id_reservation=' . $id_reservation . ' AND id_evenement=' . $id_evenement)) {
+			$id_reservations_detail = 'new';
+			$set['id_prix_objet'] = $id_prix_objet[$id_evenement];
+		} else
+			$id_reservations_detail = $reservations_detail['id_reservations_detail'];
+
 		// Pour l'enregistrement
-		$set['id_evenement']=$id_evenement;
+		$set['id_evenement'] = $id_evenement;
 
-		// Eviter l'envoi d'une notification pour chaque détail   
-		set_request('envoi_separe_actif','non');			
+		// Eviter l'envoi d'une notification pour chaque détail
+		set_request('envoi_separe_actif', 'non');
 
-		$detail=$action($id_reservations_detail,'reservations_detail',$set);
-		
+		$detail = $action($id_reservations_detail, 'reservations_detail', $set);
+
 	}
 
 	// Notifications
 
-	if ((!$statut_ancien OR $statut != $statut_ancien ) &&
-		 (isset($config['activer'])) &&
-		 (isset($config['quand']) && is_array($config['quand']) && in_array($statut,$config['quand'])) &&
-		 ($notifications = charger_fonction('notifications', 'inc', true))
-		) {
-	   //Déterminer la langue pour les notifications	
-	   $lang=isset($row['lang'])?$row['lang']:lire_config('langue_site');
-	   lang_select($lang);	
+	if ((!$statut_ancien OR $statut != $statut_ancien) && (isset($config['activer'])) && (isset($config['quand']) && is_array($config['quand']) && in_array($statut, $config['quand'])) && ($notifications = charger_fonction('notifications', 'inc', true))) {
+		//Déterminer la langue pour les notifications
+		$lang = isset($row['lang']) ? $row['lang'] : lire_config('langue_site');
+		lang_select($lang);
 		// Determiner l'expediteur
-		$options = array('statut'=>$statut,'lang'=>$lang);
-		if( $config['expediteur'] != "facteur" )
-			$options['expediteur'] = $config['expediteur_'.$config['expediteur']];
+		$options = array(
+			'statut' => $statut,
+			'lang' => $lang
+		);
+		if ($config['expediteur'] != "facteur")
+			$options['expediteur'] = $config['expediteur_' . $config['expediteur']];
 
 		// Envoyer au vendeur et au client
 		$notifications('reservation_vendeur', $id_reservation, $options);
-		if($config['client']){
+		if ($config['client']) {
 			//$row['email']=trim($row['email']);
-			if(intval($row['id_auteur']) AND $row['id_auteur']>0)$options['email']=sql_getfetsel('email','spip_auteurs','id_auteur='.$row['id_auteur']);
-			else $options['email']=$row['email'];
-				
+			if (intval($row['id_auteur']) AND $row['id_auteur'] > 0)
+				$options['email'] = sql_getfetsel('email', 'spip_auteurs', 'id_auteur=' . $row['id_auteur']);
+			else
+				$options['email'] = $row['email'];
+
 			$notifications('reservation_client', $id_reservation, $options);
 		}
-			
+
 	}
 
-	return ''; // pas d'erreur
+	return '';
+	// pas d'erreur
 }
