83decfa9a7ff2d140ed22b38f7b41b3f4b581f4d
jeremy.scherrer@netapsys.fr <jeremy.scherrer@netapsys.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-05-19 15:52:56 +0000

    amélioration ergonomie et interface en cours
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@89536 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ info_sites/trunk/js/infosites.js
@@ -0,0 +1,3 @@
+// Si des <h2> n'ayant pas la classe "titre" existent dans la div.contenu dans #content :
+// append div.sommaire <ul> à #aside
+// pour chaque <h2> trouvé : append <li><a href="#(attribut id du <h2>)">texte du <h2></a></li>
