518be50874bddfd78b473c336bdc82a95f3ba543
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-05-22 09:31:22 +0000

    compatible 3.1
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@89585 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- forum_sans_previsu/trunk/forum_sans_previsu_pipelines.php
+++ forum_sans_previsu/trunk/forum_sans_previsu_pipelines.php
@@ -1,7 +1,9 @@
 <?php
 function forum_sans_previsu_formulaire_verifier($flux){
 	if($flux['args']['form'] === 'forum'){      
-			 unset($flux['data']['previsu']);
+		
+		unset($flux['data']['previsu']);
+		unset($flux['data']['message_erreur']);
 		}
 	return $flux;
 }
