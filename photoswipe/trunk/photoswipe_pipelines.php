abf6bc784e6e132c630e00f288ed83d5a90ee392
fil@rezo.net <fil@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-05-22 20:20:20 +0000

    application systematique sur toutes les images de plus de 500px de large ; curseur loupe au survol
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@89593 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- photoswipe/trunk/photoswipe_pipelines.php
+++ photoswipe/trunk/photoswipe_pipelines.php
@@ -1,7 +1,15 @@
 <?php
 
+function photoswipe_insert_head_css($flux) {
+
+	return $flux . "<style type='text/css'>
+	img[data-photo-src] { cursor: zoom-in; }
+	</style>
+	";
+}
+
 function photoswipe_insert_head($flux){
-	//$flux = photoswipe_insert_head_css($flux); // au cas ou il n'est pas implemente
+	$flux = photoswipe_insert_head_css($flux); // au cas ou il n'est pas implemente
 
 	$flux .='
 <script src="'.(find_in_path('photoswipe_insert.js')).'" type="text/javascript"></script>
@@ -13,25 +21,40 @@ photoswipe = {
 };
 $(function() {
     photoswipe_init();
-    $("img.photoshow").live("click", photoshow);
+    if (!!$.fn.on) {
+      $(document).on("click", "img[data-photo-src]", photoshow);
+    } else if (!!$.fn.live) {
+      $("img[data-photo-src]").live("click", photoshow);
+    }
 });
 </script>
 ';
 	return $flux;
 }
 
+function photoswipe_post_propre($texte) {
+	return filtre_photoswipe_preparer($texte);
+}
 
 function filtre_photoswipe_preparer($texte) {
 	foreach (extraire_balises($texte, 'img') as $img) {
-		if ($src = extraire_attribut($img, 'src')) {
-			// pour echapper à la ligne de filtres_images_lib_mini qui remplace tout :
-			// `$tag = str_replace($src,$surcharge['src'],$tag);`
+		if ($src = extraire_attribut($img, 'src')
+		AND !extraire_attribut($img, 'data-photosrc')
+		) {
+			$l = largeur($img);
+			$h = hauteur($img);
 
-			$photo_src = str_replace('.', '__.__', $src);
-			$img2 = inserer_attribut($img, 'data-photo-src', $photo_src);
-			$img2 = inserer_attribut($img2, 'data-photo-w', largeur($img));
-			$img2 = inserer_attribut($img2, 'data-photo-h', hauteur($img));
-			$texte = str_replace($img, $img2, $texte);
+			if ($l > 500 OR $h > 300) {
+
+	// pour echapper à la ligne de filtres_images_lib_mini qui remplace tout:
+	// `$tag = str_replace($src,$surcharge['src'],$tag);`
+
+				$photo_src = str_replace('.', '__.__', $src);
+				$img2 = inserer_attribut($img, 'data-photo-src', $photo_src);
+				$img2 = inserer_attribut($img2, 'data-photo-w', $l);
+				$img2 = inserer_attribut($img2, 'data-photo-h', $h);
+				$texte = str_replace($img, $img2, $texte);
+			}
 		}
 	}
 	return $texte;
