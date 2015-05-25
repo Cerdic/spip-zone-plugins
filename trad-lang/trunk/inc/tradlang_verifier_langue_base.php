f20b2f6df81d50e930c7423262951ac0d6c5a102
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-05-25 08:01:48 +0000

    Récupérer la chaîne depuis attic si présente
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@89637 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- trad-lang/trunk/inc/tradlang_verifier_langue_base.php
+++ trad-lang/trunk/inc/tradlang_verifier_langue_base.php
@@ -22,23 +22,30 @@ function inc_tradlang_verifier_langue_base_dist($module,$langue){
 	 */
 	$langue_mere = sql_getfetsel('lang_mere','spip_tradlang_modules','module='.sql_quote($module));
 	
-	$trad_langue_mere_id = $trad_langue_cible_id = array();
+	$trad_langue_mere_id = $trad_langue_cible_id = $trad_langue_cible_id_attic = array();
 	
 	/**
-	 * On crée deux tableaux: 
+	 * On crée trois tableaux: 
 	 * -* l'ensemble des id de la langue mère non supprimés 
 	 * -* l'ensemble des id de la langue cible non supprimés
+	 * -* l'ensemble des id de la langue cible qui ont été supprimés par le passé (dans le cas où l'on doit en récupérer)
 	 */
-	$trad_langue_meres = sql_allfetsel('id','spip_tradlangs','module='.sql_quote($module).' AND lang='.sql_quote($langue_mere).' AND statut !="attic"');
+	$trad_langue_meres = sql_allfetsel('id','spip_tradlangs','module='.sql_quote($module).' AND lang='.sql_quote($langue_mere).' AND statut !="attic"','','id');
 	foreach($trad_langue_meres as $trad_langue_mere){
-		$trad_langue_mere_id[] = $trad_langue_mere['id']; 
+		$trad_langue_mere_id[] = $trad_langue_mere['id'];
 	}
-	$trad_langue_cibles  = sql_allfetsel('id','spip_tradlangs','module='.sql_quote($module).' AND lang='.sql_quote($langue).' AND statut !="attic"');
+	$trad_langue_cibles  = sql_allfetsel('id','spip_tradlangs','module='.sql_quote($module).' AND lang='.sql_quote($langue).' AND statut !="attic"','','id');
 	foreach($trad_langue_cibles as $trad_langue_cible){
 		$trad_langue_cible_id[] = $trad_langue_cible['id']; 
 	}
+	
+	$trad_langue_cibles_attic  = sql_allfetsel('id','spip_tradlangs','module='.sql_quote($module).' AND lang='.sql_quote($langue).' AND statut ="attic"','','id');
+	foreach($trad_langue_cibles_attic as $trad_langue_cible_attic){
+		$trad_langue_cible_id_attic[] = $trad_langue_cible_attic['id']; 
+	}
 
-	$inserees = $supprimees = 0;
+	$inserees = $supprimees = $recuperees = 0;
+
 	/**
 	 * $diff1 est l'ensemble des chaines manquantes dans la langue cible
 	 * et donc à insérer
@@ -54,22 +61,33 @@ function inc_tradlang_verifier_langue_base_dist($module,$langue){
 	 * et donc à supprimer
 	 */
 	$diff2 = array_diff($trad_langue_cible_id,$trad_langue_mere_id);
-	
+
 	/**
 	 * Si on a des éléments dans les diffs, on applique les modifications
 	 */
 	if((count($diff1)>0) OR (count($diff2)>0)){
 		if(isset($diff1_array) && is_array($diff1_array)){
 			foreach($diff1_array as $key => $array){
-				$array['orig'] = 0;
-				$array['lang'] = $langue;
-				$array['titre'] = $array['id'].' : '.$array['module'].' - '.$langue;
-				$array['statut'] = 'NEW';
-				unset($array['maj']);
-				unset($array['id_tradlang']);
-				unset($array['traducteur']);
-				$id_tradlang = sql_insertq('spip_tradlangs',$array);
-				$inserees++;
+				/**
+				 * La chaine était préalablement supprimée
+				 * Elle a le statut "attic"
+				 * On la récupère donc en lui donnant le statut "MODIF"
+				 */
+				if(in_array($array['id'],$trad_langue_cible_id_attic)){
+					sql_updateq('spip_tradlangs',array('statut'=>'MODIF'),'id='.sql_quote($array['id']).' AND lang='.sql_quote($langue).' AND statut="attic"');
+					//spip_log('Recuperation de l\'id '.$array['id'].' de la langue "'.$langue.'" qui était dans l\'attic','tradlang.'._LOG_ERREUR);
+					$recuperees++;
+				}else{
+					$array['orig'] = 0;
+					$array['lang'] = $langue;
+					$array['titre'] = $array['id'].' : '.$array['module'].' - '.$langue;
+					$array['statut'] = 'NEW';
+					unset($array['maj']);
+					unset($array['id_tradlang']);
+					unset($array['traducteur']);
+					$id_tradlang = sql_insertq('spip_tradlangs',$array);
+					$inserees++;
+				}
 			}
 		}
 		/**
@@ -83,9 +101,11 @@ function inc_tradlang_verifier_langue_base_dist($module,$langue){
 			}
 		}
 	}else
-		return array('0','0');
+		return array('0','0','0');
+
 	include_spip('inc/invalideur');
 	suivre_invalideur('1');
-	return array($inserees,$supprimees);
+	spip_log("insert => $inserees - suppressions => $supprimees - recuperations => $recuperees","bilan."._LOG_ERREUR);
+	return array($inserees,$supprimees,$recuperees);
 }
 ?>
