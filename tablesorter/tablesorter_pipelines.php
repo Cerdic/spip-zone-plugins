e1802455bf05ed041afc78f0e44a9ce328ac2584
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-05-27 11:04:55 +0000

    Un peu de doc et montée de version
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@89707 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- tablesorter/tablesorter_pipelines.php
+++ tablesorter/tablesorter_pipelines.php
@@ -1,7 +1,22 @@
 <?php
-
+/**
+ * Pipelines du plugin Tabelesorter
+ * 
+ * @plugin     Tablesorter
+ * @licence    GNU/GPL v3
+ * @package    SPIP\Tablesorter\Pipelines
+ */
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
+/**
+ * Insertion dans le pipeline insert_head_css (SPIP)
+ * 
+ * Ajout de la feuille de style dans le head
+ * 
+ * @pipeline insert_head_css
+ * @param string $flux Le contenu de la balise #INSERT_HEAD_CSS
+ * @return string $flux Le contenu complété de la balise
+ */
 function tablesorter_insert_head_css($flux){
 	static $done = false;
 	if (!$done) {
@@ -12,11 +27,20 @@ function tablesorter_insert_head_css($flux){
 	return $flux;
 }
 
-
+/**
+ * Insertion dans le pipeline insert_head (SPIP)
+ * 
+ * Ajout de la librairie js tablesorter et du script d'init 
+ * dans le head de l'espace public
+ * Ajout également, pour les anciennes versions de SPIP de la CSS
+ * dans le head
+ * 
+ * @pipeline insert_head
+ * @param string $flux Le contenu de la balise #INSERT_HEAD
+ * @return string $flux Le contenu complété de la balise
+ */
 function tablesorter_insert_head($flux){
-	// Insertion des librairies js
 	$flux .='<script src="'.find_in_path('javascript/jquery.tablesorter.js').'" type="text/javascript"></script>';
-	// Init de tablesorter
 	$flux .='
 	<script type="text/javascript">/* <![CDATA[ */
 	(function($){
@@ -44,6 +68,16 @@ function tablesorter_insert_head($flux){
 	return $flux;
 }
 
+/**
+ * Insertion dans le pipeline header_prive (SPIP)
+ * 
+ * Ajout de la librairie js tablesorter, du script d'init et de la CSS
+ * dans le head de l'espace privé
+ * 
+ * @pipeline header_prive
+ * @param string $flux Le contenu du head de l'espace privé
+ * @return string $flux Le contenu complété du head
+ */
 function tablesorter_header_prive($flux){
 	// Insertion des librairies js
 	$flux .='<script src="'.find_in_path('javascript/jquery.tablesorter.js').'" type="text/javascript"></script>';
