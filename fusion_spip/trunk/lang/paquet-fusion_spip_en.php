047dfc2340081a0bc509b74ae674905daa028c4e
jacques@jack31.net <jacques@jack31.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-05-29 03:33:47 +0000

    [Salvatore] [source:_plugins_/fusion_spip/trunk/lang/ paquet-fusion_spip] Export depuis http://trad.spip.net de la langue en
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@89754 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ fusion_spip/trunk/lang/paquet-fusion_spip_en.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-fusion_spip?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// F
+	'fusion_spip_description' => 'Import the full content of a source site, objects, links, documents by updating the identifiers of the objects, internal links and models.', # RELIRE
+	'fusion_spip_slogan' => 'Import and merge all the content of another website'
+);
+
+?>
