7db9fc3b6f9c3c51b595919e69130ac954207ecb
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-05-30 20:56:18 +0000

    Surcharger les autorisations du plugin medias
    Pourquoi ne peut on lier des docs à des docs?
    
    http://zone.spip.org/trac/spip-zone/changeset/89155/_core_#file3
    
    Il y a des gens qui font cela depuis des années
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@89796 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- facd/facd_autoriser.php
+++ facd/facd_autoriser.php
@@ -32,4 +32,46 @@ function autoriser_relancerconversion_facd_dist($faire, $type, $id, $qui, $opt){
 	$id_auteur = sql_getfetsel('id_auteur','spip_facd_conversions','id_facd_conversion='.intval($id));
 	return ($qui['id_auteur'] == $id_auteur) OR autoriser('configurer','','',$qui,$opt);
 }
+
+/**
+ * Surcharges des autorisations du plugin medias 
+ * depuis http://zone.spip.org/trac/spip-zone/changeset/89155/_core_#file3
+ * On doit lier des docs à d'autres docs
+ */
+if(!function_exists('autoriser_associerdocuments')){
+/** 
+ * Autoriser a associer des documents a un objet : 
+ * il faut avoir le droit de modifier cet objet
+ *  
+ * @param $faire 
+ * @param $type 
+ * @param $id 
+ * @param $qui 
+ * @param $opt 
+ * @return bool 
+ */ 
+function autoriser_associerdocuments($faire, $type, $id, $qui, $opt){
+	return autoriser('modifier',$type,$id,$qui,$opt); 
+} 
+}
+
+if(!function_exists('autoriser_dissocierdocuments')){
+/** 
+ * Autoriser a dissocier des documents a un objet : 
+ * il faut avoir le droit de modifier cet objet
+ *  
+ * @param $faire 
+ * @param $type 
+ * @param $id 
+ * @param $qui 
+ * @param $opt 
+ * @return bool 
+ */ 
+function autoriser_dissocierdocuments($faire, $type, $id, $qui, $opt){
+	if (intval($id)<0 AND $id==-$qui['id_auteur']){
+		return true; 
+	}
+	return autoriser('modifier',$type,$id,$qui,$opt); 
+}
+}
 ?>
