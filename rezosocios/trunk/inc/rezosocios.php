bf7f47d74fb266a920accce043832d3bd62748f8
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-06-03 16:14:24 +0000

    Deux filtres :
    * rezosocios_url qui retourne l'url complète du compte 
    * rezosocios_logo qui retourne le logo du type de reseau social
    
    On transforme donc le tableau des réseaux sociaux pour avoir le couple nom/url
    
    On y ajoute un pipeline pour ajouter de nouveaux types de réseaux sociaux
    
    Version 0.4.0
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@89877 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- rezosocios/trunk/inc/rezosocios.php
+++ rezosocios/trunk/inc/rezosocios.php
@@ -4,19 +4,52 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 
 function rezosocios_liste(){
 	$rezosocios = array(
-					'facebook' => 'Facebook',
-					'twitter' => 'Twitter',
-					'google+' => 'Google+',
-					'youtube' => 'Youtube',
-					'dailymotion' => 'Dailymotion',
-					'pinterest' => 'Pinterest',
-					'flickr' => 'Flickr',
-					'storify' => 'Storify',
-					'linkedin' => 'Linkedin',
-					'weibo' => 'Weibo',
-					'VK' => 'VK',
+					'facebook' => array(
+									'nom' => 'Facebook',
+									'url' => 'https://www.facebook.com/'
+								),
+					'twitter' => array(
+									'nom' => 'Twitter',
+									'url' => 'https://twitter.com/'
+								),
+					'google+' => array(
+									'nom' => 'Google+',
+									'url' => 'https://plus.google.com/+'
+								),
+					'youtube' => array(
+									'nom' => 'Youtube',
+									'url' => 'https://www.youtube.com/user/'
+								),
+					'dailymotion' => array(
+									'nom' => 'Dailymotion',
+									'url' => 'http://www.dailymotion.com/'
+								),
+					'pinterest' => array(
+									'nom' => 'Pinterest',
+									'url' => 'https://www.pinterest.com/'
+								),
+					'flickr' => array(
+									'nom' => 'Flickr',
+									'url' => 'https://www.flickr.com/photos/'
+								),
+					'storify' => array(
+									'nom' => 'Storify',
+									'url' => 'https://storify.com/'
+								),
+					'linkedin' => array(
+									'nom' => 'Linkedin',
+									'url' => 'https://www.linkedin.com/profile/view?id='
+								),
+					'weibo' => array(
+									'nom' => 'Weibo',
+									'url' => 'http://www.weibo.com/'
+								),
+					'VK' => array(
+									'nom' => 'VK',
+									'url' => 'http://vk.com/'
+								)
 				);
-	
+	$rezosocios = pipeline('rezosocios_liste',$rezosocios);
 	return $rezosocios;
 }
 ?>
--- rezosocios/trunk/rezosocios_fonctions.php
+++ rezosocios/trunk/rezosocios_fonctions.php
@@ -6,11 +6,29 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 function rezosocios_nom($nom){
 	include_spip('inc/rezosocios');
 	
-	$noms = rezosocios_liste();
+	$rezosocios = rezosocios_liste();
 	
-	if(isset($noms[$nom]))
-		$nom = $noms[$nom];
+	if(isset($rezosocios[$nom]))
+		$nom = $rezosocios[$nom]['nom'];
 	
 	return $nom;
+}
+
+function rezosocios_url($nom,$compte){
+	include_spip('inc/rezosocios');
+	
+	$rezosocios = rezosocios_liste();
+	
+	if(isset($rezosocios[$nom]))
+		$url = $rezosocios[$nom]['url'].$compte;
+	else
+		$url = false;
+	
+	return $url;
+}
+
+function rezosocios_logo($nom){
+	$logo = chemin_image($nom.'-32.png');
+	return $logo;
 }
 ?>
