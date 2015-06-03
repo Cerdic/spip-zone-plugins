c1f85b2414d1eb9b26caa4513f9cb2001c660dcf
spip@oliver-fr.org <spip@oliver-fr.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-06-03 03:33:36 +0000

    [Salvatore] [source:_plugins_/langonet/branches/v0/lang/ langonet] Export depuis http://trad.spip.net de la langue oc_ni_mis
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@89860 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ langonet/branches/v0/lang/langonet_oc_ni_mis.php
@@ -0,0 +1,97 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/langonet?lang_cible=oc_ni_mis
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'bouton_corriger' => 'Outenì li courrecioun',
+	'bouton_generer' => 'Generà',
+	'bouton_langonet' => 'LangOnet',
+	'bouton_lister' => 'Afichà',
+	'bouton_rechercher' => 'Recercà',
+	'bouton_verifier' => 'Verificà',
+	'bulle_afficher_fichier_lang' => 'Afichà lou fichié de lenga generat lou @date@',
+	'bulle_afficher_fichier_log' => 'Afichà lou log dóu @date@',
+	'bulle_corriger' => 'Telecargà lou fichié de lenga courret',
+	'bulle_telecharger_fichier_lang' => 'Telecargà lou fichié de lenga generat lou @date@',
+	'bulle_telecharger_fichier_log' => 'Telecargà lou log dóu @date@',
+
+	// E
+	'entete_log_avertissement_nonmais' => 'AVERTIMEN : item que noun apartenon au mòdulou',
+	'entete_log_avertissement_peutetre_definition' => 'AVERTIMEN : item bessai noun definit',
+	'entete_log_avertissement_peutetre_utilisation' => 'AVERTIMEN : item bessai noun utilisat',
+	'entete_log_date_creation' => 'Fichié generat lou @log_date_jour@ a @log_date_heure@.',
+	'entete_log_erreur_definition' => 'ERROUR : item dóu mòdulou noun definit',
+	'entete_log_erreur_definition_nonmais' => 'ERROUR : item d’autre mòdulou noun definit',
+	'entete_log_erreur_fonction_l' => 'ERROUR : cas d’utilisacioun de la founcioun _L()',
+	'entete_log_erreur_utilisation' => 'ERROUR : item noun utilisat',
+
+	// I
+	'info_arborescence_scannee' => 'Chausissès lou repertori de basa que l’arbouressença serà escanada',
+	'info_bloc_langues_generees' => 'Clicàs soubre l’estac aquì souta per telecargà un dei fichié de lenga generat.',
+	'info_bloc_logs_definition' => 'Clicàs soubre l’estac aquì souta per telecargà lou darrié fichié de log de verificacioun dei definicioun que mancon en un fichié de lenga.',
+	'info_bloc_logs_fonction_l' => 'Clicàs soubre l’estac aquì souta per telecargà lou darrié fichié de log de verificacioun dei utilisacioun de _L() en un arbouressença dounada.',
+	'info_bloc_logs_utilisation' => 'Clicàs soubre un estac aquì souta per telecargà lou darrié fichié de log de verificacioun dei definicioun d’un fichié de lenga.',
+	'info_chemin_langue' => 'Doussié en lou qual es instalat lou fichié de lenga (isemple : <em>plugins/rainette/lang/</em>, o <em>ecrire/lang/</em>)',
+	'info_fichier_liste' => 'Chausissès lou fichié de lenga que voulès afichà lu item, tra aquelu dau sit.',
+	'info_fichier_verifie' => 'Chausissès lou fichié de lenga da verificà tra aquelu dau sit.',
+	'info_generer' => 'Aquela oupcioun vi permete de generà, a partì d’una lenga sourgent, lou fichié de lenga d’un mòdulou en una la lenga bressai. Se lou fichié bressai jà esista, lou siéu countegut es reutilisat per coustruì lou fichié nouvèu.',
+	'info_langue' => 'Abreviacioun de la lenga (isemple : <em>fr</em>, <em>en</em>, <em>es</em>...)',
+	'info_lister' => 'Aquela oupcioun vi permete de visualisà lu item d’un fichié de lenga classificat da ordre alfabéticou.',
+	'info_mode' => 'Courrespouònde a la cadena que serà inserit en tems de la creacioun d’un item nouvèu per la lenga bressai.',
+	'info_module' => 'Courrespouònde au prefisse dou fichié de lenga sensa abreviacioun de la lenga (isemple : <em>rainette</em> per lou plugin dau meme noum, o <em>ecrire</em> per SPIP)',
+
+	// L
+	'label_arborescence_scannee' => 'Arbouressença da scanà',
+	'label_avertissement' => 'Avertimen',
+	'label_chemin_langue' => 'Loucalisacioun dóu fichié de lenga',
+	'label_correspondance' => 'Tìpou de courrespondença',
+	'label_correspondance_commence' => 'Coumença da',
+	'label_correspondance_contient' => 'Counten',
+	'label_correspondance_egal' => 'Egal a',
+	'label_erreur' => 'Errour',
+	'label_fichier_liste' => 'Fichié de lenga',
+	'label_fichier_verifie' => 'Lenga da verificà',
+	'label_langue_cible' => 'Lenga bressai',
+	'label_langue_source' => 'Lenga sourgent',
+	'label_mode' => 'Mòdou de creacioun dei item nouvéu',
+	'label_module' => 'Mòdulou',
+	'label_pattern' => 'Cadena da cercà',
+	'label_verification' => 'Tìpou de verificacioun',
+	'label_verification_definition' => 'Detecioun dei definicioun que mancon',
+	'legende_resultats' => 'Resultat de la verificacioun',
+	'legende_table' => 'Lista dei item dóu fichié de lenga chausit',
+	'legende_trouves' => 'Lista dei item trouvat (@total@)',
+
+	// M
+	'message_nok_aucun_fichier_log' => 'Mìnga fichié de log dispounible dau telecargamen',
+	'message_nok_aucune_langue_generee' => 'Mìnga fichié de lenga generat dispounible dau telecargamen',
+	'message_nok_champ_obligatoire' => 'Aquestou camp es oubligatori',
+	'message_nok_item_trouve' => 'Mìnga item de lenga courresponònde a la recerca !',
+
+	// O
+	'onglet_generer' => 'Generà una lenga',
+	'onglet_lister' => 'Afichà una lenga',
+	'onglet_rechercher' => 'Cercà un item',
+	'onglet_verifier' => 'Verificà una lenga',
+	'option_mode_vide' => 'Una cadena vuèia',
+
+	// T
+	'texte_item_defini_ou' => '<em>definit en :</em>',
+	'texte_item_utilise_ou' => '<em>utilisat en :</em>',
+	'titre_bloc_langues_generees' => 'Fichié de lenga',
+	'titre_bloc_logs_definition' => 'Definicioun que mancon',
+	'titre_bloc_logs_fonction_l' => 'Utilisacioun de _L()',
+	'titre_form_generer' => 'Generacioun dei fichié de lenga',
+	'titre_form_lister' => 'Afichage dei fichié de lenga',
+	'titre_form_rechercher_item' => 'Recerca d’escourcha en lu fichié de lenga',
+	'titre_form_verifier' => 'Verificacioun dei fichié de lenga',
+	'titre_page' => 'LangOnet',
+	'titre_page_navigateur' => 'LangOnet'
+);
+
+?>
