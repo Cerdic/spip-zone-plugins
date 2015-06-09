82452707c501bba31bf3cc1c53eee8fdea5031e4
marcimat@rezo.net <marcimat@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-06-09 16:12:48 +0000

    Divers orthographe & grammaire…
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@90052 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- uploadhtml5/trunk/lang/uploadhtml5_fr.php
+++ uploadhtml5/trunk/lang/uploadhtml5_fr.php
@@ -9,11 +9,9 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'uploadhtml5_titre' => 'Formulaire upload html5',
 
 	// C
-	'cfg_exemple' => 'Exemple',
-	'cfg_exemple_explication' => 'Explication de cet exemple',
 	'cfg_titre_parametrages' => 'Paramétrages',
     'label_charger_public' => "Script dans l'espace public",
-    'case_charger_public' => "Charger les scripts sur dans l'espace publique",
+    'case_charger_public' => "Charger les scripts sur dans l'espace public",
 
 	// T
 	'titre_page_configurer_uploadhtml5' => 'Configuration du formulaire d\'upload html5',
@@ -22,18 +20,18 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
     // M
     'max_file_size' => 'Taille maximum des fichiers',
     'explication_max_file_size' => "Taille maximum des fichiers (en MB). Il est conseillé d'utiliser la valeur du serveur.",
-    'max_file' => 'Nombre maximum de fichier',
-    'explication_max_file' => 'Nombre de maximum de fichiers qui peuvent être uploadé simultanément (0 pour ne pas limiter).',
+    'max_file' => 'Nombre maximum de fichiers',
+    'explication_max_file' => 'Nombre de maximum de fichiers qui peuvent être uploadés simultanément (0 pour ne pas limiter).',
 
     // Message de la dropzone_prive
-    'drop_ici' => 'Déposez vos fichier ici ou cliquez sur ce cadre',
-    'drop_no_support' => 'Votre navigateur ne supporte pas le Drag and Drop',
+    'drop_ici' => 'Déposez vos fichiers ici ou cliquez sur ce cadre',
+    'drop_no_support' => 'Votre navigateur ne supporte pas le Glisser-Déposer',
     'drop_fallbacktext' => "Merci d'utiliser ce formulaire à la place",
-    'drop_fichier_invalide' => 'type de fichier invalide',
+    'drop_fichier_invalide' => 'Type de fichier invalide',
     'drop_fichier_trop_gros' => 'Votre fichier est trop volumineux',
     'drop_annuler' => 'Envoie annulé',
-    'drop_annuler_confirm' => 'Annuler cette envoie ?',
-    'drop_max_file' => 'Nombre maximum de fichier atteind',
+    'drop_annuler_confirm' => 'Annuler cet envoi ?',
+    'drop_max_file' => 'Nombre maximum de fichiers atteint',
 
     // Message pour la drop_zone des logo
     'logo_drop_ici' => 'Déposez votre logo ici ou cliquez sur ce cadre'
