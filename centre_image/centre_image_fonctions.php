d6bd46d18e708b259832939fb6a95a97b5436b67
marcimat@rezo.net <marcimat@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-06-09 11:44:59 +0000

    Quelques menues corrections de notices PHP + un peu de phpdoc + un lien temporaire de documentation + suppression des lignes vides et quelques tabulations avec des espaces
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@90038 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- centre_image/centre_image_fonctions.php
+++ centre_image/centre_image_fonctions.php
@@ -1,20 +1,34 @@
 <?php
 
 
+/**
+ * Retourne les coordonnées du point d'intérêt de l'image transmise
+ *
+ * Retourne les coordonnées `[0.5, 0.5]` par défaut (si le calcul échoue par exemple).
+ * 
+ * @param string $fichier
+ *     Chemin du fichier ou balise `<img>`
+ * @return float[]
+ *     Tableau (x, y) des coordonnées du point d'intéret ;
+ *     - x entre 0 (à gauche) et 1 (à droite)
+ *     - y entre 0 (en haut) et 1 (en bas)
+**/
 function centre_image($fichier) {
 	static $spip_centre_image = array();
-	
+
 	// nettoyer le fichier (qui peut être dans un <img> ou qui peut être daté)
 	if (preg_match("/src\=/", $fichier)) $fichier = extraire_attribut($fichier, "src");
 	$fichier = preg_replace(",\?[0-9]*$,", "", $fichier);
 
 	// on mémorise le résultat -> don
-	if ($spip_centre_image["$fichier"]) return $spip_centre_image["$fichier"];
+	if (isset($spip_centre_image[$fichier])) {
+		return $spip_centre_image[$fichier];
+	}
 
 	if (file_exists($fichier)) {
-			
+
 		$md5 = $fichier;
-		if (_DIR_RACINE == "../") {
+		if (test_espace_prive()) {
 			$md5 = preg_replace(",^\.\.\/,", "", $md5);
 		}
 		$md5 = md5($md5);
@@ -24,26 +38,26 @@ function centre_image($fichier) {
 		$cache = sous_repertoire(_DIR_VAR, "cache-centre-image");
 		$cache = sous_repertoire($cache, $l1);
 		$cache = sous_repertoire($cache, $l2);
-		
+
 		$forcer = sous_repertoire(_DIR_IMG, "cache-centre-image");
-		
+
 		$fichier_json = "$cache$md5.json";
 		$fichier_forcer = "$forcer$md5.json";
-		
+
 		if (file_exists($fichier_forcer) and filemtime($fichier_forcer) > filemtime($fichier)) {
 			$res = json_decode(file_get_contents($fichier_forcer),TRUE);
 		}
 		else if (file_exists($fichier_json) and filemtime($fichier_json) > filemtime($fichier)) {
 			$res = json_decode(file_get_contents($fichier_json),TRUE);
 		} else {
-			if(function_exists("imagefilter")) {
+			if (function_exists("imagefilter")) {
 				if (preg_match(",\.(gif|jpe?g|png)($|[?]),i", $fichier, $regs)) {
 					include_spip('inc/centre_image_lib');
 					include_spip('inc/filtres_images_lib_mini');
 					$terminaison = strtolower($regs[1]);
 					$terminaison = str_replace("jpg", "jpeg", $terminaison);
 					$fonction_imagecreatefrom = "_imagecreatefrom".$terminaison;
-				
+
 					$img     = $fonction_imagecreatefrom($fichier);
 					$cropper = new _centre_image($img);
 					$res = $cropper->find_focus();
@@ -52,21 +66,40 @@ function centre_image($fichier) {
 			} else {
 				$res = array("x" => 0.5, "y" => 0.5);
 			}
-			
+
 			file_put_contents($fichier_json, json_encode($res,TRUE));
 		}
-    } else {
-    	$res = array("x" => 0.5, "y" => 0.5);
-    }
-    
-    $spip_centre_image["$fichier"] = $res;
+	} else {
+		$res = array("x" => 0.5, "y" => 0.5);
+	}
+
+	$spip_centre_image["$fichier"] = $res;
 	return $res;    
 }
 
+/**
+ * Retourne la coordonnée x du point d'intérêt de l'image transmise
+ *
+ * @uses centre_image()
+ * @param string $fichier
+ *     Chemin du fichier ou balise `<img>`
+ * @return float
+ *     Coordonnée x du point d'intéret, entre 0 (à gauche) et 1 (à droite)
+**/
 function centre_image_x($fichier) {
 	$res = centre_image($fichier);
 	return $res["x"];
 }
+
+/**
+ * Retourne la coordonnée y du point d'intérêt de l'image transmise
+ *
+ * @uses centre_image()
+ * @param string $fichier
+ *     Chemin du fichier ou balise `<img>`
+ * @return float
+ *     Coordonnée y du point d'intéret, entre 0 (en haut) et 1 (en bas)
+**/
 function centre_image_y($fichier) {
 	$res = centre_image($fichier);
 	return $res["y"];
@@ -74,16 +107,28 @@ function centre_image_y($fichier) {
 
 
 
+/**
+ * Ajoute les scripts nécessaires dans l'espace privé
+ *
+ * @pipeline header_prive
+ * @param string $flux Texte dans le head HTML
+ * @return string
+**/
 function centre_image_header_prive($flux) {
 	$flux .= "\n<script type='text/javascript' src='".find_in_path("centre_image_gestion.js")."'></script>\n";
-	
 	$flux .= "\n<script>var croix = '".find_in_path("imgs/croix.png")."'</script>";
-
 	return $flux;
 }
 
+/**
+ * Ajoute les plugins jquery ui nécessaires dans l'espace privé
+ *
+ * @pipeline jqueryui_plugins
+ * @param string[] $plugins
+ * @return string[]
+**/
 function centre_image_jqueryui_plugins($plugins) {
-	if (_DIR_RACINE == "../") {
+	if (test_espace_prive()) {
 		$plugins[] = "jquery.ui.core";
 		$plugins[] = "jquery.ui.draggable";
 	}
--- centre_image/centre_image_gestion.js
+++ centre_image/centre_image_gestion.js
@@ -1,43 +1,42 @@
 function centre_image_croix(el, x, y) {
-
 	if (el.find("img.croix_centre_image").length == 0) {
 		el.css("display", "inline-block").css("position", "relative")
 			.find("img").addClass("img_source").css("border", "1px solid green")
 			.parent()
 			.prepend("<img src='"+croix+"' class='croix_centre_image' style='cursor: move; position: absolute; margin-left: -7px; margin-top: -7px; margin-right: -7px; margin-bottom: -7px;'>");
 	}
-	
+
 	el.find("img.croix_centre_image").css("left", x+"px").css("top", y+"px")
 		.draggable({
 			containment: "parent",
 			stop: function(event, ui) {
 				var lien = el.attr("href");
 				var url = lien.replace(/^\.\.\//, '')
-				
+
 				var x = ui.position.left / el.find("img.img_source").width();
 				var y = ui.position.top / el.find("img.img_source").height();
-				
+
 				x = Math.max(0, x);
 				x = Math.min(1, x);
 				y = Math.max(0, y);
 				y = Math.min(1, y);
-				
-				
+
 				$.ajax("index.php?action=centre_image_forcer&x="+x+"&y="+y+"&url="+url);
 			}
 		});
-
-
 }
+
 function centre_image_afficher() {
+
 	$("a[href$=jpg].hasbox, a[href$=png].hasbox, a[href$=gif].hasbox").each(function(){
-		
+
 		// recuperer l'URL sans les ../
 		var lien = $(this).attr("href");
 		var url = lien.replace(/^\.\.\//, '')
 
-		if ($(this).parents(".spip_documents").length == 0) $(this).attr("data-href", url);
-
+		if ($(this).parents(".spip_documents").length == 0) {
+			$(this).attr("data-href", url);
+		}
 
 		$.getJSON( "../index.php?page=centre_image_json&url="+url, 
 			{lien: lien}, 
@@ -46,15 +45,14 @@ function centre_image_afficher() {
 				var x = data.x * el.find("img:not(.croix_centre_image)").width();
 				var y = data.y * el.find("img:not(.croix_centre_image)").height();
 				console.log(url + " / " + x + " / " + y);
-				
 				centre_image_croix(el, x, y);
-			}			
+			}
 		);
-		
+
 	});
 }
 
 
-
-
+(function($){
 $(document).ready(centre_image_afficher);
+})(jQuery);
