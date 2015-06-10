38db2f2b6cc2e1a1aad5dbc3cf64285253c3c785
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-06-10 23:58:29 +0000

    De l'indentation uniquement. On est en 2015
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@90098 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- mutualisation/trunk/inc/dirliste.php
+++ mutualisation/trunk/inc/dirliste.php
@@ -5,41 +5,42 @@
 //	    où rep1, rep2,... le nom des sous-répertoires
 // Param dir : répertoire à explorer
 
-if (!defined("_ECRIRE_INC_VERSION"))
-	return;
+if (!defined('_ECRIRE_INC_VERSION')) {
+    return;
+}
 
 echo dirliste($_GET['dir']);
 
 function dirliste($path)
 {
-	$liste = "";
-	$size = 0;
+    $liste = '';
+    $size = 0;
 
-	// Trailing slash
-	if (substr($path, -1, 1) !== DIRECTORY_SEPARATOR) {
-		$path .= DIRECTORY_SEPARATOR;
-	}
+    // Trailing slash
+    if (substr($path, -1, 1) !== DIRECTORY_SEPARATOR) {
+        $path .= DIRECTORY_SEPARATOR;
+    }
 
-	if (!is_dir($path))
-		return -1;
+    if (!is_dir($path)) {
+        return -1;
+    }
 
-	$handle=opendir($path) ;
-	while (($file = readdir($handle)) !== false) {
-		// Skip pointers
-		if ($file == '.' || $file == '..')
-			continue;
+    $handle = opendir($path);
+    while (($file = readdir($handle)) !== false) {
+        // Skip pointers
+        if ($file == '.' || $file == '..') {
+            continue;
+        }
 
-		if (is_dir($path.$file)) {
-			$liste .= '##'.$file;
-		} elseif (is_file($path.$file)) {
-			$size += filesize($path.$file);
-		} else {
-			echo $file;
-		}
-	}
-	closedir($handle) ;
+        if (is_dir($path.$file)) {
+            $liste .= '##'.$file;
+        } elseif (is_file($path.$file)) {
+            $size += filesize($path.$file);
+        } else {
+            echo $file;
+        }
+    }
+    closedir($handle);
 
-	return round($size/1024/1024,2).$liste;
+    return round($size / 1024 / 1024, 2).$liste;
 }
-
-?>
--- mutualisation/trunk/mutualiser_gerer_img.php
+++ mutualisation/trunk/mutualiser_gerer_img.php
@@ -1,6 +1,8 @@
 <?php
 
-if (!defined("_ECRIRE_INC_VERSION")) return;
+if (!defined('_ECRIRE_INC_VERSION')) {
+    return;
+}
 
 /*
  * Creer les rewrite rules
@@ -12,13 +14,14 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
  *
  */
 // sur le repertoire $dir (sites/truc/local/)
-function mutualisation_verifier_htaccess($url, $dir) {
-	// lire le .htaccess existant
-	lire_fichier($url._ACCESS_FILE_NAME, $htaccess);
-	$source = $htaccess;
+function mutualisation_verifier_htaccess($url, $dir)
+{
+    // lire le .htaccess existant
+    lire_fichier($url._ACCESS_FILE_NAME, $htaccess);
+    $source = $htaccess;
 
-	// verifier notre bloc init
-	$bloc = '####
+    // verifier notre bloc init
+    $bloc = '####
 ## ce fichier .htaccess est gere par le plugin *mutualisation*
 ##
 ## ne le modifiez pas : en cas de besoin editez ce plugin,
@@ -29,50 +32,55 @@ RewriteEngine On
 RewriteBase /
 ';
 
-	if (strpos($htaccess, $bloc) === false)
-		$htaccess = $bloc;
+    if (strpos($htaccess, $bloc) === false) {
+        $htaccess = $bloc;
+    }
 
-	$host = $_SERVER['HTTP_HOST'];
-	$racine = dirname($_SERVER['SCRIPT_NAME']); // profondeur_url();
-	if ($racine == '/') $racine='';
-	$site = basename(dirname($dir));
-	$bloc = "
+    $host = $_SERVER['HTTP_HOST'];
+    $racine = dirname($_SERVER['SCRIPT_NAME']); // profondeur_url();
+    if ($racine == '/') {
+        $racine = '';
+    }
+    $site = basename(dirname($dir));
+    $bloc = "
 
 #### 'http://$host$racine/' = 'sites/$site/'
-RewriteCond %{HTTP_HOST}%{REQUEST_URI} ^".preg_quote("$host$racine/",',')."
+RewriteCond %{HTTP_HOST}%{REQUEST_URI} ^".preg_quote("$host$racine/", ',')."
 RewriteRule .* $racine/$dir\$0 [L]
 ";
 
-	if (strpos($htaccess, $bloc) === false)
-		$htaccess .= $bloc;
+    if (strpos($htaccess, $bloc) === false) {
+        $htaccess .= $bloc;
+    }
 
-	return ($htaccess === $source
-				OR ecrire_fichier($url._ACCESS_FILE_NAME, $htaccess));
+    return ($htaccess === $source
+                or ecrire_fichier($url._ACCESS_FILE_NAME, $htaccess));
 }
-
 
 /**
  * Transformer les liens
- * sites/nom_site/(IMG|local).* en (IMG|local).*
- * 
+ * sites/nom_site/(IMG|local).* en (IMG|local).*.
+ *
  * @param string $flux
  */
-function mutualisation_traiter_url_img_courtes($flux) {
-	// IMG
-	if (!defined('_URL_IMG'))
-		define('_URL_IMG', _DIR_RACINE . _NOM_PERMANENTS_ACCESSIBLES);
-	// local
-	if (!defined('_URL_VAR'))
-		define('_URL_VAR', _DIR_RACINE . _NOM_TEMPORAIRES_ACCESSIBLES);
+function mutualisation_traiter_url_img_courtes($flux)
+{
+    // IMG
+    if (!defined('_URL_IMG')) {
+        define('_URL_IMG', _DIR_RACINE._NOM_PERMANENTS_ACCESSIBLES);
+    }
+    // local
+    if (!defined('_URL_VAR')) {
+        define('_URL_VAR', _DIR_RACINE._NOM_TEMPORAIRES_ACCESSIBLES);
+    }
 
-	if (mutualisation_verifier_htaccess(_URL_VAR, _DIR_VAR)
-	AND mutualisation_verifier_htaccess(_URL_IMG, _DIR_IMG)) {
-		return str_replace(
-			array(_DIR_VAR, _DIR_IMG),
-			array(_URL_VAR, _URL_IMG),
-		$flux);
-	} else
-		return $flux;
+    if (mutualisation_verifier_htaccess(_URL_VAR, _DIR_VAR)
+    and mutualisation_verifier_htaccess(_URL_IMG, _DIR_IMG)) {
+        return str_replace(
+            array(_DIR_VAR, _DIR_IMG),
+            array(_URL_VAR, _URL_IMG),
+        $flux);
+    } else {
+        return $flux;
+    }
 }
-
-?>
