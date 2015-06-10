3201de1ad49656dafa195de04177109a789b737f
p@henix.be <p@henix.be@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-06-10 17:45:13 +0000

    Fonction de génération de PDF
    Contrairement à l'export, cette fonction renvoie le PDF dans une
    variable au lieu de l'envoyer au navigateur
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@90085 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ dompdf/trunk/inc/generer_pdf.php
@@ -0,0 +1,32 @@
+<?php
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+/**
+ * Fonction d'export PDF
+ *
+ * @param mixed $squelette Le squelette à transformer en PDF
+ * @param mixed $contexte L'éventuel contexte'
+ * @param string $filename Le nom du fichier
+ * @param string $paper Le format du papier (letter, legal, A4, voir $PAPER_SIZES)
+ * @param string $orientation (portrait ou landscape)
+ * @access public
+ */
+function inc_generer_pdf_dist($squelette, $contexte, $filename = 'sortie.pdf', $paper = 'A4', $orientation = 'portrait') {
+
+  // On inclut la configuration DOMPDF
+  include_spip('lib/dompdf/dompdf_config.inc');
+
+  // On charge DOMPDF
+  $dompdf = new DOMPDF();
+
+  // On récupère le html du squelette.
+  $html = recuperer_fond($squelette, $contexte);
+
+  // On lance DOMPDF pour crée le PDF et le renvoyer au navigateur.
+  $dompdf->load_html($html);
+  $dompdf->set_paper($paper, $orientation);
+  $dompdf->render();
+
+  return $dompdf->output();
+}
