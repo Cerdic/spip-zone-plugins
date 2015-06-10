caf998a58d99777e541b5145f675492884b3fff8
cyp@rouquin.me <cyp@rouquin.me@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-06-10 16:27:31 +0000

    s'affiche que si tes webmestre
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@90081 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- tota11y/tota11y_pipelines.php
+++ tota11y/tota11y_pipelines.php
@@ -12,15 +12,37 @@
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 /**
- * Ajout des scripts de tota11y dans le head des pages publiques
+ * Ajouter tota11y dans l'espace public seulement pour webmestre
  *
+ * @pipeline affichage_final
+ * @note
+ *     Le pipeline affichage_final est executé à chaque hit sur toute la page
  *
- * @pipeline jquery_plugins
- */
-function tota11y_jquery_plugins($tableau) {
-		$tableau[] = 'javascript/tota11y.min.js';
-	return $tableau;
+ * @param string $page
+ *     Contenu de la page à envoyer au navigateur
+ * @return string
+ *     Contenu de la page à envoyer au navigateur
+**/
+function tota11y_affichage_final($page) {
+	if(!function_exists('autoriser'))
+		include_spip('inc/autoriser');
+
+	if (autoriser('webmestre')) {
+		$pos_head = strpos($page, '</head>');
+		if ($pos_head === false) {
+			return $page;
+		}
+
+		$incJS = '<script type="text/javascript" src="'.find_in_path("javascript/tota11y.min.js").'"></script>';
+		
+		// js avant la premiere css, ou sinon avant la fin du head
+		$pos_link = strpos($page, '<link ');
+			if (!$pos_link) {
+				$pos_link = $pos_head;
+		}
+		$page = substr_replace($page, $incJS, $pos_link, 0);
+	}
+
+	return $page;
 }
-
-
 ?>
