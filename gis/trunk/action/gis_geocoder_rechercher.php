cb74442dc9debba1b78f71bf2618b49c8172c64d
marcimat@rezo.net <marcimat@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-06-12 18:52:00 +0000

    Mise à jour du phpdoc suite à r81355 et r81560
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@90130 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- gis/trunk/action/gis_geocoder_rechercher.php
+++ gis/trunk/action/gis_geocoder_rechercher.php
@@ -3,13 +3,13 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 
 include_spip("inc/distant");
 
-/*
+/**
  * Proxy vers le service Nomatim d'OpenStreetMap.
  *
  * Cette fonction permet de transmettre une requete auprès du service
- * de recherche d'adresse d'OpenStreetMap (Nomatim). Les arguments
- * spécifiques à SPIP sont supprimés (exec=, action= et mode=), le
- * reste est transmis tel quel à Nomatim.
+ * de recherche d'adresse d'OpenStreetMap (Nomatim).
+ * 
+ * Seuls les arguments spécifiques à Nomatim sont transmis.
  */
 function action_gis_geocoder_rechercher_dist() {
 	include_spip("inc/modifier");
@@ -18,8 +18,8 @@ function action_gis_geocoder_rechercher_dist() {
 	if(!$mode || !in_array($mode, array("search", "reverse")))
 		return;
 
-	/* On filtre les arguments à renvoyer à Nomatim (liste blanche) */	
-	$arguments = collecter_requests(array("json_callback", "format", "q", "limit", "addressdetails", "accept-language", "lat", "lon"),array());
+	/* On filtre les arguments à renvoyer à Nomatim (liste blanche) */
+	$arguments = collecter_requests(array("json_callback", "format", "q", "limit", "addressdetails", "accept-language", "lat", "lon"), array());
 
 	if(!empty($arguments)) {
 		header('Content-Type: application/json; charset=UTF-8');
