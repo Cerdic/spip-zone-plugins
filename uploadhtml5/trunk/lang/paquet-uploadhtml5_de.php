c2aca8c2a745ea920e59209b99cdb198b9abd30c
klaus@spip.de <klaus@spip.de@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-06-16 06:08:11 +0000

    version allemande
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@90285 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ uploadhtml5/trunk/lang/paquet-uploadhtml5_de.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// U
+	'uploadhtml5_description' => '',
+	'uploadhtml5_nom' => 'Uploadformular in HTML5',
+	'uploadhtml5_slogan' => 'Ein Uploadformular in HTML5',
+);
+
+?>
