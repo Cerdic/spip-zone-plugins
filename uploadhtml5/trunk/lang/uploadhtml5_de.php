525ef1076d8ede3730680da3fce07e5dcf909220
klaus@spip.de <klaus@spip.de@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-06-16 06:08:37 +0000

    version allemande
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@90286 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ uploadhtml5/trunk/lang/uploadhtml5_de.php
@@ -0,0 +1,38 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// U
+	'uploadhtml5_titre' => 'Uploadformular in HTML5',
+
+	// C
+	'cfg_titre_parametrages' => 'Parameter',
+    'label_charger_public' => "Öffentlich verfügbar",
+    'case_charger_public' => "Skripte des Formulars im öffentlichen Bereich laden",
+
+	// T
+	'titre_page_configurer_uploadhtml5' => 'HTML5-Uploadformular konfigurieren',
+
+
+    // M
+    'max_file_size' => 'Maximale Dateigrösse',
+    'explication_max_file_size' => "Maximale Dateigrössein MB. Es empfiehlt sich, die Einstellungen des Servers zu verwenden.",
+    'max_file' => 'Nombre maximum de fichiers',
+    'explication_max_file' => 'Höchste Zahl der gleichzeitig übertragbaren Dateien (0 = keine Obergrenze).',
+
+    // Message de la dropzone_prive
+    'drop_ici' => 'Legen Sie Ihre Dateien hier ab, oder klicken Sie in den Kasten',
+    'drop_no_support' => 'Ihr Browser unterstützt Ziehen und Ablegen nicht.',
+    'drop_fallbacktext' => "Bitte nutzen Sie dieses Formular",
+    'drop_fichier_invalide' => 'Ungültiger Dateityp',
+    'drop_fichier_trop_gros' => 'Ihre Datei ist zu gross',
+    'drop_annuler' => 'Upload abgebrochen',
+    'drop_annuler_confirm' => 'Diesen Upload abbrechen ?',
+    'drop_max_file' => 'Höchstzahl Dateien ist erreicht',
+
+    // Message pour la drop_zone des logo
+    'logo_drop_ici' => 'Legen Sie Ihr Logo hier ab oder klicken Sie in den Kasten'
+);
