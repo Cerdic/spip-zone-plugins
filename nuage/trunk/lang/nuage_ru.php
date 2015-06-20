ba07f1c9887a967c2d4a05b252e0a3d6a2cdc867
olly.olga@gmail.com <olly.olga@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-06-20 03:35:13 +0000

    [Salvatore] [source:_plugins_/nuage/trunk/lang/ nuage] Export depuis http://trad.spip.net de la langue ru
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@90391 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ nuage/trunk/lang/nuage_ru.php
@@ -0,0 +1,19 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/nuage?lang_cible=ru
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// N
+	'noisette_description' => 'Показать облако тегов (ключевых слов)',
+	'noisette_label_afficher_titre' => 'Показать заголовок:',
+	'noisette_label_niveau_titre' => 'Изменить уровень заголовка',
+	'noisette_nom_noisette' => 'Облако',
+	'noisette_titre_defaut' => 'Теги',
+	'noisette_titre_noisette' => 'Заголовок:'
+);
+
+?>
