a57288b50a8f81b2037d4a8b06f30fb7f0cb892a
olly.olga@gmail.com <olly.olga@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-06-20 03:34:21 +0000

    [Salvatore] [source:_plugins_/multilang/trunk/lang/ multilang] Export depuis http://trad.spip.net de la langue ru
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@90385 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ multilang/trunk/lang/multilang_ru.php
@@ -0,0 +1,38 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/multilang?lang_cible=ru
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cfg_activer' => 'Включить MultiLang на странице редактирования:',
+	'cfg_boite_multilang' => 'Конфигурация плагина Multilang',
+	'cfg_descr_multilang' => 'Multilang: Плагин для управления тегами multi (многоязычности)',
+	'cfg_effacer' => 'Повторно инициализировать',
+	'cfg_explication_crayons' => 'Требуется, чтобы многоязычность была активирована в общедоступной области сайта.',
+	'cfg_langues' => 'Используемые языки',
+	'cfg_lbl_crayons' => 'Использовать multilang в crayons',
+	'cfg_lbl_espace_public' => 'Использовать многоязычие в общедоступной области сайта',
+	'cfg_lbl_formstables' => 'формы и таблицы',
+	'cfg_lbl_siteconfig' => 'конфигурации сайта',
+	'cfg_titre_multilang' => 'Multilang',
+	'champ_numero' => 'Номер',
+	'configuration_multilang' => 'Multilang',
+
+	// E
+	'explication_langues_utilisees' => 'Выберите языки для использования в меню MultiLang. Выберите «Нет», чтобы использовать их все. ',
+
+	// L
+	'label_langues_utilisees' => 'Языки, используемые с multilang',
+	'lien_desactiver' => 'Все',
+	'lien_multi_title' => 'Редактировать многоязычные поля в «@lang@»',
+	'lien_multi_title_sans' => 'Версия «@lang@» не полна. Нажмите, чтобы изменить.',
+
+	// M
+	'message_champs_readonly' => 'Многоязычные поля только для чтения, выберите язык, чтобы их изменить'
+);
+
+?>
