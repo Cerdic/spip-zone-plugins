0376b56b58da2ba014f1c61770995ab9e50cd977
olly.olga@gmail.com <olly.olga@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-06-21 03:33:16 +0000

    [Salvatore] [source:_plugins_/porte_plume_extras/codes/trunk/lang/ pp_codes] Export depuis http://trad.spip.net de la langue ru
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@90411 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ porte_plume_extras/codes/trunk/lang/pp_codes_ru.php
@@ -0,0 +1,35 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/pp_codes?lang_cible=ru
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// L
+	'label_activer_barre_edition' => 'Панель для редактирования',
+	'label_activer_barre_forum' => 'Панель для форума',
+	'label_outils_actifs' => 'Активные инструменты',
+
+	// O
+	'outil_inserer_cadre_css' => 'Вставка кода форматирования CSS (cadre)',
+	'outil_inserer_cadre_html' => 'Вставка кода форматирования HTML (cadre)',
+	'outil_inserer_cadre_php' => 'Вставка кода форматирования PHP (cadre)',
+	'outil_inserer_cadre_spip' => 'Вставка кода форматирования SPIP (cadre)',
+	'outil_inserer_cadre_xml' => 'Вставка кода форматирования XML (cadre)',
+	'outil_inserer_kbd' => 'Вставьте ввод с клавиатуры (kdb)',
+	'outil_inserer_lien_trac' => 'Вставьте ссылку на перевод SPIP',
+	'outil_inserer_pre' => 'Вставка кода преформатирования (pre)',
+	'outil_inserer_samp' => 'Вставка исходного кода (samp)',
+	'outil_inserer_var' => 'Вставка переменной (var)',
+
+	// P
+	'pp_codes' => 'Компьютерные коды для Porte Plume',
+
+	// T
+	'titre_activer_extension_sur' => 'Включить на панели инструментов редактирования?',
+	'titre_configurer_pp_codes' => 'Настройте расширение компьютерных кодов для Porte Plume'
+);
+
+?>
