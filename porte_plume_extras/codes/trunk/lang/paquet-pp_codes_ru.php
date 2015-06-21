604ec6a686003974236102cab3ed2a1deb9c0571
olly.olga@gmail.com <olly.olga@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-06-21 03:33:22 +0000

    [Salvatore] [source:_plugins_/porte_plume_extras/codes/trunk/lang/ paquet-pp_codes] Export depuis http://trad.spip.net de la langue ru
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@90412 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ porte_plume_extras/codes/trunk/lang/paquet-pp_codes_ru.php
@@ -0,0 +1,17 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-pp_codes?lang_cible=ru
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// P
+	'pp_codes_description' => 'Управление кнопками для вставки компьютерного кода с панели инструментов редактирования текста.
+Добавляет ярлык <code>[->ecrire/inc_versions.php#trac]</code>.',
+	'pp_codes_nom' => 'Компьютерные коды',
+	'pp_codes_slogan' => 'Добавляет кнопки на панель инструментов редактирования текста для вставки компьютерных кодов'
+);
+
+?>
