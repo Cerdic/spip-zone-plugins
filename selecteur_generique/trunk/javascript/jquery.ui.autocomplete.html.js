d7eba4df0ad329cc2027b6d3fb165d0aaa40ee70
brunobergot@gmail.com <brunobergot@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-06-22 17:34:48 +0000

    Mise à jour de jquery.ui.autocomplete.html.js pour éviter une erreur js
    r89313 rétablissait l'insertion de jquery.ui.autocomplete.html.js mais ce script générait une erreur javascript, sa mise à jour fixe le bug
    
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@90442 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- selecteur_generique/trunk/javascript/jquery.ui.autocomplete.html.js
+++ selecteur_generique/trunk/javascript/jquery.ui.autocomplete.html.js
@@ -12,33 +12,27 @@
 	initSource = proto._initSource;
 
 	function filter( array, term ) {
-		var matcher = new RegExp( $.ui.autocomplete.escapeRegex(term), 'i' );
+		var matcher = new RegExp( $.ui.autocomplete.escapeRegex(term), "i" );
 		return $.grep( array, function(value) {
-			return matcher.test( $( '<div/>' ).html( value.label || value.value || value ).text() );
+			return matcher.test( $( "<div>" ).html( value.label || value.value || value ).text() );
 		});
 	}
 
 	$.extend( proto, {
 		_initSource: function() {
-			if ( this.options.html  &&  $.isArray(this.options.source) )
-			{
+			if ( this.options.html && $.isArray(this.options.source) ) {
 				this.source = function( request, response ) {
 					response( filter( this.options.source, request.term ) );
 				};
-			}
-			else
-			{
+			} else {
 				initSource.call( this );
 			}
 		},
 
 		_renderItem: function( ul, item) {
-			return $( '<li><a/></li>' )
+			return $( "<li></li>" )
 			.data( "item.autocomplete", item )
-			.find( '<a/>' )
-			.addClass( item.className )
-			[ this.options.html ? "html" : "text" ]( item.label )
-			.end()
+			.append( $( "<a></a>" )[ this.options.html ? "html" : "text" ]( item.label ) )
 			.appendTo( ul );
 		}
 	});
