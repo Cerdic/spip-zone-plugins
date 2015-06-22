360dd52e3d096ef5233af0a11fcf37eaeb605a32
klaus@spip.de <klaus@spip.de@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-06-22 03:33:23 +0000

    [Salvatore] [source:_plugins_/oembed/lang/ oembed] Export depuis http://trad.spip.net de la langue de
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@90421 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ oembed/lang/oembed_de.php
@@ -0,0 +1,45 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/oembed?lang_cible=de
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'aucun_provider' => 'KeinProvider',
+
+	// E
+	'erreur_ajout_provider' => 'Fehler beim Hinzufügen des Providers.',
+	'erreur_scheme_doublon' => 'Dieses Schema ist bereits Teil der Liste.',
+	'explication_scheme' => 'verwenden sie * als Wildcard',
+	'explication_upload_url' => 'Geben sie einfach den URL der Seite an, welche das Dokument mit den <a href="http://contrib.spip.net/4595"> meistverwendeten Webseiten</a> enthält.',
+
+	// I
+	'info_1_provider' => '1 Provider',
+	'info_nb_providers' => '@nb@ Provider',
+
+	// L
+	'label_detecter_lien' => 'Provider automatisch erkennen (und die Whitelist ignorieren) ?',
+	'label_embed_auto' => 'In den Text eingefügte URLs automatisch umwandeln ?',
+	'label_endpoint' => 'Endpunkt',
+	'label_maxheight' => 'Standard-Maximalhöhe (px)',
+	'label_maxwidth' => 'Standard-Maximalbreite (px)',
+	'label_scheme' => 'Schema',
+
+	// O
+	'ok_ajout_provider' => 'Provider wurde hinzugefügt',
+
+	// S
+	'supprimer_provider' => 'Löschen',
+
+	// T
+	'titre_ajouter_provider' => 'Provider hinzufügen',
+	'titre_configuration_avancee_providers' => 'Detailkonfiguration der oEmbed Provider',
+	'titre_configurer_oembed' => 'oEmbed konfigurieren',
+	'titre_configurer_providers' => 'oEmbed Provider',
+	'titre_oembed' => 'oEmbed'
+);
+
+?>
