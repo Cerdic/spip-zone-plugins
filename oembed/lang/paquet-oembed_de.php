9d24ad9715f216092aef135942ccedee9ac7d8b9
klaus@spip.de <klaus@spip.de@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-06-22 03:33:29 +0000

    [Salvatore] [source:_plugins_/oembed/lang/ paquet-oembed] Export depuis http://trad.spip.net de la langue de
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@90422 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ oembed/lang/paquet-oembed_de.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-oembed?lang_cible=de
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// O
+	'oembed_description' => 'oEmbed Client/Provider für SPIP.',
+	'oembed_slogan' => 'oEmbed Client/Provider für SPIP'
+);
+
+?>
