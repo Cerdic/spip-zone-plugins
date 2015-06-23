80b18e91ee577d3cc46fb981ffd18527d067b173
cyp@rouquin.me <cyp@rouquin.me@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-06-23 12:58:55 +0000

    Un peu de traduction en Espagnol
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@90459 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ cookiebar/trunk/lang/cookiebar_es.php
@@ -0,0 +1,40 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+ 
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cookiebar_titre' => 'cookiebar',
+
+	// T
+	'titre_page_configurer_cookiebar' => 'Configuración cookiebar',
+  
+  // M
+  'message_message' => 'Si continúa navegando acepta el uso de “cookies” para elaborar estadísticas de visitas',
+  'message_accepttext' => 'Acepto',
+  'message_declinetext' => 'Deshabilitar las “cookies”', 
+  'message_policytext' => 'Política de confidencialidad',
+  
+    // L
+  'label_accept_button' => 'Mostrar el botón Aceptar',
+  'label_accept_button_true' => 'si',
+  'label_accept_button_false' => 'no', 
+  'label_decline_button' => 'Mostrar el botón Rechazar',
+  'label_decline_button_true' => 'si',
+  'label_decline_button_false' => 'no',  
+  'label_policy_button' => 'Mostrar el botón Política de Privacidad',
+  'label_policy_button_true' => 'si',
+  'label_policy_button_false' => 'no',
+  'label_policy_url' => 'URL del a Política de confidencialidad',
+  'label_position' => 'posición de la barra',
+  'label_position_top'   => 'Volver arriba',
+  'label_position_fixed'   => 'Volver arriba (fixed)',
+  'label_position_fixed-bottom'   => 'Pie de página (fixed)',
+  
+  
+  
+);
+
+?>
