f0705007604b948020f66fd2b5faef12c81fa141
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-06-25 15:53:57 +0000

    un bouton 'vider le panier' pour permettre de vider le panier. Il peut etre masque en css si on en veut pas. Attention, cela oblige a rajouter une premiere occurence du bouton recalculer en offscreen pour que le return sur une quantite recalcule bien le panier et le vide pas. Il faut avoir une class css offscreen ou over definies, c'est le plus souvent le cas, notamment dans la dist
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@90519 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- paniers/trunk/formulaires/panier.php
+++ paniers/trunk/formulaires/panier.php
@@ -20,20 +20,22 @@ function formulaires_panier_charger($id_panier=0){
 
 function formulaires_panier_verifier($id_panier=0){
 	$erreurs = array();
-	
-	$quantites = _request('quantites');
-	
-	if (is_array($quantites)){
-		foreach ($quantites as $objet => $objets_de_ce_type){
-			foreach ($objets_de_ce_type as $id_objet => $quantite){
-				if (!is_numeric($quantite) or $quantite!=intval($quantite) or (is_numeric($quantite) and $quantite<0)){
-					$erreurs['message_erreur'] = _T('paniers:panier_erreur_quantites');
-					$erreurs['quantites'][$objet][$id_objet] = 'erreur';
+
+	if (!_request('vider')){
+		$quantites = _request('quantites');
+
+		if (is_array($quantites)){
+			foreach ($quantites as $objet => $objets_de_ce_type){
+				foreach ($objets_de_ce_type as $id_objet => $quantite){
+					if (!is_numeric($quantite) or $quantite!=intval($quantite) or (is_numeric($quantite) and $quantite<0)){
+						$erreurs['message_erreur'] = _T('paniers:panier_erreur_quantites');
+						$erreurs['quantites'][$objet][$id_objet] = 'erreur';
+					}
 				}
 			}
 		}
 	}
-	
+
 	return $erreurs;
 }
 
@@ -43,43 +45,48 @@ function formulaires_panier_traiter($id_panier=0){
 	
 	// On commence par chercher le panier du visiteur actuel s'il n'est pas donné
 	if (!$id_panier) $id_panier = session_get('id_panier');
-	
-	$quantites = _request('quantites');
-	$ok = 0;
-	
-	if (is_array($quantites))
-		foreach($quantites as $objet => $objets_de_ce_type)
-			foreach($objets_de_ce_type as $id_objet => $quantite){
-				$quantite = intval($quantite);
-				// Si la quantite est 0, on supprime du panier
-				if (!$quantite)
-					$ok += sql_delete(
-						'spip_paniers_liens',
-						'id_panier = '.intval($id_panier).' and objet = '.sql_quote($objet).' and id_objet = '.intval($id_objet)
-					);
-				// Sinon on met à jour
-				else{
-					if ($quantite!=sql_getfetsel("quantite","spip_paniers_liens",'id_panier = '.intval($id_panier).' and objet = '.sql_quote($objet).' and id_objet = '.intval($id_objet))){
-						$ok += sql_updateq(
+
+	if (_request('vider')){
+		$supprimer_panier = charger_fonction("supprimer_panier","action");
+		$supprimer_panier($id_panier);
+	}
+	else {
+		$quantites = _request('quantites');
+		$ok = 0;
+
+		if (is_array($quantites))
+			foreach($quantites as $objet => $objets_de_ce_type)
+				foreach($objets_de_ce_type as $id_objet => $quantite){
+					$quantite = intval($quantite);
+					// Si la quantite est 0, on supprime du panier
+					if (!$quantite)
+						$ok += sql_delete(
 							'spip_paniers_liens',
-							array('quantite' => $quantite),
 							'id_panier = '.intval($id_panier).' and objet = '.sql_quote($objet).' and id_objet = '.intval($id_objet)
 						);
+					// Sinon on met à jour
+					else{
+						if ($quantite!=sql_getfetsel("quantite","spip_paniers_liens",'id_panier = '.intval($id_panier).' and objet = '.sql_quote($objet).' and id_objet = '.intval($id_objet))){
+							$ok += sql_updateq(
+								'spip_paniers_liens',
+								array('quantite' => $quantite),
+								'id_panier = '.intval($id_panier).' and objet = '.sql_quote($objet).' and id_objet = '.intval($id_objet)
+							);
+						}
 					}
 				}
-			}
-	
-	// Mais dans tous les cas on met la date du panier à jour
-	sql_updateq(
-		'spip_paniers',
-		array('date'=>'NOW()'),
-		'id_panier = '.$id_panier
-	);
-	
-	if ($ok){
-		$retours['message_ok'] = _T('paniers:panier_quantite_ok');
+		// Mais dans tous les cas on met la date du panier à jour
+		sql_updateq(
+			'spip_paniers',
+			array('date'=>'NOW()'),
+			'id_panier = '.$id_panier
+		);
+		if ($ok){
+			$retours['message_ok'] = _T('paniers:panier_quantite_ok');
+		}
+
 	}
-	
+
 	return $retours;
 }
 
