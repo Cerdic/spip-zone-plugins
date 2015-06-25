15b742ba3b6b9ced1ef4e02142324b84aa8dc853
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-06-25 15:51:46 +0000

    quand on supprime le panier, le supprimer de la session courante si il y etait, pour retrouver une session vide
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@90518 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- paniers/trunk/action/supprimer_panier.php
+++ paniers/trunk/action/supprimer_panier.php
@@ -18,7 +18,14 @@ function action_supprimer_panier_dist($arg=null) {
 	if ($id_panier = intval($arg)) {
 		sql_delete('spip_paniers', 'id_panier=' . $id_panier);
 		sql_delete('spip_paniers_liens', 'id_panier=' . $id_panier);
+
+		// supprimer le panier de la session si c'est le meme
+		// ca permet potentiellement de retrouver une session vide
+		include_spip('inc/session');
+		if ($id_panier==session_get('id_panier')){
+			session_set('id_panier');
+		}
 	}
 }
 
-?>
+
