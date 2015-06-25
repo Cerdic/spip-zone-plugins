6789f92f22a40fafcf71fcdd3cddae233dcf4361
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-06-25 16:54:02 +0000

    echappements dans la requete, utiliser la fonction paniers_supprimer_panier_en_cours()
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@90528 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- paniers/trunk/action/supprimer_panier.php
+++ paniers/trunk/action/supprimer_panier.php
@@ -16,14 +16,15 @@ function action_supprimer_panier_dist($arg=null) {
 	
 	// suppression
 	if ($id_panier = intval($arg)) {
-		sql_delete('spip_paniers', 'id_panier=' . $id_panier);
-		sql_delete('spip_paniers_liens', 'id_panier=' . $id_panier);
+		sql_delete('spip_paniers', 'id_panier=' . intval($id_panier));
+		sql_delete('spip_paniers_liens', 'id_panier=' . intval($id_panier));
 
 		// supprimer le panier de la session si c'est le meme
-		// ca permet potentiellement de retrouver une session vide
+		// ca permet potentiellement de retrouver une session vide (perf issue)
 		include_spip('inc/session');
 		if ($id_panier==session_get('id_panier')){
-			session_set('id_panier');
+			include_spip('inc/paniers');
+			paniers_supprimer_panier_en_cours();
 		}
 	}
 }
