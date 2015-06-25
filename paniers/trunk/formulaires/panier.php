e5e93041ab47840c84fa321536996358f8ed752c
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-06-25 15:55:24 +0000

    NOW() n'est pas utilisable avec sql_updateq, il aurait fallu utiliser sql_update. Cela dit on prefere utiliser la date PHP, par convention c'est celle qui sert de reference partout. + intval sur id_panier
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@90520 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- paniers/trunk/formulaires/panier.php
+++ paniers/trunk/formulaires/panier.php
@@ -78,8 +78,8 @@ function formulaires_panier_traiter($id_panier=0){
 		// Mais dans tous les cas on met la date du panier à jour
 		sql_updateq(
 			'spip_paniers',
-			array('date'=>'NOW()'),
-			'id_panier = '.$id_panier
+			array('date'=>date('Y-m-d H:i:s')),
+			'id_panier = '.intval($id_panier)
 		);
 		if ($ok){
 			$retours['message_ok'] = _T('paniers:panier_quantite_ok');
