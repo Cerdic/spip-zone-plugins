638219f064df821ebad6ecf206b5a810d2b74e6a
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-06-25 16:55:38 +0000

    perf issue : on regarde explicitement le cookie spip_panier et id_panier et id_auteur en session pour decider si on appelle la fonction paniers_id_panier_encours(). Un visiteur anonyme, un bot ou autre n'a ainsi aucun risque de generer une requete SQL. Par contre on appelle la fonction aussi dans l'espace prive, ou on peut vouloir faire des operations sur le panier en cours
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@90529 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- paniers/trunk/paniers_options.php
+++ paniers/trunk/paniers_options.php
@@ -3,23 +3,15 @@
 // Sécurité
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
-// À chaque hit en partie publique, on va chercher le panier du visiteur actuel, et sinon le créer
-if (_DIR_RESTREINT) {
-	// Si c'est pas un bot :
-	if (!_IS_BOT){
-		include_spip('inc/paniers');
-		include_spip('inc/session');
-	
-		// Si on a bien un panier
-		if (_request('action') != 'cron' and $id_panier = paniers_id_panier_encours()){
-			// On met le panier dans la session
-			session_set('id_panier', $id_panier);
-		}
-		// Sinon on s'assure que la session ne contient pas un ancien numéro
-		else{
-			session_set('id_panier');
-		}
-	}
+// À chaque hit en partie publique, on va chercher le panier du visiteur actuel si il en a un
+// on ne fait rien sur les hits visiteurs anonymes, bots, cron, etc...
+if (isset($_COOKIE[$GLOBALS['cookie_prefix'].'_panier'])
+  OR (isset($GLOBALS['visiteur_session']['id_panier']) AND $GLOBALS['visiteur_session']['id_panier'])
+  OR (isset($GLOBALS['visiteur_session']['id_auteur']) AND $GLOBALS['visiteur_session']['id_auteur'])){
+
+	// verifier/mettre a jour l'existence d'un panier en cours
+	include_spip('inc/paniers');
+	$id_panier = paniers_id_panier_encours();
+
 }
 
-?>
