3d880f97de9a73ae3b7b2dd7bc9a7c4f1890c3b3
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-06-29 03:32:55 +0000

    [Salvatore] [source:_plugins_/uploadhtml5/trunk/lang/ uploadhtml5] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@90579 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- uploadhtml5/trunk/lang/uploadhtml5_de.php
+++ uploadhtml5/trunk/lang/uploadhtml5_de.php
@@ -1,38 +1,43 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/uploadhtml5?lang_cible=de
+// ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-	// U
-	'uploadhtml5_titre' => 'Uploadformular in HTML5',
-
 	// C
+	'case_charger_public' => 'Skripte des Formulars im öffentlichen Bereich laden',
 	'cfg_titre_parametrages' => 'Parameter',
-    'label_charger_public' => "Öffentlich verfügbar",
-    'case_charger_public' => "Skripte des Formulars im öffentlichen Bereich laden",
+
+	// D
+	'drop_annuler' => 'Upload abgebrochen',
+	'drop_annuler_confirm' => 'Diesen Upload abbrechen ?',
+	'drop_fallbacktext' => 'Bitte nutzen Sie dieses Formular',
+	'drop_fichier_invalide' => 'Ungültiger Dateityp',
+	'drop_fichier_trop_gros' => 'Ihre Datei ist zu gross',
+	'drop_ici' => 'Legen Sie Ihre Dateien hier ab, oder klicken Sie in den Kasten',
+	'drop_max_file' => 'Höchstzahl Dateien ist erreicht',
+	'drop_no_support' => 'Ihr Browser unterstützt Ziehen und Ablegen nicht.',
+
+	// E
+	'explication_max_file' => 'Höchste Zahl der gleichzeitig übertragbaren Dateien (0 = keine Obergrenze).',
+	'explication_max_file_size' => 'Maximale Dateigrössein MB. Es empfiehlt sich, die Einstellungen des Servers zu verwenden.',
+
+	// L
+	'label_charger_public' => 'Öffentlich verfügbar',
+	'logo_drop_ici' => 'Legen Sie Ihr Logo hier ab oder klicken Sie in den Kasten',
+
+	// M
+	'max_file' => 'Nombre maximum de fichiers',
+	'max_file_size' => 'Maximale Dateigrösse',
 
 	// T
 	'titre_page_configurer_uploadhtml5' => 'HTML5-Uploadformular konfigurieren',
 
-
-    // M
-    'max_file_size' => 'Maximale Dateigrösse',
-    'explication_max_file_size' => "Maximale Dateigrössein MB. Es empfiehlt sich, die Einstellungen des Servers zu verwenden.",
-    'max_file' => 'Nombre maximum de fichiers',
-    'explication_max_file' => 'Höchste Zahl der gleichzeitig übertragbaren Dateien (0 = keine Obergrenze).',
-
-    // Message de la dropzone_prive
-    'drop_ici' => 'Legen Sie Ihre Dateien hier ab, oder klicken Sie in den Kasten',
-    'drop_no_support' => 'Ihr Browser unterstützt Ziehen und Ablegen nicht.',
-    'drop_fallbacktext' => "Bitte nutzen Sie dieses Formular",
-    'drop_fichier_invalide' => 'Ungültiger Dateityp',
-    'drop_fichier_trop_gros' => 'Ihre Datei ist zu gross',
-    'drop_annuler' => 'Upload abgebrochen',
-    'drop_annuler_confirm' => 'Diesen Upload abbrechen ?',
-    'drop_max_file' => 'Höchstzahl Dateien ist erreicht',
-
-    // Message pour la drop_zone des logo
-    'logo_drop_ici' => 'Legen Sie Ihr Logo hier ab oder klicken Sie in den Kasten'
+	// U
+	'uploadhtml5_titre' => 'Uploadformular in HTML5'
 );
+
+?>
--- uploadhtml5/trunk/lang/uploadhtml5_fr.php
+++ uploadhtml5/trunk/lang/uploadhtml5_fr.php
@@ -1,40 +1,43 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/uploadhtml5/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
+	// C
+	'case_charger_public' => 'Charger les scripts sur dans l’espace public',
+	'case_remplacer_editer_logo' => 'Remplacer le formulaire d’upload de logo de SPIP',
+	'cfg_titre_parametrages' => 'Paramétrages',
 
-    'uploadhtml5_titre' => 'Formulaire upload html5',
+	// D
+	'drop_annuler' => 'Envoie annulé',
+	'drop_annuler_confirm' => 'Annuler cet envoi ?',
+	'drop_fallbacktext' => 'Merci d’utiliser ce formulaire à la place',
+	'drop_fichier_invalide' => 'Type de fichier invalide',
+	'drop_fichier_trop_gros' => 'Votre fichier est trop volumineux',
+	'drop_ici' => 'Déposez vos fichiers ici ou cliquez sur ce cadre',
+	'drop_max_file' => 'Nombre maximum de fichiers atteint',
+	'drop_no_support' => 'Votre navigateur ne supporte pas le Glisser-Déposer',
 
+	// E
+	'explication_max_file' => 'Nombre de maximum de fichiers qui peuvent être uploadés simultanément (0 pour ne pas limiter).',
+	'explication_max_file_size' => 'Taille maximum des fichiers (en MB). Il est conseillé d’utiliser la valeur du serveur.',
 
-    'cfg_titre_parametrages' => 'Paramétrages',
-    'label_charger_public' => "Script dans l'espace public",
-    'case_charger_public' => "Charger les scripts sur dans l'espace public",
+	// L
+	'label_charger_public' => 'Script dans l’espace public',
+	'label_remplacer_editer_logo' => 'Remplacer logo',
+	'logo_drop_ici' => 'Déposez votre logo ici ou cliquez sur ce cadre',
 
-    'titre_page_configurer_uploadhtml5' => 'Configuration du formulaire d\'upload html5',
+	// M
+	'max_file' => 'Nombre maximum de fichiers',
+	'max_file_size' => 'Taille maximum des fichiers',
 
-    'label_remplacer_editer_logo' => "Remplacer logo",
-    'case_remplacer_editer_logo' => "Remplacer le formulaire d'upload de logo de SPIP",
+	// T
+	'titre_page_configurer_uploadhtml5' => 'Configuration du formulaire d’upload html5',
 
-    // M
-    'max_file_size' => 'Taille maximum des fichiers',
-    'explication_max_file_size' => "Taille maximum des fichiers (en MB). Il est conseillé d'utiliser la valeur du serveur.",
-
-    'max_file' => 'Nombre maximum de fichiers',
-    'explication_max_file' => 'Nombre de maximum de fichiers qui peuvent être uploadés simultanément (0 pour ne pas limiter).',
-
-    // Message de la dropzone_prive
-    'drop_ici' => 'Déposez vos fichiers ici ou cliquez sur ce cadre',
-    'drop_no_support' => 'Votre navigateur ne supporte pas le Glisser-Déposer',
-    'drop_fallbacktext' => "Merci d'utiliser ce formulaire à la place",
-    'drop_fichier_invalide' => 'Type de fichier invalide',
-    'drop_fichier_trop_gros' => 'Votre fichier est trop volumineux',
-    'drop_annuler' => 'Envoie annulé',
-    'drop_annuler_confirm' => 'Annuler cet envoi ?',
-    'drop_max_file' => 'Nombre maximum de fichiers atteint',
-
-    // Message pour la drop_zone des logo
-    'logo_drop_ici' => 'Déposez votre logo ici ou cliquez sur ce cadre'
+	// U
+	'uploadhtml5_titre' => 'Formulaire upload html5'
 );
+
+?>
