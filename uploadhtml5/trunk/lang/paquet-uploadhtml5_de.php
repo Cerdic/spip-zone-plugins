a8ec93c78443ab0725713cbc5f3b41e4b7626a5f
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-06-29 03:32:49 +0000

    [Salvatore] [source:_plugins_/uploadhtml5/trunk/lang/ paquet-uploadhtml5] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@90578 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- uploadhtml5/trunk/lang/paquet-uploadhtml5_de.php
+++ uploadhtml5/trunk/lang/paquet-uploadhtml5_de.php
@@ -1,5 +1,7 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-uploadhtml5?lang_cible=de
+// ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
@@ -8,7 +10,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// U
 	'uploadhtml5_description' => '',
 	'uploadhtml5_nom' => 'Uploadformular in HTML5',
-	'uploadhtml5_slogan' => 'Ein Uploadformular in HTML5',
+	'uploadhtml5_slogan' => 'Ein Uploadformular in HTML5'
 );
 
 ?>
