d4fe36aeb5b9e13f9f0cabb5ba2a90cdc6709679
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-06-30 19:43:51 +0000

    Ne pas recopier systematiquement le panier en cours sur la commande via le pipeline, car on peut vouloir creer une commande indepandante du panier (achat immediat d'un produit) Du coup on utilise une fonction dediee qu'on appelle explicitement lorsqu'on cree la commande d'apres le panier
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@90621 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- commandes_paniers/trunk/action/commandes_paniers.php
+++ commandes_paniers/trunk/action/commandes_paniers.php
@@ -63,7 +63,11 @@ function action_commandes_paniers_dist($arg=null){
 	// cas du bouton "Achat immediat de ce produit" qui va direct au paiement
 	// ne doit pas remplir la commande avec le panier en cours
 	include_spip('inc/commandes');
-	$id_commande = creer_commande_encours();
+
+	if ($id_commande = creer_commande_encours()){
+		panier2commande_remplir_commande($id_commande,$id_panier);
+	}
+
 
 	// Supprimer le panier ?
 	if (!$keep){
@@ -75,4 +79,60 @@ function action_commandes_paniers_dist($arg=null){
 	if (is_null(_request('redirect'))) {
 		$GLOBALS['redirect'] = generer_url_public('commande','id_commande='.$id_commande,true);
 	}
+}
+
+
+
+/**
+ * Remplir une commande d'apres un panier
+ *
+ * @param  int $id_commande
+ * @param  int $id_panier
+ */
+function panier2commande_remplir_commande($id_commande,$id_panier){
+
+	include_spip('action/editer_objet');
+	include_spip('inc/filtres');
+	include_spip('inc/paniers');
+
+	// noter le panier source dans le champ source de la commande
+	objet_modifier('commande',$id_commande,array('source'=>"panier#$id_panier"));
+
+	// recopier le contenu du panier dans la commande
+	// On récupère le contenu du panier
+	$panier = sql_allfetsel(
+		'*',
+		'spip_paniers_liens',
+		'id_panier = '.intval($id_panier)
+	);
+
+	// Pour chaque élément du panier, on va remplir la commande
+	if ($panier and is_array($panier)){
+		include_spip('spip_bonux_fonctions');
+		$fonction_prix = charger_fonction('prix', 'inc/');
+		$fonction_prix_ht = charger_fonction('ht', 'inc/prix');
+		foreach($panier as $emplette){
+			$prix_ht = $fonction_prix_ht($emplette['objet'], $emplette['id_objet'],3);
+			$prix = $fonction_prix($emplette['objet'], $emplette['id_objet'],3);
+			if($prix_ht > 0)
+				$taxe = round(($prix - $prix_ht) / $prix_ht, 3);
+			else
+				$taxe = 0;
+			// création du détail de la commande
+			if ($id_commandes_detail = objet_inserer('commandes_detail')) {
+				$set = array(
+					'id_commande' => $id_commande,
+					'objet' => $emplette['objet'],
+					'id_objet' => $emplette['id_objet'],
+					'descriptif' => generer_info_entite($emplette['id_objet'], $emplette['objet'], 'titre', '*'),
+					'quantite' => $emplette['quantite'],
+					'prix_unitaire_ht' => $prix_ht,
+					'taxe' => $taxe,
+					'statut' => 'attente'
+				);
+				objet_modifier('commandes_detail', $id_commandes_detail, $set);
+			}
+		}
+	}
+
 }
