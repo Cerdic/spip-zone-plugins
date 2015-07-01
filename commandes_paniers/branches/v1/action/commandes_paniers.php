b082818ecd0e52506b89d0c48ea8a1690312b004
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-07-01 07:45:18 +0000

    Une branche v1 pour ne pas impacter la prod avec le trunk (touti)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@90632 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ commandes_paniers/branches/v1/action/commandes_paniers.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Fonction du plugin Commandes de paniers
+ *
+ * @plugin     Commandes de Paniers
+ * @copyright  2014
+ * @author     Les Développements Durables
+ * @licence    GNU/GPL
+ * @package    SPIP\Panier2commande\Action
+ */
+
+// Sécurité
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+include_spip('inc/base');
+
+/**
+ * Créer une commande et remplir ses détails d'après le panier en cours du visiteur (ou d'après un panier donné).
+ *
+ * Pour créer une commande d'après le panier présent dans la session: 
+ *   #URL_ACTION{commandes_paniers,'',#SELF}
+ * Pour créer une commande d'après un panier précis :
+ *   #URL_ACTION{commandes_paniers,#ID_PANIER,#SELF}
+ * Sans redirection explicite, la fonction redirige vers la page de la commande 
+ *
+ * @param string $arg
+ *     identifiant du panier
+ * @return void
+**/
+function action_commandes_paniers_dist($arg=null){
+
+	// Si $arg n'est pas donné directement, le récupérer via _POST ou _GET
+	if (is_null($arg)) {
+		$securiser_action = charger_fonction('securiser_action', 'inc');
+		$arg = $securiser_action();
+	}
+
+	// Sans paramètre, récupérer $id_panier dans la session du visiteur actuel
+	if (is_null($id_panier=$arg)) {
+		include_spip('inc/session');
+		$id_panier = session_get('id_panier');
+	}
+
+	// Si aucun panier ne pas agir
+	if (is_null($id_panier))
+		return;
+
+	// création d'une commande "en cours"
+	// Ses détails sont ensuite remplis d'après le panier en session
+	// via la pipeline post_insertion
+	include_spip('inc/commandes');
+	$id_commande = creer_commande_encours();
+
+	// Plus besoin du panier
+	$supprimer_panier = charger_fonction('supprimer_panier_encours', 'action/');
+	$supprimer_panier();
+
+	// Sans redirection donnée, on redirige vers la page de la commande créée
+	if (is_null(_request('redirect'))) {   
+		$redirect = generer_url_public('commande','id_commande='.$id_commande,true);
+		include_spip('inc/headers');
+		redirige_par_entete($redirect);
+	}
+}
+?>
