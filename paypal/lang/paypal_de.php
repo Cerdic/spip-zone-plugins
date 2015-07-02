7afc94f15ebbc40824ddc1ba8506376f2ba65021
willmann@netzsachen.de <willmann@netzsachen.de@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-07-02 03:33:31 +0000

    [Salvatore] [source:_plugins_/paypal/lang/ paypal] Export depuis http://trad.spip.net de la langue de
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@90662 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ paypal/lang/paypal_de.php
@@ -0,0 +1,47 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paypal?lang_cible=de
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'api_prod' => 'Produktions API',
+	'api_test' => 'Test API',
+
+	// C
+	'configuration_paypal' => 'Paypal',
+
+	// D
+	'description_paypal' => 'Den Onlinebezahldienst Paypal einrichten',
+
+	// E
+	'explication_account' => 'Entspricht der E-Mail-Adresse des Verkäuferkontos',
+	'explication_environnement' => 'Sie können hier angeben ob sie die Paypal Testwebiste (Sandbox) oder die für gültige Transaktionen nutzen wollen.
+Welche Umgebung wollen Sie nutzen?',
+	'explication_identifiant_vendeur' => 'Entspricht der E-Mail-Adresse des Verkäuferkontos',
+	'explication_soumission' => 'An welche Paypal Seite wird das Formular gesendet?',
+	'explication_tax' => 'Eventuell der anzusetzende Mehrwertsteuersatz (in Deutschand 19.00) ',
+
+	// I
+	'info_bouton_paypal' => 'Zahlen Sie mit Paypal: es ist schnell, kostenlos und sicher!',
+
+	// L
+	'label_account' => 'Konto (E-Mail)',
+	'label_currency_code' => 'Währung',
+	'label_currency_code_eur' => 'Euro (€)',
+	'label_currency_code_usd' => 'Dollar ($)',
+	'label_environnement' => 'Umgebung',
+	'label_environnement_prod' => 'Produktionsumgebung',
+	'label_environnement_test' => 'Testumgebung',
+	'label_identifiant_vendeur' => 'Verkäufer ID',
+	'label_password' => 'API Passwort (password)',
+	'label_signature' => 'Unterschrift ',
+	'label_soumission' => 'Ausführung des Formulars',
+	'label_tax' => 'Steuern',
+	'label_username' => 'API Benutzername '
+);
+
+?>
