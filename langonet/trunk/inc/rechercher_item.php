ccb91d21546c5a195c21f48bb4dbf80032db42db
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-07-05 09:03:04 +0000

    Propagation des modifications de la recherche de texte à la recherche de raccourci.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@90713 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- langonet/trunk/inc/rechercher_item.php
+++ langonet/trunk/inc/rechercher_item.php
@@ -3,10 +3,10 @@
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 /**
- * Vérification de l'utilisation des items de langue
+ * Recherche d'une chaine représentant tout ou partie d'un item de langue de SPIP et des plugins disponibles.
  *
  * @param string $pattern
- * 		le raccourci ou une partie de celui-ci à rechercher. Ce texte est un index de tableau associatif
+ * 		le raccourci ou une partie de celui-ci à rechercher.
  * @param string $correspondance
  * 		type de correspondance : egal, commence, ou contient
  * @param array  $modules
@@ -24,55 +24,85 @@ function inc_rechercher_item($pattern, $correspondance, $modules) {
 	// On construit la liste de tous les items definis
 	// dans tous les fichiers de langue presents sur le site.
 	$langue = 'fr';
-	$tous_trad = array();
-	$tous_lang = array();
+	$traductions = array();
+	$fichiers_langue = array();
 	if ($modules) {
-		foreach ($modules as $_valeur) {
-			// L'index 0 correspond au module, l'index 1 au fichier
-			$valeurs = explode(':', $_valeur);
-			$var_source = 'i18n_' . $valeurs[0] . '_' . $langue;
-			if (empty($GLOBALS[$var_source])) {
-				$GLOBALS['idx_lang'] = $var_source;
-				$fichier_lang = $valeurs[1] . $valeurs[0] . '_' . $langue . '.php';
-				include($fichier_lang);
+		// On sauvegarde l'index de langue global si il existe car on va le modifier pendant le traitement.
+		$idx_lang_backup = '';
+		if (isset($GLOBALS['idx_lang'])) {
+			$idx_lang_backup = $GLOBALS['idx_lang'];
+		}
+
+		foreach ($modules as $_module) {
+			// L'index 0 : nom du module de langue
+			// l'index 1 : nom du répertoire contenant lang/
+			// l'index 2 : chemin du fichier de langue
+			list($nom_module, $plugin, $chemin) = explode(':', $_module);
+			$idx_lang = 'i18n_' . $nom_module . '_' . $langue;
+
+			$backup_trad = array();
+			// Si les traductions correspondant à l'index de langue sont déjà chargées on les sauvegarde pour
+			// les restaurer en fin de traitement. En effet, si l'index en cours de traitement est
+			// déjà chargé, on ne peut pas présumer du fichier de langue source car il est possible d'avoir un même
+			// module dans plusieurs plugins.
+			if (!empty($GLOBALS[$idx_lang])) {
+				$backup_trad = $GLOBALS[$idx_lang];
+				unset($GLOBALS[$idx_lang]);
 			}
+
+			// On charge le fichier de langue du module en cours de traitement
+			$GLOBALS['idx_lang'] = $idx_lang;
+			$fichier_lang = $chemin . $nom_module . '_' . $langue . '.php';
+			include($fichier_lang);
+
 			// On stocke les items dans des tableaux contenant chacun tous les items recenses
-			foreach ($GLOBALS[$var_source] as $_item => $_traduction) {
-				$tous_lang[$_item][] = $fichier_lang;
-				$tous_trad[$_item][] = $_traduction;
+			foreach ($GLOBALS[$idx_lang] as $_item => $_traduction) {
+				$fichiers_langue[$_item][] = $fichier_lang;
+				$traductions[$_item][] = $_traduction;
+			}
+
+			// On rétablit le module backupé si besoin
+			unset($GLOBALS[$idx_lang]);
+			if ($backup_trad) {
+				$GLOBALS[$idx_lang] = $backup_trad;
 			}
 		}
-		ksort($tous_lang);
+		ksort($fichiers_langue);
+
+		// On restaure l'index de langue global si besoin
+		if ($idx_lang_backup) {
+			$GLOBALS['idx_lang'] = $idx_lang_backup;
+		}
 	}
 
 	// On cherche le pattern en fonction du type de correspondance
 	$trouve= array();
-	if ($tous_trad) {
+	if ($traductions) {
 		if ($correspondance == 'egal') {
-			if ($tous_lang[$pattern]) {
-				$trouve['egal'][$pattern]['fichier'] = $tous_lang[$pattern];
-				$trouve['egal'][$pattern]['traduction'] = $tous_trad[$pattern];
+			if ($fichiers_langue[$pattern]) {
+				$trouve['egal'][$pattern]['fichier'] = $fichiers_langue[$pattern];
+				$trouve['egal'][$pattern]['traduction'] = $traductions[$pattern];
 			}
 		}
 		else {
-			reset($tous_lang);
-			while (list($_item, $_fichiers) = each($tous_lang)) {
+			reset($fichiers_langue);
+			while (list($_item, $_fichiers) = each($fichiers_langue)) {
 				$commence_par = (substr($_item, 0, strlen($pattern)) == $pattern);
 				$contient = false;
 				if ($correspondance == 'contient')
 					$contient = (strpos($_item, $pattern) !== false);
 
 				if ($_item == $pattern) {
-					$trouve['egal'][$_item]['fichier'] = $tous_lang[$_item];
-					$trouve['egal'][$_item]['traduction'] = $tous_trad[$_item];
+					$trouve['egal'][$_item]['fichier'] = $_fichiers;
+					$trouve['egal'][$_item]['traduction'] = $traductions[$_item];
 				}
 				else if ($commence_par) {
-					$trouve['commence'][$_item]['fichier'] = $tous_lang[$_item];
-					$trouve['commence'][$_item]['traduction'] = $tous_trad[$_item];
+					$trouve['commence'][$_item]['fichier'] = $_fichiers;
+					$trouve['commence'][$_item]['traduction'] = $traductions[$_item];
 				}
 				else if ($contient) {
-					$trouve['contient'][$_item]['fichier'] = $tous_lang[$_item];
-					$trouve['contient'][$_item]['traduction'] = $tous_trad[$_item];
+					$trouve['contient'][$_item]['fichier'] = $_fichiers;
+					$trouve['contient'][$_item]['traduction'] = $traductions[$_item];
 				}
 			}
 		}
