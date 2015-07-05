f2006872e4bd4fcab4676687f4f42fe2fbaee3ac
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-07-05 21:10:18 +0000

    L'interface de SPIP reste étroite depuis très longtemps. En tout cas, c'est une remarque qui revient souvent. Après un peu de recherche, j'ai trouvé ce ticket : https://core.spip.net/issues/2516 Ce plugin n'a pas pour ambition de rendre responsive l'interface de SPIP, mais de voir la possibilité ou les possiblités d'utilise des pourcentages pour indiquer les largeurs de blocs. Je n'ai pas eu grand choses à surcharger en css car, en SPIP 3.1, beaucoup de blocs sont en pourcentage. Il faut que je teste ça sous SPIP 3.0. On pourrait voir aussi par la suite pour créer une constante qui indiquerait la largeur du petit écran et celle du grand écran. Ainsi on pourra personnaliser cela juste par une ligne ou deux dans un fichier mes_options.php si les valeurs par défaut ne 'plaisent' pas
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@90737 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ spip_enlarge/trunk/lang/paquet-spip_enlarge.php
@@ -0,0 +1,15 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/autorite/lang/
+if (!defined('_ECRIRE_INC_VERSION')) {
+    return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+    // A
+    'spip_enlarge_description' => 'Ce plugin va aggrandir la largeur de la page dans l\'espace privé de SPIP.',
+    'spip_enlarge_nom' => 'Aggrandir la largeur de page',
+    'spip_enlarge_slogan' => 'Elargissez votre champ d\'action !',
+);
