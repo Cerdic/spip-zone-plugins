4a6d9d2726c81677a274e34d6cedd6eba495f164
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-07-05 03:33:19 +0000

    [Salvatore] [source:_plugins_/livraison/trunk/lang/ paquet-livraison] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@90711 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- livraison/trunk/lang/paquet-livraison_fr.php
+++ livraison/trunk/lang/paquet-livraison_fr.php
@@ -1,11 +1,11 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/shop_livraison/trunk/lang/
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/livraison/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-	// S
+	// L
 	'livraison_description' => 'Gestion de livraisons',
 	'livraison_nom' => 'Livraisons',
 	'livraison_slogan' => 'Gestion de livraisons'
