9b04cf92b5f7d44b61ea16cba4491707e0d3d4a7
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-07-07 14:15:44 +0000

    les chaines e langue de charter
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@90758 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ bootstrap/trunk/lang/charter_fr.php
@@ -0,0 +1,40 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_core_/plugins/dev/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// L
+	'label_checkbox' => 'Un choix multiple',
+	'label_checkbox_1' => 'Un premier choix',
+	'label_checkbox_2' => 'Un deuxième choix',
+	'label_checkbox_long_label' => 'Un choix multiple avec un très très long label qui utilise une class <tt>.long_label</tt>',
+	'label_checkbox_long_label_1' => 'Un premier choix',
+	'label_checkbox_long_label_2' => 'Un deuxième choix',
+	'label_checkbox_ouiounon' => 'Un choix unique a cocher',
+	'label_checkbox_ouiounon_oui' => 'Oui',
+	'label_radio' => 'Un choix unique',
+	'label_radio_non' => 'Non',
+	'label_radio_oui' => 'Oui',
+	'label_select' => 'Une saisie sous forme de select',
+	'label_select_non' => 'Non merci, pas du tout',
+	'label_select_oui' => 'C’est cela, OUI',
+	'label_text' => 'Une saisie en text',
+	'label_text_long_label' => 'Une saisie avec un très très long label qui utilise une class <tt>.long_label</tt>',
+	'label_text_obli' => 'Une saisie obligatoire',
+	'label_textarea' => 'Une saisie en bloc',
+	'label_textarea_pleine_largeur' => 'Une saisie en bloc en <tt>.pleine_largeur</tt>',
+	'label_textarea_pleine_largeur_obli' => 'Une saisie obligatoire en bloc en <tt>.pleine_largeur</tt>',
+	'legend' => 'Légende d’un fieldset, pour un groupe de saisie',
+
+	// T
+	'titre_boites' => 'Boîtes',
+	'titre_charte' => 'Charte',
+	'titre_formulaires' => 'Formulaires',
+	'titre_icones' => 'Icônes',
+	'titre_listes' => 'Listes',
+	'titre_typo' => 'Typo'
+);
+
+?>
