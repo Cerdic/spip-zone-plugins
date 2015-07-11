00a68701a89055cad8d6acd53146b6a485fb9834
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-07-11 19:42:14 +0000

    Un oubli dans le précédent commit. Depuis l'espace privé, en allant sur la page ?exec=medias_lister_disallow on peut voir ce que renvoie l'inclusion.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@90803 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- medias_dereferencer/trunk/lang/medias_dereferencer_fr.php
+++ medias_dereferencer/trunk/lang/medias_dereferencer_fr.php
@@ -1,20 +1,22 @@
 <?php
+
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
 
-if (!defined('_ECRIRE_INC_VERSION')) return;
- 
+if (!defined('_ECRIRE_INC_VERSION')) {
+    return;
+}
+
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-	// M
-	'medias_dereferencer_titre' => 'Déréférencer les médias',
+    // C
+    'cfg_exemple' => 'Exemple',
+    'cfg_exemple_explication' => 'Explication de cet exemple',
+    'cfg_titre_parametrages' => 'Paramétrages',
 
-	// C
-	'cfg_exemple' => 'Exemple',
-	'cfg_exemple_explication' => 'Explication de cet exemple',
-	'cfg_titre_parametrages' => 'Paramétrages',
+    // M
+    'medias_dereferencer_titre' => 'Déréférencer les médias',
 
-	// T
-	'titre_page_configurer_medias_dereferencer' => 'Paramétrer le plugin \"Déréférencer les médias\"',
+    // T
+    'titre_page_configurer_medias_dereferencer' => 'Paramétrer le plugin \"Déréférencer les médias\"',
+    'titre_page_medias_lister_disallow' => 'Liste des Médias interdits aux moteurs de recherche.',
 );
-
-?>
