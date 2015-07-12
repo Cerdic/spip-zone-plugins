b6dd6c3ce31095ff7f8891d7bb1c70701fa8fda7
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-07-12 09:58:49 +0000

    Pas la peine de répéter paramètre dans la page
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@90806 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- medias_dereferencer/trunk/lang/medias_dereferencer_fr.php
+++ medias_dereferencer/trunk/lang/medias_dereferencer_fr.php
@@ -14,15 +14,15 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
     'cfg_titre_parametrages' => 'Paramétrages',
 
     // L
-    'lier_document_choix_oui' => 'Oui',
     'lier_document_choix_non' => 'Non',
-    'lier_document_label' => 'Désirez-vous lier les documents ?',
+    'lier_document_choix_oui' => 'Oui',
     'lier_document_explication' => 'Il arrive parfois qu\'un document soit utilisé en raccourcis typographique sans être lié à l\'objet (articles, rubriques, etc.). Lorsque ce cas se présente, désirez-vous que le plugin "Déréférencer les médias" lie le document à l\'objet éditorial ?',
+    'lier_document_label' => 'Désirez-vous lier les documents ?',
 
     // M
     'medias_dereferencer_titre' => 'Déréférencer les médias',
 
     // T
-    'titre_page_configurer_medias_dereferencer' => 'Paramétrer le plugin "Déréférencer les médias"',
+    'titre_page_configurer_medias_dereferencer' => 'Déréférencer les médias',
     'titre_page_medias_lister_disallow' => 'Liste des Médias interdits aux moteurs de recherche.',
 );
