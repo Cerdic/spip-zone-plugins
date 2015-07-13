64002c178cc5b60df3fbb83c1e037be12927895a
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-07-13 14:03:24 +0000

    Mpffff...
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@90827 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- mediaspip_player/trunk/mediaspip_player_autoriser.php
+++ mediaspip_player/trunk/mediaspip_player_autoriser.php
@@ -16,41 +16,43 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
  * depuis http://zone.spip.org/trac/spip-zone/changeset/89155/_core_#file3
  * On doit lier des docs à d'autres docs
  */
-if(!function_exists('autoriser_associerdocuments')){
-/** 
- * Autoriser a associer des documents a un objet : 
- * il faut avoir le droit de modifier cet objet
- *  
- * @param $faire 
- * @param $type 
- * @param $id 
- * @param $qui 
- * @param $opt 
- * @return bool 
- */ 
-function autoriser_associerdocuments($faire, $type, $id, $qui, $opt){
-	return autoriser('modifier',$type,$id,$qui,$opt); 
-} 
-}
-
-if(!function_exists('autoriser_dissocierdocuments')){
-/** 
- * Autoriser a dissocier des documents a un objet : 
- * il faut avoir le droit de modifier cet objet
- *  
- * @param $faire 
- * @param $type 
- * @param $id 
- * @param $qui 
- * @param $opt 
- * @return bool 
- */ 
-function autoriser_dissocierdocuments($faire, $type, $id, $qui, $opt){
-	if (intval($id)<0 AND $id==-$qui['id_auteur']){
-		return true; 
+include_spip('plugins/installer');
+if(spip_version_compare($GLOBALS['spip_version_branche'], '3.0.20', '<')){
+	if(!function_exists('autoriser_associerdocuments')){
+	/** 
+	 * Autoriser a associer des documents a un objet : 
+	 * il faut avoir le droit de modifier cet objet
+	 *  
+	 * @param $faire 
+	 * @param $type 
+	 * @param $id 
+	 * @param $qui 
+	 * @param $opt 
+	 * @return bool 
+	 */ 
+	function autoriser_associerdocuments($faire, $type, $id, $qui, $opt){
+		return autoriser('modifier',$type,$id,$qui,$opt); 
+	} 
 	}
-	return autoriser('modifier',$type,$id,$qui,$opt); 
+	
+	if(!function_exists('autoriser_dissocierdocuments')){
+	/** 
+	 * Autoriser a dissocier des documents a un objet : 
+	 * il faut avoir le droit de modifier cet objet
+	 *  
+	 * @param $faire 
+	 * @param $type 
+	 * @param $id 
+	 * @param $qui 
+	 * @param $opt 
+	 * @return bool 
+	 */ 
+	function autoriser_dissocierdocuments($faire, $type, $id, $qui, $opt){
+		if (intval($id)<0 AND $id==-$qui['id_auteur']){
+			return true; 
+		}
+		return autoriser('modifier',$type,$id,$qui,$opt); 
+	}
+	}
 }
-}
-
 ?>
