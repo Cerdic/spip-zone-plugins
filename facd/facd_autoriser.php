da35c8a2b5aca86cf7406340e4353fe43e226944
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-07-13 13:58:05 +0000

    en théorie le bug est corrigé en 3.0.20
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@90825 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- facd/facd_autoriser.php
+++ facd/facd_autoriser.php
@@ -38,7 +38,8 @@ function autoriser_relancerconversion_facd_dist($faire, $type, $id, $qui, $opt){
  * depuis http://zone.spip.org/trac/spip-zone/changeset/89155/_core_#file3
  * On doit lier des docs à d'autres docs
  */
-if(!function_exists('autoriser_associerdocuments')){
+include_spip('plugins/installer');
+if(spip_version_compare($GLOBALS['spip_version_branche'], '3.0.20', '<') && !function_exists('autoriser_associerdocuments')){
 /** 
  * Autoriser a associer des documents a un objet : 
  * il faut avoir le droit de modifier cet objet
@@ -51,6 +52,9 @@ if(!function_exists('autoriser_associerdocuments')){
  * @return bool 
  */ 
 function autoriser_associerdocuments($faire, $type, $id, $qui, $opt){
+	if (intval($id)<0 AND $id==-$qui['id_auteur']){
+		return true;
+	}
 	return autoriser('modifier',$type,$id,$qui,$opt); 
 }
 }
