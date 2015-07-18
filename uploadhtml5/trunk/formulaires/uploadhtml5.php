a8933c89984cd100ae01b6531820d1c882b2cb22
p@henix.be <p@henix.be@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-07-18 18:04:05 +0000

    Débuter le tableau d'option
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@90890 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- uploadhtml5/trunk/formulaires/uploadhtml5.php
+++ uploadhtml5/trunk/formulaires/uploadhtml5.php
@@ -2,7 +2,6 @@
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
-function formulaires_uploadhtml5_saisies_dist($objet, $id_objet, $mode = 'auto', $ajaxReload = '') {
 /**
  * Formulaire d'upload en html5
  *
@@ -16,6 +15,7 @@ function formulaires_uploadhtml5_saisies_dist($objet, $id_objet, $mode = 'auto',
  * @access public
  * @return mixed
  */
+function formulaires_uploadhtml5_saisies_dist($objet, $id_objet, $mode = 'auto', $ajaxReload = '', $args = array()) {
     $saisies = array(
         array(
             'saisie' => 'input',
@@ -35,7 +35,7 @@ function formulaires_uploadhtml5_charger_dist($objet, $id_objet, $mode = 'auto',
     return array('ajaxReload' => $ajaxReload);
 }
 
-function formulaires_uploadhtml5_traiter_dist($objet, $id_objet, $mode = 'auto', $ajaxReload = '') {
+function formulaires_uploadhtml5_traiter_dist($objet, $id_objet, $mode = 'auto', $ajaxReload = '', $args = array()) {
 
     // upload de la dropzone
     uploadhtml5_uploader_document($objet, $id_objet, $_FILES, 'new', $mode);
