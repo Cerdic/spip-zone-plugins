d135c0915b07b6cd947cb4cf07991e3c30aedd80
p@henix.be <p@henix.be@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-07-18 18:03:58 +0000

    Docblock pour le formulaire
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@90889 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- uploadhtml5/trunk/formulaires/uploadhtml5.php
+++ uploadhtml5/trunk/formulaires/uploadhtml5.php
@@ -3,6 +3,19 @@
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 function formulaires_uploadhtml5_saisies_dist($objet, $id_objet, $mode = 'auto', $ajaxReload = '') {
+/**
+ * Formulaire d'upload en html5
+ *
+ * @param mixed $objet Objet cible
+ * @param mixed $id_objet Id de l'objet cible
+ * @param string $mode mode d'insertion des objets
+ * @param string $ajaxReload Objet ajax à recharger quand une image est uploadé
+ * @param mixed $args Tableau d'option
+ *        "redirect" => Faire une redirection après l'upload de tout les éléménts.
+ *
+ * @access public
+ * @return mixed
+ */
     $saisies = array(
         array(
             'saisie' => 'input',
