6e5fee801ef06b69de5dd5af2342cdb1c9015b73
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-07-27 17:24:18 +0000

    On augmente la classe SpipDocuments pour qu'elle sache gérer TOUS les objets toute seule comme une grande. À priori il y a déjà tout ce qu'on mettait dans Sources/Articles, donc cette dernière peut à priori être virée. À tester avant, puis ajouter une config pour dire quels objets déclarés on veut indexer. Les infos sont toujours modifiables et augmentables dans le pipeline indexer_document ensuite.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@91100 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- indexer/trunk/Sources/SpipDocuments.php
+++ indexer/trunk/Sources/SpipDocuments.php
@@ -7,8 +7,24 @@ use \Indexer\Sources\SourceInterface;
 class SpipDocuments implements SourceInterface {
 	/** SPIP récent ? spip_xx_liens ou spip_xx_yy */
 	private $tables_liens = true;
+	/** Type d'objet SPIP */
+	private $objet = null;
+	private $table_objet = null;
+	private $cle_objet = null;
 
-	public function __construct() {}
+	public function __construct($objet='') {
+		if (
+			is_string($objet)
+			and include_spip('base/objets')
+			and $objet = objet_type($objet)
+			and $table = table_objet_sql($objet)
+			and in_array($table, lister_tables_objets_sql())
+		) {
+			$this->objet = $objet;
+			$this->table_objet = $table;
+			$this->cle_objet = id_table_objet($objet);
+		}
+	}
 
 	public function __toString() { return get_class($this); }
 
@@ -20,7 +36,138 @@ class SpipDocuments implements SourceInterface {
 	 * @param string $column   Colonne affectée
 	 * @return \Indexer\Sources\Documents[]
 	 */
-	public function getDocuments($start = 0, $end = 0, $column = 'id_element') {}
+	public function getDocuments($start = 0, $end = 0, $column = '') {
+		$documents = array();
+		
+		// S'il y a bien un objet défini
+		if ($this->cle_objet) {
+			// On définit la colonne de test par défaut s'il n'y en a pas
+			if (!$column) {
+				$column = $this->cle_objet;
+			}
+			
+			$where = array();
+			if ($start) $where[] = "$column >= $start";
+			if ($end)   $where[] = "$column < $end";
+
+			$all = sql_allfetsel(
+				'*',
+				$this->table_objet, // la table de l'objet défini
+				$where, // Where
+				'', // Gr By
+				'', // Or By
+				'' // Limit
+			);
+
+			foreach ($all as $contenu) {
+				$documents[] = $this->createDocumentObjet($contenu);
+			}
+		}
+		
+		return $documents;
+	}
+	
+	/**
+	 * Retourne un Document formaté pour l'indexation avec le contenu d'un objet SPIP
+	 * 
+	 * @param array $objet
+	 * @return \Indexer\Sources\Documents
+	 **/
+	public function createDocumentObjet($contenu) {
+		include_spip('inc/filtres');
+		$doc = array('properties' => array());		
+		
+		// On cherche les éléments dont on va avoir besoin
+		$id = $contenu[$this->cle_objet];
+		$doc['id'] = $this->getObjectId($this->objet, $id);
+		$doc['uri'] = generer_url_entite_absolue($id, $this->objet);
+		$doc['properties']['objet'] = $this->objet;
+		$doc['properties']['id_objet'] = $id;
+		
+		// Pour le titre
+		if (isset($contenu['titre'])) {
+			$doc['title'] = supprimer_numero($contenu['titre']);
+		}
+		elseif (isset($contenu['nom'])) {
+			$doc['title'] = supprimer_numero($contenu['nom']);
+		}
+		else {
+			$doc['title'] = '';
+		}
+		
+		// Pour le contenu principal
+		if (isset($contenu['texte'])) {
+			$doc['content'] = $contenu['texte'];
+		}
+		elseif (isset($contenu['descriptif'])) {
+			$doc['content'] = $contenu['descriptif'];
+		}
+		elseif (isset($contenu['bio'])) {
+			$doc['content'] = $contenu['bio'];
+		}
+		else {
+			$doc['content'] = '';
+		}
+		
+		// Pour le résumé
+		// (on gère direct le cas particulier des articles qui sont dans le core et qu'on connait bien)
+		if (
+			$this->objet == 'article'
+			and $summary = trim($contenu['surtitre']."\n".$contenu['soustitre']."\n".$article['chapo'])
+		) {
+			$doc['summary'] = $summary;
+		}
+		elseif (isset($contenu['descriptif'])) {
+			$doc['summary'] = couper($contenu['descriptif'], 200);
+		}
+		else {
+			$doc['summary'] = couper($doc['content'], 200);
+		}
+		
+		// Pour la date
+		if (isset($contenu['date_redac']) and substr($contenu['date_redac'],0,4) != '0000') {
+			$doc['date'] = $contenu['date_redac'];
+		}
+		elseif (isset($contenu['date'])) {
+			$doc['date'] = $contenu['date'];
+		}
+		else {
+			$doc['date'] = '0000-00-00 00:00:00';
+		}
+		
+		// S'il y a une langue
+		if (isset($contenu['lang'])) {
+			$doc['properties']['lang'] = $contenu['lang'];
+		}
+		
+		// Les auteurs
+		if ($authors = $this->getAuthorsProperties($this->objet, $id)) {
+			$doc['properties']['authors'] = $authors;
+		}
+		
+		// Les mots/tags basiquement
+		if ($tags = $this->getTagsProperties($this->objet, $id)) {
+			$doc['properties']['tags'] = $tags;
+		}
+		
+		// On crée le Document avec les infos
+		$doc = new Document($doc);
+		
+		// On le passe dans un pipeline pour pouvoir le modifier
+		$doc = pipeline(
+			'indexer_document',
+			array(
+				'args' => array(
+					'objet ' => $this->objet,
+					'id_objet' => $id,
+					'champs' => $contenu,
+				),
+				'data' => $doc,
+			)
+		);
+		
+		return $doc;
+	}
 
 	/**
 	 * Retourne tous les documents (Peut provoquer des problèmes de mémoire !)
@@ -72,8 +219,23 @@ class SpipDocuments implements SourceInterface {
 		return array_map('array_shift', $tags);
 	}
 
-	public function getBounds() {
-		return array('min' => 0, 'max' => 0);
+	public function getBounds($column = '') {
+		$bornes = array('min' => 0, 'max' => 0);
+		
+		// S'il y a bien un objet défini
+		if ($this->cle_objet) {
+			// On définit la colonne de test par défaut s'il n'y en a pas
+			if (!$column) {
+				$column = $this->cle_objet;
+			}
+			
+			$bornes = sql_fetsel(
+				array("MIN($column) AS min", "MAX($column) AS max"),
+				$this->table_objet
+			);
+		}
+		
+		return $bornes;
 	}
 
 	/**
