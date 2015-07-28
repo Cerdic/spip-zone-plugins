69ca85bf9c1585fcbf419f9b7c74cd4fc25d217e
tcharlss@bravecassine.com <tcharlss@bravecassine.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-07-28 16:26:37 +0000

    Pas besoin du pipeline afficher_auteurs_interventions qui fait doublon avec afficher_fiche_objet sur la page exec=infos_perso (brain_damage)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@91128 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- coordonnees/trunk/coordonnees_pipelines.php
+++ coordonnees/trunk/coordonnees_pipelines.php
@@ -22,7 +22,7 @@ function coordonnees_afficher_fiche_objet($flux) {
 	$e = trouver_objet_exec($exec);
 	$type = $flux['args']['type'];
 
-	if (!$e['edition'] AND in_array(table_objet_sql($type),lire_config('coordonnees/objets')) ) {
+	if (!$e['edition'] AND in_array(table_objet_sql($type),lire_config('coordonnees/objets'))) {
 		$texte .= recuperer_fond('prive/squelettes/contenu/coordonnees_fiche_objet', array(
 			'objet' => $type,
 			'id_objet' => intval($flux['args']['id']),
@@ -36,28 +36,6 @@ function coordonnees_afficher_fiche_objet($flux) {
 			$flux['data'] = substr_replace($flux['data'],$texte,$p,0);
 		else
 			$flux['data'] .= $texte;
-	}
-
-	return $flux;
-}
-
-
-/**
- * Liste des coordonnées d'un auteur sur la page "infos_perso"
-**/
-function coordonnees_affiche_auteurs_interventions($flux) {
-	$texte = "";
-	$exec = isset($flux['args']['exec']) ? $flux['args']['exec'] : _request('exec');
-	if ($id_auteur = intval($flux['args']['id_auteur']) AND $exec != 'auteur') {
-		$texte .= recuperer_fond('prive/squelettes/contenu/coordonnees_fiche_objet', array(
-			'objet' => 'auteur',
-			'id_objet' => $id_auteur,
-			),
-			array('ajax'=>'coordonnees')
-		);
-	}
-	if ($texte) {
-		$flux['data'] .= $texte;
 	}
 
 	return $flux;
