2e2256202ef1cfadf3d52f9475896bafd7d3d930
chankalan@free.fr <chankalan@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-07-30 14:35:08 +0000

    le type de media 'projection' est déprécié : https://drafts.csswg.org/mediaqueries/#media-types
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@91203 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- legendes/legendes_pipelines.php
+++ legendes/legendes_pipelines.php
@@ -4,7 +4,7 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 
 function legendes_insert_head($flux){
 	$flux .='<script src="'._DIR_PLUGIN_LEGENDES.'javascript/jquery.annotate.js" type="text/javascript"></script>';
-	$flux .= '<link rel="stylesheet" href="'._DIR_PLUGIN_LEGENDES.'legendes.css" type="text/css" media="projection, screen, tv" />';
+	$flux .= '<link rel="stylesheet" href="'._DIR_PLUGIN_LEGENDES.'legendes.css" type="text/css" media="screen, tv" />';
 	return $flux;
 }
 
