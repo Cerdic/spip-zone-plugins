9dd4f20bad5973565711cfbeda13b8468520f919
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-08-01 12:45:18 +0000

    compatible comments + on passe en 1.0.0 et stable
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@91236 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- forum_sans_previsu/trunk/forum_sans_previsu_pipelines.php
+++ forum_sans_previsu/trunk/forum_sans_previsu_pipelines.php
@@ -1,7 +1,7 @@
 <?php
 function forum_sans_previsu_formulaire_verifier($flux){
-	if($flux['args']['form'] === 'forum'){      
-		
+	if($flux['args']['form'] === 'forum'){
+
 		unset($flux['data']['previsu']);
 		unset($flux['data']['message_erreur']);
 		}
