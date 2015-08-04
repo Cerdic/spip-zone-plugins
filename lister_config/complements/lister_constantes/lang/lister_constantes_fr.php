7f389286aed432241b7d52d6efb93af7187092e4
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-08-04 20:43:01 +0000

    - Ajout des chaines manquantes ; - On revoit un peu la CSS ; - Maj du lien de la documentation ; - up de z.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@91267 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- lister_config/complements/lister_constantes/lang/lister_constantes_fr.php
+++ lister_config/complements/lister_constantes/lang/lister_constantes_fr.php
@@ -9,9 +9,18 @@ array(
 
     // D
     'description_lister_constantes' => 'On va lister sur cette page toutes les constantes PHP de SPIP.',
+    'des_constantes' => '@nb@ constantes',
+
+    // I
+    'info_constante' => 'Constante',
+    'info_valeur' => 'Valeur',
 
     // T
     'titre_lister_constantes' => 'Lister les constantes PHP de SPIP',
     'titre_page' => 'Lister les constantes PHP de SPIP',
+    'titre_page_constantes' => 'Lister les constantes',
+
+    // U
+    'une_constante' => 'Une constante',
 
 );
