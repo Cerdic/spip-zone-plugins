1fbf71c81dc1eff01bbe1189c5311c74a2b7d259
p@henix.be <p@henix.be@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-08-06 16:09:58 +0000

    Renommer foundation-4-SPIP en foundation
    Suite à cette remarque http://contrib.spip.net/Foundation-4-Spip-utilisez-le-framework-Foundation#forum482380
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@91285 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- foundation-4-spip/trunk/lang/paquet-foundation_fr.php
+++ foundation-4-spip/trunk/lang/paquet-foundation_fr.php
@@ -6,7 +6,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// F
-	'foundation_description' => 'Plugin qui intégre le framework Foundation à l\'interface public de SPIP.',
-	'foundation_nom' => 'Foundation-4-spip',
+	'foundation_description' => "Plugin qui intégre le framework Foundation à l'interface public de SPIP.",
+	'foundation_nom' => 'Foundation',
 	'foundation_slogan' => 'Utiliser le framework Foundation dans vos squelettes',
 );
