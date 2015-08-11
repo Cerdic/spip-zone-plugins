3cbc8e27e36d8f88c6f82c81646cab1320eae18f
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-08-11 12:45:55 +0000

    [Salvatore] [source:_plugins_/cookiebar/trunk/lang/ paquet-cookiebar] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@91330 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- cookiebar/trunk/lang/paquet-cookiebar_en.php
+++ cookiebar/trunk/lang/paquet-cookiebar_en.php
@@ -1,5 +1,7 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-cookiebar?lang_cible=en
+// ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
@@ -8,7 +10,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// C
 	'cookiebar_description' => '',
 	'cookiebar_nom' => 'cookiebar',
-	'cookiebar_slogan' => 'Display a warning about the use of cookies',
+	'cookiebar_slogan' => 'Display a warning about the use of cookies'
 );
 
 ?>
--- cookiebar/trunk/lang/paquet-cookiebar_fr.php
+++ cookiebar/trunk/lang/paquet-cookiebar_fr.php
@@ -1,14 +1,14 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/cookiebar/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// C
-	'cookiebar_description' => 'Demande le consentement à l\'internaute à l\'utilisation des cookies.',
+	'cookiebar_description' => 'Demande le consentement à l’internaute à l’utilisation des cookies.',
 	'cookiebar_nom' => 'cookiebar',
-	'cookiebar_slogan' => 'Affiche un avertissement à l\'utilisation des cookies',
+	'cookiebar_slogan' => 'Affiche un avertissement à l’utilisation des cookies'
 );
 
 ?>
