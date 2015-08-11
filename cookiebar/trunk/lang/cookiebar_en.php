abc5aa1c5957f6c3233a5a34353bd6cc26bdd8ea
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-08-11 12:45:34 +0000

    [Salvatore] [source:_plugins_/cookiebar/trunk/lang/ cookiebar] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@91329 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- cookiebar/trunk/lang/cookiebar_en.php
+++ cookiebar/trunk/lang/cookiebar_en.php
@@ -1,40 +1,39 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/cookiebar?lang_cible=en
+// ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
- 
+
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// C
 	'cookiebar_titre' => 'cookiebar',
 
+	// L
+	'label_accept_button' => 'Display Accept button',
+	'label_accept_button_false' => 'no',
+	'label_accept_button_true' => 'yes',
+	'label_decline_button' => 'Display Decline button',
+	'label_decline_button_false' => 'no',
+	'label_decline_button_true' => 'yes',
+	'label_policy_button' => 'Display Privacy Policy button',
+	'label_policy_button_false' => 'no',
+	'label_policy_button_true' => 'yes',
+	'label_policy_url' => 'URL of our policy privacy',
+	'label_position' => 'Bar position',
+	'label_position_fixed' => 'Top (fixed)',
+	'label_position_fixed-bottom' => 'Bottom (fixed)',
+	'label_position_top' => 'Top',
+
+	// M
+	'message_accepttext' => 'I Understand',
+	'message_declinetext' => 'Disable Cookies',
+	'message_message' => 'We use cookies to track usage and preferences.',
+	'message_policytext' => 'Privacy Policy',
+
 	// T
-	'titre_page_configurer_cookiebar' => 'Configuration de cookiebar',
-  
-  // M
-  'message_message' => 'We use cookies to track usage and preferences.',
-  'message_accepttext' => 'I Understand',
-  'message_declinetext' => 'Disable Cookies', 
-  'message_policytext' => 'Privacy Policy',
-  
-    // L
-  'label_accept_button' => 'Display Accept button',
-  'label_accept_button_true' => 'yes',
-  'label_accept_button_false' => 'no', 
-  'label_decline_button' => 'Display Decline button',
-  'label_decline_button_true' => 'yes',
-  'label_decline_button_false' => 'no',  
-  'label_policy_button' => 'Display Privacy Policy button',
-  'label_policy_button_true' => 'yes',
-  'label_policy_button_false' => 'no',
-  'label_policy_url' => 'URL of our policy privacy',
-  'label_position' => 'Bar position',
-  'label_position_top'   => 'Top',
-  'label_position_fixed'   => 'Top (fixed)',
-  'label_position_fixed-bottom'   => 'Bottom (fixed)',
-  
-  
-  
+	'titre_page_configurer_cookiebar' => 'Configuration de cookiebar'
 );
 
 ?>
--- cookiebar/trunk/lang/cookiebar_fr.php
+++ cookiebar/trunk/lang/cookiebar_fr.php
@@ -1,58 +1,55 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/cookiebar/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
- 
+
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// C
+	'cfg_titre_parametrages' => 'Configuration de cookiebar',
 	'cookiebar_titre' => 'cookiebar',
-  'cfg_titre_parametrages' => 'Configuration de cookiebar', 
+
+	// L
+	'label_accept_button' => 'Afficher le bouton',
+	'label_accept_button_false' => 'non',
+	'label_accept_button_true' => 'oui',
+	'label_accept_txt' => 'Personnaliser le texte du bouton (facultatif)',
+	'label_accept_txt_current' => 'Intitulé du bouton par défaut',
+	'label_decline_button' => 'Afficher le bouton',
+	'label_decline_button_false' => 'non',
+	'label_decline_button_true' => 'oui',
+	'label_decline_txt' => 'Personnaliser le texte du bouton (facultatif)',
+	'label_decline_txt_current' => 'Intitulé du bouton par défaut',
+	'label_effet' => 'Disparation de la barre',
+	'label_effet_fade' => 'Fondu (fade)',
+	'label_effet_hide' => 'Caché (hide)',
+	'label_effet_slide' => 'Glissé (slide)',
+	'label_message_txt' => 'Personnaliser le message (facultatif) syntaxe &lt;multi&gt;&lt;/multi&gt; accepté',
+	'label_message_txt_current' => 'Message par défaut',
+	'label_policy_button' => 'Afficher le bouton',
+	'label_policy_button_false' => 'non',
+	'label_policy_button_true' => 'oui',
+	'label_policy_txt' => 'Personnaliser le texte du bouton (facultatif)',
+	'label_policy_txt_current' => 'Intitulé du bouton par défaut',
+	'label_policy_url' => 'Si oui, adresse de la page contenant la politique de confidentialités',
+	'label_position' => 'Position de la barre',
+	'label_position_fixed' => 'Haut de page (fixed)',
+	'label_position_fixed-bottom' => 'Bas de page (fixed)',
+	'label_position_top' => 'Haut de page',
+	'label_section_accept' => 'Bouton d’accord des cookies',
+	'label_section_decline' => 'Bouton de refus de cookies',
+	'label_section_graphisme' => 'Aspect graphique',
+	'label_section_message' => 'Message',
+	'label_section_policy' => 'Bouton de lien vers la politique de confidentialité',
+
+	// M
+	'message_accepttext' => 'J’accepte',
+	'message_declinetext' => 'Désactiver les cookies',
+	'message_message' => 'En poursuivant votre navigation sur ce site, vous acceptez l’utilisation de cookies de suivi et de préférences',
+	'message_policytext' => 'Politique de confidentialité',
 
 	// T
-	'titre_page_configurer_cookiebar' => 'Configuration de cookiebar',
-  
-  // M
-  'message_message' => 'En poursuivant votre navigation sur ce site, vous acceptez l\'utilisation de cookies de suivi et de préférences',
-  'message_accepttext' => 'J\'accepte', 
-  'message_declinetext' => 'Désactiver les cookies', 
-  'message_policytext' => 'Politique de confidentialité',
-  
-  // L
-  'label_section_accept' => 'Bouton d\'accord des cookies', 
-  'label_accept_button' => 'Afficher le bouton',
-  'label_accept_button_true' => 'oui',
-  'label_accept_button_false' => 'non', 
-  'label_accept_txt' => 'Personnaliser le texte du bouton (facultatif)',
-  'label_accept_txt_current' => 'Intitulé du bouton par défaut',
-  'label_section_decline' => 'Bouton de refus de cookies', 
-  'label_decline_button' => 'Afficher le bouton',
-  'label_decline_button_true' => 'oui',
-  'label_decline_button_false' => 'non',
-  'label_decline_txt' => 'Personnaliser le texte du bouton (facultatif)',  
-  'label_decline_txt_current' => 'Intitulé du bouton par défaut',
-  'label_section_policy' => 'Bouton de lien vers la politique de confidentialité',  
-  'label_policy_button' => 'Afficher le bouton',
-  'label_policy_button_true' => 'oui',
-  'label_policy_button_false' => 'non',
-  'label_policy_url' => 'Si oui, adresse de la page contenant la politique de confidentialités',
-  'label_policy_txt' => 'Personnaliser le texte du bouton (facultatif)',
-  'label_policy_txt_current' => 'Intitulé du bouton par défaut',
-  'label_position' => 'Position de la barre',
-  'label_position_top'   => 'Haut de page',
-  'label_position_fixed'   => 'Haut de page (fixed)',
-  'label_position_fixed-bottom'   => 'Bas de page (fixed)',
-  'label_section_graphisme' => 'Aspect graphique',
-  'label_effet' => 'Disparation de la barre',
-  'label_effet_slide' => 'Glissé (slide)',
-  'label_effet_fade' => 'Fondu (fade)',
-  'label_effet_hide' => 'Caché (hide)', 
-  'label_section_message' => 'Message',
-  'label_message_txt_current' => 'Message par défaut',
-  'label_message_txt' => 'Personnaliser le message (facultatif) syntaxe &lt;multi&gt;&lt;/multi&gt; accepté',
-  
-  
-  
+	'titre_page_configurer_cookiebar' => 'Configuration de cookiebar'
 );
 
 ?>
