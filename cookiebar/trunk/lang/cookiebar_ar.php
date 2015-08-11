8cf82c2434f5842f059386ce41d0dbf12566fbc0
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-08-11 13:31:13 +0000

    [Salvatore] [source:_plugins_/cookiebar/trunk/lang/ cookiebar] Export depuis http://trad.spip.net de la langue ar
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@91335 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ cookiebar/trunk/lang/cookiebar_ar.php
@@ -0,0 +1,17 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/cookiebar?lang_cible=ar
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// M
+	'message_accepttext' => 'أوافق',
+	'message_declinetext' => 'تعطيل ملفات تعريف الارتباط',
+	'message_message' => 'يتضمن استمرارك في تصفح هذا الموقع موافقتك على استخدام ملفات تعريف الارتباط التي تمكننا من إعداد الإحصائيات بشأن الزيارات للموقع. ',
+	'message_policytext' => 'سياسة الخصوصية'
+);
+
+?>
