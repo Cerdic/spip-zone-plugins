585139bf2a9664ec8717fe4f022b8add1d8ead49
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-08-13 13:25:57 +0000

    Pouvoir éviter les notifications en faisant dans un fichier d'options par exemple define('_GETID3_NO_NOTIFICATIONS',false);
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@91342 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- getID3/trunk/notifications/getid3_verifier_binaires.php
+++ getID3/trunk/notifications/getid3_verifier_binaires.php
@@ -18,29 +18,31 @@
  * @param unknown_type $options
  */
 function notifications_getid3_verifier_binaires($quoi, $id, $options){
-	include_spip('inc/envoyer_mail'); #pour nettoyer_titre_emails
-	if(($nb = count($options['erreurs'])) > 0){
-
-		$tous = array();
-		$result = sql_select("email","spip_auteurs","webmestre='oui'");
-
-		while ($qui = sql_fetch($result)) {
-			if ($qui['email'])
-				$tous[] = $qui['email'];
-		}
-		$destinataires = pipeline('notifications_destinataires',
-			array(
-				'args'=>array('quoi'=>$quoi,'id'=>$id,'options'=>$options),
-				'data'=>$tous)
-		);
-		$msg_mail = recuperer_fond('notifications/getid3_verifier_binaires',array('erreurs'=>$options['erreurs'],'nb' => $nb));
-		/**
-		 * Nettoyage de la liste d'emails en vérifiant les doublons
-		 * et la validité des emails
-		 */
-		notifications_nettoyer_emails($destinataires);
-		foreach ($destinataires as $email) {
-			notifications_envoyer_mails($email, $msg_mail);
+	if(!defined('_GETID3_NO_NOTIFICATIONS')){
+		include_spip('inc/envoyer_mail'); #pour nettoyer_titre_emails
+		if(($nb = count($options['erreurs'])) > 0){
+	
+			$tous = array();
+			$result = sql_select("email","spip_auteurs","webmestre='oui'");
+	
+			while ($qui = sql_fetch($result)) {
+				if ($qui['email'])
+					$tous[] = $qui['email'];
+			}
+			$destinataires = pipeline('notifications_destinataires',
+				array(
+					'args'=>array('quoi'=>$quoi,'id'=>$id,'options'=>$options),
+					'data'=>$tous)
+			);
+			$msg_mail = recuperer_fond('notifications/getid3_verifier_binaires',array('erreurs'=>$options['erreurs'],'nb' => $nb));
+			/**
+			 * Nettoyage de la liste d'emails en vérifiant les doublons
+			 * et la validité des emails
+			 */
+			notifications_nettoyer_emails($destinataires);
+			foreach ($destinataires as $email) {
+				notifications_envoyer_mails($email, $msg_mail);
+			}
 		}
 	}
 }
