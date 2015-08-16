0d01d1837b0fb3e23554c0e10e74a92f8b8a4712
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-08-16 10:38:27 +0000

    orthographe
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@91379 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- selection_d_objets/branches/1/lang/paquet-selection_objet_fr.php
+++ selection_d_objets/branches/1/lang/paquet-selection_objet_fr.php
@@ -9,7 +9,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'selection_objet_description' => 'Plugin inspiré du plugin séléction d’article. 
 	Permet de séléctionner des objets spip, de les ranger et de personnaliser leur affichage selon la rubrique choisie',
 	'selection_objet_nom' => 'Selection d’objets',
-	'selection_objet_slogan' => 'Listez vos objets spips'
+	'selection_objet_slogan' => 'Listez vos objets spip'
 );
 
 ?>
