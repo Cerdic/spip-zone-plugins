ce7202cd808ce80594ff6d7c873f4d64d3b733e0
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-08-26 23:01:26 +0000

    - Bug non des moindres : le pipeline afficher_gauche court-circuitait tout. Il remplacait tout bonnement le contenu de la colonne de gauche. - Ajout de formulaire_#FORM ; - Ajout de la class .editer pour respecter les styles de SPIP 3.1 ; - Sur les checkbox, le input est dans le label (pratique usuelle) ; - Suppression des br au profit de div.nettoyeur, de ce fait, on adopte les feuilles de style par défaut de SPIP ; - Suppression des boite_ouvrir et boite_fermer car une boite dans une boite dans une boite, ce n'est pas très esthétique. De plus, ça fait gagner quelques pixels de largeur au bloc. Sur les 'grand écran' (cf. 3 cols), ça ne fait pas de mal. - Pour les id d'input, ajout de 'champ_' devant l'id pour rester conforme à l'espace privé de SPIP ; - On est en 2015 ; - Up de z.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@91489 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- savecfg/trunk/savecfg_fonctions.php
+++ savecfg/trunk/savecfg_fonctions.php
@@ -1,11 +1,15 @@
 <?php
 
-if (!defined("_ECRIRE_INC_VERSION")) return;
+if (!defined('_ECRIRE_INC_VERSION')) {
+    return;
+}
 
-function savecfg_afficher_tout($flux) {
-	if(($flux['args']['exec'] == 'cfg' AND _request('cfg')) OR (strpos($flux['args']['exec'],'configurer_') !== false)) {
-		include_spip('inc/presentation');
-		$flux['data'] = debut_boite_info(true) . recuperer_fond('prive/formulaires_savecfg') . recuperer_fond('prive/formulaire_savecfg_import') . fin_boite_info(true);
-	}
-	return $flux;
+function savecfg_afficher_tout($flux)
+{
+    if (($flux['args']['exec'] == 'cfg' and _request('cfg')) or (strpos($flux['args']['exec'], 'configurer_') !== false)) {
+        include_spip('inc/presentation');
+        $flux['data'] = recuperer_fond('prive/formulaires_savecfg').recuperer_fond('prive/formulaire_savecfg_import').$flux['data'];
+    }
+
+    return $flux;
 }
