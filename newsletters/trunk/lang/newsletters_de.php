8ab61e605d376bc1a0d7cbe4dcf036df405261a6
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-08-27 03:36:21 +0000

    [Salvatore] [source:_plugins_/newsletters/trunk/lang/ newsletters] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@91500 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- newsletters/trunk/lang/newsletters_de.php
+++ newsletters/trunk/lang/newsletters_de.php
@@ -1,20 +1,22 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/newsletters?lang_cible=de
+// ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
- 
-$GLOBALS[$GLOBALS['idx_lang']] = array(
 
-	// N
-	'newsletters_titre' => 'Newsletter',
+$GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// C
 	'cfg_exemple' => 'Beispiel',
 	'cfg_exemple_explication' => 'Erklärung des Beispiels',
 	'cfg_titre_parametrages' => 'Einstellungen',
 
+	// N
+	'newsletters_titre' => 'Newsletter',
+
 	// T
-	'titre_page_configurer_newsletters' => 'Newsletter Konfiguration',
+	'titre_page_configurer_newsletters' => 'Newsletter Konfiguration'
 );
 
 ?>
--- newsletters/trunk/lang/newsletters_en.php
+++ newsletters/trunk/lang/newsletters_en.php
@@ -1,20 +1,22 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/newsletters?lang_cible=en
+// ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
- 
+
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-	// N
-	"newsletters_titre" => "Newsletters",
-
 	// C
-	"cfg_exemple" => "Example",
-	"cfg_exemple_explication" => "Explanation of the example",
-	"cfg_titre_parametrages" => "Settings",
+	'cfg_exemple' => 'Example',
+	'cfg_exemple_explication' => 'Explanation of the example',
+	'cfg_titre_parametrages' => 'Settings',
+
+	// N
+	'newsletters_titre' => 'Newsletters',
 
 	// T
-	"titre_page_configurer_newsletters" => "Configuring Newsletters",
+	'titre_page_configurer_newsletters' => 'Configuring Newsletters'
 );
 
 ?>
