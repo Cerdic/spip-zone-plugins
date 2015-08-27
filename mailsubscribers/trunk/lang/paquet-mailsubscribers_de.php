e7803cc4cd09670a220e322adec11f30cf7584ca
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-08-27 03:35:05 +0000

    [Salvatore] [source:_plugins_/mailsubscribers/trunk/lang/ paquet-mailsubscribers] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@91496 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- mailsubscribers/trunk/lang/paquet-mailsubscribers_de.php
+++ mailsubscribers/trunk/lang/paquet-mailsubscribers_de.php
@@ -1,5 +1,7 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-mailsubscribers?lang_cible=de
+// ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
@@ -7,7 +9,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// M
 	'mailsubscribers_description' => 'E-Mail Newsletter-Anmeldung',
-	'mailsubscribers_slogan' => 'E-Mail Newsletter-Anmeldung',
+	'mailsubscribers_slogan' => 'E-Mail Newsletter-Anmeldung'
 );
 
 ?>
