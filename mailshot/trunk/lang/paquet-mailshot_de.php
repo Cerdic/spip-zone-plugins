d0c9be7f6ad943996a45511e3698ceb05b621ecb
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-08-27 03:33:48 +0000

    [Salvatore] [source:_plugins_/mailshot/trunk/lang/ paquet-mailshot] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@91492 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- mailshot/trunk/lang/paquet-mailshot_de.php
+++ mailshot/trunk/lang/paquet-mailshot_de.php
@@ -1,12 +1,15 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-mailshot?lang_cible=de
+// ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// M
-	'mailshot_slogan' => 'Versand von Massenmails',
 	'mailshot_description' => 'Versand von Massenmails',
+	'mailshot_slogan' => 'Versand von Massenmails'
 );
+
 ?>
