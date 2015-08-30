bc38bcd1a12a64bef1741f9819baa8449dd38e26
xdjuj@apsulis.com <xdjuj@apsulis.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-08-30 22:30:11 +0000

    Permettre déjà de filtrer sur les statuts d'articles, en attendant de faire mieux et de prendre en compte les autres objets
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@91546 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- plan/trunk/lang/plan_fr.php
+++ plan/trunk/lang/plan_fr.php
@@ -5,6 +5,16 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
+	// L
+	'lien_tout_afficher' => 'tous',
+
 	// P
 	'plan_titre' => 'Plan du site dans l’espace privé',
+	
+	// T
+	'texte_statut_publie' => 'publiés',
+	'texte_statut_prop' => 'proposés à l\'évaluation',
+	'texte_statut_prepa' => 'en cours de rédaction'
+
+
 );
