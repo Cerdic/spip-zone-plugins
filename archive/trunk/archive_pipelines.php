71d4268abce6f2a4be973c74500693f192fa977e
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-08-30 18:58:23 +0000

    coquille sur le nom du champ archive_statut qui rendait la mise a jour inoperante
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@91541 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- archive/trunk/archive_pipelines.php
+++ archive/trunk/archive_pipelines.php
@@ -37,7 +37,7 @@ function archive_taches_generales_cron($taches_generales){
  */
 function archive_post_edition($flux){
 	if($flux['args']['action'] == 'instituer' && $flux['args']['statut_ancien'] != 'archive'  && $flux['args']['statut_nouveau'] == 'archive'){
-		sql_updateq($flux['args']['table'],array('archive_date' => date('Y-m-d H:i:s'),'statut_archive'=>$flux['args']['statut_ancien']),id_table_objet($flux['args']['table'])."=".intval($flux['args']['id_objet']));
+		sql_updateq($flux['args']['table'],array('archive_date' => date('Y-m-d H:i:s'),'archive_statut'=>$flux['args']['statut_ancien']),id_table_objet($flux['args']['table'])."=".intval($flux['args']['id_objet']));
 	}
 	return $flux;
 }
