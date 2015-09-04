8fa5e4dfbb1eaf45c955d0062effe722aafe9ba4
jack@jack31.net <jack@jack31.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-09-04 18:31:32 +0000

    ortographe...
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@91633 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- multidomaines/trunk/lang/multidomaines_fr.php
+++ multidomaines/trunk/lang/multidomaines_fr.php
@@ -7,13 +7,13 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 //C
-	'configurations' => 'Ce formulaire va vous permettre de configurer le site de façon général. ',
+	'configurations' => 'Ce formulaire va vous permettre de configurer le site de façon générale. ',
 
 //E
-	'explications' => 'Le plugin va lire la configuration ce la façon suivante: <br />
-	-* pour trouver le domaine du secteur : Url du secteur puis url par defaut<br />
-	-* pour trouver le squelette du secteur : Chercher le dossier suivant le schéma (www.domaine.fr puis domaine.fr) puis dossiers des squelettes du secteur.<br />
-	Un domaine peut avoir plusieurs dossiers squelettes, il faut les séparé par <b>:</b> (ex:squelettes:squelettes/sous-domaine.domaine.com)',
+	'explications' => 'Le plugin va lire la configuration de la façon suivante : <br />
+	- pour trouver le domaine du secteur : Url du secteur puis url par defaut <br />
+	- pour trouver le squelette du secteur : Chercher le dossier suivant le schéma (www.domaine.fr puis domaine.fr) puis dossiers des squelettes du secteur. <br />
+	Un domaine peut avoir plusieurs dossiers squelettes, il faut les séparer par ":" (ex:squelettes:squelettes/sous-domaine.domaine.com)',
 
 //L
 	'label_editer_url' => 'Url par defaut',
