2f64ab99b80ba67cd4140a18aacff9dc1f1f85e0
erational@erational.org <erational@erational.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-09-05 14:08:10 +0000

    cosmetique
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@91645 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- sociaux/trunk/lang/sociaux_fr.php
+++ sociaux/trunk/lang/sociaux_fr.php
@@ -9,7 +9,6 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'sociaux_titre' => 'Liens sociaux',
 
 	// C
-	'cfg_titre_parametrages' => 'Liens vers les réseaux sociaux',
   'cfg_explication' => 'Indiquez les adresses des réseaux sociaux vers lesquels vous voulez faire le lien',
   'cfg_label_css' => 'Activer l\'habillage graphique de la barre de liens avec la feuille de style basée sur la police socicon',
 
