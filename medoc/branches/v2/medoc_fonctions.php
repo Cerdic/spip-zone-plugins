6f4dc8fa6b7d2a70e3dbfe5c7c559f17a024727f
brunobergot@gmail.com <brunobergot@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-09-07 16:30:08 +0000

    Retour sur r82637 : toujours proposer le raccourci doc
    Le commit cité supprimait un fichier "dupliqué inutile" sans tenir compte des modifications qu'il contenait. Ainsi, on fait que le bloc des raccourcis des documents propose toujours le doc, ce qiu est le but initial de ce plugin :)
    
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@91682 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- medoc/branches/v2/medoc_fonctions.php
+++ medoc/branches/v2/medoc_fonctions.php
@@ -8,9 +8,6 @@ function filtre_medias_raccourcis_doc($id_document,$titre,$descriptif,$inclus,$l
 	$raccourci = '';
 	$doc = 'doc';
 	include_spip('inc/documents'); // pour la fonction affiche_raccourci_doc
-	
-	if ($mode=='image' AND (strlen($descriptif.$titre) == 0))
-		$doc = 'img';
 
 	// Affichage du raccourci <doc...> correspondant
 	$raccourci = 
