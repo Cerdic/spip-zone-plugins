e49e96492623ea97d4f52ac4f6cb64e1f3a7e017
cerf@kraal.fr <cerf@kraal.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-09-08 15:58:26 +0000

    Re-modification de l'expression régulière pour sélectionner les tirets non suivis d'un espace, mais pas ceux suivis d'un astérisque ou d'un croisillon
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@91699 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- bellespuces/bellespuces.php
+++ bellespuces/bellespuces.php
@@ -3,7 +3,7 @@
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 function bellespuces_pre_typo($texte) {
-        $texte = preg_replace('/^-\s?+/m','-* ',$texte);
+        $texte = preg_replace('/^-\s?(?!\*|#)/m','-* ',$texte);
         return $texte;
 }
 
