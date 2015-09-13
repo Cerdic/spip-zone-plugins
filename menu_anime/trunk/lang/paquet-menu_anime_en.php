081b507cb97e1eff36f64d72a4f2d30e7896727f
louis.possoz@quelfutur.org <louis.possoz@quelfutur.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-09-13 05:07:57 +0000

    test -> stable
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@91820 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- menu_anime/trunk/lang/paquet-menu_anime_en.php
+++ menu_anime/trunk/lang/paquet-menu_anime_en.php
@@ -7,11 +7,11 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// M
 	'menu_anime_description' => 'Controls the display of a menu with an elaborated style sheet: gradients, box and text shadows, rounded corners and box animation. An automatic compact version for mobiles is included in the style sheet.
-Colors and dimensions can easily (?) be modified, thanks to the included file ()menu_anime.less).
+Colors and dimensions can easily (?) be modified, thanks to the included file (menu_anime.less).
 
 Nice complement to the {{Menus}} plugin for example.',
 	'menu_anime_nom' => 'Animated dropdown menu',
-	'menu_anime_slogan' => 'Display of a multi level drop down animated menu.',
+	'menu_anime_slogan' => 'Display of a multi level animated dropdown menu.',
 );
 
 ?>
