4d1c9cdd45228fdb70e66f06958dd3dbd2b61c9b
toutati@free.fr <toutati@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-09-13 15:27:18 +0000

    Oups, debug retiré
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@91842 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- paniers/trunk/action/remplir_panier.php
+++ paniers/trunk/action/remplir_panier.php
@@ -19,9 +19,7 @@ function action_remplir_panier_dist($arg=null) {
 	$quantite = intval($quantite) ? intval($quantite) : 1;
 	// retirer un objet du panier
 	if(isset($negatif)) $quantite = intval(-$quantite);
-	
-	spip_log("quantite=$quantite et id_objet=$id_objet objet=$objet","panier_test");
-	
+		
 	// Il faut cherche le panier du visiteur en cours
 	include_spip('inc/paniers');
 	$id_panier_base = 0;
@@ -58,7 +56,6 @@ function action_remplir_panier_dist($arg=null) {
 			)
 		));
 		
-		spip_log("quantite=$quantite et quantite_deja=$quantite_deja","panier_test");
 		// Si on a déjà une quantité, on fait une mise à jour
 		if ($quantite_deja){
 			sql_updateq(
