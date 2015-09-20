2f9b705bab3d9f669f3afa994f1a900e8d5f501a
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-09-20 11:21:24 +0000

    Ajout d'autorisations spécifiques au plugin. On n'affiche pas l'item dans le sous menu si on n'a pas le droit de voir la page
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@91951 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- lister_dossiers/trunk/lister_dossiers_autorisations.php
+++ lister_dossiers/trunk/lister_dossiers_autorisations.php
@@ -17,5 +17,35 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
  * @pipeline autoriser */
 function lister_dossiers_autoriser(){}
 
+/**
+ * Autorisation d'affichage du lien vers la page lister_dossiers
+ *
+ * @param  string $faire Action demandée
+ * @param  string $type  Type d'objet sur lequel appliquer l'action
+ * @param  int    $id    Identifiant de l'objet
+ * @param  array  $qui   Description de l'auteur demandant l'autorisation
+ * @param  array  $opt   Options de cette autorisation
+ * @return bool          true s'il a le droit, false sinon
+**/
+function autoriser_listerdossiers_menu_dist($faire, $type, $id, $qui, $opt)
+{
+	return autoriser('voir', $type, $id, $qui, $opt);
+}
+
+/**
+ * Autorisation de consultation de la page "lister_dossiers"
+ *
+ * @param  string $faire Action demandée
+ * @param  string $type  Type d'objet sur lequel appliquer l'action
+ * @param  int    $id    Identifiant de l'objet
+ * @param  array  $qui   Description de l'auteur demandant l'autorisation
+ * @param  array  $opt   Options de cette autorisation
+ * @return bool          true s'il a le droit, false sinon
+**/
+function autoriser_listerdossiers_voir_dist($faire, $type, $id, $qui, $opt)
+{
+	return in_array($qui['statut'], array('0minirezo')) and $qui['webmestre'] == 'oui';
+}
+
 
 ?>
