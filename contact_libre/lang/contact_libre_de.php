1a3688382b5833617d4639ed6644b9b4d0b46004
klaus@spip.de <klaus@spip.de@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-09-21 17:59:14 +0000

    encore une traduction allemande
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@91989 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ contact_libre/lang/contact_libre_de.php
@@ -0,0 +1,11 @@
+<?php
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	
+	'modele_nom_formulaire' => 'Frei konfigurierbares Kontaktformular'
+	
+);
+
+?>
