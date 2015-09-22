d04663d4056fdb3e3c2ac05da7e0116fe4a7cb0e
spip@oliver-fr.org <spip@oliver-fr.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-09-22 03:35:20 +0000

    [Salvatore] [source:_plugins_/player/trunk/lang/ player] Export depuis http://trad.spip.net de la langue oc_ni_mis
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@92001 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ player/trunk/lang/player_oc_ni_mis.php
@@ -0,0 +1,73 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/player?lang_cible=oc_ni_mis
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'always' => 'Toujou',
+	'auto' => 'Escoundre automaticamen',
+	'autohide' => 'Escoundre automaticamen',
+
+	// B
+	'blogo_vous_ecoutez' => 'En aqueu moumen escoutàs :',
+
+	// C
+	'configuration_player' => 'Counfiguracioun dóu letour multimedia',
+
+	// L
+	'label_autoload' => 'Lançà lou cargamen e afichà la premièra image',
+	'label_autonext' => 'Legì automaticamen la video segenta',
+	'label_autoplay' => 'Legì automaticamen',
+	'label_bgcolor' => 'Coulou dóu fount : ',
+	'label_bgcolor1' => 'Premièra coulou dóu degradat dóu fount :',
+	'label_bgcolor2' => 'Segounda coulou dóu degradat dóu fount :',
+	'label_buttoncolor' => 'Coulou dei boutoun : ',
+	'label_buttonovercolor' => 'Coulou dei boutoun au survol : ',
+	'label_config' => 'URL dóu fichié tèst de counfiguracioun : ',
+	'label_configxml' => 'URL dóu fichié XML de counfiguracioun : ',
+	'label_iconplaybgcolor' => 'Coulou de fount de l’icona PLAY : ',
+	'label_iconplaycolor' => 'Coulou de l’icona PLAY : ',
+	'label_insertion_auto' => 'Ensercioun automatica',
+	'label_loop' => 'Bloucà',
+	'label_player_mp3' => 'Chausida dóu letour',
+	'label_player_mp3_dewplayer' => 'Dew player',
+	'label_player_mp3_eraplayer' => 'Erational player',
+	'label_player_mp3_mejs' => 'Media Element Player',
+	'label_player_mp3_neoplayer' => 'Neolao player',
+	'label_player_mp3_pixplayer' => 'One pixel out player',
+	'label_playercolor' => 'Coulou dóu letour : ',
+	'label_showmouse' => 'Afichage de la rateta : ',
+	'label_shownext' => 'Afichà lou boutoun NEXT',
+	'label_showprevious' => 'Afichà lou boutoun PREVIOUS',
+	'label_showstop' => 'Afichà lou boutoun STOP',
+	'label_showtime' => 'Afichà lou boutoun TIME : ',
+	'label_showtime_0' => 'Noun afichà',
+	'label_showtime_1' => 'Afichà lou boutoun',
+	'label_showvolume' => 'Afichà lou boutoun VOLUME',
+	'label_shuffle' => 'Letura aleatori',
+	'label_srtbgcolor' => 'Coulou de fount dei souta-titre : ',
+	'label_srtcolor' => 'Coulou dei souta-titre : ',
+	'label_srtsize' => 'Talha dei souta-titre : ',
+	'label_startimage' => 'La o li image de titre :',
+	'label_title' => 'Lou o lu titre : ',
+	'label_titlecolor' => 'Coulou dóu titre : ',
+
+	// M
+	'maxi' => 'Massi',
+	'mini' => 'Mini',
+	'multi' => 'Multi',
+
+	// N
+	'never' => 'Jamai',
+	'normal' => 'Nourmal',
+
+	// P
+	'player_audio' => 'Player audio',
+	'player_video' => 'Player video'
+);
+
+?>
