576134ded4acb317d03856d8f268307b0a84989e
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-09-28 12:55:11 +0000

    On copie la version actuelle dans une branche v0
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@92067 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ extraire_documents/branches/v0/extract/application_pdf.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * Extraire le contenu pour le mime type pdf
+ *
+ *
+ * @param $fichier le fichier à traiter
+ * @return Scontenu le contenu brut
+ */
+function extraire_application_pdf($fichier) {
+    $contenu = "";
+
+    // Bespoin de charger composer
+    if (!class_exists('Composer\\Autoload\\ClassLoader')) {
+		include_spip('lib/Composer/Autoload/ClassLoader');
+	}
+    include_spip('lib/TCPDF-6.2.6/tcpdf_parser');
+
+    $loader = new \Composer\Autoload\ClassLoader();
+
+    // register classes with namespaces
+    $loader->add('Smalot\PdfParser', _DIR_RACINE . 'lib/pdfparser-0.9.22/src');
+    $loader->register();
+
+    $parser = new \Smalot\PdfParser\Parser();
+    //Tenter de lire le pdf
+    try {
+        set_time_limit (0);
+        $pdf = $parser->parseFile(_DIR_RACINE.$fichier);
+    }
+    catch (Exception $e) {
+        //Pour toute exception on s'arrete et on retourne un contenu vide
+        //Les cas de figure sont entre autre les fichiers mal formés ou signés
+        return "";
+    }
+
+    // Parcourir les pages et extraire le contenu textuel
+    try {
+        foreach ($pdf->getPages() as $page) {
+            $contenu .= $page->getText();
+        }
+    }
+    catch (Exception $e) {
+        //si on ne peut extraire le texte on passe à la page suivante
+        $contenu .= "";
+    }
+
+    //Libérer les ressources
+    unset($parser);
+    unset($loader);
+
+    return $contenu;
+}
