ae34b4a7e746a590e06e7e1014ed7808d82fb82a
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-09-29 10:18:26 +0000

    Inclusion pas nécessaire ici, ecrire_config (on inclut inc/config plus haut)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@92083 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- linkcheck/trunk/action/linkcheck_reinit.php
+++ linkcheck/trunk/action/linkcheck_reinit.php
@@ -4,7 +4,6 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 
 function action_linkcheck_reinit_dist(){
 	include_spip('inc/autoriser');
-	include_spip('inc/linkcheck_fcts');
 	include_spip('inc/config');
 
 	$securiser_action = charger_fonction('securiser_action', 'inc');
@@ -12,8 +11,8 @@ function action_linkcheck_reinit_dist(){
 
 	sql_delete('spip_linkchecks');
 	sql_delete('spip_linkchecks_liens');
-	ecrire_meta('linkcheck_dernier_id_objet', 0);
-	ecrire_meta('linkcheck_dernier_objet', 0);
+	ecrire_config('linkcheck_dernier_id_objet', 0);
+	ecrire_config('linkcheck_dernier_objet', 0);
 
 	if ($redirect = _request('redirect')) {
 		include_spip('inc/headers');
