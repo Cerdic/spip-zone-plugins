e18dfc9c716422400d135a2def33f516c5e5bb7f
cyp@rouquin.me <cyp@rouquin.me@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-09-29 10:14:32 +0000

    Import du plugin themes_shortcut_url
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@92082 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ themes_shortcut_url/trunk/README.md
@@ -0,0 +1,10 @@
+SPIP theme_shortcut_url
+=======
+
+Themes public très simpliste a utiliser avec Shortcut URL si vous voulez.
+
+# Version 1.0.0
+
+## Plugin SPIP
+
+* Zcore [necessite]
---
+++ themes_shortcut_url/trunk/theme_shortcut_url_options.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Options du plugin theme_shortcut_url
+ *
+ * @author cyp
+ * @package SPIP\theme_shortcut_url\Options
+ **/
+ 
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+
+
+$GLOBALS['z_blocs'] = array('content','header','footer');
+
+?>
