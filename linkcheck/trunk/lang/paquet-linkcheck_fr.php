5a9833ad6a765c6d32bf5a31c2a337403f8e1044
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-09-30 03:33:40 +0000

    [Salvatore] [source:_plugins_/linkcheck/trunk/lang/ paquet-linkcheck] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@92120 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- linkcheck/trunk/lang/paquet-linkcheck_fr.php
+++ linkcheck/trunk/lang/paquet-linkcheck_fr.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/linkcheck/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
@@ -8,7 +8,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// L
 	'linkcheck_description' => 'Ce plugin vérifie, en tache de fond, que les liens dans les objets de SPIP, pointent toujours vers des ressources en ligne. ',
 	'linkcheck_nom' => 'LinkCheck',
-	'linkcheck_slogan' => 'Verification automatisée des liens internes et externes',
+	'linkcheck_slogan' => 'Verification automatisée des liens internes et externes'
 );
 
 ?>
