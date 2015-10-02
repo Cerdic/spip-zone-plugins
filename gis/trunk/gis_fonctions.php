768488b1a8ebc5f1057540386c0dd89c9ffd7fca
brunobergot@gmail.com <brunobergot@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-10-02 08:29:08 +0000

    version 4.31.4 : moins de décimales pour popup_anchor des icones persos
    deux chiffres après la virgules pour la valeur de popup_anchor, cela suffit largement (jluc)
    
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@92141 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- gis/trunk/gis_fonctions.php
+++ gis/trunk/gis_fonctions.php
@@ -431,7 +431,7 @@ function gis_icon_properties($img=''){
 		list($h,$w) = taille_image($icon);
 		$props .= "\n\"icon_size\": ". json_encode(array($w,$h)).",";
 		$props .= "\n\"icon_anchor\": ". json_encode(array($w/2,$h)).",";
-		$props .= "\n\"popup_anchor\": ". json_encode(array(1,-$h/1.2));
+		$props .= "\n\"popup_anchor\": ". json_encode(array(1,-round($h/1.2,2)));
 	}
 	
 	if ($shadow = find_in_path('images/marker_defaut_shadow.png')) {
