88038deb88e7a3fce38d48fb5912a1962cef82e9
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-10-03 03:33:50 +0000

    [Salvatore] [source:_plugins_/auteurs_syndic/trunk/lang/ paquet-auteurs_syndic] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@92154 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ auteurs_syndic/trunk/lang/paquet-auteurs_syndic_de.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-auteurs_syndic?lang_cible=de
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'auteurs_syndic_description' => 'Ermöglicht syndizierten Websites Autoren zuzuordnen',
+	'auteurs_syndic_nom' => 'Autoren von syndizierten Websites',
+	'auteurs_syndic_slogan' => 'Ermöglicht syndizierten Websites Autoren zuzuordnen'
+);
+
+?>
