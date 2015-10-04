0717c5bb613080be62d565c99621312e65b85120
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-10-04 17:53:46 +0000

    Dépôt initial d'un plugin qui va permettre d'agrandir la largeur de la page de l'espace privé. La documentation est ici : http://contrib.spip.net/4704
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@92182 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ spip_hop/trunk/lang/spip_hop_fr.php
@@ -0,0 +1,26 @@
+<?php
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+    return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+    // A
+    'affichage_non' => 'non',
+    'affichage_oui' => 'oui',
+    'attention' => 'Attention :',
+
+    //E
+    'etendre_selection_bandeau' => 'Souhaitez vous garder la largeur initiale pour le bandeau ?',
+    'explications' => 'La valeur renseignée doit avoir une unité de mesure : em, rem, px, %, pt.',
+
+    //P
+    'parametres' => 'Paramètres (vous êtes en affichage : "@ecran@")',
+    'petit_ecran' => 'Petit écran',
+    'grand_ecran' => 'Grand écran',
+
+    //T
+    'titre_page_configurer_spip_hop' => 'Configuration de la largeur de l\'écran',
+
+);
