b152dfc3f9bc824056caff1bbeb575dff15d105a
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-10-11 09:49:56 +0000

    La CSS aussi
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@92250 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- photospip/trunk/photospip_pipelines.php
+++ photospip/trunk/photospip_pipelines.php
@@ -22,7 +22,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
  */
 function photospip_header_prive($flux) {
 	$flux .= '
-			<link rel="stylesheet" href="' . direction_css(find_in_path(_DIR_LIB_IMGAREASELECT . 'css/imgareaselect-animated.css')) . '" type="text/css" media="all" />';
+			<link rel="stylesheet" href="' . direction_css(find_in_path(_DIR_LIB_IMGAREASELECT . 'distfiles/css/imgareaselect-animated.css')) . '" type="text/css" media="all" />';
 	$flux .= '
 			<link rel="stylesheet" href="' . direction_css(find_in_path("javascript/jQuery_ui_spinner/ui.spinner.css")) . '" type="text/css" media="all" />';
 	return $flux;
