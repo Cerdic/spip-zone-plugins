b7ae175a200f301d1148fa16b3b5812339506009
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-10-13 10:01:58 +0000

    On va créer une branche pour SPIP 2.1. Ce qui a été fait dans le trunk n'offre plus de compatibilité avec SPIP 2.1
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@92284 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ spip_hop/branches/v0_3/lang/paquet-spip_hop_fr.php
@@ -0,0 +1,14 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined('_ECRIRE_INC_VERSION')) {
+    return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+    // A
+    'spip_hop_description' => 'Ce plugin va agrandir la largeur de la page dans l\'espace privé de SPIP.',
+    'spip_hop_nom' => 'Agrandir la largeur de page',
+    'spip_hop_slogan' => 'Elargissez votre champ d\'action !',
+);
