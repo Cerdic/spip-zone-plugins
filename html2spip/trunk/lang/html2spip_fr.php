11e2b8d8298b8f4078e682825ad9b0758231132c
bystrano@gmx.ch <bystrano@gmx.ch@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-10-13 18:38:03 +0000

    commit initial - généré par La Fabrique
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@92302 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ html2spip/trunk/lang/html2spip_fr.php
@@ -0,0 +1,10 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+    // H
+    'html2spip_titre' => 'Html2Spip',
+);
---
+++ html2spip/trunk/lang/paquet-html2spip_fr.php
@@ -0,0 +1,12 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+    // H
+    'html2spip_description' => 'La librairie HTML2SPIP converti du code HTML en code SPIP, avec plus de succès que la fonction sale().',
+    'html2spip_nom' => 'Html2Spip',
+    'html2spip_slogan' => 'intègre la libraire HTML2SPIP',
+);
