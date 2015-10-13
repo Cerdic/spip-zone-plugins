6f30ef00e2976dfab79650a978cca90c911e7b79
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-10-13 12:41:53 +0000

    Rétropédalage pour ce plugin. On remet la compat avec spip 2.1. Si on désire bénéficier des évolutions du plugin, ça sera pour du SPIP 3.0 mini
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@92286 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spip_hop/branches/v0_3/spip_hop_options.php
+++ spip_hop/branches/v0_3/spip_hop_options.php
@@ -8,16 +8,12 @@
 if (!defined('_ECRIRE_INC_VERSION')) {
     return;
 }
-//
-define('_INTERDIRE_COMPACTE_HEAD_ECRIRE', true);
 // ********
 // Permettre de surcharger la taille pour le petit écran et le grand écran
 // ********
 if (!defined('_PETIT_ECRAN')) {
-    define('_PETIT_ECRAN', '');
+    define('_PETIT_ECRAN', '960px');
 }
 if (!defined('_GRAND_ECRAN')) {
-    define('_GRAND_ECRAN', '');
+    define('_GRAND_ECRAN', '1280px');
 }
-// ********
-
