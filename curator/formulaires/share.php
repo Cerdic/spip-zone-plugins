5f962cdd3912d4526558df62972891525c6f2483
nicolas.dorigny@gmail.com <nicolas.dorigny@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-10-18 15:00:26 +0000

    Simplifier le formulaire et utiliser l'API objet
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@92354 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- curator/formulaires/share.php
+++ curator/formulaires/share.php
@@ -1,39 +1,79 @@
 <?php
 
-	function formulaires_share_charger_dist($titre,$nom_site,$url_site,$texte,$id_parent,$date,$auteur,$ps){
-		$valeurs = array('titre'=>$titre,'nom_site'=>$nom_site,'url_site'=>$url_site,'texte'=>$texte,'id_parent'=>'1',$date=>'',$auteur=>'',$ps=>'');
-		
-		return $valeurs;
+function formulaires_share_charger_dist() {
+	$valeurs = array(
+		'titre'     => '',
+		'nom_site'  => '',
+		'url_site'  => '',
+		'texte'     => '',
+		'id_parent' => '1',
+		'ps'        => ''
+	);
+
+	return $valeurs;
+}
+
+function formulaires_share_verifier_dist() {
+	$erreurs      = array();
+	$obligatoires = array(
+		'titre',
+		'id_parent',
+	);
+	foreach ($obligatoires as $obligatoire) {
+		if (!_request($obligatoire)) {
+			$erreurs[$obligatoire] = _T('champ_obligatoire');
+		}
 	}
 
-	function formulaires_share_verifier_dist(){
-		
-	}
+	return $erreurs;
+}
 
-	function formulaires_share_traiter_dist($titre,$nom_site,$url_site,$texte,$id_parent,$date,$auteur,$ps,$lang){
-		$res = array();
-		$titre = _request('titre');
-		$nom_site = _request('nom_site');
-		$id_parent = _request('id_parent');
-		$url_site = _request('url_site');
-		$texte = _request('texte');
-		$date = _request('date');
-		$auteur = _request('auteur');
-		$ps = _request('ps');
-		$lang = _request('lang');
-		// creation de l'article
-		$id_article = sql_insertq( 'spip_articles', array(
-		                    'titre'=>$titre, 'id_rubrique'=>$id_parent,
-		                    'texte'=>$texte, 'statut'=>'publie', 'id_secteur'=>$id_parent,
-		                    'nom_site'=>$nom_site, 'url_site'=>$url_site, 'date'=>$date,'ps'=>$ps,'lang'=>$lang));
-		// gestion auteur            
-		sql_insertq( 'spip_auteurs_liens', array('id_auteur'=>$auteur, 'id_objet'=>$id_article, 'objet'=>'article'));
+function formulaires_share_traiter_dist() {
+	$id_auteur_session = (int)$GLOBALS['visiteur_session']['id_auteur'];
 
-		// ajout des mots clés
-		$etiquettes = _request('tags');
-		include_spip('inc/tag-machine');
-		ajouter_mots($etiquettes, $id_article, 'tags', 'articles', 'id_article');
-		
+	$titre       = _request('titre');
+	$nom_site    = _request('nom_site');
+	$id_rubrique = intval(_request('id_parent'));
+	$url_site    = _request('url_site');
+	$texte       = _request('texte');
+	$ps          = _request('ps');
+
+	// créer un article dans la bonne rubrique
+	include_spip('action/editer_objet');
+	$id_article_cree = objet_inserer('article', $id_rubrique);
+
+	if ($id_article_cree) {
+		// insérer les valeurs saisies
+		$valeurs = array(
+			'titre'    => $titre,
+			'texte'    => $texte,
+			'nom_site' => $nom_site,
+			'url_site' => $url_site,
+			'date'     => date('Y-m-d H:i:s'),
+			'ps'       => $ps,
+			'statut'   => 'publie',
+
+		);
+		objet_modifier('article', $id_article_cree, $valeurs);
+
+		// associer l'auteur
+		objet_associer(
+			array('auteur' => $id_auteur_session),
+			array('article' => $id_article_cree)
+		);
+
+		// ajout les mots clés
+		if ($etiquettes = _request('tags')) {
+			include_spip('inc/tag-machine');
+			ajouter_mots($etiquettes, $id_article_cree, 'tags', 'articles', 'id_article');
+		}
+
+		// fermer la popup
 		echo '<script language="JavaScript">self.close();</script>';
-		exit;               
+		exit;
+
 	}
+	else {
+		return array('message_erreur' => _L("Erreur lors de la création de l'article"));
+	}
+}
