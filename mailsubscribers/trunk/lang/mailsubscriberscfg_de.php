35688e63334648f4d169ce702f7d9c15207f2521
willmann@netzsachen.de <willmann@netzsachen.de@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-10-21 03:33:52 +0000

    [Salvatore] [source:_plugins_/mailsubscribers/trunk/lang/ mailsubscriberscfg] Export depuis http://trad.spip.net de la langue de
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@92405 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- mailsubscribers/trunk/lang/mailsubscriberscfg_de.php
+++ mailsubscribers/trunk/lang/mailsubscriberscfg_de.php
@@ -10,6 +10,10 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// C
 	'cfg_titre_parametrages' => 'Konfiguration der Newsletter-Anmeldung',
 
+	// I
+	'info_liste_synchronisee_automatiquement' => 'Diese Liste wird automatisch synchronisiert.',
+	'info_nouvelle_liste' => 'Neue Verteilerliste',
+
 	// L
 	'label_double_optin' => 'Überprüfung der Anmeldungen',
 	'label_double_optin_0' => 'Einfache Anmeldung',
@@ -17,11 +21,14 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'label_list_id' => 'Bezeichner',
 	'label_list_status' => 'Status',
 	'label_list_titre' => 'Name der Liste',
+	'label_listes_auto_auteurs' => 'Liste der <b>@statut@</b>',
+	'label_proposer_comment_optin_1' => 'Anmeldung zum Nuwesletter vorschlagen, wenn der Nutzer einen Kommentar schreibt',
 	'label_proposer_signup_optin_1' => 'Newsletter-Abonnenment zusammen mit der Anmeldung bei der Website anbieten',
 	'label_status_close' => 'Geschlossen',
 	'label_status_delete' => 'Gelöscht',
 	'label_status_delete_explication' => 'Diese Liste und ihre Abonnenten werden gelöscht',
 	'label_status_open' => 'Offen',
+	'legend_listes_automatiques' => 'Automatisch erstellte Listen',
 	'legend_listes_diffusion' => 'Verteiler/Listen',
 
 	// T
