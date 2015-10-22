f7b97f9d7313ad4575be56962a82bf524f3c7916
george@middleeastwatch.net <george@middleeastwatch.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-10-22 03:36:07 +0000

    [Salvatore] [source:_plugins_/polyhierarchie/trunk/lang/ paquet-polyhier] Export depuis http://trad.spip.net de la langue ar
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@92423 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ polyhierarchie/trunk/lang/paquet-polyhier_ar.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-polyhier?lang_cible=ar
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// P
+	'polyhier_description' => 'يسمح هذا الملحق بإنشاء روابط عرضية بربط أقسام ومقالات بأقسام غير أقسامها الأم.',
+	'polyhier_slogan' => 'ربط مقال او قسم بقسم او أقسام أم أخرى'
+);
+
+?>
