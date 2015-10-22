e2f92f1c23de8b18ecbda83520c4db78861fd2be
george@middleeastwatch.net <george@middleeastwatch.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-10-22 03:35:46 +0000

    [Salvatore] [source:_plugins_/polyhierarchie/trunk/lang/ polyhier] Export depuis http://trad.spip.net de la langue ar
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@92422 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ polyhierarchie/trunk/lang/polyhier_ar.php
@@ -0,0 +1,28 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/polyhier?lang_cible=ar
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'articles_associes' => 'المقالات المربوطة',
+
+	// L
+	'label_autre_parent' => 'أيضاً في القسم',
+	'label_autres_parents' => 'أيضاً في الأقسام',
+	'label_parents' => 'في الأقسام',
+
+	// P
+	'parent_obligatoire' => 'يجب تحديد قسم واحد على الأقل',
+
+	// R
+	'rubriques_associees' => 'الأقسام المربوطة',
+
+	// T
+	'titre_polyhierarchie' => 'الهرمية المتعددة'
+);
+
+?>
