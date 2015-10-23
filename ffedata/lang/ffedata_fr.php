cc76f3eb49b19c4bb7c462c241bd070e148b8b66
jack@jack31.net <jack@jack31.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-10-23 15:01:58 +0000

    Un peu d'ordre dans le fichier de langue
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@92448 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- ffedata/lang/ffedata_fr.php
+++ ffedata/lang/ffedata_fr.php
@@ -5,21 +5,17 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
  
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-	
+	// C
+	'codes_equipes_club'=> 'Tous les codes utiles pour les modèles "équipes"',	
 	// E
 	'evolution_elo'=> 'Evolution du classement',
-	
 	// F
-	'ffedata_titre' => 'Data FFE',
-
-	// C
-	'ffedata_saison' => 'Saison',
-	'ffedata_saison_explication' => 'Saison demandée : pour la saison 2014-2015 saisir 2015',
-	'cfg_titre_parametrages' => 'Paramétrages',
-	
 	'ffedata_club'=> 'Club',
 	'ffedata_club_explication' => 'Le numéro du club dans la base FFE',
-
+	'ffedata_saison' => 'Saison',
+	'ffedata_saison_explication' => 'Saison demandée : pour la saison 2014-2015 saisir 2015',
+	'ffedata_titre' => 'Data FFE',
 	// T
 	'titre_page_configurer_ffedata' => 'configure Data FFE',
+	'titre_parametrages' => 'Paramétrages',
 );
