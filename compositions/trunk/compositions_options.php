e5d2311f969b82b5b1799074211a0d5616f1fa26
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-10-27 17:19:31 +0000

    la globale marqueur_skel ne doit pas etre modifiee dans un fichier fonctions car sinon elle change d'etat entre le debut du hit ou l'on a encore calcule aucun cache, et le cas ou on a calcule un cache et on sauvegarde le cache. Du coup au debut du hit on ne reconnait pas le premier cache que l'on cherche. On charge donc les fichiers fonctions pour calculer ce cache, ce que l'on fait, et la la globale vaut a nouveau la bonne valeur et on trouve tous les autres cache valides. Et ca recommence a chaque hit, avec le cache du squelette suivant etc
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@92482 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ compositions/trunk/compositions_options.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Compositions
+ * (c) 2007-2013 Cedric Morin
+ * Distribue sous licence GPL
+ *
+ * @package SPIP\Compositions\Fonctions
+ */
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS['marqueur_skel'] = (isset($GLOBALS['marqueur_skel'])?$GLOBALS['marqueur_skel']:'').':composition';
