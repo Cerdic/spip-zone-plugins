66fdff2166361e4406b0b25d39e04b0668afb65a
spip@oliver-fr.org <spip@oliver-fr.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-10-30 04:35:28 +0000

    [Salvatore] [source:_plugins_/coloration_code/trunk/lang/ paquet-coloration_code] Export depuis http://trad.spip.net de la langue oc_ni_mis
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@92536 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- coloration_code/trunk/lang/paquet-coloration_code_oc_ni_mis.php
+++ coloration_code/trunk/lang/paquet-coloration_code_oc_ni_mis.php
@@ -16,7 +16,7 @@ _ {{<cadre class="langage">...</cadre>}}.
 Lu lengage supourtat soun aquelu que soun fournit da [->http://sourceforge.net/projects/geshi/] embé una classa suplementari : "spip".
 
 
-En mancança, se lou code metut en subrilança fa mai d’una ligna, es metut en l’amagadou souta la forma testuala e proupausat au telecargamen. Aqueu founciounamen es countroulat da una coustanta PLUGIN_COLORATION_CODE_TELECHARGE predefinida a true. Pòu estre fourçat loucalemen en ajustant la classa "sans_telechargement" o "chargement" couma _ {{<code class="php sans_telechargement">}}
+En mancança, se lou code mes en subrilança fa mai d’una ligna, es mes en l’amagadou souta la forma testuala e proupausat au telecargamen. Aqueu founciounamen es countroulat da una coustanta PLUGIN_COLORATION_CODE_TELECHARGE predefinida a true. Pòu estre fourçat loucalemen en ajustant la classa "sans_telechargement" o "chargement" couma _ {{<code class="php sans_telechargement">}}
 
 Poudès finda utilisà lou filtre {coloration_code_color} en un esquelètrou couma
 _ <code>#TEXTE**|coloration_code_color{spip,code}</code> : colore #TEXTE emb’au lengage spip en format code (sensa cadre), vèire isemple lecode.html. L’url despì l’article siguèsse <code>#URL_SITE_SPIP/spip.php?page=lecode&id_article=#ENV{id_article}</code>',
