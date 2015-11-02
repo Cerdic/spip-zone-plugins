30f2706224c00d351262eba3e3e7030bccf2bf00
tofulm@gmail.com <tofulm@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-11-02 14:33:34 +0000

    On ajoute un nouveau critere : nombre de sauvegarde a conserver.
    Cela evite la suppression de tous les dumps dans le cas où le site ne serait pas souvent visité
    
    
    
    
    
    
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@92619 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- saveauto/trunk/inc/saveauto_cleaner.php
+++ saveauto/trunk/inc/saveauto_cleaner.php
@@ -10,49 +10,58 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
  * @return array $liste
  */
 function inc_saveauto_cleaner_dist($options=array()){
-	$temps = time();
+    $temps = time();
 
-	include_spip('inc/config');
-	$jours_obso = intval(lire_config('saveauto/jours_obso'));
-	$auteur = $options['auteur'] ? $options['auteur'] : $GLOBALS['visiteur_session']['id_auteur'];
-	include_spip("inc/saveauto_repertoire_save");
-	$dir_dump = saveauto_repertoire_save(lire_config('saveauto/repertoire_save'));
+    include_spip('inc/config');
+    $jours_obso = intval(lire_config('saveauto/jours_obso'));
+    $nbr_garder = intval(lire_config('saveauto/nbr_garder'));
+    $auteur     = $options['auteur'] ? $options['auteur'] : $GLOBALS['visiteur_session']['id_auteur'];
+    include_spip("inc/saveauto_repertoire_save");
+    $dir_dump = saveauto_repertoire_save(lire_config('saveauto/repertoire_save'));
 
-	if($jours_obso > 0){
-		$prefixe = lire_config('saveauto/prefixe_save').'_';
-	    $sauvegardes = preg_files($dir_dump, "${prefixe}.+\.(zip|sql)$");
-	    $liste = array();
-	    foreach($sauvegardes as $sauvegarde) {
-			$date_fichier = filemtime($sauvegarde);
-			if ($temps > ($date_fichier + $jours_obso*3600*24)) {
-				$liste[] = $sauvegarde;
-				supprimer_fichier($sauvegarde);
-			}
-		}
+    if($jours_obso > 0){
+        $prefixe = lire_config('saveauto/prefixe_save').'_';
+        $sauvegardes = preg_files($dir_dump, "${prefixe}.+\.(zip|sql)$");
+        $nbr_sauvegardes = count($sauvegardes);
+
+        $liste = array();
+        foreach($sauvegardes as $sauvegarde) {
+            if ($nbr_garder AND $nbr_sauvegardes <= $nbr_garder) {
+                break;
+            }
+
+            $date_fichier = filemtime($sauvegarde);
+            if ($temps > ($date_fichier + $jours_obso*3600*24)) {
+                echo "j'efface : $sauvegarde <br>";
+                $liste[] = $sauvegarde;
+                supprimer_fichier($sauvegarde);
+                $nbr_sauvegardes--;
+            }
+        }
     }
 
-	// Pipeline
-	pipeline('post_sauvegarde',
-		array(
-			'args' => array(
-				'liste' => $liste,
-				'auteur' => $auteur,
-				'type' => 'saveauto_clean'
-			),
-			'data' => ''
-		)
-	);
+    // Pipeline
+    pipeline('post_sauvegarde',
+        array(
+            'args' => array(
+                'liste' => $liste,
+                'auteur' => $auteur,
+                'type' => 'saveauto_clean'
+            ),
+            'data' => ''
+        )
+    );
 
     /**
      * notifications si necessaire
      */
-	if (!$options['manuel']
-	AND ($notifications = charger_fonction('notifications', 'inc'))) {
-		$notifications('saveauto_cleaner', '',
-			array('liste' => $liste, 'auteur' => $auteur)
-		);
-	}
+    if (!$options['manuel']
+    AND ($notifications = charger_fonction('notifications', 'inc'))) {
+        $notifications('saveauto_cleaner', '',
+            array('liste' => $liste, 'auteur' => $auteur)
+        );
+    }
 
-	return $liste;
+    return $liste;
 }
 ?>
