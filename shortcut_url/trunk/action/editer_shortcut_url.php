fc4d1074558f4551c7f6f5ef0e3c288135710903
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-11-03 16:57:01 +0000

    Suppression des urls en cas de suppression du shortcut_url
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@92631 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- shortcut_url/trunk/action/editer_shortcut_url.php
+++ shortcut_url/trunk/action/editer_shortcut_url.php
@@ -25,6 +25,7 @@ function shortcut_url_supprimer($id_shortcut_url){
 	if($valide && autoriser('supprimer','shortcut_url',$valide)){
 		sql_delete("spip_shortcut_urls", "id_shortcut_url=".intval($id_shortcut_url));
 		sql_delete("spip_auteurs_liens", "id_objet=".intval($id_shortcut_url));
+		sql_delete("spip_urls", "id_objet=".intval($id_shortcut_url)." AND type=".sql_quote('shortcut_url'));
 		$id_shortcut_url = 0;
 		include_spip('inc/invalideur');
 		suivre_invalideur("id='id_shortcut_url/$id_shortcut_url'");
