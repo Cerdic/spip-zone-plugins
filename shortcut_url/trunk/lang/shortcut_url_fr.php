605e372583e93628f97279f6b92938d61b762f04
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-11-03 17:50:43 +0000

    Orthographe
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@92635 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- shortcut_url/trunk/lang/shortcut_url_fr.php
+++ shortcut_url/trunk/lang/shortcut_url_fr.php
@@ -8,7 +8,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// A
 	'ajouter_shortcut_url' => 'Ajouter une URL',
 	'ajouter_shortcut_title' => 'Ajouter votre propre raccourci',
-	'auteur_shortcut_url' => 'Cet URL a été crée par : ',
+	'auteur_shortcut_url' => 'Cet URL a été créée par : ',
 
 	// E
 	'erreur_url_invalide' => 'Veuillez insérer une URL valide',
@@ -33,15 +33,15 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'form_click' => 'Nb click',
 
 	// I
-	'icone_supprimer_shortcut_url' => 'Supprimer URL raccourci',
+	'icone_supprimer_shortcut_url' => 'Supprimer l\'URL raccourcie',
 	'icone_stats_shortcut'  => 'Statistiques',
 	'icone_stats_shortcut_url'  => 'Retour à la liste statistique',
 	'info_1_shortcut_url_click' => 'Ce lien a totalisé @nb@ click',
 	'info_nb_shortcut_url_clicks' => 'Ce lien a totalisé @nb@ clicks',
-	'info_1_shortcut_url' => '@nb@ URL raccourci',
-	'info_nb_shortcut_urls' => '@nb@ URL raccourcis',
-	'info_nb_shortcut_url_stat' => '@nb@ connexion sur l\'ensemble des URL',
-	'info_nb_shortcut_url_stats' => '@nb@ connexions sur l\'ensemble des URL',
+	'info_1_shortcut_url' => '@nb@ URL raccourcie',
+	'info_nb_shortcut_urls' => '@nb@ URL raccourcies',
+	'info_nb_shortcut_url_stat' => '@nb@ connexion sur l\'ensemble des URLs',
+	'info_nb_shortcut_url_stats' => '@nb@ connexions sur l\'ensemble des URLs',
 	'item_utiliser_shortcut_url' => 'Ajouter une URL et puis c\'est tout',
 	'item_utiliser_shortcut_title' => 'Vous pouvez définir le raccourci de votre URL (max @nb@ caractères)',
 
@@ -65,21 +65,21 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// T
 	'titre_ajouter_shortcut_url' => 'Ajouter un lien raccourci',
 	'titre_modifier_shortcut_url' => 'Modifier un lien raccourci',
-	'titre_details_url' => 'Détails de l\'URL raccourcis',
+	'titre_details_url' => 'Détails de l\'URL raccourcie',
 	'titre_page' => 'Titre de la page',
 	'titre_url' => 'URL',
 	'titre_liste_pays' => 'Liste des pays',
 	'titre_url_clicks' => 'Liste des connexions',
-	'titre_shortcut_url_auteur' => 'Liste des URL raccourcis par auteur',
+	'titre_shortcut_url_auteur' => 'Liste des URL raccourcies par auteur',
 	'titre_shortcut_url_enbase' => 'Informations sur l\'URL déjà insérée : ',
-	'titre_shortcut_url_log_detail' => 'Détails pour l\'URL: ',
+	'titre_shortcut_url_log_detail' => 'Détails pour l\'URL : ',
 	'titre_shortcut_url_liste' => 'Liste des liens raccourcis',
-	'titre_shortcut_url_liste_log' => 'Statistique URL raccourci',
+	'titre_shortcut_url_liste_log' => 'Statistique URL raccourcie',
 	'titre_shortcut_url_liste_logs' => 'Liste statistique des liens raccourcis',
-	'titre_shortcut_urls_logs' => 'Raccourcis d\'url',
+	'titre_shortcut_urls_logs' => 'Raccourcis d\'URL',
 	'titre_shortcut_url_partage' => 'Partager ce lien',
 	'titre_shortcut_url_graph_carte' => 'Raccourcis par pays',
 	'titre_shortcut_url_graph_bots' => 'Raccourcis par bots',
-	'titre_shortcut_url_graph_click' => 'Nombre de click par jour',
+	'titre_shortcut_url_graph_click' => 'Nombre de clicks par jour',
 	'titre_shortcut_url_liste_logs_bots' => 'Liste statistique des bots'
 );
