191bfd614c7a7bcae5f90ed305c28edf82340973
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-11-06 09:19:28 +0000

    Ne pas avoir cette variable de debug. Up de z pour maj par SVP
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@92677 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spip_hop/trunk/spip_hop_options.php
+++ spip_hop/trunk/spip_hop_options.php
@@ -8,8 +8,7 @@
 if (!defined('_ECRIRE_INC_VERSION')) {
     return;
 }
-//
-define('_INTERDIRE_COMPACTE_HEAD_ECRIRE', true);
+
 // ********
 // Permettre de surcharger la taille pour le petit écran et le grand écran
 // ********
@@ -19,5 +18,3 @@ if (!defined('_PETIT_ECRAN')) {
 if (!defined('_GRAND_ECRAN')) {
     define('_GRAND_ECRAN', '');
 }
-// ********
-
