a6a21bb648c8d01dc56ff79ace9b649ef558ed8b
p@henix.be <p@henix.be@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-11-06 19:07:17 +0000

    Stocker en session un tableau $_FILES trafiqué
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@92697 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- uploadhtml5/trunk/action/send_upload.php
+++ uploadhtml5/trunk/action/send_upload.php
@@ -13,16 +13,26 @@ function action_send_upload_dist($arg=null) {
     include_spip('inc/session');
     spip_log($_FILES, 'uploadhtml5');
 
-    foreach($_FILES as $fichier) {
+    foreach($_FILES as $key => $fichier) {
 
         $cache_fichier = sous_repertoire(_DIR_CACHE, 'uploadhtml5').'/'.$fichier['name'].uniqid();
 
         $contenu = spip_file_get_contents($fichier['tmp_name']);
         ecrire_fichier($cache_fichier, $contenu);
 
-        // On va stocker en session le chemin du fichier
+        /**
+         * On va stocker en session le chemin du fichier
+         * et les donnée relative à $_FILES. Cela simulera un upload multiple
+         *
+         * Cependant, on caviarde le tmp_name pour utiliser le cache
+         */
         $file = session_get('file');
-        $file[] = $cache_fichier;
+        $file[$key]['name'][] = $fichier['name'];
+        $file[$key]['type'][] = $fichier['type'];
+        $file[$key]['tmp_name'][] = $cache_fichier;
+        $file[$key]['error'][] = $fichier['error'];
+        $file[$key]['size'][] = $fichier['size'];
+
         session_set('file', $file);
 
         spip_log($file, 'uploadhtml');
