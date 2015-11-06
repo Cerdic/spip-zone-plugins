a2cfdb9c1a8ff27f23687c2c8b95a4a82d02f7f1
p@henix.be <p@henix.be@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-11-06 19:06:12 +0000

    Stocker les fichiers de la saisie en cache
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@92694 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ uploadhtml5/trunk/action/send_upload.php
@@ -0,0 +1,30 @@
+<?php
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+function action_send_upload_dist($arg=null) {
+    if (is_null($arg)){
+        $securiser_action = charger_fonction('securiser_action', 'inc');
+        $arg = $securiser_action();
+    }
+
+    // On va temporairement écrire les fichiers dans le cache.
+    include_spip('inc/flock');
+
+    spip_log($_FILES, 'uploadhtml5');
+
+    foreach($_FILES as $fichier) {
+
+        $cache_fichier = sous_repertoire(_DIR_CACHE, 'uploadhtml5').'/'.$fichier['name'];
+
+        $contenu = spip_file_get_contents($fichier['tmp_name']);
+        ecrire_fichier($cache_fichier, $contenu);
+
+        // On va stocker en session le chemin du fichier
+        $file = session_get('file');
+        $file[] = $cache_fichier;
+        session_set('file', $file);
+
+        spip_log($file, 'uploadhtml');
+    }
+}
