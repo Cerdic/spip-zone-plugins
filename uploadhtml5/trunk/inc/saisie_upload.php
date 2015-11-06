2223ba5a4bc59db3750f9766f5c8a7487d9ec94b
p@henix.be <p@henix.be@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-11-06 19:09:59 +0000

    Des fonctions pour gérer les saisies upload
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@92705 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ uploadhtml5/trunk/inc/saisie_upload.php
@@ -0,0 +1,57 @@
+<?php
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+include_spip('inc/session');
+
+
+/**
+ * Fonction qui renvoie les documents uploader dans un tableau
+ * utilisable par objet_associer
+ *
+ * @access public
+ * @return mixed
+ */
+function saisie_upload_get() {
+    // récupérer les documents en session
+    $documents = session_get('upload');
+    // On va renvoyer un tableau formaté pour passer dans objet_associer
+    return array('document' => $documents);
+}
+
+/**
+ * Détruire la session d'upload quand on à terminé
+ *
+ * @access public
+ */
+function saisie_upload_terminer() {
+    session_set('upload', null);
+}
+
+/**
+ * Traiter une saisie upload.
+ * Basiquement, on associe les documents à un objet spécifique
+ * Ensuite on nettoye la session
+ *
+ * @param mixed $objet
+ * @param mixed $id_objet
+ * @access public
+ */
+function saisie_upload_traiter($objet, $id_objet) {
+
+    include_spip('action/editer_objet');
+    include_spip('action/editer_liens');
+
+    $documents = saisie_upload_get();
+    objet_associer(
+        $documents,
+        array($objet => $id_objet)
+    );
+
+    // Le lien est fait, les documents ne doivent plus être en mode temporaire
+    foreach($documents['document'] as $id_document) {
+        objet_instituer('document', $id_document, array('statut' => 'publie'));
+    }
+
+    saisie_upload_terminer();
+}
