9f2d6f1862f3a42ba03435498c448191eb1e20b4
p@henix.be <p@henix.be@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-11-07 21:13:54 +0000

    Utiliser la nouvelle fonction de suppression
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@92735 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- uploadhtml5/trunk/action/supprimer_document_tmp.php
+++ uploadhtml5/trunk/action/supprimer_document_tmp.php
@@ -31,9 +31,7 @@ function action_supprimer_document_tmp_dist($arg=null) {
         autoriser_exception('supprimer', 'document', $arg, false);
 
         // On nettoye le tableau de session suite à la suppression
-        include_spip('inc/session');
-        $upload = session_get('upload');
-        unset($upload[array_search($arg, $upload)]);
-        session_set('upload', $upload);
+        include_spip('inc/saisie_upload');
+        saisie_supprimer_document_session($arg);
     }
 }
