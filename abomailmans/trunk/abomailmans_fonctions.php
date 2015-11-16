af2fae36db28830b19c438b00a2b69f9d4eeae52
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-11-16 14:24:09 +0000

    Indentation de code + class SPIP 3.1 sur les formulaires
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@92967 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- abomailmans/trunk/abomailmans_fonctions.php
+++ abomailmans/trunk/abomailmans_fonctions.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
  * Abomailmans
  * MaZiaR - NetAktiv
@@ -7,39 +8,49 @@
  * $Id$
 */
 
-if (!defined("_ECRIRE_INC_VERSION")) return;
-
-function nettoie_chemin($chemin){
-	$liste = explode ("/", $chemin);
-	$dernier=count($liste)-1;
-	$chemin = str_replace('.html','',$liste[$dernier]);
-	$liste2 = explode('&',$chemin);
-	$chemin = $liste2[0];
-	return $chemin;
+if (!defined('_ECRIRE_INC_VERSION')) {
+    return;
 }
 
-function noextension($chemin){
-	return str_replace('.html','',$chemin);
+function nettoie_chemin($chemin)
+{
+    $liste = explode('/', $chemin);
+    $dernier = count($liste) - 1;
+    $chemin = str_replace('.html', '', $liste[$dernier]);
+    $liste2 = explode('&', $chemin);
+    $chemin = $liste2[0];
+
+    return $chemin;
 }
 
-function recup_param($chemin){
-	$a = explode('&', $chemin);
-	$i = 1;
-	while ($i < count($a)) { 
-	    $retour.= "&".htmlspecialchars(urldecode($a[$i]));
-	    $i++;
-	}	
-	return $retour;
+function noextension($chemin)
+{
+    return str_replace('.html', '', $chemin);
 }
 
-function array_param($params){
-	parse_str($params,$output);
-	return $output;	
+function recup_param($chemin)
+{
+    $a = explode('&', $chemin);
+    $retour = '';
+    $i = 1;
+    while ($i < count($a)) {
+        $retour .= '&'.htmlspecialchars(urldecode($a[$i]));
+        ++$i;
+    }
+
+    return $retour;
 }
 
-function moins30($date) {
-	$moins30 = date('Y-m-d h:m:s', time()-24*3600*30);  
-	return $moins30;
+function array_param($params)
+{
+    parse_str($params, $output);
+
+    return $output;
 }
 
-?>
+function moins30($date)
+{
+    $moins30 = date('Y-m-d h:m:s', time() - 24 * 3600 * 30);
+
+    return $moins30;
+}
