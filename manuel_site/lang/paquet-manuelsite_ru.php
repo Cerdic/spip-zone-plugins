de96c2deb532b6673afd52f1e0b3539f1cf6fc39
olly.olga@gmail.com <olly.olga@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-11-19 04:36:52 +0000

    [Salvatore] [source:_plugins_/manuel_site/lang/ paquet-manuelsite] Export depuis http://trad.spip.net de la langue ru
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@93007 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ manuel_site/lang/paquet-manuelsite_ru.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-manuelsite?lang_cible=ru
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// M
+	'manuelsite_description' => 'Этот плагин отображает значок справки на каждой странице административной части, показывая Руководство редактора сайта. Это руководство является статьей на сайте. Плагин также предоставляет набор часто задаваемых вопросов, которые могут быть легко добавлены в ваше Руководство.',
+	'manuelsite_nom' => 'Руководство редактора сайта',
+	'manuelsite_slogan' => 'Справочник особенностей вашего сайта для ваших редакторов'
+);
+
+?>
