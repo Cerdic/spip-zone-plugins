7f5d9d66a14e9e457bf659aac07cdfc9d5e87db7
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-11-20 17:09:56 +0000

    Compatibilité SPIP 3.1
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@93026 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- visiteurs_connectes/visiteurs_fonctions.php
+++ visiteurs_connectes/visiteurs_fonctions.php
@@ -1,6 +1,6 @@
 <?php
 
-function visiteurs_connectes_compter(){
- 	    return count(preg_files(_DIR_TMP.'visites/','.'));
- 	}
-?>
+function visiteurs_connectes_compter()
+{
+    return count(preg_files(_DIR_TMP.'visites/', '.'));
+}
