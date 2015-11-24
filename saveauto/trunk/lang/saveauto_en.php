a8004886c957a503af0567586c3c99afff7b5c19
spip@allesfrans.com <spip@allesfrans.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-11-24 04:35:07 +0000

    [Salvatore] [source:_plugins_/saveauto/trunk/lang/ saveauto] Export depuis http://trad.spip.net de la langue en
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@93129 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- saveauto/trunk/lang/saveauto_en.php
+++ saveauto/trunk/lang/saveauto_en.php
@@ -7,6 +7,9 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
+	// A
+	'ajouter_webmestre' => 'Include the webmaster to the addressees',
+
 	// B
 	'bouton_sauvegarder' => 'Backup the database',
 
@@ -19,26 +22,29 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'erreur_impossible_liste_tables' => 'Can not list the tables in the database.',
 	'erreur_probleme_donnees_corruption' => 'Problem with data in the @table@ table, it may be corrupted!',
 	'erreur_repertoire_inaccessible' => 'The @rep@ directory is not open for write access.',
+	'erreur_repertoire_perso_inaccessible' => 'The configured folder @rep@ is not accessible: the SPIP backup folder will be used instead',
 
 	// H
 	'help_cfg_generale' => 'These configuration settings apply to all backups, manual or automatic.',
 	'help_contenu' => 'Choose the settings of the contents of your backup file.',
 	'help_contenu_auto' => 'Choose the content of automatic backups.',
 	'help_frequence' => 'Enter the frequency of automatic backups in days.',
-	'help_liste_tables' => 'By default, all tables are exported with the exception of tables @noexport@. If you want to choose precisely the tables to save open the list by unchecking the box below.', # MODIF
+	'help_liste_tables' => 'By default, all SPIP tables are exported with the exception of @noexport@ tables. If you want to choose the tables to be saved precisely, open the list by unchecking the box below.',
 	'help_mail_max_size' => 'Enter the maximum size in MB of the backup file beyond which the mail will not be sent (value to check with your mail provider).',
 	'help_max_zip' => 'The backup file is automatically zipped if its size is less than a threshold. Enter the threshold in megabytes (This threshold is necessary to not crash the server by making a too big zip)',
+	'help_nbr_garder' => 'Indicate the minimum number of backups that should always be saved',
 	'help_notif_active' => 'If you wish to be notified of automatic processing, enable the notifications. For automatic backup you will receive the generated file by mail if it is not too large and if the "Facteur" plugin is enabled.',
-	'help_notif_mail' => 'Enter addresses separated by commas ",". These addresses are added to the one of the webmaster.', # MODIF
+	'help_notif_mail' => 'Enter addresses separated by commas ",".',
 	'help_obsolete' => 'Enter the storage life of the backups in days',
 	'help_prefixe' => 'Enter a prefix added to the name of each  backup file',
+	'help_repertoire' => 'In order to use a backup folder different from the SPIP default, specify its path from the site root (ending with / )',
 	'help_restauration' => '<strong>Warning !!!</strong> the backups made are <strong>not in SPIP format</strong> and can’t be used with the restore tool of the database from SPIP.<br /><br />
 
 For restoration, you must use the <strong>phpmyadmin</strong> interface of your database server.<br /><br />
 
 The backups contain an SQL formatted file with the commands used to <strong>delete</strong> the existing SPIP tables and to <strong>replace</strong> them with archived data. Any data <strong>more recent</strong> than those in the backup will therefore be <strong>LOST</strong>!',
 	'help_sauvegarde_1' => 'This option allows you to save the structure and content of the database in a MySQL format file that will be stored in the folder tmp/dump/. The file is named
-<em>@prefixe@_yyyymmdd_hhmmss.</em>. The table prefix is retained.', # MODIF
+<em>@prefixe@_yyyymmdd_hhmmss.</em>. The table prefix is retained.',
 	'help_sauvegarde_2' => 'Automatic backup is enabled (frequency in days: @frequence@).',
 
 	// I
@@ -64,14 +70,17 @@ The backups contain an SQL formatted file with the commands used to <strong>dele
 	'label_frequence' => 'Frequency of the backups',
 	'label_mail_max_size' => 'Threshold for sending email',
 	'label_max_zip' => 'Threshold of the zips',
+	'label_nbr_garder' => 'How many backups to save',
 	'label_nettoyage_journalier' => 'Enable daily cleaning of archives',
 	'label_notif_active' => 'Enable the notifications',
 	'label_notif_mail' => 'Email addresses to notify',
 	'label_obsolete_jours' => 'Backup storage',
 	'label_prefixe_sauvegardes' => 'Prefix:',
+	'label_repertoire_sauvegardes' => 'Folder',
 	'label_sauvegarde_reguliere' => 'Enable regular backup',
 	'label_structure' => 'Structure of the tables',
-	'label_toutes_tables' => 'Backup all tables', # MODIF
+	'label_tables_non_spip' => 'Non-SPIP tables',
+	'label_toutes_tables' => 'Backup all SPIP tables',
 	'legend_cfg_generale' => 'General settings of the backups',
 	'legend_cfg_notification' => 'Notifications',
 	'legend_cfg_sauvegarde_reguliere' => 'Automatic processing',
@@ -87,7 +96,7 @@ The backups contain an SQL formatted file with the commands used to <strong>dele
 	'message_telechargement_nok' => 'Error downloading.',
 
 	// T
-	'titre_boite_historique' => 'MySQL backups available for download', # MODIF
+	'titre_boite_historique' => 'MySQL backups available for download in folder @dossier@',
 	'titre_boite_sauver' => 'Create a MySQL backup',
 	'titre_page_configurer' => 'Saveauto plugin configuration',
 	'titre_page_saveauto' => 'Backup the database in MySQL format'
