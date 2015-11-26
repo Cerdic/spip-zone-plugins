b0fd495f299765143854ef536ae524c0a9c8a6e1
peetdu@gmail.com <peetdu@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-11-26 15:13:40 +0000

    sauvegarde de LIM dans la branche V1.0
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@93186 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ lim/branches/V1.0/inc/lim_api.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Fonctions utiles au plugin Lim
+ *
+ * @plugin     Lim
+ * @copyright  2015
+ * @author     Pierre Miquel
+ * @licence    GNU/GPL
+ * @package    SPIP\Lim\Inc
+ */
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+/**
+ * Vérifier si il existe déjà des logos de téléchargés pour un type d'objet
+ * Exception : le logo du site (dans 'Identité du site') n'est pas pris en compte
+ * 
+ * @type string
+ * @return bool
+ */
+function lim_verifier_presence_logo($type) {
+	include_spip('inc/chercher_logo');
+	include_spip('base/objets');
+	$id_objet = id_table_objet($type);
+	$prefixe_logo = _DIR_LOGOS.type_du_logo($id_objet).'*.*';
+	$liste_logos = glob($prefixe_logo);
+
+	// ne pas prendre en compte le logo du site (id = 0)
+	if ($type == 'spip_syndic') {
+		$chercher_logo = charger_fonction('chercher_logo','inc');
+		$logo_du_site = $chercher_logo(0,'id_syndic');
+		if(!empty($logo_du_site[0])) {
+			$logo_du_site = array_slice($logo_du_site, 0, 1);
+			$liste_logos = array_diff($liste_logos, $logo_du_site);
+		}
+	}
+	
+	if (is_array($liste_logos) AND count($liste_logos) > 0) return true;
+	return false;
+}
+
+/**
+ * Vérifier si il existe déjà des pétitions
+ * @return bool
+ */
+function lim_verifier_presence_petitions() {
+	/* recherche de pétitions */
+	if (sql_countsel('spip_petitions', "statut='publie'") > 0) {
+		return true;
+	}
+	return false;
+}
+
+/**
+ * Vérifier si il existe déjà des objets dans la rubrique
+ * on renvoi un tableau avec le type et la table_objet
+ * @param int $id_rubrique
+ * @param string $objet
+ * @return bool
+ */
+function lim_verifier_presence_objets($id_rubrique, $objet) {
+	$table = table_objet_sql($objet);
+	if (sql_countsel($table, "id_rubrique=$id_rubrique") > 0) return true;
+	return false;
+}
+
+?>
---
+++ lim/branches/V1.0/lim_administrations.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Fichier gérant l'installation et désinstallation du plugin Lim
+ *
+ * @plugin     Lim
+ * @copyright  2015
+ * @author     Pierre Miquel
+ * @licence    GNU/GPL
+ * @package    SPIP\Lim\Installation
+ */
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+
+/**
+ * Fonction d'installation et de mise à jour du plugin Lim.
+ *
+ * @param string $nom_meta_base_version
+ *     Nom de la meta informant de la version du schéma de données du plugin installé dans SPIP
+ * @param string $version_cible
+ *     Version du schéma de données dans ce plugin (déclaré dans paquet.xml)
+ * @return void
+**/
+function lim_upgrade($nom_meta_base_version, $version_cible) {
+	$maj = array();
+
+	include_spip('base/upgrade');
+	maj_plugin($nom_meta_base_version, $version_cible, $maj);
+}
+
+
+/**
+ * Fonction de désinstallation du plugin Lim.
+ *
+ * @param string $nom_meta_base_version
+ *     Nom de la meta informant de la version du schéma de données du plugin installé dans SPIP
+ * @return void
+**/
+function lim_vider_tables($nom_meta_base_version) {
+
+	effacer_meta('lim');
+	effacer_meta('lim_logos');
+	effacer_meta('lim_rubriques');
+	effacer_meta($nom_meta_base_version);
+}
+
+?>
