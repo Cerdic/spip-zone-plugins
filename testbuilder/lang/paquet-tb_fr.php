d23ef7d1753ce5cdae2d6cec95f69574ca21d707
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-11-28 15:18:04 +0000

    Icones 16px pour le menu. Création d'un paquet.xml pour SPIP >= 3.0 et passage de l'entrée Testbuilder dans le menu Développement pour SPIP >= 3.1. Simplification du plugin.xml en conséquence. Up de z.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@93273 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ testbuilder/lang/paquet-tb_fr.php
@@ -0,0 +1,15 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// T
+	'tb_description' => 'Un plugin pour aider les développeurs de plugin à écrire plus vite leurs tests. Et les encourager à en écrire plus souvent !
+
+Ce plugin est un outil de développement. Il a tout son intérêt installé sur votre machine de développement, mais il est très vivement déconseillé de l\'installer sur un serveur en ligne car il manipule les scripts PHP dans l\'arborescence de votre site et permet de lancer l\'exécution de code PHP arbitraire.',
+	'tb_slogan' => 'Construire des tests systématiques',
+);
+?>
