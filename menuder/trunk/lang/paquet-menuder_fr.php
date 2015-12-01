0a601c1e510d5894fb81932f03757a45b3a41db9
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-12-01 23:11:35 +0000

    Plugin compatible SPIP 3.1
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@93329 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ menuder/trunk/lang/paquet-menuder_fr.php
@@ -0,0 +1,15 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+    return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// M
+    'menuder_description' => 'Menu déroulant dynamique full CSS, qui plonge en profondeur, sans limitation de niveaux de hiérarchie.',
+    'menuder_nom' => 'Menu déroulant',
+    'menuder_slogan' => '',
+);
