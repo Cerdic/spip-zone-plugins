e8cc7db4c98d529d81b93ff44af92fb37aba1e26
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-12-02 15:47:37 +0000

    Une erreur à la désinstallation du plugin
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@93347 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- menuder/trunk/menuder_administrations.php
+++ menuder/trunk/menuder_administrations.php
@@ -7,15 +7,16 @@ include_spip('inc/meta');
 
 function menuder_upgrade($nom_meta_base_version, $version_cible)
 {
-	spip_log('Install/upgrade du plugin', 'menuder');
 	$maj = array();
 	include_spip('base/upgrade');
 	maj_plugin($nom_meta_base_version, $version_cible, $maj);
+	spip_log('Install/upgrade du plugin ('.$nom_meta_base_version.' -> '.$version_cible.')', 'menuder');
 }
 
 function menuder_vider_tables($nom_meta_base_version)
 {
-	spip_log('Suppression des tables du plugin', 'menuder');
 	// Effacer la config
 	effacer_meta('menuder');
+	effacer_meta($nom_meta_base_version);
+	spip_log('Suppression des tables du plugin', 'menuder');
 }
