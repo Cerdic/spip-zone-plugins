5e4a30a528de7a24ab04920cb6b78cee8f0f63a3
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-12-03 22:26:59 +0000

    Quand on utilise le plugin avec SPIP 3.0 il faut surcharger tous les JS jquery&co dans le site public pour fournir une version suffisante a BS3
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@93382 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- bootstrap/trunk/bootstrap3_options.php
+++ bootstrap/trunk/bootstrap3_options.php
@@ -1,3 +1,6 @@
 <?php
 
 $GLOBALS['marqueur_skel'] = (isset($GLOBALS['marqueur_skel']) ?  $GLOBALS['marqueur_skel'] : '').":bootstrap3";
+if (strncmp($GLOBALS['spip_version_branche'],"3.0",3)==0){
+	_chemin(_DIR_PLUGIN_BOOTSTRAP3."spip3/");
+}
