6e953666e51adb74dae0d54b9087558814a3b216
tcharlss@bravecassine.com <tcharlss@bravecassine.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-12-03 17:29:36 +0000

    On passe l'état en "test", ajout de quelques explications supplémentaires dans la description du plugin, et ajout d'une icône fatcow, up de z.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@93357 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- editer_liens_simples/lang/paquet-editer_liens_simples_fr.php
+++ editer_liens_simples/lang/paquet-editer_liens_simples_fr.php
@@ -5,9 +5,18 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 // E
 	'editer_liens_simples_nom' => 'Éditer Liens Simples',
-	'editer_liens_simples_description' => 'Fourni un formulaire et une API pour lier les objets
-		sur les tables spip_xxx_yyy (et non spip_xxx_liens), directement inspiré de ce qui est fourni par SPIP
-		pour ses tables de liens.',
+	'editer_liens_simples_description' => 'Fourni un formulaire et une API pour lier les objets sur les tables spip_xxx_yyy (et non spip_xxx_liens), directement inspiré de ce qui est fourni par SPIP pour ses tables de liens.
+		
+		Les tables de liens spip_xxx_yyy servent à lier 2 types d\'objets prédéfinis, à la différence des tables de liens spip_xxx_liens qui permettent de lier n\'importe quels types d\'objets.
+		Exemple :
+		<code>spip_organisations_contacts</code> permet de lier des contacts à des organisations.
+		<code>spip_organisations_liens</code> permet de lier n\'importe quel type d\'objet à des organisations.
+		
+		L\'API d\'edition de liens simples s\'utilise comme l\'API traditionnelle, en suffixant les fonctions avec «_simples» :
+		<code>include_spip(\'action/editer_liens_simples\')</code>
+		<code>objet_associer_simples()</code>
+		<code>objet_trouver_liens_simples()</code>
+		etc.',
 	'editer_liens_simples_slogan' => 'API de liens pour les tables spip_xxx_yyy',
 );
 
