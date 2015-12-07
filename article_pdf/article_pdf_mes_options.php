ca2a98dfa4bb165971dc23de894cc5cf004ce2a1
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-12-07 15:53:14 +0000

    Petites corrections + indentation
    0.4.7
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@93425 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- article_pdf/article_pdf_mes_options.php
+++ article_pdf/article_pdf_mes_options.php
@@ -1,68 +1,70 @@
 <?php
 
-// options
+if (!defined('_ECRIRE_INC_VERSION'))
+	return;
+
 if (!defined('_DIR_LIB')) define('_DIR_LIB', _DIR_RACINE . 'lib/');
 if (!defined('_DIR_FPDF_LIB')) define('_DIR_FPDF_LIB', _DIR_LIB . 'h5c1accba-fpdf17/');
 
 
 function balise_ARTICLE_PDF_dist($p) {
-		if (!is_array($p->param))
-			$p->param=array();
+	if (!is_array($p->param))
+		$p->param=array();
 
-		// Produire le premier argument {article_pdf}
-		$texte = new Texte;
-		$texte->type='texte';
-		$texte->texte='article_pdf';
-		$param = array(0=>NULL, 1=>array(0=>$texte));
-		array_unshift($p->param, $param);
+	// Produire le premier argument {article_pdf}
+	$texte = new Texte;
+	$texte->type='texte';
+	$texte->texte='article_pdf';
+	$param = array(0=>NULL, 1=>array(0=>$texte));
+	array_unshift($p->param, $param);
 
-		// Transformer les filtres en arguments
-		for ($i=1; $i<count($p->param); $i++) {
-			if ($p->param[$i][0]) {
-				if (!strstr($p->param[$i][0], '='))
-					break;# on a rencontre un vrai filtre, c'est fini
-				$texte = new Texte;
-				$texte->type='texte';
-				$texte->texte=$p->param[$i][0];
-				$param = array(0=>$texte);
-				$p->param[$i][1] = $param;
-				$p->param[$i][0] = NULL;
-			}
+	// Transformer les filtres en arguments
+	for ($i=1; $i<count($p->param); $i++) {
+		if ($p->param[$i][0]) {
+			if (!strstr($p->param[$i][0], '='))
+				break;# on a rencontre un vrai filtre, c'est fini
+			$texte = new Texte;
+			$texte->type='texte';
+			$texte->texte=$p->param[$i][0];
+			$param = array(0=>$texte);
+			$p->param[$i][1] = $param;
+			$p->param[$i][0] = NULL;
 		}
-
-		// Appeler la balise #MODELE{article_pdf}{arguments}
-		if (!function_exists($f = 'balise_modele'))
-			$f = 'balise_modele_dist';
-		return $f($p);
 	}
+
+	// Appeler la balise #MODELE{article_pdf}{arguments}
+	if (!function_exists($f = 'balise_modele'))
+		$f = 'balise_modele_dist';
+	return $f($p);
+}
+
 function balise_RUBRIQUE_PDF_dist($p) {
-		if (!is_array($p->param))
-			$p->param=array();
+	if (!is_array($p->param))
+		$p->param=array();
 
-		// Produire le premier argument {article_pdf}
-		$texte = new Texte;
-		$texte->type='texte';
-		$texte->texte='rubrique_pdf';
-		$param = array(0=>NULL, 1=>array(0=>$texte));
-		array_unshift($p->param, $param);
+	// Produire le premier argument {article_pdf}
+	$texte = new Texte;
+	$texte->type='texte';
+	$texte->texte='rubrique_pdf';
+	$param = array(0=>NULL, 1=>array(0=>$texte));
+	array_unshift($p->param, $param);
 
-		// Transformer les filtres en arguments
-		for ($i=1; $i<count($p->param); $i++) {
-			if ($p->param[$i][0]) {
-				if (!strstr($p->param[$i][0], '='))
-					break;# on a rencontre un vrai filtre, c'est fini
-				$texte = new Texte;
-				$texte->type='texte';
-				$texte->texte=$p->param[$i][0];
-				$param = array(0=>$texte);
-				$p->param[$i][1] = $param;
-				$p->param[$i][0] = NULL;
-			}
+	// Transformer les filtres en arguments
+	for ($i=1; $i<count($p->param); $i++) {
+		if ($p->param[$i][0]) {
+			if (!strstr($p->param[$i][0], '='))
+				break;# on a rencontre un vrai filtre, c'est fini
+			$texte = new Texte;
+			$texte->type='texte';
+			$texte->texte=$p->param[$i][0];
+			$param = array(0=>$texte);
+			$p->param[$i][1] = $param;
+			$p->param[$i][0] = NULL;
 		}
-
-		// Appeler la balise #MODELE{article_pdf}{arguments}
-		if (!function_exists($f = 'balise_modele'))
-			$f = 'balise_modele_dist';
-		return $f($p);
 	}
-?>
+
+	// Appeler la balise #MODELE{article_pdf}{arguments}
+	if (!function_exists($f = 'balise_modele'))
+		$f = 'balise_modele_dist';
+	return $f($p);
+}
