34ce76cb0d9c71ec7b8360701e97f66e84bf3475
marcimat@rezo.net <marcimat@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-12-08 16:15:44 +0000

    Lorsqu'un ancien logo est présent, il arrive déjà dans une balise IMG. Du coup, il ne faut pas réappliquer dessus le filtre balise_img ! + on ajoute la classe spip_logo au singulier, comme en SPIP 3.1.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@93455 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- roles_documents/trunk/roles_documents_fonctions.php
+++ roles_documents/trunk/roles_documents_fonctions.php
@@ -1,11 +1,11 @@
 <?php
 
-function document_vu($id_objet,$objet,$id_document){
+function document_vu($id_objet, $objet, $id_document) {
 	include_spip('action/editer_liens');
-        $objets_lies=array($objet=>$id_objet);
-        $objets_source=array('document'=>$id_document);
-        $qualif = array('vu'=>'oui');
-        objet_associer($objets_source, $objets_lies, $qualif);
+	$objets_lies=array($objet=>$id_objet);
+	$objets_source=array('document'=>$id_document);
+	$qualif = array('vu'=>'oui');
+	objet_associer($objets_source, $objets_lies, $qualif);
 }
 
 function roles_documents_declarer_tables_interfaces($interfaces) {
