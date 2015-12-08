5c024290c669156de710935b7dd9730050735581
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-12-08 04:33:01 +0000

    [Salvatore] [source:_plugins_/mesfavoris/trunk/lang/ mesfavoris] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@93439 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- mesfavoris/trunk/lang/mesfavoris_fr.php
+++ mesfavoris/trunk/lang/mesfavoris_fr.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/mesfavoris/lang/
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/mesfavoris/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
