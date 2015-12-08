9dd3301d539e0dfdec71c0b3e852fe661e31ad90
tcharlss@bravecassine.com <tcharlss@bravecassine.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-12-08 20:00:34 +0000

    Ajout de 3 nouveaux paramètres à la balise #BOLO pour couper le texte sur le modèle de la balise #INTRODUCTION :
    -  #BOLO{300} : couper à 300 caractères.
    - #BOLO{50,''} : couper à 50 caractères et ne pas afficher de point de suite.
    - #BOLO{50,'',latin} : couper à 50, pas de point de suite, et utiliser le type de texte «latin» (pour l'instant, il n'y a qu'un seul type de texte).
    - #BOLO{50,null,latin} : la chaîne 'null' en 2 paramètre permet d'utiliser les points de suite par défaut, quand on veut utiliser le 3 ème paramère (qui n'a aucune utilité pour l'instant, répétons le).
    
    Up de Y pour le coup.
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@93459 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- bolo/bolo_fonctions.php
+++ bolo/bolo_fonctions.php
@@ -1,16 +1,65 @@
 <?php
 /**
- * Plugin Bolo pour Spip 2.0
- * Licence GPL (c) 2010
- * Auteur Cyril MARION - Ateliers CYM
+ * Fonction du plugin Bolo
  *
+ * @plugin     Bolo
+ * @copyright  2010
+ * @author     Cyril MARION - Ateliers CYM
+ * @licence    GPL
+ * @package    SPIP\Bolo\Fonctions
  */
 
-// La balise BOLO
+/**
+ * Compile la balise `#BOLO` chargée d'afficher du faux texte.
+ *
+ * - Le 1er argument est un chiffre qui indique la taille de coupe du texte au moyen du filtre du même nom.
+ *   Si on met autre chose qu'un nombre, le paramètre est ignoré (pas de coupe).
+ * - Le 2ème argument permet de définir les points de suite, par défaut `&nbsp;(...)`.
+ *   Quand on veut l'ignorer, on peut utiliser la chaîne `null` (cf. 3ème exemple plus bas).
+ * - Le 3ème argument permet de définir le type de texte utilisé (cf. bolo/{type}.php).
+ *
+ * @balise
+ * @uses couper()
+ * @example
+ *    ```
+ *    [(#BOLO{300})]              // coupe à 300 avec les points de suite par défaut
+ *    [(#BOLO{300,''})]           // coupe à 300 sans point de suite.
+ *    [(#BOLO{500,null,gangsta})] // coupe à 500 avec les points de suite par défaut, et utilise le texte «gangsta».
+ *    ```
+ *
+ * @param Champ $p
+ *     Pile au niveau de la balise
+ * @param string $_couper
+ *     Taille de la coupe du texte
+ * @param string $_suite
+ *     Points de suite ajoutés quand le texte est coupé
+ *     par défaut `&nbsp;(...)`
+ *     On peut utiliser la chaîne 'null' pour ignorer et utiliser la valeur par défaut.
+ * @param string $_type
+ *     Type de texte à utiliser.
+ *     par défaut `latin`
+ * @return Champ
+ *     Pile complétée par le code à générer
+ */
 function balise_BOLO($p) {
-	$type = "'latin'";
-	$p->code = "((\$x = charger_fonction($type, 'bolo', true)) ? \$x() : '')"; // si fonction bolo_latin
+
+	// longueur du texte
+	if (($v = interprete_argument_balise(1,$p)) !== null) {
+		$_couper = '(intval('.$v.') > 0) ? intval('.$v.') : null';
+	}
+	// points de suite
+	$_suite = "'&nbsp;(...)'";
+	if (($v2 = interprete_argument_balise(2,$p)) !== null) {
+		$_suite = '('.$v2.' !== \'null\') ? '.$v2.' : '.$_suite;
+	}
+	// type de texte utilisé, cf. bolo/{type}.php
+	$_type = "'latin'";
+	if (($v3 = interprete_argument_balise(3,$p)) !== null) {
+		$_type = $v3;
+	}
+
+	$p->code = "((\$bolo = charger_fonction($_type, 'bolo', true)) ? ($_couper ? couper(\$bolo(), $_couper, $_suite) : \$bolo()) : '')";
 	$p->interdire_scripts = false;
+
 	return $p;
 }
-
