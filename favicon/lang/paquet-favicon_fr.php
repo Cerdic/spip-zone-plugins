02353dc5fe43d32a12e73fb71a4b37af82dcb5c9
erational@erational.org <erational@erational.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-12-10 10:38:51 +0000

    pas de doc. c'est "auto-documenté"
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@93511 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ favicon/lang/paquet-favicon_fr.php
@@ -0,0 +1,14 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// F
+	'favicon_description' => 'Génération automatique, à partir de votre logo de site, de favicon et touch icon pour les différents navigateurs et terminaux.
+	Pour que ce soit esthétiquement réussi, votre logo de site doit être carré, de 150 pixels de côté minimum. Sinon, déposer une image nommée « favicon.png » à la racine du dossier « squelettes ».',
+	'favicon_slogan' => 'Favicon et touch icon du site',
+);
+?>
