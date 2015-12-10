e415109f596633f17c26170f8ef1661a6ec496ac
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-12-10 20:59:08 +0000

    - Màj du Framework en 1.3 ; - Un peu de ménage dans le plugin ; - On utilise la fonction find_in_path ; - Synchronisation des infos entre paquet.xml et plugin.xml ; - Ajout d'une chaine de langue pour le paquet.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@93521 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- ezcss/ezcss_pipelines.php
+++ ezcss/ezcss_pipelines.php
@@ -1,11 +1,11 @@
 <?php
 
-if (!defined("_ECRIRE_INC_VERSION")) return;
-
-function ezcss_insert_head_css($flux) {
-	$flux .= '<link rel="stylesheet" type="text/css" href="plugins/auto/ezcss/ez-plug.css" media="all" />';
-	
-	return $flux;
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
 }
 
-?>
+function ezcss_insert_head_css($flux) {
+	$flux .= '<link rel="stylesheet" type="text/css" href="'.find_in_path('css/ez-plug.css').'" media="all" />';
+
+	return $flux;
+}
---
+++ ezcss/lang/paquet-ezcss_fr.php
@@ -0,0 +1,11 @@
+<?php
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	'ezcss_description' => 'Framework CSS simple et efficace. Permet de constuire un site complexe pour tous les navigateurs récents. Contrairement aux autres framework qui utilisent une grille, celui-ci se sert de blocs que l\'on peut imbriquer ou non. Un framework CSS est utilisé essentiellement pour permettre à un site d\'apparaître de la même facon quelque soit le navigateur utilisé par le visiteur. EZ-CSS (qui se prononce easy-css) rend cette fonctionnalité très simple à mettre en place même pour des sites très complexes.',
+	'ezcss_nom' => 'EZ-CSS',
+	'ezcss_slogan' => 'EZ-CSS',
+);
