0de487e69e961d63ac9458d6f0f8db84fbdbad2f
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-12-10 21:19:00 +0000

    Oups.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@93523 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- ezcss/lang/paquet-ezcss_fr.php
+++ ezcss/lang/paquet-ezcss_fr.php
@@ -5,7 +5,7 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 }
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-	'ezcss_description' => 'Framework CSS simple et efficace. Permet de constuire un site complexe pour tous les navigateurs récents. Contrairement aux autres framework qui utilisent une grille, celui-ci se sert de blocs que l\'on peut imbriquer ou non. Un framework CSS est utilisé essentiellement pour permettre à un site d\'apparaître de la même facon quelque soit le navigateur utilisé par le visiteur. EZ-CSS (qui se prononce easy-css) rend cette fonctionnalité très simple à mettre en place même pour des sites très complexes.',
+	'ezcss_description' => 'Permet de constuire un site complexe pour tous les navigateurs récents. Contrairement aux autres framework qui utilisent une grille, celui-ci se sert de blocs que l\'on peut imbriquer ou non. Un framework CSS est utilisé essentiellement pour permettre à un site d\'apparaître de la même facon quelque soit le navigateur utilisé par le visiteur. EZ-CSS (qui se prononce easy-css) rend cette fonctionnalité très simple à mettre en place même pour des sites très complexes.',
 	'ezcss_nom' => 'EZ-CSS',
-	'ezcss_slogan' => 'EZ-CSS',
+	'ezcss_slogan' => 'Framework CSS simple et efficace',
 );
