18fb4da643a82670394a767213ec70ca41eed8fe
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-12-10 09:19:11 +0000

    Ajout du découpage du dictionnaire dans plein de dossiers, rangés dans IMG/indexer_autoxomplete (copie de M-fil)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@93505 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- indexer/trunk/spip-cli/SphinxGenererAutocomplete.php
+++ indexer/trunk/spip-cli/SphinxGenererAutocomplete.php
@@ -39,6 +39,10 @@ class SphinxGenererAutocomplete extends Command {
 		
 		// On se fait un dossier temporaire pour enregistrer le dictionnaire complet
 		$dossier_tmp = sous_repertoire(_DIR_TMP . 'sphinx/');
+		$dict_tmp = $dossier_tmp . $index . '.dict.txt';
+		
+		// On se fait un dossier final
+		$dossier_autocomplete = sous_repertoire(_DIR_IMG . 'indexer_autocomplete');
 		
 		$sphinxql = new \Sphinx\SphinxQL\SphinxQL(SPHINX_SERVER_HOST, SPHINX_SERVER_PORT);
 		
@@ -48,7 +52,18 @@ class SphinxGenererAutocomplete extends Command {
 		// On récupère le chemin du premier fichier SPI trouvé dans le dossier Sphinx
 		if ($spis = glob($dossier_data . $index . '.*.spi') and $spi = $spis[0]) {
 			if (function_exists('passthru')) {
-				passthru("indextool --dumpdict $spi > {$dossier_tmp}{$index}.dict.txt");
+				passthru("indextool --dumpdict {$spi} > {$dict_tmp}");
+				
+				// Si on a bien le dictionnaire voulu à la fin
+				if (file_exists($dict_tmp)) {
+					$output->writeln("<info>Dictionnaire correctement créé dans {$dict_tmp}</info>");
+					
+					$this->analyser_dictionnaire(
+						$dossier_autocomplete,
+						$dict_tmp,
+						find_in_path('autocomplete/exceptions.txt')
+					);
+				}
 			}
 			else {
 				$output->writeln('<error>Votre installation de PHP doit pouvoir exécuter des commandes externes avec la fonction passthru().</error>');
@@ -57,5 +72,54 @@ class SphinxGenererAutocomplete extends Command {
 		else {
 			$output->writeln("<error>Pas trouvé de fichier data/$index.*.spi</error>");
 		}
+	}
+	
+	protected function analyser_dictionnaire($rep, $dict, $exceptions) {
+		include_spip('inc/charsets');
+		$ab = '';
+		
+		$exceptions = array_map('trim', file($exceptions));
+		
+		foreach(file($dict) as $k => $l) {
+			list($keyword,$docs,$hits,$offset) = explode(',', $l);
+			
+			// Quand le tableau mots-fréquences commence, on démarre
+			if ($keyword === 'keyword' and $docs === 'docs') {
+				$start = true;
+			}
+			
+			if ($start AND ord($l) != 2) {
+				if (!in_array($keyword, $exceptions)) {
+					$_ab = strtolower(translitteration(mb_substr($keyword, 0, 2)));
+					if ($_ab !== $ab) {
+						$this->save($ab, $mots, $rep);
+						$ab = $_ab;
+						$mots = [];
+					}
+					// conserver les mots ayant un nombre suffisant d'occurrences
+					if ($hits >= 3) {
+						$mots[$keyword] = $hits;
+					}
+				}
+			}
+		}
+		
+		$this->save($ab, $mots, $rep);
+	}
+	
+	// enregistrer la liste ab - triée par hits décroissants
+	protected function save($ab, $mots, $rep) {
+		if (empty($mots)) return;
+		if (!preg_match(',[a-z][a-z],S', $ab)) return;
+		
+		arsort($mots);
+		$dump = join("\n", array_keys($mots));
+		$a = mb_substr($ab, 0,1);
+		if (!is_dir($rep.'/'.$a)) mkdir ($rep.'/'.$a);
+		$res = ($fp = fopen($rep.'/'.$a.'/'.$ab.'.txt', 'w'))
+		&& fwrite ($fp, $dump)
+		&& fclose ($fp);
+		spip_log("autocomplete dict $ab " . count($mots) . " ($res)");
+		return $res;
 	}
 }
