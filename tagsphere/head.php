7508f4db796ff9c29324ef09d370611e95bdcd60
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-12-10 22:50:13 +0000

    Plugin compatible SPIP 3.1, up de z.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@93527 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- tagsphere/head.php
+++ tagsphere/head.php
@@ -1,8 +1,8 @@
 <?php
-function tagsphere_insert_head($flux)
-{
+
+function tagsphere_insert_head($flux) {
+	// Voir pour l'utilisation de https://github.com/dynamicguy/tagcloud/ qui semble être son nouveau nom.
 	$flux .= '<script type="text/javascript" src="'._DIR_PLUGIN_TAGSPHERE.'js/jquery.tagSphere.js"></script>';
 
-return $flux;
+	return $flux;
 }
-?>
