ce1e2420c159058604a67f1cfa6afdfc974e4617
alexandra.guiderdoni@gmail.com <alexandra.guiderdoni@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-12-11 10:47:53 +0000

    Début de traduction pour l’allemand de cookiebar et pour initier les traductions sur trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@93535 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ cookiebar/trunk/lang/cookiebar_de.php
@@ -0,0 +1,17 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/cookiebar/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	
+	// M
+	'message_accepttext' => 'Ja, ich akzeptiere es',
+	'message_declinetext' => 'Cookies deaktivieren',
+	'message_message' => 'Mit Ihrem Besuch auf dieser Website akzeptieren Sie den Einsatz von Cookies, mit deren Hilfe wir Statistiken über die Nutzung der Website erstellen können',
+	'message_policytext' => 'Datenschutzerklärung '
+
+	
+);
+
+?>
