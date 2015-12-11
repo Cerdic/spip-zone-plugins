8703fecf4cd36504ff84c11fc24721186a84117f
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-12-11 19:37:41 +0000

    - Compatibilité SPIP 3.1 ; - Ajout de la sécu sur les fichiers php ; - Une chouille d'indentation ; - Up de z
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@93569 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- transaction/transaction_pipelines.php
+++ transaction/transaction_pipelines.php
@@ -1,6 +1,11 @@
 <?php
-      function transaction_insert_head($texte){
-          $texte .= '<link rel="stylesheet" type="text/css" href="'.find_in_path('css/transaction.css').'" media="all" />'."\n";
-          return $texte;
-      }
-?>
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+function transaction_insert_head($texte) {
+	$texte .= '<link rel="stylesheet" type="text/css" href="'.find_in_path('css/transaction.css').'" media="all" />'."\n";
+
+	return $texte;
+}
