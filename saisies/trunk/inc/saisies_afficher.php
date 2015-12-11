825d89a74392be9cfb56160846fce81722c20941
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-12-11 20:27:08 +0000

    Revert de r93568 pour dissociation indentation et correctif (nicod_)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@93575 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- saisies/trunk/inc/saisies_afficher.php
+++ saisies/trunk/inc/saisies_afficher.php
@@ -1,15 +1,13 @@
 <?php
 
 /**
- * Gestion de l'affichage des saisies.
+ * Gestion de l'affichage des saisies
  *
  * @return SPIP\Saisies\Afficher
- **/
+**/
 
 // Sécurité
-if (!defined('_ECRIRE_INC_VERSION')) {
-	return;
-}
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 /**
  * Indique si une saisie peut être affichée.
@@ -23,18 +21,18 @@ if (!defined('_ECRIRE_INC_VERSION')) {
  *         ou dans un de ses enfants (fieldsets)
  *
  * @param array $champ
- *                                 Tableau de description de la saisie
+ *     Tableau de description de la saisie
  * @param array $env
- *                                 Environnement transmis à la saisie, certainement l'environnement du formulaire
- * @param bool  $utiliser_editable
- *                                 - false pour juste tester le cas -1
+ *     Environnement transmis à la saisie, certainement l'environnement du formulaire
+ * @param bool $utiliser_editable
+ *     - false pour juste tester le cas -1
  *
  * @return bool
- *              Retourne un booléen indiquant l'état éditable ou pas :
- *              - true si la saisie est éditable (peut être affichée)
- *              - false sinon
+ *     Retourne un booléen indiquant l'état éditable ou pas :
+ *     - true si la saisie est éditable (peut être affichée)
+ *     - false sinon
  */
-function saisie_editable($champ, $env, $utiliser_editable = true) {
+function saisie_editable($champ, $env, $utiliser_editable=true) {
 	if ($utiliser_editable) {
 		// si le champ n'est pas éditable, on sort.
 		if (!isset($champ['editable'])) {
@@ -56,12 +54,12 @@ function saisie_editable($champ, $env, $utiliser_editable = true) {
 		// si on a le name dans l'environnement, on le teste
 		$nom = $champ['options']['nom'];
 		if (isset($env[$nom])) {
-			return $env[$nom] ? true : false;
+			return $env[$nom] ? true : false ;
 		}
 	}
 	// sinon, si on a des sous saisies
 	if (isset($champ['saisies']) and is_array($champ['saisies'])) {
-		foreach ($champ['saisies'] as $saisie) {
+		foreach($champ['saisies'] as $saisie) {
 			if (saisie_editable($saisie, $env, false)) {
 				return true;
 			}
@@ -73,33 +71,31 @@ function saisie_editable($champ, $env, $utiliser_editable = true) {
 }
 
 /**
- * Génère une saisie à partir d'un tableau la décrivant et de l'environnement.
+ * Génère une saisie à partir d'un tableau la décrivant et de l'environnement
  *
  * @param array $champ
- *                     Description de la saisie.
- *                     Le tableau doit être de la forme suivante :
- *                     array(
- *                     'saisie' => 'input',
- *                     'options' => array(
- *                     'nom' => 'le_name',
- *                     'label' => 'Un titre plus joli',
- *                     'obligatoire' => 'oui',
- *                     'explication' => 'Remplissez ce champ en utilisant votre clavier.'
- *                     )
- *                     )
+ *     Description de la saisie.
+ *     Le tableau doit être de la forme suivante :
+ *     array(
+ *     		'saisie' => 'input',
+ *     		'options' => array(
+ *     			'nom' => 'le_name',
+ *     			'label' => 'Un titre plus joli',
+ *     			'obligatoire' => 'oui',
+ *     			'explication' => 'Remplissez ce champ en utilisant votre clavier.'
+ *     		)
+ *     )
  * @param array $env
- *                     Environnement du formulaire
- *                     Permet de savoir les valeurs actuelles des contenus des saisies,
- *                     les erreurs eventuelles présentes...
- *
+ *     Environnement du formulaire
+ *     Permet de savoir les valeurs actuelles des contenus des saisies,
+ *     les erreurs eventuelles présentes...
  * @return string
- *                Code HTML des saisies de formulaire
+ *     Code HTML des saisies de formulaire
  */
-function saisies_generer_html($champ, $env = array()) {
+function saisies_generer_html($champ, $env=array()){
 	// Si le parametre n'est pas bon, on genere du vide
-	if (!is_array($champ)) {
+	if (!is_array($champ))
 		return '';
-	}
 
 	// Si la saisie n'est pas editable, on sort aussi.
 	if (!saisie_editable($champ, $env)) {
@@ -116,8 +112,8 @@ function saisies_generer_html($champ, $env = array()) {
 	}
 
 	// Peut-être des transformations à faire sur les options textuelles
-	$options = isset($champ['options']) ? $champ['options'] : array();
-	foreach ($options as $option => $valeur) {
+	$options = $champ['options'];
+	foreach ($options as $option => $valeur){
 		if ($option == 'datas') {
 			// exploser une chaine datas en tableau (applique _T_ou_typo sur chaque valeur)
 			$options[$option] = saisies_chaine2tableau($valeur);
@@ -135,7 +131,7 @@ function saisies_generer_html($champ, $env = array()) {
 	$contexte = array_merge($contexte, $options);
 
 	// Si env est définie dans les options ou qu'il y a des enfants, on ajoute tout l'environnement
-	if (isset($contexte['env']) or (isset($champ['saisies']) and is_array($champ['saisies']))) {
+	if (isset($contexte['env']) or (isset($champ['saisies']) AND is_array($champ['saisies']))) {
 		unset($contexte['env']);
 
 		// on sauve l'ancien environnement
@@ -149,7 +145,7 @@ function saisies_generer_html($champ, $env = array()) {
 		$saisies_disponibles = saisies_lister_disponibles();
 		if (isset($saisies_disponibles[$contexte['type_saisie']]) and is_array($saisies_disponibles[$contexte['type_saisie']]['options'])) {
 			$options_a_supprimer = saisies_lister_champs($saisies_disponibles[$contexte['type_saisie']]['options']);
-			foreach ($options_a_supprimer as $option_a_supprimer) {
+			foreach ($options_a_supprimer as $option_a_supprimer){
 				unset($env[$option_a_supprimer]);
 			}
 		}
@@ -157,30 +153,29 @@ function saisies_generer_html($champ, $env = array()) {
 		$contexte = array_merge($env, $contexte);
 	}
 	// Sinon on ne sélectionne que quelques éléments importants
-	else {
+	else{
 		// On récupère la liste des erreurs
 		$contexte['erreurs'] = $env['erreurs'];
 		// On récupère la langue de l'objet si existante
-		if (isset($env['langue'])) {
+		if(isset($env['langue']))
 			$contexte['langue'] = $env['langue'];
-		}
 		// On ajoute toujours le bon self
 		$contexte['self'] = self();
 	}
 
 	// Dans tous les cas on récupère de l'environnement la valeur actuelle du champ
 	// Si le nom du champ est un tableau indexé, il faut parser !
-	if (isset($contexte['nom']) and preg_match('/([\w]+)((\[[\w]+\])+)/', $contexte['nom'], $separe)) {
+	if (preg_match('/([\w]+)((\[[\w]+\])+)/', $contexte['nom'], $separe)){
 		$contexte['valeur'] = $env[$separe[1]];
 		preg_match_all('/\[([\w]+)\]/', $separe[2], $index);
 		// On va chercher au fond du tableau
-		foreach ($index[1] as $cle) {
+		foreach($index[1] as $cle){
 			$contexte['valeur'] = isset($contexte['valeur'][$cle]) ? $contexte['valeur'][$cle] : null;
 		}
 	}
 	// Sinon la valeur est juste celle du nom
 	else {
-		$contexte['valeur'] = (isset($contexte['nom']) and isset($env[$contexte['nom']]) ? $env[$contexte['nom']] : null);
+		$contexte['valeur'] = (isset($env[$contexte['nom']]) ? $env[$contexte['nom']] : null);
 	}
 
 	// Si ya des enfants on les remonte dans le contexte
@@ -196,25 +191,22 @@ function saisies_generer_html($champ, $env = array()) {
 }
 
 /**
- * Génère une vue d'une saisie à partir d'un tableau la décrivant.
+ * Génère une vue d'une saisie à partir d'un tableau la décrivant
  *
  * @see saisies_generer_html()
- *
  * @param array $saisie
- *                               Tableau de description d'une saisie
+ *     Tableau de description d'une saisie
  * @param array $env
- *                               L'environnement, contenant normalement la réponse à la saisie
+ *     L'environnement, contenant normalement la réponse à la saisie
  * @param array $env_obligatoire
- *                               ???
- *
+ *     ???
  * @return string
- *                Code HTML de la vue de la saisie
+ *     Code HTML de la vue de la saisie
  */
-function saisies_generer_vue($saisie, $env = array(), $env_obligatoire = array()) {
+function saisies_generer_vue($saisie, $env=array(), $env_obligatoire=array()){
 	// Si le paramètre n'est pas bon, on génère du vide
-	if (!is_array($saisie)) {
+	if (!is_array($saisie))
 		return '';
-	}
 
 	$contexte = array();
 
@@ -223,7 +215,7 @@ function saisies_generer_vue($saisie, $env = array(), $env_obligatoire = array()
 
 	// Peut-être des transformations à faire sur les options textuelles
 	$options = $saisie['options'];
-	foreach ($options as $option => $valeur) {
+	foreach ($options as $option => $valeur){
 		if ($option == 'datas') {
 			// exploser une chaine datas en tableau (applique _T_ou_typo sur chaque valeur)
 			$options[$option] = saisies_chaine2tableau($valeur);
@@ -236,7 +228,7 @@ function saisies_generer_vue($saisie, $env = array(), $env_obligatoire = array()
 	$contexte = array_merge($contexte, $options);
 
 	// Si env est définie dans les options ou qu'il y a des enfants, on ajoute tout l'environnement
-	if (isset($contexte['env']) or (isset($saisie['saisies']) and is_array($saisie['saisies']))) {
+	if (isset($contexte['env']) or (isset($saisie['saisies']) AND is_array($saisie['saisies']))){
 		unset($contexte['env']);
 
 		// on sauve l'ancien environnement
@@ -249,7 +241,8 @@ function saisies_generer_vue($saisie, $env = array(), $env_obligatoire = array()
 		$saisies_disponibles = saisies_lister_disponibles();
 
 		if (isset($saisies_disponibles[$contexte['type_saisie']]['options'])
-			and is_array($saisies_disponibles[$contexte['type_saisie']]['options'])) {
+			and is_array($saisies_disponibles[$contexte['type_saisie']]['options']))
+		{
 			$options_a_supprimer = saisies_lister_champs($saisies_disponibles[$contexte['type_saisie']]['options']);
 			foreach ($options_a_supprimer as $option_a_supprimer) {
 				unset($env[$option_a_supprimer]);
@@ -262,15 +255,15 @@ function saisies_generer_vue($saisie, $env = array(), $env_obligatoire = array()
 	// Dans tous les cas on récupère de l'environnement la valeur actuelle du champ
 
 	// On regarde en priorité s'il y a un tableau listant toutes les valeurs
-	if ($env['valeurs'] and is_array($env['valeurs']) and isset($env['valeurs'][$contexte['nom']])) {
+	if ($env['valeurs'] and is_array($env['valeurs']) and isset($env['valeurs'][$contexte['nom']])){
 		$contexte['valeur'] = $env['valeurs'][$contexte['nom']];
 	}
 	// Si le nom du champ est un tableau indexé, il faut parser !
-	elseif (preg_match('/([\w]+)((\[[\w]+\])+)/', $contexte['nom'], $separe)) {
+	elseif (preg_match('/([\w]+)((\[[\w]+\])+)/', $contexte['nom'], $separe)){
 		$contexte['valeur'] = $env[$separe[1]];
 		preg_match_all('/\[([\w]+)\]/', $separe[2], $index);
 		// On va chercher au fond du tableau
-		foreach ($index[1] as $cle) {
+		foreach($index[1] as $cle){
 			$contexte['valeur'] = $contexte['valeur'][$cle];
 		}
 	}
@@ -281,9 +274,8 @@ function saisies_generer_vue($saisie, $env = array(), $env_obligatoire = array()
 	}
 
 	// Si ya des enfants on les remonte dans le contexte
-	if (isset($saisie['saisies']) and is_array($saisie['saisies'])) {
+	if (isset($saisie['saisies']) AND is_array($saisie['saisies']))
 		$contexte['saisies'] = $saisie['saisies'];
-	}
 
 	if (is_array($env_obligatoire)) {
 		$contexte = array_merge($contexte, $env_obligatoire);
@@ -300,27 +292,26 @@ function saisies_generer_vue($saisie, $env = array(), $env_obligatoire = array()
  * Génère, à partir d'un tableau de saisie le code javascript ajouté à la fin de #GENERER_SAISIES
  * pour produire un affichage conditionnel des saisies ayant une option afficher_si ou afficher_si_remplissage.
  *
- * @param array  $saisies
- *                        Tableau de descriptions des saisies
+ * @param array $saisies
+ *     Tableau de descriptions des saisies
  * @param string $id_form
- *                        Identifiant unique pour le formulaire
- *
+ *     Identifiant unique pour le formulaire
  * @return text
- *              Code javascript
+ *     Code javascript
  */
-function saisies_generer_js_afficher_si($saisies, $id_form) {
+function saisies_generer_js_afficher_si($saisies,$id_form){
 	$i = 0;
-	$saisies = saisies_lister_par_nom($saisies, true);
+	$saisies = saisies_lister_par_nom($saisies,true);
 	$code = '';
 	$code .= '(function($){';
 	$code .= '$(document).ready(function(){chargement=true;';
-	$code .= 'verifier_saisies_'.$id_form." = function(form){\n";
-	foreach ($saisies as $saisie) {
-		// on utilise comme selecteur l'identifiant de saisie en priorite s'il est connu
+		$code .= 'verifier_saisies_'.$id_form." = function(form){\n";
+				foreach ($saisies as $saisie) {
+					// on utilise comme selecteur l'identifiant de saisie en priorite s'il est connu
 					// parce que conteneur_class = 'tableau[nom][option]' ne fonctionne evidement pas
 					// lorsque le name est un tableau
 					if (isset($saisie['options']['afficher_si']) or isset($saisie['options']['afficher_si_remplissage'])) {
-						++$i;
+						$i++;
 						// retrouver la classe css probable
 						switch ($saisie['saisie']) {
 							case 'fieldset':
@@ -334,7 +325,7 @@ function saisies_generer_js_afficher_si($saisies, $id_form) {
 						}
 						$afficher_si = isset($saisie['options']['afficher_si']) ? $saisie['options']['afficher_si'] : '';
 						$afficher_si_remplissage = isset($saisie['options']['afficher_si_remplissage']) ? $saisie['options']['afficher_si_remplissage'] : '';
-						$condition = implode("\n", array_filter(array($afficher_si, $afficher_si_remplissage)));
+						$condition = join("\n", array_filter(array($afficher_si, $afficher_si_remplissage)));
 						// retrouver l'identifiant
 						$identifiant = '';
 						if (isset($saisie['identifiant']) and $saisie['identifiant']) {
@@ -343,11 +334,10 @@ function saisies_generer_js_afficher_si($saisies, $id_form) {
 						// On gère le cas @plugin:non_plugin@
 						preg_match_all('#@plugin:(.+)@#U', $condition, $matches);
 						foreach ($matches[1] as $plug) {
-							if (defined('_DIR_PLUGIN_'.strtoupper($plug))) {
+							if (defined('_DIR_PLUGIN_'.strtoupper($plug)))
 								$condition = preg_replace('#@plugin:'.$plug.'@#U', 'true', $condition);
-							} else {
+							else
 								$condition = preg_replace('#@plugin:'.$plug.'@#U', 'false', $condition);
-							}
 						}
 						// On gère le cas @config:plugin:meta@ suivi d'un test
 						preg_match_all('#@config:(.+):(.+)@#U', $condition, $matches);
@@ -358,7 +348,7 @@ function saisies_generer_js_afficher_si($saisies, $id_form) {
 						// On transforme en une condition valide
 						preg_match_all('#@(.+)@#U', $condition, $matches);
 						foreach ($matches[1] as $nom) {
-							switch ($saisies[$nom]['saisie']) {
+							switch($saisies[$nom]['saisie']) {
 								case 'radio':
 								case 'oui_non':
 								case 'true_false':
@@ -371,7 +361,7 @@ function saisies_generer_js_afficher_si($saisies, $id_form) {
 									preg_match_all('#@(.+)@\s*==\s*"(.*)"$#U', $condition, $matches2);
 									foreach ($matches2[2] as $value) {
 										$condition = preg_replace('#@'.preg_quote($nom).'@#U', '($(form).find(".checkbox[name=\''.$nom.'[]\'][value='.$value.']").is(":checked") ? $(form).find(".checkbox[name=\''.$nom.'[]\'][value='.$value.']").val() : "")', $condition);
-									}
+										}
 									break;
 								default:
 									$condition = preg_replace('#@'.preg_quote($nom).'@#U', '$(form).find("[name=\''.$nom.'\']").val()', $condition);
@@ -385,14 +375,13 @@ function saisies_generer_js_afficher_si($saisies, $id_form) {
 						$code .= "\tif (".$condition.') {$(form).find("'.$sel.'").show(400);} '."\n\t";
 						$code .= 'else {if (chargement==true) {$(form).find("'.$sel.'").hide(400).css("display","none");} else {$(form).find("'.$sel.'").hide(400);};} '."\n";
 					}
-	}
-	$code .= '};';
-	$code .= '$("#afficher_si_'.$id_form.'").parents("form").each(function(){verifier_saisies_'.$id_form.'(this);});';
-	$code .= '$("#afficher_si_'.$id_form.'").parents("form").change(function(){verifier_saisies_'.$id_form.'(this);});';
+				}
+		$code .= "};";
+		$code .= '$("#afficher_si_'.$id_form.'").parents("form").each(function(){verifier_saisies_'.$id_form.'(this);});';
+		$code .= '$("#afficher_si_'.$id_form.'").parents("form").change(function(){verifier_saisies_'.$id_form.'(this);});';
 	$code .= 'chargement=false;})';
 	$code .= '})(jQuery);';
-
-	return $i > 0 ? $code : '';
+	return $i>0 ? $code : '';
 }
 
 /**
@@ -403,16 +392,15 @@ function saisies_generer_js_afficher_si($saisies, $id_form) {
  * À ce moment là, les saisies non affichées sont retirées de _request
  * (on passe leur valeur à NULL).
  *
- * @param array      $saisies
- *                            Tableau de descriptions de saisies
+ * @param array $saisies
+ *     Tableau de descriptions de saisies
  * @param array|null $env
- *                            Tableau d'environnement transmis dans inclure/voi_saisies.html,
- *                            NULL si on doit rechercher dans _request (pour saisies_verifier()).
- *
+ *     Tableau d'environnement transmis dans inclure/voi_saisies.html,
+ *     NULL si on doit rechercher dans _request (pour saisies_verifier()).
  * @return array
- *               Tableau de descriptions de saisies
+ *     Tableau de descriptions de saisies
  */
-function saisies_verifier_afficher_si($saisies, $env = null) {
+function saisies_verifier_afficher_si($saisies, $env=NULL) {
 	// eviter une erreur par maladresse d'appel :)
 	if (!is_array($saisies)) {
 		return array();
@@ -423,11 +411,10 @@ function saisies_verifier_afficher_si($saisies, $env = null) {
 			// On gère le cas @plugin:non_plugin@
 			preg_match_all('#@plugin:(.+)@#U', $condition, $matches);
 			foreach ($matches[1] as $plug) {
-				if (defined('_DIR_PLUGIN_'.strtoupper($plug))) {
+				if (defined('_DIR_PLUGIN_'.strtoupper($plug)))
 					$condition = preg_replace('#@plugin:'.$plug.'@#U', 'true', $condition);
-				} else {
+				else
 					$condition = preg_replace('#@plugin:'.$plug.'@#U', 'false', $condition);
-				}
 			}
 			// On gère le cas @config:plugin:meta@ suivi d'un test
 			preg_match_all('#@config:(.+):(.+)@#U', $condition, $matches);
@@ -436,23 +423,18 @@ function saisies_verifier_afficher_si($saisies, $env = null) {
 				$condition = preg_replace('#@config:'.$plugin.':'.$matches[2][0].'@#U', '"'.$config[$matches[2][0]].'"', $condition);
 			}
 			// On transforme en une condition valide
-			if (is_null($env)) {
+			if (is_null($env))
 				$condition = preg_replace('#@(.+)@#U', '_request(\'$1\')', $condition);
-			} else {
+			else
 				$condition = preg_replace('#@(.+)@#U', '$env["valeurs"][\'$1\']', $condition);
-			}
 			eval('$ok = '.$condition.';');
 			if (!$ok) {
 				unset($saisies[$cle]);
-				if (is_null($env)) {
-					set_request($saisie['options']['nom'], null);
-				}
+				if (is_null($env)) set_request($saisie['options']['nom'],NULL);
 			}
 		}
-		if (isset($saisies[$cle]['saisies'])) { // S'il s'agit d'un fieldset ou equivalent, verifier les sous-saisies
+		if (isset($saisies[$cle]['saisies'])) // S'il s'agit d'un fieldset ou equivalent, verifier les sous-saisies
 			$saisies[$cle]['saisies'] = saisies_verifier_afficher_si($saisies[$cle]['saisies'], $env);
-		}
 	}
-
 	return $saisies;
 }
