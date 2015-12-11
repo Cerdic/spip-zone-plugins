9740cd02d286230cb35d3377d99066d21c20f9a9
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-12-11 15:31:27 +0000

    Indentation / PHPdoc
    Petit incrément de version
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@93540 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- cookiebar/trunk/cookiebar_pipelines.php
+++ cookiebar/trunk/cookiebar_pipelines.php
@@ -1,48 +1,48 @@
 <?php
-
 /**
  * Plugin Cookie bar pour Spip 3.0.
+ * 
+ * @licence    GNU/GPL
+ * @package    SPIP\Cookiebar\Pipelines
  */
-if (!defined('_ECRIRE_INC_VERSION')) {
-    return;
-}
+if (!defined('_ECRIRE_INC_VERSION'))
+	return;
 
 /**
- * Inserer la CSS de cookie bar.
+ * Inserer la CSS de cookie bar dans le head public.
  *
- * @param $flux
- *
- * @return mixed
+ * @pipeline insert_head_css
+ * 
+ * @param string $flux
+ * 	Le contenu de la balise #INSERT_HEAD_CSS
+ * @return string
  */
-function cookiebar_insert_head_css($flux)
-{
-    if (defined('_COOKIEBAR_CSS_NON')) {
-        return $flux;
-    }
+function cookiebar_insert_head_css($flux){
+	if(defined('_COOKIEBAR_CSS_NON'))
+		return $flux;
 
-    $flux .= '<link rel="stylesheet" type="text/css" href="'.find_in_path('css/jquery.cookiebar.css').'" />';
+	$flux .= '<link rel="stylesheet" type="text/css" href="'.find_in_path('css/jquery.cookiebar.css').'" />';
 
-    return $flux;
+	return $flux;
 }
 
 /**
  * Inserer le javascript de cookiebar.
  *
- * @param $flux
- *
+ * @pipeline insert_head
+ * 
+ * @param string $flux
+ * 	Le contenu de la balise #INSERT_HEAD
  * @return mixed
  */
-function cookiebar_insert_head($flux)
-{
-    //$js_cookiebar = parametre_url(generer_url_public('jquery.cookiebar.js'), 'lang', $lang);
-    include_spip('inc/filtres');
-    if (!$lang) {
-        $lang = (isset($GLOBALS['spip_lang']) ? $GLOBALS['spip_lang'] : 'fr');
-    }
-    $js_cookiebar = produire_fond_statique('jquery.cookiebar.js', array('lang' => $lang));
+function cookiebar_insert_head($flux){
+	include_spip('inc/filtres');
+	
+	$lang = (isset($GLOBALS['spip_lang']) ? $GLOBALS['spip_lang'] : 'fr');
+	$js_cookiebar = produire_fond_statique('jquery.cookiebar.js', array('lang' => $lang));
 
-    $flux .= "<script type='text/javascript' src='$js_cookiebar'></script>\n"
-    ."<script type='text/javascript' src='".find_in_path('js/jquery.cookiebar.call.js')."'></script>";
+	$flux .= "<script type='text/javascript' src='$js_cookiebar'></script>\n";
+	$flux .= "<script type='text/javascript' src='".find_in_path('js/jquery.cookiebar.call.js')."'></script>";
 
-    return $flux;
+	return $flux;
 }
