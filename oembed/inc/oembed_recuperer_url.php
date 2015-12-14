be12c0bd867524c39fc85b4594acfe840945d9b1
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-12-14 14:59:34 +0000

    Utiliser le proxy configuré dans SPIP pour récupérer les URLs embed http://contrib.spip.net/oEmbed-4407#forum484935 + renommage variable plus clair et PHPDoc
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@93685 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- oembed/inc/oembed_recuperer_url.php
+++ oembed/inc/oembed_recuperer_url.php
@@ -7,17 +7,32 @@
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
-
-function inc_oembed_recuperer_url($data_url,$url,$format){
+/**
+ * Recuperer une URL oembed, si possible via curl et IPv4 pour contourner le bug de Youtube sur les IPv6
+ *
+ * @param string $oembed_url
+ * @param string $url
+ * @param string $format
+ * @return bool|mixed|string
+ */
+function inc_oembed_recuperer_url($oembed_url,$url,$format){
 	$data = false;
 
 	// on recupere le contenu de la page
 	// si possible via curl en IPv4 car youtube bug en IPv6
 	if (function_exists('curl_init')){
-		spip_log('Requete oembed (curl) pour '.$url.' : '.$data_url,'oembed.'._LOG_DEBUG);
+		spip_log('Requete oembed (curl) pour '.$url.' : '.$oembed_url,'oembed.'._LOG_DEBUG);
 		$c = curl_init();
 		curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($c, CURLOPT_URL, $data_url);
+		curl_setopt($c, CURLOPT_URL, $oembed_url);
+
+		if (isset($GLOBALS['meta']["http_proxy"]) AND $GLOBALS['meta']["http_proxy"]){
+		  curl_setopt($c, CURLOPT_PROXY, $GLOBALS['meta']["http_proxy"]);
+			if (isset($GLOBALS['meta']["http_noproxy"])){
+				curl_setopt($c, CURLOPT_NOPROXY, $GLOBALS['meta']["http_proxy"]);
+			}
+		}
+
 		// the real trick for Youtube :
 		// http://stackoverflow.com/questions/26089067/youtube-oembed-api-302-then-503-errors
 		curl_setopt($c, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
@@ -26,9 +41,9 @@ function inc_oembed_recuperer_url($data_url,$url,$format){
 		curl_close($c);
 	}
 	else {
-		spip_log('Requete oembed (recuperer_page) pour '.$url.' : '.$data_url,'oembed.'._LOG_DEBUG);
+		spip_log('Requete oembed (recuperer_page) pour '.$url.' : '.$oembed_url,'oembed.'._LOG_DEBUG);
 		include_spip('inc/distant');
-		$data = recuperer_page($data_url);
+		$data = recuperer_page($oembed_url);
 	}
 
 	spip_log('infos oembed brutes pour '.$url.' : '.($data?$data:'ECHEC'),'oembed.'._LOG_DEBUG);
@@ -37,7 +52,7 @@ function inc_oembed_recuperer_url($data_url,$url,$format){
 			$data = json_decode($data,true);
 		// TODO : format xml
 		//if ($format == 'xml')
-		//	$cache[$data_url] = false;
+		//	$cache[$oembed_url] = false;
 	}
 
 	return $data;
