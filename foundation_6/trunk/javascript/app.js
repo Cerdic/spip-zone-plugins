750560967b3f9f4ed88e17eb87db6443cbb03efa
p@henix.be <p@henix.be@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-12-17 23:22:49 +0000

    Ajouter le support ajax pour les reveal modal
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@93785 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ foundation_6/trunk/javascript/app.js
@@ -0,0 +1,28 @@
+/**
+ * Fichier javascript de foundation améliorer par mes soins
+ */
+
+// lancer foundation
+$(document).foundation();
+
+// Support Ajax pour les reveal-modal
+// Cela passe par une class .reveal-ajax
+$(".reveal-ajax").on('click', function (e) {
+
+	// bloquer l'événement click
+	e.preventDefault();
+
+	// Récupérer le lien ciblé par la requête ajax
+	var href = this.href;
+	// On a besoin de savoir sur quel modal il faut agir
+	var modal = $(this).data("toggle");
+
+	// Dans le cas ou la modal est vide sur toggle,
+	// on vérifie que ce n'est pas data-open qui est utilisé
+	if (!modal) {
+		modal = $(this).data("open");
+	}
+
+	// On charge la page dans la reveal
+	$("#"+modal).load(href);
+});
