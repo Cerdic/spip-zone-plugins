e24a9ad9b79ac8b4c025addee686609cd1360042
p@henix.be <p@henix.be@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-12-17 23:29:49 +0000

    Indentation
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@93805 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- foundation_6/trunk/foundation_6_fonctions.php
+++ foundation_6/trunk/foundation_6_fonctions.php
@@ -15,36 +15,36 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
  * @return string        HTML avec les iFrames modifiée pour être responsive.
  */
 function filtre_iframe_responsive($texte) {
-  include_spip('inc/foundation');
-  // On détecte tout les iFrames et on les rends responsives.
-  return preg_replace_callback('/<iframe(.+)><\/iframe>/', 'responsive', $texte);
+	include_spip('inc/foundation');
+	// On détecte tout les iFrames et on les rends responsives.
+	return preg_replace_callback('/<iframe(.+)><\/iframe>/', 'responsive', $texte);
 }
 
 /*
-*   Cette balise va permettre de rendre le squelette compatible avec toutes les versions de Foundation.
-*   La syntaxe est la suivante:
-*
-*   #COLONNES{nombre,type}
-*   nombre: le nombre de colonne foundation
-*   (optionnel) type: Dans le cas des version utilisant une syntaxe avec prefix, on lui passe le type (défaut: large)
-*/
+ *   Cette balise va permettre de rendre le squelette compatible avec toutes les versions de Foundation.
+ *   La syntaxe est la suivante:
+ *
+ *   #COLONNES{nombre,type}
+ *   nombre: le nombre de colonne foundation
+ *   (optionnel) type: Dans le cas des version utilisant une syntaxe avec prefix, on lui passe le type (défaut: large)
+ */
 
 // On va cherche trouver_syntaxe_foundation dans le inc
 include_spip('inc/foundation');
 function balise_COLONNES_dist($p) {
-  // On récupère les paramètres de la balise.
-  $nombre_colonnes = interprete_argument_balise(1, $p);
-  $type = interprete_argument_balise(2, $p);
+	// On récupère les paramètres de la balise.
+	$nombre_colonnes = interprete_argument_balise(1, $p);
+	$type = interprete_argument_balise(2, $p);
 
-  // On met une valeur par défaut à type.
-  if (!$type) {
-	  $type = "'large'";
-  }
+	// On met une valeur par défaut à type.
+	if (!$type) {
+		$type = "'large'";
+	}
 
-  // On calcule la class
-  $p->code = "class_grid_foundation($nombre_colonnes, $type).'columns'";
-  $p->interdire_scripts = false;
-  return $p;
+	// On calcule la class
+	$p->code = "class_grid_foundation($nombre_colonnes, $type).'columns'";
+	$p->interdire_scripts = false;
+	return $p;
 }
 
 /**
@@ -56,7 +56,7 @@ function balise_F_BOUTON_ACTION_dist($p){
 	for ($k=1;$k<=6;$k++){
 		$_a = interprete_argument_balise($k,$p);
 		if (!$_a) $_a="''";
-	  $args[] = $_a;
+		$args[] = $_a;
 	}
 	// supprimer les args vides
 	while(end($args)=="''" AND count($args)>2) {
@@ -76,17 +76,17 @@ function balise_F_BOUTON_ACTION_dist($p){
  * compatibilité avec les class button de foundation
  */
 function filtre_f_bouton_action_dist($libelle, $url, $class="", $confirm="", $title="", $callback=""){
-  if ($confirm) {
-    $confirm = "confirm(\"" . attribut_html($confirm) . "\")";
-    if ($callback)
-      $callback = "$confirm?($callback):false";
-    else
-      $callback = $confirm;
-  }
-  $onclick = $callback?" onclick='return ".addcslashes($callback,"'")."'":"";
-  $title = $title ? " title='$title'" : "";
-  return "<form class='bouton_action_post' method='post' action='$url'><div>".form_hidden($url)
-    ."<button type='submit' class='submit $class'$title$onclick>$libelle</button></div></form>";
+	if ($confirm) {
+		$confirm = "confirm(\"" . attribut_html($confirm) . "\")";
+		if ($callback)
+			$callback = "$confirm?($callback):false";
+		else
+			$callback = $confirm;
+	}
+	$onclick = $callback?" onclick='return ".addcslashes($callback,"'")."'":"";
+	$title = $title ? " title='$title'" : "";
+	return "<form class='bouton_action_post' method='post' action='$url'><div>".form_hidden($url)
+		     ."<button type='submit' class='submit $class'$title$onclick>$libelle</button></div></form>";
 }
 
 
@@ -100,19 +100,19 @@ function filtre_f_bouton_action_dist($libelle, $url, $class="", $confirm="", $ti
  */
 function filtre_etoile_foundation_dist ($nombre) {
 
-    $config = lire_config('foundation');
+	$config = lire_config('foundation');
 
-    if (!$config['foundation-icons']) {
-        return '<span>Les icones foundation ne sont pas activée !</span>';
-    }
+	if (!$config['foundation-icons']) {
+		return '<span>Les icones foundation ne sont pas activée !</span>';
+	}
 
-    $etoile = '<span class="foundation_etoile">';
-    for ($i=0;$i<$nombre; $i++) {
-        $etoile .= '<span class="fi-star"></span>';
-    }
-    $etoile .= '</span>';
+	$etoile = '<span class="foundation_etoile">';
+	for ($i=0;$i<$nombre; $i++) {
+		$etoile .= '<span class="fi-star"></span>';
+	}
+	$etoile .= '</span>';
 
-    return $etoile;
+	return $etoile;
 }
 
 
@@ -125,12 +125,12 @@ if (!function_exists('balise_LIRE_CONSTANTE_dist')) {
 	 * @return mixed
 	 */
 	function balise_LIRE_CONSTANTE_dist($p) {
-        $constante = interprete_argument_balise(1,$p);
+		$constante = interprete_argument_balise(1,$p);
 
-        $p->code = "constant($constante)";
+		$p->code = "constant($constante)";
 
-        $p->interdire_scripts = false;
+		$p->interdire_scripts = false;
 
-        return $p;
-    }
-}
+		return $p;
+	}
+		}
