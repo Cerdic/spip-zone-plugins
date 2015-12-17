8cf648402fe09c3932b03b070ffdba825b249bf6
p@henix.be <p@henix.be@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-12-17 23:28:47 +0000

    indentation
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@93802 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- foundation_6/trunk/formulaires/configurer_foundation_6.php
+++ foundation_6/trunk/formulaires/configurer_foundation_6.php
@@ -1,23 +1,23 @@
 <?php
 function formulaires_configurer_foundation_6_saisies() {
-  // Lire le fichier YAML qui contient la structure du formulaire.
-  include_spip('inc/yaml');
-  $saisies = array(
-    array(
-      'saisie' => 'oui_non',
-      'options' => array(
-        'nom' => 'javascript',
-        'label' => _T('foundation_6:activer_javascript'),
-        'explication' => _T('foundation_6:activer_javascript_explication')
-      )
-    )
-  );
-  return $saisies;
+	// Lire le fichier YAML qui contient la structure du formulaire.
+	include_spip('inc/yaml');
+	$saisies = array(
+		array(
+			'saisie' => 'oui_non',
+			'options' => array(
+				'nom' => 'javascript',
+				'label' => _T('foundation_6:activer_javascript'),
+				'explication' => _T('foundation_6:activer_javascript_explication')
+			)
+		)
+	);
+	return $saisies;
 }
 
 function formulaires_configurer_foundation_6_charger() {
-  // Lire la configuration de foundation
-  $config = lire_config('foundation_6');
+	// Lire la configuration de foundation
+	$config = lire_config('foundation_6');
 
-  return $config;
+	return $config;
 }
