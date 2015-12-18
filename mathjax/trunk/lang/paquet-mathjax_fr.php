2ae02f70192829588288c41ae0dcb87e3044d498
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-12-18 04:33:42 +0000

    [Salvatore] [source:_plugins_/mathjax/trunk/lang/ paquet-mathjax] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@93814 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- mathjax/trunk/lang/paquet-mathjax_fr.php
+++ mathjax/trunk/lang/paquet-mathjax_fr.php
@@ -1,15 +1,14 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/mathjax/trunk/lang/
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/mathjax/trunk/lang
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// T
-	'mathjax_description' => 'Le plugin remplace les balises &#60;math&#62; et &#60;&#47;math&#62; de SPIP.',
-	'mathjax_nom' => 'MathJax pour SPIP !',
-	'mathjax_slogan' => 'Afficher des équations, des fonctions mathématiques',
+	// M
+	'mathjax_description' => 'Le plugin remplace les balises <math> et </math> de SPIP.',
+	'mathjax_nom' => 'MathJax pour SPIP !',
+	'mathjax_slogan' => 'Afficher des équations, des fonctions mathématiques'
 );
+
 ?>
