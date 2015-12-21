dfd92e19e56858ac61e333ecc5bd051a7172a362
p@henix.be <p@henix.be@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-12-21 07:46:57 +0000

    Pouvoir définir des animations par défaut pour reveal
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@93845 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- foundation_6/trunk/foundation_6_options.php
+++ foundation_6/trunk/foundation_6_options.php
@@ -2,6 +2,16 @@
 
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
+// Utiliser la version SASS
 if (!defined('_FOUNDATION_SASS')) {
     define('_FOUNDATION_SASS', false);
+}
+
+// Animation par défaut pour les reveal modal
+if (!defined('_REVEAL_ANIMATION_IN')) {
+	define('_REVEAL_ANIMATION_IN', '');
+}
+
+if (!defined('_REVEAL_ANIMATION_OUT')) {
+	define('_REVEAL_ANIMATION_OUT', '');
 }
