498b94929e513c85b74893598088a50dae75cfe2
chankalan@free.fr <chankalan@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2015-12-29 17:01:36 +0000

    ajout de la traduction nl (Hanjo)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@94387 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ sjcycle/trunk/lang/paquet-sjcycle_nl.php
@@ -0,0 +1,13 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// S
+	'sjcycle_description' => 'Versie 2 van een instelbare diashow voor SPIP gebaseerd op plugin [sjcycle->http://jquery.malsup.com/cycle2/]. Redacteurs kunnen dishows in de tekst van hun artikel invoegen.',
+	'sjcycle_slogan' => 'Een instelbare diashow voor SPIP',
+	'sjcycle_nom' => 'SPIP Cycle2'
+);
