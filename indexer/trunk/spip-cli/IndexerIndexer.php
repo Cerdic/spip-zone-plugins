c5a1cc722c852ddf98f06577f889340941afa6df
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-01-04 12:17:13 +0000

    On ajoute une personnalisation pour la configuration des documents : le statut, car dans SPIP (enfin dans Médias) le champ statut est bien déclaré dans l'API objet mais PAS les statuts eux-mêmes (avec leur titre humain, etc). Du coup le squelette générique ne savait pas les afficher pour configurer lesquels on ignore !
    Au passage pour spip-cli : une commande DOIT être dans un fichier dédié. Donc on déplace "dump" dans un autre fichier.
    
    Et du coup on augmente la version qui n'avait pas été augmentée, avec Y, car ya eux des ajouts importants.
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@94426 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- indexer/trunk/spip-cli/IndexerIndexer.php
+++ indexer/trunk/spip-cli/IndexerIndexer.php
@@ -40,36 +40,3 @@ class IndexerIndexer extends Command {
 		//~ $output->writeln("\n<info>{$message}</info>");
 	}
 }
-
-class IndexerDump extends Command {
-	protected function configure() {
-		$this
-			->setName('indexer:dump')
-			->setDescription('Récupérer les contenus indexés.')
-			->addOption(
-				'index',
-				'i',
-				InputOption::VALUE_OPTIONAL,
-				'nom de l’index sphinx',
-				null
-			)
-			->addOption(
-				'format',
-				'f',
-				InputOption::VALUE_OPTIONAL,
-				'format de sortie : sphinx ou mysql',
-				'sphinx'
-			)
-		;
-	}
-
-	protected function execute(InputInterface $input, OutputInterface $output) {
-		include_spip('inc/indexer');
-
-		$index = $input->getOption('index');
-		$format = $input->getOption('format');
-
-		indexer_dumpsql($index, $format);
-	}
-}
-
