e5ce04b19f61a9875a47a7f7703107af206d6ca8
gouz@root-me.org <gouz@root-me.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-01-04 20:33:09 +0000

    bugfix pour avoir previsualisation du message fonctionnel
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@94437 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ messagerie/trunk/messagerie_autorisations.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin messagerie
+ * Licence GNU/GPL
+ */
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+function messagerie_autoriser(){}
+
+
+function autoriser_message_repondre($faire, $type = '', $id = 0, $qui = NULL, $opt = NULL){
+    return true;
+}
+
+
+?>
