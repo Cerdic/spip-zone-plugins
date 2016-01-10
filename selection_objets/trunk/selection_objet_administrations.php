339817b71b1765ec12f9fcb76ba86f10422d9014
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-01-10 08:11:32 +0000

    identation
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@94525 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- selection_objets/trunk/selection_objet_administrations.php
+++ selection_objets/trunk/selection_objet_administrations.php
@@ -5,8 +5,8 @@
  * Licence GNU/GPL
  */
 
-if (!defined('_ECRIRE_INC_VERSION')) return;
-
+if (!defined('_ECRIRE_INC_VERSION'))
+	return;
 
 /**
  * Fonction d'installation du plugin et de mise à jour.
@@ -14,41 +14,80 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
  * - créer la structure SQL,
  * - insérer du pre-contenu,
  * - installer des valeurs de configuration,
- * - mettre à jour la structure SQL 
-**/
+ * - mettre à jour la structure SQL
+ **/
 function selection_objet_upgrade($nom_meta_base_version, $version_cible) {
 	$maj = array();
 
-	$maj['create'] = array(array('maj_tables', array('spip_selection_objets')));
-	$maj['0.1.0'] = array(array('maj_tables', array('spip_selection_objets')));
-    $maj['0.2.0'] = array(array('maj_tables', array('spip_selection_objets')));
-    $maj['0.2.1'] = array(array('maj_tables', array('spip_selection_objets')));    
-    $maj['0.2.2'] = array(array('maj_tables', array('spip_selection_objets')));  
-    $maj['0.2.3'] = array(array('maj_tables', array('spip_selection_objets'))); 
-    $maj['0.2.4'] = array(array('maj_tables', array('spip_selection_objets')));   
-    $maj['0.3.0'] = array(array('maj_tables', array('spip_selection_objets'))); 
-    $maj['0.4.0'] = array(array('maj_tables', array('spip_selection_objets'))); 
-    $maj['0.5.0'] = array(array('maj_tables', array('spip_selection_objets')));  
-    $maj['0.5.1'] = array(
-                        array('sql_alter', "TABLE spip_selection_objets ADD KEY (id_objet)"),
-                        array('sql_alter', "TABLE spip_selection_objets ADD KEY (id_objet_dest)"),
-                        array('sql_alter', "TABLE spip_selection_objets ADD KEY (objet)"),
-                        array('sql_alter', "TABLE spip_selection_objets ADD KEY (objet_dest)"),
-                        );
+	$maj['create'] = array( array(
+			'maj_tables',
+			array('spip_selection_objets')
+		));
+	$maj['0.1.0'] = array( array(
+			'maj_tables',
+			array('spip_selection_objets')
+		));
+	$maj['0.2.0'] = array( array(
+			'maj_tables',
+			array('spip_selection_objets')
+		));
+	$maj['0.2.1'] = array( array(
+			'maj_tables',
+			array('spip_selection_objets')
+		));
+	$maj['0.2.2'] = array( array(
+			'maj_tables',
+			array('spip_selection_objets')
+		));
+	$maj['0.2.3'] = array( array(
+			'maj_tables',
+			array('spip_selection_objets')
+		));
+	$maj['0.2.4'] = array( array(
+			'maj_tables',
+			array('spip_selection_objets')
+		));
+	$maj['0.3.0'] = array( array(
+			'maj_tables',
+			array('spip_selection_objets')
+		));
+	$maj['0.4.0'] = array( array(
+			'maj_tables',
+			array('spip_selection_objets')
+		));
+	$maj['0.5.0'] = array( array(
+			'maj_tables',
+			array('spip_selection_objets')
+		));
+	$maj['0.5.1'] = array(
+		array(
+			'sql_alter',
+			"TABLE spip_selection_objets ADD KEY (id_objet)"
+		),
+		array(
+			'sql_alter',
+			"TABLE spip_selection_objets ADD KEY (id_objet_dest)"
+		),
+		array(
+			'sql_alter',
+			"TABLE spip_selection_objets ADD KEY (objet)"
+		),
+		array(
+			'sql_alter',
+			"TABLE spip_selection_objets ADD KEY (objet_dest)"
+		),
+	);
 
-
-                
 	include_spip('base/upgrade');
 	maj_plugin($nom_meta_base_version, $version_cible, $maj);
 }
-
 
 /**
  * Fonction de désinstallation du plugin.
  * Vous devez :
  * - nettoyer toutes les données ajoutées par le plugin et selection_objetn utilisation
- * - supprimer les tables et les champs créés par le plugin. 
-**/
+ * - supprimer les tables et les champs créés par le plugin.
+ **/
 function selection_objet_vider_tables($nom_meta_base_version) {
 	# quelques exemples
 	# (que vous pouvez supprimer !)
@@ -58,11 +97,10 @@ function selection_objet_vider_tables($nom_meta_base_version) {
 	sql_drop_table("spip_selection_objets");
 
 	# Nettoyer les versionnages et forums
-	sql_delete("spip_versions",              sql_in("objet", array('abuselection_objetbjet')));
-	sql_delete("spip_versions_fragments",    sql_in("objet", array('abuselection_objetbjet')));
-	sql_delete("spip_forum",                 sql_in("objet", array('abuselection_objetbjet')));
+	sql_delete("spip_versions", sql_in("objet", array('abuselection_objetbjet')));
+	sql_delete("spip_versions_fragments", sql_in("objet", array('abuselection_objetbjet')));
+	sql_delete("spip_forum", sql_in("objet", array('abuselection_objetbjet')));
 
 	effacer_meta($nom_meta_base_version);
 }
-
 ?>
