7ddc3f820992935f003bb4040c4e1c8c0a61dbba
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-01-10 08:39:53 +0000

    background blanc pour spip3.1 langues identation
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@94530 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- traduction_rubriques_autrement/branches/1/action/dereferencer_traduction_rubrique.php
+++ traduction_rubriques_autrement/branches/1/action/dereferencer_traduction_rubrique.php
@@ -1,15 +1,16 @@
 <?php
 
 function action_dereferencer_traduction_rubrique_dist() {
-	$securiser_action = charger_fonction('securiser_action','inc');
+	$securiser_action = charger_fonction('securiser_action', 'inc');
 	$arg = $securiser_action();
 
-	list($type, $id_objet) = explode('/',$arg);
+	list($type, $id_objet) = explode('/', $arg);
 	if (!$type = objet_type($type) or !$id_objet = intval($id_objet)) {
 		if (!_AJAX) {
 			include_spip('inc/minipres');
 			minipres('Arguments incompris');
-		} else {
+		}
+		else {
 			spip_log('Arguments incompris dans action dereferencer_traduction_rubrique');
 			return false;
 		}
@@ -23,16 +24,12 @@ function action_dereferencer_traduction_rubrique_dist() {
 
 	if ($id_trad_old) {
 		include_spip('inc/modifier');
-		modifier_contenu($objet, $id_objet, array('invalideur' => "id='$objet/$id_objet'"), array(
-			'id_trad' => 0
-		));
-	
+		modifier_contenu($objet, $id_objet, array('invalideur' => "id='$objet/$id_objet'"), array('id_trad' => 0));
+
 		// si la deliaison fait qu'il ne reste plus que la source
 		// dans le groupe de traduction on lui remet l'id_trad a 0
 		if (1 == $nb_dans_groupe = sql_countsel($table, array('id_trad = ' . sql_quote($id_trad_old)))) {
-			modifier_contenu($objet, $id_trad_old, array('invalideur' => "id='$objet/$id_trad_old'"), array(
-				'id_trad' => 0
-			));				
+			modifier_contenu($objet, $id_trad_old, array('invalideur' => "id='$objet/$id_trad_old'"), array('id_trad' => 0));
 		}
-	}	
+	}
 }
