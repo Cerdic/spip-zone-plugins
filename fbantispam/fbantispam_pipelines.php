2fe29990e1ee37af73a06b49859da2482fcdf90b
fabio.bertagnin@fbservices.fr <fabio.bertagnin@fbservices.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-01-12 10:41:50 +0000

    Ajout d'un panneau de configuration pour choisir le type de captcha : copie de 4 caractères, addition de deux valeurs ou multiplication de deux valeurs
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@94562 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- fbantispam/fbantispam_pipelines.php
+++ fbantispam/fbantispam_pipelines.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Plugin FB Antispam
  * (c) 2013 Fabio Bertagnin - FBServices - www.fbservices.fr
@@ -6,9 +7,8 @@
  * Licence GPL
  *
  */
-
-if (!defined("_ECRIRE_INC_VERSION")) return;
-
+if (!defined("_ECRIRE_INC_VERSION"))
+    return;
 
 /**
  * Ajouter le champ de formulaire 'nobot' au besoin
@@ -17,111 +17,146 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
  * @return array
  */
 function fbantispam_recuperer_fond($flux) {
-	$fond = strval($flux['args']['fond']);
-	if (false !== $pos = strpos($fond, 'formulaires/forum')) 
-	{
-		// on ajoute le champ 'nobot' si pas present dans le formulaire
-		$texte = &$flux['data']['texte'];
-		$pos = strpos($texte, '</form>');
-		$nobot = recuperer_fond("inclure/nobot", array('nobot' => ''));
-		$texte = substr_replace($texte, $nobot, $pos, 0);
+    $fond = strval($flux['args']['fond']);
+    if (false !== $pos = strpos($fond, 'formulaires/forum')) {
+        // on ajoute le champ 'nobot' si pas present dans le formulaire
+        $texte = &$flux['data']['texte'];
+        $pos = strpos($texte, '</form>');
+        $nobot = recuperer_fond("inclure/nobot", array('nobot' => ''));
+        $texte = substr_replace($texte, $nobot, $pos, 0);
 
-		// On ajoute le champ 'captcha' avant le bouton de submit
-		// On commence de la fin, pour se positionner dans le formulaire de saisie et non dans
-		// celui de prévisualisation
-		$pos = strrpos($texte, '<p class="boutons">', 0);
-		// S'il n'a pas trouvé le bouton, on se positionne à la fin du formulaire (moins joli !)
-		if (!$pos) $pos = strrpos($texte, '</form', 0);
-		if ($pos)
-		{
-			$cp = fbantispam_get_captcha();
-			$cps = "$cp[0]$cp[1]$cp[2]$cp[3]";
-			$captcha = recuperer_fond("inclure/captcha", array('captcha' => $cps, 'c1' => $cp[0]*2, 'c0' => $cp[1]*2, 'c2' => $cp[2]*2, 'c3' => $cp[3]*2));
-			$texte = substr_replace($texte, $captcha, $pos, 0);
-		}
-	}
-	return $flux;
+        // On ajoute le champ 'captcha' avant le bouton de submit
+        // On commence de la fin, pour se positionner dans le formulaire de saisie et non dans
+        // celui de prévisualisation
+        $pos = strrpos($texte, '<p class="boutons">', 0);
+        // S'il n'a pas trouvé le bouton, on se positionne à la fin du formulaire (moins joli !)
+        if (!$pos)
+            $pos = strrpos($texte, '</form', 0);
+        if ($pos) {
+            $type_captcha = lire_config('fbantispam/type_captcha', 'copie');
+            if ($type_captcha == 'copie')
+            {
+                $cp = fbantispam_get_captcha();
+                $cps = "$cp[0]$cp[1]$cp[2]$cp[3]";
+                $captcha = recuperer_fond("inclure/captcha", array('captcha' => $cps, 'c1' => $cp[0] * 2, 'c0' => $cp[1] * 2, 'c2' => $cp[2] * 2, 'c3' => $cp[3] * 2));
+                $texte = substr_replace($texte, $captcha, $pos, 0);
+            }
+            if ($type_captcha == 'addition')
+            {
+                $cp = fbantispam_get_captcha();
+                $cps = "$cp[1]+$cp[3]";
+                $captcha = recuperer_fond("inclure/captcha-addition", array('captcha' => $cps, 'c1' => $cp[0] * 2, 'c0' => $cp[1] * 2, 'c2' => $cp[2] * 2, 'c3' => $cp[3] * 2));
+                $texte = substr_replace($texte, $captcha, $pos, 0);
+            }
+            if ($type_captcha == 'multiplication')
+            {
+                $cp = fbantispam_get_captcha();
+                $cps = "$cp[1]x$cp[3]";
+                $captcha = recuperer_fond("inclure/captcha-multiplication", array('captcha' => $cps, 'c1' => $cp[0] * 2, 'c0' => $cp[1] * 2, 'c2' => $cp[2] * 2, 'c3' => $cp[3] * 2));
+                $texte = substr_replace($texte, $captcha, $pos, 0);
+            }
+        }
+    }
+    return $flux;
 }
 
 /**
  */
 function fbantispam_formulaire_charger($flux) {
-	$form = $flux['args']['form'];
-	$je_suis_poste = false;
-	if (isset($flux['args']['je_suis_poste']) && $flux['args']['je_suis_poste'] == 1) $je_suis_poste = true;
-	if ($form == "forum" && !$je_suis_poste) 
-	{
-		include_spip("inc/fbantispam");
-		if ($charger_formulaire = charger_fonction("charger_formulaire_forum", "fbantispam", true)) 
-		{
-			$flux = $charger_formulaire($flux);
-		}
-	}
-	return $flux;
+    $form = $flux['args']['form'];
+    $je_suis_poste = false;
+    if (isset($flux['args']['je_suis_poste']) && $flux['args']['je_suis_poste'] == 1)
+        $je_suis_poste = true;
+    if ($form == "forum" && !$je_suis_poste) {
+        include_spip("inc/fbantispam");
+        if ($charger_formulaire = charger_fonction("charger_formulaire_forum", "fbantispam", true)) {
+            $flux = $charger_formulaire($flux);
+        }
+    }
+    return $flux;
 }
 
 /**
  */
 function fbantispam_formulaire_verifier($flux) {
-	$form = $flux['args']['form'];
-	$previsu = false;
-	if (isset($flux['data']['previsu']) && $flux['data']['previsu'] != '') $previsu = true;
+    $form = $flux['args']['form'];
+    $previsu = false;
+    if (isset($flux['data']['previsu']) && $flux['data']['previsu'] != '')
+        $previsu = true;
 
-	$res = array();
+    $res = array();
 
-	if ($form == "forum" && $previsu) 
-	{
-		
-		$captcha = _request('captcha');
-		$cp0 = _request('c1')/2;
-		$cp1 = _request('c0')/2;
-		$cp2 = _request('c2')/2;
-		$cp3 = _request('c3')/2;
-		$cps = "$cp0"."$cp1"."$cp2"."$cp3";
-		if ($captcha == '')
-		{
-			$res['message_erreur'] = '<p style="background:#ffffaa;padding:4px">ERREUR : le code anti-spam n\'a pas été saisi</p>';
-			return $res;
-		}
-		if ($captcha != $cps) 
-		{
-			$res['message_erreur'] = '<p style="background:#ffffaa;padding:4px">ERREUR : le code anti-spam n\'est pas correct</p>';
-			return $res;
-			
-		}
-		$cmod = _request('cmod');
-		if ($cmod != 'fbantispam') 
-		{
-			$res['message_erreur'] = '<p style="background:#ffffaa;padding:4px">ERREUR : message non accepté (identifié à un SPAM)</p>';
-			return $res;
-			
-		}
-	}
-	return $flux;
+    if ($form == "forum" && $previsu) {
+
+        $captcha = _request('captcha');
+        $cp0 = _request('c1') / 2;
+        $cp1 = _request('c0') / 2;
+        $cp2 = _request('c2') / 2;
+        $cp3 = _request('c3') / 2;
+        $cmod = _request('cmod');
+        if ($cmod != 'fbantispam') {
+            $res['message_erreur'] = '<p style="background:#ffffaa;padding:4px">ERREUR : message non accepté (identifié à un SPAM)</p>';
+            return $res;
+        }
+        if ($captcha == '') {
+            $res['message_erreur'] = '<p style="background:#ffffaa;padding:4px">ERREUR : le code anti-spam n\'a pas été saisi</p>';
+            return $res;
+        }
+        $type_captcha = lire_config('fbantispam/type_captcha');
+        if ($type_captcha == 'copie')
+        {
+            $cps = "$cp0" . "$cp1" . "$cp2" . "$cp3";
+            if ($captcha != $cps) {
+                $res['message_erreur'] = '<p style="background:#ffffaa;padding:4px">ERREUR : le code anti-spam n\'est pas correct</p>';
+                return $res;
+            }
+        }
+        elseif ($type_captcha == 'addition')
+        {
+            $cps = $cp1 + $cp3;
+            if ($captcha != $cps) {
+                $res['message_erreur'] = '<p style="background:#ffffaa;padding:4px">ERREUR : le code anti-spam n\'est pas correct</p>';
+                return $res;
+            }
+        }
+        elseif ($type_captcha == 'multiplication')
+        {
+            $cps = $cp1 * $cp3;
+            if ($captcha != $cps) {
+                $res['message_erreur'] = '<p style="background:#ffffaa;padding:4px">ERREUR : le code anti-spam n\'est pas correct</p>';
+                return $res;
+            }
+        }
+        else
+        {
+            $res['message_erreur'] = '<p style="background:#ffffaa;padding:4px">ERREUR : pas de captcha connu</p>';
+            return $res;
+        }
+        
+    }
+    return $flux;
 }
 
 /**
  */
 function fbantispam_formulaire_traiter($flux) {
-	return $flux;
+    return $flux;
 }
 
 /**
  */
 function fbantispam_pre_edition($flux) {
-	return $flux;
+    return $flux;
 }
 
 /**
  */
-function fbantispam_get_captcha()
-{
-	$ret = array();
-	for($i=0; $i<4; $i++)
-	{
-		$ret[$i] = rand(0,9);
-	}
-	return $ret;
+function fbantispam_get_captcha() {
+    $ret = array();
+    for ($i = 0; $i < 4; $i++) {
+        $ret[$i] = rand(0, 9);
+    }
+    return $ret;
 }
 
 ?>
