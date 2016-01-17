a17e753de643b5dcf31d937e32441985acb7beae
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-01-17 21:47:41 +0000

    On vérifie aussi la compatibilité des plugins inactifs. Il est vrai que ces plugins sont désactivés à l'instant T, mais cela ne veut pas dire que ces plugins ne sont pas utilisés à un autre moment de la vie du  site. Up de y pour ce changement.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@94625 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- verifie31/lang/verifie31_fr.php
+++ verifie31/lang/verifie31_fr.php
@@ -9,15 +9,17 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
     // B
     'bilan' => '@total@ plugin(s) actif(s) - @total_notok@ plugin(s) incompatible(s)',
+    'bilan_inactif' => '@total@ plugin(s) inactif(s) - @total_notok@ plugin(s) incompatible(s)',
 
     // L
     'liste_plugins' => 'Liste des plugins actifs',
+    'liste_plugins_inactifs' => 'Liste des plugins inactifs',
 
     // T
     'titre_verifie31' => 'Vérifier la compatibilité des plugins à SPIP 3.1',
 
     // P
-   'plugin_nom' => 'Nom du plugin', 
+   'plugin_nom' => 'Nom du plugin',
    'plugin_borne' => 'Bornes',
    'plugin_compat31' => 'Compatible SPIP 3.1',
    'plugin_compat31_ok' => 'OK',
@@ -26,5 +28,3 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
    'plugin_compat31_maj_gestion' => 'Gestion des plugins',
 
 );
-
-?>
