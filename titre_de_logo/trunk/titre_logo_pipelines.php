dc7b868e9e24671597ee33b76f53e05dd1dec092
arno@rezo.net <arno@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-01-18 17:53:53 +0000

    1. Compatibilité SPIP 3.1 2. Ne plus patcher /formulaires/editer_logo, le pb de compatibilité vient de là 3. Simplifier: pas de barre de raccourcis dans le formulaire étroit. 4. Content du placement du formulaire (de retour à l’intérieur du bloc «Logo de l’article/rubrique…») 5. Pas satisfait de la validation du formulaire, qui n’est du coup pas en ajax.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@94657 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- titre_de_logo/trunk/titre_logo_pipelines.php
+++ titre_de_logo/trunk/titre_logo_pipelines.php
@@ -14,3 +14,46 @@
 if (!defined('_ECRIRE_INC_VERSION')) {
     return;
 }
+
+
+
+function titre_logo_recuperer_fond($flux) {
+	
+	
+	if ($flux["args"]["fond"] == "formulaires/editer_logo") {
+
+		$id_objet = $flux["args"]["contexte"]["id_objet"];
+		$objet = $flux["args"]["contexte"]["objet"];
+		$editable = $flux["args"]["contexte"]["_options"]["editable"];
+		
+		print_r($id_objet, $objet, $editable);
+		
+		if ($editable) {
+			
+			$objets_autorises = lire_config('titre_logo/objets_autorises');
+			$objets_autorises = (isset($objets_autorises))
+				? array_filter($objets_autorises)
+				: array();
+		
+		
+		 $table_objet = table_objet_sql($objet);
+		 
+			$texte = $flux["data"]["texte"] ;
+			
+			// Bof bof: repérer la mention «taille» dans le texte
+			// ce qui indique que le formulaire de logo contient cette mention
+			if (in_array($table_objet, $objets_autorises) 
+					&& strpos($texte, 'taille') > 0) {
+				$cont = array(
+					"objet" => "article", 
+					"id_objet" => $id_objet
+				);
+				
+				$ajouter = recuperer_fond("prive/inc_editer_titre_logo", $cont);
+				
+				$flux["data"]["texte"] = str_replace("</form>", "</form>".$ajouter, $texte);
+			}
+		}
+	}
+	return $flux;
+}
