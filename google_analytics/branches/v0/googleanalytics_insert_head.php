f61ff6b162716788f335e9ded2669d6bd4d46b77
marcimat@rezo.net <marcimat@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-01-21 18:21:49 +0000

    Passage trunk/branches pour cym.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@94698 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ google_analytics/branches/v0/googleanalytics_insert_head.php
@@ -0,0 +1,61 @@
+<?php
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+
+/**
+ * Dans SPIP 3 on utilise insert_head_css qui est safe
+ * et on insere avant les CSS, pour ne pas bloquer celles-ci
+ * (qui sont bloquees par du js inline)
+ *
+ * @param $flux
+ * @return string
+ */
+function googleanalytics_insert_head_css($flux){
+	return googleanalytics_snippet().$flux;
+}
+
+/**
+ * Dans SPIP 2 on utilise insert_head et on ajoute a la fin
+ * simplement
+ *
+ * @param $flux
+ * @return string
+ */
+function googleanalytics_insert_head($flux){
+	return $flux . googleanalytics_snippet();
+}
+
+/**
+ * Morceau de code a inserer dans la page pour traquer avec GA
+ * @return string
+ */
+function googleanalytics_snippet(){
+	include_spip('inc/config');
+	$id_google = lire_config('googleanalytics/idGoogle');
+	if ($id_google
+	  AND $id_google !== '_'
+	  AND (strncmp($id_google,"UA-xxx",6)!=0)) {
+	    if (lire_config('googleanalytics/ga_universal')) {
+	        return "<script type='text/javascript'>/*<![CDATA[*/
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) })
+  (window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  ga('create', '".$id_google."');
+    ga('send', 'pageview');
+/*]]>*/</script>\n";
+	    } else {
+		    return '<script type="text/javascript">/*<![CDATA[*/
+	var _gaq = _gaq || [];
+	_gaq.push(["_setAccount", "'.$id_google.'"]);
+	_gaq.push(["_trackPageview"]);
+	(function() {
+	  var ga = document.createElement("script"); ga.type = "text/javascript"; ga.async = true;
+	  ga.src = ("https:" == document.location.protocol ? "https://ssl" : "http://www") + ".google-analytics.com/ga.js";
+	  var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ga, s);
+	})();
+/*]]>*/</script>'."\n";
+        }
+	}
+	return "";
+}
+?>
---
+++ google_analytics/branches/v0/lang/googleanalytics_fr.php
@@ -0,0 +1,18 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	// C
+	'cfg_descriptif' => 'Ici vous pouvez indiquer votre identifiant Google Analytics.',
+	'cfg_titre' => 'Google Analytics',
+
+	// E
+	'explication_id_google' => 'Si "_" ou vide, supprime la fonctionnalit&eacute; (vide reviendra au d&eacute;faut).',
+
+	// L
+	'label_id_google' => 'Votre identifiant Google Analytics du type "UA-1234567-1"',
+	'label_ga_universal' => 'Utiliser <a href="https://support.google.com/analytics/answer/2790010?hl%3Den">Google Analytics Universal</a>',
+);
+?>
