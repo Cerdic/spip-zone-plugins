6f1f93bdc1438de01df71a7939a501c191c120ae
tcharlss@bravecassine.com <tcharlss@bravecassine.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-01-22 17:15:07 +0000

    Mise à jour de la description suite à r94622, up de z
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@94716 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- identifiants/trunk/lang/paquet-identifiants_fr.php
+++ identifiants/trunk/lang/paquet-identifiants_fr.php
@@ -8,9 +8,10 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// I
 	'identifiants_nom' => 'Identifiants',
 	'identifiants_slogan' => 'Ajouter des identifiants uniques à n\'importe quel objet.',
-	'identifiants_description' => 'Le but du plugin est de permettre de sélectionner des objets en fonction de leur identifiant.
-	Il faut penser à faire une jointure explicite avec la table spip_identifiants : <BOUCLE_rubrique(RUBRIQUES identifiants){identifiant=x}>.
-	',
+	'identifiants_description' => 'On a parfois besoin de différencier certains objets : un article sur les mentions légales, une rubrique contenant des vidéos, etc.
+	Le but du plugin est de permettre d\'attribuer des identifiants uniques à certains objets, et donc de les sélectionner en fonction de ceux-ci.
+	Ex. : <BOUCLE_rubrique(RUBRIQUES){identifiant=un_identifiant}>. Les identifiants sont stockés dans la table `spip_identifiants`.
+	L\'ajout d\'identifiants n\'est proposé que pour les tables ne possédant pas déjà une colonne `identifiant`, donc elles ne seront pas concernées par l\'utilisation de ce plugin.',
 );
 
 ?>
