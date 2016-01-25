75c1b0ce08a9126d0ed7a80eda4389a0dcb01608
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-01-25 12:55:48 +0000

    On passe en paquet.xml "dev". On reformule la fonction d'installation. On supprime "build" qui contenait un doublon depuis le départ. On met vraiment tous les imports dans "imports", avec les arrondissements donc. Et pour pas de confusion les régions dans imports/regions/ puisque plusieurs fichiers.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@94738 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- geographie/trunk/imports/geographie.php
+++ geographie/trunk/imports/geographie.php
@@ -1,5 +1,10 @@
 <?php
-function imports_geographie_dist(){
+// Sécurité
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+function imports_geographie_dist() {
 	// importer les pays
 	include_spip('imports/pays');
 	include_spip('inc/charsets');
@@ -8,10 +13,11 @@ function imports_geographie_dist(){
 
 	// importer les regions/dept/communes francaise
 	$id_pays = 70;
-	$fichiers = preg_files(_DIR_PLUGIN_GEOGRAPHIE . "imports/",'[.]txt$');
+	$fichiers = preg_files(_DIR_PLUGIN_GEOGRAPHIE . "imports/regions/",'[.]txt$');
 	sort($fichiers);
 	foreach($fichiers as $fichier) {
 		lire_fichier($fichier,$table);
+		
 		if ($table = unserialize($table)) {
 			foreach($table as $region=>$departements){
 				$id_region = sql_insertq('spip_geo_regions',array('nom'=>unicode2charset(html2unicode($region)),'id_pays'=>$id_pays));
@@ -22,21 +28,25 @@ function imports_geographie_dist(){
 						$abbr = substr($abbr,1);
 					}
 					$id_departement = sql_insertq('spip_geo_departements',array('nom'=>unicode2charset(html2unicode($departement)),'abbr'=>$abbr,'id_region'=>$id_region));
+					
 					$tabcom=array();
 					foreach($communes as $commune) {
-						if ($commune['insee']) // par securite pour eviter une insertion vide
+						if ($commune['insee']) { // par securite pour eviter une insertion vide
 							$tabcom[]=array('nom'=>unicode2charset($commune['nom']),'insee'=>$commune['insee'],'code_postal'=>$commune['cp'],'latitude'=>$commune['lat'],'longitude'=>$commune['long'],'id_departement'=>$id_departement,'id_pays'=>$id_pays);
+						}
 					}
+					
 					sql_insertq_multi('spip_geo_communes',$tabcom);
 				}
 			}
 		}
 	}
+	
 	// donner un coup de menage dans les communes car certaines ne sont pas liees au bon dept
 	$res = sql_select('id_departement,abbr','spip_geo_departements','');
 	while ($row = sql_fetch($res)){
-		if ($row['abbr'])
+		if ($row['abbr']) {
 			sql_updateq('spip_geo_communes',array('id_departement'=>$row['id_departement']),"insee LIKE '0".$row['abbr']."%'");
+		}
 	}
-
 }
