7446ed95e92d68659b26d19959f3d144c9f83037
cyril@cym.fr <cyril@cym.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-02-01 09:48:08 +0000

    Premier dépôt des fichiers du plugin Google Tag Manager
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@94854 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ gtm/lang/gtm_fr.php
@@ -0,0 +1,17 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/gis/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	'label_tag_gtm'         => 'Code du tag',
+	'explication_tag_gtm'   => 'Insérez ici le code complet du Google Tag Manager tel qu\'il vous a été fourni sur votr page Google Analytics',
+	'configurer_titre'      => 'Google Tag Manager',
+	'configurer_descriptif' => 'Configurer le Google Tag Manager pour votre site. Vous pouvez visiter ici une <a href="https://www.google.com/intl/fr/tagmanager/"> page de présentation du Google Tag Manager</a> (ou Gestionnaire de Balise Google).',
+	'placeholder'           => '<!-- Google Tag Manager -->',
+
+);
+
+?>
---
+++ gtm/lang/paquet-gtm_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/gis/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// G
+	'gtm_nom' => 'Google Tag Manager',
+	'gtm_description' => 'Cette première version du plugin Google Tag Manager permet simplement de saisir un tag GTM dans l\'interface privée. Pour l\'instant \'insérersion du tag dans les squelettes se fait en ajoutant le code <code>#CONFIG{gtp/tag_gtm}</code> juste en dessous de la balise <code></body></code>.',
+	'gtm_slogan' => 'Ajouter le Google Tag Manager a votre site'
+);
+
+?>
