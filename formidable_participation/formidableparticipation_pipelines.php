4eccc64a98ec3504c43b1ebd682bf61c86080755
p@henix.be <p@henix.be@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-02-01 15:50:07 +0000

    Règles de codage
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@94865 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- formidable_participation/formidableparticipation_pipelines.php
+++ formidable_participation/formidableparticipation_pipelines.php
@@ -9,7 +9,9 @@
  * @package    SPIP\Formidableparticipation\Pipelines
  */
 
-if (!defined('_ECRIRE_INC_VERSION')) return;
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
 /**
  * Traiter les formulaires de participation
@@ -18,41 +20,59 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
  */
 function formidableparticipation_traiter_formidableparticipation($flux){
 	//au moins une reponse et un email
-	if ($flux['args']['choix_participation'] && $flux['args']['email']){
+	if ($flux['args']['choix_participation'] && $flux['args']['email']) {
+
 		$id_evenement = $flux['args']['id_evenement'];
 		$reponse = $flux['args']['choix_participation'];
 		$email = $flux['args']['email'];
 		$id_auteur = $flux['args']['id_auteur'];
 		$nom = $flux['args']['nom'];
 		$prenom = $flux['args']['prenom'];
-		if($flux['args']['organisme']) $organisme = '('.$flux['args']['organisme'].')';
+
+		if ($flux['args']['organisme']) {
+			$organisme = '('.$flux['args']['organisme'].')';
+		}
+
 		$nom = "$prenom $nom $organisme";
-		
+
 		$champs = array(
-			'id_auteur'=>$id_auteur,
-			'nom'=>$nom,
-			'email'=>$email,
+			'id_auteur' => $id_auteur,
+			'nom' => $nom,
+			'email' => $email,
 			'reponse'=>$reponse,
 			'id_evenement'=>$id_evenement,
 			'date'=>date('Y-m-d H:i:s'),
 		);
-		
+
 		// si evenement, on insere le participant et ses données
 		// et on laisse le traitement du nombre de places à la charge du webmestre et du squelette evenements
-		if(isset($id_evenement)){
-				//on ne loge pas l'auteur, ssi l'email sur le même id_evenement existe, mettre à jour
-				if (sql_fetsel('reponse','spip_evenements_participants','id_evenement='.intval($id_evenement)." AND email=".sql_quote($email))){
-					sql_updateq("spip_evenements_participants",$champs,'id_evenement='.intval($id_evenement).' AND email='.sql_quote($email));
-				}
-				else{
-					sql_insertq("spip_evenements_participants", $champs);
-				}    
+		if (isset($id_evenement)) {
+
+			//on ne loge pas l'auteur, si l'email sur le même id_evenement existe, mettre à jour
+			$reponse = sql_fetsel(
+				'reponse',
+				'spip_evenements_participants',
+				'id_evenement='.intval($id_evenement).'
+                and email='.sql_quote($email)
+			);
+
+			if (!is_null($reponse)) {
+				sql_updateq(
+					'spip_evenements_participants',
+					$champs,
+					'id_evenement='.intval($id_evenement).'
+                    and email='.sql_quote($email)
+				);
+			} else {
+				sql_insertq('spip_evenements_participants', $champs);
+			}
 		}
-		
-		spip_log("pipeline evenement $id_evenement pour $email et id_auteur=$id_auteur reponse=$reponse","formidable_participation");
+
+		spip_log(
+			"pipeline evenement $id_evenement pour $email et id_auteur=$id_auteur reponse=$reponse",
+			'formidable_participation'
+		);
 	}
 
    return $flux;
 }
-
-?>
