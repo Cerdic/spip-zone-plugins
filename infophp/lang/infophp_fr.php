cf3d4d2e15fef18de9716e36e1d69b306aac2697
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-02-02 20:50:39 +0000

    Un tout petit plugin inspiré du code de Dolibarr pour afficher les infos PHP dans l'admin de SPIP
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@94898 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ infophp/lang/infophp_fr.php
@@ -0,0 +1,17 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	'infophp' => 'Informations PHP',
+	'parameter' => 'Paramètre',
+	'value' => 'Valeur',
+	'version' => 'Version',
+	
+);
+
+?>
---
+++ infophp/lang/paquet-infophp_fr.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	'infophp_description' => 'Ce plugin permet d\'afficher les infos PHP en restant dans l\'interface d\'admin de SPIP',
+	'infophp_nom' => 'Info PHP',
+	'infophp_slogan' => 'Afficher les info PHP de votre serveur'
+	
+);
+
+?>
