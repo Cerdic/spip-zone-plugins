36e420b47f6118818594172dd9c23231d5ee9397
chankalan@free.fr <chankalan@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-02-02 13:44:56 +0000

    des modèles pour afficher les logos dans les textes
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@94888 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ trunk/lang/paquet-afficher_logo_fr.php
@@ -0,0 +1,8 @@
+<?php
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	'afficher_logo_description' => 'Afficher un logo dans un texte.',
+	'afficher_logo_slogan' => 'Fais voir ton logo !',
+);
