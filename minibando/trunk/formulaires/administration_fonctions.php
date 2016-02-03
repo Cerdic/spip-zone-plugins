9e5c8b34b244fa5cb5355802e76c03b7b8fd7d6a
p@henix.be <p@henix.be@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-02-03 23:53:24 +0000

    Règles de codage
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@94911 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- minibando/trunk/formulaires/administration_fonctions.php
+++ minibando/trunk/formulaires/administration_fonctions.php
@@ -3,10 +3,12 @@
 include_spip('inc/bandeau');
 
 function minibando_definir_rubrique_contexte($contexte) {
-    if( !isset($contexte['id_rubrique']) && isset($contexte['id_article']) && $contexte['id_article'] ){
-        $contexte['id_rubrique'] = sql_getfetsel('id_rubrique','spip_articles','id_article='.(int)$contexte['id_article']);
+    if (!isset($contexte['id_rubrique']) && isset($contexte['id_article']) && $contexte['id_article']){
+        $contexte['id_rubrique'] = sql_getfetsel(
+	        'id_rubrique',
+	        'spip_articles',
+	        'id_article='.(int)$contexte['id_article']
+        );
     }
     return $contexte;
 }
-
-?>
