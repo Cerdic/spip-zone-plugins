625f5c27b0a21fb6917cd6f3b0f9e4e80ca45c39
p@henix.be <p@henix.be@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-02-04 17:00:33 +0000

    Commenter l'API
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@94934 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- facebook/trunk/inc/facebook.php
+++ facebook/trunk/inc/facebook.php
@@ -6,12 +6,18 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 
 include_spip('lib/facebook-php-sdk/src/Facebook/autoload');
 
-// Le SDK de Facebook utilise des session PHP,
+// Le SDK de Facebook utilise des sessions PHP,
 // Cependant, il n'est pas foutu de faire lui même ce test.
 if (!session_id()) {
     session_start();
 }
 
+/**
+ * Initialiser Facebook avec la configuration stockée dans SPIP
+ *
+ * @access public
+ * @return object L'objet Facebook crée
+ */
 function facebook() {
 
 	include_spip('inc/config');
@@ -27,6 +33,12 @@ function facebook() {
 }
 
 
+/**
+ * Obtenir un lien de connection Facebook
+ *
+ * @access public
+ * @return string Lien vers Facebook
+ */
 function facebook_lien_connection() {
 
 	$fb = facebook();
@@ -40,6 +52,12 @@ function facebook_lien_connection() {
 	return '<a href="' . htmlspecialchars($loginUrl) . '">Log in with Facebook!</a>';
 }
 
+/**
+ * On récupère le token d'accès et on le stocke dans le config SPIP
+ *
+ * @access public
+ * @return string Un message d'erreur au besoin
+ */
 function facebook_access_token() {
 
 	// S'il n'y a pas de code, pas besoin de chercher un token
@@ -105,6 +123,15 @@ function facebook_access_token() {
 }
 
 
+
+/**
+ * Poster un lien sur facebook
+ *
+ * @param string $lien
+ * @param string $message
+ * @access public
+ * @return string Un message d'erreur au besoin
+ */
 function facebook_poster_lien($lien, $message) {
 
 	include_spip('inc/config');
