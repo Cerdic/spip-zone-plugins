c3ca64a0f4d096110e20cc397bf3268bac4e2d2e
louis.possoz@quelfutur.org <louis.possoz@quelfutur.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-02-05 11:17:59 +0000

    correction guillement simple dans fichiers langage
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@94966 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- menu_anime/trunk/lang/paquet-menu_anime_fr.php
+++ menu_anime/trunk/lang/paquet-menu_anime_fr.php
@@ -8,7 +8,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// M
 	'menu_anime_description' => 'Gère l\'affichage d\'un menu au moyen d\'une feuille de style élaborée: dégradés de couleurs, ombrage des menus et textes, coins arrondis et animation des menus.
 Inclus: une version compacte automatique pour mobiles.
-Adaptation aisée (?) des couleurs et des dimensions au moyen du fichier 'menu_anime.less' inclus.
+Adaptation aisée (?) des couleurs et des dimensions au moyen du fichier \'menu_anime.less\' inclus.
 	
 Complément du plugin {{Menus}} par exemple.',
 	'menu_anime_nom' => 'Menu animé déroulant',
