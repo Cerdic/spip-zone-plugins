d45ddf90a831b61a025ea7df8fa41ddc1696d219
p@henix.be <p@henix.be@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-02-07 15:12:33 +0000

    Commentaire
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@95004 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- facebook/trunk/action/facebook_access_token.php
+++ facebook/trunk/action/facebook_access_token.php
@@ -4,6 +4,12 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
 
+
+/**
+ * Enregistrer le compte Facebook du site.
+ *
+ * @access public
+ */
 function action_facebook_access_token_dist($arg = null) {
 	if (is_null($arg)) {
 		$securiser_action = charger_fonction('securiser_action', 'inc');
@@ -13,6 +19,6 @@ function action_facebook_access_token_dist($arg = null) {
 	include_spip('inc/facebook');
 	$token = facebook_access_token();
 
-	// Ecrire le token dans la session
+	// Ecrire le token dans les meta du site
 	ecrire_config('facebook/accessToken', $token);
 }
