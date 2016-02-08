cc92da825dd7fa1bf87bc6d22a90a11c297143af
brunobergot@gmail.com <brunobergot@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-02-08 13:24:11 +0000

    maj du slogan
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@95024 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- centre_image/trunk/lang/paquet-centre_image_fr.php
+++ centre_image/trunk/lang/paquet-centre_image_fr.php
@@ -8,5 +8,5 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// C
 	'centre_image_description' => 'Permet de calculer automatiquement un centre d\'intérêt d\'une image ou de l\'indiquer manuellement.',
 	'centre_image_nom' => 'Centre image',
-	'centre_image_slogan' => 'Filtres de transformation d\'image et de couleurs',
+	'centre_image_slogan' => 'Filtres de calcul du centre d\'intérêt d\'images',
 );
