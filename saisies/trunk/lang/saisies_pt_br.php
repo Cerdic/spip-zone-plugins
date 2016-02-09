5e5c0865d868ece79a29509d3efe2b297c62c566
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-02-09 04:35:27 +0000

    [Salvatore] [source:_plugins_/saisies/trunk/lang/ saisies] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@95039 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ saisies/trunk/lang/saisies_pt_br.php
@@ -0,0 +1,111 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/saisies?lang_cible=pt_br
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'bouton_parcourir_docs_article' => 'Procurar na matéria',
+	'bouton_parcourir_docs_breve' => 'Procurar na nota',
+	'bouton_parcourir_docs_rubrique' => 'Procurar na seção',
+	'bouton_parcourir_mediatheque' => 'Procurar na biblioteca de mídia',
+
+	// C
+	'construire_action_annuler' => 'Cancelar',
+	'construire_action_configurer' => 'Configurar',
+	'construire_action_deplacer' => 'Deslocar',
+	'construire_action_dupliquer' => 'Duplicar',
+	'construire_action_dupliquer_copie' => '(cópia)',
+	'construire_action_supprimer' => 'Excluir',
+	'construire_ajouter_champ' => 'Incluir um campo',
+	'construire_attention_enregistrer' => 'Lembre-se de gravar as suas alterações!',
+	'construire_attention_modifie' => 'O formulário abaixo é diferente do formulário inicial.Você tem a possibilidade de revertê-lo ao estado em que estava, antes das suas alterações.',
+	'construire_attention_supprime' => 'As suas alterações comportam exclusões de campos. Por favor, confirme a gravação desta nova versão do formulário.',
+	'construire_aucun_champs' => 'No momento, não há nenhum campo no formulário.',
+	'construire_confirmer_supprimer_champ' => 'Você quer realmente excluir este campo?',
+	'construire_info_nb_champs_masques' => '@nb@ champ(s) masqué(s) le temps de configurer le groupe.', # RELIRE
+	'construire_position_explication' => 'Indique o campo que este deve preceder.',
+	'construire_position_fin_formulaire' => 'No fim do formulário',
+	'construire_position_fin_groupe' => 'No fim do grupo @groupe@',
+	'construire_position_label' => 'Posição do campo',
+	'construire_reinitialiser' => 'Reverter o formulário',
+	'construire_reinitialiser_confirmer' => 'Você perderá todas as suas modificações. Você quer realmente reverter à versão inicial do formulário?',
+	'construire_verifications_aucune' => 'Nenhuma',
+
+	// L
+	'label_jour' => 'Dia',
+	'label_mois' => 'Mes',
+
+	// O
+	'option_aff_art_interface_explication' => 'Exibir somente as matérias da idioma do usuário',
+	'option_aff_art_interface_label' => 'Exibição multilíngue',
+	'option_aff_langue_explication' => 'Exibi a idioma da matéria ou da seção selecionada ante do titulo',
+	'option_aff_langue_label' => 'Exibir a idioma',
+	'option_aff_rub_interface_explication' => 'Exibir somente as seções da idioma do usuário',
+	'option_aff_rub_interface_label' => 'Exibição multilíngue',
+	'option_attention_explication' => 'Uma mensagem mais importante que a explicação.',
+	'option_cacher_option_intro_label' => 'Esconder a primeira opção vazia.',
+	'option_choix_alternatif_label' => 'Permitir a proposição duma outra opção',
+	'option_choix_alternatif_label_defaut' => 'Outra opção',
+	'option_choix_alternatif_label_label' => 'Rótulo desta outra opção',
+	'option_choix_destinataires_explication' => 'Um ou vários autores que o usuário poderia escolher. Si nenhum autor esta selecionado, vai com o usuário que instalou o site.',
+	'option_choix_destinataires_label' => 'Destinatários possíveis',
+	'option_class_label' => 'Classes CSS suplementarias',
+	'option_cols_explication' => 'Largura do bloco (em números de caracteres). Este opção não esta sempre aplicada, por a causa que os estilos de seu navegador podem cancelar eles.',
+	'option_datas_explication' => 'Você deve informar uma escolha para cada linha desse jeito "key|Titulo da escolha"',
+	'option_datas_label' => 'Lista das opções possíveis',
+	'option_defaut_label' => 'Valor padrão',
+	'option_disable_avec_post_explication' => 'Igual na opção anterior, mas envia ainda o valor dentro um campo escondido.',
+	'option_disable_avec_post_label' => 'Desativar mas enviar',
+	'option_disable_explication' => 'O campo não pode mas estar foco.',
+	'option_disable_label' => 'Desativar o campo',
+	'option_explication_explication' => 'Si precisa, uma pequena sentencia descrevendo o objeto do campo.',
+	'option_explication_label' => 'Explicação',
+	'option_groupe_utilisation' => 'Utilização',
+	'option_groupe_validation' => 'Validação',
+	'option_info_obligatoire_explication' => 'Você pode alterar o valor padrão da indicação d’obrigação : <i>[Obrigatório]</i>.',
+	'option_info_obligatoire_label' => 'Indicação d’obrigação',
+	'option_inserer_barre_choix_edition' => 'barra d’edição completa',
+	'option_inserer_barre_choix_forum' => 'barra dos fóruns',
+	'option_inserer_barre_explication' => 'Inserir uma barra de ferramentas do Porte Plume, si esse plugin esta ativado.',
+	'option_inserer_barre_label' => 'Inserir uma barra de ferramentas ',
+	'option_label_case_label' => 'Etiqueta localizada no lado de caixa de verificação',
+	'option_label_explication' => 'O titulo que será exibido.',
+	'option_label_label' => 'Etiqueta',
+	'option_maxlength_explication' => 'O usuário não poderia teclar mais caracteres que esse número.',
+	'option_maxlength_label' => 'Número máximo de caracteres.',
+	'option_multiple_explication' => 'O usuário poderia selecionar vários valores.',
+	'option_multiple_label' => 'Seleção múltipla',
+	'option_nom_explication' => 'Um nome informático que vai identificar o campo.  Só pode conter letras minúsculas, números e traços "_".',
+	'option_obligatoire_label' => 'Campo obrigatório',
+	'option_option_intro_label' => 'Etiqueta da primeira escolha vazia.',
+	'option_previsualisation_explication' => 'Si o plugin Porte Plume esta ativado, adiciona uma aba para visualizar o texto digitado.',
+	'option_previsualisation_label' => 'Ativar a visualização',
+	'option_readonly_explication' => 'O campo pode ser lido, selecionado, mas não pode ser alterado.',
+	'option_readonly_label' => 'Só leitura',
+	'option_rows_explication' => 'Altura do bloco em número de linhas. Esta opção não esta sempre aplicada, porque os estilos CSS do seu navegador pode cancelar eles.',
+	'option_rows_label' => 'O número informando quantas linhas tem.',
+	'option_type_label' => 'Tipo do campo',
+	'option_type_password' => 'Texto escondido durante o enchimento.',
+	'option_type_text' => 'Normal',
+
+	// S
+	'saisie_case_explication' => 'Permite de ativar ou desativar algo.',
+	'saisie_case_titre' => 'Caixa de verificação única.',
+	'saisie_checkbox_explication' => 'Permite de escolher varias opções com caixas de verificação.',
+	'saisie_checkbox_titre' => 'Caixas de verificação.',
+	'saisie_date_explication' => 'Permite de inserir uma data com a ajuda do calendário.',
+	'saisie_radio_defaut_choix1' => 'Um',
+	'saisie_radio_defaut_choix2' => 'Dois',
+	'saisie_radio_defaut_choix3' => 'Três',
+
+	// T
+	'tout_selectionner' => 'Selecionar todo'
+);
+
+?>
