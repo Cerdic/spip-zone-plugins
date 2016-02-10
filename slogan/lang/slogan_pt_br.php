d5bf565ebcb4482203ed474272c41b0ca819df2b
rporto@calepino.com <rporto@calepino.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-02-10 04:35:10 +0000

    [Salvatore] [source:_plugins_/slogan/lang/ slogan] Export depuis http://trad.spip.net de la langue pt_br
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@95066 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ slogan/lang/slogan_pt_br.php
@@ -0,0 +1,22 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/slogan?lang_cible=pt_br
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'config_info_enregistree' => 'A configuração foi gravada',
+
+	// I
+	'info_slogan_site' => 'Slogan do seu site',
+
+	// T
+	'titre_identite_site' => 'Identidade do site'
+);
+
+?>
