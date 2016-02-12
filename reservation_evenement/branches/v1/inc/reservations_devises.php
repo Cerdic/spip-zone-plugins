c318d8b748cba9c3439d9a030426cfadc8a052ca
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-02-12 07:49:08 +0000

    donner les devises utilisées
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@95097 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ reservation_evenement/branches/v1/inc/reservations_devises.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Script de définition.
+ *
+ * @plugin     Réservation Événements
+ * @copyright  2013 -
+ * @author     Rainer Müller
+ * @licence    GNU/GPL
+ * @package    SPIP\Reservation_evenement\Pipelines
+ */
+
+
+/**
+ * Définit les devises disponibles
+ *
+ * @pipeline affiche_auteurs_interventions
+ * @param  array $flux Données du pipeline
+ * @return array       Données du pipeline
+ */
+if (!defined('_ECRIRE_INC_VERSION'))
+	return;
+
+
+function inc_reservations_devises_dist() {
+
+	if (test_plugin_actif('prix_objets')) {
+		include_spip('inc/config');
+		$config=lire_config('prix_objets/devises',array('EUR'));
+		$devises = array();
+		foreach ($config AS $devise) {
+			$devises[$devise] = traduire_devise($devise);
+		}
+	}
+	else {
+		$devise = array('EUR' => '€');
+	}
+
+	return $devises;
+}
