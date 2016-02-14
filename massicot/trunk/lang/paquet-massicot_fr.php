069aaf1335b910a169bae98e8cb01f70bbfbc009
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-02-14 04:33:00 +0000

    [Salvatore] [source:_plugins_/massicot/trunk/lang/ paquet-massicot] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@95117 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- massicot/trunk/lang/paquet-massicot_fr.php
+++ massicot/trunk/lang/paquet-massicot_fr.php
@@ -1,12 +1,16 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
-if (!defined('_ECRIRE_INC_VERSION')) return;
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/massicot/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-    // M
-    'massicot_description' => 'Ce plugin permet de modifier la taille et de recadrer les logos et les images directement dans l\'espace privé de SPIP.',
-    'massicot_nom' => 'Massicot',
-    'massicot_slogan' => 'Recadrez vos images dans SPIP !',
+	// M
+	'massicot_description' => 'Ce plugin permet de modifier la taille et de recadrer les logos et les images directement dans l’espace privé de SPIP.',
+	'massicot_nom' => 'Massicot',
+	'massicot_slogan' => 'Recadrez vos images dans SPIP !'
 );
+
+?>
