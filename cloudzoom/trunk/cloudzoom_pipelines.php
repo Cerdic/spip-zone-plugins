ba80e395f0ce2265dfc810edec7b59a3f0ba0aed
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-02-18 17:13:21 +0000

    Compat 3.1 : suppression du plugin.xml, utiliser le pipeline insert_head_css
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@95198 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- cloudzoom/trunk/cloudzoom_pipelines.php
+++ cloudzoom/trunk/cloudzoom_pipelines.php
@@ -1,10 +1,35 @@
 <?php
+/**
+ * Plugin CloudZoom
+ * Distribue sous licence GPL
+ *
+ * @package SPIP\CloudZoom\Pipelines
+ */
 
+if (!defined("_ECRIRE_INC_VERSION")){
+	return;
+}
+
+/**
+ * CSS
+ * @param string $flux
+ * @return string
+ */
+function cloudzoom_insert_head_css($flux){
+	$flux .= '<link rel="stylesheet" type="text/css" href="' . timestamp(find_in_path('css/cloudzoom.css')) . '" media="all" />' . "\n";
+	return $flux;
+
+}
+
+/**
+ * JS
+ * @param string $flux
+ * @return string
+ */
 function cloudzoom_insert_head($flux){
-	$js = find_in_path('js/cloud-zoom.1.0.2.min.js');
-	$flux	.= '<link rel="stylesheet" type="text/css" href="'.generer_url_public('cloudzoom.css').'" media="all" />'."\n"
+	$js = timestamp(find_in_path('js/cloud-zoom.1.0.2.min.js'));
+	$flux .= "<script type='text/javascript' src='$js'></script>\n";
 
-		.  "<script type='text/javascript' src='$js'></script>\n";
 	return $flux;
 
 }
