4ae64836e96e6a250f72b21850367f4d3e4e9e9a
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-02-18 22:22:38 +0000

    Sur le champ #EMAIL des ayants droit uniquement, on ne met plus de mailto automatique dessus, mais on active les traitements de propre() (moins le <p> autour) et ce sont les gens qui feront ce qu'ils voudront.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@95206 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- ayants_droit/trunk/base/ayantsdroit.php
+++ ayants_droit/trunk/base/ayantsdroit.php
@@ -25,7 +25,7 @@ function ayantsdroit_declarer_tables_interfaces($interfaces) {
 	$interfaces['table_des_tables']['droits_ayants'] = 'droits_ayants';
 	$interfaces['table_des_tables']['droits_contrats'] = 'droits_contrats';
 	
-	$interfaces['table_des_traitements']['EMAIL']['droits_ayants'] = _TRAITEMENT_RACCOURCIS;
+	$interfaces['table_des_traitements']['EMAIL']['droits_ayants'] = 'PtoBR(' . _TRAITEMENT_RACCOURCIS . ')';
 	$interfaces['table_des_traitements']['ADRESSE'][]= _TRAITEMENT_RACCOURCIS;
 	$interfaces['table_des_traitements']['CREDITS'][]= str_replace('%s', 'ayantsdroit_traiter_credits(%s)', _TRAITEMENT_RACCOURCIS);
 	
