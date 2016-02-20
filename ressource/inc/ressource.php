6f9ce6d79802e5aa336d2a4d839f9db764564093
fil@rezo.net <fil@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-02-20 20:39:35 +0000

    demander le reload du contenu (?var_mode=reload)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@95279 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- ressource/inc/ressource.php
+++ ressource/inc/ressource.php
@@ -236,6 +236,12 @@ function ressource_meta($res) {
 	if (isset($meta['extension']))
 		$meta['type_document'] = ressource_mime($meta['extension']);
 
+	// demander le reload du contenu (?var_mode=reload)
+	if (_request('var_mode') == 'reload' && autoriser('reload', 'distant')) {
+		include_spip('inc/queue');
+		queue_add_job('copie_locale', 'copier', array($src, 'force'), $file = 'inc/distant', $no_duplicate = true, $time=1, $priority=0);
+	}
+
 	return $meta;
 }
 
