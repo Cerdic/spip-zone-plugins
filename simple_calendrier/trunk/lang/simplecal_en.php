a3bc51d24d0d7eebc058915475ba440d1b7149f8
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-02-20 04:37:30 +0000

    [Salvatore] [source:_plugins_/simple_calendrier/trunk/lang/ simplecal] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@95262 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- simple_calendrier/trunk/lang/simplecal_en.php
+++ simple_calendrier/trunk/lang/simplecal_en.php
@@ -1,196 +1,184 @@
 <?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/simplecal?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// A
+	// A
 	'a_venir' => 'Coming',
-	'auteurs_titre' => 'Author(s)',
-	'auteur_numero' => 'N&deg; author',
 	'auteur_ajouter' => 'Add',
-	'auteur_retirer' => 'Remove',
 	'auteur_aucun' => 'No author !',
-	'auteur_msg_num_manquant' => "Author nb. is missing !",
-	'auteur_msg_num_incorrect' => "Author nb. is incorrect !",
-	'auteur_msg_id_inexistant' => "Auteur nb.@id_auteur@ is missing !",
-	'auteur_msg_id_dejala' => "Nb.@id_auteur@ is already an author !",
-	'auteur_msg_id_pasauteur' => "Nb.@id_auteur@ is not an author for this event !",
-	'auteur_msg_erreur' => 'Error',
 	'auteur_msg_ajout' => 'Author added !',
+	'auteur_msg_erreur' => 'Error',
+	'auteur_msg_id_dejala' => 'Nb.@id_auteur@ is already an author !',
+	'auteur_msg_id_inexistant' => 'Auteur nb.@id_auteur@ is missing !',
+	'auteur_msg_id_pasauteur' => 'Nb.@id_auteur@ is not an author for this event !',
+	'auteur_msg_num_incorrect' => 'Author nb. is incorrect !',
+	'auteur_msg_num_manquant' => 'Author nb. is missing !',
 	'auteur_msg_retrait' => 'Author removed !',
-	'alt_img_logo' => 'Simple-Calendrier',
-	'autres_evenements' => 'Other events',
-	
+	'auteur_numero' => 'N° author',
+	'auteur_retirer' => 'Remove',
+	'auteurs_titre' => 'Author(s)',
 
-// B
-	'bouton_entree' => 'Events',
+	// B
 	'bouton_admin' => 'Event',
+	'bouton_entree' => 'Events',
 
-// C
-	'description_plugin' => 'Calendar',
-	'corbeille_tous' => "@nb@ events in garbage",
-	'corbeille_un' => "1 event in garbage",
-	'config_titre' => "Simple calendrier",
-	'config_refobj_oui' => "Enable text by reference",
-	'config_refobj_non' => "Disable text by reference",
-	'config_refobj_explication' => "Enable 'text by reference' to allow articles/breves to be in calendar.",
-	'config_themes_explication' => "You can choose a theme (color set) for the 'little calendar' in public area.",
-	'config_autorisation_explication' => "You can choose if event creation are enabled for editors.",
-	'config_rubrique_explication' => "If you want to manage a simple calendar for your site, let this option disabled. It is possible to use keywords for categorised your events.<br />But, if you want to manage multiple calendar, and display distinct events for each folder, you can enable it.",
-	'config_themepublic' => "Mini-calendar theme (public section)",
-	'config_blocreference' => "References",
-	'config_blocthemes' => "Themes",
-	'config_blocautorisation' => "Autorisations",
-	'config_blocrubrique' => 'Folders affectation',
+	// C
+	'config_autorisation_explication' => 'You can choose if event creation are enabled for editors.',
 	'config_autorisation_redac_non' => 'Administrators only.',
 	'config_autorisation_redac_oui' => 'Administrators and editors',
-	'config_rubrique_partout' => 'Yes, create events in folder.',
-	'config_rubrique_secteur' => 'Yes, create events in 1st level folder (sector).',
-	'config_rubrique_non' => 'No, it makes no sens for me.',
+	'config_blocautorisation' => 'Autorisations',
+	'config_blocreference' => 'References',
+	'config_blocrubrique' => 'Folders affectation',
+	'config_blocthemes' => 'Themes',
 	'config_champs' => 'Events content',
 	'config_champs_explication' => 'Based on the layout chosen for your site, you can decide which elements in the following list should be available.',
-	
+	'config_refobj_explication' => 'Enable ’text by reference’ to allow articles/breves to be in calendar.',
+	'config_refobj_non' => 'Disable text by reference',
+	'config_refobj_oui' => 'Enable text by reference',
+	'config_rubrique_explication' => 'If you want to manage a simple calendar for your site, let this option disabled. It is possible to use keywords for categorised your events.<br />But, if you want to manage multiple calendar, and display distinct events for each folder, you can enable it.',
+	'config_rubrique_non' => 'No, it makes no sens for me.',
+	'config_rubrique_partout' => 'Yes, create events in folder.',
+	'config_rubrique_secteur' => 'Yes, create events in 1st level folder (sector).',
+	'config_themepublic' => 'Public area theme', # MODIF
+	'config_themes_explication' => 'You can choose a theme (color set) for the ’little calendar’ in public area.', # MODIF
+	'config_titre' => 'Simple calendrier',
+	'corbeille_tous' => '@nb@ events in garbage',
+	'corbeille_un' => '1 event in garbage',
 
-// D
+	// D
 	'date' => 'Date',
-	'dates' => 'Dates',
-	'date_du_au' => 'from @date_debut@ to @date_fin@',
-	'date_le' => '@date@',
-	'date_jusque' => 'to @date@',
+	'date_aout_abbr' => 'Aug.',
+	'date_avril_abbr' => 'Apr.',
 	'date_de_debut' => 'First date',
 	'date_de_fin' => 'Last date',
-	'descriptif' => 'Description',
-	'date_picto_title' => 'Select date with datepicker...',
-	'date_janvier_abbr' => 'Jan.',
-	'date_fevrier_abbr' => 'Feb.',
-	'date_mars_abbr' => 'Mar.',
-	'date_avril_abbr' => 'Apr.',
-	'date_mai_abbr' => 'May',
-	'date_juin_abbr' => 'Jun.',
-	'date_juillet_abbr' => 'Jul.',
-	'date_aout_abbr' => 'Aug.',
-	'date_septembre_abbr' => 'Sep.',
-	'date_octobre_abbr' => 'Oct.',
-	'date_novembre_abbr' => 'Nov.',
 	'date_decembre_abbr' => 'Dec.',
-	'date_precedent' => 'Previous',
-	'date_suivant' => 'Next',
-	'date_lundi_abbr' => 'Mo',
-	'date_mardi_abbr' => 'Tu',
-	'date_mercredi_abbr' => 'We',
-	'date_jeudi_abbr' => 'Th',
-	'date_vendredi_abbr' => 'Fr',
-	'date_samedi_abbr' => 'Sa',
 	'date_dimanche_abbr' => 'Su',
+	'date_du_au' => 'from @date_debut@ to @date_fin@',
+	'date_fevrier_abbr' => 'Feb.',
+	'date_janvier_abbr' => 'Jan.',
+	'date_jeudi_abbr' => 'Th',
+	'date_juillet_abbr' => 'Jul.',
+	'date_juin_abbr' => 'Jun.',
+	'date_jusque' => 'to @date@',
+	'date_le' => '@date@',
+	'date_lundi_abbr' => 'Mo',
+	'date_mai_abbr' => 'May',
+	'date_mardi_abbr' => 'Tu',
+	'date_mars_abbr' => 'Mar.',
+	'date_mercredi_abbr' => 'We',
+	'date_novembre_abbr' => 'Nov.',
+	'date_octobre_abbr' => 'Oct.',
+	'date_picto_title' => 'Select date with datepicker...',
+	'date_precedent' => 'Previous',
+	'date_samedi_abbr' => 'Sa',
+	'date_septembre_abbr' => 'Sep.',
+	'date_suivant' => 'Next',
+	'date_vendredi_abbr' => 'Fr',
+	'dates' => 'Dates',
 	'demo_aucun' => 'Aucun !',
+	'demo_description' => 'A demonstration to see how to use EVENEMENTS loop, with various criteria.',
 	'demo_les_evenements_critere' => 'Events',
-	'demo_description' => "A demonstration to see how to use EVENEMENTS loop, with various criteria.",
 	'demo_voir' => 'See',
-	
-	
-// E
-	'explication_titre' => 'Event Label',
+	'descriptif' => 'Description',
+	'description_plugin' => 'Calendar',
+
+	// E
+	'enregistrer_dabord_une_fois' => 'It’s possible ! <br /><br />But first, please save this page one time...',
+	'entree_evenement_publie' => 'Status of the event :', # MODIF
+	'evenement' => 'Event',
 	'explication_ref' => 'Reference to an article or news (ex : article23 ; breve45)',
 	'explication_texte' => 'Text for event description.',
-	'entree_evenement_publie' => 'Status of the event :',
-	'enregistrer_dabord_une_fois' => "It's possible ! <br /><br />But first, please save this page one time...",
-	'evenement' => "Event",
-	'evenements' => "Events",
-	'evenements_recents' => "Most recent events",
-	
-// F
+	'explication_titre' => 'Event Label',
+
+	// F
 	'filtres' => 'filters',
 	'filtres_rubrique_concernee' => 'concerned folder',
-	
-// H
+
+	// H
 	'html_title' => 'Calendar',
 
-// I
-	'info_titre' => 'Title',
+	// I
+	'icone_modifier_evenement' => 'Edit this event',
+	'icone_nouvel_evenement' => 'Write a new event',
+	'inconnu' => 'unknown',
+	'info_1_evenement' => 'event',
+	'info_a_valider' => '[pending]',
 	'info_date_debut' => 'First date',
 	'info_date_fin' => 'Last date',
-	'info_lieu' => 'Place',
 	'info_descriptif_rapide' => 'Brief description',
-	'info_ref' => 'Text : by reference (1\'st possibility)',
-	'info_texte' => 'Text',
-	'info_texte_2possibilites' => 'Text : specific (2\'nd possibility)',
-	'info_gauche_numero_evenement' => 'EVENT NUMBER',
-	'info_statut_encours' => "In progress",
-	'info_statut_proposee' => "Submitted for publication",
-	'info_statut_validee' => 'Published',
-	'info_statut_refusee' => 'Rejected',
-	'info_statut_poubelle' => 'In the dustbin',
-	'icone_modifier_evenement' => 'Edit this event',
-	'info_en_cours' => "[in progress]",
-	'info_a_valider' => "[pending]",
-	'item_mots_cles_association_evenements' => 'events',
+	'info_en_cours' => '[in progress]',
+	'info_evenement' => 'Event',
 	'info_evenement_libelle' => 'Events',
-	'inconnu' => 'unknown',
 	'info_evenements_liees_mot' => 'Events associated with this keyword',
-	'info_1_evenement' => '1&nbsp;event',
-	'info_nb_evenements' => '@nb@ events',
-	'info_evenement' => 'event',
-	'info_evenements_proposes' => 'Events submitted',
-	'info_tous_evenements_en_redaction' => 'All events in progress',
-	'info_tous_evenements_presents' => 'All events published in this section',
-	'info_tous_evenements_refuses' => 'All refused events',
-	'info_evenements_auteur' => "This author's events",
-	'icone_nouvel_evenement' => 'Write a new event',
-	'icone_evenements' => 'Events',
+	'info_gauche_numero_evenement' => 'EVENT NUMBER',
+	'info_lieu' => 'Place',
+	'info_ref' => 'Text : by reference', # MODIF
+	'info_statut_encours' => 'In progress',
+	'info_statut_poubelle' => 'In the dustbin',
+	'info_statut_proposee' => 'Submitted for publication',
+	'info_statut_refusee' => 'Rejected',
+	'info_statut_validee' => 'Published',
+	'info_texte' => 'Text',
+	'info_texte_2possibilites' => 'Text : specific (2’nd possibility)',
+	'info_titre' => 'Title',
+	'item_mots_cles_association_evenements' => 'events',
 
-// L
-	'liste_evenements_prop' => "Events submitted for publication.",
-	'liste_evenements_publie' => "Events published.",
-	'liste_evenements_prepa' => "Events in progress.",
-	'liste_evenements_refuse' => "Events rejected.",
-	'liste_evenements_poubelle' => 'Event in the dustbin',
-	'logo_evenement' => "LOGO EVENT",
+	// L
 	'lieu' => 'Place',
 	'liste_des_evenements' => 'Events list',
 	'liste_des_evenements_rubrique' => 'Events list for folder',
+	'liste_evenements_poubelle' => 'Event in the dustbin',
+	'liste_evenements_prepa' => 'Events in progress.',
+	'liste_evenements_prop' => 'Events submitted for publication.',
+	'liste_evenements_publie' => 'Events published.',
+	'liste_evenements_refuse' => 'Events rejected.',
+	'logo_evenement' => 'LOGO EVENT',
 
-// M
+	// M
 	'multiples_auteurs' => 'Multi auteurs',
-	
-// R
+
+	// R
+	'raccourcis_demo' => 'Démo...',
 	'raccourcis_ecrire_evenement' => 'Write a new event',
+	'raccourcis_liste_evenements_rubrique' => 'Events list for this folder',
+	'raccourcis_retour' => 'Back',
 	'raccourcis_tous_evenements' => 'All events',
 	'raccourcis_tous_evenements_rubrique' => 'Events for same folder',
-	'raccourcis_retour' => 'Back',
-	'raccourcis_ajouter_date' => 'Add a date',
-	'raccourcis_liste_evenements_rubrique' => 'Events list for this folder',
-	'raccourcis_demo' => 'D&eacute;mo...',
 	'reference_objet' => 'see',
 	'retour_accueil' => 'Back home',
 	'retour_rubrique' => 'Back to folder',
-	
 
-// T
+	// T
+	'terme_evenement' => 'event',
+	'terme_evenements' => 'events',
+	'texte' => 'Text',
+	'texte_issu_autre_objet' => 'Text related to other content',
 	'texte_modifier_evenement' => 'Edit this event',
-	'titre_nouvel_evenement' => 'New event',
-	'terme_evenement' => "event",
-	'terme_evenements' => "events",
+	'titre_boite_refobj' => 'in calendar',
+	'titre_boite_rubrique' => 'Calendar',
+	'titre_evenement_poubelle' => 'Event in the dustbin',
 	'titre_evenement_preparation' => 'Event in progress',
 	'titre_evenement_propose' => 'Event submitted for publication',
 	'titre_evenement_publie' => 'Event published',
 	'titre_evenement_refuse' => 'Event rejected',
-	'titre_evenement_poubelle' => 'Event in the dustbin',
-	'texte_issu_autre_objet' => 'Text related to other content',
-	'texte' => 'Text',
-	'titre_evenements' => 'Events',
+	'titre_evenements' => 'Events', # MODIF
+	'titre_nouvel_evenement' => 'New event',
 	'tous' => 'All',
-	'titre_boite_refobj' => 'in calendar',
-	'titre_boite_rubrique' => 'Calendar',
-	'texte_objet' => "Event",
-	'texte_objets' => "Events",
-	
-// V
-	'validation_titre' => 'Title is required !',
+
+	// V
+	'validation_corriger_svp' => 'Please correct the mistakes...',
 	'validation_date_debut' => 'First date is required.',
 	'validation_date_format' => 'Date format must be : dd/mm/yyyy',
 	'validation_refobj_format' => 'Format must be : [type][id]. ex : article23 ; breve45',
 	'validation_rubrique' => 'Folder is required.',
-	'validation_corriger_svp' => 'Please correct the mistakes...',
-	'validation_type_nexiste_pas' => "@type@ n&deg; @id_objet@ doesn't exist !",
-	
+	'validation_type_nexiste_pas' => '@type@ n° @id_objet@ doesn’t exist !'
 );
 
 ?>
