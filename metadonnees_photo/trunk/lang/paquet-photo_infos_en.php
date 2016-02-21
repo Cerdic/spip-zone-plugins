29f9a870c3cfd7c285983d6e4f35e1d19348ee3a
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-02-21 21:04:27 +0000

    [Salvatore] [source:_plugins_/metadonnees_photo/trunk/lang/ paquet-photo_infos] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@95328 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- metadonnees_photo/trunk/lang/paquet-photo_infos_en.php
+++ metadonnees_photo/trunk/lang/paquet-photo_infos_en.php
@@ -1,24 +1,22 @@
 <?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-photo_infos?lang_cible=en
+// ** ne pas modifier le fichier **
 
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-photo_infos
-// Langue: en
-// Date: 14-11-2011 09:27:17
-// Items: 3
-
-if (!defined('_ECRIRE_INC_VERSION')) return;
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// P
+	// P
 	'photo_infos_description' => 'This plugin allows to view EXIF, IPTC and GPS data from a JPEG file.
 
-EXIF informations are handled without using the PHP\'s EXIF extension, but through a script by Vinay Yadav (under LGPL license).
+EXIF informations are handled without using the PHP’s EXIF extension, but through a script by Vinay Yadav (under LGPL license).
 
 IPTC informations requires the "iptcparse" PHP function and use the class "class_iptc" by Alex Arica.',
 	'photo_infos_nom' => 'Photo metadata',
-	'photo_infos_slogan' => 'Display EXIF, IPTC and GPS data from a JPEG file',
+	'photo_infos_slogan' => 'Display EXIF, IPTC and GPS data from a JPEG file'
 );
+
 ?>
--- metadonnees_photo/trunk/lang/paquet-photo_infos_fr.php
+++ metadonnees_photo/trunk/lang/paquet-photo_infos_fr.php
@@ -1,24 +1,20 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-photo_infos
-// Langue: fr
-// Date: 14-11-2011 09:27:17
-// Items: 3
-
-if (!defined('_ECRIRE_INC_VERSION')) return;
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/metadonnees_photo/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// P
-	'photo_infos_description' => 'Ce plugin permet d\'afficher les informations EXIF, GPS et IPTC d\'un fichier JPEG.
+	// P
+	'photo_infos_description' => 'Ce plugin permet d’afficher les informations EXIF, GPS et IPTC d’un fichier JPEG.
 
-Les informations EXIF sont manipulées, sans recours à l\'extension EXIF de PHP, grâce au script de Vinay Yadav (sous licence LGPL).
+Les informations EXIF sont manipulées, sans recours à l’extension EXIF de PHP, grâce au script de Vinay Yadav (sous licence LGPL).
 
-Les informations IPTC nécessitent la fonction "iptcparse" de PHP et utilisent la classe "class_iptc" d\'Alex Arica.',
+Les informations IPTC nécessitent la fonction "iptcparse" de PHP et utilisent la classe "class_iptc" d’Alex Arica.',
 	'photo_infos_nom' => 'Metadonnées photo',
-	'photo_infos_slogan' => 'Afficher les infos EXIF, GPS et IPTC d\'un fichier JPEG',
+	'photo_infos_slogan' => 'Afficher les infos EXIF, GPS et IPTC d’un fichier JPEG'
 );
+
 ?>
