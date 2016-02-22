c7ff4040318e15f125cb11733498f768fed9d76c
klaus@spip.de <klaus@spip.de@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-02-22 04:33:07 +0000

    [Salvatore] [source:_plugins_/chosen/trunk/lang/ chosen] Export depuis http://trad.spip.net de la langue de
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@95330 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ chosen/trunk/lang/chosen_de.php
@@ -0,0 +1,33 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/chosen?lang_cible=de
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cfg_exemple' => 'Beispiel',
+	'cfg_exemple_explication' => 'Erläuterung des Beispiels',
+	'cfg_titre_parametrages' => 'Einstellungen',
+	'chosen_titre' => 'Chosen',
+
+	// E
+	'explication_selecteur' => 'Geben Sie die Chosen-Auslöser an. (CSS oder jQuery Erweiterung)',
+
+	// L
+	'label_active' => '"Chosen" im öffentlichen Bereich aktivieren',
+	'label_selecteur' => 'ID',
+	'lang_create_option' => 'Erstellen',
+	'lang_no_result' => 'Kein Ergebnis',
+	'lang_select_an_option' => 'Wählen Sie eine Option',
+	'lang_select_some_option' => 'Wählen Sie eine oder mehrere Optionen.',
+
+	// T
+	'titre_page_configurer_chosen' => 'Chosen konfigurieren'
+);
+
+?>
