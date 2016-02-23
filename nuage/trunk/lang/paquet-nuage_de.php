92662cf6893760d8458c35176986b96eea7c564a
klaus@spip.de <klaus@spip.de@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-02-23 04:39:40 +0000

    [Salvatore] [source:_plugins_/nuage/trunk/lang/ paquet-nuage] Export depuis http://trad.spip.net de la langue de
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@95369 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ nuage/trunk/lang/paquet-nuage_de.php
@@ -0,0 +1,23 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-nuage?lang_cible=de
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// N
+	'nuage_description' => 'Dieses Plugin ermöglicht die Darstellung einer Schlagwort-Übersicht, wobei die Schriftgröße von der Anzahl der Artikel abhängt, denen ein Schlagwort zugeordnet ist. SPIP kann so "Tags" erzeugen.
+
+Es stehen mehrere Anzeige-Modelle zur Verfügung:
+
+-* article_nuage : Zeigt Schlagworte in unterschiedlicher Größe je nach Häufigkeit ihrer Verwendung für Artikel der Website.
+-* rubrique_nuage :  Zeigt Schlagworte einer Hauptrubrik in unterschiedlicher Größe je nach Häufigkeit ihrer Verwendung für Artikel dieser Haupt- und ihrer Unterrubriken.
+-* nuage_popularite : Zeigt Schlagworte der Website (oder einer Schlagwortgruppe) in unterschiedlicher Größe je nach ihrer relativen Popularität (Summe der Popularität der Artikel mit diesem Schlagwort).',
+	'nuage_slogan' => 'Schlagworte mit unterschiedlichen Schriftgrößen darstellen.'
+);
+
+?>
