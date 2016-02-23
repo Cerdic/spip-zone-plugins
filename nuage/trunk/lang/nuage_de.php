88c01088ef444a1e2d3e2c6d0a4cb1aa2d7db497
klaus@spip.de <klaus@spip.de@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-02-23 04:39:23 +0000

    [Salvatore] [source:_plugins_/nuage/trunk/lang/ nuage] Export depuis http://trad.spip.net de la langue de
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@95368 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ nuage/trunk/lang/nuage_de.php
@@ -0,0 +1,21 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/nuage?lang_cible=de
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// N
+	'noisette_description' => 'Schlagwort-Cloud anzeigen',
+	'noisette_label_afficher_titre' => 'Titel anzeigen:',
+	'noisette_label_niveau_titre' => 'Ebene des Titels wählen',
+	'noisette_nom_noisette' => 'Cloud',
+	'noisette_titre_defaut' => 'Tags',
+	'noisette_titre_noisette' => 'Titel:'
+);
+
+?>
