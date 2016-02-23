c36700945f864aa5bdc1fbace7e00787e5ca7154
klaus@spip.de <klaus@spip.de@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-02-23 04:38:10 +0000

    [Salvatore] [source:_plugins_/mesfavoris/trunk/lang/ paquet-mesfavoris] Export depuis http://trad.spip.net de la langue de
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@95364 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ mesfavoris/trunk/lang/paquet-mesfavoris_de.php
@@ -0,0 +1,17 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-mesfavoris?lang_cible=de
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// M
+	'mesfavoris_description' => 'SPIP-Objekte aus- oder abwählen, die ein angemeldeter Besucher als Lesezeichen speichern kann.',
+	'mesfavoris_slogan' => 'Lesezeichen auswählen'
+);
+
+?>
