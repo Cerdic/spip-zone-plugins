3c71ea05aa755f150b86eeebd5207be7c1b0aaa1
rporto@calepino.com <rporto@calepino.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-02-24 04:33:13 +0000

    [Salvatore] [source:_plugins_/blocsdepliables/lang/ blocsdepliables] Export depuis http://trad.spip.net de la langue pt_br
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@95417 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ blocsdepliables/lang/blocsdepliables_pt_br.php
@@ -0,0 +1,47 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/blocsdepliables?lang_cible=pt_br
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'bloc_deplier' => 'Expandir',
+	'bloc_replier' => 'Contrair',
+	'blocsdepliables_titre' => 'Blocos Expansíveis',
+
+	// C
+	'cfg_exemple' => 'Exemplo',
+	'cfg_exemple_explication' => 'Explicação deste exemplo',
+	'cfg_titre_parametrages' => 'Configurações',
+
+	// E
+	'explication_cookie' => 'O status dos blocos numerados será armazenado em cookie pelo tempo da sessão, para conservar a aparência da página, em caso de retorno.',
+	'explication_unique' => 'A abertura de um bloco provocará o fechamento de todos os outros blocos da página, mantendo apenas um aberto.',
+
+	// L
+	'label_animation' => 'Animação',
+	'label_animation_aucun' => 'Nenhum',
+	'label_animation_fast' => 'Deslizamento rápido',
+	'label_animation_millisec' => 'Deslizamento durando:',
+	'label_animation_normal' => 'Deslizamento normal',
+	'label_animation_slow' => 'Deslizamento lento',
+	'label_balise_titre' => 'Tag para os títulos dos blocos',
+	'label_cookie_1' => 'Memorizar o status dos blocos',
+	'label_unique_1' => 'Um único bloco aberto por página',
+
+	// P
+	'pp_blocs_bloc' => 'Inserir um bloco fechado',
+	'pp_blocs_visible' => 'Inserir um bloco aberto',
+	'pp_un_titre' => 'Um título',
+	'pp_votre_texte' => 'Coloque o seu texto aqui',
+
+	// T
+	'titre_page_configurer_blocsdepliables' => 'Blocos expansívels'
+);
+
+?>
