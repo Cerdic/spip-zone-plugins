3aac9662700644c5e74a0feca99be1813ab07d15
rporto@calepino.com <rporto@calepino.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-02-24 04:33:30 +0000

    [Salvatore] [source:_plugins_/blocsdepliables/lang/ paquet-blocsdepliables] Export depuis http://trad.spip.net de la langue pt_br
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@95418 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ blocsdepliables/lang/paquet-blocsdepliables_pt_br.php
@@ -0,0 +1,18 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-blocsdepliables?lang_cible=pt_br
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'blocsdepliables_description' => 'Permite criar blocos que se tornam visíveis ou invisíveis ao se clicar no título.',
+	'blocsdepliables_nom' => 'Blocos Expansíveis',
+	'blocsdepliables_slogan' => 'Blocos expansíveis'
+);
+
+?>
