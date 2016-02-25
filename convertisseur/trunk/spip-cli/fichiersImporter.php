67aa99784b9bfcdafc1313f011ca8ef30de20b8a
booz@rezo.net <booz@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-02-25 17:16:47 +0000

    importer des milliers d'articles en quelques secondes avec  spip-cli : spip import -s spip_articles_txt
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@95494 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- convertisseur/trunk/spip-cli/fichiersImporter.php
+++ convertisseur/trunk/spip-cli/fichiersImporter.php
@@ -34,6 +34,13 @@ class fichiersImporter extends Command {
 				'Répertoire source',
 				'conversion_spip'
 			)
+			->addOption(
+				'dest',
+				'd',
+				InputOption::VALUE_OPTIONAL,
+				'id_rubrique de la rubrique où importer les numéros et leurs articles',
+				'1'
+			)
 		;
 	}
 
@@ -44,6 +51,7 @@ class fichiersImporter extends Command {
 		include_spip("iterateur/data");
 		
 		$source = $input->getOption('source') ;
+		$id_parent = $input->getOption('dest') ;		
 				
 		// Répertoire source, ou arrivent les fichiers Quark (/exports_quark par défaut).
 		if(!is_dir($source))
@@ -58,15 +66,42 @@ class fichiersImporter extends Command {
 			}
 			// Si c'est bon on continue
 			else{
-				$output->writeln("<info>C'est parti pour un petit import !</info>");
+				$output->writeln("<info>C'est parti pour un petit import de '$source/' dans la rubrique $id_rubrique...</info>");
 					
-				$fichiers_xml = preg_files($source . "/", "(?:(?<!\.metadata\.)txt$)");
-				
-				$output->writeln("\n<info>" . sizeof($fichiers_xml) . " fichiers à importer dans $source/</info>");
+				$fichiers = preg_files($source . "/", "(?:(?<!\.metadata\.)txt$)");
+				$output->writeln("\n<info>" . sizeof($fichiers) . " fichiers à importer dans $source/</info>");
 
-				foreach($fichiers_xml as $f){
-					$fichier = 	preg_replace("/$source.*collections\//","",$f);							
-					$output->writeln("$fichier");
+				foreach($fichiers as $f){
+					$fichier = 	basename($f) ;
+					preg_match("/^(\d{4})-\d{2}/", $fichier, $m);
+					$numero = $m[0];
+					
+					// chopper l'id_parent dans le fichier ?
+					include_spip("inc/flock");										
+					lire_fichier($f, $texte);
+					
+					// menage
+					//@@COLLECTION:esRetour ligne automatique
+					//@@SOURCE:article914237.html
+					
+					$texte = preg_replace("/@@COLLECTION.*/", "", $texte);
+					$texte = preg_replace("/@@SOURCE.*/", "", $texte);
+										
+					// faut il creer des rubriques ?
+					$id_rubrique = sql_getfetsel("id_rubrique", "spip_rubriques", "titre='$numero'");
+					
+					if(!$id_rubrique){
+						include_spip("inc/rubriques");
+						$annee = $m[1] ;
+						$id_rubrique = creer_rubrique_nommee($annee . "/" . $numero, $id_parent);
+						$output->writeln("<info>Creation de la rubrique $annee / $numero => $id_rubrique</info>");						
+					}
+					// inserer l'article
+					include_spip("inc/convertisseur");
+					$GLOBALS['auteur_session']['id_auteur'] = 1 ;				
+					inserer_conversion($texte, $id_rubrique, $f);
+					$output->writeln("<info>Insertion de l'article $f</info>");						
+										
 				}	
 
 			}
