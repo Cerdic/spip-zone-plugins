35cf08ab82b1551e2e6eb419d1e56c7c2838be1f
klaus@spip.de <klaus@spip.de@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-02-25 04:34:25 +0000

    [Salvatore] [source:_plugins_/criteres_suivant_precedent/lang/ suivant_precedent] Export depuis http://trad.spip.net de la langue de
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@95457 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ criteres_suivant_precedent/lang/suivant_precedent_de.php
@@ -0,0 +1,22 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/suivant_precedent?lang_cible=de
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'alt_lien_next' => 'Vorwärts',
+	'alt_lien_prev' => 'Zurück',
+
+	// N
+	'noisette_description' => 'Zeigt Navigationslinks zum nächsten bzw. vorigen Artikel',
+	'noisette_label_meme_rubrique' => 'Navigationslinks nur zu Artikeln der selben Rubrik anzeigen.',
+	'noisette_nom' => 'vorwärts-rückwärts-Links'
+);
+
+?>
