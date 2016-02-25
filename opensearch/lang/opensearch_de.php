05e8c3d50a2e792600bc4c764cf7e55de2453fe4
klaus@spip.de <klaus@spip.de@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-02-25 04:35:30 +0000

    [Salvatore] [source:_plugins_/opensearch/lang/ opensearch] Export depuis http://trad.spip.net de la langue de
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@95460 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ opensearch/lang/opensearch_de.php
@@ -0,0 +1,23 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/opensearch?lang_cible=de
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cfg_boite_opensearch' => 'Konfiguration des Plugins OpenSearch',
+	'cfg_descr_opensearch' => 'OpenSearch ist eine speziell für Ihre Website angepasste Suchmaschine.',
+	'cfg_inf_id_rubrique' => 'Wählen die zu durchsuchende Rubrik.',
+	'cfg_inf_page_recherche' => 'Geben die den Namen des Skeletts an, das für die Suche in Ihrer Website verwendet werden soll (zum Beispiel recherche für das Skelett recherche.html).',
+	'cfg_lbl_id_rubrique' => 'Zielrubrik',
+	'cfg_lbl_page_recherche' => 'Suchskelett',
+	'cfg_option_tout' => 'Ganze Website',
+	'cfg_titre_opensearch' => 'OpenSearch'
+);
+
+?>
