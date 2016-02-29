623dd3c5496798ff1f29b454e778475ce6fe333f
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-02-29 14:06:51 +0000

    On crée la branche v1 avant évolution du trunk/
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@95617 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ medias_dereferencer/branches/v1/lang/medias_dereferencer_fr.php
@@ -0,0 +1,28 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+    return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+    // C
+    'cfg_exemple' => 'Exemple',
+    'cfg_exemple_explication' => 'Explication de cet exemple',
+    'cfg_titre_parametrages' => 'Paramétrages',
+
+    // L
+    'lier_document_choix_non' => 'Non',
+    'lier_document_choix_oui' => 'Oui',
+    'lier_document_explication' => 'Il arrive parfois qu\'un document soit utilisé en raccourcis typographique sans être lié à l\'objet (articles, rubriques, etc.). Lorsque ce cas se présente, désirez-vous que le plugin "Déréférencer les médias" lie le document à l\'objet éditorial ?',
+    'lier_document_label' => 'Désirez-vous lier les documents ?',
+
+    // M
+    'medias_dereferencer_titre' => 'Déréférencer les médias',
+
+    // T
+    'titre_page_configurer_medias_dereferencer' => 'Déréférencer les médias',
+    'titre_page_medias_lister_disallow' => 'Liste des Médias interdits aux moteurs de recherche.',
+);
