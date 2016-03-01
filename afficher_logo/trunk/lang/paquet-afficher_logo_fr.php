241d043318ace5d25c74f1ea9687e30cfe8632c1
chankalan@free.fr <chankalan@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-03-01 14:59:14 +0000

    un logo pour afficher_logo
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@95652 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- afficher_logo/trunk/lang/paquet-afficher_logo_fr.php
+++ afficher_logo/trunk/lang/paquet-afficher_logo_fr.php
@@ -3,6 +3,6 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-	'afficher_logo_description' => 'Afficher un logo dans un texte.',
+	'afficher_logo_description' => 'Afficher un logo (d\'article, de rubrique, d\'auteur, du site...) dans un texte.',
 	'afficher_logo_slogan' => 'Fais voir ton logo !',
 );
