a924275fd127faff1ffe178ddbdc7654a479c73c
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-03-01 11:58:49 +0000

    Import des dernières modifications du trunk (règle de codage)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@95645 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- medias_dereferencer/branches/v1/genie/medias_dereferencer_htaccess.php
+++ medias_dereferencer/branches/v1/genie/medias_dereferencer_htaccess.php
@@ -19,9 +19,9 @@ function genie_medias_dereferencer_htaccess_dist($t) {
 	$message_log = array();
 	$message_log[] = "\n-----";
 	$message_log[] = date_format(date_create(), 'Y-m-d H:i:s');
-	$message_log[] = 'Fonction : '.__FUNCTION__;
+	$message_log[] = 'Fonction : ' . __FUNCTION__;
 	if (session_get('id_auteur')) {
-		$message_log[] = "L'action a été lancé par l'auteur #".session_get('id_auteur').', '.session_get('nom').' ('.session_get('statut').')';
+		$message_log[] = "L'action a été lancé par l'auteur #" . session_get('id_auteur') . ', ' . session_get('nom') . ' (' . session_get('statut') . ')';
 	} else {
 		$message_log[] = "L'action a été lancé par SPIP en tâche de fond.";
 	}
