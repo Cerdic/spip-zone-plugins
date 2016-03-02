243e8183dc715d7a109a646feeb769670226e8de
guillaume.wauquier@declic3000.com <guillaume.wauquier@declic3000.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-03-02 12:07:48 +0000

    Correction de bug  dans le chargement des fichiers et ajout d'une action pour lister les villes et leur coordonnées en ajax
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@95693 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- COG_gps/trunk/coggps_config.php
+++ COG_gps/trunk/coggps_config.php
@@ -8,27 +8,33 @@
 
 function coggps_config_tab_fichier()
 {
-return array('FR'=>array('nom'=>'Geonames : Informations géographiques FRANCE','url_fichier'=>'http://download.geonames.org/export/dump/FR.zip','nom_fichier'=>'geonames_FR.txt'));
+    return array(
+        'FR' => array(
+            'nom' => 'Geonames : Informations géographiques FRANCE',
+            'fichier' => 'http://download.geonames.org/export/dump/FR.zip',
+            'nom_fichier' => 'FR.txt'
+        )
+    );
 }
 
 function coggps_config_correspondance_colonne()
 {
-return array('FR'=>array(
-					'colonnes'=>array(
-						"lon"           =>	5,
-						"lat"           =>	4,
-						"elevation"		=>	15,
-						"elevation_moyenne"	=>	16,
-						"population"	=>	14,
-						"autre_nom"		=>	3),
-					'liaison'=>13,
-					'filtre'=>array('cle'=>7,'valeur'=>'ADM4')
-					)
+    return array(
+        'FR' => array(
+            'colonnes' => array(
+                "lon" => 5,
+                "lat" => 4,
+                "elevation" => 15,
+                "elevation_moyenne" => 16,
+                "population" => 14,
+                "autre_nom" => 3
+            ),
+            'liaison' => 13,
+            'filtre' => array('cle' => 7, 'valeur' => 'ADM4')
+        )
 
-);
+    );
 }
-
-
 
 
 ?>
