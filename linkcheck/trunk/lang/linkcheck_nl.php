f98e96278e20617a17eea049b32a976bd7a58c12
spip@allesfrans.com <spip@allesfrans.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-03-03 04:36:51 +0000

    [Salvatore] [source:_plugins_/linkcheck/trunk/lang/ linkcheck] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@95725 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ linkcheck/trunk/lang/linkcheck_nl.php
@@ -0,0 +1,93 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/linkcheck?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'afficher_alerte' => 'Waarschuwingen tonen',
+	'analyser_site_linkchecks' => 'Analyseer de links op mijn site',
+
+	// C
+	'commencer_parcours' => 'Start met zoeken',
+	'commencer_verification' => 'De links zijn gevonden, je kunt nu met de verificatie beginnen',
+	'commencer_verification_liens' => 'Start de verificatie',
+	'configuration_generale' => 'Algemene configuratie',
+	'configurer_linkcheck' => 'LinkCheck configureren',
+	'continuer_parcours_explication' => 'Ga verder met het zoeken van links',
+
+	// D
+	'descriptif' => 'Deze plugin controleert of links op deze site nog steeds naar een bestaande pagina verwijzen.',
+	'dont' => 'waarvan',
+
+	// E
+	'etat' => 'Status',
+	'etat_' => 'Onbekend',
+	'etat_deplace' => 'Verplaatst',
+	'etat_malade' => 'Twijfelachtig',
+	'etat_mort' => 'Ongeldig',
+	'etat_ok' => 'Geldig',
+
+	// I
+	'id' => 'ID',
+	'info_1_linkcheck' => 'Een link',
+	'info_aucun_linkcheck' => 'Geen enkele link gevonden',
+	'info_nb_linkchecks' => 'links',
+
+	// L
+	'lien' => 'link',
+	'lien_' => 'Niet-gecontroleerde link',
+	'lien_deplace' => 'Verplaatst',
+	'lien_malade' => 'Twijfelachtig',
+	'lien_mort' => 'Ongeldige link',
+	'lien_ok' => 'Geldig',
+	'liens' => 'links',
+	'liens_deplaces' => 'verplaatste links',
+	'liens_invalides' => 'Er bestaan ongeldige links in de inhoud van je site!',
+	'liens_malades' => 'niet te controleren links',
+	'liens_morts' => 'ongeldige links',
+	'liens_oks' => 'gevalideerde links',
+	'linkcheck' => 'LinkCheck',
+	'liste_des_liens' => 'Lijst van links',
+
+	// M
+	'mail_notification1' => 'Hallo,<br/>
+							Je ontvangt deze mail omdat een aantal links op je site problemen geeft.<br/>
+							Hieronder een samenvatting: <br/>',
+	'mail_notification2' => 'Ga naar de beheerpagina van je site om deze problemen op te lossen! Bedankt.',
+	'maj' => 'Aanpassing ',
+	'message_confirmation_reinitialiser' => 'Ben je zeker dat je de tabellen spip_linkchecks en spip_linkchecks_liens wilt ledigen',
+
+	// N
+	'non_verifie' => 'niet gecontroleerd',
+	'non_verifies' => 'niet gecontroleerd',
+	'notifier_par_courriel' => 'Bericht per email',
+
+	// P
+	'parcours_incomplete' => 'Het doorzoeken van links is onvolledig of nog niet uitgevoerd!',
+	'pas_encore_de_liens' => '<strong>Geen enkele link is beoordeeld.</strong> Voer eerst de verificatie uit, wat enkele seconden tot enkele minuten kan duren. Reageert de bladzijde niet, laadt hem dan opnieuw.',
+	'poursuivre_verification' => 'Er is nog niet geverificeerd, druk op de toets voor de verificatie',
+	'poursuivre_verification_liens' => 'Verificatie uitvoeren',
+
+	// R
+	'reinitialiser' => 'Herinitialiseren',
+	'reinitialiser_la_base' => 'Om alle handelingen te resetten, druk je op "Herinitialiseren". De tabellen spip_linkchecks en spip_linkchecks_liens worden geledigd',
+	'rescensement_incomplet' => 'De beoordeling is onvolledig! ',
+
+	// T
+	'tester_liens_linkchecks' => 'Test de links van mijn site',
+	'titre_linkcheck' => 'Verificatie van links',
+	'titre_page_configurer' => 'Configureer plugin Linkcheck',
+	'tous_les_liens' => 'Alle links',
+
+	// U
+	'url' => 'URL',
+	'utilise' => 'gebruikt in '
+);
+
+?>
