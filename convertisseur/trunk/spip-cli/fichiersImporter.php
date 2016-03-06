bdca8383208de5ed5b391fc48f95a62e43f82e97
booz@rezo.net <booz@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-03-06 16:58:01 +0000

    importer les groupes de mots clés
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@95805 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- convertisseur/trunk/spip-cli/fichiersImporter.php
+++ convertisseur/trunk/spip-cli/fichiersImporter.php
@@ -22,7 +22,7 @@ class fichiersImporter extends Command {
 	protected function configure() {
 		$this
 			->setName('fichiers:importer')
-			->setDescription('Importer des fichiers texte SPIP dans spip_articles. `spip import -s %source% -d %id_rubrique%`')
+			->setDescription('Importer des fichiers texte SPIP dans spip_articles.')
 			->setAliases(array(
 				'import' // abbréviation commune pour "import"
 			))
@@ -60,7 +60,7 @@ class fichiersImporter extends Command {
 		}	
 
 		if($id_parent == 0){
-			$output->writeln("<error>Préciser le secteur cible pour importer. spip import -d `id_secteur` </error>\n");
+			$output->writeln("<error>Préciser dans quelle rubrique importer les articles. spip import -d `id_rubrique` </error>\n");
 			exit ;
 		}	
 
@@ -73,7 +73,7 @@ class fichiersImporter extends Command {
 			}
 			// Si c'est bon on continue
 			else{
-				
+
 				// Champs d'un article
 				include_spip("base/abstract_sql");
 				$show = sql_showtable("spip_articles");
@@ -90,17 +90,10 @@ class fichiersImporter extends Command {
 					sql_query("alter table spip_articles add pages TINYTEXT NOT NULL DEFAULT ''");
 				*/
 				
-				// Ajout d'un champs pour stocker les éventuelles ins sans champs.
+				// Ajout d'un champ la premiere fois pour stocker les éventuelles ins qui n'ont pas de champs (peut-être long).
 				if(!in_array('metadonnees', $champs))
 					sql_query("alter table spip_articles add metadonnees MEDIUMTEXT NOT NULL DEFAULT ''");
 
-				// initilaiser le groupe mot technique ?
-				$id_groupe_mot_bdd = sql_getfetsel("id_groupe", "spip_groupe_mots", "titre='mots_importes'");
-				if(!$id_groupe_mot_bdd)
-					$id_groupe_mot_bdd = sql_insertq("spip_groupes_mots", array("titre" => 'mots_importes'));
-
-
-			
 				$fichiers = preg_files($source . "/", "(?:(?<!\.metadata\.)txt$)", 100000);
 
 				// start and displays the progress bar
@@ -112,9 +105,10 @@ class fichiersImporter extends Command {
 				$progress->start();
 
 				foreach($fichiers as $f){
+
 					$fichier = 	basename($f) ;
 					preg_match("/^(\d{4})-\d{2}/", $fichier, $m);
-					$numero = $m[0];
+					$mois = $m[0];
 					$annee = $m[1] ;
 					
 					// chopper l'id_parent dans le fichier ?
@@ -129,12 +123,15 @@ class fichiersImporter extends Command {
 					$texte = preg_replace("/@@SOURCE.*/", "", $texte);
 					
 					// Si des <ins> qui correspondent à des champs metadonnees connus,on les ajoute.
-					
-					$champs_metadonnees = array("motscles", "auteurs", "hierarchie");
+					$champs_metadonnees = array("mots_cles", "auteurs", "hierarchie");
+					$hierarchie = "" ;
+					$auteurs = "" ;
+					$mots_cles = "" ;					
+	
 					if (preg_match_all(",<ins[^>]+class='(.*?)'>(.*?)</ins>,ims", $texte, $z, PREG_SET_ORDER)){
 						foreach($z as $d){
 							if(in_array($d[1], $champs_metadonnees)){
-								// retenir
+								// class="truc" => $truc
 								$$d[1] = split("@@", $d[2]);
 								// virer du texte
 								$texte = substr_replace($texte, '', strpos($texte, $d[0]), strlen($d[0]));
@@ -145,120 +142,99 @@ class fichiersImporter extends Command {
 					if($hierarchie){
 						$titre_parent = $hierarchie[0] ;
 						$titre_rubrique = $hierarchie[1] ;
-						
-						// petit hack pour les rubriques par mois
-						if((intval($titre_rubrique) and intval($titre_rubrique) <= 12) OR $titre_rubrique = "00")
-							$titre_rubrique = "$titre_parent-$titre_rubrique";
+
+						$titre_rubrique = preg_replace(",^(\d{4})/(\d{2})$,", "\\2", $titre_rubrique); // perso diplo
+
 					}else{
 						$titre_parent = $annee ;
-						$titre_rubrique = "$annee-$numero" ;
-					}
-															
-					// faut il creer des rubriques ?
-					$id_rubrique = sql_getfetsel("id_rubrique", "spip_rubriques", "titre='$titre_rubrique'");
-					
-					if(!$id_rubrique){
-						include_spip("inc/rubriques");
-						$id_rubrique = creer_rubrique_nommee("$titre_parent/$titre_rubrique", $id_parent);
-						$progress->clear();
-						$progress->setMessage(" Création de rubrique $titre_parent/$titre_rubrique => $id_rubrique ", 'inforub');
-						$progress->display();
-											
+						$titre_rubrique = "$annee-$mois" ;
 					}
 					
+					include_spip("inc/rubriques");
+					$id_rubrique = creer_rubrique_nommee("$titre_parent/$titre_rubrique", $id_parent);
+					$progress->setMessage(" Création de rubrique $titre_parent/$titre_rubrique => $id_rubrique ", 'inforub');
+																
 					$progress->setMessage("", 'mot');
 					$progress->setMessage("", 'auteur');
 					
 					// inserer l'article
 					include_spip("inc/convertisseur");
-					$GLOBALS['auteur_session']['id_auteur'] = 1 ;				
+
+					// auteur par défaut 
+					$id_admin = sql_getfetsel("id_auteur", "spip_auteurs", "id_auteur=1");
+					$id_admin = ($id_admin)? $id_admin : 12166 ;
+
+					$GLOBALS['auteur_session']['id_auteur'] = $id_admin ;
+			
 					if($id_article = inserer_conversion($texte, $id_rubrique, $f)){
 						
 						// l'auteur existe t'il ? Le créer.
 						if($auteurs){
+
 							foreach($auteurs as $auteur){
-								$id_auteur_bdd = sql_getfetsel("id_auteur", "spip_auteurs", "nom=" . sql_quote($auteur));
-								if(!$id_auteur_bdd){
+								$id_auteur = sql_getfetsel("id_auteur", "spip_auteurs", "nom=" . sql_quote($auteur));
+								if(!$id_auteur){
 									$id_auteur = sql_insertq("spip_auteurs", array(
-    									"nom" => $auteur,
-    									"statut" => "1comite"
+    										"nom" => $auteur,
+    										"statut" => "1comite"
     								));
-    								// spip 3
-    								if($spip_version_branche > "3")
-										sql_insertq("spip_auteurs_liens", array(
+    								$progress->setMessage("Création de l'auteur " . $auteur, 'auteur');
+								}
+							
+								if($spip_version_branche > "3"){
+									if(!sql_getfetsel("id_auteur", "spip_auteurs_liens", "id_auteur=$id_auteur and id_objet=$id_article and objet='article'"))
+   										sql_insertq("spip_auteurs_liens", array(
 	    									"id_auteur" => $id_auteur,
 	    									"id_objet" => $id_article,
 	    									"objet" => "article"
 	    								));
-	    							else // spip 2
+	    						}else // spip 2
+									if(!sql_getfetsel("id_auteur", "spip_auteurs_articles", "id_auteur=$id_auteur and id_article=$id_article"))
 										sql_insertq("spip_auteurs_articles", array(
-	    									"id_auteur" => $id_auteur,
-	    									"id_article" => $id_article
-	    								));
-	    									
-    								$progress->setMessage("Création de l'auteur " . $auteur, 'auteur');
-								}else{
-									if(!sql_getfetsel("id_auteur", "spip_auteurs_liens", "id_auteur=$id_auteur_bdd and id_objet=$id_article and objet='article'"))
-	   									// spip 3
-	   									if($spip_version_branche > "3")
-											sql_insertq("spip_auteurs_liens", array(
-		    									"id_auteur" => $id_auteur_bdd,
-		    									"id_objet" => $id_article,
-		    									"objet" => "article"
-		    								));
-		    							else // spip 2
-											sql_insertq("spip_auteurs_articles", array(
-	    										"id_auteur" => $id_auteur,
-	    										"id_article" => $id_article
-	    									));
-								}	
+    										"id_auteur" => $id_auteur,
+    										"id_article" => $id_article
+    									));
+								
 							}
 						}
-						
-											
-						// l'auteur existe t'il ? Le créer.
-						if($motscles){
-							foreach($motscles as $mot){
-								$id_mot_bdd = sql_getfetsel("id_mot", "spip_mots", "titre=" . sql_quote($mot));
-								if(!$id_mot_bdd){
+				
+						// Créer des mots clés ?
+						if($mots_cles){
+							foreach($mots_cles as $mot){
+								// groupe mot-clé
+								list($type_mot,$titre_mot) = explode("::", $mot);
+								$type_mot = ($type_mot)? $type_mot : "Mots importés" ;
+
+								$id_groupe_mot = sql_getfetsel("id_groupe", "spip_groupes_mots", "titre=" . sql_quote($type_mot));
+								if(!$id_groupe_mot)
+									$id_groupe_mot = sql_insertq("spip_groupes_mots", array("titre" => $type_mot));								
+
+								$id_mot = sql_getfetsel("id_mot", "spip_mots", "titre=" . sql_quote($titre_mot));
+								if(!$id_mot){
 									$id_mot = sql_insertq("spip_mots", array(
-    									"titre" => $mot,
-    									"type" => "mots_importes",
-    									"id_groupe" => $id_groupe_mot_bdd
+    									"titre" => $titre_mot,
+    									"type" => $type_mot,
+    									"id_groupe" => $id_groupe_mot
     								));
-    								// spip 3
-    								if($spip_version_branche > "3")
+   									$progress->setMessage("Création du mot " . $titre_mot . " (" . $type_mot .")", 'mot');
+								}
+
+								if($spip_version_branche > "3"){
+									if(!sql_getfetsel("id_mot", "spip_mots_liens", "id_mot=$id_mot and id_objet=$id_article and objet='article'"))
 										sql_insertq("spip_mots_liens", array(
 	    									"id_mot" => $id_mot,
 	    									"id_objet" => $id_article,
 	    									"objet" => "article"
 	    								));
-	    							else // spip 2
+								}else // spip 2
+									if(!sql_getfetsel("id_mot", "spip_mots_articles", "id_mot=$id_mot and id_article=$id_article"))
 										sql_insertq("spip_mots_articles", array(
-	    									"id_mot" => $id_mot,
-	    									"id_article" => $id_article
-	    								));
-	    									
-    								$progress->setMessage("Création du mot " . $mot, 'mot');
-								}else{
-									if(!sql_getfetsel("id_mot", "spip_mots_liens", "id_mot=$id_mot_bdd and id_objet=$id_article and objet='article'"))
-	   									// spip 3
-	   									if($spip_version_branche > "3")
-											sql_insertq("spip_mots_liens", array(
-		    									"id_mot" => $id_mot_bdd,
-		    									"id_objet" => $id_article,
-		    									"objet" => "article"
-		    								));
-		    							else // spip 2
-											sql_insertq("spip_mots_articles", array(
-	    										"id_mot" => $id_auteur,
-	    										"id_article" => $id_article
-	    									));
-		
-								}	
+    										"id_mot" => $id_mot,
+    										"id_article" => $id_article
+    									));
 							}
 						}
-						
+
 						// Si tout s'est bien passé, on avance la barre
 						$progress->setMessage($f, 'filename');
 						$progress->setFormat("<fg=white;bg=blue>%message%</>\n" . "<fg=white;bg=red>%inforub% %auteur% %mot%</>\n" . '%current%/%max% [%bar%] %percent:3s%% %elapsed:6s%/%estimated:-6s% %memory:6s%' . "\n  %filename%\n\n");
@@ -272,6 +248,8 @@ class fichiersImporter extends Command {
 
 				// ensure that the progress bar is at 100%
 				$progress->finish();
+				$output->writeln("");
+
 			}
 		}
 		else{
