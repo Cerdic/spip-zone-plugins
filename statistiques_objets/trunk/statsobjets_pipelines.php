9db3a6d9968d213c6da39117a9fce3a4bebfd4b9
tcharlss@bravecassine.com <tcharlss@bravecassine.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-03-09 11:00:36 +0000

    Ajoute des stats des objets dans la boite infos, +z
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@95879 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- statistiques_objets/trunk/statsobjets_pipelines.php
+++ statistiques_objets/trunk/statsobjets_pipelines.php
@@ -150,13 +150,14 @@ function admin_statsobjets($objet, $id_objet, $var_preview = "") {
 
 	include_spip('inc/config');
 	include_spip('base/objets'); // au cas où
-	$tables_objets = lire_config('activer_statistiques_objets', array());
+	$tables_objets_configures = lire_config('activer_statistiques_objets', array());
 	$table_objet_sql = table_objet_sql($objet);
 	$id_table_objet = id_table_objet($objet);
 
-	if ($GLOBALS['meta']["activer_statistiques"] != "non"
+	if (
+		$GLOBALS['meta']["activer_statistiques"] == "oui"
 		and $objet != 'article'
-		and in_array($table_objet_sql, $tables_objets)
+		and in_array($table_objet_sql, $tables_objets_configures)
 		and objet_test_si_publie($objet, $id_objet) === true
 		and !$var_preview
 		and autoriser('voirstats')
@@ -185,4 +186,47 @@ function admin_statsobjets($objet, $id_objet, $var_preview = "") {
  **/
 function generer_url_ecrire_statistiques_objet($objet, $id_objet) {
 	return parametre_url(generer_url_ecrire('stats_visites', "objet=$objet"), 'id_objet', $id_objet);
+}
+
+
+/**
+ * Afficher le lien vers la page de statistique sur la vue d'un objet dans l'espace privé (sauf les articles)
+ *
+ * @pipeline boite_infos
+ * @param array $flux Données du pipeline
+ * @return array       Données du pipeline
+ */
+function statsobjets_boite_infos($flux) {
+
+	include_spip('inc/config');
+	include_spip('base/objets'); // au cas où
+	$tables_objets_configures = lire_config('activer_statistiques_objets', array());
+
+	if (
+		$GLOBALS['meta']["activer_statistiques"] == "oui"
+		and isset($flux['args']['type'])
+		and $objet = $flux['args']['type']
+		and isset($flux['args']['id'])
+		and $id_objet = intval($flux['args']['id'])
+	) {
+		$table_objet_sql = table_objet_sql($objet);
+		$id_table_objet = id_table_objet($objet);
+		if (
+			$objet != 'article'
+			and in_array($table_objet_sql, $tables_objets_configures)
+			and autoriser('voirstats', $objet, $id_objet)
+		){
+			$visites = sql_getfetsel('visites', $table_objet_sql, $id_table_objet.'='.intval($id_objet));
+			if ($visites > 0) {
+				$icone_horizontale = chercher_filtre('icone_horizontale');
+				$flux['data'] .= $icone_horizontale(
+					parametre_url(generer_url_ecrire('stats_visites', "objet=$objet"), 'id_objet', $id_objet),
+					_T('statistiques:icone_evolution_visites', array('visites' => $visites)),
+					"statistique-24.png"
+				);
+			}
+		}
+	}
+
+	return $flux;
 }
