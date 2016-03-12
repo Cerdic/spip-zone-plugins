5b6126f3e8263e220415cd628e54c09e81beef76
tcharlss@bravecassine.com <tcharlss@bravecassine.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-03-12 16:56:26 +0000

    Fonctions de suppression des statistiques, +z
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@95935 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ statistiques_objets/trunk/base/delete_referers.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Supprimer les referers
+ *
+ * Surcharge pour prendre en compte tous les objets éditoriaux
+ *
+ * @plugin    Statistiques des objets éditoriaux
+ * @copyright 2016
+ * @author    tcharlss
+ * @licence   GNU/GPL
+ */
+
+if (!defined("_ECRIRE_INC_VERSION")) {
+	return;
+} // securiser
+
+// faudrait plutot recuperer dans inc_serialbase et inc_auxbase
+// mais il faudra prevenir ceux qui affectent les globales qui s'y trouvent
+// Afficher la liste de ce qu'on va detruire et demander confirmation 
+// ca vaudrait mieux
+
+/**
+ * Supprimer les referers
+ *
+ * @param strinf $titre
+ * @param bool $reprise
+ * @return string
+ */
+function base_delete_referers_dist($titre = '', $reprise = '') {
+	if (!$titre) {
+		return;
+	} // anti-testeur automatique
+	sql_delete("spip_referers");
+	sql_delete("spip_referers_articles");
+	sql_delete("spip_referers_objets");
+
+	include_spip('base/objets'); // au cas où
+	$tables_objets = array_keys(lister_tables_objets_sql());
+	$trouver_table = charger_fonction('trouver_table','base');
+	foreach($tables_objets as $table){
+		$desc = $trouver_table($table);
+		if (isset($desc['field']['referers'])){
+			sql_update($table, array('referers' => 0));
+		}
+	}
+
+	// un pipeline pour detruire les tables de referers installees par les plugins ?
+	//pipeline('delete_referers', '');
+
+	spip_log("raz des referers operee redirige vers " . _request('redirect'));
+}
