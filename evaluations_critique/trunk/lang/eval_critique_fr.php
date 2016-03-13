8ee3bd3cce992c3fdcdec6715c48cbaa3b58be60
spip.franck@lien-d-amis.net <spip.franck@lien-d-amis.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-03-13 13:38:01 +0000

    des tab à la place d'espace http://www.spip.net/fr_article3497.html#regles_codage
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@95955 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- evaluations_critique/trunk/lang/eval_critique_fr.php
+++ evaluations_critique/trunk/lang/eval_critique_fr.php
@@ -2,7 +2,7 @@
 
 // Ceci est un fichier langue de SPIP -- This is a SPIP language file
 //
-///  Fichier produit par PlugOnet
+//  Fichier produit par PlugOnet
 // Module: paquet-spipr_dist
 // Langue: fr
 // Date: 29-11-2012 16:35:29
@@ -12,15 +12,15 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-    // A
-    'articles_critique'             => "Les éléments évalués",
+	// A
+	'articles_critique'             => "Les éléments évalués",
 
-    // C
-    'cfg_titre_parametrages'        => "Paramétrages du plugin Critique d'articles",
+	// C
+	'cfg_titre_parametrages'        => "Paramétrages du plugin Critique d'articles",
 
-    // E
-    'erreur_aucun_document'         => 'Aucune image dans la rubrique @id_rubrique@',
-    'erreur_aucun_article_critique' => "Aucun article critiqué dans la rubrique @id_rubrique@",
+	// E
+	'erreur_aucun_document'         => 'Aucune image dans la rubrique @id_rubrique@',
+	'erreur_aucun_article_critique' => "Aucun article critiqué dans la rubrique @id_rubrique@",
 
 )
 ;
--- evaluations_critique/trunk/lang/paquet-eval_critique_fr.php
+++ evaluations_critique/trunk/lang/paquet-eval_critique_fr.php
@@ -4,10 +4,10 @@
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-    // R
-    'eval_critique_nom'         => "Critique d'articles",
-    'eval_critique_slogan'      => "Permet d'évaluer des objets SPIP de type article",
-    'eval_critique_description' => "Ce plugin s'utilise avec le plugin Evaluations; il propose une composition de rubrique, ajoutant un formulaire d'évaluation à la page article.",
+	// R
+	'eval_critique_nom'         => "Critique d'articles",
+	'eval_critique_slogan'      => "Permet d'évaluer des objets SPIP de type article",
+	'eval_critique_description' => "Ce plugin s'utilise avec le plugin Evaluations; il propose une composition de rubrique, ajoutant un formulaire d'évaluation à la page article.",
 
 );
 
