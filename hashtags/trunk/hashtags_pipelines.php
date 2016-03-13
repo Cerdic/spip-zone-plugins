7563c7de0951d76714fb752eec265f8324cde573
spip.franck@lien-d-amis.net <spip.franck@lien-d-amis.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-03-13 12:24:15 +0000

    des tab à la place d'espace http://www.spip.net/fr_article3497.html#regles_codage
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@95952 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- hashtags/trunk/hashtags_pipelines.php
+++ hashtags/trunk/hashtags_pipelines.php
@@ -4,87 +4,87 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 
 function hashtags_post_edition($flux){
 
-    include_spip('inc/meta');
-    $cfg = lire_config('cfg_hashtags');
+	include_spip('inc/meta');
+	$cfg = lire_config('cfg_hashtags');
 
-    $table_objet = $flux['args']['table_objet'];                // 'articles',
-    $objet = $flux['args']['type'];                             // 'article'
-    $id_objet = $flux['args']['id_objet'];                      // '23'
-    
-    if ( isset($table_objet) || array_key_exists($table_objet,$cfg) ) {
+	$table_objet = $flux['args']['table_objet'];                // 'articles',
+	$objet = $flux['args']['type'];                             // 'article'
+	$id_objet = $flux['args']['id_objet'];                      // '23'
 
-        # Patterns Hashtags!
-        $pattern = "~#(!->|->|!)*(\\w|\\')+(?![^<]*[]>|[^<>]*<\\/)~u";
+	if ( isset($table_objet) || array_key_exists($table_objet,$cfg) ) {
 
-        foreach ($cfg[$table_objet]['champs'] as $champ){
+		# Patterns Hashtags!
+		$pattern = "~#(!->|->|!)*(\\w|\\')+(?![^<]*[]>|[^<>]*<\\/)~u";
 
-            if ( preg_match_all($pattern, $flux['data'][$champ], $matches) ) {
+		foreach ($cfg[$table_objet]['champs'] as $champ){
 
-                # Déclarer le tableau de mots à lier
-                $id_mots = array();
+			if ( preg_match_all($pattern, $flux['data'][$champ], $matches) ) {
 
-                # Récuperer les tags contenus  dans le texte et supprimer les doublons éventuels
-                $tags = array_unique($matches['0']);
-                $tags = preg_replace('`#(!->|!|->)*`','',$tags);
+				# Déclarer le tableau de mots à lier
+				$id_mots = array();
 
-                # Verifier si les mots clef n'existent pas déjà sur le groupe demandé
-                if ($req = sql_allfetsel('titre, id_mot', 'spip_mots', sql_in('titre', $tags))) {
-                    foreach ($req as $ligne => $champ) {
-                        $tagsConnus[] = $champ['titre'];
-                        $id_mots[] = $champ['id_mot'];
-                    }
-                }
+				# Récuperer les tags contenus  dans le texte et supprimer les doublons éventuels
+				$tags = array_unique($matches['0']);
+				$tags = preg_replace('`#(!->|!|->)*`','',$tags);
 
-                $tagsACreer = ($tagsConnus)
-                    ? array_diff($tags, $tagsConnus) : $tags;
+				# Verifier si les mots clef n'existent pas déjà sur le groupe demandé
+				if ($req = sql_allfetsel('titre, id_mot', 'spip_mots', sql_in('titre', $tags))) {
+						foreach ($req as $ligne => $champ) {
+						$tagsConnus[] = $champ['titre'];
+						$id_mots[] = $champ['id_mot'];
+						}
+				}
 
-                # Création des mots clefs inexistants et recuperation de leur id respectif
-                if (count($tagsACreer)) {
+				$tagsACreer = ($tagsConnus)
+					? array_diff($tags, $tagsConnus) : $tags;
 
-                    $grpmot = sql_fetsel('titre', 'spip_groupes_mots');
+				# Création des mots clefs inexistants et recuperation de leur id respectif
+				if (count($tagsACreer)) {
 
-                    $ins = array();
-                    foreach ($tagsACreer as $titre)
-                        $ins[] = array(
-                            'titre' => $titre,
-                            'id_groupe' => $cfg[$table_objet]['groupes'],
-                            'type' => $grpmot['titre'] );
+					$grpmot = sql_fetsel('titre', 'spip_groupes_mots');
 
-                    # Inserer tous les mots puis récuperer les ids ...
-                    if (count($ins))
-                        if (sql_insertq_multi("spip_mots", $ins))
-                            if ($req = sql_allfetsel('id_mot', 'spip_mots', sql_in('titre', $tagsACreer)))
-                                foreach ($req as $ligne => $champ)
-                                    $id_mots[] = $champ['id_mot'];
-                }
+					$ins = array();
+					foreach ($tagsACreer as $titre)
+						$ins[] = array(
+							'titre' => $titre,
+							'id_groupe' => $cfg[$table_objet]['groupes'],
+							'type' => $grpmot['titre'] );
 
-                # Charger l'api d édition de liens
-                include_spip('action/editer_liens');
+					# Inserer tous les mots puis récuperer les ids ...
+					if (count($ins))
+						if (sql_insertq_multi("spip_mots", $ins))
+							if ($req = sql_allfetsel('id_mot', 'spip_mots', sql_in('titre', $tagsACreer)))
+								foreach ($req as $ligne => $champ)
+									$id_mots[] = $champ['id_mot'];
+				}
 
-                # Associer les mots clefs a l'objet!
-                if (count($id_mots))
-                    objet_associer(array("mot" => $id_mots), array($objet => $id_objet));
-            }
-        }
-    }
+				# Charger l'api d édition de liens
+				include_spip('action/editer_liens');
 
-    return $flux;
+				# Associer les mots clefs a l'objet!
+				if (count($id_mots))
+					objet_associer(array("mot" => $id_mots), array($objet => $id_objet));
+			}
+		}
+	}
+
+	return $flux;
 }
 
 function hashtags_declarer_tables_interfaces($interfaces) {
 
-    # Ajouter les traitements qui vont bien en fonctions de la config des mot clefs
-    if (isset($GLOBALS['meta']['cfg_hashtags']) AND $cfg_hashtags = $GLOBALS['meta']['cfg_hashtags'])
-        foreach (unserialize($cfg_hashtags) as $k => $v)
-            foreach ($v['champs'] as $champ)
-                $interfaces['table_des_traitements'][strtoupper($champ)][$k] =
-                    isset ( $interfaces['table_des_traitements'][strtoupper($champ)][$k] )
-                        ? str_replace("%s",
-                                      "traitements_hashtags(%s, $v[groupes])",
-                                      $interfaces['table_des_traitements'][strtoupper($champ)][$k])
-                        : str_replace("%s",
-                                      "traitements_hashtags(%s, $v[groupes])",
-                                      $interfaces['table_des_traitements'][strtoupper($champ)][0]);
+	# Ajouter les traitements qui vont bien en fonctions de la config des mot clefs
+	if (isset($GLOBALS['meta']['cfg_hashtags']) AND $cfg_hashtags = $GLOBALS['meta']['cfg_hashtags'])
+		foreach (unserialize($cfg_hashtags) as $k => $v)
+			foreach ($v['champs'] as $champ)
+				$interfaces['table_des_traitements'][strtoupper($champ)][$k] =
+					isset ( $interfaces['table_des_traitements'][strtoupper($champ)][$k] )
+						? str_replace("%s",
+										"traitements_hashtags(%s, $v[groupes])",
+										$interfaces['table_des_traitements'][strtoupper($champ)][$k])
+						: str_replace("%s",
+										"traitements_hashtags(%s, $v[groupes])",
+										$interfaces['table_des_traitements'][strtoupper($champ)][0]);
 
-    return $interfaces;
+	return $interfaces;
 }
