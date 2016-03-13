8eae6c4c5c8afefb448502875c796edba0a012d5
spip.franck@lien-d-amis.net <spip.franck@lien-d-amis.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-03-13 18:24:11 +0000

    des tabs à la place d'espace
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@95964 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- forum_perime/forum_perime_pipelines.php
+++ forum_perime/forum_perime_pipelines.php
@@ -8,19 +8,19 @@ function forum_perime_formulaire_charger($flux){
 
 	// On traite uniquement les forums d'articles
 	if ($flux['args']['form']=="forum"){
-  
-          $objet = $flux['args']['args'][0];
-          $id_objet =  $flux['args']['args'][1];
-          
-          if ($objet=="article") {
-                   include_spip('inc/config');
-                   $duree = intval(lire_config('forum_perime/duree', 0));
-                   if ($duree > 0) {
-                         $date_perimee = date('Y-m-d H:i:s',mktime(date("H") , 0, 0, date("m")  , date("d")-$duree, date("Y")));
-                        if (!sql_countsel("spip_articles", "id_article=".intval($id_objet)." AND date > '$date_perimee'"))
-                                    return false;                           
-                   }
-          }
+
+		$objet = $flux['args']['args'][0];
+		$id_objet =  $flux['args']['args'][1];
+
+			if ($objet=="article") {
+				include_spip('inc/config');
+				$duree = intval(lire_config('forum_perime/duree', 0));
+				if ($duree > 0) {
+					$date_perimee = date('Y-m-d H:i:s',mktime(date("H") , 0, 0, date("m")  , date("d")-$duree, date("Y")));
+						if (!sql_countsel("spip_articles", "id_article=".intval($id_objet)." AND date > '$date_perimee'"))
+						return false;                           
+				}
+			}
 	}
 
 
