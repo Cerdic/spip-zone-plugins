145bbab7e5b4544e3b1124f49a82f9c18bfb0f9d
spip.franck@lien-d-amis.net <spip.franck@lien-d-amis.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-03-20 08:51:23 +0000

    Quand il y a changement de borne de compatibilité, il faut faire un z+1 minimum pour que les sites utilisant le plugin détectent le changement. - j'en profite pour faire un up concernant les necessite en mettant les premières version qui sont sous la forme x.y.z et avec des bornes de compatibilité bien défini. 0.1.4 concernant paypal http://zone.spip.org/trac/spip-zone/browser/_plugins_/paypal/plugin.xml?rev=52581 0.4.6 concernant commandes http://zone.spip.org/trac/spip-zone/browser/_plugins_/commandes/plugin.xml?rev=52556 Cela permet à plugin.spip de fournir aux personnes qui regardent le xml des infos "fiable" - Remplacement d'espaces par des tabs pour être un peu plus conforme avec http://www.spip.net/fr_article3497.html#regles_codage
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@96091 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- commandes_paypal/commandes_paypal_pipelines.php
+++ commandes_paypal/commandes_paypal_pipelines.php
@@ -8,30 +8,29 @@ function commandes_paypal_traitement_paypal($flux){
 	if (
 		$flux['args']['paypal']['custom'] == 'payer_commande'
 		and $reference = $flux['args']['paypal']['invoice']
-		and $commande = sql_fetsel('id_commande, statut, id_auteur', 'spip_commandes', 'reference = '.sql_quote($reference))
-	){
-		$id_commande = $commande['id_commande'];
-		$statut_commande = $commande['statut'];
-		$statut_paypal = $flux['args']['paypal']['payment_status'];
-		$prix_paypal = $flux['args']['paypal']['mc_gross'];
-		
-		// Si le statut Paypal est "Pending" on passe juste la commande en attente et on verra plus tard pour le reste
-		if ($statut_paypal == 'Pending'){
-			$statut_nouveau = 'attente';
-		}
-		// Si Paypal est "Completed" on vérifie que le montant correspond au prix de cette commande
+		and $commande = sql_fetsel('id_commande, statut, id_auteur', 'spip_commandes', 'reference = '.sql_quote($reference))){
+			$id_commande = $commande['id_commande'];
+			$statut_commande = $commande['statut'];
+			$statut_paypal = $flux['args']['paypal']['payment_status'];
+			$prix_paypal = $flux['args']['paypal']['mc_gross'];
+
+			// Si le statut Paypal est "Pending" on passe juste la commande en attente et on verra plus tard pour le reste
+				if ($statut_paypal == 'Pending'){
+					$statut_nouveau = 'attente';
+				}
+				// Si Paypal est "Completed" on vérifie que le montant correspond au prix de cette commande
 		elseif ($statut_paypal == 'Completed'){
 			$fonction_prix = charger_fonction('prix', 'inc/');
 			$prix_commande = $fonction_prix('commande', $id_commande);
-			 
-			 // Si on a pas assez payé
-			 if ($prix_paypal < $prix_commande){
-			 	$statut_nouveau = 'partiel';
-			 }
-			 // Sinon c'est bon
-			 else{
-			 	$statut_nouveau = 'paye';
-			 }
+
+			// Si on a pas assez payé
+			if ($prix_paypal < $prix_commande){
+				$statut_nouveau = 'partiel';
+			}
+			// Sinon c'est bon
+			else{
+				$statut_nouveau = 'paye';
+			}
 		}
 		// Sinon on dit que c'est en erreur
 		else{
@@ -39,13 +38,12 @@ function commandes_paypal_traitement_paypal($flux){
 		}
 
 		spip_log("commandes_paypal_traitement envoi vers instituer $id_commande-$statut_nouveau",'commandes');
-		
+
 		//on institue la commande
 		$action = charger_fonction('instituer_commande', 'action');
 		$action($id_commande."-".$statut_nouveau);
 
 	}
-	
 	return $flux;
 }
 
