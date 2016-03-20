c84c7a090c5465390ed0769c04d4a744b8172180
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-03-20 10:12:25 +0000

    Eviter une notice quand la configuration de l'autorisation n'est pas définie.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@96094 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- minibando/trunk/minibando_autorisations.php
+++ minibando/trunk/minibando_autorisations.php
@@ -7,7 +7,7 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 function autoriser_minibando_dist($faire, $quoi, $id, $qui, $opt) {
 	include_spip('inc/config');
 	$config = lire_config('minibando');
-	if ($config['limite_webmestre'] == 'on' and $qui['webmestre'] != 'oui') {
+	if (isset($config['limite_webmestre']) and $config['limite_webmestre'] == 'on' and $qui['webmestre'] != 'oui') {
 		return false;
 	}
 	return true;
