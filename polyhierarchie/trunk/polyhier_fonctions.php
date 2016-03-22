db6f36f13b039bca05c440abe2684e2f63f7876a
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-03-22 16:14:36 +0000

    Report de r53778 : Aucune raison de limiter la recherche à rubriques et articles : on limite à tous les objets éditoriaux déclarés.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@96144 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- polyhierarchie/trunk/polyhier_fonctions.php
+++ polyhierarchie/trunk/polyhier_fonctions.php
@@ -102,10 +102,8 @@ function critere_parents($idb, &$boucles, $crit, $tous=true) {
 		$where[] = "is_array(\$r=$argp)?sql_in('$primary',\$r):array('=', '$primary', \$r)";
 	}
 
-	if (
-		$tous !== 'directs'
-		and in_array($boucle_parent->type_requete, array('rubriques','articles'))
-	) {
+	if ($tous !== 'directs'
+	  AND in_array(table_objet_sql($boucle_parent->type_requete),array_keys(lister_tables_objets_sql()))){
 		$arg = kwote(calculer_argument_precedent($idb, id_table_objet(objet_type($boucle_parent->type_requete)), $boucles));
 		$type = objet_type($boucle_parent->type_requete);
 		$sous = "sql_get_select('rl.id_parent','spip_rubriques_liens as rl','rl.id_objet='.$arg.' AND rl.objet=\'$type\'')";
