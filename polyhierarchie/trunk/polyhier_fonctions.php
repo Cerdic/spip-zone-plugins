675fe6a461f38b30a04cbd57ec806b1301a8e39e
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-03-22 16:17:00 +0000

    Report de r53779 : Et oups, pareil pour les enfants.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@96145 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- polyhierarchie/trunk/polyhier_fonctions.php
+++ polyhierarchie/trunk/polyhier_fonctions.php
@@ -46,10 +46,8 @@ function critere_enfants($idb, &$boucles, $crit, $tous=true) {
 		$where[] = "is_array(\$r=$arg)?sql_in('$mparent',\$r):array('=', '$mparent', \$r)";
 	}
 
-	if (
-		$tous !== 'directs'
-		and in_array($boucle->type_requete,array('rubriques','articles'))
-	) {
+	if ($tous !== 'directs'
+	  AND in_array(table_objet_sql($boucle_parent->type_requete),array_keys(lister_tables_objets_sql()))){
 		$type = objet_type($boucle->type_requete);
 		$cond = "is_array(\$r=$arg)?sql_in('rl.id_parent',\$r):'rl.id_parent='.\$r";
 		$sous = "sql_get_select('rl.id_objet','spip_rubriques_liens as rl',$cond.' AND rl.objet=\'$type\'')";
