ba2c85d95cefb0cdfefa719829ef96e0d455ebb8
brunobergot@gmail.com <brunobergot@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-03-23 19:03:43 +0000

    règles de codage + bonnes pratiques sécu
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@96170 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- responsivenav/trunk/responsivenav_fonctions.php
+++ responsivenav/trunk/responsivenav_fonctions.php
@@ -1,10 +1,11 @@
 <?php
 
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
 // on définit le point de rupture par défaut
 // il est surchargeable dans le mes_options.php
 // et accepte les em, px...
 define('_RESPONSIVENAV_BREAKPOINT', '40em');
-
 
 ?>
 
--- responsivenav/trunk/responsivenav_pipelines.php
+++ responsivenav/trunk/responsivenav_pipelines.php
@@ -1,4 +1,7 @@
 <?php
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
 /**
  * Utilisations de pipelines par Responsive Nav
  *
@@ -14,21 +17,22 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
  
 // on insert la feuille de styles
 function responsivenav_insert_head_css($flux) {
-    $flux .= '<link rel="stylesheet" href="'.produire_fond_statique('css/responsive-nav.css').'" type="text/css" />';
-    $flux .= '<link rel="stylesheet" href="'.produire_fond_statique('css/responsive-nav.spip.css').'" type="text/css" />';
-    return $flux;
+	$flux .= '<link rel="stylesheet" href="'.produire_fond_statique('css/responsive-nav.css').'" type="text/css" />';
+	$flux .= '<link rel="stylesheet" href="'.produire_fond_statique('css/responsive-nav.spip.css').'" type="text/css" />';
+	return $flux;
 }
 
 // on insert le script
 function responsivenav_insert_head($flux) {
-    $flux .= '<script type="text/javascript" src="'.produire_fond_statique('js/responsive-nav.js').'"></script>';
+	$flux .= '<script type="text/javascript" src="'.produire_fond_statique('js/responsive-nav.js').'"></script>';
+
 	if (test_plugin_actif('Zpip')){
 		$flux .= '<script>$(function() {  var nav = responsiveNav(".menu.menu-container",{jsClass: "js-responsivenav"});}); </script>';
-		} else {
-			$flux .= '<script>$(function() {  var nav = responsiveNav("#nav",{jsClass: "js-responsivenav"});}); </script>';
-		}
-	
-    return $flux;
+	} else {
+		$flux .= '<script>$(function() {  var nav = responsiveNav("#nav",{jsClass: "js-responsivenav"});}); </script>';
+	}
+
+	return $flux;
 }
 
 ?>
