4357835892e2567544ea735aeb21d2d6d9a1ce5c
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-03-25 21:35:41 +0000

    - Le script sur la hauteur #content <> #aside n'a plus lieu d'être ; - Comme les extras sont en ajax, il faut qu'il y ait quelque chose à afficher… Donc, on met un commentaire html pour ne pas avoir une inclusion vide ; - Ajout des autorisations manquantes sur les aside ; - Le BO est accessible uniquement aux administrateurs complets ; - Maj de la TODO ; - Up de z pour le moment.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@96205 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- info_sites/trunk/info_sites_autorisations.php
+++ info_sites/trunk/info_sites_autorisations.php
@@ -22,3 +22,18 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 function info_sites_autoriser() {
 }
 
+/**
+ * Autorisation d'accès è l'espace privé ?
+ * Surcharge de autoriser_ecrire_dist() > ecrire/inc/autoriser.php
+ *
+ * @param  string $faire Action demandée
+ * @param  string $type  Type d'objet sur lequel appliquer l'action
+ * @param  int    $id    Identifiant de l'objet
+ * @param  array  $qui   Description de l'auteur demandant l'autorisation
+ * @param  array  $opt   Options de cette autorisation
+ * @return bool          true s'il a le droit, false sinon
+ **/
+function autoriser_ecrire($faire, $type, $id, $qui, $opt) {
+	return $qui['statut'] == '0minirezo' and !$qui['restreint'];
+}
+
