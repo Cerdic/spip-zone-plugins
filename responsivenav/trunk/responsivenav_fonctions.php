b346a4918ee93e2815217b3bba327a5c55416cdc
jeanmarie.listes@cousumain.info <jeanmarie.listes@cousumain.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-03-27 14:08:06 +0000

    broutille
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@96219 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- responsivenav/trunk/responsivenav_fonctions.php
+++ responsivenav/trunk/responsivenav_fonctions.php
@@ -2,9 +2,8 @@
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
-// On définit le point de rupture et le selecteur par défaut.
+// On définit les principales valeurs par défaut.
 // Surchargeable dans squelettes/mes_fonctions.php
-// http://responsive-nav.com
 define('_RESPONSIVENAV_BREAKPOINT', '40em');
 define('_RESPONSIVENAV_SELECTOR', '#nav');
 define('_RESPONSIVENAV_LABEL', '&#9776; Menu');
