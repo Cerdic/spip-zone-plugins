de1699310667681f03fb2c986a677074564e1516
jeanmarie.listes@cousumain.info <jeanmarie.listes@cousumain.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-03-27 10:02:23 +0000

    - possibilité de surcharger les variables de config prévues par le script - possibilité d'activer l'affichage du menu sous la forme d'un menu burger > non activé par défaut pour être accessible (b_b) - lien vers la doc (en cours)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@96217 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- responsivenav/trunk/responsivenav_fonctions.php
+++ responsivenav/trunk/responsivenav_fonctions.php
@@ -4,8 +4,10 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 
 // On définit le point de rupture et le selecteur par défaut.
 // Surchargeable dans squelettes/mes_fonctions.php
+// http://responsive-nav.com
 define('_RESPONSIVENAV_BREAKPOINT', '40em');
 define('_RESPONSIVENAV_SELECTOR', '#nav');
+define('_RESPONSIVENAV_LABEL', '&#9776; Menu');
 define('_RESPONSIVENAV_JSCLASS', 'js-responsivenav');
 
 ?>
--- responsivenav/trunk/responsivenav_pipelines.php
+++ responsivenav/trunk/responsivenav_pipelines.php
@@ -12,8 +12,6 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
  * @package    SPIP\Responsivenav\Pipelines
  */
 
-if (!defined('_ECRIRE_INC_VERSION')) return;
-
  
 // on insert la feuille de styles
 function responsivenav_insert_head_css($flux) {
@@ -22,11 +20,20 @@ function responsivenav_insert_head_css($flux) {
 	return $flux;
 }
 
-// on insert le script
+// on insert le script et on l'appelle
 function responsivenav_insert_head($flux) {
 	$flux .= '<script type="text/javascript" src="'.find_in_path('javascript/responsive-nav.js').'"></script>';
-	$flux .= '<script>/*<![CDATA[*/ ;(function($){ $(function(){ var nav = responsiveNav("'. _RESPONSIVENAV_SELECTOR .'",{jsClass: "'. _RESPONSIVENAV_JSCLASS .'"}); }); })(jQuery); /*]]>*/ </script>';
+	$flux .= '<script>/*<![CDATA[*/ ;(function($){ $(function(){ var nav = responsiveNav("'. _RESPONSIVENAV_SELECTOR .'",{jsClass: "'. _RESPONSIVENAV_JSCLASS.'"' ;
+	if (defined('_RESPONSIVENAV_ANIMATE')) {$flux .= ',animate: '._RESPONSIVENAV_ANIMATE;}
+	if (defined('_RESPONSIVENAV_TRANSITION')) {$flux .= ',transition: '._RESPONSIVENAV_TRANSITION;}
+	if (defined('_RESPONSIVENAV_LABEL')) {$flux .= ',label: "'._RESPONSIVENAV_LABEL.'"';}
+	if (defined('_RESPONSIVENAV_INSERT')) {$flux .= ',insert: "'._RESPONSIVENAV_INSERT.'"';}
+	if (defined('_RESPONSIVENAV_CUSTOMTOGGLE')) {$flux .= ',customToggle: "'._RESPONSIVENAV_CUSTOMTOGGLE.'"';}
+	if (defined('_RESPONSIVENAV_CLOSEONNAVCLICK')) {$flux .= ',closeOnNavClick: '._RESPONSIVENAV_CLOSEONNAVCLICK;}
+	if (defined('_RESPONSIVENAV_OPENPOS')) {$flux .= ',openPos: "'._RESPONSIVENAV_OPENPOS.'"';}
+	if (defined('_RESPONSIVENAV_NAVCLASS')) {$flux .= ',navClass: "'._RESPONSIVENAV_NAVCLASS.'"';}
+	if (defined('_RESPONSIVENAV_NAVACTIVECLASS')) {$flux .= ',navActiveClass: "'._RESPONSIVENAV_NAVACTIVECLASS.'"';}
+	$flux .= '}); }); })(jQuery); /*]]>*/ </script>';
 	return $flux;
 }
-
 ?>
