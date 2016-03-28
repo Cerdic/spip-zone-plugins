362d5f1295ecf274b3e0f5de2e7965791b0f9fc5
p@henix.be <p@henix.be@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-03-28 12:20:46 +0000

    Ajouter un champ pour la date d'expiration du token
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@96235 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- connecteur/trunk/base/connecteur.php
+++ connecteur/trunk/base/connecteur.php
@@ -17,12 +17,12 @@ function connecteur_declarer_tables_auxiliaires($tables_auxiliaires) {
 		'field' => array(
 			'id_auteur' => 'bigint(21) NOT NULL',
 			'type' => "varchar(25) DEFAULT '' NOT NULL",
-			'token' => 'varchar(255) NOT NULL'
+			'token' => "text NOT NULL DEFAULT ''",
+			'expire' => "datetime NOT NULL DEFAULT '0000-00-00 00:00:00'"
 		),
 		'key' => array(
 			'PRIMARY KEY' => 'id_auteur',
-			'KEY type' => 'type',
-			'KEY token' => 'token'
+			'KEY type' => 'type'
 		)
 	);
 
--- connecteur/trunk/connecteur_administrations.php
+++ connecteur/trunk/connecteur_administrations.php
@@ -8,6 +8,7 @@ function connecteur_upgrade($nom_meta_base_version, $version_cible) {
 
 	$maj = array();
 	$maj['create'] = array(array('maj_tables', array('spip_connecteur')));
+	$maj['1.0.1'] = array(array('maj_tables', array('spip_connecteur')));
 
 	include_spip('base/upgrade');
 	maj_plugin($nom_meta_base_version, $version_cible, $maj);
