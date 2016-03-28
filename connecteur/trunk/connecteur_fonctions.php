110d667496d1567aff0e8bf00750ee9d6ff7ad02
p@henix.be <p@henix.be@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-03-28 12:19:52 +0000

    Créer une fonction de création d'auteur
    On commence petit
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@96232 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- connecteur/trunk/connecteur_fonctions.php
+++ connecteur/trunk/connecteur_fonctions.php
@@ -9,13 +9,45 @@
  * @package    SPIP\Connecteur\Fonctions
  */
 
-if (!defined('_ECRIRE_INC_VERSION')) return;
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
 
-/*
- * Un fichier de fonctions permet de définir des éléments
- * systématiquement chargés lors du calcul des squelettes.
+/**
+ * Cette fonction va créer un auteur SPIP en fonction d'une source
+ * La source est définie par un plugin externe sous la forme
  *
- * Il peut par exemple définir des filtres, critères, balises, …
- * 
+ * ```
+ * function connecteur_SOURCE_dist()
+ * ```
+ * Cette fonction connecteur ce charge de fournir un tableau d'information
+ * qui servira créer l'auteur SPIP
+ *
+ * ```
+ * array('nom' => 'truc', 'email' => 'truc@machin.be')
+ * ```
+ *
+ * @param mixed $source
+ * @access public
  */
+function connecteur_creer_auteur($source, $statut = '6forum') {
+
+	// On commence par charger la fonction appeler
+	$connecteur = charger_fonction('connecteur_'.$source);
+
+	// Connecteur valide ?
+	if (is_array($connecteur)) {
+
+		// Inscrire l'auteur sur base des informations du connecteur
+		$inscrire_auteur = charger_fonction('inscrire_auteur', 'action');
+		$desc = $inscrire_auteur(
+			$statut,
+			$connecteur['email'],
+			$connecteur['nom']
+		);
+
+		// Envoyer aux pipelines
+		$desc = pipeline('post_connecteur', $desc);
+	}
+}
