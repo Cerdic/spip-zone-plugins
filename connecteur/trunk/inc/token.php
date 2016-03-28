8b0e996b09dbb0bf3249fcdc6b5cbf01eb454873
p@henix.be <p@henix.be@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-03-28 12:24:25 +0000

    Début d'un API de gestion des tokens
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@96248 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ connecteur/trunk/inc/token.php
@@ -0,0 +1,54 @@
+<?php
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+/**
+ * API de gestion des TOKEN
+ *
+ * @access public
+ */
+
+
+/**
+ * Enregistrer un token d'auteur
+ *
+ * @param int $id_auteur
+ * @param sting $type Type du token (ex: facebook)
+ * @param mixed $token le token sous forme brut
+ * @access public
+ */
+function connecteur_enregistrer_token($id_auteur, $type, $token) {
+
+	// Sérializer le token
+	$token = serialize($token);
+	sql_insertq(
+		'spip_connecteur',
+		array(
+			'id_auteur' => $id_auteur,
+			'type' => $type,
+			'token' => $token
+		)
+	);
+}
+
+/**
+ * Récupérer un token précis
+ *
+ * @param int $id_auteur
+ * @param string $type
+ * @access public
+ */
+function connecteur_get_token($id_auteur, $type) {
+	$token = sql_getfetsel(
+		'token',
+		'spip_connecteur',
+		array(
+			'id_auteur='.intval($id_auteur),
+			'type='.sql_quote($type)
+		)
+	);
+
+	return unserialize($token);
+}
