6627d18f0674d8594ab436e5c9426c06d2279bd5
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-03-29 15:01:16 +0000

    - On crée des autorisations propres à InfoSites, même si de base pour le moment, ce n'est qu'un copier-coller des objets initiaux. L'objectif est la prise en compte des rôles par projets et héritage pour les enfants des projets ; - Prise en compte du nouveau champ de Sites de projets.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@96310 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- info_sites/trunk/info_sites_autorisations.php
+++ info_sites/trunk/info_sites_autorisations.php
@@ -31,9 +31,237 @@ function info_sites_autoriser() {
  * @param  int    $id    Identifiant de l'objet
  * @param  array  $qui   Description de l'auteur demandant l'autorisation
  * @param  array  $opt   Options de cette autorisation
+ *
  * @return bool          true s'il a le droit, false sinon
  **/
 function autoriser_ecrire($faire, $type, $id, $qui, $opt) {
 	return $qui['statut'] == '0minirezo' and !$qui['restreint'];
+}
+
+// *****************************
+// Les sites de projets (projetssite)
+// *****************************
+
+/**
+ * Autorisation de créer (projetssite)
+ *
+ * @param  string $faire Action demandée
+ * @param  string $type  Type d'objet sur lequel appliquer l'action
+ * @param  int    $id    Identifiant de l'objet
+ * @param  array  $qui   Description de l'auteur demandant l'autorisation
+ * @param  array  $opt   Options de cette autorisation
+ *
+ * @return bool          true s'il a le droit, false sinon
+ **/
+function autoriser_info_sites_projetssitecreer_dist($faire, $type, $id, $qui, $opt) {
+	return in_array($qui['statut'], array(
+		'0minirezo',
+		'1comite',
+	));
+}
+
+/**
+ * Autorisation de voir (projetssite)
+ *
+ * @param  string $faire Action demandée
+ * @param  string $type  Type d'objet sur lequel appliquer l'action
+ * @param  int    $id    Identifiant de l'objet
+ * @param  array  $qui   Description de l'auteur demandant l'autorisation
+ * @param  array  $opt   Options de cette autorisation
+ *
+ * @return bool          true s'il a le droit, false sinon
+ **/
+function autoriser_info_sites_projetssitevoir_dist($faire, $type, $id, $qui, $opt) {
+	return true;
+}
+
+/**
+ * Autorisation de modifier (projetssite)
+ *
+ * @param  string $faire Action demandée
+ * @param  string $type  Type d'objet sur lequel appliquer l'action
+ * @param  int    $id    Identifiant de l'objet
+ * @param  array  $qui   Description de l'auteur demandant l'autorisation
+ * @param  array  $opt   Options de cette autorisation
+ *
+ * @return bool          true s'il a le droit, false sinon
+ **/
+function autoriser_info_sites_projetssitemodifier_dist($faire, $type, $id, $qui, $opt) {
+	return in_array($qui['statut'], array(
+		'0minirezo',
+		'1comite',
+	));
+}
+
+/**
+ * Autorisation de supprimer (projetssite)
+ *
+ * @param  string $faire Action demandée
+ * @param  string $type  Type d'objet sur lequel appliquer l'action
+ * @param  int    $id    Identifiant de l'objet
+ * @param  array  $qui   Description de l'auteur demandant l'autorisation
+ * @param  array  $opt   Options de cette autorisation
+ *
+ * @return bool          true s'il a le droit, false sinon
+ **/
+function autoriser_info_sites_projetssitesupprimer_dist($faire, $type, $id, $qui, $opt) {
+	return $qui['statut'] == '0minirezo' and !$qui['restreint'];
+}
+
+
+/**
+ * Autorisation de mise à jour (projetssite)
+ *
+ * @param  string $faire Action demandée
+ * @param  string $type  Type d'objet sur lequel appliquer l'action
+ * @param  int    $id    Identifiant de l'objet
+ * @param  array  $qui   Description de l'auteur demandant l'autorisation
+ * @param  array  $opt   Options de cette autorisation
+ *
+ * @return bool          true s'il a le droit, false sinon
+ **/
+function autoriser_info_sites_projetssitemaj_dist($faire, $type, $id, $qui, $opt) {
+	return in_array($qui['statut'], array(
+		'0minirezo',
+		'1comite',
+	));
+}
+/**
+ * Autorisation d'association (projetssite)
+ *
+ * @param  string $faire Action demandée
+ * @param  string $type  Type d'objet sur lequel appliquer l'action
+ * @param  int    $id    Identifiant de l'objet
+ * @param  array  $qui   Description de l'auteur demandant l'autorisation
+ * @param  array  $opt   Options de cette autorisation
+ *
+ * @return bool          true s'il a le droit, false sinon
+ **/
+function autoriser_info_sites_projetssitesassocier_dist($faire, $type, $id, $qui, $opt) {
+	return in_array($qui['statut'], array(
+		'0minirezo',
+		'1comite',
+	));
+}
+
+/**
+ * Autorisation de voir les données sensibles (projetssite)
+ *
+ * @param  string $faire Action demandée
+ * @param  string $type  Type d'objet sur lequel appliquer l'action
+ * @param  int    $id    Identifiant de l'objet
+ * @param  array  $qui   Description de l'auteur demandant l'autorisation
+ * @param  array  $opt   Options de cette autorisation
+ *
+ * @return bool          true s'il a le droit, false sinon
+ **/
+function autoriser_projetssitesecurite_voir($faire, $type, $id, $qui, $opt) {
+	return $qui['statut'] == '0minirezo';
+}
+
+// *****************************
+// Les projets
+// *****************************
+
+/**
+ * Autorisation de créer (projet)
+ *
+ * @param  string $faire Action demandée
+ * @param  string $type  Type d'objet sur lequel appliquer l'action
+ * @param  int    $id    Identifiant de l'objet
+ * @param  array  $qui   Description de l'auteur demandant l'autorisation
+ * @param  array  $opt   Options de cette autorisation
+ *
+ * @return bool          true s'il a le droit, false sinon
+ **/
+function autoriser_info_sites_projetcreer_dist($faire, $type, $id, $qui, $opt) {
+	return in_array($qui['statut'], array(
+		'0minirezo',
+		'1comite',
+	));
+}
+
+/**
+ * Autorisation de voir (projet)
+ *
+ * @param  string $faire Action demandée
+ * @param  string $type  Type d'objet sur lequel appliquer l'action
+ * @param  int    $id    Identifiant de l'objet
+ * @param  array  $qui   Description de l'auteur demandant l'autorisation
+ * @param  array  $opt   Options de cette autorisation
+ *
+ * @return bool          true s'il a le droit, false sinon
+ **/
+function autoriser_info_sites_projetvoir_dist($faire, $type, $id, $qui, $opt) {
+	return true;
+}
+
+/**
+ * Autorisation de modifier (projet)
+ *
+ * @param  string $faire Action demandée
+ * @param  string $type  Type d'objet sur lequel appliquer l'action
+ * @param  int    $id    Identifiant de l'objet
+ * @param  array  $qui   Description de l'auteur demandant l'autorisation
+ * @param  array  $opt   Options de cette autorisation
+ *
+ * @return bool          true s'il a le droit, false sinon
+ **/
+function autoriser_info_sites_projetmodifier_dist($faire, $type, $id, $qui, $opt) {
+	return in_array($qui['statut'], array(
+		'0minirezo',
+		'1comite',
+	));
+}
+
+/**
+ * Autorisation de supprimer (projet)
+ *
+ * @param  string $faire Action demandée
+ * @param  string $type  Type d'objet sur lequel appliquer l'action
+ * @param  int    $id    Identifiant de l'objet
+ * @param  array  $qui   Description de l'auteur demandant l'autorisation
+ * @param  array  $opt   Options de cette autorisation
+ *
+ * @return bool          true s'il a le droit, false sinon
+ **/
+function autoriser_info_sites_projetsupprimer_dist($faire, $type, $id, $qui, $opt) {
+	return $qui['statut'] == '0minirezo' and !$qui['restreint'];
+}
+
+/**
+ * Autorisation de mise à jour (projet)
+ *
+ * @param  string $faire Action demandée
+ * @param  string $type  Type d'objet sur lequel appliquer l'action
+ * @param  int    $id    Identifiant de l'objet
+ * @param  array  $qui   Description de l'auteur demandant l'autorisation
+ * @param  array  $opt   Options de cette autorisation
+ *
+ * @return bool          true s'il a le droit, false sinon
+ **/
+function autoriser_info_sites_projetmaj_dist($faire, $type, $id, $qui, $opt) {
+	return in_array($qui['statut'], array(
+		'0minirezo',
+		'1comite',
+	));
+}
+
+/**
+ * Autorisation d'association (projet)
+ *
+ * @param  string $faire Action demandée
+ * @param  string $type  Type d'objet sur lequel appliquer l'action
+ * @param  int    $id    Identifiant de l'objet
+ * @param  array  $qui   Description de l'auteur demandant l'autorisation
+ * @param  array  $opt   Options de cette autorisation
+ *
+ * @return bool          true s'il a le droit, false sinon
+ **/
+function autoriser_info_sites_projetsassocier_dist($faire, $type, $id, $qui, $opt) {
+	return in_array($qui['statut'], array(
+		'0minirezo',
+		'1comite',
+	));
 }
 
