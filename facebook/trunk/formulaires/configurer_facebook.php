46bcfd5d5bc4407fdd5cf96a76028fc9fbd1a118
p@henix.be <p@henix.be@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-03-29 08:49:58 +0000

    On ne passe au formulaire de configuration que le minimum
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@96300 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- facebook/trunk/formulaires/configurer_facebook.php
+++ facebook/trunk/formulaires/configurer_facebook.php
@@ -29,5 +29,9 @@ function formulaires_configurer_facebook_charger_dist() {
 	// Contexte du formulaire.
 	include_spip('inc/config');
 	$config = lire_config('facebook');
+	$config = array(
+		'cle' => $config['cle'],
+		'secret' => $config['secret']
+	);
 	return (empty($config)) ? array() : $config;
 }
