052cc85b920d002f9dd75eb6892fc098cf70a8e6
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-03-30 18:01:09 +0000

    une branche stable 3.0.x
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@96334 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ inscription/branches/inscription3_5x/lang/paquet-inscription3_fr.php
@@ -0,0 +1,17 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/inscription/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// I
+	'inscription3_description' => 'Plugin permettant d’avoir une gestion plus fine des comptes utilisateurs,
+lors de leur inscription et de la modification de leur profil.',
+	'inscription3_nom' => 'Inscription 3',
+	'inscription3_slogan' => 'Étendre les informations sur les auteurs'
+);
+
+?>
