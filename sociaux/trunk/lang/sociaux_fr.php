aedea0752caaf1deb45e5777c54ad3f1dac12330
peetdu@gmail.com <peetdu@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-03-31 14:29:16 +0000

    Ajout de l’entrée Liens Sociaux pour le plugin Menus
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@96348 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- sociaux/trunk/lang/sociaux_fr.php
+++ sociaux/trunk/lang/sociaux_fr.php
@@ -9,8 +9,13 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'sociaux_titre' => 'Liens sociaux',
 
 	// C
-  'cfg_explication' => 'Indiquez les adresses des réseaux sociaux vers lesquels vous voulez faire le lien',
-  'cfg_label_css' => 'Activer l\'habillage graphique de la barre de liens avec la feuille de style basée sur la police socicon',
+	'cfg_explication' => 'Indiquez les adresses des réseaux sociaux vers lesquels vous voulez faire le lien',
+	'cfg_label_css' => 'Activer l\'habillage graphique de la barre de liens avec la feuille de style basée sur la police socicon',
+
+	// M
+	'menu_description'	=> 'La liste complète des réseaux est disponible dans la documentation.',
+	'menu_nom_reseau'	=> 'Nom du réseau',
+	'menu_titre'		=> 'Lien vers un réseau social (Twitter, Facebook, etc.).',
 
 	// T
 	'titre_page_configurer_sociaux' => 'Liens vers les réseaux sociaux',
