e4aaeef77f93651c7587ab5299914e7405c973d6
marcimat@rezo.net <marcimat@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-04-01 12:49:09 +0000

    Simplifications, indentations
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@96377 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- videos/trunk/videos_pipelines.php
+++ videos/trunk/videos_pipelines.php
@@ -1,4 +1,5 @@
 <?php
+
 if (!defined("_ECRIRE_INC_VERSION")) {
 	return;
 }
@@ -7,39 +8,43 @@ function videos_ieconfig_metas($table) {
 	$table['videos']['titre'] = _T('paquet-videos:videos_nom');
 	$table['videos']['icone'] = 'prive/themes/spip/images/videos-16.png';
 	$table['videos']['metas_serialize'] = 'videos';
-
 	return $table;
 }
 
 function videos_insert_head($flux) {
 	include_spip('inc/config');
-	$flux .= "
-<!-- Variables de configuration pour le plugin Vidéo(s) -->
-<script type='text/javascript'>var CONFIG_WMODE = '" . lire_config('videos/wmode', 'opaque') . "';</script>\n" .
-		"<!-- // Vidéo(s) -->" . "\n";
-
+	$flux .= "\n<script type='text/javascript'>var CONFIG_WMODE = '" . lire_config('videos/wmode', 'opaque') . "';</script>\n";
 	return $flux;
 }
 
 function videos_insert_head_css($flux) {
 	include_spip('inc/config');
 	$css = find_in_path('theme/css/videos.css');
-	$flux .= "
-<!-- CSS pour le plugin Vidéo(s) -->" .
-		'<link rel="stylesheet" href="' . direction_css($css) . '" type="text/css" media="all" />' .
-		"<!-- // Vidéo(s) -->" . "\n";
-
+	$flux .= "\n<link rel='stylesheet' href='" . direction_css($css) . "' type='text/css' media='all' />\n";
 	return $flux;
 }
 
+/**
+ * Ajouter la librairie JS html5media sur les pages
+ * 
+ * @link https://html5media.info/
+ * @pipeline jquery_plugins
+ * @param array $scripts
+ * @return array
+ */
 function videos_jquery_plugins($scripts) {
 	$scripts[] = "lib/html5media-1.1.5/api/html5media.min.js";
-
 	return $scripts;
 }
 
+/**
+ * Ajouter le formulaire d'ajout de vidéos au pied du formulaire d'ajout de document.
+ * 
+ * @pipeline formulaire_fond
+ * @param array $flux
+ * @return array
+ */
 function videos_formulaire_fond($flux) {
-
 	if ($flux['args']['form'] == 'joindre_document') {
 		$videos = recuperer_fond(
 			'prive/contenu/videos_affiche_boite',
@@ -49,8 +54,7 @@ function videos_formulaire_fond($flux) {
 			)
 		);
 		// Injecter videos au dessus du formulaire joindre_document.
-		$flux['data'] = $flux['data'] . $videos;
+		$flux['data'] .= $videos;
 	}
-
 	return $flux;
 }
