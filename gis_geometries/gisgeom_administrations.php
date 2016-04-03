c4a3f40d38e1de7f381b2f1349ba3c36df2b4a9f
brunobergot@gmail.com <brunobergot@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-04-03 18:06:01 +0000

    version 1.7.5 : upgrade méthode d'installation
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@96420 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- gis_geometries/gisgeom_administrations.php
+++ gis_geometries/gisgeom_administrations.php
@@ -11,26 +11,29 @@ include_spip('inc/meta');
  * @param string $version_cible
  */
 function gisgeom_upgrade($nom_meta_base_version,$version_cible){
-	$current_version = '0.0';
-	if ( (!isset($GLOBALS['meta'][$nom_meta_base_version]) )
-			|| (($current_version = $GLOBALS['meta'][$nom_meta_base_version])!=$version_cible)){
-		// installation
-		if (version_compare($current_version, '0.0','<=')){
-			include_spip('base/abstract_sql');
-			// ajout des champs geo et type à la table gis
-			sql_alter("TABLE spip_gis ADD geo GEOMETRY DEFAULT '' NOT NULL AFTER lon");
-			sql_alter("TABLE spip_gis ADD type VARCHAR (25) DEFAULT '' NOT NULL AFTER zoom");
-			// renseigner spip_gis.geo avec spip_gis.lat et spip_gis.lon pour les objets existants
-			sql_update("spip_gis", array("geo"=>"GeomFromText(CONCAT('POINT(',lon,' ',lat,')'))"));
-			// renseigner spip_gis.type = point pour les objets existants
-			sql_updateq("spip_gis", array("type"=>"Point"));
-			// ajouter un index sur le champ geo
-			sql_alter("TABLE spip_gis ADD SPATIAL INDEX (geo)");
-			include_spip('inc/invalideur');
-			purger_repertoire(_DIR_VAR.'cache-js');
-			ecrire_meta($nom_meta_base_version,$current_version="1.0.0",'non');
-		}
-	}
+	$maj = array();
+
+	$maj['create'] = array(
+		// ajout des champs geo et type à la table gis
+		array('sql_alter', "TABLE spip_gis ADD geo GEOMETRY DEFAULT '' NOT NULL AFTER lon"),
+		array('sql_alter', "TABLE spip_gis ADD type VARCHAR (25) DEFAULT '' NOT NULL AFTER zoom"),
+		// renseigner spip_gis.geo avec spip_gis.lat et spip_gis.lon pour les objets existants
+		array('sql_update', 'spip_gis', array("geo"=>"GeomFromText(CONCAT('POINT(',lon,' ',lat,')'))")),
+		// renseigner spip_gis.type = point pour les objets existants
+		array('sql_updateq', 'spip_gis', array("type"=>"Point")),
+		// ajouter un index sur le champ geo
+		array('sql_alter', 'TABLE spip_gis ADD SPATIAL INDEX (geo)'),
+		// purger le cache js
+		array('gisgeom_purger_cache_js'),
+	);
+
+	include_spip('base/upgrade');
+	maj_plugin($nom_meta_base_version, $version_cible, $maj);
+}
+
+function gisgeom_purger_cache_js(){
+	include_spip('inc/invalideur');
+	purger_repertoire(_DIR_VAR.'cache-js');
 }
 
 /**
