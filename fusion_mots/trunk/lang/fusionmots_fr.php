28ac8103052d6c3016bb3644727201269d47688d
placido@roxing.net <placido@roxing.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-04-04 14:05:47 +0000

    Reprise de l'interface dans l'espace privé. Le recours à id_groupe=0 pour sélectionner tous les groupes ne fonctionnait pas pour moi (SPIP 3.1.0 [22707]) Dans la colonne navigation, on utilise un formulaire ajaxé pour la sélection du/des groupe(s) (fonctionne aussi sans js). On peut désormais sélectionner plusieurs groupes simultanément, (aucun sélectionné par défaut pour la performance). Dans le tableau, on déporte le lien vers la page mot sur l'id pour ne pas parasiter le clic ckeckbox/radio ; puis on indique le groupe d'appartenance dans l'attribut title. Ajout d'une feuille de style pour le privé. Suppression d'un fichier et d'un répertoire vides. On renvoit un "message_ok" à la suite du traitement.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@96452 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- fusion_mots/trunk/lang/fusionmots_fr.php
+++ fusion_mots/trunk/lang/fusionmots_fr.php
@@ -4,15 +4,15 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 	
-	'erreur_par'		=>'Choisissez un mot de remplacement',
-	'erreur_remplacer'	=>'Choisissez au moins un mot à remplacer',
-	'explication'		=>'Sur cette page vous pouvez fusionner des mots-clefs. Choisissez à gauche les mots-clefs à remplacer par celui choisi à droite.',
-	'explication_intro'	=>'Fusionner plusieurs mots et leurs utilisations.',
-	'choisir_groupe'	=>'Choisissez le groupe de mots dont vous souhaitez fusionner des mots', 
+	'erreur_par'		=>'Choisissez un mot de remplacement.',
+	'erreur_remplacer'	=>'Choisissez au moins un mot à remplacer.',
+	'explication'		=>'Sur cette page vous pouvez fusionner des mots-clefs. Choisissez, dans la colonne de gauche du tableau, les mots-clefs qui seront remplacés au profit de celui choisi dans la colonne de droite.',
+	'aucun_groupe'	=>'Aucun groupe de mots n\'est sélectionné.',
+	'choisir_groupe'	=>'Choisissez le groupe de mots dont vous souhaitez fusionner des mots.', 
 	'tous_groupes'		=>'Tous les groupes',
-	'fusionmots'		=>"Fusionner des mots-clef",
-	'par'				=>'Par',
-	'remplacer'			=>'Remplacer le(s) mot(s)-clef',
+	'fusionmots'		=>'Fusionner des mots-clef',
+	'vers'				=>'Vers',
+	'remplacer'			=>'Fusionner la sélection',
 	
 );
 
