d641d3baf2831b6c3da8d27d3d39e925d2999072
jeanmarie.listes@cousumain.info <jeanmarie.listes@cousumain.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-04-06 10:50:16 +0000

    Ajout de la possibilité de personnaliser la couleur de thème pour Android Chrome 39+ et Windows en définissant _FAVICON_COULEUR dans mes_options.php
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@96482 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- favicon/lang/paquet-favicon_fr.php
+++ favicon/lang/paquet-favicon_fr.php
@@ -8,7 +8,8 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// F
 	'favicon_description' => 'Génération automatique, à partir de votre logo de site, de favicon et touch icon pour les différents navigateurs et terminaux.
-	Pour que ce soit esthétiquement réussi, votre logo de site doit être carré, de 200 pixels de côté minimum. Sinon, déposer une image nommée « favicon.png » à la racine du dossier « squelettes ».',
+	Pour que ce soit esthétiquement réussi, votre logo de site doit être carré, de 200 pixels de côté minimum. Sinon, déposer une image nommée « favicon.png » à la racine du dossier « squelettes ».
+	Il est également possible de personnaliser une couleur de thème pour Android Chrome 39+ et Windows en définissant _FAVICON_COULEUR dans mes_options.php.',
 	'favicon_slogan' => 'Favicon et touch icon du site',
 );
 ?>
