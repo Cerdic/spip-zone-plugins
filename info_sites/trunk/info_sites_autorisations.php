3082349a84fb79f75353d1e2d5d1b6ece33128ba
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-04-07 09:47:33 +0000

    Uniformiser les autorisations. On verra s'il faut surcharger les autorisations par défaut de SPIP.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@96506 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- info_sites/trunk/info_sites_autorisations.php
+++ info_sites/trunk/info_sites_autorisations.php
@@ -131,6 +131,24 @@ function autoriser_infositesmaj_dist($faire, $type, $id, $qui, $opt) {
 }
 
 /**
+ * Autorisation de changer le statut
+ *
+ * @param  string $faire Action demandée
+ * @param  string $type  Type d'objet sur lequel appliquer l'action
+ * @param  int    $id    Identifiant de l'objet
+ * @param  array  $qui   Description de l'auteur demandant l'autorisation
+ * @param  array  $opt   Options de cette autorisation
+ *
+ * @return bool          true s'il a le droit, false sinon
+ **/
+function autoriser_infositesinstituer_dist($faire, $type, $id, $qui, $opt) {
+	return in_array($qui['statut'], array(
+		'0minirezo',
+		'1comite',
+	));
+}
+
+/**
  * Autorisation d'association
  *
  * @param  string $faire Action demandée
