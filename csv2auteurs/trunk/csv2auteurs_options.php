67ff44e8c866d5bc61ac332076358e515a2abe48
spip.franck@lien-d-amis.net <spip.franck@lien-d-amis.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-04-09 18:29:59 +0000

    des tabs à la place d'espaces pour être un peu  plus conforme avec: http://www.spip.net/fr_article3497.html#regles_codage
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@96552 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- csv2auteurs/trunk/csv2auteurs_options.php
+++ csv2auteurs/trunk/csv2auteurs_options.php
@@ -6,4 +6,3 @@ if (_request('exec') == 'csv2auteurs'
 	AND $GLOBALS['auteur_session']['webmestre'] == 'oui'
 	AND _request('formulaire_action') == 'csv2auteurs_importation')
 	define('_PASS_LONGUEUR_MINI', '1');
-
--- csv2auteurs/trunk/csv2auteurs_pipelines.php
+++ csv2auteurs/trunk/csv2auteurs_pipelines.php
@@ -2,15 +2,10 @@
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 
-function csv2auteurs_header_prive($flux){
-    $css   = find_in_path('csv2auteurs.css');
-    $flux .= "<link rel='stylesheet' type='text/css' media='all' href='$css' />\n";
+function csv2auteurs_header_prive($flux) {
+	$css   = find_in_path('csv2auteurs.css');
+	$flux .= "<link rel='stylesheet' type='text/css' media='all' href='$css' />\n";
 
-    return $flux;
+	return $flux;
 }
-
-
 ?>
-
-
-
