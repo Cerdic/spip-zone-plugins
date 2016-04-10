824962cb7130f9e9e098cf610e050106c3c6e587
spip.franck@lien-d-amis.net <spip.franck@lien-d-amis.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-04-10 09:14:25 +0000

    Des tabs à la place d'espace pour être un peu plus conforme avec http://www.spip.net/fr_article3497.html#regles_codage
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@96557 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- gisban/gisban_pipelines.php
+++ gisban/gisban_pipelines.php
@@ -8,10 +8,10 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
  * @param $flux
  * @return mixed
  */
-function gisban_insert_head_css($flux){
-    $flux .="\n".'<link rel="stylesheet" href="'. find_in_path('lib/leaflet.photon/leaflet.photon.css') .'" />';
-    $flux .="\n".'<link rel="stylesheet" href="'. find_in_path('css/photon_search_gis.css') .'" />';
-    return $flux;
+function gisban_insert_head_css($flux) {
+	$flux .="\n".'<link rel="stylesheet" href="'. find_in_path('lib/leaflet.photon/leaflet.photon.css') .'" />';
+	$flux .="\n".'<link rel="stylesheet" href="'. find_in_path('css/photon_search_gis.css') .'" />';
+	return $flux;
 }
 
 /**
@@ -20,9 +20,9 @@ function gisban_insert_head_css($flux){
  * @param $flux
  * @return mixed
  */
-function gisban_header_prive($flux){
-    $flux .= gisban_insert_head_css('');
-    return $flux;
+function gisban_header_prive($flux) {
+	$flux .= gisban_insert_head_css('');
+	return $flux;
 }
 
 
@@ -32,16 +32,16 @@ function gisban_header_prive($flux){
  * @param $flux
  * @return mixed
  */
-function gisban_recuperer_fond($flux){
-    if ($flux['args']['fond'] == 'modeles/carte_gis') {
-        $modele = recuperer_fond('inclure/inc-carte-gisban', $flux['data']['contexte']);
-        $flux['data']['texte'] .= "\n" . $modele;
-    }
-    if ($flux['args']['fond'] == 'javascript/gis.js') {
-        $ajouts = "var filter_gisban = '". lire_config('gisban/filtre_gisban') ."';\n";
-        $ajouts .= "\n" . spip_file_get_contents(find_in_path('lib/leaflet.photon/leaflet.photon.js'));
-        $flux['data']['texte'] .= $ajouts;
-    }
-    return $flux;
+function gisban_recuperer_fond($flux) {
+	if ($flux['args']['fond'] == 'modeles/carte_gis') {
+		$modele = recuperer_fond('inclure/inc-carte-gisban', $flux['data']['contexte']);
+		$flux['data']['texte'] .= "\n" . $modele;
+	}
+	if ($flux['args']['fond'] == 'javascript/gis.js') {
+		$ajouts = "var filter_gisban = '". lire_config('gisban/filtre_gisban') ."';\n";
+		$ajouts .= "\n" . spip_file_get_contents(find_in_path('lib/leaflet.photon/leaflet.photon.js'));
+		$flux['data']['texte'] .= $ajouts;
+	}
+	return $flux;
 }
 ?>
