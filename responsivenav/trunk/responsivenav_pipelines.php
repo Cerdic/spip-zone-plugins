c0f90796b1fa17965bd1631c867e413582334741
jeanmarie.listes@cousumain.info <jeanmarie.listes@cousumain.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-04-11 14:59:59 +0000

    Optimisation du logo. On met des retours chariot pour plus de lisibilité.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@96582 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- responsivenav/trunk/responsivenav_pipelines.php
+++ responsivenav/trunk/responsivenav_pipelines.php
@@ -8,22 +8,22 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
  * @plugin     Responsive Nav
  * @copyright  2016
  * @author     jeanmarie
- * @licence    GNU/GPL
+ * @licence    CC by-sa
  * @package    SPIP\Responsivenav\Pipelines
  */
 
  
 // on insert la feuille de styles
 function responsivenav_insert_head_css($flux) {
-	$flux .= '<link rel="stylesheet" href="'.produire_fond_statique('css/responsive-nav.css').'" type="text/css" />';
-	$flux .= '<link rel="stylesheet" href="'.produire_fond_statique('css/responsive-nav.spip.css').'" type="text/css" />';
+	$flux .="\n".'<link rel="stylesheet" href="'.produire_fond_statique('css/responsive-nav.css').'" type="text/css" />';
+	$flux .="\n".'<link rel="stylesheet" href="'.produire_fond_statique('css/responsive-nav.spip.css').'" type="text/css" />';
 	return $flux;
 }
 
 // on insert le script et on l'appelle
 function responsivenav_insert_head($flux) {
-	$flux .= '<script type="text/javascript" src="'.find_in_path('javascript/responsive-nav.js').'"></script>';
-	$flux .= '<script>/*<![CDATA[*/ ;(function($){ $(function(){ var nav = responsiveNav("'.lire_config('responsivenav/selector','#nav').'",{jsClass: "'._RESPONSIVENAV_JSCLASS.'",label: "'.lire_config('responsivenav/label','&#9776; Menu').'",insert: "'.lire_config('responsivenav/insert','before').'"' ;
+	$flux .="\n".'<script type="text/javascript" src="'.find_in_path('javascript/responsive-nav.js').'"></script>';
+	$flux .="\n".'<script>/*<![CDATA[*/ ;(function($){ $(function(){ var nav = responsiveNav("'.lire_config('responsivenav/selector','#nav').'",{jsClass: "'._RESPONSIVENAV_JSCLASS.'",label: "'.lire_config('responsivenav/label','&#9776; Menu').'",insert: "'.lire_config('responsivenav/insert','before').'"' ;
 	if (defined('_RESPONSIVENAV_ANIMATE')) {$flux .= ',animate: '._RESPONSIVENAV_ANIMATE;}
 	if (defined('_RESPONSIVENAV_TRANSITION')) {$flux .= ',transition: '._RESPONSIVENAV_TRANSITION;}
 	if (defined('_RESPONSIVENAV_CUSTOMTOGGLE')) {$flux .= ',customToggle: "'._RESPONSIVENAV_CUSTOMTOGGLE.'"';}
