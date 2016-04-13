2b0b4d2ea4af72e8d17c8b8b2a04b9db6baefea2
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-04-13 16:09:57 +0000

    Grosse nouveauté mais qui ne casse rien : on déporte la génération du tableau-json dans une fonction interne sans rapport avec "http". Cette fonction peut donc aussi être appelée n'importe où, notamment dans un squelette (pour intégrer une collection dans un item par exemple : collections imbriquées).
    On ajoute un nouveau pipeline sans le "http" dedans, qui devrait suffire pour travailler sur le tableau directement. L'ancien pipeline qui bossait sur le array est gardé pour compat pour l'instant, mais ça ferait encore plus de nettoyage de l'enlever (demander si quelqu'un l'a déjà utilisé ?).
    
    En effet, la fonction "http_collectionjson_get_collection_dist" est beaucoup plus légère et lisible et ne contient plus que le travail sur requête/réponse. La fabrication du JSON étant déportée ailleurs.
    
    On pourrait ensuite ajouter un comportement similaire pour la production du JSON d'une ressource.
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@96617 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- collection_json/trunk/http/collectionjson.php
+++ collection_json/trunk/http/collectionjson.php
@@ -5,7 +5,7 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
 
-include_spip('inc/collectionjson');
+include_spip('collectionjson_fonctions');
 
 /**
  * Contenu collection+json d'une erreur
@@ -127,81 +127,12 @@ function http_collectionjson_get_collection_dist($requete, $reponse) {
 	if ($fonction_collection = charger_fonction('get_collection', "http/$format/$collection/", true)) {
 		$reponse = $fonction_collection($requete, $reponse);
 	}
-	// Sinon on essaye de trouver différentes méthodes pour produire le JSON et en déduire les headers
-	// - pour l'instant seulement par un squelette
+	// Sinon on essaye de trouver différentes méthodes pour produire le JSON
 	else {
-		// Allons chercher un squelette de base qui génère le JSON de la collection demandée
-		// Le squelette prend en contexte les paramètres du GET uniquement
-		if ($json = recuperer_fond("http/$format/$collection", $contexte)) {
-			// On décode ce qu'on a trouvé
-			$json = json_decode($json, true);
-		}
-		// Si on ne trouve rien on essaie de s'appuyer sur l'API objet
-		else  {
-			include_spip('base/abstract_sql');
-			include_spip('base/objets');
-			
-			// Si la collection demandée ne correspond pas à une table
-			// d'objet on arrête tout
-			if (!in_array(
-				table_objet_sql($collection),
-				array_keys(lister_tables_objets_sql())
-			)) {
-				// On utilise la fonction d'erreur générique pour
-				// renvoyer dans le bon format
-				$fonction_erreur = charger_fonction('erreur', "http/$format/");
-				return $fonction_erreur(404, $requete, $reponse);
-			}
-			
-			// On génère la pagination si besoin
-			$links = array();
-			$pagination = 10;
-			$offset = isset($contexte['offset']) ? $contexte['offset'] : 0;
-			$nb_objets = sql_countsel(table_objet_sql($collection));
-			
-			// On ajoute des liens de pagination
-			if ($offset > 0) {
-				$offset_precedant = max(0, $offset-$pagination);
-				$links[] = array(
-					'rel' => 'prev',
-					'prompt' => _T('public:page_precedente'),
-					'href' => url_absolue(
-						parametre_url(self('&'), 'offset', $offset_precedant)),
-				);
-			}
-			if (($offset + $pagination) < $nb_objets) {
-				$offset_suivant = $offset + $pagination;
-				$links[] = array(
-					'rel' => 'prev',
-					'prompt' => _T('public:page_suivante'),
-					'href' => url_absolue(
-						parametre_url(self('&'), 'offset', $offset_suivant)),
-				);
-			}
-			
-			// On requête l'ensemble de cette page d'un coup
-			$table_collection = table_objet_sql($collection);
-			$cle_objet = id_table_objet($table_collection);
-			$description = lister_tables_objets_sql($table_collection);
-			$select = isset($description['champs_editables']) ? array_merge($description['champs_editables'], array($cle_objet)) : '*';
-			$lignes = sql_allfetsel($select, $table_collection,'','','',"$offset,$pagination");
-			
-			$items = array();
-			foreach ($lignes as $champs) {
-				$items[] = collectionjson_get_objet(objet_type($table_collection), $champs[$cle_objet], $requete, $champs);
-			}
-			
-			$json = array(
-				'collection' => array(
-					'version' => '1.0',
-					'href' => url_absolue(parse_url(self('&'), PHP_URL_PATH)),
-					'links' => $links,
-					'items' => $items,
-				),
-			);
-		}
+		$json = collectionjson_get_collection($collection, $contexte);
 
-		// Et on le passe dans un pipeline
+		// COMPAT : à remplacer par le nouveau pipeline "collectionjson_get_collection"
+		// peut-être à supprimer directement si personne ne l'a jamais utilisé…
 		$json = pipeline(
 			'http_collectionjson_get_collection_contenu',
 			array(
@@ -212,7 +143,15 @@ function http_collectionjson_get_collection_dist($requete, $reponse) {
 				'data' => $json,
 			)
 		);
-		// Enfin on l'encode en JSON
+	}
+	
+	// Si on finit avec un truc vide, on génère une 404
+	if (empty($json)) {
+		$fonction_erreur = charger_fonction('erreur', 'http/collectionjson/');
+		$response = $fonction_erreur(404, $requete, $reponse);
+	}
+	// Sinon on encode et on renvoie correctement la réponse
+	else {
 		$json = json_encode($json);
 		
 		$reponse->setStatusCode(200);
