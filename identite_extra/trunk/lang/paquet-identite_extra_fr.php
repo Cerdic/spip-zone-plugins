6aeaba3ba0c0049a30e617e064450e0d6bbd70d8
pierre.laszczak@gmail.com <pierre.laszczak@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-04-14 12:22:17 +0000

    Petite correction de l'appelle des champs dans les squelettes #IDENTITE_NOM_CHAMP en lieu et place de #CONFIG{identite_extra/nom_champ}
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@96624 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- identite_extra/trunk/lang/paquet-identite_extra_fr.php
+++ identite_extra/trunk/lang/paquet-identite_extra_fr.php
@@ -5,7 +5,7 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 }
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-	'identite_extra_titre' => 'Identité extra',	
-	'identite_extra_description' => 'Déclarez vos champs supplémentaires dans <em>mes_options.php</em>. Une fois saisies dans <em>?exec=configurer_identite</em>, les valeurs sont accessibles dans vos squelettes via <code>#CONFIG{identite_extra/telephone}</code>.' ,
+	'identite_extra_titre' => 'Identité extra',
+	'identite_extra_description' => 'Déclarez vos champs supplémentaires dans <code>mes_options.php</code>. Une fois saisies dans <code>?exec=configurer_identite</code>, les valeurs sont accessibles dans vos squelettes via la balise <code>#IDENTITE_NOM_CHAMP</code>.',
 	'identite_extra_slogan' => 'Étendre facilement le formulaire Identité du site',
 );
