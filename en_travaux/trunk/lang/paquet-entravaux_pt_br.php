f73ee2cf46de2f414ce612946373aa96b0a8db01
tradspip.ptipoi@dfgh.net <tradspip.ptipoi@dfgh.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-04-16 03:34:40 +0000

    [Salvatore] [source:_plugins_/en_travaux/trunk/lang/ paquet-entravaux] Export depuis http://trad.spip.net de la langue pt_br
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@96644 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ en_travaux/trunk/lang/paquet-entravaux_pt_br.php
@@ -0,0 +1,18 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-entravaux?lang_cible=pt_br
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// E
+	'entravaux_description' => 'Ative-lhe antes de uma fase de manutenção, este plugin imediatamente exibe uma página com um aviso, substituindo o site público. Cuidado: Somente o webmaster ainda pode acessar o espaço privado e o site público.',
+	'entravaux_nom' => 'Em manutenção',
+	'entravaux_slogan' => 'Torna o site em fase de manutenção.'
+);
+
+?>
