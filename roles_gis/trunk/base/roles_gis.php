7d89f13938c785ea0e4f1fb40cdd04a3d464c8a2
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-04-20 13:34:18 +0000

    Jouer avec les roles sur les POIs de GIS
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@96709 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ roles_gis/trunk/base/roles_gis.php
@@ -0,0 +1,27 @@
+<?php
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+function roles_gis_declarer_tables_objets_sql($tables){
+	 
+	array_set_merge($tables, 'spip_gis', array(
+			"roles_colonne" => "role",
+			"roles_titres" => array(
+				'action'  => 'gis_roles:role_action',
+				'informatif' => 'gis_roles:role_informatif',
+		),
+		"roles_objets" => array(
+			'cartes' => array(
+				'choix' => array('action','informatif'),
+				'defaut' => 'action'
+			)
+		)
+	));
+	$tables['spip_gis']['champs_critere_gis'][] = 'gis_liens.role as role_gis';
+	return $tables;
+}
+
+function roles_gis_declarer_tables_auxiliaires($tables) {
+	$tables['spip_gis_liens']['field']['role'] = "varchar(30) NOT NULL DEFAULT ''";
+	$tables['spip_gis_liens']['key']['PRIMARY KEY'] = "id_gis,id_objet,objet,role";
+	return $tables;
+}
