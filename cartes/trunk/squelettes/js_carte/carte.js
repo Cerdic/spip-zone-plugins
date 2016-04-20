1244eb9a46927ba4912c77be5b4c17388e322422
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-04-20 20:52:38 +0000

    à la fin du tracé d'un topojson comme fond de carte, le mettre en bas pour ne pas cacher les autres tracés sur la carte
    Utiliser map et non map1 dans l'évènement ready
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@96730 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- cartes/trunk/squelettes/js_carte/carte.js
+++ cartes/trunk/squelettes/js_carte/carte.js
@@ -1,7 +1,7 @@
 var resizemap = function (map_resize) {
 	$('.map_wrapper').height($(window).height() - $('.footer').outerHeight()-$('.main').outerHeight());
 	if (typeof map_resize != 'undefined' && typeof map_resize.invalidateSize == 'function') {
-		map_resize.invalidateSize();
+		map_resize.invalidateSize(true);
 	}
 }
 var callback_map1 = function (map) {
@@ -14,7 +14,7 @@ var callback_map1 = function (map) {
 	}
 }
 
-var country_min_topojson_addlayer = function(e){
+var country_min_topojson_addlayer = function(e,topojson){
     e.layer.eachLayer(function(dist){
 	var iso = dist.toGeoJSON().properties.ISO2.toLowerCase();
 	dist.bindPopup("<strong>"+dist.toGeoJSON().properties.NAME_FR+"</strong>",{closeButton:false,className:'popupcountry'});
@@ -26,6 +26,7 @@ var country_min_topojson_addlayer = function(e){
 		});
 	}
     });
+    window.setTimeout(function(){topojson.bringToBack()},5);
 }
 $(document).ready(function () {
 	$(window).on('resize', function () {
@@ -40,53 +41,59 @@ $(document).ready(function () {
 		if (map1) {
 			resized = map1;
 		}
-		resizemap(resized)
+		resizemap(resized);
 	});
-	jQuery('#map1').on('ready',function(map){
-		if(typeof map1 != 'undefined' && map1.options.options && map1.options.options.popup == 'control'){
-			var info = L.control();
-			info.onAdd = function (map) {
-				this._div = L.DomUtil.create('div', 'info leaflet-popup-content-wrapper');
-				this.update();
-				return this._div;
-			};
-	
-			// method that we will use to update the control based on feature properties passed
-			info.update = function (text) {
-				if(typeof text != 'undefined'){
-					L.DomUtil.setOpacity(this._div,1);
-				}
-				else {
-					L.DomUtil.setOpacity(this._div,0);
-				}
-				this._div.innerHTML = text ? '<div class="leaflet-popup-content">'+text+'</div>' : '';
-			};
-	
-			info.addTo(map1);
-			map1.eachLayer(function(layer){
-				layer.off('click').on('click',function(e,f){
-					if(layer._popup && layer._popup._content){
-						info.update(layer._popup._content);
-						map1.closePopup();
-						map1.panTo(e.latlng)
-						return false;
-					}
-				});
-			});
-			map1.on('popupopen',function(){
-			    	info.update();
-			})
-		}
-		if(typeof map1 != 'undefined' && map1.options.options && map1.options.options.layer_topojson){
-			var topojson_layer = new L.TOPOJSON(map1.options.options.layer_topojson, {async: true}),
-				fichier_topojson = map1.options.options.layer_topojson.replace(/^.*[\\\/]/, '').replace(/\./g,'_'),
-				addlayer = fichier_topojson+'_addlayer';
-			if(typeof window[addlayer] == 'function'){
-			topojson_layer.on('addlayer',function(e){
-			    	eval(eval(addlayer)(e));
-			});
+
+	jQuery('#map1').on('ready',function(e, map){
+	    if(map.options.options && map.options.options.popup == 'control'){
+		var info = L.control();
+		info.onAdd = function (map) {
+			this._div = L.DomUtil.create('div', 'info leaflet-popup-content-wrapper');
+			this.update();
+			return this._div;
+		};
+
+		// method that we will use to update the control based on feature properties passed
+		info.update = function (text) {
+			if(typeof text != 'undefined'){
+				L.DomUtil.setOpacity(this._div,1);
 			}
-			map1.addLayer(topojson_layer);
-		}
+			else {
+				L.DomUtil.setOpacity(this._div,0);
+			}
+			this._div.innerHTML = text ? '<div class="leaflet-popup-content">'+text+'</div>' : '';
+		};
+
+		info.addTo(map);
+		map.eachLayer(function(layer){
+			layer.off('click').on('click',function(e,f){
+				if(layer._popup && layer._popup._content){
+				    	console.log($.inArray('informatif',layer.feature.role));
+				    	console.log(layer.feature);
+				    	if(layer.feature.properties.role && $.inArray('informatif',layer.feature.properties.role) != -1)
+				    	    return false;
+					info.update(layer._popup._content);
+					map.closePopup();
+					map.panTo(e.latlng);
+					return false;
+				}
+			});
+		});
+		map.on('popupopen',function(){
+		    	info.update();
+		});
+	    }
+	    if(map.options.options && map.options.options.layer_topojson){
+	    	var topojson_layer = new L.TOPOJSON(map.options.options.layer_topojson, {async: true}),
+	    		fichier_topojson = map1.options.options.layer_topojson.replace(/^.*[\\\/]/, '').replace(/\./g,'_'),
+	    		addlayer = fichier_topojson+'_addlayer';
+	    	if(typeof window[addlayer] == 'function'){
+        	    	topojson_layer.on('addlayer',function(e){
+        	    	    	eval(eval(addlayer)(e,topojson_layer));
+        	    	});
+	    	}
+	    	map.addLayer(topojson_layer);
+	    	topojson_layer.bringToFront();
+	    }
 	});
 });
