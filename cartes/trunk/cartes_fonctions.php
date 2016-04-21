9dcc35f1609c64f83b3aafa9fb59bbb455606367
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-04-21 14:32:43 +0000

    Il faut déclarer la balise en fait
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@96743 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ cartes/trunk/cartes_fonctions.php
@@ -0,0 +1,14 @@
+<?php
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+/**
+ * Balise GEOMETRY_MAP pour afficher le champ bounds de la table spip_cartes au format WKT
+ *
+ * @param $p
+ * @return mixed
+ */
+function balise_geometry_map_dist($p) {
+	$p->code = '$Pile[$SP][\'geometry_map\']';
+	return $p;
+}
