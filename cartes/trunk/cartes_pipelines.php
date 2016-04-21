8890efabfd5d5d2d046fffc74782ba612918af3f
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-04-21 10:40:55 +0000

    De la doc
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@96739 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- cartes/trunk/cartes_pipelines.php
+++ cartes/trunk/cartes_pipelines.php
@@ -84,6 +84,14 @@ function cartes_optimiser_base_disparus($flux){
 	return $flux;
 }
 
+/**
+ * Utilisation du pipeline pre_boucle (SPIP)
+ * Sur les boucles cartes, bounds doit être de type textuel
+ * 
+ * @pipeline pre_boucle
+ * @param object $boucle
+ * @return object
+ */
 function cartes_pre_boucle($boucle){
 	if ($boucle->type_requete == 'cartes') {
 		$boucle->select[]= 'AsText(cartes.bounds) AS geometry_map';
@@ -91,6 +99,14 @@ function cartes_pre_boucle($boucle){
 	return $boucle;
 }
 
+/**
+ * Utilisation du pipeline recuperer_fond (SPIP)
+ * Ajouter Leaflet.label dans le script js de GIS
+ * 
+ * @pipeline recuperer_fond
+ * @param array $flux 	Données du pipeline
+ * @return array 		Données du pipeline modifiées
+ */
 function cartes_recuperer_fond($flux){
 	if ($flux['args']['fond'] == 'javascript/gis.js') {
 		$ajouts = "\n". spip_file_get_contents(find_in_path('javascript/leaflet.label-src.js'));
@@ -99,6 +115,14 @@ function cartes_recuperer_fond($flux){
 	return $flux;
 }
 
+/**
+ * Utilisation du pipeline insert_head_css (SPIP)
+ * Ajout de la css de Leaflet.label dans le head de chaque page
+ * 
+ * @pipeline insert_head_css
+ * @param string $flux 	Contenu textuel de la balise #INSERT_HEAD_CSS
+ * @return string 		Contenu de la balise modifié
+ */
 function cartes_insert_head_css($flux){
 	$flux .= "\n".'<link rel="stylesheet" href="'. find_in_path('css/leaflet.label.css') .'" />';
 	return $flux;
