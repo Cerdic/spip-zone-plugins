1ca0979818be5186d7d700ff7fb8529e76d0a69c
p@henix.be <p@henix.be@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-04-24 21:12:12 +0000

    Plugin Levenshtein
    Proposer et afficher des suggestions/corrections de mot à la recherche grâce à la distance
    de Levenshtein
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@96793 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ levenshtein/trunk/lang/paquet-levenshtein_fr.php
@@ -0,0 +1,12 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// L
+	'levenshtein_description' => '',
+	'levenshtein_nom' => 'Levenshtein',
+	'levenshtein_slogan' => '',
+);
