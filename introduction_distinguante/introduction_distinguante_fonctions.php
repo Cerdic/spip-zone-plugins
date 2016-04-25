08d6f0ce47391a86d2f31cfdb6f83991f0a41216
spip.franck@lien-d-amis.net <spip.franck@lien-d-amis.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-04-25 19:48:26 +0000

    Tes tab à la place d'espaces pour être un peu plus conforme avec http://www.spip.net/fr_article3497.html#regles_codage
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@96809 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- introduction_distinguante/introduction_distinguante_fonctions.php
+++ introduction_distinguante/introduction_distinguante_fonctions.php
@@ -96,33 +96,33 @@ function introduction_distinguante($descriptif,$chapo, $texte, $longueur, $conne
 
 	if (!defined('_INTRODUCTION_SUITE')) define('_INTRODUCTION_SUITE', '&nbsp;(...)');
 	$texte = couper($texte, $longueur, _INTRODUCTION_SUITE);
-    
-    //marquer le chapeau
-    if ($notes)
-        $notes('','empiler');
-        
-    $chapo = couper(propre($chapo),$longueur);
-    
-    if ($notes)
-		$notes('','depiler');
-    if ($chapo) {// si on a un chapo
-        $texte = str_replace($chapo,"<span class='introduction-chapo'>$chapo</span>",$texte,$replace);
-        if ($replace >0){ // si on a remplacé, c'est qu'on a le chapo complet
-                $texte = str_replace("<span class='introduction-chapo'>$chapo</span>","<span class='introduction-chapo'>$chapo</span><span class='introduction-texte'>",$texte);
-                $texte = str_replace(_INTRODUCTION_SUITE,"</span>"._INTRODUCTION_SUITE,$texte);
-            }
-        else{//sinon a un chapo coupé
-                $texte = "<span class='introduction-texte'>".str_replace(_INTRODUCTION_SUITE,"</span>"._INTRODUCTION_SUITE,$texte);
-            }
-        }
-    else {//si pas de chapo
-                $texte = "<span class='introduction-texte'>".str_replace(_INTRODUCTION_SUITE,"</span>"._INTRODUCTION_SUITE,$texte);        
-        }
-    // et reparagrapher si necessaire (coherence avec le cas descriptif)
+
+	//marquer le chapeau
+	if ($notes)
+	$notes('','empiler');
+
+	$chapo = couper(propre($chapo),$longueur);
+
+	if ($notes)
+	$notes('','depiler');
+	if ($chapo) {// si on a un chapo
+		$texte = str_replace($chapo,"<span class='introduction-chapo'>$chapo</span>",$texte,$replace);
+		if ($replace >0){ // si on a remplacé, c'est qu'on a le chapo complet
+			$texte = str_replace("<span class='introduction-chapo'>$chapo</span>","<span class='introduction-chapo'>$chapo</span><span class='introduction-texte'>",$texte);
+			$texte = str_replace(_INTRODUCTION_SUITE,"</span>"._INTRODUCTION_SUITE,$texte);
+		}
+		else{//sinon a un chapo coupé
+			$texte = "<span class='introduction-texte'>".str_replace(_INTRODUCTION_SUITE,"</span>"._INTRODUCTION_SUITE,$texte);
+		}
+	}
+	else {//si pas de chapo
+		$texte = "<span class='introduction-texte'>".str_replace(_INTRODUCTION_SUITE,"</span>"._INTRODUCTION_SUITE,$texte);        
+	}
+	// et reparagrapher si necessaire (coherence avec le cas descriptif)
 	if ($GLOBALS['toujours_paragrapher'])
-		// Fermer les paragraphes
-		$texte = paragrapher($texte, $GLOBALS['toujours_paragrapher']);
-    
+	// Fermer les paragraphes
+	$texte = paragrapher($texte, $GLOBALS['toujours_paragrapher']);
+
 	return $texte;
 }
 ?>
