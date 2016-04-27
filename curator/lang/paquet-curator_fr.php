6711042f4ea4ea52e85e2003e2a27fdb8a37eb1f
ydikoi@me.com <ydikoi@me.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-04-27 12:16:59 +0000

    Ajout de #INSERT_HEAD pour utilisation conjointe avec d’autres plugins (que Etiquettes puisse utiliser Selecteur générique ou Nuage) ; Nettoyage de fichiers inutiles ;
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@96829 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- curator/lang/paquet-curator_fr.php
+++ curator/lang/paquet-curator_fr.php
@@ -6,8 +6,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// C
-	'curator_description' => 'Utilise un bookmarklet pour référencer automatiquement une page web comme article.<br/>
-	Appeler <a href=\'?exec=curator\' target=\'_blank\'>cette page</a> et glisser le lien dans votre barre d\'outil',
+	'curator_description' => 'Utilise un bookmarklet pour référencer automatiquement une page web comme article.',
 	'curator_nom' => 'curator',
 	'curator_slogan' => 'Faites de votre site un outil de curation',
 );
