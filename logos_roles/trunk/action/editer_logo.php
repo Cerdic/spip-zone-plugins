0907105eeb3c5c900983983f6bf26acc99787172
bystrano@gmx.ch <bystrano@gmx.ch@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-05-02 05:52:06 +0000

    L'API editer_logo utilise le nouveau système à base de rôles
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@96945 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- logos_roles/trunk/action/editer_logo.php
+++ logos_roles/trunk/action/editer_logo.php
@@ -29,16 +29,29 @@ if (!defined('_ECRIRE_INC_VERSION')) {
  * @param string $etat
  *     `on` ou `off`
  */
-function logo_supprimer($objet, $id_objet, $etat) {
+function logo_supprimer($objet, $id_objet, $role) {
 	$chercher_logo = charger_fonction('chercher_logo', 'inc');
 	$objet = objet_type($objet);
 	$primary = id_table_objet($objet);
-	include_spip('inc/chercher_logo');
 
-	// existe-t-il deja un logo ?
-	$logo = $chercher_logo($id_objet, $primary, $etat);
-	if ($logo) {
+	$logo = $chercher_logo($id_objet, $primary, $role);
+	// si pas de logo on ne fait rien
+	if (! $logo) {
+		return;
+	}
+
+	include_spip('action/editer_liens');
+	// S'il n'y pas de document qui gère le logo, on le supprime avec la méthode
+	// historique
+	if (! objet_trouver_liens(array('document' => '*'), array($objet => $id_objet), array('role' => $role))) {
 		spip_unlink($logo[0]);
+	} else {
+		// Si le logo est géré par un document on ne supprime que le lien
+		objet_dissocier(
+			array('document' => '*'),
+			array($objet => $id_objet),
+			array('role' => $role)
+		);
 	}
 }
 
@@ -55,134 +68,36 @@ function logo_supprimer($objet, $id_objet, $etat) {
  * @return string
  *     Erreur, sinon ''
  */
-function logo_modifier($objet, $id_objet, $etat, $source) {
+function logo_modifier($objet, $id_objet, $role, $source) {
 	$chercher_logo = charger_fonction('chercher_logo', 'inc');
 	$objet = objet_type($objet);
 	$primary = id_table_objet($objet);
 	include_spip('inc/chercher_logo');
 	$type = type_du_logo($primary);
 
-	// nom du logo
-	$nom = $type . $etat . $id_objet;
-
-	// supprimer le logo eventueel existant
-	logo_supprimer($objet, $id_objet, $etat);
-
-	include_spip('inc/documents');
-	$erreur = '';
+	// supprimer le logo éventuel existant
+	logo_supprimer($objet, $id_objet, $role);
 
 	if (!$source) {
 		spip_log('spip_image_ajouter : source inconnue');
-		$erreur = 'source inconnue';
-
-		return $erreur;
+		return $erreur = 'source inconnue';
 	}
 
-	$file_tmp = _DIR_LOGOS . $nom . '.tmp';
-
-	$ok = false;
-	// fichier dans upload/
-	if (is_string($source)) {
-		if (file_exists($source)) {
-			$ok = @copy($source, $file_tmp);
-		} elseif (file_exists($f = determine_upload() . $source)) {
-			$ok = @copy($f, $file_tmp);
-		}
-	} // Intercepter une erreur a l'envoi
-	elseif (!$erreur = check_upload_error($source['error'], '', true)) {
-		// analyse le type de l'image (on ne fait pas confiance au nom de
-		// fichier envoye par le browser : pour les Macs c'est plus sur)
-		$ok = deplacer_fichier_upload($source['tmp_name'], $file_tmp);
+	include_spip('action/ajouter_documents');
+	$ajouter_un_document = charger_fonction('ajouter_un_document', 'action');
+	// On ne passe pas l'objet, pour éviter de créer un lien avec le rôle par
+	// défaut. On fait le lien à la main un peu plus bas.
+	$id_document = $ajouter_un_document('new', $source, null, null, 'image');
+	if (is_string($id_document)) {
+		return $erreur = $id_document;
 	}
 
-	if ($erreur) {
-		return $erreur;
-	}
-	if (!$ok or !file_exists($file_tmp)) {
-		spip_log($erreur = "probleme de copie pour $file_tmp ");
+	include_spip('action/editer_liens');
+	objet_associer(
+		array('document' => $id_document),
+		array($objet => $id_objet),
+		array('role' => $role)
+	);
 
-		return $erreur;
-	}
-
-	$size = @getimagesize($file_tmp);
-	$type = !$size ? '' : ($size[2] > 3 ? '' : $GLOBALS['formats_logos'][$size[2] - 1]);
-	if ($type) {
-		@rename($file_tmp, $file_tmp . ".$type");
-		$file_tmp = $file_tmp . ".$type";
-		$poids = filesize($file_tmp);
-
-		if (defined('_LOGO_MAX_WIDTH') or defined('_LOGO_MAX_HEIGHT')) {
-
-			if ((defined('_LOGO_MAX_WIDTH') and _LOGO_MAX_WIDTH and $size[0] > _LOGO_MAX_WIDTH)
-				or (defined('_LOGO_MAX_HEIGHT') and _LOGO_MAX_HEIGHT and $size[1] > _LOGO_MAX_HEIGHT)
-			) {
-				$max_width = (defined('_LOGO_MAX_WIDTH') and _LOGO_MAX_WIDTH) ? _LOGO_MAX_WIDTH : '*';
-				$max_height = (defined('_LOGO_MAX_HEIGHT') and _LOGO_MAX_HEIGHT) ? _LOGO_MAX_HEIGHT : '*';
-
-				// pas la peine d'embeter le redacteur avec ca si on a active le calcul des miniatures
-				// on met directement a la taille maxi a la volee
-				if (isset($GLOBALS['meta']['creer_preview']) and $GLOBALS['meta']['creer_preview'] == 'oui') {
-					include_spip('inc/filtres');
-					$img = filtrer('image_reduire', $file_tmp, $max_width, $max_height);
-					$img = extraire_attribut($img, 'src');
-					$img = supprimer_timestamp($img);
-					if (@file_exists($img) and $img !== $file_tmp) {
-						spip_unlink($file_tmp);
-						@rename($img, $file_tmp);
-						$size = @getimagesize($file_tmp);
-					}
-				}
-				// verifier au cas ou image_reduire a echoue
-				if ((defined('_LOGO_MAX_WIDTH') and _LOGO_MAX_WIDTH and $size[0] > _LOGO_MAX_WIDTH)
-					or (defined('_LOGO_MAX_HEIGHT') and _LOGO_MAX_HEIGHT and $size[1] > _LOGO_MAX_HEIGHT)
-				) {
-					spip_unlink($file_tmp);
-					$erreur = _T(
-						'info_logo_max_poids',
-						array(
-							'maxi' =>
-								_T(
-									'info_largeur_vignette',
-									array(
-										'largeur_vignette' => $max_width,
-										'hauteur_vignette' => $max_height
-									)
-								),
-							'actuel' =>
-								_T(
-									'info_largeur_vignette',
-									array(
-										'largeur_vignette' => $size[0],
-										'hauteur_vignette' => $size[1]
-									)
-								)
-						)
-					);
-				}
-			}
-		}
-
-		if (!$erreur and defined('_LOGO_MAX_SIZE') and _LOGO_MAX_SIZE and $poids > _LOGO_MAX_SIZE * 1024) {
-			spip_unlink($file_tmp);
-			$erreur = _T(
-				'info_logo_max_poids',
-				array(
-					'maxi' => taille_en_octets(_LOGO_MAX_SIZE * 1024),
-					'actuel' => taille_en_octets($poids)
-				)
-			);
-		}
-
-		if (!$erreur) {
-			@rename($file_tmp, _DIR_LOGOS . $nom . ".$type");
-		}
-	} else {
-		spip_unlink($file_tmp);
-		$erreur = _T(
-			'info_logo_format_interdit',
-			array('formats' => join(', ', $GLOBALS['formats_logos']))
-		);
-	}
-
-	return $erreur;
+	return $erreur = '';
 }
