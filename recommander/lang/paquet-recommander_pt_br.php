89beb20951b93ef3e80a3e6461e8c28a7ca0d19e
rporto@calepino.com <rporto@calepino.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-05-03 03:35:02 +0000

    [Salvatore] [source:_plugins_/recommander/lang/ paquet-recommander] Export depuis http://trad.spip.net de la langue pt_br
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@96971 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ recommander/lang/paquet-recommander_pt_br.php
@@ -0,0 +1,24 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-recommander?lang_cible=pt_br
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// R
+	'recommander_description' => 'Função recomendar uma página a um amigo.
+
+Pode ser incluída em qualquer template, no formato:
+<code>#RECOMMANDER{título da página,url da página,intro da página,assunto}</code>
+_ por eexemplo:
+<code>#RECOMMANDER{#TITRE,#URL_ARTICLE,#INTRODUCTION}</code>
+_ o url, a intro e o assunto são opcionais',
+	'recommander_nom' => 'Recomendar a um amigo',
+	'recommander_slogan' => 'Função recomendar uma página a um amigo'
+);
+
+?>
