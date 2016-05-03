ac9c2a354d9f0a3264bcb48b7b4d872026bf1cee
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-05-03 12:22:34 +0000

    Code formating, espaces et simple quotes
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@96981 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- shortcut_url/trunk/shortcut_url_administrations.php
+++ shortcut_url/trunk/shortcut_url_administrations.php
@@ -9,8 +9,9 @@
  * @licence    GNU/GPL
  * @package    SPIP\shortcut_url\administrations
  */
-
-if (!defined('_ECRIRE_INC_VERSION')) return;
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
 /**
  * Installation/maj des tables shortcut_url
@@ -18,8 +19,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
  * @param string $nom_meta_base_version
  * @param string $version_cible
  */
-function shortcut_url_upgrade($nom_meta_base_version,$version_cible){
-	
+function shortcut_url_upgrade($nom_meta_base_version, $version_cible) {
 	$maj = array();
 
 	$maj['create'] = array(
@@ -27,22 +27,24 @@ function shortcut_url_upgrade($nom_meta_base_version,$version_cible){
 	);
 
 	$maj['0.0.1'] = array(
-		array('maj_tables', array('spip_shortcut_urls_bots')),
+		array('maj_tables', array('spip_shortcut_urls_bots'))
 	);
 
 	$maj['0.0.2'] = array(
 		// Changer le type de champs pour le tri par click
-		array('sql_alter',"TABLE spip_shortcut_urls MODIFY COLUMN click bigint(11)"));
+		array('sql_alter','TABLE spip_shortcut_urls MODIFY COLUMN click bigint(11)')
+	);
 
 	$maj['0.0.3'] = array(
 		// Ajouter un index à la table spip_shortcut_urls_logs
-		array('sql_alter',"TABLE spip_shortcut_urls_logs ADD INDEX (id_shortcut_url)"));
+		array('sql_alter','TABLE spip_shortcut_urls_logs ADD INDEX (id_shortcut_url)')
+	);
 
 	$maj['0.0.4'] = array(
 		// Ajouter un index à la table spip_shortcut_urls_logs et spip_shortcut_urls_bots
 		// pour l'adresse ip
-		array('sql_alter',"TABLE spip_shortcut_urls_logs ADD INDEX (ip_address)"),
-		array('sql_alter',"TABLE spip_shortcut_urls_bots ADD INDEX (ip_address)"));
+		array('sql_alter', 'TABLE spip_shortcut_urls_logs ADD INDEX (ip_address)'),
+		array('sql_alter', 'TABLE spip_shortcut_urls_bots ADD INDEX (ip_address)'));
 
 	include_spip('base/upgrade');
 	maj_plugin($nom_meta_base_version, $version_cible, $maj);
@@ -54,14 +56,13 @@ function shortcut_url_upgrade($nom_meta_base_version,$version_cible){
  * @param string $nom_meta_base_version
  */
 function shortcut_url_vider_tables($nom_meta_base_version) {
+	sql_drop_table('spip_shortcut_urls,spip_shortcut_urls_logs,spip_shortcut_urls_bots');
 
-	sql_drop_table("spip_shortcut_urls,spip_shortcut_urls_logs,spip_shortcut_urls_bots");
-
-	# Nettoyer spip_auteur_liens
-	sql_delete("spip_auteurs_liens", sql_in("objet", array('shortcut_url')));
-	# Nettoyer les versionnages
-	sql_delete("spip_versions", sql_in("objet", array('shortcut_url')));
-	sql_delete("spip_versions_fragments", sql_in("objet", array('shortcut_url')));
-	effacer_meta("shortcut_url");
+	// Nettoyer spip_auteur_liens
+	sql_delete('spip_auteurs_liens', sql_in('objet', array('shortcut_url')));
+	// Nettoyer les versionnages
+	sql_delete('spip_versions', sql_in('objet', array('shortcut_url')));
+	sql_delete('spip_versions_fragments', sql_in('objet', array('shortcut_url')));
+	effacer_meta('shortcut_url');
 	effacer_meta($nom_meta_base_version);
 }
