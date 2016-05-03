00e8e221300687801b08a46c45d8c54082062752
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-05-03 12:18:56 +0000

    Code formating, espaces et simple quotes
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@96980 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- shortcut_url/trunk/shortcut_url_pipelines.php
+++ shortcut_url/trunk/shortcut_url_pipelines.php
@@ -9,18 +9,18 @@
  * @licence    GNU/GPL
  * @package    SPIP\shortcut_url\pipelines
  */
-
-if (!defined("_ECRIRE_INC_VERSION")) return;
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
 /**
  * Autorisation du menu d'entrée dans l'admin pour spip 3.1
- * 
- * @param string $faire, $type, $id, $qui, $opt 
+ *
+ * @param string $faire, $type, $id, $qui, $opt
  * @return string
  */
-function autoriser_menugrandeentree($faire, $type, $id, $qui, $opt){
-
-	if(!in_array($type ,array('menuaccueil','menuedition','menupublication','menuadministration','menuconfiguration','menushortcuturl'))){
+function autoriser_menugrandeentree($faire, $type, $id, $qui, $opt) {
+	if (!in_array($type, array('menuaccueil', 'menuedition', 'menupublication', 'menuadministration', 'menuconfiguration', 'menushortcuturl'))) {
 		return false;
 	}
 
@@ -29,105 +29,88 @@ function autoriser_menugrandeentree($faire, $type, $id, $qui, $opt){
 
 /**
  * Autorisation du menu d'entrée dans l'admin pour spip 3.0
- * 
- * @param string $faire, $type, $id, $qui, $opt 
+ *
+ * @param string $faire, $type, $id, $qui, $opt
  * @return string
  */
-function autoriser_revisions_menu($faire, $type, $id, $qui, $opt){
+function autoriser_revisions_menu($faire, $type, $id, $qui, $opt) {
 	return true;
 }
-
-function autoriser_mediabox_menu($faire, $type, $id, $qui, $opt){
+function autoriser_mediabox_menu($faire, $type, $id, $qui, $opt) {
 	return false;
 }
-
-function autoriser_visiteurs_menu($faire, $type, $id, $qui, $opt){
+function autoriser_visiteurs_menu($faire, $type, $id, $qui, $opt) {
 	return false;
 }
-
-function autoriser_suiviedito_menu($faire, $type, $id, $qui, $opt){
+function autoriser_suiviedito_menu($faire, $type, $id, $qui, $opt) {
 	return false;
 }
-
-function autoriser_synchro_menu($faire, $type, $id, $qui, $opt){
+function autoriser_synchro_menu($faire, $type, $id, $qui, $opt) {
 	return false;
 }
-
-function autoriser_articles_menu($faire, $type, $id, $qui, $opt){
+function autoriser_articles_menu($faire, $type, $id, $qui, $opt) {
 	return false;
 }
-
-function autoriser_rubriques_menu($faire, $type, $id, $qui, $opt){
+function autoriser_rubriques_menu($faire, $type, $id, $qui, $opt) {
 	return false;
 }
-
-function autoriser_rubrique_creer($faire, $type, $id, $qui, $opt){
+function autoriser_rubrique_creer($faire, $type, $id, $qui, $opt) {
 	return false;
 }
-
-function autoriser_documents_menu($faire, $type, $id, $qui, $opt){
+function autoriser_documents_menu($faire, $type, $id, $qui, $opt) {
 	return false;
 }
-
-function autoriser_sites_menu($faire, $type, $id, $qui, $opt){
+function autoriser_sites_menu($faire, $type, $id, $qui, $opt) {
 	return false;
 }
-
-function autoriser_mots_menu($faire, $type, $id, $qui, $opt){
+function autoriser_mots_menu($faire, $type, $id, $qui, $opt) {
 	return false;
 }
-
-function autoriser_breves_menu($faire, $type, $id, $qui, $opt){
+function autoriser_breves_menu($faire, $type, $id, $qui, $opt) {
 	return false;
 }
-
-function autoriser_voirrevisions($faire, $type, $id, $qui, $opt){
+function autoriser_voirrevisions($faire, $type, $id, $qui, $opt) {
 	return true;
 }
 
 /**
- * Ajouter un bouton stats 
- * 
- * @param string $faire, $type, $id, $qui, $opt 
+ * Ajouter un bouton stats
+ *
+ * @param string $faire, $type, $id, $qui, $opt
  * @return string
  */
-function shortcut_url_ajouter_menus($boutons_admin){
+function shortcut_url_ajouter_menus($boutons_admin) {
 	include_spip('inc/autoriser');
-	if(autoriser('menu','_menu_shortcut_url')){
+	if (autoriser('menu', '_menu_shortcut_url')) {
 		$pages = array('shortcut_url_logs', 'shortcut_url_logs_export');
-		foreach($pages as $page){
+		foreach ($pages as $page) {
 			$boutons_admin['menu_shortcut_url']->sousmenu[] = new Bouton(find_in_theme('images/shortcut_url-16.png'), 'shortcut_url:' . $page, $page);
 		}
-	}
-	else{
+	} else {
 		unset($boutons_admin['menu_shortcut_url']);
 	}
 
 	return $boutons_admin;
 }
-
-function autoriser_menushortcuturl_menu($faire, $type, $id, $qui, $opt){
-	return in_array($qui['statut'], array('1comite','0minirezo')) && count($qui['restreint']) == 0;
+function autoriser_menushortcuturl_menu($faire, $type, $id, $qui, $opt) {
+	return in_array($qui['statut'], array('1comite', '0minirezo')) && count($qui['restreint']) == 0;
 }
 
 /**
  * Affiche les URL publié par un auteur dans sa fiche
- * 
- * @param string $flux 
+ *
+ * @param string $flux
  * @return string
  */
 function shortcut_url_affiche_milieu($flux) {
-	if (trouver_objet_exec($flux['args']['exec'] == "auteur") && $flux['args']['id_auteur']){
+	if (trouver_objet_exec($flux['args']['exec'] == 'auteur') && $flux['args']['id_auteur']) {
 		$id_auteur = $flux['args']['id_auteur'];
-		$texte = recuperer_fond('prive/objets/editer/shortcut_url_auteurs',
-			array(
-				'id_auteur'=>$id_auteur
-			)
-		);
-		if ($p=strpos($flux['data'],"<!--affiche_milieu-->"))
+		$texte = recuperer_fond('prive/objets/editer/shortcut_url_auteurs', array('id_auteur' => $id_auteur));
+		if ($p = strpos($flux['data'], '<!--affiche_milieu-->')) {
 			$flux['data'] .= $texte;
-		else
-			$flux['data'] = substr_replace($flux['data'],$texte,$p,0);
+		} else {
+			$flux['data'] = substr_replace($flux['data'], $texte, $p, 0);
+		}
 	}
 
 	return $flux;
@@ -135,14 +118,13 @@ function shortcut_url_affiche_milieu($flux) {
 
 /**
  * Ajoute les css pour shortcut_url chargées dans le privé
- * 
+ *
  * @param string $flux Contenu du head HTML concernant les CSS
  * @return string       Contenu du head HTML concernant les CSS
  */
 function shortcut_url_header_prive_css($flux) {
-
 	$css = find_in_path('css/shortcut_url.css');
-	$flux .= "<link rel='stylesheet' type='text/css' media='all' href='".direction_css($css)."' />\n";
+	$flux .= "<link rel='stylesheet' type='text/css' media='all' href='" . direction_css($css) . "' />\n";
 
 	return $flux;
 }
@@ -153,8 +135,8 @@ function shortcut_url_header_prive_css($flux) {
  * @param $flux
  * @return mixed
  */
-function shortcut_url_d3js_plugins($plugins){
-	if(test_espace_prive()){
+function shortcut_url_d3js_plugins($plugins) {
+	if (test_espace_prive()) {
 		$plugins[] = 'topojson';
 	}
 	return $plugins;
