94e8c3e1f8af2e6e54e4ea3bfc76e8b4e4bd7b06
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-05-06 17:52:28 +0000

    [Salvatore] [source:_plugins_/auteur_evenement/trunk/lang/ auteur_evenement] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@97212 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- auteur_evenement/trunk/lang/auteur_evenement_es.php
+++ auteur_evenement/trunk/lang/auteur_evenement_es.php
@@ -15,4 +15,3 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// I
 	'info_evenements_auteur' => 'Los eventos de esta autora o autor'
 );
-
--- auteur_evenement/trunk/lang/auteur_evenement_fr.php
+++ auteur_evenement/trunk/lang/auteur_evenement_fr.php
@@ -13,4 +13,3 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// I
 	'info_evenements_auteur' => 'Les événements de cet auteur'
 );
-
