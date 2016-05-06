a8fba7b651157581ba2aca63ab5e5156140b6c49
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-05-06 12:41:08 +0000

    PSR SPIP
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@97005 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- trad-lang/trunk/action/tradlang_creer_langue_cible.php
+++ trad-lang/trunk/action/tradlang_creer_langue_cible.php
@@ -5,14 +5,14 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 
 /**
  * Action permettant de creer une langue cible depuis la langue mère d'un module
- * 
- * @return 
+ *
+ * @return
  */
-function action_tradlang_creer_langue_cible_dist(){
+function action_tradlang_creer_langue_cible_dist() {
 	$securiser_action = charger_fonction('securiser_action', 'inc');
 	$arg = $securiser_action();
-	if (!preg_match(",^(\w+)$,", $arg, $r)){
-		spip_log("action_tradlang_creer_langue_cible $arg pas compris",'tradlang.'._LOG_ERREUR);
+	if (!preg_match(',^(\w+)$,', $arg, $r)) {
+		spip_log("action_tradlang_creer_langue_cible $arg pas compris", 'tradlang.' . _LOG_ERREUR);
 		return false;
 	}
 
@@ -20,23 +20,23 @@ function action_tradlang_creer_langue_cible_dist(){
 
 	include_spip('inc/autoriser');
 	$lang_crea = _request('lang_crea');
-	if($lang_crea && intval($arg) && autoriser('modifier','tradlang')){
+	if ($lang_crea and intval($arg) and autoriser('modifier', 'tradlang')) {
 		// Import de la langue mere
-		$infos_module = sql_fetsel('*','spip_tradlang_modules','id_tradlang_module='.intval($arg));
-		if(!$infos_module)
-			spip_log('tradlang_creer_langue_cible : infos_module non existant','tradlang.'._LOG_ERREUR);
-		else{
-			$ajouter_code_langue = charger_fonction('tradlang_ajouter_code_langue','inc');
+		$infos_module = sql_fetsel('*', 'spip_tradlang_modules', 'id_tradlang_module = ' . intval($arg));
+		if (!$infos_module) {
+			spip_log('tradlang_creer_langue_cible : infos_module non existant', 'tradlang.' . _LOG_ERREUR);
+		} else {
+			$ajouter_code_langue = charger_fonction('tradlang_ajouter_code_langue', 'inc');
 			$ajouter_code_langue($infos_module,$lang_crea);
 		}
-	}else
-		spip_log("action_tradlang_creer_langue_cible : Module $module_nom inexistant",'tradlang.'._LOG_ERREUR);
+	} else {
+		spip_log("action_tradlang_creer_langue_cible : Module $module_nom inexistant", 'tradlang.' . _LOG_ERREUR);
+	}
 
 	$redirect = _request('redirect');
-	if($redirect){
-		$redirect = parametre_url($redirect,'var_lang_crea',$lang_crea,'&');
+	if ($redirect) {
+		$redirect = parametre_url($redirect, 'var_lang_crea', $lang_crea, '&');
 		include_spip('inc/headers');
 		redirige_par_entete($redirect);
 	}
 }
-?>
