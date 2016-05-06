1458a7aea2d2421c0c077cf91ffba12e1e636c17
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-05-06 13:31:37 +0000

    PSR SPIP
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@97007 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- trad-lang/trunk/inc/tradlang_importer_module.php
+++ trad-lang/trunk/inc/tradlang_importer_module.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * 
+ *
  * Trad-lang v1
  * Plugin SPIP de traduction de fichiers de langue
  * © Florent Jugla, Fil
- * 
+ *
  */
 
 if (!defined('_ECRIRE_INC_VERSION')) {
@@ -12,13 +12,13 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 }
 
 /**
- * 
+ *
  * Import d'un module de langue dans Trad-lang
- * 
+ *
  * @param object $module
- * @return 
+ * @return
  */
-function inc_tradlang_importer_module($module,$dir_lang=false,$new_only=false){
+function inc_tradlang_importer_module($module, $dir_lang = false, $new_only = false) {
 	include_spip('inc/texte');
 	include_spip('inc/lang_liste');
 	$ret = '';
@@ -26,235 +26,237 @@ function inc_tradlang_importer_module($module,$dir_lang=false,$new_only=false){
 	/**
 	 * On ne fournit pas de dir_lang donc on se base sur les fichiers du path
 	 */
-	if(!$dir_lang){
+	if (!$dir_lang) {
 		list($select_modules, $tous_modules) = tradlang_select_liste_rep_lang();
 		$nom_mod = $module['nom_mod'];
 		$module_choisi = $tous_modules[$nom_mod];
-		$langues_module = explode(',',$module_choisi['langues']);
+		$langues_module = explode(',', $module_choisi['langues']);
 	}
-	
-	if(!($res = sql_getfetsel('id_tradlang_module','spip_tradlang_modules','nom_mod='.sql_quote($module['nom_mod'])))){
+
+	if (!($res = sql_getfetsel('id_tradlang_module', 'spip_tradlang_modules', 'nom_mod = ' . sql_quote($module['nom_mod'])))) {
 		/**
 		 * Insertion du module dans la base
 		 */
-		$res = sql_insertq("spip_tradlang_modules",$module);
-		$mode = 'new';	
-	}else
+		$res = sql_insertq('spip_tradlang_modules', $module);
+		$mode = 'new';
+	} else {
 		$mode = 'update';
-	
-	if ($new_only && ($mode=='update')){
-		$ret .= propre(_T('tradlang:module_deja_importe',array('module'=>$module['module'])));
+	}
+
+	if ($new_only && ($mode=='update')) {
+		$ret .= propre(_T('tradlang:module_deja_importe', array('module' => $module['module'])));
 		return array($ret,false);
 	}
-	
+
 	/**
 	 * Insertion de chaque fichier de langue existant dans la base
 	 */
 	$liste_id_orig = array();
 	array_unshift($langues_module, $module['lang_mere']);
 	array_unique($langues_module);
-	foreach($langues_module as $langue){
+	foreach ($langues_module as $langue) {
 		$chs = null;
 		$fichier = $module_choisi[$langue]['fichier'];
 		$orig = 0;
-		if ($langue == $module['lang_mere'])
+		if ($langue == $module['lang_mere']) {
 			$orig = 1;
+		}
 
-		$ret .= _T('tradlang:insertionlangue')." : ".$langue."<br />";
-		$nom_fichier = _DIR_RACINE.$module['dir_lang']."/".$fichier;
-		
+		$ret .= _T('tradlang:insertionlangue')." : $langue<br />";
+		$nom_fichier = _DIR_RACINE.$module['dir_lang'] . "/$fichier";
+
 		include($nom_fichier);
 		$chs = $GLOBALS[$GLOBALS['idx_lang']];
-		if (is_null($chs))
+		if (is_null($chs)) {
 			return false;
-		
+		}
 		reset($chs);
-		
+
 		// nettoyer le contenu de ses <MODIF>
 		$statut = array();
-		foreach($chs as $id=>$v) {
+		foreach ($chs as $id => $v) {
 			if (preg_match(',^<(MODIF|NEW|PLUS_UTILISE)>,US', $v, $r)) {
 				$chs[$id] = preg_replace(',^(<(MODIF|NEW|PLUS_UTILISE)>)+,US', '', $v);
 				$statut[$id] = $r[1];
-			}
-			else
+			} else {
 				$statut[$id] = '';
+			}
 		}
-		
-		$res = sql_select("id, str, md5","spip_tradlangs","module=".sql_quote($nom_mod)." AND lang=".sql_quote($langue));
-		if($mode == 'update'){
-			if(sql_count($res)>0)
-				spip_log("Fichier de langue $langue du module $nom_mod deja inclus dans la base\n","tradlang");
+
+		$res = sql_select('id, str, md5', 'spip_tradlangs', 'module=' . sql_quote($nom_mod) . ' AND lang = ' . sql_quote($langue));
+		if ($mode == 'update' and sql_count($res) > 0) {
+			spip_log("Fichier de langue $langue du module $nom_mod deja inclus dans la base\n", 'tradlang');
 		}
 		$existant = array();
-		while ($t = sql_fetch($res))
+		while ($t = sql_fetch($res)) {
 			$existant[$t['id']] = $t['md5'];
-			
+		}
+
 		$ajoutees = $inchangees = $supprimees = $modifiees = $ignorees = 0;
-		
+
 		// Dans ce qui arrive, il y a 4 cas :
-		foreach (array_unique(
-					array_merge(array_keys($existant), array_keys($chs))
-				) as $id) {
-			if (isset($chs[$id]) AND !isset($existant[$id])){
+		foreach (array_unique(array_merge(array_keys($existant), array_keys($chs))) as $id) {
+			if (isset($chs[$id]) and !isset($existant[$id])) {
 				unset($md5);
-				if ($orig)
+				if ($orig) {
 					$md5 = md5($chs[$id]);
-				else if (!isset($liste_id_orig[$id])) {
-					spip_log("!-- Chaine $id inconnue dans la langue principale\n","tradlang");
+				} elseif (!isset($liste_id_orig[$id])) {
+					spip_log("!-- Chaine $id inconnue dans la langue principale\n", 'tradlang');
 					$ignorees++;
-				}else
+				} else {
 					$md5 = $liste_id_orig[$id];
+				}
 
 				/**
 				 * zz_timestamp_nepastraduire est ce qui nous permet de vérifier la synchro
 				 * BDD / fichiers
 				 */
-				if (isset($md5) && ($id != 'zz_timestamp_nepastraduire')){
-					sql_insertq('spip_tradlangs',array(
-						'id' => $id,
-						'id_tradlang_module' => $module['id_tradlang_module'],
-						'module' => $module["module"],
-						'str' => $chs[$id],
-						'lang' => $langue,
-						'orig' => $orig,
-						'md5' => $md5,
-						'statut' => $statut[$id]
-					));
+				if (isset($md5) && ($id != 'zz_timestamp_nepastraduire')) {
+					sql_insertq(
+						'spip_tradlangs',
+						array(
+							'id' => $id,
+							'id_tradlang_module' => $module['id_tradlang_module'],
+							'module' => $module['module'],
+							'str' => $chs[$id],
+							'lang' => $langue,
+							'orig' => $orig,
+							'md5' => $md5,
+							'statut' => $statut[$id])
+					);
 					$ajoutees++;
 				}
-			}
-			else
-			// * chaine existante
-			if (isset($chs[$id]) AND isset($existant[$id])){
-				spip_log('cas 2','tradlang');
+			} elseif (isset($chs[$id]) and isset($existant[$id])) {
+				// * chaine existante
+				spip_log('cas 2', 'tradlang');
 				// * identique ? => NOOP
 				$md5 = md5($chs[$id]);
-				if ($md5 == $existant[$id])
+				if ($md5 == $existant[$id]) {
 					$inchangees++;
-				// * modifiee ? => UPDATE
-				else {
+				} else {
+					// * modifiee ? => UPDATE
 					// modifier la chaine
 					$md5_new = $orig ? $md5 : $existant[$id];
-					sql_updateq("spip_tradlangs",array(
+					sql_updateq('spip_tradlangs', array(
 						'str' => $str,
 						'md5' => $md5_new,
 						'statut' => '',
-						), "module=".sql_quote($nom_mod)." AND lang=".sql_quote($langue)." AND id=".sql_quote($id));
-					
+						), 'module = ' . sql_quote($nom_mod).' AND lang = ' . sql_quote($langue) . ' AND id = '.sql_quote($id));
+
 					// signaler le statut MODIF de ses traductions
-					if ($orig)
-						sql_updateq("spip_tradlangs",array('statut'=>'MODIF'),"module=".sql_quote($nom_mod)." AND id=".sql_quote($id)." AND md5 !=".sql_quote($md5));
+					if ($orig) {
+						sql_updateq('spip_tradlangs', array('statut' => 'MODIF'), 'module = ' . sql_quote($nom_mod) . ' AND id = ' . sql_quote($id) . ' AND md5 != ' . sql_quote($md5));
+					}
 					$modifiees++;
 				}
-			}
-			else
-			// * chaine supprimee
-			if (!isset($chs[$id]) AND isset($existant[$id])){
-				spip_log('cas 3','tradlang');
+			} else if (!isset($chs[$id]) and isset($existant[$id])) {
+				// * chaine supprimee
+				spip_log('cas 3', 'tradlang');
 				// mettre au grenier
-				sql_updateq("spip_tradlangs",array(
-					'id' => $id,
-					'statut' => 'attic'),"id=".sql_quote($id)." AND module=".sql_quote($nom_mod));
+				sql_updateq('spip_tradlangs', array('id' => $id, 'statut' => 'attic'), 'id = ' . sql_quote($id) . ' AND module = '.sql_quote($nom_mod));
 				$supprimees++;
 			}
 
-			if ($orig AND isset($chs[$id])){
-				spip_log('cas 4','tradlang');
+			if ($orig and isset($chs[$id])) {
+				spip_log('cas 4', 'tradlang');
 				$liste_id_orig[$id]=md5($chs[$id]);
 			}
 		}
-		
+
 		/**
 		 * Si ce n'est pas la langue mère que l'on importe :
 		 * - On ajoute dans la base les chaines manquantes dans le fichier de la langue
 		 * - On met le statut attic pour les chaines en trop dans les fichiers
 		 */
-		if($langue != $module['lang_mere']){
-			$tradlang_verifier_langue_base = charger_fonction('tradlang_verifier_langue_base','inc');
+		if ($langue != $module['lang_mere']) {
+			$tradlang_verifier_langue_base = charger_fonction('tradlang_verifier_langue_base', 'inc');
 			$tradlang_verifier_langue_base($nom_mod,$langue);
 		}
 		// si le fichier est inscriptible, on sauvegarde le
 		// fichier depuis la base afin de tagguer le timestamp
-		if ($fd = @fopen($nom_fichier, "a")){
+		if ($fd = @fopen($nom_fichier, 'a')) {
 			fclose($fd);
-			$sauvegarde = charger_fonction('tradlang_sauvegarde_module','inc');
+			$sauvegarde = charger_fonction('tradlang_sauvegarde_module', 'inc');
 			$sauvegarde($module,$langue);
 		}
 		unset($GLOBALS[$GLOBALS['idx_lang']]);
-		$ret .= _T('tradlang:insertionlangueok')."<br />";
+		$ret .= _T('tradlang:insertionlangueok') . '<br />';
 	}
 	return array($ret,true);
 }
 
 /**
- * 
+ *
  * Liste les modules qui sont potentiellement traduisibles
- * 
+ *
  * @param string $name [optional] le name et id du select
  * @param string $selected [optional] l'option à sélectionner par défaut
- * @return Array un Array avec en première clé 'select_string' qui est un 
+ * @return Array un Array avec en première clé 'select_string' qui est un
  * input select utilisable et en seconde un array complet des informations des
  * fichiers de langue
  */
-function tradlang_select_liste_rep_lang($name="repertoirelangue",$selected='',$new_only='false'){
+function tradlang_select_liste_rep_lang($name = 'repertoirelangue', $selected = '', $new_only = false) {
 	$ret = '';
 	$tous_modules_en_base = array();
-	if($new_only){
+	if ($new_only) {
 		$tous_modules_en_base = array();
-		$tous_modules_base = sql_allfetsel('module','spip_tradlang_modules');
-		foreach($tous_modules_base as $module => $nom_module){
+		$tous_modules_base = sql_allfetsel('module', 'spip_tradlang_modules');
+		foreach ($tous_modules_base as $module => $nom_module) {
 			$tous_modules_en_base[] = $nom_module['module'];
 		}
 	}
 	$tous_modules = array();
-	$fichiers_lang = find_all_in_path('lang/','[a-z]?\_[a-z_]{2,7}\.php$');
+	$fichiers_lang = find_all_in_path('lang/', '[a-z]?\_[a-z_]{2,7}\.php$');
 	ksort($fichiers_lang);
-	if(count($fichiers_lang) > 0){
+	if (count($fichiers_lang) > 0) {
 		$ret .= "<select name=\"$name\" id=\"$name\" class=\"text\">\n";
-		foreach($fichiers_lang as $fichier => $chemin){
+		foreach ($fichiers_lang as $fichier => $chemin) {
 			$sel = '';
-			if(preg_match('/^([a-z]*)_([a-z_]*)\.php$/i',$fichier,$module)){
-				if(array_key_exists($module[2],$GLOBALS['codes_langues'])){
-					if(!in_array($module[1],$tous_modules_en_base)){
-						if(test_espace_prive()){
-							if(strpos($chemin, _DIR_RACINE) !== false){
+			if (preg_match('/^([a-z]*)_([a-z_]*)\.php$/i', $fichier, $module)) {
+				if (array_key_exists($module[2], $GLOBALS['codes_langues'])) {
+					if (!in_array($module[1], $tous_modules_en_base)) {
+						if (test_espace_prive()) {
+							if (strpos($chemin, _DIR_RACINE) !== false) {
 								$search = _DIR_RACINE;
-								$chemin = str_replace($search,'',$chemin);
-							}else{
+								$chemin = str_replace($search, '', $chemin);
+							} else {
 								$chemin = _DIR_RESTREINT_ABS.$chemin;
 							}
 						}
-						if(!array_key_exists($module[1],$tous_modules)){
-							if($module[1] == $selected)
-								$sel = "selected=\"selected\"";
-							$ret .= "<option value=\"".$module[1]."\"$sel>".$module[1]."</option>\n";
+						if (!array_key_exists($module[1], $tous_modules)) {
+							if ($module[1] == $selected) {
+								$sel = 'selected="selected"';
+							}
+							$ret .= '<option value="' . $module[1] . "\"$sel>" . $module[1] . "</option>\n";
 							$tous_modules[$module[1]]['repertoire'] = dirname($chemin);
 							$tous_modules[$module[1]]['langues'] = $module[2];
-						}else
-							$tous_modules[$module[1]]['langues'] .= ",".$module[2];
+						} else {
+							$tous_modules[$module[1]]['langues'] .= ','.$module[2];
+						}
 						$tous_modules[$module[1]][$module[2]] = array('fichier' => basename($chemin));
 					}
-				}
-				else if(preg_match('/^([a-z]*_[a-z]*)_([a-z_]*)\.php$/i',$fichier,$module)){
-					if(array_key_exists($module[2],$GLOBALS['codes_langues'])){
-						if(test_espace_prive()){
-							if(strpos($chemin, _DIR_RACINE) !== false){
+				} elseif (preg_match('/^([a-z]*_[a-z]*)_([a-z_]*)\.php$/i', $fichier, $module)) {
+					if (array_key_exists($module[2], $GLOBALS['codes_langues'])) {
+						if (test_espace_prive()) {
+							if (strpos($chemin, _DIR_RACINE) !== false) {
 								$search = _DIR_RACINE;
-								$chemin = str_replace($search,'',$chemin);
-							}else
+								$chemin = str_replace($search, '', $chemin);
+							} else {
 								$chemin = _DIR_RESTREINT_ABS.$chemin;
+							}
 						}
-						if(!in_array($module[1],$tous_modules_en_base)){
-							if(!array_key_exists($module[1],$tous_modules)){
-								if($module[1] == $selected)
-									$sel = "selected=\"selected\"";
-								$ret .= "<option value=\"".$module[1]."\"$sel>".$module[1]."</option>\n";
+						if (!in_array($module[1], $tous_modules_en_base)) {
+							if (!array_key_exists($module[1], $tous_modules)) {
+								if ($module[1] == $selected) {
+									$sel = 'selected="selected"';
+								}
+								$ret .= '<option value="' . $module[1] . "\"$sel>" . $module[1] . "</option>\n";
 								$tous_modules[$module[1]]['repertoire'] = dirname($chemin);
 								$tous_modules[$module[1]]['langues'] = $module[2];
-							}else
-								$tous_modules[$module[1]]['langues'] .= ",".$module[2];	
-							
+							} else {
+								$tous_modules[$module[1]]['langues'] .= ','.$module[2];
+							}
+
 							$tous_modules[$module[1]][$module[2]] = array('fichier' => basename($chemin));
 						}
 					}
@@ -267,23 +269,23 @@ function tradlang_select_liste_rep_lang($name="repertoirelangue",$selected='',$n
 }
 
 /**
- * 
+ *
  * Un select listant les langues disponibles pour un module
- * 
+ *
  * @param string $module Le module de langue
  * @param string $name [optional] Le name et l'id du champs select
  * @param string $selected [optional] La langue à sélectionner par défaut
- * @return 
+ * @return
  */
-function tradlang_select_langues_module($module, $name="langue_mere",$selected="",$option_vide=false){
+function tradlang_select_langues_module($module, $name = 'langue_mere', $selected = '', $option_vide = false) {
 	$ret = '';
 	list($select,$tous_modules) = tradlang_select_liste_rep_lang();
 	$langues = $tous_modules[$module]['langues'];
-	if(is_array($tous_modules[$module]) && (count(explode(',',$langues)) > 0)){
-		$langues = explode(',',$langues);
+	if (is_array($tous_modules[$module]) and (count(explode(',', $langues)) > 0)) {
+		$langues = explode(',', $langues);
 		$ret .= "<select class=\"text\" name=\"$name\" id=\"$name\">\n";
-		$ret .= $option_vide ? "<option value=\"\">--</option>" : ""; 
-		foreach($langues as $langue){
+		$ret .= $option_vide ? '<option value="">--</option>' : '';
+		foreach ($langues as $langue) {
 			$ret .= "<option value=\"$langue\">".traduire_nom_langue($langue)."</option>\n";
 		}
 		$ret .= "</select>\n";
@@ -291,19 +293,19 @@ function tradlang_select_langues_module($module, $name="langue_mere",$selected="
 	return $ret;
 }
 
-function tradlang_verifier_acces_fichiers($module){
+function tradlang_verifier_acces_fichiers($module) {
 	list($select,$tous_modules) = tradlang_select_liste_rep_lang();
 	$infos_modules = $tous_modules[$module];
-	
-	$langues = explode(',',$infos_modules['langues']);
+
+	$langues = explode(',', $infos_modules['langues']);
 	// test si fichier inscriptible
-	foreach($langues as $lang){
-		$fichier = _DIR_RACINE.$infos_modules['repertoire']."/".$infos_modules[$lang]['fichier'];
-		if (!$fd = @fopen($fichier, "a"))
+	foreach ($langues as $lang) {
+		$fichier = _DIR_RACINE.$infos_modules['repertoire'].'/'.$infos_modules[$lang]['fichier'];
+		if (!$fd = @fopen($fichier, 'a')) {
 			$ficnok[] = $fichier;
-		else
+		} else {
 			fclose($fd);
+		}
 	}
 	return $ficnok;
 }
-?>
--- trad-lang/trunk/inc/tradlang_supprimer_module.php
+++ trad-lang/trunk/inc/tradlang_supprimer_module.php
@@ -6,46 +6,45 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 
 /**
  * Fonction de suppression de module
- * 
+ *
  * @param int $id_tradlang_module
  * 	Identifiant numérique du module à supprimer
  * @return bool
  */
-function inc_tradlang_supprimer_module($id_tradlang_module){
-	if(intval($id_tradlang_module) > 0){
+function inc_tradlang_supprimer_module($id_tradlang_module) {
+	if (intval($id_tradlang_module) > 0) {
 		/**
 		 * Le module existe-t-il ?
 		 */
-		$module = sql_getfetsel('id_tradlang_module','spip_tradlang_modules','id_tradlang_module='.intval($id_tradlang_module));
-		if($module){
+		$module = sql_getfetsel('id_tradlang_module', 'spip_tradlang_modules', 'id_tradlang_module = ' . intval($id_tradlang_module));
+		if ($module) {
 			/**
 			 * Suppression des versions des chaines de langue
 			 */
-			$tradlangs = sql_allfetsel('id_tradlang','spip_tradlangs','id_tradlang_module='.intval($module));
+			$tradlangs = sql_allfetsel('id_tradlang', 'spip_tradlangs', 'id_tradlang_module=' . intval($module));
 			$tradlangs_supprimer = array();
-			foreach($tradlangs as $tradlang){
+			foreach ($tradlangs as $tradlang) {
 				$tradlangs_supprimer[] = $tradlang['id_tradlang'];
 			}
-			if(count($tradlangs_supprimer)){
-				sql_delete('spip_versions','objet="tradlang" AND '.sql_in('id_objet',$tradlangs_supprimer));
-				sql_delete('spip_versions_fragments','objet="tradlang" AND '.sql_in('id_objet',$tradlangs_supprimer));
+			if (count($tradlangs_supprimer)) {
+				sql_delete('spip_versions', 'objet="tradlang" AND ' . sql_in('id_objet', $tradlangs_supprimer));
+				sql_delete('spip_versions_fragments', 'objet="tradlang" AND ' . sql_in('id_objet', $tradlangs_supprimer));
 			}
 			/**
 			 * Suppression des chaines de langue, du module et des bilans
 			 */
-			sql_delete('spip_tradlangs','id_tradlang_module='.intval($module));
-			sql_delete('spip_tradlang_modules','id_tradlang_module='.intval($module));
-			sql_delete('spip_tradlangs_bilans','id_tradlang_module='.intval($module));
+			sql_delete('spip_tradlangs', 'id_tradlang_module = ' . intval($module));
+			sql_delete('spip_tradlang_modules', 'id_tradlang_module = ' . intval($module));
+			sql_delete('spip_tradlangs_bilans', 'id_tradlang_module = ' . intval($module));
 			/**
 			 * Suppression des versions du module
 			 */
-			sql_delete('spip_versions_fragments','objet="tradlang_module" AND id_objet='.intval($module));
-			sql_delete('spip_versions','objet="tradlang_module" AND id_objet='.intval($module));
+			sql_delete('spip_versions_fragments', 'objet="tradlang_module" AND id_objet=' . intval($module));
+			sql_delete('spip_versions', 'objet="tradlang_module" AND id_objet = ' . intval($module));
 			return true;
-		}else
+		} else {
 			return false;
-	}else
-		return false;
-	
+		}
+	}
+	return false;
 }
-?>
