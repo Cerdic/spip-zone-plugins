e678b5004cefc6d1487c733fb94908bcc15f07bd
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-05-07 19:40:13 +0000

    [Salvatore] [source:_plugins_/modeles_media/trunk/lang/ paquet-media] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@97419 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- modeles_media/trunk/lang/paquet-media_fr.php
+++ modeles_media/trunk/lang/paquet-media_fr.php
@@ -11,5 +11,3 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'media_description' => 'Les modèles <doc>, <img> et <emb> produisent chacun un résultat différent et ce résultat, pour les images, dépend du fait qu’elle soit dans le portfolio ou non. Ce plugin propose une nouvelle série de modèles ayant un comportement unifié et indépendant du mode des images. Les modèles existants (doc, emb, img) ne sont pas modifiés afin d’assurer la rétrocompatibilité.',
 	'media_slogan' => 'Modèles alternatifs pour l’insertion des documents'
 );
-
-?>
