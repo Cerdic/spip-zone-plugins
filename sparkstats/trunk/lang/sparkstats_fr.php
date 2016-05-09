777d4770d645d854c9ac598d657108a1761b6d1a
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-05-09 07:26:14 +0000

    PSR SPIP
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@97644 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- sparkstats/trunk/lang/sparkstats_fr.php
+++ sparkstats/trunk/lang/sparkstats_fr.php
@@ -1,7 +1,9 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/_dev_/notifications/lang
-if (!defined("_ECRIRE_INC_VERSION")) return;
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// D
@@ -13,5 +15,3 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// T
 	'titre_sparkstats' => 'Sparkstats'
 );
-
-?>
