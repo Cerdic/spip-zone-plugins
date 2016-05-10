9cecb558bb3eec9f7506b66c7ab6544f4fa4bb8d
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-05-10 22:17:19 +0000

    Comme vu dans la roadmap : - On ajoute un pipeline 'sociaux_lister' qui permet d'alimenter le formulaire de configuration du plugin ; - On déplace la librairie css/fonts dans le répertoire lib/socicon pour faciliter la maj de ladite librairie ; - On garde l'habillage par défaut dans css/sociaux.css qui ne fait pas parti, je crois, de la librairie ; - Maj de la librairie socicon 2.5 -> 3.0.3 - google-plus devient googleplus ; - Petit 'bug' : email doit être mail dans la librairie. Il faudrait reporter ce correctif là dans la branche v1. - Numéro majeur : v2.0.0
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@97692 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- sociaux/trunk/sociaux_pipelines.php
+++ sociaux/trunk/sociaux_pipelines.php
@@ -19,9 +19,33 @@ if (!defined("_ECRIRE_INC_VERSION")) {
 function sociaux_insert_head_css($head) {
 	include_spip('inc/config');
 	if (lire_config('sociaux/css', 0)) {
+		$head .= '<link rel="stylesheet" type="text/css" href="' . find_in_path('lib/socicon/styles.css') . '" />' . "\n";
 		$head .= '<link rel="stylesheet" type="text/css" href="' . find_in_path('css/sociaux.css') . '" />' . "\n";
 	}
 
 	return $head;
 }
 
+function sociaux_sociaux_lister($flux) {
+	/**
+	 * On reprend les éléments initiaux de la v1 du plugin :
+	 * facebook, twitter, instagram, google-plus, blogger, pinterest, linkedin, youtube, rss, email, tripadvisor, vimeo, flickr
+	 */
+	if (isset($flux['data'])) {
+		$flux['data']['facebook'] = 'Facebook';
+		$flux['data']['twitter'] = 'Twitter';
+		$flux['data']['instagram'] = 'Instagram';
+		$flux['data']['googleplus'] = 'Google Plus';
+		$flux['data']['blogger'] = 'Blogger';
+		$flux['data']['pinterest'] = 'Pinterest';
+		$flux['data']['linkedin'] = 'Linkedin';
+		$flux['data']['youtube'] = 'Youtube';
+		$flux['data']['rss'] = 'RSS';
+		$flux['data']['mail'] = 'E-mail'; // Dans la v1, on faisait référence à 'email' mais c'est une erreur
+		$flux['data']['tripadvisor'] = 'TripAdvisor';
+		$flux['data']['vimeo'] = 'Vimeo';
+		$flux['data']['flickr'] = 'Flickr';
+	}
+
+	return $flux;
+}
