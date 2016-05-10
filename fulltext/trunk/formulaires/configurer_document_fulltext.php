7a40605b28eba2e59b342cabf0f0d4ca004a55ce
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-05-10 07:25:21 +0000

    On enlève ce qui est commenté
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@97657 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- fulltext/trunk/formulaires/configurer_document_fulltext.php
+++ fulltext/trunk/formulaires/configurer_document_fulltext.php
@@ -1,11 +1,13 @@
 <?php
 
-if (!defined("_ECRIRE_INC_VERSION")) return;
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
-function formulaires_configurer_document_fulltext_charger_dist(){
+function formulaires_configurer_document_fulltext_charger_dist() {
 	include_spip('inc/config');
 
-	//Valeurs prealablement saisie ou par defaut/d'exemple 
+	//Valeurs prealablement saisie ou par defaut/d'exemple
 	$valeur = array(
 		'intervalle_cron' => lire_config('fulltext/intervalle_cron', 600),
 		'nb_docs'         => lire_config('fulltext/nb_docs', 5),
@@ -36,91 +38,91 @@ function formulaires_configurer_document_fulltext_charger_dist(){
 		'xlsx_index' => lire_config('fulltext/xlsx_index', 'off'),
 	);
 
-	if (defined('_FULLTEXT_DOC_EXE')){
+	if (defined('_FULLTEXT_DOC_EXE')) {
 		$valeur['doc_bin'] = _FULLTEXT_DOC_EXE;
 		$valeur['doc_bin_readonly'] = true;
 	}
-	if (defined('_FULLTEXT_DOC_CMD_OPTIONS')){
+	if (defined('_FULLTEXT_DOC_CMD_OPTIONS')) {
 		$valeur['doc_opt'] = _FULLTEXT_DOC_CMD_OPTIONS;
 		$valeur['doc_opt_readonly'] = true;
 	}
-	if (defined('_FULLTEXT_PDF_EXE')){
+	if (defined('_FULLTEXT_PDF_EXE')) {
 		$valeur['pdf_bin'] = _FULLTEXT_PDF_EXE;
-		$valeur['pdf_bin_readonly'] = true;	
+		$valeur['pdf_bin_readonly'] = true;
 	}
-	if (defined('_FULLTEXT_PDF_CMD_OPTIONS')){
+	if (defined('_FULLTEXT_PDF_CMD_OPTIONS')) {
 		$valeur['pdf_opt'] = _FULLTEXT_PDF_CMD_OPTIONS;
 		$valeur['pdf_opt_readonly'] = true;
 	}
-	if (defined('_FULLTEXT_PPT_EXE')){
+	if (defined('_FULLTEXT_PPT_EXE')) {
 		$valeur['ppt_bin'] = _FULLTEXT_PPT_EXE;
-		$valeur['ppt_bin_readonly'] = true;	
+		$valeur['ppt_bin_readonly'] = true;
 	}
-	if (defined('_FULLTEXT_PPT_CMD_OPTIONS')){
+	if (defined('_FULLTEXT_PPT_CMD_OPTIONS')) {
 		$valeur['ppt_opt'] = _FULLTEXT_PPT_CMD_OPTIONS;
 		$valeur['ppt_opt_readonly'] = true;
 	}
-	if (defined('_FULLTEXT_XLS_EXE')){
+	if (defined('_FULLTEXT_XLS_EXE')) {
 		$valeur['xls_bin'] = _FULLTEXT_XLS_EXE;
-		$valeur['xls_bin_readonly'] = true;	
+		$valeur['xls_bin_readonly'] = true;
 	}
-	if (defined('_FULLTEXT_XLS_CMD_OPTIONS')){
+	if (defined('_FULLTEXT_XLS_CMD_OPTIONS')) {
 		$valeur['xls_opt'] = _FULLTEXT_XLS_CMD_OPTIONS;
-		$valeur['xls_opt_readonly'] = true;	
+		$valeur['xls_opt_readonly'] = true;
 	}
 
 	return $valeur;
 }
 
 
-function formulaires_configurer_document_fulltext_verifier_dist(){
+function formulaires_configurer_document_fulltext_verifier_dist() {
 	$erreurs = array();
 	//Il faut au moins une seconde
-	if((!_request('intervalle_cron'))||(_request('intervalle_cron') < 1)){
+	if ((!_request('intervalle_cron'))||(_request('intervalle_cron') < 1)) {
 		$erreurs['intervalle_cron'] = _T('fulltext:erreur_intervalle_cron');
 	}
 	//Il faut au moins une documents a la fois
-	if((!_request('nb_docs'))||(_request('nb_docs') < 1)){
+	if ((!_request('nb_docs'))||(_request('nb_docs') < 1)) {
 		$erreurs['nb_docs'] = _T('fulltext:erreur_nb_docs');
-	}	
+	}
 	//Il faut au moins indexer un caractere
-	if((!_request('taille_index'))||(_request('taille_index') < 1)){
+	if ((!_request('taille_index'))||(_request('taille_index') < 1)) {
 		$erreurs['taille_index'] = _T('fulltext:erreur_taille_index');
 	}
 
 	/**
 	 * On teste les binaires
 	 */
-	$binaires = array('pdf_bin' => array('pdf_index','_FULLTEXT_PDF_EXE'),'doc_bin' => array('doc_index','_FULLTEXT_DOC_EXE'),'ppt_bin' => array('ppt_index','_FULLTEXT_PPT_EXE'),'xls_bin' => array('xls_index','_FULLTEXT_XLS_EXE'));
-	foreach($binaires as $binaire => $index){
+	$binaires = array('pdf_bin' => array('pdf_index', '_FULLTEXT_PDF_EXE'), 'doc_bin' => array('doc_index','_FULLTEXT_DOC_EXE'), 'ppt_bin' => array('ppt_index','_FULLTEXT_PPT_EXE'), 'xls_bin' => array('xls_index','_FULLTEXT_XLS_EXE'));
+	foreach ($binaires as $binaire => $index) {
 		/**
 		 * On ne teste l'exécutable que si on index sinon ça ne sert à rien
 		 */
-		if(_request($index[0]) == 'on' && !defined($index[1])){
+		if (_request($index[0]) == 'on' && !defined($index[1])) {
 			/**
 			 * Pas de binaire => on doit en avoir un pour récupérer le contenu
 			 */
-			if(!_request($binaire)){
+			if (!_request($binaire)) {
 				$erreurs[$binaire] = _T('fulltext:erreur_pdf_bin');
-			}else{
+			} else {
 				/**
 				 * On teste avec la commande de base ...
 				 * Le code de retour normal doit être 0
 				 */
-				@exec(_request($binaire),$retour_bin,$retour_bin_int);
-				if($retour_bin_int != 0){
+				@exec(_request($binaire), $retour_bin, $retour_bin_int);
+				if ($retour_bin_int != 0) {
 					/**
 					 * Si cela retourne un mauvais code d'erreur
 					 * on teste avec l'option -V (catdoc et catppt)
 					 */
-					@exec(_request($binaire).' -V',$retour_bin,$retour_bin_int);
-					if($retour_bin_int != 0){
+					@exec(_request($binaire).' -V', $retour_bin, $retour_bin_int);
+					if ($retour_bin_int != 0) {
 						/**
-						 * Sinon on fait un test que le binaire est executable 
+						 * Sinon on fait un test que le binaire est executable
 						 * Cela nécessite un chemin complet du binaire
 						 */
-						@exec('test -x '._request($binaire),$retour_bin,$retour_bin_int);
-						if($retour_bin_int != 0){
+						@exec('test -x '._request($binaire), $retour_bin, $retour_bin_int);
+						if ($retour_bin_int != 0) {
 							$erreurs[$binaire] = _T('fulltext:erreur_binaire_indisponible');
 						}
 					}
@@ -128,50 +130,48 @@ function formulaires_configurer_document_fulltext_verifier_dist(){
 			}
 		}
 	}
-	
+
 	//TODO : verifier si on a bien une version PHP superieur a 5.2 avec option Zip si on indexe les odt,docx,xlsx,pptx
-	
-	if(count($erreurs) > 0){
+	if (count($erreurs) > 0) {
 		$erreurs['message_erreur'] = _T('fulltext:erreur_verifier_configuration');
 	}
 	return $erreurs;
 }
 
-function formulaires_configurer_document_fulltext_traiter_dist(){
+function formulaires_configurer_document_fulltext_traiter_dist() {
 	//Recuperation de la configuration et serialization
 	$fulltext = serialize(array(
 		'intervalle_cron' => intval(_request('intervalle_cron')),
 		'nb_docs' => intval(_request('nb_docs')),
 		'taille_index' => intval(_request('taille_index')),
-		
+
 		'pdf_index' => _request('pdf_index'),
 		'pdf_bin' => _request('pdf_bin'),
 		'pdf_opt' => _request('pdf_opt'),
-		
+
 		'odt_index' => _request('odt_index'),
-		
+
 		'doc_index' => _request('doc_index'),
 		'doc_bin' => _request('doc_bin'),
 		'doc_opt' => _request('doc_opt'),
-		
+
 		'docx_index' => _request('docx_index'),
-		
+
 		'ppt_index' => _request('ppt_index'),
 		'ppt_bin' => _request('ppt_bin'),
 		'ppt_opt' => _request('ppt_opt'),
-		
+
 		'pptx_index' => _request('pptx_index'),
-		
+
 		'xls_index' => _request('xls_index'),
 		'xls_bin' => _request('xls_bin'),
 		'xls_opt' => _request('xls_opt'),
-		
-		'xlsx_index' => _request('xlsx_index'),	
+
+		'xlsx_index' => _request('xlsx_index'),
 	));
 	//Insere ou update ?
-	ecrire_meta('fulltext',$fulltext);
+	ecrire_meta('fulltext', $fulltext);
 	$res = array('message_ok'=>_T('fulltext:message_ok_configuration'));
 	return $res;
-	
+
 }
-?>
