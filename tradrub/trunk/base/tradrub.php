a3248bfab42249c28f88cbfb7f0ef3e22b8f3641
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-05-10 21:16:45 +0000

    PSR SPIP
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@97685 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- tradrub/trunk/base/tradrub.php
+++ tradrub/trunk/base/tradrub.php
@@ -9,7 +9,9 @@
  *     Licence GPL (c) 2008-2010 Stephane Laurent (Bill), Matthieu Marcillaud
  */
 
-if (!defined("_ECRIRE_INC_VERSION")) return;
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
 /**
  * Ajouter id_trad à la table rubriques
@@ -20,13 +22,12 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
  * @return array
  *     Description des objets editoriaux
  */
-function tradrub_declarer_tables_objets_sql($tables){
+function tradrub_declarer_tables_objets_sql($tables) {
 	// Extension de la table rubriques
 	$tables['spip_rubriques']['field']['id_trad'] = "bigint(21) DEFAULT '0' NOT NULL";
-	$tables['spip_rubriques']['texte_definir_comme_traduction_objet'] = "tradrub:texte_definir_comme_traduction_rubrique";
+	$tables['spip_rubriques']['texte_definir_comme_traduction_objet'] = 'tradrub:texte_definir_comme_traduction_rubrique';
 	return $tables;
 }
-
 
 /**
  * Ajout lors de l'insertion d'une traduction de rubrique
@@ -43,21 +44,18 @@ function tradrub_pre_insertion($flux) {
 		if ($id_rubrique_source = _request('lier_trad')) {
 			$id_parent_trad = $flux['data']['id_parent'];
 
-			$row = sql_fetsel("lang, id_secteur", "spip_rubriques", "id_rubrique=$id_parent_trad");
+			$row = sql_fetsel('lang, id_secteur', 'spip_rubriques', 'id_rubrique='.intval($id_parent_trad));
 			$lang_rub = $row['lang'];
-
-
-			$lang = "";
+			$lang = '';
 			$choisie = 'non';
 
 			// La langue a la creation : si les liens de traduction sont autorises
 			// dans les rubriques, on essaie avec la langue de l'auteur,
 			// ou a defaut celle de la rubrique
 			// Sinon c'est la langue de la rubrique qui est choisie + heritee
-			if (in_array('spip_rubriques',explode(',',$GLOBALS['meta']['multi_objets']))) {
+			if (in_array('spip_rubriques', explode(',', $GLOBALS['meta']['multi_objets']))) {
 				lang_select($GLOBALS['visiteur_session']['lang']);
-				if (in_array($GLOBALS['spip_lang'],
-				explode(',', $GLOBALS['meta']['langues_multilingue']))) {
+				if (in_array($GLOBALS['spip_lang'], explode(',', $GLOBALS['meta']['langues_multilingue']))) {
 					$lang = $GLOBALS['spip_lang'];
 					$choisie = 'oui';
 				}
@@ -74,12 +72,8 @@ function tradrub_pre_insertion($flux) {
 			// ici on ignore changer_lang qui est poste en cas de trad,
 			// car l'heuristique du choix de la langue est pris en charge ici
 			// en fonction de la config du site et de la rubrique choisie
-			set_request("changer_lang");
+			set_request('changer_lang');
 		}
-
 	}
-
 	return $flux;
 }
-
-?>
