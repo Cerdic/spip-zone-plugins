03676416c4d4f84cf33166b680e5ffb9cbe96ac4
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-05-11 03:33:02 +0000

    [Salvatore] [source:_plugins_/porte_plume_enluminures_typographiques/trunk/lang/ paquet-typoenluminee] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@97694 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- porte_plume_enluminures_typographiques/trunk/lang/paquet-typoenluminee_fr.php
+++ porte_plume_enluminures_typographiques/trunk/lang/paquet-typoenluminee_fr.php
@@ -1,27 +1,20 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-typoenluminee
-// Langue: fr
-// Date: 08-01-2012 17:02:08
-// Items: 2
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/porte_plume_enluminures_typographiques/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// T
-	'typoenluminee_description' => 'Ce plugin ajoute des raccourcis typographiques et des boutons au porte plume. Par exemple :
--* Paragraphes :
--** {{Sous-titres}} <code>{{{**</code>Texte du titre<code>}}}</code>, le nombre d\'étoiles (*) variant de 2 à 5, la barre de raccourcis ne proposant que 2 et 3
+	// T
+	'typoenluminee_description' => 'Ce plugin ajoute des raccourcis typographiques et des boutons au porte plume. Par exemple :
+-* Paragraphes :
+-** {{Sous-titres}} <code>{{{**</code>Texte du titre<code>}}}</code>, le nombre d’étoiles (*) variant de 2 à 5, la barre de raccourcis ne proposant que 2 et 3
 -** {{Centrer}} <code>[|</code>Paragraphe centré<code>|]</code>
 -** {{Aligner à droite}} <code>[/</code>Paragraphe aligné à droite<code>/]</code>
 -** {{Encadrer}} <code>[(</code>Paragraphe à encadrer<code>)]</code>
--* Caractères :
+-* Caractères :
 -** {{Mise en évidence}} <code>[*</code>texte en évidence<code>*]</code>
 -** {{Mise en évidence variante}} <code>[**</code>texte en évidence (autre couleur)<code>*]</code>
 -** {{Mise en exposant}} {&lt;sup&gt;}texte en exposant{&lt;/sup&gt;}
@@ -29,5 +22,5 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 -** {{Biffé}} {&lt;del&gt;}texte biffé{&lt;/del&gt;}
 
 ',
-	'typoenluminee_slogan' => 'Plein de raccourcis pour la mise en forme !',
+	'typoenluminee_slogan' => 'Plein de raccourcis pour la mise en forme !'
 );
