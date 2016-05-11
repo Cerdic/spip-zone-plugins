3d2aa706674d85fa77ec22cfb4ec6cdddb540779
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-05-11 03:34:48 +0000

    [Salvatore] [source:_plugins_/sommaire/trunk/lang/ paquet-sommaire] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@97699 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- sommaire/trunk/lang/paquet-sommaire_fr.php
+++ sommaire/trunk/lang/paquet-sommaire_fr.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/sommaire/trunk/lang
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
@@ -10,5 +10,5 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// S
 	'sommaire_description' => 'Générer un sommaire automatique pour les articles.',
 	'sommaire_nom' => 'Sommaire automatique',
-	'sommaire_slogan' => 'Un sommaire pour vos articles',
+	'sommaire_slogan' => 'Un sommaire pour vos articles'
 );
