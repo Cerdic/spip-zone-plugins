b88f6175ce5677c17218331d3de4eea47ee61580
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-05-11 03:34:31 +0000

    [Salvatore] [source:_plugins_/sommaire/trunk/lang/ sommaire] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@97698 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- sommaire/trunk/lang/sommaire_fr.php
+++ sommaire/trunk/lang/sommaire_fr.php
@@ -1,14 +1,11 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/sommaire/trunk/lang
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-
-	// S
-	'sommaire_titre' => 'Sommaire automatique',
 
 	// C
 	'cfg_exemple' => 'Exemple',
@@ -16,19 +13,19 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'cfg_titre_parametrages' => 'Paramétrages',
 
 	// E
-	'explication_niveau_max' => 'Profondeur maximale des sommaires. Cette valeur peut être changée au cas par cas via l\'utilisation de la balise <code>#SOMMAIRE{#TEXTE,2}</code> dans les squelettes, ou <code>&lt;sommaire|niveau_max=2&gt;</code> dans le texte des articles.',
+	'explication_niveau_max' => 'Profondeur maximale des sommaires. Cette valeur peut être changée au cas par cas via l’utilisation de la balise <code>#SOMMAIRE{#TEXTE,2}</code> dans les squelettes, ou <code>&lt;sommaire|niveau_max=2&gt;</code> dans le texte des articles.',
+	'explication_numerotation_sommaire' => 'Le sommaire peut être présenté comme une liste à puces ou une liste numérotée.',
+	'explication_sommaire_automatique_off' => 'Le sommaire est inséré dans le squelette par le webmestre.',
+	'explication_sommaire_automatique_ondemand' => 'Le sommaire est inséré uniquement en présence du raccourci <tt>&lt;sommaire&gt;</tt> dans le texte des articles.',
 
 	// L
-	'label_sommaire_automatique' => 'Insertion du sommaire d\'article',
-	'label_sommaire_automatique_off' => 'Aucune insertion dans le texte des articles',
-	'explication_sommaire_automatique_off' => 'Le sommaire est inséré dans le squelette par le webmestre.',
-	'label_sommaire_automatique_ondemand' => 'Insertion dans le texte des articles à la demande',
-	'explication_sommaire_automatique_ondemand' => 'Le sommaire est inséré uniquement en présence du raccourci <tt>&lt;sommaire&gt;</tt> dans le texte des articles.',
-	'label_sommaire_automatique_on' => 'Insertion automatique sur tous les articles',
-	'explication_numerotation_sommaire' => 'Le sommaire peut être présenté comme une liste à puces ou une liste numérotée.',
+	'label_sommaire_automatique' => 'Insertion du sommaire d’article',
 	'label_sommaire_automatique_numerote' => 'Type de liste pour le sommaire',
 	'label_sommaire_automatique_numerote_off' => 'liste à puces',
 	'label_sommaire_automatique_numerote_on' => 'liste numérotée',
+	'label_sommaire_automatique_off' => 'Aucune insertion dans le texte des articles',
+	'label_sommaire_automatique_on' => 'Insertion automatique sur tous les articles',
+	'label_sommaire_automatique_ondemand' => 'Insertion dans le texte des articles à la demande',
 	'label_sommaire_niveau_max' => 'Profondeur',
 	'label_sommaire_niveau_max_1' => '1 niveau',
 	'label_sommaire_niveau_max_2' => '2 niveaux',
@@ -37,9 +34,11 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'label_sommaire_niveau_max_5' => '5 niveaux',
 	'label_sommaire_niveau_max_6' => '6 niveaux',
 
-	// T
-	'titre_page_configurer_sommaire' => 'Sommaire automatique',
+	// S
+	'sommaire_titre' => 'Sommaire automatique',
 
-	'titre_retour_sommaire' => 'Retour au sommaire',
+	// T
 	'titre_cadre_sommaire' => 'Sommaire',
+	'titre_page_configurer_sommaire' => 'Sommaire automatique',
+	'titre_retour_sommaire' => 'Retour au sommaire'
 );
