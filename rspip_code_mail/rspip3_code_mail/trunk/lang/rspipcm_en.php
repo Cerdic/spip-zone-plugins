cd745b5d5ffa360763e6bb11b2d74e1f42aae5ea
spip.franck@lien-d-amis.net <spip.franck@lien-d-amis.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-05-11 18:51:11 +0000

    Devrait résoudre les logs qui sont présent dans  http://files.spip.net/paquets/paquets.log.txt up de z
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@97722 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- rspip_code_mail/rspip3_code_mail/trunk/lang/rspipcm_en.php
+++ rspip_code_mail/rspip3_code_mail/trunk/lang/rspipcm_en.php
@@ -5,20 +5,20 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
    // PAS DE CHAINE VIDE. Laisser au moins un espace.
    
    //D
-   "dans_le_domaine" => "in the domain",
+   'dans_le_domaine' => 'in the domain',
 
    //E
    // Usage sample: "ecrire_a_post" => " and after it,", "ecrire_a_pre" => "Before ", will be written 
    // Before <a name> and after it, [...]
-   "ecrire_a_post" => " ",
-   "ecrire_a_pre" => "Write to",
-   // Pour javascript: pas d'entites html.
-   "entrez_resultat_addition" => "Antispam. How much is",
-   // Pour javascript: pas d'entites html.
-   "erreur_entrez_resultat_addition" => "Mistake! How much is",
+   'ecrire_a_post' => ' ',
+   'ecrire_a_pre' => 'Write to',
+   // Pour javascript: pas d’entites html.
+   'entrez_resultat_addition' => 'Antispam. How much is',
+   // Pour javascript: pas d’entites html.
+   'erreur_entrez_resultat_addition' => 'Mistake! How much is',
    
    // final
-   "pour_les_distraits" => "Pas de virgule a la fin du tableau"
+   'pour_les_distraits' => 'Pas de virgule a la fin du tableau'
 
    );
      
--- rspip_code_mail/rspip3_code_mail/trunk/lang/rspipcm_fr.php
+++ rspip_code_mail/rspip3_code_mail/trunk/lang/rspipcm_fr.php
@@ -5,20 +5,20 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
    // PAS DE CHAINE VIDE. Laisser au moins un espace.
 
    //D
-   "dans_le_domaine" => "dans le domaine",
+   'dans_le_domaine' => 'dans le domaine',
 
    //E
    // Exemple d'usage: "ecrire_a_post" => " et apres lui,", "ecrire_a_pre" => "Avant ", sera ecrit 
    // Avant <un nom> et apres lui, [...]
-   "ecrire_a_post" => " ",
-   "ecrire_a_pre" => "Ecrire &agrave;",
-   // Pour javascript: pas d'entites html.
-   "entrez_resultat_addition" => "Antispam. Combien vaut",
-   // Pour javascript: pas d'entites html.
-   "erreur_entrez_resultat_addition" => "Erreur! Combien vaut",
+   'ecrire_a_post' => ' ',
+   'ecrire_a_pre' => 'Ecrire &agrave;',
+   // Pour javascript: pas d’entites html.
+   'entrez_resultat_addition' => 'Antispam. Combien vaut',
+   // Pour javascript: pas d’entites html.
+   'erreur_entrez_resultat_addition' => 'Erreur! Combien vaut',
    
    // final
-   "pour_les_distraits" => "Pas de virgule a la fin du tableau"
+   'pour_les_distraits" => "Pas de virgule a la fin du tableau'
 
    );
      
