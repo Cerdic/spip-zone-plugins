25d6828290df6b21425d5fa6f28faf46691dde1a
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-05-12 03:33:00 +0000

    [Salvatore] [source:_plugins_/intertitres_hierarchises_et_table_matieres/trunk/lang/ itdm] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@97726 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- intertitres_hierarchises_et_table_matieres/trunk/lang/itdm_fr.php
+++ intertitres_hierarchises_et_table_matieres/trunk/lang/itdm_fr.php
@@ -1,10 +1,16 @@
 <?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/intertitres_hierarchises_et_table_matieres/trunk/lang
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-	'aller_directement_a_' => 'Aller directement &agrave; &laquo;&nbsp;@titre@&nbsp;&raquo;',
-	'title_lien_table_matieres' => 'Aller &agrave; la table des mati&egrave;res de l\'article',
-	'titre_table_matieres' => 'Table des mati&egrave;res'
+
+	// A
+	'aller_directement_a_' => 'Aller directement à « @titre@ »',
+
+	// T
+	'title_lien_table_matieres' => 'Aller à la table des matières de l’article',
+	'titre_table_matieres' => 'Table des matières'
 );
