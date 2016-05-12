c56f47d23d883af1feef8fd202338c43089453e5
p@henix.be <p@henix.be@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-05-12 15:22:44 +0000

    Commentaire
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@97744 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- villebe/trunk/action/editer_liens_villebe.php
+++ villebe/trunk/action/editer_liens_villebe.php
@@ -4,7 +4,15 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
 
-
+/**
+ * Lié une ville à un objet SPIP
+ *
+ * @param int $id_villebe
+ * @param mixed $objet
+ * @param mixed $id_objet
+ * @access public
+ * @return mixed
+ */
 function lier_villebe($id_villebe, $objet, $id_objet) {
 	include_spip('action/editer_liens');
 
