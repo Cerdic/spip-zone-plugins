c5243a66670bf65b27bc61b41a0eeca55ab30aeb
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-05-16 21:49:07 +0000

    - Ajouter le statut d'un utilisateur sur sa page pour en connaitre son niveau d'autorisation ; - On surcharge les chaines de langue des statuts auteurs pour ne plus avoir un metalangage propre à SPIP que tous les utilisateurs ne connaissent pas ; - Petite sécurité sur diagnostic_maj.html : un système de versioning peut être composé, exemple 'GNU arch', 'GNU RCS'… ; - Lien vers la documentation ; - Up de z.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@97812 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ info_sites/trunk/lang/spip_fr.php
@@ -0,0 +1,17 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://trac.rezo.net/spip/spip/ecrire/lang/
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	'info_statut_auteur' => 'Statut :',
+	'statut_webmestre' => 'Utilisateur de niveau 0',
+	'titre_image_administrateur' => 'Utilisateur de niveau 1',
+	'titre_image_auteur_supprime' => 'Utilisateur supprimé',
+	'titre_image_redacteur_02' => 'Utilisateur de niveau 2',
+	'titre_image_visiteur' => 'Utilisateur de niveau 3',
+
+);
--- info_sites/trunk/squelettes/content/auteur_fonctions.php
+++ info_sites/trunk/squelettes/content/auteur_fonctions.php
@@ -1,4 +1,3 @@
 <?php
 
 include_spip('inc/puce_statut');
-
