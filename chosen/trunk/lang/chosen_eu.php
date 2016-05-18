3b4a685339d9b690f52a61399c89e79865518bd2
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-05-18 08:07:15 +0000

    Début de trad, pour la chaine la plus visible
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@97845 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ chosen/trunk/lang/chosen_eu.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de 
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// L
+	'lang_select_some_option' => 'Bilaketarako atal bat edo gehiago aukera'
+);
+
+?>
