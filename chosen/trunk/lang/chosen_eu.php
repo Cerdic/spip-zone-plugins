c230e4428bf64b70e9fb28c37c821cb1aab5855b
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-05-19 03:32:58 +0000

    [Salvatore] [source:_plugins_/chosen/trunk/lang/ chosen] Export depuis http://trad.spip.net
    La langue 'eu' devrait Ãªtre supprimÃ©e car trop peu traduite (8.33 %)
    
    
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@97865 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- chosen/trunk/lang/chosen_eu.php
+++ chosen/trunk/lang/chosen_eu.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// extrait automatiquement de 
+// extrait automatiquement de http://trad.spip.net/tradlang_module/chosen?lang_cible=eu
 // ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) {
@@ -12,5 +12,3 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// L
 	'lang_select_some_option' => 'Bilaketarako atal bat edo gehiago aukera'
 );
-
-?>
