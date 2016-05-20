030844f1e6665526d4d5d1bec04b90490132e127
fil@rezo.net <fil@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-05-20 06:51:54 +0000

    wording
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@97905 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- linkcheck/trunk/lang/linkcheck_fr.php
+++ linkcheck/trunk/lang/linkcheck_fr.php
@@ -13,7 +13,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// C
 	'commencer_parcours' => 'Commencer la recherche',
-	'commencer_verification' => 'Les liens ont été trouvé, appuyer sur le bouton pour la vérification de ces liens',
+	'commencer_verification' => 'Les liens ont été trouvés, appuyer sur le bouton pour la vérification de ces liens',
 	'commencer_verification_liens' => 'Commencer la vérification',
 	'configuration_generale' => 'Configuration générale',
 	'configurer_linkcheck' => 'Configurer LinkCheck',
@@ -56,7 +56,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// M
 	'mail_notification1' => 'Bonjour,<br/>
-							Vous recevez ce mail car certains liens présents sur votre site posent problèmes.<br/>
+							Vous recevez ce mail car certains liens présents sur votre site posent problème.<br/>
 							Veuillez trouver ci-dessous, un récapitulatif : <br/>',
 	'mail_notification2' => 'Veuillez vous rendre sur l’administration de votre site afin de résoudre ces problèmes ! Merci.',
 	'maj' => 'Mis à jour ',
@@ -72,14 +72,14 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// P
 	'parcours_incomplete' => 'Le parcours des liens du site est incomplet ou n’a pas encore été effectué !',
-	'pas_encore_de_liens' => 'Pour lancer la vérification des liens, il vous faut lancer la fonction qui va parcourir le site à la recherche de tous les liens. Suivant la taille de votre site internet, l’opération de recherche peut durer entre quelques secondes à quelques minutes. Si la page ne reponds pas, recharger simplement cette page.',
+	'pas_encore_de_liens' => 'Pour lancer la vérification des liens, il vous faut lancer la fonction qui va parcourir le site à la recherche de tous les liens. Suivant la taille de votre site internet, l’opération de recherche peut durer entre quelques secondes à quelques minutes. Si la page ne répond pas, recharger simplement cette page.',
 	'poursuivre_verification' => 'Tous les liens ne sont pas encore vérifiés, il vous faut poursuivre la vérification en cliquant sur ce bouton',
 	'poursuivre_verification_liens' => 'Poursuivre la vérification',
 
 	// R
 	'reinitialiser' => 'Réinitialiser',
 	'reinitialiser_la_base' => 'Vous souhaitez refaire toutes les opérations de recherche et de vérification des liens, vous pouvez le faire en appuyant sur le bouton "Réinitialiser". Cette action videra les tables spip_linkchecks et spip_linkchecks_liens',
-	'rescensement_incomplet' => 'Le rescensement des liens du site est incomplet ! ',
+	'rescensement_incomplet' => 'Le recensement des liens du site est incomplet ! ',
 
 	// T
 	'tester_liens_linkchecks' => 'Tester les liens de mon site',
