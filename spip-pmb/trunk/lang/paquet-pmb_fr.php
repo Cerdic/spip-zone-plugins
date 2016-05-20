721b092d119d9a79799189d03b8842bd758313a5
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-05-20 03:33:54 +0000

    [Salvatore] [source:_plugins_/spip-pmb/trunk/lang/ paquet-pmb] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@97903 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spip-pmb/trunk/lang/paquet-pmb_fr.php
+++ spip-pmb/trunk/lang/paquet-pmb_fr.php
@@ -1,26 +1,19 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-pmb
-// Langue: fr
-// Date: 06-12-2011 11:50:22
-// Items: 2
-
-if (!defined('_ECRIRE_INC_VERSION')) return;
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/spip-pmb/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// P
+	// P
 	'pmb_description' => 'Ce portail web pour [PMB->http://www.pmbservices.fr]
 	contient un squelette prêt à l’emploi compatible avec les thèmes ZPIP.
 	L’interface avec le catalogue pmb s’effectue par les webservices pmb disponibles
 	à partir de la version 3.3.
 
 	PMB est un Sigb (Système Intégré de Gestion de Bibliothèque) libre.',
-	
 	'pmb_nom' => 'SPIP-PMB',
-	'pmb_slogan' => 'Portail Web pour le logiciel PMB',
+	'pmb_slogan' => 'Portail Web pour le logiciel PMB'
 );
-?>
