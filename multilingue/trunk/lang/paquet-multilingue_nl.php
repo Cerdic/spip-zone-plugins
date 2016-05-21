732e4c524eced5f1d63578a904a6c492e349cac3
spip@allesfrans.com <spip@allesfrans.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-05-21 03:38:18 +0000

    [Salvatore] [source:_plugins_/multilingue/trunk/lang/ paquet-multilingue] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@97942 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ multilingue/trunk/lang/paquet-multilingue_nl.php
@@ -0,0 +1,31 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-multilingue?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// M
+	'multilingue_description' => 'Maak een meertalige site met een taal per hoofdrubriek.
+	
+	Deze plugin forceert de installatie van meerdere plugins
+	
+	- [Traduction entre rubriques->http://plugins.spip.net/tradrub.html]
+	- [Langue préférée par l’internaute ->http://contrib.spip.net/Plugin-Langue-preferee-par-l-internaute]
+	- [Traductions de rubriques autrement->http://contrib.spip.net/Traductions-de-rubriques-autrement-3]
+	- [Traductions d’articles autremen->http://contrib.spip.net/Traductions-d-articles-autrement-3]
+	
+	In Spip 2 of 2.1 wordt aanbevolen om eerst de plugin [Step->http://contrib.spip.net/STEP-Systeme-de-Telechargements-de-Plugins] te installeren voordat deze plugin wordt geactiveerd. Deze zal oo kde nodige plugins installeren.
+	
+	De plugin past enkele skeletten aan (van rubrieken en artikelen) alsmede enkele basisonderdelen.
+	
+	Zodra de nodige plugins zijn geactiveerd, leg je de talen van de site vast in de configuratie van de site (meertaligheid) en je site is meertalig
+	
+	',
+	'multilingue_nom' => 'Site Multilingue Facile',
+	'multilingue_slogan' => 'Maak eenvoudig een meertalige site'
+);
