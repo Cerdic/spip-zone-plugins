1c110ec394133d6f1492077f8cc015fe6aa50224
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-05-24 03:33:23 +0000

    [Salvatore] [source:_plugins_/rezosocios/trunk/lang/ rezosocios] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@97985 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- rezosocios/trunk/lang/rezosocios_fr.php
+++ rezosocios/trunk/lang/rezosocios_fr.php
@@ -1,10 +1,12 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/rezosocios/trunk/lang
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+
 	// C
 	'cfg_titre_rezosocios' => 'Configuration de "Réseaux sociaux"',
 
@@ -24,23 +26,23 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'info_url_site' => 'Lien du profil',
 
 	// L
-	'label_activer_rezosocio_objets' => 'Activer les liens de réseaux sociaux pour les contenus :',
+	'label_activer_rezosocio_objets' => 'Activer les liens de réseaux sociaux pour les contenus :',
 	'lien_ajouter_rezosocio' => 'Ajouter ce lien de réseau social',
 
 	// R
 	'rezosocio' => 'Réseau social',
-	'rezosocios' => 'Réseaux sociaux',
-	'rezosocio_editer' => 'Modifier ce lien de réseau social',
-	'rezosocio_logo' => 'Logo du lien de réseau social',
 	'rezosocio_ajouter' => 'Ajouter un lien de réseau social',
 	'rezosocio_aucun' => 'Aucun lien de réseau social',
 	'rezosocio_creer_associer' => 'Créer et associer un lien de réseau social',
+	'rezosocio_editer' => 'Modifier ce lien de réseau social',
+	'rezosocio_logo' => 'Logo du lien de réseau social',
 	'rezosocio_un' => '1 lien de réseau social',
+	'rezosocios' => 'Réseaux sociaux',
 	'rezosocios_nb' => '@nb@ liens de réseaux sociaux',
 
 	// T
 	'texte_nouveau_rezosocio' => 'Nouveau lien de réseau social',
-	'texte_rezosocio_statut' => 'Ce lien de réseau social est :',
+	'texte_rezosocio_statut' => 'Ce lien de réseau social est :',
 	'titre_ajouter_un_rezosocio' => 'Ajouter un lien de réseau social',
 	'titre_objets_lies_rezosocio' => 'Objets liés à ce réseau social',
 	'titre_page_rezosocios_page' => 'Les liens de réseaux sociaux',
