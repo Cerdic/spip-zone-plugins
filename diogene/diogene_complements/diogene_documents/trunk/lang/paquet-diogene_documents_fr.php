91213da33db3debf7d2e99951692e458f5d7086a
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-05-24 14:57:31 +0000

    Le fichier de langue du paquet.xml était absent
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@98007 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ diogene/diogene_complements/diogene_documents/trunk/lang/paquet-diogene_documents_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// D
+	'diogene_documents_description' => 'Ajoute la possibilité d\'ajouter des documents à un article depuis son propre formulaire.',
+	'diogene_documents_nom' => 'Diogene - Documents',
+	'diogene_documents_slogan' => 'Complément documents pour "Diogène"'
+
+);
