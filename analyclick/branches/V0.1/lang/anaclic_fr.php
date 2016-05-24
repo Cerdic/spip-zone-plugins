065f2e2d4a6efeec9eb70da7f30e70b7cf569128
bystrano@gmx.ch <bystrano@gmx.ch@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-05-24 09:28:44 +0000

    coquille dans les chaînes de langue
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@97990 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- analyclick/branches/V0.1/lang/anaclic_fr.php
+++ analyclick/branches/V0.1/lang/anaclic_fr.php
@@ -12,14 +12,14 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 'url_info'						=> 'La s&eacute;curisation des URLS ajoute une cl&eacute; (hash) propre au document pour éviter un accès direct aux document via son num&eacute;ro.',
 'url_check'						=> 's&eacute;curiser les URLS',
 'delais'						=> 'D&eacute;lais entre deux t&eacute;l&eacute;chargements',
-'info_afficher'					=> 'Afficher les statistiques de :', 
+'info_afficher'					=> 'Afficher les statistiques de :',
 'info_telechargement_30'		=> 'Afficher les statistiques de t&eacute;l&eacute;chargement <b>sur les 30 derniers jours</b> :',
 'info_telechargement'			=> 'Afficher les statistiques de t&eacute;l&eacute;chargement <b>depuis le d&eacute;but</b> :',
 
 'titre_evolution_stat'			=> '&Eacute;volution des t&eacute;l&eacute;chargements',
 'stat_journalieres'				=> 'Nombre de t&eacute;l&eacute;chargements par jour',
 'stat_mensuelles'				=> 'Nombre de t&eacute;l&eacute;chargements par mois',
-'stat_pour'						=> 'Satatitiques de t&eacute;l&eacute;chargements pour :',
+'stat_pour'						=> 'Statistiques de t&eacute;l&eacute;chargements pour :',
 'statistiques_doc'				=> 't&eacute;l&eacute;chargements',
 
 'no_statistique'				=> 'Pas de statistique disponible'
