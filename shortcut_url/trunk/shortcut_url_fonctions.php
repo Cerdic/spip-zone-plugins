8cbc7e7c0d0b01c1cdf2d6f7333c80e8ea48120e
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-05-24 16:36:00 +0000

    Déplacer geoip dans un répertoire lib/, autres petites modifs
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@98013 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- shortcut_url/trunk/shortcut_url_fonctions.php
+++ shortcut_url/trunk/shortcut_url_fonctions.php
@@ -15,8 +15,8 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 
 /**
  * Générer la chaine de caractères.
- * 
- * @param string $length taille de la chaîne de caractère 
+ *
+ * @param string $length taille de la chaîne de caractère
  *
  * @return string
  */
@@ -27,8 +27,8 @@ function generer_chaine($length) {
 /**
  * Créer les titres pour les liens raccourcis
  * On doit retrouver une valeur numérique dans le titre pour eviter les gros mots.
- * 
- * @param string $length taille de la chaîne de caractère 
+ *
+ * @param string $length taille de la chaîne de caractère
  *
  * @return string
  */
@@ -43,7 +43,7 @@ function generer_chaine_aleatoire($length = 5) {
 
 /**
  * Stocker les visites des clicks en séparant les humains des bots.
- * 
+ *
  * @param int $id_shortcut_url id de l'URL raccourcis
  *
  * @return bool
@@ -51,18 +51,18 @@ function generer_chaine_aleatoire($length = 5) {
 function shortcut_compteur($id_shortcut_url) {
 	if (!defined('_IS_BOT')) {
 		define(
-						'_IS_BOT',
-						isset($_SERVER['HTTP_USER_AGENT'])
-						and preg_match(
-										// mots generiques
-										',bot|slurp|crawler|spider|webvac|yandex|'
-										// MSIE 6.0 est un botnet 99,9% du temps, on traite donc ce USER_AGENT comme un bot
-										.'MSIE 6\.0|'
-										// UA plus cibles
-										.'80legs|accoona|AltaVista|ASPSeek|Baidu|Charlotte|EC2LinkFinder|eStyle|Google|Genieo|INA dlweb|InfegyAtlas|Java VM|LiteFinder|Lycos|Rambler|Scooter|ScrubbyBloglines|Yahoo|Yeti'
-										.',i',
-										(string) $_SERVER['HTTP_USER_AGENT']
-						)
+			'_IS_BOT',
+			isset($_SERVER['HTTP_USER_AGENT'])
+			and preg_match(
+				// mots generiques
+				',bot|slurp|crawler|spider|webvac|yandex|'
+				// MSIE 6.0 est un botnet 99,9% du temps, on traite donc ce USER_AGENT comme un bot
+				.'MSIE 6\.0|'
+				// UA plus cibles
+				.'80legs|accoona|AltaVista|ASPSeek|Baidu|Charlotte|EC2LinkFinder|eStyle|Google|Genieo|INA dlweb|InfegyAtlas|Java VM|LiteFinder|Lycos|Rambler|Scooter|ScrubbyBloglines|Yahoo|Yeti'
+				.',i',
+				(string) $_SERVER['HTTP_USER_AGENT']
+			)
 		);
 	}
 
@@ -82,14 +82,14 @@ function shortcut_compteur($id_shortcut_url) {
 
 		$country_code = get_geoip($referrer);
 		$click = $shorturl['click'] + 1;
-		$insert_click = sql_updateq('spip_shortcut_urls', array('click' => $click), 'id_shortcut_url='.intval($id_shortcut_url));
+		sql_updateq('spip_shortcut_urls', array('click' => $click), 'id_shortcut_url='.intval($id_shortcut_url));
 
 		if (_IS_BOT) {
 			$humain = 'bot';
-			$insert_bot = sql_insertq('spip_shortcut_urls_bots', array('id_shortcut_url' => $id_shortcut_url, 'date_modif' => $date_modif, 'referrer' => $referrer, 'user_agent' => $user_agent, 'ip_address' => $ip_address));
+			sql_insertq('spip_shortcut_urls_bots', array('id_shortcut_url' => $id_shortcut_url, 'date_modif' => $date_modif, 'referrer' => $referrer, 'user_agent' => $user_agent, 'ip_address' => $ip_address));
 		} else {
 			$humain = 'oui';
-			$insert = sql_insertq('spip_shortcut_urls_logs', array('id_shortcut_url' => $id_shortcut_url, 'date_modif' => $date_modif, 'shorturl' => $shorturl['url'], 'referrer' => $referrer, 'user_agent' => $user_agent, 'ip_address' => $ip_address, 'country_code' => $country_code, 'humain' => $humain));
+			sql_insertq('spip_shortcut_urls_logs', array('id_shortcut_url' => $id_shortcut_url, 'date_modif' => $date_modif, 'shorturl' => $shorturl['url'], 'referrer' => $referrer, 'user_agent' => $user_agent, 'ip_address' => $ip_address, 'country_code' => $country_code, 'humain' => $humain));
 		}
 	}
 
@@ -98,7 +98,7 @@ function shortcut_compteur($id_shortcut_url) {
 
 /**
  * Récupérer le user agent de l'utilisateur.
- * 
+ *
  * @return string
  */
 function get_user_agent() {
@@ -114,7 +114,7 @@ function get_user_agent() {
 
 /**
  * Récupérer le le code pays de l'utilisateur.
- * 
+ *
  * @param int $ip ip de l'utilisateur
  *
  * @return string
@@ -122,14 +122,13 @@ function get_user_agent() {
 function get_geoip($ip = null) {
 	static $gi = null;
 	if (is_null($ip)) {
-		include_spip('geoip/geoip');
+		include_spip('lib/geoip/geoip');
 		geoip_close($gi);
-
 		return;
 	}
 	if (is_null($gi)) {
-		include_spip('geoip/geoip');
-		$gi = geoip_open(find_in_path('geoip/GeoIP.dat'), GEOIP_STANDARD);
+		include_spip('lib/geoip/geoip');
+		$gi = geoip_open(find_in_path('lib/geoip/GeoIP.dat'), GEOIP_STANDARD);
 	}
 
 	return geoip_country_code_by_addr($gi, $ip);
