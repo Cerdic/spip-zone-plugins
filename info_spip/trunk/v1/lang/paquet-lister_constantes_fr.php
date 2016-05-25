126e38e57aeccf1b1106bff0786d35fdba0b53de
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-05-25 11:59:06 +0000

    Mise à jour du plugin 'Lister les constantes PHP de SPIP' en v1.1.0
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@98032 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ info_spip/trunk/v1/lang/paquet-lister_constantes_fr.php
@@ -0,0 +1,14 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined('_ECRIRE_INC_VERSION')) {
+    return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+    // L
+    'lister_constantes_description' => 'Ce plugin permet de lister constantes PHP de SPIP',
+    'lister_constantes_nom' => 'Lister les constantes PHP de SPIP',
+    'lister_constantes_slogan' => 'Le support de votre application.',
+);
