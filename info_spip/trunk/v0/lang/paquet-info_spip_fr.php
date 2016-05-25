ad284e9a2d88b1b563edcf5d5f5ce89bce1a9939
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-05-25 11:58:06 +0000

    Mise à jour du plugin 'Info SPIP' en v0.9.7
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@98029 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ info_spip/trunk/v0/lang/paquet-info_spip_fr.php
@@ -0,0 +1,19 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// P
+	'info_spip_description' => 'Ce plugin ne fait qu\'afficher la configuration du site sur lequel il est actif.
+Il faut renseigner une clé dans la page de configuration pour que la page de config puisse être active (web service).
+On affiche les informations équivalentes au plugin "Sites pour projets" avec qui il va de pair.
+Ce plugin est toutefois autonome.',
+	'info_spip_nom' => 'Info SPIP',
+	'info_spip_slogan' => 'Garder un oeil sur votre serveur.',
+);
+
+?>
