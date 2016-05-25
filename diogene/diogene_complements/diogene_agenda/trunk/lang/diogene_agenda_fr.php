885aae86288733101bd4465f4b5e614da5263352
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-05-25 03:32:55 +0000

    [Salvatore] [source:_plugins_/diogene/diogene_complements/diogene_agenda/trunk/lang/ diogene_agenda] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@98019 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- diogene/diogene_complements/diogene_agenda/trunk/lang/diogene_agenda_fr.php
+++ diogene/diogene_complements/diogene_agenda/trunk/lang/diogene_agenda_fr.php
@@ -1,21 +1,23 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/diogene/diogene_complements/diogene_agenda/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+
 	// E
-	'evenement_supprimer' => 'Supprimer l\'événement',
+	'evenement_supprimer' => 'Supprimer l’événement',
 
 	// F
 	'form_legend_agenda' => 'Événement',
 
 	// L
-	'label_agenda_caches' => 'Champs de l\'agenda à ne pas afficher',
+	'label_agenda_caches' => 'Champs de l’agenda à ne pas afficher',
 	'label_agenda_legende' => 'Légende de la partie "événement" du formulaire',
 	'label_agenda_multiple' => 'Permettre de lier plusieurs événements à un article',
-	'label_agenda_obligatoire' => 'L\'événement est obligatoire',
+	'label_agenda_obligatoire' => 'L’événement est obligatoire',
 	'label_cfg_ajout_agenda' => 'Événement',
 
 	// T
