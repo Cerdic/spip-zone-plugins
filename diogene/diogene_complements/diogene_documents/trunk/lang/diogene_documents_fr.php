d0883e2610f8ce133a9924d2d1a878cd368e2ea7
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-05-25 03:33:29 +0000

    [Salvatore] [source:_plugins_/diogene/diogene_complements/diogene_documents/trunk/lang/ diogene_documents] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@98021 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- diogene/diogene_complements/diogene_documents/trunk/lang/diogene_documents_fr.php
+++ diogene/diogene_complements/diogene_documents/trunk/lang/diogene_documents_fr.php
@@ -1,19 +1,21 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// extrait automatiquement de http://trad.spip.net/tradlang_module/diogene_geo?lang_cible=en
-// ** ne pas modifier le fichier **
-
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/diogene/diogene_complements/diogene_documents/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// E
 	'explication_documents_un_par_un' => 'Afficher autant de bouton parcourir que le nombre maximal de documents (si supérieur à 1).',
+
+	// L
+	'label_ajouter_nouveaux_documents' => 'Ajouter des documents',
 	'label_champs_documents' => 'Champs associés aux documents',
 	'label_documents_un_par_un' => 'Téléverser les documents un par un',
-	'label_ajouter_nouveaux_documents' => 'Ajouter des documents',
 	'label_editer_documents_articles' => 'Éditer les documents liés',
-	'label_nombre_documents' => 'Nombre de document maximal (0 pour l\'infini)',
-	'lien_supprimer_document' => 'Supprimer ce document',
+	'label_nombre_documents' => 'Nombre de document maximal (0 pour l’infini)',
 	'legende_documents' => 'Documents',
+	'lien_supprimer_document' => 'Supprimer ce document'
 );
