ed6baadef76bb2d4280056edb1a0f8bce420ab32
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-05-25 03:33:14 +0000

    [Salvatore] [source:_plugins_/diogene/diogene_complements/diogene_agenda/trunk/lang/ paquet-diogene_agenda] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@98020 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- diogene/diogene_complements/diogene_agenda/trunk/lang/paquet-diogene_agenda_fr.php
+++ diogene/diogene_complements/diogene_agenda/trunk/lang/paquet-diogene_agenda_fr.php
@@ -1,5 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/diogene/diogene_complements/diogene_agenda/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
@@ -7,8 +8,7 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// D
-	'diogene_agenda_description' => 'Ajoute la possibilité d\'ajouter un évènement à un article depuis son propre formulaire.',
+	'diogene_agenda_description' => 'Ajoute la possibilité d’ajouter un évènement à un article depuis son propre formulaire.',
 	'diogene_agenda_nom' => 'Diogène - Agenda',
 	'diogene_agenda_slogan' => 'Complément agenda pour "Diogène"'
-
 );
