70e2c06c29f8d93180d64fe883f77238cbb453c7
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-05-25 12:01:15 +0000

    Mise à jour du plugin 'minibando' en v1.3.8
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@98037 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ info_spip/trunk/v1/formulaires/administration_fonctions.php
@@ -0,0 +1,15 @@
+<?php
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+include_spip('inc/bandeau');
+
+function minibando_definir_rubrique_contexte($contexte) {
+	if (!isset($contexte['id_rubrique']) && isset($contexte['id_article']) && $contexte['id_article']) {
+		$contexte['id_rubrique'] = sql_getfetsel('id_rubrique', 'spip_articles', 'id_article=' . (int)$contexte['id_article']);
+	}
+
+	return $contexte;
+}
