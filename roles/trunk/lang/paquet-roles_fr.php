06daf03a4633426dc8c3237a31015e7d50502d8d
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-05-25 03:34:27 +0000

    [Salvatore] [source:_plugins_/roles/trunk/lang/ paquet-roles] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@98024 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- roles/trunk/lang/paquet-roles_fr.php
+++ roles/trunk/lang/paquet-roles_fr.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/roles/trunk/lang
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
@@ -8,7 +8,7 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// R
-	'roles_description' => 'Ce plugin sert d\'API pour définir des rôles sur des tables de liaisons.',
+	'roles_description' => 'Ce plugin sert d’API pour définir des rôles sur des tables de liaisons.',
 	'roles_nom' => 'Rôles',
-	'roles_slogan' => 'Typer des liaisons',
+	'roles_slogan' => 'Typer des liaisons'
 );
