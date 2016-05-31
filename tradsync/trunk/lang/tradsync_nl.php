6e988b79c212d3442f6a2fe5b2b861759474201b
spip@allesfrans.com <spip@allesfrans.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-05-31 03:36:25 +0000

    [Salvatore] [source:_plugins_/tradsync/trunk/lang/ tradsync] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@98130 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ tradsync/trunk/lang/tradsync_nl.php
@@ -0,0 +1,116 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/tradsync?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'articles_mots_differents' => 'Artikelen',
+	'articles_non_traduits' => 'Artikelen',
+	'articles_originaux_modifies' => 'Artikelen',
+
+	// C
+	'comparer_arborescence' => 'Vergelijk de hiërarchie',
+	'configurer_tradsync' => 'Hulp bij vertaling',
+	'configurer_tradsync_description' => 'Configureer de diverse elementen om de geleverde hulp te verfijnen.',
+
+	// E
+	'editer_ce_mot' => 'Pas dit trefwoord aan',
+	'editer_cet_article' => 'Pas dit artikel aan',
+	'editer_cette_rubrique' => 'Pas deze rubriek aan',
+	'espace_traducteurs' => 'Vertaalzone',
+	'explication_comparer_arborescence' => '
+		Twee navigatievelden naast elkaar. Deze worden gebruikt om visueel
+		de hiërarchie van de twee structuren te vergelijken.
+	',
+	'explication_espace_traducteurs' => '
+		De vertaalzone biedt hulp aan de taak vertalingen te zoeken
+		die gemaakt of aangepast moeten worden.
+	',
+	'explication_groupe_mots_multi' => '
+		Kies de trefwoordengroep die zal worden gebruikt om alle vertaalde woorden te selecteren
+		met &lt;multi&gt; bakens in de titels. Zo is het mogelijk te controleren of een
+		vertaling is gemaakt voor trefwoorden in die groep.
+	',
+	'explication_langue' => 'Selecteer de te gebruiken taal voor het controleren
+		van de status van vertalingen.',
+	'explication_traductions_a_realiser' => '
+		Dit gebied toont alle artikelen, rubrieken en trefwoorden
+		die nog niet lijken te zijn vertaald.
+	',
+	'explication_traductions_a_verifier' => '
+		Dit gebied toont alle artikelen en rubrieken met het 
+		originele artikel dat na vertaling moet worden aangepast.
+	',
+	'explication_traductions_de_mots' => '
+		Toon de trefwoorden die nog niet lijken te zijn vertaald.
+	',
+	'explication_traductions_mots_differents' => '
+		Toon de vertaalde objecten die niet exact hetzelfde aantal
+		toegewezen trefwoorden hebben als het originele object.
+	',
+	'explication_traductions_mots_synchroniser' => '
+		De «synchronisatie» kopieert alle trefwoorden van get originele element
+		naar het vertaalde element. Let er wel op dat eventuele extra toegewezen
+		trefwoorden niet van het object worden verwijderd.
+	',
+
+	// I
+	'info_actions' => 'Acties',
+	'info_revisions' => 'Revisies',
+	'info_source' => 'Bron',
+	'info_statut' => 'Status',
+	'info_statut_court' => 'S',
+	'info_titre' => 'Titel',
+	'info_type' => 'Groep',
+
+	// L
+	'label_groupe_mots_multi' => 'Multi trefwoordengroepen',
+	'label_langue' => 'Doeltaal',
+	'langue' => 'Taal',
+	'langue_court' => 'T',
+	'langue_en_cours' => 'De momenteel gekozen taal is: <strong>@langue@</strong>.',
+	'liste_des_lieux' => 'Te onderzoeken gebieden',
+
+	// M
+	'mots_cles_differents' => 'Van het origineel afwijkende trefwoorden',
+	'mots_cles_non_traduits' => 'Trefwoorden',
+	'mots_non_traduits' => 'Niet-vertaalde trefwoorden',
+
+	// N
+	'navigateurs' => 'Navigators',
+	'non_traduit' => 'Onvertaald of niet gedeclareerde vertalingen',
+
+	// O
+	'origine_modifiee' => 'Aangepast origineel',
+
+	// R
+	'rubriques_mots_differents' => 'Rubrieken',
+	'rubriques_non_traduites' => 'Rubrieken',
+	'rubriques_originales_modifiees' => 'Rubrieken',
+
+	// S
+	'second_navigateur' => 'Tweede navigator',
+	'selection_de_la_langue_cible' => 'Keuze doeltaal',
+	'synchroniser' => 'Synchronisatie',
+	'synchroniser_tout' => 'Synchroniseer alles',
+
+	// T
+	'traductions_a_realiser' => 'Te maken vertalingen',
+	'traductions_a_verifier' => 'Te controleren vertalingen',
+	'traductions_de_mots' => 'Vertaling van trefwoorden',
+	'traductions_mots_differents' => 'Synchroniseer trefwoorden',
+
+	// V
+	'voir_article_source' => 'Toon het bronartikel',
+	'voir_les_revisions_de_la_source' => 'Toon de revisies van de bron',
+	'voir_rubrique_source' => 'Toon de bronrubriek',
+
+	// Z
+	'zbug_critere_necessite_parametre' => 'De voorwaarde {@critere@} vereist een parameter'
+);
