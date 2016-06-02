dc3bd24c72f53b6e2fab20854f05681d3b8f0631
rporto@calepino.com <rporto@calepino.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-06-02 03:33:37 +0000

    [Salvatore] [source:_plugins_/crayons/lang/ crayons] Export depuis http://trad.spip.net de la langue pt_br
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@98167 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- crayons/lang/crayons_pt_br.php
+++ crayons/lang/crayons_pt_br.php
@@ -10,7 +10,12 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
+	'absence_cle_primaire' => 'Sem chave primária!',
+	'activation_barre_impossible' => 'Um outro plugin impede o uso da barra de formatação na área pública.',
 	'annuler' => 'Cancelar',
+
+	// B
+	'bouton_supprimer' => 'Excluir',
 
 	// D
 	'deja_modifie' => 'Já alterado',
@@ -20,12 +25,45 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'editer' => 'Editar',
 	'editer_@type@_@id@' => 'Editar @type@ @id@ completamente',
 	'editer_tout' => 'Editar tudo',
-	'explication_exec_autorise' => 'Pages autorisées à la modification
-		(?exec=...) : (, pour séparer les exec).', # MODIF
-	'explication_reduire_logo' => 'Réduire le logo s’il dépasse
-		la taille maximale indiquee en pixels. Exemple : 100. (0 = jamais)', # MODIF
+	'effets' => 'Efeitos',
+	'explication_exec_autorise' => 'Páginas liberadas para modificação
+		(?exec=...) : (, para separar os exec).',
+	'explication_introduction_article' => 'A introdução de uma matéria exibe a descrição da matéria; se não existir, exibe os seus primeiros 600 caracteres (introdução mais texto).',
+	'explication_reduire_logo' => 'Reduzir o logo se ultrapassar o tamanho máximo indicado em pixels. Exemplo: 100.  (0 = jamais)',
+
+	// I
+	'info_experimental' => 'Experimental',
+
+	// L
+	'label_activer_barre_typo' => 'Ativar a barra de formatação nos crayons?',
+	'label_activer_crayons_prive' => 'Ativar crayons na área restrita.',
+	'label_barre_typo' => 'Barra de formatação',
+	'label_clickhide' => 'Um clique fora esconde os crayons',
+	'label_effets' => 'Escolha dos efeitos',
+	'label_espaceprive' => 'Crayons na área restrita?',
+	'label_exec_autorise' => 'Páginas liberadas',
+	'label_filet' => 'Linha',
+	'label_message' => 'Exibir uma mensagem',
+	'label_msg_abandon' => 'No cancelamento',
+	'label_msg_no_change' => 'Se não houve alteração',
+	'label_reduire_logo' => 'Reduzir os logos',
+	'label_upload_document' => 'Permitir a anexação de documentos por arrastar/soltar no crayon article.texte',
+	'label_yellow_fade' => 'Fade',
+	'legend_controleur_fichier_remplacer' => 'Substituir este documento "@extension@"',
+	'legend_controleur_hyperlien' => 'Hyperlink',
+	'legend_controleur_logo' => 'Escolher um logo:',
+	'legend_controleur_logo_remplacer' => 'substituir este logo',
+	'legend_controleur_qui' => 'Quem?',
+	'legend_controleur_vignette' => 'Escolher uma miniatura:',
+	'legend_controleur_vignette_remplacer' => 'Substituir esta miniatura',
+	'legend_documents' => 'Upload de documentos',
+	'legend_editer_prive' => 'Editar a área restrita (experimental)',
+	'legend_introduction_article' => 'Texto introdutório da matéria',
+	'legend_introduction_rubrique' => 'Texto introdutório da seção',
 
 	// M
+	'message_image_obligatoire' => 'É necessário escolher uma imagem',
+	'messages' => 'Mensages',
 	'modifie_par_ailleurs' => 'Alterado em outro lugar',
 
 	// N
@@ -39,6 +77,13 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// S
 	'sauvegarder' => 'Fazer cópia de segurança das alterações?',
 	'svp_copier_coller' => 'Desculpe, copie/cole as suas alterações e recomece',
+
+	// T
+	'titre_config_crayons' => 'Configurar os crayons',
+	'titre_crayons' => 'Crayons',
+
+	// U
+	'update_impossible' => 'A atualização falhou.',
 
 	// V
 	'veuillez_patienter' => 'Aguarde, por favor...'
