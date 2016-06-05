4da54ea552a41ff5e1f5fee66c77182667b9a09f
fil@rezo.net <fil@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-06-05 20:53:06 +0000

    ne jamais renvoye un cache mal signe, bug introduit en http://zone.spip.org/trac/spip-zone/changeset/71231 et commenté en http://core.spip.org/projects/spip/repository/revisions/23018
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@98210 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- memoization/trunk/public/cacher.php
+++ memoization/trunk/public/cacher.php
@@ -91,7 +91,7 @@ function cache_valide(&$page, $date) {
 
 	// controle de la signature
 	if ($page['sig'] !== cache_signature($page))
-		return _IS_BOT?-1:1;
+		return 1;
 
 	// #CACHE{n,statique} => on n'invalide pas avec derniere_modif
 	// cf. ecrire/public/balises.php, balise_CACHE_dist()
