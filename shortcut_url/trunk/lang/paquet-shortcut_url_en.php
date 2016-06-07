4be78efdfd646a12dfaef56c95817ffbffccf9bc
cyp@rouquin.me <cyp@rouquin.me@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-06-07 14:34:59 +0000

    Ajout du paquet pour shortcut_url
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@98240 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ shortcut_url/trunk/lang/paquet-shortcut_url_en.php
@@ -0,0 +1,12 @@
+<?php
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// S
+	'shortcut_url_description' => 'Shortcut URL for SPIP',
+	'shortcut_url_slogan' => 'Shortcut URL for SPIP',
+);
---
+++ shortcut_url/trunk/lang/paquet-shortcut_url_fr.php
@@ -0,0 +1,12 @@
+<?php
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// S
+	'shortcut_url_description' => 'Raccourcisseur d\'URL pour SPIP',
+	'shortcut_url_slogan' => 'Raccourcisseur d\'URL pour SPIP',
+);
