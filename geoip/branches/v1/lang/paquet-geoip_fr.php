0a554078b1070af9b9719b1a18ec1eada47688cb
cyp@rouquin.me <cyp@rouquin.me@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-06-07 14:33:06 +0000

    Ajout du paquet geoip
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@98238 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ geoip/branches/v1/lang/paquet-geoip_fr.php
@@ -0,0 +1,12 @@
+<?php
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// G
+	'geoip_description' => 'geoIP version1',
+	'geoip_slogan' => 'geoIP version1',
+);
