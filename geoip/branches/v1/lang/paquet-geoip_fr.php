31685fb883ecad65c4882da3ff45abcd77b415b6
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-06-08 03:33:28 +0000

    [Salvatore] [source:_plugins_/geoip/branches/v1/lang/ paquet-geoip] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@98242 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- geoip/branches/v1/lang/paquet-geoip_fr.php
+++ geoip/branches/v1/lang/paquet-geoip_fr.php
@@ -1,12 +1,13 @@
 <?php
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/geoip/branches/v1/lang/
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// G
+	// G
 	'geoip_description' => 'geoIP version1',
-	'geoip_slogan' => 'geoIP version1',
+	'geoip_slogan' => 'geoIP version1'
 );
