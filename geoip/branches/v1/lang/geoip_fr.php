853373b44e78f5cc3975681ecac8018970b53d76
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-06-08 03:33:05 +0000

    [Salvatore] [source:_plugins_/geoip/branches/v1/lang/ geoip] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@98241 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- geoip/branches/v1/lang/geoip_fr.php
+++ geoip/branches/v1/lang/geoip_fr.php
@@ -1,15 +1,15 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_core_/plugins/monitor/lang/
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/geoip/branches/v1/lang/
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-	
+
 	// I
-	'ip_code_pays' => 'L\'ip @ip@ a le code pays :',
+	'ip_code_pays' => 'L’ip @ip@ a le code pays :',
 
-	'titre_geoip' => 'Test de GeoIP',
-
+	// T
+	'titre_geoip' => 'Test de GeoIP'
 );
