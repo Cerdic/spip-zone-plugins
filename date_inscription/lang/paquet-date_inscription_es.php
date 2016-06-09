e663259e50933a250921b1675f61c2f3b1be38da
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-06-09 09:31:07 +0000

    un paquet.xml et une icone
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@98269 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ date_inscription/lang/paquet-date_inscription_es.php
@@ -0,0 +1,20 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+// Fichier produit par PlugOnet
+// Module: paquet-date_inscription
+// Langue: es
+// Date: 09-06-2016 11:23:34
+// Items: 3
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// D
+	'date_inscription_description' => 'Un nuevo campo en la table de autores: date_inscription (fecha de inscripción)',
+	'date_inscription_nom' => 'Fecha de inscripción',
+	'date_inscription_slogan' => 'Una fecha de inscripción para los autores',
+);
+?>
---
+++ date_inscription/lang/paquet-date_inscription_fr.php
@@ -0,0 +1,20 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+// Fichier produit par PlugOnet
+// Module: paquet-date_inscription
+// Langue: fr
+// Date: 09-06-2016 11:23:34
+// Items: 3
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// D
+	'date_inscription_description' => 'Un champ supplémentaire pour la table auteurs : date_inscription',
+	'date_inscription_nom' => 'Date d\'inscription',
+	'date_inscription_slogan' => 'Une date d\'inscription pour les auteurs',
+);
+?>
