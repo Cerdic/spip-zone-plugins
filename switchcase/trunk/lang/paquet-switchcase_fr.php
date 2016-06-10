768028de8e03a0240ee32e446e16b7d49bdb2c9e
jluc@no-log.org <jluc@no-log.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-06-10 22:29:07 +0000

    le secret d'une page plugins.spip bien renseignée
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@98317 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ switchcase/trunk/lang/paquet-switchcase_fr.php
@@ -0,0 +1,14 @@
+<?php
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/switchcase/trunk/lang/
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'switchcase_nom' => 'Balises #SWITCH et #CASE',
+	'switchcase_slogan' => 'Les balises #SWITCH, #CASE ET #DEFAULT implémentent les contrôles switch... case... default... du php et du C'
+);
