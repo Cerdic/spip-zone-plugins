0fb361e2a8a264a3cb9c887198de90a2a133628a
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-06-12 18:20:08 +0000

    Création du tag de la v1.1.1
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@98368 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ medias_dereferencer/tags/1.1.1/lang/paquet-medias_dereferencer_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// M
+	'medias_dereferencer_description' => 'Il arrive que pour des raisons de flux éditorial, des documents soient liés à des articles, des rubriques ou tout autre objet éditorial. Ces objets sont publiés ou dépubliés selon les besoins des rédacteurs. Mais les documents restent accessibles aux moteurs de recherches. Ce plugin permet de changer le statut des documents qui ne sont plus utilisés par les objets éditoriaux. Il créera aussi une liaison entre le document et l\'objet qui l\'utilise en raccourcis typographiques.',
+	'medias_dereferencer_nom' => 'Déréférencer les médias',
+	'medias_dereferencer_slogan' => 'Laisser mes documents tranquilles!',
+);
+
+?>
---
+++ medias_dereferencer/tags/1.1.1/medias_dereferencer_autorisations.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Définit les autorisations du plugin Déréférencer les médias
+ *
+ * @plugin     Déréférencer les médias
+ * @copyright  2015
+ * @author     Teddy Payet
+ * @licence    GNU/GPL
+ * @package    SPIP\Medias_dereferencer\Autorisations
+ */
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+
+/**
+ * Fonction d'appel pour le pipeline
+ * @pipeline autoriser */
+function medias_dereferencer_autoriser(){}
+
+
+
+
+?>
