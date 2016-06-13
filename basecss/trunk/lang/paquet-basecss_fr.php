8cf6da9b92de6fc620ffa2da3df8ffe7b0fb3df5
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-06-13 14:07:58 +0000

    Normalisons les fichiers (xml, structure des formulaires, repertoires de prive/) pour SPIP 3.0+ puisque cette branche ne porte plus la compat 2.x
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@98383 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ basecss/trunk/lang/paquet-basecss_fr.php
@@ -0,0 +1,19 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+// Fichier produit par PlugOnet
+// Module: paquet-basecss
+// Langue: fr
+// Date: 13-06-2016 15:57:19
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'basecss_description' => 'Ce framework CSS pour SPIP fournit une base de travail pour l\'intégration, sous la forme d\'un jeu de feuilles de style dynamiques et paramétrables, proposant les préréglages fondamentaux.',
+	'basecss_slogan' => 'Framework CSS pour SPIP',
+);
+?>
