ff4742ce37abb01a98a2cb56c053a9d5a5629554
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-06-14 14:12:50 +0000

    Indentation
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@98401 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- shortcut_url/trunk/shortcut_url_fonctions.php
+++ shortcut_url/trunk/shortcut_url_fonctions.php
@@ -51,18 +51,18 @@ function generer_chaine_aleatoire($length = 5) {
 function shortcut_compteur($id_shortcut_url) {
 	if (!defined('_IS_BOT')) {
 		define(
-						'_IS_BOT',
-						isset($_SERVER['HTTP_USER_AGENT'])
-						and preg_match(
-										// mots generiques
-										',bot|slurp|crawler|spider|webvac|yandex|'
-										// MSIE 6.0 est un botnet 99,9% du temps, on traite donc ce USER_AGENT comme un bot
-										.'MSIE 6\.0|'
-										// UA plus cibles
-										.'80legs|accoona|AltaVista|ASPSeek|Baidu|Charlotte|EC2LinkFinder|eStyle|Google|Genieo|INA dlweb|InfegyAtlas|Java VM|LiteFinder|Lycos|Rambler|Scooter|ScrubbyBloglines|Yahoo|Yeti'
-										.',i',
-										(string) $_SERVER['HTTP_USER_AGENT']
-						)
+			'_IS_BOT',
+			isset($_SERVER['HTTP_USER_AGENT'])
+			and preg_match(
+							// mots generiques
+							',bot|slurp|crawler|spider|webvac|yandex|'
+							// MSIE 6.0 est un botnet 99,9% du temps, on traite donc ce USER_AGENT comme un bot
+							.'MSIE 6\.0|'
+							// UA plus cibles
+							.'80legs|accoona|AltaVista|ASPSeek|Baidu|Charlotte|EC2LinkFinder|eStyle|Google|Genieo|INA dlweb|InfegyAtlas|Java VM|LiteFinder|Lycos|Rambler|Scooter|ScrubbyBloglines|Yahoo|Yeti'
+							.',i',
+							(string) $_SERVER['HTTP_USER_AGENT']
+			)
 		);
 	}
 
