2b7a8cf0beb506d8d03dbf72e27f345772ee1dbd
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-06-15 12:41:12 +0000

    Pas besoin du cartel
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@98424 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- shortcut_url/trunk/action/api_shortcut_url.php
+++ shortcut_url/trunk/action/api_shortcut_url.php
@@ -1,22 +1,12 @@
 <?php
 
-/***************************************************************************\
- *  SPIP, Systeme de publication pour l'internet                           *
- *                                                                         *
- *  Copyright (c) 2001-2014                                                *
- *  Arnaud Martin, Antoine Pitrou, Philippe Riviere, Emmanuel Saint-James  *
- *                                                                         *
- *  Ce programme est un logiciel libre distribue sous licence GNU/GPL.     *
- *  Pour plus de details voir le fichier COPYING.txt ou l'aide en ligne.   *
-\***************************************************************************/
-
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
 
 /**
  * Acces aux shortcut_url
- * 
+ *
  * URLs de la forme :
  * shortcut_url.api/id_shortcut_url
  *
