53be9cb931895a24dafa134df9f60b015b9be438
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-06-17 08:43:31 +0000

    PSR et ne pas se trimballer de fichiers inutiles
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@98456 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- lister_logos/trunk/lang/lister_logos_fr.php
+++ lister_logos/trunk/lang/lister_logos_fr.php
@@ -2,42 +2,40 @@
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
 
 if (!defined('_ECRIRE_INC_VERSION')) {
-    return;
+	return;
 }
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-    // C
-    'cfg_exemple' => 'Exemple',
-    'cfg_exemple_explication' => 'Explication de cet exemple',
-    'cfg_titre_parametrages' => 'Paramétrages',
-    'controle_max_height' => 'Trop hauts',
-    'controle_max_height_explication' => 'Les logos ne doivent pas faire plus de @nb@ pixels de hauteur. Cette valeur a été configurée par le webmestre du site. Veuillez réduire leur hauteur le cas échéant, s\'il vous plaît.',
-    'controle_max_size' => 'Trop lourds',
-    'controle_max_size_explication' => 'Les logos ne doivent pas peser plus de @nb@ ko. Cette valeur a été configurée par le webmestre du site. Veuillez réduire le poids de ces logos le cas échéant, s\'il vous plaît.',
-    'controle_max_width' => 'Trop larges',
-    'controle_max_width_explication' => 'Les logos ne doivent pas faire plus de @nb@ pixels de largeur. Cette valeur a été configurée par le webmestre du site. Veuillez réduire leur largeur le cas échéant, s\'il vous plaît.',
+	// C
+	'cfg_exemple' => 'Exemple',
+	'cfg_exemple_explication' => 'Explication de cet exemple',
+	'cfg_titre_parametrages' => 'Paramétrages',
+	'controle_max_height' => 'Trop hauts',
+	'controle_max_height_explication' => 'Les logos ne doivent pas faire plus de @nb@ pixels de hauteur. Cette valeur a été configurée par le webmestre du site. Veuillez réduire leur hauteur le cas échéant, s\'il vous plaît.',
+	'controle_max_size' => 'Trop lourds',
+	'controle_max_size_explication' => 'Les logos ne doivent pas peser plus de @nb@ ko. Cette valeur a été configurée par le webmestre du site. Veuillez réduire le poids de ces logos le cas échéant, s\'il vous plaît.',
+	'controle_max_width' => 'Trop larges',
+	'controle_max_width_explication' => 'Les logos ne doivent pas faire plus de @nb@ pixels de largeur. Cette valeur a été configurée par le webmestre du site. Veuillez réduire leur largeur le cas échéant, s\'il vous plaît.',
 
-    // I
-    'info_logos' => 'Logos',
-    'info_1_logo' => 'Un logo',
-    'info_nb_logos' => '@nb@ logos',
+	// I
+	'info_logos' => 'Logos',
+	'info_1_logo' => 'Un logo',
+	'info_nb_logos' => '@nb@ logos',
 
-    // L
-    'lister_logos_titre' => 'Lister les logos',
-    'logo_off' => 'Logo de survol',
-    'logo_on' => 'Logo normal',
+	// L
+	'lister_logos_titre' => 'Lister les logos',
+	'logo_off' => 'Logo de survol',
+	'logo_on' => 'Logo normal',
 
-    // O
-    'objet_supprime' => 'Objet supprimé&nbsp;?',
-    'objets_label' => 'Objets éditoriaux&nbsp;:',
+	// O
+	'objet_supprime' => 'Objet supprimé&nbsp;?',
+	'objets_label' => 'Objets éditoriaux&nbsp;:',
 
-    // P
-    'pas_de_logos' => 'Il n\'y a pas de logos pour cet objet éditorial.',
+	// P
+	'pas_de_logos' => 'Il n\'y a pas de logos pour cet objet éditorial.',
 
-    // T
-    'titre_page' => 'Les logos',
-    'titre_page_configurer_lister_logos' => 'Les logos, on en fait quoi ?',
+	// T
+	'titre_page' => 'Les logos',
+	'titre_page_configurer_lister_logos' => 'Les logos, on en fait quoi ?',
 );
-
-?>
--- lister_logos/trunk/lister_logos_options.php
+++ lister_logos/trunk/lister_logos_options.php
@@ -9,13 +9,15 @@
  * @package    SPIP\Lister_logos\Options
  */
 
-if (!defined('_ECRIRE_INC_VERSION')) return;
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
-if ( !defined('_LOGO_MAX_SIZE') and ini_get('upload_max_filesize') ) {
-    // Si on a `upload_max_filesize` renseigné dans le fichier php.ini,
-    // autant s'en servir pour la constante.
-    $upload_max_filesize = floatval(ini_get('upload_max_filesize')) * 1024;
-    define('_LOGO_MAX_SIZE', $upload_max_filesize);
+if (!defined('_LOGO_MAX_SIZE') and ini_get('upload_max_filesize')) {
+	// Si on a `upload_max_filesize` renseigné dans le fichier php.ini,
+	// autant s'en servir pour la constante.
+	$upload_max_filesize = floatval(ini_get('upload_max_filesize')) * 1024;
+	define('_LOGO_MAX_SIZE', $upload_max_filesize);
 }
 
 if (!defined('_LOGO_MAX_WIDTH')) {
@@ -24,4 +26,3 @@ if (!defined('_LOGO_MAX_WIDTH')) {
 if (!defined('_LOGO_MAX_HEIGHT')) {
 	define('_LOGO_MAX_HEIGHT', '');
 }
-?>
