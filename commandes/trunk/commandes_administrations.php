1c71cc45c7cdbea47430d3484aaddc58fb2be35a
gornety@no-log.org <gornety@no-log.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-06-20 18:42:28 +0000

    4 décimales pour taxe de la table spip_commandes_details pour accepter la tva à taux réduit pour DOM de 1,05%
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@98497 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- commandes/trunk/commandes_administrations.php
+++ commandes/trunk/commandes_administrations.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Fichier gérant l'installation et désinstallation du plugin Commandes
  *
@@ -8,9 +9,9 @@
  * @licence    GPL 3
  * @package    SPIP\Commandes\Installation
  */
-
 // Sécurité
-if (!defined('_ECRIRE_INC_VERSION')) return;
+if (!defined('_ECRIRE_INC_VERSION'))
+    return;
 
 /**
  * Fonction d'installation et de mise à jour du plugin Commandes.
@@ -23,103 +24,108 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
  * @param string $version_cible
  *     Version du schéma de données dans ce plugin (déclaré dans paquet.xml)
  * @return void
-**/
+ * */
 function commandes_upgrade($nom_meta_base_version, $version_cible) {
-	include_spip('commandes_fonctions');
-	include_spip('inc/config');
-	$maj = array();
+    include_spip('commandes_fonctions');
+    include_spip('inc/config');
+    $maj = array();
 
-	$config = lire_config('commandes');
-	if (!is_array($config)) {
-		$config = array();
-	}
-	
-	$id_webmestre = commandes_id_premier_webmestre();
-	
-	$config = array_merge(array(
-		'duree_vie'                 => '1',
-		'activer'                   => '',
-		'quand'                     => array_keys(commandes_lister_statuts()),
-		'expediteur'                => 'webmaster',
-		'expediteur_webmaster'      => $id_webmestre,
-		'expediteur_administrateur' => '',
-		'expediteur_email'          => '',
-		'vendeur'                   => 'webmaster',
-		'vendeur_webmaster'         => $id_webmestre,
-		'vendeur_administrateur'    => '',
-		'vendeur_email'             => '',
-		'client'                    => 'on'
-	), $config);
+    $config = lire_config('commandes');
+    if (!is_array($config)) {
+        $config = array();
+    }
 
-	$maj['create'] = array(
-		array(
-			'maj_tables', array('spip_commandes','spip_commandes_details', 'spip_commandes_liens'),
-		),
-		array(
-			'ecrire_config', 'commandes', $config
-		),
-	);
-	
-	$maj['0.2']  = array(
-		array('maj_tables', array('spip_commandes_details'))
-	);
-	
-	$maj['0.3']  = array(
-		'ecrire_config','commandes', array('duree_vie'=>3600)
-	);
-	
-	$maj['0.4']  = array(
-		array('sql_alter', 'TABLE spip_commandes ADD mode varchar(25) not null default ""')
-	);
-	
-	$maj['0.5.0'] = array(
-		array(
-			'sql_updateq',
-			'spip_commandes_details',
-			array('statut' => 'attente'),
-			array(
-				'statut = ""',
-				'id_commande IN (select id_commande from spip_commandes where statut in ("encours","attente","paye","partiel","erreur"))',
-			),
-		),
-		array(
-			'sql_updateq',
-			'spip_commandes_details',
-			array('statut' => 'envoye'),
-			array(
-				'statut = ""',
-				'id_commande IN (select id_commande from spip_commandes where statut="envoye")',
-			),
-		),
-		array(
-			'sql_updateq',
-			'spip_commandes_details',
-			array('statut' => 'retour'),
-			array(
-				'statut = ""',
-				'id_commande IN (select id_commande from spip_commandes where statut in ("retour","retour_partiel"))',
-			)
-		),
-	);
-	
-	// Ajouter une table de liens pour les commandes
-	$maj['0.6.0'] = array(
-		array('maj_tables', array('spip_commandes_liens')),
-	);
-	
-	// Ajouter des champs (bank_uid, echeances_type, echeances) pour gérer d'éventuels renouvellements bancaires automatiques
-	$maj['0.7.0'] = array(
-		array('maj_tables', array('spip_commandes')),
-	);
+    $id_webmestre = commandes_id_premier_webmestre();
 
-	// ajout du champ source
-	$maj['0.7.1']  = array(
-		array('maj_tables', array('spip_commandes'))
-	);
+    $config = array_merge(array(
+        'duree_vie' => '1',
+        'activer' => '',
+        'quand' => array_keys(commandes_lister_statuts()),
+        'expediteur' => 'webmaster',
+        'expediteur_webmaster' => $id_webmestre,
+        'expediteur_administrateur' => '',
+        'expediteur_email' => '',
+        'vendeur' => 'webmaster',
+        'vendeur_webmaster' => $id_webmestre,
+        'vendeur_administrateur' => '',
+        'vendeur_email' => '',
+        'client' => 'on'
+            ), $config);
 
+    $maj['create'] = array(
+        array(
+            'maj_tables', array('spip_commandes', 'spip_commandes_details', 'spip_commandes_liens'),
+        ),
+        array(
+            'ecrire_config', 'commandes', $config
+        ),
+    );
 
-	include_spip('base/upgrade');
-	maj_plugin($nom_meta_base_version, $version_cible, $maj);
+    $maj['0.2'] = array(
+        array('maj_tables', array('spip_commandes_details'))
+    );
+
+    $maj['0.3'] = array(
+        'ecrire_config', 'commandes', array('duree_vie' => 3600)
+    );
+
+    $maj['0.4'] = array(
+        array('sql_alter', 'TABLE spip_commandes ADD mode varchar(25) not null default ""')
+    );
+
+    $maj['0.5.0'] = array(
+        array(
+            'sql_updateq',
+            'spip_commandes_details',
+            array('statut' => 'attente'),
+            array(
+                'statut = ""',
+                'id_commande IN (select id_commande from spip_commandes where statut in ("encours","attente","paye","partiel","erreur"))',
+            ),
+        ),
+        array(
+            'sql_updateq',
+            'spip_commandes_details',
+            array('statut' => 'envoye'),
+            array(
+                'statut = ""',
+                'id_commande IN (select id_commande from spip_commandes where statut="envoye")',
+            ),
+        ),
+        array(
+            'sql_updateq',
+            'spip_commandes_details',
+            array('statut' => 'retour'),
+            array(
+                'statut = ""',
+                'id_commande IN (select id_commande from spip_commandes where statut in ("retour","retour_partiel"))',
+            )
+        ),
+    );
+
+    // Ajouter une table de liens pour les commandes
+    $maj['0.6.0'] = array(
+        array('maj_tables', array('spip_commandes_liens')),
+    );
+
+    // Ajouter des champs (bank_uid, echeances_type, echeances) pour gérer d'éventuels renouvellements bancaires automatiques
+    $maj['0.7.0'] = array(
+        array('maj_tables', array('spip_commandes')),
+    );
+
+    // ajout du champ source
+    $maj['0.7.1'] = array(
+        array('maj_tables', array('spip_commandes'))
+    );
+
+    // tva à taux réduit 1,05% pour les DOM, il faut 4 décimales pour le champ taxe
+    $maj['0.7.2'] = array(
+        array('maj_tables', array('spip_commandes')),
+        array('sql_alter', 'TABLE spip_commandes_details CHANGE taxe taxe DECIMAL(4,4) NULL DEFAULT NULL')
+    );
+
+    include_spip('base/upgrade');
+    maj_plugin($nom_meta_base_version, $version_cible, $maj);
 }
 
 /**
@@ -128,15 +134,15 @@ function commandes_upgrade($nom_meta_base_version, $version_cible) {
  * @param string $nom_meta_base_version
  *     Nom de la meta informant de la version du schéma de données du plugin installé dans SPIP
  * @return void
-**/
+ * */
 function commandes_vider_tables($nom_meta_base_version) {
-	sql_drop_table("spip_commandes,spip_commandes_details");
+    sql_drop_table("spip_commandes,spip_commandes_details");
 
-	# Nettoyer les versionnages et forums
-	sql_delete("spip_versions",              sql_in("objet", array('commande')));
-	sql_delete("spip_versions_fragments",    sql_in("objet", array('commande')));
+    # Nettoyer les versionnages et forums
+    sql_delete("spip_versions", sql_in("objet", array('commande')));
+    sql_delete("spip_versions_fragments", sql_in("objet", array('commande')));
 
-	effacer_meta($nom_meta_base_version);
+    effacer_meta($nom_meta_base_version);
 }
 
 ?>
