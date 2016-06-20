9113f6c4bb5320a9f50a5d4e92afe8c7d1b2f9c2
gornety@no-log.org <gornety@no-log.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-06-20 18:54:02 +0000

    4 décimales pour taxe de la table spip_produits pour accepter la tva à taux réduit pour DOM de 1,05%
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@98498 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- produits/trunk/base/produits.php
+++ produits/trunk/base/produits.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Déclarations relatives à la base de données
  *
@@ -8,9 +9,8 @@
  * @licence    GNU/GPL
  * @package    SPIP\Produits\Pipelines
  */
-
-if (!defined('_ECRIRE_INC_VERSION')) return;
-
+if (!defined('_ECRIRE_INC_VERSION'))
+    return;
 
 /**
  * Déclaration des alias de tables et filtres automatiques de champs
@@ -23,17 +23,16 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
  */
 function produits_declarer_tables_interfaces($interfaces) {
 
-	$interfaces['table_des_tables']['produits'] = 'produits';
-	
-	// Champs date sur les tables
-	$interface['table_date']['produits'] = 'date';
-	
-	// Déclaration du titre
-	$interface['table_titre']['produits'] = 'titre, "" as lang';
+    $interfaces['table_des_tables']['produits'] = 'produits';
 
-	return $interfaces;
+    // Champs date sur les tables
+    $interface['table_date']['produits'] = 'date';
+
+    // Déclaration du titre
+    $interface['table_titre']['produits'] = 'titre, "" as lang';
+
+    return $interfaces;
 }
-
 
 /**
  * Déclaration des objets éditoriaux
@@ -46,68 +45,65 @@ function produits_declarer_tables_interfaces($interfaces) {
  */
 function produits_declarer_tables_objets_sql($tables) {
 
-	$tables['spip_produits'] = array(
-		'type' => 'produit',
-		'principale' => "oui",
-		'field'=> array(
-			"id_produit"         => "bigint(21) NOT NULL",
-			"id_rubrique"        => "bigint(21) NOT NULL DEFAULT 0", 
-			"id_secteur"         => "bigint(21) NOT NULL DEFAULT 0", 
-			"titre"              => "text NOT NULL",
-			"reference"          => "tinytext NOT NULL DEFAULT ''",
-			"descriptif"         => "text NOT NULL DEFAULT ''",
-			"texte"              => "longtext NOT NULL",
-			"prix_ht"            => "float not null default 0",
-			"taxe"               => "decimal(4,3) default null",
-			"statut"             => "varchar(20)  DEFAULT '0' NOT NULL", 
-			"lang"               => "VARCHAR(10) NOT NULL DEFAULT ''",
-			"date"               => "datetime NOT NULL DEFAULT '0000-00-00 00:00:00'", 
-			"date_com"           => "datetime NOT NULL DEFAULT '0000-00-00 00:00:00'",
-			"langue_choisie"     => "VARCHAR(3) DEFAULT 'non'", 
-			"id_trad"            => "bigint(21) NOT NULL DEFAULT 0",
-			"immateriel"         => "tinyint(1) NOT NULL DEFAULT 0",
-			"poids"              => "bigint(21) NOT NULL DEFAULT 0", // poids en g
-			"largeur"            => "bigint(21) NOT NULL DEFAULT 0", // largeur en cm
-			"longueur"           => "bigint(21) NOT NULL DEFAULT 0", // longueur en cm
-			"hauteur"            => "bigint(21) NOT NULL DEFAULT 0", // hauteur en cm
-			"maj"                => "TIMESTAMP"
-		),
-		'key' => array(
-			"PRIMARY KEY"        => "id_produit",
-			"KEY id_rubrique"    => "id_rubrique", 
-			"KEY id_secteur"     => "id_secteur", 
-			"KEY lang"           => "lang", 
-			"KEY id_trad"        => "id_trad", 
-			"KEY statut"         => "statut", 
-		),
-		'titre' => "titre AS titre, lang AS lang",
-		'date' => "date",
-		'champs_editables'  => array('titre', 'reference', 'prix_ht', 'taxe', 'descriptif', 'texte', 'immateriel', 'poids', 'largeur', 'longueur', 'hauteur'),
-		'champs_versionnes' => array('titre', 'reference', 'prix_ht', 'taxe', 'descriptif', 'texte'),
-		'rechercher_champs' => array("titre" => 4, "reference"=> 4 ,"descriptif" => 2, "texte" => 1),
-		'statut_textes_instituer' => array(
-			'prepa'    => 'texte_statut_en_cours_redaction',
-			'prop'     => 'texte_statut_propose_evaluation',
-			'publie'   => 'texte_statut_publie',
-			'refuse'   => 'texte_statut_refuse',
-			'poubelle' => 'texte_statut_poubelle',
-		),
-		'statut'=> array(
-			array(
-				'champ'     => 'statut',
-				'publie'    => 'publie',
-				'previsu'   => 'publie,prop,prepa',
-				'post_date' => 'date', 
-				'exception' => array('statut','tout')
-			)
-		),
-		'texte_changer_statut' => 'produits:produit_statut', 
-		
+    $tables['spip_produits'] = array(
+        'type' => 'produit',
+        'principale' => "oui",
+        'field' => array(
+            "id_produit" => "bigint(21) NOT NULL",
+            "id_rubrique" => "bigint(21) NOT NULL DEFAULT 0",
+            "id_secteur" => "bigint(21) NOT NULL DEFAULT 0",
+            "titre" => "text NOT NULL",
+            "reference" => "tinytext NOT NULL DEFAULT ''",
+            "descriptif" => "text NOT NULL DEFAULT ''",
+            "texte" => "longtext NOT NULL",
+            "prix_ht" => "float not null default 0",
+            "taxe" => "decimal(4,4) default null",
+            "statut" => "varchar(20)  DEFAULT '0' NOT NULL",
+            "lang" => "VARCHAR(10) NOT NULL DEFAULT ''",
+            "date" => "datetime NOT NULL DEFAULT '0000-00-00 00:00:00'",
+            "date_com" => "datetime NOT NULL DEFAULT '0000-00-00 00:00:00'",
+            "langue_choisie" => "VARCHAR(3) DEFAULT 'non'",
+            "id_trad" => "bigint(21) NOT NULL DEFAULT 0",
+            "immateriel" => "tinyint(1) NOT NULL DEFAULT 0",
+            "poids" => "bigint(21) NOT NULL DEFAULT 0", // poids en g
+            "largeur" => "bigint(21) NOT NULL DEFAULT 0", // largeur en cm
+            "longueur" => "bigint(21) NOT NULL DEFAULT 0", // longueur en cm
+            "hauteur" => "bigint(21) NOT NULL DEFAULT 0", // hauteur en cm
+            "maj" => "TIMESTAMP"
+        ),
+        'key' => array(
+            "PRIMARY KEY" => "id_produit",
+            "KEY id_rubrique" => "id_rubrique",
+            "KEY id_secteur" => "id_secteur",
+            "KEY lang" => "lang",
+            "KEY id_trad" => "id_trad",
+            "KEY statut" => "statut",
+        ),
+        'titre' => "titre AS titre, lang AS lang",
+        'date' => "date",
+        'champs_editables' => array('titre', 'reference', 'prix_ht', 'taxe', 'descriptif', 'texte', 'immateriel', 'poids', 'largeur', 'longueur', 'hauteur'),
+        'champs_versionnes' => array('titre', 'reference', 'prix_ht', 'taxe', 'descriptif', 'texte'),
+        'rechercher_champs' => array("titre" => 4, "reference" => 4, "descriptif" => 2, "texte" => 1),
+        'statut_textes_instituer' => array(
+            'prepa' => 'texte_statut_en_cours_redaction',
+            'prop' => 'texte_statut_propose_evaluation',
+            'publie' => 'texte_statut_publie',
+            'refuse' => 'texte_statut_refuse',
+            'poubelle' => 'texte_statut_poubelle',
+        ),
+        'statut' => array(
+            array(
+                'champ' => 'statut',
+                'publie' => 'publie',
+                'previsu' => 'publie,prop,prepa',
+                'post_date' => 'date',
+                'exception' => array('statut', 'tout')
+            )
+        ),
+        'texte_changer_statut' => 'produits:produit_statut',
+    );
 
-	);
-
-	return $tables;
+    return $tables;
 }
-
 
 ?>
--- produits/trunk/produits_administrations.php
+++ produits/trunk/produits_administrations.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Fichier gérant l'installation et désinstallation du plugin produits
  *
@@ -8,9 +9,8 @@
  * @licence    GNU/GPL
  * @package    SPIP\Produits\Installation
  */
-
-if (!defined('_ECRIRE_INC_VERSION')) return;
-
+if (!defined('_ECRIRE_INC_VERSION'))
+    return;
 
 /**
  * Fonction d'installation et de mise à jour du plugin produits.
@@ -20,23 +20,27 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
  * @param string $version_cible
  *     Version du schéma de données dans ce plugin (déclaré dans paquet.xml)
  * @return void
-**/
+ * */
 function produits_upgrade($nom_meta_base_version, $version_cible) {
-	$maj = array();
+    $maj = array();
 
-	$maj['create'] = array(
-		array('maj_tables', array('spip_produits'))
-	);
+    $maj['create'] = array(
+        array('maj_tables', array('spip_produits'))
+    );
 
-	$maj['1.1.0'] = array(
-		// ajout des champs immateriel/poids/longueur/largeur/hauteur
-		array('maj_tables', array('spip_produits')),
-	);
+    $maj['1.1.0'] = array(
+        // ajout des champs immateriel/poids/longueur/largeur/hauteur
+        array('maj_tables', array('spip_produits')),
+    );
+    $maj['1.1.1'] = array(
+        // ajout des champs immateriel/poids/longueur/largeur/hauteur
+        array('maj_tables', array('spip_produits')),
+        array('sql_alter', 'TABLE spip_produits CHANGE taxe taxe DECIMAL(4,4) NULL DEFAULT NULL')
+    );
 
-	include_spip('base/upgrade');
-	maj_plugin($nom_meta_base_version, $version_cible, $maj);
+    include_spip('base/upgrade');
+    maj_plugin($nom_meta_base_version, $version_cible, $maj);
 }
-
 
 /**
  * Fonction de désinstallation du plugin produits.
@@ -44,16 +48,16 @@ function produits_upgrade($nom_meta_base_version, $version_cible) {
  * @param string $nom_meta_base_version
  *     Nom de la meta informant de la version du schéma de données du plugin installé dans SPIP
  * @return void
-**/
+ * */
 function produits_vider_tables($nom_meta_base_version) {
 
-	sql_drop_table("spip_produits");
+    sql_drop_table("spip_produits");
 
-	# Nettoyer les versionnages et forums
-	sql_delete("spip_versions",              sql_in("objet", array('produit')));
-	sql_delete("spip_versions_fragments",    sql_in("objet", array('produit')));
+    # Nettoyer les versionnages et forums
+    sql_delete("spip_versions", sql_in("objet", array('produit')));
+    sql_delete("spip_versions_fragments", sql_in("objet", array('produit')));
 
-	effacer_meta($nom_meta_base_version);
+    effacer_meta($nom_meta_base_version);
 }
 
 ?>
