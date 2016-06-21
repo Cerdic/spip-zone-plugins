f733378abd0a476f44d8d827a42f47b87a07aec0
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-06-21 17:58:30 +0000

    sans log
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@98512 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- lister_logos/trunk/prive/objets/liste/logos_fonctions.php
+++ lister_logos/trunk/prive/objets/liste/logos_fonctions.php
@@ -15,7 +15,6 @@ function logo_on_existe($id_objet, $_id_objet) {
 	$chercher_logo = charger_fonction('chercher_logo', 'inc');
 	$logo = $chercher_logo($id_objet, $_id_objet);
 	if (count($logo) == 0) {
-		spip_log($logo, 'test.'._LOG_ERREUR);
 		return false;
 	}
 	return true;
