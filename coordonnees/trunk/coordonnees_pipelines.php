c4e9e3e4da9c6cd9da838e7275424e8fe5a8ffe4
arnaud.berard@mister-graphx.com <arnaud.berard@mister-graphx.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-06-22 09:38:41 +0000

    **formattage et PSR spip**
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@98523 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- coordonnees/trunk/coordonnees_pipelines.php
+++ coordonnees/trunk/coordonnees_pipelines.php
@@ -9,7 +9,8 @@
  * @package    SPIP\Coordonnees\Pipelines
  */
 
-if (!defined('_ECRIRE_INC_VERSION')) return;
+if (!defined('_ECRIRE_INC_VERSION')) { return;
+}
 
 /*
  * function coordonnees_ieconfig_metas
@@ -19,9 +20,9 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
  * @param $table
  */
 function coordonnees_ieconfig_metas($table) {
-    $table['coordonnees']['titre'] = _T('paquet-coordonnees:coordonnees_nom');
-    $table['coordonnees']['icone'] = 'prive/themes/spip/images/addressbook-16.png';
-    $table['coordonnees']['metas_serialize'] = 'contacts_et_organisations';
+	$table['coordonnees']['titre'] = _T('paquet-coordonnees:coordonnees_nom');
+	$table['coordonnees']['icone'] = 'prive/themes/spip/images/addressbook-16.png';
+	$table['coordonnees']['metas_serialize'] = 'contacts_et_organisations';
 	
 	return $table;
 }
@@ -30,26 +31,27 @@ function coordonnees_ieconfig_metas($table) {
  * sur la page de visualisation des objets associes
 **/
 function coordonnees_afficher_fiche_objet($flux) {
-	$texte = "";
+	$texte = '';
 	$exec = isset($flux['args']['exec']) ? $flux['args']['exec'] : _request('exec');
 	$e = trouver_objet_exec($exec);
 	$type = $flux['args']['type'];
 
 	$id_objet = $flux['args']['id'];
-	$editable = (in_array(table_objet_sql($type),lire_config('coordonnees/objets'))?1:0);
+	$editable = (in_array(table_objet_sql($type), lire_config('coordonnees/objets'))?1:0);
 	$has = false;
-	if (!$editable){
-		if (
-		     sql_countsel('spip_adresses_liens','objet='.sql_quote($type).' AND id_objet='.intval($id_objet))
-		  OR sql_countsel('spip_numeros_liens','objet='.sql_quote($type).' AND id_objet='.intval($id_objet))
-		  OR sql_countsel('spip_emails_liens','objet='.sql_quote($type).' AND id_objet='.intval($id_objet))
+	if (!$editable) {
+		if (sql_countsel('spip_adresses_liens', 'objet='.sql_quote($type).' AND id_objet='.intval($id_objet))
+		  or sql_countsel('spip_numeros_liens', 'objet='.sql_quote($type).' AND id_objet='.intval($id_objet))
+		  or sql_countsel('spip_emails_liens', 'objet='.sql_quote($type).' AND id_objet='.intval($id_objet))
 		) {
 			$has = true;
 		}
 	}
 
-	if (!$e['edition'] and ($editable OR $has)) {
-		$texte .= recuperer_fond('prive/squelettes/contenu/coordonnees_fiche_objet', array(
+	if (!$e['edition'] and ($editable or $has)) {
+		$texte .= recuperer_fond(
+			'prive/squelettes/contenu/coordonnees_fiche_objet',
+			array(
 			'objet' => $type,
 			'id_objet' => intval($flux['args']['id']),
 			'editable' => $editable,
@@ -59,10 +61,10 @@ function coordonnees_afficher_fiche_objet($flux) {
 	}
 
 	if ($texte) {
-		if ($p=strpos($flux['data'],"<!--afficher_fiche_objet-->"))
-			$flux['data'] = substr_replace($flux['data'],$texte,$p,0);
-		else
-			$flux['data'] .= $texte;
+		if ($p=strpos($flux['data'], '<!--afficher_fiche_objet-->')) {
+			$flux['data'] = substr_replace($flux['data'], $texte, $p, 0);
+		} else { 			$flux['data'] .= $texte;
+		}
 	}
 
 	return $flux;
@@ -74,16 +76,17 @@ function coordonnees_afficher_fiche_objet($flux) {
  * sur la page de visualisation des coordonnées
 **/
 function coordonnees_affiche_gauche($flux) {
-	$texte = "";
+	$texte = '';
 	$exec = isset($flux['args']['exec']) ? $flux['args']['exec'] : _request('exec');
 	$e = trouver_objet_exec($exec);
-	if (
-		!$e['edition']
+	if (!$e['edition']
 		and $type = $e['type']
-		and in_array($type,array('adresse','email','numero'))
+		and in_array($type, array('adresse','email','numero'))
 		and $id_coordonnee = $flux['args']["id_${type}"]
 	) {
-		$texte .= recuperer_fond("prive/squelettes/contenu/utilisations_${type}", array(
+		$texte .= recuperer_fond(
+			"prive/squelettes/contenu/utilisations_${type}",
+			array(
 			"id_${type}" => intval($id_coordonnee)
 			),
 			array('ajax'=>true)
@@ -105,10 +108,8 @@ function coordonnees_affiche_gauche($flux) {
  * @param int $n
  * @return int
  */
-function coordonnees_optimiser_base_disparus($flux){
+function coordonnees_optimiser_base_disparus($flux) {
 	include_spip('action/editer_liens');
-	$flux['data'] += objet_optimiser_liens(array('adresse'=>'*', 'numero'=>'*', 'email'=>'*'),'*');
+	$flux['data'] += objet_optimiser_liens(array('adresse'=>'*', 'numero'=>'*', 'email'=>'*'), '*');
 	return $flux;
 }
-
-
