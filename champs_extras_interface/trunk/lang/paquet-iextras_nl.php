c22f69a5ba5143e367cdbdcf49b27e9f603a15fd
spip@allesfrans.com <spip@allesfrans.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-06-22 03:33:15 +0000

    [Salvatore] [source:_plugins_/champs_extras_interface/trunk/lang/ paquet-iextras] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@98514 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ champs_extras_interface/trunk/lang/paquet-iextras_nl.php
@@ -0,0 +1,17 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-iextras?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// I
+	'iextras_description' => 'Voegt aan het privé gedeelte een volledige interface toe voor het beheer van additionele velden in de editoriale objecten van SPIP.',
+	'iextras_nom' => 'Extra Velden (Champs Extras Interface)',
+	'iextras_slogan' => 'Een grafische interface voor het beheer van extra velden',
+	'iextras_titre' => 'Extra Velden (Champs Extras Interface)'
+);
