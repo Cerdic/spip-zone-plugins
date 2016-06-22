9805c7d01313ca5b8c4e88e79e876b7f59124c75
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-06-22 17:46:41 +0000

    Rah voilà enfin les bonnes autorisations…
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@98531 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- api_syntaxe/trunk/api_syntaxe_autorisations.php
+++ api_syntaxe/trunk/api_syntaxe_autorisations.php
@@ -9,7 +9,7 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 function api_syntaxe_autoriser($flux) { return $flux; }
 
 // API de syntaxe SPIP seulement pour les comptes utilisateurices
-function autoriser_spip_syntaxe_post_collection_dist($faire, $quoi, $id, $qui, $options) {
+function autoriser_spipsyntaxe_post_collection_dist($faire, $quoi, $id, $qui, $options) {
 	include_spip('inc/session');
 	
 	if ($id_auteur = session_get('id_auteur') and $id_auteur > 0) {
@@ -17,4 +17,16 @@ function autoriser_spip_syntaxe_post_collection_dist($faire, $quoi, $id, $qui, $
 	}
 	
 	return false;
+}
+function autoriser_propre_post_collection_dist($faire, $quoi, $id, $qui, $options) {
+	return autoriser($faire, '_spip_syntaxe', $id, $qui, $options);
+}
+function autoriser_typo_post_collection_dist($faire, $quoi, $id, $qui, $options) {
+	return autoriser($faire, '_spip_syntaxe', $id, $qui, $options);
+}
+function autoriser_T_post_collection_dist($faire, $quoi, $id, $qui, $options) {
+	return autoriser($faire, '_spip_syntaxe', $id, $qui, $options);
+}
+function autoriser_T_ou_typo_post_collection_dist($faire, $quoi, $id, $qui, $options) {
+	return autoriser($faire, '_spip_syntaxe', $id, $qui, $options);
 }
