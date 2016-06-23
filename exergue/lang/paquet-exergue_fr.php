fd51a37f0b743dfa3aea8dbba245286b33ca03e0
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-06-23 21:36:46 +0000

    Séparer l'appel du JS de la CSS.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@98542 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ exergue/lang/paquet-exergue_fr.php
@@ -0,0 +1,13 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// D
+	'exergue_description' => 'Permet de reprendre un paragraphe d’un article pour le mettre en évidence. Il s’agit d’une phrase du texte que l’on fait ressortir dans la maquette, histoire de rythmer la page et de faire ressortir un point accrocheur. Le contenu est duppliqué en Javascript. On peut choisir la zone de duplication explicitement avec <code>[exergueNNN<-]</code> où NNN est le numéro d’ordre de l’exergue concernée.',
+	'exergue_nom' => 'Exergue',
+	'exergue_slogan' => 'Gérer des exergues : <code><exergue></exergue></code> et <code>[exergueNNN<-]</code>'
+);
