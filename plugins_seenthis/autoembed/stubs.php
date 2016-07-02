0bce5909accb377ad4631f635ebe80c094677a48
fil@rezo.net <fil@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-07-02 21:26:15 +0000

    il y avait encore du mixed-content dans les vignettes youtube
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@98646 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- plugins_seenthis/autoembed/stubs.php
+++ plugins_seenthis/autoembed/stubs.php
@@ -37,13 +37,13 @@
 $AutoEmbed_stubs = array(
   array(
     'title' => 'YouTube',
-    'website' => 'http://www.youtube.com',
+    'website' => 'https://www.youtube.com',
     'url-match' => 'http://(?:video\.google\.(?:com|com\.au|co\.uk|de|es|fr|it|nl|pl|ca|cn)/(?:[^"]*?))?(?:(?:www|au|br|ca|es|fr|de|hk|ie|in|il|it|jp|kr|mx|nl|nz|pl|ru|tw|uk)\.)?youtube\.com(?:[^"]*?)?(?:&|&amp;|/|\?|;|\%3F|\%2F)(?:video_id=|v(?:/|=|\%3D|\%2F))([0-9a-z-_]{11})',
-    'embed-src' => 'http://www.youtube.com/v/$2&rel=0&fs=1&hd=1',
+    'embed-src' => 'https://www.youtube.com/v/$2&rel=0&fs=1&hd=1',
     'embed-width' => '480',
     'embed-height' => '295',
-    'image-src' => 'http://img.youtube.com/vi/$2/0.jpg',
-    'iframe-player' => 'http://www.youtube.com/embed/$2?rel=0&amp;autoplay=1',
+    'image-src' => 'https://img.youtube.com/vi/$2/0.jpg',
+    'iframe-player' => 'https://www.youtube.com/embed/$2?rel=0&amp;autoplay=1',
   ),
   array(
     'title' => 'YouTube',
@@ -57,9 +57,9 @@ $AutoEmbed_stubs = array(
   ),
   array(
     'title' => 'YouTube (Playlists)',
-    'website' => 'http://www.youtube.com',
+    'website' => 'https://www.youtube.com',
     'url-match' => 'http://(?:(?:www|au|br|ca|es|fr|de|hk|ie|in|il|it|jp|kr|mx|nl|nz|pl|ru|tw|uk)\.)?youtube\.com(?:[^"]*?)?(?:&|&amp;|/|\?|;)(?:id=|p=|p/)([0-9a-f]{16})',
-    'embed-src' => 'http://www.youtube.com/p/$2&rel=0&fs=1',
+    'embed-src' => 'https://www.youtube.com/p/$2&rel=0&fs=1',
     'embed-width' => '480',
     'embed-height' => '385',
   ),
