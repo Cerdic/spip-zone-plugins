02f5b5b869fecc65c85201d6d684a3025127228c
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-07-03 08:09:31 +0000

    bug affichage icon enlever tabs au lieu d'espaces
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@98648 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- reservations_multiples/branches/v1/lang/reservations_multiples_en.php
+++ reservations_multiples/branches/v1/lang/reservations_multiples_en.php
@@ -1,29 +1,32 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/reservations_multiples?lang_cible=en
+// ** ne pas modifier le fichier **
 
-if (!defined('_ECRIRE_INC_VERSION')) return;
- 
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+
 	// C
 	'cfg_titre_parametrages' => 'Paramétrages',
-	
-	//L
-	'label_ajouter'=>'Add',	
-	'label_enlever'=>'Remove',		
-	'label_reserver_amie'=>'I would like to register one more person:',
-	
-	//M
-	'message_ok_reservations_pour'=>'Réservé pour : @noms@',
-	
-	//N
+
+	// L
+	'label_ajouter' => 'Add',
+	'label_enlever' => 'Remove',
+	'label_reserver_amie' => 'I would like to register one more person:',
+
+	// M
+	'message_ok_reservations_pour' => 'Réservé pour : @noms@',
+
+	// N
 	'nombre_limite' => 'Nombre limites',
-	'nombre_limite_explication' => 'Si vide aucune limitation',	
-	
+	'nombre_limite_explication' => 'Si vide aucune limitation',
+
 	// R
 	'reservations_multiples_titre' => 'Réservations multiples',
-	
-	// T
-	'titre_page_configurer_reservations_multiples' => 'Réservations multiples',
-);
 
-?>
+	// T
+	'titre_page_configurer_reservations_multiples' => 'Réservations multiples'
+);
--- reservations_multiples/branches/v1/lang/reservations_multiples_fr.php
+++ reservations_multiples/branches/v1/lang/reservations_multiples_fr.php
@@ -1,40 +1,39 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
-if (!defined('_ECRIRE_INC_VERSION')) return;
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/reservations_multiples/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// C
 	'cfg_titre_parametrages' => 'Paramétrages',
 
-  // E
-  'explication_multiple_inscriptions' => 'Pour chaque événement il es possible d\'indiquer le nombre de réservations',
-  'explication_multiple_personnes' => 'Pour chaque personne aditionnelle il est alors possible/nécessaire de renseigner ses détails personnels',
-  'explication_quantite' => 'Si vide, la quantité sera 1',
+	// E
+	'explication_multiple_inscriptions' => 'Pour chaque événement il es possible d’indiquer le nombre de réservations',
+	'explication_multiple_personnes' => 'Pour chaque personne aditionnelle il est alors possible/nécessaire de renseigner ses détails personnels',
+	'explication_quantite' => 'Si vide, la quantité sera 1',
 
-	//L
-	'label_ajouter'=>'Ajouter',
-	'label_enlever'=>'Enlever',
-	'label_multiple_inscriptions'=>'Permettre d\'indiquer le nombre d\'inscriptions',
-	'label_multiple_personnes'=>'Permettre l\'inscription de plusieurs personnes',
-	'label_reserver_amie'=>'J\'aimerai également inscrire une autre persone :',
+	// L
+	'label_ajouter' => 'Ajouter',
+	'label_enlever' => 'Enlever',
+	'label_multiple_inscriptions' => 'Permettre d’indiquer le nombre d’inscriptions',
+	'label_multiple_personnes' => 'Permettre l’inscription de plusieurs personnes',
+	'label_reserver_amie' => 'J’aimerai également inscrire une autre persone :',
 
-	//M
-	'message_ok_reservations_pour'=>'Réservé pour : @noms@',
+	// M
+	'message_ok_reservations_pour' => 'Réservé pour : @noms@',
 
-	//N
-	'nom_reservation_multiple_simple'=>'Réservation multiple simple',
+	// N
+	'nom_reservation_multiple_simple' => 'Réservation multiple simple',
 	'nombre_limite' => 'Nombre limites',
 	'nombre_limite_explication' => 'Si vide aucune limitation',
-
 
 	// R
 	'reservations_multiples_titre' => 'Réservations multiples',
 
 	// T
 	'titre_page_configurer_reservations_multiples' => 'Réservations multiples',
-	'type_lien_multiple_personnes' => 'Réservation multiple, reśervation d\'origine : @reference@',	
+	'type_lien_multiple_personnes' => 'Réservation multiple, reśervation d’origine : @reference@'
 );
-
-?>
