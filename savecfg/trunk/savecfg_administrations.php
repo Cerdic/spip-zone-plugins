045adac1b12b59ce6e956cc472503f78a79b258e
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-07-05 12:28:36 +0000

    C'est à se demander comment ça fonctionnait ;-)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@98680 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- savecfg/trunk/savecfg_administrations.php
+++ savecfg/trunk/savecfg_administrations.php
@@ -12,36 +12,19 @@ if (!defined('_ECRIRE_INC_VERSION')) {
     return;
 }
 
-/*
-function savecfg_install($action)
-{
-    switch ($action) {
-        case 'test':
-            return (isset($GLOBALS['meta']['savecfg_base_version']) and ($GLOBALS['meta']['savecfg_base_version'] >= $version_base));
-            break;
-        case 'install':
-            savecfg_upgrade('savecfg_base_version', 0.2);
-            break;
-        case 'uninstall':
-            savecfg_vider_tables('savecfg_base_version');
-            break;
-    }
-}
-*/
-
 function savecfg_upgrade($nom_meta_base_version, $version_cible)
 {
     include_spip('inc/meta');
-    $current_version = 0.0;
+    $current_version = '0.0.0';
     if ((!isset($GLOBALS['meta'][$nom_meta_base_version])) || (($current_version = $GLOBALS['meta'][$nom_meta_base_version]) != $version_cible)) {
-        if ($current_version == 0.0) {
+        if ($current_version == '0.0.0') {
             include_spip('base/create');
             include_spip('base/abstract_sql');
             include_spip('base/savecfg_install');
             creer_base();
             ecrire_meta($nom_meta_base_version, $version_cible, 'non');
         }
-        if (version_compare($current_version, '0.2', '<')) {
+        if (version_compare($current_version, '0.2.0', '<')) {
             sql_alter('TABLE `spip_savecfg` DROP `version`');
         }
         ecrire_metas();
