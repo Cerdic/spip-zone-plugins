5e9fd6b0c05274ac2a0330645774c29e898e1910
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-07-05 18:14:44 +0000

    PSR
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@98688 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- intranet/trunk/intranet_options.php
+++ intranet/trunk/intranet_options.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Plugin Intranet
- * 
+ *
  * (c) 2013 kent1
  * Distribue sous licence GPL
  *
@@ -11,56 +11,57 @@
  * Autoriser a voir le site en Intranet : par defaut toute personne identifiée
  * @return mixed
  */
-function autoriser_intranet_dist()
-{
-    $autoriser = false;
-    // On laisse en premier le cas du user connecté, cas le plus courant pour l'utilisation de ce plugin
-    if (isset($GLOBALS['visiteur_session']['id_auteur']) &&    $GLOBALS['visiteur_session']['id_auteur'] > 0    )
-    {
-        $autoriser = true;
-    } else {
-        /*cas intranet definit par une IP ou un range d'ip ...à faire : compatibilité IPV6 */
-        if (!function_exists('lire_config'))
-            include_spip('inc/config');
+function autoriser_intranet_dist() {
+	$autoriser = false;
+	// On laisse en premier le cas du user connecté, cas le plus courant pour l'utilisation de ce plugin
+	if (isset($GLOBALS['visiteur_session']['id_auteur']) && $GLOBALS['visiteur_session']['id_auteur'] > 0) {
+		$autoriser = true;
+	} else {
+		/*cas intranet definit par une IP ou un range d'ip ...à faire : compatibilité IPV6 */
+		if (!function_exists('lire_config')) {
+			include_spip('inc/config');
+		}
 
-        //récupération ip du client
-        if (isset($_SERVER['REMOTE_ADDR'])) $ip = $_SERVER['REMOTE_ADDR'];
-        $long_ip = ip2long($ip);
+		//récupération ip du client
+		if (isset($_SERVER['REMOTE_ADDR'])) {
+			$ip = $_SERVER['REMOTE_ADDR'];
+		}
+		$long_ip = ip2long($ip);
 
-        $ranges = explode(',', lire_config('intranet/plageip', ' '));
-        foreach ($ranges as $range) {
-            //Range d'ip contenant - comme séparateur
-            if (preg_match ("/-/",$range))  {
-                $ranges_2 = explode ('-', $range) ;
-                $low_long_ip = ip2long($ranges_2[0]);
-                $high_long_ip = ip2long($ranges_2[1]);
-                if ($long_ip <= $high_long_ip && $low_long_ip <= $long_ip) {
-                    $autoriser = true;
-                    break; // on a trouvé une ip bonne on ne continue pas plus loin
-                }
-            } // Ip individuelle
-            else {
-                if ($long_ip == ip2long($range)) {
-                    $autoriser = true;
-                    break;
-                }
-            }
-        }
-    }
-    return $autoriser;
+		$ranges = explode(',', lire_config('intranet/plageip', ' '));
+		foreach ($ranges as $range) {
+			//Range d'ip contenant - comme séparateur
+			if (preg_match('/-/', $range)) {
+				$ranges_2 = explode('-', $range);
+				$low_long_ip = ip2long($ranges_2[0]);
+				$high_long_ip = ip2long($ranges_2[1]);
+				if ($long_ip <= $high_long_ip && $low_long_ip <= $long_ip) {
+					$autoriser = true;
+					break; // on a trouvé une ip bonne on ne continue pas plus loin
+				}
+			} // Ip individuelle
+			else {
+				if ($long_ip == ip2long($range)) {
+					$autoriser = true;
+					break;
+				}
+			}
+		}
+	}
+	return $autoriser;
 }
 
 // dans le site public
 // si auteur pas autorise : placer sur un cache dedie
-if (!test_espace_prive()){
-  include_spip('inc/autoriser');
-  if (!autoriser('intranet')) {
-    if (isset($GLOBALS['marqueur'])) {
-      $GLOBALS['marqueur'].= ':intranet_out';
-    } else {
-      $GLOBALS['marqueur'] = 'intranet_out';
-    }
-  }
+if (!test_espace_prive()) {
+	include_spip('inc/autoriser');
+	if (!autoriser('intranet')) {
+		if (isset($GLOBALS['marqueur'])) {
+			$GLOBALS['marqueur'].= ':intranet_out';
+		} else {
+			$GLOBALS['marqueur'] = 'intranet_out';
+		}
+	}
 }
 
 /**
@@ -69,21 +70,18 @@ if (!test_espace_prive()){
  * @param array $flux
  * @return array
  */
-function intranet_styliser($flux){
-	if ( 
-		!test_espace_prive()
-		AND strpos($flux['args']['fond'],'/')===false
-		AND !in_array(substr($flux['args']['fond'],-3),array('.js','.css'))
-		AND include_spip('inc/autoriser')
-		AND !autoriser('intranet')
-		AND include_spip('inc/config')
-		AND ($pages_ok = array_filter(pipeline('intranet_pages_ok',array_merge(array('robots.txt','spip_pass','favicon.ico','informer_auteur'),explode(',',lire_config('intranet/pages_intranet',' '))))))
-		AND !in_array($flux['args']['fond'],$pages_ok)
-		AND !in_array($flux['args']['contexte'][_SPIP_PAGE],$pages_ok)){
-			$fond = trouver_fond('inclure/intranet','',true);
+function intranet_styliser($flux) {
+	if (!test_espace_prive()
+		and strpos($flux['args']['fond'], '/') === false
+		and !in_array(substr($flux['args']['fond'], -3), array('.js','.css'))
+		and include_spip('inc/autoriser')
+		and !autoriser('intranet')
+		and include_spip('inc/config')
+		and ($pages_ok = array_filter(pipeline('intranet_pages_ok', array_merge(array('robots.txt','spip_pass','favicon.ico','informer_auteur'), explode(',', lire_config('intranet/pages_intranet', ' '))))))
+		and !in_array($flux['args']['fond'], $pages_ok)
+		and !in_array($flux['args']['contexte'][_SPIP_PAGE], $pages_ok)) {
+			$fond = trouver_fond('inclure/intranet', '', true);
 			$flux['data'] = $fond['fond'];
 	}
 	return $flux;
 }
-
-?>
