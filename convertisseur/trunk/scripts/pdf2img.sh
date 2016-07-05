e70bf69cfe00367b1e8fd1171fa318619e5240e9
booz@rezo.net <booz@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-07-05 11:31:59 +0000

    numéroter les JPG extrait d'un PDF en partant de 1 et non pas 0 comme fait imagemagick convert
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@98679 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- convertisseur/trunk/scripts/pdf2img.sh
+++ convertisseur/trunk/scripts/pdf2img.sh
@@ -21,5 +21,25 @@ echo "conversion (shave $shave) de $pdf dans $fichier_dest"
 # rogner : -shave 40x40
 # redimensionner : -resize 1500
 
+nb_pages=$(pdfinfo "$pdf" | grep Pages | awk '{print $2}')
+
 # une seule page
 convert -verbose -colorspace RGB -resize 1500 -interlace none -density 300 -background white -alpha remove -quality 80 -shave "$shave" "$pdf" "$fichier_dest"
+
+# si on a affaire a un fichier multi pages on renumérote +1 pour ne pas démarrer à 0
+if [[ $nb_pages > 1 ]]
+	then
+		cd "$dest"
+		ls -r| while read f ; do 
+
+			p=$(echo $f | grep -Eo "(\d+).jpg" | grep -Eo "\d+" | sed -e 's/^0//g')
+			pp=$((p+1))
+			(($pp < 10)) && pp="0$pp"
+			#pp=${pp/001/01}
+			g=$(echo "${f/-$p.jpg/_$pp.jpg}" | sed -e 's/001/01/g')
+			g=$(echo $g | sed -e 's/-0.jpg/_01.jpg/g' )	
+			# echo "$f > $p > $pp > $g"
+			mv "$f" "$g"
+		
+		done
+fi
