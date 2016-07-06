bd7aa928490224d7808fa2a6de84e39097b7cd8e
rporto@calepino.com <rporto@calepino.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-07-06 03:33:34 +0000

    [Salvatore] [source:_plugins_/comments/trunk/lang/ comments] Export depuis http://trad.spip.net de la langue pt_br
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@98696 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- comments/trunk/lang/comments_pt_br.php
+++ comments/trunk/lang/comments_pt_br.php
@@ -10,9 +10,23 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// C
+	'cfg_explication_nom_obligatoire' => 'Para comentar, é necessário informar um nome ou apelido. Você pode optar por deixar obrigatório informar um e-mail.',
+	'cfg_forum_champs_obligatoires' => 'Campos obrigatórios',
+	'cfg_forum_format_fil' => 'Lista ou tópicos?',
 	'cfg_forum_longueur' => 'Tamanho das mensagens',
 	'cfg_forum_longueur_maxi' => 'Tamanho máximo',
 	'cfg_forum_longueur_mini' => 'Tamanho mínimo',
+	'cfg_forum_permalien' => 'Link permanente',
+	'cfg_label_apparence_permalien' => 'Exibição do link permanente no comentário',
+	'cfg_label_apparence_permalien_aucun' => 'Nenhum',
+	'cfg_label_apparence_permalien_compteur' => 'Contador (1.)',
+	'cfg_label_apparence_permalien_diese' => 'Cerquilha (#)',
+	'cfg_label_apparence_permalien_picto' => 'Ícone',
+	'cfg_label_email_obligatoire' => 'Tornar informação de e-mail obrigatória',
+	'cfg_label_presentation_fil' => 'Apresentação do tópico de discussão',
+	'cfg_label_presentation_fil_liste' => 'Lista de comentários (linear)',
+	'cfg_label_presentation_fil_thread' => 'Comentários agrupados (em tópicos)',
+	'cfg_label_presentation_fil_thread1' => 'Comentários agrupados em um nível',
 	'comment' => 'comentário',
 	'comments' => 'comentários',
 	'comments_h' => 'Seus comentários',
@@ -27,7 +41,9 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// L
 	'label_email' => 'E-mail (não divulgado)',
 	'label_nom' => 'Nome',
+	'label_notification' => 'Avise-me por e-mail de todos os novos comentários desta discussão',
 	'label_url' => 'O seu website',
+	'lien_suivre_commentaires' => 'Acompanhar os comentários:',
 
 	// M
 	'moderation_info' => 'Atenção, a sua mensagem só será exibida após ter sido relida e aprovada.',
@@ -40,7 +56,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'reponse_comment_ok' => 'Obrigado pelo seu comentário!',
 
 	// S
-	'saisie_texte_info' => 'Este formulário aceita os atalhos de formatação SPIP <code>[-&gt;url] {{negrito}} {itálico} &lt;quote&gt; &lt;code&gt;</code>  e o código HTML <code>&lt;q&gt; &lt;del&gt; &lt;ins&gt;</code>. Para criar parágrafos, simplesmente deixe linhas em branco.', # MODIF
+	'saisie_texte_info' => 'Este formulário aceita os atalhos de formatação SPIP <code>[-&gt;url] {{negrito}} {itálico} &lt;quote&gt; &lt;code&gt;</code>  e o código HTML <code>&lt;q&gt; &lt;del&gt; &lt;ins&gt;</code>. Para criar parágrafos, simplesmente deixe linhas em branco.',
 	'saisie_texte_legend' => 'Inclua aqui o seu comentário',
 	'submit1' => 'Visualizar',
 	'submit2' => 'Confirmar o envio',
