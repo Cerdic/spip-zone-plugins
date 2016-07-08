d1e4749bcf9d4d13cf55ce641546341c709c7875
brunobergot@gmail.com <brunobergot@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-07-08 16:05:15 +0000

    version 1.2.4 : éviter une erreur lors de la modification de la date de publication si la case "Ne pas utiliser..." est cochée
    On passait à tord dans le test else if ($v=_request($k."_jour") AND !dater_recuperer_date_saisie($v)) alors que la case était bien cochée. Du coup, on évite toute vérification si c'est le cas, et un peu de mise en forme en cadeau ;)
    
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@98739 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- depublie/trunk/depublie_pipelines.php
+++ depublie/trunk/depublie_pipelines.php
@@ -82,12 +82,15 @@ function depublie_formulaire_charger($flux){
 function depublie_formulaire_verifier($flux){
 	if ($flux['args']['form'] == 'dater' && _request('changer')){
 		$k='date_depublie';
-		if(_request('sans_depublie')!=1 && _request('date_jour') && _request($k."_jour") && (dater_recuperer_date_saisie(_request('date_jour')) >= dater_recuperer_date_saisie(_request($k."_jour"))))
-			$flux[$k] = _T('depublie:erreur_date_superieure');
-		else if ($v=_request($k."_jour") AND !dater_recuperer_date_saisie($v))
-			$flux[$k] = _T('format_date_incorrecte');
-		elseif ($v=_request($k."_heure") AND !dater_recuperer_heure_saisie($v))
-			$flux[$k] = _T('format_heure_incorrecte');
+		if (_request('sans_depublie') != 1) {
+			if(_request('date_jour') && _request($k."_jour") && (dater_recuperer_date_saisie(_request('date_jour')) >= dater_recuperer_date_saisie(_request($k."_jour")))) {
+				$flux[$k] = _T('depublie:erreur_date_superieure');
+			} else if ($v=_request($k."_jour") AND !dater_recuperer_date_saisie($v)) {
+				$flux[$k] = _T('format_date_incorrecte');
+			} elseif ($v=_request($k."_heure") AND !dater_recuperer_heure_saisie($v)) {
+				$flux[$k] = _T('format_heure_incorrecte');
+			}
+		}
 	}
 	return $flux;
 }
