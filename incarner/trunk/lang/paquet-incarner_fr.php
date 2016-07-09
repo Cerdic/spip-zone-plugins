403f4a1fab4fa7a30e19b3de45412c68f68c2fe9
bystrano@gmx.ch <bystrano@gmx.ch@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-07-09 13:10:00 +0000

    commit initial - par la Fabrique
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@98758 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ incarner/trunk/lang/paquet-incarner_fr.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// I
+	'incarner_description' => 'Permet aux webmestres de changer d\'utilisateur en 1 clic. Très pratique pour débugger des autorisations.
+NE PAS ACTIVER EN PRODUCTION !',
+	'incarner_nom' => 'Incarner',
+	'incarner_slogan' => 'Changer rapidement d\'utilisateur durant le développement.',
+);
+
+?>
