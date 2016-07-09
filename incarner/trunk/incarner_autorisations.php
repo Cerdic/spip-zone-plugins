66b7547e74f130b0455a5fc4bfcf4af2bf416c98
bystrano@gmx.ch <bystrano@gmx.ch@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-07-09 13:12:35 +0000

    commentaire
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@98768 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- incarner/trunk/incarner_autorisations.php
+++ incarner/trunk/incarner_autorisations.php
@@ -15,6 +15,8 @@
 function incarner_autoriser() {
 }
 
+/* On n'est autorisé à changer d'utilisateur que si l'on est webmestre, ou qu'on
+	 a un cookie connu. */
 function autoriser_incarner_dist($faire, $type, $id, $qui, $opt) {
 
 	include_spip('inc/config');
