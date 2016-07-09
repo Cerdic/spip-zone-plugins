107525d27ff5caae533294310d66f8ff6234ecae
bystrano@gmx.ch <bystrano@gmx.ch@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-07-09 13:11:02 +0000

    meilleure description
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@98762 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- incarner/trunk/lang/paquet-incarner_fr.php
+++ incarner/trunk/lang/paquet-incarner_fr.php
@@ -6,8 +6,11 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// I
-	'incarner_description' => 'Permet aux webmestres de changer d\'utilisateur en 1 clic. Très pratique pour débugger des autorisations.
-NE PAS ACTIVER EN PRODUCTION !',
+	'incarner_description' => "Permet aux webmestres de changer d'utilisateur très facilement. Très pratique pour débugger des autorisations.
+
+On peux changer d'utilisateur en visitant l'url spip.php?action=incarner&login=toto
+
+Ce plugin est un trou de sécurité, NE PAS ACTIVER EN PRODUCTION !",
 	'incarner_nom' => 'Incarner',
 	'incarner_slogan' => 'Changer rapidement d\'utilisateur durant le développement.',
 );
