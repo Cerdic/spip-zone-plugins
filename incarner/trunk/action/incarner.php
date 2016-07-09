c4bfad800f002565f3efb8be3a4f38e1af442fe6
bystrano@gmx.ch <bystrano@gmx.ch@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-07-09 13:11:18 +0000

    permet de se déconnecter avec &logout=public
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@98763 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- incarner/trunk/action/incarner.php
+++ incarner/trunk/action/incarner.php
@@ -2,6 +2,8 @@
 
 function action_incarner_dist () {
 
+  include_spip('inc/headers');
+
   if ( ! autoriser('incarner')) {
     include_spip('inc/headers');
     http_status(403);
@@ -9,7 +11,11 @@ function action_incarner_dist () {
   }
 
   include_spip('inc/auth');
-  $login  = _request('login');
-  $auteur = auth_identifier_login($login, '');
+  if ($login  = _request('login')) {
+    $auteur = auth_identifier_login($login, '');
+    auth_loger($auteur);
+  } else if (_request('logout')) {
+    redirige_par_entete(html_entity_decode(generer_url_action('logout', 'logout=public', False, True)));
+  }
 
 }
