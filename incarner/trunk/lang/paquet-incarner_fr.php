ad8213ecea9ec45d554634b6b8916e0a94b9b99d
bystrano@gmx.ch <bystrano@gmx.ch@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-07-09 13:14:57 +0000

    On passe en test + complète la description
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@98777 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- incarner/trunk/lang/paquet-incarner_fr.php
+++ incarner/trunk/lang/paquet-incarner_fr.php
@@ -6,7 +6,8 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// I
 	'incarner_description' => "Permet aux webmestres de changer d'utilisateur très facilement. Très pratique pour débugger des autorisations.
 
-On peux changer d'utilisateur en visitant l'url spip.php?action=incarner&login=toto
+Ce plugin ajoute un lien « se connecter en tant que XX » sur chaque page auteur dans l'espace privé, ainsi qu'un lien « redevenir XX » côté public, qui permet de redevenir le webmestre qu'on était à la base.
+On peux aussi changer d'utilisateur en visitant l'url spip.php?action=incarner&login=toto
 
 Ce plugin est un trou de sécurité, NE PAS ACTIVER EN PRODUCTION !",
 	'incarner_nom' => 'Incarner',
