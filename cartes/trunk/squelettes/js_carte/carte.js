e612cbcc477339c6e550011415791c0380358b01
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-07-10 10:51:57 +0000

    Diverses améliorations
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@98791 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- cartes/trunk/squelettes/js_carte/carte.js
+++ cartes/trunk/squelettes/js_carte/carte.js
@@ -1,4 +1,5 @@
-var lang_direction = $('html').attr('dir') ? $('html').attr('dir') : 'ltr';
+var lang_direction = $('html').attr('dir') ? $('html').attr('dir') : 'ltr',
+	informatifs = [];
 
 var resizemap = function (map_resize) {
 	$('.map_wrapper').height($(window).height() - $('.footer').outerHeight()-$('.main').outerHeight());
@@ -18,17 +19,16 @@ var callback_map1 = function (map) {
 
 var country_min_topojson_addlayer = function(e,topojson){
 	e.layer.eachLayer(function(dist){
-	var iso = dist.toGeoJSON().properties.ISO2.toLowerCase();
-	dist.bindPopup("<strong>"+dist.toGeoJSON().properties.NAME_FR+"</strong>",{closeButton:false,className:'popupcountry'});
-	if(dist.options){
-		dist.options.className = 'country country-'+iso;
-	}else {
-		dist.eachLayer(function (dist2) {
-			dist2.options.className = 'country country-'+iso;
-		});
-	}
+		var iso = dist.toGeoJSON().properties.ISO2.toLowerCase();
+		if(dist.options){
+			dist.options.className = 'country country-'+iso;
+		}else {
+			dist.eachLayer(function (dist2) {
+				dist2.options.className = 'country country-'+iso;
+			});
+		}
 	});
-	window.setTimeout(function(){topojson.bringToBack()},5);
+	window.setTimeout(function(){topojson.bringToBack();resizemap(map1);},20);
 }
 $(document).ready(function () {
 	$(window).on('resize', function () {
@@ -42,11 +42,16 @@ $(document).ready(function () {
 		var resized = false;
 		if (map1) {
 			resized = map1;
+			map1.on('viewreset',function(){
+				map1.invalidateSize();
+				informatifs.forEach(function(e){console.log(e);e.label._updatePosition();})
+			});
 		}
 		resizemap(resized);
 	});
 
 	jQuery('#map1').on('ready',function(e, map){
+		var label_bloque = false;
 		if(map.options.options && map.options.options.popup == 'control'){
 		var info = L.control();
 		info.onAdd = function (map) {
@@ -71,6 +76,31 @@ $(document).ready(function () {
 			layer.off('click').on('click',function(e,f){
 				if(layer._popup && layer._popup._content){
 					info.update(layer._popup._content);
+					if (label_bloque && map1._layers[label_bloque] && map1._layers[label_bloque].label) {
+						options_bloque = map1._layers[label_bloque].label.options;
+						options_bloque.noHide = false;
+						layerx = map1._layers[label_bloque];
+						layerx.options.zIndexOffset = 0;
+						if (layerx.feature && layerx.feature.properties && (layerx.feature.properties.title || (map.options.langue && layerx.feature.properties['title_'+map.options.langue]))) {
+							title = (map.options.langue && layerx.feature.properties['title_'+map.options.langue]) || layerx.feature.properties.title;
+							layerx.unbindLabel().bindLabel(title, options_bloque);
+						}
+						
+						map.removeLayer(layerx);
+						layerx.addTo(map);
+					}
+					if (layer.label) {
+						options = layer.label.options;
+						options.noHide = true
+						label_bloque = layer._leaflet_id;
+						if (layer.feature && layer.feature.properties && (layer.feature.properties.title || (map.options.langue && layer.feature.properties['title_'+map.options.langue]))) {
+							title = (map.options.langue && layer.feature.properties['title_'+map.options.langue]) || layer.feature.properties.title;
+							layer.unbindLabel().bindLabel(layer.feature.properties.title, options);
+						}
+						map.removeLayer(layer);
+						layer.options.zIndexOffset = 1000;
+						layer.addTo(map);
+					}
 					map.closePopup();
 					map.panTo(e.latlng);
 					return false;
@@ -78,36 +108,41 @@ $(document).ready(function () {
 			});
 		});
 		map.on('popupopen',function(){
-				info.update();
+			info.update();
 		});
 		}
 		map.eachLayer(function(layer){
-		/**
-		 * Gestion des points informatifs
-		 * 
-		 * Ils ne sont pas clickable et sélectionnables
-		 * Ils n'ont pas de popup
-		 * On leur met un label qui est leur titre
-		 */
-		if(layer.feature && layer.feature.properties && layer.feature.properties.role && $.inArray('informatif',layer.feature.properties.role) != -1){
-			layer.options.clickable = false;
-			layer.options.riseOnHover = true;
-			layer.options.keyboard = false;
-			layer.options.icon.options.labelAnchor = [4,-4];
-			if (lang_direction == 'rtl') {
-				var offset = [60, -15]
-			} else {
-				var offset = [20, -15]
+			/**
+			 * Gestion des points informatifs
+			 * 
+			 * Ils ne sont pas clickable et sélectionnables
+			 * Ils n'ont pas de popup
+			 * On leur met un label qui est leur titre
+			 */
+			if(layer.feature && layer.feature.properties && layer.feature.properties.role && $.inArray('informatif',layer.feature.properties.role) != -1){
+				layer.options.clickable = false;
+				layer.options.riseOnHover = true;
+				layer.options.keyboard = false;
+				layer.options.icon.options.labelAnchor = [4,-4];
+				if (lang_direction == 'rtl') {
+					var offset = [60, -15]
+				} else {
+					var offset = [20, -15]
+				}
+				layer.unbindPopup().bindLabel(layer.feature.properties.title, { noHide: true, className: 'informatif', direction: 'right', lang_direction : lang_direction, offset: offset});
+				map.removeLayer(layer);
+				layer.addTo(map);
+				informatifs.push(layer);
+			} else if (map.options.options && map.options.options.label && layer.feature && layer.feature.properties && layer.feature.properties.title) {
+				if (layer.options && typeof layer.options.riseOnHover != 'undefined')
+					layer.options.riseOnHover = true;
+					if (layer.feature.properties.title || (map.options.langue && layer.feature.properties['title_'+map.options.langue])) {
+						title = (map.options.langue && layer.feature.properties['title_'+map.options.langue]) || layer.feature.properties.title;
+						layer.bindLabel(title, { className: 'action', direction: 'auto', lang_direction : lang_direction});
+					}
+				map.removeLayer(layer);
+				layer.addTo(map);
 			}
-			layer.unbindPopup().bindLabel(layer.feature.properties.title, { noHide: true, className: 'informatif', direction: 'right', lang_direction : lang_direction, offset: offset});
-			map.removeLayer(layer);
-			layer.addTo(map);
-		} else if (map.options.options && map.options.options.label && layer.feature && layer.feature.properties) {
-			layer.options.riseOnHover = true;
-			layer.bindLabel(layer.feature.properties.title, { className: 'action', direction: 'auto', lang_direction : lang_direction});
-			map.removeLayer(layer);
-			layer.addTo(map);
-		}
 		});
 		if(map.options.options && map.options.options.layer_topojson){
 			var topojson_layer = new L.TOPOJSON(map.options.options.layer_topojson, {async: true}),
