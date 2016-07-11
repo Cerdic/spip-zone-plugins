8b706ad10ace8db3c8c569b0e0102fb640d999ae
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-07-11 08:56:34 +0000

    Report de [98786] : Bugfix action [enlever de toutes les zones] ​http://contrib.spip.net/Acces-Restreint-3-0#forum488177 (Merci Cédric)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@98822 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- acces_restreint/branches/v3.3/action/retirer_zone.php
+++ acces_restreint/branches/v3.3/action/retirer_zone.php
@@ -16,10 +16,13 @@ function action_retirer_zone_dist(){
 		$type = $regs[2];
 		$id_objet = intval($regs[3]);
 		include_spip('action/editer_zone');
-		if ($id_objet=='-1')
+		if ($id_objet=='-1') {
 			accesrestreint_revision_zone_objets_lies($id_zone,array(),$type,'set');
-		else
+		} elseif ($id_zone=='-1') {
+			accesrestreint_revision_zone_objets_lies(array(), $id_objet, $type, 'del');
+		} else {
 			accesrestreint_revision_zone_objets_lies($id_zone,$id_objet,$type,'del');
+		}
 	}
 }
 
