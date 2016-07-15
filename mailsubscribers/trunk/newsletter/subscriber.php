6983f5190418ecd7e3ad61e9a05c3e001414a4d6
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-07-15 18:20:13 +0000

    on peut passer une liste en option de newsletter/subscribe, qui est alors prise en compte pour fournir url_unsubscribe qui s'appliquera a cette seule liste
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@98895 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- mailsubscribers/trunk/newsletter/subscriber.php
+++ mailsubscribers/trunk/newsletter/subscriber.php
@@ -15,19 +15,23 @@ include_spip('mailsubscribers_fonctions');
  * Decrit les informations d'un inscrit
  * Pour retirer une liste il faut desinscrire
  *
- * @param $email
+ * @param string $email
  *   champ obligatoire
+ * @param array $options
+ *   array :
+ *     array listes : liste(s) pour l'unsubscribe
  * @return bool|array
  *   false si n'existe pas
  *   array :
  *     string email
  *     string nom
- *     array listes
+ *     deprecated array listes
  *     string lang
  *     string status : on|pending|off
+ *     array subscriptions
  *     string url_unsubscribe : url de desabonnement
  */
-function newsletter_subscriber_dist($email) {
+function newsletter_subscriber_dist($email, $options = array()) {
 
 	// chercher si un tel email est deja en base
 	$infos = sql_fetsel("email,nom,'' as listes,lang,'' as status,jeton,id_mailsubscriber", 'spip_mailsubscribers',
@@ -35,6 +39,15 @@ function newsletter_subscriber_dist($email) {
 	if ($infos) {
 		$infos = mailsubscribers_informe_subscriber($infos);
 
+		// si on est dans le contexte d'une liste unique connue, modifier l'url_unsubscribe
+		if (isset($options['listes'])
+			and is_array($options['listes'])
+			and count($options['listes'])==1
+		  and $id = reset($options['listes'])
+			and $id = mailsubscribers_normaliser_nom_liste($id)
+		  and isset($infos['subscriptions'][$id]['url_unsubscribe'])){
+			$infos['url_unsubscribe'] = $infos['subscriptions'][$id]['url_unsubscribe'];
+		}
 		return $infos;
 	}
 
