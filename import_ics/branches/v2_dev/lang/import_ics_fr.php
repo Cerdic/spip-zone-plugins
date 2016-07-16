2e18e4bdbd237f39bdad3a67524d240b0aa858c2
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-07-16 17:07:28 +0000

    on va créer un formulaire de configuration du plugin
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@98911 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- import_ics/branches/v2_dev/lang/import_ics_fr.php
+++ import_ics/branches/v2_dev/lang/import_ics_fr.php
@@ -5,6 +5,8 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
+  // C
+	"cfg_configurer"   => "Configurer l'import de fichiers ICS",
 	// I
 	'import_ics_titre' => 'Import_ics',
 );
