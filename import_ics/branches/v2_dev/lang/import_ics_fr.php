ed51920cac58268e8c6136a1c0178728f794a041
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-07-17 10:07:45 +0000

    un petit message d'avertissement
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@98917 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- import_ics/branches/v2_dev/lang/import_ics_fr.php
+++ import_ics/branches/v2_dev/lang/import_ics_fr.php
@@ -4,9 +4,10 @@
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-
-  // C
-	"cfg_configurer"   => "Configurer l'import de fichiers ICS",
+	
+  	// C
+	"cfg_attention"    => "Attention: les modifications effectuées sur ce formulaire n'affecteront pas les almanachs déjà créés et les évènements associés.", 
+	"cfg_configurer"   => "Configurer l'import de fichiers ICS",	
 	
 	//G
 	"groupe_mots"			=> "Groupe de mots",
