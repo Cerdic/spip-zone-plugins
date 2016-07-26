10c16b719f02f136f732e6811282898fb94dcc1a
peetdu@gmail.com <peetdu@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-07-26 09:37:52 +0000

    pétouilles
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@99004 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- emplois/trunk/lang/paquet-emplois_fr.php
+++ emplois/trunk/lang/paquet-emplois_fr.php
@@ -7,14 +7,14 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// E
 	'emplois_description' => 'Ce plugin comporte deux objets :
-	- <b>Offre d\'emplois</b>
-	- <b>CVs</b>
+
+	- <b>Offre d\'emploi</b>
+	- <b>CV</b>
 
 	Il est possible d\'activer l\'un ou l\'autre, ou les deux selon vos besoins.
 
-	Des fonctionnalités ont été mises en place afin de facilement utiliser les formulaires correspondants depuis l\'espace publique de votre site, dont notamment la possibilité pour le déposant de joindre un fichier PDF.'.
-
-	,
+	- <b>depuis l\'espace public</b>
+	Des fonctionnalités ont été mises en place afin de faciliter l\'utilisation des formulaires correspondants depuis l\'espace publique, dont notamment la possibilité pour le déposant de joindre un fichier PDF.',
 	'emplois_nom' => 'Emplois',
-	'emplois_slogan' => 'Gestion de dépôts de CV et d\'offres d\'emploi',
+	'emplois_slogan' => 'Gestion de dépôts de CV et d\'offre d\'emploi',
 );
