3179e378fbb7de754ef610c3227884c26abd4127
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-07-31 19:30:53 +0000

    pas d'underscore dans le nom du plugin
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@99071 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- import_ics/branches/v2_dev/lang/paquet-import_ics_fr.php
+++ import_ics/branches/v2_dev/lang/paquet-import_ics_fr.php
@@ -7,7 +7,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// I
 	'import_ics_description' => 'Importez les événements de sites distants dans votre base de données d\'événements SPIP',
-	'import_ics_nom' => 'Import_ics',
+	'import_ics_nom' => 'Import ics',
 	'import_ics_slogan' => 'Importez vos événements',
 );
 
