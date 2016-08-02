fea6c553c9d556b3382a84eeca42063800a279ad
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-08-02 03:35:15 +0000

    [Salvatore] [source:_plugins_/import_ics/trunk/lang/ import_ics] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@99096 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- import_ics/trunk/lang/import_ics_fr.php
+++ import_ics/trunk/lang/import_ics_fr.php
@@ -1,35 +1,37 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
-if (!defined('_ECRIRE_INC_VERSION')) return;
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/import_ics/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-	
-	//A
-	"archive"					 => "archive",
-	"archiver"				 => "archivé",
-  	// C
-	"cfg_attention"    => "Attention: les modifications effectuées sur ce formulaire n'affecteront pas les almanachs déjà créés et les évènements associés.", 
-	"cfg_configurer"   => "Configurer l'import de fichiers ICS",	
-	
+
+	// A
+	'archive' => 'archive',
+	'archiver' => 'archivé',
+
+	// C
+	'cfg_attention' => 'Attention : les modifications effectuées sur ce formulaire n’affecteront pas les almanachs déjà créés et les évènements associés.',
+	'cfg_configurer' => 'Configurer l’import de fichiers ICS',
+
 	// D
-	"depublier_anciens_evts" => "Dépublier les anciens évènements",
-	"depublier_anciens_evts_explication" => "Cocher cette case si vous souhaitez que les évènements qui ne sont plus présents dans un flux distant soient automatiquement basculés à en «&nbsp;archivés&nbsp;»",
-	
-	//G
-	"groupe_mots"			=> "Groupe de mots",
-	"groupe_mots_explication" =>"Lors de la création d'un almanach, forcer à choisir un mot clef dans le groupe.",
+	'depublier_anciens_evts' => 'Dépublier les anciens évènements',
+	'depublier_anciens_evts_explication' => 'Cocher cette case si vous souhaitez que les évènements qui ne sont plus présents dans un flux distant soient automatiquement basculés à en « archivés »',
+
+	// G
+	'groupe_mots' => 'Groupe de mots',
+	'groupe_mots_explication' => 'Lors de la création d’un almanach, forcer à choisir un mot clef dans le groupe.',
+
 	// I
 	'import_ics_titre' => 'Import_ics',
-	
+
 	// M
-	"mot" 		=> "Mot",
-	"mot_explication" => "Vous pouvez aussi, alternativement, forcer l'association à un mot clef particulier.",
-	"mot_facultatif"  => "Permettre de pas choisir de mot-clef",
-	"mot_facultatif_explication" =>"Par défaut, un mot-clef doit doit être associé à un almanach et aux évènement liés. Cette option permet de rendre cela facultatif.",
+	'mot' => 'Mot',
+	'mot_explication' => 'Vous pouvez aussi, alternativement, forcer l’association à un mot clef particulier.',
+	'mot_facultatif' => 'Permettre de pas choisir de mot-clef',
+	'mot_facultatif_explication' => 'Par défaut, un mot-clef doit doit être associé à un almanach et aux évènement liés. Cette option permet de rendre cela facultatif.',
 
-  //V
-	"v_php" => "Réglage verrouillé en PHP."
+	// V
+	'v_php' => 'Réglage verrouillé en PHP.'
 );
-
-?>
