4dcb1171b673ead2f0c16dcb568991a8701f62ac
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-08-02 03:34:52 +0000

    [Salvatore] [source:_plugins_/import_ics/trunk/lang/ almanach] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@99095 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- import_ics/trunk/lang/almanach_fr.php
+++ import_ics/trunk/lang/almanach_fr.php
@@ -1,34 +1,35 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
-if (!defined('_ECRIRE_INC_VERSION')) return;
-
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/import_ics/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
-
 	'ajouter_lien_almanach' => 'Ajouter cet almanach',
 	'appliquer_decalage' => 'Appliquer le décalage',
-	'appliquer_decalage_explication' => 'Voulez-vous appliquer le décalage à tous les événements associés à cet almanach (il est préférable de supprimer l\'ancien décalage puis d\'appliquer le nouveau) ?',
+	'appliquer_decalage_explication' => 'Voulez-vous appliquer le décalage à tous les événements associés à cet almanach (il est préférable de supprimer l’ancien décalage puis d’appliquer le nouveau) ?',
+	'attendee' => 'Intervenant(e)',
 	'aucun_evenement' => 'Cet almanach ne contient aucun événement. Si vous le désirez, vous pouvez le supprimer dans la liste des almanachs.
 ',
-  'attendee'					 => 	'Intervenant(e)',
 
-	//C
-	'confirmation_appliquer_decalage' => 'Êtes-vous bien certain de vouloir appliquer ce décalage ?',
-	'confirmation_suppression_evenements' => 'Êtes vous certain(e) de vouloir supprimer les événements de l\'almanach \"@titre_almanach@\" ?',
-	'confirmation_mise_a_jour_evenements' => 'Voulez-vous réellement mettre à jour la liste des événements de l\'almanach \"@titre_almanach@\" ?\nCela peut prendre un certain temps.',
+	// C
 	'choix_salle' => 'Tous les événements se verront attribuer cette salle dans le gestionnaire de ressources.',
+	'confirmation_appliquer_decalage' => 'Êtes-vous bien certain de vouloir appliquer ce décalage ?',
+	'confirmation_mise_a_jour_evenements' => 'Voulez-vous réellement mettre à jour la liste des événements de l’almanach \\"@titre_almanach@\\" ?\\nCela peut prendre un certain temps.',
+	'confirmation_suppression_evenements' => 'Êtes vous certain(e) de vouloir supprimer les événements de l’almanach \\"@titre_almanach@\\" ?',
 
 	// D
 	'decalage' => 'Décalage',
-	'decalage_explication'=>"Vous pouvez tenter un décalage global des horaires s'ils n'apparaissent pas à la bonne heure.", 
+	'decalage_explication' => 'Vous pouvez tenter un décalage global des horaires s’ils n’apparaissent pas à la bonne heure.',
+
 	// E
 	'explication_id_article' => 'Choisissez un article qui va recevoir les événements importés.',
-	'explication_titre' => 'Titre de l\'almanach',
-	'explication_url' => 'URL d\'origine du calendrier',
-	'explication_resa_auto' => 'On peut réserver automatiquement une salle pour tous les événements d\'un même almanach (modifiable individuellement ensuite).',
+	'explication_resa_auto' => 'On peut réserver automatiquement une salle pour tous les événements d’un même almanach (modifiable individuellement ensuite).',
+	'explication_titre' => 'Titre de l’almanach',
+	'explication_url' => 'URL d’origine du calendrier',
 
 	// H
 	'heure' => '@nb@ heure',
@@ -39,37 +40,38 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'icone_modifier_almanach' => 'Modifier cet almanach',
 	'info_1_almanach' => 'Un almanach',
 	'info_almanachs_auteur' => 'Les almanachs de cet auteur',
-	'info_evenement_almanach' => 'Les événements de cet almanach',
 	'info_aucun_almanach' => 'Aucun almanach',
 	'info_derniere_synchronisation' => 'La dernière synchronisation de cet almanach a été effectuée le',
-	'info_jamais_synchro' => 'Cet almanach n\'a jamais été synchronisé',
-
+	'info_evenement_almanach' => 'Les événements de cet almanach',
+	'info_jamais_synchro' => 'Cet almanach n’a jamais été synchronisé',
 	'info_nb_almanachs' => '@nb@ almanachs',
 	'info_supprimer_almanach' => 'Supprimer',
 	'info_supprimer_evenements' => 'Supprimer ces événements',
 
 	// L
-	'label_id_article' => 'Article d\'accueil de l\'almanach',
-	'label_id_mot' => 'Type des événements de cet almanach :',
+	'label_id_article' => 'Article d’accueil de l’almanach',
+	'label_id_mot' => 'Type des événements de cet almanach :',
 	'label_titre' => 'Titre',
 	'label_url' => 'URL',
 	'lien_synchro_almanach' => 'Mettre à jour cet almanach maintenant',
-	'lieu'						 =>		'Lieu',
+	'lieu' => 'Lieu',
 
-  //
-	'mot' 						 =>   'Mot associé aux évènements de cet almanach',
-	'mot_explication'  =>   'Le mot sélectionné sera automatiquement associé aux évènements de cet almanach.',
-	'mot_explication_verrouille'  =>   'Le mot sélectionné sera automatiquement associé aux évènements de cet almanach. Le choix du mot a été verrouillé dans la configuration du plugin.',
-  //N
-	'notes'						 =>		'Notes',
+	// M
+	'mot' => 'Mot associé aux évènements de cet almanach',
+	'mot_explication' => 'Le mot sélectionné sera automatiquement associé aux évènements de cet almanach.',
+	'mot_explication_verrouille' => 'Le mot sélectionné sera automatiquement associé aux évènements de cet almanach. Le choix du mot a été verrouillé dans la configuration du plugin.',
 
-  //O
-	'origin'					 => 	'Origine',
-  
-	//P
+	// N
+	'notes' => 'Notes',
+
+	// O
+	'origin' => 'Origine',
+
+	// P
 	'pas_de_decalage' => 'Pas de décalage.',
 	'plusieurs_evenements' => '@nb@ événements',
-	'purger_almanach' => 'Si vous voulez supprimer cet almanach, vous devez tout d\'abord en supprimer le contenu.',
+	'purger_almanach' => 'Si vous voulez supprimer cet almanach, vous devez tout d’abord en supprimer le contenu.',
+
 	// R
 	'regenerer_almanach' => 'Vous pouvez aussi restaurer son contenu en tentant une nouvelle synchronisation.',
 	'resa_auto' => 'Réservation automatique',
@@ -87,10 +89,8 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'titre_almanachs_rubrique' => 'Almanachs de la rubrique',
 	'titre_langue_almanach' => 'Langue de cet almanach',
 	'titre_logo_almanach' => 'Logo de cet almanach',
-	'type_evenement' => 'Tous les événements de l\'almanach auront le même type. Vous pourrez modifier cela individuellement.',
+	'type_evenement' => 'Tous les événements de l’almanach auront le même type. Vous pourrez modifier cela individuellement.',
 
-	//U
-	'un_evenement' => '@nb@ événement',
+	// U
+	'un_evenement' => '@nb@ événement'
 );
-
-?>
