318f74def24b390505d59017ea9a3b19f3e48d11
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-08-02 16:50:40 +0000

    une action abandonner_commande qui delegue a supprimer_commande (appel conventionnel depuis le plugin bank suite a abandonner_transaction-
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@99104 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ commandes/trunk/action/abandonner_commande.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Action du plugin Commandes
+ *
+ * @plugin     Commandes
+ * @copyright  2014
+ * @author     Ateliers CYM, Matthieu Marcillaud, Les Développements Durables
+ * @licence    GPL 3
+ * @package    SPIP\Commandes\Action
+ */
+
+// Sécurité
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+/**
+ * Abandonne une commande liee a une transaction (appel conventionnel par abandonner_transaction)
+ *
+ * @uses action_supprimer_commande_dist()
+ *
+ * @param $arg string
+ *     id_commande : identifiant de la commande
+ * @return void
+ */
+function action_supprimer_commande_dist($arg=null) {
+
+	// Si $arg n'est pas donné directement, le récupérer via _POST ou _GET
+	if (is_null($arg)){
+		$securiser_action = charger_fonction('securiser_action', 'inc');
+		$arg = $securiser_action();
+	}
+	$id_commande = $arg;
+
+	// pas de statut abandon pour le moment, on passe en poubelle donc
+	$supprimer_commande = charger_fonction('supprimer_commande','action');
+	$supprimer_commande($id_commande);
+
+}
+
