7ef6020dec5a962a5e56f582dc3fd1997bfdb744
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-08-02 03:35:30 +0000

    [Salvatore] [source:_plugins_/import_ics/trunk/lang/ paquet-import_ics] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@99097 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- import_ics/trunk/lang/paquet-import_ics_fr.php
+++ import_ics/trunk/lang/paquet-import_ics_fr.php
@@ -1,14 +1,14 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
-if (!defined('_ECRIRE_INC_VERSION')) return;
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/import_ics/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// I
-	'import_ics_description' => 'Importez les événements de sites distants dans votre base de données d\'événements SPIP',
+	'import_ics_description' => 'Importez les événements de sites distants dans votre base de données d’événements SPIP',
 	'import_ics_nom' => 'Import ics',
-	'import_ics_slogan' => 'Importez vos événements',
+	'import_ics_slogan' => 'Importez vos événements'
 );
-
-?>
