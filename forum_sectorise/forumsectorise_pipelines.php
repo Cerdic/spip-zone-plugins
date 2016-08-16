a5125a6f95ce213d5306c2e16b4c6947e653eba4
p@henix.be <p@henix.be@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-08-16 08:23:58 +0000

    Compatible 3.1
    La fonction table_objet_sql existe en SPIP 2.1
    On l'utilise plutôt qu'une comparaison de version, cela permet la
    compatibilité SPIP 3.1.
    
    Testé sur 3.1.0 et 2.1.28
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@99175 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- forum_sectorise/forumsectorise_pipelines.php
+++ forum_sectorise/forumsectorise_pipelines.php
@@ -4,14 +4,10 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 function forumsectorise_pre_insertion($flux) {
 	include_spip('inc/plugin');
 	include_spip('inc/config');
-	if(spip_version_compare($GLOBALS['spip_version_branche'],"2.1.99","<")) {
-		$table = 'spip_articles' ;
-	} else {
-		$table = 'spip_article' ;
-	}
 
 	$conf_forumsectorise = lire_config('forumsectorise');
-	if (($flux['args']['table'] == $table) &&
+
+	if (($flux['args']['table'] == table_objet_sql('article')) &&
 		 in_array($flux['data']['id_secteur'], $conf_forumsectorise['ident_secteur'])) {
 		$flux['data']['accepter_forum'] = $conf_forumsectorise['type'];
 	}
