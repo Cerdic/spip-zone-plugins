42bc972c3b99248773fcc56ddf41b79aaadb07ff
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-08-16 19:23:01 +0000

    Quelques chaines de langue génériques
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@99181 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ cvt_rechercher/trunk/lang/rechercher_fr.php
@@ -0,0 +1,11 @@
+<?php
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	'resultats_1' => 'Un résultat',
+	'resultats_aucun' => 'Aucun résultat',
+	'resultats_nb' => '@nb@ résultats',
+);
