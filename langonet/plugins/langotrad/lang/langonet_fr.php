e7d5503698b707c0fe1d4eed2065a7f8cfe32bd3
erational@erational.org <erational@erational.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-08-17 13:38:11 +0000

    ajout d'un repertoire pour des sous-plugins pour langonet ajout du plugin langotrad qui permet d'editer et gerer des traductions
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@99184 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ langonet/plugins/langotrad/lang/langonet_fr.php
@@ -0,0 +1,34 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+    'bouton_traduire' => 'OK',
+    'bouton_editer' => 'OK',
+
+    // F
+    'message_ok_fichier_edite' => 'Le fichier modifié a été crée avec succès à l\'adresse: @fichier@',
+    'message_ok_fichier_traduit' => 'Le fichier traduction a été crée avec succès à l\'adresse: @fichier@',
+
+	// I
+    'info_editer' => 'Cette option vous permet d\'éditer les items d\'un fichier de langue. Le fichier généré n\'écrase pas le fichier original mais il est stocké dans le répertoire temporaire de tmp/langonet.',
+    'info_fichier_liste_editer' => 'Choisissez le fichier de langue dont vous voulez éditer les items, parmi ceux présents dans le site.',
+    'info_traduire' => 'Cette option vous permet de générer une traduction d\'un fichier de langue.',
+
+
+	// O
+	'onglet_traduire' => 'Traduire une langue',
+    'onglet_editer' => 'Éditer une langue',
+
+    // T
+    'titre_form_traduire' => 'Génération d\'une traduction',
+    'titre_form_editer' => 'Édition d\'une langue',
+
+
+
+);
+
+
---
+++ langonet/plugins/langotrad/lang/paquet-langotrad_fr.php
@@ -0,0 +1,12 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+    // L
+	'langotrad_description' => 'Sous plugin de LangOnet pour l\'édition et la traduction de vos chaines de langues.',
+	'langotrad_slogan' => 'Editer et traduire vos fichiers de langue',
+);
