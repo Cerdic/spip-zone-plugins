774cfb214500b16355aaca731b012f04483ad058
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-08-29 15:01:02 +0000

    PSR SPIP et optimisation de l'image
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@99268 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- bellespuces/bellespuces.php
+++ bellespuces/bellespuces.php
@@ -1,8 +1,10 @@
 <?php
 
-if (!defined('_ECRIRE_INC_VERSION')) return;
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
 function bellespuces_pre_typo($texte) {
-        $texte = preg_replace('/^-\s?(?!\*|#|-)/m','-* ',$texte);
-        return $texte;
+	$texte = preg_replace('/^-\s?(?!\*|#|-)/m', '-* ', $texte);
+	return $texte;
 }
