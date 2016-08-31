77a0d3c16e71e6d9280542833a839cdf4d91c9bb
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-08-31 18:18:30 +0000

    debuger appliquer_decalage
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@99312 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- import_ics/trunk/action/appliquer_decalage.php
+++ import_ics/trunk/action/appliquer_decalage.php
@@ -13,7 +13,7 @@
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 include_spip('lib/iCalcreator.class'); /*pour la librairie icalcreator incluse dans le plugin icalendar*/
-
+include_spip('action/editer_objet');
 function action_appliquer_decalage_dist() {
 
 //vérification de l'auteur en cours//
@@ -26,6 +26,23 @@ $arg = $securiser_action();
 	}
 
 	$id_almanach = $r[1];
+	$liens = sql_allfetsel('E.uid, E.id_evenement',
+		                        'spip_evenements AS E
+		                        INNER JOIN spip_almanachs_liens AS L
+		                        ON E.id_evenement = L.id_objet AND L.id_almanach='.intval($id_almanach),"E.horaire!=".sql_quote("non"));	
+
+	$decalage = intval(sql_fetsel("decalage","spip_almanachs","id_almanach=$id_almanach"));
+	
+	$champs_sql = array(
+		"date_debut" => "DATE_ADD(date_debut, INTERVAL  $decalage HOUR)",
+		"date_fin" => "DATE_ADD(date_fin, INTERVAL  $decalage HOUR)",
+	);
+	foreach ($liens as $l){
+		$id_evenement = intval($l["id_evenement"]);
+		autoriser_exception('evenement','modifier',$id_evenement);
+		objet_modifier('evenement',$id_evenement,$champs_sql);
+		autoriser_exception('evenement','modifier',$id_evenement,false);
+	}
 
 }
 
