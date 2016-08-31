4a178fa109f176b500a86f9be4855de67b0dc1cc
xdjuj@apsulis.com <xdjuj@apsulis.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-08-31 23:13:25 +0000

    Ne pas oublier de chaines de langue
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@99319 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- deplacer_rubriques/lang/deplacer_rubriques_fr.php
+++ deplacer_rubriques/lang/deplacer_rubriques_fr.php
@@ -4,11 +4,11 @@
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-	'action_deplacer'							=> "Déplacer les rubriques",
-/* Page de Configuration */
-	'configuration_deplacer_rubriques'			=> "D&eacute;placer Rubriques",
-	'titre_deplacer_rubriques'					=> "Déplacer des rubriques en nombre",
-	'label_restreindre_val_deplacer_rubriques'		=> "Rubrique(s) &agrave; d&eacute;placer",
-	'label_cible_deplacer_rubriques'		=> "Rubrique cible",
-	'valider_deplacement'	=> "Déplacer les rubriques",
+	'action_deplacer' => "Déplacer les rubriques",
+	'configuration_deplacer_rubriques' => "D&eacute;placer Rubriques",
+	'deplacer_rubriques_titre' => "Déplacement de rubriques",
+	'titre_deplacer_rubriques' => "Déplacer des rubriques en nombre",
+	'label_restreindre_val_deplacer_rubriques' => "Rubrique(s) &agrave; d&eacute;placer",
+	'label_cible_deplacer_rubriques' => "Rubrique cible",
+	'valider_deplacement' => "Déplacer les rubriques",
 );
