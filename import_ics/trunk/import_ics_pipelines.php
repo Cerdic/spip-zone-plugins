8d788c44a317cb39d2bda1568998b73c06c1aefe
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-09-01 09:45:11 +0000

    publier automatiquement les evenements lorsqu'on publie un almanach > une chose en moins sur la todolist
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@99326 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- import_ics/trunk/import_ics_pipelines.php
+++ import_ics/trunk/import_ics_pipelines.php
@@ -35,4 +35,48 @@ function import_ics_evenement_liaisons_colonne_gauche($flux){
 	$flux["data"]= $flux["data"].recuperer_fond("prive/objets/infos/evenement_liaisons_almanach",$flux['args']);
 	return $flux;
 }
-?>
+
+
+/**
+ * Synchroniser le statut des evenements lorsqu'on publie/depublie un almanach
+ * @param array $flux
+ * @return array
+ */
+function import_ics_post_edition($flux) {
+	if (isset($flux['args']['table'])
+		and $flux['args']['table']=='spip_almanachs'
+		and $flux['args']['action'] == 'instituer'
+		and $id_almanach = $flux['args']['id_objet']
+		and isset($flux['data']['statut'])
+		and $statut = $flux['data']['statut']
+		and $statut != $statut_ancien) {
+		$set = array();
+		switch ($statut) {
+			case 'poubelle':
+				// on passe aussi tous les evenements associes a la poubelle, sans distinction
+				$set['statut'] = 'poubelle';
+				break;
+			case 'publie':
+				// on passe aussi tous les evenements prop en publie
+				$set['statut'] = 'publie';
+				$where[] = "statut='prop'";
+				break;
+			case 'prop':
+				$set['statut'] = 'prop';
+				$where[] = "statut='publie'";
+				break;
+			}
+		if (count($set)) {
+			include_spip('action/editer_evenement');
+			$res = sql_select('E.id_evenement', 'spip_evenements AS E
+			INNER JOIN spip_almanachs_liens AS L
+			ON E.id_evenement = L.id_objet AND L.id_almanach='.intval($id_almanach), $where);
+			// et on applique a tous les evenements lies a l'almanach
+			while ($row = sql_fetch($res)) {
+				evenement_modifier($row['id_evenement'], $set);
+			}
+			sql_free($res);
+		}
+	}
+	return $flux;
+}
