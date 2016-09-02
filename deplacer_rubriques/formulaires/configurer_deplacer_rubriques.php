dc3aed1e737082db183498da693111037f68f5ba
guy.cesaro@gmail.com <guy.cesaro@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-09-02 14:03:56 +0000

    Quelques vérifications : la rubrique cible et les rubriques à déplacer doivent exister, la rubrique cible ne doit pas être dans les rubrique à déplacer.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@99346 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- deplacer_rubriques/formulaires/configurer_deplacer_rubriques.php
+++ deplacer_rubriques/formulaires/configurer_deplacer_rubriques.php
@@ -6,14 +6,41 @@ function formulaires_configurer_deplacer_rubriques_charger_dist(){
 
 function formulaires_configurer_deplacer_rubriques_verifier_dist(){
 		$erreurs = array();
-		foreach(array('rubriques_a_deplacer','rubrique_cible') as $obligatoire)
-				if (!_request($obligatoire)) $erreurs[$obligatoire] = 'Ce champ est obligatoire';
+		
+		foreach(array('rubriques_a_deplacer','rubrique_cible') as $obligatoire) {
+			if (!_request($obligatoire)) {
+				$erreurs[$obligatoire] = 'Ce champ est obligatoire';
+				$erreur_obligatoire = true;
+			}
+		}
+		//on ne va pas plus loin si les champs obligatoires ne sont pas remplis
+		if ($erreur_obligatoire)
+			return $erreurs;
 	   
-		if (_request('dry_run')=="oui")
-				$erreurs['dry_run'] = 'oui';
- 
+		if (_request('dry_run')== 'oui') $erreurs['dry_run'] = 'oui';
+			
+		
+		include_spip('base/abstract_sql');
+		$rubrique_cible = intval(_request('rubrique_cible'));
+		$rubriques_a_deplacer = explode(',' , _request('rubriques_a_deplacer'));
+		
+
+		// Vérifier que la rubrique cible existe
+		if (!sql_fetsel('1', "spip_rubriques", "id_rubrique=$rubrique_cible"))
+			$erreurs['rubrique_cible'] = _T('deplacer_rubrique:erreur_rubrique_cible_inexistante');
+		
+		// Vérifier que la rubrique cible n'est pas dans les rubriques à déplacer
+		if (in_array($rubrique_cible, $rubriques_a_deplacer))
+			$erreurs['rubrique_cible'] = _T('deplacer_rubrique:erreur_rubrique_cible_dans_rubriques_a_deplacer');
+
+		foreach ($rubriques_a_deplacer as $key => $value) {
+			if (!sql_fetsel('1', 'spip_rubriques', 'id_rubrique='.intval($value))){
+				$erreurs['rubriques_a_deplacer'] = _T('deplacer_rubrique:erreur_rubrique_inexistante', array('id_rubrique', intval($value)));
+			}
+		}
+		
 		if (count($erreurs))
-				$erreurs['message_erreur'] = 'Votre saisie contient des erreurs !';
+			$erreurs['message_erreur'] = 'Votre saisie contient des erreurs !';
 		return $erreurs;
 }
 
--- deplacer_rubriques/lang/deplacer_rubriques_fr.php
+++ deplacer_rubriques/lang/deplacer_rubriques_fr.php
@@ -4,11 +4,14 @@
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-	'action_deplacer' => "Déplacer les rubriques",
-	'configuration_deplacer_rubriques' => "D&eacute;placer Rubriques",
-	'deplacer_rubriques_titre' => "Déplacement de rubriques",
-	'titre_deplacer_rubriques' => "Déplacer des rubriques en nombre",
-	'label_restreindre_val_deplacer_rubriques' => "Rubrique(s) &agrave; d&eacute;placer",
-	'label_cible_deplacer_rubriques' => "Rubrique cible",
-	'valider_deplacement' => "Déplacer les rubriques",
+	'action_deplacer' => 'Déplacer les rubriques',
+	'configuration_deplacer_rubriques' => 'Déplacer Rubriques',
+	'deplacer_rubriques_titre' => 'Déplacement de rubriques',
+	'erreur_rubrique_cible_dans_rubriques_a_deplacer' => 'La rubrique cible ne peut pas être dans les rubriques à déplacer',
+	'erreur_rubrique_cible_inexistante' => 'La rubrique cible n\'existe pas',
+	'erreur_rubrique_inexistante' => 'La rubrique @id_rubrique@ n\'existe pas',
+	'titre_deplacer_rubriques' => 'Déplacer des rubriques en nombre',
+	'label_restreindre_val_deplacer_rubriques' => 'Rubrique(s) à déplacer',
+	'label_cible_deplacer_rubriques' => 'Rubrique cible',
+	'valider_deplacement' => 'Déplacer les rubriques',
 );
