dc9fc722031223587c37de87d762c3fd21763338
prigent.yohann@gmail.com <prigent.yohann@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-09-12 02:20:25 +0000

    On va le refaire… désolé :/ plus l’habitude d’SVN :(
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@99464 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- lang/paquet-multilinguisme_par_domaines_fr.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (!defined('_ECRIRE_INC_VERSION')) return;
-
-$GLOBALS[$GLOBALS['idx_lang']] = array(
-	'multilinguisme_par_domaines_description' => 'Pouvoir définir des nom de domaines différents pour chaque langue',
-	'multilinguisme_par_domaines_slogan' => 'Séparer les langues par nom de domaines',
-);
-?>
