1b6d097d5bbf13af9e0ebdbe6654b20d4cfeec97
prigent.yohann@gmail.com <prigent.yohann@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-09-12 02:21:57 +0000

    Import d’un plugin permettant de gérer un/plusieurs noms de domaine par langue, sur l’exemple de http://programmer.spip.net/Forcer-la-langue-selon-le-visiteur
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@99465 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ multilinguisme_par_domaines/lang/multilinguisme_par_domaines_fr.php
@@ -0,0 +1,11 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	'configuration_titre' => 'Multilinguisme par domaines',
+	'configuration_domaines' => 'Configuration des domaines',
+	'configuration_domaines_explication' => 'Indiquez pour chacune des langues, les domaines à utiliser (nom d\'hôte seulement), séparés par un saut de ligne',
+);
