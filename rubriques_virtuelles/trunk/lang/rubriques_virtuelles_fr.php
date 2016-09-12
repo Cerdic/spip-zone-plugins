b6a4fa46d0d772f66d649631c22e08898e226f77
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-09-12 18:03:45 +0000

    Un texte ok
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@99478 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- rubriques_virtuelles/trunk/lang/rubriques_virtuelles_fr.php
+++ rubriques_virtuelles/trunk/lang/rubriques_virtuelles_fr.php
@@ -8,5 +8,6 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// T
+	'texte_rubrique_virtuel' => 'Rubrique virtuelle',
 	'texte_rubrique_reference_mais_redirige' => 'rubrique référencée dans votre site, mais redirigée vers une autre URL.'
 );
