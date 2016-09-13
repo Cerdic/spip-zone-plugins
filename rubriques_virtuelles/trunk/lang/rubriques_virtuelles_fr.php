5766ed9d2ff9009c85f1ecd73ca3dcc6ff30b981
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-09-13 03:33:07 +0000

    [Salvatore] [source:_plugins_/rubriques_virtuelles/trunk/lang/ rubriques_virtuelles] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@99486 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- rubriques_virtuelles/trunk/lang/rubriques_virtuelles_fr.php
+++ rubriques_virtuelles/trunk/lang/rubriques_virtuelles_fr.php
@@ -1,14 +1,16 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/rubriques_virtuelles/lang
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/rubriques_virtuelles/trunk/lang
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+
 	// I
-	'info_renvoi_rubrique' => 'Cette rubrique renvoie à la page :',
+	'info_renvoi_rubrique' => 'Cette rubrique renvoie à la page :',
+
 	// T
-	'texte_rubrique_virtuel' => 'Rubrique virtuelle',
-	'texte_rubrique_reference_mais_redirige' => 'rubrique référencée dans votre site, mais redirigée vers une autre URL.'
+	'texte_rubrique_reference_mais_redirige' => 'rubrique référencée dans votre site, mais redirigée vers une autre URL.',
+	'texte_rubrique_virtuel' => 'Rubrique virtuelle'
 );
