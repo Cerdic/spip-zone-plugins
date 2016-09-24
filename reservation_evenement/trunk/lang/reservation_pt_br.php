c727f8f0573e123a50aabb94c3c0d669a1bf843d
rporto@calepino.com <rporto@calepino.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-09-24 03:36:04 +0000

    [Salvatore] [source:_plugins_/reservation_evenement/trunk/lang/ reservation] Export depuis http://trad.spip.net de la langue pt_br
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@99566 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- reservation_evenement/trunk/lang/reservation_pt_br.php
+++ reservation_evenement/trunk/lang/reservation_pt_br.php
@@ -11,38 +11,76 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
 	'affichage_par' => 'Exibir por:',
+	'ajouter_lien_reservation' => 'Incluir esta reserva',
 
 	// B
 	'bonjour' => 'Olá',
 
+	// C
+	'choix_precis' => 'Escolha estrita',
+	'complet' => 'Completo',
+	'cron_explication' => 'Para qualquer evento com o fechamento automático ativado. Quando o evento da reserva passar, a reserva será automaticamente fechada pelo sistema. Se "fechamento" for selecionado em "Disparador", um e-mail de fechamento será enviado. Limpe o cahce para que a função seja ativada corretamente.',
+	'cron_fieldset' => 'Fechamento automático',
+	'cron_label' => 'Fechar automaticamente uma reserva',
+
 	// D
+	'designation' => 'Designação',
 	'details_reservation' => 'Detalhes da reserva:',
 
 	// E
 	'erreur_email_utilise' => 'Esse email já está cadastrado. Favor conectar-se, ou inserir outro endereço de email',
-	'explication_envoi_separe' => 'A troca de status de Detalhe da Reserva para <div><b>"@statuts@"</b></div> gerará o envio de uma notificação!', # MODIF
+	'erreur_pas_evenement' => 'Não há nenhum evento com inscrição aberta, no momento.',
+	'evenement_cloture' => 'Evento encerrado',
+	'explication_client' => 'Escolha um cliente entre os autores ou informe os dados do cliente, abaixo',
+	'explication_email_reutilisable' => 'Permitir reutilizar um e-mail de um autor Spip no momento de uma reserva sem registro',
+	'explication_enregistrement_inscrit' => 'Registrar como autor Spip',
+	'explication_envoi_separe' => 'A troca de status de um Detalhe da Reserva para 
+<div><b>"@statuts@"</b></div> 
+gerará o envio de uma notificação!',
 	'explication_envoi_separe_detail' => 'A troca do status para <div><strong>"@statuts@"</strong></div> gerará o envio de uma notificação!',
+	'explication_login' => '<a rel="nofollow" class="login_modal" href="@url@" title="@titre_login@">Conecte-se</a> se você já tem cadastro no site',
 	'explication_nombre_evenements' => 'O número mínimo de reservas para que a promoção seja disponibilizada.',
-	'explication_nombre_evenements_choix' => 'Se vazio ou 0, este número é igual ao número de @objet_promotion@s escolhidos acima', # RELIRE
+	'explication_nombre_evenements_choix' => 'Se vazio ou 0, este número será igual ao número de @objet_promotion@s escolhidos acima',
+	'explication_objet_promotion' => 'Se definido no nível de matéria, serão incluídos todos os eventos disponíveis para a reserva da matéria.',
+
+	// F
+	'formulaire_public' => 'Formulário público',
 
 	// I
+	'icone_cacher' => 'Esconder',
 	'icone_creer_reservation' => 'Criar uma reserva',
+	'icone_modifier_client' => 'Alterar este cliente',
 	'icone_modifier_reservation' => 'Modificar esta reserva',
+	'info_1_client' => 'Um cliente',
 	'info_1_reservation' => 'Uma reserva',
+	'info_1_reservation_liee' => 'Uma reserva vinculada',
+	'info_aucun_client' => 'Nenhum cliente',
 	'info_aucun_reservation' => 'Nenhuma reserva',
+	'info_nb_clients' => '@nb@ clientes',
 	'info_nb_reservations' => '@nb@ reservas',
+	'info_nb_reservations_liees' => '@nb@ reservas vinculadas',
 	'info_reservations_auteur' => 'Reservas desse autor',
 	'info_voir_reservations_poubelle' => 'Ver reservas postas na lixeira',
+	'inscription' => 'Inscrição',
+	'inscrire' => 'Inscrever-se',
+	'inscrire_liste_attente' => 'Escolher outro curso ou se inscrever na lista de espera.',
 
 	// L
-	'label_date' => 'Data:', # RELIRE
+	'label_action_cloture' => 'Encerramento automático:',
+	'label_client' => 'Cliente:',
+	'label_date' => 'Data:',
 	'label_date_paiement' => 'Data do pagamento:',
-	'label_donnees_auteur' => 'Dados Autor:', # RELIRE
+	'label_donnees_auteur' => 'Dados Autor:',
 	'label_email' => 'Email:',
+	'label_email_reutilisable' => 'Permitir reutilizar um endereço de e-mail:',
+	'label_enregistrement_inscrit' => 'Permitir que o visitante se registre ao fazer uma reserva:',
+	'label_enregistrement_inscrit_obligatoire' => 'Tornar o registro obrigatório:',
 	'label_enregistrer' => 'Quero me cadastrar no site:',
 	'label_id_auteur' => 'Id autor:',
+	'label_inscription' => 'Inscrição:',
 	'label_lang' => 'Língua:',
-	'label_maj' => 'Gerado em:', # RELIRE
+	'label_maj' => 'Gerado em:',
+	'label_modifier_identifiants_personnels' => 'Alterar os dados pessoais:',
 	'label_mot_passe' => 'Senha:',
 	'label_mot_passe2' => 'Repita a senha:',
 	'label_nom' => 'Nome:',
@@ -53,29 +91,39 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'label_reference' => 'Referência:',
 	'label_reservation' => 'Reserva :',
 	'label_statut' => 'Status:',
+	'label_statut_calculer_auto' => 'Calcular automaticamente o status aceito da reserva:',
+	'label_statut_calculer_auto_explication' => 'Ao alterar o status para aceito, verificar se todos os detalhes de reserva estão com o status aceito, caso contrário, será definido o status aceito parcialmente para a reserva.',
 	'label_statut_defaut' => 'Status padrão :',
+	'label_statuts_complet' => 'O(s) status completo(s) :',
 	'label_type_paiement' => 'Tipo de pagamento:',
 	'label_type_selection' => 'Tipo de seleção:',
+	'legend_donnees_auteur' => 'Os dados do cliente',
+	'legend_donnees_reservation' => 'Os daods da reserva',
 
 	// M
 	'merci_de_votre_reservation' => 'Agradecemos a sua inscrição, que foi efetuada com sucesso.',
 	'message_erreur' => 'Seu preenchimento contém erros!',
+	'message_evenement_cloture' => 'O evento @titre@ acaba de encerrar. <br />Agradecemos pela sua participação.',
+	'message_evenement_cloture_vendeur' => 'O evento @titre@ acaba de encerrar. <br />O sistema acaba de enviar uma mensagem de encerramento para @client@ - @email@.',
 	'montant' => 'Montante',
 
 	// N
 	'nom_reservation_multiples_evenements' => 'Reserva de vários eventos',
-	'notifications_activer_explication' => 'Enviar por email notificações de reservas?', # RELIRE
+	'notifications_activer_explication' => 'Enviar as notificações de reservas por email?',
 	'notifications_activer_label' => 'Ativar',
 	'notifications_cfg_titre' => 'Notificações',
 	'notifications_client_explication' => 'Enviar notificações ao cliente?',
 	'notifications_client_label' => 'Cliente',
+	'notifications_destinataire_explication' => 'Escolher o(s) destinatário(s) das notificações',
+	'notifications_destinataire_label' => 'Destinatário',
 	'notifications_envoi_separe' => 'Ativar o modo Envio Separado para o status:',
 	'notifications_envoi_separe_explication' => 'Permite ativar separadamente o envio de notificações para cada Detalhe de Reserva',
 	'notifications_expediteur_administrateur_label' => 'Escolha um administrador:',
 	'notifications_expediteur_choix_administrateur' => 'um administrador',
 	'notifications_expediteur_choix_email' => 'um email',
+	'notifications_expediteur_choix_facteur' => 'idem plugin Facteur',
 	'notifications_expediteur_choix_webmaster' => 'um webmaster',
-	'notifications_expediteur_email_label' => 'Email do remetente:', # RELIRE
+	'notifications_expediteur_email_label' => 'Informar um e-mail:',
 	'notifications_expediteur_explication' => 'Escolher o remetente das notificações para o vendedor e o comprador',
 	'notifications_expediteur_label' => 'Remetente',
 	'notifications_expediteur_webmaster_label' => 'Escolher um webmaster:',
@@ -96,6 +144,9 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'par_articles' => 'matérias',
 	'par_evenements' => 'eventos',
 	'par_reservations' => 'reservas',
+	'periodicite_cron_explication' => 'Período após o qual o sistema verifica se as reservas deverão ser encerradas (min 600: 10 min)',
+	'periodicite_cron_label' => 'Periodicidade do cron em segundos',
+	'places_disponibles' => 'Lugares disponíveis:',
 
 	// R
 	'reaservation_montant' => 'Montante',
@@ -105,31 +156,42 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'reservation_client' => 'Cliente',
 	'reservation_date' => 'Data:',
 	'reservation_de' => 'Reserva de',
-	'reservation_enregistre' => 'Sua inscrição foi registrada com sucesso. Você receberá um email de confirmação. Se não chegar, verifique sua caixa de spam.', # MODIF
+	'reservation_enregistre' => 'Sua inscrição foi registrada com sucesso. Você receberá um email de confirmação. Se não chegar, verifique sua caixa de spam.',
 	'reservation_numero' => 'Reserva:',
 	'reservation_reference_numero' => 'Referência n° ',
+	'rubrique_reservation_explication' => 'Permite restringir o uso deste plugin a zona(s) específica(s)',
+	'rubrique_reservation_label' => 'Definir a(s) zona(s) de utilização deste plugin',
 
 	// S
 	'simple' => 'Simples',
 	'statuts_complet_explication' => 'Os status do detalhe da reserva levados em conta para determinar se o evento está completo.',
 	'sujet_une_reservation_accepte' => 'Reserva confirmada de @nom@',
+	'sujet_une_reservation_accepte_part' => 'Reserva parcialmente confirmada para @nom@',
+	'sujet_une_reservation_cloture' => 'Evento encerrado para @nom@',
 	'sujet_votre_reservation_accepte' => '@nom@ : confirmação de sua reserva',
+	'sujet_votre_reservation_accepte_part' => '@nom@: confirmação parcial da sua reserva',
+	'sujet_votre_reservation_cloture' => '@nom@: encerramento do evento',
 
 	// T
 	'texte_ajouter_reservation' => 'Adicionar uma reserva',
 	'texte_changer_statut_reservation' => 'Essa reserva está:',
 	'texte_exporter' => 'exportar',
 	'texte_statut_accepte' => ' aceito',
+	'texte_statut_accepte_part' => 'aceita parcialmente',
 	'texte_statut_attente' => ' em lista de espera',
 	'texte_statut_attente_paiement' => ' aguardando pagamento',
+	'texte_statut_cloture' => ' encerrado',
 	'texte_statut_encours' => ' em andamento',
 	'texte_statut_poubelle' => ' no lixo',
 	'texte_statut_refuse' => 'Resposta negada',
 	'texte_voir' => 'ver',
 	'titre_client' => 'Cliente',
+	'titre_clients' => 'Clientes',
 	'titre_envoi_separe' => 'Modo Envio Separado ativado',
 	'titre_reservation' => 'Reservas',
 	'titre_reservations' => 'Reservas',
+	'total' => 'Total',
+	'type_lien' => 'Vinculado com a reserva @reference@',
 
 	// U
 	'une_reservation_de' => 'Uma reserva de: ',
