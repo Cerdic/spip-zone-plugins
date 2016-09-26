cfdbd10abb6a277dcb41207561c804f1705b0b16
rporto@calepino.com <rporto@calepino.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-09-26 03:38:20 +0000

    [Salvatore] [source:_plugins_/socialtags/lang/ paquet-socialtags] Export depuis http://trad.spip.net de la langue pt_br
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@99605 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ socialtags/lang/paquet-socialtags_pt_br.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-socialtags?lang_cible=pt_br
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// S
+	'socialtags_description' => 'O plugin Social Tags permite incluir ícones de compartilhamento de links em sites como Digg, Facebook, Delicious...
+Além disso, se o plugin Google+1 estiver ativo, é possível incluir o botão correspondente na lista de links do Social tags. Oferece igualmente snippets configuráveis que permitem exibir os badges e boxes do tipo "Curtir" de Facebook.',
+	'socialtags_slogan' => 'Ativar os botões de compartilhamento de links nas redes sociais.'
+);
