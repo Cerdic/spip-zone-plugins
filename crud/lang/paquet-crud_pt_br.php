6be91fbe4381df58d9deda852a3d4d4ff843a22a
rporto@calepino.com <rporto@calepino.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-09-27 03:37:00 +0000

    [Salvatore] [source:_plugins_/crud/lang/ paquet-crud] Export depuis http://trad.spip.net de la langue pt_br
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@99645 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ crud/lang/paquet-crud_pt_br.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-crud?lang_cible=pt_br
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'crud_description' => 'Uma interface C(r)UD para o SPIP que pode ser usada como intermediária de uma ação definida pelo seu URL, ou por chamada direta.',
+	'crud_slogan' => 'Interface de criação, atualização e exclusão de um objeto'
+);
