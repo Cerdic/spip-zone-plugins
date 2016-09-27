16c5262a3048ec6404a3051a61da17c1be648fe3
rporto@calepino.com <rporto@calepino.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-09-27 03:37:21 +0000

    [Salvatore] [source:_plugins_/diogene/diogene/trunk/lang/ diogene] Export depuis http://trad.spip.net de la langue pt_br
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@99646 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ diogene/diogene/trunk/lang/diogene_pt_br.php
@@ -0,0 +1,178 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/diogene?lang_cible=pt_br
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'bouton_editer_diogene' => 'Templates de formulários',
+	'bouton_supprimer_diogene' => 'Excluir este template',
+
+	// C
+	'champ_date_publication' => 'Data de publicação',
+	'champ_date_publication_anterieure' => 'Data de publicação anterior',
+	'champ_forum' => 'Ativar / desativar os fóruns',
+	'choix_langue' => 'Idioma',
+
+	// D
+	'diogene' => 'Template de formulário',
+	'diogene_statuts' => 'Diogenes (Status)',
+	'diogenes' => 'Templates de formulários',
+
+	// E
+	'editer_diogene_explication' => 'Diogenes permite alterar os formulários de edição clássicos, removendo ou incluindo certos campos.',
+	'editer_diogenes_titre' => 'Os templates de formulários',
+	'erreur' => 'Erro',
+	'erreur_autorisation_login_publier' => 'Você precisa <a href="@url@" class="spip_in">estar identificado para publicar aqui</a>.',
+	'erreur_autorisation_modifier_site' => 'O seu status não permite alterar este site',
+	'erreur_autorisation_statut_publier' => 'O seu status não permite publicar neste site.',
+	'erreur_autorisation_statut_publier_limite' => 'Você atingiu a quantidade limite de objetos <a href="@url@">propostos para avaliação</a> (@nb@)',
+	'erreur_diogene_multiple_page' => 'Você só pode ter um único template "página" neste site',
+	'erreur_droits_objet_publier' => 'As suas credenciais não permitem a publicação deste tipo de objeto.',
+	'erreur_forums' => 'Erro na escolha da configuração dos fóruns',
+	'erreur_id_parent_id_rubrique' => 'Você não pode subordinar esta seção a ela mesma.',
+	'erreur_identifiant_existant' => 'Este ID já existe',
+	'erreur_objet_diogene_max' => 'O número máximo de máscaras (@max@) para este tipo de objeto (@objet@) já foi atingido.',
+	'erreur_objet_non_diogene' => 'Este tipo de objeto não é levado em conta (@objet@).',
+	'erreur_objet_publier' => 'O tipo de objeto que você pretende publicar não existe.',
+	'erreur_objet_publier_aucun' => 'Você não pode publicar nenhum objeto a partir desta página',
+	'erreur_secteur_diogene_inexistant' => 'Atenção, a seção relativa a este template não existe.',
+	'erreur_valeur_float' => 'O valor "@champ@" deve ser um número',
+	'erreur_valeur_int' => 'O valor "@champ@" deve ser um número fixo.',
+	'explication_article_deja_traduit' => 'Esta matéria já está traduzida em: ',
+	'explication_info_type' => 'Este ID deve ser único e não conter nenhum caracter especial',
+	'explication_nombre_attente' => 'Número máximo de objetos propostos para avaliação (não afeta os adminstradores, deixar 0 para ilimitado).',
+	'explication_rubrique_statut' => 'Uma seção é considerada visível (publicada) quando ela contém ao menos um outro objeto publicado.',
+	'explication_statut_auteur' => 'Este campo define o status mínimo para poder criar um objeto correspondente a este template.',
+	'explication_statut_auteur_publier' => 'Este campo define o status mínimo para poder publicar definitivamtente um objeto correspondente a este template.',
+	'explication_workflow_simplifie' => 'Apenas gerenciar as ações "Publicar" e "Excluir"',
+
+	// F
+	'formulaire_modifier_diogene' => 'Alterar o template (Diogenes)',
+	'formulaire_nouveau' => 'Novo template de formulário',
+
+	// I
+	'icone_editer_diogene_nouveau' => 'Novo template (@type@)',
+	'icone_modifier_diogene' => 'Alterar este template',
+	'icone_nouveau_diogene' => 'Criar um novo template',
+	'info_1_diogene' => '1 template',
+	'info_aucun_diogene' => 'Nenhum template',
+	'info_aucune_sous_rubrique' => 'Ainda não existe uma seção correspondente.',
+	'info_diogenes_rien_publie' => 'Você ainda não publicou nada.',
+	'info_menu_diogene' => 'Elemento do menu para o plugin Diogenes',
+	'info_nb_diogenes' => '@nb@ templates',
+	'info_numero_diogene' => 'Template número',
+	'info_publier_rubrique' => 'Publicar nesta seção:',
+	'info_referencement_automatise' => 'As informações de referenciamento automatizado',
+	'info_rubrique_new' => 'Nova seção vazia',
+	'info_rubrique_publie' => 'Seção visível',
+	'info_rubrique_vide' => 'Seção vazia',
+	'info_statut' => 'Status: ',
+	'info_syndication' => 'Sindicação',
+	'info_traduction_article' => 'Esta matéria é uma tradução de « <a href="@url@">@titre@</a> ».',
+	'info_type' => 'ID',
+
+	// L
+	'label_cacher_heure' => 'Hora',
+	'label_cacher_heure_case' => 'Não exibir a hora para as datas',
+	'label_cextras_enleves' => 'Campos extras a não exibir',
+	'label_champs_ajoutes' => 'Campos a incluir',
+	'label_champs_caches' => 'Campos a não exibir',
+	'label_change_statut_normal' => 'Alterar o status da sua matéria',
+	'label_change_statut_normal_site' => 'Alterar o status deste site',
+	'label_description' => 'Descrição',
+	'label_explications_logo' => 'Explicações do logo',
+	'label_limiter_rubriques' => 'Limitar às seções',
+	'label_limiter_secteur' => 'Limitar ao setor',
+	'label_logo_site' => 'Logo do site',
+	'label_menu' => 'Menu público',
+	'label_menu_case' => 'Exibir no menu público',
+	'label_nombre_attente' => 'Número máximo em espera',
+	'label_polyhier' => 'Poli-hierarquia',
+	'label_polyhier_desactiver' => 'Desativar a poli-hierarquia',
+	'label_rubrique_par_defaut' => 'Publicar por padrão na seção',
+	'label_statut_auteur' => 'Status mínimo dos autores',
+	'label_statut_auteur_publier' => 'Status mínimo dos autores que podem publicar definitivamente',
+	'label_workflow_simplifie' => 'Fluxo de trabalho simplificado',
+	'label_workflow_simplifie_case' => 'Ativar o gerenciamento simplificado do fluxo de publicação',
+	'legende_champs_diogene' => 'Campos do formulário',
+	'legende_selecteur_statut' => 'Status',
+	'legende_statuts_diogene' => 'Status',
+	'libelle_logo_diogene' => 'Logo do template',
+	'lien_creer' => 'Criar: ',
+	'lien_creer_version' => 'Criar uma versão em: @lang@',
+	'lien_editer_publication' => 'Editar esta publicação',
+	'lien_editer_rubrique' => 'Editar esta seção',
+	'lien_editer_site' => 'Editar este site',
+	'lien_modifier_version' => 'Altrar a versão em: @lang@',
+	'lien_modifier_version_originale' => 'Alterar a versão original em: @lang@',
+	'lien_publier_nouvelle_rubrique' => 'Publicar uma nova seção (@type@)',
+	'lien_version' => 'Versão: @lang@',
+
+	// M
+	'message_article_traduit_en' => 'Esta matéria já existe em:',
+	'message_confirm_depublier' => 'Você quer realmente despublicar?',
+	'message_confirm_poubelle' => 'Você quer realmente excluir?',
+	'message_confirm_sup' => 'Você quer realmente continuar a exclusão?',
+	'message_diogene_update' => 'O template de formulário foi atualizado',
+	'message_erreur_general' => 'O formulário contém erros, por favor, verifique o seu conteúdo.',
+	'message_id_parent_unique' => 'Este objeto só pode ser criado na seção «@rubrique@»',
+	'message_objet_cree' => '"@titre@" foi criado.',
+	'message_objet_mis_a_jour' => '"@titre@" foi atualizado.',
+	'message_objet_mis_a_jour_lien' => 'Você pode consultá-lo clicando <a href="@url@" class="spip_in">neste link</a>.',
+	'message_objet_supprime' => '"@titre@" foi excluído.',
+	'message_pas_auteur' => 'Atenção, você não está listado como autor deste objeto.',
+	'message_valider_action' => 'Você tem certeza?',
+	'message_valider_suppression' => 'Você que realmente escluir este template?',
+	'modifier_rubriques' => 'Alterar as seções',
+	'modifier_vos_objets' => 'Alterar as suas publicações',
+
+	// O
+	'option_statut_changer' => 'Alterar o status em: @statut@',
+	'option_statut_laisser' => 'Deixar o status em: @statut@',
+
+	// P
+	'publier_titre' => 'Publicar: ',
+	'publier_titre_hierarchie' => 'Publicar',
+
+	// S
+	'statuts' => 'Status',
+
+	// T
+	'texte_choix_rubrique' => 'Publicar na seção',
+	'texte_statut_en_cours_poubelle_normal' => 'Deixar a sua matéria na lixeira',
+	'texte_statut_en_cours_poubelle_site' => 'Deixar este site na lixeira',
+	'texte_statut_en_cours_prop_normal' => 'Deixar a sua matéria proposta para publicação',
+	'texte_statut_en_cours_prop_site' => 'Deixar este site proposto para publicação',
+	'texte_statut_en_cours_publie_normal' => 'Deixar a sua matéria publicada',
+	'texte_statut_en_cours_publie_site' => 'Deixar este site publicado',
+	'texte_statut_en_cours_redaction' => 'Deixar a sua matéria em fase de redação',
+	'texte_statut_poubelle_normal' => 'Colocar a sua matéria na lixeira',
+	'texte_statut_poubelle_normal_pas_auteur' => 'Colocar esta matéria na lixeira',
+	'texte_statut_poubelle_site' => 'Colocar este site na lixeira',
+	'texte_statut_prop_normal' => 'Propos a sua matéria para publicação',
+	'texte_statut_prop_site' => 'Propor este site para publicação',
+	'texte_statut_publie_normal' => 'Publicar definitivamente a sua matéria',
+	'texte_statut_publie_site' => 'Publicar definitivamente este site',
+	'texte_statut_redaction' => 'Colocar a sua matéria em fase de redação',
+	'texte_statut_redaction_sans_statut' => 'Colocar a sua matéria em fase de redação',
+	'title_page_publier' => 'Publicar',
+	'title_page_publier_titre' => 'Publicar: @titre@',
+	'titre_lien_publier' => 'Publicar',
+	'titre_modification_article' => 'Alteração da matéria: @titre@',
+	'titre_modification_rubrique' => 'Alteração da seção: @titre@',
+	'titre_modification_site' => 'Alteração do site: @titre@',
+	'titre_modifier_article' => 'Alterar esta publicação',
+	'titre_modifier_publication' => 'Alterar esta publicação',
+	'titre_modifier_rubrique' => 'Alterar esta seção',
+	'titre_modifier_site' => 'Alterar este site',
+	'titre_publier_nouveau_page' => 'Um novo objeto',
+	'titre_publier_proposes' => 'Um objeto proposto (@nb@)',
+	'titre_publier_proposes_page' => 'Um objeto proposto',
+	'titre_publier_proposes_page_vous' => 'Os seus objetos propostos'
+);
