2e5d5018072b96f53ad6a8255bbf8da8ddfc3914
rporto@calepino.com <rporto@calepino.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-09-27 03:36:40 +0000

    [Salvatore] [source:_plugins_/crud/lang/ crud] Export depuis http://trad.spip.net de la langue pt_br
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@99644 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ crud/lang/crud_pt_br.php
@@ -0,0 +1,24 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/crud?lang_cible=pt_br
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// E
+	'erreur_action_erronee' => 'CRUD: ação @action@ incorreta',
+	'erreur_action_inconnue_table' => 'CRUD: ação @action@ desconhecida para a tabela @table@',
+	'erreur_article_inconnue' => 'CRUD: a matéria @id@ não existe',
+	'erreur_creation' => 'CRUD: não foi possível criar o objeto do tipo "@objet@" (verifique as suas credenciais)',
+	'erreur_info_obligatoire' => 'CRUD: o campo @info@ é obrigatório',
+	'erreur_objet_inexistant' => 'CRUD: o objeto @objet@ #@id_objet@ não existe',
+	'erreur_rubrique_inconnue' => 'CRUD: a seção @id@ não existe',
+	'erreur_suppression' => 'CRUD: erro de exclusão do objeto "@objet@" #@id_objet@ (verifique as suas credenciais)',
+	'erreur_table_erronee' => 'CRUD: tabela @table@ incorreta',
+	'erreur_table_inconnue' => 'CRUD: tabela @table@ desconhecida',
+	'erreur_update' => 'CRUD: erro de atualização do objeto "@objet@" #@id@ (verifique as suas credenciais)'
+);
