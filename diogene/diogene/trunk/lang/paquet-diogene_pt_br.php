fac5af837893f822e8f191a7f88efb4ccc97d410
rporto@calepino.com <rporto@calepino.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-09-27 03:37:46 +0000

    [Salvatore] [source:_plugins_/diogene/diogene/trunk/lang/ paquet-diogene] Export depuis http://trad.spip.net de la langue pt_br
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@99647 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ diogene/diogene/trunk/lang/paquet-diogene_pt_br.php
@@ -0,0 +1,19 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-diogene?lang_cible=pt_br
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// D
+	'diogene_description' => 'O plugin Diogenes permite criar máscaras de formulários especiais por setor, para os três objetos específicos:
+-* matérias;
+-* seções;
+-* sites
+
+Permite definir, em função de um setor particular, uma máscara de formulário por objeto, incluindo ou excluindo campos, para adequar o formulário de edição ao que se pretende realmente publicar nesses setores, mas igualmente agrupar num mesmo formulário de edição todas as informações necessárias para publicar com rapidez.'
+);
