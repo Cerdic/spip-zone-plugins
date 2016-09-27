73728a738b96d510fec29cd803d79802268a5a99
willmann@netzsachen.de <willmann@netzsachen.de@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-09-27 03:34:43 +0000

    [Salvatore] [source:_plugins_/albums/trunk/lang/ paquet-albums] Export depuis http://trad.spip.net de la langue de
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@99639 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ albums/trunk/lang/paquet-albums_de.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-albums?lang_cible=de
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'albums_nom' => 'Alben',
+	'albums_slogan' => 'Verwaltung von Dokumentengruppen in Form von Alben'
+);
