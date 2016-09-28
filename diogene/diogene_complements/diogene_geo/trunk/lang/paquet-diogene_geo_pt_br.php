1e696869f8999bf6b790de1c67e228958c7ab0b8
rporto@calepino.com <rporto@calepino.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-09-28 03:35:10 +0000

    [Salvatore] [source:_plugins_/diogene/diogene_complements/diogene_geo/trunk/lang/ paquet-diogene_geo] Export depuis http://trad.spip.net de la langue pt_br
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@99676 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ diogene/diogene_complements/diogene_geo/trunk/lang/paquet-diogene_geo_pt_br.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-diogene_geo?lang_cible=pt_br
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// D
+	'diogene_geo_description' => 'Incluir a possibilidade de selecionar a posição geográfica de um objeto a partir do seu formulário de edição, se esta opção estiver ativa na máscara de formulário do Diogenes',
+	'diogene_geo_nom' => 'Diogenes - Geo',
+	'diogene_geo_slogan' => 'Complemento geográfico para o "Diogenes"'
+);
