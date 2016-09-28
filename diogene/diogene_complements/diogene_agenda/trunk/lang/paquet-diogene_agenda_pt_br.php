c0a1384d737c36b8cf33af655496b6883a54e3df
rporto@calepino.com <rporto@calepino.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-09-28 03:34:12 +0000

    [Salvatore] [source:_plugins_/diogene/diogene_complements/diogene_agenda/trunk/lang/ paquet-diogene_agenda] Export depuis http://trad.spip.net de la langue pt_br
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@99672 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ diogene/diogene_complements/diogene_agenda/trunk/lang/paquet-diogene_agenda_pt_br.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-diogene_agenda?lang_cible=pt_br
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// D
+	'diogene_agenda_description' => 'Adicionar a possibilidade de incluir um evento a uma matéria a partir do seu próprio formulário.',
+	'diogene_agenda_nom' => 'Diogenes - Agenda',
+	'diogene_agenda_slogan' => 'Complemento agenda para o "Diogenes"'
+);
