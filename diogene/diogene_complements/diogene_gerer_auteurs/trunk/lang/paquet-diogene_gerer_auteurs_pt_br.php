dcc2d3e470ee6ea376f0828387278f522c3b930e
rporto@calepino.com <rporto@calepino.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-09-28 03:35:38 +0000

    [Salvatore] [source:_plugins_/diogene/diogene_complements/diogene_gerer_auteurs/trunk/lang/ paquet-diogene_gerer_auteurs] Export depuis http://trad.spip.net de la langue pt_br
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@99678 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ diogene/diogene_complements/diogene_gerer_auteurs/trunk/lang/paquet-diogene_gerer_auteurs_pt_br.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-diogene_gerer_auteurs?lang_cible=pt_br
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// D
+	'diogene_gerer_auteurs_description' => 'Permite incluir via Diogenes um campo para associar ou dissociar um ou mais autores num formulário de edição de objeto.',
+	'diogene_gerer_auteurs_nom' => 'Diogenes - Autores',
+	'diogene_gerer_auteurs_slogan' => 'Complemento "autores" para o "Diogenes"'
+);
