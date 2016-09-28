7d60a2c44b9d3d3ad158be6e5b35d67c483a6d69
rporto@calepino.com <rporto@calepino.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-09-28 03:34:40 +0000

    [Salvatore] [source:_plugins_/diogene/diogene_complements/diogene_documents/trunk/lang/ paquet-diogene_documents] Export depuis http://trad.spip.net de la langue pt_br
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@99674 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ diogene/diogene_complements/diogene_documents/trunk/lang/paquet-diogene_documents_pt_br.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-diogene_documents?lang_cible=pt_br
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// D
+	'diogene_documents_description' => 'Adicionar a possibilidade de incluir documentos em uma matéria a partir do seu próprio formulário.',
+	'diogene_documents_nom' => 'Diogenes - Documentos',
+	'diogene_documents_slogan' => 'Complemento documentos para o "Diogenes"'
+);
