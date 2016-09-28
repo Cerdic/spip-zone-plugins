70b93b38b52c1c8519167a33eb22441e6b3de52c
rporto@calepino.com <rporto@calepino.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-09-28 03:36:23 +0000

    [Salvatore] [source:_plugins_/diogene/diogene_complements/diogene_mots/trunk/lang/ diogene_mots] Export depuis http://trad.spip.net de la langue pt_br
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@99681 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ diogene/diogene_complements/diogene_mots/trunk/lang/diogene_mots_pt_br.php
@@ -0,0 +1,27 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/diogene_mots?lang_cible=pt_br
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// E
+	'erreur_confirmer_creation_mots_nouveaux' => 'Confirmar mesmo assim a criação neste grupo?',
+	'erreur_mot_dans_autre_groupe' => 'A palavra @mot@ já existe no grupo @groupe@. ',
+
+	// F
+	'form_legend' => 'Seletor de palavras-chave',
+	'form_legend_public' => 'Palavras-chave',
+
+	// L
+	'label_montrer_titre_et_descriptif' => 'Descrição',
+	'label_montrer_titre_et_descriptif_case' => 'Se existir, exibir a descrição da palavra entre parêntesis',
+	'label_mots_creer_dans_public' => 'Permite a criação de novas palavras neste grupos',
+	'label_mots_facultatifs' => 'Grupos de palavras opcionais',
+	'label_mots_obligatoires' => 'Grupos de palavras obrigatórias',
+	'limiter_usage_mots' => 'Limitar a uma lista de palavras para o grupo « @groupe@ »'
+);
