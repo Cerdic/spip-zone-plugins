19af2bc95e399bb15e3357c05c61ffcebb6c99a8
rporto@calepino.com <rporto@calepino.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-09-28 03:35:54 +0000

    [Salvatore] [source:_plugins_/diogene/diogene_complements/diogene_licence/trunk/lang/ diogene_licence] Export depuis http://trad.spip.net de la langue pt_br
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@99679 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ diogene/diogene_complements/diogene_licence/trunk/lang/diogene_licence_pt_br.php
@@ -0,0 +1,17 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/diogene_licence?lang_cible=pt_br
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// F
+	'form_legend' => 'Licença',
+
+	// L
+	'label_licence_defaut' => 'Licença padrão'
+);
