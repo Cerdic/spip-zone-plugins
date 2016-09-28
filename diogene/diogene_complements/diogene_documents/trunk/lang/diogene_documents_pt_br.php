f40b1030adad05b8e817ecb8dd14be83216bb5f8
rporto@calepino.com <rporto@calepino.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-09-28 03:34:26 +0000

    [Salvatore] [source:_plugins_/diogene/diogene_complements/diogene_documents/trunk/lang/ diogene_documents] Export depuis http://trad.spip.net de la langue pt_br
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@99673 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ diogene/diogene_complements/diogene_documents/trunk/lang/diogene_documents_pt_br.php
@@ -0,0 +1,23 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/diogene_documents?lang_cible=pt_br
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// E
+	'explication_documents_un_par_un' => 'Exibir tantos botões navegar quanto o número máximo de documentos (se superior a 1).',
+
+	// L
+	'label_ajouter_nouveaux_documents' => 'Incluir documentos',
+	'label_champs_documents' => 'Campos associados aos documentos',
+	'label_documents_un_par_un' => 'Transferir os documentos um a um',
+	'label_editer_documents_articles' => 'Editar os documentos vinculados',
+	'label_nombre_documents' => 'Número máximo de documentos (0 para ilimitado)',
+	'legende_documents' => 'Documentos',
+	'lien_supprimer_document' => 'Excluir este documento'
+);
