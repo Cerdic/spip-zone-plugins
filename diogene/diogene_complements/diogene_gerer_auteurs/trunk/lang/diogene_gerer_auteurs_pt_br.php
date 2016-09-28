b9fba476e7596c6e27ef1171b732ce0d60aba6f7
rporto@calepino.com <rporto@calepino.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-09-28 03:35:24 +0000

    [Salvatore] [source:_plugins_/diogene/diogene_complements/diogene_gerer_auteurs/trunk/lang/ diogene_gerer_auteurs] Export depuis http://trad.spip.net de la langue pt_br
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@99677 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ diogene/diogene_complements/diogene_gerer_auteurs/trunk/lang/diogene_gerer_auteurs_pt_br.php
@@ -0,0 +1,25 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/diogene_gerer_auteurs?lang_cible=pt_br
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// E
+	'explication_cfg_ajout_auteurs' => 'Incluir a possibilidade de adicionar / excluir os autores nas matérias criadas (usando o plugin "Diogenes - Autores")',
+	'explication_diogene_gerer_auteurs' => 'Para incluir uma outra pessoa como autor, ela deve ser membro do site.',
+
+	// F
+	'form_legend' => 'Os autores',
+
+	// L
+	'label_cfg_ajout_auteurs' => 'Inclusão/ exclusão de autores',
+	'label_diogene_gerer_auteurs' => 'Inclusão e exclusão de autor(es)',
+
+	// M
+	'message_impossible_supprimer_auteur' => 'Você não pode se excluir da lista de autores. Você perderia os seus direitos de edição.'
+);
