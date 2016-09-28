1b1fcb90c9ae481961f0887d55c0c1903db939cb
rporto@calepino.com <rporto@calepino.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-09-28 03:33:57 +0000

    [Salvatore] [source:_plugins_/diogene/diogene_complements/diogene_agenda/trunk/lang/ diogene_agenda] Export depuis http://trad.spip.net de la langue pt_br
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@99671 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ diogene/diogene_complements/diogene_agenda/trunk/lang/diogene_agenda_pt_br.php
@@ -0,0 +1,27 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/diogene_agenda?lang_cible=pt_br
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// E
+	'evenement_supprimer' => 'Excluir o evento',
+
+	// F
+	'form_legend_agenda' => 'Evento',
+
+	// L
+	'label_agenda_caches' => 'Campos da agenda não exibidos',
+	'label_agenda_legende' => 'Legenda da parte "evento" do formulário',
+	'label_agenda_multiple' => 'Permitir a vinculação de vários eventos a uma matéria',
+	'label_agenda_obligatoire' => 'O evento é obrigatório',
+	'label_cfg_ajout_agenda' => 'Evento',
+
+	// T
+	'titre_evenements_lies' => 'Eventos vinculados'
+);
