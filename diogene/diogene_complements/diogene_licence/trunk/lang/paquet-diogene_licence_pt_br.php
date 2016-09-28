531ab6a0397d61e33ca454428fc0fcd89610f0c7
rporto@calepino.com <rporto@calepino.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-09-28 03:36:09 +0000

    [Salvatore] [source:_plugins_/diogene/diogene_complements/diogene_licence/trunk/lang/ paquet-diogene_licence] Export depuis http://trad.spip.net de la langue pt_br
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@99680 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ diogene/diogene_complements/diogene_licence/trunk/lang/paquet-diogene_licence_pt_br.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-diogene_licence?lang_cible=pt_br
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// D
+	'diogene_licence_description' => 'Incluir a possibilidade de selecionar a licença de um objeto a partirdo seu formulário de edição, se esta opcão estiver ativa na máscara de formulário do Diogenes',
+	'diogene_licence_nom' => 'Diogenes - Licença',
+	'diogene_licence_slogan' => 'Complemento "licença" para o "Diogenes"'
+);
