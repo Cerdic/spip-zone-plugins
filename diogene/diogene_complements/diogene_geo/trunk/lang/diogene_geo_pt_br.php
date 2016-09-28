1442e9cf1f4f069d938eeb87d2dccb9035c6db18
rporto@calepino.com <rporto@calepino.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-09-28 03:34:55 +0000

    [Salvatore] [source:_plugins_/diogene/diogene_complements/diogene_geo/trunk/lang/ diogene_geo] Export depuis http://trad.spip.net de la langue pt_br
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@99675 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ diogene/diogene_complements/diogene_geo/trunk/lang/diogene_geo_pt_br.php
@@ -0,0 +1,46 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/diogene_geo?lang_cible=pt_br
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'adresse' => 'Endereço',
+
+	// F
+	'form_legend' => 'Informações geográficas',
+
+	// G
+	'gis_info_descriptif' => 'Descrição do ponto',
+	'gis_info_titre' => 'Título do ponto',
+
+	// L
+	'label_geo_afficher' => 'Exibir as informações de geolocalização',
+	'label_geo_cacher' => 'Ocultar o mapa por padrão',
+	'label_geo_cacher_defaut' => 'Ocultar o mapa e os campos de geolocalização por padrão',
+	'label_geo_forcer_existant' => 'Forçar a seleção de um ponto',
+	'label_geo_forcer_existant_defaut' => 'Impor a escolha de um ponto geolocalizado preexistente no site (não exibirá o mapa)',
+	'label_recherche' => 'Exibir um campo de busca no mapa.',
+	'label_supprimer_gis' => 'Excluir as informações geográficas vinculadas',
+	'latitude' => 'Latitude',
+	'longitude' => 'Longitude',
+
+	// M
+	'message_article_geolocaliser' => 'Geolocalizar esta página',
+	'message_postion_auto' => 'A sua mídia foi posicionada automaticamente, de acordo com os seus metadados',
+
+	// N
+	'non_localisable' => 'Não localizável',
+	'non_localisable_case' => 'Esta mídia não é localizável',
+
+	// R
+	'recherche' => 'Busca',
+
+	// Z
+	'zoom' => 'Zoom'
+);
