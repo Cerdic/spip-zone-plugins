f2fe2a843dca240705f0623f20bdd2fd6597fc28
rporto@calepino.com <rporto@calepino.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-09-28 03:38:55 +0000

    [Salvatore] [source:_plugins_/nospam/lang/ paquet-nospam] Export depuis http://trad.spip.net de la langue pt_br
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@99691 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ nospam/lang/paquet-nospam_pt_br.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-nospam?lang_cible=pt_br
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// N
+	'nospam_description' => 'Alto ao spam! Filtre os spams nas mensagens, sem incomodar os usuários legítimos.',
+	'nospam_slogan' => 'Limitar o risco de spams nos fóruns'
+);
