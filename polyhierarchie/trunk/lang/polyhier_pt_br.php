d90f8d4263fc72e6e522ad239641cdf68d047174
rporto@calepino.com <rporto@calepino.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-09-28 03:39:11 +0000

    [Salvatore] [source:_plugins_/polyhierarchie/trunk/lang/ polyhier] Export depuis http://trad.spip.net de la langue pt_br
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@99692 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ polyhierarchie/trunk/lang/polyhier_pt_br.php
@@ -0,0 +1,28 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/polyhier?lang_cible=pt_br
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'articles_associes' => 'Matérias vinculadas',
+
+	// L
+	'label_autre_parent' => 'Igualmente na seção',
+	'label_autres_parents' => 'Igualmente nas seções',
+	'label_parents' => 'Nas seções',
+
+	// P
+	'parent_obligatoire' => 'Você deve indicar pelo menos uma seção',
+
+	// R
+	'rubriques_associees' => 'Seções vinculadas',
+
+	// T
+	'titre_polyhierarchie' => 'Poli-hierarquia'
+);
