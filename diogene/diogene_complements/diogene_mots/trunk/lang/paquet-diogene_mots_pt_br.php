9662377cd4f9708c426437b9bd90ed8a3b30efe9
rporto@calepino.com <rporto@calepino.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-09-28 03:36:37 +0000

    [Salvatore] [source:_plugins_/diogene/diogene_complements/diogene_mots/trunk/lang/ paquet-diogene_mots] Export depuis http://trad.spip.net de la langue pt_br
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@99682 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ diogene/diogene_complements/diogene_mots/trunk/lang/paquet-diogene_mots_pt_br.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-diogene_mots?lang_cible=pt_br
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// D
+	'diogene_mots_description' => 'Complemento de palavras-chave para o "Diogenes"',
+	'diogene_mots_nom' => 'Diogenes - Palavras',
+	'diogene_mots_slogan' => 'Complemento palavras-chave para o "Diogenes"'
+);
