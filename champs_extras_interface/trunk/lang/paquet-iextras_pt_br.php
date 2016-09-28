1d40a8a01a2737b15bf5d4ba2ae8793a564a72f5
rporto@calepino.com <rporto@calepino.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-09-28 03:33:13 +0000

    [Salvatore] [source:_plugins_/champs_extras_interface/trunk/lang/ paquet-iextras] Export depuis http://trad.spip.net de la langue pt_br
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@99668 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ champs_extras_interface/trunk/lang/paquet-iextras_pt_br.php
@@ -0,0 +1,17 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-iextras?lang_cible=pt_br
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// I
+	'iextras_description' => 'Inclui, na área restrita, uma interface completa de gerenciamento de campos suplementares nos objetos editoriais.',
+	'iextras_nom' => 'Campos Extras (Interface)',
+	'iextras_slogan' => 'Oferece uma interface gráfica para gerenciar os campos extras',
+	'iextras_titre' => 'Campos Extras (Interface)'
+);
