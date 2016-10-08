354832c48febbd13711bf9e93b90335969841d6a
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-10-08 03:34:19 +0000

    [Salvatore] [source:_plugins_/spip_hop/trunk/lang/ spip_hop] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@99861 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spip_hop/trunk/lang/spip_hop_en.php
+++ spip_hop/trunk/lang/spip_hop_en.php
@@ -1,4 +1,7 @@
 <?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/spip_hop?lang_cible=en
+// ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
@@ -6,26 +9,23 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-	// A
-	'important' => 'Important :',
-
-	//E
+	// E
 	'etendre_selection_bandeau' => 'Do you wish to keep the initial banner width ?',
-
-	'explications' => 'The value must be followed by a unit : em, rem, px, %, pt.',
-
-	'info_constante_definie' => 'A value has been defined for this field in a file <em>xxx_options.php</em>',
+	'explications' => 'The value must be followed by a unit : em, rem, px, %, pt.',
 
 	// F
 	'forcer_grand_ecran' => 'Force large display for every author ?',
 
+	// I
+	'important' => 'Important :',
+	'info_constante_definie' => 'A value has been defined for this field in a file <em>xxx_options.php</em>',
+
 	// O
 	'options' => 'Options',
 
-	//P
+	// P
 	'parametres' => 'Setting (Currently set to: "@ecran@")',
 
-	//T
-	'titre_page_configurer_spip_hop' => 'Settings for width display',
-
+	// T
+	'titre_page_configurer_spip_hop' => 'Settings for width display'
 );
