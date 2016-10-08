d17144b8b54007ba2c6ca875571a4dac982fbcb6
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-10-08 03:34:35 +0000

    [Salvatore] [source:_plugins_/spip_hop/trunk/lang/ paquet-spip_hop] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@99862 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spip_hop/trunk/lang/paquet-spip_hop_fr.php
+++ spip_hop/trunk/lang/paquet-spip_hop_fr.php
@@ -1,14 +1,14 @@
 <?php
-
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/spip_hop/trunk/lang
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-	// A
-	'spip_hop_description' => 'Ce plugin va agrandir la largeur de la page dans l\'espace privé de SPIP.',
+	// S
+	'spip_hop_description' => 'Ce plugin va agrandir la largeur de la page dans l’espace privé de SPIP.',
 	'spip_hop_nom' => 'Agrandir la largeur de page',
-	'spip_hop_slogan' => 'Elargissez votre champ d\'action !',
+	'spip_hop_slogan' => 'Elargissez votre champ d’action !'
 );
