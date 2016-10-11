3087738f4d8ada4eda66ed80815bc366255f9f95
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-10-11 11:52:07 +0000

    PSR SPIP
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@99888 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- article_accueil/trunk/base/article_accueil.php
+++ article_accueil/trunk/base/article_accueil.php
@@ -5,17 +5,16 @@
  * Distribue sous licence GPL
  *
  */
-if (!defined("_ECRIRE_INC_VERSION")) return;
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 /**
  * Declaration des tables principales
  *
  * @param array $tables_principales
  * @return array
  */
-function article_accueil_declarer_tables_principales($tables_principales){
-	
+function article_accueil_declarer_tables_principales($tables_principales) {
 	$tables_principales['spip_rubriques']['field']['id_article_accueil'] = "bigint(21) DEFAULT '0' NOT NULL";
 	return $tables_principales;
 }
-
-?>
