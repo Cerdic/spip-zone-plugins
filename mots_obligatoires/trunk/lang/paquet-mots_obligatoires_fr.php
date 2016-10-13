b3fbeb429ed880cd7f8e0047492a7e0a917fa2a3
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-10-13 17:49:42 +0000

    paquet.xml minimal
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@99931 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ mots_obligatoires/trunk/lang/paquet-mots_obligatoires_fr.php
@@ -0,0 +1,11 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/import_ics/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	'mots_obligatoires_description' => 'Ce plugin empêche la publication d\'objets (articles, documents etc.) auquel aucun mot clef d\'un groupe marqué comme important ne sont attribués.',
+	'mots_obligatoires_slogan' => 'Ne publier que les objets correctement étiquetés'
+);
