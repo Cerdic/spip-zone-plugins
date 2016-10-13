bb76a4be3caf82f510cd5bcd920f6da20a89010e
bystrano@gmx.ch <bystrano@gmx.ch@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-10-13 14:27:03 +0000

    Traduction du plugin en anglais.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@99925 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ massicot/trunk/lang/massicot_en.php
@@ -0,0 +1,28 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/massicot/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// E
+	'erreur_image_trop_petite' => 'This image is too small for this preset.',
+	'erreur_parametre_manquant' => 'The parameter @parametre@ is mandatory !',
+
+	// L
+	'label_annuler' => 'Cancel',
+	'label_dimensions' => 'Size of the resulting cropped image : ',
+	'label_format' => 'size presets : ',
+
+	// M
+	'massicot_titre' => 'Image cropper',
+	'massicoter' => 'Crop the image',
+
+	// R
+	'reinitialiser' => 'Reset',
+
+	// Z
+	'zoom' => 'Zoom'
+);
---
+++ massicot/trunk/lang/paquet-massicot_en.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/massicot/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// M
+	'massicot_description' => 'This plugin adds a crop & resize functionality to logos and images in the private area.',
+	'massicot_nom' => 'Image cropper',
+	'massicot_slogan' => 'Crop and resize your images in SPIP!'
+);
