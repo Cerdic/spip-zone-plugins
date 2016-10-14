6ad77bbe61a0a3b8b6541c57b244d11f67c417af
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-10-14 12:26:09 +0000

    PSR SPIP
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@99941 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- menus/trunk/inc/menus_autoriser.php
+++ menus/trunk/inc/menus_autoriser.php
@@ -1,19 +1,21 @@
 <?php
 
 // Sécurité
-if (!defined("_ECRIRE_INC_VERSION")) return;
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
-function menus_autoriser(){}
+function menus_autoriser() {
+}
 
 function autoriser_menu_modifier_dist($faire, $type, $id, $qui, $opt) {
-	if ($qui['statut'] == '0minirezo' and !$qui['restreint'])
+	if ($qui['statut'] == '0minirezo' and !$qui['restreint']) {
 		return true;
-	else
+	} else {
 		return false;
+	}
 }
 
 function autoriser_menus_tous_dist($faire, $type, $id, $qui, $opt) {
 	return autoriser('modifier', 'menu', $id, $qui, $opt);
 }
-
-?>
