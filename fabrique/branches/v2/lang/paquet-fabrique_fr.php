29b828f212075f850731854bc6cd959e81250fd2
marcimat@rezo.net <marcimat@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-10-21 08:15:05 +0000

    Faisons une branche finalement, car de futures modifications du trunk se révèlent plus longues et fastidieuses que prévues.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@100009 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ fabrique/branches/v2/lang/paquet-fabrique_fr.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/fabrique/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// F
+	'fabrique_description' => 'Ce plugin permet de générer le code de plugins pour SPIP.
+		Il peut gérer la création d’objets éditoriaux simples. L’objectif n’est pas de faire créer un
+		plugin parfait, mais une base solide, rapide à mettre en place, à éditer ensuite à votre goût.',
+	'fabrique_slogan' => 'Outil pour créer des plugins SPIP.',
+	'fabrique_titre' => 'Fabrique'
+);
