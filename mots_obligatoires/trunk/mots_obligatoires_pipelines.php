4faf72126303909151e9fa4903ac32ef8b6c70c5
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-10-21 11:37:33 +0000

    rendre compatible avec le plugin motus
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@100031 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- mots_obligatoires/trunk/mots_obligatoires_pipelines.php
+++ mots_obligatoires/trunk/mots_obligatoires_pipelines.php
@@ -6,20 +6,31 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 
 function mots_obligatoires_formulaire_verifier($flux){
 	$statut = _request ('statut');
+
 	if ($flux['args']['form'] == 'instituer_objet' and $statut == 'publie'){// seulement si on publie
 		$groupes_erreur = array();// stocker les groupes qui posent problème
 		$objet = $flux['args']['args'][0];
 		$id_objet = $flux['args']['args'][1];
 		
+		$info_plugin = chercher_filtre('info_plugin');
+		$motus = $info_plugin('motus','est_actif');
+
 		// Récupérer les groupes de mot clefs 
 		// 1. Liés à cet objet 
 		// 2. Marqués comme important
+		// 3. Si MOTUS chargé, concerné par la rubrique en cours
 		$groupes = sql_select('id_groupe,titre','spip_groupes_mots',
 			array("obligatoire=".sql_quote('oui'),
 			"tables_liees LIKE ".sql_quote("$objet%")
 		));
 		while ($groupe = sql_fetch($groupes)){
 			$id_groupe = $groupe['id_groupe'];
+			if ($motus){// motus?
+				$autorisation = autoriser('afficherselecteurmots','groupemots',$id_groupe,'',array('objet'=>$objet,'id_objet'=>$id_objet));
+				if (!$autorisation){// si le groupe de mot clef courant ne peut pas être associé à l'objet courant?
+					continue; // on passe au groupe de mots suivant
+				}
+			}
 			$mots = sql_select("mots.id_mot",'spip_mots AS `mots` INNER JOIN spip_mots_liens AS L1 ON (L1.id_mot = mots.id_mot)',
 				array(
 					"L1.objet=".sql_quote($objet),
