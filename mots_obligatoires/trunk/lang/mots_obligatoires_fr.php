fd1fb2d463230420e42c0c600afdda9347eeb11f
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-10-22 03:33:32 +0000

    [Salvatore] [source:_plugins_/mots_obligatoires/trunk/lang/ mots_obligatoires] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@100050 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- mots_obligatoires/trunk/lang/mots_obligatoires_fr.php
+++ mots_obligatoires/trunk/lang/mots_obligatoires_fr.php
@@ -1,11 +1,13 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/import_ics/trunk/lang/
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/mots_obligatoires/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-	'mot_manquant' => 'Impossible de publier, il manque un mot-clef pour le groupe suivant&nbsp;:',
-	'mots_manquants' => 'Impossibile de publier, il manque des mots-clef pour les groupes suivants&nbsp;:'
+
+	// M
+	'mot_manquant' => 'Impossible de publier, il manque un mot-clef pour le groupe suivant :',
+	'mots_manquants' => 'Impossibile de publier, il manque des mots-clef pour les groupes suivants :'
 );
