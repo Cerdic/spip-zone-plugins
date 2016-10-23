ace6a9f1c33a47f9a4639fd696ef0e1fbf5962c5
tetue@rezo.net <tetue@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-10-23 04:58:00 +0000

    Parce que tous les boutons ne sont pas des submit, renomons le sélecteur en .btn, plus générique.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@100078 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ varicelle/v2/varicelle_options.php
@@ -0,0 +1,11 @@
+<?php
+
+$GLOBALS['spip_pipeline']['affichage_final'] .= '|varicelle';
+
+// Utiliser la class .btn dans SPIP :
+function varicelle($texte) {
+	$texte = str_replace('class="submit', 'class="btn submit', $texte);
+	return $texte;
+}
+
+?>
