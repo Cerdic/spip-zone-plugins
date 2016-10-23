f22fd7b4c1e30c7f56da7a7fd749de417c2aa4de
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-10-23 13:00:39 +0000

    fichiers qui n'ont rien à faire à la racine
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@100086 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- varicelle/varicelle_pipelines.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function varicelle_insert_head_css($flux) {
-	$flux .= '<link rel="stylesheet" type="text/css" href="'.find_in_path('css/varicelle.css').'" media="all" />'."\n";
-	return $flux;
-}
-
-?>
