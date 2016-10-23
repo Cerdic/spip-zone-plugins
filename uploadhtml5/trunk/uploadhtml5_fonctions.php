1d3afd86f9d0b2a050d6c6d1a22521d32b510390
p@henix.be <p@henix.be@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-10-23 12:11:25 +0000

    PSR
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@100080 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- uploadhtml5/trunk/uploadhtml5_fonctions.php
+++ uploadhtml5/trunk/uploadhtml5_fonctions.php
@@ -24,44 +24,44 @@ if (!defined('_ECRIRE_INC_VERSION')) {
  */
 function uploadhtml5_uploader_document($objet, $id_objet, $files, $id_document = 'new', $mode = 'auto') {
 
-    // tester l'autorisation d'ajout de document
-    include_spip('inc/autoriser');
-    /* S'il n'y a pas d'id_objet, c'est qu'on crée un nouveau
+	// tester l'autorisation d'ajout de document
+	include_spip('inc/autoriser');
+	/* S'il n'y a pas d'id_objet, c'est qu'on crée un nouveau
        document. Les autorisations seront gérées en aval dans
        ajouter_document. */
-    if ($id_objet and (!autoriser('joindredocument', $objet, $id_objet))) {
-        return false;
-    }
+	if ($id_objet and (!autoriser('joindredocument', $objet, $id_objet))) {
+		return false;
+	}
 
-    // On va créer le tableau des documents.
-    $docs = array();
-    foreach ($files as $doc) {
-        // pas de fichier vide
-        if (!empty($doc['name'])) {
-            $docs[] = $doc;
-        }
-    }
+	// On va créer le tableau des documents.
+	$docs = array();
+	foreach ($files as $doc) {
+		// pas de fichier vide
+		if (!empty($doc['name'])) {
+			$docs[] = $doc;
+		}
+	}
 
-    // On fait un test au cas ou
-    if (!empty($docs)) {
-        // On ajoute les documents a un objet SPIP.
-        $ajouter_documents = charger_fonction('ajouter_documents', 'action');
-        $res = $ajouter_documents(
-            $id_document,
-            $docs,
-            $objet, // Article, rubrique, autre objet
-            $id_objet,
-            $mode
-        );
+	// On fait un test au cas ou
+	if (!empty($docs)) {
+		// On ajoute les documents a un objet SPIP.
+		$ajouter_documents = charger_fonction('ajouter_documents', 'action');
+		$res = $ajouter_documents(
+			$id_document,
+			$docs,
+			$objet, // Article, rubrique, autre objet
+			$id_objet,
+			$mode
+		);
 
-        // Obfusquer les noms de fichiers
-        if (_UPLOADHTML5_OBFUSQUER == true) {
+		// Obfusquer les noms de fichiers
+		if (_UPLOADHTML5_OBFUSQUER == true) {
 	        foreach ($res as $key => $id_document) {
 		        obfusquer_document($id_document);
 	        }
-        }
-        return $res;
-    }
+		}
+		return $res;
+	}
 }
 
 /**
@@ -75,42 +75,42 @@ function uploadhtml5_uploader_document($objet, $id_objet, $files, $id_document =
  */
 function uploadhtml5_uploader_logo($objet, $id_objet, $fichier) {
 
-    // Autorisation de mettre un logo?
-    include_spip('inc/autoriser');
-    if (!autoriser('iconifier', $objet, $id_objet)) {
-        return false;
-    }
+	// Autorisation de mettre un logo?
+	include_spip('inc/autoriser');
+	if (!autoriser('iconifier', $objet, $id_objet)) {
+		return false;
+	}
 
-    // On commence par invalider le cache de l'objet
-    include_spip('inc/invalideur');
-    suivre_invalideur("id='$objet/$id_objet'");
+	// On commence par invalider le cache de l'objet
+	include_spip('inc/invalideur');
+	suivre_invalideur("id='$objet/$id_objet'");
 
-    include_spip('action/editer_logo');
-    // Version SPIP 3.1 de cette fonction:
-    if (function_exists('logo_modifier')) {
-        return logo_modifier($objet, $id_objet, 'on', $fichier);
-    }
+	include_spip('action/editer_logo');
+	// Version SPIP 3.1 de cette fonction:
+	if (function_exists('logo_modifier')) {
+		return logo_modifier($objet, $id_objet, 'on', $fichier);
+	}
 
-    include_spip('action/iconifier');
-    $chercher_logo = charger_fonction('chercher_logo', 'inc');
-    $ajouter_image = charger_fonction('spip_image_ajouter', 'action');
+	include_spip('action/iconifier');
+	$chercher_logo = charger_fonction('chercher_logo', 'inc');
+	$ajouter_image = charger_fonction('spip_image_ajouter', 'action');
 
-    $type = type_du_logo(id_table_objet($objet));
-    $logo = $chercher_logo($id_objet, id_table_objet($objet));
+	$type = type_du_logo(id_table_objet($objet));
+	$logo = $chercher_logo($id_objet, id_table_objet($objet));
 
-    if ($logo) {
-        spip_unlink($logo[0]);
-    }
+	if ($logo) {
+		spip_unlink($logo[0]);
+	}
 
-    // Dans le cas d'un tableau, on présume que c'est un $_FILES et on passe directement
-    if (is_array($fichier)) {
-        $err = $ajouter_image($type.'on'.$id_objet, ' ', $fichier, true);
-    } else {
-        // Sinon, on caviarde la fonction ajouter_image
-        $err = $ajouter_image($type.'on'.$id_objet, ' ', array('tmp_name' => $fichier), true);
-    }
+	// Dans le cas d'un tableau, on présume que c'est un $_FILES et on passe directement
+	if (is_array($fichier)) {
+		$err = $ajouter_image($type.'on'.$id_objet, ' ', $fichier, true);
+	} else {
+		// Sinon, on caviarde la fonction ajouter_image
+		$err = $ajouter_image($type.'on'.$id_objet, ' ', array('tmp_name' => $fichier), true);
+	}
 
-    return ($err) ? $err : true;
+	return ($err) ? $err : true;
 }
 
 /**
@@ -123,24 +123,24 @@ function uploadhtml5_uploader_logo($objet, $id_objet, $fichier) {
  */
 function trouver_mime_type($type) {
 
-    // Si le type est logo on récupère automatiquement les formats de
-    // logo défini par SPIP
-    if ($type == 'logo') {
-        global $formats_logos;
-        $type = $formats_logos;
-    } else {
-        // on explode pour passer $type dans sql_in
-        $type = explode(',', $type);
-    }
+	// Si le type est logo on récupère automatiquement les formats de
+	// logo défini par SPIP
+	if ($type == 'logo') {
+		global $formats_logos;
+		$type = $formats_logos;
+	} else {
+		// on explode pour passer $type dans sql_in
+		$type = explode(',', $type);
+	}
 
-    // On récupère les mimes types demandé par la fonction
-    $mime_type = sql_allfetsel('mime_type', 'spip_types_documents', sql_in('extension', $type));
+	// On récupère les mimes types demandé par la fonction
+	$mime_type = sql_allfetsel('mime_type', 'spip_types_documents', sql_in('extension', $type));
 
-    // Simplifier le tableau
-    $mime_type = array_column($mime_type, 'mime_type');
+	// Simplifier le tableau
+	$mime_type = array_column($mime_type, 'mime_type');
 
-    // Renvoyer une chaine utilisable
-    return implode(',', $mime_type);
+	// Renvoyer une chaine utilisable
+	return implode(',', $mime_type);
 }
 
 
@@ -150,32 +150,32 @@ function trouver_mime_type($type) {
  * Cela devrait être dans une fonction du core de SPIP non ?
  */
 function titre_cadre_logo($objet, $id_objet) {
-    $balise_img = chercher_filtre('balise_img');
-    $img = $balise_img(chemin_image('image-24.png'), '', 'cadre-icone');
-    $libelles = pipeline('libeller_logo', $GLOBALS['logo_libelles']);
-    $libelle = (($id_objet or $objet != 'rubrique') ? $objet : 'racine');
-    if (isset($libelles[$libelle])) {
-        $libelle = $libelles[$libelle];
-    } elseif ($libelle = objet_info($objet, 'texte_logo_objet')) {
-        $libelle = _T($libelle);
-    } else {
-        $libelle = _L('Logo');
-    }
-    switch ($objet) {
-        case 'article':
+	$balise_img = chercher_filtre('balise_img');
+	$img = $balise_img(chemin_image('image-24.png'), '', 'cadre-icone');
+	$libelles = pipeline('libeller_logo', $GLOBALS['logo_libelles']);
+	$libelle = (($id_objet or $objet != 'rubrique') ? $objet : 'racine');
+	if (isset($libelles[$libelle])) {
+		$libelle = $libelles[$libelle];
+	} elseif ($libelle = objet_info($objet, 'texte_logo_objet')) {
+		$libelle = _T($libelle);
+	} else {
+		$libelle = _L('Logo');
+	}
+	switch ($objet) {
+		case 'article':
 	        $libelle .= ' ' . aide('logoart');
 	        break;
-        case 'breve':
+		case 'breve':
 	        $libelle .= ' ' . aide('breveslogo');
 	        break;
-        case 'rubrique':
+		case 'rubrique':
 	        $libelle .= ' ' . aide('rublogo');
 	        break;
-        default:
+		default:
 	        break;
-    }
+	}
 
-    return $img . $libelle;
+	return $img . $libelle;
 }
 
 /**
@@ -222,7 +222,6 @@ function renommer_document($ancien_chemin, $nouveau_chemin, $id_document = null)
 
 	// Mettre à jour la base de donnée avec le nouveau chemin au besoin
 	if (!is_null($id_document)) {
-
 		// Dans la base de donnée il ne faut pas avoir le dossier IMG
 		$nouveau_chemin = str_replace(_DIR_IMG, '', $nouveau_chemin);
 		sql_updateq('spip_documents', array('fichier' => $nouveau_chemin), 'id_document='.intval($id_document));
