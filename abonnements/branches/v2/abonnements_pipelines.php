b824783a1655f107cc92b5fb6197b6afaf4aa758
tcharlss@bravecassine.com <tcharlss@bravecassine.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-10-24 13:57:35 +0000

    v2.3.6 : inclure editer_liens
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@100125 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- abonnements/branches/v2/abonnements_pipelines.php
+++ abonnements/branches/v2/abonnements_pipelines.php
@@ -19,7 +19,7 @@ function abonnements_optimiser_base_disparus($flux){
 
 	//Offres d'abonnement à la poubelle
 	sql_delete("spip_abonnements_offres", "statut='poubelle' AND maj < ".$flux['args']['date']);
-	
+
 	//Supprimer les abonnements lies à une offre d'abonnement inexistante
 	$res = sql_select("DISTINCT abonnements.id_abonnements_offre","spip_abonnements AS abonnements
 						LEFT JOIN spip_abonnements_offres AS offres
@@ -29,7 +29,7 @@ function abonnements_optimiser_base_disparus($flux){
 
 	//Abonnements à la poubelle
 	sql_delete("spip_abonnements", "statut='poubelle' AND maj < ".$flux['args']['date']);
-	
+
 	include_spip('action/editer_liens');
 	$flux['data'] += objet_optimiser_liens(array('abonnement'=>'*'),'*');
 	return $flux;
@@ -43,9 +43,9 @@ function abonnements_post_edition($flux){
 	if ($flux['args']['table'] == 'spip_abonnements') {
 		$abonnement = sql_fetsel('*', 'spip_abonnements', 'id_abonnement = '.intval($flux['args']['id_objet']));
 		$offre = sql_fetsel('*', 'spip_abonnements_offres', 'id_abonnements_offre = '.intval($abonnement['id_abonnements_offre']));
-		
+
 		$modifs = array();
-		
+
 		// Si l'échéance est VIDE, et que pourtant l'offre parente A BIEN une durée
 		// alors c'est qu'il faut initialiser l'échéance !
 		if ($abonnement['date_fin'] == '0000-00-00 00:00:00' and ($duree = $offre['duree']) > 0){
@@ -64,10 +64,10 @@ function abonnements_post_edition($flux){
 					$ajout ='';
 					break;
 			}
-			
+
 			// Calcul de la date de fin
 			$modifs['date_fin'] = date('Y-m-d H:i:s', strtotime($abonnement['date_debut'].$ajout));
-			
+
 			$modifs = pipeline(
 				'abonnement_initialisation_dates',
 				array(
@@ -75,7 +75,7 @@ function abonnements_post_edition($flux){
 					'data' => $modifs
 				)
 			);
-			
+
 			// Si les dates ont été changées, on change le tableau de l'abonnement pour le test de statut qui suivra
 			if (isset($modifs['date_debut'])) {
 				$abonnement['date_debut'] = $modifs['date_debut'];
@@ -84,7 +84,7 @@ function abonnements_post_edition($flux){
 				$abonnement['date_fin'] = $modifs['date_fin'];
 			}
 		}
-		
+
 		// Si le statut est "prepa" c'est une création et on doit changer ça
 		// car pour l'instant SPIP ne permet pas de déclarer le statut par défaut !
 		if ($abonnement['statut'] == 'prepa') {
@@ -92,6 +92,7 @@ function abonnements_post_edition($flux){
 		}
 		// Si on a mis l'abonnement à la poubelle, on doit enlever les tâches liées
 		elseif ($abonnement['statut'] == 'poubelle') {
+			include_spip('action/editer_liens');
 			$liens = objet_trouver_liens(array('job' => '*'), array('abonnement' => $abonnement['id_abonnement']));
 			if ($liens and is_array($liens)){
 				// Et on les supprime toutes !
@@ -100,7 +101,7 @@ function abonnements_post_edition($flux){
 				}
 			}
 		}
-		
+
 		// Seulement si personne n'a modifié le statut manuellement, alors on check les dates pour statufier
 		if (!$flux['data']['statut']) {
 			$jourdhui = date('Y-m-d H:i:s');
@@ -120,13 +121,13 @@ function abonnements_post_edition($flux){
 				$modifs['statut'] = 'inactif';
 			}
 		}
-		
+
 		// S'il y a des modifs à faire on appelle l'API de modif
 		if (!empty($modifs)){
 			include_spip('action/editer_objet');
 			objet_modifier('abonnement', $flux['args']['id_objet'], $modifs);
 		}
-		
+
 		// Si dans les modifications demandées au départ, il y a la date de fin, on reprogramme la désactivation
 		if (isset($flux['data']['date_fin'])) {
 			include_spip('inc/abonnements');
@@ -157,7 +158,7 @@ function abonnements_post_edition($flux){
 				// On crée ou renouvelle
 				$action = charger_fonction('creer_ou_renouveler_abonnement', 'action/');
 				$retour = $action($id_auteur.'/'.$id_abonnements_offre);
-				
+
 				// Si on a un retour correct avec un abonnement
 				if (
 					is_array($retour)
@@ -174,7 +175,7 @@ function abonnements_post_edition($flux){
 			}
 		}
 	}
-	
+
 	return $flux;
 }
 
@@ -197,7 +198,7 @@ function abonnements_affiche_gauche($flux){
 			array('id_abonnements_offre' => $flux['args']['id_abonnements_offre'])
 		);
 	}
-	
+
 	return $flux;
 }
 
@@ -209,7 +210,7 @@ function abonnements_affiche_milieu($flux){
 	if ($e = trouver_objet_exec($flux['args']['exec'])
 	  AND $e['type'] == 'auteur'
 	  AND $e['edition'] == false) {
-		
+
 		$id_auteur = $flux['args']['id_auteur'];
 
 		$ins = recuperer_fond('prive/squelettes/inclure/abonnements_auteur',array('id_auteur'=>$id_auteur));
@@ -217,8 +218,8 @@ function abonnements_affiche_milieu($flux){
 			$flux['data'] = substr_replace($flux['data'],$ins,$p,0);
 		else
 			$flux['data'] .= $ins;
-		
+
 	}
-	
+
 	return $flux;
 }
