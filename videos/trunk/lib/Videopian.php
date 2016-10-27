b1082d3096df99235f2de8d5ea30c8d02ccf3cb8
prigent.yohann@gmail.com <prigent.yohann@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-10-27 10:50:58 +0000

    Utiliser les fonctions de SPIP pour récupérer l’url. Ça permet de profiter des paramétrages déjà fait (proxy, taille max, …) Vu que la lib a l’air super vieille, et déjà forkée, je me permets de forker encore cette lib.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@100221 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- videos/trunk/lib/Videopian.php
+++ videos/trunk/lib/Videopian.php
@@ -150,23 +150,11 @@ class Videopian {
      * 
     */
     private static function url_get_contents($url) {
-        if (function_exists('curl_exec')){ 
-            $conn = curl_init($url);
-            curl_setopt($conn, CURLOPT_SSL_VERIFYPEER, true);
-            curl_setopt($conn, CURLOPT_FRESH_CONNECT,  true);
-            curl_setopt($conn, CURLOPT_RETURNTRANSFER, 1);
-            $url_get_contents_data = (curl_exec($conn));
-            curl_close($conn);
-        }elseif(function_exists('file_get_contents')){
-            $url_get_contents_data = file_get_contents($url);
-        }elseif(function_exists('fopen') && function_exists('stream_get_contents')){
-            $handle = fopen ($url, "r");
-            $url_get_contents_data = stream_get_contents($handle);
-        }else{
-            $url_get_contents_data = false;
-        }
+        include_spip('inc/distant');
+
+        $url_get_contents_data = recuperer_url($url);
         
-        return $url_get_contents_data;
+        return $url_get_contents_data['page'];
     }
     
     
