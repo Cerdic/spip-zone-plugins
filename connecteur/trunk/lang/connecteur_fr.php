4ee6fe7716d39e29f23d014668d1992f86c6f7f9
p@henix.be <p@henix.be@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-10-30 19:08:47 +0000

    PSR
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@100275 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- connecteur/trunk/lang/connecteur_fr.php
+++ connecteur/trunk/lang/connecteur_fr.php
@@ -1,7 +1,8 @@
 <?php
-// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
 
-if (!defined('_ECRIRE_INC_VERSION')) return;
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
