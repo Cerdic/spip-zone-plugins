d23d95f8442eed1a8f8e325c8d84932111d285aa
p@henix.be <p@henix.be@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-11-01 16:20:25 +0000

    Une constante pour désactiver totalement la relecture temporaire
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@100312 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spip-bonux-3/spip_bonux_options.php
+++ spip-bonux-3/spip_bonux_options.php
@@ -10,34 +10,40 @@
 // On inclu une bonne fois pour toutes array_column
 include_spip('lib/array_column/array_column');
 
-if (_request('var_mode')=='preview'
-	and $cle = _request('var_relecture')) {
-	include_spip('spip_bonux_fonctions');
-	if (previsu_verifier_cle_temporaire($cle)) {
-		include_spip('inc/autoriser');
-		autoriser_exception('previsualiser', '', 0);
-		define('_VAR_PREVIEW_EXCEPTION', true);
-	}
+if (!defined('_RELECTURE')) {
+	define('_RELECTURE', true);
 }
 
-function spip_bonux_affichage_final($flux) {
-	if (defined('_VAR_PREVIEW') and _VAR_PREVIEW) {
-		$p = stripos($flux, '</body>');
-		$url_relecture = parametre_url(self(), 'var_mode', 'preview', '&');
-		$js = '';
-		if (!defined('_VAR_PREVIEW_EXCEPTION')) {
-			$url_relecture = parametre_url($url_relecture, 'var_relecture', previsu_cle_temporaire(), '&');
-			$label = 'Relecture temporaire';
-		} else {
-			$label = _T('previsualisation');
-			$js = "jQuery('.spip-previsu').html('Relecture temporaire');";
+if (_RELECTURE) {
+	if (_request('var_mode')=='preview'
+		and $cle = _request('var_relecture')) {
+		include_spip('spip_bonux_fonctions');
+		if (previsu_verifier_cle_temporaire($cle)) {
+			include_spip('inc/autoriser');
+			autoriser_exception('previsualiser', '', 0);
+			define('_VAR_PREVIEW_EXCEPTION', true);
 		}
-		$js .= "jQuery('#spip-admin').append('<a class=\"spip-admin-boutons review_link\" href=\"$url_relecture\">$label</a>');";
-		$js = "jQuery(function(){ $js });";
-		$js = "<script>$js</script>";
-		$flux = substr_replace($flux, $js, $p, 0);
 	}
-	return $flux;
+
+	function spip_bonux_affichage_final($flux) {
+		if (defined('_VAR_PREVIEW') and _VAR_PREVIEW) {
+			$p = stripos($flux, '</body>');
+			$url_relecture = parametre_url(self(), 'var_mode', 'preview', '&');
+			$js = '';
+			if (!defined('_VAR_PREVIEW_EXCEPTION')) {
+				$url_relecture = parametre_url($url_relecture, 'var_relecture', previsu_cle_temporaire(), '&');
+				$label = 'Relecture temporaire';
+			} else {
+				$label = _T('previsualisation');
+				$js = "jQuery('.spip-previsu').html('Relecture temporaire');";
+			}
+			$js .= "jQuery('#spip-admin').append('<a class=\"spip-admin-boutons review_link\" href=\"$url_relecture\">$label</a>');";
+			$js = "jQuery(function(){ $js });";
+			$js = "<script>$js</script>";
+			$flux = substr_replace($flux, $js, $p, 0);
+		}
+		return $flux;
+	}
 }
 
 if (!defined('_ECRIRE_INC_VERSION')) {
