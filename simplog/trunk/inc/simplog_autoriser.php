c826779f3e3d16279cf51a40c0ba816989bffa8f
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-11-02 17:49:47 +0000

    Ajout d'un bouton et d'une action de suppression d'un log en cours d'affichage. Passage en PSR SPIP.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@100329 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- simplog/trunk/inc/simplog_autoriser.php
+++ simplog/trunk/inc/simplog_autoriser.php
@@ -1,22 +1,23 @@
 <?php
 
 // Sécurité
-if (!defined("_ECRIRE_INC_VERSION")) return;
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
 
 // declarer la fonction du pipeline
-function simplog_autoriser(){}
+function simplog_autoriser() {
+}
 
 
 // Seul admin a acces aux logs
 function autoriser_simplog_voir_dist($faire, $type, $id, $qui, $opt) {
-    //spip_log ( 'autoriser_simplog_voir_dist'.$qui['statut'], 'simplog');
-    return ($qui['statut'] == '0minirezo');
+	//spip_log ( 'autoriser_simplog_voir_dist'.$qui['statut'], 'simplog');
+	return ($qui['statut'] == '0minirezo');
 }
 
 // affichage bouton
 function autoriser_simplog_bt_dist($faire, $type, $id, $qui, $opt) {
-    return autoriser('voir','simplog');
+	return autoriser('voir', 'simplog');
 }
-
-?>
--- simplog/trunk/prive/squelettes/contenu/simplog_fonctions.php
+++ simplog/trunk/prive/squelettes/contenu/simplog_fonctions.php
@@ -1,9 +1,12 @@
 <?php
 
-if (!defined("_ECRIRE_INC_VERSION")) return;
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
-if (!defined('_SIMPLOG_LIGNE'))
+if (!defined('_SIMPLOG_LIGNE')) {
 	define('_SIMPLOG_LIGNE', '#^(.*:\d\d)\s(.*)\s\(pid\s(.*)\)\s(?:(.+):L(\d+):(\w+\(\)):)?:([bipru]*):([^:]*):\s(.*)$#i');
+}
 
 function simplog_phraser_log($fichier) {
 	$tableau = array();
@@ -21,18 +24,16 @@ function simplog_phraser_ligne($contenu) {
 	$tableau = array();
 
 	foreach ($contenu as $_ligne) {
-		if ($l = trim($_ligne) AND $l != '[-- rotate --]') {
+		if ($l = trim($_ligne) and $l != '[-- rotate --]') {
 			preg_match(_SIMPLOG_LIGNE, $_ligne, $matches);
-			if (!isset($matches[1])
-			OR !$matches[1]) {
+			if (!isset($matches[1])	or !$matches[1]) {
 				// Ce n'est pas une nouvelle ligne mais la suite du texte de la ligne en cours
-				$tableau[count($tableau)-1]['texte'] .= "\n" . trim($_ligne);
-			}
-			else {
+				$tableau[count($tableau) - 1]['texte'] .= "\n" . trim($_ligne);
+			} else {
 				$ligne['date'] = date('Y-m-d H:i:s', strtotime($matches[1]));
 				$ligne['ip'] = trim($matches[2]);
 				$ligne['pid'] = trim($matches[3]);
-				$ligne['hit'] = _T('simplog:info_hit_'. strtolower(trim($matches[7])));
+				$ligne['hit'] = _T('simplog:info_hit_' . strtolower(trim($matches[7])));
 				$ligne['gravite'] = strtolower(trim($matches[8]));
 				$ligne['texte'] = contruire_texte(trim($matches[9]), trim($matches[4]), trim($matches[5]), trim($matches[6]));
 				$ligne['index'] = count($tableau);
@@ -49,13 +50,11 @@ function contruire_texte($message, $fichier, $ligne, $fonction) {
 	$label_ligne = _T('simplog:label_ligne');
 	$label_fonction = _T('simplog:label_fonction');
 	$texte = '<ul>'
-		. ($fichier ? "<li><em>$label_fichier</em> : $fichier</li>" : '')
-		. ($ligne ? "<li><em>$label_ligne</em> : $ligne</li>" : '')
-		. ($fonction ? "<li><em>$label_fonction</em> : $fonction</li>" : '')
-		. ($message ? "<li>$message</li>" : '')
-		. '</ul>';
+			 . ($fichier ? "<li><em>$label_fichier</em> : $fichier</li>" : '')
+			 . ($ligne ? "<li><em>$label_ligne</em> : $ligne</li>" : '')
+			 . ($fonction ? "<li><em>$label_fonction</em> : $fonction</li>" : '')
+			 . ($message ? "<li>$message</li>" : '')
+			 . '</ul>';
 
 	return $texte;
 }
-
-?>
