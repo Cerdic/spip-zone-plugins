56c4465e07c37bcc0ac70abe739ee8e16dbafa60
ixabel@ikas.eus <ixabel@ikas.eus@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-11-15 04:33:13 +0000

    [Salvatore] [source:_plugins_/mediaspip_player/trunk/lang/ mediaspip_player] Export depuis http://trad.spip.net de la langue eu
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@100509 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ mediaspip_player/trunk/lang/mediaspip_player_eu.php
@@ -0,0 +1,87 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/mediaspip_player?lang_cible=eu
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'alt_cover_fichier' => 'Dokumentuaren logoa',
+
+	// B
+	'bouton_ajouter_conversion' => 'Ajouter une conversion',
+	'bouton_fullscreen' => 'Pantaila osoa modura pasatu',
+	'bouton_fullscreen_full' => 'Itzuli pantaila normalera',
+	'bouton_lire' => 'Irakurri',
+	'bouton_loading' => 'Chargement',
+	'bouton_loop' => 'Errepetitu luzaran',
+	'bouton_loop_looped' => 'Désactiver la boucle',
+	'bouton_mute' => 'Soinua kendu',
+	'bouton_pause' => 'Pausa',
+	'bouton_seeking' => 'Seeking',
+	'bouton_unmute' => 'Soinua aktibatu',
+	'bouton_volume' => 'Bolumena',
+	'bouton_volume_muted' => 'Soinua desaktibatu',
+
+	// C
+	'cfg_titre_html5_player' => 'Lecteur HTML5',
+
+	// E
+	'erreur_valeur_int' => 'La valeur de ce champ doit être un nombre.',
+	'erreur_valeur_int_inf' => 'La valeur de ce champ doit être inférieure à @nb@.',
+	'erreur_valeur_int_sup' => 'La valeur de ce champ doit être supérieure à @nb@.',
+	'explication_cookie_volume' => 'Enregistrer l’état du volume dans un cookie afin de le récupérer pour chaque lecteur même en changeant de page.',
+	'explication_infos_embed' => 'Afficher un bandeau indiquant le titre et l’url du site sur les documents en embed.',
+	'explication_mousewheel_non_charger' => 'Ne pas utiliser la molette de la souris pour régler le volume.',
+	'explication_slider_non_charger' => 'Ne pas utiliser les sliders pour le volume et la barre de progression de lecture.',
+	'explication_slider_volume_orientation' => 'Permet de modifier l’orientation du slider de volume (dépend du thème utilisé en général).',
+	'explication_video_hauteur_embed' => 'Définit la hauteur des vidéos par défaut utilisée pour le code d’exportation.',
+	'explication_video_largeur_embed' => 'Définit la largeur des vidéos par défaut utilisée pour le code d’exportation.',
+
+	// I
+	'info_ecoule' => 'Iragan denbora',
+	'info_erreur' => 'Errorea',
+	'info_nb_conversion' => '@nb@ conversions liées au document',
+	'info_restant' => 'Gelditzen den denbora',
+	'info_streaming' => 'Streaming',
+	'info_total' => 'Iraupena',
+	'info_une_conversion' => 'Une conversion liée au document',
+
+	// L
+	'label_cookie_volume' => 'Enregistrer le volume dans un cookie',
+	'label_hauteur' => 'Hauteur',
+	'label_infos_embed' => 'Afficher le bandeau sur les embed',
+	'label_largeur' => 'Largeur',
+	'label_mousewheel_non_charger' => 'Ne pas charger mousewheel',
+	'label_slider_non_charger' => 'Ne pas charger les sliders',
+	'label_slider_volume_orientation' => 'Orientation du slider de volume',
+	'label_video_hauteur_embed' => 'Hauteur des vidéos pour l’exportation',
+	'label_video_largeur_embed' => 'Largeur des vidéos pour l’exportation',
+
+	// M
+	'message_erreur_flash' => 'Media hau begiratzeko flash player instalatu edo eguneratu behar duzu (gutienezko bertsioa: 10)<br /><a href="http://get.adobe.com/fr/flashplayer/" class="spip_out">Azken bertsioa</a> deskarga ezazu.',
+	'message_erreur_flash_mobile' => 'Media hau irakurgai duzu flash irakurgai baten bidez. Zure plataformak programa honen instalazioa ez du uzten.',
+
+	// S
+	'statut_error' => 'Badirudi zure nabigatzaileak ezin duela dokumentu hau irakurri.',
+	'statut_error_broken' => 'Badirudi bideo hau ezin dela irakurri.',
+	'statut_error_network' => 'Sare arazo bat badago, geroago saia ezazu.',
+	'statut_error_stopped' => 'Deskarga prozesua gelditu egin da.',
+	'statut_loop' => 'En boucle',
+	'statut_mute' => 'Soinurik gabe',
+	'statut_pause' => 'Pausa',
+	'statut_play' => 'Irakurketa',
+	'statut_seek_back' => 'Retourne à :',
+	'statut_seek_to' => 'Avance à :',
+	'statut_unloop' => 'Begizta desaktibatua',
+	'statut_unmute' => 'Soinua biraktibatua da',
+	'statut_volume' => 'Bolumena',
+
+	// V
+	'valeur_horizontal' => 'Horizontal',
+	'valeur_vertical' => 'Vertical'
+);
