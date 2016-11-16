c08100780a45c251071ab8b3a00f883eb40311e9
ixabel@ikas.eus <ixabel@ikas.eus@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-11-16 04:33:09 +0000

    [Salvatore] [source:_plugins_/newsletters/trunk/lang/ newsletter] Export depuis http://trad.spip.net de la langue eu
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@100525 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ newsletters/trunk/lang/newsletter_eu.php
@@ -0,0 +1,76 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/newsletter?lang_cible=eu
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'ajouter_lien_newsletter' => 'Berripapera hau gehitu',
+
+	// E
+	'explication_baked' => 'Edizio aurreratua: zuzenean berripaperaren HTMLa eta testua alda dezakezu. Ez da gehiago automatikoki sortuko. Edizio soil batera pasatzen bazara zure aldaketak galduko dira.',
+	'explication_baked_publie' => 'Berripaper hau balditua da: bere HTMLa edo testua zuzendu dezakezu bainan bere argitalpena ezin duzu aldatu ezta ere berriz sortu modeloaren bidez.',
+	'explication_html_page_vide' => 'Permet d’afficher dans le navigateur une version différente de celle envoyée par email (pour des raisons techniques ou éditoriales). Laissez le vide si vous voulez afficher la même version dans l’email et dans le navigateur.',
+
+	// I
+	'icone_creer_newsletter' => 'Berripaper bat sortu',
+	'icone_dupliquer_newsletter' => 'Berripapera bikoiztu',
+	'icone_modifier_newsletter' => 'Berripapera aldatu',
+	'info_1_mailsubscriber' => 'harpideduna',
+	'info_1_newsletter' => 'berripapera',
+	'info_1_newsletter_prog' => 'programatutako berripapera',
+	'info_aucun_newsletter' => 'Berripaperik ez',
+	'info_email_inscrit_newletter' => 'Zure izena eman duzu, mailbidez elkarte honen berrien eskuratzeko:',
+	'info_email_not_displaying' => 'E-posta hau ez da ongi agertzen?',
+	'info_email_reception' => 'E-posta hau honi bidalia izan zaio:',
+	'info_email_voir_en_ligne' => 'Berripapera zure nabigatzailean ikusi',
+	'info_nb_mailsubscribers' => '@nb@ harpidedun',
+	'info_nb_newsletters' => '@nb@ berripaper',
+	'info_nb_newsletters_prog' => '@nb@ berripaper programatuta',
+	'info_newsletter_generer' => 'Berripapera sortu (HTML bertsioa eta testu bertsioa)',
+	'info_newsletters_auteur' => 'Autore honen berripaperak',
+	'info_preview_version_html_email' => 'HTML bertsioa e-postentzat',
+	'info_preview_version_html_page' => 'HTML bertsioa sarean',
+	'info_preview_version_html_page_noiframe' => '(iframik gabe erakutsi)',
+	'info_preview_version_texte_email' => 'Testu-email bertsioa',
+	'info_publier_pour_envoyer' => 'Berripaper hau argitaratu, zerrenda bati igortzeko',
+	'info_test_sujet' => 'PROBA',
+
+	// L
+	'label_a_lire_sur' => '@site@ webgunean irakurri',
+	'label_baked_1' => 'Berripaperaren edizio aurreratua',
+	'label_chapo' => 'Chapeau',
+	'label_date_redac' => 'Argitalpen data',
+	'label_html_email' => 'HTML bertsioa',
+	'label_html_page' => 'Nabigatzailean agertzen den HTML bertsioa',
+	'label_inscrit_newsletter' => 'Zure izena eman duzu mailbidez gune honen berrien eskuratzeko: @site@',
+	'label_masquer_fond' => 'Berripaper modeloak ezkutatu:',
+	'label_patron' => 'Modeloa',
+	'label_publie' => 'Argitaratze data:',
+	'label_selection_articles' => 'Hautatutako artikuluak',
+	'label_selection_edito' => 'Argitalpenen hautaketa (artikulu & atal)',
+	'label_selection_rubriques' => 'Atalen hautaketa',
+	'label_texte' => 'Testu',
+	'label_texte_email' => 'Testu bertsioa',
+	'label_titre' => 'Titulua',
+	'lien_lire_en_ligne' => 'Sarean irakurri',
+
+	// R
+	'retirer_lien_newsletter' => 'Berripapera kendu',
+	'retirer_tous_liens_newsletters' => 'Berripaper guziak kendu',
+
+	// T
+	'texte_ajouter_newsletter' => 'Berripaper bat gehitu',
+	'texte_changer_statut_newsletter' => 'Berripapera honen egoera:',
+	'texte_creer_associer_newsletter' => 'Berripaper bat sortu eta lotu (associer)',
+	'titre_langue_newsletter' => 'Berripaper honen hizkuntza',
+	'titre_logo_newsletter' => 'Berripaper honen logoa',
+	'titre_newsletter' => 'Berripapera',
+	'titre_newsletters' => 'Berripaperak',
+	'titre_newsletters_rubrique' => 'Atalaren berripaperak'
+);
