12b075b14c5e7c4cf5aa99734842c3a92ff84794
spip@allesfrans.com <spip@allesfrans.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-11-17 04:35:31 +0000

    [Salvatore] [source:_plugins_/simplog/trunk/lang/ simplog] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@100542 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- simplog/trunk/lang/simplog_nl.php
+++ simplog/trunk/lang/simplog_nl.php
@@ -13,7 +13,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'actualiser' => 'Werk de lijst bij',
 
 	// C
-	'clic_rafraichir' => 'Het bestand herladen', # MODIF
+	'clic_rafraichir' => 'Het bestand opnieuw laden',
 	'colonne_date' => 'Datum',
 	'colonne_gravite' => 'Ernst',
 	'colonne_hit' => 'Hit',
@@ -26,6 +26,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// F
 	'fichier' => 'Bestand« @fichier@ »',
+	'filtre_gravite_aucun' => 'Alle gradaties',
 
 	// I
 	'info_hit_pri' => 'Privé',
@@ -40,6 +41,10 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// M
 	'message_fichier_choisir' => 'Kies een te analyseren bestand.',
+	'message_fichier_filtre_vide' => 'Het bestand bevat geen registratie met gradatie « @gravite@ ».',
+	'message_fichier_non_log' => 'Het bestand "@fichier@" is geen log of is niet langer beschikbaar.',
+	'message_fichier_suppression' => 'Weet je zekere dat je het logbestand wilt verwijderen.',
+	'message_fichier_vide' => 'Het bestand is leeg.',
 
 	// T
 	'tri_croissant' => 'Oplopend sorteren',
