b9c841c774c740c9cfb94542b1aff72f40cc0436
jack@jack31.net <jack@jack31.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-11-17 18:13:52 +0000

    Amélioration des fichiers de langue
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@100554 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- pgn4spip/trunk/lang/pgn4spip_fr.php
+++ pgn4spip/trunk/lang/pgn4spip_fr.php
@@ -1,7 +1,7 @@
 <?php
 /**********************************************************************************
- * @Subject French resources: this is a SPIP 2 or 3 language file
- * @package pgn4spip plugin to embed pgn4web chessboard in a SPIP article
+ * @Subject French resources: this is a SPIP language file
+ * @package pgn4spip plugin to embed pgn4web chessboard in a SPIP 2.x or 3.x article
  * @copyright copyright (c) 2012 Matt Chesstale 
  * @license GNU General Public License version 3
  **********************************************************************************/
@@ -11,110 +11,73 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-'ok'			=> 'OK',
-'reinit'		=> 'Remettre les valeurs par défaut',
-'config_reinit'	=> 'La configuration a été remise avec les valeurs par défaut',
+// Only lowercase in the key. 
+'ok'				=>	'OK',
+'reinit'			=>	'Remettre les valeurs par défaut',
+'config_reinit'		=>	'La configuration a été remise avec les valeurs par défaut',
+'boxtitle'			=>	'Configuration générale de l’échiquier et du PGN',
+'squareclick'		=>	'Chaque case de l’échiquier a une infobulle et une fonction.<br />
+Pour inverser l’échiquier, cliquer sur la case <b>e7</b> ou appuyer sur touche "<b>f</b>".',
+'module'			=>	'Module',
+'horizontal'		=>	'horizontal',
+'vertical'			=>	'vertical',
+'auto'				=>	'Hauteur automatique',
+'manual'			=>	'manuelle : ',
+'pixels'			=>	' pixels',
+'chessboard'		=>	'Echiquier',
+'square'			=>	'Case : ',
+'white'				=>	'blanche',
+'black'				=>	'noire',
+'piece'				=>	'Taille pièce :',
+'default'			=>	'auto',
+'font'				=>	'Police :',
+'alpha'				=>	'alpha',
+'merida'			=>	'merida',
+'uscf'				=>	'uscf',
+'svgchess'			=>	'svg',
+'focus'				=>	'Focus',
+'border'			=>	'Bord',
+'background'		=>	'Fond',
+'pgn'				=>	'PGN',
+'pgnheader'			=>	'Entête',
+'pgnmove'			=>	'Coup',
+'pgncomment'		=>	'Commentaire',
+'pgnfocus'			=>	'Focus',
+'pgndelay'			=>	'Délai (ms)',
+'showmoves'			=>	'Notation',
+'figurine'			=>	'figurine',
+'text'				=>	'texte',
+'puzzle'			=>	'puzzle',
+'hidden'			=>	'cachée',
+'live'				=>	'live',
+'ctrlbrowse'		=>	'Bouton',
+'ctrlarrow'			=>	'Flèche',
+'ctrlbckgrnd'		=>	'Fond',
+'custom'			=>	'plat',
+'standard'			=>	'3D',
+'newline'			=>	'bloc',
+'inline'			=>	'suite',
+'hidden2'			=>	'caché;',
+'focusborder'		=>	'bord',
+'focussquare'		=>	'case',
+'liverate'			=>	'live (mn)',
+'autoplay'			=>	'Jeu auto :',
+'none'				=>	'aucun',
+'game'				=>	'1 fois',
+'loop'				=> 	'boucle',
+'conf_part_pgn'		=>	'Configuration particulière d’un fichier pgn dans un article SPIP',
+'syntaxe'			=>	'Syntaxe',
+'url_def_pgn'		=>	'http://fr.wikipedia.org/wiki/Portable_Game_Notation',
+'prm_example'		=>	'[pgn</b> <i>prm1</i><b>=</b><i>valeur1</i> <i>prm2</i></i><b>=</b><i>valeur2</i> ...</i><b>]',
+'algebric_notation' =>	'Notation algébrique',
+'nombre'			=>	'nombre',
+'optional_param'	=>	'Liste des paramètres optionnels',
+'or'				=>	'ou',
+'by_default' 		=>	'par défaut',
+'initial'			=>	'initiale',
+'in_article'		=>	'Dans le corps d’un article SPIP consacré à la défense Alekhine',
+'example'			=>	'Exemple',
+'before'			=>	'avant',
+'after'				=>	'après'
 
-'boxtitle'		=> 'Configuration générale de l’échiquier et du PGN',
-
-'squareclick'	=> <<<EOT
-Chaque case de l'&eacute;chiquier a une infobulle et une fonction.<br />
-Pour inverser l'&eacute;chiquier, cliquer sur la case <b>e7</b> ou appuyer sur touche "<b>f</b>".
-EOT
-,
-'module'		=> 'Module',
-'horizontal'	=> 'horizontal',
-'vertical'		=> 'vertical',
-
-'auto'			=> 'Hauteur automatique',
-'manual'		=> 'manuelle : ',
-'pixels'		=> ' pixels',
-
-'chessboard'	=> 'Echiquier',
-'square'		=> 'Case : ',
-'white'			=> 'blanche',
-'black'			=> 'noire',
-
-'piece'			=> 'Taille pièce :',
-'default'		=> 'auto',
-'font'			=> 'Police :',
-'alpha'			=> 'alpha',
-'merida'		=> 'merida',
-'uscf'			=> 'uscf',
-'svgchess'		=> 'svg',
-
-'focus'			=> 'Focus',
-'border'		=> 'Bord',
-'background'	=> 'Fond',
-
-'pgn'			=> 'PGN',
-'pgnheader'		=> 'Entête',
-'pgnmove'		=> 'Coup',
-'pgncomment'	=> 'Commentaire',
-'pgnfocus'		=> 'Focus',
-'pgndelay'		=> 'Délai (ms)',
-
-'showmoves'		=> 'Notation',
-'figurine'		=> 'figurine',
-'text'			=> 'texte',
-'puzzle'		=> 'puzzle',
-'hidden'		=> 'cachée',
-'live'			=> 'live',
-
-'ctrlbrowse'	=> 'Bouton',
-'ctrlarrow'		=> 'Flèche',
-'ctrlbckgrnd'	=> 'Fond',
-
-'custom'		=> 'plat',
-'standard'		=> '3D',
-
-'newline'		=> 'bloc',
-'inline'		=> 'suite',
-'hidden2'		=> 'caché;',
-
-'focusborder'	=> 'bord',
-'focussquare'	=> 'case',
-
-'liverate'		=> 'live (mn)',
-
-'autoplay'		=> 'Jeu auto :',
-'none'			=> 'aucun',
-'game'			=> '1 fois',
-'loop'			=> 'boucle',
-
-'conflocalpgn'	=> <<<EOT
-<legend>Configuration <i>particulière</i> d'un PGN dans un article SPIP</legend>
-<fieldset><legend>Syntaxe</legend>
-<b>[pgn</b> <i>prm1</i><b>=</b><i>valeur1</i> <i>prm2</i></i><b>=</b><i>valeur2</i> ...</i><b>]</b> <i>notation algébrique <a href="http://fr.wikipedia.org/wiki/Portable_Game_Notation" target="_blank">PGN</a> US</i> <b>[<font color="red">/</font>pgn]</b>
-</fieldset>
-<br />
-Liste des <a href="../plugins/pgn4spip/pgn4web/board.html?help=true" target="_blank">paramètres</a> optionnels (" | " = "ou"; [] = par d&eacute;faut; () = initiale) :
-<table cellspacing="5">
-<tr><td><b><u>f</u></b>rame<b><u>H</u></b>eight=[""] | <i>nombre</i><br />
-		fh=[""] | <i>nombre</i><br />
-		<b>s</b>quare<b>S</b>ize=<i>nombre</i><br />
-		<b>i</b>nitial<b>V</b>ariation=<i>nombre</i>
-	</td>
-	<td><b>l</b>ayout=[<b>h</b>]orizontal | (<b>v</b>)ertical<br />
-		<b>i</b>nitial<b>G</b>ame=[<b>f</b>]irst | (<b>l</b>)ast | (<b>r</b>)andom | <i>nombre</i><br />
-		<b>p</b>iece<b>S</b>ize=[<b>d</b>]efault | <i>nombre</i><br />
-		<b>a</b>utoplay<b>M</b>ode=[<b>n</b>]one | (<b>g</b>)ame | (<b>l</b>)oop
-	</td>
-</tr>
-<tr><td colspan="2"><b>m</b>oves<b>D</b>isplay=[<b>f</b>]igurine | (<b>t</b>)ext | (<b>p</b>)uzzle | (<b>h</b>)idden | (<b>l</b>)ive<br />
-					<b>i</b>nitial<b>H</b>alfmove=[<b>s</b>]tart | (<b>e</b>)nd | (<b>r</b>)andom | (<b>c</b>)omment | <i>nombre</i><br />
-					<b>p</b>iece<b>F</b>ont=[<b>d</b>]efault | (<b>a</b>)lpha | (<b>m</b>)erida | (<b>u</b>)scf | (<b>s</b>)vgchess
-	</td>
-</tr>
-</table>
-Exemple dans le corps d'un article SPIP consacré à la <a href="http://fr.wikipedia.org/wiki/D%C3%A9fense_Alekhine" target="_blank">défense Alekhine</a> :<br />
-<textarea rows=6 cols=55>
-Avant
-[pgn frameHeight=500 squareSize=46 pieceSize=40 pf=m
- movesDisplay=figurine layout=vertical am=loop]
-1. e4 Nf6 2.e5 Nd5 3.c4 Nb6
-[/pgn]
-Après
-</textarea>
-EOT
 );
