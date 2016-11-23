cb9d75835ce3f5e974e3ef8d2a475eb983511759
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-11-23 23:10:34 +0000

    la version 3.0.0 utilise un WHERE plutot que des requetes imbriqué problème: si on a un {!id_mot=XX} comme critère, le compilateur de SPIP produit des requetes SQL au mieux non pertinentes, au pire qui plante on revient à l'ancien système
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@100642 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- critere_mots/trunk/critere_mots_fonctions.php
+++ critere_mots/trunk/critere_mots_fonctions.php
@@ -38,112 +38,43 @@ function critere_mots_dist($idb, &$boucles, $crit,$id_ou_titre=false) {
 	} else {
 		$quoi = '@$Pile[0]["mots"]';
 	}
-	if (!_CRITERE_MOTS_OPTIMISE){
-		$boucle->hash .= '
-		// {MOTS}
-		$prepare_mots = charger_fonction(\'prepare_mots\', \'inc\');
-		$mots_where = $prepare_mots('.$quoi.', "'.$boucle->id_table.'", "'.$crit->cond.'", '.$score.', "' . $boucle->sql_serveur . '","'.$id_ou_titre.'");
-		';
+	$boucle->hash .= '
+	// {MOTS}
+	$prepare_mots = charger_fonction(\'prepare_mots\', \'inc\');
+	$mots_where = $prepare_mots('.$quoi.', "'.$boucle->id_table.'", "'.$crit->cond.'", '.$score.', "' . $boucle->sql_serveur . '","'.$id_ou_titre.'");
+	';
 
-		$t = $boucle->id_table . '.' . $boucle->primary;
-		if (!in_array($t, $boucles[$idb]->select)) {
-			$boucle->select[]= $t; # pour postgres, neuneu ici
-		}
-
-		$boucle->where[] = "\n\t\t".'$mots_where';
-		if ($tri==True) {
-
-			$boucle->jointures[]="mots_liens" ;
-			$boucle->from['mots_liens'] = "spip_mots_liens";
-			$boucle->join["mots_liens"] = array(
-			    "'$boucle->id_table'",
-			    "'id_objet'",
-			    "'$id_objet'",
-			    "'mots_liens.objet='.sql_quote('$objet_delatable')");
-			$boucle->where[] = "\n\t\t".'sql_in(\'mots_liens.id_mot\',sql_quote('.$quoi.'))';
-			$boucle->group[] = "mots_liens.id_objet";
-			if ($crit->param[0][2]->texte == "tri") // si dans le sens ascendant
-			    $boucle->order[] = "'COUNT(mots_liens.id_objet) ASC'";
-			else
-			    $boucle->order[] = "'COUNT(mots_liens.id_objet) DESC'";
-			
-			// Pseudo critère "Si"
-			$boucle->hash .= "\n\tif (!isset(\$si_init)) { \$command['si'] = array(); \$si_init = true; }\n";
-			$boucle->hash .= "\t\$command['si'][] = (count($quoi) > '0');";
-		}
-	}
-	else{
-
-			$boucle->hash .= '
-				// {MOTS}
-				$prepare_mots_where = charger_fonction(\'prepare_mots_where\', \'inc\');
-				$mots_where = $prepare_mots_where('.$quoi.', "'.$crit->cond.'","'.$id_ou_titre.'");
-				$prepare_mots_having = charger_fonction(\'prepare_mots_having\', \'inc\');
-				$mots_having = $prepare_mots_having('.$quoi.', '.$score.', "'.$crit->cond.'");
-				$prepare_mots_group = charger_fonction(\'prepare_mots_group\', \'inc\');
-				$mots_group = $prepare_mots_group('.$quoi.',"'.$_table.'","'.$objet_delatable.'","'.$crit->cond.'");
-				';
-			 $boucle->from['mots_liens'] = "spip_mots_liens";
-			 $boucle->join["mots_liens"] = array(
-			     "'$_table'",
-			     "'id_objet'",
-			     "'id_$objet_delatable'",
-			     "'mots_liens.objet='.sql_quote('$objet_delatable')");
-			$boucle->group[] = '$mots_group';
-			$boucle->having[] = '$mots_having';
-			$boucle->where[] = '$mots_where';
-			if ($tri==True){
-				if ($crit->param[0][2]->texte == "tri") // si dans le sens ascendant
-						$boucle->order[] = "'COUNT(mots_liens.id_objet) ASC'";
-				else
-						$boucle->order[] = "'COUNT(mots_liens.id_objet) DESC'";
-				// Pseudo critère "Si"
-				$boucle->hash .= "\n\tif (!isset(\$si_init)) { \$command['si'] = array(); \$si_init = true; }\n";
-				$boucle->hash .= "\t\$command['si'][] = (count($quoi) > '0');";
-			}
+	$t = $boucle->id_table . '.' . $boucle->primary;
+	if (!in_array($t, $boucles[$idb]->select)) {
+		$boucle->select[]= $t; # pour postgres, neuneu ici
 	}
 
-}
+	$boucle->where[] = "\n\t\t".'$mots_where';
+	if ($tri==True) {
 
-function inc_prepare_mots_group_dist($mots,$table,$objet_delatable,$cond='') {
-	if (!is_array($mots)
-	OR !$mots = array_filter($mots)) {
-		// traiter le cas {mots?}
-		if ($cond)
-			return '';
+		$boucle->jointures[]="mots_liens" ;
+		$boucle->from['mots_liens'] = "spip_mots_liens";
+		$boucle->join["mots_liens"] = array(
+		    "'$boucle->id_table'",
+		    "'id_objet'",
+		    "'$id_objet'",
+		    "'mots_liens.objet='.sql_quote('$objet_delatable')");
+		$boucle->where[] = "\n\t\t".'sql_in(\'mots_liens.id_mot\',sql_quote('.$quoi.'))';
+		$boucle->group[] = "mots_liens.id_objet";
+		if ($crit->param[0][2]->texte == "tri") // si dans le sens ascendant
+		    $boucle->order[] = "'COUNT(mots_liens.id_objet) ASC'";
 		else
-		// {mots} mais pas de mot dans l'url
-			return '0=1';
+		    $boucle->order[] = "'COUNT(mots_liens.id_objet) DESC'";
+		
+		// Pseudo critère "Si"
+		$boucle->hash .= "\n\tif (!isset(\$si_init)) { \$command['si'] = array(); \$si_init = true; }\n";
+		$boucle->hash .= "\t\$command['si'][] = (count($quoi) > '0');";
 	}
-	return $table.'.id_'.$objet_delatable;
+
+
 }
 
-function inc_prepare_mots_having_dist($mots,$score='100%',$cond='') {
-	if (!is_array($mots)
-	OR !$mots = array_filter($mots)) {
-		// traiter le cas {mots?}
-		if ($cond)
-			return '';
-		else
-		// {mots} mais pas de mot dans l'url
-			return '0=1';
-	}
-	$score = trim ($score);
-	$i = count ($mots);
-	// si on a un % dans le score, c'est que c'est un %age
-	if (substr($score,-1)=='%'){
-		 $score = str_replace('%','',$score);
-		 $score = ceil($score/100 * $i) ;
-	}
-	elseif ((0 < $score) and ($score < 1)){
-		 $score = ceil($score * $i) ;
-	}
-	else{
-		  pass ;
-		 }
-	
-	return ("COUNT(mots_liens.id_objet) >= $score"); 
-}
+
 
 function critere_mots_selon_id_dist($idb, &$boucles, $crit){
     critere_mots_dist($idb, $boucles, $crit,'id');
@@ -152,43 +83,6 @@ function critere_mots_selon_titre_dist($idb, &$boucles, $crit){
     critere_mots_dist($idb, $boucles, $crit,'titre');
 }
 
-function inc_prepare_mots_where_dist($mots, $cond=false,$id_ou_titre=false) {
-	$score = trim($score);
-	if (!is_array($mots)
-	OR !$mots = array_filter($mots)) {
-		// traiter le cas {mots?}
-		if ($cond)
-			return '';
-		else
-		// {mots} mais pas de mot dans l'url
-			return '0=1';
-	}
-	
-	$wh = array();
-  //selon le cas, on sélectionne sur les titres ou sur les id
-  if (!$id_ou_titre){
-      foreach($mots as $mot) {
-          if (preg_match(',^[1-9][0-9]*$,', $mot))
-              $id_mot = $mot;
-          else
-              $id_mot = sql_getfetsel('id_mot', 'spip_mots', 'titre='.sql_quote($mot));
-          		$wh[] = "(mots_liens.id_mot=".sql_quote($id_mot).")";
-      }
-  }
-	  elseif($id_ou_titre == 'id'){
-	   	foreach($mots as $id_mot) {
-			$wh[] = "(mots_liens.id_mot=".sql_quote($id_mot).")";
-	   }
-	}
-	elseif($id_ou_titre == 'titre'){
-		foreach($mots as $mot) {
-	        $id_mot = sql_getfetsel('id_mot', 'spip_mots', 'titre='.sql_quote($mot));
-					$wh[] = "(mots_liens.id_mot=".sql_quote($id_mot).")";	   
-				}
-	}
-  $str = implode($wh,' OR ');
-  return "($str)";
-}
 
 function inc_prepare_mots_dist($mots, $table='articles', $cond=false, $score, $serveur='',$id_ou_titre=false) {
   $score = trim($score);
