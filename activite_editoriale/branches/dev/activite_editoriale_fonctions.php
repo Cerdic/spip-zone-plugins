ad4d2c2d61df2e8df1123471f5a2600622ff98b5
chankalan@free.fr <chankalan@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-11-29 12:15:26 +0000

    changement de version y, état dev, PSR
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@100752 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- activite_editoriale/branches/dev/activite_editoriale_fonctions.php
+++ activite_editoriale/branches/dev/activite_editoriale_fonctions.php
@@ -1,12 +1,7 @@
 <?php
 if (!defined("_ECRIRE_INC_VERSION")) return;
+
+// obtenir l'age de la rubrique en nombre de jour
 function age_rubrique($date_str) {
-	/*
-	$date = new DateTime();
-	$age = $date->diff(new DateTime($date_str));
-	return $age->d;
-	*/
 	return intval((time() - strtotime($date_str)) / (60 * 60 * 24));
 }
-
-?>
