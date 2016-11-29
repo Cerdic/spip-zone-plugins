141471c9fda2be844d04ee60dbe632d37f778cf3
erational@erational.org <erational@erational.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-11-29 13:43:20 +0000

    version 1.1.0 - ajout d'un menu de configuration - ajout d'un modèle général - on reprend le modèle article
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@100760 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ metaplus/trunk/lang/paquet-metasplus_fr.php
@@ -0,0 +1,12 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// M
+	'metasplus_description' => 'Améliorez l\'indexation de vos articles dans les moteurs et leur affichage sur les réseaux sociaux grâce aux métadonnées Dublin Core, Open Graph et Twitter Card.',
+	'metasplus_slogan' => 'Métadonnées pour Open Graph, Duclin Core et Twitter',
+);
