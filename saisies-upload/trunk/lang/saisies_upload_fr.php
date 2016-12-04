37aa73f4b477307a09f229181c6180111f38270d
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-12-04 12:58:53 +0000

    début pour une saisie simple : envoie d'un document en précisent le mimetype autorisé
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@100846 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ saisies-upload/trunk/lang/saisies_upload_fr.php
@@ -0,0 +1,23 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-cvtupload
+// Langue: fr
+// Date: 13-06-2012 14:56:15
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// C
+	'saisie_upload_titre' => 'Fichier à envoyer',
+	'saisie_upload_explication' => 'Un fichier à envoyer, qui sera stocké dans la médiathèque.',
+// O
+	'option_groupe_utilisation_mime_type' => 'Utilisation (type MIME)', 		
+	'option_uniquement_mime_type_label' => 'Uniquement les types MIME suivants', 
+	'option_tous_mime_type_label' =>'Autoriser tous les types MIME permis par SPIP',
+
+);
