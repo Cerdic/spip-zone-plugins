b9be185526acd75fd44f92c01f4a7b286b4fb09f
vlentz@churchill.fr <vlentz@churchill.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-12-08 04:34:16 +0000

    [Salvatore] [source:_plugins_/selections_editoriales/trunk/lang/ selections_contenu] Export depuis http://trad.spip.net de la langue en
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@100915 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ selections_editoriales/trunk/lang/selections_contenu_en.php
@@ -0,0 +1,51 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/selections_contenu?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'ajouter_lien_selections_contenu' => 'Add this selected content', # RELIRE
+
+	// C
+	'champ_css_explication' => 'One or more CSS classes to add to this selected content', # RELIRE
+	'champ_css_label' => 'CSS classes', # RELIRE
+	'champ_descriptif_label' => 'Description',
+	'champ_id_selection_label' => 'Selection',
+	'champ_titre_explication' => 'If the title is empty, it will try to fill itself magically', # RELIRE
+	'champ_titre_label' => 'Title',
+	'champ_url_explication' => 'The URL of a page (http://example) or the shortcut of a SPIP content (article123, rubrique456, etc.).', # RELIRE
+	'champ_url_label' => 'URL',
+
+	// D
+	'deplacer' => 'Drag and drop this content', # RELIRE
+	'deplacer_apres' => 'Move after', # RELIRE
+	'deplacer_avant' => 'Move before', # RELIRE
+
+	// I
+	'icone_creer_selections_contenu' => 'Create a selected content', # RELIRE
+	'icone_modifier_selections_contenu' => 'Modify this selected content', # RELIRE
+	'info_1_selections_contenu' => 'A selected content', # RELIRE
+	'info_aucun_selections_contenu' => 'No selected content', # RELIRE
+	'info_nb_selections_contenus' => '@nb@ selected contents', # RELIRE
+	'info_selections_contenus_auteur' => 'Selected contents by this author', # RELIRE
+
+	// R
+	'retirer_lien_selections_contenu' => 'Remove this selected content', # RELIRE
+	'retirer_tous_liens_selections_contenus' => 'Remove all selected contents', # RELIRE
+
+	// T
+	'texte_ajouter_selections_contenu' => 'Add a selected content', # RELIRE
+	'texte_changer_statut_selections_contenu' => 'This selected content is:', # RELIRE
+	'texte_creer_associer_selections_contenu' => 'Create and link a selected content', # RELIRE
+	'titre_langue_selections_contenu' => 'Language of this selected content', # RELIRE
+	'titre_logo_selections_contenu' => 'Logo of this selected content', # RELIRE
+	'titre_selections_contenu' => 'Selected content', # RELIRE
+	'titre_selections_contenus' => 'Selected contents', # RELIRE
+	'titre_selections_contenus_rubrique' => 'Selected contents in the section' # RELIRE
+);
