4dd952057a27c56b71bc3fff4ed8eb254a058579
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-12-10 13:24:07 +0000

    adapation de la saisie fichiers à la pluralité des fichiers, nécessite la nouvelle version du plugin saisies pour ne pas avoir de problème de label
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@100972 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- cvt-upload/trunk/lang/cvtupload_fr.php
+++ cvt-upload/trunk/lang/cvtupload_fr.php
@@ -15,6 +15,8 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// S
 	'saisie_fichiers_nb_fichiers_explication' => 'Ceci détermine le nombre de champs d\'envoi de fichier disponibles.', 	
 	'saisie_fichiers_nb_fichiers_label' => 'Nombre maximum de fichiers à envoyer',
+	'saisie_fichiers_plusieur_label'=> 'Fichier n° @nb@',
+	'saisie_fichiers_unseul_label'=> 'Fichier unique',
 	'saisie_fichiers_titre' => 'Un ou plusieurs fichiers', 
 	'saisie_fichiers_explication' => 'Envoyer un ou plusieurs fichiers en s\'assurant de ne pas avoir à les renvoyer en cas de non validation des autres champs du formulaire.'
 );
