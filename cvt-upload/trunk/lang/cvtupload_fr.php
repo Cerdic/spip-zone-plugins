192331d8ddc4734d335fe7489673522b10afe868
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-12-10 15:57:13 +0000

    chaînes de langue déplacées dans verifier
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@100990 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- cvt-upload/trunk/lang/cvtupload_fr.php
+++ cvt-upload/trunk/lang/cvtupload_fr.php
@@ -6,12 +6,6 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-	// E
-	'erreur_dimension_image' => 'Le fichier @name@ est trop grand : @taille@ (maximum @taille_max@).',
-	'erreur_taille_image' => 'Le fichier @name@ est trop lourd : @taille@ (maximum @taille_max@).',
-	'erreur_type_image' => 'Le fichier @name@ n\'est pas une image',
-	'erreur_type_non_autorise' => 'Le fichier @name@ n\'est pas d\'un type autorisé.', 
-
 	// S
 	'saisie_fichiers_nb_fichiers_explication' => 'Ceci détermine le nombre de champs d\'envoi de fichier disponibles.', 	
 	'saisie_fichiers_nb_fichiers_label' => 'Nombre maximum de fichiers à envoyer',
