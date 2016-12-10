0d857662d2730e15cb25febf9255cfb248197b24
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-12-10 13:23:51 +0000

    on fait un yaml plus conforme aux standard des yaml des autres saisies. Pour le moment on ne gère pas encore la question des vérifications. Il reste à adapter le .html concernant le nombre d'envoi
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@100971 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- cvt-upload/trunk/lang/cvtupload_fr.php
+++ cvt-upload/trunk/lang/cvtupload_fr.php
@@ -10,7 +10,12 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'erreur_dimension_image' => 'Le fichier @name@ est trop grand : @taille@ (maximum @taille_max@).',
 	'erreur_taille_image' => 'Le fichier @name@ est trop lourd : @taille@ (maximum @taille_max@).',
 	'erreur_type_image' => 'Le fichier @name@ n\'est pas une image',
-	'erreur_type_non_autorise' => 'Le fichier @name@ n\'est pas d\'un type autorisé.' 
-	
+	'erreur_type_non_autorise' => 'Le fichier @name@ n\'est pas d\'un type autorisé.', 
+
+	// S
+	'saisie_fichiers_nb_fichiers_explication' => 'Ceci détermine le nombre de champs d\'envoi de fichier disponibles.', 	
+	'saisie_fichiers_nb_fichiers_label' => 'Nombre maximum de fichiers à envoyer',
+	'saisie_fichiers_titre' => 'Un ou plusieurs fichiers', 
+	'saisie_fichiers_explication' => 'Envoyer un ou plusieurs fichiers en s\'assurant de ne pas avoir à les renvoyer en cas de non validation des autres champs du formulaire.'
 );
 
