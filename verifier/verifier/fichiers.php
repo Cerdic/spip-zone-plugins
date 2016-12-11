26e6ab1b52e43bd24f5d31d4b5540e05e82c3ec1
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-12-11 13:18:35 +0000

    début de vérification de fichiers, pour le moment vérifier mime_type précis
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@101022 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ verifier/verifier/fichiers.php
@@ -0,0 +1,67 @@
+<?php
+
+// Sécurité
+if (!defined("_ECRIRE_INC_VERSION")) {
+	return;
+}
+
+/**
+ * Vérifier une saisie d'envoi de fichiers
+ *
+ * @param array $valeur
+ *   Le sous tableau de $_FILES à vérifier, $_FILES['logo'] par exemple
+ *   Doit être un champ avec un ou plusieurs upload
+ * @param array $options
+ *   Options à vérifier :
+ *   - mime au choix 'image','tous_spip','specifique'
+ *   - mime_specifique (si l'option 'mime_specifique' est choisi ci-dessus)
+ *   - taille_max (en Kio)
+ *   - largeur_max (en px)
+ *   - hauteur_max (en px)
+ * @param array &$erreurs_par_fichier
+ *   Un tableau, passé par référence, qui contient le détail des erreurs fichier de $_FILES['fichier'] par fichier
+ * @return string
+ */
+function verifier_fichiers_dist($valeur, $options, &$erreurs_par_fichier) {
+	if (!is_array($valeur['tmp_name'])){//si on reçoit une info de type fichier unique, on bascule comme si on était fichier multiple
+		$old_valeur = $valeur;
+		$valeur = array();
+		foreach ($old_valeur as $propriete=>$val){
+			$valeur[$propriete][0] = $val;
+		}
+	}
+	
+	foreach ($valeur['tmp_name'] as $cle=>$tmp_name){//On parcourt tous les fichiers
+		if ($valeur['error'][$cle]!=0){//On vérifie uniquement les fichiers bien expediés
+			continue;	
+		}
+		if ($erreur=verifier_fichier_mime($valeur,$cle,$options)){// On commence par vérifier le type
+			$erreurs_par_fichier[$cle] = $erreur;
+		}
+	}
+	if (!empty($erreurs_par_fichier)){
+		return implode($erreurs_par_fichier,"<br />"); 
+	}
+	return '';
+}
+
+/**
+ * Vérifier le mime type d'une saisie d'envoi de fichiers
+ *
+ * @param array $valeur
+ *   Le sous tableau de $_FILES à vérifier, $_FILES['logo'] par exemple
+ *   Doit être un champ plusieurs uploads
+ * @param int $cle
+ *   La clé du tableau qu'on vérifie
+ * @param array $options
+ *   Les options tels que passés à verifier_fichiers()
+ * @return string
+ */
+function verifier_fichier_mime($valeur,$cle,$options){
+	if ($options['mime'] == 'specifique'){
+		if (!in_array($valeur['type'][$cle],$options['mime_specifique'])){
+			return _T('verifier:erreur_type_non_autorise',array('name'=>$valeur['name'][$cle]));
+		}	
+	}	
+	return '';
+}
