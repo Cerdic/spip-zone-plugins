270ef949104d078573544766b77bea54ea0fd0ee
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-12-12 04:33:15 +0000

    [Salvatore] [source:_plugins_/cvt-upload/trunk/lang/ paquet-cvtupload] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@101066 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- cvt-upload/trunk/lang/paquet-cvtupload_fr.php
+++ cvt-upload/trunk/lang/paquet-cvtupload_fr.php
@@ -1,19 +1,13 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-cvtupload
-// Langue: fr
-// Date: 13-06-2012 14:56:15
-// Items: 2
-
-if (!defined('_ECRIRE_INC_VERSION')) return;
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/cvt-upload/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// C
+	// C
 	'cvtupload_description' => 'Fournit des outils pour aider à charger des fichiers proprement depuis un formulaire CVT.',
-	'cvtupload_slogan' => 'Télécharger proprement des fichiers depuis un formulaire CVT',
+	'cvtupload_slogan' => 'Télécharger proprement des fichiers depuis un formulaire CVT'
 );
-?>
