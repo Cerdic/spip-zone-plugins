f72cdecfe95abbab8eecadeded339d2ddbf20bc2
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-12-12 04:32:57 +0000

    [Salvatore] [source:_plugins_/cvt-upload/trunk/lang/ cvtupload] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@101065 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- cvt-upload/trunk/lang/cvtupload_fr.php
+++ cvt-upload/trunk/lang/cvtupload_fr.php
@@ -1,17 +1,17 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-if (!defined('_ECRIRE_INC_VERSION')) return;
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/cvt-upload/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// S
-	'saisie_fichiers_nb_fichiers_explication' => 'Ceci détermine le nombre de champs d\'envoi de fichier disponibles.', 	
+	'saisie_fichiers_explication' => 'Envoyer un ou plusieurs fichiers en s’assurant de ne pas avoir à les renvoyer en cas de non validation des autres champs du formulaire.',
+	'saisie_fichiers_nb_fichiers_explication' => 'Ceci détermine le nombre de champs d’envoi de fichier disponibles.',
 	'saisie_fichiers_nb_fichiers_label' => 'Nombre maximum de fichiers à envoyer',
-	'saisie_fichiers_plusieur_label'=> 'Fichier n° @nb@',
-	'saisie_fichiers_unseul_label'=> 'Fichier unique',
-	'saisie_fichiers_titre' => 'Un ou plusieurs fichiers', 
-	'saisie_fichiers_explication' => 'Envoyer un ou plusieurs fichiers en s\'assurant de ne pas avoir à les renvoyer en cas de non validation des autres champs du formulaire.'
+	'saisie_fichiers_plusieur_label' => 'Fichier n° @nb@',
+	'saisie_fichiers_titre' => 'Un ou plusieurs fichiers',
+	'saisie_fichiers_unseul_label' => 'Fichier unique'
 );
-
