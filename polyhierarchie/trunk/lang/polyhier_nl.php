c9ac624a2a8448d6951f70d4a16300b8bc12aca8
hanjo.spip@gmail.com <hanjo.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-12-13 04:33:52 +0000

    [Salvatore] [source:_plugins_/polyhierarchie/trunk/lang/ polyhier] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@101092 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- polyhierarchie/trunk/lang/polyhier_nl.php
+++ polyhierarchie/trunk/lang/polyhier_nl.php
@@ -16,6 +16,11 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'label_autre_parent' => 'Ook in de rubriek',
 	'label_autres_parents' => 'Ook in de rubrieken',
 	'label_parents' => 'In de rubrieken',
+	'label_profondeur_chemin' => 'Niveau van de polyhiërarchische paden in het privé gedeelte',
+	'label_profondeur_chemin_0' => 'Volledige pad',
+	'label_profondeur_chemin_1' => 'Alleen de bovenliggende rubriek',
+	'label_profondeur_chemin_2' => '2 rubrieken',
+	'label_profondeur_chemin_3' => '3 rubrieken',
 
 	// P
 	'parent_obligatoire' => 'Je moet ten minste één rubriek aangeven',
