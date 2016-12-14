900e776a7a25782d9fa13b7674f8bb7c3b294997
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-12-14 04:33:28 +0000

    [Salvatore] [source:_plugins_/numerotation/trunk/lang/ numero] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@101117 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- numerotation/trunk/lang/numero_fr.php
+++ numerotation/trunk/lang/numero_fr.php
@@ -1,29 +1,25 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-numero
-// Langue: fr
-// Date: 25-04-2012 12:25:32
-// Items: 2
-
-if (!defined('_ECRIRE_INC_VERSION')) return;
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/numerotation/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// N
-	'info_numeroter_objets' => 'Re-numéroter les @objets@',
+	// I
 	'info_denumeroter_objets' => 'Enlever la numerotation des @objets@',
-	'info_numeroter_rubriques' => 'Re-numéroter les sous-rubriques',
 	'info_denumeroter_rubriques' => 'Enlever la numerotation des sous-rubriques',
-	'info_numeroter_secteurs' => 'Re-numéroter les rubriques',
 	'info_denumeroter_secteurs' => 'Enlever la numerotation des rubriques',
+	'info_numeroter_objets' => 'Re-numéroter les @objets@',
+	'info_numeroter_rubriques' => 'Re-numéroter les sous-rubriques',
+	'info_numeroter_secteurs' => 'Re-numéroter les rubriques',
 
+	// L
 	'label_precedent' => 'Classer après',
 	'label_precedent_0' => 'En premier',
 
-	'titre_formulaire_numeroter' => 'Numeroter',
+	// T
 	'texte_sous_rubriques' => 'Sous rubriques',
+	'titre_formulaire_numeroter' => 'Numeroter'
 );
-?>
