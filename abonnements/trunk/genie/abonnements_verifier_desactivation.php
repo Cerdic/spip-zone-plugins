9d23cb6dc1d012c265fc9004e3cc42a501b35606
tcharlss@bravecassine.com <tcharlss@bravecassine.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-12-16 17:39:44 +0000

    version 3.1.13 : quand on vérifie les abonnements à désactiver, tenir compte des dates de fin dépassées, pas juste celles dans le futur (+ léger formatage de la requête).
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@101146 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- abonnements/trunk/genie/abonnements_verifier_desactivation.php
+++ abonnements/trunk/genie/abonnements_verifier_desactivation.php
@@ -8,16 +8,17 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
  */
 function genie_abonnements_verifier_desactivation_dist($time){
 	include_spip('base/abstract_sql');
-	$jourdhui = date('Y-m-d H:i:s');
 	
-	// On va chercher tous les abonnements actifs + ayant une date de fin future + sans job lié
+	// On va chercher tous les abonnements actifs + ayant une date de fin + sans job lié
 	if ($a_changer = sql_allfetsel(
 		'id_abonnement, date_fin',
-		'spip_abonnements as a left join spip_jobs_liens as l on l.objet="abonnement" and l.id_objet=a.id_abonnement left join spip_jobs as j on j.fonction="abonnements_desactiver" and j.id_job=l.id_job',
+		'spip_abonnements as a'.
+			' LEFT JOIN spip_jobs_liens AS l ON l.objet = "abonnement" AND l.id_objet = a.id_abonnement'.
+			' LEFT JOIN spip_jobs AS j ON j.fonction = "abonnements_desactiver" AND j.id_job = l.id_job',
 		array(
 			'a.statut = "actif"',
-			'a.date_fin > '.sql_quote($jourdhui),
-			'l.id_job is null',
+			'a.date_fin > ' . sql_quote('0000-00-00 00:00:00'),
+			'l.id_job IS NULL',
 		)
 	) and is_array($a_changer)) {
 		include_spip('inc/abonnements');
