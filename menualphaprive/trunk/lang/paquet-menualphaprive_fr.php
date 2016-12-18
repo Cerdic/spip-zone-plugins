5c669ca97d55138bb46e4834c62f29d0bc03feb1
nicolas.dorigny@gmail.com <nicolas.dorigny@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-12-18 18:30:39 +0000

    Ajout du plugin Menu privé alphabétique, qui trie les items du menu privé principal (le bandeau) par ordre alphabétique.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@101175 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ menualphaprive/trunk/lang/paquet-menualphaprive_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// M
+	'menualphaprive_description' => '',
+	'menualphaprive_nom'         => 'Menu privé alphabétique',
+	'menualphaprive_slogan'      => 'Trier les items du menu de l\'espace privé par ordre alphabétique.',
+);
---
+++ menualphaprive/trunk/prive/squelettes/inclure/barre-nav_fonctions.php
@@ -0,0 +1,25 @@
+<?php
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+include_spip('inc/bandeau');
+include_spip('action/menu_rubriques');
+
+function sort_menu($menu_complet) {
+
+	foreach ($menu_complet as $menu) {
+		if ($menu->sousmenu) {
+			$sous_menu = $menu->sousmenu;
+			$libelles  = array();
+			foreach ($sous_menu as $key => $row) {
+				$libelles[$key] = _T($row->libelle);
+			}
+			array_multisort($libelles, SORT_ASC, $sous_menu);
+			$menu->sousmenu = $sous_menu;
+		}
+	}
+
+	return $menu_complet;
+}
