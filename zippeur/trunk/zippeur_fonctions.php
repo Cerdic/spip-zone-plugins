e7d561a21a9ffa62d820b928da6084f89f0d43a1
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-12-22 20:48:29 +0000

    notice php en moins
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@101286 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- zippeur/trunk/zippeur_fonctions.php
+++ zippeur/trunk/zippeur_fonctions.php
@@ -89,7 +89,7 @@ function zippeur($array,$date='',$cmd='',$nom='',$plat='oui',$delai='0'){
 	include_spip('inc/flock');
 	$enbase = sql_fetsel('id_zip,fichiers,date_modif','spip_zippeur',"`nom`='$nom'");
 	/* On vérifie si le zip existe*/
-	if (count(preg_files($chemin))==0 or!$enbase['id_zip'] or $enbase['date_modif']!=$date or count($array)!=$enbase['fichiers'] or (_NO_CACHE!=0 and !_NO_CACHE_SAUF_ZIPPEUR)){
+	if (count(preg_files($chemin))==0 or!$enbase['id_zip'] or $enbase['date_modif']!=$date or count($array)!=$enbase['fichiers'] or (_NO_CACHE!=0 and !defined('_NO_CACHE_SAUF_ZIPPEUR'))){
 		
 		if(zippeur_zipper($chemin,$array,$cmd,$plat))
 		{
