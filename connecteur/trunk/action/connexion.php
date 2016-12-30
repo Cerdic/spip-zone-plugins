2782295ffe0f28925cb8f5c8ffcadf1157ccf4ce
p@henix.be <p@henix.be@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2016-12-30 13:10:45 +0000

    spip_logger les erreurs de connexion Facebook
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@101437 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- connecteur/trunk/action/connexion.php
+++ connecteur/trunk/action/connexion.php
@@ -43,28 +43,31 @@ function action_connexion_dist($arg = null) {
 
 		// Envoyer au pipeline les informations de la personne
 		$auteur_info = pipeline('pre_connecteur', $auteur_info);
-
 		// Est-ce que l'email est déjà présent dans la base de donnée ?
-		$verifier = charger_fonction('verifier', 'inc');
-		if (!$verifier($auteur_info['email'], 'email', array('disponible' => true))) {
-			// L'auteur n'est pas encore dans la base de donnée : on le crée
-			$auteur = connecteur_creer_auteur($auteur_info);
-
-			// On enregistre le token
-			connecteur_save_token($auteur['id_auteur'], $type, $token);
-
-			// On va update la source de l'auteur
-			include_spip('action/editer_auteur');
-			auteur_modifier($auteur['id_auteur'], array('source' => $type));
-
-			// Et enfin on connecte la personne
-			connecteur_connecter($auteur);
+		if (empty($auteur_info['email'])) {
+			spip_log('aucun email fourni par Facebook', 'facebook'._LOG_ERREUR);
+			spip_log($auteur_info, 'facebook'._LOG_ERREUR);
 		} else {
-			// Sinon, on connecte l'auteur
-			$auteur = connecteur_connecter($auteur_info);
-			connecteur_save_token($auteur['id_auteur'], $type, $token);
-		}
+			$verifier = charger_fonction('verifier', 'inc');
+			if (!$verifier($auteur_info['email'], 'email', array('disponible' => true))) {
+				// L'auteur n'est pas encore dans la base de donnée : on le crée
+				$auteur = connecteur_creer_auteur($auteur_info);
 
+				// On enregistre le token
+				connecteur_save_token($auteur['id_auteur'], $type, $token);
+
+				// On va update la source de l'auteur
+				include_spip('action/editer_auteur');
+				auteur_modifier($auteur['id_auteur'], array('source' => $type));
+
+				// Et enfin on connecte la personne
+				connecteur_connecter($auteur);
+			} else {
+				// Sinon, on connecte l'auteur
+				$auteur = connecteur_connecter($auteur_info);
+				connecteur_save_token($auteur['id_auteur'], $type, $token);
+			}
+		}
 		// Envoyer aux pipelines
 		$desc = pipeline('post_connecteur', array('auteur' => $auteur, 'info' => $auteur_info));
 	}
