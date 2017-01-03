7279639506a5df9eaf0fee6afd6c5853b7c15b02
id@dutailly.net <id@dutailly.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-01-03 04:32:43 +0000

    [Salvatore] [source:_plugins_/bonne_annee/lang/ paquet-bonne_annee] Export depuis http://trad.spip.net de la langue es
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@101491 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ bonne_annee/lang/paquet-bonne_annee_es.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-bonne_annee?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'bonne_annee_description' => 'Cada año durante el mes de Enero, muestra un mensaje de felicitación para el año nuevo.
+Añadir <code>#INCLURE{fond=bonne_annee}</code> en su paginas.',
+	'bonne_annee_nom' => 'Feliz año nuevo'
+);
