bb5adebe457f0da785a887bfa1d4c569551504c9
marcimat@rezo.net <marcimat@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-01-08 22:26:48 +0000

    Plugin «Minidoc» pour tester différents affichages dans la liste des documents liés à un objet éditorial.
    3 boutons apparaîssent à droite des titres (Illustrations, portfolio, documents). 
    - Le premier (par défaut) est l'affichage actuel
    - Le second est un affichage en liste, mais bien plus compact
    - Le 3è un affichage en cases compactes également.
    
    C'est tout en JS + Css.
    Les boutons sont petits (et pas très jojo) (si quelqu'un à mieux). Ça va à la souris évidemment, mais c'est pas une présentation idéale en tactile (c'est mieux que rien).
    
    Concept à tester / explorer / affiner. 
    
    
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@101566 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ minidoc/trunk/javascript/minidoc.js
@@ -0,0 +1,39 @@
+(function($) {
+	$(document).ready(function(){
+		var minidoc = function() {
+			$('#portfolios h3').each(function () {
+				var titre = $(this);
+				var liste = titre.next('.liste_items.documents');
+
+				titre.append(
+					"<div class='minidoc'>"
+					+ "<span class='icone grand on' title='Affichage en grand'></span>"
+					+ "<span class='icone liste' title='Affichage en liste courte'></span>"
+					+ "<span class='icone cases' title='Affichage en cases'></span>"
+					+ "</div>"
+				);
+			});
+
+			$('.minidoc > .grand').click(function () {
+				$(this).parent().find('.icone').removeClass('on').end().end().addClass('on');
+				var liste = $(this).parents('h3').next('.liste_items.documents');
+				liste.removeClass('documents_cases').removeClass('documents_liste');
+			});
+
+			$('.minidoc > .liste').click(function () {
+				$(this).parent().find('.icone').removeClass('on').end().end().addClass('on');
+				var liste = $(this).parents('h3').next('.liste_items.documents');
+				liste.removeClass('documents_cases').addClass('documents_liste');
+			});
+
+			$('.minidoc > .cases').click(function () {
+				$(this).parent().find('.icone').removeClass('on').end().end().addClass('on');
+				var liste = $(this).parents('h3').next('.liste_items.documents');
+				liste.removeClass('documents_liste').addClass('documents_cases');
+			});
+		}
+		minidoc();
+		onAjaxLoad(minidoc);
+	});
+
+})(jQuery);
---
+++ minidoc/trunk/lang/paquet-minidoc_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// M
+	'minidoc_description' => '',
+	'minidoc_nom' => 'Documents en vue réduite',
+	'minidoc_slogan' => 'Permet de réduire la vue des documents sur les objets éditoriaux.',
+);
