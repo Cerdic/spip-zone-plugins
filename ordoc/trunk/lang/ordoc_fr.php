0edbe5a4154c34d104ffe797ab53f4d5a534ed99
marcimat@rezo.net <marcimat@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-01-08 10:30:46 +0000

    Plugin «Ordoc» permettant d'ordonner les documents liés sur des objets. (en développement mais fonctionnel).
    - /!\ Surcharge du plugin media : prive/squelettes/inclure/portfolio-documents.html
    - Ajoute un champ "ordre" sur la table spip_documents_liens
    - Ajoute une icone "deplacer" à côté du lien "modifier" sur les listes de documents dans la vue d'un objet éditorial (sur les listes d'illustrations, portfolio et documents)
    - Ajoute une action qui enregistre, après déplacement l'ordre des documents de cette liste (l'action est plus ou moins générique et pourrait fonctionner avec d'autres objets) (1)
    - portfolio-documents.html est surchargé pour : - trier par "ordre" les 3 listes en priorité, ajouter le JS.
    - le pipeline "document_desc_actions" lui ajoute le curseur de déplacement sur le modèle d'affichage d'un document.
    
    Notes:
    1) Le test d'autorisation dans cette action n'est pas parfait cependant.
    2) Il y a un souci avec le plugin «Roles de documents» cependant, qui ne conserve pas l'ordre actuel de l'élément lorsqu'un rôle est ajouté.
    3) Si quelqu'un veut dessiner/fournir un logo meilleur, ça serait super :)
    
    
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@101545 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ ordoc/trunk/lang/ordoc_fr.php
@@ -0,0 +1,19 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	// D
+	'deplacer_document' => 'Déplacer ce document',
+
+	// E
+	'erreur_autorisation' => 'Échec de l’autorisation.',
+	'erreur_deplacement_impossible' => 'Déplacement impossible.',
+	'erreur_objet_absent' => 'Informations reçues incomplètes.',
+
+	// O
+	'ordoc_titre' => 'Ordre des documents',
+);
