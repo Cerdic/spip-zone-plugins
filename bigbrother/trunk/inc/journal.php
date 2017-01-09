b6923468c314db88b8bb0ff80afd5e570a731616
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-01-09 17:44:34 +0000

    PSR
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@101596 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- bigbrother/trunk/inc/journal.php
+++ bigbrother/trunk/inc/journal.php
@@ -1,9 +1,11 @@
 <?php
 
-if (!defined("_ECRIRE_INC_VERSION")) return;
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
 
-/*
+/**
  * Consigner une phrase dans le journal de bord du site
  *
  * Cette fonction surcharge celle de SPIP, afin d'enregistrer les informations en base
@@ -19,39 +21,40 @@ function inc_journal_dist($phrase, $opt = array()) {
 	static $action = array();
 	
 	// Si c'est un robot ou s'il n'y a aucune phrase, on ne fait jamais rien
-	if (_IS_BOT or !strlen($phrase)){
+	if (_IS_BOT or !strlen($phrase)) {
 		return false;
 	}
 	
 	// On ajoute les précisions à la fin de la phrase de log
-	if ($opt)
+	if ($opt) {
 		$phrase .= " :: ".str_replace("\n", ' ', join(', ',$opt));
+	}
 	
 	// Garde-t-on aussi l'IP en mémoire ?
-	if((lire_config('bigbrother/enregistrer_ip') == 'oui') && !$opt['infos']['ip']){
+	if((lire_config('bigbrother/enregistrer_ip') == 'oui') and !$opt['infos']['ip']) {
 		$opt['infos']['ip'] = $GLOBALS['ip'];
 	}
 	
 	// On peut surcharger la fonction pour une action précise
-	if($f = charger_fonction($opt['faire'],'journal',true)){
+	if($f = charger_fonction($opt['faire'], 'journal', true)) {
 		$ok = $f($opt);
 	}
 	// Sinon par défaut on enregistre en base de données
-	else{
+	else {
 		$champs = array(
 			'id_auteur' => $opt['qui'],
-			'action' => $opt['faire'],
-			'id_objet' => $opt['id'],
-			'objet' => $opt['quoi'],
-			'infos' => $opt['infos'],
-			'date' => $opt['date'] ? $opt['date'] : date('Y-m-d H:i:s', time()),
+			'action'    => $opt['faire'],
+			'id_objet'  => $opt['id'],
+			'objet'     => $opt['quoi'],
+			'infos'     => $opt['infos'],
+			'date'      => $opt['date'] ? $opt['date'] : date('Y-m-d H:i:s', time()),
 		);
 		// Envoyer aux plugins
 		$champs = pipeline('pre_edition',
 			array(
 				'args' => array(
 					'table' => 'spip_journal',
-					'action'=>'inserer'
+					'action' =>'inserer'
 				),
 				'data' => $champs
 			)
@@ -68,31 +71,40 @@ function inc_journal_dist($phrase, $opt = array()) {
 		if (
 			!isset($action[$champs['action']][$champs['objet']][$champs['id']]['id_journal'])
 			or !intval($action[$champs['action']][$champs['objet']][$champs['id']]['id_journal'])
-		){
+		) {
 			$infos = $champs['infos'];
+			
 			// Si les infos sont vides, on fait chaine vide
-			if (is_null($infos) or empty($infos)){ $infos = ''; }
+			if (is_null($infos) or empty($infos)){
+				$infos = '';
+			}
+			
 			// On sérialise les infos supplémentaires pour la base
-			if(is_array($champs['infos'])){
+			if(is_array($champs['infos'])) {
 				$champs['infos'] = serialize($infos);
 			}
+			
 			// On insère
 			$ok = sql_insertq(
 				'spip_journal',
 				$champs
 			);
+			
 			// On garde en mémoire ce qui vient d'être journalisé dans ce hit PHP
 			$action[$champs['action']][$champs['objet']][$champs['id']] = array('id_journal'=>$ok,'infos'=>$infos);
 		}
 		// Sinon on met à jour, et on fusionne les infos
-		else{
+		else {
 			$id_journal = $action[$champs['action']][$champs['objet']][$champs['id']]['id_journal'];
 			$infos = $action[$champs['action']][$champs['objet']][$champs['id']]['infos'];
-			if (is_array($champs['infos']) and is_array($infos)){
+			
+			if (is_array($champs['infos']) and is_array($infos)) {
 				// On fusionne les infos supplémentaires
 				$infos = array_merge_recursive($infos, $champs['infos']);
 				// Si les infos sont vides, on fait chaine vide
-				if (empty($infos)){ $infos = ''; }
+				if (empty($infos)){
+					$infos = '';
+				}
 				// On sérialise pour la base
 				$champs['infos'] = serialize($infos);
 				// On met à jour la ligne en base
@@ -102,7 +114,10 @@ function inc_journal_dist($phrase, $opt = array()) {
 					'id_journal = '.intval($id_journal)
 				);
 				// On met à jour la variable statique
-				$action[$champs['action']][$champs['objet']][$champs['id']] = array('id_journal'=>$$id_journal,'infos'=>$infos);
+				$action[$champs['action']][$champs['objet']][$champs['id']] = array(
+					'id_journal' => $$id_journal,
+					'infos'      => $infos
+				);
 			}
 		}
 	}
@@ -113,5 +128,3 @@ function inc_journal_dist($phrase, $opt = array()) {
 	
 	return $ok;
 }
-
-?>
