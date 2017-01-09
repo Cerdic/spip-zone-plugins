ba5aa3cb98dd5ad3132499dc38ff39a071391d8c
marcimat@rezo.net <marcimat@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-01-09 12:35:44 +0000

    Sur une idée de nicod_ : Itiliser des cookies pour sauver le choix de selection des illustrations / portfolio et documents (séparément). 3 cookies sont utilisés donc.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@101585 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- minidoc/trunk/javascript/minidoc.js
+++ minidoc/trunk/javascript/minidoc.js
@@ -5,6 +5,11 @@
 				var titre = $(this);
 				var liste = titre.next('.liste_items.documents');
 
+				var identifiant = liste.attr('id');
+				if ($.inArray(identifiant, ['illustrations', 'portfolio', 'documents']) < 0) {
+					identifiant = null;
+				}
+
 				titre.append(
 					"<div class='minidoc'>"
 					+ "<span class='icone grand on' title='Affichage en grand'></span>"
@@ -13,23 +18,37 @@
 					+ "</div>"
 				);
 
-				titre.find('.minidoc > .grand').click(function () {
-					$(this).parent().find('.icone').removeClass('on').end().end().addClass('on');
-					var liste = $(this).parents('h3').next('.liste_items.documents');
+				var changer_affichage_doccuments = function(me, bouton, classe) {
+					$(me).parent().find('.icone').removeClass('on').end().end().addClass('on');
+					var liste = $(me).parents('h3').next('.liste_items.documents');
 					liste.removeClass('documents_cases').removeClass('documents_liste');
-				});
+					if (classe) {
+						liste.addClass(classe);
+					}
+					if (identifiant) {
+						$.cookie('affichage-' + identifiant, bouton);
+					}
+				};
 
-				titre.find('.minidoc > .liste').click(function () {
-					$(this).parent().find('.icone').removeClass('on').end().end().addClass('on');
-					var liste = $(this).parents('h3').next('.liste_items.documents');
-					liste.removeClass('documents_cases').addClass('documents_liste');
+				titre.find('.minidoc > .grand').click(function () {
+					changer_affichage_doccuments(this, 'grand', null);
 				});
 
 				titre.find('.minidoc > .cases').click(function () {
-					$(this).parent().find('.icone').removeClass('on').end().end().addClass('on');
-					var liste = $(this).parents('h3').next('.liste_items.documents');
-					liste.removeClass('documents_liste').addClass('documents_cases');
+					console.log('clic');
+					changer_affichage_doccuments(this, 'cases', 'documents_cases');
 				});
+
+				titre.find('.minidoc > .liste').click(function () {
+					changer_affichage_doccuments(this, 'liste', 'documents_liste');
+				});
+
+				if (identifiant) {
+					var defaut = $.cookie('affichage-' + identifiant);
+					if (defaut) {
+						titre.find('.minidoc > .' + defaut).trigger('click');
+					}
+				}
 			});
 		}
 		minidoc();
