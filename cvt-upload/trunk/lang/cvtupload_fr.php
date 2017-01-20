824d3b14c855e940f05aee561c7a6ad06bd70302
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-01-20 04:32:50 +0000

    [Salvatore] [source:_plugins_/cvt-upload/trunk/lang/ cvtupload] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@101842 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- cvt-upload/trunk/lang/cvtupload_fr.php
+++ cvt-upload/trunk/lang/cvtupload_fr.php
@@ -12,7 +12,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// S
 	'saisie_fichiers_explication' => 'Envoyer un ou plusieurs fichiers en s’assurant de ne pas avoir à les renvoyer en cas de non validation des autres champs du formulaire.',
-	'saisie_fichiers_label_fichiers_individuels_label' => 'Utiliser un label pour le champ de chaque fichier individuel ?', 
+	'saisie_fichiers_label_fichiers_individuels_label' => 'Utiliser un label pour le champ de chaque fichier individuel ?',
 	'saisie_fichiers_nb_fichiers_explication' => 'Ceci détermine le nombre de champs d’envoi de fichier disponibles.',
 	'saisie_fichiers_nb_fichiers_label' => 'Nombre maximum de fichiers à envoyer',
 	'saisie_fichiers_plusieur_label' => 'Fichier n° @nb@',
