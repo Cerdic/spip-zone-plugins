91e13aae242bcdd958ba559a68c02c39c6c549c9
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-01-23 04:45:56 +0000

    [Salvatore] [source:_plugins_/facteur/trunk/lang/ facteur] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@102078 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- facteur/trunk/lang/facteur_oc_ni_mis.php
+++ facteur/trunk/lang/facteur_oc_ni_mis.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// extrait automatiquement de http://trad.spip.net/tradlang_module/facteur?lang_cible=oc_ni_mis
+// extrait automatiquement de https://trad.spip.net/tradlang_module/facteur?lang_cible=oc_ni_mis
 // ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) {
