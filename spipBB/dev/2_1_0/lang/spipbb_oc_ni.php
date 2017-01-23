27ebc84c2eba1f3b21f5055f682dc5ff73c0323a
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-01-23 04:54:18 +0000

    [Salvatore] [source:_plugins_/spipBB/dev/2_1_0/lang/ spipbb] Export depuis http://trad.spip.net
    La langue 'ar' devrait Ãªtre supprimÃ©e car trop peu traduite (20.23 %)
    La langue 'bg' devrait Ãªtre supprimÃ©e car trop peu traduite (11.40 %)
    La langue 'cs' devrait Ãªtre supprimÃ©e car trop peu traduite (11.86 %)
    La langue 'eo' devrait Ãªtre supprimÃ©e car trop peu traduite (14.19 %)
    La langue 'it' devrait Ãªtre supprimÃ©e car trop peu traduite (25.12 %)
    La langue 'it_fem' devrait Ãªtre supprimÃ©e car trop peu traduite (11.86 %)
    La langue 'lb' devrait Ãªtre supprimÃ©e car trop peu traduite (11.86 %)
    La langue 'oc_lnc' devrait Ãªtre supprimÃ©e car trop peu traduite (11.86 %)
    La langue 'oc_ni' devrait Ãªtre supprimÃ©e car trop peu traduite (11.86 %)
    La langue 'pt_br' devrait Ãªtre supprimÃ©e car trop peu traduite (23.02 %)
    
    
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@102406 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spipBB/dev/2_1_0/lang/spipbb_oc_ni.php
+++ spipBB/dev/2_1_0/lang/spipbb_oc_ni.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// extrait automatiquement de http://trad.spip.net/tradlang_module/spipbb?lang_cible=oc_ni
+// extrait automatiquement de https://trad.spip.net/tradlang_module/spipbb?lang_cible=oc_ni
 // ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) {
