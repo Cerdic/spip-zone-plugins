08eb994446acf188397db39d72680b849c5c5eb3
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-01-23 04:52:47 +0000

    [Salvatore] [source:_plugins_/saisies/trunk/lang/ saisies] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@102277 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- saisies/trunk/lang/saisies_ca.php
+++ saisies/trunk/lang/saisies_ca.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// extrait automatiquement de http://trad.spip.net/tradlang_module/saisies?lang_cible=ca
+// extrait automatiquement de https://trad.spip.net/tradlang_module/saisies?lang_cible=ca
 // ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) {
