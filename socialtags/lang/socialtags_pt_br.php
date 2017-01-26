fe4b7f5f5c66f303e19664b88353f096757c7dd3
rporto@calepino.com <rporto@calepino.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-01-26 04:32:46 +0000

    [Salvatore] [source:_plugins_/socialtags/lang/ socialtags] Export depuis http://trad.spip.net de la langue pt_br
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@102461 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ socialtags/lang/socialtags_pt_br.php
@@ -0,0 +1,77 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/socialtags?lang_cible=pt_br
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cfg_activer_javascript' => 'Por favor, ative o Javascript.',
+	'cfg_badge_fb' => 'Badge/Widget Facebook',
+	'cfg_badge_fb_existe' => 'Se você possui um Badge Facebook (<a href="http://www.facebook.com/facebook-widgets/" class="spip_out">ajuda no site FB</a>), indique abaixo o seu conteúdo, tanto em JS, quanto em HTML.',
+	'cfg_badge_html' => 'Badge HTML',
+	'cfg_badge_html_ajouter_1' => '(incluir no seu template',
+	'cfg_badge_html_ajouter_2' => ', ou numa matéria',
+	'cfg_badge_js' => 'Badge JS',
+	'cfg_badge_js_1' => ' (incluído automaticamente em <code>@code@</code>)',
+	'cfg_en_bas' => 'No fim de cada página',
+	'cfg_exemples' => 'Exemplos (nos templates dist) :',
+	'cfg_explication_boite_info' => 'Você pode escolher aqui os sites de compartilhamento das suas matérias.',
+	'cfg_inserer' => 'Inserir o bloco social-tags abaixo do bloco a seguir:',
+	'cfg_insertion' => 'Inclusão nas páginas:',
+	'cfg_liste_site' => 'Lista de mídias sociais',
+	'cfg_plusone' => 'Google plus one',
+	'cfg_plusone_existe' => 'Para ativar o Google+1, instale o plugin (<a href="http://contrib.spip.net/Google-1" class="spip_out">de ajuda em spip-contrib</a>).',
+	'cfg_referer' => 'Exibir o badge e os botões apenas para os visitantes originários dos respectivos sites.',
+	'cfg_selector' => 'Seletor:',
+	'cfg_sous_bloc' => 'No bloco de título das páginas de matérias',
+	'cfg_sous_div' => 'Na div chamada id=socialtags',
+	'cfg_titre_boite_info' => 'Configuração do plugin Social Tags',
+	'cfg_titre_socialtags' => 'Social Tags',
+	'cfg_tout_cocher' => 'Marcar tudo',
+	'cfg_tout_decocher' => 'Desmarcar tudo',
+	'cfg_wopen' => 'Modo pop-up',
+	'cfg_wopen_exp' => 'Abrir o link para o serviço numa nova janela',
+
+	// D
+	'description_noisette_badge_facebook' => 'Exibir o badge Facebook definido na página de configuração',
+	'description_noisette_facebook_like' => 'Exibir o botão Facebook <b>Curtir</b> ou <b>Recomendar</b> e enventualmente <b>Enviar</b>',
+	'description_noisette_facebook_like_box' => 'Exibir o box Facebook <b>Curtir</b>. ele exibe o número de fans que curitam a página Facebook referenciada',
+
+	// N
+	'noisette_action_like' => 'Curtir',
+	'noisette_action_recommand' => 'Recomendar',
+	'noisette_colorscheme_dark' => 'Fundo escuro',
+	'noisette_colorscheme_light' => 'Fundo claro',
+	'noisette_label_action' => 'Ação',
+	'noisette_label_afficher_send' => 'Botão Enviar?',
+	'noisette_label_afficher_titre_noisette' => 'Exibir um título?',
+	'noisette_label_border_color' => 'Inserir o código de cor da borda do box (no formato #123ABC)',
+	'noisette_label_colorscheme' => 'Aparência',
+	'noisette_label_header' => 'Exibir o cabeçalho do box «Visite-nos no Facebook» ?',
+	'noisette_label_height' => 'Altura',
+	'noisette_label_href' => 'URL da página Facebook',
+	'noisette_label_layout' => 'Texto',
+	'noisette_label_niveau_titre' => 'Escolher o nível do título',
+	'noisette_label_show_faces' => 'Exibir as fotos de perfil no botão?',
+	'noisette_label_stream' => 'Exibir o fluxo de matérias da página do Facebook ?',
+	'noisette_label_titre_noisette' => 'Título:',
+	'noisette_label_titre_noisette_perso' => 'Se título personalizado:',
+	'noisette_label_width' => 'Largura',
+	'noisette_layout_box_count' => 'Total unicamente abaixo',
+	'noisette_layout_button_count' => 'Total unicamente à direita',
+	'noisette_layout_standard' => 'Texo completo à direita',
+	'noisette_titre_perso' => 'Título personalizado',
+	'nom_noisette_badge_facebook' => 'Badge Facebook',
+	'nom_noisette_facebook_like' => 'Botão Facebook-like',
+	'nom_noisette_facebook_like_box' => 'Box Facebook-like',
+
+	// T
+	'titre_menu' => 'Social Tags',
+	'titre_mon_badge_facebook' => 'Meu badge facebook',
+	'titre_sur_facebook' => 'No Facebook'
+);
