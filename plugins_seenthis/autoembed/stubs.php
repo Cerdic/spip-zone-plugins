cb35db4f5171457100858187fed19b192a1f8067
brunobergot@gmail.com <brunobergot@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-01-29 18:44:58 +0000

    réparer les embed de archive.org
    je ne suis pas pleinement satifsfait de la taille en fonction du fait qu'on récupère de l'audio ou de la video, mais vu la merde qui est envoyé dans les metas og ou twittruc c'est pas gagné pour faire mieux
    
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@102562 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- plugins_seenthis/autoembed/stubs.php
+++ plugins_seenthis/autoembed/stubs.php
@@ -164,10 +164,10 @@ $AutoEmbed_stubs = array(
   array(
     'title' => 'Archive.org',
     'website' => 'http://www.archive.org',
-    'url-match' => 'http://(?:www\.)?archive\.org/download/((?:[0-9a-z_-]*?)/(?:[0-9a-z_-]*?)\.flv)',
-    'embed-src' => 'http://www.archive.org/flow/FlowPlayerLight.swf?config=%7Bembedded%3Atrue%2CshowFullScreenButton%3Atrue%2CshowMuteVolumeButton%3Atrue%2CshowMenu%3Atrue%2CautoBuffering%3Afalse%2CautoPlay%3Afalse%2CinitialScale%3A%27fit%27%2CmenuItems%3A%5Bfalse%2Cfalse%2Cfalse%2Cfalse%2Ctrue%2Ctrue%2Cfalse%5D%2CusePlayOverlay%3Afalse%2CshowPlayListButtons%3Atrue%2CplayList%3A%5B%7Burl%3A%27$2%27%7D%5D%2CcontrolBarGloss%3A%27high%27%2CshowVolumeSlider%3Atrue%2CbaseURL%3A%27http%3A%2F%2Fwww%2Earchive%2Eorg%2Fdownload%2F%27%2Cloop%3Afalse%2CcontrolBarBackgroundColor%3A%270x000000%27%7D',
-    'embed-width' => '480',
-    'embed-height' => '360',
+    'url-match' => 'http://(?:www\.)?archive\.org/(?:details|embed)/([\d\w\-_][^/\?\#]+)',
+    'embed-width' => '400',
+    'embed-height' => '300',
+    'iframe-player' => 'https://archive.org/embed/$2&playlist=1',
   ),
   array(
     'title' => 'Atom',
