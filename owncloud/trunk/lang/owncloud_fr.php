d80690ea59aec03a69acbe304887d5d1958dd80c
cyp@rouquin.me <cyp@rouquin.me@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-01-31 17:44:07 +0000

    Pétouille de design
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@102582 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- owncloud/trunk/lang/owncloud_fr.php
+++ owncloud/trunk/lang/owncloud_fr.php
@@ -57,7 +57,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'message_confirmation_recuperation_erreur_owncloud' => 'La récupération s’est mal déroulée',
 	'message_confirmation_importer_tout_media' => 'L\'importation s’est bien déroulée',
 	'message_confirmation_importer_tout_media_erreur' => 'L\'importation s’est mal déroulée',
-	'message_importer_tout_media' => 'Importer tous les médias dans les documents SPIP',
+	'message_importer_tout_media' => 'Importer tous les médias dans la médiathèque',
 
 	// O
 	'owncloud' => 'Owncloud',
