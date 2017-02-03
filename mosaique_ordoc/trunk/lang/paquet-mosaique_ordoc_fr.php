417609421540a8a1407c2cd8d3c46390725ff5d2
erational@erational.org <erational@erational.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-02-03 10:00:46 +0000

    un plugin pour passer de mosaique à ordoc
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@102598 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ mosaique_ordoc/trunk/lang/paquet-mosaique_ordoc_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// M
+	'mosaique_ordoc_description' => 'Ce plugin de maintenance permet de passer du plugin mosaique vers ordoc sans perdre l\'ordre de vos documents.',
+	'mosaique_ordoc_nom' => 'Migration de Mosaïque vers Ordoc',
+	'mosaique_ordoc_slogan' => 'Migration des données du plugin mosaique vers ordoc',
+);
