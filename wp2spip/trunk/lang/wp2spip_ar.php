af6c5639e60e1f2bac6b7fa6a7b307b6cf676cdf
george@middleeastwatch.net <george@middleeastwatch.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-02-06 04:32:52 +0000

    [Salvatore] [source:_plugins_/wp2spip/trunk/lang/ wp2spip] Export depuis http://trad.spip.net de la langue ar
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@102671 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ wp2spip/trunk/lang/wp2spip_ar.php
@@ -0,0 +1,53 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/wp2spip?lang_cible=ar
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'alerte_generation_fichier' => 'سيتم وضع ملف الاسترداد في مجاد tmp/dump',
+
+	// B
+	'bouton_recuperer_documents' => 'جلب المستندات',
+	'bouton_reparer_catlist' => 'تصليح لوائح التصنيفات',
+	'bouton_reparer_liens_internes' => 'تصليح الروابط الداخلية',
+
+	// E
+	'erreur_aucun_tag' => 'لم يتم العثور على اي علامة.',
+	'erreur_fichier_non_cree' => 'لم يتم انشاء ملف الاسترداد.',
+	'erreur_non_webmestre' => 'يجب التمتع بحقوق المشرف على الموقع.',
+	'explication_post_import' => 'تتيح هذه الصفحة عرض ما لم يتم فهمه نتيجة جلب قاعدة ووردبرس.',
+
+	// I
+	'info_erreur_aucun_mot' => 'لم يتم العثور على اي مفتاح!',
+	'info_erreur_aucune_rubrique' => 'الرجاء انشاء تصنيف واحد على الاقل في ووردبرس',
+	'info_nombre_documents' => '@nb@ مستند.',
+	'info_nombre_documents_orphelins' => '@nb@ مستند يتيم لن يتم جلبه',
+	'info_nombre_evenements' => '@nb@ حدث.',
+	'info_nombre_pages' => '@nb@ صفحة فريدة (يستحسن تثبيت ملحق الصفحات الفريدة - pages uniques - اذا لم يكن مثبتاً، والا ستعتبر هذه الصفحات مقالات يجب ترتيبها في اقسام ثم نشرها).',
+	'info_nombre_posts' => '@nb@ مقال منشور.',
+
+	// L
+	'label_evenements' => 'الأحداث',
+	'label_generer_fichier' => 'انشاء الملف بتنسيق SPIP',
+
+	// M
+	'message_aller_maintenance' => 'يرجى الذهاب الى الصفحة <a href=\'@url@\' title=\'\'>صيانة</a> في الموفع لجلب قاعدة wp2spip.xml.',
+	'message_attention_import' => 'تنبيه، هذا الاسترجاع سيحل محل قاعدة SPIP الحالية.',
+	'message_conversion_ok' => 'تم تحويل قاعدة ووردبرس الى تنسيق xml SPIP ٢٫١٫١٢!',
+	'message_notice_site_vide' => 'لا يبدو ان موقع SPIP فارغ، سيتم فقدان محتواه اذا تم استرجاع القاعدة من الملف الذي انشئ في ووردبرس.',
+	'mot' => 'كلمة توضيحية واحدة',
+	'mots' => '@nb@ كلمة توضيحية',
+
+	// T
+	'tag' => 'علامة واحدة',
+	'tags' => '@nb@ علامة',
+	'titre_contenu_base' => 'محتوى قاعدة بيانات ووردبرس',
+	'titre_page_migration' => 'الانتقال من ووردبرس الىSPIP',
+	'titre_post_import' => 'عمليات ما بعد الجلب من ووردبرس'
+);
