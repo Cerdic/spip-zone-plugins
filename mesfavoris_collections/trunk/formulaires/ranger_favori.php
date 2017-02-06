1b37cfd211fc8970fa5c999f1f6ea5d1c7a9caae
tcharlss@bravecassine.com <tcharlss@bravecassine.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-02-06 17:37:14 +0000

    v1.0.3 : lorsqu'on range le favori d'un objet dans une collection, ne pas proposer les collections où l'objet se trouve déjà.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@102685 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- mesfavoris_collections/trunk/formulaires/ranger_favori.php
+++ mesfavoris_collections/trunk/formulaires/ranger_favori.php
@@ -11,14 +11,28 @@ function formulaires_ranger_favori_saisies_dist($objet, $id_objet, $redirect='')
 	$saisies = array();
 	$id_auteur = intval(session_get('id_auteur'));
 	
-	// On teste les collections de l'utilisateurice en cours
-	if ($nb = sql_countsel('spip_favoris_collections', 'id_auteur = '.$id_auteur) and $nb > 0) {
+	// On teste les collections de l'utilisateurice en cours,
+	// en excluant celles où l'objet est déjà rangé.
+	$collections = sql_allfetsel(
+		'col.id_favoris_collection',
+		'spip_favoris_collections AS col' .
+			' LEFT JOIN spip_favoris AS fav' .
+				' ON col.id_favoris_collection = fav.id_favoris_collection' .
+				' AND fav.objet = ' . sql_quote($objet) .
+				' AND fav.id_objet = ' . intval($id_objet),
+		array(
+			'col.id_auteur = ' . intval($id_auteur),
+			'fav.id_favoris_collection IS NULL'
+		)
+	);
+	$collections = array_map(function($v){return array_shift($v);},$collections);
+	if (count($collections) > 0) {
 		$saisies[] = array(
 			'saisie' => 'favoris_collection',
 			'options' => array(
 				'nom' => 'id_favoris_collection',
 				'label' => _T('favoris_collection:ranger_id_favoris_collection_label'),
-				'id_auteur' => $id_auteur, // uniquement les collections de cet utilisateur
+				'id_favoris_collection' => $collections, // uniquement les collections de cet utilisateur et pas déjà utilisées
 			),
 		);
 	}
