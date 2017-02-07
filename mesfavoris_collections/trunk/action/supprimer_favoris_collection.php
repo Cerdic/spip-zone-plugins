f8a97b57cd975c421bfa9e78270f57e0c7f1c430
tcharlss@bravecassine.com <tcharlss@bravecassine.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-02-07 17:03:29 +0000

    version 1.0.4 : une option pour conserver les favoris qui se retrouveront orphelins quand on supprime une collection. Exemple : #URL_ACTION_AUTEUR{supprimer_favoris_collection, #ID_FAVORIS_COLLECTION-orphelins, #SELF}
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@102728 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- mesfavoris_collections/trunk/action/supprimer_favoris_collection.php
+++ mesfavoris_collections/trunk/action/supprimer_favoris_collection.php
@@ -3,11 +3,28 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
 
-function action_supprimer_favoris_collection_dist($id_favoris_collection=null) {
-	if (is_null($id_favoris_collection)) {
+/**
+ * Supprimer une collection de favoris
+ *
+ * Optionnellement, on peut demander de conserver les favoris orphelins
+ * (qui ne se trouvent plus dans aucune collection après cette action).
+ *
+ * @example
+ * #URL_ACTION_AUTEUR{supprimer_favoris_collection, #ID_FAVORIS_COLLECTION, #SELF}
+ * Idem, mais en conservant les orphelins :
+ * #URL_ACTION_AUTEUR{supprimer_favoris_collection, #ID_FAVORIS_COLLECTION-orphelins, #SELF}
+ *
+ * @param string|null $arg
+ *     paramètres séparés par des tirets : param1-param2-paramN
+ *     id_favoris_collection : identifiant de la collection à supprimer
+ *     orphelins             : pour indiquer de conserver les favoris orphelins
+ */
+function action_supprimer_favoris_collection_dist($arg=null) {
+	if (is_null($arg)) {
 		$securiser_action = charger_fonction('securiser_action', 'inc');
-		$id_favoris_collection = $securiser_action();
+		$arg = $securiser_action();
 	}
+	list($id_favoris_collection, $garder_orphelins) = explode('-', $arg);
 	
 	include_spip('inc/mesfavoris');
 	include_spip('inc/autoriser');
@@ -18,6 +35,34 @@ function action_supprimer_favoris_collection_dist($id_favoris_collection=null) {
 		and autoriser('supprimer', 'favoris_collection', $id_favoris_collection)
 	) {
 		// D'abord on supprime tout l'intérieur
+		// Si besoin, avant de procéder, on sort de la collection les favoris
+		// qui se retrouveront orphelins, ainsi ils ne seront pas effacés.
+		// [FIXME] on devrait pouvoir trouver les (futurs) orphelins en une seule requête.
+		if ($garder_orphelins) {
+			if ($favoris = sql_allfetsel('*', 'spip_favoris', array('id_favoris_collection' => $id_favoris_collection))){
+				foreach ($favoris as $favori){
+					if (!sql_countsel(
+						'spip_favoris',
+						array(
+							'objet = ' . sql_quote($favori['objet']),
+							'id_objet = ' . intval($favori['id_objet']),
+							'id_favoris_collection != ' . intval($id_favoris_collection),
+							//'id_auteur = ' . sql_quote($favori['id_auteur']), // nécessaire ou pas ?
+						)
+					)){
+						sql_updateq(
+							'spip_favoris',
+							array(
+								'id_favoris_collection' => 0,
+							),
+							array(
+								'id_favori = ' . intval($favori['id_favori']),
+							)
+						);
+					}
+				}
+			}
+		}
 		mesfavoris_supprimer(array('id_favoris_collection' => $id_favoris_collection));
 		
 		// Puis on supprime la collection elle-même
