3ad591027a6f3e19866b8ce31cbc76dc96e9d3d4
nicolas.dorigny@gmail.com <nicolas.dorigny@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-02-09 00:38:23 +0000

    Surcharge de la balise `#RANG` qui renvoie une chaine vide si le rang est null ou zéro.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@102774 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- rang/trunk/rang_fonctions.php
+++ rang/trunk/rang_fonctions.php
@@ -12,3 +12,17 @@
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 include_spip('inc/rang_api');
+
+/**
+ * Surcharge de la balise `#RANG`
+ * 
+ * Appelle balise_RANG_dist(), mais renvoie une chaine vide si le rang est null ou zéro
+ * 
+ */
+function balise_RANG($p) {
+	$p = balise_RANG_dist($p);
+	
+	$p->code = "(intval($p->code) == 0 ? '' : $p->code)";
+	
+	return $p;
+}
