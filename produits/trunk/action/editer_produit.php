a58d8c1da74d39e03a433a58fb636b1dd0a5d48d
p@henix.be <p@henix.be@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-02-10 07:15:29 +0000

    PSR pour ce fichier
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@102795 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- produits/trunk/action/editer_produit.php
+++ produits/trunk/action/editer_produit.php
@@ -1,17 +1,18 @@
 <?php
 
-// Sécurité
-if (!defined('_ECRIRE_INC_VERSION')) return;
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
 /**
  * Action de création / Modification d'un produit
  * @param string|null $arg
  * @return array
  */
-function action_editer_produit_dist($arg=null) {
+function action_editer_produit_dist($arg = null) {
 	include_spip('inc/autoriser');
-	$err="";
-	if (is_null($arg)){
+	$err = '';
+	if (is_null($arg)) {
 		$securiser_action = charger_fonction('securiser_action', 'inc');
 		$arg = $securiser_action();
 	}
@@ -22,10 +23,13 @@ function action_editer_produit_dist($arg=null) {
 	}
 
 	// Enregistre l'envoi dans la BD
-	if ($id_produit > 0) $err = produit_modifier($id_produit);
+	if ($id_produit > 0) {
+		$err = produit_modifier($id_produit);
+	}
 
-	if ($err)
-		spip_log("echec editeur produit: $err",_LOG_ERREUR);
+	if ($err) {
+		spip_log('echec editeur produit: '.$err, _LOG_ERREUR);
+	}
 
 	return array($id_produit,$err);
 }
@@ -38,37 +42,41 @@ function action_editer_produit_dist($arg=null) {
  *   Un tableau avec les champs par défaut lors de l'insertion
  * @return int
  */
-function produit_inserer($id_rubrique,$set=null) {
+function produit_inserer($id_rubrique, $set = null) {
 	$id_produit = false;
-	
+
 	// On insère seulement s'il y a une rubrique correcte
-	if ($id_rubrique = intval($id_rubrique)){
+	if ($id_rubrique = intval($id_rubrique)) {
 		$champs = array();
 		// Si id_rubrique vaut 0 ou n'est pas definie, creer le produit dans la premiere rubrique racine
 		if (!$id_rubrique = intval($id_rubrique)) {
-			$row = sql_fetsel('id_rubrique, id_secteur, lang', 'spip_rubriques', 'id_parent=0','', '0+titre,titre', "1");
+			$row = sql_fetsel('id_rubrique, id_secteur, lang', 'spip_rubriques', 'id_parent=0','', '0+titre,titre', '1');
 			$id_rubrique = $row['id_rubrique'];
-		} else $row = sql_fetsel('lang, id_secteur', 'spip_rubriques', "id_rubrique=".intval($id_rubrique));
+		} else {
+			$row = sql_fetsel('lang, id_secteur', 'spip_rubriques', 'id_rubrique='.intval($id_rubrique));
+		}
 
 		$lang_rub = $row['lang'];
 		$champs['id_rubrique'] = $id_rubrique;
 		// On propage le secteur
 		$champs['id_secteur'] = $row['id_secteur'];
-	
+
 		// Dans un premier temps La langue a la creation : c'est la langue de la rubrique ou du site
 		$champs['lang'] = $lang_rub ? $lang_rub : $GLOBALS['meta']['langue_site'];
-		
+
 		// La date de tout de suite
 		$champs['date'] = date('Y-m-d H:i:s');
-		
+
 		// Le statut en cours de redac
 		$champs['statut'] = 'prop';
-		
-		if ($set)
+
+		if ($set) {
 			$champs = array_merge($champs, $set);
+		}
 
 		// Envoyer aux plugins avant insertion
-		$champs = pipeline('pre_insertion',
+		$champs = pipeline(
+			'pre_insertion',
 			array(
 				'args' => array(
 					'table' => 'spip_produits',
@@ -79,7 +87,8 @@ function produit_inserer($id_rubrique,$set=null) {
 		// Insérer l'objet
 		$id_produit = sql_insertq('spip_produits', $champs);
 		// Envoyer aux plugins après insertion
-		pipeline('post_insertion',
+		pipeline(
+			'post_insertion',
 			array(
 				'args' => array(
 					'table' => 'spip_produits',
@@ -99,14 +108,14 @@ function produit_inserer($id_rubrique,$set=null) {
  * @param array $set
  * @return string
  */
-function produit_modifier($id_produit, $set=null) {
+function produit_modifier($id_produit, $set = null) {
 	$err = '';
 
 	include_spip('inc/modifier');
 	include_spip('inc/filtres');
 	$c = collecter_requests(
 		// white list
-		objet_info('produit','champs_editables'),
+		objet_info('produit', 'champs_editables'),
 		// black list
 		array('date','statut','id_parent'),
 		// donnees eventuellement fournies
@@ -114,25 +123,31 @@ function produit_modifier($id_produit, $set=null) {
 	);
 
 	// Si le produit est publie, invalider les caches et demander sa reindexation
-	$t = sql_getfetsel("statut", "spip_produits", "id_produit=".intval($id_produit));
+	$t = sql_getfetsel('statut', 'spip_produits', 'id_produit='.intval($id_produit));
 	$invalideur = $indexation = false;
 	if ($t == 'publie') {
 		$invalideur = "id='produit/$id_produit'";
 		$indexation = true;
 	}
 
-	if ($err = objet_modifier_champs('produit', $id_produit,
+
+	$err = objet_modifier_champs(
+		'produit',
+		$id_produit,
 		array(
 			'data' => $set,
 			'nonvide' => array('titre' => _T('info_sans_titre')),
 			'invalideur' => $invalideur,
 			'indexation' => $indexation,
 		),
-		$c))
+		$c
+	);
+	if ($err) {
 		return $err;
+	}
 
 	// Modification de statut, changement de rubrique ?
-	$c = collecter_requests(array('date', 'statut', 'id_parent'),array(),$set);
+	$c = collecter_requests(array('date', 'statut', 'id_parent'), array(), $set);
 	$err = produit_instituer($id_produit, $c);
 
 	return $err;
@@ -147,28 +162,29 @@ function produit_modifier($id_produit, $set=null) {
  * @param bool $calcul_rub
  * @return string
  */
-function produit_instituer($id_produit, $c, $calcul_rub=true){
+function produit_instituer($id_produit, $c, $calcul_rub = true){
 	include_spip('inc/autoriser');
 	include_spip('inc/rubriques');
 	include_spip('inc/modifier');
-	
-	$row = sql_fetsel("statut, date, id_rubrique", "spip_produits", "id_produit=$id_produit");
+
+	$row = sql_fetsel('statut, date, id_rubrique', 'spip_produits', 'id_produit='.intval($id_produit));
 	$id_rubrique = $row['id_rubrique'];
 	$statut_ancien = $statut = $row['statut'];
 	$date_ancienne = $date = $row['date'];
 	$champs = array();
-	
+
 	$d = isset($c['date']) ? $c['date'] : null;
 	$s = isset($c['statut']) ? $c['statut'] : $statut;
-	
+
 	// On ne modifie le statut que si c'est autorisé
-	if ($s != $statut or ($d AND $d != $date)) {
-		if (autoriser('publierdans', 'rubrique', $id_rubrique))
+	if ($s != $statut or ($d and $d != $date)) {
+		if (autoriser('publierdans', 'rubrique', $id_rubrique)) {
 			$statut = $champs['statut'] = $s;
-		else if (autoriser('modifier', 'produit', $id_produit) and $s != 'publie')
+		} elseif (autoriser('modifier', 'produit', $id_produit) and $s != 'publie') {
 			$statut = $champs['statut'] = $s;
-		else
+		} else {
 			spip_log("editer_produit $id_produit refus " . join(' ', $c));
+		}
 
 		// En cas de publication, fixer la date a "maintenant"
 		// sauf si $c commande autre chose
@@ -176,20 +192,21 @@ function produit_instituer($id_produit, $c, $calcul_rub=true){
 		// En cas de proposition d'un produit (mais pas depublication), idem
 		if ($champs['statut'] == 'publie'
 			or ($champs['statut'] == 'prop' and ($d or !in_array($statut_ancien, array('publie', 'prop'))))
-		){
-			if ($d or strtotime($d=$date)>time())
+		) {
+			if ($d or strtotime($d = $date) > time()) {
 				$champs['date'] = $date = $d;
-			else
+			} else {
 				$champs['date'] = $date = date('Y-m-d H:i:s');
+			}
 		}
 	}
-	
+
 	// Verifier que la rubrique demandee existe et est differente
 	// de la rubrique actuelle
 	if ($id_parent = $c['id_parent']
 		and $id_parent != $id_rubrique
-		and (sql_fetsel('1', 'spip_rubriques', "id_rubrique=$id_parent"))
-	){
+		and (sql_fetsel('1', 'spip_rubriques', 'id_rubrique='.intval($id_parent)))
+	) {
 		$champs['id_rubrique'] = $id_parent;
 
 		// Si le produit était publié
@@ -197,10 +214,11 @@ function produit_instituer($id_produit, $c, $calcul_rub=true){
 		// repasser le produit en statut 'proposé'.
 		if ($statut == 'publie'
 			and !autoriser('publierdans', 'rubrique', $id_rubrique)
-		)
+		) {
 			$champs['statut'] = 'prop';
+		}
 	}
-	
+
 	// Envoyer aux plugins
 	$champs = pipeline(
 		'pre_edition',
@@ -215,23 +233,25 @@ function produit_instituer($id_produit, $c, $calcul_rub=true){
 		)
 	);
 
-	if (!count($champs)) return;
-	
+	if (!count($champs)) {
+		return;
+	}
+
 	// Envoyer les modifications et calculer les héritages
 	editer_produit_heritage($id_produit, $id_rubrique, $statut_ancien, $champs, $calcul_rub);
-	
+
 	// Invalider les caches
 	include_spip('inc/invalideur');
 	suivre_invalideur("id='id_produit/$id_produit'");
-	
+
 	if ($date) {
 		$t = strtotime($date);
 		$p = @$GLOBALS['meta']['date_prochain_postdate'];
-		if ($t > time() AND (!$p OR ($t < $p))) {
+		if ($t > time() and (!$p or ($t < $p))) {
 			ecrire_meta('date_prochain_postdate', $t);
 		}
 	}
-	
+
 	// Pipeline
 	pipeline(
 		'post_edition',
@@ -245,19 +265,19 @@ function produit_instituer($id_produit, $c, $calcul_rub=true){
 			'data' => $champs
 		)
 	);
-	
+
 	// Notifications
 	if ($notifications = charger_fonction('notifications', 'inc', true)) {
 		$notifications('produit_instituer', $id_produit,
 			array('statut' => $statut, 'statut_ancien' => $statut_ancien, 'date'=>$date)
 		);
 	}
-	
+
 	return '';
 }
 
 // Modifie le produit en calculant les héritages
-function editer_produit_heritage($id_produit, $id_rubrique, $statut, $champs, $cond=true) {
+function editer_produit_heritage($id_produit, $id_rubrique, $statut, $champs, $cond = true) {
 	// Si on deplace le produit
 	// changer aussi son secteur et sa langue (si héritée)
 	if (isset($champs['id_rubrique'])) {
@@ -272,17 +292,17 @@ function editer_produit_heritage($id_produit, $id_rubrique, $statut, $champs, $c
 #		}
 	}
 
-	if (!$champs) return;
+	if (!$champs) {
+		return;
+	}
 
-	sql_updateq('spip_produits', $champs, "id_produit=$id_produit");
+	sql_updateq('spip_produits', $champs, 'id_produit='.intval($id_produit));
 
 	// Changer le statut des rubriques concernees
 
 	if ($cond) {
 		include_spip('inc/rubriques');
-		$postdate = ($GLOBALS['meta']["post_dates"] == "non" and isset($champs['date']) and (strtotime($champs['date']) < time())) ? $champs['date'] : false;
+		$postdate = ($GLOBALS['meta']['post_dates'] == 'non' and isset($champs['date']) and (strtotime($champs['date']) < time())) ? $champs['date'] : false;
 		calculer_rubriques_if($id_rubrique, $champs, $statut, $postdate);
 	}
 }
-
-?>
