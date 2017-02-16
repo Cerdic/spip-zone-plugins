9514d8c0e8c226cc6fe6343abad3054bbed9dafa
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-02-16 04:33:15 +0000

    [Salvatore] [source:_plugins_/autorite/trunk/lang/ autorite] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@102882 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ autorite/trunk/lang/autorite_de.php
@@ -0,0 +1,84 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/autorite?lang_cible=de
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'activer_mots_cles' => 'Verwaltung per Schlagwort aktivieren',
+	'admin_complets' => 'Haupt-Administratoren',
+	'admin_restreints' => 'Rubrik/Unteradministratoren?',
+	'admin_tous' => 'Alle Administratoren (auch Unter-Administratoren)',
+	'administrateur' => 'Administrator',
+	'admins' => 'Administratoren',
+	'admins_redacs' => 'Administratoren und Redakteure',
+	'admins_rubriques' => 'Rubrikadministratoren haben:',
+	'attention_crayons' => '<small><strong>Achtung!</strong> Die folgenden Einstellungen funktionieren nur, wenn Sie ein Interface zur textbearbeitung wie <a href="http://contrib.spip.net/Les-Crayons">den Stift</a>) nutzen.</small>',
+	'attention_version' => 'Achtung, die folgenden Einstellungen funktionieren nicht mit Ihrer SPIP-Version:',
+	'auteur_message_advitam' => 'Der Autor eines Beitrags, für immer',
+	'auteur_message_heure' => 'Der Autor eines Beitrags, eine Stunde lang',
+	'auteur_modifie_article' => '<strong>Autor ändert Artikel</strong>: jeder Redakteur kann seine veröffentlichten Artikel ändern.
+	<br />
+	<i>N.B.: Diese Einstellung betrifft auch angemeldete Leser, wenn ein entsprechendes Interface installiert ist.</i>',
+	'auteur_modifie_email' => '<strong>Redakteur ändert Email</strong>: Jeder Redakteur kann seine Mailadresse auf seiner persönlichen Seite ändern.',
+	'auteur_modifie_forum' => '<strong>Autor moderiert Forum</strong>: Jeder Autor kann die Foren seiner Artikel moderieren.',
+	'auteur_modifie_petition' => '<strong>Autor moderiert Petition</strong>: Jeder Autor kann die Petitionen zu seinen Artikeln moderieren.',
+
+	// C
+	'config_auteurs' => 'Konfiguration der Autoren',
+	'config_auteurs_rubriques' => 'Welche Arten Autor können <b>Rubriken zugeordnet werden</b>?',
+	'config_auteurs_statut' => 'Welchen Status erhält ein neuer Autor <b>als Grundeinstellung</b> ?',
+	'config_plugin_qui' => 'Wer darf <strong>Plugin-Einstellungen ändern</strong> (Aktivierung ...) ?',
+	'config_site' => 'Website-Konfiguration',
+	'config_site_qui' => 'Chi può <strong>modificare la configurazione</strong> del sito?',
+	'crayons' => 'Bleistift',
+
+	// D
+	'deja_defini' => 'Die folgenden Rechte sind schon woanders definiert worden:',
+	'descriptif_1' => 'Questa schermata di configurazione è riservata ai webmaster del sito :',
+	'droits_des_auteurs' => 'Rechte der Autoren',
+	'droits_des_redacteurs' => 'Rechte der Redakteure',
+	'droits_idem_admins' => 'gli stessi diritti degli amministratori',
+
+	// E
+	'espace_wiki' => 'Spazio wiki',
+
+	// F
+	'forums_qui' => '<strong>Forum :</strong> chi può modificare il contenuto dei forum :',
+
+	// I
+	'info_gere_rubriques' => 'Verwaltet die folgenden Rubriken:',
+	'info_gere_rubriques_2' => 'Ich verwalte die folgenden Rubriken:',
+	'infos_selection' => '(più sezioni possono essere selezionate tenendo premuto il tasto shift)',
+
+	// P
+	'personne' => 'Person',
+	'publication' => 'Veröffentlichung',
+
+	// R
+	'redac_tous' => 'Alle Redakteure',
+	'redacs' => 'für Redakteure der Seite',
+	'redacteur' => 'Redakteur',
+	'redacteur_lire_stats' => '<strong>Tatistikanzeige für Redakteure</strong>: Redakteure können die Statistiken ansehen.',
+	'refus_1' => '<p>Nur die Webmaster der Seite',
+
+	// T
+	'tous' => 'Alle',
+	'tout_deselectionner' => 'alles abwählen',
+
+	// V
+	'valeur_defaut' => '(Standardwert)',
+	'visiteur' => 'Besucher',
+	'visiteurs_anonymes' => 'Anonyme Besucher dürfen neue Seiten anlegen.',
+	'visiteurs_enregistres' => 'für angemeldete Besucher',
+	'visiteurs_tous' => 'allen Besuchern der Seite.',
+
+	// W
+	'webmestre' => 'Der Webmaster',
+	'webmestres' => 'Webmaster'
+);
