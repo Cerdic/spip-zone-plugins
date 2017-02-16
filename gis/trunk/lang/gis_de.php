4146247c71c1f0b877379e9084720b66f9af8fb5
willmann@netzsachen.de <willmann@netzsachen.de@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-02-16 04:33:18 +0000

    [Salvatore] [source:_plugins_/gis/trunk/lang/ gis] Export depuis http://trad.spip.net de la langue de
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@102883 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- gis/trunk/lang/gis_de.php
+++ gis/trunk/lang/gis_de.php
@@ -14,17 +14,20 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'aucun_objet' => 'Kein Objekt',
 
 	// B
+	'bouton_annuler_title' => 'Bearbeitung abbrechen, Änderungen werden nicht übernommen.',
+	'bouton_enregistrer_title' => 'Änderungen speichern.',
 	'bouton_lier' => 'Diesen Punkt verknüpfen',
 	'bouton_supprimer_gis' => 'Diesen Punkt endgültig löschen',
 	'bouton_supprimer_lien' => 'Diese Verknüpfung löschen',
 
 	// C
 	'cfg_descr_gis' => 'Geoinformationssystem<br /><a href="http://contrib.spip.net/4189" class="spip_out">zur Dokumentation</a>.',
-	'cfg_inf_adresse' => 'Adressfelder anzeigen (Land, Satdt, Region, Adresse ...)',
+	'cfg_inf_adresse' => 'Adressfelder anzeigen (Land, Stadt, Region, Adresse ...)',
 	'cfg_inf_bing' => 'Der Layer Bing Aerial benötigt einen Schlüssel zu erzeugen auf der  <a href=\'@url@\' class="spip_out">Bing Website</a>.',
 	'cfg_inf_geocoder' => 'Geocoding aktivieren (Suche nach einer Adresse, Anzeige von Adressen zu den Koordinaten).',
 	'cfg_inf_geolocaliser_user_html5' => 'Wenn es der Browser des Nutzers erlaubt, wird sein ungefährer Standort als Voreinstellung für die Position von Punkten genommen.',
-	'cfg_inf_google' => 'Diese API benötigt einen Schlüssel. Zu erzeugen auf der <a href=\'@url@\' class="spip_out">le GoogleMaps Seite </a>.', # MODIF
+	'cfg_inf_google' => 'Diese API benötigt einen Schlüssel. Zu erzeugen auf der <a href=\'@url@\' class="spip_out">der GoogleMaps Seite </a>.',
+	'cfg_inf_styles' => 'Zusätzliche Felder für den Stil anzeigen (Farbe, Transparenz, Dicke...)',
 	'cfg_lbl_activer_objets' => 'Geotargeting für folgende Inhalte aktivieren:',
 	'cfg_lbl_adresse' => 'Adressfelder anzeigen',
 	'cfg_lbl_api' => 'Karten-API ',
@@ -36,19 +39,25 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'cfg_lbl_layer_defaut' => 'Standardlayer',
 	'cfg_lbl_layers' => 'Vorgeschlagene Layer',
 	'cfg_lbl_maptype' => 'Kartentyp',
+	'cfg_lbl_styles' => 'Felder für Stil anzeigen',
 	'cfg_titre_gis' => 'GIS Einstellungen',
 
 	// E
 	'editer_gis_editer' => 'Punkt bearbeiten',
 	'editer_gis_nouveau' => 'Neuen Punkt anlegen',
 	'editer_gis_titre' => 'GIS Punkte',
-	'erreur_geocoder' => 'Ihre Suche ergab kein Ergebnis:', # MODIF
+	'erreur_geocoder' => 'Ihre Suche ergab kein Ergebnis:',
 	'erreur_recherche_pas_resultats' => 'Kein Punkt passt zu Ihrer Suche.',
 	'erreur_xmlrpc_lat_lon' => 'Länge und Breite müssen als Argumente übergeben werden.',
 	'explication_api_forcee' => 'Die API wird durch ein Plugin oder Skelett erzwungen.',
+	'explication_color' => 'Linienfarbe im CSS-Format (Standardwert: #0033FF)',
+	'explication_fillcolor' => 'Füllfarbe im CSS-Format (Standardwert: vererbt von der Linienfarbe)',
+	'explication_fillopacity' => 'Transparenz der Füllung von 0 bis 1 (Standardwert: 0.2)',
 	'explication_import' => 'Eine Datei im GPX oder KML - Format importieren.',
 	'explication_layer_forcee' => 'Der Layer wird durch ein anderes Plugin oder Skelett erzwungen.',
 	'explication_maptype_force' => 'Der Kartentyp wird durch ein anderes Plugin oder Skelett erzwungen.',
+	'explication_opacity' => 'Transparenz der Linien von 0 bis 1 (Standardwert: 0.5)',
+	'explication_weight' => 'Linienstärke (Standardwert: 5)',
 
 	// F
 	'formulaire_creer_gis' => 'GIS-Punkt anlegen:',
@@ -80,7 +89,10 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'label_adress' => 'Adresse',
 	'label_code_pays' => 'Länderkürzel',
 	'label_code_postal' => 'Postleitzahl',
+	'label_color' => 'Farbe',
 	'label_departement' => 'Bundesland',
+	'label_fillcolor' => 'Füllfarbe',
+	'label_fillopacity' => 'Transparenz der Füllung',
 	'label_import' => 'Importieren',
 	'label_inserer_modele_articles' => 'verknüpft mit Artikeln',
 	'label_inserer_modele_articles_sites' => 'verknüpft mit Artikeln und Websites',
@@ -113,15 +125,27 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'label_inserer_modele_rubriques' => 'verknüpft mit Rubriken',
 	'label_inserer_modele_sites' => 'verknüpft mit Websites',
 	'label_inserer_modele_titre_carte' => 'Titel der Karte',
+	'label_opacity' => 'Transparenz',
 	'label_pays' => 'Land',
 	'label_rechercher_address' => 'Adresse suchen',
 	'label_rechercher_point' => 'Punkt suchen',
 	'label_region' => 'Region',
 	'label_ville' => 'Stadt',
+	'label_weight' => 'Stärke',
 	'lat' => 'Breite',
 	'libelle_logo_gis' => 'LOGO DES PUNKTS',
 	'lien_ajouter_gis' => 'Deisen Punkt hinzufügen',
 	'lon' => 'Länge',
+
+	// M
+	'message_limite_atteinte' => 'Es gibt mehr GIS-Punkte als das aktuelle Anzeigelimit.<br /> Wenn Sie alle anzeigen wollen <a href="@url@">hier klicken</a>.',
+
+	// O
+	'onglet_carte' => 'Karte',
+	'onglet_liste' => 'Liste',
+
+	// P
+	'placeholder_geocoder' => 'Eine Adresse, eine Stadt, ein Land, eine Sehenswürdigkeit...',
 
 	// T
 	'telecharger_gis' => 'Im Format @format@ herunterladen',
@@ -133,14 +157,24 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'titre_bloc_creer_point' => 'Neuen Punkt verknüpfen',
 	'titre_bloc_points_lies' => 'Verknüpfte Punkte',
 	'titre_bloc_rechercher_point' => 'Punkt suchen',
+	'titre_limite_atteinte' => 'Anzeigelimit erreicht (@limite@)',
 	'titre_nombre_utilisation' => 'Eine Verwendung',
 	'titre_nombre_utilisations' => '@nb@ Verwendungen',
 	'titre_nouveau_point' => 'Neuer Punkt',
 	'titre_objet' => 'Ttiel',
 	'toolbar_actions_title' => 'Zeichnen abbrechen',
+	'toolbar_buttons_circle' => 'Einen Kreis zeichnen',
 	'toolbar_buttons_marker' => 'Einen Punkt zeichnen',
 	'toolbar_buttons_polygon' => 'Ein Polygon zeichnen',
 	'toolbar_buttons_polyline' => 'Eine Linie zeichnen',
+	'toolbar_buttons_rectangle' => 'Ein Rechteck zeichnen',
+	'toolbar_edit_buttons_edit' => 'Objekt bearbeiten',
+	'toolbar_edit_buttons_editdisabled' => 'Kein bearbeitbares Objekt',
+	'toolbar_edit_buttons_remove' => 'Objekt löschen',
+	'toolbar_edit_buttons_removedisabled' => 'Kein löschbares Objekt',
+	'toolbar_edit_handlers_edit_tooltip_subtext' => 'Abbrechen klicken um die Änderungen zu verwerfen.',
+	'toolbar_edit_handlers_edit_tooltip_text' => 'Die Marker verschieben, um das Objekt zu ändern.',
+	'toolbar_edit_handlers_remove_tooltip_text' => 'Zum Löschen auf das Objekt klicken',
 	'toolbar_handlers_marker_tooltip_start' => 'Klicken um Marker zu setzen',
 	'toolbar_handlers_polygon_tooltip_cont' => 'Klicken um das Polygonzeichnen   fortzusetzen',
 	'toolbar_handlers_polygon_tooltip_end' => 'Auf den ersten Punkt klicken um das Polygon abzuschliessen',
@@ -148,6 +182,8 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'toolbar_handlers_polyline_tooltip_cont' => 'Klicken um die Linie fortzusetzen',
 	'toolbar_handlers_polyline_tooltip_end' => 'Letzten Punkt klicken um Linie abzuschließen',
 	'toolbar_handlers_polyline_tooltip_start' => 'Klicken um Linie zu beginnen',
+	'toolbar_handlers_rectangle_tooltip_start' => 'Klicken und ziehen um ein Rechteck zu zeichnen',
+	'toolbar_handlers_simpleshape_tooltip_end' => 'Maustaste loslassen um das Design abzuschliessen',
 	'toolbar_undo_text' => 'Letzten Punkt löschen',
 	'toolbar_undo_title' => 'Letzten Punkt löschen',
 
