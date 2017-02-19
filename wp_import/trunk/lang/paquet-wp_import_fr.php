ec42d532e3b88c6c044aa6e0b9aa2d5300577578
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-02-19 04:32:52 +0000

    [Salvatore] [source:_plugins_/wp_import/trunk/lang/ paquet-wp_import] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@102938 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- wp_import/trunk/lang/paquet-wp_import_fr.php
+++ wp_import/trunk/lang/paquet-wp_import_fr.php
@@ -1,10 +1,14 @@
 <?php
-if (!defined('_ECRIRE_INC_VERSION')) return;
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/wp_import/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-	'wp_import_nom' => "Importer Wordpress",
-	'wp_import_slogan' => "Grignotez Wordpress et savourez SPIP...",
-	'wp_import_description' => "Importer la sauvegarde XML d'un site Wordpress dans votre site SPIP."
-	
+	// W
+	'wp_import_description' => 'Importer la sauvegarde XML d’un site Wordpress dans votre site SPIP.',
+	'wp_import_nom' => 'Importer Wordpress',
+	'wp_import_slogan' => 'Grignotez Wordpress et savourez SPIP...'
 );
