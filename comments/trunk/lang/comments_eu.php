73fbba2a76d70256f45403212369a096e6f79661
otalazt@gmail.com <otalazt@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-02-19 04:32:42 +0000

    [Salvatore] [source:_plugins_/comments/trunk/lang/ comments] Export depuis http://trad.spip.net de la langue eu
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@102936 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ comments/trunk/lang/comments_eu.php
@@ -0,0 +1,66 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/comments?lang_cible=eu
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cfg_forum_champs_obligatoires' => 'Beharrezko eremuak',
+	'cfg_forum_format_fil' => 'Zerrenda ala haria?',
+	'cfg_forum_longueur' => 'Mezuen luzetasuna',
+	'cfg_forum_longueur_maxi' => 'Gehieneko luzetasuna',
+	'cfg_forum_longueur_mini' => 'Gutxieneko luzetasun a',
+	'cfg_forum_permalien' => 'Lotura iraunkorra',
+	'cfg_label_apparence_permalien' => 'Iruzkinari lotura iraunkorraren itxura',
+	'cfg_label_apparence_permalien_aucun' => 'Bat ere ez',
+	'cfg_label_apparence_permalien_compteur' => 'Kontagailua (1.)',
+	'cfg_label_apparence_permalien_diese' => 'Diesea (#)',
+	'cfg_label_apparence_permalien_picto' => 'Piktoa',
+	'cfg_label_email_obligatoire' => 'Helbide elektroniko bat ezartzea behartu',
+	'cfg_label_nom_obligatoire' => 'Izen edo goitizen bat ezartzea behartu',
+	'cfg_label_presentation_fil' => 'Eztabaida hariaren aurekzpena',
+	'cfg_label_presentation_fil_liste' => 'Iruzkinen zerrenda (zabalean)',
+	'cfg_label_presentation_fil_thread' => 'Iruzkinen lerrokatzea (harika)',
+	'cfg_label_presentation_fil_thread1' => 'Iruzkinen lerrokatzea (maila bakarrean)',
+	'comment' => 'Iruzkin',
+	'comments' => 'Iruzkin',
+	'comments_h' => 'Zure iruzkinak',
+
+	// D
+	'date_heure_a' => 'ordua:',
+	'date_jour_le' => 'Data:',
+
+	// F
+	'forum_qui_etes_vous' => 'Nor zara?',
+
+	// L
+	'label_email' => 'Helbide elektronikoa (ez da agertuko)',
+	'label_nom' => 'Izena',
+	'label_notification' => 'Eztabaida honetako iruzkin guztiak gutun elektronikoz igor iezazkidazue',
+	'label_url' => 'Zure webgunea',
+	'lien_suivre_commentaires' => 'Iuzkinak jarraitu:',
+
+	// M
+	'moderation_info' => 'Kontuz, zure iruzkina berrirakurria eta adostua izan eta agertuko da.',
+
+	// P
+	'permalink_to' => 'Iruzkinari lotura iraunkorra',
+
+	// R
+	'reponse_comment_modere' => 'Zure iruzkina behar bezala grabatua izan da, eta berrirakurketa batean beha dago argitaratua izan aitzin.',
+	'reponse_comment_ok' => 'Milesker zure ekarpenagatik!',
+
+	// S
+	'saisie_texte_info' => 'Inprimaki honek SPIPen lastermarkak onartzen ditu <code>[-&gt;url] {{gizena}} {etzana} &lt;quote&gt; &lt;code&gt;</code> eta HTML koda <code>&lt;q&gt; &lt;del&gt; &lt;ins&gt;</code>. Paragrafo batzuen sortzeko, lerro huts batzuk utz iezazkizu.',
+	'saisie_texte_legend' => 'Zure iruzkina hemen gehitu',
+	'submit1' => 'Aurreikusi',
+	'submit2' => 'Bidalketa baieztatu',
+
+	// T
+	'titre_comments' => 'Iruzkinak'
+);
