00743c49f4d68ffa7d0e341a021811c625b09d8d
otalazt@gmail.com <otalazt@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-02-20 04:32:38 +0000

    [Salvatore] [source:_plugins_/afficher_objets/lang/ afficher_objets] Export depuis http://trad.spip.net de la langue eu
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@102948 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ afficher_objets/lang/afficher_objets_eu.php
@@ -0,0 +1,20 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/afficher_objets?lang_cible=eu
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// I
+	'info_statut' => 'Egoera',
+
+	// N
+	'nb_articles' => '@nb@ artikulu',
+
+	// R
+	'rang' => 'Lerroa'
+);
