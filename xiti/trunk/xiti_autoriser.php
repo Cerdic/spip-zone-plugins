ed8f776d2f84e52f66bc50d016d5501609a2a93e
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-02-21 08:14:38 +0000

    Un peu de PHPdoc
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@102986 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- xiti/trunk/xiti_autoriser.php
+++ xiti/trunk/xiti_autoriser.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Autorisations pour Xiti
  *
@@ -17,6 +16,19 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 function xiti_autoriser(){
 }
 
+/**
+ * Autorisation de créer un Niveau 2
+ *
+ * Il faut que les niveaux deux soient activés
+ * Seuls les administrateurs peuvent créer des niveaux deux
+ *
+ * @param string $faire
+ * @param string $quoi
+ * @param int $id
+ * @param array $qui
+ * @param array $opts
+ * @return boolean
+ */
 function autoriser_xitiniveau_creer_dist($faire, $quoi, $id, $qui, $opts) {
 	if (!function_exists('lire_config')) {
 		inclure_spip('inc/config');
@@ -25,10 +37,36 @@ function autoriser_xitiniveau_creer_dist($faire, $quoi, $id, $qui, $opts) {
 	return lire_config('xiti/niveaux_deux') == 'on' and $qui['statut'] == '0minirezo';
 }
 
+
+/**
+ * Autorisation de modifier un Niveau 2
+ *
+ * Identique à celle de création
+ *
+ * @param string $faire
+ * @param string $quoi
+ * @param int $id
+ * @param array $qui
+ * @param array $opts
+ * @return boolean
+ */
 function autoriser_xitiniveau_modifier_dist($faire, $quoi, $id, $qui, $opts) {
 	return autoriser('creer', 'xitiniveau', $id, $qui, $opts);
 }
 
+/**
+ * Autorisation de supprimer un Niveau 2
+ *
+ * Il faut que rien ne soit lié au niveau deux
+ * Sinon, identique à celle de création
+ *
+ * @param string $faire
+ * @param string $quoi
+ * @param int $id
+ * @param array $qui
+ * @param array $opts
+ * @return boolean
+ */
 function autoriser_xitiniveau_supprimer_dist($faire, $quoi, $id, $qui, $opts) {
 	if (sql_getfetsel('id_objet', 'spip_xiti_niveaux_liens', 'id_xiti_niveau='.intval($id))) {
 		return false;
@@ -36,15 +74,34 @@ function autoriser_xitiniveau_supprimer_dist($faire, $quoi, $id, $qui, $opts) {
 	return lire_config('xiti/niveaux_deux') == 'on' and $qui['statut'] == '0minirezo';
 }
 
+/**
+ * Autorisation d'accès au menu des niveaux 2
+ *
+ * Identique à celle de création
+ *
+ * @param string $faire
+ * @param string $quoi
+ * @param int $id
+ * @param array $qui
+ * @param array $opts
+ * @return boolean
+ */
 function autoriser_xitiniveaux_menu_dist($faire, $type, $id, $qui, $opt) {
 	return autoriser('creer', 'xitiniveau', $id, $qui, $opt);
 }
 
 /**
- * On ne peut joindre ou délier un niveau xiti qu'a un objet qu'on a le droit d'editer
+ * Autorisation de lier un niveau 2
+ *
+ * On ne peut joindre ou délier un niveau xiti qu'a un objet que l'on a le droit d'editer
  * Il faut aussi que les niveaux xiti aient ete actives sur les objets concernes
  *
- * @return bool
+ * @param string $faire
+ * @param string $quoi
+ * @param int $id
+ * @param array $qui
+ * @param array $opts
+ * @return boolean
  */
 function autoriser_lierxitiniveau_dist($faire, $type, $id, $qui, $opt) {
 	include_spip('inc/config');
@@ -52,6 +109,19 @@ function autoriser_lierxitiniveau_dist($faire, $type, $id, $qui, $opt) {
 		and (($id > 0 and autoriser('modifier', $type, $id, $qui, $opt)));
 }
 
+/**
+ * Autorisation de dissocier un niveau 2
+ *
+ * On ne peut joindre ou délier un niveau xiti qu'a un objet que l'on a le droit d'editer
+ * Il faut aussi que les niveaux xiti aient ete actives sur les objets concernes
+ *
+ * @param string $faire
+ * @param string $quoi
+ * @param int $id
+ * @param array $qui
+ * @param array $opts
+ * @return boolean
+ */
 function autoriser_dissocierxitiniveau_dist($faire, $type, $id, $qui, $opt) {
 	return autoriser('lierxitiniveau', $type, $id, $qui, $opt);
 }
