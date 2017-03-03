4667d3e3682312da187380e1ca54aeae0fcf4782
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-03-03 12:38:06 +0000

    - Ne pas ajouter recommander dans le pipeline de nospam s'il est déjà présent. - Up de z
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@103134 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- recommander/recommander_options.php
+++ recommander/recommander_options.php
@@ -1,6 +1,10 @@
 <?php
 
-if (!defined('_ECRIRE_INC_VERSION')) return;
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
 // brancher le plugin sur nospam
-$GLOBALS['formulaires_no_spam'][] = 'recommander';
+if (isset($GLOBALS['formulaires_no_spam']) and is_array($GLOBALS['formulaires_no_spam']) and !in_array('recommander', $GLOBALS['formulaires_no_spam'])) {
+	$GLOBALS['formulaires_no_spam'][] = 'recommander';
+}
