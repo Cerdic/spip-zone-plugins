38a769285ca7cb2a8cb5b72ba5c5f5a273bdc737
peetdu@gmail.com <peetdu@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-03-07 13:32:35 +0000

    Si les articles n’ont pas été sélectionnés dans la config, renvoyer le tableau ‘liste article’ sans gestion du Rang
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@103185 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- rang/trunk/rang_fonctions.php
+++ rang/trunk/rang_fonctions.php
@@ -26,3 +26,18 @@ function balise_RANG($p) {
 	
 	return $p;
 }
+
+/**
+ * Détecte si l'objet a été selectionné dans la configuration du plugin
+ *
+ * @param string $objet
+ *     article, rubrique, etc.
+ *
+ * @return bool
+ *
+ **/
+function rang_objet_dans_config($objet) {
+	$table = table_objet_sql($objet);
+	$liste = explode(',', lire_config('rang_objets'));
+	return in_array($table, $liste);
+}
