a017f844c84049b7d7892b6c2fa3952c7af22a93
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-03-08 21:40:33 +0000

    Ajout d'une fonctionnalité permettant d'enlever les caractères accentués, transformer les ponctuations et les espaces en un underscore. Ce séparateur est configurable. Up de z.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@103203 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spip-bonux-3/spip_bonux_fonctions.php
+++ spip-bonux-3/spip_bonux_fonctions.php
@@ -133,3 +133,29 @@ function push_table_valeur($table, $chemin, $valeur, $condition = true, $cle_fin
 	return $table;
 }
 }
+
+if (!function_exists('nom_machine')) {
+	/**
+	 * Cette fonction permet de transformer en nom machine, soit en enlevant tous les accents, toutes les ponctuations. Les espaces sont remplacés par le séparateur `_`.
+	 *
+	 * @param string $subject    Texte à transformer en nom machine
+	 * @param string $separateur Par défaut, un underscore `_`.
+	 *
+	 * @return string
+	 */
+	function nom_machine($subject, $separateur = '_') {
+		include_spip('inc/charsets');
+		$nom_tmp = trim($subject); // On enlève les espaces indésirables
+		$nom_tmp = translitteration($nom_tmp); // On enlève les accents et cie
+		$nom_tmp = preg_replace(",(/|[[:punct:][:space:]]+),u", $separateur,
+			$nom_tmp); // On enlève les espaces et les slashs
+		$nom_tmp = preg_replace("/(" . $separateur . "+)/", $separateur, $nom_tmp); // pas de double underscores
+		if (preg_match("/" . $separateur . "$/", $nom_tmp)) {
+			$nom_tmp = trim($nom_tmp, $separateur); // On ne doit pas terminer par le séparateur
+		}
+		$nom_tmp = preg_replace("/'/", $separateur, $nom_tmp); // pas d'apostrophes
+		$nom_tmp = strtolower($nom_tmp); // On met en minuscules
+
+		return $nom_tmp;
+	}
+}
