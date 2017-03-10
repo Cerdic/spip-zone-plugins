fbf55218dbbc979fa290de3deea0fd0e75df5cf1
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-03-10 22:43:32 +0000

    PSR SPIP
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@103227 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spip_piwik/trunk/inc/piwik_recuperer_data.php
+++ spip_piwik/trunk/inc/piwik_recuperer_data.php
@@ -1,10 +1,12 @@
 <?php
 
-if (!defined("_ECRIRE_INC_VERSION")) return;
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
 /**
  * Fonction de communication avec l'API REST du serveur Piwik
- * 
+ *
  * @param string $piwik_url Url du serveur
  * @param string $token_auth Le token d'autentification du serveur
  * @param string $module [optional]
@@ -13,21 +15,19 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
  * @param array $options [optional]
  * @return string Le contenu de la réponse
  */
-function inc_piwik_recuperer_data_dist($piwik_url,$token_auth,$module='API',$method,$format='PHP',$options=array()){
-	
-	$url = parametre_url($piwik_url,'token_auth',$token_auth);
-	$url = parametre_url($url,'module','API','&');
-	$url = parametre_url($url,'format',$format,'&');
-	$url = parametre_url($url,'method',$method,'&');
-	if(is_array($options)){
-		foreach($options as $cle => $val){
-			$url = parametre_url($url,$cle,$val,'&');
+function inc_piwik_recuperer_data_dist($piwik_url, $token_auth, $module = 'API', $method, $format = 'PHP', $options = array()) {
+	$url = parametre_url($piwik_url, 'token_auth', $token_auth);
+	$url = parametre_url($url, 'module', 'API', '&');
+	$url = parametre_url($url, 'format', $format, '&');
+	$url = parametre_url($url, 'method', $method, '&');
+	if (is_array($options)) {
+		foreach ($options as $cle => $val) {
+			$url = parametre_url($url, $cle, $val, '&');
 		}
 	}
-	
+
 	include_spip('inc/distant');
 	$content = recuperer_page($url);
-	
+
 	return $content;
 }
-?>
--- spip_piwik/trunk/piwik_pipelines.php
+++ spip_piwik/trunk/piwik_pipelines.php
@@ -1,30 +1,33 @@
 <?php
 /**
  * Plugin Piwik
- * 
+ *
  * @package SPIP\Piwik\Pipelines
  */
- 
-if (!defined("_ECRIRE_INC_VERSION")) return;
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
 /**
  * Insertion dans le pipeline insert_head (SPIP)
- * 
+ *
  * Ajout du code de piwik dans le head si configuré comme tel
- * 
+ *
  * @param string $flux
  * 		Le contenu de la balise #INSERT_HEAD
  * @return string $flux
  * 		Le contenu de la balise #INSERT_HEAD modifié
- * 
+ *
  */
-function piwik_insert_head($flux){
+function piwik_insert_head($flux) {
 	$options = array();
-	
-	if(!function_exists('lire_config'))
+
+	if (!function_exists('lire_config')) {
 		include_spip('inc/config');
-	
-	if(lire_config('piwik/mode_insertion') == 'pipeline'){
+	}
+
+	if (lire_config('piwik/mode_insertion') == 'pipeline') {
 		$options['type'] = 'public';
 		$flux .= piwik_head_js($options);
 	}
@@ -34,7 +37,7 @@ function piwik_insert_head($flux){
 
 /**
  * Insertion dans le pipeline header_prive (SPIP)
- * 
+ *
  * Insertion du code de Piwik dans l'espace privé si configuré comme tel
  *
  * @param string $flux
@@ -42,17 +45,18 @@ function piwik_insert_head($flux){
  * @return string $flux
  * 		Le contenu du head privé modifié
  */
-function piwik_header_prive($flux){
+function piwik_header_prive($flux) {
 	$options = array();
-	
-	if(!function_exists('lire_config'))
+
+	if (!function_exists('lire_config')) {
 		include_spip('inc/config');
-	
-	if(lire_config('piwik/piwik_prive')){
-		if(is_array(lire_config('piwik/restreindre_statut_prive'))){
+	}
+
+	if (lire_config('piwik/piwik_prive')) {
+		if (is_array(lire_config('piwik/restreindre_statut_prive'))) {
 			$options['statuts_restreints'] = lire_config('piwik/restreindre_statut_prive');
 		}
-		if(is_array(lire_config('piwik/restreindre_auteurs_prive'))){
+		if (is_array(lire_config('piwik/restreindre_auteurs_prive'))) {
 			$options['auteurs_restreints'] = lire_config('piwik/restreindre_auteurs_prive');
 		}
 		$options['type'] = 'prive';
@@ -65,46 +69,49 @@ function piwik_header_prive($flux){
  * La fonction de génération du code du tracker javascript
  *
  * @param array $options [optional]
- * 		
+ *
  * @return
  */
-function piwik_head_js($options=array()){
-	if(!function_exists('lire_config'))
+function piwik_head_js($options = array()) {
+	if (!function_exists('lire_config')) {
 		include_spip('inc/config');
+	}
 
-	$config = lire_config('piwik',array('id_piwik'=>false,'urlpiwik'=>false));
+	$config = lire_config('piwik', array('id_piwik' => false, 'urlpiwik' => false));
 	$id_piwik = $config['idpiwik'];
 	$url_piwik = $config['urlpiwik'];
 	$conformite_cnil = '';
-	if($config['conformite_cnil']=="on"){
+	if ($config['conformite_cnil'] == 'on') {
 		$conformite_cnil = '
-		'.recuperer_fond("inc/js-cnil");
+		'.recuperer_fond('inc/js-cnil');
 	}
 	$afficher_js = true;
 
 	$ret = '';
 
-	if($url_piwik && $id_piwik){
-		if((isset($options['statut_restreint']) && $options['statut_restreint']) || (isset($options['auteurs_restreints']) && $options['auteurs_restreints'])){
+	if ($url_piwik && $id_piwik) {
+		if ((isset($options['statut_restreint']) and $options['statut_restreint'])
+			or (isset($options['auteurs_restreints']) and $options['auteurs_restreints'])) {
 			$statut = isset($GLOBALS['visiteur_session']['statut']) ? $GLOBALS['visiteur_session']['statut'] : '';
-			$id_auteur = isset($GLOBALS['visiteur_session']['id_auteur']) ? $GLOBALS['visiteur_session']['id_auteur'] : '';
-			if(in_array($statut,$options['statuts_restreints'])){
+			$id_auteur = isset($GLOBALS['visiteur_session']['id_auteur']) ?
+				$GLOBALS['visiteur_session']['id_auteur'] : '';
+			if (in_array($statut, $options['statuts_restreints'])) {
 				$afficher_js = false;
 			}
-			if($afficher_js && in_array($id_auteur,$options['auteurs_restreints'])){
+			if ($afficher_js && in_array($id_auteur, $options['auteurs_restreints'])) {
 				$afficher_js = false;
 			}
 		}
 
-		if($afficher_js){
+		if ($afficher_js) {
 			$ret .= "
 <script type='text/javascript'>var _paq = _paq || [];$conformite_cnil
-	(function(){ var u=(('https:' == document.location.protocol) ? 'https://$url_piwik/' : 'http://$url_piwik/'); 
-	_paq.push(['setSiteId', $id_piwik]); 
-	_paq.push(['setTrackerUrl', u+'piwik.php']); 
-	_paq.push(['trackPageView']); 
-	_paq.push(['enableLinkTracking']); 
-	var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0]; g.type='text/javascript'; g.defer=true; g.async=true; g.src=u+'piwik.js'; 
+	(function(){ var u=(('https:' == document.location.protocol) ? 'https://$url_piwik/' : 'http://$url_piwik/');
+	_paq.push(['setSiteId', $id_piwik]);
+	_paq.push(['setTrackerUrl', u+'piwik.php']);
+	_paq.push(['trackPageView']);
+	_paq.push(['enableLinkTracking']);
+	var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0]; g.type='text/javascript'; g.defer=true; g.async=true; g.src=u+'piwik.js';
 	s.parentNode.insertBefore(g,s); })();
 </script>";
 		}
@@ -116,39 +123,39 @@ function piwik_head_js($options=array()){
 /**
  * Insertion dans le pipeline formulaire_verifier (SPIP)
  * On vérifie les valeurs des champs fournis
- * 
+ *
  * @param array $flux
  * 		Le contexte du pipeline
  * @return array $flux
  * 		Le contexte du pipeline modifié auquel on a ajouté nos erreurs potentielles
  */
-function piwik_formulaire_verifier($flux){
-	if($flux['args']['form'] == 'configurer_piwik'){
+function piwik_formulaire_verifier($flux) {
+	if ($flux['args']['form'] == 'configurer_piwik') {
 		$obligatoires = array('token','user','urlpiwik');
-		foreach($obligatoires as $obligatoire){
-			if(!_request($obligatoire)){
+		foreach ($obligatoires as $obligatoire) {
+			if (!_request($obligatoire)) {
 				$flux['data'][$obligatoire] = _T('info_obligatoire');
 			}
 		}
-		
+
 		$piwik_token = _request('token');
-		if (!$flux['data']['token'] && !preg_match('/^[a-f0-9]{32}$/i',$piwik_token)) {
+		if (!$flux['data']['token'] && !preg_match('/^[a-f0-9]{32}$/i', $piwik_token)) {
 			$flux['data']['token'] = _T('piwik:cfg_erreur_token');
 			return $flux;
 		}
 		$piwik_url = 'http://'._request('urlpiwik').'/';
-		
-		$piwik_recuperer_data = charger_fonction('piwik_recuperer_data','inc');
-		
+
+		$piwik_recuperer_data = charger_fonction('piwik_recuperer_data', 'inc');
+
 		/**
 		 * Vérifier la correspondance nom d'utilisateur/ token
 		 * Qui nous permettra par la suite de définir d'autres choses
 		 */
 		$method_verif_user = 'UsersManager.getUser';
 		$options_user = array('userLogin'=>_request('user'));
-		$datas_user = $piwik_recuperer_data($piwik_url,$piwik_token,'',$method_verif_user,'PHP',$options_user);
-		if(is_array($datas_user = unserialize($datas_user))){
-			if(!$flux['data']['user'] && $flux['data']['result'] == 'error'){
+		$datas_user = $piwik_recuperer_data($piwik_url, $piwik_token, '', $method_verif_user, 'PHP', $options_user);
+		if (is_array($datas_user = unserialize($datas_user))) {
+			if (!$flux['data']['user'] and $flux['data']['result'] == 'error') {
 				$flux['data']['user'] = _T('piwik:cfg_erreur_user_token');
 			}
 			/**
@@ -156,28 +163,27 @@ function piwik_formulaire_verifier($flux){
 			 * Si non : mettre une meta comme quoi il n'est pas admin pour créer des sites
 			 */
 			$method_verif_user_bis = 'UsersManager.getUsers';
-			$datas_user_bis = $piwik_recuperer_data($piwik_url,$piwik_token,'',$method_verif_user_bis,'PHP');
+			$datas_user_bis = $piwik_recuperer_data($piwik_url, $piwik_token, '', $method_verif_user_bis, 'PHP');
 			$datas_user_bis = unserialize($datas_user_bis);
-			if(is_array($datas_user_bis) && ($datas_user_bis['result'] == 'error')){
+			if (is_array($datas_user_bis) and ($datas_user_bis['result'] == 'error')) {
 				ecrire_meta('piwik_admin', 'non');
-			}else{
+			} else {
 				ecrire_meta('piwik_admin', 'oui');
 				unset($flux['data']['user']);
 			}
 		}
-	
+
 		/**
-		 * Récupération de la liste des sites où cet utilisateur 
+		 * Récupération de la liste des sites où cet utilisateur
 		 * a les droits d'admin
 		 */
 		$method = 'SitesManager.getSitesWithAdminAccess';
-		$datas = $piwik_recuperer_data($piwik_url,$piwik_token,'',$method,'PHP');
-		if(!$flux['data']['urlpiwik'] && !is_array(unserialize($datas))){
+		$datas = $piwik_recuperer_data($piwik_url, $piwik_token, '', $method, 'PHP');
+		if (!$flux['data']['urlpiwik'] and !is_array(unserialize($datas))) {
 			$flux['data']['urlpiwik'] = _T('piwik:cfg_erreur_recuperation_data');
-		}else{
-			ecrire_meta('piwik_sites_dispo', $datas);	
+		} else {
+			ecrire_meta('piwik_sites_dispo', $datas);
 		}
 	}
 	return $flux;
 }
-?>
