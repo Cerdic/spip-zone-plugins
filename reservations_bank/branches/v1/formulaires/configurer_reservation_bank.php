a9a910037f13db86a98dc574c01eb310f34efbc4
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-03-11 08:44:55 +0000

    les fichiers
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@103237 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ reservations_bank/branches/v1/formulaires/configurer_reservation_bank.php
@@ -0,0 +1,32 @@
+<?php
+
+// Sécurité
+if (!defined('_ECRIRE_INC_VERSION'))
+	return;
+
+function formulaires_configurer_reservation_bank_saisies_dist() {
+	include_spip('inc/config');
+	$config = lire_config('reservation_bank');
+
+	return array(
+		array(
+			'saisie' => 'fieldset',
+			'options' => array(
+				'nom' => 'fieldset_parametres',
+				'label' => _T('reservation_evenement:cfg_titre_parametrages')
+			),
+
+			'saisies' => array(
+				array(
+					'saisie' => 'oui_non',
+					'options' => array(
+						'nom' => 'cacher_paiement_public',
+						'label' => _T('reservation_bank:label_cacher_paiement_public'),
+						'defaut' => $config['cacher_paiement_public']
+					)
+				),
+			),
+		)
+	);
+}
+?>
