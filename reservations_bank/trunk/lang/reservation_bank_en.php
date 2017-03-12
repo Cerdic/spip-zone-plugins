3a6d4117310468d4cd66e9bff86e7272d01c8a1a
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-03-12 04:32:54 +0000

    [Salvatore] [source:_plugins_/reservations_bank/trunk/lang/ reservation_bank] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@103258 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- reservations_bank/trunk/lang/reservation_bank_en.php
+++ reservations_bank/trunk/lang/reservation_bank_en.php
@@ -1,10 +1,13 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/reservation_bank?lang_cible=en
+// ** ne pas modifier le fichier **
 
-if (!defined('_ECRIRE_INC_VERSION')) return;
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-
 
 	// L
 	'label_cacher_paiement_public' => 'do not display the paiment form on the public site',
@@ -18,7 +21,6 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'message_erreur_montant_reservations_detail' => 'The amount should not be higher than @montant_ouvert@ (open amount)',
 	'montant_paye' => 'Paid :',
 
-
 	// P
 	'paiement_reservation' => 'Paiment of the booking #@id_reservation@',
 
@@ -30,5 +32,5 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'texte_statut_attente_paye' => ' paid - in waiting list',
 	'titre_page_configurer_reservation_bank' => 'Settings Booking Bank',
 	'titre_paiement_reservation' => 'Paiment of the Booking',
-	'titre_payer_reservation' => 'Pay the booking',
+	'titre_payer_reservation' => 'Pay the booking'
 );
