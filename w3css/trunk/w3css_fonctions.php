8a9da634c94c585e6b0f964dd7ec6483deee5f41
placido@roxing.net <placido@roxing.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-03-13 15:32:59 +0000

    v2.99.0 Peaufiner encore les styles des éléments spip (surtout forms, message d'erreur). D'autres éléments facultatifs passent en commentaires (à l'intégrateur de les reprendre manuellement dans le fichier perso.scss.html si besoin).
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@103267 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- w3css/trunk/w3css_fonctions.php
+++ w3css/trunk/w3css_fonctions.php
@@ -1,8 +1,10 @@
 <?php
 
-if (!defined('_ECRIRE_INC_VERSION')) return;
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
-// changer le prefixe w3- à la volée
+// changer le prefixe w3- à la volée dans le CSS généré
 function w3css_namespace($fond){
 
   $ns = substr(lire_config('w3css/namespace') ,0,5); // 5 caractères max
