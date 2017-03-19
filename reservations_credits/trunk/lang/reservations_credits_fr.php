34a6c347f1b09ec6547099ccb5d00dd3d1662806
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-03-19 04:32:55 +0000

    [Salvatore] [source:_plugins_/reservations_credits/trunk/lang/ reservations_credits] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@103339 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- reservations_credits/trunk/lang/reservations_credits_fr.php
+++ reservations_credits/trunk/lang/reservations_credits_fr.php
@@ -1,12 +1,12 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
-if (!defined('_ECRIRE_INC_VERSION')) return;
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/reservations_credits/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// R
-	'reservations_credits_titre' => 'Réservations Crédits',
+	'reservations_credits_titre' => 'Réservations Crédits'
 );
-
-?>
