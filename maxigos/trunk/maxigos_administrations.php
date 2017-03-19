e6dcdf50f333801c6c9fb4e1730f12e33ac51359
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-03-19 16:09:45 +0000

    correction fonction d'installation / déinstallation
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@103345 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- maxigos/trunk/maxigos_administrations.php
+++ maxigos/trunk/maxigos_administrations.php
@@ -1,14 +1,23 @@
 <?php
 
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
-function maxigos_install() {
-	$maxigos_res = sql_countsel('spip_types_documents', 'extension="sgf"');
-	if ($maxigos_res == 0) {sql_insertq('spip_types_documents', array('titre'=>'SGF', 'extension'=>'sgf', 'inclus'=>'non', 'upload'=>'oui'));}
+include_spip('base/upgrade');
+function maxigos_upgrade($nom_meta_base_version, $version_cible) {
+	$maj['create'] = array(
+		array(
+				'sql_insertq',
+				'spip_types_documents', 
+				array('titre'=>'SGF', 
+							'extension'=>'sgf', 
+							'inclus'=>'non', 
+							'mime_type'=>'application/x-go-sgf',
+							'upload'=>'oui')
+			)
+		);
+	maj_plugin($nom_meta_base_version, $version_cible, $maj);
 }
 
-function maxigos_unistall() {
-	$maxigos_res = sql_countsel('spip_types_documents', 'extension="sgf"');
-	if ($maxigos_res > 1) {sql_delete('spip_types_documents', 'extension="sgf"');}
+function maxigos_vider_tables($nom_meta_base_version) {
+	effacer_meta($nom_meta_base_version);
 }
-
-?>
