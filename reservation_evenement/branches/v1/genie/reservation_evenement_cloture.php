574962c3e4cf8a237af0de3e2e1ebc4e5a0f594a
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-03-19 08:04:42 +0000

    v1.23.16: eviter problèmes de nommage de table sous spip 3.1
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@103342 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- reservation_evenement/branches/v1/genie/reservation_evenement_cloture.php
+++ reservation_evenement/branches/v1/genie/reservation_evenement_cloture.php
@@ -10,40 +10,40 @@
  */
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
-	
+
 //Cloturer un évènement
 function genie_reservation_evenement_cloture_dist ($t) {
 	$date=date('Y-m-d G:i:s');
-	
+
 	include_spip('action/editer_objet');
 
 	//Sélection des détails de réservation d'événements passé  qui ont action_cloture activé
 	$sql=sql_select(
-		'id_reservations_detail,spip_reservations_details.id_evenement,date_fin',
-		'spip_reservations_details,spip_evenements',
-		'spip_reservations_details.statut="accepte" AND 
-			spip_reservations_details.id_evenement=spip_evenements.id_evenement AND
-			spip_evenements.date_fin <="'.$date.'" AND
-			spip_evenements.action_cloture =1' 
+		'id_reservations_detail,t1.id_evenement,date_fin',
+		'spip_reservations_details t1,spip_evenements t2',
+		't1.statut="accepte" AND
+			t1.id_evenement=t2.id_evenement AND
+			t2.date_fin <="'.$date.'" AND
+			t2.action_cloture =1'
 		);
-	
-	$id_evenement=array();	
+
+	$id_evenement=array();
 	while($data=sql_fetch($sql)){
 		if(!$date_fin=sql_getfetsel('date_fin','spip_evenements','id_evenement_source='.$data['id_evenement'],'','date_fin DESC','1'))
 		$date_fin=$data['date_fin'];
 
-		spip_log('cron évènement cloturé I date fin:'.strtotime($date_fin).', date:'.strtotime($date).' id_evenement:'.$data['id_evenement'].', id_reservations_detail:'.$data['id_reservations_detail'],'reservation_evenement');	
-			
+		spip_log('cron évènement cloturé I date fin:'.strtotime($date_fin).', date:'.strtotime($date).' id_evenement:'.$data['id_evenement'].', id_reservations_detail:'.$data['id_reservations_detail'],'reservation_evenement');
+
 		//Déclencher le changement de statut et les actions qui en dépendent
 		if(strtotime($date_fin)<=strtotime($date)){
 			spip_log('cron évènement cloturé II date fin:'.$date_fin.', date:'.$date.' id_evenement:'.$data['id_evenement'].', id_reservations_detail:'.$data['id_reservations_detail'],'reservation_evenement');
 			set_request('envoi_separe_actif','oui'); //Nécessaire pour permettre l'envoi du mail
 			objet_instituer('reservations_detail',$data['id_reservations_detail'],array('statut'=>'cloture'));
-			$id_evenement[]	= $data['id_evenement'];		
+			$id_evenement[]	= $data['id_evenement'];
 		}
 	};
-	
+
 	if (count($id_evenement)>0) sql_updateq('spip_evenements',array('action_cloture'=>3),'id_evenement IN ('.implode(',',$id_evenement).')');
-	
+
 	return 1;
 }
