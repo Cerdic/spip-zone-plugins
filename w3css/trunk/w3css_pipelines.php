65a20c658fab63f94b5afa69d826170c11896a97
pierre.laszczak@gmail.com <pierre.laszczak@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-03-20 09:54:35 +0000

    Uniquement le webmaster doit pouvoir recalculer le scss. c'est sale mais faute de trouver mieux...
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@103351 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- w3css/trunk/w3css_pipelines.php
+++ w3css/trunk/w3css_pipelines.php
@@ -10,9 +10,10 @@ function w3css_insert_head_css($flux){
 	$dir =  $GLOBALS['path'] . _NOM_TEMPORAIRES_ACCESSIBLES;
 	$fsdir = $_SERVER['DOCUMENT_ROOT'] .'/'. $dir;
 	$filename = "w3.css";
-
+	
 	# On utilise le compilateur scss uniquement en var_mode recalcul ou css ( et pas calcul )
-	if ( _request('var_mode') === "recalcul" || _request('var_mode') === "css" ) {
+	if ( isset($GLOBALS['visiteur_session']['webmestre'])
+		 && ( _request('var_mode') === "recalcul" || _request('var_mode') === "css") ) {
 		$data = scss_css(recuperer_fond('css/w3.css'));
 		ecrire_fichier($fsdir . $filename, $data);
 		$flux .= "<link rel='stylesheet' href='" . $dir . $filename . "' type='text/css' media='all' />\n";
