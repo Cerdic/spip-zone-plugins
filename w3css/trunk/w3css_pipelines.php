3650a68a455f84a3eed19223a7face3cfbcbfe29
pierre.laszczak@gmail.com <pierre.laszczak@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-03-20 09:07:29 +0000

    Ne pas utiliser le compilateur scss sur les hits ?var_mode=calcul
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@103350 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- w3css/trunk/w3css_pipelines.php
+++ w3css/trunk/w3css_pipelines.php
@@ -6,9 +6,20 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 
 function w3css_insert_head_css($flux){
 
-		if ($f = produire_fond_statique('css/w3.css', array('format'=>'css')) ) {
-			$flux .= "<link rel='stylesheet' href='" . $f . "' type='text/css' media='all' />\n";
-		}
+	# Definir les chemins, stocker le css généré dans /local/w3.css
+	$dir =  $GLOBALS['path'] . _NOM_TEMPORAIRES_ACCESSIBLES;
+	$fsdir = $_SERVER['DOCUMENT_ROOT'] .'/'. $dir;
+	$filename = "w3.css";
+
+	# On utilise le compilateur scss uniquement en var_mode recalcul ou css ( et pas calcul )
+	if ( _request('var_mode') === "recalcul" || _request('var_mode') === "css" ) {
+		$data = scss_css(recuperer_fond('css/w3.css'));
+		ecrire_fichier($fsdir . $filename, $data);
+		$flux .= "<link rel='stylesheet' href='" . $dir . $filename . "' type='text/css' media='all' />\n";
+	}
+	else {
+		$flux .= "<link rel='stylesheet' href='" . $dir . $filename . "' type='text/css' media='all' />\n";
+	}
 
     return $flux;
 }
