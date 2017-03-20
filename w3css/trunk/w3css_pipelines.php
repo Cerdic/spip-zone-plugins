9fcba06736cc71bb93cafdf3ff9a06d562da88bc
placido@roxing.net <placido@roxing.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-03-20 11:44:13 +0000

    En fait, on peut se simplifier la vie  à condition d'appliquer scss_css à la suite de produire_fond_statique, et non pas avant.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@103353 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- w3css/trunk/w3css_pipelines.php
+++ w3css/trunk/w3css_pipelines.php
@@ -6,21 +6,10 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 
 function w3css_insert_head_css($flux){
 
-	# Definir les chemins, stocker le css généré dans /local/w3.css
-	$dir =  $GLOBALS['path'] . _NOM_TEMPORAIRES_ACCESSIBLES;
-	$fsdir = $_SERVER['DOCUMENT_ROOT'] .'/'. $dir;
-	$filename = "w3.css";
-	
-	# On utilise le compilateur scss uniquement en var_mode recalcul ou css ( et pas calcul )
-	if ( isset($GLOBALS['visiteur_session']['webmestre'])
-		 && ( _request('var_mode') === "recalcul" || _request('var_mode') === "css") ) {
-		$data = scss_css(recuperer_fond('css/w3.css'));
-		ecrire_fichier($fsdir . $filename, $data);
-		$flux .= "<link rel='stylesheet' href='" . $dir . $filename . "' type='text/css' media='all' />\n";
-	}
-	else {
-		$flux .= "<link rel='stylesheet' href='" . $dir . $filename . "' type='text/css' media='all' />\n";
-	}
+		if ($f = produire_fond_statique('css/w3.css', array('format'=>'css')) ) {
+			$f = (lire_config('w3css/extend') == 'on') ? scss_css($f) : $f;
+			$flux .= "\n".'<link rel="stylesheet" href="' . $f . '" type="text/css" />'."\n";
+		}
 
     return $flux;
 }
