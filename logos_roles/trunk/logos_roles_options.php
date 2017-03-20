c73b7549559db3b1b98a4a03958b0c875ff61833
bystrano@gmx.ch <bystrano@gmx.ch@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-03-20 16:40:14 +0000

    on crée des presets pour le massicot aux dimensions demandées par les logos
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@103391 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- logos_roles/trunk/logos_roles_options.php
+++ logos_roles/trunk/logos_roles_options.php
@@ -14,3 +14,25 @@ if (! isset($GLOBALS['roles_logos']['logo_survol'])) {
 		'objets' => array_map('table_objet', array_keys(lister_tables_objets_sql())),
 	);
 }
+
+// On crée des presets pour le massicot aux dimensions des logos
+include_spip('inc/plugin');
+if (plugin_est_installe('massicot')) {
+
+	if (! (isset($GLOBALS['presets_format_massicot']) and is_array($GLOBALS['presets_format_massicot']))) {
+		$GLOBALS['presets_format_massicot'] = array();
+	}
+
+	foreach (lister_logos_roles() as $role => $label) {
+
+		if (isset($GLOBALS['roles_logos'][$role]['dimensions'])) {
+			$dimensions = $GLOBALS['roles_logos'][$role]['dimensions'];
+
+			$GLOBALS['presets_format_massicot'][] = array(
+				'nom' => $label,
+				'largeur' => $dimensions['largeur'],
+				'hauteur' => $dimensions['hauteur'],
+			);
+		}
+	}
+}
