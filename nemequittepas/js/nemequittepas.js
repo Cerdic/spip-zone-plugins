8d816bb5916bcc4704c7a717bd4050a33bcd6b12
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-03-21 23:54:15 +0000

    Repasse en test car ça ne marche pas sur le bouton retour d'édition d'un article (on perd le contenu édité).
    Ne marchait pas sur la modification d'un article existant. onAjaxLoad à la rescousse !
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@103436 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- nemequittepas/js/nemequittepas.js
+++ nemequittepas/js/nemequittepas.js
@@ -1,6 +1,10 @@
-$(document).ready(function () {
-    $(function() {
-		// With a custom message
-		$('.formulaire_editer form').areYouSure( {'message':'Voulez-vous vraiment quitter la page et perdre votre saisie ?'} );
+function nemequittepas() {
+	jQuery('.formulaire_editer form').areYouSure( {'message':'Voulez-vous vraiment quitter la page et perdre votre saisie ?'} );
+}
+
+jQuery(document).ready(function () {
+	jQuery(window).load(function() {
+		nemequittepas();
+		onAjaxLoad(nemequittepas);
 	});
 });
