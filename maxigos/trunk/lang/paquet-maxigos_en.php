5dfd26a982fd2ef9d69d4c56d357dacc04e1a04d
robert.sebille@cassiopea.org <robert.sebille@cassiopea.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-03-24 08:24:13 +0000

    Corrections mineures: categorie, logo, commentaires
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@103474 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- maxigos/trunk/lang/paquet-maxigos_en.php
+++ maxigos/trunk/lang/paquet-maxigos_en.php
@@ -1,11 +1,11 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/albums/trunk/lang/
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/maxigos/trunk/lang/
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
-	'maxigos_description' => 'Maxigos proposes a model for Spip which allows the implementation of the main functionalities of [maxiGos-> http://jeudego.org/maxiGos/], « a set of tools allowing to insert parts, problems and diagrams of Go saved in sgf format in a web page. »
+	'maxigos_description' => 'maxigos proposes a model for Spip which allows the implementation of the main functionalities of [maxiGos-> http://jeudego.org/maxiGos/], « a set of tools allowing to insert parts, problems and diagrams of Go saved in sgf format in a web page. »
 
 The sgf content can be written as a model parameter or contained in a sgf file ([Smart Game Format-> https://en.wikipedia.org/wiki/Smart_Game_Format]).
 
