46c651ba76b6416d54ba8023e5d32776d392830b
lspeciale@gmail.com <lspeciale@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-03-26 12:52:06 +0000

    Description et slogan ne s'affichent pa. On verra
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@103558 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ tablos/trunk/lang/paquet-tablos_fr.php
@@ -0,0 +1,9 @@
+<?php
+	if (!defined('_ECRIRE_INC_VERSION')) {
+		return;
+	}
+	$GLOBALS[$GLOBALS['idx_lang']] = array(
+		'tablos_description' => 'Ce plugin permet d\'importer un fichier csv (provenant d\'un logiciel tableur) et de l\'afficher.',
+		'tablos_slogan' => 'Affichage des CSVs',	
+);
+?>
