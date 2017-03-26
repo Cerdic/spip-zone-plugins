6447db94dc2ca2fcdb4f3a8e21505d1bf6e1d0e5
klaus@spip.de <klaus@spip.de@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-03-26 03:32:58 +0000

    [Salvatore] [source:_plugins_/simplog/trunk/lang/ simplog] Export depuis http://trad.spip.net de la langue de
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@103539 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ simplog/trunk/lang/simplog_de.php
@@ -0,0 +1,52 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/simplog?lang_cible=de
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'actualiser' => 'Liste aktualisieren', # RELIRE
+
+	// C
+	'clic_rafraichir' => 'erneut laden', # RELIRE
+	'colonne_date' => 'Datum',
+	'colonne_gravite' => 'Gewicht', # RELIRE
+	'colonne_hit' => 'Hit',
+	'colonne_ip' => 'IP',
+	'colonne_pid' => 'PID', # RELIRE
+	'colonne_texte' => 'Text',
+
+	// D
+	'description_simplog' => 'Diese Seite ermöglicht das Einsehen der Logdateien in den Ordnern von SPIP.', # RELIRE
+
+	// F
+	'fichier' => 'Datei « @fichier@ »',
+	'filtre_gravite_aucun' => 'Alle Gewichte', # RELIRE
+
+	// I
+	'info_hit_pri' => 'Privat', # RELIRE
+	'info_hit_pub' => 'Öffentlich', # RELIRE
+
+	// L
+	'label_fichier' => 'Datei',
+	'label_fonction' => 'Funktion',
+	'label_ligne' => 'Zeile',
+	'liste_fic' => 'Logdateiliste', # RELIRE
+	'logs' => 'Logdateien einsehen', # RELIRE
+
+	// M
+	'message_fichier_choisir' => 'Wählen Sie eine zu anlysierende Datei.', # RELIRE
+	'message_fichier_filtre_vide' => 'Diese Datei enthält keinen Eintrag mit dem Gewicht « @gravite@ ».', # RELIRE
+	'message_fichier_non_log' => 'Die Datei « @fichier@ » ist keine Logdatei oder ist nicht mehr vorhanden.', # RELIRE
+	'message_fichier_suppression' => 'Wollen Sie die Logdatei wirklich löschen?', # RELIRE
+	'message_fichier_vide' => 'Die Datei ist leer.', # RELIRE
+
+	// T
+	'tri_croissant' => 'Aufsteigende Sortierung', # RELIRE
+	'tri_decroissant' => 'Absteigende Sortierung' # RELIRE
+);
