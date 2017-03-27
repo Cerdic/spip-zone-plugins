78bca95f0c56eb5e129168e6881d2532c4941eed
erational@erational.org <erational@erational.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-03-27 12:17:48 +0000

    PSR et pétouilles
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@103609 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- verifie31/lang/paquet-verifie31_fr.php
+++ verifie31/lang/paquet-verifie31_fr.php
@@ -2,15 +2,14 @@
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
 
 if (!defined('_ECRIRE_INC_VERSION')) {
-    return;
+	return;
 }
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-    // V
-    'verifie31_description' => 'Le plugin permet de vérifier si les plugins d\un site SPIP 3.0 sont compatibles avec la version 3.1.',
-    'verifie31_nom' => 'Lister les plugins compatibles SPIP 3.1',
-    'verifie31_slogan' => 'Vos plugins sont-ils compatibles à SPIP 3.1 ?',
+	// V
+	'verifie31_description' => 'Le plugin permet de vérifier si les plugins d\un site SPIP 3.0 sont compatibles avec la version 3.1.',
+	'verifie31_nom' => 'Lister les plugins compatibles SPIP 3.1',
+	'verifie31_slogan' => 'Vos plugins sont-ils compatibles à SPIP 3.1 ?',
 );
 
-?>
--- verifie31/prive/squelettes/contenu/verifie31_fonctions.php
+++ verifie31/prive/squelettes/contenu/verifie31_fonctions.php
@@ -1,5 +1,6 @@
 <?php
-if (!defined('_ECRIRE_INC_VERSION')) return;
-
-include_spip("inc/plugin");       // pour le filtre plugin_version_compatible
-?>
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+// pour le filtre plugin_version_compatible
+include_spip("inc/plugin");
