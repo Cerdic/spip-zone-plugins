e478dc2bb5e3e5c099d0f7178e1fca492a772469
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-04-01 08:39:32 +0000

    v 1.2.0 - Enlever le message de confirmation original et ne montrer que le récapitulatif (plus clair) - Permettre de choisir si le formulaire de paiment s'affiche avant ou après le récapitulatif - Salvatore
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@103680 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- reservations_bank/branches/v1/lang/reservation_bank_fr.php
+++ reservations_bank/branches/v1/lang/reservation_bank_fr.php
@@ -1,24 +1,25 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
-if (!defined('_ECRIRE_INC_VERSION')) return;
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/reservations_bank/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-
 	// L
-	'label_cacher_paiement_public' => 'ne pas proposer de formulaire de paiement sur le site public',
+	'label_cacher_paiement_public' => 'Ne pas proposer de formulaire de paiement sur le site public',
 	'label_fieldset_montant_detail' => 'Spécifiez le montant (en @devise@) pour chaque détail de réservation',
 	'label_fieldset_specifier' => 'Spécifier',
+	'label_preceder_formulaire' => 'Préceder le formulaire de paiment au récapitulatif de la commande.',
 	'label_specifier_montant' => 'Spécifier le montant',
 
 	// M
 	'merci_de_votre_reservation_paiement' => 'Nous avons bien validé votre réservation <b>@reference@</b>.',
-	'message_erreur_montant_credit' => 'Vous avez depassé la limite de votre crédit qui es de @credit@ !',
+	'message_erreur_montant_credit' => 'Vous avez depassé la limite de votre crédit qui es de @credit@ !',
 	'message_erreur_montant_reservations_detail' => 'Le montant ne doit pas être supérieure à @montant_ouvert@ (montant encore à payer)',
-	'message_paiement_vendeur' => 'Mode de paiement : "@mode@",  voir <a href="@url@">détail</a>',
-	'montant_paye' => 'Payé :',
-
+	'message_paiement_vendeur' => 'Mode de paiement : "@mode@",  voir <a href="@url@">détail</a>',
+	'montant_paye' => 'Payé :',
 
 	// P
 	'paiement_commande' => 'Paiement de la commande #@id_commande@',
@@ -26,15 +27,13 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// R
 	'reservation_bank_titre' => 'Réservations Bank',
-	'reservation_paiement_reference' => 'Référence du paiement: @reference@',
+	'reservation_paiement_reference' => 'Référence du paiement : @reference@',
 
 	// T
-	'texte_statut_attente_part' => ' payé partiellement- dans liste d\'attente',
-	'texte_statut_attente_paye' => ' payé - dans liste d\'attente',
+	'texte_statut_attente_part' => ' payé partiellement- dans liste d’attente',
+	'texte_statut_attente_paye' => ' payé - dans liste d’attente',
 	'titre_page_configurer_reservation_bank' => 'Configuration Réservations Bank',
 	'titre_paiement_reservation' => 'Paiement de la réservation',
-	'titre_paiement_vendeur' => 'Paiement :',
-	'titre_payer_reservation' => 'Payez la réservation',
+	'titre_paiement_vendeur' => 'Paiement :',
+	'titre_payer_reservation' => 'Payez la réservation'
 );
-
-?>
