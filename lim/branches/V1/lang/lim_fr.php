6effd846b523ea99d5a088f1a08f5389b7543ba7
peetdu@gmail.com <peetdu@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-04-03 11:22:48 +0000

    Report de r103737 : Pouvoir désactiver certains champs du formulaire Auteurs (Bio, pgp, site Web). Compatibilité SPIP 3.2
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@103738 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- lim/branches/V1/lang/lim_fr.php
+++ lim/branches/V1/lang/lim_fr.php
@@ -14,8 +14,12 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'cfg_titre_petitions' => 'Pétitions',
 	'cfg_titre_forums_publics' => 'Forums Publics',
 	'cfg_titre_afficher_logos' => 'Désactiver les logos par contenu',
+	'cfg_titre_divers' => 'Divers',
+	'cfg_titre_champs_auteur' => 'Formulaire Auteur',
 	'cfg_titre_restriction_par_rubrique' => 'Restreindre les contenus par rubrique',
 	'cfg_select_objets' => 'sélectionner les objets',
+	'cfg_select_objets' => 'sélectionner les objets',
+	
 
 	'info_creer_dans_rubrique_non_autorise' => 'Vous ne pouvez pas créer dans cette rubrique',
 	'info_deplacer_dans_rubrique_non_autorise' => 'Vous ne pouvez pas déplacer dans cette rubrique',
@@ -25,9 +29,13 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'label_desactiver_article_par_rubrique' => "Cocher les rubriques où désactiver le bouton ",
 	'label_forums_publics' => "Désactiver l'affichage du bloc Forum dans les pages Articles ?",
 	'label_petitions' => "Désactiver l'affichage du bloc Pétitions dans les pages Articles ?",
+	'label_desactive_auteur_bio' => "Désactiver l'affichage du champ <i>Qui est l’auteur</i> ?",
+	'label_desactive_auteur_pgp' => "Désactiver l'affichage du champ <i>Clé PGP</i> ?",
+	'label_desactive_auteur_liens_sites' => "Désactiver l'affichage du fieldset <i>Lien Site</i> ?",
 
 	// O
 	'onglet_lim' => 'Forums Publics et Pétitions',
+	'onglet_lim_divers' => 'Divers',
 	'onglet_lim_logo' => 'Logos',
 	'onglet_lim_rubriques' => 'Contenus par rubrique',
 
