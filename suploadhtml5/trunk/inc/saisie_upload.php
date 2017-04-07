0dc47f06edc6ee2320ccaa78286354b1291e1c96
p@henix.be <p@henix.be@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-04-07 15:11:38 +0000

    Des tabs au lieu des espaces
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@103816 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- suploadhtml5/trunk/inc/saisie_upload.php
+++ suploadhtml5/trunk/inc/saisie_upload.php
@@ -39,15 +39,15 @@ function chercher_saisie_upload($saisies) {
  * @return mixed
  */
 function saisie_upload_get() {
-    // récupérer les documents en session
-    $documents = session_get('upload');
+	// récupérer les documents en session
+	$documents = session_get('upload');
 
-    if (empty($documents)) {
-        return false;
-    }
+	if (empty($documents)) {
+		return false;
+	}
 
-    // On va renvoyer un tableau formaté pour passer dans objet_associer
-    return array('document' => $documents);
+	// On va renvoyer un tableau formaté pour passer dans objet_associer
+	return array('document' => $documents);
 }
 
 /**
@@ -56,7 +56,7 @@ function saisie_upload_get() {
  * @access public
  */
 function saisie_upload_terminer() {
-    session_set('upload', null);
+	session_set('upload', null);
 }
 
 /**
@@ -66,9 +66,9 @@ function saisie_upload_terminer() {
  * @access public
  */
 function saisie_supprimer_document_session($id_document) {
-    $upload = session_get('upload');
-    unset($upload[array_search($id_document, $upload)]);
-    session_set('upload', $upload);
+	$upload = session_get('upload');
+	unset($upload[array_search($id_document, $upload)]);
+	session_set('upload', $upload);
 }
 
 /**
@@ -83,40 +83,40 @@ function saisie_supprimer_document_session($id_document) {
  */
 function saisie_upload_traiter($objet, $id_objet, $lien_direct = false) {
 
-    include_spip('action/editer_objet');
-    include_spip('action/editer_liens');
+	include_spip('action/editer_objet');
+	include_spip('action/editer_liens');
 
-    // Récupérer les documents et associer à l'objet
-    $documents = saisie_upload_get();
+	// Récupérer les documents et associer à l'objet
+	$documents = saisie_upload_get();
 
-    if (!$documents) {
-        return false;
-    }
+	if (!$documents) {
+		return false;
+	}
 
-    if (!$lien_direct) {
-	    objet_associer(
-		    $documents,
-		    array($objet => $id_objet)
-	    );
-    } else {
-	    // Traitement des liens directs entre les objets
-	    // Lorsqu'il y a un champ id_document sur un objet
-	    $table = table_objet_sql($objet);
-	    $cle_primaire = id_table_objet($objet);
-	    sql_updateq(
-		    $table,
-		    array('id_document' => $documents['document'][0]),
-		    $cle_primaire.'='.$id_objet
-	    );
-    }
+	if (!$lien_direct) {
+		objet_associer(
+			$documents,
+			array($objet => $id_objet)
+		);
+	} else {
+		// Traitement des liens directs entre les objets
+		// Lorsqu'il y a un champ id_document sur un objet
+		$table = table_objet_sql($objet);
+		$cle_primaire = id_table_objet($objet);
+		sql_updateq(
+			$table,
+			array('id_document' => $documents['document'][0]),
+			$cle_primaire.'='.$id_objet
+		);
+	}
 
    // Le lien est fait, les documents ne doivent plus être en mode temporaire
-    foreach ($documents['document'] as $id_document) {
-        objet_instituer('document', $id_document, array('statut' => 'publie'));
-    }
+	foreach ($documents['document'] as $id_document) {
+		objet_instituer('document', $id_document, array('statut' => 'publie'));
+	}
 
-    // Terminer l'upload en nettoyant la session
-    saisie_upload_terminer();
+	// Terminer l'upload en nettoyant la session
+	saisie_upload_terminer();
 }
 
 /**
@@ -125,40 +125,40 @@ function saisie_upload_traiter($objet, $id_objet, $lien_direct = false) {
  * @param mixed $objet
  * @param mixed $id_objet
  * @param bool $supprimer
- *        Supprime le fichier logo de la session et de la médiathèque si true
+ *		  Supprime le fichier logo de la session et de la médiathèque si true
  * @access public
  */
 function saisie_upload_traiter_logo($objet, $id_objet, $supprimer = true) {
 
-    // On prend le premier fichier image de la saisie et on le transforme en logo
-    $documents = saisie_upload_get();
+	// On prend le premier fichier image de la saisie et on le transforme en logo
+	$documents = saisie_upload_get();
 
-    if (!$documents) {
-        return false;
-    }
+	if (!$documents) {
+		return false;
+	}
 
-    $fichier = sql_fetsel(
-        'id_document,fichier',
-        'spip_documents',
-        array(
-            'media='.sql_quote('image'),
-            sql_in('id_document', $documents['document'])
-        ),
-        '',
-        '',
-        '0,1'
-    );
+	$fichier = sql_fetsel(
+		'id_document,fichier',
+		'spip_documents',
+		array(
+			'media='.sql_quote('image'),
+			sql_in('id_document', $documents['document'])
+		),
+		'',
+		'',
+		'0,1'
+	);
 
-    // On utilise ce fichier de la médiathèque comme logo
-    include_spip('uploadhtml5_fonctions');
-    uploadhtml5_uploader_logo($objet, $id_objet, _DIR_IMG.$fichier['fichier']);
+	// On utilise ce fichier de la médiathèque comme logo
+	include_spip('uploadhtml5_fonctions');
+	uploadhtml5_uploader_logo($objet, $id_objet, _DIR_IMG.$fichier['fichier']);
 
-    // Supprime le fichier logo de la session et de la médiathèque
-    if ($supprimer) {
-        saisie_supprimer_document_session($fichier['id_document']);
-        $supprimer_document = charger_fonction('supprimer_document_tmp', 'action');
-        $supprimer_document($fichier['id_document']);
-    }
+	// Supprime le fichier logo de la session et de la médiathèque
+	if ($supprimer) {
+		saisie_supprimer_document_session($fichier['id_document']);
+		$supprimer_document = charger_fonction('supprimer_document_tmp', 'action');
+		$supprimer_document($fichier['id_document']);
+	}
 }
 
 /**
@@ -168,7 +168,7 @@ function saisie_upload_traiter_logo($objet, $id_objet, $supprimer = true) {
  * @access public
  */
 function saisie_upload_charger($id_document) {
-    $uploads = session_get('upload') ?: array();
-    $uploads[] = $id_document;
-    session_set('upload', $uploads);
+	$uploads = session_get('upload') ?: array();
+	$uploads[] = $id_document;
+	session_set('upload', $uploads);
 }
