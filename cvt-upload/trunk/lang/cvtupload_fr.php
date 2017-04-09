9d6095520c34997987bf7d39e363316499e4cab9
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-04-09 03:32:48 +0000

    [Salvatore] [source:_plugins_/cvt-upload/trunk/lang/ cvtupload] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@103841 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- cvt-upload/trunk/lang/cvtupload_fr.php
+++ cvt-upload/trunk/lang/cvtupload_fr.php
@@ -12,6 +12,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// R
 	'remplacer_fichier' => 'Remplacer ce fichier',
+
 	// S
 	'saisie_fichiers_explication' => 'Envoyer un ou plusieurs fichiers en s’assurant de ne pas avoir à les renvoyer en cas de non validation des autres champs du formulaire.',
 	'saisie_fichiers_label_fichiers_individuels_label' => 'Utiliser un label pour le champ de chaque fichier individuel ?',
