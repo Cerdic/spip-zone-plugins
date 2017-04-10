12afdda71d702f8aa5893e1df984e5aea0381c68
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-04-10 15:28:05 +0000

    Eviter de trop morceler le code : le plugin panier est naturellement predispose pour fonctionner avec les commandes du plugin commandes, sans plugin intermediaire Aucune rupture de compatibilite : * Cela n'empeche pas de l'utiliser avec autre chose si c'etait le cas avant * Si le plugin panier2commande reste actif, pas de collision, tout continue de fonctionner, mais il n'est plus utile, donc
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@103860 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- paniers/trunk/paniers_options.php
+++ paniers/trunk/paniers_options.php
@@ -15,6 +15,20 @@ if (isset($_COOKIE[$GLOBALS['cookie_prefix'].'_panier'])
 
 }
 
+// si on a une transformation panier=>commande en attente et qu'on est connecte,
+// creer la commande
+if (  isset($_COOKIE['spip_pwl']) AND $_COOKIE['spip_pwl']
+	AND isset($GLOBALS['visiteur_session']['id_auteur']) AND $GLOBALS['visiteur_session']['id_auteur']){
+
+	include_spip('inc/filtres');
+	$contexte = decoder_contexte_ajax($_COOKIE['spip_pwl'],'spip_pwl');
+	$arg = reset($contexte);
+	$commandes_paniers = charger_fonction("commandes_paniers","action");
+	$commandes_paniers($arg);
+	include_spip('inc/cookie');
+	spip_setcookie("spip_pwl",$_COOKIE['spip_pwl'] = '',0);
+}
+
 
 /**
  * Calculer rapidement le nombre de produits dans un panier
@@ -24,4 +38,35 @@ if (isset($_COOKIE[$GLOBALS['cookie_prefix'].'_panier'])
 function paniers_nombre_produits($id_panier){
 	$quantite = intval(sql_getfetsel("SUM(quantite)","spip_paniers_liens","id_panier=".intval($id_panier)));
 	return $quantite;
+}
+
+// Eviter une collistion de fonction si le plugin deprecie panier2commande est encore actif
+if (!defined('_DIR_PLUGIN_PANIER2COMMANDE')){
+	/**
+	 * Creer la commande si connecte ou renvoyer vers la page de login
+	 * @param null $arg
+	 */
+	function action_commandes_paniers_if_loged_dist($arg = null){
+
+		// Si $arg n'est pas donne directement, le recuperer via _POST ou _GET
+		if (is_null($arg)){
+			$securiser_action = charger_fonction('securiser_action', 'inc');
+			$arg = $securiser_action();
+		}
+
+		// si on est identifie, on peut passer a la commande directement
+		if (isset($GLOBALS['visiteur_session']['id_auteur']) AND $GLOBALS['visiteur_session']['id_auteur']){
+			$commandes_paniers = charger_fonction("commandes_paniers", "action");
+			$commandes_paniers($arg);
+		}
+		// sinon on note le arg pour creer la commande des qu'on est idenfie
+		// et on redirige vers la page d'idendification
+		else {
+			include_spip('inc/cookie');
+			include_spip('inc/filtres');
+			spip_setcookie("spip_pwl", encoder_contexte_ajax(array($arg), 'spip_pwl'));
+
+			$GLOBALS['redirect'] = parametre_url(generer_url_public('qui'), 'url', _request('redirect'));
+		}
+	}
 }
--- paniers/trunk/paniers_pipelines.php
+++ paniers/trunk/paniers_pipelines.php
@@ -48,4 +48,35 @@ function paniers_insert_head_css($flux){
 	return $flux;
 }
 
-?>
+
+/**
+ * Sur une transformation de commande en attente
+ * on supprime le panier source si besoin
+ * @param $flux
+ * @return @flux
+ */
+function paniers_post_edition($flux){
+
+	// Si on est dans le cas d'une commande qui passe de attente/en cours=>paye/livre/erreur
+	if ($flux['args']['table']=='spip_commandes'
+	  AND $id_commande=$flux['args']['id_objet']
+	  AND $flux['args']['action']=='instituer'
+	  AND isset($flux['data']['statut'])
+    AND !in_array($flux['data']['statut'],array('attente','encours'))
+	  AND in_array($flux['args']['statut_ancien'],array('attente','encours'))
+	  AND $commande = sql_fetsel('id_commande, source', 'spip_commandes', 'id_commande='.intval($id_commande))){
+
+		if (preg_match(",^panier#(\d+)$,",$commande['source'],$m)){
+			$id_panier = intval($m[1]);
+			$supprimer_panier = charger_fonction('supprimer_panier', 'action/');
+			$supprimer_panier($id_panier);
+
+			// nettoyer une eventuelle double commande du meme panier
+			sql_updateq("spip_commandes",array('source'=>''),"source=".sql_quote($commande['source']));
+			#spip_log('suppression panier '.$id_panier,'paniers');
+		}
+
+	}
+
+	return $flux;
+}
