ad165d597f2358c81840b8382657588afc7a0ff6
rporto@calepino.com <rporto@calepino.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-04-14 03:32:50 +0000

    [Salvatore] [source:_plugins_/dictionnaires/trunk/lang/ paquet-dictionnaires] Export depuis http://trad.spip.net de la langue pt_br
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@103956 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ dictionnaires/trunk/lang/paquet-dictionnaires_pt_br.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-dictionnaires?lang_cible=pt_br
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// D
+	'dictionnaires_description' => 'Permite criar dicionários agrupando definições. Os termos definidos são detetados automaticamente nos textos do site e é possível inserí-los no HTML do texto de maneira acessível.',
+	'dictionnaires_nom' => 'Dicionários',
+	'dictionnaires_slogan' => 'Criar dicionários'
+);
