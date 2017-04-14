964b06f7e758e58b8ae699183fb8586cbeb0321f
rporto@calepino.com <rporto@calepino.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-04-14 03:32:49 +0000

    [Salvatore] [source:_plugins_/dictionnaires/trunk/lang/ dictionnaire] Export depuis http://trad.spip.net de la langue pt_br
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@103955 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ dictionnaires/trunk/lang/dictionnaire_pt_br.php
@@ -0,0 +1,44 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/dictionnaire?lang_cible=pt_br
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'champ_actif_explication' => 'Um dicionário ativo terá os seus termos detectados nos conteúdos do site.',
+	'champ_actif_label' => 'Ativo',
+	'champ_actif_non' => 'Inativo',
+	'champ_actif_oui' => 'Ativo',
+	'champ_descriptif_label' => 'Descrição',
+	'champ_titre_label' => 'Título',
+	'champ_type_defaut_label' => 'Abreviação padrão',
+	'champ_type_defaut_label_case' => 'As novas definições deste dicionário serão, por padrão, abreviações. ',
+	'changer_statut' => 'Este dicionário está:',
+	'configurer_champ_remplacer_premier_abbr_label' => 'Substituir unicamente a primeira ocorrência de uma abreviação.',
+	'configurer_champ_remplacer_premier_defaut_label' => 'Substituir unicamente a primeira ocorrência de uma definição.',
+	'configurer_titre' => 'Configurar os dicionários',
+	'confirmer_supprimer_dictionnaire' => 'Você deseja realmente excluir este dicionário e as suas definições?',
+
+	// I
+	'icone_creer_dictionnaire' => 'Incluir um novo dicionário',
+	'icone_modifier_dictionnaire' => 'Alterar este dicionário',
+	'icone_supprimer_dictionnaire' => 'Excluir este dicionário',
+	'importer_acronymes_descriptif' => 'Siglas e acrônimos',
+	'importer_acronymes_titre' => 'Siglas',
+	'info_1_dictionnaire' => 'Um dicionário',
+	'info_aucun_dictionnaire' => 'Nenhum dicionário',
+	'info_nb_dictionnaires' => '@nb@ dicionários',
+	'info_nouveau_dictionnaire' => 'novo dicionário',
+	'info_numero_dictionnaire' => 'Dicionário número:',
+	'info_supprimer_dictionnaire' => 'excluir este dicionário',
+
+	// T
+	'titre_dictionnaire' => 'Dicionário',
+	'titre_dictionnaires' => 'Dicionários',
+	'titre_logo_dictionnaire' => 'Ícone deste dicionário'
+);
