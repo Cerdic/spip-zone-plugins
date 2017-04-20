50d37c8ea672c8cded42d7ff6558b7befbaa4997
associate-it@ambascience-usa.org <associate-it@ambascience-usa.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-04-20 20:56:57 +0000

    Creation du fichier de langue backoffice en anglais
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@104033 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ cvt-upload/trunk/lang/paquet-cvtupload_en.php
@@ -0,0 +1,13 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/cvt-upload/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cvtupload_description' => 'Provides tools to load files properly from a CVT form.',
+	'cvtupload_slogan' => 'Download properlu files from a CVT form'
+);
