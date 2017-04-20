c77bcbe55e3ec412c16c49db301873ff514a764b
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-04-20 11:02:19 +0000

    un peu de trim pour tester le champ url
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@104027 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- import_ics/trunk/formulaires/editer_almanach.php
+++ import_ics/trunk/formulaires/editer_almanach.php
@@ -66,10 +66,10 @@ function formulaires_editer_almanach_verifier_dist($id_almanach='new', $retour='
 	$id_article=str_replace("article|","",$le_id_article[0]);
 	set_request("id_article",$id_article);
 	$erreurs = formulaires_editer_objet_verifier('almanach',$id_almanach, array('titre', 'url', 'id_article'));
-	
+
 	$verifier = charger_fonction('verifier', 'inc/');
 	$erreur_url = $verifier(
-			_request('url'),
+			trim(_request('url')),
 			'url',
 			array(
 				'mode'=>'php_filter',
@@ -109,7 +109,7 @@ function formulaires_editer_almanach_traiter_dist($id_almanach='new', $retour=''
 
 	#on recupère l'id de l'almanach dont on aura besoin plus tard
 	$id_almanach = $chargement['id_almanach'];
-	$url = _request("url");
+	$url = trim(_request("url"));
 	$id_article = _request("id_article");
 	$decalage['ete'] = _request("decalage_ete");
 	$decalage['hiver'] = _request("decalage_hiver");
