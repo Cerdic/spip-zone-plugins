e382b3af47f3e04c7a24c03a2fcd8802002487f2
associate-it@ambascience-usa.org <associate-it@ambascience-usa.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-04-20 20:55:57 +0000

    Creation du fichier de langue en anglais
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@104032 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ cvt-upload/trunk/lang/cvtupload_en.php
@@ -0,0 +1,25 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/cvt-upload/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// E
+	'erreur_fichier_supprime' => 'You have deleted the file « @nom@ ». This is why you see this form again.',
+
+	// R
+	'remplacer_fichier' => 'Replace this file',
+
+	// S
+	'saisie_fichiers_explication' => 'Send one or several files while not having to resend them in case of non validation of other fields of the form.',
+	'saisie_fichiers_label_fichiers_individuels_label' => 'Use a label for the field of each individual file?',
+	'saisie_fichiers_nb_fichiers_explication' => 'This determines the number of available upload fields.',
+	'saisie_fichiers_nb_fichiers_label' => 'Maximum number of uploadable files',
+	'saisie_fichiers_plusieur_label' => 'File n° @nb@',
+	'saisie_fichiers_titre' => 'File(s)',
+	'saisie_fichiers_unseul_label' => 'Only one file',
+	'supprimer_fichier' => 'Delete this file'
+);
