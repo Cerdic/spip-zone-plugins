1273ee8de32e1445ababb3fea8dc993acc30d7ec
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-04-25 18:00:54 +0000

    v 1.1.6
    - Integration du menu de config dans celui de réservation événement
    - Salvatore
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@104089 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- notifications_archive/branches/v1/notifications_archive_pipelines.php
+++ notifications_archive/branches/v1/notifications_archive_pipelines.php
@@ -10,13 +10,41 @@
  */
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
-	
 
+/**a tâce cron.e crons.
+ *
+ * @pipeline taches_generales_cron
+ *
+ * @param array $flux
+ *        	Données du pipeline
+ * @return array Données du pipeline
+ */
 function notifications_archive_taches_generales_cron($taches){
 	$taches['eliminer_notifications'] = 24*3600; // tous les jours
 	return $taches;
 }
 
+/**
+ * Ajouter les configurations dans celle de réservation événements.
+ *
+ * @pipeline reservation_evenement_objets_configuration
+ *
+ * @param array $flux
+ *        	Données du pipeline
+ * @return array Données du pipeline
+ */
+function notifications_archive_reservation_evenement_objets_configuration($flux) {
+
+	$objets = array(
+		'notifications_archive' => array(
+			'label' => _T('notifications_archive:notifications_archive_titre'),
+		),
+	);
+
+	$flux['data'] = array_merge($flux['data'], $objets);
+
+	return $flux;
+}
 
 
 ?>
