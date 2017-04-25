3c7aa46d5342bcf69a135badeb11c0de95d613b0
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-04-25 17:00:15 +0000

    Ne plus utiliser d'action sécurisée pour les clics de campagne. On change un peu l'ordre des tests pour faire moins de requêtes SQL du coup. Ça devrait donc résoudre les bugs de minipres parfois.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@104084 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- campagnes/trunk/action/cliquer_campagne.php
+++ campagnes/trunk/action/cliquer_campagne.php
@@ -12,39 +12,43 @@ include_spip('base/abstract_sql');
  */
 function action_cliquer_campagne_dist($arg=null) {
 	if (is_null($arg)){
-		$securiser_action = charger_fonction('securiser_action', 'inc');
-		$arg = $securiser_action();
+		//~ $securiser_action = charger_fonction('securiser_action', 'inc');
+		//~ $arg = $securiser_action();
+		$arg = _request('arg');
 	}
 
 	// Si on a bien un id valide et que c'est pas un bot
-	if ($id_campagne = intval($arg)
+	if (
+		$id_campagne = intval($arg)
 		and !_IS_BOT
-		and $campagne = sql_fetsel('url,id_encart', 'spip_campagnes', 'id_campagne = '.$id_campagne)
-	){
+	) {
 		include_spip('inc/campagnes');
-		$infos = campagnes_recuperer_infos_visiteur();
+		$infos = campagnes_recuperer_infos_visiteur();		
 		
-		// On cherche la page d'où est venu le clic, soit explicitement soit par le referer
-		$page = _request('referer');
-		if (!$page){ $page = ltrim(str_replace($GLOBALS['url_site'], '', $_SERVER['HTTP_REFERER']), '/'); }
-		
-		// On ajoute la date et la pub
-		$infos = array_merge($infos, array('id_campagne' => $id_campagne, 'id_encart' => $campagne['id_encart'], 'page' => $page, 'date' => 'NOW()'));
-		
-		// Commencer par chercher si ce qu'on veut insérer
+		// On teste si la personne n'a pas déjà cliqué sur ce lien
 		// Attention, le champ date étant de type DATE, il faut réduire NOW à juste la DATE
-		$ok = sql_fetsel(
-			"id_campagne",
-			'spip_campagnes_clics',
-			array(
-				"id_campagne=$id_campagne",
-				"id_encart=".$campagne['id_encart'],
-				"cookie=".sql_quote($infos['cookie']),
-				"date=DATE(NOW())"
+		if (
+			!sql_fetsel(
+				'id_campagne',
+				'spip_campagnes_clics',
+				array(
+					"id_campagne=$id_campagne",
+					'cookie='.sql_quote($infos['cookie']),
+					'date=DATE(NOW())',
 				)
-			);
-				
-		if ($ok === null){
+			)
+			// Et que la campagne voulue existe bien !
+			and $campagne = sql_fetsel('url,id_encart', 'spip_campagnes', 'id_campagne = '.$id_campagne)
+		) {
+			// On cherche la page d'où est venu le clic, soit explicitement soit par le referer
+			$page = _request('referer');
+			if (!$page) {
+				$page = ltrim(str_replace($GLOBALS['url_site'], '', $_SERVER['HTTP_REFERER']), '/');
+			}
+			
+			// On ajoute la date et la pub dans les infos à garder
+			$infos = array_merge($infos, array('id_campagne' => $id_campagne, 'id_encart' => $campagne['id_encart'], 'page' => $page, 'date' => 'NOW()'));
+			
 			// On enregistre le clic
 			$ok = sql_insertq(
 				'spip_campagnes_clics',
@@ -59,10 +63,3 @@ function action_cliquer_campagne_dist($arg=null) {
 		}
 	}
 }
-
-/*
-<BOUCLE_stats_clics(RECLAMES_CLICS campagnes){id_campagne}{date>=#ENV{date_debut,}}>
-</BOUCLE_stats_clics>
-*/
-
-?>
