4e1f774e324f0d91fbb84040dd0a003f762cf901
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-04-27 14:37:12 +0000

    Faire une redirection sur la page d'édition de l'objet plus propre
    Utiliser les fonction d'édition d'objet si possible
    
    Refuser le traitement en ajax
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@104104 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- titre_de_logo/trunk/formulaires/editer_titre_logo.php
+++ titre_de_logo/trunk/formulaires/editer_titre_logo.php
@@ -15,7 +15,6 @@ include_spip('base/abstract_sql');
 include_spip('inc/config');
 
 function formulaires_editer_titre_logo_charger_dist($objet, $id_objet, $retour = '', $options = array()) {
-
 	$exec = (_request('exec')) ? _request('exec') : '';
 	$objet = objet_type($objet);
 	$table_objet = table_objet_sql($objet);
@@ -34,23 +33,8 @@ function formulaires_editer_titre_logo_charger_dist($objet, $id_objet, $retour =
 	} elseif (!in_array($table_objet, $objets_autorises)) {
 		return false;
 	}
-	// Si on a des options, on les stocke
-	if (!is_array($options)) {
-		$options = unserialize($options);
-		if (is_array($options) and array_key_exists('_pipelines', $options)) {
-			// Comme on est un formulaire dans un formulaire,
-			// on enlève le _pipeline du premier formulaire
-			unset($options['_pipelines']);
-		}
-		if (is_array($options) and array_key_exists('_options', $options)) {
-			$options = array_merge($options, $options['_options']);
-			// Comme on est un formulaire dans un formulaire,
-			// on enlève le _options du premier formulaire
-			unset($options['_options']);
-		}
-	}
 
-	$requete_sql = sql_fetsel('titre_logo,descriptif_logo', $table_objet, "$_id_objet=$id_objet");
+	$requete_sql = sql_fetsel('titre_logo,descriptif_logo', $table_objet, $_id_objet.'='.intval($id_objet));
 	if ($requete_sql) {
 		$titre_logo = $requete_sql['titre_logo'];
 		$descriptif_logo = $requete_sql['descriptif_logo'];
@@ -62,9 +46,8 @@ function formulaires_editer_titre_logo_charger_dist($objet, $id_objet, $retour =
 		'titre_logo' => $titre_logo,
 		'descriptif_logo' => $descriptif_logo,
 		'exec' => $exec,
-		//'logo_on' => $options['logo_on'],
 		'_options' => $options,
-		);
+	);
 
 	return $valeurs;
 }
@@ -82,20 +65,24 @@ function formulaires_editer_titre_logo_traiter_dist($objet, $id_objet, $retour =
 	$descriptif_logo = _request('descriptif_logo');
 	$_id_objet = id_table_objet($objet);
 
-	sql_updateq(
-		$table_objet,
-		array('titre_logo' => $titre_logo, 'descriptif_logo' => $descriptif_logo),
-		$_id_objet.'='.$id_objet
-	);
-	$update_sql = sql_fetsel(
-		'titre_logo,descriptif_logo',
-		$table_objet,
-		'titre_logo='.sql_quote($titre_logo).' AND descriptif_logo='.sql_quote($descriptif_logo)
-	);
+	if (include_spip('action/editer_'.$objet) && function_exists($objet.'_modifier')) {
+		$function = charger_fonction('modifier', $objet);
+		$erreur = $function($id_objet, array('titre_logo' => $titre_logo, 'descriptif_logo' => $descriptif_logo));
+		if (!$modif) {
+			$update_sql = true;
+		}
+	} else {
+		$update_sql = sql_updateq(
+			$table_objet,
+			array('titre_logo' => $titre_logo, 'descriptif_logo' => $descriptif_logo),
+			$_id_objet.'='.intval($id_objet)
+		);
+	}
 
 	if ($update_sql) {
+		refuser_traiter_formulaire_ajax();
 		$res['message_ok'] = _T('info_modification_enregistree');
-		$res['redirect'] = "index.php?exec=$objet&$_id_objet=$id_objet";
+		$res['redirect'] = generer_url_entite($id_objet, $objet);
 	} else {
 		$res['message_erreur'] = _T('avis_erreur');
 	}
