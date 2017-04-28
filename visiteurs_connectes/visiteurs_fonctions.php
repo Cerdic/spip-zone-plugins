15b5899e265636a42f37dc5a6af5cdbb08863765
erational@erational.org <erational@erational.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-04-28 07:06:13 +0000

    PSR, Sécu, SPIP 3.2
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@104115 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- visiteurs_connectes/visiteurs_fonctions.php
+++ visiteurs_connectes/visiteurs_fonctions.php
@@ -1,6 +1,9 @@
 <?php
 
-function visiteurs_connectes_compter()
-{
-    return count(preg_files(_DIR_TMP.'visites/', '.'));
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+function visiteurs_connectes_compter() {
+	return count(preg_files(_DIR_TMP.'visites/', '.'));
 }
