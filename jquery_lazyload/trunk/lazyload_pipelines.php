3a4328606fc2d8fcbcee2f9b1f48b48a5a442b3d
homere1974@yahoo.fr <homere1974@yahoo.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-04-28 13:43:37 +0000

    Rien que pour son logo, ce plugin méritait de marcher. Repassé en 'test' au cas où.
    - maj du script jquery
    - surcharge les modèles d'image
    - actif sur l'espace privé
    - fallback qui va bien
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@104116 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- jquery_lazyload/trunk/lazyload_pipelines.php
+++ jquery_lazyload/trunk/lazyload_pipelines.php
@@ -5,7 +5,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 function lazyload_insert_head($flux){
 	if (function_exists('lire_config')) {
 		
-		$selecteur = lire_config("lazyload/selecteur","#contenu img");
+		$selecteur = lire_config("lazyload/selecteur","img");
 		$distance = lire_config("lazyload/distance","0");
 		$event = lire_config("lazyload/event");
 		if ($event != "")
@@ -13,11 +13,11 @@ function lazyload_insert_head($flux){
 		$effect = lire_config("lazyload/effect");
 		if ($effect != "")
 			$effect = 'effect : "'.$effect.'",';
-		
+		$flux .= '<style>.lazy { display: none }</style>';
 		$flux .= '<script type="text/javascript" src="'._DIR_PLUGIN_LAZYLOAD.'javascript/jquery.lazyload.js"></script>';
 		$flux .= '<script type="text/javascript">
 					$(function(){
-						$("'.$selecteur.'").lazyload({
+						$("'.$selecteur.'.lazy").show().lazyload({
 							threshold : '.$distance.',
 							placeholder : "'._DIR_PLUGIN_LAZYLOAD.'images/grey.gif",
 							'.$event.'
@@ -26,6 +26,12 @@ function lazyload_insert_head($flux){
 					});
 				</script>';
 	}
+	return $flux;
+}
+function lazyload_insert_head_prive($flux){
+	$flux .= '<style>.lazy { display: none; }</style>';
+	$flux .= '<script type="text/javascript" src="'._DIR_PLUGIN_LAZYLOAD.'javascript/jquery.lazyload.js"></script>';
+	$flux .= '<script type="text/javascript">$(function(){ $("img.lazy").show().lazyload(); });</script>';
 	return $flux;
 }
 
