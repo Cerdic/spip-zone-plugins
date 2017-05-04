e4ab3a601b5d426c0b1686fdfd175ced83e7ee51
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-05-04 14:58:56 +0000

    lorsqu'on envoie une image, la vue affiche la miniature de l'image
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@104163 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- formidable/trunk/formidable_fonctions.php
+++ formidable/trunk/formidable_fonctions.php
@@ -188,7 +188,11 @@ function formidable_ajouter_action_recuperer_fichier($saisie_a_modifier, $nom_sa
 					$valeur['nom']
 				);
 				$saisie_a_modifier[$i]['url'] = $url;
-				$saisie_a_modifier[$i]['vignette'] = $vignette_par_defaut($valeur['extension'], false);
+				if (in_array($valeur['extension'],array('png','jpg','gif'))) {
+					$saisie_a_modifier[$i]['vignette'] = _DIR_FICHIERS_FORMIDABLE."formulaire_$id_formulaire/reponse_$id_formulaires_reponse/$nom_saisie/".$valeur['nom'];
+				}	else {
+					$saisie_a_modifier[$i]['vignette'] = $vignette_par_defaut($valeur['extension'], false);
+				}
 			}
 		}
 	}
