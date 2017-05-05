91c4778adbc6629cb71aeaf076e6c4c71a692af0
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-05-05 09:04:25 +0000

    Fix plusieurs playliste dans la meme page : le js d'init echouait dans l'init des 2e et suivantes playlistes du fait d'une erreur de conception On refactore en mettant en commun la fonction d'init qui est rappelee apres chaque getScript d'un plugin et tente a chaque fois d'initialiser tous les players qui le peuvent
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@104184 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- player/trunk/javascript/mejs-init.js
+++ player/trunk/javascript/mejs-init.js
@@ -2,7 +2,7 @@ var mejsloader;
 (function(){
 	var L=mejsloader;
 	if (typeof L == "undefined")
-		mejsloader = L = {gs:null,plug:{},css:{},init:null,c:0,cssload:null};
+		mejsloader = L = {gs:null,plug:{},css:{},init:null,c:0,cssload:null,to_run:{}};
 	if (!L.init){
 		L.cssload = function (f){
 			if (typeof L.css[f]=="undefined"){
@@ -12,6 +12,66 @@ var mejsloader;
 				stylesheet.rel = 'stylesheet';
 				stylesheet.type = 'text/css';
 				document.getElementsByTagName('head')[0].appendChild(stylesheet);
+			}
+		}
+		L.runplayers = function(infos){
+			if (infos) {
+				L.to_run[infos.id] = infos;
+				L.to_run[infos.id].cssLoaded = false;
+				L.to_run[infos.id].toRun = true;
+				//console.log(L.to_run);
+			}
+			for (var id in L.to_run){
+				var opt = L.to_run[id];
+				var run = L.to_run[id].toRun;
+				//console.log(id);
+				//console.log(opt);
+				if (!L.to_run[id].cssLoaded) {
+					for (var c in opt.css){
+						L.cssload(opt.css[c]);
+					}
+					L.to_run[id].cssLoaded = true;
+				}
+				for (var p in opt.plugins){
+					//console.log(p);
+					//console.log(L.plug[p]);
+					// load this plugin
+					if (typeof L.plug[p]=="undefined"){
+						//console.log("Load Plugin "+p);
+						run = false;
+						L.plug[p] = false;
+						jQuery.getScript(opt.plugins[p], function (){
+							L.plug[p] = true;
+							L.runplayers(false);
+						});
+					}
+					// this plugin is loading
+					else if (L.plug[p]==false){
+						//console.log("Plugin "+p+" loading...");
+						run = false; // will be run on next call of runplayer
+					}
+					else {
+						//console.log("Plugin "+p+" loaded");
+					}
+				}
+				if (run){
+					mejs.MediaFeatures.isChromium = false; // bugfix Chromium : can now play mpx sound files
+					new MediaElementPlayer('#'+id, jQuery.extend(opt.options, {
+						"success": function (media){
+							function togglePlayingState(){
+								jQuery(media).closest('.mejs-inner').removeClass(media.paused ? 'playing' : 'paused').addClass(media.paused ? 'paused' : 'playing');
+							}
+
+							togglePlayingState();
+							media.addEventListener('play', togglePlayingState, false);
+							media.addEventListener('playing', togglePlayingState, false);
+							media.addEventListener('pause', togglePlayingState, false);
+							media.addEventListener('paused', togglePlayingState, false);
+							if (jQuery('#'+id).attr('autoplay')) media.play();
+						}
+					}));
+					L.to_run[id].toRun = false;
+				}
 			}
 		}
 		L.init = function (){
@@ -25,58 +85,12 @@ var mejsloader;
 						id = "mejs-"+(me.attr('data-id'))+"-"+(L.c++);
 						me.attr('id', id);
 					}
-					var opt = {options: {}, plugins: {}, css: []}, i, v;
+					var opt = {id:id, options: {}, plugins: {}, css: []}, i, v;
 					for (i in opt){
 						if (v = me.attr('data-mejs'+i)) opt[i] = jQuery.parseJSON(v);
 					}
-					function runthisplayer(){
-						var run = true;
-						//console.log(css);
-						for (var c in opt.css){
-							L.cssload(opt.css[c]);
-						}
-						for (var p in opt.plugins){
-							//console.log(p);
-							//console.log(L.plug[p]);
-							// load this plugin
-							if (typeof L.plug[p]=="undefined"){
-								//console.log("Load Plugin "+p);
-								run = false;
-								L.plug[p] = false;
-								jQuery.getScript(opt.plugins[p], function (){
-									L.plug[p] = true;
-									runthisplayer();
-								});
-							}
-							// this plugin is loading
-							else if (L.plug[p]==false){
-								//console.log("Plugin "+p+" loading...");
-								run = false;
-							}
-							else {
-								//console.log("Plugin "+p+" loaded");
-							}
-						}
-						if (run){
-							mejs.MediaFeatures.isChromium = false; // bugfix Chromium : can now play mpx sound files
-							new MediaElementPlayer('#'+id, jQuery.extend(opt.options, {
-								"success": function (media){
-									function togglePlayingState(){
-										jQuery(media).closest('.mejs-inner').removeClass(media.paused ? 'playing' : 'paused').addClass(media.paused ? 'paused' : 'playing');
-									}
 
-									togglePlayingState();
-									media.addEventListener('play', togglePlayingState, false);
-									media.addEventListener('playing', togglePlayingState, false);
-									media.addEventListener('pause', togglePlayingState, false);
-									media.addEventListener('paused', togglePlayingState, false);
-									if (me.attr('autoplay')) media.play();
-								}
-							}));
-						}
-					}
-
-					runthisplayer();
+					L.runplayers(opt);
 				})
 			})(jQuery);
 		}
