8f4714f6ebb3f68034c5d0f307bc667b5aff63f0
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-05-07 14:57:26 +0000

    Un warning PHP en moins
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@104286 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- lazysizes/trunk/lazysizes_pipelines.php
+++ lazysizes/trunk/lazysizes_pipelines.php
@@ -27,10 +27,12 @@ function lazysizes_insert_head_public($flux) {
 	$active_addons = lire_config('lazysizes/addons');
 	$ls_addons = lazysizes_addons();
 	
-	foreach($active_addons as $addon => $state){
-		if(array_key_exists($addon, $ls_addons)){
-			$file = timestamp(find_in_path('javascript/addons/'.$addon.'/'.$ls_addons[$addon].'.js'));
-			$flux .= "<script type='text/javascript' src='$file' ></script>\n"; 
+	if (is_array($active_addons)) {
+		foreach($active_addons as $addon => $state){
+			if(array_key_exists($addon, $ls_addons)){
+				$file = timestamp(find_in_path('javascript/addons/'.$addon.'/'.$ls_addons[$addon].'.js'));
+				$flux .= "<script type='text/javascript' src='$file' ></script>\n"; 
+			}
 		}
 	}
 			
