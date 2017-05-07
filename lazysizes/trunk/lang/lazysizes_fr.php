975287b97bec05a3f096458e11f166a866e1db97
arnaud.berard@mister-graphx.com <arnaud.berard@mister-graphx.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-05-07 15:54:04 +0000

    chaines de langue de l'option css
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@104298 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- lazysizes/trunk/lang/lazysizes_fr.php
+++ lazysizes/trunk/lang/lazysizes_fr.php
@@ -7,6 +7,9 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'cfg_titre_page'	=> 'Configurer LazySizes',
+	'cfg_explication_css'	=> 'Inssérer les styles dans l\'entête des pages',
+	'cfg_label_css'	=> 'Insertion des css',
+
 	'lazysizes_titre'	=> 'LazySizes',
 
 
