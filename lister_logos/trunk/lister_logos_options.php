9c66765302d8759224fa6363108fd4c1868b848f
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-05-09 08:31:17 +0000

    - Compatibilité SPIP 3.2 ; - Up de z.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@104405 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- lister_logos/trunk/lister_logos_options.php
+++ lister_logos/trunk/lister_logos_options.php
@@ -3,7 +3,7 @@
  * Options du plugin Lister les logosau chargement
  *
  * @plugin     Lister les logos
- * @copyright  2014
+ * @copyright  2014-2017
  * @author     Teddy Payet
  * @licence    GNU/GPL
  * @package    SPIP\Lister_logos\Options
