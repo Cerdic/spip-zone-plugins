2b2730a035cd58a448ac1a29ecd1607762e4415b
rporto@calepino.com <rporto@calepino.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-05-10 03:32:49 +0000

    [Salvatore] [source:_plugins_/porte_plume_partout/trunk/lang/ paquet-ppp] Export depuis http://trad.spip.net de la langue pt_br
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@104435 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ porte_plume_partout/trunk/lang/paquet-ppp_pt_br.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-ppp?lang_cible=pt_br
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// P
+	'ppp_description' => 'Porte Plume Partout inclui a barra de edição nos campos DESCRIPTIF, CHAPO e PS',
+	'ppp_nom' => 'Porte Plume Partout',
+	'ppp_slogan' => 'Exibir o Porte Plume na maioria dos campos de conteúdo'
+);
