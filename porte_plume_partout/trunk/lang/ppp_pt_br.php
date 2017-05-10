53fc5da03c94b15e1dfc888a03899347d39eec52
rporto@calepino.com <rporto@calepino.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-05-10 03:32:49 +0000

    [Salvatore] [source:_plugins_/porte_plume_partout/trunk/lang/ ppp] Export depuis http://trad.spip.net de la langue pt_br
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@104434 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ porte_plume_partout/trunk/lang/ppp_pt_br.php
@@ -0,0 +1,32 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/ppp?lang_cible=pt_br
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'bouton_mettre_a_jour' => 'Atualizar',
+
+	// C
+	'cfg_attention' => 'Atenção',
+	'cfg_description' => 'em função dos seus templates, só ative o Porte-Plume nos campos em que o uso dos atalhos não gerará erros de XHTML',
+	'cfg_titre' => 'Porte Plume Partout',
+
+	// E
+	'explication_personnalisation' => 'Indique o alvo dos elementos que usarão a barra tipográfica (expressão CSS ou extenção jQuery)',
+
+	// L
+	'label_hauteur_champ' => 'Meia altura da tela',
+	'label_personnalisation' => 'Seletor personalizado',
+	'legend_barre_typo' => 'Ativar a barra tipográfica em:',
+	'legend_hauteur_champ' => 'Altura do campo texto para as matérias',
+	'legend_personnalisation' => 'Personalização',
+
+	// S
+	'supprimer' => 'Retornar aos valores padrão'
+);
