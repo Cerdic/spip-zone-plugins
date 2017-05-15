8a37855c85d81f91640f1f7988d660e475bb35c2
tcharlss@bravecassine.com <tcharlss@bravecassine.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-05-15 15:41:35 +0000

    version 1.0.3 : si on ne trouve pas d'image, on va chercher une vignette
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@104528 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- logo_auto/branches/logo_auto_php/logo_auto_pipelines.php
+++ logo_auto/branches/logo_auto_php/logo_auto_pipelines.php
@@ -27,6 +27,30 @@ function logo_auto_quete_logo_objet($flux) {
 				'timestamp' => @filemtime($chemin_complet),
 			);
 		}
+		// Si on ne trouve pas d'image, on cherche une vignette de document
+		elseif ($vignette = sql_fetsel(
+			'v.fichier',
+			array(
+				'spip_documents_liens as l',
+				'spip_documents as v
+					inner join spip_documents as d
+					on d.id_vignette = v.id_document
+					and d.id_document = l.id_document',
+			),
+			array(
+				'l.objet = '.sql_quote($flux['args']['objet']),
+				'l.id_objet = '.intval($flux['args']['id_objet']),
+			),
+			'', //group
+			'0+d.titre, d.id_document'
+		)) {
+			$chemin_complet = _DIR_IMG . $vignette['fichier'];
+			
+			$flux['data'] = array(
+				'chemin' => $chemin_complet,
+				'timestamp' => @filemtime($chemin_complet),
+			);
+		}
 	}
 	
 	return $flux;
