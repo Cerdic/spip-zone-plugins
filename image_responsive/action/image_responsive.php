cef7d8725493194647614c8aa267b24ef1cb86e8
prigent.yohann@gmail.com <prigent.yohann@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-05-18 18:17:58 +0000

    Un petit ajout pour supporter le plugin Mutualisation
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@104576 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- image_responsive/action/image_responsive.php
+++ image_responsive/action/image_responsive.php
@@ -7,5 +7,9 @@ function action_image_responsive() {
 	$dpr = _request("dpr");
 	$xsendfile = _request("xsendfile");
 
+	// Gérer le plugin mutualisation
+	if (defined('_DIR_SITE'))
+		$img = _DIR_SITE.$img;
+
 	retour_image_responsive($img, $taille, $dpr, $xsendfile, "http");
 }
