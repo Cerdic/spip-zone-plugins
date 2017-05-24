dbfdb0be1170a302f11aa1a8cb9b5fb00f838c13
hanjo.spip@gmail.com <hanjo.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-05-24 03:33:26 +0000

    [Salvatore] [source:_plugins_/multilingue/trunk/lang/ paquet-multilingue] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@104625 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- multilingue/trunk/lang/paquet-multilingue_nl.php
+++ multilingue/trunk/lang/paquet-multilingue_nl.php
@@ -14,18 +14,18 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	
 	Deze plugin forceert de installatie van meerdere plugins
 	
-	- [Traduction entre rubriques->http://plugins.spip.net/tradrub.html]
-	- [Langue préférée par l’internaute ->http://contrib.spip.net/Plugin-Langue-preferee-par-l-internaute]
-	- [Traductions de rubriques autrement->http://contrib.spip.net/Traductions-de-rubriques-autrement-3]
-	- [Traductions d’articles autremen->http://contrib.spip.net/Traductions-d-articles-autrement-3]
+	- [Traduction entre rubriques->https://plugins.spip.net/tradrub.html]
+	- [Langue préférée par l’internaute ->https://contrib.spip.net/Plugin-Langue-preferee-par-l-internaute]
+	- [Traductions de rubriques autrement->https://contrib.spip.net/Traductions-de-rubriques-autrement-3]
+	- [Traductions d’articles autremen->https://contrib.spip.net/Traductions-d-articles-autrement-3]
 	
-	In Spip 2 of 2.1 wordt aanbevolen om eerst de plugin [Step->http://contrib.spip.net/STEP-Systeme-de-Telechargements-de-Plugins] te installeren voordat deze plugin wordt geactiveerd. Deze zal oo kde nodige plugins installeren.
+	In Spip 2 of 2.1 wordt aanbevolen om eerst de plugin [Step->https://contrib.spip.net/STEP-Systeme-de-Telechargements-de-Plugins] te installeren voordat deze plugin wordt geactiveerd. Deze zal oo kde nodige plugins installeren.
 	
 	De plugin past enkele skeletten aan (van rubrieken en artikelen) alsmede enkele basisonderdelen.
 	
 	Zodra de nodige plugins zijn geactiveerd, leg je de talen van de site vast in de configuratie van de site (meertaligheid) en je site is meertalig
 	
-	', # MODIF
+	',
 	'multilingue_nom' => 'Site Multilingue Facile',
 	'multilingue_slogan' => 'Maak eenvoudig een meertalige site'
 );
