869239b5cd35dd47e2082d9df39d3d83ed41d810
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-06-04 13:44:16 +0000

    Un plugin pour renvoyer le visiteur sur la page en cours dans la langue fixée par son navigateur. Le plugin fixe le cookie de langue automatiquement si ce dernier n'est pas déjà positionné.
    Merci à Marcimat et Rastapopoulos sur IRC pour leurs réponses et la base du code.
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@104760 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ autolang/trunk/autolang_options.php
@@ -0,0 +1,21 @@
+<?php
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+function autolang_aiguiller(){
+	$langue = (isset($_COOKIE['spip_lang']) ? $_COOKIE['spip_lang'] : '');
+	if (!$langue) {
+		include_spip('inc/lang');
+		$langues = explode(',', $GLOBALS['meta']['langues_multilingue']);
+		$langue = utiliser_langue_visiteur();
+		if (!in_array($langue, $langues)) {
+			$langue = $GLOBALS['meta']['langue_site'];
+		}
+		include_spip('inc/cookie');
+		spip_setcookie('spip_lang', $langue);
+		include_spip('inc/headers');
+		redirige_par_entete(parametre_url(self(),'var_lang',$langue));
+	}
+}
+
+autolang_aiguiller();
