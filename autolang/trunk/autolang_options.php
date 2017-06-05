2e18b7e066372ad3a1ffcd9b5b732215818f6c14
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-06-05 09:20:55 +0000

    Il fallait vraiment ce test : il permet de ne pas prendre la langue du visiteur si elle n'est pas utilisée dans le site. (Marcimat 1, Rasta 0)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@104769 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- autolang/trunk/autolang_options.php
+++ autolang/trunk/autolang_options.php
@@ -7,6 +7,10 @@ function autolang_aiguiller(){
 	if (!$langue) {
 		include_spip('inc/lang');
 		$langue = utiliser_langue_visiteur();
+		$langues = explode(',', $GLOBALS['meta']['langues_multilingue']);
+		if (!in_array($langue, $langues)) {
+			$langue = $GLOBALS['meta']['langue_site'];
+		}
 		include_spip('inc/cookie');
 		spip_setcookie('spip_lang', $langue);
 		include_spip('inc/headers');
