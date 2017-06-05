1925da00b0bcff0c96e91ef7b7aa2499c5d891f5
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-06-05 07:57:55 +0000

    1.4.1 compatibilité avec plugin rang - mode compact permet maintenant le drag and drop - salvatore
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@104766 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- traduction_articles_autrement/branches/1.0/lang/paquet-taa_es.php
+++ traduction_articles_autrement/branches/1.0/lang/paquet-taa_es.php
@@ -10,11 +10,11 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// T
-	'taa_description' => 'Modificación del interface privado de spip a fin de dar al utilizador otro enfoque    de la traducción de los artículos pero manteniendo el sistema de traducción original de spip. Para poder utilizar la apariencia compacta de las listas de  artículos (opción disponible via cfg), hay que instalar  el plugin [afficher_objets->https://files.spip.net/spip-zone/afficher_objets.zip].
-  Las listas compactas funcionan ahora igualmente en un sitio con multilingüismo por sección. Hay que instalar para ello el plugin [Traduction entre rubrique->https://files.spip.net/spip-zone/tradrub.zip]
+	'taa_description' => 'Modificación del interface privado de spip a fin de dar al utilizador otro enfoque    de la traducción de los artículos pero manteniendo el sistema de traducción original de spip. Para poder utilizar la apariencia compacta de las listas de  artículos (opción disponible via cfg), hay que instalar  el plugin [afficher_objets->http://files.spip.org/spip-zone/afficher_objets.zip].
+  Las listas compactas funcionan ahora igualmente en un sitio con multilingüismo por sección. Hay que instalar para ello el plugin [Traduction entre rubrique->http://files.spip.org/spip-zone/tradrub.zip]
 
-Y para poder aprovechar de un sistema de traducción en el sentido del presente plugin, instale además el plugin  [Traductions rubriques autrement->https://files.spip.net/spip-zone/traduction_rubriques_autrement.zip]. Los botones de traducción aparecerán  en lo alto de la sección con la posibilidad de crear las traducciones que falten cliqueando  en la lengüeta gris. El plugin intentará buscar la sección correspondiente en el sector de la lengua, si la sección parent (padre) está bien traducida.
-',
+Y para poder aprovechar de un sistema de traducción en el sentido del presente plugin, instale además el plugin  [Traductions rubriques autrement->http://files.spip.org/spip-zone/traduction_rubriques_autrement.zip]. Los botones de traducción aparecerán  en lo alto de la sección con la posibilidad de crear las traducciones que falten cliqueando  en la lengüeta gris. El plugin intentará buscar la sección correspondiente en el sector de la lengua, si la sección parent (padre) está bien traducida.
+', # MODIF
 	'taa_nom' => 'Traducción de articulos de otra manera',
 	'taa_slogan' => 'Hacer que la traducción de artículos sea fácil.'
 );
--- traduction_articles_autrement/branches/1.0/lang/paquet-taa_pt_br.php
+++ traduction_articles_autrement/branches/1.0/lang/paquet-taa_pt_br.php
@@ -10,11 +10,11 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// T
-	'taa_description' => 'Alteração da interface da área restrita do SPIP para dar ao usuário uma outra abordagem para a tradução das matérias, mantendo o sistema de tradução original do SPIP. Para poder usar a exibição compacta das listas de matérias (opção disponível via CFG), é necessário instalar o plugin [afficher_objets->https://files.spip.net/spip-zone/afficher_objets.zip].
+	'taa_description' => 'Alteração da interface da área restrita do SPIP para dar ao usuário uma outra abordagem para a tradução das matérias, mantendo o sistema de tradução original do SPIP. Para poder usar a exibição compacta das listas de matérias (opção disponível via CFG), é necessário instalar o plugin [afficher_objets->http://files.spip.org/spip-zone/afficher_objets.zip].
 
-As listas compactas passam a funcionar igualmente em sites com multilinguismo por seção. Para isso, é necessário instalar o plugin [Tradução entre seções->https://files.spip.net/spip-zone/tradrub.zip].
+As listas compactas passam a funcionar igualmente em sites com multilinguismo por seção. Para isso, é necessário instalar o plugin [Tradução entre seções->http://files.spip.org/spip-zone/tradrub.zip].
 
-E para poder se beneficiar de um sistema de tradução semelhante a esteduções  plugin, instale também o plugin [Tradução de seções  alternativa->https://files.spip.net/spip-zone/traduction_rubriques_autrement.zip]. Os botões de tradução serão exibidos no alto da seção com a possibilidade de criar as traduções em falta clicando na aba cinza. O plugin tentará procurar a seção correspondente no setor do idioma, se a seção precedente estiver traduzida.',
+E para poder se beneficiar de um sistema de tradução semelhante a esteduções  plugin, instale também o plugin [Tradução de seções  alternativa->http://files.spip.org/spip-zone/traduction_rubriques_autrement.zip]. Os botões de tradução serão exibidos no alto da seção com a possibilidade de criar as traduções em falta clicando na aba cinza. O plugin tentará procurar a seção correspondente no setor do idioma, se a seção precedente estiver traduzida.', # MODIF
 	'taa_nom' => 'Tradução de matérias alternativa',
 	'taa_slogan' => 'Torna a tradução das matérias mais fácil.'
 );
