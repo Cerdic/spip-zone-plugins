b9ca0a787a1f555840d730fb679c01f0944d6188
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-06-05 10:30:33 +0000

    Finalement, recopie à une ligne près de 2 fontions de inc/lang.php juste parce que l'une d'elle prend toutes les langues de SPIP, et non celle déclarées en utilisation. cf : $liste_langues = ',' . @$GLOBALS['meta']['langues_proposees'] 		. ',' . @$GLOBALS['meta']['langues_multilingue'] . ',';
    Comme ça, si on a un visiteur qui a dans sont navigateur, dans cet ordre : ES, EN, FR (langues acceptées)
    Et que le site n'est en FR, EN
    Alors, il voir le site dans la première langue disponible dans le site qu'il accepte (EN ici).
    
    Sans ça, le site était affiché en ES :(
    
    
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@104770 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- autolang/trunk/autolang_options.php
+++ autolang/trunk/autolang_options.php
@@ -2,15 +2,61 @@
 
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
+function autolang_changer_langue($lang) {
+
+	$liste_langues = ',' . @$GLOBALS['meta']['langues_multilingue'] . ',';
+
+	// Si la langue demandee n'existe pas, on essaie d'autres variantes
+	// Exemple : 'pt-br' => 'pt_br' => 'pt'
+	$lang = str_replace('-', '_', trim($lang));
+	if (!$lang) {
+		return false;
+	}
+
+	if (strpos($liste_langues, ",$lang,") !== false
+		or ($lang = preg_replace(',_.*,', '', $lang)
+			and strpos($liste_langues, ",$lang,") !== false)
+	) {
+
+		return $lang;
+	} else {
+		return false;
+	}
+}
+
+function autolang_utiliser_langue_visiteur() {
+
+	$l = (!test_espace_prive() ? 'spip_lang' : 'spip_lang_ecrire');
+	if (isset($_COOKIE[$l])) {
+		if (autolang_changer_langue($l = $_COOKIE[$l])) {
+			return $l;
+		}
+	}
+
+	if (isset($GLOBALS['visiteur_session']['lang'])) {
+		if (autolang_changer_langue($l = $GLOBALS['visiteur_session']['lang'])) {
+			return $l;
+		}
+	}
+
+	if (isset($_SERVER['HTTP_ACCEPT_LANGUAGE'])) {
+		foreach (explode(',', $_SERVER['HTTP_ACCEPT_LANGUAGE']) as $s) {
+			if (preg_match('#^([a-z]{2,3})(-[a-z]{2,3})?(;q=[0-9.]+)?$#i', trim($s), $r)) {
+				if (autolang_changer_langue($l = strtolower($r[1]))) {
+					return $l;
+				}
+			}
+		}
+	}
+
+	return utiliser_langue_site();
+}
+
 function autolang_aiguiller(){
 	$langue = (isset($_COOKIE['spip_lang']) ? $_COOKIE['spip_lang'] : '');
 	if (!$langue) {
 		include_spip('inc/lang');
-		$langue = utiliser_langue_visiteur();
-		$langues = explode(',', $GLOBALS['meta']['langues_multilingue']);
-		if (!in_array($langue, $langues)) {
-			$langue = $GLOBALS['meta']['langue_site'];
-		}
+		$langue = autolang_utiliser_langue_visiteur();
 		include_spip('inc/cookie');
 		spip_setcookie('spip_lang', $langue);
 		include_spip('inc/headers');
