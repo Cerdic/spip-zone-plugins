c73e07717bf373a28e84a763d106811c1083531f
toutati@free.fr <toutati@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-06-14 14:55:18 +0000

    réorganisation des fonctions, but pas de trou de sécu y voir clair passer en restreint facilement rester compatible avec les anciens usages
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@104863 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- article_pdf/article_pdf_fonctions.php
+++ article_pdf/article_pdf_fonctions.php
@@ -48,3 +48,86 @@ function pdf_first_clean($texte){
 function filtre_supprimer_param_logo($texte){
 	return preg_replace('`\?[0-9]*$`','',$texte);
 }
+
+/**
+ * [(#TITRE|pdf_nommer{article, #ID_ARTICLE})]
+ *
+ * Fonction reprise de SPIP (plugin dist urls_etendues)
+ * pour le filtre url_nettoyer
+ *
+**/
+
+function pdf_nommer($titre_parent, $objet, $id_parent){
+//au choix, selon le cas (voir les avantages et les inconvéniants de chaque selon le site):
+//	$path_pdf = sous_repertoire(_DIR_CACHE, "article_PDF"); // stockage dans le cache SPIP
+	$path_pdf = sous_repertoire(_DIR_IMG, "article_PDF"); //stockage sous le dossier IMG
+	
+	include_spip('action/editer_url');
+	$titre = url_nettoyer($titre_parent,60);
+	$lettre = substr($objet, 0, 1);
+	$nom_pdf = $titre."_".$lettre.$id_parent.".pdf";
+	return $path_pdf.$nom_pdf;
+}
+
+/**
+ *
+ * [(#TITRE|pdf_nommer_ancien{article, #ID_ARTICLE})]
+ * 
+**/
+function pdf_nommer_ancien($titre_parent, $objet='article', $id_article){
+	
+	//$titre_article = translitteration(corriger_caracteres('[(#TITRE|supprimer_numero|pdf_first_clean|supprimer_tags|texte_script)]'));
+	$titre_article = translitteration(corriger_caracteres(texte_script(supprimer_tags(pdf_first_clean($titre_parent)))));
+	
+	// Reprise du code de urls/propres.php (pourquoi c'est pas une fonction de l'API de SPIP ?)
+	
+	// on va convertir tous les caracteres de ponctuation et espaces
+	// a l'exception de l'underscore (_), car on veut le conserver dans l'url
+	$titre_article = str_replace('_', chr(7), $titre_article);
+	$titre_article = @preg_replace(',[[:punct:][:space:]]+,u', ' ', $titre_article);
+	$titre_article = str_replace(chr(7), '_', $titre_article);
+
+	// S'il reste trop de caracteres non latins, les gerer comme wikipedia
+	// avec rawurlencode :
+	if (preg_match_all(",[^a-zA-Z0-9 _]+,", $titre_article, $r, PREG_SET_ORDER)) {
+		foreach ($r as $regs) {
+			$titre_article = substr_replace($titre_article, rawurlencode($regs[0]),
+				strpos($titre_article, $regs[0]), strlen($regs[0]));
+		}
+	}
+
+	// S'il reste trop peu, on retombe sur article12
+	if (strlen($titre_article) == 0 ) {
+		$titre_article = "article";
+	}
+
+	// Sinon couper les mots et les relier par des tirets
+	else {
+		$mots = preg_split(",[^a-zA-Z0-9_%]+,", $titre_article);
+		$titre_article = '';
+		foreach ($mots as $mot) {
+			if (!strlen($mot)) continue;
+			$titre_article2 = $titre_article.'-'.$mot;
+
+			// Si on depasse _URLS_PROPRES_MAX caracteres, s'arreter
+			// ne pas compter 3 caracteres pour %E9 mais un seul
+			$long = preg_replace(',%.,', '', $titre_article2);
+			if (strlen($long) > _URLS_PROPRES_MAX) {
+				break;
+			}
+
+			$titre_article = $titre_article2;
+		}
+		$titre_article = substr($titre_article, 1);
+
+		// On enregistre en utf-8 dans la base
+		$titre_article = rawurldecode($titre_article);
+
+		if (strlen($titre_article) == 0)
+			$titre_article = "article";
+	}
+
+	$files_pdf = $titre_article."_a".$id_article.".pdf";
+	return $files_pdf;
+	
+}
