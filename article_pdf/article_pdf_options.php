3cae4fe1c189959fd8b28e040fd07f2aca747ded
toutati@free.fr <toutati@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-06-14 21:46:35 +0000

    oups, corrections du commit précédent, la balise pdf_restreint n'est pas encore d'actualité … et un up pour prendre en compte les nettoyages
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@104869 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- article_pdf/article_pdf_options.php
+++ article_pdf/article_pdf_options.php
@@ -13,37 +13,6 @@ if (!defined('_DIR_STOCK_PDF')) {
 if (!defined('_DIR_LIB')) define('_DIR_LIB', _DIR_RACINE . 'lib/');
 if (!defined('_DIR_FPDF_LIB')) define('_DIR_FPDF_LIB', _DIR_LIB . 'h5c1accba-fpdf17/');
 
-function balise_ARTICLE_PDF_RESTREINT_dist($p) {
-	if (!is_array($p->param))
-		$p->param=array();
-
-	// Produire le premier argument {article_pdf_restreint}
-	$texte = new Texte;
-	$texte->type='texte';
-	$texte->texte='article_pdf_restreint';
-	$param = array(0=>NULL, 1=>array(0=>$texte));
-	array_unshift($p->param, $param);
-
-	// Transformer les filtres en arguments
-	for ($i=1; $i<count($p->param); $i++) {
-		if ($p->param[$i][0]) {
-			if (!strstr($p->param[$i][0], '='))
-				break;# on a rencontre un vrai filtre, c'est fini
-			$texte = new Texte;
-			$texte->type='texte';
-			$texte->texte=$p->param[$i][0];
-			$param = array(0=>$texte);
-			$p->param[$i][1] = $param;
-			$p->param[$i][0] = NULL;
-		}
-	}
-
-	// Appeler la balise #MODELE{article_pdf_restreint}{arguments}
-	if (!function_exists($f = 'balise_modele'))
-		$f = 'balise_modele_dist';
-	return $f($p);
-}
-
 function balise_ARTICLE_PDF_dist($p) {
 	if (!is_array($p->param))
 		$p->param=array();
