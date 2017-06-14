72a5dd356d1e35b0850d8185a77ceacc87576a38
erational@erational.org <erational@erational.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-06-14 08:57:56 +0000

    PSR + sécurité sur un vieux machin
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@104854 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- onglets_texte/trunk/onglets_texte_pipelines.php
+++ onglets_texte/trunk/onglets_texte_pipelines.php
@@ -1,7 +1,13 @@
 <?php
 
-	$p = explode(basename(_DIR_PLUGINS)."/",str_replace('\\','/',realpath(dirname(__FILE__))));
-	define('_DIR_PLUGIN_ONGLETS_TEXTE',(_DIR_PLUGINS.end($p)));
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+
+
+$p = explode(basename(_DIR_PLUGINS)."/",str_replace('\\','/',realpath(dirname(__FILE__))));
+define('_DIR_PLUGIN_ONGLETS_TEXTE',(_DIR_PLUGINS.end($p)));
 
 function onglets_texte_insert_head($flux){
 	$flux .= '<script type="text/javascript" src="'.find_in_path('javascript/mes_onglets.js').'"></script>';
@@ -12,5 +18,3 @@ function onglets_texte_insert_head_css($flux){
 	$flux .= '<link rel="stylesheet" href="'.find_in_path('css/tab.css').'" type="text/css" media="all" />';
 	return $flux;
 }
-
-?>
