bcfbf8fd23cdbb15e8682c9223559a2b217bd078
marcimat@rezo.net <marcimat@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-06-17 17:33:56 +0000

    Ajout de filtres & d’un critère pour pouvoir sélectionner des points proches d’un tracé. Nécessite Mysql 5.6+ ou Mariadb pour être utilisés.
    # Filtre zoner_geometrie
    
    `zoner_geometrie($geometrie, $type[, $distance[, $tolerance]])`
    
    Pour une géométrie donnée, au format $type (json, wkt, ...), calcule une zone (un polygone) qui couvre la géométrie à une distance donnée (0.05 par défaut ici).
    En d’autre terme, applique la fonction Mysql ST_Buffer() sur la géométrie en entrée. La distance est exprimée en degré décimal d’angle si la géométrie a des points latitude/longitude (en tout cas c'est ce que j’en comprends).
    Pour que le polygone calculé ne contienne pas une immensité de points, on simplifie le polygone d’entrée et le polygone de sortie (ça réduit le nombre de points). La réduction prend en compte un paramètre de tolérance, qui vaut par défaut $distance/20.
    
    # Filtre simplifier_geometrie
    
    `simplifier_geometrie($geometrie, $type, $tolerance)`
    
    Simplifie le nombre de points d’une géométrie, en utilisant la tolérance indiquée (en degré décimal d’angle je suppute, si la géométrie est en latitude/longitude).
    Elle est utilisée automatiquement par la fonction zone_geometrie().
    
    # Critère geometrie_dans_polygone
    
    `{geometrie_dans_polygone #GET{polygone}`
    
    Pour un polygone calculé, par exemple avec zoner_geometrie() tel que 
    `#SET{polygone,#GEOMETRY|zoner_geometrie{wkt,0.05}` dans une boucle GIS, récupère les géométries qui sont contenues dans le polygone.
    
    Exemple, récupérer les Points (et pas d’autre forme, mais ce serait possible) dans une zone : 
    `<BOUCLE_points_proches(GIS){type=Point}{geometrie_dans_polygone #GET{poly}}>`
    
    
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@104889 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- gis_geometries/gisgeom_fonctions.php
+++ gis_geometries/gisgeom_fonctions.php
@@ -109,3 +109,123 @@ function geometry_styles_to_json($geometry_styles) {
 	}
 	return json_encode($styles);
 }
+
+
+
+/**
+ * Renvoie une géométrie couvrant tous les points à une distance donnée de la géométrie d'entrée.
+ *
+ * Cela permet de calculer une zone autour d’un point, d’une ligne ou d’un polygone, à une certaine distance.
+ * Retourne une géométrie (polygone) simplifiée.
+ *
+ * @filtre
+ * @example
+ *     ```
+ *     Dans une boucle GIS :
+ *     #SET{poly,#GEOMETRY|zoner_geometrie{wkt,0.05}}
+ *     #SET{poly,#GEOMETRY|zoner_geometrie{wkt,0.05,0.005}}
+ *     ```
+ *
+ * @note
+ *     Nécessite Mysql 5.6+ ou Mariadb.
+ *
+ * @param string $geometrie
+ *     Géométrie
+ * @param float $distance
+ *     En degrés décimal (si l’unité de géométrie est latitude / longitude…)
+ * @param float|null $tolerance
+ *     Tolérance de simplification des tracés (null, appliquera la valeur par défaut ($distance/20)),
+ *     en degré décimal (si l’unité de géométrie est latitude / longitude…) ;
+ * @return mixed
+ */
+function zoner_geometrie($geometrie, $type = 'wkt', $distance = 0.05, $tolerance = null) {
+	// tolérance… à peu près 1/20 de l’unité de distance (pifomètre)
+	if (is_null($tolerance)) {
+		$tolerance = $distance / 20;
+	}
+
+	// Simplifier le tracé d’origine… le récupérer en wkt
+	$geometrie = simplifier_geometrie($geometrie, $type, $tolerance, 'wkt');
+
+	// Obtenir le zonage demandé
+	$select = 'GeomFromText(' . sql_quote($geometrie) . ')';
+	$select = 'ST_Buffer(' . $select . ', ' . floatval($distance) . ')';
+	#$select = 'ST_Simplify(' . $select . ', ' . floatval($tolerance) . ')'; // mysql 5.7+ (non testé)
+	$select = 'ST_AsText(' . $select . ')';
+	$zone = sql_getfetsel($select);
+
+	// Simplifié le tracé de la zone, le récupérer au format d’entrée
+	$zone = simplifier_geometrie($zone, 'wkt', $tolerance, $type);
+	return $zone;
+}
+
+/**
+ * Simplifier une forme géométrique
+ *
+ * Diminue le nombre de points d’une forme géométrique (lignes, polygones, ...)
+ *
+ * @param mixed $data
+ *     Forme à simplifer, pouvant être lue par geoPHP (json, wkt, ...)
+ * @param string $type
+ *     Type d’entrée de `$data` (json, wkt, ...)
+ * @param float|null $tolerance
+ *     Tolérance de la réduction (en degré décimal si les unités sont latitude/longitude)
+ * @param string $type_sortie
+ *     Type de sortie de la simplification de `$data` (json, wkt, ...)
+ *     Par défaut, le type indiqué en entrée.
+ * @retrun mixed
+ *     Forme simplifiée, dans le même format qu’en entrée.
+ */
+function simplifier_geometrie($data, $type = 'wkt', $tolerance = null, $type_sortie = null) {
+	if (!class_exists('geoPHP_Simplify')) {
+		include_spip('inc/geoPHP_Simplify');
+	}
+	$geometry = geoPHP::load($data, $type);
+	$geometry = geoPHP::geometryReduce($geometry);
+	$geometry = geoPHP_Simplify::geometrySimplify($geometry, $tolerance);
+	$data = $geometry->out($type_sortie ? $type_sortie : $type);
+	return $data;
+}
+
+
+/**
+ * Compile le critère `geometrie_dans_polygone` qui sélectionne des points contenus dans un polygone donné.
+ *
+ * @critere
+ * @see zoner_geometrie() pour créer une zone (polygone) à partir d’un tracé.
+ * @example
+ *     ```
+ *     Dans une boucle GIS : #SET{poly,#GEOMETRY|zoner_geometrie{wkt,0.05}}
+ *     Puis :
+ *     <BOUCLE_points_proches(GIS){type=Point}{geometrie_dans_polygone #GET{poly}}> ...
+ *     ```
+ * @note
+ *     - Nécessite Mysql 5.6 ou Mariadb
+ *     - Si le polygone est trop complexe (nombreux points, le calcul peut être trop long),
+ *       c'est pour cela que zoner_geometrie() simplifie le tracé automatiquement.
+ *
+ * @param string $idb Identifiant de la boucle
+ * @param array $boucles AST du squelette
+ * @param Critere $crit Paramètres du critère dans cette boucle
+ * @return void
+ **/
+function critere_geometrie_dans_polygone_dist($idb, &$boucles, $crit) {
+	$not = $crit->not;
+	$boucle = &$boucles[$idb];
+
+	// prendre en priorite un identifiant en parametre {branche XX}
+	if (isset($crit->param[0])) {
+		$polygon = calculer_liste($crit->param[0], array(), $boucles, $boucles[$idb]->id_parent);
+		// sinon on le prend chez une boucle parente
+	} else {
+		return array('zbug_critere_inconnu', array('critere' => $crit->op));
+	}
+
+	$polygon = kwote($polygon, $boucle->sql_serveur, 'TEXT');
+	if ($not) {
+		$boucle->where[] = array("'='", "'ST_WITHIN(gis.geo, GeomFromText(' . $polygon . '))'", "'0'");
+	} else {
+		$boucle->where[] = array("'='", "'ST_WITHIN(gis.geo, GeomFromText(' . $polygon . '))'", "'1'");
+	}
+
+}
