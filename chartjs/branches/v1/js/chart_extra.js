4fa53a58d6494c60122e49dbdda91d3df83a9ec4
tcharlss@bravecassine.com <tcharlss@bravecassine.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-06-27 09:06:19 +0000

    On met la version stable dans une branche, le temps de faire évoluer le trunk
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@104985 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ chartjs/branches/v1/js/chart_extra.js
@@ -0,0 +1,7 @@
+/**********************************************
+ * configuration globale de chart.js
+ *
+ * doc.  http://www.chartjs.org/docs/
+ ***********************************************/    
+
+Chart.defaults.global.responsive = true;
---
+++ chartjs/branches/v1/lang/chartjs_fr.php
@@ -0,0 +1,21 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+ 
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+     // D
+     'demo_titre' => 'Demonstration Chart.js',
+     'demo_pie' => 'Camembert (Type: Pie)',
+     'demo_doughnut' => 'Beignet (Type:  Doughnut)',
+     'demo_polaire' => 'Polaire (Type: polarArea)',
+     'demo_bar' => 'Barre (Type: bar)',
+     'demo_line' => 'Courbe (Type: line)',
+     'demo_radar' => 'Radar (Type: radar)',
+     'demo_spip_data' => 'Graphes dynamiques avec boucle SPIP',
+     'demo_spip_bigdata' => 'Graphes dynamiques avec boucle SPIP DATAS',
+     'demo_spip_legend' => 'Cours du Yen en Eur des 30 derniers jours',
+);
+
+?>
