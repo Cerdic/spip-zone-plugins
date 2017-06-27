cdc3977e5d04245007848f99ec5c13395ed473eb
tcharlss@bravecassine.com <tcharlss@bravecassine.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-06-27 18:20:18 +0000

    Début de la refacto du modèle : dans la v2 de la lib, tout est affaire de datasets, donc on fusionne les paramètres 'data' et 'datasets. Ça reste compatible avec les syntaxes des 2 paramètres, et on peut aussi passer directement des tableaux. Il est aussi possible de passer un tableau 'complet' avec toutes les options, si on veut vraiment controler complètement le rendu (couleurs, etc.). On rajoutera les autres paramètres optionnels par la suite.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@104998 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- chartjs/trunk/modeles/chart_fonctions.php
+++ chartjs/trunk/modeles/chart_fonctions.php
@@ -17,4 +17,48 @@ function chartjs_hex2rgb($hex) {
 	   $rgb = array($r, $g, $b);
 	   return implode(",", $rgb); // returns the rgb values separated by commas
 }
-?>
+
+// Convertir des mots spéparés par des virgules en tableau (pour utiliser dans un array_map)
+function chartjs_explode_virgule($texte){
+	$tableau = explode(',', $texte);
+	return $tableau;
+}
+
+// Typer les valeurs texte d'un tableau
+function chartjs_array_typecaster($table){
+	$table = chartjs_array_map_recursive('chartjs_typecaster', $table);
+	return $table;
+}
+
+// Typer une valeur texte en entier ou booléen
+function chartjs_typecaster($v){
+	if (is_numeric($v)) {
+		$v = floatval($v);
+	} elseif ($v == 'true') {
+		$v = true;
+	} elseif ($v == 'false') {
+		$v = false;
+	}
+	return $v;
+}
+
+// Pas dans php :(
+function chartjs_array_map_recursive($function, $table) {
+	$out = array();
+	if (is_array($table)) {
+		foreach ($table as $k => $v) {
+			$out[$k] = ($v and is_array($v)) ? chartjs_array_map_recursive($function, $v) : $function($v);
+		}
+	}
+	return $out;
+}
+
+// Pas dans php :(
+function chartjs_array_filter_recursive($input) {
+	foreach ($input as &$value) {
+		if (is_array($value)) {
+			$value = chartjs_array_filter_recursive($value);
+		}
+	}
+	return array_filter($input);
+}
