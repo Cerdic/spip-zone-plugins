1e0b0b5722259370003388ac7c51f13ad35fc7b4
peetdu@gmail.com <peetdu@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-06-28 10:18:38 +0000

    Guillemet
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@105012 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- clil/trunk/lang/paquet-clil_fr.php
+++ clil/trunk/lang/paquet-clil_fr.php
@@ -7,7 +7,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// C
 	'clil_description' => '<p>CLIL : Commission de Liaison Interprofessionnelle du Livre.</p>
-	Ajoute dans le formulaire Livre du plugin Bouquinerie un sélecteur de thème CLIL. Ce thème CLIL répond à la norme norme ONIX 3.0 et permet d\'améliorer le référencement de vos livres sur les différents sites de ventes.<br>
+	Ajoute dans le formulaire Livre du plugin Bouquinerie un sélecteur de thème CLIL. Ce thème CLIL répond à la norme ONIX 3.0 et permet d’améliorer le référencement de vos livres sur les différents sites de ventes.<br>
 
 	- <b>Configuration</b> : Vous pouvez aller dans la page de gestion du plugin (Publication -> Thèmes CLIL) et sélectionner parmi les 1052 thèmes de la CLIL ceux spécifiques à vos besoins.',
 	'clil_nom' => 'Thèmes CLIL',
