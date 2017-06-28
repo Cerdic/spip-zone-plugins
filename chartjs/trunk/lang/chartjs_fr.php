5233e3719e1e185263f08ab968877c3c7bb00afb
tcharlss@bravecassine.com <tcharlss@bravecassine.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-06-28 11:27:04 +0000

    Ajout des types scatter et bubble
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@105013 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- chartjs/trunk/lang/chartjs_fr.php
+++ chartjs/trunk/lang/chartjs_fr.php
@@ -5,17 +5,19 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
  
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-     // D
-     'demo_titre' => 'Demonstration Chart.js',
-     'demo_pie' => 'Camembert (Type: Pie)',
-     'demo_doughnut' => 'Beignet (Type:  Doughnut)',
-     'demo_polaire' => 'Polaire (Type: polarArea)',
-     'demo_bar' => 'Barre (Type: bar)',
-     'demo_line' => 'Courbe (Type: line)',
-     'demo_radar' => 'Radar (Type: radar)',
-     'demo_spip_data' => 'Graphes dynamiques avec boucle SPIP',
-     'demo_spip_bigdata' => 'Graphes dynamiques avec boucle SPIP DATAS',
-     'demo_spip_legend' => 'Cours du Yen en Eur des 30 derniers jours',
+		// D
+		'demo_titre' => 'Demonstration du plugin Chart.js',
+		'demo_pie' => 'Camembert',
+		'demo_doughnut' => 'Beignet',
+		'demo_polaire' => 'Polaire',
+		'demo_bar' => 'Barre',
+		'demo_line' => 'Courbe',
+		'demo_radar' => 'Radar',
+		'demo_bubble' => 'Bulles',
+		'demo_type' => 'Type: @type@',
+		'demo_spip_data' => 'Graphes dynamiques avec boucle SPIP',
+		'demo_spip_bigdata' => 'Graphes dynamiques avec boucle SPIP DATAS',
+		'demo_spip_legend' => 'Cours du Yen en Eur des 30 derniers jours',
+		'demo_titre_types' => 'Types de graphiques',
+		'demo_titre_exemples_avances' => 'Exemples avancés',
 );
-
-?>
