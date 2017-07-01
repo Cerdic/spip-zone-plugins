76d473e1111aec7ede7dd5807d60a74156d1fbdb
p@henix.be <p@henix.be@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-07-01 21:04:10 +0000

    Init
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@105074 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ ingredient/trunk/ingredient_fonctions.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Fonctions utiles au plugin ingrédients
+ *
+ * @plugin     ingrédients
+ * @copyright  2015
+ * @author     Phenix
+ * @licence    GNU/GPL
+ * @package    SPIP\Ingredient\Fonctions
+ */
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+
+/*
+ * Un fichier de fonctions permet de définir des éléments
+ * systématiquement chargés lors du calcul des squelettes.
+ *
+ * Il peut par exemple définir des filtres, critères, balises, …
+ * 
+ */
+
+?>
