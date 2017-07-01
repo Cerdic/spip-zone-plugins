cdf1fbb89954b983f2cdd94b204c9439b9698ff2
p@henix.be <p@henix.be@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-07-01 21:04:33 +0000

    Bloquer la suppression d'un ingredient qui possède des liens
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@105110 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- ingredient/trunk/ingredient_autorisations.php
+++ ingredient/trunk/ingredient_autorisations.php
@@ -107,6 +107,17 @@ function autoriser_ingredient_modifier_dist($faire, $type, $id, $qui, $opt) {
  * @return bool          true s'il a le droit, false sinon
 **/
 function autoriser_ingredient_supprimer_dist($faire, $type, $id, $qui, $opt) {
+
+    // Il faut vérifier qu'il n'y a pas d'ingredient associer pour pouvoir le supprimer
+    include_spip('action/editer_liens');
+    $liens = objet_trouver_liens(
+        array('ingredients'=>$id),
+        array('article' => '*')
+    );
+    // S'il y a un lien, on empèche la suppression
+    if (!empty($liens))
+        return false;
+
 	return $qui['statut'] == '0minirezo' AND !$qui['restreint'];
 }
 
@@ -124,4 +135,3 @@ function autoriser_ingredient_supprimer_dist($faire, $type, $id, $qui, $opt) {
 function autoriser_associeringredients_dist($faire, $type, $id, $qui, $opt) {
 	return $qui['statut'] == '0minirezo' AND !$qui['restreint'];
 }
-
