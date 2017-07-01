33b4c213622e239a7880814c2d192f2019177ff9
Maïeul Rouquette <Maeul Rouquette@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-07-01 12:49:55 +0000

    possibilité d'avoir des PJ directement encloses dans le mail : constante + case à cocher + chaîne de langue
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@105058 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ formidable/branches/pj_embarque/genie/formidable_hasher_ip.php
@@ -0,0 +1,32 @@
+<?php
+
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+/**
+* CNIL -- Informatique et libertes
+*
+* masquer le numero IP des vieilles réponses
+* date de reference = 4 mois
+* definir a 0 pour desactiver
+* même valeur par défaut que pour les forums
+**/
+function genie_formidable_hasher_ip_dist($t) {
+	if (!defined('_CNIL_PERIODE')) {
+		define('_CNIL_PERIODE', 3600*24*31*4);
+	}
+
+	if (_CNIL_PERIODE) {
+		$critere_cnil = 'date<"'.date('Y-m-d', time()-_CNIL_PERIODE).'"'
+			. ' AND statut != "spam"'
+			. ' AND (ip LIKE "%.%" OR ip LIKE "%:%")'; # ipv4 ou ipv6
+		$c = sql_countsel('spip_formulaires_reponses', $critere_cnil);
+		if ($c>0) {
+			spip_log("CNIL: masquer IP de $c réponses anciennes à formidable", 'formidable');
+			sql_update('spip_formulaires_reponses', array('ip' => 'MD5(ip)'), $critere_cnil);
+			return $c;
+		}
+	}
+	return 0;
+}
