dbf9d503c1264c00f7bde4f67d9023a8fd570984
p@henix.be <p@henix.be@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-07-01 21:04:17 +0000

    Formulaire de création de lien
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@105085 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ ingredient/trunk/formulaires/liens_ingredients.php
@@ -0,0 +1,51 @@
+<?php
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+function formulaires_liens_ingredients_saisies_dist() {
+    $saisies = array(
+        array(
+            'saisie' => 'ingredients',
+            'options' => array(
+                'nom' => 'ingredient',
+                'label' => _T('ingredient:titre_ingredient'),
+                'class' => 'selectize'
+            )
+        ),
+        array(
+            'saisie' => 'input',
+            'options' => array(
+                'nom' => 'quantite',
+                'label' => _T('ingredient:champ_titre_quantite')
+            )
+        )
+    );
+
+    return $saisies;
+}
+
+/*
+ *   Fonction de vérification, cela fonction avec un tableau d'erreur.
+ *   Le tableau est formater de la sorte:
+ *   if (!_request('NomErreur')) {
+ *       $erreurs['message_erreur'] = '';
+ *       $erreurs['NomErreur'] = '';
+ *   }
+ *   Pensez à utiliser _T('info_obligatoire'); pour les éléments obligatoire.
+ */
+function formulaires_liens_ingredients_verifier_dist() {
+    $erreurs = array();
+
+    return $erreurs;
+}
+
+function formulaires_liens_ingredients_traiter_dist() {
+    //Traitement du formulaire.
+
+    // Donnée de retour.
+    return array(
+        'editable' => true,
+        'message_ok' => '',
+        'redirect' => ''
+    );
+}
