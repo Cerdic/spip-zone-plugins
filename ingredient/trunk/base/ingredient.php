0d7ecd2a632cc57d80d60eee57c7e210682b8f17
p@henix.be <p@henix.be@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-07-01 21:04:41 +0000

    PSR
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@105121 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- ingredient/trunk/base/ingredient.php
+++ ingredient/trunk/base/ingredient.php
@@ -2,14 +2,15 @@
 /**
  * Déclarations relatives à la base de données
  *
- * @plugin     ingrédients
+ * @plugin	   ingrédients
  * @copyright  2015
- * @author     Phenix
- * @licence    GNU/GPL
- * @package    SPIP\Ingredient\Pipelines
+ * @author	   Phenix
+ * @licence	   GNU/GPL
+ * @package	   SPIP\Ingredient\Pipelines
  */
 
-if (!defined('_ECRIRE_INC_VERSION')) return;
+if (!defined('_ECRIRE_INC_VERSION')) { return;
+}
 
 
 /**
@@ -17,9 +18,9 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
  *
  * @pipeline declarer_tables_interfaces
  * @param array $interfaces
- *     Déclarations d'interface pour le compilateur
+ *	   Déclarations d'interface pour le compilateur
  * @return array
- *     Déclarations d'interface pour le compilateur
+ *	   Déclarations d'interface pour le compilateur
  */
 function ingredient_declarer_tables_interfaces($interfaces) {
 
@@ -34,31 +35,31 @@ function ingredient_declarer_tables_interfaces($interfaces) {
  *
  * @pipeline declarer_tables_objets_sql
  * @param array $tables
- *     Description des tables
+ *	   Description des tables
  * @return array
- *     Description complétée des tables
+ *	   Description complétée des tables
  */
 function ingredient_declarer_tables_objets_sql($tables) {
 
 	$tables['spip_ingredients'] = array(
 		'type' => 'ingredient',
-		'principale' => "oui",
+		'principale' => 'oui',
 		'field'=> array(
-			"id_ingredient"      => "bigint(21) NOT NULL",
-			"titre"              => "text NOT NULL DEFAULT ''",
-			"descriptif"         => "text DEFAULT '' NOT NULL COLLATE NOCASE",
-			"texte"              => "longtext DEFAULT '' NOT NULL COLLATE NOCASE",
-			"maj"                => "TIMESTAMP"
+			'id_ingredient'		 => 'bigint(21) NOT NULL',
+			'titre'				 => "text NOT NULL DEFAULT ''",
+			'descriptif'		 => "text DEFAULT '' NOT NULL COLLATE NOCASE",
+			'texte'				 => "longtext DEFAULT '' NOT NULL COLLATE NOCASE",
+			'maj'				 => 'TIMESTAMP'
 		),
 		'key' => array(
-			"PRIMARY KEY"        => "id_ingredient",
+			'PRIMARY KEY'		 => 'id_ingredient',
 		),
 		'titre' => "titre AS titre, '' AS lang",
 		 #'date' => "",
-		'champs_editables'  => array('titre', 'descriptif', 'texte', 'unite'),
+		'champs_editables'	=> array('titre', 'descriptif', 'texte', 'unite'),
 		'champs_versionnes' => array('titre', 'descriptif', 'texte', 'unite'),
-		'rechercher_champs' => array("titre" => 8, "descriptif" => 7, "texte" => 7),
-		'tables_jointures'  => array('spip_ingredients_liens'),
+		'rechercher_champs' => array('titre' => 8, 'descriptif' => 7, 'texte' => 7),
+		'tables_jointures'	=> array('spip_ingredients_liens'),
 	);
 
 	return $tables;
@@ -70,28 +71,25 @@ function ingredient_declarer_tables_objets_sql($tables) {
  *
  * @pipeline declarer_tables_auxiliaires
  * @param array $tables
- *     Description des tables
+ *	   Description des tables
  * @return array
- *     Description complétée des tables
+ *	   Description complétée des tables
  */
 function ingredient_declarer_tables_auxiliaires($tables) {
 
 	$tables['spip_ingredients_liens'] = array(
 		'field' => array(
-			"id_ingredient"      => "bigint(21) DEFAULT '0' NOT NULL",
-			"id_objet"           => "bigint(21) DEFAULT '0' NOT NULL",
-			"objet"              => "VARCHAR(25) DEFAULT '' NOT NULL",
-			"quantite"           => "text DEFAULT '' NOT NULL",
-			"vu"                 => "VARCHAR(6) DEFAULT 'non' NOT NULL"
+			'id_ingredient'		 => "bigint(21) DEFAULT '0' NOT NULL",
+			'id_objet'			 => "bigint(21) DEFAULT '0' NOT NULL",
+			'objet'				 => "VARCHAR(25) DEFAULT '' NOT NULL",
+			'quantite'			 => "text DEFAULT '' NOT NULL",
+			'vu'				 => "VARCHAR(6) DEFAULT 'non' NOT NULL"
 		),
 		'key' => array(
-			"PRIMARY KEY"        => "id_ingredient,id_objet,objet",
-			"KEY id_ingredient"  => "id_ingredient"
+			'PRIMARY KEY'		 => 'id_ingredient,id_objet,objet',
+			'KEY id_ingredient'	 => 'id_ingredient'
 		)
 	);
 
 	return $tables;
 }
-
-
-?>
