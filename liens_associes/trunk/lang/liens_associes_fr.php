e604f333a8078e9c25fb26ce7a46a79964b8fe08
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-07-02 03:32:50 +0000

    [Salvatore] [source:_plugins_/liens_associes/trunk/lang/ liens_associes] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@105148 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- liens_associes/trunk/lang/liens_associes_fr.php
+++ liens_associes/trunk/lang/liens_associes_fr.php
@@ -1,19 +1,19 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/liens_associes/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-	// L
-	'label_lier_objets' => 'Objets pour lesquels les Liens associés seront proposé :',
-	'liens_associes_titre' => 'Liens associés',
-
 	// C
 	'cfg_titre_parametrages' => 'Paramétrages',
 
+	// L
+	'label_lier_objets' => 'Objets pour lesquels les Liens associés seront proposé :',
+	'liens_associes_titre' => 'Liens associés',
+
 	// T
-	'titre_page_configurer_liens_associes' => 'Configurations',
+	'titre_page_configurer_liens_associes' => 'Configurations'
 );
