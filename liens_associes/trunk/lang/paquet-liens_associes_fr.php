774ff7e9e046770d74a02b504f57cc3bc15950cc
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-07-02 03:32:51 +0000

    [Salvatore] [source:_plugins_/liens_associes/trunk/lang/ paquet-liens_associes] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@105149 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- liens_associes/trunk/lang/paquet-liens_associes_fr.php
+++ liens_associes/trunk/lang/paquet-liens_associes_fr.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/liens_associes/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
@@ -10,5 +10,5 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// L
 	'liens_associes_description' => 'Gérer les liens associés',
 	'liens_associes_nom' => 'Liens associés',
-	'liens_associes_slogan' => 'Gérer les liens associés',
+	'liens_associes_slogan' => 'Gérer les liens associés'
 );
