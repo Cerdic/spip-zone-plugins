d9b2d79cf0872bda65b8961e192c1674b9da3f85
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-07-04 11:55:47 +0000

    Rendre les vidéos async comme youtube
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@105184 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ oembed/oembed/input/posttraite_facebook_video.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Plugin oEmbed
+ * Licence GPL3
+ *
+ */
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+function oembed_input_posttraite_facebook_video_dist($data, $url_orig) {
+	$id_video = preg_replace(',http.*facebook.com\/.*videos\/,', '', $data['url']);
+	$id_video = rtrim($id_video, '/');
+	if (intval($id_video) > 0) {
+		include_spip('inc/distant');
+		/**
+		 * Récupérer les données opengraph de la video
+		 * @var string $data2
+		 */
+		$data2 = recuperer_page('https://graph.facebook.com/'.$id_video.'/');
+		if ($data2) {
+			$data2 = json_decode($data2, true);
+			if (is_array($data2) and isset($data2['picture'])) {
+				$data['thumbnail_url'] = $data2['picture'];
+			}
+			if (is_array($data2) and isset($data2['embed_html'])) {
+				$embed = $data2['embed_html'];
+				$data['source'] = $data['url'];
+				$data['html'] = $data2['embed_html'];
+				unset($data['url']);
+			}
+		}
+	}
+	return $data;
+}
