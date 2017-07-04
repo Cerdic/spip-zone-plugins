920749519cc76c715e05c97a17ae18f60d47dac5
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-07-04 12:57:57 +0000

    Si on a une description => titre
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@105192 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- oembed/oembed/input/posttraite_facebook_video.php
+++ oembed/oembed/input/posttraite_facebook_video.php
@@ -21,14 +21,19 @@ function oembed_input_posttraite_facebook_video_dist($data, $url_orig) {
 		$data2 = recuperer_page('https://graph.facebook.com/'.$id_video.'/');
 		if ($data2) {
 			$data2 = json_decode($data2, true);
-			if (is_array($data2) and isset($data2['picture'])) {
-				$data['thumbnail_url'] = $data2['picture'];
-			}
-			if (is_array($data2) and isset($data2['embed_html'])) {
-				$embed = $data2['embed_html'];
-				$data['source'] = $data['url'];
-				$data['html'] = $data2['embed_html'];
-				unset($data['url']);
+			if (is_array($data2)) {
+				if (isset($data2['picture'])) {
+					$data['thumbnail_url'] = $data2['picture'];
+				}
+				if (isset($data2['embed_html'])) {
+					$embed = $data2['embed_html'];
+					$data['source'] = $data['url'];
+					$data['html'] = $data2['embed_html'];
+					unset($data['url']);
+				}
+				if (isset($data2['description'])) {
+					$data['title'] = $data2['description'];
+				}
 			}
 		}
 	}
