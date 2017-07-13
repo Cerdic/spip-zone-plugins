3ba2d855ac0fc035e75ff1b9c01f89ac2758fdcd
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-07-13 03:32:41 +0000

    [Salvatore] [source:_plugins_/selections_editoriales/trunk/lang/ selections_contenu] Export depuis http://trad.spip.net de la langue en
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@105293 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- selections_editoriales/trunk/lang/selections_contenu_en.php
+++ selections_editoriales/trunk/lang/selections_contenu_en.php
@@ -10,42 +10,42 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
-	'ajouter_lien_selections_contenu' => 'Add this selected content', # RELIRE
+	'ajouter_lien_selections_contenu' => 'Add this selected content',
 
 	// C
-	'champ_css_explication' => 'One or more CSS classes to add to this selected content', # RELIRE
-	'champ_css_label' => 'CSS classes', # RELIRE
+	'champ_css_explication' => 'One or more CSS classes to add to this selected content',
+	'champ_css_label' => 'CSS classes',
 	'champ_descriptif_label' => 'Description',
 	'champ_id_selection_label' => 'Selection',
-	'champ_titre_explication' => 'If the title is empty, it will try to fill itself magically', # RELIRE
+	'champ_titre_explication' => 'If the title is empty, it will try to fill itself magically',
 	'champ_titre_label' => 'Title',
-	'champ_url_explication' => 'The URL of a page (http://example) or the shortcut of a SPIP content (article123, rubrique456, etc.).', # RELIRE
+	'champ_url_explication' => 'The URL of a page (http://example) or the shortcut of a SPIP content (article123, rubrique456, etc.).',
 	'champ_url_label' => 'URL',
 
 	// D
-	'deplacer' => 'Drag and drop this content', # RELIRE
-	'deplacer_apres' => 'Move after', # RELIRE
-	'deplacer_avant' => 'Move before', # RELIRE
+	'deplacer' => 'Drag and drop this content',
+	'deplacer_apres' => 'Move after',
+	'deplacer_avant' => 'Move before',
 
 	// I
-	'icone_creer_selections_contenu' => 'Create a selected content', # RELIRE
-	'icone_modifier_selections_contenu' => 'Modify this selected content', # RELIRE
-	'info_1_selections_contenu' => 'A selected content', # RELIRE
-	'info_aucun_selections_contenu' => 'No selected content', # RELIRE
-	'info_nb_selections_contenus' => '@nb@ selected contents', # RELIRE
-	'info_selections_contenus_auteur' => 'Selected contents by this author', # RELIRE
+	'icone_creer_selections_contenu' => 'Create a selected content',
+	'icone_modifier_selections_contenu' => 'Modify this selected content',
+	'info_1_selections_contenu' => 'A selected content',
+	'info_aucun_selections_contenu' => 'No selected content',
+	'info_nb_selections_contenus' => '@nb@ selected contents',
+	'info_selections_contenus_auteur' => 'Selected contents by this author',
 
 	// R
-	'retirer_lien_selections_contenu' => 'Remove this selected content', # RELIRE
-	'retirer_tous_liens_selections_contenus' => 'Remove all selected contents', # RELIRE
+	'retirer_lien_selections_contenu' => 'Remove this selected content',
+	'retirer_tous_liens_selections_contenus' => 'Remove all selected contents',
 
 	// T
-	'texte_ajouter_selections_contenu' => 'Add a selected content', # RELIRE
-	'texte_changer_statut_selections_contenu' => 'This selected content is:', # RELIRE
-	'texte_creer_associer_selections_contenu' => 'Create and link a selected content', # RELIRE
-	'titre_langue_selections_contenu' => 'Language of this selected content', # RELIRE
-	'titre_logo_selections_contenu' => 'Logo of this selected content', # RELIRE
-	'titre_selections_contenu' => 'Selected content', # RELIRE
-	'titre_selections_contenus' => 'Selected contents', # RELIRE
-	'titre_selections_contenus_rubrique' => 'Selected contents in the section' # RELIRE
+	'texte_ajouter_selections_contenu' => 'Add a selected content',
+	'texte_changer_statut_selections_contenu' => 'This selected content is:',
+	'texte_creer_associer_selections_contenu' => 'Create and link a selected content',
+	'titre_langue_selections_contenu' => 'Language of this selected content',
+	'titre_logo_selections_contenu' => 'Logo of this selected content',
+	'titre_selections_contenu' => 'Selected content',
+	'titre_selections_contenus' => 'Selected contents',
+	'titre_selections_contenus_rubrique' => 'Selected contents in the section'
 );
