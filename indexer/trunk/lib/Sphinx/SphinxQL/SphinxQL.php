702536148128486898b474cbfb799fe10420f0b9
marcimat@rezo.net <marcimat@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-07-26 10:08:59 +0000

    Loger quel host/port ne se connecte pas, si besoin.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@105459 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- indexer/trunk/lib/Sphinx/SphinxQL/SphinxQL.php
+++ indexer/trunk/lib/Sphinx/SphinxQL/SphinxQL.php
@@ -21,11 +21,12 @@ class SphinxQL {
 		try {
 			$this->sql = new \MySQLi($this->host, null, null, null, $this->port);
 		} catch (\Exception $e) {
+			spip_log('Connexion erronnée pour host : ' . $this->host . ' , port: ' . $this->port, 'indexer.' . _LOG_ERREUR);
 			spip_log($e->getMessage(), 'indexer.' . _LOG_ERREUR);
 			mysqli_report(MYSQLI_REPORT_OFF);
 			return false;
 		}
-		mysqli_report(MYSQLI_REPORT_OFF); 
+		mysqli_report(MYSQLI_REPORT_OFF);
 		return !!!$this->sql->connect_error;
 	}
 
