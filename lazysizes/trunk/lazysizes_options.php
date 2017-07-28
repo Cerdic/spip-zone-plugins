1fc3729596a22922a258d70ad8e2974905c066f5
arnaud.berard@mister-graphx.com <arnaud.berard@mister-graphx.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-07-28 07:09:30 +0000

    on désactive la protection interdire_script dans l'espace privé, pour pouvoir utiliser les modèles et que les contenu apparaissent en preview de l'editeur
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@105484 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- lazysizes/trunk/lazysizes_options.php
+++ lazysizes/trunk/lazysizes_options.php
@@ -4,6 +4,12 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
 
+// globale filtrer_javascript :
+// https://core.spip.net/projects/spip/repository/entry/spip/ecrire/inc/texte.php#L142
+// -1 : protection dans l'espace privé et public
+// 0  : protection dans l'espace public
+// 1  : aucune protection
+$GLOBALS['filtrer_javascript'] = 0;
 
 if (defined('_LAZYSIZES_AUTH_DEMO')?
 		_LAZYSIZES_AUTH_DEMO
