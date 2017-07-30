60377c5fb96de95cc69cbd0ff2bae86d37a1c0b8
arnaud.berard@mister-graphx.com <arnaud.berard@mister-graphx.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-07-30 09:22:07 +0000

    v4.0.0.11 correctif pour la prévisualisation dans l'espace privé - necessite htmlpurifier - surcharge de la fonction safehtml_dist - prise en charge des elements html5 dans la config de htmlpurifier figure,figcaption,… cf: https://github.com/lukusw/php-htmlpurfier-html5/blob/master/htmlpurifier_html5.php - le modele picture tiens compte des definitions d'attributs configurable
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@105529 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ lazysizes/trunk/inc/safehtml.php
@@ -0,0 +1,80 @@
+<?php
+
+/***************************************************************************\
+ *  SPIP, Systeme de publication pour l'internet                           *
+ *                                                                         *
+ *  Copyright (c) 2001-2017                                                *
+ *  Arnaud Martin, Antoine Pitrou, Philippe Riviere, Emmanuel Saint-James  *
+ *                                                                         *
+ *  Ce programme est un logiciel libre distribue sous licence GNU/GPL.     *
+ *  Pour plus de details voir le fichier COPYING.txt ou l'aide en ligne.   *
+\***************************************************************************/
+
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+function inc_safehtml($t) {
+	static $purifier;
+
+	include_spip('inc/memoization');
+	if (function_exists('cache_get')
+	AND $a = cache_get($cle = 'safehtml:'.md5($t)))
+		return $a;
+
+	include_spip('lib/HTMLPurifier.standalone');
+	
+
+	$config = HTMLPurifier_Config::createDefault();
+	$config->set('Cache.SerializerPath', preg_replace(',/$,', '', realpath(_DIR_TMP)));
+	$def = $config->getHTMLDefinition(true);
+	
+	// HTML5
+	// https://github.com/lukusw/php-htmlpurfier-html5/blob/master/htmlpurifier_html5.php
+	// http://developers.whatwg.org/grouping-content.html
+    $def->addElement('figure', 'Block', 'Optional: (figcaption, Flow) | (Flow, figcaption) | Flow', 'Common');
+    $def->addElement('figcaption', 'Inline', 'Flow', 'Common');
+	
+	// http://developers.whatwg.org/the-video-element.html#the-video-element
+    $def->addElement('video', 'Block', 'Optional: (source, Flow) | (Flow, source) | Flow', 'Common', array(
+      'src' => 'URI',
+      'type' => 'Text',
+      'width' => 'Length',
+      'height' => 'Length',
+      'poster' => 'URI',
+      'preload' => 'Enum#auto,metadata,none',
+      'controls' => 'Bool',
+    ));
+	
+    $def->addElement('source', 'Block', 'Flow', 'Common', array(
+      'src' => 'URI',
+      'type' => 'Text',
+    ));
+	
+	// http://developers.whatwg.org/text-level-semantics.html
+    $def->addElement('s',    'Inline', 'Inline', 'Common');
+    $def->addElement('var',  'Inline', 'Inline', 'Common');
+    $def->addElement('sub',  'Inline', 'Inline', 'Common');
+    $def->addElement('sup',  'Inline', 'Inline', 'Common');
+    $def->addElement('mark', 'Inline', 'Inline', 'Common');
+    $def->addElement('wbr',  'Inline', 'Empty', 'Core');
+	
+	
+	$def->addAttribute('img', 'data-src', 'URI');
+	$def->addAttribute('img', 'data-sizes', 'CDATA');
+	
+	//var_dump($def);
+	if (!isset($purifier))
+		$purifier = new HTMLPurifier($config);
+	
+	// HTML Purifier prefere l'utf-8
+	if ($GLOBALS['meta']['charset'] == 'utf-8')
+		$t = $purifier->purify($t);
+	else
+		$t = unicode_to_charset($purifier->purify(charset2unicode($t)));
+
+	if (function_exists('cache_set'))
+		cache_set($cle, $t);
+
+	return $t;
+
+}
