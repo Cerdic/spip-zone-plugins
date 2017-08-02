75db053ee3fffc497a5d65a37a9887562063bb60
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-08-02 03:33:34 +0000

    [Salvatore] [source:_plugins_/reservations_multiples/trunk/lang/ reservations_multiples] Export depuis http://trad.spip.net de la langue en
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@105596 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- reservations_multiples/trunk/lang/reservations_multiples_en.php
+++ reservations_multiples/trunk/lang/reservations_multiples_en.php
@@ -12,21 +12,32 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// C
 	'cfg_titre_parametrages' => 'Paramétrages',
 
+	// E
+	'explication_multiple_inscriptions' => 'Possibility to specify the number of bookings for each event.',
+	'explication_multiple_personnes' => 'For each additional person you will be able to specify its personal details.',
+	'explication_quantite' => 'If empty, quantity will be 1',
+
 	// L
 	'label_ajouter' => 'Add',
 	'label_enlever' => 'Remove',
-	'label_reserver_amie' => 'I would like to register one more person:', # MODIF
+	'label_multiple_inscriptions' => 'Allows to specify the the number of bookings',
+	'label_multiple_personnes' => 'Allows to book for several persons.',
+	'label_reserver_amie' => 'I would like to register one more person:',
 
 	// M
 	'message_ok_reservations_pour' => 'Réservé pour : @noms@',
 
 	// N
-	'nombre_limite' => 'Nombre limites',
-	'nombre_limite_explication' => 'Si vide aucune limitation',
+	'nom_reservation_multiple_simple' => 'Multiple Booking simple',
+	'nombre_limite' => 'Limited number',
+	'nombre_limite_explication' => 'No limitation if empty',
 
 	// R
-	'reservations_multiples_titre' => 'Réservations multiples',
+	'reservation_supplementaire' => 'Additional booking:',
+	'reservations_multiples_titre' => 'Multiple bookings',
+	'reservations_supplementaires' => 'Additional bookings:',
 
 	// T
-	'titre_page_configurer_reservations_multiples' => 'Réservations multiples'
+	'titre_page_configurer_reservations_multiples' => 'Multiple bookings',
+	'type_lien_multiple_personnes' => 'Multiple booking, original booking: @reference@'
 );
