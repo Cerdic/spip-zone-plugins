174d130e521333728f2e50f74fe724d2176380f1
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-08-10 19:40:24 +0000

    Pétouilles de phpdoc et autre fin de php.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@105707 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- n-core/trunk/lang/paquet-ncore_fr.php
+++ n-core/trunk/lang/paquet-ncore_fr.php
@@ -1,7 +1,9 @@
 <?php
 // Ceci est un fichier langue de SPIP -- This is a SPIP language file
 
-if (!defined('_ECRIRE_INC_VERSION')) return;
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
@@ -9,4 +11,3 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'ncore_description' => 'Framework de gestion de noisettes. Ce plugin fournit les mécanismes permettant de lister et de compiler les noisettes.',
 	'ncore_slogan' => 'Un cœur fondant aux noisettes',
 );
-?>
--- n-core/trunk/ncore/ncore.php
+++ n-core/trunk/ncore/ncore.php
@@ -81,11 +81,12 @@ function ncore_noisette_lister_signatures($service) {
  * 		Tableau associatif à 3 entrées fournissant les descriptions des noisettes nouvelles, obsolètes et modifiées:
  * 		- `obsoletes` : liste des identifiants de noisette devenus obsolètes
  * 		- `modifiees` : liste des descriptions des noisettes dont le fichier YAML a été modifié
- *		- `nouvelles` : liste des descriptions de nouvelles noisettes. Cet index est le seul fourni si l'indicateur
- * 		                $recharger est à `true`
+ *      - `nouvelles` : liste des descriptions de nouvelles noisettes.
+ * 		Si $recharger est à `true`, seul l'index `nouvelles` est fourni dans le tableau $noisettes.
+ *
  * @param bool		$recharger
- *      Indique si le chargement en cours est forcé ou pas. Cela permet à la fonction N-Core ou du service
- *        concerné d'optimiser le traitement sachant que seules les noisettes nouvelles sont fournies.
+ *      Indique si le chargement en cours est forcé ou pas. Cela permet à la fonction N-Core ou au service
+ *      concerné d'optimiser le traitement sachant que seules les noisettes nouvelles sont fournies.
  *
  * @return bool
  * 		`true` si le traitement s'est bien déroulé, `false` sinon.
