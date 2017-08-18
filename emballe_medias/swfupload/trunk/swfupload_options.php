da7a81eb0ffd62ea32169ebe11de51d84c2517dd
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-08-18 13:20:42 +0000

    Devraient passer en 3.1.x si non, ben on débuguera
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@105833 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- emballe_medias/swfupload/trunk/swfupload_options.php
+++ emballe_medias/swfupload/trunk/swfupload_options.php
@@ -1,7 +1,7 @@
 <?php
 
-if (!defined("_ECRIRE_INC_VERSION")) return;
+if (!defined("_ECRIRE_INC_VERSION")) {
+    return;
+}
 
 define('_DIR_LIB_SWFUPLOAD',_DIR_RACINE.'lib/SWFUpload_v2.2.0.1_Core/');
-
-?>
