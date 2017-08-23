bbc6461e697fecc7a5e00c8be04d471dfe243397
p@henix.be <p@henix.be@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-08-23 16:38:28 +0000

    Charger les scripts de foundation avec la librairie jQl (spip 3.1 seulement).
    Dans le cas ou jQl est activé globalement, on référence juste les scripts 
    pour les intégrer directement.
    
    En plus du gain de performance, cela permet de corrigé le bug du plugin foundation "sticky" qui fonctionnait 
    aléatoirement en utilisant $.getScript.
    
    Dans le cas de SPIP 3.0, on continue d'utiliser $.getScript, faute de mieux.
    
    On fait un up de Y car ce n'est pas annodin comme changement.
    
    
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@105911 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- foundation_6/trunk/inc/foundation.php
+++ foundation_6/trunk/inc/foundation.php
@@ -60,3 +60,42 @@ function class_grid_foundation($nombre_colonnes, $type) {
 		return $type.'-'.$nombre_colonnes.' ';
 	}
 }
+
+/**
+ * Utiliser jQl pour charger les scripts de foundation
+ *
+ * @param array $files les fichiers à envoyée dans jQl
+ * @access public
+ */
+function jQlfoundation($files) {
+	$js = '';
+
+	// Dans le cas ou jQl n'est pas activé pour tout les scripts
+	if (!defined('_JS_ASYNC_LOAD') and !test_espace_prive()) {
+		// On cherche dans un premier temps si jQl existe
+		lire_fichier(find_in_path('lib/jQl/jQl.min.js'), $jQl);
+		if ($jQl) {
+			// Inclure les librairie utile du compresseur
+			include_spip('inc/compresseur_concatener');
+			include_spip('inc/compresseur_minifier');
+
+			// traitement des fichiers, on concatène et minifie le tout
+			$foundation_js = concatener_fichiers($files);
+			$foundation_js = minifier($foundation_js[0]);
+
+			// charger et utiliser jQl sur les fichiers de foundation
+			$js = "<script type=\"text/javascript\">\n".$jQl."\n";
+			$js .= 'jQl.loadjQ("'.trim($foundation_js).'");'."\n";
+			$js .= '</script>';
+		}
+	} elseif (defined('_JS_ASYNC_LOAD') and !test_espace_prive()) {
+		// Dans le cas ou jQl est activé pour tout les scripts, on va simplement
+		// les référencés de manière "classique" pour qu'ils soit intégrer par
+		// le compresseur.
+		foreach ($files as $file) {
+			$js .= '<script type="text/javascript" src="'.trim($file).'"/>';
+		}
+	}
+
+	return $js;
+}
