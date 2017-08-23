0081382e09c5117df2c04854354e14b2f7d6d007
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-08-23 15:38:35 +0000

    Évitons un warning PHP sur une constante non définie
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@105910 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- menu_langues_liens/mll_options.php
+++ menu_langues_liens/mll_options.php
@@ -1,5 +1,8 @@
 <?php
 if (!defined("_ECRIRE_INC_VERSION")) return;
+if (!defined("_MLL_LANGUES_MULTILINGUE")) {
+	define("_MLL_LANGUES_MULTILINGUE", NULL);
+}
 
 $GLOBALS['forcer_lang'] = true;
 ?>
