bda341403869480f61853ed629d24a1522a1c41d
marcimat@rezo.net <marcimat@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-08-28 21:24:59 +0000

    Si aucune couleur n’est indiqué par défaut pour l’input de coloration (ie: value=''), alors le nouveau picker écrit en rgb().
    Cela change le comportemant de l’ancienne palette, alors on remet quelque chose de plus proches qu’avant :
    - si l’input n’a pas de valeur au chargement (et pas de transparence demandée) alors on écrit en hexadécimal.
    - sinon, on laisse faire la lib, qui analyse la couleur par défaut, et produira le même type de sortie, 
    sauf si la couche alpha est demandée, ça sort du rgba() dans ce cas je crois tout le temps.
    
    (merci nicod_)
    
    
    
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@105970 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- palette/trunk/javascript/palette.js
+++ palette/trunk/javascript/palette.js
@@ -11,10 +11,16 @@ jQuery(function($){
 			var opt = options;
 			if (me.hasClass('withalpha') || me.data('palette-withalpha')) {
 				opt.opacity = true;
+			} else if (!me.val()) {
+				// #hex par défaut si aucune valeur à l’origine, et pas de couche alpha
+				opt.renderCallback = function($elm, toggled) {
+					if (!this.color.options.opacity) {
+						$elm.val('#' + this.color.colors.HEX);
+					}
+				};
 			}
 			me.colorPicker(opt);
 		});
-		
 	}
 	coloriagePalette();
 	onAjaxLoad(coloriagePalette);
