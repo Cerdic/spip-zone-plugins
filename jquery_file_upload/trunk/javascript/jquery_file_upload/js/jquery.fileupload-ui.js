d6b5b5cd919bbcfe9af41a9d487d2f6b4eb2c381
brunobergot@gmail.com <brunobergot@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-08-30 14:56:45 +0000

    report de r105983 / compat SPIP 3.1
    on ne touche pas au numéro de version, qui est synchro sur celui de la lib (à confirmer)
    
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@105985 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- jquery_file_upload/trunk/javascript/jquery_file_upload/js/jquery.fileupload-ui.js
+++ jquery_file_upload/trunk/javascript/jquery_file_upload/js/jquery.fileupload-ui.js
@@ -84,7 +84,7 @@
             // widget (via file input selection, drag & drop or add API call).
             // See the basic file upload widget for more information:
             add: function (e, data) {
-                var that = $(this).data('fileupload'),
+                var that = $(this).data('blueimpUI-fileupload') || $(this).data('fileupload'),
                     options = that.options,
                     files = data.files;
                 $(this).fileupload('process', data).done(function () {
@@ -110,7 +110,7 @@
             },
             // Callback for the start of each file upload request:
             send: function (e, data) {
-                var that = $(this).data('fileupload');
+                var that = $(this).data('blueimpUI-fileupload') || $(this).data('fileupload');
                 if (!data.isValidated) {
                     if (!data.maxNumberOfFilesAdjusted) {
                         that._adjustMaxNumberOfFiles(-data.files.length);
@@ -139,7 +139,7 @@
             },
             // Callback for successful uploads:
             done: function (e, data) {
-                var that = $(this).data('fileupload'),
+                var that = $(this).data('blueimpUI-fileupload') || $(this).data('fileupload'),
                     template;
                 if (data.context) {
                     data.context.each(function (index) {
@@ -189,7 +189,7 @@
             },
             // Callback for failed (abort or error) uploads:
             fail: function (e, data) {
-                var that = $(this).data('fileupload'),
+                var that = $(this).data('blueimpUI-fileupload') || $(this).data('fileupload'),
                     template;
                 if (data.maxNumberOfFilesAdjusted) {
                     that._adjustMaxNumberOfFiles(data.files.length);
@@ -272,7 +272,7 @@
             },
             // Callback for uploads start, equivalent to the global ajaxStart event:
             start: function (e) {
-                var that = $(this).data('fileupload');
+                var that = $(this).data('blueimpUI-fileupload') || $(this).data('fileupload');
                 that._transition($(this).find('.fileupload-progress')).done(
                     function () {
                         that._trigger('started', e);
@@ -281,7 +281,7 @@
             },
             // Callback for uploads stop, equivalent to the global ajaxStop event:
             stop: function (e) {
-                var that = $(this).data('fileupload');
+                var that = $(this).data('blueimpUI-fileupload') || $(this).data('fileupload');
                 that._transition($(this).find('.fileupload-progress')).done(
                     function () {
                         $(this).find('.progress')
@@ -294,7 +294,7 @@
             },
             // Callback for file deletion:
             destroy: function (e, data) {
-                var that = $(this).data('fileupload');
+                var that = $(this).data('blueimpUI-fileupload') || $(this).data('fileupload');
                 if (data.url) {
                     $.ajax(data);
                     that._adjustMaxNumberOfFiles(1);
