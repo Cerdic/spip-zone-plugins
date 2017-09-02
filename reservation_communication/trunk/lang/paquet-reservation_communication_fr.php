54a3242f494ec880bec345e9d411b51121864ff5
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-09-02 03:32:52 +0000

    [Salvatore] [source:_plugins_/reservation_communication/trunk/lang/ paquet-reservation_communication] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@106038 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- reservation_communication/trunk/lang/paquet-reservation_communication_fr.php
+++ reservation_communication/trunk/lang/paquet-reservation_communication_fr.php
@@ -1,14 +1,14 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
-if (!defined('_ECRIRE_INC_VERSION')) return;
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/reservation_communication/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// R
 	'reservation_communication_description' => 'Permet de communique plus facilement avec vos clients',
 	'reservation_communication_nom' => 'Réservation Comunications',
-	'reservation_communication_slogan' => 'Communiquer avec vos clients',
+	'reservation_communication_slogan' => 'Communiquer avec vos clients'
 );
-
-?>
