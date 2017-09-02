ac59b07045addd96515cc5d253b26d2d4fe45ee7
erational@erational.org <erational@erational.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-09-02 11:48:04 +0000

    sitemap un complet comprenant les images d'apres les travaux originaux d'orsal https://www.orsal.fr/Sitemap-avec-images-pour-SPIP
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@106047 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ sitemap_images/trunk/lang/paquet-sitemap_images_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// S
+	'sitemap_images_description' => 'Remplace le sitemap fourni par défaut dans SPIP par un sitemap plus complet comprenant les images du site',
+	'sitemap_images_nom' => 'Sitemap images',
+	'sitemap_images_slogan' => 'Sitemap comprenant les images',
+);
