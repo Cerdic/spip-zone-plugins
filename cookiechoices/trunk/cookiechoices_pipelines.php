350541211939423cefcdaddfe55cfc23d2ad92b4
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-09-08 07:18:42 +0000

    Un peu de PSR
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@106146 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- cookiechoices/trunk/cookiechoices_pipelines.php
+++ cookiechoices/trunk/cookiechoices_pipelines.php
@@ -4,9 +4,9 @@
  *
  */
 
-if (!defined("_ECRIRE_INC_VERSION")) return;
-
-
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
 /**
  * Inserer le javascript de cookiechoices
@@ -15,19 +15,22 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
  * @return mixed
  */
 function cookiechoices_affichage_final($page){
-    include_spip('inc/filtres');
-    $lang = extraire_attribut($page, 'lang');
-    if (!$lang) $lang = $GLOBALS["spip_lang"];
-    $js_cookiechoices = produire_fond_statique("cookiechoices_call.js", array("lang"=>$lang));
+	include_spip('inc/filtres');
+	$lang = extraire_attribut($page, 'lang');
+	if (!$lang) {
+		$lang = $GLOBALS["spip_lang"];
+	}
+	$js_cookiechoices = produire_fond_statique("cookiechoices_call.js", array('lang' => $lang));
 
-    $script .=
-               "<script type='text/javascript' src='".find_in_path('js/cookiechoices.js')."'></script>\n"
-             . "<script type='text/javascript' src='$js_cookiechoices'></script>\n";
+	$script .=
+		"<script type='text/javascript' src='".find_in_path('js/cookiechoices.js')."'></script>\n"
+		. "<script type='text/javascript' src='$js_cookiechoices'></script>\n";
 
-    $pos_body = strpos($page, '</body>');
-    if ($pos_body > 0)// pour placer le script uniquement sur les pages HTML
-          return substr_replace($page, $script, $pos_body, 0);
+	$pos_body = strpos($page, '</body>');
+	if ($pos_body > 0) {// pour placer le script uniquement sur les pages HTML
+		return substr_replace($page, $script, $pos_body, 0);
+	}
 
-    return $page;
+	return $page;
 }
 
