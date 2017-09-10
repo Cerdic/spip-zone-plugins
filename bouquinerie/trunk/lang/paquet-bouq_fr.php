d7ac3056031f061bc19081b0c60afe49fcc52396
peetdu@gmail.com <peetdu@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-09-10 12:50:55 +0000

    ajout d’une description du plugin. En espérant que cela débloque aussi l’affichage du slogan (?!)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@106172 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- bouquinerie/trunk/lang/paquet-bouq_fr.php
+++ bouquinerie/trunk/lang/paquet-bouq_fr.php
@@ -8,7 +8,13 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// B
-	'bouq_description' => '',
 	'bouq_nom' => 'Bouquinerie',
 	'bouq_slogan' => 'Gestion des livres et de leurs auteurs',
+	'bouq_description' => 'Ce plugin se veut être le plus petit dénominateur commun pour une gestion de livres et de leurs auteurs. Il devrait pouvoir être utile aux cas suivants :
+
+		- Maison d’édition de livre,
+		- Librairie,
+		- Bibliothèque,
+		- Boutique en ligne de vente de livre,
+		- etc.',
 );
