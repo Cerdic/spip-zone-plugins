bbc792a7c5003a920172990132ad6e430fe5f539
p@henix.be <p@henix.be@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-09-10 11:59:46 +0000

    Bon j'en ai marre de chipotter le chargement Async. Cela ne fonctionne pas bien avec foundation.
    Du coup, on fait un truc bien moche pour charger les scripts après le </body>.
    On renomme les fichiers en conséquent.
    
    
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@106168 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- foundation_6/trunk/foundation_6_pipelines.php
+++ foundation_6/trunk/foundation_6_pipelines.php
@@ -13,22 +13,7 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
 
-/*
- *   Pipeline Insert_head
- */
-function foundation_6_insert_head($flux) {
-
-	// Si on est en mode app, on revoie le bon squelette
-	if (_FOUNDATION_SASS) {
-		$flux .= recuperer_fond('inclure/head-foundation-app');
-	} else {
-		$flux .= recuperer_fond('inclure/head-foundation');
-	}
-
-	return $flux;
-}
-
-/*
+/**
  *   Pipeline Insert_head_css
  */
 function foundation_6_insert_head_css($flux) {
@@ -38,6 +23,32 @@ function foundation_6_insert_head_css($flux) {
 		$flux .= recuperer_fond('inclure/css/head-foundation-app');
 	} else {
 		$flux .= recuperer_fond('inclure/css/head-foundation');
+	}
+
+	return $flux;
+}
+
+/**
+ * Pipeline affichage_final
+ * On insert foundation a la fin du body des pages du site
+ *
+ * @param string $flux html de la page
+ * @access public
+ * @return string
+ */
+function foundation_6_affichage_final($flux) {
+	include_spip('inc/config');
+	if (lire_config('foundation_6/javascript')) {
+		if (_FOUNDATION_SASS) {
+			$js = recuperer_fond('inclure/js-foundation-app');
+		} else {
+			$js = recuperer_fond('inclure/js-foundation');
+		}
+		$pos_body = strpos($flux, '</body>');
+
+		if ($pos_body) {
+			return substr_replace($flux, $js, $pos_body, 0);
+		}
 	}
 
 	return $flux;
