efae25f3c38e5edd26d3b7550b819c72cae50069
jrynik@yahoo.co.uk <jrynik@yahoo.co.uk@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-09-11 03:32:59 +0000

    [Salvatore] [source:_plugins_/verifier/lang/ verifier] Export depuis http://trad.spip.net de la langue sk
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@106195 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- verifier/lang/verifier_sk.php
+++ verifier/lang/verifier_sk.php
@@ -11,12 +11,12 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// E
 	'erreur_code_postal' => 'Toto PSČ je neplatné.',
-	'erreur_comparaison_egal' => 'Hodnota sa musí zhodovať s poľom "@nom_champ@"', # MODIF
-	'erreur_comparaison_egal_type' => 'Hodnota musí sa musí zhodovať s poľom "@nom_champ@" a musí byť rovnakého typu', # MODIF
-	'erreur_comparaison_grand' => 'Hodnota musí byť väčšia ako pole "@nom_champ@"', # MODIF
-	'erreur_comparaison_grand_egal' => 'Hodnota musí byť väčšia alebo rovná poľu "@nom_champ@"', # MODIF
-	'erreur_comparaison_petit' => 'Hodnota musí byť menšia ako pole "@nom_champ@"', # MODIF
-	'erreur_comparaison_petit_egal' => 'Hodnota musí byť menšia alebo rovná ako "@nom_champ@"', # MODIF
+	'erreur_comparaison_egal' => 'Hodnota musí byť rovná poľu "@nom_champ@".',
+	'erreur_comparaison_egal_type' => 'Hodnota musí byť rovná poľu"@nom_champ@" a musí byť rovnakého typu.',
+	'erreur_comparaison_grand' => 'Hodnota musí byť väčšia ako pole "@nom_champ@"',
+	'erreur_comparaison_grand_egal' => 'Hodnota musí byť väčšia alebo rovná ako pole "@nom_champ@"',
+	'erreur_comparaison_petit' => 'Hodnota musí byť menšia ako pole "@nom_champ@".',
+	'erreur_comparaison_petit_egal' => 'Hodnota musí byť menšia alebo rovná ako "@nom_champ@"',
 	'erreur_couleur' => 'Kód farby nie je platný.',
 	'erreur_date' => 'Tento dátum je neplatný.',
 	'erreur_date_format' => 'Formát dátumu je neplatný.',
@@ -26,13 +26,13 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'erreur_email_nondispo' => 'E-mailová adresa <em>@email@</em> sa už používa.',
 	'erreur_entier' => 'Hodnota musí byť celé číslo.',
 	'erreur_entier_entre' => 'Hodnota musí byť medzi @min@ a @max@.',
-	'erreur_entier_max' => 'Hodnota musí byť menšia ako @max@.', # MODIF
-	'erreur_entier_min' => 'Hodnota musí byť väčšia ako @min@.', # MODIF
+	'erreur_entier_max' => 'Hodnota musí byť menšia alebo rovná ako @max@.',
+	'erreur_entier_min' => 'Hodnota musí byť väčšia alebo rovná ako @min@.',
 	'erreur_heure' => 'Zadaný čas neexistuje.',
 	'erreur_heure_format' => 'Formát času, ktorý ste zadali, nie je akceptovaný.',
 	'erreur_id_document' => 'Tento identifikátor dokumentu nie je platný.',
 	'erreur_inconnue_generique' => 'Formát nie je správny.',
-	'erreur_isbn' => 'Číslo ISBN nie je platné (napr. 978-2-1234-5680-3 alebo 2-1234-5680-X)', # MODIF
+	'erreur_isbn' => 'Číslo ISBN nie je platné (napr.: 978-2-1234-5680-3 alebo 2-1234-5680-X)',
 	'erreur_isbn_13_X' => 'Číslo ISBN-13 sa nemôže končiť písmenom X.',
 	'erreur_isbn_G' => 'Musí sa začínať číslami 978 alebo 979.',
 	'erreur_isbn_nb_caracteres' => 'Číslo ISBN musí mať 10 alebo 13 znakov okrem spojovníkov (teraz má @nb@).',
@@ -40,6 +40,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'erreur_isbn_segment' => 'V skupine "@segment@" je naviac @nb@ číslo/čísel.',
 	'erreur_isbn_segment_lettre' => 'V skupine "@segment@" nesmie byť žiadne písmeno.',
 	'erreur_numerique' => 'Formát čísla je neplatný.',
+	'erreur_objet' => 'Tento objekt je neplatný',
 	'erreur_regex' => 'Reťazec pravidelného výrazu má nesprávny formát.',
 	'erreur_siren' => 'IČO je neplatné.',
 	'erreur_siret' => 'Obchodné meno s IČOm je neplatné.',
@@ -49,8 +50,8 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'erreur_taille_min' => 'Údaj nesmie mať menej ako @min@ znakov (teraz má @nb@).',
 	'erreur_telephone' => 'Telefónne číslo je neplatné.',
 	'erreur_url' => 'Internetová adresa <em>@url@</em> je neplatná.',
-	'erreur_url_protocole' => 'Zadaná adresa <em>(@url@)</em> sa musí začínať s @protocole@', # MODIF
-	'erreur_url_protocole_exact' => 'Zadaná adresa <em>(@url@)</em> sa nezačína s platným protokolom (napr. http://)', # MODIF
+	'erreur_url_protocole' => 'Zadaná adresa <em>(@url@)</em> sa musí začínať @protocole@',
+	'erreur_url_protocole_exact' => 'Zadaná adresa <em>(@url@)</em> sa nezačína platným protokolom (napr. http://)',
 
 	// N
 	'normaliser_option_date' => 'Štandardizovať dátum?',
@@ -58,10 +59,21 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'normaliser_option_date_en_datetime' => 'Vo formáte "Dátum a čas" (pre SQL)',
 
 	// O
+	'option_code_postal_pays_label' => 'Krajina',
+	'option_comparaison_champ_champ_label' => 'Pole',
+	'option_comparaison_champ_comparaison_label' => 'Porovnávanie',
+	'option_comparaison_champ_egal' => '== Rovná sa',
+	'option_comparaison_champ_egal_type' => '=== identický (rovnaký typ)',
+	'option_comparaison_champ_grand' => '> väčší',
+	'option_comparaison_champ_grand_egal' => '>= väčší alebo rovný',
+	'option_comparaison_champ_nom_champ_label' => 'Názov poľa',
+	'option_comparaison_champ_petit' => '< menší',
+	'option_comparaison_champ_petit_egal' => '<= menší alebo rovný',
 	'option_couleur_normaliser_label' => 'Štandardizovať kód farby?',
 	'option_couleur_type_hexa' => 'Kód farby v hexadecimálnom (šetnástkovom) formáte',
 	'option_couleur_type_label' => 'Typ overenia, ktorý sa má použiť',
 	'option_decimal_nb_decimales_label' => 'Počet desatinných miest',
+	'option_decimal_separateur_label' => 'Oddeľovač desatinných miest',
 	'option_email_disponible_label' => 'Dostupná adresa',
 	'option_email_disponible_label_case' => 'Skontrolujte, či túto adresu už nepoužíva iný používateľ',
 	'option_email_mode_5322' => 'Skontrolovať podľa najprísnejších dostupných štandardov',
@@ -70,6 +82,10 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'option_email_mode_strict' => 'Prísnejšia kontrola',
 	'option_entier_max_label' => 'Maximálna hodnota',
 	'option_entier_min_label' => 'Minimálna hodnota',
+	'option_fichiers_hauteur_max_label' => 'Maximálna šírka obrázka (v px)',
+	'option_fichiers_largeur_max_label' => 'Maximálna veľkosť obrázka (v px)',
+	'option_fichiers_mime_label' => 'Typ Mime a prípona',
+	'option_id_objet_objet_label' => 'Názov objektu (v jednotnom čísle)',
 	'option_regex_modele_label' => 'Hodnota sa musí zhodovať s týmto výrazom',
 	'option_siren_siret_mode_label' => 'Určite to chcete potvrdiť?',
 	'option_siren_siret_mode_siren' => 'IČO',
@@ -89,6 +105,8 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'option_url_type_protocole_web' => 'Internetové protokoly: HTTP alebo HTTPS',
 
 	// T
+	'type_code_postal' => 'PSČ',
+	'type_comparaison_champ' => 'Porovnanie',
 	'type_couleur' => 'Farba',
 	'type_couleur_description' => 'Skontroluje, či je hodnota zadaný kód farby.',
 	'type_date' => 'Dátum',
@@ -101,6 +119,10 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'type_email_disponible_description' => 'Skontrolujte, či e-mailovú adresu už nepoužíva iný používateľ systému.',
 	'type_entier' => 'Celé číslo',
 	'type_entier_description' => 'Skontroluje, či je hodnotou celé číslo s možnosťou obmedziť ju dvoma hraničnými hodnotami.',
+	'type_fichiers' => 'Vlastnosti súboru',
+	'type_id_document' => 'Číslo dokumentu',
+	'type_id_objet' => 'Číslo objektu',
+	'type_isbn' => 'Číslo ISBN',
 	'type_regex' => 'Pravidelný výraz',
 	'type_regex_description' => 'Skontrolujte, že hodnota sa zhoduje so zadaným výrazom. Viac informácií o používaní pravidelných výrazov nájdete v <a href="http://fr2.php.net/manual/en/reference.pcre.pattern.syntax.php">internetovompomocníkovi k PHP.</a>',
 	'type_siren_siret' => 'IČO alebo IČO a obchodné meno',
