7a75c0c209d4d7e5672fc7ab68d5d18d87b66a1a
filipaun@gmail.com <filipaun@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-09-12 03:32:52 +0000

    [Salvatore] [source:_plugins_/emballe_medias/emballe_medias/trunk/lang/ paquet-emballe_medias] Export depuis http://trad.spip.net de la langue pt_br
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@106209 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ emballe_medias/emballe_medias/trunk/lang/paquet-emballe_medias_pt_br.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-emballe_medias?lang_cible=pt_br
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// E
+	'emballe_medias_description' => 'Formulário configurável que embala mídias a partir do espaço público...',
+	'emballe_medias_nom' => 'Embala Mídias',
+	'emballe_medias_slogan' => 'Embalar seus documentos conformemente'
+);
