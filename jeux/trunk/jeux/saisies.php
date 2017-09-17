57c2cb18ad5b68c68b32c1eef2de78f57c3bc2dd
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-09-17 10:39:36 +0000

    les saisies du plugins jeux ne sont pas les saisies du plugins saisies. On préfixe pour éviter les conflits (cf https://contrib.spip.net/Des-jeux-dans-vos-articles#forum493411) on en profite pour supprimer le tag php de fermeture
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@106317 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- jeux/trunk/jeux/saisies.php
+++ jeux/trunk/jeux/saisies.php
@@ -6,14 +6,14 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 #---------------------------------------------------#
 #  Plugin  : jeux                                   #
 #  Auteur  : Patrice Vanneufville, 2006             #
-#  Contact : patrice¡.!vanneufville¡@!laposte¡.!net #
+#  Contact : patriceï¿½.!vanneufvilleï¿½@!laposteï¿½.!net #
 #  Licence : GPL                                    #
 #--------------------------------------------------------------------------#
 #  Documentation : https://contrib.spip.net/Des-jeux-dans-vos-articles  #
 #--------------------------------------------------------------------------#
 /*
 
-Insere élément de formulaire dans vos articles !
+Insere ï¿½lï¿½ment de formulaire dans vos articles !
 ------------------------------------------------
 
 separateurs obligatoires : [texte], [saisie]
@@ -59,10 +59,10 @@ Syntaxe a utiliser apres [saisie] (tous les parametres sont optionnels) :
 	...
 	(value = Votre texte par defaut, pour certains types de saisie)
 	- nom par defaut : saisie
-Le nom : 
+Le nom :
 	un mot sans espace compose de lettres/chiffres/:._-
 Les types reconnus sont les type HTML :
-	button, checkbox, color, date, datetime, datetime-local 
+	button, checkbox, color, date, datetime, datetime-local
 	email, file, hidden, image, month, number, password
 	radio, range, search, select, tel, text, textarea, time, url, week
 	- types non reconnus : submit reset
@@ -82,57 +82,57 @@ function jeux_saisies_init() {
 	";
 }
 
-function saisies_trouve_texte($type, $texte) {
-	if(!in_array($type, array('button', 'checkbox', 'color', 'date', 'datetime', 'datetime-local', 
-		'email', /*'file', */'hidden', 'image', 'month', 'number', 'password', 'radio', 'range', 
+function jeux_saisies_trouve_texte($type, $texte) {
+	if(!in_array($type, array('button', 'checkbox', 'color', 'date', 'datetime', 'datetime-local',
+		'email', /*'file', */'hidden', 'image', 'month', 'number', 'password', 'radio', 'range',
 		/*'select', */'search', 'tel', 'text', 'textarea', 'time', 'url', 'week'))) return '';
 	$texte = preg_split(',\bvalue *=,', $texte, 2);
 	return isset($texte[1])?trim($texte[1]):'';
 }
 
-function saisies_trouve_nom($nom, $defaut='saisie') {
+function jeux_saisies_trouve_nom($nom, $defaut='saisie') {
 	$nom = trim($nom);
 	return preg_match(',[a-z][a-z0-9:_\.\-]*,i', $nom)?$nom:$defaut;
 }
 
-function saisies_trouve_type($type, $defaut='text') {
+function jeux_saisies_trouve_type($type, $defaut='text') {
 	$type = trim($type);
-	return in_array($type, array('button', 'checkbox', 'color', 'date', 'datetime', 'datetime-local', 
-		'email', 'file', 'hidden', 'image', 'month', 'number', 'password', 'radio', 'range', 
+	return in_array($type, array('button', 'checkbox', 'color', 'date', 'datetime', 'datetime-local',
+		'email', 'file', 'hidden', 'image', 'month', 'number', 'password', 'radio', 'range',
 		'select', 'search', 'tel', 'text', 'textarea', 'time', 'url', 'week'))?$type:$defaut;
 }
 
-function saisies_label($texte) {
+function jeux_saisies_label($texte) {
  $texte = explode(' ', trim($texte), 2);
- $for = saisies_trouve_nom($texte[0]);
- return isset($texte[1])?'<div class="saisies_label"><label for="'.$for.'">'.$texte[1].'</label></div>':'';
+ $for = jeux_saisies_trouve_nom($texte[0]);
+ return isset($texte[1])?'<div class="jeux_saisies_label"><label for="'.$for.'">'.$texte[1].'</label></div>':'';
 }
 
-function saisies_saisie(&$saisies, $indexJeux, $texte) {
+function jeux_saisies_saisie(&$jeux_saisies, $indexJeux, $texte) {
  static $indexSaisie = 0;
  $lignes = preg_split("/[\r\n]+/", trim($texte));
  $texte = explode(' ', trim($lignes[0]), 3); unset($lignes[0]);
- $type = isset($texte[1])?saisies_trouve_type($texte[1],''):'';
- if(!$type) { 
- 	if($type = saisies_trouve_type($texte[0], '')) {
+ $type = isset($texte[1])?jeux_saisies_trouve_type($texte[1],''):'';
+ if(!$type) {
+ 	if($type = jeux_saisies_trouve_type($texte[0], '')) {
 		$texte[0] = ''; $nom = 'saisie';
 	} else {
-		$type = 'text'; $nom = saisies_trouve_nom($texte[0], '');
+		$type = 'text'; $nom = jeux_saisies_trouve_nom($texte[0], '');
 		if($nom) $texte[0] = ''; else $nom = 'saisie';
 	}
  } else {
-	$nom = saisies_trouve_nom($texte[0]);
+	$nom = jeux_saisies_trouve_nom($texte[0]);
 	$texte[0] = $texte[1] = '';
  }
- if(isset($saisies[$nom])) { $nom = "$type / $nom"; $type = 'erreur'; }
+ if(isset($jeux_saisies[$nom])) { $nom = "$type / $nom"; $type = 'erreur'; }
  $texte = trim(join(' ', $texte)); if($texte) $texte = ' '.$texte;
  if($correction = jeux_form_correction($indexJeux)) {
  	$value = jeux_form_reponse($indexJeux, $indexSaisie, $nom);
-	$saisies[$nom] = $nom.':'.(is_array($value)?join(' / ', $value):$value);
+	$jeux_saisies[$nom] = $nom.':'.(is_array($value)?join(' / ', $value):$value);
 	$disabled = ' disabled="disabled"';
  } else {
- 	$value = saisies_trouve_texte($type, join("\n", $lignes));
-	$saisies[$nom] = $disabled = '';
+ 	$value = jeux_saisies_trouve_texte($type, join("\n", $lignes));
+	$jeux_saisies[$nom] = $disabled = '';
  }
  list($idInput, $nameInput) = jeux_idname($indexJeux, $indexSaisie++, $nom);
 
@@ -148,13 +148,13 @@ function saisies_saisie(&$saisies, $indexJeux, $texte) {
 		foreach($lignes as $l) {
  			$l = explode('=', $l, 2); list($a, $b) = array(trim($l[0]), trim($l[1]));
 			$checked = ($correction && ( (is_array($value) && in_array($a, $value)) || $a==$value))
-				?' selected="selected"':'';			
+				?' selected="selected"':'';
 			$res .= isset($l[1])?'<option value="'.attribut_html($a)."\"$checked>".$b.'</option>'
 				:("<option$checked>".trim($l[0]).'</option>');
 		}
 		$res .= '</select>';
         break;
-    case 'radio': 
+    case 'radio':
     case 'checkbox':
 		$tmp = '<input type="'.$type.'" name="'.$nameInput.($type=='checkbox'?'[]':'')."\"$disabled value=\"";
 		$res = array(); $indexCB = 0;
@@ -162,7 +162,7 @@ function saisies_saisie(&$saisies, $indexJeux, $texte) {
  			$l = explode('=', $l, 2); $id = $idInput.'-'.$indexCB++;
 			if(!isset($l[1])) $l[1] = $l[0]; list($a, $b) = array(trim($l[0]), trim($l[1]));
 			$checked = ($correction && ( (is_array($value) && in_array($a, $value)) || $a==$value))
-				?' checked="checked"':'';			
+				?' checked="checked"':'';
 			$res[] = $tmp.attribut_html($a).'" id="'.$id.'"'.$checked.'><label style="display:inline;" for="'.$id.'">'.$b.'</label>';
 		}
 		$res = join('<br/>', $res);
@@ -171,34 +171,34 @@ function saisies_saisie(&$saisies, $indexJeux, $texte) {
 		if($value) $value = ' value="' . attribut_html($value).'"';
 		$res = "<input type=\"$type\" id=\"$idInput\" name=\"$nameInput\"$disabled$value$texte>";
   }
- return "<div class='saisies_saisie'>$res</div>";
+ return "<div class='jeux_saisies_saisie'>$res</div>";
 }
 
 // traitement du jeu : jeu_{mon_jeu}()
 function jeux_saisies($texte, $indexJeux, $form=true) {
 	// initialisation
-	$saisies_[$indexJeux] = array();
-	$saisies = &$saisies_[$indexJeux];
+	$jeux_saisies_[$indexJeux] = array();
+	$jeux_saisies = &$jeux_saisies_[$indexJeux];
 	$titre = $html = false;
 	$id_jeu = _request('id_jeu');
 
 	// parcours des [separateurs]
-	$tableau = jeux_split_texte('saisies', $texte); 
+	$tableau = jeux_split_texte('jeux_saisies', $texte);
 	foreach($tableau as $i => $valeur) if ($i & 1) {
 	 if ($valeur==_JEUX_TITRE) $titre = $tableau[$i+1];
-	  elseif ($valeur==_JEUX_TEXTE && strlen($tableau[$i+1])) $html .= '<div class="saisies_texte">'.$tableau[$i+1].'</div>';
-	  elseif ($valeur==_JEUX_LABEL) $html .= saisies_label($tableau[$i+1]);
-	  elseif ($valeur==_JEUX_SAISIE) $html .= saisies_saisie($saisies, $indexJeux, $tableau[$i+1]);
+	  elseif ($valeur==_JEUX_TEXTE && strlen($tableau[$i+1])) $html .= '<div class="jeux_saisies_texte">'.$tableau[$i+1].'</div>';
+	  elseif ($valeur==_JEUX_LABEL) $html .= jeux_saisies_label($tableau[$i+1]);
+	  elseif ($valeur==_JEUX_SAISIE) $html .= jeux_saisies_saisie($jeux_saisies, $indexJeux, $tableau[$i+1]);
 	}
-	
+
 	// calcul des extremes
-	$tete = '<div class="jeux_cadre saisies">'.($titre?'<div class="jeux_titre saisies_titre">'.$titre.'<hr /></div>':'');
+	$tete = '<div class="jeux_cadre jeux_saisies">'.($titre?'<div class="jeux_titre jeux_saisies_titre">'.$titre.'<hr /></div>':'');
 	$pied = '';
 	if(jeux_form_correction($indexJeux)) {
-		// mode correction 
+		// mode correction
 		if($form) $pied .= jeux_bouton(jeux_config('bouton_refaire'), $id_jeu, $indexJeux);
 		// stockage des resultats, mais sans les afficher
-		jeux_afficher_score(0, 0, $id_jeu, join(" | ", $saisies));
+		jeux_afficher_score(0, 0, $id_jeu, join(" | ", $jeux_saisies));
 	} else {
 		// mode formulaire
 		if($form) {
@@ -209,4 +209,3 @@ function jeux_saisies($texte, $indexJeux, $form=true) {
 
 	return $tete.$html.$pied.'</div>';
 }
-?>
