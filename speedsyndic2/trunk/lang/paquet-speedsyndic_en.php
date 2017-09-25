31d9c34526c6786568f604a2e464ffb9211bc87d
erational@erational.org <erational@erational.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-09-25 16:56:41 +0000

    et hop le plugin pour SPIP3
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@106418 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ speedsyndic2/trunk/lang/paquet-speedsyndic_en.php
@@ -0,0 +1,12 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+if (!defined("_ECRIRE_INC_VERSION")) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// S
+	'speedsyndic_description' => 'This plugins allows for a faster syndication of certain selected feeds.',
+);
