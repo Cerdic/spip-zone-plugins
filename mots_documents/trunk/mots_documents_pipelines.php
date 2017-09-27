dadea8acee8ff3c80732afe69d828d1d8b4faf8a
tcharlss@bravecassine.com <tcharlss@bravecassine.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-09-27 11:24:43 +0000

    un peu de ménage, expression régulière plus précise + up de y
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@106443 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- mots_documents/trunk/mots_documents_pipelines.php
+++ mots_documents/trunk/mots_documents_pipelines.php
@@ -23,16 +23,12 @@ function mots_documents_recuperer_fond($flux){
 	if ($flux['args']['fond'] == 'prive/squelettes/inclure/mediatheque-navigation'
 		and sql_countsel('spip_groupes_mots', 'tables_liees LIKE '.sql_quote('%documents%'))
 	) {
-		// Option 1 : une nouvelle barre d'onglets (mais ça fait beaucoup quand même)
-		//$navigation_mots = recuperer_fond('prive/squelettes/inclure/mediatheque-navigation-mots', $flux['args']['contexte'])
-		//$cherche = "/(<\/div>)(\s+<div class=[\"']nettoyeur[\"']>)/is"; // FIXME : à améliorer, très spécifique au markup quand même
-		//$remplace = "$1$navigation_mots$2";
-		//$flux['data']['texte'] = preg_replace($cherche, $remplace, $flux['data']['texte']);
 
-		// Option 2 : un simple select ajouté dans les onglets existants
-		$select_mots = recuperer_fond('prive/squelettes/inclure/mediatheque-navigation-mots-select', $flux['args']['contexte']);
-		$cherche = "/(<\/ul>\s*)(<\/div>)/is"; // FIXME hum hum... sans marqueur où parseur HTML c'est compliqué de s'insérer où on veut
-		$remplace = "$1$select_mots$2";
+		$fond_mots = recuperer_fond('prive/squelettes/inclure/mediatheque-navigation-mots', $flux['args']['contexte']);
+		// On s'insère après le dernier <ul> de la barre d'onglets secondaires
+		// Bon, sans parseur, c'est la galère
+		$cherche = "#<ul\s+class=[\"']sanstitre[\"']>\s*(?:<li[^>]*>(?!.*<li>).*?</li>\s*)+\s*</ul>#i";
+		$remplace = "$0$fond_mots";
 		$flux['data']['texte'] = preg_replace($cherche, $remplace, $flux['data']['texte']);
 	}
 	return $flux;
