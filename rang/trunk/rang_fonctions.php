2430a9cff7e08ac12e20da90a06c0fe219419fab
peetdu@gmail.com <peetdu@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-10-01 21:39:45 +0000

    Surcharge d’une fonction du plugin Mots
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@106483 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- rang/trunk/rang_fonctions.php
+++ rang/trunk/rang_fonctions.php
@@ -43,3 +43,43 @@ function rang_objet_dans_config($objet) {
 	$liste = explode(',', lire_config('rang/rang_objets'));
 	return in_array($table, $liste);
 }
+
+/**
+ * SURCHARGE :Compte le nombre d'objets associés pour chaque type d'objet, liés
+ * à un mot clé donné.
+ *
+ * @pipeline_appel afficher_nombre_objets_associes_a
+ *
+ * @param int $id_mot
+ *     Identifiant du mot clé
+ * @param int $id_groupe
+ *     Identifiant du groupe parent
+ * @return string[]
+ *     Tableau de textes indiquant le nombre d'éléments tel que '3 articles'
+ **/
+function objets_associes_mot($id_mot, $id_groupe) {
+	static $occurrences = array();
+
+	// calculer tous les liens du groupe d'un coup
+	if (!isset($occurrences[$id_groupe])) {
+		$occurrences[$id_groupe] = calculer_utilisations_mots($id_groupe);
+	}
+
+	$associes = array();
+	$tables = lister_tables_objets_sql();
+	foreach ($tables as $table_objet_sql => $infos) {
+		$nb = (isset($occurrences[$id_groupe][$table_objet_sql][$id_mot]) ? $occurrences[$id_groupe][$table_objet_sql][$id_mot] : 0);
+		if ($nb) {
+			$associes[] = objet_afficher_nb($nb, $infos['type']);
+		}
+	}
+
+	$associes = pipeline(
+		'afficher_nombre_objets_associes_a',
+		array('args' => array('objet' => 'mot', 'id_objet' => $id_mot),
+		'data' => $associes)
+	);
+
+	return $associes;
+
+}
