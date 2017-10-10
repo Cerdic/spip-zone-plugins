a3d8e70d76a4920d64b6518e3e85924416507fe0
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-10-10 09:52:09 +0000

    Inclure inc/config lorsque l'on en a besoin
    Lorsque l'on transforme un png en jpg car plus adéquat (opération très lourde car double compression etc...), on conserve le png quand même quitte à avoir double cache.
    
    Ainsi au recalcul, alors que l'image ne doit pas être recréée normalement, on en dispose toujours.
    
    On vérifie dans ce cas là si une version jpg existe, si oui, c'est celle ci à utiliser.
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@106709 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- smush_images/trunk/inc/smush_image.php
+++ smush_images/trunk/inc/smush_image.php
@@ -61,7 +61,6 @@ function inc_smush_image_dist($im) {
  * 		Le nouveau tag image
  */
 function image_smush($im) {
-	include_spip('inc/config');
 	$fonction = array('smush', array());
 	$image = _image_valeurs_trans($im, 'smush', false, $fonction);
 
@@ -75,6 +74,8 @@ function image_smush($im) {
 	// resultat plus beau, mais tres lourd
 	// Et: indispensable pour preserver transparence!
 	if (file_exists($image['fichier']) && $creer) {
+		include_spip('inc/config');
+
 		$im = $image['fichier'];
 		$dest = $image['fichier_dest'];
 
@@ -104,6 +105,7 @@ function image_smush($im) {
 		}
 
 		if ($format == 'PNG') {
+			$dest_jpg = $file_size_jpg = false;
 			/**
 			 * On est sur un PNG
 			 */
@@ -114,6 +116,9 @@ function image_smush($im) {
 			 */
 			if (count($out_alpha) == 0) {
 				$dest_jpg = $tmp.'.jpg';
+				/**
+				 * Conversion en jpg vers dest_jpg
+				 */
 				exec($magick.'convert '.$im.' '.$dest_jpg);
 				if (lire_config('jpegoptim_casse', 'oui') != 'oui') {
 					$compression = '';
@@ -131,7 +136,7 @@ function image_smush($im) {
 				$file_size_jpg = filesize($dest_jpg);
 			}
 			/**
-			 * Conversion en PNG
+			 * Optimisation du PNG
 			 */
 			$nq = substr($im, 0, -4).'-nq8.png';
 			if (file_exists($dest)) {
@@ -143,7 +148,9 @@ function image_smush($im) {
 			 * Comparaison des version jpg et png si cela est le cas
 			 */
 			if (isset($dest_jpg) AND $dest_jpg && $file_size_jpg && file_exists($dest) && filesize($dest) > $file_size_jpg) {
-				spip_unlink($dest);
+				/**
+				 * Si on garde dest_jpg, on ne supprime pas dest pour ne pas la recalculer plus tard
+				 */
 				$im = $dest = $dest_jpg;
 			} elseif (isset($dest_jpg) AND $dest_jpg && file_exists($dest_jpg)) {
 				spip_unlink($dest_jpg);
@@ -190,13 +197,21 @@ function image_smush($im) {
 		$image_compare = isset($im_original) ? $im_original : $im;
 		if (!file_exists($dest) || (filesize($dest) > filesize($image_compare))) {
 			spip_unlink($dest);
-			spip_log('Image de même taille - On retourne un vieux cache '.$image['fichier_dest'], 'images');
+			spip_log('Smush - Image de même taille - On retourne un vieux cache '.$image['fichier_dest'], 'images');
 			@copy($image_compare, $dest);
 		}
 	} else {
-		spip_log('On retourne un vieux cache '.$image['fichier_dest'], 'images');
-		@copy($image['fichier'], $image['fichier_dest']);
-		return _image_ecrire_tag($image, array('src' => $image['fichier_dest']));
+		/**
+		 * Si c'est un png et que l'on a encore la dernière version optimisée (on ne doit donc pas créer)
+		 * On vérifie que l'on n'a pas un jpg plus léger qui traine,
+		 * si oui, on utilise le jpg
+		 */
+		if ($image['format_source'] == 'png' && file_exists(rtrim($image['fichier_dest'],'.png').'.jpg')) {
+			spip_log('Smush - On utilise un jpg ici à la place du jpg','images');
+			$src = rtrim($image['fichier_dest'],'.png').'.jpg';
+			$im = _image_ecrire_tag($image, array('src' => $src));
+		}
+		return $im;
 	}
 	return _image_ecrire_tag($image, array('src' => $dest));
 }
