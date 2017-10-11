b23a4359a4a7093fccf9c67e16c83e15747a9f75
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-10-11 19:59:57 +0000

    Bien utilisée l'url en paramètre s'il y en a une dans $url = parametre_url(_request('url'), 'var_mode', '');
    Eviter les &amp; dans les URLS 
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@106760 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- shortcut_url/trunk/action/api_shortcut_url_creer.php
+++ shortcut_url/trunk/action/api_shortcut_url_creer.php
@@ -15,9 +15,9 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 function action_api_shortcut_url_creer($url = false) {
 	if (autoriser('creer', 'shortcuturl')) {
 		if (!$url) {
-			$url = _request('url');
+			$url = urldecode(_request('url'));
 		}
-		$url = parametre_url(_request('url'), 'var_mode', '');
+		$url = parametre_url($url, 'var_mode', '', '&');
 		if (!$url or $url == './') {
 			header('Content-Type: application/json');
 			die(json_encode(array('error' => '405', 'message' => 'Missing URL parameter')));
@@ -34,7 +34,7 @@ function action_api_shortcut_url_creer($url = false) {
 					die(json_encode(array('url' => url_absolue($shortcut_url), 'new' => false)));
 				} else {
 					$editer_objet = charger_fonction('editer_objet', 'action');
-					list($id, $err) = $editer_objet('new', 'shortcut_url', $set);
+					list($id, $err) = $editer_objet('new', 'shortcut_url', array('url' => $url));
 					include_spip('inc/invalideur');
 					suivre_invalideur(0);
 					header('Content-Type: application/json');
