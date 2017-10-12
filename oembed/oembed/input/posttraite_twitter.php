819657bb13b78517d0b90038c90ff57e68387b31
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-10-12 18:59:59 +0000

    Recuperer l'image principale du touit pour afficher en tete de la pseudo twitter card qu'on genere en blockquote (mais ca alourdit le processus et ca risque de casser a la prochaine maj de twitter
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@106795 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- oembed/oembed/input/posttraite_twitter.php
+++ oembed/oembed/input/posttraite_twitter.php
@@ -13,5 +13,30 @@ function oembed_input_posttraite_twitter_dist($data) {
 
 	$data['html'] = trim(preg_replace(',<script[^>]*></script>,i', '', $data['html']));
 
+	// verifier l'URL du tweet si on peut trouver une image dans les og:image
+	// pour afficher en tete de card
+	if ($res = recuperer_url($data['url'], array('taille_max'=>16384))
+	  and $res['page']) {
+		$metas = extraire_balises($res['page'], 'meta');
+		$src = '';
+		$has_image = false;
+		foreach ($metas as $meta) {
+			$property = extraire_attribut($meta, 'property');
+			if ($property == 'og:image') {
+				$src = extraire_attribut($meta, 'content');
+			}
+			if ($property == 'og:image:user_generated' and extraire_attribut($meta, 'content')==='true') {
+				$has_image = true;
+			}
+			if ($has_image and $src) {
+				break;
+			}
+		}
+		if ($has_image and $src) {
+			$src = str_replace(':large', ':small', $src);
+			$data['html'] = "<img src='$src' class='thumbnail p' />" . $data['html'];
+		}
+	}
+
 	return $data;
 }
