ac8b9f0c52ae1058ddae91a05d007e8d7eb7a832
abelass <abelass@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-10-13 07:16:50 +0000

    decimal au lieu de float
    - todo actualiser la bd verifier que cela ne casse rien
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@106808 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- prix_objets/trunk/base/prix_objets.php
+++ prix_objets/trunk/base/prix_objets.php
@@ -3,9 +3,9 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 
 function prix_objets_declarer_tables_interfaces($tables_interfaces){
 
-    $tables_interfaces['table_des_tables']['prix_objets'] = 'prix_objets';
-    
-    return $tables_interfaces;
+		$tables_interfaces['table_des_tables']['prix_objets'] = 'prix_objets';
+
+		return $tables_interfaces;
 }
 
 function prix_objets_declarer_tables_principales($tables_principales){
@@ -13,23 +13,23 @@ function prix_objets_declarer_tables_principales($tables_principales){
 		"id_prix_objet" 	=> "bigint(21) NOT NULL",
 		"id_objet" 	=> "bigint(21) NOT NULL",
 		"titre"   => "varchar(255)  DEFAULT '' NOT NULL",
-		"reference"   => "varchar(255)  DEFAULT '' NOT NULL",		
-		'objet' => 'varchar(25) not null default ""',		
+		"reference"   => "varchar(255)  DEFAULT '' NOT NULL",
+		'objet' => 'varchar(25) not null default ""',
 		"code_devise" 	=> "varchar(3) NOT NULL",
-		"prix_ht" 		=> "float (38,2) NOT NULL",
-        "prix"       => "float (38,2) NOT NULL",	
-        "taxe"   => "varchar(10)  DEFAULT '' NOT NULL",        	
+		"prix_ht" 		=> "decimal(15,4) NOT NULL DEFAULT '0.000'",
+		"prix"       => "decimal(15,4) NOT NULL DEFAULT '0.000'",
+		"taxe"   => "varchar(10)  DEFAULT '' NOT NULL",
 		);
-	
+
 	$spip_prix_objets_key = array(
 		"PRIMARY KEY" 	=> "id_prix_objet",
 		"KEY id_objet"	=> "id_objet",
 		);
-		
+
 	$spip_prix_objets_join = array(
 		"id_prix_objet"	=> "id_prix_objet",
 		"id_objet"	=> "id_objet",
-		"id_objet"	=> "id_article",		
+		"id_objet"	=> "id_article",
 		);
 
 	$tables_principales['spip_prix_objets'] = array(
@@ -37,9 +37,9 @@ function prix_objets_declarer_tables_principales($tables_principales){
 		'key' => &$spip_prix_objets_key,
 		'join' => &$spip_prix_objets_join
 	);
-	
+
 	return $tables_principales;
-	
+
 	}
 
 
