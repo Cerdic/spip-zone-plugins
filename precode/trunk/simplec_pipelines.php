c8365d2f5cafb00dcebf5f6e3bfddad007d19af4
nicolas.dorigny@gmail.com <nicolas.dorigny@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-10-15 16:50:33 +0000

    On ajoute la lib JS de clipboard au chargement (du coup on utilise plutôt le pipeline insert_head), et le crédit pour la lib.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@106887 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- precode/trunk/simplec_pipelines.php
+++ precode/trunk/simplec_pipelines.php
@@ -1,8 +1,9 @@
 <?php
 
-function simplec_insert_head_css($flux) {
+function simplec_insert_head($flux) {
 	$flux .= '<link rel="stylesheet" href="'.find_in_path('css/simplec.css').'" type="text/css" media="all" />';
+	$flux .= '<script type="text/javascript" src="'.find_in_path('js/clipboard.min.js').'"></script>';
+	$flux .= '<script type="text/javascript" src="'.find_in_path('js/simplec.js').'"></script>';
 	return $flux;
 }
 
-?>
