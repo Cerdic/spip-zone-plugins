55720bf45f63d007c02f8fd46fc713a117faf905
nicolas.dorigny@gmail.com <nicolas.dorigny@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-10-15 16:50:33 +0000

    On ajoute le code JS qui copie/colle. Ça fonctionne pour démo sur programmer.spip, avec le markup de coloration_code : à tester / finaliser
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@106888 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ precode/trunk/lang/simplec_fr.php
@@ -0,0 +1,12 @@
+<?php
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'bouton_copier_aria_label' => 'copier le code du bloc',
+	'bouton_copier_label' => 'Copier',
+);
