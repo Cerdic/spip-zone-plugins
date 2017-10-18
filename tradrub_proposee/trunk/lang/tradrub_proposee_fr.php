ca2e9cb0131255fafe36d09a92682e9e8c8ad08b
kent1@arscenic.info <kent1@arscenic.info@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-10-18 19:17:28 +0000

    Les infos pour le paquet.xml et le lien vers la doc proposée
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@107037 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- tradrub_proposee/trunk/lang/tradrub_proposee_fr.php
+++ tradrub_proposee/trunk/lang/tradrub_proposee_fr.php
@@ -1,6 +1,5 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans https://forge.diplomatie.gouv.fr/isvn/DCP/trunk/plugins_spip/fdiplo_squelettes/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
@@ -11,5 +10,5 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'info_raccourcis' => 'Raccourcis :',
 	'label_interdit_meme_rubrique' => 'Interdire la création de traduction dans la même rubrique',
 	'label_interdit_meme_secteur' => 'Interdire la création de traduction dans le même secteur (un secteur par langue)',
-	'titre_tradrub_proposee' => 'TradRub proposée',
+	'titre_tradrub_proposee' => 'Proposition de rubriques pour les traduction',
 );
