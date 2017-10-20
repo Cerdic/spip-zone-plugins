b7a421d202743251f7bae0e9c57ee0f0a00db1e2
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-10-20 08:02:03 +0000

    une branche pour la v1, en attendant la v2 pour SPIP 3.2
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@107065 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ agenda_fullcalendar_facile/branches/v1/lang/paquet-fullcalendar_facile_fr.php
@@ -0,0 +1,12 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_core_/plugins/organiseur/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// O
+	'fullcalendar_facile_description' => 'Un modèle et c\'est tout',
+	'fullcalendar_facile_slogan' => 'Un calendrier de manière simple'
+);
+
