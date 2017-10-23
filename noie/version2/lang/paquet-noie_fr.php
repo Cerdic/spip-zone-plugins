442bf8194ab418cdcb5de06601adcc9742c25fa2
guillaume.wauquier@declic3000.com <guillaume.wauquier@declic3000.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-10-23 15:49:24 +0000

    Ajout des fichier de traduction paquet et du logo au format png
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@107120 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ noie/version2/lang/paquet-noie_fr.php
@@ -0,0 +1,12 @@
+<?php
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// N
+	'noie_description' => 'Affichez un gentil message aux internautes surfant avec un vieil Internet Explorer afin de les pr&#233;venir qu\'ils ne visitent pas le site dans les meilleures conditions.',
+	'noie_nom' => 'Changez de navigateur !',
+	'noie_slogan' => 'Changez de navigateur pour mieux voir le Web !'
+);
