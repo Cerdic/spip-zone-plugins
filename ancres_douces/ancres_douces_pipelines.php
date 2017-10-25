5c38e9de7ca1a1edba6275e78634f5ef3649f2bb
marcimat@rezo.net <marcimat@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-10-25 11:34:20 +0000

    Cosmétique du code.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@107160 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- ancres_douces/ancres_douces_pipelines.php
+++ ancres_douces/ancres_douces_pipelines.php
@@ -4,37 +4,40 @@ if (!defined("_ECRIRE_INC_VERSION")) return;	#securite
 
 // Pour par exemple limiter l'action de ancres_douces au div #contenu ou aux divs de classe .ancres_douces,
 // inserez dans votre config/mes_options.php les lignes :
-// define ('ANCRES_DOUCES_CONTEXTE','#contenu'); 
+// define ('CONTEXTE_ANCRES_DOUCES','#contenu');
 // ou 
-// define ('ANCRES_DOUCES_CONTEXTE','.ancres_douces'); 
+// define ('CONTEXTE_ANCRES_DOUCES','.ancres_douces');
 
 function ancresdouces_insert_head($flux){
 
-if (!defined('CONTEXTE_ANCRES_DOUCES'))
-	$appel_ancres_douces='jQuery';
-else $appel_ancres_douces='$(\''.CONTEXTE_ANCRES_DOUCES.'\')';
+	if (!defined('CONTEXTE_ANCRES_DOUCES')) {
+		$appel_ancres_douces = 'jQuery';
+	} else {
+		$appel_ancres_douces='$(\'' . CONTEXTE_ANCRES_DOUCES . '\')';
+	}
 
-// Inclusion des scripts jquery
-$flux .= '<script src="'.url_absolue(find_in_path("js/jquery.scrollto.js")).'" type="text/javascript"></script>';
-$flux .= '<script src="'.url_absolue(find_in_path("js/jquery.localscroll.js")).'" type="text/javascript"></script>';
+	// Inclusion des scripts jquery
+	$flux .= '<script src="'.url_absolue(find_in_path("js/jquery.scrollto.js")).'" type="text/javascript"></script>';
+	$flux .= '<script src="'.url_absolue(find_in_path("js/jquery.localscroll.js")).'" type="text/javascript"></script>';
 
-// Code d'init
-$flux .= '<script type="text/javascript">/* <![CDATA[ */
-function ancre_douce_init() {if(typeof jQuery.localScroll=="function")'.$appel_ancres_douces.'.localScroll({hash:true,onAfter:function( anchor, settings ){
+	// Code d'init
+	$flux .= '<script type="text/javascript">/* <![CDATA[ */
+function ancre_douce_init() {
+	if (typeof jQuery.localScroll=="function") {
+		' . $appel_ancres_douces . '.localScroll({hash:true, onAfter:function(anchor, settings){ 
 			jQuery(anchor).attr(\'tabindex\', -1).on(\'blur focusout\', function () {
-    
-                // when focus leaves this element, 
-                // remove the tabindex attribute
-                jQuery(this).removeAttr(\'tabindex\');
-    
-            }).focus();
-		}});}
-if(window.jQuery)jQuery(document).ready(function() {
+				// when focus leaves this element, 
+				// remove the tabindex attribute
+				jQuery(this).removeAttr(\'tabindex\');
+			}).focus();
+		}});
+	}
+}
+if (window.jQuery) jQuery(document).ready(function() {
 	ancre_douce_init();
 	onAjaxLoad(ancre_douce_init);
 });
 /* ]]> */</script>';
 
-return $flux;
+	return $flux;
 }
-?>
