17b9c5060d3568da3bdc00fe1550ba33656bbffa
bystrano@gmx.ch <bystrano@gmx.ch@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-10-29 13:22:25 +0000

    retire un log qui provoquait un warning
    et commente les autres logs
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@107200 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- requeteursql/inc/pipeline_affiche_fiche_objet.php
+++ requeteursql/inc/pipeline_affiche_fiche_objet.php
@@ -7,16 +7,15 @@
  */
 
 function requeteursql_afficher_fiche_objet($flux) {
-	spip_log($flux);
+	// spip_log($flux);
 	if ($flux['args']['type']=='sql_requete') {
 		$id_sql_requete = _request('id_sql_requete');
 		$result = sql_select('requetesql', 'spip_sql_requetes', "id_sql_requete = $id_sql_requete");
-		spip_log($sql);
 		if ($res = sql_fetch($result)) {
 			$sql = $res['requetesql'];
 			if ($res = sql_query("$sql LIMIT 0,100")) {
 				$aRes = sql_fetch_all($res);
-				spip_log($aRes);
+				// spip_log($aRes);
 				$flux['data'] .= recuperer_fond(
 					'prive/squelettes/fiche_objet/sql_requete',
 					array('res' => $aRes),
