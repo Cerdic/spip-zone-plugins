1894617c30c6e8b7a8cd2d210b800aef9659981f
bystrano@gmx.ch <bystrano@gmx.ch@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-10-29 13:22:25 +0000

    éviter une notice
    ce test d'autorisation ne sert à rien, la variable $id_sql_requete n'est pas
    définie.
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@107201 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- requeteursql/formulaires/editer_sql_requete_tables.php
+++ requeteursql/formulaires/editer_sql_requete_tables.php
@@ -1,12 +1,9 @@
 <?php
 
 function formulaires_editer_sql_requete_tables_charger_dist() {
-	if (!autoriser('modifier', 'sqlrequete', $id_sql_requete)) {
-		$valeurs['editable'] = false;
-	} else {
-		$valeurs['tables'] = sql_alltable('%');
-		$valeurs['table'] =_request('table');
-	}
+  $valeurs['tables'] = sql_alltable('%');
+  $valeurs['table'] =_request('table');
+
   return $valeurs;
 }
 
