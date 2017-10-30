5d6d0e2e9a55232b58aac1099c77d783c098ae45
labergerie@lesmoutonssauvages.com <labergerie@lesmoutonssauvages.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-10-30 15:11:57 +0000

    Plug en test & lang plus complets & inclure des noisettes & gérer les inclusions dans les articles et bénéficier des documents joints
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@107222 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- swiper/trunk/lang/swiper_fr.php
+++ swiper/trunk/lang/swiper_fr.php
@@ -10,13 +10,18 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
 	'ajouter_lien_swiper' => 'Ajouter ce swiper',
+	//
 
 	// C
 	'champ_titre_label' => 'Titre',
+	'cfg_titre_parametrages' => 'Options',
 	'confirmer_supprimer_swiper' => 'Confirmez-vous la suppression de ce swiper ?',
+	'couleur' => 'Couleur',
+	'couleur_bleu' => 'Bleu',
+	'couleur_blanc' => 'Blanc',
 
-	'nom_site'=>'Nom du lien',
-	'url_site'=>'Lien',
+
+
 
 	// I
 	'icone_creer_swiper' => 'Créer un swiper',
@@ -26,12 +31,21 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'info_nb_swipers' => '@nb@ swiper',
 	'info_swipers_auteur' => 'Les swiper de cet auteur',
 
+	// N
+	'navigation' => 'Navigation (Flèches)',
+	'nom_site'=>'Nom du lien',
+
+	// P
+	'pagination' => 'Pagination (Points)',
+
 	// R
 	'retirer_lien_swiper' => 'Retirer ce swiper',
 	'retirer_tous_liens_swipers' => 'Retirer tous les swiper',
 
 	// S
+	'scrollbar' => 'Scrollbar (Barre de défilement)',
 	'supprimer_swiper' => 'Supprimer ce swiper',
+	'swiper_options' => 'Options manuelles',
 
 	// T
 	'texte_ajouter_swiper' => 'Ajouter un swiper',
@@ -41,7 +55,11 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'titre_langue_swiper' => 'Langue de ce swiper',
 	'titre_logo_swiper' => 'Logo de ce swiper',
 	'titre_objets_lies_swiper' => 'Liés à ce swiper',
+	'titre_page_configurer_swiper' => 'Configuration de vos swipers',
 	'titre_swiper' => 'Swiper',
 	'titre_swipers' => 'Swiper',
 	'titre_swipers_rubrique' => 'Swiper de la rubrique',
+
+	//
+	'url_site'=>'Lien',
 );
