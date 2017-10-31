336fd599b506040fac5b9ef48bb0b328f4e4034c
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-10-31 09:40:48 +0000

    Dans BootStrap3 la classe hide masque avec un important et ne permet plus de reveler le formulaire, on la retire donc par precaution
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@107229 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- recommander/javascript/recommander.js
+++ recommander/javascript/recommander.js
@@ -1,6 +1,6 @@
 jQuery(function(){
 	(function ($) {
-	$("div#formulaire_recommander").hide().css("height","");
+	$("div#formulaire_recommander").hide().css("height","").removeClass('hide');
 	$("#recommander>h2, #recommander_bouton").click(function(){
 		$("div#formulaire_recommander:visible").slideUp("slow");
 		$("div#formulaire_recommander:hidden").slideDown("slow");
