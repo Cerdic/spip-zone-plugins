960658439dea797e656e59e476a3ff2e6a0dbd95
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-11-09 14:34:35 +0000

    Formatage et indentation PSR SPIP
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@107433 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- vider_rubrique/action/vider_rubrique.php
+++ vider_rubrique/action/vider_rubrique.php
@@ -8,11 +8,13 @@
  *                                                                         *
  *  Ce programme est un logiciel libre distribue sous licence GNU/GPL.     *
  *  Pour plus de details voir le fichier COPYING.txt ou l'aide en ligne.   *
-\***************************************************************************/
+ * \***************************************************************************/
 
-if (!defined('_ECRIRE_INC_VERSION')) return;
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
-include_spip('inc/charsets');	# pour le nom de fichier
+include_spip('inc/charsets');    # pour le nom de fichier
 
 /**
  * Effacer le contenu d'une rubrique
@@ -20,8 +22,8 @@ include_spip('inc/charsets');	# pour le nom de fichier
  * @param null $id_rubrique
  * @return void
  */
-function action_vider_rubrique_dist($arguments=null) {
-	if (is_null($arguments)){
+function action_vider_rubrique_dist($arguments = null) {
+	if (is_null($arguments)) {
 		$securiser_action = charger_fonction('securiser_action', 'inc');
 		$arguments = $securiser_action();
 	}
@@ -29,10 +31,13 @@ function action_vider_rubrique_dist($arguments=null) {
 	$message = "Suppression des articles de la rubrique ";
 	$arguments = explode(':', $arguments);
 	$id_rubrique = $arguments[0];
-	if($arguments[1]){ $vider_arbo=$arguments[1]; $message = "Suppression de l'arborescence complète de la rubrique "; }
-	spip_log($message.$id_rubrique, "vider_rubrique");
-	if (intval($id_rubrique)){
-		$contexte = array('id_rubrique'=>$id_rubrique,'vider_arbo'=>$vider_arbo);
+	if ($arguments[1]) {
+		$vider_arbo = $arguments[1];
+		$message = "Suppression de l'arborescence complète de la rubrique ";
+	}
+	spip_log($message . $id_rubrique, "vider_rubrique");
+	if (intval($id_rubrique)) {
+		$contexte = array('id_rubrique' => $id_rubrique, 'vider_arbo' => $vider_arbo);
 		$suppression = recuperer_fond("admin/vider_rubrique", $contexte);
 	}
 }
--- vider_rubrique/vider_rubrique_pipelines.php
+++ vider_rubrique/vider_rubrique_pipelines.php
@@ -5,17 +5,19 @@
  * Licence GPL (c) 2012 - Apsulis
  * Suppression de tout le contenu d'une rubrique
  *
-\***************************************************************************/
+ * \***************************************************************************/
 
-if (!defined('_ECRIRE_INC_VERSION')) return;
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
-function vider_rubrique_boite_infos($flux){
+function vider_rubrique_boite_infos($flux) {
 	include_spip('inc/config');
 	$actif = lire_config('vider_rubrique/config/activer');
 	$roles = lire_config('vider_rubrique/config/auteurs_autorises');
 	$restreindre = lire_config('vider_rubrique/config/restreindre');
 	$restreindre_val = lire_config('vider_rubrique/config/restreindre_valeur');
-	$les_rubriques = explode(',',$restreindre_val);
+	$les_rubriques = explode(',', $restreindre_val);
 	$type = $flux['args']['type'];
 
 	/*
@@ -24,11 +26,13 @@ function vider_rubrique_boite_infos($flux){
 		- etc.
 		Là c'est du mode un peu à l'arrache, mais ça fait ce qu'on demande
 	*/
-	if(autoriser("webmestre") && $actif=="oui"){
-		if (($id = intval($flux['args']['id'])) && ($type=='rubrique')) {
-			if($restreindre=="oui" && !in_array($id,$les_rubriques)) return $flux;
-			 
-			$contexte = array('id_rubrique'=>$id);
+	if (autoriser("webmestre") && $actif == "oui") {
+		if (($id = intval($flux['args']['id'])) && ($type == 'rubrique')) {
+			if ($restreindre == "oui" && !in_array($id, $les_rubriques)) {
+				return $flux;
+			}
+
+			$contexte = array('id_rubrique' => $id);
 			$flux["data"] .= recuperer_fond("noisettes/bouton_vider_rubrique", $contexte);
 		}
 	}
@@ -36,9 +40,10 @@ function vider_rubrique_boite_infos($flux){
 	return $flux;
 }
 
-function vider_rubrique_jqueryui_plugins($plugins){
-	if (test_espace_prive()){
+function vider_rubrique_jqueryui_plugins($plugins) {
+	if (test_espace_prive()) {
 		$plugins[] = "jquery.ui.dialog";
 	}
+
 	return $plugins;
 }
