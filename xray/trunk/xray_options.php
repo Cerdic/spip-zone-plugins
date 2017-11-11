77db45d1f476aa1dfbdebef7dbe0659e3e0afc36
jluc@no-log.org <jluc@no-log.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-11-11 08:26:15 +0000

    test sur l'activation de memoization et APC
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@107450 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- xray/trunk/xray_options.php
+++ xray/trunk/xray_options.php
@@ -2,8 +2,17 @@
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
-if (isset($_GET['exec']) and ($_GET['exec']=='xray')) {
-	include_once ('xray_apc.php');
-	exit;
-}
+global $Memoization;
+$cfg = @unserialize($GLOBALS['meta']['memoization']);
 
+if (isset($_GET['exec']) and ($_GET['exec']=='xray')) {
+	if ($Memoization and ($Memoization->methode == 'apc')
+	and $cfg and ($cfg['methode']=='apc')) {
+		include_once ('xray_apc.php');
+		exit;
+	}
+	else {
+		echo "Erreur : le plugin XRay nécessite d'activer le plugin memoization avec APC";
+		exit;
+	};
+}
