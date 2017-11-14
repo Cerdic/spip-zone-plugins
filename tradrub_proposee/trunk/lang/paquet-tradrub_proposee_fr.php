ff8d7b6867d29e50064cdb625056ee2e346e5925
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-11-14 17:44:46 +0000

    [Salvatore] [source:_plugins_/tradrub_proposee/trunk/lang/ paquet-tradrub_proposee] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@107563 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- tradrub_proposee/trunk/lang/paquet-tradrub_proposee_fr.php
+++ tradrub_proposee/trunk/lang/paquet-tradrub_proposee_fr.php
@@ -1,12 +1,14 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/tradrub_proposee/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+
 	// T
-	'tradrub_proposee_description' => 'Ce plugin simplifie le choix de rubrique de destination d\'une nouvelle traduction d\'article ou de rubrique.',
+	'tradrub_proposee_description' => 'Ce plugin simplifie le choix de rubrique de destination d’une nouvelle traduction d’article ou de rubrique.',
 	'tradrub_proposee_nom' => 'Proposition de rubriques pour les traduction',
 	'tradrub_proposee_slogan' => 'Faciliter le choix de rubrique de destination de traductions.'
 );
