fc0b4060bb4c47b0c75840cbdd07cb40f20f824a
klaus@spip.de <klaus@spip.de@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-11-14 17:44:44 +0000

    [Salvatore] [source:_plugins_/trad-lang/trunk/lang/ paquet-tradlang] Export depuis http://trad.spip.net de la langue de
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@107561 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ trad-lang/trunk/lang/paquet-tradlang_de.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-tradlang?lang_cible=de
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// T
+	'tradlang_description' => 'Mit diesem Plugin werden Spachdateien direkt in SPIP verwaltet.',
+	'tradlang_slogan' => 'Sprachdateien verwalten'
+);
