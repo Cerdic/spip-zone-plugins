db217b2a70534dcb8231ab64c1454d9e0211d994
filipaun@gmail.com <filipaun@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-11-14 17:43:58 +0000

    [Salvatore] [source:_plugins_/porte_plume_enluminures_typographiques/trunk/lang/ enlumtypo] Export depuis http://trad.spip.net de la langue pt_br
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@107527 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ porte_plume_enluminures_typographiques/trunk/lang/enlumtypo_pt_br.php
@@ -0,0 +1,45 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/enlumtypo?lang_cible=pt_br
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'barre_alignerdroite' => '[/Alinhar a direita/] o parágrafo',
+	'barre_alignergauche' => '[!Alinhar a esquerda!] o parágrafo',
+	'barre_avances' => 'Significado, significado!',
+	'barre_barre' => '&lt;del&gt;Tachar&lt;/del&gt; o texto',
+	'barre_boutonsavances' => 'Colocar significados adicionais, para usar com moderação e discernimento!',
+	'barre_cadre' => 'Colocar em uma &lt;cadre&gt;zona de digitação de texto&lt;/cadre&gt;',
+	'barre_centrer' => '[|Centrar|] o parágrafo',
+	'barre_code' => 'Formatar um &lt;code&gt;código informático&lt;/code&gt;',
+	'barre_encadrer' => '[(Enquadrar)] o parágrafo',
+	'barre_exposant' => 'Colocar o texto em &lt;sup&gt;sobrescrito&lt;/sup&gt;',
+	'barre_formatages_speciaux' => 'Formatação especial',
+	'barre_indice' => 'Colocar o texto em &lt;sub&gt;subescrito&lt;/sub&gt;',
+	'barre_intertitre2' => 'Transformar em {{{**intertítulo nível dois}}}',
+	'barre_intertitre3' => 'Transformar em {{{***intertítulo nível três}}}',
+	'barre_miseenevidence' => 'Colocar o texto em [*evidência*]',
+	'barre_miseenevidence2' => 'Colocar o texto em [**evidência*] (2a cor)',
+	'barre_petitescapitales' => 'Colocar o texto em &lt;sc&gt;Versalete&lt;/sc&gt;',
+	'barre_poesie' => 'Formatar como uma &lt;poesie&gt;poesia&lt;/poesie&gt;',
+	'barre_tableau' => 'Inserir/modificar (selecionar antes) uma tabela',
+	'bouton_reinitialiser' => 'Reinicializar',
+
+	// C
+	'cfg_insertcss' => 'Inserção CSS',
+	'cfg_puces' => 'Tratamento de marcadores',
+	'cfg_titraille' => 'Titraille',
+	'configuration_typoenluminee' => 'Enluminures Typographiques',
+
+	// T
+	'tableau_enregistrer' => 'Salvar',
+	'tableau_prem_ligne' => 'Primeira linha',
+	'tableau_resume' => 'Resumo',
+	'tableau_titre' => 'Título'
+);
