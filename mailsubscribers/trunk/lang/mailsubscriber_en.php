231b3acb0f15a6f3bd01809169d6af02ea7aa398
hanjo.spip@gmail.com <hanjo.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-11-14 17:44:11 +0000

    [Salvatore] [source:_plugins_/mailsubscribers/trunk/lang/ mailsubscriber] Export depuis http://trad.spip.net de la langue en
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@107536 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- mailsubscribers/trunk/lang/mailsubscriber_en.php
+++ mailsubscribers/trunk/lang/mailsubscriber_en.php
@@ -34,6 +34,8 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// E
 	'erreur_adresse_existante' => 'This email address is already in the list',
 	'erreur_technique_subscribe' => 'A technical error made impossible to record your subscription.',
+	'explication_listes_diffusion_option_defaut' => 'One or more list identifiers separated by a comma',
+	'explication_listes_diffusion_option_statut' => 'Filter the lists by status',
 	'explication_to_email' => 'People’s email to invite to subscribe to newsletter (several address separated by comma. 5 maximum emails)',
 
 	// I
@@ -63,6 +65,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'label_inscription' => 'Subscription',
 	'label_lang' => 'Language',
 	'label_listes' => 'Lists',
+	'label_listes_diffusion_option_statut' => 'Status',
 	'label_listes_import_subscribers' => 'Subscribe to lists',
 	'label_mailsubscriber_optin' => 'I want to receive the Newsletter',
 	'label_message_invite_email_subscribe' => 'Message to send with the invitation',
@@ -97,6 +100,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'texte_ajouter_mailsubscriber' => 'Add subscriber to the newsletter',
 	'texte_avertissement_import' => 'A <tt>status column</tt> is supplied, the data will be imported as is , overwriting those that may already exist for some email.',
 	'texte_changer_statut_mailsubscriber' => 'This user is subscribed to the newsletter :',
+	'texte_import_export_bonux' => 'To import or export the lists, please install plugin <a href="https://plugins.spip.net/spip_bonux">SPIP-Bonux</a>',
 	'texte_statut_en_attente_confirmation' => 'pending confirmation',
 	'texte_statut_pas_encore_inscrit' => 'not registered',
 	'texte_statut_refuse' => 'suspended',
@@ -115,12 +119,12 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'unsubscribe_deja_texte' => 'The email address @email@ is not in our mailing list.',
 	'unsubscribe_sujet_email' => '[@nom_site_spip@] Unsubscribe from Newsletter',
 	'unsubscribe_texte_confirmer_email_1' => 'Please click on button to confirm unsubscribe of email @email@: ',
-	'unsubscribe_texte_confirmer_email_liste_1' => 'Please confirm your request to remove @email@ from the list « @titre_liste@ » by clicking this button: ', # MODIF
+	'unsubscribe_texte_confirmer_email_liste_1' => 'Please confirm your request to remove @email@ from the list « @titre_liste@ » by clicking this button: ',
 	'unsubscribe_texte_email_1' => 'The email address @email@ has been removed from our mailing list.',
 	'unsubscribe_texte_email_2' => 'We hope to see you soon on @nom_site_spip@.',
 	'unsubscribe_texte_email_3' => 'In case of error, or if you change your mind, you can re-subscribe at any time using the following link :
 @url_subscribe@',
-	'unsubscribe_texte_email_liste_1' => 'Email address @email@ has been removed from our distribution list « @titre_liste@ ».', # MODIF
+	'unsubscribe_texte_email_liste_1' => 'Email address @email@ has been removed from our distribution list « @titre_liste@ ».',
 	'unsubscribe_titre_email' => 'Unsubscribe from Newsletter',
-	'unsubscribe_titre_email_liste' => 'Removal from list « <b>@titre_liste@</b> »' # MODIF
+	'unsubscribe_titre_email_liste' => 'Removal from list « <b>@titre_liste@</b> »'
 );
