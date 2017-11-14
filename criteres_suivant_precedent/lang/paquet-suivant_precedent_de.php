f072b43dc5c9e84aa1c17bc3f69c78d9b41e3feb
klaus@spip.de <klaus@spip.de@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-11-14 17:43:54 +0000

    [Salvatore] [source:_plugins_/criteres_suivant_precedent/lang/ paquet-suivant_precedent] Export depuis http://trad.spip.net de la langue de
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@107526 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ criteres_suivant_precedent/lang/paquet-suivant_precedent_de.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-suivant_precedent?lang_cible=de
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// S
+	'suivant_precedent_description' => 'Dieses Plugin experimentiert mit dem Auffinden von nachfolgenden und vorangehenden Elementen in den Ergebnissen einer Schleife. Diese Anwendungweise ist vor allem bei der Gestaltung von Navigationselementen in Rubriken- und Themen-Listen (Schlagworte) hilfreich.',
+	'suivant_precedent_nom' => 'Kriterium Nächstes / Voriges',
+	'suivant_precedent_slogan' => 'Fügt den Kriterien einer Schleife <code>{suivant}</code> (nächstes Element) und <code>{precedent}</code> (voriges Element) hinzu. '
+);
