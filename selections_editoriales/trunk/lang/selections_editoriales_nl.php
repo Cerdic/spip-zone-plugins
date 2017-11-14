a9cca0cc1249ec8675678658d4395eca48acc8c0
hanjo.spip@gmail.com <hanjo.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-11-14 17:44:34 +0000

    [Salvatore] [source:_plugins_/selections_editoriales/trunk/lang/ selections_editoriales] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@107554 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ selections_editoriales/trunk/lang/selections_editoriales_nl.php
@@ -0,0 +1,26 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/selections_editoriales?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'configurer_objets_label' => 'Activeer het kunnen toevoegen van redactionele selecties bij de volgende objecten:',
+
+	// I
+	'info_aucune_utilisation' => 'Deze selectie is aan geen enkel object gekoppeld.',
+	'info_des_utilisations' => '@nb@ toepassingen',
+	'info_une_utilisation' => '1 toepassing',
+
+	// S
+	'selection_vue' => 'Selectie gezien in de tekst',
+	'selections_editoriales_titre' => 'Redactionele selecties',
+
+	// T
+	'titre_page_configurer_selections_editoriales' => 'Configuratie van redactionele selecties'
+);
