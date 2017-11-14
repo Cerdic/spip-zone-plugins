e239fa527c84e183f4a799f487889f70946bd013
klaus@spip.de <klaus@spip.de@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-11-14 17:44:20 +0000

    [Salvatore] [source:_plugins_/nospam/lang/ paquet-nospam] Export depuis http://trad.spip.net de la langue de
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@107544 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- nospam/lang/paquet-nospam_de.php
+++ nospam/lang/paquet-nospam_de.php
@@ -10,5 +10,6 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// N
-	'nospam_description' => 'SPAM stoppen! Ein Filter ohne Nebenwirkungen für ehrliche Nutzer.'
+	'nospam_description' => 'SPAM stoppen! Ein Filter ohne Nebenwirkungen für ehrliche Nutzer.',
+	'nospam_slogan' => 'Begrenzt das SPAM-Risiko in Foren'
 );
