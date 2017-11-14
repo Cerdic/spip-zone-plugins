7a3d742511911f51a26547f7692b342558755c3c
klaus@spip.de <klaus@spip.de@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-11-14 17:44:43 +0000

    [Salvatore] [source:_plugins_/todo/trunk/lang/ paquet-todo] Export depuis http://trad.spip.net de la langue de
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@107560 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ todo/trunk/lang/paquet-todo_de.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-todo?lang_cible=de
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// T
+	'todo_description' => 'Ergänzt SPIP  um typografische Kürzel, mit denen ToDo-Listen in SPIP-Textobjekten erfaßt und angezeigt werden können. Die vorliegende Version 2 erweitert die Liste der Status-Modi und erlaubt, es Aufgaben eine Priorität zuzuweisen, sowie Bezeichnungen und Standardinformationen wie Start- und Enddatum oder Versions- und Commit-Nummern zuzuordnen.',
+	'todo_slogan' => 'Aufgaben schnell auflisten'
+);
