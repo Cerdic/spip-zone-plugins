6df92855b5e85130c5ffb01bf95ecc440c2eec77
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-11-14 17:44:45 +0000

    [Salvatore] [source:_plugins_/tradrub_proposee/trunk/lang/ tradrub_proposee] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@107562 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- tradrub_proposee/trunk/lang/tradrub_proposee_fr.php
+++ tradrub_proposee/trunk/lang/tradrub_proposee_fr.php
@@ -1,14 +1,23 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/tradrub_proposee/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-	'erreur_interdit_meme_rubrique' => 'Il est interdit de créer une traduction dans la même rubrique que l\'originale.',
-	'erreur_interdit_meme_secteur' => 'Il est interdit de créer une traduction dans le même secteur que l\'originale.',
+
+	// E
+	'erreur_interdit_meme_rubrique' => 'Il est interdit de créer une traduction dans la même rubrique que l’originale.',
+	'erreur_interdit_meme_secteur' => 'Il est interdit de créer une traduction dans le même secteur que l’originale.',
+
+	// I
 	'info_raccourcis' => 'Raccourcis :',
+
+	// L
 	'label_interdit_meme_rubrique' => 'Interdire la création de traduction dans la même rubrique',
 	'label_interdit_meme_secteur' => 'Interdire la création de traduction dans le même secteur (un secteur par langue)',
-	'titre_tradrub_proposee' => 'Proposition de rubriques pour les traduction',
+
+	// T
+	'titre_tradrub_proposee' => 'Proposition de rubriques pour les traduction'
 );
