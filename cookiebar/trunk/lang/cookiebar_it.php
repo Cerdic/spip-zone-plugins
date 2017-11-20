b7ba1bd4f85c7320e9ef72ae69225a39db6e3ca4
alm82@live.it <alm82@live.it@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-11-20 04:36:03 +0000

    [Salvatore] [source:_plugins_/cookiebar/trunk/lang/ cookiebar] Export depuis http://trad.spip.net de la langue it
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@107642 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ cookiebar/trunk/lang/cookiebar_it.php
@@ -0,0 +1,35 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/cookiebar?lang_cible=it
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cfg_titre_parametrages' => 'Configurazione della barra dei cookies',
+
+	// L
+	'label_accept_button' => 'Visualizzazione del pulsante',
+	'label_accept_button_false' => 'no',
+	'label_accept_button_true' => 'si',
+	'label_accept_txt' => 'Personalizza il testo del pulsante (facoltativo)',
+	'label_accept_txt_current' => 'Etichetta di default',
+	'label_decline_button' => 'Visualizzazione del pulsante',
+	'label_decline_button_false' => 'no',
+	'label_decline_button_true' => 'si',
+	'label_decline_txt' => 'Personalizza il testo del pulsante (facoltativo)',
+	'label_decline_txt_current' => 'Etichetta di default',
+	'label_effet' => 'Scomparsa della barra',
+	'label_effet_fade' => 'Dissolvenza',
+	'label_message_txt' => 'Personalizza il messaggio (facoltativo)
+viene accettata la sintassi &lt;multi&gt;&lt;/multi&gt;',
+	'label_policy_button' => 'Visualizzazione del pulsante',
+	'label_policy_button_false' => 'no',
+	'label_policy_button_true' => 'si',
+	'label_policy_txt' => 'Personalizza il testo del pulsante (facoltativo)',
+	'label_policy_txt_current' => 'Etichetta di default'
+);
