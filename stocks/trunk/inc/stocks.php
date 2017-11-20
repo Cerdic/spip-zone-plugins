4a031d82105e1d24dd3046317b98623377999bc5
p@henix.be <p@henix.be@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-11-20 21:38:36 +0000

    PSR pour ce fichier
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@107673 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- stocks/trunk/inc/stocks.php
+++ stocks/trunk/inc/stocks.php
@@ -1,7 +1,7 @@
 <?php
 
 // Sécurité
-if (!defined("_ECRIRE_INC_VERSION")) {
+if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
 
@@ -11,18 +11,18 @@ if (!defined("_ECRIRE_INC_VERSION")) {
  * @param $objet
  * @param $id_objet
 */
-function get_quantite($objet,$id_objet) {
+function get_quantite($objet, $id_objet) {
 
-    $table_stocks = table_objet_sql('stocks');
+	$table_stocks = table_objet_sql('stocks');
 
-    return sql_getfetsel(
-        'quantite',
-        $table_stocks,
-        array(
-            "objet = ".sql_quote($objet),
-            "id_objet = ".intval($id_objet)
-        )
-    );
+	return sql_getfetsel(
+		'quantite',
+		$table_stocks,
+		array(
+			'objet = '.sql_quote($objet),
+			'id_objet = '.intval($id_objet)
+		)
+	);
 }
 
 /**
@@ -32,37 +32,38 @@ function get_quantite($objet,$id_objet) {
  * @param $id_objet
  * @param $quantite
 */
-function set_quantite($objet,$id_objet,$quantite) {
+function set_quantite($objet, $id_objet, $quantite) {
 
 	$table_stocks = table_objet_sql('stocks');
-    $quantite = intval($quantite);
-        
-	$insert = sql_insertq(
-				$table_stocks,
-				array(
-					"objet" => $objet,
-					"id_objet" => intval($id_objet),
-					"quantite" => $quantite
-				)
-			);
-	if(!$insert){		
-		$update = sql_update(
-            $table_stocks,
-            array(
-                "quantite" => intval($quantite)
-            ),
-            array(
-                "objet = ".sql_quote($objet),
-                "id_objet = ".intval($id_objet)
-            )
-        );		
-	}
-    
+	$quantite = intval($quantite);
 
-    if ($insert || $update)
-        return $quantite;
-    else
-        return false;
+	$insert = sql_insertq(
+		$table_stocks,
+		array(
+			'objet' => $objet,
+			'id_objet' => intval($id_objet),
+			'quantite' => $quantite
+		)
+	);
+
+	if (!$insert) {
+		$update = sql_update(
+			$table_stocks,
+			array(
+				'quantite' => intval($quantite)
+			),
+			array(
+				'objet = '.sql_quote($objet),
+				'id_objet = '.intval($id_objet)
+			)
+		);
+	}
+
+	if ($insert || $update) {
+		return $quantite;
+	} else {
+		return false;
+	}
 }
 
 /**
@@ -72,28 +73,30 @@ function set_quantite($objet,$id_objet,$quantite) {
  * @param $id_objet
  * @param $quantite {int} peut être négatif dans ce cas c'est une décrémentation
 */
-function incrementer_quantite($objet,$id_objet,$quantite) {
+function incrementer_quantite($objet, $id_objet, $quantite) {
 
-    $table_stocks = table_objet_sql('stocks');
+	$table_stocks = table_objet_sql('stocks');
 
-    $quantite = intval($quantite);
+	$quantite = intval($quantite);
 
-    if ($quantite == 0)
-        return 0;
+	if ($quantite == 0) {
+		return 0;
+	}
 
-    if ($quantite > 0)
-        $set = array("quantite" => "quantite + ".abs($quantite));
-    else 
-        $set = array("quantite" => "quantite - ".abs($quantite));
+	if ($quantite > 0) {
+		$set = array('quantite' => 'quantite + '.abs($quantite));
+	} else {
+		$set = array('quantite' => 'quantite - '.abs($quantite));
+	}
 
-    $update = sql_update(
-        $table_stocks,
-        $set ,
-        array(
-            "objet = ".sql_quote($objet),
-            "id_objet = ".intval($id_objet)
-        )
-    );
+	$update = sql_update(
+		$table_stocks,
+		$set,
+		array(
+			'objet = '.sql_quote($objet),
+			'id_objet = '.intval($id_objet)
+		)
+	);
 
-    return $update;
+	return $update;
 }
