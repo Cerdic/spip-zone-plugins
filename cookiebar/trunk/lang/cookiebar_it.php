74ca9291a74a7ebb6cc9bfaaf44b020c4fd18af7
alm82@live.it <alm82@live.it@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-11-21 04:35:41 +0000

    [Salvatore] [source:_plugins_/cookiebar/trunk/lang/ cookiebar] Export depuis http://trad.spip.net de la langue it
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@107679 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- cookiebar/trunk/lang/cookiebar_it.php
+++ cookiebar/trunk/lang/cookiebar_it.php
@@ -11,6 +11,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// C
 	'cfg_titre_parametrages' => 'Configurazione della barra dei cookies',
+	'cookiebar_titre' => 'Cookiebar',
 
 	// L
 	'label_accept_button' => 'Visualizzazione del pulsante',
@@ -25,11 +26,33 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'label_decline_txt_current' => 'Etichetta di default',
 	'label_effet' => 'Scomparsa della barra',
 	'label_effet_fade' => 'Dissolvenza',
+	'label_effet_hide' => 'Nascosto',
+	'label_effet_slide' => 'Scivolamento (diapositiva)',
 	'label_message_txt' => 'Personalizza il messaggio (facoltativo)
 viene accettata la sintassi &lt;multi&gt;&lt;/multi&gt;',
+	'label_message_txt_current' => 'Messaggio predefinito',
 	'label_policy_button' => 'Visualizzazione del pulsante',
 	'label_policy_button_false' => 'no',
 	'label_policy_button_true' => 'si',
 	'label_policy_txt' => 'Personalizza il testo del pulsante (facoltativo)',
-	'label_policy_txt_current' => 'Etichetta di default'
+	'label_policy_txt_current' => 'Etichetta di default',
+	'label_policy_url' => 'Se SI, indirizzo della pagina contenente l’informativa sulla privacy',
+	'label_position' => 'Posizione della barra',
+	'label_position_fixed' => 'In testa alla pagina (fissa)',
+	'label_position_fixed-bottom' => 'In fondo alla pagina (fissa)',
+	'label_position_top' => 'In testa alla pagina',
+	'label_section_accept' => 'Pulsante per l’accettazione dei cookies',
+	'label_section_decline' => 'Pulsante di rifiuto dei cookies',
+	'label_section_graphisme' => 'Aspetto grafico',
+	'label_section_message' => 'Messaggio',
+	'label_section_policy' => 'Pulsante di collegamento all’informativa sulla privacy',
+
+	// M
+	'message_accepttext' => 'Accetto',
+	'message_declinetext' => 'Disattiva i cookies',
+	'message_message' => 'Proseguendo la visita a questo sito, accetti l’utilizzo dei cookie e delle preferenze di tracciamento',
+	'message_policytext' => 'Informativa sulla privacy',
+
+	// T
+	'titre_page_configurer_cookiebar' => 'Configura Cookiebar'
 );
