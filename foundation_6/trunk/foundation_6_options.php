5dd1a5aa4a38d513b41a140fb901509273e0309b
p@henix.be <p@henix.be@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-11-25 08:50:46 +0000

    Contournée https://core.spip.net/issues/4050 en appelant directement minifier_js
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@107737 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- foundation_6/trunk/foundation_6_options.php
+++ foundation_6/trunk/foundation_6_options.php
@@ -17,3 +17,9 @@ if (!defined('_REVEAL_ANIMATION_IN')) {
 if (!defined('_REVEAL_ANIMATION_OUT')) {
 	define('_REVEAL_ANIMATION_OUT', '');
 }
+
+/**
+ * Dans le fichier javascript/spip.foundation.js.html, on utilise la fonction
+ * minifier_js, on inclut donc directement le fichier ici
+ */
+include_spip('inc/compresseur_minifier');
--- foundation_6/trunk/foundation_6_pipelines.php
+++ foundation_6/trunk/foundation_6_pipelines.php
@@ -39,7 +39,7 @@ function foundation_6_insert_head_css($flux) {
 function foundation_6_affichage_final($flux) {
 	include_spip('inc/config');
 	if (lire_config('foundation_6/javascript')) {
-		$js = '<script async src="'.minifier(produire_fond_statique('javascript/spip.foundation.js'), 'js').'" type="text/javascript"></script>';
+		$js = '<script async src="'.produire_fond_statique('javascript/spip.foundation.js').'" type="text/javascript"></script>';
 		$pos_body = strpos($flux, '</body>');
 
 		if ($pos_body) {
