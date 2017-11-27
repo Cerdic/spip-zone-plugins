d6db2b5a786db6020f97b6863fd7dbda25ca62d7
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-11-27 10:36:21 +0000

    compatibilite https
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@107767 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- webfonts/branches/webfonts2/webfonts2_pipelines.php
+++ webfonts/branches/webfonts2/webfonts2_pipelines.php
@@ -20,7 +20,7 @@ function webfonts2_insert_head_css($flux){
 		$fonts = array_map('urlencode',$fonts);
 		$fonts = implode('|',$fonts);
 		if (strlen($fonts)) {
-			$code = '<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family='.$fonts.'" id="webfonts" />';
+			$code = '<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family='.$fonts.'" id="webfonts" />';
 			// le placer avant les autres CSS du flux
 			if (($p = strpos($flux,"<link"))!==false)
 				$flux = substr_replace($flux,$code,$p,0);
