c3d8ce1f3314a16449b5f9c97c278f2ee1af05bc
jacques@jack31.net <jacques@jack31.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-11-28 04:35:42 +0000

    [Salvatore] [source:_plugins_/palette/trunk/lang/ palette] Export depuis http://trad.spip.net de la langue en
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@107781 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- palette/trunk/lang/palette_en.php
+++ palette/trunk/lang/palette_en.php
@@ -12,8 +12,13 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// C
 	'config_desc' => '<strong>Palette</strong> automatically adds a colour picker wheel to an input field,
   by simply applying the CSS class "palette" to it',
-	'config_exemple' => 'Example:', # MODIF
-	'config_lien_doc' => '<a href="http://contrib.spip.net/Palette" class="spip_out">Cf. documentation</a>', # MODIF
+	'config_exemple' => 'Palette call examples:',
+	'config_exemple_code_source' => 'Examples code source',
+	'config_exemple_couleur' => 'Normal color',
+	'config_exemple_couleur_alpha' => 'Color with transparency',
+	'config_exemples_couleurs_vides' => 'No default value on the field',
+	'config_lien_doc' => '<a href="https://contrib.spip.net/Palette" class="spip_out">Cf. documentation</a>',
+	'config_titre' => 'Configure the Palette color picker.',
 
 	// F
 	'fermer' => 'Close palette',
