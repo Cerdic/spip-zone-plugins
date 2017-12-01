fed86cdf583ffe921847015022a56e5e2b071868
arnaud.berard@mister-graphx.com <arnaud.berard@mister-graphx.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-12-01 08:14:09 +0000

    ajout d'un formulaire plus adapté a la vue de gestion des stock certe mal nommé ^^ propose soit de créer le stock avec en input le stock par défaut pré-rempli soit de le modifier si il existe
    les stocks étant des liens ils ne sont créés que sur les nouveaux produits, les anciens sinon étaients signalé avec le stock par défaut alors qu'il n'étaient pas créés, on les différencie dans la page de gestion des stocks
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@107813 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ stocks/trunk/formulaires/changer_quantite.php
@@ -0,0 +1,58 @@
+<?php
+/*
+ * Squelette
+ * (c) 2016 
+ * Distribue sous licence GPL
+ *
+ * @url - http://programmer.spip.net/-Formulaires-35-
+ * 
+ *
+ */
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+
+
+function formulaires_changer_quantite_charger_dist($objet, $id_objet, $retour = ''){
+include_spip('inc/stocks');
+	$quantite = get_quantite($objet,$id_objet);
+	$stock_default = lire_config('stocks/quantite_default');
+	$valeurs = array(
+		'objet' => $objet,
+		'id_objet' => $id_objet,
+		'is_stock' => ($quantite) ? true : false ,
+		'_quantite' => isset($quantite) ? $quantite : $stock_default
+	);
+	
+
+	return $valeurs;
+}
+
+function formulaires_changer_quantite_verifier_dist($objet,$id_objet,$retour = ''){
+	$erreurs = array();
+	foreach(array('_quantite') as $champ) {
+		if (!_request($champ)) {
+			$erreurs[$champ] = "Cette information est obligatoire !";
+		}
+	}
+	if (!is_numeric(_request('_quantite'))) {
+		$erreurs['_quantite'] = "Doit être un nombre";
+	}
+	if (count($erreurs)) {
+		$erreurs['message_erreur'] = "Erreur dans votre saisie";
+	}
+	return $erreurs;
+}
+
+function formulaires_changer_quantite_traiter_dist($objet,$id_objet,$retour = ''){
+include_spip('inc/stocks');
+	$quantite = _request('_quantite');
+	set_quantite($objet,$id_objet,$quantite);
+	set_request('is_stock', true);
+	
+	return array('message_ok'=>_T('stocks:reponse_ok'),
+				 'editable'=>true);
+}
+
+?>
