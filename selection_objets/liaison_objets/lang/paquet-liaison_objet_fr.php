aad91eacd2f7976de92f009f6bb843476b900796
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-12-03 20:07:53 +0000

    description correct du plugin
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@107841 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- selection_objets/liaison_objets/lang/paquet-liaison_objet_fr.php
+++ selection_objets/liaison_objets/lang/paquet-liaison_objet_fr.php
@@ -8,8 +8,7 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// S
-	'liaison_objet_description' => 'Plugin inspiré du plugin sélection d’article. 
-	Permet de sélectionner des objets spip, de les ranger et de personnaliser leur affichage selon la rubrique choisie',
-	'liaison_objet_nom' => 'Sélection d’objets',
-	'liaison_objet_slogan' => 'Listez vos objets spip'
+	'liaison_objet_description' => 'Permet de lier des objets entre eux.',
+	'liaison_objet_nom' => 'Liaison d’objets',
+	'liaison_objet_slogan' => 'Lier vos objets spip'
 );
