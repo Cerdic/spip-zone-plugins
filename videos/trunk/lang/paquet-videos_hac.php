69a7d35e8fd03dfa7e9c11fa343f7cb3be949e3a
ako@mail.com <ako@mail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-12-06 04:36:04 +0000

    [Salvatore] [source:_plugins_/videos/trunk/lang/ paquet-videos] Export depuis http://trad.spip.net de la langue hac
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@107881 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ videos/trunk/lang/paquet-videos_hac.php
@@ -0,0 +1,18 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-videos?lang_cible=hac
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// V
+	'videos_description' => 'ئی ھۊرئاۋېزیا پەی ئاۋردەو ڤیدیۆی و کوڵە  تۊماری چە ڕاو کۆپیکەردەی و لکنای لینکی ھۊرگیریا چە (Dailymotion, Vimeo, Youtube, CultureBox)
+			بەکاربەردەیش پېسەو دۆکومێنتی. ھەر پاسە HTML5 نیشاندای شېۋەکا MP4, H264, Ogg,
+			WebM et Mkv چە وێبگېڵو مۆبایلیەنە.',
+	'videos_nom' => 'ڤیدیۆ',
+	'videos_slogan' => 'ڕووپەڕو ڕاۋەبەری نموونەی پەی ئاۋردەو ڤیدیۆی'
+);
