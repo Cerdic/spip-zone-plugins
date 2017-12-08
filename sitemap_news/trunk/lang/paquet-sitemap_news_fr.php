fcc969110e087d2915cac39d8beb0115cc720f8f
erational@erational.org <erational@erational.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-12-08 11:16:25 +0000

    icone + espacement entre les mots-clés
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@107913 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- sitemap_news/trunk/lang/paquet-sitemap_news_fr.php
+++ sitemap_news/trunk/lang/paquet-sitemap_news_fr.php
@@ -1,7 +1,9 @@
 <?php
 
 // Sécurité
-if (!defined('_ECRIRE_INC_VERSION')) return;
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'sitemap_news_description' => 'Pour ne pas avoir à changer ses URLs dans un format précis, et pour être indexé plus facilement dans Google News, il est préférable de fournir à ce moteur de recherche un fichier Sitemap dédié aux actualités, dont la documentation est ici : http://support.google.com/news/publisher/bin/answer.py?hl=fr&answer=74288',
