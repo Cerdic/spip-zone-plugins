48da3196cf353dcb827a86d902643f8fbcb2066b
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-12-10 04:37:50 +0000

    [Salvatore] [source:_plugins_/liaison_objet/trunk/lang/ liaison_objet] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@107949 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- liaison_objet/trunk/lang/liaison_objet_de.php
+++ liaison_objet/trunk/lang/liaison_objet_de.php
@@ -20,7 +20,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// E
 	'erreur_deja_lie' => 'Dieses Objekt wurde bereits für diese Liste ausgewählt',
 	'explication_choix_multi' => 'Mithilfe der Taste Ctrl oder Maj können Sie mehrere auswählen (Apfel oder Maj für Macs)',
-	'explication_liaison_choix_illimite' => 'Standardmäßig kann ein Objekt nur einmal an ein Zielobjekt angeknüpft werden, möchten sie diese Einschränkung aufheben?', # MODIF
+	'explication_liaison_choix_illimite' => 'Standardmäßig kann ein Objekt nur einmal an ein Zielobjekt angeknüpft werden, möchten sie diese Einschränkung aufheben?',
 	'explication_liaison_cible' => 'Wenn kein Ziel ausgewählt ist, werden alle berücksichtigt',
 	'explication_liaison_objets_cible' => 'Zielobjekte an welche Auswahlobjekte geknüpft werden können.',
 	'explication_liaison_type_liens' => 'Sie können Verknüpfungsarten festlegen: Fügen Sie eine Art pro Zeile ein, gefolgt von einer Komma und einer Beschreibung ein. Die Beschreibung kann eine Sprachkette sein.',
@@ -38,9 +38,17 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'label_id_objet_dest' => 'Id Zielobjekt',
 	'label_objet' => 'Objekt',
 	'label_objet_dest' => 'Zielobjekt',
+	'label_objets_liaison' => 'Objekte mit denen man verlinken kann',
 	'label_ordre' => 'Rang',
 	'label_titre' => 'Titel',
 	'label_url' => 'Url',
+	'liaison_choix_illimite' => 'Die Wahleinschränkung per Objekt aufheben',
+	'liaison_cible' => 'Ziel Auswahl für',
+	'liaison_objet' => 'Auswahl',
+	'liaison_objet_cible_titre' => 'Auswahl Zielobjekte',
+	'liaison_objets_pour' => 'Objekt Auswahl für',
+	'liaison_type_lien' => 'Verknüpfungsart',
+	'liaison_type_liens' => 'Verknüpfungsart festlegen',
 
 	// O
 	'objet_destination_choisis' => 'Ausgewählte/ Ziel/e:',
@@ -52,28 +60,15 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'racine' => 'Oberste Ebene',
 	'retirer_lien_objet' => 'Dieses Objekt aus der Auswahl entfernen',
 	'retirer_tous_liens_liaison_objets' => 'Die gesamte Objekt Auswahl entfernen',
-	'rubriques_choisis' => 'Ausgewählte Rubriken:',
-
-	// S
-	'liaison_choix_illimite' => 'Die Wahleinschränkung per Objekt aufheben',
-	'liaison_cible' => 'Ziel Auswahl für',
-	'liaison_objet' => 'Auswahl',
-	'liaison_objet_cible_titre' => 'Auswahl Zielobjekte', # MODIF
-	'liaison_objets_pour' => 'Objekt Auswahl für',
-	'label_objets_liaison' => 'Objekte mit denen man verlinken kann',
-	'liaison_type_lien' => 'Verknüpfungsart',
-	'liaison_type_liens' => 'Verknüpfungsart festlegen',
-	'liaisone' => 'Ausgewählt:',
 
 	// T
 	'texte_ajouter_objet' => 'Ein Objekt auswählen',
 	'texte_changer_statut_objet' => 'Dieses Ausgewählte Objekt ist:',
 	'texte_creer_asliaison_objetcier_objet' => 'Ein Objekt anlegen und verknüpfen',
 	'titre_langue_objet' => 'Sprache dieses Ausgewählten Objektes',
-	'titre_logo_liaison_objet' => 'Logo dieses Ausgewählten Objektes',
 	'titre_liaison_objet' => 'Ausgewähltes Objekt',
 	'titre_liaison_objets' => 'Ausgewählte Objekte',
-	'titre_liaison_objets_rubrique' => 'Ausgewählte Objekte dieser Rubrik',
+	'titre_logo_liaison_objet' => 'Logo dieses Ausgewählten Objektes',
 	'type_liens_defaut' => 'Standard Verknüpfunsart, kann weiter unten überschrieben werden',
 	'type_liens_surcharge' => 'die standard Verknüpfunsarten überschreiben'
 );
