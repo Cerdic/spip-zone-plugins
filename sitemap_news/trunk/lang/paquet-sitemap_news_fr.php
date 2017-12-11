bf2bf7921064be103c674f91c33f218be4096695
erational@erational.org <erational@erational.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-12-11 13:39:56 +0000

    orth + reformulation
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@107989 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- sitemap_news/trunk/lang/paquet-sitemap_news_fr.php
+++ sitemap_news/trunk/lang/paquet-sitemap_news_fr.php
@@ -6,7 +6,7 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 }
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-	'sitemap_news_description' => 'Pour ne pas avoir à changer ses URLs dans un format précis, et pour être indexé plus facilement dans Google News, il est préférable de fournir à ce moteur de recherche un fichier Sitemap dédié aux actualités, dont la documentation est ici : http://support.google.com/news/publisher/bin/answer.py?hl=fr&answer=74288',
+	'sitemap_news_description' => 'Pour être indexé plus facilement dans Google News, il est préférable de fournir un fichier Sitemap dédié aux actualités, dont la documentation est disponible : http://support.google.com/news/publisher/bin/answer.py?hl=fr&answer=74288',
 	'sitemap_news_nom' => 'Sitemap d’actualités',
-	'sitemap_news_slogan' => 'Fourni un fichier Sitemap dédié aux actualités du site',
+	'sitemap_news_slogan' => 'Fournit un fichier Sitemap dédié aux actualités du site',
 );
