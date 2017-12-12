794f1b43f5e032e76de0b47430eba3d67f46f5f3
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-12-12 16:54:16 +0000

    v 1.3.10
    éviter erreur dans la config 
    
    
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@108023 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- reservations_bank/branches/v1/reservation_bank_fonctions.php
+++ reservations_bank/branches/v1/reservation_bank_fonctions.php
@@ -34,20 +34,14 @@ function rb_inserer_transaction($id_reservation) {
 }
 
 /**
- * Retourne les prestataires simple (pas besoin d'une application externe à spip).
+ * Retourne les prestataires simple (pas besoin d'une application externe à spip) pour une réservation.
  *
  * @param integer $id_reservation
  *        	L'id de la réservation.
  *
  * @return array
  */
-function rb_prestataires_simples_actives($id_reservation) {
-	// Les prestas coonfigurés.
-	include_spip ( 'inc/bank' );
-
-	$prestas_actifs = bank_lister_configs ();
-
-
+function rb_prestataires_simples_actives_reservation($id_reservation) {
 	$fonction_prix = charger_fonction('prix', 'inc/');
 	$sql = sql_select('id_reservations_detail', 'spip_reservations_details', 'id_reservation=' . $id_reservation);
 	$prix_details = array();
@@ -56,25 +50,39 @@ function rb_prestataires_simples_actives($id_reservation) {
 	}
 	$prix = array_sum($prix_details);
 
-	// Si montant supérieur 'a 0, les types de prestas nécessitant pas de callback du fournisseur.
+	// Si montant supérieur à 0, on prend les types de prestas nécessitant pas de callback du fournisseur.
 	if ($prix > 0) {
-
-		$prestas_simple = array(
-			'cheque',
-			'virement'
-		);
-		$prestataires_simples_actives = array();
-		foreach ( $prestas_simple as $presta ) {
-			if (isset ($prestas_actifs [$presta] ) and $prestas_actifs [$presta] ['actif']) {
-				$prestataires_simples_actives [$presta] = _T ( 'bank:label_presta_' . $presta );
-			}
-		}
-
+		$prestataires = rb_prestataires_simples_actives();
 	}
 	// Sinon presta = gratuit.
 	else{
-		$prestataires_simples_actives = array('gratuit' => _T ( 'bank:titre_bouton_payer_gratuit'));
+		$prestataires = array('gratuit' => _T ( 'bank:titre_bouton_payer_gratuit'));
 	}
 
-	return $prestataires_simples_actives;
+	return $prestataires;
+}
+
+/**
+ * Retourne les prestataires simple (pas besoin d'une application externe à spip).
+ *
+ * @return array
+ */
+function rb_prestataires_simples_actives() {
+	// Les prestas coonfigurés.
+	include_spip ( 'inc/bank' );
+
+	$prestas_actifs = bank_lister_configs ();
+
+	$prestas_simple = array(
+		'cheque',
+		'virement'
+	);
+	$prestataires = array();
+	foreach ($prestas_simple as $presta ) {
+		if (isset ($prestas_actifs [$presta] ) and $prestas_actifs [$presta] ['actif']) {
+			$prestataires[$presta] = _T ( 'bank:label_presta_' . $presta );
+		}
+	}
+
+	return $prestataires;
 }
