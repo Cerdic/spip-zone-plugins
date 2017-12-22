f5bc6283640ac7665c0fbdd3ea5eda9982b0b36a
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-12-22 11:39:22 +0000

    Vu que c'est tous les mêmes clés, aucun intérêt de mapper un par un, autant balancer le même tableau. Ce qui permet d'ajouter d'autres infos reconnues par facteur comme les pièces jointes, etc. (Rainer)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@108147 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- notifications_avancees/trunk/notifications/modes/email.php
+++ notifications_avancees/trunk/notifications/modes/email.php
@@ -9,28 +9,7 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 function notifications_modes_email_envoyer_dist($contact, $contenu, $options) {
 	// S'il y a le plugin Facteur, on peut faire un truc plus propre
 	if (defined('_DIR_PLUGIN_FACTEUR')) {
-		$corps = array(
-			'texte' => $contenu['texte'],
-		);
-		// Si on a une version HTML
-		if ($contenu['html']) {
-			$corps['html'] = $contenu['html'];
-		}
-		//si un expéditeur est défini
-		if ($contenu['from']) {
-			$corps['from'] = $contenu['from'];
-		}
-		//si un nom d'expéditeur est défini
-		if ($contenu['nom_envoyeur']) {
-			$corps['nom_envoyeur'] = $contenu['nom_envoyeur'];
-		}
-		// S'il y a un Reply-to
-		if ($options['repondre_a']) {
-			$corps['repondre_a'] = $options['repondre_a'];
-			if ($options['nom_repondre_a']) {
-				$corps['nom_repondre_a'] = $options['nom_repondre_a'];
-			}
-		}
+		$corps = $contenu;
 	} // Sinon c'est juste le texte
 	else {
 		$corps = $contenu['texte'];
