d969a01a0bb71bce24da27380bc6cc54f77eb31a
arnaud.berard@mister-graphx.com <arnaud.berard@mister-graphx.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-12-22 15:11:20 +0000

    premier jet de quelques fonctions utilitaires de requète sur l'api et servant pour la page de recherche des webfonts
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@108148 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- webfonts/branches/webfonts2/webfonts2_fonctions.php
+++ webfonts/branches/webfonts2/webfonts2_fonctions.php
@@ -21,3 +21,44 @@ function googlefont_request($webfonts,$subsets=''){
 	$request = "https://fonts.googleapis.com/css?family=$fonts".$subset;
 	return $request;
 }
+
+
+function google_font_search($fonts, $search){
+	$res = array();
+	foreach($fonts['items'] as $item){
+		( preg_match('/' . trim($search) . '/i', $item['family']) ) ? $res[] = $item : false;
+	}
+	
+	return $res;
+}
+
+
+
+
+function lister_webfonts(){
+	$fonts = pipeline('fonts_list',array(
+		'args'=>array(),
+		'data'=>$fonts
+	));
+	return $fonts;
+}
+
+function googlefont_api_get($api_key,$sort=false,$category=false){
+	// Requète en GET sur //https://www.googleapis.com/webfonts/v1/webfonts?key=_GOOGLE_API_KEY
+	$url = 'https://www.googleapis.com/webfonts/v1/webfonts?key='.$api_key;
+	(strlen($sort)) ? $url .= '&sort='.$sort : $sort = false ;
+	(strlen($category)) ? $url .= '&category='.$category : $category = false;	
+		
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+	curl_setopt($ch, CURLOPT_HEADER, false);
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_REFERER, $url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+	$result = curl_exec($ch);
+	curl_close($ch);
+	$googlefonts = json_decode($result, true);
+	
+	return $googlefonts;
+}
