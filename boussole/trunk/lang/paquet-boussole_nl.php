e3a816ef3d6e1a7bbd43a0b803b4ca3ed7844659
hanjo.spip@gmail.com <hanjo.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-12-27 04:36:09 +0000

    [Salvatore] [source:_plugins_/boussole/trunk/lang/ paquet-boussole] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@108197 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ boussole/trunk/lang/paquet-boussole_nl.php
@@ -0,0 +1,17 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-boussole?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'boussole_description' => 'Installeer deze plugin op je site en je krijgt in je skeletten toegang tot alle sites van de SPIP Galaxy, gebruik makend van de voorgestelde modellen, "noisettes" of  Z pagina.
+_ je kunt deze plugin ook gebruiken om je eigen lijst van sites weer te geven op de door jou gewenste manier.',
+	'boussole_nom' => 'Kompas',
+	'boussole_slogan' => 'De goede adressen in de SPIP Galaxy of daarbuiten!'
+);
