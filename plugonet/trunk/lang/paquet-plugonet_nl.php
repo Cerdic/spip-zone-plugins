caa4a5491b3dad5cd87769d57037add02469b9d0
hanjo.spip@gmail.com <hanjo.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-12-27 04:36:36 +0000

    [Salvatore] [source:_plugins_/plugonet/trunk/lang/ paquet-plugonet] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@108206 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ plugonet/trunk/lang/paquet-plugonet_nl.php
@@ -0,0 +1,17 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-plugonet?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// P
+	'plugonet_description' => 'Deze plugin maakt een nieuw paquet.xml bestand op basis van het plugin.xml bestand. Controle opties zijn beschikbaar om vooraf enkele bakens van het oorspronkelijke bestand aan te passen.
+
+Daarnaast maakt PlugOnet het mogelijk om handmatig aangemaakte  paquet.xml bestanden te controleren en het biedt hulp bij elk baken van de nieuwe DTD.',
+	'plugonet_slogan' => 'Migreer de DTD van een plugin naar die van een pakket'
+);
