4607fa9889eda0f5c6f2c228e63f4be39322a4f4
hanjo.spip@gmail.com <hanjo.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-12-27 04:36:11 +0000

    [Salvatore] [source:_plugins_/boussole_spip/trunk/lang/ paquet-bouspip] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@108199 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ boussole_spip/trunk/lang/paquet-bouspip_nl.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-bouspip?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'bouspip_description' => 'Deze plugin levert de naam; de URL en de omschrijving van alle sites van de SPIP Galaxy. Hij moet worden gebruikt in combinatie met  plugin Boussole (Kompas) in server modus om via een webservice informatie te leveren aan alle sites die de SPIP Kompas willen weergeven.',
+	'bouspip_nom' => 'SPIP Kompas',
+	'bouspip_slogan' => 'De goede adressen van de SPIP Galaxy!'
+);
