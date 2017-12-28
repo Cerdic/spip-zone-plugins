0bc88b131fc06c022c14993c2c00fca6e21ae708
klaus@spip.de <klaus@spip.de@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-12-28 04:36:25 +0000

    [Salvatore] [source:_plugins_/linkcheck/trunk/lang/ linkcheck] Export depuis http://trad.spip.net de la langue de
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@108212 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ linkcheck/trunk/lang/linkcheck_de.php
@@ -0,0 +1,103 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/linkcheck?lang_cible=de
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'afficher_alerte' => 'Warnungen anzeigen',
+	'analyser_site_linkchecks' => 'Links meiner Website analysieren',
+
+	// B
+	'bouton_exporter_selection' => 'Auswahl als CSV exportieren',
+	'bouton_tester_lien' => 'Link testen',
+
+	// C
+	'commencer_parcours' => 'Suche starten',
+	'commencer_verification' => 'Die Links wurden gefunden. Überprüfung mit Klick auf Schaltfläche starten',
+	'commencer_verification_liens' => 'Überprüfung starten',
+	'configuration_generale' => 'Allgemeine Einstellungen',
+	'configurer_linkcheck' => 'LinkCheck konfigurieren',
+	'continuer_parcours_explication' => 'Website weiter nach Links durchsuchen',
+
+	// D
+	'descriptif' => 'Dieses Plugin überprüft im Hintergrund, ob die Links in SPIP-Objekten immer noch auf online-Ressourcen zeigen.',
+	'distant_non' => 'Interne Links',
+	'distant_oui' => 'Externe Links',
+
+	// E
+	'etat' => 'Status',
+	'etat_' => 'Unbekannt',
+	'etat_deplace' => 'Verschoben',
+	'etat_malade' => 'Defekt',
+	'etat_mort' => 'Tot',
+	'etat_ok' => 'Gültig',
+
+	// I
+	'id' => 'ID',
+	'info_1_linkcheck' => 'Ein Link',
+	'info_aucun_lien' => 'Es wurde noch kein Link erfasst.',
+	'info_aucun_linkcheck' => 'Kein Link gefunden',
+	'info_dont_non_verifie' => 'davon sind <span id="nb_lien_inconnu">@nb@</span> ungeprüft.',
+	'info_dont_non_verifies' => 'davon sind <span id="nb_lien_inconnu">@nb@</span> ungeprüft.',
+	'info_nb_linkchecks' => '@nb@ Links',
+
+	// L
+	'lien' => 'Link',
+	'lien_' => 'Link nicht überprüft',
+	'lien_deplace' => 'Verschoben',
+	'lien_malade' => 'Defekt',
+	'lien_mort' => 'Toter Link',
+	'lien_ok' => 'Gültig',
+	'liens' => 'Links',
+	'liens_deplaces' => 'verschobene Links',
+	'liens_invalides' => 'Ihr Website enthält ungültige Links (Tot : @mort@ / Defekt : @malade@ / Verschoben : @deplace@) !',
+	'liens_malades' => 'defekte Links',
+	'liens_morts' => 'tote Links',
+	'liens_oks' => 'gültige Links',
+	'liens_publies' => 'online sichtbar',
+	'liens_publies_non' => 'Online nicht sichtbar',
+	'linkcheck' => 'LinkCheck',
+	'liste_des_liens' => 'Linkliste',
+
+	// M
+	'mail_notification1' => 'Hallo,<br/>
+							Sie erhalten diese Mail, weil Ihre Website problematische Links enthält.<br/>
+							Hier eine Zusammenfassung: <br/>',
+	'mail_notification2' => 'Bitte lösen Sie diese Probleme im Redaktionssystem Ihrer Website. Vielen Dank.',
+	'maj' => 'Aktualisierung',
+	'message_confirmation_reinitialiser' => 'Sind Sie sicher, dass Sie die Tabellen spip_linkchecks und spip_linkchecks_liens leeren wollen?',
+
+	// N
+	'notifier_par_courriel' => 'Email-Benachrichtigung',
+
+	// O
+	'ouvrenouvelonglet' => 'neuer Reiter',
+
+	// P
+	'parcours_incomplete' => 'Die Linksuche in Ihrer Website ist nicht abgeschlossen oder wurde noch nicht durchgeführt!',
+	'pas_encore_de_liens' => 'Um die Linküberprüfung zu starten, müssen Sie die Funktion starten, die Ihre Website nach Links durchsucht. Je nach der Größe Ihrer Website kann das ein paar Sekunden, Minuten oder Stunden dauern. Bitte laden Sie diese Seite erneut, wenn sie nicht reagiert.',
+	'poursuivre_verification' => 'Die Linkprüfung ist noch nicht abgeschlossen. Bitte setzen Sie sie fort, indem Sie auf diese Schaltfläche klicken.',
+	'poursuivre_verification_liens' => 'Linkprüfung fortsetzen',
+
+	// R
+	'redirection' => 'Weiterleitung',
+	'reinitialiser' => 'Zurücksetzen',
+	'reinitialiser_la_base' => 'Wenn Sie die Linksuche und -überprüfung neu starten möchten, klicken Sie bitte auf die Schaltfläche "Zurücksetzen". Diese Aktion leert die Tabellen spip_linkchecks und spip_linkchecks_liens.',
+	'rescensement_incomplet' => 'Die Linksuche ist noch nicht abgeschlossen! ',
+
+	// T
+	'tester_liens_linkchecks' => 'Links meiner Website überprüfen',
+	'titre_linkcheck' => 'Linkprüfer',
+	'titre_page_configurer' => 'Plugin Linkcheck konfigurieren',
+	'tous_les_liens' => 'Alle',
+
+	// U
+	'url' => 'URL',
+	'utilise' => 'in '
+);
