204a4866ed99591e0ff586ed54e04cf2a30fe67d
arnaud.berard@mister-graphx.com <arnaud.berard@mister-graphx.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-12-28 10:45:24 +0000

    une balise FONT_INDEX pour récupérer facilement le fichier .json d'index des fonts (pour le moment) une fonction font_declaration pour générer facilement les reges @font-face
    un set de font_stacks emprunté a bourbon pour les futures fallbacks fonts …
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@108219 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- webfonts/branches/webfonts2/webfonts2_fonctions.php
+++ webfonts/branches/webfonts2/webfonts2_fonctions.php
@@ -44,6 +44,9 @@ function google_font_search($fonts, $search){
 	return $res;
 }
 
+// permet aux plugins themes de spécifier leur webfonts
+// via la pipeline fonts_list
+
 function lister_webfonts(){
 	$fonts = pipeline('fonts_list',array(
 		'args'=>array(),
@@ -52,12 +55,23 @@ function lister_webfonts(){
 	return $fonts;
 }
 
+function balise_FONT_INDEX_dist($p){
+	$p->code = "get_font_index()";
+	$p->interdire_scripts = false;
+	return $p;
+}
+
+function get_font_index(){
+	lire_fichier(_DIR_TMP.'/googlefont_list.json',$respons);
+	return $respons;
+}
+
 /**
  * googlefont_api_get
  *
  * retourne l'index complet de la typothèque via l'API
  *
- * @todo à stocker en copie locale ou dans /tmp ? 
+ * @todo index.json à stocker en copie locale ou dans /tmp ? 
 */
 function googlefont_api_get($api_key,$sort=false,$category=false){
 	// Requète en GET sur //https://www.googleapis.com/webfonts/v1/webfonts?key=_GOOGLE_API_KEY
@@ -77,4 +91,88 @@ function googlefont_api_get($api_key,$sort=false,$category=false){
 	$googlefonts = json_decode($result, true);
 	
 	return $googlefonts;
+}
+
+
+/*
+ * fontface_declaration
+ *
+ * 
+@font-face {
+    font-family: 'open_sansitalic';
+    src: url('OpenSans-Italic-webfont.eot');
+    src: url('OpenSans-Italic-webfont.eot?#iefix') format('embedded-opentype'),
+         url('OpenSans-Italic-webfont.woff2') format('woff2'),
+         url('OpenSans-Italic-webfont.woff') format('woff'),
+         url('OpenSans-Italic-webfont.ttf') format('truetype'),
+         url('OpenSans-Italic-webfont.svg#open_sansitalic') format('svg');
+    font-weight: normal;
+    font-style: normal;
+
+}
+
+@param $font array family|file|weight|style
+@param $formats array extension|format default 
+
+*/
+
+function fontface_declaration($font, $formats = array('.woff'=>'woff','.woff2'=>'woff2','.ttf'=>'truetype')){ 
+	
+	$default = array(
+		'family'=>'Dutissimo',
+		'file'=>'squelettes-dist/polices/dutissimo',
+		'weight'=>'400',
+		'style'=>'normal'
+	);	
+	$font = array_merge($default,$font);
+	$font_files = '';
+	$i = 1;
+	foreach($formats as $extension => $format){
+		$font_files .="url('".$font['file'].$extension."') format('$format')";
+		($i < count($formats)) ? $font_files .=", " : $font_files .=";";	
+		$i++;		
+	}
+
+	$declaration = <<<EOT
+@font-face {
+    font-family: '{$font['family']}';
+	src: $font_files
+    font-weight:{$font['weight']};
+    font-style:{$font['style']};
+}
+EOT;
+
+	return $declaration;
+}
+
+/*
+ * function font_sets
+ * @param $fonts
+ */
+
+function font_sets($fonts) {
+	
+}
+
+
+
+function font_stacks(){
+	return array(
+		'sans-serif' => [
+			'Helvetica' => '"Helvetica Neue", "Helvetica", "Arial", sans-serif',
+			'Lucida' => '"Lucida Grande", "Lucida Sans Unicode", "Geneva", "Verdana", sans-serif',
+			'Verdana' => '"Verdana", "Geneva", sans-serif',
+			'System' => '-apple-system, BlinkMacSystemFont, "Avenir Next", "Avenir", "Segoe UI", "Lucida Grande", "Helvetica Neue", "Helvetica", "Fira Sans", "Roboto", "Noto", "Droid Sans", "Cantarell", "Oxygen", "Ubuntu", "Franklin Gothic Medium", "Century Gothic", "Liberation Sans", sans-serif',	
+		],
+		'serif'=> [
+			'Garamond' => '"Garamond", "Baskerville", "Baskerville Old Face", "Hoefler Text", "Times New Roman", serif',
+			'Georgia' => '"Georgia", "Times", "Times New Roman", serif',
+			'Hoefler' => '"Hoefler Text", "Baskerville Old Face", "Garamond", "Times New Roman", serif',
+		],
+		'monospace'=> [
+			'Consolas' => '"Consolas", "monaco", monospace',
+			'Courrier' => '"Courier New", "Courier", "Lucida Sans Typewriter", "Lucida Typewriter", monospace',
+			'Monaco' => '"Monaco", "Consolas", "Lucida Console", monospace'
+		]
+	);
 }
