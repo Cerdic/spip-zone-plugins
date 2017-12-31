ec5e3525663db372efb7b32dd4e39f63cde932dd
klaus@spip.de <klaus@spip.de@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-12-31 04:36:49 +0000

    [Salvatore] [source:_plugins_/tradrub_proposee/trunk/lang/ tradrub_proposee] Export depuis http://trad.spip.net de la langue de
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@108269 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ tradrub_proposee/trunk/lang/tradrub_proposee_de.php
@@ -0,0 +1,25 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/tradrub_proposee?lang_cible=de
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// E
+	'erreur_interdit_meme_rubrique' => 'Es ist verboten, eine Übersetzung innerhalb der selben Rubrik wie das Original anzulegen.',
+	'erreur_interdit_meme_secteur' => 'Es ist verboten, eine Übersetzung innerhalb der selben Haupt-Rubrik wie das Original anzulegen..',
+
+	// I
+	'info_raccourcis' => 'Abkürzungen:',
+
+	// L
+	'label_interdit_meme_rubrique' => 'Anlegen von Übersetzungen innerhalb der selben Rubrik verbieten',
+	'label_interdit_meme_secteur' => 'Anlegen von Übersetzungen innerhalb der selben Haupt-Rubrik verbieten (eine Haupt-Rubrik pro Sprache)',
+
+	// T
+	'titre_tradrub_proposee' => 'Rubriken für Übersetzungen vorschlagen'
+);
