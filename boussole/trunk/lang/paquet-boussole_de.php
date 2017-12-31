770323eebaa077370978a0fc4de648515462f881
klaus@spip.de <klaus@spip.de@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2017-12-31 04:36:15 +0000

    [Salvatore] [source:_plugins_/boussole/trunk/lang/ paquet-boussole] Export depuis http://trad.spip.net de la langue de
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@108259 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ boussole/trunk/lang/paquet-boussole_de.php
@@ -0,0 +1,17 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-boussole?lang_cible=de
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'boussole_description' => 'Installieren Sie dieses Plugin in Ihrer Website, um direkt aus Ihren Skeletten auf alle Websites der SPIP-Galaxis zuzugreifen. Ihnen stehen die Modelle, Noisettes und die Z-Seite zur Verfügung.
+_ Bastler können dieses Plugin auch nutzen, um ihre eigene Liste an Websites zugänglich zu machen und eigene Designs zu gestalten.',
+	'boussole_nom' => 'Kompass',
+	'boussole_slogan' => 'Die besten Adressen aus der SPIP-Galaxis und von anderswo!'
+);
