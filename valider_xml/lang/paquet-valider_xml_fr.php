7907cb5661ee13d4ce22282371f41e82a31e7368
marcimat@rezo.net <marcimat@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-01-03 09:59:04 +0000

    Ce plugin "Valider XML" reçoit l’actuel exec=valider_xml de SPIP < 3.3-dev.
    Cette page de visualisation ne sert pas directement à SPIP, donc je la sors du core, d’autant qu’elle ne fonctionne actuellement pas :
    - tester un répertoire donne de nombreuses notices et le tableau résultant n’affiche pas grand chose,
    - tester une url est impossible (bloquée par l’écran de sécurité)
    
    Si la fonctionnalité est utile à certain·es, le plugin est là pour être modifié, amélioré, etc.
    
    
    
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@108307 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ valider_xml/lang/paquet-valider_xml_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// V
+	'valider_xml_description' => 'Historiquement, la page exec=valider_xml permet de d’analyser un fichier  ou tout un répertoire pour vérifier la syntaxe XML. Ce plugin repropose cette page.',
+	'valider_xml_nom' => 'Valider XML',
+	'valider_xml_slogan' => 'Visualisation de validations XML',
+);
