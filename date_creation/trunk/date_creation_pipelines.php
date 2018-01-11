9df65e8f994126aa3795ff72ec80015d13b3b1d5
nicolas.dorigny@gmail.com <nicolas.dorigny@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-01-11 20:11:15 +0000

    On change le préfixe tant que c'est encore frais : date_creation au lieu de datecreation
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@108459 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ date_creation/trunk/date_creation_pipelines.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Utilisations de pipelines par Date de création
+ *
+ * @plugin     Date de création
+ * @copyright  2018
+ * @author     nicod_
+ * @licence    GNU/GPL
+ * @package    SPIP\Datecreation\Pipelines
+ */
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+include_spip('inc/config');
+
+/**
+ * Enregistrer la date de creation lors de l'insertion d'un objet
+ *
+ * @param array $flux
+ *
+ * @return array
+ */
+function date_creation_pre_insertion($flux) {
+	include_spip('date_creation_administrations');
+	date_creation_creer_champs_date_creation();
+
+	$tables = unserialize(lire_config('date_creation/objets'));
+	if (is_array($tables) && in_array($flux['args']['table'], $tables)) {
+		$flux['data']['date_creation'] = date('Y-m-d H:i:s');
+	}
+
+	return $flux;
+}
+
+/**
+ * Afficher la date de creation sur la fiche d'un objet
+ *
+ * @param array $flux
+ *
+ * @return array
+ */
+function date_creation_afficher_contenu_objet($flux) {
+
+	$tables = unserialize(lire_config('date_creation/objets'));
+	$table  = table_objet_sql($flux['args']['type']);
+
+	if (is_array($tables)
+		&& in_array($table, $tables)
+		&& $id_objet = $flux['args']['id_objet']
+	) {
+		$id_table_objet = id_table_objet($flux['args']['type']);
+		$date_creation  = sql_getfetsel('date_creation', $table, $id_table_objet . '=' . intval($id_objet));
+		$date_creation  = intval($date_creation) ? affdate_heure($date_creation) : _T('date_creation:non_renseignee');
+		$flux['data']   .= '<div><strong>' . propre(_T('date_creation:date_creation') . " :</strong> " . $date_creation) . '</div>';
+	}
+
+	return $flux;
+}
---
+++ date_creation/trunk/lang/paquet-date_creation_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// D
+	'date_creation_nom'         => 'Date de création',
+	'date_creation_slogan'      => 'Une date de création sur tous les objets de SPIP',
+	
+);
