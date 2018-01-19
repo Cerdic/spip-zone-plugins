edd3b0e76fa0dd48f43f73cd13fd104fc45345ca
tcharlss@bravecassine.com <tcharlss@bravecassine.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-01-19 07:36:11 +0000

    Un squelette à inclure pour présenter un résumé de la commande d'offre d'abonnement en cours (pour un tunnel de commande par ex.). Donc après qu'on a choisi une offre dans le formulair de commande. Le markup est identique aux offres présentées dans ce dernier. Afficher aussi la durée des offres (le titre n'est pas forcément 'abonnement mensuel|annuel', ça peut n'importe quoi).
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@108564 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- commandes_abonnements/trunk/lang/abonnementsoffre_fr.php
+++ commandes_abonnements/trunk/lang/abonnementsoffre_fr.php
@@ -5,9 +5,17 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 }
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
 	'champ_montant_minimum_label' => 'Montant minimum',
 	'champ_montant_perso_label' => 'Montant personnalisé',
 	'champ_montant_perso_label_case' => 'Activer la personnalisation du montant',
 	'champ_renouvellement_auto_label' => 'Renouvellement',
 	'champ_renouvellement_auto_label_case' => 'Activer le renouvellement automatique',
+
+	// I
+	'info_paiement_unique' => 'Paiement unique',
+	'info_paiement_renouvelable' => 'Prélèvement SEPA',
+	'info_dates_from_to' => 'Du @date_debut@ au @date_fin@',
+	'info_dates_from' => 'À partir du @date_debut@',
 );
