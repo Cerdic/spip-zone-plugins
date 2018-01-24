638a8ffe7ca3048ce27f4294da481ce57b577358
tcharlss@bravecassine.com <tcharlss@bravecassine.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-01-24 13:35:50 +0000

    Branche v1 et trunk en dev temporairement
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@108602 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ metaplus/branches/v1/lang/metasplus_fr.php
@@ -0,0 +1,18 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// L
+	'label_reseaux' => 'Quels métas voulez-vous ne pas afficher ?',
+	'label_dublincore' => 'Dublin Core',
+	'label_opengraph' => 'Open Graph',
+	'label_twitter' => 'Twitter',
+	// T
+	'titre_config_metasplus'	=>	'Configurer Métas +',
+
+);
