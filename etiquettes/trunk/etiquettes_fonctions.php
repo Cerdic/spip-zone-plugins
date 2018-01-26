e093a6cf0dbf40e42c20ccbd498607e7c18a27c4
tofulm@gmail.com <tofulm@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-01-26 09:24:09 +0000

    On ajoute le hack de la fonction boolval compatible php >= 5.5
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@108637 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- etiquettes/trunk/etiquettes_fonctions.php
+++ etiquettes/trunk/etiquettes_fonctions.php
@@ -11,20 +11,27 @@
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 function etiquettes_position_quot($valeur){
-	
+
 	if (($position = strpos($valeur, "&quot;")) === false)
 		return 100000;
 	else
 		return $position;
-	
+
 }
 
 function ajouter_etiquettes($texte, $id, $groupe_defaut='tags', $type, $id_type, $clear){
-	
+
 	include_spip('inc/tag-machine');
 	$clear = ($clear == "true");
 	ajouter_liste_mots($texte, $id, $groupe_defaut, $type, $id_type, $clear);
-	
+
+}
+
+// Hack for old php versions to use boolval()
+if (!function_exists('boolval')) {
+	function boolval($val) {
+		return (bool) $val;
+	}
 }
 
 ?>
