507b28b3277022a7453804ec113c233c12e28077
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-01-31 14:49:18 +0000

    Ajout des icones du plugin ; - On reprend les feuilles de styles de la librairie Socicon 3.6 ; - Ajout du lien vers la documentation du plugin ; - Up de z
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@108713 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- socicon/trunk/socicon_pipelines.php
+++ socicon/trunk/socicon_pipelines.php
@@ -10,26 +10,40 @@
  */
 
 if (!defined('_ECRIRE_INC_VERSION')) {
-	return;
+  return;
 }
 
-function socicon_sociaux_lister($flux){
-	include_spip('inc/config');
-	$socicon_config = lire_config('socicon');
+function socicon_sociaux_lister($flux) {
+  include_spip('inc/config');
+  $socicon_config = lire_config('socicon');
 
-	if (empty($socicon_config) or count($socicon_config) == 0) {
-		$socicon_config = array('facebook','twitter','instagram','googleplus','blogger','pinterest','linkedin','youtube','rss','mail','tripadvisor','vimeo','flickr');
-	}
-	$sociaux_pipe = array_keys($flux['data']);
-	$diff = array_diff($sociaux_pipe, $socicon_config);
-	if (is_array($diff) and count($diff)) {
-		foreach ($diff as $value) {
-			unset($flux['data'][$value]);
-		}
-	}
-	foreach ($socicon_config as $value) {
-		$flux['data'][$value] = ucwords($value);
-	}
+  if (empty($socicon_config) or count($socicon_config) == 0) {
+    $socicon_config = array(
+      'facebook',
+      'twitter',
+      'instagram',
+      'googleplus',
+      'blogger',
+      'pinterest',
+      'linkedin',
+      'youtube',
+      'rss',
+      'mail',
+      'tripadvisor',
+      'vimeo',
+      'flickr',
+    );
+  }
+  $sociaux_pipe = array_keys($flux['data']);
+  $diff = array_diff($sociaux_pipe, $socicon_config);
+  if (is_array($diff) and count($diff)) {
+    foreach ($diff as $value) {
+      unset($flux['data'][$value]);
+    }
+  }
+  foreach ($socicon_config as $value) {
+    $flux['data'][$value] = ucwords($value);
+  }
 
-	return $flux;
+  return $flux;
 }
