797679945baf8e2217a0e74bc33281e7d7f9934d
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-01-31 11:43:03 +0000

    Mise a jour du UA sur les requetes CURL pour reparer l'oembed Mixcloud
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@108710 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- oembed/inc/oembed_recuperer_url.php
+++ oembed/inc/oembed_recuperer_url.php
@@ -31,7 +31,7 @@ function inc_oembed_recuperer_url($oembed_url, $url, $format) {
 		curl_setopt($c, CURLOPT_FOLLOWLOCATION, true);
 		curl_setopt($c, CURLOPT_SSL_VERIFYHOST, false);
 		curl_setopt($c, CURLOPT_SSL_VERIFYPEER, false);
-		$browser = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.16 (KHTML, like Gecko) \Chrome/24.0.1304.0 Safari/537.16';
+		$browser = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:58.0) Gecko/20100101 Firefox/58.0';
 		curl_setopt($c, CURLOPT_USERAGENT, $browser);
 		//curl_setopt($c, CURLOPT_SSLVERSION, 1);
 
