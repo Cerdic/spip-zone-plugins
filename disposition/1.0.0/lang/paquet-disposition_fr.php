f5aa1644c8ccf20913241715f8d780d9f823c689
mikhael.myara@ies.univ-montp2.fr <mikhael.myara@ies.univ-montp2.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-02-04 11:09:23 +0000

    Passage du format "plugin.xml" à "paquet.xml"
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@108793 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ disposition/1.0.0/lang/paquet-disposition_fr.php
@@ -0,0 +1,16 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+// 
+// Module: paquet-animatecss
+
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// S
+	'disposition_description' => 'Rend possible la mise en page d\'un article avec des blocs responsive dans l\'espace de rédaction via Bootstrap. Complément idéal pour SPIP-R.',
+	'disposition_slogan' => 'Ajoute de nouveaux modèles exploitant certaines fonctionnatliés de bootstrap : blocs responsive avec styles CSS, carousel, boutons, objets visuels. Patche bootstrap pour rendre les boites modales exploitables. Patche faiblement image_responsive.',
+);
+?>
