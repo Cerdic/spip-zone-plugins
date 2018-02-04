eb68b45730b72b0490743b3030a08e114ba3e07d
mikhael.myara@ies.univ-montp2.fr <mikhael.myara@ies.univ-montp2.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-02-04 11:12:41 +0000

    Passage du format "plugin.xml" à "paquet.xml"
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@108796 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ picto/1.0.0/lang/paquet-picto_fr.php
@@ -0,0 +1,16 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+// 
+// Module: paquet-animatecss
+
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// S
+	'picto_description' => 'Permet d\'utiliser FontAwesome sous SPIP',
+	'picto_slogan' => 'Permet d\'utiliser FontAwesome sous SPIP',
+);
+?>
