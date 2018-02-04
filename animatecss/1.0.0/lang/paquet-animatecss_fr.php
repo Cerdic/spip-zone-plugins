2a85f5097165bb9854c61ff35be57d680cfc5141
mikhael.myara@ies.univ-montp2.fr <mikhael.myara@ies.univ-montp2.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-02-04 11:05:03 +0000

    Passage du format "plugin.xml" à "paquet.xml"
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@108790 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ animatecss/1.0.0/lang/paquet-animatecss_fr.php
@@ -0,0 +1,16 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+// 
+// Module: paquet-animatecss
+
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// S
+	'animatecss_description' => 'Permet d\'utiliser AnimateCSS sous SPIP',
+	'animatecss_slogan' => 'Permet d\'utiliser AnimateCSS sous SPIP',
+);
+?>
