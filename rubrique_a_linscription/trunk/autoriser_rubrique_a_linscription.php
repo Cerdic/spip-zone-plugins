3c81189c02351c302f75cefb86f25684ee45d3fe
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-02-04 12:08:08 +0000

    formatage un peu plus lisible
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@108798 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- rubrique_a_linscription/trunk/autoriser_rubrique_a_linscription.php
+++ rubrique_a_linscription/trunk/autoriser_rubrique_a_linscription.php
@@ -3,43 +3,46 @@ function rubrique_a_linscription_autoriser(){}
 
 // Selon la config, autoriser ou pas l'inscription direct d'administrateur. Attention ca suppose qu'on utilise pas de formulaire d'inscription autre que ceux qui font des admins restreints, sinon ca devient dangereux
 function autoriser_0minirezo_inscrireauteur($faire, $quoi, $id, $qui, $opt){
-	spip_log("sncf",$id);
 	$rubrique_a_linscription_statut = lire_config("rubrique_a_linscription/statut");
-	return $rubrique_a_linscription_statut==$quoi;
+	return $rubrique_a_linscription_statut == $quoi;
 }
 
 function autoriser_rubrique_a_linscription($faire, $type, $id, $qui,  $opt){
-	if ($faire	== 'configurer'){
+	if ($faire == 'configurer'){
 		include_spip('inc/autoriser');
 		return autoriser_configurer_dist($faire,$type,$id,$qui,$opt);	
 	}
 }
 
 function autoriser_rubrique_creerarticledans($faire, $type, $id, $qui, $opt) {
-	
 	include_spip('inc/autoriser');
-	$espace_prive_creer 	= lire_config('rubrique_a_linscription/espace_prive_creer');
-	$resultat = sql_fetsel('rubrique_a_linscription','spip_auteurs','id_auteur='.$qui['id_auteur']);
-	if (!$espace_prive_creer or is_null($resultat['rubrique_a_linscription'])){
-		return $id AND autoriser('voir','rubrique',$id);
-	}
-	else {
-		
+	$espace_prive_creer = lire_config('rubrique_a_linscription/espace_prive_creer');
+	$resultat = sql_fetsel(
+		'rubrique_a_linscription',
+		'spip_auteurs',
+		'id_auteur='.$qui['id_auteur']
+	);
+	if (!$espace_prive_creer 
+			or is_null($resultat['rubrique_a_linscription'])
+		) {
+			return $id AND autoriser('voir','rubrique',$id);
+	} else {
+
 		if ($resultat['rubrique_a_linscription'] == $id){
 			return true;	
-		}
-		else{
+		} else{
 			return false;
 		}
 	}
 }
 
 function autoriser_voir($faire, $type, $id, $qui, $opt) {
-
 	include_spip('inc/autoriser');
-
 	$espace_prive_voir = lire_config('rubrique_a_linscription/espace_prive_voir');
-	$resultat = sql_fetsel('rubrique_a_linscription,statut','spip_auteurs','id_auteur='.$qui['id_auteur']);
+	$resultat = sql_fetsel(
+		'rubrique_a_linscription,statut',
+		'spip_auteurs',
+		'id_auteur='.$qui['id_auteur']);
 
 	
 	if (!$espace_prive_voir or is_null($resultat['rubrique_a_linscription'])){
@@ -48,33 +51,47 @@ function autoriser_voir($faire, $type, $id, $qui, $opt) {
 	
 	$liste_rubriques_auteur = remonter_hierarchie_rubriques(liste_rubriques_auteur($qui['id_auteur']));
 	
-	
-	if ($type == 'document')
+	if ($type == 'document') {
 		return autoriser_document_voir_dist($faire, $type, $id, $qui, $opt);
+	}
+
 	if ($type == 'groupemots') {
-		$acces = sql_fetsel("comite,forum", "spip_groupes_mots", "id_groupe=".intval($id));
-		if ($qui['statut']=='1comite' AND ($acces['comite'] == 'oui' OR $acces['forum'] == 'oui'))
-			return true;
-		if ($qui['statut']=='6forum' AND $acces['forum'] == 'oui')
-			return true;
+		$acces = sql_fetsel(
+			"comite,forum", 
+			"spip_groupes_mots", 
+			"id_groupe=".intval($id)
+		);
+		
+		if ($qui['statut'] == '1comite' 
+				and (
+					$acces['comite'] == 'oui' 
+					or $acces['forum'] == 'oui')
+				) {
+					return true;
+		}
+		
+		if ($qui['statut']=='6forum' and $acces['forum'] == 'oui') {
+				return true;
+		}
 		return false;
 	}
 	if ($type == 'rubrique'){
 		return in_array($id,$liste_rubriques_auteur);
 	}
+
 	if ($type == 'article' or $type == 'breve'){
-		$rubrique_objet = sql_getfetsel('id_rubrique','spip_'.$type.'s','id_'.$type.'='.$id);
+		$rubrique_objet = sql_getfetsel(
+			'id_rubrique',
+			'spip_'.$type.'s',
+			'id_'.$type.'='.$id);
 		
 		if (in_array($rubrique_objet,$liste_rubriques_auteur)){
-			
 			return autoriser_voir_dist($faire, $type, $id, $qui, $opt);
-		}
-		else{
+		} else {
 			return false;	
 		}
 	}
 	return autoriser_voir_dist($faire, $type, $id, $qui, $opt);
-	
 	
 }
 function remonter_hierarchie_rubriques($rubriques){
@@ -82,9 +99,18 @@ function remonter_hierarchie_rubriques($rubriques){
 	include_spip('base/abstract');
 	foreach ($rubriques as $id_rubrique){
 		$toutes_rubriques[]	= $id_rubrique;
-		while ($id_rubrique = sql_getfetsel("id_parent","spip_rubriques","id_rubrique=" . $id_rubrique,"","","", "", $connect)){
-			settype($id_rubrique,'int')	;
-		 	$toutes_rubriques[] = $id_rubrique ;
+
+		while ($id_rubrique = sql_getfetsel(
+			"id_parent",
+			"spip_rubriques",
+			"id_rubrique=" . $id_rubrique,
+			"",
+			"",
+			"", 
+			"", $connect)
+		) {
+			settype($id_rubrique,'int');
+		 	$toutes_rubriques[] = $id_rubrique;
 		 }	 
 	}
 	return $toutes_rubriques;
--- rubrique_a_linscription/trunk/rubrique_a_linscription.php
+++ rubrique_a_linscription/trunk/rubrique_a_linscription.php
@@ -1,40 +1,44 @@
 <?php
-/*charger*/
+
 function rubrique_a_linscription_formulaire_charger($flux){
 	$explicite = lire_config('rubrique_a_linscription/formulaire_explicite');
 	$statut = lire_config('rubrique_a_linscription/statut');
 	$prive_voir = lire_config('rubrique_a_linscription/espace_prive_voir');
 
 	if (
-	    ($flux['args']['form']=='inscription' or $flux['args']['form']=='inscription_avec_rubrique') 
-	    and lire_config('accepter_inscriptions')=='oui' 
+			($flux['args']['form'] == 'inscription' 
+			or $flux['args']['form'] == 'inscription_avec_rubrique') 
+	    and lire_config('accepter_inscriptions') == 'oui' 
 	    and (!$explicite) 
-	    or ($explicite == 'on' and $flux['args']['form']=='inscription_avec_rubrique')
+			or (
+					$explicite == 'on' 
+					and $flux['args']['form'] == 'inscription_avec_rubrique'
+				)
 	   ){
-		
-		
-		$flux['args']['args'][0] = $statut; 
-		$flux['data']['_commentaire'] = _T('rubrique_a_linscription:rubrique_reserve_'.$statut.'_'.$prive_voir);
-		
+			 $flux['args']['args'][0] = $statut; 
+			 $flux['data']['_commentaire'] = _T('rubrique_a_linscription:rubrique_reserve_'.$statut.'_'.$prive_voir);
 	}
 	return $flux;
 }
+
 
 function rubrique_a_linscription_formulaire_verifier($flux){
 	$priver_creer = lire_config('rubrique_a_linscription/espace_prive_creer');
 
-	if ($flux['args']['form']=='editer_article' and $prive_creer=='on') {
-		$id_rubrique =_request('id_parent');
-		settype($id_rubrique,"string");
-		if (! autoriser('creerarticledans','rubrique',$id_rubrique)){
-			$flux['data']['erreurs']['id_parent'] = _T('rubrique_a_linscription:pas_autoriser_rubriquer_creerarticledans');
-		}
-	}
+	if ($flux['args']['form'] == 'editer_article' 
+			and $prive_creer == 'on') {
+
+				$id_rubrique =_request('id_parent');
+				settype($id_rubrique,"string");
+
+				if (!autoriser('creerarticledans','rubrique',$id_rubrique)) {
+					$flux['data']['erreurs']['id_parent'] = _T('rubrique_a_linscription:pas_autoriser_rubriquer_creerarticledans');
+				}
+			}
 	return $flux;
 }
 
 
-/* Traiter */
 function rubrique_a_linscription_formulaire_traiter($flux){
 	$explicite = lire_config('rubrique_a_linscription/formulaire_explicite');
 	$statut = lire_config('rubrique_a_linscription/statut');
@@ -44,78 +48,90 @@ function rubrique_a_linscription_formulaire_traiter($flux){
 	$mail_prive = lire_config('rubrique_a_linscription/mail_prive');
 
 	if (
-	    ($flux['args']['form']=='inscription' or $flux['args']['form']=='inscription_avec_rubrique') 
+			($flux['args']['form'] == 'inscription' 
+			or $flux['args']['form'] == 'inscription_avec_rubrique') 
 	    and (!$explicite) 
-	    or ($explicite=='on' 
-	    and $flux['args']['form']=='inscription_avec_rubrique')
+			or (
+					$explicite == 'on' 
+					and $flux['args']['form'] == 'inscription_avec_rubrique')
 	   ){
 
-		// Récuperation des paramètres
-		$mail = _request('mail_inscription');
-		$nom_inscription = str_replace('@',' (chez) ',_request('nom_inscription'));
-		include_spip('base/abstract_sql');
-		$id_auteur = sql_getfetsel('id_auteur','spip_auteurs','email='.sql_quote($mail));
+				// Récuperation des paramètres
+				$mail = _request('mail_inscription');
+				$nom_inscription = str_replace('@',' (chez) ',_request('nom_inscription'));
+				include_spip('base/abstract_sql');
+				$id_auteur = sql_getfetsel('id_auteur','spip_auteurs','email='.sql_quote($mail));
 		
 		
-		//Modification du statut temporaire
-		sql_updateq('spip_auteurs',array('prefs'=>$statut),'id_auteur='.$id_auteur); 
+				//Modification du statut temporaire
+				sql_updateq('spip_auteurs',array('prefs'=>$statut),'id_auteur='.$id_auteur); 
 
 
-		// Utilise comme rubrique mere celle qui est passé explicitement au formulaire ou celle de la config ?
-		if  (isset ($flux["args"]["args"][0])) {
-			$reqtest = sql_select('id_rubrique','spip_rubriques',"id_rubrique=".$flux["args"]["args"][0]);
-			if ($reqtest){
-				$id_parent =  $flux["args"]["args"][0];
-			}		
-		}
-		// Création de la rubrique
-		include_spip('inc/rubriques');
-		$titre_rubrique = _T('rubrique_a_linscription:titre_rubrique',array('nom'=>$nom_inscription));
-		$id_rubrique = creer_rubrique_nommee($titre_rubrique, $id_parent);
+				// Utiliser comme rubrique mere celle qui est passé explicitement au formulaire ou celle de la config ?
+				if (isset ($flux["args"]["args"][0])) {
+					$reqtest = sql_select('id_rubrique','spip_rubriques',"id_rubrique=".$flux["args"]["args"][0]);
+					if ($reqtest) {
+						$id_parent =  $flux["args"]["args"][0];
+					}		
+				}
+
+				// Création de la rubrique
+				include_spip('inc/rubriques');
+				$titre_rubrique = _T('rubrique_a_linscription:titre_rubrique',array('nom'=>$nom_inscription));
+				$id_rubrique = creer_rubrique_nommee($titre_rubrique, $id_parent);
 		
 		
-		sql_insertq('spip_auteurs_liens', array(
-		'id_auteur' => $id_auteur,
-		'objet'		=>'rubrique',
-		'vu'		=>'non',
-		'id_objet' => $id_rubrique));
-		spip_log('Création de la rubrique '.$id_rubrique.' pour l\'auteur '.$nom_inscription.' ( '.$mail.' )','rubrique_a_linscription');
+				sql_insertq('spip_auteurs_liens', array(
+					'id_auteur' => $id_auteur,
+					'objet'		=>'rubrique',
+					'vu'		=>'non',
+					'id_objet' => $id_rubrique)
+				);
+
+				spip_log('Création de la rubrique '.$id_rubrique.' pour l\'auteur '.$nom_inscription.' ( '.$mail.' )','rubrique_a_linscription');
 		
-		//On ajoute la rubrique chez l'auteur
-		sql_update('spip_auteurs',array('rubrique_a_linscription'=>$id_rubrique),"id_auteur=$id_auteur");
+				//On ajoute la rubrique chez l'auteur
+				sql_update('spip_auteurs',array('rubrique_a_linscription'=>$id_rubrique),"id_auteur=$id_auteur");
 		
-		//Création du mot clef associé
-		if($groupe_mots){
-			$type   = sql_getfetsel('titre','spip_groupes_mots','id_groupe='.$meta['groupe_mots']);
-			if ($type){
-				$id_mot = sql_insertq('spip_mots',array('id_groupe'=>$groupe_mots,'type'=>$type,'titre'=>_T('rubrique_a_linscription:mot_clef_de',array('nom'=>$nom_inscription))));
-				spip_log("Création du mot clef dans le groupe $type pour l'auteur $nom_inscription (id mot = $id_mot)",'rubrique_a_linscription');
+				//Création du mot clef associé
+				if($groupe_mots) {
+					$type   = sql_getfetsel('titre','spip_groupes_mots','id_groupe='.$meta['groupe_mots']);
+					if ($type) {
+						$id_mot = sql_insertq('spip_mots',array(
+							'id_groupe' => $groupe_mots,
+							'type' => $type,
+							'titre' => _T('rubrique_a_linscription:mot_clef_de',array('nom'=>$nom_inscription))
+							)
+						);
+						spip_log("Création du mot clef dans le groupe $type pour l'auteur $nom_inscription (id mot = $id_mot)",'rubrique_a_linscription');
 			}
 		}
 		
-		//Envoyer mails
-		
-		if ($mail_public or $mail_prive){
-			
-			$envoyer_mail 	= charger_fonction('envoyer_mail','inc');
-			
-			$corps		  	= _T('rubrique_a_linscription:mail_adresse_rubrique');
-			include_spip('inc/utils');
-			
-			if ($mail_public){
-				$corps 		.= 	"-".url_absolue(generer_url_public("rubrique","id_rubrique=$id_rubrique"))."\n";
-			}
-			
-			if ($mail_prive){
-				$corps 		.= 	"-".generer_url_ecrire("rubrique","id_rubrique=$id_rubrique")."\n";
-			}
-			include_spip('inc/filtres');
-			
-			$envoyer_mail($mail,'['.extraire_multi(lire_config('nom_site')).']'. _T('rubrique_a_linscription:titre_mail_adresse_rubrique'),$corps);
-		}
-		
+				//Envoyer mails
+				
+				if ($mail_public or $mail_prive){
+					
+					$envoyer_mail = charger_fonction('envoyer_mail','inc');
+					
+					$corps = _T('rubrique_a_linscription:mail_adresse_rubrique');
+					include_spip('inc/utils');
+					
+					if ($mail_public) {
+						$corps .= "-".url_absolue(generer_url_public("rubrique","id_rubrique=$id_rubrique"))."\n";
+					}
+					
+					if ($mail_prive) {
+						$corps .= "-".generer_url_ecrire("rubrique","id_rubrique=$id_rubrique")."\n";
+					}
+					include_spip('inc/filtres');
+
+					$titre = 	'['.extraire_multi(lire_config('nom_site')).']'. _T('rubrique_a_linscription:titre_mail_adresse_rubrique');
+					$envoyer_mail(
+						$mail,
+						$titre,
+						$corps);
+				}
 	}
-
 	return $flux;	
 }
 
