38697bc6698e82917999fdde0b2961b7f2ccc2cf
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-02-05 21:53:03 +0000

    mutualisation de la fonction qui génère les noisettes, pour pouvoir être appelée ailleurs
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@108827 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- profils/trunk/inc/profils.php
+++ profils/trunk/inc/profils.php
@@ -92,6 +92,132 @@ function profils_chercher_saisies_objet($objet) {
 }
 
 /**
+ * Cherche les saisies configurées pour un profil pour tel formulaire (inscription ou édition du profil)
+ * 
+ * @param string $form
+ * 		Quel formulaire : "inscription" ou "edition" 
+ * @param $id_auteur 
+ * 		Identifiant du compte utilisateur dont on cherche les saisies
+ * @param $id_ou_identifiant_profil 
+ * 		Identifiant du profil, notamment si c'est une création
+ * @return array
+ * 		Retourne le tableau des saisies du profil
+ */
+function profils_chercher_saisies_profil($form, $id_auteur='', $id_ou_identifiant_profil='') {
+	$saisies = array();
+	
+	// S'il y a un id_auteur on cherche s'il a un profil
+	if (intval($id_auteur) > 0 and $id_profil = sql_getfetsel('id_profil', 'spip_auteurs', 'id_auteur = '.intval($id_auteur))) {
+		$id_ou_identifiant_profil = $id_profil;
+	}
+	
+	// On ne continue que si on a un profil sous la main
+	if ($profil = profils_chercher_profil($id_ou_identifiant_profil) and $config = $profil['config']) {
+		foreach (array('auteur', 'organisation', 'contact') as $objet) {
+			// Si c'est autre chose que l'utilisateur, faut le plugin qui va avec et que ce soit activé
+			if ($objet == 'auteur' or (defined('_DIR_PLUGIN_CONTACTS') and $config["activer_$objet"])) {
+				// On récupère les champs pour cet objet ET ses champs extras s'il y a
+				$saisies_objet = profils_chercher_saisies_objet($objet);
+				$saisies_a_utiliser = array();
+				
+				// Pour chaque chaque champ vraiment configuré
+				foreach ($config[$objet] as $champ => $config_champ) {
+					// On cherche la saisie pour ce champ SI c'est pour le form demandé
+					if (in_array($form, $config_champ) and $saisie = saisies_chercher($saisies_objet, $champ)) {
+						// On modifie son nom
+						$saisie['options']['nom'] = $objet . '[' . $saisie['options']['nom'] . ']';
+						// On modifie son obligatoire suivant la config
+						$saisie['options']['obligatoire'] = in_array('obligatoire', $config_champ) ? 'oui' : false;
+						// On ajoute la saisie
+						$saisies_a_utiliser[] = $saisie;
+					}
+				}
+				
+				// On cherche des coordonnées pour cet objet
+				if (
+					defined('_DIR_PLUGIN_COORDONNEES')
+					and $config["activer_coordonnees_$objet"]
+					and $coordonnees = $config['coordonnees'][$objet]
+				) {
+					// Pour chaque type de coordonnéees (num, email, adresse)
+					foreach ($coordonnees as $coordonnee => $champs) {
+						// Pour chaque champ ajouté
+						foreach ($champs as $cle => $champ) {
+							// Si ce cette coordonnées est configurée pour le form demandé
+							if ($champ[$form]) {
+								// Attention, si pas de type, on transforme ici en ZÉRO
+								if (!$champ['type']) {
+									$champ['type'] = 0;
+								}
+								// On va chercher les saisies de ce type de coordonnées
+								$saisies_coordonnee = profils_chercher_saisies_objet($coordonnee);
+								// On vire le titre libre
+								$saisies_coordonnee = saisies_supprimer($saisies_coordonnee, 'titre');
+								// On change le nom de chacun des champs
+								$saisies_coordonnee =  saisies_transformer_noms(
+									$saisies_coordonnee,
+									'/^\w+$/',
+									"coordonnees[$objet][$coordonnee][${champ['type']}][\$0]"
+								);
+								// On reconstitue le label
+								$label = $champ['label'] ? $champ['label'] : _T(objet_info(table_objet_sql($coordonnee), 'texte_objet'));
+								if ($champ['type'] and !$champ['label']) {
+									$label .= ' (' . coordonnees_lister_types_coordonnees(objet_type($coordonnee), $champ['type']) . ')';
+								}
+								// Si c'est un numéro ou un email on change peut-être le label du champ lui-même et le obligatoire
+								if (in_array($coordonnee, array('numeros', 'emails'))) {
+									$saisies_coordonnee = saisies_modifier(
+										$saisies_coordonnee,
+										"coordonnees[$objet][$coordonnee][${champ['type']}][" . objet_type($coordonnee) . ']',
+										array(
+											'options' => array(
+												'label' => $label,
+												'obligatoire' => $champ['obligatoire'] ? 'oui' : false,
+											),
+										)
+									);
+									// On ajoute enfin
+									$saisies_a_utiliser	= array_merge($saisies_a_utiliser, $saisies_coordonnee);
+								}
+								// Alors que si c'est une adresse on l'utilise pour le groupe de champs
+								else {
+									$saisies_a_utiliser[] = array(
+										'saisie' => 'fieldset',
+										'options' => array(
+											'nom' => "groupe_${coordonnee}_$cle",
+											'label' => $label,
+										),
+										'saisies' => $saisies_coordonnee,
+									);
+								}
+							}
+						}
+					}
+				}
+				
+				// On teste s'il faut un groupe de champs ou pas pour cet objet
+				if ($saisies_a_utiliser and $legend = $config["activer_groupe_$objet"]) {
+					$saisies[] = array(
+						'saisie' => 'fieldset',
+						'options' => array(
+							'nom' => "groupe_$objet",
+							'label' => $legend,
+						),
+						'saisies' => $saisies_a_utiliser,
+					);
+				}
+				// Sinon on les ajoute directement
+				else {
+					$saisies = array_merge($saisies, $saisies_a_utiliser);
+				}
+			}
+		}
+	}
+		
+	return $saisies;
+}
+
+/**
  * Récupérer tous les identifiants des objets liés à un profil
  *
  * @param int $id_auteur=0
