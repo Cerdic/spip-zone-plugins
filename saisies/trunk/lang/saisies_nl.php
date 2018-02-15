69400e5b7fa90fa4bf89b1ca5319ece7b42a5b02
hanjo.spip@gmail.com <hanjo.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-02-15 04:37:23 +0000

    [Salvatore] [source:_plugins_/saisies/trunk/lang/ saisies] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@108959 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- saisies/trunk/lang/saisies_nl.php
+++ saisies/trunk/lang/saisies_nl.php
@@ -60,6 +60,8 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'option_aff_rub_interface_label' => 'Meertalige display',
 	'option_afficher_si_explication' => 'Geef de voorwaarde op voor het vertonen van het veld in functie van de waarde van andere velden. De identificatie van de andere velden moet tussen <code>@</code> worden geplaatst. <br />Bijvoorbeeld <code>@selection_1@=="Toto"</code> geeft de voorwaarde aan dat het veld moet worden getoond wanneer veld <code>selection_1</code> de waarde <code>Toto</code> heeft.',
 	'option_afficher_si_label' => 'Tonen onder voorwaarde',
+	'option_afficher_si_remplissage_uniquement_explication' => 'Door het aanvinken van dit vakje wordt de voorwaardelijke weergave alleen toegepast bij het invullen van het formulier en niet bij het weergeven van de resultaten.',
+	'option_afficher_si_remplissage_uniquement_label' => 'Alleen bij het invullen van het formulier',
 	'option_attention_explication' => 'Een boodschap die belangrijker is dan de uitleg.',
 	'option_attention_label' => 'Waarschuwing',
 	'option_attribut_title_label' => 'Zet een title attribuut op het label, dat de waarde van het veld bevat. Gebruiken met voorbehoud.',
