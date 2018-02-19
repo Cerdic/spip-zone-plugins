c7ebdf51f68e7bb640b215754f2a272ebdc6ddd3
george@middleeastwatch.net <george@middleeastwatch.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-02-19 04:37:19 +0000

    [Salvatore] [source:_plugins_/newsletters/trunk/lang/ newsletters] Export depuis http://trad.spip.net de la langue ar
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@109007 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ newsletters/trunk/lang/newsletters_ar.php
@@ -0,0 +1,22 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/newsletters?lang_cible=ar
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cfg_exemple' => 'مثال',
+	'cfg_exemple_explication' => 'تفسير هذا المثال',
+	'cfg_titre_parametrages' => 'إعداد',
+
+	// N
+	'newsletters_titre' => 'النشرات البريدية',
+
+	// T
+	'titre_page_configurer_newsletters' => 'إعداد النشرات البريدية'
+);
