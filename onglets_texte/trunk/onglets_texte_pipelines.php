89207b231809674b4dee69e3e6679045bd43b4b9
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-02-20 09:43:46 +0000

    Eviter un warning PHP. Up de z.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@109051 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- onglets_texte/trunk/onglets_texte_pipelines.php
+++ onglets_texte/trunk/onglets_texte_pipelines.php
@@ -5,16 +5,19 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 }
 
 
+$p = explode(basename(_DIR_PLUGINS) . "/", str_replace('\\', '/', realpath(dirname(__FILE__))));
+if (!defined('_DIR_PLUGIN_ONGLETS_TEXTE')) {
+	define('_DIR_PLUGIN_ONGLETS_TEXTE', (_DIR_PLUGINS . end($p)));
+}
 
-$p = explode(basename(_DIR_PLUGINS)."/",str_replace('\\','/',realpath(dirname(__FILE__))));
-define('_DIR_PLUGIN_ONGLETS_TEXTE',(_DIR_PLUGINS.end($p)));
+function onglets_texte_insert_head($flux) {
+	$flux .= '<script type="text/javascript" src="' . find_in_path('javascript/mes_onglets.js') . '"></script>';
 
-function onglets_texte_insert_head($flux){
-	$flux .= '<script type="text/javascript" src="'.find_in_path('javascript/mes_onglets.js').'"></script>';
 	return $flux;
 }
 
-function onglets_texte_insert_head_css($flux){
-	$flux .= '<link rel="stylesheet" href="'.find_in_path('css/tab.css').'" type="text/css" media="all" />';
+function onglets_texte_insert_head_css($flux) {
+	$flux .= '<link rel="stylesheet" href="' . find_in_path('css/tab.css') . '" type="text/css" media="all" />';
+
 	return $flux;
 }
