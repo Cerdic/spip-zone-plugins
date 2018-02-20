5abbbf69bf5aa29acf50e471e52dcd050c2c380f
rporto@calepino.com <rporto@calepino.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-02-20 04:37:39 +0000

    [Salvatore] [source:_plugins_/uploadhtml5/trunk/lang/ uploadhtml5] Export depuis http://trad.spip.net de la langue pt_br
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@109046 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- uploadhtml5/trunk/lang/uploadhtml5_pt_br.php
+++ uploadhtml5/trunk/lang/uploadhtml5_pt_br.php
@@ -13,6 +13,8 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'case_charger_public' => 'Carregar os scripts na área pública',
 	'case_remplacer_editer_logo' => 'Substituir o formulário de upload de logo do SPIP',
 	'cfg_titre_parametrages' => 'Parâmetros',
+	'contain' => 'Reduzir',
+	'crop' => 'Reenquadrar',
 
 	// D
 	'drop_annuler' => 'Envio cancelado',
@@ -27,6 +29,8 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// E
 	'explication_max_file' => 'Número máximo de arquivos que podem ser transferidos simultaneamente (0 para não limitar).',
 	'explication_max_file_size' => 'Tamanhho máximo dos arquivos (em MB). É aconselhável usar o valor do servidor (@uploadmaxsize@).',
+	'explication_resizeMethod' => 'Método de redução das imagens no caso da altura <strong>e</strong> da largura serem forçadas.',
+	'explication_resizeWidth' => 'A imagem será reduzida a estas dimensões antes de ser enviada. Se apenas um desses valores for trocado, altura oou argura, a proporção da imagem será respeitada. ',
 
 	// L
 	'label_charger_public' => 'Script no espaço público',
@@ -37,7 +41,14 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'max_file' => 'Número máximo de arquivos',
 	'max_file_size' => 'Tamanho máximo dos arquivos',
 
+	// R
+	'resizeHeight' => 'Reenquadrar/reduzir a altura',
+	'resizeMethod' => 'Método',
+	'resizeQuality' => 'Qualidade (em %)',
+	'resizeWidth' => 'Reenquadrar/reduzir a largura',
+
 	// T
+	'titre_fieldset_image' => 'Alterar as imagens',
 	'titre_page_configurer_uploadhtml5' => 'Configuração do formulário de upload html5',
 
 	// U
