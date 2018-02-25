f8167daf139994907011e9f5b83e316f101f894a
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-02-25 20:43:56 +0000

    paquet.xml + fichiers de langues (très) minimaux
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@109141 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ saisie_evenements/trunk/lang/paquet-saisie_evenement_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// S
+	'saisie_evenement_description' => 'Ce plugin fournit une saisie permettant de choisir entre un et plusieurs événements gérés par le plugin Agenda.
+Utile par exemple pour des formulaires d\'inscription personnalisés avec Formidable.',
+	'saisie_evenement_nom' => 'Saisie de choix d\'évènement',
+	'saisie_evenement_slogan' => 'Choisissez vos dates!',
+);
