6f3028309c878fd4f986f9f9044964b26fbf5556
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-02-26 04:37:04 +0000

    [Salvatore] [source:_plugins_/saisie_evenements/trunk/lang/ paquet-saisie_evenement] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@109148 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- saisie_evenements/trunk/lang/paquet-saisie_evenement_fr.php
+++ saisie_evenements/trunk/lang/paquet-saisie_evenement_fr.php
@@ -1,5 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/saisie_evenements/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
@@ -8,7 +9,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// S
 	'saisie_evenement_description' => 'Ce plugin fournit une saisie permettant de choisir entre un et plusieurs événements gérés par le plugin Agenda.
-Utile par exemple pour des formulaires d\'inscription personnalisés avec Formidable.',
-	'saisie_evenement_nom' => 'Saisie de choix d\'évènement',
-	'saisie_evenement_slogan' => 'Choisissez vos dates!',
+Utile par exemple pour des formulaires d’inscription personnalisés avec Formidable.',
+	'saisie_evenement_nom' => 'Saisie de choix d’évènement',
+	'saisie_evenement_slogan' => 'Choisissez vos dates !'
 );
