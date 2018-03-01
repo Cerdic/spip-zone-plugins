f9fa3497f1b4442fc72a4bf960d640b8b3737a33
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-03-01 04:36:23 +0000

    [Salvatore] [source:_plugins_/saisie_evenements/trunk/lang/ saisie_evenement] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@109216 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- saisie_evenements/trunk/lang/saisie_evenement_fr.php
+++ saisie_evenements/trunk/lang/saisie_evenement_fr.php
@@ -1,11 +1,15 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/saisie_evenements/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-	'saisie_evenement_titre' => 'Sélecteur d\'événement', 
-	'Un choix d\'un ou plusieurs événements'
 
+	// S
+	'saisie_evenement_titre' => 'Sélecteur d’événement',
+
+	// 0
+	'0' => 'Un choix d’un ou plusieurs événements'
 );
