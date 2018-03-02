192c161e80bf53e351e57b1c6c30d645a052ec6f
booz@rezo.net <booz@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-03-02 13:14:47 +0000

    debug
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@109238 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- stats_data/stats_data_pipelines.php
+++ stats_data/stats_data_pipelines.php
@@ -1,7 +1,7 @@
-<?
+<?php
 
 function stats_data_header_prive($flux){
 	$css = find_in_path('css/stats_data.css');
-	$flux .= "<link rel='stylesheet' type='text/css' media='all' href='$css' />\n";
+	$flux .= "<link rel='stylesheet' type='text/css' media='all' href='$css' />\n" ;
 	return $flux;
 }
