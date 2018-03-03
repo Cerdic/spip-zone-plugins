436c642d52f744a6007e8ee8fc91645b5e832ef2
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-03-03 17:23:11 +0000

    constructeur : pouvoir selectionner les événements sur des identifiants divers
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@109271 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- saisie_evenements/trunk/lang/saisie_evenements_fr.php
+++ saisie_evenements/trunk/lang/saisie_evenements_fr.php
@@ -6,8 +6,23 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 }
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+	'ids_evenement_label' => 'Événements', 
+	'ids_evenement_explication' => 'Proposer les événements suivants.', 
+	'ids_article_label' => 'Articles', 
+	'ids_article_explication' => 'Proposer les événements des articles des articles suivants.', 
 
+	'ids_rubrique_label' => 'Rubrique', 
+	'ids_rubrique_explication' => 'Proposer les événements des articles dans les rubriques suivantes.', 
+
+	'ids_branche_label' => 'Branche', 
+	'ids_branche_explication' => 'Proposer les événements des articles dans les branches suivantes. Une branche correspond à une rubrique et ses sous-rubriques.', 
+
+	'ids_mot_label' => 'Mot clé', 
+	'ids_mot_explication' => 'Proposer les événements ayant le(s) mot(s) clé suivant.', 
 	// S
-	'saisie_evenements_titre' => 'Sélecteur d’événements', 
 	'saisie_evenements_explication' => 'Un choix d’un ou plusieurs événements',
+	'saisie_evenements_titre' => 'Sélecteur d’événements',
+	'saisie_evenements_ids_texte' => 'Les événements proposés peuvent être choisis par leurs identifiants, ou bien par leur association à des articles, des rubriques, des mots.<br /> 
+	Pour ce faire, il faut indiquer un identifiant, éventuellement plusieurs séparés par des virgules, dans les champs <emph>a hoc</emph>.<br />
+	Si plusieurs critères de sélections sont définis, un ET logique est utilisé. Ainsi, si vous mettez 4 dans le champ article et 2 dans le champ mot, les évènements appartennant à l\'article&nbsp;4 tout en ayant le mot-clé&nbsp;2 seront proposés.'
 );
