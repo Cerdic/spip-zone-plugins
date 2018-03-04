001f029166fc7c197cc04c9d9d6980ac32f725dd
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-03-04 04:37:51 +0000

    [Salvatore] [source:_plugins_/saisie_evenements/trunk/lang/ saisie_evenements] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@109282 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- saisie_evenements/trunk/lang/saisie_evenements_fr.php
+++ saisie_evenements/trunk/lang/saisie_evenements_fr.php
@@ -6,42 +6,43 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 }
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// D
+	'date_debut_max_fixe_explication' => 'Proposer uniquement les événements qui commencent AVANT la date suivante (incluse).',
 	'date_debut_max_fixe_label' => 'Date de début maximale',
-	'date_debut_max_fixe_explication' => 'Proposer uniquement les événements qui commencent AVANT la date suivante (incluse).', 
-	'date_debut_min_mobile_label' => 'Date de début minimale (mobile)',
-	'date_debut_min_mobile_explication' => 'Proposer uniquement les événements qui commencent  à partir de <i>x</i> jours. Pour les événements qui commencent demain ou plus tard, mettre 1. Pour les événements qui commencent  hier ou plus tard, mettre -1.', 
+	'date_debut_max_mobile_explication' => 'Proposer uniquement les événements qui commencent avant <i>x</i> jours. Pour les événements qui commencent demain au plus tard demain, mettre 1. Pour les événements qui commencent au plus tard hier, mettre -1.',
 	'date_debut_max_mobile_label' => 'Date de début maximale (mobile)',
-	'date_debut_max_mobile_explication' => 'Proposer uniquement les événements qui commencent avant <i>x</i> jours. Pour les événements qui commencent demain au plus tard demain, mettre 1. Pour les événements qui commencent au plus tard hier, mettre -1.', 
-
+	'date_debut_min_fixe_explication' => 'Proposer uniquement les événements qui commencent APRÈS la date suivante (incluse).',
 	'date_debut_min_fixe_label' => 'Date de début minimale',
-	'date_debut_min_fixe_explication' => 'Proposer uniquement les événements qui commencent APRÈS la date suivante (incluse).', 
-	'option_type_affichage_label' => 'Présentation des événements', 
-	'option_type_affichage_titre' => 'Uniquement le titre de l\'événement',
-	'option_type_affichage_date' => 'Uniquement la date de l\'événement',
-	'option_type_affichage_titre_date' => 'Le titre et la date de l\'événement',
-	
-	'option_type_choix_label' => 'Type de choix', 
-	'option_type_choix_radio' => 'Choix unique (boutons radios)',
+	'date_debut_min_mobile_explication' => 'Proposer uniquement les événements qui commencent  à partir de <i>x</i> jours. Pour les événements qui commencent demain ou plus tard, mettre 1. Pour les événements qui commencent  hier ou plus tard, mettre -1.',
+	'date_debut_min_mobile_label' => 'Date de début minimale (mobile)',
+
+	// I
+	'ids_article_explication' => 'Proposer les événements des articles des articles suivants.',
+	'ids_article_label' => 'Articles',
+	'ids_branche_explication' => 'Proposer les événements des articles dans les branches suivantes. Une branche correspond à une rubrique et ses sous-rubriques.',
+	'ids_branche_label' => 'Branche',
+	'ids_evenement_explication' => 'Proposer les événements suivants.',
+	'ids_evenement_label' => 'Événements',
+	'ids_mot_explication' => 'Proposer les événements ayant le(s) mot(s) clé suivant.',
+	'ids_mot_label' => 'Mot clé',
+	'ids_rubrique_explication' => 'Proposer les événements des articles dans les rubriques suivantes.',
+	'ids_rubrique_label' => 'Rubrique',
+
+	// O
+	'option_type_affichage_date' => 'Uniquement la date de l’événement',
+	'option_type_affichage_label' => 'Présentation des événements',
+	'option_type_affichage_titre' => 'Uniquement le titre de l’événement',
+	'option_type_affichage_titre_date' => 'Le titre et la date de l’événement',
 	'option_type_choix_checkbox' => 'Choix multiples (case à cocher)',
-	
-	'ids_evenement_label' => 'Événements', 
-	'ids_evenement_explication' => 'Proposer les événements suivants.', 
-	'ids_article_label' => 'Articles', 
-	'ids_article_explication' => 'Proposer les événements des articles des articles suivants.', 
+	'option_type_choix_label' => 'Type de choix',
+	'option_type_choix_radio' => 'Choix unique (boutons radios)',
 
-	'ids_rubrique_label' => 'Rubrique', 
-	'ids_rubrique_explication' => 'Proposer les événements des articles dans les rubriques suivantes.', 
-
-	'ids_branche_label' => 'Branche', 
-	'ids_branche_explication' => 'Proposer les événements des articles dans les branches suivantes. Une branche correspond à une rubrique et ses sous-rubriques.', 
-
-	'ids_mot_label' => 'Mot clé', 
-	'ids_mot_explication' => 'Proposer les événements ayant le(s) mot(s) clé suivant.', 
 	// S
+	'saisie_evenements_chronologie_texte' => 'Les critères de date pour les choix des événements sont cumulatifs avec les critères précédents d’association à des objets.',
 	'saisie_evenements_explication' => 'Un choix d’un ou plusieurs événements',
-	'saisie_evenements_titre' => 'Sélecteur d’événements',
-	'saisie_evenements_chronologie_texte' => 'Les critères de date pour les choix des événements sont cumulatifs avec les critères précédents d\'association à des objets.', 
 	'saisie_evenements_ids_texte' => 'Les événements proposés peuvent être choisis par leurs identifiants, ou bien par leur association à des articles, des rubriques, des mots.<br /> 
 	Pour ce faire, il faut indiquer un identifiant, éventuellement plusieurs séparés par des virgules, dans les champs <emph>a hoc</emph>.<br />
-	Si plusieurs critères de sélections sont définis, un ET logique est utilisé. Ainsi, si vous mettez 4 dans le champ article et 2 dans le champ mot, les évènements appartennant à l\'article&nbsp;4 tout en ayant le mot-clé&nbsp;2 seront proposés.'
+	Si plusieurs critères de sélections sont définis, un ET logique est utilisé. Ainsi, si vous mettez 4 dans le champ article et 2 dans le champ mot, les évènements appartennant à l’article 4 tout en ayant le mot-clé 2 seront proposés.',
+	'saisie_evenements_titre' => 'Sélecteur d’événements'
 );
