dcfd444c02d8ac305e944f164778991959d8fcb4
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-03-13 23:38:35 +0000

    le champ de la rubrique parente, c'est id_parent, pas id_rubrique
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@109485 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- declarerparent/trunk/declarerparent_pipelines.php
+++ declarerparent/trunk/declarerparent_pipelines.php
@@ -20,11 +20,10 @@ if (!defined('_ECRIRE_INC_VERSION')) {
  * @return array
  */
 function declarerparent_declarer_tables_objets_sql($tables) {
-
 	$tables['spip_articles']['parent'] = array('type' => 'rubrique', 'champ' => 'id_rubrique');
-	$tables['spip_rubriques']['parent'] = array('type' => 'rubrique', 'champ' => 'id_rubrique');
+	$tables['spip_rubriques']['parent'] = array('type' => 'rubrique', 'champ' => 'id_parent');
 	$tables['spip_mots']['parent'] = array('type' => 'groupe_mot', 'champ' => 'id_groupe');
 	$tables['spip_produits']['parent'] = array('type' => 'rubrique', 'champ' => 'id_rubrique');
-
+	
 	return $tables;
 }
