02540e879137020cf209221f78a68e9319f23a7c
erational@erational.org <erational@erational.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-03-15 10:19:13 +0000

    PSR et surtout suppression d'un espace blanc qui pétait des headers
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@109534 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- typo_guillemets/typo_guillemets_fonctions.php
+++ typo_guillemets/typo_guillemets_fonctions.php
@@ -1,6 +1,8 @@
 <?php
 
-if (!defined("_ECRIRE_INC_VERSION")) return;
+if (!defined("_ECRIRE_INC_VERSION")) {
+	return;
+}
 
 function typo_guillemets_insert_head_css($flux) {
 	$flux .= '<link rel="stylesheet" type="text/css" href="'.find_in_path('css/typo_guillemets.css').'" media="all" />'."\n";
@@ -177,4 +179,3 @@ function typo_guillemets_remplacements($texte) {
 	return str_replace("\01", '"', $texte);
 }
 
-?>
