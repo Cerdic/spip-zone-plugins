e7d7dcb516cb40abab17d72aa4b4f2a6db600efe
gpl@long-player.net <gpl@long-player.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-03-16 04:36:15 +0000

    [Salvatore] [source:_plugins_/mots_techniques/trunk/lang/ motstechniques] Export depuis http://trad.spip.net de la langue eo
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@109544 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ mots_techniques/trunk/lang/motstechniques_eo.php
@@ -0,0 +1,17 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/motstechniques?lang_cible=eo
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'bouton_mots_cles_techniques' => 'La ŝlosilvortoj de tiu grupo estos kaŝitaj defaŭlte en la publika spaco ?',
+
+	// I
+	'info_mots_cles_techniques' => 'Grupo de teknikaj ŝlosilvortoj :'
+);
