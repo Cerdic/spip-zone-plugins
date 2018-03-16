7a084602e067d798497ec4b1ef2c31e0d7ac0a5c
gpl@long-player.net <gpl@long-player.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-03-16 04:35:58 +0000

    [Salvatore] [source:_plugins_/crayons/trunk/lang/ crayons] Export depuis http://trad.spip.net
    La langue 'ar' devrait Ãªtre supprimÃ©e car trop peu traduite (26.92 %)
    La langue 'br' devrait Ãªtre supprimÃ©e car trop peu traduite (26.92 %)
    La langue 'eu' devrait Ãªtre supprimÃ©e car trop peu traduite (23.08 %)
    La langue 'gl' devrait Ãªtre supprimÃ©e car trop peu traduite (28.85 %)
    La langue 'it_fem' devrait Ãªtre supprimÃ©e car trop peu traduite (26.92 %)
    La langue 'km' devrait Ãªtre supprimÃ©e car trop peu traduite (9.62 %)
    La langue 'lb' devrait Ãªtre supprimÃ©e car trop peu traduite (26.92 %)
    La langue 'oc_lnc' devrait Ãªtre supprimÃ©e car trop peu traduite (26.92 %)
    La langue 'ro' devrait Ãªtre supprimÃ©e car trop peu traduite (26.92 %)
    La langue 'sv' devrait Ãªtre supprimÃ©e car trop peu traduite (26.92 %)
    La langue 'tr' devrait Ãªtre supprimÃ©e car trop peu traduite (26.92 %)
    La langue 'tw' devrait Ãªtre supprimÃ©e car trop peu traduite (26.92 %)
    La langue 'zh_tw' devrait Ãªtre supprimÃ©e car trop peu traduite (26.92 %)
    
    
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@109539 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- crayons/trunk/lang/crayons_eo.php
+++ crayons/trunk/lang/crayons_eo.php
@@ -10,7 +10,12 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
+	'absence_cle_primaire' => 'Neniu ĉefŝlosilo !',
+	'activation_barre_impossible' => 'Alia kromprogramo malpermesas la uzon de la Plumingo en la publika spaco.',
 	'annuler' => 'Nuligi',
+
+	// B
+	'bouton_supprimer' => 'Forigi',
 
 	// D
 	'deja_modifie' => 'Jam modifita',
@@ -20,8 +25,44 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'editer' => 'Redakti',
 	'editer_@type@_@id@' => 'Tute redakti @type@ @id@ ',
 	'editer_tout' => 'Ĉion redakti',
+	'effets' => 'Efektoj',
+	'explication_exec_autorise' => 'Paĝoj, kiuj estos modifeblaj (?exec=...) (, por dislimi la „exec“, * por ĉiuj) :',
+	'explication_introduction_article' => 'La artikola enkonduko montras aŭ la priskribon de la artikolo, aŭ la unuajn 600 signojn (ĉapo, poste teksto).',
+	'explication_reduire_logo' => 'Malpliigi la vinjeton, se ĝi superas la maksimuman grandon indikitan en bilderoj („pixels“, „px“). Ekzemple : 100. (0 = neniam.)',
+
+	// I
+	'info_experimental' => 'Eksperimenta',
+
+	// L
+	'label_activer_barre_typo' => 'Aktivigi la tipografian breton en la krajonoj ?',
+	'label_activer_crayons_prive' => 'Aktivigi krajonoj en la privata spaco.',
+	'label_barre_typo' => 'Tipografia breto',
+	'label_clickhide' => 'Ekstera alklako kaŝas la krajonoj',
+	'label_effets' => 'Elekto de efektoj',
+	'label_espaceprive' => 'Krajonoj en la privata spaco ?',
+	'label_exec_autorise' => 'Rajtigita paĝoj',
+	'label_filet' => 'Linio',
+	'label_message' => 'Montri mesaĝon',
+	'label_msg_abandon' => 'Se rezigno',
+	'label_msg_no_change' => 'Se neniu ŝanĝo',
+	'label_reduire_logo' => 'Malpliigi la vinjeton',
+	'label_upload_document' => 'Permesi dokument-aldonon per ŝovo-demeto sur la krajono artikolo.teksto („article.texte“)',
+	'label_yellow_fade' => 'Fado',
+	'legend_controleur_fichier_remplacer' => 'Anstataŭigi tiun dokumenton „@extension@“',
+	'legend_controleur_hyperlien' => 'Ligilo',
+	'legend_controleur_logo' => 'Elekti vinjeton :',
+	'legend_controleur_logo_remplacer' => 'Anstataŭigi tiun vinjeton',
+	'legend_controleur_qui' => 'Kiu ?',
+	'legend_controleur_vignette' => 'Elekti vinjeton :',
+	'legend_controleur_vignette_remplacer' => 'Anstataŭigi tiun vinjeton',
+	'legend_documents' => 'Alŝuto de dokumentoj',
+	'legend_editer_prive' => 'Modifi la privatan spacon (eksperimenta)',
+	'legend_introduction_article' => 'Enkonduka teksto de la artikolo',
+	'legend_introduction_rubrique' => 'Enkonduka teksto de la rubriko',
 
 	// M
+	'message_image_obligatoire' => 'Vi devas elekti bildon',
+	'messages' => 'Mesaĝoj',
 	'modifie_par_ailleurs' => 'Modifita dumtempe',
 
 	// N
@@ -35,6 +76,13 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// S
 	'sauvegarder' => 'Ĉu konservi modifojn ?',
 	'svp_copier_coller' => 'Ni pardonpetas, kopigluu viajn ŝanĝojn kaj rekomencu',
+
+	// T
+	'titre_config_crayons' => 'Agordi la krojonojn',
+	'titre_crayons' => 'Krajonoj',
+
+	// U
+	'update_impossible' => 'Ĝisdatigo fiaskis.',
 
 	// V
 	'veuillez_patienter' => 'Bonvolu atendi...'
