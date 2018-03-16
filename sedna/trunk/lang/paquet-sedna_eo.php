f8d936d3bb55ad2de16e13493a243d49f19d8f5c
gpl@long-player.net <gpl@long-player.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-03-16 04:36:26 +0000

    [Salvatore] [source:_plugins_/sedna/trunk/lang/ paquet-sedna] Export depuis http://trad.spip.net de la langue eo
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@109548 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ sedna/trunk/lang/paquet-sedna_eo.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-sedna?lang_cible=eo
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// S
+	'sedna_description' => 'Sedna estas kolektilo de RSS-fluoj (kaj ATOM-, ktp.), bazita sur la abonligitaj retejoj de SPIP. Ĝi inspiras sin iel de „Sarari RSS“ rilate al la aspekto.
+Ĝi celas esti kaj resti kode simpla, kaj rapidŝarga. Kaj iom estetika, ankaŭ.',
+	'sedna_slogan' => 'RSS-kolektilo'
+);
