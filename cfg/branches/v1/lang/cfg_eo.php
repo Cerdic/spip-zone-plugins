3d6f43ef9fbc3295beed8875eb182b1670f3276e
gpl@long-player.net <gpl@long-player.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-03-16 04:35:55 +0000

    [Salvatore] [source:_plugins_/cfg/branches/v1/lang/ cfg] Export depuis http://trad.spip.net de la langue eo
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@109538 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ cfg/branches/v1/lang/cfg_eo.php
@@ -0,0 +1,64 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/cfg?lang_cible=eo
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'bouton_effacer' => 'Forigi',
+	'bouton_mettre_a_jour' => 'Ĝisdatigi',
+	'bouton_reset' => 'Restarigi',
+
+	// C
+	'cfg' => 'CFG',
+	'choisir_module_a_configurer' => 'Elektu la agordotan kromaĵon',
+	'config_enregistree' => 'Registrado de <b>@nom@</b> farita',
+	'config_supprimee' => 'Forigado de <b>@nom@</b> farita',
+	'configuration_modules' => 'Agordo de la kromaĵoj',
+
+	// E
+	'erreur_copie_fichier' => 'Ne eblas kopii la dosieron @fichier@ al ĝia definitiva loko',
+	'erreur_enregistrement' => 'Eraro okazis dum la registrado de <b>@nom@</b>',
+	'erreur_lecture' => 'Ne eblas legi @nom@',
+	'erreur_open_w_fichier' => 'Ne eblas malfermi la dosieron @fichier@ por skribado',
+	'erreur_suppression' => 'Eraro okazis dum forigado de <b>@nom@</b>',
+	'erreur_suppression_fichier' => 'Ne eblas forigi la dosieron @fichier@.',
+	'erreur_type_id' => 'La kampo @champ@ devas komenciĝi per litero aŭ substreko',
+	'erreur_type_idnum' => 'La kampo @champ@ devas esti nombro',
+	'erreur_type_pwd' => 'La kampo @champ@ devas havi pli ol 5 signoj',
+
+	// I
+	'id_manquant' => 'Mankanta identigilo',
+	'installation_librairies' => 'Elŝuto de bibliotekoj',
+	'installation_liste_libs' => 'Listo de la bibliotekoj',
+	'installer_dossier_lib' => 'Vi devas kri dosierujon @dir@ radike de via SPIP kun skribpermeso.',
+	'installer_lib_192' => 'Por instali bibliotekon, malkompaktigu mane la zip-on kaj kopiu ĝian enhavon en la dosierujon @dir@.',
+
+	// L
+	'label_activer' => 'Aktivigi',
+	'label_obligatoire' => 'Vi devas skribi ion',
+
+	// N
+	'nom_table_manquant' => 'SQL-tabel-nomo mankanta',
+	'nouveau' => 'Nova',
+
+	// O
+	'ok' => 'Bone',
+
+	// P
+	'pas_de_champs_dans' => 'Neniu kampo trovita en @nom@',
+	'pas_de_changement' => 'Neniu ŝanĝo en <b>@nom@</b>',
+
+	// R
+	'refus_configuration_administrateur' => 'Nur administrantoj el la retejo estas permesitaj modifi tiujn agordojn.',
+	'refus_configuration_webmestre' => 'Nur retejestroj estas permesitaj modifi tiujn agordojn.',
+	'reset' => 'Forigi',
+
+	// S
+	'supprimer' => 'Restarigi la defaŭltajn valorojn'
+);
