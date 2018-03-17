ec28fa7e604e5286b420d4da639639fcd5fa7e7a
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-03-17 15:39:07 +0000

    On n'affiche le bouton pour dupliquer aussi les enfants seulement s'il y en a
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@109571 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- duplicator/trunk/duplicator_pipelines.php
+++ duplicator/trunk/duplicator_pipelines.php
@@ -27,18 +27,23 @@ function duplicator_boite_infos($flux){
 	) {
 		include_spip('inc/filtres');
 		include_spip('inc/actions');
+		include_spip('base/objets_parents');
 		
+		// Un bouton pour dupliquer le contenu
 		$flux["data"] .= bouton_action(
 			_T('duplicator:action_dupliquer_contenu'),
 			generer_action_auteur('dupliquer_objet', "$objet/$id_objet")
 		);
 		
-		$flux["data"] .= bouton_action(
-			_T('duplicator:action_dupliquer_contenu_enfants'),
-			generer_action_auteur('dupliquer_objet', "$objet/$id_objet/enfants"),
-			'',
-			_T('duplicator:action_dupliquer_contenu_enfants_confirmer')
-		);
+		// Un bouton pour dupliquer aussi les enfants, seulement si on trouve des enfants possibles
+		if (type_objet_info_enfants($objet) and objet_trouver_enfants($objet, $id_objet)) {
+			$flux["data"] .= bouton_action(
+				_T('duplicator:action_dupliquer_contenu_enfants'),
+				generer_action_auteur('dupliquer_objet', "$objet/$id_objet/enfants"),
+				'',
+				_T('duplicator:action_dupliquer_contenu_enfants_confirmer')
+			);
+		}
 	}
 	
 	//~ if(autoriser("dupliquer", "rubrique")){
