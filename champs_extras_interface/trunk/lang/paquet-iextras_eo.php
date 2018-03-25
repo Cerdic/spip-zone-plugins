e97d94eeff4ab2e79ab4f09186925696f30857b4
gpl@long-player.net <gpl@long-player.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-03-25 03:36:49 +0000

    [Salvatore] [source:_plugins_/champs_extras_interface/trunk/lang/ paquet-iextras] Export depuis http://trad.spip.net de la langue eo
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@109667 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ champs_extras_interface/trunk/lang/paquet-iextras_eo.php
@@ -0,0 +1,17 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-iextras?lang_cible=eo
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// I
+	'iextras_description' => 'Aldonas en la privata spaco kompletan interfacon por mastrumi ekstrajn kampojn en redaktaj objektoj.',
+	'iextras_nom' => 'Ekstraj kampoj (interfaco)',
+	'iextras_slogan' => 'Disponigas grafikan interfacon por mastrumi ekstrajn kampojn',
+	'iextras_titre' => 'Ekstraj kampoj (interfaco)'
+);
