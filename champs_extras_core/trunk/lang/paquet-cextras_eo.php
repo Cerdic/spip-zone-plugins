f0ea96c94c3c4ff9bb866fab9f61200d3badf5dc
gpl@long-player.net <gpl@long-player.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-03-25 03:36:47 +0000

    [Salvatore] [source:_plugins_/champs_extras_core/trunk/lang/ paquet-cextras] Export depuis http://trad.spip.net de la langue eo
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@109665 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ champs_extras_core/trunk/lang/paquet-cextras_eo.php
@@ -0,0 +1,17 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-cextras?lang_cible=eo
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cextras_description' => 'Disponigas simplan API-on, kiu ebligas krei novajn kampojn en la redaktajn objektojn. Tiu estas do la baza ilo por aliaj kromprogramoj, interalie por „Interfaco por ekstraj kampoj“, kiu aldonas grafikan interfacon al ĉi tiu kromprogramo.',
+	'cextras_nom' => 'Ekstraj kampoj',
+	'cextras_slogan' => 'API por krei novajn kampojn en la redaktaj objektoj.',
+	'cextras_titre' => 'Ekstraj kampoj'
+);
