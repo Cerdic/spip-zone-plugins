e1e6e49ff2e6829c740b6083b3997de74b555860
gpl@long-player.net <gpl@long-player.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-03-25 03:36:48 +0000

    [Salvatore] [source:_plugins_/champs_extras_interface/trunk/lang/ iextras] Export depuis http://trad.spip.net de la langue eo
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@109666 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ champs_extras_interface/trunk/lang/iextras_eo.php
@@ -0,0 +1,115 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/iextras?lang_cible=eo
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'action_associer' => 'mastrumi tiun kampon',
+	'action_associer_title' => 'Mastrumi la montradon de tiu ĉi ekstra kampo',
+	'action_desassocier' => 'malasocii',
+	'action_desassocier_title' => 'Ne plu mastrumi la montradon de tiu ĉi ekstra kampo',
+	'action_descendre' => 'malsupren',
+	'action_descendre_title' => 'Movi la kampon de unu pozicio malsupren',
+	'action_modifier' => 'modifi',
+	'action_modifier_title' => 'Modifi la agordojn de tiu ekstra kampo',
+	'action_monter' => 'supren',
+	'action_monter_title' => 'Movi la kampon de unu pozicio supren',
+	'action_supprimer' => 'forigi',
+	'action_supprimer_title' => 'Forigi komplete la kampon el la datumbazo',
+
+	// C
+	'caracteres_autorises_champ' => 'Taŭgaj signoj : sensupersignaj literoj, ciferoj, - kaj _',
+	'caracteres_interdits' => 'Iuj uzitaj signoj ne taŭgas por tiu ĉi kampo.',
+	'champ_deja_existant' => 'Samnoma kampo jam ekzistas en tiu tabelo.',
+	'champ_sauvegarde' => 'Ekstra kampo registrita !',
+	'champs_extras' => 'Ekstraj kampoj',
+	'champs_extras_de' => 'Ekstraj kampoj de : @objet@',
+
+	// E
+	'erreur_action' => 'Ago @action@ nekonata.',
+	'erreur_enregistrement_champ' => 'Problemo dum kreado de la ekstra kampo.',
+
+	// I
+	'icone_creer_champ_extra' => 'Krei novan ekstran kampon.',
+	'info_description_champ_extra' => 'Tiu paĝo ebligas la mastrumadon de la ekstraj kampoj, t.e. de la kromaj kampoj en la tabeloj de SPIP, uzeblaj en redakto-formularoj.',
+	'info_description_champ_extra_creer' => 'Vi povas krei novajn kampojn, kiuj montriĝos tiam en tiu ĉi paĝo, en la kadro „Listo de la redaktaj objektoj“, kaj ankaŭ en la formularoj.',
+	'info_description_champ_extra_presents' => 'Fine, se kampoj jam ekzistas en via datumbazo, sed ne estas deklaritaj (de kromprogramo aŭ skeletaro), vi povas uzi tiun kromprogramon por mastrumi ilin. Tiuj kampoj, se ili ekzistas, aperas en kadro „Listo de la ne mastrumataj kampoj“',
+	'info_modifier_champ_extra' => 'Modifi ekstran kampon.',
+	'info_nouveau_champ_extra' => 'Nova ekstra kampo.',
+	'info_saisie' => 'Enigo :',
+
+	// L
+	'label_attention' => 'Tre gravaj klarigoj',
+	'label_champ' => 'Kamponomo',
+	'label_class' => 'CSS-klasoj',
+	'label_datas' => 'Valorlisto',
+	'label_explication' => 'Klarigoj pri la enigo',
+	'label_label' => 'Eniga etikedo',
+	'label_obligatoire' => 'Ĉu deviga ?',
+	'label_rechercher' => 'Serĉo',
+	'label_rechercher_ponderation' => 'Serĉopezo',
+	'label_restrictions_auteur' => 'Laŭ aûtoro',
+	'label_restrictions_branches' => 'Laŭ branĉo',
+	'label_restrictions_groupes' => 'Laŭ grupo',
+	'label_restrictions_secteurs' => 'Laŭ radikrubriko',
+	'label_saisie' => 'Tipo de enigo',
+	'label_sql' => 'SQL-difino',
+	'label_table' => 'Objekto',
+	'label_traitements' => 'Aŭtomataj traktadoj',
+	'label_versionner' => 'Uzi versikontrolon por la enhavo de la kampo',
+	'legend_declaration' => 'Deklaro',
+	'legend_options_saisies' => 'Enigaj opcioj',
+	'legend_options_techniques' => 'Teknikaĵoj',
+	'legend_restriction' => 'Limigo',
+	'legend_restrictions_modifier' => 'Modifi la enigon',
+	'legend_restrictions_voir' => 'Vidi la enigon',
+	'liste_des_extras' => 'Listo de la ekstraj kampoj',
+	'liste_des_extras_possibles' => 'Listo de la ne mastrumataj kampoj',
+	'liste_objets_applicables' => 'Listo de la redaktaj objektoj',
+
+	// N
+	'nb_element' => '1 elemento',
+	'nb_elements' => '@nb@ elementoj',
+
+	// P
+	'precisions_pour_attention' => 'Por iu tre grava. Uzu moderege ! Povas esti lingvoĉeno „kromprogramo:cxeno“.',
+	'precisions_pour_class' => 'Aldoni CSS-klasojn al elemento. apartigitajn per spacoj. Ekz. : „inserer_barre_edition“ (enmeti redaktan breton) por bloko el la kromprogramo Plumingo.',
+	'precisions_pour_datas' => 'Iuj kampotipoj postulas liston de eblaj valoroj : indiku tie po unu linie, sekvita de komo kaj priskribo. Malplena linio por defaŭlta valoro. Priskribo povas esti lingvoĉeno.',
+	'precisions_pour_explication' => 'Vi povas aldoni plian informon pri la enigo.	Povas esti lingvoĉeno „kromprogramo:cxeno“.',
+	'precisions_pour_label' => 'Povas esti lingvoĉeno „kromprogramo:cxeno“.',
+	'precisions_pour_nouvelle_saisie' => 'Ebligas ŝanĝi la tipon de tiukampa enigo',
+	'precisions_pour_nouvelle_saisie_attention' => 'Atentu, ŝanĝo de eniga tipo perdigas la enigajn opciojn, kiuj ne estas komunaj kun la nova elektita enigo.',
+	'precisions_pour_rechercher' => 'Ĉu uzi en la serĉilo ?',
+	'precisions_pour_rechercher_ponderation' => 'SPIP uzas nombron dum serĉo en kolumno per pezkoeficiento. Tiu ebligas reliefigi la plej trafajn kolumnoj (titolo, ekz.). La koeficiento uzata ĉe la ekstraj kampoj estas defaŭlte 2. Por doni ideon al vi, SPIP uzas 8 por la titolo, 1 por la teksto.',
+	'precisions_pour_restrictions_branches' => 'Branĉaj identigiloj permesataj (disigilo „ :“)',
+	'precisions_pour_restrictions_groupes' => 'Grupaj identigiloj permesataj (disigilo „ :“)',
+	'precisions_pour_restrictions_secteurs' => 'Radikrubrikaj identigiloj permesataj (disigilo „ :“)',
+	'precisions_pour_saisie' => 'Montri enigon de tipo :',
+	'precisions_pour_traitements' => 'Apliki aŭtomate traktadon por la kreita marko #NOMO_DE_LA_KAMPO :',
+
+	// R
+	'radio_restrictions_auteur_admin' => 'Nur la mastrumantoj (eĉ limigitaj)',
+	'radio_restrictions_auteur_aucune' => 'Ĉiuj povas',
+	'radio_restrictions_auteur_webmestre' => 'Nur la retejestro',
+	'radio_traitements_aucun' => 'Neniu',
+	'radio_traitements_raccourcis' => 'Traktado de la SPIP-markoj („propre“)',
+	'radio_traitements_typo' => 'Nur tipografiaj traktadoj („typo“)',
+
+	// S
+	'saisies_champs_extras' => 'El „Ekstra kampoj“',
+	'saisies_saisies' => 'El „Enigoj“',
+	'supprimer_reelement' => 'Ĉu forigi tiun kampon ?',
+
+	// T
+	'titre_iextras' => 'Ekstraj kampoj',
+	'titre_page_iextras' => 'Ekstraj kampoj',
+
+	// V
+	'veuillez_renseigner_ce_champ' => 'Bonvolu plenigi tiun kampon !'
+);
