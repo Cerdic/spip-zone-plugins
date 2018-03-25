c58e49e5560b1ca2a836d2fe37ec53ad0c788ff3
gpl@long-player.net <gpl@long-player.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-03-25 03:36:46 +0000

    [Salvatore] [source:_plugins_/champs_extras_core/trunk/lang/ cextras] Export depuis http://trad.spip.net de la langue eo
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@109664 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ champs_extras_core/trunk/lang/cextras_eo.php
@@ -0,0 +1,23 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/cextras?lang_cible=eo
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cextra_par_defaut' => 'Defaŭlta valoro',
+
+	// P
+	'pas_auteur' => 'neniu aŭtoro',
+
+	// T
+	'type' => '@type@',
+
+	// Z
+	'zbug_balise_argument_non_texte' => 'La argumento @nb@ en la marko @balise@ estu de tipo „teksto“'
+);
