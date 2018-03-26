774a0e8b25abc087dbe94f35b6bcb467f504ba75
gpl@long-player.net <gpl@long-player.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-03-26 03:36:23 +0000

    [Salvatore] [source:_plugins_/facteur/trunk/lang/ paquet-facteur] Export depuis http://trad.spip.net de la langue eo
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@109685 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ facteur/trunk/lang/paquet-facteur_eo.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-facteur?lang_cible=eo
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// F
+	'facteur_description' => 'Leterportisto prizorgas la disdonadon de la retmesaĝoj, kun formo HTML-a, teksta aŭ ambaŭ.',
+	'facteur_nom' => 'Leterportisto',
+	'facteur_slogan' => 'Ĝi disdonas viajn retmesaĝojn'
+);
