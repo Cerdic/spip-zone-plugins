a1685356b150ae61a7d2eb54ffa6fe6ef900a835
rporto@calepino.com <rporto@calepino.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-03-28 03:37:09 +0000

    [Salvatore] [source:_plugins_/fabrique/trunk/lang/ paquet-fabrique] Export depuis http://trad.spip.net de la langue pt_br
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@109710 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ fabrique/trunk/lang/paquet-fabrique_pt_br.php
@@ -0,0 +1,17 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-fabrique?lang_cible=pt_br
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// F
+	'fabrique_description' => 'Este plugin permite gerar o código de plugins para o SPIP.
+Ele pode gerenciar a criação de objetos editoriais simples. O objetivo não é criar um plugin perfeito, mas uma base sólida, rapidamente montada, para ser editada em seguida de acordo com a sua necessidade.',
+	'fabrique_slogan' => 'Ferramenta para criar plugins do SPIP.',
+	'fabrique_titre' => 'Fábrica'
+);
