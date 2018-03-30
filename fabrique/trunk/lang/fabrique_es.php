b2fb9ac19799f288a9b9d15ffb67c055e37cc6ec
freddy@belvil.net <freddy@belvil.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-03-30 03:36:54 +0000

    [Salvatore] [source:_plugins_/fabrique/trunk/lang/ fabrique] Export depuis http://trad.spip.net de la langue es
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@109733 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ fabrique/trunk/lang/fabrique_es.php
@@ -0,0 +1,243 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/fabrique?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'action_incomprise' => '¡La acción @f_action@ no es compatible!',
+	'aide_creation_peupler_table' => 'Ayuda para crear una tabla',
+	'aide_creation_squelette_fabrique' => 'Ayuda para crear un esqueleto Fabrica',
+	'autorisation_administrateur' => '≥ Administrador completo',
+	'autorisation_administrateur_restreint' => '≥ Administrador restringido',
+	'autorisation_defaut' => 'Por defecto (@defaut@)',
+	'autorisation_jamais' => 'Nunca',
+	'autorisation_redacteur' => '≥ Redactor',
+	'autorisation_toujours' => 'Siempre',
+	'autorisation_webmestre' => 'Webmaster',
+	'avertissement_champs' => 'No ingrese la clave primaria aquí (@ object_id @),
+ni ninguno de los campos especiales (id_rubrique, lang, etc.) propuestos en la sección « Campos especiales » o en las secciones « Enlaces ».',
+
+	// B
+	'bouton_ajouter_champ' => 'Añadir un campo',
+	'bouton_ajouter_objet' => 'Añadir un objeto editorial',
+	'bouton_calculer' => 'Calcular',
+	'bouton_charger' => 'Cargar el respaldo',
+	'bouton_charger_sauvegarde_attention' => '¡La carga de una copia de seguridad borrará la información del complemento que se está creando actualmente!',
+	'bouton_creer' => 'Crear el plugin ',
+	'bouton_exporter' => 'Exportar',
+	'bouton_menu_edition' => 'Menú edición',
+	'bouton_outils_rapides' => 'Herramientas rápidas',
+	'bouton_reinitialiser_autorisations' => 'Restablecer los permisos',
+	'bouton_reinitialiser_chaines' => 'Restablecer las cadenas de lenguaje de este objeto',
+	'bouton_renseigner_objet' => 'Rellenar este objeto',
+	'bouton_reset' => 'Restablecer el formulario',
+	'bouton_supprimer_champ' => 'Eliminar este campo',
+	'bouton_supprimer_logo' => 'Eliminar este logo',
+	'bouton_supprimer_objet' => 'Eliminar este objeto editorial',
+
+	// C
+	'c_fabrique_dans_plugins' => '¡Haz las pruebas más fáciles!',
+	'c_fabrique_info' => 'Creando el plugin',
+	'c_fabrique_info_texte' => 'Esta herramienta facilita la creación de una base de código de complemento.
+Aunque el código del producto es funcional, probablemente no sea lo que esperas exactamente, ¡y ese no es el objetivo! La Fabrique crea las bases de archivos y códigos, pero probablemente tendrá que modificarlos de acuerdo con lo que realmente desea.
+<br /> <br />
+Le recomendamos que comprenda de antemano el funcionamiento de los complementos, SPIP y sus esqueletos, y si desea administrar objetos editoriales, la operación de tuberías, autorizaciones, formularios.
+Sin embargo, este complemento también se puede utilizar para estudiar el código generado de acuerdo con las opciones que marca.',
+	'c_fabrique_zone' => 'Wow es muy facil !',
+	'c_fabrique_zone_texte' => 'Vous allez certainement apprécier le fait de produire un plugin
+		gérant un ou plusieurs objets éditoriaux. Tant mieux !
+		<br /><br />
+		Méfiez-vous cependant ! Si créer un plugin est facile, le maintenir dans le temps,
+		gérer sa documentation, sa vie, est bien plus difficile.
+		Le meilleur moyen pour maintenir un plugin implique en général
+		deux conditions : qu’il soit utile et qu’il soit partagé ; partagé au sens où d’autres
+		développeurs et contributeurs peuvent intervenir dessus et l’améliorer.
+		Chez SPIP, les plugins partagés, avec un code libre,
+		peuvent être hébergés sur l’espace de collaboration SPIP Zone.
+		<br /><br />
+		Avant donc de vous lancer dans la création d’un nouveau plugin, vérifiez qu’il
+		n’existe pas dans l’espace de collaboration de SPIP un plugin déjà équivalent
+		sur lequel vous pourriez apporter vos améliorations, votre documentation.
+		Il est plus intéressant pour tout le monde qu’il y ait peu de doublons mais
+		des plugins fonctionnels et pérennes !
+	',
+	'calcul_effectue' => 'Calculo realizado',
+	'chaine_ajouter_lien_objet' => 'Añadir este tipo @type@',
+	'chaine_ajouter_lien_objet_feminin' => 'Añadir este @type@',
+	'chaine_icone_creer_objet' => 'Crear un @type@',
+	'chaine_icone_creer_objet_feminin' => 'Crear un @type@',
+	'chaine_icone_modifier_objet' => 'Editar este @type@',
+	'chaine_icone_modifier_objet_feminin' => 'Editar este @type@',
+	'chaine_info_1_objet' => 'Un @type@',
+	'chaine_info_1_objet_feminin' => 'Un @type@',
+	'chaine_info_aucun_objet' => 'Sin @type@',
+	'chaine_info_aucun_objet_feminin' => 'Sin @type@',
+	'chaine_info_nb_objets' => '@nb@ @objets@',
+	'chaine_info_nb_objets_feminin' => '@nb@ @objets@',
+	'chaine_info_objets_auteur' => 'Los @objets@ de este autor',
+	'chaine_info_objets_auteur_feminin' => 'Los @objets@ de este autor',
+	'chaine_retirer_lien_objet' => 'Eliminar este @type@',
+	'chaine_retirer_lien_objet_feminin' => 'Eliminar este @type@',
+	'chaine_retirer_tous_liens_objets' => 'Eliminar todos los @objets@',
+	'chaine_retirer_tous_liens_objets_feminin' => 'Eliminar todo los @objets@',
+	'chaine_texte_ajouter_objet' => 'Añadir un @type@',
+	'chaine_texte_ajouter_objet_feminin' => 'Añadir un @type@',
+	'chaine_texte_changer_statut_objet' => 'Este @type@ es :',
+	'chaine_texte_changer_statut_objet_feminin' => 'Este @type@ es :',
+	'chaine_texte_creer_associer_objet' => 'Crear y asociar un @type@',
+	'chaine_texte_creer_associer_objet_feminin' => 'Crear y asociar un @type@',
+	'chaine_texte_definir_comme_traduction_objet' => 'Este @type@ es una traducción del @type@ numero :',
+	'chaine_texte_definir_comme_traduction_objet_feminin' => 'Este @type@ es una traducción del @type@ numero :',
+	'chaine_titre_langue_objet' => 'Lenguaje de este @type@',
+	'chaine_titre_langue_objet_feminin' => 'Lenguaje de este @type@',
+	'chaine_titre_logo_objet' => 'Logo de este @type@',
+	'chaine_titre_logo_objet_feminin' => 'Logo de este @type@',
+	'chaine_titre_objet' => '@mtype@',
+	'chaine_titre_objet_feminin' => '@mtype@',
+	'chaine_titre_objets' => '@mobjets@',
+	'chaine_titre_objets_feminin' => '@mobjets@',
+	'chaine_titre_objets_rubrique' => '@mobjets@ de la sección',
+	'chaine_titre_objets_rubrique_feminin' => '@mobjets@ de la sección',
+	'champ_ajoute' => 'Un campo ha sido añadido',
+	'champ_auto_rempli' => 'El campo se completará automáticamente si lo deja en blanco.',
+	'champ_deplace' => 'El campo a sido movido',
+	'champ_supprime' => 'El campo ha sido eliminado',
+	'chargement_effectue' => 'Cargando hecho',
+	'config_exemple' => 'Ejemplo',
+	'config_exemple_explication' => 'Explicación de este ejemplo',
+	'config_titre_parametrages' => 'Ajustes',
+
+	// D
+	'datalist_aide' => 'Algunos navegadores pueden ofrecer autocompletar escribiendo una flecha hacia abajo en el teclado o haciendo doble clic en el cuadro de entrada.',
+
+	// E
+	'echappement_accolades' => '{ }',
+	'echappement_crochets' => '[ ]',
+	'echappement_diese' => '#',
+	'echappement_idiome' => '&lt; :',
+	'echappement_inclure' => '&lt;INCLURE',
+	'echappement_parentheses' => '( )',
+	'echappement_php' => '&lt; ?php',
+	'echappement_tag_boucle' => '&lt; de boucle',
+	'erreur_chargement_fichier' => 'El archivo enviado no pudo ser emitido. La restauración no se realiza.',
+	'erreur_copie_sauvegarde' => 'La copia de seguridad de @dir@ no se pudo hacer. El complemento como precaución no se ha regenerado.
+La causa probable proviene de derechos insuficientes este directorio fuente para el servidor.',
+	'erreur_envoi_fichier' => 'Error al enviar el archivo.',
+	'erreur_suppression_sauvegarde' => 'La copia de seguridad anterior (@dir@) no se pudo eliminar. El complemento como precaución no se ha regenerado.
+La causa probable es la creación de archivos adicionales en el complemento que no tienen suficientes derechos para ser manejados por el servidor.',
+	'erreurs' => 'Hay errores !',
+	'experimental_explication' => '<strong>Parte experimental!</strong><br />
+La durabilidad de los campos no está garantizada.
+Esta parte puede cambiar o desaparecer en futuras versiones.',
+	'explication_fichiers' => 'Seleccione los archivos para procesar.',
+	'explication_fichiers_echafaudes' => 'SPIP guarda automáticamente estos archivos en caché si están ausentes.
+Sin embargo, puede crear algunos de ellos para modificarlos y cambiar el comportamiento predeterminado de lo que SPIP ofrece.
+Además, estos archivos a veces tienen adiciones mínimas de características, luego se indican.',
+	'explication_reinitialiser' => 'Esto borra la información del complemento que se está creando actualmente. ¡Comenzarás desde cero!',
+	'explication_roles' => 'Experimentalmente, los roles se pueden administrar en los enlaces utilizando el complemento « Rôles ».',
+	'explication_sauvegarde' => 'La Fabrique crea un archivo de respaldo (<code>fabrique_{prefixe}.php</code>) dentro de cada plugin que crea. Puede restaurar este archivo aquí enviándolo al servidor o utilizando uno de los archivos ya presentes.',
+	'explication_tables_hors_normes' => 'Una tabla respeta las normas por defecto de SPIP cuando se nombra con un plural « s » (como <code>spip_choses</code>) y cuando su clave principal se basa en el nombre de la tabla singular (como <code>id_chose</code>). En otros casos, debe completar cierta información a continuación.',
+
+	// F
+	'fabrique_dev_intro' => 'Esta herramienta ayuda a crear esqueletos para Fabrique',
+	'fabrique_dev_titre' => 'Desarrollo de la Fabrique',
+	'fabrique_intro' => 'Herramienta para crear complementos',
+	'fabrique_outils' => 'Herramientas',
+	'fabrique_peuple_intro' => 'Esta herramienta ayuda a crear un archivo y una función de relleno de una tabla en el momento de la instalación del complemento',
+	'fabrique_peuple_titre' => 'Un objeto persona',
+	'fabrique_plugin' => 'Fabrica de @plugin@',
+	'fabrique_restaurer_titre' => 'Restaurar o reinicializar fabrique',
+	'fabrique_titre' => 'La Fabrique',
+	'fichier_importation_cree_dans' => 'Importar archivo creado en el directorio <code>@dir@</code>, archivo <code>@import@</code> con @lignes@ lineas para un total de @taille@',
+	'fichiers_importations_compresses_cree_dans' => 'Importar el archivo creado en el directorio <code>@dir@</code>, archivos <code>@import@</code> en <code>@donnees_compressees@</code>, con @lignes@ lineas para un total de @taille@',
+
+	// I
+	'image_supprimee' => 'La imagen a sido eliminada',
+	'insertion_code_explication' => 'Esta parte le permite insertar código en algunas partes proporcionadas por Fabrique. ¡Tenga cuidado, sin embargo, de que este código siempre sea válido!',
+
+	// L
+	'label_auteur' => 'Nombre del autor',
+	'label_auteur_lien' => 'URL del autor',
+	'label_auteurs_liens' => '¿Enlace a autores?',
+	'label_auteurs_liens_explication' => 'Agregue el formulario de enlaces de autor a este objeto.',
+	'label_boutons' => 'Botones',
+	'label_boutons_explication' => 'Insertar botones en estos lugares:',
+	'label_caracteristiques' => 'Características',
+	'label_categorie' => 'Categoría',
+	'label_champ_date_publication' => 'Campo SQL de fecha',
+	'label_champ_est_editable' => 'Puede ser editado',
+	'label_champ_est_obligatoire' => 'Es obligatorio',
+	'label_champ_est_versionne' => 'Puede ser versionado',
+	'label_champ_langues' => 'Gestión de idioma',
+	'label_champ_statut' => 'Campo <strong>estado</strong>',
+	'label_champ_titre' => 'Calcular los títulos',
+	'label_charger_depuis_table_sql' => 'Definir desde una tabla SQL',
+	'label_cle_primaire' => 'Clave primaria',
+	'label_cle_primaire_explication' => 'Ejemplo « id_chose »',
+	'label_cle_primaire_sql' => 'Definición de SQL para la clave primaria',
+	'label_cle_primaire_sql_explication' => 'Definición de SQL para la clave primaria',
+	'label_code_resultat' => 'Código transformado',
+	'label_code_squelette' => 'Código de esqueleto de origen',
+	'label_colonne_sql' => 'Columna SQL',
+	'label_colonne_sql_explication' => 'Un nombre de campo para SQL. Ejemplo « post_scriptum »',
+	'label_compatibilite' => 'Compatibilidad',
+	'label_definition_sql' => 'Definición SQL',
+	'label_description' => 'Descripción',
+	'label_documentation_url' => 'Documentación (url)',
+	'label_etat' => 'Estado',
+	'label_exemples' => 'Insertar ejemplos',
+	'label_exemples_explication' => '¿Añadir comentario en archivos de complemento de ejemplos de código y textos de ayuda?',
+	'label_explication' => 'Frase de explicación para el campo',
+	'label_fichier_administrations' => '¿Archivo de administración?',
+	'label_fichier_administrations_explication' => 'Crear el archivo de instalación / desinstalación?',
+	'label_fichier_autorisations' => 'Permisos',
+	'label_fichier_fonctions' => 'Funciones',
+	'label_fichier_options' => 'Opciones',
+	'label_fichier_pipelines' => 'Tuberías',
+	'label_fichier_sauvegarde' => 'Archivo de respaldo',
+	'label_fichier_sauvegarde_ordinateur' => 'En tu computadora',
+	'label_fichier_sauvegarde_serveur' => 'En el servidor',
+	'label_fichiers' => 'Archivos',
+	'label_formulaire_configuration' => '¿Formulario de configuración?',
+	'label_formulaire_configuration_titre' => 'Título de la pagina de configuración',
+	'label_genre' => 'Tipo de medio',
+	'label_genre_explication' => 'Se usa para pre-calcular el texto de las cadenas de idioma.',
+	'label_genre_feminin' => 'Femenino',
+	'label_genre_masculin' => 'Masculino',
+	'label_inserer_administrations_desinstallation' => 'Completar la desinstalación en la función <code>vider_table()</code>',
+	'label_inserer_administrations_fin' => 'Al final del archivo para insertar nuevas funciones',
+	'label_inserer_administrations_maj' => 'Complete <code>$maj</code> en la función <code>upgrade()</code>',
+	'label_inserer_base_tables_fin' => 'Al final del archivo para insertar nuevas funciones',
+	'label_inserer_paquet' => 'En el nivel de dependencias',
+	'label_libelle_champ_explication' => 'Un nombre de campo para humanos. Ejemplo « Post-Scriptum »',
+	'label_licence' => 'Licencia',
+	'label_logo' => 'Logo',
+	'label_logo_taille' => 'Logo de @taille@px',
+	'label_logo_variantes' => 'Variantes de logos ?',
+	'label_nom' => 'Nombre',
+	'label_nom_pluriel' => 'Nombre Plural',
+	'label_nom_pluriel_explication' => 'Ejemplo « Choses »',
+	'label_nom_singulier' => 'Nombre singular',
+	'label_nom_singulier_explication' => 'Ejemplo « Chose »',
+	'label_prefixe' => 'Prefijo',
+	'label_recherche' => 'Búsqueda',
+	'label_roles' => 'Lista de roles',
+	'label_saisie' => 'Tipo de entrada',
+	'label_saisie_options' => 'Opciones de entrada',
+	'label_saisies' => 'Ingresar',
+	'label_saisies_explication' => 'Crear entradas y sus vistas',
+	'label_schema' => 'Esquema',
+	'label_schema_explication' => 'Versión de la estructura de datos',
+	'label_slogan' => 'Eslogan',
+	'label_table' => 'Nombre de la tabla SQL',
+	'label_table_compresser_donnees' => 'Comprimir los datos ?',
+	'label_table_compresser_donnees_explication' => 'Util si la tabla es grande !',
+	'label_table_destination' => 'Tabla SQL de destino',
+	'label_table_explication' => 'Por ejemplo « spip_choses »'
+);
