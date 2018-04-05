c393694528e4c15e4b26130a863232945ebfce01
spip.franck@lien-d-amis.net <spip.franck@lien-d-amis.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-04-05 17:52:33 +0000

    Un peu de rangement pour salvator
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@109813 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- agenda/branches/v3/lang/saisie_evenements_fr.php
+++ agenda/branches/v3/lang/saisie_evenements_fr.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/saisie_evenements/trunk/lang/
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/agenda/branches/v3/lang/
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
