dd98b181872e4a81ca16719459a2a8d12aaa4929
peetdu@gmail.com <peetdu@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-04-09 15:57:48 +0000

    copie à partir de wp_import/trunk
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@109853 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ wp_import/branches/V1/lang/paquet-wp_import_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/wp_import/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// W
+	'wp_import_description' => 'Importer la sauvegarde XML d’un site Wordpress dans votre site SPIP.',
+	'wp_import_nom' => 'Importer Wordpress',
+	'wp_import_slogan' => 'Grignotez Wordpress et savourez SPIP...'
+);
