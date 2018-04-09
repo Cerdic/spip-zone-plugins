9b78a9718ba24a947cbcf76fecd29eb71ef25ba5
cy.altern@gmail.com <cy.altern@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-04-09 11:23:10 +0000

    Ajout d'une option de configuration permettant d'utiliser un document à la place du logo du site pour les propriétés "og:image" et "twitter:image"
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@109840 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- metasplus/trunk/lang/metasplus_fr.php
+++ metasplus/trunk/lang/metasplus_fr.php
@@ -6,12 +6,15 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 }
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-
+	// E 
+	'explication_id_doc_logo' => 'Facultatif: id_document pour l\'image du site',
+	
 	// L
-	'label_reseaux' => 'Quels métas voulez-vous ne pas afficher ?',
+	'label_reseaux' => 'Cochez les métas que vous ne voulez pas afficher',
 	'label_dublincore' => 'Dublin Core',
 	'label_opengraph' => 'Open Graph',
 	'label_twitter' => 'Twitter',
+	'label_id_doc_logo' => 'permet d\'utiliser une image de votre choix à la place du logo du site de SPIP<br>propriétés "og:image" et "twitter:image". Format conseillé: 1,91:1',
 	// T
 	'titre_config_metasplus'	=>	'Configurer Métas +',
 
