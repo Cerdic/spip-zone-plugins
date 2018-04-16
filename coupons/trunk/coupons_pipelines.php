f20f051745fe743fa4684ffb5c7214739d17d3d4
nicolas.dorigny@gmail.com <nicolas.dorigny@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-04-16 21:15:10 +0000

    Ne pas multiplier par la quantité pour obtenir le montant du coupon, mais générer un coupon par bon cadeau
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@109918 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- coupons/trunk/coupons_pipelines.php
+++ coupons/trunk/coupons_pipelines.php
@@ -43,44 +43,32 @@ function coupons_post_edition($flux) {
 
 		foreach ($infos_bon_cadeau as $bon) {
 
-			// ne pas recréer le coupon s'il existe déjà
-			if (!sql_getfetsel(
-				'id_coupon',
-				'spip_coupons',
-				'id_commandes_detail_origine = ' . $bon['id_commandes_detail'])) {
-
+			for($i=0; $i<$bon['quantite']; $i++) {
 				// montant TTC du bon de réduction
-				$montant_coupon = $bon['quantite'] * $bon['prix_unitaire_ht'] * (1 + $bon['taxe']);
+				$montant_coupon = $bon['prix_unitaire_ht'] * (1 + $bon['taxe']);
 				if ($montant_coupon) {
 					include_spip('action/editer_objet');
-					$id_coupon = objet_inserer('coupon');
-					$valeurs_coupon   = array(
-						'montant'                    => $montant_coupon,
-						'restriction_taxe'           => $bon['taxe'],
+					$id_coupon      = objet_inserer('coupon');
+					$valeurs_coupon = array(
+						'montant'                     => $montant_coupon,
+						'restriction_taxe'            => $bon['taxe'],
 						'id_commandes_detail_origine' => $bon['id_commandes_detail'],
-						'titre'                      => $bon['titre'] . ' ' . supprimer_tags(prix_formater($montant_coupon)),
-						'code'                       => coupon_generer_code(),
+						'titre'                       => $bon['titre'],
+						'code'                        => coupon_generer_code(),
 					);
 					objet_modifier('coupon', $id_coupon, $valeurs_coupon);
 
 					// envoi d'une notification
-					if($destinataires = lire_config('coupons/emails_notifications')) {
+					if ($destinataires = lire_config('coupons/emails_notifications')) {
 						$expediteur   = lire_config('email_webmaster');
 						$texte        = recuperer_fond('notifications/notification_coupon', $valeurs_coupon);
 						$envoyer_mail = charger_fonction('envoyer_mail', 'inc');
 						$envoyer_mail($destinataires, _T('coupons:creation_coupon'), $texte, $expediteur);
 					}
 
-
 					spip_log('création du coupon ' . $id_coupon, 'coupons');
 					spip_log($valeurs_coupon, 'coupons');
-
-				} else {
-					// comment est ce possible ?
-					spip_log('montant coupon null ', 'coupons');
-					spip_log($bon, 'coupons');
-				}
-
+				} 
 			}
 
 		}
