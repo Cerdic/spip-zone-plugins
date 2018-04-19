7dd90ad7b71c0aee4878bc289531a796ea841347
abelass <abelass@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-04-19 09:35:30 +0000

    les promotions von vers le plugin dédié
    https://github.com/abelass/promotions_reservations
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@109946 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- reservation_evenement/trunk/reservation_evenement_options.php
+++ reservation_evenement/trunk/reservation_evenement_options.php
@@ -14,5 +14,3 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 // Active no spam sur le formulaire reservation.
 $GLOBALS['formulaires_no_spam'][] = 'reservation';
 
-// Ajoute le plugin aux promotions.
-$GLOBALS['promotion_plugin']['reservation_evenement'] = _T('reservation_evenement:reservation_evenement_titre');
