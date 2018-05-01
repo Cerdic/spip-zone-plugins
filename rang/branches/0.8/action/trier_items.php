adb976fedcc4fd6dd635c585199f8ee7aadde90d
nicod@lerebooteux.fr <nicod@lerebooteux.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-05-01 13:14:37 +0000

    Préparation d'une nouvelle branche, probablement temporaire
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@110053 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ rang/branches/0.8/action/trier_items.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Fonctions utiles au plugin Rang
+ *
+ * @plugin     Rang
+ * @copyright  2016
+ * @author     Peetdu
+ * @licence    GNU/GPL
+ * @package    SPIP\Rang\Fonctions
+ */
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+/**
+ * réordonner les rangs de la liste suite à un nouveau classement
+ *
+ * @param array $tab
+ *     tableau des items de la liste suite à une modification du classement
+ * @param string $page
+ * 		quelle pagination
+ * @param string $objet
+ *     sur quel objet faire le classement
+ * @param string $rubrique optionel
+ *     dans quelle rubrique faire le classement
+ *
+**/
+function action_trier_items_dist() {
+
+	include_spip('base/objets');
+	include_spip('base/objets_parents');
+
+	$tab		= _request('trier');
+	$page 		= _request('debut_liste');
+	$objet		= _request('objet');
+	$id_parent	= _request('id_parent');
+
+	$table		= table_objet_sql($objet);
+	$id_objet	= id_table_objet($objet);
+	$objet_type = objet_type($objet);
+	if ($id_parent != 'rien') {
+	 	$parent	= type_objet_info_parent($objet_type);
+		$parent_champ = $parent['0']['champ'];
+	}
+
+	// reclassement !
+	foreach ($tab as $key => $value) {
+		$rang	= $page + $key + 1; // le classement commence à 1, pas à 0
+		$id		= intval(substr($value, 3));
+		if ($id_parent == 'rien') {
+			$where = "$id_objet=$id";
+		}
+		else {
+			$where = "$id_objet=$id AND $parent_champ=$id_parent";
+		}
+		sql_updateq($table, array('rang' => $rang), $where);
+	}
+
+	include_spip('inc/invalideur');
+	suivre_invalideur("rang");
+
+}
---
+++ rang/branches/0.8/inc/rang_api.php
@@ -0,0 +1,206 @@
+<?php
+/**
+ * Fonctions utiles au plugin Rang
+ *
+ * @plugin     Rang
+ * @copyright  2016
+ * @author     Peetdu
+ * @licence    GNU/GPL
+ * @package    SPIP\Rang\Fonctions
+ */
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+/**
+ * Construire la liste des objets à exclure de la configuration
+ * 
+ * @return array
+ *	tableau des tables SPIP à exclure (ex : spip_auteurs, spip_mots, etc.)
+ */
+function rang_objets_a_exclure() {
+	$exclus = array();
+
+	// on exclu toujours les objets suivants
+	$liste_toujours_exclus = array('spip_auteurs', 'spip_documents', 'spip_groupes_mots', 'spip_messages');
+	$exclus = array_merge($exclus, $liste_toujours_exclus);
+	
+	// Pour le moment, on ne gère pas les rubriques elles-memes
+	array_push($exclus, 'spip_rubriques');
+
+	// et on ne gère pas les breves et sites
+	array_push($exclus, 'spip_syndic');
+	array_push($exclus, 'spip_breves');
+
+	return $exclus;
+}
+
+/**
+ * Tester si un mot-clé est lié
+ *
+ * @param array $tab
+ *     tableau des items de la liste suite à une modification du classement
+ * @param string $objet
+ *     sur quel objet faire le classement
+ * @param string $rubrique optionel
+ *     dans quelle rubrique faire le classement
+ * @return array
+ *     note : sans ce return (a priori inutile, la fonction plante (???)
+**/
+function rang_objet_select($id_mot, $id_objet, $objet) {
+	$id_mot = intval($id_mot);
+	$id_objet = intval($id_objet);
+	$res = sql_countsel('spip_mots_liens', array("id_mot=$id_mot", "id_objet=$id_objet", "objet=$objet"));
+	if ($res > 0 ) return true;
+	else return false;
+}
+
+
+/**
+ * supprimer un mot, puis réordonner
+ *
+ * @param string $id
+ *     id de la forme mot_xx
+ * @return array
+ *     note : sans ce return (a priori inutile, la fonction plante (???)
+**/
+// function rang_supprimer_item($id){
+// 	// suppression du mot
+// 	$new_id = intval(substr($id, 4));
+// 	sql_delete('spip_mots', "id_mot=$new_id");
+
+// 	//récupérer le tableau id/rang actuel
+// 	$id_grp = sql_getfetsel('id_groupe', 'spip_groupes_mots', 'titre='.sql_quote('Actualités'));
+// 	$res = sql_select('id_mot', 'spip_mots', "id_groupe=$id_grp", '', 'rang');
+// 	while ($tab = sql_fetch($res)){
+// 		$new_tab[] = 'id_mot_'.$tab['id_mot'];
+// 	}
+
+// 	//  réordonnement !
+// 	$tab_result = rang_trier_items($new_tab);
+
+// 	return $tab_result;
+// }
+
+/**
+ * Créer les champs 'rang' sur les tables des objets reçus en paramètre
+ * et initialiser la valeur du rang
+ * TODO : à compléter !
+ *
+ * @param array $objets
+ *     liste d'objets
+ **/
+function rang_creer_champs ($objets) {
+	foreach ($objets as $key => $table) {
+
+		if (!empty($table)) {
+			// si le champ 'rang' n'existe pas, le créer et le remplir
+			$champs_table = sql_showtable($table);
+			if (!isset($champs_table['field']['rang'])) {
+
+				// créer le champ 'rang'
+				sql_alter('TABLE '.$table.' ADD rang SMALLINT NOT NULL');
+
+				// remplir #1 : si aucun numero_titre n'est trouvé, on met la valeur de l'id_prefixe dans rang
+				if (!rang_tester_presence_numero($table)) {
+					$id = id_table_objet($table);
+					$desc = lister_tables_objets_sql($table);
+					if (isset($desc['field']['id_rubrique'])) {
+						$quelles_rubriques = sql_allfetsel('id_rubrique', $table, '', 'id_rubrique');
+
+						foreach ($quelles_rubriques as $key => $value) {
+							$id_rub =  $value['id_rubrique'];
+							$quelles_items = sql_allfetsel($id, $table, 'id_rubrique='.$id_rub);
+
+							$i = 1;
+							foreach ($quelles_items as $key => $value) {
+								$id_prefixe = $value[$id];
+								sql_update($table, array( 'rang' => $i ), "$id = $id_prefixe");
+								$i++;
+							}
+						}
+					}
+				}
+
+				// remplir #2 sinon , recuperer le numero_titre et l'insérer dans rang
+				// à faire !!
+			}
+		}
+	}
+}
+
+
+/**
+ * Construction, a partir des objets selectionnes, des chemins de sources vers les listes correspondantes
+ * Ce tableau sera ensuite comparé à la valeur $flux['data']['source'] fourni par le pipeline recuperer_fond()
+ *
+ * @return array
+ *     les chemins sources vers les listes où activer Rang
+ **/
+
+function rang_get_sources() {
+	include_spip('inc/config');
+	// mettre en cache le tableau calculé
+	static $sources;
+	if(is_array($sources)){
+		return $sources;
+	}
+	
+	$sources = array();
+	$objets_selectionnes = lire_config('rang/rang_objets');
+	$objets=explode(',',$objets_selectionnes);
+
+	foreach ($objets as $value) {
+		$objet = table_objet($value);
+		if (!empty($value)) {
+			$source = 'prive/objets/liste/'.$objet;
+			$sources[] = $source;
+		}
+
+		// cas objets historiques
+		if ($objet == 'mots') {
+			$source = 'prive/objets/liste/mots-admin';
+			$sources[] = $source;
+		}
+	}
+
+	// tempo : test sur des liaisons
+	$sources[] = 'prive/objets/liste/mots_lies';
+
+	return $sources;
+}
+
+/**
+ * Calculer le rang pour la nouvelle occurence de l’objet
+ * @param string $table
+ * @param int $id_objet
+ * @return int
+ */
+ function rang_classer_dernier($table, $id_objet) {
+
+ 	$objet_type = objet_type($table);
+ 	include_spip('base/objets_parents');
+
+	// si l'objet à un parent…
+	if ($parent = type_objet_info_parent($objet_type)) {
+		$id_table_objet = id_table_objet($table);
+		$parent_champ = $parent['0']['champ'];
+		$id_parent = sql_getfetsel($parent_champ, $table, "$id_table_objet = $id_objet");
+		$rang = sql_getfetsel('max(rang)', $table, "$parent_champ = $id_parent");
+	} else {
+	// si pas de parent, c'est plus simple
+		$rang = sql_getfetsel('max(rang)', $table);
+	}
+	 
+	// todo : on classe l'objet à la fin (rang max) mais on pourrait vouloir le classer au début
+	// il faudrait donc une configuration pour ça, et dans ce cas reclasser tous les autres à un rang++
+	$dernier = $rang+1;
+
+	return $dernier;
+ }
+
+function rang_tester_presence_numero($table) {
+	return false;
+}
+
