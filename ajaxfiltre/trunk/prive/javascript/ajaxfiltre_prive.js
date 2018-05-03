6fbc3dd2cba4d71bbdded6be53bdfe988716f85c
nicod@lerebooteux.fr <nicod@lerebooteux.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-05-03 22:51:18 +0000

    Ce plugin permet de créer une navigation par facettes qui recharge une liste d’objets en ajax.
    Fonctionne avec n’importe quelle liste d’objets, il suffit d’ajouter le nom liste-objets au paramètre ajax dans prive/squelettes/contenu/pataes.html, et de créer un squelette prive/squelettes/navigation/patates.html dans lequel on peut utiliser des saisies.
    
    Voir la page de démo (?exec=ajaxfiltre_articles) et ces trois squelettes documentés :
        /ajaxfiltre/prive/objets/liste/ajaxfiltre_articles.html
        /ajaxfiltre/prive/squelettes/contenu/ajaxfiltre_articles.html
        /ajaxfiltre/prive/squelettes/navigation/ajaxfiltre_articles.html
    
    
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@110094 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ ajaxfiltre/trunk/prive/javascript/ajaxfiltre_prive.js
@@ -0,0 +1,71 @@
+(function($) {
+
+	$.ajaxFiltre = function(el, options) {
+		var base = this;
+
+		base.init = function() {
+			base.$el = $(el);
+			base.el = el;
+			base.options = $.extend({}, $.ajaxFiltre.defaultOptions, options);
+
+			base.$el.find('select, input[type=checkbox], input[type=radio]').on('change', function() {
+				base.$el.submit();
+			});
+
+			base.$el.on('submit', function(e) {
+				e.preventDefault();
+				
+				var formData = base.$el.serializeArray().reduce(function(obj, item) {
+					// si le name comporte des [] on construit un tableau
+					if(item.name.indexOf('[]') !== -1) {
+						var name = item.name.replace(/[\[\]]+/g, '');
+						if(!obj.hasOwnProperty(name)) {
+							obj[name] = [];
+						}
+						obj[name].push(item.value);
+					} else {
+						obj[item.name] = item.value;
+					}
+					return obj;
+				}, {});
+
+				// passer une valeur vide explicite pour les checkbox dont le name comporte des [] et dans lesquels rien n'est coché
+				var $checkradio = base.$el.find('input[type=checkbox][name*="[]"]');
+				$.each($checkradio,function(){
+					var checkRadioName = $(this).attr('name');
+					// si rien n'est coché
+					if(!base.$el.find('input[name="'+checkRadioName+'"]:checked').length){
+						// supprimer les données du nom avec []
+						delete formData[checkRadioName];
+						// ajouter un tableau vide sur le nom sans [] 
+						formData[checkRadioName.replace(/[\[\]]+/g, '')] = [];
+					}
+				});
+				
+				console.log(formData);
+				
+				ajaxReload(base.options.ajaxTarget, {args: formData});
+			});
+
+		};
+
+		base.init();
+	};
+
+	$.ajaxFiltre.defaultOptions = {
+		ajaxTarget: "liste-objets"
+	};
+
+	$.fn.ajaxFiltre = function(options) {
+		return this.each(function() {
+			(new $.ajaxFiltre(this, options));
+		});
+	};
+
+})(jQuery);
+
+$(function() {
+
+	$('.formulaire_navigation_filtre form').ajaxFiltre({ajaxTarget: 'liste-objets'});
+
+});
