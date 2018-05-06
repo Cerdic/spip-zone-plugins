8b72c7582d7dd4e039699a63507ce36067088a51
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-05-06 14:58:47 +0000

    On supprime le fichier des balises et on les insère directement dans le fichier fonctions
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@110115 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- noizetier/trunk/noizetier_fonctions.php
+++ noizetier/trunk/noizetier_fonctions.php
@@ -15,6 +15,22 @@ include_spip('inc/noizetier_type_noisette');
 // ------------------------- API NOISETTES : COMPLEMENT ---------------------------
 // --------------------------------------------------------------------------------
 
+function balise_NOIZETIER_NOISETTE_PREVIEW_dist($p) {
+	$id_noisette = champ_sql('id_noisette', $p);
+	$type_noisette = champ_sql('type_noisette', $p);
+	$parametres = champ_sql('parametres', $p);
+
+	$inclusion = "recuperer_fond(
+		'noisette_preview',
+		array_merge(unserialize($parametres), array('type_noisette' => $type_noisette))
+	)";
+
+	$p->code = "$inclusion";
+	$p->interdire_scripts = false;
+
+	return $p;
+}
+
 
 // --------------------------------------------------------------------------------
 // ------------------------- API CONTENEURS : COMPLEMENT --------------------------
@@ -129,15 +145,94 @@ function noizetier_bloc_compter_noisettes($page_ou_objet) {
 	return (isset($blocs_compteur[$page_ou_objet]) ? $blocs_compteur[$page_ou_objet] : array());
 }
 
+// Cette balise renvoie la description complète ou l'info donnée d'un bloc
+function balise_NOIZETIER_BLOC_INFOS_dist($p) {
+	$bloc = interprete_argument_balise(1, $p);
+	$bloc = str_replace('\'', '"', $bloc);
+	$information = interprete_argument_balise(2, $p);
+	$information = isset($information) ? str_replace('\'', '"', $information) : '""';
+	$p->code = "calculer_infos_bloc($bloc, $information)";
+
+	return $p;
+}
+
+function calculer_infos_bloc($bloc = '', $information = '') {
+
+	include_spip('inc/noizetier_bloc');
+	return noizetier_bloc_lire($bloc, $information);
+}
+
+
 // -------------------------------------------------------------------
 // ---------------------------- API PAGES ----------------------------
 // -------------------------------------------------------------------
+
+// Cette balise renvoie le tableau de la liste des objets possédant des noisettes configurées
+function balise_NOIZETIER_PAGE_INFOS_dist($p) {
+
+	// Récupération des arguments de la balise.
+	// -- seul l'argument information est optionnel.
+	$page = interprete_argument_balise(1, $p);
+	$page = str_replace('\'', '"', $page);
+	$information = interprete_argument_balise(3, $p);
+	$information = isset($information) ? str_replace('\'', '"', $information) : '""';
+
+	// Calcul de la balise
+	$p->code = "calculer_infos_page($page, $information)";
+
+	return $p;
+}
+
+function calculer_infos_page($page, $information = '') {
+
+	include_spip('inc/noizetier_page');
+	return noizetier_page_lire($page, $information);
+}
 
 
 // --------------------------------------------------------------------
 // ---------------------------- API OBJETS ----------------------------
 // --------------------------------------------------------------------
 
+// Cette balise renvoie le tableau de la liste des objets possédant des noisettes configurées
+function balise_NOIZETIER_OBJET_INFOS_dist($p) {
+
+	// Récupération des arguments de la balise.
+	// -- seul l'argument information est optionnel.
+	$objet = interprete_argument_balise(1, $p);
+	$objet = str_replace('\'', '"', $objet);
+	$id_objet = interprete_argument_balise(2, $p);
+	$id_objet = isset($id_objet) ? $id_objet : '0';
+	$information = interprete_argument_balise(3, $p);
+	$information = isset($information) ? str_replace('\'', '"', $information) : '""';
+
+	// Calcul de la balise
+	$p->code = "calculer_infos_objet($objet, $id_objet, $information)";
+
+	return $p;
+}
+
+function calculer_infos_objet($objet, $id_objet, $information = '') {
+
+	include_spip('inc/noizetier_objet');
+	return noizetier_objet_lire($objet, $id_objet, $information);
+}
+
+
+// Cette balise renvoie le tableau de la liste des objets possédant des noisettes configurées
+function balise_NOIZETIER_OBJET_LISTE_dist($p) {
+
+	// Aucun argument à la balise.
+	$p->code = "calculer_liste_objets()";
+
+	return $p;
+}
+
+function calculer_liste_objets() {
+
+	include_spip('inc/noizetier_objet');
+	return noizetier_objet_repertorier();
+}
 
 // --------------------------------------------------------------------
 // ------------------------- API CONFIGURATION ------------------------
@@ -187,9 +282,3 @@ function noizetier_configuration_est_modifiee($entite, $identifiant) {
 
 	return $est_modifiee;
 }
-
-
-// --------------------------------------------------------------------
-// ------------------------------ BALISES -----------------------------
-// --------------------------------------------------------------------
-include_spip('public/noizetier_balises');
