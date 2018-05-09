ea443aa49748b181035522e6e77d2053c0d72dc7
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-05-09 22:35:53 +0000

    Juste un peu d'info en plus dans la description
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@110172 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- declarerparent/trunk/lang/paquet-declarerparent_fr.php
+++ declarerparent/trunk/lang/paquet-declarerparent_fr.php
@@ -8,7 +8,7 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// D
-	'declarerparent_description' => 'En attendant https://core.spip.net/issues/3844',
-	'declarerparent_nom' => 'Déclarer parent',
-	'declarerparent_slogan' => 'Définir la relation a l‘objet parent dans la déclaration des objets',
+	'declarerparent_description' => 'Cette fonctionnalité a vocation à intégrer le noyau de l’API de déclaration des objets. En attendant https://core.spip.net/issues/3844',
+	'declarerparent_nom' => 'Déclarer le parent',
+	'declarerparent_slogan' => 'Définir la relation à l‘objet parent dans la déclaration des objets',
 );
