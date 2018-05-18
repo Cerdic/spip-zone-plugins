265587b37f9563e0eacce9f4132de270aeca734a
pierrekuhn82@gmail.com <pierrekuhn82@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-05-18 07:10:21 +0000

    Version 0.10.20 : notice en moins sur le wiki
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@110297 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- autorite/trunk/autorite_options.php
+++ autorite/trunk/autorite_options.php
@@ -10,17 +10,17 @@ $GLOBALS['autorite'] = @unserialize($GLOBALS['meta']['autorite']);
 if ((is_array($GLOBALS['autorite'])) AND 
     (
         (isset($GLOBALS['autorite']['espace_wiki']) AND 
-            $GLOBALS['autorite']['espace_wiki_anonyme']
-        ) 
-        OR 
-        (isset($GLOBALS['autorite']['espace_wiki_motsclef']) AND 
-            $GLOBALS['autorite']['espace_wiki_motsclef_anonyme']
-        )
+			isset($GLOBALS['autorite']['espace_wiki_anonyme'])
+		) 
+		OR 
+		(isset($GLOBALS['autorite']['espace_wiki_motsclef']) AND 
+			isset($GLOBALS['autorite']['espace_wiki_motsclef_anonyme'])
+		)
     )) {
 	if (!function_exists('analyse_droits_rapide')) {
-	    function analyse_droits_rapide() {
-		    return true;
-	    }
+		function analyse_droits_rapide() {
+			return true;
+		}
 	} else {
 		$autorite_erreurs[] = 'analyse_droits_rapide';
     }
