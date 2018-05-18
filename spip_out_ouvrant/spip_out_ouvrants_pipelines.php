0790553ef01da7ffec448c974070f639d03322ca
pierrekuhn82@gmail.com <pierrekuhn82@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-05-18 07:53:05 +0000

    Version 1.3.2 : Notice en moins avec find_in_path
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@110298 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spip_out_ouvrant/spip_out_ouvrants_pipelines.php
+++ spip_out_ouvrant/spip_out_ouvrants_pipelines.php
@@ -1,16 +1,8 @@
 <?php
 
-
-
 function spip_out_ouvrants_insert_head($flux) {
 
-	$p=explode(basename(_DIR_PLUGINS)."/",str_replace('\\','/',realpath(dirname(__FILE__))));
-	define('_DIR_PLUGIN_SPIP_OUT_OUVRANTS',(_DIR_PLUGINS.end($p)));
+	$flux .= '<script type="text/javascript" src="'.find_in_path('spip_out_ouvrants.js').'"></script>';
 
-
-	$flux .= '<script  src="'._DIR_PLUGIN_SPIP_OUT_OUVRANTS.'spip_out_ouvrants.js" type="text/javascript"></script>';
-	
 	return $flux;
 }
-
-?>
