91041f5c6cc37d02f73cfb6dcfd330192c6bb48f
klaus@spip.de <klaus@spip.de@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-05-27 14:40:48 +0000

    Traduction allemande pour Cookiechoices - RGPD oblige
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@110403 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ cookiechoices/trunk/lang/paquet-cookiechoices_de.php
@@ -0,0 +1,13 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cookiechoices_description' => 'Zustimmung zur Verwendung von Cookies einholen.',
+	'cookiechoices_nom' => 'CookieChoices',
+	'cookiechoices_slogan' => 'Zeigt einen Hinweis zur Verwendung von Cookies an und ermöglicht ausdrüciche Zustimmung',
+);
+
