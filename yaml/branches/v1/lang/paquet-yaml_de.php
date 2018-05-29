9b17aea1e45a0dee058d249bfff6fa2742138efd
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-05-29 17:31:41 +0000

    Passage en trunk/branches du plugin YAML avant rafraichissement
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@110445 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ yaml/branches/v1/lang/paquet-yaml_de.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-yaml?lang_cible=de
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// Y
+	'yaml_description' => 'Dieses Plugin stellt die Funtkionen zum lesen/schreiben des Formats YAML bereit :
+	<code>yaml_decode()</code> und <code>yaml_encode()</code>. Es ermöglicht die Verwendung des Formats YAML in Schleifen (DATA).',
+	'yaml_slogan' => 'Ein einfaches Dateiformat um Datenlisten zu bearbeiten'
+);
