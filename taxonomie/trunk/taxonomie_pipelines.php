7a05966a9095ce7b1def5184601e3e7c31bc3f7d
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-06-05 20:07:10 +0000

    Correction de notice dans un pipeline.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@110523 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- taxonomie/trunk/taxonomie_pipelines.php
+++ taxonomie/trunk/taxonomie_pipelines.php
@@ -62,44 +62,46 @@ function taxonomie_pre_edition($flux) {
 **/
 function taxonomie_post_edition($flux) {
 
-	$table = $flux['args']['table'];
-	$id = intval($flux['args']['id_objet']);
-	$action = $flux['args']['action'];
+	if (isset($flux['args']['table'], $flux['args']['id_objet'], $flux['args']['action'])) {
+		$table = $flux['args']['table'];
+		$id = intval($flux['args']['id_objet']);
+		$action = $flux['args']['action'];
 
-	// Traitements particuliers de l'objet taxon quand celui-ci est institué manuellement
-	if (($table == 'spip_taxons') and $id) {
-		// Instituer : on ne peut instituer qu'une espèce dont aucun enfant n'est publié. Il est donc inutile de
-		// considérer ces cas.
-		if ($action == 'instituer') {
-			// On vérifie qu'on institue l'espèce de 'prop' à 'publie'. Si c'est le cas, alors on vérifie
-			// qu'il est aussi nécessaire d'instituer à 'publie' les enfants de type espèce encore à prop.
-			$statut_nouveau = $flux['data']['statut'];
-			$statut_ancien = $flux['args']['statut_ancien'];
-			if (($statut_ancien == 'prop') and ($statut_nouveau == 'publie')) {
-				// On récupère le TSN et le TSN parent de l'espèce concernée.
-				$from = 'spip_taxons';
-				$select = array('tsn', 'tsn_parent');
-				$where = array("id_taxon=$id");
-				$taxon = sql_fetsel($select, $from, $where);
+		// Traitements particuliers de l'objet taxon quand celui-ci est institué manuellement
+		if (($table == 'spip_taxons') and $id) {
+			// Instituer : on ne peut instituer qu'une espèce dont aucun enfant n'est publié. Il est donc inutile de
+			// considérer ces cas.
+			if ($action == 'instituer') {
+				// On vérifie qu'on institue l'espèce de 'prop' à 'publie'. Si c'est le cas, alors on vérifie
+				// qu'il est aussi nécessaire d'instituer à 'publie' les enfants de type espèce encore à prop.
+				$statut_nouveau = $flux['data']['statut'];
+				$statut_ancien = $flux['args']['statut_ancien'];
+				if (($statut_ancien == 'prop') and ($statut_nouveau == 'publie')) {
+					// On récupère le TSN et le TSN parent de l'espèce concernée.
+					$from = 'spip_taxons';
+					$select = array('tsn', 'tsn_parent');
+					$where = array("id_taxon=$id");
+					$taxon = sql_fetsel($select, $from, $where);
 
-				// On récupère les ascendants de type espèce de l'espèce concernée si ils existent.
-				include_spip('taxonomie_fonctions');
-				$ascendance = taxon_informer_ascendance($id, $taxon['tsn_parent'], 'ascendant');
-				if ($ascendance) {
-					// On publie les taxons en évitant une ré-entrance (donc sans appeler l'api objet)
-					// mais en utilisant directement une mise à jour sql.
-//					include_spip('action/editer_objet');
-					foreach ($ascendance as $_parent) {
-						if ($_parent['espece'] == 'oui') {
-							if (($_parent['statut'] <> 'publie')) {
-								$maj = array('statut' => $statut_nouveau, 'edite' => 'oui');
-								sql_updateq($from, $maj, 'id_taxon=' . intval($_parent['id_taxon']));
-//								objet_modifier('taxon', intval($_parent['id_taxon']), array('statut' => $statut_nouveau));
+					// On récupère les ascendants de type espèce de l'espèce concernée si ils existent.
+					include_spip('taxonomie_fonctions');
+					$ascendance = taxon_informer_ascendance($id, $taxon['tsn_parent'], 'ascendant');
+					if ($ascendance) {
+						// On publie les taxons en évitant une ré-entrance (donc sans appeler l'api objet)
+						// mais en utilisant directement une mise à jour sql.
+	//					include_spip('action/editer_objet');
+						foreach ($ascendance as $_parent) {
+							if ($_parent['espece'] == 'oui') {
+								if (($_parent['statut'] <> 'publie')) {
+									$maj = array('statut' => $statut_nouveau, 'edite' => 'oui');
+									sql_updateq($from, $maj, 'id_taxon=' . intval($_parent['id_taxon']));
+	//								objet_modifier('taxon', intval($_parent['id_taxon']), array('statut' => $statut_nouveau));
+								}
+							} else {
+								// Dès que l'on est sur un taxon non espèce on peut s'arrêter vu que les ascendants sont
+								// classé du parent le plus proche au plus éloigné.
+								break;
 							}
-						} else {
-							// Dès que l'on est sur un taxon non espèce on peut s'arrêter vu que les ascendants sont
-							// classé du parent le plus proche au plus éloigné.
-							break;
 						}
 					}
 				}
