279fe98f390ce1a73df77be7631c2461e00781ab
tcharlss@bravecassine.com <tcharlss@bravecassine.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-06-16 11:12:34 +0000

    v 0.4 : ajout de 2 paramètres optionnels à la balise #COULEUR. #COULEUR{parent} va chercher la couleur du parent de l'objet en fallback. #COULEUR{parent,recursif} fait la même chose mais de façon récursive, utile si par exemple on a défini une couleur à un secteur, et qu'on veut la connaître dans un article au fin fond d'une sous-rubrique. Nécessite l'API déclarer parent qui est dans un plugin séparé pour l'instant. Je ne le mets pas en dépendance, c'est un petit bonus si l'API est présente. Renommage de l'ancienne fonction privée utilisée par la balise en un nom plus générique et réutilisable dans les squelettes si besoin. Un peu de phpDoc et de PSR
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@110691 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- couleur_objet/trunk/couleur_objet_fonctions.php
+++ couleur_objet/trunk/couleur_objet_fonctions.php
@@ -1,17 +1,132 @@
 <?php
 
-// https://programmer.spip.net/Recuperer-objet-et-id_objet
-function balise_COULEUR($p){
-	// on prend nom de la cle primaire de l'objet pour calculer sa valeur
+/**
+ * Compile la balise #COULEUR
+ *
+ * Renvoie la couleur associée à un objet
+ *
+ * Elle accepte 2 paramètres :
+ * - #COULEUR{parent} pour prendre la couleur du parent en fallback
+ * - #COULEUR{parent,recursif} même chose, mais cherche le parent récursivement
+ * Attention, ne fonctionne que s'il y a l'API de déclaration des parents.
+ *
+ * @see
+ * https://programmer.spip.net/Recuperer-objet-et-id_objet
+ *
+ * @param Champ $p
+ *     Pile au niveau de la balise
+ * @return Champ
+ *     Pile complétée par le code à générer
+ */
+function balise_COULEUR($p) {
+
+	// On prend nom de la clé primaire de l'objet pour calculer sa valeur
 	$_id_objet = $p->boucles[$p->id_boucle]->primary;
-	$id_objet = champ_sql($_id_objet, $p);
-	$objet = $p->boucles[$p->id_boucle]->id_table;
-	$p->code = "calculer_balise_COULEUR('$objet', $id_objet)";
+	$id_objet  = champ_sql($_id_objet, $p);
+	$objet     = $p->boucles[$p->id_boucle]->id_table;
+	// 1er paramètre : prendre le parent en fallback
+	// On vérifie juste si le texte est présent, peu importe la valeur
+	$_parent = "false";
+	if (($v = interprete_argument_balise(1, $p)) !== null) {
+		$_parent = "strlen($v) ? true : false";
+	}
+	// 2ème paramètre : chercher le parent récursivement
+	// On vérifie juste si le texte est présent, peu importe la valeur
+	$_recursif = "false";
+	if (($v2 = interprete_argument_balise(2, $p)) !== null) {
+		$_recursif = "strlen($v2) ? true : false";
+	}
+
+	$p->code = "objet_couleur('$objet', $id_objet, $_parent, $_recursif)";
+
 	return $p;
 }
-function calculer_balise_COULEUR($objet,$id_objet){
+
+/**
+ * Trouver la couleur d'un objet ou de son parent
+ *
+ * @uses objet_couleur_parent()
+ *
+ * @param string $objet
+ *     Type de l'objet
+ * @param int $id_objet
+ *     Identifiant de l'objet
+ * @param boolean $parent
+ *     true pour chercher la couleur du parent
+ * @param boolean $recursif
+ *     true pour chercher les parents récursivement
+ * @return string|false
+ *     La couleur ou false si rien trouvé
+ */
+function objet_couleur($objet, $id_objet, $parent = false, $recursif = false) {
+
+	include_spip('base/objets');
+
 	$objet = objet_type($objet);
-	$row = sql_fetsel("couleur_objet", "spip_couleur_objet_liens", "objet=".sql_quote($objet)." AND id_objet=".intval($id_objet));
-	$couleur_objet = $row['couleur_objet'];
+	$couleur_objet = sql_getfetsel(
+		'couleur_objet',
+		'spip_couleur_objet_liens',
+		array(
+			'objet=' . sql_quote($objet),
+			'id_objet=' . intval($id_objet)
+		)
+	);
+
+	// Si besoin, on prend la couleur du parent
+	if (
+		!$couleur
+		and $parent
+	) {
+		$couleur_objet = objet_couleur_parent($objet, $id_objet, $recursif);
+	}
+
 	return $couleur_objet;
+}
+
+/**
+ * Trouver la couleur du parent d'un objet
+ *
+ * @note
+ * Nécéssite l'API de déclaration des parents
+ *
+ * @uses objet_trouver_parent()
+ * 
+ * @param string $objet
+ *     Type de l'objet
+ * @param int $id_objet
+ *     Identifiant de l'objet
+ * @param boolean $recursif
+ *     true pour chercher les parents récursivement
+ * @return string|null
+ *     La couleur ou null si rien trouvé
+ */
+function objet_couleur_parent($objet, $id_objet, $recursif = false) {
+
+	$couleur = false;
+
+	// Uniquement si l'API existe
+	if (
+		include_spip('base/objets_parents')
+		and $parent = objet_trouver_parent($objet, $id_objet)
+	) {
+		$couleur = sql_getfetsel(
+			'couleur_objet',
+			'spip_couleur_objet_liens',
+			array (
+				'objet = ' . sql_quote($parent['objet']),
+				'id_objet = ' . intval($parent['id_objet']),
+			)
+		);
+
+		// Si besoin on cherche récursivement
+		if (
+			!$couleur
+			and $recursif
+		) {
+			$couleur = objet_couleur_parent($parent['objet'], $parent['id_objet'], true);
+		}
+
+	}
+
+	return $couleur;
 }
