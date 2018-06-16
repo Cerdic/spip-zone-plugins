d5aa076e55bfe811cda8fd26d7ec54625bc454e8
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-06-16 15:51:59 +0000

    Mise au point de la demo. Ca permet de se rendre compte que certains YAML existant sont un peu permissifs (saisies input et email).
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@110694 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- yaml/trunk/demo/yaml_fonctions.php
+++ yaml/trunk/demo/yaml_fonctions.php
@@ -6,7 +6,7 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 function ajouter_libyaml($librairies) {
 
 	if (function_exists('yaml_parse')) {
-		$librairies[] = 'libyaml';
+		$librairies['libyaml'] = 'libYAML';
 	}
 
 	return $librairies;
@@ -25,15 +25,46 @@ function decoder_fichier_yaml($filename, $options = array()) {
 	return array('lib' => sinon($options['library'], 'sfyaml'), 'duree' => $duree*1000, 'yaml' => $parsed);
 }
 
-function decoder_fichier_yaml_avec_inclusions($filename, $options = array()) {
-	$timestamp_debut = microtime(true);
+function comparer_decodage($fichier, $libraries) {
 
 	include_spip('inc/yaml');
-	$file = find_in_path($filename);
-	$parsed = yaml_charger_inclusions(yaml_decode_file($file, $options));
 
-	$timestamp_fin = microtime(true);
-	$duree = $timestamp_fin - $timestamp_debut;
+	$compared = array();
+	$first = array();
 
-	return array('lib' => sinon($options['library'], 'sfyaml'), 'duree' => $duree*1000, 'yaml' => $parsed);
+	foreach ($libraries as $_library) {
+		$parsed = yaml_decode_file($fichier, array('library' => $_library));
+		if (!$compared) {
+			$first = $parsed;
+			$compared[$_library] = 'référence';
+		} else {
+			$difference = array_diff_assoc_recursive($first, $parsed, $fichier);
+			$compared[$_library] = $difference ? bel_env($difference, true) : 'idem';
+		}
+	}
+
+	return $compared;
+}
+
+function array_diff_assoc_recursive($array1, $array2, $fichier) {
+
+    $difference = array();
+
+    foreach ($array1 as $_key => $_value) {
+        if (is_array($_value)) {
+            if (!isset($array2[$_key]) || !is_array($array2[$_key])) {
+                $difference[$_key] = $_value;
+            } else {
+                $new_diff = array_diff_assoc_recursive($_value, $array2[$_key], $fichier);
+                if (!empty($new_diff))
+                    $difference[$_key] = $new_diff;
+            }
+        } elseif (!is_array($array2)) {
+        	echo $fichier, ' - ', $_key, ' - ', $_value, '<br />';
+        } elseif (!array_key_exists($_key, $array2) || ($array2[$_key] !== $_value)) {
+            $difference[$_key] = $_value;
+        }
+    }
+
+    return $difference;
 }
