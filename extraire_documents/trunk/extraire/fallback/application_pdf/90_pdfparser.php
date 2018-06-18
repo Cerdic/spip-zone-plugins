d18385fbae7d35b5cec8861777dd26b85c264275
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-06-18 16:14:04 +0000

    indentation
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@110744 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- extraire_documents/trunk/extraire/fallback/application_pdf/90_pdfparser.php
+++ extraire_documents/trunk/extraire/fallback/application_pdf/90_pdfparser.php
@@ -3,14 +3,13 @@
 /**
  * Tester si cette méthode d'extraction est disponible
  **/
-function extraire_fallback_application_pdf_90_pdfparser_test_dist() {
+function extraire_fallback_application_pdf_90_pdfparser_test_dist(){
 	if (
 		find_in_path('lib/TCPDF-6.2.17')
 		and find_in_path('lib/pdfparser-0.12.0/src')
-	) {
+	){
 		return true;
-	}
-	else {
+	} else {
 		return false;
 	}
 }
@@ -22,53 +21,51 @@ function extraire_fallback_application_pdf_90_pdfparser_test_dist() {
  * @param $fichier le fichier à traiter
  * @return Scontenu le contenu brut
  **/
-function extraire_fallback_application_pdf_90_pdfparser_extraire_dist($fichier) {
-    $infos = array('contenu' => false);
-    $contenu = '';
+function extraire_fallback_application_pdf_90_pdfparser_extraire_dist($fichier){
+	$infos = array('contenu' => false);
+	$contenu = '';
 
-    // Bespoin de charger composer
-    if (!class_exists('Composer\\Autoload\\ClassLoader')) {
+	// Bespoin de charger composer
+	if (!class_exists('Composer\\Autoload\\ClassLoader')){
 		include_spip('lib/Composer/Autoload/ClassLoader');
 	}
-    include_spip('lib/TCPDF-6.2.17/tcpdf_parser');
+	include_spip('lib/TCPDF-6.2.17/tcpdf_parser');
 
-    $loader = new \Composer\Autoload\ClassLoader();
+	$loader = new \Composer\Autoload\ClassLoader();
 
-    // register classes with namespaces
-    $loader->add('Smalot\PdfParser', find_in_path('lib/pdfparser-0.12.0/src'));
-    $loader->register();
+	// register classes with namespaces
+	$loader->add('Smalot\PdfParser', find_in_path('lib/pdfparser-0.12.0/src'));
+	$loader->register();
 
-    $parser = new \Smalot\PdfParser\Parser();
-    //Tenter de lire le pdf
-    try {
-        set_time_limit (0);
-        $pdf = $parser->parseFile($fichier);
-    }
-    catch (Exception $e) {
-        //Pour toute exception on s'arrete et on retourne un contenu vide
-        //Les cas de figure sont entre autre les fichiers mal formés ou signés
-        return '';
-    }
+	$parser = new \Smalot\PdfParser\Parser();
+	//Tenter de lire le pdf
+	try {
+		set_time_limit(0);
+		$pdf = $parser->parseFile($fichier);
+	} catch (Exception $e) {
+		//Pour toute exception on s'arrete et on retourne un contenu vide
+		//Les cas de figure sont entre autre les fichiers mal formés ou signés
+		return '';
+	}
 
-    // Parcourir les pages et extraire le contenu textuel
-    try {
-        foreach ($pdf->getPages() as $page) {
-            $contenu .= $page->getText();
-        }
-    }
-    catch (Exception $e) {
-        //si on ne peut extraire le texte on passe à la page suivante
-        $contenu .= '';
-    }
+	// Parcourir les pages et extraire le contenu textuel
+	try {
+		foreach ($pdf->getPages() as $page){
+			$contenu .= $page->getText();
+		}
+	} catch (Exception $e) {
+		//si on ne peut extraire le texte on passe à la page suivante
+		$contenu .= '';
+	}
 
-    //Libérer les ressources
-    unset($parser);
-    unset($loader);
-	
+	//Libérer les ressources
+	unset($parser);
+	unset($loader);
+
 	// Si on a trouvé du texte
-	if ($contenu) {
+	if ($contenu){
 		$infos['contenu'] = $contenu;
 	}
-	
-    return $infos;
+
+	return $infos;
 }
