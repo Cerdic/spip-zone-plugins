a4060ee30b789491240863c98f6aea0106b5600a
nicod@lerebooteux.fr <nicod@lerebooteux.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-06-22 17:15:12 +0000

    Un plugin qui a bien failli s'appeler moteur : le rubriqueur sert à créer des rubriques, celui-ci sert à créer des mots. On colle toute une liste dans un champ texte, on choisit le groupe, et zou.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@110806 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ mots_creer/trunk/formulaires/motscreer.php
@@ -0,0 +1,43 @@
+<?php
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+function formulaires_motscreer_charger_dist() {
+	return array(
+		'id_groupe' => '',
+		'mots'       => '',
+	);
+}
+
+function formulaires_motscreer_verifier_dist() {
+	$retour = array();
+	if (!_request('mots')) {
+		$retour['mots'] = _T('info_obligatoire');
+	}
+	if (!_request('id_groupe')) {
+		$retour['id_groupe'] = _T('info_obligatoire');
+	}
+	if(!autoriser('modifier','groupemots',_request('id_groupe'))){
+		$retour['id_groupe'] = _T('motscreer:pas_autorise');
+	}
+	return $retour;
+}
+
+function formulaires_motscreer_traiter_dist() {
+	include_spip('action/editer_mot');
+	
+	$id_groupe = intval(_request('id_groupe'));
+	$mots = array_filter(preg_split('#[\r\n]#',_request('mots')));
+
+	foreach ($mots as $mot) {
+		$id_mot = mot_inserer($id_groupe);
+		mot_modifier($id_mot,array('titre' => $mot));
+	}
+	
+	return array(
+		'message_ok' => _T('motscreer:mots_crees'),
+		'editable'   => true,
+	);
+}
---
+++ mots_creer/trunk/lang/motscreer_fr.php
@@ -0,0 +1,25 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'bouton_creer'      => 'Créer les mots',
+
+	// G
+	'groupe'            => 'Dans le groupe',
+
+	// M
+	'mots'              => 'Mots à créer',
+	'mots_crees'        => 'Les mots ont été créées',
+	'mots_explications' => 'Un mot par ligne',
+	'motscreer_titre'   => 'Créer des mots clés',
+
+	// P
+	'pas_autorise'      => 'Vous ne pouvez pas créer de mots dans ce groupe.',
+
+);
