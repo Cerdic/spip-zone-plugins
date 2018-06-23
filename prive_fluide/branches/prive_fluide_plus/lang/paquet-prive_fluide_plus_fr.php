74aa7010ba5c6c19b1cd9220e688d70818ba7cae
nicod@lerebooteux.fr <nicod@lerebooteux.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-06-23 21:43:45 +0000

    Surcharge de l'espace privé fluide en version 100% de large : travail principalement sur les formulaires
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@110829 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ prive_fluide/branches/prive_fluide_plus/lang/paquet-prive_fluide_plus_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// P
+	'prive_fluide_plus_description' => '',
+	'prive_fluide_plus_nom' => 'Espace Privé fluide ++',
+	'prive_fluide_plus_slogan' => 'Surcharge de l\'Espace privé fluide',
+);
