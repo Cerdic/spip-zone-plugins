78114b9a196d2b6b6a4a817f092d3231856f6939
pierrekuhn82@gmail.com <pierrekuhn82@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-06-27 05:54:02 +0000

    On met dans le trunk une coppir de la branche pour repartir sur la base
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@110892 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ passe_complexe/trunk/passe_complexe_options.php
@@ -0,0 +1,18 @@
+<?php
+
+
+function passe_complexe_header_prive($flux){
+  if (_request('exec')=='auteur_infos'){
+	include_spip('inc/passe_complexe');
+	$flux .= passe_complexe_generer_javascript("input[name=new_pass]");
+  } else if (_request('exec')=='cfg'){
+	include_spip('inc/passe_complexe');
+	$flux .= passe_complexe_generer_javascript("input.type_pwd");
+  } else if (_request('p')){
+	include_spip('inc/passe_complexe');
+	$flux .= passe_complexe_generer_javascript("input#oubli");
+}
+  return $flux;
+}
+
+?>
