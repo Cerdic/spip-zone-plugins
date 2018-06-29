ccb297e18cb433e244ba064a92849d3e9334d232
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-06-29 03:39:26 +0000

    [Salvatore] [source:_plugins_/passe_complexe/trunk/lang/ paquet-passe_complexe] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@110936 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- passe_complexe/trunk/lang/paquet-passe_complexe_fr.php
+++ passe_complexe/trunk/lang/paquet-passe_complexe_fr.php
@@ -1,12 +1,13 @@
 <?php
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/passe_complexe/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-	// A
+	// P
 	'passe_complexe_description' => 'Ce plugin ajoute un testeur de complexité dans les formulaires de choix de mot de passe de SPIP.',
 	'passe_complexe_slogan' => 'Vérifier la complexité des mots de passe'
 );
