7c985b4b08f2afdc41f26c8bd9a9125e60b51239
hanjo.spip@gmail.com <hanjo.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-07-02 03:40:15 +0000

    [Salvatore] [source:_plugins_/jeux/trunk/lang/ jeux] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@110958 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ jeux/trunk/lang/jeux_nl.php
@@ -0,0 +1,127 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/jeux?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'afficher_solution' => 'Toon de oplossing',
+	'alphabet' => 'ABCDEFGHIJKLMNOPQRSTUVWXYZ-',
+	'aucune_erreur' => 'Geen fout',
+	'auteur' => 'Auteur',
+
+	// B
+	'bonneReponse' => 'Het juiste antwoord is:',
+	'bravo' => 'Gefeliciteerd!',
+
+	// C
+	'cfg_affichage' => 'De weergave van het spel',
+	'cfg_correction' => 'De correctie van het',
+	'cfg_donner_indices' => 'Tips weergeven',
+	'cfg_donner_reponse' => 'Toon het antwoord',
+	'cfg_donner_solution' => 'Geef de juiste antwoorden',
+	'cfg_introduction' => 'Deze parameters zijn de standaard configuratie voor alle spellen van de website. Ze kunnen individueel worden overschreven via de interne configuratie van een spel (scheidingsteken [config]).<br/><br/>Sommige spellen gebruiken bepaalde parameters niet.',
+	'cfg_scores' => 'De scores van het spel',
+	'cfg_type_resultat' => 'Opname scores:',
+	'colonne_n' => 'kolom @n@',
+	'configuration_jeux' => 'Configureer de spellen',
+	'correction' => 'Correctie:',
+	'corriger' => 'Controleer',
+
+	// E
+	'envoyer' => 'Verzenden',
+	'erreur_doublon' => 'Fout: dubbel',
+	'erreur_scripts' => 'Scripts zijn niet actief: je kunt het spel hier niet spelen!',
+	'erreur_syntaxe' => 'Syntax fout',
+	'erreur_taille_grille' => 'Fout: onjuiste afmeting',
+
+	// G
+	'grille_invalide' => 'Het raster is ongeldig',
+	'grille_valide' => 'Het raster is geldig',
+
+	// I
+	'indices' => 'Tips',
+
+	// J
+	'jeu_type' => 'Type',
+	'jeu_vide' => 'LEEG',
+	'jeux' => 'Spellen',
+
+	// L
+	'ligne_n' => 'rij @n@',
+
+	// M
+	'modules_dispos' => 'Beschikbare add-ons',
+	'multi_jeux' => 'Multi Games',
+
+	// N
+	'n_erreurs' => '@n@ fout(en)',
+	'n_vides' => '@n@ lege cellen',
+	'nouveau_jeu' => 'Nieuw spel',
+
+	// O
+	'options' => 'Opties',
+
+	// P
+	'point' => ' pnt',
+	'points' => ' pnt',
+
+	// R
+	'recommencer' => 'Opnieuw spelen',
+	'reinitialiser' => 'Resetten',
+	'rejouer' => 'Herspelen',
+	'reponse' => 'Antwoord',
+	'reponseFausse' => 'Het antwoord is ONJUIST!',
+	'reponseJuste' => 'Dit antwoord is JUIST!',
+	'reponseNulle' => 'Je gaf GEEN ANTWOORD op deze vraag!',
+	'resultat' => 'Resultaat',
+	'resultat2_' => 'Niet bepaald?',
+	'resultat2_aucun' => 'Geen registratie',
+	'resultat2_defaut' => 'Afhankelijk van de algemene configuratie van de plugin',
+	'resultat2_dernier' => 'Per auteur, alleen het laatste resultaat',
+	'resultat2_meilleur' => 'Per auteur, alleen het beste resultaat',
+	'resultat2_meilleurs' => 'Alle beste resultaten',
+	'resultat2_premier' => 'Per auteur, alleen het eerste resultaat',
+	'resultat2_tous' => 'Alle resultaten',
+	'resultat_aucun' => 'Geen enkele',
+	'resultat_defaut' => 'Standaard',
+	'resultat_dernier' => 'De laatste',
+	'resultat_meilleur' => 'De beste',
+	'resultat_meilleurs' => 'Alle beste',
+	'resultat_premier' => 'De eerste',
+	'resultat_tous' => 'Alle',
+	'resultats' => 'Resultaten',
+	'resultats_auteur' => 'Resultaten van @nom@',
+	'resultats_jeu' => 'De resultaten van het spel @id@ (@titre@)',
+	'resultats_jeux' => 'Spelresultaten',
+
+	// S
+	'sans_titre_prive' => 'Geen privé titel',
+	'sans_type' => 'Zonder type',
+	'score' => 'Score:',
+	'solution' => 'Oplossing',
+
+	// T
+	'titre_page_configurer_jeux' => 'Comfiguratie van de spellen plugin',
+	'total' => 'Totaal',
+	'type_resultat' => 'Resultaattype',
+
+	// U
+	'une_erreur' => 'Een fout',
+	'une_vide' => 'Een lege cel',
+
+	// V
+	'valider' => 'Valideren',
+	'verifier' => 'Controleer',
+	'verifier_validite' => 'Controleer de geldigheid ',
+	'voir_auteur' => 'Bekijk auteur',
+	'voir_jeu' => 'Bekijk spel',
+	'voir_resultats' => 'Bekijk resultaten',
+	'votre_choix' => 'Jouw keuze: ',
+	'votre_reponse' => 'Jouw antwoord: '
+);
