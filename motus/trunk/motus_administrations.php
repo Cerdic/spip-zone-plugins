3239dbefac38fed2dc1ecd982a8894a3168c5d46
nicod@lerebooteux.fr <nicod@lerebooteux.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-07-04 14:58:21 +0000

    Le champ rubrique_on est un peu court quand on lui met beaucoup de rubriques
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@111008 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- motus/trunk/motus_administrations.php
+++ motus/trunk/motus_administrations.php
@@ -21,6 +21,7 @@ include_spip('base/motus');
 function motus_upgrade($nom_meta_base_version,$version_cible){
 	$maj = array();
 	cextras_api_upgrade(motus_declarer_champs_extras(), $maj['create']);
+	$maj['1.0.1'] = array(array('motus_update_rubrique_on'));
 
 	include_spip('base/upgrade');
 	maj_plugin($nom_meta_base_version, $version_cible, $maj);
@@ -38,3 +39,9 @@ function motus_vider_tables($nom_meta_base_version) {
 	effacer_meta($nom_meta_base_version);
 }
 
+/**
+ * Mise à jour du type du champ rubrique_on de varchar(255) à text
+ */
+function motus_update_rubrique_on(){
+	sql_alter("TABLE spip_groupes_mots CHANGE rubriques_on rubriques_on text DEFAULT '' NOT NULL");
+}
