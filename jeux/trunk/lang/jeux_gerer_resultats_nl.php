27b8be0446dafdd52ecf985f48be73978f45bc95
hanjo.spip@gmail.com <hanjo.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-07-04 03:39:14 +0000

    [Salvatore] [source:_plugins_/jeux/trunk/lang/ jeux_gerer_resultats] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@110986 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ jeux/trunk/lang/jeux_gerer_resultats_nl.php
@@ -0,0 +1,27 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/jeux_gerer_resultats?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'compacter' => 'Resultaten samenvoegen',
+	'confirmer_choix' => 'Je moet je keuze bevestigen',
+
+	// P
+	'pour_auteur' => 'Voor deze auteur:',
+	'pour_jeu' => 'Voor dit spel:',
+	'pour_tous' => 'Voor alle resultaten:',
+
+	// R
+	'resultats_compactes' => 'Resultaten samengevoegd!',
+	'resultats_supprimes' => 'Resultaten verwijderd!',
+
+	// S
+	'supprimer' => 'Resultaten verwijderen'
+);
