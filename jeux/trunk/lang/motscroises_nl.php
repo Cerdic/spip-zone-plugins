390c1ebf0bbb8c8a5e1c01729bbb519b9bcae4c2
hanjo.spip@gmail.com <hanjo.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-07-04 03:39:16 +0000

    [Salvatore] [source:_plugins_/jeux/trunk/lang/ motscroises] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@110987 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ jeux/trunk/lang/motscroises_nl.php
@@ -0,0 +1,22 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/motscroises?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// H
+	'horizontalement' => 'Horizontaal:',
+
+	// T
+	'table_summary' => 'Kruiswoordraadsel met @largeur@ kolommen en @hauteur@ rijen',
+	'titre' => 'Kruiswoordraadsel',
+	'titre_court' => 'Kruiswoord',
+
+	// V
+	'verticalement' => 'Verticaal:'
+);
