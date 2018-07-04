53e218f8d54aaaa550f802c2355882f3f76eacec
hanjo.spip@gmail.com <hanjo.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-07-04 03:39:24 +0000

    [Salvatore] [source:_plugins_/jeux/trunk/lang/ sudoku] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@110991 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ jeux/trunk/lang/sudoku_nl.php
@@ -0,0 +1,19 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/sudoku?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// R
+	'regle' => 'Spelregel: vul het raster met cijfers op een zodanige manier dat iedere kolom en rij (van @largeur@ bij @hauteur@) slechts eenmaal de cijfers van 1 tot @max@ bevat.',
+
+	// T
+	'table_summary' => 'Sudoku puzzel met @largeur@ kolommen en @hauteur@ rijen',
+	'titre' => 'Sudoku puzzel',
+	'titre_court' => 'Sudoku'
+);
