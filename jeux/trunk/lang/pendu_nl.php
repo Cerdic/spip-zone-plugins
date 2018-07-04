ca19158f94bcd987321d5ad313cb9fd9e7c26ee8
hanjo.spip@gmail.com <hanjo.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-07-04 03:39:20 +0000

    [Salvatore] [source:_plugins_/jeux/trunk/lang/ pendu] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@110989 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ jeux/trunk/lang/pendu_nl.php
@@ -0,0 +1,26 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/pendu?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cliquez' => 'Klik op de letter die je wilt kiezen:',
+
+	// F
+	'fini' => 'Je hangt!\\n\\nHet gezochte woord: ',
+
+	// R
+	'regle' => 'Spelregels: Vind het woord.
+_ Kies steeds een letter. Staat de letter in het woord, dan verschijnt deze op de juiste plaats.
+_ Je hoofd, een arm, een been... Na zes foute letters hang je!',
+
+	// T
+	'titre' => 'Galgje',
+	'titre_court' => 'Galgje'
+);
