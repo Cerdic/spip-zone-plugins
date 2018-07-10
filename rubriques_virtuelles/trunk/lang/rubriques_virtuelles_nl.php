b7bbc077779cd427a4a88eb4e4b636eea1709756
hanjo.spip@gmail.com <hanjo.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-07-10 03:39:12 +0000

    [Salvatore] [source:_plugins_/rubriques_virtuelles/trunk/lang/ rubriques_virtuelles] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@111055 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ rubriques_virtuelles/trunk/lang/rubriques_virtuelles_nl.php
@@ -0,0 +1,18 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/rubriques_virtuelles?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// I
+	'info_renvoi_rubrique' => 'Deze rubriek verwijst door naar pagina:',
+
+	// T
+	'texte_rubrique_reference_mais_redirige' => 'rubriek die in je site wordt weergegeven, maar doorverwijst naar een andere URL.',
+	'texte_rubrique_virtuel' => 'Virtuele Rubriek'
+);
