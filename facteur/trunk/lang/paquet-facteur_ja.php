f6f63163155db296be7fe976a8be69bc1e174c95
origenius@mvb.biglobe.ne.jp <origenius@mvb.biglobe.ne.jp@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-07-11 03:38:49 +0000

    [Salvatore] [source:_plugins_/facteur/trunk/lang/ paquet-facteur] Export depuis http://trad.spip.net de la langue ja
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@111087 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ facteur/trunk/lang/paquet-facteur_ja.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-facteur?lang_cible=ja
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// F
+	'facteur_nom' => '配達人',
+	'facteur_slogan' => 'メールを配達してくださる。'
+);
