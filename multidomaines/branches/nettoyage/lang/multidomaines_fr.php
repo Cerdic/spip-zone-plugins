69d6e8e5c75bcd15799c7bdb8f806bee43ad3af0
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-07-15 21:02:21 +0000

    On fait une branche expe, pour essayer d'y voir plus clair. Et en premier on déplace la config plus proprement avec une case par id_rubrique, pas mélangé dans le nom des champs genre truc_123. Pour le défaut, on a une clé defaut. Et donc on modifie le code pour appeler ce nouveau rangement.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@111135 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ multidomaines/branches/nettoyage/lang/multidomaines_fr.php
@@ -0,0 +1,30 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+//C
+	'configurations' => 'Ce formulaire va vous permettre de configurer le site de façon générale. ',
+
+//E
+	'explications' => 'Le plugin va lire la configuration de la façon suivante : <br />
+	- pour trouver le domaine du secteur : Url du secteur puis url par defaut <br />
+	- pour trouver le squelette du secteur : Chercher le dossier suivant le schéma (www.domaine.fr puis domaine.fr) puis dossiers des squelettes du secteur. <br />
+	Un domaine peut avoir plusieurs dossiers squelettes, il faut les séparer par ":" (ex:squelettes:squelettes/sous-domaine.domaine.com)',
+
+//L
+	'label_editer_url' => 'Url par defaut',
+	'label_editer_url_rubrique' => 'Url de la rubrique',
+	'label_editer_url_secteur' => 'Url du secteur',
+	'label_squelette' => 'Dossier des secteurs',
+	'label_squelette_rubrique' => 'Dossiers des squelettes de la rubrique',
+	'label_squelette_secteur' => 'Dossiers des squelettes du secteur',
+
+//T
+	'titre_multidomaines' => 'Multidomaines',
+
+);
+?>
