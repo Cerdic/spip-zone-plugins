d468029a29eef759f63688dfd4d97fbd881f84e8
tcharlss@bravecassine.com <tcharlss@bravecassine.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-07-17 10:14:09 +0000

    Plan des chapitres : quand on dépliait un noeud, les liens à l'intérieur n'étaient pas cliquables -> relancer la fonction. Il reste un bug : quand on sélectionne programmatiquement un noeud (le chapitre en cours), la lib est censée ouvrir les parents mais ça ne fonctionne pas, cf. https://github.com/vakata/jstree/issues/1623.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@111153 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- chapitres/trunk/prive/javascript/chapitres_plan.js
+++ chapitres/trunk/prive/javascript/chapitres_plan.js
@@ -1,13 +1,13 @@
 /**
  * Chapitres : gestion du plan
  *
- * Mettre ne jstree
+ * Mettre en jstree
  * Mettre en sticky
  */
 jQuery(function($){
 
 	var plan = '#chapitres-plan';
-	var plan_parent = '.chapitres-plan';
+	var plan_conteneur = '.chapitres-plan';
 
 	// Initialiser jstree
 	$(plan).jstree({
@@ -22,26 +22,20 @@ jQuery(function($){
 	// Des choses après le chargement de jstree
 	$(plan).bind('ready.jstree', function(e, data) {
 
-		// Déplier ceux à la racine
-		$(this).jstree('open_node', $(plan + ' > ul > li'));
-		// Déplier le chapitre en cours (toute la branche si vue complète)
+		// Déplier le chapitre ou la branche en cours
 		if ($(this).hasClass('editables')) {
 			$(this).jstree('open_all', $(plan + ' li.expose'));
 		} else {
 			$(this).jstree('open_node', $(plan + ' li.expose'));
 		}
-		// Sélectionner le chapitre en cours
-		$(this).jstree('select_node', $(plan + ' li.expose'));
+		// Sélectionner le chapitre en cours (censé ouvrir les parents mais ne fonctionne pas)
+		$(this).jstree('select_node', $(plan + ' li.expose'), true, false);
+
 		// Liens cliquables
-		$(plan + ' li a').click(function(e) {
-			var href = $(this).attr('href');
-			if (href !== '#') {
-				document.location.href = href;
-			}
-		});
+		liens_cliquables();
 
 		// Boutons de pliage
-		$(plan_parent + ' .pliage a').click(function(e) {
+		$(plan_conteneur + ' .pliage a').click(function(e) {
 			if ($(this).data('pliage') == 'deplier') {
 				$(plan).jstree('open_all');
 			} else {
@@ -51,17 +45,30 @@ jQuery(function($){
 		});
 
 		// Sticky
-		$(plan_parent).stick_in_parent({
+		$(plan_conteneur).stick_in_parent({
 			parent: '#navigation'
 		});
 		// Recalculer le sticky avec le pliage
 		$(plan).on('open_all.jstree', function(){
-			$(plan_parent).trigger('sticky_kit:recalc');
+			$(plan_conteneur).trigger('sticky_kit:recalc');
 		});
 		$(plan).on('close_all.jstree', function(){
-			$(plan_parent).trigger('sticky_kit:recalc');
+			$(plan_conteneur).trigger('sticky_kit:recalc');
 		});
 
 	});
+
+	// Liens cliquables lors des changements
+	$(plan).bind('open_node.jstree open_all.jstree view_all', function(e, data) {
+		liens_cliquables();
+	});
+	function liens_cliquables(){
+		$(plan + ' li a').click(function(e) {
+			var href = $(this).attr('href');
+			if (href !== '#') {
+				document.location.href = href;
+			}
+		});
+	}
 
 });
