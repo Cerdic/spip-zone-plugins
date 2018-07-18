2ce333ec826dec2185dd53789afedd0fb0c78e02
tcharlss@bravecassine.com <tcharlss@bravecassine.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-07-18 16:05:10 +0000

    Précisons que l'identifiant est optionnel pour éviter les confusions
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@111174 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- multidomaines/branches/nettoyage/lang/multidomaines_fr.php
+++ multidomaines/branches/nettoyage/lang/multidomaines_fr.php
@@ -14,7 +14,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	- pour trouver le domaine du secteur : Url du secteur puis url par defaut <br />
 	- pour trouver le squelette du secteur : Chercher le dossier suivant le schéma (www.domaine.fr puis domaine.fr) puis dossiers des squelettes du secteur. <br />
 	Un domaine peut avoir plusieurs dossiers squelettes, il faut les séparer par ":" (ex:squelettes:squelettes/sous-domaine.domaine.com)',
-	'explication_identifiant' => 'Nom informatique : charactères alphanumériques ou «_»',
+	'explication_identifiant' => 'Nom informatique optionnel pour identifier le secteur : charactères alphanumériques ou «_»',
 
 	// I
 	'info_domaine_non' => 'Ce domaine n\'est pas configuré',
