b761e7b5078bc8d3b630b8b814463bb9a8a5866e
chankalan@choc0.net <chankalan@choc0.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-07-27 00:20:23 +0000

    la v3 de mediaboxavectexte pour SPIP 3.2.1
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@111210 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ mediaboxavectexte/trunk/lang/mediaboxavectexte_fr.php
@@ -0,0 +1,9 @@
+<?php
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// M
+	'masquer_afficher' => 'masquer/afficher le texte',
+);
