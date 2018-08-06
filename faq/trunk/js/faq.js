a406ecda013e7d6c18ad9bc2e65bf8fc36d197f7
tcharlss@bravecassine.com <tcharlss@bravecassine.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-08-06 14:47:47 +0000

    Relancer le script en cas de rechargement ajax
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@111272 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- faq/trunk/js/faq.js
+++ faq/trunk/js/faq.js
@@ -1,6 +1,16 @@
 jQuery(function($){
-	$('dl.faq > dt').addClass("close").click(function(){
-		$(this).toggleClass("close").next().toggle('fast');
-		return false;
-	}).next().hide();
+	function init_faq() {
+		$('dl.faq > dt').addClass("close").click(function(){
+			$(this).toggleClass("close").next().toggle('fast');
+			return false;
+		}).next().hide();
+	}
+	init_faq();
+	// Relancer lors de rechargements ajax
+	if (window.jQuery){
+		$(function(){
+			onAjaxLoad(init_faq);
+		});
+	}
+	
 });
