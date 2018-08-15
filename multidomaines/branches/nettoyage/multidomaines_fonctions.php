dc59699d92c05b56cd9cc2a663e7134e7d7de566
tcharlss@bravecassine.com <tcharlss@bravecassine.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-08-15 16:13:48 +0000

    Notice php suite (commité trop vite :/)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@111326 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- multidomaines/branches/nettoyage/multidomaines_fonctions.php
+++ multidomaines/branches/nettoyage/multidomaines_fonctions.php
@@ -27,16 +27,16 @@ function calculer_URL_SECTEUR($id_rubrique) {
 	// remonter les rubriques jusqu'à trouver une url multidomaine
 	// attention lire_config() renvoie un tableau complet si on demande une clé qui n'existe pas ($id_rubrique)
 	include_spip('inc/config');
-	if ($id_rubrique) {
-		$url = lire_config("multidomaines/$id_rubrique/url");
-	} else {
-		$url = null;
-	}
-	$id_rubrique_courante = $id_rubrique;
-	while (!$url && $id_rubrique_courante) {
-		$id_parent = sql_getfetsel("id_parent", "spip_rubriques", "id_rubrique=" . intval($id_rubrique_courante));
-		$url = lire_config("multidomaines/$id_parent/url");
-		$id_rubrique_courante = $id_parent;
+	if (
+		$url = lire_config("multidomaines/$id_rubrique/url")
+		and is_string($url)
+	) {
+		$id_rubrique_courante = $id_rubrique;
+		while (!$url && $id_rubrique_courante) {
+			$id_parent = sql_getfetsel("id_parent", "spip_rubriques", "id_rubrique=" . intval($id_rubrique_courante));
+			$url = lire_config("multidomaines/$id_parent/url");
+			$id_rubrique_courante = $id_parent;
+		}
 	}
 
 	// sinon, url par défaut
@@ -44,20 +44,6 @@ function calculer_URL_SECTEUR($id_rubrique) {
 		$url = lire_config('multidomaines/defaut/url');
 	}
 	if (empty($url)) {
-		$url = lire_config('adresse_site');
-	}
-
-	// Rustine
-	// [FIXME] des fois on se retrouve avec le contenu de lire_config('multidomaines');
-	if (
-		is_array($url)
-		and !empty($url['defaut']['url'])
-	) {
-		$url = $url['defaut']['url'];
-	} elseif (
-		is_array($url)
-		and empty($url['defaut']['url'])
-	) {
 		$url = lire_config('adresse_site');
 	}
 
