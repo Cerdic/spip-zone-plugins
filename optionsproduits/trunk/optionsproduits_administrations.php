8bb0cd1acefd90c694ed612ece70fa7e835e66c5
nicod@lerebooteux.fr <nicod@lerebooteux.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-08-17 15:43:39 +0000

    Gérer les dépendances et configurer Rang à l'installation.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@111348 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- optionsproduits/trunk/optionsproduits_administrations.php
+++ optionsproduits/trunk/optionsproduits_administrations.php
@@ -38,6 +38,13 @@ function optionsproduits_upgrade($nom_meta_base_version, $version_cible) {
 		// recréer la clé du panier avec les options
 		array('sql_alter', 'TABLE `spip_paniers_liens` DROP PRIMARY KEY'),
 		array('sql_alter', 'TABLE `spip_paniers_liens` ADD PRIMARY KEY (`id_panier`, `id_objet`, `objet`, `options`)',),
+
+		// ajouter les options et groupes à la config des objets géré par Rang
+		array('optionsproduits_configure_rang'),
+	);
+
+	$maj['1.0.1'] = array(
+		array('optionsproduits_configure_rang'),
 	);
 
 	include_spip('base/upgrade');
@@ -58,9 +65,34 @@ function optionsproduits_vider_tables($nom_meta_base_version) {
 	sql_drop_table('spip_options_liens');
 	sql_drop_table('spip_optionsgroupes');
 
-	# Nettoyer les versionnages et forums
+	// Nettoyer les versionnages et forums
 	sql_delete('spip_versions', sql_in('objet', array('option')));
 	sql_delete('spip_versions_fragments', sql_in('objet', array('option')));
 
 	effacer_meta($nom_meta_base_version);
+
+	// Retirer les options et groupes d'options dans la liste des objets géré par le plugin Rang
+	$tables         = lire_config('rang/rang_objets');
+	$tables         = explode(',', $tables);
+	unset($tables['spip_options']);
+	unset($tables['spip_optionsgroupes']);
+	ecrire_config('rang/rang_objets', implode(',', $tables));
 }
+
+/**
+ * Ajouter les options et groupes d'options dans la liste des objets géré par le plugin Rang
+ *
+ * @return void
+ **/
+function optionsproduits_configure_rang() {
+	$tables         = lire_config('rang/rang_objets');
+	$tables         = explode(',', $tables);
+	$tables_options = array(
+		'spip_options',
+		'spip_optionsgroupes',
+	);
+	$tables         = array_unique(array_merge($tables, $tables_options));
+	ecrire_config('rang/rang_objets', implode(',', $tables));
+	// créer les champs 'rang' dans les tables
+	rang_creer_champs($tables_options);
+} 
