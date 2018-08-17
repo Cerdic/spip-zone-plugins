ed9e47e2a584c1709954077f277c4032e7a25312
abelass <abelass@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-08-17 12:36:07 +0000

    éviter erreur quand pas de champs extras présents
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@111340 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- reservations_multiples/trunk/squelettes/inclure/inc-reservation_connection_extras_fonctions.php
+++ reservations_multiples/trunk/squelettes/inclure/inc-reservation_connection_extras_fonctions.php
@@ -7,13 +7,14 @@
  * @param integer $nombre
  * @return array
  */
-function champs_extras_multiples(array $champs_extras, $nombre) {
-	// Adapter les champs extras
-	foreach ($champs_extras as $key => $value) {
-		$nom_champ = $value['options']['nom'] . '_' . $nombre;
-		set_request($nom_champ, '');#)
-		$champs_extras[$key]['options']['nom'] = $nom_champ;
+function champs_extras_multiples($champs_extras, $nombre) {
+	if (is_array($champs_extras)) {
+		// Adapter les champs extras
+		foreach ($champs_extras as $key => $value) {
+			$nom_champ = $value['options']['nom'] . '_' . $nombre;
+			set_request($nom_champ, '');#)
+			$champs_extras[$key]['options']['nom'] = $nom_champ;
+		}
 	}
-
 	return $champs_extras;
 }
