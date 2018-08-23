580a85d87da19d7b8ccf04c284495fd42fe2152a
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-08-23 19:11:11 +0000

    Mise à jour de la démo qui permet de voir pour cahque code spip quelle norme est utilisée pour son code.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@111408 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- isocode/trunk/demo/isocode_fonctions.php
+++ isocode/trunk/demo/isocode_fonctions.php
@@ -12,7 +12,7 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 /**
  * @return array
  */
-function isocode_verifier_codes_spip() {
+function isocode_erreur_codes_spip() {
 
 	$codes_verifies = array();
 
@@ -25,7 +25,7 @@ function isocode_verifier_codes_spip() {
 				// Si le code a une taille de 2 caractères on recherche de suite dans la table iso639codes
 				// un élément dont le code ISO639-1 est égal.
 				$where = array('code_639_1=' . sql_quote($_code));
-				$codes_iso = sql_allfetsel($select, $from, $where);
+				$codes_iso = sql_fetsel($select, $from, $where);
 				if ($codes_iso) {
 					$codes_verifies['ok']['iso6391'][$_code] = $codes_iso;
 				} else {
@@ -56,6 +56,59 @@ function isocode_verifier_codes_spip() {
 
 	return $codes_verifies;
 }
+
+/**
+ * @return array
+ */
+function isocode_verifier_codes_spip() {
+
+	$codes_verifies = array();
+
+	include_spip('inc/lang_liste');
+	if ($GLOBALS['codes_langues']) {
+		foreach ($GLOBALS['codes_langues'] as $_code => $_nom) {
+			$from = array('spip_iso639codes');
+			$select = array('*');
+			$default = array('nom_spip' => $_nom, 'type_code' => '', 'erreur' => false);
+			if (strlen($_code) == 2) {
+				// Si le code a une taille de 2 caractères on recherche de suite dans la table iso639codes
+				// un élément dont le code ISO639-1 est égal.
+				$where = array('code_639_1=' . sql_quote($_code));
+				$codes_iso = sql_fetsel($select, $from, $where);
+				if ($codes_iso) {
+					$codes_verifies[$_code] = array_merge($default, $codes_iso);
+					$codes_verifies[$_code]['type_code'] = '639-1';
+				} else {
+					$codes_verifies[$_code]['erreur'] = true;
+				}
+			} elseif (strlen($_code) == 3) {
+				// Si le code a une taille de 3 caractères on recherche :
+				// - dans la table iso639codes un élément dont le code ISO639-3 est égal.
+				// - et sinon dans la table iso639families un élément dont le code ISO639-5 est égal.
+				$where = array('code_639_3=' . sql_quote($_code));
+				$codes_iso = sql_fetsel($select, $from, $where);
+				if ($codes_iso) {
+					$codes_verifies[$_code] = array_merge($default, $codes_iso);
+					$codes_verifies[$_code]['type_code'] = '639-3';
+				} else {
+					$where = array('code_639_5=' . sql_quote($_code));
+					$code_famille = sql_fetsel($select, array('spip_iso639families'), $where);
+					if ($code_famille) {
+						$codes_verifies[$_code] = array_merge($default, $code_famille);
+						$codes_verifies[$_code]['type_code'] = '639-5';
+					} else {
+						$codes_verifies[$_code]['erreur'] = true;
+					}
+				}
+			} else {
+				$codes_verifies[$_code]['erreur'] = true;
+			}
+		}
+	}
+
+	return $codes_verifies;
+}
+
 
 /**
  * @return array
