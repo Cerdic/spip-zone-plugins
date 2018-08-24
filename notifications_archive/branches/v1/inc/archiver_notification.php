bafd414ffe61b4eb77eb9d00e31115dd5b2b0768
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-08-24 15:36:52 +0000

    v1.1.10 - corriger url breadcrumb - mettre la déclaration de la pipeline dans le paquet.xml - enlever les fichiers inutilisés
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@111423 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- notifications_archive/branches/v1/inc/archiver_notification.php
+++ notifications_archive/branches/v1/inc/archiver_notification.php
@@ -8,20 +8,19 @@
  * @licence    GNU/GPL
  * @package    SPIP\Notifications_archive\Inc
  */
+if (!defined('_ECRIRE_INC_VERSION'))
+	return;
 
-if (!defined('_ECRIRE_INC_VERSION')) return;
-
-function inc_archiver_notification_dist($o){
+function inc_archiver_notification_dist($o) {
 	include_spip('inc/config');
-	$config=lire_config('notifications_archive',array());
-	if(isset($config[$o['type']]['activer']) AND $config[$o['type']]['activer']=='on'){
-		include_spip('inc/editer');    	
-		foreach($o AS $champ=>$valeur){
-			if(is_array($valeur))$valeur=implode(',',$valeur);
-			set_request($champ,$valeur);
+	$config = lire_config('notifications_archive', array());
+	if (isset($config[$o['type']]['activer']) and $config[$o['type']]['activer'] == 'on') {
+		include_spip('inc/editer');
+		foreach ($o as $champ => $valeur) {
+			if (is_array($valeur))
+				$valeur = implode(',', $valeur);
+			set_request($champ, $valeur);
 		}
-		formulaires_editer_objet_traiter('notification','new');
-	}   
+		formulaires_editer_objet_traiter('notification', 'new');
+	}
 }
-
-?>
