d49930ddad3f4f0c50241e4a21acaa06824cfd82
Maïeul Rouquette <Maeul Rouquette@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-08-24 16:56:06 +0000

    pas besoin de deux points sur un label, les 2 points sont sous entendus par le fait même qu'il s'agisse d'un label
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@111424 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- moderation_moderee/trunk/lang/moderation_fr.php
+++ moderation_moderee/trunk/lang/moderation_fr.php
@@ -7,9 +7,9 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	//C
 	"titre" => "Mod&eacute;ration Mod&eacute;r&eacute;e",
 	"config_true" => "<div style='text-align: center;'>Ce plugin vous permet de mod&eacute;rer la mod&eacute;ration des forums publics.</div>",
-	"0minirezo_config" => "Activer la Mod&eacute;ration Mod&eacute;r&eacute;e pour les administrateurs enregistr&eacute;s :",
-	"1comite_config" => "Activer la Mod&eacute;ration Mod&eacute;r&eacute;e pour les r&eacute;dacteurs enregistr&eacute;s :",
-	"6forum_config" => "Activer la Mod&eacute;ration Mod&eacute;r&eacute;e pour les visiteurs enregistr&eacute;s :",
+	"0minirezo_config" => "Activer la Mod&eacute;ration Mod&eacute;r&eacute;e pour les administrateurs enregistr&eacute;s",
+	"1comite_config" => "Activer la Mod&eacute;ration Mod&eacute;r&eacute;e pour les r&eacute;dacteurs enregistr&eacute;s",
+	"6forum_config" => "Activer la Mod&eacute;ration Mod&eacute;r&eacute;e pour les visiteurs enregistr&eacute;s",
 
 	);
 ?>
