79aa5f3ea2df2078d991810b8acf9ec02a5468a9
nicod@lerebooteux.fr <nicod@lerebooteux.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-08-29 16:25:56 +0000

    Intercepter le submit pour lancer la mise à jour en ajax Principalement pour la recherche par mot clé : on saisit, on appuie sur entrée, et ça va vite :)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@111461 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- ajaxfiltre/trunk/prive/javascript/ajaxfiltre_prive.js
+++ ajaxfiltre/trunk/prive/javascript/ajaxfiltre_prive.js
@@ -9,39 +9,45 @@
 			base.options = $.extend({}, $.ajaxFiltre.defaultOptions, options);
 
 			base.$el.find('select, input[type=checkbox], input[type=radio]').on('change', function() {
-				
-				var formData = base.$el.serializeArray().reduce(function(obj, item) {
-					// si le name comporte des [] on construit un tableau
-					if(item.name.indexOf('[]') !== -1) {
-						var name = item.name.replace(/[\[\]]+/g, '');
-						if(!obj.hasOwnProperty(name)) {
-							obj[name] = [];
-						}
-						obj[name].push(item.value);
-					} else {
-						obj[item.name] = item.value;
-					}
-					return obj;
-				}, {});
-
-				// passer une valeur vide explicite pour les checkbox dont le name comporte des [] et dans lesquels rien n'est coché
-				var $checkradio = base.$el.find('input[type=checkbox][name*="[]"]');
-				$.each($checkradio,function(){
-					var checkRadioName = $(this).attr('name');
-					// si rien n'est coché
-					if(!base.$el.find('input[name="'+checkRadioName+'"]:checked').length){
-						// supprimer les données du nom avec []
-						delete formData[checkRadioName];
-						// ajouter un tableau vide sur le nom sans [] 
-						formData[checkRadioName.replace(/[\[\]]+/g, '')] = [];
-					}
-				});
-				
-				console.log(formData);
-				
-				ajaxReload(base.options.ajaxTarget, {args: formData});
+				base.query();
 			});
 
+			base.$el.on('submit', function(e){
+				base.query();
+				e.preventDefault();
+			});
+		};
+
+		base.query = function() {
+			var formData = base.$el.serializeArray().reduce(function(obj, item) {
+				// si le name comporte des [] on construit un tableau
+				if(item.name.indexOf('[]') !== -1) {
+					var name = item.name.replace(/[\[\]]+/g, '');
+					if(!obj.hasOwnProperty(name)) {
+						obj[name] = [];
+					}
+					obj[name].push(item.value);
+				} else {
+					obj[item.name] = item.value;
+				}
+				return obj;
+			}, {});
+
+			// passer une valeur vide explicite pour les checkbox dont le name comporte des [] et dans lesquels rien n'est coché
+			var $checkradio = base.$el.find('input[type=checkbox][name*="[]"]');
+			$.each($checkradio,function(){
+				var checkRadioName = $(this).attr('name');
+				// si rien n'est coché
+				if(!base.$el.find('input[name="'+checkRadioName+'"]:checked').length){
+					// supprimer les données du nom avec []
+					delete formData[checkRadioName];
+					// ajouter un tableau vide sur le nom sans [] 
+					formData[checkRadioName.replace(/[\[\]]+/g, '')] = [];
+				}
+			});
+
+			// recharger la liste d'objets
+			ajaxReload(base.options.ajaxTarget, {args: formData});
 		};
 
 		base.init();
@@ -60,7 +66,5 @@
 })(jQuery);
 
 $(function() {
-
-	$('.formulaire_navigation_filtre form').ajaxFiltre({ajaxTarget: 'liste-objets'});
-
+	$('.formulaire_navigation_filtre form').ajaxFiltre();
 });
