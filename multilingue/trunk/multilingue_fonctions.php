7f412f1dd689e8f02291ccd75977a52116acaddd
abelass <abelass@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-09-20 13:42:07 +0000

    Enlever l'inclusiion automatique du logo original pour Spip 3.2, merci brian
    Spip 3.2 le copie lords de la traduction.
    Cette fonction provoque alors l'effacement du logo original
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@111636 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- multilingue/trunk/multilingue_fonctions.php
+++ multilingue/trunk/multilingue_fonctions.php
@@ -2,37 +2,32 @@
 if (!defined('_ECRIRE_INC_VERSION'))
 	return;
 
-include_spip('inc/config');
+include_spip('plugins/installer');
 
-if (!lire_config('multilingue/desactiver_traduction_logo_objet'))
-	$traduire_logo = 'ok';
-if (($traduire_logo == 'ok') and (!test_plugin_actif('logos_roles'))) {
-	// surcharge de https://code.spip.net/@inc_chercher_logo_dist pour ajouter
-	function inc_chercher_logo($id, $_id_objet, $mode = 'on') {
-		include_spip('inc/chercher_logo');
-		global $formats_logos;
-		// attention au cas $id = '0' pour LOGO_SITE_SPIP : utiliser intval()
+// Spip 3.2 ajoute le logo original à sa traduction, du coup élimine l'original avec cette fonction.
+if (spip_version_compare($GLOBALS['spip_version_affichee'], '3.2.0', '<')) {
+	include_spip('inc/config');
 
-		$type = type_du_logo($_id_objet);
+	if (!lire_config('multilingue/desactiver_traduction_logo_objet')) {
+		$traduire_logo = 'ok';
+	}
 
-		$objet = str_replace('id_', '', $_id_objet);
-		$tables = lister_tables_objets_sql();
-		$table = 'spip_' . $objet . 's';
+	if (($traduire_logo == 'ok') and (!test_plugin_actif('logos_roles'))) {
 
-		foreach ($formats_logos as $format) {
-			$nom = $type . $mode . intval($id);
-			if (@file_exists($d = (_DIR_LOGOS . $nom . '.' . $format))) {
-				return array(
-					$d,
-					_DIR_LOGOS,
-					$nom,
-					$format,
-					@filemtime($d)
-				);
-			}
-			// si pas de logo on cherche si l'article d'origine en a un
-			elseif ($tables[$table]['field']['id_trad'] and $id_trad = sql_getfetsel('id_trad', $table, $_id_objet . '=' . intval($id)) and _request('exec') != $objet) {
-				$nom = $type . $mode . intval($id_trad);
+		// surcharge de https://code.spip.net/@inc_chercher_logo_dist pour ajouter
+		function inc_chercher_logo($id, $_id_objet, $mode = 'on') {
+			include_spip('inc/chercher_logo');
+			global $formats_logos;
+			// attention au cas $id = '0' pour LOGO_SITE_SPIP : utiliser intval()
+
+			$type = type_du_logo($_id_objet);
+
+			$objet = str_replace('id_', '', $_id_objet);
+			$tables = lister_tables_objets_sql();
+			$table = 'spip_' . $objet . 's';
+
+			foreach ($formats_logos as $format) {
+				$nom = $type . $mode . intval($id);
 				if (@file_exists($d = (_DIR_LOGOS . $nom . '.' . $format))) {
 					return array(
 						$d,
@@ -42,10 +37,23 @@ if (($traduire_logo == 'ok') and (!test_plugin_actif('logos_roles'))) {
 						@filemtime($d)
 					);
 				}
-				;
+				// si pas de logo on cherche si l'article d'origine en a un
+				elseif ($tables[$table]['field']['id_trad'] and $id_trad = sql_getfetsel('id_trad', $table, $_id_objet . '=' . intval($id)) and _request('exec') != $objet) {
+					$nom = $type . $mode . intval($id_trad);
+					if (@file_exists($d = (_DIR_LOGOS . $nom . '.' . $format))) {
+						return array(
+							$d,
+							_DIR_LOGOS,
+							$nom,
+							$format,
+							@filemtime($d)
+						);
+					}
+					;
+				}
 			}
+			// coherence de type pour servir comme filtre (formulaire_login)
+			return array();
 		}
-		// coherence de type pour servir comme filtre (formulaire_login)
-		return array();
 	}
 }
