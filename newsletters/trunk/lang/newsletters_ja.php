4e3cb1315ac396a73eedcf335f4abc43330dff02
origenius@mvb.biglobe.ne.jp <origenius@mvb.biglobe.ne.jp@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-09-22 03:38:34 +0000

    [Salvatore] [source:_plugins_/newsletters/trunk/lang/ newsletters] Export depuis http://trad.spip.net de la langue ja
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@111657 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ newsletters/trunk/lang/newsletters_ja.php
@@ -0,0 +1,22 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/newsletters?lang_cible=ja
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cfg_exemple' => '例',
+	'cfg_exemple_explication' => 'この例の説明',
+	'cfg_titre_parametrages' => '設定',
+
+	// N
+	'newsletters_titre' => 'ニュースレター',
+
+	// T
+	'titre_page_configurer_newsletters' => 'ニュースレターの設定'
+);
