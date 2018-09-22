04722c22bae973ee4c780b4b39e2fa040934077d
origenius@mvb.biglobe.ne.jp <origenius@mvb.biglobe.ne.jp@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-09-22 03:38:14 +0000

    [Salvatore] [source:_plugins_/ancres_douces/lang/ paquet-ancresdouces] Export depuis http://trad.spip.net de la langue ja
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@111654 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ ancres_douces/lang/paquet-ancresdouces_ja.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-ancresdouces?lang_cible=ja
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'ancresdouces_description' => 'このプラグインは、訪問者が読まれている記事のアンカーを指し示すリンクをクリックすると、ページの連続的なスクロールを可能にする。',
+	'ancresdouces_nom' => '柔和なアンカー',
+	'ancresdouces_slogan' => '柔和なアンカー'
+);
