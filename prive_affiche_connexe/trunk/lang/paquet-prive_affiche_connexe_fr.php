8edd8c466bdf86d4dca072cd7771a28f81c0cbcb
tcharlss@bravecassine.com <tcharlss@bravecassine.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-10-07 17:50:24 +0000

    Un plugin expérimental compagnon de « espace privé fluide » : il déplace les éléments ajoutés via le pipeline « affiche_milieu » dans la colonne de droite sur les pages des objets éditoriaux, et c'est tout. Il s'agit de tester une réponse à une problématique : plus on active de plugins, plus le contenu de affiche_milieu prend de la place au détriment du contenu principal. On se retrouve avec un mur de choses connexes secondaires AVANT le contenu principal. On teste donc de les déplacer de côté pour moins perturber la lecture et l'édition des objets éditoriaux. Évidemment, cela introduit pleins de détails d'affichage à régler (des choses qui dépassent, etc.). Il faut aussi que l'interface soit suffisemment large pour que ça fonctionne, c'est pourquoi « espace privé fluide » est une dépendance obligatoire. Ça reste un «proof of concept» car on est obligé de surcharger des squelettes et de faire des hacks dans les pipelines.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@111895 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ prive_affiche_connexe/trunk/lang/paquet-prive_affiche_connexe_fr.php
@@ -0,0 +1,18 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// P
+	'prive_affiche_connexe_nom' => 'Espace privé : affiche connexe',
+	'prive_affiche_connexe_slogan' => 'Sus à « affiche_milieu » !',
+	'prive_affiche_connexe_description' => 'Ce plugin expérimental est un compagnon pour « Espace privé fluide ». Il déplace les éléments ajoutés via le pipeline « affiche_milieu » dans la colonne de droite (et quelques éléments en plus : formulaire de date et des auteurs liés).
+
+	Il s\'agit en général de choses connexes au contenu principal : des objets liés, des options de configuration, etc. Cependant l\'emplacement d\'origine pose vite problème quand ce bloc est rempli par de multiples plugins : le contenu principal se trouve repoussé tout en bas.
+
+	On expérimente donc en déplaçant ces choses connexes de côté, de façon à ce qu\'ils ne perturbent pas la lecture et l\'édition du contenu principal.',
+);
