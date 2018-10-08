211e69aa9b8a1283436a830d1e6ae2d5b45cdde1
tcharlss@bravecassine.com <tcharlss@bravecassine.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-10-08 12:38:09 +0000

    Déclarer le parent (pour duplicator et cie)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@111904 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- selections_editoriales/trunk/base/selections_editoriales.php
+++ selections_editoriales/trunk/base/selections_editoriales.php
@@ -97,7 +97,8 @@ function selections_editoriales_declarer_tables_objets_sql($tables) {
 		'champs_editables'  => array('rang', 'objet', 'id_objet', 'titre', 'url', 'descriptif', 'id_selection', 'css'),
 		'champs_versionnes' => array('objet', 'id_objet', 'titre', 'url', 'descriptif', 'css'),
 		'rechercher_champs' => array('titre' => 8, 'descriptif' => 4),
-		'tables_jointures'  => array()
+		'tables_jointures'  => array(),
+		'parent' => array('type' => 'selection', 'champ' => 'id_selection'),
 	);
 
 
