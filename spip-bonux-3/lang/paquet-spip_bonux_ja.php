1ba104de2d0a8a992b44e009ae9985a6d8043146
origenius@mvb.biglobe.ne.jp <origenius@mvb.biglobe.ne.jp@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-10-10 03:41:17 +0000

    [Salvatore] [source:_plugins_/spip-bonux-3/lang/ paquet-spip_bonux] Export depuis http://trad.spip.net de la langue ja
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@111943 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ spip-bonux-3/lang/paquet-spip_bonux_ja.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-spip_bonux?lang_cible=ja
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// S
+	'spip_bonux_description' => 'SPIPにいろいろな機能（ボーナス）を追加するプラグインです。楽しみにしてください。',
+	'spip_bonux_slogan' => 'SPIPに機能（ボーナス）を追加するプラグイン'
+);
