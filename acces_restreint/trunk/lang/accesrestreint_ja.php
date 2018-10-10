fae0bde7a8111f1827f3022f7cd6289766ea7c48
origenius@mvb.biglobe.ne.jp <origenius@mvb.biglobe.ne.jp@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-10-10 03:40:20 +0000

    [Salvatore] [source:_plugins_/acces_restreint/trunk/lang/ accesrestreint] Export depuis http://trad.spip.net de la langue ja
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@111931 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ acces_restreint/trunk/lang/accesrestreint_ja.php
@@ -0,0 +1,105 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/accesrestreint?lang_cible=ja
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'ajouter_droits_auteur' => 'このゾーンを管理するために、アクセス権を私に与えること。',
+	'aucune_zone' => 'ゾーン無し',
+	'auteur' => '著者@nb@名',
+	'auteurs' => '著者@nb@名',
+	'auteurs_tous' => '接続された全ユーザー',
+
+	// B
+	'bouton_configurer_acces' => '.htaccessファイルを設定する',
+	'bouton_creer_la_zone' => '新規ゾーンを作成する',
+
+	// C
+	'colonne_id' => '#',
+	'confirmer_ajouter_auteurs' => 'この著者をゾーンに追加してよろしいですか。',
+	'confirmer_retirer_auteur_zone' => 'このエリアからこの作者を削除してもよろしいですか？',
+	'confirmer_retirer_auteurs' => 'このゾーンからすべての作者を削除してもよろしいですか？',
+	'confirmer_retirer_rubrique_zone' => 'このゾーンからこのセクションを削除してもよろしいですか？',
+	'confirmer_supprimer_zone' => 'このゾーンを削除してもよろしいですか？',
+	'creer_zone' => '新規ゾーンを作成する',
+
+	// D
+	'descriptif' => '記述',
+
+	// E
+	'explication_creer_htaccess' => 'このオプションを使用すると、関連するテキストが公開されていない場合、添付されたドキュメントを読み取ることができなくなります。',
+
+	// I
+	'icone_menu_config' => '制限付きアクセス',
+	'icone_supprimer_zone' => 'このゾーンを削除する',
+	'info_1_zone' => 'ゾーン１つ',
+	'info_acces_restreint' => 'このページへのアクセスは制限されています。 続行するにはログインしてください。',
+	'info_ajouter_auteur' => 'この著者を追加する',
+	'info_ajouter_auteurs' => 'すべての著者を追加する',
+	'info_ajouter_zones' => 'すべてのゾーンを追加する',
+	'info_aucun_acces' => 'アクセス権無し',
+	'info_aucun_auteur' => 'このゾーンには著者無し',
+	'info_aucune_zone' => 'ゾーン無し',
+	'info_auteurs_lies_zone' => 'このゾーンへのアクセス権を持つ著者',
+	'info_lien_action_proteger' => 'このセクションへのアクセスを保護する',
+	'info_nb_zones' => 'ゾーンは@nb@つ',
+	'info_page' => 'このページでは、サイトの制限されたゾーンを管理できます。',
+	'info_retirer_auteurs' => '全著者を取り外す',
+	'info_retirer_zone' => 'ゾーンから取り外す',
+	'info_retirer_zones' => 'すべてのゾーンから取り外す',
+	'info_rubrique_dans_zone' => 'このセクションは、ゾーンに属しています。',
+	'info_rubrique_dans_zones' => 'このセクションは、ゾーンに属しています。',
+
+	// L
+	'label_creer_htaccess' => 'URLによって添付されたドキュメントへのアクセス',
+	'label_creer_htaccess_non' => '読み取り許可',
+	'label_creer_htaccess_oui' => '読み取り禁止',
+	'label_droits_acces' => 'アクセス権',
+	'label_titre_zone' => '件名',
+
+	// M
+	'modifier_zone' => 'ゾーンを編集する',
+
+	// P
+	'page_zones_acces' => '制限付きアクセス',
+	'par_titre' => '件名で',
+	'privee' => 'プライベート',
+	'publique' => '公開',
+
+	// R
+	'rubrique' => 'セクション@nb@つ',
+	'rubriques' => 'セクション@nb@つ',
+	'rubriques_zones_acces' => 'ゾーンにあるセクション',
+
+	// S
+	'selectionner_une_zone' => 'ゾーンを選択する',
+
+	// T
+	'texte_ajouter_zone' => 'ゾーンを追加する',
+	'texte_creer_associer_zone' => 'ゾーンを作成して関連付ける',
+	'titre' => '件名',
+	'titre_ajouter_zone' => 'ゾーンに参加する',
+	'titre_boite_protegee_non' => '保護なしのアクセス',
+	'titre_boite_protegee_oui' => '保護されたアクセス',
+	'titre_cadre_modifier_zone' => 'ゾーンを編集する',
+	'titre_page_config' => 'アクセスを設定する',
+	'titre_table' => 'すべてのアクセスゾーン',
+	'titre_zone_acces' => '制限付きアクセスゾーン',
+	'titre_zones_acces' => '制限付きアクセスゾーン',
+	'toutes' => '全',
+
+	// V
+	'voir_toutes' => 'すべてのゾーンを見る',
+
+	// Z
+	'zone_numero' => 'ゾーン番号：',
+	'zone_restreinte_autoriser_si_connexion_label' => '接続しているユーザーにこのゾーンへのアクセスを許可する',
+	'zone_restreinte_espace_prive' => '管理エリアでこのゾーンへのアクセスを制限する',
+	'zone_restreinte_publique' => '公開サイトでこのゾーンへのアクセスを制限する'
+);
