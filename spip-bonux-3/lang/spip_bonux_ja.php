84cc8f2c08b389f2ce9e87b16edec368de7b9a0e
origenius@mvb.biglobe.ne.jp <origenius@mvb.biglobe.ne.jp@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-10-10 03:41:14 +0000

    [Salvatore] [source:_plugins_/spip-bonux-3/lang/ spip_bonux] Export depuis http://trad.spip.net de la langue ja
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@111942 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ spip-bonux-3/lang/spip_bonux_ja.php
@@ -0,0 +1,36 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/spip_bonux?lang_cible=ja
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'afficher_calendrier' => 'カレンダーを表示する',
+	'annee_precedente' => '前の年',
+	'annee_suivante' => '次の年',
+	'annuler_recherche' => '検索を取り消す',
+
+	// B
+	'bouton_fermer' => '閉じる',
+
+	// E
+	'erreur_date' => 'この日付は正しくはありません。',
+	'erreur_date_corrigee' => '日付が修正されました。',
+	'erreur_heure' => '時間は正しくありません。',
+	'erreur_heure_corrigee' => '時間が修正されました。',
+
+	// I
+	'id_rapide' => 'クイック追加',
+
+	// M
+	'mois_precedent' => '前の月',
+	'mois_suivant' => '次の月',
+
+	// P
+	'pages' => 'ページ'
+);
