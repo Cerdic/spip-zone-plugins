5093fc1d9e6ee98c85e1dd2285b504f2f0dd7566
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-10-18 15:19:35 +0000

    v 1.1.0 - Ajouter les codes iso
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@112064 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- continents/branches/v1/continents_administrations.php
+++ continents/branches/v1/continents_administrations.php
@@ -3,36 +3,51 @@
  * Fichier gérant l'installation et désinstallation du plugin Continents
  *
  * @plugin     Continents
- * @copyright  2013
+ * @copyright  2013 - 2018
  * @author     Rainer Müller
  * @licence    GNU/GPL
  * @package    SPIP\Continents\Installation
  */
-
-if (!defined('_ECRIRE_INC_VERSION')) return;
-
+if (!defined('_ECRIRE_INC_VERSION'))
+	return;
 
 function continents_upgrade($nom_meta_base_version, $version_cible) {
 	$maj = array();
 
-
 	include_spip('base/upgrade');
-    include_spip('base/continents_peupler');
-    $maj['create'] = array(
-        array('maj_tables', array('spip_pays','spip_continents')),
-        array('peupler_base_continents'),
-        array('inserer_table_pays'),        
-        );
-    
+	include_spip('base/continents_peupler');
+	$maj['create'] = array(
+		array(
+			'maj_tables',
+			array(
+				'spip_pays',
+				'spip_continents'
+			)
+		),
+		array(
+			'peupler_base_continents'
+		),
+		array(
+			'inserer_table_pays'
+		)
+	);
+	$maj['1.1.0'] = array(
+		array(
+			'maj_tables',
+			array(
+				'spip_continents'
+			)
+		),
+		array(
+			'inserer_codes_iso'
+		)
+	);
+
 	maj_plugin($nom_meta_base_version, $version_cible, $maj);
 }
 
-
 function continents_vider_tables($nom_meta_base_version) {
-
-	 sql_drop_table("spip_continents");
+	sql_drop_table("spip_continents");
 
 	effacer_meta($nom_meta_base_version);
 }
-
-?>
