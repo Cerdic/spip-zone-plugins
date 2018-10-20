6838bde57fd439e1dd43e0d518a737810b59fdbf
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-10-20 06:08:40 +0000

    formatage
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@112083 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- continents/trunk/base/continents.php
+++ continents/trunk/base/continents.php
@@ -12,51 +12,54 @@
 //
 // Structure des tables
 //
+if (!defined("_ECRIRE_INC_VERSION"))
+	return;
 
-if (!defined("_ECRIRE_INC_VERSION")) return;
+function continents_declarer_tables_interfaces($interface) {
 
-function continents_declarer_tables_interfaces($interface){
+	// -- Table des tables ----------------------------------------------------
+	$interface['table_des_tables']['continents'] = 'continents';
 
-
-
-		//-- Table des tables ----------------------------------------------------
-
-		$interface['table_des_tables']['continents']='continents';
-
-		return $interface;
+	return $interface;
 }
 
-function continents_declarer_tables_principales($tables_principales){
-		$spip_continents = array(
-				"id_continent"  => "SMALLINT NOT NULL",
-				"nom"     => "varchar(255) NOT NULL",
-				"code_onu"  => "SMALLINT NOT NULL",
-				"code_iso_a2"			=> "varchar(2) NOT NULL default ''",
-				"code_iso_a3"	=> "varchar(3) NOT NULL default ''",
-				"latitude"  => 'text not null default ""',
-				"longitude"     => 'text not null default ""',
-				"zoom"  => 'text not null default ""',
-				"maj"       => "TIMESTAMP");
+function continents_declarer_tables_principales($tables_principales) {
+	$spip_continents = array(
+		"id_continent" => "SMALLINT NOT NULL",
+		"nom" => "varchar(255) NOT NULL",
+		"code_onu" => "SMALLINT NOT NULL",
+		"code_iso_a2" => "varchar(2) NOT NULL default ''",
+		"code_iso_a3" => "varchar(3) NOT NULL default ''",
+		"latitude" => 'text not null default ""',
+		"longitude" => 'text not null default ""',
+		"zoom" => 'text not null default ""',
+		"maj" => "TIMESTAMP"
+	);
 
-		$spip_continents_key = array(
-				"PRIMARY KEY" => "id_continent",
-				"KEY code_onu" => "code_onu",
-				);
+	$spip_continents_key = array(
+		"PRIMARY KEY" => "id_continent",
+		"KEY code_onu" => "code_onu"
+	);
 
+	$tables_principales['spip_continents'] = array(
+		'field' => &$spip_continents,
+		'key' => &$spip_continents_key,
+		'join' => array(
+			'id_continent' => 'id_continent'
+		)
+	);
 
-		$tables_principales['spip_continents'] = array(
-				'field' => &$spip_continents,
-				'key' => &$spip_continents_key,
-				'join'=> array(
-						'id_continent' => 'id_continent'
-				)
-				);
+	$tables_principales['spip_pays'] = array(
+		'field' => array(
+			'id_continent' => "SMALLINT NOT NULL"
+		),
+		'key' => array(
+			'KEY id_continent' => "id_continent"
+		),
+		'join' => array(
+			'id_continent' => "id_continent"
+		)
+	);
 
-		$tables_principales['spip_pays']=array(
-				'field'=>array('id_continent'=>"SMALLINT NOT NULL"),
-				'key'=>array('KEY id_continent'=>"id_continent"),
-				'join'=>array('id_continent'=>"id_continent"),
-		);
-
-		return $tables_principales;
+	return $tables_principales;
 }
