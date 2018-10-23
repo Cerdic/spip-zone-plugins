5255a8420d7dfda33326f8c1edfbe1430f636363
jacques@jack31.net <jacques@jack31.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-10-23 03:38:52 +0000

    [Salvatore] [source:_plugins_/formidable/trunk/lang/ formulaires_reponse] Export depuis http://trad.spip.net de la langue en
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@112125 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ formidable/trunk/lang/formulaires_reponse_en.php
@@ -0,0 +1,31 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/formulaires_reponse?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'changer_statut' => 'This form response is:',
+
+	// F
+	'formulaires_reponse_numero' => 'Form response no @nb@',
+
+	// I
+	'icone_creer_formulaire' => 'Create a new form response',
+
+	// M
+	'modifier_formulaire' => 'Edit this form response',
+
+	// R
+	'reponses_donnees' => 'Answers given:',
+
+	// T
+	'titre_formulaires_reponse' => 'Form response',
+	'titre_formulaires_reponses' => 'Form responses',
+	'titre_logo_formulaires_reponse' => 'Logo of this form response'
+);
