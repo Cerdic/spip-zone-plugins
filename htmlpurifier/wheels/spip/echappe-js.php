79ecfff31f1e5befcc33915fad1ceb771e39d716
gouz@root-me.org <gouz@root-me.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-10-28 13:34:16 +0000

    surcharge de la wheel echappe-js pour un filtrage complet des balises HTML : a débattre (donc pas de version++)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@112187 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ htmlpurifier/wheels/spip/echappe-js.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Fonctions utiles pour la wheel echappe-js
+ *
+ * @SPIP\Textwheel\Wheel\SPIP\Fonctions
+ **/
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+function echappe_anti_xss($match) {
+    static $safehtml;
+    if (!is_array($match) or !strlen($match[0])) {
+        return "";
+    }
+    $texte = &$match[0];
+    
+    if (preg_match("@^<[a-z]{1,5}( class=['\"][a-z _-]+['\"])?>$@iS", $texte)){
+      return $texte;
+    }
+    
+    if (!isset($safehtml)) {
+        $safehtml = charger_fonction('safehtml', 'inc', true);
+    }
+    $stexte = $safehtml($texte);
+    return $stexte;
+}
