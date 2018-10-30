9369ec3bcc77d56695d6846e8a592abecf543239
arnaud.berard@mister-graphx.com <arnaud.berard@mister-graphx.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-10-30 13:43:58 +0000

    trad de la doc, expliquer ce que que font les addons dans le formulaire de config (à défaut de doc, parce que mm moi je m'en rapelle plus sinon)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@112218 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- lazysizes/trunk/lang/lazysizes_fr.php
+++ lazysizes/trunk/lang/lazysizes_fr.php
@@ -6,6 +6,16 @@
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+	// E
+	'explication_artdirect'=>'{{Art Direction :}}
+		L\'extension artdirect vous permet de contrôler totalement
+	 la direction artistique via votre CSS.
+	 [Documentation->https://github.com/aFarkas/lazysizes/tree/gh-pages/plugins/artdirect] ',
+	'explication_parent-fit'=>'{{Parent Fit}}
+	étend la fonctionnalité <code>data-values = "auto"</code> afin de calculer également les bonnes tailles pour <code>object-fit: constain | cover </code>en hauteur (et en largeur) en général.
+	[Documentation->https://github.com/aFarkas/lazysizes/blob/gh-pages/plugins/parent-fit/]',
+
+	// C
 	'cfg_titre_page'	=> 'Configurer LazySizes',
 	'cfg_explication_css'	=> 'Inssérer les styles dans l\'entête des pages',
 	'cfg_label_css'	=> 'Insertion des css',
