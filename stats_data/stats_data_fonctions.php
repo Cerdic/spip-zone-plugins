45142a628f012e42a944b6fbff6b5b176fc7173c
booz@rezo.net <booz@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-11-02 20:16:44 +0000

    se fier a engin-list.txt pour la liste de moteur et afficher les liens pour chaque site affluent
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@112286 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- stats_data/stats_data_fonctions.php
+++ stats_data/stats_data_fonctions.php
@@ -1,5 +1,6 @@
 <?php
 
+// a passer en fichier inc/
 function inc_termes_recherches_to_array($referers){
 	foreach($referers as $r){
 		$termes_bruts[] = strtolower(
@@ -16,4 +17,42 @@ function inc_termes_recherches_to_array($referers){
 	arsort($termes);
 	
 	return $termes ;
+}
+
+function regexp_moteurs(){
+	include_spip("inc/referenceurs");
+	$e = stats_load_engines() ;
+	foreach($e as $m)
+		if($m[0] != "IP" AND $m[0] != "(email)")
+			$moteurs[] = str_replace("!","",strtolower($m[0])) ; // yahoo!
+	
+	return implode("|",array_unique($moteurs)) ;
+}
+
+function url2domaine($url){
+	$u = parse_url($url);
+	$u = $u["host"] ;
+	$u = preg_replace(",(co|com|qc)\.\w{2},","tld",$u);
+	$u = explode(".",$u);
+	$d = array_pop($u);
+	$d = array_pop($u);
+	return $d;
+}
+
+function compter_referers($ref){
+	foreach($ref as $url){
+		$u = parse_url($url);
+		$cle = preg_replace(",^https?://,","",$url);
+		$cle = preg_replace(",/$,","",$cle);
+		// nettoyage en plus
+		$cle = preg_replace(",\?amp=\d+$,","",$cle); // twitter
+		$cle = preg_replace(",\?fbclid=.+$,","",$cle); // fb
+		
+		$r[$cle]["url"] = $url ;
+		$r[$cle]["visites"]++;
+		$r[$cle]["path"] = preg_replace(',^/,','',$u["path"]);
+		
+	}
+	
+	return $r ;
 }
