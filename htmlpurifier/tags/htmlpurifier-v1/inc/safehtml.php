b8ea07ff5f521bdf2709d19fc8131be99955e6c5
gouz@root-me.org <gouz@root-me.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-11-03 08:17:12 +0000

    htmlpurifier : ajout du tag htmlpurifier-v1 et creation du trunk
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@112290 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ htmlpurifier/tags/htmlpurifier-v1/inc/safehtml.php
@@ -0,0 +1,46 @@
+<?php
+
+/***************************************************************************\
+ *  SPIP, Systeme de publication pour l'internet                           *
+ *                                                                         *
+ *  Copyright (c) 2001-2017                                                *
+ *  Arnaud Martin, Antoine Pitrou, Philippe Riviere, Emmanuel Saint-James  *
+ *                                                                         *
+ *  Ce programme est un logiciel libre distribue sous licence GNU/GPL.     *
+ *  Pour plus de details voir le fichier COPYING.txt ou l'aide en ligne.   *
+\***************************************************************************/
+
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+function inc_safehtml($t) {
+	static $purifier;
+
+	include_spip('inc/memoization');
+	if (function_exists('cache_get')
+	AND $a = cache_get($cle = 'safehtml:'.md5($t)))
+		return $a;
+
+	include_spip('lib/HTMLPurifier.standalone');
+	
+
+	$config = HTMLPurifier_Config::createDefault();
+
+	$config->set('Cache.SerializerPath', preg_replace(',/$,', '', realpath(_DIR_TMP)));
+	$config->set('Attr.AllowedFrameTargets', array('_blank'));
+	
+	if (!isset($purifier))
+		$purifier = new HTMLPurifier($config);
+	
+	// HTML Purifier prefere l'utf-8
+	if ($GLOBALS['meta']['charset'] == 'utf-8')
+		$t = $purifier->purify($t);
+	else
+		$t = unicode_to_charset($purifier->purify(charset2unicode($t)));
+
+	if (function_exists('cache_set'))
+		cache_set($cle, $t);
+
+	return $t;
+
+}
