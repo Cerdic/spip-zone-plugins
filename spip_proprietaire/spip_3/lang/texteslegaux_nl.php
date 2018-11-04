fe1970ddfecd37611d68aa639f3b9be7c50ace9c
hanjo.spip@gmail.com <hanjo.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-11-04 04:39:08 +0000

    [Salvatore] [source:_plugins_/spip_proprietaire/spip_3/lang/ texteslegaux] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@112315 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ spip_proprietaire/spip_3/lang/texteslegaux_nl.php
@@ -0,0 +1,134 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/texteslegaux?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'business_cards' => '{{{Visitekaartjes}}}',
+
+	// C
+	'carte_visite_info' => 'Je kunt hier onze {{visitekaartjes}} bekijken of downloaden door op onderstaande links te klikken:
+- {{[visitekaartje van de site->@classique@]}}
+- {{[visitekaartje (compleet)->@complete@]}}
+- {{[visitekaartje van de verantwoordelijke->@responsable@]}}
+- {{[visitekaartje van de beheerder->@administrateur@]}}
+- {{[visitekaartje van de webmaster->@webmaster@]}}',
+	'conditions_utilisation_abus' => '{{{Misbruik}}}
+
+Overeenkomstig de Franse [wet nr 2004-575 van 21 juni 2004 voor het vertrouwen in de digitale economie|Bekijk de wetstekst (legifrance.gouv.fr)->http://www.legifrance.gouv.fr/affichTexte.do?cidTexte=JORFTEXT000000801164], om omstreden inhoud te melden of als u het slachtoffer bent van frauduleus gebruik van de site {{[@nom_site@->@url_site@]}}, neem je contact op met de beheerder van de site op het volgende e-mailadres: [@proprietaire_mail_administratif@->mailto:@proprietaire_mail_administratif@].
+',
+	'conditions_utilisation_cookies' => '{{{Cookies}}}
+
+Sommige persoonlijke informatie en cookie-markers kunnen ongevraagd door {{[@nom_site@->@url_site@]}} worden opgeslagen op de computer van de bezoeker ({"cookies" of "JAVA applets"}). Tenzij uitdrukkelijk anders vermeld [*] zijn deze technische processen zijn niet essentieel voor de goede werking van {{[@nom_site@->@url_site@]}} en het gebruik ervan kan uiteraard worden uitgeschakeld in de browser die door de internetgebruiker wordt gebruikt, zonder dat dit van invloed is op zijn of haar navigatie. {{Deze informatie wordt niet door de site bewaard nadat de gebruiker is uitgelogd.}}
+
+<small>[*] Het is nuttig om hier te specificeren dat de toegang tot de verschillende privé-zones van de site kan vereisen dat de gebruiker om veiligheidsredenen het plaatsen van een cookie op zijn persoonlijke computer accepteert.</small>
+',
+	'conditions_utilisation_infos_personnelles' => '{{{Verzamelen en behandelen van persoonlijke gegevens}}}
+
+{{De eventueel door {{[@nom_site@->@url_site@]}} verzamelde informatie wordt nooit aan derden doorgegeven.}}
+<br />Behalve in bijzondere gevallen is deze informatie afkomstig van de vrijwillige registratie van een door de internetgebruiker verstrekt e-mailadres, waardoor hij of zij documentatie of een nieuwsbrief kan ontvangen, de inschrijving in het redactiecomité van de site kan aanvragen of informatie kan inwinnen over eender welk punt. 
+
+Overeenkomstig de Franse [wet nr 78-17 van 6 januari 1978 met betrekking tot informatietechnologie, bestanden en vrijheden|Bekijk de wetstekst (legifrance.gouv.fr)->http://www.legifrance.gouv.fr/affichTexte.do?cidTexte=JORFTEXT000000886460], {{heet de internetgebruiker het recht op toegang tot, rectificatie en verwijdering van persoonlijke gegevens die zijn opgeslagen door {{[@nom_site@->@url_site@]}}, die hij te allen tijde kan uitoefenen met de beheerder van {{[@nom_site@->@url_site@]}}.}} Voor dit soort verzoek stuur je een e-mail naar @proprietaire_forme@ @proprietaire_nom@ of via het adres: [@proprietaire_mail_administratif@->mailto:@proprietaire_mail_administratif@].
+@cnil_texte@
+',
+	'conditions_utilisation_intro' => '{{{Feitelijke acceptatie}}}
+
+{{Door gebruikmaking van {{[@nom_site@->@url_site@]}} neemt de internetgebruiker kennis van de hier vermelde voorwaarden en accepteert deze.}}
+',
+	'conditions_utilisation_liens' => '{{{Links naar @nom_site@}}}
+
+Het plaatsen van links naar {{[@nom_site@->@url_site@]}} mag zonder toestemming zolang zij niet voor commerciële of reclamedoeleinden worden gebruikt. Je kunt de onderstaande links vrijelijk gebruiken op elke vorm van medium.
+
+||Hyperlinks naar @nom_site@|Deze mogen vrij worden gebruikt.||
+|{{Overzicht van de link}}|{{HTML Code}}|
+|<a href="@url_site@" title="@nom_site@ - @descriptif_site@">@url_site@</a>|<code>
+<a href="@url_site@" title="@nom_site@ - @descriptif_site@">@url_site@</a>
+</code>|
+|<a href="@url_site@" title="@nom_site@ - @descriptif_site@"><img src="@logo_site@" border="0" style="max-width:120px ;max-height:120px ;" /></a>|<code>
+<a href="@url_site@" title="@nom_site@ - @descriptif_site@"><img src="@logo_site@" border="0" style="max-width:120px ;max-height:120px ;" /></a>
+</code>|
+',
+	'conditions_utilisation_responsabilite' => '{{{Beperking van aansprakelijkheid}}}
+
+{{[@nom_site@->@url_site@]}} bevat informatie die beschikbaar wordt gesteld door externe bedrijven of hyperlinks naar andere sites of andere externe bronnen die niet zijn ontwikkeld door {{[@nom_site@->@url_site@]}}. Het gedrag van de doelsites, dat kwaadaardig kan zijn, kan niet worden gekoppeld aan de verantwoordelijkheid van {{[@nom_site@->@url_site@]}}. Meer in het algemeen wordt de inhoud van deze site uitsluitend ter informatie aangeboden. Het is de verantwoordelijkheid van de gebruiker om deze informatie verstandig te gebruiken. {{[@nom_site@->@url_site@]}} kan niet aansprakelijk kan worden gesteld voor informatie, adviezen en aanbevelingen van derden.
+
+Voor zover{{[@nom_site@->@url_site@]}} deze sites of externe bronnen niet kan toetsen, kan {{[@nom_site@->@url_site@]}} kan niet verantwoordelijk worden gehouden voor de beschikbaarheid van deze externe sites of bronnen en kan niet verantwoordelijk worden gesteld voor de inhoud, reclame, producten, diensten of enig ander materiaal dat op of via deze externe sites of bronnen beschikbaar is. Bovendien kan {{[@nom_site@->@url_site@]}} niet aansprakelijk worden gesteld voor enige schade of verlies, bewezen of vermeend, die voortvloeit uit of in verband staat met het gebruik van of het vertrouwen op de inhoud, goederen of diensten die beschikbaar zijn op deze externe sites of bronnen. 
+',
+	'copyright_info' => '© Copyright @date@ @nom_site@ | Alle rechten voorbehouden.',
+	'createur_mentions_legales' => '[createur<-]{{{Ontwerper / Maker}}}
+
+De gehele site {{[@nom_site@->@url_site@]}}, ontwerp, grafisch ontwerp en toepassingen, zijn gemaakt en ontwikkeld @createur_administrateur_texte@ door @createur_forme@ {{@createur_nom@}}. @createur_web@ @createur_mail_texte@
+',
+	'createur_mentions_legales_idem' => '[createur<-]{{{Ontwerper / Maker}}}
+
+De site {{[@nom_site@->@url_site@]}} werd ontwikkeld door de uitgever.
+',
+
+	// E
+	'egalement_administrateur' => ' en is beheerd',
+	'enregistre_pres' => 'geregistreerd bij',
+
+	// H
+	'hebergeur_mentions_legales' => '[hebergeur<-]{{{Hosting bedrijf}}}
+
+{{[@nom_site@->@url_site@]}} wordt gehost door @hebergeur_forme@ {{@hebergeur_nom@}}@type_serveur_texte@@os_serveur_texte@. @hebergeur_web@ @hebergeur_mail_texte@
+
+{{@proprietaire_nom@}} wijst elke verantwoordelijkheid af voor eventuele onderbrekingen van de site {{[@nom_site@->@url_site@]}} en haar diensten.
+',
+	'hebergeur_mentions_legales_idem' => '[hebergeur<-]{{{Hosting bedrijf}}}
+
+De site {{[@nom_site@->@url_site@]}} wordt gehost door de uitgever.
+',
+
+	// M
+	'mention_cnil' => 'À ce titre, {{[@nom_site@->@url_site@]}} a fait l’objet d’une déclaration auprès de la {{<abbr title="Commission Nationale de l’Informatique et des Libertés - www.cnil.fr">CNIL</abbr>}} le @date_cnil@, enregistrée sous la référence {{@numero_cnil@.}}',
+	'mentions_legales_copyright' => '{{{Inhoud en reproductierechten}}}
+
+Overeenkomstig [artikelen L.111-1 en L.123-1 van de Code de la Propriété Intellectuelle|Bekijk de wetstekst (legifrance.gouv.fr)->http://www.legifrance.gouv.fr/affichCode.do?cidTexte=LEGITEXT000006069414], alle inhoud van deze site ({tekst, afbeeldingen, video&#39;s en alle media in het algemeen}), tenzij uitdrukkelijk anders vermeld, is {{beschermd door auteursrecht}}}. De reproductie, zelfs gedeeltelijk, van de inhoud van de pagina&#39;s van deze site zonder voorafgaande toestemming van @proprietaire_forme@ [{{@proprietaire_nom@}}->#proprietaire] is streng verboden.
+',
+	'mentions_legales_fonctionnement' => '{{{Functioneren van de site}}}
+
+De pagina&#39;s en inhoud van deze site worden gegenereerd door de gratis software {{SPIP}}, gedistribueerd onder de GNU / GPL licentie. Je kunt er vrij gebruik van maken om een eigen website te maken. Voor meer informatie verwijzen wij je naar de website: [spip.net->https://www.spip.net].
+
+<small>
+{{SPIP, Système de Publication pour l’Internet
+<br />Copyright © 2001-2018, Arnaud Martin, Antoine Pitrou, Philippe Rivière en Emmanuel Saint-James.}}
+</small>
+',
+
+	// O
+	'os_du_serveur' => ' gebruikmakend van een systeem {{@os_serveur@}}',
+
+	// P
+	'pas_config' => 'Er is niet genoeg informatie om deze pagina te genereren ... Je vindt meer informatie op [de configuratie-bladzijde van plugin "Mentions Légales"->@url_config@].',
+	'pour_les_contacter' => 'Om contact met hen op te nemen, gebruik je het e-mailadres: [@mail@->mailto:@mail@].',
+	'proprietaire_mentions_legales' => '[editeur<-]{{{Eigenaar / Uitgever}}}
+
+{{[@nom_site@->@url_site@]}} is een website gepubliceerd door @proprietaire_forme@ {{@proprietaire_nom@}} en gepubliceerd onder leiding van {{@proprietaire_nom_responsable@}}{@proprietaire_fonction_responsable_texte@}. De gehele site {{[@nom_site@->@url_site@]}}, pagina&#39;s en inhoud, is eigendom van @proprietaire_forme@ {{@proprietaire_nom@}}. @proprietaire_web@
+<br />Contact administratif du site {{[@nom_site@->@url_site@]}} : [@proprietaire_mail_administratif@->mailto:@proprietaire_mail_administratif@].
+',
+
+	// R
+	'reportez_vous_au_site' => 'Voor meer informatie verwijzen wij naar de website: [->@site@].',
+
+	// S
+	'sous_le_numero' => 'onder het nummer',
+	'sur_un_serveur' => ', op een {{@serveur@}} server',
+
+	// T
+	'test_fichier_langue' => 'TEST',
+
+	// V
+	'vcard_info' => 'Als je gebruik maakt van een e-mail manager, mobiele telefoon, adresboek of e-mail organizer, kun je onze volledige contactgegevens hieronder downloaden op {{’vCard’ formaat}}.
+
+Afhankelijk van je systeem kun je:
+- het volgende URL-adres direct gebruiken om deze informatie automatisch te laden: {{@vcard_url@}}
+- de kaart in je browser bekijken: {{[->@vcard_url@]}}
+- de gegevens opslaan op je computer ({geef indien nodig bestandsextensie {{’.VCF’}} aan}): {{[->@vcard_url_download@]}}'
+);
