bcaf63dec8061ad93a066706f24ddf3e30449de0
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-11-10 22:11:13 +0000

    Suite à discussion sur la liste de la Zone, laisser le nom du fichier original tel quel. Permet de le récupérer pour titrer les documents à partir du nom du fichier.
    Utilisé par SoyezCréateurs avec :
    https://zone.spip.net/trac/spip-zone/browser/spip-zone/_squelettes_/soyezcreateurs_net/trunk/plugins/soyezcreateurs/inc/titrer_document.php
    
    Et dans le _options.php :
    define('_TITRER_DOCUMENTS', true ); # (cf https://www.spip.net/fr_article5674.html)
    
    
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@112389 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- cvt-upload/trunk/inc/cvtupload.php
+++ cvt-upload/trunk/inc/cvtupload.php
@@ -124,11 +124,6 @@ function cvtupload_deplacer_fichier($fichier, $repertoire, $form, $deplacer = tr
 	}
 
 	foreach ($fichier['name'] as $cle => $nom) {
-		// On commence par transformer le nom du fichier pour éviter les conflits, on supprime notamment les accents
-		$nom = preg_replace(',\.\.+,', '.', $nom); // pas de .. dans le nom du doc
-		$nom = preg_replace("/[^.=\w-]+/", "_",
-			translitteration(preg_replace("/<[^>]*>/", '', $nom)));
-		$nom = strtolower($nom);
 		if (// Si le fichier a bien un nom et qu'il n'y a pas d'erreur associé à ce fichier
 			($nom != null)
 			and ($fichier['error'][$cle] == 0)
