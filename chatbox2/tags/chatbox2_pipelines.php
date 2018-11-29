d152263bdc7be33263a76fe4737670d404002485
pierre.troller@gmail.com <pierre.troller@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-11-29 06:40:29 +0000

    c'est mieux avec les fichiers
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@112564 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ chatbox2/tags/chatbox2_pipelines.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Utilisations de pipelines par chatbox2
+ *
+ * @plugin     chatbox2
+ * @copyright  2018
+ * @author     Ptroll
+ * @licence    GNU/GPL
+ * @package    SPIP\Chatbox2\Pipelines
+ */
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+
+/*
+ * Insérer la css des chats dans l'espace public 
+ *
+ * @pipeline header_prive
+ * @param string $head
+ * @return string
+ */
+function chatbox2_insert_head_css($flux){
+			$flux .= '<link rel="stylesheet" href="'.direction_css(find_in_path('css/chatbox2.css')).'" type="text/css" />';
+			$flux .= '<link rel="stylesheet" href="'.direction_css(find_in_path('css/all.min.css')).'" type="text/css" />';
+	return $flux;
+}
