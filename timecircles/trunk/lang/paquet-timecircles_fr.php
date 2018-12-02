96b6fcf2478b31de1ee2ed77796a72f28abdcf6b
l.oiseau2nuit@gmail.com <l.oiseau2nuit@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-12-02 02:36:51 +0000

    coquilles
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@112597 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- timecircles/trunk/lang/paquet-timecircles_fr.php
+++ timecircles/trunk/lang/paquet-timecircles_fr.php
@@ -13,6 +13,6 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// T
-	'timecircles_description' => 'Ce plugin propose d\'embarquer la librairie-plugin TimeCircles v1.5.3 de [Wim Barrelds->https://wimbarelds.com] dans SPIP et de l\'exploiter pour ajouter de jolis décomptes live à votre site, sur la base d\'une date arbitraire communiquée au modèle ou, de celle d'un évenement de l\'agenda. Le plugin propose 4 modèles utilisables selon le cas de figure.',
+	'timecircles_description' => 'Ce plugin propose d\'embarquer la librairie-plugin TimeCircles v1.5.3 de [Wim Barrelds->https://wimbarelds.com] dans SPIP et de l\'exploiter pour ajouter de jolis décomptes live à votre site, sur la base d\'une date arbitraire communiquée au modèle ou, de celle d\'un évenement de l\'agenda. Le plugin propose 3 modèles utilisables selon le cas de figure.',
 	'timecircles_slogan' => 'jQuery TimeCircles (Wim Barrlds) pour SPIP 3.1+',
 );
