f4e61bff2ae96f4a02535ddc317356445fc1cce3
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-12-04 04:41:21 +0000

    [Salvatore] [source:_plugins_/timecircles/trunk/lang/ timecircles] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@112619 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- timecircles/trunk/lang/timecircles_en.php
+++ timecircles/trunk/lang/timecircles_en.php
@@ -1,24 +1,26 @@
 <?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/timecircles?lang_cible=en
+// ** ne pas modifier le fichier **
 
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-// Fichier produit par Loiseau2nuit
-// Module: timecircles
-// Langue: en
-// Date: 03-12-2018
-// Items: 5
-
-if (!defined('_ECRIRE_INC_VERSION')) return;
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+
 	// D
 	'days' => 'Days',
+
 	// H
 	'hours' => 'Hours',
+
 	// M
 	'minutes' => 'Minutes',
+
 	// S
 	'seconds' => 'Seconds',
+
 	// Y
-	'years' => 'Years',
+	'years' => 'Years'
 );
--- timecircles/trunk/lang/timecircles_fr.php
+++ timecircles/trunk/lang/timecircles_fr.php
@@ -1,24 +1,24 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-// Fichier produit par Loiseau2nuit
-// Module: timecircles
-// Langue: fr
-// Date: 03-12-2018
-// Items: 5
-
-if (!defined('_ECRIRE_INC_VERSION')) return;
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/timecircles/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+
 	// D
 	'days' => 'Jours',
+
 	// H
 	'hours' => 'Heures',
+
 	// M
 	'minutes' => 'Minutes',
+
 	// S
 	'seconds' => 'Secondes',
+
 	// Y
-	'years' => 'Ann&eacute;es',
+	'years' => 'Années'
 );
