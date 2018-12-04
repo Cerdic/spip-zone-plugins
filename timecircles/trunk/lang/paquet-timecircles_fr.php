cdf4f74c614dffe885697f142dbdbbd9531841d9
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-12-04 04:41:24 +0000

    [Salvatore] [source:_plugins_/timecircles/trunk/lang/ paquet-timecircles] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@112620 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- timecircles/trunk/lang/paquet-timecircles_fr.php
+++ timecircles/trunk/lang/paquet-timecircles_fr.php
@@ -1,18 +1,13 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-// Fichier produit par Loiseau2nuit
-// Module: paquet-timecircles
-// Langue: fr
-// Date: 30-11-2018 14:28:35
-// Items: 2
-
-if (!defined('_ECRIRE_INC_VERSION')) return;
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/timecircles/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// T
-	'timecircles_description' => 'Ce plugin propose d\'embarquer la librairie-plugin TimeCircles v1.5.3 de [Wim Barrelds->https://wimbarelds.com] dans SPIP et de l\'exploiter pour ajouter de jolis décomptes live à votre site, sur la base d\'une date arbitraire communiquée au modèle ou, de celle d\'un évenement de l\'agenda. Le plugin propose 3 modèles utilisables selon le cas de figure.',
-	'timecircles_slogan' => 'jQuery TimeCircles (Wim Barrlds) pour SPIP 3.1+',
+	'timecircles_description' => 'Ce plugin propose d’embarquer la librairie-plugin TimeCircles v1.5.3 de [Wim Barrelds->https://wimbarelds.com] dans SPIP et de l’exploiter pour ajouter de jolis décomptes live à votre site, sur la base d’une date arbitraire communiquée au modèle ou, de celle d’un évenement de l’agenda. Le plugin propose 3 modèles utilisables selon le cas de figure.',
+	'timecircles_slogan' => 'jQuery TimeCircles (Wim Barrlds) pour SPIP 3.1+'
 );
