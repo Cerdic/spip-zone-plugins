4f58367e42144ba7a771db862cc3222267ab1fa3
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-12-09 12:12:49 +0000

    coquille doc
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@112687 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- formidable/trunk/formulaires/editer_formulaire_traitements.php
+++ formidable/trunk/formulaires/editer_formulaire_traitements.php
@@ -115,7 +115,7 @@ function formulaires_editer_formulaire_traitements_traiter($id_formulaire) {
 	$traitements_choisis = array_flip($traitements_choisis);
 	$traitements = array_intersect_key($traitements, $traitements_choisis);
 
-	// Si besoin, on créeun dossier pour stocker les fichiers
+	// Si besoin, on crée un dossier pour stocker les fichiers
 
 	$erreur_creation_dossier = formidable_creer_dossier_formulaire($id_formulaire);
 	if ($erreur_creation_dossier) {
