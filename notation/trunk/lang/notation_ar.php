065a26afaa574819824f90bd29aa17075b8ffe89
superyms@yahoo.fr <superyms@yahoo.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-12-15 04:38:57 +0000

    [Salvatore] [source:_plugins_/notation/trunk/lang/ notation] Export depuis http://trad.spip.net de la langue ar
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@112823 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ notation/trunk/lang/notation_ar.php
@@ -0,0 +1,109 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/notation?lang_cible=ar
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'acces' => 'سهولة الوصول',
+	'afficher_tables' => 'عرض التقييمات',
+	'aide' => 'مساعدة',
+	'articles' => 'مقالات',
+	'auteur' => 'مؤلف',
+
+	// B
+	'bouton_radio_fermee' => 'مغلقة',
+	'bouton_radio_ouvert' => 'مفتوحة',
+	'bouton_voter' => 'تقييم',
+
+	// C
+	'change_note_label' => 'السماح للناخبين بتغيير تقييمهم',
+	'configuration_notation' => 'ضبط التقييمات',
+	'creation' => 'إنشاء جداول البيانات',
+	'creation_des_tables_mysql' => 'إنشاء جداول البيانات',
+	'cree' => 'تم إنشاء الجداول',
+	'creer_tables' => 'إنشاء جداول البيانات',
+
+	// D
+	'date' => 'تاريخ',
+	'derniers_votes' => 'أحدث التقييمات',
+	'destruction' => 'تدمير جداول البيانات',
+	'detruit' => 'الجداول دمرت ...',
+
+	// E
+	'effacer_tables' => 'مسح جداول البيانات',
+	'err_balise' => '[ NOTATION_ERR : العلامة خارج المقال ]',
+	'err_db_notation' => '[خطأ في التقييم: تدوين واحد لكل مقالة]',
+
+	// I
+	'info_acces' => 'فتح التقييم:',
+	'info_fonctionnement_note' => 'كيف يعمل التقييم',
+	'info_methode_id' => 'التحقق من تفرد التقييمات',
+	'info_modifications' => 'تغييرات التقييم',
+	'info_ponderation' => 'يعطي عامل الترجيح مزيدًا من الوزن للمقالات التي حصلت على تقييمات كافية. <br /> أدخل أدناه عدد التقييمات التي تعتقد أن التقييم موثوق بها.',
+	'info_vote_unique_auteur' => 'إذا كنت ترغب في ضمان تفرد التقييم ، فحد من التقييم للأشخاص المسجلين <b>فقط</b>.',
+	'item_all' => 'للجميع',
+	'item_aut' => 'للمؤلفين',
+	'item_id' => 'تقييم واحد فقط لكل مستخدم',
+	'item_ide' => 'للمسجلين',
+	'item_ip' => 'تقييم واحد عن طريق IP',
+
+	// J
+	'jaidonnemonavis' => 'أنا قدمت رأيي!',
+	'jaime' => 'أحب',
+	'jaimepas' => 'لاأحب',
+	'jaimeplus' => 'لم أعد أحب',
+	'jechangedavis' => 'أنا أسحب رأيي',
+
+	// M
+	'moyenne' => 'المتوسط',
+	'moyennep' => 'المتوسط المرجح',
+
+	// N
+	'nb_etoiles' => 'قيمة التقييمات',
+	'nbobjets_note' => 'عدد العناصر التي لها تقييم:',
+	'nbvotes' => 'عدد التقييمات',
+	'nbvotes_moyen' => 'متوسط عدد التقييمات لكل عنصر',
+	'nbvotes_total' => 'إجمالي عدد التقييمات على الموقع:',
+	'note' => 'التقييم',
+	'note_1' => 'التقييم: 1',
+	'note_10' => 'التقييم: 10',
+	'note_2' => 'التقييم: 2',
+	'note_3' => 'التقييم: 3',
+	'note_4' => 'التقييم: 4',
+	'note_5' => 'التقييم: 5',
+	'note_6' => 'التقييم: 6',
+	'note_7' => 'التقييم: 7',
+	'note_8' => 'التقييم: 8',
+	'note_9' => 'التقييم: 9',
+	'note_pond' => 'التقييمات المرجحة',
+	'notes' => 'التقييمات',
+
+	// O
+	'objets' => 'عناصر',
+
+	// P
+	'param' => 'ضبط',
+	'ponderation' => 'ترجيح التقييم',
+
+	// T
+	'titre_ip' => 'طريقة عمل:',
+	'topnb' => 'العناصر العشرة الأكثر تقييمًا',
+	'topten' => 'أفضل 10 التقييمات',
+	'toptenp' => 'أفضل 10 التقييمات (المرجحة)',
+	'totaux' => 'الإجماليات',
+
+	// V
+	'valeur_nb_etoiles' => 'التقييمات من 1 إلى',
+	'valeur_ponderation' => 'عامل الترجيح',
+	'vos_notes' => 'لديك أفضل 5 تقييمات',
+	'vote' => 'رأي',
+	'voter' => 'إبداء الرأي:',
+	'votes' => 'اراء',
+	'votre_note' => 'نتيجتك'
+);
