585a2c1c9f20f6259408feeea90bbe9914a958c7
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-12-16 22:28:26 +0000

    analyser correctement les liens qui contiennent des parenthèses type https://fr.wikipedia.org/wiki/(125592)_Buthiers
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@112851 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- linkcheck/trunk/inc/linkcheck_fcts.php
+++ linkcheck/trunk/inc/linkcheck_fcts.php
@@ -85,7 +85,7 @@ function linkcheck_lister_liens($champs) {
 	 * TODO : trouver une regexp mieux que cela et complète
 	 * @var string $classe_alpha
 	 */
-	$classe_alpha = 'a-zA-Z0-9\x{00a1}-\x{FFFF}';
+	$classe_alpha = 'a-zA-Z0-9\x{00a1}-\x{FFFF}\(\)';
 	$tab_expreg = array(
 		"('|\"| |\.|\->|\]|,|;|\s)(((((http|https|ftp|ftps)://)?www\.)|((http|https|ftp|ftps)://(?:\S+(?::\S*)?@)?.([".$classe_alpha."'\-]*\.)?))(['".$classe_alpha."'0-9\-\+]*\.)+([a-zA-Z0-9]{2,9})(?::\d{2,5})?(/[".$classe_alpha."=.?&~_;\-\+\@\:\,/%#]*)?)('|\"| |\.|,|;|\s|\|\->])?",
 		'(\->)([a-zA-Z]{3,10}[0-9]{1,})\]'
