fe96682d6e7df4b2abc6f3076752f0c60614866e
superyms@yahoo.fr <superyms@yahoo.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-12-18 04:38:44 +0000

    [Salvatore] [source:_plugins_/en_travaux/trunk/lang/ paquet-entravaux] Export depuis http://trad.spip.net de la langue ar
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@112897 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ en_travaux/trunk/lang/paquet-entravaux_ar.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-entravaux?lang_cible=ar
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// E
+	'entravaux_description' => 'لتنشيط قبل مرحلة الصيانة، وهذا البرنامج المساعد يعرض فورا صفحة تحذير، {{بدلا من}} الموقع العام. تحذير: لا يزال مشرف الموقع فقط يصل إلى المنطقة الخاصة والموقع العام.',
+	'entravaux_nom' => 'في صيانة الموقع',
+	'entravaux_slogan' => 'الدخول في مرحلة الصيانة'
+);
