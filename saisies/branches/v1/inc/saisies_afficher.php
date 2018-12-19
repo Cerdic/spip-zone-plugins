a399255db0b45613ebad59bbeefd8a1945c106be
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-12-19 20:11:06 +0000

    coquille commentaire php
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@112971 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- saisies/branches/v1/inc/saisies_afficher.php
+++ saisies/branches/v1/inc/saisies_afficher.php
@@ -395,7 +395,7 @@ function saisies_verifier_afficher_si($saisies, $env=NULL) {
 	foreach ($saisies as $cle => $saisie) {
 		if (isset($saisie['options']['afficher_si'])) {
 			$condition = $saisie['options']['afficher_si'];
-			// Si tentative de log malicieux, on rejete
+			// Si tentative de code malicieux, on rejete
 			if (!saisies_verifier_securite_afficher_si($condition)) {
 				spip_log("Afficher_si malicieuse : $condition", "saisies"._LOG_CRITIQUE);
 				$condition = '$ok';
