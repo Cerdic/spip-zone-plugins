f8f451b2765efabf94b2ebed6d431e2a6c455e79
jluc@no-log.org <jluc@no-log.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-12-20 09:01:14 +0000

    APCu inside (ladnet)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@112978 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- xray/trunk/xray_options.php
+++ xray/trunk/xray_options.php
@@ -25,13 +25,14 @@ function xray_marqueur_invisible($t) {
 
 global $Memoization;
 $cfg = @unserialize($GLOBALS['meta']['memoization']);
-if ($Memoization and ($Memoization->methode == 'apc')
-and $cfg and ($cfg['methode']=='apc')) {
+if ($Memoization and 
+	(($Memoization->methode == 'apc') or ($Memoization->methode == 'apcu'))
+	and $cfg and (($cfg['methode']=='apc') or ($cfg['methode']=='apcu'))) {
 	include_once ('xray_apc.php');
 	exit;
 }
 else {
-	echo "Erreur : le plugin XRay nécessite d'activer le plugin memoization avec APC";
+	echo "Erreur : le plugin XRay nécessite d'activer le plugin memoization avec APC ou APCu";
 	exit;
 };
 
