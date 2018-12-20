25308eee1ae18c7d26203fd5880efd7ed22622d2
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-12-20 11:36:55 +0000

    Quand on a des grosses listes de subscribers une bonne pratique conseillee par les plateformes d'envoi de mail consiste a n'envoyer qu'aux inscrits "actifs" (ie qu'on a vu vivants il y a moins de 6 mois, c'est a dire ayant ouvert ou lu un mail) En attendant de gerer ca automatiquement dans la gestion des inscrits dans le plugin, voici un spip-cli pour nettoyer des listes des inscrits zombies spip mailsubscribinglist:clean --from='6 month'
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@112980 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ mailsubscribers/trunk/spip-cli/MailsubscribinglistClean.php
@@ -0,0 +1,159 @@
+<?php
+namespace Spip\Cli\Command;
+
+use Spip\Cli\Console\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Helper\ProgressHelper;
+
+class MailsubscribinglistClean extends Command {
+	protected function configure() {
+		$this
+			->setName('mailsubscribinglist:clean')
+			->setDescription('Nettoyer les listes en desinscrivant tous les subscribers qui n\'ont pas été vu vivants depuis plus de N mois')
+			->addOption(
+				'from',
+				null,
+				InputOption::VALUE_OPTIONAL,
+				'Duree d\'inactivité à prendre en compte au format strtotime : \'10 days\', \'6 months\'…',
+				''
+			)
+			->addOption(
+				'listes',
+				null,
+				InputOption::VALUE_OPTIONAL,
+				'Listes à nettoyer, séparées par des virgules. Par défaut, toutes',
+				null
+			)
+			->addOption(
+				'notify',
+				null,
+				InputOption::VALUE_NONE,
+				'indique si on veut ou non notifier par email les désabonnements, ce qui permettrait aux personnes de se réinscrire si besoin'
+			)
+			->addOption(
+				'yes',
+				'y',
+				InputOption::VALUE_NONE,
+				'Desinscrire sans demander confirmation'
+			)
+		;
+	}
+
+	protected function execute(InputInterface $input, OutputInterface $output) {
+
+		include_spip('inc/filtres');
+
+		#global $spip_racine;
+		#global $spip_loaded;
+
+		$from = $input->getOption('from');
+		if (!$from) {
+			$from = strtotime('-6 months');
+		}
+		else {
+			$from = strtotime('-' . $from);
+		}
+		if (!$from) {
+			$output->writeln("<error>from invalide</error>");
+			exit(1);
+		}
+		$from = date('Y-m-d H:i:s', $from);
+
+		$options = array();
+		if ($input->getOption('notify')) {
+			$options['notify'] = true;
+		}
+		else {
+			$options['notify'] = false;
+		}
+
+		$in_listes = '';
+		$listes = $input->getOption('listes');
+		if (!is_null($listes)) {
+			$listes = explode(',', $listes);
+			$options['listes'] = $listes;
+
+			$id_mailsubscribinglists = sql_allfetsel('id_mailsubscribinglist', 'spip_mailsubscribinglists', sql_in('identifiant', $listes));
+			$id_mailsubscribinglists = array_column($id_mailsubscribinglists, 'id_mailsubscribinglist');
+			$in_listes = " AND " . sql_in('id_mailsubscribinglist', $id_mailsubscribinglists);
+		}
+
+		// charger en memoire TOUS les emails ayant ete vu vivants depuis $from
+		$email_alive = sql_allfetsel('distinct email', 'spip_mailshots_destinataires', sql_in('statut', array('clic', 'read')) . ' AND date>' . sql_quote($from));
+		$email_alive = array_column($email_alive, 'email');
+		$output->writeln("<info>Emails vus vivants depuis $from : " . count($email_alive) . "</info>");
+
+
+		$id_mailsubscribers_alive = sql_allfetsel("id_mailsubscriber", "spip_mailsubscribers", sql_in('email', $email_alive));
+		$id_mailsubscribers_alive = array_column($id_mailsubscribers_alive, 'id_mailsubscriber');
+		$output->writeln("<info>Mailsubscribers vus vivants depuis $from : " . count($id_mailsubscribers_alive) . "</info>");
+
+		// trouver tous les zombies
+		$id_mailsubscribers_zombies = sql_allfetsel('id_mailsubscriber', 'spip_mailsubscribers', "statut='valide' AND " . sql_in('id_mailsubscriber', $id_mailsubscribers_alive, 'NOT'));
+		$id_mailsubscribers_zombies = array_column($id_mailsubscribers_zombies, 'id_mailsubscriber');
+		$output->writeln("Mailsubscribers zombies depuis $from : " . count($id_mailsubscribers_zombies));
+
+		// et restreindre aux listes demandées uniquement (si besoin)
+		$id_mailsubscribers_unsub = sql_allfetsel("DISTINCT id_mailsubscriber", 'spip_mailsubscriptions', sql_in('id_mailsubscriber',$id_mailsubscribers_zombies). " AND statut='valide' AND id_segment=0" . $in_listes);
+		$id_mailsubscribers_unsub = array_column($id_mailsubscribers_unsub, 'id_mailsubscriber');
+		$nb_unsub = count($id_mailsubscribers_unsub);
+		$this->io->care("Mailsubscribers a désabonner".($listes ? " des listes ". implode(',',$listes) : '') . " : " . $nb_unsub);
+
+		// compter par liste pour indication
+		$details = sql_allfetsel("id_mailsubscribinglist, count(id_mailsubscriber) as N", 'spip_mailsubscriptions', sql_in('id_mailsubscriber',$id_mailsubscribers_zombies). " AND statut='valide' AND id_segment=0" . $in_listes,'id_mailsubscribinglist');
+		foreach ($details as $d) {
+			if ($l = sql_fetsel('identifiant, titre', 'spip_mailsubscribinglists', 'id_mailsubscribinglist='.intval($d['id_mailsubscribinglist']))) {
+				$this->io->care("#".$d['id_mailsubscribinglist'] . " " . $l['titre'] . " : " . $d['N']);
+			}
+		}
+
+
+		// verifier que aucun des emails qu'on va unsub n'a ete vu vivant (double check donc, qu'on a pas fait d'erreur dans la selection)
+		$emails_unsub = sql_allfetsel('email', 'spip_mailsubscribers', sql_in('id_mailsubscriber', $id_mailsubscribers_unsub));
+		$emails_unsub = array_column($emails_unsub, 'email');
+		$alive_unsub = sql_allfetsel('email, max(date) as date_max, statut', 'spip_mailshots_destinataires', sql_in('statut', array('clic', 'read')) . ' AND ' . sql_in('email', $emails_unsub) . ' AND date>' . sql_quote($from), 'email');
+		if (count($alive_unsub)) {
+			$output->writeln("<error>Probleme de selections : on retrouve ".count($alive_unsub)." emails vivants dans ceux qu'on veut desinscrire</error>");
+			exit(1);
+		}
+
+		if (
+			!$input->getOption('yes')
+			and !$this->io->confirm("Désinscrire les $nb_unsub subscribers".($listes ? " des listes ". implode(',',$listes) : '')." ?", false)
+		){
+			$this->io->care("Action annulée");
+			return;
+		}
+
+
+		MailsubscribinglistClean::unsubscribeAll($this->io, $emails_unsub, $options);
+	}
+
+	public static function logRecord($io, $fichier_log, $texte) {
+		$io->text($texte);
+		file_put_contents($fichier_log, rtrim($texte) . "\n",FILE_APPEND);
+	}
+
+	public static function unsubscribeAll($io, $emails, $options) {
+
+		$now = time();
+		$d = date('Ymd-His', $now);
+		$fichier_log = _DIR_LOG . 'mailsubscribinglist-clean-'.$d.'.log';
+		$io->care("Ecriture des desinscriptions dans $fichier_log");
+
+		MailsubscribinglistClean::logRecord($io, $fichier_log,'# '. date('Y-m-d H:i:s', $now) . ' Desinscriptions ' . json_encode($options));
+
+		$nb_total = count($emails);
+		$nb = 0;
+		$unsubscribe = charger_fonction('unsubscribe', 'newsletter');
+		foreach ($emails as $email) {
+			$nb++;
+			MailsubscribinglistClean::logRecord($io, $fichier_log,"$nb/$nb_total: $email");
+			$unsubscribe($email, $options);
+		}
+
+	}
+}
