cb85741a1e688e4554a13ad9efe5d8b9602604a4
henry.izurieta@gmail.com <henry.izurieta@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2018-12-23 04:39:15 +0000

    [Salvatore] [source:_plugins_/sommaire/trunk/lang/ sommaire] Export depuis http://trad.spip.net de la langue es
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@113092 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ sommaire/trunk/lang/sommaire_es.php
@@ -0,0 +1,47 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/sommaire?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cfg_exemple' => 'Ejemplo',
+	'cfg_exemple_explication' => 'Explicación de este ejemplo',
+	'cfg_titre_parametrages' => 'Parámetros',
+
+	// E
+	'explication_niveau_max' => 'Máxima profundidad de los resúmenes. Este valor se puede cambiar caso por caso utilizando la baliza <code>#SOMMAIRE{#TEXTE,2}</code> en los esqueletos, o <code>&lt;sommaire|niveau_max=2&gt;</code> en el texto de los artículos.
+',
+	'explication_numerotation_sommaire' => 'El sumario se puede presentar como una lista con viñetas o una lista numerada.',
+	'explication_sommaire_automatique_off' => 'El webmaster inserta el resumen (sommaire) en el esqueleto.',
+	'explication_sommaire_automatique_ondemand' => 'El resumen se inserta solo en presencia del acceso directo <tt>&lt;sommaire&gt;</tt> en el texto del artículo.',
+
+	// L
+	'label_sommaire_automatique' => 'Insertando el sumario del artículo',
+	'label_sommaire_automatique_numerote' => 'Viñetas para el sumario ',
+	'label_sommaire_automatique_numerote_off' => 'lista de viñetas',
+	'label_sommaire_automatique_numerote_on' => 'lista numerada',
+	'label_sommaire_automatique_off' => 'No se nserta en el texto de los artículos.',
+	'label_sommaire_automatique_on' => 'Inserción automática en todos los artículos',
+	'label_sommaire_automatique_ondemand' => 'Se inserta en el texto de los artículos bajo demanda.',
+	'label_sommaire_niveau_max' => 'Profundidad',
+	'label_sommaire_niveau_max_1' => '1 nivel',
+	'label_sommaire_niveau_max_2' => '2 niveles',
+	'label_sommaire_niveau_max_3' => '3 niveles',
+	'label_sommaire_niveau_max_4' => '4 niveles',
+	'label_sommaire_niveau_max_5' => '5 niveles',
+	'label_sommaire_niveau_max_6' => '6 niveles',
+
+	// S
+	'sommaire_titre' => 'Sumario automático',
+
+	// T
+	'titre_cadre_sommaire' => 'Sumario',
+	'titre_page_configurer_sommaire' => 'Sumario automático',
+	'titre_retour_sommaire' => 'Volver al sumario'
+);
