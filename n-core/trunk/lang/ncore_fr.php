47a0aba4165898602b9b3f6436724fc25dceb6a6
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-01-01 10:07:38 +0000

    Et bien non on remet le couvert...
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@113184 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- n-core/trunk/lang/ncore_fr.php
+++ n-core/trunk/lang/ncore_fr.php
@@ -10,12 +10,14 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 // E
 	'erreur_type_noisette_inactif' => 'Le type de noisette @type_noisette@ est inactivé car le ou les plugins suivants sont désactivés : @plugins@.',
 
-// L
+// S
 	'saisie_conteneur_balise_label' => 'Balise matérialisant le conteneur',
 	'saisie_conteneur_css_label' => 'Style à affecter à la balise conteneur',
+
+// P
+	'preview_conteneur' => 'Balise englobante : @balise@',
 
 // T
 	'type_noisette_conteneur_titre' => 'Bloc conteneur',
 	'type_noisette_conteneur_description' => 'Conteneur pouvant accueillir des noisettes',
-	'type_noisette_conteneur_preview' => 'Balise englobante : @balise@',
 );
