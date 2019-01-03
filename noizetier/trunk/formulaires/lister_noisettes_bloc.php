93b752bbc45a961c2825ceb42e398fcf235aeb43
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-01-03 09:46:34 +0000

    Un peu de nettoyage. Je me demande quand même si le traiter du formulaire LISTER_NOISETTES_BLOC a un intérêt encore ?
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@113202 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- noizetier/trunk/formulaires/lister_noisettes_bloc.php
+++ noizetier/trunk/formulaires/lister_noisettes_bloc.php
@@ -3,7 +3,7 @@
  * Ce fichier contient le formulaire listant les noisettes incluses dans un conteneur de type (page, bloc)
  * ou (objet, bloc).
  * Ce formulaire autorise le déplacement de noisette ou l'ajout de noisette dans le conteneur par simple
- * glisser-déposer
+ * glisser-déposer.
  *
  * @package SPIP\NOIZETIER\NOISETTE\FORMULAIRE
  */
@@ -13,7 +13,6 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 
 
 /**
- *
  * La fonction charger déclare les champs postés et y intègre les valeurs par défaut.
  *
  * @param array|string $page_ou_objet
@@ -23,10 +22,6 @@ if (!defined('_ECRIRE_INC_VERSION')) {
  *        `objet` et `id_objet`.
  * @param string       $bloc
  * 		Bloc de page au sens Z.
- * @param int          $id_noisette
- * 		Identifiant de la noisette de type conteneur dans laquelle inclure une noisette.
- * @param string $redirect
- * 		URL de redirection. La valeur dépend du type d'édition.
  *
  * @return array
  * 		Tableau des champs postés pour l'affichage du formulaire.
@@ -57,10 +52,6 @@ function formulaires_lister_noisettes_bloc_charger_dist($page_ou_objet, $bloc) {
  *        `objet` et `id_objet`.
  * @param string       $bloc
  * 		Bloc de page au sens Z.
- * @param int          $id_noisette
- * 		Identifiant de la noisette de type conteneur dans laquelle inclure une noisette.
- * @param string $redirect
- * 		URL de redirection. La valeur dépend du type d'édition.
  *
  * @return array
  * 		Tableau des champs postés pour l'affichage du formulaire.
@@ -75,36 +66,7 @@ function formulaires_lister_noisettes_bloc_traiter_dist($page_ou_objet, $bloc) {
 	$id_conteneur = _request('conteneur_id');
 	$conteneur = noizetier_conteneur_decomposer($id_conteneur);
 
-	if (autoriser('configurerpage', 'noizetier', 0, '', $conteneur)) {
-		// Récupération de l'ordre des noisettes, des id de conteneur, des rangs de chaque noisette
-		// et du nombre total de noisettes dans le bloc.
-		$ordre = _request('ordre');
-		$nb_noisettes = intval(_request('nb_noisettes'));
-
-		if (count($ordre) > $nb_noisettes) {
-			// On vient d'ajouter par glisser-déposer une nouvelle noisette, on la rajoute au rang choisi.
-			// -- Identifier la noisette qui vient d'être glissée dans le bloc et retenir son rang : c'est la seule
-			//    valeur de type chaine qui n'est pas un id de noisette.
-			$index = array_search(0, array_map('intval', $ordre));
-			$type_noisette = $ordre[$index];
-			$rang = $index + 1;
-
-			include_spip('inc/ncore_noisette');
-//			if ($id_noisette = noisette_ajouter('noizetier', $type_noisette, $conteneur, $rang)) {
-//				$retour['message_ok'] = _T('info_modification_enregistree');
-//			} else {
-//				$retour['message_erreur'] = _T('noizetier:erreur_mise_a_jour');
-//			}
-		} else {
-			// On vient juste de changer l'ordre des noisettes, on réordonne toute la liste.
-			include_spip('inc/noizetier_noisette');
-//			if (noizetier_noisette_ordonner($ordre)) {
-//				$retour['message_ok'] = _T('info_modification_enregistree');
-//			} else {
-//				$retour['message_erreur'] = _T('noizetier:erreur_mise_a_jour');
-//			}
-		}
-	} else {
+	if (!autoriser('configurerpage', 'noizetier', 0, '', $conteneur)) {
 		$retour['message_erreur'] = _T('noizetier:probleme_droits');
 	}
 
