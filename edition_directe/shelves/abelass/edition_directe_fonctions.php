49547c8ad543be7099768a931df25a0cb627c633
abelass <abelass@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-01-04 13:14:28 +0000

    Merge branch 'master' into 3.0
    Conflicts:
    	edition_directe_fonctions.php
    	paquet.xml
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@113239 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- edition_directe/shelves/abelass/edition_directe_fonctions.php
+++ edition_directe/shelves/abelass/edition_directe_fonctions.php
@@ -17,31 +17,31 @@ function objets_edition_directe(){
 	// Sie rien n'est configuré, on se base sur les objets éditables de la séléction personelle de l'auteur
 	if(count($objets)<1){
 		$objets=lister_objets($prefs);
-		}
+	}
 	else{
 		//Sinon on prend les objets de la config, si l'auteur ne la pas désactivé
 		$objets2=array();
 		foreach($objets AS $objet){
-		if($prefs['edition_directe'][$objet]!='inactive')$objets2[]=$objet;
-			}
-		$objets=$objets2;
+			if($prefs['edition_directe'][$objet]!='inactive')$objets2[]=$objet;
 		}
+		$objets=$objets2;
+	}
 	// Si l'auteur à activé des objets qui ne sont pas dans la configuration initiale, les prendre quand même en compte
 	if(is_array($prefs['edition_directe'])){
 		$objets_prefs=array();
 		foreach($prefs['edition_directe'] AS $o=>$pref){
 			if($pref!='inactive')$objets_prefs[]=$o;
-			}
-		$objets=array_merge($objets,$objets_prefs);
 		}
+		$objets=array_merge($objets,$objets_prefs);
+	}
 
 	//Pipeline
-	pipeline('edition_directe_controle',array(
-				'args'=>array(
+	$pipeline= pipeline('edition_directe_controle',array(
+		'args'=>array(
 			'objet'=>$objet
-				),
-				'data'=>$objets
-		));
+		),
+		'data'=>$objets
+	));
 	return $objets;
 }
 
@@ -55,7 +55,6 @@ function lister_objets($prefs){
 	$objets=array();
 	foreach($liste_objets AS $o=>$valeur){
 		if($valeur['editable'] AND $valeur['page'] AND $prefs['edition_directe'][$valeur['page']]!='inactive')$objets[]=$valeur['page'];
-		}
-	return $objets;
 	}
-
+	return $objets;
+}
