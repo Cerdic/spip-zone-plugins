5e227731c298c9014cec91165be85cd24db9cede
abelass <abelass@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-01-04 14:11:30 +0000

    nettoyage et commentaires de base
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@113248 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- edition_directe/trunk/action/edition_directe_auteur.php
+++ edition_directe/trunk/action/edition_directe_auteur.php
@@ -1,24 +1,31 @@
 <?php
+/**
+ * Action d'édition du plugin Edition_directe
+ *
+ * @plugin     Edition_directe
+ * @copyright  2011 - 2018
+ * @author     Rainer Müller
+ * @licence    GNU/GPL
+ * @package    SPIP\Edition_directe\Action
+ */
 
-if (!defined("_ECRIRE_INC_VERSION")) return;
-
+if (!defined("_ECRIRE_INC_VERSION"))
+	return;
 
 function action_edition_directe_auteur_dist() {
 	include_spip('inc/session');
 
 	$securiser_action = charger_fonction('securiser_action', 'inc');
 	$arg = $securiser_action();
-	$id_auteur=session_get('id_auteur');
-	
-	$prefs=session_get('prefs');
- 
-	
-	list($action,$objet)=explode('-',$arg);
+	$id_auteur = session_get('id_auteur');
 
-	$prefs['edition_directe'][$objet]=$action;
-	
+	$prefs = session_get('prefs');
 
-	sql_updateq('spip_auteurs',array('prefs'=>serialize($prefs)),'id_auteur='.$id_auteur);
+	list ($action, $objet) = explode('-', $arg);
 
-	}
-?>
+	$prefs['edition_directe'][$objet] = $action;
+
+	sql_updateq('spip_auteurs', array(
+		'prefs' => serialize($prefs)
+	), 'id_auteur=' . $id_auteur);
+}
--- edition_directe/trunk/edition_directe_fonctions.php
+++ edition_directe/trunk/edition_directe_fonctions.php
@@ -1,61 +1,72 @@
 <?php
+/**
+ * Fonctions utiles au plugin Edition_directe
+ *
+ * @plugin     Edition_directe
+ * @copyright  2011 - 2018
+ * @author     Rainer Müller
+ * @licence    GNU/GPL
+ * @package    SPIP\Edition_directe\Fonctions
+ */
+
 // teste si l'objet est en mode edition directe ou non
-function objets_edition_directe(){
+function objets_edition_directe() {
 	include_spip('inc/config');
 	include_spip('inc/session');
 
-
 	// Récuperer la séléction des objets depuis la config
-	$objets=lire_config('edition_directe/objets', []);
+	$objets = lire_config('edition_directe/objets', []);
 
-	//Récupère les préférence de l'auteur, pour émodifier le choix par défaut
-	$prefs=session_get('prefs');
+	// Récupère les préférence de l'auteur, pour émodifier le choix par défaut
+	$prefs = session_get('prefs');
 
-	// Apparament session_get  retourne suivant le contexte, un tableau serialisé, assurer qu'il ne soir pas sérialisé
-	if(!is_array($prefs))$prefs=unserialize($prefs);
+	// Apparament session_get retourne suivant le contexte, un tableau serialisé, assurer qu'il ne soir pas sérialisé
+	if (!is_array($prefs))
+		$prefs = unserialize($prefs);
 
 	// Sie rien n'est configuré, on se base sur les objets éditables de la séléction personelle de l'auteur
-	if(count($objets)<1){
-		$objets=lister_objets($prefs);
+	if (count($objets) < 1) {
+		$objets = lister_objets($prefs);
+	}
+	else {
+		// Sinon on prend les objets de la config, si l'auteur ne la pas désactivé
+		$objets2 = array();
+		foreach ($objets as $objet) {
+			if ($prefs['edition_directe'][$objet] != 'inactive')
+				$objets2[] = $objet;
 		}
-	else{
-		//Sinon on prend les objets de la config, si l'auteur ne la pas désactivé
-		$objets2=array();
-		foreach($objets AS $objet){
-		if($prefs['edition_directe'][$objet]!='inactive')$objets2[]=$objet;
-			}
-		$objets=$objets2;
-		}
+		$objets = $objets2;
+	}
 	// Si l'auteur à activé des objets qui ne sont pas dans la configuration initiale, les prendre quand même en compte
-	if(is_array($prefs['edition_directe'])){
-		$objets_prefs=array();
-		foreach($prefs['edition_directe'] AS $o=>$pref){
-			if($pref!='inactive')$objets_prefs[]=$o;
-			}
-		$objets=array_merge($objets,$objets_prefs);
+	if (is_array($prefs['edition_directe'])) {
+		$objets_prefs = array();
+		foreach ($prefs['edition_directe'] as $o => $pref) {
+			if ($pref != 'inactive')
+				$objets_prefs[] = $o;
 		}
+		$objets = array_merge($objets, $objets_prefs);
+	}
 
-	//Pipeline
-	pipeline('edition_directe_controle',array(
-				'args'=>array(
-			'objet'=>$objet
-				),
-				'data'=>$objets
-		));
+	// Pipeline
+	pipeline('edition_directe_controle', array(
+		'args' => array(
+			'objet' => $objet
+		),
+		'data' => $objets
+	));
 	return $objets;
 }
 
-
 // Liste les objets disponible pour l'édition directe
-function lister_objets($prefs){
+function lister_objets($prefs) {
 	include_spip('base/objets');
 
 	// Lister les objets éditables auf ceux qui sont désactivé par l'utilisateur
-	$liste_objets=lister_tables_objets_sql();
-	$objets=array();
-	foreach($liste_objets AS $o=>$valeur){
-		if($valeur['editable'] AND $valeur['page'] AND $prefs['edition_directe'][$valeur['page']]!='inactive')$objets[]=$valeur['page'];
-		}
-	return $objets;
+	$liste_objets = lister_tables_objets_sql();
+	$objets = array();
+	foreach ($liste_objets as $valeur) {
+		if ($valeur['editable'] and $valeur['page'] and $prefs['edition_directe'][$valeur['page']] != 'inactive')
+			$objets[] = $valeur['page'];
 	}
-
+	return $objets;
+}
