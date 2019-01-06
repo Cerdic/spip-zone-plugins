1da6703da7b000c0279a9fdef97e32079daba074
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-01-06 14:55:27 +0000

    Mise à jour de la description du plugin.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@113275 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- exclure_secteur/trunk/lang/exclure_sect_fr.php
+++ exclure_secteur/trunk/lang/exclure_sect_fr.php
@@ -1,7 +1,8 @@
 <?php
 // Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-if (!defined('_ECRIRE_INC_VERSION')) return;
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
--- exclure_secteur/trunk/lang/paquet-exclure_sect_fr.php
+++ exclure_secteur/trunk/lang/paquet-exclure_sect_fr.php
@@ -1,28 +1,20 @@
 <?php
-
 // Ceci est un fichier langue de SPIP -- This is a SPIP language file
 
-///  Fichier produit par PlugOnet
-// Module: paquet-exclure_sect
-// Langue: fr
-// Date: 18-12-2011 12:10:03
-// Items: 2
-
-if (!defined('_ECRIRE_INC_VERSION')) return;
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 // E
 	'exclure_sect_description' => 'Ce plugin exclut automatiquement les secteurs séléctionnés des boucles ARTICLES / SITES / RUBRIQUES / BREVES / FORUMS (suivant la régle de SPIP, qui ne prend en compte que les forums liés à des articles).
 
-Attention, pour le moment, ne le fait pas sur les jointures (par exemple, n\'exclura pas les forums qui appartiennent à un article du secteur).
+Attention, pour le moment, le plugin ne gère pas les jointures (par exemple, il n\'exclura pas les forums qui appartiennent à un article d\'un secteur exclu).
 
-On peut faire une exception avec le critére <code>{tout_secteur}</code>. 
-Si on précise le secteur explicitement, par <code>{id_secteur=x}</code>, <code>{id_secteur==x}</code> ou <code>{id_secteur IN X}</code>, le réglage ne sera pas pris en compte.
+Si dans une boucle on précise le secteur explicitement avec un critère <code>{id_secteur=valeur}</code>, <code>{id_secteur==regexp}</code> ou <code>{id_secteur IN liste}</code>, le traitement automatique d\'exclusion est désactivé.
 
-Possibilité aussi de configurer le plugin pour que <code>{tout}</code> soit équivalent à <code>{tout_voir}</code>.
-
-On peut aussi configurer pour que que si l\'id principal du nom de boucle est passée via <code>{id_boucle}</code> ou <code>{id_boucle==X}</code> ou <code>{id_boucle=X}</code> ou <code>{id_boucle IN X}</code>, cela fait sauter l\'exclusion des secteurs. (Depuis la 1.2)',
-	'exclure_sect_slogan' => 'Exclure automatiquement des boucles certains secteurs',
+On peut désactiver l\'exclusion automatique en utilisant le critère <code>{tout_voir}</code>. 
+On peut aussi configurer le plugin pour que si l\'id principal du nom de boucle fait partie d\'un critère inclusif <code>{id_type_objet}</code>, <code>{type_objet==regexp}</code>, <code>{type_objet=valeur}</code> ou <code>{type_objet IN liste}</code> le traitement d\'exclusion soit désactivé (depuis la version 1.2)',
+	'exclure_sect_slogan' => 'Exclure automatiquement certains secteurs des boucles SPIP',
 );
-?>
