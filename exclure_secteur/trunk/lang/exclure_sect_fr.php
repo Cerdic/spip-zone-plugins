93de3d0a9bb5905b936c0118770edf7e31d1d42b
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-01-06 14:39:02 +0000

    Mise au point du formulaire de configuration avec les standards SPIP actuels sachant qu'à priori on ne veut pas nécessiter Saisies pour ce petit plugin. Comme les items de langue ne sont pas traduits on en profite pour revoir la nomenclature. Ajout de l'autorisation configuration pour la page de ...configuration.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@113274 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- exclure_secteur/trunk/lang/exclure_sect_fr.php
+++ exclure_secteur/trunk/lang/exclure_sect_fr.php
@@ -1,28 +1,27 @@
 <?php
-
 // Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-///  Fichier produit par PlugOnet
-// Module: paquet-exclure_sect
-// Langue: fr
-// Date: 18-12-2011 12:10:03
-// Items: 2
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// C
-	'cfg_avances'			=> 'Réglages avancés du plugin',
-	'cfg_exclure_secteur'		=> 'Configuration du plugin exclure-secteur',
-	'cfg_explication'		=> 'Secteurs à exclure',
-	'cfg_explication_exclure_sect'	=> 'Choisissez les secteurs à exclure. Ceux-ci n\'apparaîtront pas sur le site public, à moins d\'utiliser le critère <code>{tout_voir}</code>',
-	'cfg_id_explicite'		=> 'Ignorer les boucles sur lesquelles l\'identifiant de l\'objet est explicité ou pris dans le contexte ? <small>(Permet de ne pas modifier le squelette article.html)</small>',
-	'cfg_tout'			=> 'Le critère <code>{tout}</code> est-il équivalent au critère <code>{tout_voir}</code> ?',
-	'cfg_label_tout'		=> 'Critère <code>{tout}</code>',
-	'cfg_label_id_explicite'	=> 'Identifiant explicite',
+	'configurer_form_titre'	=> 'Configuration',
+	'configurer_page_titre'	=> 'Plugin Exclure-secteur',
+	'configurer_menu_entree' =>'Exclure des secteurs',
 
 	// E
-	'exclure_secteur'		=>'Exclure secteur',
+	'secteurs_exclus_fieldset' => 'Secteurs à exclure',
+	'secteurs_exclus_explication'	=> 'Choisissez les secteurs à exclure. Ceux-ci n\'apparaîtront pas sur le site public, à moins d\'utiliser le critère <code>{tout_voir}</code>',
+
+	// I
+	'id_explicite_label' => 'Identifiant explicite',
+	'id_explicite_explication' => 'Le critère <code>{tout}</code> est-il équivalent au critère <code>{tout_voir}</code> ?',
+
+	// R
+	'reglages_avances_fieldset'	=> 'Réglages avancés du plugin',
+
+	// T
+	'tout_label' => 'Critère <code>{tout}</code>',
+	'tout_explication' => 'Ignorer les boucles sur lesquelles l\'identifiant de l\'objet est explicité ou pris dans le contexte ? <small>(Permet de ne pas modifier certains squelettes comme article.html)</small>',
 );
-?>
