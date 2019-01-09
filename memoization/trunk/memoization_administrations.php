a0152fbe242d09e994b9d2f37a58900d67f09b0a
jluc@no-log.org <jluc@no-log.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-01-09 10:13:36 +0000

    mm : mieux vaut un fichier d'administrations + fil : gogogo
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@113296 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ memoization/trunk/memoization_administrations.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Fichier gérant l'installation et la désinstallation du plugin
+ *
+ * @package SPIP\memoization\Installation
+**/
+
+// Sécurité
+if (!defined('_ECRIRE_INC_VERSION')) {
+		return;
+}
+
+/**
+ * Installation/maj des tables de formidable...
+ *
+ * @param string $nom_meta_base_version
+ *	 Nom de la meta informant de la version du schéma de données du plugin installé dans SPIP
+ * @param string $version_cible
+ *	 Version du schéma de données dans ce plugin (déclaré dans paquet.xml)
+ * @return void
+ */
+function formidable_upgrade($nom_meta_base_version, $version_cible) {
+	// Création des tables
+	include_spip('base/create');
+	include_spip('base/abstract_sql');
+	include_spip('inc/formidable_migrer_depuis_ft');
+
+	$maj = array();
+	// Pas de create
+
+	$maj['20190109'] = array(
+		array('memoization_migrer_config_pages')
+	);
+	include_spip('base/upgrade');
+	maj_plugin($nom_meta_base_version, $version_cible, $maj);
+}
+
+function memoization_migrer_config_pages() {
+	include_spip('inc/config');
+	debug_log ("memoization_migrer_config_pages done", "memoization_migrer_config_pages");
+	if ($methode=lire_config('memoization/pages')) {
+		ecrire_config('memoization/methode', $methode);
+		effacer_config('memoization/pages');
+	}
+}
+
+function memoization_vider_tables($nom_meta_base_version) {
+	include_spip('inc/config');
+	include_spip('inc/meta');
+
+	effacer_config("memoization");
+	effacer_meta($nom_meta_base_version);
+}
