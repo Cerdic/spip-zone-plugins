a75d2535566d8f1c88613a64af7a5ff14bab48c9
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-01-09 17:29:05 +0000

    array par défaut
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@113336 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- liaison_objet/trunk/liaison_objet_pipelines.php
+++ liaison_objet/trunk/liaison_objet_pipelines.php
@@ -148,7 +148,7 @@ function liaison_objet_formulaire_charger($flux) {
 		$objets_cibles = lire_config('liaison_objet/objets_cible', array());
 
 		foreach ($objets_cibles as $objet) {
-			if ($types_lien = lire_config('liaison_objet/type_liens_' . $objet)) {
+			if ($types_lien = lire_config('liaison_objet/type_liens_' . $objet, array())) {
 				$flux['data']['type_liens_' . $objet] = '';
 				foreach ($types_lien as $key => $value) {
 					if ($key)
