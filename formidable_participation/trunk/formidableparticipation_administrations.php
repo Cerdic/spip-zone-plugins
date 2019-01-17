b900504af3024e31f3fbaa26ecbc3336247b2ada
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-01-17 10:22:14 +0000

    ne pas oublier de migrer les anciens traitements pour ne pas casser le fonctionnement lors de la mise à jour
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@113469 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- formidable_participation/trunk/formidableparticipation_administrations.php
+++ formidable_participation/trunk/formidableparticipation_administrations.php
@@ -21,11 +21,40 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 **/
 function formidableparticipation_upgrade($nom_meta_base_version, $version_cible) {
 	$maj = array();
+	$maj['install'] = array();
+	$maj['1.1.0'] = array(
+		array('formidableparticipation_upgrade_1_1_0')
+	);
 
 	include_spip('base/upgrade');
 	maj_plugin($nom_meta_base_version, $version_cible, $maj);
 }
 
+/**
+ * Met à jour vers le schema 1.1.0 des traitements participations.
+ * A savoir, prend les traitements de participation existants et ajoute automatiquement
+ * participation_auto = 'variable'
+ * evenement_type = 'fixe'
+ * Ceci permet de ne pas casser les évènements
+**/
+function formidableparticipation_upgrade_1_1_0() {
+	include_spip('inc/sql');
+	$res = sql_select('id_formulaire,traitements', 'spip_formulaires');
+	while ($row = sql_fetch($res)) {
+		$traitements = unserialize($row['traitements']);
+		if (isset($traitements['participation'])) {
+			$participation = &$traitements['participation'];
+			if (!isset($participation['participation_auto'])) {
+				$participation['participation_auto'] = 'variable';
+			}
+			if (!isset($participation['evenement_type'])) {
+				$participation['evenement_type'] = 'fixe';
+			}
+			$traitements = serialize($traitements);
+			sql_updateq('spip_formulaires',array('traitements' => $traitements), 'id_formulaire='.$row['id_formulaire']);
+		}
+	}
+}
 
 /**
  * Fonction de désinstallation du plugin Formulaires de participation.
@@ -39,5 +68,3 @@ function formidableparticipation_vider_tables($nom_meta_base_version) {
 
 	effacer_meta($nom_meta_base_version);
 }
-
-?>
