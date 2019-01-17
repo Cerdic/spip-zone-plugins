df9d61beb863ffa0846200422f285d2180ef2da2
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-01-17 19:19:18 +0000

    [Salvatore] [source:_plugins_/formidable_participation/trunk/lang/ formidableparticipation] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@113477 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- formidable_participation/trunk/lang/formidableparticipation_fr.php
+++ formidable_participation/trunk/lang/formidableparticipation_fr.php
@@ -1,37 +1,37 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
-if (!defined('_ECRIRE_INC_VERSION')) return;
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/formidable_participation/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// F
 	'formidableparticipation_titre' => 'Formulaires de participation',
 
-	'traiter_participation_titre' => 'Participation à un évènement',
-	'traiter_participation_description' => 'Inscrire comme participant·e à un évènement',
-
-	'traiter_participation_champ_email_label' => 'Champ de l\'Email',
-	'traiter_participation_champ_choix_participation_label' => 'Champ de la participation',
+	// T
 	'traiter_participation_champ_choix_participation_explication' => 'Le champ qui propose participation ou non-participation',
-	'traiter_participation_champ_participation_oui'=>'Valeur pour la participation',
-	'traiter_participation_champ_participation_non'=>'Valeur pour la non-participation',
-	'traiter_participation_id_evenement_participation' => 'id_evenement',
-	'traiter_participation_id_evenement_participation_explication' => 'Permet de lier les réponses à un évènement',
-	'traiter_participation_option_champ_nom_label' => 'Champ du Nom',
-	'traiter_participation_option_champ_prenom_label' => 'Champ du Prénom',
-	'traiter_participation_option_champ_organisme_label' => 'Champ de l\'Organisme',
-	'traiter_participation_necessite_explication' => 'Pour la prise en charge de la participation, il est nécessaire d\'enregistrer les résultats dans la base de données.',
+	'traiter_participation_champ_choix_participation_label' => 'Champ de la participation',
+	'traiter_participation_champ_email_label' => 'Champ de l’Email',
+	'traiter_participation_champ_evenement_participation_label' => 'Champ d’évènement',
+	'traiter_participation_champ_evenement_type_fixe_label' => 'Évènement fixe',
+	'traiter_participation_champ_evenement_type_label' => 'Type d’évènement',
+	'traiter_participation_champ_evenement_type_variable_label' => 'Évènement variable - dépendant d’un champ',
+	'traiter_participation_champ_participant' => 'Informations sur le ou la participant·e',
+	'traiter_participation_champ_participation_auto_choix_auto_label' => 'Le remplissage du formulaire implique automatiquement la participation à l’évènement',
+	'traiter_participation_champ_participation_auto_choix_variable_label' => 'La participation à l’événement dépend d’un champ du formulaire',
 	'traiter_participation_champ_participation_auto_label' => 'Participation',
 	'traiter_participation_champ_participation_label' => 'Champs de participation',
-	'traiter_participation_champ_participant' => 'Informations sur le ou la participant·e',
-	'traiter_participation_champ_participation_auto_choix_auto_label' => 'Le remplissage du formulaire implique automatiquement la participation à l\'évènement',
-	'traiter_participation_champ_participation_auto_choix_variable_label' => 'La participation à l\'événement dépend d\'un champ du formulaire',
-	'traiter_participation_evenement_label' => "Choix de l'évènement auquel participer",
-	'traiter_participation_champ_evenement_type_label' => 'Type d\'évènement',
-	'traiter_participation_champ_evenement_type_fixe_label' => 'Évènement fixe',
-	'traiter_participation_champ_evenement_type_variable_label' => 'Évènement variable - dépendant d\'un champ',
-	'traiter_participation_champ_evenement_participation_label' => 'Champ d\'évènement',
+	'traiter_participation_champ_participation_non' => 'Valeur pour la non-participation',
+	'traiter_participation_champ_participation_oui' => 'Valeur pour la participation',
+	'traiter_participation_description' => 'Inscrire comme participant·e à un évènement',
+	'traiter_participation_evenement_label' => 'Choix de l’évènement auquel participer',
+	'traiter_participation_id_evenement_participation' => 'id_evenement',
+	'traiter_participation_id_evenement_participation_explication' => 'Permet de lier les réponses à un évènement',
+	'traiter_participation_necessite_explication' => 'Pour la prise en charge de la participation, il est nécessaire d’enregistrer les résultats dans la base de données.',
+	'traiter_participation_option_champ_nom_label' => 'Champ du Nom',
+	'traiter_participation_option_champ_organisme_label' => 'Champ de l’Organisme',
+	'traiter_participation_option_champ_prenom_label' => 'Champ du Prénom',
+	'traiter_participation_titre' => 'Participation à un évènement'
 );
-
-?>
