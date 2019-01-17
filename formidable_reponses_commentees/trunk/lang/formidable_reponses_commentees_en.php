a1bf8556736a3d62e9484c50547e23299c7b0e1e
gilles.vincent@gmail.com <gilles.vincent@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-01-17 04:37:15 +0000

    [Salvatore] [source:_plugins_/formidable_reponses_commentees/trunk/lang/ formidable_reponses_commentees] Export depuis http://trad.spip.net de la langue en
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@113455 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ formidable_reponses_commentees/trunk/lang/formidable_reponses_commentees_en.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/formidable_reponses_commentees?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'commentaire_apres_reponse_explication' => 'Enter a comment that will be provided to the user after he sent his reply. You can use the shortcuts of SPIP.',
+	'commentaire_apres_reponse_label' => 'Comment to display after the answer'
+);
