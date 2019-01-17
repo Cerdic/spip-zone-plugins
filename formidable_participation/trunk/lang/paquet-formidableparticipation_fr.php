7e8ddb724b9d407f820aab9da9789f5e00255fd7
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-01-17 19:19:20 +0000

    [Salvatore] [source:_plugins_/formidable_participation/trunk/lang/ paquet-formidableparticipation] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@113478 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- formidable_participation/trunk/lang/paquet-formidableparticipation_fr.php
+++ formidable_participation/trunk/lang/paquet-formidableparticipation_fr.php
@@ -1,14 +1,14 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
-if (!defined('_ECRIRE_INC_VERSION')) return;
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/formidable_participation/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// F
-	'formidableparticipation_description' => 'Outil qui permet de générer un pipeline de participation lors de la saisie d\'un formulaire du plugin Formidable',
+	'formidableparticipation_description' => 'Outil qui permet de générer un pipeline de participation lors de la saisie d’un formulaire du plugin Formidable',
 	'formidableparticipation_nom' => 'Formulaire de participation Formidable',
-	'formidableparticipation_slogan' => 'Extension du plugin formidable',
+	'formidableparticipation_slogan' => 'Extension du plugin formidable'
 );
-
-?>
