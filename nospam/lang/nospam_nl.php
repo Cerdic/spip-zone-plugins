38e217f757354c5d0002e70711896ebc3e23a749
hanjo.spip@gmail.com <hanjo.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-01-24 04:38:56 +0000

    [Salvatore] [source:_plugins_/nospam/lang/ nospam] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@113595 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- nospam/lang/nospam_nl.php
+++ nospam/lang/nospam_nl.php
@@ -15,11 +15,11 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'erreur_jeton' => 'Dit bericht kan niet worden aanvaard. Voer het nogmaals in!',
 	'erreur_spam' => 'Dit bericht kan niet worden aanvaard!',
 	'erreur_spam_doublon' => 'Er bestaat al een identiek bericht!',
-	'erreur_spam_ip' => 'Trop de commentaires nuit à la qualité !',
+	'erreur_spam_ip' => 'Te veel opmerkingen zijn nadelig voor de kwaliteit!',
 	'erreur_url_deja_spammee' => 'Dit bericht bevat koppelingen die op SPAM lijken. Verwijder ze aub.',
 
 	// F
-	'forum_saisie_texte_info' => 'Dit formulier accepteert alleen SPIP code <code>[-&gt;url] {{gras}} {italique} &lt;quote&gt; &lt;code&gt;</code> &lt;cadre&gt;</cadre> en HTML code <code>&lt;q&gt; &lt;del&gt; &lt;ins&gt;</code>. Op paragrafen aan te maken, kun je gewoon een paar regels leeg laten.',
+	'forum_saisie_texte_info' => 'Dit formulier accepteert alleen SPIP code <code>[-&gt;url] {{vet}} {cursief} &lt;quote&gt; &lt;code&gt;</code> &lt;cadre&gt;</cadre> en HTML code <code>&lt;q&gt; &lt;del&gt; &lt;ins&gt;</code>. Op paragrafen aan te maken, kun je gewoon een paar regels leeg laten.',
 
 	// I
 	'info_ip_suspecte' => 'Uw IP-adres wordt ervan verdacht SPAM te verzenden. Laat uw goede bedoelingen blijken.',
