c887292435d7094df568247845d6cedd6bbfea47
nicod@lerebooteux.fr <nicod@lerebooteux.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-01-24 19:58:54 +0000

    Si on utilise les sous rubriques dans la configuration multidomaines (en activant _MULTIDOMAINE_RUBRIQUE), créer une hiérarchie visuelle pour identifier les secteurs / rubriques. Déplacer l'explication dans le texte d'introduction et raccourcir les label_squelette pour gagner un peu de place en hauteur. Remplacer les ul/li par des div si possible.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@113604 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- multidomaines/branches/nettoyage/lang/multidomaines_fr.php
+++ multidomaines/branches/nettoyage/lang/multidomaines_fr.php
@@ -13,8 +13,9 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'explications' => 'Le plugin va lire la configuration de la façon suivante : <br />
 	- pour trouver le domaine du secteur : Url du secteur puis url par defaut <br />
 	- pour trouver le squelette du secteur : Chercher le dossier suivant le schéma (www.domaine.fr puis domaine.fr) puis dossiers des squelettes du secteur. <br />
-	Un domaine peut avoir plusieurs dossiers squelettes, il faut les séparer par ":" (ex:squelettes:squelettes/sous-domaine.domaine.com)',
-	'explication_identifiant' => 'Nom informatique optionnel pour identifier le secteur : charactères alphanumériques ou «_»',
+	Un domaine peut avoir plusieurs dossiers squelettes, il faut les séparer par ":" (ex:squelettes:squelettes/sous-domaine.domaine.com)<br>
+	<br>
+	Identifiant : Nom informatique optionnel pour identifier le secteur : charactères alphanumériques ou «_»',
 
 	// I
 	'info_domaine_non' => 'Le domaine « @domaine@ » n\'est pas configuré',
@@ -25,8 +26,8 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'label_editer_url_rubrique' => 'Url de la rubrique',
 	'label_editer_url_secteur' => 'Url du secteur',
 	'label_squelette' => 'Dossier des secteurs',
-	'label_squelette_rubrique' => 'Dossiers des squelettes de la rubrique',
-	'label_squelette_secteur' => 'Dossiers des squelettes du secteur',
+	'label_squelette_rubrique' => 'Dossiers des squelettes',
+	'label_squelette_secteur' => 'Dossiers des squelettes',
 	'label_menu_id_domaine' => 'N° ou identifiant du secteur',
 	'label_identifiant' => 'Identifiant',
 
