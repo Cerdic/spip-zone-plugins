ae91efb4cf1fc9b8f24a4891d579464c528696b3
tcharlss@bravecassine.com <tcharlss@bravecassine.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-01-28 22:45:20 +0000

    Après discussions, on transforme la constante _METASPLUS_MASQUER_AUTEURS en option de configuration. Mise au propre du formulaire de config en passant (indentation, classes, tout ça). On force les gens à supprimer l'ancienne image avant d'en ajouter une nouvelle. NB : je retire l'ajax du formulaire car sinon ça affiche une erreur avec le bouton de suppression (même si l'action se déroule comme il faut)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@113664 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- metasplus/trunk/formulaires/configurer_metasplus.php
+++ metasplus/trunk/formulaires/configurer_metasplus.php
@@ -1,27 +1,44 @@
 <?php
-if (!defined('_ECRIRE_INC_VERSION')) return;
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
 function formulaires_configurer_metasplus_traiter_dist() {
-	include_spip('inc/cvt_configurer');
-	if ($documents = $_FILES) {
-		$mode = 'auto';
-		include_spip('action/editer_liens');
-		$ajouter_document = charger_fonction('ajouter_documents', 'action');
-		$Tid_doc = $ajouter_document(0, $documents, null, 0 ,$mode);
-		$id_document = $Tid_doc[0];
 
-		if ($id_document) {
-			sql_updateq('spip_documents',array('statut' => 'publie'), 'id_document='.intval($id_document));
-			set_request('id_doc_logo',$id_document);
-		}
-	}
+	include_spip('inc/cvt_configurer');
+	refuser_traiter_formulaire_ajax();
 
 	$retours = array();
 
-	// On enregistre la nouvelle configuration
-	$trace = cvtconf_formulaires_configurer_enregistre('configurer_metasplus', array());
+	// En cas de suppression de l'image, ne rien faire
+	$supprimer_document = (_request('action') == 'supprimer_document');
+	if (!$supprimer_document) {
 
-	$retours['message_ok'] = _T('config_info_enregistree') . $trace;
+		// Enregistrement de l'image
+		if ($documents = $_FILES) {
+			$mode = 'auto';
+			include_spip('action/editer_liens');
+			$ajouter_document = charger_fonction('ajouter_documents', 'action');
+			if (
+				$document = $ajouter_document(0, $documents, null, 0, $mode)
+				and $id_document = intval($document[0])
+			) {
+				;
+				sql_updateq('spip_documents',array('statut' => 'publie'), 'id_document='.intval($id_document));
+				set_request('id_doc_logo',$id_document);
+			}
+		}
+
+		// Enregistrement de la configuration
+		$trace = cvtconf_formulaires_configurer_enregistre('configurer_metasplus', array());
+		$retours['message_ok'] = _T('config_info_enregistree') . $trace;
+	}
+
+	// Si on traite le logo, on force une redirection
+	if ($id_document or $supprimer_document) {
+		$retours['redirect'] = parametre_url(self(), 'id_document', $id_document, '&');
+	}
+
 	$retours['editable'] = true;
 
 	return $retours;
--- metasplus/trunk/lang/metasplus_fr.php
+++ metasplus/trunk/lang/metasplus_fr.php
@@ -9,19 +9,22 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// E
 	'explication_id_doc_logo' => 'Permet d\'utiliser une image de votre choix à la place du logo du site SPIP (propriétés "og:image" et "twitter:image", format conseillé: 1,91:1)',
+	'explication_masquer_auteurs' => 'Cochez cette option si vous ne souhaitez pas diffuser le nom des rédacteurs des contenus : le nom du site sera utilisé à la place.',
 
 	// I
 	'icone_previsu' => 'Aperçu des métas+',
 	'icone_previsu_title' => 'Aperçu des métadonnées Dublin Core, Open Graph et Twitter.',
 
 	// L
-	'label_reseaux' => 'Cochez les métas à afficher',
+	'label_reseaux' => 'Cochez les métas à inclure',
 	'label_dublincore' => 'Dublin Core',
 	'label_opengraph' => 'Open Graph',
 	'label_twitter' => 'Twitter',
-	'logo_bis' => 'Ajouter une image qui remplacera celle du site',
+	'logo_bis' => 'Image du site',
 	'label_previsu_fond' => 'Fond utilisé',
 	'label_previsu_objet' => 'Objet éditorial',
+	'label_masquer_auteurs' => 'Auteurs',
+	'label_case_masquer_auteurs' => 'Utiliser le nom du site comme auteur des contenus',
 
 	// T
 	'titre_config_metasplus' => 'Configurer Métas +',
