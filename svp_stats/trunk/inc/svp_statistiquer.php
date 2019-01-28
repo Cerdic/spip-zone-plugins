d749f4452ea08301be34baa057a785f23c94824b
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-01-28 18:33:20 +0000

    Mise au point des statistiques par branche et plugin. Passage en stable du plugin et up de y
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@113661 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- svp_stats/trunk/inc/svp_statistiquer.php
+++ svp_stats/trunk/inc/svp_statistiquer.php
@@ -35,13 +35,13 @@ function svp_actualiser_stats() {
 				// Si le plugin est bien dans la base on peut lui mettre a jour ses statistiques
 				sql_updateq('spip_plugins', 
 							array('nbr_sites'=> $_stat->sites, 'popularite'=> floatval(trim($_stat->pourcentage, '%'))),
-							'id_plugin=' . sql_quote($id_plugin));
+							'id_plugin=' . intval($id_plugin));
 			}
 		}
 	}
 
 	// Détermination de la date (mois année) pour l'historique.
-	$date = date('y-m');
+	$date = date('m-y');
 
 	// Récupération des statistiques par branche SPIP et mise en base dans la table spip_plugins_stats.
 	// La liste des branches en cours est fournie par SVP dans un tableau global.
@@ -54,22 +54,23 @@ function svp_actualiser_stats() {
 			// On ne fait que loger l'erreur car celle-ci n'a pas d'incidence sur le comportement
 			// de SVP
 			spip_log("MODULE STATS - Réponse du serveur incorrecte ou mal formée. Les statistiques de la branche ${_branche} ne seront pas mises à jour", 'svp_actions.' . _LOG_ERREUR);
-			retour = false;
+			$retour = false;
 		} else {
 			foreach ($stats as $_stat) {
 				$prefixe = strtoupper($_stat->nom);
-				$where = array('prefixe='. sql_quote($prefixe), 'branche=' . sql_quote($_branche));
-				// Suivant que l'enregistrement du plugin pour la branche donnée existe ou pas, on met à jour ou on insère les statistiques.
+				$where = array('prefixe='. sql_quote($prefixe), 'branche_spip=' . sql_quote($_branche));
+				// Suivant que l'enregistrement du plugin pour la branche donnée existe ou pas, on met à jour
+				// ou on insère les statistiques.
 				$historique = array();
-				if ($historique_existant = getfetsel('historique', 'spip_plugins_stats', $where)) {
+				if ($historique_existant = sql_getfetsel('historique', 'spip_plugins_stats', $where)) {
 					// Les stats existent déjà, on les met à jour avec l'historique mensuel.
 					$historique = unserialize($historique_existant);
 					$historique[$date] = $_stat->sites;
 					sql_updateq(
 						'spip_plugins_stats', 
 						array(
-							'nbr_sites'=> $_stat->sites, 
-							'popularite'=> floatval(trim($_stat->pourcentage, '%')),
+							'nbr_sites'  => $_stat->sites,
+							'popularite' => floatval(trim($_stat->pourcentage, '%')),
 							'historique' => serialize($historique)
 						),
 						$where
@@ -80,11 +81,11 @@ function svp_actualiser_stats() {
 					sql_insertq(
 						'spip_plugins_stats', 
 						array(
-							'prefixe'  => $prefixe,
-							'branche' => $_branche,
-							'nbr_sites'  => $_stat->sites,
-							'popularite' => floatval(trim($_stat->pourcentage, '%')),
-							'historique' => serialize($historique)
+							'prefixe'      => $prefixe,
+							'branche_spip' => $_branche,
+							'nbr_sites'    => $_stat->sites,
+							'popularite'   => floatval(trim($_stat->pourcentage, '%')),
+							'historique'   => serialize($historique)
 						)
 					);
 				}
