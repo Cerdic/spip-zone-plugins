8daf656d03f5cf5497778db04526f63d6ccc527e
tcharlss@bravecassine.com <tcharlss@bravecassine.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-01-30 20:44:21 +0000

    Fix form de config : les boutons d'action ne font pas bon ménage avec les formulaires de config. Tous les champs présents dans le formulaire sont pris en compte, du coup le traitement faisait systématiquement la suppression de l'image. On remet le bouton de suppression en simple lien (je subodore que la solution la plus propre serait de garder le bouton d'action et de personnaliser le charger, mais bon, pas le temps là). Du coup on remet l'ajax tant qu'à faire.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@113693 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- metasplus/trunk/formulaires/configurer_metasplus.php
+++ metasplus/trunk/formulaires/configurer_metasplus.php
@@ -6,36 +6,31 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 function formulaires_configurer_metasplus_traiter_dist() {
 
 	include_spip('inc/cvt_configurer');
-	refuser_traiter_formulaire_ajax();
+	//refuser_traiter_formulaire_ajax();
 
 	$retours = array();
 
-	// En cas de suppression de l'image, ne rien faire
-	$supprimer_document = (_request('action') == 'supprimer_document');
-	if (!$supprimer_document) {
-
-		// Enregistrement de l'image
-		if ($documents = $_FILES) {
-			$mode = 'auto';
-			include_spip('action/editer_liens');
-			$ajouter_document = charger_fonction('ajouter_documents', 'action');
-			if (
-				$document = $ajouter_document(0, $documents, null, 0, $mode)
-				and $id_document = intval($document[0])
-			) {
-				;
-				sql_updateq('spip_documents',array('statut' => 'publie'), 'id_document='.intval($id_document));
-				set_request('id_doc_logo',$id_document);
-			}
+	// Enregistrement de l'image
+	if ($documents = $_FILES) {
+		$mode = 'auto';
+		include_spip('action/editer_liens');
+		$ajouter_document = charger_fonction('ajouter_documents', 'action');
+		if (
+			$document = $ajouter_document(0, $documents, null, 0, $mode)
+			and $id_document = intval($document[0])
+		) {
+			;
+			sql_updateq('spip_documents',array('statut' => 'publie'), 'id_document='.intval($id_document));
+			set_request('id_doc_logo',$id_document);
 		}
-
-		// Enregistrement de la configuration
-		$trace = cvtconf_formulaires_configurer_enregistre('configurer_metasplus', array());
-		$retours['message_ok'] = _T('config_info_enregistree') . $trace;
 	}
 
+	// Enregistrement de la configuration
+	$trace = cvtconf_formulaires_configurer_enregistre('configurer_metasplus', array());
+	$retours['message_ok'] = _T('config_info_enregistree') . $trace;
+
 	// Si on traite le logo, on force une redirection
-	if ($id_document or $supprimer_document) {
+	if ($id_document) {
 		$retours['redirect'] = parametre_url(self(), 'id_document', $id_document, '&');
 	}
 
