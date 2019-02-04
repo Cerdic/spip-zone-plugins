7cafa969e647cd92b53b69f402925f667e2b4587
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-02-04 14:33:01 +0000

    dans le devellopement php d'une balise, mettre bien des string et pas des constantes (signalé par Laurent Bloch sur la liste)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@113732 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- licence/balise/licence.php
+++ licence/balise/licence.php
@@ -10,9 +10,9 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 
 function balise_LICENCE_dist ($p){
 	if (($logo = interprete_argument_balise (1, $p))==NULL)
-		$logo = 'oui';
+		$logo = "'oui'";
 	if (($lien = interprete_argument_balise (2, $p))==NULL)
-		$lien = 'oui';
+		$lien = "'oui'";
 	$p->code = "licence_affiche(".champ_sql('id_licence', $p).",".$logo.",".$lien.")";
 	$p->interdire_scripts = false;
 	return $p;
