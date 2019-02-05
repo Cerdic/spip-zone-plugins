dbd85865916723c22c5f59f462aef221870fe9ea
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-02-05 14:32:47 +0000

    Ajout de la description et du slogan du plugin.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@113751 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ cache/trunk/lang/paquet-cache_fr.php
@@ -0,0 +1,13 @@
+<?php
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// N
+	'cache_description' => 'Framework de gestion des caches pour plugins. Ce plugin fournit les API et les mécanismes permettant d\'écrire, de lire et de supprimer des caches spécifiques à un plus utilisateur.',
+	'cache_slogan' => 'La fabrique de caches pour plugins',
+);
