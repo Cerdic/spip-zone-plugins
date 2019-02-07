5ff0c2d70641e70e61ebf4f7391f0b848ea1fed5
l.oiseau2nuit@gmail.com <l.oiseau2nuit@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-02-07 16:50:58 +0000

    les fichier.js.html ne chargeaient pas et renvoyaient une 403 lorsque l'utilisateur n'était pas connecté. retour sur un état qui fonctionne en attendant de toruver une solution qui fonctionne avec le (futur) formulaire de config
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@113802 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- timecircles/trunk/timecircles_pipelines.php
+++ timecircles/trunk/timecircles_pipelines.php
@@ -32,7 +32,45 @@ function timecircles_header_prive($flux){
  * load timecircles' js on the website
  **/
 function timecircles_insert_head($flux){
-	$flux .= '<script src="'.generer_url_public('lib/timecircles.js').'" type="text/javascript"></script>'
-	. '<script src="'.find_in_path('js/timecircles.js').'" type="text/javascript"></script>';
+	$flux .= '<script src="'.find_in_path('js/timecircles.js').'" type="text/javascript"></script>'
+	.<<<EOF
+<script type="text/javascript">
+<!--
+jQuery(document).ready(function(){
+	$(".DateCountdown").TimeCircles();
+	$(".CountDownTimer").TimeCircles({ time: { Days: { show: false }, Hours: { show: false } }});
+	$(".PageOpenTimer").TimeCircles();
+	var updateTime = function(){
+		var date = $("#date").val();
+		var time = $("#time").val();
+		var datetime = date + ' ' + time + ':00';
+		$(".DateCountdown").data('date', datetime).TimeCircles().start();
+	}
+	$("#date").change(updateTime).keyup(updateTime);
+	$("#time").change(updateTime).keyup(updateTime);
+	/* Start and stop are methods applied on the public TimeCircles instance */
+	$(".startTimer").click(function() {
+		$(".CountDownTimer").TimeCircles().start();
+	});
+	$(".stopTimer").click(function() {
+		$(".CountDownTimer").TimeCircles().stop();
+	});
+	/* Fade in and fade out are examples of how chaining can be done with TimeCircles */
+	$(".fadeIn").click(function() {
+		$(".PageOpenTimer").fadeIn();
+	});
+	$(".fadeOut").click(function() {
+		$(".PageOpenTimer").fadeOut();
+	});
+	$(window).on('resize', function(){
+    $('.DateCountdown').TimeCircles().rebuild();
+		$('.CountDownTimer').TimeCircles().rebuild();
+		$('.PageOpenTimer').TimeCircles().rebuild();
+	});
+});
+-->		
+</script>
+EOF;
+
 	return $flux;
 }
