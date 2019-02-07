7940da096caf96fcdfdc57c47f96a680bc117fa8
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-02-07 14:52:07 +0000

    Débuter l'intégration de Cache Factory dans N-Core
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@113794 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ n-core/trunk/cache/ncore.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Ce fichier contient les fonctions de service nécessité par le plugin Cache Factory.
+ *
+ */
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+
+/**
+ * Renvoie la configuration spécifique des caches de Taxonomie.
+ *
+ * @param string $plugin
+ *        Identifiant qui permet de distinguer le module appelant qui peut-être un plugin comme le noiZetier ou
+ *        un script. Pour un plugin, le plus pertinent est d'utiliser le préfixe.
+ *
+ * @return array
+ *        Tableau de la configuration brute du plugin N-Core.
+ */
+function ncore_cache_configurer($plugin) {
+
+	// Initialisation du tableau de configuration avec les valeurs par défaut du plugin Cache.
+	$configuration = array(
+		'racine'        => _DIR_CACHE,
+		'nom'           => array('objet', 'type'),
+		'extension'     => '.php',
+		'securisation'  => true,
+		'serialisation' => true,
+		'separateur'    => '-'
+	);
+
+	return $configuration;
+}
