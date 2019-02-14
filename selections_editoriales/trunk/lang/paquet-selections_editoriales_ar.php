e822a71bd9191bef46a40aab82a2a43390399dc5
george@middleeastwatch.net <george@middleeastwatch.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-02-14 04:38:56 +0000

    [Salvatore] [source:_plugins_/selections_editoriales/trunk/lang/ paquet-selections_editoriales] Export depuis http://trad.spip.net de la langue ar
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@113901 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ selections_editoriales/trunk/lang/paquet-selections_editoriales_ar.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-selections_editoriales?lang_cible=ar
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// S
+	'selections_editoriales_description' => 'انشاء لوائح محتوى بإعطائها عناوين وترتيب ووصف وربط صور بها. يمكن استخدام هذه الوظيفة لإدارة التحرير في الصفحة الأساسية للموقع مثلاً.',
+	'selections_editoriales_nom' => 'اختيار المفضل التحريري',
+	'selections_editoriales_slogan' => 'انشاد لوائح من العناصر المهمة في الموقع'
+);
