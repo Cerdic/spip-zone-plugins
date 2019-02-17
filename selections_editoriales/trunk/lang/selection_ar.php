1cec2726c29fee6cdc2c4690c4b7560c1ef368a6
george@middleeastwatch.net <george@middleeastwatch.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-02-17 04:38:54 +0000

    [Salvatore] [source:_plugins_/selections_editoriales/trunk/lang/ selection] Export depuis http://trad.spip.net de la langue ar
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@113946 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ selections_editoriales/trunk/lang/selection_ar.php
@@ -0,0 +1,61 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/selection?lang_cible=ar
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'ajouter_lien_selection' => 'إضافة هذا المحتوى المفضّل',
+
+	// B
+	'bouton_enlever_selection' => 'إزالة الربط',
+	'bouton_modifier_selection' => 'تعديل',
+	'bouton_supprimer_selection' => 'حذف',
+
+	// C
+	'champ_css_explication' => 'أنماط css مضافة الى هذا الاختيار.',
+	'champ_css_label' => 'Css',
+	'champ_descriptif_label' => 'الوصف',
+	'champ_identifiant_explication' => 'معرّف نصي فريد يتيح جلب هذا الاختيار بسهولة.',
+	'champ_identifiant_label' => 'المعرّف',
+	'champ_limite_explication' => 'الحد الاقصى للمحتويات في الاختيار.',
+	'champ_limite_label' => 'الحد الاقصى',
+	'champ_nombre_contenus' => 'عدد العناصر',
+	'champ_titre_label' => 'العنوان',
+
+	// E
+	'erreur_identifiant_existant' => 'هذا المعرّف مستخدم سابقاً في الاختيار «@selection@».',
+	'erreur_limite_entier' => 'يجب ان يكون الحد الاقصى عدد صحيح موجب.',
+
+	// I
+	'icone_creer_selection' => 'إنشاء مفضّل تحريري',
+	'icone_modifier_selection' => 'تعديل هذا المفضّل التحريري',
+	'info_1_selection' => 'مفضّل تحريري واحد',
+	'info_aucun_selection' => 'لا يوجد اي مفضّل تحريري',
+	'info_nb_selections' => '@nb@ مفضّل تحريري',
+	'info_selections_auteur' => 'المفضّلات التحريرية لهذا المؤلف',
+
+	// R
+	'retirer_lien_selection' => 'سحب هذا المفضّل التحريري',
+	'retirer_tous_liens_selections' => 'سحب كل المفضّلات التحريرية',
+
+	// T
+	'texte_ajouter_selection' => 'إضافة مفضّل تحريري',
+	'texte_ajouter_selection_explication' => 'إنشاء مفضّل تحريري او اختيار واحد من القائمة أدناه.',
+	'texte_changer_statut_selection' => 'هذا المفضّل التحريري:',
+	'texte_creer_associer_selection' => 'إنشاء مفضّل تحريري وربطه',
+	'titre_langue_selection' => 'لغة هذا المفضّل التحريري',
+	'titre_logo_selection' => 'رمز هذا المفضّل التحريري',
+	'titre_nouvelle_selection' => 'مفضّل تحريري جديد',
+	'titre_selection' => 'المفضّل التحريري',
+	'titre_selection_nouvelle' => 'الاختيار',
+	'titre_selections' => 'المفضّلات التحريرية',
+	'titre_selections_autonomes' => 'مفضّلات تحريرية مستقلة',
+	'titre_selections_objets' => 'مفضّلات تحريرية مرتبطة بمحتوى',
+	'titre_selections_rubrique' => 'مفضّلات القسم التحريرية'
+);
