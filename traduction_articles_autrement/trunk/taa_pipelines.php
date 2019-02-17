1603af96d03313dfff4261e8754bf377d03b03e3
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-02-17 16:58:08 +0000

    En cas de langue par secteur, l'article doit avoir la langue de la rubrique parente.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@113955 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- traduction_articles_autrement/trunk/taa_pipelines.php
+++ traduction_articles_autrement/trunk/taa_pipelines.php
@@ -35,7 +35,7 @@ function taa_pre_insertion($flux) {
 		if ($lang = _request('lang_dest')) {
 			$flux['data']['lang'] = $lang;
 		}
-		elseif (test_plugin_actif('tradrub') && $id_rubrique = _request('id_rubrique') && $id_rubrique > 0) {
+		elseif (test_plugin_actif('tradrub')) {
 			$id_rubrique = _request('id_parent') ? _request('id_parent') : _request('id_rubrique');
 			$lang = sql_getfetsel('lang', 'spip_rubriques', 'id_rubrique=' . $id_rubrique);
 			$flux['data']['lang'] = $lang;
@@ -66,6 +66,37 @@ function taa_recuperer_fond($flux) {
 	if ($flux['args']['fond'] == 'prive/objets/liste/articles' and _request('exec') != 'article' and !lire_config('taa/liste_compacte_desactive')) {
 
 		$flux['texte'] = recuperer_fond('prive/objets/liste/articles_compacte', $flux['args']['contexte']);
+	}
+
+	return $flux;
+}
+
+/**
+ * Agit lors de l’édition d’un élément éditorial, lorsque l’utilisateur édite les champs ou change le statut de l’objet.
+ * Il est appelé juste avant l’enregistrement des données.
+ * On peut s’en servir pour contrôler ou modif
+ *
+ * @pipeline pre_edition
+ *
+ * @param array $flux
+ *   Les données du pipeline
+ *
+ * @return array
+ *   Les donées du pipeleine.
+ */
+function taa_pre_edition($flux) {
+	$table = $flux['args']['table'];
+	// Si tradrub actif, on suppose le  système de secteur par langue.
+	// L'article doit donc avoir la mème langue que la rubrique parente.
+	if ($table == 'spip_articles' and test_plugin_actif('tradrub')) {
+		spip_log($flux, 'teste');
+		$rubrique = sql_fetsel('id_rubrique,lang', $table, 'id_article=' . $flux['args']['id_objet']);
+		if ($lang = sql_getfetsel(
+				'lang',
+				'spip_rubriques',
+				'id_rubrique=' . $rubrique['id_rubrique']) and $lang != $rubrique['lang']) {
+			$flux['data']['lang'] = $lang;
+		}
 	}
 
 	return $flux;
