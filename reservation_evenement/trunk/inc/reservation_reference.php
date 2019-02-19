b82f5f69059b6ceec247705de914e290cee1c577
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-02-19 16:19:38 +0000

    meilleure référence, garantir qu'elle soit unique
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@113967 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- reservation_evenement/trunk/inc/reservation_reference.php
+++ reservation_evenement/trunk/inc/reservation_reference.php
@@ -3,8 +3,16 @@
 // Sécurité
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
-function inc_reservation_reference_dist($id_auteur=0){
-	return time();
-}
+function inc_reservation_reference_dist($id_reservation){
+	if ($date = sql_getfetsel('date', 'spip_reservations', 'id_reservation=' . intval($id_reservation))) {
+		$t = strtotime($date);
+	}
+	else {
+		$t = $_SERVER['REQUEST_TIME'];
+	}
 
-?>
+	// format YYYYMMDDNNNNNN
+	$reference = date('Ymd', $t) . str_pad(intval($id_reservation), 6, '0', STR_PAD_LEFT);
+
+	return $reference;
+}
