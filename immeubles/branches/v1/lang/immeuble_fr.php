4386c8582b3305b9aa09a374165cdabe88e7228b
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-02-23 12:38:09 +0000

    Objet éditorial immeubles, utilisé par location d'immeubles
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@114062 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ immeubles/branches/v1/lang/immeuble_fr.php
@@ -0,0 +1,48 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'ajouter_lien_immeuble' => 'Ajouter ce immeuble',
+
+	// C
+	'champ_chambres_label' => 'Chambres',
+	'champ_descriptif_label' => 'Déscriptif',
+	'champ_lits_doubles_label' => 'Lits doubles',
+	'champ_lits_simples_label' => 'Lits simples',
+	'champ_titre_label' => 'Titre',
+	'confirmer_supprimer_immeuble' => 'Confirmez-vous la suppression de cet immeuble ?',
+
+	// I
+	'icone_creer_immeuble' => 'Créer un immeuble',
+	'icone_modifier_immeuble' => 'Modifier cet immeuble',
+	'info_1_immeuble' => 'Un immeuble',
+	'info_aucun_immeuble' => 'Aucun immeuble',
+	'info_immeubles_auteur' => 'Les immeubles de cet auteur',
+	'info_nb_immeubles' => '@nb@ immeubles',
+
+	// R
+	'retirer_lien_immeuble' => 'Retirer cet immeuble',
+	'retirer_tous_liens_immeubles' => 'Retirer tous les immeubles',
+
+	// S
+	'supprimer_immeuble' => 'Supprimer cet immeuble',
+
+	// T
+	'texte_ajouter_immeuble' => 'Ajouter un immeuble',
+	'texte_changer_statut_immeuble' => 'Cet immeuble est :',
+	'texte_creer_associer_immeuble' => 'Créer et associer un immeuble',
+	'texte_definir_comme_traduction_immeuble' => 'Cet immeuble est une traduction du immeuble numéro :',
+	'titre_immeuble' => 'Immeuble',
+	'titre_immeubles' => 'Immeubles',
+	'titre_immeubles_rubrique' => 'Immeubles de la rubrique',
+	'titre_langue_immeuble' => 'Langue de cet immeuble',
+	'titre_logo_immeuble' => 'Logo de cet immeuble',
+	'titre_objets_lies_immeuble' => 'Liés à cet immeuble',
+);
