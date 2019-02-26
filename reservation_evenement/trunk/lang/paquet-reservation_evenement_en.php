b45cce9d468eb92756aff2d7dfe9f7efb4917fb9
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-02-26 04:40:05 +0000

    [Salvatore] [source:_plugins_/reservation_evenement/trunk/lang/ paquet-reservation_evenement] Export depuis http://trad.spip.net de la langue en
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@114113 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ reservation_evenement/trunk/lang/paquet-reservation_evenement_en.php
@@ -0,0 +1,22 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-reservation_evenement?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// R
+	'reservation_evenement_description' => 'This plugin, which requires the agenda plugin offers advanced management of event booking.
+
+It contains :
+- A form with multiple event selection, personal data extensible via plugin extras fields
+- Reservation management and booking details
+- Export via csv
+- Management of automatic email sending according to the status of the reservation (to be defined in the configuration panel)',
+	'reservation_evenement_nom' => 'Event booking',
+	'reservation_evenement_slogan' => 'Event booking system'
+);
