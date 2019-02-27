5ce14b1172d3320ba3d3bc7f89af46ec763d91c7
george@middleeastwatch.net <george@middleeastwatch.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-02-27 10:07:31 +0000

    Langue arabe pour Magnet
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@114133 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ magnet/trunk/lang/magnet_ar.php
@@ -0,0 +1,18 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/comments/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'label_magnetize' => 'مغنطة',
+	'label_demagnetize' => 'إزالة',
+	'label_up' => 'صعود',
+	'label_down' => 'نزول',
+
+	'titre_page_configurer' => 'إعداد ملحق المغناطيس',
+	'label_activer_magnet_objets' => 'تفعيل المغنايس على العناصر',
+);
+
+?>
---
+++ magnet/trunk/lang/paquet-magnet_ar.php
@@ -0,0 +1,13 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/comments/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'magnet_description' => '',
+	'magnet_slogan' => 'مغنطة المقالات لسحبها الى الصفحة الأساسية في الموقع',
+);
+
+?>
