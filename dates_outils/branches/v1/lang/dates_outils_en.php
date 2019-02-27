722aa225d161651efd143b437f4e001a950a3af4
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-02-27 17:03:59 +0000

    1.0.11 Traductions en anglais
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@114156 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ dates_outils/branches/v1/lang/dates_outils_en.php
@@ -0,0 +1,38 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'champ_annee_label' => 'Year:',
+	'champ_date_label' => 'Date:',
+	'champ_date_debut_label' => 'from:',
+	'champ_date_fin_label' => 'to:',
+
+	// D
+	'date_au' => 'on',
+	'date_du' => 'from',
+	'dates_outils_titre' => 'Date tools',
+
+	// I
+	'info_date_debut' => 'from',
+	'info_date_fin' => 'to',
+
+	// J
+	'champ_jour_label' => 'Day:',
+
+	// M
+	'champ_mois_label' => 'Month:',
+
+	// N
+	'nuits' => 'nights',
+
+	// P
+	'periode_label' => 'Periode:',
+
+
+);
