bb2355e29ca90ab0bfcac64ab7c948226f77e136
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-03-01 04:39:46 +0000

    [Salvatore] [source:_plugins_/magnet/trunk/lang/ paquet-magnet] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@114190 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- magnet/trunk/lang/paquet-magnet_ar.php
+++ magnet/trunk/lang/paquet-magnet_ar.php
@@ -1,13 +1,15 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/comments/trunk/lang/
-if (!defined('_ECRIRE_INC_VERSION')) return;
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-magnet?lang_cible=ar
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-	// C
+	// M
 	'magnet_description' => '',
-	'magnet_slogan' => 'مغنطة المقالات لسحبها الى الصفحة الأساسية في الموقع',
+	'magnet_slogan' => 'مغنطة المقالات لسحبها الى الصفحة الأساسية في الموقع'
 );
-
-?>
--- magnet/trunk/lang/paquet-magnet_fr.php
+++ magnet/trunk/lang/paquet-magnet_fr.php
@@ -1,13 +1,13 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/comments/trunk/lang/
-if (!defined('_ECRIRE_INC_VERSION')) return;
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/magnet/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-	// C
+	// M
 	'magnet_description' => '',
-	'magnet_slogan' => 'Aimanter les articles pour les faire remonter en Home',
+	'magnet_slogan' => 'Aimanter les articles pour les faire remonter en Home'
 );
-
-?>
