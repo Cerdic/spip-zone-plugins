7b3c767d301e6cb47e44b938395744bdccdfa20e
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-03-02 04:39:13 +0000

    [Salvatore] [source:_plugins_/location_objets/trunk/lang/ objets_locations_detail] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@114242 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- location_objets/trunk/lang/objets_locations_detail_fr.php
+++ location_objets/trunk/lang/objets_locations_detail_fr.php
@@ -1,10 +1,9 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/location_objets/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
-
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
@@ -12,16 +11,16 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'ajouter_lien_objets_locations_detail' => 'Ajouter ce location detail',
 
 	// C
-	'champ_devise_label' => 'Devise&nbsp;: ',
-	'champ_duree_label' => 'Durée :',
-	'champ_id_objet_label' => 'Id objet : ',
-	'champ_objet_label' => 'Objet: ',
-	'champ_prix_ht_label' => 'Prix : ',
-	'champ_prix_unitaire_ht_label' => 'Prix unitaire ht&nbsp;:',
-	'champ_prix_label' => 'Prix :',
-	'champ_quantite_label' => 'Quantité :',
-	'champ_reduction_label' => 'Réduction :',
-	'champ_taxe_label' => 'Taxe :',
+	'champ_devise_label' => 'Devise : ',
+	'champ_duree_label' => 'Durée :',
+	'champ_id_objet_label' => 'Id objet : ',
+	'champ_objet_label' => 'Objet : ',
+	'champ_prix_ht_label' => 'Prix : ',
+	'champ_prix_label' => 'Prix :',
+	'champ_prix_unitaire_ht_label' => 'Prix unitaire ht :',
+	'champ_quantite_label' => 'Quantité :',
+	'champ_reduction_label' => 'Réduction :',
+	'champ_taxe_label' => 'Taxe :',
 	'confirmer_supprimer_objets_locations_detail' => 'Confirmez-vous la suppression de cet location detail ?',
 
 	// I
@@ -30,8 +29,8 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'info_1_objets_locations_detail' => 'Un location detail',
 	'info_aucun_objets_locations_detail' => 'Aucun location detail',
 	'info_nb_objets_locations_details' => '@nb@ location details',
-	'info_objets_locations_details_espace' => 'Les locations de cet espace : ',
-	'info_objets_locations_details_immeuble' => 'Les locations de cet immeuble : ',
+	'info_objets_locations_details_espace' => 'Les locations de cet espace : ',
+	'info_objets_locations_details_immeuble' => 'Les locations de cet immeuble : ',
 
 	// R
 	'retirer_lien_objets_locations_detail' => 'Retirer ce location detail',
@@ -50,5 +49,5 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'titre_objets_lies_objets_locations_detail' => 'Liés à ce location detail',
 	'titre_objets_locations_detail' => 'Location detail',
 	'titre_objets_locations_details' => 'Location details',
-	'titre_objets_locations_details_rubrique' => 'Location details de la rubrique',
+	'titre_objets_locations_details_rubrique' => 'Location details de la rubrique'
 );
