485f534dc6f86a26e115bd383d0d014fef4b8f50
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-03-02 04:39:10 +0000

    [Salvatore] [source:_plugins_/location_immeubles/trunk/lang/ paquet-location_immeubles] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@114240 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- location_immeubles/trunk/lang/paquet-location_immeubles_fr.php
+++ location_immeubles/trunk/lang/paquet-location_immeubles_fr.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/location_immeubles/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
@@ -8,7 +8,7 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// L
-	'location_immeubles_description' => 'Plugins qui assembles divers plugins tiers et propose de squelettes pour permettre de gérer la location d\'immeubles',
-	'location_immeubles_nom' => 'Location d\'immeubles',
-	'location_immeubles_slogan' => 'Louez vos immeubles',
+	'location_immeubles_description' => 'Plugins qui assembles divers plugins tiers et propose de squelettes pour permettre de gérer la location d’immeubles',
+	'location_immeubles_nom' => 'Location d’immeubles',
+	'location_immeubles_slogan' => 'Louez vos immeubles'
 );
