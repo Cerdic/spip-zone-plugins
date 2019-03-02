85d1b1b27eb9af2ace71ba6c23f9358bc67c1daa
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-03-02 04:39:37 +0000

    [Salvatore] [source:_plugins_/objets_infos_extras/trunk/lang/ paquet-objets_infos_extras] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@114252 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- objets_infos_extras/trunk/lang/paquet-objets_infos_extras_fr.php
+++ objets_infos_extras/trunk/lang/paquet-objets_infos_extras_fr.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/objets_infos_extras/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
@@ -10,5 +10,5 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// O
 	'objets_infos_extras_description' => 'Ajouter des informations extras à vos objets',
 	'objets_infos_extras_nom' => 'Infos extras pour objets',
-	'objets_infos_extras_slogan' => 'Des informations extras à vos objets',
+	'objets_infos_extras_slogan' => 'Des informations extras à vos objets'
 );
