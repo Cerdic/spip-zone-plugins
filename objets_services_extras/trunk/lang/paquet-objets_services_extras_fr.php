5f826f0b980d35973cd78f7b09c4a3576ddb3e7b
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-03-02 04:39:43 +0000

    [Salvatore] [source:_plugins_/objets_services_extras/trunk/lang/ paquet-objets_services_extras] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@114255 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- objets_services_extras/trunk/lang/paquet-objets_services_extras_fr.php
+++ objets_services_extras/trunk/lang/paquet-objets_services_extras_fr.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/objets_services_extras/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
@@ -10,5 +10,5 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// O
 	'objets_services_extras_description' => 'Gérer des services extras pour les objets',
 	'objets_services_extras_nom' => 'Services extras pour objets',
-	'objets_services_extras_slogan' => 'Des services extras pour vos objets',
+	'objets_services_extras_slogan' => 'Des services extras pour vos objets'
 );
