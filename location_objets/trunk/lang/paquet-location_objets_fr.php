393327ece892db95c9a585ecd3a63d3b55238096
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-03-02 04:39:17 +0000

    [Salvatore] [source:_plugins_/location_objets/trunk/lang/ paquet-location_objets] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@114244 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- location_objets/trunk/lang/paquet-location_objets_fr.php
+++ location_objets/trunk/lang/paquet-location_objets_fr.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/location_objets/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
@@ -8,7 +8,7 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// L
-	'location_objets_description' => 'Gère la location d\'objets',
-	'location_objets_nom' => 'Location d\'objets',
-	'location_objets_slogan' => 'Louer vos objets',
+	'location_objets_description' => 'Gère la location d’objets',
+	'location_objets_nom' => 'Location d’objets',
+	'location_objets_slogan' => 'Louer vos objets'
 );
