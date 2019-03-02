ec9e907fec3825fa54c6c966b2b0a9849995318d
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-03-02 04:39:32 +0000

    [Salvatore] [source:_plugins_/objets_disponibilites/trunk/lang/ paquet-objets_disponibilites] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@114249 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- objets_disponibilites/trunk/lang/paquet-objets_disponibilites_fr.php
+++ objets_disponibilites/trunk/lang/paquet-objets_disponibilites_fr.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/objets_disponibilites/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
@@ -10,5 +10,5 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// O
 	'objets_disponibilites_description' => 'Système de gestion de disponibilité de vos objets.',
 	'objets_disponibilites_nom' => 'Disponibilites objets',
-	'objets_disponibilites_slogan' => 'Gère la disponibilité d\'un objet',
+	'objets_disponibilites_slogan' => 'Gère la disponibilité d’un objet'
 );
