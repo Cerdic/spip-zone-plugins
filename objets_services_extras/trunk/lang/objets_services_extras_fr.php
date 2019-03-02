73515de01bcd7b21f6b814d27723a3b2fc676c96
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-03-02 04:39:41 +0000

    [Salvatore] [source:_plugins_/objets_services_extras/trunk/lang/ objets_services_extras] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@114254 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- objets_services_extras/trunk/lang/objets_services_extras_fr.php
+++ objets_services_extras/trunk/lang/objets_services_extras_fr.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/objets_services_extras/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
@@ -8,16 +8,14 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// C
-	'champ_chambres_objets' => 'Activer l’ajout d\'informations extras sur les contenus :',
+	'cfg_exemple' => 'Exemple',
+	'cfg_exemple_explication' => 'Explication de cet exemple',
+	'cfg_titre_parametrages' => 'Paramétrages',
+	'champ_chambres_objets' => 'Activer l’ajout d’informations extras sur les contenus :',
 
 	// O
 	'objets_services_extras_titre' => 'Services extras pour objets',
 
-	// C
-	'cfg_exemple' => 'Exemple',
-	'cfg_exemple_explication' => 'Explication de cet exemple',
-	'cfg_titre_parametrages' => 'Paramétrages',
-
 	// T
-	'titre_page_configurer_objets_services_extras' => 'Parametres services extras pour objets',
+	'titre_page_configurer_objets_services_extras' => 'Parametres services extras pour objets'
 );
