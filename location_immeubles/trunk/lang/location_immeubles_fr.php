f6d0a075d1000bf5ee6e3117981649417fb55e24
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-03-02 04:39:08 +0000

    [Salvatore] [source:_plugins_/location_immeubles/trunk/lang/ location_immeubles] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@114239 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- location_immeubles/trunk/lang/location_immeubles_fr.php
+++ location_immeubles/trunk/lang/location_immeubles_fr.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/location_immeubles/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
@@ -8,8 +8,8 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// C
-	'champ_personnes_label' => 'Personnes :',
+	'champ_personnes_label' => 'Personnes :',
 
 	// L
-	'location_immeubles_titre' => 'Location d\'immeubles',
+	'location_immeubles_titre' => 'Location d’immeubles'
 );
