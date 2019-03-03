d6412413efd6834c08b28bfc19f867db2b8175e4
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-03-03 04:38:35 +0000

    [Salvatore] [source:_plugins_/location_objets/trunk/lang/ location_objets] Export depuis http://trad.spip.net de la langue en
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@114288 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ location_objets/trunk/lang/location_objets_en.php
@@ -0,0 +1,63 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/location_objets?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cfg_titre_formulaire' => 'Form',
+	'cfg_titre_parametrages' => 'Settings',
+	'champ_location_extras_objets_label' => 'Activate objects to use as extra service:',
+
+	// E
+	'explication_statut_loue' => 'The status of the rental that determines if an object is rented.',
+
+	// I
+	'info_1_objet_loue' => 'One rented object',
+	'info_nb_objets_loues' => '@nb@ rented objects.',
+
+	// L
+	'label_date_paiement' => 'Payment date:',
+	'label_statut' => 'Status:',
+	'label_statut_defaut' => 'Default status:',
+	'label_statut_loue' => 'Rented status:',
+	'location_extras_objets_explication' => 'These services will be included in the rental form, and may be selected separately.',
+	'location_objets_titre' => 'Object rental',
+
+	// N
+	'notifications_activer_explication' => 'Send rental notifications?',
+	'notifications_activer_label' => 'Enable',
+	'notifications_cfg_titre' => 'Notifications',
+	'notifications_client_explication' => 'Send notifications to the customer?',
+	'notifications_client_label' => 'Customer',
+	'notifications_destinataire_explication' => 'Choose the recipient (s) of notifications',
+	'notifications_destinataire_label' => 'Recipients',
+	'notifications_expediteur_administrateur_label' => 'Choose an administrator:',
+	'notifications_expediteur_choix_administrateur' => 'an administrator',
+	'notifications_expediteur_choix_email' => 'an email',
+	'notifications_expediteur_choix_facteur' => 'same as Postman plugin',
+	'notifications_expediteur_choix_webmaster' => 'a webmaster',
+	'notifications_expediteur_email_label' => 'Enter an email:',
+	'notifications_expediteur_explication' => 'Choose the sender of the notifications for the seller and the buyer',
+	'notifications_expediteur_label' => 'Sender',
+	'notifications_expediteur_webmaster_label' => 'Choose a webmaster',
+	'notifications_parametres' => 'Notification settings',
+	'notifications_quand_explication' => 'Which change (s) of status triggers (s) the sending of a notification?',
+	'notifications_quand_label' => 'Trigger',
+	'notifications_vendeur_administrateur_label' => 'Choose one or more administrators :',
+	'notifications_vendeur_choix_administrateur' => 'administrators',
+	'notifications_vendeur_choix_email' => 'emails',
+	'notifications_vendeur_choix_webmaster' => 'webmasters',
+	'notifications_vendeur_email_explication' => 'Enter one or more emails separated by commas:',
+	'notifications_vendeur_email_label' => 'Email:',
+	'notifications_vendeur_label' => 'Seller',
+
+	// T
+	'titre_page_configurer_location_objets' => 'Objects rental',
+	'titre_tous' => 'All'
+);
