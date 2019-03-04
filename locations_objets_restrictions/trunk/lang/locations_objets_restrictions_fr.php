e961b9c230ad3a786aef4ecd57fc47f6d25be17e
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-03-04 04:39:12 +0000

    [Salvatore] [source:_plugins_/locations_objets_restrictions/trunk/lang/ locations_objets_restrictions] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@114322 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- locations_objets_restrictions/trunk/lang/locations_objets_restrictions_fr.php
+++ locations_objets_restrictions/trunk/lang/locations_objets_restrictions_fr.php
@@ -1,19 +1,19 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/locations_objets_restrictions/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-	// L
-	'locations_objets_restrictions_titre' => 'Locations d\'objets - restrictions',
-
 	// C
-	'champ_chambres_objets' => 'Activer l’ajout de restrictions sur les contenus :',
 	'cfg_titre_parametrages' => 'Paramétrages',
+	'champ_chambres_objets' => 'Activer l’ajout de restrictions sur les contenus :',
+
+	// L
+	'locations_objets_restrictions_titre' => 'Locations d’objets - restrictions',
 
 	// T
-	'titre_page_configurer_locations_objets_restrictions' => 'Locations d\'objets - restrictions',
+	'titre_page_configurer_locations_objets_restrictions' => 'Locations d’objets - restrictions'
 );
