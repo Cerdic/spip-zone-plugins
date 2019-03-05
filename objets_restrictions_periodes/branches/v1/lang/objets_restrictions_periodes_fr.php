bfa1f2cc24a053d899aea24d52121973576c177d
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-03-05 15:42:51 +0000

    v1.1.3 - traductions en anglais
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@114352 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- objets_restrictions_periodes/branches/v1/lang/objets_restrictions_periodes_fr.php
+++ objets_restrictions_periodes/branches/v1/lang/objets_restrictions_periodes_fr.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/objets_restrictions_periodes/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
@@ -8,21 +8,21 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// C
-	'champ_duree_label' => ' Durée :',
-	'champ_duree_minimale_label' => 'Durée minimale :',
-	'champ_jour_debut_label' => 'Jour début :',
-	'champ_jour_fin_label' => 'Jour fin :',
-	'champ_type_label' => 'Type :',
+	'champ_duree_label' => ' Durée :',
+	'champ_duree_minimale_label' => 'Durée minimale :',
+	'champ_jour_debut_label' => 'Jour début :',
+	'champ_jour_fin_label' => 'Jour fin :',
+	'champ_type_label' => 'Type :',
 	'choix_duree' => 'Durée',
 	'choix_jours' => 'Jours',
 
 	// E
 	'erreur_duree' => 'Durée minimale de @duree@ @entite_duree@',
-	'erreur_jours' => 'La @date@ doit être un @jour@ !',
+	'erreur_jours' => 'La @date@ doit être un @jour@ !',
 
 	// O
 	'objets_restrictions_periodes_titre' => 'Objets restrictions périodes',
 
 	// T
-	'titre_page_configurer_objets_restrictions_periodes' => 'Objets restrictions périodes',
+	'titre_page_configurer_objets_restrictions_periodes' => 'Objets restrictions périodes'
 );
