243bd7e0aa36b6bf75a69661bde872ed209bbcb7
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-03-05 04:39:34 +0000

    [Salvatore] [source:_plugins_/location_immeubles/trunk/lang/ paquet-location_immeubles] Export depuis http://trad.spip.net de la langue en
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@114343 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ location_immeubles/trunk/lang/paquet-location_immeubles_en.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-location_immeubles?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// L
+	'location_immeubles_description' => 'Plugin that assembles various third-party plugins to create a management system for the rental of buildings.',
+	'location_immeubles_nom' => 'Rental of buildings',
+	'location_immeubles_slogan' => 'Rent your buildings'
+);
