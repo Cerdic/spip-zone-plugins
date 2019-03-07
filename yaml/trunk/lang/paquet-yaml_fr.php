a24f64ca0a02bca02597bda438bdefd37d1c082b
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-03-07 20:31:25 +0000

    [Salvatore] [source:_plugins_/yaml/trunk/lang/ paquet-yaml] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@114381 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- yaml/trunk/lang/paquet-yaml_fr.php
+++ yaml/trunk/lang/paquet-yaml_fr.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/yaml/lang/
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/yaml/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
