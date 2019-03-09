0bfd8a4d8295741939177aef551f98c74950f361
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-03-09 17:34:38 +0000

    Fonction de service inutile
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@114411 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spiperipsum/trunk/cache/spiperipsum.php
+++ spiperipsum/trunk/cache/spiperipsum.php
@@ -35,26 +35,3 @@ function spiperipsum_cache_configurer($plugin) {
 
 	return $configuration;
 }
-
-
-/**
- * Complète la description canonique d'un cache.
- * Le plugin Spiper Ipsum...
- *
- * @param string $plugin
- *        Identifiant qui permet de distinguer le module appelant qui peut-être un plugin comme le noiZetier
- *        ou un script. Pour un plugin, le plus pertinent est d'utiliser le préfixe.
- * @param array  $cache
- *       Tableau identifiant le cache pour lequel on veut construire le nom.
- * @param string $fichier_cache
- *        Fichier cache désigné par son chemin complet.
- * @param array  $configuration
- *        Configuration complète des caches du plugin utilisateur lue à partir de la meta de stockage.
- *
- * @return array
- *         Description du cache complétée par un ensemble de données propres au plugin.
- */
-function spiperipsum_cache_completer($plugin, $cache, $fichier_cache, $configuration) {
-
-	return $cache;
-}
