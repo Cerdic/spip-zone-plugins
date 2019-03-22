e20ab26403980ccd93a5eb37397d15d6f99b0aa0
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-03-22 09:59:08 +0000

    Branch '/branches/v2' created from /trunk:113956
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@114600 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ traduction_articles_autrement/branches/v2/lang/paquet-taa_pt_br.php
@@ -0,0 +1,20 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-taa?lang_cible=pt_br
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// T
+	'taa_description' => 'Alteração da interface da área restrita do SPIP para dar ao usuário uma outra abordagem para a tradução das matérias, mantendo o sistema de tradução original do SPIP. Para poder usar a exibição compacta das listas de matérias (opção disponível via CFG), é necessário instalar o plugin [afficher_objets->https://files.spip.net/spip-zone/afficher_objets.zip].
+
+As listas compactas passam a funcionar igualmente em sites com multilinguismo por seção. Para isso, é necessário instalar o plugin [Tradução entre seções->https://files.spip.net/spip-zone/tradrub.zip]
+
+E para poder se beneficiar de um sistema de tradução semelhante a esteduções  plugin, instale também o plugin [Tradução de seções  alternativa->https://files.spip.net/spip-zone/traduction_rubriques_autrement.zip].  Os botões de tradução serão exibidos no alto da seção com a possibilidade de criar as traduções em falta clicando na aba cinza. O plugin tentará procurar a seção correspondente no setor do idioma, se a seção precedente estiver traduzida.',
+	'taa_nom' => 'Tradução de matérias alternativa',
+	'taa_slogan' => 'Torna a tradução das matérias mais fácil.'
+);
