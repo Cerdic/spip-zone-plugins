ffeebdd1b0b444767f2739c0756930a19bb766f2
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-04-02 16:42:21 +0000

    installe un système de secteur par langue
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@114736 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ secteur_langue/trunk/lang/paquet-secteur_langue_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// S
+	'secteur_langue_description' => 'Mise en place d\'un Spip multilingue avec un secteur par langue',
+	'secteur_langue_nom' => 'Secteur par langue',
+	'secteur_langue_slogan' => 'Spip multilingue avec un secteur par langue',
+);
