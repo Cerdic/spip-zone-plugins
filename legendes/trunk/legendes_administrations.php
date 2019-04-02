0f78c246c4e3f02a4194b3ce1a54d4449422ca35
bruno@eliaz.fr <bruno@eliaz.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-04-02 12:38:27 +0000

    update procédure install/désinstall
    utiliser declarer_tables_objets_sql, supprimer la config lors de la désinstallation, un peu de PSR
    
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@114713 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- legendes/trunk/legendes_administrations.php
+++ legendes/trunk/legendes_administrations.php
@@ -1,23 +1,20 @@
 <?php
+
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
-include_spip('inc/meta');
+function legendes_upgrade($nom_meta_base_version, $version_cible) {
+	$maj = array();
 
-function legendes_upgrade($nom_meta_base_version,$version_cible){
-	$current_version = "0.0";
-	if (	(!isset($GLOBALS['meta'][$nom_meta_base_version]))
-			|| (($current_version = $GLOBALS['meta'][$nom_meta_base_version])!=$version_cible)){
-		include_spip('base/legendes');
-		if ($current_version==0.0){
-			include_spip('base/create');
-			include_spip('base/abstract_sql');
-			creer_base();
-			ecrire_meta($nom_meta_base_version,$current_version=$version_cible,'non');
-		}
-	}
+	$maj['create'] = array(
+		array('maj_tables', array('spip_legendes'))
+	);
+
+	include_spip('base/upgrade');
+	maj_plugin($nom_meta_base_version, $version_cible, $maj);
 }
 
 function legendes_vider_tables($nom_meta_base_version) {
-	sql_drop_table("spip_legendes");
+	sql_drop_table('spip_legendes');
 	effacer_meta($nom_meta_base_version);
+	effacer_meta('legendes');
 }
