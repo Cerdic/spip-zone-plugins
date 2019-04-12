53be0221e64d7a3735880820c598956346a02c05
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-04-12 15:53:44 +0000

    v1.0.5
    - Un squelette pour l'information
    - Salvatore
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@114942 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- objets_infos_extras/branches/v1/lang/objets_infos_extras_fr.php
+++ objets_infos_extras/branches/v1/lang/objets_infos_extras_fr.php
@@ -1,19 +1,19 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/objets_infos_extras/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
+	// C
+	'cfg_titre_parametrages' => 'Paramétrages',
+	'champ_chambres_objets' => 'Activer l’ajout d’informations extras sur les contenus :',
+
 	// O
 	'objets_infos_extras_titre' => 'Infos extras pour objets',
 
-	// C
-	'champ_chambres_objets' => 'Activer l’ajout d\'informations extras sur les contenus :',
-	'cfg_titre_parametrages' => 'Paramétrages',
-
 	// T
-	'titre_page_configurer_objets_infos_extras' => 'Parametres infos extras pour objets',
+	'titre_page_configurer_objets_infos_extras' => 'Parametres infos extras pour objets'
 );
