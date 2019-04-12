d0483d5492d5a2fddfeb7934d4e0811984591d14
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-04-12 09:38:20 +0000

    orthographe
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@114935 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- saisies/trunk/lang/saisies_fr.php
+++ saisies/trunk/lang/saisies_fr.php
@@ -226,7 +226,7 @@ Vous trouverez la <a href="https://contrib.spip.net/5080" target="_blank">docume
 	'tout_selectionner' => 'Tout (dé)sélectionner',
 
 	// V
-	'verifier_valeurs_acceptables_explication' => 'Vérifier que la valeurs postées se trouve parmis celle autorisées lors de la définition des champs. Ne pas utilisez cette options si vous remplissez dynamiquement des champs dans vos squelettes ou grâce à javascript.',
+	'verifier_valeurs_acceptables_explication' => 'Vérifier que la valeur postée se trouve parmis celles autorisées lors de la définition des champs. Ne pas utiliser cette options si vous remplissez dynamiquement des champs dans vos squelettes ou les remplissez grâce à javascript.',
 	'verifier_valeurs_acceptables_label' => 'Vérifier les valeurs acceptables',
 	'vue_sans_reponse' => '<i>Sans réponse</i>',
 
