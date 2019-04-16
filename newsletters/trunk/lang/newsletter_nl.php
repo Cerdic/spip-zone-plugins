1b67e0bbee3b0503673212b03e82bda8bcb3024e
hanjo.spip@gmail.com <hanjo.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-04-16 03:39:38 +0000

    [Salvatore] [source:_plugins_/newsletters/trunk/lang/ newsletter] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@114991 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- newsletters/trunk/lang/newsletter_nl.php
+++ newsletters/trunk/lang/newsletter_nl.php
@@ -10,69 +10,69 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
-	'ajouter_lien_newsletter' => 'Voeg deze nieuwsbrief toe', # MODIF
+	'ajouter_lien_newsletter' => 'Voeg deze nieuwsbrief toe',
 
 	// E
-	'explication_baked' => 'Geavanceerd bewerken: Je kunt de HTML en de tekst van de nieuwsbrief rechtstreeks bewerken. Ze zal niet automatisch worden gegenereerd. Als je terugkeert naar het normale bewerken gaan de wijzigingen verloren.', # MODIF
-	'explication_baked_publie' => 'Deze nieuwsbrief is definitief: je kunt de HTML of tekst corrigeren, maar je kunt de redactie niet veranderen of het model regenereren.', # MODIF
-	'explication_html_page_vide' => 'Toont in de browser een andere versie dan de per e-mail verzonden versie. Laat leeg als je dezelfde versie in e-mail en browser wilt.', # MODIF
+	'explication_baked' => 'Geavanceerd bewerken: Je kunt de HTML en de tekst van de nieuwsbrief rechtstreeks bewerken. Ze zal niet automatisch worden gegenereerd. Als je terugkeert naar het normale bewerken gaan de wijzigingen verloren.',
+	'explication_baked_publie' => 'Deze nieuwsbrief is definitief: je kunt de HTML of tekst corrigeren, maar je kunt de redactie niet veranderen of het model regenereren.',
+	'explication_html_page_vide' => 'Toont in de browser een andere versie dan de per e-mail verzonden versie. Laat leeg als je dezelfde versie in e-mail en browser wilt.',
 
 	// I
-	'icone_creer_newsletter' => 'Een nieuwsbrief maken', # MODIF
-	'icone_dupliquer_newsletter' => 'Deze infobrief dupliceren', # MODIF
-	'icone_modifier_newsletter' => 'Bewerk deze nieuwsbrief', # MODIF
-	'info_1_mailsubscriber' => '1 geregistreerde', # MODIF
-	'info_1_newsletter' => '1 nieuwsbrief', # MODIF
-	'info_1_newsletter_prog' => '1 geprogrammeerde nieuwsbrief', # MODIF
-	'info_aucun_newsletter' => 'Geen nieuwsbrief', # MODIF
-	'info_email_inscrit_newletter' => 'Je bent ingeschreven om per mail nieuws te ontvangen van:', # MODIF
-	'info_email_not_displaying' => 'Wordt deze e-mail niet correct weergegeven?', # MODIF
-	'info_email_reception' => 'Deze mail werd verzonden aan:', # MODIF
-	'info_email_voir_en_ligne' => 'Bekijk deze nieuwsbrief in je browser', # MODIF
-	'info_nb_mailsubscribers' => '@nb@ geregistreerden', # MODIF
-	'info_nb_newsletters' => '@nb@ nieuwsbrieven', # MODIF
-	'info_nb_newsletters_prog' => '@nb@ Geprogrammeerde nieuwsbrieven', # MODIF
-	'info_newsletter_generer' => 'Genereerde Nieuwsbrief (HTML Version Text Version)', # MODIF
-	'info_newsletters_auteur' => 'De nieuwsbrieven van deze auteur', # MODIF
-	'info_optionnel' => '(optioneel)', # MODIF
-	'info_preview_version_html_email' => 'HTML Version e-mail', # MODIF
-	'info_preview_version_html_page' => 'HTML-versie online', # MODIF
-	'info_preview_version_html_page_noiframe' => '(weergave zonder iframe)', # MODIF
-	'info_preview_version_texte_email' => 'Tekst versie e-mail', # MODIF
-	'info_publier_pour_envoyer' => 'Publiceer deze nieuwsbrief voor verzending naar een lijst', # MODIF
-	'info_test_sujet' => 'TEST', # MODIF
+	'icone_creer_newsletter' => 'Een nieuwsbrief maken',
+	'icone_dupliquer_newsletter' => 'Deze infobrief dupliceren',
+	'icone_modifier_newsletter' => 'Bewerk deze nieuwsbrief',
+	'info_1_mailsubscriber' => '1 geregistreerde',
+	'info_1_newsletter' => '1 nieuwsbrief',
+	'info_1_newsletter_prog' => '1 geprogrammeerde nieuwsbrief',
+	'info_aucun_newsletter' => 'Geen nieuwsbrief',
+	'info_email_inscrit_newletter' => 'Je bent ingeschreven om per e-mail nieuws te ontvangen van:',
+	'info_email_not_displaying' => 'Wordt deze e-mail niet correct weergegeven?',
+	'info_email_reception' => 'Deze mail werd verzonden aan:',
+	'info_email_voir_en_ligne' => 'Bekijk deze nieuwsbrief in je browser',
+	'info_nb_mailsubscribers' => '@nb@ geregistreerden',
+	'info_nb_newsletters' => '@nb@ nieuwsbrieven',
+	'info_nb_newsletters_prog' => '@nb@ Geprogrammeerde nieuwsbrieven',
+	'info_newsletter_generer' => 'Nieuwsbrief maken (HTML en tekst versie)',
+	'info_newsletters_auteur' => 'De nieuwsbrieven van deze auteur',
+	'info_optionnel' => '(optioneel)',
+	'info_preview_version_html_email' => 'HTML e-mail versie',
+	'info_preview_version_html_page' => 'HTML online versie',
+	'info_preview_version_html_page_noiframe' => '(weergave zonder iframe)',
+	'info_preview_version_texte_email' => 'Tekst e-mail versie',
+	'info_publier_pour_envoyer' => 'Publiceer deze nieuwsbrief voor verzending naar een lijst',
+	'info_test_sujet' => 'TEST',
 
 	// L
-	'label_a_lire_sur' => 'Nieuwsbrief @site@', # MODIF
-	'label_baked_1' => 'Geavanceerde editie van de nieuwsbrief', # MODIF
-	'label_chapo' => 'Introductie', # MODIF
-	'label_date_redac' => 'Uitgegeven op', # MODIF
-	'label_html_email' => 'HTML-versie', # MODIF
-	'label_html_page' => 'HTML-versie weergegeven in de browser', # MODIF
-	'label_inscrit_newsletter' => 'Je bent ingeschreven voor de nieuwsbrief van @site@', # MODIF
-	'label_masquer_fond' => 'Verberg modellen van de nieuwsbrief:', # MODIF
-	'label_patron' => 'Model', # MODIF
-	'label_publie' => 'Gepubliceerd op:', # MODIF
-	'label_selection_articles' => 'Selectie van artikelen', # MODIF
-	'label_selection_edito' => 'Redactionele selectie (artikelen & rubrieken)', # MODIF
-	'label_selection_rubriques' => 'Selectie van rubrieken', # MODIF
-	'label_texte' => 'Tekst', # MODIF
-	'label_texte_email' => 'Tekst versie', # MODIF
-	'label_titre' => 'Titel', # MODIF
-	'legend_configuration_adresse_envoi' => 'Verzendadres voor de verzending van deze nieuwsbrief', # MODIF
-	'lien_lire_en_ligne' => 'Lees online', # MODIF
+	'label_a_lire_sur' => 'Nieuwsbrief @site@',
+	'label_baked_1' => 'Geavanceerde editie van de nieuwsbrief',
+	'label_chapo' => 'Introductie',
+	'label_date_redac' => 'Uitgegeven op',
+	'label_html_email' => 'HTML-versie',
+	'label_html_page' => 'HTML-versie weergegeven in de browser',
+	'label_inscrit_newsletter' => 'Je bent ingeschreven voor de nieuwsbrief van @site@',
+	'label_masquer_fond' => 'Verberg modellen van de nieuwsbrief:',
+	'label_patron' => 'Model',
+	'label_publie' => 'Gepubliceerd op:',
+	'label_selection_articles' => 'Selectie van artikelen',
+	'label_selection_edito' => 'Redactionele selectie (artikelen & rubrieken)',
+	'label_selection_rubriques' => 'Selectie van rubrieken',
+	'label_texte' => 'Tekst',
+	'label_texte_email' => 'Tekst versie',
+	'label_titre' => 'Titel',
+	'legend_configuration_adresse_envoi' => 'Verzendadres voor de verzending van deze nieuwsbrief',
+	'lien_lire_en_ligne' => 'Lees online',
 
 	// R
-	'retirer_lien_newsletter' => 'Verwijder deze nieuwsbrief', # MODIF
-	'retirer_tous_liens_newsletters' => 'Verwijder alle nieuwsbrieven', # MODIF
+	'retirer_lien_newsletter' => 'Verwijder deze nieuwsbrief',
+	'retirer_tous_liens_newsletters' => 'Verwijder alle nieuwsbrieven',
 
 	// T
-	'texte_ajouter_newsletter' => 'Voeg een nieuwsbrief toe', # MODIF
-	'texte_changer_statut_newsletter' => 'Deze nieuwsbrief is:', # MODIF
-	'texte_creer_associer_newsletter' => 'Maak en associëer een nieuwsbrief', # MODIF
-	'titre_langue_newsletter' => 'Taal van deze nieuwsbrief', # MODIF
-	'titre_logo_newsletter' => 'Logo van deze nieuwsbrief', # MODIF
-	'titre_newsletter' => 'Nieuwsbrief', # MODIF
-	'titre_newsletters' => 'Nieuwsbrieven', # MODIF
-	'titre_newsletters_rubrique' => 'Nieuwsbrieven van de rubriek' # MODIF
+	'texte_ajouter_newsletter' => 'Voeg een nieuwsbrief toe',
+	'texte_changer_statut_newsletter' => 'Deze nieuwsbrief is:',
+	'texte_creer_associer_newsletter' => 'Maak en associëer een nieuwsbrief',
+	'titre_langue_newsletter' => 'Taal van deze nieuwsbrief',
+	'titre_logo_newsletter' => 'Logo van deze nieuwsbrief',
+	'titre_newsletter' => 'Nieuwsbrief',
+	'titre_newsletters' => 'Nieuwsbrieven',
+	'titre_newsletters_rubrique' => 'Nieuwsbrieven van de rubriek'
 );
