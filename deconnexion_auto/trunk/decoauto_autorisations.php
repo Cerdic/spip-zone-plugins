22dc6cf3921cbb3e0dcef1d4b2c0c2b256a0381f
tofulm@gmail.com <tofulm@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-04-17 14:49:49 +0000

    Petit plugin qui permet de deconnecter un utilisateur au bout d'un certain temps. Ce temps est configurable dans le parametrage du plugin. Au login d'un auteur, on stocke en session cette valeur. cela permettra d'ajouter un temps personnalisé pour chaque auteur (évolution).
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@115013 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ deconnexion_auto/trunk/decoauto_autorisations.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Définit les autorisations du plugin Déconnexion Automatique
+ *
+ * @plugin     Déconnexion Automatique
+ * @copyright  2019
+ * @author     tofulm
+ * @licence    GNU/GPL
+ * @package    SPIP\Decoauto\Autorisations
+ */
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+
+/**
+ * Fonction d'appel pour le pipeline
+ * @pipeline autoriser */
+function decoauto_autoriser() {
+}
---
+++ deconnexion_auto/trunk/lang/paquet-decoauto_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// D
+	'decoauto_description' => '',
+	'decoauto_nom' => 'Déconnexion Automatique',
+	'decoauto_slogan' => '',
+);
