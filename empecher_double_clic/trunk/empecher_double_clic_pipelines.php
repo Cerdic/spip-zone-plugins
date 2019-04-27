86d2105c06226f98d508ddbf850b737ae8e59f0f
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-04-27 16:51:13 +0000

    inserer automatiquement un js (merci nicod) après les formulaires
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@115073 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ empecher_double_clic/trunk/empecher_double_clic_pipelines.php
@@ -0,0 +1,15 @@
+<?php
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+/**
+ * Insérer automatiquement l'appel au js dans les formulaires
+ * @param array $flux
+ * @return array $flux modifié
+ **/
+function empecher_double_clic_formulaire_fond($flux) {
+	$flux['data'] .= "<script type='text/javascript' src='".find_in_path('js/empecher_double_clic.js')."'></script>";
+	return $flux;
+}
---
+++ empecher_double_clic/trunk/js/empecher_double_clic.js
@@ -0,0 +1,3 @@
+$('form').submit(function(event) {
+  $(this).find('input[type="submit"], button[type="submit"]').addClass('js-sending').attr('disabled', 'disabled');
+});
