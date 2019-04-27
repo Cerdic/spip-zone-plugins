a2fd884e2fb2ac6f65b8ac638daf01807163522d
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-04-27 16:58:39 +0000

    ne pas executer le code avant la fin du chargment
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@115076 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- empecher_double_clic/trunk/js/empecher_double_clic.js
+++ empecher_double_clic/trunk/js/empecher_double_clic.js
@@ -1,3 +1,5 @@
-$('form').submit(function(event) {
-  $(this).find('input[type="submit"], button[type="submit"]').addClass('js-sending').attr('disabled', 'disabled');
+$(function() {
+	$('form').submit(function(event) {
+		$(this).find('input[type="submit"], button[type="submit"]').addClass('js-sending').attr('disabled', 'disabled');
+	});
 });
