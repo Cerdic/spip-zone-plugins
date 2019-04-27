95e75e9764fd5aeec1a6d40ce1ddd54fdf02cff5
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-04-27 15:42:51 +0000

    On essaye à nouveau de faire apparaitre le slogan.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@115065 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- uploadify/trunk/lang/paquet-uploadify_fr.php
+++ uploadify/trunk/lang/paquet-uploadify_fr.php
@@ -6,7 +6,6 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// U
 	'uploadify_description' => 'Ce plugin permet aux rédacteurs de sélectionner et télécharger plusieurs fichiers en une seule fois en s\'affranchissant également de la limite de taille imposée par les serveurs (en général 2 ou 3 Mo).',
-	'uploadify_slogan' => 'Télécharger des fichiers par lots'
+	'uploadify_slogan' => 'Télécharger des fichiers par lots',
 
 );
-
