a706ab1ba5c7ed2b66618532be1ff1aebc349996
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-04-27 16:48:20 +0000

    Ajout d'un slogan et d'une description. Ce plugin pourrait être zippé non ?
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@115072 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ updateheaders/lang/paquet-updateheaders_fr.php
@@ -0,0 +1,11 @@
+<?php
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// U
+	'updateheaders_description' => 'Il arrive que pour des raisons de sécurité, on ait envie ou besoin de modifier certaines en-têtes HTTP transmises par le serveur. Ce plugin a pour but de faciliter ces modifications.',
+	'updateheaders_slogan' => 'Masquer les en-têtes HTTP '
+);
