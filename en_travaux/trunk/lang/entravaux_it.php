dd8a57bc1439b41ea527421a632787188bb12940
alm82@live.it <alm82@live.it@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-04-30 03:40:13 +0000

    [Salvatore] [source:_plugins_/en_travaux/trunk/lang/ entravaux] Export depuis http://trad.spip.net de la langue it
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@115090 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- en_travaux/trunk/lang/entravaux_it.php
+++ en_travaux/trunk/lang/entravaux_it.php
@@ -14,16 +14,23 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// E
 	'en_travaux' => 'In manutenzione',
-	'erreur_droit' => 'Vous devez être Webmestre pour utiliser le plugin En Travaux !',
+	'erreur_droit' => 'Devi essere Webmaster per poter usare il plugin "In Manutenzione"!',
 
 	// I
+	'info_disallow_robot' => 'Il tuo sito è protetto contro i robot ed i motori di ricerca durante la manutenzione.',
+	'info_maintenance_en_cours' => 'Il sito è in manutenzione! Solo i Webmaster del sito possono vedere il sito pubblico ed accedere all’ area privata.',
 	'info_message' => 'Questa pagina consente di visualizzare un messaggio temporaneo su tutte le pagine del sito durante una sessione di manutenzione.',
-	'info_travaux_texte' => 'Questo sito è stato in manutenzione.
-_ Torna più tardi...',
+	'info_travaux_texte' => 'Questo sito è in manutenzione, torna più tardi...',
+
+	// L
+	'label_disallow_robots' => 'Proteggi il sito da robot e motori di ricerca',
 
 	// M
 	'message_temporaire' => 'Il vostro messaggio di manutenzione:',
 
 	// P
-	'parametrage_page_travaux' => 'Configurazione della pagina di manutenzione'
+	'parametrage_page_travaux' => 'Configurazione della pagina di manutenzione',
+
+	// T
+	'texte_lien_publier' => 'Rendi pubblico il sito'
 );
