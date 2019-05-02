8c18ed761529acab34d0c9110655440cea6cea7a
alm82@live.it <alm82@live.it@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-05-02 03:40:28 +0000

    [Salvatore] [source:_plugins_/ancres_douces/lang/ paquet-ancresdouces] Export depuis http://trad.spip.net de la langue it
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@115158 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ ancres_douces/lang/paquet-ancresdouces_it.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-ancresdouces?lang_cible=it
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'ancresdouces_description' => 'Questo plug-in consente uno scorrimento continuo della pagina quando il visitatore fa clic su un collegamento che punta a un’ancora dell’articolo da leggere.',
+	'ancresdouces_nom' => 'Ancres Douces',
+	'ancresdouces_slogan' => 'Collegamenti mobidi'
+);
