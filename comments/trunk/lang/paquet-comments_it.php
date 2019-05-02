a745240b3ff6eb33d4adcecffb2a72d376d805cf
alm82@live.it <alm82@live.it@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-05-02 03:40:35 +0000

    [Salvatore] [source:_plugins_/comments/trunk/lang/ paquet-comments] Export depuis http://trad.spip.net de la langue it
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@115160 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ comments/trunk/lang/paquet-comments_it.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-comments?lang_cible=it
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'comments_description' => 'ATTENZIONE, VERSIONE IN SVILUPPO PER SPIP 3!!!<br /> Mostrare messaggi in lista, in modo da commentare i blog, con una forma semplificata. Commenti microformattati, nomenclatura omogenea.',
+	'comments_slogan' => 'I commenti, semplicemente'
+);
