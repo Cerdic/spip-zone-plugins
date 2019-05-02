525026ebcc4ace7a46a7caa17f5e5edfb2922320
alm82@live.it <alm82@live.it@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-05-02 03:41:23 +0000

    [Salvatore] [source:_plugins_/opensearch/lang/ paquet-opensearch] Export depuis http://trad.spip.net de la langue it
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@115169 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ opensearch/lang/paquet-opensearch_it.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-opensearch?lang_cible=it
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// O
+	'opensearch_description' => 'Plugin che consente ai visitatori del tuo sito di aggiungere un motore di ricerca personalizzato al proprio browser.',
+	'opensearch_slogan' => 'Il tuo sito come motore di ricerca'
+);
