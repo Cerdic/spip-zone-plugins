cbe495fd8f2d05980fd317e7a1a76038a27c429e
alm82@live.it <alm82@live.it@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-05-02 03:41:44 +0000

    [Salvatore] [source:_plugins_/todo/trunk/lang/ paquet-todo] Export depuis http://trad.spip.net de la langue it
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@115173 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ todo/trunk/lang/paquet-todo_it.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-todo?lang_cible=it
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// T
+	'todo_description' => 'Aggiunge nuove scorciatoie tipografiche per descrivere facilmente gli elenchi di cose da fare nel contenuto SPIP. Questa versione 2 estende l’elenco degli stati disponibili e consente la qualificazione delle attività per priorità, etichette e informazioni tipizzate quali date di inizio e fine o numeri di commit e di versione.',
+	'todo_slogan' => 'Elenca rapidamente le cose da fare'
+);
