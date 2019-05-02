7e55f9788b736c381e1c29dfa60681a6ce74afbd
alm82@live.it <alm82@live.it@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-05-02 03:41:49 +0000

    [Salvatore] [source:_plugins_/zen-garden/trunk/lang/ zengarden] Export depuis http://trad.spip.net de la langue it
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@115174 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ zen-garden/trunk/lang/zengarden_it.php
@@ -0,0 +1,37 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/zengarden?lang_cible=it
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'apercevoir_ce_theme' => 'Vedi',
+
+	// B
+	'bandeau_personalisable' => 'E’ possibile personalizzare la copertina di questo tema',
+
+	// C
+	'choisir_ce_theme' => 'Scegliere',
+	'choix_theme' => 'Scegli il tuo tema',
+
+	// D
+	'desactiver_ce_theme' => 'Disattiva e torna allo stile predefinito',
+
+	// I
+	'info_page' => 'Puoi scegliere un tema proposto o semplicemente vederne l’anteprima senza modificare l’aspetto del sito per i tuoi visitatori',
+	'intitule_compatiblite_squelette' => 'Compatibile solo con temi',
+	'intitule_version' => 'versione',
+
+	// S
+	'switcher_activer' => 'Attiva il tema per il sito pubblico',
+	'switcher_desactiver' => 'Disattiva lo schema',
+
+	// T
+	'theme_actif' => 'Questo tema è attualmente utilizzato',
+	'themes' => 'Temi'
+);
