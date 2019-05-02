0aa3e868ab20d7e7c288d6a5f838782b0d060efe
alm82@live.it <alm82@live.it@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-05-02 03:40:47 +0000

    [Salvatore] [source:_plugins_/fulltext/trunk/lang/ paquet-fulltext] Export depuis http://trad.spip.net de la langue it
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@115163 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ fulltext/trunk/lang/paquet-fulltext_it.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-fulltext?lang_cible=it
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// F
+	'fulltext_slogan' => 'Indicizzazione FULLTEXT per velocizzare le ricerche'
+);
