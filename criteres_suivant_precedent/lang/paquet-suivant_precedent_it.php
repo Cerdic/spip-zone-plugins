324e71dcd22e19f255fce162bd81a2c8fc5dce43
alm82@live.it <alm82@live.it@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-05-02 03:40:38 +0000

    [Salvatore] [source:_plugins_/criteres_suivant_precedent/lang/ paquet-suivant_precedent] Export depuis http://trad.spip.net de la langue it
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@115161 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ criteres_suivant_precedent/lang/paquet-suivant_precedent_it.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-suivant_precedent?lang_cible=it
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// S
+	'suivant_precedent_description' => 'Questo plugin è un esperimento per trovare, in un dato ciclo, l’elemento successivo o l’elemento precedente. Questo utilizzo è utile soprattutto per creare navigazioni per argomento o tema (parole chiave).',
+	'suivant_precedent_nom' => 'Criteri Avanti / Indietro',
+	'suivant_precedent_slogan' => 'Aggiunge criteri per ciclo <code>{suivant}</code> e <code>{precedent}</code> (avanti e indietro)'
+);
