4f5f98838f05fc194f55b9150ecebc072a1036d2
alm82@live.it <alm82@live.it@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-05-02 03:41:32 +0000

    [Salvatore] [source:_plugins_/selecteur_generique/trunk/lang/ paquet-selecteurgenerique] Export depuis http://trad.spip.net de la langue it
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@115170 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ selecteur_generique/trunk/lang/paquet-selecteurgenerique_it.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-selecteurgenerique?lang_cible=it
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// S
+	'selecteurgenerique_description' => 'Sostituisce i selettori (autori, parole, argomenti) con qualcosa di più leggero e veloce, in grado di elaborare quanti più dati possibile.',
+	'selecteurgenerique_nom' => 'Selettore generico',
+	'selecteurgenerique_slogan' => 'Miglioramento dei selettori dell’area privata'
+);
