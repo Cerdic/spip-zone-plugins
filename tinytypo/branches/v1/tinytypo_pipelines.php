2e42dcca55c4f183069d15396bfd18c531873a51
tetue@rezo.net <tetue@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-05-03 23:40:04 +0000

    Ce plugin a déménagé : https://github.com/tetue/tinytypo-spip
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@115231 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- tinytypo/branches/v1/tinytypo_pipelines.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function tinytypo_insert_head_css($flux) {
-	$flux .= '<link rel="stylesheet" href="'.find_in_path('css/tinytypo.css').'" type="text/css" media="all" />';
-	return $flux;
-}
-
-?>
