477cf6028d6272c744f2d48aa4d8fa4c2f32670f
yannx.spip@hotmail.fr <yannx.spip@hotmail.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-05-03 22:39:10 +0000

    et une petite précision dans la description
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@115228 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- recherche_mots_cles/lang/paquet-recherche_sti_fr.php
+++ recherche_mots_cles/lang/paquet-recherche_sti_fr.php
@@ -3,7 +3,7 @@
     $GLOBALS[$GLOBALS['idx_lang']] = array(
             'recherche_sti_nom' => "Outil de recherche par mots clés",
             'recherche_sti_slogan' => "Plugin d'aide pour la recherche d'articles",
-            'recherche_sti_description' => "Permet de rechercher des articles à partir,
-                                                            d'un groupe de mots clés pré-sélectionnés.",
+            'recherche_sti_description' => "Le modèle permet de rechercher des articles à partir,
+                                                 de mots clés de groupes pré-sélectionnés.",
     );
 ?>
