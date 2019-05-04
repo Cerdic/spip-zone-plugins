96f9a8a77c10bfdb941a46b83930aee92ecb7048
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-05-04 03:40:15 +0000

    [Salvatore] [source:_plugins_/periodes/trunk/lang/ paquet-periodes] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@115239 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- periodes/trunk/lang/paquet-periodes_fr.php
+++ periodes/trunk/lang/paquet-periodes_fr.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/periodes/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
@@ -10,5 +10,5 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// P
 	'periodes_description' => 'Permet de définir de périodes',
 	'periodes_nom' => 'Périodes',
-	'periodes_slogan' => 'Gestion de périodes',
+	'periodes_slogan' => 'Gestion de périodes'
 );
