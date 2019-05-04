2e0333779bd4c410f3517e86fb053112c2c1d528
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-05-04 03:40:23 +0000

    [Salvatore] [source:_plugins_/prix_objets_periodes/trunk/lang/ paquet-prix_objets_periodes] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@115242 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- prix_objets_periodes/trunk/lang/paquet-prix_objets_periodes_fr.php
+++ prix_objets_periodes/trunk/lang/paquet-prix_objets_periodes_fr.php
@@ -1,14 +1,14 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/prix_objets_periodes/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) {
-  return;
+	return;
 }
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-  // P
-  'prix_objets_periodes_description' => 'Adapter les prix selon la/les date/s choisis',
-  'prix_objets_periodes_nom' => 'Prix objets par périodes',
-  'prix_objets_periodes_slogan' => 'Des prix selon la date',
+	// P
+	'prix_objets_periodes_description' => 'Adapter les prix selon la/les date/s choisis',
+	'prix_objets_periodes_nom' => 'Prix objets par périodes',
+	'prix_objets_periodes_slogan' => 'Des prix selon la date'
 );
