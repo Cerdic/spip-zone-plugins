73b34dede018dd79e89ae05437c02eb31f4fe6c8
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-05-04 03:40:21 +0000

    [Salvatore] [source:_plugins_/prix_objets_periodes/trunk/lang/ prix_objets_periodes] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@115241 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- prix_objets_periodes/trunk/lang/prix_objets_periodes_fr.php
+++ prix_objets_periodes/trunk/lang/prix_objets_periodes_fr.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/prix_objets_periodes/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
@@ -11,5 +11,5 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'prix_objets_periodes_titre' => 'Prix objets par périodes',
 
 	// T
-	'titre_page_configurer_prix_objets_periodes' => 'Paramètres Prix objets par périodes',
+	'titre_page_configurer_prix_objets_periodes' => 'Paramètres Prix objets par périodes'
 );
