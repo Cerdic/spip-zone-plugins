205371e09fa20f8e8acd5ae96433d02e238995de
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-05-04 03:40:20 +0000

    [Salvatore] [source:_plugins_/prix_objets_periodes/trunk/lang/ po_periode] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@115240 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- prix_objets_periodes/trunk/lang/po_periode_fr.php
+++ prix_objets_periodes/trunk/lang/po_periode_fr.php
@@ -1,10 +1,9 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/prix_objets_periodes/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
-
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
@@ -12,6 +11,6 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'ajouter_lien_po_periode' => 'Ajouter cette période de prix',
 
 	// C
-	'champ_criteres_label' => 'Critères de sélection : ',
-	'champ_id_prix_extension_po_periode' => 'Choisissez une période pour ce prix :',
+	'champ_criteres_label' => 'Critères de sélection : ',
+	'champ_id_prix_extension_po_periode' => 'Choisissez une période pour ce prix :'
 );
