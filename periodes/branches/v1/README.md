10e22762a916643450d60d2bf4c07843afe1446d
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-05-05 06:17:42 +0000

    - salvatore - actualisation lien vers doc sur contrib
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@115250 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- periodes/branches/v1/README.md
+++ periodes/branches/v1/README.md
@@ -1,2 +1,33 @@
-# periodes
+# Périodes
 Gestion de périodes
+
+## Dépendances
+- [Saisies](https://plugins.spip.net/saisies.html)
+- [Dates outils](https://plugins.spip.net/dates_outils.html)
+
+## Édition
+Définissez vos périodes sous Édition/Périodes
+
+Choisissez d'abord le type de période :
+- Dates
+- Jour de semaine
+
+Puis définissez si le prix doit s'appliquer quand:
+- il y a `coïncidence` entre les dates soumises et celle de la période définie.
+- les dates soumises sont `exclues` de la période définie.
+
+Ensuite déterminez la période (date début/ fin ou bien jour début/fin)
+
+### Dates
+Choisissez si vous voulez introduire la date complète ou incomplète.
+
+Si incomplète - lors de vérification si une date donnée tombe dans la période choisie, les bouts incomplets de la période (jour, mois ou année) seront remplacés par le correspondant de la date donnée. Si vous voulez par exemple fixer une période égale pour chaque année, il vous suffit de ne pas définir l'année.
+
+### Jour semaine
+Sélectionnez le jour de la semaine désiré
+
+## Utilisation
+
+Ce plugin est utilisé par les plugins suivants :
+- [Prix objets par périodes](https://plugins.spip.net/prix_objets_periodes.html)
+- [Objets restrictions périodes](https://plugins.spip.net/objets_restrictions_periodes.html)
--- periodes/branches/v1/lang/periode_fr.php
+++ periodes/branches/v1/lang/periode_fr.php
@@ -1,10 +1,9 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/periodes/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
-
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
@@ -12,20 +11,20 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'ajouter_lien_periode' => 'Ajouter cette période',
 
 	// C
-	'champ_annee_label' => 'Année :',
-	'champ_criteres_label' => 'Criteres :',
+	'champ_annee_label' => 'Année :',
+	'champ_criteres_label' => 'Criteres :',
 	'champ_date_complete_label' => 'Date complète',
-	'champ_descriptif_label' => 'Descriptif :',
-	'champ_jour_debut_label' => 'Jour debut :',
-	'champ_jour_fin_label' => 'Jour fin :',
-	'champ_jour_label' => 'Jour :',
-	'champ_jour_nombre_label' => 'Jour nombre :',
-	'champ_mois_label' => 'Mois :',
-	'champ_operateur_2_label' => 'Operateur 2 :',
-	'champ_operateur_label' => 'Operateur :',
-	'champ_periode_label' => 'Période :',
+	'champ_descriptif_label' => 'Descriptif :',
+	'champ_jour_debut_label' => 'Jour debut :',
+	'champ_jour_fin_label' => 'Jour fin :',
+	'champ_jour_label' => 'Jour :',
+	'champ_jour_nombre_label' => 'Jour nombre :',
+	'champ_mois_label' => 'Mois :',
+	'champ_operateur_2_label' => 'Operateur 2 :',
+	'champ_operateur_label' => 'Operateur :',
+	'champ_periode_label' => 'Période :',
 	'champ_titre_label' => 'Titre',
-	'champ_type_label' => 'Type :',
+	'champ_type_label' => 'Type :',
 	'choix_coincide_label' => 'Coïncidence (Période contient au moins une date du contexte)',
 	'choix_exclu_label' => 'Exclues (Période ne contient aucune date du contexte)',
 	'choix_specifique_label' => 'Choix spécifique',
@@ -34,7 +33,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// E
 	'erreur_dates_non_completes' => 'Sélectionnez au moin un jour, mois ou année sous "Date_début" ou "Date fin".',
 	'erreur_dates_un_minimum' => 'Il faut au moins un date début ou fin.',
-	'explication_date_complete_label' => 'Si non, permet de chosir jour, mois et année séparément. Util par example pour définir une période indépendament de l\'année',
+	'explication_date_complete_label' => 'Si non, permet de chosir jour, mois et année séparément. Util par example pour définir une période indépendament de l’année',
 
 	// I
 	'icone_creer_periode' => 'Créer une période',
@@ -54,10 +53,10 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// T
 	'texte_ajouter_periode' => 'Ajouter une période',
 	'texte_changer_statut_periode' => 'Cette période est :',
-	'titre_choix_coincide' => 'Coïncidence',
-	'titre_choix_exclu' => 'Exclues',
 	'texte_creer_associer_periode' => 'Créer et associer une période',
 	'texte_definir_comme_traduction_periode' => 'Cette période est une traduction de la période numéro :',
+	'titre_choix_coincide' => 'Coïncidence',
+	'titre_choix_exclu' => 'Exclues',
 	'titre_langue_periode' => 'Langue de cette période',
 	'titre_logo_periode' => 'Logo de cette période',
 	'titre_objets_lies_periode' => 'Liés à cette période',
@@ -65,6 +64,6 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'titre_periodes' => 'Périodes',
 	'titre_periodes_rubrique' => 'Périodes de la rubrique',
 	'type_date' => 'Dates',
-	'type_jour_semaine' => 'Jour de semaine',
 	'type_jour_nombre' => 'Nombre de jour',
+	'type_jour_semaine' => 'Jour de semaine'
 );
