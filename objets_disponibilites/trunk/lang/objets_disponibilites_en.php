6e6129a1964eb426b603e3b8d04c3c13e8fa09c4
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-05-10 14:04:04 +0000

    Nettoyage chaînes de langues
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@115313 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ objets_disponibilites/trunk/lang/objets_disponibilites_en.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/objets_disponibilites/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+
+	'champ_disponibilites_objets' => 'Enable adding availability on content:',
+
+);
