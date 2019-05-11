99b3b14fe6aa5ab7de5e6b4e0889d6d63a62ad5e
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-05-11 07:08:32 +0000

    v1.1.4 : traductions en anglais
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@115340 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ prix_objets_periodes/branches/v1/lang/po_periode_en.php
@@ -0,0 +1,18 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/po_periode?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'ajouter_lien_po_periode' => 'Add this price period',
+
+	// C
+	'champ_criteres_label' => 'Selection criteria :',
+	'champ_id_prix_extension_po_periode' => 'Choose a period for this price:'
+);
--- prix_objets_periodes/branches/v1/lang/prix_objets_periodes_fr.php
+++ prix_objets_periodes/branches/v1/lang/prix_objets_periodes_fr.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/prix_objets_periodes/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
@@ -8,8 +8,8 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// P
-	'prix_objets_periodes_titre' => 'Prix objets par périodes',
+	'prix_objets_periodes_titre' => 'Périodes de prix',
 
 	// T
-	'titre_page_configurer_prix_objets_periodes' => 'Paramètres Prix objets par périodes',
+	'titre_page_configurer_prix_objets_periodes' => 'Paramètres de périodes de prix'
 );
