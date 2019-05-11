b9724fda833e8fee6a9809e6c87be6078b40b7e3
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-05-11 03:41:45 +0000

    [Salvatore] [source:_plugins_/prix_objets_periodes/trunk/lang/ paquet-prix_objets_periodes] Export depuis http://trad.spip.net de la langue en
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@115330 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ prix_objets_periodes/trunk/lang/paquet-prix_objets_periodes_en.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-prix_objets_periodes?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// P
+	'prix_objets_periodes_description' => 'Adapt prices according to a chosen  period of dates', # MODIF
+	'prix_objets_periodes_nom' => 'Price periods', # MODIF
+	'prix_objets_periodes_slogan' => 'Prices according to the date'
+);
