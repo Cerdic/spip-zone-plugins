b112cc6a0ac44e207dcfe18d7c895923393c6733
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-05-11 03:41:24 +0000

    [Salvatore] [source:_plugins_/objets_disponibilites/trunk/lang/ disponibilite_date] Export depuis http://trad.spip.net de la langue en
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@115321 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ objets_disponibilites/trunk/lang/disponibilite_date_en.php
@@ -0,0 +1,41 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/disponibilite_date?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'ajouter_lien_disponibilite_date' => 'Add this period of available dates',
+
+	// C
+	'champ_disponible_label' => 'Available',
+	'confirmer_supprimer_disponibilite_date' => 'Do you confirm the deletion of this period of available dates?',
+
+	// I
+	'icone_creer_disponibilite_date' => 'Create a period of available dates',
+	'icone_modifier_disponibilite_date' => 'Edit this period of available dates',
+	'info_1_disponibilite_date' => 'A period of available dates',
+	'info_aucun_disponibilite_date' => 'No period of available dates',
+	'info_nb_disponibilite_dates' => '@nb@ periods of available dates',
+
+	// R
+	'retirer_lien_disponibilite_date' => 'Remove this period of available dates',
+	'retirer_tous_liens_disponibilite_dates' => 'Remove all available date periods',
+
+	// S
+	'supprimer_disponibilite_date' => 'Delete this period of available dates',
+
+	// T
+	'texte_ajouter_disponibilite_date' => 'Add a period of available dates',
+	'texte_changer_statut_disponibilite_date' => 'This period of available dates is:',
+	'texte_creer_associer_disponibilite_date' => 'Create and associate a period of available dates',
+	'titre_disponibilite_date' => 'Period of available dates',
+	'titre_disponibilite_dates' => 'Periods of available dates',
+	'titre_logo_disponibilite_date' => 'Logo of this period of available dates',
+	'titre_objets_lies_disponibilite_date' => 'Linked to this period of available dates'
+);
