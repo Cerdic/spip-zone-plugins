be0455658ed8516aad37166fd762a9c5057b8198
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-05-11 06:59:34 +0000

    v1.1.5 : traductions en anglais
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@115338 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ periodes/branches/v1/lang/periode_en.php
@@ -0,0 +1,75 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/periode?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'ajouter_lien_periode' => 'Add this period',
+
+	// C
+	'champ_annee_label' => 'Year:',
+	'champ_criteres_label' => 'Criteria:',
+	'champ_date_complete_label' => 'Full date',
+	'champ_jour_debut_label' => 'Beginning day:',
+	'champ_jour_fin_label' => 'End day:',
+	'champ_jour_label' => 'Day:',
+	'champ_jour_nombre_label' => 'Day number:',
+	'champ_mois_label' => 'Month:',
+	'champ_operateur_2_label' => 'Operator 2:',
+	'champ_operateur_label' => 'Operator:',
+	'champ_periode_label' => 'Period:',
+	'champ_type_label' => 'Type:',
+	'choix_coincide_label' => 'Coincidence (Period contains at least one context date)',
+	'choix_exclu_label' => 'Excluded (Period contains no context date)',
+	'choix_specifique_label' => 'Specific choice',
+	'confirmer_supprimer_periode' => '48/5000
+Do you confirm the deletion of this period?',
+
+	// E
+	'erreur_dates_non_completes' => '
+Meintest du: Sélectionnez au moins un jour, mois ou année sous "Date_début" ou "Date fin".
+76/5000
+Select at least one day, month, or year under "Beginning day" or "End date".', # MODIF
+	'erreur_dates_un_minimum' => 'It takes at least a start or end date.',
+	'explication_date_complete_label' => 'If not, allow to choose day, month and year separately. Use for example to define a period independently of the year', # MODIF
+
+	// I
+	'icone_creer_periode' => 'Create a period',
+	'icone_modifier_periode' => 'Edit this period',
+	'info_1_periode' => 'A period',
+	'info_aucun_periode' => 'No period',
+	'info_nb_periodes' => '@nb@ periods',
+	'info_periodes_auteur' => 'The periods of this author',
+
+	// R
+	'retirer_lien_periode' => 'Remove this period',
+	'retirer_tous_liens_periodes' => '
+27/5000
+Remove all periods',
+
+	// S
+	'supprimer_periode' => 'Delete this period',
+
+	// T
+	'texte_ajouter_periode' => 'Add a period',
+	'texte_changer_statut_periode' => 'This period is:',
+	'texte_creer_associer_periode' => 'Create and associate a period',
+	'texte_definir_comme_traduction_periode' => 'This period is a translation of the period number:',
+	'titre_choix_coincide' => 'Coincidence',
+	'titre_choix_exclu' => 'Excluded', # MODIF
+	'titre_langue_periode' => 'Language of this period',
+	'titre_logo_periode' => 'Logo of this period',
+	'titre_objets_lies_periode' => 'Linked to this period',
+	'titre_periode' => 'Period',
+	'titre_periodes' => 'Periods',
+	'titre_periodes_rubrique' => 'Periods of the section',
+	'type_date' => 'Dates',
+	'type_jour_nombre' => 'Number of days',
+	'type_jour_semaine' => 'Weekday'
+);
