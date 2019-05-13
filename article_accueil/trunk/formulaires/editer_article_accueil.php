cb3329e188b86259cdc507060fc0a6204d10bfba
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-05-13 18:34:18 +0000

    Changement de look du formulaire : on affiche l'article choisi dans une table pour y inclure simplement la puce de statut et l'id. On revoit le formulaire en radio boutons afin d'y visualiser aussi la puce de statut. Ajout de deux options accessibles par le pipeline formulaire_charger(): - possibilité de changer la liste des statuts possibles - possibilité de préciser une condition supplémentaire en utilisant le critère where. Dans le charger remplir la variable '_where'.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@115367 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- article_accueil/trunk/formulaires/editer_article_accueil.php
+++ article_accueil/trunk/formulaires/editer_article_accueil.php
@@ -18,14 +18,23 @@ if (!defined('_ECRIRE_INC_VERSION')) {
  * @return array
  */
 function formulaires_editer_article_accueil_charger($id_rubrique) {
+
 	$valeurs = array();
+	
+	// On passe au formulaire l'id de la rubrique, la liste des statuts d'article autorisés
+	// et une condition where qui est initialisée à '' par défaut.
+	// Ainsi, il est possible à un plugin de modifier la liste des statuts et le where pour
+	// influer sur le sélecteur d'articles.
 	$valeurs['id_rubrique'] = $id_rubrique;
-	include_spip('base/abstract_sql');
-	$valeurs['id_article_accueil'] = sql_getfetsel(
-		'id_article_accueil',
-		'spip_rubriques',
-		'id_rubrique='.intval($id_rubrique)
-	);
+	$valeurs['_statuts'] = array('prepa', 'prop', 'publie');
+	$valeurs['_where'] = '';
+
+	// On détermine si un article est déjà sélectionné ou pas.
+	$valeurs['id_article_accueil'] = 0;
+	if ($id = sql_getfetsel('id_article_accueil', 'spip_rubriques', 'id_rubrique='.intval($id_rubrique))) {
+		$valeurs['id_article_accueil'] = $id;
+	}
+
 	return $valeurs;
 }
 
@@ -38,6 +47,11 @@ function formulaires_editer_article_accueil_charger($id_rubrique) {
  */
 function formulaires_editer_article_accueil_traiter($id_rubrique) {
 
+	$retour = array(
+		'message_ok' => '',
+		'editable'   => true
+	);
+	
 	if (!_request('annuler')) {
 		$update = array();
 		if (!is_null($id_accueil = _request('id_article_accueil'))) {
@@ -47,5 +61,5 @@ function formulaires_editer_article_accueil_traiter($id_rubrique) {
 		}
 	}
 
-	return array('message_ok'=>'','editable'=>true);
+	return $retour;
 }
--- article_accueil/trunk/lang/article_accueil_fr.php
+++ article_accueil/trunk/lang/article_accueil_fr.php
@@ -12,7 +12,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'aucun_article_accueil' => 'Aucun article',
 
 	// L
-	'label_id_article_accueil' => 'Article d’accueil',
+	'label_id_article_accueil' => 'Choisir un article',
 
 	// R
 	'rubrique_article_en_accueil' => 'Article d’accueil :'
