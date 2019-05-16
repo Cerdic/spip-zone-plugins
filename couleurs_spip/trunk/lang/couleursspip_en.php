5cc1ce6c101275a2117f88d32f512a33d4d45900
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-05-16 09:48:29 +0000

    traduction anglaise (Ben, spip-contrib)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@115407 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ couleurs_spip/trunk/lang/couleursspip_en.php
@@ -0,0 +1,18 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/couteau_suisse/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	'colorer_fond' => 'To colour the background',
+	'colorer_texte' => 'To colour the text',
+
+	'pp_couleur_fond' => 'background @couleur@',
+	'pp_couleur_icone_fond' => 'B',
+	'pp_couleur_icone_texte' => 'T',
+	'pp_couleur_texte' => 'Text @couleur@',
+
+);
+
+?>
---
+++ couleurs_spip/trunk/lang/paquet-couleurs_spip_en.php
@@ -0,0 +1,16 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'couleurs_spip_description' => 'This plugin allow you to change the colour of parts of texts.
+	To change the typo of the text (colour, size), just select and add a tag like <<code>cs_rouge</code>><cs_rouge>My red text !!</cs><<code>/cs</code>>. 
+	Adding the styles in the file css/couleurs_spip.css - COPY IT in your SQUELETTES/CSS folder, and customized it.
+	You can add as many tags you want.',
+	'couleurs_spip_slogan' => 'Customize your texts with colours',
+);
+?>
