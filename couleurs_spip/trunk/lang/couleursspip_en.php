470e0aab11c089aa08b4187034801e15380ef4ae
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-05-17 03:40:48 +0000

    [Salvatore] [source:_plugins_/couleurs_spip/trunk/lang/ couleursspip] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@115411 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- couleurs_spip/trunk/lang/couleursspip_en.php
+++ couleurs_spip/trunk/lang/couleursspip_en.php
@@ -1,18 +1,21 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/couteau_suisse/lang/
-if (!defined('_ECRIRE_INC_VERSION')) return;
+// extrait automatiquement de https://trad.spip.net/tradlang_module/couleursspip?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
+	// C
 	'colorer_fond' => 'To colour the background',
 	'colorer_texte' => 'To colour the text',
 
+	// P
 	'pp_couleur_fond' => 'background @couleur@',
 	'pp_couleur_icone_fond' => 'B',
 	'pp_couleur_icone_texte' => 'T',
-	'pp_couleur_texte' => 'Text @couleur@',
-
+	'pp_couleur_texte' => 'Text @couleur@'
 );
-
-?>
