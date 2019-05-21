ce774bf7b0805b7f76bfcafc4278afe0ba325c64
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-05-21 16:13:17 +0000

    charset fix sur la puce
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@115451 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- bootstrap/trunk/bootstrap4_options.php
+++ bootstrap/trunk/bootstrap4_options.php
@@ -2,9 +2,8 @@
 
 $GLOBALS['marqueur_skel'] = (isset($GLOBALS['marqueur_skel']) ?  $GLOBALS['marqueur_skel'] : '').":bootstrap4";
 // la puce sans image si jamais on est pas encore en SPIP 3.3+
-$GLOBALS['puce'] = '<span class="spip-puce ltr"><b>?</b></span>';
-$GLOBALS['puce_rtl'] = '<span class="spip-puce rtl"><b>?</b></span>';
-
+$GLOBALS['puce'] = '<span class="spip-puce ltr"><b>–</b></span>';
+$GLOBALS['puce_rtl'] = '<span class="spip-puce rtl"><b>–</b></span>';
 
 function bootstrap4_affichage_final($flux){
 	if (
