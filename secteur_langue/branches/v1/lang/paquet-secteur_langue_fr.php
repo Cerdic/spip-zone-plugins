c9bc0f4654ae6921b38b4ce9f4ff96e02d65f250
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-05-28 07:41:08 +0000

    salvatore
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@115502 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- secteur_langue/branches/v1/lang/paquet-secteur_langue_fr.php
+++ secteur_langue/branches/v1/lang/paquet-secteur_langue_fr.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/secteur_langue/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
@@ -8,7 +8,7 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// S
-	'secteur_langue_description' => 'Mise en place d\'un Spip multilingue avec un secteur par langue',
+	'secteur_langue_description' => 'Mise en place d’un Spip multilingue avec un secteur par langue',
 	'secteur_langue_nom' => 'Secteur par langue',
-	'secteur_langue_slogan' => 'Spip multilingue avec un secteur par langue',
+	'secteur_langue_slogan' => 'Spip multilingue avec un secteur par langue'
 );
