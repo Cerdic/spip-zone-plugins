6fd9d121008fb4374aa73d0471fa101b24b451f3
nicod@lerebooteux.fr <nicod@lerebooteux.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-06-06 12:49:29 +0000

    Réparer le génie qui ne fonctionnait plus
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@115556 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- liens_contenus/trunk/genie/lienscontenus_queue_process.php
+++ liens_contenus/trunk/genie/lienscontenus_queue_process.php
@@ -1,5 +1,6 @@
 <?php
 function genie_lienscontenus_queue_process_dist($t, $verbose = 'no') {
+	include_spip('inc/lienscontenus');
 	$queued = sql_select("*", "spip_liens_contenus_todo", "", "", "date_added", "0,20");
 	while ($objet = sql_fetch($queued)) {
         lienscontenus_referencer_liens($objet['type_objet_contenant'], $objet['id_objet_contenant']);
