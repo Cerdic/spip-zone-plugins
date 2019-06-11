eb066afe1f77f43f14ddc3ecfb18a0bcf6dba639
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-06-11 14:54:14 +0000

    Version refactorée et fonctionnelle du plugin SVP API permettant d'y intégrer les collections catégories/tags et affectations fournies par SVP Typologie
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@115611 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- svp_api/trunk/inc/verifier_requete_svp.php
+++ svp_api/trunk/inc/verifier_requete_svp.php
@@ -9,44 +9,6 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
 
-/**
- * Déclare les collections accessibles via HTTP GET.
- * Par défaut, le plugin propose les collections `plugins` et `depots`.
- *
- * @pipeline declarer_collections_svp
- *
- * @return array
- * 		   Description des collections.
-**/
-function requete_declarer_collections() {
-
-	// Les index désignent les collections, le tableau associé contient les filtres admissibles.
-	// -- Par défaut, svpapi fournit deux collections, plugins et depots.
-	$collections = array(
-		'plugins' => array(
-			'ressource' => 'prefixe',
-			'module'    => 'svpapi',
-			'filtres'   => array(
-				array(
-					'critere' => 'compatible_spip'
-				),
-			)
-		),
-		'depots'  => array(
-			'module'  => 'svpapi',
-			'filtres' => array(
-				array(
-					'nom' => 'type'
-				)
-			)
-		)
-	);
-
-	$collections = pipeline('declarer_collections_svp', $collections);
-
-	return $collections;
-}
-
 
 /**
  * Détermine si le serveur est capable de répondre aux requêtes SVP.
