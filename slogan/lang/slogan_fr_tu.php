2fcae89d0a961796da16239a860f94c4aa886398
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-06-17 03:39:58 +0000

    [Salvatore] [source:_plugins_/slogan/lang/ slogan] Export depuis http://trad.spip.net de la langue fr_tu
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@115702 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ slogan/lang/slogan_fr_tu.php
@@ -0,0 +1,20 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/slogan?lang_cible=fr_tu
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'config_info_enregistree' => 'La configuration a été enregistrée',
+
+	// I
+	'info_slogan_site' => 'Slogan de ton site',
+
+	// T
+	'titre_identite_site' => 'Identité du site'
+);
