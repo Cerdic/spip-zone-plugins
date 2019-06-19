dac171e90252a0d84cf8d20ec481ee7f378caf1f
toutati@free.fr <toutati@free.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-06-19 16:07:49 +0000

    Fichier de langue pour internationaliser le plugin !
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@115728 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ selection_articles/trunk/lang/pb_selection_fr.php
@@ -0,0 +1,20 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	
+	//A
+	'ajouter_article_numero' => 'Ajouter',
+
+	// P
+	'selection_articles' => 'Sélection d’articles',
+	'selection_article_numero' => 'Sélectionner l’article numéro : ',
+	
+	//R
+	'retirer_de_la_liste' => 'Retirer de la liste',
+);
