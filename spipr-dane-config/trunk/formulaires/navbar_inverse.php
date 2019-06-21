04996fdc8efd19e6503757e0df60865ce9ceead6
dominique.lepaisant@ac-caen.fr <dominique.lepaisant@ac-caen.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-06-21 08:35:57 +0000

    Correction commit 115743
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@115744 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spipr-dane-config/trunk/formulaires/navbar_inverse.php
+++ spipr-dane-config/trunk/formulaires/navbar_inverse.php
@@ -32,7 +32,7 @@ function formulaires_navbar_inverse_charger_dist() {
     	'inverser_couleur' => lire_config('sdc/navbar/inverser_couleur') == 'color1' ? 'on': '',
         'couleur_liens' => lire_config('sdc/navbar/couleur_liens', 'white'),
         'couleur_liens_hover' => lire_config('sdc/navbar/couleur_liens_hover'),
-        'scrolltofixed' => array(lire_config('sdc/navbar/scrolltofixed'))
+        'scrolltofixed' => lire_config('sdc/navbar/scrolltofixed')
     );
     
     return $valeurs;
@@ -90,7 +90,7 @@ function formulaires_navbar_inverse_traiter_dist() {
             ecrire_config('sdc/navbar/couleur_liens', _request('couleur_liens'));
             ecrire_config('sdc/navbar/couleur_liens_hover', _request('couleur_liens_hover'));
             ecrire_config('sdc/navbar/inverser_couleur', _request('inverser_couleur'));
-            ecrire_config('sdc/navbar/scrolltofixed', _request('navbar_scrolltofixed')[0]);
+            ecrire_config('sdc/navbar/scrolltofixed', _request('scrolltofixed'));
         }
         if ($inverser_couleur) {
             set_request("inverser_couleur", $inverser_couleur);
@@ -121,6 +121,7 @@ function formulaires_navbar_inverse_traiter_dist() {
         effacer_config('sdc/navbar/inverser_couleur');
         effacer_config('sdc/navbar/couleur_liens');
         effacer_config('sdc/navbar/couleur_liens_hover');
+        effacer_config('sdc/navbar/scrolltofixed');
         set_request('couleur_liens', 'white');
         set_request('couleur_liens_hover', '');
         
