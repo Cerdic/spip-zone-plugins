d3fcecea27911f602ae409717557ca57013f6d53
gouz@root-me.org <gouz@root-me.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-06-26 13:28:18 +0000

    augmentation de la limite sur preg_files() pour gerer jusqu'a 100k visiteurs
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@115792 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- visiteurs_connectes/visiteurs_fonctions.php
+++ visiteurs_connectes/visiteurs_fonctions.php
@@ -5,5 +5,5 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 }
 
 function visiteurs_connectes_compter() {
-	return count(preg_files(_DIR_TMP.'visites/', '.'));
+	return count(preg_files(_DIR_TMP.'visites/', '.', 100000));
 }
