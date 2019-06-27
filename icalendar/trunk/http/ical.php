d35a9a85b5c24e0e02eb801260a47061082ea239
gouz@root-me.org <gouz@root-me.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-06-27 09:36:10 +0000

    version icalendar compatible avec le plugin http > 1.0.0
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@115801 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- icalendar/trunk/http/ical.php
+++ icalendar/trunk/http/ical.php
@@ -4,63 +4,75 @@
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 /*
- * Implémentation d'un serveur REST pour iCal
- * (le vrai truc bien serait d'implémenter au moins une partie de CalDAV, mais on en est pas là)
+ * Implémentation d'un serveur REST pour APP (AtomPub)
  */ 
 
-
-/*
- * GET sur la racine du serveur iCal
- * http://site/rest.api/ical
+/**
+ * Rien, car en Atom il n'y a malheureusement pas de gestion des erreurs pour l'instant
+ *
+ * @param int $code Le code HTTP de l'erreur à générer
+ * @return string Retourne une chaîne vide
  */
-function http_ical_get_index_dist(){
-	
+function http_ical_erreur_dist($code, $requete, $reponse){
+	$reponse->setStatusCode($code);
+	$reponse->setContent('');
+	return $reponse;
 }
 
 /*
  * GET sur une collection
- * http://site/rest.api/ical/all
+ * http://site/http.api/ical/all
  */
-function http_ical_get_collection_dist($collection){
+function http_ical_get_collection_dist($requete, $reponse){
+	$collection = $requete->attributes->get('collection');
+	$contexte = $requete->query->all();
+	
 	// Pour l'instant on va simplement chercher un squelette du nom de la collection
 	// Le squelette prend en contexte les paramètres du GET uniquement
-	if ($flux = recuperer_fond("http/ical/$collection", $_GET)){
-		header('Status: 200 OK');
-		header("Content-type: text/calendar; charset=utf-8");
-		echo $flux;
-		exit;
+	if ($flux = recuperer_fond("http/ical/$collection", $contexte)){
+		$reponse->setStatusCode(200);
+		$reponse->setCharset('utf-8');
+		$reponse->headers->set('Content-Type', 'application/atom+xml');
+		$reponse->setContent($flux);
 	}
 	// Si on ne trouve rien c'est que ça n'existe pas
 	else{
-		header('Status: 404 Not Found');
-		exit;
+		$reponse->setStatusCode(404);
 	}
+	
+	return $reponse;
 }
 
 /*
  * GET sur une ressource
- * http://site/rest.api/ical/patates
+ * http://site/http.api/ical/event
  */
-function http_ical_get_ressource_dist($collection, $ressource){
+ 
+function http_ical_get_ressource_dist($requete, $reponse){
+
+	// Quelque soit la collection, tous les événements ont le même squelette
+	// Le squelette prend en contexte les paramètres du GET + l'identifiant de l'évenement en essayant de faire au mieux
+	$ressource = $requete->attributes->get('ressource');
+
 	// Quelque soit la collection, tous les événements ont le même squelette
 	// Le squelette prend en contexte les paramètres du GET + l'identifiant de l'évenement en essayant de faire au mieux
 	$contexte = array(
 		'id_evenement' => $ressource,
 		'ressource' => $ressource,
 	);
-	$contexte = array_merge($_GET, $contexte);
+	$contexte = array_merge($requete->query->all(), $contexte);
 	
 	if ($flux = recuperer_fond("http/ical/event", $contexte)){
-		header('Status: 200 OK');
-		header("Content-type: text/calendar; charset=utf-8");
-		echo $flux;
-		exit;
+		$reponse->setStatusCode(200);
+		$reponse->setCharset('utf-8');
+		$reponse->headers->set("Content-type: text/calendar; charset=utf-8");
+		$reponse->setContent($flux);
 	}
 	// Si on ne trouve rien c'est que ça n'existe pas
 	else{
-		header('Status: 404 Not Found');
-		exit;
+		$reponse->setStatusCode(404);
 	}
+	
+	return $reponse;
 }
 
-?>
