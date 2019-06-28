55f81ed12703d9009577fae14173927f461e6f15
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-06-28 23:39:35 +0000

    Il ne faut pas oublier de confirmer le statut de l'inscription avant de connecter la personne.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@115821 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- inscription_connexion/trunk/inscriptionconnexion_pipelines.php
+++ inscription_connexion/trunk/inscriptionconnexion_pipelines.php
@@ -18,6 +18,12 @@ function inscriptionconnexion_formulaire_traiter($flux){
 		and $id_auteur = intval($flux['data']['id_auteur'])
 		and $auteur = sql_fetsel('*', 'spip_auteurs', 'id_auteur = '.$id_auteur)
 	){
+		// Quand on décide d'utiliser ce plugin, c'est qu'on veut outrepasser la confirmation par email
+		// donc on le fait ici, il faut confirmer son statut avant de connecter
+		include_spip('action/inscrire_auteur');
+		$auteur = confirmer_statut_inscription($auteur);
+		
+		// Puis on connecte
 		include_spip('inc/auth');
 		auth_loger($auteur);
 	}
