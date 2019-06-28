39bce52aa8d9d101e6b7e85ae540880511b12b80
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-06-28 12:08:55 +0000

    Revert d'une partie de [110430] qui n'avait aucun rapport avec le commit. Il y avait déjà un utilise qui assurait de passer après, mais à aucun moment on ne nécessite les fonctionnalités de ce plugin.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@115816 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- inscription_motdepasse/trunk/inscriptionmotdepasse_autorisations.php
+++ inscription_motdepasse/trunk/inscriptionmotdepasse_autorisations.php
@@ -1,10 +1,11 @@
 <?php
 
 // Sécurité
-if (!defined('_ECRIRE_INC_VERSION')) return;
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
 function inscriptionmotdepasse_autoriser(){}
-
 
 function autoriser_loger($faire, $quoi, $id, $qui, $opt) {    
     if ( $qui['statut'] == '5poubelle' or $qui['statut'] == 'nouveau' ) return false;
