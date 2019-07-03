5bed859360272d929063a0d5864f2a19569c131f
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-07-03 12:11:45 +0000

    mise a jour et optimisation de la lib
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@115850 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- adaptive_images/trunk/lib/geometrize/geometrize.init.php
+++ adaptive_images/trunk/lib/geometrize/geometrize.init.php
@@ -1,7 +1,9 @@
 <?php
 
 /**
- * PHP geomtrize version compilee d'apres https://github.com/Tw1ddle/geometrize-haxe
+ * PHP version of https://github.com/Tw1ddle/geometrize-haxe/commit/e6ed1ab8c3867ac0da5bfea25ee4e7204ad286be
+ * Based on PHP code generated with Haxe on this base version
+ * + manual optimization&refactoring
  *
  * boot PHP utils classes and start autoload for geometrize & al
  */
--- adaptive_images/trunk/lib/geometrize/readme.md
+++ adaptive_images/trunk/lib/geometrize/readme.md
@@ -1,2 +1,3 @@
-PHP compiled version of https://github.com/Tw1ddle/geometrize-haxe
-(to be optimized)
+PHP version of https://github.com/Tw1ddle/geometrize-haxe/commit/e6ed1ab8c3867ac0da5bfea25ee4e7204ad286be
+Based on PHP code generated with Haxe on this base version
++ manual optimization&refactoring
