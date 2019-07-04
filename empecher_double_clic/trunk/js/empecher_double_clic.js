555cf444814cd3b8ee5fa8245533ba0cadfe235d
nicod@lerebooteux.fr <nicod@lerebooteux.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-07-04 17:22:15 +0000

    Ne pas désactiver les #BOUTON_ACTION après le clic. Tester la présence de la classe après le clic plutôt que dans le sélecteur jQuery (perf)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@115868 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- empecher_double_clic/trunk/js/empecher_double_clic.js
+++ empecher_double_clic/trunk/js/empecher_double_clic.js
@@ -1,8 +1,10 @@
 $(function() {
 	$('form').submit(function() {
-		$(this).addClass('js-sending')
-			.find('input[type="submit"], button[type="submit"]')
-			.addClass('js-button-sending')
-			.on('click',function (e) {e.preventDefault();});
+		if(!$(this).hasClass('bouton_action_post')) {
+			$(this).addClass('js-sending')
+				.find('input[type="submit"], button[type="submit"]')
+				.addClass('js-button-sending')
+				.on('click', function(e) {e.preventDefault();});
+		}
 	});
 });
