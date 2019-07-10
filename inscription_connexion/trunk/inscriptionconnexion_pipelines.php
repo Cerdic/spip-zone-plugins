6cef75d866f13bacad8676b58e1bf8640ff4c65d
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-07-10 10:53:34 +0000

    erreur si compte existant (marcimat)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@115913 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- inscription_connexion/trunk/inscriptionconnexion_pipelines.php
+++ inscription_connexion/trunk/inscriptionconnexion_pipelines.php
@@ -6,6 +6,25 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 }
 
 /**
+ * Impossible de s'inscrire avec un email déjà existant avec ce plugin
+ *
+ * @pipeline formulaire_traiter
+ * @param array $flux
+ * @return array
+ */
+function inscriptionconnexion_formulaire_verifier($flux){
+	if (
+		$flux['args']['form'] == 'inscription'
+		and $email = _request('mail_inscription')
+		and $auteur = sql_fetsel('*', 'spip_auteurs', 'email = '.sql_quote($email))
+	){
+		$flux['message_erreur'] = _T('inscriptionconnexion:erreur_compte_existant');
+	}
+	
+	return $flux;
+}
+
+/**
  * Connecte le visiteur une fois que l'inscription est terminée correctement
  *
  * @pipeline formulaire_traiter
---
+++ inscription_connexion/trunk/lang/inscriptionconnexion_fr.php
@@ -0,0 +1,8 @@
+<?php
+
+// Sécurité
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	'erreur_compte_existant' => 'Un compte existe déjà avec cet adresse email. Connectez-vous avec ou bien utilisez une autre adresse.',
+);
