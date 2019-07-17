7de5e8e413528b20be051d10feb84a8612d61ca9
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-07-17 19:40:44 +0000

    paquet.xml + lang
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@115991 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ saisie_calcul/trunk/lang/paquet-saisie_calcul_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/saisies/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// S
+	'saisie_calcul_description' => 'Une saisie dont la valeur est le résultat d\'un calcul depuis d\'autres saisies',
+	'saisie_calcul_nom' => 'Saisie de calcul',
+	'saisies_slogan' => 'Une saisie pour les calculer toutes !'
+);
