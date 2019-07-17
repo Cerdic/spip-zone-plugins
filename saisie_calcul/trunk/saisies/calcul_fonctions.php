1162d9eb33a4b41f6b0fa73c8f5d71a9535e0247
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-07-17 20:12:39 +0000

    verification php de l'acceptabilité des réponses
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@115999 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- saisie_calcul/trunk/saisies/calcul_fonctions.php
+++ saisie_calcul/trunk/saisies/calcul_fonctions.php
@@ -16,6 +16,32 @@ function saisie_calcul_2_js($expr) {
 	return $expr;
 }
 
+
+/**
+ * Transforme une expression de calcul entrée par l'utilisateur/trice en expression interprétable en JS
+ * @param string $expr
+ * @return string
+**/
+function saisie_calcul_2_php($expr) {
+	$expr = saisie_calcul_securiser($expr);
+	$expr = preg_replace("#@(.*)@#U", 'floatval(_request(\'$1\'))', $expr);
+	$expr = str_replace(",",".",$expr);// virgule -> point
+	$expr = str_replace("ROUND","saisie_calcul_arrondi",$expr);
+	return $expr;
+}
+/**
+ * Une fonction d'arrondi php qui imite la fonction d'arrondi JS
+ * @param int $valeur
+ * @param int
+**/
+function saisie_calcul_arrondi($valeur) {
+	if ($valeur > 0) {
+		return round($valeur, 0, PHP_ROUND_HALF_UP);
+	} else {
+		return round($valeur, 0, PHP_ROUND_HALF_DOWN);
+	}
+}
+
 /**
  * Sécurise une expression de calcul passée en paramètre.
  * N'autorise que :
