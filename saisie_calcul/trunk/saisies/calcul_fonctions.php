38adce610ee4f8e7fe00084e089a21844b00bc4e
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-07-17 20:24:43 +0000

    ne pas confondre concaténation et addition : s'assurer que les valeurs des champs soit tjr converti en flottant, en js
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@116000 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- saisie_calcul/trunk/saisies/calcul_fonctions.php
+++ saisie_calcul/trunk/saisies/calcul_fonctions.php
@@ -10,7 +10,7 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 **/
 function saisie_calcul_2_js($expr) {
 	$expr = saisie_calcul_securiser($expr);
-	$expr = preg_replace("#@(.*)@#U", "$('#champ_$1').val()", $expr);
+	$expr = preg_replace("#@(.*)@#U", "parseFloat($('#champ_$1').val())", $expr);
 	$expr = str_replace(",",".",$expr);// virgule -> point
 	$expr = str_replace("ROUND","Math.round",$expr);
 	return $expr;
