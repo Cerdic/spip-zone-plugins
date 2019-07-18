55a7a8b96098118aa620d80270a33206a2876345
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-07-18 14:03:27 +0000

    Compatibilité SPIP 3.3 : plugin.xml n'est plus supporté
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@116008 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ porte_plume_changement_langue/trunk/lang/paquet-pp_chgt_lg_fr.php
@@ -0,0 +1,19 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+// Fichier produit par PlugOnet
+// Module: paquet-pp_chgt_lg
+// Langue: fr
+// Date: 18-07-2019 13:58:58
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// P
+	'pp_chgt_lg_description' => 'Ajoute un bouton au porte-plume permettant d\'indiquer un changement de langue dans le texte.',
+	'pp_chgt_lg_slogan' => 'Extension changement de langue pour Porte-Plume',
+);
+?>
