0f51161aa9f812a8018c5b8a560465b0fb8c3125
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-07-18 14:04:39 +0000

    Compatibilité SPIP 3.3 : plugin.xml n'est plus supporté
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@116009 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ citations_bien_balisees/lang/paquet-citations_bb_fr.php
@@ -0,0 +1,21 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+// Fichier produit par PlugOnet
+// Module: paquet-citations_bb
+// Langue: fr
+// Date: 18-07-2019 13:58:58
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'citations_bb_description' => 'Ce plugin transforme automatiquement le raccourci {&lt;quote&gt;} en {&lt;q&gt;} (et {&lt;/quote&gt;} en {&lt;/q&gt;}) si la citation ne contient pas de retour à la ligne.
+
+	L\'objectif est de respecter les usages en HTML (les citations courtes doivent être entourées par &lt;q&gt;, les citations contenant des paragraphes doivent être entourées des &lt;blockquote&gt;).',
+	'citations_bb_slogan' => 'Traitement typographique des citations courtes',
+);
+?>
