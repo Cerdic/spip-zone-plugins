e0e4c5bad3231fcacc231f135f4aa91ab662b446
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-07-20 22:42:47 +0000

    Revert "js de calcul en début de formulaire, pour que cela passe avant les afficher_si". Puisqu'on appelle le trigger 'change', ca peut et doit passer après les afficher_si, qui seront rappelés le cas échéant.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@116072 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- saisie_calcul/trunk/saisie_calcul_pipelines.php
+++ saisie_calcul/trunk/saisie_calcul_pipelines.php
@@ -12,7 +12,7 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 function saisie_calcul_formulaire_fond($flux) {
 	static $flag;
 	if (!$flag and stripos($flux['data'], 'saisie_calcul')!==false) {
-		$flux['data'] = "<script type='text/javascript' src='".find_in_path('js/saisie_calcul.js')."'></script>".$flux['data'];
+		$flux['data'] .= "<script type='text/javascript' src='".find_in_path('js/saisie_calcul.js')."'></script>";
 		$flag = true;
 	}
 	return $flux;
