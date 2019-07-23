4bfbd20b75d1c12360d7f31aa38496ecf00f8104
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-07-23 03:41:30 +0000

    [Salvatore] [source:_plugins_/saisie_calcul/trunk/lang/ saisie_calcul] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@116098 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- saisie_calcul/trunk/lang/saisie_calcul_fr.php
+++ saisie_calcul/trunk/lang/saisie_calcul_fr.php
@@ -1,13 +1,15 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/saisies/trunk/lang/
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/saisie_calcul/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
+	// O
 	'option_calcul_label' => 'Calcul à effectuer',
+
 	// S
-	'saisie_calcul_titre' => 'Résultat d\'un calcul'
+	'saisie_calcul_titre' => 'Résultat d’un calcul'
 );
