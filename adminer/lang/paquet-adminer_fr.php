97bb24f718e1186c69495e56a7aaad3ff1405cc8
nicod@lerebooteux.fr <nicod@lerebooteux.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-08-02 19:27:17 +0000

    Le fichier de langue pour le paquet.xml d'adminer
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@116206 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ adminer/lang/paquet-adminer_fr.php
@@ -0,0 +1,13 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/fabrique/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	
+	'adminer_description' => 'Utiliser Adminer sur la base SPIP',
+	'adminer_slogan' => 'Utiliser Adminer pour explorer la base SPIP',
+	'adminer_titre' => 'Adminer'
+);
