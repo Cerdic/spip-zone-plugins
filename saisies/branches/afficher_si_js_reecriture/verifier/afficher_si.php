522124ba36c37af0e0c5bd7156df917ada684521
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-08-17 14:23:05 +0000

    constructeur de formulaire : verifier la syntaxe des tests d'afficher_si (nicod)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@117572 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ saisies/branches/afficher_si_js_reecriture/verifier/afficher_si.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * API de vérification : vérification de la validité d'un test afficher_si
+ *
+ * @plugin     saisies
+ */
+
+// Sécurité
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+/**
+ *
+ *
+ *
+ *
+ * @param string $valeur
+ *   La valeur à vérifier.
+ * @return string
+ *   Retourne une chaine vide si c'est valide, sinon une chaine expliquant l'erreur.
+ */
+function verifier_afficher_si_dist($valeur) {
+	include_spip('inc/saisies_afficher_si_commun');
+	$erreur = _T('saisies:erreur_syntaxe_afficher_si');
+	if (!saisies_afficher_si_verifier_syntaxe($valeur)) {
+		return $erreur;
+	} else {
+		return '';
+	}
+}
