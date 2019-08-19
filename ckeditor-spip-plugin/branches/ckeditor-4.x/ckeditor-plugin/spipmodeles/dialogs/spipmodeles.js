9be7cb80b1e827d2e04c54b575aff93478146c14
cam.lafit <cam.lafit@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-08-19 13:18:28 +0000

    Beautifier sur le plugin modele pour ckeditor
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@117594 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- ckeditor-spip-plugin/branches/ckeditor-4.x/ckeditor-plugin/spipmodeles/dialogs/spipmodeles.js
+++ ckeditor-spip-plugin/branches/ckeditor-4.x/ckeditor-plugin/spipmodeles/dialogs/spipmodeles.js
@@ -1,122 +1,122 @@
 function paramTypeChange(paramType, tag, valSelect, valInput, tags) {
-	var param = paramType.getValue() ;
-	var result = '' ;
-	for(var ftag in tags) {
+	var param = paramType.getValue();
+	var result = '';
+	for (var ftag in tags) {
 		if (tag == tags[ftag][0]) {
 			if (tags[ftag][3][param] && (tags[ftag][3][param].length > 1)) {
-				valSelect.clear() ;
-				for(var fval in tags[ftag][3][param]) {
-					valSelect.add(tags[ftag][3][param][fval].replace(/^.*\|/, ''), tags[ftag][3][param][fval].replace(/\|.*$/, '')) ;
+				valSelect.clear();
+				for (var fval in tags[ftag][3][param]) {
+					valSelect.add(tags[ftag][3][param][fval].replace(/^.*\|/, ''), tags[ftag][3][param][fval].replace(/\|.*$/, ''));
 				}
-				valSelect.getElement().show() ;
-				valInput.getElement().hide() ;
+				valSelect.getElement().show();
+				valInput.getElement().hide();
 			} else if (tags[ftag][3][param] && (tags[ftag][3][param].length == 1)) {
-				result = tags[ftag][3][param][0].replace(/\|.*$/, '') ;
-				valInput.getElement().hide() ;
-				valSelect.getElement().hide() ;
+				result = tags[ftag][3][param][0].replace(/\|.*$/, '');
+				valInput.getElement().hide();
+				valSelect.getElement().hide();
 			} else {
-				valInput.getElement().show() ;
-				valSelect.getElement().hide() ;
+				valInput.getElement().show();
+				valSelect.getElement().hide();
 			}
 		}
 	}
-	return result ;
+	return result;
 }
 
 function count(tab) {
-	var c = 0 ;
-	for(var item in tab) {
+	var c = 0;
+	for (var item in tab) {
 		c++;
 	}
-	return c ;
+	return c;
 }
 
-CKEDITOR.dialog.add('spipmodelesDlg', function(editor){
-    var lang, m, tag, tags, tagtype, value, valDocId, valSelect, valInput, paramType, paramLigne, paramHidden, selectDocId, val_start, val_end, separateur ;
-	lang = editor.lang.spipmodeles ;
-    return {
-		title:lang.label,
-		minWidth:400,
-		minHeight:200,
-		onLoad: function() {
-			valSelect = this.getContentElement('tab1', 'paramvalSelect') ;
-			valInput = this.getContentElement('tab1', 'paramvalInput') ;
-			paramType = this.getContentElement('tab1', 'paramType') ;
-			paramLigne = this.getContentElement('tab1', 'paramLigne') ;
-			valDocId = this.getContentElement('tab1', 'docId' ) ;
-			selectDocId = this.getContentElement('tab1', 'selectDocId' ) ;
-			tags = this.tags ;
+CKEDITOR.dialog.add('spipmodelesDlg', function (editor) {
+	var lang, m, tag, tags, tagtype, value, valDocId, valSelect, valInput, paramType, paramLigne, paramHidden, selectDocId, val_start, val_end, separateur;
+	lang = editor.lang.spipmodeles;
+	return {
+		title: lang.label,
+		minWidth: 400,
+		minHeight: 200,
+		onLoad: function () {
+			valSelect = this.getContentElement('tab1', 'paramvalSelect');
+			valInput = this.getContentElement('tab1', 'paramvalInput');
+			paramType = this.getContentElement('tab1', 'paramType');
+			paramLigne = this.getContentElement('tab1', 'paramLigne');
+			valDocId = this.getContentElement('tab1', 'docId');
+			selectDocId = this.getContentElement('tab1', 'selectDocId');
+			tags = this.tags;
 
-			valSelect.getElement().getParent().setAttribute('style','padding:0;margin:0;width:auto;') ;
-			valInput.getElement().getParent().setAttribute('style','padding:0;margin:0;width:auto;') ;
+			valSelect.getElement().getParent().setAttribute('style', 'padding:0;margin:0;width:auto;');
+			valInput.getElement().getParent().setAttribute('style', 'padding:0;margin:0;width:auto;');
 		},
-		onShow: function() {
-			value = tag = this.tag_requested ;
-			for(var ftag in this.tags) {
+		onShow: function () {
+			value = tag = this.tag_requested;
+			for (var ftag in this.tags) {
 				if (value == this.tags[ftag][0]) { // on trouve le tag
-					if (this.tags[ftag][7]=='html'){
-						val_start='"';val_end='"';separateur=' ';
-					} else{
-						val_start='';val_end='';separateur='|';
-					}
-					this.getContentElement('tab1', 'docId').setLabel(lang.promptId.replace(/%id%/, this.tags[ftag][1]?this.tags[ftag][1]:'<'+tag+'>')) ;
-					tagtype = this.tags[ftag][5] ;
-					if (tagtype == 'num-aucun') {
-						this.getContentElement('tab1', 'numPanel').getElement().hide() ;
+					if (this.tags[ftag][7] == 'html') {
+						val_start = '"'; val_end = '"'; separateur = ' ';
 					} else {
-						this.getContentElement('tab1', 'numPanel').getElement().show() ;
+						val_start = ''; val_end = ''; separateur = '|';
+					}
+					this.getContentElement('tab1', 'docId').setLabel(lang.promptId.replace(/%id%/, this.tags[ftag][1] ? this.tags[ftag][1] : '<' + tag + '>'));
+					tagtype = this.tags[ftag][5];
+					if (tagtype == 'num-aucun') {
+						this.getContentElement('tab1', 'numPanel').getElement().hide();
+					} else {
+						this.getContentElement('tab1', 'numPanel').getElement().show();
 					}
 					paramType.clear();
-					for(var param in this.tags[ftag][3]) {
-						paramType.add(param.replace(/\|.*$/, '') , param ) ;
+					for (var param in this.tags[ftag][3]) {
+						paramType.add(param.replace(/\|.*$/, ''), param);
 					}
-					if(count(this.tags[ftag][3]) == 0) {
-						this.getContentElement('tab1', 'paramSet').getElement().hide() ;
+					if (count(this.tags[ftag][3]) == 0) {
+						this.getContentElement('tab1', 'paramSet').getElement().hide();
 					} else {
-						this.getContentElement('tab1', 'paramSet').getElement().show() ;
+						this.getContentElement('tab1', 'paramSet').getElement().show();
 					}
-					paramHidden = paramTypeChange(paramType, value, valSelect, valInput, tags) ;
-					if ( this.tags[ftag][4] == 'thisobject' ) { // on demande l'id de l'objet en cours d'édition
-						selectDocId.getElement().hide() ;
-						valDocId.setValue ( editor.config.spip_contexte.id ) ;
-					} else if ( this.tags[ftag][4] ) { // on demande une liste
-						var liste ;
+					paramHidden = paramTypeChange(paramType, value, valSelect, valInput, tags);
+					if (this.tags[ftag][4] == 'thisobject') { // on demande l'id de l'objet en cours d'édition
+						selectDocId.getElement().hide();
+						valDocId.setValue(editor.config.spip_contexte.id);
+					} else if (this.tags[ftag][4]) { // on demande une liste
+						var liste;
 						try {
 							var json = $.ajax({
-								url: CKEDITOR.spipurl + '?doctype=' + this.tags[ftag][4] + '&page=spiplistes-json', 
+								url: CKEDITOR.spipurl + '?doctype=' + this.tags[ftag][4] + '&page=spiplistes-json',
 								async: false, // pour avoir une réponse synchrone
 								global: false,
 								dataType: 'json'
-							}).responseText ;
-							liste = $.parseJSON(json) ;
-						} catch(e) {
-							alert(lang.page_introuvable + CKEDITOR.spipurl + '?doctype=' + this.tags[ftag][4] + '&page=spiplistes-json' ) ;
-							liste = [] ;
+							}).responseText;
+							liste = $.parseJSON(json);
+						} catch (e) {
+							alert(lang.page_introuvable + CKEDITOR.spipurl + '?doctype=' + this.tags[ftag][4] + '&page=spiplistes-json');
+							liste = [];
 						}
-						selectDocId =  this.getContentElement('tab1', 'selectDocId') ;
-						selectDocId.clear() ;
-						for(var id in liste) {
-							selectDocId.add(liste[id], id) ;
+						selectDocId = this.getContentElement('tab1', 'selectDocId');
+						selectDocId.clear();
+						for (var id in liste) {
+							selectDocId.add(liste[id], id);
 						}
-						selectDocId.getElement().show() ;
-						valDocId.setValue( selectDocId.getValue() ) ;
+						selectDocId.getElement().show();
+						valDocId.setValue(selectDocId.getValue());
 					} else {
-						selectDocId.getElement().hide() ;
-						valDocId.setValue( '' ) ;
+						selectDocId.getElement().hide();
+						valDocId.setValue('');
 					}
 				}
 			}
 
 		},
-		onOk: function() {
-			var id = this.getValueOf('tab1','docId');
+		onOk: function () {
+			var id = this.getValueOf('tab1', 'docId');
 			if ((!id && (!tagtype || tagtype.match(/^num-(facultatif|aucun)$/))) || id.match(/\d+/)) {
-				value = tag + (id?id:'') ; // si XX alors il *faut* un id numerique; sinon l'id est facultatif
-				insertTag(editor, value, tag, paramLigne.getValue(), this.tags) ;
-				return true ;
+				value = tag + (id ? id : ''); // si XX alors il *faut* un id numerique; sinon l'id est facultatif
+				insertTag(editor, value, tag, paramLigne.getValue(), this.tags);
+				return true;
 			} else {
-				alert(lang.erreur_nombre_obligatoire) ;
-				return false ;
+				alert(lang.erreur_nombre_obligatoire);
+				return false;
 			}
 		},
 		contents: [
@@ -126,82 +126,82 @@ CKEDITOR.dialog.add('spipmodelesDlg', function(editor){
 				title: lang.parametres_des_modeles,
 				elements: [
 					{
-						id:'panel1',
-						type:'vbox',
+						id: 'panel1',
+						type: 'vbox',
 						children: [
 							{
-								type:'hbox',
-								id:'numPanel',
+								type: 'hbox',
+								id: 'numPanel',
 								children: [
 									{
-										type:'text',
-										label:'',
-										width:'80px',
-										id:'docId',
-										labelLayout:'horizontal'
+										type: 'text',
+										label: '',
+										width: '80px',
+										id: 'docId',
+										labelLayout: 'horizontal'
 									},
 									{
-										type:'select',
-										width:'80px',
-										id:'selectDocId',
+										type: 'select',
+										width: '80px',
+										id: 'selectDocId',
 										items: [],
-										onChange: function() { valDocId.setValue( selectDocId.getValue() ) ; }
+										onChange: function () { valDocId.setValue(selectDocId.getValue()); }
 									}
-								]	
+								]
 							},
 							{
-								type:'fieldset',
-								id:'paramSet',
-								label:lang.parametres,
+								type: 'fieldset',
+								id: 'paramSet',
+								label: lang.parametres,
 								children: [
 									{
-										type:'vbox',
-										children: [	
+										type: 'vbox',
+										children: [
 											{
-												type:'hbox',
+												type: 'hbox',
 												children: [
 													{
-														type:'select',
-														id:'paramType',
-														label:lang.parametre,
+														type: 'select',
+														id: 'paramType',
+														label: lang.parametre,
 														items: [],
-														onChange: function() { 
-															paramHidden = paramTypeChange(paramType, value, valSelect, valInput, tags) ;
+														onChange: function () {
+															paramHidden = paramTypeChange(paramType, value, valSelect, valInput, tags);
 														}
 													},
 													{
-														type:'select',
-														id:'paramvalSelect',
-														label:lang.valeur,
+														type: 'select',
+														id: 'paramvalSelect',
+														label: lang.valeur,
 														items: []
 													},
 													{
-														type:'text',
-														id:'paramvalInput',
-														label:lang.valeur
+														type: 'text',
+														id: 'paramvalInput',
+														label: lang.valeur
 													}
 												]
 											},
-											{ 
-												type:'button',
-												id:'addparam',
-												label:lang.ajoute_parametre,
-												onClick: function()  {
-													var type = paramType.getValue() ;
-													var valeur = (valSelect.isVisible() ? 
-															valSelect.getValue() : 
-															( valInput.isVisible() ? 
-																valInput.getValue() : 
-																paramHidden 
-															)
-														) ; 
+											{
+												type: 'button',
+												id: 'addparam',
+												label: lang.ajoute_parametre,
+												onClick: function () {
+													var type = paramType.getValue();
+													var valeur = (valSelect.isVisible() ?
+														valSelect.getValue() :
+														(valInput.isVisible() ?
+															valInput.getValue() :
+															paramHidden
+														)
+													);
 													if (type.match(/\|/)) {
-														type = type.replace(/^.*\|/, '') ;
+														type = type.replace(/^.*\|/, '');
 													} else {
-														type = '' ;
+														type = '';
 													}
-													var oldvalue = paramLigne.getValue() ;
-													paramLigne.setValue( (oldvalue ? oldvalue+separateur : '') + type + ( type && valeur ? '=' : '' ) + val_start + valeur + val_end ) ;  
+													var oldvalue = paramLigne.getValue();
+													paramLigne.setValue((oldvalue ? oldvalue + separateur : '') + type + (type && valeur ? '=' : '') + val_start + valeur + val_end);
 												}
 											}
 										]
@@ -209,9 +209,9 @@ CKEDITOR.dialog.add('spipmodelesDlg', function(editor){
 								]
 							},
 							{
-								type:'text',
-								id:'paramLigne',
-								label:lang.parametres
+								type: 'text',
+								id: 'paramLigne',
+								label: lang.parametres
 							}
 						]
 					}
