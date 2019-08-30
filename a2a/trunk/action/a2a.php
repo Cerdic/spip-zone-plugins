4829d8141b79b9bc625e716184033186ffd5b628
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-08-30 19:38:57 +0000

    suppression dans les 2 sens : ne pas dupliquer le code
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@117685 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- a2a/trunk/action/a2a.php
+++ a2a/trunk/action/a2a.php
@@ -81,23 +81,12 @@ function action_a2a_supprimer_lien_dist($id_article_cible, $id_article, $type_li
 		'type_liaison=' . sql_quote($type_liaison)
 	));
 	if ($type == 'both') {
-		$rang = sql_getfetsel('rang', 'spip_articles_lies', 'id_article=' . sql_quote($id_article_cible) . ' AND id_article_lie=' . sql_quote($id_article)  . ' AND type_liaison=' . sql_quote($type_liaison));
-
-		// on recupere les articles lies dont le rang est superieur a celui a supprimer
-		$res = sql_select('*', 'spip_articles_lies', 'id_article=' . sql_quote($id_article_cible) . ' AND rang>' . sql_quote($rang));
-		//on boucle sur le resultat et on met a jour le rang des articles lies avant suppression
-		while($r = sql_fetch($res)) {
-			sql_update('spip_articles_lies', array('rang' => sql_quote(--$r["rang"])), 'id_article=' . sql_quote($r["id_article"]) . ' AND id_article_lie=' . sql_quote($r["id_article_lie"]));
-		}
-		sql_delete('spip_articles_lies',  array(
-			'id_article = ' . sql_quote($id_article_cible),
-			'id_article_lie = ' . sql_quote($id_article),
-			'type_liaison=' . sql_quote($type_liaison)
-		));
+		action_a2a_supprimer_lien_dist($id_article, $id_article_cible, $type_liaison='');
+	} else {
+		// invalider les caches
+		include_spip('inc/invalideur');
+		suivre_invalideur("id='id_article/$id_article'");
 	}
-	// invalider les caches
-	include_spip('inc/invalideur');
-	suivre_invalideur("id='id_article/$id_article'");
 	return true;
 }
 
