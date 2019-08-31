ce169415d9a2bce1a6e0cd4be3dff81402a29c9f
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-08-31 10:52:09 +0000

    - Utilisation des fonctions de SPIP pour retrouver les infos sur l'objet ; - Mise en place d'une fonction pour retrouver la chaine de langue d'un objet. Cela évite d'avoir un code pas très lisible dans les squelettes ; - Dans la fonction doc_tech_chaine_langue(), on charge le fichier de langue pour avoir la traduction où que l'on soit dans les squelettes. J'ai rencontré le problème avec le plugin Pays où la langue n'était pas chargée avec ['i18n_pays_fr']. - Up de y. TODO : créer une fonction qui permettrait de récupérer les chaines de langue utilisées dans les formulaires/editer_xxxx.html de l'objet éditorial. Ça serait peut-être un peu complexe pour formulaires/editer_document.html et formulaires/editer_auteur.html
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@117694 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- doc_tech/trunk/doc_tech_fonctions.php
+++ doc_tech/trunk/doc_tech_fonctions.php
@@ -13,6 +13,13 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
 
+/**
+ * Lister tous les objets ayant une chaine de langue de type lang/objet_lang.php
+ * Exemple : `lang/projet_fr.php`
+ * `lang/forum_fr.php`
+ *
+ * @return array
+ */
 function doc_tech_lister_objet() {
 	include_spip('base/objets');
 	include_spip('inc/config');
@@ -33,4 +40,33 @@ function doc_tech_lister_objet() {
 	}
 
 	return $liste_objet;
+}
+
+
+function doc_tech_chaine_langue($objet, $champ, $sufix) {
+	$langue_site = $GLOBALS['meta']['langue_site'];
+	$traduction = '';
+	include_spip('inc/traduire');
+	charger_langue($langue_site, $objet);
+	/*
+	echo "<pre>" . print_r($objet, true) . "\n"
+		. print_r($champ, true) . "\n"
+		. print_r($sufix, true) . "\n"
+		. "</pre>";
+	*/
+	/**
+	 * Dans certains cas, une chaine de langue est bien trouvée par cette fonction. cf. page ?exec=doc_tech
+	 * Et dans d'autres cas, cf. ?exec=doc_tech_lang, la chaine de langue 'correcte' n'est pas trouvée. Exemple: objet pays (avec le plugin Pays)
+	 */
+	if (isset($GLOBALS['i18n_' . $objet . '_' . $langue_site])) {
+		if (isset($GLOBALS['i18n_' . $objet . '_' . $langue_site]['champ_' . $champ . '_' . $sufix])) {
+			$traduction = $GLOBALS['i18n_' . $objet . '_' . $langue_site]['champ_' . $champ . '_' . $sufix];
+		} else if (isset($GLOBALS['i18n_' . $objet . '_' . $langue_site][$champ . '_' . $sufix])) {
+				$traduction = $GLOBALS['i18n_' . $objet . '_' . $langue_site][$champ . '_' . $sufix] ;
+		} else if (isset($GLOBALS['i18n_' . $objet . '_' . $langue_site][$sufix . '_' . $champ])) {
+					$traduction = $GLOBALS['i18n_' . $objet . '_' . $langue_site][$sufix . '_' . $champ];
+		}
+	}
+
+	return $traduction;
 }
