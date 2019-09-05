24bbade1ee31f837d89c45de3152c7f06758a2e0
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-09-05 17:32:58 +0000

    un var_dump qui trainait
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@117723 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- zippeur/trunk/genie/zippeur_effacer_zip.php
+++ zippeur/trunk/genie/zippeur_effacer_zip.php
@@ -6,7 +6,6 @@ function genie_zippeur_effacer_zip_dist($t){
 	$info = sql_select("id_zip,nom",'spip_zippeur','date_zip +INTERVAL delai_suppression SECOND < NOW() AND delai_suppression > 0','0,'._ZIPPEUR_MAX_EFFACER_ZIP); 
 	while ($ligne = sql_fetch($info) ){
 		defined('_DIR_SITE') ? $chemin = _DIR_SITE._NOM_TEMPORAIRES_ACCESSIBLES.'cache-zip/'.$ligne['nom'].".zip" : $chemin = _DIR_RACINE._NOM_TEMPORAIRES_ACCESSIBLES.'cache-zip/'.$ligne['nom'].".zip";
-		var_dump($chemin);
 		function_exists ('effacer_repertoire') ? effacer_repertoire(zippeur_chemin_dossier_local().$ligne['nom']) else "SPIP < 3, impossible d'effacer ".zippeur_chemin_dossier_local().$ligne['nom'],"zippeur");
 		if (supprimer_fichier($chemin) or !file_exists($chemin)){
 			spip_log("Suppression de ".$chemin,"zippeur");
