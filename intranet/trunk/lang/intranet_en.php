3776a68e892462aa3a5069ef2f6e37c1d2003a06
jacques@jack31.net <jacques@jack31.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-09-10 03:40:45 +0000

    [Salvatore] [source:_plugins_/intranet/trunk/lang/ intranet] Export depuis http://trad.spip.net de la langue en
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@117796 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- intranet/trunk/lang/intranet_en.php
+++ intranet/trunk/lang/intranet_en.php
@@ -13,6 +13,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'cfg_titre_intranet' => 'Intranet/Extranet',
 
 	// E
+	'explications_hosts' => 'Host names, separated by commas, allowed to connect.',
 	'explications_message' => 'Customized message to display above the login form.',
 	'explications_pages_intranet' => 'Add SPIP specific pages accessible without being connected (separate them with commas ",").',
 	'explications_plageip' => 'IP or IP range to view the entire website without being connected. Example: <code> 10.5.0.1-10.5.22.13,10.6.134.132 </ code>',
@@ -22,6 +23,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'info_intranet_texte' => 'This site is accessible only to those who are logged in.',
 
 	// L
+	'label_hosts' => 'Authorized Hosts',
 	'label_intranet_ouverts' => 'Allow to remove editorial objects one by one from the intranet',
 	'label_message' => 'Customizing the message',
 	'label_pages_intranet' => 'Accessible pages',
