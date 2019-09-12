80e3b83c8f777269b37af96953f656c91cde899a
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-09-12 21:30:01 +0000

    phpdoc sur _T_ou_typo correspondant à celle de saisies
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@117838 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spip-bonux-3/spip_bonux_options.php
+++ spip-bonux-3/spip_bonux_options.php
@@ -65,19 +65,21 @@ function spip_bonux_affichage_final($flux) {
 }
 
 
-/**
- * une fonction qui regarde si $texte est une chaine de langue
- * de la forme <:qqch:>
- * si oui applique _T()
- * si non applique typo() suivant le mode choisi
- *
- * @param unknown_type $valeur Une valeur à tester. Si c'est un tableau, la fonction s'appliquera récursivement dessus.
- * @param string $mode_typo Le mode d'application de la fonction typo(), avec trois valeurs possibles "toujours", "jamais" ou "multi".
- * @return unknown_type Retourne la valeur éventuellement modifiée.
- */
 if (!function_exists('_T_ou_typo')) {
+	/**
+	 * une fonction qui regarde si $texte est une chaine de langue
+	 * de la forme <:qqch:>
+	 * si oui applique _T()
+	 * si non applique typo() suivant le mode choisi
+	 *
+	 * @param mixed $valeur
+	 *     Une valeur à tester. Si c'est un tableau, la fonction s'appliquera récursivement dessus.
+	 * @param string $mode_typo
+	 *     Le mode d'application de la fonction typo(), avec trois valeurs possibles "toujours", "jamais" ou "multi".
+	 * @return mixed
+	 *     Retourne la valeur éventuellement modifiée.
+	 */
 	function _T_ou_typo($valeur, $mode_typo = 'toujours') {
-
 		// Si la valeur est bien une chaine (et pas non plus un entier déguisé)
 		if (is_string($valeur) and !intval($valeur)) {
 			// Si la chaine est du type <:truc:> on passe à _T()
