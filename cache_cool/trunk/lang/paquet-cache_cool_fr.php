aa77b46e9a53673b00aea2853a93b39fed85c855
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-09-25 15:53:37 +0000

    Passage au format paquet.xll, v1, et on supporte SPIP 3.1+
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@117957 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ cache_cool/trunk/lang/paquet-cache_cool_fr.php
@@ -0,0 +1,19 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+// Fichier produit par PlugOnet
+// Module: paquet-cache_cool
+// Langue: fr
+// Date: 25-09-2019 17:48:49
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cache_cool_description' => 'Servir le cache froid pour aller plus vite, et calculer en différé en tâche de fond',
+	'cache_cool_slogan' => 'Servir les pages au plus vite !',
+);
+?>
