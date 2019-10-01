353b682ff023b5f680c4fcf7c7963647ae8fa354
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-10-01 03:42:23 +0000

    [Salvatore] [source:_plugins_/verifier_plugins/trunk/lang/ paquet-verifier_plugins] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@118003 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- verifier_plugins/trunk/lang/paquet-verifier_plugins_fr.php
+++ verifier_plugins/trunk/lang/paquet-verifier_plugins_fr.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/verifier_plugins/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
@@ -10,5 +10,5 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// V
 	'verifier_plugins_description' => 'Vérifie la compatiblité de vos plugins avant une mise à jour de version majeure de SPIP',
 	'verifier_plugins_nom' => 'Vérifier la compatibilité de vos plugins',
-	'verifier_plugins_slogan' => 'Vérifie la compatiblité de vos plugins avant une mise à jour de version majeure de SPIP',
+	'verifier_plugins_slogan' => 'Vérifie la compatiblité de vos plugins avant une mise à jour de version majeure de SPIP'
 );
