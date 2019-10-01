4fcac34426c03e28eab40ada480d018a301ea5ff
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-10-01 03:42:20 +0000

    [Salvatore] [source:_plugins_/verifier_plugins/trunk/lang/ verifier_plugins] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@118001 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- verifier_plugins/trunk/lang/verifier_plugins_fr.php
+++ verifier_plugins/trunk/lang/verifier_plugins_fr.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/verifier_plugins/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
@@ -15,20 +15,19 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'liste_plugins' => 'Liste des plugins actifs',
 	'liste_plugins_inactifs' => 'Liste des plugins inactifs',
 
-	// T
-	'titre_verifier_plugins' => 'Vérifier la compatibilité des plugins avec un autre version de SPIP',
-	'titre_verifier_plugins_version' => 'Vérifier la compatibilité des plugins avec SPIP&nbsp;@version@',
-
 	// P
-	'plugin_nom' => 'Nom du plugin',
 	'plugin_borne' => 'Bornes',
-	'plugin_compat_version' => 'Compatible SPIP&nbsp;@version@',
-	'plugin_compat_version_ok' => 'OK',
-	'plugin_compat_version_notok' => 'Le plugin actuellement installé est signalé comme incompatible ou à vérifier',
+	'plugin_compat_version' => 'Compatible SPIP @version@',
 	'plugin_compat_version_maj' => 'La dernière version du plugin sur la zone est compatible, veuillez mettre votre plugin à jour.',
 	'plugin_compat_version_maj_gestion' => 'Gestion des plugins',
+	'plugin_compat_version_notok' => 'Le plugin actuellement installé est signalé comme incompatible ou à vérifier',
+	'plugin_compat_version_ok' => 'OK',
+	'plugin_nom' => 'Nom du plugin',
+
+	// T
+	'titre_verifier_plugins' => 'Vérifier la compatibilité des plugins avec un autre version de SPIP',
+	'titre_verifier_plugins_version' => 'Vérifier la compatibilité des plugins avec SPIP @version@',
 
 	// V
-	'version_cible' => 'Afficher la compatibilité des plugins pour',
-
+	'version_cible' => 'Afficher la compatibilité des plugins pour'
 );
