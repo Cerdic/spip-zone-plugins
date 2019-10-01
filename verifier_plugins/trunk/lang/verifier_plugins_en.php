6eda9252d3220841288bb5f0b87113bc59cd218d
jacques@jack31.net <jacques@jack31.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-10-01 03:42:18 +0000

    [Salvatore] [source:_plugins_/verifier_plugins/trunk/lang/ verifier_plugins] Export depuis http://trad.spip.net de la langue en
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@118000 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ verifier_plugins/trunk/lang/verifier_plugins_en.php
@@ -0,0 +1,35 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/verifier_plugins?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'bilan' => '@total@ active plugin(s) - @total_notok@ incompatible plugin(s)',
+	'bilan_inactif' => '@total@ active plugin(s) - @total_notok@ incompatible plugin(s)',
+
+	// L
+	'liste_plugins' => 'List of active plugins',
+	'liste_plugins_inactifs' => 'List of inactive plugins',
+
+	// P
+	'plugin_borne' => 'Limits', # RELIRE
+	'plugin_compat_version' => 'SPIP @version@ compatible ',
+	'plugin_compat_version_maj' => 'The latest version of the plugin on the zone is compatible, please update your plugin.',
+	'plugin_compat_version_maj_gestion' => 'Plugins management',
+	'plugin_compat_version_notok' => 'The plugin currently installed is reported as incompatible or to be checked',
+	'plugin_compat_version_ok' => 'OK',
+	'plugin_nom' => 'Plugin name',
+
+	// T
+	'titre_verifier_plugins' => 'Check the compatibility of plugins with another version of SPIP',
+	'titre_verifier_plugins_version' => 'Check plugin compatibility with SPIP @version@',
+
+	// V
+	'version_cible' => 'Display plugin compatibility for'
+);
