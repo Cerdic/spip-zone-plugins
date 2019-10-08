964fa47645983c97c3081952840b62d10d15f10c
Samuel <Samuel@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-10-08 03:41:20 +0000

    Ajout du choix de la méthode de transport (image, xhr ou beacon)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@118059 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ google_analytics/branches/transport_method/lang/googleanalytics_en.php
@@ -0,0 +1,25 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	// C
+	'cfg_descriptif' => 'Here you can provide your Google Analytics ID.',
+	'cfg_titre' => 'Google analytics',
+
+	// E
+	'explication_id_google' => 'If "_" or empty, removes the functionality (empty revert to default).',
+
+	// L
+	'label_id_google' => 'Your Google Analytics ID like "UA-12345"',
+	'label_ga_universal' => 'Use <a href="https://support.google.com/analytics/answer/2790010?hl%3Den">Google Analytics Universal</a>',
+	'label_ga_transport' => 'Transport method (Only with Universal)',
+
+	// T
+	'transport_par_defaut' => 'Default',
+	'transport_image' => 'image',
+	'transport_xhr' => 'xhr',
+	'transport_beacon' => 'beacon (if supported by browser)',
+);
+?>
