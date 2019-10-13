9e3a4b315c4c44f8888db309da22c0703ba89f1d
jcvilleneuve@cegetel.net <jcvilleneuve@cegetel.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-10-13 15:10:06 +0000

    ajout d'un icone dans le porte-plume
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@118126 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- onglets_texte/trunk/onglets_texte_pipelines.php
+++ onglets_texte/trunk/onglets_texte_pipelines.php
@@ -21,3 +21,27 @@ function onglets_texte_insert_head_css($flux) {
 
 	return $flux;
 }
+
+function onglets_texte_porte_plume_barre_pre_charger($barres){
+	$barre_edition = &$barres['edition'];
+
+	$module_barre = "barre_outils";
+	if (intval($GLOBALS['spip_version_branche'])>2)
+	$module_barre = "barreoutils";
+					
+	$barre_edition->ajouterApres('barre_cadre', array(
+		"id"          => 'onglets_texte',
+		"name"        => 'Insérer un système d\'onglets',
+		"className"   => "outil_onglets",
+		"replaceWith" => "\n<onglet|debut|titre=Titre du premier onglet>\ncontenu du premier onglet\n\n<onglet|titre=Titre du deuxième onglet>\ncontenu du deuxième onglet\n\n<onglet|titre=Titre du troisième onglet>\ncontenu du troisième onglet\n\n<onglet|fin>\n",
+		"display"     => true,
+	));
+	
+	return $barres;
+}
+
+function onglets_texte_porte_plume_lien_classe_vers_icone($flux){
+	return array_merge($flux, array(
+		'outil_onglets' => array('onglets.png','0'),
+	));
+}
