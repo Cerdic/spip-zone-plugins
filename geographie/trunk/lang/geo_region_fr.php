cb420d6472953b2d49c98ac4e7ab8472641e59e2
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-10-14 17:09:28 +0000

    Saisies région : un constructeur YAML. Soit dis en passant, il faudrait passer aux nouvelles régions, non ?
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@118137 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- geographie/trunk/lang/geo_region_fr.php
+++ geographie/trunk/lang/geo_region_fr.php
@@ -18,6 +18,8 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'info_geo_regions_auteur' => 'Les régions de cet auteur',
 	'info_nb_geo_regions' => '@nb@ régions',
 
+	// L
+	'label_region' => 'Région',
 	// R
 	'retirer_lien_geo_region' => 'Retirer cette région',
 	'retirer_tous_liens_geo_regions' => 'Retirer toutes les régions',
@@ -32,5 +34,6 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'titre_geo_region' => '(Géo) Région',
 	'titre_geo_regions' => '(Géo) Régions',
 	'titre_geo_regions_rubrique' => 'Régions de la rubrique',
+	'titre_saisie_region' => 'La liste des régions de France',
 	'titre_logo_geo_region' => 'Logo de cette région',
 );
