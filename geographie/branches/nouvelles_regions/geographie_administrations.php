b217dab88a5f120724c89b23e83633eb1814bd74
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-10-14 17:11:42 +0000

    Création d'une branche pour travailler sur la migration vers les nouvelles régions, selon la méthode proposée par Cédric.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@118138 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ geographie/branches/nouvelles_regions/geographie_administrations.php
@@ -0,0 +1,109 @@
+<?php
+// Sécurité
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+/**
+ * Fonction d'installation et de mise à jour du plugin Géographie
+ *
+ * @param string $nom_meta_base_version
+ *     Nom de la meta informant de la version du schéma de données du plugin installé dans SPIP
+ * @param string $version_cible
+ *     Version du schéma de données dans ce plugin (déclaré dans paquet.xml)
+ *
+ * @return void
+ **/
+function geographie_upgrade($nom_meta_base_version, $version_cible) {
+	$maj = array();
+
+	// Installation du plugin
+	$maj['create'] = array(
+		array('creer_base'),
+		array('geographie_upgrade_importer_geographie'), // importation de presque toute la géo sauf arrondissements
+		array('geographie_upgrade_importer_arrondissements'), // importation des arrondissements
+	);
+
+	// On refait la base des pays
+	$maj['0.2.0'] = array(
+		array('sql_drop_table', 'spip_geo_pays'),
+		array('maj_tables', 'spip_geo_pays'),
+		array('geographie_upgrade_importer_pays'),
+	);
+
+	// On ajoute les arrondissements
+	$maj['0.3.0'] = array(
+		array('maj_tables', 'spip_geo_arrondissements'),
+		array('geographie_upgrade_importer_arrondissements'),
+	);
+
+	// On refait encore les pays
+	$maj['0.4.0'] = array(
+		array('sql_drop_table', 'spip_geo_pays'),
+		array('maj_tables', 'spip_geo_pays'),
+		array('geographie_upgrade_importer_pays'),
+	);
+
+	// Coquille dans la description de midi-pyrénnées
+	$maj['0.4.2'] = array(
+		array('sql_delete', 'spip_geo_departements', 'nom=' . sql_quote('09')),
+	);
+
+	// Toutes les tables de liens
+	$maj['1.0.0'] = array(
+		array(
+			'maj_tables',
+			array(
+				'spip_geo_pays_liens',
+				'spip_geo_regions_liens',
+				'spip_geo_departements_liens',
+				'spip_geo_arrondissements_liens',
+				'spip_geo_communes_liens',
+			),
+		),
+	);
+
+	include_spip('base/upgrade');
+	maj_plugin($nom_meta_base_version, $version_cible, $maj);
+}
+
+function geographie_upgrade_importer_geographie() {
+	if ($importer_geographie = charger_fonction('geographie', 'imports')) {
+		$importer_geographie();
+	}
+}
+
+function geographie_upgrade_importer_arrondissements() {
+	if ($importer_arrondissements = charger_fonction('arrondissements', 'imports')) {
+		$importer_arrondissements();
+	}
+}
+
+function geographie_upgrade_importer_pays() {
+	include_spip('imports/pays');
+	sql_insertq_multi('spip_geo_pays', $GLOBALS['liste_pays']);
+}
+
+/**
+ * Fonction de désinstallation du plugin Géographie
+ *
+ * @param string $nom_meta_base_version
+ *     Nom de la meta informant de la version du schéma de données du plugin installé dans SPIP
+ *
+ * @return void
+ **/
+function geographie_vider_tables($nom_meta_base_version) {
+	sql_drop_table('spip_geo_pays');
+	sql_drop_table('spip_geo_regions');
+	sql_drop_table('spip_geo_departements');
+	sql_drop_table('spip_geo_arrondissements');
+	sql_drop_table('spip_geo_communes');
+
+	sql_drop_table('spip_geo_pays_liens');
+	sql_drop_table('spip_geo_regions_liens');
+	sql_drop_table('spip_geo_departements_liens');
+	sql_drop_table('spip_geo_arrondissements_liens');
+	sql_drop_table('spip_geo_communes_liens');
+
+	effacer_meta($nom_meta_base_version);
+}
---
+++ geographie/branches/nouvelles_regions/geographie_autorisations.php
@@ -0,0 +1,130 @@
+<?php
+/**
+ * Définit les autorisations du plugin GéoCulture : noyau fonctionnel
+ *
+ * @plugin     GéoCulture : noyau fonctionnel
+ * @copyright  2016
+ * @author     Les Développements Durables
+ * @licence    GNU/GPL
+ * @package    SPIP\Geoculture_core\Autorisations
+ */
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+/**
+ * Fonction d'appel pour le pipeline
+ *
+ * @pipeline autoriser
+ */
+function geographie_autoriser() {
+}
+
+// -----------------
+// Objet geo_communes
+
+/**
+ * Autorisation de voir un élément de menu (geocommunes)
+ *
+ * @param  string $faire Action demandée
+ * @param  string $type  Type d'objet sur lequel appliquer l'action
+ * @param  int    $id    Identifiant de l'objet
+ * @param  array  $qui   Description de l'auteur demandant l'autorisation
+ * @param  array  $opt   Options de cette autorisation
+ *
+ * @return bool          true s'il a le droit, false sinon
+ **/
+function autoriser_geocommunes_menu_dist($faire, $type, $id, $qui, $opt) {
+	return true;
+}
+
+/**
+ * Autorisation de voir le bouton d'accès rapide de création (geocommune)
+ *
+ * @param  string $faire Action demandée
+ * @param  string $type  Type d'objet sur lequel appliquer l'action
+ * @param  int    $id    Identifiant de l'objet
+ * @param  array  $qui   Description de l'auteur demandant l'autorisation
+ * @param  array  $opt   Options de cette autorisation
+ *
+ * @return bool          true s'il a le droit, false sinon
+ **/
+function autoriser_geocommunecreer_menu_dist($faire, $type, $id, $qui, $opt) {
+	return autoriser('creer', 'geoculture_artiste', '', $qui, $opt);
+}
+
+/**
+ * Autorisation de créer (geocommune)
+ *
+ * @param  string $faire Action demandée
+ * @param  string $type  Type d'objet sur lequel appliquer l'action
+ * @param  int    $id    Identifiant de l'objet
+ * @param  array  $qui   Description de l'auteur demandant l'autorisation
+ * @param  array  $opt   Options de cette autorisation
+ *
+ * @return bool          true s'il a le droit, false sinon
+ **/
+function autoriser_geocommune_creer_dist($faire, $type, $id, $qui, $opt) {
+	return in_array($qui['statut'], array('0minirezo', '1comite'));
+}
+
+/**
+ * Autorisation de voir (geocommune)
+ *
+ * @param  string $faire Action demandée
+ * @param  string $type  Type d'objet sur lequel appliquer l'action
+ * @param  int    $id    Identifiant de l'objet
+ * @param  array  $qui   Description de l'auteur demandant l'autorisation
+ * @param  array  $opt   Options de cette autorisation
+ *
+ * @return bool          true s'il a le droit, false sinon
+ **/
+function autoriser_geocommune_voir_dist($faire, $type, $id, $qui, $opt) {
+	return true;
+}
+
+/**
+ * Autorisation de modifier (geocommune)
+ *
+ * @param  string $faire Action demandée
+ * @param  string $type  Type d'objet sur lequel appliquer l'action
+ * @param  int    $id    Identifiant de l'objet
+ * @param  array  $qui   Description de l'auteur demandant l'autorisation
+ * @param  array  $opt   Options de cette autorisation
+ *
+ * @return bool          true s'il a le droit, false sinon
+ **/
+function autoriser_geocommune_modifier_dist($faire, $type, $id, $qui, $opt) {
+	return $qui['statut'] == '0minirezo' AND !$qui['restreint'];
+}
+
+/**
+ * Autorisation de supprimer (geocommune)
+ *
+ * @param  string $faire Action demandée
+ * @param  string $type  Type d'objet sur lequel appliquer l'action
+ * @param  int    $id    Identifiant de l'objet
+ * @param  array  $qui   Description de l'auteur demandant l'autorisation
+ * @param  array  $opt   Options de cette autorisation
+ *
+ * @return bool          true s'il a le droit, false sinon
+ **/
+function autoriser_geocommune_supprimer_dist($faire, $type, $id, $qui, $opt) {
+	return $qui['statut'] == '0minirezo' AND !$qui['restreint'];
+}
+
+/**
+ * Autorisation de lier/délier l'élément (geocommunes)
+ *
+ * @param  string $faire Action demandée
+ * @param  string $type  Type d'objet sur lequel appliquer l'action
+ * @param  int    $id    Identifiant de l'objet
+ * @param  array  $qui   Description de l'auteur demandant l'autorisation
+ * @param  array  $opt   Options de cette autorisation
+ *
+ * @return bool          true s'il a le droit, false sinon
+ **/
+function autoriser_associergeocommunes_dist($faire, $type, $id, $qui, $opt) {
+	return in_array($qui['statut'], array('0minirezo', '1comite'));
+}
