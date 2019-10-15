65b8ebac8449a91b1b77328877cd66a7553e52dc
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-10-15 16:17:04 +0000

    finalement pas le bon plan, on supprime cette branche
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@118153 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- geographie/branches/nouvelles_regions/lang/geographie_fr.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (!defined('_ECRIRE_INC_VERSION')) {
-	return;
-}
-
-$GLOBALS[$GLOBALS['idx_lang']] = array(
-	'configurer_communes_lier_objets_label' => 'Activer la liaison de communes sur les contenus',
-	'configurer_titre' => 'Paramétrer les éléments géographiques',
-);
