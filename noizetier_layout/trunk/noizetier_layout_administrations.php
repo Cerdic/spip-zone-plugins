2fb0e67ec2672235dfd70f5d86de5b3a419bd345
Charles Razack <Charles Razack@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-10-18 15:29:49 +0000

    Allez on est sympa : migration des données suite aux derniers renommages. up z.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@118205 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- noizetier_layout/trunk/noizetier_layout_administrations.php
+++ noizetier_layout/trunk/noizetier_layout_administrations.php
@@ -31,6 +31,11 @@ function noizetier_layout_upgrade($nom_meta_base_version, $version_cible) {
 		array('ecrire_config', 'noizetier_layout', $config),
 	);
 
+	// schéma 0.2.0 : renommages
+	$maj['0.2.0'] = array(
+		array('noizetier_layout_maj_020'),
+	);
+
 	include_spip('base/upgrade');
 	maj_plugin($nom_meta_base_version, $version_cible, $maj);
 }
@@ -50,4 +55,70 @@ function noizetier_layout_vider_tables($nom_meta_base_version) {
 	effacer_meta($nom_meta_base_version);
 	// On efface la configuration du plugin
 	effacer_meta('noizetier_layout');
+}
+
+/**
+ * Mise à jour vers le schema 0.2.0
+ *
+ * Mettre à jour les noisettes suite à divers renommages.
+ *
+ * @return void
+ */
+function noizetier_layout_maj_020() {
+	include_spip('base/abstract_sql');
+	if ($noisettes = sql_allfetsel('*', 'spip_noisettes')) {
+		foreach ($noisettes as $noisette) {
+			$set = array();
+			$type_ancien = 'conteneur_row';
+			$type_nouveau = 'conteneur';
+			// type_noisette : `conteneur_row` → `conteneur`
+			if ($noisette['type_noisette'] == $type_ancien) {
+				$set['type_noisette'] = $type_nouveau;
+			}
+			// id_conteneur : `conteneur_row` → `conteneur`
+			if (
+				$id_conteneur = str_replace($type_ancien, $type_nouveau, $noisette['id_conteneur'])
+				and $id_conteneur != $noisette['id_conteneur']
+			) {
+				$set['id_conteneur'] = $id_conteneur;
+			}
+			// conteneur : `conteneur_row` → `conteneur`
+			if (
+				$conteneur = unserialize($noisette['conteneur'])
+				and $conteneur['type_noisette'] == $type_ancien
+			) {
+				$conteneur['type_noisette'] = $type_nouveau;
+				$set['conteneur'] = serialize($conteneur);
+			}
+			// parametres : `css_container` → `css_grid_container` etc.
+			if ($parametres = unserialize($noisette['parametres'])) {
+				$map = array(
+					'css_container' => 'css_grid_container',
+					'css_row'       => 'css_grid_row',
+					'css_column'    => 'css_grid_column',
+				);
+				foreach ($map as $avant => $apres) {
+					if (isset($parametres[$avant])) {
+						$parametres[$apres] = $parametres[$avant];
+						unset($parametres[$avant]);
+					}
+				}
+				$parametres = serialize($parametres);
+				if ($parametres != $noisette['parametres']) {
+					$set['parametres'] = $parametres;
+				}
+			}
+			// màj
+			if ($set) {
+				sql_updateq(
+					'spip_noisettes',
+					$set,
+					'id_noisette='.intval($noisette['id_noisette'])
+				);
+			}
+		}
+	}
+	// Invalider le cache des types de noisettes
+	include_spip('inc/ncore_type_noisette');
+	type_noisette_charger('noizetier', true);
 }
