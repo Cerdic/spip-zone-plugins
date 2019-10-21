cce4f6b4b397a02fd800d2c83aca6e96529b8dd8
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-10-21 15:07:52 +0000

    Début de mise en conformité de l'api avec ezcollection
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@118237 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ svp_api/trunk/svpapi_pipelines.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Ce fichier contient la fonction de déclaration des configurations de collections. Elle appelle le pipeline
+ * `declarer_collections_svp` pour les plugins qui le souhaitent (voir SVP Typologie).
+ *
+ * @package SPIP\SVPAPI\EZCOLLECTION\COLLECTION
+ */
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+/**
+ * Déclare les collections accessibles via HTTP GET.
+ * Par défaut, le plugin propose les collections `plugins` et `depots`.
+ *
+ * @pipeline liste_ezcollection
+ *
+ * @param array $collections Configuration des collections déjà déclarées.
+ *
+ * @return array Collections complétées.
+**/
+function svpapi_liste_ezcollection($collections) {
+
+	// Initialisation du tableau des collections
+	if (!$collections) {
+		$collections = array();
+	}
+
+	// Les index désignent les collections, le tableau associé contient les filtres admissibles.
+	$collections['plugins'] = array(
+		'ressource' => 'prefixe',
+		'module'    => 'svpapi',
+		'filtres'   => array(
+			array(
+				'critere'         => 'compatible_spip',
+				'est_obligatoire' => false
+			),
+		)
+	);
+
+	$collections['depots'] = array(
+		'module'  => 'svpapi',
+		'filtres' => array(
+			array(
+				'critere'         => 'type',
+				'est_obligatoire' => false
+			)
+		)
+	);
+
+	return $collections;
+}
