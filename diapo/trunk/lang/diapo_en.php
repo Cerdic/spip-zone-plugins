b668f74591988f0e4b03e34dc4633ee9c5f16af1
jcvilleneuve@cegetel.net <jcvilleneuve@cegetel.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-10-22 11:23:10 +0000

    Adaptatation du plugin à spip3
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@118255 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- diapo/trunk/lang/diapo_en.php
+++ diapo/trunk/lang/diapo_en.php
@@ -4,6 +4,17 @@
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 'diapo'=>'Slideshow',
+'diapo_config'=>'Diapo plugin configuration',
+'nombre_vignettes'=>'number of thumbnails per page',
+'nombre_vignettes_explication'=>'default 15',
+'taille_diapo_grand'=>'maximum size of the big picture',
+'taille_diapo_grand_explication'=>'slideshow and centered, default 500',
+'taille_diapo_petit'=>'maximum size of the small picture',
+'taille_diapo_petit_explication'=>'with thumbnails on the side, default 300',
+'taille_vignettes'=>'thumbnail size',
+'taille_vignettes_explication'=>'default 60',
+'temps_pause'=>'pause time of the slideshow',
+'temps_pause_explication'=>'default 5000',
 'txt_icodiapo'=>'Slideshow play/pause',
 'txt_icoleft'=>'Thumbnails on the left',
 'txt_icoright'=>'Thumbnails on the right',
--- diapo/trunk/lang/diapo_fr.php
+++ diapo/trunk/lang/diapo_fr.php
@@ -4,6 +4,17 @@
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 'diapo'=>'Diaporama',
+'diapo_config'=>'Configuration du plugin Diapo',
+'nombre_vignettes'=>'nombre de vignettes par page',
+'nombre_vignettes_explication'=>'par défaut 15',
+'taille_diapo_grand'=>'taille maximum de la grande image',
+'taille_diapo_grand_explication'=>'diaporama et centré, par défaut 500',
+'taille_diapo_petit'=>'taille maximum de la petite image',
+'taille_diapo_petit_explication'=>'avec vignettes sur le côté, par défaut 300',
+'taille_vignettes'=>'taille des vignettes',
+'taille_vignettes_explication'=>'par défaut 60',
+'temps_pause'=>'temps de pause du diaporama',
+'temps_pause_explication'=>'par défaut 5000',
 'txt_icodiapo'=>'Diaporama lecture/pause',
 'txt_icoleft'=>'Vignettes &agrave; gauche',
 'txt_icoright'=>'Vignettes &agrave; droite',
@@ -15,6 +26,3 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 'ico_center'=>'&nbsp;',
 'loading'=>'Chargement en cours...'
 );
-
-
-?>
