172231f72998b72ec511613a8643943e21135a14
jluc@no-log.org <jluc@no-log.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-10-22 05:50:10 +0000

    passage de diapo en trunk v1 dev et branches v0 stable
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@118249 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ diapo/branches/v0/lang/diapo_en.php
@@ -0,0 +1,20 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+'diapo'=>'Slideshow',
+'txt_icodiapo'=>'Slideshow play/pause',
+'txt_icoleft'=>'Thumbnails on the left',
+'txt_icoright'=>'Thumbnails on the right',
+'txt_icocenter'=>'Thumbnails on top (large image)',
+'ico_diapo_play'=>'&gt;',
+'ico_diapo_pause'=>'||',
+'ico_left'=>'&nbsp;',
+'ico_right'=>'&nbsp;',
+'ico_center'=>'&nbsp;',
+'loading'=>'Chargement en cours...'
+);
+
+
+?>
---
+++ diapo/branches/v0/lang/diapo_fr.php
@@ -0,0 +1,20 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+'diapo'=>'Diaporama',
+'txt_icodiapo'=>'Diaporama lecture/pause',
+'txt_icoleft'=>'Vignettes &agrave; gauche',
+'txt_icoright'=>'Vignettes &agrave; droite',
+'txt_icocenter'=>'Vignettes en haut (grande image)',
+'ico_diapo_play'=>'&gt;',
+'ico_diapo_pause'=>'||',
+'ico_left'=>'&nbsp;',
+'ico_right'=>'&nbsp;',
+'ico_center'=>'&nbsp;',
+'loading'=>'Chargement en cours...'
+);
+
+
+?>
