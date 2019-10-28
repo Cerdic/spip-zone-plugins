5f0aaafb805fd19a9608578420d8ff0051a06ba9
Charles Razack <Charles Razack@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-10-28 17:52:09 +0000

    Ajout d'un fichier de langue en anglais (juste une partie des traductions). Si mes informations sont exactes, il ne devrait pas être écrasé lors de l'ajout du plugin dans les traductions. Sinon, pas de chance !
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@118325 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ profils/trunk/lang/profil_en.php
@@ -0,0 +1,100 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'ajouter_lien_profil' => 'Add this profil',
+
+	// C
+	'champ_config_activer_coordonnees_auteur_label_case' => 'Ajouter des coordonnées pour le compte utilisateur',
+	'champ_config_activer_coordonnees_contact_label_case' => 'Ajouter des coordonnées pour le contact',
+	'champ_config_activer_coordonnees_organisation_label_case' => 'Ajouter des coordonnées pour l’organisation',
+	'champ_config_activer_contact_explication_texte' => 'En activant à la fois des informations d’organisation et de contact, c’est toujours l’organisation qui sera la fiche principale liée au compte utilisateur, et le contact sera lié à l’organisation.',
+	'champ_config_activer_contact_label_case' => 'Ajouter des informations de contact (personne physique)',
+	'champ_config_activer_groupe_contact_label' => 'Regrouper les champs du contact',
+	'champ_config_activer_groupe_explication' => 'Si ce champ est rempli, cela créera un groupe de champ avec cet intitulé.',
+	'champ_config_activer_groupe_organisation_label' => 'Regrouper les champs de l’organisation',
+	'champ_config_activer_organisation_label_case' => 'Ajouter des informations d’organisation (personne morale)',
+	'champ_config_auteur_caption' => 'Informations du compte utilisateur',
+	'champ_config_contact_caption' => 'Informations du contact',
+	'champ_config_organisation_caption' => 'Informations de l’organisation',
+	'champ_config_colonne_edition_label' => 'Édition',
+	'champ_config_colonne_inscription_label' => 'Inscription',
+	'champ_config_colonne_obligatoire_label' => 'Obligatoire',
+	'champ_config_coordonnees_auteur_caption' => 'Coordonnées du compte utilisateur',
+	'champ_config_coordonnees_choix_sans_type' => 'Sans type',
+	'champ_config_coordonnees_contact_caption' => 'Coordonnées du contact',
+	'champ_config_coordonnees_label_label' => 'Label facultatif',
+	'champ_config_coordonnees_organisation_caption' => 'Coordonnées de l’organisation',
+	'champ_config_coordonnees_type_label' => 'Type',
+	'champ_config_coordonnees_explication' => 'Pour ajouter plusieurs coordonnées, il en faut au moins une de cochée, si besoin revalidez le formulaire.',
+	'champ_config_id_annuaire_explication' => 'Vous avez activé l’utilisation des annuaires multiples. Si aucun annuaire n’est choisi, un annuaire du même nom que le profil sera créé.',
+	'champ_config_id_annuaire_label' => 'Quel annuaire',
+	'champ_config_objet_label' => 'Objet',
+	'champ_config_champ_label' => 'Champ/objet lié',
+	'champ_groupe_auteur_label' => 'Compte utilisateur',
+	'champ_groupe_contact_label' => 'Contact',
+	'champ_groupe_organisation_label' => 'Organisation',
+	'champ_identifiant_label' => 'Identifiant',
+	'champ_titre_label' => 'Titre',
+	'comptes_aucun' => 'Aucun compte pour ce profil',
+	'comptes_creer' => 'Créer un nouveau compte pour ce profil',
+	'comptes_liste_detaille' => 'Voir la liste détaillée',
+	'comptes_modifier' => 'Modifier un compte de profil',
+	'confirmer_supprimer_profil' => 'Confirmez-vous la suppression de cet profil ?',
+	
+	// E
+	'envoyer_notification_label_case' => 'Send a notification inviting to register a password',
+	'erreur_email_obligatoire' => 'Vous devez configurer un email obligatoire dans les champs ou dans les coordonnées.',
+	'errreur_identifiant_existant' => 'That id is already in use',
+	'exporter_champ_seulement_colonnes_label_case' => 'don’t export data, only columns',
+	'exporter_titre' => 'Exportation',
+	// I
+	'icone_creer_profil' => 'Créer un profil',
+	'icone_modifier_profil' => 'Modifier ce profil',
+	'importer_titre' => 'Importation',
+	'info_1_profil' => 'Un profil',
+	'info_aucun_profil' => 'Aucun profil',
+	'info_nb_profils' => '@nb@ profils',
+	'info_profils_auteur' => 'Les profils de cet auteur',
+	
+	// N
+	'notification_motdepasse_titre' => 'New account',
+	'notification_motdepasse_texte' => 'A new account was created for you on the website @nom_site_spip@ (@adresse_site@)
+
+Please follow that link :
+
+    @sendcookie@
+
+You’ll be able to register a password
+and connect to the site with your email address @email@.
+
+',
+	
+	// R
+	'retirer_lien_profil' => 'Remove this profil',
+	'retirer_tous_liens_profils' => 'Remove all profils',
+
+	// S
+	'supprimer_profil' => 'Delete that profil',
+
+	// T
+	'texte_ajouter_profil' => 'Add a profil',
+	'texte_changer_statut_profil' => 'This profil is :',
+	'texte_creer_associer_profil' => 'Create and associate a profil',
+	'texte_definir_comme_traduction_profil' => 'Ce profil est une traduction du profil numéro :',
+	'titre_langue_profil' => 'Langue de ce profil',
+	'titre_logo_profil' => 'Profil logo',
+	'titre_objets_lies_profil' => 'Liés à ce profil',
+	'titre_profil' => 'Profil',
+	'titre_profils' => 'Profils',
+	'titre_profils_rubrique' => 'Section’s profils',
+	'tri_croissant' => 'Tri croissant',
+	'tri_decroissant' => 'Tri décroissant',
+);
