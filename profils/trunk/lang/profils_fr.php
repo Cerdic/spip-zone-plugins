78401bc9697e705864a120f12612003ca1440667
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-10-29 04:41:01 +0000

    [Salvatore] [source:_plugins_/profils/trunk/lang/ profils] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@118328 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- profils/trunk/lang/profils_fr.php
+++ profils/trunk/lang/profils_fr.php
@@ -1,13 +1,15 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/profils/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+
 	// E
 	'erreur_autoriser_profil' => 'Ce compte n’existe pas ou vous n’avez pas le droit de le modifier',
+
 	// P
-	'profils_titre' => 'Profils',
+	'profils_titre' => 'Profils'
 );
