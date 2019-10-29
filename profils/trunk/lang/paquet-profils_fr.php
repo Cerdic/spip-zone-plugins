71b36119850cdcf6f16aaab1469f57340371e564
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-10-29 04:41:04 +0000

    [Salvatore] [source:_plugins_/profils/trunk/lang/ paquet-profils] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@118330 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- profils/trunk/lang/paquet-profils_fr.php
+++ profils/trunk/lang/paquet-profils_fr.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/profils/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
@@ -8,7 +8,7 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// P
-	'profils_description' => 'Ce plugin permet de créer des profils d\'utilisateurs en configurant quelles informations ils peuvent contenir (une fiche de personne ou d\'organisation, des coordonnées, etc). Ensuite il est possible de dire qu\'un compte utilisateur utilise tel profil.',
+	'profils_description' => 'Ce plugin permet de créer des profils d’utilisateurs en configurant quelles informations ils peuvent contenir (une fiche de personne ou d’organisation, des coordonnées, etc). Ensuite il est possible de dire qu’un compte utilisateur utilise tel profil.',
 	'profils_nom' => 'Profils',
-	'profils_slogan' => 'Gérer des profils d\'utilisateurs',
+	'profils_slogan' => 'Gérer des profils d’utilisateurs'
 );
