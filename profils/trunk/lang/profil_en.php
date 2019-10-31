f7094ac8ce89a1f227a8b472a06b04c58aa505cc
tcharlss@bravecassine.com <tcharlss@bravecassine.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-10-31 04:41:09 +0000

    [Salvatore] [source:_plugins_/profils/trunk/lang/ profil] Export depuis http://trad.spip.net de la langue en
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@118352 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- profils/trunk/lang/profil_en.php
+++ profils/trunk/lang/profil_en.php
@@ -13,47 +13,70 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'ajouter_lien_profil' => 'Add this profil',
 
 	// C
+	'champ_config_activer_contact_explication_texte' => 'When activating both contact and organization informations, the organization will be the main object associated to the user account, and the contact associated to the organization.',
+	'champ_config_activer_contact_label_case' => 'Add informations about the contact (physical person)',
+	'champ_config_activer_coordonnees_auteur_label_case' => 'Add contact information for the user account',
+	'champ_config_activer_coordonnees_contact_label_case' => 'Add contact information for the contact',
+	'champ_config_activer_coordonnees_organisation_label_case' => 'Add contact information for the organization',
+	'champ_config_activer_groupe_contact_label' => 'Group the contact fields together',
+	'champ_config_activer_groupe_explication' => 'Filling this field will create a group of fields with that title',
+	'champ_config_activer_groupe_organisation_label' => 'Group the organization fields together',
+	'champ_config_activer_organisation_label_case' => 'Add informations about the organization (corporation)',
+	'champ_config_auteur_caption' => 'Informations about the user account',
+	'champ_config_champ_label' => 'Field/object associated',
+	'champ_config_colonne_edition_label' => 'Edition',
 	'champ_config_colonne_inscription_label' => 'Inscription',
 	'champ_config_colonne_obligatoire_label' => 'Required',
+	'champ_config_contact_caption' => 'Informations about the contact',
+	'champ_config_coordonnees_auteur_caption' => 'Contact information of the user account',
+	'champ_config_coordonnees_choix_sans_type' => 'No type',
+	'champ_config_coordonnees_contact_caption' => 'Contact information of the contact', # RELIRE
+	'champ_config_coordonnees_explication' => 'To add contact information, at least one need to be checked: submit the form if needed.',
+	'champ_config_coordonnees_label_label' => 'Facultative label',
+	'champ_config_coordonnees_organisation_caption' => 'Contact information of the organization',
 	'champ_config_coordonnees_type_label' => 'Type',
+	'champ_config_id_annuaire_explication' => 'You have activated the use of multiple directories. If no directory is selected, one will be created withh the same name as the profil.',
 	'champ_config_id_annuaire_label' => 'Quel annuaire',
-	'champ_config_objet_label' => 'Objet',
-	'champ_config_organisation_caption' => 'Informations de l’organisation',
-	'champ_groupe_auteur_label' => 'Compte utilisateur',
+	'champ_config_objet_label' => 'Object',
+	'champ_config_organisation_caption' => 'Informations about the organization',
+	'champ_groupe_auteur_label' => 'User account',
 	'champ_groupe_contact_label' => 'Contact',
-	'champ_groupe_organisation_label' => 'Organisation',
-	'champ_identifiant_label' => 'Identifiant',
-	'champ_titre_label' => 'Titre',
-	'comptes_aucun' => 'Aucun compte pour ce profil',
-	'comptes_creer' => 'Créer un nouveau compte pour ce profil',
-	'comptes_liste_detaille' => 'Voir la liste détaillée',
-	'comptes_modifier' => 'Modifier un compte de profil',
-	'confirmer_supprimer_profil' => 'Confirmez-vous la suppression de cet profil ?',
+	'champ_groupe_organisation_label' => 'Organization',
+	'champ_identifiant_label' => 'Identifier',
+	'champ_titre_label' => 'Title',
+	'comptes_aucun' => 'No user account for this profil',
+	'comptes_creer' => 'Create a new user account for this profil',
+	'comptes_liste_detaille' => 'View detailled list',
+	'comptes_modifier' => 'Edit a profil account',
+	'confirmer_supprimer_profil' => 'Confirm the deletion of this profil?',
 
 	// E
 	'envoyer_notification_label_case' => 'Send a notification inviting to register a password',
-	'erreur_email_obligatoire' => 'Vous devez configurer un email obligatoire dans les champs ou dans les coordonnées.',
+	'erreur_email_obligatoire' => 'Vous must set an email in the fields or the contact information ',
 	'errreur_identifiant_existant' => 'That id is already in use',
 	'exporter_champ_seulement_colonnes_label_case' => 'don’t export data, only columns',
-	'exporter_titre' => 'Exportation',
+	'exporter_titre' => 'Export',
 
 	// I
-	'icone_creer_profil' => 'Créer un profil',
-	'icone_modifier_profil' => 'Modifier ce profil',
-	'importer_titre' => 'Importation',
-	'info_1_profil' => 'Un profil',
-	'info_aucun_profil' => 'Aucun profil',
+	'icone_creer_profil' => 'Create a profil',
+	'icone_modifier_profil' => 'Edit this profil',
+	'importer_titre' => 'Import',
+	'info_1_profil' => 'One profil',
+	'info_1_profil_compte' => 'One profil account',
+	'info_aucun_profil' => 'No profil',
+	'info_nb_profil_comptes' => '@nb@ profil accounts',
 	'info_nb_profils' => '@nb@ profils',
-	'info_profils_auteur' => 'Les profils de cet auteur',
+	'info_nb_profils_comptes' => 'Profil accounts',
+	'info_profils_auteur' => 'Author’s profils',
 
 	// N
-	'notification_motdepasse_texte' => 'A new account was created for you on the website @nom_site_spip@ (@adresse_site@)
+	'notification_motdepasse_texte' => 'A new account has just been created for you on the website @nom_site_spip@ (@adresse_site@)
 
 Please follow that link :
 
     @sendcookie@
 
-You’ll be able to register a password
+You can then set a password
 and connect to the site with your email address @email@.
 
 ',
@@ -64,19 +87,19 @@ and connect to the site with your email address @email@.
 	'retirer_tous_liens_profils' => 'Remove all profils',
 
 	// S
-	'supprimer_profil' => 'Delete that profil',
+	'supprimer_profil' => 'Delete this profil',
 
 	// T
 	'texte_ajouter_profil' => 'Add a profil',
-	'texte_changer_statut_profil' => 'This profil is :',
+	'texte_changer_statut_profil' => 'This profil is:',
 	'texte_creer_associer_profil' => 'Create and associate a profil',
 	'texte_definir_comme_traduction_profil' => 'Ce profil est une traduction du profil numéro :',
-	'titre_langue_profil' => 'Langue de ce profil',
+	'titre_langue_profil' => 'Profil’s lang',
 	'titre_logo_profil' => 'Profil logo',
-	'titre_objets_lies_profil' => 'Liés à ce profil',
+	'titre_objets_lies_profil' => 'Associated to this profil',
 	'titre_profil' => 'Profil',
 	'titre_profils' => 'Profils',
 	'titre_profils_rubrique' => 'Section’s profils',
-	'tri_croissant' => 'Tri croissant',
-	'tri_decroissant' => 'Tri décroissant'
+	'tri_croissant' => 'Sort ascending',
+	'tri_decroissant' => 'Sort descending'
 );
