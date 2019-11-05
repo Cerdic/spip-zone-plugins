7d2532741c44b414ccd8b7d8585443351bde724e
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-11-05 00:12:00 +0000

    Quelques changements dans les fonctions mais presque rien
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@118427 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- icalendar/trunk/http/ical.php
+++ icalendar/trunk/http/ical.php
@@ -4,18 +4,28 @@
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 /*
- * Implémentation d'un serveur REST pour APP (AtomPub)
+ * Implémentation d'un serveur REST pour iCal
+ * (le vrai truc bien serait d'implémenter au moins une partie de CalDAV, mais on en est pas là)
  */ 
 
 /**
- * Rien, car en Atom il n'y a malheureusement pas de gestion des erreurs pour l'instant
+ * Gestion des erreurs (apparemment il n'y a rien de prévu pour iCal)
  *
  * @param int $code Le code HTTP de l'erreur à générer
- * @return string Retourne une chaîne vide
+ * @return string Retourne le contenu de l'erreur à renvoyer dans la réponse
  */
-function http_ical_erreur_dist($code, $requete, $reponse){
+function http_ical_erreur_dist($code, $requete, $reponse) {
 	$reponse->setStatusCode($code);
 	$reponse->setContent('');
+	
+	return $reponse;
+}
+
+/*
+ * GET sur la racine du serveur iCal
+ * http://site/http.api/ical
+ */
+function http_ical_get_index_dist($requete, $reponse) {
 	return $reponse;
 }
 
@@ -23,21 +33,23 @@ function http_ical_erreur_dist($code, $requete, $reponse){
  * GET sur une collection
  * http://site/http.api/ical/all
  */
-function http_ical_get_collection_dist($requete, $reponse){
+function http_ical_get_collection_dist($requete, $reponse) {
+	$format = $requete->attributes->get('format');
 	$collection = $requete->attributes->get('collection');
 	$contexte = $requete->query->all();
 	
 	// Pour l'instant on va simplement chercher un squelette du nom de la collection
 	// Le squelette prend en contexte les paramètres du GET uniquement
-	if ($flux = recuperer_fond("http/ical/$collection", $contexte)){
+	if ($ics = recuperer_fond("http/$format/$collection", $contexte)){
 		$reponse->setStatusCode(200);
 		$reponse->setCharset('utf-8');
-		$reponse->headers->set('Content-Type', 'application/atom+xml');
-		$reponse->setContent($flux);
+		$reponse->headers->set('Content-Type', 'text/calendar');
+		$reponse->setContent($ics);
 	}
 	// Si on ne trouve rien c'est que ça n'existe pas
 	else{
-		$reponse->setStatusCode(404);
+		$fonction_erreur = charger_fonction('erreur', "http/$format/");
+		$response = $fonction_erreur(404, $requete, $reponse);
 	}
 	
 	return $reponse;
@@ -45,34 +57,32 @@ function http_ical_get_collection_dist($requete, $reponse){
 
 /*
  * GET sur une ressource
- * http://site/http.api/ical/event
+ * http://site/http.api/ical/all/123
  */
- 
-function http_ical_get_ressource_dist($requete, $reponse){
-
-	// Quelque soit la collection, tous les événements ont le même squelette
-	// Le squelette prend en contexte les paramètres du GET + l'identifiant de l'évenement en essayant de faire au mieux
+function http_ical_get_ressource_dist($requete, $reponse) {
+	$format = $requete->attributes->get('format');
+	$collection = $requete->attributes->get('collection');
 	$ressource = $requete->attributes->get('ressource');
-
+	
 	// Quelque soit la collection, tous les événements ont le même squelette
 	// Le squelette prend en contexte les paramètres du GET + l'identifiant de l'évenement en essayant de faire au mieux
 	$contexte = array(
 		'id_evenement' => $ressource,
 		'ressource' => $ressource,
 	);
-	$contexte = array_merge($requete->query->all(), $contexte);
+	$contexte = array_merge($requete->query->all(), $requete->attributes->all(), $contexte);
 	
-	if ($flux = recuperer_fond("http/ical/event", $contexte)){
+	if ($ics = recuperer_fond("http/$format/event", $contexte)){
 		$reponse->setStatusCode(200);
 		$reponse->setCharset('utf-8');
-		$reponse->headers->set("Content-type: text/calendar; charset=utf-8");
-		$reponse->setContent($flux);
+		$reponse->headers->set('Content-Type', 'text/calendar');
+		$reponse->setContent($ics);
 	}
 	// Si on ne trouve rien c'est que ça n'existe pas
 	else{
-		$reponse->setStatusCode(404);
+		$fonction_erreur = charger_fonction('erreur', "http/$format/");
+		$response = $fonction_erreur(404, $requete, $reponse);
 	}
 	
 	return $reponse;
 }
-
