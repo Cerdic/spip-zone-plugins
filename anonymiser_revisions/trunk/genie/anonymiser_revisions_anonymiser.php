daddea871b348914cb8bdcf43055f97c6f100dfa
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-11-06 14:46:33 +0000

    version minimum du plugin, avec juste un genie
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@118442 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ anonymiser_revisions/trunk/genie/anonymiser_revisions_anonymiser.php
@@ -0,0 +1,26 @@
+<?php
+
+// Sécurité
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+include_spip('inc/ppps');
+include_spip('inc/sql');
+
+/**
+ * Dans l'historique des révisions, hash à intervalle régulier les ip
+ * @param int $t le temps depuis l'execution de la dernière tâche
+ * @return int positif si réussi
+**/
+function genie_anonymiser_revisions_anonymiser($t) {
+	if (_CNIL_PERIODE) {
+		$critere_cnil = 'date<"'.date('Y-m-d', time()-_CNIL_PERIODE).'"'
+			. ' AND (id_auteur LIKE "%.%" OR id_auteur LIKE "%:%")'; # ipv4 ou ipv6
+		$c = sql_countsel('spip_versions', $critere_cnil);
+		if ($c>0) {
+			spip_log("CNIL: masquer IP de $c versions anciennes", 'revisions');
+			sql_update('spip_versions', array('id_auteur' => 'MD5(id_auteur)'), $critere_cnil);
+			return $c;
+		}
+	}
+}
---
+++ anonymiser_revisions/trunk/lang/paquet-anonymiser_revisions_fr.php
@@ -0,0 +1,13 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/formidable/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// F
+	'anonymiser_revisions_description' => 'Anonymiser les révisions plus anciennes que _CNIL_PERIODE en hashant les adresse IP',
+	'anonymiser_revisions_slogan' => 'Stop surveillance !'
+);
