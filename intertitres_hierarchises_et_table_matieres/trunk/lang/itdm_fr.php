0eeb2d74af8f1e690383b7ccb5487fadf2711e38
gildas.cotomale@gmail.com <gildas.cotomale@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-11-09 18:26:42 +0000

    Permettre la prise en charge par Inserer Modeles
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@118491 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- intertitres_hierarchises_et_table_matieres/trunk/lang/itdm_fr.php
+++ intertitres_hierarchises_et_table_matieres/trunk/lang/itdm_fr.php
@@ -10,6 +10,18 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// A
 	'aller_directement_a_' => 'Aller directement à « @titre@ »',
 
+	// I
+	'inserer_extrait' => "un extrait du texte d'un article",
+	'inserer_partie' => "un extrait d'une section d'un article",
+	'inserer_renvoi' => "un renvoi vers une section d'un article",
+	'inserer_table' => "la table des matières d'un article",
+
+	// L
+	'label_article' => 'Article cible',
+	'label_debut' => 'Premier mot affiché (décompte à partir de zéro)',
+	'label_partie' => "Partie de l'article sous la forme X.Y.Z numériques",
+	'label_taille' => 'Nombre de mots affichés (décompte à partir de zéro)',
+
 	// T
 	'title_lien_table_matieres' => 'Aller à la table des matières de l’article',
 	'titre_table_matieres' => 'Table des matières'
