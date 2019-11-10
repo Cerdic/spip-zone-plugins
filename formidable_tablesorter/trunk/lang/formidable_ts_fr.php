9958e11cc84445d17ef7c5e8c8ee3c8bee5af529
Maïeul Rouquette <Maeul Rouquette@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-11-10 19:50:27 +0000

    fichier de lang
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@118503 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ formidable_tablesorter/trunk/lang/formidable_ts_fr.php
@@ -0,0 +1,12 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/formidable_tablesorter/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// F
+	'tableau_reponses' => 'Tableau des réponses',
+);
