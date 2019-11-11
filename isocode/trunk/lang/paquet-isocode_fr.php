d75822c82f6bdc6cb1c2f843f5b5eb2a4f67c7f1
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-11-11 14:08:22 +0000

    Renommage du plugin (pas le préfixe)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@118516 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- isocode/trunk/lang/paquet-isocode_fr.php
+++ isocode/trunk/lang/paquet-isocode_fr.php
@@ -7,7 +7,7 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// B
-	'isocode_description' => 'Ce plugin installe la base de divers codes standard comme les codes de langues ISO-639, les codes d\'écritures ISO-15924, les codes des pays ISO-3166... Le plugin fournit aussi une API ainsi qu\'un service web pour la consultation des données.',
-	'isocode_nom'         => 'Codes ISO',
-	'isocode_slogan'      => 'Codes ISO pour les langues, les pays et autres objets'
+	'isocode_description' => 'Ce plugin installe la base de divers codes standard comme les codes de langues ISO-639, les codes d\'écritures ISO-15924, les codes des pays ISO-3166... Le plugin fournit aussi une API REST pour la consultation des données.',
+	'isocode_nom'         => 'Nomenclatures officielles',
+	'isocode_slogan'      => 'Nomenclatures pour les langues, les pays et autres éléments'
 );
