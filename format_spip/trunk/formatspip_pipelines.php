43291f67c92e2b9cc493e7bb7b362d02c22e6f39
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-11-13 22:37:34 +0000

    [FormatSPIP] Compatibilité SPIP 3.2 (merci au code du CS) et champs supplémentaires pour être exhaustif
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@118615 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- format_spip/trunk/formatspip_pipelines.php
+++ format_spip/trunk/formatspip_pipelines.php
@@ -4,6 +4,7 @@
 
 	function formatspip_affiche_milieu($flux){
 		switch($flux['args']['exec']) {
+			case 'article':
 			case 'articles':
 				$id_article = $flux['args']['id_article'];
 				// le formulaire qu'on ajoute
@@ -18,35 +19,62 @@
 	function formatspip_affiche($id_article){
 		include_spip('inc/presentation');
 		
-		$q = spip_query("SELECT descriptif, chapo, texte, ps FROM spip_articles WHERE id_article=$id_article");
+		$q = spip_query("SELECT titre, surtitre, soustitre, descriptif, chapo, texte, ps, virtuel, nom_site, url_site FROM spip_articles WHERE id_article=$id_article");
 		$row = spip_fetch_array($q);
 		$txt = '';
+		if (strlen($row['titre'])>0) {
+			$txt .= "----- "._T('info_titre')." -----\n";
+			$txt .= $row['titre']."\n\n";
+		}
+		if (strlen($row['surtitre'])>0) {
+			$txt .= "----- "._T('info_surtitre')." -----\n";
+			$txt .= $row['surtitre']."\n\n";
+		}
+		if (strlen($row['soustitre'])>0) {
+			$txt .= "----- "._T('texte_sous_titre')." -----\n";
+			$txt .= $row['soustitre']."\n\n";
+		}
 		if (strlen($row['descriptif'])>0) {
-			$txt .= "----- "._T('texte_descriptif_rapide')." -----\n\n";
+			$txt .= "----- "._T('texte_descriptif_rapide')." -----\n";
 			$txt .= $row['descriptif']."\n\n";
 		}
 		if (strlen($row['chapo'])>0) {
-			$txt .= "----- "._T('info_chapeau')." -----\n\n";
+			$txt .= "----- "._T('info_chapeau')." -----\n";
 			$txt .= $row['chapo']."\n\n";
 		}
+		if (strlen($row['url_site'])>0) {
+			$txt .= "----- "._T('entree_liens_sites')." -----\n";
+			$txt .= _T('info_titre') . $row['nom_site']."\n";
+			$txt .= _T('info_url') . $row['url_site']."\n\n";
+		}
 		if (strlen($row['texte'])>0) {
-			$txt .= "----- "._T('info_texte')." -----\n\n";
+			$txt .= "----- "._T('info_texte')." -----\n";
 			$txt .= $row['texte']."\n\n";
 		}
 		if (strlen($row['ps'])>0) {
-			$txt .= "----- "._T('info_post_scriptum')." -----\n\n";
+			$txt .= "----- "._T('info_post_scriptum')." -----\n";
 			$txt .= $row['ps']."\n\n";
 		}
+		if (strlen($row['virtuel'])>0) {
+			$txt .= "----- "._T('bouton_redirection')." -----\n";
+			$txt .= $row['virtuel']."\n\n";
+		}
 		
-		$flux = '';
-		$bouton = bouton_block_invisible("formatspip");
-		$flux .= debut_cadre_enfonce("../"._DIR_PLUGIN_FORMATSPIP."/images/formatspip-24.png", true, '', $bouton._T('formatspip:texte_formatspip'));
-		$flux .= debut_block_invisible("formatspip");
-		$flux .= '<textarea cols="55" rows="20">'.$txt.'</textarea>';
-		$flux .= fin_block();
-		$flux .= fin_cadre_enfonce(true);
+		$txt = '<pre style="white-space: pre-wrap;       /* css-3 */
+ white-space: -moz-pre-wrap;  /* Mozilla, since 1999 */
+ white-space: -pre-wrap;      /* Opera 4-6 */
+ white-space: -o-pre-wrap;    /* Opera 7 */
+ word-wrap: break-word;       /* Internet Explorer 5.5+ */">'. $txt .'<pre>';
+		
+		// compatibilite avec SPIP 1.92
+		$compat = function_exists('bouton_block_depliable');
+		$bouton = $compat?bouton_block_depliable(_T('formatspip:texte_formatspip'), 'invisible', "formatspip")
+			:bouton_block_invisible("formatspip")._T('formatspip:texte_formatspip');
+		$bloc = $compat?debut_block_depliable(false, "formatspip")
+			:debut_block_invisible("formatspip");
+		return debut_cadre_enfonce("../"._DIR_PLUGIN_FORMATSPIP."/images/formatspip-24.png", true, '', $bouton)
+			. $bloc	. $txt . fin_block()
+			. fin_cadre_enfonce(true);
 		
 		return $flux;
 	}
-
-?>
