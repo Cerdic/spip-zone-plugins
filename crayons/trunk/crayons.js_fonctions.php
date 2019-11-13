197b052dfa747df409a4c0f4e3e3750e4d5c1c64
Cerdic <Cerdic@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-11-13 14:24:11 +0000

    exit la surcharge du js packer, on utilise la fonction minifier() du compresseur, prete a l'emploi
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@118582 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- crayons/trunk/crayons.js_fonctions.php
+++ crayons/trunk/crayons.js_fonctions.php
@@ -27,22 +27,11 @@ function pack_cQuery($chemin) {
 	// On ne compacte PAS deux fois (c'est inutile et en plus ca bugge)
 	if (!strlen($flux)
 		or _request('debug_crayons') // mode debug des crayons
-		or ($GLOBALS['meta']['auto_compress_js'] == 'oui' 	// le vieil auto_compress_js
-		and @file_exists(_DIR_RESTREINT.'inc/compacte_js.php'))
-		or !function_exists('test_espace_prive')// ou l'espace prive
-		or test_espace_prive()) {
+		or !function_exists('minifier')
+		) {
 		return $flux;
 	}
 
-	include_spip('lib/JavaScriptPacker/class.JavaScriptPacker');
-	$packer = new JavaScriptPacker($flux, 0, true, false);
-
-	// en cas d'echec (?) renvoyer l'original
-	if (strlen($t = $packer->pack())) {
-		return $t;
-	}
-
-	// erreur
-	spip_log('erreur de pack_js');
+	$flux = minifier($flux, 'js');
 	return $flux;
 }
