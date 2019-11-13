c738c459ce48e399beae8adc83c292fabe77786d
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-11-13 20:01:05 +0000

    Configuration des liaisons pour les pays.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@118611 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- geographie/branches/dev/lang/geographie_fr.php
+++ geographie/branches/dev/lang/geographie_fr.php
@@ -4,6 +4,6 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 }
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-	'configurer_communes_lier_objets_label' => 'Activer la liaison de communes sur les contenus',
+	'configurer_lier_objets_label' => 'Activer la liaison sur les contenus',
 	'configurer_titre' => 'Paramétrer les éléments géographiques',
 );
