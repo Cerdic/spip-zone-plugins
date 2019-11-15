6f152e683dacab34189f3da97eee7a7ccb29cffd
tcharlss@bravecassine.com <tcharlss@bravecassine.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-11-15 04:40:59 +0000

    [Salvatore] [source:_plugins_/multilang/trunk/lang/ multilang] Export depuis http://trad.spip.net de la langue en
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@118640 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- multilang/trunk/lang/multilang_en.php
+++ multilang/trunk/lang/multilang_en.php
@@ -15,11 +15,14 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'cfg_descr_multilang' => 'Multilang: plugin to manage multi tags',
 	'cfg_effacer' => 'Re-init',
 	'cfg_explication_crayons' => 'Requires multilang to be activated in the public space of the site.',
+	'cfg_formulaires_autres_explication' => 'Comma-separated jQuery selectors: <code>.formulaire_editer_patate, .formulaire_machiner_truc:not([bidule])</code>',
+	'cfg_formulaires_autres_label' => 'Other forms',
+	'cfg_formulaires_label' => 'Forms',
 	'cfg_langues' => 'Used languages',
 	'cfg_lbl_crayons' => 'Use multilang in the pencils',
 	'cfg_lbl_espace_public' => 'Use multilang in the public space',
 	'cfg_lbl_formstables' => 'of forms&tables',
-	'cfg_lbl_siteconfig' => 'of the website configuration', # MODIF
+	'cfg_lbl_siteconfig' => 'Website configuration (welcome and identity)',
 	'cfg_titre_multilang' => 'Multilang',
 	'champ_numero' => 'Number',
 	'configuration_multilang' => 'Multilang',
@@ -28,6 +31,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'explication_langues_utilisees' => 'Select the languages to use in the multilang menu. Select none to use them all.',
 
 	// L
+	'label_langues' => 'Langs',
 	'label_langues_utilisees' => 'Used languages by multilang',
 	'lien_desactiver' => 'All',
 	'lien_multi_title' => 'Edit multilingual fields in «@lang@»',
