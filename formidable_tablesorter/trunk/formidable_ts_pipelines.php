a5af6c37e13a25bedf01ac01b68d89c9c72c96c0
Maïeul Rouquette <Maeul Rouquette@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-11-18 10:19:44 +0000

    Plus de lien
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@118682 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- formidable_tablesorter/trunk/formidable_ts_pipelines.php
+++ formidable_tablesorter/trunk/formidable_ts_pipelines.php
@@ -10,9 +10,15 @@ if (!defined('_ECRIRE_INC_VERSION')) {
  **/
 function formidable_ts_affiche_gauche($flux) {
 	$args = $flux['args'];
-	if (in_array($args['exec'], array( 'formulaire', 'formulaires_analyse', 'formulaires_reponses'))) {
+	if (in_array($args['exec'], array( 'formulaire', 'formulaires_analyse', 'formulaires_reponses','formulaires_reponse'))) {
+		include_spip('inc/presentation');
+		if (isset($args['id_formulaire'])) {
+			$id_formulaire = $args['id_formulaire'];
+		} else {
+			$id_formulaire = sql_getfetsel('id_formulaire','spip_formulaires_reponses','id_formulaires_reponse='.$args['id_formulaires_reponse']);
+		}
 		$boite_fermer = boite_fermer();
-		$url = parametre_url(generer_url_ecrire('formidable_tablesorter'),'id_formulaire',$args['id_formulaire']);
+		$url = parametre_url(generer_url_ecrire('formidable_tablesorter'),'id_formulaire',$id_formulaire);
 		$lien = icone_horizontale(_T('formidable_ts:tableau_reponses'), $url, 'formulaire-reponses-24');
 		$flux['data'] = str_replace($boite_fermer,"$lien\n\r$boite_fermer", $flux['data']);
 	}
