7682e01f1d7c4a309b96d644278eb66c03e7b435
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-11-28 13:45:25 +0000

    une option pour ne pas insérer automatiquement les scripts et css sur l'espace privé et publique. Décochée par défaut
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@118804 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ tablesorter/trunk/lang/tablesorter_fr.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/tablesorter/lang/
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'configurer_tablesorter' => 'Configuration du plugin tablesorter',
+
+	//
+	'no_insert' => 'Ne pas insérer automatiquement le script et css sur les pages publiques et privées',
+);
