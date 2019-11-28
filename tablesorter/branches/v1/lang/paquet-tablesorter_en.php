aed03c1c9aecf449c3e8a635f30d6258554e17ad
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-11-28 12:55:38 +0000

    Une branches v1, avant qu'on s'intéresse au trunk
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@118799 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ tablesorter/branches/v1/lang/paquet-tablesorter_en.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-tablesorter?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// T
+	'tablesorter_description' => 'This plugin allows you to sort the tables having the CSS class "spip" by clicking on a column header (HTML table  must have the tags thead and tbody). See all sorting options on the [->http://tablesorter.com] Web site.',
+	'tablesorter_slogan' => 'With tableSorter, the sort will be wonderfull!'
+);
---
+++ tablesorter/branches/v1/lang/paquet-tablesorter_sk.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-tablesorter?lang_cible=sk
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// T
+	'tablesorter_description' => 'Tento zásuvný modul vám umožňuje triediť tabuľky s CSS triedou "spip" kliknutím na záhlavie stĺpca (v HTML tabuľke musia byť tagy thead a tbody). Všetky možnosti triedenia si môžete prezrieť na stránke [http://tablesorter.com.->http://tablesorter.com]',
+	'tablesorter_slogan' => 'S tableSorterom bude triedenie najlepšie!'
+);
