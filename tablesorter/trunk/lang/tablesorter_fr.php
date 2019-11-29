fc4ceca0d45f815808db9e334c145ae44afc557c
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-11-29 04:41:10 +0000

    [Salvatore] [source:_plugins_/tablesorter/trunk/lang/ tablesorter] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@118831 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- tablesorter/trunk/lang/tablesorter_fr.php
+++ tablesorter/trunk/lang/tablesorter_fr.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/tablesorter/lang/
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/tablesorter/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
@@ -10,6 +10,6 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// C
 	'configurer_tablesorter' => 'Configuration du plugin tablesorter',
 
-	//
-	'no_insert' => 'Ne pas insérer automatiquement le script et css sur les pages publiques et privées',
+	// N
+	'no_insert' => 'Ne pas insérer automatiquement le script et css sur les pages publiques et privées'
 );
