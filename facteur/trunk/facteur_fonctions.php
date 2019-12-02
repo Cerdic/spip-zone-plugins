08584c15a3d3c9c082ded989bd64f090f6cd15f0
Cerdic <Cerdic@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-12-02 12:30:19 +0000

    un seul pipeline, autant le mettre dans le fichier fonctions qui est maintenant tout leger
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@118857 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- facteur/trunk/facteur_fonctions.php
+++ facteur/trunk/facteur_fonctions.php
@@ -72,3 +72,25 @@ function facteur_mail_html2text($html){
 	$facteur_mail_html2text = charger_fonction('facteur_mail_html2text', 'inc');
 	return $facteur_mail_html2text($html);
 }
+
+
+/**
+ * Insertion dans le pipeline formulaire_fond (SPIP)
+ *
+ * On indique dans le formulaire de configuration de l'identité du site
+ * que facteur surchargera l'email configuré ici pour envoyer les emails
+ *
+ * @param array $flux
+ * 		Le contexte du pipeline
+ * @return array $flux
+ * 		Le contexte du pipeline modifé
+ */
+function facteur_formulaire_fond($flux) {
+	if (($flux['args']['form'] == 'configurer_identite')
+		&& (isset($GLOBALS['meta']['facteur_adresse_envoi']) && $GLOBALS['meta']['facteur_adresse_envoi'] == 'oui')
+		&& (isset($GLOBALS['meta']['facteur_adresse_envoi_email']) && strlen($GLOBALS['meta']['facteur_adresse_envoi_email']) > 0)) {
+		$ajout = '<p class="notice">'._T('facteur:message_identite_email').'</p>';
+		$flux['data'] = preg_replace(",(<(?:div|li) [^>]*class=[\"']editer editer_email_webmaster.*>)(.*<label),Uims", "\\1".$ajout."\\2", $flux['data'], 1);
+	}
+	return $flux;
+}
