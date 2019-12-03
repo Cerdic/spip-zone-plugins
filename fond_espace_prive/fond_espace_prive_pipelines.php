179a1e795324649b92ed81bef5531feed91694d0
arno@rezo.net <arno@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-12-03 14:34:38 +0000

    Modifie désormais tout #minipres (attention: effets de bords à surveiller…). Permet notamment de looker spip_pass.html
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@118886 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- fond_espace_prive/fond_espace_prive_pipelines.php
+++ fond_espace_prive/fond_espace_prive_pipelines.php
@@ -8,7 +8,7 @@ function fond_espace_prive_insert_head_css($flux) {
 	if ( !$fond = find_in_path(_DIR_IMG."fond_espace_prive.jpg")) $fond = find_in_path("fond_espace_prive.jpg");
 
 	$flux .= "\n<link rel='stylesheet' type='text/css' media='all' href='".find_in_path("fond_espace_login.css")."'>\n";
-	$flux .= "<style>body.page_login{background-image:url(".$fond.")}</style>";
+	$flux .= "<style>#minipres{background-image:url(".$fond.")}</style>";
 	return $flux;
 }
 
