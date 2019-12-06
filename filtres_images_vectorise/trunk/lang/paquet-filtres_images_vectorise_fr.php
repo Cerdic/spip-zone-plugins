75fc9e63261fdd5d1f868b0b4cbc52b1d98f5873
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-12-06 12:28:34 +0000

    Une icone SVG et chaines de langue du paquet
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@118940 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ filtres_images_vectorise/trunk/lang/paquet-filtres_images_vectorise_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// R
+	'filtres_images_vectorise_description' => 'Des filtres pour transformer vos photos JPG en images vectorielles SVG avec mode artistique.',
+	'filtres_images_vectorise_nom' => 'Image Vectorise',
+	'filtres_images_vectorise_slogan' => 'Vectoriser des photos avec style !',
+);
+
+?>
