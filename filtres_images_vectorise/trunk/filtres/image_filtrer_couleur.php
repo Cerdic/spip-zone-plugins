06349a28d585a411f550b315639b7f545e69b877
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-12-07 11:50:17 +0000

    enrichissement du filtre image_filtrer_couleur
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@118946 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- filtres_images_vectorise/trunk/filtres/image_filtrer_couleur.php
+++ filtres_images_vectorise/trunk/filtres/image_filtrer_couleur.php
@@ -16,20 +16,27 @@
  * @param string $img
  * @param string $couleur
  * @param int $rayon
+ * @param string $operation
+ *   masque|mask : cree un masque noir et blanc (pixels de la couleur cherchee sont en noir, pixels tres differents sont en blanc)
+ *   couleur|color : créé un masque couleur/opacite (pixels de la couleur cherchee sont en opacite 1, pixels tres differents sont en blanc transparent) - opacite et clarete varient
+ *   opacite|opacity : créé un masque opacite (pixels de la couleur cherchee sont en opacite 1, pixels tres differents sont transparent) - seule l'opacite varie
+ *   retire|remove : les pixes de la couleur cherchee sont rendus transparents, on ne garde que les pixels tres eloignes en couleur
+ * @param string $ext
  * @return string
  */
-function image_filtrer_couleur($img, $couleur, $rayon=100, $operation='keep') {
+function image_filtrer_couleur($img, $couleur, $rayon=100, $operation='masque', $ext='png') {
 
 	$fonction = "image_filtrer_couleur";
 	$args = func_get_args();
-	$cache = _image_valeurs_trans($img, "filtrer_couleur-$couleur-$rayon-$operation", false, [$fonction, $args]);
+	$cache = _image_valeurs_trans($img, "filtrer_couleur-$couleur-$rayon-$operation", $ext, [$fonction, $args]);
+
 	if (!$cache) {
 		return false;
 	}
 
 	$im = $cache["fichier"];
 	$dest = $cache["fichier_dest"];
-	if ($cache["creer"]) {
+	if (true or $cache["creer"]) {
 
 		$im = $cache["fonction_imagecreatefrom"]($im);
 		$w = $cache["largeur"];
@@ -42,6 +49,26 @@ function image_filtrer_couleur($img, $couleur, $rayon=100, $operation='keep') {
 		imagefill ($im_, 0, 0, $color_t);
 
 		$cc = _couleur_hex_to_dec($couleur);
+
+		switch ($operation) {
+			case 'retire':
+			case 'remove':
+				$filtre_pixel = 'pixel_filtrer_couleur_retire';
+				break;
+			case 'couleur':
+			case 'color':
+				$filtre_pixel = 'pixel_filtrer_couleur_couleur';
+				break;
+			case 'opacite':
+			case 'opacity':
+				$filtre_pixel = 'pixel_filtrer_couleur_opacite';
+				break;
+			case 'masque':
+			case 'mask':
+			default:
+				$filtre_pixel = 'pixel_filtrer_couleur_masque';
+				break;
+		}
 
 		for ($x = 0; $x < $w; $x++) {
 			for ($y=0; $y < $h; $y++) {
@@ -57,19 +84,12 @@ function image_filtrer_couleur($img, $couleur, $rayon=100, $operation='keep') {
 					+ ($cc['blue'] - $b) * ($cc['blue'] - $b);
 				$d = sqrt($d) / $rayon;
 
-				if ($operation === 'remove') {
-					$d = min(max($d-0.1,0),1);
-					$d = 1 - $d;
-					$a = min(round((127 - $a) * $d) + $a, 127);
-					$r = max(0,min($r + round((255-$r) * $d),255));
-					$g = max(0,min($g + round((255-$g) * $d),255));
-					$b = max(0,min($b + round((255-$b) * $d),255));
-					$color = ImageColorAllocateAlpha( $im_, $r, $g, $b , $a);
+				switch ($operation) {
+					case 'retire':
+					case 'remove':
 				}
-				else {
-					$d = round(min($d,1) * 255);
-					$color = ImageColorAllocateAlpha( $im_, $d, $d, $d , $a );
-				}
+				list ($r, $g, $b, $a) = $filtre_pixel($d, $r , $g, $b, $a, $cc['red'], $cc['green'], $cc['blue']);
+				$color = ImageColorAllocateAlpha( $im_, $r, $g, $b , $a);
 				imagesetpixel ($im_, $x, $y, $color);
 			}
 		}
@@ -79,4 +99,39 @@ function image_filtrer_couleur($img, $couleur, $rayon=100, $operation='keep') {
 	}
 
 	return _image_ecrire_tag($cache,array('src'=>$dest));
+}
+
+function pixel_filtrer_couleur_retire($d, $pixel_r, $pixel_g, $pixel_b, $pixel_a, $search_r, $search_g, $search_b) {
+
+	$d = min(max($d-0.1,0),1);
+	$d = 1 - $d;
+	$a = min(round((127 - $pixel_a) * $d) + $pixel_a, 127);
+	$r = max(0,min($pixel_r + round((255-$pixel_r) * $d),255));
+	$g = max(0,min($pixel_g + round((255-$pixel_g) * $d),255));
+	$b = max(0,min($pixel_b + round((255-$pixel_b) * $d),255));
+
+	return array($r, $g, $b, $a);
+}
+
+function pixel_filtrer_couleur_masque($d, $pixel_r, $pixel_g, $pixel_b, $pixel_a, $search_r, $search_g, $search_b) {
+	$d = round(min($d,1) * 255);
+	return array($d, $d, $d, $pixel_a);
+}
+
+function pixel_filtrer_couleur_opacite($d, $pixel_r, $pixel_g, $pixel_b, $pixel_a, $search_r, $search_g, $search_b) {
+	$d = min($d,1);
+	$a = min(round((127 - $pixel_a) * $d) + $pixel_a, 127);
+
+	return array($search_r, $search_g, $search_b, $a);
+}
+
+function pixel_filtrer_couleur_couleur($d, $pixel_r, $pixel_g, $pixel_b, $pixel_a, $search_r, $search_g, $search_b) {
+	$d = min($d,1);
+	$a = min(round((127 - $pixel_a) * $d) + $pixel_a, 127);
+
+	$r = max(0,min($search_r + round((255-$search_r) * $d),255));
+	$g = max(0,min($search_g + round((255-$search_g) * $d),255));
+	$b = max(0,min($search_b + round((255-$search_b) * $d),255));
+
+	return array($r, $g, $b, $a);
 }
