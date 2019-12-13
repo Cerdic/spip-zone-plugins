b5f8059b79cd7d8f696eea9f54c7fb443ef29aad
vincent.callies@gmail.com <vincent.callies@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-12-13 11:10:35 +0000

    ajout de la fonction autoriser_pensebetecreer_menu_dist manquante on nourrit les fonctions autoriser_pensebete_modifier/voir/supprimer_dist
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@119025 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- pensebetes/pensebetes_autorisations.php
+++ pensebetes/pensebetes_autorisations.php
@@ -26,9 +26,10 @@ function autoriser_associerpensebetes_dist($faire, $type, $id, $qui, $opt) {
 }
 
 
-// -----------------
-// Objet pensebete
-
+/**
+ * Fonctions relatives aux menus de l'interface privée
+ *
+ */
 
 /**
  * Autorisation de voir un élément de menu (pensebetes)
@@ -60,6 +61,25 @@ function autoriser_pensebetecreer_menu_dist($faire, $type, $id, $qui, $opt) {
 }
 
 /**
+ * Autorisation de voir le bouton d'outil collaboratif du mur
+ *
+ * @param  string $faire Action demandée
+ * @param  string $type  Type d'objet sur lequel appliquer l'action
+ * @param  int    $id    Identifiant de l'objet
+ * @param  array  $qui   Description de l'auteur demandant l'autorisation
+ * @param  array  $opt   Options de cette autorisation
+ * @return bool          true s'il a le droit, false sinon
+**/
+function autoriser_murs_menu_dist($faire, $type, $id, $qui, $opt) {
+	return true;
+}
+
+/**
+ * Fonctions relatives à l'Objet pensebete
+ *
+ */
+
+/**
  * Autorisation de créer (pensebete)
  *
  * @param  string $faire Action demandée
@@ -76,6 +96,8 @@ function autoriser_pensebete_creer_dist($faire, $type, $id, $qui, $opt) {
 /**
  * Autorisation de voir (pensebete)
  *
+ * On ne peut voir  les pensebetes dont on est l'auteur ou le destinataire.
+ *
  * @param  string $faire Action demandée
  * @param  string $type  Type d'objet sur lequel appliquer l'action
  * @param  int    $id    Identifiant de l'objet
@@ -84,11 +106,31 @@ function autoriser_pensebete_creer_dist($faire, $type, $id, $qui, $opt) {
  * @return bool          true s'il a le droit, false sinon
 **/
 function autoriser_pensebete_voir_dist($faire, $type, $id, $qui, $opt) {
-	return true;
+	if (!$id_auteur=intval($qui['id_auteur'])) {
+		return false;
+	}
+	if (!$id_donneur=intval($opt['id_donneur']) AND !$id_receveur=intval($opt['id_receveur'])) {
+		$row = sql_fetsel('id_donneur,id_receveur', 'spip_pensebetes', 'id_pensebete=' . intval($id));
+		if (!$row)
+			return false;
+		$id_donneur = $row['id_donneur'];$id_receveur = $row['id_receveur'];
+	}
+
+	if (in_array($id_auteur, array($id_donneur,$id_receveur))) {
+		return true;
+	}
+	// on peut tout faire si on est admin mais c'est pas cool
+	if ($qui['statut'] == '0minirezo') {
+		return true;
+	}
+
+	return false;
 }
 
 /**
  * Autorisation de modifier (pensebete)
+ *
+ * On ne peut modifier que les pensebetes dont on est l'auteur.
  *
  * @param  string $faire Action demandée
  * @param  string $type  Type d'objet sur lequel appliquer l'action
@@ -98,11 +140,30 @@ function autoriser_pensebete_voir_dist($faire, $type, $id, $qui, $opt) {
  * @return bool          true s'il a le droit, false sinon
 **/
 function autoriser_pensebete_modifier_dist($faire, $type, $id, $qui, $opt) {
-	return true;
+	if (!$id_auteur=intval($qui['id_auteur'])) {
+		return false;
+	}
+	if (!$id_donneur=intval($opt['id_donneur'])) {
+		$id_donneur = sql_getfetsel('id_donneur', 'spip_pensebetes', 'id_pensebete=' . intval($id));
+		if (!$id_donneur)
+		return false;
+	}
+
+	if ($id_auteur==$id_donneur) {
+		return true;
+	}
+	// on peut tout faire si on est admin mais c'est pas cool
+	if ($qui['statut'] == '0minirezo') {
+		return true;
+	}
+
+	return false;
 }
 
 /**
  * Autorisation de supprimer (pensebete)
+ *
+ * On ne peut supprimer tous les pensebetes que l'on peut voir.
  *
  * @param  string $faire Action demandée
  * @param  string $type  Type d'objet sur lequel appliquer l'action
@@ -112,6 +173,6 @@ function autoriser_pensebete_modifier_dist($faire, $type, $id, $qui, $opt) {
  * @return bool          true s'il a le droit, false sinon
 **/
 function autoriser_pensebete_supprimer_dist($faire, $type, $id, $qui, $opt) {
-	return true;
+	return autoriser_pensebete_voir_dist($faire, $type, $id, $qui, $opt);
 }
 
