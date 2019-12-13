0fd2a67a8224a78111c5774b6f2c8dbf130a3d70
vincent.callies@gmail.com <vincent.callies@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-12-13 11:12:03 +0000

    PHPDoc n'est pas content sans en-tête de fichier. On le contente.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@119028 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- pensebetes/inc/precharger_pensebete.php
+++ pensebetes/inc/precharger_pensebete.php
@@ -1,15 +1,15 @@
 <?php
 
-/***************************************************************************\
- *  SPIP, Systeme de publication pour l'internet                           *
- *                                                                         *
- *  Copyright (c) 2001-2011                                                *
- *  Arnaud Martin, Antoine Pitrou, Philippe Riviere, Emmanuel Saint-James  *
- *                                                                         *
- *  Ce programme est un logiciel libre distribue sous licence GNU/GPL.     *
- *  Pour plus de details voir le fichier COPYING.txt ou l'aide en ligne.   *
-\***************************************************************************/
-
+/**
+ * Préchargement de l'objet editorial pensebete
+ *
+ * @plugin Pensebetes
+ * @copyright  2019
+ * @author     Vincent CALLIES
+ * @licence    GNU/GPL
+ * @package SPIP\Pensebetes\Inc
+ */
+ 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 
