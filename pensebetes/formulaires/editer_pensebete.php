631138b464c7c6485d70e13ed09b2daa763725b7
vincent.callies@gmail.com <vincent.callies@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-12-13 11:12:32 +0000

    PHPDoc n'est pas content sans en-tête de fichier. On le contente.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@119029 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- pensebetes/formulaires/editer_pensebete.php
+++ pensebetes/formulaires/editer_pensebete.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * Gestion du formulaire de d'édition de pensebete
+ *
+ * @plugin Pensebetes
+ * @copyright  2019
+ * @author     Vincent CALLIES
+ * @licence    GNU/GPL
+ * @package SPIP\Pensebetes\Formulaires
+ */
 
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
@@ -6,6 +15,14 @@ include_spip('inc/actions');
 include_spip('inc/editer');
 
 
+/**
+ * Chargement du formulaire d'édition de pensebete
+ *
+ * Déclarer les champs postés et y intégrer les valeurs par défaut
+ *
+ * @uses formulaires_editer_objet_charger()
+ *
+ */
 function formulaires_editer_pensebete_charger_dist($id_pensebete='new', $id_rubrique=0, $retour='', $lier_trad=0, $config_fonc='', $row=array(), $hidden=''){
 	$valeurs = formulaires_editer_objet_charger('pensebete',$id_pensebete,$id_rubrique,$lier_trad,$retour,$config_fonc,$row,$hidden);
 	if (empty($valeurs['id_donneur']))
@@ -20,10 +37,24 @@ function formulaires_editer_pensebete_identifier_dist($id_pensebete='new', $id_r
 	return serialize(array(intval($id_pensebete)));
 }
 
+/**
+ * Vérification du formulaire d'édition de pensebete
+ *
+ * @uses formulaires_editer_objet_verifier()
+ *
+ */
 function formulaires_editer_pensebete_verifier_dist($id_pensebete='new', $id_rubrique=0, $retour='', $lier_trad=0, $config_fonc='', $row=array(), $hidden=''){
 	return formulaires_editer_objet_verifier('pensebete', $id_pensebete);
 }
 
+/**
+ * Traitement du formulaire d'édition de pensebete
+ *
+ * Le traitement effectue une mise à zéro de l'id_auteur pour éviter des associations considérées comme inutiles.
+ *
+ * @uses formulaires_editer_objet_traiter()
+ *
+ */
 function formulaires_editer_pensebete_traiter_dist($id_pensebete='new', $id_rubrique=0, $retour='', $lier_trad=0, $config_fonc='', $row=array(), $hidden=''){
 	set_request('id_auteur','');// éviter que l'auteur soit associé au pense-bête.
 	return formulaires_editer_objet_traiter('pensebete',$id_pensebete,$id_rubrique,$lier_trad,$retour,$config_fonc,$row,$hidden);
