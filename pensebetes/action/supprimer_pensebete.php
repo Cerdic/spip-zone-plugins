313d9c70eff3c29da565a633c40c0808086fdf51
vincent.callies@gmail.com <vincent.callies@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-12-13 11:13:44 +0000

    PHPDoc n'est pas content sans en-tête de fichier. On le contente. ajout message minipres si suppression non autorisée ajoute d'une redirection en cas de suppression
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@119031 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- pensebetes/action/supprimer_pensebete.php
+++ pensebetes/action/supprimer_pensebete.php
@@ -1,5 +1,14 @@
 <?php
-
+/**
+ * Gestion de l'action supprimer_pensebete
+ *
+ * @plugin Pensebetes
+ * @copyright  2019
+ * @author     Vincent CALLIES
+ * @licence    GNU/GPL
+ * @package SPIP\Pensebetes\Actions
+ */
+ 
 if (!defined("_ECRIRE_INC_VERSION")) return;
  
 function action_supprimer_pensebete_dist($id_pensebete=null){
@@ -8,6 +17,12 @@ function action_supprimer_pensebete_dist($id_pensebete=null){
 		$securiser_action = charger_fonction('securiser_action', 'inc');
 		$id_pensebete = $securiser_action();
 	}
+
+	if (!autoriser('pensebete_supprimer', 'pensebete', $id_pensebete)) {
+		include_spip('inc/minipres');
+		minipres(_T('pensebete:erreur_titre'),_T('pensebete:erreur_suppression'));
+		exit;
+	}	
 	
 	// on va faire
 	spip_log("Demande de suppression du pense-bête n°$id_pensebete par l'auteur n°".
@@ -25,6 +40,13 @@ function action_supprimer_pensebete_dist($id_pensebete=null){
 	// cas suppression
 	if ($id_pensebete) {
 		sql_delete('spip_pensebetes',  'id_pensebete=' . intval($id_pensebete));
+		// si l'on est en train de visualiser le contenu du pense-bête
+		// sa suppression ne permet pas la redirection prévue :
+		if (_request('exec')=='pensebete') {
+			include_spip('inc/headers');
+			$redirect=generer_url_ecrire('pensebetes');
+			redirige_par_entete($redirect);
+		}
 	}
 	else {
 		spip_log("action_supprimer_pensebete_dist : suppession d'$id_pensebete pas possible",
