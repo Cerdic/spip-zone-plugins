ee61a4df9edb9021509cb3158adc4df177dcc835
vincent.callies@gmail.com <vincent.callies@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-12-15 08:32:00 +0000

    utilisation de la configuration pour l'affichage (optionnel) des pense-bêtes sur l'accueil. utilisation de la configuration pour l'affichage (optionnel) des boites d'activité sur l'accueil et les auteurs. utilisation de la configuration pour l'affichage (optionnel) des pense-bêtes sur n'importe quel objet éditorial
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@119076 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- pensebetes/pensebetes_pipelines.php
+++ pensebetes/pensebetes_pipelines.php
@@ -13,16 +13,39 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
 
+/**
+ * Ajout de contenu à gauche de la page,
+ *
+ * @pipeline affiche_gauche
+ * @param  array $flux Données du pipeline
+ * @return array       Données du pipeline
+ */
 function pensebetes_affiche_gauche($flux){
-
-    if ($flux['args']['exec'] == 'auteur'){
-	include_spip('inc/presentation');
-      $flux['data'] .= debut_cadre_relief('pensebete-24.png',true,'',_T('pensebete:titre_pensebetes')); 
-        $flux['data'] .= recuperer_fond('prive/squelettes/inclure/pensebetes_donnes',array('id_auteur'=>$flux['args']['id_auteur']));
-        $flux['data'] .= recuperer_fond('prive/squelettes/inclure/pensebetes_recus',array('id_auteur'=>$flux['args']['id_auteur']));
-        $flux['data'] .= fin_cadre_relief(true);
+    if (in_array($flux['args']['exec'],array('auteur','accueil'))) {
+		$mes_boites=lire_config('pensebetes/mes_boites');
+		if (in_array($flux['args']['exec'],$mes_boites)) {
+			include_spip('inc/presentation');
+			$balise_img = chercher_filtre('balise_img');
+			$img = $balise_img(chemin_image('pensebete-16.png'), "", '');
+			if ($flux['args']['exec']=='accueil')
+			 	$flux['args']['id_auteur']=$GLOBALS['visiteur_session']['id_auteur'];
+    		if ($flux['args']['id_auteur']==$GLOBALS['visiteur_session']['id_auteur']) {
+				$titre ="<a href='".generer_url_ecrire('murs')."'>"._T('pensebete:titre_activite_mur')."</a></h3>";
+        		$soustitre1 = "<a href='".generer_url_ecrire('murs','quoi=mien')."'>".$img." </a>";
+        		$soustitre2 = "<a href='".generer_url_ecrire('murs','quoi=autres')."'>".$img." </a>";
+   			}
+    		else {
+	    		$titre = _T('pensebete:titre_activite_mur'); 
+    			$soustitre1=$soustitre2= "$img ";
+    		}
+	    	$flux['data'] .= debut_cadre_relief('mur-24.png',true,'',$titre); 
+    		$flux['data'] .= $soustitre1;
+    		$flux['data'] .= recuperer_fond('prive/squelettes/inclure/pensebetes_donnes',array('id_auteur'=>$flux['args']['id_auteur']));
+   			$flux['data'] .= $soustitre2; 
+    		$flux['data'] .= recuperer_fond('prive/squelettes/inclure/pensebetes_recus',array('id_auteur'=>$flux['args']['id_auteur']));
+    		$flux['data'] .= fin_cadre_relief(true);
+    	}
     }
-
     return $flux;
 }
 
@@ -39,38 +62,28 @@ function pensebetes_affiche_milieu($flux) {
 	$texte = "";
 	$e = trouver_objet_exec($flux['args']['exec']);
 
-/*	// pensebetes sur les articles
-	if ($e['type'] == 'article' AND !$e['edition']) {
-		$texte = recuperer_fond('prive/objets/editer/liens', array(
+	// Poser des pense-bêtes sur les objets éditoriaux éditables sélectionnés dans la configuration
+	if (in_array($e['type'], lire_config('pensebetes/mes_pensebetes')) AND !$e['edition']) {
+			$texte = recuperer_fond('prive/objets/editer/liens', array(
 			'table_source' => 'pensebetes',
 			'objet' => $e['type'],
 			'id_objet' => $flux['args'][$e['id_table_objet']],
 			'id_donneur' => $GLOBALS['visiteur_session']['id_auteur'],
-			#'editable'=>autoriser('associerpensebetes',$e['type'],$e['id_objet'])?'oui':'non'
-		));
+			'editable'=>autoriser('associerpensebetes',$e['type'],$e['id_objet'])?'oui':'non'
+			));
 	}
 
-	// pensebetes sur les rubriques
-	if ($e['type'] == 'rubrique' AND !$e['edition']) {
-		$texte = recuperer_fond('prive/objets/editer/liens', array(
-			'table_source' => 'pensebetes',
-			'objet' => $e['type'],
-			'id_objet' => $flux['args'][$e['id_table_objet']],
-			'id_donneur' => $GLOBALS['visiteur_session']['id_auteur'],
-			#'editable'=>autoriser('associerpensebetes',$e['type'],$e['id_objet'])?'oui':'non'
-		));
-	}
-*/
-	// pensebetes sur le mur d'accueil de l'auteur
-	if ($flux['args']['exec']=='accueil'){
-		$ajout= recuperer_fond('prive/squelettes/inclure/pensebete_accueil',
+	// Poser des pense-bêtes dans les lieux sélectionnés dans la configuration (le mur d'accueil de l'auteur pour l'instant)
+	if (in_array($flux['args']['exec'], lire_config('pensebetes/mes_lieux'))) {
+		$ajout= recuperer_fond('prive/squelettes/inclure/pensebete_'.$flux['args']['exec'],
 				array(
 					'id_receveur' => $GLOBALS['visiteur_session']['id_auteur']
-					#'editable'=>autoriser('associerpensebetes',$e['type'],$e['id_objet'])?'oui':'non'
 					)
 				);
-				$flux['data']=$ajout.$flux['data'];
-		}
+		$flux['data']=$ajout.$flux['data'];
+	}
+	
+	// injection des données
 	if ($texte) {
 		if ($p = strpos($flux['data'], '<!--affiche_milieu-->')) {
 			$flux['data'] = substr_replace($flux['data'], $texte, $p, 0);
