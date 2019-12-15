ec5839c4a0554cab3c0ff32714d7fcea86634f15
vincent.callies@gmail.com <vincent.callies@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-12-15 08:33:22 +0000

    maj avec définition de la configuration par défaut
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@119078 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- pensebetes/pensebetes_administrations.php
+++ pensebetes/pensebetes_administrations.php
@@ -19,6 +19,14 @@ function pensebetes_upgrade($nom_meta_base_version, $version_cible){
 	$maj = array();
 	$maj['create'] = array(
 		array('maj_tables', array('spip_pensebetes', 'spip_pensebetes_liens')),
+		array('ecrire_config','pensebetes/mes_objets', array('article')),
+		array('ecrire_config','pensebetes/mes_lieux', array('accueil')),
+		array('ecrire_config','pensebetes/mes_boites', array('accueil','auteur')),
+	);
+	$maj['1.0.1'] = array(
+		array('ecrire_config','pensebetes/mes_objets', array('article')),
+		array('ecrire_config','pensebetes/mes_lieux', array('accueil')),
+		array('ecrire_config','pensebetes/mes_boites', array('accueil','auteur')),
 	);
 	include_spip('base/upgrade');
 	maj_plugin($nom_meta_base_version, $version_cible, $maj);
@@ -27,6 +35,9 @@ function pensebetes_upgrade($nom_meta_base_version, $version_cible){
 function pensebetes_vider_tables($nom_meta_base_version) {
 	sql_drop_table("spip_pensebetes");
 	sql_drop_table("spip_pensebetes_liens");
+	effacer_config("pensebetes/mes_objets");
+	effacer_config("pensebetes/mes_lieux");
+	effacer_config("pensebetes/mes_boites");
 	effacer_meta($nom_meta_base_version);
 }
 
