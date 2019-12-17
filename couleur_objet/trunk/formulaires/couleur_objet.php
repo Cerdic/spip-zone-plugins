cc16e7b14073f21cf9b48d3ea5febe61f68daf11
marcimat@rezo.net <marcimat@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-12-17 15:32:27 +0000

    Soyons fous : introduisons des fonctions un peu plus API dans inc/objet_couleur : - lire_objet_couleur($objet, $id_objet) (alias de objet_couleur : on pourrait inverser remarque) - modifier_objet_couleur($objet, $id_objet, $couleur) - supprimer_objet_couleur($objet, $id_objet)
    On l’utilise dans le formulaire de définition des couleurs.
    Ça permet donc de définir des couleurs depuis un autre lieu.
    
    
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@119102 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- couleur_objet/trunk/formulaires/couleur_objet.php
+++ couleur_objet/trunk/formulaires/couleur_objet.php
@@ -22,24 +22,14 @@ function formulaires_couleur_objet_charger_dist($objet,$id_objet,$couleur_objet)
 	return $valeurs;
 }
 
-function formulaires_couleur_objet_traiter_dist($objet,$id_objet,$couleur_objet){
+function formulaires_couleur_objet_traiter_dist($objet, $id_objet, $couleur_objet) {
+	include_spip('inc/couleur_objet');
 	if (_request('supprimer')){
-		// requête sql dans spip_couleur_objet_liens pour supprimer la ligne où #ID_OBJET = $id_objet et #OBJET = $objet
-		sql_delete("spip_couleur_objet_liens", "id_objet=".intval($id_objet)." AND objet=".sql_quote($objet));
+		supprimer_objet_couleur($objet, $id_objet);
 		set_request('couleur_objet','');
 	}
 	else {
 		$couleur_objet = _request('couleur_objet');
-		$where =  "objet=".sql_quote($objet)." AND id_objet=".intval($id_objet);
-		// si la ligne $id_objet / $objet existe dans la table spip_couleur_objet_liens alors on fait sql_updateq
-		if (sql_countsel('spip_couleur_objet_liens', array(
-			"objet=" . sql_quote($objet),
-			"id_objet=" . sql_quote($id_objet)
-		))) {
-			sql_updateq('spip_couleur_objet_liens', array('couleur_objet' => $couleur_objet), $where);
-		}else{
-			// si la ligne $id_objet / $objet n'existe pas dans la table spip_couleur_objet_liens alors on fait sql_insertq
-			sql_insertq('spip_couleur_objet_liens', array('id_objet' => $id_objet, 'objet' => $objet, 'couleur_objet' => $couleur_objet));
-		}
+		modifier_objet_couleur($objet, $id_objet, $couleur_objet);
 	}
 }
