e2ecb1784491ac499b960c0df885a9d0c75a9a64
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-12-30 09:55:59 +0000

    [Salvatore] [source:_plugins_/nospam/trunk/lang/ nospam] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@119440 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- nospam/trunk/lang/nospam_fr.php
+++ nospam/trunk/lang/nospam_fr.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/nospam/lang/
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/nospam/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
@@ -20,11 +20,11 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'forum_saisie_texte_info' => 'Pour la mise en forme de votre message, ce formulaire n’accepte que les raccourcis SPIP <code>[-&gt;url] {{gras}} {italique} &lt;quote&gt; &lt;code&gt;</code> &lt;cadre&gt;</cadre> et le code HTML <code>&lt;q&gt; &lt;del&gt; &lt;ins&gt;</code>. Pour créer des paragraphes, laissez simplement des lignes vides.',
 
 	// I
-	'info_ip_suspecte' => 'Votre adresse IP est suspectée de procéder à du SPAM. Merci de confirmer votre bonne intention.',
 	'info_alt_antispam' => 'Antispam silencieux - aucun contenu affiché',
+	'info_ip_suspecte' => 'Votre adresse IP est suspectée de procéder à du SPAM. Merci de confirmer votre bonne intention.',
 
 	// L
 	'label_je_ne_suis_pas_un_robot' => 'Je ne suis pas un robot',
 	'label_message_licite' => 'Ceci n’est pas un message abusif',
-	'libelle_je_ne_suis_pas_un_robot' => 'Cliquez pour confirmer que vous n\'êtes pas un robot',
+	'libelle_je_ne_suis_pas_un_robot' => 'Cliquez pour confirmer que vous n’êtes pas un robot'
 );
