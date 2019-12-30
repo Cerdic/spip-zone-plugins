79cc47cba2b1f3c14b97d264f8354420a4521f7d
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-12-30 09:55:44 +0000

    [Salvatore] [source:_plugins_/licence/trunk/lang/ licence] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@119432 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- licence/trunk/lang/licence_fr.php
+++ licence/trunk/lang/licence_fr.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/licence/lang/
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/licence/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
