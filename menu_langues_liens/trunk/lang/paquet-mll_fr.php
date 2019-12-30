582bf668ddac96cac838cf0634936414a6ce2f35
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2019-12-30 09:55:54 +0000

    [Salvatore] [source:_plugins_/menu_langues_liens/trunk/lang/ paquet-mll] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@119438 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- menu_langues_liens/trunk/lang/paquet-mll_fr.php
+++ menu_langues_liens/trunk/lang/paquet-mll_fr.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/menu_langues_liens/lang/
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/menu_langues_liens/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
