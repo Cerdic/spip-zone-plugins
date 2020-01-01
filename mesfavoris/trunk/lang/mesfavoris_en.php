756f4114350775ff48ae805a5304145db0daa7ed
jacques@jack31.net <jacques@jack31.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-01-01 04:38:52 +0000

    [Salvatore] [source:_plugins_/mesfavoris/trunk/lang/ mesfavoris] Export depuis http://trad.spip.net de la langue en
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@119499 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- mesfavoris/trunk/lang/mesfavoris_en.php
+++ mesfavoris/trunk/lang/mesfavoris_en.php
@@ -12,21 +12,38 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// A
 	'add' => 'Add',
 	'add_to' => 'Add to favorites',
-	'ajoute_le' => 'Added', # MODIF
+	'ajoute_le' => 'Added on',
 
 	// C
+	'ce_favori' => 'Bookmark',
+	'change_to' => 'Change',
 	'configurer' => 'Configure',
 
+	// D
+	'dans_categorie' => 'in the category',
+
+	// E
+	'explication_taille_formulaire' => 'You can customize the size of the form simply by using CSS:',
+
 	// I
+	'info_1_favori' => '1 favorite',
+	'info_aucune_categorie' => 'No defined category',
+	'info_categorie_filtre' => 'Filter',
+	'info_categories_tous' => 'All categories',
+	'info_nb_favoris' => '@nb@ favorites',
 	'info_nombre_favoris_objet' => 'Added @nb@ times as favorite.', # Pas utile dans le plugin lui-même mais peut l'être dans les squelettes
 
 	// L
+	'label_style_formulaire_bookmark' => 'Bookmark',
+	'label_style_formulaire_coeur' => 'Heart',
 	'legend_formulaire_public' => 'Public form',
 	'legend_style_formulaire' => 'Form style',
 	'login_first' => 'Sorry, this action requires you to be authenticated. Please log in first',
 
 	// M
 	'mes_favoris' => 'My favs',
+	'modifie_le' => 'Modified on',
+	'mon_favori' => 'My bookmark',
 
 	// R
 	'remove' => 'Remove',
@@ -34,7 +51,9 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// S
 	'ses_favoris' => 'His favorites',
+	'son_favori' => 'His bookmark', # RELIRE
 
 	// V
-	'vos_favoris' => 'Your favorites' # Utile pour modifier le titre de l'inclusion
+	'vos_favoris' => 'Your favorites', # Utile pour modifier le titre de l'inclusion
+	'votre_favori' => 'Your bookmark'
 );
