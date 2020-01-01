dec86950fb98a47e36c8abe3d9d2231b4b882431
george@middleeastwatch.net <george@middleeastwatch.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-01-01 04:38:54 +0000

    [Salvatore] [source:_plugins_/mesfavoris/trunk/lang/ paquet-mesfavoris] Export depuis http://trad.spip.net de la langue ar
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@119501 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ mesfavoris/trunk/lang/paquet-mesfavoris_ar.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-mesfavoris?lang_cible=ar
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// M
+	'mesfavoris_description' => 'يحدد عنصراً ما لإدارة عناصر (مقالات، أقسام ...) مفضلة لزائرين معرّفين',
+	'mesfavoris_slogan' => 'تحديد المفضلات'
+);
