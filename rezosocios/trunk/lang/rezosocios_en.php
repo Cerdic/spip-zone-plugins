2e5bbe9efa7478e925b8bb4b47eee73ccd5e834d
jacques@jack31.org <jacques@jack31.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-01-07 07:59:59 +0000

    [Salvatore] [source:_plugins_/rezosocios/trunk/lang/ rezosocios] Export depuis http://trad.spip.net de la langue en
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@119675 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ rezosocios/trunk/lang/rezosocios_en.php
@@ -0,0 +1,58 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/rezosocios?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cfg_titre_rezosocios' => 'Setting up "Social Networks" ',
+
+	// E
+	'erreur_url_utilisee' => 'The social network profile link is already in use.',
+
+	// I
+	'icone_creation_rezosocio' => 'Create a social network link ',
+	'icone_rezosocios' => 'Social networks',
+	'icone_supprimer_rezosocio' => 'Delete this social network',
+	'info_langue' => 'Profile Language',
+	'info_modifier_rezosocio' => 'Change social network link',
+	'info_nom_compte' => 'Account Name',
+	'info_recherche_rezosocio_zero' => 'No results in social networks for the search "@cherche_rezosocio@"',
+	'info_retirer_rezosocio' => 'Remove this social network link',
+	'info_retirer_rezosocios' => 'Remove all social networks',
+	'info_type_rezosocios' => 'Type of social network',
+	'info_url_site' => 'Profile Link',
+
+	// L
+	'label_activer_rezosocio_objets' => 'Enable social networking links for:',
+	'lien_ajouter_rezosocio' => 'Add this social network link',
+
+	// R
+	'rezosocio' => 'Social network',
+	'rezosocio_ajouter' => 'Add a social network link',
+	'rezosocio_aucun' => 'No social network links',
+	'rezosocio_creer_associer' => 'Create and associate a social network link',
+	'rezosocio_editer' => 'Change this social network link',
+	'rezosocio_logo' => 'Social network link logol',
+	'rezosocio_un' => '1 social network link',
+	'rezosocios' => 'Social networks',
+	'rezosocios_nb' => '@nb@ social networking links',
+
+	// T
+	'texte_nouveau_rezosocio' => 'New social network link',
+	'texte_rezosocio_statut' => 'This social network link is:',
+	'titre_ajouter_un_rezosocio' => 'Add a social network link',
+	'titre_objets_lies_rezosocio' => 'Objects related to this social network',
+	'titre_page_rezosocios_page' => 'Social network links',
+	'titre_tweetsde' => 'Tweets from @@name@',
+	'twitter_hashtag' => 'Twitter (hashtag)',
+
+	// Y
+	'youtube_channel' => 'Youtube (channel)',
+	'youtube_user' => 'Youtube (user account)'
+);
