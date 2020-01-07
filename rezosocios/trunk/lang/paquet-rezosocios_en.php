c2823e51dddaa659ea791f72eccc88d7673b9923
jacques@jack31.org <jacques@jack31.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-01-07 08:00:01 +0000

    [Salvatore] [source:_plugins_/rezosocios/trunk/lang/ paquet-rezosocios] Export depuis http://trad.spip.net de la langue en
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@119676 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ rezosocios/trunk/lang/paquet-rezosocios_en.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-rezosocios?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// R
+	'rezosocios_description' => 'Social networks',
+	'rezosocios_slogan' => 'Management of social network links in SPIP '
+);
