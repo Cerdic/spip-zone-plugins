b5be91abab37614be2bcecce00d81ee59ec6e063
hanjo.spip@gmail.com <hanjo.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-01-10 04:38:48 +0000

    [Salvatore] [source:_plugins_/verifier_plugins/trunk/lang/ paquet-verifier_plugins] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@119718 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ verifier_plugins/trunk/lang/paquet-verifier_plugins_nl.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-verifier_plugins?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// V
+	'verifier_plugins_description' => 'Verifieer de compatibiliteit van je plugins voordat je SPIP naar een nieuwe versie opwaardeert',
+	'verifier_plugins_nom' => 'Verifieer de compatibiliteit van jouw plugins',
+	'verifier_plugins_slogan' => 'Verifieer de compatibiliteit van je plugins voordat je SPIP naar een nieuwe versie opwaardeert'
+);
