718e52c608e0add6bc0ecefb630d71750fea0a0d
peetdu@gmail.com <peetdu@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-01-13 18:01:55 +0000

    Le trunk va changer son intervalle de compatibilité avec la nouvelle version majeure V2 de NoSpam. Et du coup, deviendra elle même une nouvelle version majeure V2. On sauvegarde la version actuelle dans une branche V1. Cela fera du bruit dans Landerneau
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@119746 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ contact_libre/branches/V1/lang/paquet-contact_libre_fr.php
@@ -0,0 +1,13 @@
+<?php
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	'contact_libre_description' => 'Formulaire de contact libre auquel on peut passer directement une adresse mail et une url de r&#233;f&#233;rence (ic&#244;ne de http://www.androidicons.com/).',
+	'contact_libre_nom' => 'Contact libre',
+	'contact_libre_slogan' => 'Un formulaire de contact simple'
+
+);
