5f340d864e38ad11fe6ba00762d1cf433fa8cb47
Cerdic <Cerdic@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-01-13 17:03:09 +0000

    Refactoring : utiliser de preference id_tradlang_module en entree mais supporter aussi module pour compat avec tous les appels
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@119737 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- trad-lang/trunk/inc/tradlang_verifier_langue_base.php
+++ trad-lang/trunk/inc/tradlang_verifier_langue_base.php
@@ -13,17 +13,41 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 /**
  * Fonction de vérification de la concordance d'une langue x par rapport à la langue mère
  *
- * @param string $module
- * 		Le nom du module
+ * @param int $id_tradlang_module
+ * 		id du module concerne
+ *    ou module (deprecated)
  * @param string $langue
  * 		La langue à comparer
+ * @return array
  */
-function inc_tradlang_verifier_langue_base_dist($module, $langue) {
+function inc_tradlang_verifier_langue_base_dist($id_tradlang_module, $langue) {
+
+	// compat anciens appels avec module
+	if (!is_numeric($id_tradlang_module)) {
+		$module = $id_tradlang_module;
+		$ids = sql_allfetsel('id_tradlang_module', 'spip_tradlang_modules', 'module='.sql_quote($module));
+		$ids = array_column($ids, 'id_tradlang_module');
+		$res = [];
+		foreach ($ids as $id) {
+			$res[] = inc_tradlang_verifier_langue_base_dist($id, $langue);
+		}
+		$inserees = array_sum(array_column($res, 0));
+		$supprimees = array_sum(array_column($res, 1));
+		$recuperees = array_sum(array_column($res, 2));
+
+		return array($inserees,$supprimees,$recuperees);
+	}
+
+	if (!$row_module = sql_fetsel('*', 'spip_tradlang_modules', 'id_tradlang_module=' . intval($id_tradlang_module))) {
+		return array(0,0,0);
+	}
+	$where_module = 'id_tradlang_module=' . intval($id_tradlang_module);
+	$module = $row_module['module']; // pour les logs
+
 	/**
 	 * Quelle est la langue mère
 	 */
-	$langue_mere = sql_getfetsel('lang_mere', 'spip_tradlang_modules', 'module = ' . sql_quote($module));
-
+	$langue_mere = $row_module['lang_mere'];
 	$trad_langue_mere_id = $trad_langue_cible_id = $trad_langue_cible_id_attic = array();
 
 	/**
@@ -33,19 +57,14 @@ function inc_tradlang_verifier_langue_base_dist($module, $langue) {
 	 * -* l'ensemble des id de la langue cible qui ont été supprimés par le passé
 	 * (dans le cas où l'on doit en récupérer)
 	 */
-	$trad_langue_meres = sql_allfetsel('id', 'spip_tradlangs', 'module = ' . sql_quote($module) . ' AND lang = ' . sql_quote($langue_mere) . ' AND statut !="attic"', '', 'id');
-	foreach ($trad_langue_meres as $trad_langue_mere) {
-		$trad_langue_mere_id[] = $trad_langue_mere['id'];
-	}
-	$trad_langue_cibles  = sql_allfetsel('id', 'spip_tradlangs', 'module = ' . sql_quote($module) . ' AND lang = ' . sql_quote($langue).' AND statut !="attic"', '', 'id');
-	foreach ($trad_langue_cibles as $trad_langue_cible) {
-		$trad_langue_cible_id[] = $trad_langue_cible['id'];
-	}
+	$trad_langue_mere_id = sql_allfetsel('id', 'spip_tradlangs', "$where_module AND lang=" . sql_quote($langue_mere) . " AND statut!='attic'", '', 'id');
+	$trad_langue_mere_id = array_column($trad_langue_mere_id, 'id');
 
-	$trad_langue_cibles_attic  = sql_allfetsel('id', 'spip_tradlangs', 'module=' . sql_quote($module) . ' AND lang = ' . sql_quote($langue) . ' AND statut ="attic"', '', 'id');
-	foreach ($trad_langue_cibles_attic as $trad_langue_cible_attic) {
-		$trad_langue_cible_id_attic[] = $trad_langue_cible_attic['id'];
-	}
+	$trad_langue_cible_id  = sql_allfetsel('id', 'spip_tradlangs', "$where_module AND lang=" . sql_quote($langue) . " AND statut!='attic'", '', 'id');
+	$trad_langue_cible_id = array_column($trad_langue_cible_id, 'id');
+
+	$trad_langue_cible_id_attic  = sql_allfetsel('id', 'spip_tradlangs', "$where_module AND lang=" . sql_quote($langue) . " AND statut='attic'", '', 'id');
+	$trad_langue_cible_id_attic = array_column($trad_langue_cible_id_attic, 'id');
 
 	$inserees = $supprimees = $recuperees = 0;
 
@@ -57,7 +76,29 @@ function inc_tradlang_verifier_langue_base_dist($module, $langue) {
 	 */
 	$diff1 = array_diff($trad_langue_mere_id, $trad_langue_cible_id);
 	if (count($diff1) > 0) {
-		$diff1_array = sql_allfetsel('*', 'spip_tradlangs', 'module = ' . sql_quote($module) . ' AND lang = ' . sql_quote($langue_mere) . ' AND ' . sql_in('id', $diff1));
+		$diff1_array = sql_allfetsel('*', 'spip_tradlangs', "$where_module AND lang=" . sql_quote($langue_mere) . ' AND ' . sql_in('id', $diff1));
+		foreach ($diff1_array as $diff) {
+			/**
+			 * La chaine était préalablement supprimée
+			 * Elle a le statut "attic"
+			 * On la récupère donc en lui donnant le statut "MODIF"
+			 */
+			if (in_array($diff['id'], $trad_langue_cible_id_attic)) {
+				$titre = $diff['id'].' : '.$diff['module'].' - '.$langue;
+				sql_updateq('spip_tradlangs', array('statut' => 'MODIF', 'titre' => $titre), "$where_module AND id=" . sql_quote($diff['id']) . ' AND lang=' . sql_quote($langue) . " AND statut='attic'");
+				$recuperees++;
+			} else {
+				$diff['orig'] = 0;
+				$diff['lang'] = $langue;
+				$diff['titre'] = $diff['id'].' : '.$diff['module'].' - '.$langue;
+				$diff['statut'] = 'NEW';
+				unset($diff['maj']);
+				unset($diff['id_tradlang']);
+				unset($diff['traducteur']);
+				$id_tradlang = sql_insertq('spip_tradlangs', $diff);
+				$inserees++;
+			}
+		}
 	}
 
 	/**
@@ -65,51 +106,19 @@ function inc_tradlang_verifier_langue_base_dist($module, $langue) {
 	 * et donc à supprimer
 	 */
 	$diff2 = array_diff($trad_langue_cible_id, $trad_langue_mere_id);
-
-	/**
-	 * Si on a des éléments dans les diffs, on applique les modifications
-	 */
-	if ((count($diff1) > 0) or (count($diff2) > 0)) {
-		if (isset($diff1_array) and is_array($diff1_array)) {
-			foreach ($diff1_array as $key => $array) {
-				/**
-				 * La chaine était préalablement supprimée
-				 * Elle a le statut "attic"
-				 * On la récupère donc en lui donnant le statut "MODIF"
-				 */
-				if (in_array($array['id'], $trad_langue_cible_id_attic)) {
-					$titre = $array['id'].' : '.$array['module'].' - '.$langue;
-					sql_updateq('spip_tradlangs', array('statut' => 'MODIF', 'titre' => $titre), 'id = ' . sql_quote($array['id']).' AND lang='.sql_quote($langue).' AND statut="attic"');
-					$recuperees++;
-				} else {
-					$array['orig'] = 0;
-					$array['lang'] = $langue;
-					$array['titre'] = $array['id'].' : '.$array['module'].' - '.$langue;
-					$array['statut'] = 'NEW';
-					unset($array['maj']);
-					unset($array['id_tradlang']);
-					unset($array['traducteur']);
-					$id_tradlang = sql_insertq('spip_tradlangs', $array);
-					$inserees++;
-				}
-			}
+	if (count($diff2) > 0) {
+		foreach ($diff2 as $id) {
+			sql_updateq('spip_tradlangs', array('statut' => 'attic'), "$where_module AND id=" . sql_quote($id) . ' AND lang=' . sql_quote($langue));
+			$supprimees++;
 		}
-		/**
-		 * On donne le statut attic aux chaînes en trop
-		 * On incrémente le nombre de chaînes supprimées
-		 */
-		if (count($diff2) > 0) {
-			foreach ($diff2 as $key => $id) {
-				sql_updateq('spip_tradlangs', array('statut' => 'attic'), 'id = ' . sql_quote($id).' AND lang = ' . sql_quote($langue).' AND module = '.sql_quote($module));
-				$supprimees++;
-			}
-		}
-	} else {
-		return array('0','0','0');
 	}
 
-	include_spip('inc/invalideur');
-	suivre_invalideur('1');
-	spip_log("insert => $inserees - suppressions => $supprimees - recuperations => $recuperees", 'bilan.'._LOG_ERREUR);
-	return array($inserees,$supprimees,$recuperees);
+	if ($inserees + $supprimees + $recuperees > 0) {
+		include_spip('inc/invalideur');
+		suivre_invalideur('1');
+		spip_log("$module: insert => $inserees - suppressions => $supprimees - recuperations => $recuperees", 'bilan.'._LOG_ERREUR);
+		return array($inserees,$supprimees,$recuperees);
+	}
+
+	return array(0,0,0);
 }
