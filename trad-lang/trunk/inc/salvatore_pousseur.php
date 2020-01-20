22e8aef65131e028e81d8ff839adf8d788df3d18
Cerdic <Cerdic@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-01-20 13:53:10 +0000

    commit et push d'un module dans une fonction separee pour plus de lisibilite
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@119861 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- trad-lang/trunk/inc/salvatore_pousseur.php
+++ trad-lang/trunk/inc/salvatore_pousseur.php
@@ -62,127 +62,148 @@ function salvatore_pousser($liste_sources, $dir_modules=null, $dir_depots=null) 
 		// on peut poser un .salvatore.ignore.{module} manuellement pour forcer salvatore a ne jamais pousser certains modules
 		// (gestion de tensions sur certains plugins/modules)
 
-		$file_commit = $dir_module . '/' . $module . '.commit.json';
 
 		if (file_exists($dir_module . '/.salvatore.ignore.' . $module)) {
-			salvatore_log("<info>Module $module ignoré</info>");
+			salvatore_log("<comment>Module $module ignoré</comment>");
 		}
 		else {
-			if (!file_exists($file_commit)
-			  or !$commit_infos = file_get_contents($file_commit)
-			  or !$commit_infos = json_decode($commit_infos, true)) {
-				salvatore_log("<info>Module $module rien à faire (pas de fichier $file_commit ou fichier invalide)</info>");
-			}
-			else {
-				// on a la liste des fichiers a commit
-				$message_commit = '';
-				if (isset($commit_infos['.message'])) {
-					$message_commit = trim($commit_infos['.message']) . "\n";
-					unset($commit_infos['.message']);
-				}
-				$dir_depot = $dir_depots . $source['dir_checkout'];
-				$subdir = '';
-				if (isset($source['dir'])) {
-					$subdir = $source['dir'] . DIRECTORY_SEPARATOR;
-				}
-
-				// reorganiser les fichiers a commit et preparer les messages de commit
-				// - ignorer les fichiers non modifies, ou non versionnes et qui ne doivent pas etre ajoutes
-				// - regrouper par auteur
-
-				$commits_todo = array();
-				$salvatore_status_file = "salvatore_" . $source['methode'] . "_status_file";
-				$salvatore_commit_files = "salvatore_" . $source['methode'] . "_commit_files";
-				$salvatore_push_repository = "salvatore_" . $source['methode'] . "_push_repository";
-
-				foreach ($commit_infos as $what => $commit_info) {
-
-					$file = $commit_info['file_name'];
-
-					if ($commit_info['lastmodified'] or $commit_info['must_add']) {
-						$status = $salvatore_status_file($dir_depots . $source['dir_checkout'], $subdir . $file);
-
-						// fichier nouveau ou modifie (sinon on l'ignore)
-						if ($status) {
-							$author = 0;
-							if (!empty($commit_info['author'])) {
-								$author = $commit_info['author'];
-							}
-							// si c'est le xml et qu'on a un seul auteur de commit, on lui fait commit aussi le xml
-							elseif($what === '.xml' and count($commits_todo)===1) {
-								$author = array_keys($commits_todo);
-								$author = reset($author);
-							}
-
-							if (!isset($commits_todo[$author])) {
-								$commits_todo[$author] = array(
-									'files' => array(),
-									'message' => []
-								);
-								if ($message_commit) {
-									$commits_todo[$author]['message'][] = $message_commit;
-								}
-							}
-							$commits_todo[$author]['files'][] = $subdir . $file;
-							if ($what === '.xml') {
-								$message = "[Salvatore] [source:$subdir $module] Mise a jour du bilan depuis $url_gestionnaire";
-							}
-							else {
-								$message = "[Salvatore] [source:$subdir $module] Export depuis $url_gestionnaire";
-							}
-							if (!empty($commit_info['lang'])) {
-								$message .= " de la langue " . $commit_info['lang'];
-							}
-							if (!empty($commit_info['message'])) {
-								$message .= "\n            " . $commit_info['message'];
-							}
-							$commits_todo[$author]['message'][] = $message;
-						}
-
-					}
-
-				}
-
-				// on peut maintenant lancer les commits
-				// ajoutons les credentials dans la source pour pouvoir commit ou push
-				$url_with_credentials = salvatore_set_credentials($source['methode'], $source['url'], $source['module']);
-				$parts = parse_url($url_with_credentials);
-				if (!empty($parts['user']) and !empty($parts['pass'])){
-					$source['user'] = $parts['user'];
-					$source['pass'] = $parts['pass'];
-				}
-
-				foreach ($commits_todo as $author => $commit_todo) {
-					if (!$author) {
-						$author = _SALVATORE_AUTHOR_COMMITS;
-					}
-					$message = implode("\n", $commit_todo['message']);
-					salvatore_log("Commit de <info>$author</info> :" . implode(', ', $commit_todo['files']));
-					salvatore_log("\t" . str_replace("\n", "\n\t", $message));
-
-					list($res,$out) = $salvatore_commit_files($dir_depots . $source['dir_checkout'], $commit_todo['files'], $message, $author, empty($source['user']) ? null : $source['user'], empty($source['pass']) ? null : $source['pass']);
-					salvatore_log($out);
-					if (!$res) {
-						salvatore_fail("[Pousseur] Erreur sur $module", "Erreur lors du commit :\n$out");
-					}
-				}
-
-				// tous les commits sont faits
-				// on peut supprimer le fichier qui liste les commits
-				@unlink($file_commit);
-
-				// et push si besoin
-				// ne fera rien en svn (deja pushe)
-				list($res,$out) = $salvatore_push_repository($dir_depots . $source['dir_checkout'], empty($source['user']) ? null : $source['user'], empty($source['pass']) ? null : $source['pass']);
-				salvatore_log($out);
-				if (!$res) {
-					salvatore_fail("[Pousseur] Erreur sur $module", "Erreur lors du commit :\n$out");
-				}
+			$res = savatore_commit_and_push_module($source, $dir_modules, $dir_depots, $url_gestionnaire);
+			if ($res) {
+				salvatore_log("Module $module <info>OK</info>\n");
 			}
 		}
-
 	}
 
 	return true;
 }
 
+/**
+ * Commiter et pusher les modifs sur un module
+ * @param array $source
+ * @param string $dir_modules
+ * @param string $dir_depots
+ * @param string $url_gestionnaire
+ * @return bool
+ * @throws Exception
+ */
+function savatore_commit_and_push_module($source, $dir_modules, $dir_depots, $url_gestionnaire) {
+
+	$dir_module = $dir_modules . $source['dir_module'];
+	$module = $source['module'];
+
+	$file_commit = $dir_module . '/' . $module . '.commit.json';
+
+	if (!file_exists($file_commit)
+	  or !$commit_infos = file_get_contents($file_commit)
+	  or !$commit_infos = json_decode($commit_infos, true)) {
+		salvatore_log("<comment>Module $module rien à faire (pas de fichier $file_commit ou fichier invalide)</comment>");
+		return false;
+	}
+
+	// on a la liste des fichiers a commit
+	$message_commit = '';
+	if (isset($commit_infos['.message'])) {
+		$message_commit = trim($commit_infos['.message']) . "\n";
+		unset($commit_infos['.message']);
+	}
+
+	$subdir = '';
+	if (isset($source['dir'])) {
+		$subdir = $source['dir'] . DIRECTORY_SEPARATOR;
+	}
+
+	// reorganiser les fichiers a commit et preparer les messages de commit
+	// - ignorer les fichiers non modifies, ou non versionnes et qui ne doivent pas etre ajoutes
+	// - regrouper par auteur
+
+	$commits_todo = array();
+	$salvatore_status_file = "salvatore_" . $source['methode'] . "_status_file";
+	$salvatore_commit_files = "salvatore_" . $source['methode'] . "_commit_files";
+	$salvatore_push_repository = "salvatore_" . $source['methode'] . "_push_repository";
+
+	foreach ($commit_infos as $what => $commit_info) {
+
+		$file = $commit_info['file_name'];
+
+		if ($commit_info['lastmodified'] or $commit_info['must_add']) {
+			$status = $salvatore_status_file($dir_depots . $source['dir_checkout'], $subdir . $file);
+
+			// fichier nouveau ou modifie (sinon on l'ignore)
+			if ($status) {
+				$author = 0;
+				if (!empty($commit_info['author'])) {
+					$author = $commit_info['author'];
+				}
+				// si c'est le xml et qu'on a un seul auteur de commit, on lui fait commit aussi le xml
+				elseif($what === '.xml' and count($commits_todo)===1) {
+					$author = array_keys($commits_todo);
+					$author = reset($author);
+				}
+
+				if (!isset($commits_todo[$author])) {
+					$commits_todo[$author] = array(
+						'files' => array(),
+						'message' => []
+					);
+					if ($message_commit) {
+						$commits_todo[$author]['message'][] = $message_commit;
+					}
+				}
+				$commits_todo[$author]['files'][] = $subdir . $file;
+				if ($what === '.xml') {
+					$message = "[Salvatore] [source:$subdir $module] Mise a jour du bilan depuis $url_gestionnaire";
+				}
+				else {
+					$message = "[Salvatore] [source:$subdir $module] Export depuis $url_gestionnaire";
+				}
+				if (!empty($commit_info['lang'])) {
+					$message .= " de la langue " . $commit_info['lang'];
+				}
+				if (!empty($commit_info['message'])) {
+					$message .= "\n            " . $commit_info['message'];
+				}
+				$commits_todo[$author]['message'][] = $message;
+			}
+
+		}
+
+	}
+
+	// on peut maintenant lancer les commits
+	// ajoutons les credentials dans la source pour pouvoir commit ou push
+	$url_with_credentials = salvatore_set_credentials($source['methode'], $source['url'], $source['module']);
+	$parts = parse_url($url_with_credentials);
+	if (!empty($parts['user']) and !empty($parts['pass'])){
+		$source['user'] = $parts['user'];
+		$source['pass'] = $parts['pass'];
+	}
+
+	foreach ($commits_todo as $author => $commit_todo) {
+		if (!$author) {
+			$author = _SALVATORE_AUTHOR_COMMITS;
+		}
+		$message = implode("\n", $commit_todo['message']);
+		salvatore_log("Commit de <info>$author</info> :" . implode(', ', $commit_todo['files']));
+		salvatore_log("\t" . str_replace("\n", "\n\t", $message));
+
+		list($res,$out) = $salvatore_commit_files($dir_depots . $source['dir_checkout'], $commit_todo['files'], $message, $author, empty($source['user']) ? null : $source['user'], empty($source['pass']) ? null : $source['pass']);
+		salvatore_log($out);
+		if (!$res) {
+			salvatore_fail("[Pousseur] Erreur sur $module", "Erreur lors du commit :\n$out");
+		}
+	}
+
+	// tous les commits sont faits
+	// on peut supprimer le fichier qui liste les commits
+	@unlink($file_commit);
+
+	// et push si besoin
+	// ne fera rien en svn (deja pushe)
+	list($res,$out) = $salvatore_push_repository($dir_depots . $source['dir_checkout'], empty($source['user']) ? null : $source['user'], empty($source['pass']) ? null : $source['pass']);
+	salvatore_log($out);
+	if (!$res) {
+		salvatore_fail("[Pousseur] Erreur sur $module", "Erreur lors du commit :\n$out");
+	}
+
+	return true;
+}
