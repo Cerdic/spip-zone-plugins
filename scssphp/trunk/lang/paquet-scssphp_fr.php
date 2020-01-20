5164eba389e450bfd9db0f32bc00c59fcf04cfd1
Cerdic <Cerdic@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-01-20 14:07:38 +0000

    [Salvatore] [source:lang/ paquet-scssphp] Export depuis https://trad.spip.net de la langue fr [Salvatore] [source:lang/ paquet-scssphp] Mise a jour du bilan depuis https://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@119872 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- scssphp/trunk/lang/paquet-scssphp_fr.php
+++ scssphp/trunk/lang/paquet-scssphp_fr.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/scssphp/trunk
+// Fichier source, a modifier dans https://git.spip.net/spip-contrib-extensions/scssphp.git
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
