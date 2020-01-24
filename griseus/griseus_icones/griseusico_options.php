e70ab5e1d57de3f1b513a92c3999ba342616dca0
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-01-24 12:20:21 +0000

    Revert avant passage en trunk
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@120019 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ griseus/griseus_icones/griseusico_options.php
@@ -0,0 +1,6 @@
+<?php
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS['theme_prive_defaut'] = 'griseus';
+
+?>
---
+++ griseus/griseus_icones/lang/paquet-griseusico_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// G
+	'griseusico_nom' => 'Griseus : icônes espace privé',
+	'griseusico_slogan' => 'Un jeu d\'icônes pour l\'espace privé',
+	'griseusico_description' => 'Ce plugin fait partie du thème "Griseus". Il remplace les icônes de l\'espace privé par un jeu d\'icônes qui se veut sobre et tente de faire en sorte de ne pas détourner l\'attention du contenu.'
+);
+
+?>
