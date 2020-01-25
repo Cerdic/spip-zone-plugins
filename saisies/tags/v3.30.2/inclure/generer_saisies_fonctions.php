1b50bf480302f51000646ddeda75b57179737aca
Maïeul Rouquette <Maeul Rouquette@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-01-25 19:21:37 +0000

    Tag '/tags/v3.30.2' created from /trunk:119193
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@120636 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ saisies/tags/v3.30.2/inclure/generer_saisies_fonctions.php
@@ -0,0 +1,12 @@
+<?php
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+if (!defined('_SAISIES_AFFICHER_SI_JS_SHOW')) {
+	define ('_SAISIES_AFFICHER_SI_JS_SHOW', 'show(400)');
+}
+if (!defined('_SAISIES_AFFICHER_SI_JS_HIDE')) {
+	define ('_SAISIES_AFFICHER_SI_JS_HIDE', 'hide(400)');
+}
