b99e59d77951c03865af42576f35d455f33455f0
Maïeul Rouquette <Maeul Rouquette@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-01-25 12:04:23 +0000

    formulaire pour remettre les infos_obligatoire, partout
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@120117 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ formidable_rio/trunk/lang/formidable_rio_fr.php
@@ -0,0 +1,11 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/formidable/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	'texte_submit' => 'Réinitialiser',
+	'explication' => 'En cliquant sur ce boutons, vous réinitialiserait la valeur pour indiquer que les champs sont obligatoires.'
+);
