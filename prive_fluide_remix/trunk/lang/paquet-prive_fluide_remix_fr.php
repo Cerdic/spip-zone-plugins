2cedd46ba718e623f53ee9ea837fca1fd03b08c4
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-01-25 10:53:21 +0000

    Oups revert de r120075 et retour du plugin
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@120089 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ prive_fluide_remix/trunk/lang/paquet-prive_fluide_remix_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// P
+	'prive_fluide_remix_description' => '',
+	'prive_fluide_remix_nom' => 'Espace privé plus large',
+	'prive_fluide_remix_slogan' => 'Adaptation du plugin prive_fluie de Tcharlss, en largeur fixe',
+);
