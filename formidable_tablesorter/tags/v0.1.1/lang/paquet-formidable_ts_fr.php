9ebb350ee604e9704c2c2ea06945d642e21c813f
spip.franck@lien-d-amis.net <spip.franck@lien-d-amis.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-01-25 19:22:08 +0000

    Tag '/tags/v0.1.1' created from /trunk:118710
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@120737 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ formidable_tablesorter/tags/v0.1.1/lang/paquet-formidable_ts_fr.php
@@ -0,0 +1,13 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/formidable_tablesorter/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// F
+	'formidable_ts_description' => 'Affichage des réponses formidable sous forme de tableaux triables et filtrables.',
+	'formidable_ts_slogan' => 'Visualiser synthétiquement les réponses'
+);
