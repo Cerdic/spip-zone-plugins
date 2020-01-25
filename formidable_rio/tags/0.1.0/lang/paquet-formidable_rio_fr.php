8a020971e060e0e240483098a2b42a6e84f4ffb9
Maïeul Rouquette <Maeul Rouquette@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-01-25 12:25:36 +0000

    Tag '/tags/0.1.0' created from /trunk:120117
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@120125 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ formidable_rio/tags/0.1.0/lang/paquet-formidable_rio_fr.php
@@ -0,0 +1,13 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/formidable/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// F
+	'formidable_rio' => 'Restaurer facilement la valeur par défaut pour indiquer l\'obligation d\'une saisie formidable',
+	'formidable_rio' => 'Prévenir de l\'obligation, c\'est bien !',
+);
