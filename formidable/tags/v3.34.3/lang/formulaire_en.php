0a0a1a45404e8e95efbd82cb8b3a0a8bd591bfb8
spip.franck@lien-d-amis.net <spip.franck@lien-d-amis.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-01-25 19:21:08 +0000

    Tag '/tags/v3.34.3' created from /trunk:113269
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@120562 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ formidable/tags/v3.34.3/lang/formulaire_en.php
@@ -0,0 +1,22 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/formulaire?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// I
+	'icone_creer_formulaire' => 'Create a new form',
+
+	// M
+	'modifier_formulaire' => 'Edit this form',
+
+	// T
+	'titre_formulaire' => 'Form',
+	'titre_formulaires' => 'Forms',
+	'titre_logo_formulaire' => 'Logo of this form'
+);
