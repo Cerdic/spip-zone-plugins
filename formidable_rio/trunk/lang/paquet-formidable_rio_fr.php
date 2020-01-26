16a628aeb16ddd45b169559290b4427555f597b4
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-01-26 04:40:27 +0000

    [Salvatore] [source:_plugins_/formidable_rio/trunk/lang/ paquet-formidable_rio] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@120755 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- formidable_rio/trunk/lang/paquet-formidable_rio_fr.php
+++ formidable_rio/trunk/lang/paquet-formidable_rio_fr.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/formidable/trunk/lang/
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/formidable_rio/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
@@ -8,6 +8,5 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// F
-	'formidable_rio' => 'Restaurer facilement la valeur par défaut pour indiquer l\'obligation d\'une saisie formidable',
-	'formidable_rio' => 'Prévenir de l\'obligation, c\'est bien !',
+	'formidable_rio' => 'Prévenir de l’obligation, c’est bien !'
 );
