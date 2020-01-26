3b84eacd61470044d629e8dbca2357a66444f088
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-01-26 15:18:28 +0000

    Report de r117561 : "Voir la note" sous forme de chaîne de langue Author: maieul@maieul.net Date: 2019-08-15 00:34:28 +0200 (Jeu, 15 aoû 2019)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@120797 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ bigfoot/lang/bigfoot_fr.php
@@ -0,0 +1,8 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	"voir_note" => 'Voir la note',
+);
