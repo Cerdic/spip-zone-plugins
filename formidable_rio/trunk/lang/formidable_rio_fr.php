96b7f38e8359e6ff01d1075849308ee05a9c2c1b
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-01-26 04:40:26 +0000

    [Salvatore] [source:_plugins_/formidable_rio/trunk/lang/ formidable_rio] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@120754 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- formidable_rio/trunk/lang/formidable_rio_fr.php
+++ formidable_rio/trunk/lang/formidable_rio_fr.php
@@ -1,11 +1,15 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/formidable/trunk/lang/
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/formidable_rio/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-	'texte_submit' => 'Réinitialiser',
-	'explication' => 'En cliquant sur ce boutons, vous réinitialiserez la valeur pour indiquer que les champs sont obligatoires.'
+
+	// E
+	'explication' => 'En cliquant sur ce boutons, vous réinitialiserez la valeur pour indiquer que les champs sont obligatoires.',
+
+	// T
+	'texte_submit' => 'Réinitialiser'
 );
