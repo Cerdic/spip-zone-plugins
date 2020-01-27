cc159800b027cd7eacb8a96096c1d84e15fb95a0
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-01-27 15:42:00 +0000

    Report de r118991 : [Salvatore] [source:_plugins_/prix/trunk/lang/ paquet-prix] Export depuis http://trad.spip.net Author: salvatore@rezo.net Date: 2019-12-11 05:38:49 +0100 (Mer, 11 déc 2019)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@120839 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- prix/lang/paquet-prix_fr.php
+++ prix/lang/paquet-prix_fr.php
@@ -1,14 +1,14 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/prix/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-	// L
-	'prix_description' => 'Interface de programmation pour conna&#238;tre le prix d’un objet SPIP. Ce plugin est un outil pour d&#233;veloppeur.',
+	// P
+	'prix_description' => 'Interface de programmation pour connaître le prix d’un objet SPIP. Ce plugin est un outil pour développeur.',
 	'prix_nom' => 'API Prix',
-	'prix_slogan' => 'API pour conna&#238;tre le prix d’un objet',
+	'prix_slogan' => 'API pour connaître le prix d’un objet'
 );
