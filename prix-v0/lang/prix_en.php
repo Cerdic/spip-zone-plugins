73a76c639b722f6cc75dc762784becd802d690d9
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-01-27 15:39:03 +0000

    Branche v0 de prix
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@120834 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ prix-v0/lang/prix_en.php
@@ -0,0 +1,22 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/prix?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// L
+	'label_prix_ht' => 'ET price',
+	'label_prix_ttc' => 'ATI price',
+	'label_taxes' => 'Taxes',
+	'label_total_ht' => 'Total ET',
+	'label_total_ttc' => 'Total ATI',
+
+	// P
+	'prix_ht' => '@prix@ ET',
+	'prix_ttc' => '@prix@ ATI'
+);
