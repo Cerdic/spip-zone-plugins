af4f33d64dfeeb6bcf2a28d2efa60f520bc0cf00
Cerdic <Cerdic@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-01-29 17:29:17 +0000

    [Salvatore] [source:lang/ paquet-scssphp] Export depuis https://trad.spip.net de la langue en [Salvatore] [source:lang/ paquet-scssphp] Mise a jour du bilan depuis https://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@120879 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ scssphp/trunk/lang/paquet-scssphp_en.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-scssphp?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// S
+	'scssphp_description' => 'Integration of PHP implementation from SCSS [->https://scssphp.github.io/scssphp/]. Needs at least PHP 5.6 to works fine.',
+	'scssphp_titre' => 'SCSS PHP'
+);
