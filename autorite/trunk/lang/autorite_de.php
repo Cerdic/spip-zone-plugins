1e0c012cca2199f36233a76a12bf23e40d456ac5
klaus@spip.de <klaus@spip.de@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-02-03 04:38:20 +0000

    [Salvatore] [source:_plugins_/autorite/trunk/lang/ autorite] Export depuis http://trad.spip.net de la langue de
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@120913 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- autorite/trunk/lang/autorite_de.php
+++ autorite/trunk/lang/autorite_de.php
@@ -35,14 +35,14 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'config_auteurs_statut' => 'Welchen Status erhält ein neuer Autor <b>als Grundeinstellung</b> ?',
 	'config_plugin_qui' => 'Wer darf <strong>Plugin-Einstellungen ändern</strong> (Aktivierung ...) ?',
 	'config_site' => 'Website-Konfiguration',
-	'config_site_qui' => 'Chi può <strong>modificare la configurazione</strong> del sito?',
+	'config_site_qui' => 'Wer darf die <strong>Website-Konfiguration</strong> ändern?',
 	'crayons' => 'Bleistift',
 
 	// D
 	'deja_defini' => 'Die folgenden Rechte sind schon woanders definiert worden:',
 	'deja_defini_suite' => 'Das Plugin « Autorität » kann manche der folgenden Einstellungen nicht ändern, so dass sie u.U. nicht mehr funktionieren.
 	<br />Um diese EInschränkung aufzuheben sollten Sie prüfen, ob Ihre Datei <tt>mes_options.php</tt> oder ein aktives Plugin) diese Funktionen definiert.',
-	'descriptif_1' => 'Questa schermata di configurazione è riservata ai webmaster del sito :',
+	'descriptif_1' => 'Diese Konfigurationssseite ist nur für Webmaster zugänglich :',
 	'descriptif_2' => '<p>Wenn Sie diese Liste ändern möchten, bearbeiten Sie bitte die Datei <tt>config/mes_options.php</tt> (sollte sie nicht existieren, muss sie angelegt werden) und tragen sie dort die IDs der Webmaster in diesem Format ein:</p>
 <pre>&lt;?php
   define(
@@ -58,7 +58,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	</small>',
 	'droits_des_auteurs' => 'Rechte der Autoren',
 	'droits_des_redacteurs' => 'Rechte der Redakteure',
-	'droits_idem_admins' => 'gli stessi diritti degli amministratori',
+	'droits_idem_admins' => 'die selben Rechte wie alle Administratoren',
 	'droits_limites' => 'auf diese Rubriken beschränkte Rechte',
 
 	// E
@@ -67,7 +67,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'espace_publieur' => 'Wiki-Bereich / offene Publikation',
 	'espace_publieur_detail' => 'Wählen Sie einen der folgenden Bereiche für das offene Publizieren durch eingetragene Redakteure und / oder Besucher der Website (Voraussetzung sind ein Interface zur Texteingabe wie der {Stift} und ein Formular zum Anlegen des Artikels) :',
 	'espace_publieur_qui' => 'Sie können das Veröffentlichen durch weitere Nutzer zusätzlich zu den Administratoren freigeben:',
-	'espace_wiki' => 'Spazio wiki',
+	'espace_wiki' => 'Wiki-Bereich',
 	'espace_wiki_detail' => 'Wählen Sie eine Hauptrubrik als Wiki aus, deren Inhalte im öffentlichen Bereich der Website durch jede/n bearbeitet werden können (vorausgesetzt die textbearbeitung wird z.B. durch den {Stift} ermöglicht):',
 	'espace_wiki_mots_cles' => 'Wiki-Bereich per Schlagwort festlegen',
 	'espace_wiki_mots_cles_detail' => 'Wählen Sie hier die Schlagworte, die den Wiki-Modus aktivieren (vorausgesetzt ein Interface zur Textbearbeitung wie der {Stift} ist verfügbar).',
@@ -75,13 +75,13 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'espace_wiki_qui' => 'Sie können dieses Wiki anderen Benutzern als den Administratoren öffnen:',
 
 	// F
-	'forums_qui' => '<strong>Forum :</strong> chi può modificare il contenuto dei forum :',
+	'forums_qui' => '<strong>Forum :</strong> Wer darf Foreninhalte ändern :',
 
 	// I
 	'icone_menu_config' => 'Autorität',
 	'info_gere_rubriques' => 'Verwaltet die folgenden Rubriken:',
 	'info_gere_rubriques_2' => 'Ich verwalte die folgenden Rubriken:',
-	'infos_selection' => '(più sezioni possono essere selezionate tenendo premuto il tasto shift)',
+	'infos_selection' => '(Sie können mehrere Bereiche mit der Umschalttaste auswählen.)',
 	'interdire_admin' => 'Setzen Sie die folgenden Optionen, um Administratoren das Recht zum Anlegen neuer Objekte zu entziehen:',
 
 	// M
@@ -100,7 +100,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// P
 	'pas_acces_espace_prive' => '<strong>Kein Zugang zum Redaktionssystem:</strong> Die Redakteure können nicht auf den internen Bereich zugreifen.',
-	'personne' => 'Person',
+	'personne' => 'Niemand',
 	'petitions_qui' => '<strong>Unterschriften:</strong> Wer die Unterschriften von Petitionen ändern darf:',
 	'publication' => 'Veröffentlichung',
 	'publication_qui' => 'Wer auf dieser Website veröffentlichen darf:',
@@ -111,6 +111,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'redacteur' => 'Redakteur',
 	'redacteur_lire_stats' => '<strong>Statistik für Redakteure</strong>: Redakteure können die Zugriffsstatistiken einsehen.',
 	'redacteur_modifie_article' => '<strong>Reakteure ändern Vorschläge</strong>: Jeder Redakteur darf vorgeschlagene Artikel ändern, auch wenn er nicht der Autor ist.',
+	'redacteurs_voir_auteurs' => '<strong>Redakteur sieht Autoren</strong> : Sollen die Redakteure <strong>die Liste der Autoren und ihrer Mailadressen</strong>ebenso einsehen können wie deren <strong>interne Infoseiten</strong> ?',
 	'refus_1' => '<p>Nur die Webmaster der Seite',
 	'refus_2' => 'dürfen diese Einstellungen ändern.</p>
 <p>Mehr Informationen bietet <a href="https://contrib.spip.net/Autorite">die Dokumentation</a>.</p>',
