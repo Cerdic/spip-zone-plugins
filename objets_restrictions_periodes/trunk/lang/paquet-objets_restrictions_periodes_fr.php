2cba2accbb12e2a9c4e4dc0eb38f8111d026d9e0
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-02-03 13:41:18 +0000

    [Salvatore] [source: paquet-objets_restrictions_periodes] Export depuis https://trad.spip.net de la langue fr [Salvatore] [source: paquet-objets_restrictions_periodes] Mise a jour du bilan depuis https://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@121309 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- objets_restrictions_periodes/trunk/lang/paquet-objets_restrictions_periodes_fr.php
+++ objets_restrictions_periodes/trunk/lang/paquet-objets_restrictions_periodes_fr.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/objets_restrictions_periodes/trunk/lang
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
@@ -10,5 +10,5 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// O
 	'objets_restrictions_periodes_description' => 'Ajoute des restrictions par période au plugin locations_objets_restrictions',
 	'objets_restrictions_periodes_nom' => 'Objets restrictions périodes',
-	'objets_restrictions_periodes_slogan' => 'Extension eériodes pour le plugin plugin locations_objets_restrictions',
+	'objets_restrictions_periodes_slogan' => 'Extension eériodes pour le plugin plugin locations_objets_restrictions'
 );
