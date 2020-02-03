abd6f7bbd107b3d35a7ea1c8238dc9ba1b1f5863
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-02-03 13:39:45 +0000

    [Salvatore] [source: paquet-informerpremiereconnexion] Export depuis https://trad.spip.net de la langue fr [Salvatore] [source: paquet-informerpremiereconnexion] Mise a jour du bilan depuis https://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@121181 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- informer_premiere_connexion/trunk/lang/paquet-informerpremiereconnexion_fr.php
+++ informer_premiere_connexion/trunk/lang/paquet-informerpremiereconnexion_fr.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/informer_premiere_connexion/trunk/lang/
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/informer_premiere_connexion/trunk/lang
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
