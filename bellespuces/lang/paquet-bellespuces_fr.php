d69b67ccffc6de303db8ea8f8c63c47e324436cc
erational@erational.org <erational@erational.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-02-03 11:06:58 +0000

    Ajout d'un paquet.xml pour pouvoir continuer à utiliser le plugin en SPIP 3.3
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@120926 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ bellespuces/lang/paquet-bellespuces_fr.php
@@ -0,0 +1,19 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+// Fichier produit par PlugOnet
+// Module: paquet-bellespuces
+// Langue: fr
+// Date: 03-02-2020 10:53:37
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'bellespuces_description' => 'Ce plugin traite le raccourci SPIP « <code>-</code> » comme « <code>-*</code> », générant de vraies listes (<code>ul/li</code>) dans les deux cas.',
+	'bellespuces_slogan' => 'De vraies listes à puces avec « <code>-</code> », comme « <code>-*</code> » !',
+);
+?>
