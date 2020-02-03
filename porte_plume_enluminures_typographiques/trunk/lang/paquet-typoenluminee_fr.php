e1a7be1fe4e12a5b7a455a963f8efbfc2f43f754
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-02-03 13:39:02 +0000

    [Salvatore] [source: paquet-typoenluminee] Export depuis https://trad.spip.net de la langue fr [Salvatore] [source: paquet-typoenluminee] Mise a jour du bilan depuis https://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@121121 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- porte_plume_enluminures_typographiques/trunk/lang/paquet-typoenluminee_fr.php
+++ porte_plume_enluminures_typographiques/trunk/lang/paquet-typoenluminee_fr.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/porte_plume_enluminures_typographiques/trunk/lang/
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/porte_plume_enluminures_typographiques/trunk/lang
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
