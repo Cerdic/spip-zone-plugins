ecae65a52a98575a5a4528662ae6917bed71f1c0
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-02-03 13:40:19 +0000

    [Salvatore] [source: paquet-locations_objets_restrictions] Export depuis https://trad.spip.net de la langue fr [Salvatore] [source: paquet-locations_objets_restrictions] Mise a jour du bilan depuis https://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@121230 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- locations_objets_restrictions/trunk/lang/paquet-locations_objets_restrictions_fr.php
+++ locations_objets_restrictions/trunk/lang/paquet-locations_objets_restrictions_fr.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/locations_objets_restrictions/trunk/lang
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
@@ -9,6 +9,6 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// L
 	'locations_objets_restrictions_description' => 'Permet de gérer des restrictions pou vos objets louables',
-	'locations_objets_restrictions_nom' => 'Locations d\'objets - restrictions',
-	'locations_objets_restrictions_slogan' => 'Imposer des restriction á vos objets louables',
+	'locations_objets_restrictions_nom' => 'Locations d’objets - restrictions',
+	'locations_objets_restrictions_slogan' => 'Imposer des restriction á vos objets louables'
 );
