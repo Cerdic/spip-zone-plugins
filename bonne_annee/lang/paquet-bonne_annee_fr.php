c4e3c78afebd8a8dfcc8d2623c9cb4f1ec0def73
Salvatore <Salvatore@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-02-05 17:12:31 +0000

    [Salvatore] [source:lang/ paquet-bonne_annee] Export depuis https://trad.spip.net de la langue fr [Salvatore] [source:lang/ paquet-bonne_annee] Mise a jour du bilan depuis https://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@122075 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- bonne_annee/lang/paquet-bonne_annee_fr.php
+++ bonne_annee/lang/paquet-bonne_annee_fr.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/bonne_annee/lang
+// Fichier source, a modifier dans https://git.spip.net/spip-contrib-extensions/bonne_annee.git
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
