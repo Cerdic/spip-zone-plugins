97d336fa3ecc06fabbbaa861755b4edf6f942b08
Salvatore <Salvatore@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-02-05 15:54:57 +0000

    [Salvatore] [source:lang/ paquet-videos] Export depuis https://trad.spip.net de la langue fr [Salvatore] [source:lang/ paquet-videos] Mise a jour du bilan depuis https://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@122048 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- videos/trunk/lang/paquet-videos_fr.php
+++ videos/trunk/lang/paquet-videos_fr.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/videos/trunk/lang
+// Fichier source, a modifier dans https://git.spip.net/spip-contrib-extensions/videos.git
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
