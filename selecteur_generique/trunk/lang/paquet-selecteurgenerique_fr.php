157981995d82614ed1345851df4504f5cb89d69a
Salvatore <Salvatore@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-02-05 15:48:50 +0000

    [Salvatore] [source:lang/ paquet-selecteurgenerique] Export depuis https://trad.spip.net de la langue fr [Salvatore] [source:lang/ paquet-selecteurgenerique] Mise a jour du bilan depuis https://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@121986 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- selecteur_generique/trunk/lang/paquet-selecteurgenerique_fr.php
+++ selecteur_generique/trunk/lang/paquet-selecteurgenerique_fr.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/selecteur_generique/trunk/lang
+// Fichier source, a modifier dans https://git.spip.net/spip-contrib-extensions/selecteur_generique.git
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
