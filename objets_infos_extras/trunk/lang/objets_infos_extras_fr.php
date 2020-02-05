a82476b27fc7afce5c2376670634456a419912fe
Salvatore <Salvatore@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-02-05 15:40:42 +0000

    [Salvatore] [source:lang/ objets_infos_extras] Export depuis https://trad.spip.net de la langue fr [Salvatore] [source:lang/ objets_infos_extras] Mise a jour du bilan depuis https://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@121904 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- objets_infos_extras/trunk/lang/objets_infos_extras_fr.php
+++ objets_infos_extras/trunk/lang/objets_infos_extras_fr.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/objets_infos_extras/trunk/lang
+// Fichier source, a modifier dans https://git.spip.net/spip-contrib-extensions/objets_infos_extras.git
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
