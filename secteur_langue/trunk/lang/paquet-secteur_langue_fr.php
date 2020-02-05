b89e48be19c423f0bb5b9b8c9fae3101bb05f460
Salvatore <Salvatore@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-02-05 15:48:23 +0000

    [Salvatore] [source:lang/ paquet-secteur_langue] Export depuis https://trad.spip.net de la langue fr [Salvatore] [source:lang/ paquet-secteur_langue] Mise a jour du bilan depuis https://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@121982 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- secteur_langue/trunk/lang/paquet-secteur_langue_fr.php
+++ secteur_langue/trunk/lang/paquet-secteur_langue_fr.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/secteur_langue/trunk/lang
+// Fichier source, a modifier dans https://git.spip.net/spip-contrib-extensions/secteur_langue.git
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
