e3fa75e7b4e486ccd6d29964f17cbeb17415add0
Salvatore <Salvatore@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-02-05 15:40:28 +0000

    [Salvatore] [source:lang/ ocr] Export depuis https://trad.spip.net de la langue fr [Salvatore] [source:lang/ ocr] Mise a jour du bilan depuis https://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@121900 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- ocr/trunk/lang/ocr_fr.php
+++ ocr/trunk/lang/ocr_fr.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/ocr/trunk/lang
+// Fichier source, a modifier dans https://git.spip.net/spip-contrib-extensions/ocr.git
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
