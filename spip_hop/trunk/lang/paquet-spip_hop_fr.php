3e96f72c9d65e7fcabf4eedd3dfbd89af07097ec
Salvatore <Salvatore@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-02-06 09:16:15 +0000

    [Salvatore] [source:lang/ paquet-spip_hop] Export depuis https://trad.spip.net de la langue fr [Salvatore] [source:lang/ paquet-spip_hop] Mise a jour du bilan depuis https://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@122148 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spip_hop/trunk/lang/paquet-spip_hop_fr.php
+++ spip_hop/trunk/lang/paquet-spip_hop_fr.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/spip_hop/trunk/lang
+// Fichier source, a modifier dans https://git.spip.net/spip-contrib-extensions/spip_hop.git
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
