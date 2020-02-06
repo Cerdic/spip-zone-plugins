20902b623ea68cbf348db556ec530982fe526981
Salvatore <Salvatore@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-02-06 10:10:02 +0000

    [Salvatore] [source:lang/ ecrire_message] Export depuis https://trad.spip.net de la langue fr [Salvatore] [source:lang/ ecrire_message] Mise a jour du bilan depuis https://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@122164 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- messagerie/branches/messagerie_2_0/lang/ecrire_message_fr.php
+++ messagerie/branches/messagerie_2_0/lang/ecrire_message_fr.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/messagerie/branches/messagerie_2_0/lang
+// Fichier source, a modifier dans https://git.spip.net/spip-contrib-extensions/messagerie.git
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
