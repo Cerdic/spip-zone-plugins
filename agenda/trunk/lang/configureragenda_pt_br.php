e3e97fa314da2625cc5879a74255296a3545ae12
Salvatore <Salvatore@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-02-07 07:48:30 +0000

    [Salvatore] [source:lang/ configureragenda] Export depuis https://trad.spip.net de la langue pt_br [Salvatore] [source:lang/ configureragenda] Mise a jour du bilan depuis https://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@122239 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- agenda/trunk/lang/configureragenda_pt_br.php
+++ agenda/trunk/lang/configureragenda_pt_br.php
@@ -9,6 +9,9 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
+	// E
+	'explication_effacer_evenements_passes' => 'Informe o número de dias após o qual um evento passado será excluído automaticamente (0 ou um número negativo para desativar)',
+
 	// L
 	'label_affichage_debut' => 'Início da lista',
 	'label_affichage_debut_date_jour' => 'Data do dia',
@@ -22,13 +25,18 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'label_affichage_duree' => 'Listar os eventos em',
 	'label_articlepardefaut' => 'ID da matéria padrão',
 	'label_descriptif' => 'Descrição',
+	'label_effacer_evenements_passes' => 'Excluir os eventos passados',
+	'label_filtres_statut_1' => 'Propor os filtros de status',
 	'label_insert_head_css_1' => 'Inserir automaticamente os estilos padrão da agenda',
+	'label_synchro_statut_1' => 'Sincronizar automaticamente os status dos eventos com os das matérias',
 	'label_titre' => 'Título da página',
 	'label_url_evenement' => 'Exibição de um evento',
 	'label_url_evenement_article' => 'na página da matéria associada',
 	'label_url_evenement_evenement' => 'numa página dedicada para cada evento',
-	'legend_presentation_agenda' => 'Apresentação da agenda', # MODIF
+	'legend_nettoyage_agenda' => 'Limpeza regular da agenda',
+	'legend_presentation_agenda' => 'Apresentação da agenda no espaço privado',
+	'legend_presentation_agenda_public' => 'Apresentação da agenda no site público',
 
 	// T
-	'titre_configuration' => 'Exibição da Agenda' # MODIF
+	'titre_configuration' => 'Configuração da Agenda'
 );
