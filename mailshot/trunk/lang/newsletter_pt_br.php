fdfd451d613fa0e8861049894bedd7649e79351f
Salvatore <Salvatore@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-02-07 07:49:22 +0000

    [Salvatore] [source:lang/ newsletter] Export depuis https://trad.spip.net de la langue pt_br [Salvatore] [source:lang/ newsletter] Mise a jour du bilan depuis https://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@122250 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- mailshot/trunk/lang/newsletter_pt_br.php
+++ mailshot/trunk/lang/newsletter_pt_br.php
@@ -10,17 +10,19 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// B
-	'bouton_envoyer' => 'Envoyer !', # MODIF
-	'bouton_tester_envoi' => 'Tester !', # MODIF
+	'bouton_envoyer' => 'Enviar!',
+	'bouton_tester_envoi' => 'Testar!',
 
 	// I
-	'info_envoi_unique_newsletter' => 'Envoyer à 1 personne', # MODIF
-	'info_envoi_unique_reussi' => 'La newsletter a bien été envoyée à @email@', # MODIF
-	'info_envoyer_newsletter' => 'Envoyer l’infolettre', # MODIF
-	'info_test_envoye' => 'La newsletter a été envoyée en test à @email@', # MODIF
-	'info_tester_newsletter' => 'Tester avec un envoi unique', # MODIF
+	'info_envoi_unique_newsletter' => 'Enviar para 1 pessoa',
+	'info_envoi_unique_reussi' => 'A newsletter foi enviada para @email@',
+	'info_envoyer_newsletter' => 'Enviar a newsletter',
+	'info_test_envoye' => 'O teste da newsletter foi enviado para @email@',
+	'info_tester_newsletter' => 'Testar com um envio único',
 
 	// L
-	'label_email_test' => 'à l’adresse email', # MODIF
-	'label_liste' => 'aux abonnés de ' # MODIF
+	'label_date_start' => 'Planejar o envio',
+	'label_email_test' => 'para o endereço de e-mail',
+	'label_liste' => 'para os assinantes de',
+	'label_resend_oui' => 'Reenviar para os destinatários que já receberam'
 );
