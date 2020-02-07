c4a3cf45c593c788dee51d0217422ec0b6b3f7cf
Salvatore <Salvatore@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-02-07 07:49:50 +0000

    [Salvatore] [source:lang/ prix_objets] Export depuis https://trad.spip.net de la langue pt_br [Salvatore] [source:lang/ prix_objets] Mise a jour du bilan depuis https://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@122256 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- prix_objets/trunk/lang/prix_objets_pt_br.php
+++ prix_objets/trunk/lang/prix_objets_pt_br.php
@@ -14,6 +14,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'ajouter_prix' => 'Incluir um preço',
 
 	// C
+	'champ_prix_total_label' => 'Preço total',
 	'choisir' => 'Selecionar',
 	'choix_devise' => 'Moeda',
 	'choix_devises' => 'Seleção de moedas',
@@ -30,12 +31,19 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'htva' => 'impostos não incluídos',
 
 	// I
+	'info_1_prix' => 'Um preço vinculado',
+	'info_actions' => 'Ações',
+	'info_aucun_prix_objets' => 'Nenhum preço',
+	'info_extensions' => 'Extensões',
+	'info_nb_prix' => '@nb@ preços vinculados',
 	'info_prix' => 'Preço',
 	'infos_generales' => 'Informações Gerais',
 	'infos_produits' => 'Informações Produtos',
 
 	// L
+	'label_prix_par_objet_mode' => 'Modo de cálculo do preço:',
 	'label_selection_objet_prix' => 'Incluir um preço aos objetos a seguir:',
+	'legende_prix_par_objet' => 'Cálculo do preço por objeto',
 
 	// P
 	'prix' => 'Preço',
@@ -53,5 +61,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'taxes_surcharge' => 'Aplicar outra taxa (em %)',
 	'titre_page_configurer_prix_objets_objets' => 'Configurar Preços Objetos',
 	'titre_prix_objets' => 'Preço',
+	'titre_prix_par_objet_mode_global' => 'Global (preço da primeira coincidência aplicada globalmente)',
+	'titre_prix_par_objet_mode_prorata' => 'Pro-rata (média dos preços de cada elemento de uma sequência - por exemplo, cada data de um período)',
 	'ttc' => 'impostos incluídos'
 );
