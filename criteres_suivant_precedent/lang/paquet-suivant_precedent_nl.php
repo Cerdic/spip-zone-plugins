bda206ac2874481cf470fa54249d40103ba02e1e
Salvatore <Salvatore@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-02-07 08:50:02 +0000

    [Salvatore] [source:lang/ paquet-suivant_precedent] Export depuis https://trad.spip.net de la langue nl [Salvatore] [source:lang/ paquet-suivant_precedent] Mise a jour du bilan depuis https://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@122265 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ criteres_suivant_precedent/lang/paquet-suivant_precedent_nl.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-suivant_precedent?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// S
+	'suivant_precedent_description' => 'Met deze experimentele plugin kan in een lus het vorige en volgende element gevonden worden. Dit kan met name van nut zijn voor het maken van een navigatie per rubriek of thema (trefwoorden).',
+	'suivant_precedent_nom' => 'Voorwaarden Volgende / Vorige',
+	'suivant_precedent_slogan' => 'Voegt de voorwaarden <code>{suivant}</code> (volgende) en <code>{precedent}</code> (vorige) toe'
+);
