c4a6bd9aec729f28ee08b08c097705fedb45b77b
Salvatore <Salvatore@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-02-08 17:07:29 +0000

    [Salvatore] [source:lang/ configureragenda] Export depuis https://trad.spip.net de la langue en
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@122288 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- agenda/trunk/lang/configureragenda_en.php
+++ agenda/trunk/lang/configureragenda_en.php
@@ -9,6 +9,9 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
+	// A
+	'avertissement_effacer_evenements' => 'Warning: this option irreversibly deletes data from the database', # RELIRE
+
 	// L
 	'label_affichage_debut' => 'Top of the list',
 	'label_affichage_debut_date_jour' => 'Today’s date',
@@ -21,13 +24,18 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'label_affichage_debut_mois_passe' => 'previous @mois@',
 	'label_affichage_duree' => 'List events on',
 	'label_articlepardefaut' => 'Identification of default article',
+	'label_delai_effacer_evenements_passes' => 'Delay before deleting (days)', # RELIRE
 	'label_descriptif' => 'Descriptive',
+	'label_effacer_evenements_passes' => 'Delete past events', # RELIRE
+	'label_filtres_statut_1' => 'Display status filters', # RELIRE
 	'label_insert_head_css_1' => 'Automatically insert the default styles of the agenda',
+	'label_notifier_insitituer_1' => 'Send a notification to administrators for events proposal and publication', # RELIRE
 	'label_synchro_statut_1' => 'Events are automaticaly published/unpublished with the article they are linked to', # RELIRE
 	'label_titre' => 'Page title',
 	'label_url_evenement' => 'Event display',
 	'label_url_evenement_article' => 'on the page of the related article',
 	'label_url_evenement_evenement' => 'on a dedicated page for each event',
+	'legend_nettoyage_agenda' => 'Periodic cleaning of the agenda', # RELIRE
 	'legend_presentation_agenda' => 'Agenda appearance', # RELIRE
 	'legend_presentation_agenda_prive' => 'Agenda display in the admin area', # RELIRE
 	'legend_presentation_agenda_public' => 'Agenda display on the public site', # RELIRE
