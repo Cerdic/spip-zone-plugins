fee5301e651b83766a758ab8cb834277f31ad052
Cerdic <Cerdic@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-02-08 13:41:20 +0000

    Retablir les chaines de langues supprimees par 491bb260
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@122279 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- agenda/trunk/lang/configureragenda_fr.php
+++ agenda/trunk/lang/configureragenda_fr.php
@@ -32,9 +32,11 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'label_url_evenement_article' => 'sur la page de l’article associé',
 	'label_url_evenement_evenement' => 'sur une page dédiée pour chaque événement',
 	'legend_nettoyage_agenda' => 'Nettoyage régulier de l’agenda',
-	'legend_presentation_agenda' => 'Présentation de l’agenda dans l’espace privé',
+	'legend_presentation_agenda' => 'Présentation de l’agenda',
+	'legend_presentation_agenda_prive' => 'Présentation de l’agenda dans l’espace privé',
 	'legend_presentation_agenda_public' => 'Présentation de l’agenda sur le site public',
 
 	// T
 	'titre_configuration' => 'Configuration de l’Agenda'
+	'titre_affichage_agenda_public' => 'Affichage de l’Agenda sur le site public'
 );
