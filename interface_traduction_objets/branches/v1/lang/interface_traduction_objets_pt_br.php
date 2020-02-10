75affa35e1e83c42ed821c6b80105aa17026b780
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-02-10 18:09:44 +0000

    v1.2.1 traductions pt_br merci @Ricardo Porto
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@122364 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- interface_traduction_objets/branches/v1/lang/interface_traduction_objets_pt_br.php
+++ interface_traduction_objets/branches/v1/lang/interface_traduction_objets_pt_br.php
@@ -9,10 +9,16 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
+	// I
+	'info_desactiver_interface_traduction' => 'Desativar a interface de tradução',
+	'info_desactiver_liste_compacte' => 'Desativar as listas compactas',
+	'interface_traduction_objets_titre' => 'Interface de tradução para objetos',
+
 	// O
 	'options' => 'Alterar as opções de idioma',
 
 	// T
+	'titre_page_configurer_interface_traduction_objets' => 'Configuração',
 	'traductions' => 'Traduções',
 
 	// V
