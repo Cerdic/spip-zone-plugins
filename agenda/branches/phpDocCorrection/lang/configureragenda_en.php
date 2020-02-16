22eb34160d0fc24f617aea2e7449168e264f8d41
Maïeul Rouquette <Maeul Rouquette@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-02-16 12:46:26 +0000

    phpDoc à compléter
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@122502 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ agenda/branches/phpDocCorrection/lang/configureragenda_en.php
@@ -0,0 +1,45 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/configureragenda?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'avertissement_effacer_evenements' => 'Warning: this option irreversibly deletes data from the database', # RELIRE
+
+	// L
+	'label_affichage_debut' => 'Top of the list',
+	'label_affichage_debut_date_jour' => 'Today’s date',
+	'label_affichage_debut_date_veille' => 'Yesteday’s date',
+	'label_affichage_debut_debut_mois' => 'Beginning of the month',
+	'label_affichage_debut_debut_mois_1' => 'Début d’année',
+	'label_affichage_debut_debut_mois_prec' => 'Début du mois précédent',
+	'label_affichage_debut_debut_semaine' => 'Beginning of the week',
+	'label_affichage_debut_debut_semaine_prec' => 'Beginning of the previous week',
+	'label_affichage_debut_mois_passe' => 'previous @mois@',
+	'label_affichage_duree' => 'List events on',
+	'label_articlepardefaut' => 'Identification of default article',
+	'label_delai_effacer_evenements_passes' => 'Delay before deleting (days)', # RELIRE
+	'label_descriptif' => 'Descriptive',
+	'label_effacer_evenements_passes' => 'Delete past events', # RELIRE
+	'label_insert_head_css_1' => 'Automatically insert the default styles of the agenda',
+	'label_notifier_insitituer_1' => 'Send a notification to administrators for events proposal and publication', # RELIRE
+	'label_synchro_statut_1' => 'Events are automaticaly published/unpublished with the article they are linked to', # RELIRE
+	'label_titre' => 'Page title',
+	'label_url_evenement' => 'Event display',
+	'label_url_evenement_article' => 'on the page of the related article',
+	'label_url_evenement_evenement' => 'on a dedicated page for each event',
+	'legend_nettoyage_agenda' => 'Periodic cleaning of the agenda', # RELIRE
+	'legend_presentation_agenda' => 'Agenda appearance', # RELIRE
+	'legend_presentation_agenda_prive' => 'Agenda display in the admin area', # RELIRE
+	'legend_presentation_agenda_public' => 'Agenda display on the public site', # RELIRE
+
+	// T
+	'titre_affichage_agenda_public' => 'Agenda display on the public site', # RELIRE
+	'titre_configuration' => 'Agenda configuration' # RELIRE
+);
