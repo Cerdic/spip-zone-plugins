41e29adc5ba9260e3e5d0ed6abc0635ead11bc69
RastaPopoulos <RastaPopoulos@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-02-18 14:53:27 +0000

    Dernière version de pays et du coup pour les adresses, c'est pas la France en dur (je sais même pas pourquoi yavait ça, ça aurait dû être au moinde rien par défaut)… mais bien la config du pays par défaut.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@122531 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ coordonnees/branches/dev-adresses/lang/paquet-coordonnees_en.php
@@ -0,0 +1,9 @@
+<?php
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	'coordonnees_nom' => "Address book",
+	'coordonnees_slogan' => "Postal Adresses , Phone numbers ... for your authors",
+	'coordonnees_description' => "Provides the ability to add postal details (addresses, e-mail, phone numbers) to SPIP objects, specifically to authors. Several addresses or phone numbers can be given to each single author.",
+);
+?>
