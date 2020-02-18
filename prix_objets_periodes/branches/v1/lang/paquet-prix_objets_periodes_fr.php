319027e66b7685848a42e94aa9277e72e4667076
Rainer Müller <Rainer Mller@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-02-18 16:17:36 +0000

    v1.1.5 compatible spip 3.3
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@122558 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- prix_objets_periodes/branches/v1/lang/paquet-prix_objets_periodes_fr.php
+++ prix_objets_periodes/branches/v1/lang/paquet-prix_objets_periodes_fr.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/prix_objets_periodes/trunk/lang/
+// Fichier source, a modifier dans https://git.spip.net/spip-contrib-extensions/prix_objets_periodes.git
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
