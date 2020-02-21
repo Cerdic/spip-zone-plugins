2b66928d1e6ce6e8cf6683112fb8528a9aa5f855
RastaPopoulos <RastaPopoulos@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-02-21 16:32:03 +0000

    Oups… mauvais param, et toujours recalculer les champs
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@122622 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- coordonnees/shelves/RastaPopoulos/action/api_adresses_par_pays.php
+++ coordonnees/shelves/RastaPopoulos/action/api_adresses_par_pays.php
@@ -17,14 +17,14 @@ function action_api_adresses_par_pays_dist() {
 		and $saisies_pays = coordonnees_adresses_saisies_par_pays($code_pays, _request('obligatoire'))
 	){
 		// On remet le bon identifiant
-		if ($identifiant = _request('identifiant')) {
+		if ($identifiant = _request('adresse-id')) {
 			foreach ($saisies_pays as $cle=>$saisie) {
 				$saisies_pays[$cle]['options']['attributs'] = 'data-adresse-id="'.$identifiant.'"';
 			}
 		}
 		
 		// On génère le HTML de ces saisies, avec l'environnement envoyé
-		$contexte = array_merge($_GET, array('saisies' => $saisies_pays));
+		$contexte = array_merge($_GET, array('saisies' => $saisies_pays, 'timestamp' => time()));
 		$html = recuperer_fond('inclure/generer_saisies', $contexte);
 		
 		// On renvoie tout ça
