1a7c559f19916a7ec4dc73a69e0385865731a2a4
Maïeul Rouquette <Maeul Rouquette@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-02-21 22:26:29 +0000

    Pb #13. Pour afficher la différence entre 2 révisions d'un formulaire, utiliser un diff sur yaml plutôt que sur tableau serialisé pour les saisies et les traitements. C'est plus lisible.
    Evidement l'idéal serait de pouvoir construire un vrai système de diff
    de tableau, où d'en trouver un existant, mais bon, c'est plus complexe,
    et c'est déjà un progrès.
    
    Mais du coup on laisse encore ouvert #13.
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@122633 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ formidable/trunk/afficher_diff/formulaire_traitements.php
@@ -0,0 +1,24 @@
+<?php
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+include_spip('inc/diff');
+
+/**
+ * Afficher le diff des saisies de formulaire
+ *
+ * @param string $champ
+ * @param string $old
+ * @param string $new
+ * @param string $format
+ *   apercu, diff ou complet
+ * @return string
+ */
+function afficher_diff_formulaire_traitements($champ, $old, $new, $format = 'diff') {
+	$afficher_diff = charger_fonction('array_serialized', 'afficher_diff', true);
+	return $afficher_diff($champ, $old, $new, 'complet');
+}
+
+
