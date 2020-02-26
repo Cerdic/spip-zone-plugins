4c635ae81864fd609f17703d05b159e45fb251f3
RastaPopoulos <RastaPopoulos@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-02-26 12:40:09 +0000

    Je sais pas quoi pour finir de fusionner ? (RastaPopoulos pige rien à git, 50ème épisode
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@122729 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- coordonnees/branches/dev-adresses/lang/coordonnees_nl.php
+++ coordonnees/branches/dev-adresses/lang/coordonnees_nl.php
@@ -1,11 +1,15 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/coordonnees?lang_cible=nl
+// ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
 	'adresse' => 'Adres',
 	'adresse_perso' => 'Domicilie',
 	'adresse_pro' => 'Werkadres',
@@ -16,20 +20,28 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'ajouter_email_court' => 'Email toevoegen',
 	'ajouter_telephone' => 'Een nummer toevoegen',
 	'ajouter_telephone_court' => 'Nummer toevoegen',
+
+	// B
 	'bouton_dissocier' => 'verwijderen',
 	'bouton_dissocier_adresse' => 'Dit adres verwijderen',
 	'bouton_dissocier_email' => 'Dit email verwijderen',
 	'bouton_dissocier_numero' => 'Deze nummer verwijderen',
+
+	// C
 	'configuration_coordonnees' => 'Configuratie van de coördinaten',
 	'confirmer_suppression_adresse' => 'Bent u zeker dat u dit adres wil afschaffen ?',
 	'confirmer_suppression_email' => 'Bent u zeker dat u dit email wil afschaffen ?',
 	'confirmer_suppression_numero' => 'Bent u zeker dat u deze nummer wil afschaffen ?',
+
+	// E
 	'editer_adresse' => 'Een adres wijzigen',
 	'editer_email' => 'Een email wijzigen',
 	'editer_numero' => 'Een nummer wijzigen',
 	'emails' => 'Emails',
 	'explication_objets_actifs' => 'Op welke editoriale voorwerp moeten de coordinaten voorgesteld ?',
 	'explication_type_email' => 'De type kan « perso » of « pro » zijn',
+
+	// I
 	'info_1_adresse' => '1 adres',
 	'info_1_email' => '1 email',
 	'info_1_numero' => '1 nummer',
@@ -48,6 +60,8 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'item_nouveau_numero' => 'Nieuwe nummer ',
 	'item_nouvel_email' => 'Nieuwe email',
 	'item_nouvelle_adresse' => 'Nieuwe adres',
+
+	// L
 	'label_boite_postale' => 'Postbus',
 	'label_code_postal' => 'Poste code',
 	'label_complement' => 'Aanvulling',
@@ -66,22 +80,32 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'logo_adresse' => 'Logo van het adres',
 	'logo_email' => 'Logo van de email',
 	'logo_numero' => 'Logo van de nummer',
+
+	// M
 	'modifier_adresse' => 'Dit adres veranderen',
 	'modifier_email' => 'Deze email veranderen',
 	'modifier_numero' => 'Deze nummer veranderen',
+
+	// N
 	'nouveau_numero' => 'Nieuwe nummer ',
 	'nouvel_email' => 'Nieuwe email',
 	'nouvelle_adresse' => 'Nieuwe adres',
 	'numero' => 'Nummer  ',
 	'numeros' => 'Nummers',
+
+	// P
 	'placeholder_complement_adresse' => 'verdieping, nummer van het gebouw, …',
 	'placeholder_email' => 'email@site.com',
 	'placeholder_titre_adresse' => 'Naam om het adres te identificeren',
 	'placeholder_titre_email' => 'Naam om de email te identificeren',
 	'placeholder_titre_numero' => 'Naam om de telefoonnummer te identificeren',
+
+	// S
 	'supprimer_adresse' => 'Dit adres afschaffen',
 	'supprimer_email' => 'Deze email afschaffen',
 	'supprimer_numero' => 'Deze nummer afeschaffen',
+
+	// T
 	'titre_coordonnees' => 'Coördinaten',
 	'type_adr_dom' => 'Residentieel',
 	'type_adr_home' => 'Persoonelijke',
@@ -115,5 +139,5 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'type_tel_textphone' => 'textoverschrijver',
 	'type_tel_video' => 'Visioconference',
 	'type_tel_voice' => 'Vocal',
-	'type_tel_work' => 'Professionneel',
+	'type_tel_work' => 'Professionneel'
 );
--- coordonnees/branches/dev-adresses/lang/coordonnees_sk.php
+++ coordonnees/branches/dev-adresses/lang/coordonnees_sk.php
@@ -3,7 +3,9 @@
 // extrait automatiquement de https://trad.spip.net/tradlang_module/coordonnees?lang_cible=sk
 // ** ne pas modifier le fichier **
 
-if (!defined('_ECRIRE_INC_VERSION')) return;
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
@@ -25,6 +27,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'editer_adresse' => 'Upraviť adresu',
 	'editer_email' => 'Upraviť e-mail',
 	'editer_numero' => 'Upraviť telefónne číslo',
+	'email' => 'E-mail',
 	'emails' => 'E-maily',
 	'explication_objets_actifs' => 'Ktoré redakčné objekty ponúkajú podrobnosti?',
 	'explication_type_email' => 'Typ môže byť súkromný alebo do práce',
@@ -32,9 +35,9 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// I
 	'info_adresse_utilisee_par' => 'Túto adresu používa:',
 	'info_email_utilise_par' => 'Tento e-mail používa:',
-	'info_gauche_numero_adresse' => 'Č. adresy',
-	'info_gauche_numero_email' => 'Č. e-mailu',
-	'info_gauche_numero_numero' => 'P. č. tel. čísla',
+	'info_gauche_numero_adresse' => 'Č. adresy', # MODIF
+	'info_gauche_numero_email' => 'Č. e-mailu', # MODIF
+	'info_gauche_numero_numero' => 'P. č. tel. čísla', # MODIF
 	'info_numero_utilise_par' => 'Toto telefónne číslo používa:',
 	'item_nouveau_numero' => 'Nové telefónne číslo',
 	'item_nouvel_email' => 'Nový e-mail',
@@ -50,7 +53,8 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'label_pays' => 'Krajina',
 	'label_region' => 'Región (kraj)',
 	'label_telephone' => 'Telefón',
-	'label_titre' => 'Nadpis',
+	'label_titre' => 'Titulok',
+	'label_type' => 'Typ',
 	'label_type_adresse' => 'Typ adresy',
 	'label_type_email' => 'Typ e-mailu',
 	'label_type_numero' => 'Typ telefónneho čísla',
@@ -86,6 +90,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'type_email_pref' => 'Preferovaný',
 	'type_email_x400' => 'X.400',
 	'type_mel_home' => 'Domov',
+	'type_mel_intl' => 'Medzinárodná',
 	'type_mel_work' => 'Práca',
 	'type_tel_bbs' => 'Odkazová služba',
 	'type_tel_car' => 'Auto',
@@ -105,5 +110,3 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'type_tel_voice' => 'Hlasový',
 	'type_tel_work' => 'Do práce'
 );
-
-?>
