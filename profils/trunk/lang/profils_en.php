ad4fa63e2e873df2221db698f0034e85cab83eeb
Salvatore <Salvatore@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-02-28 00:27:01 +0000

    [Salvatore] [source:lang/ profils] Export depuis https://trad.spip.net de la langue en [Salvatore] [source:lang/ profils] Mise a jour du bilan depuis https://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@122746 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- profils/trunk/lang/profils_en.php
+++ profils/trunk/lang/profils_en.php
@@ -1,6 +1,8 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans https://git.spip.net/spip-contrib-extensions/profils.git
+// extrait automatiquement de https://trad.spip.net/tradlang_module/profils?lang_cible=en
+// ** ne pas modifier le fichier **
+
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
