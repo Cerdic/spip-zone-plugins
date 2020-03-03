9fbd8e92a97e08ef99b7bb7a8c00493663026ba2
cy.altern@gmail.com <cy.altern@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-03-03 20:04:36 +0000

    un peu de doc ne fait pas de mal + compatibilité SPIP 3.3
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@122822 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spip_configs/trunk/lang/paquet-configs_fr.php
+++ spip_configs/trunk/lang/paquet-configs_fr.php
@@ -9,7 +9,9 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// C
 	'configs_description' => 'Ce plugin ajoute une table de configuration générale : spip_configs
-	et un balise #MA_CONFIG\{ma_valeur\} ou #MA_CONFIG\{prefixe/nom_valeur\}',
+	et un balise #MA_CONFIG\{ma_valeur\} ou #MA_CONFIG\{prefixe/nom_valeur\}.
+	Pour permettre l\'édition des configs il faut activer le plugin crayons 
+	et dans sa configuration cocher "Crayons dans le privé" et mettre "configurer_configs" dans le champ "Pages autorisées"',
 	'configs_nom' => 'SPIP Configs ',
 	'configs_slogan' => 'Regrouper les configs des plugins dans une meme table',
 );
