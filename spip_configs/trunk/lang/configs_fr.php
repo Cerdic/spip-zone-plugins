8a491ceb453601d379fc9e63d345007832750cc4
cy.altern@gmail.com <cy.altern@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-03-03 20:18:21 +0000

    Autre accès pour les explications
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@122825 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spip_configs/trunk/lang/configs_fr.php
+++ spip_configs/trunk/lang/configs_fr.php
@@ -12,6 +12,10 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'cfg_exemple' => 'Exemple',
 	'cfg_exemple_explication' => 'Explication de cet exemple',
 	'cfg_titre_parametrages' => 'Paramétrages',
+	
+	// H
+	'howto_edition' => 'Pour permettre l\'édition des configs il faut activer le plugin Crayons 
+	et dans sa configuration cocher "Crayons dans le privé" et mettre "configurer_configs" dans le champ "Pages autorisées"',
 
 	// T
 	'titre_page_configurer_configs' => 'SPIP Configs ',
