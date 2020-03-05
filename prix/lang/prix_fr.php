dacd36f284479b68b94abe5905e8f0bd32e52270
Salvatore <Salvatore@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-03-05 00:27:19 +0000

    [Salvatore] [source:lang/ prix] Export depuis https://trad.spip.net de la langue fr [Salvatore] [source:lang/ prix] Mise a jour du bilan depuis https://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@122847 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- prix/lang/prix_fr.php
+++ prix/lang/prix_fr.php
@@ -10,22 +10,22 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// C
 	'cfg_choisir_option' => 'Veuillez choisir une option',
 	'cfg_devise_label' => 'Devise par défaut',
-	'cfg_locale_label' => 'Langue par défaut',
 	'cfg_locale_explication' => 'Langue à utiliser pour formater les prix. C’est par défaut la langue du site, mais vous pouvez préciser une langue régionale si nécessaire (anglais australien, anglais des états-unis, etc.)',
+	'cfg_locale_label' => 'Langue par défaut',
 	'cfg_titre_parametrages' => 'Configuration',
 
 	// I
-	'icone_voir_devises' => 'Voir les devises',
 	'icone_voir_demo' => 'Page de démonstration',
+	'icone_voir_devises' => 'Voir les devises',
 	'info_1_devises' => '1 devise',
 	'info_nb_devises' => '@nb@ devises',
 
 	// L
 	'label_devise_code' => 'Code alphabétique',
 	'label_devise_code_num' => 'Code numérique',
-	'label_devise_symbole' => 'Symbole',
-	'label_devise_nom' => 'Nom',
 	'label_devise_fraction' => 'Fraction',
+	'label_devise_nom' => 'Nom',
+	'label_devise_symbole' => 'Symbole',
 	'label_prix_ht' => 'Prix HT',
 	'label_prix_ttc' => 'Prix TTC',
 	'label_taxes' => 'Taxes',
@@ -33,7 +33,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'label_total_ttc' => 'Total TTC',
 
 	// M
-	'message_extensions_php_ou' => 'Une des extensions PHP suivante est nécessaire : <em>@ext@</em>. <br>Installez-en une ou contactez votre hébergeur (de préférence la 1ère listée).',
+	'message_extensions_php_ou' => 'Une des extensions PHP suivante est nécessaire : <em>@ext@</em>. <br>Installez-en une ou contactez votre hébergeur (de préférence la 1ère listée).',
 
 	// P
 	'prix_ht' => '@prix@ HT',
@@ -42,5 +42,5 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// T
 	'titre_devises' => 'Devises',
 	'titre_page_configurer' => 'Configurer Prix',
-	'titre_prix' => 'Prix',
+	'titre_prix' => 'Prix'
 );
