147f2ca195d4a4c20e65c363f9932105a8eff15b
Salvatore <Salvatore@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-03-07 20:07:55 +0000

    [Salvatore] [source:lang/ prix] Export depuis https://trad.spip.net de la langue en [Salvatore] [source:lang/ prix] Export depuis https://trad.spip.net de la langue es [Salvatore] [source:lang/ prix] Mise a jour du bilan depuis https://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@122891 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- prix/lang/prix_en.php
+++ prix/lang/prix_en.php
@@ -9,14 +9,40 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
+	// C
+	'cfg_choisir_option' => 'Please choose an option',
+	'cfg_devise_label' => 'Default currency',
+	'cfg_locale_explication' => 'Language used for formatting prices. It is the site language by default, but you can specify a regional one if necessary (Australian English, English of the United States, etc.)', # RELIRE
+	'cfg_locale_label' => 'Default language',
+	'cfg_titre_parametrages' => 'Configuration',
+
+	// I
+	'icone_voir_demo' => 'Demo page',
+	'icone_voir_devises' => 'View currencies',
+	'info_1_devises' => '1 currency',
+	'info_nb_devises' => '@nb@ currencies',
+
 	// L
+	'label_devise_code' => 'Alphabetical code',
+	'label_devise_code_num' => 'Numeric code',
+	'label_devise_fraction' => 'Fraction',
+	'label_devise_nom' => 'Name',
+	'label_devise_symbole' => 'Symbol',
 	'label_prix_ht' => 'ET price',
 	'label_prix_ttc' => 'ATI price',
 	'label_taxes' => 'Taxes',
 	'label_total_ht' => 'Total ET',
 	'label_total_ttc' => 'Total ATI',
 
+	// M
+	'message_extensions_php_ou' => 'One of the following PHP extensions is required: <em>@ext@</em>. <br> Install one or contact your host (preferably the first listed).', # RELIRE
+
 	// P
 	'prix_ht' => '@prix@ ET',
-	'prix_ttc' => '@prix@ ATI'
+	'prix_ttc' => '@prix@ ATI',
+
+	// T
+	'titre_devises' => 'Currencies',
+	'titre_page_configurer' => 'Configure Prices',
+	'titre_prix' => 'Price'
 );
---
+++ prix/lang/prix_es.php
@@ -0,0 +1,47 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/prix?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cfg_choisir_option' => 'Por favor elige una opción', # RELIRE
+	'cfg_devise_label' => 'Moneda por defecto', # RELIRE
+	'cfg_locale_explication' => 'Idioma a utilizar para formatear precios. Por defecto, es el idioma del sitio, pero puede especificar un idioma regional si es necesario (inglés australiano, inglés de los Estados Unidos, etc.)', # RELIRE
+	'cfg_locale_label' => 'Idioma por defecto', # RELIRE
+	'cfg_titre_parametrages' => 'Configuracion', # RELIRE
+
+	// I
+	'icone_voir_demo' => 'Página de demostración', # RELIRE
+	'icone_voir_devises' => 'Ver monedas', # RELIRE
+	'info_1_devises' => '1 moneda', # RELIRE
+	'info_nb_devises' => '@nb@ monedas', # RELIRE
+
+	// L
+	'label_devise_code' => 'Código alfabético', # RELIRE
+	'label_devise_code_num' => 'Código numérico', # RELIRE
+	'label_devise_fraction' => 'Fracción', # RELIRE
+	'label_devise_nom' => 'apellido', # RELIRE
+	'label_devise_symbole' => 'Símbolo', # RELIRE
+	'label_prix_ht' => 'Precio excl.', # RELIRE
+	'label_prix_ttc' => 'Precio incl.', # RELIRE
+	'label_taxes' => 'Impuestos', # RELIRE
+	'label_total_ttc' => 'Precio total', # RELIRE
+
+	// M
+	'message_extensions_php_ou' => 'Se requiere una de las siguientes extensiones PHP: <em>@ext@</em>. <br> Instale uno o póngase en contacto con su host (preferiblemente el primero en la lista).', # RELIRE
+
+	// P
+	'prix_ht' => '@prix@ excl.', # RELIRE
+	'prix_ttc' => '@prix@ incl.', # RELIRE
+
+	// T
+	'titre_devises' => 'Monedas', # RELIRE
+	'titre_page_configurer' => 'Configurar Precio', # RELIRE
+	'titre_prix' => 'Precio' # RELIRE
+);
