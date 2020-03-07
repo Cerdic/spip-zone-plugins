7b204ba08224e616eaa44d145ded1d09f70bd9c6
Charles Razack <Charles Razack@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-03-07 17:24:05 +0000

    Comme dans abonnements, ajout d'un footer surchargeable dans le résumé d'un abonnement. Utiliser une chaîne de langue dans le formulaire.  Et màj de la version en dépendance.
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@122875 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- commandes_abonnements/trunk/lang/abonnementsoffre_fr.php
+++ commandes_abonnements/trunk/lang/abonnementsoffre_fr.php
@@ -6,6 +6,9 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
+	// B
+	'bouton_choisir_offre' => 'Choisir cette offre',
+
 	// C
 	'champ_montant_minimum_label' => 'Montant minimum',
 	'champ_montant_perso_label' => 'Montant personnalisé',
@@ -13,6 +16,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'champ_renouvellement_auto_label' => 'Renouvellement',
 	'champ_renouvellement_auto_label_case' => 'Activer le renouvellement automatique',
 	
+	// E
 	'explication_renouvellement' => 'Si vous avez déjà un abonnement actif à cette offre, en vous connectant, celui-ci sera prolongé à partir de sa date de fin.',
 	
 	// I
