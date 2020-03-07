4692736123c5d320c1768cd421b9ac88bc638d12
Charles Razack <Charles Razack@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-03-07 16:33:24 +0000

    Depuis 85c62674c4 il y a un statut « abandonne », on l'utilise pour l'action abandonner_commande au lieu de mettre à la poubelle
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@122870 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- commandes/trunk/action/abandonner_commande.php
+++ commandes/trunk/action/abandonner_commande.php
@@ -10,18 +10,18 @@
  */
 
 // Sécurité
-if (!defined("_ECRIRE_INC_VERSION")) return;
+if (!defined("_ECRIRE_INC_VERSION")) {
+	return;
+}
 
 /**
  * Abandonne une commande liee a une transaction (appel conventionnel par abandonner_transaction)
- *
- * @uses action_supprimer_commande_dist()
  *
  * @param $arg string
  *     id_commande : identifiant de la commande
  * @return void
  */
-function action_abandonner_commande_dist($arg=null) {
+function action_abandonner_commande_dist($arg = null) {
 
 	// Si $arg n'est pas donné directement, le récupérer via _POST ou _GET
 	if (is_null($arg)){
@@ -30,9 +30,15 @@ function action_abandonner_commande_dist($arg=null) {
 	}
 	$id_commande = $arg;
 
-	// pas de statut abandon pour le moment, on passe en poubelle donc
-	$supprimer_commande = charger_fonction('supprimer_commande','action');
-	$supprimer_commande($id_commande);
+	// Passer le statut en abandonné
+	if ($id_commande = intval($id_commande)) {
+		include_spip('base/abstract_sql');
+		spip_log("Commande $id_commande → abandon", 'commandes');
+		sql_updateq(
+			'spip_commandes',
+			array('statut' => 'abandonne'),
+			'id_commande = '.intval($id_commande)
+		);
+	}
 
 }
-
