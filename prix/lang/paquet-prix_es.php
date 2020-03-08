6f58e60fb6adf4e714e287325bc4407936a5fde6
Salvatore <Salvatore@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-03-08 00:27:01 +0000

    [Salvatore] [source:lang/ paquet-prix] Export depuis https://trad.spip.net de la langue es [Salvatore] [source:lang/ paquet-prix] Mise a jour du bilan depuis https://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@122892 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ prix/lang/paquet-prix_es.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-prix?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// P
+	'prix_description' => 'Interfaz de programación para conocer el precio de un objeto SPIP. Este complemento es una herramienta para desarrolladores.', # RELIRE
+	'prix_nom' => 'Precio API', # RELIRE
+	'prix_slogan' => 'API para saber el precio de un objeto' # RELIRE
+);
