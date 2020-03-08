e27ea0602a3288eee25c59e6f764ee52be1116f1
Maïeul Rouquette <Maeul Rouquette@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-03-08 19:37:00 +0000

    ne pas supprimer les inscriptions aux evenements que l'on vient juste de créer (cas des inscriptions à de multiples evenements)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@122900 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ formidable_participation/branches/bug_multiple_Jack/lang/formidableparticipation_fr.php
@@ -0,0 +1,41 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans https://git.spip.net/spip-contrib-extensions/formidable_participation.git
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// F
+	'formidableparticipation_titre' => 'Formulaires de participation',
+
+	// T
+	'traiter_participation_autoriser_email_multiple_label' => 'Permettre à une même adresse email de s’inscrire plusieurs fois',
+	'traiter_participation_champ_choix_participation_explication' => 'Le champ qui propose participation ou non-participation',
+	'traiter_participation_champ_choix_participation_label' => 'Champ de la participation',
+	'traiter_participation_champ_email_label' => 'Champ de l’Email',
+	'traiter_participation_champ_evenement_participation_label' => 'Champ d’évènement',
+	'traiter_participation_champ_evenement_type_fixe_label' => 'Évènement fixe',
+	'traiter_participation_champ_evenement_type_label' => 'Type d’évènement',
+	'traiter_participation_champ_evenement_type_variable_label' => 'Évènement variable - dépendant d’un champ',
+	'traiter_participation_champ_participant' => 'Informations sur le ou la participant·e',
+	'traiter_participation_champ_participation_auto_choix_auto_label' => 'Le remplissage du formulaire implique automatiquement la participation à l’évènement',
+	'traiter_participation_champ_participation_auto_choix_variable_label' => 'La participation à l’événement dépend d’un champ du formulaire',
+	'traiter_participation_champ_participation_auto_label' => 'Participation',
+	'traiter_participation_champ_participation_label' => 'Champs de participation',
+	'traiter_participation_champ_participation_non' => 'Valeur pour la non-participation',
+	'traiter_participation_champ_participation_oui' => 'Valeur pour la participation',
+	'traiter_participation_description' => 'Inscription à un évènement',
+	'traiter_participation_evenement_label' => 'Choix de l’évènement auquel participer',
+	'traiter_participation_id_evenement_participation' => 'id_evenement',
+	'traiter_participation_id_evenement_participation_explication' => 'Permet de lier les réponses à un évènement',
+	'traiter_participation_necessite_explication' => 'Pour la prise en charge de la participation, il est nécessaire d’enregistrer les résultats dans la base de données.',
+	'traiter_participation_option_champ_nb_inscriptions_explication' => 'Le nombre d’inscription correspondra au total des champs cochés.',
+	'traiter_participation_option_champ_nb_inscriptions_label' => 'Champ indiquant le nombre d’inscriptions',
+	'traiter_participation_option_champ_nom_label' => 'Champ du Nom',
+	'traiter_participation_option_champ_organisme_label' => 'Champ de l’Organisme',
+	'traiter_participation_option_champ_prenom_label' => 'Champ du Prénom',
+	'traiter_participation_option_plusieurs_fois_label' => 'Inscrire plusieurs fois la personne à l’évènement (inscription de groupe)',
+	'traiter_participation_titre' => 'Participation à un évènement'
+);
