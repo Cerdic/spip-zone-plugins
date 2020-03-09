50f50bbdd1e1b20b0cc6ebb871243e971b5e9d6f
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-03-09 00:27:04 +0000

    [Salvatore] [source: paquet-collections] Export depuis https://trad.spip.net de la langue pt_br [Salvatore] [source: paquet-collections] Mise a jour du bilan depuis https://trad.spip.net Credits : filipaun <filipaun@gmail.com>
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@122904 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ emballe_medias/media_collections/trunk/lang/paquet-collections_pt_br.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-collections?lang_cible=pt_br
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'collections_description' => 'Criar coleções de mídias, de álbuns...',
+	'collections_nom' => 'Coleções de mídias',
+	'collections_slogan' => 'Criar coleções de mídias'
+);
