b845dd20b27b3c65c5bfa9568b727dad0cb801f8
salvatore@rezo.net <salvatore@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-03-09 00:27:02 +0000

    [Salvatore] [source: collection] Export depuis https://trad.spip.net de la langue pt_br [Salvatore] [source: collection] Mise a jour du bilan depuis https://trad.spip.net Credits : filipaun <filipaun@gmail.com>
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@122903 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ emballe_medias/media_collections/trunk/lang/collection_pt_br.php
@@ -0,0 +1,148 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/collection?lang_cible=pt_br
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'ajouter_lien_collection' => 'Acrescentar esta coleção',
+
+	// B
+	'bouton_diogene_statut_poubelle_normal' => 'Colocar esta coleção na lixeira',
+	'bouton_diogene_statut_poubelle_normal_pas_auteur' => 'Colocar esta coleção na lixeira',
+	'bouton_dissocier' => 'Desassociar',
+	'bouton_fermer_collection' => 'Transformar esta coleção particular',
+	'bouton_media_associer' => 'Associar a uma coleção',
+	'bouton_ouvrir_collection' => 'Transformar esta coleção colaborativa',
+	'bouton_quitter_collection' => 'Não colaborar mais com esta coleção',
+	'bouton_rejoindre_collection' => 'Colaborar com esta coleção',
+
+	// C
+	'collections_titre' => 'As coleções',
+
+	// D
+	'description_page_collections' => 'Conjunto de coleções do site.',
+	'diogene_champ_collection' => 'Acréscimo a uma coleção',
+
+	// E
+	'erreur_association_collection' => 'A associação a uma coleção falhou.',
+	'erreur_autorisation_statut_modifier' => 'Você não está autorizado a modificar esta coleção.',
+	'erreur_collection_non_existante' => 'Esta coleção não existe.',
+	'erreur_media_document' => 'Atenção. O tipo de documento não corresponde ao tipo de coleção.',
+	'erreur_selectionner_un_media' => 'Escolha ao menos uma mídia.',
+	'explication_genre' => 'Que tipos de média podem ser anexados a esta coleção?',
+	'explication_genre_defaut' => 'Valor padrão para os tipos de mídia que podem ser anexados (útil para a criação rápida).',
+	'explication_medias_ajouter' => 'As mídias abaixo não estão associadas à coleção e podem corresponder ao seu tipo. Selecione aquelas que deseja acrescentar.',
+	'explication_medias_modifier' => 'As mídias abaixo estão associadas à coleção. Para desassociar algumas, selecione-as e aperte o botão de suprimir.',
+	'explication_medias_modifier_drag' => 'Você pode reordenar as mídias arrastando / soltando.',
+	'explication_participer' => 'Clique no botão abaixo para participar desta coleção.',
+	'explication_participer_non' => 'Aperte no botão abaixo para deixar de participar desta coleção.',
+	'explication_type' => 'Qual é uso da coleção?',
+	'explication_type_defaut' => 'Valor padrão do uso da coleção (útil para a criação rápida).',
+
+	// G
+	'genre_mixed' => 'Mix (todos os tipos)',
+	'genre_musique' => 'Sons (unicamente de tipo sonoro: música...)',
+	'genre_photo' => 'Imagens (unicamente de tipo imagem: fotos, ícones...)',
+	'genre_video' => 'Vídeos (unicamente de tipo vídeo)',
+
+	// I
+	'icone_creer_collection' => 'Criar uma coleção',
+	'icone_modifier_collection' => 'Modificar esta coleção',
+	'info_1_collection' => 'Uma coleção',
+	'info_aucun_collection' => 'Nenhuma coleção disponível',
+	'info_collaborateurs' => 'Os coleaboradores:',
+	'info_collection_creation' => 'Coleção criada em <abbr title="@date_abbr@">@date@</abbr> <span class="auteurs">por @auteurs@</span>.',
+	'info_collection_maj' => 'Atualizado: ',
+	'info_collections_auteur' => 'As coleções deste autor',
+	'info_media_dans_collection' => 'Na coleção: ',
+	'info_media_dans_collections' => 'Nas @nb@ coleções: ',
+	'info_medias_1' => 'Uma mídia',
+	'info_medias_aucun' => 'Nenhuma mídia',
+	'info_medias_nb' => '@nb@ mídias',
+	'info_nb_collections' => '@nb@ coleções',
+	'info_non_autorise_creer_collection' => 'O autor "@id_auteur@" não está autorizado a criar uma coleção.',
+	'info_non_autorise_modifier_collection' => 'O autor "@id_auteur@" não está autorizado a modificar a coleção @id@.',
+	'info_rang_collection' => 'Ranking na coleção',
+
+	// L
+	'label_choix_collection' => 'Associar a uma coleção',
+	'label_collections_ajouter' => 'Acrescentar a uma coleção',
+	'label_collections_retirer' => 'Retirar da coleção',
+	'label_date' => 'Data',
+	'label_date_point' => 'Data:',
+	'label_descriptif' => 'Descrição',
+	'label_descriptif_point' => 'Descrição:',
+	'label_genre' => 'Tipos de mídia',
+	'label_genre_defaut' => 'Tipos de mídia padrão',
+	'label_genre_point' => 'Tipos de mídia:',
+	'label_id_admin' => 'Administrador da coleção',
+	'label_id_admin_point' => 'Administrador da coleção:',
+	'label_mediabox' => 'MediaBox',
+	'label_mediabox_long' => 'Não utilizar o MediaBox para exibir o formulário de associação de mídias a uma coleção.',
+	'label_titre' => 'Título',
+	'label_titre_creer_rapide' => 'Título da coleção',
+	'label_titre_point' => 'Título:',
+	'label_type' => 'Uso',
+	'label_type_defaut' => 'Uso padrão',
+	'label_type_point' => 'Uso:',
+	'legende_collection' => 'Coleções',
+	'legende_medias_collection_ajouter' => 'Acrescentar mídias',
+	'legende_medias_collection_modifier' => 'Modificar a lista de mídias',
+	'lien_editer_collection' => 'Editar esta coleção',
+	'lien_toutes_collections' => 'Todas as coleções',
+
+	// M
+	'message_aucune_collection_critere' => 'Nenhuma coleção corresponde aos seus critérios.', # Utile pour des squelettes
+	'message_aucune_collection_publiee' => 'Nenhuma coleção está atualmente publicada.', # Utile pour des squelettes
+	'message_collection_aucun_media_publie' => 'Nenhuma mídia a qual você é autor e correspondente ao tipo da coleção está disponível.',
+	'message_collection_media_utiliser_autres_auteurs' => 'Você pode associar as mídias de outros autores utilizando o formulário de acréscimo em cada página de mídia.',
+	'message_collection_media_utiliser_interface' => 'Publique uma ou várias mídias via a interface dedicada para poder as acrescentar nesta coleção.',
+	'message_collection_reorganisee' => 'A coleção foi reorganizada.',
+	'message_collection_vide' => 'A coleção está vazia',
+	'message_nombre_ajoute' => '1 mídia foi associada a esta coleção.',
+	'message_nombre_ajoutes' => '@nb@ mídias foram associadas a esta coleção.',
+	'message_nombre_dissocie' => '1 mídia foi desassociada da coleção.',
+	'message_nombre_dissocies' => '@nb@ mídias foram desassociadas da coleção.',
+
+	// O
+	'optgroup_collections_autres' => 'As outras coleções (colaborativas)',
+	'optgroup_collections_votre' => 'Suas coleções',
+	'option_erreur_genre' => 'O tipo não corresponde à mídia: @genre@',
+
+	// P
+	'publier_une_collection' => 'Uma coleção',
+	'publier_une_collection_desc' => 'Criar uma coleção de mídias.',
+
+	// R
+	'retirer_lien_collection' => 'Retirar esta coleção',
+	'retirer_tous_liens_collections' => 'Retirar todas as coleções',
+
+	// T
+	'tab_associer_collection' => 'Módias da coleção',
+	'tab_editer_collection' => 'Edição da coleção',
+	'texte_ajouter_collection' => 'Acrescentar uma coleção',
+	'texte_changer_statut_collection' => 'Esta coleção está:',
+	'texte_creer_associer_collection' => 'Criar e associar uma coleção',
+	'texte_statut_publie' => 'publicada online',
+	'title_voir_media' => 'Ver a mídia: @titre@',
+	'titre_collection' => 'Coleção',
+	'titre_collections' => 'Coleções',
+	'titre_collections_rubrique' => 'Sessões da sessão',
+	'titre_creer_collection_rapide' => 'Criar uma coleção',
+	'titre_langue_collection' => 'Idioma desta coleção',
+	'titre_logo_collection' => 'Logotipo desta oleção',
+	'titre_media' => 'Mídia #@rang@ : @titre@',
+	'titre_modifier_collection' => 'Modificar a coleção: @titre@',
+	'titre_modifier_collection_diogene' => 'Modificar esta coleção',
+	'titre_page_collections' => 'As coleções',
+	'titre_page_configurer_collections' => 'Configurar as coleções',
+	'titre_participer' => 'Participar',
+	'type_coop' => 'Colaborativo',
+	'type_perso' => 'Particular'
+);
