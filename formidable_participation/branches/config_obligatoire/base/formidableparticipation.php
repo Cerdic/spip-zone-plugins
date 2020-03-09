f703431ec4c25e3dbc2cdc5fcc8ab3ed4a4f2400
Maïeul Rouquette <Maeul Rouquette@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-03-09 09:34:09 +0000

    Branch '/branches/config_obligatoire' deleted
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@122910 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- formidable_participation/branches/config_obligatoire/base/formidableparticipation.php
+++
@@ -1,7 +0,0 @@
-<?php
-if (!defined("_ECRIRE_INC_VERSION")) return;
-
-function formidableparticipation_declarer_tables_auxiliaires($tables) {
-			$table['spip_evenements_participants']['field']['id_formulaires_reponse'] = "bigint(21)";
-	return $tables;
-}
