a19ec78e094b7f4a66096436853f77e3e9df65f2
BoOz <BoOz@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-03-10 18:00:36 +0000

    ajouter une recherche des abonnés dans la page de suivi des abonnements
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@122940 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ abonnements/shelves/BoOz/lang/abonnement_fr.php
@@ -0,0 +1,106 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans https://git.spip.net/spip-contrib-extensions/abonnements.git
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'ajouter_lien_abonnement' => 'Ajouter cet abonnement',
+
+	// C
+	'champ_date_au_label' => 'Au',
+	'champ_date_debut_label' => 'Début de l’abonnement',
+	'champ_date_du_label' => 'Du',
+	'champ_date_echeance_label' => 'Prochaine échéance',
+	'champ_date_fin_allonger_label' => 'Vous pouvez modifier la date de fin',
+	'champ_date_fin_label' => 'Fin de l‘abonnement',
+	'champ_dates_debut_label' => 'Débuts des abonnements',
+	'champ_dates_fin_label' => 'Fin des abonnements',
+	'champ_id_abonnements_offre_label' => 'Offre d’abonnement',
+	'champ_id_auteur_label' => 'Utilisateur',
+	'champ_notifier_statut_label' => 'Status des abonnements',
+
+	// E
+	'erreur_id_abonnements_offre' => 'Vous devez créer un abonnement pour une offre existante.',
+
+	// I
+	'icone_creer_abonnement' => 'Créer un abonnement',
+	'icone_modifier_abonnement' => 'Modifier cet abonnement',
+	'icone_renouveler_abonnement' => 'Renouveler cet abonnement',
+	'info_1_abonnement' => 'Un abonnement',
+	'info_1_abonnement_actif' => 'Un abonnement actif',
+	'info_1_abonnement_inactif' => 'Un abonnement inactif',
+	'info_1_abonnement_notifier' => 'Un abonnement à notifier',
+	'info_abonnements_auteur' => 'Les abonnements de cet auteur',
+	'info_aucun_abonnement' => 'Aucun abonnement',
+	'info_aucun_abonnement_actif' => 'Aucun abonnement actif',
+	'info_aucun_abonnement_inactif' => 'Aucun abonnement inactif',
+	'info_aucun_abonnement_notifier' => 'Aucun abonnement à notifier',
+	'info_date_fin' => 'fin le @date@',
+	'info_nb_abonnements' => '@nb@ abonnements',
+	'info_nb_abonnements_actifs' => '@nb@ abonnements actifs',
+	'info_nb_abonnements_inactifs' => '@nb@ abonnements inactifs',
+	'info_nb_abonnements_notifier' => '@nb@ abonnements à notifier',
+	'info_numero_abbr' => 'n°',
+	'info_numero_abbr_maj' => 'N°',
+
+	// J
+	'job_desactivation' => 'Désactivation de l’abonnement @id@',
+
+	// L
+	'label_date_a_partir' => 'À partir du',
+	'label_date_depuis' => 'Depuis le',
+	'label_dates' => 'Dates',
+	'label_duree' => 'Durée',
+	'label_montant' => 'Montant',
+	'label_statut' => 'Statut',
+
+	// N
+	'notification_echeance_chapo' => '<p>Bonjour @nom@,</p>',
+	'notification_echeance_corps' => '<p>Bonjour @nom@,</p>
+		<p>Vous recevez ce courriel car vous avez un abonnement au site @nom_site_spip@ avec l’offre "@offre@".</p>
+		<p>Cet abonnement arrive à échéance dans : <strong>@echeance@</strong>.<br/>
+		Nous vous invitons donc à le renouveler avant son expiration.</p>
+		<p>Merci de votre confiance, et n’hésitez pas à nous contacter pour toute information complémentaire.</p>',
+	'notification_echeance_corps_apres' => '<p>Vous recevez ce courriel car vous aviez un abonnement au site @nom_site_spip@ avec l’offre « @offre@ ».</p>
+	<p>Cet abonnement est arrivé à échéance il y a : <strong>@echeance@</strong>.<br/>
+	Nous vous invitons donc à le renouveler.</p>',
+	'notification_echeance_corps_avant' => '<p>Vous recevez ce courriel car vous avez un abonnement au site @nom_site_spip@ avec l’offre « @offre@ ».</p>
+	<p>Cet abonnement arrive à échéance dans : <strong>@echeance@</strong>.<br/>
+	Nous vous invitons donc à le renouveler avant son expiration.</p>',
+	'notification_echeance_corps_pendant' => '<p>Vous recevez ce courriel car vous êtes abonné.e au site @nom_site_spip@ avec l’offre « @offre@ ».</p>
+	<p>Cet abonnement arrive à échéance aujourd’hui.<br/>
+	Nous vous invitons donc à le renouveler avant son expiration.</p>',
+	'notification_echeance_signature' => '<p>Merci de votre confiance, et n’hésitez pas à nous contacter pour toute information complémentaire.</p>',
+	'notification_echeance_sujet_jours_apres' => 'Votre abonnement est terminé depuis @duree@ jour(s) !',
+	'notification_echeance_sujet_jours_avant' => 'Votre abonnement se termine dans @duree@ jour(s) !',
+	'notification_echeance_sujet_jours_pendant' => 'Votre abonnement se termine aujourd’hui !',
+	'notification_echeance_sujet_mois_apres' => 'Votre abonnement est terminé depuis @duree@ mois !',
+	'notification_echeance_sujet_mois_avant' => 'Votre abonnement se termine dans @duree@ mois !',
+	'notification_echeance_sujet_mois_pendant' => 'Votre abonnement se termine ce mois-ci !',
+
+	// R
+	'retirer_lien_abonnement' => 'Retirer cet abonnement',
+	'retirer_tous_liens_abonnements' => 'Retirer tous les abonnements',
+
+	// S
+	'statut_actif' => 'actif',
+	'statut_actifs' => 'actifs',
+	'statut_inactif' => 'désactivé',
+	'statut_inactifs' => 'désactivés',
+	'statut_tous' => 'tous',
+
+	// T
+	'texte_ajouter_abonnement' => 'Ajouter un abonnement',
+	'texte_changer_statut_abonnement' => 'Cet abonnement est :',
+	'texte_creer_associer_abonnement' => 'Créer et associer un abonnement',
+	'titre_abonnement' => 'Abonnement',
+	'titre_abonnements' => 'Abonnements',
+	'titre_abonnements_rubrique' => 'Abonnements de la rubrique',
+	'titre_abonnements_suivre' => 'Suivre les abonnements',
+	'titre_langue_abonnement' => 'Langue de cet abonnement',
+	'titre_logo_abonnement' => 'Logo de cet abonnement'
+);
---
+++ abonnements/shelves/BoOz/lang/abonnementsoffre_en.php
@@ -0,0 +1,74 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/abonnementsoffre?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'ajouter_lien_abonnementsoffre' => 'Add this subscription offer',
+
+	// C
+	'champ_descriptif_label' => 'Description',
+	'champ_duree_0' => 'No limit',
+	'champ_duree_label' => 'Duration',
+	'champ_periode_choix_heures' => 'Hours',
+	'champ_periode_choix_jours' => 'Days',
+	'champ_periode_choix_mois' => 'Month',
+	'champ_periode_label' => 'Period type',
+	'champ_periode_nb_heures' => '@nb@ hour(s)',
+	'champ_periode_nb_jours' => '@nb@ day(s)',
+	'champ_periode_nb_mois' => '@nb@ months',
+	'champ_prix_0' => 'Free',
+	'champ_prix_ht_label' => 'Price excl. tax',
+	'champ_prix_label' => 'Price',
+	'champ_prix_ttc_label' => 'Price incl. tax',
+	'champ_quand_choix_apres' => 'After',
+	'champ_quand_choix_avant' => 'Before',
+	'champ_quand_choix_pendant' => 'The same day',
+	'champ_quand_label' => 'When ?',
+	'champ_taxe_label' => 'VAT (%)',
+	'champ_titre_label' => 'Title',
+	'configurer_notifications' => 'Configure the notifications',
+	'configurer_notifications_quand' => 'When ?',
+
+	// E
+	'erreur_notification_doublon' => 'This notification is already registered',
+
+	// I
+	'icone_creer_abonnementsoffre' => 'Create a subscription offer',
+	'icone_modifier_abonnementsoffre' => 'Modify this subscription offer',
+	'info_1_abonnementsoffre' => 'A subscription offer',
+	'info_1_jours_apres' => '1 day after',
+	'info_1_jours_avant' => '1 day before',
+	'info_1_mois' => '1 month',
+	'info_1_mois_apres' => '1 month after',
+	'info_1_mois_avant' => '1 month before',
+	'info_abonnementsoffres_auteur' => 'Subscription offers of this author',
+	'info_aucun_abonnementsoffre' => 'No subscription offer',
+	'info_aucune_notification' => 'No notification',
+	'info_nb_abonnementsoffres' => '@nb@ subscription offers',
+	'info_nb_jours_apres' => '@nb@ days after',
+	'info_nb_jours_avant' => '@nb@ days before',
+	'info_nb_mois' => '@nb@ months',
+	'info_nb_mois_apres' => '@nb@ month after',
+	'info_nb_mois_avant' => '@nb@ month before',
+
+	// R
+	'retirer_lien_abonnementsoffre' => 'Remove this subscription offer',
+	'retirer_tous_liens_abonnementsoffres' => 'Remove all subscription offers',
+
+	// T
+	'texte_ajouter_abonnementsoffre' => 'Add a subscription offer',
+	'texte_changer_statut_abonnementsoffre' => 'This subscription offer is:',
+	'texte_creer_associer_abonnementsoffre' => 'Create and associate a subscription offer',
+	'titre_abonnementsoffre' => 'Subscription offer',
+	'titre_abonnementsoffres' => 'Subscription offers',
+	'titre_abonnementsoffres_rubrique' => 'Subscription offers of the section',
+	'titre_langue_abonnementsoffre' => 'Language in this subscription offer',
+	'titre_logo_abonnementsoffre' => 'Logo of this subscription offer'
+);
