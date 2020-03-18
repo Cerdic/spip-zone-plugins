cbe013979f29a61c93d1fa0e9e622216ef8f8632
Matthieu Marcillaud <Matthieu Marcillaud@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-03-18 15:17:45 +0000

    Varicelle v1 devrait passer le stade SPIP 3.3 aussi avec un paquet.xml
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123001 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ varicelle/branches/v1/lang/paquet-varicelle_fr.php
@@ -0,0 +1,8 @@
+<?php
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	'varicelle_description' => 'De jolis boutons : séduisants, légers et performants, pour styler tous les boutons du site. S’applique aux boutons des formulaires SPIP et à tout ce qui porte le sélecteur <code>.submit</code>',
+	'varicelle_slogan' => 'De jolis bouton !',
+);
