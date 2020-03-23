15eae60986c4cc005df416cca88eb97305f3f287
Salvatore <Salvatore@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-03-23 00:27:01 +0000

    [Salvatore] [source:lang/ abonnementsoffre] Export depuis https://trad.spip.net de la langue fr [Salvatore] [source:lang/ abonnementsoffre] Mise a jour du bilan depuis https://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123045 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- commandes_abonnements/trunk/lang/abonnementsoffre_fr.php
+++ commandes_abonnements/trunk/lang/abonnementsoffre_fr.php
@@ -1,5 +1,6 @@
 <?php
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans https://git.spip.net/spip-contrib-extensions/commandes_abonnements.git
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
@@ -15,17 +16,17 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'champ_montant_perso_label_case' => 'Activer la personnalisation du montant',
 	'champ_renouvellement_auto_label' => 'Renouvellement',
 	'champ_renouvellement_auto_label_case' => 'Activer le renouvellement automatique',
-	
+
 	// E
 	'explication_renouvellement' => 'Si vous avez déjà un abonnement actif à cette offre, en vous connectant, celui-ci sera prolongé à partir de sa date de fin.',
-	
+
 	// I
-	'info_paiement_unique' => 'Unique',
-	'info_paiement_renouvelable' => 'Prélèvement SEPA',
-	'info_renouvellement_auto' => 'Renouvellement automatique',
-	'info_dates_from_to' => 'Du @date_debut@ au @date_fin@',
 	'info_dates_from' => 'À partir du @date_debut@',
+	'info_dates_from_to' => 'Du @date_debut@ au @date_fin@',
 	'info_duree' => 'Durée',
 	'info_paiement' => 'Paiement',
 	'info_paiement_auto' => 'Périodicité du prélèvement',
+	'info_paiement_renouvelable' => 'Prélèvement SEPA',
+	'info_paiement_unique' => 'Unique',
+	'info_renouvellement_auto' => 'Renouvellement automatique'
 );
