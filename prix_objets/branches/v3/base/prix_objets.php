f364d23877feb8626451a5150fba95c82603ab41
Rainer Müller <Rainer Mller@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-03-24 17:12:43 +0000

    Branch '/branches/v3' created from /trunk:123086
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123087 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ prix_objets/branches/v3/base/prix_objets.php
@@ -0,0 +1,122 @@
+<?php
+/**
+ * Déclarations relatives à la base de données
+ *
+ * @plugin     Prix Objets
+ * @copyright  2012 - 2020
+ * @author     Rainer Müller
+ * @licence    GNU/GPL
+ * @package    SPIP\Promotions_commandes\Pipelines
+ */
+if (! defined("_ECRIRE_INC_VERSION"))
+	return;
+
+/**
+ * Déclaration des alias de tables et filtres automatiques de champs
+ *
+ * @pipeline declarer_tables_interfaces
+ * @param array $interfaces
+ *     Déclarations d'interface pour le compilateur
+ * @return array
+ *     Déclarations d'interface pour le compilateur
+ */
+function prix_objets_declarer_tables_interfaces($tables_interfaces) {
+	$tables_interfaces['table_des_tables']['prix_objets'] = 'prix_objets';
+
+	return $tables_interfaces;
+}
+
+/**
+ * Déclaration des tables principales.
+ *
+ * @pipeline declarer_tables_interfaces
+ * @param array $tables_principales
+ *     Déclarations des tables principales pour le compilateur
+ * @return array
+ *     Déclarations des tables principales pour le compilateur
+ */
+function prix_objets_declarer_tables_principales($tables_principales) {
+	$spip_prix_objets = array(
+		"id_prix_objet" => "bigint(21) NOT NULL",
+		"id_prix_objet_source" => "bigint(21) NOT NULL",
+		'objet' => 'varchar(25) not null default ""',
+		"id_objet" => "bigint(21) NOT NULL",
+		"titre" => "varchar(255)  DEFAULT '' NOT NULL",
+		"code_devise" => "varchar(3) NOT NULL",
+		"prix_ht" => "decimal(15,2) NOT NULL DEFAULT '0.00'",
+		"prix" => "decimal(15,2) NOT NULL DEFAULT '0.00'",
+		"prix_total" => "int(1) NOT NULL DEFAULT '0'",
+		"taxe" => "varchar(10)  DEFAULT '' NOT NULL",
+		'extension' => 'varchar(50) not null default ""',
+		"id_extension" => "bigint(21) NOT NULL",
+		"rang_lien" => "int(4) NOT NULL DEFAULT '0'",
+	);
+
+	$spip_prix_objets_key = array(
+		"PRIMARY KEY" => "id_prix_objet",
+		"KEY id_objet" => "id_prix_objet_source,id_objet,objet,id_extension,extension"
+	);
+
+	$spip_prix_objets_join = array(
+		"id_prix_objet" => "id_prix_objet",
+		"id_objet" => "id_objet",
+		"id_objet" => "id_article"
+	);
+
+	$tables_principales['spip_prix_objets'] = array(
+		'field' => &$spip_prix_objets,
+		'key' => &$spip_prix_objets_key,
+		'join' => &$spip_prix_objets_join
+	);
+
+	return $tables_principales;
+}
+
+/**
+ * Actualise la bd
+ *
+ * @param string $version_cible
+ *        	la version de la bd
+ */
+function po_upgrade($version_cible) {
+
+	// Remplace les champs "id_EXTENSION" par id_extension extension.
+	if ($version_cible == '2.0.0') {
+		$trouver_table = charger_fonction('trouver_table', 'base');
+		$table = 'spip_prix_objets';
+		$decription_table = $trouver_table($table);
+		include_spip('inc/prix_objets');
+
+		$extensions = array(
+			'declinaison',
+			'po_periode'
+		);
+
+		foreach ($extensions as $extension) {
+			if ($identifiant_extension = id_table_objet($extension) and
+				isset($decription_table['field'][$identifiant_extension])) {
+					$sql = sql_select('*', 'spip_prix_objets',
+						$identifiant_extension . '>0');
+
+					while ($data = sql_fetch($sql)) {
+						sql_insertq('spip_prix_objets',
+							array(
+								'id_prix_objet_source' => $data['id_prix_objet'],
+								'extension' => $extension,
+								'id_extension' => $data[$identifiant_extension],
+								'objet' => $data['objet'],
+								'id_objet' => $data['id_objet'],
+								'titre' => extraire_multi(
+									supprimer_numero(
+										generer_info_entite(
+											$data[$identifiant_extension],
+											$extension,
+											'titre', '*'))),
+								'prix' => $data['prix_ht']
+							));
+					}
+					sql_alter("TABLE $table DROP COLUMN  $identifiant_extension");
+				}
+		}
+	}
+}
---
+++ prix_objets/branches/v3/prix_objets_pipelines.php
@@ -0,0 +1,130 @@
+<?php
+/**
+ * Utilisations de pipelines par Prix Objets
+ *
+ * @plugin     Prix Objets
+ * @copyright  2012 - 2020
+ * @author     Rainer Müller
+ * @licence    GNU/GPL
+ * @package    SPIP\Prix_objets\Pipelines
+ */
+if (!defined("_ECRIRE_INC_VERSION"))
+	return;
+
+	/**
+	 * Ajouter du contenu sur les pages exec/ de SPIP, après le contenu prévu au centre de la page.
+	 *
+	 * @pipeline affiche_milieu
+	 *
+	 * @param array $flux
+	 * @return array
+	 */
+function prix_objets_affiche_milieu($flux) {
+	// affichage du formulaire d'activation désactivation projets
+	include_spip('inc/config');
+	$objets = lire_config('prix_objets/objets_prix', array());
+	$e = trouver_objet_exec($flux['args']['exec']);
+	$type = $e['type'];
+	$id_table_objet = $e['id_table_objet'];
+	$id = intval($flux['args'][$id_table_objet]);
+	if (in_array($type, $objets)) {
+		if ($type == 'article') {
+			$id_article = $flux['args']['id_article'];
+			$rubriques_produits = rubrique_prix($id_article);
+			if (($rubriques_produits and $id_article) or (!$rubriques_produits)) {
+				$contexte = array(
+					'id_objet' => $id_article,
+					'objet' => 'article'
+				);
+				$contenu = recuperer_fond('prive/objets/editer/prix', $contexte, array(
+					'ajax' => true
+				));
+				if ($p = strpos($flux['data'], "<!--affiche_milieu-->"))
+					$flux['data'] = substr_replace($flux['data'], $contenu, $p, 0);
+				else
+					$flux['data'] .= $contenu;
+			}
+		}
+		elseif ($id) {
+			$contexte = array(
+				'id_objet' => $id,
+				'objet' => $type
+			);
+			$contenu = recuperer_fond('prive/objets/editer/prix', $contexte, array(
+				'ajax' => true
+			));
+			if ($p = strpos($flux['data'], "<!--affiche_milieu-->"))
+				$flux['data'] = substr_replace($flux['data'], $contenu, $p, 0);
+			else
+				$flux['data'] .= $contenu;
+		}
+	}
+	return $flux;
+}
+
+/**
+ * Declare l'object pour le Plugin shop https://github.com/abelass/shop.
+ *
+ * @pipeline shop_objets
+ *
+ * @param array $flux
+ * @return array
+ */
+ function prix_objets_shop_objets($flux) {
+	$flux['data']['prix_objets'] = array(
+		'action' => 'prix_objets',
+		'nom_action' => _T('prix_objets:prix_objets_titre'),
+		'icone' => 'prix_objets-16.png',
+		'configurer' => array(
+			'titre' => _T('prix_objets:titre_prix_objets'),
+			'chemin' => 'prive/squelettes/contenu/configurer_prix_objets'
+		)
+	);
+
+	return $flux;
+}
+
+/**
+ * Ajouter les configurations dans celle de réservation événements.
+ *
+ * @pipeline reservation_evenement_objets_configuration
+ *
+ * @param array $flux
+ *        	Données du pipeline
+ * @return array Données du pipeline
+ */
+function prix_objets_reservation_evenement_objets_configuration($flux) {
+
+	$flux['data']['prix_objets'] = array(
+		'label' => _T('paquet-prix_objets:prix_objets_nom'),
+	);
+
+	return $flux;
+}
+
+/**
+ * Ajouter des contenus dans la partie <head> des pages de l’espace privé.
+ *
+ * @pipeline header_prive
+ *
+ * @param array $flux
+ * @return array
+ */
+function prix_objets_header_prive($flux){
+	$flux .= '<link rel="stylesheet" href="' . _DIR_PLUGIN_PRIX_OBJETS .'css/prix_objets_admin.css" type="text/css" media="all" />';
+	return $flux;
+}
+
+/**
+ * Active des modules de jquery ui
+ *
+ * @pipeline jqueryui_plugins
+ *
+ * @param array $scripts
+ *        	Données du pipeline
+ * @return array
+ */
+function prix_objets_jqueryui_plugins($scripts) {
+	$scripts[] = "jquery.ui.sortable";
+	return $scripts;
+}
