764d3b26882f98a9848e9b5b4cbda6ae49ceeb79
Salvatore <Salvatore@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-03-24 00:27:21 +0000

    [Salvatore] [source:lang/ newsletter] Export depuis https://trad.spip.net de la langue de [Salvatore] [source:lang/ newsletter] Mise a jour du bilan depuis https://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123067 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- mailsubscribers/trunk/lang/newsletter_de.php
+++ mailsubscribers/trunk/lang/newsletter_de.php
@@ -11,16 +11,23 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// B
 	'bouton_subscribe' => 'Newsletter bestellen',
+	'bouton_subscribe_multiples' => 'Mich für diese Newsletter anmelden',
 	'bouton_unsubscribe' => 'Newsletter abbestellen',
 	'bouton_unsubscribe_all' => 'ALLEN Newslettern Abbestellen',
+	'bouton_unsubscribe_multiples' => 'Mich von diesen Newslettern abmelden',
 
 	// L
 	'label_email_subscribe' => 'Email',
 	'label_email_unsubscribe' => 'Email',
 	'label_link_unsubscribe' => 'Diesen Newsletter abbestellen',
+	'label_subscribe_lists' => 'Anmeldung für die Listen',
+	'label_unsubscribe_lists' => 'Abmeldung von den Listen',
 
 	// S
 	'subscribe_message_ok' => 'Ihre Anmeldung unter der Adresse @email@ wird bearbeitet.',
+	'subscribe_message_ok_confirm' => 'Sie erhalten eine E-Mail an @email@ um ihre Anmeldung zu bestätigen.',
+	'subscribe_message_ok_confirm_invite_pluriel' => 'Eine Einladungs-E-Mail wurde an folgende Adressen versandt @email@.',
+	'subscribe_message_ok_confirm_invite_singulier' => 'Eine Einladungs-E-Mail an @email@ wurde versandt.',
 
 	// U
 	'unsubscribe_message_ok' => 'Die Adresse @email@ wurde aus der Empfängerliste gestrichen.'
