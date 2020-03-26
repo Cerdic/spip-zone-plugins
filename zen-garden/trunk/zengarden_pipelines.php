a9a98fa305af33f57491df10dff5e96a0d06b711
Cerdic <Cerdic@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-03-26 10:43:34 +0000

    encore un peu de mise en forme, amelioration de l'ergo et code mort
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123146 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- zen-garden/trunk/zengarden_pipelines.php
+++ zen-garden/trunk/zengarden_pipelines.php
@@ -23,10 +23,6 @@ if (!defined('_ECRIRE_INC_VERSION')) {
  * 		Le code html du head complété
  */
 function zengarden_header_prive($flux){
-	if (_request('exec') == 'zengarden')
-		$flux .='
-		<script src="'.find_in_path('javascript/jquery.qtip-1.0.0-rc3.js').'" type="text/javascript"></script>
-		<script src="'.find_in_path('javascript/jquery.qtip.activate.js').'" type="text/javascript"></script>';
 
 	return $flux;
 }
