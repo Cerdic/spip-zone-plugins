d329b55c64fba31a434440d8165ac7e2329d6aa5
Salvatore <Salvatore@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-03-26 10:20:02 +0000

    [Salvatore] [source:lang/ configureragenda] Export depuis https://trad.spip.net de la langue nl [Salvatore] [source:lang/ configureragenda] Mise a jour du bilan depuis https://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123142 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- agenda/trunk/lang/configureragenda_nl.php
+++ agenda/trunk/lang/configureragenda_nl.php
@@ -9,6 +9,12 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
+	// A
+	'avertissement_effacer_evenements' => 'Let op: deze optie verwijdert de gegevens in de database definitief',
+
+	// E
+	'explication_timezone_defaut' => 'De standaard tijdzone (@timezone@) wordt gebruikt bij het opslaan van data in de database',
+
 	// L
 	'label_affichage_debut' => 'Begin van de lijst',
 	'label_affichage_debut_date_jour' => 'Huidige datum',
@@ -21,10 +27,13 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'label_affichage_debut_mois_passe' => '@mois@ vooraf',
 	'label_affichage_duree' => 'De evenementen opsommen op',
 	'label_articlepardefaut' => 'Standaardartikel (id)',
+	'label_delai_effacer_evenements_passes' => 'Verwijderen na (dagen)',
 	'label_descriptif' => 'Omschrijving',
 	'label_effacer_evenements_passes' => 'Automatisch voorbije evenementen wissen',
+	'label_fuseaux_horaires_1' => 'Houd bij de evenementen rekening met de tijdzone',
 	'label_insert_head_css_1' => 'Automatisch de standaardstijlen van de agenda invoegen',
-	'label_synchro_statut_1' => 'De status van evenementen automatisch synchroniseren met die van de artikelen', # MODIF
+	'label_notifier_insitituer_1' => 'Informeer beheerders over het voorstellen en publiceren van evenementen',
+	'label_synchro_statut_1' => 'Evenementen worden automatisch gepubliceerd/verwijderd met het gekoppelde artikelen',
 	'label_titre' => 'Paginatitel',
 	'label_url_evenement' => 'Weergave van een evenement',
 	'label_url_evenement_article' => 'op de pagina van het bijbehorende artikel',
@@ -35,6 +44,6 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'legend_presentation_agenda_public' => 'Presentatie van de agenda op de publieke site',
 
 	// T
-	'titre_affichage_agenda_public' => 'Weergave van de Agenda', # MODIF
+	'titre_affichage_agenda_public' => 'Weergave van de Agenda op de publieke site',
 	'titre_configuration' => 'Configuratie van de Agenda'
 );
