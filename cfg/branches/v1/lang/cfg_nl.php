341747d66401cd054ef02e1983b24b7cbd28b2a2
Salvatore <Salvatore@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-03-26 10:15:03 +0000

    [Salvatore] [source:lang/ cfg] Export depuis https://trad.spip.net de la langue nl [Salvatore] [source:lang/ cfg] Mise a jour du bilan depuis https://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123141 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- cfg/branches/v1/lang/cfg_nl.php
+++ cfg/branches/v1/lang/cfg_nl.php
@@ -18,33 +18,33 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'cfg' => 'CFG',
 	'choisir_module_a_configurer' => 'Kiest de te configureren module.',
 	'config_enregistree' => 'Uitgevoerde registratie van <b>@nom@</b>',
-	'config_supprimee' => 'Afschaffing van <b>@nom@</b> uitgevoerd ',
+	'config_supprimee' => 'Verwijdering van <b>@nom@</b> uitgevoerd ',
 	'configuration_modules' => 'Configuratie van de modules',
 
 	// E
-	'erreur_copie_fichier' => 'Onmogelijk om het bestand @fichier@ aan zijn definitieve plaats tekopiëren',
-	'erreur_enregistrement' => 'Een fout is bij de registratie van voorgekomen <b>@nom@</b>',
+	'erreur_copie_fichier' => 'Onmogelijk om het bestand @fichier@ naar zijn definitieve plaats te kopiëren',
+	'erreur_enregistrement' => 'Er is een fout opgetreden bij de registratie van <b>@nom@</b>',
 	'erreur_lecture' => 'Onmogelijk om te lezen @nom@',
-	'erreur_open_w_fichier' => 'Onmogelijk om het bestand @fichier@ in schrift te openen',
-	'erreur_suppression' => 'Een fout is bij de afschaffing van voorgekomen <b>@nom@</b>',
-	'erreur_suppression_fichier' => 'Onmogelijk om het bestand af te schaffen @fichier@.',
-	'erreur_type_id' => 'Het veld @champ@ moet met een brief of een beginnen onderstreept',
+	'erreur_open_w_fichier' => 'Onmogelijk om het bestand @fichier@ beschrijfbaar te openen',
+	'erreur_suppression' => 'Er is een fout opgetreden bij het verwijderen van <b>@nom@</b>',
+	'erreur_suppression_fichier' => 'Het bestand @fichier@ kan niet worden verwijderd.',
+	'erreur_type_id' => 'Het veldnaam @champ@ moet met een letter of een liggend streepje beginnen',
 	'erreur_type_idnum' => 'Het veld @champ@ moet numeriek zijn',
-	'erreur_type_pwd' => 'Het veld @champ@ moet meer dan 5 karakters hebben',
+	'erreur_type_pwd' => 'Het veldnaam @champ@ moet meer dan 5 tekens hebben',
 
 	// I
 	'id_manquant' => 'Ontbrekende identificatie',
-	'installation_librairies' => 'Download van boekhandels',
-	'installation_liste_libs' => 'Lijst van de boekhandels',
-	'installer_dossier_lib' => 'U moet een dossier @dir@ aan de wortel van uw SPIP creëren dat toestemmingen van schrift heeft.',
-	'installer_lib_192' => 'Om een boekhandel te plaatsen, décompressez manueel zip en kopiëert zijn inhoud in het dossier @dir@.',
+	'installation_librairies' => 'Download van bibliotheken',
+	'installation_liste_libs' => 'Lijst van de bibliotheken',
+	'installer_dossier_lib' => 'Je moet een map @dir@ met schrijfrechten bovenin de structuur van SPIP creëren.',
+	'installer_lib_192' => 'Om een bibliotheek te plaatsen, pak je het zip-bestand uit en je kopieert de inhoud in map @dir@.',
 
 	// L
 	'label_activer' => 'Activeren',
-	'label_obligatoire' => 'Verplicht beslag',
+	'label_obligatoire' => 'Verplichte invoer',
 
 	// N
-	'nom_table_manquant' => 'Ontbrekende naam van de tafel SQL',
+	'nom_table_manquant' => 'De naam van de SQL-tabel ontbreekt',
 	'nouveau' => 'Nieuw',
 
 	// O
@@ -55,10 +55,10 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'pas_de_changement' => 'Geen verandering in <b>@nom@</b>',
 
 	// R
-	'refus_configuration_administrateur' => 'Enkel de beheerders van de plaats worden gemachtigd om deze parameters te wijzigen.',
+	'refus_configuration_administrateur' => 'Enkel de beheerders van de site zijn gemachtigd om deze parameters te wijzigen.',
 	'refus_configuration_webmestre' => 'Enkel webmestres van de plaats worden toegestaan om deze parameters te wijzigen.',
 	'reset' => 'Uitwissen',
 
 	// S
-	'supprimer' => 'Vervang de standaardwaarden'
+	'supprimer' => 'Zet de standaardwaarden terug'
 );
