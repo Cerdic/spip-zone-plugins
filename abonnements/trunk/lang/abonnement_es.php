3dd684e73b379764a27d9fcf2af01254e246062f
Salvatore <Salvatore@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-03-27 16:10:03 +0000

    [Salvatore] [source:lang/ abonnement] Export depuis https://trad.spip.net de la langue es [Salvatore] [source:lang/ abonnement] Mise a jour du bilan depuis https://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123171 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- abonnements/trunk/lang/abonnement_es.php
+++ abonnements/trunk/lang/abonnement_es.php
@@ -13,12 +13,17 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'ajouter_lien_abonnement' => 'Añadir esta inscripción',
 
 	// C
+	'champ_date_au_label' => 'Al',
 	'champ_date_debut_label' => 'Principio de la suscripción',
+	'champ_date_du_label' => 'Desde',
 	'champ_date_echeance_label' => 'Próxima instancia',
 	'champ_date_fin_allonger_label' => 'Usted puede modificar la fecha de vencimiento',
 	'champ_date_fin_label' => 'Vencimiento',
+	'champ_dates_debut_label' => 'Comienzo de la suscripción',
+	'champ_dates_fin_label' => 'Fin de la suscripción',
 	'champ_id_abonnements_offre_label' => 'Oferta de iscripción',
 	'champ_id_auteur_label' => 'Usuario',
+	'champ_notifier_statut_label' => 'Estatus de la suscripción',
 
 	// E
 	'erreur_id_abonnements_offre' => 'Usted debe crear una inscripción para una oferta existente  ',
@@ -27,16 +32,20 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'icone_creer_abonnement' => 'Crear  una inscripción',
 	'icone_modifier_abonnement' => 'Modificar esta inscripción',
 	'icone_renouveler_abonnement' => 'Renovar esta inscripción',
-	'info_1_abonnement' => 'Una inscripción', # MODIF
-	'info_1_abonnement_actif' => 'Una inscripción activa', # MODIF
-	'info_1_abonnement_inactif' => 'Una inscripción inactiva', # MODIF
+	'info_1_abonnement' => 'Una inscripción',
+	'info_1_abonnement_actif' => 'Una inscripción activa',
+	'info_1_abonnement_inactif' => 'Una inscripción inactiva',
+	'info_1_abonnement_notifier' => '1 suscripción a notificar',
 	'info_abonnements_auteur' => 'Las inscripciones de este autor',
 	'info_aucun_abonnement' => 'Ninguna inscripción',
 	'info_aucun_abonnement_actif' => 'Ninguna inscripción activa',
 	'info_aucun_abonnement_inactif' => 'Ninguna inscripción inactiva',
+	'info_aucun_abonnement_notifier' => 'Ninguina suscripción a notificar',
+	'info_date_fin' => 'Termina el @date@',
 	'info_nb_abonnements' => '@nb@  inscripciones',
 	'info_nb_abonnements_actifs' => '@nb@  inscripciones activas',
 	'info_nb_abonnements_inactifs' => '@nb@ inscripciones inactivas',
+	'info_nb_abonnements_notifier' => '@nb@ suscripción a notificar',
 	'info_numero_abbr' => 'n°',
 	'info_numero_abbr_maj' => 'N°',
 
@@ -44,15 +53,32 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'job_desactivation' => 'Desactivación de la inscripción @id@',
 
 	// L
+	'label_date_a_partir' => 'A partir del',
+	'label_date_depuis' => 'Desde el',
+	'label_dates' => 'Fechas',
+	'label_duree' => 'Duración',
+	'label_montant' => 'Monto',
 	'label_statut' => 'Estatus',
 
 	// N
+	'notification_echeance_chapo' => '<p>Buenos días @nom@,</p>',
 	'notification_echeance_corps' => '<p>Buenos días @nom@,</p>
 		<p>Usted recibe este correo porque usted está inscrito en el sitio @nom_site_spip@ con la oferta "@offre@".</p>
 		<p>Su inscripción  vence dentro de : 
 <strong>@echeance@</strong>.<br/>
 		Le invitamos a renovarla antes de que expire.</p>
-		<p>Gracias por su confianza, y no dude en contactarnos para cualquier información complementaria.</p>', # MODIF
+		<p>Gracias por su confianza, y no dude en contactarnos para cualquier información complementaria.</p>',
+	'notification_echeance_corps_apres' => '<p>Usted recibe este mail  por su suscripción al sitio @nom_site_spip@ con el modo « @offre@ ».</p>
+ <p>Esta suscripción llegó a su término el: <strong>@echeance@</strong>.<br/>
+ le invitamos a renovarla antes que expire.</p>',
+	'notification_echeance_corps_pendant' => '<p>usted recibe este mail por su suscripción al sitio @nom_site_spip@ con el modo « @offre@ ».</p> <p>Esta suscripción llegó a su término hoy le invitamos a renovarla antes que expire.</p>',
+	'notification_echeance_signature' => '<p>Gracias por su confianza y no dude en contactarnos para cualinformación complementaria.<p>',
+	'notification_echeance_sujet_jours_apres' => 'Su suscripción terminñó desde @duree@ día(s)) ',
+	'notification_echeance_sujet_jours_avant' => 'Su suscripción se termjna en @duree@ día(s)) ',
+	'notification_echeance_sujet_jours_pendant' => 'Su suscripción se termina hoy',
+	'notification_echeance_sujet_mois_apres' => 'Su suscripción terminó desde @duree@ mes',
+	'notification_echeance_sujet_mois_avant' => 'Su suscripción termina en @duree@ meses',
+	'notification_echeance_sujet_mois_pendant' => 'Su suscripción termina este mes',
 
 	// R
 	'retirer_lien_abonnement' => 'Retirar esta inscripción',
@@ -60,7 +86,10 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// S
 	'statut_actif' => 'activa',
+	'statut_actifs' => 'activos',
 	'statut_inactif' => 'desactivada',
+	'statut_inactifs' => 'desactivados',
+	'statut_tous' => 'Todos',
 
 	// T
 	'texte_ajouter_abonnement' => 'Añadir una inscripción',
