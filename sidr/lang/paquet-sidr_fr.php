e47025396281a74f24c41931e07e117eb8db9ddd
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-03-29 21:14:22 +0000

    [Sidr] Compatible SPIP 3.3, il faut mettre le paquet.xml
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123183 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ sidr/lang/paquet-sidr_fr.php
@@ -0,0 +1,19 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+// Fichier produit par PlugOnet
+// Module: paquet-sidr
+// Langue: fr
+// Date: 29-03-2020 21:04:09
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// S
+	'sidr_description' => 'Menu de côté responsive basé sur le plugin jQuery [Sidr->http://www.berriart.com/sidr/].',
+	'sidr_slogan' => 'Menu de côté responsive',
+);
+?>
