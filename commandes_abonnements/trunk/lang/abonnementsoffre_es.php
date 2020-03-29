dc2280f962cb09612480da5bed01fc037f140cf0
Salvatore <Salvatore@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-03-29 07:35:02 +0000

    [Salvatore] [source:lang/ abonnementsoffre] Export depuis https://trad.spip.net de la langue es [Salvatore] [source:lang/ abonnementsoffre] Mise a jour du bilan depuis https://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123179 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ commandes_abonnements/trunk/lang/abonnementsoffre_es.php
@@ -0,0 +1,34 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/abonnementsoffre-commandes_abonnements?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'bouton_choisir_offre' => 'Escoger esta propuesta',
+
+	// C
+	'champ_montant_minimum_label' => 'Monto mínimo',
+	'champ_montant_perso_label' => 'Monto personalizado',
+	'champ_montant_perso_label_case' => 'Activar la personalización del monto',
+	'champ_renouvellement_auto_label' => 'Renovar',
+	'champ_renouvellement_auto_label_case' => 'Activar la renovación automática',
+
+	// E
+	'explication_renouvellement' => 'Si usted ya tiene una suscripción activa en esta propuesta, la suscripciñon será prolongada a partir de su fecha de expiracioón.',
+
+	// I
+	'info_dates_from' => 'Deste @date_debut@',
+	'info_dates_from_to' => 'Deste @date_debut@ al @date_fin@',
+	'info_duree' => 'Duración',
+	'info_paiement' => 'Pago',
+	'info_paiement_auto' => 'Periodicidad del pago automático',
+	'info_paiement_renouvelable' => 'Pago automático PAT',
+	'info_paiement_unique' => 'Único',
+	'info_renouvellement_auto' => 'Renovación automática'
+);
