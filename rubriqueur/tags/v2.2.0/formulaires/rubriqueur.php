f893281b5378aa4f38a42c7e4318afdd10af352f
nicod_ <nicod_@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-04 16:46:05 +0000

    Tag '/tags/v2.2.0' created from /trunk:122958
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123266 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ rubriqueur/tags/v2.2.0/formulaires/rubriqueur.php
@@ -0,0 +1,167 @@
+<?php
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+include_spip('action/editer_rubrique');
+include_spip('action/editer_article');
+
+define('_RUBRIQUEUR_DEUX_POINTS_SUBSTITUT', '%%DEUXPOINTS%%');
+
+function formulaires_rubriqueur_charger_dist() {
+
+	$langues_utilisees = liste_options_langues('var_lang');
+	$langues = array();
+	foreach ($langues_utilisees as $langue) {
+		$langues[$langue] = spip_ucfirst(html_entity_decode($GLOBALS['codes_langues'][$langue]));
+	}
+
+	return array(
+		'rubrique_racine' => '',
+		'rubriques'       => '',
+		'numeroter'       => '',
+		'creer_articles'  => '',
+		'langue'          => _request('langue') ? _request('langue') : lire_config('langue_site'),
+		'langues'         => $langues,
+	);
+}
+
+function formulaires_rubriqueur_verifier_dist() {
+	$retour = array();
+	if (!_request('rubriques')) {
+		$retour['rubriques'] = _T('champ_obligatoire');
+	}
+	$rubrique_racine = picker_selected(_request('rubrique_racine'), 'rubrique');
+	$rubrique_racine = array_pop($rubrique_racine);
+	if (!autoriser('creerrubriquedans', 'rubrique', $rubrique_racine)) {
+		$retour['message_erreur'] = _T('rubriqueur:pas_autorise');
+	}
+
+	// confirmation intermédiaire
+	if (!$retour && _request('confirmer') != 'on') {
+		$data = _rubriqueur_parse_texte(_request('rubriques'), 'previsu');
+		// les données calculées sont un tableau ? l'analyse yaml a réussi
+		if (is_array($data)) {
+			$previsu = '{{{' . _T('rubriqueur:apercu_import') . '}}}';
+			if ((int)_request('rubrique_racine')) {
+				$previsu .= _T('rubriqueur:dans_la_rubrique') . ' {{' . sql_getfetsel('titre', 'spip_rubriques', 'id_rubrique=' . $rubrique_racine) . '}}';
+			} else {
+				$previsu .= _T('rubriqueur:a_la_racine');
+			}
+			$langue = _request('langue');
+			if(!$langue){
+				$langue = lire_config('langue_site');
+			}
+			$previsu .= _rubriqueur_traiter_rubrique($data, $rubrique_racine, 'previsu', 0, '', $langue, _request('numeroter'), _request('creer_articles'));
+			$retour['previsu'] = $previsu;
+			$retour['message_erreur'] = _T('rubriqueur:confirmer_import');
+		} 
+		// sinon, on retourne le message d'erreur
+		else {
+			$retour['erreur_analyse'] = _T('rubriqueur:erreur_analyse') . "\n\n" . '{{'.$data.'}}';
+			$retour['message_erreur'] = _T('rubriqueur:erreur_analyse');
+		}
+	}
+
+	return $retour;
+}
+
+function formulaires_rubriqueur_traiter_dist() {
+	$rubrique_racine = 0;
+	if(_request('rubrique_racine')) {
+		$rubrique_racine = array_pop(picker_selected(_request('rubrique_racine'), 'rubrique'));
+	}
+	$rubriques = _rubriqueur_parse_texte(_request('rubriques'));
+	$langue = _request('langue');
+	if(!$langue){
+		$langue = lire_config('langue_site');
+	}
+	
+	_rubriqueur_traiter_rubrique($rubriques, $rubrique_racine, 'creer', 0, '', $langue, _request('numeroter'), _request('creer_articles'));
+
+	// mettre à jour les status, id_secteur et profondeur
+	include_spip('inc/rubriques');
+	calculer_rubriques();
+	propager_les_secteurs();
+
+	return array(
+		'message_ok' => _T('rubriqueur:rubriques_creees'),
+		'editable'   => false,
+	);
+}
+
+function _rubriqueur_traiter_rubrique($rubriques, $id_parent = 0, $mode = 'creer', $profondeur = 0, $retour = '', $langue = '', $numeroter = '', $creer_articles = '') {
+	if(!is_array($rubriques)) {
+		return;
+	}
+
+	$index_article = 1;
+	$index_rubrique = 1;
+	static $index_articles_rubriques = array();
+	
+	foreach ($rubriques as $key => $value) {
+		if (!is_numeric($key)) {
+
+			// Créer une rubrique
+			$index_articles_rubriques[$profondeur+1] = 1;
+			$titre = (in_array('rubriques',$numeroter) ? ($index_rubrique * 10) . '. ' : '') . str_replace(_RUBRIQUEUR_DEUX_POINTS_SUBSTITUT, ':', $key);
+			if ($mode == 'creer') {
+				$id_rubrique = rubrique_inserer($id_parent, array('titre' => $titre, 'lang' => $langue, 'langue_choisie' => 'oui'));
+			} else {
+				$id_rubrique = null;
+				$retour .= "\n" . '-' . str_repeat('*', $profondeur) . '* <span class="rubrique">' . $titre . '</span>';
+			}
+			$retour .= _rubriqueur_traiter_rubrique($value, $id_rubrique, $mode, $profondeur + 1, '', $langue, $numeroter, $creer_articles);
+			$index_rubrique++;
+			
+			if($creer_articles) {
+				// Ajouter un article par défaut
+				$index = isset($index_articles_rubriques[$profondeur+1]) ? $index_articles_rubriques[$profondeur+1] : 1;
+				$titre = (in_array('articles',$numeroter) ? ($index * 10) . '. ' : '') . str_replace(_RUBRIQUEUR_DEUX_POINTS_SUBSTITUT, ':', $key);
+				if ($mode == 'creer') {
+					article_inserer($id_rubrique, array('titre' => $titre, 'lang' => $langue, 'langue_choisie' => 'oui', 'statut' => 'publie'));
+				} else {
+					$retour .= "\n" . '-' . str_repeat('*', $profondeur+1) . '* <span class="article">' . $titre . '</span>';
+				}
+			}
+			
+		} else {
+			
+			// Créer un article
+			$titre = (in_array('articles',$numeroter) ? ($index_article * 10) . '. ' : '') . str_replace(_RUBRIQUEUR_DEUX_POINTS_SUBSTITUT, ':', $value);
+			if ($mode == 'creer') {
+				article_inserer($id_parent, array('titre' => $titre, 'lang' => $langue, 'langue_choisie' => 'oui', 'statut' => 'publie'));
+			} else {
+				$retour .= "\n" . '-' . str_repeat('*', $profondeur) . '* <span class="article">' . $titre . '</span>';
+			}
+			$index_article++;
+			$index_articles_rubriques[$profondeur] = $index_article;
+			
+		} 
+	}
+
+	return $retour;
+}
+
+function _rubriqueur_parse_texte($texte, $mode = 'creer', $indentation = '  ') {
+	require_spip('inc/yaml');
+
+	// transformer le texte en YAML pour pouvoir le décoder 
+
+	// remplacer les : des titres par un marqueur qu'on supprimera ensuite
+	$yaml = str_replace(':', _RUBRIQUEUR_DEUX_POINTS_SUBSTITUT, $texte);
+	// ajouter : à la fin de chaque ligne pour indiquer les sous rubriques
+	$yaml = preg_replace('#^(\s*)([^\r\n]+).*$#m', '$1$2:', $yaml);
+	// supprimer les : sur les lignes d'articles (pas d'enfants)
+	$yaml = preg_replace('#^(\s*)(\-\s)([^:\r\n]+)(:).*$#m', '$1- $3', $yaml);
+
+	// retourner un tableau en cas de succès, une chaine en cas d'erreur
+	try {
+		$retour = yaml_decode($yaml);
+	} catch (Exception $e) {
+		$retour = $e->getMessage();
+	}
+
+	return $retour;
+}
---
+++ rubriqueur/tags/v2.2.0/lang/rubriqueur_fr.php
@@ -0,0 +1,64 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'apercu_import'                => 'Voilà les rubriques qui vont être créées :',
+	'a_la_racine'                  => 'À la racine du site',
+
+	// B
+	'bouton_creer'                 => 'Créer les rubriques',
+
+	// C
+	'confirmer_import'             => 'Confirmer l\'import',
+	'creer_articles'               => 'Créer un article (au statut publié) dans chaque rubrique, qui portera le même nom que le rubrique',
+
+	// D
+	'dans_la_rubrique'             => 'Dans la rubrique :',
+
+	// E
+	'erreur_analyse'               => 'Erreur dans l\'analyse des rubriques',
+
+	// L
+	'langue'                       => 'Langue :',
+
+	// N
+	'numeroter'                    => 'Numéroter :',
+	'numeroter_explication'        => 'Numéroter automatiquement de 10 en 10',
+	'numeroter_articles'           => 'les articles',
+	'numeroter_rubriques'          => 'les rubriques',
+
+	// P
+	'pas_autorise'                 => 'Vous n\'êtes pas autorisé•e à modifier cette rubrique',
+
+	// R
+	'rubrique_racine'              => 'Dans la rubrique :',
+	'rubrique_racine_explications' => 'Choisir la rubrique dans laquelle les rubriques ci dessous seront créées, sinon les rubriques seront créées à la racine du site.',
+	'rubriques'                    => 'Rubriques à créer :',
+	'rubriques_creees'             => 'Les rubriques ont été créées',
+	'rubriques_explications'       => 'Une ligne par rubrique
+<br>Deux espaces en début de ligne pour créer une sous rubrique ou un article.
+<br>Les articles sont identifiés par un tiret et un espace devant le titre.
+<br><br>Exemple :
+<pre>Rubrique 1
+  Sous rubrique 1.1
+    - Un article dans la rubrique 1.1
+  Sous rubrique 1.2
+    - Article : un premier
+    - Article : un deuxième
+Rubrique 2
+  Sous rubrique 2.1
+    Sous sous rubrique 2.1.1
+      - Un autre article
+      - Et encore un autre
+    Sous sous rubrique 2.1.2, sans article
+  Sous rubrique 2.2
+  Sous rubrique 2.3</pre>',
+	'rubriqueur_titre'             => 'Créer des rubriques',
+
+);
