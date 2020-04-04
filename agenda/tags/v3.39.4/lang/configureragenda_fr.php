1d4929e150532ea826c61fdb7caad76d1b2123b7
Cerdic <Cerdic@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-04 12:03:20 +0000

    Tag '/tags/v3.39.4' created from /branches/v3:122339
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123260 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ agenda/tags/v3.39.4/lang/configureragenda_fr.php
@@ -0,0 +1,43 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans https://git.spip.net/spip-contrib-extensions/agenda.git
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'avertissement_effacer_evenements' => 'Attention : cette option supprime des données en base de façon irreversible',
+
+	// L
+	'label_affichage_debut' => 'Début de la liste',
+	'label_affichage_debut_date_jour' => 'Date du jour',
+	'label_affichage_debut_date_veille' => 'Date de la veille',
+	'label_affichage_debut_debut_mois' => 'Début du mois',
+	'label_affichage_debut_debut_mois_1' => 'Début d’année',
+	'label_affichage_debut_debut_mois_prec' => 'Début du mois précédent',
+	'label_affichage_debut_debut_semaine' => 'Début de la semaine',
+	'label_affichage_debut_debut_semaine_prec' => 'Début de la semaine précédente',
+	'label_affichage_debut_mois_passe' => '@mois@ précédent',
+	'label_affichage_duree' => 'Lister les événements sur',
+	'label_articlepardefaut' => 'Identifiant article par défaut',
+	'label_delai_effacer_evenements_passes' => 'Délai avant effacement (jours)',
+	'label_descriptif' => 'Descriptif',
+	'label_effacer_evenements_passes' => 'Effacer les évènement passés',
+	'label_insert_head_css_1' => 'Insérer automatiquement les styles par défaut de l’agenda',
+	'label_notifier_insitituer_1' => 'Notifier aux admins les propositions et publications d’événements',
+	'label_synchro_statut_1' => 'Les événements sont automatiquement publiés/dépubliés avec l’article auquel ils sont liés',
+	'label_titre' => 'Titre de la page',
+	'label_url_evenement' => 'Affichage d’un événement',
+	'label_url_evenement_article' => 'sur la page de l’article associé',
+	'label_url_evenement_evenement' => 'sur une page dédiée pour chaque événement',
+	'legend_nettoyage_agenda' => 'Nettoyage régulier de l’agenda',
+	'legend_presentation_agenda' => 'Présentation de l’agenda',
+	'legend_presentation_agenda_prive' => 'Présentation de l’agenda dans l’espace privé',
+	'legend_presentation_agenda_public' => 'Présentation de l’agenda sur le site public',
+
+	// T
+	'titre_affichage_agenda_public' => 'Affichage de l’Agenda sur le site public',
+	'titre_configuration' => 'Configuration de l’Agenda'
+);
