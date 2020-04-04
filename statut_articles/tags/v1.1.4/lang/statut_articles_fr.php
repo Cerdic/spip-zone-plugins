d0a12208f8e108c28b3152a91e6795d647cff4a1
spip.franck@lien-d-amis.net <spip.franck@lien-d-amis.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-04 16:46:05 +0000

    Tag '/tags/v1.1.4' created from /trunk:116095
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123268 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ statut_articles/tags/v1.1.4/lang/statut_articles_fr.php
@@ -0,0 +1,29 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'bouton_changer' => 'Changer le statut de ces articles',
+
+	// C
+	'champ_rubrique' => 'Les articles de la rubrique :',
+	'champ_nouveau_statut' => 'Choisir le nouveau statut',
+	'champ_date_debut' => 'Publiés après : (inclus)',
+	'champ_date_fin' => 'Publiés avant : (inclus)',
+	'champ_statut' => 'Ayant le statut :',
+
+	// M
+	'menu_changer_statuts' => 'Statuts d\'articles',
+
+	// S
+	'statut_articles_modifies' => 'Les statuts des articles ont été modifiés',
+	'statut_articles_titre' => 'Statut articles',
+
+	// T
+	'titre_changer_statuts' => 'Changer des statuts d\'articles',
+);
