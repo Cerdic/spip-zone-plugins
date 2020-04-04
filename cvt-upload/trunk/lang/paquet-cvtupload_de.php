843c0c90f79ac93641215e2398567a8a07db999f
Salvatore <Salvatore@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-04 23:27:13 +0000

    [Salvatore] [source:lang/ paquet-cvtupload] Export depuis https://trad.spip.net de la langue de [Salvatore] [source:lang/ paquet-cvtupload] Mise a jour du bilan depuis https://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123273 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ cvt-upload/trunk/lang/paquet-cvtupload_de.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-cvtupload?lang_cible=de
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cvtupload_description' => 'Werkzeug zum Hochladen per CVT Formular.',
+	'cvtupload_slogan' => 'Dateien per CVT-Formular hochladen'
+);
