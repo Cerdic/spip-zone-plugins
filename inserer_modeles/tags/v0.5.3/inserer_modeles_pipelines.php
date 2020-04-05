9895591054f9f67cb43960a3fccd71f0a8d40536
spip.franck@lien-d-amis.net <spip.franck@lien-d-amis.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-05 16:05:03 +0000

    Tag '/tags/v0.5.3' created from /branches/v_0:113106
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123589 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ inserer_modeles/tags/v0.5.3/inserer_modeles_pipelines.php
@@ -0,0 +1,78 @@
+<?php
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+function inserer_modeles_affiche_droite($flux){
+	if (in_array($flux['args']['exec'],array('articles_edit','breves_edit','rubriques_edit','mots_edit'))) {
+		include_spip('inc/inserer_modeles');
+		if (count(inserer_modeles_lister_formulaires_modeles())>0)
+			$flux['data'] .= recuperer_fond('inclure/inserer_modeles',$flux['args']);
+	}
+	return $flux;
+}
+
+// Insertion dans le porte-plume
+function inserer_modeles_porte_plume_barre_pre_charger($barres) {
+	include_spip('inc/inserer_modeles');
+	$formulaires_modeles = inserer_modeles_lister_formulaires_modeles();
+	
+	if (count($formulaires_modeles)>0) {
+		$barre = &$barres['edition'];
+		$barre->ajouterApres('grpCaracteres', array(
+					"id" => "sepInsMod",
+					"separator" => "---------------",
+					"display"   => true,
+		));
+		
+		$sous_menu = array();
+		foreach ($formulaires_modeles as $nom => $formulaire) {
+			if (substr($nom,-5)=='.yaml')
+				$nom = substr($nom,0,-5);
+			$sous_menu[] = array(
+				"id" => 'inserer_modele_'.$nom,
+				"name" => _T_ou_typo($formulaire['nom']),
+				"className" => 'outil_inserer_modele_'.$nom,
+				"beforeInsert" => "function() {jQuery.modalboxload('".generer_url_public(
+					'inclure/inserer_modeles',
+					"modalbox=oui&formulaire_modele=$nom&id_article='+$(\"[name='id_article']\").val()+'&id_rubrique='+$(\"[name='id_rubrique']\").val()+'&id_breve='+$(\"[name='id_breve']\").val()"
+				).",{minHeight: '90%'});}",
+				"display" => true
+			);
+		}
+		
+		$barre->ajouterApres('sepInsMod', array(
+			//groupe inserer_modeles et bouton inserer_modeles
+			"id"          => 'inserer_modeles',
+			"name"        => _T('inserer_modeles:outil_inserer_modeles'),
+			"key"         => "M",
+			"className"   => 'outil_inserer_modeles',
+			"beforeInsert" => "function() {jQuery.modalboxload('".generer_url_public(
+				'inclure/inserer_modeles',
+				"modalbox=oui&id_article='+$(\"[name='id_article']\").val()+'&id_rubrique='+$(\"[name='id_rubrique']\").val()+'&id_breve='+$(\"[name='id_breve']\").val()"
+			).",{minHeight: '90%'});}",
+			"display"     => true,
+			"dropMenu"    => $sous_menu
+		 ));
+	}
+	
+	return $barres;
+}
+
+// Icônes pour le porte-plume
+
+function inserer_modeles_porte_plume_lien_classe_vers_icone($flux) {
+	include_spip('inc/inserer_modeles');
+	$formulaires_modeles = inserer_modeles_lister_formulaires_modeles();
+	if (count($formulaires_modeles)>0) {
+		$icones = array();
+		$icones['outil_inserer_modeles'] = 'inserer_modeles.png';
+		foreach ($formulaires_modeles as $nom => $formulaire) {
+			if (substr($nom,-5)=='.yaml')
+				$nom = substr($nom,0,-5);
+			$icones['outil_inserer_modele_'.$nom] = $formulaire['icone_barre'];
+		}
+		return array_merge($flux, $icones);
+	} else
+		return $flux;
+}
+
+?>
---
+++ inserer_modeles/tags/v0.5.3/lang/inserer_modeles_fr.php
@@ -0,0 +1,20 @@
+<?php
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	
+	'titre_inserer_modeles' => 'Ins&eacute;rer un mod&egrave;le',
+	'choisir_modele' => 'Que souhaitez-vous ins&eacute;rer&nbsp;?',
+	'bouton_choisir' => 'Choisir',
+	'bouton_inserer' => 'Ins&eacute;rer',
+	'item_center' => 'au centre',
+	'item_left' => '&agrave; gauche',
+	'item_right' => '&agrave; droite',
+	'titre_inserer' => 'Ins&eacute;rer @modele@',
+	'erreur_choix_modele' => 'Vous devez choisir un mod&egrave;le.',
+	'label_alignement' => 'Alignement&nbsp;:',
+	'message_copier_code' => 'Vous pouvez recopier le code ci-dessous dans votre texte ou modifier les param&egrave;tres de votre mod&egrave;le.',
+	'outil_inserer_modeles' => 'Ins&eacute;rer un mod&egrave;le',
+);
+
+?>
