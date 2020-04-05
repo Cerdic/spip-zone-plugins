7bd5eee8445c2df35431a979137b52cc9ff6455b
Salvatore <Salvatore@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-05 16:05:03 +0000

    Tag '/tags/v1.0.0' created from /trunk:122130
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123708 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ modeles_facebook/tags/v1.0.0/lang/fbmodeles_en.php
@@ -0,0 +1,111 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/fbmodeles?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cf_navigation' => 'See [column navigation->@url@]',
+	'cfg_comment_appid' => 'Identifier of an application specific to your website; requires that you create the application before.',
+	'cfg_comment_border_color' => 'Enter a hexadecimal color code WITH the initial sharp.',
+	'cfg_comment_colorscheme' => 'Select here the predetermined profile by modules that will be used for display.',
+	'cfg_comment_font' => 'Here you can select the font to be used to display modules.',
+	'cfg_comment_identifiants' => '{{Use the fields below to specify different identifiers you want to use.}} They are not mandatory, but can help us to track precisely statistics offered by Facebook.',
+	'cfg_comment_pageid' => 'Identifier for a facebook page, it requires you have created the page.',
+	'cfg_comment_reglages' => '{{Here you can choose some settings of the Facebook javascript tools.}} By default, the models use the language XFBML ({SDK Facebook JavaScript}) but you can disable this feature, the tools will be loaded into frames.',
+	'cfg_comment_url_page' => 'Full URL of your page or Facebook profile; it will be used by default by the models (URL like "<code>http://www.facebook.com/...</ code>").',
+	'cfg_comment_userid' => 'User(s) Identifier(s) for the plugins administrators. You can specify multiple ones separated by a comma. ',
+	'cfg_comment_xfbml' => 'Use the Facebook SDK javascript library and associated language. If you choose "No", the modules will be displayed in iframe.',
+	'cfg_descr' => 'Here you need to define the different identifiers provided by the Facebook system.<br /><br />More: [->http://www.facebook.com/insights/].
+
+To include the tags "Open Graph" in the header of your public pages, you must include the model "insert_head_og" by passing the environment <code>#MODELE{insert_head_og}{env}</code>. 
+<br /><br />More: [->http://developers.facebook.com/docs/opengraph/].',
+	'cfg_descr_titre' => 'Facebook models',
+	'cfg_identifiants' => 'Facebook logins',
+	'cfg_label_appid' => 'Identifier "App ID" application ',
+	'cfg_label_border_color' => 'Default border color',
+	'cfg_label_colorscheme' => 'Color profile',
+	'cfg_label_font' => 'Default font',
+	'cfg_label_pageid' => 'Login "Page ID" page',
+	'cfg_label_titre' => 'Configuration Facebook models',
+	'cfg_label_url_page' => 'Page URL or profile',
+	'cfg_label_userid' => 'Identifier "User ID" user',
+	'cfg_label_xfbml' => 'XFBML use',
+	'cfg_reglages' => 'Default settings',
+
+	// D
+	'defaut' => 'Default',
+	'doc_chapo' => 'the plugin Models Facebook for SPIP 2.0 ({and more}) proposes a set of models, or hazelnuts, allowing quick and easy use of social plugins provided by Facebook.',
+	'doc_en_ligne' => 'Documentation',
+	'doc_titre_court' => 'Facebook models documentation',
+	'doc_titre_page' => 'Documentation page of the Facebook Models plugin',
+	'documentation' => '{{{Plugin use}}}
+
+As shown above, the models are directly included with the desired options.
+
+Each model has a list of options, some of which are mandatory for its display. For a complete list, refer to the information in the header models files in directory "<code>modeles/</code>" of the plugin.
+
+The plugin also provides an {{Open Graph}} model generating information, the meta information used by Facebook, specific to each SPIP object. To use it, you must manually add "{{insert_head_og}}" in the header of your skeletons.
+
+{{Warning -}} This model needs to receive the current environment, you must include the following in each of the page skeletons  ({"article.html", "rubrique.html" ...}) and not in the global header inclusion ({"inc_head.html"}):
+<cadre class=\'spip\'>
+{{#MODELE{insert_head_og}{env}}}
+</cadre>',
+
+	// E
+	'exemple' => '{{{Exemple}}}
+
+Different blocks below show you an example of each model with dummy values​​. Refer to the corresponding model for options.',
+
+	// F
+	'fb_modeles' => 'Facebook models',
+
+	// I
+	'info_doc' => 'If you are having problems viewing this page [click here->@link@].',
+	'info_doc_titre' => 'Note on the display of this page',
+	'info_skel_contrib' => 'Complete documentation online on spip-contrib: [->https://contrib.spip.net/?article3872].',
+	'info_skel_doc' => 'This manual page is designed as a skeleton SPIP operating with the standard distribution ({files from the "squelettes-dist/"}). If you are unable to view the page, or if your site uses its own skeletons, the links below can manage its display:
+
+-* [Mode "plain text"->@mode_brut@] ({simple html + tag INSERT_HEAD})
+-* [Mode "skeleton Zpip"->@mode_zpip@] ({compatible Z skeleton})
+-* [Mode "skeleton SPIP"->@mode_spip@] ({compatible distribution})',
+
+	// J
+	'javascript_inactif' => 'Javascript is disabled in your browser. Some features of this tool will be inactive ...',
+
+	// L
+	'licence' => 'Plugin for SPIP 2.0+ : {{"Facebook Models" - copyright © 2009 [Piero Wbmstr->https://contrib.spip.net/PieroWbmstr] under licence [GPL->http://www.opensource.org/licenses/gpl-3.0.html] }}.',
+
+	// N
+	'new_window' => 'New window',
+	'non' => 'No',
+
+	// O
+	'oui' => 'Yes',
+
+	// P
+	'page_test' => 'Test page (local)',
+	'page_test_in_new_window' => 'Test page in new window',
+	'personnalisation' => '{{{Customization}}}
+
+Each model presents its content in a block such as <code>div</ code> with CSS classes <code>fb_modeles fb_XXX</ code> where {{XXX}} is the name of the model. This allows customization of styles for all models and for each of them.
+
+
+Eg for Facebook "Send" module:
+<cadre class="spip">
+<div class="fb_modeles fb_send">
+     ... content ... 
+</div>
+</cadre>',
+
+	// S
+	'sep' => '----',
+
+	// T
+	'titre_original' => 'Facebook Models, plugin for SPIP 2.0+'
+);
---
+++ modeles_facebook/tags/v1.0.0/lang/fbmodeles_sk.php
@@ -0,0 +1,112 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/fbmodeles?lang_cible=sk
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cf_navigation' => 'Pozri [stĺpec s navigáciou->@url@]',
+	'cfg_comment_appid' => 'Identifikátor na hlasovaciu stránku aplikácie; aplikácia sa musí vytvoriť.',
+	'cfg_comment_border_color' => 'Zadajte kód farby v šestnástkovej sústave AJ S úvodnou mriežkou.',
+	'cfg_comment_colorscheme' => 'Tu vyberte predvolený profil podľa modulov, ktoré sa použijú na zobrazenie.',
+	'cfg_comment_font' => 'Tu si môžete vybrať písmo, ktoré sa použije na zobrazenie modulov.',
+	'cfg_comment_identifiants' => '{{Na zadanie rôznych ID, ktoré chcete používať, použite polia, ktoré sa nachádzajú nižšie.}} Nie sú povinné, ale môžu pomôcť napríklad na sledovanie presných štatistík, ktoré ponúka Facebook.',
+	'cfg_comment_pageid' => 'Identifikátor facebookovskej stránky; stránku treba vytvoriť.',
+	'cfg_comment_reglages' => '{{Tu si môžete upraviť niektoré nastavenia pre javaskriptové nástroje Facebooku.}} Podľa predvolených nastavení šablóny využívajú jazyk XFBML ({SDK javascript Facebook,}) ale túto funkciu môžete deaktivovať, nástroje sa potom budú spúšťať v rámoch.',
+	'cfg_comment_url_page' => 'Celá internetová adresa vašej stránky alebo vášho profilu na Facebooku; budú ju využívať šablóny v predvolených nastaveniach (URL typu "<code>http://www.facebook.com/...</code>").',
+	'cfg_comment_userid' => 'Prihlacovací (-ie) údaj(e) administrátorov zásuvných modulov. Môžete ich zadať viac, a oddeliť ich čiarkami.',
+	'cfg_comment_xfbml' => 'Využitie javaskriptovej knižnice SDK Facebook a priradeného jazyka. Ak si zvolíte možnosť "nie", moduly budú zobrazené v režime "in-iframe" (v ráme).',
+	'cfg_descr' => 'Tu musíte definovať rôzne identifikátory, ktoré vám poskytla sociálna sieť Facebook.<br /><br />Viac informácií: [->http://www.facebook.com/insights/].
+
+Na vkladanie značiek (resp. tagov) "Open Graph" do hlavičky svojich publikovaných stránok musíte vložiť šablónu "insert_head_og" vložením prostredia: {{<code>#MODELE{insert_head_og}{env}</code>.
+<br /><br />Viac informácií: [http://developers.facebook.com/docs/opengraph/.->http://developers.facebook.com/docs/opengraph/].',
+	'cfg_descr_titre' => 'Šablóny Facebooku',
+	'cfg_identifiants' => 'Prihlásenie na Facebook',
+	'cfg_label_appid' => 'Prihlásenie pre aplikáciu "App ID"',
+	'cfg_label_border_color' => 'Predvolená farba pozadia',
+	'cfg_label_colorscheme' => 'Farebná schéma',
+	'cfg_label_font' => 'Predvolené písmo',
+	'cfg_label_pageid' => 'Prihlásenie pre stránku "Page ID"',
+	'cfg_label_titre' => 'Nastavenia šablón Facebooku',
+	'cfg_label_url_page' => 'URL stránky alebo profilu',
+	'cfg_label_userid' => 'Prihlásenie používateľa "User ID"',
+	'cfg_label_xfbml' => 'Použitie XFBML',
+	'cfg_reglages' => 'Predvolené nastavenia',
+
+	// D
+	'defaut' => 'Predvolené',
+	'doc_chapo' => 'Zásuvný modul Šablóny pre Facebook pre SPIP 2.0 ({a vyšší}) ponúka rad šablón alebo orieškov umožňujúcich jednoducho a rýchlo využívať zásuvné moduly sociálnej siete, ktoré poskytuje Facebook.',
+	'doc_en_ligne' => 'Dokumentácia',
+	'doc_titre_court' => 'Dokumentácia Šablón Facebooku',
+	'doc_titre_page' => 'Stránka s dokumentáciou zásuvného modulu Šablóny Facebooku',
+	'documentation' => '{{{Použivanie zásuvného modulu}}}
+
+Ako vidno vyššie, šablóny sa používajú priamo podľa zvolených nastavení.
+
+Každá šablóna môže mať niekoľko nastavení, z ktorých niektoré sú potrebné na jej zobrazenie. Kompletný zoznam nastavení nájdete v hlavičke súborov šablóny v priečinku <code>"modeles/"</code> zásuvného modulu.
+
+Zásuvný modul ponúka aj informácie súvisiace s vytvorením šablóny {{Open Graph}}, meta dáta, ktoré využíva Facebook, samostatne pre každý objekt SPIPu. Na to, aby ste túto funkciu mohli využívať, musíte do hlavičky svojich šablón manuálne pridať šablónu  "{{insert_head_og}}".
+
+{{Pozor}} – táto šablóna si vyžaduje aktuálne prostredie, musíte ju vložiť do šablóny každej stránky  ({"article.html", "rubrique.html" ...}) a nielen do globálnej hlavičky ({"inc_head.html"}), a to takto: 
+<cadre class=\'spip\'>
+{{#MODELE{insert_head_og}{env}}}
+</cadre>
+',
+
+	// E
+	'exemple' => '{{{Príklad}}}
+
+Rôzne bloky, ktoré sa nachádzajú nižšie, slúžia ako príklad každej šablóny s fiktívnymi hodnotami. Ak chcete nastaviť možnosti, prejdite na konkrétnu šablónu.',
+
+	// F
+	'fb_modeles' => 'Šablóny Facebooku',
+
+	// I
+	'info_doc' => 'Ak sa vám táto stránka nezobrazuje správne, [kliknite sem.->@link@]',
+	'info_doc_titre' => 'Poznámka o zobrazení tejto stránky',
+	'info_skel_contrib' => 'Stránka s kompletnou dokumentáciou spip-contribu online: [->https://contrib.spip.net/?article3872].',
+	'info_skel_doc' => 'Táto stránka dokumentácie je vytvorená ako šablóna SPIPu, ktorá funguje spolu so štandardne distribuovanou verziou ({súbory adresára "squelettes-dist/"}). Ak túto stránku neviete zobraziť alebo ak vaša stránka používa vlastné šablóny, s úpravou zobrazenia tejto stránky dokumentácie vám pomôžu tieto odkazy:
+
+-* [formát "neformátovaný text"->@mode_brut@] ({jednoduché HTML + tag INSERT_HEAD})
+-* [formát "šablóna Zpip"->@mode_zpip@] ({kompatibilné so šablónou Z})
+-* [formát "šablóna SPIPu"->@mode_spip@] ({kompatibilné s distribuovanou verziou})',
+
+	// J
+	'javascript_inactif' => 'Javascript je vo vašom prehliadači vypnutý. Niektoré funkcie nebudú fungovať.',
+
+	// L
+	'licence' => 'Zásuvný modul pre SPIP >2.0: {{"Šablóny pre Facebook" – copyright © 2009 [Piero Wbmstr->https://contrib.spip.net/PieroWbmstr] s licenciou [GPL->http://www.opensource.org/licenses/gpl-3.0.html].}}',
+
+	// N
+	'new_window' => 'Nové okno',
+	'non' => 'Nie',
+
+	// O
+	'oui' => 'Áno',
+
+	// P
+	'page_test' => 'Testovacia stránka (lokálne)',
+	'page_test_in_new_window' => 'Testovacia stránka v novom okne',
+	'personnalisation' => '{{{Prispôsobenie}}}
+
+Každá šablóna má svoj text v bloku <code>div,</code> ktorý súvisí s tredami CSS typu <code>fb_modeles fb_XXX,</code> kde {{XXX}} je názov šablóny. To umožňuje prispôsobenie štýlon pre všetky šablóny spolu i pre každú osobitne.
+
+
+Par exemple pour le module Facebook "Send" :
+<cadre class="spip">
+<div class="fb_modeles fb_send">
+     ... contenu ... 
+</div>
+</cadre>',
+
+	// S
+	'sep' => '----',
+
+	// T
+	'titre_original' => 'Šablóny pre Facebook, zásuvný modul pre SPIP >2.0'
+);
