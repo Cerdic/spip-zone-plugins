972c5b69d9c95ee3780ddee047c4ab197956f89e
rastapopoulos@spip.org <rastapopoulos@spip.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-05 16:05:03 +0000

    Tag '/tags/v0.8.5' created from /branches/v0:106517
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123530 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ fulltext/tags/v0.8.5/fulltext_autoriser.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * 
+ * Autorisations spécifiques à Fulltext
+ * 
+ */
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+/**
+ * Fonction pour le pipeline, n'a rien a effectuer
+ *
+ * @return
+ */
+function fulltext_autoriser(){}
+
+function autoriser_fulltext_fulltext_bouton_dist($faire, $type, $id, $qui, $opt) {
+	return autoriser('configurer');
+}
+?>
