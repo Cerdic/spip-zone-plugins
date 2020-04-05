8f3cfb09c7738ec367f613c68259318227bccb4b
Rainer Müller <Rainer Mller@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-05 15:20:07 +0000

    Tag '/tags/v3.3.3' created from /branches/3.0:122426
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123467 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ edition_directe/tags/v3.3.3/lang/edir_es.php
@@ -0,0 +1,24 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/edir?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'activer_edition_directe' => 'Activar la Edición Directa para:',
+	'activer_edition_directe_objet' => 'Activar la Edición Directa para el objeto ',
+
+	// D
+	'desactiver_edition_directe_objet' => 'Desactivar la Edición Directa para el objeto ',
+
+	// T
+	'titre_plugin' => 'Edición Directa',
+
+	// V
+	'vider_cache' => 'Vaciar al cache'
+);
