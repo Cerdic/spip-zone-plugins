17e5936d8dfeaef8823b86fe70f11ac3ce2ecea7
peetdu@gmail.com <peetdu@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-05 15:20:07 +0000

    Tag '/tags/v1.3.5' created from /trunk:114174
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123443 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ declarerparent/tags/v1.3.5/lang/paquet-declarerparent_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// D
+	'declarerparent_description' => 'Cette fonctionnalité a vocation à intégrer le noyau de l’API de déclaration des objets. En attendant https://core.spip.net/issues/3844',
+	'declarerparent_nom' => 'Déclarer le parent',
+	'declarerparent_slogan' => 'Définir la relation à l‘objet parent dans la déclaration des objets',
+);
