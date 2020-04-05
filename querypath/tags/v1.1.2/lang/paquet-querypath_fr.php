c3ab14868cf84677dc3bf6a78ec0768f9fb37e8e
xdjuj@apsulis.com <xdjuj@apsulis.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-05 16:05:03 +0000

    Tag '/tags/v1.1.2' created from /trunk:106657
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123836 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ querypath/tags/v1.1.2/lang/paquet-querypath_fr.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// Q
+	'querypath_description' => 'Query Path (http://querypath.org/) est une librairie PHP permettant de récupérer, sélectionner et manipuler un contenu HTML comme le fait la librairie javascript jQuery.
+
+Ce plugin permet d\'utiliser cette librairie dans SPIP.',
+	'querypath_nom' => 'Query Path',
+	'querypath_slogan' => 'Du jQuery en PHP, et bien plus encore',
+);
+
+?>
