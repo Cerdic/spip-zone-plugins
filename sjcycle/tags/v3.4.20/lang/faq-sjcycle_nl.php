f4ccc2b6ca34214b9fff1ad1af8a544e06d20880
chankalan@choc0.net <chankalan@choc0.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-05 16:05:03 +0000

    Tag '/tags/v3.4.20' created from /trunk:104595
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123909 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ sjcycle/tags/v3.4.20/lang/faq-sjcycle_nl.php
@@ -0,0 +1,9 @@
+<?php
+// Pour le plugin Manuel de redaction du site https://contrib.spip.net/Manuel-de-redaction-du-site
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	'diapo_q' => 'Hoe kun je een diashow in een artikel opnemen?',
+	'diapo' => 'Nadat je afbeeldingen aan een artikel hebt toegevoegd, kan je een diashow aan je tekst toevoegen met <code><sjcycleXX></code> (XX is het artikelnummer). Er is een aantal opties beschikbaar, die je kunt zien door op de Help knop in de linkerkolom te drukken (wanneer je een artikel bewerkt)',
+);
---
+++ sjcycle/tags/v3.4.20/sjcycle_pipelines.php
@@ -0,0 +1,33 @@
+<?php
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+
+function sjcycle_insert_head_css($flux) {
+	include_spip('inc/config');
+	$tooltip = lire_config('sjcycle/tooltip');
+
+	if ($tooltip) {
+		$flux .="\n".'<link rel="stylesheet" href="'.find_in_path('lib/jquery.tooltip.css').'" type="text/css" media="all" />';
+	}
+	$flux .= "\n".'<link rel="stylesheet" type="text/css" href="'.find_in_path('css/sjcycle.css').'" media="all" />'."\n";
+	return $flux;
+}
+
+function sjcycle_insert_head($flux) {
+	include_spip('inc/config');
+	$tooltip = lire_config('sjcycle/tooltip');
+
+	$flux .="\n".'<script src="'.find_in_path('lib/jquery.cycle2.js').'" type="text/javascript"></script>';
+	$flux .="\n".'<script src="'.find_in_path('lib/jquery.cycle2.flip.js').'" type="text/javascript"></script>';
+	$flux .="\n".'<script src="'.find_in_path('lib/jquery.cycle2.carousel.js').'" type="text/javascript"></script>';
+	$flux .="\n".'<script src="'.find_in_path('lib/jquery.cycle2.scrollVert.js').'" type="text/javascript"></script>';
+	$flux .="\n".'<script src="'.find_in_path('lib/jquery.cycle2.shuffle.js').'" type="text/javascript"></script>';
+	$flux .="\n".'<script src="'.find_in_path('lib/jquery.cycle2.tile.js').'" type="text/javascript"></script>';
+
+	if ($tooltip) {
+		$flux .="\n".'<script src="'.find_in_path('lib/jquery.tooltip.js').'" type="text/javascript" charset="utf-8"></script>';
+	}
+
+	return $flux;
+}
