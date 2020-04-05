f415040e9aad94b1aeb936782b9fa3eef53407ce
spip.franck@lien-d-amis.net <spip.franck@lien-d-amis.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-05 11:59:06 +0000

    Tag '/tags/v1.1.2' created from /branches/v_1:103864
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123316 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ anythingslider/tags/v1.1.2/lang/anythingslider_fr.php
@@ -0,0 +1,14 @@
+<?php
+
+// Sécurité
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	'anythingslider' => 'AnythingSlider',
+	'configurer_anythingslider' => 'Configurer AnythingSlider',
+	'explication_configurer_anythingslider' => 'Choisissez les éléments à insérer dans l\'en-tête des pages publiques (voir la <a href="https://github.com/ProLoser/AnythingSlider/wiki">documentation du script</a>).',
+	'label_scripts' => 'Scripts optionnels',
+	'label_themes' => 'Thèmes',
+);
+
+?>
