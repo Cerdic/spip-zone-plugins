a5756baaa0680cdde6ee0e6a8f9f2d3b082dd54a
spip.franck@lien-d-amis.net <spip.franck@lien-d-amis.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-05 11:59:06 +0000

    Tag '/tags/v1.0.6' created from /branches/v1:103864
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123320 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ article_accueil/tags/v1.0.6/lang/article_accueil_en.php
@@ -0,0 +1,17 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	// A
+	'article_accueil' => 'Home article',
+	'aucun_article_accueil' => 'No article',
+	
+	// L
+	'label_id_article_accueil' => 'Home article',
+	
+	// R
+	'rubrique_article_en_accueil' => 'Home article&nbsp;:',
+);
+?>
---
+++ article_accueil/tags/v1.0.6/lang/article_accueil_es.php
@@ -0,0 +1,17 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	// A
+	'article_accueil' => 'Artículo destacado',
+	'aucun_article_accueil' => 'Ningún artículo',
+	
+	// L
+	'label_id_article_accueil' => 'Artículo destacado',
+	
+	// R
+	'rubrique_article_en_accueil' => 'Artículo destacado actual:',
+);
+?>
