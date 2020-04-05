75f126b98a33850ad4b66571f92dabf540e6858e
Salvatore <Salvatore@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-05 15:20:07 +0000

    Tag '/tags/v2.5.5' created from /trunk:122697
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123412 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ coordonnees/tags/v2.5.5/action/editer_email.php
@@ -0,0 +1,91 @@
+<?php
+
+/**
+ * Plugin Coordonnées
+ * Licence GPL (c) 2010 Matthieu Marcillaud
+**/
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+function action_editer_email_dist($arg = null) {
+	if (is_null($arg)){
+		$securiser_action = charger_fonction('securiser_action', 'inc');
+		$arg = $securiser_action();
+	}
+
+	// pas d'email ? on en cree une nouvelle, mais seulement si 'oui' en argument.
+	if (!$id_email = intval($arg)) {
+		if (!in_array($arg, array('oui', 'new'))) {
+			include_spip('inc/headers');
+			redirige_url_ecrire();
+		}
+		$id_email = insert_email();
+	}
+
+	$err = '';
+	if ($id_email) {
+		$err = revisions_emails($id_email);
+	}
+	return array($id_email, $err);
+}
+
+
+function insert_email($c = '') {
+	$champs = array(
+		'email' => _T('coordonnees:item_nouvel_email')
+	);
+
+	// Envoyer aux plugins
+	$champs = pipeline('pre_insertion', array(
+		'args' => array(
+			'table' => 'spip_emails',
+		),
+		'data' => $champs
+	));
+
+	$id_email = sql_insertq("spip_emails", $champs);
+
+	if (!$c) {
+		$c = array(
+			'objet' => _request('objet'),
+			'id_objet' => _request('id_objet'),
+			'type' => _request('type')
+		);
+	}
+
+	// ajouter la liaison si presente
+	if (!empty($c['objet']) and !empty($c['id_objet'])) {
+		if (empty($c['type'])) {
+			$c['type'] = '';
+		}
+		$c['id_email'] = $id_email;
+		sql_insertq("spip_emails_liens", $c);
+	}
+
+	return $id_email;
+}
+
+
+// Enregistrer certaines modifications d'un email
+function revisions_emails($id_email, $c = false) {
+
+	include_spip('inc/modifier');
+
+	// recuperer les champs dans POST s'ils ne sont pas transmis
+	if ($c === false) {
+		$c = collecter_requests(
+			objet_info('emails', 'champs_editables'),
+			array('maj')
+		);
+	}
+
+	$retour = objet_modifier_champs('email', $id_email, array(
+			'invalideur' => "id='id_email/$id_email'"
+		),
+		$c);
+
+	sql_update("spip_emails_liens", array(
+			'type' => sql_quote(_request('type'))
+		), "id_email=".intval($id_email)." AND id_objet=".intval(_request('id_objet'))." AND objet=".sql_quote(_request('objet')));
+
+	return $retour;
+}
