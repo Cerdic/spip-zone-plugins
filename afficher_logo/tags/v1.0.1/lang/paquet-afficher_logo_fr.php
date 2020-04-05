ff2be1ee16f8f3fca07d7c5ebf30eeaeb547c598
chankalan@choc0.net <chankalan@choc0.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-05 11:59:06 +0000

    Tag '/tags/v1.0.1' created from /trunk:106796
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123300 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ afficher_logo/tags/v1.0.1/lang/paquet-afficher_logo_fr.php
@@ -0,0 +1,8 @@
+<?php
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	'afficher_logo_description' => 'Afficher un logo (d\'article, de rubrique, d\'auteur, du site...) dans un texte.',
+	'afficher_logo_slogan' => 'Fais voir ton logo !',
+);
