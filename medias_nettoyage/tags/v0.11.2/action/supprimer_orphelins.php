07316021428849a1d86062723a5c7f9ba24498fa
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-05 16:05:03 +0000

    Tag '/tags/v0.11.2' created from /branches/v0:116243
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123663 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ medias_nettoyage/tags/v0.11.2/action/supprimer_orphelins.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Plugin Nettoyer la médiathèque.
+ *
+ * @plugin     Nettoyer la médiathèque
+ *
+ * @copyright  2014-2017
+ * @author     Teddy Payet
+ * @licence    GNU/GPL
+ */
+if (!defined('_ECRIRE_INC_VERSION')) {
+    return;
+}
+
+function action_supprimer_orphelins_dist()
+{
+    $securiser_action = charger_fonction('securiser_action', 'inc');
+    $arg = $securiser_action();
+}
---
+++ medias_nettoyage/tags/v0.11.2/medias_nettoyage_options.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Options principales du plugin "Nettoyer la médiathèque".
+ *
+ * @plugin     Nettoyer la médiathèque
+ *
+ * @copyright  2014-2017
+ * @author     Teddy Payet
+ * @licence    GNU/GPL
+ */
+if (!defined('_ECRIRE_INC_VERSION')) {
+    return;
+}
+
+if (!defined('_MEDIAS_NETTOYAGE_PAQUET')) {
+    define('_MEDIAS_NETTOYAGE_PAQUET', 200);
+}
+
+if (!defined('_MEDIAS_NETTOYAGE_REP_ORPHELINS')) {
+    define('_MEDIAS_NETTOYAGE_REP_ORPHELINS', _DIR_IMG.'orphelins/');
+}
