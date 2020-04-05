a67eede9ca61b7a143552baf3a2cf10b09b515ec
Salvatore <Salvatore@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-05 16:05:03 +0000

    Tag '/tags/v1.0.1' created from /trunk:121796
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123584 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ informer_premiere_connexion/tags/v1.0.1/lang/informerpremiereconnexion_fr.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans https://git.spip.net/spip-contrib-extensions/informer_premiere_connexion.git
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'configuration_texte_label' => 'Message à afficher à l’utilisateur⋅trice qui vient de confirmer son inscription',
+	'configuration_titre' => 'Configurer le message de première connexion',
+
+	// T
+	'texte_defaut' => 'Votre compte utilisateur a bien été créé !'
+);
