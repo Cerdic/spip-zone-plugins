6565cc987a64ca66e268058051c12768687e7e2d
Salvatore <Salvatore@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-05 11:59:06 +0000

    Tag '/tags/v2.2.0' created from /trunk:121682
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123308 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ alerte_urgence/tags/v2.2.0/lang/alerte_fr.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans https://git.spip.net/spip-contrib-extensions/alerte_urgence.git
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'configurer_limiter_accueil_label' => 'Limiter l’alerte à la page d’accueil',
+	'configurer_texte_label' => 'Alerte d’urgence à diffuser :',
+	'configurer_titre' => 'Configurer l’alerte d’urgence',
+	'configurer_utilisateurs_explication' => 'Par défaut, seuls les administrateurs complets peuvent modifier l’alerte. On peut sélectionner ici des utilisateurs supplémentaires.',
+	'configurer_utilisateurs_label' => 'Utilisateurs supplémentaires autorisés'
+);
