6839e169e10e45a6d9d5e9670171fc6a57e2ef57
tetue@rezo.net <tetue@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-05 16:05:03 +0000

    Tag '/tags/v2.3.3' created from /trunk:115226
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123671 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ medoc/tags/v2.3.3/lang/paquet-medoc_fr.php
@@ -0,0 +1,18 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/medoc/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// M
+	'medoc_description' => 'Patch unifiant le comportement et la syntaxe d\'insertion des documents, pour soigner l\'inconstance d\'affichage dont souffrent les documents de SPIP. Désormais, c\'est vous qui décidez !
+
+- Le raccourci &lt;doc&gt; affiche toujours une vignette cliquable
+- Syntaxe d\'insertion unifiée : <docXXX|left>, <docXXX|img|right>, <docXXX|emb|center>
+- Rétrocompatibilité préservée',
+	'medoc_nom' => 'Modèle &lt;doc&gt; unifié',
+	'medoc_slogan' => 'L\'insertion de documents simplifiée !'
+);
+
+?>
---
+++ medoc/tags/v2.3.3/medoc_fonctions.php
@@ -0,0 +1,28 @@
+<?php
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+function filtre_medias_raccourcis_doc($id_document,$titre,$descriptif,$inclus,$largeur,$hauteur,$mode,$vu,$media=null){
+	$raccourci = '';
+	$doc = 'doc';
+	include_spip('inc/documents'); // pour la fonction affiche_raccourci_doc
+
+	// Affichage du raccourci <doc...> correspondant
+	$raccourci = 
+		  affiche_raccourci_doc($doc, $id_document, 'left')
+		. affiche_raccourci_doc($doc, $id_document, 'center')
+		. affiche_raccourci_doc($doc, $id_document, 'right');
+	if ($mode=='document'
+		AND ($inclus == "embed" OR $inclus == "image")
+		AND (($largeur > 0 AND $hauteur > 0) 
+		OR in_array($media,array('video','audio')))) {
+		$raccourci =
+		  "<span>"._T('medias:info_inclusion_vignette')."</span>"
+		. $raccourci
+		. "<span>"._T('medias:info_inclusion_directe')."</span>"
+		. affiche_raccourci_doc('doc', $id_document.'|emb', 'left')
+		. affiche_raccourci_doc('doc', $id_document.'|emb', 'center')
+		. affiche_raccourci_doc('doc', $id_document.'|emb', 'right');
+	}
+	return "<div class='raccourcis'>".$raccourci."</div>";
+}
