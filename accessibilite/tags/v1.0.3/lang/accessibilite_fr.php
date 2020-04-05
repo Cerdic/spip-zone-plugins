63dc7f92c4ea72af3340ae851cebe6891b5f266f
spip.franck@lien-d-amis.net <spip.franck@lien-d-amis.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-05 10:07:20 +0000

    Tag '/tags/v1.0.3' created from /branches/v1:103864
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123284 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ accessibilite/tags/v1.0.3/lang/accessibilite_fr.php
@@ -0,0 +1,16 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+ 'voir_grand' => 'voir en grand cette image',
+ 'pages_dernieres' => 'derni&egrave;re page',
+ 'pages_premieres' => 'premi&egrave;re page',
+ 'resultats' => 'R&eacute;sultats',
+ 'resultats_precedent' => 'R&eacute;sultats pr&eacute;c&eacute;dents',
+ 'resultats_suivant' => 'R&eacute;sultats suivants'
+
+);
+
+?>
