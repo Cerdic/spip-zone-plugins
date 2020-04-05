ee895c47910bdad6cd81e155e306d70e8b24e0af
chankalan@choc0.net <chankalan@choc0.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-05 16:05:03 +0000

    Tag '/tags/v2.0.2' created from /branches/v1:111214
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123665 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ mediaboxavectexte/tags/v2.0.2/lang/paquet-mediaboxavectexte_fr.php
@@ -0,0 +1,11 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// M
+	'mediaboxavectexte_description' => 'Ce plugin surcharge le plugin MEDIABOX (plugin-dist/mediabox) pour afficher lors de l\'agrandissement de l\'image à la fois le titre et la description de cette image.',
+	'mediaboxavectexte_nom' => 'MediaBox avec texte',
+	'mediaboxavectexte_slogan' => 'Boîte multimédia image + texte'
+);
