598daef96fda7e9702ea011b4ddeeae1ebfe8708
spip.franck@lien-d-amis.net <spip.franck@lien-d-amis.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-05 16:05:03 +0000

    Tag '/tags/v1.0.4' created from /trunk:117696
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123841 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ rao/tags/v1.0.4/lang/paquet-rao_fr.php
@@ -0,0 +1,12 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// R
+	'rao_description' => 'Ce plugin offre un formulaire permettant de délier un auteur actuellement lié à des objets éditoriaux (tel que des articles), et d\'en lier un autre à la place. Il est ainsi possible d\'affecter tous les articles à l\'auteur «Camille» à la place de l\'auteur «Dominique». ',
+	'rao_nom' => 'Réassocier auteurs objets',
+	'rao_slogan' => 'Réaffecte un auteur à la place d\'un autre pour les objets éditoriaux sélectionnés.',
+);
