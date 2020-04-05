c80423e8082684a5eb1957bcee715eb1cf9b500b
Salvatore <Salvatore@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-05 16:05:03 +0000

    Tag '/tags/v3.1.0' created from /trunk:122259
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123911 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ skeleditor/tags/v3.1.0/lang/paquet-skeleditor_fr.php
@@ -0,0 +1,13 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans https://git.spip.net/spip-contrib-extensions/skeleditor.git
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// S
+	'skeleditor_description' => 'Editeur de squelettes, css, js, dans l’espace privé',
+	'skeleditor_slogan' => 'Editeur de squelettes'
+);
---
+++ skeleditor/tags/v3.1.0/lang/skeleditor_en.php
@@ -0,0 +1,95 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/skeleditor?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'action_download' => 'Download',
+	'action_nouveau' => 'New File',
+	'action_supprimer' => 'Delete',
+	'action_upload' => 'Send a file',
+
+	// C
+	'copy_comment' => '  Copied @date@ by @nom@
+  original file @source@',
+	'creer' => 'Create',
+
+	// E
+	'editer_skel' => 'Edit the template',
+	'editer_skel_debug' => 'Edit the template (debug mode)',
+	'effacer_confirme' => 'Do you really want to delete this file ?',
+	'erreur_creation_sous_dossier' => 'Can’t create @dir@ sub-directory',
+	'erreur_droits' => 'error: can’t create the file. Please check the template directory’s rights',
+	'erreur_ecrire_dans_sous_dossier' => 'Can’t write in @dir@ sub-directory',
+	'erreur_ecriture_fichier' => 'error: can’t write to file',
+	'erreur_edition_ecriture' => 'error: not writable file',
+	'erreur_fichier_inexistant' => 'This file doesn’t exist',
+	'erreur_fichier_modif_coucourante' => 'This file was modified while you changed it. Here is the new version:',
+	'erreur_fichier_modif_coucourante_votre_version' => 'Merge it with your version and save it:',
+	'erreur_fichier_supprime' => 'This file was deleted while being edited. Please copy the content and create a new file if needed.
+',
+	'erreur_ouvert_ecrit' => 'error: can’t open or edit this file',
+	'erreur_ouverture_fichier' => 'error: can’t open the file',
+	'erreur_overwrite' => 'error: this file already exists',
+	'erreur_parsing' => 'Fatal error while parsing the BOUCLES',
+	'erreur_sansgene' => 'Error: unauthorized action',
+	'erreur_type_interdit' => 'It is not allowed to use this kind of file.',
+	'explications_creer_dossier_squelettes' => 'To use the template editor, create a  <b>squelettes/</b> folder, if there isn’t already one, in your website’s root directory, then grant write access permissions to that folder.',
+	'explications_filename_fichier_chemin' => 'You can fill in a full path to set the file up in a sub-directory (eg: "modeles/document.html")',
+
+	// F
+	'fichier' => 'file',
+	'fichier_choix' => 'Please choose the file you want to edit or view.',
+	'fichier_efface_ok' => 'File deleted successfully !',
+	'fichier_enregistre' => 'The file was saved',
+	'fichier_sauvegarde_date' => 'file saved @ ',
+	'fichier_upload' => 'Upload a file',
+	'fichier_upload_ok' => 'File uploaded successfully !',
+
+	// I
+	'info_copie' => 'This file will be copied in the <em>@dir@</em> directory when saved',
+
+	// L
+	'label_code' => 'Spip code:',
+	'label_creer_fichier' => 'Create a file',
+	'label_file' => 'File',
+	'label_filename_squelette' => 'Name',
+	'label_nouveau_fichier' => 'New file',
+	'label_renommer_fichier' => 'Rename or move
+',
+
+	// P
+	'parseur_altern' => 'Alternate',
+	'parseur_apres' => 'After',
+	'parseur_avant' => 'Before',
+	'parseur_contenu' => 'Content',
+	'parseur_horsboucle' => 'OUTSIDE THE LOOPS',
+	'parseur_milieu' => 'Inside',
+	'parseur_param' => 'Parameters:',
+	'parseur_titre' => 'Loop parser',
+
+	// R
+	'rechercher' => 'Search',
+	'rechercher_ou_remplacer' => 'or',
+	'remplacer' => 'replace',
+	'remplacer_par' => 'it by',
+	'repertoire' => 'directory',
+
+	// S
+	'sauver' => 'Save',
+	'skeleditor_description' => 'Lets you edit current template files',
+	'skeleditor_dossier' => 'template directory: ',
+	'squelettes' => 'Templates&CSS',
+
+	// T
+	'target' => 'target',
+
+	// U
+	'upload' => 'Upload'
+);
