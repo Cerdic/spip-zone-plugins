4bacedff2a7837f767b4825289bc12282cf5ae12
spip.franck@lien-d-amis.net <spip.franck@lien-d-amis.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-05 16:05:03 +0000

    Tag '/tags/v1.0.3' created from /trunk:117696
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123968 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ test_url/tags/v1.0.3/lang/paquet-testurl_fr.php
@@ -0,0 +1,11 @@
+<?php
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// T
+	'testurl_description' => 'Chercher et tester l\'ensemble des liens contenu dans vos articles, rubriques, sites syndiqués dans votre site SPIP. Vous pourrez donc en un clic  conna&#238;tre les liens bris&#233;s ou d&#233;fectueux.',
+	'testurl_nom' => 'Test urls',
+	'testurl_slogan' => 'Faites la chasse aux liens brisés',
+);
+?>
