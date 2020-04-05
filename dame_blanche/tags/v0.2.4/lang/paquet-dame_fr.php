be9118c41c1ffd3d249e2f608a2c8adf8e846ea4
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-05 15:20:07 +0000

    Tag '/tags/v0.2.4' created from /trunk:119295
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123437 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ dame_blanche/tags/v0.2.4/lang/paquet-dame_fr.php
@@ -0,0 +1,13 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// D
+	'dame_description' => 'Permet de sortir un paragraphe d’un article pour le mettre en évidence. Il s’agit d’une phrase du texte que l’on fait ressortir dans la maquette, histoire de rythmer la page et de faire ressortir un point accrocheur. La plupart des gens appellent ça l’exergue, mais c’est erroné : ça s’appelle une dame blanche (l’exergue, c’est une citation qui n’est pas tirée du texte lui-même, et qu’on met en début d’article ou de livre.)',
+	'dame_nom' => 'Dame blanche',
+	'dame_slogan' => 'Gérer des dames blanches dans textwheel : <code><dame></dame></code>'
+);
