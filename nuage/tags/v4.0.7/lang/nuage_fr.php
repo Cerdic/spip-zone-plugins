338e396b1e12448b781d1b0be833619294b23481
Salvatore <Salvatore@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-05 16:05:03 +0000

    Tag '/tags/v4.0.7' created from /trunk:121895
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123754 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ nuage/tags/v4.0.7/lang/nuage_fr.php
@@ -0,0 +1,17 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans https://git.spip.net/spip-contrib-extensions/nuage.git
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// N
+	'noisette_description' => 'Affiche un nuage des mots clés',
+	'noisette_label_afficher_titre' => 'Afficher le titre :',
+	'noisette_label_niveau_titre' => 'Choisir le niveau du titre',
+	'noisette_nom_noisette' => 'Nuage',
+	'noisette_titre_defaut' => 'Tags',
+	'noisette_titre_noisette' => 'Titre :'
+);
---
+++ nuage/tags/v4.0.7/lang/paquet-nuage_sk.php
@@ -0,0 +1,20 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-nuage?lang_cible=sk
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// N
+	'nuage_description' => 'Tento zásuvný modul umožňuje zobraziť kľúčové slová neusporiadane rôznou  veľkosťou písma v závislosti od počtu položiek priradených ku kľúčovému slovu. Inými slovami spip umožňuje vytvárať "značky"!
+
+	K dispozícii je niekoľko modelov:
+-* article_nuage: zobrazí kľúčové slová článku, ktorých veľkosť písma sa odlišuje podľa frekvencie kľúčových slov v článku na stránke,
+-* rubrique_nuage: zobrazí kľúčové slová článkov vetvy, ktorých veľkosť písma sa bude odlišovať podľa frekvencie kľúčových slov v článkoch danej vetvy,
+-* nuage_popularite: zobrazí kľúčové slová stránky (alebo skupiny), ktorých veľkosť písma sa bude odlišovať podľa ich relatívnej popularity (súčet popularity článkov s daným kľúčovým slovom)',
+	'nuage_slogan' => 'Zobrazí kľúčové slová s rôznou veľkosťou písma'
+);
