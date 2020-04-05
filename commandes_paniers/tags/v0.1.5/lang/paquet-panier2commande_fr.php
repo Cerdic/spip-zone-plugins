7f1f57315e7456450269fe4abfd08e728407f4ec
tcharlss@hotmail.fr <tcharlss@hotmail.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-05 15:20:07 +0000

    Tag '/tags/v0.1.5' created from /branches/v0:88081
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123387 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ commandes_paniers/tags/v0.1.5/lang/paquet-panier2commande_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// P
+	'panier2commande_description' => 'Permet de remplir une commande avec le contenu d\'un panier.',
+	'panier2commande_nom' => 'Commandes de Paniers',
+	'panier2commande_slogan' => 'Remplir une commande',
+);
+
+?>
