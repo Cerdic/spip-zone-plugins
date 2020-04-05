aa10dac5f6b1254a5bc69167a4b9b62c73d7d5cd
spip.franck@lien-d-amis.net <spip.franck@lien-d-amis.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-05 15:20:07 +0000

    Tag '/tags/v0.2.6' created from /trunk:103714
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123473 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ elfinder/tags/v0.2.6/lang/paquet-elfinder_fr.php
@@ -0,0 +1,19 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-elfinder
+// Langue: fr
+// Date: 25-04-2012 12:25:32
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// N
+	'elfinder_description' => 'Ce plugin permet de parcourir l\'arborescence de son site SPIP grâce au gestionnaire de fichiers elFinder (les rubriques sont des dossiers,les autres objets sont les fichiers). Les fonctionnalités basiques des gestionnaires de fichiers sont présentes (déplacer, copier, créer un dossier, charger des fichier). ',
+	'elfinder_slogan' => 'Explorer votre site SPIP',
+);
+?>
