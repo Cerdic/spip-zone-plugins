3445e0908ef76f41439d8c737e86d6dc4014ed00
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-05 16:05:03 +0000

    Tag '/tags/v1.31.1' created from /branches/v1:119273
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123815 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ popup/tags/v1.31.1/lang/spipopup_fr.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * @name 		JavascriptPopup_lang_FR
+ * @author 		Piero Wbmstr <piero.wbmstr@gmail.com>
+ */
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// Description
+	'nom' => 'Balise &#035;POPUP',
+
+// CFG
+	'titre_descr_cfg' => 'Configuration de l\'outil "Popup"',
+	'descr_cfg' => 'Documentation du plugin pour plus d\'infos : [contrib.spip.net/?article3573->https://contrib.spip.net/?article3573]',
+	'legend_cfg_balise' => 'Concernant la fen&ecirc;tre externe',
+	'skel_defaut' => 'Squelette par d&eacute;faut utilis&eacute; pour afficher le contenu de la fen&ecirc;tre',
+	'skel_defaut_comment' => 'Il s\'agit initialement du squelette "popup_defaut.html" pr&eacute;sent &agrave; la racine du plugin.',
+	'width_and_height' => 'Taille de la fen&ecirc;te',
+	'width' => 'Largeur (en pixels)',
+	'height' => 'Hauteur (en pixels)',
+	'titre_popup' => 'Nom JavaScript de la fen&ecirc;tre',
+	'titre_popup_comment' => 'Vous pourrez par la suite utiliser ce nom pour la d&eacute;signer dans vos scripts ("window.popup").',
+	'options_popup' => 'Options JavaScript pass&eacute;es &agrave; la nouvelle fen&ecirc;tre',
+	'options_popup_comment' => 'Indiquez ici un tableau d\'options qui seront pass&eacute;es &agrave; la nouvelle fen&ecirc;tre sous la forme d\'un tableau du genre : "variable1: valeur1, variable2: valeur2, ..." (ex.: "location: 0, scrollbars: 1").',
+
+// Popup
+	'popup_titre' => 'Bo&#238;te de dialogue',
+	'btn_imprimer' => 'Imprimer',
+	'btn_imprimer_ttl' => 'Imprimer cette page',
+	'btn_fermer_fenetre' => 'Fermer',
+	'btn_fermer_fenetre_ttl' => 'Fermer cette fen&#234;tre',
+	'btn_haut_page' => 'Haut de page',
+	'btn_haut_page_ttl' => 'Retour en haut de page',
+	'nouvelle_fenetre' => '[Nouvelle fen&#234;tre]',
+	'retour_fenetre' => '[Retour en fen&#234;tre principale]',
+
+	// E
+	// editor
+	'editor' => 'Editeur de lien popup',
+	'editor_titre_inserer_lien_popup' => 'Insérer un lien popup',
+	'editor_lien' => 'Lien à insérer (URL ou objet SPIP)',
+	'editor_texte' => 'Texte du lien',
+	'editor_skel' => 'Squelette de page SPIP',
+	'editor_titre' => 'Titre du lien (au passage de la souris)',
+	'editor_erreur_entree_obligatoire' => 'Cette entrée est obligatoire !',
+	'editor_enregistrer' => 'Enregistrer',
+
+	// I
+	'inserer_lien_popup' => 'Insérer un lien en fenêtre popup',
+);
+?>
---
+++ popup/tags/v1.31.1/spipopup_pipelines.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * @name 		JavascriptPopup_pipelines
+ * @author 		Piero Wbmstr <piero.wbmstr@gmail.com>
+ */
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+/**
+ * Insertion du Javascript en en-tete
+ * @necessite La balise #INSERT_HEAD en en-tete de squelettes
+ * @utilise Pipeline 'insert_head'
+ */
+function spipopup_insert_head($flux){
+	spipopup_config();
+	$flux .= "\n<script src='".find_in_path('javascript/spipopup.js')
+		."' type='text/javascript'></script>"
+		."\n<script type='text/javascript'>var popup_settings={default_popup_name:'"
+		.POPUP_TITRE."',default_popup_width:'".POPUP_WIDTH."',default_popup_height:'"
+		.POPUP_HEIGHT."',default_popup_options:'".POPUP_OPTIONS_DEFAUT."'};</script>"
+		."\n";
+	return $flux;
+}
+
+function spipopup_porte_plume_barre_pre_charger($barres){
+	$barre = &$barres['edition'];
+
+	$link = $barre->get('link');
+	$link["dropMenu"] = array(
+			// poesie spip
+			array(
+				"id"          => 'popup_link',
+				"name"        => _T('spipopup:inserer_lien_popup'),
+				"className"   => "outil_barre_popup", 
+				"replaceWith" => 'function(markitup) { zone_selection = markitup.textarea; window.open("?exec=popup_edit", "popup_editor","scrollbars=yes,resizable=yes,width=480,height=400"); }',
+				"display"     => true,
+			),
+	);
+	$barre->set('link', $link);	
+
+	return $barres;
+}
+
+function spipopup_porte_plume_lien_classe_vers_icone($flux){
+	return array_merge($flux, array(
+		'outil_barre_popup' => array('popup.png','0'),
+	));
+}
+?>
