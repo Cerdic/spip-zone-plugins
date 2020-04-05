5b1b49cb35a2d09570e600e3364a44f551b1a88b
pro@martinroy.fr <pro@martinroy.fr@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-05 16:05:03 +0000

    Tag '/knacss/tags/v1.0.1' created from /knacss/trunk:90934
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123610 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ knacss/tags/v1.0.1/lang/paquet-knacsspip_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// K
+	'knacsspip_description' => '',
+	'knacsspip_nom' => 'Knacsspip',
+	'knacsspip_slogan' => 'Intégration du micro framework CSS Knacss à SPIP',
+);
+
+?>
