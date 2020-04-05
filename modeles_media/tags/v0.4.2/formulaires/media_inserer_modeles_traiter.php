41763c5cc50a463ca834f295006d0ff19b82882b
spip.franck@lien-d-amis.net <spip.franck@lien-d-amis.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-05 16:05:03 +0000

    Tag '/tags/v0.4.2' created from /branches/v_0:104327
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123709 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ modeles_media/tags/v0.4.2/formulaires/media_inserer_modeles_traiter.php
@@ -0,0 +1,40 @@
+<?php
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+function formulaires_media_inserer_modeles_traiter_dist($champs){
+	$code = '<'._request('modele');
+	if (_request('id_modele') && _request('id_modele')!='')
+		$code .= _request('id_modele');
+	if (_request('variante') && _request('variante')!='')
+		$code .= '|'._request('variante');
+	if (_request('classe') && _request('classe')!='')
+		$code .= '|'._request('classe');
+	if (_request('align') && _request('align')!='')
+		$code .= '|'._request('align');
+	foreach ($champs as $champ) {
+		if($champ != 'modele' && $champ != 'variante' && $champ != 'classe' && $champ != 'id_modele' && $champ != 'align' && _request($champ) && _request($champ)!='') {
+			// Cas de la légende
+			if($champ == 'legende' && _request($champ)=='personnalisee')
+				$code .= '';
+			elseif (_request('legende')!='personnalisee' && in_array($champ,array('titre','descriptif','credits','type','poids')))
+				$code .= '';
+			// Cas de la taille
+			elseif($champ == 'taille' && _request($champ)=='personnalisee')
+				$code .= '';
+			elseif (_request('taille')!='personnalisee' && in_array($champ,array('hauteur','largeur')))
+				$code .= '';
+			// Cas de titre_lien
+			elseif ($champ == 'titre_lien' && _request('lien')=='')
+				$code .= '';
+			// Cas général
+			elseif($champ == _request($champ))
+				$code .= "|$champ";
+			else
+				$code .= "|$champ="._request($champ);
+		}
+	}
+	$code .= '>';
+	return $code;
+}
+
+?>
