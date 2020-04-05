d67a1723f2fc66fbc0c053a6d534e80b29cb4a77
teddy.spip@gmail.com <teddy.spip@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-05 16:05:03 +0000

    Tag '/tags/v1.1.3' created from /branches/v1:86774
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123639 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ lister_logos/tags/v1.1.3/lister_logos_fonctions.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Fonctions utiles au plugin Lister les logos
+ *
+ * @plugin     Lister les logos
+ * @copyright  2014
+ * @author     Teddy Payet
+ * @licence    GNU/GPL
+ * @package    SPIP\Lister_logos\Fonctions
+ */
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+
+?>
---
+++ lister_logos/tags/v1.1.3/lister_logos_pipelines.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Utilisations de pipelines par Lister les logos
+ *
+ * @plugin     Lister les logos
+ * @copyright  2014
+ * @author     Teddy Payet
+ * @licence    GNU/GPL
+ * @package    SPIP\Lister_logos\Pipelines
+ */
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+	
+
+
+
+?>
