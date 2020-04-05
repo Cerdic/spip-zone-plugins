d363ce4b3d01573bda4171466dda8f335619c694
spip.franck@lien-d-amis.net <spip.franck@lien-d-amis.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-05 16:05:03 +0000

    Tag '/tags/v1.2.11' created from /branches/1:117696
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123991 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ traduction_rubriques_autrement/tags/v1.2.11/lang/paquet-trad_rub_pt_br.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-trad_rub?lang_cible=pt_br
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// T
+	'trad_rub_description' => 'Este plugin, destinado aos sites multilingues com [um setor por idioma->http://contrib.spip.net/Un-exemple-de-site-multilingue], oferece uma abordagem semelhante, no nível da navegação e do gerenciameento das traduções das seções, à do plugin [Tradução de matérias alternativa->http://files.spip.org/spip-zone/traduction_articles_autrement.zip], cuja instalação é recomendada. Como o SPIP não oferece nativamente a tradução das seções, a instalação do plugin [Tradução das seções->http://files.spip.org/spip-zone/tradrub.zip] é necessário.', # MODIF
+	'trad_rub_nom' => 'Tradução das seções alternativa',
+	'trad_rub_slogan' => 'Navegar entre as seções traduzidas.'
+);
