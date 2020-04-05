fadf683dfd1692387302f7b63bef36ee14a94239
erational@erational.org <erational@erational.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-05 16:05:03 +0000

    Tag '/tags/v1.3.0' created from /trunk:108447
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123714 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ motpassecomplexe/tags/v1.3.0/lang/motpassecomplexe_fr.php
@@ -0,0 +1,17 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// M
+	'motpassecomplexe_titre' => 'Mot de passe complexe',
+
+	// I
+	'info_passe_trop_court' => 'Le mot de passe doit contenir au moins @nb@ caractères dont @nb_min@ minuscule(s),  @nb_maj@ majuscules(s), @nb_int@ chiffes(s) et @nb_spe@ caractère(s) spéciaux.',
+);
+
+
