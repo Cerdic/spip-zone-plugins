c2614fa5ab590544c9cb55372df9a3cc50e0cd7f
Rainer Müller <Rainer Mller@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-05 16:05:03 +0000

    Tag '/tags/v1.1.6' created from /branches/v1:122582
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123645 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ location_objets_bank/tags/v1.1.6/lang/location_objets_bank_en.php
@@ -0,0 +1,32 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/location_objets_bank?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// L
+	'location_objets_bank_titre' => 'Bookings for objects - payments',
+	'location_paiement_reference' => 'Payment reference : @reference@',
+
+	// M
+	'merci_de_votre_location_paiement' => 'Your booking has been validated <b>@reference@</b>.',
+	'message_paiement_vendeur' => 'Payment method : "@mode@", see <a href="@url@">detail</a>',
+
+	// P
+	'paiement_acompte' => 'Please pay a deposit of @acompte@',
+
+	// T
+	'texte_statut_partiel' => 'Partially paid',
+	'texte_statut_paye' => 'Paid',
+	'titre_choix_paiement' => 'Payment method selection:',
+	'titre_choix_paiement_securise' => 'I choose my secure payment method:',
+	'titre_paiement' => 'Payment',
+	'titre_paiement_location' => 'Payment of the booking',
+	'titre_paiement_vendeur' => 'Payment:',
+	'titre_payer_location' => 'Pay the booking'
+);
