185b85b5d8f4e7fe91f1369c8c29c23888003620
Charles Razack <Charles Razack@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-05 16:05:03 +0000

    Tag '/tags/v1.2.4' created from /trunk:122922
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123821 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ prive_fluide/tags/v1.2.4/lang/paquet-prive_fluide_fr.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/prive_fluide/trunk/lang
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// S
+	'prive_fluide_nom' => 'Espace privé fluide',
+	'prive_fluide_slogan' => 'Un espace privé qui utilise toute la largeur disponible',
+	'prive_fluide_description' => 'Ce plugin est une petite rustine qui permet à l\'interface d\'administration de SPIP d\'occuper toute la largeur de l\'écran. Pas de distinction sur ce point entre les options « petit écran » et « grand écran », une fois activé, on prend toute la largeur disponible, point barre ! La seule différence quand on active le mode « grand écran », c\'est que la colonne extra passe à droite.',
+
+);
---
+++ prive_fluide/tags/v1.2.4/lang/prive_fluide_fr.php
@@ -0,0 +1,13 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/prive_fluide/trunk/lang
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// M
+	'message_configurer_largeur_ecran' => '[Plugin espace privé fluide] Cette option n\'a plus d\'effet !',
+
+);
