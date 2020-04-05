52266fcae3c530a40efb586649330884684cde96
Salvatore <Salvatore@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-05 16:05:03 +0000

    Tag '/tags/v2.0.3' created from /trunk:121863
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123704 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ minus/tags/v2.0.3/lang/minus_fr.php
@@ -0,0 +1,12 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans https://git.spip.net/spip-contrib-extensions/minus.git
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// T
+	'trop_majuscule' => 'Votre titre contient beaucoup de majuscules. Êtes vous sûr de ne pas vouloir les mettre en minuscules ? Sur Internet, les majuscules donnent l’impression que l’on crie : utilisez-les avec parcimonie.'
+);
