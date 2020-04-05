d8b6508e918b21c86b6a64ff1fa58e4fae63e673
spip.franck@lien-d-amis.net <spip.franck@lien-d-amis.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-05 16:05:03 +0000

    Tag '/tags/v2.1.15' created from /branches/v2.1:103864
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123895 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ seminaire/tags/v2.1.15/lang/seminaire_fr.php
@@ -0,0 +1,70 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+ 
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'attention_mot_cle'			 =>		'Il faut IMPERATIVEMENT mettre un mot cl&eacute; sur l&rsquo;&eacute;v&eacute;nement',
+	'abstract'					 => 	'R&eacute;sum&eacute;',
+	'apreciser'					 =>		'A pr&eacute;ciser',
+	'aujour'					 =>		'Les &eacute;v&eacute;nements de la journ&eacute;e',
+	'a_venir'					 =>		'Les prochains s&eacute;minaires',
+	'attendee'					 => 	'Nom de l&rsquo;intervenant',
+	'attention_type'			 =>		'Le choix du type d&rsquo;&eacute;v&eacute;nement d&eacute;termine le tri qui lui sera appliqu&eacute;.',
+	'abonnement'				 =>		'S&rsquo;abonner au calendrier',
+
+	// C
+	'cfg_exemple'				 => 	'Exemple',
+	'cfg_exemple_explication'	 => 	'Explication de cet exemple',
+	'cfg_titre_parametrages'	 => 	'Paramétrages',
+	'choix_mot'					 =>		'Type d&rsquo;&eacute;v&eacute;nement',
+	'creer_evenement'			 =>		'Ajouter un &eacute;v&eacute;nement', 
+
+	// E
+	'erreur_install_groupe_categories' => 'erreur &agrave; la cr&eacute;ation du groupe',
+	'evenement_titre'			 =>		'Titre de l&rsquo;expos&eacute;',
+	'evenements_depuis_debut'	 =>		'Archives',	
+	'evenements_du_jour'		 =>		'Les &eacute;v&eacute;nements du jour',	
+	'evenements_seul'			 =>		'D&eacute;tails d&rsquo;un &eacute;v&eacutenement',	
+	'exemple'					 =>		'Par exemple : CMI - Salle R164',
+
+	// L
+	'lien_tout_replier'			 =>		'Sans r&eacute;sum&eacute;',
+	'lien_tout_deplier'			 =>		'Avec r&eacute;sum&eacute;',
+	'lieu'						 =>		'Lieu',
+
+	// M
+	'mots_categorie_kitcnrs'	 => 	'Les mots cl&eacute;s à affecter aux articles pour que les &eacute;v&eacute;nements soient encore mieux tri&eacute;s',
+	'mots_cles_categories'		 =>		'Les diff&eacute;rentes cat&eacute;gories de s&eacute;minaires, groupes de travail et &eacute;v&eacute;nements exceptionnels',
+	'mots_cles_techniques_kitcnrs'=>	'Les mots cl&eacute;s pour rep&eacute;rer plus finement les diff&eacute;rents types d&rsquo;&eacute;v&eacute;nements',
+
+	// N
+	'nom_de_l_intervenant'		 =>		'Intervenant &agrave; pr&eacute;ciser',
+	'notes'						 =>		'Notes de dernières minutes',
+
+	// O
+	'origin'					 => 	'Institut d&rsquo;origine de l&rsquo;intervenant',
+	'oubli_titre'				 =>		'Ne pas oublier de mettre le titre de l&rsquo;&eacute;v&eacute;nement',
+
+
+	// P
+	'plus'						 =>		'En savoir plus',
+	'precisions_name'			 =>		'le nom sera affich&eacute; dans le calendrier et dans le mini calendrier',
+	'precisions_origin'			 =>		'N&rsquo;apparaît que dans les d&eacute;tails de l&rsquo;&eacute;v&eacute;nement',
+	'precisions_abstract'		 =>		'pour mettre du code LATEX, ajoutez les balises <math> </math> en d&eacute;but et fin de texte (ex :  <math>La fraction $\frac{1}{2}$</math>)',
+	'precisions_notes'			 =>		'N&rsquo;apparaît que dans les d&eacute;tails de l&rsquo;&eacute;v&eacute;nement',
+
+	// S
+	'seminaire_titre' => 'Séminaire',
+
+	// T
+	'titre_agenda'				 =>		'Agenda Scientifique',
+	'titre_evenement'			 =>		'Titre à préciser',
+	'titre_page_configurer_seminaire' => 'seminaire',
+	'type'						 =>		'Type d&rsquo;&eacute;v&eacute;nement',
+	'type_evenement'			 => ' Le choix d\'un type d\'événement est obligatoire.',
+);
+
+?>
