7c06a1286280e95f6eaba18525f0a49ed37cd1c2
Charles Razack <Charles Razack@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-05 16:05:03 +0000

    Tag '/tags/v2.3.5' created from /trunk:123185
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123699 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ metasplus/tags/v2.3.5/lang/metasplus_fr.php
@@ -0,0 +1,32 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans https://git.spip.net/spip-contrib-extensions/metasplus.git
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// E
+	'explication_id_doc_logo' => 'Permet d’utiliser une image de votre choix à la place du logo du site SPIP (propriétés "og:image" et "twitter:image", format conseillé : 1,91:1)',
+	'explication_masquer_auteurs' => 'Cochez cette option si vous ne souhaitez pas diffuser le nom des rédacteurs des contenus : le nom du site sera utilisé à la place.',
+
+	// I
+	'icone_previsu' => 'Aperçu des métas+',
+	'icone_previsu_title' => 'Aperçu des métadonnées Dublin Core, Open Graph et Twitter.',
+
+	// L
+	'label_case_masquer_auteurs' => 'Utiliser le nom du site comme auteur des contenus',
+	'label_dublincore' => 'Dublin Core',
+	'label_masquer_auteurs' => 'Auteurs',
+	'label_opengraph' => 'Open Graph',
+	'label_previsu_fond' => 'Fond utilisé',
+	'label_previsu_objet' => 'Objet éditorial',
+	'label_reseaux' => 'Cochez les métas à inclure',
+	'label_twitter' => 'Twitter',
+	'logo_bis' => 'Image du site',
+
+	// T
+	'titre_config_metasplus' => 'Configurer Métas +',
+	'titre_previsu' => 'Aperçu des métadonnées Dublin Core, Open Graph et Twitter.'
+);
