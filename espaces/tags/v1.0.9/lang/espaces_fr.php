fe4494cbfcddfc38b5c1ebf202a2425e7919b6cf
Rainer Müller <Rainer Mller@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-05 15:20:07 +0000

    Tag '/tags/v1.0.9' created from /branches/v1:122574
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123481 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ espaces/tags/v1.0.9/lang/espaces_fr.php
@@ -0,0 +1,19 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans https://git.spip.net/spip-contrib-extensions/espaces.git
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cfg_titre_parametrages' => 'Paramétrages',
+	'champ_chambres_objets' => 'Activer l’ajout d’informations extras sur les contenus :',
+
+	// E
+	'espaces_titre' => 'Espaces',
+
+	// T
+	'titre_page_configurer_espaces' => 'Espaces'
+);
---
+++ espaces/tags/v1.0.9/lang/paquet-espaces_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans https://git.spip.net/spip-contrib-extensions/espaces.git
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// E
+	'espaces_description' => 'Gestion d’espaces',
+	'espaces_nom' => 'Espaces',
+	'espaces_slogan' => 'Gestion d’espaces'
+);
