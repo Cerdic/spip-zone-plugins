abecdbc8b30b75b5c9ed6f023c88c7f58d5c0221
spip.franck@lien-d-amis.net <spip.franck@lien-d-amis.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-05 11:59:06 +0000

    Tag '/tags/v2.0.1' created from /branches/v2:103720
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123371 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ cloudzoom/tags/v2.0.1/lang/cloudzoom_en.php
@@ -0,0 +1,66 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://trac.rezo.net/spip/spip/ecrire/lang/
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// A
+
+'adjustx' => 'adjustx',
+'adjusty' => 'adjusty',
+
+// B
+
+'bottom' => 'bottom',
+
+// F
+
+'false' => 'false',
+
+// I
+
+'inside' => 'inside',
+
+// L
+
+'left' => 'left',
+'lensopacity' => 'lens opacity',
+
+// O
+
+'opacityexplain' => 'between 0 and 1',
+
+//P
+
+'parametre' => 'Display settings',
+'position' => 'position',
+'pixel' => '(pixels)',
+
+//R
+
+'right' => 'right',
+
+// S
+
+'softfocus' => 'softfocus',
+'softfocusexplain' => 'doesn\'t work with tint',
+'showtitle' => 'show title',
+
+//T
+
+'titre_menu_cloudzoom' => 'CloudZoom',
+'top' => 'top',
+'tint' => 'tint',
+'tintexplain' => 'hex format (ex:#fff). doesn\'t work with softfocus',
+'tintopacity' => 'tint opacity',
+'true' => 'true',
+'titleopacity' => 'title opcity',
+
+// Z
+'zoom' => 'zoom in on image',
+'zoomwidth' => 'zoomwidth',
+'zoomheight' => 'zoomheight',
+);
+
+?>
