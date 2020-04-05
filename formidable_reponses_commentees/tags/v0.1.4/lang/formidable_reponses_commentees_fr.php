4c936cae9a41c4b7778961c6ae5d47b434b240cf
Salvatore <Salvatore@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-05 15:20:07 +0000

    Tag '/tags/v0.1.4' created from /trunk:121770
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123518 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ formidable_reponses_commentees/tags/v0.1.4/lang/formidable_reponses_commentees_fr.php
@@ -0,0 +1,13 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans https://git.spip.net/spip-contrib-extensions/formidable_reponses_commentees.git
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'commentaire_apres_reponse_explication' => 'Saisissez un commentaire qui sera fournie à l’utilisateur après qu’il ait envoyé sa réponse. Vous pouvez utiliser les raccourcis de SPIP.',
+	'commentaire_apres_reponse_label' => 'Commentaire à afficher après la réponse'
+);
---
+++ formidable_reponses_commentees/tags/v0.1.4/lang/formidable_reponses_commentees_pt_br.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/formidable_reponses_commentees?lang_cible=pt_br
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'commentaire_apres_reponse_explication' => 'Informe um comentário que será mostrado ao usuário após ele ter enviado a sua resposta. Você pode usar os atalhos do SPIP.',
+	'commentaire_apres_reponse_label' => 'Comentário a exibir após a resposta'
+);
