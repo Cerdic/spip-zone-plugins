92f2a5231aaf97753fc876abe4d9854a1608a97c
spip.franck@lien-d-amis.net <spip.franck@lien-d-amis.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-05 16:05:03 +0000

    Tag '/tags/v1.0.1' created from /trunk:103864
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123576 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ imagesactives/tags/v1.0.1/lang/imagesactives_fr.php
@@ -0,0 +1,13 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+    'images_actives:'=>'Images Actives',
+    'pas_d_images_actives'=>'Pas de document associé',
+    'chargement'=>'Chargement',
+    'sans_titre'=>'Pas d\'image active ici',
+);
+
+?>
