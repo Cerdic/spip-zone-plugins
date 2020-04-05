b14862653779910f48a4ce368b85d96c971794ef
spip.franck@lien-d-amis.net <spip.franck@lien-d-amis.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-05 16:05:03 +0000

    Tag '/tags/v0.1.2' created from /trunk:116095
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123742 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ noizetier_doc/tags/v0.1.2/lang/noizetier_doc_fr.php
@@ -0,0 +1,12 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	'bouton_noizetier_doc' => "Documentation des noisettes",
+	'titre_page_noisettes_doc' => "Documentation des noisettes",
+	'noisettes_autres' => 'Autres noisettes',
+);
---
+++ noizetier_doc/tags/v0.1.2/lang/paquet-noizetier_doc_fr.php
@@ -0,0 +1,12 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/plugonet/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	'noizetier_doc_description' => 'Ce plugin permet de lister depuis l\'espace privé de SPIP l\'ensemble des noisettes à disposition sur votre site.',
+	'noizetier_doc_slogan' => 'Des noisettes, oui, documentées, c\'est mieux!'
+);
