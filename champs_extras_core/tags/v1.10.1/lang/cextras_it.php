c57a717e9486c081fa50c183d3340facb7abee24
spip.franck@lien-d-amis.net <spip.franck@lien-d-amis.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-05 11:59:06 +0000

    Tag '/tags/v1.10.1' created from /branches/v1:103720
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123358 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ champs_extras_core/tags/v1.10.1/lang/cextras_it.php
@@ -0,0 +1,23 @@
+<?php
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cextra_par_defaut' => 'Valore predefinito',
+	
+	//T
+	'table_article' => 'Articoli',
+	'table_auteur' => 'Autori',
+	'table_breve' => 'Brevi',
+	'table_groupes_mot' => 'Gruppi di parole',
+	'table_mot' => 'Parole',
+	'table_rubrique' => 'Rubriche',
+	'table_site' => 'Siti',
+	
+	'type' => '@type@',
+#	'type_input'=>'Afficher une saisie input de type texte',
+#	'type_textarea'=>'Afficher une saisie textarea',
+);
+?>
