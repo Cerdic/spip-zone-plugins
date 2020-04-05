839b73e2e9e19f597a6656e2e8271ee16dbbfad3
suske@brubel.net <suske@brubel.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-05 16:05:03 +0000

    Tag '/tags/v0.6.2' created from /trunk:109083
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123954 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ supersized/tags/v0.6.2/lang/paquet-supersized_fr.php
@@ -0,0 +1,16 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+// Module: paquet-supersized
+// Langue: fr
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// A-Z
+	'supersized_description' => 'Intégration dans SPIP du plugin «jQuery Supersized slideshow» (sous licence MIT)',
+	'supersized_nom' => 'Supersized slideshow',
+	'supersized_slogan' => 'Un slideshow en plein &#233;cran pour vos images'
+);
+?>
