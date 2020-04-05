46d699ec28c89c5929680f88b1ef6a7af95798f0
Rainer Müller <Rainer Mller@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-05 16:05:03 +0000

    Tag '/tags/v2.4.2' created from /trunk:122412
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123823 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ prix_objets/tags/v2.4.2/action/ordonner_prix_objets.php
@@ -0,0 +1,102 @@
+<?php
+
+/**
+ * Action ordonnant un lien sur une table de liens, adapté du plugin Media
+ *
+ * @plugin     Prix Objets
+ * @copyright  2012 - 2020
+ * @author     Rainer Müller
+ * @licence    GNU/GPL
+ * @package    SPIP\Prix_objets\Action
+ */
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+function action_ordonner_prix_objets_dist() {
+
+	include_spip('inc/autoriser');
+	include_spip('base/objets');
+	include_spip('action/editer_liens');
+
+	// source (table spip_xx_liens)
+	$objet = 'prix_objet';
+
+	// objet lié
+	$objet_lie = _request('objet_lie');
+	$id_objet_lie = intval(_request('id_objet_lie'));
+
+	// ordre des éléments
+	$ordre = _request('ordre');
+
+	if (!$objet or !$objet_lie or !$id_objet_lie OR !$ordre) {
+		return envoyer_json_erreur(_T('medias:erreur_objet_absent') . ' ' . _T('medias:erreur_deplacement_impossible'));
+	}
+
+	if (!autoriser('modifier', $objet_lie, $id_objet_lie)) {
+		return envoyer_json_erreur(_T('medias:erreur_autorisation') . ' ' . _T('medias:erreur_deplacement_impossible'));
+	}
+
+	$_id_objet = 'id_prix_objet';
+	$table_liens = 'spip_prix_objets';
+
+	$success = $errors = array();
+
+	$actuels = sql_allfetsel(
+		array($_id_objet . ' AS id', 'rang_lien'),
+		$table_liens,
+		array(
+			sql_in($_id_objet, $ordre),
+			'objet = ' . sql_quote($objet_lie),
+			'id_objet = ' . sql_quote($id_objet_lie)
+		)
+		);
+
+	$futurs = array_flip($ordre);
+	// ordre de 1 à n (pas de 0 à n).
+	array_walk($futurs, function(&$v) { $v++; });
+
+	$updates = array();
+
+	foreach ($actuels as $l) {
+		if ($futurs[$l['id']] !== $l['rang_lien']) {
+			$updates[$l['id']] = $futurs[$l['id']];
+		}
+	}
+
+	if ($updates) {
+		foreach ($updates as $id => $ordre) {
+			sql_updateq(
+				$table_liens,
+				array('rang_lien' => $ordre),
+				array(
+					$_id_objet . ' = ' . $id,
+					'objet = ' . sql_quote($objet_lie),
+					'id_objet = ' . sql_quote($id_objet_lie)
+				)
+				);
+		}
+	}
+
+	return envoyer_json_envoi(array(
+		'done' => true,
+		'success' => $success,
+		'errors' => $errors,
+	));
+
+}
+
+
+function envoyer_json_envoi($data) {
+	header('Content-Type: application/json; charset=' . $GLOBALS['meta']['charset']);
+	echo json_encode($data);
+}
+
+function envoyer_json_erreur($msg) {
+	return envoyer_json_envoi(array(
+		'done' => false,
+		'success' => array(),
+		'errors' => array($msg)
+	));
+}
---
+++ prix_objets/tags/v2.4.2/prix_objets_administrations.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * Fichier gérant l'installation et désinstallation du plugin Prix Objets
+ *
+ * @plugin     Prix Objets
+ * @copyright  2012 - 2020
+ * @author     Rainer Müller
+ * @licence    GNU/GPL
+ * @package    SPIP\Reservation_evenement\Installation
+ */
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+
+/**
+ * Fonction d'installation et de mise à jour du plugin Prix Objets.
+ *
+ * Vous pouvez :
+ *
+ * - créer la structure SQL,
+ * - insérer du pre-contenu,
+ * - installer des valeurs de configuration,
+ * - mettre à jour la structure SQL
+ *
+ * @param string $nom_meta_base_version
+ *        	Nom de la meta informant de la version du schéma de données du plugin installé dans SPIP
+ * @param string $version_cible
+ *        	Version du schéma de données dans ce plugin (déclaré dans paquet.xml)
+ * @return void
+ *
+ */
+function prix_objets_upgrade($nom_meta_base_version, $version_cible) {
+	include_spip('inc/config');
+
+	$maj = array();
+	$maj['create'] = array(
+		array('maj_tables', array('spip_prix_objets'))
+	);
+	$maj['1.1.0']  = array(
+		array('sql_alter','TABLE spip_prix_objets RENAME TO spip_prix_objets')
+	);
+	$maj['1.1.2']  = array(
+		array('sql_alter','TABLE spip_prix_objets CHANGE prix prix_ht float (38,2) NOT NULL'),
+		array('maj_tables', array('spip_prix_objets')),
+	);
+	$maj['1.1.3']  = array(
+		array('sql_alter','TABLE spip_prix_objets CHANGE prix prix float (38,2) NOT NULL'),
+	);
+	$maj['1.1.4']  = array(
+		array('sql_alter','TABLE spip_prix_objets CHANGE id_prix id_prix_objet bigint(21) NOT NULL'),
+	);
+	$maj['1.1.5'] = array(array('maj_tables', array('spip_prix_objets')));
+	$maj['1.2.4'] = array(array('maj_tables', array('spip_prix_objets')));
+	$maj['1.3.0'] = array(array('maj_tables', array('spip_prix_objets')));
+	$maj['1.4.0'] = array(
+		array('ecrire_config', 'prix_objets',lire_config('shop_prix',array())),
+		array('ecrire_config', 'shop_prix',array()),
+		array('effacer_meta', 'shop_prix_base_version')
+	);
+	$maj['1.5.0']  = array(
+		array('sql_alter','TABLE spip_prix_objets CHANGE prix prix decimal(15,2) NOT NULL DEFAULT "0.00"'),
+		array('sql_alter','TABLE spip_prix_objets CHANGE prix_ht prix_ht decimal(15,2) NOT NULL DEFAULT "0.00"'),
+	);
+	$maj['2.0.0']  = array(
+		array('maj_tables', array('spip_prix_objets')),
+		array('sql_alter','TABLE spip_prix_objets DROP INDEX id_objet'),
+		array('sql_alter','TABLE spip_prix_objets ADD INDEX `id_objet` (`id_objet`,`id_prix_objet_source`,`objet`,`id_extension`,`extension`)'),
+		array('po_upgrade','2.0.0'),
+	);
+	$maj['2.1.1']  = array(
+		array('maj_tables', array('spip_prix_objets')),
+	);
+	$maj['2.1.4']  = array(
+		array('maj_tables', array('spip_prix_objets')),
+	);
+
+	include_spip('base/upgrade');
+	maj_plugin($nom_meta_base_version, $version_cible, $maj);
+}
+
+
+/**
+ * Fonction de désinstallation du plugin Prix Objets.
+ *
+ * Vous devez :
+ *
+ * - nettoyer toutes les données ajoutées par le plugin et son utilisation
+ * - supprimer les tables et les champs créés par le plugin.
+ *
+ * @param string $nom_meta_base_version
+ *        	Nom de la meta informant de la version du schéma de données du plugin installé dans SPIP
+ * @return void
+ *
+ */
+function prix_objets_vider_tables($nom_meta_base_version) {
+
+	sql_drop_table("spip_prix_objets");
+
+	effacer_meta($nom_meta_base_version);
+}
