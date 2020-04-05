62bc313638315c44fb77d6898a1243d4fd033dfb
spip.franck@lien-d-amis.net <spip.franck@lien-d-amis.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-05 16:05:03 +0000

    Tag '/tags/v1.0.3' created from /trunk:106681
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123525 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ forum_sans_previsu/tags/v1.0.3/forum_sans_previsu_pipelines.php
@@ -0,0 +1,10 @@
+<?php
+function forum_sans_previsu_formulaire_verifier($flux){
+	if($flux['args']['form'] === 'forum'){
+
+		unset($flux['data']['previsu']);
+		unset($flux['data']['message_erreur']);
+	}
+	return $flux;
+}
+?>
---
+++ forum_sans_previsu/tags/v1.0.3/lang/forum_fr.php
@@ -0,0 +1,10 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_core_/plugins/forum/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	'forum_voir_avant' => 'Envoyer',
+)
+?>
