49805b078067942920a1dcf7417f722c87196385
Cerdic <Cerdic@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-05 16:05:03 +0000

    Tag '/tags/v2.9.1' created from /trunk:123146
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@124023 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ zen-garden/tags/v2.9.1/lang/paquet-zengarden_es.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-zengarden?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// Z
+	'zengarden_description' => ' ',
+	'zengarden_slogan' => 'Un jardín Zen para los esqueletos'
+);
---
+++ zen-garden/tags/v2.9.1/lang/paquet-zengarden_fr.php
@@ -0,0 +1,13 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans https://git.spip.net/spip-contrib-extensions/zen-garden.git
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// Z
+	'zengarden_description' => ' ',
+	'zengarden_slogan' => 'Un jardin Zen pour les squelettes'
+);
