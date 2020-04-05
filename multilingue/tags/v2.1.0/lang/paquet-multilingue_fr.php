2458d9173d89b714579fae98564defb04e24e916
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-05 16:05:03 +0000

    Tag '/tags/v2.1.0' created from /branches/v2:122386
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123727 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ multilingue/tags/v2.1.0/lang/paquet-multilingue_fr.php
@@ -0,0 +1,26 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans https://git.spip.net/spip-contrib-extensions/multilingue.git
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// M
+	'multilingue_description' => 'Met en place un site multilingue avec une langue par secteur.
+
+	Le plugin force l’installation de plusieurs plugin
+
+	- [Langue préférée par l’internaute ->https://contrib.spip.net/Plugin-Langue-preferee-par-l-internaute]
+	- [Interface de traduction pour objets->https://plugins.spip.net/interface_traduction_objets.html]
+	- [Secteur par langue->https://plugins.spip.net/secteur_langue.html]
+
+	Le plugin modifie quelques squelettes (rubriques et articles) et quelques noisettes de bases.
+
+	Une fois installé les plugins nécessaires, il vous suffira de déterminer les langues du site dans "Configuration"->"Mulilinguisme/Gestion des langues, et voilà votre site est multilingue
+
+	',
+	'multilingue_nom' => 'Site multilingue facile',
+	'multilingue_slogan' => 'Créer facilement un site multilingue'
+);
---
+++ multilingue/tags/v2.1.0/lang/paquet-multilingue_nl.php
@@ -0,0 +1,28 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-multilingue?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// M
+	'multilingue_description' => 'Maak een meertalige site met een taal per hoofdrubriek.
+	
+	Deze plugin forceert de installatie van meerdere plugins
+	
+	- [Langue préférée par l’internaute ->https://contrib.spip.net/Plugin-Langue-preferee-par-l-internaute]
+	- [Interface de traduction pour objets->https://plugins.spip.net/interface_traduction_objets.html]
+	- [Secteur par langue->https://plugins.spip.net/secteur_langue.html]
+	
+	De plugin past enkele skeletten aan (van rubrieken en artikelen) alsmede enkele basisonderdelen.
+	
+	Zodra de nodige plugins zijn geactiveerd, leg je de talen van de site vast in de configuratie van de site (meertaligheid) en je site is meertalig
+	
+	',
+	'multilingue_nom' => 'Site Multilingue Facile',
+	'multilingue_slogan' => 'Maak eenvoudig een meertalige site'
+);
