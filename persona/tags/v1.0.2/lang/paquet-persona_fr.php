514c16db6471c023a41ecf3841ee3b4ff82cbe68
Salvatore <Salvatore@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-05 16:05:03 +0000

    Tag '/tags/v1.0.2' created from /trunk:121928
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123798 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ persona/tags/v1.0.2/lang/paquet-persona_fr.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans https://git.spip.net/spip-contrib-extensions/persona.git
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// P
+	'persona_description' => 'persona est une méthode d’authentification proposée par Mozilla. Elle repose sur l’emploi de l’email de l’auteur comme clé de connexion (et non pas d’une URL comme pour OpenID). Ce plugin implémente persona dans SPIP : connexion, création de compte, signature des messages de forum ou des pétitions…
+
+A noter : persona est une technologie expérimentale, et le plugin persona pour SPIP est lui aussi expérimental.',
+	'persona_slogan' => 'Authentification des visiteurs via persona'
+);
