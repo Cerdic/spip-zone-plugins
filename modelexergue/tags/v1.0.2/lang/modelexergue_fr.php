253264a8d3f035625f3fe0193fa78ae58c936a56
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-05 16:05:03 +0000

    Tag '/tags/v1.0.2' created from /trunk:107238
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123706 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ modelexergue/tags/v1.0.2/lang/modelexergue_fr.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// M
+	'modelexergue_titre' => 'Modèle exergue',
+	// T
+	'modelexergue_texteinserer' => 'Texte à mettre en exergue',
+	// U
+	'modelexergue_inserer' => 'un exergue',
+);
---
+++ modelexergue/tags/v1.0.2/lang/paquet-modelexergue_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// M
+	'modelexergue_description' => '',
+	'modelexergue_nom' => 'Modèle exergue',
+	'modelexergue_slogan' => 'Insérez des exergues dans vos textes',
+);
