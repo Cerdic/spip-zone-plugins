fc05e884ebd9a19d52de6636ba0398baf318a87b
marcimat@rezo.net <marcimat@rezo.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-05 16:05:03 +0000

    Tag '/tags/v0.3.0' created from /branches/outils_article0:108650
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123774 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ outils_article/tags/v0.3.0/lang/outilsarticle_es.php
@@ -0,0 +1,22 @@
+<?php
+
+	$GLOBALS[$GLOBALS['idx_lang']] = array(
+		'titre'				=>	'Caja de herramientas',
+		'taille'			=>	'Tamaño ',
+		'pdf'				=>	'Formato pdf ',
+		'impression'		=>	'Impresión ',
+		'impression_title'	=>	'versión imprimible del artículo ',
+		'auteur'			=>	'Del mismo autor ',
+		'reaction'			=>	'Reacciones ',
+		'reagir'			=>	'Reaccionar al artículo ',
+		'config_titre'		=>	'Caja de herramientas para artículo ',
+		'config_descr'		=>	'puede configurar los elementos que deben indicarse en la cojea a herramientas ',
+		'elements'			=>	'Elementos que deben indicarse ',
+		'mail'				=>	'enviar por email',
+		'compteur'			=>	'muchas reacciones ',
+		'note'				=>	'',
+		'rss'				=>	'',
+		'rssforum'			=>	'',
+	);
+
+?>
---
+++ outils_article/tags/v0.3.0/lang/outilsarticle_fr.php
@@ -0,0 +1,43 @@
+<?php
+
+	$GLOBALS[$GLOBALS['idx_lang']] = array(
+		'titre'				=>	'Boite &agrave; outils',
+		'taille'			=>	'Taille du texte',
+		'textemoins'		=>	'diminuer le texte',
+		'texteplus'			=>	'augmenter le texte',
+		'pdf'				=>	'Format pdf',
+		'impression'		=>	'Impression',
+		'impression_title'	=>	'version imprimable de l\'article',
+		'auteur'			=>	'Du m&ecirc;me auteur',
+		'reaction'			=>	'R&eacute;actions',
+		'reagir'			=>	'R&eacute;agir &agrave; l\'article',
+		'config_titre'		=>	'Boite &agrave; outils pour article',
+		'config_descr'		=>	'vous pouvez configurer les &eacute;l&eacute;ments &agrave; afficher dans la boite &agrave; outils',
+		'elements'			=>	'El&eacute;ments &agrave; afficher',
+		'mail'				=>	'envoyer par mail',
+		'compteur'			=>	'nombre de r&eacute;actions',
+		'note'				=>	'donner une note &agrave; l\'article avec le plugin notation',
+		'rss'				=>	'Syndiquer la rubrique',
+		'rssforum'			=>	'Syndiquer le forum de l\'article',
+		'descriptif'		=>	'
+		Vous pouvez configurer les &eacute;l&eacute;ments &agrave; afficher dans la boite &agrave; outils<br />
+Documentation compl&egrave;te sur <a href="https://contrib.spip.net/Boite-a-outils-pour-article">contrib</a><br />
+
+Plugins compl&eacute;mentaires
+<ul>
+<li><a href="https://contrib.spip.net/Plugin-Article-PDF">Article PDF</a>
+
+<li><a href="https://contrib.spip.net/Plugin-envoyer-a-un-ami">Envoyer &agrave; un ami</a>
+
+<li>Recommander &agrave; un ami</a>
+
+<li><a href="https://contrib.spip.net/Texte-agrandi-ou-pleine-page">Bouton texte</a>
+
+<li><a href="https://contrib.spip.net/Notation-d-articles-SPIP">Notation</a>
+
+<li><a href="https://contrib.spip.net/cfg-references">cfg</a>
+
+</ul>'
+	);
+
+?>
