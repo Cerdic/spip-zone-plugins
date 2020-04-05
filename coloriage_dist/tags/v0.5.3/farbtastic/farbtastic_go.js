af26b81cd81582bce0a1a5a9bf5b544d25b5ebad
jluc@no-log.org <jluc@no-log.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-05 15:20:07 +0000

    Tag '/tags/v0.5.3' created from /branches/v0:115382
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123381 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ coloriage_dist/tags/v0.5.3/farbtastic/farbtastic_go.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    var f = $.farbtastic('#picker');
+    var p = $('#picker').css('opacity', 0.25);
+    var selected;
+    $('.colorwell')
+      .each(function () { f.linkTo(this); $(this).css('opacity', 0.75); })
+      .focus(function() {
+        if (selected) {
+          $(selected).css('opacity', 0.75).removeClass('colorwell-selected');          
+        }
+        f.linkTo(this);
+        p.css('opacity', 1);
+        $(selected = this).css('opacity', 1).addClass('colorwell-selected');
+      });
+});
