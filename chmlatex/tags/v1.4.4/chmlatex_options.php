2f43af39b828406509b81c6960c35f245fed8789
spip.franck@lien-d-amis.net <spip.franck@lien-d-amis.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-05 11:59:06 +0000

    Tag '/tags/v1.4.4' created from /trunk:117696
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123364 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ chmlatex/tags/v1.4.4/chmlatex_options.php
@@ -0,0 +1,74 @@
+<?php
+function to_win1252($texte)
+{
+$texte = @iconv("UTF-8","Windows-1252//TRANSLIT",$texte);
+return $texte;
+}
+
+function chmlatex_propre_latex($t)
+{
+
+	$t = latex_echappe_equation($t,true);
+	//$t = latex_traiter_modeles($t);
+	$t = traiter_modeles($t);
+
+	// Application du filtre Belles Puces https://contrib.spip.net/Belles-puces
+	$t = preg_replace('/^-\s+/m','-* ',$t);
+
+	$t = appliquer_regles_wheel($t,array('latex/latex.yaml'));
+	$t = latex_echappe_coloration($t);
+	$t = echappe_html($t);
+
+	$t = echappe_retour(echappe_retour($t),'latex');
+	$t = appliquer_regles_wheel($t,array('latex/latex-retour.yaml'));
+	$t = preg_replace("#\^#iU",'\$\\hat{~}\$',$t);
+	$t = latex_echappe_equation($t,false);
+
+	$t = preg_replace("#<u>#iU",'\\underline{',$t);
+	$t = preg_replace("#</u>#iU",'}',$t);
+	$t = preg_replace("#<p.*>#iU",' ',$t);
+	$t = preg_replace("#</p>#iU",' ',$t);
+	$t = preg_replace("#<sup>#iU",'\$\^{',$t);
+	$t = preg_replace("#</sup>#iU",'}\$',$t);
+	$t = preg_replace("#<sub>#iU",'\$_{',$t);
+	$t = preg_replace("#</sub>#iU",'}\$',$t);
+	$t = preg_replace("#<code.*>#iU",' ',$t);
+	$t = preg_replace("#</html>#iU",' ',$t);
+	$t = preg_replace("#<html>#iU",' ',$t);
+	$t = preg_replace("#</strike>#iU",'}',$t);
+	$t = preg_replace("#<tr>#iU",' ',$t);
+	$t = preg_replace("#</td> </tr>#iU",' ',$t);
+	$t = preg_replace("#</tr>#iU",' ',$t);
+	$t = preg_replace("#<td>#iU",' ',$t);
+	$t = preg_replace("#</td>#iU",' ',$t);
+	$t = preg_replace("#</table>#iU",' ',$t);
+	$t = preg_replace("#<table>#iU",' ',$t);
+	$t = preg_replace("#</span>#iU",' ',$t);
+	$t = preg_replace("#<span class='(.*)'>#iU",' ',$t);
+
+	$t = str_replace("<latex\\_strike>",'\\sout{',$t);
+	$t = str_replace("<latex\\_table>",'',$t);
+	$t = str_replace("\\begin{english}",'',$t);
+	$t = str_replace("\\end{english}",'',$t);
+	$t = str_replace("\\end{minted}",'',$t);
+	$t = str_replace("\\begin{minted}",'',$t);
+	$t = str_replace("\\end\\emph{english}",'',$t);
+	$t = str_replace("\\end\\emph{minted}",'',$t);
+	$t = str_replace("\\verb¡",'',$t);
+	$t = str_replace("\\$",'$',$t);
+
+	$t = str_replace("\\&gt;",'>',$t);
+	$t = str_replace("\\&lt;",'<',$t);
+	$t = str_replace("\\&eacute;",'é',$t);
+	$t = str_replace("\\&egrave;",'è',$t);
+	$t = str_replace("\\&aegrave;",'à',$t);
+	$t = str_replace("\\&quot;",'"',$t);
+	$t = str_replace("\\_",'_',$t);
+	//$t = str_replace("latex\\_",'',$t);
+	$t = str_replace("\\&reg;",' \textregistered',$t);
+	$t = str_replace("\\&ge;",'≥',$t);
+	$t = str_replace("\\&le;",'≤',$t);
+
+	return $t;
+}
+?>
---
+++ chmlatex/tags/v1.4.4/formulaires/fchmlatex.php
@@ -0,0 +1,480 @@
+<?php
+if (!defined("_ECRIRE_INC_VERSION")) return;
+include_spip('inc/yaml');
+include_spip('inc/headers');
+
+function formulaires_fchmlatex_charger_dist($self)
+{
+	if (isset($_GET['num']))
+	{
+		//si il y a un parametre num dans l'url alors nous ne plus au début de l'execution du script
+		$num = _request('num');
+		$sDirExport = getDirExport();
+		$data = yaml_decode_file($sDirExport.'liste.yaml');
+		$a = $data[$num];
+		$max = count($data);
+
+		if ($max != 0) $avancement = round($num*100/count($data),0);
+
+		$format = _request('format');
+
+		if($num<$max)
+		{
+			$code = recuperer_fond('inc/chmlatex_progression', array(
+				'boite_titre' => _T("chmlatex:generation_$format"),
+				'titre_objet' => _T('public:'.$a['type']).' '.$a['id'].' : '.$a['titre'],
+				'progression' => $avancement)
+			);
+
+			echo $code;
+
+			$secteur = _request('secteur');
+			$sFunctionAction = $format.'_export';
+			$sFunctionAction($a,$num,$secteur,_request('langue'));
+			$num = $num + 1;
+			$url = parametre_url($self, 'num', $num);
+			$url = str_replace('&amp;','&',$url);
+			echo '<script>window.location = "'.$url.'"</script>';
+		}
+		elseif($num==$max)
+		{
+			$sZipFileName = getZipFileName();
+			zipDir($sDirExport ,$sZipFileName);
+			return array(
+				'message_ok'=>"<a href='$sZipFileName'>"._T("chmlatex:telecharger_$format")."</a>",
+				'format'    => $format,
+				'langue'    => _request('langue'));
+		}
+	}
+	return array();
+}
+
+
+function formulaires_fchmlatex_verifier_dist()
+{
+	$erreurs = array();
+	// verifier que les champs obligatoires sont bien la :
+	foreach(array('secteur_region') as $obligatoire)
+		if (!_request($obligatoire)) $erreurs[$obligatoire] = 'Ce champ est obligatoire';
+
+	if (count($erreurs))
+			$erreurs['message_erreur'] = 'Votre saisie contient des erreurs !';
+	return $erreurs;
+}
+
+
+/**
+ * Traitement du formulaire CVT :
+ * Préparation de l'export et rechargement javascript de la page pour traiter le premier objet
+ * @param $self adresse de la page du formulaire
+ * @author Hicham Gartit, David Dorchies
+ */
+function formulaires_fchmlatex_traiter_dist($self)
+{
+	$format = _request('format_export');
+	$langue = _request('langue_export');
+	$secteur = _request('secteur_region');
+
+	// Suppression du dossier d'export
+	$sDirExport = getDirExport('format_export','langue_export');
+	//~ spip_log($sDirExport,'chm');
+	if (file_exists($sDirExport)) delTree($sDirExport);
+	// Suppression du ZIP
+	$sZipFileName = getZipFileName('format_export','langue_export');
+	if (file_exists($sZipFileName)) unlink($sZipFileName);
+	// Suppression du YAML
+	if (file_exists($sDirExport.'liste.yaml')) unlink($sDirExport.'liste.yaml');
+	// Suppression du log
+	if (file_exists($sDirExport.'export.log')) unlink($sDirExport.'export.log');
+	// Création des dossiers d'export
+	if (!file_exists($sDirExport)) mkdir($sDirExport, 0777);
+	if (!file_exists($sDirExport.'images')) mkdir($sDirExport.'images', 0777);
+	if ($format=='tex' && !file_exists($sDirExport.'inclus'))
+		mkdir($sDirExport.'inclus', 0777);
+	// Création du fichier YAML contenant la liste des rubriques et articles du secteur
+	cree_yaml($sDirExport,$langue,$secteur);
+	// Construction de l'URL de rechargement javascript du formulaire
+	$url = parametre_url($self, 'num', '0');
+	$url = parametre_url($url, 'format', $format);
+	$url = parametre_url($url, 'secteur', $secteur);
+	$url = parametre_url($url, 'langue', $langue);
+	$url = str_replace('&amp;','&',$url);
+	echo'<script>window.location = "'.$url.'"</script>';
+	return array();
+}
+
+
+/**
+ * Fonction pour transformer les balises img en \includegraphics
+ */
+function tex_img2includegraphics($matches)
+{
+	$chemin = $matches[1];
+
+	if(substr($chemin, 0, strlen('../')) === '../' || substr($chemin, 0, strlen('http')) === 'http')
+	{
+		$chemin = str_replace(' ','%20', $chemin);
+		$source = $chemin;
+	}
+	else
+	{
+		$source = '../'.$chemin;
+	}
+	return '\includegraphics{'.$source.'}';
+}
+
+
+/**
+ * Traitement des balises latex includegraphics - copie des images
+ */
+function tex_copie_images($matches)
+{
+	$d = getDirExport()."images/";
+	$nomimg = pathinfo($matches[1], PATHINFO_FILENAME);
+	$extimg = pathinfo($matches[1], PATHINFO_EXTENSION);
+	$ext = substr($extimg , 0, 3);
+	$chemin = str_replace($extimg,$ext,$matches[1]);
+	$nm = $nomimg.'.'.$ext;
+	$nom = str_replace("\\", "", $nm);
+
+	if(strstr($chemin, '}'))
+	{
+		$chemin = substr($chemin, 0, strpos($chemin, "}"));
+	}
+
+	$chemin = str_replace('%20',' ', $chemin);
+	if(strstr($nom, '}'))
+	{
+		$nom = substr($nom, 0, strpos($nom, "}"));
+	}
+	$dest = $d.$nom;
+	$chemin = str_replace("\\_",'_',$chemin);
+
+	copy($chemin,$dest);
+	if ($ext == 'gif')
+	{
+		$ext = 'png';
+		$n = $nomimg.'.png';
+		$n1 = str_replace("\\", "", $n);
+		if (imagepng(imagecreatefromstring(file_get_contents($d.$nom)), $d.$n1))
+			{
+				unlink($d.$nom);
+			}
+	}
+	$nm = $nomimg.'.'.$ext;
+	$nom = str_replace("\\", "", $nm);
+	if(strstr($nom, '}'))
+	{
+		$nom = substr($nom, 0, strpos($nom, "}"));
+	}
+	$copie = 'images/'.$nom;
+	$include = str_replace('includegraphics{','includegraphics[max width=\textwidth]{',$matches[0]);
+	return str_replace($matches[1],$nom,$include);
+}
+
+
+/**
+ * Post-traitement d'une page pour l'export Latex
+ * @param $code code de la page à traiter
+ * @return Code traité
+ */
+function tex_post_traitement($code) {
+	//Traitement des images
+	$code = preg_replace_callback('#<img.*src="(.*)".*>#iU','tex_img2includegraphics',$code);
+	$code = preg_replace_callback("#<img.*src='(.*)'.*>#iU",'tex_img2includegraphics',$code);
+	$code = preg_replace_callback("#\\includegraphics{(.*)}#i",'tex_copie_images',$code);
+	$code = str_replace("\\\\includegraphics{",'\\includegraphics{',$code);
+	return $code;
+}
+
+
+/**
+ * Renvoie le dossier d'exportation
+ * @author David Dorchies
+ * @date 09/06/2015
+ */
+function getDirExport($format = 'format', $langue = 'langue') {
+	$s = _DIR_RACINE . _NOM_TEMPORAIRES_INACCESSIBLES;
+	$s .= _request($format).'_'._request($langue).'/';
+	return $s;
+}
+
+
+/**
+ * Renvoie le nom du fichier ZIP
+ * @author David Dorchies
+ * @date 09/06/2015
+ */
+function getZipFileName($format = 'format', $langue = 'langue') {
+	$s = _DIR_RACINE . _NOM_TEMPORAIRES_ACCESSIBLES;
+	$s .= _request($format).'_'._request($langue).'.zip';
+	return $s;
+}
+
+
+/**
+ * Traitement des balises img pour export HTML/CHM.
+ * Copie du fichier image dans le dossier d'export et modification du ilen
+ * @param $matches [0]: balise img, [1]: lien vers l'image
+ * @return lien modifié pour pointer vers le dossier d'export
+ * @author Hicham Gartit
+ */
+function html_image($matches)
+{
+	$chemin = $matches[1];
+	$nomimg = pathinfo($chemin, PATHINFO_FILENAME);
+	$extimg = pathinfo($chemin, PATHINFO_EXTENSION);
+	$nom = $nomimg.'.'.$extimg;
+	//~ spip_log($chemin,'html_image');
+	if(substr($chemin, 0, strlen('../')) === '../' || substr($chemin, 0, strlen('http')) === 'http')
+	{
+		$chemin = str_replace(' ','%20', $chemin);
+		$source = $chemin;
+	}
+	else
+	{
+		$source = '../'.$chemin;
+	}
+	$dest = getDirExport().'images/'.$nom;
+	$copie = 'images/'.$nom;
+	$ret = str_replace($matches[1],$copie,$matches[0]);
+	copy($source,$dest);
+	if(!file_exists($dest)) {
+		file_put_contents(getDirExport().'export.log',
+			'Image not found: '.$source."\n",FILE_APPEND);
+	}
+	return $ret;
+}
+
+
+/**
+ * Traitement des liens entre articles et rubriques pour l'export HTML/CHM
+ * Transforme les liens du type /?exec=article&id_article=nnn et /?exec=rubrique&id_rubrique=nnn
+ * en lien articlennn.html et rubriquennn.html
+ * @param $matches lien absolu ou relatif
+ * @author Hicham Gartit, David Dorchies
+ */
+function html_lien($matches)
+{
+	$chemin = $matches[1];
+	$nomf = pathinfo($chemin, PATHINFO_FILENAME);
+	$ext = pathinfo($chemin, PATHINFO_EXTENSION);
+	$nom = $nomf.'.'.$ext;
+	$id = 0;
+
+	if(substr($chemin, 0, strlen('../')) === '../' || substr($chemin, 0, strlen('http')) === 'http')
+	{
+		if(substr($chemin, 0, strlen($GLOBALS['meta']['adresse_site'].'/ecrire/?exec=')) === $GLOBALS['meta']['adresse_site'].'/ecrire/?exec=')
+		{
+			$tAncre = explode('#',$chemin); // Traitement des ancres
+			$chemin = $tAncre[0];
+			if(strstr($chemin, 'exec=article'))
+			{
+				$id = substr(strstr($chemin, "id_article="),11);
+				// Il faut s'assurer que l'id correspond à la traduction de la langue en cours
+				$art = sql_fetsel(array('id_trad','lang'), 'spip_articles', "id_article=$id");
+				$langue = _request('langue');
+				if($art['lang']!=$langue) {
+					// Le lien ne pointe pas vers la bonne traduction de l'article
+					$art2 = sql_fetsel('id_article', 'spip_articles',
+						array('id_trad='.$art['id_trad'],"lang='$langue'",'statut="publie"'));
+						//~ spip_log(sql_get_select('id_article', 'spip_articles',
+						//~ array('id_trad='.$art['id_trad'],array('id_trad='.$art['id_trad'],"lang='$langue'",'statut="publie"'))),'lien');
+					if(isset($art2['id_article'])) {
+						// La bonne traduction existe
+						$id = $art2['id_article'];
+					} elseif($art['id_trad']!=0) {
+						// La bonne traduction n'existe pas, on utilise l'article de rÃ©fÃ©rence
+						$id = $art['id_trad'];
+					}
+				}
+				$type = 'article';
+			}
+			else if(strstr($chemin, 'exec=rubrique'))
+			{
+				$id = substr(strstr($chemin, "id_rubrique="),12);
+				$type = 'rubrique';
+			}
+			$nom = $type.$id.'.html';
+			if(isset($tAncre[1])) $nom .= '#'.$tAncre[1]; // Rajout de l'ancre si elle existe
+
+			return str_replace($matches[1],$nom,$matches[0]);
+		}
+	}
+	return $matches[0];
+}
+
+
+/**
+ * Ecriture de la liste des articles et rubriques dans liste.yaml
+ */
+function cree_yaml($sDirExport,$langue,$secteur)
+{
+				$yaml = recuperer_fond("yaml/index", array('id_rubrique' => $secteur,'lang' => $langue,));
+				file_put_contents($sDirExport.'liste.yaml',$yaml);
+}
+
+
+/**
+ * Export HTML/CHM
+ */
+function html_export($a,$num,$secteur,$langue)
+{
+	 $sDirExport = getDirExport();
+	 if ($num == 0)
+	{
+		// Rubrique parente
+		$t = recuperer_fond("chm/index", array('id_rubrique' => $secteur,'lang' => $langue,));
+		$rubriqueParent = $sDirExport."rubrique$secteur.html";
+		file_put_contents($rubriqueParent ,$t);
+		copy($rubriqueParent,$sDirExport.'index.html');
+
+		// Fichiers HHC, HHK et HHP
+		$t = recuperer_fond("hhc/index", array('id_rubrique' => $secteur,'lang' => $langue,));
+		file_put_contents($sDirExport.'chmlatex.hhc',$t);
+		$t = recuperer_fond("chm/hhk", array('id_rubrique' => $secteur,'lang' => $langue,));
+		file_put_contents($sDirExport.'chmlatex.hhk',$t);
+		$t = recuperer_fond("chm/hhp", array('id_rubrique' => $secteur,'lang' => $langue,));
+		file_put_contents($sDirExport.'chmlatex.hhp',$t);
+
+		// fichier chm/css.html : traitement des images
+		$t = recuperer_fond("chm/css");
+		$t = preg_replace_callback("#url\('(.*)'\);#iU",'html_image',$t);
+		file_put_contents($sDirExport.'chm.css',$t);
+	}
+
+	//Traitement des pages HTML contenues dans liste.yaml
+	$id = $a['id'];
+	if($a['type']=='article')
+	{
+		// traiter article article#ID_ARTICLE.html
+		$code = recuperer_fond("chm/article_content", array('id_article' => $a['id'],'lang' => $langue,));
+		$n = 'article'.$id;
+	}
+	else
+	{
+		// traiter rubrique rubrique#ID_RUBRIQUE.html
+		$code = recuperer_fond("chm/index", array('id_rubrique' => $a['id'],'lang' => $langue,));
+		$n = 'rubrique'.$id;
+	}
+
+	//Traitement des images
+	$code = preg_replace_callback('#<img.*src="(.*)".*>#isU','html_image',$code);
+	$code = preg_replace_callback("#<img.*src='(.*)'.*>#isU",'html_image',$code);
+
+	//Traitement des liens
+	$code = preg_replace_callback("#href='(.*?)'#i",'html_lien',$code);
+	$code = preg_replace_callback('#href="(.*?)"#i','html_lien',$code);
+
+	// Suppression des liens de téléchargement du plugin Coloration Code
+	$code = preg_replace('#<p class=\'download code_download\'>.*</p>#iU','',$code);
+
+	// Enregistrement du fichier HTML
+	file_put_contents("$sDirExport$n.html",$code);
+}
+
+
+/**
+ * Export LaTeX
+ */
+function tex_export($a,$num,$secteur,$langue)
+{
+	$sDirExport = getDirExport();
+	if ($num == 0)
+	{
+		// Document maître
+		$code = recuperer_fond("tex/index", array('id_rubrique' => $secteur,'lang' => $langue,));
+		file_put_contents($sDirExport.'chmlatex_'.$langue.'.tex',$code);
+
+		// 1ère de couverture
+		$code = recuperer_fond("tex/premiere", array('id_rubrique' => $secteur,'lang' => $langue,));
+		$code = tex_post_traitement($code);
+		file_put_contents($sDirExport.'inclus/premiere.tex',$code);
+
+		// 4ème de couverture
+		$code = recuperer_fond("tex/derniere", array('id_rubrique' => $secteur,'lang' => $langue,));
+		$code = tex_post_traitement($code);
+		file_put_contents($sDirExport.'inclus/derniere.tex',$code);
+	}
+
+	// Traitement des articles et rubriques
+	$id = $a['id'];
+	if($a['type']=='article')
+	{
+		$n = 'article'.$id;
+		//traiter article article#ID_ARTICLE.html
+		$code = recuperer_fond("tex/article_content", array('id_article' => $a['id'],'lang' => $langue,));
+	}
+	else
+	{
+		$n = 'rubrique'.$id;
+		//traiter rubrique rubrique#ID_RUBRIQUE.html
+		$code = recuperer_fond("tex/rubrique_content", array('id_rubrique' => $a['id'],'lang' => $langue,));
+	}
+	$code = tex_post_traitement($code);
+	file_put_contents($sDirExport."inclus/$n.tex",$code);
+}
+
+
+/**
+ * Suppression d'un dossier et de son contenu
+ * @param $dir Dossier à supprimer
+ * @return retourne TRUE en cas de succès ou FALSE si une erreur survient
+ */
+function delTree($dir)
+{
+	$files = array_diff(scandir($dir), array('.','..'));
+	foreach ($files as $file)
+		{
+		  (is_dir("$dir/$file")) ? delTree("$dir/$file") : unlink("$dir/$file");
+		}
+	return rmdir($dir);
+}
+
+
+/**
+ * Ajout récursif d'un dossier dans un fichier ZIP
+ * @param $folder
+ * @param $zipFile
+ * @param $exclusiveLength
+ */
+function folderToZip($folder, &$zipFile, $exclusiveLength)
+{
+	$handle = opendir($folder);
+
+	while (false !== $f = readdir($handle)) {
+	  if ($f != '.' && $f != '..') {
+		$filePath = "$folder/$f";
+		// Remove prefix from file path before add to zip.
+		$localPath = substr($filePath, $exclusiveLength);
+		if (is_file($filePath)) {
+		  $zipFile->addFile($filePath, $localPath);
+		} elseif (is_dir($filePath)) {
+		  // Add sub-directory.
+		  $zipFile->addEmptyDir($localPath);
+		  folderToZip($filePath, $zipFile, $exclusiveLength);
+		}
+	  }
+	}
+	closedir($handle);
+}
+
+
+/**
+ * Création d'un fichier ZIP à partir d'un dossier
+ * @param $sourcePath Dossier à compresser
+ * @param $outZipPath Nom du fichier ZIP
+ */
+function zipDir($sourcePath, $outZipPath)
+{
+	$pathInfo = pathInfo($sourcePath);
+	$parentPath = $pathInfo['dirname'];
+	$dirName = $pathInfo['basename'];
+	$z = new ZipArchive();
+	$z->open($outZipPath, ZIPARCHIVE::CREATE);
+	$z->addEmptyDir($dirName);
+	folderToZip($sourcePath, $z, strlen("$parentPath/"));
+	$z->close();
+}
+
+?>
