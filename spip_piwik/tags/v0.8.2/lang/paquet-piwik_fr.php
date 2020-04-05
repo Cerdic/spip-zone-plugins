ae1e88085da2a2e6920752d65827729854759470
Salvatore <Salvatore@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-05 16:05:03 +0000

    Tag '/tags/v0.8.2' created from /trunk:121934
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123933 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ spip_piwik/tags/v0.8.2/lang/paquet-piwik_fr.php
@@ -0,0 +1,13 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans https://git.spip.net/spip-contrib-extensions/spip_piwik.git
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// P
+	'piwik_description' => 'Ajoute le script de statistiques Piwik (analyseur de trafic web) sur les pages du site.',
+	'piwik_slogan' => 'Interfacer Spip et Piwik'
+);
