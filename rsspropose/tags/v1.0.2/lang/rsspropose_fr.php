ea08049a2d233cfc26e731cf1076107537094065
yves.costiou@gmail.com <yves.costiou@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-05 16:05:03 +0000

    Tag '/tags/v1.0.2' created from /trunk:104101
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123867 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ rsspropose/tags/v1.0.2/lang/rsspropose_fr.php
@@ -0,0 +1,26 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	
+	'cfg_titre_parametrages' => 'Paramètres du plugin',
+	'choixobjets' => 'choisir les objets à intégrer au flux RSS',
+	
+	// L
+	
+	'legende' =>'Clé de sécurité',
+	
+	// T
+	
+	'titre_page_configurer_rsspropose' => 'Un flux RSS pour suivre les validations',
+	'token' => 'Indiquez la clé de votre choix',
+	
+	
+);
