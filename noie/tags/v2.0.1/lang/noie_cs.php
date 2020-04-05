02d88e153cf5f1d822f019b22f536cda13a7c0a9
Salvatore <Salvatore@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-05 16:05:03 +0000

    Tag '/tags/v2.0.1' created from /trunk:122132
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123738 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ noie/tags/v2.0.1/lang/noie_cs.php
@@ -0,0 +1,17 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/noie?lang_cible=cs
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// T
+	'toocool_alt' => 'Příliš cool pro IE',
+	'toocool_info' => 'Web, který si chcete prohlížet, je označen "Too Cool for Internet Explorer".
+<br />Prosím najděte si chvilku na vyzkoušení některého z oblíbených prohlížečů: <a href="http://www.w3junkies.com/toocool/index.php?language=en">http://www.w3junkies.com/toocool/</a>',
+	'toocool_titre' => 'Příliš cool pro Internet Explorer'
+);
+
+?>
---
+++ noie/tags/v2.0.1/lang/noie_pt.php
@@ -0,0 +1,17 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/noie?lang_cible=pt
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// T
+	'toocool_alt' => 'Too Cool for IE',
+	'toocool_info' => 'O site que você está tentando visitar foi classificado como "Bom Demais para o Internet Explorer".
+<br />Por favor, experimente um destes incríveis Browsers : <a href="http://www.w3junkies.com/toocool/index.php?language=br">http://www.w3junkies.com/toocool/</a>',
+	'toocool_titre' => 'Bom Demais para o Internet Explorer'
+);
+
+?>
