a9ff831ae04f462bfde9b83ced1ea32421596e57
abelass@gmail.com <abelass@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-05 16:05:03 +0000

    Tag '/tags/v1.0.16' created from /branches/0:111979
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123726 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ multilingue/tags/v1.0.16/lang/multilingue_de.php
@@ -0,0 +1,33 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/multilingue?lang_cible=de
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'afficher_menu_select' => 'Das Dropdown Menü (select) benutzen',
+
+	// D
+	'desactiver_traduction_logo_objet' => 'Die Möglichkeit deaktivieren, dass der Tag #LOGO_OBJET dass Logo des Ursprungsobjekts einfügt, wenn kein Objektlogo vorhanden ist.',
+
+	// E
+	'explication_desactiver_traduction_logo_objet' => 'Wenn ein Objekt eine Übersetzung ist (Vorhandensein einer id_trad wie bei Artikeln und Rubriken) lässt dieses Plugin z.B. den  Tag #LOGO_ARTICLE nach dem Logo des Referenzartikels suchen, wenn kein eigenes Logo vorhanden ist.',
+
+	// L
+	'logo_objet' => 'Objektlogo',
+
+	// M
+	'menu_langue' => 'Sprachmenü',
+	'multilingue_titre' => 'Mehrsprachige Webseite Leichtgemacht',
+
+	// P
+	'precision_menu_select' => 'Funktioniert nur wenn Sie die ursprünglichen INCLUREs des Plugins benutzen',
+
+	// T
+	'titre_page_configurer_multilingue' => 'Einstellungen von Mehrsprachige Webseite Leichtgemacht'
+);
