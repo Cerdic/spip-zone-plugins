d7043676b86484802555b1d43b07308d866d3c75
spip.franck@lien-d-amis.net <spip.franck@lien-d-amis.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-05 16:05:03 +0000

    Tag '/tags/v0.7.4' created from /branches/v0:104336
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123890 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ selecteur_generique/tags/v0.7.4/lang/selecteurgenerique_en.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/selecteurgenerique?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// P
+	'pas_suggestion' => 'No suggestions'
+);
+
+?>
---
+++ selecteur_generique/tags/v0.7.4/lang/selecteurgenerique_sk.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/selecteurgenerique?lang_cible=sk
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// P
+	'pas_suggestion' => 'Bez návrhov'
+);
+
+?>
