36e3aef48301570bad98cf167fc8e01a30b8a834
maieul@maieul.net <maieul@maieul.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-05 16:05:03 +0000

    Tag '/tags/v1.4.2' created from /branches/v1:118799
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123965 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ tablesorter/tags/v1.4.2/lang/paquet-tablesorter_fr.php
@@ -0,0 +1,13 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/tablesorter/lang/
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// T
+	'tablesorter_description' => 'Ce plugin permet de trier les tableaux portant la class CSS "spip" en cliquant simplement sur l’entête d’une colonne (le tableau HTML doit donc posséder les tags thead et tbody). Voir en ligne toutes les options de tri, sur le site de [->http://tablesorter.com].',
+	'tablesorter_slogan' => 'Avec tableSorter, le tri sera top !'
+);
---
+++ tablesorter/tags/v1.4.2/tablesorter_pipelines.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * Pipelines du plugin Tabelesorter
+ *
+ * @plugin     Tablesorter
+ * @licence    GNU/GPL v3
+ * @package    SPIP\Tablesorter\Pipelines
+ */
+if (!defined("_ECRIRE_INC_VERSION")) {
+	return;
+}
+
+/**
+ * Insertion dans le pipeline insert_head_css (SPIP)
+ *
+ * Ajout de la feuille de style dans le head
+ *
+ * @pipeline insert_head_css
+ * @param string $flux Le contenu de la balise #INSERT_HEAD_CSS
+ * @return string $flux Le contenu complété de la balise
+ */
+function tablesorter_insert_head_css($flux) {
+	static $done = false;
+	if (!$done) {
+		$done = true;
+		$flux .= '<link rel="stylesheet" href="' . direction_css(find_in_path('css/tablesorter.css')) . '" type="text/css" />';
+	}
+
+	return $flux;
+}
+
+/**
+ * Insertion dans le pipeline insert_head (SPIP)
+ *
+ * Ajout de la librairie js tablesorter et du script d'init
+ * dans le head de l'espace public
+ * Ajout également, pour les anciennes versions de SPIP de la CSS
+ * dans le head
+ *
+ * @pipeline insert_head
+ * @param string $flux Le contenu de la balise #INSERT_HEAD
+ * @return string $flux Le contenu complété de la balise
+ */
+function tablesorter_insert_head($flux) {
+	$flux .= '<script src="' . find_in_path('javascript/jquery.tablesorter.js') . '" type="text/javascript"></script>';
+	$flux .= '
+	<script type="text/javascript">/* <![CDATA[ */
+	(function($){
+		$(function(){
+			var tablesorter_init = function(){
+				$("table.spip").not(".ss_tablesort").each(function(){
+					var options = {};
+					if($(this).find("th.ts_disabled").size() >= 1){
+						options.headers = {};
+						$(this).find("th").each(function(index,value){
+							if($(this).is(".ts_disabled"))
+								options.headers[index] = {sorter : false}; 
+						});
+					}
+					$(this).tablesorter(options);
+				});
+			}
+			tablesorter_init();
+			onAjaxLoad(tablesorter_init);
+		});
+	})(jQuery);
+	/* ]]> */</script>';
+
+	$flux .= tablesorter_insert_head_css('');
+	// compat pour les vieux spip
+	return $flux;
+}
+
+/**
+ * Insertion dans le pipeline header_prive (SPIP)
+ *
+ * Ajout de la librairie js tablesorter, du script d'init et de la CSS
+ * dans le head de l'espace privé
+ *
+ * @pipeline header_prive
+ * @param string $flux Le contenu du head de l'espace privé
+ * @return string $flux Le contenu complété du head
+ */
+function tablesorter_header_prive($flux) {
+	// Insertion des librairies js
+	$flux .= '<script src="' . find_in_path('javascript/jquery.tablesorter.js') . '" type="text/javascript"></script>';
+	// Inclusion des styles du plugin
+	$flux .= '<link rel="stylesheet" href="' . direction_css(find_in_path('css/tablesorter.css')) . '" type="text/css" />';
+	// Init de tablesorter
+	$flux .= '
+	<script type="text/javascript">/* <![CDATA[ */
+	(function($){
+		$(function(){
+			$("table.spip").tablesorter();
+		});
+	})(jQuery);
+	/* ]]> */</script>';
+	return $flux;
+}
+?>
