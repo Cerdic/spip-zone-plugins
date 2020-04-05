e7078ec3fd7c36781286c7fcaaf618744004ac97
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-05 16:05:03 +0000

    Tag '/tags/v0.1.6' created from /trunk:107766
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@124009 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ webfonts/tags/v0.1.6/webfonts_pipeline.php
@@ -0,0 +1,48 @@
+<?php
+/*
+ * Plugin xxx
+ * (c) 2009 xxx
+ * Distribue sous licence GPL
+ *
+ */
+
+function webfonts_insert_head_css($flux){
+	static $done = false;
+	if (!$done){
+		$fonts = $GLOBALS['meta']['googlefonts_api'];
+		$fonts = array_map('trim',explode("\n",$fonts));
+		$fonts = array_map('urldecode',$fonts); // passer les + en ' '
+
+		// version directe google font api
+		$fonts = array_map('urlencode',$fonts);
+		$fonts = implode('|',$fonts);
+		if (strlen($fonts)) {
+			$code = '<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family='.$fonts.'" id="webfonts" />';
+			// le placer avant les autres CSS du flux
+			if (($p = strpos($flux,"<link"))!==false)
+				$flux = substr_replace($flux,$code,$p,0);
+			// sinon a la fin
+			else
+				$flux .= $code;
+		}
+
+		// version loader js, mais qui genere une requete google api...
+		/*
+		$fonts = array_map('addslashes',$fonts);
+		$fonts = implode("', '",$fonts);
+		if (strlen($fonts)) {
+			$fonts = "'$fonts'";
+			$code = '<script src="'.find_in_path('javascript/webfont.js').'" id="webfonts"></script>'
+			."<script>WebFont.load({
+  google: {
+    families: [$fonts]
+  }
+});</script>";
+			$flux = $code.$flux; // on le place en premier !
+		}*/
+		$done = true;
+	}
+	return $flux;
+}
+
+
