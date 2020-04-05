287ac84984c9dc9818dc956529a2d9057b317edd
Salvatore <Salvatore@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-05 15:20:07 +0000

    Tag '/tags/v3.2.0' created from /trunk:122113
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123422 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ couleurs_spip/tags/v3.2.0/lang/paquet-couleurs_spip_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans https://git.spip.net/spip-contrib-extensions/couleurs_spip.git
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'couleurs_spip_description' => 'Ce plugin permet de modifier l’apparence de parties de texte. Pour mettre certains mots en couleur ou modifier leur taille ou toute autre caractéristique typographique, il suffit d’entourer ceux-ci de balises de type <<code>cs_rouge</code>><cs_rouge>mon texte rouge</cs><<code>/cs</code>>. 
+	En ajoutant des styles dans le fichier css/couleurs_spip.css - le recopier dans ce cas dans le dossier squelettes utilisé -, on peut ajouter autant de balise qu’on le désire.',
+	'couleurs_spip_slogan' => 'Des textes en couleur'
+);
