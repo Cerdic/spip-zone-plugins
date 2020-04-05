1eb40afe264a99904767c7c868c77306cbd1566b
eric@smellup.net <eric@smellup.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-05 16:05:03 +0000

    Tag '/tags/v1.0.4' created from /trunk:115065
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123998 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ uploadify/tags/v1.0.4/lang/uploadify_fr.php
@@ -0,0 +1,25 @@
+<?php
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	// A
+	'actuellement_lie' => 'Fichier(s) actuellement li&eacute;(s)',
+	
+	// F
+	'fichiers_joints' => 'Fichiers joints',
+	
+	// O
+	'option_groupe_description' => 'Upload de documents',
+	
+	// P
+	'parcourir' => 'Parcourir...',
+	
+	// S
+	'saisie_upload_titre' => 'Upload de documents',
+	'saisie_upload_explication' => 'Ajoute un bouton au formulaire permettant d\'uploader des documents',
+	
+	// T
+	"televerser" => "T&eacute;l&eacute;verser en lot ou t&eacute;l&eacute;verser de gros fichiers"
+	
+);
+
