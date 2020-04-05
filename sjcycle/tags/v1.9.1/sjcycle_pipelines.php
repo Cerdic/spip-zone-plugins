0022d677effc49cf0e1c33a6641a54071bf47d5c
spip.franck@lien-d-amis.net <spip.franck@lien-d-amis.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-05 16:05:03 +0000

    Tag '/tags/v1.9.1' created from /branches/v1:113106
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123907 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ sjcycle/tags/v1.9.1/sjcycle_pipelines.php
@@ -0,0 +1,33 @@
+<?php
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+include_spip('inc/config');
+
+function sjcycle_insert_head_css($flux){
+	static $done = false;
+	if (!$done) {
+		$done = true;
+		$conf_jcycle = lire_config('sjcycle');
+		if($conf_jcycle["tooltip"]) {
+			$flux .="\n".'<link rel="stylesheet" href="'.url_absolue(find_in_path('javascript/jquery.tooltip.css')).'" type="text/css" media="all" />';
+		}
+		$flux .= '<link rel="stylesheet" href="'.url_absolue(generer_url_public('sjcycle.css')).'" type="text/css" media="all" />';
+	}
+
+	return $flux;
+}
+
+function sjcycle_insert_head($flux){
+	$conf_jcycle = lire_config('sjcycle');
+	$flux .="\n".'<script src="'.url_absolue(find_in_path('javascript/jquery.cycle.all.js')).'" type="text/javascript"></script>';
+	if($conf_jcycle["tooltip"]) {
+		$flux .="\n".'<script src="'.url_absolue(find_in_path('javascript/jquery.tooltip.js')).'" type="text/javascript" charset="utf-8"></script>';
+	}
+
+	$flux .= sjcycle_insert_head_css(''); // compat pour les vieux spip
+
+	return $flux;
+}
+
+?>
