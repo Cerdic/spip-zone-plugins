475e7423382304610bbe565ecfd95b0602d707c3
real3t@gmail.com <real3t@gmail.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-05 16:05:03 +0000

    Tag '/tags/v0.2.1' created from /trunk:116305
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123976 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ timelineme/tags/v0.2.1/lang/paquet-timelineme_fr.php
@@ -0,0 +1,7 @@
+<?php
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	'timelineme_description' => 'Les articles d\'une rubrique alignés sur une frise des temps.',
+	'timelineme_slogan' => '',
+	
+);
---
+++ timelineme/tags/v0.2.1/lang/timelineme_fr.php
@@ -0,0 +1,8 @@
+<?php
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	//A
+	'voir_plus' => 'Voir plus',
+	'voir_moins' => 'Voir moins'
+	
+);
