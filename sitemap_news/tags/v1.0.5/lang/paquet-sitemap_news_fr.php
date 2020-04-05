4498d0909e4a2ee756e64056b7f2876f1bdd421e
erational@erational.org <erational@erational.org@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-05 16:05:03 +0000

    Tag '/tags/v1.0.5' created from /trunk:107989
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123905 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ sitemap_news/tags/v1.0.5/lang/paquet-sitemap_news_fr.php
@@ -0,0 +1,12 @@
+<?php
+
+// Sécurité
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	'sitemap_news_description' => 'Pour être indexé plus facilement dans Google News, il est préférable de fournir un fichier Sitemap dédié aux actualités, dont la documentation est disponible : http://support.google.com/news/publisher/bin/answer.py?hl=fr&answer=74288',
+	'sitemap_news_nom' => 'Sitemap d’actualités',
+	'sitemap_news_slogan' => 'Fournit un fichier Sitemap dédié aux actualités du site',
+);
