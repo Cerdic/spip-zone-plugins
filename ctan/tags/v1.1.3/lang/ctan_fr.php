bef645405acf4da939a131558f71e25acaff91ad
Maïeul Rouquette <Maeul Rouquette@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-05 15:20:07 +0000

    Tag '/tags/v1.1.3' created from /trunk:105353
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123435 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ ctan/tags/v1.1.3/lang/ctan_fr.php
@@ -0,0 +1,18 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-correction_liens_internes
+// Langue: fr
+// Date: 29-10-2013 17:24:58
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// C
+	'inactif' => "inactif",
+);
+?>
