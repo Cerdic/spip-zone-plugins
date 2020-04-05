7c1f6cda31d1072c45506d04864bc9dc89d06a0d
spip.franck@lien-d-amis.net <spip.franck@lien-d-amis.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-05 15:20:07 +0000

    Tag '/tags/v0.5.6' created from /branches/v1:103864
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123507 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ fonctions_images/tags/v0.5.6/fonctions_images_fonctions.php
@@ -0,0 +1,54 @@
+<?php
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+/**
+ * Si on est en 2.1 etc...
+ */
+if(is_array($GLOBALS['spip_matrice'])){
+	$GLOBALS['spip_matrice']['image_rgb2hsv'] = 'filtres/images_complements.php';
+	$GLOBALS['spip_matrice']['image_hsv2rgb'] = 'filtres/images_complements.php';
+	$GLOBALS['spip_matrice']['image_estampage_alpha'] = 'filtres/images_complements.php';
+	$GLOBALS['spip_matrice']['image_saturer'] = 'filtres/images_complements.php';
+	$GLOBALS['spip_matrice']['image_niveaux_gris_auto'] = 'filtres/images_complements.php';
+	$GLOBALS['spip_matrice']['image_podpod'] = 'filtres/images_complements.php';
+	$GLOBALS['spip_matrice']['image_courbe'] = 'filtres/images_complements.php';
+	$GLOBALS['spip_matrice']['image_float'] = 'filtres/images_complements.php';
+	$GLOBALS['spip_matrice']['image_contour_alpha'] = 'filtres/images_complements.php';
+	$GLOBALS['spip_matrice']['image_sincity'] = 'filtres/images_complements.php';
+	$GLOBALS['spip_matrice']['image_dispersion'] = 'filtres/images_complements.php';
+	$GLOBALS['spip_matrice']['image_rgb2hsl'] = 'filtres/images_complements.php';
+	$GLOBALS['spip_matrice']['image_hue2rgb'] = 'filtres/images_complements.php';
+	$GLOBALS['spip_matrice']['image_hsl2rgb'] = 'filtres/images_complements.php';
+	$GLOBALS['spip_matrice']['image_reflechir'] = 'filtres/images_complements.php';
+	$GLOBALS['spip_matrice']['image_negatif'] = 'filtres/images_complements.php';
+	$GLOBALS['spip_matrice']['image_monochrome'] = 'filtres/images_complements.php';
+	$GLOBALS['spip_matrice']['image_merge'] = 'filtres/images_complements.php';
+
+	$GLOBALS['spip_matrice']['couleur_chroma'] = 'filtres/couleurs_complements.php';
+	$GLOBALS['spip_matrice']['couleur_saturer'] = 'filtres/couleurs_complements.php';
+	$GLOBALS['spip_matrice']['couleur_tableau_chroma'] = 'filtres/couleurs_complements.php';
+	$GLOBALS['spip_matrice']['couleur_teinter'] = 'filtres/couleurs_complements.php';
+	$GLOBALS['spip_matrice']['couleur_inverserluminosite'] = 'filtres/couleurs_complements.php';
+	$GLOBALS['spip_matrice']['couleur_foncerluminosite'] = 'filtres/couleurs_complements.php';
+	$GLOBALS['spip_matrice']['couleur_eclaircirluminosite'] = 'filtres/couleurs_complements.php';
+	$GLOBALS['spip_matrice']['couleur_melanger'] = 'filtres/couleurs_complements.php';
+	$GLOBALS['spip_matrice']['couleur_hexa_to_dec'] = 'filtres/couleurs_complements.php';
+}
+else{
+	include_spip('filtres/images_complements');
+	include_spip('filtres/couleurs_complements');
+}
+
+/*
+ * autorise les filtres images sur les chemins.
+ * #CHEMIN{fichier}|en_image|image_sepia{14579c}
+ *
+ * Cette fonction est devenue inutile en 1.9.3 [10980]
+ *
+ */
+function en_image($url, $alt=''){
+	return 	"<img src='". $url ."' alt='". $alt ."' />";
+}
+
+?>
