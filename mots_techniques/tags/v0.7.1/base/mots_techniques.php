85a60795ff98116c3ff619082595301c8d7c2867
spip.franck@lien-d-amis.net <spip.franck@lien-d-amis.net@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-05 16:05:03 +0000

    Tag '/tags/v0.7.1' created from /branches/v0:103720
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@123720 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ mots_techniques/tags/v0.7.1/base/mots_techniques.php
@@ -0,0 +1,30 @@
+<?php
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+function mots_techniques_declarer_champs_extras($champs = array()){
+	// ajout du champ technique
+	$champs[] = new ChampExtra(array(
+		'table' => 'groupes_mot', // sur quelle table ?
+		'champ' => 'technique', // nom sql
+		'label' => 'motstechniques:info_mots_cles_techniques', // chaine de langue 'prefix:cle'
+		'precisions' => 'motstechniques:bouton_mots_cles_techniques', // chaine de langue 'prefix:cle'
+		'type' => 'oui-non', // type de saisie
+		'sql' => "varchar(15) NOT NULL DEFAULT ''", // declaration sql		
+	));
+	return $champs;
+}
+
+function mots_techniques_declarer_tables_principales($tables_principales){
+	// ajout de la jointure pour {technique=...} sur boucle MOT
+	$tables_principales['spip_mots']['join']["id_mot"] = "id_mot";
+	$tables_principales['spip_mots']['join']["id_groupe"] = "id_groupe";
+	return $tables_principales;
+}
+
+function mots_techniques_declarer_tables_interfaces($interface){
+	$interface['tables_jointures']['spip_mots'][] = 'groupes_mots';		
+	return $interface;
+}
+
+?>
