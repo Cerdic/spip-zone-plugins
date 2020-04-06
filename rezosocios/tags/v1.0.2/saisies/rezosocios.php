7e0c2e81515a4feef806ba59a1f7fc6ae16df50f
Charles Razack <Charles Razack@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-06 14:51:35 +0000

    Tag '/tags/v1.0.2' created from /trunk:124076
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@124077 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ rezosocios/tags/v1.0.2/saisies/rezosocios.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Fonctions spécifiques à une saisie
+ *
+ * @package SPIP\Saisies\rezosocio
+ */
+
+
+// Sécurité
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+
+/**
+ * Vérifie que la valeur postée
+ * correspond aux valeurs proposées lors de la config de valeur
+ * @param string $valeur la valeur postée
+ * @param array $description la description de la saisie
+ * @return bool true si valeur ok, false sinon,
+ */
+function rezosocios_valeurs_acceptables($valeur, $description) {
+
+	if (!empty($description['options']['multiple'])) {
+		include_spip('saisies/selection_multiple');
+		$fonction = selection_multiple_valeurs_acceptables();
+	} else {
+		include_spip('saisies/selection');
+		$fonction = selection_valeurs_acceptables();
+	}
+
+	return $fonction($valeur, $description);
+}
