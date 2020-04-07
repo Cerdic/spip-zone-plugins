032a3c7af5d535568cefe2cde8cedec3353c27fa
Salvatore <Salvatore@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-07 23:27:01 +0000

    [Salvatore] [source:lang/ abonnement] Export depuis https://trad.spip.net de la langue ja [Salvatore] [source:lang/ abonnement] Mise a jour du bilan depuis https://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@124087 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ abonnements/trunk/lang/abonnement_ja.php
@@ -0,0 +1,67 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/abonnement-abonnements?lang_cible=ja
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'ajouter_lien_abonnement' => 'この定期購読を追加',
+
+	// C
+	'champ_date_au_label' => '終了：',
+	'champ_date_debut_label' => 'この定期購読の開始日',
+	'champ_date_du_label' => '開始：',
+	'champ_date_echeance_label' => '直近の期限',
+	'champ_date_fin_allonger_label' => '終了日の修正が可能です。',
+	'champ_date_fin_label' => 'この定期購読の終了日',
+	'champ_dates_debut_label' => '定期購読の開始日',
+	'champ_dates_fin_label' => '定期購読の終了日',
+	'champ_id_abonnements_offre_label' => '定期購読プラン', # RELIRE
+	'champ_id_auteur_label' => '利用者', # RELIRE
+	'champ_notifier_statut_label' => '定期購読のステータス',
+
+	// E
+	'erreur_id_abonnements_offre' => '定期購読プランから選択してください', # RELIRE
+
+	// I
+	'icone_creer_abonnement' => '定期購読を申し込む',
+	'icone_modifier_abonnement' => 'この定期購読を変更する',
+	'icone_renouveler_abonnement' => 'この定期購読を更新する。',
+	'info_1_abonnement' => '1件の定期購読',
+	'info_1_abonnement_actif' => '1件の有効な定期購読',
+	'info_1_abonnement_inactif' => '1件の無効な定期購読', # RELIRE
+	'info_aucun_abonnement' => '定期購読はありません',
+	'info_aucun_abonnement_actif' => '有効な定期購読はありません',
+	'info_aucun_abonnement_inactif' => '無効な定期購読はありません', # RELIRE
+	'info_date_fin' => '終了日：@date@',
+	'info_nb_abonnements' => '@nb@ 件の定期購読',
+	'info_nb_abonnements_actifs' => '@nb@ 件の有効な定期購読',
+	'info_nb_abonnements_inactifs' => '@nb@ 件の無効な定期購読',
+	'info_numero_abbr' => '番号',
+	'info_numero_abbr_maj' => '番号',
+
+	// J
+	'job_desactivation' => '定期購読 @id@　を無効にする',
+
+	// L
+	'label_date_a_partir' => '開始：',
+	'label_date_depuis' => '開始
+開始：',
+	'label_dates' => '日付',
+	'label_duree' => '期間',
+	'label_montant' => '合計',
+
+	// N
+	'notification_echeance_chapo' => '<p>こんにちは @nom@さん,</p>',
+	'notification_echeance_sujet_jours_apres' => 'あなたの定期購読は @duree@ 日前に終了しています !',
+	'notification_echeance_sujet_jours_avant' => 'あなたの定期購読は あと@duree@日で終了します !',
+	'notification_echeance_sujet_jours_pendant' => 'あなたの定期購読は今日終了します !',
+	'notification_echeance_sujet_mois_apres' => 'あなたの定期購読は @duree@ か月前に終了しました !',
+	'notification_echeance_sujet_mois_avant' => 'あなたの定期購読はあと@duree@か月で終了します !',
+	'notification_echeance_sujet_mois_pendant' => 'あなたの定期購読は今月で終了します !'
+);
