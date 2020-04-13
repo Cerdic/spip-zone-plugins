5a7191af45e36d350d8c7c1ce1b92342ee831ebb
Salvatore <Salvatore@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-13 23:27:14 +0000

    [Salvatore] [source:lang/ abonnementsoffre] Export depuis https://trad.spip.net de la langue ja [Salvatore] [source:lang/ abonnementsoffre] Mise a jour du bilan depuis https://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@124172 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ abonnements/trunk/lang/abonnementsoffre_ja.php
@@ -0,0 +1,60 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/abonnementsoffre-abonnements?lang_cible=ja
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'ajouter_lien_abonnementsoffre' => 'この定期購読プランを追加する',
+
+	// B
+	'bouton_annuler' => '取り消し',
+	'bouton_notifier' => '通知する',
+	'bouton_verifier' => '確認する',
+
+	// C
+	'champ_ajouter_notification_label' => '通知を追加する',
+	'champ_descriptif_label' => '説明', # RELIRE
+	'champ_duree_0' => '無期限',
+	'champ_duree_label' => '期間',
+	'champ_immateriel_label' => '無形',
+	'champ_immateriel_label_case' => 'このプランは無形サービスです',
+	'champ_periode_choix_heures' => '時間',
+	'champ_periode_choix_jours' => '日',
+	'champ_periode_choix_mois' => '月',
+	'champ_periode_label' => '期間のタイプ',
+	'champ_periode_nb_heures' => '@nb@ 時間',
+	'champ_periode_nb_jours' => '@nb@ 日',
+	'champ_periode_nb_mois' => '@nb@ か月',
+	'champ_prix_0' => '無料',
+	'champ_prix_ht_label' => '税抜き価格',
+	'champ_prix_label' => '価格',
+	'champ_prix_ttc_label' => '税込み価格',
+	'champ_quand_choix_apres' => '後',
+	'champ_quand_choix_avant' => '前',
+	'champ_quand_choix_pendant' => '当日',
+	'champ_quand_label' => 'いつ？',
+	'champ_taxe_label' => '消費税 (%)',
+	'champ_titre_label' => 'タイトル', # RELIRE
+	'configurer_notifications' => '通知を設定する',
+	'configurer_notifications_quand' => 'いつ ?',
+
+	// E
+	'erreur_notification_doublon' => 'この通知は既に登録されています',
+	'explication_configurer_notifications_abonnementsoffre' => '通知の自動送信',
+	'explication_envoyer_notifications_abonnementsoffre' => '通知を今すぐに送信',
+
+	// I
+	'icone_creer_abonnementsoffre' => '定期購読プランを作成',
+	'icone_modifier_abonnementsoffre' => 'この定期購読プランを修正する',
+	'info_1_abonnementsoffre' => '1件の定期購読プラン',
+	'info_1_an' => '1 年',
+	'info_1_heure' => '1 時間',
+	'info_1_jour' => '1 日',
+	'info_1_mois' => '1ヶ月'
+);
