d972fda84e8d773371b14a6aba3d81e8e12e0b14
Cerdic <Cerdic@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-19 21:39:48 +0000

    Tag '/tags/v2.2.3' created from /trunk:122827
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@124261 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ scssphp/tags/v2.2.3/lang/paquet-scssphp_fr.php
@@ -0,0 +1,13 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans https://git.spip.net/spip-contrib-extensions/scssphp.git
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// S
+	'scssphp_description' => 'Intégration de l’implémentation php de SCSS [->https://scssphp.github.io/scssphp/]. Il faut avoir au minimum PHP 5.6 pour le bon fonctionnement de cette intégration.',
+	'scssphp_titre' => 'SCSS PHP'
+);
