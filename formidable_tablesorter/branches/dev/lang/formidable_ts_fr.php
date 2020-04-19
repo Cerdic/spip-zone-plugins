075727e2bd534e22001e6d6401e993331ee1b46e
Maïeul Rouquette <Maeul Rouquette@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-19 14:11:59 +0000

    un branche de dev pour ajouter de nouvelles fonctionnalités
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@124245 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ formidable_tablesorter/branches/dev/lang/formidable_ts_fr.php
@@ -0,0 +1,27 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans https://git.spip.net/spip-contrib-extensions/formidable_tablesorter.git
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'colonnes' => 'Choix des colonnes',
+
+	// E
+	'exporter' => 'Exporter',
+
+	// I
+	'imprimer' => 'Imprimer le tableau',
+
+	// N
+	'nb_lignes' => 'Nombre de lignes affichées :',
+
+	// R
+	'reset' => 'Réinitialiser les fitlres',
+
+	// T
+	'tableau_reponses' => 'Tableau des réponses'
+);
