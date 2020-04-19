fc9ac55eb70ac170cb2d303636d5a054debc4f6c
Maïeul Rouquette <Maeul Rouquette@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-19 20:32:21 +0000

    Pouvoir modifier les champx extras associés à une réponse
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@124256 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ formidable/branches/issue17/afficher_diff/formulaire_saisies.php
@@ -0,0 +1,23 @@
+<?php
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+
+/**
+ * Afficher le diff des saisies de formulaire
+ *
+ * @param string $champ
+ * @param string $old
+ * @param string $new
+ * @param string $format
+ *   apercu, diff ou complet
+ * @return string
+ */
+function afficher_diff_formulaire_saisies($champ, $old, $new, $format = 'diff') {
+	$afficher_diff = charger_fonction('array_serialized', 'afficher_diff', true);
+	return $afficher_diff($champ, $old, $new, 'complet');
+}
+
+
