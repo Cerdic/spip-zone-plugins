33f999956a88e193187c0e8c5cad23ede605f08f
Salvatore <Salvatore@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-20 17:50:03 +0000

    [Salvatore] [source:lang/ paquet-massicot] Export depuis https://trad.spip.net de la langue pt_br [Salvatore] [source:lang/ paquet-massicot] Mise a jour du bilan depuis https://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@124270 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ massicot/trunk/lang/paquet-massicot_pt_br.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-massicot?lang_cible=pt_br
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// M
+	'massicot_description' => 'Este plugin permite modificar o tamanho e e reenquadrar os logotipos e as imagens diretamente no espaço privado de SPIP. ',
+	'massicot_nom' => 'Massicot',
+	'massicot_slogan' => 'Reenquadre suas imagens no SPIP!'
+);
