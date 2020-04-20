405300a1bce5bcb085c3c38dde83d205a3b9a67c
tcharlss <tcharlss@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-20 16:48:32 +0000

    Ne PAS prendre la taxe par défaut dans la fonction de prix non plus. Re up de z
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@124266 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- produits/trunk/prix/produit.php
+++ produits/trunk/prix/produit.php
@@ -3,21 +3,16 @@
 // Sécurité
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
-function prix_produit_dist($id_objet, $prix_ht){
+function prix_produit_dist($id_objet, $prix_ht) {
 	$prix = $prix_ht;
-	
+
 	// S'il y a une taxe de définie explicitement dans le produit, on applique en priorité
 	if (($id_produit = intval($id_objet)) > 0
 		and include_spip('base/abstract_sql')
 		and ($taxe = floatval(sql_getfetsel('taxe', 'spip_produits', 'id_produit = '.$id_produit))) !== 0
-	){
+	) {
 		$prix += $prix*$taxe;
 	}
-	// Sinon on applique la taxe par défaut
-	else{
-		include_spip('inc/config');
-		$prix += $prix*lire_config('produits/taxe', 0);
-	}
-	
+
 	return $prix;
 }
