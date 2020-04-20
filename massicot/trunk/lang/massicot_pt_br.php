83819100a552a981f7f11afbb5fd19b7d7e7d00f
Salvatore <Salvatore@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-20 17:45:02 +0000

    [Salvatore] [source:lang/ massicot] Export depuis https://trad.spip.net de la langue pt_br [Salvatore] [source:lang/ massicot] Mise a jour du bilan depuis https://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@124269 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ massicot/trunk/lang/massicot_pt_br.php
@@ -0,0 +1,30 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/massicot?lang_cible=pt_br
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// E
+	'erreur_image_trop_petite' => 'Esta imagem é muito pequena para este formato.',
+	'erreur_parametre_manquant' => 'O parâmetro @parametre@ é obrigatório!',
+
+	// L
+	'label_annuler' => 'Anular',
+	'label_dimensions' => 'Tamanho da imagem reenquadrada em pixels:',
+	'label_format' => 'dimensões predefinidas:',
+
+	// M
+	'massicot_titre' => 'Massicot',
+	'massicoter' => 'Reenquadrar a imagem',
+
+	// R
+	'reinitialiser' => 'Reinicializar',
+
+	// Z
+	'zoom' => 'Zoom'
+);
