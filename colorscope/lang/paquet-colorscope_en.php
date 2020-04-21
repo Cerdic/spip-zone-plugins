441f2d1e62f65bc53f10192780809853d1ace1ee
Salvatore <Salvatore@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-21 23:27:02 +0000

    [Salvatore] [source:lang/ paquet-colorscope] Export depuis https://trad.spip.net de la langue en [Salvatore] [source:lang/ paquet-colorscope] Export depuis https://trad.spip.net de la langue fr [Salvatore] [source:lang/ paquet-colorscope] Mise a jour du bilan depuis https://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@124292 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- colorscope/lang/paquet-colorscope_en.php
+++ colorscope/lang/paquet-colorscope_en.php
@@ -1,19 +1,15 @@
 <?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/colorscope-paquet-xml-colorscope?lang_cible=en
+// ** ne pas modifier le fichier **
 
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-// Fichier produit par PlugOnet
-// Module: paquet-colorscope
-// Langue: en
-// Date: 29-03-2020 16:13:57
-// Items: 2
-
-if (!defined('_ECRIRE_INC_VERSION')) return;
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// C
 	'colorscope_description' => 'Replaces hexadecimal color codes in a SPIP text, with a bloc with the relevant color in background, mentionning its hexa code.',
-	'colorscope_slogan' => 'Give colours to hex codes',
+	'colorscope_slogan' => 'Give colours to hex codes'
 );
-
