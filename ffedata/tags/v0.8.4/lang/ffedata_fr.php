df3e9af7a16fa125e91f0dadad360af5eab4a306
Jack31 <Jack31@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-24 09:27:56 +0000

    Tag '/tags/v0.8.4' created from /trunk:124310
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@124311 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ ffedata/tags/v0.8.4/lang/ffedata_fr.php
@@ -0,0 +1,28 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+ 
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'codes_equipes_club'=> 'Tous les codes utiles pour les modèles "équipes"',	
+	// E
+	'evolution_elo'=> 'Evolution du classement',
+	// F
+	'ffedata_club'=> 'Club',
+	'ffedata_club_explication' => 'Le numéro du club dans la base FFE',
+	'ffedata_saison' => 'Saison',
+	'ffedata_saison_explication' => 'Saison demandée : pour la saison 2014-2015 saisir 2015',
+	'ffedata_titre' => 'Data FFE',
+	'ffedata_departement'=>'Département',
+	'ffedata_saisir_code_departement'=>'saisir le numéro du département',
+	'ffedata_donnees_formulaire'=>'les données du webservice dans formidable pour l’inscription',
+	// S
+	'saisie_nrffe_titre' => 'Numéro de licence FFE',
+	'saisie_nrffe_explication' => 'Le numéro de licence',	
+	// T
+	'titre_page_informations_ffedata' => 'Informations utiles pour utiliser FFE Data',
+	'titre_informations_equipes_clubs' => 'Les informations sur les équipes d’un club',
+	'titre_informations_clubs_departement'=>'La liste des clubs d’un département',
+);
---
+++ ffedata/tags/v0.8.4/lang/paquet-ffedata_fr.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// F
+	'ffedata_description' => 'Ce plugin permet de récupérer les informations stockées sur le webservice de la FFE
+	exemples :
+- pour le modèle ffe_joueurs : &lt;ffe_joueurs|Ref=XXXX&gt; ou XXXX est le code du club recherché.
+- pour le modèle ffe_liste_clubs : &lt;ffe_liste_clubs|Ref=XX&gt; ou XX est le numéro du département recherché.
+	',
+	'ffedata_nom' => 'Data FFE',
+	'ffedata_slogan' => 'Les infos du webservice de la Fédération Française des Echecs',
+);
