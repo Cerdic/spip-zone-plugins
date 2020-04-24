644f57d210fe8aa7c0ea025d6f2e70bf92d9c3f8
Maïeul Rouquette <Maeul Rouquette@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-24 14:10:54 +0000

    Merge branch 'issue21' into 4.3.0RC
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@124318 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ formidable/branches/4.3.0RC/lang/formulaire_ru.php
@@ -0,0 +1,22 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/formulaire?lang_cible=ru
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// I
+	'icone_creer_formulaire' => 'Создать новую форму',
+
+	// M
+	'modifier_formulaire' => 'Изменить форму',
+
+	// T
+	'titre_formulaire' => 'Форма',
+	'titre_formulaires' => 'Формы',
+	'titre_logo_formulaire' => 'Логотип формы'
+);
