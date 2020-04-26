bd796e9ad5477a2334a167e00da1a1a5c3552d84
Salvatore <Salvatore@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-26 18:35:43 +0000

    Tag '/tags/v3.1.0' created from /trunk:121817
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@124342 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ player/tags/v3.1.0/lang/player_en.php
@@ -0,0 +1,120 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/player?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// _
+	'_blank' => 'New window',
+	'_self' => 'Current window',
+
+	// A
+	'always' => 'Always',
+	'auto' => 'Auto-hide',
+	'autohide' => 'Auto-hide',
+
+	// B
+	'blogo_vous_ecoutez' => 'You are now listening:',
+
+	// C
+	'configuration_player' => 'Configure Multimedia Player',
+
+	// L
+	'label_autoload' => 'Load and show the starting picture',
+	'label_autonext' => 'Autoplay next video',
+	'label_autoplay' => 'Autoplay',
+	'label_bgcolor' => 'Background color: ',
+	'label_bgcolor1' => 'First gradient color: ',
+	'label_bgcolor2' => 'Second gradient color: ',
+	'label_buffer' => 'Buffering time in seconds: ',
+	'label_buffermessage' => 'Buffering message: ',
+	'label_buttoncolor' => 'Buttons color: ',
+	'label_buttonovercolor' => 'Buttons-hover color: ',
+	'label_config' => 'URL of Configuration text file: ',
+	'label_configxml' => 'URL of Configuration XML file: ',
+	'label_currentflvcolor' => 'Selected video color: ',
+	'label_flv' => 'URLs of video files to load: ',
+	'label_height' => 'Force player height: ',
+	'label_iconplaybgalpha' => 'PLAY icon background transparency: ',
+	'label_iconplaybgcolor' => 'PLAY icon background color: ',
+	'label_iconplaycolor' => 'PLAY icon color: ',
+	'label_insertion_auto' => 'Automatic insert',
+	'label_insertion_auto_inline_mini' => 'Add a mini-player befor each .mp3 link',
+	'label_insertion_auto_player_end' => 'Add a player at the en of the text for each MP3 link',
+	'label_loadingcolor' => 'Loadbar color: ',
+	'label_loop' => 'Loop',
+	'label_margin' => 'Video margins: ',
+	'label_onclick' => 'Action-click on the video (’none’, ’playpause’, or destination URL): ',
+	'label_onclicktarget' => 'URL’s target on click: ',
+	'label_ondoubleclick' => 'Double-click action on video (’none’, ’fullscreen’, ’playpause’, or URL to open): ',
+	'label_ondoubleclicktarget' => 'URL’s target on double-click: ',
+	'label_player_mp3' => 'Player’s choice',
+	'label_player_mp3_dewplayer' => 'Dew player',
+	'label_player_mp3_eraplayer' => 'Erational player',
+	'label_player_mp3_mejs' => 'Media Element Player',
+	'label_player_mp3_neoplayer' => 'Neolao player',
+	'label_player_mp3_pixplayer' => 'One pixel out player',
+	'label_playeralpha' => 'Player background transparency: ',
+	'label_playercolor' => 'Player color: ',
+	'label_playertimeout' => 'Time (in seconds) before player hides: ',
+	'label_playlisttextcolor' => 'Playlist font-color: ',
+	'label_playonload' => 'Play video on load',
+	'label_scrollbarcolor' => 'Scrollbar color: ',
+	'label_scrollbarovercolor' => 'Scrollbar-hover color: ',
+	'label_scrollbarsize' => 'Scrollbar dimensions: ',
+	'label_shortcut' => 'Activate keyborad shortcuts',
+	'label_showfullscreen' => 'Show FULLSCREEN button',
+	'label_showiconplay' => 'Show PLAY icon on video',
+	'label_showmouse' => 'Show mouse pointer: ',
+	'label_shownext' => 'Show NEXT button',
+	'label_showopen' => 'Show OPEN button (2 for the list): ',
+	'label_showplayer' => 'Show buttons bar: ',
+	'label_showprevious' => 'Show PREVIOUS button',
+	'label_showstop' => 'Show STOP button',
+	'label_showswitchsubtitles' => 'Display HIDE/SHOW SUBTITLES button',
+	'label_showtime' => 'Show TIME button: ',
+	'label_showtime_0' => 'Hide',
+	'label_showtime_1' => 'Show button',
+	'label_showtime_2' => 'Show remaining time',
+	'label_showtitleandstartimage' => 'Display title and starting picture in the same time',
+	'label_showtitlebackground' => 'Show title background: ',
+	'label_showvolume' => 'Show VOLUME button',
+	'label_shuffle' => 'Random playing',
+	'label_skin' => 'URL of jpeg (non-progressive) file to load: ',
+	'label_slidercolor1' => 'First gradient color on the bar: ',
+	'label_slidercolor2' => 'Second gradient color on the bar: ',
+	'label_sliderovercolor' => 'Bar-hover color: ',
+	'label_srt' => 'Use SRT subtitles',
+	'label_srtbgcolor' => 'Subtitles background-color: ',
+	'label_srtcolor' => 'Subtitles font-color: ',
+	'label_srtsize' => 'Subtitles font-size: ',
+	'label_startimage' => 'Title image(s):',
+	'label_title' => 'Title(s): ',
+	'label_titlecolor' => 'Title font-color: ',
+	'label_titlesize' => 'Title font-size: ',
+	'label_top1' => 'Pictures over video: ',
+	'label_videobgcolor' => 'Background color when no video: ',
+	'label_videodelay' => 'Title display time on video change: ',
+	'label_volume' => 'Initial volume level: ',
+	'label_width' => 'Force player width: ',
+
+	// M
+	'maxi' => 'Maxi',
+	'mini' => 'Mini',
+	'multi' => 'Multi',
+
+	// N
+	'never' => 'never',
+	'normal' => 'Normal',
+
+	// P
+	'pas_de_lecteur_video' => 'No video playback capabilities.',
+	'player_audio' => 'Audio Player',
+	'player_nom' => 'Multimedia Player',
+	'player_video' => 'Video Player'
+);
