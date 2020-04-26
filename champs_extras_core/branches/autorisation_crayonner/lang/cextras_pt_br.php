347605d9a058da025609ee0acdc4799af94e41ac
Maïeul Rouquette <Maeul Rouquette@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-26 18:35:43 +0000

    vérifier le crayonnage sur un champ extra en fonction des restrictions propres au champ
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@124340 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ champs_extras_core/branches/autorisation_crayonner/lang/cextras_pt_br.php
@@ -0,0 +1,23 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/cextras?lang_cible=pt_br
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cextra_par_defaut' => 'Valor padrão',
+
+	// P
+	'pas_auteur' => 'sem autor',
+
+	// T
+	'type' => '@type@',
+
+	// Z
+	'zbug_balise_argument_non_texte' => 'O argumento @nb@ na tag @balise@ deve ser do tipo texto'
+);
