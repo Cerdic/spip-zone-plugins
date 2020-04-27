5fc2860c35b5dfeca61c2db4fbe60f796005780e
clem <clem@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-27 21:09:19 +0000

    éviter de tout casser : fournir l'ancien JS si la version de SPIP <= 3.0
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@124355 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- jquery_masonry/jquerymasonry_pipelines.php
+++ jquery_masonry/jquerymasonry_pipelines.php
@@ -2,6 +2,11 @@
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 function jquerymasonry_insert_head($flux){
+	// compat : jusqu'à SPIP 3.0 compris utiliser jquerymasonry v 2.1.0, après: la derniere maj (3.3.2)
+	include_spip('plugins/installer'); // spip_version_compare dans SPIP 3.x
+	include_spip('inc/plugin'); // spip_version_compare dans SPIP 2.x
+	$js_acharger = spip_version_compare('[2.1;3.0]', spip_version()) ? 'jquery.masonry.js' : 'jquery.masonry_2.1.0.js';
+	
 	$executer="";
 	$styles="";
 	include_spip('inc/config');
@@ -26,7 +31,7 @@ function jquerymasonry_insert_head($flux){
 
 	// S'il y a au moins un element
 	if($conf_jquerymasonry["nombre"]>0) {
-		$flux .= "\n".'<script src="'.url_absolue(find_in_path('javascript/jquery.masonry.js')).'" type="text/javascript"></script>';
+		$flux .= "\n".'<script src="'.url_absolue(find_in_path('javascript/'.$js_acharger)).'" type="text/javascript"></script>';
 		if($conf_jquerymasonry["multicolonne".$i] != "on") {
 			$flux .= "\n".'<style type="text/css">'.$styles.'</style>';
 		}
