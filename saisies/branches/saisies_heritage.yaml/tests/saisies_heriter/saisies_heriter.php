2037e62e7cc648c7170310c811377328ba5438ce
Maïeul Rouquette <Maeul Rouquette@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-28 17:45:45 +0000

    pour @Rastapoloulos : tests unitaires/exemple d'héritage sucessif, avec changement recursif des valeurs par défaut (en l'occurence, une option sur la vérification)
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@124383 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- saisies/branches/saisies_heritage.yaml/tests/saisies_heriter/saisies_heriter.php
+++ saisies/branches/saisies_heritage.yaml/tests/saisies_heriter/saisies_heriter.php
@@ -319,6 +319,293 @@
 				1 => 'toto',
 				2 => 'tests/saisies/'
 			),
+			'1' => array(
+				0 =>  array (
+					'titre' => 'titi',
+					'description' => 'titi_description',
+					'icone' => 'plugins/saisies/images/saisies_input.png',
+					'options' =>
+					array (
+						0 =>
+						array (
+							'saisie' => 'fieldset',
+							'options' =>
+							array (
+								'nom' => 'description',
+								'label' => '<:saisies:option_groupe_description:>',
+							),
+							'saisies' =>
+							array (
+								0 =>
+								array (
+									'saisie' => 'input',
+									'options' =>
+									array (
+										'nom' => 'label',
+										'label' => '<:saisies:option_label_label:>',
+										'explication' => '<:saisies:option_label_explication:>',
+										'size' => 50,
+									),
+								),
+								1 =>
+								array (
+									'options' =>
+									array (
+										'nom' => 'defaut',
+										'label' => 'toto',
+										'size' => 50,
+									),
+									'verifier' =>
+									array (
+										'type' => 'toto_verifier',
+										'options' =>
+										array (
+											'normaliser' => true,
+										),
+									),
+									'saisie' => 'toto',
+								),
+								2 =>
+								array (
+									'saisie' => 'input',
+									'options' =>
+									array (
+										'nom' => 'explication',
+										'label' => '<:saisies:option_explication_label:>',
+										'explication' => '<:saisies:option_explication_explication:>',
+										'size' => 50,
+									),
+								),
+							),
+						),
+						1 =>
+						array (
+							'saisie' => 'fieldset',
+							'options' =>
+							array (
+								'nom' => 'utilisation',
+								'label' => '<:saisies:option_groupe_utilisation:>',
+							),
+							'saisies' =>
+							array (
+								0 =>
+								array (
+									'saisie' => 'input',
+									'options' =>
+									array (
+										'nom' => 'maxlength',
+										'label' => '<:saisies:option_maxlength_label:>',
+										'explication' => '<:saisies:option_maxlength_explication:>',
+									),
+									'verifier' =>
+									array (
+										'type' => 'entier',
+										'options' =>
+										array (
+											'min' => 1,
+										),
+									),
+								),
+								1 =>
+								array (
+									'saisie' => 'case',
+									'options' =>
+									array (
+										'nom' => 'disable',
+										'label_case' => '<:saisies:option_disable_label:>',
+										'explication' => '<:saisies:option_disable_explication:>',
+									),
+								),
+								2 =>
+								array (
+									'saisie' => 'case',
+									'options' =>
+									array (
+										'nom' => 'disable_avec_post',
+										'label_case' => '<:saisies:option_disable_avec_post_label:>',
+										'explication' => '<:saisies:option_disable_avec_post_explication:>',
+									),
+								),
+								3 =>
+								array (
+									'saisie' => 'case',
+									'options' =>
+									array (
+										'nom' => 'readonly',
+										'label_case' => '<:saisies:option_readonly_label:>',
+										'explication' => '<:saisies:option_readonly_explication:>',
+									),
+								),
+							),
+						),
+						2 =>
+						array (
+							'saisie' => 'fieldset',
+							'options' =>
+							array (
+								'nom' => 'affichage',
+								'label' => '<:saisies:option_groupe_affichage:>',
+							),
+							'saisies' =>
+							array (
+								0 =>
+								array (
+									'saisie' => 'textarea',
+									'options' =>
+									array (
+										'nom' => 'afficher_si',
+										'label' => '<:saisies:option_afficher_si_label:>',
+										'explication' => '<:saisies:option_afficher_si_explication:>',
+										'rows' => 5,
+									),
+									'verifier' =>
+									array (
+										'type' => 'afficher_si',
+									),
+								),
+								1 =>
+								array (
+									'saisie' => 'case',
+									'options' =>
+									array (
+										'nom' => 'afficher_si_remplissage_uniquement',
+										'label' => '<:saisies:option_afficher_si_remplissage_uniquement_label:>',
+										'label_case' => '<:saisies:option_afficher_si_remplissage_uniquement_label_case:>',
+										'explication' => '<:saisies:option_afficher_si_remplissage_uniquement_explication:>',
+									),
+								),
+								2 =>
+								array (
+									'saisie' => 'input',
+									'options' =>
+									array (
+										'nom' => 'attention',
+										'label' => '<:saisies:option_attention_label:>',
+										'explication' => '<:saisies:option_attention_explication:>',
+										'size' => 50,
+									),
+								),
+								3 =>
+								array (
+									'saisie' => 'input',
+									'options' =>
+									array (
+										'nom' => 'class',
+										'label' => '<:saisies:option_class_label:>',
+										'size' => 50,
+									),
+								),
+								4 =>
+								array (
+									'saisie' => 'input',
+									'options' =>
+									array (
+										'nom' => 'conteneur_class',
+										'label' => '<:saisies:option_conteneur_class_label:>',
+										'size' => 50,
+									),
+								),
+								5 =>
+								array (
+									'saisie' => 'input',
+									'options' =>
+									array (
+										'nom' => 'size',
+										'label' => '<:saisies:option_size_label:>',
+										'explication' => '<:saisies:option_size_explication:>',
+									),
+									'verifier' =>
+									array (
+										'type' => 'entier',
+										'options' =>
+										array (
+											'min' => 1,
+										),
+									),
+								),
+								6 =>
+								array (
+									'saisie' => 'radio',
+									'options' =>
+									array (
+										'nom' => 'autocomplete',
+										'label' => '<:saisies:option_autocomplete_label:>',
+										'explication' => '<:saisies:option_autocomplete_explication:>',
+										'datas' =>
+										array (
+											'defaut' => '<:saisies:option_autocomplete_defaut:>',
+											'on' => '<:saisies:option_autocomplete_on:>',
+											'off' => '<:saisies:option_autocomplete_off:>',
+										),
+										'defaut' => 'defaut',
+									),
+								),
+							),
+						),
+						3 =>
+						array (
+							'saisie' => 'fieldset',
+							'options' =>
+							array (
+								'nom' => 'validation',
+								'label' => '<:saisies:option_groupe_validation:>',
+							),
+							'saisies' =>
+							array (
+								0 =>
+								array (
+									'saisie' => 'case',
+									'options' =>
+									array (
+										'nom' => 'obligatoire',
+										'label_case' => '<:saisies:option_obligatoire_label:>',
+									),
+								),
+								1 =>
+								array (
+									'saisie' => 'input',
+									'options' =>
+									array (
+										'nom' => 'info_obligatoire',
+										'label' => '<:saisies:option_info_obligatoire_label:>',
+										'explication' => '<:saisies:option_info_obligatoire_explication:>',
+									),
+								),
+								2 =>
+								array (
+									'saisie' => 'input',
+									'options' =>
+									array (
+										'nom' => 'erreur_obligatoire',
+										'label' => '<:saisies:option_erreur_obligatoire_label:>',
+										'explication' => '<:saisies:option_erreur_obligatoire_explication:>',
+									),
+								),
+							),
+						),
+					),
+					'defaut' =>
+					array (
+						'options' =>
+						array (
+							'label' => 'titi',
+							'size' => 40,
+							'sql' => 'BIGINT (255)',
+							'readonly' => 'on',
+						),
+						'verifier' =>
+						array (
+							'type' => 'toto',
+							'options' =>
+							array (
+								'normaliser' => false,
+							),
+						),
+					),
+				),
+				1 => 'titi',
+				2 => 'tests/saisies/'
+			),
 		);
 		return $essais;
 	}
