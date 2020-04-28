6d4357626ed39816bf27e66baa031a21bd6916fc
Salvatore <Salvatore@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-28 23:27:11 +0000

    [Salvatore] [source:lang/ profil] Export depuis https://trad.spip.net de la langue ja [Salvatore] [source:lang/ profil] Mise a jour du bilan depuis https://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@124390 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ profils/trunk/lang/profil_ja.php
@@ -0,0 +1,66 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/profil?lang_cible=ja
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'ajouter_lien_profil' => 'このプロファイルを追加する',
+
+	// C
+	'champ_config_activer_coordonnees_auteur_label_case' => 'ユーザーアカウントの連絡先を追加する',
+	'champ_config_activer_coordonnees_organisation_label_case' => '団体の連絡先を追加する',
+	'champ_config_activer_organisation_label_case' => '団体（法人）の情報を追加する',
+	'champ_config_auteur_caption' => 'ユーザーアカウントの情報',
+	'champ_config_colonne_inscription_label' => '登録', # RELIRE
+	'champ_config_colonne_obligatoire_label' => '必須',
+	'champ_config_coordonnees_auteur_caption' => 'ユーザーアカウントの連絡先',
+	'champ_config_coordonnees_choix_sans_type' => 'タイプなし',
+	'champ_config_coordonnees_organisation_caption' => '団体の連絡先',
+	'champ_config_coordonnees_type_label' => 'タイプ',
+	'champ_groupe_auteur_label' => 'ユーザーアカウント',
+	'champ_titre_label' => 'タイトル',
+	'comptes_liste_detaille' => '詳細リストを見る',
+	'confirmer_supprimer_profil' => 'このプロファイルを本当に削除しますか ?',
+
+	// E
+	'envoyer_notification_label_case' => 'パスワードを作成するように通知を送信する',
+	'exporter_titre' => 'エクスポート',
+
+	// I
+	'icone_creer_profil' => 'プロファイルを作成する',
+	'icone_modifier_profil' => 'このプロファイルを修正する',
+	'importer_titre' => 'インポート',
+	'info_1_profil' => 'プロファイル',
+	'info_1_profil_compte' => 'このプロファイルのアカウント',
+	'info_aucun_profil' => 'プロファイルなし',
+	'info_nb_profil_comptes' => 'このプロファイルの@nb@ 個のアカウント ',
+	'info_nb_profils' => '@nb@ 個のプロファイル',
+	'info_nb_profils_comptes' => 'このプロファイルのアカウント', # RELIRE
+
+	// N
+	'notification_motdepasse_titre' => '新規のアカウント',
+
+	// R
+	'retirer_lien_profil' => 'このプロファイルを取り消す',
+	'retirer_tous_liens_profils' => 'すべてのプロファイルを取り消す',
+
+	// S
+	'supprimer_profil' => 'このプロファイルを削除する',
+
+	// T
+	'texte_ajouter_profil' => 'プロファイルを追加する',
+	'texte_changer_statut_profil' => 'このプロファイルは :',
+	'texte_definir_comme_traduction_profil' => 'このプロファイルは次のプロファイル番号の翻訳です :',
+	'titre_langue_profil' => 'このプロファイルの使用言語',
+	'titre_logo_profil' => 'このプロファイルのロゴ',
+	'titre_profil' => 'プロファイル',
+	'titre_profils' => 'プロファイル',
+	'tri_croissant' => '昇順に並べる',
+	'tri_decroissant' => '降順に並べる'
+);
