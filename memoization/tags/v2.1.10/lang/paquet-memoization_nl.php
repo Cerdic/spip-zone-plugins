c1576489e47b56dfd23b8f0ad98b8517ca87c51b
Maïeul Rouquette <Maeul Rouquette@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-28 16:17:28 +0000

    Tag '/tags/v2.1.10' created from /trunk:124377
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@124378 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ memoization/tags/v2.1.10/lang/paquet-memoization_nl.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-memoization?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// M
+	'memoization_description' => 'Mémoïzation is het Franse woord voor memoïlering, een computerbegrip, waarvan Wikipedia de volgende definitie geeft:"een techniek die bestaat in het verkorten van de uitvoeringstijd van een functie door de resultaten van de ene naar de andere keer te onthouden". Het is ook de naam van een nieuwe functiebibliotheek voor PHP, die mijn experimenten op XCache opvolgt.',
+	'memoization_slogan' => 'Beheer van de cache van SPIP volgens diverse methodes.'
+);
