c1164a3de5f75ef933406aef36af409b8dbd13d8
Salvatore <Salvatore@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-29 23:27:38 +0000

    [Salvatore] [source:lang/ profil] Export depuis https://trad.spip.net de la langue ja [Salvatore] [source:lang/ profil] Mise a jour du bilan depuis https://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@124432 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- profils/trunk/lang/profil_ja.php
+++ profils/trunk/lang/profil_ja.php
@@ -13,23 +13,48 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'ajouter_lien_profil' => 'このプロファイルを追加する',
 
 	// C
+	'champ_config_activer_contact_explication_texte' => '団体の情報と個人連絡先情報の両方を有効にした場合、ユーザーアカウントに紐づけされる基本データは団体の情報となり、個人連絡先はその団体に紐づけされます。',
+	'champ_config_activer_contact_label_case' => '個人の連絡先情報を追加する',
 	'champ_config_activer_coordonnees_auteur_label_case' => 'ユーザーアカウントの連絡先を追加する',
+	'champ_config_activer_coordonnees_contact_label_case' => '個人の連絡先を追加する',
 	'champ_config_activer_coordonnees_organisation_label_case' => '団体の連絡先を追加する',
+	'champ_config_activer_groupe_contact_label' => '個人連絡先の欄をグループ化する',
+	'champ_config_activer_groupe_explication' => 'この欄に記入すると、そのタイトルでグループが作成されます。', # RELIRE
+	'champ_config_activer_groupe_organisation_label' => '団体の欄をグループ化する',
 	'champ_config_activer_organisation_label_case' => '団体（法人）の情報を追加する',
 	'champ_config_auteur_caption' => 'ユーザーアカウントの情報',
+	'champ_config_champ_label' => '関連する欄/題 ',
+	'champ_config_colonne_edition_label' => '編集',
 	'champ_config_colonne_inscription_label' => '登録', # RELIRE
 	'champ_config_colonne_obligatoire_label' => '必須',
+	'champ_config_contact_caption' => '個人連絡先情報',
 	'champ_config_coordonnees_auteur_caption' => 'ユーザーアカウントの連絡先',
 	'champ_config_coordonnees_choix_sans_type' => 'タイプなし',
+	'champ_config_coordonnees_contact_caption' => '個人連絡先情報',
+	'champ_config_coordonnees_explication' => '複数の連絡先を追加するには、少なくとも一つにチェックを入れなければなりません。必要ならば書式を変更してください。',
+	'champ_config_coordonnees_label_label' => '任意ラベル',
 	'champ_config_coordonnees_organisation_caption' => '団体の連絡先',
 	'champ_config_coordonnees_type_label' => 'タイプ',
+	'champ_config_id_annuaire_explication' => '複数の名簿の使用が有効になっています。名簿の選択がない場合は、プロファイルと同じ名前の名簿が作成されます。', # RELIRE
+	'champ_config_id_annuaire_label' => 'どの名簿',
+	'champ_config_objet_label' => '題', # RELIRE
+	'champ_config_organisation_caption' => '団体の情報',
 	'champ_groupe_auteur_label' => 'ユーザーアカウント',
+	'champ_groupe_contact_label' => '個人連絡先',
+	'champ_groupe_organisation_label' => '団体',
+	'champ_identifiant_label' => '識別子',
 	'champ_titre_label' => 'タイトル',
+	'comptes_aucun' => 'このプロファイルのアカウントはありません',
+	'comptes_creer' => 'このプロファイルのアカウントを新規作成する',
 	'comptes_liste_detaille' => '詳細リストを見る',
+	'comptes_modifier' => 'プロファイルのアカウントを修正する',
 	'confirmer_supprimer_profil' => 'このプロファイルを本当に削除しますか ?',
 
 	// E
 	'envoyer_notification_label_case' => 'パスワードを作成するように通知を送信する',
+	'erreur_email_obligatoire' => '必須メールアドレスを欄内か連絡先内で設定してください',
+	'errreur_identifiant_existant' => 'この識別子は既に使われています',
+	'exporter_champ_seulement_colonnes_label_case' => 'データはエクスポートせずに、列だけをエクスポート',
 	'exporter_titre' => 'エクスポート',
 
 	// I
@@ -42,8 +67,19 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'info_nb_profil_comptes' => 'このプロファイルの@nb@ 個のアカウント ',
 	'info_nb_profils' => '@nb@ 個のプロファイル',
 	'info_nb_profils_comptes' => 'このプロファイルのアカウント', # RELIRE
+	'info_profils_auteur' => 'この著者のプロファイル', # RELIRE
 
 	// N
+	'notification_motdepasse_texte' => '@nom_site_spip@ (@adresse_site@)
+このサイトにあなたの新規アカウントが作成されました。
+
+次のアドレスにアクセスしてください :
+
+    @sendcookie@
+
+パスワードを設定し、メールアドレス@email@でアクセス可能になります。
+
+',
 	'notification_motdepasse_titre' => '新規のアカウント',
 
 	// R
@@ -56,11 +92,14 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// T
 	'texte_ajouter_profil' => 'プロファイルを追加する',
 	'texte_changer_statut_profil' => 'このプロファイルは :',
+	'texte_creer_associer_profil' => 'プロファイルを作成しひもづける',
 	'texte_definir_comme_traduction_profil' => 'このプロファイルは次のプロファイル番号の翻訳です :',
 	'titre_langue_profil' => 'このプロファイルの使用言語',
 	'titre_logo_profil' => 'このプロファイルのロゴ',
+	'titre_objets_lies_profil' => 'このプロファイルに関連した',
 	'titre_profil' => 'プロファイル',
 	'titre_profils' => 'プロファイル',
+	'titre_profils_rubrique' => '見出しのプロファイル', # RELIRE
 	'tri_croissant' => '昇順に並べる',
 	'tri_decroissant' => '降順に並べる'
 );
