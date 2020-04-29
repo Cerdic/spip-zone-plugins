7dd9bd9ca024f014cda9b61344c0ca92128e4f10
Salvatore <Salvatore@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-04-29 23:27:30 +0000

    [Salvatore] [source:lang/ profils] Export depuis https://trad.spip.net de la langue ja [Salvatore] [source:lang/ profils] Mise a jour du bilan depuis https://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@124431 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ profils/trunk/lang/profils_ja.php
@@ -0,0 +1,24 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/profils?lang_cible=ja
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'configurer_email_unique_explication' => 'SPIPは既に他のアカウントで使われているメールアドレスでの新規登録をブロックします。このオプションは、プロファイルを更新する際、既にほかで使われているメールアドレスへあとから変更することも禁止します。',
+	'configurer_email_unique_label_case' => '複数のアカウントで同じメールアドレスの使用を禁止する',
+	'configurer_id_profil_defaut_label' => 'デフォールトのプロファイル',
+	'configurer_titre' => 'プロファイルの設定',
+
+	// E
+	'erreur_autoriser_profil' => 'このアカウントは存在しないか、修正する権限がありません',
+	'erreur_email_unique' => 'このアドレスは他のアカウントで既に使用されています。他のアドレスを使用してください。',
+
+	// P
+	'profils_titre' => 'プロファイル'
+);
