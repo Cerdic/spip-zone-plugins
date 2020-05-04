46001834443b848090b3c062185f43635b55f5e1
clem <clem@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-05-04 09:11:45 +0000

    Branch '/branches/post_set' created from /trunk:124146
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@124496 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ crayons/branches/post_set/controleurs/article_intro3.php
@@ -0,0 +1,38 @@
+<?php
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+// un controleur qui n'utilise que php et les inputs défauts
+function controleurs_article_intro3_dist($regs) {
+	list(,$crayon,$type,$champ,$id) = $regs;
+	$valeur = valeur_colonne_table($type, array('descriptif', 'chapo', 'texte'), $id);
+	if ($valeur === false) {
+		return array("$type $id $champ: " . _U('crayons:pas_de_valeur'), 6);
+	}
+
+	$n = new Crayon('article-intro3-' . $id, $valeur, array('hauteurMini' => 234));
+
+	$return = array(
+		// html
+		$n->formulaire(
+			// champs et attributs propres
+			array(
+			'descriptif' => array('type' => 'texte', 'attrs' => array(
+				'style' => 'height:' . ceil($n->hauteur*2/13) . 'px;' .
+							'width:' . $n->largeur . 'px;')),
+			'chapo' =>  array('type' => 'texte', 'attrs' => array(
+				'style' => 'height:' . ceil($n->hauteur*4/13) . 'px;' .
+							'width:' . $n->largeur . 'px;')),
+			'texte' =>  array('type' => 'texte', 'attrs' => array(
+				'style' => 'height:' . ceil($n->hauteur*4/13) . 'px;' .
+							'width:' . $n->largeur . 'px;'))) //,
+			// attributs communs :( marche pas pour style , pas 2 fois ?
+			// array('style' => 'width:' . $n->largeur . 'px;')
+		),
+		// status
+		null);
+
+	return $return;
+}
