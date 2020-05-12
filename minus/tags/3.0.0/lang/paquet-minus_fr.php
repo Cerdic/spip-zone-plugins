e1e381345370076ecefdef70c94cb5477a2d38fc
Maïeul Rouquette <Maeul Rouquette@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-05-12 08:41:31 +0000

    Tag '/tags/3.0.0' created from /trunk:124636
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@124637 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ minus/tags/3.0.0/lang/paquet-minus_fr.php
@@ -0,0 +1,19 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+// Fichier produit par PlugOnet
+// Module: paquet-minus
+// Langue: fr
+// Date: 12-05-2020 10:38:06
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// M
+	'minus_description' => 'Permet de forcer les rédacteurs et rédactrices à ne pas mettre des titres entièrement en majuscules.',
+	'minus_slogan' => 'Ne criez pas !',
+);
+?>
