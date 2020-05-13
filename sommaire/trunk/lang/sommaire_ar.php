ccfbca09f5888af9c195b6c95b7a671d44c439d7
Salvatore <Salvatore@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-05-13 23:27:40 +0000

    [Salvatore] [source:lang/ sommaire] Export depuis https://trad.spip.net de la langue ar [Salvatore] [source:lang/ sommaire] Mise a jour du bilan depuis https://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@124687 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ sommaire/trunk/lang/sommaire_ar.php
@@ -0,0 +1,46 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/sommaire?lang_cible=ar
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cfg_exemple' => 'مثال',
+	'cfg_exemple_explication' => 'تفسير المثال',
+	'cfg_titre_parametrages' => 'إعدادات',
+
+	// E
+	'explication_niveau_max' => 'الحد الأقصى لعمق الفهرس. يمكن تغيير هذه القيمة حسب الطلب باستخدام علامة <code>#SOMMAIRE{#TEXTE,2}</code> في الصفحات النموذجية او <code>&lt;sommaire|niveau_max=2&gt;</code> في نص المقالات.',
+	'explication_numerotation_sommaire' => 'يظهر الفهرس كقائمة نقطية او قائمة مرقمة.',
+	'explication_sommaire_automatique_off' => 'الفهرس مدرج في الصفحة النموذجية من قبل المشرف على الموقع.',
+	'explication_sommaire_automatique_ondemand' => 'لا يتم إدراج الفهرس الا بوجود المختصر <tt>&lt;sommaire&gt;</tt> في نص المقالات.',
+
+	// L
+	'label_sommaire_automatique' => 'إدراج الفهرس في المقال',
+	'label_sommaire_automatique_numerote' => 'نوع قائمة الفهرس',
+	'label_sommaire_automatique_numerote_off' => 'قائمة نقطية',
+	'label_sommaire_automatique_numerote_on' => 'قائمة مرقمة',
+	'label_sommaire_automatique_off' => 'عدم إدراج في نص المقالات',
+	'label_sommaire_automatique_on' => 'إدراج آلي في كل المقالات',
+	'label_sommaire_automatique_ondemand' => 'إدراج في نص المقالات حسب الطلب',
+	'label_sommaire_niveau_max' => 'العمق',
+	'label_sommaire_niveau_max_1' => 'مستوى واحد',
+	'label_sommaire_niveau_max_2' => 'مستويان',
+	'label_sommaire_niveau_max_3' => '٣ مستويات',
+	'label_sommaire_niveau_max_4' => '٤ مستويات',
+	'label_sommaire_niveau_max_5' => '٥ مستويات',
+	'label_sommaire_niveau_max_6' => '٦ مستويات',
+
+	// S
+	'sommaire_titre' => 'فهرس آلي',
+
+	// T
+	'titre_cadre_sommaire' => 'الفهرس',
+	'titre_page_configurer_sommaire' => 'فهرس آلي',
+	'titre_retour_sommaire' => 'عودة الى الفهرس'
+);
