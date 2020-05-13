14d06d40ac9ff402cc8ef0e0897606ba57d6b78b
Salvatore <Salvatore@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-05-13 23:27:50 +0000

    [Salvatore] [source:lang/ paquet-sommaire] Export depuis https://trad.spip.net de la langue ar [Salvatore] [source:lang/ paquet-sommaire] Mise a jour du bilan depuis https://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@124688 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ sommaire/trunk/lang/paquet-sommaire_ar.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-sommaire?lang_cible=ar
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// S
+	'sommaire_description' => 'إنشاء فهرس آلي للمقالاتز',
+	'sommaire_nom' => 'فهرس آلي',
+	'sommaire_slogan' => 'فهرس لمقالاتكم'
+);
