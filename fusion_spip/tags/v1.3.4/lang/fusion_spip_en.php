2f65515eb43f2c69fb9fee2b553b123230e4c976
nicod_ <nicod_@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-05-18 18:54:04 +0000

    Tag '/tags/v1.3.4' created from /trunk:124752
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@124753 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ fusion_spip/tags/v1.3.4/lang/fusion_spip_en.php
@@ -0,0 +1,59 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/fusion_spip?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'base' => 'Source site',
+	'base_desc' => 'The source site database must be<a href=""/ecrire/?exec=admin_tech">declared as an external database</a>.<br/>
+	The base of the source site must be in the same version as the host site',
+	'bouton_importer' => 'Start to merge',
+	'bouton_supprimer' => 'Delete the merge',
+
+	// C
+	'confirme_traduire_documents_doublons' => 'Translate duplicate documents using <multi> tags.',
+	'confirme_warning' => 'Confirm the merge of the databases?',
+
+	// D
+	'dossier_existe_pas' => 'The directory @dossier@ doesn’t exist',
+	'dossier_pas_lisible' => 'The directory @dossier@ cannot be read',
+
+	// E
+	'erreur_img_accessible' => 'Your IMG directory is not accessible. It is impossible either to write a file to the root, or to create a subdirectory.',
+	'erreur_traduction_document' => 'Your databases are in different languages, ticking the following box, you can translate the contents of duplicate documents using <multi> tags.',
+	'erreur_versions' => 'The host site and the source site are not in the same database version:
+		<br/>- host is in version @vhote@
+		<br/>- source is in version @vsource@',
+	'erreur_versions_impossible' => 'It is not possible to check the imported database (spip_meta table )',
+
+	// I
+	'img_dir' => 'Physical path of the documents',
+	'img_dir_desc' => 'To copy the documents from the source site in the host site, indicate their physical path (absolute path on the hard disk, for example <code>/home/edgard/www/edgard_spip/IMG</code>). If the field is empty, no document will be imported, you will have to copy them manually.',
+
+	// M
+	'maj_base' => 'Update of the database',
+	'manque_champs_source' => 'The fields "@diff@" are missing in the table "@table@" of the source database',
+	'manque_table_source' => 'The table "@table@" is missing in the source database',
+	'message_img_dir_nok' => 'Please precise the path',
+	'message_import_nok' => 'Error during the merge',
+	'message_import_ok' => 'Fusion ended<br>detailed log: <code>tmp/log/fusion_spip_fusion_spip*.log</code><br><br>Here is a summary of the imported objects:<br>',
+	'message_suppression_ok' => 'Deleted objects',
+
+	// R
+	'referers' => 'Do not process referrers (inbound links)',
+
+	// S
+	'secteur' => 'Sector',
+	'secteur_desc' => 'To import the source site  in a sector, otherwise it will be imported at the root',
+	'stats' => 'Do not process statistics',
+
+	// T
+	'titre_fusion_spip' => 'Spip Websites Fusion',
+	'titre_fusion_spip_suppression' => 'Deletion'
+);
---
+++ fusion_spip/tags/v1.3.4/lang/fusion_spip_fr.php
@@ -0,0 +1,59 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans https://git.spip.net/spip-contrib-extensions/fusion_spip.git
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'base' => 'Site source',
+	'base_desc' => 'La base de données du site source doit être <a href="/ecrire/?exec=admin_tech">déclarée comme base externe</a>.<br/>
+	La base du site source doit être dans la même version que celle du site hôte',
+	'bouton_importer' => 'Démarrer la fusion',
+	'bouton_supprimer' => 'Supprimer la fusion',
+
+	// C
+	'confirme_traduire_documents_doublons' => 'Traduire les documents en doublons en utilisant les champs multis',
+	'confirme_warning' => 'Confirmer la fusion des bases ?',
+
+	// D
+	'dossier_existe_pas' => 'Le répertoire @dossier@ n’existe pas',
+	'dossier_pas_lisible' => 'Le répertoire @dossier@ n’est pas accessible en lecture',
+
+	// E
+	'erreur_img_accessible' => 'Votre répertoire IMG n’est pas accessible. Il est impossible soit d’y écrire un fichier à la racine, soit d’y créer un sous-répertoire.',
+	'erreur_traduction_document' => 'Vos bases sont dans des langues différentes, en cochant la case suivante, vous pouvez traduire le contenu des documents en doublon en utilisant les champs multis.',
+	'erreur_versions' => 'Le site hôte et le site source ne sont pas dans la même version de base de données :
+		<br/>- hôte est en version : @vhote@
+		<br/>- source est en version : @vsource@',
+	'erreur_versions_impossible' => 'Impossible de vérifier la version de la base de données importée (table spip_meta introuvable)',
+	'erreur_version_indeterminee' => 'indéterminée (clé version_installee de la table spip_meta introuvable)',
+	'erreur_source_inaccessible' => 'Erreur de lecture du répertoire IMG de la source',
+
+	// I
+	'img_dir' => 'Chemin physique des documents',
+	'img_dir_desc' => 'Pour copier les documents du site source dans le site hôte, indiquez leur chemin physique (chemin absolu sur le disque dur, par exemple <code>/home/edgard/www/edgard_spip/IMG</code>). Si le champ est vide, aucun document ne sera importé, vous devrez les copier manuellemnent.',
+
+	// M
+	'maj_base' => 'Mise à jour de la base de données',
+	'manque_champs_source' => 'Les champs "@diff@" manquent dans la table "@table@" de la base source',
+	'manque_table_source' => 'La table "@table@" est absente dans la base source',
+	'message_img_dir_nok' => 'Merci de préciser le chemin',
+	'message_import_nok' => 'Erreur lors de la fusion',
+	'message_import_ok' => 'Fusion terminée<br>Log détaillé : <code>tmp/log/fusion_spip_fusion_spip*.log</code><br><br>Voici un résumé des objets importés :<br>',
+	'message_suppression_ok' => 'Objets supprimés',
+
+	// R
+	'referers' => 'Ne pas traiter les referers (liens entrants)',
+
+	// S
+	'secteur' => 'Secteur',
+	'secteur_desc' => 'Pour importer le site source dans un secteur, sinon il sera importé à la racine',
+	'stats' => 'Ne pas traiter les statistiques',
+
+	// T
+	'titre_fusion_spip' => 'Fusion de sites Spip',
+	'titre_fusion_spip_suppression' => 'Suppression'
+);
