eff74656013e540fd2569f2bdeb15d00fae72f50
RealET <RealET@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-05-18 11:31:40 +0000

    Pouvoir avoir un crayon sur un lien
    Refs #5 : résout https://git.spip.net/spip-contrib-extensions/crayons/issues/5
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@124735 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ crayons/branches/crayonsurlien/lang/crayons_it_fem.php
@@ -0,0 +1,41 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/crayons?lang_cible=it_fem
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'annuler' => 'Annulla',
+
+	// D
+	'deja_modifie' => 'Già modificato',
+	'donnees_mal_formatees' => 'Dati mal formattati',
+
+	// E
+	'editer' => 'Modifica',
+	'editer_@type@_@id@' => 'Modifica @type@ @id@ in intero',
+	'editer_tout' => 'Modifica tutto',
+
+	// M
+	'modifie_par_ailleurs' => 'Modificato altrove',
+
+	// N
+	'non_autorise' => 'Non autorizzato',
+	'non_implemente' => 'Non implementato',
+
+	// P
+	'pas_de_modification' => 'Nessuna modifica',
+	'pas_de_valeur' => 'Nessun valore',
+
+	// S
+	'sauvegarder' => 'Registrare le modifiche?',
+	'svp_copier_coller' => 'Spiacente, copia/incolla le modifiche e comincia di nuovo',
+
+	// V
+	'veuillez_patienter' => 'Attendere...'
+);
