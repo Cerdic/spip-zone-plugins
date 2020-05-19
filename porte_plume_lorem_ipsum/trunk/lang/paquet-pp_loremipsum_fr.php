ed840b09444866bd380c4599420e64a09e0d0813
jeanmarie <jeanmarie@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-05-19 13:48:50 +0000

    Fichier de langu pour le plugin
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@124762 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ porte_plume_lorem_ipsum/trunk/lang/paquet-pp_loremipsum_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// H
+	'pp_loremipsum_description' => 'Cette extension ajoute des boutons pour créer du texte factice dans la barre d\'outil de SPIP.',
+	'pp_loremipsum_nom' => 'Lorem Ipsum pour Porte Plume',
+	'pp_loremipsum_slogan' => 'Des boutons pour créer du texte factice',
+);
