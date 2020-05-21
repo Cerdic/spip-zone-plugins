e421bb07ff1f4058f869c33273fa1825f9a0484d
Jacques Pyrat <Jacques Pyrat@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-05-21 10:52:24 +0000

    Tag '/tags/v4.4.3' created from /trunk:124772
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@124773 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ formidable/tags/v4.4.3/lang/paquet-formidable_nl.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-formidable?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// F
+	'formidable_description' => 'Formulieren maken met behulp van een grafische interface en configuratie van gekoppelde verwerkingen (per email verzenden, antwoorden registreren, enz).',
+	'formidable_slogan' => 'Formulieren genereren'
+);
