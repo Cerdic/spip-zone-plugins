cee65961209fd5054c673d6880b48b3dfe9fc079
Matthieu Marcillaud <Matthieu Marcillaud@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-05-27 07:55:22 +0000

    Un formulaire pour indiquer où charger le script de chart.js
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@124828 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- chartjs/trunk/chartjs_pipelines.php
+++ chartjs/trunk/chartjs_pipelines.php
@@ -7,15 +7,30 @@
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 /**
- * Inserer la CSS de chartjs
+ * Inserer la CSS de chartjs dans le privé
  *
  * @param $flux
  * @return mixed
  */
-function chartjs_insert_head_css($flux){
+function chartjs_header_prive_css($flux){
+	return chartjs_insert_head_css($flux, 'prive');
+}
+
+function chartjs_header_prive($flux){
+	return chartjs_insert_head($flux, 'prive');
+}
+
+/**
+ * Inserer la CSS de chartjs dans le public
+ *
+ * @param $flux
+ * @return mixed
+ */
+function chartjs_insert_head_css($flux, $espace = 'public') {
   $flux .= '<link rel="stylesheet" type="text/css" href="'.find_in_path("css/chartjs.css").'" />';
   return $flux;
 }
+
 
 /**
  * Inserer le javascript de chartjs
@@ -23,9 +38,13 @@ function chartjs_insert_head_css($flux){
  * @param $flux
  * @return mixed
  */
-function chartjs_insert_head($flux){
-	# Todo: moment.js est nécessaire pour gérer correctement les graphiques avec des dates.
-	#$flux .=  "<script type='text/javascript' src='" . find_in_path('lib/moment/moment-with-locales.min.js') . "'></script>";
-	$flux .=  "<script type='text/javascript' src='" . find_in_path('lib/chartjs/Chart.js') . "'></script>";
+function chartjs_insert_head($flux, $espace = 'public') {
+	include_spip('inc/config');
+	$charger = lire_config('chartjs/charger_scripts');
+	if ($charger and in_array($espace, $charger)) {
+		# Todo: moment.js est nécessaire pour gérer correctement les graphiques avec des dates.
+		#$flux .=  "<script type='text/javascript' src='" . find_in_path('lib/moment/moment-with-locales.min.js') . "'></script>";
+		$flux .=  "<script type='text/javascript' src='" . find_in_path('lib/chartjs/Chart.js') . "'></script>";
+	}
 	return $flux;
 }
--- chartjs/trunk/lang/chartjs_fr.php
+++ chartjs/trunk/lang/chartjs_fr.php
@@ -5,19 +5,29 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
  
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-		// D
-		'demo_titre' => 'Demonstration du plugin Chart.js',
-		'demo_pie' => 'Camembert',
-		'demo_doughnut' => 'Beignet',
-		'demo_polaire' => 'Polaire',
-		'demo_bar' => 'Barre',
-		'demo_line' => 'Courbe',
-		'demo_radar' => 'Radar',
-		'demo_bubble' => 'Bulles',
-		'demo_type' => 'Type: @type@',
-		'demo_spip_data' => 'Graphes dynamiques avec boucle SPIP',
-		'demo_spip_bigdata' => 'Graphes dynamiques avec boucle SPIP DATAS',
-		'demo_spip_legend' => 'Cours du Yen en Eur des 30 derniers jours',
-		'demo_titre_types' => 'Types de graphiques',
-		'demo_titre_exemples_avances' => 'Exemples avancés',
+	// C
+	'champ_charger_scripts_label' => 'Chargement des scripts de Chart.js',
+	'champ_charger_scripts_explication' => 'Charger systématiquement les scripts Chart.js ? Par défaut, ils sont chargés uniquement lors de l’utilisation du modèle <code>chart</code>. Pour des usages plus spécifiques, vous pouvez les charger systématiquement sur toutes les pages.',
+	'champ_charger_scripts_public_case' => 'Charger Chart.js systématiquement dans l’espace public',
+	'champ_charger_scripts_prive_case' => 'Charger Chart.js systématiquement dans l’espace privé',
+
+	// D
+	'demo_titre' => 'Demonstration du plugin Chart.js',
+	'demo_pie' => 'Camembert',
+	'demo_doughnut' => 'Beignet',
+	'demo_polaire' => 'Polaire',
+	'demo_bar' => 'Barre',
+	'demo_line' => 'Courbe',
+	'demo_radar' => 'Radar',
+	'demo_bubble' => 'Bulles',
+	'demo_type' => 'Type: @type@',
+	'demo_spip_data' => 'Graphes dynamiques avec boucle SPIP',
+	'demo_spip_bigdata' => 'Graphes dynamiques avec boucle SPIP DATAS',
+	'demo_spip_legend' => 'Cours du Yen en Eur des 30 derniers jours',
+	'demo_titre_types' => 'Types de graphiques',
+	'demo_titre_exemples_avances' => 'Exemples avancés',
+
+	// T
+	'titre_formulaire_configurer_chartjs' => 'Configuration',
+	'titre_page_configurer_chartjs' => 'Configurer Chart.js',
 );
