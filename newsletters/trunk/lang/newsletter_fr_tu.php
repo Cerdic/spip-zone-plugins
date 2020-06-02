026eabfab2978bc3fa72dce870133089cd45477c
Salvatore <Salvatore@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-06-02 11:30:05 +0000

    [Salvatore] [source:lang/ newsletter] Export depuis https://trad.spip.net de la langue fr_tu
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@124918 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ newsletters/trunk/lang/newsletter_fr_tu.php
@@ -0,0 +1,78 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/newsletter-newsletters?lang_cible=fr_tu
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'ajouter_lien_newsletter' => 'Ajouter cette infolettre',
+
+	// E
+	'explication_baked' => 'Édition avancée : tu peux modifier directement le HTML et le texte de l’infolettre. Elle ne sera plus générée automatiquement. Si tu repasses en édition normale tes modifications seront perdues.',
+	'explication_baked_publie' => 'Cette infolettre est figée : tu peux corriger son HTML ou son texte mais tu ne peux plus modifier son éditorial ni la regénérer à partir de son modèle.',
+	'explication_html_page_vide' => 'Permet d’afficher dans le navigateur une version différente de celle envoyée par email (pour des raisons techniques ou éditoriales). Laisse-le vide si tu veux afficher la même version dans l’email et dans le navigateur.',
+
+	// I
+	'icone_creer_newsletter' => 'Créer une infolettre',
+	'icone_dupliquer_newsletter' => 'Dupliquer cette infolettre',
+	'icone_modifier_newsletter' => 'Modifier cette infolettre',
+	'info_1_mailsubscriber' => '1 inscrit',
+	'info_1_newsletter' => '1 infolettre',
+	'info_1_newsletter_prog' => '1 infolettre programmée',
+	'info_aucun_newsletter' => 'Aucune infolettre',
+	'info_email_inscrit_newletter' => 'Tu es inscrit pour recevoir des nouvelles par mail de :',
+	'info_email_not_displaying' => 'Cet email ne s’affiche pas correctement ?',
+	'info_email_reception' => 'Cet email a été envoyé à :',
+	'info_email_voir_en_ligne' => 'Voir cette Infolettre dans ton navigateur',
+	'info_nb_mailsubscribers' => '@nb@ inscrits',
+	'info_nb_newsletters' => '@nb@ infolettres',
+	'info_nb_newsletters_prog' => '@nb@ infolettres programmées',
+	'info_newsletter_generer' => 'Générer l’infolettre (Version HTML et version texte)',
+	'info_newsletters_auteur' => 'Les infolettres de cet auteur',
+	'info_optionnel' => '(optionnel)',
+	'info_preview_version_html_email' => 'Version HTML email',
+	'info_preview_version_html_page' => 'Version HTML en ligne',
+	'info_preview_version_html_page_noiframe' => '(afficher sans iframe)',
+	'info_preview_version_texte_email' => 'Version Texte email',
+	'info_publier_pour_envoyer' => 'Publie cette infolettre pour l’envoyer à une liste',
+	'info_test_sujet' => 'TEST',
+
+	// L
+	'label_a_lire_sur' => 'À lire sur @site@', # MODIF
+	'label_baked_1' => 'Édition avancée de l’infolettre', # MODIF
+	'label_chapo' => 'Chapeau',
+	'label_date_redac' => 'Date de rédaction',
+	'label_html_email' => 'Version HTML',
+	'label_html_page' => 'Version HTML affichée dans le navigateur',
+	'label_inscrit_newsletter' => 'Tu es inscrit pour recevoir des nouvelles par mail de @site@',
+	'label_masquer_fond' => 'Masquer les modèles d’infolettre :',
+	'label_patron' => 'Modèle',
+	'label_publie' => 'Publié le :',
+	'label_selection_articles' => 'Sélection d’articles', # MODIF
+	'label_selection_edito' => 'Sélection éditoriale (articles&rubriques)', # MODIF
+	'label_selection_rubriques' => 'Sélection de rubriques', # MODIF
+	'label_texte' => 'Texte',
+	'label_texte_email' => 'Version Texte',
+	'label_titre' => 'Titre',
+	'legend_configuration_adresse_envoi' => 'Adresse d’envoi pour les envois de cette infolettre ',
+	'lien_lire_en_ligne' => 'Lire en ligne',
+
+	// R
+	'retirer_lien_newsletter' => 'Retirer cette infolettre',
+	'retirer_tous_liens_newsletters' => 'Retirer toutes les infolettres',
+
+	// T
+	'texte_ajouter_newsletter' => 'Ajouter une infolettre',
+	'texte_changer_statut_newsletter' => 'Cette infolettre est :',
+	'texte_creer_associer_newsletter' => 'Créer et associer une infolettre',
+	'titre_langue_newsletter' => 'Langue de cette infolettre',
+	'titre_logo_newsletter' => 'Logo de cette infolettre',
+	'titre_newsletter' => 'Infolettre',
+	'titre_newsletters' => 'Infolettres',
+	'titre_newsletters_rubrique' => 'Infolettres de la rubrique'
+);
