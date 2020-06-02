19ea7ac0330311056401453196eac8e83ef2b586
Salvatore <Salvatore@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-06-02 11:40:14 +0000

    [Salvatore] [source:lang/ paquet-trad_rub] Export depuis https://trad.spip.net de la langue fr_tu [Salvatore] [source:lang/ paquet-trad_rub] Mise a jour du bilan depuis https://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@124920 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- traduction_rubriques_autrement/trunk/lang/paquet-trad_rub_fr_tu.php
+++ traduction_rubriques_autrement/trunk/lang/paquet-trad_rub_fr_tu.php
@@ -10,7 +10,7 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// T
-	'trad_rub_description' => 'Ce plugin, destiné pour les sites multilingues avec [un secteur par langue->https://contrib.spip.net/Un-exemple-de-site-multilingue], t’offre une approche similaire au niveau de la navigation et de la gestion de traductions des rubriques à celle du plugin [Traduction articles autrement->https://files.spip.org/spip-zone/traduction_articles_autrement.zip] dont l’installation est recommandé. Comme spip ne propose pas nativement la traduction des rubriques l’installation du plugin [Traduction entre rubrique->https://files.spip.org/spip-zone/tradrub.zip] est nécessaire.', # MODIF
+	'trad_rub_description' => 'Ce plugin, destiné pour les sites multilingues avec [un secteur par langue->https://contrib.spip.net/Un-exemple-de-site-multilingue], t’offre une approche similaire au niveau de la navigation et de la gestion de traductions des rubriques à celle du plugin [Traduction articles autrement->https://files.spip.org/spip-zone/traduction_articles_autrement.zip] dont l’installation est recommandé. Comme spip ne propose pas nativement la traduction des rubriques l’installation du plugin [Traduction entre rubrique->https://files.spip.org/spip-zone/tradrub.zip] est nécessaire.',
 	'trad_rub_nom' => 'Traductions de rubriques autrement',
 	'trad_rub_slogan' => 'Naviguer entre les rubriques traduites.'
 );
