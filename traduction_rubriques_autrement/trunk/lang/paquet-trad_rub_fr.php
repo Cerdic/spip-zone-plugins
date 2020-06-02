34241b6b3912782d7d712ad0368a1459dad95644
RealET <RealET@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-06-02 11:00:45 +0000

    Orthographe, bis
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@124915 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- traduction_rubriques_autrement/trunk/lang/paquet-trad_rub_fr.php
+++ traduction_rubriques_autrement/trunk/lang/paquet-trad_rub_fr.php
@@ -8,7 +8,7 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// T
-	'trad_rub_description' => 'Ce plugin, destiné pour les sites mutltilingues avec [un secteur par langue->https://contrib.spip.net/Un-exemple-de-site-multilingue], vous offre une approche similaire au niveau de la navigation et de la gestion de traductions des rubriques à celle du plugin [Traduction articles autrement->https://files.spip.net/spip-zone/traduction_articles_autrement.zip] dont l’installation est recommandé. Comme spip ne propose pas nativement la traduction des rubriques l’installation du plugin [Traduction entre rubrique->https://files.spip.net/spip-zone/tradrub.zip] est nécessaire.',
+	'trad_rub_description' => 'Ce plugin, destiné pour les sites multilingues avec [un secteur par langue->https://contrib.spip.net/Un-exemple-de-site-multilingue], vous offre une approche similaire au niveau de la navigation et de la gestion de traductions des rubriques à celle du plugin [Traduction articles autrement->https://files.spip.net/spip-zone/traduction_articles_autrement.zip] dont l’installation est recommandé. Comme spip ne propose pas nativement la traduction des rubriques l’installation du plugin [Traduction entre rubrique->https://files.spip.net/spip-zone/tradrub.zip] est nécessaire.',
 	'trad_rub_nom' => 'Traductions de rubriques autrement',
 	'trad_rub_slogan' => 'Naviguer entre les rubriques traduites.'
 );
