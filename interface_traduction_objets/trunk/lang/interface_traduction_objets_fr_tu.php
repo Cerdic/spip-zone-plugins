db3eb5bdb6c4c8594a8a0f228b885bc3c956440d
Salvatore <Salvatore@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-06-02 10:50:14 +0000

    [Salvatore] [source:lang/ interface_traduction_objets] Export depuis https://trad.spip.net de la langue fr_tu [Salvatore] [source:lang/ interface_traduction_objets] Mise a jour du bilan depuis https://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@124907 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- interface_traduction_objets/trunk/lang/interface_traduction_objets_fr_tu.php
+++ interface_traduction_objets/trunk/lang/interface_traduction_objets_fr_tu.php
@@ -9,10 +9,16 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
+	// I
+	'info_desactiver_interface_traduction' => 'Désactiver l’interface de traduction',
+	'info_desactiver_liste_compacte' => 'Désactiver les listes compactes',
+	'interface_traduction_objets_titre' => 'Interface de traduction pour objets',
+
 	// O
 	'options' => 'Modifier les options de langues',
 
 	// T
+	'titre_page_configurer_interface_traduction_objets' => 'Configuration',
 	'traductions' => 'Trad',
 
 	// V
