126f4a677a855e06bc5863dcabe8223be0752044
Maïeul Rouquette <Maeul Rouquette@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-06-06 14:47:20 +0000

    issue #32 champ date_soumission : déclaration + installation + remplissage avec la date actuelle si on est sur une ancienne version
    On profère l'appelation date_soumission à date_reponse car date_reponse
    est peu clair, puisque c'est un champ d'un objet _reponse.
    date_soumission permet de dire : "là date à laquelle la réponse a été
    soumise"
    
    oups
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@125016 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ formidable/branches/issue32/lang/formulaire_fr.php
@@ -0,0 +1,20 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans https://git.spip.net/spip-contrib-extensions/formidable.git
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// I
+	'icone_creer_formulaire' => 'Créer un nouveau formulaire',
+
+	// M
+	'modifier_formulaire' => 'Modifier ce formulaire',
+
+	// T
+	'titre_formulaire' => 'Formulaire',
+	'titre_formulaires' => 'Formulaires',
+	'titre_logo_formulaire' => 'Logo de ce formulaire'
+);
