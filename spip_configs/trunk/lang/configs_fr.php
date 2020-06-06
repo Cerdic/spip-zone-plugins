187cce8a1b306cf9e036fe528c4d8176d92bb1b5
clem <clem@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-06-06 09:41:12 +0000

    ajout de l'accès dans le menu Configuration. Mise à la bonne taille des pictos 16/32/64/128
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@125008 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- spip_configs/trunk/lang/configs_fr.php
+++ spip_configs/trunk/lang/configs_fr.php
@@ -12,6 +12,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'cfg_exemple' => 'Exemple',
 	'cfg_exemple_explication' => 'Explication de cet exemple',
 	'cfg_titre_parametrages' => 'Paramétrages',
+	'configuration_configs' => 'SPIP configs',
 	
 	// H
 	'howto_edition' => 'Pour permettre l\'édition des configs il faut activer le plugin Crayons 
