5e44c32edaea14516da8560a6f22b66223c8b411
Maïeul Rouquette <Maeul Rouquette@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-06-12 13:48:50 +0000

    Les logs peuvent contenir : - des dump de tableaux PHP : dans ce cas il faut conserver les indentations et autres - du json : dans ce cas il ne faut pas que les `{ }` soit compris comme de l'italique.
    Pour permettre tout cela, et puisqu'au niveau du squelette #TEXTE est
    automatiquement passé par `propre()` :
    - on évite de trimer les lignes;
    - on envellope dans `<code>`;
    - on s'assure de la cohérence du markup au niveau des ul/li en utilisant
    les raccourcis SPIP plutôt qu'en mettant des `<ul><li>` en dur.
    
    Commite vu avec @eric qui m'a dit "vas y et on verra les cas
    problématique après" :)
    
    Close #1
    
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@125161 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- simplog/trunk/prive/squelettes/contenu/simplog_fonctions.php
+++ simplog/trunk/prive/squelettes/contenu/simplog_fonctions.php
@@ -28,33 +28,37 @@ function simplog_phraser_ligne($contenu) {
 			preg_match(_SIMPLOG_LIGNE, $_ligne, $matches);
 			if (!isset($matches[1])	or !$matches[1]) {
 				// Ce n'est pas une nouvelle ligne mais la suite du texte de la ligne en cours
-				$tableau[count($tableau) - 1]['texte'] .= "\n" . trim($_ligne);
+				$tableau[count($tableau) - 1]['texte'] .= "\n" . $_ligne;
 			} else {
 				$ligne['date'] = date('Y-m-d H:i:s', strtotime($matches[1]));
 				$ligne['ip'] = trim($matches[2]);
 				$ligne['pid'] = trim($matches[3]);
 				$ligne['hit'] = _T('simplog:info_hit_' . strtolower(trim($matches[7])));
 				$ligne['gravite'] = strtolower(trim($matches[8]));
-				$ligne['texte'] = contruire_texte(trim($matches[9]), trim($matches[4]), trim($matches[5]), trim($matches[6]));
+				$ligne['texte'] = simplelog_contruire_texte($matches[9], trim($matches[4]), trim($matches[5]), trim($matches[6]));
 				$ligne['index'] = count($tableau);
 				$tableau[] = $ligne;
 			}
 		}
 	}
-
+	$tableau = array_map('simplelog_finir_markup_texte',$tableau);
 	return $tableau;
 }
 
-function contruire_texte($message, $fichier, $ligne, $fonction) {
+function simplelog_finir_markup_texte($ligne) {
+	$ligne['texte'] = unicode2charset($ligne['texte']."\n\r</code>");
+	return $ligne;
+}
+
+function simplelog_contruire_texte($message, $fichier, $ligne, $fonction) {
 	$label_fichier = _T('simplog:label_fichier');
 	$label_ligne = _T('simplog:label_ligne');
 	$label_fonction = _T('simplog:label_fonction');
-	$texte = '<ul>'
-			 . ($fichier ? "<li><em>$label_fichier</em> : $fichier</li>" : '')
-			 . ($ligne ? "<li><em>$label_ligne</em> : $ligne</li>" : '')
-			 . ($fonction ? "<li><em>$label_fonction</em> : $fonction</li>" : '')
-			 . ($message ? "<li>$message</li>" : '')
-			 . '</ul>';
+	$texte = ''
+		 . ($fichier ? "-*<em>$label_fichier</em> : $fichier" : '')
+		 . ($ligne ? "-*<em>$label_ligne</em> : $ligne" : '')
+		 . ($fonction ? "-*<em>$label_fonction</em> : $fonction" : '')
+		 . ($message ? "<code>\n\r$message" : '<code>');
 
 	return $texte;
 }
