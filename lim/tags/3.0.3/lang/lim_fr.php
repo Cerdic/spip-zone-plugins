b8823f23e9c52f61e42959cb40ac40091277ca07
Akilia <Akilia@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-06-12 06:51:17 +0000

    Tag '/tags/3.0.3' created from /trunk:125145
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@125146 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ lim/tags/3.0.3/lang/lim_fr.php
@@ -0,0 +1,44 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+ }
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// L
+	'lim_titre' => 'Lim',
+
+	// C
+	'cfg_exemple' => 'Exemple',
+	'cfg_exemple_explication' => 'Explication de cet exemple',
+	'cfg_titre_forums_publics' => 'Forums Publics',
+	'cfg_titre_afficher_logos' => 'Désactiver les logos par contenu',
+	'cfg_titre_divers' => 'Divers',
+	'cfg_titre_champs_auteur' => 'Formulaire Auteur',
+	'cfg_titre_restriction_par_rubrique' => 'Restreindre les contenus par rubrique',
+	'cfg_select_objets' => 'Selectionner les contenus',
+	
+	
+
+	'info_creer_dans_rubrique_non_autorise' => 'Vous ne pouvez pas créer dans cette rubrique',
+	'info_deplacer_dans_rubrique_non_autorise' => 'Vous ne pouvez pas déplacer dans cette rubrique',
+
+	// L
+	'label_activer_logo_objets' => 'Cocher les contenus où désactiver le formulaire Logo',
+	'label_desactiver_article_par_rubrique' => "Cocher les rubriques où désactiver le bouton ",
+	'label_forums_publics' => "Désactiver l'affichage du bloc Forum dans les pages Articles ?",
+	'label_desactive_auteur_bio' => "Désactiver l'affichage du champ <i>Qui est l’auteur</i> ?",
+	'label_desactive_auteur_email' => "Désactiver l'affichage du champ <i>Adresse email</i> ?",
+	'label_desactive_auteur_liens_sites' => "Désactiver l'affichage du fieldset <i>Lien Site</i> ?",
+	'label_desactive_auteur_pgp' => "Désactiver l'affichage du champ <i>Clé PGP</i> ?",
+
+	// O
+	'onglet_lim' => 'Forums Publics et divers',
+	'onglet_lim_logo' => 'Logos',
+	'onglet_lim_rubriques' => 'Contenus par rubrique',
+
+	// T
+	'titre_page_configurer_lim' => 'LIM : Alléger l\'espace privé',
+);
---
+++ lim/tags/3.0.3/lang/paquet-lim_fr.php
@@ -0,0 +1,22 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// L
+	'lim_description' => '&nbsp;<br>3 parties sont proposées pour allèger le back-office:
+
+	- <b>Forums publics</b> : désactiver l\'affichage de ce bloc dans les pages articles,
+
+	- <b>Divers</b> : désactiver l\'affichage de certains champs (Bio, PGP,etc.) dans le formulaire auteur,
+
+	- <b>Logos</b> : si vous utiliser les logos, vous pourrez alors désactiver leur affichage au cas par cas (articles, auteurs, mots-clés, etc.),
+
+	- <b>Contenus par rubriques</b> : Choisir les rubriques où désactiver l\'édition d\'articles, brèves, sites référencés et autres objets éditoriaux.',
+	'lim_nom' => 'Lim',
+	'lim_slogan' => 'Alléger l\'espace privé de blocs et boutons inutiles dans votre projet',
+);
