0f540719fbb6dfbc23c487a8f984102d179e3597
Salvatore <Salvatore@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-06-16 02:55:02 +0000

    [Salvatore] [source:lang/ paquet-gis] Export depuis https://trad.spip.net de la langue ja [Salvatore] [source:lang/ paquet-gis] Mise a jour du bilan depuis https://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@125217 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ gis/trunk/lang/paquet-gis_ja.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-gis?lang_cible=ja
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// G
+	'gis_description' => 'このプラグインで、地理的に配置されたポイントを作成します。ポイントを、SPIPオブジェクトに添付すると、ページの地図上に表示することになります。
+リーフレットライブラリのおかげで、さまざまなサプライヤーの地図の背景を使用することができます。',
+	'gis_slogan' => '地理情報システム'
+);
