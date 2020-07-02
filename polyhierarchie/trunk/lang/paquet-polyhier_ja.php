93c9b2275ce82b88aff9ec2be46647e0bb444fdd
Salvatore <Salvatore@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2020-07-02 02:30:02 +0000

    [Salvatore] [source:lang/ paquet-polyhier] Export depuis https://trad.spip.net de la langue ja [Salvatore] [source:lang/ paquet-polyhier] Mise a jour du bilan depuis https://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_plugins_@125340 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ polyhierarchie/trunk/lang/paquet-polyhier_ja.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-polyhier?lang_cible=ja
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// P
+	'polyhier_description' => 'セクションや記事を直接の親以外のセクションに付けることで、クロスリンクを作成することができるプラグインです。',
+	'polyhier_slogan' => '複数の親のセクションに記事や他のセクションを付けること。'
+);
